702498ca092ed5a04e0eb68ca28051dc2b1d49d5
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-01-15 21:25:24 -0800

    Bug fixes for Wink

--- homeassistant/components/switch/wink.py
+++ homeassistant/components/switch/wink.py
@@ -4,8 +4,8 @@ import logging
 # pylint: disable=no-name-in-module, import-error
 import homeassistant.external.wink.pywink as pywink
 
-from homeassistant.helpers import ToggleDevice
-from homeassistant.const import ATTR_FRIENDLY_NAME, CONF_ACCESS_TOKEN
+from homeassistant.components.wink import WinkToggleDevice
+from homeassistant.const import CONF_ACCESS_TOKEN
 
 
 # pylint: disable=unused-argument
@@ -32,32 +32,4 @@ def devices_discovered(hass, config, info):
 
 def get_switches():
     """ Returns the Wink switches. """
-    return [WinkSwitch(switch) for switch in pywink.get_switches()]
-
-
-class WinkSwitch(ToggleDevice):
-    """ represents a WeMo switch within home assistant. """
-
-    def __init__(self, wink):
-        self.wink = wink
-        self.state_attr = {ATTR_FRIENDLY_NAME: wink.name()}
-
-    def get_name(self):
-        """ Returns the name of the switch if any. """
-        return self.wink.name()
-
-    def turn_on(self, **kwargs):
-        """ Turns the switch on. """
-        self.wink.setState(True)
-
-    def turn_off(self):
-        """ Turns the switch off. """
-        self.wink.setState(False)
-
-    def is_on(self):
-        """ True if switch is on. """
-        return self.wink.state()
-
-    def get_state_attributes(self):
-        """ Returns optional state attributes. """
-        return self.state_attr
+    return [WinkToggleDevice(switch) for switch in pywink.get_switches()]
--- homeassistant/components/wink.py
+++ homeassistant/components/wink.py
@@ -8,10 +8,10 @@ import homeassistant.external.wink.pywink as pywink
 
 from homeassistant import bootstrap
 from homeassistant.loader import get_component
-from homeassistant.helpers import validate_config
+from homeassistant.helpers import validate_config, ToggleDevice
 from homeassistant.const import (
-    EVENT_PLATFORM_DISCOVERED, ATTR_SERVICE, ATTR_DISCOVERED,
-    CONF_ACCESS_TOKEN)
+    EVENT_PLATFORM_DISCOVERED, CONF_ACCESS_TOKEN,
+    ATTR_SERVICE, ATTR_DISCOVERED, ATTR_FRIENDLY_NAME)
 
 DOMAIN = "wink"
 DEPENDENCIES = []
@@ -48,3 +48,44 @@ def setup(hass, config):
             })
 
     return True
+
+
+class WinkToggleDevice(ToggleDevice):
+    """ represents a WeMo switch within home assistant. """
+
+    def __init__(self, wink):
+        self.wink = wink
+
+    @property
+    def unique_id(self):
+        """ Returns the id of this WeMo switch """
+        return "{}.{}".format(self.__class__, self.wink.deviceId())
+
+    @property
+    def name(self):
+        """ Returns the name of the light if any. """
+        return self.wink.name()
+
+    @property
+    def is_on(self):
+        """ True if light is on. """
+        return self.wink.state()
+
+    @property
+    def state_attributes(self):
+        """ Returns optional state attributes. """
+        return {
+            ATTR_FRIENDLY_NAME: self.wink.name()
+        }
+
+    def turn_on(self, **kwargs):
+        """ Turns the switch on. """
+        self.wink.setState(True)
+
+    def turn_off(self):
+        """ Turns the switch off. """
+        self.wink.setState(False)
+
+    def update(self):
+        """ Update state of the light. """
+        self.wink.wait_till_desired_reached()
