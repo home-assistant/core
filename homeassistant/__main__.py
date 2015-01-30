6878fc254a3bf65d69e395af8cf9f6b7bf6d72ad
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-01-30 08:28:04 -0800

    Add frontend to default config

--- homeassistant/__main__.py
+++ homeassistant/__main__.py
@@ -78,7 +78,7 @@ def ensure_config_path(config_dir):
     if not os.path.isfile(config_path):
         try:
             with open(config_path, 'w') as conf:
-                conf.write("[http]\n\n")
+                conf.write("[frontend]\n\n")
                 conf.write("[discovery]\n\n")
         except IOError:
             print(('Fatal Error: No configuration file found and unable '
