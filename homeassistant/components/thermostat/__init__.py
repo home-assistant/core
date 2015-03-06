68668fc658eb1292da9e754782d90c3e0bd44fa2
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-03-05 23:17:05 -0800

    Light/Switch/Thermostat: use new extract_from_service helper

--- homeassistant/components/thermostat/__init__.py
+++ homeassistant/components/thermostat/__init__.py
@@ -56,18 +56,11 @@ def setup(hass, config):
     component = DeviceComponent(_LOGGER, DOMAIN, hass, SCAN_INTERVAL)
     component.setup(config)
 
-    thermostats = component.devices
-
     def thermostat_service(service):
         """ Handles calls to the services. """
 
         # Convert the entity ids to valid light ids
-        target_thermostats = [thermostats[entity_id] for entity_id
-                              in extract_entity_ids(hass, service)
-                              if entity_id in thermostats]
-
-        if not target_thermostats:
-            target_thermostats = thermostats.values()
+        target_thermostats = component.extract_from_service(service)
 
         if service.service == SERVICE_SET_AWAY_MODE:
             away_mode = service.data.get(ATTR_AWAY_MODE)
