72b930af8fbb115d037e900a48d5a7c1c728c672
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-03-05 22:53:11 -0800

    Fix: Hue reports state correctly if 2 services called quickly

--- homeassistant/components/light/hue.py
+++ homeassistant/components/light/hue.py
@@ -13,7 +13,7 @@ from homeassistant.components.light import (
     ATTR_FLASH, FLASH_LONG, FLASH_SHORT)
 
 MIN_TIME_BETWEEN_SCANS = timedelta(seconds=10)
-MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(seconds=1)
+MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(milliseconds=100)
 
 PHUE_CONFIG_FILE = "phue.conf"
 
