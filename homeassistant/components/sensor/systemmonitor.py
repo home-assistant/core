828c78cb1f5404e536a3d05955f82b0b184b8cd8
theolind <theodor.lindquist@gmail.com>
2015-03-07 15:49:58 +0100

    fixed style errors

--- homeassistant/components/sensor/systemmonitor.py
+++ homeassistant/components/sensor/systemmonitor.py
@@ -12,7 +12,7 @@ from homeassistant.const import (
 import psutil
 import logging
 
-sensor_types = {
+SENSOR_TYPES = {
     'disk_use_percent': ['Disk Use', '%'],
     'disk_use': ['Disk Use', 'GiB'],
     'disk_free': ['Disk Free', 'GiB'],
@@ -24,31 +24,32 @@ sensor_types = {
 
 _LOGGER = logging.getLogger(__name__)
 
+
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """ Sets up the sensors """
 
-    devices = []
+    dev = []
     for resource in config['resources']:
         if 'arg' not in resource:
             resource['arg'] = ''
-        if resource['type'] not in sensor_types:
+        if resource['type'] not in SENSOR_TYPES:
             _LOGGER.error('Sensor type: "%s" does not exist', resource['type'])
         else:
-            devices.append(SystemMonitorSensor(resource['type'], resource['arg']))
+            dev.append(SystemMonitorSensor(resource['type'], resource['arg']))
 
-    add_devices(devices)
+    add_devices(dev)
 
 
 class SystemMonitorSensor(Device):
     """ A system monitor sensor """
 
     def __init__(self, type, argument=''):
-        self._name = sensor_types[type][0] + ' ' + argument
+        self._name = SENSOR_TYPES[type][0] + ' ' + argument
         self.argument = argument
         self.type = type
         self._state = None
-        self.unit_of_measurement = sensor_types[type][1]
+        self.unit_of_measurement = SENSOR_TYPES[type][1]
         self.update()
 
     @property
