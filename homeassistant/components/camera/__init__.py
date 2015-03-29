da94b9354f0c04edeeff6b107ddf4b924faaa600
jamespcole <jamespcole@gmail.com>
2015-03-29 19:34:43 +1100

    refactored the IP camera code

--- homeassistant/components/camera/__init__.py
+++ homeassistant/components/camera/__init__.py
@@ -6,143 +6,64 @@ Component to interface with various cameras that can be monitored.
 import mimetypes
 import requests
 import logging
+from homeassistant.helpers.entity import Entity
+import time
 from datetime import timedelta
 import re
-
-#import Image
-
-from homeassistant.loader import get_component
-import homeassistant.util as util
 from homeassistant.const import (
-    STATE_OPEN)
-from homeassistant.helpers import (
-    platform_devices_from_config)
-from homeassistant.components import group, discovery, wink
-from homeassistant.helpers import Device
+    HTTP_NOT_FOUND)
 
-from homeassistant.helpers import (
-    generate_entity_id, extract_entity_ids, config_per_platform)
+
+from homeassistant.helpers.entity_component import EntityComponent
+
 
 DOMAIN = 'camera'
-DEPENDENCIES = []
-
+DEPENDENCIES = ['http']
 GROUP_NAME_ALL_CAMERAS = 'all_cameras'
-ENTITY_ID_ALL_CAMERAS = group.ENTITY_ID_FORMAT.format(
-    GROUP_NAME_ALL_CAMERAS)
-
+SCAN_INTERVAL = 30
 ENTITY_ID_FORMAT = DOMAIN + '.{}'
 
-MIN_TIME_BETWEEN_SCANS = timedelta(seconds=1)
-
 # Maps discovered services to their platforms
-DISCOVERY_PLATFORMS = {
-}
-
-_LOGGER = logging.getLogger(__name__)
-
-CAMERA_HTTP_PROXY_REGISTERED = False
-
-def is_on(hass, entity_id=None):
-    """ Returns if the camera is open based on the statemachine. """
-    entity_id = entity_id or ENTITY_ID_ALL_CAMERAS
-
-    return hass.states.is_state(entity_id, STATE_OPEN)
-
+DISCOVERY_PLATFORMS = {}
 
 
 def setup(hass, config):
-    """ Track states and offer events for cameras. """
-    logger = logging.getLogger(__name__)
+    """ Track states and offer events for sensors. """
 
     
 
-    # cameras = platform_devices_from_config(
-    #     config, DOMAIN, hass, ENTITY_ID_FORMAT, logger)    
-    
-    cameras = {}
+    component = EntityComponent(
+        logging.getLogger(__name__), DOMAIN, hass, SCAN_INTERVAL,
+        DISCOVERY_PLATFORMS)
 
-    # Track all cameras in a group
-    camera_group = group.Group(
-        hass, GROUP_NAME_ALL_CAMERAS, cameras.keys(), False)
+    component.setup(config)
 
-
-
-
-    def add_cameras(new_cameras):
-        """ Add cameras to the component to track. """
-        for camera in new_cameras:
-            if camera is not None and camera not in cameras.values():
-
-                camera.hass = hass
-                camera.entity_id = generate_entity_id(
-                    ENTITY_ID_FORMAT, camera.name, cameras.keys())
-
-                cameras[camera.entity_id] = camera
-
-                camera.update_ha_state()
-
-        camera_group.update_tracked_entity_ids(cameras.keys())
-
-    for p_type, p_config in config_per_platform(config, DOMAIN, _LOGGER):
-        platform = get_component(ENTITY_ID_FORMAT.format(p_type))
-
-        if platform is None:
-            _LOGGER.error("Unknown type specified: %s", p_type)
-
-    platform.setup_platform(hass, p_config, add_cameras)
-
-    @util.Throttle(MIN_TIME_BETWEEN_SCANS)
-    def update_camera_states(now):
-        """ Update states of all cameras. """
-        global CAMERA_HTTP_PROXY_REGISTERED
-
-        if CAMERA_HTTP_PROXY_REGISTERED is not True:
-            # Register the handler for the camera image proxy
-            if 'http' not in hass.components:
-                _LOGGER.error('Dependency http is not loaded')
-            else: 
-                CAMERA_HTTP_PROXY_REGISTERED = True
-                hass.http.register_path('GET', re.compile(r'/api/camera_proxy/(?P<entity_id>[a-zA-Z\._0-9]+)'), _proxy_camera_image)
-                _LOGGER.info("registered camera proxy http endpoint")
-
-        if cameras:
-            logger.info("Updating camera states")
-
-            for camera in cameras.values():
-                camera.update_ha_state()
-    
 
     def _proxy_camera_image(handler, path_match, data):
         """ Proxies the camera image via the HA server. """
         entity_id = path_match.group('entity_id')
-        
-        camera = cameras.get(entity_id)
-        response = camera.get_camera_image()
-        handler.wfile.write(response.content)
 
-    #check if we can register the http path yet
-    
-    if 'http' not in hass.components:
-        _LOGGER.error('Dependency http is not loaded')
-    else: 
-        CAMERA_HTTP_PROXY_REGISTERED = True
-        hass.http.register_path('GET', re.compile(r'/api/camera_proxy/(?P<entity_id>[a-zA-Z\._0-9]+)'), _proxy_camera_image)
-        _LOGGER.info("registered camera proxy http endpoint")
+        camera = None
+        if entity_id in component.entities.keys():
+            camera = component.entities[entity_id]
 
-    update_camera_states(None)
+        if camera:
+            response = camera.get_camera_image()
+            handler.wfile.write(response.content)
+        else:
+            handler.send_response(HTTP_NOT_FOUND)
 
-    # Fire every 10 seconds
-    hass.track_time_change(update_camera_states, second=range(0, 60, 10))
+    hass.http.register_path('GET', re.compile(r'/api/camera_proxy/(?P<entity_id>[a-zA-Z\._0-9]+)'), _proxy_camera_image, require_auth=True)
 
     return True
 
 
-
-class Camera(Device):
+class Camera(Entity):
     """ Base class for cameras. """
 
     def __init__(self, hass, device_info):
         #super().__init__(hass, device_info)
+        self.hass = hass
         self.device_info = device_info
         self.BASE_URL = device_info.get('base_url')
         if not self.BASE_URL.endswith('/'):
@@ -164,9 +85,11 @@ class Camera(Device):
 
     @property
     def state_attributes(self):
-        attr = super().state_attributes       
-        attr['entity_picture'] = '/api/camera_proxy/' + self.entity_id
         """ Returns optional state attributes. """
+        attr = super().state_attributes
+        attr['still_image_url'] = '/api/camera_proxy/' + self.entity_id
+        attr['entity_picture'] = '/api/camera_proxy/' + self.entity_id + '?api_password=' + self.hass.http.api_password + '&time=' + str(time.time())
+        
         return attr
 
     @property
--- homeassistant/components/camera/dlink.py
+++ homeassistant/components/camera/dlink.py
@@ -1,7 +1,7 @@
-import mimetypes
-import requests
 import logging
-
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
+from homeassistant.helpers import validate_config
+from homeassistant.components.camera import DOMAIN
 from homeassistant.components.camera import Camera
 
 _LOGGER = logging.getLogger(__name__)
@@ -9,27 +9,13 @@ _LOGGER = logging.getLogger(__name__)
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """ Find and return Vera lights. """
-    try:
-        # base_url = config.get('vera_controller_url')
-        # if not base_url:
-        #     _LOGGER.error("The required parameter 'vera_controller_url' was not found in config")
-        #     return False
+    try:        
+        if not validate_config({DOMAIN: config},
+                           {DOMAIN: ['base_url', CONF_USERNAME, CONF_PASSWORD]},
+                           _LOGGER):
+            return None
 
-        # device_data_str = config.get('device_data')        
-        # device_data = None
-        # if device_data_str:
-        #     try:
-        #         device_data = json.loads(device_data_str)
-        #     except Exception as json_ex:
-        #         _LOGGER.error('Vera lights error parsing device info, should be in the format [{"id" : 12, "name": "Lounge Light"}]: %s', json_ex)
-
-        # controller = veraApi.VeraController(base_url)
-        # devices = controller.get_devices('Switch')
-
-        cameras = [DlinkCamera(hass, '', '', ''), DlinkCamera(hass, '', '', '')]
-        # for device in devices:
-        #     if is_switch_a_light(device_data, device.deviceId):
-        #         lights.append(VeraLight(device, get_extra_device_data(device_data, device.deviceId)))
+        cameras = [DlinkCamera(hass, config)]
 
         add_devices_callback(cameras)
     except Exception as inst:
