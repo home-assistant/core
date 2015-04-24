5e4173c7f31cb1a3c695c1dc6cf506f4132eb4fb
jamespcole <jamespcole@gmail.com>
2015-04-25 03:31:27 +1000

    Added the mjpeg stream instead of static image loading

--- homeassistant/components/camera/__init__.py
+++ homeassistant/components/camera/__init__.py
@@ -3,6 +3,7 @@ homeassistant.components.camera
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Component to interface with various cameras that can be monitored.
 """
+import urllib3
 import mimetypes
 import requests
 import logging
@@ -57,115 +58,44 @@ def setup(hass, config):
     hass.http.register_path('GET', re.compile(r'/api/camera_proxy/(?P<entity_id>[a-zA-Z\._0-9]+)'), _proxy_camera_image, require_auth=True)
 
 
-
-
-    def request_headers():
-        return {
-            'Cache-Control': 'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0',
-            'Connection': 'close',
-            'Content-Type': 'multipart/x-mixed-replace;boundary=%s' % boundary,
-            'Expires': 'Mon, 3 Jan 2000 12:34:56 GMT',
-            'Pragma': 'no-cache',
-        }
-
-    def image_headers(length):
-        return {
-            'X-Timestamp': time.time(),
-            'Content-Length': length,
-            # FIXME: mime-type must be set according file content
-            'Content-Type': 'image/jpeg',
-        }
-
     def _proxy_camera_mjpeg_stream(handler, path_match, data):
         """ Proxies the camera image via the HA server. """
         entity_id = path_match.group('entity_id')
-        print('test')
-
-        boundary = 'boundarydonotcross'
-
-        image_headers = {
-            'X-Timestamp': time.time(),
-            'Content-Length': 0,
-            # FIXME: mime-type must be set according file content
-            'Content-Type': 'image/jpeg',
-        }
-
-        request_headers = {
-            'Cache-Control': 'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0',
-            'Connection': 'close',
-            'Content-Type': 'multipart/x-mixed-replace; boundary=%s' % boundary,
-            'Expires': 'Mon, 3 Jan 2000 12:34:56 GMT',
-            'Pragma': 'no-cache',
-            #'Content-type': 'image/jpeg'
-        }
 
         camera = None
         if entity_id in component.entities.keys():
             camera = component.entities[entity_id]
 
         if camera:
+            try:
+                camera.is_streaming = True
+                camera.update_ha_state()
 
-            handler.send_response(200)
-            # response = camera.get_camera_image()
-            # handler.wfile.write(response.content)
+                http = urllib3.PoolManager()
+                handler.request.sendall(bytes('HTTP/1.1 200 OK\r\n', 'utf-8'))
+                handler.request.sendall(bytes('Content-type: multipart/x-mixed-replace; boundary=--jpgboundary\r\n\r\n', 'utf-8'))
+                handler.request.sendall(bytes('--jpgboundary\r\n', 'utf-8'))
+                count = 0
+                while True:
 
-            # for k, v in request_headers.items():
-            #     #print(k, v)
-            #     handler.send_header(k, v)
+                    headers = urllib3.util.make_headers(basic_auth=camera.username + ':' + camera.password)
+                    req = http.request('GET', camera.still_image_url, headers = headers)
 
+                    headersStr = ''
+                    headersStr = headersStr + 'Content-length: ' + str(len(req.data)) + '\r\n'
+                    headersStr = headersStr + 'Content-type: image/jpeg\r\n'
+                    headersStr = headersStr + '\r\n'
 
-            handler.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=%s' % boundary)
-            handler.end_headers()
-
-            #handler.end_headers()
-            #handler.wfile.write(bytes('\n', 'ascii'))
-
-            for idx in range(0,100):
-                #print('test')
-
-                # Response headers (multipart)
-
-                # handler.wfile.write(bytes('\r\n', 'ascii'))
-                # handler.wfile.write(bytes(boundary, 'ascii'))
-                # handler.wfile.write(bytes('\r\n', 'ascii'))
-
-                #handler.end_headers()
-
-                # handler.end_headers()
-                # # Part headers
-                response = camera.get_camera_image(True)
-                #print(response.encoding)
-                #image_headers['Content-Length'] = len(response.content)
-                image_headers = response.headers
-                # for k, v in image_headers.items():
-                #     print(k, v)
-                #     handler.send_header(k, v)
-                #handler.send_header('Content-type', 'image/jpeg')
-               #handler.wfile.write(bytes("Content-type: video/x-motion-jpeg", 'ascii'))
-                #handler.end_headers()
-                #handler.end_headers()
-                #handler.end_headers()
-
-                #for chunk in response.iter_content(1024):
-                # img_bytes = response.raw.read(20)
-                # while len(img_bytes) > 0:
-                #     handler.wfile.write(img_bytes)
-                #     img_bytes = response.raw.read(20)
-
-                #for chunk in response.iter_content(1024):
-                #    handler.wfile.write(chunk)
-                #handler.wfile.write(response.content)
-                #handler.end_headers()
-                handler.wfile.write(response.content)
-
-
-                #handler.wfile.write(bytes('\r\n', 'ascii'))
-
-
-                print(str.encode(boundary))
-
+                    handler.request.sendall(bytes(headersStr, 'utf-8') + req.data + bytes('\r\n', 'utf-8'))
+                    handler.request.sendall(bytes('--jpgboundary\r\n', 'utf-8'))
+            except Exception:
+                camera.is_streaming = False
+                camera.update_ha_state()
         else:
             handler.send_response(HTTP_NOT_FOUND)
+
+        camera.is_streaming = False
+        camera.update_ha_state()
 
     hass.http.register_path('GET', re.compile(r'/api/camera_proxy_stream/(?P<entity_id>[a-zA-Z\._0-9]+)'), _proxy_camera_mjpeg_stream, require_auth=True)
     return True
@@ -185,6 +115,8 @@ class Camera(Entity):
             self.BASE_URL = self.BASE_URL + '/'
         self.username = device_info.get('username')
         self.password = device_info.get('password')
+        self.is_streaming = False
+
 
     def get_camera_image(self, stream=False):
         response = requests.get(self.still_image_url, auth=(self.username, self.password), stream=stream)
@@ -197,6 +129,15 @@ class Camera(Entity):
         else:
             return super().name
 
+    @property
+    def state(self):
+        """ Returns the state of the entity. """
+        if self.is_streaming:
+            return "Streaming"
+        else:
+            return "Idle"
+
+
 
     @property
     def state_attributes(self):
@@ -206,6 +147,7 @@ class Camera(Entity):
         attr['brand'] = self.device_info.get('brand', 'generic')
         attr['still_image_url'] = '/api/camera_proxy/' + self.entity_id
         attr[ATTR_ENTITY_PICTURE] = '/api/camera_proxy/' + self.entity_id + '?api_password=' + self.hass.http.api_password + '&time=' + str(time.time())
+        attr['stream_url'] = '/api/camera_proxy_stream/' + self.entity_id
 
         return attr
 
