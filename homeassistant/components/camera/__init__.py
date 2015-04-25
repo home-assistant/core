fa69aa654ad1aa4c1e5850487e0776cfd2811dad
jamespcole <jamespcole@gmail.com>
2015-04-26 01:08:52 +1000

    lots of new features and refactoring

--- homeassistant/components/camera/__init__.py
+++ homeassistant/components/camera/__init__.py
@@ -7,13 +7,18 @@ import urllib3
 import mimetypes
 import requests
 import logging
+import netifaces
 from homeassistant.helpers.entity import Entity
 import time
 from datetime import timedelta
 import re
 from homeassistant.const import (
     ATTR_ENTITY_PICTURE,
-    HTTP_NOT_FOUND)
+    HTTP_NOT_FOUND,
+    ATTR_ENTITY_ID,
+    SERVICE_TURN_ON,
+    SERVICE_TURN_OFF
+    )
 
 
 from homeassistant.helpers.entity_component import EntityComponent
@@ -27,12 +32,11 @@ ENTITY_ID_FORMAT = DOMAIN + '.{}'
 
 # Maps discovered services to their platforms
 DISCOVERY_PLATFORMS = {}
+ATTR_FRIENDLY_LOG_MESSAGE = "friendly_log_message"
 
 
 def setup(hass, config):
     """ Track states and offer events for sensors. """
-
-
 
     component = EntityComponent(
         logging.getLogger(__name__), DOMAIN, hass, SCAN_INTERVAL,
@@ -67,9 +71,16 @@ def setup(hass, config):
             camera = component.entities[entity_id]
 
         if camera:
+            message = "{0} started streaming to {1}".format(camera.name, handler.address_string())
+            hass.bus.fire(
+                "camera_stream_started", {"component": DOMAIN,
+                ATTR_ENTITY_ID: entity_id,
+                ATTR_FRIENDLY_LOG_MESSAGE: message})
+
             try:
                 camera.is_streaming = True
                 camera.update_ha_state()
+
 
                 http = urllib3.PoolManager()
                 handler.request.sendall(bytes('HTTP/1.1 200 OK\r\n', 'utf-8'))
@@ -91,16 +102,41 @@ def setup(hass, config):
             except Exception:
                 camera.is_streaming = False
                 camera.update_ha_state()
+
+            message = "{0} stopped streaming to {1}".format(camera.name, handler.address_string())
+            hass.bus.fire(
+                "camera_stream_stopped", {"component": DOMAIN,
+                ATTR_ENTITY_ID: entity_id,
+                ATTR_FRIENDLY_LOG_MESSAGE: message})
+
         else:
             handler.send_response(HTTP_NOT_FOUND)
 
         camera.is_streaming = False
-        camera.update_ha_state()
 
     hass.http.register_path('GET', re.compile(r'/api/camera_proxy_stream/(?P<entity_id>[a-zA-Z\._0-9]+)'), _proxy_camera_mjpeg_stream, require_auth=True)
+
+    def handle_motion_detection_service(service):
+        """ Handles calls to the camera services. """
+        target_cameras = component.extract_from_service(service)
+        feature_name = service.data.get('feature', 'motion_detection')
+        for camera in target_cameras:
+            if feature_name == 'configure_ftp':
+                if service.service == SERVICE_TURN_ON:
+                    camera.set_ftp_details()
+            else:
+                if service.service == SERVICE_TURN_ON:
+                    camera.enable_motion_detection()
+                else:
+                    camera.disable_motion_detection()
+
+            camera.update_ha_state(True)
+
+    hass.services.register(DOMAIN, SERVICE_TURN_ON, handle_motion_detection_service)
+    hass.services.register(DOMAIN, SERVICE_TURN_OFF, handle_motion_detection_service)
+
     return True
 
-    #
 
 
 class Camera(Entity):
@@ -116,7 +152,22 @@ class Camera(Entity):
         self.username = device_info.get('username')
         self.password = device_info.get('password')
         self.is_streaming = False
+        # these are the camera functions and capabilities initialised
+        # to defaults, these should be overridden in derived classes
+        self._is_motion_detection_supported = False
+        self._is_motion_detection_enabled = False
 
+        self._is_ftp_upload_supported = False
+        self._is_ftp_upload_enabled = False
+
+        self._is_ftp_configured = False
+        self._ftp_host = ''
+        self._ftp_port = 21
+        self._ftp_username = ''
+        self._ftp_password = ''
+
+        # The address of HA on the local lan
+        self._ha_lan_address = self.get_ha_lan_address()
 
     def get_camera_image(self, stream=False):
         response = requests.get(self.still_image_url, auth=(self.username, self.password), stream=stream)
@@ -138,7 +189,6 @@ class Camera(Entity):
             return "Idle"
 
 
-
     @property
     def state_attributes(self):
         """ Returns optional state attributes. """
@@ -148,6 +198,12 @@ class Camera(Entity):
         attr['still_image_url'] = '/api/camera_proxy/' + self.entity_id
         attr[ATTR_ENTITY_PICTURE] = '/api/camera_proxy/' + self.entity_id + '?api_password=' + self.hass.http.api_password + '&time=' + str(time.time())
         attr['stream_url'] = '/api/camera_proxy_stream/' + self.entity_id
+        attr['ha_lan_address'] = self._ha_lan_address
+
+        attr.update(self.function_attributes)
+        # function_attrs = self.function_attributes
+        # for key in function_attrs.keys():
+        #     attr[key] = function_attrs[key]
 
         return attr
 
@@ -157,3 +213,62 @@ class Camera(Entity):
         if self.device_info.get('still_image_url'):
             return self.BASE_URL + self.device_info.get('still_image_url')
         return self.BASE_URL + 'image.jpg'
+
+    def enable_motion_detection(self):
+        if not self.is_motion_detection_supported:
+            return False
+        if self.is_motion_detection_enabled:
+            return True
+
+    def disable_motion_detection(self):
+        if not self.is_motion_detection_supported:
+            return False
+        if self.is_motion_detection_enabled:
+            return True
+
+    def set_ftp_details(self):
+        if not self.is_motion_detection_supported:
+            return False
+
+    @property
+    def is_motion_detection_supported(self):
+        return self._is_motion_detection_supported
+
+    @property
+    def is_motion_detection_enabled(self):
+        return self._is_motion_detection_enabled
+
+    @property
+    def is_ftp_upload_supported(self):
+        return self._is_ftp_upload_supported
+
+    @property
+    def is_ftp_upload_enabled(self):
+        return self._is_ftp_upload_enabled
+
+    @property
+    def is_ftp_configured(self):
+        return self._is_ftp_configured
+
+    @property
+    def function_attributes(self):
+        attr = {}
+        attr['is_motion_detection_supported'] = self.is_motion_detection_supported
+        attr['is_motion_detection_enabled'] = self.is_motion_detection_enabled
+        attr['is_ftp_upload_supported'] = self.is_ftp_upload_supported
+        attr['is_ftp_upload_enabled'] = self.is_ftp_upload_enabled
+        attr['is_ftp_configured'] = self.is_ftp_configured
+        return attr
+
+    def get_ha_lan_address(self):
+        interfaces = netifaces.interfaces()
+        for i in interfaces:
+            if i == 'lo':
+                continue
+            iface = netifaces.ifaddresses(i).get(netifaces.AF_INET)
+            if iface != None:
+                for j in iface:
+                    ip = j['addr']
+                    if not ip == None:
+                        return ip
+        return None
--- homeassistant/components/camera/dlink.py
+++ homeassistant/components/camera/dlink.py
@@ -1,15 +1,17 @@
 import logging
+import requests
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 from homeassistant.helpers import validate_config
 from homeassistant.components.camera import DOMAIN
 from homeassistant.components.camera import Camera
+from bs4 import BeautifulSoup
 
 _LOGGER = logging.getLogger(__name__)
 
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """ Find and return Vera lights. """
-    try:        
+    try:
         if not validate_config({DOMAIN: config},
                            {DOMAIN: ['base_url', CONF_USERNAME, CONF_PASSWORD]},
                            _LOGGER):
@@ -23,13 +25,197 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
         return False
 
 def get_camera(hass, device_info):
-    return DlinkCamera(hass, device_info) 
+    return DlinkCamera(hass, device_info)
+
+
+# class DlinkCamera(Camera):
+#     def __init__(self, hass, device_info):
+#         super().__init__(hass, device_info)
+
+#     @property
+#     def still_image_url(self):
+#         """ This should be implemented by different camera models. """
+#         return self.BASE_URL + 'image.jpg'
 
 class DlinkCamera(Camera):
     def __init__(self, hass, device_info):
         super().__init__(hass, device_info)
+        self._is_motion_detection_supported = True
+        self._is_ftp_upload_supported = True
+
+        # Holds the form data so we can post updates back to the web UI
+        self._web_ui_form_data = {}
+        self.get_all_settings()
 
     @property
     def still_image_url(self):
         """ This should be implemented by different camera models. """
         return self.BASE_URL + 'image.jpg'
+
+    def get_all_settings(self):
+        res = requests.get(self.BASE_URL + 'motion.htm', auth=(self.username, self.password))
+        motion_settings = BeautifulSoup(res.content)
+
+        print('----------------------------------------------------------------------------------')
+        settings = self.extract_form_fields(motion_settings)
+        self._is_motion_detection_enabled = True if settings.get('MotionDetectionEnable') == '1' else False
+        print(settings)
+        self._web_ui_form_data['motion'] = settings
+
+        print('motion detection ' + str(self._is_motion_detection_enabled))
+
+        res = requests.get(self.BASE_URL + 'upload.htm', auth=(self.username, self.password))
+        upload_settings = BeautifulSoup(res.content)
+
+        u_settings = self.extract_form_fields(upload_settings)
+        self._is_ftp_upload_enabled = True if u_settings.get('FTPScheduleEnable') == '1' else False
+
+        print(self._is_ftp_upload_enabled)
+
+        print(u_settings)
+
+        self._web_ui_form_data['upload'] = u_settings
+
+        self._is_ftp_configured = True if not u_settings.get('FTPHostAddress') == '' else False
+        self._ftp_host = u_settings.get('FTPHostAddress')
+        self._ftp_port = u_settings.get('FTPPortNumber')
+        self._ftp_username = u_settings.get('FTPUserName')
+        self._ftp_password = u_settings.get('FTPPassword')
+
+        print(self._is_ftp_configured)
+        print(self.get_ha_lan_address())
+
+    def enable_motion_detection(self):
+        can_enable = super().enable_motion_detection()
+        if can_enable == False:
+            return can_enable
+
+        self._web_ui_form_data['motion']['MotionDetectionEnable'] = 1
+        self._web_ui_form_data['motion']['MotionDetectionScheduleMode'] = 0
+        r = requests.post(self.BASE_URL + 'setSystemMotion', data=self._web_ui_form_data['motion'], auth=(self.username, self.password))
+
+        self.get_all_settings()
+        self.update_ha_state(True)
+
+        return True
+
+    def disable_motion_detection(self):
+        can_enable = super().disable_motion_detection()
+        if can_enable == False:
+            return can_enable
+
+        self._web_ui_form_data['motion']['MotionDetectionEnable'] = 0
+        r = requests.post(self.BASE_URL + 'setSystemMotion', data=self._web_ui_form_data['motion'], auth=(self.username, self.password))
+
+        self.get_all_settings()
+        self.update_ha_state(True)
+
+        return True
+
+    def set_ftp_details(self):
+        can_enable = super().set_ftp_details()
+        if can_enable == False:
+            return can_enable
+
+        self._web_ui_form_data['upload']['FTPHostAddress'] = self.get_ha_lan_address()
+        self._web_ui_form_data['upload']['FTPPortNumber'] = 21
+        self._web_ui_form_data['upload']['FTPUserName'] = 'test'
+        self._web_ui_form_data['upload']['FTPPassword'] = 'test'
+        self._web_ui_form_data['upload']['FTPDirectoryPath'] = '/'
+
+        self._web_ui_form_data['upload']['FTPScheduleEnable'] = '1'
+        self._web_ui_form_data['upload']['FTPScheduleMode'] = '2'
+
+        r = requests.post(self.BASE_URL + 'setSystemFTP', data=self._web_ui_form_data['upload'], auth=(self.username, self.password))
+
+        print(r.content)
+
+        self.get_all_settings()
+        self.update_ha_state(True)
+
+        return True
+
+
+    # From https://gist.github.com/simonw/104413
+    def extract_form_fields(self, soup, include_without_name=False):
+        "Turn a BeautifulSoup form in to a dict of fields and default values"
+        fields = {}
+        for input in soup.findAll('input'):
+            name = ''
+            # ignore submit/image with no name attribute
+            if input['type'] in ('submit', 'image') and not input.has_attr('name'):
+                continue
+
+            # print(input)
+            if input == None:
+                continue
+
+            if not input.has_attr('name') and not input.has_attr('id'):
+                continue
+
+            if not input.has_attr('name') and input.has_attr('id') and include_without_name:
+                name = input['id']
+            elif input.has_attr('name'):
+                name = input['name']
+            else:
+                continue
+
+
+
+            # single element nome/value fields
+            if input['type'] in ('text', 'hidden', 'password', 'submit', 'image'):
+                value = ''
+                if input.has_attr('value'):
+                    value = input['value']
+                fields[name] = value
+                continue
+
+            # checkboxes and radios
+            if input['type'] in ('checkbox', 'radio'):
+                value = ''
+                if input.has_attr('checked'):
+                    if input.has_attr('value'):
+                        value = input['value']
+                    else:
+                        value = 'on'
+                if name in fields.keys() and value:
+                    fields[name] = value
+
+                if not name in fields.keys():
+                    fields[name] = value
+
+                continue
+
+            # assert False, 'input type %s not supported' % input['type']
+
+        # textareas
+        for textarea in soup.findAll('textarea'):
+            fields[name] = textarea.string or ''
+
+        # select fields
+        for select in soup.findAll('select'):
+            if not select.has_attr('name'):
+                continue
+            value = ''
+            options = select.findAll('option')
+            is_multiple = select.has_attr('multiple')
+
+            selected_options = [
+                option for option in options
+                if option.has_attr('selected')
+            ]
+
+            # If no select options, go with the first one
+            if not selected_options and options:
+                selected_options = [options[0]]
+
+            if not is_multiple:
+                assert(len(selected_options) < 2)
+                if len(selected_options) == 1:
+                    value = selected_options[0].text if not selected_options[0].has_attr('value') else selected_options[0]['value']
+            else:
+                value = [option.text if not option.has_attr('value') else option['value'] for option in selected_options]
+
+            fields[select['name']] = value
+
+        return fields
