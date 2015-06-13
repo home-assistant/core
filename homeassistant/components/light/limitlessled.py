8655a62c8c0ada486ca45ddd8d519e15cde5db8a
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-06-13 16:42:09 -0700

    Refactor basic light structure

--- homeassistant/components/light/limitlessled.py
+++ homeassistant/components/light/limitlessled.py
@@ -23,9 +23,8 @@ light:
 """
 import logging
 
-from homeassistant.helpers.entity import ToggleEntity
-from homeassistant.const import STATE_ON, STATE_OFF, DEVICE_DEFAULT_NAME
-from homeassistant.components.light import ATTR_BRIGHTNESS
+from homeassistant.const import DEVICE_DEFAULT_NAME
+from homeassistant.components.light import Light, ATTR_BRIGHTNESS
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -43,18 +42,12 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     lights = []
     for i in range(1, 5):
         if 'group_%d_name' % (i) in config:
-            lights.append(
-                LimitlessLED(
-                    led,
-                    i,
-                    config['group_%d_name' % (i)]
-                )
-            )
+            lights.append(LimitlessLED(led, i, config['group_%d_name' % (i)]))
 
     add_devices_callback(lights)
 
 
-class LimitlessLED(ToggleEntity):
+class LimitlessLED(Light):
     """ Represents a LimitlessLED light """
 
     def __init__(self, led, group, name):
@@ -65,7 +58,7 @@ class LimitlessLED(ToggleEntity):
         self.led.off(self.group)
 
         self._name = name or DEVICE_DEFAULT_NAME
-        self._state = STATE_OFF
+        self._state = False
         self._brightness = 100
 
     @property
@@ -79,33 +72,26 @@ class LimitlessLED(ToggleEntity):
         return self._name
 
     @property
-    def state(self):
-        """ Returns the name of the device if any. """
-        return self._state
-
-    @property
-    def state_attributes(self):
-        """ Returns optional state attributes. """
-        if self.is_on:
-            return {
-                ATTR_BRIGHTNESS: self._brightness,
-            }
+    def brightness(self):
+        return self._brightness
 
     @property
     def is_on(self):
         """ True if device is on. """
-        return self._state == STATE_ON
+        return self._state
 
     def turn_on(self, **kwargs):
         """ Turn the device on. """
-        self._state = STATE_ON
+        self._state = True
 
         if ATTR_BRIGHTNESS in kwargs:
             self._brightness = kwargs[ATTR_BRIGHTNESS]
 
         self.led.set_brightness(self._brightness, self.group)
+        self.update_ha_state()
 
     def turn_off(self, **kwargs):
         """ Turn the device off. """
-        self._state = STATE_OFF
+        self._state = False
         self.led.off(self.group)
+        self.update_ha_state()
