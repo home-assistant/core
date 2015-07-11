eac2a54f45ea75a13812cb9d2c870b2ce3437782
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-07-10 23:17:26 -0700

    Add camera to demo

--- homeassistant/components/camera/generic.py
+++ homeassistant/components/camera/generic.py
@@ -46,7 +46,6 @@ the password for accessing your camera
 """
 import logging
 from requests.auth import HTTPBasicAuth
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 from homeassistant.helpers import validate_config
 from homeassistant.components.camera import DOMAIN
 from homeassistant.components.camera import Camera
@@ -58,7 +57,8 @@ _LOGGER = logging.getLogger(__name__)
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """ Adds a generic IP Camera. """
-    if not validate_config({DOMAIN: config}, {DOMAIN: ['still_image_url']}, _LOGGER):
+    if not validate_config({DOMAIN: config}, {DOMAIN: ['still_image_url']},
+                           _LOGGER):
         return None
 
     add_devices_callback([GenericCamera(config)])
@@ -75,15 +75,14 @@ class GenericCamera(Camera):
         self._name = device_info.get('name', 'Generic Camera')
         self._username = device_info.get('username')
         self._password = device_info.get('password')
-
-        self._still_image_url += device_info.get('still_image_url', 'image.jpg')
+        self._still_image_url = device_info['still_image_url']
 
     def camera_image(self):
         """ Return a still image reponse from the camera """
         if self._username and self._password:
             response = requests.get(
                 self._still_image_url,
-                auth=HTTPBasicAuth(self._username,self._password))
+                auth=HTTPBasicAuth(self._username, self._password))
         else:
             response = requests.get(self._still_image_url)
 
