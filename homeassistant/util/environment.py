dea0fcc84576cec389f93d666c2d8b3ceeb6812c
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-07-11 00:02:42 -0700

    Improve virtual env detection

--- homeassistant/util/environment.py
+++ homeassistant/util/environment.py
@@ -4,4 +4,5 @@ import sys
 
 def is_virtual():
     """ Return if we run in a virtual environtment. """
-    return sys.base_prefix != sys.prefix
+    # Check supports venv && virtualenv
+    return sys.base_prefix != sys.prefix or hasattr(sys, 'real_prefix')
