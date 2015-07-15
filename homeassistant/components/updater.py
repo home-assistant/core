b3e172b7361bf0367cf9532ef6a9f7443ab7de72
Ryan Kraus <rmkraus@gmail.com>
2015-07-14 21:28:08 -0400

    Applied suggested fixes to Updater component
    Applied the suggested fixes to the Updater component, recompiled the
    frontend, updated subproject home-assistant-polymer to latest master
    version.

--- homeassistant/components/updater.py
+++ homeassistant/components/updater.py
@@ -17,15 +17,14 @@ from homeassistant.helpers.entity import Entity
 # homeassistant constants
 DOMAIN = "updater"
 DEPENDENCIES = []
-REQUIREMENTS = ['requests>=2.0', 'gitpython>=1.0.1']
+REQUIREMENTS = ['gitpython>=1.0.1']
 CONF_PID_FILE = "pid_file"
 CONF_LOG_FILE = "log_file"
 CONF_REPO_NAME = "repo_name"
 CONF_REPO_BRANCH = "repo_branch"
-HA_SOURCE_DIR = os.path.abspath(os.path.join(homeassistant.__file__,
-                                             '..', '..'))
+HA_SOURCE_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(homeassistant.__file__), '..'))
 GH_API_CALL = 'https://api.github.com/repos/{repo}/commits?sha={branch}'
-SCAN_INTERVAL = 60 * 60 * 12
 
 # setup logger
 _LOGGER = logging.getLogger(__name__)
@@ -41,9 +40,36 @@ def setup(hass, config):
     repo_name = config['updater'].get(CONF_REPO_NAME, 'balloob/home-assistant')
     repo_branch = config['updater'].get(CONF_REPO_BRANCH, 'master')
 
+    # create updater service
+    def run_update(service_call):
+        ''' run the updater '''
+        upscript = os.path.join(HA_SOURCE_DIR, 'scripts', 'update')
+        mypid = str(os.getpid())
+        cmd = [upscript, HA_SOURCE_DIR, mypid]
+
+        if log_file is not None:
+            cmd.append(log_file)
+            if pid_file is not None:
+                cmd.append(pid_file)
+
+        subprocess.call(cmd)
+    hass.services.register(DOMAIN, 'update', run_update)
+
+    # find local copy info
+    try:
+        from git import Repo
+    except ImportError:
+        _LOGGER.error('Missing package gitpython')
+        current_sha = None
+    else:
+        repo = Repo(HA_SOURCE_DIR)
+        if repo.bare:
+            current_sha = None
+        else:
+            current_sha = repo.head.commit.hexsha
+
     # create component entity
-    Updater(hass, _LOGGER, repo_name=repo_name, branch=repo_branch,
-            pid_file=pid_file, log_file=log_file)
+    Updater(hass, current_sha, repo_name, repo_branch, pid_file, log_file)
 
     return True
 
@@ -55,34 +81,29 @@ class Updater(Entity):
     entity_id = 'updater.updater'
     name = 'Updater'
 
-    def __init__(self, hass, logger, **kwargs):
-        try:
-            from git import Repo
-        except ImportError:
-            logger.error('Missing package gitpython')
-            self._repo_class = None
-        else:
-            self._repo_class = Repo
-
+    def __init__(self, hass, current_sha, repo_name, branch, pid_file,
+                 log_file):
         self.hass = hass
-        self._logger = logger
+        self.config = {'repo_name': repo_name, 'branch': branch,
+                       'pid_file': pid_file, 'log_file': log_file}
 
-        self.config = kwargs
+        # register update check event
+        def check_ha_update(event):
+            """ Check GitHub for avaialable update """
+            self.update()
+        hass.track_time_change(check_ha_update, hour=0, minute=0, second=0)
 
-        hass.track_time_change(self.update, hour=0, minute=0, second=0)
-        self.hass.services.register(DOMAIN, 'update', self.run_update)
-
-        self.versions = {'newest_sha': None,
-                         'newest_msg': None,
-                         'newest_date': None,
-                         'newest_url': None,
-                         'current_sha': None}
+        # initialize data
+        self.versions = {'newest_sha': None, 'newest_msg': None,
+                         'newest_date': None, 'newest_url': None,
+                         'current_sha': current_sha}
 
         self.update()
 
-    def update(self, *args, **kwargs):
+    def update(self):
         ''' Update the state of the entity '''
         _LOGGER.info('Looking for updates.')
+
         # pull data from github
         github_resp = requests.get(
             GH_API_CALL.format(repo=self.config['repo_name'],
@@ -94,16 +115,8 @@ class Updater(Entity):
             self.versions['newest_date'] = \
                 github_data[0]['commit']['author']['date']
             self.versions['newest_url'] = github_data[0]['html_url']
-
-        # find local copy info
-        if self._repo_class is not None:
-            repo = self._repo_class(HA_SOURCE_DIR)
-            if repo.bare:
-                self.versions['current_sha'] = None
-            else:
-                self.versions['current_sha'] = repo.head.commit.hexsha
         else:
-            self.versions['current_sha'] = None
+            _LOGGER.warning('Error communicating with GitHub')
 
         # update with HA
         self.update_ha_state()
@@ -117,27 +130,13 @@ class Updater(Entity):
     @property
     def state(self):
         ''' Is an update available? '''
-        return not self.hidden
+        return 'up_to_date' if self.hidden else 'update_available'
 
     @property
     def state_attributes(self):
         ''' current entity state attributes '''
-        return {'hidden': self.hidden,
-                'local_sha': self.versions['current_sha'],
+        return {'local_sha': self.versions['current_sha'],
                 'remote_sha': self.versions['newest_sha'],
                 'message': self.versions['newest_msg'],
                 'date': self.versions['newest_date'],
                 'link': self.versions['newest_url']}
-
-    def run_update(self, *args, **kwargs):
-        ''' run the updater '''
-        upscript = os.path.join(HA_SOURCE_DIR, 'scripts', 'update')
-        mypid = str(os.getpid())
-        cmd = [upscript, HA_SOURCE_DIR, mypid]
-
-        if self.config['log_file'] is not None:
-            cmd.append(self.config['log_file'])
-            if self.config['pid_file'] is not None:
-                cmd.append(self.config['pid_file'])
-
-        subprocess.call(cmd)
