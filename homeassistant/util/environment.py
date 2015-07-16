955aa0e01f7a926be5843b9d53e600feda061150
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-07-15 18:37:24 -0700

    Increase robustness dependency installation

--- homeassistant/util/environment.py
+++ homeassistant/util/environment.py
@@ -5,4 +5,5 @@ import sys
 def is_virtual():
     """ Return if we run in a virtual environtment. """
     # Check supports venv && virtualenv
-    return sys.base_prefix != sys.prefix or hasattr(sys, 'real_prefix')
+    return (getattr(sys, 'base_prefix', sys.prefix) != sys.prefix or
+            hasattr(sys, 'real_prefix'))
