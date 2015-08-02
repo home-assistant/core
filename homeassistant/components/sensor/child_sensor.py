d264c4b36b76c0cccaad8aab89edab486b448ad6
jamespcole <james@inoutput.io>
2015-08-02 23:53:39 +1000

    Documentation for the new child_component

--- homeassistant/components/sensor/child_sensor.py
+++ homeassistant/components/sensor/child_sensor.py
@@ -2,6 +2,61 @@
 homeassistant.components.sensor.child_sensor
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+This component is designed to enable other components to create a sensor
+component to monitor one of its state attributes without the nned to create
+an entirely new sensor component.  This can be especially useful when
+integrating with device that has several states you wish to monitor, eg. many
+of my z-wave components are battery operated and it means a battery sensor can
+easily be created for the component.
+
+
+BASIC USAGE:
+
+In order to create child sensors you need to use the "discovery" mechanism to
+create multiple sensors on setup of another component.  The child sensor will
+subscribe to the state change event of the parent entity and will monitor a
+specified state attribute and use its value for its own state.
+
+To implement you should add the following to your component:
+
+from homeassistant.components.sensor import (
+    DISCOVER_CHILD_SENSORS)
+
+from homeassistant.const import (
+    EVENT_PLATFORM_DISCOVERED)
+
+...
+// put this is the component class or setup_platform function
+
+data = {}
+data['name'] = 'Battery Level'
+data['parent_entity_id'] = self.entity_id
+data['watched_variable'] = 'batterylevel'
+data['initial_state'] = 100
+data['unit_of_measurement'] = '%'
+data['state_attributes'] = {}
+self._hass.bus.fire(
+    EVENT_PLATFORM_DISCOVERED, {
+        ATTR_SERVICE: DISCOVER_CHILD_SENSORS,
+        ATTR_DISCOVERED: data})
+
+This example will monitor the value of 'batterylevel' in the parent component's
+'state.attributes' dictionary.  The state card will ned be visible on the UI
+and available to automations etc.
+
+'unit_of_measurement' is optional.
+The 'state_attributes' is also optional but if a dictionary is passed in the
+items will be added to the child sensors state attributes.
+
+NOTES:
+
+- At the moment this can only be created via the 'discovery' mechanism.  So do
+not add it into your config file
+
+- You need to add 'sensor' or 'child_sensor' as a dependency for your component
+otherwise there is no guarantee this component will be loaded when the
+discovery event fires.
+
 """
 
 from homeassistant.helpers.entity import Entity
