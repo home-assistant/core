8029d3e592714f2173d3147ae18768523418ad46
Fabian Affolter <fabian@affolter-engineering.ch>
2015-08-06 19:00:18 +0200

    remove some blank lines

--- homeassistant/components/switch/transmission.py
+++ homeassistant/components/switch/transmission.py
@@ -1,7 +1,6 @@
 """
 homeassistant.components.switch.transmission
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 Enable or disable Transmission BitTorrent client Turtle Mode
 
 Configuration:
@@ -38,19 +37,14 @@ Your Transmission username, if you use authentication
 name
 *Optional
 The name to use when displaying this Transmission instance.
-
-
 """
-
 from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
 from homeassistant.const import STATE_ON, STATE_OFF
 
 from homeassistant.helpers.entity import ToggleEntity
 # pylint: disable=no-name-in-module, import-error
 import transmissionrpc
-
 from transmissionrpc.error import TransmissionError
-
 import logging
 
 _LOGGING = logging.getLogger(__name__)
@@ -59,7 +53,7 @@ REQUIREMENTS = ['transmissionrpc>=0.11']
 
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
-    """ Sets up the sensors. """
+    """ Sets up the transmission sensor. """
     host = config.get(CONF_HOST)
     username = config.get(CONF_USERNAME, None)
     password = config.get(CONF_PASSWORD, None)
@@ -87,7 +81,6 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
 
 
 class TransmissionSwitch(ToggleEntity):
-
     """ A Transmission sensor. """
 
     def __init__(self, transmission_client, name):
