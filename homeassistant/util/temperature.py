086961d1093b466583c9c8125515981b242c0a2a
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-08-16 22:06:01 -0700

    Add temperature util, helpers

--- homeassistant/util/temperature.py
+++ homeassistant/util/temperature.py
@@ -6,11 +6,11 @@ Temperature util functions.
 """
 
 
-def f_to_c(fahrenheit):
+def fahrenheit_to_celcius(fahrenheit):
     """ Convert a Fahrenheit temperature to Celcius. """
     return (fahrenheit - 32.0) / 1.8
 
 
-def c_to_f(celcius):
+def celcius_to_fahrenheit(celcius):
     """ Convert a Celcius temperature to Fahrenheit. """
     return celcius * 1.8 + 32.0
