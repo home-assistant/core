0afb0e737f536ded32a0d6fa01fe527de70ae1f3
Stefan Jonasson <stefan.jonasson@sitedirect.se>
2015-09-17 08:39:41 +0200

    flake8 fix

--- homeassistant/components/automation/time.py
+++ homeassistant/components/automation/time.py
@@ -32,7 +32,7 @@ def trigger(hass, config, action):
             return False
         hours, minutes, seconds = after.hour, after.minute, after.second
     elif CONF_HOURS in config or CONF_MINUTES in config \
-        or CONF_SECONDS in config:
+         or CONF_SECONDS in config:
         hours = convert(config.get(CONF_HOURS), int)
         minutes = convert(config.get(CONF_MINUTES), int)
         seconds = convert(config.get(CONF_SECONDS), int)
