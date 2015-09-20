d0008f6ca850eea5f477425e4a7b59abecc34184
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-09-20 11:46:01 -0700

    Fix tests and linting

--- homeassistant/components/device_tracker/owntracks.py
+++ homeassistant/components/device_tracker/owntracks.py
@@ -21,10 +21,21 @@ def setup_scanner(hass, config, see):
 
     def owntracks_location_update(topic, payload, qos):
         """ MQTT message received. """
+
+        # Docs on available data:
+        # http://owntracks.org/booklet/tech/json/#_typelocation
+
         parts = topic.split('/')
-        data = json.loads(payload)
+        try:
+            data = json.loads(payload)
+        except ValueError:
+            # If invalid JSON
+            return
+        if data.get('_type') != 'location':
+            return
         dev_id = '{}_{}'.format(parts[1], parts[2])
-        see(dev_id=dev_id, host_name=parts[1], gps=[data['lat'], data['lon']])
+        see(dev_id=dev_id, host_name=parts[1], gps=(data['lat'], data['lon']),
+            gps_accuracy=data['acc'], battery=data['batt'])
 
     mqtt.subscribe(hass, LOCATION_TOPIC, owntracks_location_update, 1)
 
