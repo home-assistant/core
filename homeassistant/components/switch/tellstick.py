16c2827465f7151cd5e33802633846e63023f1a4
Stefan Jonasson <stefan.jonasson@sitedirect.se>
2015-09-23 09:50:12 +0200

    Removed the check for callback_dispatcher

--- homeassistant/components/switch/tellstick.py
+++ homeassistant/components/switch/tellstick.py
@@ -34,7 +34,7 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
 
     # pylint: disable=no-member
     #if telldus.TelldusCore.callback_dispatcher is None:
-        dispatcher = DirectCallbackDispatcher()
+    dispatcher = DirectCallbackDispatcher()
     core = telldus.TelldusCore(callback_dispatcher=dispatcher)
     #else:
     #    core = telldus.TelldusCore()
