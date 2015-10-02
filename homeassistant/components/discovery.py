a05b4cc5b6939c08aaf245b5d42961acfd99e218
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-10-02 06:48:46 -0700

    Update netdisco dependency

--- homeassistant/components/discovery.py
+++ homeassistant/components/discovery.py
@@ -19,7 +19,7 @@ from homeassistant.const import (
 
 DOMAIN = "discovery"
 DEPENDENCIES = []
-REQUIREMENTS = ['netdisco==0.4.1']
+REQUIREMENTS = ['netdisco==0.4.2']
 
 SCAN_INTERVAL = 300  # seconds
 
