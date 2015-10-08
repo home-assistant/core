85bf6cb5686fc547eadd831f7cbd1597d9a3ed2e
Tom Duijf <tom.duijf@gmail.com>
2015-10-08 10:01:10 +0000

    Added pylint disables

--- homeassistant/components/device_tracker/snmp.py
+++ homeassistant/components/device_tracker/snmp.py
@@ -76,7 +76,7 @@ class SnmpScanner(object):
 
         self.lock = threading.Lock()
 
-        self.last_results = {}
+        self.last_results = []
 
         # Test the router is accessible
         data = self.get_snmp_data()
@@ -90,6 +90,8 @@ class SnmpScanner(object):
         self._update_info()
         return [client['mac'] for client in self.last_results]
 
+    # Ignoring no-self-use warning
+    # pylint: disable=R0201
     def get_device_name(self, device):
         """ Returns the name of the given device or None if we don't know. """
         # We have no names
@@ -126,10 +128,12 @@ class SnmpScanner(object):
         )
 
         if errindication:
+            #pylint: disable=W1202
             _LOGGER.error("SNMPLIB error: {}".format(errindication))
             return
         if errstatus:
             err = "SNMP error: {} at {}"
+            #pylint: disable=W1202
             _LOGGER.error(err.format(errstatus.prettyPrint(),
                                      errindex and
                                      restable[-1][int(errindex)-1]
