452fd25a7720e3e58dffa8ca9e9b8428b27b9c96
Fabian Affolter <fabian@affolter-engineering.ch>
2015-10-13 20:42:05 +0200

    Move configuration details to docs

--- homeassistant/components/alarm_control_panel/mqtt.py
+++ homeassistant/components/alarm_control_panel/mqtt.py
@@ -1,62 +1,10 @@
 """
 homeassistant.components.alarm_control_panel.mqtt
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 This platform enables the possibility to control a MQTT alarm.
-In this platform, 'state_topic' and 'command_topic' are required.
-The alarm will only change state after receiving the a new state
-from 'state_topic'. If these messages are published with RETAIN flag,
-the MQTT alarm will receive an instant state update after subscription
-and will start with correct state. Otherwise, the initial state will
-be 'unknown'.
 
-Configuration:
-
-alarm_control_panel:
-  platform: mqtt
-  name: "MQTT Alarm"
-  state_topic: "home/alarm"
-  command_topic: "home/alarm/set"
-  qos: 0
-  payload_disarm: "DISARM"
-  payload_arm_home: "ARM_HOME"
-  payload_arm_away: "ARM_AWAY"
-  code: "mySecretCode"
-
-Variables:
-
-name
-*Optional
-The name of the alarm. Default is 'MQTT Alarm'.
-
-state_topic
-*Required
-The MQTT topic subscribed to receive state updates.
-
-command_topic
-*Required
-The MQTT topic to publish commands to change the alarm state.
-
-qos
-*Optional
-The maximum QoS level of the state topic. Default is 0.
-This QoS will also be used to publishing messages.
-
-payload_disarm
-*Optional
-The payload do disarm alarm. Default is "DISARM".
-
-payload_arm_home
-*Optional
-The payload to set armed-home mode. Default is "ARM_HOME".
-
-payload_arm_away
-*Optional
-The payload to set armed-away mode. Default is "ARM_AWAY".
-
-code
-*Optional
-If defined, specifies a code to enable or disable the alarm in the frontend.
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/alarm_control_panel.mqtt.html
 """
 import logging
 import homeassistant.components.mqtt as mqtt
