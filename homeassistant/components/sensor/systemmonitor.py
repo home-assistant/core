da8288668e507ba37bf9bb2c105f1d05a12724c9
Fabian Affolter <fabian@affolter-engineering.ch>
2015-10-13 23:41:46 +0200

    Upgrade psutil to 3.2.2

--- homeassistant/components/sensor/systemmonitor.py
+++ homeassistant/components/sensor/systemmonitor.py
@@ -64,7 +64,7 @@ import homeassistant.util.dt as dt_util
 from homeassistant.helpers.entity import Entity
 from homeassistant.const import STATE_ON, STATE_OFF
 
-REQUIREMENTS = ['psutil==3.0.0']
+REQUIREMENTS = ['psutil==3.2.2']
 SENSOR_TYPES = {
     'disk_use_percent': ['Disk Use', '%'],
     'disk_use': ['Disk Use', 'GiB'],
