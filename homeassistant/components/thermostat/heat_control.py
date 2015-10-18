294b214299b4d5bee6870fe9572b5260e56a8acf
Fabian Affolter <fabian@affolter-engineering.ch>
2015-10-18 20:05:53 +0200

    Add link to docs

--- homeassistant/components/thermostat/heat_control.py
+++ homeassistant/components/thermostat/heat_control.py
@@ -3,59 +3,8 @@ homeassistant.components.thermostat.heat_control
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Adds support for a thermostat.
 
-Specify a start time, end time and a target temperature.
-If the the current temperature is lower than the target temperature,
-and the time is between start time and end time, the heater will
-be turned on. Opposite if the the temperature is higher than the
-target temperature the heater will be turned off.
-
-If away mode is activated the target temperature is sat to a min
-temperature (min_temp in config). The min temperature is also used
-as target temperature when no other temperature is specified.
-
-If the heater is manually turned on, the target temperature will
-be sat to 100*C. Meaning the thermostat probably will never turn
-off the heater.
-If the heater is manually turned off, the target temperature will
-be sat according to normal rules. (Based on target temperature
-for given time intervals and the min temperature.)
-
-A target temperature sat with the set_temperature function will
-override all other rules for the target temperature.
-
-Config:
-
-[thermostat]
-platform=heat_control
-
-name = Name of thermostat
-
-heater = entity_id for heater switch,
-         must be a toggle device
-
-target_sensor = entity_id for temperature sensor,
-                target_sensor.state must be temperature
-
-time_temp = start_time-end_time:target_temp,
-
-min_temp = minimum temperature, used when away mode is
-           active or no other temperature specified.
-
-Example:
-[thermostat]
-platform=heat_control
-name = Stue
-heater = switch.Ovn_stue
-target_sensor = tellstick_sensor.Stue_temperature
-time_temp = 0700-0745:17,1500-1850:20
-min_temp = 10
-
-For the example the heater will turn on at 0700 if the temperature
-is lower than 17*C away mode is false. Between 0700 and 0745 the
-target temperature will be 17*C. Between 0745 and 1500 no temperature
-is specified. so the min_temp of 10*C will be used. From 1500 to 1850
-the target temperature is 20*, but if away mode is true the target
-temperature will be sat to 10*C
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/thermostat.heat_control.html
 """
 import logging
 import datetime
