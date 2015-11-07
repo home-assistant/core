e8e78427a77045a993374f6ff07d6aaa87c98478
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-11-07 01:25:33 -0800

    Light: base color now in RGB instead of XY

--- homeassistant/components/light/limitlessled.py
+++ homeassistant/components/light/limitlessled.py
@@ -18,12 +18,42 @@ import logging
 
 from homeassistant.const import DEVICE_DEFAULT_NAME
 from homeassistant.components.light import (Light, ATTR_BRIGHTNESS,
-                                            ATTR_XY_COLOR, ATTR_EFFECT,
+                                            ATTR_RGB_COLOR, ATTR_EFFECT,
                                             EFFECT_COLORLOOP, EFFECT_WHITE)
-from homeassistant.util.color import color_RGB_to_xy
 
 _LOGGER = logging.getLogger(__name__)
 REQUIREMENTS = ['ledcontroller==1.1.0']
+
+COLOR_TABLE = {
+    'white': (0xFF, 0xFF, 0xFF),
+    'violet': (0xEE, 0x82, 0xEE),
+    'royal_blue': (0x41, 0x69, 0xE1),
+    'baby_blue': (0x87, 0xCE, 0xFA),
+    'aqua': (0x00, 0xFF, 0xFF),
+    'royal_mint': (0x7F, 0xFF, 0xD4),
+    'seafoam_green': (0x2E, 0x8B, 0x57),
+    'green': (0x00, 0x80, 0x00),
+    'lime_green': (0x32, 0xCD, 0x32),
+    'yellow': (0xFF, 0xFF, 0x00),
+    'yellow_orange': (0xDA, 0xA5, 0x20),
+    'orange': (0xFF, 0xA5, 0x00),
+    'red': (0xFF, 0x00, 0x00),
+    'pink': (0xFF, 0xC0, 0xCB),
+    'fusia': (0xFF, 0x00, 0xFF),
+    'lilac': (0xDA, 0x70, 0xD6),
+    'lavendar': (0xE6, 0xE6, 0xFA),
+}
+
+
+def _distance_squared(rgb1, rgb2):
+    """ Return sum of squared distances of each color part. """
+    return sum((val1-val2)**2 for val1, val2 in zip(rgb1, rgb2))
+
+
+def _rgb_to_led_color(rgb_color):
+    """ Convert an RGB color to the closest LedController color string. """
+    return sorted((_distance_squared(rgb_color, color), name)
+                  for name, color in COLOR_TABLE.items())[0][1]
 
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
@@ -106,49 +136,15 @@ class RGBWLimitlessLED(LimitlessLED):
         super().__init__(pool, controller_id, group, name, 'rgbw')
 
         self._brightness = 100
-        self._xy_color = color_RGB_to_xy(255, 255, 255)
-
-        # Build a color table that maps an RGB color to a color string
-        # recognized by LedController's set_color method
-        self._color_table = [(color_RGB_to_xy(*x[0]), x[1]) for x in [
-            ((0xFF, 0xFF, 0xFF), 'white'),
-            ((0xEE, 0x82, 0xEE), 'violet'),
-            ((0x41, 0x69, 0xE1), 'royal_blue'),
-            ((0x87, 0xCE, 0xFA), 'baby_blue'),
-            ((0x00, 0xFF, 0xFF), 'aqua'),
-            ((0x7F, 0xFF, 0xD4), 'royal_mint'),
-            ((0x2E, 0x8B, 0x57), 'seafoam_green'),
-            ((0x00, 0x80, 0x00), 'green'),
-            ((0x32, 0xCD, 0x32), 'lime_green'),
-            ((0xFF, 0xFF, 0x00), 'yellow'),
-            ((0xDA, 0xA5, 0x20), 'yellow_orange'),
-            ((0xFF, 0xA5, 0x00), 'orange'),
-            ((0xFF, 0x00, 0x00), 'red'),
-            ((0xFF, 0xC0, 0xCB), 'pink'),
-            ((0xFF, 0x00, 0xFF), 'fusia'),
-            ((0xDA, 0x70, 0xD6), 'lilac'),
-            ((0xE6, 0xE6, 0xFA), 'lavendar'),
-        ]]
+        self._rgb_color = COLOR_TABLE['white']
 
     @property
     def brightness(self):
         return self._brightness
 
     @property
-    def color_xy(self):
-        return self._xy_color
-
-    def _xy_to_led_color(self, xy_color):
-        """ Convert an XY color to the closest LedController color string. """
-        def abs_dist_squared(p_0, p_1):
-            """ Returns the absolute value of the squared distance """
-            return abs((p_0[0] - p_1[0])**2 + (p_0[1] - p_1[1])**2)
-
-        candidates = [(abs_dist_squared(xy_color, x[0]), x[1]) for x in
-                      self._color_table]
-
-        # First candidate in the sorted list is closest to desired color:
-        return sorted(candidates)[0][1]
+    def rgb_color(self):
+        return self._rgb_color
 
     def turn_on(self, **kwargs):
         """ Turn the device on. """
@@ -157,8 +153,8 @@ class RGBWLimitlessLED(LimitlessLED):
         if ATTR_BRIGHTNESS in kwargs:
             self._brightness = kwargs[ATTR_BRIGHTNESS]
 
-        if ATTR_XY_COLOR in kwargs:
-            self._xy_color = kwargs[ATTR_XY_COLOR]
+        if ATTR_RGB_COLOR in kwargs:
+            self._rgb_color = _rgb_to_led_color(kwargs[ATTR_RGB_COLOR])
 
         effect = kwargs.get(ATTR_EFFECT)
 
@@ -168,7 +164,7 @@ class RGBWLimitlessLED(LimitlessLED):
             self.pool.execute(self.controller_id, "white", self.group)
         else:
             self.pool.execute(self.controller_id, "set_color",
-                              self._xy_to_led_color(self._xy_color),
+                              self._rgb_color,
                               self.group)
 
         # Brightness can be set independently of color
