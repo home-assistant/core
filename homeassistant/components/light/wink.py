7f4c13c3822f63e9f6e56f6ddccd4dfda3db1d54
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-11-08 19:41:22 -0800

    Upgrade Wink version to v0.1.1

--- homeassistant/components/light/wink.py
+++ homeassistant/components/light/wink.py
@@ -13,8 +13,8 @@ from homeassistant.components.wink import WinkToggleDevice
 from homeassistant.const import CONF_ACCESS_TOKEN
 
 REQUIREMENTS = ['https://github.com/balloob/python-wink/archive/'
-                'c2b700e8ca866159566ecf5e644d9c297f69f257.zip'
-                '#python-wink==0.1']
+                '9eb39eaba0717922815e673ad1114c685839d890.zip'
+                '#python-wink==0.1.1']
 
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
