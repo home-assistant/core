03bcc0c0c4c1b7a88d3ce72abeac6eed52ee7d14
happyleaves <happyleaves.tfr@gmail.com>
2015-11-14 14:14:02 -0500

    s20 switch support

---
+++ homeassistant/components/switch/s20.py
@@ -0,0 +1,73 @@
+"""
+homeassistant.components.switch.s20
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Support for Orvibo S20 Wifi Smart Switches.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/switch.s20/
+"""
+import logging
+
+from homeassistant.components.switch import SwitchDevice
+
+from orvibo.s20 import S20, S20Exception
+
+DEFAULT_NAME = "Orbivo S20 Switch"
+REQUIREMENTS = ['orbivo==1.0.0']
+_LOGGER = logging.getLogger(__name__)
+
+
+# pylint: disable=unused-argument
+def setup_platform(hass, config, add_devices_callback, discovery_info=None):
+    """ Find and return S20 switches. """
+    if config.get('host') is None:
+        _LOGGER.error("Missing required variable: host")
+        return
+    try:
+        s20 = S20(config.get('host'))
+        add_devices_callback([S20Switch(config.get('name', DEFAULT_NAME),
+                                        s20)])
+    except S20Exception as exception:
+        _LOGGER.error(exception)
+
+
+class S20Switch(SwitchDevice):
+    """ Represents an S20 switch. """
+    def __init__(self, name, s20):
+        self._name = name
+        self._s20 = s20
+
+    @property
+    def should_poll(self):
+        """ No polling needed. """
+        return False
+
+    @property
+    def name(self):
+        """ The name of the switch. """
+        return self._name
+
+    @property
+    def is_on(self):
+        """ True if device is on. """
+        try:
+            return self._s20.on
+        except S20Exception as exception:
+            _LOGGER.error(exception)
+            return False
+
+    def turn_on(self, **kwargs):
+        """ Turn the device on. """
+        try:
+            self._s20.on = True
+        except S20Exception as exception:
+            _LOGGER.error(exception)
+        self.update_ha_state()
+
+    def turn_off(self, **kwargs):
+        """ Turn the device off. """
+        try:
+            self._s20.on = False
+        except S20Exception as exception:
+            _LOGGER.error(exception)
+        self.update_ha_state()
