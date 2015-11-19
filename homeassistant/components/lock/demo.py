fa7391cdf684bb643bba77fbeb79ef0691fa37c0
miniconfig <dev@miniconfig.com>
2015-11-19 16:54:55 -0500

    Changed do_lock and do_unlock methods to lock and unlock. Implemented state method. Fixed locked method for demo interface. Changed LockDevice to extend Entity instead of ToggleEntity

--- homeassistant/components/lock/demo.py
+++ homeassistant/components/lock/demo.py
@@ -41,16 +41,19 @@ class DemoLock(LockDevice):
         return self._icon
 
     @property
-    def is_locked(self):
+    def locked(self):
         """ True if device is locked. """
-        return self._state
+        if self._state == STATE_LOCKED:
+            return True
+        else:
+            return False
 
-    def do_lock(self, **kwargs):
+    def lock(self, **kwargs):
         """ Lock the device. """
         self._state = STATE_LOCKED
         self.update_ha_state()
 
-    def do_unlock(self, **kwargs):
+    def unlock(self, **kwargs):
         """ Unlock the device. """
         self._state = STATE_UNLOCKED
         self.update_ha_state()
