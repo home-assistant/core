21acb5d973f98032bda9bcab8c462f2c5388701b
Oliver van Porten <oliver@van-porten.de>
2015-11-20 22:45:09 +0100

    refactor format mqtt format parser

--- homeassistant/components/mqtt/__init__.py
+++ homeassistant/components/mqtt/__init__.py
@@ -129,13 +129,24 @@ def setup(hass, config):
     return True
 
 
-class JsonFmtParser(object):
-  """ Implements a json parser on xpath"""
-  def __init__(self, jsonpath):
-    self._expr = jsonpath_rw.parse(jsonpath)
+class FmtParser(object):
+  """ wrapper for all supported formats """
+  
+  class _JsonFmtParser(object):
+    """ Implements a json parser on xpath"""
+    def __init__(self, jsonpath):
+      self._expr = jsonpath_rw.parse(jsonpath)
+    def __call__(self, payload):
+      match = self._expr.find(json.loads(payload))
+      return match[0].value if len(match) > 0 else None 
+      
+  def __init__(self, fmt):
+    if fmt.startswith('json:'):
+      self._parser = FmtParser._JsonFmtParser(fmt[5:])
+    else:
+      self._parser = lambda x: x
   def __call__(self, payload):
-    match = self._expr.find(json.loads(payload))
-    return match[0].value if len(match) > 0 else None
+    return self._parser(payload)
 
 
 # This is based on one of the paho-mqtt examples:
