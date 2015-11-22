100400f14940bfade04ed29c51434ec55f3ac83d
Oliver van Porten <oliver@van-porten.de>
2015-11-22 16:28:21 +0100

    move requirements to single line to not to affect coverage

--- homeassistant/components/mqtt/__init__.py
+++ homeassistant/components/mqtt/__init__.py
@@ -31,8 +31,7 @@ SERVICE_PUBLISH = 'publish'
 EVENT_MQTT_MESSAGE_RECEIVED = 'MQTT_MESSAGE_RECEIVED'
 
 DEPENDENCIES = []
-REQUIREMENTS = ['paho-mqtt==1.1',
-                'jsonpath-rw==1.4.0']
+REQUIREMENTS = ['paho-mqtt==1.1', 'jsonpath-rw==1.4.0']
 
 CONF_BROKER = 'broker'
 CONF_PORT = 'port'
