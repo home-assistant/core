262fb2f12aa56b42272b27eefcbe277a073fecf6
sfam <sfam21@gmail.com>
2015-11-25 18:13:39 +0000

    rename component to motor and services to open/close/stop

--- tests/components/motor/test_mqtt.py
+++ tests/components/motor/test_mqtt.py
@@ -1,19 +1,19 @@
 """
-tests.components.rollershutter.test_mqtt
+tests.components.motor.test_mqtt
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Tests mqtt rollershutter.
+Tests mqtt motor.
 """
 import unittest
 
 from homeassistant.const import STATE_OPEN, STATE_CLOSED, STATE_UNKNOWN
 import homeassistant.core as ha
-import homeassistant.components.rollershutter as rollershutter
+import homeassistant.components.motor as motor
 from tests.common import mock_mqtt_component, fire_mqtt_message
 
 
-class TestRollershutterMQTT(unittest.TestCase):
-    """ Test the MQTT rollershutter. """
+class TestMotorMQTT(unittest.TestCase):
+    """ Test the MQTT motor. """
 
     def setUp(self):  # pylint: disable=invalid-name
         self.hass = ha.HomeAssistant()
@@ -24,43 +24,43 @@ class TestRollershutterMQTT(unittest.TestCase):
         self.hass.stop()
 
     def test_controlling_state_via_topic(self):
-        self.assertTrue(rollershutter.setup(self.hass, {
-            'rollershutter': {
+        self.assertTrue(motor.setup(self.hass, {
+            'motor': {
                 'platform': 'mqtt',
                 'name': 'test',
                 'state_topic': 'state-topic',
                 'command_topic': 'command-topic',
                 'qos': 0,
-                'payload_up': 'UP',
-                'payload_down': 'DOWN',
+                'payload_open': 'OPEN',
+                'payload_close': 'CLOSE',
                 'payload_stop': 'STOP'
             }
         }))
 
-        state = self.hass.states.get('rollershutter.test')
+        state = self.hass.states.get('motor.test')
         self.assertEqual(STATE_UNKNOWN, state.state)
 
         fire_mqtt_message(self.hass, 'state-topic', '0')
         self.hass.pool.block_till_done()
 
-        state = self.hass.states.get('rollershutter.test')
+        state = self.hass.states.get('motor.test')
         self.assertEqual(STATE_CLOSED, state.state)
 
         fire_mqtt_message(self.hass, 'state-topic', '50')
         self.hass.pool.block_till_done()
 
-        state = self.hass.states.get('rollershutter.test')
+        state = self.hass.states.get('motor.test')
         self.assertEqual(STATE_OPEN, state.state)
 
         fire_mqtt_message(self.hass, 'state-topic', '100')
         self.hass.pool.block_till_done()
 
-        state = self.hass.states.get('rollershutter.test')
+        state = self.hass.states.get('motor.test')
         self.assertEqual(STATE_OPEN, state.state)
 
     def test_sending_mqtt_commands(self):
-        self.assertTrue(rollershutter.setup(self.hass, {
-            'rollershutter': {
+        self.assertTrue(motor.setup(self.hass, {
+            'motor': {
                 'platform': 'mqtt',
                 'name': 'test',
                 'state_topic': 'state-topic',
@@ -69,54 +69,54 @@ class TestRollershutterMQTT(unittest.TestCase):
             }
         }))
 
-        state = self.hass.states.get('rollershutter.test')
+        state = self.hass.states.get('motor.test')
         self.assertEqual(STATE_UNKNOWN, state.state)
 
-        rollershutter.move_up(self.hass, 'rollershutter.test')
+        motor.call_open(self.hass, 'motor.test')
         self.hass.pool.block_till_done()
 
-        self.assertEqual(('command-topic', 'UP', 2),
+        self.assertEqual(('command-topic', 'OPEN', 2),
                          self.mock_publish.mock_calls[-1][1])
-        state = self.hass.states.get('rollershutter.test')
+        state = self.hass.states.get('motor.test')
         self.assertEqual(STATE_UNKNOWN, state.state)
 
     def test_state_attributes_current_position(self):
-        self.assertTrue(rollershutter.setup(self.hass, {
-            'rollershutter': {
+        self.assertTrue(motor.setup(self.hass, {
+            'motor': {
                 'platform': 'mqtt',
                 'name': 'test',
                 'state_topic': 'state-topic',
                 'command_topic': 'command-topic',
-                'payload_up': 'UP',
-                'payload_down': 'DOWN',
+                'payload_open': 'OPEN',
+                'payload_close': 'CLOSE',
                 'payload_stop': 'STOP'
             }
         }))
 
         state_attributes_dict = self.hass.states.get(
-            'rollershutter.test').attributes
+            'motor.test').attributes
         self.assertFalse('current_position' in state_attributes_dict)
 
         fire_mqtt_message(self.hass, 'state-topic', '0')
         self.hass.pool.block_till_done()
         current_position = self.hass.states.get(
-            'rollershutter.test').attributes['current_position']
+            'motor.test').attributes['current_position']
         self.assertEqual(0, current_position)
 
         fire_mqtt_message(self.hass, 'state-topic', '50')
         self.hass.pool.block_till_done()
         current_position = self.hass.states.get(
-            'rollershutter.test').attributes['current_position']
+            'motor.test').attributes['current_position']
         self.assertEqual(50, current_position)
 
         fire_mqtt_message(self.hass, 'state-topic', '101')
         self.hass.pool.block_till_done()
         current_position = self.hass.states.get(
-            'rollershutter.test').attributes['current_position']
+            'motor.test').attributes['current_position']
         self.assertEqual(50, current_position)
 
         fire_mqtt_message(self.hass, 'state-topic', 'non-numeric')
         self.hass.pool.block_till_done()
         current_position = self.hass.states.get(
-            'rollershutter.test').attributes['current_position']
+            'motor.test').attributes['current_position']
         self.assertEqual(50, current_position)
