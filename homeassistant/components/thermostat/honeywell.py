9b5e0178821c96b7a1b8f297bbbc1fbdbda31631
Andrew Stock <github@andrew-stock.com>
2015-12-01 21:30:44 +0000

    Fixing exception

--- homeassistant/components/thermostat/honeywell.py
+++ homeassistant/components/thermostat/honeywell.py
@@ -73,7 +73,7 @@ class RoundThermostat(ThermostatDevice):
     def target_temperature(self):
         """ Returns the temperature we try to reach. """
         if self._is_dhw:
-            raise NotImplementedError
+            return None
         return self._target_temperature
 
     def set_temperature(self, temperature):
