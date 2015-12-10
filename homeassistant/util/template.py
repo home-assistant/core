f83326c5070c989381215e4b6de79be26ddefe49
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-12-09 16:20:09 -0800

    Add template support

---
+++ homeassistant/util/template.py
@@ -0,0 +1,58 @@
+"""
+homeassistant.util.template
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Template utility methods for rendering strings with HA data.
+"""
+# pylint: disable=too-few-public-methods
+from jinja2.sandbox import SandboxedEnvironment
+
+ENV = SandboxedEnvironment()
+
+
+def forgiving_round(value, precision):
+    """ Rounding method that accepts strings. """
+    try:
+        return round(float(value), precision)
+    except ValueError:
+        # If value can't be converted to float
+        return value
+
+
+ENV.filters['round'] = forgiving_round
+
+
+def render(hass, template):
+    """ Render given template. """
+    return ENV.from_string(template).render(
+        states=AllStates(hass))
+
+
+class AllStates(object):
+    """ Class to expose all HA states as attributes. """
+    def __init__(self, hass):
+        self._hass = hass
+
+    def __getattr__(self, name):
+        return DomainStates(self._hass, name)
+
+    def __iter__(self):
+        return iter(sorted(self._hass.states.all(),
+                           key=lambda state: state.entity_id))
+
+
+class DomainStates(object):
+    """ Class to expose a specific HA domain as attributes. """
+
+    def __init__(self, hass, domain):
+        self._hass = hass
+        self._domain = domain
+
+    def __getattr__(self, name):
+        return self._hass.states.get('{}.{}'.format(self._domain, name))
+
+    def __iter__(self):
+        return iter(sorted(
+            (state for state in self._hass.states.all()
+             if state.domain == self._domain),
+            key=lambda state: state.entity_id))
