b5d04e7beb28e6e26536da9d27b41f3ca1dc3201
Erik <saturlight@gmail.com>
2015-12-15 22:05:55 +0100

    lint ok

--- homeassistant/components/sensor/eliqonline.py
+++ homeassistant/components/sensor/eliqonline.py
@@ -29,22 +29,26 @@ REQUIREMENTS = ['eliqonline==1.0.11']
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
 
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
+    """ Set up the sensors """
+
     import eliqonline
 
     access_token = config.get(CONF_ACCESS_TOKEN)
-    name         = config.get(CONF_NAME)
-    channel_id   = config.get("channel_id")
+    name = config.get(CONF_NAME)
+    channel_id = config.get("channel_id")
 
     if not access_token:
         _LOGGER.error(
             "Configuration Error"
-            "Please make sure you have configured your access token which can be aquired from https://my.eliq.se/user/settings/api")
+            "Please make sure you have configured your access token " +
+            "that can be aquired from https://my.eliq.se/user/settings/api")
         return None
 
     api = eliqonline.API(access_token)
     add_devices([EliqSensor(api, channel_id, name)])
-        
+
 
 class EliqSensor(Entity):
     """ Implements a Eliq sensor. """
@@ -55,31 +59,28 @@ class EliqSensor(Entity):
             self._name = name + " " + self._name
         self._unit_of_measurement = "W"
         self._state = STATE_UNKNOWN
-        
+
         self.api = api
         self.channel_id = channel_id
         self.update()
-
 
     @property
     def name(self):
         """ Returns the name. """
         return self._name
 
-
     @property
     def unit_of_measurement(self):
         """ Unit of measurement of this entity, if any. """
         return self._unit_of_measurement
-
 
     @property
     def state(self):
         """ Returns the state of the device. """
         return self._state
 
-
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """ Gets the latest data """
-        self._state = int(self.api.get_data_now(channelid=self.channel_id).power)
+        response = self.api.get_data_now(channelid=self.channel_id)
+        self._state = int(response.power)
