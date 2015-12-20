7156ed062ed352b521433d6778540feb0f01afe1
Erik <saturlight@gmail.com>
2015-12-20 19:48:19 +0100

    support for switches

--- homeassistant/components/switch/tellduslive.py
+++ homeassistant/components/switch/tellduslive.py
@@ -5,12 +5,16 @@ Support for Tellstick switches using Tellstick Net and the Telldus Live online s
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/switch.tellduslive/
+
 """
 import logging
 
-from homeassistant.const import STATE_UNKNOWN
+from homeassistant.const import (STATE_ON, STATE_OFF, STATE_UNKNOWN)
 from homeassistant.components import tellduslive
 from homeassistant.helpers.entity import ToggleEntity
+
+from tellcore.constants import (
+    TELLSTICK_TURNON, TELLSTICK_TURNOFF, TELLSTICK_TOGGLE)
 
 _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = ['tellduslive']
@@ -19,20 +23,25 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     """ Find and return Tellstick switches. """
     switches = tellduslive.NETWORK.get_switches()
     # fixme: metadata groups etc
+    STATES = {0:                 STATE_UNKNOWN,
+              TELLSTICK_TURNON:  STATE_ON,
+              TELLSTICK_TURNOFF: STATE_OFF}
     add_devices([TelldusLiveSwitch(switch["name"], 
-                                   switch["id"]) \
+                                   switch["id"],
+                                   STATES[switch["state"]])
                  for switch in switches])
 
 class TelldusLiveSwitch(ToggleEntity):
     """ Represents a Tellstick switch. """
     
-    def __init__(self, name, switch_id):
+    def __init__(self, name, switch_id, state):
         self._name = name
         self._id = switch_id
+        self._state = state
 
     @property
     def should_poll(self):
-        """ Tells Home Assistant not to poll this entity. """
+        """ Tells Home Assistant to poll this entity. """
         return True
 
     @property
@@ -43,22 +52,27 @@ class TelldusLiveSwitch(ToggleEntity):
     @property
     def state(self):
         """ Returns the state. """
-        return STATE_UNKNOWN
+        return self._state
 
     #@property
     #def state_attributes(self):
     #    """ Returns optional state attributes. """
     #    pass
 
-    #@property
-    #def is_on(self):
-    #    """ True if switch is on. """
-    #    pass
+    @property
+    def is_on(self):
+        """ True if switch is on. """
+        return self.state() == STATE_ON
 
     def turn_on(self, **kwargs):
         """ Turns the switch on. """
-        pass
+        response = tellduslive.NETWORK.request("device/turnOn", {"id": self._id})
+        if response["status"] == "success":
+            self._state = STATE_ON
 
     def turn_off(self, **kwargs):
         """ Turns the switch off. """
-        pass
+        response = tellduslive.NETWORK.request("device/turnOff", {"id": self._id})
+        if response["status"] == "success":
+            self._state = STATE_OFF
+
