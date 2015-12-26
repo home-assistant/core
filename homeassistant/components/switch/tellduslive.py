666cd222db81f5af45e3a369533c3e547cdbcb98
Erik <erik.eriksson@yahoo.com>
2015-12-26 22:31:33 +0100

    moved dependency on external constants

--- homeassistant/components/switch/tellduslive.py
+++ homeassistant/components/switch/tellduslive.py
@@ -10,37 +10,32 @@ https://home-assistant.io/components/switch.tellduslive/
 """
 import logging
 
-from tellcore.constants import (
-    TELLSTICK_TURNON, TELLSTICK_TURNOFF)
-
 from homeassistant.const import (STATE_ON, STATE_OFF, STATE_UNKNOWN)
 from homeassistant.components import tellduslive
 from homeassistant.helpers.entity import ToggleEntity
 
 _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = ['tellduslive']
-TELLSTICK_UNKNOWN = 0
-STATES = {TELLSTICK_UNKNOWN: STATE_UNKNOWN,
-          TELLSTICK_TURNON:  STATE_ON,
-          TELLSTICK_TURNOFF: STATE_OFF}
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """ Find and return Tellstick switches. """
     switches = tellduslive.NETWORK.get_switches()
     add_devices([TelldusLiveSwitch(switch["name"],
-                                   switch["id"],
-                                   STATES[switch["state"]])
+                                   switch["id"])
                  for switch in switches if switch["type"] == "device"])
-
 
 class TelldusLiveSwitch(ToggleEntity):
     """ Represents a Tellstick switch. """
 
-    def __init__(self, name, switch_id, state):
+    from tellcore.constants import (
+        TELLSTICK_TURNON, TELLSTICK_TURNOFF)
+
+    def __init__(self, name, switch_id):
         self._name = name
         self._id = switch_id
-        self._state = state
+        self._state = STATE_UNKNOWN
+        self.update()
 
     @property
     def should_poll(self):
@@ -51,27 +46,31 @@ class TelldusLiveSwitch(ToggleEntity):
     def name(self):
         """ Returns the name of the switch if any. """
         return self._name
-
-    @property
-    def state(self):
-        """ Returns the state. """
-        return self._state
-
+        
+    def update(self):
+        STATES = {self.TELLSTICK_TURNON:  STATE_ON,
+                  self.TELLSTICK_TURNOFF: STATE_OFF}
+        switches = tellduslive.NETWORK.get_switches()
+        #from pprint import pprint
+        #pprint(switches)
+        for switch in switches:
+            if switch["id"] == self._id:
+                self._state = STATES[switch["state"]]
+                   
     @property
     def is_on(self):
         """ True if switch is on. """
-        return self.state == STATE_ON
+        self.update()
+        return self._state == STATE_ON
 
     def turn_on(self, **kwargs):
         """ Turns the switch on. """
-        response = tellduslive.NETWORK.request("device/turnOn",
-                                               {"id": self._id})
-        if response["status"] == "success":
+        if tellduslive.NETWORK.check_request("device/turnOn",
+                                             {"id": self._id}):
             self._state = STATE_ON
-
+                
     def turn_off(self, **kwargs):
         """ Turns the switch off. """
-        response = tellduslive.NETWORK.request("device/turnOff",
-                                               {"id": self._id})
-        if response["status"] == "success":
+        if tellduslive.NETWORK.check_request("device/turnOff",
+                                             {"id": self._id}):
             self._state = STATE_OFF
