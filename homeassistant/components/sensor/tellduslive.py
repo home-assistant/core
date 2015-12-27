9fd59684ccfc047f72e409f199ef39c0269a9175
Erik <erik.eriksson@yahoo.com>
2015-12-27 10:53:50 +0100

    get battery level and last update time as well

--- homeassistant/components/sensor/tellduslive.py
+++ homeassistant/components/sensor/tellduslive.py
@@ -9,10 +9,13 @@ https://home-assistant.io/components/sensor.tellduslive/
 """
 import logging
 
-from homeassistant.const import TEMP_CELCIUS
+from datetime import datetime
+
+from homeassistant.const import TEMP_CELCIUS, ATTR_BATTERY_LEVEL
 from homeassistant.helpers.entity import Entity
 from homeassistant.components import tellduslive
 
+ATTR_LAST_UPDATED = "time_last_updated"
 
 _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = ['tellduslive']
@@ -34,7 +37,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     for component in sensors:
         for sensor in component["data"]:
             # one component can have more than one sensor
-            # (e.g. humidity and temperature)
+            # (e.g. both humidity and temperature)
             devices.append(TelldusLiveSensor(component["id"],
                                              component["name"],
                                              sensor["name"]))
@@ -49,6 +52,8 @@ class TelldusLiveSensor(Entity):
         self._sensor_type = sensor_type
         self._state = None
         self._name = sensor_name + ' ' + SENSOR_TYPES[sensor_type][0]
+        self._last_update = None
+        self._battery_level = None
         self.update()
 
     @property
@@ -62,6 +67,15 @@ class TelldusLiveSensor(Entity):
         return self._state
 
     @property
+    def state_attributes(self):
+        attrs = dict()
+        if self._battery_level is not None:
+            attrs[ATTR_BATTERY_LEVEL] = self._battery_level
+        if self._last_update is not None:
+            attrs[ATTR_LAST_UPDATED] = self._last_update
+        return attrs
+
+    @property
     def unit_of_measurement(self):
         return SENSOR_TYPES[self._sensor_type][1]
 
@@ -70,10 +84,17 @@ class TelldusLiveSensor(Entity):
         return SENSOR_TYPES[self._sensor_type][2]
 
     def update(self):
-        self._state = tellduslive.NETWORK.get_sensor_value(self._sensor_id,
-                                                           self._sensor_type)
+        values = tellduslive.NETWORK.get_sensor_value(self._sensor_id,
+                                                      self._sensor_type)
+        self._state, self._battery_level, self._last_update = values
+
         self._state = float(self._state)
         if self._sensor_type == SENSOR_TYPE_TEMP:
             self._state = round(self._state, 1)
         elif self._sensor_type == SENSOR_TYPE_HUMIDITY:
             self._state = int(round(self._state))
+
+        self._battery_level = round(self._battery_level * 100 / 255)  # percent
+        self._battery_level = "%d %%" % self._battery_level
+
+        self._last_update = str(datetime.fromtimestamp(self._last_update))
