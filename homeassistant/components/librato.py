d796ea101d25f4bad5f89709898edacb0257e27d
Jesse Newland <jesse@github.com>
2016-01-02 14:56:25 -0600

    Introduce Librato metrics component

---
+++ homeassistant/components/librato.py
@@ -0,0 +1,108 @@
+"""
+homeassistant.components.librato
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Send metrics to Librato Metrics
+
+"""
+import logging
+from homeassistant.helpers import validate_config
+from homeassistant.const import (EVENT_STATE_CHANGED, STATE_ON, STATE_OFF,
+                                 STATE_UNLOCKED, STATE_LOCKED, STATE_UNKNOWN)
+from homeassistant.components.sun import (STATE_ABOVE_HORIZON,
+                                          STATE_BELOW_HORIZON)
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "librato"
+DEPENDENCIES = []
+
+REQUIREMENTS = ['librato-metrics==0.8.5']
+
+CONF_USER = 'user'
+CONF_TOKEN = 'token'
+CONF_NAMESPACE = 'namespace'
+
+
+# pylint: disable=too-many-branches
+def setup(hass, config):
+    """ Setup the Librato component. """
+
+    import librato
+
+    if not validate_config(config, {DOMAIN: [CONF_USER, CONF_TOKEN]}, _LOGGER):
+        return False
+
+    conf = config[DOMAIN]
+    user = conf[CONF_USER]
+    token = conf[CONF_TOKEN]
+    namespace = conf.get(CONF_NAMESPACE, '')
+    if namespace != '':
+        namespace = namespace + '.'
+
+    metrics = librato.connect(
+        user,
+        token,
+        sanitizer=librato.sanitize_metric_name)
+
+    def metrics_listener(event):
+        """ Listen for new messages on the bus and send metrics to Librato. """
+        state = event.data.get('new_state')
+
+        with metrics.new_queue() as metrics_queue:
+
+            # First, count the event using service-side aggregation.
+            # http://support.metrics.librato.com/knowledgebase/articles/215845
+            if isinstance(state.entity_id, str):
+                attributes_hash = {
+                    'summarize_function': 'sum',
+                    'aggregate': True
+                }
+                metrics_queue.add(
+                    (namespace +
+                     'events.' +
+                     state.entity_id +
+                     '.' +
+                     event.event_type),
+                    1,
+                    attributes=attributes_hash)
+            else:
+                return
+
+            # Attempt to parse the state and try to transform it into a float.
+            _state = None
+            if state.state in (STATE_ON, STATE_LOCKED, STATE_ABOVE_HORIZON):
+                _state = 1
+            elif state.state in (STATE_OFF, STATE_UNLOCKED, STATE_UNKNOWN,
+                                 STATE_BELOW_HORIZON):
+                _state = 0
+            else:
+                _state = state.state
+                if _state and isinstance(_state, str):
+                    try:
+                        _state = float(_state)
+                    except ValueError:
+                        _state = None
+
+                if _state and isinstance(_state, int):
+                    _state = float(_state)
+
+            if _state is not None:
+                metrics_queue.add(
+                    (namespace + 'states.' + state.entity_id),
+                    _state)
+
+            # Now inspect the attributes and submit anything that looks like a
+            # float
+            attributes = dict(state.attributes)
+            for key in attributes:
+                try:
+                    value = float(attributes[key])
+                    metrics_queue.add(
+                        (namespace + 'states.' + state.entity_id + '.' + key),
+                        value)
+                except ValueError:
+                    pass
+
+    hass.bus.listen(EVENT_STATE_CHANGED, metrics_listener)
+
+    return True
---
+++ tests/components/test_librato.py
@@ -0,0 +1,93 @@
+"""
+tests.test_component_librato
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Tests the librato compoment.
+"""
+# pylint: disable=protected-access,too-many-public-methods
+import unittest
+import unittest
+from unittest.mock import patch
+from tests.common import mock_state_change_event
+
+import librato
+
+import homeassistant.core as ha
+from homeassistant.components import librato as librato_component
+from homeassistant.const import STATE_ON
+
+class TestComponentsLibrato(unittest.TestCase):
+    """ Tests homeassistant.components.librato module. """
+
+    def setUp(self):  # pylint: disable=invalid-name
+        """ Init needed objects. """
+        self.hass = ha.HomeAssistant()
+
+    def tearDown(self):  # pylint: disable=invalid-name
+        """ Stop down stuff we started. """
+        self.hass.stop()
+
+    def test_config_validation(self):
+        """ Ensure valid configuration is required """
+        self.assertFalse(librato_component.setup(self.hass, {'librato': {}}))
+
+    def test_setup(self):
+        """ Ensure setup returns True when proper configuration is provided """
+        self.assertTrue(librato_component.setup(self.hass, {
+                'librato': {
+                    'user': 'foo',
+                    'token': 'foo'
+                }
+            }))
+
+    @patch('librato.queue.Queue.add')
+    def test_event_submitted(self, mock_librato):
+        """ Test that events are recorded even for unknown states """
+        self.assertTrue(librato_component.setup(self.hass, {
+                'librato': {
+                    'user': 'foo',
+                    'token': 'foo'
+                }
+            }))
+
+        state = ha.State('test.entity', 'string state')
+        mock_state_change_event(self.hass, state)
+        self.hass.pool.block_till_done()
+        self.assertEqual(1, mock_librato.call_count)
+
+    @patch('librato.queue.Queue.add')
+    def test_state_submitted(self, mock_librato):
+        """ Test that known states are submitted """
+        self.assertTrue(librato_component.setup(self.hass, {
+                'librato': {
+                    'user': 'foo',
+                    'token': 'foo'
+                }
+            }))
+
+        state = ha.State('test.entity',STATE_ON)
+        mock_state_change_event(self.hass, state)
+        self.hass.pool.block_till_done()
+        self.assertEqual(2, mock_librato.call_count)
+
+    @patch('librato.queue.Queue.add')
+    def test_attributes_submitted(self, mock_librato):
+        """ Test that numerical attributes are parsed and submitted. """
+        self.assertTrue(librato_component.setup(self.hass, {
+                'librato': {
+                    'user': 'foo',
+                    'token': 'foo'
+                }
+            }))
+
+        state = ha.State(
+            'test.entity',
+            STATE_ON,
+            {
+                'battery_level': 99,
+                'another_floatish': '0.5',
+                'string': 'string'
+            })
+        mock_state_change_event(self.hass, state)
+        self.hass.pool.block_till_done()
+        self.assertEqual(4, mock_librato.call_count)
