5576649d60ec349bcc4c99a3630b1f4970775a3d
Erik <molobrakos@users.noreply.github.com>
2016-01-04 21:36:39 +0100

    Update eliqonline.py

--- homeassistant/components/sensor/eliqonline.py
+++ homeassistant/components/sensor/eliqonline.py
@@ -73,5 +73,5 @@ class EliqSensor(Entity):
         try:
             response = self.api.get_data_now(channelid=self.channel_id)
             self._state = int(response.power)
-        except TypeError: # raised by eliqonline library on any HTTP error
+        except TypeError:  # raised by eliqonline library on any HTTP error
             pass
