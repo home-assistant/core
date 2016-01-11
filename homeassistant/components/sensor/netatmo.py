5bc3489f497c1d2f7bf542316ccbd24bc6434cad
crysosteros <crysosteros@gmail.com>
2016-01-11 19:25:06 +0000

    netatmo: Added a seperate Data class to correctly use the Throttle and to produce less unnecessary traffic

--- homeassistant/components/sensor/netatmo.py
+++ homeassistant/components/sensor/netatmo.py
@@ -34,7 +34,6 @@ CONF_ID = 'client_id'
 CONF_SECRET = 'client_secret'
 
 
-# pylint: disable=unused-variable
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """ Gets a list of the available stations and sets up the sensors. """
 
@@ -56,26 +55,52 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
                                   'client_secret': client_secret,
                                   'username': username,
                                   'password': password}, timeout=10)
-    access_token = request.json()["access_token"]
-    request = requests.get(SENSOR_URL + access_token, timeout=10).json()
+    request = requests.get(SENSOR_URL + request.json()["access_token"],
+                           timeout=10).json()
+    data = NetatmoData(cred)
     for device in request["body"]["devices"]:
         for data_type in device["data_type"]:
-            dev.append(NetatmoSensor(hass, cred, data_type,
+            dev.append(NetatmoSensor(hass, data, data_type,
                                      device["module_name"]))
         for module in device["modules"]:
             for data_type in module["data_type"]:
-                dev.append(NetatmoSensor(hass, cred, data_type,
+                dev.append(NetatmoSensor(hass, data, data_type,
                                          module["module_name"]))
     add_devices(dev)
 
 
-# pylint: disable=too-many-arguments
-class NetatmoSensor(Entity):
-    """ Implements a netatmo sensor. """
+class NetatmoData(object):
+    """ Connects to the Netatmo api and holds data for sensors. """
 
-    def __init__(self, hass, cred, data_type, module_name):
-        self._hass = hass
+    def __init__(self, cred):
         self._cred = cred
+        self._request = None
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        """ Connects to the Netatmo server end updates the sensor. """
+        token = requests.post("https://api.netatmo.net/oauth2/token",
+                              data={'grant_type': 'password',
+                                    'client_id': self._cred[CONF_ID],
+                                    'client_secret': self._cred[CONF_SECRET],
+                                    'username': self._cred[CONF_USERNAME],
+                                    'password': self._cred[CONF_PASSWORD]},
+                              timeout=10)
+        self._request = requests.get(SENSOR_URL + token.json()["access_token"],
+                                     timeout=10).json()
+
+    def data(self):
+        """ Returns the last request data. """
+        return self._request
+
+
+class NetatmoSensor(Entity):
+    """ Implements a netatmo sensor and parses the data gotten from
+    the data class. """
+
+    def __init__(self, hass, data, data_type, module_name):
+        self._hass = hass
+        self._data = data
         self._state = STATE_UNKNOWN
         self._data_type = data_type
         self._name = module_name
@@ -97,18 +122,10 @@ class NetatmoSensor(Entity):
         """ Returns the state of the device. """
         return self._state
 
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """ Gets the latest data from Netatmo API and updates the state. """
-        request = requests.post("https://api.netatmo.net/oauth2/token",
-                                data={'grant_type': 'password',
-                                      'client_id': self._cred[CONF_ID],
-                                      'client_secret': self._cred[CONF_SECRET],
-                                      'username': self._cred[CONF_USERNAME],
-                                      'password': self._cred[CONF_PASSWORD]},
-                                timeout=10)
-        access_token = request.json()["access_token"]
-        request = requests.get(SENSOR_URL + access_token, timeout=10).json()
+        self._data.update()
+        request = self._data.data()
         for device in request["body"]["devices"]:
             if self._name == device["module_name"]:
                 self._state = device["dashboard_data"][self._data_type]
