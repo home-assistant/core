c615272c06e27ee78424eaf649570be3a3c2e012
pavoni <mail@gregdowling.com>
2016-01-21 23:17:19 +0000

    Tidy.

--- tests/components/sensor/test_template.py
+++ tests/components/sensor/test_template.py
@@ -12,33 +12,28 @@ import homeassistant.core as ha
 import homeassistant.components.sensor as sensor
 
 
-@pytest.mark.usefixtures('betamax_session')
 class TestSensorYr:
     """ Test the Yr sensor. """
 
     def setup_method(self, method):
         self.hass = ha.HomeAssistant()
-        self.hass.config.latitude = 32.87336
-        self.hass.config.longitude = 117.22743
 
     def teardown_method(self, method):
         """ Stop down stuff we started. """
         self.hass.stop()
 
     def test_template(self, betamax_session):
-        with patch('homeassistant.components.sensor.yr.requests.Session',
-                   return_value=betamax_session):
-            assert sensor.setup(self.hass, {
-                'sensor': {
-                    'platform': 'template',
-                    'sensors': {
-                        'test_template_sensor': {
-                            'value_template':
-                                '{{ states.sensor.test_state.state }}'
-                        }
+        assert sensor.setup(self.hass, {
+            'sensor': {
+                'platform': 'template',
+                'sensors': {
+                    'test_template_sensor': {
+                        'value_template':
+                            '{{ states.sensor.test_state.state }}'
                     }
                 }
-            })
+            }
+        })
 
         state = self.hass.states.get('sensor.test_template_sensor')
         assert state.state == ''
