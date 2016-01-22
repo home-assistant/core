875ec1670f926f8cd5f99f038916e4f2ae72c6ad
pavoni <mail@gregdowling.com>
2016-01-22 16:42:12 +0000

    Revise region handling, support iBeacons.
    Tidy.
    
    Don't set location to NOT_HOME when exiting a beacon region.
    
    Ignore zero size zones for GPS location priority.
    
    Improve events lofic, handle iBeacons.
    
    Add locks around critical sections.
    
    Fix typo
    
    Revise locking.
    
    Reduce traces.

--- homeassistant/components/device_tracker/owntracks.py
+++ homeassistant/components/device_tracker/owntracks.py
@@ -8,16 +8,22 @@ https://home-assistant.io/components/device_tracker.owntracks/
 """
 import json
 import logging
+import threading
 
 import homeassistant.components.mqtt as mqtt
 from homeassistant.const import (STATE_HOME, STATE_NOT_HOME)
 
 DEPENDENCIES = ['mqtt']
 
-CONF_TRANSITION_EVENTS = 'use_events'
+STATE_OWNTRACKS_LAST_LOCATION = 'owntracks.location_{}'
+STATE_OWNTRACKS_LAST_LOCATION_ATTR = {'hidden': True}
+
 LOCATION_TOPIC = 'owntracks/+/+'
 EVENT_TOPIC = 'owntracks/+/+/event'
 
+_LOGGER = logging.getLogger(__name__)
+
+LOCK = threading.Lock()
 
 def setup_scanner(hass, config, see):
     """ Set up an OwnTracks tracker. """
@@ -31,17 +37,25 @@ def setup_scanner(hass, config, see):
             data = json.loads(payload)
         except ValueError:
             # If invalid JSON
-            logging.getLogger(__name__).error(
+            _LOGGER.error(
                 'Unable to parse payload as JSON: %s', payload)
             return
 
         if not isinstance(data, dict) or data.get('_type') != 'location':
             return
 
-        parts = topic.split('/')
+        dev_id, host_name = _parse_topic(topic)
+
+        # Block updates if we're in a region
+        LOCK.acquire()
+        if _is_blocked(dev_id):
+            _LOGGER.info("Owntracks update rejected - inside region")
+            LOCK.release()
+            return
+
         kwargs = {
-            'dev_id': '{}_{}'.format(parts[1], parts[2]),
-            'host_name': parts[1],
+            'dev_id': dev_id,
+            'host_name': host_name,
             'gps': (data['lat'], data['lon']),
         }
         if 'acc' in data:
@@ -50,6 +64,8 @@ def setup_scanner(hass, config, see):
             kwargs['battery'] = data['batt']
 
         see(**kwargs)
+        LOCK.release()
+
 
     def owntracks_event_update(topic, payload, qos):
         """ MQTT event (geofences) received. """
@@ -60,47 +76,110 @@ def setup_scanner(hass, config, see):
             data = json.loads(payload)
         except ValueError:
             # If invalid JSON
-            logging.getLogger(__name__).error(
+            _LOGGER.error(
                 'Unable to parse payload as JSON: %s', payload)
             return
 
         if not isinstance(data, dict) or data.get('_type') != 'transition':
             return
 
-        # check if in "home" fence or other zone
-        location = ''
-        if data['event'] == 'enter':
+        dev_id, host_name = _parse_topic(topic)
 
-            if data['desc'].lower() == 'home':
-                location = STATE_HOME
-            else:
-                location = data['desc']
+        if data['desc'].lower() == 'home':
+            location = STATE_HOME
+        else:
+            location = data['desc']
+
+        kwargs = {
+            'dev_id': dev_id,
+            'host_name': host_name,
+            'gps': (data['lat'], data['lon'])
+        }
+
+        if 'acc' in data:
+            kwargs['gps_accuracy'] = data['acc']
+
+        if data['event'] == 'enter':
+            zone = hass.states.get("zone.{}".format(location))
+            LOCK.acquire()
+
+            if zone is not None:
+                kwargs['location_name'] = location
+                _block_updates(dev_id, location)
+
+                if data['t'] == 'b':
+                    # For beacon events - the zone location is more
+                    # accurate than gps
+                    kwargs['gps'] = (
+                        zone.attributes['latitude'],
+                        zone.attributes['longitude'])
+                    kwargs['gps_accuracy'] = 1
+
+            see(**kwargs)
+            LOCK.release()
+
+            # Block location update while we're in a region
 
         elif data['event'] == 'leave':
-            location = STATE_NOT_HOME
+            if not _valid_leave(dev_id, location):
+                return
+
+            see(**kwargs)
+
+            _block_updates(dev_id, '')
+            _force_state_change_if_needed(dev_id, location, kwargs)
         else:
-            logging.getLogger(__name__).error(
+            _LOGGER.error(
                 'Misformatted mqtt msgs, _type=transition, event=%s',
                 data['event'])
             return
 
+    def _valid_leave(dev_id, location):
+        """Check owntracks region is the right one to leave"""
+        state_id = STATE_OWNTRACKS_LAST_LOCATION.format(dev_id)
+        state = hass.states.get(state_id)
+        if state is None:
+            return True
+        entry_location = state.state
+        if entry_location == '':
+            return True
+        if entry_location.lower() == location.lower():
+            return True
+        _LOGGER.info("Owntracks leave region %s rejected - in region %s", location, entry_location)
+        return False
+
+    def _block_updates(dev_id, location):
+        """ Add owntracks region to block location updates """
+        hass.states.set(
+            STATE_OWNTRACKS_LAST_LOCATION.format(dev_id),
+            location,
+            STATE_OWNTRACKS_LAST_LOCATION_ATTR)
+
+    def _is_blocked(dev_id):
+        """Block updates if we're in a region"""
+        state = hass.states.get(
+            STATE_OWNTRACKS_LAST_LOCATION.format(dev_id))
+        blocked = (state is not None and
+                  state.state != '')
+        return blocked
+
+    def _parse_topic(topic):
         parts = topic.split('/')
-        kwargs = {
-            'dev_id': '{}_{}'.format(parts[1], parts[2]),
-            'host_name': parts[1],
-            'gps': (data['lat'], data['lon']),
-            'location_name': location,
-        }
-        if 'acc' in data:
-            kwargs['gps_accuracy'] = data['acc']
+        dev_id = '{}_{}'.format(parts[1], parts[2])
+        host_name = parts[1]
+        return(dev_id, host_name)
 
-        see(**kwargs)
+    def _force_state_change_if_needed(dev_id, location, kwargs):
+        # if gps location didn't set new state, force to away
+        current_location = hass.states.get(
+            "device_tracker.{}".format(dev_id)).state
 
-    use_events = config.get(CONF_TRANSITION_EVENTS)
+        if current_location.lower() == location.lower():
+            kwargs['location_name'] = STATE_NOT_HOME
+            see(**kwargs)
 
-    if use_events:
-        mqtt.subscribe(hass, EVENT_TOPIC, owntracks_event_update, 1)
-    else:
-        mqtt.subscribe(hass, LOCATION_TOPIC, owntracks_location_update, 1)
+    mqtt.subscribe(hass, LOCATION_TOPIC, owntracks_location_update, 1)
+
+    mqtt.subscribe(hass, EVENT_TOPIC, owntracks_event_update, 1)
 
     return True
