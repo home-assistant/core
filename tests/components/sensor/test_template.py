26807b605fd6090a3107b33a923161122b46b633
pavoni <mail@gregdowling.com>
2016-01-22 09:37:20 +0000

    Tidy, add test for ValueException logic.

--- tests/components/sensor/test_template.py
+++ tests/components/sensor/test_template.py
@@ -12,8 +12,8 @@ import homeassistant.core as ha
 import homeassistant.components.sensor as sensor
 
 
-class TestSensorYr:
-    """ Test the Yr sensor. """
+class TestTemplateSensor:
+    """ Test the Template sensor. """
 
     def setup_method(self, method):
         self.hass = ha.HomeAssistant()
@@ -22,14 +22,14 @@ class TestSensorYr:
         """ Stop down stuff we started. """
         self.hass.stop()
 
-    def test_template(self, betamax_session):
+    def test_template(self):
         assert sensor.setup(self.hass, {
             'sensor': {
                 'platform': 'template',
                 'sensors': {
                     'test_template_sensor': {
                         'value_template':
-                            '{{ states.sensor.test_state.state }}'
+                            "{{ states.sensor.test_state.state }}"
                     }
                 }
             }
@@ -42,3 +42,22 @@ class TestSensorYr:
         self.hass.pool.block_till_done()
         state = self.hass.states.get('sensor.test_template_sensor')
         assert state.state == 'Works'
+
+    def test_template_syntax_error(self):
+        assert sensor.setup(self.hass, {
+            'sensor': {
+                'platform': 'template',
+                'sensors': {
+                    'test_template_sensor': {
+                        'value_template':
+                            "{% if rubbish %}"
+                    }
+                }
+            }
+        })
+
+
+        self.hass.states.set('sensor.test_state', 'Works')
+        self.hass.pool.block_till_done()
+        state = self.hass.states.get('sensor.test_template_sensor')
+        assert state.state == 'unknown'
