15c88050e4acb1e79f3fef64469bc31c35e910bc
Oliver van Porten <oliver@van-porten.de>
2016-01-24 22:17:58 +0100

    remove filters directory, move tag to template module

--- homeassistant/util/filters/__init__.py
+++
@@ -1,8 +0,0 @@
-hass_custom_filters = {}
-def custom_filter(fname):
-    def custom_fiter_decorator(func):
-        global hass_custom_filters
-        assert not fname in hass_custom_filters, "Filter %s already registered" % fname
-        hass_custom_filters[fname] = func
-        return func
-    return custom_fiter_decorator
--- homeassistant/util/filters/json.py
+++
@@ -1,8 +0,0 @@
-from homeassistant.util.filters import custom_filter
-import json
-
-
-@custom_filter('json')
-def tojson(value):
-  return json.loads(value)
-
