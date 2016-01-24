6137ca6208816f6bbcefb6333a4e673dacd8934d
Flyte <flyte@failcode.co.uk>
2016-01-24 08:02:14 +0000

    Add zigbee components.

---
+++ homeassistant/components/switch/zigbee.py
@@ -0,0 +1,19 @@
+"""
+homeassistant.components.switch.zigbee
+
+Contains functionality to use a ZigBee device as a switch.
+"""
+from homeassistant.components.zigbee import (
+    ZigBeeDigitalOut, ZigBeeDigitalOutConfig)
+
+
+DEPENDENCIES = ["zigbee"]
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """
+    Create and add an entity based on the configuration.
+    """
+    add_entities([
+        ZigBeeDigitalOut(hass, ZigBeeDigitalOutConfig(config))
+    ])
