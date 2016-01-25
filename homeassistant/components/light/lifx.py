b6d5fa0c44299eb1857ffc2fea173fe084235620
Tim <tim@weeble.info>
2016-01-25 03:34:24 +0000

    Bump version of liffylights

--- homeassistant/components/light/lifx.py
+++ homeassistant/components/light/lifx.py
@@ -28,7 +28,7 @@ from homeassistant.components.light import \
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['liffylights==0.4']
+REQUIREMENTS = ['liffylights==0.5']
 DEPENDENCIES = []
 
 CONF_SERVER = "server"        # server address configuration item
