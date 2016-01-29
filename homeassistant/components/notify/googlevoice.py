8ba434cdde27af4edb86afe8a87dbc27b811d4c5
William Scanlon <scanlonw1@gmail.com>
2016-01-29 14:14:27 -0500

    Added target support for googlevoice

--- homeassistant/components/notify/googlevoice.py
+++ homeassistant/components/notify/googlevoice.py
@@ -9,8 +9,8 @@ https://home-assistant.io/components/notify.free_mobile/
 import logging
 from homeassistant.helpers import validate_config
 from homeassistant.components.notify import (
-    DOMAIN, BaseNotificationService)
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_PHONE_NUMBER
+    DOMAIN, ATTR_TARGET, BaseNotificationService)
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 
 _LOGGER = logging.getLogger(__name__)
 REQUIREMENTS = ['https://github.com/w1ll1am23/pygooglevoice-sms/archive/'
@@ -21,36 +21,32 @@ def get_service(hass, config):
 
     if not validate_config({DOMAIN: config},
                            {DOMAIN: [CONF_USERNAME,
-                                     CONF_PASSWORD,
-                                     CONF_PHONE_NUMBER]},
+                                     CONF_PASSWORD]},
                            _LOGGER):
         return None
 
     return GoogleVoiceSMSNotificationService(config[CONF_USERNAME],
-                                      config[CONF_PASSWORD], config[CONF_PHONE_NUMBER])
+                                      config[CONF_PASSWORD])
 
 
 # pylint: disable=too-few-public-methods
 class GoogleVoiceSMSNotificationService(BaseNotificationService):
     """ Implements notification service for the Google Voice SMS service. """
 
-    def __init__(self, username, password, phone_number):
+    def __init__(self, username, password):
         from googlevoicesms import Voice
         self.voice = Voice()
-        self.number = phone_number
         self.username = username
         self.password = password
 
     def send_message(self, message="", **kwargs):
-        """ Send a message to the Free Mobile user cell. """
-        resp = self.voice.login(self.username, self.password)
-        resp = self.voice.send_sms(self.number, message)
+        """ Send SMS to specified target user cell. """
 
-        if resp.status_code == 400:
-            _LOGGER.error("At least one parameter is missing")
-        elif resp.status_code == 402:
-            _LOGGER.error("Too much SMS send in a few time")
-        elif resp.status_code == 403:
-            _LOGGER.error("Wrong Username/Password")
-        elif resp.status_code == 500:
-            _LOGGER.error("Server error, try later")
+        targets = kwargs.get(ATTR_TARGET)
+        self.voice.login(self.username, self.password)
+        
+        for target in targets:
+            resp = self.voice.send_sms(target, message)
+        
+        self.voice.logout()
+
