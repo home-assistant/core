c163f2e4387e6d3230195c80cb49ddd186a5375a
Paulus Schoutsen <paulus.schoutsen@mycase.com>
2016-01-29 18:44:21 -0800

    Add tests for color util

--- tests/util/test_color.py
+++ tests/util/test_color.py
@@ -20,3 +20,20 @@ class TestColorUtil(unittest.TestCase):
 
         self.assertEqual((0.6400744994567747, 0.3299705106316933),
                          color_util.color_RGB_to_xy(255, 0, 0))
+
+    def test_color_xy_brightness_to_RGB(self):
+        """ Test color_RGB_to_xy. """
+        self.assertEqual((0, 0, 0),
+                         color_util.color_xy_brightness_to_RGB(1, 1, 0))
+
+        self.assertEqual((255, 235, 214),
+                         color_util.color_xy_brightness_to_RGB(.35, .35, 255))
+
+        self.assertEqual((255, 0, 45),
+                         color_util.color_xy_brightness_to_RGB(1, 0, 255))
+
+        self.assertEqual((0, 255, 0),
+                         color_util.color_xy_brightness_to_RGB(0, 1, 255))
+
+        self.assertEqual((0, 83, 255),
+                         color_util.color_xy_brightness_to_RGB(0, 0, 255))
