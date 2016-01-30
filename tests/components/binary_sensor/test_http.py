f1e603af6c2681b50c98062c4447f175243a9aae
Fabian Affolter <fabian@affolter-engineering.ch>
2016-01-30 17:18:40 +0100

    Update tests

--- tests/components/binary_sensor/test_http.py
+++ tests/components/binary_sensor/test_http.py
@@ -2,10 +2,11 @@
 tests.components.binary_sensor.test_http
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Tests Home Assistant HTTP binary sensor does what it should do.
+Tests HTTP binary sensor.
 """
 # pylint: disable=protected-access,too-many-public-methods
 import unittest
+import json
 from unittest.mock import patch
 
 import requests
@@ -21,13 +22,15 @@ from tests.common import get_test_home_assistant
 
 SERVER_PORT = 8127
 HTTP_BASE_URL = "http://127.0.0.1:{}".format(SERVER_PORT)
+API_PASSWORD = "test1234"
+HA_HEADERS = {const.HTTP_HEADER_HA_AUTH: API_PASSWORD}
+ENTITY_ID = 'binary_sensor.test'
 
 hass = None
 
-
-def _url(endpoint=""):
-    """ Helper method to generate URLs. """
-    return '{}/{}/{}'.format(HTTP_BASE_URL, 'api/binary_sensor', endpoint)
+def _url(path=""):
+    """ Helper method to generate urls. """
+    return '{}{}'.format(HTTP_BASE_URL, path)
 
 
 @patch('homeassistant.components.http.util.get_local_ip',
@@ -37,6 +40,7 @@ def setUpModule(mock_get_local_ip):   # pylint: disable=invalid-name
     global hass
 
     hass = get_test_home_assistant()
+    hass.states.set(ENTITY_ID, 'off')
 
     # Set up server
     bootstrap.setup_component(hass, http.DOMAIN, {
@@ -62,45 +66,63 @@ def tearDownModule():   # pylint: disable=invalid-name
     """ Stops the Home Assistant server. """
     hass.stop()
 
+
 class TestBinarySensorHTTP(unittest.TestCase):
     """ Test the HTTP binary sensor. """
 
     def setup_method(self, method):
         self.hass = ha.HomeAssistant()
 
-    def test_wrong_endpoint(self):
-        """ Test with unknown endpoint (GET & POST). """
-        req = requests.get(_url(endpoint="another_sensor"))
-        self.assertEqual(404, req.status_code)
-        req = requests.post(_url(endpoint="another_sensor"))
-        self.assertEqual(404, req.status_code)
-
-    # Doesn't work because something is wrong with the URL...i assume...
-    def test_setting_sensor_value_via_http_message(self):
+    def test_sensor(self):
         """ Test for setting the sensor value. """
-        self.assertTrue(binary_sensor.setup(self.hass, {
+        self.assertTrue(binary_sensor.setup(hass, {
             'binary_sensor': {
                 'platform': 'http',
                 'name': 'test',
-                'endpoint': 'test',
-                'method': 'GET',
-                'value_template': '{{ value_json.payload }}',
             }
         }))
 
-        params_on = {'state': '1'}
-        params_off = {'state': '0'}
+        state = hass.states.get(ENTITY_ID)
+        self.assertEqual(STATE_OFF, state.state)
 
-        req = requests.get(_url(endpoint="test"), params=params_on)
-        self.assertEqual(404, req.status_code)
-        self.hass.states.set('binary_sensor.test', params_on['state'])
-        self.hass.pool.block_till_done()
-        state = self.hass.states.get('binary_sensor.test')
-        self.assertEqual(True, bool(int(state.state)))
+        hass.states.set(ENTITY_ID, 'on')
+        state = hass.states.get('binary_sensor.test')
+        self.assertEqual(STATE_ON, state.state)
 
-        req = requests.get(_url(endpoint="test"), params=params_off)
-        self.assertEqual(404, req.status_code)
-        self.hass.states.set('binary_sensor.test', params_off['state'])
-        self.hass.pool.block_till_done()
-        state = self.hass.states.get('binary_sensor.test')
-        self.assertEqual(False, bool(int(state.state)))
+        hass.states.set(ENTITY_ID, 'off')
+        state = hass.states.get('binary_sensor.test')
+        self.assertEqual(STATE_OFF, state.state)
+
+    def test_api_get_state(self):
+        """ Test if the sensor allows us to get a state. """
+        req = requests.get(
+            _url(const.URL_API_STATES_ENTITY.format(ENTITY_ID)),
+            headers=HA_HEADERS)
+
+        data = ha.State.from_dict(req.json())
+        state = hass.states.get(ENTITY_ID)
+
+        self.assertEqual(state.state, data.state)
+        self.assertEqual(state.last_changed, data.last_changed)
+        self.assertEqual(state.attributes, data.attributes)
+
+    def test_api_set_state(self):
+        """ Test if we can set the state of an binary sensor. """
+        hass.states.set(ENTITY_ID, 'not_to_be_set')
+
+        requests.post(_url(const.URL_API_STATES_ENTITY.format(ENTITY_ID)),
+                      data=json.dumps({"state": "off"}),
+                      headers=HA_HEADERS)
+
+        self.assertEqual("off", hass.states.get(ENTITY_ID).state)
+
+    # pylint: disable=invalid-name
+    def test_api_state_change_with_no_data(self):
+        """ Test if API sends appropriate error if we omit state. """
+        req = requests.post(
+            _url(const.URL_API_STATES_ENTITY.format(
+                ENTITY_ID)),
+            data=json.dumps({}),
+            headers=HA_HEADERS)
+
+        self.assertEqual(400, req.status_code)
