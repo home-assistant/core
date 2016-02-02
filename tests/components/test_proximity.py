a365f8a0124caaefadc310507a9fcf0ddfa9eb6b
Bart274 <de_van_bos@msn.com>
2016-02-02 13:47:47 +0100

    Add first version of test file of proximity component

---
+++ tests/components/test_proximity.py
@@ -0,0 +1,51 @@
+"""
+tests.components.proximity
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Tests proximity component.
+"""
+
+import homeassistant.core as ha
+from homeassistant.components import proximity
+
+from tests.common import (
+    get_test_home_assistant)
+
+class TestProximity:
+    """ Test the Proximity component. """
+
+    def setup_method(self, method):
+        self.hass = ha.HomeAssistant()
+
+    def teardown_method(self, method):
+        """ Stop down stuff we started. """
+        self.hass.stop()
+
+    def test_proximity(self):
+        self.hass = get_test_home_assistant()
+        
+        self.hass.states.set(
+            'zone.home', 'zoning',
+            {
+                'name': 'home',
+                'latitude': 2.1,
+                'longitude': 1.1,
+                'radius': 10
+            })
+    
+        assert proximity.setup(self.hass, {
+            'proximity': {
+                'zone': 'home',
+                'devices': {
+                    '- device_tracker.test'
+                }
+            }
+        })
+
+        state = self.hass.states.get('proximity.home')
+        assert state.state == 'not set'
+
+        self.hass.states.set('proximity.home', '0')
+        self.hass.pool.block_till_done()
+        state = self.hass.states.get('proximity.home')
+        assert state.state == '0'
