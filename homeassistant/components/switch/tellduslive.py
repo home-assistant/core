1d7ccdf858f2b16d805ee51747f011105e96fb66
Erik <saturlight@gmail.com>
2016-02-03 22:31:28 +0100

    reworked telldus live support

--- homeassistant/components/switch/tellduslive.py
+++ homeassistant/components/switch/tellduslive.py
@@ -10,7 +10,6 @@ https://home-assistant.io/components/switch.tellduslive/
 """
 import logging
 
-from homeassistant.const import (STATE_ON, STATE_OFF, STATE_UNKNOWN)
 from homeassistant.components import tellduslive
 from homeassistant.helpers.entity import ToggleEntity
 
@@ -21,54 +20,45 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     """ Find and return Tellstick switches. """
     if discovery_info is None:
         return
-    switches = tellduslive.NETWORK.get_switches()
-    add_devices([TelldusLiveSwitch(switch["name"],
-                                   switch["id"])
-                 for switch in switches if switch["type"] == "device"])
+    add_devices(TelldusLiveSwitch(switch) for switch in discovery_info)
 
 
 class TelldusLiveSwitch(ToggleEntity):
     """ Represents a Tellstick switch. """
 
-    def __init__(self, name, switch_id):
-        self._name = name
+    def __init__(self, switch_id):
         self._id = switch_id
-        self._state = STATE_UNKNOWN
         self.update()
+        _LOGGER.debug("created switch %s", self)
+
+    def update(self):
+        tellduslive.NETWORK.update_switches()
+        self._switch = tellduslive.NETWORK.get_switch(self._id)
 
     @property
     def should_poll(self):
         """ Tells Home Assistant to poll this entity. """
-        return False
+        return True
 
     @property
     def name(self):
         """ Returns the name of the switch if any. """
-        return self._name
+        return self._switch["name"]
 
-    def update(self):
-        from tellive.live import const
-        state = tellduslive.NETWORK.get_switch_state(self._id)
-        if state == const.TELLSTICK_TURNON:
-            self._state = STATE_ON
-        elif state == const.TELLSTICK_TURNOFF:
-            self._state = STATE_OFF
-        else:
-            self._state = STATE_UNKNOWN
+    @property
+    def available(self):
+        return not self._switch.get("offline", False)
 
     @property
     def is_on(self):
         """ True if switch is on. """
-        return self._state == STATE_ON
+        from tellive.live import const
+        return self._switch["state"] == const.TELLSTICK_TURNON
 
     def turn_on(self, **kwargs):
         """ Turns the switch on. """
-        if tellduslive.NETWORK.turn_switch_on(self._id):
-            self._state = STATE_ON
-            self.update_ha_state()
+        tellduslive.NETWORK.turn_switch_on(self._id)
 
     def turn_off(self, **kwargs):
         """ Turns the switch off. """
-        if tellduslive.NETWORK.turn_switch_off(self._id):
-            self._state = STATE_OFF
-            self.update_ha_state()
+        tellduslive.NETWORK.turn_switch_off(self._id)
