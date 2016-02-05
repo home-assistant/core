33996f2f91700b59263b0a87fdfa2aaa99cbb48f
Cameron Bulock <cbulock@liquidweb.com>
2016-02-05 18:16:42 -0500

    Added ability to set color temp on Wink lights

--- homeassistant/components/light/wink.py
+++ homeassistant/components/light/wink.py
@@ -8,11 +8,16 @@ https://home-assistant.io/components/light.wink/
 """
 import logging
 
-from homeassistant.components.light import ATTR_BRIGHTNESS
+from homeassistant.components.light import (ATTR_BRIGHTNESS, ATTR_COLOR_TEMP)
 from homeassistant.components.wink import WinkToggleDevice
 from homeassistant.const import CONF_ACCESS_TOKEN
 
 REQUIREMENTS = ['python-wink==0.4.2']
+
+TEMP_MIN = 1900               # Wink minimum temperature
+TEMP_MAX = 6500               # Wink maximum temperature
+TEMP_MIN_HASS = 154           # home assistant minimum temperature
+TEMP_MAX_HASS = 500           # home assistant maximum temperature
 
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
@@ -40,13 +45,20 @@ class WinkLight(WinkToggleDevice):
     # pylint: disable=too-few-public-methods
     def turn_on(self, **kwargs):
         """ Turns the switch on. """
+
+        temp_kelvin = None
+
         brightness = kwargs.get(ATTR_BRIGHTNESS)
 
-        if brightness is not None:
-            self.wink.set_state(True, brightness=brightness / 255)
+        if ATTR_COLOR_TEMP in kwargs:
+            temp_kelvin = int(((TEMP_MAX - TEMP_MIN) *
+                               (kwargs[ATTR_COLOR_TEMP] - TEMP_MIN_HASS) /
+                               (TEMP_MAX_HASS - TEMP_MIN_HASS)) + TEMP_MIN)
 
-        else:
-            self.wink.set_state(True)
+        if brightness is not None:
+            brightness = brightness / 255
+
+        self.wink.set_state(True, brightness, temp_kelvin)
 
     @property
     def state_attributes(self):
@@ -54,8 +66,14 @@ class WinkLight(WinkToggleDevice):
 
         if self.is_on:
             brightness = self.wink.brightness()
+            temp_kelvin = self.wink.color_temperature_kelvin()
 
             if brightness is not None:
                 attr[ATTR_BRIGHTNESS] = int(brightness * 255)
+            if temp_kelvin is not None:
+                attr[ATTR_COLOR_TEMP] = int(TEMP_MIN_HASS +
+                                            (TEMP_MAX_HASS - TEMP_MIN_HASS) *
+                                            (temp_kelvin - TEMP_MIN) /
+                                            (TEMP_MAX - TEMP_MIN))
 
         return attr
