1158cb966b0ee096ee633d09eb2dde65594688b7
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-02-04 22:26:02 -0800

    Mock util.location by default

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,10 +2,23 @@ import betamax
 
 from homeassistant.util import location
 
-from .common import mock_detect_location_info
-
 with betamax.Betamax.configure() as config:
     config.cassette_library_dir = 'tests/cassettes'
 
-# This hits a 3rd party server. Always mock it.
-location.detect_location_info = mock_detect_location_info
+# Automatically called during different setups. Too often forgotten
+# so mocked by default.
+location.detect_location_info = lambda: location.LocationInfo(
+    ip='1.1.1.1',
+    country_code='US',
+    country_name='United States',
+    region_code='CA',
+    region_name='California',
+    city='San Diego',
+    zip_code='92122',
+    time_zone='America/Los_Angeles',
+    latitude='2.0',
+    longitude='1.0',
+    use_fahrenheit=True,
+)
+
+location.elevation = lambda latitude, longitude: 0
