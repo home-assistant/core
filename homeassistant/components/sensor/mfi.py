49cdf51fc6f632e1ba1813a3a1355f8f1c484982
Fabian Affolter <fabian@affolter-engineering.ch>
2016-02-07 11:52:17 +0100

    Move configuration details to docs

--- homeassistant/components/sensor/mfi.py
+++ homeassistant/components/sensor/mfi.py
@@ -1,36 +1,11 @@
 """
 homeassistant.components.sensor.mfi
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Support for Ubiquiti mFi Sensors.
+Support for Ubiquiti mFi sensors.
 
-Configuration:
-
-sensor:
-  platform: mfi
-  host: ADDRESS
-  port: PORT
-  username: USERNAME
-  password: PASSWORD
-
-Variable:
-
-host
-*Required
-ADDRESS is the IP or hostname of your mFi controller.
-
-port
-*Optional
-PORT is the port of your mFi controller (usually 6443)
-
-username
-*Required
-USERNAME is the mFi admin username
-
-password
-*Required
-PASSWORD is the mFi admin user's password
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.mfi/
 """
-
 import logging
 
 from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD,
