dfd4364c35489a619e38007f7e6fa2da43f1b96f
t30 <mett_1@hotmail.com>
2016-02-18 02:39:23 +0000

    Clean up code + fix

--- homeassistant/components/rollershutter/command_rollershutter.py
+++ homeassistant/components/rollershutter/command_rollershutter.py
@@ -15,28 +15,23 @@ from homeassistant.util import template
 
 _LOGGER = logging.getLogger(__name__)
 
-# DEFAULT_NAME = "Command Rollershutter"
-
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """ Find and return rollershutter controlled by shell commands. """
 
     rollershutters = config.get('rollershutters', {})
     devices = []
-#    logger = logging.getLogger(__name__)
 
     for dev_name, properties in rollershutters.items():
         devices.append(
             CommandRollershutter(
                 hass,
                 properties.get('name', dev_name),
-                # properties.get('name', DEFAULT_NAME),
                 properties.get('upcmd', 'true'),
                 properties.get('downcmd', 'true'),
                 properties.get('stopcmd', 'true'),
                 properties.get('statecmd', False),
-                properties.get(CONF_VALUE_TEMPLATE, False)))
-
+                properties.get(CONF_VALUE_TEMPLATE, '{{ value }}')))
     add_devices_callback(devices)
 
 
@@ -50,7 +45,7 @@ class CommandRollershutter(RollershutterDevice):
 
         self._hass = hass
         self._name = name
-        self._state = 0  # False
+        self._state = None  # Unknown
         self._command_up = command_up
         self._command_down = command_down
         self._command_stop = command_stop
@@ -58,7 +53,7 @@ class CommandRollershutter(RollershutterDevice):
         self._value_template = value_template
 
     @staticmethod
-    def _rollershutter(command):
+    def _move_rollershutter(command):
         """ Execute the actual commands. """
         _LOGGER.info('Running command: %s', command)
 
@@ -80,17 +75,10 @@ class CommandRollershutter(RollershutterDevice):
         except subprocess.CalledProcessError:
             _LOGGER.error('Command failed: %s', command)
 
-    @staticmethod
-    def _query_state_code(command):
-        """ Execute state command for return code. """
-        _LOGGER.info('Running state command: %s', command)
-        return subprocess.call(command, shell=True) == 0
-
     @property
     def should_poll(self):
         """ Only poll if we have statecmd. """
         return self._command_state is not None
-#        return False
 
     @property
     def name(self):
@@ -103,7 +91,6 @@ class CommandRollershutter(RollershutterDevice):
         Return current position of rollershutter.
         None is unknown, 0 is closed, 100 is fully open.
         """
-#        return None
         return self._state
 
     def _query_state(self):
@@ -111,9 +98,7 @@ class CommandRollershutter(RollershutterDevice):
         if not self._command_state:
             _LOGGER.error('No state command specified')
             return
-        if self._value_template:
-            return self._query_state_value(self._command_state)
-        return self._query_state_code(self._command_state)
+        return self._query_state_value(self._command_state)
 
     def update(self):
         """ Update device state. """
@@ -126,24 +111,12 @@ class CommandRollershutter(RollershutterDevice):
 
     def move_up(self, **kwargs):
         """ Move the rollershutter up. """
-        self._rollershutter(self._command_up)
-#        if (self._rollershutter(self._command_up) and
-#                not self._command_state):
-#            self._state = True  # Up
-#            self.update_ha_state()
+        self._move_rollershutter(self._command_up)
 
     def move_down(self, **kwargs):
         """ Move the rollershutter down. """
-        self._rollershutter(self._command_down)
-#        if (self._rollershutter(self._command_down) and
-#                not self._command_state):
-#            self._state = True  # Down
-#            self.update_ha_state()
+        self._move_rollershutter(self._command_down)
 
     def stop(self, **kwargs):
         """ Stop the device. """
-        self._rollershutter(self._command_stop)
-#        if (self._rollershutter(self._command_stop) and
-#                not self._command_state):
-#            self._state = False  # Stop
-#            self.update_ha_state()
+        self._move_rollershutter(self._command_stop)
--- tests/components/rollershutter/test_command_rollershutter.py
+++ tests/components/rollershutter/test_command_rollershutter.py
@@ -40,6 +40,53 @@ class TestCommandRollerShutter(unittest.TestCase):
             self.assertEqual('foo bar', result)
             mock_run.assert_called_once_with('runme', shell=True)
 
+    def test_state_attributes_current_position(self):
+        # self.assertTrue(rollershutter.setup(self.hass, {
+        add_devices = mock.MagicMock()
+        self.assertTrue(cmd_rs.setup_platform(self.hass, {
+            'rollershutter': {
+                'platform': 'command_rollershutter',
+                'name': 'test',
+                'upcmd': 'up-cmd',
+                'downcmd': 'down-cmd',
+                'stopcmd': 'stop-cmd',
+                'statecmd': 'state-cmd'
+            }
+        # }))
+        }, add_devices))
+
+        state_attributes_dict = self.hass.states.get(
+            'rollershutter.test').attributes
+        self.assertFalse('current_position' in state_attributes_dict)
+
+        # fire_mqtt_message(self.hass, 'state-topic', '0')
+        fire_state_changed(self.hass, 'state-cmd', '0')
+        self.hass.pool.block_till_done()
+        current_position = self.hass.states.get(
+            'rollershutter.test').attributes['current_position']
+        self.assertEqual(0, current_position)
+
+        # fire_mqtt_message(self.hass, 'state-topic', '50')
+        fire_state_changed(self.hass, 'state-cmd', '50')
+        self.hass.pool.block_till_done()
+        current_position = self.hass.states.get(
+            'rollershutter.test').attributes['current_position']
+        self.assertEqual(50, current_position)
+
+        # fire_mqtt_message(self.hass, 'state-topic', '101')
+        fire_state_changed(self.hass, 'state-cmd', '101')
+        self.hass.pool.block_till_done()
+        current_position = self.hass.states.get(
+            'rollershutter.test').attributes['current_position']
+        self.assertEqual(50, current_position)
+
+        # fire_mqtt_message(self.hass, 'state-topic', 'non-numeric')
+        fire_state_changed(self.hass, 'state-cmd', 'non-numeric')
+        self.hass.pool.block_till_done()
+        current_position = self.hass.states.get(
+            'rollershutter.test').attributes['current_position']
+        self.assertEqual(50, current_position)
+
     def test_state_value(self):
 #        add_devices = mock.MagicMock()
         with tempfile.TemporaryDirectory() as tempdirname:
@@ -64,7 +111,7 @@ class TestCommandRollerShutter(unittest.TestCase):
 #            }, add_devices))
 
             state = self.hass.states.get('rollershutter.test')
-            self.assertEqual('closed', state.state)
+            self.assertEqual('unknown', state.state)
 
             rollershutter.move_up(self.hass, 'rollershutter.test')
             self.hass.pool.block_till_done()
