b64b113268f4228a851f09a4835265818e95a0c0
William Scanlon <scanlonw1@gmail.com>
2016-02-21 12:01:32 -0500

    Update wink version

--- homeassistant/components/wink.py
+++ homeassistant/components/wink.py
@@ -16,7 +16,7 @@ from homeassistant.helpers.entity import ToggleEntity
 from homeassistant.loader import get_component
 
 DOMAIN = "wink"
-REQUIREMENTS = ['python-wink==0.6.0']
+REQUIREMENTS = ['python-wink==0.6.1']
 
 DISCOVER_LIGHTS = "wink.lights"
 DISCOVER_SWITCHES = "wink.switches"
