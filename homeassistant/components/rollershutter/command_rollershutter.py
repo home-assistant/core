cc43b88c86abc46ac7b915f74521ebc794064bb6
t30 <mett_1@hotmail.com>
2016-02-21 22:11:30 +0000

    sync with last command_rollershutter files

--- homeassistant/components/rollershutter/command_rollershutter.py
+++ homeassistant/components/rollershutter/command_rollershutter.py
@@ -15,28 +15,24 @@ from homeassistant.util import template
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "Command Rollershutter"
-
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """ Find and return rollershutter controlled by shell commands. """
 
-    devices = config.get('rollershutters', {})
-    rollershutters = []
-#    logger = logging.getLogger(__name__)
+    rollershutters = config.get('rollershutters', {})
+    devices = []
 
-    for dev_name, properties in devices.items():
-        rollershutters.append(
+    for dev_name, properties in rollershutters.items():
+        devices.append(
             CommandRollershutter(
                 hass,
                 properties.get('name', dev_name),
                 properties.get('upcmd', 'true'),
                 properties.get('downcmd', 'true'),
                 properties.get('stopcmd', 'true'),
-                properties.get('statecmd', 'true'),
-                properties.get(CONF_VALUE_TEMPLATE, False)))
-
-    add_devices_callback(rollershutters)
+                properties.get('statecmd', False),
+                properties.get(CONF_VALUE_TEMPLATE, '{{ value }}')))
+    add_devices_callback(devices)
 
 
 # pylint: disable=too-many-arguments, too-many-instance-attributes
@@ -49,7 +45,7 @@ class CommandRollershutter(RollershutterDevice):
 
         self._hass = hass
         self._name = name
-        self._state = False
+        self._state = None  # Unknown
         self._command_up = command_up
         self._command_down = command_down
         self._command_stop = command_stop
@@ -57,7 +53,7 @@ class CommandRollershutter(RollershutterDevice):
         self._value_template = value_template
 
     @staticmethod
-    def _rollershutter(command):
+    def _move_rollershutter(command):
         """ Execute the actual commands. """
         _LOGGER.info('Running command: %s', command)
 
@@ -79,17 +75,10 @@ class CommandRollershutter(RollershutterDevice):
         except subprocess.CalledProcessError:
             _LOGGER.error('Command failed: %s', command)
 
-    @staticmethod
-    def _query_state_code(command):
-        """ Execute state command for return code. """
-        _LOGGER.info('Running state command: %s', command)
-        return subprocess.call(command, shell=True) == 0
-
     @property
     def should_poll(self):
         """ Only poll if we have statecmd. """
         return self._command_state is not None
-#        return False
 
     @property
     def name(self):
@@ -102,7 +91,6 @@ class CommandRollershutter(RollershutterDevice):
         Return current position of rollershutter.
         None is unknown, 0 is closed, 100 is fully open.
         """
-#        return None
         return self._state
 
     def _query_state(self):
@@ -110,9 +98,7 @@ class CommandRollershutter(RollershutterDevice):
         if not self._command_state:
             _LOGGER.error('No state command specified')
             return
-        if self._value_template:
-            return CommandRollershutter._query_state_value(self._command_state)
-        return CommandRollershutter._query_state_code(self._command_state)
+        return self._query_state_value(self._command_state)
 
     def update(self):
         """ Update device state. """
@@ -121,26 +107,16 @@ class CommandRollershutter(RollershutterDevice):
             if self._value_template:
                 payload = template.render_with_possible_json_value(
                     self._hass, self._value_template, payload)
-            self._state = (payload.lower() == "true")
+            self._state = int(payload)
 
     def move_up(self, **kwargs):
         """ Move the rollershutter up. """
-#        CommandRollershutter._rollershutter(self._command_up)
-        if (CommandRollershutter._rollershutter(self._command_up) and
-                not self._command_state):
-            self._state = True  # Up
-            self.update_ha_state()
+        self._move_rollershutter(self._command_up)
 
     def move_down(self, **kwargs):
         """ Move the rollershutter down. """
-        if (CommandRollershutter._rollershutter(self._command_down) and
-                not self._command_state):
-            self._state = True  # Down
-            self.update_ha_state()
+        self._move_rollershutter(self._command_down)
 
     def stop(self, **kwargs):
         """ Stop the device. """
-        if (CommandRollershutter._rollershutter(self._command_stop) and
-                not self._command_state):
-            self._state = False  # Stop
-            self.update_ha_state()
+        self._move_rollershutter(self._command_stop)
