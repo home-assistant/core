5a3857631b383cd7165060f6d8d3c1d30faeb4fa
Gert <gert.joos@gmail.com>
2016-02-26 12:16:25 +0100

    Update teamspeak.py
    Fixed several whitespace and formatting problems
    Moved import

--- homeassistant/components/sensor/teamspeak.py
+++ homeassistant/components/sensor/teamspeak.py
@@ -10,7 +10,6 @@ import logging
 from homeassistant.helpers.entity import Entity
 
 REQUIREMENTS = ['ts3==0.7.1']
-import ts3
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = 'Teamspeak Server'
@@ -27,7 +26,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     host = config.get('host', None)
     username = config.get('username', None)
     password = config.get('password', None)
-    virtualserver_id = config.get('virtualserver_id',0)
+    virtualserver_id = config.get('virtualserver_id', 0)
 
     if host is None:
         _LOGGER.error('No teamspeak host specified')
@@ -37,26 +36,29 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         return
     elif password is None:
         _LOGGER.error('No teamspeak password specified')
-        return    
-        
-    add_devices([TeamspeakSensor(name,host,username,password,virtualserver_id)])
+        return
+
+    add_devices([
+        TeamspeakSensor(name, host, username, password, virtualserver_id)
+    ])
 
 
 class TeamspeakSensor(Entity):
     """ A Teamspeak Server sensor. """
 
-    def __init__(self, name,host,username,password,virtual_server_id):
+    def __init__(self, name, host, username, password, virtual_server_id):
+        import ts3
         self._name = name
         self._state = None
         self._unit_of_measurement = 'Users'
         self.info = None
-        
+
         self._host = host
         self._username = username
         self._password = password
         self._virtual_server_id = virtual_server_id
 
-        self._ts3conn = ts3.query.TS3Connection(self._host)        
+        self._ts3conn = ts3.query.TS3Connection(self._host)
 
         self.connect()
         self.update()
@@ -93,26 +95,31 @@ class TeamspeakSensor(Entity):
         return ICON
 
     def connect(self):
-        # Note, that the client will wait for the response and raise a
-        # **TS3QueryError** if the error id of the response is not 0.
         try:
             self._ts3conn.login(
-            client_login_name=self._username,
-            client_login_password=self._password
+                client_login_name=self._username,
+                client_login_password=self._password
             )
             self._ts3conn.use(sid=1)
-            return True            
+            return True
         except ts3.query.TS3QueryError as err:
             _LOGGER.error("Teamspeak Login failed:" + err.resp.error["msg"])
             return False
-
-                
 
     def update(self):
         """ Gets the latest data and updates the state. """
         if not self._ts3conn.is_connected():
             if not self.connect():
                 return
-        usersOnline = int(self._ts3conn.serverlist()[self._virtual_server_id]['virtualserver_clientsonline']) - int(self._ts3conn.serverlist()[self._virtual_server_id]['virtualserver_queryclientsonline'])
+
+        clientsOnline = int(
+            self._ts3conn.serverlist()
+            [self._virtual_server_id]['virtualserver_clientsonline']
+        )
+        queryClientsOnline = int(
+            self._ts3conn.serverlist()
+            [self._virtual_server_id]['virtualserver_queryclientsonline']
+        )
+        usersOnline = clientsOnline - queryClientsOnline
         self._state = usersOnline
         self.info = self._ts3conn.serverlist()[self._virtual_server_id]
