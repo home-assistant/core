2aef0b46600186b33a4a04ee0e03ac651d708eff
Jan Harkes <jaharkes@cs.cmu.edu>
2016-03-01 23:25:18 -0500

    Update netdisco to 0.5.4

--- homeassistant/components/discovery.py
+++ homeassistant/components/discovery.py
@@ -15,7 +15,7 @@ from homeassistant.const import (
     EVENT_PLATFORM_DISCOVERED)
 
 DOMAIN = "discovery"
-REQUIREMENTS = ['netdisco==0.5.3']
+REQUIREMENTS = ['netdisco==0.5.4']
 
 SCAN_INTERVAL = 300  # seconds
 
