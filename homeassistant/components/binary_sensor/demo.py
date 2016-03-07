f85d8cfdb5e6d7e9455bab112681cfff6f78fe6e
Fabian Affolter <fabian@affolter-engineering.ch>
2016-03-07 20:21:08 +0100

    Fix PEP257 issues

--- homeassistant/components/binary_sensor/demo.py
+++ homeassistant/components/binary_sensor/demo.py
@@ -17,7 +17,9 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 class DemoBinarySensor(BinarySensorDevice):
     """A Demo binary sensor."""
+
     def __init__(self, name, state, sensor_class):
+        """Initialize the demo sensor."""
         self._name = name
         self._state = state
         self._sensor_type = sensor_class
