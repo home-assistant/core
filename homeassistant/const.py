a6d083e0b8c83d69ac11762faee12a103a3dde6e
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-03-26 01:02:30 -0700

    Version bump to 0.17.0.dev0

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 """Constants used by Home Assistant components."""
 
-__version__ = "0.16.0"
+__version__ = "0.17.0.dev0"
 REQUIRED_PYTHON_VER = (3, 4)
 
 # Can be used to specify a catch all when registering state or event listeners.
