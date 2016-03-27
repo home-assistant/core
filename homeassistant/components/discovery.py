23a5782655e826ee4206b143eb66609cdb68205b
Daniel J. Kemp <Daniel.Kemp@especiallyrelative.net>
2016-03-27 14:41:52 -0400

    Add onkyo receiver, and source select support
    Added onkyo receiver component
    
    Added support for input source selection to media players,
    and do the onkyo receiver component.

--- homeassistant/components/discovery.py
+++ homeassistant/components/discovery.py
@@ -25,6 +25,7 @@ SERVICE_CAST = 'google_cast'
 SERVICE_NETGEAR = 'netgear_router'
 SERVICE_SONOS = 'sonos'
 SERVICE_PLEX = 'plex_mediaserver'
+SERVICE_ONKYO = 'onkyo'
 SERVICE_SQUEEZEBOX = 'logitech_mediaserver'
 
 SERVICE_HANDLERS = {
@@ -34,6 +35,7 @@ SERVICE_HANDLERS = {
     SERVICE_NETGEAR: 'device_tracker',
     SERVICE_SONOS: 'media_player',
     SERVICE_PLEX: 'media_player',
+    SERVICE_ONKYO: 'media_player',
     SERVICE_SQUEEZEBOX: 'media_player',
 }
 
