82e39328214fb55e0d186b5e8dfabbd06e466325
Hydreliox <hydreliox@gmail.com>
2016-04-01 19:36:26 +0200

    Fix for PR

--- homeassistant/components/sensor/t411.py
+++ homeassistant/components/sensor/t411.py
@@ -11,13 +11,8 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 from datetime import timedelta
 import logging
-import math
-
 
 REQUIREMENTS = ['T411API==0.1.5', 'humanize==0.5.1']
-
-import t411api
-import humanize
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -25,6 +20,7 @@ _LOGGER = logging.getLogger(__name__)
 # Data needed is not very flutuates very quickly so
 # updating more than once per hour seems useless
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=3600)
+
 
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -35,13 +31,13 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
                            _LOGGER):
         return False
 
+    import t411api
     api = t411api.T411API()
     try:
         api.connect(config.get(CONF_USERNAME, None), config.get(CONF_PASSWORD, None))
     except t411api.API.APIError as inst:
         _LOGGER.error(inst.args)
         return False
-
 
     add_devices([
         T411Sensor('username', api, config.get(CONF_USERNAME, None), config.get(CONF_PASSWORD, None)),
@@ -69,6 +65,8 @@ class T411Sensor(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
+        import humanize
+        import math
         if (self._name == 'username'):
             return self._t411.user()['username']
         elif (self._name == 'downloaded'):
@@ -80,7 +78,7 @@ class T411Sensor(Entity):
         elif (self._name == 'ratio'):
             down = float(self._t411.user()['downloaded'])
             up = float(self._t411.user()['uploaded'])
-            ratio = math.floor(((up / down) * 100))/100
+            ratio = math.floor(((up / down) * 100)) / 100
             return ratio
         else:
             return None
@@ -101,6 +99,7 @@ class T411Sensor(Entity):
 
     @property
     def unit_of_measurement(self):
+        import humanize
         """Return the unit this state is expressed in."""
         if (self._name == 'downloaded'):
             down = float(self._t411.user()['downloaded'])
@@ -114,6 +113,7 @@ class T411Sensor(Entity):
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """Call the T411 API to update the data."""
+        import t411api
         try:
             self._t411.connect(self._username, self._password)
         except t411api.API.APIError as inst:
