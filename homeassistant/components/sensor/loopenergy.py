ee61f948103356b0413c683d24c5af6fd08dcdd0
pavoni <mail@gregdowling.com>
2016-04-02 11:04:09 +0100

    Bump pyloopenergy version - to support reconnect.

--- homeassistant/components/sensor/loopenergy.py
+++ homeassistant/components/sensor/loopenergy.py
@@ -13,7 +13,7 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "loopenergy"
 
-REQUIREMENTS = ['pyloopenergy==0.0.6']
+REQUIREMENTS = ['pyloopenergy==0.0.7']
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
