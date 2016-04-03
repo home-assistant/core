f6d584af096d8f7bc03b600b2ec35ae2681655bc
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-04-02 20:10:57 -0700

    Allow platforms to specify PLATFORM_SCHEMA

--- homeassistant/helpers/entity_component.py
+++ homeassistant/helpers/entity_component.py
@@ -79,24 +79,22 @@ class EntityComponent(object):
         platform = prepare_setup_platform(
             self.hass, self.config, self.domain, platform_type)
 
-        if platform is None:
-            return
+        # Config > Platform > Component
+        scan_interval = platform_config.get(
+            CONF_SCAN_INTERVAL,
+            getattr(platform, 'SCAN_INTERVAL', self.scan_interval))
 
         try:
-            # Config > Platform > Component
-            scan_interval = platform_config.get(
-                CONF_SCAN_INTERVAL,
-                getattr(platform, 'SCAN_INTERVAL', self.scan_interval))
             platform.setup_platform(
                 self.hass, platform_config,
                 EntityPlatform(self, scan_interval).add_entities,
                 discovery_info)
-            platform_name = '{}.{}'.format(self.domain, platform_type)
-            self.hass.config.components.append(platform_name)
+
+            self.hass.config.components.append(
+                '{}.{}'.format(self.domain, platform_type))
         except Exception:  # pylint: disable=broad-except
             self.logger.exception(
                 'Error while setting up platform %s', platform_type)
-            return
 
     def add_entity(self, entity):
         """Add entity to component."""
