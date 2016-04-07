e22eb32136d0e9312fb4b3497293fa4420de6f3d
Jan Harkes <jaharkes@cs.cmu.edu>
2016-04-07 11:33:32 -0400

    Be flexible in what we accept for script.delay configuration.
    Accept delay configuration even when someone forgets to indent the time
    specification.
    
    Also removed 'weeks' and 'microseconds' from acceptable delay values.
    There is a new homeassistant release every 2 weeks and running scripts
    are not persisting across restarts. And there is still the option of
    using (weeks*7) days if the long delay is really necessary.
    
    And if someone really depends on microsecond delay precision we are
    unlikely to be able to provide this accuracy, even milliseconds is
    suspect for that matter but will at least allow us to specify some
    subsecond delay.

--- tests/components/test_script.py
+++ tests/components/test_script.py
@@ -206,6 +206,45 @@ class TestScript(unittest.TestCase):
 
         self.assertEqual(2, len(calls))
 
+    def test_alt_delay(self):
+        """Test alternative delay config format."""
+        event = 'test_event'
+        calls = []
+
+        def record_event(event):
+            """Add recorded event to set."""
+            calls.append(event)
+
+        self.hass.bus.listen(event, record_event)
+
+        assert _setup_component(self.hass, 'script', {
+            'script': {
+                'test': {
+                    'sequence': [{
+                        'event': event,
+                    }, {
+                        'delay': None,
+                        'seconds': 5
+                    }, {
+                        'event': event,
+                    }]
+                }
+            }
+        })
+
+        script.turn_on(self.hass, ENTITY_ID)
+        self.hass.pool.block_till_done()
+
+        self.assertTrue(script.is_on(self.hass, ENTITY_ID))
+        self.assertEqual(1, len(calls))
+
+        future = dt_util.utcnow() + timedelta(seconds=5)
+        fire_time_changed(self.hass, future)
+        self.hass.pool.block_till_done()
+
+        self.assertFalse(script.is_on(self.hass, ENTITY_ID))
+        self.assertEqual(2, len(calls))
+
     def test_cancel_while_delay(self):
         """Test the cancelling while the delay is present."""
         event = 'test_event'
