3899d29ed80bb1b6b69938413626c6e7864f9e8d
xifle <github@mailiverse.eu>
2016-04-09 18:55:45 +0200

    Added first (non-working) implementation for mold_indicator test

---
+++ tests/components/sensor/test_moldindicator.py
@@ -0,0 +1,42 @@
+"""The tests for the MoldIndicator sensor"""
+import unittest
+
+import homeassistant.components.sensor as sensor
+from homeassistant.const import (ATTR_UNIT_OF_MEASUREMENT,
+                                 TEMP_CELCIUS, TEMP_FAHRENHEIT)
+
+from tests.common import get_test_home_assistant
+
+
+class TestSensorMoldIndicator(unittest.TestCase):
+    """Test the MoldIndicator sensor."""
+
+    def setUp(self):  # pylint: disable=invalid-name
+        """Setup things to be run when tests are started."""
+        self.hass = get_test_home_assistant()
+        self.hass.states.set('test.indoortemp', '20',
+                             {ATTR_UNIT_OF_MEASUREMENT: TEMP_CELCIUS})
+        self.hass.states.set('test.outdoortemp', '10',
+                             {ATTR_UNIT_OF_MEASUREMENT: TEMP_CELCIUS})
+        self.hass.states.set('test.indoorhumidity', '50',
+                             {ATTR_UNIT_OF_MEASUREMENT: '%'})
+
+    def tearDown(self):  # pylint: disable=invalid-name
+        """Stop down everything that was started."""
+        self.hass.stop()
+
+    def test_calculation(self):
+        """Test the mold indicator sensor"""
+        self.assertTrue(sensor.setup(self.hass, {
+            'sensor': {
+                'platform': 'mold_indicator',
+                'indoor_temp_sensor': 'test.indoortemp',
+                'outdoor_temp_sensor': 'test.outdoortemp',
+                'indoor_humidity_sensor': 'test.indoorhumidity',
+                'calibration_factor': 2.0
+            }
+        }))
+
+        moldind = self.hass.states.get('sensor.moldindicator')
+        print(moldind.state)
+       # self.assertEqual('')
