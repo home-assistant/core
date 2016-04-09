ff2aff332f521d92c9f37405878cbc754f488236
Teagan M. Glenn <that@teagantotally.rocks>
2016-04-09 13:50:08 -0600

    Create Devices
    Convert api devices to insteon devices and use filter helper method

--- homeassistant/components/light/insteon_hub.py
+++ homeassistant/components/light/insteon_hub.py
@@ -4,15 +4,14 @@ Support for Insteon Hub lights.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/insteon_hub/
 """
-from homeassistant.components.insteon_hub import INSTEON, InsteonToggleDevice
+from homeassistant.components.insteon_hub import (INSTEON, InsteonToggleDevice, filter)
 
-
+DEVICE_CATEGORIES = [{
+    'DevCat': 1,
+    'SubCat': [46]}]
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the Insteon Hub light platform."""
     devs = []
-    for device in INSTEON.devices:
-        if device.DeviceCategory == "Switched Lighting Control":
-            devs.append(InsteonToggleDevice(device))
-        if device.DeviceCategory == "Dimmable Lighting Control":
-            devs.append(InsteonToggleDevice(device))
+    for device in filter(INSTEON.devices, DEVICE_CATEGORIES):
+        devs.append(InsteonToggleDevice(device))
     add_devices(devs)
