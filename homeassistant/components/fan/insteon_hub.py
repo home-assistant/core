bd76c9aa587060add3be0520bee02869d6909d09
Teagan M. Glenn <that@teagantotally.rocks>
2016-04-10 15:04:38 -0600

    Insteon Fans
    Filter devices for insteon fans

---
+++ homeassistant/components/fan/insteon_hub.py
@@ -0,0 +1,21 @@
+"""
+Support for Insteon Hub lights.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/insteon_hub/
+"""
+from homeassistant.components.insteon_hub import (INSTEON, InsteonFanDevice, filter)
+
+DEVICE_CATEGORIES=[
+    {
+        'DevCat': 1,
+        'SubCat': [46]
+    }
+]
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the Insteon Hub light platform."""
+    devs = []
+    for device in filter(INSTEON.devices, DEVICE_CATEGORIES):
+        devs.append(InsteonFanDevice(device))
+    add_devices(devs)
