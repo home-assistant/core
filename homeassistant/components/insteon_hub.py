5dc06fd6d1d35c9c59b5c0dc0070ed5675762e82
Teagan M. Glenn <that@teagantotally.rocks>
2016-04-10 16:59:03 -0600

    Missed self

--- homeassistant/components/insteon_hub.py
+++ homeassistant/components/insteon_hub.py
@@ -188,5 +188,5 @@ class InsteonFanDevice(LevelEntity, InsteonDevice):
     def set_level(self, level, **kwargs):
         """Set's the fan speed."""
         resp = self.node.send_command('fan', {'speed': level}, wait=True)
-        if is_successful(resp):
+        if self.is_successful(resp):
             self._value = level
