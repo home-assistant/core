983c89b620b42cde4bf23726919d5d8e265bc8b7
Markus Peter <mpeter@mpeter.name>
2016-04-16 21:49:08 +0200

    tox runs successfully

--- homeassistant/components/thermostat/eq3btsmart.py
+++ homeassistant/components/thermostat/eq3btsmart.py
@@ -5,13 +5,12 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/thermostat.max/
 """
 import logging
+import struct
+from datetime import datetime
 
 from homeassistant.components.thermostat import ThermostatDevice
 from homeassistant.const import TEMP_CELCIUS
 from homeassistant.helpers.temperature import convert
-
-import struct
-from datetime import datetime
 
 REQUIREMENTS = ['bluepy>=1.0.0']
 
@@ -19,13 +18,13 @@ CONF_MAC = 'mac'
 CONF_DEVICES = 'devices'
 CONF_ID = 'id'
 
-PROPERTY_WRITE_HANDLE = 0x411
-PROPERTY_NTFY_HANDLE = 0x421
-PROPERTY_ID_VALUE_PACKED = struct.pack('B', 0)
-PROPERTY_GETINFO_VALUE_PACKED = struct.pack('B', 3)
-PROPERTY_TEMPERATURE_VALUE = 0x41
-PROPERTY_TEMPERATURE_VALUE_PACKED = struct.pack(
-    'B', PROPERTY_TEMPERATURE_VALUE)
+PROP_WRITE_HANDLE = 0x411
+PROP_NTFY_HANDLE = 0x421
+PROP_ID_VALUE_PACKED = struct.pack('B', 0)
+PROP_GETINFO_VALUE_PACKED = struct.pack('B', 3)
+PROP_TEMPERATURE_VALUE = 0x41
+PROP_TEMPERATURE_VALUE_PACKED = struct.pack(
+    'B', PROP_TEMPERATURE_VALUE)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -36,7 +35,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     for name, device_cfg in config[CONF_DEVICES].items():
         mac = device_cfg[CONF_MAC]
-        devices.append(eq3BTSmartThermostat(mac, name))
+        devices.append(EQ3BTSmartThermostat(mac, name))
 
     add_devices(devices)
     return True
@@ -55,53 +54,55 @@ class EQ3BTSmartThermostat(ThermostatDevice):
         self._target_temperature = -1
         self._mode = -1
         self._mode_readable = ''
-        self._requestValue = -1
-        self._requestHandle = -1
-        
-        self._delegate = self.getDelegate()
+        self._request_value = -1
+        self._request_handle = -1
 
         self._conn = btle.Peripheral()
         self._connect()
         self.update()
 
-    def getDelegate():
+    def get_delegate(self):
         """Return the notification handler."""
         from bluepy import btle
-                
+
         class EQ3BTSmartDelegate(btle.DefaultDelegate):
             """Class for Callback handling."""
 
             def __init__(self, _thermostat):
                 """Initialize the Callback handler."""
-
                 btle.DefaultDelegate.__init__(self)
-                self._thermostat = thermostat
+                self._thermostat = _thermostat
 
-            def handleNotification(self, cHandle, data):
+            def handleNotification(self, handle, data):
                 """Handle Callback from a Bluetooth (GATT) request."""
-                if cHandle == PROPERTY_NTFY_HANDLE:
-                    if self._thermostat._requestValue == PROPERTY_GETINFO_VALUE_PACKED:
-                        self._thermostat._mode = data[2] & 1
-                        self._thermostat._mode_readable = self.decodeMode(data[2])
-                        self._thermostat._target_temperature = data[5] / 2.0
-        
+                self._thermostat.handle_notification(handle, data)
+
         return EQ3BTSmartDelegate(self)
+
+    def handle_notification(self, handle, data):
+        """Handle Callback from a Bluetooth (GATT) request."""
+        if handle == PROP_NTFY_HANDLE:
+            if self._request_value == PROP_GETINFO_VALUE_PACKED:
+                self._mode = data[2] & 1
+                self._mode_readable = self.decode_mode(data[2])
+                self._target_temperature = data[5] / 2.0
 
     def _connect(self):
         """Connect to the Bluetooth thermostat."""
         _LOGGER.info("EQ3 Smart BLE: connecting to " + self._name +
                      " " + self._mac)
         self._conn.connect(self._mac)
-        self._conn.withDelegate(self._delegate)
-        self._setTime()
+        delegate = self.get_delegate()
+        self._conn.withDelegate(delegate)
+        self._set_time()
 
-    def _setTime(self):
+    def _set_time(self):
         """Set the correct time into the thermostat."""
         time = datetime.now()
         value = struct.pack('BBBBBB', int(time.strftime("%y")),
                             time.month, time.day, time.hour,
                             time.minute, time.second)
-        self.writeCommandRaw(PROPERTY_WRITE_HANDLE, value)
+        self.write_command_raw(PROP_WRITE_HANDLE, value)
 
     def _disconnect(self):
         """Close the Bluetooth connection."""
@@ -119,9 +120,7 @@ class EQ3BTSmartThermostat(ThermostatDevice):
 
     @property
     def current_temperature(self):
-        """This thermostat can not report the current temperature.
-           So return the target temperature.
-        """
+        """Can not report temperature, so return target_temperature."""
         return self.target_temperature
 
     @property
@@ -131,9 +130,9 @@ class EQ3BTSmartThermostat(ThermostatDevice):
 
     def set_temperature(self, temperature):
         """Set new target temperature."""
-        value = struct.pack('BB', PROPERTY_TEMPERATURE_VALUE,
+        value = struct.pack('BB', PROP_TEMPERATURE_VALUE,
                             int(temperature*2))
-        self.writeRequestRaw(PROPERTY_WRITE_HANDLE, value)
+        self.write_request_raw(PROP_WRITE_HANDLE, value)
         self._target_temperature = temperature
 
     @property
@@ -151,7 +150,8 @@ class EQ3BTSmartThermostat(ThermostatDevice):
         """Return the maximum temperature."""
         return round(convert(30.5, TEMP_CELCIUS, self.unit_of_measurement))
 
-    def decodeMode(self, mode):
+    @staticmethod
+    def decode_mode(mode):
         """Convert the numerical mode to a human-readable description."""
         ret = ""
         if mode & 1:
@@ -170,38 +170,37 @@ class EQ3BTSmartThermostat(ThermostatDevice):
 
         return ret
 
-    def writeRequest(self, handle, value):
+    def write_request(self, handle, value):
         """Write a GATT Command with callback."""
-        self.writeCommand(handle, value, True)
+        self.write_command(handle, value, True)
 
-    def writeRequestRaw(self, handle, value):
-        """Write a GATT Command with callback
-           without utf-8 converting the input.
-        """
-        self.writeCommandRaw(handle, value, True)
+    def write_request_raw(self, handle, value):
+        """Write a GATT Command with callback - no utf-8."""
+        self.write_command_raw(handle, value, True)
 
-    def writeCommand(self, handle, value, waitForIt=False):
+    def write_command(self, handle, value, wait_for_it=False):
         """Write a GATT Command without callback."""
-        self.writeCommand(handle, value.encode('utf-8'), waitForIt)
+        self.write_command(handle, value.encode('utf-8'), wait_for_it)
 
-    def writeCommandRaw(self, handle, value, waitForIt=False, exception=False):
-        """Write a GATT Command without callback
-           without utf-8 converting the input.
-        """
+    def write_command_raw(self, handle, value,
+                          wait_for_it=False, exception=False):
+        """Write a GATT Command without callback - not utf-8."""
+        from bluepy import btle
+
         try:
-            self._requestHandle = handle
-            self._requestValue = value
-            self._conn.writeCharacteristic(handle, value, waitForIt)
-            if waitForIt:
-                while self._conn.waitForNotifications(0.1):
+            self._request_handle = handle
+            self._request_value = value
+            self._conn.writeCharacteristic(handle, value, wait_for_it)
+            if wait_for_it:
+                while self._conn.waitForNotifications(1):
                     continue
         except btle.BTLEException:
             if exception is False:
                 self._disconnect()
                 self._connect()
-                self.writeCommandRaw(handle, value, waitForIt, True)
+                self.write_command_raw(handle, value, wait_for_it, True)
 
-    def update(self, exception=False):
+    def update(self):
         """Update the data from the thermostat."""
-        self.writeRequestRaw(PROPERTY_WRITE_HANDLE,
-                             PROPERTY_GETINFO_VALUE_PACKED)
+        self.write_request_raw(PROP_WRITE_HANDLE,
+                               PROP_GETINFO_VALUE_PACKED)
