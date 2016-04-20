cc36e99d20e4d60a64feedb2d2f89f9ae5760c38
infamy <alex@pixelfactor.ca>
2016-04-19 18:16:42 -0700

    Throw a warning if deprecated method is used

--- homeassistant/util/temperature.py
+++ homeassistant/util/temperature.py
@@ -1,7 +1,11 @@
 """Temperature util functions."""
 
+import logging
+
 def fahrenheit_to_celcius(fahrenheit):
 	"""**DEPRECATED** Convert a Fahrenheit temperature to Celsius."""
+	logging.getLogger(__name__).warning(
+		"fahrenheit_to_celcius is now fahrenheit_to_celsius correcting a spelling mistake")
 	return fahrenheit_to_celsius(fahrenheit)
 
 
@@ -12,6 +16,8 @@ def fahrenheit_to_celsius(fahrenheit):
 
 def celcius_to_fahrenheit(celcius):
 	"""**DEPRECATED** Convert a Celsius temperature to Fahrenheit."""
+	logging.getLogger(__name__).warning(
+		"celcius_to_fahrenheit is now celsius_to_fahrenheit correcting a spelling mistake")
 	return celsius_to_fahrenheit(celcius)
 	
 
