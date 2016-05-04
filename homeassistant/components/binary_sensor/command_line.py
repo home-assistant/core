3360f292eaa6710b70901ad9959a7ff8f2f3baa4
Fabian Affolter <fabian@affolter-engineering.ch>
2016-05-05 00:28:51 +0200

    Fix typo

--- homeassistant/components/binary_sensor/command_line.py
+++ homeassistant/components/binary_sensor/command_line.py
@@ -1,5 +1,5 @@
 """
-Support for custom shell commands to to retrieve values.
+Support for custom shell commands to retrieve values.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/binary_sensor.command/
