928ed2bce03772c2b7cd9c49a3613d2a4216fad0
Rowan Hine <greenturtwig@gmail.com>
2016-05-08 18:13:58 +0100

    Made line 52 shorter

--- homeassistant/components/media_player/gpmdp.py
+++ homeassistant/components/media_player/gpmdp.py
@@ -49,7 +49,8 @@ class GPMDP(MediaPlayerDevice):
         """Get the latest details from the player."""
         import socket
         try:
-            self._ws = self._connection(("ws://" + self._address + ":5672"), timeout=1)
+            self._ws = self._connection(("ws://" + self._address + ":5672"),
+                                        timeout=1)
             state = self._ws.recv()
             if ((json.loads(state))['payload']) is True:
                 self._ws.recv()
