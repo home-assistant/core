d780cca41534a1d938aed5a5623198ad0a8cc601
Robbie Trencheny <me@robbiet.us>
2016-05-12 11:45:19 -0700

    Add deprecation warning for travel_mode outside options and other minor fixes

--- homeassistant/components/sensor/google_travel_time.py
+++ homeassistant/components/sensor/google_travel_time.py
@@ -72,10 +72,14 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     travel_mode = config.get(CONF_TRAVEL_MODE)
     mode = options.get(CONF_MODE)
 
-    if travel_mode is not None and mode is None:
-        options[CONF_MODE] = travel_mode
+    if travel_mode is not None:
+        wstr = ("Google Travel Time: travel_mode is deprecated, please add "
+                "mode to the options dictionary instead!")
+        _LOGGER.warning(wstr)
+        if mode is None:
+            options[CONF_MODE] = travel_mode
 
-    titled_mode = options[CONF_MODE].title()
+    titled_mode = mode.title()
     formatted_name = "Google Travel Time - {}".format(titled_mode)
     name = config.get(CONF_NAME, formatted_name)
     api_key = config.get(CONF_API_KEY)
@@ -156,9 +160,9 @@ class GoogleTravelTimeSensor(Entity):
         departure_time = self._options.get('departure_time')
         arrival_time = self._options.get('arrival_time')
         if departure_time is not None and arrival_time is not None:
-            estr = ("Google Travel Time: You can not provide both arrival",
-                    "and departure times! Clearing the arrival time...")
-            _LOGGER.warn(" ".join(estr))
+            wstr = ("Google Travel Time: You can not provide both arrival "
+                    "and departure times! Deleting the arrival time...")
+            _LOGGER.warning(wstr)
             del self._options['arrival_time']
 
         self._matrix = self._client.distance_matrix(self._origin,
