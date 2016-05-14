d02fe64ea5d80d0e6c6b26c6b8a473ca00b530e1
Daniel <Daniel.H.Iversen@ntnu.no>
2016-05-14 20:18:33 +0200

    Add test for yaml enviroment

--- tests/util/test_yaml.py
+++ tests/util/test_yaml.py
@@ -1,6 +1,7 @@
 """Test Home Assistant yaml loader."""
 import io
 import unittest
+import os
 
 from homeassistant.util import yaml
 
@@ -25,6 +26,26 @@ class TestYaml(unittest.TestCase):
     def test_duplicate_key(self):
         """Test simple dict."""
         conf = "key: thing1\nkey: thing2"
+        try:
+            with io.StringIO(conf) as f:
+                yaml.yaml.safe_load(f)
+        except Exception:
+            pass
+        else:
+            assert 0
+
+    def test_enviroment_variable(self):
+        """Test config file with enviroment variable."""
+        os.environ["PASSWORD"] = "secret_password"
+        conf = "password: !env_var PASSWORD"
+        with io.StringIO(conf) as f:
+            doc = yaml.yaml.safe_load(f)
+        assert doc['password'] == "secret_password"
+        del os.environ["PASSWORD"]
+
+    def test_invalid_enviroment_variable(self):
+        """Test config file with no enviroment variable sat."""
+        conf = "password: !env_var PASSWORD"
         try:
             with io.StringIO(conf) as f:
                 yaml.yaml.safe_load(f)
