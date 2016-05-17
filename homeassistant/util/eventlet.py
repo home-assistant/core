f3c5736b9ea46c7f775ad542d61b67ab15654cbe
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-05-16 19:47:15 -0700

    Clean up EventStream

--- homeassistant/util/eventlet.py
+++
@@ -1,9 +0,0 @@
-"""Eventlet util methods."""
-
-
-def spawn(hub, func, *args, **kwargs):
-    """Spawn a function on specified hub."""
-    import eventlet
-    gthread = eventlet.greenthread.GreenThread(hub.greenlet)
-    hub.schedule_call_global(0, gthread.switch, func, args, kwargs)
-    return gthread
