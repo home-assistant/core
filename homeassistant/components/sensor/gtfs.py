0d379572c4c221874bf50fe727d110d20cdfa46f
Robbie Trencheny <me@robbiet.us>
2016-05-20 15:05:35 -0700

    Actually pygtfs 0.1.3

--- homeassistant/components/sensor/gtfs.py
+++ homeassistant/components/sensor/gtfs.py
@@ -15,7 +15,7 @@ _LOGGER = logging.getLogger(__name__)
 
 REQUIREMENTS = ["https://github.com/robbiet480/pygtfs/archive/"
                 "bc766176358ff2762710b762bd1608428e24674a.zip#"
-                "pygtfs==0.1.2"]
+                "pygtfs==0.1.3"]
 
 ICON = "mdi:train"
 
