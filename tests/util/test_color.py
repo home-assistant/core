b0c74566ed4d42714c21eefbf277817afedd925a
Nolan Gilley <nkgilley@gmail.com>
2016-05-21 13:19:27 -0400

    Change color_RGB_to_xy formula & return brightness (#2095)
    * Use RGB to XY calculations from Philips Hue developer site
    
    * uppercase X,Y,Z
    
    * rename cx,cy to x,y
    
    * return brightness in color_RGB_to_xy
    
    * remove try/catch
    
    * update existing platforms using color_RGB_to_xy
    
    * improve wemo w/ jaharkes suggestion
    
    * allow brightness override of rgb_to_xy

--- tests/util/test_color.py
+++ tests/util/test_color.py
@@ -9,16 +9,17 @@ class TestColorUtil(unittest.TestCase):
     # pylint: disable=invalid-name
     def test_color_RGB_to_xy(self):
         """Test color_RGB_to_xy."""
-        self.assertEqual((0, 0), color_util.color_RGB_to_xy(0, 0, 0))
-        self.assertEqual((0.3127159072215825, 0.3290014805066623),
+        self.assertEqual((0, 0, 0), color_util.color_RGB_to_xy(0, 0, 0))
+        self.assertEqual((0.32, 0.336, 255),
                          color_util.color_RGB_to_xy(255, 255, 255))
 
-        self.assertEqual((0.15001662234042554, 0.060006648936170214),
+        self.assertEqual((0.136, 0.04, 12),
                          color_util.color_RGB_to_xy(0, 0, 255))
 
-        self.assertEqual((0.3, 0.6), color_util.color_RGB_to_xy(0, 255, 0))
+        self.assertEqual((0.172, 0.747, 170),
+                         color_util.color_RGB_to_xy(0, 255, 0))
 
-        self.assertEqual((0.6400744994567747, 0.3299705106316933),
+        self.assertEqual((0.679, 0.321, 80),
                          color_util.color_RGB_to_xy(255, 0, 0))
 
     def test_color_xy_brightness_to_RGB(self):
