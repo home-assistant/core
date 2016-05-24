df28fafce947549929121e92359b5313b9fa7ae2
Josh Wright <jshwright@gmail.com>
2016-05-23 20:26:11 -0400

    Fix TLS with eventlet
    This fixes a simple error on my part when implementing the WSGI stuff.
    
    eventlet.wrap_ssl() returns a wrapped socket, it does not modify the
    object passed to it. We need to grab the returned value and use that.

--- homeassistant/components/http.py
+++ homeassistant/components/http.py
@@ -243,7 +243,7 @@ class HomeAssistantWSGI(object):
 
         sock = eventlet.listen((self.server_host, self.server_port))
         if self.ssl_certificate:
-            eventlet.wrap_ssl(sock, certfile=self.ssl_certificate,
+            sock = eventlet.wrap_ssl(sock, certfile=self.ssl_certificate,
                               keyfile=self.ssl_key, server_side=True)
         wsgi.server(sock, self)
 
