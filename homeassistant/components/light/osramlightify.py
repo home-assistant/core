84fb47c7823b72d905c8e33ca617f46997ee8ec7
Olimpiu Rob <olimpiu.rob@gmail.com>
2016-05-26 21:14:51 +0300

    Fixed linting issues

--- homeassistant/components/light/osramlightify.py
+++ homeassistant/components/light/osramlightify.py
@@ -1,22 +1,21 @@
 """
-homeassistant.components.light.osramlightify
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Support for Osram Lightify.
 
-Osram Lightify platform that implements lights. Largely built off the demo
-example.
-Uses: https://github.com/aneumeier/python-lightify for the Osram light interface.
+Uses: https://github.com/aneumeier/python-lightify for the Osram light
+interface.
 
 In order to use the platform just add the following to the configuration.yaml:
 
 light:
   platform: osramlightify
-  host: hostname or ip
+  host: <hostname_or_ip>
 
 Todo:
 Add support for Non RGBW lights.
 """
 
 import logging
+import socket
 from datetime import timedelta
 
 from homeassistant import util
@@ -41,15 +40,15 @@ MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(milliseconds=100)
 
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
-    """ Find and return lights. """
+    """Find and return lights."""
     import lightify
     host = config.get(CONF_HOST)
     if host:
         try:
             bridge = lightify.Lightify(host)
-        except Exception as e:
-            msg = 'Error connecting to bridge at: {} due to: {}'.format(host,
-                                                                        str(e))
+        except socket.error as err:
+            msg = 'Error connecting to bridge: {} due to: {}'.format(host,
+                                                                     str(err))
             _LOGGER.exception(msg)
             return False
         setup_bridge(bridge, add_devices_callback)
@@ -59,12 +58,12 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
 
 
 def setup_bridge(bridge, add_devices_callback):
-    """ Setup the Lightify bridge """
+    """Setup the Lightify bridge."""
     lights = {}
 
     @util.Throttle(MIN_TIME_BETWEEN_SCANS, MIN_TIME_BETWEEN_FORCED_SCANS)
     def update_lights():
-        """ Update the lights objects with latest info from bridge"""
+        """Update the lights objects with latest info from bridge."""
         bridge.update_all_light_status()
 
         new_lights = []
@@ -86,25 +85,27 @@ def setup_bridge(bridge, add_devices_callback):
 
 
 class OsramLightifyLight(Light):
-    """ Defines an Osram Lightify Light """
+    """Defines an Osram Lightify Light."""
+
     def __init__(self, light_id, light, update_lights):
+        """Initialize the light."""
         self._light = light
         self._light_id = light_id
         self.update_lights = update_lights
 
     @property
     def name(self):
-        """ Returns the name of the device if any. """
+        """Return the name of the device if any."""
         return self._light.name()
 
     @property
     def rgb_color(self):
-        """ Last RGB color value set. """
+        """Last RGB color value set."""
         return self._light.rgb()
 
     @property
     def color_temp(self):
-        """Return the color temperature"""
+        """Return the color temperature."""
         o_temp = self._light.temp()
         temperature = int(TEMP_MIN_HASS + (TEMP_MAX_HASS - TEMP_MIN_HASS) *
                           (o_temp - TEMP_MIN) / (TEMP_MAX - TEMP_MIN))
@@ -112,20 +113,19 @@ class OsramLightifyLight(Light):
 
     @property
     def brightness(self):
-        """ Brightness of this light between 0..255. """
+        """Brightness of this light between 0..255."""
         return int(self._light.lum() * 2.55)
 
     @property
     def is_on(self):
-        """ Update Status to True if device is on. """
+        """Update Status to True if device is on."""
         self.update_lights()
-        _LOGGER.debug("is_on light state for light: %s is: %s " %
-                      (self._light.name(), self._light.on()))
+        _LOGGER.debug("is_on light state for light: %s is: %s",
+                      self._light.name(), self._light.on())
         return self._light.on()
 
     def turn_on(self, **kwargs):
-        """ Turn the device on. """
-
+        """Turn the device on."""
         brightness = 100
         if self.brightness:
             brightness = int(self.brightness / 2.55)
@@ -136,15 +136,15 @@ class OsramLightifyLight(Light):
             fade = 0
 
         if ATTR_RGB_COLOR in kwargs:
-            r, g, b = kwargs[ATTR_RGB_COLOR]
-            self._light.set_rgb(r, g, b, fade)
+            red, green, blue = kwargs[ATTR_RGB_COLOR]
+            self._light.set_rgb(red, green, blue, fade)
 
         if ATTR_BRIGHTNESS in kwargs:
             brightness = int(kwargs[ATTR_BRIGHTNESS] / 2.55)
 
         if ATTR_COLOR_TEMP in kwargs:
-            ct = kwargs[ATTR_COLOR_TEMP]
-            kelvin = int(((TEMP_MAX - TEMP_MIN) * (ct - TEMP_MIN_HASS) /
+            color_t = kwargs[ATTR_COLOR_TEMP]
+            kelvin = int(((TEMP_MAX - TEMP_MIN) * (color_t - TEMP_MIN_HASS) /
                           (TEMP_MAX_HASS - TEMP_MIN_HASS)) + TEMP_MIN)
             self._light.set_temperature(kelvin, fade)
 
@@ -152,7 +152,7 @@ class OsramLightifyLight(Light):
         self.update_ha_state()
 
     def turn_off(self, **kwargs):
-        """ Turn the device off. """
+        """Turn the device off."""
         if ATTR_TRANSITION in kwargs:
             fade = kwargs[ATTR_TRANSITION] * 10
         else:
