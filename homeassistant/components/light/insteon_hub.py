951c15a12f2c820077ac8a3342cfbb102b472129
Warren Konkel <wkonkel@gmail.com>
2016-05-26 09:32:25 -0700

    Insteon support for brightness

--- homeassistant/components/light/insteon_hub.py
+++ homeassistant/components/light/insteon_hub.py
@@ -4,7 +4,12 @@ Support for Insteon Hub lights.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/insteon_hub/
 """
-from homeassistant.components.insteon_hub import INSTEON, InsteonToggleDevice
+from homeassistant.components.insteon_hub import INSTEON
+
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS, ATTR_COLOR_TEMP, ATTR_EFFECT, ATTR_FLASH, ATTR_RGB_COLOR,
+    ATTR_TRANSITION, ATTR_XY_COLOR, EFFECT_COLORLOOP, EFFECT_RANDOM,
+    FLASH_LONG, FLASH_SHORT, Light)
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -16,3 +21,51 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         if device.DeviceCategory == "Dimmable Lighting Control":
             devs.append(InsteonToggleDevice(device))
     add_devices(devs)
+
+
+class InsteonToggleDevice(Light):
+    """An abstract Class for an Insteon node."""
+
+    def __init__(self, node):
+        """Initialize the device."""
+        self.node = node
+        self._value = 0
+
+    @property
+    def name(self):
+        """Return the the name of the node."""
+        return self.node.DeviceName
+
+    @property
+    def unique_id(self):
+        """Return the ID of this insteon node."""
+        return self.node.DeviceID
+
+    @property
+    def brightness(self):
+        """Return the brightness of this light between 0..255."""
+        return self._value / 100 * 256
+
+    def update(self):
+        """Update state of the sensor."""
+        resp = self.node.send_command('get_status', wait=True)
+        try:
+            self._value = resp['response']['level']
+        except KeyError:
+            pass
+
+    @property
+    def is_on(self):
+        """Return the boolean response if the node is on."""
+        return self._value != 0
+
+    def turn_on(self, **kwargs):
+        """Turn device on."""
+        if ATTR_BRIGHTNESS in kwargs:
+            self.node.send_command('on', kwargs[ATTR_BRIGHTNESS] / 256 * 100)
+        else:
+            self.node.send_command('on')
+
+    def turn_off(self, **kwargs):
+        """Turn device off."""
+        self.node.send_command('off')
