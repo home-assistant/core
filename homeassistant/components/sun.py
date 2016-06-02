f812f934910ccd44e8747de5b4b11a6e5ff33424
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-06-01 23:02:46 -0700

    Fix Norway (#2197)

--- homeassistant/components/sun.py
+++ homeassistant/components/sun.py
@@ -165,20 +165,28 @@ class Sun(Entity):
 
     def update_as_of(self, utc_point_in_time):
         """Calculate sun state at a point in UTC time."""
+        import astral
+
         mod = -1
         while True:
-            next_rising_dt = self.location.sunrise(
-                utc_point_in_time + timedelta(days=mod), local=False)
-            if next_rising_dt > utc_point_in_time:
-                break
+            try:
+                next_rising_dt = self.location.sunrise(
+                    utc_point_in_time + timedelta(days=mod), local=False)
+                if next_rising_dt > utc_point_in_time:
+                    break
+            except astral.AstralError:
+                pass
             mod += 1
 
         mod = -1
         while True:
-            next_setting_dt = (self.location.sunset(
-                utc_point_in_time + timedelta(days=mod), local=False))
-            if next_setting_dt > utc_point_in_time:
-                break
+            try:
+                next_setting_dt = (self.location.sunset(
+                    utc_point_in_time + timedelta(days=mod), local=False))
+                if next_setting_dt > utc_point_in_time:
+                    break
+            except astral.AstralError:
+                pass
             mod += 1
 
         self.next_rising = next_rising_dt
