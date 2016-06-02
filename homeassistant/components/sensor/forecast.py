a868be46870422d1d6066aa4d7880300c9fbb226
Edward Romano <oudeis.metis@gmail.com>
2016-06-02 16:47:54 -0700

    Refactor Forecast.io

--- homeassistant/components/sensor/forecast.py
+++ homeassistant/components/sensor/forecast.py
@@ -6,6 +6,8 @@ https://home-assistant.io/components/sensor.forecast/
 """
 import logging
 from datetime import timedelta
+from requests.exceptions import ConnectionError as ConnectError, \
+    HTTPError, Timeout
 
 from homeassistant.const import CONF_API_KEY, TEMP_CELSIUS
 from homeassistant.helpers.entity import Entity
@@ -48,21 +50,9 @@ MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=120)
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the Forecast.io sensor."""
-    import forecastio
-
-    if None in (hass.config.latitude, hass.config.longitude):
-        _LOGGER.error("Latitude or longitude not set in Home Assistant config")
-        return False
-
-    try:
-        forecast = forecastio.load_forecast(config.get(CONF_API_KEY, None),
-                                            hass.config.latitude,
-                                            hass.config.longitude)
-        forecast.currently()
-    except ValueError:
-        _LOGGER.error(
-            "Connection error "
-            "Please check your settings for Forecast.io.")
+    api_key = config.get(CONF_API_KEY, None)
+    if None in (hass.config.latitude, hass.config.longitude, api_key):
+        _LOGGER.error("Latitude, longitude, or API key missing from config")
         return False
 
     if 'units' in config:
@@ -72,19 +62,22 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     else:
         units = 'us'
 
-    data = ForeCastData(config.get(CONF_API_KEY, None),
-                        hass.config.latitude,
-                        hass.config.longitude,
-                        units)
+    try:
+        data = ForeCastData(api_key, hass.config.latitude,
+                            hass.config.longitude, units)
+        data.update_currently()
+    except ValueError as error:
+        _LOGGER.error(error)
+        return False
 
-    dev = []
+    sensors = []
     for variable in config['monitored_conditions']:
-        if variable not in SENSOR_TYPES:
-            _LOGGER.error('Sensor type: "%s" does not exist', variable)
+        if variable in SENSOR_TYPES:
+            sensors.append(ForeCastSensor(data, variable))
         else:
-            dev.append(ForeCastSensor(data, variable))
+            _LOGGER.error('Sensor type: "%s" does not exist', variable)
 
-    add_devices(dev)
+    add_devices(sensors)
 
 
 # pylint: disable=too-few-public-methods
@@ -95,20 +88,10 @@ class ForeCastSensor(Entity):
         """Initialize the sensor."""
         self.client_name = 'Weather'
         self._name = SENSOR_TYPES[sensor_type][0]
-        self.forecast_client = weather_data
+        self.sdk = weather_data
         self.type = sensor_type
         self._state = None
-        self._unit_system = self.forecast_client.unit_system
-        if self._unit_system == 'si':
-            self._unit_of_measurement = SENSOR_TYPES[self.type][1]
-        elif self._unit_system == 'us':
-            self._unit_of_measurement = SENSOR_TYPES[self.type][2]
-        elif self._unit_system == 'ca':
-            self._unit_of_measurement = SENSOR_TYPES[self.type][3]
-        elif self._unit_system == 'uk':
-            self._unit_of_measurement = SENSOR_TYPES[self.type][4]
-        elif self._unit_system == 'uk2':
-            self._unit_of_measurement = SENSOR_TYPES[self.type][5]
+
         self.update()
 
     @property
@@ -124,41 +107,42 @@ class ForeCastSensor(Entity):
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement of this entity, if any."""
-        return self._unit_of_measurement
+        self.sdk.update_unit_of_measurement(self.type)
+        return self.sdk.unit_of_measurement
 
     @property
     def unit_system(self):
         """Return the unit system of this entity."""
-        return self._unit_system
+        return self.sdk.unit_system
 
     # pylint: disable=too-many-branches,too-many-statements
     def update(self):
         """Get the latest data from Forecast.io and updates the states."""
         import forecastio
 
-        self.forecast_client.update()
+        self.sdk.update()
 
         try:
             if self.type == 'minutely_summary':
-                self.forecast_client.update_minutely()
-                self._state = self.forecast_client.data_minutely.summary
+                self.sdk.update_minutely()
+                self._state = self.sdk.data_minutely.summary
                 return
 
             elif self.type == 'hourly_summary':
-                self.forecast_client.update_hourly()
-                self._state = self.forecast_client.data_hourly.summary
+                self.sdk.update_hourly()
+                self._state = self.sdk.data_hourly.summary
                 return
 
             elif self.type == 'daily_summary':
-                self.forecast_client.update_daily()
-                self._state = self.forecast_client.data_daily.summary
+                self.sdk.update_daily()
+                self._state = self.sdk.data_daily.summary
                 return
 
         except forecastio.utils.PropertyUnavailable:
             return
 
-        self.forecast_client.update_currently()
-        data = self.forecast_client.data_currently
+        self.sdk.update_currently()
+        data = self.sdk.data_currently
 
         try:
             if self.type == 'summary':
@@ -214,6 +198,7 @@ class ForeCastData(object):
 
         self.data = None
         self.unit_system = None
+        self.unit_of_measurement = None
         self.data_currently = None
         self.data_minutely = None
         self.data_hourly = None
@@ -226,10 +211,13 @@ class ForeCastData(object):
         """Get the latest data from Forecast.io."""
         import forecastio
 
-        self.data = forecastio.load_forecast(self._api_key,
-                                             self.latitude,
-                                             self.longitude,
-                                             units=self.units)
+        try:
+            self.data = forecastio.load_forecast(self._api_key,
+                                                 self.latitude,
+                                                 self.longitude,
+                                                 units=self.units)
+        except (ConnectError, HTTPError, Timeout, ValueError) as error:
+            raise ValueError("Unable to init Forecast.io. - %s", error)
         self.unit_system = self.data.json['flags']['units']
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
@@ -251,3 +239,18 @@ class ForeCastData(object):
     def update_daily(self):
         """Update daily data."""
         self.data_daily = self.data.daily()
+
+    def update_unit_of_measurement(self, sensor_type):
+        """Update units based on unit system."""
+        if self.unit_system == 'si':
+            self.unit_of_measurement = SENSOR_TYPES[sensor_type][1]
+        elif self.unit_system == 'us':
+            self.unit_of_measurement = SENSOR_TYPES[sensor_type][2]
+        elif self.unit_system == 'ca':
+            self.unit_of_measurement = SENSOR_TYPES[sensor_type][3]
+        elif self.unit_system == 'uk':
+            self.unit_of_measurement = SENSOR_TYPES[sensor_type][4]
+        elif self.unit_system == 'uk2':
+            self.unit_of_measurement = SENSOR_TYPES[sensor_type][5]
+        else:
+            self.unit_of_measurement = None
