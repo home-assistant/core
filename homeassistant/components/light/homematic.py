eab8bb724a9b10dada834d34d74666a7d669997b
root <root@debian.dibu60.local>
2016-06-11 00:48:53 +0200

    Added Homematic implementation

---
+++ homeassistant/components/light/homematic.py
@@ -0,0 +1,91 @@
+import logging
+
+"""
+The homematic custom light platform.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/light.homematic/
+"""
+
+from homeassistant.components.light import (ATTR_BRIGHTNESS, Light)
+import homeassistant.components.homematic as homematic
+
+_LOGGER = logging.getLogger(__name__)
+
+REQUIREMENTS = ['pyhomematic==0.1.2']
+
+# List of component names (string) your component depends upon.
+DEPENDENCIES = ['homematic']
+
+
+def setup_platform(hass, config, add_callback_devices, discovery_info=None):
+    return homematic.setup_hmdevice_entity_helper(HMLight, config, add_callback_devices)
+
+
+class HMLight(homematic.HMDevice, Light):
+    """Represents an Homematic Light in Home Assistant."""
+            
+    @property
+    def brightness(self):
+        """Return the brightness of this light between 0..255"""
+        if self._dimmer:
+            return int(self._level * 255)
+        else:
+            return None
+    
+    @property
+    def is_on(self):
+        """Return True if light is on."""
+        if self._is_connected:
+            if self._dimmer:
+                return self._level > 0
+            else:
+                return self._state
+        else:
+            return False
+        
+    def turn_on(self, **kwargs):
+        """Turn the light on."""
+        if self._is_connected:
+            if ATTR_BRIGHTNESS in kwargs and self._dimmer:
+                percent_bright = float(kwargs[ATTR_BRIGHTNESS]) / 255
+                self._hmdevice.level = percent_bright
+            else:
+                self._state = True
+                self._hmdevice.on()
+
+    def turn_off(self, **kwargs):
+        """Turn the light off."""
+        if self._is_connected:
+            if not self._dimmer:
+                self._state = False
+            self._hmdevice.off()
+            
+    def connect_to_homematic(self):
+        """Configuration specific to device after connection with pyhomematic is established"""
+        def event_received(device, caller, attribute, value):
+            attribute = str(attribute).upper()
+            if attribute == 'LEVEL':
+                self._level = float(value)
+            elif attribute == 'STATE':
+                self._state = bool(value)
+            elif attribute == 'UNREACH':
+                self._is_available = not bool(value)
+            else:
+                return
+            self.update_ha_state()
+
+        super().connect_to_homematic()
+
+        if hasattr(self._hmdevice, 'level'):
+            self._dimmer = True 
+        else:
+            self._dimmer = False 
+        if self._is_available:
+            _LOGGER.debug("Setting up light device %s" % self._hmdevice._ADDRESS)
+            self._hmdevice.setEventCallback(event_received)
+            if self._dimmer:
+                self._level = self._hmdevice.level
+            else:
+                self._state = self._hmdevice.is_on
+            self.update_ha_state()
---
+++ homeassistant/components/rollershutter/homematic.py
@@ -0,0 +1,89 @@
+import logging
+
+"""
+The homematic custom rollershutter platform.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/rollershutter.homematic/
+"""
+from homeassistant.const import (STATE_OPEN, STATE_CLOSED, STATE_UNKNOWN)
+from homeassistant.components.rollershutter import RollershutterDevice, ATTR_CURRENT_POSITION
+import homeassistant.components.homematic as homematic
+
+
+_LOGGER = logging.getLogger(__name__)
+
+REQUIREMENTS = ['pyhomematic==0.1.2']
+
+# List of component names (string) your component depends upon.
+DEPENDENCIES = ['homematic']
+
+
+def setup_platform(hass, config, add_callback_devices, discovery_info=None):
+    return homematic.setup_hmdevice_entity_helper(HMRollershutter, config, add_callback_devices)
+
+
+class HMRollershutter(homematic.HMDevice, RollershutterDevice):
+    """Represents an Homematic Rollershutter in Home Assistant."""
+    
+    @property
+    def current_position(self):
+        """Return current position of roller shutter.
+        None is unknown, 0 is closed, 100 is fully open.
+        """
+        if self._is_connected:
+            return int((1 - self._level) * 100)
+        else:
+            return None
+        
+    def position(self, **kwargs):
+        """Move the roller shutter to a defined position between 0 (closed) and 100 (fully open)"""
+        if self._is_connected:
+            if ATTR_CURRENT_POSITION in kwargs:
+                position = float(kwargs[ATTR_CURRENT_POSITION])
+                position = min(100, max(0, position))
+                self._hmdevice.level = (100 - position) / 100.0
+
+    @property
+    def state(self):
+        """Return the state of the roller shutter."""
+        current = self.current_position
+
+        if current is None:
+            return STATE_UNKNOWN
+
+        return STATE_CLOSED if current == 100 else STATE_OPEN
+
+    def move_up(self, **kwargs):
+        """Move the roller shutter up."""
+        if self._is_connected:
+            self._hmdevice.move_up()
+
+    def move_down(self, **kwargs):
+        """Move the roller shutter down."""
+        if self._is_connected:
+            self._hmdevice.move_down()
+
+    def stop(self, **kwargs):
+        """Stop the device."""
+        if self._is_connected:
+            self._hmdevice.stop()
+
+    def connect_to_homematic(self):
+        """Configuration specific to device after connection with pyhomematic is established"""
+        def event_received(device, caller, attribute, value):
+            attribute = str(attribute).upper()
+            if attribute == 'LEVEL':
+                self._level = float(value)
+            elif attribute == 'UNREACH':
+                self._is_available = not bool(value)
+            else:
+                return
+            self.update_ha_state()
+
+        super().connect_to_homematic()
+        if self._is_available:
+            _LOGGER.debug("Setting up rollershutter %s" % self._hmdevice._ADDRESS)
+            self._hmdevice.setEventCallback(event_received)
+            self._level = self._hmdevice.level
+            self.update_ha_state()
