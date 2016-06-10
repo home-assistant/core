c099874c744daf28736b86f61ff4bd1245c8a3e0
Daniel Perna <danielperna84@gmail.com>
2016-06-11 00:50:48 +0200

    Added rest I just forgot

--- homeassistant/components/rollershutter/__init__.py
+++ homeassistant/components/rollershutter/__init__.py
@@ -35,9 +35,11 @@ DISCOVERY_PLATFORMS = {}
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_CURRENT_POSITION = 'current_position'
+SERVICE_CURRENT_POSITION = 'current_position'
 
 ROLLERSHUTTER_SERVICE_SCHEMA = vol.Schema({
     vol.Optional(ATTR_ENTITY_ID): cv.entity_ids,
+    ATTR_CURRENT_POSITION: cv.byte,
 })
 
 
@@ -83,6 +85,10 @@ def setup(hass, config):
                 rollershutter.move_down()
             elif service.service == SERVICE_STOP:
                 rollershutter.stop()
+            elif service.service == SERVICE_POSITION:
+                if ATTR_CURRENT_POSITION in dat:
+                    params[ATTR_CURRENT_POSITION] = util.convert(dat.get(ATTR_CURRENT_POSITION), int, params.get(ATTR_CURRENT_POSITION))
+                    rollershutter.position(**params)
 
             if rollershutter.should_poll:
                 rollershutter.update_ha_state(True)
@@ -101,6 +107,10 @@ def setup(hass, config):
     hass.services.register(DOMAIN, SERVICE_STOP,
                            handle_rollershutter_service,
                            descriptions.get(SERVICE_STOP),
+                           schema=ROLLERSHUTTER_SERVICE_SCHEMA)
+    hass.services.register(DOMAIN, SERVICE_CURRENT_POSITION,
+                           handle_rollershutter_service,
+                           descriptions.get(SERVICE_CURRENT_POSITION),
                            schema=ROLLERSHUTTER_SERVICE_SCHEMA)
     return True
 
@@ -149,4 +159,8 @@ class RollershutterDevice(Entity):
 
     def stop(self, **kwargs):
         """Stop the roller shutter."""
+        raise NotImplementedError()
+
+    def position(self, **kwargs):
+        """Position."""
         raise NotImplementedError()
