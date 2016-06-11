Hansen8601/home-assistant

Migrate to generic discovery method

homeassistant/components/ecobee.py
@@ -8,15 +8,12 @@ import logging
 import os
 from datetime import timedelta
 
-from homeassistant import bootstrap
-from homeassistant.const import (
-    ATTR_DISCOVERED, ATTR_SERVICE, CONF_API_KEY, EVENT_PLATFORM_DISCOVERED)
+from homeassistant.components import discovery
+from homeassistant.const import CONF_API_KEY
 from homeassistant.loader import get_component
 from homeassistant.util import Throttle
 
 DOMAIN = "ecobee"
-DISCOVER_THERMOSTAT = "ecobee.thermostat"
-DISCOVER_SENSORS = "ecobee.sensor"
 NETWORK = None
 HOLD_TEMP = 'hold_temp'
 
@@ -70,23 +67,11 @@ def setup_ecobee(hass, network, config):
         configurator = get_component('configurator')
         configurator.request_done(_CONFIGURING.pop('ecobee'))
 
-    # Ensure component is loaded
-    bootstrap.setup_component(hass, 'thermostat', config)
-    bootstrap.setup_component(hass, 'sensor', config)
-
     hold_temp = config[DOMAIN].get(HOLD_TEMP, False)
 
-    # Fire thermostat discovery event
-    hass.bus.fire(EVENT_PLATFORM_DISCOVERED, {
-        ATTR_SERVICE: DISCOVER_THERMOSTAT,
-        ATTR_DISCOVERED: {'hold_temp': hold_temp}
-    })
-
-    # Fire sensor discovery event
-    hass.bus.fire(EVENT_PLATFORM_DISCOVERED, {
-        ATTR_SERVICE: DISCOVER_SENSORS,
-        ATTR_DISCOVERED: {}
-    })
+    discovery.load_platform(hass, 'thermostat', DOMAIN,
+                            {'hold_temp': hold_temp}, config)
+    discovery.load_platform(hass, 'sensor', DOMAIN, None, config)
 
 
 # pylint: disable=too-few-public-methods

homeassistant/components/tellstick.py
@@ -8,11 +8,8 @@ import logging
 import threading
 import voluptuous as vol
 
-from homeassistant import bootstrap
-from homeassistant.const import (
-    ATTR_DISCOVERED, ATTR_SERVICE,
-    EVENT_PLATFORM_DISCOVERED, EVENT_HOMEASSISTANT_STOP)
-from homeassistant.loader import get_component
+from homeassistant.components import discovery
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.helpers.entity import Entity
 
 DOMAIN = "tellstick"
@@ -23,11 +20,6 @@ _LOGGER = logging.getLogger(__name__)
 
 ATTR_SIGNAL_REPETITIONS = "signal_repetitions"
 DEFAULT_SIGNAL_REPETITIONS = 1
-
-DISCOVER_SWITCHES = "tellstick.switches"
-DISCOVER_LIGHTS = "tellstick.lights"
-DISCOVERY_TYPES = {"switch": DISCOVER_SWITCHES,
-                   "light": DISCOVER_LIGHTS}
 
 ATTR_DISCOVER_DEVICES = "devices"
 ATTR_DISCOVER_CONFIG = "config"
@@ -57,17 +49,11 @@ def _discover(hass, config, found_devices, component_name):
     _LOGGER.info("discovered %d new %s devices",
                  len(found_devices), component_name)
 
-    component = get_component(component_name)
-    bootstrap.setup_component(hass, component.DOMAIN,
-                              config)
-
     signal_repetitions = config[DOMAIN].get(ATTR_SIGNAL_REPETITIONS)
 
-    hass.bus.fire(EVENT_PLATFORM_DISCOVERED,
-                  {ATTR_SERVICE: DISCOVERY_TYPES[component_name],
-                   ATTR_DISCOVERED: {ATTR_DISCOVER_DEVICES: found_devices,
-                                     ATTR_DISCOVER_CONFIG:
-                                         signal_repetitions}})
+    discovery.load_platform(hass, component_name, DOMAIN, {
+        ATTR_DISCOVER_DEVICES: found_devices,
+        ATTR_DISCOVER_CONFIG: signal_repetitions}, config)
 
 
 def setup(hass, config):

homeassistant/components/vera.py
@@ -5,16 +5,13 @@ For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/vera/
 """
 import logging
-
 from collections import defaultdict
+
 from requests.exceptions import RequestException
 
-from homeassistant import bootstrap
-from homeassistant.const import (
-    ATTR_SERVICE, ATTR_DISCOVERED,
-    EVENT_HOMEASSISTANT_STOP, EVENT_PLATFORM_DISCOVERED)
+from homeassistant.components import discovery
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.helpers.entity import Entity
-from homeassistant.loader import get_component
 
 REQUIREMENTS = ['pyvera==0.2.10']
 
@@ -27,27 +24,17 @@ VERA_CONTROLLER = None
 CONF_EXCLUDE = 'exclude'
 CONF_LIGHTS = 'lights'
 
-BINARY_SENSOR = 'binary_sensor'
-SENSOR = 'sensor'
-LIGHT = 'light'
-SWITCH = 'switch'
-
 DEVICE_CATEGORIES = {
-    'Sensor': BINARY_SENSOR,
-    'Temperature Sensor': SENSOR,
-    'Light Sensor': SENSOR,
-    'Humidity Sensor': SENSOR,
-    'Dimmable Switch': LIGHT,
-    'Switch': SWITCH,
-    'Armable Sensor': SWITCH,
-    'On/Off Switch': SWITCH,
+    'Sensor': 'binary_sensor',
+    'Temperature Sensor': 'sensor',
+    'Light Sensor': 'sensor',
+    'Humidity Sensor': 'sensor',
+    'Dimmable Switch': 'light',
+    'Switch': 'switch',
+    'Armable Sensor': 'switch',
+    'On/Off Switch': 'switch',
     # 'Window Covering': NOT SUPPORTED YET
 }
-
-DISCOVER_BINARY_SENSORS = 'vera.binary_sensors'
-DISCOVER_SENSORS = 'vera.sensors'
-DISCOVER_LIGHTS = 'vera.lights'
-DISCOVER_SWITCHES = 'vera.switchs'
 
 VERA_DEVICES = defaultdict(list)
 
@@ -100,19 +87,13 @@ def setup(hass, base_config):
         dev_type = DEVICE_CATEGORIES.get(device.category)
         if dev_type is None:
             continue
-        if dev_type == SWITCH and device.device_id in lights_ids:
-            dev_type = LIGHT
+        if dev_type == 'switch' and device.device_id in lights_ids:
+            dev_type = 'light'
         VERA_DEVICES[dev_type].append(device)
 
-    for comp_name, discovery in (((BINARY_SENSOR, DISCOVER_BINARY_SENSORS),
-                                  (SENSOR, DISCOVER_SENSORS),
-                                  (LIGHT, DISCOVER_LIGHTS),
-                                  (SWITCH, DISCOVER_SWITCHES))):
-        component = get_component(comp_name)
-        bootstrap.setup_component(hass, component.DOMAIN, base_config)
-        hass.bus.fire(EVENT_PLATFORM_DISCOVERED,
-                      {ATTR_SERVICE: discovery,
-                       ATTR_DISCOVERED: {}})
+    for component in 'binary_sensor', 'sensor', 'light', 'switch':
+        discovery.load_platform(hass, component, DOMAIN, None, base_config)
+
     return True
 
 

home-assistant/core

homeassistant/components/ecobee.py
@@ -8,15 +8,12 @@ import logging
 import os
 from datetime import timedelta
 
-from homeassistant import bootstrap
-from homeassistant.const import (
-    ATTR_DISCOVERED, ATTR_SERVICE, CONF_API_KEY, EVENT_PLATFORM_DISCOVERED)
+from homeassistant.helpers import discovery
+from homeassistant.const import CONF_API_KEY
 from homeassistant.loader import get_component
 from homeassistant.util import Throttle
 
 DOMAIN = "ecobee"
-DISCOVER_THERMOSTAT = "ecobee.thermostat"
-DISCOVER_SENSORS = "ecobee.sensor"
 NETWORK = None
 HOLD_TEMP = 'hold_temp'
 
@@ -70,23 +67,11 @@ def setup_ecobee(hass, network, config):
         configurator = get_component('configurator')
         configurator.request_done(_CONFIGURING.pop('ecobee'))
 
-    # Ensure component is loaded
-    bootstrap.setup_component(hass, 'thermostat', config)
-    bootstrap.setup_component(hass, 'sensor', config)
-
     hold_temp = config[DOMAIN].get(HOLD_TEMP, False)
 
-    # Fire thermostat discovery event
-    hass.bus.fire(EVENT_PLATFORM_DISCOVERED, {
-        ATTR_SERVICE: DISCOVER_THERMOSTAT,
-        ATTR_DISCOVERED: {'hold_temp': hold_temp}
-    })
-
-    # Fire sensor discovery event
-    hass.bus.fire(EVENT_PLATFORM_DISCOVERED, {
-        ATTR_SERVICE: DISCOVER_SENSORS,
-        ATTR_DISCOVERED: {}
-    })
+    discovery.load_platform(hass, 'thermostat', DOMAIN,
+                            {'hold_temp': hold_temp}, config)
+    discovery.load_platform(hass, 'sensor', DOMAIN, None, config)
 
 
 # pylint: disable=too-few-public-methods

homeassistant/components/tellstick.py
@@ -8,11 +8,8 @@ import logging
 import threading
 import voluptuous as vol
 
-from homeassistant import bootstrap
-from homeassistant.const import (
-    ATTR_DISCOVERED, ATTR_SERVICE,
-    EVENT_PLATFORM_DISCOVERED, EVENT_HOMEASSISTANT_STOP)
-from homeassistant.loader import get_component
+from homeassistant.helpers import discovery
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.helpers.entity import Entity
 
 DOMAIN = "tellstick"
@@ -23,11 +20,6 @@ _LOGGER = logging.getLogger(__name__)
 
 ATTR_SIGNAL_REPETITIONS = "signal_repetitions"
 DEFAULT_SIGNAL_REPETITIONS = 1
-
-DISCOVER_SWITCHES = "tellstick.switches"
-DISCOVER_LIGHTS = "tellstick.lights"
-DISCOVERY_TYPES = {"switch": DISCOVER_SWITCHES,
-                   "light": DISCOVER_LIGHTS}
 
 ATTR_DISCOVER_DEVICES = "devices"
 ATTR_DISCOVER_CONFIG = "config"
@@ -57,17 +49,11 @@ def _discover(hass, config, found_devices, component_name):
     _LOGGER.info("discovered %d new %s devices",
                  len(found_devices), component_name)
 
-    component = get_component(component_name)
-    bootstrap.setup_component(hass, component.DOMAIN,
-                              config)
-
     signal_repetitions = config[DOMAIN].get(ATTR_SIGNAL_REPETITIONS)
 
-    hass.bus.fire(EVENT_PLATFORM_DISCOVERED,
-                  {ATTR_SERVICE: DISCOVERY_TYPES[component_name],
-                   ATTR_DISCOVERED: {ATTR_DISCOVER_DEVICES: found_devices,
-                                     ATTR_DISCOVER_CONFIG:
-                                         signal_repetitions}})
+    discovery.load_platform(hass, component_name, DOMAIN, {
+        ATTR_DISCOVER_DEVICES: found_devices,
+        ATTR_DISCOVER_CONFIG: signal_repetitions}, config)
 
 
 def setup(hass, config):

homeassistant/components/vera.py
@@ -5,16 +5,13 @@ For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/vera/
 """
 import logging
-
 from collections import defaultdict
+
 from requests.exceptions import RequestException
 
-from homeassistant import bootstrap
-from homeassistant.const import (
-    ATTR_SERVICE, ATTR_DISCOVERED,
-    EVENT_HOMEASSISTANT_STOP, EVENT_PLATFORM_DISCOVERED)
+from homeassistant.helpers import discovery
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.helpers.entity import Entity
-from homeassistant.loader import get_component
 
 REQUIREMENTS = ['pyvera==0.2.10']
 
@@ -27,27 +24,17 @@ VERA_CONTROLLER = None
 CONF_EXCLUDE = 'exclude'
 CONF_LIGHTS = 'lights'
 
-BINARY_SENSOR = 'binary_sensor'
-SENSOR = 'sensor'
-LIGHT = 'light'
-SWITCH = 'switch'
-
 DEVICE_CATEGORIES = {
-    'Sensor': BINARY_SENSOR,
-    'Temperature Sensor': SENSOR,
-    'Light Sensor': SENSOR,
-    'Humidity Sensor': SENSOR,
-    'Dimmable Switch': LIGHT,
-    'Switch': SWITCH,
-    'Armable Sensor': SWITCH,
-    'On/Off Switch': SWITCH,
+    'Sensor': 'binary_sensor',
+    'Temperature Sensor': 'sensor',
+    'Light Sensor': 'sensor',
+    'Humidity Sensor': 'sensor',
+    'Dimmable Switch': 'light',
+    'Switch': 'switch',
+    'Armable Sensor': 'switch',
+    'On/Off Switch': 'switch',
     # 'Window Covering': NOT SUPPORTED YET
 }
-
-DISCOVER_BINARY_SENSORS = 'vera.binary_sensors'
-DISCOVER_SENSORS = 'vera.sensors'
-DISCOVER_LIGHTS = 'vera.lights'
-DISCOVER_SWITCHES = 'vera.switchs'
 
 VERA_DEVICES = defaultdict(list)
 
@@ -100,19 +87,13 @@ def setup(hass, base_config):
         dev_type = DEVICE_CATEGORIES.get(device.category)
         if dev_type is None:
             continue
-        if dev_type == SWITCH and device.device_id in lights_ids:
-            dev_type = LIGHT
+        if dev_type == 'switch' and device.device_id in lights_ids:
+            dev_type = 'light'
         VERA_DEVICES[dev_type].append(device)
 
-    for comp_name, discovery in (((BINARY_SENSOR, DISCOVER_BINARY_SENSORS),
-                                  (SENSOR, DISCOVER_SENSORS),
-                                  (LIGHT, DISCOVER_LIGHTS),
-                                  (SWITCH, DISCOVER_SWITCHES))):
-        component = get_component(comp_name)
-        bootstrap.setup_component(hass, component.DOMAIN, base_config)
-        hass.bus.fire(EVENT_PLATFORM_DISCOVERED,
-                      {ATTR_SERVICE: discovery,
-                       ATTR_DISCOVERED: {}})
+    for component in 'binary_sensor', 'sensor', 'light', 'switch':
+        discovery.load_platform(hass, component, DOMAIN, None, base_config)
+
     return True
 
 