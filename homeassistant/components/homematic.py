1d0fc5e16cce925e312edee506742a4247c8d9b7
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-06-20 15:55:21 +0200

    restruct HA-Homematic part 1

--- homeassistant/components/homematic.py
+++ homeassistant/components/homematic.py
@@ -20,23 +20,17 @@ from homeassistant.const import EVENT_HOMEASSISTANT_STOP,\
                                 ATTR_SERVICE,\
                                 ATTR_DISCOVERED
 from homeassistant.loader import get_component
+from homeassistant.helpers.entity import Entity
 import homeassistant.bootstrap
 
 DOMAIN = 'homematic'
-REQUIREMENTS = ['pyhomematic==0.1.3']
+REQUIREMENTS = ['pyhomematic==0.1.4']
 
 
 HOMEMATIC_DEVICES = {}
 
 HOMEMATIC = None
-HA_HOMEMATIC_DEVICES = None
-DEVICES_NOT_REGISTERED = []
-
-LOCAL_IP = "local_ip"
-LOCAL_PORT = "local_port"
-REMOTE_IP = "remote_ip"
-REMOTE_PORT = "remote_port"
-AUTODETECT = "autodetect"
+HOMEMATIC_AUTODETECT = False
 
 DISCOVER_SWITCHES = "homematic.switch"
 DISCOVER_LIGHTS = "homematic.light"
@@ -57,35 +51,52 @@ HM_DEVICE_TYPES = {
     DISCOVER_ROLLERSHUTTER: ['HMRollerShutter']
 }
 
+HM_ATTRIBUTE_SUPPORT = {
+    "LOWBAT": "Battery",
+    "ERROR": "Sabotage",
+    "RSSI_DEVICE": "RSSI"
+}
+
 _LOGGER = logging.getLogger(__name__)
 
 
 # pylint: disable=unused-argument
 def setup(hass, config):
     """Setup the Homematic component."""
-    global HOMEMATIC
+    global HOMEMATIC, HOMEMATIC_AUTODETECT
 
     from pyhomematic import HMConnection
 
-    local_ip = config[DOMAIN].get(LOCAL_IP)
-    local_port = config[DOMAIN].get(LOCAL_PORT)
-    remote_ip = config[DOMAIN].get(REMOTE_IP)
-    remote_port = config[DOMAIN].get(REMOTE_PORT)
-    autodetect = str(config[DOMAIN].get(AUTODETECT, False)).upper() == 'TRUE'
+    local_ip = config[DOMAIN].get("local_ip", None)
+    local_port = config[DOMAIN].get("local_port", 8943)
+    remote_ip = config[DOMAIN].get("remote_ip", None)
+    remote_port = config[DOMAIN].get("remote_port", 2001)
+    autodetect = config[DOMAIN].get("autodetect", False)
 
-    if local_ip is None \
-            or local_port is None \
-            or remote_ip is None \
-            or remote_port is None:
-        _LOGGER.error("Missing required configuration item %s, %s, %s or %s",
-                      LOCAL_IP, LOCAL_PORT, REMOTE_IP, REMOTE_PORT)
-        return
+    if remote_ip is None or local_ip is None:
+        _LOGGER.error("Missing remote CCU/Homegear or local address")
+        return False
 
-    # Only required because there is no access on created entities.
-    # Any better way how to make the devices variable accessible in
-    # all homematic components?
+    # Create server thread
+    HOMEMATIC_AUTODETECT = autodetect
+    HOMEMATIC = HMConnection(local=local_ip,
+                             localport=local_port,
+                             remote=remote_ip,
+                             remoteport=remote_port,
+                             systemcallback=system_callback_handler,
+                             interface_id='homeassistant')
 
-    def system_callback_handler(src, *args):
+    # Start server thread, connect to homegear, initialize to receive events
+    HOMEMATIC.start()
+
+    # Stops server when Homeassistant is shuting down
+    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, HOMEMATIC.stop)
+    hass.config.components.append(DOMAIN)
+
+    return True
+
+
+def system_callback_handler(src, *args):
         """Callback handler."""
         if src == 'newDevices':
             # pylint: disable=unused-variable
@@ -100,8 +111,8 @@ def setup(hass, config):
             for dev in key_dict:
                 try:
                     if dev in HOMEMATIC_DEVICES:
-                        for channel in HOMEMATIC_DEVICES[dev]:
-                            channel.connect_to_homematic()
+                        for hm_element in HOMEMATIC_DEVICES[dev]:
+                            hm_element.link_homematic()
                     else:
                         devices_not_created.append(dev)
                 # pylint: disable=broad-except
@@ -111,7 +122,7 @@ def setup(hass, config):
                         (str(dev), str(err))))
             # If configuration allows auto detection of devices,
             # all devices not configured are added.
-            if autodetect and devices_not_created:
+            if HOMEMATIC_AUTODETECT and devices_not_created:
                 for component_name, func_get_devices, discovery_type in (
                         ('switch', get_switches, DISCOVER_SWITCHES),
                         ('light', get_lights, DISCOVER_LIGHTS),
@@ -149,23 +160,8 @@ def setup(hass, config):
                             )
                 for dev in devices_not_created:
                     if dev in HOMEMATIC_DEVICES:
-                        HOMEMATIC_DEVICES[dev].connect_to_homematic()
-
-    # Create server thread
-    HOMEMATIC = HMConnection(local=local_ip,
-                             localport=local_port,
-                             remote=remote_ip,
-                             remoteport=remote_port,
-                             systemcallback=system_callback_handler,
-                             interface_id='homeassistant')
-    # Start server thread, connect to homegear, initialize to receive events
-    HOMEMATIC.start()
-
-    # Stops server when Homeassistant is shuting down
-    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, HOMEMATIC.stop)
-    hass.config.components.append(DOMAIN)
-
-    return True
+                        for hm_element in HOMEMATIC_DEVICES[dev]:
+                            hm_element.link_homematic()
 
 
 def get_switches(keys=None):
@@ -218,40 +214,41 @@ def setup_hmdevice_entity_helper(hmdevicetype, config, add_callback_devices):
     if HOMEMATIC is None:
         _LOGGER.error('Error setting up hmevice: Server not configured.')
         return False
+
     address = config.get('address', None)
     if address is None:
         _LOGGER.error("Error setting up Device '%s': " +
                       "'address' missing in configuration.", address)
         return False
+
+    # create a new HA homematic object
     new_device = hmdevicetype(config)
     if address not in HOMEMATIC_DEVICES:
         HOMEMATIC_DEVICES[address] = []
     HOMEMATIC_DEVICES[address].append(new_device)
+
+    # add to HA
     add_callback_devices([new_device])
     return True
 
 
-class HMDevice:
+class HMDevice(Entity):
     """Homematic device base object."""
 
     def __init__(self, config):
         """Initialize generic HM device."""
-        self._config = config
-        self._address = config.get('address', None)
-        self._name = config.get('name', None)
+        self._name = config.get("name", None)
+        self._address = config.get("address", None)
+        self._channel = config.get("button", 1)
+        self._state = config.get("param", None)
+        self._hidden = config.get("hidden", False)
+        self._data = {}
+        self._hmdevice = None
+        self._connected = False
+        self._available = False
+
         if not self._name:
             self._name = self._address
-        self._state = None
-        self._hmdevice = None
-        self._is_connected = False
-        self._is_available = False
-
-    def connect_to_homematic(self):
-        """Connect to Homematic."""
-        if self._address in HOMEMATIC.devices:
-            self._hmdevice = HOMEMATIC.devices[self._address]
-            self._is_connected = True
-            self._is_available = not self._hmdevice.UNREACH
 
     @property
     def should_poll(self):
@@ -266,9 +263,89 @@ class HMDevice:
     @property
     def assumed_state(self):
         """Return True if unable to access real state of the light."""
-        return not self.available
+        return not self._available
 
     @property
     def available(self):
         """Return True if light is available."""
-        return self._is_available
+        return self._available
+
+    @property
+    def hidden(self):
+        """Return True if the entity should be hidden from UIs."""
+        return self._hidden
+
+    def link_homematic(self):
+        """Connect to homematic."""
+        # exists a HM device from pyhomematic?
+        if self._address in HOMEMATIC.devices:
+            # init
+            self._hmdevice = HOMEMATIC.devices[self._address]
+            self._connected = True
+            self._available = not self._hmdevice.UNREACH
+
+            # check is HM class okay for HA class
+            if self._check_hm_to_ha_object() and \
+               self._element <= self._hmdevice.ELEMENT:
+                # init datapoints of this object
+                self._init_data()
+                self._subscribe_homematic_events()
+
+            # update HA
+            self.update_ha_state()
+
+    def _hm_event_callback(self, device, caller, attribute, value):
+        """ Handle all pyhomematic device events """
+        have_change = False
+
+        # is data needed for this instance?
+        if attribute in self._data:
+            # data have change?
+            if self._data[attribute] != value:
+                self._data[attribute] = value
+                have_change = True
+
+        # if available have change
+        if attribute is "UNREACH":
+            self._available = bool(value)
+            have_change = True
+
+        # if it change data, update HA
+        if have_change:
+            self.update_ha_state()
+
+    def _subscribe_homematic_events(self):
+        """ Subscribe all requered events to handle his job """
+        channels_to_sub = {}
+
+        # fill data to channels_to_sub from hmdevice metadata
+        for metadata in (self._hmdevice.SENSORNODE, self._hmdevice.BINARYNODE,
+                         self._hmdevice.ATTRIBUTENODE,
+                         self._hmdevice.WRITENODE):
+            for node, channel in metadata.items():
+                # data are needed for this instance
+                if node in self._data:
+                    # chan is current channel
+                    if channel == 'n' or channel is None:
+                        channel = self._channel
+                    # prepare for subscription
+                    channels_to_sub.update({channel: True})
+
+        # set callbacks
+        for channel in channels_to_sub:
+            self._hmdevice.setEventCallback(callback=self._hm_event_callback,
+                                            bequeath=false,
+                                            channel=channel)
+
+    def _check_hm_to_ha_object(self):
+        """ Check if possible to use the HM Object as this HA type """
+        if not self._connected or self._hmdevice is None:
+            _LOGGER.error("HA object is not linked to homematic.")
+            return False
+
+        # check if button option is correct set for this object
+        if self._channel <= self._hmdevice.ELEMENT:
+            _LOGGER.critical("Button option is not correct for this object!")
+            return False
+
+        return True
