c1c2445c6c978356bc95280d6ea7a5938fcab83b
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-06-21 18:51:13 +0200

    fix next range of pylinter

--- homeassistant/components/thermostat/homematic.py
+++ homeassistant/components/thermostat/homematic.py
@@ -110,14 +110,14 @@ class HMThermostat(homematic.HMDevice, ThermostatDevice):
         Check if possible to use the HM Object as this HA type
         NEED overwrite by inheret!
         """
-        from pyhomematic.devicetypes.thermostats import HMThermostat
+        import pyhomematic.devicetypes.thermostats
 
         # Check compatibility from HMDevice
         if not super()._check_hm_to_ha_object():
             return False
 
         # check if the homematic device correct for this HA device
-        if isinstance(self._hmdevice, HMThermostat):
+        if isinstance(self._hmdevice, thermostats.HMThermostat):
             return True
 
         _LOGGER.critical("This %s can't be use as thermostat", self._name)
