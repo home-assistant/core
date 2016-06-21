857925dedf5186fb55a61cf4c754c5419cf653bc
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-06-21 19:09:41 +0200

    last one...

--- homeassistant/components/sensor/homematic.py
+++ homeassistant/components/sensor/homematic.py
@@ -68,7 +68,7 @@ class HMSensor(homematic.HMDevice):
             return False
 
         # if exists user value?
-        if self._state and not (self._state in self._hmdevice.SENSORNODE):
+        if self._state not in self._hmdevice.SENSORNODE:
             _LOGGER.critical("This %s have no sensor with %s!", self._name,
                              self._state)
             return False
