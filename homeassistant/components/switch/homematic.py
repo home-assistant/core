a99fccde874e85c2aa40f55679b6d042dbaef325
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-06-21 10:23:26 +0200

    moded for new homematic support

--- homeassistant/components/switch/homematic.py
+++ homeassistant/components/switch/homematic.py
@@ -11,12 +11,14 @@ Configuration:
 
 switch:
   - platform: homematic
-    address: "<Homematic address for device>" # e.g. "JEQ0XXXXXXX"
-    name: "<User defined name>" (optional)
+    address: <Homematic address for device> # e.g. "JEQ0XXXXXXX"
+    name: <User defined name> (optional)
+    button: <Channel for toggle> (optional)
 """
 
 import logging
 from homeassistant.components.switch import SwitchDevice
+from homeassistant.const import STATE_UNKNOWN
 import homeassistant.components.homematic as homematic
 
 _LOGGER = logging.getLogger(__name__)
@@ -41,12 +43,55 @@ class HMSwitch(homematic.HMDevice, SwitchDevice):
 
     def turn_on(self, **kwargs):
         """Turn the switch on."""
-        if self._connected:
+        if self.available:
             self._hmdevice.on()
             self._set_state(True)
 
     def turn_off(self, **kwargs):
         """Turn the switch off."""
-        if self._connected:
+        if self.available:
             self._hmdevice.off()
             self._set_state(False)
+
+    def _check_hm_to_ha_object(self):
+        """
+        Check if possible to use the HM Object as this HA type
+        NEED overwrite by inheret!
+        """
+        from pyhomematic.devicetypes.actors import Dimmer, Switch
+
+        # Check compatibility from HMDevice
+        if not super()._check_hm_to_ha_object():
+            return False
+
+        # check if the homematic device correct for this HA device
+        if isinstance(self._hmdevice, Switch):
+            return True
+        if isinstance(self._hmdevice, Dimmer):
+            return True
+
+        return False
+
+    def _init_data_struct(self):
+        """
+        Generate a data struct (self._data) from hm metadata
+        NEED overwrite by inheret!
+        """
+        from pyhomematic.devicetypes.actors import Dimmer,\
+            Switch, SwitchPowermeter
+
+        # use STATE
+        if isinstance(self._hmdevice, Switch):
+            self._state = "STATE"
+
+        # use LEVEL
+        if isinstance(self._hmdevice, Dimmer):
+            self._state = "LEVEL"
+
+        # need sensor value for SwitchPowermeter
+        if isinstance(self._hmdevice, SwitchPowermeter):
+            for node in self._hmdevice.SENSORNODE:
+                self._data[node] = STATE_UNKNOWN
+
+        # add state to data struct
+        self._set_state(STATE_UNKNOWN)
