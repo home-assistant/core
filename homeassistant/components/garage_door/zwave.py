98c5a8d891e2a51b4e605b42c5108db07c1effe8
Turbokongen <turbokongen@hotmail.com>
2016-06-22 13:03:14 +0200

    Refactor of zwave discovery

--- homeassistant/components/garage_door/zwave.py
+++ homeassistant/components/garage_door/zwave.py
@@ -25,6 +25,13 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     node = zwave.NETWORK.nodes[discovery_info[zwave.ATTR_NODE_ID]]
     value = node.values[discovery_info[zwave.ATTR_VALUE_ID]]
 
+    if value.command_class != zwave.COMMAND_CLASS_SWITCH_BINARY:
+        return
+    if value.type != zwave.TYPE_BOOL:
+        return
+    if value.genre != zwave.GENRE_USER:
+        return
+
     value.set_change_verified(False)
     add_devices([ZwaveGarageDoor(value)])
 
