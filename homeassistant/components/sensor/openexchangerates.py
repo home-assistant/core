ae74f26174cd04197e310290c440ff09542e6b9e
arsaboo <arsaboo@gmx.com>
2016-06-22 22:35:57 -0400

    Update openexchangerates.py

--- homeassistant/components/sensor/openexchangerates.py
+++ homeassistant/components/sensor/openexchangerates.py
@@ -82,7 +82,8 @@ class OpenexchangeratesData(object):
         """Get the latest data from openexchangerates."""
         try:
             result = requests.get(self._resource, params={'base': self._base,
-                                                          'app_id': self._api_key},
+                                                          'app_id':
+                                                          self._api_key},
                                   timeout=10)
             if result.status_code == 200:
                 self.data = result.json()['rates']
