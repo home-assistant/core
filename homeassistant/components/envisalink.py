12e26d25a57821f629a683ed5340850100ee25dc
Dan Cinnamon <Cinntax@users.noreply.github.com>
2016-06-23 00:48:16 -0500

    Bump to pyenvisalink 1.0 (#2358)

--- homeassistant/components/envisalink.py
+++ homeassistant/components/envisalink.py
@@ -12,7 +12,7 @@ from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.discovery import load_platform
 
-REQUIREMENTS = ['pyenvisalink==0.9', 'pydispatcher==2.0.5']
+REQUIREMENTS = ['pyenvisalink==1.0', 'pydispatcher==2.0.5']
 
 _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'envisalink'
