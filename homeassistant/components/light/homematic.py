00990f1fc2d9e0708ff300062db9e182ad54c584
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-06-25 21:03:37 +0200

    Revert "fix autodiscovery"
    This reverts commit 86ccf26a1a41a73ecb5d4514d66f4aacce4bf349.

--- homeassistant/components/light/homematic.py
+++ homeassistant/components/light/homematic.py
@@ -29,8 +29,6 @@ DEPENDENCIES = ['homematic']
 
 def setup_platform(hass, config, add_callback_devices, discovery_info=None):
     """Setup the platform."""
-    if discovery_info:
-        config = discovery_info
     return homematic.setup_hmdevice_entity_helper(HMLight,
                                                   config,
                                                   add_callback_devices)
