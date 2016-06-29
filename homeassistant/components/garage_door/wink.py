8441bdeadbb006e81ed99c3e616f423d78ca00ca
William Scanlon <wjs.scanlon@gmail.com>
2016-06-29 17:16:53 -0400

    Wink subscription support (#2324)

--- homeassistant/components/garage_door/wink.py
+++ homeassistant/components/garage_door/wink.py
@@ -7,9 +7,10 @@ https://home-assistant.io/components/garage_door.wink/
 import logging
 
 from homeassistant.components.garage_door import GarageDoorDevice
-from homeassistant.const import CONF_ACCESS_TOKEN, ATTR_BATTERY_LEVEL
+from homeassistant.components.wink import WinkDevice
+from homeassistant.const import CONF_ACCESS_TOKEN
 
-REQUIREMENTS = ['python-wink==0.7.7']
+REQUIREMENTS = ['python-wink==0.7.8', 'pubnub==3.7.8']
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -31,37 +32,17 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
                 pywink.get_garage_doors())
 
 
-class WinkGarageDoorDevice(GarageDoorDevice):
+class WinkGarageDoorDevice(WinkDevice, GarageDoorDevice):
     """Representation of a Wink garage door."""
 
     def __init__(self, wink):
         """Initialize the garage door."""
-        self.wink = wink
-        self._battery = self.wink.battery_level
-
-    @property
-    def unique_id(self):
-        """Return the ID of this wink garage door."""
-        return "{}.{}".format(self.__class__, self.wink.device_id())
-
-    @property
-    def name(self):
-        """Return the name of the garage door if any."""
-        return self.wink.name()
-
-    def update(self):
-        """Update the state of the garage door."""
-        self.wink.update_state()
+        WinkDevice.__init__(self, wink)
 
     @property
     def is_closed(self):
         """Return true if door is closed."""
         return self.wink.state() == 0
-
-    @property
-    def available(self):
-        """True if connection == True."""
-        return self.wink.available
 
     def close_door(self):
         """Close the door."""
@@ -70,16 +51,3 @@ class WinkGarageDoorDevice(GarageDoorDevice):
     def open_door(self):
         """Open the door."""
         self.wink.set_state(1)
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes."""
-        if self._battery:
-            return {
-                ATTR_BATTERY_LEVEL: self._battery_level,
-            }
-
-    @property
-    def _battery_level(self):
-        """Return the battery level."""
-        return self.wink.battery_level * 100
