d89af54f693c284fa0586d8e19d6311a9d51945c
Teagan M. Glenn <that@teagantotally.rocks>
2016-07-02 11:28:53 -0600

    Logs

--- homeassistant/components/switch/isy994.py
+++ homeassistant/components/switch/isy994.py
@@ -36,7 +36,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     # Import ISY doors programs
     for folder_name, states in (('HA.doors', [STATE_ON, STATE_OFF]),
                                 ('HA.switches', [STATE_ON, STATE_OFF])):
-        logger.debug('PROGRAMS: ' + str(ISY.programs))
+        logger.debug('PROGRAMS: ' + str(ISY.programs['My Programs']))
+        logger.debug('PROGRAMS: ' + dict(ISY.programs['My Programs']))
         try:
             logger.debug('CHECKING ' + folder_name);
             folder = ISY.programs['My Programs'][folder_name]
