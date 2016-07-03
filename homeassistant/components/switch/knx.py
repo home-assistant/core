0e5aca9ec3e10c1e7985c5ce42957bb6aa8bad68
Daniel Matuschek <daniel@hifiberry.com>
2016-07-03 16:39:21 +0200

    Implementation of a KNX platform driver and a KNX switch

---
+++ homeassistant/components/switch/knx.py
@@ -0,0 +1,42 @@
+"""
+Support KNX switching actuators.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/switch.knx/
+
+(c) 2016 Daniel Matuschek <info@open-homeautomation.com>
+"""
+from homeassistant.components.switch import SwitchDevice
+from homeassistant.components.knx import (
+    KNXConfig, KNXGroupAddress)
+
+DEPENDENCIES = ["knx"]
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Create and add an entity based on the configuration."""
+    add_entities([
+        KNXSwitch(hass, KNXConfig(config))
+    ])
+
+
+class KNXSwitch(KNXGroupAddress, SwitchDevice):
+    """Representation of a KNX switch device."""
+
+    def turn_on(self, **kwargs):
+        """Turn the switch on.
+
+        This sends a value 0 to the group address of the device
+        """
+        self.group_write(1)
+        self._state = [1]
+        self.update_ha_state()
+
+    def turn_off(self, **kwargs):
+        """Turn the switch off.
+
+        This sends a value 1 to the group address of the device
+        """
+        self.group_write(0)
+        self._state = [0]
+        self.update_ha_state()
