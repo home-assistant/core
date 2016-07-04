e98d8becff108d614f6ffb1ec532896da842e0a0
Teagan M. Glenn <that@teagantotally.rocks>
2016-07-04 13:33:28 -0600

    Use response content value

--- homeassistant/components/notify/twitter.py
+++ homeassistant/components/notify/twitter.py
@@ -54,7 +54,7 @@ class TwitterNotificationService(BaseNotificationService):
           path = data['media']
           _LOGGER.debug('PATH: ' + path);
           if 'http' in path:
-            file = requests.get(path).data
+            file = requests.get(path).content
             data = Image.open(StringIO.open(file))
             _LOGGER.debug('IMAGE OPENED')
           else:
