8a0e4a059054291406183845a10179aa9ada4951
Brent <bah2830@users.noreply.github.com>
2016-07-08 01:09:02 -0500

    Added attributes to the statsd data (#2440)
    * Added attributes to the statsd data
    
    * Updated to allow optional attribute logging

--- tests/components/test_statsd.py
+++ tests/components/test_statsd.py
@@ -49,7 +49,7 @@ class TestStatsd(unittest.TestCase):
         self.assertTrue(hass.bus.listen.called)
 
     @mock.patch('statsd.StatsClient')
-    def test_event_listener(self, mock_client):
+    def test_event_listener_defaults(self, mock_client):
         """Test event listener."""
         config = {
             'statsd': {
@@ -66,10 +66,54 @@ class TestStatsd(unittest.TestCase):
                  STATE_ON: 1,
                  STATE_OFF: 0}
         for in_, out in valid.items():
-            state = mock.MagicMock(state=in_)
+            state = mock.MagicMock(state=in_,
+                                   attributes={"attribute key": 3.2})
             handler_method(mock.MagicMock(data={'new_state': state}))
-            mock_client.return_value.gauge.assert_called_once_with(
-                state.entity_id, out, statsd.DEFAULT_RATE)
+            mock_client.return_value.gauge.assert_has_calls([
+                mock.call(state.entity_id, out, statsd.DEFAULT_RATE),
+            ])
+
+            mock_client.return_value.gauge.reset_mock()
+
+            mock_client.return_value.incr.assert_called_once_with(
+                state.entity_id, rate=statsd.DEFAULT_RATE)
+            mock_client.return_value.incr.reset_mock()
+
+        for invalid in ('foo', '', object):
+            handler_method(mock.MagicMock(data={
+                'new_state': ha.State('domain.test', invalid, {})}))
+            self.assertFalse(mock_client.return_value.gauge.called)
+            self.assertFalse(mock_client.return_value.incr.called)
+
+    @mock.patch('statsd.StatsClient')
+    def test_event_listener_attr_details(self, mock_client):
+        """Test event listener."""
+        config = {
+            'statsd': {
+                'host': 'host',
+                'log_attributes': True
+            }
+        }
+        hass = mock.MagicMock()
+        statsd.setup(hass, config)
+        self.assertTrue(hass.bus.listen.called)
+        handler_method = hass.bus.listen.call_args_list[0][0][1]
+
+        valid = {'1': 1,
+                 '1.0': 1.0,
+                 STATE_ON: 1,
+                 STATE_OFF: 0}
+        for in_, out in valid.items():
+            state = mock.MagicMock(state=in_,
+                                   attributes={"attribute key": 3.2})
+            handler_method(mock.MagicMock(data={'new_state': state}))
+            mock_client.return_value.gauge.assert_has_calls([
+                mock.call("%s.state" % state.entity_id,
+                          out, statsd.DEFAULT_RATE),
+                mock.call("%s.attribute_key" % state.entity_id,
+                          3.2, statsd.DEFAULT_RATE),
+            ])
+
             mock_client.return_value.gauge.reset_mock()
 
             mock_client.return_value.incr.assert_called_once_with(
