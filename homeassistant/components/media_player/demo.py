69e8bf522e395a5fccd543eefeab9e9dcb368282
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-07-15 13:58:29 +0200

    add to demo platform

--- homeassistant/components/media_player/demo.py
+++ homeassistant/components/media_player/demo.py
@@ -8,7 +8,7 @@ from homeassistant.components.media_player import (
     MEDIA_TYPE_MUSIC, MEDIA_TYPE_TVSHOW, MEDIA_TYPE_VIDEO, SUPPORT_NEXT_TRACK,
     SUPPORT_PAUSE, SUPPORT_PLAY_MEDIA, SUPPORT_PREVIOUS_TRACK,
     SUPPORT_TURN_OFF, SUPPORT_TURN_ON, SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_SET,
-    SUPPORT_SELECT_SOURCE, MediaPlayerDevice)
+    SUPPORT_SELECT_SOURCE, SUPPORT_CLEAR_PLAYLIST, MediaPlayerDevice)
 from homeassistant.const import STATE_OFF, STATE_PAUSED, STATE_PLAYING
 
 
@@ -32,7 +32,7 @@ YOUTUBE_PLAYER_SUPPORT = \
 
 MUSIC_PLAYER_SUPPORT = \
     SUPPORT_PAUSE | SUPPORT_VOLUME_SET | SUPPORT_VOLUME_MUTE | \
-    SUPPORT_TURN_ON | SUPPORT_TURN_OFF
+    SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_CLEAR_PLAYLIST
 
 NETFLIX_PLAYER_SUPPORT = \
     SUPPORT_PAUSE | SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_SELECT_SOURCE
@@ -256,6 +256,10 @@ class DemoMusicPlayer(AbstractDemoPlayer):
         if self._cur_track < len(self.tracks) - 1:
             self._cur_track += 1
             self.update_ha_state()
+
+    def clear_playlist(self):
+        """Clear players playlist."""
+        self.tracks = []
 
 
 class DemoTVShowPlayer(AbstractDemoPlayer):
