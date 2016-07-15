961bb0819fdac2d60b82ed15ce161800c483fa4c
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-07-15 17:36:09 +0200

    fix unit test

--- tests/components/media_player/test_demo.py
+++ tests/components/media_player/test_demo.py
@@ -41,13 +41,11 @@ class TestDemoMediaPlayer(unittest.TestCase):
     def test_clear_playlist(self):
         """Test clear playlist."""
         assert mp.setup(self.hass, {'media_player': {'platform': 'demo'}})
-        state = self.hass.states.get(entity_id)
-        assert 1 < len(state.attributes.get('tracks'))
+        assert self.hass.states.is_state(entity_id, 'playing')
 
         mp.clear_playlist(self.hass, entity_id)
         self.hass.pool.block_till_done()
-        state = self.hass.states.get(entity_id)
-        assert 0 == len(state.attributes.get('tracks'))
+        assert self.hass.states.is_state(entity_id, 'off')
 
     def test_volume_services(self):
         """Test the volume service."""
