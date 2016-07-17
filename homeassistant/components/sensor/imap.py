0a4747ee4e5340dff724545b4e37af181b68e443
Daniel J. Kemp <daniel@especiallyrelative.net>
2016-07-17 10:45:08 -0400

    Fix bug with imap sensor
    Fixed bug where the new connection was not saved when a reconnect
    attempt was made; broadended the exception catching.

--- homeassistant/components/sensor/imap.py
+++ homeassistant/components/sensor/imap.py
@@ -90,12 +90,11 @@ class ImapSensor(Entity):
             self.connection.select()
             self._unread_count = len(self.connection.search(
                 None, 'UnSeen')[1][0].split())
-        except imaplib.IMAP4.abort:
+        except imaplib.IMAP4.error:
             _LOGGER.info("Connection to %s lost, attempting to reconnect",
                          self._server)
             try:
-                self._login()
-                self.update()
+                self.connection = self._login()
             except imaplib.IMAP4.error:
                 _LOGGER.error("Failed to reconnect.")
 
