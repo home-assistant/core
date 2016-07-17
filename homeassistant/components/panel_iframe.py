f476f57fb45a13c7b03c48d637d92f4c9cb6844c
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-07-16 22:32:36 -0700

    Add panel_iframe component

---
+++ homeassistant/components/panel_iframe.py
@@ -0,0 +1,29 @@
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.frontend import register_built_in_panel
+
+DOMAIN = 'panel_iframe'
+DEPENDENCIES = ['frontend']
+
+CONF_TITLE = 'title'
+CONF_ICON = 'icon'
+CONF_URL = 'url'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        cv.slug: {
+            vol.Optional(CONF_TITLE): cv.string,
+            vol.Optional(CONF_ICON): cv.icon,
+            vol.Required(CONF_URL): vol.Url(),
+        }})}, extra=vol.ALLOW_EXTRA)
+
+
+def setup(hass, config):
+    """Setup iframe frontend panels."""
+    for url_name, info in config[DOMAIN].items():
+        register_built_in_panel(
+            hass, 'iframe', info.get(CONF_TITLE), info.get(CONF_ICON),
+            url_name, {'url': info[CONF_URL]})
+
+    return True
