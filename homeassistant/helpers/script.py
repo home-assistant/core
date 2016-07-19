a8a414f097f29a73e1733a8bfd58d89441fe8077
Teagan M. Glenn <that@teagantotally.rocks>
2016-07-18 22:01:04 -0600

    Parse time delta from string

--- homeassistant/helpers/script.py
+++ homeassistant/helpers/script.py
@@ -88,7 +88,7 @@ class Script():
                         self._delay_listener = None
                         self.run(variables)
 
-                    delay = template.render(self.hass, action[CONF_DELAY], None)
+                    delay = parseTimeDelta(template.render(self.hass, action[CONF_DELAY], None))
 
                     self._delay_listener = track_point_in_utc_time(
                         self.hass, script_delay,
