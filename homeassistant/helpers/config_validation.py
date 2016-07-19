477c4e1a87dbf602e24a12eba68b675991534e6a
Teagan M. Glenn <that@teagantotally.rocks>
2016-07-18 23:32:10 -0600

    Styling

--- homeassistant/helpers/config_validation.py
+++ homeassistant/helpers/config_validation.py
@@ -376,7 +376,9 @@ CONDITION_SCHEMA = vol.Any(
 
 _SCRIPT_DELAY_SCHEMA = vol.Schema({
     vol.Optional(CONF_ALIAS): string,
-    vol.Required("delay"): vol.Any(vol.All(time_period, positive_timedelta), template)
+    vol.Required("delay"): vol.Any( \
+        vol.All(time_period, positive_timedelta), \
+        template)
 })
 
 SCRIPT_SCHEMA = vol.All(
