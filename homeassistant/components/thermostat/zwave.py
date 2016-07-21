b43bc04992eff2bcf450463dc7b29c634f4d66df
John Arild Berentsen <turbokongen@hotmail.com>
2016-07-21 12:20:43 +0200

    Thermostat and hvac status fix for #2465 (#2585)

--- homeassistant/components/thermostat/zwave.py
+++ homeassistant/components/thermostat/zwave.py
@@ -81,7 +81,8 @@ class ZWaveThermostat(zwave.ZWaveDeviceEntity, ThermostatDevice):
 
     def value_changed(self, value):
         """Called when a value has changed on the network."""
-        if self._value.value_id == value.value_id:
+        if self._value.value_id == value.value_id or \
+           self._value.node == value.node:
             self.update_properties()
             self.update_ha_state()
 
