72acdb14a44a5f7af73cbcebfaff8e7200bc8850
Dean Camera <dean@fourwalledcubicle.com>
2016-07-28 21:12:33 +1000

    Add MPC-HC remote command capabilities for basic control.

--- homeassistant/components/media_player/mpchc.py
+++ homeassistant/components/media_player/mpchc.py
@@ -9,11 +9,15 @@ import re
 import requests
 
 from homeassistant.components.media_player import (
-    MediaPlayerDevice)
+    SUPPORT_VOLUME_MUTE, SUPPORT_PAUSE, SUPPORT_STOP, SUPPORT_NEXT_TRACK,
+    SUPPORT_PREVIOUS_TRACK, SUPPORT_VOLUME_STEP, MediaPlayerDevice)
 from homeassistant.const import (
     STATE_OFF, STATE_IDLE, STATE_PAUSED, STATE_PLAYING)
 
 _LOGGER = logging.getLogger(__name__)
+
+SUPPORT_MPCHC = SUPPORT_VOLUME_MUTE | SUPPORT_PAUSE | SUPPORT_STOP | \
+    SUPPORT_PREVIOUS_TRACK | SUPPORT_NEXT_TRACK | SUPPORT_VOLUME_STEP
 
 
 # pylint: disable=unused-argument
@@ -48,13 +52,24 @@ class MpcHcDevice(MediaPlayerDevice):
             response = requests.get("{}/variables.html".format(self._url),
                                     data=None, timeout=3)
 
-            mpchc_variables = re.findall(r'<p id="(.+?)">(.+?)</p>', response.text)
+            mpchc_variables = re.findall(r'<p id="(.+?)">(.+?)</p>',
+                                         response.text)
 
             self._player_variables = dict()
             for var in mpchc_variables:
                 self._player_variables[var[0]] = var[1].lower()
         except requests.exceptions.RequestException:
             _LOGGER.error("Could not connect to MPC-HC at: %s", self._url)
+
+    def _send_command(self, command_id):
+        """Send a command to MPC-HC via its window message ID."""
+        try:
+            params = {"wm_command": command_id}
+            requests.get("{}/command.html".format(self._url),
+                         params=params, timeout=3)
+        except requests.exceptions.RequestException:
+            _LOGGER.error("Could not send command %d to MPC-HC at: %s",
+                          command_id, self._url)
 
     @property
     def name(self):
@@ -95,8 +110,44 @@ class MpcHcDevice(MediaPlayerDevice):
         """Duration of current playing media in seconds."""
         duration = self._player_variables.get('durationstring',
                                               "00:00:00").split(':')
-
         return \
             int(duration[0]) * 3600 + \
             int(duration[1]) * 60 + \
             int(duration[2])
+
+    @property
+    def supported_media_commands(self):
+        """Flag of media commands that are supported."""
+        return SUPPORT_MPCHC
+
+    def volume_up(self):
+        """Volume up the media player."""
+        self._send_command(907)
+
+    def volume_down(self):
+        """Volume down media player."""
+        self._send_command(908)
+
+    def mute_volume(self, mute):
+        """Mute the volume."""
+        self._send_command(909)
+
+    def media_play(self):
+        """Send play command."""
+        self._send_command(887)
+
+    def media_pause(self):
+        """Send pause command."""
+        self._send_command(888)
+
+    def media_stop(self):
+        """Send stop command."""
+        self._send_command(890)
+
+    def media_next_track(self):
+        """Send next track command."""
+        self._send_command(921)
+
+    def media_previous_track(self):
+        """Send previous track command."""
+        self._send_command(920)
