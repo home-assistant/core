39ca04f3bd02e69ad96c06a3d24260719fbc6cff
Teagan M. Glenn <that@teagantotally.rocks>
2016-07-30 13:01:45 -0600

    Private methods, value type has already been checked.

--- homeassistant/util/distance.py
+++ homeassistant/util/distance.py
@@ -56,59 +56,29 @@ def convert(value, unit_1, unit_2):
 
 def __miles_to_meters(miles):
     """Convert miles to meters."""
-    if not isinstance(miles, Number):
-        err = '{} is not a numeric value.'.format(str(miles))
-        _LOGGER.error(err)
-        raise TypeError(err)
-
     return miles * 1609.344
 
 
 def __feet_to_meters(feet):
     """Convert feet to meters."""
-    if not isinstance(feet, Number):
-        err = '{} is not a numeric value.'.format(str(feet))
-        _LOGGER.error(err)
-        raise TypeError(err)
-
     return feet * 0.3048
 
 
 def __kilometers_to_meters(kilometers):
     """Convert kilometers to meters."""
-    if not isinstance(kilometers, Number):
-        err = '{} is not a numeric value.'.format(str(kilometers))
-        _LOGGER.error(err)
-        raise TypeError(err)
-
     return kilometers * 1000
 
 
 def __meters_to_miles(meters):
     """Convert meters to miles."""
-    if not isinstance(meters, Number):
-        err = '{} is not a numeric value.'.format(str(meters))
-        _LOGGER.error(err)
-        raise TypeError(err)
-
     return meters * 0.000621371
 
 
 def __meters_to_feet(meters):
     """Convert meters to feet."""
-    if not isinstance(meters, Number):
-        err = '{} is not a numeric value.'.format(str(meters))
-        _LOGGER.error(err)
-        raise TypeError(err)
-
     return meters * 3.28084
 
 
 def __meters_to_kilometers(meters):
     """Convert meters to kilometers."""
-    if not isinstance(meters, Number):
-        err = '{} is not a numeric value.'.format(str(meters))
-        _LOGGER.error(err)
-        raise TypeError(err)
-
     return meters * 0.001
