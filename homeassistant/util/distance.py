bc5c7eff31255ea7126a7139750b0d6aa08b2074
Teagan M. Glenn <that@teagantotally.rocks>
2016-07-29 18:58:15 -0600

    Add type check and errors

--- homeassistant/util/distance.py
+++ homeassistant/util/distance.py
@@ -8,53 +8,82 @@ VALID_UNITS = ['km', 'm', 'ft', 'mi']
 
 def kilometers_to_miles(km):
     """Convert the given kilometers to miles."""
+    if not isinstance(km, (int, long, float)):
+        raise TypeError(str(km) ' is not of numeric type')
+
     return km * 0.621371
 
 
 def miles_to_kilometers(mi):
     """Convert the given miles to kilometers."""
+    if not isinstance(mi, (int, long, float)):
+        raise TypeError(str(mi) ' is not of numeric type')
+
     return mi * 1.60934
 
 
 def kilometers_to_meters(km):
     """Convert the given kilometers to meters."""
+    if not isinstance(km, (int, long, float)):
+        raise TypeError(str(km) ' is not of numeric type')
+
     return km * 1000
 
 
 def meters_to_kilometers(m):
     """Convert the given meters to kilometers."""
+    if not isinstance(m, (int, long, float)):
+        raise TypeError(str(m) ' is not of numeric type')
+
     return m * 0.001
 
 
 def meters_to_feet(m):
     """Convert the given meters to feet."""
+    if not isinstance(m, (int, long, float)):
+        raise TypeError(str(m) ' is not of numeric type')
+
     return m * 3.28084
 
 
 def feet_to_meters(ft):
     """Convert the given feet to meters."""
+    if not isinstance(ft, (int, long, float)):
+        raise TypeError(str(ft) ' is not of numeric type')
+
     return ft * 0.3048
 
 
 def feet_to_miles(ft):
     """Convert the given feet to miles."""
+    if not isinstance(ft, (int, long, float)):
+        raise TypeError(str(ft) ' is not of numeric type')
+
     return ft * 0.000189394
 
 
 def miles_to_ft(mi):
     """Convert the given miles to feet."""
+    if not isinstance(mi, (int, long, float)):
+        raise TypeError(str(mi) ' is not of numeric type')
+
     return mi * 5280
 
 
 def convert(value, unit_1, unit_2):
     """Convert one unit of measurement to another."""
+    if not isinstance(value, (int, long, float)):
+        raise TypeError(str(value) ' is not of numeric type')
+
     if unit_1 == unit_2:
         return value
 
     if unit_1 not in VALID_UNITS:
-        _LOGGER.error('Unknown unit of measure: ' + unit_1)
+        _LOGGER.error('Unknown unit of measure: ' + str(unit_1))
+        raise ValueError('Unknown unit of measure: ' + str(unit_1))
     elif unit_2 not in VALID_UNITS:
-        _LOGGER.error('Unknown unit of measure: ' + unit_2)
+        _LOGGER.error('Unknown unit of measure: ' + str(unit_2))
+        raise ValueError('Unknown unit of measure: ' + str(unit_2))
 
     if unit_1 == 'mi':
         if unit_2 == 'km':
