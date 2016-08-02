8a438bf27b14a1e1ff33986ba394221cd80b18fc
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-08-02 09:15:28 -0700

    Update frontend

--- homeassistant/components/frontend/version.py
+++ homeassistant/components/frontend/version.py
@@ -2,7 +2,7 @@
 
 FINGERPRINTS = {
     "core.js": "fb9a1af0cf7d39f3041d3fa67e16e81c",
-    "frontend.html": "59cfb32aa3b0ad19ed437e741e75243d",
+    "frontend.html": "fb24d923539bd6cf82a96b0a93b33aec",
     "mdi.html": "f6c6cc64c2ec38a80e91f801b41119b3",
     "panels/ha-panel-dev-event.html": "8f63a091246408228beebf276440c06e",
     "panels/ha-panel-dev-info.html": "34e2df1af32e60fffcafe7e008a92169",
@@ -12,5 +12,5 @@ FINGERPRINTS = {
     "panels/ha-panel-history.html": "efe1bcdd7733b09e55f4f965d171c295",
     "panels/ha-panel-iframe.html": "d920f0aa3c903680f2f8795e2255daab",
     "panels/ha-panel-logbook.html": "d74e56ed214dd5aa64fc16196d0345e6",
-    "panels/ha-panel-map.html": "2286ecd8fa48097a88926b3ce0acff66"
+    "panels/ha-panel-map.html": "39d4793bb67cedbbdbd1d79bdadc9c3f"
 }
