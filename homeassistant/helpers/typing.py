5a48ce6c14f74b7e91530550783aa3147f0c52fb
Johann Kellerman <kellerza@gmail.com>
2016-08-03 09:14:20 +0200

    No more NewType

--- homeassistant/helpers/typing.py
+++ homeassistant/helpers/typing.py
@@ -1,18 +1,12 @@
 """Typing Helpers for Home-Assistant."""
-# pylint: disable=invalid-name
-try:
-    from typing import NewType, Dict, Any
-except ImportError:
-    from typing import Dict, Any
-
-    def NewType(name: str, typ: Any) -> Any:
-        """Fake NewType, required for Python 3.5.1."""
-        return typ
+from typing import Dict, Any
 
 import homeassistant.core
 
-ConfigType = NewType('ConfigType', Dict[str, Any])
+# pylint: disable=invalid-name
+
+ConfigType = Dict[str, Any]
 HomeAssistantType = homeassistant.core.HomeAssistant
 
 # Custom type for recorder Queries
-QueryType = NewType('QueryType', Any)
+QueryType = Any
