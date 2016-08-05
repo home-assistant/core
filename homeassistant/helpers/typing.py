34998583c4aeae503dbb68da3d7b8b1510ec6483
Fabián Heredia Montiel <fabianherediajr@gmail.com>
2016-08-05 14:42:55 -0500

    Fix: Circular dependencies of internal files

--- homeassistant/helpers/typing.py
+++ homeassistant/helpers/typing.py
@@ -1,12 +1,20 @@
 """Typing Helpers for Home-Assistant."""
+from datetime import date, datetime
 from typing import Dict, Any
 
-import homeassistant.core
+
+# HACK: mypy/pytype will import, other interpreters will not; this is to avoid
+#       circular dependencies where the type is needed.
+# pylint: disable=using-constant-test,unused-import
+if False:
+    from homeassistant.core import HomeAssistant  # NOQA
+# ENDHACK
 
 # pylint: disable=invalid-name
-
 ConfigType = Dict[str, Any]
-HomeAssistantType = homeassistant.core.HomeAssistant
+Date = date
+DateTime = datetime
+HomeAssistantType = 'HomeAssistant'
 
 # Custom type for recorder Queries
 QueryType = Any
