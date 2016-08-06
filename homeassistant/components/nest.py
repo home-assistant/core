eaa42b2011212243b829cf18118f8c4c9c239595
Robby Grossman <robby@freerobby.com>
2016-08-05 23:00:27 -0400

    Implement support for NEST structures.

--- homeassistant/components/nest.py
+++ homeassistant/components/nest.py
@@ -9,17 +9,20 @@ import socket
 
 import voluptuous as vol
 
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, CONF_STRUCTURE
 
 REQUIREMENTS = ['python-nest==2.9.2']
 DOMAIN = 'nest'
 
 NEST = None
 
+STRUCTURES_TO_INCLUDE = None
+
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_USERNAME): str,
-        vol.Required(CONF_PASSWORD): str
+        vol.Required(CONF_PASSWORD): str,
+        vol.Optional(CONF_STRUCTURE): vol.Any(str, list),
     })
 }, extra=vol.ALLOW_EXTRA)
 
@@ -30,8 +33,11 @@ def devices():
     """Generator returning list of devices and their location."""
     try:
         for structure in NEST.structures:
-            for device in structure.devices:
-                yield (structure, device)
+            if structure.name in STRUCTURES_TO_INCLUDE:
+                for device in structure.devices:
+                    yield (structure, device)
+            else:
+                _LOGGER.info("Ignoring structure %s, not in %s", structure.name, STRUCTURES_TO_INCLUDE)
     except socket.error:
         _LOGGER.error("Connection error logging into the nest web service.")
 
@@ -40,8 +46,11 @@ def protect_devices():
     """Generator returning list of protect devices."""
     try:
         for structure in NEST.structures:
-            for device in structure.protectdevices:
-                yield(structure, device)
+            if structure.name in STRUCTURES_TO_INCLUDE:
+                for device in structure.protectdevices:
+                    yield(structure, device)
+            else:
+                _LOGGER.info("Ignoring structure %s, not in %s", structure.name, STRUCTURES_TO_INCLUDE)
     except socket.error:
         _LOGGER.error("Connection error logging into the nest web service.")
 
@@ -50,6 +59,7 @@ def protect_devices():
 def setup(hass, config):
     """Setup the Nest thermostat component."""
     global NEST
+    global STRUCTURES_TO_INCLUDE
 
     conf = config[DOMAIN]
     username = conf[CONF_USERNAME]
@@ -59,4 +69,14 @@ def setup(hass, config):
 
     NEST = nest.Nest(username, password)
 
+    if not CONF_STRUCTURE in conf:
+        STRUCTURES_TO_INCLUDE = []
+        for structure in NEST.structures:
+            STRUCTURES_TO_INCLUDE.append(structure.name)
+    elif isinstance(conf[CONF_STRUCTURE], list):
+        STRUCTURES_TO_INCLUDE = conf[CONF_STRUCTURE]
+    else:
+        STRUCTURES_TO_INCLUDE = [conf[CONF_STRUCTURE]]
+
+    _LOGGER.info("Structures to include: %s", STRUCTURES_TO_INCLUDE)
     return True
