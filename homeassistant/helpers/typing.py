039ffb83ca1fa1ad1c0cfb719996ad7bb9a89599
Fabián Heredia Montiel <fabianherediajr@gmail.com>
2016-08-06 10:54:38 -0500

    Improve NewType check

--- homeassistant/helpers/typing.py
+++ homeassistant/helpers/typing.py
@@ -1,13 +1,12 @@
 """Typing Helpers for Home-Assistant."""
-from sys import version_info
 from typing import Dict, Any
 
 # NOTE: NewType added to typing in 3.5.2 in June, 2016; Since 3.5.2 includes
 #       security fixes everyone on 3.5 should upgrade "soon"
-NEWTYPE_TEST = version_info >= (3, 5, 2) or version_info[:2] == (3, 4)
-if NEWTYPE_TEST:
+try:
     from typing import NewType
-
+except ImportError:
+    NewType = None
 
 # HACK: mypy/pytype will import, other interpreters will not; this is to avoid
 #       circular dependencies where the type is needed.
@@ -21,7 +20,7 @@ if False:
 # ENDHACK
 
 # pylint: disable=invalid-name
-if NEWTYPE_TEST:
+if NewType:
     ConfigType = NewType('ConfigType', Dict[str, Any])
     HomeAssistantType = NewType('HomeAssistantType', 'HomeAssistant')
     UnitSystemType = NewType('UnitSystemType', 'UnitSystem')
