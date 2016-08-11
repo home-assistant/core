cc4ffbdd9323104ddaddf3014fc4d88332531e20
David Straub <david.straub@tum.de>
2016-08-11 16:52:43 +0200

    Add blank lines after class docstrings

--- homeassistant/components/sensor/fritzbox_callmonitor.py
+++ homeassistant/components/sensor/fritzbox_callmonitor.py
@@ -44,6 +44,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 # pylint: disable=too-few-public-methods
 class FritzBoxCallSensor(Entity):
     """Implementation of a Fritz!Box call monitor."""
+    
     def __init__(self, name):
         """Initialize the sensor."""
         self._state = VALUE_DEFAULT
@@ -82,6 +83,7 @@ class FritzBoxCallSensor(Entity):
 # pylint: disable=too-few-public-methods
 class FritzBoxCallMonitor(object):
     """Event listener to monitor calls on the Fritz!Box."""
+
     def __init__(self, host, port, sensor):
         """Initialize Fritz!Box monitor instance."""
         self.host = host
