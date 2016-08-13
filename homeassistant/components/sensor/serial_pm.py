16b0fcad0f35b2f23d82e3e899f541f0be69541f
Open Home Automation <daniel@open-homeautomation.com>
2016-08-13 23:16:06 +0200

    Bugfix: removed conf_platform (#2811)
    * Bugfix: removed conf_platform
    
    * Remove unused import
    
    * Fix for wrong update

--- homeassistant/components/sensor/serial_pm.py
+++ homeassistant/components/sensor/serial_pm.py
@@ -7,7 +7,7 @@ https://home-assistant.io/components/sensor.serial_pm/
 import logging
 import voluptuous as vol
 
-from homeassistant.const import CONF_NAME, CONF_PLATFORM
+from homeassistant.const import CONF_NAME
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -21,7 +21,6 @@ CONF_SERIAL_DEVICE = "serial_device"
 CONF_BRAND = "brand"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_PLATFORM): 'serial_pm',
     vol.Optional(CONF_NAME, default=""): cv.string,
     vol.Required(CONF_SERIAL_DEVICE): cv.string,
     vol.Required(CONF_BRAND): cv.string,
