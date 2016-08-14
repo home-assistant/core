5b356f05d8c3b4bd04df044e37588496fa3192a4
Daniel Matuschek <daniel@hifiberry.com>
2016-08-14 08:39:56 +0200

    Renamed to bluetooth_le tracker Removed gattlib from tests Minor code cleanup

--- homeassistant/components/device_tracker/bluetooth_le_tracker.py
+++ homeassistant/components/device_tracker/bluetooth_le_tracker.py
@@ -15,12 +15,15 @@ import homeassistant.util.dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['gattlib']
+REQUIREMENTS = ['gattlib==0.20150805']
 
 BLE_PREFIX = 'BLE_'
 
+
 def setup_scanner(hass, config, see):
-    """Setup the BLE Scanner."""
+    """Setup the Bluetooth LE Scanner."""
+    # pylint: disable=import-error
+    from gattlib import DiscoveryService
 
     def see_device(address, name):
         """Mark a device as seen."""
@@ -28,9 +31,7 @@ def setup_scanner(hass, config, see):
 
     def discover_ble_devices():
         """Discover Bluetooth LE devices."""
-        from gattlib import DiscoveryService
-
-        _LOGGER.debug("Discovering BLE devices")
+        _LOGGER.debug("Discovering Bluetooth LE devices")
         service = DiscoveryService()
         devices = service.discover(10)
         _LOGGER.debug("Bluetooth LE devices discovered = %s", devices)
@@ -57,14 +58,14 @@ def setup_scanner(hass, config, see):
     track_new = util.convert(config.get(CONF_TRACK_NEW), bool,
                              len(devs_to_track) == 0)
     if not devs_to_track and not track_new:
-        _LOGGER.warning("No BLE devices to track!")
+        _LOGGER.warning("No Bluetooth LE devices to track!")
         return False
 
     interval = util.convert(config.get(CONF_SCAN_INTERVAL), int,
                             DEFAULT_SCAN_INTERVAL)
 
     def update_ble(now):
-        """Lookup BLE bluetooth device and update status."""
+        """Lookup Bluetooth LE devices and update status."""
         devs = discover_ble_devices()
         for mac in devs_to_track:
             _LOGGER.debug("Checking " + mac)
@@ -79,7 +80,7 @@ def setup_scanner(hass, config, see):
                 if address not in devs_to_track and \
                   address not in devs_donot_track:
                     devs_to_track.append(address)
-                    _LOGGER.info("Discovered BLE device %s", address)
+                    _LOGGER.info("Discovered Bluetooth LE device %s", address)
                     see_device(address, devs[address])
 
         track_point_in_utc_time(hass, update_ble,
