15665959e997308bd6e144ec11574b343e8e229d
Robbie Trencheny <me@robbiet.us>
2016-08-15 15:45:42 -0700

    Add callback support

--- homeassistant/components/notify/html5.py
+++ homeassistant/components/notify/html5.py
@@ -8,12 +8,13 @@ import os
 import logging
 import json
 import time
+import uuid
 
 import voluptuous as vol
 from voluptuous.humanize import humanize_error
 
 from homeassistant.const import (
-    HTTP_BAD_REQUEST, HTTP_INTERNAL_SERVER_ERROR)
+    HTTP_BAD_REQUEST, HTTP_INTERNAL_SERVER_ERROR, HTTP_UNAUTHORIZED)
 from homeassistant.util import ensure_unique_string
 from homeassistant.components.notify import (
     ATTR_TARGET, ATTR_TITLE, ATTR_DATA, BaseNotificationService,
@@ -31,6 +32,7 @@ DEPENDENCIES = ["frontend"]
 _LOGGER = logging.getLogger(__name__)
 
 REGISTRATIONS_FILE = "html5_push_registrations.conf"
+TAGS_FILE = "html5_push_tags.conf"
 
 ATTR_GCM_SENDER_ID = 'gcm_sender_id'
 ATTR_GCM_API_KEY = 'gcm_api_key'
@@ -48,18 +50,23 @@ REGISTER_SCHEMA = vol.Schema({
     vol.Required(ATTR_BROWSER): vol.In(['chrome', 'firefox'])
 })
 
+NOTIFY_CALLBACK_EVENT = "html5_notification"
+
 
 def get_service(hass, config):
     """Get the HTML5 push notification service."""
     json_path = hass.config.path(REGISTRATIONS_FILE)
+    tags_path = hass.config.path(TAGS_FILE)
 
     registrations = _load_config(json_path)
+    tags = _load_config(tags_path)
 
     if registrations is None:
         return None
 
     hass.wsgi.register_view(
         HTML5PushRegistrationView(hass, registrations, json_path))
+    hass.wsgi.register_view(HTML5PushCallbackView(hass, tags_path))
 
     gcm_api_key = config.get('gcm_api_key')
     gcm_sender_id = config.get('gcm_sender_id')
@@ -67,7 +74,8 @@ def get_service(hass, config):
     if gcm_sender_id is not None:
         add_manifest_json_key('gcm_sender_id', config.get('gcm_sender_id'))
 
-    return HTML5NotificationService(gcm_api_key, registrations)
+    return HTML5NotificationService(gcm_api_key, registrations, tags,
+                                    tags_path)
 
 
 def _load_config(filename):
@@ -131,25 +139,66 @@ class HTML5PushRegistrationView(HomeAssistantView):
 
         return self.json_message("Push notification subscriber registered.")
 
+class HTML5PushCallbackView(HomeAssistantView):
+    """Accepts push registrations from a browser."""
+
+    requires_auth = False
+    url = "/api/notify.html5/callback"
+    name = "api:notify.html5/callback"
+
+    def __init__(self, hass, tags_path):
+        """Init HTML5PushCallbackView."""
+        super().__init__(hass)
+        self.tags_path = tags_path
+
+    def post(self, request):
+        """Accept the POST request for push registrations event callback."""
+        if request.json.get('tag') is None:
+            return self.json_message("No tag provided!", HTTP_UNAUTHORIZED)
+        self.tags = _load_config(self.tags_path)
+        tag = request.json['tag']
+        if tag in self.tags.keys():
+            if self.tags[tag]['timestamp'] - int(time.time()) > 86400:
+                msg = "{} is an expired tag!".format(tag)
+                return self.json_message(msg, HTTP_UNAUTHORIZED)
+            else:
+                event_name = "{}.{}".format(NOTIFY_CALLBACK_EVENT,
+                                            request.json['event'])
+                event_payload = {"targets": self.tags[tag]['targets']}
+                event_payload.update(request.json)
+                self.hass.bus.fire(event_name, event_payload)
+                return self.json({"status": "ok",
+                                  "event": request.json['event']})
+        else:
+            msg = "{} is not a valid tag!".format(request.json['tag'])
+            return self.json_message(msg, HTTP_UNAUTHORIZED)
+
 # pylint: disable=too-few-public-methods
 class HTML5NotificationService(BaseNotificationService):
     """Implement the notification service for HTML5."""
 
     # pylint: disable=too-many-arguments
-    def __init__(self, gcm_key, registrations):
+    def __init__(self, gcm_key, registrations, tags, tags_path):
         """Initialize the service."""
         self._gcm_key = gcm_key
         self.registrations = registrations
+        self.tags = tags
+        self.tags_path = tags_path
 
     def send_message(self, message="", **kwargs):
         """Send a message to a user."""
         from pywebpush import WebPusher
 
+        tag = str(uuid.uuid4())
+
+        timestamp = int(time.time())
+
         payload = {
             'body': message,
-            'title': kwargs.get(ATTR_TITLE),
             'icon': '/static/icons/favicon-192x192.png',
-            'timestamp': int(time.time()*1000) # Javascript ms since epoch
+            'tag': tag,
+            'timestamp': (timestamp*1000), # Javascript ms since epoch
+            'title': kwargs.get(ATTR_TITLE)
         }
 
         data = kwargs.get(ATTR_DATA)
@@ -163,6 +212,11 @@ class HTML5NotificationService(BaseNotificationService):
             targets = self.registrations.keys()
         elif not isinstance(targets, list):
             targets = [targets]
+
+        self.tags[tag] = {"timestamp": timestamp, "targets": list(targets)}
+
+        if not _save_config(self.tags_path, self.tags):
+            _LOGGER.error("Error saving %s to the tags file!", tag)
 
         for target in targets:
             info = self.registrations.get(target)
