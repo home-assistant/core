a91f8191ca0b783ed42df64bd521fc6672c0627a
Fabian Affolter <fabian@affolter-engineering.ch>
2016-08-15 19:37:45 +0200

    Initial step to migrate to voluptuous

--- homeassistant/components/binary_sensor/rest.py
+++ homeassistant/components/binary_sensor/rest.py
@@ -6,27 +6,43 @@ https://home-assistant.io/components/binary_sensor.rest/
 """
 import logging
 
-from homeassistant.components.binary_sensor import (BinarySensorDevice,
-                                                    SENSOR_CLASSES)
-from homeassistant.components.sensor.rest import RestData
-from homeassistant.const import CONF_VALUE_TEMPLATE
-from homeassistant.helpers import template
+import voluptuous as vol
 
-_LOGGER = logging.getLogger(__name__)
+from homeassistant.components.binary_sensor import (
+    BinarySensorDevice, SENSOR_CLASSES, PLATFORM_SCHEMA)
+from homeassistant.components.sensor.rest import RestData
+from homeassistant.const import (
+    CONF_PAYLOAD, CONF_NAME, CONF_VALUE_TEMPLATE, CONF_METHOD, CONF_RESOURCE,
+    CONF_SENSOR_CLASS)
+from homeassistant.helpers import template
+import homeassistant.helpers.config_validation as cv
 
 DEFAULT_NAME = 'REST Binary Sensor'
 DEFAULT_METHOD = 'GET'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_RESOURCE): vol.Coerce(url),
+    vol.Optional(CONF_METHOD, default=DEFAULT_METHOD): vol.In(['POST', 'GET']),
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_PAYLOAD): cv.string,
+    vol.Optional(CONF_SENSOR_CLASS): vol.In(SENSOR_CLASSES),
+    vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+})
+
+_LOGGER = logging.getLogger(__name__)
 
 
 # pylint: disable=unused-variable
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the REST binary sensor."""
-    resource = config.get('resource', None)
-    method = config.get('method', DEFAULT_METHOD)
-    payload = config.get('payload', None)
+    name = config.get(CONF_NAME)
+    resource = config.get(CONF_RESOURCE)
+    method = config.get(CONF_METHOD)
+    payload = config.get(CONF_PAYLOAD)
     verify_ssl = config.get('verify_ssl', True)
+    sensor_class = config.get(CONF_SENSOR_CLASS)
+    value_template = config.get(CONF_VALUE_TEMPLATE)
 
-    sensor_class = config.get('sensor_class')
     if sensor_class not in SENSOR_CLASSES:
         _LOGGER.warning('Unknown sensor class: %s', sensor_class)
         sensor_class = None
@@ -39,11 +55,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         return False
 
     add_devices([RestBinarySensor(
-        hass,
-        rest,
-        config.get('name', DEFAULT_NAME),
-        sensor_class,
-        config.get(CONF_VALUE_TEMPLATE))])
+        hass, rest, name, sensor_class, value_template)])
 
 
 # pylint: disable=too-many-arguments
