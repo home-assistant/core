0fcc72e1d668ea46f0e9afdbb5fcd54a6e8ddb97
Robbie Trencheny <me@robbiet.us>
2016-08-16 15:35:18 -0700

    Add action and callback support to html5 (#2855).
    Remove registrations from the callback view since we always get the latest anyway.
    
    We dont put an audience in the claims so we will never hit this error.
    
    Bring tests back up to where they were before callbacks.
    
    Only import jwt where necessary
    
    Fix bracket spacing errors
    
    Fix JWT decode check for loop
    
    Remove stale comment.
    
    Add tests for the callback system.
    
    Shorten line
    
    Disable pylint broad-except and change e to jwt_decode_error.
    
    Verify expiration
    
    Remove duplicate jwt.exceptions.DecodeError
    
    Catch no keys matched and return False

--- homeassistant/components/notify/html5.py
+++ homeassistant/components/notify/html5.py
@@ -8,12 +8,13 @@ import os
 import logging
 import json
 import time
+import datetime
 
 import voluptuous as vol
 from voluptuous.humanize import humanize_error
 
 from homeassistant.const import (
-    HTTP_BAD_REQUEST, HTTP_INTERNAL_SERVER_ERROR)
+    HTTP_BAD_REQUEST, HTTP_INTERNAL_SERVER_ERROR, HTTP_UNAUTHORIZED)
 from homeassistant.util import ensure_unique_string
 from homeassistant.components.notify import (
     ATTR_TARGET, ATTR_TITLE, ATTR_DATA, BaseNotificationService,
@@ -24,13 +25,13 @@ from homeassistant.helpers import config_validation as cv
 
 REQUIREMENTS = ['https://github.com/web-push-libs/pywebpush/archive/'
                 'e743dc92558fc62178d255c0018920d74fa778ed.zip#'
-                'pywebpush==0.5.0']
+                'pywebpush==0.5.0', 'PyJWT==1.4.2']
 
-DEPENDENCIES = ["frontend"]
+DEPENDENCIES = ['frontend']
 
 _LOGGER = logging.getLogger(__name__)
 
-REGISTRATIONS_FILE = "html5_push_registrations.conf"
+REGISTRATIONS_FILE = 'html5_push_registrations.conf'
 
 ATTR_GCM_SENDER_ID = 'gcm_sender_id'
 ATTR_GCM_API_KEY = 'gcm_api_key'
@@ -48,6 +49,8 @@ REGISTER_SCHEMA = vol.Schema({
     vol.Required(ATTR_BROWSER): vol.In(['chrome', 'firefox'])
 })
 
+NOTIFY_CALLBACK_EVENT = 'html5_notification'
+
 
 def get_service(hass, config):
     """Get the HTML5 push notification service."""
@@ -60,6 +63,7 @@ def get_service(hass, config):
 
     hass.wsgi.register_view(
         HTML5PushRegistrationView(hass, registrations, json_path))
+    hass.wsgi.register_view(HTML5PushCallbackView(hass, json_path))
 
     gcm_api_key = config.get('gcm_api_key')
     gcm_sender_id = config.get('gcm_sender_id')
@@ -76,7 +80,7 @@ def _load_config(filename):
         return {}
 
     try:
-        with open(filename, "r") as fdesc:
+        with open(filename, 'r') as fdesc:
             inp = fdesc.read()
 
         # In case empty file
@@ -85,17 +89,17 @@ def _load_config(filename):
 
         return json.loads(inp)
     except (IOError, ValueError) as error:
-        _LOGGER.error("Reading config file %s failed: %s", filename, error)
+        _LOGGER.error('Reading config file %s failed: %s', filename, error)
         return None
 
 
 def _save_config(filename, config):
     """Save configuration."""
     try:
-        with open(filename, "w") as fdesc:
+        with open(filename, 'w') as fdesc:
             fdesc.write(json.dumps(config, indent=4, sort_keys=True))
     except (IOError, TypeError) as error:
-        _LOGGER.error("Saving config file failed: %s", error)
+        _LOGGER.error('Saving config file failed: %s', error)
         return False
     return True
 
@@ -103,8 +107,8 @@ def _save_config(filename, config):
 class HTML5PushRegistrationView(HomeAssistantView):
     """Accepts push registrations from a browser."""
 
-    url = "/api/notify.html5"
-    name = "api:notify.html5"
+    url = '/api/notify.html5'
+    name = 'api:notify.html5'
 
     def __init__(self, hass, registrations, json_path):
         """Init HTML5PushRegistrationView."""
@@ -129,7 +133,87 @@ class HTML5PushRegistrationView(HomeAssistantView):
             return self.json_message('Error saving registration.',
                                      HTTP_INTERNAL_SERVER_ERROR)
 
-        return self.json_message("Push notification subscriber registered.")
+        return self.json_message('Push notification subscriber registered.')
+
+
+class HTML5PushCallbackView(HomeAssistantView):
+    """Accepts push registrations from a browser."""
+
+    requires_auth = False
+    url = '/api/notify.html5/callback'
+    name = 'api:notify.html5/callback'
+
+    def __init__(self, hass, json_path):
+        """Init HTML5PushCallbackView."""
+        super().__init__(hass)
+        self.json_path = json_path
+
+    def decode_jwt(self, token):
+        """Find the registration that signed this JWT and return it."""
+        import jwt
+        registrations = _load_config(self.json_path)
+        for reg in registrations.values():
+            auth_key = reg['subscription']['keys']['auth']
+            try:
+                payload = jwt.decode(token, auth_key)
+            except jwt.exceptions.DecodeError:
+                continue
+            # pylint: disable=broad-except
+            except Exception as jwt_decode_error:
+                return jwt_decode_error
+            else:
+                if payload == {}:
+                    return False
+                else:
+                    return payload
+
+    # The following is based on code from Auth0
+    # https://auth0.com/docs/quickstart/backend/python
+    def json_error(self, code, description):
+        """Return a JSON formatted forbidden error."""
+        return self.json({'code': code, 'description': description},
+                         status_code=HTTP_UNAUTHORIZED)
+
+    # pylint: disable=too-many-return-statements
+    def check_authorization_header(self, request):
+        """Check the authorization header."""
+        import jwt
+        auth = request.headers.get('Authorization', None)
+        if not auth:
+            return self.json_error('authorization_header_missing',
+                                   'Authorization header is expected')
+
+        parts = auth.split()
+
+        if parts[0].lower() != 'bearer':
+            return self.json_error('invalid_header',
+                                   'Authorization header must '
+                                   'start with Bearer')
+        elif len(parts) == 1:
+            return self.json_error('invalid_header',
+                                   'Token not found')
+        elif len(parts) > 2:
+            return self.json_error('invalid_header',
+                                   'Authorization header must be Bearer token')
+
+        token = parts[1]
+        try:
+            self.decode_jwt(token)
+        except jwt.exceptions.ExpiredSignature:
+            return self.json_error('token_expired',
+                                   'token is expired')
+        return True
+
+    def post(self, request):
+        """Accept the POST request for push registrations event callback."""
+        auth_check = self.check_authorization_header(request)
+        if auth_check is not True:
+            return auth_check
+        event_name = '{}.{}'.format(NOTIFY_CALLBACK_EVENT,
+                                    request.json['type'])
+        self.hass.bus.fire(event_name, request.json)
+        return self.json({'status': 'ok',
+                          'event': request.json['type']})
 
 
 # pylint: disable=too-few-public-methods
@@ -149,6 +233,7 @@ class HTML5NotificationService(BaseNotificationService):
 
     def send_message(self, message="", **kwargs):
         """Send a message to a user."""
+        import jwt
         from pywebpush import WebPusher
 
         timestamp = int(time.time())
@@ -182,9 +267,16 @@ class HTML5NotificationService(BaseNotificationService):
         for target in targets:
             info = self.registrations.get(target)
             if info is None:
-                _LOGGER.error("%s is not a valid HTML5 push notification"
-                              " target!", target)
+                _LOGGER.error('%s is not a valid HTML5 push notification'
+                              ' target!', target)
                 continue
+
+            jwt_exp = (datetime.datetime.fromtimestamp(timestamp) +
+                       datetime.timedelta(days=7))
+            jwt_secret = info[ATTR_SUBSCRIPTION]['keys']['auth']
+            jwt_claims = {'exp': jwt_exp, 'nbf': timestamp, 'iat': timestamp}
+            payload['data']['jwt'] = jwt.encode(jwt_claims,
+                                                jwt_secret).decode('utf-8')
 
             WebPusher(info[ATTR_SUBSCRIPTION]).send(
                 json.dumps(payload), gcm_key=self._gcm_key, ttl='86400')
