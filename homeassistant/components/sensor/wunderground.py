d38a743948a2262e8ce8144ba3ad9ec882990573
arsaboo <arsaboo@gmx.com>
2016-08-17 17:04:11 -0400

    Update wunderground.py

--- homeassistant/components/sensor/wunderground.py
+++ homeassistant/components/sensor/wunderground.py
@@ -49,8 +49,7 @@ SENSOR_TYPES = {
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the Wundeground sensor.
-    
+    """Setup the Wunderground sensor.
     payload = config.get('payload', None)
     rest = WUndergroundData(_RESOURCE,
                             config.get(CONF_PWS_ID),
