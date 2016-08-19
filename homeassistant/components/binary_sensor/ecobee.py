5069065a5c3b2e063c5d39524a15ac7de7896e4b
Nolan Gilley <nkgilley@gmail.com>
2016-08-19 03:11:56 -0400

    binary occupancy sensor (#2869)

--- homeassistant/components/binary_sensor/ecobee.py
+++ homeassistant/components/binary_sensor/ecobee.py
@@ -5,14 +5,9 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.ecobee/
 """
 from homeassistant.components import ecobee
-from homeassistant.const import TEMP_FAHRENHEIT
-from homeassistant.helpers.entity import Entity
+from homeassistant.components.binary_sensor import BinarySensorDevice
 
 DEPENDENCIES = ['ecobee']
-SENSOR_TYPES = {
-    'temperature': ['Temperature', TEMP_FAHRENHEIT],
-    'humidity': ['Humidity', '%']
-}
 
 ECOBEE_CONFIG_FILE = 'ecobee.conf'
 
@@ -26,25 +21,24 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     for index in range(len(data.ecobee.thermostats)):
         for sensor in data.ecobee.get_remote_sensors(index):
             for item in sensor['capability']:
-                if item['type'] not in ('temperature', 'humidity'):
+                if item['type'] != 'occupancy':
                     continue
 
-                dev.append(EcobeeSensor(sensor['name'], item['type'], index))
+                dev.append(EcobeeBinarySensor(sensor['name'], index))
 
     add_devices(dev)
 
 
-class EcobeeSensor(Entity):
+class EcobeeBinarySensor(BinarySensorDevice):
     """Representation of an Ecobee sensor."""
 
-    def __init__(self, sensor_name, sensor_type, sensor_index):
+    def __init__(self, sensor_name, sensor_index):
         """Initialize the sensor."""
-        self._name = sensor_name + ' ' + SENSOR_TYPES[sensor_type][0]
+        self._name = sensor_name + ' Occupancy'
         self.sensor_name = sensor_name
-        self.type = sensor_type
         self.index = sensor_index
         self._state = None
-        self._unit_of_measurement = SENSOR_TYPES[sensor_type][1]
+        self._sensor_class = 'motion'
         self.update()
 
     @property
@@ -53,19 +47,19 @@ class EcobeeSensor(Entity):
         return self._name.rstrip()
 
     @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
+    def is_on(self):
+        """Return the status of the sensor."""
+        return self._state == 'true'
 
     @property
     def unique_id(self):
         """Return the unique ID of this sensor."""
-        return "sensor_ecobee_{}_{}".format(self._name, self.index)
+        return "binary_sensor_ecobee_{}_{}".format(self._name, self.index)
 
     @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement this sensor expresses itself in."""
-        return self._unit_of_measurement
+    def sensor_class(self):
+        """Return the class of this sensor, from SENSOR_CLASSES."""
+        return self._sensor_class
 
     def update(self):
         """Get the latest state of the sensor."""
@@ -73,10 +67,6 @@ class EcobeeSensor(Entity):
         data.update()
         for sensor in data.ecobee.get_remote_sensors(self.index):
             for item in sensor['capability']:
-                if (item['type'] == self.type and
+                if (item['type'] == 'occupancy' and
                         self.sensor_name == sensor['name']):
-                    if (self.type == 'temperature' and
-                            item['value'] != 'unknown'):
-                        self._state = float(item['value']) / 10
-                    else:
-                        self._state = item['value']
+                    self._state = item['value']
