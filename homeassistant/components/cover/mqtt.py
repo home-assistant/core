e088a4bd362973821b35c7ecce1b487198473a1b
John Arild Berentsen <turbokongen@hotmail.com>
2016-08-21 17:24:49 +0200

    Update mqtt with garage door

--- homeassistant/components/cover/mqtt.py
+++ homeassistant/components/cover/mqtt.py
@@ -10,9 +10,11 @@ import voluptuous as vol
 
 import homeassistant.components.mqtt as mqtt
 from homeassistant.components.cover import CoverDevice
-from homeassistant.const import CONF_NAME, CONF_VALUE_TEMPLATE
+from homeassistant.const import (
+    CONF_NAME, CONF_VALUE_TEMPLATE, CONF_OPTIMISTIC, STATE_OPEN,
+    STATE_CLOSED)
 from homeassistant.components.mqtt import (
-    CONF_STATE_TOPIC, CONF_COMMAND_TOPIC, CONF_QOS)
+    CONF_STATE_TOPIC, CONF_COMMAND_TOPIC, CONF_QOS, CONF_RETAIN)
 from homeassistant.helpers import template
 import homeassistant.helpers.config_validation as cv
 
@@ -23,42 +25,55 @@ DEPENDENCIES = ['mqtt']
 CONF_PAYLOAD_OPEN = 'payload_open'
 CONF_PAYLOAD_CLOSE = 'payload_close'
 CONF_PAYLOAD_STOP = 'payload_stop'
+CONF_STATE_OPEN = 'state_open'
+CONF_STATE_CLOSED = 'state_closed'
 
 DEFAULT_NAME = "MQTT Cover"
 DEFAULT_PAYLOAD_OPEN = "OPEN"
 DEFAULT_PAYLOAD_CLOSE = "CLOSE"
 DEFAULT_PAYLOAD_STOP = "STOP"
+DEFAULT_OPTIMISTIC = False
+DEFAULT_RETAIN = False
 
 PLATFORM_SCHEMA = mqtt.MQTT_RW_PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_PAYLOAD_OPEN, default=DEFAULT_PAYLOAD_OPEN): cv.string,
     vol.Optional(CONF_PAYLOAD_CLOSE, default=DEFAULT_PAYLOAD_CLOSE): cv.string,
     vol.Optional(CONF_PAYLOAD_STOP, default=DEFAULT_PAYLOAD_STOP): cv.string,
+    vol.Optional(CONF_STATE_OPEN, default=STATE_OPEN): cv.string,
+    vol.Optional(CONF_STATE_CLOSED, default=STATE_CLOSED): cv.string,
+    vol.Optional(CONF_OPTIMISTIC, default=DEFAULT_OPTIMISTIC): cv.boolean,
+    vol.Optional(CONF_RETAIN, default=DEFAULT_RETAIN): cv.boolean,
+
 })
 
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """Add MQTT Rollershutter."""
-    add_devices_callback([MqttRollershutter(
+    add_devices_callback([MqttCover(
         hass,
         config[CONF_NAME],
         config.get(CONF_STATE_TOPIC),
         config[CONF_COMMAND_TOPIC],
         config[CONF_QOS],
+        config[CONF_RETAIN],
+        config[CONF_STATE_OPEN],
+        config[CONF_STATE_CLOSED],
         config[CONF_PAYLOAD_OPEN],
         config[CONF_PAYLOAD_CLOSE],
         config[CONF_PAYLOAD_STOP],
+        config[CONF_OPTIMISTIC],
         config.get(CONF_VALUE_TEMPLATE)
     )])
 
 
-# pylint: disable=abstract-method
 # pylint: disable=too-many-arguments, too-many-instance-attributes
-class MqttRollershutter(CoverDevice):
+class MqttCover(CoverDevice):
     """Representation of a roller shutter that can be controlled using MQTT."""
 
     def __init__(self, hass, name, state_topic, command_topic, qos,
-                 payload_open, payload_close, payload_stop, value_template):
+                 retain, state_open, state_closed, payload_open, payload_close,
+                 payload_stop, optimistic, value_template):
         """Initialize the roller shutter."""
         self._state = None
         self._hass = hass
@@ -69,23 +84,35 @@ class MqttRollershutter(CoverDevice):
         self._payload_open = payload_open
         self._payload_close = payload_close
         self._payload_stop = payload_stop
-
-        if self._state_topic is None:
-            return
+        self._state_open = state_open
+        self._state_closed = state_closed
+        self._retain = retain
+        self._optimistic = optimistic or state_topic is None
 
         def message_received(topic, payload, qos):
             """A new MQTT message has been received."""
             if value_template is not None:
                 payload = template.render_with_possible_json_value(
                     hass, value_template, payload)
-            if payload.isnumeric() and 0 <= int(payload) <= 100:
+            if payload == self._state_open:
+                self._state = 100
+                self.update_ha_state()
+            elif payload == self._state_closed:
+                self._state = 0
+                self.update_ha_state()
+            elif payload.isnumeric() and 0 <= int(payload) <= 100:
                 self._state = int(payload)
                 self.update_ha_state()
             else:
                 _LOGGER.warning(
-                    "Payload is expected to be an integer between 0 and 100")
-
-        mqtt.subscribe(hass, self._state_topic, message_received, self._qos)
+                    "Payload is not True or False or"
+                    " integer(0-100) %s", payload)
+        if self._state_topic is None:
+            # Force into optimistic mode.
+            self._optimistic = True
+        else:
+            mqtt.subscribe(hass, self._state_topic, message_received,
+                           self._qos)
 
     @property
     def should_poll(self):
@@ -108,14 +135,22 @@ class MqttRollershutter(CoverDevice):
     def open_cover(self, **kwargs):
         """Move the roller shutter up."""
         mqtt.publish(self.hass, self._command_topic, self._payload_open,
-                     self._qos)
+                     self._qos, self._retain)
+        if self._optimistic:
+            # Optimistically assume that cover has changed state.
+            self._state = 100
+            self.update_ha_state()
 
     def close_cover(self, **kwargs):
         """Move the roller shutter down."""
         mqtt.publish(self.hass, self._command_topic, self._payload_close,
-                     self._qos)
+                     self._qos, self._retain)
+        if self._optimistic:
+            # Optimistically assume that cover has changed state.
+            self._state = 0
+            self.update_ha_state()
 
     def stop_cover(self, **kwargs):
         """Stop the device."""
         mqtt.publish(self.hass, self._command_topic, self._payload_stop,
-                     self._qos)
+                     self._qos, self._retain)
