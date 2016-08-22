50ac029edffb8830de35b8945263bae27843496c
John Arild Berentsen <turbokongen@hotmail.com>
2016-08-22 12:46:46 +0200

    update test_demo.py

--- tests/components/cover/test_demo.py
+++ tests/components/cover/test_demo.py
@@ -2,8 +2,10 @@
 import unittest
 import homeassistant.util.dt as dt_util
 
-from homeassistant.components.cover import demo
-from tests.common import fire_time_changed, get_test_home_assistant
+from homeassistant.components import cover
+from tests.common import get_test_home_assistant, fire_time_changed
+
+ENTITY_COVER = 'cover.living_room_window'
 
 
 class TestCoverDemo(unittest.TestCase):
@@ -12,6 +14,9 @@ class TestCoverDemo(unittest.TestCase):
     def setUp(self):  # pylint: disable=invalid-name
         """Setup things to be run when tests are started."""
         self.hass = get_test_home_assistant()
+        self.assertTrue(cover.setup(self.hass, {'cover': {
+            'platform': 'demo',
+        }}))
 
     def tearDown(self):  # pylint: disable=invalid-name
         """Stop down everything that was started."""
@@ -19,74 +24,73 @@ class TestCoverDemo(unittest.TestCase):
 
     def test_close_cover(self):
         """Test closing the cover."""
-        entity = demo.DemoCover(self.hass, 'test', 100, None)
-        entity.close_cover()
-
+        state = self.hass.states.get(ENTITY_COVER)
+        self.assertEqual(70, state.attributes.get('current_position'))
+        cover.close_cover(self.hass, ENTITY_COVER)
         fire_time_changed(self.hass, dt_util.utcnow())
         self.hass.pool.block_till_done()
-        self.assertEqual(90, entity.current_cover_position)
+        self.assertEqual(0, state.attributes.get('current_position'))
 
     def test_open_cover(self):
         """Test opening the cover."""
-        entity = demo.DemoCover(self.hass, 'test', 0, None)
-        entity.open_cover()
-
+        state = self.hass.states.get(ENTITY_COVER)
+        self.assertEqual(70, state.attributes.get('current_position'))
+        cover.open_cover(self.hass, ENTITY_COVER)
         fire_time_changed(self.hass, dt_util.utcnow())
         self.hass.pool.block_till_done()
-        self.assertEqual(10, entity.current_cover_position)
+        self.assertEqual(100, state.attributes.get('current_position'))
 
     def test_set_cover_position(self):
         """Test moving the cover to a specific position."""
-        entity = demo.DemoCover(self.hass, 'test', 0, None)
-        entity.set_cover_position(10)
-
+        state = self.hass.states.get(ENTITY_COVER)
+        self.assertEqual(70, state.attributes.get('current_position'))
+        cover.set_cover_position(self.hass, 10, ENTITY_COVER)
         fire_time_changed(self.hass, dt_util.utcnow())
         self.hass.pool.block_till_done()
-        self.assertEqual(10, entity.current_cover_position)
+        self.assertEqual(10, state.attributes.get('current_position'))
 
     def test_stop_cover(self):
         """Test stopping the cover."""
-        entity = demo.DemoCover(self.hass, 'test', 0, None)
-        entity.open_cover()
-        entity.stop_cover()
-
+        state = self.hass.states.get(ENTITY_COVER)
+        self.assertEqual(70, state.attributes.get('current_position'))
+        cover.open_cover(self.hass, ENTITY_COVER)
+        cover.stop_cover(self.hass, ENTITY_COVER)
         fire_time_changed(self.hass, dt_util.utcnow())
-        self.hass.pool.block_till_done()
-        self.assertEqual(0, entity.current_cover_position)
+        self.assertEqual(70, state.attributes.get('current_position'))
 
     def test_close_cover_tilt(self):
         """Test closing the cover tilt."""
-        entity = demo.DemoCover(self.hass, 'test', None, 100)
-        entity.close_cover_tilt()
-
+        state = self.hass.states.get(ENTITY_COVER)
+        self.assertEqual(50, state.attributes.get('current_tilt_position'))
+        cover.close_cover_tilt(self.hass, ENTITY_COVER)
         fire_time_changed(self.hass, dt_util.utcnow())
         self.hass.pool.block_till_done()
-        self.assertEqual(90, entity.current_cover_tilt_position)
+        self.assertEqual(0, state.attributes.get('current_tilt_position'))
 
     def test_open_cover_tilt(self):
         """Test opening the cover tilt."""
-        entity = demo.DemoCover(self.hass, 'test', None, 0)
-        entity.open_cover_tilt()
-
+        state = self.hass.states.get(ENTITY_COVER)
+        self.assertEqual(50, state.attributes.get('current_tilt_position'))
+        cover.open_cover_tilt(self.hass, ENTITY_COVER)
         fire_time_changed(self.hass, dt_util.utcnow())
         self.hass.pool.block_till_done()
-        self.assertEqual(10, entity.current_cover_tilt_position)
+        self.assertEqual(100, state.attributes.get('current_tilt_position'))
 
     def test_set_cover_tilt_position(self):
         """Test moving the cover til to a specific position."""
-        entity = demo.DemoCover(self.hass, 'test', None, 0)
-        entity.set_cover_tilt_position(10)
-
+        state = self.hass.states.get(ENTITY_COVER)
+        self.assertEqual(50, state.attributes.get('current_tilt_position'))
+        cover.set_cover_tilt_position(self.hass, 90, ENTITY_COVER)
         fire_time_changed(self.hass, dt_util.utcnow())
         self.hass.pool.block_till_done()
-        self.assertEqual(10, entity.current_cover_tilt_position)
+        self.assertEqual(90, state.attributes.get('current_tilt_position'))
 
     def test_stop_cover_tilt(self):
         """Test stopping the cover tilt."""
-        entity = demo.DemoCover(self.hass, 'test', None, 0)
-        entity.open_cover_tilt()
-        entity.stop_cover_tilt()
-
+        state = self.hass.states.get(ENTITY_COVER)
+        self.assertEqual(50, state.attributes.get('current_tilt_position'))
+        cover.close_cover_tilt(self.hass, ENTITY_COVER)
+        cover.stop_cover_tilt(self.hass, ENTITY_COVER)
         fire_time_changed(self.hass, dt_util.utcnow())
         self.hass.pool.block_till_done()
-        self.assertEqual(0, entity.current_cover_tilt_position)
+        self.assertEqual(50, state.attributes.get('current_tilt_position'))
