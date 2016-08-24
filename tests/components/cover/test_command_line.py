9dc394b1a26a0e2d4c94031ff1d1dd366f633229
Fabian Affolter <fabian@affolter-engineering.ch>
2016-08-24 23:36:26 +0200

    Migrate to voluptuous

--- tests/components/cover/test_command_line.py
+++ tests/components/cover/test_command_line.py
@@ -20,8 +20,8 @@ class TestCommandCover(unittest.TestCase):
         self.hass.config.latitude = 32.87336
         self.hass.config.longitude = 117.22743
         self.rs = cmd_rs.CommandCover(self.hass, 'foo',
-                                      'cmd_open', 'cmd_close',
-                                      'cmd_stop', 'cmd_state',
+                                      'command_open', 'command_close',
+                                      'command_stop', 'command_state',
                                       None)  # FIXME
 
     def teardown_method(self, method):
@@ -47,10 +47,10 @@ class TestCommandCover(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tempdirname:
             path = os.path.join(tempdirname, 'cover_status')
             test_cover = {
-                'statecmd': 'cat {}'.format(path),
-                'opencmd': 'echo 1 > {}'.format(path),
-                'closecmd': 'echo 1 > {}'.format(path),
-                'stopcmd': 'echo 0 > {}'.format(path),
+                'command_state': 'cat {}'.format(path),
+                'command_open': 'echo 1 > {}'.format(path),
+                'command_close': 'echo 1 > {}'.format(path),
+                'command_stop': 'echo 0 > {}'.format(path),
                 'value_template': '{{ value }}'
             }
             self.assertTrue(cover.setup(self.hass, {
