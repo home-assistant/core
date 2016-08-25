bc5eaa2bbd4a3bbdb1fa832a35d434a6bec0db9f
samowens <sam@ivoryowensconsulting.co.uk>
2016-08-25 22:56:42 +0100

    added docs

--- homeassistant/components/notify/apns.py
+++ homeassistant/components/notify/apns.py
@@ -1,19 +1,90 @@
-"""Support for Apple push notification service."""
+"""
+The APNS platform uses the Apple Push Notification service (APNS) to deliver
+notifications from Home Assistant.
+
+To use the APNS service you will need an apple developer account
+and you will need to create an App to receive push notifications.
+For more information see the apple developer documentation.
+
+Sample Configuration:
+
+    notify:
+      platform: apns
+      name: doorbell_app
+      sandbox: true
+      cert_file: doorbell_app.pem
+
+Configuration Variables:
+
+    name: The name of the app.
+    sandbox: If true notifications will be sent to the sandbox (test) notification service.
+    cert_file: The certificate to use to authenticate with the APNS service.
+
+Usage:
+
+    The APNS platform will register two services, notify/[app_name] and apns/[app_name].
+
+    apns/app_name:
+        This service will register device id's with home assistant. In order to
+        receive a notification a device must be registered. The app on the
+        device can use this service to send its id during startup, the id will
+        be stored in the [app_name]_apns.yaml.
+
+        See didRegisterForRemoteNotificationsWithDeviceToken in the apple developer
+        documentation for more information.
+
+
+    notify/app_name
+        This service will send messages to a registered device. The following parameters can be used:
+
+        message:
+            The message to send
+
+        target:
+            The desired state of the device, only devices that match the state will receive messages.
+            To enable state tracking a registered device must have a device_tracking_id added to the
+            [app_name]_apns.yaml file. If this id matches a device in known_devices.yaml its state
+            will be tracked.
+
+        data:
+            badge:
+                The number to display as the badge of the app ic
+            sound:
+                The name of a sound file in the app bundle or in the Library/Sounds folder.
+            category:
+                Provide this key with a string value that represents the identifier
+                property of the UIMutableUserNotificationCategory
+            content_available:
+                Provide this key with a value of 1 to indicate that new content is available.
+"""
 import logging
 import os
+import voluptuous as vol
 
 from homeassistant.helpers.event import track_state_change
 from homeassistant.config import load_yaml_config_file
 from homeassistant.components.notify import (
     ATTR_TARGET, ATTR_DATA, BaseNotificationService)
+import homeassistant.helpers.config_validation as cv
 
 DOMAIN = "apns"
 APNS_DEVICES = "apns.yaml"
 DEVICE_TRACKER_DOMAIN = "device_tracker"
+SERVICE_REGISTER = "register"
 
+ATTR_PUSH_ID = "push_id"
+ATTR_NAME = "name"
+
+REGISTER_SERVICE_SCHEMA = vol.Schema({
+    vol.Required(ATTR_PUSH_ID): cv.template,
+    vol.Optional(ATTR_NAME, default=None): cv.string,
+})
 
 def get_service(hass, config):
     """Return push service."""
+
+    descriptions = load_yaml_config_file(
+        os.path.join(os.path.dirname(__file__), 'apns_services.yaml'))
 
     name = config.get("name")
     if name is None:
@@ -28,7 +99,11 @@ def get_service(hass, config):
     sandbox = bool(config.get('sandbox', False))
 
     service = ApnsNotificationService(hass, name, sandbox, cert_file)
-    hass.services.register(DOMAIN, name, service.register)
+    hass.services.register(DOMAIN,
+                           name,
+                           service.register,
+                           descriptions.get(SERVICE_REGISTER),
+                           schema=REGISTER_SERVICE_SCHEMA)
     return service
 
 
@@ -148,11 +223,11 @@ class ApnsNotificationService(BaseNotificationService):
     def register(self, call):
         """Register a device to receive push messages."""
 
-        push_id = call.data.get("push_id")
+        push_id = call.data.get(ATTR_PUSH_ID)
         if push_id is None:
             return False
 
-        device_name = call.data.get("name")
+        device_name = call.data.get(ATTR_NAME)
         current_device = self.devices.get(push_id)
         current_tracking_id = None if current_device is None \
             else current_device.tracking_device_id
