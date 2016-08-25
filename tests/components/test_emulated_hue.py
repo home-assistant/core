9bf431e28e9a7f879862764c0a56091359245db5
Matthew Bowen <matthew@mgbowen.com>
2016-08-25 13:37:35 -0700

    Started writing tests for emulated_hue.

---
+++ tests/components/test_emulated_hue.py
@@ -0,0 +1,286 @@
+import time
+import json
+import threading
+import asyncio
+
+import unittest
+import requests
+
+from homeassistant import bootstrap, const
+from homeassistant.components import emulated_hue, http, light, mqtt
+
+from tests.common import get_test_instance_port, get_test_home_assistant
+
+HTTP_SERVER_PORT = get_test_instance_port()
+BRIDGE_SERVER_PORT = get_test_instance_port()
+MQTT_BROKER_PORT = get_test_instance_port()
+
+BRIDGE_URL_BASE = "http://127.0.0.1:{}".format(BRIDGE_SERVER_PORT) + "{}"
+JSON_HEADERS = {const.HTTP_HEADER_CONTENT_TYPE: const.CONTENT_TYPE_JSON}
+
+mqtt_broker = None
+
+
+def setUpModule():
+    global mqtt_broker
+
+    mqtt_broker = MQTTBroker('127.0.0.1', MQTT_BROKER_PORT)
+    mqtt_broker.start()
+
+
+def tearDownModule():
+    global mqtt_broker
+
+    mqtt_broker.stop()
+
+
+def setup_hass_instance(emulated_hue_config):
+    hass = get_test_home_assistant()
+
+    bootstrap.setup_component(
+        hass, http.DOMAIN,
+        {http.DOMAIN: {http.CONF_SERVER_PORT: HTTP_SERVER_PORT}})
+
+    bootstrap.setup_component(hass, emulated_hue.DOMAIN, emulated_hue_config)
+
+    return hass
+
+
+def start_hass_instance(hass):
+    hass.start()
+    time.sleep(0.05)
+
+
+class TestEmulatedHue(unittest.TestCase):
+    hass = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.hass = setup_hass_instance({
+            emulated_hue.DOMAIN: {
+                emulated_hue.CONF_LISTEN_PORT: BRIDGE_SERVER_PORT
+            }})
+
+        start_hass_instance(cls.hass)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.hass.stop()
+
+    def test_description_xml(self):
+        import xml.etree.ElementTree as ET
+
+        result = requests.get(
+            BRIDGE_URL_BASE.format('/description.xml'), timeout=5)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertTrue('text/xml' in result.headers['content-type'])
+
+        # Make sure the XML is parsable
+        try:
+            ET.fromstring(result.text)
+        except:
+            self.fail('description.xml is not valid XML!')
+
+    def test_create_username(self):
+        request_json = {'devicetype': 'my_device'}
+
+        result = requests.post(
+            BRIDGE_URL_BASE.format('/api'), data=json.dumps(request_json),
+            timeout=5)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertTrue('application/json' in result.headers['content-type'])
+
+        resp_json = result.json()
+        success_json = resp_json[0]
+
+        self.assertTrue('success' in success_json)
+        self.assertTrue('username' in success_json['success'])
+
+
+class TestEmulatedHueExposedByDefault(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.hass = setup_hass_instance({
+            emulated_hue.DOMAIN: {
+                emulated_hue.CONF_LISTEN_PORT: BRIDGE_SERVER_PORT,
+                emulated_hue.CONF_EXPOSE_BY_DEFAULT: True
+            }
+        })
+
+        bootstrap.setup_component(cls.hass, mqtt.DOMAIN, {
+            'mqtt': {
+                'broker': '127.0.0.1',
+                'port': MQTT_BROKER_PORT
+            }
+        })
+
+        bootstrap.setup_component(cls.hass, light.DOMAIN, {
+            'light': [
+                {
+                    'platform': 'mqtt',
+                    'name': 'Office light',
+                    'state_topic': 'office/rgb1/light/status',
+                    'command_topic': 'office/rgb1/light/switch',
+                    'brightness_state_topic': 'office/rgb1/brightness/status',
+                    'brightness_command_topic': 'office/rgb1/brightness/set',
+                    'payload_on': 'ON',
+                    'payload_off': 'OFF',
+                    'optimistic': True
+                },
+                {
+                    'platform': 'mqtt',
+                    'name': 'Bedroom light',
+                    'state_topic': 'bedroom/rgb1/light/status',
+                    'command_topic': 'bedroom/rgb1/light/switch',
+                    'brightness_state_topic': 'bedroom/rgb1/brightness/status',
+                    'brightness_command_topic': 'bedroom/rgb1/brightness/set',
+                    'payload_on': 'ON',
+                    'payload_off': 'OFF',
+                    'optimistic': True
+                },
+                {
+                    'platform': 'mqtt',
+                    'name': 'Kitchen light',
+                    'state_topic': 'kitchen/rgb1/light/status',
+                    'command_topic': 'kitchen/rgb1/light/switch',
+                    'brightness_state_topic': 'kitchen/rgb1/brightness/status',
+                    'brightness_command_topic': 'kitchen/rgb1/brightness/set',
+                    'payload_on': 'ON',
+                    'payload_off': 'OFF',
+                    'optimistic': True
+                }
+            ]
+        })
+
+        start_hass_instance(cls.hass)
+
+        # Kitchen light is explicitly excluded from being exposed
+        kitchen_light_entity = cls.hass.states.get('light.kitchen_light')
+        attrs = dict(kitchen_light_entity.attributes)
+        attrs[emulated_hue.ATTR_EMULATED_HUE] = False
+        cls.hass.states.set(
+            kitchen_light_entity.entity_id, kitchen_light_entity.state,
+            attributes=attrs)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.hass.stop()
+
+    def test_discover_lights(self):
+        result = requests.get(
+            BRIDGE_URL_BASE.format('/api/username/lights'), timeout=5)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertTrue('application/json' in result.headers['content-type'])
+
+        result_json = result.json()
+
+        # Make sure the lights we added to the config are there
+        self.assertTrue('light.office_light' in result_json)
+        self.assertTrue('light.bedroom_light' in result_json)
+        self.assertTrue('light.kitchen_light' not in result_json)
+
+    def test_get_light_state(self):
+        # Turn office light on and set to 127 brightness
+        self.hass.services.call(
+            light.DOMAIN, const.SERVICE_TURN_ON,
+            {
+                const.ATTR_ENTITY_ID: 'light.office_light',
+                light.ATTR_BRIGHTNESS: 127
+            },
+            blocking=True)
+
+        office_json = self.perform_get_light_state('light.office_light', 200)
+
+        self.assertEqual(office_json['state']['on'], True)
+        self.assertEqual(office_json['state']['bri'], 127)
+
+        # Turn bedroom light off
+        self.hass.services.call(
+            light.DOMAIN, const.SERVICE_TURN_OFF,
+            {
+                const.ATTR_ENTITY_ID: 'light.bedroom_light'
+            },
+            blocking=True)
+
+        bedroom_json = self.perform_get_light_state('light.bedroom_light', 200)
+
+        self.assertEqual(bedroom_json['state']['on'], False)
+        self.assertEqual(bedroom_json['state']['bri'], 0)
+
+        # Make sure kitchen light isn't accessible
+        kitchen_url = '/api/username/lights/{}'.format('light.kitchen_light')
+        kitchen_result = requests.get(
+            BRIDGE_URL_BASE.format(kitchen_url), timeout=5)
+
+        self.assertEqual(kitchen_result.status_code, 404)
+
+    def perform_get_light_state(self, entity_id, expected_status):
+        result = requests.get(
+            BRIDGE_URL_BASE.format(
+                '/api/username/lights/{}'.format(entity_id)), timeout=5)
+
+        self.assertEqual(result.status_code, expected_status)
+
+        if expected_status == 200:
+            self.assertTrue(
+                'application/json' in result.headers['content-type'])
+
+            return result.json()
+
+        return None
+
+
+class MQTTBroker(object):
+    """Encapsulates an embedded MQTT broker."""
+
+    def __init__(self, host, port):
+        """Initialize a new instance."""
+        from hbmqtt.broker import Broker
+
+        self._loop = asyncio.new_event_loop()
+
+        hbmqtt_config = {
+            'listeners': {
+                'default': {
+                    'max-connections': 50000,
+                    'type': 'tcp',
+                    'bind': '{}:{}'.format(host, port)
+                }
+            },
+            'auth': {
+                'plugins': ['auth.anonymous'],
+                'allow-anonymous': True
+            }
+        }
+
+        self._broker = Broker(config=hbmqtt_config, loop=self._loop)
+
+        self._thread = threading.Thread(target=self._run_loop)
+        self._started_ev = threading.Event()
+
+    def start(self):
+        """Start the broker."""
+        self._thread.start()
+        self._started_ev.wait()
+
+    def stop(self):
+        """Stop the broker."""
+        self._loop.call_soon_threadsafe(asyncio.async, self._broker.shutdown())
+        self._loop.call_soon_threadsafe(self._loop.stop)
+        self._thread.join()
+
+    def _run_loop(self):
+        asyncio.set_event_loop(self._loop)
+        self._loop.run_until_complete(self._broker_coroutine())
+
+        self._started_ev.set()
+
+        self._loop.run_forever()
+        self._loop.close()
+
+    @asyncio.coroutine
+    def _broker_coroutine(self):
+        yield from self._broker.start()
