826ec9b9d7140451c030d978e09f31517b5b938a
Teagan Glenn <teagan@aptotude.com>
2016-08-25 06:47:07 -0600

    Add a Fan component and support for an Insteon Hub Fan (#2964)
    * Fan component and service definitions
    * Insteon Hub fan support

--- homeassistant/components/light/insteon_hub.py
+++ homeassistant/components/light/insteon_hub.py
@@ -4,11 +4,13 @@ Support for Insteon Hub lights.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/insteon_hub/
 """
-from homeassistant.components.insteon_hub import INSTEON
+from homeassistant.components.insteon_hub import (INSTEON, InsteonDevice)
 from homeassistant.components.light import (ATTR_BRIGHTNESS,
                                             SUPPORT_BRIGHTNESS, Light)
 
 SUPPORT_INSTEON_HUB = SUPPORT_BRIGHTNESS
+
+DEPENDENCIES = ['insteon_hub']
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -16,62 +18,62 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     devs = []
     for device in INSTEON.devices:
         if device.DeviceCategory == "Switched Lighting Control":
-            devs.append(InsteonToggleDevice(device))
+            devs.append(InsteonLightDevice(device))
         if device.DeviceCategory == "Dimmable Lighting Control":
-            devs.append(InsteonToggleDevice(device))
+            devs.append(InsteonDimmableDevice(device))
     add_devices(devs)
 
 
-class InsteonToggleDevice(Light):
-    """An abstract Class for an Insteon node."""
+class InsteonLightDevice(InsteonDevice, Light):
+    """A representation of a light device."""
 
-    def __init__(self, node):
+    def __init__(self, node: object) -> None:
         """Initialize the device."""
-        self.node = node
+        super(InsteonLightDevice, self).__init__(node)
         self._value = 0
 
-    @property
-    def name(self):
-        """Return the the name of the node."""
-        return self.node.DeviceName
-
-    @property
-    def unique_id(self):
-        """Return the ID of this insteon node."""
-        return self.node.DeviceID
-
-    @property
-    def brightness(self):
-        """Return the brightness of this light between 0..255."""
-        return self._value / 100 * 255
-
-    def update(self):
-        """Update state of the sensor."""
-        resp = self.node.send_command('get_status', wait=True)
+    def update(self) -> None:
+        """Update state of the device."""
+        resp = self._node.send_command('get_status', wait=True)
         try:
             self._value = resp['response']['level']
         except KeyError:
             pass
 
     @property
-    def is_on(self):
+    def is_on(self) -> None:
         """Return the boolean response if the node is on."""
         return self._value != 0
 
+    def turn_on(self, **kwargs) -> None:
+        """Turn device on."""
+        if self._send_command('on'):
+            self._value = 100
+
+    def turn_off(self, **kwargs) -> None:
+        """Turn device off."""
+        if self._send_command('off'):
+            self._value = 0
+
+
+class InsteonDimmableDevice(InsteonLightDevice):
+    """A representation for a dimmable device."""
+
     @property
-    def supported_features(self):
+    def brightness(self) -> int:
+        """Return the brightness of this light between 0..255."""
+        return round(self._value / 100 * 255, 0)  # type: int
+
+    @property
+    def supported_features(self) -> int:
         """Flag supported features."""
         return SUPPORT_INSTEON_HUB
 
-    def turn_on(self, **kwargs):
+    def turn_on(self, **kwargs) -> None:
         """Turn device on."""
+        level = 100  # type: int
         if ATTR_BRIGHTNESS in kwargs:
-            self._value = kwargs[ATTR_BRIGHTNESS] / 255 * 100
-            self.node.send_command('on', self._value)
-        else:
-            self._value = 100
-            self.node.send_command('on')
+            level = round(kwargs[ATTR_BRIGHTNESS] / 255 * 100, 0)  # type: int
 
-    def turn_off(self, **kwargs):
-        """Turn device off."""
-        self.node.send_command('off')
+        if self._send_command('on', level=level):
+            self._value = level
