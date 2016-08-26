a304e757aebd84279213b99f2ec095d458a100e7
Teagan M. Glenn <that@teagantotally.rocks>
2016-08-25 19:54:28 -0600

    Update demo tests

--- tests/components/fan/test_demo.py
+++ tests/components/fan/test_demo.py
@@ -30,11 +30,35 @@ class TestDemoFan(unittest.TestCase):
         """Test turning on the device."""
         self.assertEqual(STATE_OFF, self.get_entity().state)
 
-        fan.turn_on(FAN_ENTITY_ID)
-        self.hass.block_till_done()
+        fan.turn_on(self.hass, FAN_ENTITY_ID)
         self.assertNotEqual(STATE_OFF, self.get_entity().state)
 
-        fan.turn_on(FAN_ENTITY_ID, fan.SPEED_HIGH)
-        self.hass.block_till_done()
+        fan.turn_on(self.hass, FAN_ENTITY_ID, fan.SPEED_HIGH)
         self.assertEqual(fan.SPEED_HIGH, self.get_entity().state)
 
+    def test_turn_on(self):
+        """Test turning off the device."""
+        self.assertEqual(STATE_OFF, self.get_entity().state)
+
+        fan.turn_on(self.hass, FAN_ENTITY_ID)
+        self.assertNotEqual(STATE_OFF, self.get_entity().state)
+
+        fan.turn_off(self.hass, FAN_ENTITY_ID)
+        self.assertEqual(fan.STATE_OFF, self.get_entity().state)
+
+    def test_set_speed(self):
+        """Test setting the speed of the device."""
+        self.assertEqual(STATE_OFF, self.get_entity().state)
+
+        fan.set_speed(self.hass, FAN_ENTITY_ID, fan.SPEED_LOW)
+        self.assertEqual(fan.SPEED_LOW, self.get_entity().state)
+
+    def test_oscillate(self):
+        """Test oscillating the fan."""
+        self.assertFalse(self.get_entity().attributes.get('oscillating'))
+
+        fan.oscillate(self.hass, FAN_ENTITY_ID, True)
+        self.assertTrue(self.get_entity().attributes.get('oscillating'))
+
+        fan.oscillate(self.hass, FAN_ENTITY_ID, False)
+        self.assertFalse(self.get_entity().attributes.get('oscillating'))
