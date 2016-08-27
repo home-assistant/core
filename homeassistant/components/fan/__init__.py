fdb6de4d2332f05d6797068c2da797f9fcc87abf
Teagan Glenn <teagan@aptotude.com>
2016-08-27 14:53:12 -0600

    Fan demo (#2976)
    * Update attr to property and default state method
    
    * State prop is defined in parent class
    
    * Demo platform fan
    
    * PyDoc
    
    * Copy-pasta artifact
    
    * PyDoc
    
    * Linting
    
    * Raise error if turn_off and turn_on not implemented
    
    * Update demo platform
    
    * Initial unit test commit
    
    * Readability
    
    * Unneeded typing
    
    * Should inherit from fan entity
    
    * Turn off polling
    
    * Initial oscillating flag
    
    * Pass HASS into demo
    
    * Typing
    
    * Invoke set_speed instead of setting directly
    
    * Service update
    
    * Update demo tests
    
    * Forgot to block after service call.
    
    * linting
    
    * Test to make sure not implemented is thrown
    
    * Is On Method test
    
    * Update const to match string
    
    * Update services yaml
    
    * Toggle method
    
    * Toggle service
    
    * Typing
    
    * TYPE O
    
    * Attribute check
    
    * Type-o
    
    * Type-o
    
    * Put typing back
    
    * ToggleEntity
    
    * Linting
    
    * Linting
    
    * Oops
    
    * Stale prints
    
    * Demo support

--- homeassistant/components/fan/__init__.py
+++ homeassistant/components/fan/__init__.py
@@ -11,10 +11,10 @@ import voluptuous as vol
 
 from homeassistant.components import group
 from homeassistant.config import load_yaml_config_file
-from homeassistant.const import (
-    STATE_OFF, SERVICE_TURN_ON,
-    SERVICE_TURN_OFF, ATTR_ENTITY_ID)
-from homeassistant.helpers.entity import Entity
+from homeassistant.const import (SERVICE_TURN_ON, SERVICE_TOGGLE,
+                                 SERVICE_TURN_OFF, ATTR_ENTITY_ID,
+                                 STATE_UNKNOWN)
+from homeassistant.helpers.entity import ToggleEntity
 from homeassistant.helpers.entity_component import EntityComponent
 from homeassistant.helpers.config_validation import PLATFORM_SCHEMA  # noqa
 import homeassistant.helpers.config_validation as cv
@@ -43,12 +43,12 @@ SPEED_HIGH = 'high'
 
 ATTR_SPEED = 'speed'
 ATTR_SPEED_LIST = 'speed_list'
-ATTR_OSCILLATE = 'oscillate'
+ATTR_OSCILLATING = 'oscillating'
 
 PROP_TO_ATTR = {
     'speed': ATTR_SPEED,
     'speed_list': ATTR_SPEED_LIST,
-    'oscillate': ATTR_OSCILLATE,
+    'oscillating': ATTR_OSCILLATING,
     'supported_features': ATTR_SUPPORTED_FEATURES,
 }  # type: dict
 
@@ -68,8 +68,12 @@ FAN_TURN_OFF_SCHEMA = vol.Schema({
 
 FAN_OSCILLATE_SCHEMA = vol.Schema({
     vol.Required(ATTR_ENTITY_ID): cv.entity_ids,
-    vol.Required(ATTR_OSCILLATE): cv.boolean
+    vol.Required(ATTR_OSCILLATING): cv.boolean
 })  # type: dict
+
+FAN_TOGGLE_SCHEMA = vol.Schema({
+    vol.Required(ATTR_ENTITY_ID): cv.entity_ids
+})
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -77,7 +81,8 @@ _LOGGER = logging.getLogger(__name__)
 def is_on(hass, entity_id: str=None) -> bool:
     """Return if the fans are on based on the statemachine."""
     entity_id = entity_id or ENTITY_ID_ALL_FANS
-    return not hass.states.is_state(entity_id, STATE_OFF)
+    state = hass.states.get(entity_id)
+    return state.attributes[ATTR_SPEED] not in [SPEED_OFF, STATE_UNKNOWN]
 
 
 # pylint: disable=too-many-arguments
@@ -102,12 +107,21 @@ def turn_off(hass, entity_id: str=None) -> None:
     hass.services.call(DOMAIN, SERVICE_TURN_OFF, data)
 
 
+def toggle(hass, entity_id: str=None) -> None:
+    """Toggle all or specified fans."""
+    data = {
+        ATTR_ENTITY_ID: entity_id
+    }
+
+    hass.services.call(DOMAIN, SERVICE_TOGGLE, data)
+
+
 def oscillate(hass, entity_id: str=None, should_oscillate: bool=True) -> None:
     """Set oscillation on all or specified fan."""
     data = {
         key: value for key, value in [
             (ATTR_ENTITY_ID, entity_id),
-            (ATTR_OSCILLATE, should_oscillate),
+            (ATTR_OSCILLATING, should_oscillate),
         ] if value is not None
     }
 
@@ -180,38 +194,47 @@ def setup(hass, config: dict) -> None:
     return True
 
 
-class FanEntity(Entity):
+class FanEntity(ToggleEntity):
     """Representation of a fan."""
 
     # pylint: disable=no-self-use, abstract-method
 
-    def set_speed(self: Entity, speed: str) -> None:
+    def set_speed(self: ToggleEntity, speed: str) -> None:
         """Set the speed of the fan."""
         pass
 
-    def turn_on(self: Entity, speed: str=None) -> None:
+    def turn_on(self: ToggleEntity, speed: str=None, **kwargs) -> None:
         """Turn on the fan."""
-        pass
+        raise NotImplementedError()
 
-    def turn_off(self: Entity) -> None:
+    def turn_off(self: ToggleEntity, **kwargs) -> None:
         """Turn off the fan."""
-        pass
+        raise NotImplementedError()
 
-    def oscillate(self: Entity) -> None:
+    def oscillate(self: ToggleEntity, oscillating: bool) -> None:
         """Oscillate the fan."""
         pass
 
     @property
-    def speed_list(self: Entity) -> list:
+    def is_on(self):
+        """Return true if the entity is on."""
+        return self.state_attributes.get(ATTR_SPEED, STATE_UNKNOWN) \
+            not in [SPEED_OFF, STATE_UNKNOWN]
+
+    @property
+    def speed_list(self: ToggleEntity) -> list:
         """Get the list of available speeds."""
         return []
 
     @property
-    def state_attributes(self: Entity) -> dict:
+    def state_attributes(self: ToggleEntity) -> dict:
         """Return optional state attributes."""
         data = {}  # type: dict
 
         for prop, attr in PROP_TO_ATTR.items():
+            if not hasattr(self, prop):
+                continue
+
             value = getattr(self, prop)
             if value is not None:
                 data[attr] = value
@@ -219,6 +242,6 @@ class FanEntity(Entity):
         return data
 
     @property
-    def supported_features(self: Entity) -> int:
+    def supported_features(self: ToggleEntity) -> int:
         """Flag supported features."""
         return 0
