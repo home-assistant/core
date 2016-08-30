ce6dc69f9cec63a0a6e9af80b28d560ab02f54e1
Johann Kellerman <kellerza@gmail.com>
2016-08-30 18:22:52 +0200

    Device tracker component & platform validation. No more home_range. (#2908)
    * Device tracker component & platform validation. No more home_range.
    
    * Mock, bluetooth
    
    * Renamed _CONFIG_SCHEMA. Raise warning for #1606
    
    * test duplicates
    
    * Fix assert
    
    * Coverage
    
    * Typing
    
    * T fixes

--- homeassistant/helpers/typing.py
+++ homeassistant/helpers/typing.py
@@ -1,24 +1,13 @@
 """Typing Helpers for Home-Assistant."""
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
 
-# NOTE: NewType added to typing in 3.5.2 in June, 2016; Since 3.5.2 includes
-#       security fixes everyone on 3.5 should upgrade "soon"
-try:
-    from typing import NewType
-except ImportError:
-    NewType = None
+import homeassistant.core
 
 # pylint: disable=invalid-name
-if NewType:
-    ConfigType = NewType('ConfigType', Dict[str, Any])
 
-    # Custom type for recorder Queries
-    QueryType = NewType('QueryType', Any)
+GPSType = Tuple[float, float]
+ConfigType = Dict[str, Any]
+HomeAssistantType = homeassistant.core.HomeAssistant
 
-# Duplicates for 3.5.1
-# pylint: disable=invalid-name
-else:
-    ConfigType = Dict[str, Any]  # type: ignore
-
-    # Custom type for recorder Queries
-    QueryType = Any  # type: ignore
+# Custom type for recorder Queries
+QueryType = Any
