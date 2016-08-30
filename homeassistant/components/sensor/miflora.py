55f073f46566e1a7f5f6d3a2c6d131ca588069d5
Daniel Matuschek <info@hifiberry.com>
2016-08-30 09:32:09 +0200

    Removed default from monitored conditions

--- homeassistant/components/sensor/miflora.py
+++ homeassistant/components/sensor/miflora.py
@@ -34,7 +34,7 @@ SENSOR_TYPES = {
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_MAC): cv.string,
-    vol.Required(CONF_MONITORED_CONDITIONS, default=[]):
+    vol.Required(CONF_MONITORED_CONDITIONS):
         vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_FORCE_UPDATE, default=False): cv.boolean,
