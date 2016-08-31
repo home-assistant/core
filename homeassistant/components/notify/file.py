22a8b85913af131d746f3a20756c216599580c5d
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-08-31 18:12:34 +0200

    Use voluptuous for file (#3049)

--- homeassistant/components/notify/file.py
+++ homeassistant/components/notify/file.py
@@ -7,24 +7,28 @@ https://home-assistant.io/components/notify.file/
 import logging
 import os
 
+import voluptuous as vol
+
 import homeassistant.util.dt as dt_util
 from homeassistant.components.notify import (
-    ATTR_TITLE, DOMAIN, BaseNotificationService)
-from homeassistant.helpers import validate_config
+    ATTR_TITLE, PLATFORM_SCHEMA, BaseNotificationService)
+from homeassistant.const import CONF_FILENAME
+import homeassistant.helpers.config_validation as cv
+
+CONF_TIMESTAMP = 'timestamp'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_FILENAME): cv.string,
+    vol.Optional(CONF_TIMESTAMP, default=False): cv.boolean,
+})
 
 _LOGGER = logging.getLogger(__name__)
 
 
 def get_service(hass, config):
     """Get the file notification service."""
-    if not validate_config({DOMAIN: config},
-                           {DOMAIN: ['filename',
-                                     'timestamp']},
-                           _LOGGER):
-        return None
-
-    filename = config['filename']
-    timestamp = config['timestamp']
+    filename = config[CONF_FILENAME]
+    timestamp = config[CONF_TIMESTAMP]
 
     return FileNotificationService(hass, filename, timestamp)
 
@@ -48,7 +52,7 @@ class FileNotificationService(BaseNotificationService):
                     '-' * 80)
                 file.write(title)
 
-            if self.add_timestamp == 1:
+            if self.add_timestamp:
                 text = '{} {}\n'.format(dt_util.utcnow().isoformat(), message)
                 file.write(text)
             else:
