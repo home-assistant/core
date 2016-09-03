41dc3ba65b068e398424f582456d3741a62c5082
Robbie Trencheny <me@robbiet.us>
2016-09-03 15:59:20 -0700

    0.27.2 (#3151)
    * Host should be optional for apcupsd component (#3072)
    
    * Zwave climate Bugfix: if some setpoints have different units, we should fetch the o… (#3078)
    
    * Bugfix: if some setpoints have different units, we should fetch the one that are active.
    
    * Move order of population for first time detection
    
    * Default to config if None unit_of_measurement
    
    * unit fix (#3083)
    
    * humidity slider (#3088)
    
    * If device was off target temp was null. Default to Heating setpoint (#3091)
    
    * Fix for BLE device tracker (#3019)
    
    * Bug fix tracked devices
    * Added scan_duration configuration parameter
    
    * fix homematic climate implementation (#3114)
    
    * Allow 'None' MAC to be loaded from known_devices (#3102)
    
    * Climate and cover bugfix (#3097)
    
    * Avoid None comparison for zwave cover.
    
    * Just rely on unit from config for unit_of_measurement
    
    * Explicit return None
    
    * Mqtt (#11)
    
    * Explicit return None
    
    * Missing service and wrong service name defined
    
    * Mqtt state was inverted, and never triggering
    
    * Fixed Homematic cover (#3116)
    
    * Add missing docstrings (fix PEP257 issues) (#3098)
    
    * Add missing docstrings (fix PEP257 issues)
    
    * Finish sentence
    
    * Merge pull request #3130 from turbokongen/zwave_fixes
    
    Bugfix. climate and covermqt
    
    * Back out insteon hub and fan changes (#3062)
    
    * Bump version
    
    * Special frontend build for 0.27.2

--- homeassistant/components/cover/homematic.py
+++ homeassistant/components/cover/homematic.py
@@ -11,7 +11,7 @@ properly configured.
 import logging
 from homeassistant.const import STATE_UNKNOWN
 from homeassistant.components.cover import CoverDevice,\
-    ATTR_CURRENT_POSITION
+    ATTR_POSITION
 import homeassistant.components.homematic as homematic
 
 _LOGGER = logging.getLogger(__name__)
@@ -41,16 +41,16 @@ class HMCover(homematic.HMDevice, CoverDevice):
         None is unknown, 0 is closed, 100 is fully open.
         """
         if self.available:
-            return int((1 - self._hm_get_state()) * 100)
+            return int(self._hm_get_state() * 100)
         return None
 
     def set_cover_position(self, **kwargs):
         """Move the cover to a specific position."""
         if self.available:
-            if ATTR_CURRENT_POSITION in kwargs:
-                position = float(kwargs[ATTR_CURRENT_POSITION])
+            if ATTR_POSITION in kwargs:
+                position = float(kwargs[ATTR_POSITION])
                 position = min(100, max(0, position))
-                level = (100 - position) / 100.0
+                level = position / 100.0
                 self._hmdevice.set_level(level, self._channel)
 
     @property
