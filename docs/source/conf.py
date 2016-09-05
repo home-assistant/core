5144547b709e0ce3ac9c720801f9d16e93439e7e
Robbie Trencheny <me@robbiet.us>
2016-09-05 02:25:03 -0700

    Try out the RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -124,7 +124,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
