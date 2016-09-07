6351ddf429b17c9c6df214da7374cfcd2ec75b35
Willems Davy <joyrider3774@hotmail.com>
2016-09-07 20:11:47 +0200

    pylint test fixes

--- homeassistant/components/sensor/emoncms.py
+++ homeassistant/components/sensor/emoncms.py
@@ -3,6 +3,7 @@ Support for monitoring emoncms feeds.
 
 https://emoncms.org
 """
+from datetime import timedelta
 import logging
 import requests
 import voluptuous as vol
@@ -16,7 +17,7 @@ from homeassistant.const import (
     CONF_UNIT_OF_MEASUREMENT, CONF_ID, CONF_SCAN_INTERVAL)
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers import template
-from datetime import timedelta
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -43,7 +44,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 })
 
-# entity attributes
 ATTR_SIZE = 'Size'
 ATTR_LASTUPDATETIME = 'LastUpdateTime'
 ATTR_TAG = 'Tag'
@@ -53,12 +53,13 @@ ATTR_FEEDNAME = 'FeedName'
 ATTR_LASTUPDATETIMESTR = 'LastUpdateTimeStr'
 
 
-def getId(SensorID, feedtag, feedname, feedid, feeduserid):
+def get_id(sensorid, feedtag, feedname, feedid, feeduserid):
     """Return unique identifier for feed / sensor."""
-    return "emoncms" + SensorID + "_" + feedtag + "_" + \
+    return "emoncms" + sensorid + "_" + feedtag + "_" + \
            feedname + "_" + feedid + "_" + feeduserid
 
 
+# pylint: disable=too-many-locals
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the Emoncms sensor."""
     apikey = config.get(CONF_API_KEY)
@@ -75,7 +76,6 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     data = EmonCmsData(hass, url, apikey, interval)
     data.update(dt_util.utcnow())
 
-    """fail setup_platform if no data was returned."""
     if data.data is None:
         return False
 
@@ -88,43 +88,30 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     for elem in data.data:
         if include_length == 0:
             if exclude_length == 0:
-                """Add All feeds."""
                 sensors.append(EmonCmsSensor(hass, data, None, value_template,
                                              unit_of_measurement, sensorid,
-                                             elem["id"], elem["name"],
-                                             elem["userid"], elem["tag"],
-                                             elem["size"], elem["time"],
-                                             elem["value"]))
+                                             elem))
             else:
-                """Add All feeds except ..."""
                 if not elem["id"] in exclude_feeds:
                     sensors.append(EmonCmsSensor(hass, data, None,
                                                  value_template,
                                                  unit_of_measurement,
                                                  sensorid,
-                                                 elem["id"], elem["name"],
-                                                 elem["userid"], elem["tag"],
-                                                 elem["size"], elem["time"],
-                                                 elem["value"]))
+                                                 elem))
 
         else:
             if exclude_length == 0:
-                """Only include these feed id's ..."""
                 if elem["id"] in include_feeds:
                     name = None
                     if include_names_length > 0:
                         if index < include_names_length:
                             name = include_feeds_names[index]
                             index = index + 1
-                    sensors.append(EmonCmsSensor(hass, data, name,
-                                                 value_template,
-                                                 unit_of_measurement,
-                                                 sensorid,
-                                                 elem["id"], elem["name"],
-                                                 elem["userid"], elem["tag"],
-                                                 elem["size"], elem["time"],
-                                                 elem["value"]))
-
+                sensors.append(EmonCmsSensor(hass, data, name,
+                                             value_template,
+                                             unit_of_measurement,
+                                             sensorid,
+                                             elem))
             else:
                 _LOGGER.error("Both config values '%s' and '%s' are " +
                               "specified this is not valid! " +
@@ -135,31 +122,33 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     add_devices(sensors)
 
 
+# pylint: disable=too-many-instance-attributes
 class EmonCmsSensor(Entity):
     """Implementation of an EmonCmsSensor sensor."""
 
+    # pylint: disable=too-many-arguments
     def __init__(self, hass, data, name, value_template,
-                 unit_of_measurement, sensorid,
-                 feedid, feedname, userid, tag, size,
-                 lastupdatetime, value):
+                 unit_of_measurement, sensorid, elem):
         """Initialize the sensor."""
         if name is None:
-            self._name = "emoncms" + sensorid + "_feedid_" + feedid
+            self._name = "emoncms" + sensorid + "_feedid_" + elem["id"]
         else:
             self._name = name
-        self._identifier = getId(sensorid, tag, feedname, feedid, userid)
+        self._identifier = get_id(sensorid, elem["tag"],
+                                  elem["name"], elem["id"],
+                                  elem["userid"])
         self._hass = hass
         self._data = data
-        self._feedid = feedid
-        self._userid = userid
-        self._tag = tag
-        self._feedname = feedname
         self._value_template = value_template
-        self._size = size
-        self._lastupdatetime = lastupdatetime
-        self._value = value
         self._unit_of_measurement = unit_of_measurement
         self._sensorid = sensorid
+        self._feedid = elem["id"]
+        self._userid = elem["userid"]
+        self._tag = elem["tag"]
+        self._feedname = elem["name"]
+        self._size = elem["size"]
+        self._lastupdatetime = elem["time"]
+        self._value = elem["value"]
         if self._value_template is not None:
             self._state = template.render_with_possible_json_value(
                 self._hass, self._value_template, self._value, "N/A")
@@ -203,9 +192,9 @@ class EmonCmsSensor(Entity):
         found = False
         if self._data.data is not None:
             for elem in self._data.data:
-                id = getId(self._sensorid, elem["tag"], elem["name"],
-                           elem["id"], elem["userid"])
-                if id == self._identifier:
+                elemid = get_id(self._sensorid, elem["tag"], elem["name"],
+                                elem["id"], elem["userid"])
+                if elemid == self._identifier:
                     found = True
                     self._lastupdatetime = elem["time"]
                     self._size = elem["size"]
@@ -221,6 +210,7 @@ class EmonCmsSensor(Entity):
             self.update_ha_state()
 
 
+# pylint: disable=too-few-public-methods
 class EmonCmsData(object):
     """The class for handling the data retrieval."""
 
@@ -236,28 +226,20 @@ class EmonCmsData(object):
         """Get the latest data."""
         try:
             try:
-                r = requests.get(self._url, params={"apikey": self._apikey},
-                                 verify=False, allow_redirects=True,
-                                 timeout=5)
-            except requests.exceptions.RequestException as e:
-                _LOGGER.error(e)
+                req = requests.get(self._url, params={"apikey": self._apikey},
+                                   verify=False, allow_redirects=True,
+                                   timeout=5)
+            except requests.exceptions.RequestException as exception:
+                _LOGGER.error(exception)
                 return
 
-            if r.status_code == 200:
-                self.data = r.json()
+            if req.status_code == 200:
+                self.data = req.json()
             else:
                 _LOGGER.error("please verify if the specified config value "
                               "'%s' is correct! (HTTP Status_code = %d)",
-                              CONF_URL, r.status_code)
+                              CONF_URL, req.status_code)
         finally:
-            """
-               during setup don't schedule new call, however
-               if setup was succesfull (there
-               should be already data availible) keep trying
-               since the connection could be temporary down
-               (server reboot for example at least in my use
-               case it can happen).
-            """
             if self.data is not None:
                 track_point_in_utc_time(self._hass,
                                         self.update,
