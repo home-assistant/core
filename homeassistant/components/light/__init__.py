b198d84493d5763c0be3f21fad5cfd6b254b4893
Marc Pabst <mail@marcpabst.de>
2016-09-12 11:36:04 +0200

    Update __init__.py

--- homeassistant/components/light/__init__.py
+++ homeassistant/components/light/__init__.py
@@ -125,7 +125,7 @@ def is_on(hass, entity_id=None):
 
 # pylint: disable=too-many-arguments
 def turn_on(hass, entity_id=None, transition=None, brightness=None,
-            rgb_color=None, xy_color=None, color_temp=None, white_value=None, 
+            rgb_color=None, xy_color=None, color_temp=None, white_value=None,
             profile=None, flash=None, effect=None, color_name=None):
     """Turn all or specified light on."""
     data = {
