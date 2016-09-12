c5e0347d0c36c1f57c07585dd78326a93aea5bcb
Ben Bangert <ben@groovie.org>
2016-09-12 12:56:18 -0700

    Fix version tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,34 +8,34 @@ from homeassistant import __main__ as main
 def test_validate_python(mock_exit):
     """Test validate Python version method."""
     with patch('sys.version_info',
-               new_callable=PropertyMock(return_value=[2, 7, 8])):
+               new_callable=PropertyMock(return_value=(2, 7, 8))):
         main.validate_python()
         assert mock_exit.called is True
 
     mock_exit.reset_mock()
 
     with patch('sys.version_info',
-               new_callable=PropertyMock(return_value=[3, 2, 0])):
+               new_callable=PropertyMock(return_value=(3, 2, 0))):
         main.validate_python()
         assert mock_exit.called is True
 
     mock_exit.reset_mock()
 
     with patch('sys.version_info',
-               new_callable=PropertyMock(return_value=[3, 4, 1])):
+               new_callable=PropertyMock(return_value=(3, 4, 1))):
         main.validate_python()
         assert mock_exit.called is True
 
     mock_exit.reset_mock()
 
     with patch('sys.version_info',
-               new_callable=PropertyMock(return_value=[3, 4, 2])):
+               new_callable=PropertyMock(return_value=(3, 4, 2))):
         main.validate_python()
         assert mock_exit.called is False
 
     mock_exit.reset_mock()
 
     with patch('sys.version_info',
-               new_callable=PropertyMock(return_value=[3, 5, 1])):
+               new_callable=PropertyMock(return_value=(3, 5, 1))):
         main.validate_python()
         assert mock_exit.called is False
