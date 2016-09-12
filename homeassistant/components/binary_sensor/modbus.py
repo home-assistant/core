ca0b0ad121e400e35b57a0237f69fa3cad4df067
Per Sandström <per.j.sandstrom@gmail.com>
2016-09-12 22:28:19 +0200

    abbf53cde7c4481f2f78683ca6d7f5676d4263c2

--- homeassistant/components/binary_sensor/modbus.py
+++ homeassistant/components/binary_sensor/modbus.py
@@ -1,15 +1,15 @@
 """
-Support for Modbus switches.
+Support for Modbus Coil sensors.
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/switch.modbus/
+https://home-assistant.io/components/binary_sensor.modbus/
 """
 import logging
 import voluptuous as vol
 
 import homeassistant.components.modbus as modbus
 from homeassistant.const import CONF_NAME
-from homeassistant.helpers.entity import ToggleEntity
+from homeassistant.components.binary_sensor import BinarySensorDevice
 from homeassistant.helpers import config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 
@@ -24,52 +24,38 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_COILS): [{
         vol.Required(CONF_COIL): cv.positive_int,
         vol.Required(CONF_NAME): cv.string,
-        vol.Optional(CONF_SLAVE): cv.positive_int,
+        vol.Optional(CONF_SLAVE): cv.positive_int
     }]
 })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Read configuration and create Modbus devices."""
-    switches = []
-    for coil in config.get("coils"):
-        switches.append(ModbusCoilSwitch(
+    """Setup Modbus binary sensors."""
+    sensors = []
+    for coil in config.get(CONF_COILS):
+        sensors.append(ModbusCoilSensor(
             coil.get(CONF_NAME),
             coil.get(CONF_SLAVE),
             coil.get(CONF_COIL)))
-    add_devices(switches)
+    add_devices(sensors)
 
 
-class ModbusCoilSwitch(ToggleEntity):
-    """Representation of a Modbus switch."""
+class ModbusCoilSensor(BinarySensorDevice):
+    """Modbus coil sensor."""
 
-    # pylint: disable=too-many-arguments
     def __init__(self, name, slave, coil):
-        """Initialize the switch."""
+        """Initialize the modbus coil sensor."""
         self._name = name
         self._slave = int(slave) if slave else None
         self._coil = int(coil)
-        self._is_on = None
+        self._value = None
 
     @property
     def is_on(self):
-        """Return true if switch is on."""
-        return self._is_on
-
-    @property
-    def name(self):
-        """Return the name of the switch."""
-        return self._name
-
-    def turn_on(self, **kwargs):
-        """Set switch on."""
-        modbus.HUB.write_coil(self._slave, self._coil, True)
-
-    def turn_off(self, **kwargs):
-        """Set switch off."""
-        modbus.HUB.write_coil(self._slave, self._coil, False)
+        """Return the state of the sensor."""
+        return self._value
 
     def update(self):
-        """Update the state of the switch."""
+        """Update the state of the sensor."""
         result = modbus.HUB.read_coils(self._slave, self._coil, 1)
-        self._is_on = bool(result.bits[0])
+        self._value = result.bits[0]
