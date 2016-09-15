58e9075a587bdd991c7263de0e751e080b8bcfa7
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-09-15 14:35:40 +0200

    Split ffmpeg to compoment (#3396)
    Add an optional extended description…

--- homeassistant/components/camera/ffmpeg.py
+++ homeassistant/components/camera/ffmpeg.py
@@ -9,42 +9,28 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components.camera import (Camera, PLATFORM_SCHEMA)
+from homeassistant.components.ffmpeg import (
+    run_test, get_binary, CONF_INPUT, CONF_EXTRA_ARGUMENTS)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import CONF_NAME
 
-REQUIREMENTS = ['ha-ffmpeg==0.12']
+DEPENDENCIES = ['ffmpeg']
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_INPUT = 'input'
-CONF_FFMPEG_BIN = 'ffmpeg_bin'
-CONF_EXTRA_ARGUMENTS = 'extra_arguments'
-CONF_RUN_TEST = 'run_test'
-
-DEFAULT_BINARY = 'ffmpeg'
 DEFAULT_NAME = 'FFmpeg'
-DEFAULT_RUN_TEST = True
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_INPUT): cv.string,
     vol.Optional(CONF_EXTRA_ARGUMENTS): cv.string,
-    vol.Optional(CONF_FFMPEG_BIN, default=DEFAULT_BINARY): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_RUN_TEST, default=DEFAULT_RUN_TEST): cv.boolean,
 })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup a FFmpeg Camera."""
-    from haffmpeg import Test
-
-    # run Test
-    if config.get(CONF_RUN_TEST):
-        test = Test(config.get(CONF_FFMPEG_BIN))
-        if not test.run_test(config.get(CONF_INPUT)):
-            _LOGGER.error("FFmpeg '%s' test fails!", config.get(CONF_INPUT))
-            return
-
+    if not run_test(config.get(CONF_INPUT)):
+        return
     add_devices([FFmpegCamera(config)])
 
 
@@ -57,12 +43,11 @@ class FFmpegCamera(Camera):
         self._name = config.get(CONF_NAME)
         self._input = config.get(CONF_INPUT)
         self._extra_arguments = config.get(CONF_EXTRA_ARGUMENTS)
-        self._ffmpeg_bin = config.get(CONF_FFMPEG_BIN)
 
     def camera_image(self):
         """Return a still image response from the camera."""
         from haffmpeg import ImageSingle, IMAGE_JPEG
-        ffmpeg = ImageSingle(self._ffmpeg_bin)
+        ffmpeg = ImageSingle(get_binary())
 
         return ffmpeg.get_image(self._input, output_format=IMAGE_JPEG,
                                 extra_cmd=self._extra_arguments)
@@ -71,7 +56,7 @@ class FFmpegCamera(Camera):
         """Generate an HTTP MJPEG stream from the camera."""
         from haffmpeg import CameraMjpeg
 
-        stream = CameraMjpeg(self._ffmpeg_bin)
+        stream = CameraMjpeg(get_binary())
         stream.open_camera(self._input, extra_cmd=self._extra_arguments)
         return response(
             stream,
