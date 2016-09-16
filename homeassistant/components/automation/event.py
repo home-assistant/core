9620a7515c9af706b5dcb6225a7c7e97afde8bf0
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-09-15 22:48:55 -0700

    Automation - Event: Use coroutine

--- homeassistant/components/automation/event.py
+++ homeassistant/components/automation/event.py
@@ -4,6 +4,7 @@ Offer event listening automation rules.
 For more details about this automation rule, please refer to the documentation
 at https://home-assistant.io/components/automation/#event-trigger
 """
+import asyncio
 import logging
 
 import voluptuous as vol
@@ -28,15 +29,16 @@ def trigger(hass, config, action):
     event_type = config.get(CONF_EVENT_TYPE)
     event_data = config.get(CONF_EVENT_DATA)
 
+    @asyncio.coroutine
     def handle_event(event):
         """Listen for events and calls the action when data matches."""
         if not event_data or all(val == event.data.get(key) for key, val
                                  in event_data.items()):
-            action({
-                'trigger': {
-                    'platform': 'event',
-                    'event': event,
-                },
-            })
+            hass.add_job(action, {
+                    'trigger': {
+                        'platform': 'event',
+                        'event': event,
+                    },
+                })
 
     return hass.bus.listen(event_type, handle_event)
