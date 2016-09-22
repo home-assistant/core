a05babc0a705a205e9e57cc61af2af352a698e5f
Aaron Irving <william.irving2@ngc.com>
2016-09-21 21:23:52 -0400

    Added check for channel in kodi media player to play channel from PVR.

--- homeassistant/components/media_player/kodi.py
+++ homeassistant/components/media_player/kodi.py
@@ -316,4 +316,7 @@ class KodiDevice(MediaPlayerDevice):
 
     def play_media(self, media_type, media_id, **kwargs):
         """Send the play_media command to the media player."""
-        self._server.Player.Open({"item": {"file": str(media_id)}})
+        if media_type == "CHANNEL":
+            self._server.Player.Open({"item": {"channelid": int(media_id)}})
+        else:
+            self._server.Player.Open({"item": {"file": str(media_id)}})
