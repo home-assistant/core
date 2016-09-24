5ff2e9b4943fd40679b825b558b27e0b1d0c8c02
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-09-24 00:03:44 -0700

    Filter out falsey platform configs

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -111,14 +111,6 @@ class TestBootstrap:
             'platform_conf.whatever', MockPlatform('whatever'))
 
         assert not bootstrap._setup_component(self.hass, 'platform_conf', {
-            'platform_conf': None
-        })
-
-        assert not bootstrap._setup_component(self.hass, 'platform_conf', {
-            'platform_conf': {}
-        })
-
-        assert not bootstrap._setup_component(self.hass, 'platform_conf', {
             'platform_conf': {
                 'hello': 'world',
                 'invalid': 'extra',
@@ -150,11 +142,26 @@ class TestBootstrap:
             }
         })
 
+        self.hass.config.components.remove('platform_conf')
+
         assert bootstrap._setup_component(self.hass, 'platform_conf', {
             'platform_conf': [{
                 'platform': 'whatever',
                 'hello': 'world',
             }]
+        })
+
+        self.hass.config.components.remove('platform_conf')
+
+        # Any falsey paltform config will be ignored (None, {}, etc)
+        assert bootstrap._setup_component(self.hass, 'platform_conf', {
+            'platform_conf': None
+        })
+
+        self.hass.config.components.remove('platform_conf')
+
+        assert bootstrap._setup_component(self.hass, 'platform_conf', {
+            'platform_conf': {}
         })
 
     def test_component_not_found(self):
