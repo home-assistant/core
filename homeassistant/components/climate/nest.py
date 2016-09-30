573ba028d8ed11a2f228ca4b21cd7d88b7d0a919
Jeffery Wilson <jeff@jeffalwilson.com>
2016-09-30 01:46:29 -0400

    Make Nest state reflect current operation not current operation mode

--- homeassistant/components/climate/nest.py
+++ homeassistant/components/climate/nest.py
@@ -63,6 +63,15 @@ class NestThermostat(ClimateDevice):
         return TEMP_CELSIUS
 
     @property
+    def state(self):
+        if self.device.hvac_ac_state:
+            return STATE_COOL
+        elif self.device.hvac_heater_state:
+            return STATE_HEAT
+        else:
+            return STATE_IDLE
+
+    @property
     def device_state_attributes(self):
         """Return the device specific state attributes."""
         # Move these to Thermostat Device and make them global
