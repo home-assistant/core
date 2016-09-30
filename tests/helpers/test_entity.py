0b41d873ae603a2998ede375e49fcfddf73998e8
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-09-30 00:12:05 -0700

    Clean up helpers/entity.py tests

--- tests/helpers/test_entity.py
+++ tests/helpers/test_entity.py
@@ -11,9 +11,54 @@ from homeassistant.const import ATTR_HIDDEN
 from tests.common import get_test_home_assistant
 
 
-def require_hass(method):
-    method.__test_hass = True
-    return method
+def test_generate_entity_id_requires_hass_or_ids():
+    """Ensure we require at least hass or current ids."""
+    fmt = 'test.{}'
+    with pytest.raises(ValueError):
+        entity.generate_entity_id(fmt, 'hello world')
+
+
+def test_generate_entity_id_given_keys():
+    """Test generating an entity id given current ids."""
+    fmt = 'test.{}'
+    assert entity.generate_entity_id(
+        fmt, 'overwrite hidden true', current_ids=[
+            'test.overwrite_hidden_true']) == 'test.overwrite_hidden_true_2'
+    assert entity.generate_entity_id(
+        fmt, 'overwrite hidden true', current_ids=[
+            'test.another_entity']) == 'test.overwrite_hidden_true'
+
+
+def test_async_update_support(event_loop):
+    """Test async update getting called."""
+    sync_update = []
+    async_update = []
+
+    class AsyncEntity(entity.Entity):
+        hass = MagicMock()
+        entity_id = 'sensor.test'
+
+        def update(self):
+            sync_update.append([1])
+
+    ent = AsyncEntity()
+    ent.hass.loop = event_loop
+
+    @asyncio.coroutine
+    def test():
+        yield from ent.async_update_ha_state(True)
+
+    event_loop.run_until_complete(test())
+
+    assert len(sync_update) == 1
+    assert len(async_update) == 0
+
+    ent.async_update = lambda: async_update.append(1)
+
+    event_loop.run_until_complete(test())
+
+    assert len(sync_update) == 1
+    assert len(async_update) == 1
 
 
 class TestHelpersEntity(object):
@@ -21,9 +66,6 @@ class TestHelpersEntity(object):
 
     def setup_method(self, method):
         """Setup things to be run when tests are started."""
-        if not hasattr(method, '__test_hass'):
-            return
-
         self.entity = entity.Entity()
         self.entity.entity_id = 'test.overwrite_hidden_true'
         self.hass = self.entity.hass = get_test_home_assistant()
@@ -32,19 +74,13 @@ class TestHelpersEntity(object):
     def teardown_method(self, method):
         """Stop everything that was started."""
         entity.set_customize({})
-
-        if not hasattr(method, '__test_hass'):
-            return
-
         self.hass.stop()
 
-    @require_hass
     def test_default_hidden_not_in_attributes(self):
         """Test that the default hidden property is set to False."""
         assert ATTR_HIDDEN not in self.hass.states.get(
             self.entity.entity_id).attributes
 
-    @require_hass
     def test_overwriting_hidden_property_to_true(self):
         """Test we can overwrite hidden property to True."""
         entity.set_customize({self.entity.entity_id: {ATTR_HIDDEN: True}})
@@ -53,59 +89,9 @@ class TestHelpersEntity(object):
         state = self.hass.states.get(self.entity.entity_id)
         assert state.attributes.get(ATTR_HIDDEN)
 
-    def test_generate_entity_id_requires_hass_or_ids(self):
-        """Ensure we require at least hass or current ids."""
-        fmt = 'test.{}'
-        with pytest.raises(ValueError):
-            entity.generate_entity_id(fmt, 'hello world')
-
-    @require_hass
     def test_generate_entity_id_given_hass(self):
         """Test generating an entity id given hass object."""
         fmt = 'test.{}'
-        assert 'test.overwrite_hidden_true_2' == \
-               entity.generate_entity_id(fmt, 'overwrite hidden true',
-                                         hass=self.hass)
-
-    def test_generate_entity_id_given_keys(self):
-        """Test generating an entity id given current ids."""
-        fmt = 'test.{}'
-        assert 'test.overwrite_hidden_true_2' == \
-               entity.generate_entity_id(
-                    fmt, 'overwrite hidden true',
-                    current_ids=['test.overwrite_hidden_true'])
-        assert 'test.overwrite_hidden_true' == \
-               entity.generate_entity_id(fmt, 'overwrite hidden true',
-                                         current_ids=['test.another_entity'])
-
-    def test_async_update_support(self, event_loop):
-        """Test async update getting called."""
-
-        sync_update = []
-        async_update = []
-
-        class AsyncEntity(entity.Entity):
-            hass = MagicMock()
-            entity_id = 'sensor.test'
-
-            def update(self):
-                sync_update.append([1])
-
-        ent = AsyncEntity()
-        ent.hass.loop = event_loop
-
-        @asyncio.coroutine
-        def test():
-            yield from ent.async_update_ha_state(True)
-
-        event_loop.run_until_complete(test())
-
-        assert len(sync_update) == 1
-        assert len(async_update) == 0
-
-        ent.async_update = lambda: async_update.append(1)
-
-        event_loop.run_until_complete(test())
-
-        assert len(sync_update) == 1
-        assert len(async_update) == 1
+        assert entity.generate_entity_id(
+            fmt, 'overwrite hidden true',
+            hass=self.hass) == 'test.overwrite_hidden_true_2'
