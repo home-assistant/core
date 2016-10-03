3cdcc11554417194bb567bba505c233bc6dc9f06
Daniel Høyer Iversen <mail@dahoiv.net>
2016-10-03 22:21:03 +0200

    farcy fix

--- homeassistant/components/input_boolean.py
+++ homeassistant/components/input_boolean.py
@@ -13,7 +13,8 @@ from homeassistant.const import (
     SERVICE_TOGGLE, STATE_ON)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import ToggleEntity
-from homeassistant.helpers.entity_component import EntityComponent, generate_entity_id
+from homeassistant.helpers.entity_component import (EntityComponent,
+                                                    generate_entity_id)
 from homeassistant.helpers import extract_domain_configs
 
 DOMAIN = 'input_boolean'
@@ -70,8 +71,9 @@ def setup(hass, config):
             name = cfg.get(CONF_NAME)
             state = cfg.get(CONF_INITIAL, False)
             icon = cfg.get(CONF_ICON)
+            entity_id = generate_entity_id(ENTITY_ID_FORMAT, object_id, hass)
 
-            entities.append(InputBoolean(object_id, name, state, icon))
+            entities.append(InputBoolean(entity_id, name, state, icon))
 
     if not entities:
         return False
@@ -103,9 +105,9 @@ def setup(hass, config):
 class InputBoolean(ToggleEntity):
     """Representation of a boolean input."""
 
-    def __init__(self, object_id, name, state, icon):
+    def __init__(self, entity_id, name, state, icon):
         """Initialize a boolean input."""
-        self.entity_id = generate_entity_id(ENTITY_ID_FORMAT, object_id)
+        self.entity_id = entity_id
         self._name = name
         self._state = state
         self._icon = icon
