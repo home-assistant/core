22c2f7547e4351c0ccf27bdcfc6fd814218ba593
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-10-03 22:39:27 -0700

    Address asyncio comments (#3663)
    * Template platforms: create_task instead of yield from
    
    * Automation: less yielding, more create_tasking
    
    * Helpers.script: less yielding, more create_tasking
    
    * Deflake logbook test
    
    * Deflake automation reload config test
    
    * MQTT: Use async_add_job and threaded_listener_factory
    
    * Deflake other logbook test
    
    * lint
    
    * Add test for automation trigger service
    
    * MQTT client can be called from within async

--- homeassistant/components/switch/template.py
+++ homeassistant/components/switch/template.py
@@ -91,7 +91,7 @@ class SwitchTemplate(SwitchDevice):
         @asyncio.coroutine
         def template_switch_state_listener(entity, old_state, new_state):
             """Called when the target device changes state."""
-            yield from self.async_update_ha_state(True)
+            hass.loop.create_task(self.async_update_ha_state(True))
 
         track_state_change(hass, entity_ids, template_switch_state_listener)
 
