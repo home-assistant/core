a4106b85105d32e873c3a0abf5c729fd7aabc47b
Willems Davy <joyrider3774@hotmail.com>
2016-10-05 22:42:33 +0200

    fix a pylint error that happend on the py35 tests

--- homeassistant/components/sensor/haveibeenpwned.py
+++ homeassistant/components/sensor/haveibeenpwned.py
@@ -34,7 +34,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the RESTful sensor."""
-
     emails = config.get(CONF_EMAIL)
     data = HaveIBeenPwnedData(emails)
 
