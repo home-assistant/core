a9988367dbbe0b9731e35d9d283c6099e50271a1
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2016-10-12 01:59:54 -0400

    Fixed Fitbit restingHeartRate field to grab the correct field from dictionary
    In [31]: r['activities-heart'][-1].get('value')
    Out[31]:
    {'customHeartRateZones': [],
     'heartRateZones': [{'caloriesOut': 126.18348,
       'max': 94,
       'min': 30,
       'minutes': 67,
       'name': 'Out of Range'},
      {'caloriesOut': 27.21339,
       'max': 131,
       'min': 94,
       'minutes': 5,
       'name': 'Fat Burn'},
      {'caloriesOut': 0, 'max': 159, 'min': 131, 'minutes': 0, 'name': 'Cardio'},
      {'caloriesOut': 0, 'max': 220, 'min': 159, 'minutes': 0, 'name': 'Peak'}],
     'restingHeartRate': 69}
    
    In [32]: r['activities-heart'][-1].get('value').get('restingHeartRate')
    Out[32]: 69

--- homeassistant/components/sensor/fitbit.py
+++ homeassistant/components/sensor/fitbit.py
@@ -403,7 +403,7 @@ class FitbitSensor(Entity):
         response = self.client.time_series(self.resource_type, period='7d')
         self._state = response[container][-1].get('value')
         if self.resource_type == 'activities/heart':
-            self._state = response[container][-1].get('restingHeartRate')
+            self._state = response[container][-1].get('value').get('restingHeartRate')
         config_contents = {
             ATTR_ACCESS_TOKEN: self.client.client.token['access_token'],
             ATTR_REFRESH_TOKEN: self.client.client.token['refresh_token'],
