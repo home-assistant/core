d6d34bdb51c44f350e0e6677b9730f0241b90dfa
Georgi Kirichkov <kirichkov@users.noreply.github.com>
2016-10-15 07:10:04 +0300

    Relaxes the configuration options for influxdb (#3869)
    * Relaxes the configuration options for influxdb
    
    By default influxdb allows unauthenticated access
    Home Assistant required at least username and password to be present to properly submit data to influxdb
    
    * Removes unused import of 'copy'
    
    The copy module was used only in the removed test case responsible for testing the missing keys
    
    * Updates InfluxDB config schema to require user and password
    
    Current InfluxDB (v 1.0) can work without any authentication, but when authentication is enabled both username and password should be set.
    
    * Removes extra white space in test_influxdb.py

--- tests/components/test_influxdb.py
+++ tests/components/test_influxdb.py
@@ -1,5 +1,4 @@
 """The tests for the InfluxDB component."""
-import copy
 import unittest
 from unittest import mock
 
@@ -53,18 +52,23 @@ class TestInfluxDB(unittest.TestCase):
         self.assertEqual(EVENT_STATE_CHANGED,
                          self.hass.bus.listen.call_args_list[0][0][0])
 
-    def test_setup_missing_keys(self, mock_client):
-        """Test the setup with missing keys."""
+    def test_setup_minimal_config(self, mock_client):
+        """Tests the setup with minimal configuration."""
+        config = {
+            'influxdb': {}
+        }
+
+        assert setup_component(self.hass, influxdb.DOMAIN, config)
+
+    def test_setup_missing_password(self, mock_client):
+        """Test the setup with existing username and missing password."""
         config = {
             'influxdb': {
-                'username': 'user',
-                'password': 'pass',
+                'username': 'user'
             }
         }
-        for missing in config['influxdb'].keys():
-            config_copy = copy.deepcopy(config)
-            del config_copy['influxdb'][missing]
-            assert not setup_component(self.hass, influxdb.DOMAIN, config_copy)
+
+        assert not setup_component(self.hass, influxdb.DOMAIN, config)
 
     def test_setup_query_fail(self, mock_client):
         """Test the setup for query failures."""
