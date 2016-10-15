0f1c604112f3532e01a74b8fd9a92817a0472839
Willems Davy <joyrider3774@hotmail.com>
2016-10-15 16:14:11 +0200

    - switch to track_point_in_time to send all data at foxed interval - don't use json_dump - switch to http post instead of http get

--- homeassistant/components/emoncms_history.py
+++ homeassistant/components/emoncms_history.py
@@ -5,7 +5,7 @@ For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/emoncms_history/
 """
 import logging
-import json
+from datetime import timedelta
 
 import voluptuous as vol
 import requests
@@ -13,10 +13,12 @@ import requests
 from homeassistant.const import (
     CONF_API_KEY, CONF_WHITELIST,
     CONF_URL, STATE_UNKNOWN,
-    STATE_UNAVAILABLE)
+    STATE_UNAVAILABLE,
+    CONF_SCAN_INTERVAL)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers import state as state_helper
-from homeassistant.helpers.event import track_state_change
+from homeassistant.helpers.event import track_point_in_time
+from homeassistant.util import dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -29,6 +31,7 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(CONF_URL): cv.string,
         vol.Required(CONF_INPUTNODE): cv.positive_int,
         vol.Required(CONF_WHITELIST): cv.entity_ids,
+        vol.Optional(CONF_SCAN_INTERVAL, default=30): cv.positive_int,
     }),
 }, extra=vol.ALLOW_EXTRA)
 
@@ -37,52 +40,56 @@ def setup(hass, config):
     """Setup the emoncms_history component."""
     conf = config[DOMAIN]
     whitelist = conf.get(CONF_WHITELIST)
-    # defined here so that previous values that did not change
-    # are send as well, otherwise emoncms will list the inputs
-    # as inactive and possibly send emails that feeds based
-    # on the input value have not changed (if the user enabled
-    # that with the extra, optional scripts)
-    json_body = {}
 
-    def _state_changed(entity_id, old_state, new_state):
-        """Send new states to emoncms."""
-        if new_state is None or new_state in (STATE_UNKNOWN, "",
-                                              STATE_UNAVAILABLE):
-            return
-
+    def send_data(url, apikey, node, payload):
+        """Send payload data to emoncms."""
         try:
-            _state = state_helper.state_as_number(new_state)
-        except ValueError:
-            return
+            fullurl = "{}/input/post.json".format(url)
+            req = requests.post(fullurl,
+                                params={"node": node},
+                                data={"apikey": apikey,
+                                      "data": payload},
+                                allow_redirects=True,
+                                timeout=5)
 
-        json_body[entity_id] = _state
-        json_str = json.dumps(json_body, separators=(",", ":"))
-        json_str = json_str.replace("\"", "")
-        json_str = json_str.replace("{", "").replace("}", "")
-        json_str = "{{{}}}".format(json_str)
+        except requests.exceptions.RequestException:
+            _LOGGER.error("Error saving data '%s' to '%s'",
+                          payload, fullurl)
 
-        send_data(conf.get(CONF_URL), conf.get(CONF_API_KEY),
-                  str(conf.get(CONF_INPUTNODE)), json_str)
+        else:
+            if req.status_code != 200:
+                _LOGGER.error("Error saving data '%s' to '%s'" +
+                              "(http status code = %d)", payload,
+                              fullurl, req.status_code)
 
-    track_state_change(hass, whitelist, _state_changed)
+    def update_emoncms(time):
+        """Send whitelisted entities states reguarly to emoncms."""
+        payload_dict = {}
 
+        for entity_id in whitelist:
+            state = hass.states.get(entity_id)
+
+            if state is None or state.state in (
+                    STATE_UNKNOWN, "", STATE_UNAVAILABLE):
+                continue
+
+            try:
+                payload_dict[entity_id] = state_helper.state_as_number(
+                    state)
+            except ValueError:
+                continue
+
+        if len(payload_dict) > 0:
+            payload = "{%s}" % ",".join("{}:{}".format(key, val)
+                                        for key, val in
+                                        payload_dict.items())
+
+            send_data(conf.get(CONF_URL), conf.get(CONF_API_KEY),
+                      str(conf.get(CONF_INPUTNODE)), payload)
+
+        track_point_in_time(hass, update_emoncms, time +
+                            timedelta(seconds=conf.get(
+                                CONF_SCAN_INTERVAL)))
+
+    update_emoncms(dt_util.utcnow())
     return True
-
-
-def send_data(url, apikey, node, jsonstr):
-    """Send the collected data to emoncms."""
-    try:
-        fullurl = "{}/input/post.json".format(url)
-        req = requests.get(fullurl, params={"apikey": apikey,
-                                            "node": node,
-                                            "json": jsonstr},
-                           allow_redirects=True, timeout=5)
-
-    except requests.exceptions.RequestException:
-        _LOGGER.error("Error saving data '%s' to '%s'", json, fullurl)
-
-    else:
-        if req.status_code != 200:
-            _LOGGER.error("Error saving data '%s' to '%s'" +
-                          "(http status code = %d)", json, fullurl,
-                          req.status_code)
