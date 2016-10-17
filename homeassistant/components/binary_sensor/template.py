b70bd57d452c83d2f5e9810ee68242ec582e6781
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-10-17 07:00:55 +0200

    Async template (#3909)
    * port binary_sensor/template
    
    * port sensor/template
    
    * port switch/template
    
    * fix unittest
    
    * fix
    
    * use task instead yield on it
    
    * fix unittest
    
    * fix unittest v2
    
    * fix invalid config
    
    * fix lint
    
    * fix unuset import

--- homeassistant/components/binary_sensor/template.py
+++ homeassistant/components/binary_sensor/template.py
@@ -17,8 +17,8 @@ from homeassistant.const import (
     ATTR_FRIENDLY_NAME, ATTR_ENTITY_ID, CONF_VALUE_TEMPLATE,
     CONF_SENSOR_CLASS, CONF_SENSORS)
 from homeassistant.exceptions import TemplateError
-from homeassistant.helpers.entity import generate_entity_id
-from homeassistant.helpers.event import track_state_change
+from homeassistant.helpers.entity import async_generate_entity_id
+from homeassistant.helpers.event import async_track_state_change
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
@@ -35,7 +35,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def async_setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup template binary sensors."""
     sensors = []
 
@@ -61,8 +61,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     if not sensors:
         _LOGGER.error('No sensors added')
         return False
-    add_devices(sensors)
 
+    hass.loop.create_task(add_devices(sensors))
     return True
 
 
@@ -74,21 +74,22 @@ class BinarySensorTemplate(BinarySensorDevice):
                  value_template, entity_ids):
         """Initialize the Template binary sensor."""
         self.hass = hass
-        self.entity_id = generate_entity_id(ENTITY_ID_FORMAT, device,
-                                            hass=hass)
+        self.entity_id = async_generate_entity_id(ENTITY_ID_FORMAT, device,
+                                                  hass=hass)
         self._name = friendly_name
         self._sensor_class = sensor_class
         self._template = value_template
         self._state = None
 
-        self.update()
+        self._async_render()
 
         @callback
         def template_bsensor_state_listener(entity, old_state, new_state):
             """Called when the target device changes state."""
             hass.loop.create_task(self.async_update_ha_state(True))
 
-        track_state_change(hass, entity_ids, template_bsensor_state_listener)
+        async_track_state_change(
+            hass, entity_ids, template_bsensor_state_listener)
 
     @property
     def name(self):
@@ -112,7 +113,11 @@ class BinarySensorTemplate(BinarySensorDevice):
 
     @asyncio.coroutine
     def async_update(self):
-        """Get the latest data and update the state."""
+        """Update the state from the template."""
+        self._async_render()
+
+    def _async_render(self):
+        """Render the state from the template."""
         try:
             self._state = self._template.async_render().lower() == 'true'
         except TemplateError as ex:
