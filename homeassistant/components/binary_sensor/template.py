b81385539a1b70f1fd8ff854c18fc1a00c65e1f4
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-10-18 07:27:32 +0200

    Bugfix Template sensors (#3931)

--- homeassistant/components/binary_sensor/template.py
+++ homeassistant/components/binary_sensor/template.py
@@ -35,6 +35,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
+@asyncio.coroutine
 def async_setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup template binary sensors."""
     sensors = []
