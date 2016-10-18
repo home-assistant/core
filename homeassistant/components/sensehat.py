acf89a17f6dc9e43dd80475b611e5f8fdfa90a53
Farmin Farzin <farmin.f@gmail.com>
2016-10-18 16:10:57 +0200

    fixes for PR

--- homeassistant/components/sensehat.py
+++ homeassistant/components/sensehat.py
@@ -1,6 +1,8 @@
 """
-A component which show the value of temperature, humidity and pressure from Sense HAT board in the form of service.
-Interval time between each reading is a minute by default but it is configurable from configuration file.
+A component which show the value of temperature, humidity and pressure 
+from Sense HAT board in the form of service.
+Interval time between each reading is a minute by default
+but it is configurable from configuration file.
 
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/
@@ -11,52 +13,51 @@ import os
 import time
 
 
-# The domain of your component. Should be equal to the name of your component.
+""" The domain of your component. Should be equal to the name of your component."""
 DOMAIN = "sensehat"
 
 
-# get CPU temperature
 def get_cpu_temp():
-  res = os.popen("vcgencmd measure_temp").readline()
-  t = float(res.replace("temp=","").replace("'C\n",""))
-  return(t)
+   """ get CPU temperature """
+   res = os.popen("vcgencmd measure_temp").readline()
+   t = float(res.replace("temp=", "").replace("'C\n", ""))
+   return(t)
 
-# use moving average to get better readings
+
 def get_average(x):
-  if not hasattr(get_average, "t"):
-    get_average.t = [x,x,x]
-  get_average.t[2] = get_average.t[1]
-  get_average.t[1] = get_average.t[0]
-  get_average.t[0] = x
-  xs = (get_average.t[0]+get_average.t[1]+get_average.t[2])/3
-  return(xs)
+   """ use moving average to get better readings """
+   if not hasattr(get_average, "t"):
+      get_average.t = [x, x, x]
+   get_average.t[2] = get_average.t[1]
+   get_average.t[1] = get_average.t[0]
+   get_average.t[0] = x
+   xs = (get_average.t[0]+get_average.t[1]+get_average.t[2])/3
+   return(xs)
 
 sense = SenseHat()
-
 INTERVAL_DEFAULT = 60
 
+
 def setup(hass, config):
-    
-    def get_temp(call):
-       while True:
-          t1 = sense.get_temperature_from_humidity()
-          t2 = sense.get_temperature_from_pressure()
-          t_cpu = get_cpu_temp()
-          t = (t1+t2)/2
-          temperature_value = t - ((t_cpu-t)/1.5)
-          temperature_value = get_average(temperature_value)
-          humidity_value = sense.get_humidity()
-          pressure_value = sense.get_pressure()          
-          
-          hass.states.set('sensehat.temperature',round(temperature_value,2))
-          hass.states.set('sensehat.humidity',  round(humidity_value,2))
-          hass.states.set('sensehat.pressure', round(pressure_value,2))
+   """ main setup function """
+   def get_temp(call):
+      while True:
+         t1 = sense.get_temperature_from_humidity()
+         t2 = sense.get_temperature_from_pressure()
+         t_cpu = get_cpu_temp()
+         t = (t1+t2)/2
+         temperature_value = t - ((t_cpu-t)/1.5)
+         temperature_value = get_average(temperature_value)
+         humidity_value = sense.get_humidity()
+         pressure_value = sense.get_pressure()
+         hass.states.set('sensehat.temperature', round(temperature_value, 2))
+         hass.states.set('sensehat.humidity',  round(humidity_value, 2))
+         hass.states.set('sensehat.pressure', round(pressure_value, 2))
+         interval = config[DOMAIN].get('interval', INTERVAL_DEFAULT)
+         time.sleep(interval)
 
-          interval = config[DOMAIN].get('interval', INTERVAL_DEFAULT)
-          time.sleep(interval)
+   # Register our service with Home Assistant.
+   hass.services.register(DOMAIN, 'sensehat_temp', get_temp)
 
-    # Register our service with Home Assistant.
-    hass.services.register(DOMAIN, 'sensehat_temp', get_temp)
-
-    # Return boolean to indicate that initialization was successfully.
-    return True
+   # Return boolean to indicate that initialization was successfully.
+   return True
--- homeassistant/components/sensor/sensehat.py
+++ homeassistant/components/sensor/sensehat.py
@@ -1,40 +1,42 @@
 """
-A component which show the value of temperature, humidity and pressure from Sense HAT board in the form 
-of platform with graphs in history.
+A component which show the value of temperature, humidity and pressure 
+from Sense HAT board in the form of platform with graphs in history.
 
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/
 """
+
 from homeassistant.const import TEMP_CELSIUS
 from homeassistant.helpers.entity import Entity
 from sense_hat import SenseHat
 import os
 
-
-# get CPU temperature
 def get_cpu_temp():
-  res = os.popen("vcgencmd measure_temp").readline()
-  t = float(res.replace("temp=","").replace("'C\n",""))
-  return(t)
+    """ get CPU temperature """
+    res = os.popen("vcgencmd measure_temp").readline()
+    t = float(res.replace("temp=", "").replace("'C\n", ""))
+    return(t)
 
-# use moving average to get better readings
+
 def get_average(x):
-  if not hasattr(get_average, "t"):
-    get_average.t = [x,x,x]
-  get_average.t[2] = get_average.t[1]
-  get_average.t[1] = get_average.t[0]
-  get_average.t[0] = x
-  xs = (get_average.t[0]+get_average.t[1]+get_average.t[2])/3
-  return(xs)
+    """ use moving average to get better readings """
+    if not hasattr(get_average, "t"):
+       get_average.t = [x, x, x]
+    get_average.t[2] = get_average.t[1]
+    get_average.t[1] = get_average.t[0]
+    get_average.t[0] = x
+    xs = (get_average.t[0]+get_average.t[1]+get_average.t[2])/3
+    return(xs)
 
 sense = SenseHat()
-sense.clear()
+
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
     add_devices([SensehatSensor_temperature()])
     add_devices([SensehatSensor_humidity()])
     add_devices([SensehatSensor_pressure()])
+
 
 class SensehatSensor_temperature(Entity):
     """Representation of a  Temperature Sensor."""
@@ -46,18 +48,20 @@ class SensehatSensor_temperature(Entity):
 
     @property
     def state(self):
+        """ Return state of the sensor"""
         t1 = sense.get_temperature_from_humidity()
         t2 = sense.get_temperature_from_pressure()
         t_cpu = get_cpu_temp()
         t = (t1+t2)/2
         t_corr = t - ((t_cpu-t)/1.5)
-        t_corr = get_average(t_corr)        
+        t_corr = get_average(t_corr)
         return t_corr
 
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement."""
         return TEMP_CELSIUS
+
 
 class SensehatSensor_humidity(Entity):
     """Representation of a Humidity Sensor."""
@@ -69,6 +73,7 @@ class SensehatSensor_humidity(Entity):
 
     @property
     def state(self):
+        """ Return state of the sensor"""
         humidity = sense.get_humidity()
         return humidity
 
@@ -87,6 +92,7 @@ class SensehatSensor_pressure(Entity):
 
     @property
     def state(self):
+        """ Return state of the sensor"""
         pressure = sense.get_pressure()
         return pressure
 
