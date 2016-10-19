bb6fc0f206ca0d74063ad7393dca7b3d5df1e16e
turbokongen <turbokongen@hotmail.com>
2016-10-19 21:44:03 +0200

    review changes

--- homeassistant/components/camera/verisure.py
+++ homeassistant/components/camera/verisure.py
@@ -15,8 +15,6 @@ from homeassistant.util import Throttle
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = 'Verisure File'
-
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the Camera."""
@@ -29,33 +27,29 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     if not smartcam_dict:
         return False
     device_id = list(smartcam_dict.keys())
-    file_path = os.path.join(hass.config.config_dir)
+    directory_path = hass.config.config_dir
     if not os.access(file_path, os.R_OK):
-        _LOGGER.error("file path %s is not readable", file_path)
+        _LOGGER.error("file path %s is not readable", directory_path)
         return False
     for device_id in smartcam_dict:
-        add_devices([VerisureFile(device_id, file_path, smartcam_dict)])
+        add_devices([VerisureFile(device_id, directory_path, smartcam_dict)])
 
 
 # pylint: disable=too-many-instance-attributes
 class VerisureFile(Camera):
     """Local camera."""
 
-    def __init__(self, device_id, file_path, smartcam_dict):
+    def __init__(self, device_id, directory_path, smartcam_dict):
         """Initialize Verisure File Camera component."""
         super().__init__()
 
         self._delete_image = None
-        self._device_id = device_id
-        self._file_path = file_path
-        self._new_image_id = None
-        self._image_id = None
+        self._directory_path = directory_path
         self._image = None
         self._smartcam_dict = smartcam_dict
         self._smartcam_old_dict = {}
-        self._images = []
-        _LOGGER.debug('self._smartcam_dict=%s, self._file_path=%s',
-                      self._smartcam_dict, self._file_path)
+        _LOGGER.debug('self._smartcam_dict=%s, self._directory_path=%s',
+                      self._smartcam_dict, self._directory_path)
 
     def camera_image(self):
         """Return image response."""
@@ -70,42 +64,40 @@ class VerisureFile(Camera):
         if self._smartcam_old_dict == self._smartcam_dict:
             _LOGGER.debug('old and new dict is equal')
             return
-        else:
-            for self._device_id in self._smartcam_dict:
-                self._images = list(self._smartcam_dict.items())
-                self._new_image_id = self._images[0][1][0]
-                _LOGGER.debug('self._device_id=%s, self._images=%s, '
-                              'self._new_image_id=%s', self._device_id,
-                              self._images, self._new_image_id)
-                if self._new_image_id == '-1' or \
-                   self._image_id == self._new_image_id:
-                    _LOGGER.debug('The image is the same, or loading image_id')
-                    return
-                else:
-                    _LOGGER.debug('Download new image %s', self._new_image_id)
-                    hub.my_pages.smartcam.download_image(self._device_id,
-                                                         self._new_image_id,
-                                                         self._file_path)
-                    self._smartcam_old_dict = self._smartcam_dict
-                    if self._image_id:
-                        _LOGGER.debug('self._image_id=%s', self._image_id)
-                        self._delete_image = os.path.join(self._file_path,
-                                                          '{}{}'.format(
-                                                              self._image_id,
-                                                              '.jpg'))
-                        _LOGGER.debug('Deleting %s', self._delete_image)
-                        os.remove(self._delete_image)
-                        self._image_id = self._new_image_id
-                        self._image = os.path.join(self._file_path,
-                                                   '{}{}'.format(
-                                                       self._image_id,
-                                                       '.jpg'))
-                    else:
-                        self._image = os.path.join(self._file_path,
-                                                   '{}{}'.format(
-                                                       self._new_image_id,
-                                                       '.jpg'))
-                        self._image_id = self._new_image_id
+        for self._device_id in self._smartcam_dict:
+            self._images = list(self._smartcam_dict.items())
+            self._new_image_id = self._images[0][1][0]
+            _LOGGER.debug('self._device_id=%s, self._images=%s, '
+                          'self._new_image_id=%s', self._device_id,
+                          self._images, self._new_image_id)
+            if (self._new_image_id == '-1' or
+                self._image_id == self._new_image_id):
+                _LOGGER.debug('The image is the same, or loading image_id')
+                return
+            _LOGGER.debug('Download new image %s', self._new_image_id)
+            hub.my_pages.smartcam.download_image(self._device_id,
+                                                 self._new_image_id,
+                                                 self._directory_path)
+            self._smartcam_old_dict = self._smartcam_dict
+            if self._image_id:
+                _LOGGER.debug('self._image_id=%s', self._image_id)
+                self._delete_image = os.path.join(self._directory_path,
+                                                 '{}{}'.format(
+                                                      self._image_id,
+                                                      '.jpg'))
+                _LOGGER.debug('Deleting %s', self._delete_image)
+                os.remove(self._delete_image)
+                self._image_id = self._new_image_id
+                self._image = os.path.join(self._directory_path,
+                                           '{}{}'.format(
+                                               self._image_id,
+                                               '.jpg'))
+            else:
+                self._image = os.path.join(self._directory_path,
+                                           '{}{}'.format(
+                                              self._new_image_id,
+                                              '.jpg'))
+                self._image_id = self._new_image_id
 
     @Throttle(timedelta(seconds=30))
     def update_imagelist(self):
