60f33082e8c3e92f046d78f95d84d2289df695b0
Johann Kellerman <kellerza@gmail.com>
2016-10-23 18:10:58 +0200

    catch...

--- homeassistant/util/yaml.py
+++ homeassistant/util/yaml.py
@@ -43,7 +43,7 @@ def load_yaml(fname: str) -> Union[List, Dict]:
     except yaml.YAMLError as exc:
         _LOGGER.error(exc)
         raise HomeAssistantError(exc)
-    catch UnicodeDecodeError as exc:
+    except UnicodeDecodeError as exc:
         _LOGGER.error('Unable to read file %s: %s', fname, exc)
         raise HomeAssistantError(exc)
 
