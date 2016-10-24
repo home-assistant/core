64414c7191eb8b10532c8d6445c03b4c31537212
Farmin Farzin <farmin.f@gmail.com>
2016-10-24 10:02:16 +0200

    add platform to coveragerc and fix lint issues

--- homeassistant/components/sensor/sensehat.py
+++ homeassistant/components/sensor/sensehat.py
@@ -16,57 +16,57 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def get_cpu_temp():
-    # get CPU temperature 
+    """get CPU temperature"""
     res = os.popen("vcgencmd measure_temp").readline()
-    t = float(res.replace("temp=", "").replace("'C\n", ""))
-    return t
+    t_cpu = float(res.replace("temp=", "").replace("'C\n", ""))
+    return t_cpu
 
 
-def get_average(x):
-    # use moving average to get better readings
-    if not hasattr(get_average, "t"):
-        get_average.t = [x, x, x]
-    get_average.t[2] = get_average.t[1]
-    get_average.t[1] = get_average.t[0]
-    get_average.t[0] = x
-    xs = (get_average.t[0]+get_average.t[1]+get_average.t[2])/3
-    return xs
+def get_average(temperature):
+    """use moving average to get better readings"""
+    if not hasattr(get_average, "temp"):
+        get_average.temp = [temperature, temperature, temperature]
+    get_average.temp[2] = get_average.temp[1]
+    get_average.temp[1] = get_average.temp[0]
+    get_average.temp[0] = temperature
+    temp_avg = (get_average.temp[0]+get_average.temp[1]+get_average.temp[2])/3
+    return temp_avg
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
-    add_devices([SensehatSensor_temperature(hass)])
-    add_devices([SensehatSensor_humidity(hass)])
-    add_devices([SensehatSensor_pressure(hass)])
+    add_devices([temperature(hass)])
+    add_devices([humidity(hass)])
+    add_devices([pressure(hass)])
 
 
-class SensehatSensor_temperature(Entity):
-    #Representation of a  Temperature Sensor.
+class temperature(Entity):
+    """Representation of a  Temperature Sensor."""
     
     def __init__(self, hass):
-        #Initialize the sensor
+        """Initialize the sensor"""
         self._temp = None
-        # Get initial state
+        """Get initial state"""
         hass.pool.add_job(
             JobPriority.EVENT_STATE, (self.update_ha_state, True))
 
     @property
     def name(self):
-        # Return the name of the sensor.
+        """Return the name of the sensor."""
         return 'Temperature'
 
     @property
     def state(self):
-        # Return state of the sensor
+        """Return state of the sensor"""
         return self._temp
 
     @property
     def unit_of_measurement(self):
-        # Return the unit of measurement
+        """Return the unit of measurement"""
         return TEMP_CELSIUS
 
     def update(self, *args):
-        # Get the latest data
+        """Get the latest data"""
         from sense_hat import SenseHat
         sense = SenseHat()
         temp_from_h = sense.get_temperature_from_humidity()
@@ -78,67 +78,65 @@ class SensehatSensor_temperature(Entity):
         self._temp = t_correct
         
 
-
-class SensehatSensor_humidity(Entity):
-    # Representation of a Humidity Sensor.
+class humidity(Entity):
+    """Representation of a Humidity Sensor."""
 
     def __init__(self, hass):
-        #Initialize the sensor.
+        """Initialize the sensor."""
         self._humidity = None
-        # Get initial state
+        """Get initial state"""
         hass.pool.add_job(
             JobPriority.EVENT_STATE, (self.update_ha_state, True))
 
     @property
     def name(self):
-        # Return the name of the sensor.
+        """Return the name of the sensor."""
         return 'Humidity'
 
     @property
     def state(self):
-        # Return state of the sensor
+        """Return state of the sensor"""
         return self._humidity
 
     @property
     def unit_of_measurement(self):
-        # Return the unit of measurement.
+        """Return the unit of measurement."""
         return '%'
 
     def update(self, *args):
-        # Get the latest data
+        """Get the latest data"""
         from sense_hat import SenseHat
         sense = SenseHat()
         self._humidity = sense.get_humidity()
 
 
-
-class SensehatSensor_pressure(Entity):
-    # Representation of a Pressure Sensor.
+class pressure(Entity):
+    """Representation of a Pressure Sensor."""
 
     def __init__(self, hass):
-        # Initialize the sensor.
+        """Initialize the sensor."""
         self._pressure = None
-        # Get initial state
+        """Get initial state"""
         hass.pool.add_job(
             JobPriority.EVENT_STATE, (self.update_ha_state, True))
 
     @property
     def name(self):
-        # Return the name of the sensor.
+        """Return the name of the sensor."""
         return 'Pressure'
 
     @property
     def state(self):
-        # Return state of the sensor.
+        """Return state of the sensor."""
         return self._pressure
 
     @property
     def unit_of_measurement(self):
-        # Return the unit of measurement.
+        """Return the unit of measurement."""
         return 'mb'
 
     def update(self, *args):
-        # Get the latest data
+        """Get the latest data"""
         from sense_hat import SenseHat
         sense = SenseHat()
         self._pressure = sense.get_pressure()
