10cc50ef583727453f8c68428e38a580715ae7ee
amahlaka <arttu.mahlakaarto@gmail.com>
2016-10-27 17:46:39 +0300

    fixes in turn_on
    i hope this wont break anything again

--- homeassistant/components/light/mipow.py
+++ homeassistant/components/light/mipow.py
@@ -11,16 +11,21 @@ from homeassistant.components.light import (
     ATTR_EFFECT,
     ATTR_FLASH,
     ATTR_RGB_COLOR,
+    EFFECT_COLORLOOP,
     EFFECT_RAINBOW,
     EFFECT_CANDLE,
     FLASH_LONG,
     FLASH_SHORT,
+    SUPPORT_BRIGHTNESS,
+    SUPPORT_EFFECT, SUPPORT_FLASH, SUPPORT_RGB_COLOR,
     Light,
 )
 _LOGGER = logging.getLogger(__name__)
 CHARNAME = "0000fffc-0000-1000-8000-00805f9b34fb"
-REQUIREMENTS = ['pygatt[GATTTOOL]==3.0.0', 'pexpect==4.0.1']
+REQUIREMENTS = ['pygatt[GATTTOOL]==3.0.0']
 # pylint: disable=unused-argument
+SUPPORT_MIPOW = (SUPPORT_BRIGHTNESS | SUPPORT_EFFECT | SUPPORT_FLASH |
+                 SUPPORT_RGB_COLOR)
 
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
@@ -82,7 +87,7 @@ class Mipow(Light):
         """Connect to lamp."""
         import pygatt
         if self._connection is not None:
-            if self._connection.connected:
+            if self._connection._connected:
                 return self._connection
         else:
             if self._adapter is not None:
@@ -108,17 +113,24 @@ class Mipow(Light):
     @property
     def rgb_color(self):
         """Read rgb color."""
+
         return self._rgb_color
 
     @property
     def rgb_bright(self):
         """Read brightness."""
+
         return self._rgb_bright
 
     @property
     def brightness(self):
         """Return brightness."""
         return self._rgb_bright
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        return SUPPORT_MIPOW
 
     @property
     def is_on(self):
@@ -128,52 +140,35 @@ class Mipow(Light):
     def turn_on(self, **kwargs):
         """Turn the device on."""
         self._connection = self.connect()
-
-        if (ATTR_BRIGHTNESS in kwargs
-                and ATTR_RGB_COLOR in kwargs and ATTR_FLASH not in kwargs):
-            self._rgb_color = [x for x in kwargs[ATTR_RGB_COLOR]]
-            brgb = ([kwargs[ATTR_BRIGHTNESS], self._rgb_color[0],
-                     self._rgb_color[1], self._rgb_color[2]])
-
-            self._connection.char_write_handle(0x0025, brgb)
-        elif (ATTR_RGB_COLOR in kwargs and ATTR_BRIGHTNESS not in kwargs
-              and ATTR_FLASH not in kwargs):
-            self._rgb_color = [x for x in kwargs[ATTR_RGB_COLOR]]
-            brgb = [0]
-            brgb.append(self._rgb_color[0])
-            brgb.append(self._rgb_color[1])
-            brgb.append(self._rgb_color[2])
-
-            self._connection.char_write_handle(0x0025, brgb)
-        elif (ATTR_BRIGHTNESS in kwargs and ATTR_RGB_COLOR not in kwargs
-              and ATTR_FLASH not in kwargs):
-            brgb = [kwargs[ATTR_BRIGHTNESS], 0, 0, 0]
-            self._rgb_color = [255, 255, 255]
-            self._connection.char_write_handle(0x0025, brgb)
+        cm = ([0x00, 0x00, 0x14, 0x01])
+        cmd = ([0x00, 0x00, 0x14, 0x02])
+        if (ATTR_RGB_COLOR in kwargs):
+                self._rgb_color = [x for x in kwargs[ATTR_RGB_COLOR]]
+                brgb = [0]
+                brgb.append(self._rgb_color[0])
+                brgb.append(self._rgb_color[1])
+                brgb.append(self._rgb_color[2])
+        elif ATTR_BRIGHTNESS in kwargs:
+                brgb = ([kwargs[ATTR_BRIGHTNESS], 0, 0, 0])
         effect = kwargs.get(ATTR_EFFECT)
-        if effect == EFFECT_RAINBOW:
-            val = bytearray([0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x14, 0x00])
-            self._connection.char_write_handle(0x0023, val)
-        if effect == EFFECT_CANDLE:
-            val = bytearray([0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x14, 0x00])
-            self._connection.char_write_handle(0x0023, val)
-        if ATTR_FLASH in kwargs and ATTR_RGB_COLOR in kwargs:
-            self._rgb_color = [x for x in kwargs[ATTR_RGB_COLOR]]
-            bri = 0
-            red = self._rgb_color[0]
-            green = self._rgb_color[1]
-            blue = self._rgb_color[2]
+        if ATTR_EFFECT in kwargs:
+            if effect == EFFECT_COLORLOOP:
+                mode = 0x03
+            elif effect == EFFECT_RAINBOW:
+                mode = 0x02
+            elif effect == EFFECT_CANDLE:
+                mode = 0x04
+            outp = ([0xff, 0x00, 0x00, 0x00, mode, 0x00, 0x14, 0x00])
+            self._connection.char_write_handle(0x0023, outp)
+        if ATTR_FLASH in kwargs:
             flash = kwargs.get(ATTR_FLASH)
             if flash == FLASH_LONG:
-                speed = 0x14
+                outp = brgb + cmd
+                self._connection.char_write_handle(0x0023, outp)
             if flash == FLASH_SHORT:
-                speed = 0x00
-            mode = 00
-            val = bytearray(
-                [bri, red, green, blue, mode, 0x00, speed, 0x00])
-            self._connection.char_write_handle(0x0023, val)
+                outp = brgb + cm
+                self._connection.char_write_handle(0x0023, outp)
         elif not self.is_on:
-
             self._connection.char_write_handle(0x0025, [255, 0, 0, 0])
 
     def turn_off(self, **kwargs):
