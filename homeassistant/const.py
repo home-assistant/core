89da622b85f21498664a1f32fe5e9c85e128e1ab
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-11-02 21:53:52 +0100

    Change event loop on windows (#4075)
    * Change event loop on windows
    
    * fix
    
    * split PR
    
    * remove set event loop
    
    * Add paulus suggestion
    
    * fix missing import
    
    * revert stuff from PR Splitting
    
    * fix event loop on test

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -6,6 +6,7 @@ PATCH_VERSION = '0.dev0'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 REQUIRED_PYTHON_VER = (3, 4, 2)
+REQUIRED_PYTHON_VER_WIN = (3, 5, 2)
 
 PROJECT_NAME = 'Home Assistant'
 PROJECT_PACKAGE_NAME = 'homeassistant'
