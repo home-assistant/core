f95a4dc12041c6384fdd6ca51a70d5305770fd7d
Jan Losinski <losinskij@gmail.com>
2016-11-02 05:50:27 +0100

    Introduce a send_delay for pilight component (#4051)
    * Add a method to throttle calls to services
    
    This adds CallRateDelayThrottle. This is a class that provides an
    decorator to throttle calls to services. Instead of the Throttle in
    homeassistant.util it does this by delaying all subsequent calls
    instead of just dropping them. Dropping of calls would be bad if we
    call services to actual change the state of a connected hardware (like
    rf controlled power plugs).
    
    Ihe delay is done by rescheduling the call using
    track_point_in_utc_time from homeassistant.helpers.event so it should
    not block the mainloop at all.
    
    * Add unittests for CallRateDelayThrottle
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>
    
    * Introduce a send_delay for pilight component
    
    If pilight is used with a "pilight USB Nano" between the daemon and the
    hardware, we must use a delay between sending multiple signals.
    Otherwise the hardware will just skip random codes. We hit this
    condition for example, if we switch a group of pilight switches on or
    off. Without the delay, random switch signals will not be transmitted by
    the RF transmitter.
    
    As this seems not necessary, if the transmitter is directly connected
    via GPIO, we introduce a optional configuration to set the delay.
    
    * Add unittests for pilight send_delay handling
    
    This adds an unittest to test the delayed calls to the send_code
    service.

--- homeassistant/components/pilight.py
+++ homeassistant/components/pilight.py
@@ -5,10 +5,16 @@ For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/pilight/
 """
 import logging
+import functools
 import socket
+import threading
+
+from datetime import timedelta
 
 import voluptuous as vol
 
+from homeassistant.helpers.event import track_point_in_utc_time
+from homeassistant.util import dt as dt_util
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import (
     EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP, CONF_HOST, CONF_PORT,
@@ -18,8 +24,12 @@ REQUIREMENTS = ['pilight==0.1.1']
 
 _LOGGER = logging.getLogger(__name__)
 
+
+CONF_SEND_DELAY = "send_delay"
+
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 5000
+DEFAULT_SEND_DELAY = 0.0
 DOMAIN = 'pilight'
 
 EVENT = 'pilight_received'
@@ -37,7 +47,9 @@ CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
         vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-        vol.Optional(CONF_WHITELIST, default={}): {cv.string: [cv.string]}
+        vol.Optional(CONF_WHITELIST, default={}): {cv.string: [cv.string]},
+        vol.Optional(CONF_SEND_DELAY, default=DEFAULT_SEND_DELAY):
+            vol.Coerce(float),
     }),
 }, extra=vol.ALLOW_EXTRA)
 
@@ -48,6 +60,8 @@ def setup(hass, config):
 
     host = config[DOMAIN][CONF_HOST]
     port = config[DOMAIN][CONF_PORT]
+    send_throttler = CallRateDelayThrottle(hass,
+                                           config[DOMAIN][CONF_SEND_DELAY])
 
     try:
         pilight_client = pilight.Client(host=host, port=port)
@@ -68,6 +82,7 @@ def setup(hass, config):
 
     hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, stop_pilight_client)
 
+    @send_throttler.limited
     def send_code(call):
         """Send RF code to the pilight-daemon."""
         # Change type to dict from mappingproxy since data has to be JSON
@@ -103,3 +118,58 @@ def setup(hass, config):
     pilight_client.set_callback(handle_received_code)
 
     return True
+
+
+# pylint: disable=too-few-public-methods
+class CallRateDelayThrottle(object):
+    """Helper class to provide service call rate throttling.
+
+    This class provides a decorator to decorate service methods that need
+    to be throttled to not exceed a certain call rate per second.
+    One instance can be used on multiple service methods to archive
+    an overall throttling.
+
+    As this uses track_point_in_utc_time to schedule delayed executions
+    it should not block the mainloop.
+    """
+
+    def __init__(self, hass, delay_seconds: float):
+        """Initialize the delay handler."""
+        self._delay = timedelta(seconds=max(0.0, delay_seconds))
+        self._queue = []
+        self._active = False
+        self._lock = threading.Lock()
+        self._next_ts = dt_util.utcnow()
+        self._schedule = functools.partial(track_point_in_utc_time, hass)
+
+    def limited(self, method):
+        """Decorator to delay calls on a certain method."""
+        @functools.wraps(method)
+        def decorated(*args, **kwargs):
+            """The decorated function."""
+            if self._delay.total_seconds() == 0.0:
+                method(*args, **kwargs)
+                return
+
+            def action(event):
+                """The action wrapper that gets scheduled."""
+                method(*args, **kwargs)
+
+                with self._lock:
+                    self._next_ts = dt_util.utcnow() + self._delay
+
+                    if len(self._queue) == 0:
+                        self._active = False
+                    else:
+                        next_action = self._queue.pop(0)
+                        self._schedule(next_action, self._next_ts)
+
+            with self._lock:
+                if self._active:
+                    self._queue.append(action)
+                else:
+                    self._active = True
+                    schedule_ts = max(dt_util.utcnow(), self._next_ts)
+                    self._schedule(action, schedule_ts)
+
+        return decorated
