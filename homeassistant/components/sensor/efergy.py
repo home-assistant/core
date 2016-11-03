bed177d3928380a3f957b6547b0619c65cfbd0d6
Javier Gonzalez Calleja <jgonzalez@nimbusconcept.com>
2016-11-03 15:48:46 +0100

    Extending efergy component for get the amount of energy consumed in a period

--- homeassistant/components/sensor/efergy.py
+++ homeassistant/components/sensor/efergy.py
@@ -25,17 +25,19 @@ CONF_CURRENCY = 'currency'
 CONF_PERIOD = 'period'
 
 CONF_INSTANT = 'instant_readings'
+CONF_AMOUNT = 'amount'
 CONF_BUDGET = 'budget'
 CONF_COST = 'cost'
 
 SENSOR_TYPES = {
     CONF_INSTANT: ['Energy Usage', 'kW'],
+    CONF_AMOUNT: ['Energy Consumed', 'kW'],
     CONF_BUDGET: ['Energy Budget', None],
     CONF_COST: ['Energy Cost', None],
 }
 
 TYPES_SCHEMA = vol.In(
-    [CONF_INSTANT, CONF_BUDGET, CONF_COST])
+    [CONF_INSTANT, CONF_AMOUNT, CONF_BUDGET, CONF_COST])
 
 SENSORS_SCHEMA = vol.Schema({
     vol.Required(CONF_SENSOR_TYPE): TYPES_SCHEMA,
@@ -104,6 +106,12 @@ class EfergySensor(Entity):
                 url_string = _RESOURCE + 'getInstant?token=' + self.app_token
                 response = get(url_string)
                 self._state = response.json()['reading'] / 1000
+            elif self.type == 'amount':
+                url_string = _RESOURCE + 'getEnergy?token=' + self.app_token \
+                    + '&offset=' + self.utc_offset + '&period=' \
+                    + self.period
+                response = get(url_string)
+                self._state = response.json()['sum']
             elif self.type == 'budget':
                 url_string = _RESOURCE + 'getBudget?token=' + self.app_token
                 response = get(url_string)
