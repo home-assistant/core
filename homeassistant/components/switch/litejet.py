61a0976752946d7e8fb4c530fa7d5698b0853867
Fabian Affolter <mail@fabian-affolter.ch>
2016-11-04 02:43:42 +0100

    Use port instead of url and fix PEP257 issues (#4192)

--- homeassistant/components/switch/litejet.py
+++ homeassistant/components/switch/litejet.py
@@ -5,6 +5,7 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/switch.litejet/
 """
 import logging
+
 import homeassistant.components.litejet as litejet
 from homeassistant.components.switch import SwitchDevice
 
@@ -16,7 +17,7 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the LiteJet switch platform."""
+    """Set up the LiteJet switch platform."""
     litejet_ = hass.data['litejet_system']
 
     devices = []
@@ -28,7 +29,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 
 class LiteJetSwitch(SwitchDevice):
-    """Represents a single LiteJet switch."""
+    """Representation of a single LiteJet switch."""
 
     def __init__(self, hass, lj, i, name):
         """Initialize a LiteJet switch."""
