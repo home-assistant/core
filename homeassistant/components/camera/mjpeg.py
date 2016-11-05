7ddbf3ebdd90af82c75828712788f0aad6b89b42
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-11-05 15:29:22 -0700

    Remove chunked encoding (#4230)

--- homeassistant/components/camera/mjpeg.py
+++ homeassistant/components/camera/mjpeg.py
@@ -112,7 +112,6 @@ class MjpegCamera(Camera):
 
         response = web.StreamResponse()
         response.content_type = stream.headers.get(CONTENT_TYPE_HEADER)
-        response.enable_chunked_encoding()
 
         yield from response.prepare(request)
 
