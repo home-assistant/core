2778e15cb53a4e780da646aee6c3fe06c48c6e62
PollieKrismis <PollieXmas@icloud.com>
2016-11-09 06:45:14 +0200

    I think the penny dropped on how to use tests

--- tests/components/boiler/test_demo.py
+++ tests/components/boiler/test_demo.py
@@ -32,37 +32,45 @@ class TestDemoBoiler(unittest.TestCase):
     def test_setup_params(self):
         """Test the initial parameters."""
         state = self.hass.states.get(ENTITY_GEYSERWISE)
-        # do I also need to set:
-        # name, type, diff_temp, unit_of_measure
+        # test attributes as set by components/boiler/demo.py
         self.assertEqual(55, state.attributes.get('target_water_temperature'))
         self.assertEqual('off', state.attributes.get('away_mode'))
         self.assertEqual("off", state.attributes.get('guest_mode'))
         self.assertEqual("off", state.attributes.get('holiday_mode'))
-        self.assertEqual("idle", state.attributes.get('operation_mode'))
+        self.assertEqual("heat", state.attributes.get('operation_mode'))
         self.assertEqual('off', state.attributes.get('boost'))
 
     def test_default_setup_params(self):
         """Test the setup with default parameters."""
         state = self.hass.states.get(ENTITY_GEYSERWISE)
-        self.assertEqual(30, state.attributes.get('current_water_temp'))
-        self.assertEqual(65, state.attributes.get('target_water_temperature'))
+        # Current temp not available in demo
+        self.assertEqual(None,
+                         state.attributes.get('current_water_temperature'))
+        # Demo platform initialises to 55
+        self.assertEqual(55,
+                         state.attributes.get('target_water_temperature'))
 
     def test_set_only_target_temp_bad_attr(self):
         """Test setting the target temperature without required attribute."""
         state = self.hass.states.get(ENTITY_GEYSERWISE)
-        self.assertEqual(65, state.attributes.get('target_water_temperature'))
+        self.assertEqual(55, state.attributes.get('target_water_temperature'))
+        # None should not be allowed as target temp
         boiler.set_temperature(self.hass, None, ENTITY_GEYSERWISE)
         self.hass.block_till_done()
-        self.assertEqual(65, state.attributes.get('target_water_temperature'))
+        # None should be ignored so target temp should stay 55 (default)
+        self.assertEqual(55, state.attributes.get('target_water_temperature'))
 
     def test_set_only_target_temp(self):
         """Test the setting of the target temperature."""
         state = self.hass.states.get(ENTITY_GEYSERWISE)
-        self.assertEqual(50, state.attributes.get('target_water_temperature'))
-        boiler.set_temperature(self.hass, 55, ENTITY_GEYSERWISE)
+        # Default should be 55 as per demo platform
+        self.assertEqual(55, state.attributes.get('target_water_temperature'))
+        # Try setting a new target temp
+        boiler.set_temperature(self.hass, 65, ENTITY_GEYSERWISE)
         self.hass.block_till_done()
         state = self.hass.states.get(ENTITY_GEYSERWISE)
-        self.assertEqual(55, state.attributes.get('target_water_temperature'))
+        # New temp should now be 65
+        self.assertEqual(65, state.attributes.get('target_water_temperature'))
 
     def test_set_only_target_temp_with_convert(self):
         """Test the setting of the target temperature."""
@@ -77,24 +85,29 @@ class TestDemoBoiler(unittest.TestCase):
         """Test setting operation mode without required attribute, and
            check the state."""
         state = self.hass.states.get(ENTITY_GEYSERWISE)
-        self.assertEqual("idle", state.attributes.get('operation_mode'))
-        self.assertEqual("idle", state.state)
+        # Demo platform set geyserwise default to heat
+        self.assertEqual("heat", state.attributes.get('operation_mode'))
+        self.assertEqual("heat", state.state)
+        # None should not be allowed as valid mode
         boiler.set_operation_mode(self.hass, None, ENTITY_GEYSERWISE)
         self.hass.block_till_done()
         state = self.hass.states.get(ENTITY_GEYSERWISE)
-        self.assertEqual("idle", state.attributes.get('operation_mode'))
-        self.assertEqual("idle", state.state)
+        # Mode should have been ignored and stayed as heat
+        self.assertEqual("heat", state.attributes.get('operation_mode'))
+        self.assertEqual("heat", state.state)
 
     def test_set_operation(self):
         """Test setting of new operation mode."""
         state = self.hass.states.get(ENTITY_GEYSERWISE)
         self.assertEqual("heat", state.attributes.get('operation_mode'))
         self.assertEqual("heat", state.state)
-        boiler.set_operation_mode(self.hass, "pump", ENTITY_GEYSERWISE)
+        # Try setting mode to idle
+        boiler.set_operation_mode(self.hass, "idle", ENTITY_GEYSERWISE)
         self.hass.block_till_done()
         state = self.hass.states.get(ENTITY_GEYSERWISE)
-        self.assertEqual("pump", state.attributes.get('operation_mode'))
-        self.assertEqual("pump", state.state)
+        # Test if new mode was accepted
+        self.assertEqual("idle", state.attributes.get('operation_mode'))
+        self.assertEqual("idle", state.state)
 
     def test_set_away_mode_bad_attr(self):
         """Test setting the away mode without required attribute."""
