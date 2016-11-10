31a2fe14e18ba1e431fc98cf728400c3f2513a6f
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-11-10 23:38:55 +0100

    change verify

--- homeassistant/components/camera/synology.py
+++ homeassistant/components/camera/synology.py
@@ -59,7 +59,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Setup a Synology IP Camera."""
-    if config.get(CONF_VERIFY_SSL):
+    if not config.get(CONF_VERIFY_SSL):
         connector = aiohttp.TCPConnector(verify_ssl=False)
 
         @callback
