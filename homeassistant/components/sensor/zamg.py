7b7a2f734d7b2ff4fbacef9f3e0c0fe2c1e33263
Martin J. Laubach <mjl@laubach.at>
2016-11-14 11:51:30 +0100

    Lose license and whatever.

--- homeassistant/components/sensor/zamg.py
+++ homeassistant/components/sensor/zamg.py
@@ -1,29 +1,7 @@
 # ------------------------------------------------------------------------
 # coding=utf-8
-# ------------------------------------------------------------------------
+#
 #  Created by Martin J. Laubach on 2016-11-07
-#  Copyright (c) 2016 Martin J. Laubach.
-#
-# The MIT License (MIT)
-# Permission is hereby granted, free of charge, to any person
-# obtaining a copy of this software and associated documentation files
-# (the "Software"), to deal in the Software without restriction,
-# including without limitation the rights to use, copy, modify, merge,
-# publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so,
-# subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
 # ------------------------------------------------------------------------
 
 """
@@ -134,7 +112,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 # ------------------------------------------------------------------------
-# pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup platform."""
     station_id = config.get(CONF_STATION_ID)
