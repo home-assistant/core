106e2d68b52838d8901f869d92bf80c3a25ba8ff
Andrea Dainese <andrea.dainese@gmail.com>
2016-11-14 11:50:01 +0100

    Merged from sfiorini

--- homeassistant/components/device_tracker/gpslogger.py
+++ homeassistant/components/device_tracker/gpslogger.py
@@ -1,13 +1,18 @@
 """
-Support for GPSLogger platform.
+Support for the GPSLogger platform.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/device_tracker.gpslogger/
 """
+import asyncio
+from functools import partial
 import logging
 
 from homeassistant.const import HTTP_UNPROCESSABLE_ENTITY
 from homeassistant.components.http import HomeAssistantView
+# pylint: disable=unused-import
+from homeassistant.components.device_tracker import (  # NOQA
+    DOMAIN, PLATFORM_SCHEMA)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,26 +21,32 @@ DEPENDENCIES = ['http']
 
 def setup_scanner(hass, config, see):
     """Setup an endpoint for the GPSLogger application."""
-    hass.wsgi.register_view(LocationSet(hass, see))
+    hass.http.register_view(GPSLoggerView(hass, see))
 
     return True
 
 
-class LocationSet(HomeAssistantView):
-    """View to handle locative requests."""
+class GPSLoggerView(HomeAssistantView):
+    """View to handle gpslogger requests."""
 
     url = '/api/gpslogger'
     name = 'api:gpslogger'
 
     def __init__(self, hass, see):
-        """Initialize Location url endpoints."""
+        """Initialize GPSLogger url endpoints."""
         super().__init__(hass)
         self.see = see
 
+    @asyncio.coroutine
     def get(self, request):
-        """Location message received as GET."""
-        data = request.values
+        """A GPSLogger message received as GET."""
+        res = yield from self._handle(request.GET)
+        return res
 
+    @asyncio.coroutine
+    # pylint: disable=too-many-return-statements
+    def _handle(self, data):
+        """Handle gpslogger request."""
         if 'latitude' not in data or 'longitude' not in data:
             return ('Latitude and longitude not specified.',
                     HTTP_UNPROCESSABLE_ENTITY)
@@ -45,29 +56,19 @@ class LocationSet(HomeAssistantView):
             return ('Device id not specified.',
                     HTTP_UNPROCESSABLE_ENTITY)
 
-        latitude = data['latitude']
-        longitude = data['longitude']
-        device = data['device']
-
-        _LOGGER.error('Received message from GPSLogger: %s (%s, %s)', device,
-                      latitude, longitude)
-
-        kwargs = {
-            'dev_id': data['device'],
-            'gps': (float(data['latitude']), float(data['longitude']))
-        }
+        device = data['device'].replace('-', '')
+        gps_location = (data['latitude'], data['longitude'])
+        accuracy = 200
+        battery = -1
 
         if 'accuracy' in data:
-            kwargs['gps_accuracy'] = int(float(data['accuracy']))
+            accuracy = int(float(data['accuracy']))
         if 'battery' in data:
-            kwargs['battery'] = float(data['battery'])
+            battery = float(data['battery'])
 
-        self.see(**kwargs)
-        return 'Setting location for {}'.format(data['device'])
+        yield from self.hass.loop.run_in_executor(
+            None, partial(self.see, dev_id=device,
+                          gps=gps_location, battery=battery,
+                          gps_accuracy=accuracy))
 
-    # pylint: disable=no-self-use
-    def post(self, request):
-        """Location message received as POST."""
-        _LOGGER.debug('Received unidentified POST message from GPSLogger')
-        return ('Received unidentified message: POST',
-                HTTP_UNPROCESSABLE_ENTITY)
+        return 'Setting location for {}'.format(device)
