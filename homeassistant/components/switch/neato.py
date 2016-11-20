5cf46a6e0324fdfb6240dc688cb53fb5a5d6b868
turbokongen <turbokongen@hotmail.com>
2016-11-20 20:50:21 +0100

    Responsiveness

--- homeassistant/components/switch/neato.py
+++ homeassistant/components/switch/neato.py
@@ -103,6 +103,7 @@ class NeatoConnectedSwitch(ToggleEntity):
             self.robot.start_cleaning()
         elif self.type == SWITCH_TYPE_SCHEDULE:
             self.robot.enable_schedule()
+        self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs):
         """Turn the switch off."""
@@ -111,3 +112,4 @@ class NeatoConnectedSwitch(ToggleEntity):
             self.robot.send_to_base()
         elif self.type == SWITCH_TYPE_SCHEDULE:
             self.robot.disable_schedule()
+        self.schedule_update_ha_state()
