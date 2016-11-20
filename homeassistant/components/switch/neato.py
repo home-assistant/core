274b1784f0933013d6bd4471a0ed3413bf787965
turbokongen <turbokongen@hotmail.com>
2016-11-20 21:32:34 +0100

    Delay was not needed as commands does not return until done.

--- homeassistant/components/switch/neato.py
+++ homeassistant/components/switch/neato.py
@@ -103,7 +103,6 @@ class NeatoConnectedSwitch(ToggleEntity):
             self.robot.start_cleaning()
         elif self.type == SWITCH_TYPE_SCHEDULE:
             self.robot.enable_schedule()
-        self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs):
         """Turn the switch off."""
@@ -112,4 +111,3 @@ class NeatoConnectedSwitch(ToggleEntity):
             self.robot.send_to_base()
         elif self.type == SWITCH_TYPE_SCHEDULE:
             self.robot.disable_schedule()
-        self.schedule_update_ha_state()
