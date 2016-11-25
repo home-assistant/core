e9a1e1445e1ec4ff5caf88da782ca56a595163c5
Valentin Alexeev <valentin.alekseev@gmail.com>
2016-11-25 22:22:13 +0200

    Major: avoid update() in property fetch, Major: implement source support, Major: single device per media player instance, Major: support for volume / mute controls

--- homeassistant/components/media_player/dunehd.py
+++ homeassistant/components/media_player/dunehd.py
@@ -9,15 +9,21 @@ from homeassistant.components.media_player import (
 	SUPPORT_PAUSE, SUPPORT_PLAY_MEDIA,
 	SUPPORT_TURN_OFF, SUPPORT_TURN_ON,
 	SUPPORT_SELECT_SOURCE, SUPPORT_CLEAR_PLAYLIST, PLATFORM_SCHEMA, MediaPlayerDevice)
-from homeassistant.const import (CONF_HOST, STATE_OFF, STATE_PAUSED, STATE_PLAYING)
+from homeassistant.const import (CONF_HOST, CONF_NAME, STATE_OFF, STATE_PAUSED, STATE_PLAYING)
 
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 
 REQUIREMENTS = ['pdunehd==1.0']
 
+DEFAULT_NAME = "DuneHD"
+
+CONF_SOURCES = "sources"
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-	vol.Required(CONF_HOST): cv.ensure_list,
+	vol.Required(CONF_HOST): cv.string,
+	vol.Optional(CONF_SOURCES): cv.ordered_dict(cv.string, cv.string),
+	vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
 })
 
 DUNEHD_PLAYER_SUPPORT = \
@@ -26,31 +32,50 @@ DUNEHD_PLAYER_SUPPORT = \
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
 	"""Setup the media player demo platform."""
-	dev = []
+	sources = config.get(CONF_SOURCES, {})
 
-	for host in config[CONF_HOST]:
-		from pdunehd import DuneHDPlayer
-		dev.append(DuneHDPlayerEntity(DuneHDPlayer(host)))
-
-	add_devices(dev)
+	from pdunehd import DuneHDPlayer
+	add_devices([DuneHDPlayerEntity(DuneHDPlayer(config[CONF_HOST]), config[CONF_NAME], sources)])
 
 class DuneHDPlayerEntity(MediaPlayerDevice):
-	def __init__(self, player):
+	def __init__(self, player, name, sources):
 		self._player = player
+		self._name = name
+		self._sources = sources
+		self.update()
 
 	def update(self):
-		self._player.updateState()
+		self._state = self._player.updateState()
 		return True
 
 	@property
 	def state(self):
-		state = self._player.updateState()
-		if 'playback_state' in state:
-			if state['playback_state'] == 'playing':
+		if 'playback_state' in self._state:
+			if self._state['playback_state'] == 'playing':
 				return STATE_PLAYING
 			else:
 				return STATE_PAUSED
 		return STATE_OFF
+
+	@property
+	def name(self):
+		"""Return the name of the device."""
+		return self._name
+
+	@property
+	def volume_level(self):
+		"""Volume level of the media player (0..1)."""
+		return int(self._state.get('playback_volume', 0)) / 100
+
+	@property
+	def is_volume_muted(self):
+		"""Boolean if volume is currently muted."""
+		return int(self._state.get('playback_mute', 0)) == 1
+
+	@property
+	def source_list(self):
+		"""List of available input sources."""
+		return list(self._sources.keys())
 
 	@property
 	def supported_media_commands(self):
@@ -87,4 +112,8 @@ class DuneHDPlayerEntity(MediaPlayerDevice):
 	@property
 	def media_title(self):
 		"""Current media source."""
-		return self._player.getLastState().get('playback_url', 'Not playing')
+		return self._state.get('playback_url', 'Not playing')
+
+	def select_source(self, source):
+		"""Select input source."""
+		self._player.launchMediaUrl(self._sources.get(source))
