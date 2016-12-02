e4b2330fea0d9a743f21c3bc213eced9013c5db4
Brent Hughes <bah2830@users.noreply.github.com>
2016-12-02 01:02:58 -0600

    InfluxDB: Fixed attributes that are lists causing invalid syntax (#4642)
    * Fixed attributes that are lists cuasing invalid influx syntax
    
    * Added bool and fixed mixed data type issue
    
    * Fixed changing nearly all data types to float causing some worse influxdb errors. whoops
    
    * Added line to end of file

--- homeassistant/components/influxdb.py
+++ homeassistant/components/influxdb.py
@@ -120,7 +120,11 @@ def setup(hass, config):
 
         for key, value in state.attributes.items():
             if key != 'unit_of_measurement':
-                json_body[0]['fields'][key] = value
+                if isinstance(value, (str, float, bool)):
+                    json_body[0]['fields'][key] = value
+                elif isinstance(value, int):
+                    # Prevent column data errors in influxDB.
+                    json_body[0]['fields'][key] = float(value)
 
         json_body[0]['tags'].update(tags)
 
