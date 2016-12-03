d3b62e1fe1a97c0a6178ab488d63856f11558bc5
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-12-03 10:18:00 -0800

    Requirements use zip instead of git (#4692)

--- homeassistant/components/nest.py
+++ homeassistant/components/nest.py
@@ -18,8 +18,8 @@ _CONFIGURING = {}
 _LOGGER = logging.getLogger(__name__)
 
 REQUIREMENTS = [
-    'git+https://github.com/technicalpickles/python-nest.git'
-    '@0be5c8a6307ee81540f21aac4fcd22cc5d98c988'  # nest-cam branch
+    'http://github.com/technicalpickles/python-nest'
+    '/archive/0be5c8a6307ee81540f21aac4fcd22cc5d98c988.zip'  # nest-cam branch
     '#python-nest==3.0.0']
 
 DOMAIN = 'nest'
