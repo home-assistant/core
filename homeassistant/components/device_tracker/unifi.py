7fcbe0b143733858eb380602746170192f000076
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2016-12-04 01:11:52 -0500

    Added persistent_notification in case of error during Unifi device_tracker setup (#4682)

--- homeassistant/components/device_tracker/unifi.py
+++ homeassistant/components/device_tracker/unifi.py
@@ -9,6 +9,7 @@ import urllib
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
+import homeassistant.loader as loader
 from homeassistant.components.device_tracker import DOMAIN, PLATFORM_SCHEMA
 from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
 
@@ -18,6 +19,9 @@ REQUIREMENTS = ['urllib3', 'unifi==1.2.5']
 _LOGGER = logging.getLogger(__name__)
 CONF_PORT = 'port'
 CONF_SITE_ID = 'site_id'
+
+NOTIFICATION_ID = 'unifi_notification'
+NOTIFICATION_TITLE = 'Unifi Device Tracker Setup'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_HOST, default='localhost'): cv.string,
@@ -38,10 +42,18 @@ def get_scanner(hass, config):
     site_id = config[DOMAIN].get(CONF_SITE_ID)
     port = config[DOMAIN].get(CONF_PORT)
 
+    persistent_notification = loader.get_component('persistent_notification')
     try:
         ctrl = Controller(host, username, password, port, 'v4', site_id)
     except urllib.error.HTTPError as ex:
-        _LOGGER.error('Failed to connect to unifi: %s', ex)
+        _LOGGER.error('Failed to connect to Unifi: %s', ex)
+        persistent_notification.create(
+            hass, 'Failed to connect to Unifi. '
+            'Error: {}<br />'
+            'You will need to restart hass after fixing.'
+            ''.format(ex),
+            title=NOTIFICATION_TITLE,
+            notification_id=NOTIFICATION_ID)
         return False
 
     return UnifiScanner(ctrl)
