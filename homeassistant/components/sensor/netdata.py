31ee8d0e3b8252163b0dcb86b3e797ae64cd6f04
Fabian Affolter <fabian@affolter-engineering.ch>
2016-12-05 10:52:45 +0100

    Improvement of the work done by @ezar

--- homeassistant/components/sensor/netdata.py
+++ homeassistant/components/sensor/netdata.py
@@ -1,5 +1,6 @@
 """
 Support gathering system information of hosts which are running netdata.
+
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.netdata/
 """
@@ -9,12 +10,12 @@ from datetime import timedelta
 import requests
 import voluptuous as vol
 
+import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
     CONF_HOST, CONF_PORT, STATE_UNKNOWN, CONF_NAME, CONF_RESOURCES)
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
-import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 _RESOURCE = 'api/v1'
@@ -33,15 +34,17 @@ SENSOR_TYPES = {
     'memory_buffers': ['RAM Buffers', 'MiB', 'system.ram', 'buffers', 1],
     'swap_free': ['Swap Free', 'MiB', 'system.swap', 'free', 1],
     'swap_used': ['Swap Used', 'MiB', 'system.swap', 'used', 1],
-    'processes_running': ['Processes Running', 'Count', 'system.processes', 'running', 0],
-    'processes_blocked': ['Processes Blocked', 'Count', 'system.processes', 'blocked', 0],
+    'processes_running': ['Processes Running', 'Count', 'system.processes',
+                          'running', 0],
+    'processes_blocked': ['Processes Blocked', 'Count', 'system.processes',
+                          'blocked', 0],
     'system_load': ['System Load', '15 min', 'system.processes', 'running', 2],
     'system_io_in': ['System IO In', 'Count', 'system.io', 'in', 0],
     'system_io_out': ['System IO Out', 'Count', 'system.io', 'out', 0],
 }
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST, default=DEFAULT_HOST): cv.string,
+    vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
     vol.Optional(CONF_RESOURCES, default=['memory_free']):
@@ -51,14 +54,14 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 # pylint: disable=unused-variable
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the Netdata sensor."""
+    """Set up the Netdata sensor."""
     name = config.get(CONF_NAME)
     host = config.get(CONF_HOST)
     port = config.get(CONF_PORT)
     url = 'http://{}:{}'.format(host, port)
     version_url = '{}/version.txt'.format(url)
     data_url = '{}/{}/data?chart='.format(url, _RESOURCE)
-    var_conf = config.get(CONF_RESOURCES)
+    resources = config.get(CONF_RESOURCES)
 
     try:
         response = requests.get(version_url, timeout=10)
@@ -69,17 +72,17 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         _LOGGER.error("No route to resource/endpoint: %s", url)
         return False
 
-    v = {}
+    values = {}
     for key, value in sorted(SENSOR_TYPES.items()):
-    	if key in var_conf:
-    	    v.setdefault(value[2], []).append(key)
+        if key in resources:
+            values.setdefault(value[2], []).append(key)
 
     dev = []
-    for chart in v:
-    	rest_url = rest_url ='{}{}&{}'.format(data_url, chart, _REALTIME)
-    	rest = NetdataData(rest_url)
-    	for id in v[chart]:
-    	    dev.append(NetdataSensor(rest, name, id))
+    for chart in values:
+        rest_url = '{}{}&{}'.format(data_url, chart, _REALTIME)
+        rest = NetdataData(rest_url)
+        for sensor_type in values[chart]:
+            dev.append(NetdataSensor(rest, name, sensor_type))
 
     add_devices(dev)
 
@@ -90,38 +93,36 @@ class NetdataSensor(Entity):
     def __init__(self, rest, name, sensor_type):
         """Initialize the sensor."""
         self.rest = rest
-        self._name = name
         self.type = sensor_type
-        self._state = STATE_UNKNOWN
-        self._unit_of_measurement = SENSOR_TYPES[sensor_type][1]
+        self._name = '{} {}'.format(name, SENSOR_TYPES[self.type][0])
+        self._precision = SENSOR_TYPES[self.type][4]
+        self._unit_of_measurement = SENSOR_TYPES[self.type][1]
         self.update()
 
     @property
     def name(self):
         """The name of the sensor."""
-        if self._name is None:
-            return SENSOR_TYPES[self.type][0]
-        else:
-            return '{} {}'.format(self._name, SENSOR_TYPES[self.type][0])
+        return self._name
 
     @property
     def unit_of_measurement(self):
         """Return the unit the value is expressed in."""
         return self._unit_of_measurement
 
-    # pylint: disable=too-many-return-statements
     @property
     def state(self):
         """Return the state of the resources."""
         value = self.rest.data
 
         if value is not None:
-            id = SENSOR_TYPES[self.type][3]
-            if id in value:
-                return "{0:.{1}f}".format(value[id], SENSOR_TYPES[self.type][4])
+            netdata_id = SENSOR_TYPES[self.type][3]
+            if netdata_id in value:
+                return "{0:.{1}f}".format(value[netdata_id], self._precision)
+            else:
+                return STATE_UNKNOWN
 
     def update(self):
-        """Get the latest data from REST API."""
+        """Get the latest data from Netdata REST API."""
         self.rest.update()
 
 
@@ -131,15 +132,15 @@ class NetdataData(object):
     def __init__(self, resource):
         """Initialize the data object."""
         self._resource = resource
-        self.data = dict()
+        self.data = None
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """Get the latest data from the Netdata REST API."""
         try:
-            response = requests.get(self._resource, timeout=10)
-            json = response.json()
-            self.data = {k: v for k, v in zip(json["labels"], json["data"][0])}
+            response = requests.get(self._resource, timeout=5)
+            det = response.json()
+            self.data = {k: v for k, v in zip(det['labels'], det['data'][0])}
 
         except requests.exceptions.ConnectionError:
             _LOGGER.error("No route to host/endpoint: %s", self._resource)
