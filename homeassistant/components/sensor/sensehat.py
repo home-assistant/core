d640779f48c7ea9ab8333d92b321529359d89680
farmin <farmin.farzin@techedgegroup.com>
2016-12-06 10:39:45 +0100

    adding sensehat plugin

---
+++ homeassistant/components/sensor/sensehat.py
@@ -0,0 +1,139 @@
+"""
+Support for Sense HAT sensors.
+
+For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/
+"""
+import os
+import logging
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.helpers.entity import Entity
+
+REQUIREMENTS = ['https://github.com/RPi-Distro/python-sense-hat'
+                '/archive/master.zip#sense-hat==2.2.0']
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def get_cpu_temp():
+    """Get CPU temperature."""
+    res = os.popen("vcgencmd measure_temp").readline()
+    t_cpu = float(res.replace("temp=", "").replace("'C\n", ""))
+    return t_cpu
+
+
+def get_average(temp_base):
+    """Use moving average to get better readings."""
+    if not hasattr(get_average, "temp"):
+        get_average.temp = [temp_base, temp_base, temp_base]
+    get_average.temp[2] = get_average.temp[1]
+    get_average.temp[1] = get_average.temp[0]
+    get_average.temp[0] = temp_base
+    temp_avg = (get_average.temp[0]+get_average.temp[1]+get_average.temp[2])/3
+    return temp_avg
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the sensor platform."""
+    add_devices([AddTemperature(hass)])
+    add_devices([AddHumidity(hass)])
+    add_devices([AddPressure(hass)])
+
+
+class AddTemperature(Entity):
+    """Representation of a Temperature Sensor."""
+
+    def __init__(self, hass):
+        """Initialize the sensor."""
+        self._temp = None
+        """Get initial state."""
+        hass.add_job(self.update_ha_state, True)
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Temperature'
+
+    @property
+    def state(self):
+        """Return state of the sensor."""
+        return self._temp
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return TEMP_CELSIUS
+
+    def update(self, *args):
+        """Get the latest data."""
+        from sense_hat import SenseHat
+        sense = SenseHat()
+        temp_from_h = sense.get_temperature_from_humidity()
+        temp_from_p = sense.get_temperature_from_pressure()
+        t_cpu = get_cpu_temp()
+        t_total = (temp_from_h+temp_from_p)/2
+        t_correct = t_total - ((t_cpu-t_total)/1.5)
+        t_correct = get_average(t_correct)
+        self._temp = t_correct
+
+
+class AddHumidity(Entity):
+    """Representation of a Humidity Sensor."""
+
+    def __init__(self, hass):
+        """Initialize the sensor."""
+        self._humidity = None
+        """Get initial state."""
+        hass.add_job(self.update_ha_state, True)
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Humidity'
+
+    @property
+    def state(self):
+        """Return state of the sensor."""
+        return self._humidity
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return '%'
+
+    def update(self, *args):
+        """Get the latest data."""
+        from sense_hat import SenseHat
+        sense = SenseHat()
+        self._humidity = sense.get_humidity()
+
+
+class AddPressure(Entity):
+    """Representation of a Pressure Sensor."""
+
+    def __init__(self, hass):
+        """Initialize the sensor."""
+        self._pressure = None
+        """Get initial state."""
+        hass.add_job(self.update_ha_state, True)
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Pressure'
+
+    @property
+    def state(self):
+        """Return state of the sensor."""
+        return self._pressure
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return 'mb'
+
+    def update(self, *args):
+        """Get the latest data."""
+        from sense_hat import SenseHat
+        sense = SenseHat()
+        self._pressure = sense.get_pressure()
