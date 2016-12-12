f013ef257af0f8f17574c6d5447cc3a6c2e22dbf
Abhishek Anand (@brixpro-home) <abhishek.anand.iitg@gmail.com>
2016-12-11 19:05:18 -0500

    now automaticlly computing data_at, which needs to be updated 14 days.
    Hopefully, no manual updates would ever be needed for the config of this
    component.
    
    changes in config entries:
    1) removed data_at
    2) added home_url, a string denoting the homepage of the Google Plus
    profile page of the person whose location is being tracked.
    
    Did not check the aboutme.google.com url (self mode).

--- homeassistant/components/device_tracker/gplus.py
+++ homeassistant/components/device_tracker/gplus.py
@@ -6,7 +6,9 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/device_tracker.gplus/
 """
 import logging
+import json
 import voluptuous as vol
+from bs4 import BeautifulSoup
 import homeassistant.helpers.config_validation as cv
 
 from homeassistant.const import (CONF_SCAN_INTERVAL, CONF_ID, CONF_URL,
@@ -24,8 +26,7 @@ CONF_SID = 'cookie_sid'
 CONF_SSID = 'cookie_ssid'
 CONF_HSID = 'cookie_hsid'
 CONF_FREQ = 'data_freq'
-CONF_AT = 'data_at'
-#CONF_ACCURACY = 'accuracy'
+CONF_HOME_URL = 'home_url'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_ID): cv.string,
@@ -34,7 +35,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_SSID): cv.string,
     vol.Required(CONF_HSID): cv.string,
     vol.Required(CONF_FREQ): cv.string,
-    vol.Required(CONF_AT): cv.string,
+    vol.Required(CONF_HOME_URL): cv.string,
     #    vol.Optional(CONF_ACCURACY, default=100): cv.positive_int,
     vol.Optional(CONF_INTERVAL, default=1): vol.All(cv.positive_int,
                                                     vol.Range(min=1)),
@@ -55,8 +56,8 @@ def setup_scanner(hass, config, see):
     cookie_hsid = config[CONF_HSID]
     cookie_ssid = config[CONF_SSID]
     data_freq = config[CONF_FREQ]
-    data_at = config[CONF_AT]
     url = config[CONF_URL]
+    hurl = config[CONF_HOME_URL]
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0',
@@ -75,17 +76,38 @@ def setup_scanner(hass, config, see):
 
     data = {
         'f.req': data_freq,
-        'at': data_at,
+        'at': None,
     }
 
-    def get_position(now):
+    #update data['at'], which may change every 14 days.
+    #Otherwise, Google rejects location queries.
+    #visit the Google plus profile page, and extract the field from the first script in it
+    def update_data_at():
+        api_request = requests.get(
+            hurl, headers=headers, cookies=cookies)
+        soup = BeautifulSoup(api_request.text, 'html.parser')
+        scripts = soup.find_all('script')
+        script = (scripts[0]).contents[0]
+        #print(script.body.attrs)
+        _LOGGER.error(script)
+        sdict = json.loads(script[25:-1])
+        data['at'] = sdict['SNlM0e']
+        _LOGGER.info(data['at'])
+
+    def get_position(_):
         """Get device position."""
+        if data['at'] is None:
+            update_data_at()
         api_request = requests.post(url, headers=headers, cookies=cookies,
                                     data=data, timeout=15)
         if api_request.ok:
             ans = api_request.text
             matched_lines = [line for line in ans.split(
                 '\n') if "www.google.com/maps/" in line]
+            if len(matched_lines) == 0:
+                _LOGGER.error("Google didn't send the location. Updating data['at']")
+                update_data_at()
+
             line = matched_lines[0]
             words = line.split(',')
             latitude = words[12]
