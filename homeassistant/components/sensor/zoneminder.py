67a445fe8752b6fd489d4ca314d1df1f4a041d6e
Kyle Johnson <kjohnson@gnulnx.net>
2016-12-13 16:29:08 +0000

    Rework zoneminder sensor to include monitor status.
    This is a follow-up to #4794 and the sensor now allows you to
    choose which variable(s) (monitor event count, monitor
    function, monitor status) you care about.

--- homeassistant/components/sensor/zoneminder.py
+++ homeassistant/components/sensor/zoneminder.py
@@ -6,13 +6,29 @@ https://home-assistant.io/components/sensor.zoneminder/
 """
 import logging
 
-from homeassistant.const import STATE_UNKNOWN
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+        STATE_UNKNOWN, CONF_MONITORED_VARIABLES)
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 import homeassistant.components.zoneminder as zoneminder
 
 _LOGGER = logging.getLogger(__name__)
 
 DEPENDENCIES = ['zoneminder']
+
+SENSOR_TYPES = {
+        'events',
+        'function',
+        'status'
+}
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_MONITORED_VARIABLES, default=[]):
+        [vol.In(SENSOR_TYPES)],
+})
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -21,29 +37,29 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     monitors = zoneminder.get_state('api/monitors.json')
     for i in monitors['monitors']:
-        sensors.append(
-            ZMSensorMonitors(int(i['Monitor']['Id']), i['Monitor']['Name'])
-        )
-        sensors.append(
-            ZMSensorEvents(int(i['Monitor']['Id']), i['Monitor']['Name'])
-        )
+        for variable in config[CONF_MONITORED_VARIABLES]:
+            sensors.append(ZMSensor(int(i['Monitor']['Id']), i['Monitor']['Name'], variable))
 
     add_devices(sensors)
 
 
-class ZMSensorMonitors(Entity):
+class ZMSensor(Entity):
     """Get the status of each ZoneMinder monitor."""
 
-    def __init__(self, monitor_id, monitor_name):
+    def __init__(self, monitor_id, monitor_name, sensor_type):
         """Initiate monitor sensor."""
         self._monitor_id = monitor_id
         self._monitor_name = monitor_name
-        self._state = None
+        self._name = monitor_name + ' ' + sensor_type.capitalize()
+        self._type = sensor_type
+        self._state = STATE_UNKNOWN
+
+        self.update()
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return '{} Status'.format(self._monitor_name)
+        return self._name
 
     @property
     def state(self):
@@ -51,44 +67,22 @@ class ZMSensorMonitors(Entity):
         return self._state
 
     def update(self):
-        """Update the sensor."""
-        monitor = zoneminder.get_state(
-            'api/monitors/%i.json' % self._monitor_id
-        )
-        if monitor['monitor']['Monitor']['Function'] is None:
-            self._state = STATE_UNKNOWN
-        else:
+        """Update the sensor state and status."""
+
+        if self._type == "function":
+            monitor = zoneminder.get_state(
+                'api/monitors/%i.json' % self._monitor_id
+            )
             self._state = monitor['monitor']['Monitor']['Function']
-
-
-class ZMSensorEvents(Entity):
-    """Get the number of events for each monitor."""
-
-    def __init__(self, monitor_id, monitor_name):
-        """Initiate event sensor."""
-        self._monitor_id = monitor_id
-        self._monitor_name = monitor_name
-        self._state = None
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return '{} Events'.format(self._monitor_name)
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return 'Events'
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    def update(self):
-        """Update the sensor."""
-        event = zoneminder.get_state(
-            'api/events/index/MonitorId:%i.json' % self._monitor_id
-        )
-
-        self._state = event['pagination']['count']
+        elif self._type == "status":
+            monitor = zoneminder.get_state(
+                'api/monitors/alarm/id:%i/command:status.json' %
+                self._monitor_id
+            )
+            self._state = int(monitor['status'])
+        elif self._type == "events":
+            event = zoneminder.get_state(
+                    'api/events/index/MonitorId:%i.json' %
+                    (self._monitor_id)
+            )
+            self._state = event['pagination']['count']
