56e093a9d8e1f79be8d3eadd3daf417685ad4829
magas0 <magas0@users.noreply.github.com>
2016-12-12 21:31:53 -0500

    Removed return False so the Panasonic Viera TV can be added even if it doesn't connect

--- homeassistant/components/media_player/panasonic_viera.py
+++ homeassistant/components/media_player/panasonic_viera.py
@@ -56,9 +56,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     try:
         remote.get_mute()
     except (socket.timeout, TimeoutError, OSError):
-        _LOGGER.error('Panasonic Viera TV is not available at %s:%d',
+        _LOGGER.error('Panasonic Viera TV teststestis not available at %s:%d',
                       host, port)
-        return False
 
     add_devices([PanasonicVieraTVDevice(name, remote)])
     return True
