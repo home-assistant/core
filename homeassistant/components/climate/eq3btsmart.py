70e48231a36705dbd9eea0d7c4515c18f5095b19
Teemu Rytilahti <tpr@iki.fi>
2016-12-17 18:49:48 +0100

    eq3btsmart: support modes and clean up the code to use climatedevice's features

--- homeassistant/components/climate/eq3btsmart.py
+++ homeassistant/components/climate/eq3btsmart.py
@@ -8,18 +8,22 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.components.climate import ClimateDevice, PLATFORM_SCHEMA
+from homeassistant.components.climate import (
+    ClimateDevice, PLATFORM_SCHEMA, PRECISION_HALVES,
+    STATE_UNKNOWN, STATE_AUTO, STATE_ON, STATE_OFF,
+)
 from homeassistant.const import (
     CONF_MAC, TEMP_CELSIUS, CONF_DEVICES, ATTR_TEMPERATURE)
-from homeassistant.util.temperature import convert
+
 import homeassistant.helpers.config_validation as cv
 
 REQUIREMENTS = ['bluepy_devices==0.2.0']
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_MODE = 'mode'
-ATTR_MODE_READABLE = 'mode_readable'
+STATE_BOOST = "boost"
+STATE_AWAY = "away"
+STATE_MANUAL = "manual"
 
 DEVICE_SCHEMA = vol.Schema({
     vol.Required(CONF_MAC): cv.string,
@@ -48,10 +52,22 @@ class EQ3BTSmartThermostat(ClimateDevice):
 
     def __init__(self, _mac, _name):
         """Initialize the thermostat."""
-        from bluepy_devices.devices import eq3btsmart
+        # we want to avoid name clash with this module..
+        from bluepy_devices.devices import eq3btsmart as eq3
+
+        self.modes = {eq3.EQ3BTSMART_UNKOWN: STATE_UNKNOWN,
+                      eq3.EQ3BTSMART_AUTO: STATE_AUTO,
+                      # away mode is handled separately, leaving here just for commentary
+                      eq3.EQ3BTSMART_AWAY: STATE_AWAY,
+                      eq3.EQ3BTSMART_CLOSED: STATE_OFF,
+                      eq3.EQ3BTSMART_OPEN: STATE_ON,
+                      eq3.EQ3BTSMART_MANUAL: STATE_MANUAL,
+                      eq3.EQ3BTSMART_BOOST: STATE_BOOST}
+
+        self.reverse_modes = {v: k for k, v in self.modes.items()}
 
         self._name = _name
-        self._thermostat = eq3btsmart.EQ3BTSmartThermostat(_mac)
+        self._thermostat = eq3.EQ3BTSmartThermostat(_mac)
 
     @property
     def name(self):
@@ -62,6 +78,11 @@ class EQ3BTSmartThermostat(ClimateDevice):
     def temperature_unit(self):
         """Return the unit of measurement that is used."""
         return TEMP_CELSIUS
+
+    @property
+    def precision(self):
+        """Return eq3bt's precision 0.5"""
+        return PRECISION_HALVES
 
     @property
     def current_temperature(self):
@@ -81,24 +102,41 @@ class EQ3BTSmartThermostat(ClimateDevice):
         self._thermostat.target_temperature = temperature
 
     @property
-    def device_state_attributes(self):
-        """Return the device specific state attributes."""
-        return {
-            ATTR_MODE: self._thermostat.mode,
-            ATTR_MODE_READABLE: self._thermostat.mode_readable,
-        }
+    def current_operation(self):
+        """Current mode."""
+        return self.modes[self._thermostat.mode]
+
+    @property
+    def operation_list(self):
+        """List of available operation modes."""
+        return [x for x in self.modes.values()]
+
+    def set_operation_mode(self, operation_mode):
+        """Set operation mode."""
+        self._thermostat.mode = self.reverse_modes[operation_mode]
+
+    def turn_away_mode_off(self):
+        """Away mode off turns to AUTO mode."""
+        self.set_operation_mode(STATE_AUTO)
+
+    def turn_away_mode_on(self):
+        """Sets away mode on."""
+        self.set_operation_mode(STATE_AWAY)
+
+    @property
+    def is_away_mode_on(self):
+        """Returns whether we are away."""
+        return self.current_operation == STATE_AWAY
 
     @property
     def min_temp(self):
         """Return the minimum temperature."""
-        return convert(self._thermostat.min_temp, TEMP_CELSIUS,
-                       self.unit_of_measurement)
+        return self._thermostat.min_temp
 
     @property
     def max_temp(self):
         """Return the maximum temperature."""
-        return convert(self._thermostat.max_temp, TEMP_CELSIUS,
-                       self.unit_of_measurement)
+        return self._thermostat.max_temp
 
     def update(self):
         """Update the data from the thermostat."""
