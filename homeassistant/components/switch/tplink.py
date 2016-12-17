a943bfd33279c6b71a478e0eb51a3252f01d2db1
Georgi Kirichkov <kirichkov@users.noreply.github.com>
2016-12-17 13:06:08 +0200

    Updates comment to denote support for HS200 switch

--- homeassistant/components/switch/tplink.py
+++ homeassistant/components/switch/tplink.py
@@ -1,5 +1,5 @@
 """
-Support for TPLink HS100/HS110 smart switch.
+Support for TPLink HS100/HS110/HS200 smart switch.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/switch.tplink/
