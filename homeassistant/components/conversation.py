e64851972a811860012e8e0019c359c969d17245
doudz <sebastien.ramage@gmail.com>
2016-12-19 14:24:00 +0100

    enhanced conversation with multi-lang
    enhanced conversation with multi-lang support (currently en, fr, es)

--- homeassistant/components/conversation.py
+++ homeassistant/components/conversation.py
@@ -12,8 +12,9 @@ import voluptuous as vol
 
 from homeassistant import core
 from homeassistant.const import (
-    ATTR_ENTITY_ID, SERVICE_TURN_OFF, SERVICE_TURN_ON)
+    ATTR_ENTITY_ID, SERVICE_TURN_OFF, SERVICE_TURN_ON, )
 import homeassistant.helpers.config_validation as cv
+from custom_components.tts.picotts import SUPPORT_LANGUAGES
 
 REQUIREMENTS = ['fuzzywuzzy==0.14.0']
 
@@ -21,7 +22,17 @@ ATTR_TEXT = 'text'
 
 DOMAIN = 'conversation'
 
-REGEX_TURN_COMMAND = re.compile(r'turn (?P<name>(?: |\w)+) (?P<command>\w+)')
+COMMANDS = {'en':{'turn_on':r'(?P<command>turn on|activ\w+|enable|open|launch|start) (?P<name>(?: |\w)+)',
+                  'turn_off':r'(?P<command>turn off|disactiv\w+|disable|close) (?P<name>(?: |\w)+)',
+                  },
+            'fr':{'turn_on':r'(?P<command>allum\w+|lanc\w+|activ\w+|ouvr\w+) (?P<name>(?: |\w)+)',
+                  'turn_off':r'(?P<command>étein\w+|ferm\w+|stop|arrêt\w+|désactiv\w+) (?P<name>(?: |\w)+)',
+                  'repeat':r'(?P<command>répèt\w+) (?P<name>(?: |\w)+)',
+                  },
+            'es':{'turn_on':r'(?P<command>enciend\w+) (?P<name>(?: |\w)+)',
+                  'turn_off':r'(?P<command>apag\w+) (?P<name>(?: |\w)+)',
+                  },
+            }
 
 SERVICE_PROCESS = 'process'
 
@@ -29,8 +40,17 @@ SERVICE_PROCESS_SCHEMA = vol.Schema({
     vol.Required(ATTR_TEXT): vol.All(cv.string, vol.Lower),
 })
 
+CONF_LANG = 'language'
+
+DEFAULT_LANG = 'en'
+
+SUPPORT_LANGUAGES = ['en', 'fr']
+
+
 CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({}),
+    DOMAIN: vol.Schema({
+                        vol.Optional(CONF_LANG,default=DEFAULT_LANG): vol.In(SUPPORT_LANGUAGES),
+                        }),
 }, extra=vol.ALLOW_EXTRA)
 
 
@@ -40,36 +60,52 @@ def setup(hass, config):
     from fuzzywuzzy import process as fuzzyExtract
 
     logger = logging.getLogger(__name__)
+    language = config[DOMAIN].get(CONF_LANG,DEFAULT_LANG)
+    commands = COMMANDS[language]
+    for c in commands:
+        commands[c] = re.compile(commands[c])
 
     def process(service):
         """Parse text into commands."""
+        
+        language = config[DOMAIN].get(CONF_LANG,DEFAULT_LANG)
+        
         text = service.data[ATTR_TEXT]
-        match = REGEX_TURN_COMMAND.match(text)
-
+        match = None
+        for command in commands:
+            match = commands[command].match(text)
+            if match:
+                break
+            
         if not match:
             logger.error("Unable to process: %s", text)
             return
 
-        name, command = match.groups()
+#         command = match.group('command')
+        name = match.group('name')
+        if command == 'repeat':
+            hass.services.call('tts', 'picotts_say', {
+                'message':name,
+                'entity_id':'media_player.vlc'
+            }, blocking=True)
+            return
         entities = {state.entity_id: state.name for state in hass.states.all()}
-        entity_ids = fuzzyExtract.extractOne(
-            name, entities, score_cutoff=65)[2]
+        entity_ids = fuzzyExtract.extractOne(name, entities, score_cutoff=65)[2]
 
         if not entity_ids:
             logger.error(
                 "Could not find entity id %s from text %s", name, text)
             return
 
-        if command == 'on':
+        if command == 'turn_on':
             hass.services.call(core.DOMAIN, SERVICE_TURN_ON, {
                 ATTR_ENTITY_ID: entity_ids,
             }, blocking=True)
 
-        elif command == 'off':
+        elif command == 'turn_off':
             hass.services.call(core.DOMAIN, SERVICE_TURN_OFF, {
                 ATTR_ENTITY_ID: entity_ids,
             }, blocking=True)
-
         else:
             logger.error('Got unsupported command %s from text %s',
                          command, text)
