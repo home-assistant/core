53dde0e4e141a2c2d825960003ad87189e4f3c31
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-12-18 17:35:42 -0800

    Unbreak dev

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -3,7 +3,6 @@
 MAJOR_VERSION = 0
 MINOR_VERSION = 36
 PATCH_VERSION = '0.dev0'
->>>>>>> master
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 REQUIRED_PYTHON_VER = (3, 4, 2)
