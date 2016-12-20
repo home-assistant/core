d7c160e8a6bae7d355bd1e0a030e7b895a1bc7a3
Jerry Workman <jerry.workman@gmail.com>
2016-12-19 21:16:43 -0500

    Update tilt.py with recomendations
    Removed core references and wait. Use timers for both delayed actions.
    Still looking for asyncio and unittest examples.

--- homeassistant/components/switch/tilt.py
+++ homeassistant/components/switch/tilt.py
@@ -5,37 +5,57 @@ Documentation: https://home-assistant.io/components/switch.tilt/
 Jerry Workman <jerry.workman@gmail.com>
 """
 
-import time
 from threading import Timer
-import logging
 from homeassistant.components.switch import SwitchDevice
+from homeassistant.components import switch
 from homeassistant.const import (STATE_ON, STATE_OFF)
-import homeassistant.components as core
-
+# from homeassistant.components.switch import PLATFORM_SCHEMA
+# import voluptuous as vol
+# import homeassistant.helpers.config_validation as cv
+import logging
+import pprint
 _LOGGER = logging.getLogger(__name__)
-DOMAIN = 'tilt'
-DEFAULT_CONTACT_DELAY = 1  # relay switch on time in seconds
+pp = pprint.PrettyPrinter(indent=4)
+
+DOMAIN = 'switch.tilt'
+CONF_TILT_SENSOR = 'tilt_sensor'
+CONF_SWITCH = 'switch'
+CONF_CONTACT_DELAY = 'contact_delay'
+CONF_CONTACT_RUN_TIME = 'run_time'
+DEFAULT_CONTACT_DELAY = 1  # momentary contact relay switch on time (sec)
 DEFAULT_RUN_TIME = 10      # seconds required for door to open or close
+
+"""
+Not using vol because I don't know how to apply it to multiple devices
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+        vol.Required(CONF_TILT_SENSOR, default=None): cv.string,
+        vol.Required(CONF_SWITCH, default=None): cv.string,
+        vol.Optional(CONF_CONTACT_DELAY, default=DEFAULT_CONTACT_DELAY): cv.positive_int,
+        vol.Optional(CONF_CONTACT_RUN_TIME, default=DEFAULT_RUN_TIME): cv.positive_int,
+})
+"""
 
 
 # pylint: disable=unused-argument
-def setup_platform(hass, config, add_devices_callback, discovery_info=None):
+def setup_platform(hass, config, add_entities, discovery_info=None):
     """Add all tilt sensors."""
+    _LOGGER.debug("CONFIG: %s", pp.pformat(config))
     switches = config.get('switches', {})
     devices = []
 
     for dev_name, properties in switches.items():
-        _LOGGER.debug("Adding switch %s", dev_name)
+        _LOGGER.debug("Adding switch %s: %s, %s", dev_name, properties.get(CONF_TILT_SENSOR),
+                      properties.get(CONF_SWITCH))
         devices.append(
             TiltSwitch(
                 hass,
                 dev_name,
-                properties['tilt_sensor'],
-                properties['switch'],
-                properties.get('contact_delay', DEFAULT_CONTACT_DELAY),
-                properties.get('run_time', DEFAULT_RUN_TIME),
-            ))
-    add_devices_callback(devices)
+                properties.get(CONF_TILT_SENSOR),
+                properties.get(CONF_SWITCH),
+                properties.get(CONF_CONTACT_DELAY, DEFAULT_CONTACT_DELAY),
+                properties.get(CONF_CONTACT_RUN_TIME, DEFAULT_RUN_TIME)),
+            )
+    add_entities(devices)
 
 
 class TiltSwitch(SwitchDevice):
@@ -45,87 +65,85 @@ class TiltSwitch(SwitchDevice):
     """
 
     # pylint: disable=too-many-instance-attributes,too-many-arguments
-    def __init__(self, hass, dev_name, tilt_sensor, switch,
+    def __init__(self, hass, dev_name, tilt_sensor_id, switch_id,
                  contact_delay=DEFAULT_CONTACT_DELAY,
                  run_time=DEFAULT_RUN_TIME):
         """init the class"""
         self._name = dev_name
         self._hass = hass
-        self._tilt_sensor = tilt_sensor
-        self._switch = switch
+        self._tilt_sensor = tilt_sensor_id
+        self._switch = switch_id
         self._contact_delay = contact_delay
         self._run_time = run_time
-        self._state = None
+        self._state = STATE_OFF  # Assume closed
         self._running = False  # is door in the process of opening or closing
-        self._timer = None
-        self._insure_relay_is_off()
-        self.update()
+        self._delay_timer = None
+        self._run_timer = None
 
     @property
     def name(self):
         """Return sensor name."""
         return self._name
 
-    def _get_state(self):
-        """@:returns HA state for binary_sensor (On or Off)"""
-        tilt = self._hass.states.get(self._tilt_sensor)
-        try:
-            _LOGGER.debug('tilt _get_state %s is %s', self._name, tilt.state)
-        except AttributeError:
-            pass
-        if tilt:
-            return tilt.state
-        else:
-            return None
-
-    def _insure_relay_is_off(self):
-        """in case that the relay is stuck on turn it off"""
-        core.turn_off(self._hass, self._switch)
-
-    def update(self):
-        """Update state of device."""
-        if self._running:
-            _LOGGER.debug('tilt switch %s running, assumed state is %s',
-                          self._name, self._state)
-            return self._state
-        else:
-            tilt = self._get_state()  # on or off
-            _LOGGER.debug('tilt switch %s is %s', self._name, tilt)
+    @property
+    def state(self):
+        return self._state
 
     @property
     def is_on(self):
-        """@:returns True if on"""
-        return True if self._state == STATE_ON else False
+        return self._state == STATE_ON
 
-    def toggle(self):
+    def turn_on(self, **kwargs):
+        """Open garage door."""
+        _LOGGER.debug('tilt trying to open %s', self._name)
+        if not self.is_on:
+            _LOGGER.debug('tilt opening %s', self._name)
+            self._toggle()
+        self._state = STATE_ON
+        self.schedule_update_ha_state()
+
+    def turn_off(self, **kwargs):
+        """Close garage door."""
+        _LOGGER.debug('tilt trying to close %s', self._name)
+        if self.is_on:
+            _LOGGER.debug('closing %s', self._name)
+            self._toggle()
+        self._state = STATE_OFF
+        self.schedule_update_ha_state()
+
+    def _insure_relay_is_off(self):
+        """in case that the relay is stuck on turn it off"""
+        # self.hass.states.set(self._switch, STATE_OFF)
+        switch.turn_off(self.hass, self._switch)  # more intuitive
+
+    def _toggle(self):
         """Simulate a momentary contact button press."""
 
-        def _stop_timer():
+        def _stop_delay_timer():
+            """Open momentary relay switch"""
+            switch.turn_off(self.hass, self._switch)
+
+        def _stop_run_timer():
             """The door should be open/closed by now so update state"""
             _LOGGER.debug('tilt switch %s stopping timer', self._name)
             self._running = False
             self._insure_relay_is_off()
-            self._state = self._get_state()
 
-        core.turn_on(self._hass, self._switch)
+        _LOGGER.debug("switch.turn_on %s", self._switch)
+        switch.turn_on(self.hass, self._switch)
         self._running = True
-        time.sleep(self._contact_delay)
-        core.turn_off(self._hass, self._switch)
-        self._timer = Timer(self._run_time, _stop_timer)
-        self._timer.start()
+        self._delay_timer = Timer(self._contact_delay, _stop_delay_timer)
+        self._delay_timer.start()
+        self._run_timer = Timer(self._run_time, _stop_run_timer)
+        self._run_timer.start()
 
-    def turn_on(self, **kwargs):
-        """Open garage door."""
-        if not self.is_on:
-            _LOGGER.debug('tilt opening %s', self._name)
-            self.toggle()
-        self._state = STATE_ON
-        self.update_ha_state()
-
-    def turn_off(self, **kwargs):
-        """Close garage door."""
-        if self.is_on:
-            _LOGGER.debug('closing %s', self._name)
-            self.toggle()
-        self._state = STATE_OFF
-        self.update_ha_state()
+    def update(self):
+        """Copy state of tilt sensor state of device."""
+        if self._running:
+            _LOGGER.debug('tilt switch %s running, assumed state is %s',
+                          self._name, self._state)
+        else:
+            _LOGGER.debug('updating, tilt sensor is %s', self._tilt_sensor)
+            tilt = self._hass.states.get(self._tilt_sensor)
+            _LOGGER.debug('tilt switch %s is %s', self._name, tilt)
+            self._state = tilt.state
