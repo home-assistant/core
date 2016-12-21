1fe7cb3dd819ee2e7afff1dff78d22a9e7d5156d
Martin Vacula <vacula@gmail.com>
2016-12-21 22:44:30 +0100

    unnecessary pylint statement removed

--- homeassistant/components/bbb_gpio.py
+++ homeassistant/components/bbb_gpio.py
@@ -4,7 +4,6 @@ Support for controlling GPIO pins of a Beaglebone Black.
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/bbb_gpio/
 """
-# pylint: disable=import-error
 import logging
 
 from homeassistant.const import (
