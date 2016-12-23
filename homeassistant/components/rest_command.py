da130b278a3d166841cafb9b4c5ad9081387ae27
pvizeli <pvizeli@syshack.ch>
2016-12-23 17:16:51 +0100

    New component rest_command

---
+++ homeassistant/components/rest_command.py
@@ -0,0 +1,115 @@
+"""
+Exposes regular rest commands as services.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/rest_command/
+"""
+import asyncio
+import logging
+
+import aiohttp
+import async_timeout
+import voluptuous as vol
+
+from homeassistant.const import CONF_TIMEOUT
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import homeassistant.helpers.config_validation as cv
+
+DOMAIN = 'rest_command'
+
+_LOGGER = logging.getLogger(__name__)
+
+SERVICE_GET = 'get'
+SERVICE_POST = 'post'
+SERVICE_PUT = 'put'
+SERVICE_DELETE = 'delete'
+
+ATTR_URL = 'url'
+ATTR_USERNAME = 'username'
+ATTR_PASSWORD = 'password'
+ATTR_PAYLOAD = 'payload'
+ATTR_PARAMS = 'params'
+
+DEFAULT_TIMEOUT = 10
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): vol.Coerce(int),
+    }),
+}, extra=vol.ALLOW_EXTRA)
+
+SERVICE_SCHEMA = vol.Schema({
+    vol.Required(ATTR_URL): cv.template,
+    vol.Optional(ATTR_PARAMS): {cv.match_all: cv.string},
+    vol.Optional(ATTR_USERNAME): cv.string,
+    vol.Optional(ATTR_PASSWORD): cv.string,
+})
+
+SERVICE_PAYLOAD_SCHEMA = SERVICE_SCHEMA.extend({
+    vol.Optional(ATTR_PAYLOAD): vol.Any(cv.string, {cv.match_all: cv.string}),
+})
+
+
+@asyncio.coroutine
+def async_setup(hass, config):
+    """Setup the rest_command component."""
+    conf = config.get(DOMAIN, {})
+
+    @asyncio.coroutine
+    def async_service_handler(call):
+        """Execute a shell command service."""
+        websession = async_get_clientsession(hass)
+
+        params = None
+        if ATTR_PARAMS in call.data:
+            params = call.data[ATTR_PARAMS]
+
+        auth = None
+        if ATTR_USERNAME in call.data:
+            username = call.data[ATTR_USERNAME]
+            password = call.data.get(ATTR_PASSWORD, '')
+            auth = aiohttp.BasicAuth(username, password=password)
+
+        payload = None
+        if ATTR_PAYLOAD in call.data:
+            payload = call.data[ATTR_PAYLOAD]
+            if isinstance(payload, str):
+                payload = bytes(payload, 'utf-8')
+
+        request = None
+        try:
+            with async_timeout.timeout(conf[CONF_TIMEOUT], loop=hass.loop):
+                request = yield from getattr(websession, call.service)(
+                    call.data[ATTR_URL],
+                    params=params,
+                    data=payload,
+                    auth=auth
+                )
+
+                if request.status == 200:
+                    _LOGGER.info("Success call %s.", request.url)
+                    return
+
+                _LOGGER.warning(
+                    "Error %d on call %s.", request.status, request.url)
+        except asyncio.TimeoutError:
+            _LOGGER.warning("Timeout call %s.", request.url)
+
+        except aiohttp.errors.ClientError:
+            _LOGGER.error("Client error %s.", request.url)
+
+        finally:
+            if request is not None:
+                yield from request.release()
+
+    # register services
+
+    for name in ['get', 'delete']:
+        hass.services.async_register(
+            DOMAIN, name, async_service_handler, schema=SERVICE_SCHEMA)
+
+    for name in ['post', 'put']:
+        hass.services.async_register(
+            DOMAIN, name, async_service_handler, schema=SERVICE_PAYLOAD_SCHEMA)
+
+    return True
