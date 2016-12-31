d974e6582e3a860e76bff9dfb6e811a62680d42f
Christiaan Blom <g.c.blom14@gmail.com>
2016-12-31 18:28:40 +0100

    Cleaned up code

--- homeassistant/components/notify/discord.py
+++ homeassistant/components/notify/discord.py
@@ -12,24 +12,19 @@ from homeassistant.components.notify import (
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['aiohttp==1.1.6','websockets','discord.py==0.16.0']
+REQUIREMENTS = ['aiohttp==1.1.0','websockets','discord.py==0.16.0']
 
 CONF_CLIENT_ID = 'client_id'
-#CONF_CHANNEL_ID = 'channel_id'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_CLIENT_ID): cv.string,
-    #vol.Required(CONF_CHANNEL_ID): cv.string,
+    vol.Required(CONF_CLIENT_ID): cv.string
 })
 
 
 def get_service(hass, config):
     """Get the Discord notification service."""
-
-
     try:
         client_id=config.get(CONF_CLIENT_ID)
-        #channel_id=config.get(CONF_CHANNEL_ID)
     except RandomError:
         _LOGGER.error(
             "Please specify a client ID")
@@ -38,17 +33,16 @@ def get_service(hass, config):
 
 class DiscordNotificationService(BaseNotificationService):
     """Implement the notification service for Discord"""
-
     def __init__(self,hass,client_id):
         """Initialize the service."""
-        import discord
         self.client_id = client_id
         self.hass = hass
-        #self.discord_bot = discord.Client(loop=self.hass.loop)
 
     async def async_send_message(self,message,target):
+        """Login to Discord and send message"""
         import discord
         discord_bot = discord.Client(loop=self.hass.loop)
+
         """Logs in"""
         await discord_bot.login(self.client_id)
 
@@ -58,9 +52,7 @@ class DiscordNotificationService(BaseNotificationService):
           await discord_bot.send_message(channel, message)
         
         """Closes connection and logs out"""
-        #print("logging out")
         await discord_bot.logout()
-        #print("closing connection")
         await discord_bot.close()
 
     def send_message(self, message=None,target=None, **kwargs):
