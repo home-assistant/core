0c575e54b728e62dbc03044965acbe0ad01be61d
David McNett <nugget@macnugget.org>
2017-01-01 17:54:19 -0600

    Conform to Python/project style requirements

--- homeassistant/components/media_player/anthemav.py
+++ homeassistant/components/media_player/anthemav.py
@@ -1,12 +1,13 @@
 """
-Support for Anthem Network Receivers and Processors
+Support for Anthem Network Receivers and Processors.
 
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/media_player.anthemav/
 """
 import logging
 import asyncio
-import voluptuous as vol
 
-DOMAIN = 'anthemav'
+import voluptuous as vol
 
 from homeassistant.components.media_player import (
     PLATFORM_SCHEMA, SUPPORT_TURN_OFF, SUPPORT_TURN_ON, SUPPORT_SELECT_SOURCE,
@@ -14,14 +15,13 @@ from homeassistant.components.media_player import (
 from homeassistant.const import (
     CONF_NAME, CONF_HOST, CONF_PORT, STATE_OFF, STATE_ON, STATE_UNKNOWN,
     EVENT_HOMEASSISTANT_STOP, CONF_SCAN_INTERVAL)
-from homeassistant.helpers.entity import Entity
-from homeassistant.core import callback
-from homeassistant.helpers.event import async_track_state_change
 import homeassistant.helpers.config_validation as cv
+
+REQUIREMENTS = ['anthemav>=1.1.3']
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['anthemav>=1.1.3']
+DOMAIN = 'anthemav'
 
 DEFAULT_NAME = "Anthem AVR"
 
@@ -32,14 +32,15 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Required(CONF_HOST): cv.string,
     vol.Required(CONF_PORT): cv.port,
-    vol.Optional(CONF_SCAN_INTERVAL): vol.All(vol.Coerce(int), vol.Range(min=1))
+    vol.Optional(CONF_SCAN_INTERVAL): vol.All(vol.Coerce(int))
     })
 
 SCAN_INTERVAL = 120
 
+
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
-
+    """Set up our socket to the AVR."""
     import anthemav
 
     host = config.get(CONF_HOST)
@@ -47,9 +48,10 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
 
     SCAN_INTERVAL = config.get(CONF_SCAN_INTERVAL) or 120
 
-    _LOGGER.info('Provisioning Anthem AVR device at '+host+':'+str(port)+' with a '+str(SCAN_INTERVAL)+' second scan interval')
+    _LOGGER.info('Provisioning Anthem AVR device at %s:%d scan every %d sec'
+                 % (host, port, SCAN_INTERVAL))
 
-    device = AnthemAVR(hass,host,port)
+    device = AnthemAVR(hass, host, port)
 
     yield from async_add_devices([device])
 
@@ -57,126 +59,155 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
         _LOGGER.info("Received update callback from AVR: %s" % message)
         hass.async_add_job(device.async_update_ha_state)
 
-    avr =  yield from anthemav.Connection.create(host=host,port=port,loop=hass.loop,update_callback=anthemav_update_callback)
+    avr = yield from anthemav.Connection.create(
+            host=host, port=port,
+            loop=hass.loop, update_callback=anthemav_update_callback)
+
     device._avr = avr
 
-    _LOGGER.warn('dump_devicedata: '+device.dump_avrdata)
-    _LOGGER.warn('dump_conndata: '+avr.dump_conndata)
-    _LOGGER.warn('dump_rawdata: '+avr.protocol.dump_rawdata)
+    _LOGGER.debug('dump_devicedata: '+device.dump_avrdata)
+    _LOGGER.debug('dump_conndata: '+avr.dump_conndata)
+    _LOGGER.debug('dump_rawdata: '+avr.protocol.dump_rawdata)
 
-    #transport, _ = yield from hass.loop.create_task(avr)
-    #hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, transport.close)
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, device._avr.close)
+
 
 class AnthemAVR(MediaPlayerDevice):
-    def __init__(self,hass,host,port):
+    """Entity reading values from Anthem AVR protocol."""
+
+    def __init__(self, hass, host, port):
+        """"Initialize entity with hass, host, and port."""
         super().__init__()
         self.hass = hass
         self._host = host
         self._port = port
 
-    def poll_and_return(self,property,dval):
+    def _poll_and_return(self, property, dval):
         if self.reader:
             pval = getattr(self.reader, property)
-            _LOGGER.debug('query for '+property+' returned from avr object: '+str(pval))
+            _LOGGER.debug('query '+property+' returned from avr: '+str(pval))
             return pval
         else:
-            _LOGGER.debug('query for '+property+' returned from fallback: '+str(dval))
+            _LOGGER.debug('query '+property+' returned default: '+str(dval))
             return dval
 
-    def poll_or_null(self,property):
+    def _poll_or_null(self, property):
         if self.reader:
             pval = getattr(self.reader, property)
-            _LOGGER.debug('query for '+property+' returned from avr object: '+str(pval))
+            _LOGGER.debug('query '+property+' returned from avr: '+str(pval))
             return pval
         else:
-            return 
+            return
 
     @property
     def reader(self):
+        """Expose the protocol with smart wrapper."""
         if hasattr(self, '_avr'):
             if hasattr(self._avr, 'protocol'):
                 return self._avr.protocol
 
     @reader.setter
-    def reader(self,value):
+    def reader(self, value):
         self._reader = value
 
     @property
     def supported_media_commands(self):
+        """Return flag of media commands that are supported."""
         return SUPPORT_ANTHEMAV
 
     @property
     def name(self):
-        return self.poll_and_return('model',DEFAULT_NAME)
+        """Return name of device."""
+        return self._poll_and_return('model', DEFAULT_NAME)
 
     @property
     def state(self):
-        pwrstate = self.poll_or_null('power')
+        """Return state of power on/off."""
+        pwrstate = self._poll_or_null('power')
 
-        if pwrstate == True:
+        if pwrstate is True:
             return STATE_ON
-        elif pwrstate == False:
+        elif pwrstate is False:
             return STATE_OFF
         else:
             return STATE_UNKNOWN
 
     @property
     def volume_level(self):
-        return self.poll_and_return('volume_as_percentage',0.0)
+        """Return volume level from 0 to 1."""
+        return self._poll_and_return('volume_as_percentage', 0.0)
 
     @property
     def media_title(self):
-        return self.poll_and_return('input_name','No Source')
+        """Return current input name (closest we have to media title)."""
+        return self._poll_and_return('input_name', 'No Source')
 
     @property
     def app_name(self):
-        return self.poll_and_return('video_input_resolution_text','')+' '+self.poll_and_return('audio_input_name','')
+        """Return details about current video and audio stream."""
+        return self._poll_and_return('video_input_resolution_text', '') + ' ' \
+            + self._poll_and_return('audio_input_name', '')
 
     @property
     def source(self):
-        return self.poll_and_return('input_name',"Unknown")
+        """Return currently selected input."""
+        return self._poll_and_return('input_name', "Unknown")
 
     @property
     def source_list(self):
-        return self.poll_and_return('input_list',["Unknown"])
+        """Return all active, configured inputs."""
+        return self._poll_and_return('input_list', ["Unknown"])
 
     def media_play(self):
+        """Unsupported."""
         return
 
     def select_source(self, source):
-        self.update_avr('input_name',source)
+        """Change AVR to the designated source (by name)."""
+        self.update_avr('input_name', source)
 
     def turn_off(self):
-        self.update_avr('power',False)
+        """Turn AVR power off."""
+        self.update_avr('power', False)
 
     def turn_on(self):
-        self.update_avr('power',True)
+        """Turn AVR power on."""
+        self.update_avr('power', True)
 
     def volume_up(self):
+        """Unsupported."""
         _LOGGER.debug('volume up')
 
     def volume_down(self):
+        """Unsupported."""
         _LOGGER.debug('volume down')
 
     def set_volume_level(self, volume):
-        self.update_avr('volume_as_percentage',volume)
-        
-    def mute_volume(self, mute):
-        _LOGGER.debug('Request to mute %s',str(mute))
+        """Set AVR volume (0 to 1)."""
+        self.update_avr('volume_as_percentage', volume)
 
-    def update_avr(self,property,value):
+    def mute_volume(self, mute):
+        """Engage AVR mute."""
+        _LOGGER.debug('Request to mute %s', str(mute))
+
+    def update_avr(self, property, value):
+        """Update a property in the AVR."""
         _LOGGER.info('Sending command to AVR: set '+property+' to '+str(value))
         if hasattr(self, 'reader'):
-            setattr(self.reader, property,value)
+            setattr(self.reader, property, value)
         else:
             _LOGGER.warn('Unable to issue command to missing AVR')
 
     @asyncio.coroutine
     def async_update(self):
+        """Vestigial function unneeeded because this platform is local push."""
         _LOGGER.info('async_update invoked')
-        _LOGGER.warn(self.dump_avrdata)
+        # _LOGGER.debug(self.dump_avrdata)
 
     @property
     def dump_avrdata(self):
+        """Return state of avr object for debugging forensics."""
         attrs = vars(self)
-        return('dump_avrdata: '+', '.join("%s: %s" % item for item in attrs.items()))
+        return(
+            'dump_avrdata: '
+            + ', '.join('%s: %s' % item for item in attrs.items()))
