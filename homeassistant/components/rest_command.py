63fd329a7eb95e777c38c77cc2c40326b973f4d8
pvizeli <pvizeli@syshack.ch>
2017-01-05 10:29:07 +0100

    change handling like other command

--- homeassistant/components/rest_command.py
+++ homeassistant/components/rest_command.py
@@ -6,14 +6,14 @@ https://home-assistant.io/components/rest_command/
 """
 import asyncio
 import logging
-import os
 
 import aiohttp
 import async_timeout
 import voluptuous as vol
 
-from homeassistant.const import CONF_TIMEOUT
-from homeassistant.config import load_yaml_config_file
+from homeassistant.const import (
+    CONF_TIMEOUT, CONF_USERNAME, CONF_PASSWORD, CONF_URL, CONF_PAYLOAD,
+    CONF_METHOD)
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 
@@ -21,105 +21,95 @@ DOMAIN = 'rest_command'
 
 _LOGGER = logging.getLogger(__name__)
 
-SERVICE_GET = 'get'
-SERVICE_POST = 'post'
-SERVICE_PUT = 'put'
-SERVICE_DELETE = 'delete'
-
-ATTR_URL = 'url'
-ATTR_USERNAME = 'username'
-ATTR_PASSWORD = 'password'
-ATTR_PAYLOAD = 'payload'
-ATTR_PARAMS = 'params'
-
 DEFAULT_TIMEOUT = 10
+DEFAULT_METHOD = 'get'
+
+SUPPORT_REST_METHODS = [
+    'get',
+    'post',
+    'put',
+    'delete',
+]
+
+COMMAND_SCHEMA = vol.Schema({
+    vol.Required(CONF_URL): cv.template,
+    vol.Optional(CONF_METHOD, default=DEFAULT_METHOD):
+        vol.All(vol.Lower, vol.In(SUPPORT_REST_METHODS)),
+    vol.Optional(CONF_USERNAME): cv.string,
+    vol.Optional(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_PAYLOAD): cv.template,
+    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): vol.Coerce(int),
+})
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-        vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): vol.Coerce(int),
+        cv.slug: COMMAND_SCHEMA,
     }),
 }, extra=vol.ALLOW_EXTRA)
-
-SERVICE_SCHEMA = vol.Schema({
-    # pylint: disable=no-value-for-parameter
-    vol.Required(ATTR_URL): vol.Url(),
-    vol.Optional(ATTR_PARAMS): {cv.match_all: cv.string},
-    vol.Optional(ATTR_USERNAME): cv.string,
-    vol.Optional(ATTR_PASSWORD): cv.string,
-})
-
-SERVICE_PAYLOAD_SCHEMA = SERVICE_SCHEMA.extend({
-    vol.Optional(ATTR_PAYLOAD): vol.Any({cv.match_all: cv.string}, cv.string),
-})
 
 
 @asyncio.coroutine
 def async_setup(hass, config):
     """Setup the rest_command component."""
-    conf = config.get(DOMAIN, {})
+    websession = async_get_clientsession(hass)
 
-    descriptions = yield from hass.loop.run_in_executor(
-        None, load_yaml_config_file, os.path.join(
-            os.path.dirname(__file__), 'services.yaml')
-    )
+    def async_register_rest_command(name, command_config):
+        """Create service for rest command."""
+        timeout = command_config[CONF_TIMEOUT]
+        method = command_config[CONF_METHOD]
 
-    @asyncio.coroutine
-    def async_service_handler(call):
-        """Execute a shell command service."""
-        websession = async_get_clientsession(hass)
-
-        params = None
-        if ATTR_PARAMS in call.data:
-            params = call.data[ATTR_PARAMS]
+        template_url = command_config[CONF_URL]
+        template_url.hass = hass
 
         auth = None
-        if ATTR_USERNAME in call.data:
-            username = call.data[ATTR_USERNAME]
-            password = call.data.get(ATTR_PASSWORD, '')
+        if CONF_USERNAME in command_config:
+            username = command_config[CONF_USERNAME]
+            password = command_config.get(CONF_PASSWORD, '')
             auth = aiohttp.BasicAuth(username, password=password)
 
-        payload = None
-        if ATTR_PAYLOAD in call.data:
-            payload = call.data[ATTR_PAYLOAD]
-            if isinstance(payload, str):
-                payload = bytes(payload, 'utf-8')
+        template_payload = None
+        if CONF_PAYLOAD in command_config:
+            template_payload = command_config[CONF_PAYLOAD]
+            template_payload.hass = hass
 
-        request = None
-        try:
-            with async_timeout.timeout(conf[CONF_TIMEOUT], loop=hass.loop):
-                request = yield from getattr(websession, call.service)(
-                    call.data[ATTR_URL],
-                    params=params,
-                    data=payload,
-                    auth=auth
-                )
+        @asyncio.coroutine
+        def async_service_handler(service):
+            """Execute a shell command service."""
+            payload = None
+            if template_payload:
+                payload = bytes(
+                    template_payload.async_render(variables=service.data),
+                    'utf-8')
 
-                if request.status == 200:
-                    _LOGGER.info("Success call %s.", request.url)
-                    return
+            request = None
+            try:
+                with async_timeout.timeout(timeout, loop=hass.loop):
+                    request = yield from getattr(websession, method)(
+                        template_url.async_render(variables=service.data),
+                        data=payload,
+                        auth=auth
+                    )
 
-                _LOGGER.warning(
-                    "Error %d on call %s.", request.status, request.url)
-        except asyncio.TimeoutError:
-            _LOGGER.warning("Timeout call %s.", request.url)
+                    if request.status == 200:
+                        _LOGGER.info("Success call %s.", request.url)
+                        return
 
-        except aiohttp.errors.ClientError:
-            _LOGGER.error("Client error %s.", request.url)
+                    _LOGGER.warning(
+                        "Error %d on call %s.", request.status, request.url)
+            except asyncio.TimeoutError:
+                _LOGGER.warning("Timeout call %s.", request.url)
 
-        finally:
-            if request is not None:
-                yield from request.release()
+            except aiohttp.errors.ClientError:
+                _LOGGER.error("Client error %s.", request.url)
 
-    # register services
+            finally:
+                if request is not None:
+                    yield from request.release()
 
-    for name in [SERVICE_GET, SERVICE_DELETE]:
-        hass.services.async_register(
-            DOMAIN, name, async_service_handler, descriptions[DOMAIN][name],
-            schema=SERVICE_SCHEMA)
+        # register services
+        hass.services.async_register(DOMAIN, name, async_service_handler)
 
-    for name in [SERVICE_POST, SERVICE_PUT]:
-        hass.services.async_register(
-            DOMAIN, name, async_service_handler, descriptions[DOMAIN][name],
-            schema=SERVICE_PAYLOAD_SCHEMA)
+    for command, command_config in config[DOMAIN].items():
+        async_register_rest_command(command, command_config)
 
     return True
