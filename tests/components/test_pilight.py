4ab3832d24de3ef64ef9ed43ddf88e1494f5c79e
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-01-06 02:40:01 +0100

    Add teardown method to pilight tests
    This is necessary to stop the HomeAssistant instance that was started
    in the setUp method. Without this there happen random test failures.
    
    This is necessary to stabilize the tests for PR #5045.
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- tests/components/test_pilight.py
+++ tests/components/test_pilight.py
@@ -70,6 +70,11 @@ class TestPilight(unittest.TestCase):
         """Setup things to be run when tests are started."""
         self.hass = get_test_home_assistant()
 
+    def tearDown(self):
+        """Stop everything that was started."""
+        if self.hass.is_running:
+            self.hass.stop()
+
     @patch('homeassistant.components.pilight._LOGGER.error')
     def test_connection_failed_error(self, mock_error):
         """Try to connect at 127.0.0.1:5000 with socket error."""
@@ -343,6 +348,11 @@ class TestPilightCallrateThrottler(unittest.TestCase):
     def setUp(self):  # pylint: disable=invalid-name
         """Setup things to be run when tests are started."""
         self.hass = get_test_home_assistant()
+
+    def tearDown(self):
+        """Stop everything that was started."""
+        if self.hass.is_running:
+            self.hass.stop()
 
     def test_call_rate_delay_throttle_disabled(self):
         """Test that the limiter is a noop if no delay set."""
