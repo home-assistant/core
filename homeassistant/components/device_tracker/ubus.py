2f91299ad81ce6999b18292308c6a58cb87bab82
Teemu Rytilahti <tpr@iki.fi>
2017-01-11 20:11:13 +0100

    WIP: device_tracker.ubus: use dhcp service instead of current multi-step process
    Using dhcp service allows getting the wanted information without
    reading extra files and giving extra permissions.
    
    This commit also adds some more error checking & debug facilities for further development.
    
    TBD: As MAC is not properly encoded, the vendor value can't be fetched currently.

--- homeassistant/components/device_tracker/ubus.py
+++ homeassistant/components/device_tracker/ubus.py
@@ -30,6 +30,21 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string
 })
 
+# From http://lxr.mein.io/source/ubus/ubusmsg.h#L99
+UBUS_STATUS_OK = 0
+UBUS_STATUS_INVALID_COMMAND = 1
+UBUS_STATUS_INVALID_ARGUMENT = 2
+UBUS_STATUS_METHOD_NOT_FOUND = 3
+UBUS_STATUS_NOT_FOUND = 4
+UBUS_STATUS_NO_DATA = 5
+UBUS_STATUS_PERMISSION_DENIED = 6
+UBUS_STATUS_TIMEOUT = 7
+UBUS_STATUS_NOT_SUPPORTED = 8
+UBUS_STATUS_UNKNOWN_ERROR = 9
+UBUS_STATUS_CONNECTION_FAILED = 10
+UBUS_STATUS_NO_DATA = 5
+UBUS_STATUS_PERMISSION_DENIED = 6
+
 
 def get_scanner(hass, config):
     """Validate the configuration and return an ubus scanner."""
@@ -50,101 +65,92 @@ class UbusDeviceScanner(DeviceScanner):
         host = config[CONF_HOST]
         username, password = config[CONF_USERNAME], config[CONF_PASSWORD]
 
-        self.parse_api_pattern = re.compile(r"(?P<param>\w*) = (?P<value>.*);")
         self.lock = threading.Lock()
         self.last_results = {}
         self.url = 'http://{}/ubus'.format(host)
 
         self.session_id = _get_session_id(self.url, username, password)
-        self.hostapd = []
-        self.leasefile = None
-        self.mac2name = None
         self.success_init = self.session_id is not None
 
     def scan_devices(self):
         """Scan for new devices and return a list with found device IDs."""
         self._update_info()
-        return self.last_results
-
-    def get_device_name(self, device):
-        """Return the name of the given device or None if we don't know."""
-        with self.lock:
-            if self.leasefile is None:
-                result = _req_json_rpc(self.url, self.session_id,
-                                       'call', 'uci', 'get',
-                                       config="dhcp", type="dnsmasq")
-                if result:
-                    values = result["values"].values()
-                    self.leasefile = next(iter(values))["leasefile"]
-                else:
-                    return
-
-            if self.mac2name is None:
-                result = _req_json_rpc(self.url, self.session_id,
-                                       'call', 'file', 'read',
-                                       path=self.leasefile)
-                if result:
-                    self.mac2name = dict()
-                    for line in result["data"].splitlines():
-                        hosts = line.split(" ")
-                        self.mac2name[hosts[1].upper()] = hosts[3]
-                else:
-                    # Error, handled in the _req_json_rpc
-                    return
-
-            return self.mac2name.get(device.upper(), None)
+        return self.last_results.keys()
 
     @Throttle(MIN_TIME_BETWEEN_SCANS)
     def _update_info(self):
-        """Ensure the information from the Luci router is up to date.
-
-        Returns boolean if scanning successful.
-        """
         if not self.success_init:
             return False
 
         with self.lock:
-            _LOGGER.info("Checking ARP")
+            _LOGGER.info("Checking DHCP leases..")
 
-            if not self.hostapd:
-                hostapd = _req_json_rpc(self.url, self.session_id,
-                                        'list', 'hostapd.*', '')
-                self.hostapd.extend(hostapd.keys())
+            self.last_results.clear()
+            client_macs = []
+            for v in ["ipv4leases", "ipv6leases"]:
+                clients = _req_json_rpc(self.url, self.session_id,
+                                    "call", "dhcp", v)
+                #_LOGGER.error("clients: %s" % clients)
+                for network in clients["device"]:
+                    _LOGGER.debug("Checking network %s" % network)
+                    for lease in clients["device"][network]["leases"]:
+                        _LOGGER.debug("[%s] client: %s" % (network, lease))
+                        client_macs.append(lease["mac"])
+                        self.last_results[lease["mac"]] = lease
 
-            self.last_results = []
-            results = 0
-            for hostapd in self.hostapd:
-                result = _req_json_rpc(self.url, self.session_id,
-                                       'call', hostapd, 'get_clients')
+            _LOGGER.error("Scan found %s clients" % len(client_macs))
 
-                if result:
-                    results = results + 1
-                    self.last_results.extend(result['clients'].keys())
+            return bool(client_macs)
 
-            return bool(results)
+        return False
 
+    def get_device_name(self, device):
+        _LOGGER.info("Getting name for %s", device)
+        return self.last_results[device]["hostname"]
 
 def _req_json_rpc(url, session_id, rpcmethod, subsystem, method, **params):
     """Perform one JSON RPC operation."""
-    data = json.dumps({"jsonrpc": "2.0",
-                       "id": 1,
-                       "method": rpcmethod,
-                       "params": [session_id,
-                                  subsystem,
-                                  method,
-                                  params]})
+
+    data = {"jsonrpc": "2.0",
+            "id": 1,
+            "method": rpcmethod,
+            "params": [session_id,
+                       subsystem,
+                       method,
+                       params]}
+    data_json = json.dumps(data)
+    _LOGGER.debug("> %s (%s)", data["method"], data["params"])
 
     try:
-        res = requests.post(url, data=data, timeout=5)
+        res = requests.post(url, data=data_json, timeout=5)
 
     except requests.exceptions.Timeout:
         return
+
+    _LOGGER.debug("< %s", res)
 
     if res.status_code == 200:
         response = res.json()
 
         if rpcmethod == "call":
-            return response["result"][1]
+            retcode = response["result"][0]
+            if retcode != UBUS_STATUS_OK:
+                if retcode == UBUS_STATUS_PERMISSION_DENIED:
+                    _LOGGER.error("Not enough permissions!")
+                    return
+                elif retcode == UBUS_STATUS_NO_DATA:
+                    _LOGGER.error("Empty leases file!")
+                    return
+                else:
+                    _LOGGER.error("Got ubus error: %s" % retcode)
+                    return
+
+            if "error" in response:
+                _LOGGER.error("Got error from ubus for call %s(%s): %s", data["method"], data["params"], response["error"])
+                return response["error"]
+
+            res = response["result"][1]
+            return res
         else:
             return response["result"]
 
@@ -154,4 +160,6 @@ def _get_session_id(url, username, password):
     res = _req_json_rpc(url, "00000000000000000000000000000000", 'call',
                         'session', 'login', username=username,
                         password=password)
+    #  TODO check for valid login!
+    _LOGGER.error("Request and got token for %s?!: %s" % (username, res))
     return res["ubus_rpc_session"]
