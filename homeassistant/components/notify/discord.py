5ab47661eadd982c18e026c1a921e46c32260417
Christiaan Blom <g.c.blom14@gmail.com>
2017-01-14 20:22:32 +0100

    Changes for Hound

--- homeassistant/components/notify/discord.py
+++ homeassistant/components/notify/discord.py
@@ -4,7 +4,7 @@ import asyncio
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.notify import (
-    ATTR_DATA, PLATFORM_SCHEMA, BaseNotificationService)
+    PLATFORM_SCHEMA, BaseNotificationService)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,38 +20,39 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 def get_service(hass, config):
     """Get the Discord notification service."""
     try:
-        client_id=config.get(CONF_TOKEN)
-    except RandomError:
-        _LOGGER.error("Please specify a client ID")
+        client_id = config.get(CONF_TOKEN)
+    except:
+        _LOGGER.error("Please specify a token")
         return None
     return DiscordNotificationService(hass, client_id)
 
 
 class DiscordNotificationService(BaseNotificationService):
-    """Implement the notification service for Discord"""
+    """Implement the notification service for Discord."""
+
     def __init__(self, hass, client_id):
         """Initialize the service."""
         self.client_id = client_id
         self.hass = hass
 
-    async def async_send_message(self, message,target):
-        """Login to Discord and send message"""
+    async def async_send_message(self, message, target):
+        """Login to Discord and send message."""
         import discord
         discord_bot = discord.Client(loop=self.hass.loop)
 
-        """Logs in"""
+        """Logs in."""
         await discord_bot.login(self.client_id)
 
-        """Gets channel ID(s) and sends message"""
+        """Gets channel ID(s) and sends message."""
         for channelid in target:
-          channel = discord.Object(id=channelid)
-          await discord_bot.send_message(channel, message)
-        
-        """Closes connection and logs out"""
+            channel = discord.Object(id=channelid)
+            await discord_bot.send_message(channel, message)
+
+        """Closes connection and logs out."""
         await discord_bot.logout()
         await discord_bot.close()
 
     def send_message(self, message=None, target=None, **kwargs):
-        """Send a message using Discord"""
-        asyncio.gather(self.async_send_message(message,target),
+        """Send a message using Discord."""
+        asyncio.gather(self.async_send_message(message, target), 
                        loop=self.hass.loop)
