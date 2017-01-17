c4b801a0653f60626493d20923a49a41ab69d47a
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-01-17 01:57:25 -0500

    Introduced Amcrest camera sensors and bump Amcrest module version (#5310)
    * Introduced Amcrest camera sensors
    
    * Makes script/gen_requirements_all.py happy
    
    * Bump Amcrest version across all components
    
    * - Adjusted scan_interval to 10 seconds
    
    - Filtering HTTPError and ConnectTimeout exceptions
    
    - Removed @Throttle decorator

--- homeassistant/components/camera/amcrest.py
+++ homeassistant/components/camera/amcrest.py
@@ -20,7 +20,7 @@ from homeassistant.const import (
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
-REQUIREMENTS = ['amcrest==1.0.0']
+REQUIREMENTS = ['amcrest==1.1.0']
 
 _LOGGER = logging.getLogger(__name__)
 
