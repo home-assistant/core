13cb504619c4b5a2b5f9d0d1441a2898c52f14d8
Teemu Rytilahti <tpr@iki.fi>
2017-01-18 22:43:04 +0100

    ubus: rework to use setup_scanner api, and dhcp & iwinfo services
    This commit makes ubus tracker to use dhcp and iwinfo
    services for device tracking, allowing instantaneous device recognition,
    thanks to using wifi radio for tracking the connected devices.
    dhcp service is required for hostname fetching.
    
    Ports the component also to use setup_scanner() api to provide
    more information about the tracked devices.

--- homeassistant/components/device_tracker/ubus.py
+++ homeassistant/components/device_tracker/ubus.py
@@ -6,21 +6,17 @@ https://home-assistant.io/components/device_tracker.ubus/
 """
 import json
 import logging
-import re
-import threading
-from datetime import timedelta
+from enum import IntEnum
 
 import requests
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.device_tracker import (
-    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+    PLATFORM_SCHEMA, DEFAULT_SCAN_INTERVAL, SOURCE_TYPE_ROUTER)
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
-from homeassistant.util import Throttle
-
-# Return cached results if last scan was less then this time ago.
-MIN_TIME_BETWEEN_SCANS = timedelta(seconds=5)
+from homeassistant import util
+from homeassistant.helpers.event import track_point_in_utc_time
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,30 +26,29 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string
 })
 
+
 # From http://lxr.mein.io/source/ubus/ubusmsg.h#L99
-UBUS_STATUS_OK = 0
-UBUS_STATUS_INVALID_COMMAND = 1
-UBUS_STATUS_INVALID_ARGUMENT = 2
-UBUS_STATUS_METHOD_NOT_FOUND = 3
-UBUS_STATUS_NOT_FOUND = 4
-UBUS_STATUS_NO_DATA = 5
-UBUS_STATUS_PERMISSION_DENIED = 6
-UBUS_STATUS_TIMEOUT = 7
-UBUS_STATUS_NOT_SUPPORTED = 8
-UBUS_STATUS_UNKNOWN_ERROR = 9
-UBUS_STATUS_CONNECTION_FAILED = 10
-UBUS_STATUS_NO_DATA = 5
-UBUS_STATUS_PERMISSION_DENIED = 6
+class UbusStatus(IntEnum):
+    """Ubus status codes."""
+    UBUS_STATUS_OK = 0
+    UBUS_STATUS_INVALID_COMMAND = 1
+    UBUS_STATUS_INVALID_ARGUMENT = 2
+    UBUS_STATUS_METHOD_NOT_FOUND = 3
+    UBUS_STATUS_NOT_FOUND = 4
+    UBUS_STATUS_NO_DATA = 5
+    UBUS_STATUS_PERMISSION_DENIED = 6
+    UBUS_STATUS_TIMEOUT = 7
+    UBUS_STATUS_NOT_SUPPORTED = 8
+    UBUS_STATUS_UNKNOWN_ERROR = 9
+    UBUS_STATUS_CONNECTION_FAILED = 10
 
 
-def get_scanner(hass, config):
-    """Validate the configuration and return an ubus scanner."""
-    scanner = UbusDeviceScanner(config[DOMAIN])
-
-    return scanner if scanner.success_init else None
+class UbusException(Exception):
+    """UbusException gets raised when receiving errors from ubus."""
+    pass
 
 
-class UbusDeviceScanner(DeviceScanner):
+class UbusDeviceScanner:
     """
     This class queries a wireless router running OpenWrt firmware.
 
@@ -65,48 +60,83 @@ class UbusDeviceScanner(DeviceScanner):
         host = config[CONF_HOST]
         username, password = config[CONF_USERNAME], config[CONF_PASSWORD]
 
-        self.lock = threading.Lock()
-        self.last_results = {}
         self.url = 'http://{}/ubus'.format(host)
-
         self.session_id = _get_session_id(self.url, username, password)
-        self.success_init = self.session_id is not None
 
-    def scan_devices(self):
-        """Scan for new devices and return a list with found device IDs."""
-        self._update_info()
-        return self.last_results.keys()
+    def update(self, see):
+        """Fetch clients and leases from the router."""
+        clients = self._get_devices()
+        leases = self._get_leases()
 
-    @Throttle(MIN_TIME_BETWEEN_SCANS)
-    def _update_info(self):
-        if not self.success_init:
-            return False
+        for client in clients:
+            mac = client["mac"].replace(":", "").lower()
+            for lease in leases:
+                if lease["mac"] == mac:
+                    lease.update(client)
+                    # example lease
+                    # {'ip': '192.168.250.186', 'inactive': 3530,
+                    # 'hostname': 'XXXX', 'valid': -23916,
+                    # 'mac': '50:C7:BF:XX:XX:XX', 'noise': -95, 'signal': -48}
+                    extra_attrs = {
+                        "ip": lease["ip"],
+                        "signal": lease["signal"],
+                        "noise": lease["noise"]
+                    }
+                    # _LOGGER.info("Seen: %s", lease)
+                    see(mac=lease["mac"], host_name=lease["hostname"],
+                        source_type=SOURCE_TYPE_ROUTER,
+                        attributes=extra_attrs)
 
-        with self.lock:
-            _LOGGER.info("Checking DHCP leases..")
+    def _get_devices(self):
+        """Request all connected devices."""
+        clients = []
 
-            self.last_results.clear()
-            client_macs = []
-            for v in ["ipv4leases", "ipv6leases"]:
-                clients = _req_json_rpc(self.url, self.session_id,
-                                    "call", "dhcp", v)
-                #_LOGGER.error("clients: %s" % clients)
-                for network in clients["device"]:
-                    _LOGGER.debug("Checking network %s" % network)
-                    for lease in clients["device"][network]["leases"]:
-                        _LOGGER.debug("[%s] client: %s" % (network, lease))
-                        client_macs.append(lease["mac"])
-                        self.last_results[lease["mac"]] = lease
+        try:
+            ifaces = _req_json_rpc(self.url, self.session_id,
+                                   "call", "iwinfo", "devices")
+        except UbusException as ex:
+            _LOGGER.error("Unable to fetch interfaces: %s", ex)
+            return
 
-            _LOGGER.error("Scan found %s clients" % len(client_macs))
+        # _LOGGER.debug("Found %s ifaces: %s", len(ifaces), ifaces)
+        for iface in ifaces["devices"]:
+            devices = _req_json_rpc(self.url, self.session_id,
+                                    "call", "iwinfo", "assoclist",
+                                    device=iface)
+            if "results" in devices:
+                for dev in devices["results"]:
+                    # _LOGGER.debug("device: %s", dev)
+                    clients.append(dev)
 
-            return bool(client_macs)
+        # example client
+        # [{'signal': -47, 'inactive': 310,
+        # 'tx': {'mcs': 7, '40mhz': False, 'rate': 65000, 'short_gi': False},
+        # 'mac': 'F0:B4:29:XX:XX:XX',
+        # 'rx': {'mcs': 7, '40mhz': False, 'rate': 72200, 'short_gi': True},
+        # 'noise': -95}]
+        # _LOGGER.info("Found %s clients: %s", len(clients),
+        #             [client["mac"] for client in clients])
 
-        return False
+        return clients
 
-    def get_device_name(self, device):
-        _LOGGER.info("Getting name for %s", device)
-        return self.last_results[device]["hostname"]
+    def _get_leases(self):
+        """Get all DHCP leases to obtain hostnames."""
+        leases = []
+        for ip_version in ["ipv4leases", "ipv6leases"]:
+            lease_res = _req_json_rpc(self.url, self.session_id,
+                                      "call", "dhcp", ip_version)
+            for network in lease_res["device"]:
+                # _LOGGER.debug("Checking network %s" % network)
+                for lease in lease_res["device"][network]["leases"]:
+                    # _LOGGER.debug("[%s] client: %s", network, lease)
+                    leases.append(lease)
+
+        # example lease
+        # {'mac': '286c07xxxxxx', 'valid': -7471,
+        # 'hostname': 'XXXXX','ip': '192.168.250.132'}
+
+        return leases
+
 
 def _req_json_rpc(url, session_id, rpcmethod, subsystem, method, **params):
     """Perform one JSON RPC operation."""
@@ -125,34 +155,36 @@ def _req_json_rpc(url, session_id, rpcmethod, subsystem, method, **params):
         res = requests.post(url, data=data_json, timeout=5)
 
     except requests.exceptions.Timeout:
+        _LOGGER.error("Got got timeout when doing a request on %s", url)
         return
 
-    _LOGGER.debug("< %s", res)
+    _LOGGER.debug("< %s", res.text)
 
-    if res.status_code == 200:
-        response = res.json()
+    if res.status_code != 200:
+        _LOGGER.error("Got invalid status for the call: %s", res.raw)
+        return
 
-        if rpcmethod == "call":
-            retcode = response["result"][0]
-            if retcode != UBUS_STATUS_OK:
-                if retcode == UBUS_STATUS_PERMISSION_DENIED:
-                    _LOGGER.error("Not enough permissions!")
-                    return
-                elif retcode == UBUS_STATUS_NO_DATA:
-                    _LOGGER.error("Empty leases file!")
-                    return
-                else:
-                    _LOGGER.error("Got ubus error: %s" % retcode)
-                    return
+    response = res.json()
 
-            if "error" in response:
-                _LOGGER.error("Got error from ubus for call %s(%s): %s", data["method"], data["params"], response["error"])
-                return response["error"]
+    if rpcmethod == "call":
+        if "error" in response:
+            _LOGGER.error("Got error from ubus for call %s(%s): %s",
+                          data["method"], data["params"], response["error"])
+            raise UbusException(response["error"])
 
-            res = response["result"][1]
-            return res
-        else:
-            return response["result"]
+        if "result" not in response:
+            _LOGGER.error("Ubus reply has no result dict: %s", response)
+            raise UbusException("Got no result: %s" % response)
+
+        retcode = response["result"][0]
+        if retcode != UbusStatus.UBUS_STATUS_OK:
+            _LOGGER.error("Got error from ubus: %s", UbusStatus(retcode))
+            raise UbusException("Got ubus error: %s" % UbusStatus(retcode))
+
+        res = response["result"][1]
+        return res
+    else:
+        return response["result"]
 
 
 def _get_session_id(url, username, password):
@@ -160,6 +192,51 @@ def _get_session_id(url, username, password):
     res = _req_json_rpc(url, "00000000000000000000000000000000", 'call',
                         'session', 'login', username=username,
                         password=password)
-    #  TODO check for valid login!
-    _LOGGER.error("Request and got token for %s?!: %s" % (username, res))
+
+    # example session
+    # note, sessions do not seem to expire..
+    # {'ubus_rpc_session': '95533928ec0bde5603c408c0eaa314d3',
+    # 'acls': {'uci': {'dhcp': ['read']},
+    #          'ubus': {'iwinfo': ['devices', 'assoclist'],
+    #          'session': ['access', 'login'],
+    #          'dhcp': ['ipv4leases', 'ipv6leases']},
+    # 'access-group': {'hass': ['read'], 'unauthenticated': ['read']}},
+    # 'expires': 300, 'timeout': 300, 'data': {'username': 'hass'}}
+
+    _LOGGER.debug("Got session, verifying required permissions: %s", res)
+    if "acls" not in res:
+        raise UbusException("Session does not have acls: %s" % res)
+
+    if not res["acls"]["ubus"]:
+        raise UbusException("Session does not have ubus acl: %s" % res)
+
+    ubus_acls = res["acls"]["ubus"]
+    if not ubus_acls.keys() & {'dhcp', 'iwinfo'}:
+        raise UbusException("ACL 'dhcp' or 'iwinfo' missing: %s" % res)
+
+    _LOGGER.debug("Got necessary permissions, we're good to go!")
+
     return res["ubus_rpc_session"]
+
+
+def setup_scanner(hass, config, see):
+    """Setup an endpoint for the ubus logger."""
+    try:
+        _LOGGER.info("Trying to start the scanner..")
+        scanner = UbusDeviceScanner(config)
+        interval = DEFAULT_SCAN_INTERVAL
+        _LOGGER.info("Started ubustracker with interval=%s", interval)
+
+        def update(now):
+            """Update all the hosts on every interval time."""
+            _LOGGER.info("Update called..")
+            scanner.update(see)
+            track_point_in_utc_time(hass, update, now + interval)
+            return True
+
+        return update(util.dt.utcnow())
+    except UbusException as ex:
+        _LOGGER.error("Got exception: %s", ex)
+        return False
+
+    return True
