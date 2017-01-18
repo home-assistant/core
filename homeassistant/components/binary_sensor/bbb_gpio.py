bf23dc30af4b8dc1f007555f6f0f54a3a68b3f3c
Martin Vacula <vacula@gmail.com>
2017-01-18 15:03:54 +0100

    trailing white space

--- homeassistant/components/binary_sensor/bbb_gpio.py
+++ homeassistant/components/binary_sensor/bbb_gpio.py
@@ -41,7 +41,7 @@ PIN_SCHEMA = vol.Schema({
     vol.Required(CONF_NAME): cv.string,
     vol.Optional(CONF_BOUNCETIME, default=DEFAULT_BOUNCETIME): cv.positive_int,
     vol.Optional(CONF_INVERT_LOGIC, default=DEFAULT_INVERT_LOGIC): cv.boolean,
-    vol.Optional(CONF_PULL_MODE, default=DEFAULT_PULL_MODE): 
+    vol.Optional(CONF_PULL_MODE, default=DEFAULT_PULL_MODE):
         vol.In(['UP', 'DOWN'])
 })
 
