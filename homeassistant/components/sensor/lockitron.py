7bf76a289ebad8f98e2923c7b99fc80bc1374969
Rick Breidenstein <RickB17@users.noreply.github.com>
2017-01-19 21:46:21 -0500

    Update lockitron.py

--- homeassistant/components/sensor/lockitron.py
+++ homeassistant/components/sensor/lockitron.py
@@ -40,7 +40,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the Lockitron sensors."""
-
     lock_uuid = config.get(CONF_LOCK_UUID)
     access_token = config.get(CONF_ACCESS_TOKEN)
     lock_name = config.get(CONF_LOCK_NAME)
@@ -55,7 +54,6 @@ class LockitronSensor(Entity):
 
     def __init__(self, lock_name, state, lock_uuid, access_token):
         """Initialize the Lockitron sensor."""
-
         self._lock_name = lock_name
         self._state = state
         self._lock_uuid = lock_uuid
@@ -64,19 +62,16 @@ class LockitronSensor(Entity):
     @property
     def should_poll(self):
         """Enable polling"""
-
         return True
 
     @property
     def name(self):
         """Return the name of the sensor."""
-
         return self._lock_name
 
     @property
     def state(self):
         """Return the state of the sensor."""
-
         try:
             url = (
                 "https://api.lockitron.com/v2/locks/" + self._lock_uuid +
@@ -91,7 +86,6 @@ class LockitronSensor(Entity):
 
     def update(self):
         """Get the latest data with a shell command."""
-
         _LOGGER.info('Updating Lockitron state for ' + self._lock_name)
 
         """Return the state of the sensor."""
@@ -117,12 +111,9 @@ class LockitronSensorData(object):
 
     def update(self):
         """Get the latest data with a shell command."""
-
         _LOGGER.info('Updating Lockitron state for ' + self._lock_name)
-        _LOGGER.info(self._lock_uuid)
 
         """Return the state of the sensor."""
-
         self._state = ''
         try:
             url = (
