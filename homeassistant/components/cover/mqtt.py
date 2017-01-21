c78cfb67245bcca50e98d7a23cb982eace7d1a06
Robbie Trencheny <me@robbiet.us>
2017-01-20 21:05:08 -0800

    Expand MQTT cover platform to support tilting

--- homeassistant/components/cover/mqtt.py
+++ homeassistant/components/cover/mqtt.py
@@ -28,6 +28,18 @@ CONF_PAYLOAD_STOP = 'payload_stop'
 CONF_STATE_OPEN = 'state_open'
 CONF_STATE_CLOSED = 'state_closed'
 
+CONF_TILT_STATE_TOPIC = 'tilt_state_topic'
+CONF_TILT_COMMAND_TOPIC = 'tilt_command_topic'
+CONF_TILT_PAYLOAD_OPEN = 'tilt_payload_open'
+CONF_TILT_PAYLOAD_CLOSE = 'tilt_payload_close'
+CONF_TILT_PAYLOAD_STOP = 'tilt_payload_stop'
+CONF_TILT_STATE_OPEN = 'tilt_state_open'
+CONF_TILT_STATE_CLOSED = 'tilt_state_closed'
+
+CONF_TILT_OPTIMISTIC = 'tilt_optimistic'
+
+CONF_TILT_VALUE_TEMPLATE = 'tilt_value_template'
+
 DEFAULT_NAME = 'MQTT Cover'
 DEFAULT_PAYLOAD_OPEN = 'OPEN'
 DEFAULT_PAYLOAD_CLOSE = 'CLOSE'
@@ -42,7 +54,21 @@ PLATFORM_SCHEMA = mqtt.MQTT_RW_PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_PAYLOAD_STOP, default=DEFAULT_PAYLOAD_STOP): cv.string,
     vol.Optional(CONF_STATE_OPEN, default=STATE_OPEN): cv.string,
     vol.Optional(CONF_STATE_CLOSED, default=STATE_CLOSED): cv.string,
+    vol.Required(CONF_TILT_COMMAND_TOPIC): mqtt.valid_publish_topic,
+    vol.Optional(CONF_TILT_STATE_TOPIC): mqtt.valid_subscribe_topic,
+    vol.Optional(CONF_TILT_PAYLOAD_OPEN,
+                 default=DEFAULT_PAYLOAD_OPEN): cv.string,
+    vol.Optional(CONF_TILT_PAYLOAD_CLOSE,
+                 default=DEFAULT_PAYLOAD_CLOSE): cv.string,
+    vol.Optional(CONF_TILT_PAYLOAD_STOP,
+                 default=DEFAULT_PAYLOAD_STOP): cv.string,
+    vol.Optional(CONF_TILT_STATE_OPEN,
+                 default=STATE_OPEN): cv.string,
+    vol.Optional(CONF_TILT_STATE_CLOSED,
+                 default=STATE_CLOSED): cv.string,
     vol.Optional(CONF_OPTIMISTIC, default=DEFAULT_OPTIMISTIC): cv.boolean,
+    vol.Optional(CONF_TILT_OPTIMISTIC, default=DEFAULT_OPTIMISTIC): cv.boolean,
+    vol.Optional(CONF_TILT_VALUE_TEMPLATE): cv.template,
 })
 
 
@@ -51,11 +77,16 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     value_template = config.get(CONF_VALUE_TEMPLATE)
     if value_template is not None:
         value_template.hass = hass
+    tilt_value_template = config.get(CONF_TILT_VALUE_TEMPLATE)
+    if tilt_value_template is not None:
+        tilt_value_template.hass = hass
     add_devices([MqttCover(
         hass,
         config.get(CONF_NAME),
         config.get(CONF_STATE_TOPIC),
         config.get(CONF_COMMAND_TOPIC),
+        config.get(CONF_TILT_STATE_TOPIC),
+        config.get(CONF_TILT_COMMAND_TOPIC),
         config.get(CONF_QOS),
         config.get(CONF_RETAIN),
         config.get(CONF_STATE_OPEN),
@@ -63,32 +94,53 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         config.get(CONF_PAYLOAD_OPEN),
         config.get(CONF_PAYLOAD_CLOSE),
         config.get(CONF_PAYLOAD_STOP),
+        config.get(CONF_TILT_STATE_OPEN),
+        config.get(CONF_TILT_STATE_CLOSED),
+        config.get(CONF_TILT_PAYLOAD_OPEN),
+        config.get(CONF_TILT_PAYLOAD_CLOSE),
+        config.get(CONF_TILT_PAYLOAD_STOP),
         config.get(CONF_OPTIMISTIC),
+        config.get(CONF_TILT_OPTIMISTIC),
         value_template,
+        tilt_value_template,
     )])
 
 
 class MqttCover(CoverDevice):
     """Representation of a cover that can be controlled using MQTT."""
 
-    def __init__(self, hass, name, state_topic, command_topic, qos,
+    def __init__(self, hass, name, state_topic, command_topic,
+                 tilt_state_topic, tilt_command_topic, qos,
                  retain, state_open, state_closed, payload_open, payload_close,
-                 payload_stop, optimistic, value_template):
+                 payload_stop, tilt_state_open, tilt_state_closed,
+                 tilt_payload_open, tilt_payload_close, tilt_payload_stop,
+                 optimistic, tilt_optimistic, value_template,
+                 tvt):
         """Initialize the cover."""
         self._position = None
         self._state = None
+        self._tilt_position = None
+        self._tilt_state = None
         self._hass = hass
         self._name = name
         self._state_topic = state_topic
         self._command_topic = command_topic
+        self._tilt_state_topic = tilt_state_topic
+        self._tilt_command_topic = tilt_command_topic
         self._qos = qos
         self._payload_open = payload_open
         self._payload_close = payload_close
         self._payload_stop = payload_stop
         self._state_open = state_open
         self._state_closed = state_closed
+        self._tilt_payload_open = tilt_payload_open
+        self._tilt_payload_close = tilt_payload_close
+        self._tilt_payload_stop = tilt_payload_stop
+        self._tilt_state_open = tilt_state_open
+        self._tilt_state_closed = tilt_state_closed
         self._retain = retain
         self._optimistic = optimistic or state_topic is None
+        self._tilt_optimistic = tilt_optimistic or tilt_state_topic is None
 
         @callback
         def message_received(topic, payload, qos):
@@ -121,6 +173,37 @@ class MqttCover(CoverDevice):
             mqtt.subscribe(hass, self._state_topic, message_received,
                            self._qos)
 
+        @callback
+        def tilt_message_received(topic, payload, qos):
+            """A new tilt MQTT message has been received."""
+            if tvt is not None:
+                payload = tvt.async_render_with_possible_json_value(
+                    payload)
+            if payload == self._tilt_state_open:
+                self._tilt_state = False
+                hass.async_add_job(self.async_update_ha_state())
+            elif payload == self._tilt_state_closed:
+                self._tilt_state = True
+                hass.async_add_job(self.async_update_ha_state())
+            elif payload.isnumeric() and 0 <= int(payload) <= 100:
+                if int(payload) > 0:
+                    self._tilt_state = False
+                else:
+                    self._tilt_state = True
+                self._tilt_position = int(payload)
+                hass.async_add_job(self.async_update_ha_state())
+            else:
+                _LOGGER.warning(
+                    "Tilt Payload is not True, False, or integer (0-100): %s",
+                    payload)
+
+        if self._tilt_state_topic is None:
+            # Force into optimistic mode.
+            self._tilt_optimistic = True
+        else:
+            mqtt.subscribe(hass, self._tilt_state_topic, tilt_message_received,
+                           self._qos)
+
     @property
     def should_poll(self):
         """No polling needed."""
@@ -144,6 +227,11 @@ class MqttCover(CoverDevice):
         """
         return self._position
 
+    @property
+    def current_cover_tilt_position(self):
+        """Return the current tilt position of the cover."""
+        return self._tilt_position
+
     def open_cover(self, **kwargs):
         """Move the cover up."""
         mqtt.publish(self.hass, self._command_topic, self._payload_open,
@@ -152,6 +240,15 @@ class MqttCover(CoverDevice):
             # Optimistically assume that cover has changed state.
             self._state = False
             self.schedule_update_ha_state()
+
+    def open_cover_tilt(self, **kwargs):
+        """Open the cover tilt."""
+        mqtt.publish(self.hass, self._tilt_command_topic,
+                     self._tilt_payload_open, self._qos, self._retain)
+        if self._tilt_optimistic:
+            # Optimistically assume that cover has changed state.
+            self._tilt_state = False
+            self.update_ha_state()
 
     def close_cover(self, **kwargs):
         """Move the cover down."""
@@ -162,7 +259,41 @@ class MqttCover(CoverDevice):
             self._state = True
             self.schedule_update_ha_state()
 
+    def close_cover_tilt(self, **kwargs):
+        """Close the cover tilt."""
+        mqtt.publish(self.hass, self._tilt_command_topic,
+                     self._tilt_payload_close, self._qos, self._retain)
+        if self._tilt_optimistic:
+            # Optimistically assume that cover has changed state.
+            self._tilt_state = True
+            self.update_ha_state()
+
     def stop_cover(self, **kwargs):
         """Stop the device."""
         mqtt.publish(self.hass, self._command_topic, self._payload_stop,
                      self._qos, self._retain)
+
+    def stop_cover_tilt(self, **kwargs):
+        """Stop the cover tilt."""
+        mqtt.publish(self.hass, self._tilt_command_topic,
+                     self._tilt_payload_stop, self._qos, self._retain)
+
+    def set_cover_position(self, position, **kwargs):
+        """Move the cover to a specific position."""
+        mqtt.publish(self.hass, self._command_topic, position,
+                     self._qos, self._retain)
+        if self._optimistic:
+            # Optimistically assume that cover has changed state.
+            self._state = False
+            self._position = position
+            self.update_ha_state()
+
+    def set_cover_tilt_position(self, tilt_position, **kwargs):
+        """Move the cover til to a specific position."""
+        mqtt.publish(self.hass, self._tilt_command_topic, tilt_position,
+                     self._qos, self._retain)
+        if self._tilt_optimistic:
+            # Optimistically assume that cover has changed state.
+            self._tilt_state = False
+            self._tilt_position = tilt_position
+            self.update_ha_state()
