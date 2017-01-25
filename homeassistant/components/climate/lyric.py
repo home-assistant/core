fcd0f7092a98a03a0a24d903584a5a580d3004b7
Bram Kragten <mail@bramkragten.nl>
2017-01-25 21:20:45 +0100

    WIP Lyric component initial

---
+++ homeassistant/components/climate/lyric.py
@@ -0,0 +1,235 @@
+"""
+Support for Honeywell Lyric thermostats.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/climate.lyric/
+"""
+import logging
+
+import voluptuous as vol
+
+from homeassistant.components.lyric import DATA_LYRIC
+from homeassistant.components.climate import (
+    STATE_AUTO, STATE_COOL, STATE_HEAT, ClimateDevice,
+    PLATFORM_SCHEMA, ATTR_TARGET_TEMP_HIGH, ATTR_TARGET_TEMP_LOW,
+    ATTR_TEMPERATURE)
+from homeassistant.const import (
+    TEMP_CELSIUS, TEMP_FAHRENHEIT,
+    CONF_SCAN_INTERVAL, STATE_ON, STATE_OFF, STATE_UNKNOWN)
+
+DEPENDENCIES = ['lyric']
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_SCAN_INTERVAL):
+        vol.All(vol.Coerce(int), vol.Range(min=1)),
+})
+
+STATE_ECO = 'eco'
+STATE_HEAT_COOL = 'heat-cool'
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the Lyric thermostat."""
+    if discovery_info is None:
+        return
+
+    _LOGGER.debug("Setting up lyric thermostat")
+
+    temp_unit = hass.config.units.temperature_unit
+
+    add_devices(
+        [LyricThermostat(location, device, temp_unit)
+         for location, device in hass.data[DATA_LYRIC].thermostats()],
+        True
+    )
+
+
+class LyricThermostat(ClimateDevice):
+    """Representation of a Lyric thermostat."""
+
+    def __init__(self, location, device, temp_unit):
+        """Initialize the thermostat."""
+        self._unit = temp_unit
+        self.location = location
+        self.device = device
+        self._fan_list = [STATE_ON, STATE_AUTO]
+
+        # Not all lyric devices support cooling and heating remove unused
+        self._operation_list = [STATE_OFF]
+
+        # Add supported lyric thermostat features
+        #if self.device.can_heat:
+        self._operation_list.append(STATE_HEAT)
+
+#         if self.device.can_cool:
+#             self._operation_list.append(STATE_COOL)
+# 
+#         if self.device.can_heat and self.device.can_cool:
+#             self._operation_list.append(STATE_AUTO)
+# 
+#         self._operation_list.append(STATE_ECO)
+
+        # feature of device
+        self._has_fan = False#self.device.has_fan
+
+        # data attributes
+        self._away = None
+        self._location = None
+        self._name = None
+        self._humidity = None
+        self._target_temperature = None
+        self._temperature = None
+        self._temperature_scale = None
+        self._mode = None
+        self._fan = None
+        self._eco_temperature = None
+        self._is_locked = None
+        self._locked_temperature = None
+        self._min_temperature = None
+        self._max_temperature = None
+
+    @property
+    def name(self):
+        """Return the name of the lyric, if any."""
+        return self._name
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        return self._temperature_scale
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        return self._temperature
+
+    @property
+    def current_operation(self):
+        """Return current operation ie. heat, cool, idle."""
+        if self._mode in [STATE_HEAT, STATE_COOL, STATE_OFF, STATE_ECO]:
+            return self._mode
+        elif self._mode == STATE_HEAT_COOL:
+            return STATE_AUTO
+        else:
+            return STATE_UNKNOWN
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        if self._mode != STATE_HEAT_COOL and not self.is_away_mode_on:
+            return self._target_temperature
+        else:
+            return None
+
+    @property
+    def target_temperature_low(self):
+        """Return the lower bound temperature we try to reach."""
+        if (self.is_away_mode_on or self._mode == STATE_ECO) and \
+                self._eco_temperature[0]:
+            # eco_temperature is always a low, high tuple
+            return self._eco_temperature[0]
+        if self._mode == STATE_HEAT_COOL:
+            return self._target_temperature[0]
+        else:
+            return None
+
+    @property
+    def target_temperature_high(self):
+        """Return the upper bound temperature we try to reach."""
+        if (self.is_away_mode_on or self._mode == STATE_ECO) and \
+                self._eco_temperature[1]:
+            # eco_temperature is always a low, high tuple
+            return self._eco_temperature[1]
+        if self._mode == STATE_HEAT_COOL:
+            return self._target_temperature[1]
+        else:
+            return None
+
+    @property
+    def is_away_mode_on(self):
+        """Return if away mode is on."""
+        return self._away
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        target_temp_low = kwargs.get(ATTR_TARGET_TEMP_LOW)
+        target_temp_high = kwargs.get(ATTR_TARGET_TEMP_HIGH)
+        if self._mode == STATE_HEAT_COOL:
+            if target_temp_low is not None and target_temp_high is not None:
+                temp = (target_temp_low, target_temp_high)
+        else:
+            temp = kwargs.get(ATTR_TEMPERATURE)
+        _LOGGER.debug("Lyric set_temperature-output-value=%s", temp)
+        #self.device.target = temp
+        self.device.temperatureSetpoint = temp
+
+    def set_operation_mode(self, operation_mode):
+        """Set operation mode."""
+        if operation_mode in [STATE_HEAT, STATE_COOL, STATE_OFF, STATE_ECO]:
+            device_mode = operation_mode
+        elif operation_mode == STATE_AUTO:
+            device_mode = STATE_HEAT_COOL
+        #self.device.mode = device_mode
+
+    @property
+    def operation_list(self):
+        """List of available operation modes."""
+        return self._operation_list
+
+    def turn_away_mode_on(self):
+        """Turn away on."""
+        self.structure.away = True
+
+    def turn_away_mode_off(self):
+        """Turn away off."""
+        self.structure.away = False
+
+    @property
+    def current_fan_mode(self):
+        """Return whether the fan is on."""
+        if self._has_fan:
+            # Return whether the fan is on
+            return STATE_ON if self._fan else STATE_AUTO
+        else:
+            # No Fan available so disable slider
+            return None
+
+    @property
+    def fan_list(self):
+        """List of available fan modes."""
+        return self._fan_list
+
+    def set_fan_mode(self, fan):
+        """Turn fan on/off."""
+        #self.device.fan = fan.lower()
+
+    @property
+    def min_temp(self):
+        """Identify min_temp in Lyric API or defaults if not available."""
+        return self._min_temperature
+
+    @property
+    def max_temp(self):
+        """Identify max_temp in Lyric API or defaults if not available."""
+        return self._max_temperature
+
+    def update(self):
+        """Cache value from Python-lyric."""
+        self._location = 'locatie'#self.device.where
+        self._name = self.device.name
+        self._humidity = self.device.indoorHumidity
+        self._temperature = self.device.indoorTemperature
+        self._mode = STATE_HEAT
+        self._target_temperature = self.device.temperatureSetpoint
+        self._fan = False
+        self._away = False
+        self._eco_temperature = 0
+        self._locked_temperature = 0
+        self._min_temperature = self.device.minCoolSetpoint
+        self._max_temperature = self.device.maxHeatSetpoint
+        self._is_locked = False#self.device.is_locked
+        if self.device.units == 'Celsius':
+            self._temperature_scale = TEMP_CELSIUS
+        else:
+            self._temperature_scale = TEMP_FAHRENHEIT
---
+++ homeassistant/components/lyric.py
@@ -0,0 +1,199 @@
+"""
+Support for Honeywell Lyric devices.
+
+For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/lyric/
+"""
+import logging
+import socket
+import asyncio
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers import discovery
+from homeassistant.const import (CONF_FILENAME, HTTP_BAD_REQUEST, HTTP_OK)
+from homeassistant.loader import get_component
+from homeassistant.components.http import HomeAssistantView
+
+_CONFIGURING = {}
+_LOGGER = logging.getLogger(__name__)
+
+DEPENDENCIES = ['http']
+
+REQUIREMENTS = [
+    'https://github.com/bramkragten/python-lyric'
+    '/archive/v0.0.1-alpha.1.zip'
+    '#python-lyric==0.0.1']
+
+DOMAIN = 'lyric'
+
+DATA_LYRIC = 'lyric'
+
+LYRIC_CONFIG_FILE = 'lyric.conf'
+CONF_CLIENT_ID = 'client_id'
+CONF_CLIENT_SECRET = 'client_secret'
+CONF_REDIRECT_URI = 'redirect_uri'
+CONF_LOCATIONS = 'locations'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_CLIENT_ID): cv.string,
+        vol.Required(CONF_CLIENT_SECRET): cv.string,
+        vol.Optional(CONF_REDIRECT_URI): cv.string,
+        vol.Optional(CONF_LOCATIONS): vol.All(cv.ensure_list, cv.string)
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+
+def request_configuration(lyric, hass, config):
+
+    """Request configuration steps from the user."""
+    configurator = get_component('configurator')
+    if 'lyric' in _CONFIGURING:
+        _LOGGER.debug("configurator failed")
+        configurator.notify_errors(
+            _CONFIGURING['lyric'], "Failed to configure, please try again.")
+        return
+
+    def lyric_configuration_callback(data):
+        """The actions to do when our configuration callback is called."""
+        _LOGGER.debug("configurator callback")
+        pin = data.get('pin')
+        setup_lyric(hass, lyric, config, pin=pin)
+
+    hass.http.register_view(LyricAuthenticateView(lyric))
+
+    _CONFIGURING['lyric'] = configurator.request_config(
+        hass, "Lyric", lyric_configuration_callback,
+        description=('To configure Lyric, click Request Authorization below, '
+                     'log into your Lyric account, if you get a successfull authorize, click continue. '
+                     'Else enter the URL you are forwared to below and press continue'),
+        link_name='Request Authorization',
+        link_url=lyric.getauthorize_url,
+        submit_caption="Continue...",
+        fields=[{'id': 'pin', 'name': 'Enter the URL', 'type': ''}]
+    )
+
+
+def setup_lyric(hass, lyric, config, pin=None):
+    """Setup Lyric Devices."""
+    if pin is not None:
+        _LOGGER.debug("pin acquired, requesting access token")
+        lyric.authorization_response(pin)
+
+    if lyric._token is None:
+        _LOGGER.debug("no access_token, requesting configuration")
+        request_configuration(lyric, hass, config)
+        return
+
+    if 'lyric' in _CONFIGURING:
+        _LOGGER.debug("configuration done")
+        configurator = get_component('configurator')
+        configurator.request_done(_CONFIGURING.pop('lyric'))
+
+    _LOGGER.debug("proceeding with setup")
+    conf = config[DOMAIN]
+    hass.data[DATA_LYRIC] = LyricDevice(hass, conf, lyric)
+
+    _LOGGER.debug("proceeding with discovery")
+    discovery.load_platform(hass, 'climate', DOMAIN, {}, config)
+#    discovery.load_platform(hass, 'sensor', DOMAIN, {}, config)
+#    discovery.load_platform(hass, 'binary_sensor', DOMAIN, {}, config)
+    _LOGGER.debug("setup done")
+
+    return True
+
+
+def setup(hass, config):
+    """Setup the Lyric thermostat component."""
+    from lyric import lyric
+
+    if 'lyric' in _CONFIGURING:
+        return
+
+    conf = config[DOMAIN]
+    client_id = conf[CONF_CLIENT_ID]
+    client_secret = conf[CONF_CLIENT_SECRET]
+    filename = config.get(CONF_FILENAME, LYRIC_CONFIG_FILE)
+    token_cache_file = hass.config.path(filename)
+    redirect_uri = conf.get(CONF_REDIRECT_URI, hass.config.api.base_url + '/api/lyric/authenticate')
+
+    lyric = lyric.Lyric(
+        token_cache_file=token_cache_file,
+        client_id=client_id, client_secret=client_secret, 
+        app_name='Home Assistant', redirect_uri=redirect_uri)
+
+    setup_lyric(hass, lyric, config)
+
+    return True
+
+
+class LyricDevice(object):
+    """Structure Lyric functions for hass."""
+
+    def __init__(self, hass, conf, lyric):
+        """Init Lyric Devices."""
+        self.hass = hass
+        self.lyric = lyric
+
+        if CONF_LOCATIONS not in conf:
+            self._location = [s.name for s in lyric.locations]
+        else:
+            self._location = conf[CONF_LOCATIONS]
+        _LOGGER.debug("Locations to include: %s", self._location)
+
+    def thermostats(self):
+        """Generator returning list of thermostats and their location."""
+        try:
+            for location in self.lyric.locations:
+                if location.name in self._location:
+                    for device in location.thermostats:
+                        yield (location, device)
+                else:
+                    _LOGGER.debug("Ignoring location %s, not in %s",
+                                  location.name, self._location)
+        except socket.error:
+            _LOGGER.error(
+                "Connection error logging into the lyric web service.")
+
+    def waterLeakDetectors(self):
+        """Generator returning list of water leak detectors."""
+        try:
+            for location in self.lyric.locations:
+                if location.name in self._location:
+                    for device in location.waterLeakDetectors:
+                        yield(location, device)
+                else:
+                    _LOGGER.debug("Ignoring location %s, not in %s",
+                                  location.name, self._location)
+        except socket.error:
+            _LOGGER.error(
+                "Connection error logging into the lyric web service.")
+
+class LyricAuthenticateView(HomeAssistantView):
+    """Handle redirects from lyric oauth2 api, to authenticate."""
+
+    url = '/api/lyric/authenticate'
+    name = 'api:lyric:authenticate'
+    #extra_urls = ['/api/lyric/authenticate/']
+    requires_auth = False
+
+    def __init__(self, lyric):
+        """Initialize Lyric Setup url endpoints."""
+        self.lyric = lyric
+
+    @asyncio.coroutine
+    def get(self, request):
+        """Handle a GET request."""
+
+        hass = request.app['hass']
+        data = request.GET
+
+        if 'code' not in data or 'state' not in data:
+          return self.json_message('Authentication failed, not the right variables, try again.',
+                  HTTP_BAD_REQUEST)
+
+        self.lyric.authorization_code(code=data['code'], state=data['state'])
+
+        return self.json_message('Got the respons! You can close this window now, and click "Continue" in the configurator.')
