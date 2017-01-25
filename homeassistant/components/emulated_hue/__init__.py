6ccf4a6cd079bcc864c38275f0c8adc0e131ee2b
jeremydk <jeremydk@gmail.com>
2017-01-25 11:39:08 -0800

    Commenting change for clarity.

--- homeassistant/components/emulated_hue/__init__.py
+++ homeassistant/components/emulated_hue/__init__.py
@@ -175,7 +175,7 @@ class Config(object):
         self.exposed_domains = conf.get(
             CONF_EXPOSED_DOMAINS, DEFAULT_EXPOSED_DOMAINS)
         
-        # Calculated effective advertised IP and port
+        # Calculated effective advertised IP and port for network isolation
         self.advertise_ip = conf.get(CONF_ADVERTISE_IP) or self.host_ip_addr
 
         self.advertise_port = conf.get(CONF_ADVERTISE_PORT) or self.listen_port
