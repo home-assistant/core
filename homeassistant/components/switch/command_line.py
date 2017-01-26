7d9551ea6299e5a02e31c2686097a07260b651d1
Daniel Høyer Iversen <mail@dahoiv.net>
2017-01-26 23:08:35 +0100

    command line

--- homeassistant/components/switch/command_line.py
+++ homeassistant/components/switch/command_line.py
@@ -124,8 +124,7 @@ class CommandSwitch(SwitchDevice):
     @property
     def assumed_state(self):
         """Return true if we do optimistic updates."""
-        return (self._command_state is False or
-                self._command_state is None)
+        return self._command_state is None
 
     def _query_state(self):
         """Query for state."""
