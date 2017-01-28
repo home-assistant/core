d07f9f14c36de4de60e0787942e836602e711a2c
Johan Bloemberg <github@ijohan.nl>
2017-01-28 01:51:56 +0100

    Use rflink that allows send_command_ack to be safely cancelled.

--- homeassistant/components/rflink.py
+++ homeassistant/components/rflink.py
@@ -36,7 +36,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 import voluptuous as vol
 
-REQUIREMENTS = ['rflink==0.0.18']
+REQUIREMENTS = ['rflink==0.0.19']
 
 DOMAIN = 'rflink'
 
