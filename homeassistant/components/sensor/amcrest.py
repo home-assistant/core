a31dc741781a93b02da1fdc3d559642b9c423d3f
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-01-31 03:19:49 -0500

    Fixes issue #5627 by bumping external Amcrest module to version 1.1.4 (#5662)
    Add an optional extended description…

--- homeassistant/components/sensor/amcrest.py
+++ homeassistant/components/sensor/amcrest.py
@@ -20,7 +20,7 @@ import homeassistant.loader as loader
 
 from requests.exceptions import HTTPError, ConnectTimeout
 
-REQUIREMENTS = ['amcrest==1.1.3']
+REQUIREMENTS = ['amcrest==1.1.4']
 
 _LOGGER = logging.getLogger(__name__)
 
