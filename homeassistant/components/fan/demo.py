7132dd4ede24f28d12c34208287b8c284e91d817
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-02-02 21:07:00 +0100

    Migrate fan component to async. (#5723)
    * Migrate fan component to async.
    
    * Fix lint

--- homeassistant/components/fan/demo.py
+++ homeassistant/components/fan/demo.py
@@ -56,8 +56,10 @@ class DemoFan(FanEntity):
         """Get the list of available speeds."""
         return [STATE_OFF, SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH]
 
-    def turn_on(self, speed: str=SPEED_MEDIUM) -> None:
+    def turn_on(self, speed: str=None) -> None:
         """Turn on the entity."""
+        if speed is None:
+            speed = SPEED_MEDIUM
         self.set_speed(speed)
 
     def turn_off(self) -> None:
@@ -68,17 +70,17 @@ class DemoFan(FanEntity):
     def set_speed(self, speed: str) -> None:
         """Set the speed of the fan."""
         self._speed = speed
-        self.update_ha_state()
+        self.schedule_update_ha_state()
 
     def set_direction(self, direction: str) -> None:
         """Set the direction of the fan."""
         self.direction = direction
-        self.update_ha_state()
+        self.schedule_update_ha_state()
 
     def oscillate(self, oscillating: bool) -> None:
         """Set oscillation."""
         self.oscillating = oscillating
-        self.update_ha_state()
+        self.schedule_update_ha_state()
 
     @property
     def current_direction(self) -> str:
