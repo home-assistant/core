82c75bb4a20bae29c588707a13062be4a5dddd37
Philipp Schmitt <philipp@schmitt.co>
2017-02-02 12:58:51 +0100

    Make sure there is no IO inside properties

--- homeassistant/components/lock/nuki.py
+++ homeassistant/components/lock/nuki.py
@@ -45,21 +45,25 @@ class NukiLock(LockDevice):
     def __init__(self, nuki_lock):
         """Initialize the lock."""
         self._nuki_lock = nuki_lock
+        self._locked = None
+        self._name = None
 
     @property
     def name(self):
         """Return the name of the lock."""
-        return self._nuki_lock.name
+        return self._name
 
     @property
     def is_locked(self):
         """Return true if lock is locked."""
-        return self._nuki_lock.is_locked
+        return self._locked
 
     @Throttle(MIN_TIME_BETWEEN_SCANS, MIN_TIME_BETWEEN_FORCED_SCANS)
     def update(self):
         """Update the nuki lock properties."""
         self._nuki_lock.update()
+        self._name = self._nuki_lock.name
+        self._locked = self._nuki_lock.is_locked
 
     def lock(self, **kwargs):
         """Lock the device."""
