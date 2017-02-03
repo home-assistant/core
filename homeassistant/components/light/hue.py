f30c5a6a0a8d95b3df4e834bd4476b33029f9ace
Trevor <tboyce021@gmail.com>
2017-02-03 12:13:59 -0600

    Revert "Fix hue lightgroups not syncing state (#5702)"

--- homeassistant/components/light/hue.py
+++ homeassistant/components/light/hue.py
@@ -200,8 +200,7 @@ def setup_bridge(host, hass, add_devices, filename, allow_unreachable,
 
         for light_id, info in api_lights.items():
             if light_id not in lights:
-                lights[light_id] = HueLight(hass,
-                                            int(light_id), info,
+                lights[light_id] = HueLight(int(light_id), info,
                                             bridge, update_lights,
                                             bridge_type, allow_unreachable,
                                             allow_in_emulated_hue)
@@ -219,7 +218,6 @@ def setup_bridge(host, hass, add_devices, filename, allow_unreachable,
 
             if lightgroup_id not in lightgroups:
                 lightgroups[lightgroup_id] = HueLight(
-                    hass,
                     int(lightgroup_id), info, bridge, update_lights,
                     bridge_type, allow_unreachable, allow_in_emulated_hue,
                     True)
@@ -282,11 +280,10 @@ def request_configuration(host, hass, add_devices, filename,
 class HueLight(Light):
     """Representation of a Hue light."""
 
-    def __init__(self, hass, light_id, info, bridge, update_lights,
+    def __init__(self, light_id, info, bridge, update_lights,
                  bridge_type, allow_unreachable, allow_in_emulated_hue,
                  is_group=False):
         """Initialize the light."""
-        self.hass = hass
         self.light_id = light_id
         self.info = info
         self.bridge = bridge
