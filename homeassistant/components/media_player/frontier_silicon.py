b70b858ac943c458de05d20edc5b3ed06a19dc9d
zhelev <krasimir.zhelev@fau.de>
2017-02-05 16:19:24 +0100

    Removing white space from an empty line

--- homeassistant/components/media_player/frontier_silicon.py
+++ homeassistant/components/media_player/frontier_silicon.py
@@ -91,7 +91,7 @@ class FrontierSiliconDevice(MediaPlayerDevice):
     def get_fs(self):
         """Create a fsapi session."""
         from fsapi import FSAPI
-        
+
         return FSAPI(self._device_url, self._password)
 
     # Properties
