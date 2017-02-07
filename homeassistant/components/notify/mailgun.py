a1d6e1eed53d0899a71af2996ebbe35d7ef494d4
Philipp Schmitt <philipp@schmitt.co>
2017-02-07 11:02:03 +0100

    Update dependency to version 1.3
    - The provided credentials (including the domain) are now checked during
    startup, as requested by @balloob
    - The domain name is now optional
    - There's a new config item "sandbox" which indicates whether to use the
    sandboxed domain in case the domain is not set

--- homeassistant/components/notify/mailgun.py
+++ homeassistant/components/notify/mailgun.py
@@ -17,53 +17,93 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 REQUIREMENTS = ['https://github.com/pschmitt/pymailgun/'
-                'archive/1.2.zip#'
-                'pymailgun==1.2']
+                'archive/1.3.zip#'
+                'pymailgun==1.3']
 
 # Images to attach to notification
 ATTR_IMAGES = 'images'
 
+# Configuration item for the domain to use.
+CONF_SANDBOX = 'sandbox'
+
 # Default sender name
 DEFAULT_SENDER = 'hass@{domain}'
+# Default sandbox value
+DEFAULT_SANDBOX = False
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_DOMAIN): cv.string,
     vol.Required(CONF_TOKEN): cv.string,
-    vol.Optional(CONF_SENDER): vol.Email(),
     vol.Required(CONF_RECIPIENT): vol.Email(),
+    vol.Optional(CONF_DOMAIN): cv.string,
+    vol.Optional(CONF_SENDER): vol.Email(),
+    vol.Optional(CONF_SANDBOX, default=DEFAULT_SANDBOX): cv.boolean,
 })
 
 
 def get_service(hass, config, discovery_info=None):
     """Get the Mailgun notification service."""
-    return MailgunNotificationService(config.get(CONF_DOMAIN),
-                                      config.get(CONF_TOKEN),
-                                      config.get(CONF_SENDER),
-                                      config.get(CONF_RECIPIENT))
+    mailgun_service = MailgunNotificationService(config.get(CONF_DOMAIN),
+                                                 config.get(CONF_SANDBOX),
+                                                 config.get(CONF_TOKEN),
+                                                 config.get(CONF_SENDER),
+                                                 config.get(CONF_RECIPIENT))
+    if mailgun_service.connection_is_valid():
+        return mailgun_service
+    else:
+        return None
 
 
 class MailgunNotificationService(BaseNotificationService):
     """Implement a notification service for the Mailgun mail service."""
 
-    def __init__(self, domain, token, sender, recipient):
+    def __init__(self, domain, sandbox, token, sender, recipient):
         """Initialize the service."""
-        from pymailgun import Client
-        self._sender = sender if sender is not None \
-                              else DEFAULT_SENDER.format(domain=domain)
+        self._client = None  # Mailgun API client
+        self._domain = domain
+        self._sandbox = sandbox
+        self._token = token
+        self._sender = sender
         self._recipient = recipient
-        self._client = Client(token, domain)
+
+    def initialize_client(self):
+        """ Initialize the connection to Mailgun. """
+        from pymailgun import Client
+        self._client = Client(self._token, self._domain, self._sandbox)
+        _LOGGER.info('MAILGUN SANDBOX: {}'.format(self._sandbox))
+        _LOGGER.info('MAILGUN DOMAIN: {}'.format(self._client.domain))
+        self._domain = self._client.domain
+        if not self._sender:
+            self._sender = DEFAULT_SENDER.format(domain=self._domain)
+
+    def connection_is_valid(self):
+        """ Check whether the provided credentials are valid. """
+        from pymailgun import (MailgunCredentialsError, MailgunDomainError)
+        try:
+            self.initialize_client()
+        except MailgunCredentialsError:
+            _LOGGER.exception('Invalid credentials')
+            return False
+        except MailgunDomainError as e:
+            _LOGGER.exception(e)
+            return False
+        return True
 
     def send_message(self, message="", **kwargs):
         """Send a mail to the recipient."""
+        from pymailgun import MailgunError
         subject = kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT)
         data = kwargs.get(ATTR_DATA)
         files = data.get(ATTR_IMAGES) if data else None
 
-        resp = self._client.send_mail(sender=self._sender, to=self._recipient,
-                                      subject=subject, text=message,
-                                      files=files)
-        if not resp.ok:
-            _LOGGER.error('Failed to send message: '
-                          '[{}] {}'.format(resp.status_code, resp.json()))
-        else:
-            _LOGGER.debug('Message sent: {}'.format(resp.json()))
+        try:
+            # Initialize the client in case it was not.
+            if self._client is None:
+                self.initialize_client()
+            resp = self._client.send_mail(sender=self._sender,
+                                          to=self._recipient,
+                                          subject=subject,
+                                          text=message,
+                                          files=files)
+            _LOGGER.debug('Message sent: {}'.format(resp))
+        except MailgunError as e:
+            _LOGGER.exception('Failed to send message: {}'.format(e))
