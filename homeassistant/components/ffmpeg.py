bc35cf0716b169d2f871b7fcfa7510abdd6cff84
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-02-08 21:57:15 +0100

    Pump ffmpeg version

--- homeassistant/components/ffmpeg.py
+++ homeassistant/components/ffmpeg.py
@@ -18,7 +18,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
 DOMAIN = 'ffmpeg'
-REQUIREMENTS = ["ha-ffmpeg==1.2"]
+REQUIREMENTS = ["ha-ffmpeg==1.3"]
 
 _LOGGER = logging.getLogger(__name__)
 
