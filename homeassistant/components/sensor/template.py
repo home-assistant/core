cbfd555053d96e0be3c4c8c2e6f8a691723344d0
Daniel Hoyer Iversen <Daniel.H.Iversen@ntnu.no>
2017-02-08 16:13:47 +0100

    improve error message when template is none

--- homeassistant/components/sensor/template.py
+++ homeassistant/components/sensor/template.py
@@ -144,9 +144,9 @@ class SensorTemplate(Entity):
                 if ex.args and ex.args[0].startswith(
                         "UndefinedError: 'None' has no attribute"):
                     # Common during HA startup - so just a warning
-                    _LOGGER.warning('Could not render template %s,'
+                    _LOGGER.warning('Could not render icon template %s,'
                                     ' the state is unknown.', self._name)
                     return
                 self._icon = super().icon
-                _LOGGER.error('Could not render template %s: %s',
+                _LOGGER.error('Could not render icon template %s: %s',
                               self._name, ex)
