8f6019a7f57cd492e4f1ca6cbec8de5da04bb13c
apo-mak <apo-mak@users.noreply.github.com>
2017-02-12 09:06:37 +0200

    Add files via upload

--- homeassistant/components/c_Elta.py
+++ homeassistant/components/c_Elta.py
@@ -16,8 +16,8 @@ def tracking(str):
         result = ' the ', tracking_number,' dose not seem like an ELTA tracking number...'
         _LOGGER.error('Invalid key {0}. Key must be 13 characters'.format(tracking_number))
     else:
-        headers = {'Cookie':'lang=en'}
-        r = requests.post('http://www.elta-courier.gr/track.php', data = {'number': tracking_number}, headers=headers)
+        headers = {'Cookie': 'lang=en'}
+        r = requests.post('http://www.elta-courier.gr/track.php', data={'number': tracking_number}, headers=headers)
         t_result = r.json()['result'][tracking_number]['result']
         if t_result != 'wrong number':
             p_date = t_result[-1]['date']
