959dfa532bf03540b6ae0b925a379379d4530e62
jumpkick <ironstorm@gmail.com>
2017-02-12 14:51:02 -0500

    Requires qnapstats 0.2.2
    verify_ssl was added to 0.2.2
    better error handling will happen in 0.2.3+

--- homeassistant/components/sensor/qnap.py
+++ homeassistant/components/sensor/qnap.py
@@ -18,7 +18,7 @@ import homeassistant.helpers.config_validation as cv
 
 import voluptuous as vol
 
-REQUIREMENTS = ['qnapstats==0.2.1']
+REQUIREMENTS = ['qnapstats>=0.2.2']
 
 _LOGGER = logging.getLogger(__name__)
 
