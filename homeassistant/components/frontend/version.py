77f83684d3e5935a189268bd2a2ade786c69982d
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-02-12 11:35:36 -0800

    Update frontend (#5923)

--- homeassistant/components/frontend/version.py
+++ homeassistant/components/frontend/version.py
@@ -2,18 +2,18 @@
 
 FINGERPRINTS = {
     "core.js": "adfeb513cf650acf763e284d76a48d6b",
-    "frontend.html": "cfd75c944ab14912cfbb4fdd9027579c",
+    "frontend.html": "addef1d5c8990c9443e97adefd19cc63",
     "mdi.html": "c1dde43ccf5667f687c418fc8daf9668",
     "micromarkdown-js.html": "93b5ec4016f0bba585521cf4d18dec1a",
-    "panels/ha-panel-config.html": "fb50a25da4b4cfbb0d9a74bb22a31db9",
+    "panels/ha-panel-config.html": "e669c4c0b00eeddd8e05afcac11525fe",
     "panels/ha-panel-dev-event.html": "5c82300b3cf543a92cf4297506e450e7",
-    "panels/ha-panel-dev-info.html": "0469024d94d6270a8680df2be44ba916",
+    "panels/ha-panel-dev-info.html": "1e305a9af8fd6e7be77f3ddf215c336f",
     "panels/ha-panel-dev-service.html": "9f749635e518a4ca7991975bdefdb10a",
     "panels/ha-panel-dev-state.html": "7d069ba8fd5379fa8f59858b8c0a7473",
     "panels/ha-panel-dev-template.html": "2b618508510afa5281c9ecae0c3a3dbd",
     "panels/ha-panel-history.html": "8955c1d093a2c417c89ed90dd627c7d3",
     "panels/ha-panel-iframe.html": "d920f0aa3c903680f2f8795e2255daab",
-    "panels/ha-panel-logbook.html": "f36297a894524518fa70883f264492b0",
+    "panels/ha-panel-logbook.html": "7eb06cf9fdeab6683bcd755276f571aa",
     "panels/ha-panel-map.html": "9c8c7924ba8f731560c9f4093835cc26",
     "websocket_test.html": "575de64b431fe11c3785bf96d7813450"
 }
