b6f51f5a3be0d01d648f685e5d8567b95c1fe384
tuxinger <tuxinger@gmail.com>
2017-02-12 17:18:33 +0000

    fixed formating and naming

--- homeassistant/components/media_player/mqtt.py
+++ homeassistant/components/media_player/mqtt.py
@@ -3,8 +3,9 @@ Support for interfacing with MQTT enabled Media Players.
 
 Configuration:
 
-To use the media_player/mqtt component you will need to adapt (should be more or less self explaining)
-the following configuration and add it to your configuration.yaml file.
+To use the media_player/mqtt component you will need to adapt (should be
+more or less self explaining) the following configuration and add it to
+your configuration.yaml file.
 
 media_player:
   - platform: mqtt
@@ -54,8 +55,11 @@ from homeassistant.components.media_player import (
     SUPPORT_VOLUME_STEP, SUPPORT_SELECT_SOURCE, MediaPlayerDevice,
     PLATFORM_SCHEMA)
 from homeassistant.const import (
-    CONF_NAME, STATE_OFF, STATE_ON, STATE_IDLE, STATE_PAUSED, STATE_PLAYING, STATE_STANDBY,
-    STATE_UNKNOWN,)
+    CONF_NAME,
+    STATE_OFF,
+    STATE_ON,
+    STATE_UNKNOWN,
+)
 import homeassistant.components.mqtt as mqtt
 import homeassistant.helpers.config_validation as cv
 
@@ -68,7 +72,7 @@ DEFAULT_MIN_VOLUME = 0
 DEFAULT_MAX_VOLUME = 1
 
 SUPPORT_MQTT_MEDIA = SUPPORT_VOLUME_STEP | SUPPORT_VOLUME_MUTE | \
-    SUPPORT_TURN_ON | SUPPORT_TURN_OFF  | SUPPORT_VOLUME_SET | \
+    SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_VOLUME_SET | \
     SUPPORT_SELECT_SOURCE
 
 CONF_MIN_VOLUME = 'min_volume'
@@ -77,8 +81,6 @@ CONF_SOURCE_DICT = 'sources'
 CONF_POWER_DICT = 'power'
 CONF_VOL_DICT = 'volume'
 CONF_MUTE_DICT = 'mute'
-
-
 
 
 SOURCE_DICT_SCHEMA = vol.Schema({
@@ -122,7 +124,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the MQTT_MEDIA platform."""
-    add_devices([MQTT_MEDIA(
+    add_devices([MQTTMedia(
         hass,
         config.get(CONF_NAME),
         config.get(CONF_MIN_VOLUME),
@@ -134,10 +136,19 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     )])
 
 
-class MQTT_MEDIA(MediaPlayerDevice):
+class MQTTMedia(MediaPlayerDevice):
     """Representation of a MQTT_MEDIA Receiver."""
 
-    def __init__(self, hass, name, min_volume, max_volume, power_dict, vol_dict, mute_dict, source_dict):
+    def __init__(
+            self,
+            hass,
+            name,
+            min_volume,
+            max_volume,
+            power_dict,
+            vol_dict,
+            mute_dict,
+            source_dict):
         """Initialize the MQTT_MEDIA Receiver device."""
 
         self._name = name
@@ -151,17 +162,32 @@ class MQTT_MEDIA(MediaPlayerDevice):
         self._state = STATE_UNKNOWN
         self._mute = STATE_OFF
         self._source = None
-        self._payload_off="OFF"
-        self._payload_on="ON"
-        self._retain=False
+        self._payload_off = "OFF"
+        self._payload_on = "ON"
+        self._retain = False
 
-        self._qos=0
+        self._qos = 0
 
-        mqtt.subscribe(hass, self._source_dict["status_topic"], self.message_source_selected, self._qos)
-        mqtt.subscribe(hass, self._power_dict["status_topic"], self.message_power_changed, self._qos)
-        mqtt.subscribe(hass, self._vol_dict["status_topic"], self.message_volume_changed, self._qos)
-        mqtt.subscribe(hass, self._mute_dict["status_topic"], self.message_mute_changed, self._qos)
-
+        mqtt.subscribe(
+            hass,
+            self._source_dict["status_topic"],
+            self.message_source_selected,
+            self._qos)
+        mqtt.subscribe(
+            hass,
+            self._power_dict["status_topic"],
+            self.message_power_changed,
+            self._qos)
+        mqtt.subscribe(
+            hass,
+            self._vol_dict["status_topic"],
+            self.message_volume_changed,
+            self._qos)
+        mqtt.subscribe(
+            hass,
+            self._mute_dict["status_topic"],
+            self.message_mute_changed,
+            self._qos)
 
     def message_source_selected(self, topic, payload, qos):
         """A new MQTT input select message has been received."""
@@ -171,24 +197,23 @@ class MQTT_MEDIA(MediaPlayerDevice):
     def message_power_changed(self, topic, payload, qos):
         """A new MQTT power message has been received."""
         if payload.upper() == "ON":
-            self._state=STATE_ON
+            self._state = STATE_ON
         if payload.upper() == "OFF":
-            self._state=STATE_OFF
+            self._state = STATE_OFF
         self.update_ha_state()
 
     def message_volume_changed(self, topic, payload, qos):
         """A new MQTT volume message has been received."""
-        vol=abs(self._min_volume - int(payload)) / abs(self._min_volume - self._max_volume)
-        self._volume=vol
+        self._volume = abs(self._min_volume - int(payload)) / \
+            abs(self._min_volume - self._max_volume)
         self.update_ha_state()
-
 
     def message_mute_changed(self, topic, payload, qos):
         """A new MQTT mute message has been received."""
         if payload.upper() == "ON":
-            self._mute=True
+            self._mute = True
         if payload.upper() == "OFF":
-            self._mute=False
+            self._mute = False
         self.update_ha_state()
 
     @property
@@ -226,29 +251,59 @@ class MQTT_MEDIA(MediaPlayerDevice):
 
     def turn_off(self):
         """Turn the media player off."""
-        mqtt.publish(self.hass, self._power_dict["command_topic"], self._power_dict["payload_off"], self._power_dict["qos"], self._power_dict["retain"])
+        mqtt.publish(
+            self.hass,
+            self._power_dict["command_topic"],
+            self._power_dict["payload_off"],
+            self._power_dict["qos"],
+            self._power_dict["retain"])
 
     def turn_on(self):
         """Turn the media player on."""
-        mqtt.publish(self.hass, self._power_dict["command_topic"], self._power_dict["payload_on"], self._power_dict["qos"], self._power_dict["retain"])
+        mqtt.publish(
+            self.hass,
+            self._power_dict["command_topic"],
+            self._power_dict["payload_on"],
+            self._power_dict["qos"],
+            self._power_dict["retain"])
 
     def volume_up(self):
         """Volume up the media player."""
-        mqtt.publish(self.hass, self._vol_dict["command_up_topic"], self._vol_dict["payload"], self._vol_dict["qos"], self._vol_dict["retain"])
+        mqtt.publish(
+            self.hass,
+            self._vol_dict["command_up_topic"],
+            self._vol_dict["payload"],
+            self._vol_dict["qos"],
+            self._vol_dict["retain"])
 
     def volume_down(self):
         """Volume down the media player."""
-        mqtt.publish(self.hass, self._vol_dict["command_down_topic"], self._vol_dict["payload"], self._vol_dict["qos"], self._vol_dict["retain"])
+        mqtt.publish(
+            self.hass,
+            self._vol_dict["command_down_topic"],
+            self._vol_dict["payload"],
+            self._vol_dict["qos"],
+            self._vol_dict["retain"])
 
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
-        vol=self._min_volume + round(abs(self._min_volume - self._max_volume) * volume)
-        mqtt.publish(self.hass, self._vol_dict["command_set_topic"], vol, self._vol_dict["qos"], self._vol_dict["retain"])
+        vol_val = self._min_volume + \
+            round(abs(self._min_volume - self._max_volume) * volume)
+        mqtt.publish(
+            self.hass,
+            self._vol_dict["command_set_topic"],
+            vol_val,
+            self._vol_dict["qos"],
+            self._vol_dict["retain"])
 
     def select_source(self, source):
         """Select input source."""
-        mqtt.publish(self.hass, self._source_dict["command_topic"], source, self._source_dict["qos"], self._source_dict["retain"])
-        
+        mqtt.publish(
+            self.hass,
+            self._source_dict["command_topic"],
+            source,
+            self._source_dict["qos"],
+            self._source_dict["retain"])
 
     @property
     def source(self):
@@ -263,6 +318,16 @@ class MQTT_MEDIA(MediaPlayerDevice):
     def mute_volume(self, mute):
         """Mute (true) or unmute (false) media player."""
         if self._mute:
-            mqtt.publish(self.hass, self._mute_dict["command_topic"], self._mute_dict["payload_on"], self._mute_dict["qos"], self._mute_dict["retain"])
+            mqtt.publish(
+                self.hass,
+                self._mute_dict["command_topic"],
+                self._mute_dict["payload_on"],
+                self._mute_dict["qos"],
+                self._mute_dict["retain"])
         else:
-            mqtt.publish(self.hass, self._mute_dict["command_topic"], self._mute_dict["payload_off"], self._mute_dict["qos"], self._mute_dict["retain"])
+            mqtt.publish(
+                self.hass,
+                self._mute_dict["command_topic"],
+                self._mute_dict["payload_off"],
+                self._mute_dict["qos"],
+                self._mute_dict["retain"])
