3c31bbb79a98a5f023710c469ac9c023f5db6365
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-02-12 21:33:13 -0800

    Add Z-Wave config panel

--- homeassistant/components/config/__init__.py
+++ homeassistant/components/config/__init__.py
@@ -1,12 +1,16 @@
 """Component to interact with Hassbian tools."""
 import asyncio
 
-from homeassistant.bootstrap import async_prepare_setup_platform
+from homeassistant.core import callback
+from homeassistant.const import EVENT_COMPONENT_LOADED
+from homeassistant.bootstrap import (
+    async_prepare_setup_platform, ATTR_COMPONENT)
 from homeassistant.components.frontend import register_built_in_panel
 
 DOMAIN = 'config'
 DEPENDENCIES = ['http']
 SECTIONS = ('core', 'hassbian')
+ON_DEMAND = ('zwave', )
 
 
 @asyncio.coroutine
@@ -14,16 +18,39 @@ def async_setup(hass, config):
     """Setup the hassbian component."""
     register_built_in_panel(hass, 'config', 'Configuration', 'mdi:settings')
 
-    for panel_name in SECTIONS:
+    @asyncio.coroutine
+    def setup_panel(panel_name):
+        """Setup a panel."""
         panel = yield from async_prepare_setup_platform(hass, config, DOMAIN,
                                                         panel_name)
 
         if not panel:
-            continue
+            return
 
         success = yield from panel.async_setup(hass)
 
         if success:
-            hass.config.components.add('{}.{}'.format(DOMAIN, panel_name))
+            key = '{}.{}'.format(DOMAIN, panel_name)
+            hass.bus.async_fire(EVENT_COMPONENT_LOADED, {ATTR_COMPONENT: key})
+            hass.config.components.add(key)
+
+    tasks = [setup_panel(panel_name) for panel_name in SECTIONS]
+
+    for panel_name in ON_DEMAND:
+        if panel_name in hass.config.components:
+            tasks.append(setup_panel(panel_name))
+
+    if tasks:
+        yield from asyncio.wait(tasks, loop=hass.loop)
+
+    @callback
+    def component_loaded(event):
+        """Respond to components being loaded."""
+        panel_name = event.data.get(ATTR_COMPONENT)
+
+        if panel_name in ON_DEMAND:
+            setup_panel(panel_name)
+
+    hass.bus.async_listen(EVENT_COMPONENT_LOADED, component_loaded)
 
     return True
---
+++ homeassistant/components/config/zwave.py
@@ -0,0 +1,76 @@
+"""Component to interact with Hassbian tools."""
+import asyncio
+import os
+import voluptuous as vol
+import yaml
+
+from homeassistant.components.http import HomeAssistantView
+from homeassistant.components.zwave import _ZWAVE_CUSTOMIZE_SCHEMA_ENTRY
+from homeassistant.util.yaml import load_yaml
+
+
+DEVICE_CONFIG = 'zwave_device_config.yml'
+
+
+@asyncio.coroutine
+def async_setup(hass):
+    """Setup the hassbian config."""
+    hass.http.register_view(DeviceConfigView)
+    return True
+
+
+class DeviceConfigView(HomeAssistantView):
+    """Hassbian packages endpoint."""
+
+    url = '/api/config/zwave/device_config/{entity_id}'
+    name = 'api:config:zwave:device_config:update'
+
+    @asyncio.coroutine
+    def get(self, request, entity_id):
+        """Fetch device specific config."""
+        hass = request.app['hass']
+        current = yield from hass.loop.run_in_executor(
+            None, _read, hass.config.path(DEVICE_CONFIG))
+        return self.json(current.get(entity_id, {}))
+
+    @asyncio.coroutine
+    def post(self, request, entity_id):
+        """Validate config and return results."""
+        try:
+            data = yield from request.json()
+        except ValueError:
+            return self.json_message('Invalid JSON specified', 400)
+
+        try:
+            data = _ZWAVE_CUSTOMIZE_SCHEMA_ENTRY(data)
+        except vol.Invalid as err:
+            return self.json_message('Message malformed: {}'.format(err), 400)
+
+        hass = request.app['hass']
+        path = hass.config.path(DEVICE_CONFIG)
+        current = yield from hass.loop.run_in_executor(
+            None, _read, hass.config.path(DEVICE_CONFIG))
+        current.setdefault(entity_id, {}).update(data)
+
+        yield from hass.loop.run_in_executor(
+            None, _write, hass.config.path(path), current)
+
+        return self.json({
+            'result': 'ok',
+        })
+
+
+def _read(path):
+    """Read YAML helper."""
+    if not os.path.isfile(path):
+        with open(path, 'w'):
+            pass
+        return {}
+
+    return load_yaml(path)
+
+
+def _write(path, data):
+    """Write YAML helper."""
+    with open(path, 'w') as outfile:
+        yaml.safe_dump(data, outfile, default_flow_style=False)
