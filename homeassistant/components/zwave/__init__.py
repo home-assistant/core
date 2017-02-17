6948041b8d3bdb3551f9c02de5097722573de3e0
Andrey <andrey-git@users.noreply.github.com>
2017-02-17 22:03:55 +0200

    Add 'print_node' service to zwave (#6069)
    * add print
    
    * Add 'print_node' service to zwave.

--- homeassistant/components/zwave/__init__.py
+++ homeassistant/components/zwave/__init__.py
@@ -150,6 +150,10 @@ PRINT_CONFIG_PARAMETER_SCHEMA = vol.Schema({
     vol.Required(const.ATTR_CONFIG_PARAMETER): vol.Coerce(int),
 })
 
+PRINT_NODE_SCHEMA = vol.Schema({
+    vol.Required(const.ATTR_NODE_ID): vol.Coerce(int),
+})
+
 CHANGE_ASSOCIATION_SCHEMA = vol.Schema({
     vol.Required(const.ATTR_ASSOCIATION): cv.string,
     vol.Required(const.ATTR_NODE_ID): vol.Coerce(int),
@@ -543,6 +547,12 @@ def setup(hass, config):
         _LOGGER.info("Config parameter %s on Node %s : %s",
                      param, node_id, get_config_value(node, param))
 
+    def print_node(service):
+        """Print all information about z-wave node."""
+        node_id = service.data.get(const.ATTR_NODE_ID)
+        node = NETWORK.nodes[node_id]
+        nice_print_node(node)
+
     def set_wakeup(service):
         """Set wake-up interval of a node."""
         node_id = service.data.get(const.ATTR_NODE_ID)
@@ -656,6 +666,11 @@ def setup(hass, config):
                                descriptions[
                                    const.SERVICE_SET_WAKEUP],
                                schema=SET_WAKEUP_SCHEMA)
+        hass.services.register(DOMAIN, const.SERVICE_PRINT_NODE,
+                               print_node,
+                               descriptions[
+                                   const.SERVICE_PRINT_NODE],
+                               schema=PRINT_NODE_SCHEMA)
 
     # Setup autoheal
     if autoheal:
