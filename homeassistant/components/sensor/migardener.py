c69a212ba35c7823133550e41755c7a0a6b88488
Christian Kühnel <christian.kuehnel@gmail.com>
2017-02-18 15:24:06 +0100

    first implementation of data reading via MQTT and a simulator to provide test data

--- homeassistant/components/sensor/migardener.py
+++ homeassistant/components/sensor/migardener.py
@@ -1,164 +1,67 @@
-"""Sensor for monitoring plants with Xiaomi Mi Plant sensors.
-
-To read the sensor data and send it via MQTT,
-see https://github.com/ChristianKuehnel/plantgateway
+"""
+Sensor for monitoring plants with Xiaomi Mi Plant sensors
 """
 
-import json
+#import logging
 import voluptuous as vol
 from homeassistant.const import (
-    CONF_PLATFORM, CONF_NAME, STATE_UNKNOWN, ATTR_BATTERY_LEVEL,
-    TEMP_CELSIUS, ATTR_TEMPERATURE, ATTR_SERVICE,
-    ATTR_UNIT_OF_MEASUREMENT, ATTR_ICON)
+    CONF_PLATFORM, CONF_NAME, STATE_UNKNOWN, CONF_UNIT_OF_MEASUREMENT, ATTR_BATTERY_LEVEL, TEMP_CELSIUS, ATTR_TEMPERATURE)
 import homeassistant.components.mqtt as mqtt
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.mqtt import CONF_STATE_TOPIC
 from homeassistant.helpers.entity import Entity
 from homeassistant.core import callback
 
-
 DEFAULT_NAME = 'MiGardener'
 DEPENDENCIES = ['mqtt']
-
-READING_BATTERY = 'battery'
-READING_TEMPERATURE = ATTR_TEMPERATURE
-READING_MOISTURE = 'moisture'
-READING_CONDUCTIVITY = 'conductivity'
-READING_BRIGHTNESS = 'brightness'
-CAST_FUNCTION = 'cast_function'
-
-CONF_MIN_BATTERY_LEVEL = 'min_' + READING_BATTERY
-CONF_MIN_TEMPERATURE = 'min_' + READING_TEMPERATURE
-CONF_MAX_TEMPERATURE = 'max_' + READING_TEMPERATURE
-CONF_MIN_MOISTURE = 'min_' + READING_MOISTURE
-CONF_MAX_MOISTURE = 'max_' + READING_MOISTURE
-CONF_MIN_CONDUCTIVITY = 'min_' + READING_CONDUCTIVITY
-CONF_MAX_CONDUCTIVITY = 'max_' + READING_CONDUCTIVITY
-CONF_MIN_BRIGHTNESS = 'min_' + READING_BRIGHTNESS
-CONF_MAX_BRIGHTNESS = 'max_' + READING_BRIGHTNESS
-
 
 PLATFORM_SCHEMA = vol.Schema({
     vol.Required(CONF_PLATFORM): cv.string,
     vol.Required(CONF_NAME): cv.string,
     vol.Required(CONF_STATE_TOPIC): mqtt.valid_subscribe_topic,
-    vol.Optional(ATTR_SERVICE): cv.string,
-    vol.Optional(CONF_MIN_BATTERY_LEVEL): cv.positive_int,
-    vol.Optional(CONF_MIN_TEMPERATURE): cv.small_float,
-    vol.Optional(CONF_MAX_TEMPERATURE): cv.small_float,
-    vol.Optional(CONF_MIN_MOISTURE): cv.positive_int,
-    vol.Optional(CONF_MAX_MOISTURE): cv.positive_int,
-    vol.Optional(CONF_MIN_CONDUCTIVITY): cv.positive_int,
-    vol.Optional(CONF_MAX_CONDUCTIVITY): cv.positive_int,
-    vol.Optional(CONF_MIN_BRIGHTNESS): cv.positive_int,
-    vol.Optional(CONF_MAX_BRIGHTNESS): cv.positive_int,
 })
+
+ATTRIBUTES = (
+    ('battery', ATTR_BATTERY_LEVEL, '%'),
+    ('temperature', ATTR_TEMPERATURE, TEMP_CELSIUS),
+    ('moisture', 'moisture', '%'),
+    ('conductivity', 'conductivity', 'µS/cm'),
+    ('brightness', 'brightness', 'lux'),
+)
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up MiGardener."""
+    """Set up MQTT Sensor."""
     if discovery_info is not None:
         config = PLATFORM_SCHEMA(discovery_info)
 
-    mg = MiGardener(hass, config)
-    add_devices([mg])
+    for attrib in ATTRIBUTES:
+        add_devices([MiGardener(
+            hass,
+            config.get(CONF_NAME),
+            config.get(CONF_STATE_TOPIC),
+            attrib[0],
+            attrib[1],
+            attrib[2],
+        )])
 
 
 class MiGardener(Entity):
-    """Mi Gardener reads measurements from a
-    Xiaomi Mi plant sensor via MQTT.
 
-    It also checks the measurements against
-    configurable min and max values.
-    """
-
-    READINGS = {
-        READING_BATTERY: {
-            ATTR_UNIT_OF_MEASUREMENT:  '%',
-            'min': CONF_MIN_BATTERY_LEVEL,
-            'icon': 'mdi:battery-outline'
-        },
-        READING_TEMPERATURE: {
-            ATTR_UNIT_OF_MEASUREMENT: TEMP_CELSIUS,
-            'min': CONF_MIN_TEMPERATURE,
-            'max': CONF_MAX_TEMPERATURE,
-            'icon': 'mdi:thermometer'
-        },
-        READING_MOISTURE: {
-            ATTR_UNIT_OF_MEASUREMENT: '%',
-            'min': CONF_MIN_MOISTURE,
-            'max': CONF_MAX_MOISTURE,
-            'icon': 'mdi:water'
-        },
-        READING_CONDUCTIVITY: {
-            ATTR_UNIT_OF_MEASUREMENT: 'µS/cm',
-            'min': CONF_MIN_CONDUCTIVITY,
-            'max': CONF_MAX_CONDUCTIVITY,
-            'icon': 'mdi:emoticon-poop'
-        },
-        READING_BRIGHTNESS: {
-            ATTR_UNIT_OF_MEASUREMENT: 'lux',
-            'min': CONF_MIN_BRIGHTNESS,
-            'max': CONF_MAX_BRIGHTNESS,
-            'icon': 'mdi:white-balance-sunny'
-        }
-    }
-
-    def __init__(self, hass, config):
-        """Default constructor."""
-        self._hass = hass
-        self._config = config
+    def __init__(self, hass, device_name, state_topic, mqtt_attrib_name, attrib_name, unit_of_measurement):
         self._state = STATE_UNKNOWN
-        self._name = config[CONF_NAME]
-        self._state_topic = config[CONF_STATE_TOPIC]
-        self._battery = None
-        self._moisture = None
-        self._conductivity = None
-        self._temperature = None
-        self._brightness = None
-        self._icon = None
+        self.hass = hass
+        self._name = '{} {}'.format(device_name, attrib_name)
+        self._state_topic = '{}/{}'.format(state_topic,mqtt_attrib_name)
+        self._unit_of_measurement = unit_of_measurement
 
         @callback
-        def message_received(topic, payload, qos):
+        def message_received(topic, payload, _):
             """A new MQTT message has been received."""
-            data = json.loads(payload)
-            self._update_state(data)
-            self._hass.async_add_job(self.async_update_ha_state())
+            self._state = payload
+            hass.async_add_job(self.async_update_ha_state())
 
         mqtt.subscribe(hass, self._state_topic, message_received)
-
-    def _update_state(self, data):
-        """"Update the state of the class based on the
-        data received via MQTT.
-        """
-
-        self._battery = int(data[READING_BATTERY])
-        self._brightness = int(data[READING_BRIGHTNESS])
-        self._moisture = int(data[READING_MOISTURE])
-        self._conductivity = int(data[READING_CONDUCTIVITY])
-        self._temperature = float(data[READING_TEMPERATURE])
-
-        result = []
-        for sensor_name, params in self.READINGS.items():
-            value = getattr(self, '_{}'.format(sensor_name))
-
-            if 'min' in params and params['min'] in self._config:
-                min_value = self._config[params['min']]
-                if value < min_value:
-                    result.append('{} low'.format(sensor_name))
-                    self._icon = params['icon']
-
-            if 'max' in params and params['max'] in self._config:
-                max_value = self._config[params['max']]
-                if value > max_value:
-                    result.append('{} high'.format(sensor_name))
-                    self._icon = params['icon']
-
-        if len(result) == 0:
-            self._state = 'ok'
-            self._icon = 'mdi:thumb-up'
-        else:
-            self._state = ', '.join(result)
 
     @property
     def should_poll(self):
@@ -171,22 +74,11 @@ class MiGardener(Entity):
         return self._name
 
     @property
+    def unit_of_measurement(self):
+        """Return the unit this state is expressed in."""
+        return self._unit_of_measurement
+
+    @property
     def state(self):
         """Return the state of the entity."""
         return self._state
-
-    @property
-    def state_attributes(self):
-        """provide the individual measurements from the
-        sensor in the attributes of the device.
-        """
-
-        attrib = {
-            ATTR_BATTERY_LEVEL: self._battery,
-            READING_BRIGHTNESS: self._brightness,
-            READING_MOISTURE: self._moisture,
-            READING_CONDUCTIVITY: self._conductivity,
-            READING_TEMPERATURE: self._temperature,
-            ATTR_ICON: self._icon,
-        }
-        return attrib
--- tests/components/sensor/migardener_simulator.py
+++ tests/components/sensor/migardener_simulator.py
@@ -1,55 +1,42 @@
-"""Generate test data for migardener the built in MQTT server.
+"""
+generate test data for migardener the built in MQTT server
 
-This program will mock a plant gatway
-(https://github.com/ChristianKuehnel/plantgateway) and send data
-in short intervals to the build-in MQTT server of Home Assistant instance.
-
-It can be used for testing the data input and processing in the
-migardener.py sensor.
 """
 
 import yaml
 import os
 import paho.mqtt.client as mqtt
 import time
-import json
 
 ATTRIBUTES = {
-    """data used in simulating the different measurements of the sensor.
-    first entry: min value
-    second entry: max value
-    third entry: increment
-    """
     'battery': (0, 100, 5),
     'temperature': (-20.0, 30.0, 5.3),
-    'brightness': (0, 10000, 400),
+    'brightness': (0, 10000, 500),
     'moisture': (0, 100, 5),
-    'conductivity': (0, 2000, 300),
+    'conductivity': (0, 2000, 200),
 }
 
 
 def main():
-    """Main method, will mock the plant gateway."""
     config_path = os.path.expanduser('~/.homeassistant/configuration.yaml')
-    with open(config_path, 'r') as config_file:
+    with open(config_path,'r' ) as config_file:
         config = yaml.load(config_file)
     api_password = config['http']['api_password']
 
     client = mqtt.Client()
     client.username_pw_set('homeassistant', api_password)
-    client.connect('localhost', port=1883)
+    client.connect('localhost',port=1883)
     client.loop_start()
 
     values = dict()
-    for name, params in ATTRIBUTES.items():
+    for name,params in ATTRIBUTES.items():
         values[name] = params[0]
 
     while True:
-        for name, params in ATTRIBUTES.items():
+        for name,params in ATTRIBUTES.items():
             values[name] = (values[name] + params[2]) % params[1]
-        json_payload = json.dumps(values)
-        client.publish('test/simulated_plant', json_payload)
-        print('published {}'.format(json_payload))
+            client.publish('test/simulated_plant/{}'.format(name),values[name])
+            print('published {}={}'.format(name,values[name]))
         time.sleep(5)
 
 
