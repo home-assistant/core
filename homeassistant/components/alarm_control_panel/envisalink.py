ff8cc790b70e9ca676548a543420dbe00f098b4d
pvizeli <pvizeli@syshack.ch>
2017-02-22 18:14:24 +0100

    fix lint

--- homeassistant/components/alarm_control_panel/envisalink.py
+++ homeassistant/components/alarm_control_panel/envisalink.py
@@ -100,7 +100,7 @@ class EnvisalinkAlarm(EnvisalinkDevice, alarm.AlarmControlPanel):
             hass, SIGNAL_PARTITION_UPDATE, self._update_callback)
 
     @callback
-    def update_callback(self, partition):
+    def _update_callback(self, partition):
         """Update HA state, if needed."""
         if partition is None or int(partition) == self._partition_number:
             self.hass.async_add_job(self.async_update_ha_state())
