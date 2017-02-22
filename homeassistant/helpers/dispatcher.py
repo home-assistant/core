6b56ed970a6e03da57c825291db472af8ed6c2b1
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-02-22 22:02:19 +0100

    add unittest & threadded functions

--- homeassistant/helpers/dispatcher.py
+++ homeassistant/helpers/dispatcher.py
@@ -5,9 +5,17 @@ from homeassistant.core import callback
 DATA_DISPATCHER = 'dispatcher'
 
 
+def dispatcher_connect(hass, signal, target):
+    """Connect a callable function to a singal."""
+    hass.add_job(async_dispatcher_connect, hass, signal, target)
+
+
 @callback
 def async_dispatcher_connect(hass, signal, target):
-    """Connect a callable function to a singal."""
+    """Connect a callable function to a singal.
+
+    This method must be run in the event loop.
+    """
     if DATA_DISPATCHER not in hass.data:
         hass.data[DATA_DISPATCHER] = {}
 
@@ -17,9 +25,17 @@ def async_dispatcher_connect(hass, signal, target):
     hass.data[DATA_DISPATCHER][signal].append(target)
 
 
+def dispatcher_send(hass, signal, *args):
+    """Send signal and data."""
+    hass.add_job(async_dispatcher_send, hass, signal, *args)
+
+
 @callback
 def async_dispatcher_send(hass, signal, *args):
-    """Send signal and data."""
+    """Send signal and data.
+
+    This method must be run in the event loop.
+    """
     target_list = hass.data.get(DATA_DISPATCHER, {}).get(signal, [])
 
     for target in target_list:
