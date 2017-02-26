9490cb4c8f33bd3da546ca86d2d1d34e8df7c5f7
Pierre Ståhl <pierre.staahl@gmail.com>
2017-02-26 23:15:44 +0100

    Add service to change log levels (#6221)
    * Add service to change log levels
    
    * Fix review comments

--- homeassistant/components/logger.py
+++ homeassistant/components/logger.py
@@ -4,14 +4,21 @@ Component that will help set the level of logging for components.
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/logger/
 """
+import asyncio
 import logging
+import os
 from collections import OrderedDict
 
 import voluptuous as vol
 
+from homeassistant.config import load_yaml_config_file
 import homeassistant.helpers.config_validation as cv
 
 DOMAIN = 'logger'
+
+DATA_LOGGER = 'logger'
+
+SERVICE_SET_LEVEL = 'set_level'
 
 LOGSEVERITY = {
     'CRITICAL': 50,
@@ -29,12 +36,19 @@ LOGGER_LOGS = 'logs'
 
 _VALID_LOG_LEVEL = vol.All(vol.Upper, vol.In(LOGSEVERITY))
 
+SERVICE_SET_LEVEL_SCHEMA = vol.Schema({cv.string: _VALID_LOG_LEVEL})
+
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Optional(LOGGER_DEFAULT): _VALID_LOG_LEVEL,
         vol.Optional(LOGGER_LOGS): vol.Schema({cv.string: _VALID_LOG_LEVEL}),
     }),
 }, extra=vol.ALLOW_EXTRA)
+
+
+def set_level(hass, logs):
+    """Set log level for components."""
+    hass.services.call(DOMAIN, SERVICE_SET_LEVEL, logs)
 
 
 class HomeAssistantLogFilter(logging.Filter):
@@ -61,7 +75,8 @@ class HomeAssistantLogFilter(logging.Filter):
         return record.levelno >= default
 
 
-def setup(hass, config=None):
+@asyncio.coroutine
+def async_setup(hass, config):
     """Setup the logger component."""
     logfilter = {}
 
@@ -72,20 +87,25 @@ def setup(hass, config=None):
             config.get(DOMAIN)[LOGGER_DEFAULT]
         ]
 
-    # Compute log severity for components
-    if LOGGER_LOGS in config.get(DOMAIN):
-        for key, value in config.get(DOMAIN)[LOGGER_LOGS].items():
-            config.get(DOMAIN)[LOGGER_LOGS][key] = LOGSEVERITY[value]
+    def set_log_levels(logpoints):
+        """Set the specified log levels."""
+        logs = {}
 
-        logs = OrderedDict(
+        # Preserve existing logs
+        if LOGGER_LOGS in logfilter:
+            logs.update(logfilter[LOGGER_LOGS])
+
+        # Add new logpoints mapped to correc severity
+        for key, value in logpoints.items():
+            logs[key] = LOGSEVERITY[value]
+
+        logfilter[LOGGER_LOGS] = OrderedDict(
             sorted(
-                config.get(DOMAIN)[LOGGER_LOGS].items(),
+                logs.items(),
                 key=lambda t: len(t[0]),
                 reverse=True
             )
         )
-
-        logfilter[LOGGER_LOGS] = logs
 
     logger = logging.getLogger('')
     logger.setLevel(logging.NOTSET)
@@ -94,5 +114,22 @@ def setup(hass, config=None):
     for handler in logging.root.handlers:
         handler.setLevel(logging.NOTSET)
         handler.addFilter(HomeAssistantLogFilter(logfilter))
+
+    if LOGGER_LOGS in config.get(DOMAIN):
+        set_log_levels(config.get(DOMAIN)[LOGGER_LOGS])
+
+    @asyncio.coroutine
+    def async_service_handler(service):
+        """Handle logger services."""
+        set_log_levels(service.data)
+
+    descriptions = yield from hass.loop.run_in_executor(
+        None, load_yaml_config_file, os.path.join(
+            os.path.dirname(__file__), 'services.yaml'))
+
+    hass.services.async_register(
+        DOMAIN, SERVICE_SET_LEVEL, async_service_handler,
+        descriptions[DOMAIN].get(SERVICE_SET_LEVEL),
+        schema=SERVICE_SET_LEVEL_SCHEMA)
 
     return True
