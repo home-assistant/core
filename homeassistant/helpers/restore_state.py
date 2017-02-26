61909e873f894591419ef25015df8360650886c3
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-02-26 14:38:06 -0800

    Feature/reorg recorder (#6237)
    * Re-organize recorder
    
    * Fix history
    
    * Fix history stats
    
    * Fix restore state
    
    * Lint
    
    * Fix session reconfigure
    
    * Move imports around
    
    * Do not start recording till HASS started
    
    * Lint
    
    * Fix logbook
    
    * Fix race condition recorder init
    
    * Better reporting on errors

--- homeassistant/helpers/restore_state.py
+++ homeassistant/helpers/restore_state.py
@@ -7,7 +7,7 @@ from homeassistant.core import HomeAssistant, CoreState, callback
 from homeassistant.const import EVENT_HOMEASSISTANT_START
 from homeassistant.components.history import get_states, last_recorder_run
 from homeassistant.components.recorder import (
-    async_get_instance, DOMAIN as _RECORDER)
+    wait_connection_ready, DOMAIN as _RECORDER)
 import homeassistant.util.dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
@@ -25,7 +25,7 @@ def _load_restore_cache(hass: HomeAssistant):
 
     hass.bus.listen_once(EVENT_HOMEASSISTANT_START, remove_cache)
 
-    last_run = last_recorder_run()
+    last_run = last_recorder_run(hass)
 
     if last_run is None or last_run.end is None:
         _LOGGER.debug('Not creating cache - no suitable last run found: %s',
@@ -38,7 +38,7 @@ def _load_restore_cache(hass: HomeAssistant):
     last_end_time = last_end_time.replace(tzinfo=dt_util.UTC)
     _LOGGER.debug("Last run: %s - %s", last_run.start, last_end_time)
 
-    states = get_states(last_end_time, run=last_run)
+    states = get_states(hass, last_end_time, run=last_run)
 
     # Cache the states
     hass.data[DATA_RESTORE_CACHE] = {
@@ -58,7 +58,7 @@ def async_get_last_state(hass, entity_id: str):
                       hass.state)
         return None
 
-    yield from async_get_instance()  # Ensure recorder ready
+    yield from wait_connection_ready(hass)
 
     if _LOCK not in hass.data:
         hass.data[_LOCK] = asyncio.Lock(loop=hass.loop)
