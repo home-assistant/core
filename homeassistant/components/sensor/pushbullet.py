90f4ad34e9508b2a65a864564deee0d1366f4d37
Joakim af Sandeberg <joakim@afsandeberg.se>
2017-02-27 15:13:46 +0000

    Final change by the hound (?)

--- homeassistant/components/sensor/pushbullet.py
+++ homeassistant/components/sensor/pushbullet.py
@@ -35,8 +35,10 @@ SENSOR_TYPES = {
 }
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_MONITORED_CONDITIONS, default=['title', 'body']): vol.All(
-        cv.ensure_list, vol.Length(min=1), [vol.In(SENSOR_TYPES.keys())]),
+    vol.Optional(CONF_MONITORED_CONDITIONS,
+        default=['title', 'body']): vol.All(
+            cv.ensure_list, vol.Length(min=1),
+            [vol.In(SENSOR_TYPES.keys())]),
     vol.Required(CONF_API_KEY): cv.string,
 })
 
