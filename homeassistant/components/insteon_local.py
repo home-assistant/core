7425a2747a6e35bbe00402a3ef6fb207faf03573
Cameron Llewellyn <cameron.b.llewellyn@gmail.com>
2017-03-02 08:27:05 -0600

    Update insteon_local.py

--- homeassistant/components/insteon_local.py
+++ homeassistant/components/insteon_local.py
@@ -13,7 +13,7 @@ from homeassistant.const import (
     CONF_PASSWORD, CONF_USERNAME, CONF_HOST, CONF_PORT, CONF_TIMEOUT)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['insteonlocal==0.46']
+REQUIREMENTS = ['insteonlocal==0.47']
 
 _LOGGER = logging.getLogger(__name__)
 
