ac2c6e9722df4d2a50d1e965bff5581da2a80370
happyleavesaoc <happyleaves.tfr@gmail.com>
2017-03-03 02:14:51 -0500

    twilio component (#6348)
    * twilio component
    
    * add http dependency to twilio
    
    * fire->async_fire

--- homeassistant/components/notify/twilio_call.py
+++ homeassistant/components/notify/twilio_call.py
@@ -9,21 +9,18 @@ import urllib
 
 import voluptuous as vol
 
+from homeassistant.components.twilio import DATA_TWILIO
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.notify import (
     ATTR_TARGET, PLATFORM_SCHEMA, BaseNotificationService)
 
 _LOGGER = logging.getLogger(__name__)
-REQUIREMENTS = ["twilio==5.7.0"]
+DEPENDENCIES = ["twilio"]
 
 
-CONF_ACCOUNT_SID = "account_sid"
-CONF_AUTH_TOKEN = "auth_token"
 CONF_FROM_NUMBER = "from_number"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_ACCOUNT_SID): cv.string,
-    vol.Required(CONF_AUTH_TOKEN): cv.string,
     vol.Required(CONF_FROM_NUMBER):
         vol.All(cv.string, vol.Match(r"^\+?[1-9]\d{1,14}$")),
 })
@@ -31,13 +28,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def get_service(hass, config, discovery_info=None):
     """Get the Twilio Call notification service."""
-    # pylint: disable=import-error
-    from twilio.rest import TwilioRestClient
-
-    twilio_client = TwilioRestClient(config[CONF_ACCOUNT_SID],
-                                     config[CONF_AUTH_TOKEN])
-
-    return TwilioCallNotificationService(twilio_client,
+    return TwilioCallNotificationService(hass.data[DATA_TWILIO],
                                          config[CONF_FROM_NUMBER])
 
 
