e331eb116d22e907ea4854d0968abf444778c182
pvizeli <pvizeli@syshack.ch>
2017-03-09 12:00:50 +0100

    pump version 0.3 / make a lot of improvments

--- homeassistant/components/android_ip_webcam.py
+++ homeassistant/components/android_ip_webcam.py
@@ -12,7 +12,8 @@ import voluptuous as vol
 from homeassistant.core import callback
 from homeassistant.const import (
     CONF_NAME, CONF_HOST, CONF_PORT, CONF_USERNAME, CONF_PASSWORD,
-    CONF_SENSORS, CONF_SWITCHES, CONF_TIMEOUT, CONF_SCAN_INTERVAL)
+    CONF_SENSORS, CONF_SWITCHES, CONF_TIMEOUT, CONF_SCAN_INTERVAL,
+    CONF_PLATFORM)
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers import discovery
 import homeassistant.helpers.config_validation as cv
@@ -21,14 +22,17 @@ from homeassistant.helpers.dispatcher import (
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import async_track_point_in_utc_time
 from homeassistant.util.dt import utcnow
+from homeassistant.components.camera.mjpeg import (
+    CONF_MJPEG_URL, CONF_STILL_IMAGE_URL, DOMAIN as MJPEG_DOMAIN)
 
 DOMAIN = 'android_ip_webcam'
-REQUIREMENTS = ["pydroid-ipcam==0.2"]
+REQUIREMENTS = ["pydroid-ipcam==0.3"]
 
 SCAN_INTERVAL = timedelta(seconds=10)
 
 DATA_IP_WEBCAM = 'android_ip_webcam'
 
+ATTR_HOST = 'host'
 ATTR_VID_CONNS = 'Video Connections'
 ATTR_AUD_CONNS = 'Audio Connections'
 
@@ -119,9 +123,11 @@ SENSORS = ['audio_connections', 'battery_level', 'battery_temp',
 
 SIGNAL_UPDATE_DATA = 'android_ip_webcam_update'
 
+CONF_AUTO_DISCOVERY = 'auto_discovery'
 CONF_MOTION_SENSOR = 'motion_sensor'
 
-DEFAULT_MOTION_SENSOR = True
+DEFAULT_AUTO_DISCOVERY = True
+DEFAULT_MOTION_SENSOR = False
 DEFAULT_NAME = 'IP Webcam'
 DEFAULT_PORT = 8080
 DEFAULT_TIMEOUT = 10
@@ -137,11 +143,14 @@ CONFIG_SCHEMA = vol.Schema({
             cv.time_period,
         vol.Inclusive(CONF_USERNAME, 'authentication'): cv.string,
         vol.Inclusive(CONF_PASSWORD, 'authentication'): cv.string,
-        vol.Optional(CONF_SWITCHES, default=SWITCHES):
+        vol.Optional(CONF_AUTO_DISCOVERY, default=DEFAULT_AUTO_DISCOVERY):
+            cv.boolean,
+        vol.Optional(CONF_SWITCHES, default=[]):
             vol.All(cv.ensure_list, [vol.In(SWITCHES)]),
-        vol.Optional(CONF_SENSORS, default=SENSORS):
+        vol.Optional(CONF_SENSORS, default=[]):
             vol.All(cv.ensure_list, [vol.In(SENSORS)]),
-        vol.Optional(CONF_MOTION_SENSOR, default=DEFAULT_MOTION_SENSOR): bool
+        vol.Optional(CONF_MOTION_SENSOR, default=DEFAULT_MOTION_SENSOR):
+            cv.boolean,
     })])
 }, extra=vol.ALLOW_EXTRA)
 
@@ -162,7 +171,11 @@ def async_setup(hass, config):
         password = cam_config.get(CONF_PASSWORD)
         name = cam_config[CONF_NAME]
         interval = cam_config[CONF_SCAN_INTERVAL]
+        switches = cam_config[CONF_SWITCHES]
+        sensors = cam_config[CONF_SENSORS]
+        motion = cam_config[CONF_MOTION_SENSOR]
 
+        # init ip webcam
         cam = PyDroidIPCam(
             hass.loop, websession, host, cam_config[CONF_PORT],
             username=username, password=password,
@@ -179,12 +192,27 @@ def async_setup(hass, config):
                 hass, async_update_data, utcnow() + interval)
 
         yield from async_update_data(None)
+
+        # use autodiscovery to detect sensors/configs
+        if cam_config[CONF_AUTO_DISCOVERY]:
+            if not cam.available:
+                _LOGGER.error(
+                    "Android webcam %s not found for discovery!", host)
+                return
+
+            sensors = cam.enabled_sensors
+            switches = cam.enabled_settings
+            if 'motion_active' in sensors:
+                sensors.pop('motion_active')
+                motion = True
+
+        # load platforms
         webcams[host] = cam
 
         mjpeg_camera = {
-            'platform': 'mjpeg',
-            'mjpeg_url': cam.mjpeg_url,
-            'still_image_url': cam.image_url,
+            CONF_PLATFORM: MJPEG_DOMAIN,
+            CONF_MJPEG_URL: cam.mjpeg_url,
+            CONF_STILL_IMAGE_URL: cam.image_url,
             CONF_NAME: name,
         }
         if username and password:
@@ -193,13 +221,6 @@ def async_setup(hass, config):
                 CONF_PASSWORD: password
             })
 
-        if cam_config[CONF_MOTION_SENSOR]:
-            hass.async_add_job(discovery.async_load_platform(
-                hass, 'binary_sensor', DOMAIN, {
-                    CONF_HOST: host,
-                    CONF_NAME: name,
-                }, config))
-
         hass.async_add_job(discovery.async_load_platform(
             hass, 'camera', 'mjpeg', mjpeg_camera, config))
 
@@ -207,15 +228,23 @@ def async_setup(hass, config):
             hass, 'sensor', DOMAIN, {
                 CONF_NAME: name,
                 CONF_HOST: host,
-                CONF_SENSORS: cam_config[CONF_SENSORS],
+                CONF_SENSORS: sensors,
             }, config))
 
         hass.async_add_job(discovery.async_load_platform(
             hass, 'switch', DOMAIN, {
                 CONF_NAME: name,
                 CONF_HOST: host,
-                CONF_SWITCHES: cam_config[CONF_SWITCHES],
+                CONF_SWITCHES: switches,
             }, config))
+
+        if motion:
+            hass.async_add_job(discovery.async_load_platform(
+                hass, 'binary_sensor', DOMAIN, {
+                    CONF_HOST: host,
+                    CONF_NAME: name,
+                }, config))
+
 
     tasks = [async_setup_ipcamera(conf) for conf in config[DOMAIN]]
     if tasks:
@@ -258,7 +287,7 @@ class AndroidIPCamEntity(Entity):
     @property
     def device_state_attributes(self):
         """Return the state attributes."""
-        state_attr = {}
+        state_attr = {ATTTR_HOST: self._host}
         if self._ipcam.status_data is None:
             return state_attr
 
@@ -266,6 +295,5 @@ class AndroidIPCamEntity(Entity):
             self._ipcam.status_data.get('video_connections')
         state_attr[ATTR_AUD_CONNS] = \
             self._ipcam.status_data.get('audio_connections')
-        state_attr.update(self._ipcam.current_settings)
 
         return state_attr
