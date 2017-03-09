1b7f89a907553e05ba1eddf9ed9532bd3fa647eb
Robbie Trencheny <me@robbiet.us>
2017-03-08 23:57:54 -0800

    Add required changes to support MQTT discovery that I previously missed. See #6481.

--- homeassistant/components/light/mqtt_json.py
+++ homeassistant/components/light/mqtt_json.py
@@ -72,6 +72,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Setup a MQTT JSON Light."""
+    if discovery_info is not None:
+        config = PLATFORM_SCHEMA(discovery_info)
     async_add_devices([MqttJson(
         config.get(CONF_NAME),
         config.get(CONF_EFFECT_LIST),
--- homeassistant/components/light/mqtt_template.py
+++ homeassistant/components/light/mqtt_template.py
@@ -66,6 +66,9 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Setup a MQTT Template light."""
+    if discovery_info is not None:
+        config = PLATFORM_SCHEMA(discovery_info)
+
     async_add_devices([MqttTemplate(
         hass,
         config.get(CONF_NAME),
