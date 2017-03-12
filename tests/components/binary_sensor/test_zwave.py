8639a7574db47f154665bbd7be9f031e7ff5325c
Adam Mills <adam@armills.info>
2017-03-12 18:33:24 -0400

    Test fixes

--- tests/components/binary_sensor/test_zwave.py
+++ tests/components/binary_sensor/test_zwave.py
@@ -33,7 +33,7 @@ def test_get_device_detects_workaround_sensor(mock_openzwave):
     """Test that workaround returns a binary sensor."""
     node = MockNode(manufacturer_id='010f', product_type='0b00')
     value = MockValue(data=False, node=node,
-                      command_class=const.COMMAND_CLASS_SENSOR_BINARY)
+                      command_class=const.COMMAND_CLASS_SENSOR_ALARM)
 
     device = zwave.get_device(node=node, value=value, node_config={})
     assert isinstance(device, zwave.ZWaveBinarySensor)
@@ -49,17 +49,20 @@ def test_get_device_detects_sensor(mock_openzwave):
     assert isinstance(device, zwave.ZWaveBinarySensor)
 
 
-def test_binary_sensor_value_changed(mock_openzwave):
+@asyncio.coroutine
+def test_binary_sensor_value_changed(hass, mock_openzwave):
     """Test value changed for binary sensor."""
     node = MockNode()
     value = MockValue(data=False, node=node,
                       command_class=const.COMMAND_CLASS_SENSOR_BINARY)
     device = zwave.get_device(node=node, value=value, node_config={})
+    device.hass = hass
 
     assert not device.is_on
 
     value.data = True
-    value_changed(value)
+    yield from hass.loop.run_in_executor(None, value_changed, value)
+    yield from hass.async_block_till_done()
 
     assert device.is_on
 
@@ -71,13 +74,19 @@ def test_trigger_sensor_value_changed(hass, mock_openzwave):
         manufacturer_id='013c', product_type='0002', product_id='0002')
     value = MockValue(data=False, node=node)
     device = zwave.get_device(node=node, value=value, node_config={})
-    device.hass = hass
 
     assert not device.is_on
 
     value.data = True
     yield from hass.loop.run_in_executor(None, value_changed, value)
+    yield from hass.async_block_till_done()
+    assert device.invalidate_after is None
 
+    device.hass = hass
+
+    value.data = True
+    yield from hass.loop.run_in_executor(None, value_changed, value)
+    yield from hass.async_block_till_done()
     assert device.is_on
 
     test_time = device.invalidate_after - datetime.timedelta(seconds=1)
