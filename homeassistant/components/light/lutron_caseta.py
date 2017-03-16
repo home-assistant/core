564b82b8ceeb4a8e9501bfa4fef452fe41a3e3d2
gurumitts <miteshcoder1001@gmail.com>
2017-03-15 23:58:48 -0500

    Merges into light, removes component

--- homeassistant/components/light/lutron_caseta.py
+++ homeassistant/components/light/lutron_caseta.py
@@ -3,22 +3,34 @@ import logging
 
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS, SUPPORT_BRIGHTNESS, Light)
-from homeassistant.components.lutron_caseta import (
-    LUTRON_CASETA_DEVICES, LUTRON_CASETA_SMARTBRIDGE)
 from homeassistant.components.light.lutron import to_hass_level
+from homeassistant.components.light.lutron import to_lutron_level
 
-DEPENDENCIES = ['lutron_caseta']
+REQUIREMENTS = ['https://github.com/gurumitts/pylutron-caseta/archive/v0.2.0.zip#'
+                'pylutron-caseta==v0.2.0']
 
 _LOGGER = logging.getLogger(__name__)
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup Lutron lights."""
+    """Setup Lutron walldimmers and switches as lights."""
+    from pylutron_caseta import smartbridge
+
+    bridge = smartbridge.Smartbridge(
+        hostname=config['host'],
+        username=config['user'],
+        password=config['password'])
+
+    supported_types = ['WallDimmer']
+
+    caseta_devices = bridge.get_devices()
     devs = []
-    for _caseta_light in hass.data[LUTRON_CASETA_DEVICES]['light']:
-        dev = LutronCasetaLight(hass, _caseta_light,
-                                hass.data[LUTRON_CASETA_SMARTBRIDGE])
-        devs.append(dev)
+    _LOGGER.error(caseta_devices)
+    for device in caseta_devices:
+        _LOGGER.error(device)
+        if caseta_devices[device]["type"] in supported_types:
+            dev = LutronCasetaLight(hass, caseta_devices[device], bridge)
+            devs.append(dev)
     add_devices(devs, True)
 
     return True
@@ -27,15 +39,19 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class LutronCasetaLight(Light):
     """Representation of a Lutron Light, including dimmable."""
 
-    def __init__(self, hass, device_info, smartbridge):
+    def __init__(self, hass, device_info, bridge):
         """Initialize the light."""
         self._prev_brightness = None
         self._device_id = device_info["device_id"]
         self._device_type = device_info["type"]
         self._device_name = device_info["name"]
         self._state = None
-        self._smartbridge = smartbridge
+        self._smartbridge = bridge
+        self._smartbridge.add_subscriber(self._device_id, self._update_callback)
         self.update()
+
+    def _update_callback(self):
+        self.schedule_update_ha_state()
 
     @property
     def name(self):
@@ -50,7 +66,7 @@ class LutronCasetaLight(Light):
     @property
     def brightness(self):
         """Return the brightness of the light."""
-        return to_hass_level(self._state["value"])
+        return to_hass_level(self._state["current_state"])
 
     def turn_on(self, **kwargs):
         """Turn the light on."""
@@ -58,11 +74,11 @@ class LutronCasetaLight(Light):
             brightness = kwargs[ATTR_BRIGHTNESS]
         else:
             brightness = 100
-        self._state = self._smartbridge.set_value(self._device_id, brightness)
+        self._smartbridge.set_value(self._device_id, to_lutron_level(brightness))
 
     def turn_off(self, **kwargs):
         """Turn the light off."""
-        self._state = self._smartbridge.set_value(self._device_id, 0)
+        self._smartbridge.set_value(self._device_id, 0)
 
     @property
     def device_state_attributes(self):
@@ -74,8 +90,14 @@ class LutronCasetaLight(Light):
     @property
     def is_on(self):
         """Return true if device is on."""
-        return self._state["value"] > 0
+        _LOGGER.debug(self._state)
+        return self._state["current_state"] > 0
 
     def update(self):
         """Called when forcing a refresh of the device."""
-        self._state = self._smartbridge.get_state(self._device_id)
+        self._state = self._smartbridge.get_device_by_id(self._device_id)
+
+    @property
+    def should_poll(self):
+        """No polling needed."""
+        return False
