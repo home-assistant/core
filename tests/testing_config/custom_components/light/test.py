55791a1282c9944fa98f8b83ef81f4a83b1ab205
Anders Melchiorsen <amelchio@nogoto.net>
2017-03-21 00:06:32 +0100

    Adapt test

--- tests/testing_config/custom_components/light/test.py
+++ tests/testing_config/custom_components/light/test.py
@@ -4,20 +4,33 @@ Provide a mock switch platform.
 Call init before using it in your tests to ensure clean test data.
 """
 from homeassistant.const import STATE_ON, STATE_OFF
+from homeassistant.components.light import ( SUPPORT_XY_COLOR )
 from tests.common import MockToggleDevice
 
 
 DEVICES = []
 
+class MockLightDevice(MockToggleDevice):
+    """Provide a mock light device."""
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        return SUPPORT_XY_COLOR
+
+    @property
+    def brightness(self):
+        """Return the brightness of this light between 0..255."""
+        return None
 
 def init(empty=False):
     """Initalize the platform with devices."""
     global DEVICES
 
     DEVICES = [] if empty else [
-        MockToggleDevice('Ceiling', STATE_ON),
-        MockToggleDevice('Ceiling', STATE_OFF),
-        MockToggleDevice(None, STATE_OFF)
+        MockLightDevice('Ceiling', STATE_ON),
+        MockLightDevice('Ceiling', STATE_OFF),
+        MockLightDevice(None, STATE_OFF)
     ]
 
 
