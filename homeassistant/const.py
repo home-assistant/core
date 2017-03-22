e57869390fe15264c8d0527d1fb057bde2f3d422
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-03-22 08:50:54 -0700

    Constrain core dependencies to core versions (#6738)
    * Require at least pip 7.1
    
    * Write and use constraint files for packages
    
    * Update gen_requirements_all.py

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -7,6 +7,7 @@ __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 REQUIRED_PYTHON_VER = (3, 4, 2)
 REQUIRED_PYTHON_VER_WIN = (3, 5, 2)
+CONSTRAINT_FILE = 'package_constraints.txt'
 
 PROJECT_NAME = 'Home Assistant'
 PROJECT_PACKAGE_NAME = 'homeassistant'
