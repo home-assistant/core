f4cde107f36aea104ce0aecc30acd9ef5191b343
Teemu Rytilahti <tpr@iki.fi>
2017-03-26 16:15:06 +0200

    yeelight: adjust supported features on update()
    Earlier we checked for the type only during the initialization,
    but this won't work when the bulb is disconnected during the init,
    causing failures to adjust rgb&color temperature even if those should be supported.
    
    fixes #6692

--- homeassistant/components/light/yeelight.py
+++ homeassistant/components/light/yeelight.py
@@ -179,9 +179,6 @@ class YeelightLight(Light):
                 self._bulb_device = yeelight.Bulb(self._ipaddr)
                 self._bulb_device.get_properties()  # force init for type
 
-                btype = self._bulb_device.bulb_type
-                if btype == yeelight.BulbType.Color:
-                    self._supported_features |= SUPPORT_YEELIGHT_RGB
                 self._available = True
             except yeelight.BulbException as ex:
                 self._available = False
@@ -202,6 +199,9 @@ class YeelightLight(Light):
         import yeelight
         try:
             self._bulb.get_properties()
+
+            if self._bulb_device.bulb_type == yeelight.BulbType.Color:
+                self._supported_features |= SUPPORT_YEELIGHT_RGB
 
             self._is_on = self._properties.get("power") == "on"
 
