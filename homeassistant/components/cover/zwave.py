884bac4199f46fa7ba861df86aea1694dfca2253
turbokongen <turbokongen@hotmail.com>
2017-03-27 17:04:03 +0200

    Revert removal of workaround

--- homeassistant/components/cover/zwave.py
+++ homeassistant/components/cover/zwave.py
@@ -62,9 +62,11 @@ class ZwaveRollershutter(zwave.ZWaveDeviceEntity, CoverDevice):
 
         if self.values.open and self.values.close and \
                 self._open_id is None and self._close_id is None:
-            if self._invert:
+            if (self._invert or 
+                self._workaround == workaround.WORKAROUND_REVERSE_OPEN_CLOSE):
                 self._open_id = self.values.close.value_id
                 self._close_id = self.values.open.value_id
+                self._workaround = None
             else:
                 self._open_id = self.values.open.value_id
                 self._close_id = self.values.close.value_id
