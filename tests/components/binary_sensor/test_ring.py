4e052bfe9155faf9cb4b685e0f3758005a84ccb2
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-03-31 11:53:56 -0400

    Introduced Ring binary sensors and refactored Ring component (#6520)
    * - Introduced Ring binary_sensor.
    
    - Added unittest for Ring binary_sensor.
    
    - Bumped ring_doorbell 3rd party module.
    
    * Updated requirements
    
    * Added correct file for unittest
    
    * - Introduced Ring binary_sensor.
    
    - Added unittest for Ring binary_sensor.
    
    - Bumped ring_doorbell 3rd party module.
    
    * Updated requirements
    
    * Added correct file for unittest
    
    * Added extra sensors last_ding and last_motion
    
    * Modified Ring binary_sensor and sensor to inherit DOMAIN configuration
    
    * Moved static to top ring.py
    
    * Fixed requirements
    
    * Bump version ring_doorbell to 0.1.2
    
    * testing unittests
    
    * Use hass.data dict instead GLOBALS
    
    * Fixed unittests
    
    * Bump ring_doorbell to 0.1.3
    
    * Updated unittest and coverted to use decorator @requests_mock.Mocker()
    
    * Updated ring_session with lower case

--- tests/components/binary_sensor/test_ring.py
+++ tests/components/binary_sensor/test_ring.py
@@ -1,16 +1,16 @@
-"""The tests for the Ring sensor platform."""
+"""The tests for the Ring binary sensor platform."""
 import unittest
 import requests_mock
 
-from homeassistant.components.sensor import ring
+from homeassistant.components.binary_sensor import ring
 from homeassistant.components import ring as base_ring
 
 from tests.components.test_ring import ATTRIBUTION, VALID_CONFIG
 from tests.common import get_test_home_assistant, load_fixture
 
 
-class TestRingSensorSetup(unittest.TestCase):
-    """Test the Ring platform."""
+class TestRingBinarySensorSetup(unittest.TestCase):
+    """Test the Ring Binary Sensor platform."""
 
     DEVICES = []
 
@@ -25,12 +25,7 @@ class TestRingSensorSetup(unittest.TestCase):
         self.config = {
             'username': 'foo',
             'password': 'bar',
-            'monitored_conditions': [
-                'battery',
-                'last_activity',
-                'last_ding',
-                'last_motion',
-                'volume']
+            'monitored_conditions': ['ding', 'motion'],
         }
 
     def tearDown(self):
@@ -38,14 +33,15 @@ class TestRingSensorSetup(unittest.TestCase):
         self.hass.stop()
 
     @requests_mock.Mocker()
-    def test_sensor(self, mock):
-        """Test the Ring senskor class and methods."""
+    def test_binary_sensor(self, mock):
+        """Test the Ring sensor class and methods."""
         mock.post('https://api.ring.com/clients_api/session',
                   text=load_fixture('ring_session.json'))
         mock.get('https://api.ring.com/clients_api/ring_devices',
                  text=load_fixture('ring_devices.json'))
-        mock.get('https://api.ring.com/clients_api/doorbots/987652/history',
-                 text=load_fixture('ring_doorbots.json'))
+        mock.get('https://api.ring.com/clients_api/dings/active',
+                 text=load_fixture('ring_ding_active.json'))
+
         base_ring.setup(self.hass, VALID_CONFIG)
         ring.setup_platform(self.hass,
                             self.config,
@@ -54,23 +50,13 @@ class TestRingSensorSetup(unittest.TestCase):
 
         for device in self.DEVICES:
             device.update()
-            if device.name == 'Front Door Battery':
-                self.assertEqual(100, device.state)
-                self.assertEqual('lpd_v1',
-                                 device.device_state_attributes['kind'])
-                self.assertNotEqual('chimes',
-                                    device.device_state_attributes['type'])
-            if device.name == 'Downstairs Volume':
-                self.assertEqual(2, device.state)
-                self.assertEqual('1.2.3',
-                                 device.device_state_attributes['firmware'])
-                self.assertEqual('mdi:bell-ring', device.icon)
-                self.assertEqual('chimes',
-                                 device.device_state_attributes['type'])
-            if device.name == 'Front Door Last Activity':
-                self.assertFalse(device.device_state_attributes['answered'])
+            if device.name == 'Front Door Ding':
+                self.assertEqual('on', device.state)
                 self.assertEqual('America/New_York',
                                  device.device_state_attributes['timezone'])
+            elif device.name == 'Front Door Motion':
+                self.assertEqual('off', device.state)
+                self.assertEqual('motion', device.device_class)
 
             self.assertIsNone(device.entity_picture)
             self.assertEqual(ATTRIBUTION,
