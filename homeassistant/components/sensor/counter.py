0999d89f66f08b797325f5369e3c2f09fddf4f6b
Tom Matheussen <tommatheussen5@hotmail.com>
2017-04-01 18:38:08 +0200

    Removed old imports

--- homeassistant/components/sensor/counter.py
+++ homeassistant/components/sensor/counter.py
@@ -5,14 +5,12 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.counter/
 """
 import logging
-from datetime import timedelta
 
 import voluptuous as vol
 
 from homeassistant.components.sensor import DOMAIN, PLATFORM_SCHEMA
 from homeassistant.const import CONF_NAME
 from homeassistant.helpers.entity import Entity
-from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
