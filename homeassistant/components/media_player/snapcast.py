fa377652e4525a64e74a6f49a5011635a5b0c04f
MechaXL <mechaxl@gmail.com>
2017-04-04 00:04:08 -0500

    Adding only connected snapcast clients

--- homeassistant/components/media_player/snapcast.py
+++ homeassistant/components/media_player/snapcast.py
@@ -46,7 +46,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
                       host, port)
         return False
 
-    add_devices([SnapcastDevice(client) for client in server.clients])
+    add_devices([SnapcastDevice(client) for client in server.clients if client.connected])
 
 
 class SnapcastDevice(MediaPlayerDevice):
