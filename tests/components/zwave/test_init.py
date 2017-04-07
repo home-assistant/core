a5e698ad082a6eafd2b2732891472053cb15b174
Andrey <andrey-git@users.noreply.github.com>
2017-04-07 08:39:35 +0300

    Preserve customize glob order. (#6963)
    * Preserve customize glob order.
    
    * add tests

--- tests/components/zwave/test_init.py
+++ tests/components/zwave/test_init.py
@@ -1,7 +1,11 @@
 """Tests for the Z-Wave init."""
 import asyncio
+import unittest
+from collections import OrderedDict
 
 from homeassistant.bootstrap import async_setup_component
+from homeassistant.components.zwave import (
+    CONFIG_SCHEMA, CONF_DEVICE_CONFIG_GLOB)
 
 
 @asyncio.coroutine
@@ -34,3 +38,14 @@ def test_invalid_device_config(hass, mock_openzwave):
         }})
 
     assert not result
+
+
+class TestZwave(unittest.TestCase):
+    """Test zwave init."""
+
+    def test_device_config_glob_is_ordered(self):
+        """Test that device_config_glob preserves order."""
+        conf = CONFIG_SCHEMA(
+            {'zwave': {CONF_DEVICE_CONFIG_GLOB: OrderedDict()}})
+        self.assertIsInstance(
+            conf['zwave'][CONF_DEVICE_CONFIG_GLOB], OrderedDict)
