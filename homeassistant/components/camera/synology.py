d99c3835371163d66f950048c0208f675894c88d
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-04-09 23:19:22 -0700

    Fix Synology camera content type (#7010)

--- homeassistant/components/camera/synology.py
+++ homeassistant/components/camera/synology.py
@@ -81,7 +81,9 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
                 params=query_payload
             )
 
-        query_resp = yield from query_req.json()
+        # Skip content type check because Synology doesn't return JSON with
+        # right content type
+        query_resp = yield from query_req.json(content_type=None)
         auth_path = query_resp['data'][AUTH_API]['path']
         camera_api = query_resp['data'][CAMERA_API]['path']
         camera_path = query_resp['data'][CAMERA_API]['path']
