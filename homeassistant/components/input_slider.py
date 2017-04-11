114c027fc3258c6d3125019da1c45479073ff1b2
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-04-11 09:22:43 -0700

    Update input_slider.py

--- homeassistant/components/input_slider.py
+++ homeassistant/components/input_slider.py
@@ -174,7 +174,7 @@ class InputSlider(Entity):
         state = yield from async_get_last_state(self.hass, self.entity_id)
         value = state and float(state.state)
 
-        # Check against False because value can be 0
+        # Check against None because value can be 0
         if value is not None and self._minimum <= value <= self._maximum:
             self._current_value = value
         else:
