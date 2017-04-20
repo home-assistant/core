42c7220d6f5de0b776ab76aae1db8e130a6af9d5
Henrik Nicolaisen <hmn@area-51.dk>
2017-04-20 09:59:27 +0200

    - changed updater to only do updated if they succed and handle calls when tv is off better by only doing 1 remote call - show all sources instead of only connected, to fix source selection when unit is powered off - fixed sources so they can launch apps and select sources

--- homeassistant/components/media_player/webostv.py
+++ homeassistant/components/media_player/webostv.py
@@ -171,31 +171,32 @@ class LgWebOSDevice(MediaPlayerDevice):
         """Retrieve the latest data."""
         from websockets.exceptions import ConnectionClosed
         try:
-            self._state = STATE_PLAYING
-            self._muted = self._client.get_muted()
-            self._volume = self._client.get_volume()
-            self._current_source_id = self._client.get_input()
-            self._source_list = {}
-            self._app_list = {}
+            current_input = self._client.get_input()
+            if current_input != None:
+                self._current_source_id = current_input
+                if self._state in (STATE_UNKNOWN, STATE_OFF):
+                    self._state = STATE_PLAYING
 
-            custom_sources = self._customize.get(CONF_SOURCES, [])
+            if self._state != STATE_OFF:
+                self._muted = self._client.get_muted()
+                self._volume = self._client.get_volume()
 
-            for app in self._client.get_apps():
-                self._app_list[app['id']] = app
-                if app['id'] == self._current_source_id:
-                    self._current_source = app['title']
-                    self._source_list[app['title']] = app
-                elif (app['id'] in custom_sources or
-                      any(word in app['title'] for word in custom_sources) or
-                      any(word in app['id'] for word in custom_sources)):
-                    self._source_list[app['title']] = app
+                self._source_list = {}
+                self._app_list = {}
+                custom_sources = self._customize.get(CONF_SOURCES, [])
 
-            for source in self._client.get_inputs():
-                if not source['connected']:
-                    continue
-                app = self._app_list[source['appId']]
-                self._source_list[app['title']] = app
+                for app in self._client.get_apps():
+                    self._app_list[app['id']] = app
+                    if app['id'] == self._current_source_id:
+                        self._current_source = app['title']
+                        self._source_list[app['title']] = app
+                    elif (app['id'] in custom_sources or
+                          any(word in app['title'] for word in custom_sources) or
+                          any(word in app['id'] for word in custom_sources)):
+                        self._source_list[app['title']] = app
 
+                for source in self._client.get_inputs():
+                    self._source_list[source['label']] = source
         except (OSError, ConnectionClosed):
             self._state = STATE_OFF
 
@@ -292,9 +293,14 @@ class LgWebOSDevice(MediaPlayerDevice):
 
     def select_source(self, source):
         """Select input source."""
-        self._current_source_id = self._source_list[source]['id']
-        self._current_source = self._source_list[source]['title']
-        self._client.launch_app(self._source_list[source]['id'])
+        if self._source_list.get(source).get('title'):
+            self._current_source_id = self._source_list[source]['id']
+            self._current_source = self._source_list[source]['title']
+            self._client.launch_app(self._source_list[source]['id'])
+        elif self._source_list.get(source).get('label'):
+            self._current_source_id = self._source_list[source]['id']
+            self._current_source = self._source_list[source]['label']
+            self._client.set_input(self._source_list[source]['id'])
 
     def media_play(self):
         """Send play command."""
