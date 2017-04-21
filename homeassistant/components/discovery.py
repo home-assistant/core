f641287aa24ab2ee142d8c35cc4ec7b806931cd4
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-04-21 03:45:15 +0200

    Add HassIO to discovery component (#7195)
    * Add HassIO to autodiscovery
    
    * Fix tests
    
    * fix tests
    
    * fix test v2
    
    * fix mock test
    
    * call

--- homeassistant/components/discovery.py
+++ homeassistant/components/discovery.py
@@ -10,6 +10,7 @@ import asyncio
 import json
 from datetime import timedelta
 import logging
+import os
 
 import voluptuous as vol
 
@@ -29,12 +30,14 @@ SERVICE_NETGEAR = 'netgear_router'
 SERVICE_WEMO = 'belkin_wemo'
 SERVICE_HASS_IOS_APP = 'hass_ios'
 SERVICE_IKEA_TRADFRI = 'ikea_tradfri'
+SERVICE_HASSIO = 'hassio'
 
 SERVICE_HANDLERS = {
     SERVICE_HASS_IOS_APP: ('ios', None),
     SERVICE_NETGEAR: ('device_tracker', None),
     SERVICE_WEMO: ('wemo', None),
     SERVICE_IKEA_TRADFRI: ('tradfri', None),
+    SERVICE_HASSIO: ('hassio', None),
     'philips_hue': ('light', 'hue'),
     'google_cast': ('media_player', 'cast'),
     'panasonic_viera': ('media_player', 'panasonic_viera'),
@@ -123,6 +126,10 @@ def async_setup(hass, config):
     def schedule_first(event):
         """Schedule the first discovery when Home Assistant starts up."""
         async_track_point_in_utc_time(hass, scan_devices, dt_util.utcnow())
+
+        # discovery local services
+        if 'HASSIO' in os.environ:
+            hass.async_add_job(new_service_found(SERVICE_HASSIO, {}))
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, schedule_first)
 
