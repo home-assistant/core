46a2d6b05661e2d6e0fe3cd6128b17af106c50b9
Joakim af Sandeberg <joakim@afsandeberg.se>
2017-04-21 14:44:08 +0000

    Added docstrings, removed API_KEY from log, changed imports

--- homeassistant/components/sensor/pushbullet.py
+++ homeassistant/components/sensor/pushbullet.py
@@ -5,12 +5,8 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.pushbullet/
 """
 import logging
-import threading
 import voluptuous as vol
 
-from pushbullet import PushBullet
-from pushbullet import InvalidKeyError
-from pushbullet import Listener
 
 from homeassistant.const import (CONF_API_KEY, CONF_MONITORED_CONDITIONS)
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -46,13 +42,14 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    # Get the PushBullet notification service.
+    """Get the PushBullet notification service."""
+    from pushbullet import PushBullet
+    from pushbullet import InvalidKeyError
     try:
         pushbullet = PushBullet(config.get(CONF_API_KEY))
     except InvalidKeyError:
-        _LOGGER.error(
-            "Wrong API key supplied.{" + config.get(CONF_API_KEY) + "}"
-            "Get it at https://www.pushbullet.com/account")
+        _LOGGER.error("Wrong API key supplied." \
+                "Get it at https://www.pushbullet.com/account")
         return None
     # Create a common data provider
     pbprovider = PushBulletNotificationProvider(pushbullet)
@@ -64,15 +61,20 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 
 class PushBulletNotificationSensor(Entity):
-    # Fetches data via a filter from the common pushbullet provider
+    """Fetches data via a filter from the common pushbullet provider."""
 
     def __init__(self, pb, element):
+        """Initialize the service."""
         self.pushbullet = pb
         self._element = element
         self._state = None
         self._state_attributes = None
 
     def update(self):
+        """Fetches the latest data from the sensor.
+
+        This will fetch the 'sensor reading' into self._state but also all
+        attributes into self._state_attributes."""
         try:
             self._state = self.pushbullet.data[self._element]
             self._state_attributes = self.pushbullet.data
@@ -81,21 +83,26 @@ class PushBulletNotificationSensor(Entity):
 
     @property
     def name(self):
+        """Returns the name of the sensor."""
         return "Pushbullet " + self._element
 
     @property
     def state(self):
+        """Returns the current state of the sensor."""
         return self._state
 
     @property
     def device_state_attributes(self):
+        """Returns all known attributes of the sensor."""
         return self._state_attributes
 
 
 class PushBulletNotificationProvider():
-    # Provider for an account, leading to multiple sensors
+    """Provider for an account, leading to one or more sensors."""
 
     def __init__(self, pb):
+        """Starts to retrieve pushes from the given Pushbullet instance."""
+        import threading
         self.pushbullet = pb
         self._data = None
         self.thread = threading.Thread(target=self.retrieve_pushes)
@@ -103,14 +110,23 @@ class PushBulletNotificationProvider():
         self.thread.start()
 
     def on_push(self, data):
+        """Method to update the current data.
+
+        Currently only monitors pushes but might be extended to monitor
+        different kinds of Pushbullet events."""
         if data["type"] == "push":
             self._data = data["push"]
 
     @property
     def data(self):
+        """The current data stored in the provider."""
         return self._data
 
     def retrieve_pushes(self):
+        """The method to run the daemon thread in.
+
+        Spawns a new Listener and links it to self.on_push."""
+        from pushbullet import Listener
         self.listener = Listener(account=self.pushbullet,
                                  on_push=self.on_push)
         _LOGGER.debug("getting pushes")
