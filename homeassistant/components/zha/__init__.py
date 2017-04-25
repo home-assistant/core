92a4f5427b9f56bb15298ef3194cd6a25762da0d
Russell Cloran <rcloran@gmail.com>
2017-04-24 17:11:52 -0700

    Update bellows to 0.2.7

--- homeassistant/components/zha/__init__.py
+++ homeassistant/components/zha/__init__.py
@@ -16,7 +16,7 @@ import homeassistant.helpers.config_validation as cv
 
 
 # Definitions for interfacing with the rest of HA
-REQUIREMENTS = ['bellows==0.2.6']
+REQUIREMENTS = ['bellows==0.2.7']
 
 DOMAIN = 'zha'
 
