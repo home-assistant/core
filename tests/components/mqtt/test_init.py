32bee5714859ce980fab77094025ca15896b44fb
amigian74 <steven.woelk@papendorf.info>
2017-05-02 10:16:33 +0200

    function names changed (line too long)

--- tests/components/mqtt/test_init.py
+++ tests/components/mqtt/test_init.py
@@ -213,29 +213,29 @@ class TestMQTT(unittest.TestCase):
         """Test the subscription of wildcard topics."""
         mqtt.subscribe(self.hass, '+/test-topic/#', self.record_calls)
 
-        fire_mqtt_message(self.hass, 'hello/test-topic', 'test-payload')
+        fire_mqtt_message(self.hass, 'hi/test-topic', 'test-payload')
 
         self.hass.block_till_done()
         self.assertEqual(1, len(self.calls))
-        self.assertEqual('hello/test-topic', self.calls[0][0])
+        self.assertEqual('hi/test-topic', self.calls[0][0])
         self.assertEqual('test-payload', self.calls[0][1])
 
     def test_subscribe_topic_level_wildcard_and_wildcard_subtree_topic(self):
         """Test the subscription of wildcard topics."""
         mqtt.subscribe(self.hass, '+/test-topic/#', self.record_calls)
 
-        fire_mqtt_message(self.hass, 'hello/test-topic/here-iam', 'test-payload')
+        fire_mqtt_message(self.hass, 'hi/test-topic/here-iam', 'test-payload')
 
         self.hass.block_till_done()
         self.assertEqual(1, len(self.calls))
-        self.assertEqual('hello/test-topic/here-iam', self.calls[0][0])
+        self.assertEqual('hi/test-topic/here-iam', self.calls[0][0])
         self.assertEqual('test-payload', self.calls[0][1])
 
-    def test_subscribe_topic_level_wildcard_and_wildcard_level_wildcard_no_match(self):
+    def test_subscribe_topic_level_wildcard_and_wildcard_level_no_match(self):
         """Test the subscription of wildcard topics."""
         mqtt.subscribe(self.hass, '+/test-topic/#', self.record_calls)
 
-        fire_mqtt_message(self.hass, 'hello/here-iam/test-topic', 'test-payload')
+        fire_mqtt_message(self.hass, 'hi/here-iam/test-topic', 'test-payload')
 
         self.hass.block_till_done()
         self.assertEqual(0, len(self.calls))
@@ -244,7 +244,7 @@ class TestMQTT(unittest.TestCase):
         """Test the subscription of wildcard topics."""
         mqtt.subscribe(self.hass, '+/test-topic/#', self.record_calls)
 
-        fire_mqtt_message(self.hass, 'hello/another-test-topic', 'test-payload')
+        fire_mqtt_message(self.hass, 'hi/another-test-topic', 'test-payload')
 
         self.hass.block_till_done()
         self.assertEqual(0, len(self.calls))
