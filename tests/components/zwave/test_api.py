5fd17680c3f8e2c596ed360a1396435ebf7b16a6
turbokongen <turbokongen@hotmail.com>
2017-05-09 14:49:37 +0200

    Tests groups

--- tests/components/zwave/test_api.py
+++ tests/components/zwave/test_api.py
@@ -8,8 +8,8 @@ from tests.common import mock_http_component_app
 
 
 @asyncio.coroutine
-def test_get_groups_nogroups_node(hass, test_client, mock_openzwave):
-    """Test getting groupdata on node without groups."""
+def test_get_groups(hass, test_client, mock_openzwave):
+    """Test getting groupdata on node."""
     app = mock_http_component_app(hass)
     ZWaveNodeGroupView().register(app.router)
 
@@ -38,6 +38,48 @@ def test_get_groups_nogroups_node(hass, test_client, mock_openzwave):
         }
     }
 
+@asyncio.coroutine
+def test_get_groups_nogroups(hass, test_client, mock_openzwave):
+    """Test getting groupdata on node without groups."""
+    app = mock_http_component_app(hass)
+    ZWaveNodeGroupView().register(app.router)
+
+    network = hass.data[ZWAVE_NETWORK] = MagicMock()
+    network.nodes.get().groups_to_dict.return_value = {}
+    group = MagicMock()
+    group.associations = None
+    group.associations_instances = None
+    group.label = None
+    group.max_associations = None
+    mock_openzwave.group.ZWaveGroup.return_value = group
+
+    client = yield from test_client(app)
+
+    resp = yield from client.get('/api/zwave/groups/2')
+
+    assert resp.status == 200
+    result = yield from resp.json()
+
+    assert result == {}
+
+@asyncio.coroutine
+def test_get_groups_nonode(hass, test_client, mock_openzwave):
+    """Test getting groupdata on nonexisting node."""
+    app = mock_http_component_app(hass)
+    ZWaveNodeGroupView().register(app.router)
+
+    network = hass.data[ZWAVE_NETWORK] = MagicMock()
+    network.nodes = {1: 1, 5: 5}
+
+    client = yield from test_client(app)
+
+    resp = yield from client.get('/api/zwave/groups/2')
+
+    assert resp.status == 404
+    result = yield from resp.json()
+
+    assert result == {'message': 'Node not found'}
+
 
 @asyncio.coroutine
 def test_get_config_noconfig_node(hass, test_client):
@@ -58,6 +100,25 @@ def test_get_config_noconfig_node(hass, test_client):
 
 
 @asyncio.coroutine
+def test_get_config_nonode(hass, test_client, mock_openzwave):
+    """Test getting groupdata on nonexisting node."""
+    app = mock_http_component_app(hass)
+    ZWaveNodeConfigView().register(app.router)
+
+    network = hass.data[ZWAVE_NETWORK] = MagicMock()
+    network.nodes = {1: 1, 5: 5}
+
+    client = yield from test_client(app)
+
+    resp = yield from client.get('/api/zwave/config/2')
+
+    assert resp.status == 404
+    result = yield from resp.json()
+
+    assert result == {'message': 'Node not found'}
+
+
+@asyncio.coroutine
 def test_get_usercodes_nousercode_node(hass, test_client):
     """Test getting usercodes on node without usercodes."""
     hass.data[ZWAVE_NETWORK] = MagicMock()
@@ -73,3 +134,23 @@ def test_get_usercodes_nousercode_node(hass, test_client):
     result = yield from resp.json()
 
     assert result == {}
+
+
+@asyncio.coroutine
+def test_get_usercodes_nonode(hass, test_client, mock_openzwave):
+    """Test getting groupdata on nonexisting node."""
+    app = mock_http_component_app(hass)
+    ZWaveUserCodeView().register(app.router)
+
+    network = hass.data[ZWAVE_NETWORK] = MagicMock()
+    network.nodes = {1: 1, 5: 5}
+
+    client = yield from test_client(app)
+
+    resp = yield from client.get('/api/zwave/usercodes/2')
+
+    assert resp.status == 404
+    result = yield from resp.json()
+
+    assert result == {'message': 'Node not found'}
+
