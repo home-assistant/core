6848fe1683e4fd999bd48195a73b47bc3333322f
Fabian Affolter <mail@fabian-affolter.ch>
2017-05-10 12:06:57 +0200

    Update docstrings and log messages (#7526)

--- homeassistant/components/light/limitlessled.py
+++ homeassistant/components/light/limitlessled.py
@@ -4,7 +4,6 @@ Support for LimitlessLED bulbs.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/light.limitlessled/
 """
-
 import logging
 
 import voluptuous as vol
