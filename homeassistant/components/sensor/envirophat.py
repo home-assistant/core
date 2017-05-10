1f6cc31dc4aff8a8e8c9d191dd267fc88484f7cb
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-05-09 21:28:46 -0700

    Fix update logic.

--- homeassistant/components/sensor/envirophat.py
+++ homeassistant/components/sensor/envirophat.py
@@ -66,7 +66,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     for variable in config[CONF_DISPLAY_OPTIONS]:
         dev.append(EnvirophatSensor(data, variable))
 
-    add_devices(dev)
+    add_devices(dev, True)
 
 
 class EnvirophatSensor(Entity):
@@ -79,7 +79,6 @@ class EnvirophatSensor(Entity):
         self._unit_of_measurement = SENSOR_TYPES[sensor_types][1]
         self.type = sensor_types
         self._state = None
-        self.update()
 
     @property
     def name(self):
