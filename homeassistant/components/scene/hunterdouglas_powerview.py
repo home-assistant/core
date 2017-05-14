ba6b87c78fe1a50ad89747bbfbbf137cf5509bf4
sanders76 <s.teunissen@gmail.com>
2017-05-14 19:55:45 +0200

    first commit

--- homeassistant/components/scene/hunterdouglas_powerview.py
+++ homeassistant/components/scene/hunterdouglas_powerview.py
@@ -7,44 +7,62 @@ https://home-assistant.io/components/scene.hunterdouglas_powerview/
 import logging
 
 from homeassistant.components.scene import Scene, DOMAIN
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.entity import generate_entity_id
+import asyncio
 
 _LOGGER = logging.getLogger(__name__)
-REQUIREMENTS = [
-    'https://github.com/sander76/powerviewApi/archive'
-    '/246e782d60d5c0addcc98d7899a0186f9d5640b0.zip#powerviewApi==0.3.15'
-]
+REQUIREMENTS = ['aiopvapi==1.1']
 
 HUB_ADDRESS = 'address'
 
-
-# pylint: disable=unused-argument
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up the powerview scenes stored in a Powerview hub."""
-    from powerview_api import powerview
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
+    """Set up home assistant scene entries."""
+    from aiopvapi.hub import Hub
 
     hub_address = config.get(HUB_ADDRESS)
+    websession = async_get_clientsession(hass)
 
-    _pv = powerview.PowerView(hub_address)
-    try:
-        _scenes = _pv.get_scenes()
-        _rooms = _pv.get_rooms()
-    except ConnectionError:
-        _LOGGER.exception("Error connecting to powerview "
-                          "hub with ip address: %s", hub_address)
+
+    _hub = Hub(hub_address,hass.loop,websession)
+    _scenes = _hub.scenes.get_scenes()
+    _rooms = _hub.rooms.get_rooms()
+
+    if _scenes and _rooms:
+        async_add_devices(PowerViewScene(hass, _scene, _rooms, _hub)
+                for _scene in _scenes['sceneData'])
+        return True
+    else:
         return False
-    add_devices(PowerViewScene(hass, scene, _rooms, _pv)
-                for scene in _scenes['sceneData'])
 
-    return True
+# pylint: disable=unused-argument
+# def setup_platform(hass, config, add_devices, discovery_info=None):
+#     """Set up the powerview scenes stored in a Powerview hub."""
+#     from powerview_api import powerview
+#
+#     hub_address = config.get(HUB_ADDRESS)
+#
+#     _pv = powerview.PowerView(hub_address)
+#     try:
+#         _scenes = _pv.get_scenes()
+#         _rooms = _pv.get_rooms()
+#     except ConnectionError:
+#         _LOGGER.exception("Error connecting to powerview "
+#                           "hub with ip address: %s", hub_address)
+#         return False
+#     add_devices(PowerViewScene(hass, scene, _rooms, _pv)
+#                 for scene in _scenes['sceneData'])
+#
+#     return True
 
 
 class PowerViewScene(Scene):
     """Representation of a Powerview scene."""
 
-    def __init__(self, hass, scene_data, room_data, pv_instance):
+    def __init__(self, hass, scene_data, room_data, hub):
         """Initialize the scene."""
-        self.pv_instance = pv_instance
+        self.hub = hub
         self.hass = hass
         self.scene_data = scene_data
         self._sync_room_data(room_data)
@@ -74,6 +92,10 @@ class PowerViewScene(Scene):
         """Icon to use in the frontend."""
         return 'mdi:blinds'
 
-    def activate(self):
-        """Activate the scene. Tries to get entities into requested state."""
-        self.pv_instance.activate_scene(self.scene_data["id"])
+    def async_activate(self):
+        """Activate scene. Try to get entities into requested state.
+
+        This method must be run in the event loop and returns a coroutine.
+        """
+        yield from self.hub.scenes.activate_scene(self.scene_data["id"])
+
