e2e58e6acc5bb34c2a5d30e36ff7a34a926888dd
Adam Mills <adam@armills.info>
2017-05-15 03:34:30 -0400

    Automation State Change For timer attribute fix (#7584)

--- tests/components/automation/test_state.py
+++ tests/components/automation/test_state.py
@@ -333,6 +333,40 @@ class TestAutomationState(unittest.TestCase):
         self.hass.block_till_done()
         self.assertEqual(0, len(self.calls))
 
+    def test_if_fires_on_entity_change_with_for_attribute_change(self):
+        """Test for firing on entity change with for and attribute change."""
+        assert setup_component(self.hass, automation.DOMAIN, {
+            automation.DOMAIN: {
+                'trigger': {
+                    'platform': 'state',
+                    'entity_id': 'test.entity',
+                    'to': 'world',
+                    'for': {
+                        'seconds': 5
+                    },
+                },
+                'action': {
+                    'service': 'test.automation'
+                }
+            }
+        })
+
+        utcnow = dt_util.utcnow()
+        with patch('homeassistant.core.dt_util.utcnow') as mock_utcnow:
+            mock_utcnow.return_value = utcnow
+            self.hass.states.set('test.entity', 'world')
+            self.hass.block_till_done()
+            mock_utcnow.return_value += timedelta(seconds=4)
+            fire_time_changed(self.hass, mock_utcnow.return_value)
+            self.hass.states.set('test.entity', 'world',
+                                 attributes={"mock_attr": "attr_change"})
+            self.hass.block_till_done()
+            self.assertEqual(0, len(self.calls))
+            mock_utcnow.return_value += timedelta(seconds=4)
+            fire_time_changed(self.hass, mock_utcnow.return_value)
+            self.hass.block_till_done()
+            self.assertEqual(1, len(self.calls))
+
     def test_if_fires_on_entity_change_with_for(self):
         """Test for firing on entity change with for."""
         assert setup_component(self.hass, automation.DOMAIN, {
@@ -389,6 +423,51 @@ class TestAutomationState(unittest.TestCase):
 
             # Time travel 10 secs into the future
             mock_utcnow.return_value = point2
+            self.hass.bus.fire('test_event')
+            self.hass.block_till_done()
+            self.assertEqual(1, len(self.calls))
+
+    def test_if_fires_on_for_condition_attribute_change(self):
+        """Test for firing if contition is on with attribute change."""
+        point1 = dt_util.utcnow()
+        point2 = point1 + timedelta(seconds=4)
+        point3 = point1 + timedelta(seconds=8)
+        with patch('homeassistant.core.dt_util.utcnow') as mock_utcnow:
+            mock_utcnow.return_value = point1
+            self.hass.states.set('test.entity', 'on')
+            assert setup_component(self.hass, automation.DOMAIN, {
+                automation.DOMAIN: {
+                    'trigger': {
+                        'platform': 'event',
+                        'event_type': 'test_event',
+                    },
+                    'condition': {
+                        'condition': 'state',
+                        'entity_id': 'test.entity',
+                        'state': 'on',
+                        'for': {
+                            'seconds': 5
+                        },
+                    },
+                    'action': {'service': 'test.automation'},
+                }
+            })
+
+            # not enough time has passed
+            self.hass.bus.fire('test_event')
+            self.hass.block_till_done()
+            self.assertEqual(0, len(self.calls))
+
+            # Still not enough time has passed, but an attribute is changed
+            mock_utcnow.return_value = point2
+            self.hass.states.set('test.entity', 'on',
+                                 attributes={"mock_attr": "attr_change"})
+            self.hass.bus.fire('test_event')
+            self.hass.block_till_done()
+            self.assertEqual(0, len(self.calls))
+
+            # Enough time has now passed
+            mock_utcnow.return_value = point3
             self.hass.bus.fire('test_event')
             self.hass.block_till_done()
             self.assertEqual(1, len(self.calls))
