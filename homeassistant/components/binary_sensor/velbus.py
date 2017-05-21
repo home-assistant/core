261d41c429b98c202a10dff61b4e2490d123eb99
Thomas Delaet <thomas@delaet.org>
2017-05-21 02:41:09 +0200

    Added support for Velbus

---
+++ homeassistant/components/binary_sensor/velbus.py
@@ -0,0 +1,83 @@
+"""
+Support for Velbus switches.
+
+For more details about this platform, please refer to the documentation at XXX
+"""
+from homeassistant.core import callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.components.binary_sensor import BinarySensorDevice
+
+import asyncio
+import logging
+import velbus
+
+from custom_components.velbus import (VELBUS_MESSAGE)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_entities,  # noqa: D401
+                         discovery_info=None):
+    """Setup Velbus switches."""
+    controller = hass.data['VelbusController']
+    async_add_entities(
+        VelbusSwitch(hass, switch, controller) for switch in discovery_info)
+    return True
+
+
+class VelbusSwitch(BinarySensorDevice):
+    """Representation of a Velbus Switch."""
+
+    def __init__(self, hass, switch, controller):
+        """Initialize a Velbus light."""
+        self._name = switch['name']
+        self._module = switch['module']
+        self._channel = switch['channel']
+        self._is_pushbutton = 'is_pushbutton' in switch \
+                              and switch['is_pushbutton']
+        self._state = False
+        self._controller = controller
+        self._hass = hass
+
+    @asyncio.coroutine
+    def async_added_to_hass(self):
+        """Add listener for Velbus messages on bus."""
+        async_dispatcher_connect(
+            self._hass, VELBUS_MESSAGE, self._on_message
+        )
+
+    @callback
+    def _on_message(self, message):
+        if isinstance(message, velbus.PushButtonStatusMessage):
+            if message.address == self._module and \
+               self._channel in message.get_channels():
+                if self._is_pushbutton:
+                    if self._channel in message.closed:
+                        self._toggle()
+                    else:
+                        pass
+                else:
+                    self._toggle()
+
+    def _toggle(self):
+        if self._state is True:
+            self._state = False
+        else:
+            self._state = True
+        self._hass.async_add_job(self.async_update_ha_state())
+
+    @property
+    def should_poll(self):
+        """No polling needed."""
+        return False
+
+    @property
+    def name(self):
+        """Return the display name of this switch."""
+        return self._name
+
+    @property
+    def is_on(self):
+        """Return true if the switch is on."""
+        return self._state
---
+++ homeassistant/components/light/velbus.py
@@ -0,0 +1,95 @@
+"""
+Support for Velbus lights.
+
+For more details about this platform, please refer to the documentation at XXX
+"""
+from homeassistant.components.light import Light
+import asyncio
+import logging
+import velbus
+from custom_components.velbus import (VELBUS_MESSAGE)
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.core import callback
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_entities,  # noqa: D401
+                         discovery_info=None):
+    """Setup Lights."""
+    controller = hass.data['VelbusController']
+    lights = []
+    for light in discovery_info:
+        lights.append(VelbusLight(hass, light, controller))
+    async_add_entities(lights)
+    delay = 10
+    loop = asyncio.get_event_loop()
+    for light in lights:
+        loop.call_later(delay, light.get_status)
+        delay = delay + 2
+    return True
+
+
+class VelbusLight(Light):
+    """Representation of a Velbus Light."""
+
+    def __init__(self, hass, light, controller):
+        """Initialize a Velbus light."""
+        self._name = light['name']
+        self._module = light['module']
+        self._channel = light['channel']
+        self._state = False
+        self._hass = hass
+        self._controller = controller
+
+    @asyncio.coroutine
+    def async_added_to_hass(self):
+        """Add listener for Velbus messages on bus."""
+        async_dispatcher_connect(
+            self._hass, VELBUS_MESSAGE, self._on_message
+        )
+
+    @callback
+    def _on_message(self, message):
+        if isinstance(message, velbus.RelayStatusMessage) and \
+           message.address == self._module and \
+           message.channel == self._channel:
+            self._state = message.is_on()
+            self._hass.async_add_job(self.async_update_ha_state())
+
+    @property
+    def name(self):
+        """Return the display name of this light."""
+        return self._name
+
+    @property
+    def should_poll(self):
+        """Disable polling."""
+        return False
+
+    @property
+    def is_on(self):
+        """Return true if the light is on."""
+        return self._state
+
+    def turn_on(self, **kwargs):
+        """Instruct the light to turn on."""
+        message = velbus.SwitchRelayOnMessage()
+        message.set_defaults(self._module)
+        message.relay_channels = [self._channel]
+        self._controller.send(message)
+
+    def turn_off(self, **kwargs):
+        """Instruct the light to turn off."""
+        message = velbus.SwitchRelayOffMessage()
+        message.set_defaults(self._module)
+        message.relay_channels = [self._channel]
+        self._controller.send(message)
+
+    def get_status(self):
+        """Retrieve current status."""
+        message = velbus.ModuleStatusRequestMessage()
+        message.set_defaults(self._module)
+        message.channels = [self._channel]
+        self._controller.send(message)
