be66c2c60edf80963d7db44e557252635a425284
tedstriker <github@meins.org>
2017-05-27 10:37:17 +0200

    Resolve conflict after PR #7658
    Changed run_in_executor to async_add_job

--- homeassistant/components/persistent_notification.py
+++ homeassistant/components/persistent_notification.py
@@ -122,8 +122,8 @@ def async_setup(hass, config):
 
         hass.states.async_remove(entity_id)
 
-    descriptions = yield from hass.loop.run_in_executor(
-        None, load_yaml_config_file, os.path.join(
+    descriptions = yield from hass.async_add_job(
+        load_yaml_config_file, os.path.join(
             os.path.dirname(__file__), 'services.yaml')
     )
 
