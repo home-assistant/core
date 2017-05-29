907220d83690e61177cd68875f15597bac6aafae
Andrew <andrewneisch@gmail.com>
2017-05-29 18:14:59 -0500

    Update date_countdown.py

--- homeassistant/components/sensor/date_countdown.py
+++ homeassistant/components/sensor/date_countdown.py
@@ -2,10 +2,10 @@
 Date Countdown
 For more details about this sensor please refer to the documentation at
 https://home-assistant.io/components/sensor.date_countdown/
-
 """
 
 import datetime
+from datetime import timedelta
 import logging
 
 import voluptuous as vol
@@ -14,11 +14,19 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import CONF_NAME
 from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+
 
 _LOGGER = logging.getLogger(__name__)
 
+ATTR_DAYS = 'Days'
+ATTR_HOURS = 'Hours'
+ATTR_MINUTES = 'Minutes'
+
 DEFAULT_NAME = "Countdown"
 CONF_DATE = 'date'
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=1)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_DATE): cv.string,
@@ -42,6 +50,8 @@ class Countdown(Entity):
         self.end_date = end_date
         self._name = sensor_name
         self._state = None
+        self._data = {}
+        self.update()
 
     @property
     def name(self):
@@ -52,19 +62,34 @@ class Countdown(Entity):
     def state(self):
         """Return the state of the sensor."""
         return self._state
+    
+    @property
+    def device_state_attributes(self):
+        return {
+            ATTR_DAYS: self._data.get("days"),
+            ATTR_HOURS: self._data.get("hours"),
+            ATTR_MINUTES: self._data.get("minutes")
+        }
 
     @property
     def icon(self):
         """Icon to use in the frontend, if any."""
         return 'mdi:calendar'
 
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
-        end_date = datetime.datetime.strptime(self.end_date, '%m-%d-%Y %H:%M')
+        """Calculate time until end"""
+        end_date = datetime.datetime.strptime(self.end_date, '%d-%m-%Y %H:%M')
         days = (end_date - datetime.datetime.now())
 
         days, seconds = days.days, days.seconds
         hours = seconds // 3600
         minutes = (seconds % 3600) // 60
         seconds = seconds % 60
+        
+        self._data["days"] = days
+        self._data["hours"] = hours
+        self._data["minutes"] = minutes
+        
         self._state = str(days) + " days " + str(hours) \
             + " hours " + str(minutes) + " minutes"
