6e68f045a658dd91f7329bb568980fa1d33e7d62
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-05-30 06:55:06 +0200

    Cleanup telegram / Add url to webhook (#7824)
    * Cleanup telegram / Add url to webhook
    
    * fix lint
    
    * Fix lint

--- homeassistant/components/telegram_bot/polling.py
+++ homeassistant/components/telegram_bot/polling.py
@@ -12,13 +12,16 @@ import async_timeout
 from aiohttp.client_exceptions import ClientError
 
 from homeassistant.components.telegram_bot import (
-    CONF_ALLOWED_CHAT_IDS, BaseTelegramBotEntity)
+    CONF_ALLOWED_CHAT_IDS, BaseTelegramBotEntity,
+    PLATFORM_SCHEMA as TELEGRAM_PLATFORM_SCHEMA)
 from homeassistant.const import (
     EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP, CONF_API_KEY)
 from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 _LOGGER = logging.getLogger(__name__)
+
+PLATFORM_SCHEMA = TELEGRAM_PLATFORM_SCHEMA
 
 
 @asyncio.coroutine
