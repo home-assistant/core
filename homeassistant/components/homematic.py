ada518790f8d3d6432124de529501dfc8ae6a568
Daniel Perna <danielperna84@gmail.com>
2017-06-10 01:29:23 +0200

    Cleanup

--- homeassistant/components/homematic.py
+++ homeassistant/components/homematic.py
@@ -1,5 +1,5 @@
 """
-Support for Homematic devices.
+Support for HomeMatic devices.
 
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/homematic/
