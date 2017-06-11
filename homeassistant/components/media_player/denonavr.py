b69d47f66d4bccdc325263c813428a5cc3483009
Oliver <scarface-4711@users.noreply.github.com>
2017-06-11 12:02:32 +0200

    Bugfixing with version 0.4.4 of denonavr (#7995)

--- homeassistant/components/media_player/denonavr.py
+++ homeassistant/components/media_player/denonavr.py
@@ -19,7 +19,7 @@ from homeassistant.const import (
     CONF_NAME, STATE_ON)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['denonavr==0.4.3']
+REQUIREMENTS = ['denonavr==0.4.4']
 
 _LOGGER = logging.getLogger(__name__)
 
