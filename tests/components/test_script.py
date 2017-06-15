4cc29dd3bbb45fc69ec7bbc4fe8b9b7bc6f47234
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-06-15 23:29:06 +0200

    fix test p6

--- tests/components/test_script.py
+++ tests/components/test_script.py
@@ -63,6 +63,7 @@ class TestScriptComponent(unittest.TestCase):
 
         self.hass.bus.listen(event, record_event)
 
+        assert setup_component(self.hass, 'group' {'group': {}})
         assert setup_component(self.hass, 'script', {
             'script': {
                 'test': {
@@ -74,8 +75,7 @@ class TestScriptComponent(unittest.TestCase):
                         'event': event,
                     }]
                 }
-            },
-            'group': {},
+            }
         })
 
         script.turn_on(self.hass, ENTITY_ID)
