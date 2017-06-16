49fba3b164721e24a3b47feefd8fb9f3a699b2b5
Diogo Gomes <dgomes@av.it.pt>
2017-06-16 23:16:28 +0100

    Added bytes and packets sensors from IGD

--- homeassistant/components/upnp.py
+++ homeassistant/components/upnp.py
@@ -5,6 +5,7 @@ It will also keep the status of the IGD found.
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/upnp/
 """
+import asyncio
 import logging
 from urllib.parse import urlsplit
 
@@ -12,6 +13,7 @@ import voluptuous as vol
 
 from homeassistant.const import (EVENT_HOMEASSISTANT_STOP)
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import discovery
 
 REQUIREMENTS = ['miniupnpc']
 
@@ -20,38 +22,52 @@ _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = ['api']
 DOMAIN = 'upnp'
 
+DATA_UPNP = 'UPNP'
+
 CONF_ENABLE_PORT_MAPPING = 'port_mapping'
+CONF_UNITS = 'unit'
+
+UNITS = {
+    "Bytes": 1,
+    "KBytes": 1024,
+    "MBytes": 1024**2,
+    "GBytes": 1024**3,
+}
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Optional(CONF_ENABLE_PORT_MAPPING, default=True): cv.boolean,
+        vol.Optional(CONF_UNITS, default="MBytes"): vol.In(UNITS),
     }),
 }, extra=vol.ALLOW_EXTRA)
 
 
 # pylint: disable=import-error, no-member, broad-except
-def setup(hass, config):
+@asyncio.coroutine
+def async_setup(hass, config):
     """Register a port mapping for Home Assistant via UPnP."""
     import miniupnpc
 
     upnp = miniupnpc.UPnP()
+    hass.data[DATA_UPNP] = upnp
 
     upnp.discoverdelay = 200
     upnp.discover()
     try:
         upnp.selectigd()
-        hass.states.set(DOMAIN+".igd", upnp.statusinfo()[0])
-
     except Exception:
         _LOGGER.exception("Error when attempting to discover an UPnP IGD")
         return False
 
-    base_url = urlsplit(hass.config.api.base_url)
-    host = base_url.hostname
-    external_port = internal_port = base_url.port
+    unit = config[DOMAIN].get(CONF_UNITS)
+    discovery.load_platform(hass, 'sensor', DOMAIN, {'unit': unit }, config)
 
     port_mapping = config[DOMAIN].get(CONF_ENABLE_PORT_MAPPING)
     if port_mapping: 
+        base_url = urlsplit(hass.config.api.base_url)
+        host = base_url.hostname
+        external_port = internal_port = base_url.port
+
         upnp.addportmapping(
             external_port, 'TCP', host, internal_port, 'Home Assistant', '')
 
@@ -62,3 +78,4 @@ def setup(hass, config):
         hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, deregister_port)
 
     return True
+
