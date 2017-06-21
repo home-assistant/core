2289fd05a02b9899a287c4fd52550feda9ceb771
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-06-21 16:49:07 +0200

    fix lint part 2

--- homeassistant/components/sensor/bme280.py
+++ homeassistant/components/sensor/bme280.py
@@ -79,6 +79,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
                  default=DEFAULT_DELTA_TEMP): vol.Coerce(float),
 })
 
+
 # pylint: disable=import-error
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
