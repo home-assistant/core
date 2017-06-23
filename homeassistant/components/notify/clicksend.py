8e870b9486651a70acf79f14641ed92904506ed4
Fabian Affolter <mail@fabian-affolter.ch>
2017-06-23 22:20:05 +0200

    Remove obvious comments and set constant

--- homeassistant/components/notify/clicksend.py
+++ homeassistant/components/notify/clicksend.py
@@ -4,28 +4,25 @@ Clicksend platform for notify component.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/notify.clicksend/
 """
-
-# Import dependencies.
-import logging
 import json
+import logging
 import requests
 
 import voluptuous as vol
 
-from homeassistant.const import (CONF_USERNAME, CONF_API_KEY,
-                                 CONF_RECIPIENT, HTTP_HEADER_CONTENT_TYPE,
-                                 CONTENT_TYPE_JSON)
 import homeassistant.helpers.config_validation as cv
+from homeassistant.const import (
+    CONF_USERNAME, CONF_API_KEY, CONF_RECIPIENT, HTTP_HEADER_CONTENT_TYPE,
+    CONTENT_TYPE_JSON)
 from homeassistant.components.notify import (
     PLATFORM_SCHEMA, BaseNotificationService)
 
-# Get logger instance.
 _LOGGER = logging.getLogger(__name__)
 
-# Set platform  parameters.
 BASE_API_URL = 'https://rest.clicksend.com/v3'
 
-# Validate parameter schema.
+HEADERS = {HTTP_HEADER_CONTENT_TYPE: CONTENT_TYPE_JSON}
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string,
     vol.Required(CONF_API_KEY): cv.string,
@@ -36,7 +33,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 def get_service(hass, config, discovery_info=None):
     """Get the ClickSend notification service."""
     if _authenticate(config) is False:
-        _LOGGER.exception("You are not authorized to access ClickSend.")
+        _LOGGER.exception("You are not authorized to access ClickSend")
         return None
 
     return ClicksendNotificationService(config)
@@ -56,18 +53,15 @@ class ClicksendNotificationService(BaseNotificationService):
         data = ({'messages': [{'source': 'hass.notify', 'from': self.recipient,
                                'to': self.recipient, 'body': message}]})
 
-        headers = {HTTP_HEADER_CONTENT_TYPE: CONTENT_TYPE_JSON}
-
         api_url = "{}/sms/send".format(BASE_API_URL)
 
-        resp = requests.post(api_url, data=json.dumps(data), headers=headers,
+        resp = requests.post(api_url, data=json.dumps(data), headers=HEADERS,
                              auth=(self.username, self.api_key), timeout=5)
 
         obj = json.loads(resp.text)
         response_msg = obj['response_msg']
         response_code = obj['response_code']
 
-        # Display error when failed.
         if resp.status_code != 200:
             _LOGGER.error("Error %s : %s (Code %s)", resp.status_code,
                           response_msg, response_code)
@@ -76,9 +70,7 @@ class ClicksendNotificationService(BaseNotificationService):
 def _authenticate(config):
     """Authenticate with ClickSend."""
     api_url = '{}/account'.format(BASE_API_URL)
-    headers = {HTTP_HEADER_CONTENT_TYPE: CONTENT_TYPE_JSON}
-
-    resp = requests.get(api_url, headers=headers,
+    resp = requests.get(api_url, headers=HEADERS,
                         auth=(config.get(CONF_USERNAME),
                               config.get(CONF_API_KEY)), timeout=5)
 
