f38a52b08d8a8bfa13a36c72564666234e086330
azogue <eugenio.panadero@gmail.com>
2017-06-26 12:14:50 +0200

    add new telegram_bot platform to only send messages

---
+++ homeassistant/components/telegram_bot/broadcast.py
@@ -0,0 +1,26 @@
+"""
+Telegram bot implementation to send messages only.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/telegram_bot.broadcast/
+"""
+import asyncio
+import logging
+
+from homeassistant.components.telegram_bot import (
+    PLATFORM_SCHEMA as TELEGRAM_PLATFORM_SCHEMA)
+from homeassistant.const import CONF_API_KEY
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORM_SCHEMA = TELEGRAM_PLATFORM_SCHEMA
+
+
+@asyncio.coroutine
+def async_setup_platform(hass, config):
+    """Set up the Telegram broadcast platform."""
+    # Check the API key works
+    import telegram
+    bot = telegram.Bot(config[CONF_API_KEY])
+    _LOGGER.debug("Telegram broadcast platform setup with bot %s", bot.name)
+    return True
