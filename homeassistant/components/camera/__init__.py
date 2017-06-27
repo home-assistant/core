33362c987a1f51551f69ba9f607ddfa492a87337
Viswanathan Swaminathan <viswa.swami@gmail.com>
2017-06-27 16:03:09 -0400

    Fixing the hound bot comment

--- homeassistant/components/camera/__init__.py
+++ homeassistant/components/camera/__init__.py
@@ -57,13 +57,15 @@ CAMERA_SERVICE_SCHEMA = vol.Schema({
 def enable_motion_detection(hass, entity_id=None):
     """Enable Motion Detection."""
     data = {ATTR_ENTITY_ID: entity_id} if entity_id else None
-    hass.async_add_job(hass.services.async_call(DOMAIN, SERVICE_EN_MOTION, data))
+    hass.async_add_job(hass.services.async_call(
+        DOMAIN, SERVICE_EN_MOTION, data))
 
 
 def disable_motion_detection(hass, entity_id=None):
     """Disable Motion Detection."""
     data = {ATTR_ENTITY_ID: entity_id} if entity_id else None
-    hass.async_add_job(hass.services.async_call(DOMAIN, SERVICE_DISEN_MOTION, data))
+    hass.async_add_job(hass.services.async_call(
+        DOMAIN, SERVICE_DISEN_MOTION, data))
 
 
 @asyncio.coroutine
