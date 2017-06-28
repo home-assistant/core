e7c8094da9f9a9aaedf0d474d887dd2e4072ef0a
robmarkcole <robmarkcole@gmail.com>
2017-06-28 07:02:20 +0100

    Major update - throttle data object

--- homeassistant/components/sensor/tube_state.py
+++ homeassistant/components/sensor/tube_state.py
@@ -8,6 +8,7 @@ import logging
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.const import ATTR_ATTRIBUTION
+from homeassistant.util import Throttle
 
 from datetime import datetime, timedelta
 import requests
@@ -22,13 +23,13 @@ TUBE_LINES= ['Bakerloo',
              'Central',
              'Circle',
              'District',
-             'Hammersmith-city',
+             'Hammersmith & City',
              'Jubilee',
              'Metropolitan',
              'Northern',
              'Piccadilly',
              'Victoria',
-             'Waterloo-city']
+             'Waterloo & City']
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -40,9 +41,12 @@ _LOGGER = logging.getLogger(__name__)      # enable logging to console
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
+
+    data = TubeData()  # fetch the data dict
+    data.update()      # Get the init data
     sensors = []
-    for tube in config.get(CONF_LINE):
-        sensors.append(LondonTubeSensor(tube))
+    for line in config.get(CONF_LINE):
+        sensors.append(LondonTubeSensor(line, data))
 
     add_devices(sensors, True)
     _LOGGER.info("The tube_state component is ready!")
@@ -51,22 +55,21 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 class LondonTubeSensor(Entity):    # Entity
     """
-    Sensor that reads the status of a tube lines using the TFL API.
+    Sensor that reads the status of a line from TubeData.
     """
-    API_URL_BASE = "https://api.tfl.gov.uk/line/{}/status"
+
     ICON = 'mdi:subway'
 
-    def __init__(self, name):
+    def __init__(self, name, data):
         """Initialize the sensor."""
-        self._name = name             # the name of the line
-        self._data = {}
-        self._url = self.API_URL_BASE
-        self._state = 'Updating'
-        self._description = ['Updating']
+        self._name = name             # the name of the line from the allowed list
+        self._data = data
+        self._state = None
+        self._description = None
 
     @property
     def name(self):
-        """Return the line name of the sensor."""
+        """Return the name of the sensor."""
         return self._name
 
     @property
@@ -87,21 +90,48 @@ class LondonTubeSensor(Entity):    # Entity
         return attrs
 
     def update(self):
-        """Perform an API request and update the sensor."""
-        response = requests.get(self._url.format(self._name.lower()))
+        """Update the sensor."""
+        self._data.update()      # update the data object
+        self._state = self._data.data[self.name]['State']
+        self._description = self._data.data[self.name]['Description']
 
+
+class TubeData(object):
+    """Get the latest tube data from TFL."""
+
+    def __init__(self):
+        """Initialize the data object."""
+        self.data = None
+
+    @Throttle(SCAN_INTERVAL)  # update only once in scan interval
+    def update(self):
+        """Get the latest data from TFL."""
+        URL = 'https://api.tfl.gov.uk/line/mode/tube/status'
+        response = requests.get(URL)
+        _LOGGER.info("TFL Request made")
         if response.status_code != 200:
             _LOGGER.warning("Invalid response from API")
-
+            #print("Invalid response from API")
         else:
-            self._data = response.json()[0]['lineStatuses']
-            statuses = [status['statusSeverityDescription']
-                        for status in self._data] # get all statuses on a line
+            self.data = parse_API_response(response.json())
 
-            if 'Good Service' in statuses:
-                self._state = 'Good Service'
-                self._description = 'Nothing to report'
-            else:
-                self._state = ' + '.join(sorted(set(statuses)))
-                self._description = [status['reason'] for status
-                                     in self._data] # get the reasons
+
+def parse_API_response(response):
+    '''Take in the TFL API json response to
+       https://api.tfl.gov.uk/line/mode/tube/status'''
+    lines = [line['name'] for line in response]    # All available lines
+    data_dict = dict.fromkeys(lines)
+
+    for line in response:                     # Assign data via key
+        statuses = [status['statusSeverityDescription'] for status in line['lineStatuses']]
+        state = ' + '.join(sorted(set(statuses)))
+
+        if state == 'Good Service':   # if good status, this is the only status returned
+            reason =  'Nothing to report'
+        else:
+            reason = ' *** '.join([status['reason'] for status in line['lineStatuses']])
+
+        attr = {'State': state, 'Description': reason}
+        data_dict[line['name']] = attr
+
+    return data_dict
