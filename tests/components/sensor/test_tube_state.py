90ee414174a39e22736d318b6d4556ddb88f1548
robmarkcole <robmarkcole@gmail.com>
2017-07-02 07:26:18 +0100

    Make requested changes to test, not working
    Test fails with:
    
    AssertionError: assert 0 > 0
    where 0 = len([])
    
    Surely I need tube_state.setup_platform ?

--- tests/components/sensor/test_tube_state.py
+++ tests/components/sensor/test_tube_state.py
@@ -11,20 +11,10 @@ from tests.common import load_fixture, get_test_home_assistant
 class TestLondonTubeSensor(unittest.TestCase):
     """Test the tube_state platform."""
 
-    def add_entities(self, new_entities, update_before_add=False):
-        """Mock add entities."""
-        if update_before_add:
-            for entity in new_entities:
-                entity.update()
-
-        for entity in new_entities:
-            self.entities.append(entity)
-
     def setUp(self):
         """Initialize values for this testcase class."""
         self.hass = get_test_home_assistant()
         self.config = {CONF_LINE: ['London Overground']}
-        self.entities = []
 
     def tearDown(self):
         """Stop everything that was started."""
@@ -34,16 +24,11 @@ class TestLondonTubeSensor(unittest.TestCase):
     def test_setup(self, mock_req):
         """Test for operational tube_state sensor with proper attributes."""
         mock_req.get(URL, text=load_fixture('tube_state.json'))
-        tube_state.setup_platform(self.hass, self.config, self.add_entities)
-        self.assertEqual(len(self.entities), 1)
         self.assertTrue(
             setup_component(self.hass, 'sensor', {'tube_state': self.config}))
-        # state = self.hass.states.get('sensor.london_overground')
-        # assert state.state == 'Minor Delays'
-        # assert state.attributes.get('Description') == 'something'
 
-        sensor = self.entities[0]
-        self.assertEqual(sensor.name, 'London Overground')
-        self.assertEqual(sensor.state, 'Minor Delays')
-        self.assertEqual(sensor.device_state_attributes['Description'],
-                         'something')
+        ids = self.hass.states.entity_ids()
+        assert len(ids) > 0
+        state = self.hass.states.get('sensor.london_overground')
+        assert state.state == 'Minor Delays'
+        assert state.attributes.get('Description') == 'something'
