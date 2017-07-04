85ebbe655dca9c96a117f68cec202ac93aba92b5
James Nimmo <james@nimmo.net.nz>
2017-07-04 21:13:27 +1200

    Added required blank line

--- homeassistant/components/climate/intesishome.py
+++ homeassistant/components/climate/intesishome.py
@@ -77,6 +77,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 class IntesisAC(ClimateDevice):
     """Representation of an IntesisHome thermostat."""
+    
     def __init__(self, deviceid, device):
         """Initialize the thermostat."""
         _LOGGER.debug('Added climate device with state: %s', repr(device))
