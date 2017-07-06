a61256ded3ce9db8edad6603a4940cfd18f57155
jeroen <jeroen_ter_heerdt@hotmail.com>
2017-07-06 19:16:48 +0200

    Added support for Egardia / Woonveilig alarm control panel

--- homeassistant/components/alarm_control_panel/egardia.py
+++ homeassistant/components/alarm_control_panel/egardia.py
@@ -1,182 +1,137 @@
 """
-Interfaces with Egardia/Woonveilig alarm control panel.
+Interfaces with Egardia / Woonveilig alarm control panel.
 
-For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/alarm_control_panel.egardia/
+For more details about this platform, please refer to the $
+https://home-assistant.io/components/alarm_control_panel.egardia
 """
+
 import logging
 
-import requests
 import voluptuous as vol
 
 import homeassistant.components.alarm_control_panel as alarm
-import homeassistant.exceptions as exc
-import homeassistant.helpers.config_validation as cv
 from homeassistant.components.alarm_control_panel import PLATFORM_SCHEMA
 from homeassistant.const import (
-    CONF_PORT, CONF_HOST, CONF_PASSWORD, CONF_USERNAME, STATE_UNKNOWN,
-    CONF_NAME, STATE_ALARM_DISARMED, STATE_ALARM_ARMED_HOME,
-    STATE_ALARM_ARMED_AWAY, STATE_ALARM_TRIGGERED)
+    CONF_PORT,CONF_HOST, CONF_PASSWORD, CONF_USERNAME, STATE_UNKNOWN, CONF_CODE, CONF_NAME,
+    STATE_ALARM_DISARMED, STATE_ALARM_ARMED_HOME, STATE_ALARM_ARMED_AWAY,
+    EVENT_HOMEASSISTANT_STOP)
+import homeassistant.helpers.config_validation as cv
+import homeassistant.loader as loader
 
-REQUIREMENTS = ['pythonegardia==1.0.18']
+#REQUIREMENTS = ['egardia-python==1.0.0']
+REQUIREMENTS = ['requests']
+#PYTHON IMPORTS (should be 3rd party!)
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_REPORT_SERVER_CODES = 'report_server_codes'
-CONF_REPORT_SERVER_ENABLED = 'report_server_enabled'
-CONF_REPORT_SERVER_PORT = 'report_server_port'
-
 DEFAULT_NAME = 'Egardia'
-DEFAULT_PORT = 80
-DEFAULT_REPORT_SERVER_ENABLED = False
-DEFAULT_REPORT_SERVER_PORT = 85
+DEFAULT_PORT = '80'
 DOMAIN = 'egardia'
-
 NOTIFICATION_ID = 'egardia_notification'
 NOTIFICATION_TITLE = 'Egardia'
 
-STATES = {
-    'ARM': STATE_ALARM_ARMED_AWAY,
-    'DAY HOME': STATE_ALARM_ARMED_HOME,
-    'DISARM': STATE_ALARM_DISARMED,
-    'HOME': STATE_ALARM_ARMED_HOME,
-    'TRIGGERED': STATE_ALARM_TRIGGERED,
-    'UNKNOWN': STATE_UNKNOWN,
-}
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string,
-    vol.Required(CONF_PASSWORD): cv.string,
-    vol.Required(CONF_USERNAME): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-    vol.Optional(CONF_REPORT_SERVER_CODES): vol.All(cv.ensure_list),
-    vol.Optional(CONF_REPORT_SERVER_ENABLED,
-                 default=DEFAULT_REPORT_SERVER_ENABLED): cv.boolean,
-    vol.Optional(CONF_REPORT_SERVER_PORT, default=DEFAULT_REPORT_SERVER_PORT):
-        cv.port,
+vol.Optional(CONF_NAME,default=DEFAULT_NAME):cv.string,
+vol.Required(CONF_PASSWORD): cv.string,
+vol.Required(CONF_USERNAME): cv.string,
+vol.Required(CONF_HOST): cv.string,
+vol.Optional(CONF_PORT,default=DEFAULT_PORT):cv.string,
 })
 
-
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up the Egardia platform."""
-    from pythonegardia import egardiadevice
-
-    name = config.get(CONF_NAME)
-    username = config.get(CONF_USERNAME)
-    password = config.get(CONF_PASSWORD)
-    host = config.get(CONF_HOST)
-    port = config.get(CONF_PORT)
-    rs_enabled = config.get(CONF_REPORT_SERVER_ENABLED)
-    rs_port = config.get(CONF_REPORT_SERVER_PORT)
-    rs_codes = config.get(CONF_REPORT_SERVER_CODES)
-
-    try:
-        egardiasystem = egardiadevice.EgardiaDevice(
-            host, port, username, password, '')
-    except requests.exceptions.RequestException:
-        raise exc.PlatformNotReady()
-    except egardiadevice.UnauthorizedError:
-        _LOGGER.error("Unable to authorize. Wrong password or username")
-        return False
-
-    add_devices([EgardiaAlarm(
-        name, egardiasystem, hass, rs_enabled, rs_port, rs_codes)], True)
-
+	"""Set up the Egardia platform."""
+	name = config.get(CONF_NAME)
+	username = config.get(CONF_USERNAME)
+	password = config.get(CONF_PASSWORD)
+	host = config.get(CONF_HOST)
+	port = config.get(CONF_PORT)
+	add_devices([EgardiaAlarm(name,host,port,username,password)])
 
 class EgardiaAlarm(alarm.AlarmControlPanel):
-    """Representation of a Egardia alarm."""
+	"""Representation of a Egardia alarm."""
+	def __init__(self, name,host,port,username,password):
+		self._host = host
+		self._port = port
+		self._name = name
+		self._username = username
+		self._password = password
 
-    def __init__(self, name, egardiasystem, hass, rs_enabled=False,
-                 rs_port=None, rs_codes=None):
-        """Initialize object."""
-        self._name = name
-        self._egardiasystem = egardiasystem
-        self._status = STATE_UNKNOWN
-        self._rs_enabled = rs_enabled
-        self._rs_port = rs_port
-        self._hass = hass
+	@property
+	def name(self):
+		"""Return the name of the device."""
+		return self._name
 
-        if rs_codes is not None:
-            self._rs_codes = rs_codes[0]
-        else:
-            self._rs_codes = rs_codes
+	@property
+	def state(self):
+		"""Return the state of the device."""
+		self.update()
+		if self._status == 'ARM':
+			state = STATE_ALARM_ARMED_AWAY
+		elif self._status == 'HOME':
+			state = STATE_ALARM_ARMED_HOME
+		elif self._status == 'DAY HOME':
+			state = STATE_ALARM_ARMED_HOME
+		elif self._status == 'NIGHT HOME':
+			state = STATE_ALARM_ARMED_HOME
+		elif self._status == 'DISARM':
+			state = STATE_ALARM_DISARMED
+		else:
+			state = STATE_UNKNOWN
+		return state
 
-        if self._rs_enabled:
-            self.listen_to_system_status()
+	def getState(self):
+		import requests
+		#Get status
+		r = self.doRequest('get', 'panelCondGet')
+		statustext = r.text
+		ind1 = statustext.find('mode_a1 : "')
+		statustext = statustext[ind1+11:]
+		ind2 = statustext.find('"')
+		status = statustext[:ind2]
+		_LOGGER.info("Egardia alarm status: "+status)
+		return status.upper()
 
-    @property
-    def name(self):
-        """Return the name of the device."""
-        return self._name
+	def doRequest(self,requestType,action, payload = None):
+		import requests
+		requestType = requestType.upper()
+		_LOGGER.info("Egardia doRequest, type: "+requestType+", url: "+self.buildURL()+action+", payload: "+str(payload)+", auth=("+self._username+","+self._password+")")
+		if requestType =='GET':
+			return requests.get(self.buildURL()+action,auth=(self._username,self._password))
+		elif requestType == 'POST':
+			return requests.post(self.buildURL()+action,data=payload, auth=(self._username,self._password))
+		else:
+			return None
+	def buildURL(self):
+		return 'http://'+self._host+':'+self._port+'/action/'
 
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return self._status
+	def update(self):
+		"""Update the alarm status."""
+		self._status = self.getState()
+ 
+	def alarm_disarm(self, code=None):
+		"""Send disarm command."""
+		r = self.sendCondition(4)
+		_LOGGER.info("Egardia alarm disarming, result: "+r)
 
-    def handle_system_status_event(self, event):
-        """Handle egardia_system_status_event."""
-        if event.data.get('status') is not None:
-            statuscode = event.data.get('status')
-            status = self.lookupstatusfromcode(statuscode)
-            self.parsestatus(status)
+	def alarm_arm_home(self, code=None):
+		"""Send arm home command."""
+		r = self.sendCondition(1)
+		_LOGGER.info("Egardia alarm arming home, result: "+r)
 
-    def listen_to_system_status(self):
-        """Subscribe to egardia_system_status event."""
-        self._hass.bus.listen(
-            'egardia_system_status', self.handle_system_status_event)
+	def alarm_arm_away(self, code=None):
+		"""Send arm away command."""
+		#ARM the alarm
+		r = self.sendCondition(0)
+		_LOGGER.info("Egardia alarm arming away, result: "+r)
 
-    def lookupstatusfromcode(self, statuscode):
-        """Look at the rs_codes and returns the status from the code."""
-        status = 'UNKNOWN'
-        if self._rs_codes is not None:
-            statuscode = str(statuscode).strip()
-            for i in self._rs_codes:
-                val = str(self._rs_codes[i]).strip()
-                if ',' in val:
-                    splitted = val.split(',')
-                    for code in splitted:
-                        code = str(code).strip()
-                        if statuscode == code:
-                            status = i.upper()
-                            break
-                elif statuscode == val:
-                    status = i.upper()
-                    break
-        return status
-
-    def parsestatus(self, status):
-        """Parse the status."""
-        newstatus = ([v for k, v in STATES.items()
-                      if status.upper() == k][0])
-        self._status = newstatus
-
-    def update(self):
-        """Update the alarm status."""
-        status = self._egardiasystem.getstate()
-        self.parsestatus(status)
-
-    def alarm_disarm(self, code=None):
-        """Send disarm command."""
-        try:
-            self._egardiasystem.alarm_disarm()
-        except requests.exceptions.RequestException as err:
-            _LOGGER.error("Egardia device exception occurred when "
-                          "sending disarm command: %s", err)
-
-    def alarm_arm_home(self, code=None):
-        """Send arm home command."""
-        try:
-            self._egardiasystem.alarm_arm_home()
-        except requests.exceptions.RequestException as err:
-            _LOGGER.error("Egardia device exception occurred when "
-                          "sending arm home command: %s", err)
-
-    def alarm_arm_away(self, code=None):
-        """Send arm away command."""
-        try:
-            self._egardiasystem.alarm_arm_away()
-        except requests.exceptions.RequestException as err:
-            _LOGGER.error("Egardia device exception occurred when "
-                          "sending arm away command: %s", err)
+	def sendCondition(self,p):
+		import requests
+		#Send payload to panelCondPost
+		payload = {'area': '1', 'mode': p}
+		r = self.doRequest('POST', 'panelCondPost', payload)
+		statustext = r.text
+		ind1 = statustext.find('result : ')
+		statustext = statustext[ind1+9:]
+		ind2 = statustext.find(',')
+		statustext = statustext[:ind2]
+		return statustext
