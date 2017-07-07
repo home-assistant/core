d571ffc4855ca6fd6962f4f8d39e65807816e514
Andrew <andrewneisch@gmail.com>
2017-07-07 13:38:06 -0500

    Update radiotherm.py

--- homeassistant/components/climate/radiotherm.py
+++ homeassistant/components/climate/radiotherm.py
@@ -1,5 +1,6 @@
 """
 Support for Radio Thermostat wifi-enabled home thermostats.
+
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/climate.radiotherm/
 """
@@ -219,6 +220,7 @@ class RadioThermostat(ClimateDevice):
 
     def turn_away_mode_on(self):
         """Turn away on.
+        
         The RTCOA app simulates away mode by using a hold.
         """
         away_temp = None
