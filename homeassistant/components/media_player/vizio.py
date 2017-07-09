4f790771815c948d92a8f5be16426777e86345ae
Vlad Kornev <vladimir.kornev@gmail.com>
2017-07-09 06:05:58 -0700

    Requested changes Added new config params

--- homeassistant/components/media_player/vizio.py
+++ homeassistant/components/media_player/vizio.py
@@ -32,13 +32,16 @@ from homeassistant.const import (
 )
 from homeassistant.helpers import config_validation as cv
 
-REQUIREMENTS = ['git+https://github.com/vkorn/pyvizio.git'
-                '@master#pyvizio==0.0.1']
+REQUIREMENTS = ['pyvizio==0.0.2']
 
 _LOGGER = logging.getLogger(__name__)
 
+CONF_SUPPRESS_WARNING = 'suppress_warning'
+CONF_VOLUME_STEP = 'volume_step'
+
 ICON = 'mdi:television'
 DEFAULT_NAME = 'Vizio SmartCast'
+DEFAULT_VOLUME_STEP = 1
 DEVICE_NAME = 'Python Vizio'
 DEVICE_ID = 'pyvizio'
 MIN_TIME_BETWEEN_SCANS = timedelta(seconds=10)
@@ -52,34 +55,45 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
     vol.Required(CONF_ACCESS_TOKEN): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_SUPPRESS_WARNING, default=False): cv.boolean,
+    vol.Optional(CONF_VOLUME_STEP, default=DEFAULT_VOLUME_STEP):
+        vol.All(vol.Coerce(int), vol.Range(min=1, max=10)),
 })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the VizioTV media player platform."""
     host = config.get(CONF_HOST)
-    if host is None:
-        _LOGGER.error('No host info')
-        return False
     token = config.get(CONF_ACCESS_TOKEN)
-    if token is None:
-        _LOGGER.error('No token info')
-        return False
     name = config.get(CONF_NAME)
+    volume_step = config.get(CONF_VOLUME_STEP)
 
-    add_devices([VizioDevice(host, token, name)], True)
+    device = VizioDevice(host, token, name, volume_step)
+    if device.validate_setup() is False:
+        _LOGGER.error('Failed to setup Vizio TV platform, '
+                      'please check if host and API key are correct.')
+        return False
+
+    if config.get(CONF_SUPPRESS_WARNING):
+        import requests
+        from requests.packages.urllib3.exceptions import InsecureRequestWarning
+        _LOGGER.warning('InsecureRequestWarning is disabled '
+                        'because of Vizio platform configuration.')
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    add_devices([device], True)
 
 
 class VizioDevice(MediaPlayerDevice):
     """Media Player implementation which performs REST requests to TV."""
 
-    def __init__(self, host, token, name):
+    def __init__(self, host, token, name, volume_step):
         """Initialize Vizio device."""
         import pyvizio
         self._device = pyvizio.Vizio(DEVICE_ID, host, DEFAULT_NAME, token)
         self._name = name
         self._state = STATE_UNKNOWN
         self._volume_level = None
+        self._volume_step = volume_step
         self._current_input = None
         self._available_inputs = None
 
@@ -164,8 +178,12 @@ class VizioDevice(MediaPlayerDevice):
 
     def volume_up(self):
         """Increasing volume of the TV."""
-        self._device.vol_up()
+        self._device.vol_up(num=self._volume_step)
 
     def volume_down(self):
         """Decreasing volume of the TV."""
-        self._device.vol_down()
+        self._device.vol_down(num=self._volume_step)
+
+    def validate_setup(self):
+        """Validating if host is available and key is correct."""
+        return self._device.get_current_volume() is not None
