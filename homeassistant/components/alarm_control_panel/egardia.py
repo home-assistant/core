11f98bbef902664122c0bec296ee301f2cc79817
jeroen <jeroen_ter_heerdt@hotmail.com>
2017-07-11 15:54:18 +0200

    Adding exception handling

--- homeassistant/components/alarm_control_panel/egardia.py
+++ homeassistant/components/alarm_control_panel/egardia.py
@@ -19,7 +19,7 @@ from homeassistant.const import (
 import homeassistant.helpers.config_validation as cv
 import homeassistant.exceptions as exc
 
-REQUIREMENTS = ['pythonegardia==1.0.8']
+REQUIREMENTS = ['pythonegardia==1.0.9']
 
 _LOGGER = logging.getLogger(__name__)
 
