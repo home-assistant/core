23b9bf6f4a09f3a7351744ceb173a7716a328425
minchik <minchik@gmail.com>
2017-07-18 16:05:05 +0300

    Encapsulate logic inside MediaExtractor class

--- homeassistant/components/media_extractor.py
+++ homeassistant/components/media_extractor.py
@@ -21,8 +21,8 @@ DOMAIN = 'media_extractor'
 DEPENDENCIES = ['media_player']
 
 CONF_CUSTOMIZE_ENTITIES = 'customize'
-CONF_DEFAULT_STREAM_FORMAT = 'default_format'
-DEFAULT_STREAM_FORMAT = 'best'
+CONF_DEFAULT_STREAM_QUERY = 'default_query'
+DEFAULT_STREAM_QUERY = 'best'
 
 
 def setup(hass, config):
@@ -33,42 +33,7 @@ def setup(hass, config):
 
     def play_media(call):
         """Get stream URL and send it to the media_player.play_media."""
-        media_url = call.data.get(ATTR_MEDIA_CONTENT_ID)
-
-        try:
-            stream_selector = get_stream_query_selector(media_url)
-        except YDNetworkException:
-            _LOGGER.error("Could not retrieve data for the URL: %s",
-                          media_url)
-            return
-
-        media_content_type = call.data.get(ATTR_MEDIA_CONTENT_TYPE)
-        default_stream_format = config[DOMAIN].get(
-            CONF_DEFAULT_STREAM_FORMAT, DEFAULT_STREAM_FORMAT)
-        entities_config = config[DOMAIN].get(CONF_CUSTOMIZE_ENTITIES, {})
-        entities = call.data.get(ATTR_ENTITY_ID, [])
-
-        if len(entities) == 0:
-            pass
-
-        for entity_id in entities:
-            stream_format_query = entities_config.get(
-                entity_id, {}).get(media_content_type, default_stream_format)
-
-            try:
-                stream_url = stream_selector(stream_format_query)
-            except YDQueryException:
-                continue
-            else:
-                data = {k: v for k, v in call.data.items()
-                        if k != ATTR_MEDIA_CONTENT_ID and k != ATTR_ENTITY_ID}
-                data[ATTR_MEDIA_CONTENT_ID] = stream_url
-                data[ATTR_ENTITY_ID] = entity_id
-
-                hass.async_add_job(
-                    hass.services.async_call(
-                        MEDIA_PLAYER_DOMAIN, SERVICE_PLAY_MEDIA, data)
-                )
+        MediaExtractor(hass, config[DOMAIN], call.data).extract_and_send()
 
     hass.services.register(DOMAIN,
                            SERVICE_PLAY_MEDIA,
@@ -79,63 +44,135 @@ def setup(hass, config):
     return True
 
 
-class YDNetworkException(Exception):
-    """Media extractor network exception."""
+class MEDownloadException(Exception):
+    """Media extractor download exception."""
 
     pass
 
 
-class YDQueryException(Exception):
+class MEQueryException(Exception):
     """Media extractor query exception."""
 
     pass
 
 
-def get_stream_query_selector(media_url):
-    """Extract stream URL from the media URL."""
-    from youtube_dl import YoutubeDL
-    from youtube_dl.utils import DownloadError, ExtractorError
+class MediaExtractor:
+    """Class which encapsulates all extraction logic."""
 
-    ydl = YoutubeDL({'quiet': True, 'logger': _LOGGER})
+    def __init__(self, hass, component_config, call_data):
+        """Initialize media extractor."""
+        self.hass = hass
+        self.config = component_config
+        self.call_data = call_data
 
-    try:
-        all_media_streams = ydl.extract_info(media_url, process=False)
-    except DownloadError:
-        # This exception will be logged by youtube-dl itself
-        raise YDNetworkException()
+    def get_media_url(self):
+        """Return media content url."""
+        return self.call_data.get(ATTR_MEDIA_CONTENT_ID)
 
-    if 'entries' in all_media_streams:
-        _LOGGER.warning("Playlists are not supported, "
-                        "looking for the first video")
+    def get_entities(self):
+        """Return list of entities."""
+        return self.call_data.get(ATTR_ENTITY_ID, [])
+
+    def extract_and_send(self):
+        """Extract exact stream format for each entity_id and play it."""
         try:
-            selected_stream = next(all_media_streams['entries'])
-        except StopIteration:
-            _LOGGER.error("Playlist is empty")
-            raise YDNetworkException()
-    else:
-        selected_stream = all_media_streams
+            stream_selector = self.get_stream_selector()
+        except MEDownloadException:
+            _LOGGER.error("Could not retrieve data for the URL: %s",
+                          self.get_media_url())
+        else:
+            entities = self.get_entities()
 
-    try:
-        media_info = ydl.process_ie_result(selected_stream, download=False)
-    except (ExtractorError, DownloadError):
-        # This exception will be logged by youtube-dl itself
-        raise YDNetworkException()
+            if len(entities) == 0:
+                self.call_media_player_service(stream_selector, None)
 
-    def stream_query_selector(stream_format_query):
-        """Find stream url that match stream_format_query."""
+            for entity_id in entities:
+                self.call_media_player_service(stream_selector, entity_id)
+
+    def get_stream_selector(self):
+        """Return format selector for the media URL."""
+        from youtube_dl import YoutubeDL
+        from youtube_dl.utils import DownloadError, ExtractorError
+
+        ydl = YoutubeDL({'quiet': True, 'logger': _LOGGER})
+
         try:
-            format_selector = ydl.build_format_selector(stream_format_query)
-        except (SyntaxError, ValueError) as e:
-            _LOGGER.error(e)
-            raise YDQueryException()
+            all_media = ydl.extract_info(self.get_media_url(),
+                                         process=False)
+        except DownloadError:
+            # This exception will be logged by youtube-dl itself
+            raise MEDownloadException()
+
+        if 'entries' in all_media:
+            _LOGGER.warning("Playlists are not supported, "
+                            "looking for the first video")
+            try:
+                selected_media = next(all_media['entries'])
+            except StopIteration:
+                _LOGGER.error("Playlist is empty")
+                raise MEDownloadException()
+        else:
+            selected_media = all_media
 
         try:
-            queried_stream = next(format_selector(media_info))
-        except (KeyError, StopIteration):
-            _LOGGER.error("Could not extract stream for the query: %s",
-                          stream_format_query)
-            raise YDQueryException()
+            media_info = ydl.process_ie_result(selected_media,
+                                               download=False)
+        except (ExtractorError, DownloadError):
+            # This exception will be logged by youtube-dl itself
+            raise MEDownloadException()
 
-        return queried_stream['url']
+        def stream_selector(query):
+            """Find stream url that matches query."""
+            try:
+                format_selector = ydl.build_format_selector(query)
+            except (SyntaxError, ValueError, AttributeError) as ex:
+                _LOGGER.error(ex)
+                raise MEQueryException()
 
-    return stream_query_selector
+            try:
+                requested_stream = next(format_selector(media_info))
+            except (KeyError, StopIteration):
+                _LOGGER.error("Could not extract stream for the query: %s",
+                              query)
+                raise MEQueryException()
+
+            return requested_stream['url']
+
+        return stream_selector
+
+    def call_media_player_service(self, stream_selector, entity_id):
+        """Call media_player.play_media service."""
+        stream_query = self.get_stream_query_for_entity(entity_id)
+
+        try:
+            stream_url = stream_selector(stream_query)
+        except MEQueryException:
+            _LOGGER.error("Wrong query format: %s", stream_query)
+            return
+        else:
+            data = {k: v for k, v in self.call_data.items()
+                    if k != ATTR_ENTITY_ID}
+            data[ATTR_MEDIA_CONTENT_ID] = stream_url
+
+            if entity_id:
+                data[ATTR_ENTITY_ID] = entity_id
+
+            self.hass.async_add_job(
+                self.hass.services.async_call(
+                    MEDIA_PLAYER_DOMAIN, SERVICE_PLAY_MEDIA, data)
+            )
+
+    def get_stream_query_for_entity(self, entity_id):
+        """Get stream format query for entity."""
+        default_stream_query = self.config.get(CONF_DEFAULT_STREAM_QUERY,
+                                               DEFAULT_STREAM_QUERY)
+
+        if entity_id:
+            media_content_type = self.call_data.get(ATTR_MEDIA_CONTENT_TYPE)
+
+            return self.config \
+                .get(CONF_CUSTOMIZE_ENTITIES, {}) \
+                .get(entity_id, {}) \
+                .get(media_content_type, default_stream_query)
+
+        return default_stream_query
