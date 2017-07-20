32922a542986f5972c0c99be16e689189aab6df4
Viswanathan Swaminathan <viswa.swami@gmail.com>
2017-07-20 12:27:07 -0400

    As per comment from @balloob, Instead of directly using the URL to talk to foscam, used a 3rd party foscam library to communicate with it. This library already has support to enable/disable motion detection and also APIs to change the motion detection schedule etc. Need to add more support in the pyfoscam 3rd party library for checking if motion was detected or even if sound was detected. Once that is done, we can add that into HASS as well.

--- homeassistant/components/camera/foscam.py
+++ homeassistant/components/camera/foscam.py
@@ -16,13 +16,14 @@ from homeassistant.helpers import config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_IP = 'ip'
+REQUIREMENTS = ['pyfoscam==1.2']
 
-CAMERA_ARMED = "armed"
-CAMERA_DISARMED = "disarmed"
+CONF_IP = 'ip'
 
 DEFAULT_NAME = 'Foscam Camera'
 DEFAULT_PORT = 88
+
+FOSCAM_COMM_ERROR = -8
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_IP): cv.string,
@@ -36,97 +37,37 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up a Foscam IP Camera."""
-    add_devices([FoscamCamera(config)])
+    add_devices([FoscamCam(config)])
 
 
-class FoscamCamera(Camera):
+class FoscamCam(Camera):
     """An implementation of a Foscam IP camera."""
 
     def __init__(self, device_info):
         """Initialize a Foscam camera."""
-        super(FoscamCamera, self).__init__()
+        super(FoscamCam, self).__init__()
 
         ip_address = device_info.get(CONF_IP)
         port = device_info.get(CONF_PORT)
-
-        self._base_url = 'http://{}:{}/'.format(ip_address, port)
-
-        uri_template = self._base_url \
-            + 'cgi-bin/CGIProxy.fcgi?' \
-            + 'cmd=snapPicture2&usr={}&pwd={}'
-
         self._username = device_info.get(CONF_USERNAME)
         self._password = device_info.get(CONF_PASSWORD)
-        self._snap_picture_url = uri_template.format(
-            self._username,
-            self._password
-        )
         self._name = device_info.get(CONF_NAME)
         self._motion_status = False
 
-        _LOGGER.info("Using the following URL for %s: %s",
-                     self._name, uri_template.format('***', '***'))
+        from foscam import FoscamCamera
+
+        self._foscam_session = FoscamCamera(ip_address, port, self._username, self._password)
 
     def camera_image(self):
         """Return a still image reponse from the camera."""
         # Send the request to snap a picture and return raw jpg data
         # Handle exception if host is not reachable or url failed
         try:
-            response = requests.get(self._snap_picture_url, timeout=10)
-        except requests.exceptions.ConnectionError:
+            result, response = self._foscam_session.snap_picture_2()
+        except:
             return None
-        else:
-            return response.content
 
-    @property
-    def name(self):
-        """Return the name of this camera."""
-        return self._name
-
-    def set_motion_status(self, mode):
-        """Post to the camera and enable motion detection."""
-        if mode == CAMERA_ARMED:
-            enabled = '1'
-        else:
-            enabled = '0'
-
-        # Fill the URI with the command to enable motion detection
-        # Along with that as per foscam spec we have to set
-        # sensitivity: how much sensitivity camera should detect
-        # trigger interval: interval between each motion triggers
-        # schedule: set schedule for days. default is 24x7
-        # area: sort of like zones. default is all areas visible to cam
-        uri_template = self._base_url \
-            + 'cgi-bin/CGIProxy.fcgi?' \
-            + 'cmd=setMotionDetectConfig' \
-            + '&Enable={}&usr={}&pwd={}' \
-            + '&linkage=0&snapInterval=3' \
-            + '&sensitivity=2&triggerInterval=0' \
-            + '&schedule0=281474976710655' \
-            + '&schedule1=281474976710655' \
-            + '&schedule2=281474976710655' \
-            + '&schedule3=281474976710655' \
-            + '&schedule4=281474976710655' \
-            + '&schedule5=281474976710655' \
-            + '&schedule6=281474976710655' \
-            + '&area0=1024&area1=1023' \
-            + '&area2=1024&area3=1023' \
-            + '&area4=1024&area5=1023' \
-            + '&area6=1024&area7=1023' \
-            + '&area8=1024&area9=1023'
-
-        _set_motion_status_url = uri_template.format(
-            enabled,
-            self._username,
-            self._password
-        )
-
-        try:
-            response = requests.get(_set_motion_status_url, timeout=10)
-        except requests.exceptions.ConnectionError:
-            return None
-        else:
-            return response.content
+        return response
 
     @property
     def motion_detection_enabled(self):
@@ -135,10 +76,21 @@ class FoscamCamera(Camera):
 
     def enable_motion_detection(self):
         """Enable motion detection in camera."""
-        self._motion_status = True
-        self.set_motion_status(CAMERA_ARMED)
+        try:
+            self._foscam_session.enable_motion_detection()
+            self._motion_status = True
+        except:
+            self._motion_status = False
 
     def disable_motion_detection(self):
-        """Disable motion detection in camera."""
-        self._motion_status = False
-        self.set_motion_status(CAMERA_DISARMED)
+        """Disable motion detection."""
+        try:
+            self._foscam_session.disable_motion_detection()
+            self._motion_status = False
+        except:
+            self._motion_status = True
+
+    @property
+    def name(self):
+        """Return the name of this camera."""
+        return self._name
