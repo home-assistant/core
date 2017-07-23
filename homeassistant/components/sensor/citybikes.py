24e8497fb484923690d6639c474fd6df335dcc85
Chia-liang Kao <clkao@users.noreply.github.com>
2017-07-23 03:22:49 -0500

    Fix STATION_SCHEMA validation on longitude (#8610)

--- homeassistant/components/sensor/citybikes.py
+++ homeassistant/components/sensor/citybikes.py
@@ -84,7 +84,7 @@ STATION_SCHEMA = vol.Schema({
     vol.Required(ATTR_FREE_BIKES): cv.positive_int,
     vol.Required(ATTR_EMPTY_SLOTS): cv.positive_int,
     vol.Required(ATTR_LATITUDE): cv.latitude,
-    vol.Required(ATTR_LONGITUDE): cv.latitude,
+    vol.Required(ATTR_LONGITUDE): cv.longitude,
     vol.Required(ATTR_ID): cv.string,
     vol.Required(ATTR_NAME): cv.string,
     vol.Required(ATTR_TIMESTAMP): cv.string,
