4a7415b5404b74bf67d71f63f8cfcada0801c4e5
Julius Mittenzwei <julius@mittenzwei.com>
2017-07-23 21:08:24 +0200

    bumping version to 0.6.1

--- homeassistant/components/xknx/__init__.py
+++ homeassistant/components/xknx/__init__.py
@@ -41,7 +41,7 @@ SUPPORTED_DOMAINS = [
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['xknx==0.6.0']
+REQUIREMENTS = ['xknx==0.6.1']
 
 TUNNELING_SCHEMA = vol.Schema({
     vol.Required(CONF_HOST): cv.string,
