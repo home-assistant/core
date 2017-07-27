d487fd9127f9e146b6ef65c865bbe502a1812604
Julius Mittenzwei <julius@mittenzwei.com>
2017-07-27 23:51:58 +0200

    updated version

--- homeassistant/components/xknx.py
+++ homeassistant/components/xknx.py
@@ -34,7 +34,7 @@ SUPPORTED_DOMAINS = [
 
 _LOGGER = logging.getLogger(__name__)
 
-#REQUIREMENTS = ['xknx==0.6.2']
+#REQUIREMENTS = ['xknx==0.7.0']
 
 TUNNELING_SCHEMA = vol.Schema({
     vol.Required(CONF_HOST): cv.string,
