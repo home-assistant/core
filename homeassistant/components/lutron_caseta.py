a76f6422b22fcfdb1e3be1fe82f88fe13e6523a5
kfcook <809694+kfcook@users.noreply.github.com>
2017-07-28 15:16:12 -0400

    Updated pylutron_caseta version number

--- homeassistant/components/lutron_caseta.py
+++ homeassistant/components/lutron_caseta.py
@@ -14,7 +14,7 @@ from homeassistant.const import CONF_HOST
 from homeassistant.helpers import discovery
 from homeassistant.helpers.entity import Entity
 
-REQUIREMENTS = ['pylutron-caseta==0.2.6']
+REQUIREMENTS = ['pylutron-caseta==0.2.7']
 
 _LOGGER = logging.getLogger(__name__)
 
