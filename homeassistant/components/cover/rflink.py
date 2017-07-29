fab28270f031a74f37fa619aaa4a3f6463db31e2
Pascal Osinga <pascalosinga@iMac-van-Pascal.local>
2017-07-29 10:28:59 +0200

    fixed the errors

--- homeassistant/components/cover/rflink.py
+++ homeassistant/components/cover/rflink.py
@@ -8,13 +8,11 @@ import logging
 
 
 from homeassistant.components.rflink import (
-    CONF_ALIASES, CONF_ALIASSES, CONF_AUTOMATIC_ADD, CONF_DEVICE_DEFAULTS,
-    CONF_DEVICES, CONF_FIRE_EVENT, CONF_GROUP, CONF_GROUP_ALIASES,
-    CONF_GROUP_ALIASSES, CONF_IGNORE_DEVICES, CONF_NOGROUP_ALIASES,
-    CONF_NOGROUP_ALIASSES, CONF_SIGNAL_REPETITIONS, DATA_DEVICE_REGISTER,
-    DATA_ENTITY_GROUP_LOOKUP, DATA_ENTITY_LOOKUP, DEVICE_DEFAULTS_SCHEMA,
-    DOMAIN, EVENT_KEY_COMMAND, EVENT_KEY_ID, CoverableRflinkDevice, cv,
-    remove_deprecated, vol)
+    CONF_ALIASSES, CONF_DEVICE_DEFAULTS, CONF_DEVICES, CONF_FIRE_EVENT,
+    CONF_GROUP, CONF_GROUP_ALIASSES,  CONF_NOGROUP_ALIASSES,
+    CONF_SIGNAL_REPETITIONS, DATA_ENTITY_GROUP_LOOKUP, DATA_ENTITY_LOOKUP,
+    DEVICE_DEFAULTS_SCHEMA, DOMAIN, EVENT_KEY_COMMAND, CoverableRflinkDevice,
+    cv, vol)
 from homeassistant.components.cover import (
     CoverDevice, ATTR_TILT_POSITION, SUPPORT_OPEN_TILT,
     SUPPORT_CLOSE_TILT, SUPPORT_STOP_TILT, SUPPORT_SET_TILT_POSITION,
@@ -26,7 +24,24 @@ DEPENDENCIES = ['rflink']
 
 _LOGGER = logging.getLogger(__name__)
 
-TYPE_RTS = 'RTS'
+
+CONF_ALIASES = 'aliases'
+CONF_ALIASSES = 'aliases'
+CONF_GROUP_ALIASES = 'group_aliases'
+CONF_GROUP_ALIASSES = 'group_aliases'
+CONF_GROUP = 'group'
+CONF_NOGROUP_ALIASES = 'nogroup_aliases'
+CONF_NOGROUP_ALIASSES = 'nogroup_aliases'
+CONF_DEVICE_DEFAULTS = 'device_defaults'
+CONF_DEVICES = 'devices'
+CONF_AUTOMATIC_ADD = 'automatic_add'
+CONF_FIRE_EVENT = 'fire_event'
+CONF_IGNORE_DEVICES = 'ignore_devices'
+CONF_RECONNECT_INTERVAL = 'reconnect_interval'
+CONF_SIGNAL_REPETITIONS = 'signal_repetitions'
+CONF_WAIT_FOR_ACK = 'wait_for_ack'
+
+
 
 PLATFORM_SCHEMA = vol.Schema({
     vol.Required(CONF_PLATFORM): DOMAIN,
@@ -35,11 +50,11 @@ PLATFORM_SCHEMA = vol.Schema({
     vol.Optional(CONF_DEVICES, default={}): vol.Schema({
         cv.string: {
             vol.Optional(CONF_NAME): cv.string,
-            vol.Optional(CONF_ALIASES, default=[]):
+            vol.Optional(CONF_ALIASSES, default=[]):
                 vol.All(cv.ensure_list, [cv.string]),
-            vol.Optional(CONF_GROUP_ALIASES, default=[]):
+            vol.Optional(CONF_GROUP_ALIASSES, default=[]):
                 vol.All(cv.ensure_list, [cv.string]),
-            vol.Optional(CONF_NOGROUP_ALIASES, default=[]):
+            vol.Optional(CONF_NOGROUP_ALIASSES, default=[]):
                 vol.All(cv.ensure_list, [cv.string]),
             vol.Optional(CONF_FIRE_EVENT, default=False): cv.boolean,
             vol.Optional(CONF_SIGNAL_REPETITIONS): vol.Coerce(int),
@@ -47,31 +62,6 @@ PLATFORM_SCHEMA = vol.Schema({
         },
     }),
 })
-
-def entity_type_for_device_id(device_id):
-    """Return entity class for protocol of a given device_id.
-
-    Async friendly.
-    """
-    entity_type_mapping = {
-        # KlikAanKlikUit support both dimmers and on/off switches on the same
-        # protocol
-        'newkaku': TYPE_HYBRID,
-    }
-    protocol = device_id.split('_')[0]
-    return entity_type_mapping.get(protocol, None)
-
-
-def entity_class_for_type(entity_type):
-    """Translate entity type to entity class.
-
-    Async friendly.
-    """
-    entity_device_mapping = {
-        TYPE_RTS: CoverableRflinkDevice,
-    }
-
-    return entity_device_mapping.get(entity_type)
 
 
 def devices_from_config(domain_config, hass=None):
@@ -89,82 +79,30 @@ def devices_from_config(domain_config, hass=None):
         if config[CONF_GROUP]:
             hass.data[DATA_ENTITY_GROUP_LOOKUP][
                 EVENT_KEY_COMMAND][device_id].append(device)
-        for _id in config[CONF_ALIASES]:
+        for _id in config[CONF_ALIASSES]:
             hass.data[DATA_ENTITY_LOOKUP][
                 EVENT_KEY_COMMAND][_id].append(device)
             hass.data[DATA_ENTITY_GROUP_LOOKUP][
                 EVENT_KEY_COMMAND][_id].append(device)
         # group_aliasses only respond to group commands
-        for _id in config[CONF_GROUP_ALIASES]:
-            hass.data[DATA_ENTITY_GROUP_LOOKUP][
-                EVENT_KEY_COMMAND][_id].append(device)
+        #for _id in config[CONF_GROUP_ALIASSES]:
+        #    hass.data[DATA_ENTITY_GROUP_LOOKUP][
+        #        EVENT_KEY_COMMAND][_id].append(device)
         # nogroup_aliasses only respond to normal commands
-        for _id in config[CONF_NOGROUP_ALIASES]:
-            hass.data[DATA_ENTITY_LOOKUP][
-                EVENT_KEY_COMMAND][_id].append(device)
+        #for _id in config[CONF_NOGROUP_ALIASSES]:
+        #    hass.data[DATA_ENTITY_LOOKUP][
+        #        EVENT_KEY_COMMAND][_id].append(device)
 
     return devices
 
 
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
-    """Set up the Rflink light platform."""
+    """Set up the Rflink platform."""
     async_add_devices(devices_from_config(config, hass))
-
-    @asyncio.coroutine
-    def add_new_device(event):
-        """Check if device is known, otherwise add to list of known devices."""
-        device_id = event[EVENT_KEY_ID]
-
-        entity_type = entity_type_for_device_id(event[EVENT_KEY_ID])
-        entity_class = entity_class_for_type(entity_type)
-
-        device_config = config[CONF_DEVICE_DEFAULTS]
-        device = entity_class(device_id, hass, **device_config)
-        async_add_devices([device])
-
-        # Register entity to listen to incoming Rflink events
-        hass.data[DATA_ENTITY_LOOKUP][
-            EVENT_KEY_COMMAND][device_id].append(device)
-
-        # Schedule task to process event after entity is created
-        hass.async_add_job(device.handle_event, event)
-
-        ## niet goed
-        if config[CONF_AUTOMATIC_ADD]:
-            hass.data[DATA_DEVICE_REGISTER][EVENT_KEY_COMMAND] = add_new_device
 
 
 class RflinkCover(CoverableRflinkDevice, CoverDevice):
-    """Rflink light device which sends out only 'on' commands"""
-    
-    @property
-    def entity_id(self):
-        """Return entity id."""
-        return "cover.{}".format(self.name)
-        # return "light.{}".format(self.name)
+    """Representation of a Rflink cover."""
 
-    def _handle_event(self, event):
-        """Adjust state if Rflink picks up a remote command for this device."""
-        self.cancel_queued_send_commands()
-
-        command = event['command']
-        if command == 'on':
-            # if the state is unknown or false, it gets set as true
-            # if the state is true, it gets set as false
-            self._state = self._state in [STATE_UNKNOWN, False]
-
-    @asyncio.coroutine
-    def open_cover(self, **kwargs):
-        """Turn the device up."""
-        yield from self._async_handle_command('turn_up')
-
-    @asyncio.coroutine
-    def close_cover(self, **kwargs):
-        """Turn the device down."""
-        yield from self._async_handle_command('turn_down')
-
-    @asyncio.coroutine
-    def stop_cover(self, **kwargs):
-        """Stop device"""
-        yield from self._async_handle_command('stop')
+    pass
