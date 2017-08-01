d5ffd04df8c6166f221fbf17482f60d851e8e0b0
Teemu Rytilahti <tpr@iki.fi>
2017-08-02 01:52:57 +0200

    Support changing the bulb color for tplink smartbulbs, related to #8766

--- homeassistant/components/light/tplink.py
+++ homeassistant/components/light/tplink.py
@@ -7,12 +7,17 @@ https://home-assistant.io/components/light.tplink/
 import logging
 from homeassistant.const import (CONF_HOST, CONF_NAME)
 from homeassistant.components.light import (
-    Light, ATTR_BRIGHTNESS, ATTR_COLOR_TEMP, ATTR_KELVIN,
-    SUPPORT_BRIGHTNESS, SUPPORT_COLOR_TEMP)
+    Light, ATTR_BRIGHTNESS, ATTR_COLOR_TEMP, ATTR_KELVIN, ATTR_RGB_COLOR,
+    SUPPORT_BRIGHTNESS, SUPPORT_COLOR_TEMP, SUPPORT_RGB_COLOR)
 from homeassistant.util.color import \
     color_temperature_mired_to_kelvin as mired_to_kelvin
-from homeassistant.util.color import \
-    color_temperature_kelvin_to_mired as kelvin_to_mired
+from homeassistant.util.color import (
+    color_RGB_to_hsv, color_hsv_to_RGB,
+    color_temperature_kelvin_to_mired as kelvin_to_mired)
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from pyHS100 import SmartBulb
 
 REQUIREMENTS = ['pyHS100==0.2.4.2']
 
@@ -44,7 +49,7 @@ class TPLinkSmartBulb(Light):
 
     def __init__(self, smartbulb, name):
         """Initialize the bulb."""
-        self.smartbulb = smartbulb
+        self.smartbulb = smartbulb  # type: SmartBulb
 
         # Use the name set on the device if not set
         if name is None:
@@ -55,6 +60,7 @@ class TPLinkSmartBulb(Light):
         self._state = None
         self._color_temp = None
         self._brightness = None
+        self._rgb = None
         _LOGGER.debug("Setting up TP-Link Smart Bulb")
 
     @property
@@ -72,6 +78,11 @@ class TPLinkSmartBulb(Light):
         if ATTR_BRIGHTNESS in kwargs:
             brightness = kwargs.get(ATTR_BRIGHTNESS, self.brightness or 255)
             self.smartbulb.brightness = brightness_to_percentage(brightness)
+        if ATTR_RGB_COLOR:
+            rgb = kwargs.get(ATTR_RGB_COLOR)
+            self._rgb = rgb
+            self.smartbulb.hsv = color_RGB_to_hsv(rgb[0], rgb[1], rgb[2])
+
         self.smartbulb.state = self.smartbulb.BULB_STATE_ON
 
     def turn_off(self):
@@ -87,6 +98,10 @@ class TPLinkSmartBulb(Light):
     def brightness(self):
         """Return the brightness of this light between 0..255."""
         return self._brightness
+
+    @property
+    def rgb_color(self):
+        return self._rgb
 
     @property
     def is_on(self):
@@ -106,10 +121,15 @@ class TPLinkSmartBulb(Light):
                         self.smartbulb.color_temp != 0):
                     self._color_temp = kelvin_to_mired(
                         self.smartbulb.color_temp)
+                h, s, v = self.smartbulb.hsv
+                self._rgb = color_hsv_to_RGB(h, s, v)
         except (SmartPlugException, OSError) as ex:
             _LOGGER.warning('Could not read state for %s: %s', self.name, ex)
 
     @property
     def supported_features(self):
         """Flag supported features."""
+        supported_features = SUPPORT_TPLINK
+        if self.smartbulb.is_color:
+            supported_features += SUPPORT_RGB_COLOR
         return SUPPORT_TPLINK
