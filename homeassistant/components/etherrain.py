60a5e36a0ceccc2d691d667336795f13a5e3b274
Herb Peyerl <hpeyerl+gitlab@beer.org>
2017-08-05 06:51:25 -0600

    pass flake8

--- homeassistant/components/etherrain.py
+++ homeassistant/components/etherrain.py
@@ -1,25 +1,26 @@
-"""
-Support for Etherrain/8
+""".
+
+Support for Etherrain/8.
 
 """
 import logging
-from urllib.parse import urljoin
 
 import requests
 import voluptuous as vol
 
 from homeassistant.const import (
-    CONF_PATH, CONF_HOST, CONF_SSL, CONF_PASSWORD, CONF_USERNAME)
+    CONF_HOST, CONF_PASSWORD, CONF_USERNAME)
 import homeassistant.helpers.config_validation as cv
+
 
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_SSL = False
 DEFAULT_TIMEOUT = 10
 DOMAIN = 'etherrain'
-STATE=1
-WATER_ON=2
-WATER_OFF=3
+STATE = 1
+WATER_ON = 2
+WATER_OFF = 3
 
 LOGIN_RETRIES = 2
 
@@ -61,7 +62,8 @@ def login():
     _LOGGER.info("Attempting to login to EtherRain")
 
     # ergetcfg.cgi?lu=admin\&lp=deadbeef
-    url = '{0}/ergetcfg.cgi?lu={1}&lp={2}'.format(ER['server_origin'],ER['username'],ER['password'])
+    url = '{0}/ergetcfg.cgi?lu={1}&lp={2}'.format(
+        ER['server_origin'], ER['username'], ER['password'])
     req = requests.get(url, timeout=DEFAULT_TIMEOUT)
 
     if not req.ok:
@@ -75,29 +77,31 @@ def login():
 def _er_request(data=None):
     """Perform an EtherRain request."""
     valve = 0
-    duration=0
+    duration = 0
     cmd = 0
     if data is not 'None':
-        cmd=data["command"]
-        valve=data["valve"]
-        duration=data["duration"]
+        cmd = data["command"]
+        valve = data["valve"]
+        duration = data["duration"]
     if cmd == STATE:
-        #_LOGGER.info("Get state".format(valve,duration))
+        # _LOGGER.info("Get state".format(valve,duration))
         url = '{0}/result.cgi?xs'.format(ER['server_origin'])
         duration = 0
     if cmd == WATER_OFF:
-        #_LOGGER.info("Water off".format(valve,duration))
+        # _LOGGER.info("Water off".format(valve,duration))
         url = '{0}/result.cgi?xr'.format(ER['server_origin'])
     if cmd == WATER_ON:
-        _LOGGER.info("Set {0} to {1} minutes".format(valve,duration))
-        valves = [ "0", "0", "0", "0", "0", "0", "0", "0", "0" ]
+        _LOGGER.info("Set {0} to {1} minutes".format(valve, duration))
+        valves = ["0", "0", "0", "0", "0", "0", "0", "0", "0"]
         valves[valve] = duration
-        url = '{0}/result.cgi?xi=0:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}'.format(ER['server_origin'], valves[1], valves[2], valves[3], valves[4], valves[5], valves[6], valves[7], valves[8] )
+        url = '{0}/result.cgi?xi=0:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}'.format(
+            ER['server_origin'], valves[1], valves[2], valves[3], valves[4],
+            valves[5], valves[6], valves[7], valves[8])
 
     for _ in range(LOGIN_RETRIES):
-        #_LOGGER.info("url is {0}".format(url))
+        # _LOGGER.info("url is {0}".format(url))
         req = requests.get(url)
-        #_LOGGER.info("Returned: {0}".format(req.status_code))
+        # _LOGGER.info("Returned: {0}".format(req.status_code))
 
         if not req.ok:
             login()
@@ -108,7 +112,7 @@ def _er_request(data=None):
 
     return req
 
-# pylint: disable=no-member
+
 # retrieve current status
 # http://<er_addr>/result.cgi?xs
 #
@@ -123,10 +127,9 @@ def _er_request(data=None):
 #       ri: 0 <br>
 #       rn: 0 <br>
 # </body>
-#
 def get_state(valve):
-    """Get a state from the EtherRain API service."""
-    data={}
+    """Get the current state of a valve."""
+    data = {}
     data["valve"] = valve
     data["duration"] = 0
     data["command"] = STATE
@@ -137,23 +140,25 @@ def get_state(valve):
         line = b_line.decode('utf8').strip()
         # _LOGGER.info("iterating: {0}".format(line))
         if ":" in line:
-            attr,value=line.split(":")
-            value = value.replace(" <br>","").strip()
+            attr, value = line.split(":")
+            value = value.replace(" <br>", "").strip()
             attr = attr.strip()
-            if attr in [ 'ac', 'os', 'cs', 'rz', 'ri', 'rn' ] :
-                status[attr]=value
+            if attr in ['ac', 'os', 'cs', 'rz', 'ri', 'rn']:
+                status[attr] = value
 
-    # ri contains the last valve to run.  os is the current state of that valve. (ready or busy)
-    # (XXX: The valve number returned is 0-7 but the watering command takes 1-8)
+    # ri contains the last valve to run.  os is the current state of that
+    # valve. (ready or busy)
+    # (XXX: The valve number returned is 0-7 but the watering command
+    # takes 1-8)
     if 'os' in status and status['os'] == 'WT':
-        # _LOGGER.info("valve is {0} and is waiting".format(valve, int(status['ri'])))
+        # _LOGGER.info("valve={0} and waiting".format(valve, status['ri']))
         return 1
     if 'ri' in status and int(status['ri']) == valve-1:
         if 'os' in status and status['os'] == 'RD':
-            # _LOGGER.info("last valve is {0} and is ready".format(valve, int(status['ri'])))
+            # _LOGGER.info("valve={0} and ready".format(valve, status['ri']))
             return 0
         if 'os' in status and status['os'] == 'BZ':
-            # _LOGGER.info("valve is {0} and is busy".format(valve, int(status['ri'])))
+            # _LOGGER.info("valve={0} and busy".format(valve, status['ri']))
             return 1
     else:
         return 0
@@ -161,5 +166,6 @@ def get_state(valve):
 
 # pylint: disable=no-member
 def change_state(valve_data):
+    """Change the state of a valve."""
     _LOGGER.info("Change State: {0}".format(valve_data))
     return _er_request(data=valve_data)
