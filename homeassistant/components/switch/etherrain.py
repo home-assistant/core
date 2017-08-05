86494a18263bb5e01d5ae8721d962f3a8b11b8ca
Herb Peyerl <hpeyerl+gitlab@beer.org>
2017-08-05 07:42:21 -0600

    remove command_on/command_off. Change icon

--- homeassistant/components/switch/etherrain.py
+++ homeassistant/components/switch/etherrain.py
@@ -8,7 +8,6 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components.switch import (SwitchDevice, PLATFORM_SCHEMA)
-from homeassistant.const import (CONF_COMMAND_ON, CONF_COMMAND_OFF)
 import homeassistant.components.etherrain as er
 import homeassistant.helpers.config_validation as cv
 
@@ -18,35 +17,29 @@ _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = ['etherrain']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_COMMAND_ON): cv.string,
-    vol.Required(CONF_COMMAND_OFF): cv.string,
     vol.Required("valve_id"): cv.positive_int,
 })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Etherrain irrigation platform."""
-    on_state = config.get(CONF_COMMAND_ON)
-    off_state = config.get(CONF_COMMAND_OFF)
     valve_id = config.get("valve_id")
     valve_name = config.get("name")
     _LOGGER.info("Setting up etherrain switch {0}".format(valve_id))
 
-    add_devices([ERValveSwitches(valve_id, valve_name, on_state, off_state)])
+    add_devices([ERValveSwitches(valve_id, valve_name)])
 
 
 class ERValveSwitches(SwitchDevice):
     """Representation of an Etherrain valve."""
 
-    icon = 'mdi:record-rec'
-
-    def __init__(self, valve_id, valve_name, on_state, off_state):
+    def __init__(self, valve_id, valve_name):
         """Initialize ERValveSwitches."""
         self._valve_id = valve_id
         self._valve_name = valve_name
         self._duration = 0
-        self._on_state = on_state
-        self._off_state = off_state
+        self._on_state = 1
+        self._off_state = 0
         self._state = None
 
     @property
