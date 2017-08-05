5601847c9a96009fc1ed4ab7fd4fee413578b2df
Fabian Affolter <fabian@affolter-engineering.ch>
2017-08-05 12:45:55 +0200

    Do not call update() in constructor

--- homeassistant/components/sensor/supervisord.py
+++ homeassistant/components/sensor/supervisord.py
@@ -16,6 +16,9 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
+ATTR_DESCRIPTION = 'description'
+ATTR_GROUP = 'group'
+
 DEFAULT_URL = 'http://localhost:9001/RPC2'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -37,7 +40,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     add_devices(
         [SupervisorProcessSensor(info, supervisor_server)
-         for info in processes])
+         for info in processes], True)
 
 
 class SupervisorProcessSensor(Entity):
@@ -47,7 +50,6 @@ class SupervisorProcessSensor(Entity):
         """Initialize the sensor."""
         self._info = info
         self._server = server
-        self.update()
 
     @property
     def name(self):
@@ -59,15 +61,15 @@ class SupervisorProcessSensor(Entity):
         """Return the state of the sensor."""
         return self._info.get('statename')
 
-    def update(self):
-        """Update device state."""
-        self._info = self._server.supervisor.getProcessInfo(
-            self._info.get('name'))
-
     @property
     def device_state_attributes(self):
         """Return the state attributes."""
         return {
-            'group': self._info.get('group'),
-            'description': self._info.get('description')
+            ATTR_DESCRIPTION: self._info.get('description'),
+            ATTR_GROUP: self._info.get('group'),
         }
+
+    def update(self):
+        """Update device state."""
+        self._info = self._server.supervisor.getProcessInfo(
+            self._info.get('name'))
