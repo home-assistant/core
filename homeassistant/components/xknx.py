b9603dceaee6cf9e03cf76acf02ca08e40eca33c
Julius Mittenzwei <julius@mittenzwei.com>
2017-08-06 21:44:43 +0200

    bumped version - 0.7.3 contains some bugfixes

--- homeassistant/components/xknx.py
+++ homeassistant/components/xknx.py
@@ -39,7 +39,7 @@ SUPPORTED_DOMAINS = [
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['xknx==0.7.2']
+REQUIREMENTS = ['xknx==0.7.3']
 
 TUNNELING_SCHEMA = vol.Schema({
     vol.Required(CONF_HOST): cv.string,
