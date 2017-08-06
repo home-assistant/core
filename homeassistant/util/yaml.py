744b70cd707f390196f8e3827b210556a00ba191
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-08-06 09:13:20 -0700

    Change to error

--- homeassistant/util/yaml.py
+++ homeassistant/util/yaml.py
@@ -185,9 +185,9 @@ def _ordered_dict(loader: SafeLineLoader,
 
         if key in seen:
             fname = getattr(loader.stream, 'name', '')
-            _LOGGER.warning(
-                'YAML file % contains duplicate key "%". '
-                'Check lines % and %.', fname, key, seen[key], line)
+            _LOGGER.error(
+                'YAML file %s contains duplicate key "%s". '
+                'Check lines %d and %d.', fname, key, seen[key], line)
         seen[key] = line
 
     return _add_reference(OrderedDict(nodes), loader, node)
