96313d26e7e21f3811ab1b2fd58c8ccb819716fd
Andy Castille <klikini@robiotic.net>
2017-08-06 10:34:09 -0500

    DoorBird base component

---
+++ homeassistant/components/doorbird.py
@@ -0,0 +1,49 @@
+"""
+Support for a DoorBird video doorbell.
+"""
+
+import logging
+import voluptuous as vol
+
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+import homeassistant.helpers.config_validation as cv
+
+REQUIREMENTS = ['DoorBirdPy==0.0.4']
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = 'doorbird'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+
+def setup(hass, config):
+    """Set up the DoorBird component."""
+    ip = config[DOMAIN].get(CONF_HOST)
+    username = config[DOMAIN].get(CONF_USERNAME)
+    password = config[DOMAIN].get(CONF_PASSWORD)
+
+    from doorbirdpy import DoorBird
+    device = DoorBird(ip, username, password)
+    status = device.ready()
+
+    if status[0]:
+        _LOGGER.info("Connected to DoorBird at {} as {}"
+                     .format(ip, username))
+        hass.data[DOMAIN] = device
+        _LOGGER.debug("DBDBG set device")
+        return True
+    elif status[1] == 401:
+        _LOGGER.error("Authorization rejected by DoorBird at {} for {}"
+                      .format(ip, username))
+        return False
+    else:
+        _LOGGER.error("Could not connect to DoorBird at {} as {}: DBP error {}"
+                      .format(ip, username, status[1]))
+        return False
