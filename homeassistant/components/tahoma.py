407941979a6df978c98e87b137283aa5260b62ea
Philip Kleimeyer <philip.kleimeyer@googlemail.com>
2017-08-08 11:42:39 +0200

    fix tahoma api

--- homeassistant/components/tahoma.py
+++ homeassistant/components/tahoma.py
@@ -14,7 +14,7 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import (slugify)
 
-REQUIREMENTS = ['tahoma-api==0.0.7']
+REQUIREMENTS = ['tahoma-api==0.0.8']
 
 _LOGGER = logging.getLogger(__name__)
 
