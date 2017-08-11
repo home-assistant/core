13b7c692f56dc88520a18ad81f497f7c1e83488a
Brian Cribbs <briancribbs@gmail.com>
2017-08-11 11:43:27 -0400

    fixing emulated hue issue and testing it

--- tests/components/emulated_hue/test_init.py
+++ tests/components/emulated_hue/test_init.py
@@ -33,6 +33,63 @@ def test_config_google_home_entity_id_to_number():
         assert handle.write.call_count == 1
 
         entity_id = conf.number_to_entity_id('1')
+
+
+def test_config_google_home_entity_id_to_number_altered():
+    """Test config adheres to the type."""
+    conf = Config(Mock(), {
+        'type': 'google_home'
+    })
+
+    mop = mock_open(read_data=json.dumps({'21': 'light.test2'}))
+    handle = mop()
+
+    with patch('homeassistant.components.emulated_hue.open', mop, create=True):
+        number = conf.entity_id_to_number('light.test')
+        assert number == '22'
+        assert handle.write.call_count == 1
+        assert json.loads(handle.write.mock_calls[0][1][0]) == {
+            '21': 'light.test2',
+            '22': 'light.test',
+        }
+
+        number = conf.entity_id_to_number('light.test')
+        assert number == '22'
+        assert handle.write.call_count == 1
+
+        number = conf.entity_id_to_number('light.test2')
+        assert number == '21'
+        assert handle.write.call_count == 1
+
+        entity_id = conf.number_to_entity_id('21')
+        assert entity_id == 'light.test2'
+
+def test_config_google_home_entity_id_to_number_empty():
+    """Test config adheres to the type."""
+    conf = Config(Mock(), {
+        'type': 'google_home'
+    })
+
+    mop = mock_open(read_data='')
+    handle = mop()
+
+    with patch('homeassistant.components.emulated_hue.open', mop, create=True):
+        number = conf.entity_id_to_number('light.test')
+        assert number == '1'
+        assert handle.write.call_count == 1
+        assert json.loads(handle.write.mock_calls[0][1][0]) == {
+            '1': 'light.test',
+        }
+
+        number = conf.entity_id_to_number('light.test')
+        assert number == '1'
+        assert handle.write.call_count == 1
+
+        number = conf.entity_id_to_number('light.test2')
+        assert number == '2'
+        assert handle.write.call_count == 2
+
+        entity_id = conf.number_to_entity_id('2')
         assert entity_id == 'light.test2'
 
 
