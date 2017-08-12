650063c5ea2c7cbd0224b45d2fb8c1098b897cb0
BioSehnsucht <biosehnsucht@gmail.com>
2017-08-11 23:05:39 -0500

    Change HipChat notify service to use python-simple-hipchat-v2.

--- homeassistant/components/notify/hipchat.py
+++ homeassistant/components/notify/hipchat.py
@@ -5,8 +5,6 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/notify.hipchat/
 """
 import logging
-import json
-import requests
 
 import voluptuous as vol
 
@@ -17,7 +15,7 @@ from homeassistant.const import (
     CONF_TOKEN)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = []
+REQUIREMENTS = ['python-simple-hipchat-v2==0.0.1']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -33,24 +31,19 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_COLOR, default='yellow'): cv.string,
     vol.Optional(CONF_NOTIFY, default=False): cv.boolean,
     vol.Optional(CONF_FORMAT, default='text'): cv.string,
-    vol.Optional(CONF_HOST, default='api.hipchat.com'): cv.string,
+    vol.Optional(CONF_HOST, default='https://api.hipchat.com/'): cv.string,
 })
 
 
 def get_service(hass, config, discovery_info=None):
     """Get the HipChat notification service."""
-    try:
-        return HipchatNotificationService(
-            config[CONF_TOKEN],
-            config[CONF_ROOM],
-            config[CONF_COLOR],
-            config[CONF_NOTIFY],
-            config[CONF_FORMAT],
-            config[CONF_HOST])
-
-    except requests.HTTPError as err:
-        _LOGGER.exception(err.args)
-        return None
+    return HipchatNotificationService(
+        config[CONF_TOKEN],
+        config[CONF_ROOM],
+        config[CONF_COLOR],
+        config[CONF_NOTIFY],
+        config[CONF_FORMAT],
+        config[CONF_HOST])
 
 
 class HipchatNotificationService(BaseNotificationService):
@@ -62,6 +55,7 @@ class HipchatNotificationService(BaseNotificationService):
     def __init__(self, token, default_room, default_color, default_notify,
                  default_format, host):
         """Initialize the service."""
+        import hipchat_v2
         self._token = token
         self._default_room = default_room
         if default_color in self._valid_colors:
@@ -75,51 +69,35 @@ class HipchatNotificationService(BaseNotificationService):
         self._default_format = default_format
         self._host = host
 
-        self.auth_test()
-
-    def auth_test(self):
-        """Validate configuration."""
-        self.send_message('Authentication Test', auth_test=True)
+        self._hipchat = hipchat_v2.HipChat(token=self._token, url=self._host)
 
     def send_message(self, message="", **kwargs):
         """Send a message."""
-        room = ''
-        payload = {
-            'message': message,
-            'color': self._default_color,
-            'notify': self._default_notify,
-            'format': self._default_format,
-            }
-        auth_test = False
+        color = self._default_color
+        notify = self._default_notify
+        message_format = self._default_format
 
         if kwargs.get(ATTR_DATA) is not None:
             data = kwargs.get(ATTR_DATA)
             if ((data.get(CONF_COLOR) is not None)
                     and (data.get(CONF_COLOR) in self._valid_colors)):
-                payload['color'] = data.get(CONF_COLOR)
+                color = data.get(CONF_COLOR)
             if ((data.get(CONF_NOTIFY) is not None)
                     and isinstance(data.get(CONF_NOTIFY), bool)):
-                payload['notify'] = data.get(CONF_NOTIFY)
+                notify = data.get(CONF_NOTIFY)
             if ((data.get(CONF_FORMAT) is not None)
                     and (data.get(CONF_FORMAT) in self._valid_formats)):
-                payload['message_format'] = data.get(CONF_FORMAT)
+                message_format = data.get(CONF_FORMAT)
 
         if kwargs.get(ATTR_TARGET) is None:
             targets = [self._default_room]
         else:
             targets = kwargs.get(ATTR_TARGET)
 
-        if kwargs.get('auth_test') is not None:
-            auth_test = kwargs.get('auth_test')
-
         for room in targets:
 
-            url = "https://{0}/v2/room/{1}/notification".format(self._host,
-                                                                room)
-            if auth_test:
-                url = url + '?auth_test=true'
-            headers = {'Content-type': 'application/json'}
-            headers['Authorization'] = "Bearer " + self._token
-            req = requests.post(url, data=json.dumps(payload),
-                                headers=headers)
-            req.raise_for_status()
+            self._hipchat.message_room(room_id=room,
+                                       message=message,
+                                       color=color,
+                                       notify=notify,
+                                       message_format=message_format)
