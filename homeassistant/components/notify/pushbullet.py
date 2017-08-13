bb49d76eefcf713e5465cf4052ee9ebb753946ba
Daniel Høyer Iversen <mail@dahoiv.net>
2017-08-13 20:44:02 +0200

    pushbullet

--- homeassistant/components/notify/pushbullet.py
+++ homeassistant/components/notify/pushbullet.py
@@ -115,7 +115,7 @@ class PushBulletNotificationService(BaseNotificationService):
                 if url:
                     self.pushbullet.push_link(
                         title, url, body=message, email=tname)
-                if self.hass.config.is_allowed_path(filepath):
+                if filepath and self.hass.config.is_allowed_path(filepath):
                     with open(filepath, "rb") as fileh:
                         filedata = self.pushbullet.upload_file(fileh, filepath)
                         self.pushbullet.push_file(title=title, body=message,
