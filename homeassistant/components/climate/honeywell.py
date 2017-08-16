e9167788e096258663537dc8d0ec4106ee9457e0
Eric Glanz <eric@ericglanz.com>
2017-08-16 16:29:01 -0600

    added current humidity for US thermostats

--- homeassistant/components/climate/honeywell.py
+++ homeassistant/components/climate/honeywell.py
@@ -13,14 +13,14 @@ import voluptuous as vol
 
 from homeassistant.components.climate import (
     ClimateDevice, PLATFORM_SCHEMA, ATTR_FAN_MODE, ATTR_FAN_LIST,
-    ATTR_OPERATION_MODE, ATTR_OPERATION_LIST)
+    ATTR_OPERATION_MODE, ATTR_OPERATION_LIST, ATTR_CURRENT_HUMIDITY)
 from homeassistant.const import (
     CONF_PASSWORD, CONF_USERNAME, TEMP_CELSIUS, TEMP_FAHRENHEIT,
     ATTR_TEMPERATURE)
 import homeassistant.helpers.config_validation as cv
 
 REQUIREMENTS = ['evohomeclient==0.2.5',
-                'somecomfort==0.4.1']
+                'somecomfort==0.5.0']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -36,7 +36,7 @@ CONF_REGION = 'region'
 DEFAULT_AWAY_TEMPERATURE = 16
 DEFAULT_COOL_AWAY_TEMPERATURE = 30
 DEFAULT_HEAT_AWAY_TEMPERATURE = 16
-DEFAULT_REGION = 'eu'
+DEFAULT_REGION = 'us'
 REGIONS = ['eu', 'us']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -53,6 +53,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
+    hass.states.set('is.custom', 'true')
     """Set up the Honeywell thermostat."""
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
@@ -120,6 +121,7 @@ class RoundThermostat(ClimateDevice):
         """Initialize the thermostat."""
         self.client = client
         self._current_temperature = None
+        self._current_humidity = None
         self._target_temperature = None
         self._name = 'round connected'
         self._id = zone_id
@@ -142,6 +144,11 @@ class RoundThermostat(ClimateDevice):
     def current_temperature(self):
         """Return the current temperature."""
         return self._current_temperature
+
+    @property
+    def current_humidity(self):
+        """Return the current humidity."""
+        return self._current_humidity
 
     @property
     def target_temperature(self):
@@ -253,6 +260,11 @@ class HoneywellUSThermostat(ClimateDevice):
         return self._device.current_temperature
 
     @property
+    def current_humidity(self):
+        """Return the current humidity."""
+        return self._device.current_humidity
+
+    @property
     def target_temperature(self):
         """Return the temperature we try to reach."""
         if self._device.system_mode == 'cool':
@@ -302,10 +314,16 @@ class HoneywellUSThermostat(ClimateDevice):
         data = {
             ATTR_FAN: (self.is_fan_on and 'running' or 'idle'),
             ATTR_FAN_MODE: self._device.fan_mode,
-            ATTR_OPERATION_MODE: self._device.system_mode,
+            ATTR_OPERATION_MODE: self._device.system_mode
         }
         data[ATTR_FAN_LIST] = somecomfort.FAN_MODES
         data[ATTR_OPERATION_LIST] = somecomfort.SYSTEM_MODES
+        """
+            somecomfort does not expose uiData.IndoorHumiditySensorAvailable
+            and returns a value of 128 when no humidity sensor is present
+        """
+        if self._device.current_humidity != 128:
+            data[ATTR_CURRENT_HUMIDITY] = self._device.current_humidity
         return data
 
     @property
@@ -365,6 +383,7 @@ class HoneywellUSThermostat(ClimateDevice):
         while retries > 0:
             try:
                 self._device.refresh()
+
                 break
             except (somecomfort.client.APIRateLimited, OSError,
                     requests.exceptions.ReadTimeout) as exp:
@@ -406,3 +425,4 @@ class HoneywellUSThermostat(ClimateDevice):
 
         self._device = devices[0]
         return True
+
