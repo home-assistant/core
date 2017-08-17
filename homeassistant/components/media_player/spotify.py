5b6b428de54b71411cca9d25f7b9facf95a14e4e
Tom Matheussen <tommatheussen5@hotmail.com>
2017-08-17 22:39:20 +0200

    Check if album image(s) exist in spotify (#9024)
    * Check if album image(s) exist in spotify
    
    * Actually set the image to None
    
    * Simplified using ternary operator

--- homeassistant/components/media_player/spotify.py
+++ homeassistant/components/media_player/spotify.py
@@ -185,7 +185,8 @@ class SpotifyMediaPlayer(MediaPlayerDevice):
             self._artist = ', '.join([artist.get('name')
                                       for artist in item.get('artists')])
             self._uri = current.get('uri')
-            self._image_url = item.get('album').get('images')[0].get('url')
+            images = item.get('album').get('images')
+            self._image_url = images[0].get('url') if images else None
         # Playing state
         self._state = STATE_PAUSED
         if current.get('is_playing'):
