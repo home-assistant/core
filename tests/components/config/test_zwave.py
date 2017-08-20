f58161ae32433edd8ce46502edfbe6a264409cc0
turbokongen <turbokongen@hotmail.com>
2017-08-20 14:40:48 +0200

    Update tests

--- tests/components/config/test_zwave.py
+++ tests/components/config/test_zwave.py
@@ -143,10 +143,11 @@ def test_get_values(hass, test_client):
 
     node = MockNode(node_id=1)
     value = MockValue(value_id=123456, node=node, label='Test Label',
-                      instance=1, index=2)
+                      instance=1, index=2, poll_intensity=0)
     values = MockEntityValues(primary=value)
     node2 = MockNode(node_id=2)
-    value2 = MockValue(value_id=234567, node=node2, label='Test Label 2')
+    value2 = MockValue(value_id=234567, node=node2, label='Test Label 2',
+                       poll_intensity=2)
     values2 = MockEntityValues(primary=value2)
     hass.data[const.DATA_ENTITY_VALUES] = [values, values2]
 
@@ -162,6 +163,7 @@ def test_get_values(hass, test_client):
             'label': 'Test Label',
             'instance': 1,
             'index': 2,
+            'poll_intensity': 0,
         }
     }
 
