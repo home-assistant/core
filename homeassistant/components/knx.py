5f142d76ee4ce2243b495d1836fa610677f90d3a
Julius Mittenzwei <julius@mittenzwei.com>
2017-08-20 13:01:27 +0200

    changed dependency

--- homeassistant/components/knx.py
+++ homeassistant/components/knx.py
@@ -35,7 +35,7 @@ ATTR_DISCOVER_DEVICES = 'devices'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['xknx==0.7.8']
+REQUIREMENTS = ['xknx==0.7.9']
 
 TUNNELING_SCHEMA = vol.Schema({
     vol.Required(CONF_HOST): cv.string,
