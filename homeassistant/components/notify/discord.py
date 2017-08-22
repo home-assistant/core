649b08c1b634a4ed9fc828825c36fae20dbb90cc
Fabian Affolter <fabian@affolter-engineering.ch>
2017-08-22 08:36:24 +0200

    Upgrade discord.py to 0.16.10

--- homeassistant/components/notify/discord.py
+++ homeassistant/components/notify/discord.py
@@ -13,7 +13,7 @@ from homeassistant.components.notify import (
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['discord.py==0.16.8']
+REQUIREMENTS = ['discord.py==0.16.10']
 
 CONF_TOKEN = 'token'
 
