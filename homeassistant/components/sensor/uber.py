44b3292ffddc9c7895f5fb19084e4d2682732ee7
Fabian Affolter <mail@fabian-affolter.ch>
2017-08-22 09:23:11 +0200

    Upgrade uber_rides to 0.5.1 (#9080)

--- homeassistant/components/sensor/uber.py
+++ homeassistant/components/sensor/uber.py
@@ -14,7 +14,7 @@ from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 
-REQUIREMENTS = ['uber_rides==0.4.1']
+REQUIREMENTS = ['uber_rides==0.5.1']
 
 _LOGGER = logging.getLogger(__name__)
 
