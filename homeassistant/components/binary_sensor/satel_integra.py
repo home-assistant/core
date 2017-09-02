5a869cd98e36e9a7fa833739546917b8ed9fdbdf
Krzysztof Machelski <krzysztof.machelski+dont-spam@gmail.com>
2017-09-02 23:33:50 +0200

    Simplified interface for monitoring of zones. Zones monitoring working, status monitoring not tested, arming/disarming not working.

--- homeassistant/components/binary_sensor/satel_integra.py
+++ homeassistant/components/binary_sensor/satel_integra.py
@@ -1,5 +1,5 @@
 """
-Support for AlarmDecoder zone states- represented as binary sensors.
+Support for Satel Integra zone states- represented as binary sensors.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/binary_sensor.alarmdecoder/
@@ -11,22 +11,21 @@ from homeassistant.core import callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.components.binary_sensor import BinarySensorDevice
 
-from homeassistant.components.alarmdecoder import (ZONE_SCHEMA,
-                                                   CONF_ZONES,
-                                                   CONF_ZONE_NAME,
-                                                   CONF_ZONE_TYPE,
-                                                   SIGNAL_ZONE_FAULT,
-                                                   SIGNAL_ZONE_RESTORE)
+from homeassistant.components.satel_integra import (ZONE_SCHEMA,
+                                                    CONF_ZONES,
+                                                    CONF_ZONE_NAME,
+                                                    CONF_ZONE_TYPE,
+                                                    SIGNAL_ZONES_UPDATED,
+                                                    )
 
-
-DEPENDENCIES = ['alarmdecoder']
+DEPENDENCIES = ['satel_integra']
 
 _LOGGER = logging.getLogger(__name__)
 
 
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
-    """Set up the AlarmDecoder binary sensor devices."""
+    """Set up the Satel Integra binary sensor devices."""
     configured_zones = discovery_info[CONF_ZONES]
 
     devices = []
@@ -35,7 +34,7 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
         device_config_data = ZONE_SCHEMA(configured_zones[zone_num])
         zone_type = device_config_data[CONF_ZONE_TYPE]
         zone_name = device_config_data[CONF_ZONE_NAME]
-        device = AlarmDecoderBinarySensor(
+        device = SatelIntegraBinarySensor(
             hass, zone_num, zone_name, zone_type)
         devices.append(device)
 
@@ -44,8 +43,8 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     return True
 
 
-class AlarmDecoderBinarySensor(BinarySensorDevice):
-    """Representation of an AlarmDecoder binary sensor."""
+class SatelIntegraBinarySensor(BinarySensorDevice):
+    """Representation of an Satel Integra binary sensor."""
 
     def __init__(self, hass, zone_number, zone_name, zone_type):
         """Initialize the binary_sensor."""
@@ -61,10 +60,7 @@ class AlarmDecoderBinarySensor(BinarySensorDevice):
     def async_added_to_hass(self):
         """Register callbacks."""
         async_dispatcher_connect(
-            self.hass, SIGNAL_ZONE_FAULT, self._fault_callback)
-
-        async_dispatcher_connect(
-            self.hass, SIGNAL_ZONE_RESTORE, self._restore_callback)
+            self.hass, SIGNAL_ZONES_UPDATED, self._zones_updated)
 
     @property
     def name(self):
@@ -98,15 +94,9 @@ class AlarmDecoderBinarySensor(BinarySensorDevice):
         return self._zone_type
 
     @callback
-    def _fault_callback(self, zone):
+    def _zones_updated(self, zones):
         """Update the zone's state, if needed."""
-        if zone is None or int(zone) == self._zone_number:
-            self._state = 1
-            self.hass.async_add_job(self.async_update_ha_state())
-
-    @callback
-    def _restore_callback(self, zone):
-        """Update the zone's state, if needed."""
-        if zone is None or int(zone) == self._zone_number:
-            self._state = 0
+        if self._zone_number in zones and \
+                        self._state != zones[self._zone_number]:
+            self._state = zones[self._zone_number]
             self.hass.async_add_job(self.async_update_ha_state())
--- homeassistant/components/satel_integra.py
+++ homeassistant/components/satel_integra.py
@@ -1,75 +1,187 @@
 """
-This component provides basic support for Abode Home Security system.
+Support for Satel Integra devices.
 
 For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/abode/
+https://home-assistant.io/components/alarmdecoder/
 """
+import asyncio
 import logging
 
 import voluptuous as vol
-from requests.exceptions import HTTPError, ConnectTimeout
-from homeassistant.helpers import discovery
-from homeassistant.helpers import config_validation as cv
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_NAME
 
-REQUIREMENTS = ['abodepy==0.7.1']
+import homeassistant.helpers.config_validation as cv
+from homeassistant.core import callback
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
+from homeassistant.helpers.discovery import async_load_platform
+from homeassistant.helpers.dispatcher import async_dispatcher_send
+from homeassistant.const import (
+    STATE_ALARM_ARMED_AWAY, STATE_ALARM_ARMED_HOME, STATE_ALARM_DISARMED,
+    STATE_UNKNOWN, STATE_ALARM_TRIGGERED)
+
+from satel_integra.satel_integra import AsyncSatel
+
+DEFAULT_ALARM_NAME = 'satel_integra'
+DEFAULT_PORT = 7094
+
+REQUIREMENTS = ['satel_integra==0.1.0']
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_ATTRIBUTION = "Data provided by goabode.com"
+DOMAIN = 'satel_integra'
 
-DOMAIN = 'abode'
-DEFAULT_NAME = 'Abode'
-DATA_ABODE = 'data_abode'
-DEFAULT_ENTITY_NAMESPACE = 'abode'
+DATA_AD = 'satel_integra'
 
-NOTIFICATION_ID = 'abode_notification'
-NOTIFICATION_TITLE = 'Abode Security Setup'
+CONF_DEVICE_HOST = 'host'
+CONF_DEVICE_PORT = 'port'
+CONF_ZONE_NAME = 'name'
+CONF_ZONE_TYPE = 'type'
+CONF_ZONES = 'zones'
+
+ZONES = 'zones'
+
+DEFAULT_DEVICE_HOST = 'localhost'
+DEFAULT_DEVICE_PORT = 10000
+
+DEFAULT_ZONE_TYPE = 'opening'
+
+SIGNAL_PANEL_MESSAGE = 'satel_integra.panel_message'
+SIGNAL_PANEL_ARM_AWAY = 'satel_integra.panel_arm_away'
+SIGNAL_PANEL_ARM_HOME = 'satel_integra.panel_arm_home'
+SIGNAL_PANEL_DISARM = 'satel_integra.panel_disarm'
+
+SIGNAL_ZONES_UPDATED = 'satel_integra.zones_updated'
+
+ZONE_SCHEMA = vol.Schema({
+    vol.Required(CONF_ZONE_NAME): cv.string,
+    vol.Optional(CONF_ZONE_TYPE, default=DEFAULT_ZONE_TYPE): cv.string})
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-        vol.Required(CONF_USERNAME): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string,
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Required(CONF_DEVICE_HOST, default=DEFAULT_DEVICE_HOST): cv.string,
+        vol.Required(CONF_DEVICE_PORT, default=DEFAULT_DEVICE_PORT): cv.port,
+        vol.Optional(CONF_ZONES): {vol.Coerce(int): ZONE_SCHEMA},
     }),
 }, extra=vol.ALLOW_EXTRA)
 
 
-def setup(hass, config):
-    """Set up Abode component."""
-    conf = config[DOMAIN]
-    username = conf.get(CONF_USERNAME)
-    password = conf.get(CONF_PASSWORD)
+class MockSatelIntegra:
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+        self._status = "disarmed"
 
-    try:
-        data = AbodeData(username, password)
-        hass.data[DATA_ABODE] = data
+    @asyncio.coroutine
+    def connect(self):
+        _LOGGER.info("Connecting host: %s, port %s", self._host, self._port)
+        yield from asyncio.sleep(1)
+        _LOGGER.debug("connected")
+        return True
 
-        for component in ['binary_sensor', 'alarm_control_panel']:
-            discovery.load_platform(hass, component, DOMAIN, {}, config)
+    @asyncio.coroutine
+    def get_status(self):
+        status = {}
+        _LOGGER.debug("Getting status ")
+        yield from asyncio.sleep(1)
 
-    except (ConnectTimeout, HTTPError) as ex:
-        _LOGGER.error("Unable to connect to Abode: %s", str(ex))
-        hass.components.persistent_notification.create(
-            'Error: {}<br />'
-            'You will need to restart hass after fixing.'
-            ''.format(ex),
-            title=NOTIFICATION_TITLE,
-            notification_id=NOTIFICATION_ID)
+        from random import randint, choice
+        _LOGGER.debug("Randomly updatating status....")
+        if choice([True, False]):
+            _LOGGER.debug("Alarm update!")
+            message = {"alarm_status": self._status}
+            status[SIGNAL_PANEL_MESSAGE] = message
+        else:
+            _LOGGER.debug("Zones update!")
+            zones = {1: 0,
+                     2: 0,
+                     3: 0}
+            # Set one of them to "1"
+            zones[randint(1, 3)] = 1
+            status[ZONES] = zones
+        _LOGGER.debug("got status %s", status)
+        return status
+
+    @asyncio.coroutine
+    def arm_home(self, code):
+        _LOGGER.info("Arming home")
+        yield from asyncio.sleep(1)
+        self._status = "armed_home"
+        _LOGGER.debug("Armed home")
+
+    @asyncio.coroutine
+    def arm_away(self, code):
+        _LOGGER.info("Arming away")
+        yield from asyncio.sleep(1)
+        self._status = "armed_away"
+        _LOGGER.debug("Armed away")
+
+    @asyncio.coroutine
+    def disarm(self, code):
+        _LOGGER.info("Disarming...")
+        yield from asyncio.sleep(1)
+        self._status = "disarmed"
+        _LOGGER.debug("Disarmed")
+
+    def close(self):
+        _LOGGER.info("Closing alarm")
+        yield from asyncio.sleep(1)
+        _LOGGER.debug("Done")
+
+
+@asyncio.coroutine
+def async_setup(hass, config):
+    """Set up for the Satel Integra devices."""
+
+    conf = config.get(DOMAIN)
+
+    zones = conf.get(CONF_ZONES)
+    controller = False
+    host = conf.get(CONF_DEVICE_HOST)
+    port = conf.get(CONF_DEVICE_PORT)
+
+    controller = AsyncSatel(host, port, zones, hass.loop)
+
+    hass.data[DATA_AD] = controller
+
+    result = yield from controller.connect()
+
+    if not result:
         return False
 
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP,
+                               lambda s: controller.close())
+
+    hass.async_add_job(
+        async_load_platform(hass, 'alarm_control_panel', DOMAIN, conf,
+                            config))
+
+    if zones:
+        hass.async_add_job(async_load_platform(
+            hass, 'binary_sensor', DOMAIN, {CONF_ZONES: zones}, config))
+
+    @callback
+    def alarm_status_update_callback(status):
+        _LOGGER.debug("Alarm status callback, status: %s", status)
+
+        if status["alarm_status"] == "armed":
+            if status["mode"] == 0:
+                hass_alarm_state = STATE_ALARM_ARMED_AWAY
+            else:
+                hass_alarm_status = STATE_ALARM_ARMED_HOME
+        elif status["alarm_status"] == "triggered":
+            hass_alarm_status = STATE_ALARM_TRIGGERED
+
+        _LOGGER.debug("Sending hass_alarm_status: %s...", hass_alarm_status)
+        async_dispatcher_send(hass, SIGNAL_PANEL_MESSAGE, hass_alarm_status)
+
+    @callback
+    def zones_update_callback(status):
+        _LOGGER.debug("Zones callback , status: %s", status)
+        async_dispatcher_send(hass, SIGNAL_ZONES_UPDATED, status[ZONES])
+
+    hass.async_add_job(asyncio.ensure_future(controller.keep_alive()))
+    hass.async_add_job(asyncio.ensure_future(
+        controller.monitor_status(alarm_status_update_callback,
+                                  zones_update_callback))
+    )
+
     return True
-
-
-class AbodeData:
-    """Shared Abode data."""
-
-    def __init__(self, username, password):
-        """Initialize Abode oject."""
-        import abodepy
-
-        self.abode = abodepy.Abode(username, password)
-        self.devices = self.abode.get_devices()
-
-        _LOGGER.debug("Abode Security set up with %s devices",
-                      len(self.devices))
