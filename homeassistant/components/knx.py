04ceccc7f0baaa199170493b67efa0e37a1dd29f
Julius Mittenzwei <julius@mittenzwei.com>
2017-09-03 15:50:21 +0200

    bumped version

--- homeassistant/components/knx.py
+++ homeassistant/components/knx.py
@@ -35,7 +35,7 @@ ATTR_DISCOVER_DEVICES = 'devices'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['xknx==0.7.10']
+REQUIREMENTS = ['xknx==0.7.11']
 
 TUNNELING_SCHEMA = vol.Schema({
     vol.Required(CONF_HOST): cv.string,
