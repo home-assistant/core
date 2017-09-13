648d565b7110070052ed7e88817df948403dc0e5
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-09-13 23:35:06 +0200

    more struct component

--- homeassistant/components/alexa/smart_home.py
+++ homeassistant/components/alexa/smart_home.py
@@ -5,8 +5,11 @@ from uuid import uuid4
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_NAME = 'name'
 ATTR_HEADER = 'header'
+ATTR_NAME = 'name'
+ATTR_NAMESPACE = 'namespace'
+ATTR_MESSAGE_ID = 'messageId'
+ATTR_PAYLOAD = 'payload'
 ATTR_PAYLOAD_VERSION = 'payloadVersion'
 
 
@@ -26,6 +29,31 @@ def handle_message(hass, message):
     funct_name = message[ATTR_HEADER][ATTR_NAME]
     if funct_name not in MAPPING_API:
         _LOGGER.warning("Unsupported API request %s", funct_name)
-        return api_error()
+        return api_error(message)
 
     return (yield from MAPPING_API[funct_name](hass, message))
+
+
+def api_message(name, namespace, payload=None):
+    """Create a API formated message.
+
+    Async friendly.
+    """
+    payload = payload or {}
+    return {
+        ATTR_HEADER: {
+            ATTR_MESSAGE_ID: uuid4(),
+            ATTR_NAME: name,
+            ATTR_NAMESPACE: namespace,
+            ATTR_PAYLOAD_VERSION: '2',
+        },
+        ATTR_PAYLOAD: payload,
+    }
+
+
+def api_error(request, exc='DriverInternalError'):
+    """Create a API formated error message.
+
+    Async friendly.
+    """
+    return api_message(exc, request[ATTR_HEADER][ATTR_NAMESPACE])
