ce040055ffcbd8158c349948024415f9875cd8bc
Andrey F. Kupreychik <foxel@quickfox.ru>
2017-09-14 01:03:16 +0700

    Review feedback

--- homeassistant/components/device_tracker/keenetic_ndms2.py
+++ homeassistant/components/device_tracker/keenetic_ndms2.py
@@ -4,12 +4,10 @@ Support for Zyxel Keenetic NDMS2 based routers.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/device_tracker.keenetic_ndms2/
 """
-from datetime import timedelta
 import logging
-
-import requests
 from collections import namedtuple
 
+import requests
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
@@ -17,14 +15,12 @@ import homeassistant.util.dt as dt_util
 from homeassistant.components.device_tracker import (
     DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
 from homeassistant.const import (
-    CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+    CONF_HOST, CONF_PASSWORD, CONF_USERNAME
 )
 
 _LOGGER = logging.getLogger(__name__)
 
 CONF_EXCLUDE = 'exclude'
-# Interval in minutes to assume these hosts are still home
-CONF_HOME_INTERVAL = 'home_interval'
 # Interface name to track devices for. Most likely one will not need to
 # change it from default 'Home'. This is needed not to track Guest WI-FI-
 # clients and router itself
@@ -38,7 +34,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
     vol.Required(CONF_INTERFACE, default=DEFAULT_INTERFACE): cv.string,
-    vol.Required(CONF_HOME_INTERVAL, default=0): cv.positive_int,
     vol.Optional(CONF_EXCLUDE, default=[]):
         vol.All(cv.ensure_list, vol.Length(min=1)),
 })
@@ -64,7 +59,6 @@ class KeeneticNDMS2DeviceScanner(DeviceScanner):
         self._url = 'http://%s/rci/show/ip/arp' % config[CONF_HOST]
         self._interface = config[CONF_INTERFACE]
         self._exclude = config[CONF_EXCLUDE]
-        self._home_interval = timedelta(minutes=config[CONF_HOME_INTERVAL])
 
         self._username = config.get(CONF_USERNAME)
         self._password = config.get(CONF_PASSWORD)
@@ -88,22 +82,11 @@ class KeeneticNDMS2DeviceScanner(DeviceScanner):
         return None
 
     def _update_info(self):
-        """Get ARP from keenetic router
-        """
+        """Get ARP from keenetic router."""
         _LOGGER.info("Fetching...")
 
-        if self._home_interval:
-            boundary = dt_util.now() - self._home_interval
-            last_results = [device for device in self.last_results
-                            if device.last_update > boundary]
-            if last_results:
-                exclude_hosts = self._exclude + [device.ip for device
-                                                 in last_results]
-            else:
-                exclude_hosts = self._exclude
-        else:
-            last_results = []
-            exclude_hosts = self._exclude
+        last_results = []
+        exclude_hosts = self._exclude
 
         # doing a request
         try:
@@ -112,18 +95,18 @@ class KeeneticNDMS2DeviceScanner(DeviceScanner):
                 self._username, self._password
             ))
         except requests.exceptions.Timeout:
-            _LOGGER.exception(
+            _LOGGER.error(
                 "Connection to the router timed out at URL %s", self._url)
             return False
         if res.status_code != 200:
-            _LOGGER.exception(
+            _LOGGER.error(
                 "Connection failed with http code %s", res.status_code)
             return False
         try:
             result = res.json()
         except ValueError:
             # If json decoder could not parse the response
-            _LOGGER.exception("Failed to parse response from router")
+            _LOGGER.error("Failed to parse response from router")
             return False
 
         # parsing response
