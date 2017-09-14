f952e5dbe5ae095585b2cc017de63c94c85dc573
Daniel Høyer Iversen <daniel.h.iversen@ntnu.no>
2017-09-14 06:42:44 -0400

    update discovery and tests

--- homeassistant/components/vacuum/xiaomi_miio.py
+++ homeassistant/components/vacuum/xiaomi_miio.py
@@ -27,7 +27,7 @@ _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = 'Xiaomi Vacuum cleaner'
 ICON = 'mdi:google-circles-group'
-PLATFORM = 'xiaomi_miiio'
+PLATFORM = 'xiaomi_miio'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
