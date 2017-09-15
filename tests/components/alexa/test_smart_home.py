7744dd265d10b8f7ca8c384df211c92b92ab6d90
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-09-15 22:55:53 +0200

    more tests

--- tests/components/alexa/test_smart_home.py
+++ tests/components/alexa/test_smart_home.py
@@ -5,6 +5,8 @@ import pytest
 
 from homeassistant.components.alexa import smart_home
 
+from tests.common import async_mock_service
+
 
 def test_create_api_message():
     """Create a API message."""
@@ -85,3 +87,95 @@ def test_discovery_request(hass):
             continue
 
         raise AssertionError("Unknown appliance!")
+
+
+@asyncio.coroutine
+def test_api_entity_not_exists(hass):
+    """Test api turn on process without entity."""
+    msg_switch = smart_home.api_message(
+        'TurnOnRequest', 'Alexa.ConnectedHome.Control', {
+            'appliance': {
+                'applianceId': 'switch#test'
+            }
+        })
+
+    call_switch = async_mock_service(hass, 'switch', 'turn_on')
+
+    resp = yield from smart_home.async_api_turn_on(hass, msg_switch)
+    assert len(call_switch) == 0
+    assert resp['header']['name'] == 'DriverInternalError'
+    assert resp['header']['namespace'] == 'Alexa.ConnectedHome.Control'
+
+
+@asyncio.coroutine
+def test_api_turn_on(hass):
+    """Test api turn on process."""
+    msg_switch = smart_home.api_message(
+        'TurnOnRequest', 'Alexa.ConnectedHome.Control', {
+            'appliance': {
+                'applianceId': 'switch#test'
+            }
+        })
+    msg_light = smart_home.api_message(
+        'TurnOnRequest', 'Alexa.ConnectedHome.Control', {
+            'appliance': {
+                'applianceId': 'light#test'
+            }
+        })
+
+    # settup test devices
+    hass.states.async_set(
+        'switch.test', 'off', {'friendly_name': "Test switch"})
+
+    hass.states.async_set(
+        'light.test', 'off', {'friendly_name': "Test light"})
+
+    call_switch = async_mock_service(hass, 'switch', 'turn_on')
+    call_light = async_mock_service(hass, 'light', 'turn_on')
+
+    resp = yield from smart_home.async_api_turn_on(hass, msg_switch)
+    assert len(call_switch) == 1
+    assert call_switch[0].data['entity_id'] == 'switch.test'
+    assert resp['header']['name'] == 'TurnOnConfirmation'
+
+    resp = yield from smart_home.async_api_turn_on(hass, msg_light)
+    assert len(call_light) == 1
+    assert call_light[0].data['entity_id'] == 'light.test'
+    assert resp['header']['name'] == 'TurnOnConfirmation'
+
+
+@asyncio.coroutine
+def test_api_turn_off(hass):
+    """Test api turn off process."""
+    msg_switch = smart_home.api_message(
+        'TurnOffRequest', 'Alexa.ConnectedHome.Control', {
+            'appliance': {
+                'applianceId': 'switch#test'
+            }
+        })
+    msg_light = smart_home.api_message(
+        'TurnOffRequest', 'Alexa.ConnectedHome.Control', {
+            'appliance': {
+                'applianceId': 'light#test'
+            }
+        })
+
+    # settup test devices
+    hass.states.async_set(
+        'switch.test', 'on', {'friendly_name': "Test switch"})
+
+    hass.states.async_set(
+        'light.test', 'on', {'friendly_name': "Test light"})
+
+    call_switch = async_mock_service(hass, 'switch', 'turn_off')
+    call_light = async_mock_service(hass, 'light', 'turn_off')
+
+    resp = yield from smart_home.async_api_turn_off(hass, msg_switch)
+    assert len(call_switch) == 1
+    assert call_switch[0].data['entity_id'] == 'switch.test'
+    assert resp['header']['name'] == 'TurnOffConfirmation'
+
+    resp = yield from smart_home.async_api_turn_off(hass, msg_light)
+    assert len(call_light) == 1
+    assert call_light[0].data['entity_id'] == 'light.test'
+    assert resp['header']['name'] == 'TurnOffConfirmation'
