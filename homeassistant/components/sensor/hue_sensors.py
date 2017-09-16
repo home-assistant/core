26ff7379be962efe66ede24b01865fa7cc06f71b
robmarkcole <robmarkcole@gmail.com>
2017-09-16 07:36:05 +0100

    Adds hue_sensors
    Component for reading Hue motion sensors and 4 button remotes

---
+++ homeassistant/components/sensor/hue_sensors.py
@@ -0,0 +1,176 @@
+"""
+Sensor for checking the status of Hue sensors.
+"""
+import json
+import logging
+from datetime import timedelta
+
+import requests
+
+from homeassistant.const import (CONF_FILENAME)
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+
+_LOGGER = logging.getLogger(__name__)
+PHUE_CONFIG_FILE = 'phue.conf'
+SCAN_INTERVAL = timedelta(seconds=1)
+
+
+def load_conf(filepath):
+    """Return the URL for API requests."""
+    with open(filepath, 'r') as fp:
+        data = json.load(fp)
+        ip = list(data.keys())[0]
+        username = data[ip]['username']
+        url = 'http://' + ip + '/api/' + username + '/sensors'
+    return url
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the Tube sensor."""
+    filename = config.get(CONF_FILENAME, PHUE_CONFIG_FILE)  # returns the IP
+    filepath = hass.config.path(filename)
+    url = load_conf(filepath)
+    data = HueSensorData(url)
+    data.update()
+    sensors = []
+    for key in data.data.keys():
+        sensors.append(HueSensor(key, data))
+    add_devices(sensors, True)
+
+
+class HueSensorData(object):
+    """Get the latest sensor data."""
+
+    def __init__(self, url):
+        """Initialize the object."""
+        self.url = url
+        self.data = None
+
+    # Update only once in scan interval.
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        """Get the latest data"""
+        response = requests.get(self.url)
+        if response.status_code != 200:
+            _LOGGER.warning("Invalid response from API")
+        else:
+            self.data = parse_hue_api_response(response.json())
+
+
+class HueSensor(Entity):
+    """Class to hold Hue Sensor basic info."""
+
+    ICON = 'mdi:run-fast'
+
+    def __init__(self, hue_id, data):
+        self._hue_id = hue_id
+        self._data = data    # data is in .data
+        self._name = self._data.data[self._hue_id]['name']
+        self._model = self._data.data[self._hue_id]['model']
+        self._state = self._data.data[self._hue_id]['state']
+        self._attributes = {}
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return self.ICON
+
+    @property
+    def device_state_attributes(self):
+        """Only motion sensors have attributes currently, but could extend."""
+        return self._attributes
+
+    def update(self):
+        """Update the sensor."""
+        self._data.update()
+        self._state = self._data.data[self._hue_id]['state']
+        if self._model == 'SML001':
+            self._attributes['light_level'] = self._data.data[
+                self._hue_id]['light_level']
+            self._attributes['temperature'] = self._data.data[
+                self._hue_id]['temperature']
+        elif self._model == 'RWL021':
+            self._attributes['last updated'] = self._data.data[
+                self._hue_id]['last_updated']
+
+
+def parse_hue_api_response(response):
+    """Take in the Hue API json response."""
+    data_dict = {}    # The list of sensors, referenced by their hue_id.
+
+    # Loop over all keys (1,2 etc) to identify sensors and get data.
+    for key in response.keys():
+        sensor = response[key]
+
+        if sensor['modelid'] in ['RWL021', 'SML001']:
+            _key = sensor['uniqueid'].split(':')[-1][0:5]
+
+            if sensor['modelid'] == 'RWL021':
+                data_dict[_key] = parse_RWL021(sensor)
+            else:
+                if _key not in data_dict.keys():
+                    data_dict[_key] = parse_SML001(sensor)
+                else:
+                    data_dict[_key].update(parse_SML001(sensor))
+
+        elif sensor['modelid'] == 'HA_GEOFENCE':
+            data_dict['Geofence'] = parse_GEOFENCE(sensor)
+    return data_dict
+
+
+def parse_SML001(response):
+    '''Parse the json for a SML001 Hue motion sensor and return the data.'''
+    if 'ambient light' in response['name']:
+        data = {'light_level': response['state']['lightlevel']}
+
+    elif 'temperature' in response['name']:
+        data = {'temperature': response['state']['temperature']/100.0}
+
+    else:
+        # Some logic to conver 'Hall Sensor' to 'Hall Motion Sensor'
+        name_raw = response['name']
+        arr = name_raw.split()
+        arr.insert(-1, 'motion')
+        name = ' '.join(arr)
+
+        data = {'model': response['modelid'],
+                'state': response['state']['presence'],
+                'name': name}
+    return data
+
+
+def parse_RWL021(response):
+    '''Parse the json response for a RWL021 Hue remote and return the data.
+       If button held for 2 seconds then a hold.'''
+    # check if long or short hold
+    press = str(response['state']['buttonevent'])
+
+    if press[-1] in ['0', '2']:    # 1002, 4001 etc, check if even
+        button = str(press)[0] + '_click'
+    else:
+        button = str(press)[0] + '_hold'
+
+    data = {'model': 'RWL021',
+            'name': response['name'],
+            'state': button,
+            'last_updated': response['state']['lastupdated'].split('T')}
+    return data
+
+
+def parse_GEOFENCE(response):
+    '''Parse the json response for a GEOFENCE and return the data'''
+    data = {'name': response['name'],
+            'model': 'Geofence',
+            'state': response['state']['presence']}
+    return data
---
+++ tests/components/sensor/test_hue_sensors.py
@@ -0,0 +1,54 @@
+"""The tests for the hue_sensors platform."""
+import re
+
+import requests_mock
+import unittest
+
+from homeassistant.components.sensor.hue_sensors import (
+    HueSensorData, HueSensor)
+from tests.common import load_fixture, get_test_home_assistant
+
+
+class TestHueSensor(unittest.TestCase):
+    """Test the uk_transport platform."""
+
+    def setUp(self):
+        """Initialize values for this testcase class."""
+        self.hass = get_test_home_assistant()
+
+    def tearDown(self):
+        """Stop everything that was started."""
+        self.hass.stop()
+
+    @requests_mock.Mocker()
+    def test_hue_sensors(self, mock_req):
+        """Test for hue_sensors."""
+        with requests_mock.mock() as mock_req:
+            mock_url = 'http://mock'
+            mock_req.get(mock_url, text=load_fixture('hue_sensors.json'))
+            data = HueSensorData(mock_url)
+            data.update()
+            sensors = []
+            for key in data.data.keys():
+                sensor = HueSensor(key, data)
+                sensor.update()
+                sensors.append(sensor)
+            assert len(sensors) == 6
+            for sensor in sensors:
+                if sensor.name == 'Living room motion sensor':
+                    assert sensor.state is False
+                    assert sensor.device_state_attributes[
+                        'light_level'] == 0
+                    assert sensor.device_state_attributes[
+                        'temperature'] == 21.38
+                elif sensor.name == 'Living room remote':
+                    assert sensor.state == '1_hold'
+                    assert sensor.device_state_attributes[
+                        'last updated'] == ['2017-09-15', '16:35:00']
+                elif sensor.name == 'Robins iPhone':
+                    assert sensor.state is True
+                else:
+                    assert sensor.name in [
+                        'Bedroom motion sensor',
+                        'Remote bedroom',
+                        'Hall motion Sensor']
