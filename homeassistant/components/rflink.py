321d0aae0ab2582cd66545015472c0bfc765c03c
Pascal Osinga <pascal@osinga.me>
2017-09-19 20:53:14 +0200

    removed :

--- homeassistant/components/rflink.py
+++ homeassistant/components/rflink.py
@@ -100,7 +100,7 @@ def identify_event_type(event):
         return EVENT_KEY_COMMAND
     elif EVENT_KEY_SENSOR in event:
         return EVENT_KEY_SENSOR
-    return: 'unknown'
+    return 'unknown'
     else:
         return 'unknown'
 
