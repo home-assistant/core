df1e08fc6a00ad852e0af62eb0b3e2f8cceecdcb
gautric <gautric@redhat.com>
2017-09-25 17:53:20 +0200

    split monitored condition to be independent

--- homeassistant/components/sensor/sytadin.py
+++ homeassistant/components/sensor/sytadin.py
@@ -11,7 +11,7 @@ import logging
 from datetime import timedelta
 import voluptuous as vol
 
-from homeassistant.const import (LENGTH_KILOMETERS)
+from homeassistant.const import (LENGTH_KILOMETERS, CONF_NAME)
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -19,7 +19,7 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['lxml', 'requests']
+REQUIREMENTS = ['lxml']
 
 SYSTADIN = 'http://www.sytadin.fr/sys/barometres_de_la_circulation.jsp.html'
 
@@ -31,16 +31,25 @@ TRAFFIC_JAM_REGEX = '([0-9]+)'
 MEAN_VELOCITY_REGEX = '([0-9]+)'
 CONGESTION_REGEX = '([0-9]+.[0-9]+)'
 
-CONF_MONITORED_CONDITIONS = 'monitored_conditions'
+CONF_MONITORED_CONDITION = 'monitored_condition'
 OPTION_TRAFFIC_JAM = 'traffic_jam'
 OPTION_MEAN_VELOCITY = 'mean_velocity'
 OPTION_CONGESTION = 'congestion'
 CONF_UPDATE_INTERVAL = 'update_interval'
 
+SENSOR_TYPES = {
+    OPTION_TRAFFIC_JAM: ('Sytadin Traffic Jam', LENGTH_KILOMETERS),
+    OPTION_MEAN_VELOCITY: ('Sytadin Mean Velocity', LENGTH_KILOMETERS+'/h'),
+    OPTION_CONGESTION: ('Sytadin Congestion', '')
+}
+
 TIMEOUT = 10
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_MONITORED_CONDITIONS): cv.string,
+    vol.Optional(CONF_MONITORED_CONDITION): cv.string,
+    vol.Optional(CONF_NAME): cv.string,
+    # vol.Optional(CONF_MONITORED_CONDITION, default=''):
+    #     vol.All(cv.string, [vol.In(SENSOR_TYPES)]),
     vol.Optional(CONF_UPDATE_INTERVAL, default=timedelta(seconds=300)): (
         vol.All(cv.time_period, cv.positive_timedelta)),
 })
@@ -49,26 +58,35 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
 
     options = [
-        it.strip() for it in config.get(CONF_MONITORED_CONDITIONS).split(',')
+        it.strip() for it in config.get(CONF_MONITORED_CONDITION).split(',')
     ]
 
+    name = config.get(CONF_NAME)
+
     if OPTION_TRAFFIC_JAM in options:
+        if not name:
+            name = SENSOR_TYPES.get(OPTION_TRAFFIC_JAM)[0]
         add_devices([
-            SytadinSensor('Traffic Jam', SYSTADIN, TRAFFIC_JAM_XPATH,
-                          TRAFFIC_JAM_REGEX, LENGTH_KILOMETERS,
+            SytadinSensor(name, SYSTADIN, TRAFFIC_JAM_XPATH,
+                          TRAFFIC_JAM_REGEX, SENSOR_TYPES.get(OPTION_TRAFFIC_JAM)[1],
                           config.get(CONF_UPDATE_INTERVAL))
             ])
     if OPTION_MEAN_VELOCITY in options:
+        if not name:
+            name = SENSOR_TYPES.get(OPTION_MEAN_VELOCITY)[0]
         add_devices([
-            SytadinSensor('Mean Velocity', SYSTADIN,
+            SytadinSensor(name, SYSTADIN,
                           MEAN_VELOCITY_XPATH, MEAN_VELOCITY_REGEX,
-                          LENGTH_KILOMETERS+'/h',
+                          SENSOR_TYPES.get(OPTION_MEAN_VELOCITY)[1]+'/h',
                           config.get(CONF_UPDATE_INTERVAL))
             ])
     if OPTION_CONGESTION in options:
+        if not name:
+            name = SENSOR_TYPES.get(OPTION_CONGESTION)[0]
         add_devices([
-            SytadinSensor('Congestion', SYSTADIN, CONGESTION_XPATH,
-                          CONGESTION_REGEX, '',
+            SytadinSensor(name, SYSTADIN, CONGESTION_XPATH,
+                          CONGESTION_REGEX,
+                          SENSOR_TYPES.get(OPTION_CONGESTION)[1],
                           config.get(CONF_UPDATE_INTERVAL))
             ])
 
@@ -89,7 +107,7 @@ class SytadinSensor(Entity):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Sytadin ' + self._name
+        return self._name
 
     @property
     def state(self):
