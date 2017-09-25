12a87c3362b75a08c09119b82b37ae7f51188eeb
Stefan Lehmann <Lehmann@kuz-leipzig.de>
2017-09-25 14:55:46 +0200

    switch: use adsvar for connection

--- homeassistant/components/switch/ads.py
+++ homeassistant/components/switch/ads.py
@@ -15,23 +15,27 @@ _LOGGER = logging.getLogger(__name__)
 DEFAULT_NAME = 'ADS Switch'
 DEPENDENCIES = ['ads']
 
+CONF_ADSVAR = 'adsvar'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONF_ADSVAR): cv.string,
+    vol.Optional(CONF_NAME): cv.string,
 })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     dev_name = config.get(CONF_NAME)
-    add_devices([AdsSwitch(dev_name)])
+    ads_var = config.get(CONF_ADSVAR)
+    add_devices([AdsSwitch(dev_name, ads_var)])
 
 
 class AdsSwitch(ads.AdsDevice, ToggleEntity):
     """ Representation of an Ads switch device. """
 
-    def __init__(self, dev_name):
+    def __init__(self, dev_name, ads_var):
         ads.AdsDevice.__init__(self)
         self.dev_name = dev_name
+        self.ads_var = ads_var
         self._on_state = False
 
     @property
@@ -43,11 +47,11 @@ class AdsSwitch(ads.AdsDevice, ToggleEntity):
         return self.dev_name
 
     def turn_on(self, **kwargs):
-        self.write_by_name(self.dev_name, True, pyads.PLCTYPE_BOOL)
+        self.write_by_name(self.ads_var, True, pyads.PLCTYPE_BOOL)
         self._on_state = True
 
     def turn_off(self, **kwargs):
-        self.write_by_name(self.dev_name, False, pyads.PLCTYPE_BOOL)
+        self.write_by_name(self.ads_var, False, pyads.PLCTYPE_BOOL)
         self._on_state = False
 
     def value_changed(self, val):
