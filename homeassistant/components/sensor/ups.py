ed5db05e737fd5e72e1dd07962db76cd08c0c43e
Aaron Bach <bachya1208@gmail.com>
2017-09-26 12:46:01 -0600

    Added unit of measurement

--- homeassistant/components/sensor/ups.py
+++ homeassistant/components/sensor/ups.py
@@ -83,6 +83,11 @@ class UPSSensor(Entity):
         """Return the state of the sensor."""
         return self._state
 
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return 'packages'
+
     def _update(self):
         """Update device state."""
         import upsmychoice
