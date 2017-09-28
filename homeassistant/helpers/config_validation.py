b90bfd7711db3758e9533e4100e525a43d40555f
Lukas Barth <mail+github@tinloaf.de>
2017-09-28 23:57:49 +0200

    Add an input_datetime (#9313)
    * Initial proposal for the input_datetime
    
    * Linting
    
    * Further linting, don't define time validation twice
    
    * Make pylint *and* flake8 happy at the same time
    
    * Move todos to the PR to make lint happy
    
    * Actually validate the type of date/time
    
    * First testing
    
    * Linting
    
    * Address code review issues
    
    * Code review: Remove forgotten print()s
    
    * Make set_datetime a coroutine
    
    * Create contains_at_least_one_key_value CV method, use it
    
    * Add timestamp to the attributes
    
    * Test and fix corner case where restore data is bogus
    
    * Add FIXME
    
    * Fix date/time setting
    
    * Fix Validation
    
    * Merge date / time validation, add tests
    
    * Simplify service data validation
    
    * No default for initial state, allow 'unknown' as state
    
    * cleanup
    
    * fix schema

--- homeassistant/helpers/config_validation.py
+++ homeassistant/helpers/config_validation.py
@@ -1,5 +1,6 @@
 """Helpers for config validation using voluptuous."""
-from datetime import timedelta, datetime as datetime_sys
+from datetime import (timedelta, datetime as datetime_sys,
+                      time as time_sys, date as date_sys)
 import os
 import re
 from urllib.parse import urlparse
@@ -53,6 +54,21 @@ def has_at_least_one_key(*keys: str) -> Callable:
             if k in keys:
                 return obj
         raise vol.Invalid('must contain one of {}.'.format(', '.join(keys)))
+
+    return validate
+
+
+def has_at_least_one_key_value(*items: list) -> Callable:
+    """Validate that at least one (key, value) pair exists."""
+    def validate(obj: Dict) -> Dict:
+        """Test (key,value) exist in dict."""
+        if not isinstance(obj, dict):
+            raise vol.Invalid('expected dictionary')
+
+        for item in obj.items():
+            if item in items:
+                return obj
+        raise vol.Invalid('must contain one of {}.'.format(str(items)))
 
     return validate
 
@@ -142,6 +158,38 @@ time_period_dict = vol.All(
     has_at_least_one_key('days', 'hours', 'minutes',
                          'seconds', 'milliseconds'),
     lambda value: timedelta(**value))
+
+
+def time(value) -> time_sys:
+    """Validate and transform a time."""
+    if isinstance(value, time_sys):
+        return value
+
+    try:
+        time_val = dt_util.parse_time(value)
+    except TypeError:
+        raise vol.Invalid('Not a parseable type')
+
+    if time_val is None:
+        raise vol.Invalid('Invalid time specified: {}'.format(value))
+
+    return time_val
+
+
+def date(value) -> date_sys:
+    """Validate and transform a date."""
+    if isinstance(value, date_sys):
+        return value
+
+    try:
+        date_val = dt_util.parse_date(value)
+    except TypeError:
+        raise vol.Invalid('Not a parseable type')
+
+    if date_val is None:
+        raise vol.Invalid("Could not parse date")
+
+    return date_val
 
 
 def time_period_str(value: str) -> timedelta:
@@ -295,16 +343,6 @@ def template_complex(value):
         return value
 
     return template(value)
-
-
-def time(value):
-    """Validate time."""
-    time_val = dt_util.parse_time(value)
-
-    if time_val is None:
-        raise vol.Invalid('Invalid time specified: {}'.format(value))
-
-    return time_val
 
 
 def datetime(value):
