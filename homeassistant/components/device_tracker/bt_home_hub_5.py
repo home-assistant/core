589029cb3d4ecdc6b55a44d663dba66e96ce0f93
Aidan Hobson Sayers <aidanhs@cantab.net>
2017-09-30 16:43:16 +0100

    Add option to get device details from dhcp table
    The unauthenticated homepage version gives you truncated names

--- homeassistant/components/device_tracker/bt_home_hub_5.py
+++ homeassistant/components/device_tracker/bt_home_hub_5.py
@@ -8,6 +8,7 @@ import logging
 import re
 import xml.etree.ElementTree as ET
 import json
+from hashlib import md5
 from urllib.parse import unquote
 
 import requests
@@ -16,13 +17,19 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.device_tracker import (
     DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
-from homeassistant.const import CONF_HOST
+from homeassistant.const import CONF_HOST, CONF_PASSWORD
+
+REQUIREMENTS = ['beautifulsoup4==4.6.0']
 
 _LOGGER = logging.getLogger(__name__)
 _MAC_REGEX = re.compile(r'([0-9a-f]{2}\:){5}[0-9a-f]{2}')
+_HOMEPAGE = '?active_page=9098'  # router homepage: has device list, no login
+_AUTHPAGE = '?active_page=9100'  # any page that prompts for login
+_DHCPPAGE = '?active_page=9140'  # page containing DHCP table
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string
+    vol.Optional(CONF_HOST, default='192.168.1.254'): cv.string,
+    vol.Optional(CONF_PASSWORD, default=None): cv.string,
 })
 
 
@@ -40,12 +47,12 @@ class BTHomeHub5DeviceScanner(DeviceScanner):
     def __init__(self, config):
         """Initialise the scanner."""
         _LOGGER.info("Initialising BT Home Hub 5")
-        self.host = config.get(CONF_HOST, '192.168.1.254')
+        self.baseurl = 'http://{}/index.cgi'.format(config[CONF_HOST])
+        self.password = config[CONF_PASSWORD]
         self.last_results = {}
-        self.url = 'http://{}/index.cgi?active_page=9098'.format(self.host)
 
         # Test the router is accessible
-        data = _get_homehub_data(self.url)
+        data = _get_homehub_data(self.baseurl, self.password)
         self.success_init = data is not None
 
     def scan_devices(self):
@@ -75,7 +82,7 @@ class BTHomeHub5DeviceScanner(DeviceScanner):
 
         _LOGGER.info("Scanning")
 
-        data = _get_homehub_data(self.url)
+        data = _get_homehub_data(self.baseurl, self.password)
 
         if not data:
             _LOGGER.warning("Error scanning devices")
@@ -86,21 +93,80 @@ class BTHomeHub5DeviceScanner(DeviceScanner):
         return True
 
 
-def _get_homehub_data(url):
+def _get_homehub_data(baseurl, password=None):
     """Retrieve data from BT Home Hub 5 and return parsed result."""
+    if password is not None:
+        cookiedict = _auth_homehub(baseurl, password)
+        if cookiedict is None:
+            _LOGGER.error("Failed to retrieve cookie for BT Home Hub")
+            return
+        response = _try_request('GET', '{}{}'.format(baseurl, _DHCPPAGE),
+                                cookies=cookiedict)
+        if response is None:
+            return
+    else:
+        # Fall back to non-authenticating method, may truncate device names
+        response = _try_request('GET', '{}{}'.format(baseurl, _HOMEPAGE))
+        if response is None:
+            return
+    return _parse_homehub_device_table_from_html(response.text)
+
+
+def _auth_homehub(baseurl, password):
+    """Get an authenticated cookie from the homehub."""
+    from bs4 import BeautifulSoup
+
+    # Extract the required params for authenticating the cookie
+    response = _try_request('GET', '{}{}'.format(baseurl, _AUTHPAGE))
+    if response is None:
+        return
+    cookie_name = 'rg_cookie_session_id'
+    cookiedict = {cookie_name: response.cookies[cookie_name]}
+    soup = BeautifulSoup(response.text, 'html.parser')
+    request_id = soup.find(attrs={'name': 'request_id'}).attrs['value']
+    post_token = soup.find(attrs={'name': 'post_token'}).attrs['value']
+    auth_key = soup.find(attrs={'name': 'auth_key'}).attrs['value']
+    password_field = soup.find(id='password').attrs['name']
+    # Probably not actually utf-8, but default passwords are ascii anyway
+    md5_pass = md5((password + auth_key).encode('utf-8')).hexdigest()
+
+    # Perform authenticating request, mimicking browser as much as reasonable
+    response = _try_request('POST', baseurl, cookies=cookiedict, data={
+        'request_id': request_id,
+        'active_page': '9148',
+        'active_page_str': 'bt_login',
+        'mimic_button_field': 'submit_button_login_submit: ..',
+        'button_value': '',
+        'post_token': post_token,
+        password_field: '',
+        'md5_pass': md5_pass,
+        'auth_key': auth_key,
+    })
+    if response is None or 'BT Home Hub Manager - Login' in response.text:
+        return
+    return cookiedict
+
+
+def _try_request(method, url, **kwargs):
+    """Send request to BT Home Hub 5."""
     try:
-        response = requests.get(url, timeout=8)
+        response = requests.request(method, url, timeout=8, **kwargs)
     except requests.exceptions.Timeout:
         _LOGGER.exception("Connection to the router timed out")
         return
     if response.status_code == 200:
-        return _parse_homehub_response(response.text)
+        return response
     else:
-        _LOGGER.error("Invalid response from Home Hub: %s", response)
+        _LOGGER.error("Invalid response from Home Hub when %s %s: %s",
+                      method, url, response)
 
 
-def _parse_homehub_response(data_str):
-    """Parse the BT Home Hub 5 data format."""
+def _parse_homehub_device_table_from_html(data_str):
+    """Find and parse a table of devices from the BT Home Hub 5.
+
+    The homepage and DHCP table page tables are similar enough that we can
+    use common code.
+    """
     from bs4 import BeautifulSoup
 
     # Find the beginning of devices table (the header)
@@ -109,7 +175,7 @@ def _parse_homehub_response(data_str):
     if macaddr_header is None:
         _LOGGER.error("Could not find 'MAC Address' header")
         return
-    table_header_row = macaddr_header.parent.parent # -> th -> tr
+    table_header_row = macaddr_header.parent.parent  # -> th -> tr
     if table_header_row.name != 'tr':
         _LOGGER.error("Header row not in the expected place")
         return
@@ -119,16 +185,21 @@ def _parse_homehub_response(data_str):
     mac_address_col = None
     device_name_col = None
     for i, headercell in enumerate(table_header_row.children):
-        if headercell.find(text='MAC Address'): mac_address_col = i
-        elif headercell.find(text='Device'): device_name_col = i
+        if headercell.find(text='MAC Address'):
+            mac_address_col = i
+        elif headercell.find(text='Device'):
+            device_name_col = i
     if device_name_col is None or mac_address_col is None:
-        _LOGGER.error("Couldn't identify column nos of mac address and device name")
+        _LOGGER.error("Couldn't find columns of mac address and device name")
         return
 
-    # Run through all the rows of the table, hunting for anything with a MAC
+    # Run through all the rows of the table, hunting for anything with a MAC.
+    # Rows of device tables seem to have alternating bgcolor, so repeat until
+    # we run out of them.
     devices = {}
     tablerow = table_header_row
-    while tablerow.next_sibling is not None and 'bgcolor' in tablerow.next_sibling.attrs:
+    while (tablerow.next_sibling is not None and
+            'bgcolor' in tablerow.next_sibling.attrs):
         tablerow = tablerow.next_sibling
         cells = list(tablerow.children)
         mac_address = cells[mac_address_col].text
