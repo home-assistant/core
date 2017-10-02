73a180d4c777f1ff520f626134ab7193ebda07ae
Egor Tsinko <egort@universalpsi.com>
2017-10-02 10:03:15 -0600

    updated comments and cleaned up code

--- homeassistant/components/media_player/monoprice.py
+++ homeassistant/components/media_player/monoprice.py
@@ -1,12 +1,16 @@
 """
-Support for interfacing with Monoprice via serial interface
+Support for interfacing with Monoprice 6 zone home audio controller
+via serial interface.
+
+https://www.monoprice.com/product?p_id=10761
 """
 import logging
 
 import voluptuous as vol
 from homeassistant.components.media_player import (
-    SUPPORT_TURN_ON, SUPPORT_TURN_OFF, SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_SET,
-    SUPPORT_VOLUME_STEP, SUPPORT_SELECT_SOURCE, MediaPlayerDevice, PLATFORM_SCHEMA)
+    SUPPORT_TURN_ON, SUPPORT_TURN_OFF, SUPPORT_VOLUME_MUTE,
+    SUPPORT_VOLUME_SET,SUPPORT_VOLUME_STEP, SUPPORT_SELECT_SOURCE,
+    MediaPlayerDevice, PLATFORM_SCHEMA)
 from homeassistant.const import (CONF_PORT, STATE_OFF, STATE_ON, CONF_NAME)
 import homeassistant.helpers.config_validation as cv
 
@@ -15,8 +19,9 @@ REQUIREMENTS = ['pymonoprice==0.2']
 
 _LOGGER = logging.getLogger(__name__)
 
-SUPPORT_MONOPRICE = SUPPORT_VOLUME_MUTE | SUPPORT_VOLUME_SET | SUPPORT_VOLUME_STEP |\
-                    SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_SELECT_SOURCE
+SUPPORT_MONOPRICE = SUPPORT_VOLUME_MUTE | SUPPORT_VOLUME_SET |\
+                    SUPPORT_VOLUME_STEP |SUPPORT_TURN_ON |\
+                    SUPPORT_TURN_OFF | SUPPORT_SELECT_SOURCE
 
 ZONE_SCHEMA = vol.Schema({
     vol.Required(CONF_NAME): cv.string,
@@ -39,7 +44,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """ Sets up the monoprice platform. """
+    """ Sets up the Monoprice 6-zone amplifier platform. """
 
     port = config.get(CONF_PORT)
 
@@ -50,15 +55,17 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     from pymonoprice import Monoprice
     monoprice = Monoprice(port)
 
-    sources = { source_id: extra[CONF_NAME] for source_id, extra in config[CONF_SOURCES].items()}
+    sources = {source_id: extra[CONF_NAME] for source_id, extra
+               in config[CONF_SOURCES].items()}
 
     for zone_id, extra in config[CONF_ZONES].items():
         _LOGGER.info("Adding zone {} - {}".format(zone_id, extra[CONF_NAME]))
         add_devices([MonopriceZone(monoprice, sources, zone_id, extra[CONF_NAME])], True)
     return True
 
+
 class MonopriceZone(MediaPlayerDevice):
-    """ Represents a monoprice zone. """
+    """ Represents a Monoprice amplifier zone. """
 
     # pylint: disable=too-many-public-methods
 
@@ -67,9 +74,10 @@ class MonopriceZone(MediaPlayerDevice):
         # dict source_id -> source name
         self._source_id_name = sources
         # dict source name -> source_id
-        self._source_name_id = {v:k for k,v in sources.items()}
+        self._source_name_id = {v: k for k, v in sources.items()}
         # ordered list of all source names
-        self._source_names = sorted(self._source_name_id.keys(), key=lambda v: self._source_name_id[v])
+        self._source_names = sorted(self._source_name_id.keys(),
+                                    key=lambda v: self._source_name_id[v])
         self._zone_id = zone_id
         self._name = zone_name
 
@@ -79,6 +87,7 @@ class MonopriceZone(MediaPlayerDevice):
         self._mute = None
 
     def update(self):
+        """Retrieve latest state."""
         state = self._monoprice.zone_status(self._zone_id)
         if not state:
             return False
@@ -125,7 +134,7 @@ class MonopriceZone(MediaPlayerDevice):
     @property
     def source_list(self):
         """List of available input sources."""
-        return list(self._source_names)
+        return self._source_names
 
     def select_source(self, source):
         """ Set input source. """
@@ -143,20 +152,24 @@ class MonopriceZone(MediaPlayerDevice):
         self._monoprice.set_power(self._zone_id, False)
 
     def mute_volume(self, mute):
-        """ mute (true) or unmute (false) media player. """
+        """ Mute (true) or unmute (false) media player. """
         self._monoprice.set_mute(self._zone_id, mute)
 
     def set_volume_level(self, volume):
-        """ set volume level, range 0..1. """
+        """ Set volume level, range 0..1. """
         self._monoprice.set_volume(self._zone_id, int(volume * 38))
 
     def volume_up(self):
         """Volume up the media player."""
-        self._volume = min(self._volume + 1, 38)
-        self._monoprice.set_volume(self._zone_id, int(self._volume))
+        if self._volume is None:
+            return
+        self._monoprice.set_volume(self._zone_id,
+                                   min(self._volume + 1, 38))
 
     def volume_down(self):
         """Volume down media player."""
-        self._volume = max(self._volume - 1, 0)
-        self._monoprice.set_volume(self._zone_id, int(self._volume))
+        if self._volume is None:
+            return
+        self._monoprice.set_volume(self._zone_id,
+                                   max(self._volume - 1, 0))
 
