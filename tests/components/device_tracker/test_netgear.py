cf99d6410529c42418ba100c10dc7a99b2fedf53
Egor Tsinko <etsinko@gmail.com>
2017-10-03 21:08:19 -0600

    fixed broken test

--- tests/components/device_tracker/test_netgear.py
+++ tests/components/device_tracker/test_netgear.py
@@ -1,6 +1,6 @@
 """Test netgear platform."""
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, patch
 
 from homeassistant.components.device_tracker import DOMAIN
 from homeassistant.const import (
@@ -12,12 +12,12 @@ from components.device_tracker import netgear
 class NetgearTest(unittest.TestCase):
     """Test netgear platform."""
 
-    @patch('pynetgear.Netgear', Mock())
     def test_get_scanner_valid_parameters(self):
         """Test get_scanner() with valid config."""
         config = {DOMAIN: {CONF_HOST: 'a', CONF_PASSWORD: 'b',
                            CONF_USERNAME: 'c', CONF_PORT: 80}}
-        net = netgear.get_scanner(None, config)
+        with patch.dict('sys.modules', pynetgear=MagicMock()):
+            net = netgear.get_scanner(None, config)
         self.assertTrue(isinstance(net, netgear.NetgearDeviceScanner))
 
     def test_get_scanner_invalid_parameters(self):
