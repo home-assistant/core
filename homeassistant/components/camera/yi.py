3e2740c8af0a9f9cb20d312e0d271a875a97aec8
Aaron Bach <bachya1208@gmail.com>
2017-10-08 10:30:16 -0600

    Rewrite using ftplib

--- homeassistant/components/camera/yi.py
+++ homeassistant/components/camera/yi.py
@@ -1,9 +1,10 @@
 """
-This component provides basic support for Xiaomi Cameras (HiSilicon Hi3518e V200).
+This component provides support for Xiaomi Cameras (HiSilicon Hi3518e V200).
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/camera.yi/
 """
+import asyncio
 import logging
 
 import voluptuous as vol
@@ -16,7 +17,6 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_aiohttp_proxy_stream
 
 DEPENDENCIES = ['ffmpeg']
-REQUIREMENTS = ['aioftp==0.8.0']
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_BRAND = 'YI Home Camera'
@@ -82,63 +82,63 @@ class YiCamera(Camera):
         """Camera brand."""
         return DEFAULT_BRAND
 
-    async def get_latest_video_url(self):
+    def get_latest_video_url(self):
         """Retrieve the latest video file from the customized Yi FTP server."""
-        from aioftp import ClientSession
+        from ftplib import FTP, error_perm
 
-        async with ClientSession(self.host, 21, self.user,
-                                 self.passwd) as client:
-            dirs = [
-                p
-                for p, i in sorted(
-                    await client.list(self.path),
-                    key=lambda k: k[1]['modify'],
-                    reverse=True) if i['type'] == 'dir'
-            ]
-            try:
-                latest_dir = dirs[0]
-                await client.change_directory(latest_dir)
-                files = [
-                    p
-                    for p, _ in sorted(
-                        await client.list(),
-                        key=lambda k: k[1]['modify'],
-                        reverse=True) if '.mp4' in str(p)
-                ]
+        ftp = FTP(self.host)
+        try:
+            ftp.login(self.user, self.passwd)
+        except error_perm as exc:
+            _LOGGER.error('There was an error while logging into the camera')
+            _LOGGER.debug(exc)
+            return False
 
-                try:
-                    return 'ftp://{0}:{1}@{2}:{3}{4}'.format(
-                        self.user, self.passwd, self.host, self.port,
-                        '{0}/{1}'.format(latest_dir, files[0]))
-                except IndexError:
-                    print("Couldn't find any MP4 files")
-            except IndexError:
-                print("There aren't any folders at that path: {0}".format(
-                    self.path))
+        try:
+            ftp.cwd(self.path)
+        except error_perm as exc:
+            _LOGGER.error('Unable to find path: %s', self.path)
+            _LOGGER.debug(exc)
+            return False
 
-    async def async_camera_image(self):
+        dirs = [d for d in ftp.nlst() if '.' not in d]
+        if not dirs:
+            _LOGGER.warning("There don't appear to be any uploaded videos")
+            return False
+
+        latest_dir = dirs[-1]
+        ftp.cwd(latest_dir)
+        latest_video = ftp.nlst()[-1]
+
+        return 'ftp://{0}:{1}@{2}:{3}{4}/{5}/{6}'.format(
+            self.user, self.passwd, self.host, self.port, self.path,
+            latest_dir, latest_video)
+
+    @asyncio.coroutine
+    def async_camera_image(self):
         """Return a still image response from the camera."""
         from haffmpeg import ImageFrame, IMAGE_JPEG
 
-        url = await self.get_latest_video_url()
+        url = self.get_latest_video_url()
 
         if url != self._last_url:
             ffmpeg = ImageFrame(self._manager.binary, loop=self.hass.loop)
-            self._last_image = await ffmpeg.get_image(
+            self._last_image = yield from ffmpeg.get_image(
                 url, output_format=IMAGE_JPEG, extra_cmd=self._extra_arguments)
             self._last_url = url
 
         return self._last_image
 
-    async def handle_async_mjpeg_stream(self, request):
+    @asyncio.coroutine
+    def handle_async_mjpeg_stream(self, request):
         """Generate an HTTP MJPEG stream from the camera."""
         from haffmpeg import CameraMjpeg
 
         stream = CameraMjpeg(self._manager.binary, loop=self.hass.loop)
-        await stream.open_camera(
+        yield from stream.open_camera(
             self._last_url, extra_cmd=self._extra_arguments)
 
-        await async_aiohttp_proxy_stream(
+        yield from async_aiohttp_proxy_stream(
             self.hass, request, stream,
             'multipart/x-mixed-replace;boundary=ffserver')
-        await stream.close()
+        yield from stream.close()
