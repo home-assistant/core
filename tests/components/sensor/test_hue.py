07e9648fccfea646a081a42601c4b40edf35515d
robmarkcole <robmarkcole@gmail.com>
2017-10-08 20:47:48 +0100

    Updates CODEOWNERS

--- tests/components/sensor/test_hue.py
+++ tests/components/sensor/test_hue.py
@@ -1,10 +1,12 @@
 """The tests for the hue sensors platform."""
 
-import requests_mock
+import json
 import unittest
+from unittest.mock import patch
 
 from homeassistant.setup import setup_component
-from tests.common import get_test_home_assistant
+from tests.common import (
+    get_test_home_assistant, load_fixture)
 
 DOMAIN = 'hue'
 VALID_CONFIG = {
@@ -18,14 +20,16 @@ class TestHueSensor(unittest.TestCase):
     def setUp(self):
         """Initialize values for this testcase class."""
         self.hass = get_test_home_assistant()
-        self.config = VALID_CONFIG
 
     def tearDown(self):
         """Stop everything that was started."""
         self.hass.stop()
 
-    @requests_mock.Mocker()
+    @patch('hue_sensors.get_response_from_url',
+           return_value=json.loads(load_fixture('hue_sensors.json')))
     def test_setup(self, mock_req):
         """Test for operational tube_state sensor with proper attributes."""
         self.assertTrue(
-            setup_component(self.hass, 'sensor', {'sensor': self.config}))
+            setup_component(self.hass, 'sensor', {'sensor': VALID_CONFIG}))
+        #living_room_remote = self.hass.states.get('sensor.living_room_remote')
+        #assert living_room_remote.state == '1_hold'
