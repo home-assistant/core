214e2ae164dc7b9f66c78a0bcee9397b96a9211c
Aaron Bach <bachya1208@gmail.com>
2017-10-08 11:33:11 -0600

    Empty folder should be info, not warning

--- homeassistant/components/camera/yi.py
+++ homeassistant/components/camera/yi.py
@@ -107,7 +107,7 @@ class YiCamera(Camera):
         ftp.cwd(latest_dir)
         videos = ftp.nlst()
         if not videos:
-            _LOGGER.warning('Video folder "%s" is empty; delaying', latest_dir)
+            _LOGGER.info('Video folder "%s" is empty; delaying', latest_dir)
             return False
 
         return 'ftp://{0}:{1}@{2}:{3}{4}/{5}/{6}'.format(
