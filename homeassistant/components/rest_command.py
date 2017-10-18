7992850d8204646489d54a538764534dd64e4dbb
yazz007 <yazz0007@gmail.com>
2017-10-18 15:48:07 +0200

    Add digest authentication

--- homeassistant/components/rest_command.py
+++ homeassistant/components/rest_command.py
@@ -107,21 +107,21 @@ def async_setup(hass, config):
                         try:
                             url = template_url.async_render(variables=service.data)
                             if command_config[CONF_METHOD] == 'get':
-                                response = requests.get(url, data=payload, timeout=10, auth=auth, headers=headers)
+                                request = requests.get(url, data=payload, timeout=10, auth=auth, headers=headers)
                             elif command_config[CONF_METHOD] == 'post':
-                                response = requests.post(url, data=payload, timeout=10, auth=auth, headers=headers)
+                                request = requests.post(url, data=payload, timeout=10, auth=auth, headers=headers)
                             elif command_config[CONF_METHOD] == 'delete':
-                                response = requests.delete(url, data=payload, timeout=10, auth=auth, headers=headers)
+                                request = requests.delete(url, data=payload, timeout=10, auth=auth, headers=headers)
                             elif command_config[CONF_METHOD] == 'put':
-                                response = requests.put(url, data=payload, timeout=10, auth=auth, headers=headers)
+                                request = requests.put(url, data=payload, timeout=10, auth=auth, headers=headers)
                             elif command_config[CONF_METHOD] == 'head':
-                                response = requests.head(url, data=payload, timeout=10, auth=auth, headers=headers)
+                                request = requests.head(url, data=payload, timeout=10, auth=auth, headers=headers)
                             elif command_config[CONF_METHOD] == 'options':
-                                response = requests.options(url, data=payload, timeout=10, auth=auth, headers=headers)
+                                request = requests.options(url, data=payload, timeout=10, auth=auth, headers=headers)
                                 
-                            return response.content
+                            return request
                         except requests.exceptions.RequestException as error:
-                            _LOGGER.error("Rest command error %s.", request.url)
+                            _LOGGER.error("Rest command error %s.", url)
 
                         response = yield from self.hass.async_add_job(
                             fetch)
