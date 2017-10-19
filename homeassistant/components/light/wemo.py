00e228dbee9ba6a3bdb17b02dcd74e4f788f6fb1
angel12 <spencer@angelsolutionsnm.com>
2017-10-19 14:14:41 -0600

    Add more properties for dimmer

--- homeassistant/components/light/wemo.py
+++ homeassistant/components/light/wemo.py
@@ -7,7 +7,7 @@ https://home-assistant.io/components/switch.wemo/
 import logging
 
 from homeassistant.components.light import (
-    ATTR_BRIGHTNESS_PCT, ATTR_BRIGHTNESS, SUPPORT_BRIGHTNESS, Light, DOMAIN)
+    ATTR_BRIGHTNESS_PCT, ATTR_BRIGHTNESS, SUPPORT_BRIGHTNESS, SUPPORT_TRANSITION, Light, DOMAIN)
 from homeassistant.util import convert
 from homeassistant.const import (
     STATE_OFF, STATE_ON,)
@@ -15,7 +15,12 @@ from homeassistant.loader import get_component
 
 DEPENDENCIES = ['wemo']
 
+MIN_TIME_BETWEEN_SCANS = timedelta(seconds=10)
+MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(milliseconds=100)
+
 _LOGGER = logging.getLogger(__name__)
+
+SUPPORT_WEMO = (SUPPORT_BRIGHTNESS | SUPPORT_TRANSITION)
 
 ATTR_SENSOR_STATE = 'sensor_state'
 ATTR_SWITCH_MODE = 'switch_mode'
@@ -23,6 +28,7 @@ ATTR_CURRENT_STATE_DETAIL = 'state_detail'
 
 WEMO_ON = 1
 WEMO_OFF = 0
+WEMO_STANDBY = 8
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """Set up discovered WeMo dimmers."""
@@ -40,11 +46,8 @@ class WemoDimmer(light)
     ""Representation of a WeMo dimmer""
 
     def __init__(self, device):
-        """Initialize the WeMo switch."""
+        """Initialize the WeMo dimmer."""
         self.wemo = device
-        self.insight_params = None
-        self.maker_params = None
-        self.coffeemaker_mode = None
         self._state = None
         # look up model name once as it incurs network traffic
         self._model_name = self.wemo.model_name
@@ -63,4 +66,40 @@ class WemoDimmer(light)
             return
         self.schedule_update_ha_state()
 
+        # Used for value change event handling
+        self.value_added()
+        self.update_properties()
+
+    @property
+    def should_poll(self):
+        """No polling needed with subscriptions."""
+        if self._model_name == 'Insight':
+            return True
+        return False
+
+    @property
+    def unique_id(self):
+        """Return the ID of this WeMo dimmer."""
+        return "{}.{}".format(self.__class__, self.wemo.serialnumber)
+
+    @property
+    def name(self):
+        """Return the name of the dimmer if any."""
+        return self.wemo.name
+
+    @property
+    def is_on(self):
+        """Return true if dimmer is on. Standby is on."""
+        return self._state
         
+    def turn_on(self, **kwargs):
+        """Turn the dimmer on."""
+        self._state = WEMO_ON
+        self.wemo.on()
+        self.schedule_update_ha_state()
+
+    def turn_off(self, **kwargs):
+        """Turn the dimmer off."""
+        self._state = WEMO_OFF
+        self.wemo.off()
+        self.schedule_update_ha_state()
