131fb4e039689da557cbb0191ddcbec0112c06c3
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-10-21 02:32:11 -0400

    Refactored attributions and removed extra refresh method.

--- homeassistant/components/camera/ring.py
+++ homeassistant/components/camera/ring.py
@@ -15,7 +15,7 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.components.ring import DATA_RING, CONF_ATTRIBUTION
 from homeassistant.components.camera import Camera, PLATFORM_SCHEMA
 from homeassistant.components.ffmpeg import DATA_FFMPEG
-from homeassistant.const import ATTR_ATTRIBUTION
+from homeassistant.const import ATTR_ATTRIBUTION, CONF_SCAN_INTERVAL
 from homeassistant.helpers.aiohttp_client import async_aiohttp_proxy_stream
 from homeassistant.util import dt as dt_util
 
@@ -25,8 +25,12 @@ DEPENDENCIES = ['ring', 'ffmpeg']
 
 _LOGGER = logging.getLogger(__name__)
 
+SCAN_INTERVAL = timedelta(seconds=90)
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_FFMPEG_ARGUMENTS): cv.string,
+    vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL):
+        cv.time_period,
 })
 
 
@@ -57,8 +61,8 @@ class RingCam(Camera):
         self._name = self._camera.name
         self._ffmpeg = hass.data[DATA_FFMPEG]
         self._ffmpeg_arguments = device_info.get(CONF_FFMPEG_ARGUMENTS)
-        self._last_video_id = None
-        self._video_url = None
+        self._last_video_id = self._camera.last_recording_id
+        self._video_url = self._camera.recording_url(self._last_video_id)
         self._expires_at = None
         self._utcnow = None
 
@@ -70,18 +74,16 @@ class RingCam(Camera):
     @property
     def device_state_attributes(self):
         """Return the state attributes."""
-        attrs = {}
-
-        attrs[ATTR_ATTRIBUTION] = CONF_ATTRIBUTION
-        attrs['device_id'] = self._camera.id
-        attrs['firmware'] = self._camera.firmware
-        attrs['kind'] = self._camera.kind
-        attrs['timezone'] = self._camera.timezone
-        attrs['type'] = self._camera.family
-        attrs['video_url'] = self._video_url
-        attrs['video_id'] = self._last_video_id
-
-        return attrs
+        return {
+            ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
+            'device_id': self._camera.id,
+            'firmware': self._camera.firmware,
+            'kind': self._camera.kind,
+            'timezone': self._camera.timezone,
+            'type': self._camera.family,
+            'video_url': self._video_url,
+            'video_id': self._last_video_id
+        }
 
     @asyncio.coroutine
     def async_camera_image(self):
@@ -114,29 +116,26 @@ class RingCam(Camera):
             'multipart/x-mixed-replace;boundary=ffserver')
         yield from stream.close()
 
+    @property
+    def should_poll(self):
+        """Update the image periodically."""
+        return True
+
     def update(self):
         """Update camera entity and refresh attributes."""
-        self._utcnow = dt_util.utcnow()
-
-        if not self._last_video_id or not self._video_url:
-            self._refresh_attrs()
-
-        # refresh last_video_id if a new video has been published
-        if self._last_video_id != self._camera.last_recording_id:
-            self._refresh_attrs()
-
-        if self._utcnow >= self._expires_at:
-            self._refresh_attrs()
-
-    def _refresh_attrs(self):
-        _LOGGER.debug("Updated Ring DoorBell camera attributes.")
-        self._last_video_id = self._camera.last_recording_id
-        self._video_url = self._camera.recording_url(self._last_video_id)
-
         # extract the video expiration from URL
         x_amz_expires = int(self._video_url.split('&')[0].split('=')[-1])
         x_amz_date = self._video_url.split('&')[1].split('=')[-1]
 
+        self._utcnow = dt_util.utcnow()
         self._expires_at = \
             timedelta(seconds=x_amz_expires) + \
             dt_util.as_utc(datetime.strptime(x_amz_date, "%Y%m%dT%H%M%SZ"))
+
+        if self._last_video_id != self._camera.last_recording_id:
+            _LOGGER.debug("Updated Ring DoorBell last_video_id")
+            self._last_video_id = self._camera.last_recording_id
+
+        if self._utcnow >= self._expires_at:
+            _LOGGER.debug("Updated Ring DoorBell video_url")
+            self._video_url = self._camera.recording_url(self._last_video_id)
