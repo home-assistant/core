c2a1b73a6128724e6db37ce6dcbf1f1ddc24e5ab
Adam Mills <adam@armills.info>
2017-10-22 14:29:59 -0400

    Include vehicle:events scope for automatic

--- homeassistant/components/device_tracker/automatic.py
+++ homeassistant/components/device_tracker/automatic.py
@@ -35,7 +35,7 @@ CONF_CURRENT_LOCATION = 'current_location'
 
 DEFAULT_TIMEOUT = 5
 
-DEFAULT_SCOPE = ['location', 'vehicle:profile', 'trip']
+DEFAULT_SCOPE = ['location', 'vehicle:profile', 'trip', 'vehicle:events']
 FULL_SCOPE = DEFAULT_SCOPE + ['current_location']
 
 ATTR_FUEL_LEVEL = 'fuel_level'
