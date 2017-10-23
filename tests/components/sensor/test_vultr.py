6e6bac3c241a31fadd3c82c1dbdf6da6a881c0a6
Adam Cooper <adam@genericstudent.com>
2017-10-23 19:50:43 +0100

    Added better testing of vultr sensor

--- tests/components/sensor/test_vultr.py
+++ tests/components/sensor/test_vultr.py
@@ -1,9 +1,14 @@
 """The tests for the Vultr sensor platform."""
+import pytest
 import unittest
 import requests_mock
+import voluptuous as vol
 
 from components.sensor import vultr
 from components import vultr as base_vultr
+from components.vultr import CONF_SUBS
+from homeassistant.const import (
+    CONF_MONITORED_CONDITIONS, CONF_PLATFORM)
 
 from tests.components.test_vultr import VALID_CONFIG
 from tests.common import (
@@ -73,20 +78,61 @@ class TestVultrSensorSetup(unittest.TestCase):
             elif device.name == 'my failed server Pending Charges':
                 self.assertEqual("not a number", device.state)
 
+    def test_invalid_sensor_config(self):
+        """Test config type failures."""
+        with pytest.raises(vol.Invalid):  # No subs
+            vultr.PLATFORM_SCHEMA({
+                CONF_PLATFORM: base_vultr.DOMAIN,
+                CONF_MONITORED_CONDITIONS: vultr.MONITORED_CONDITIONS
+            })
+        with pytest.raises(vol.Invalid):  # No monitored_conditions
+            vultr.PLATFORM_SCHEMA({
+                CONF_PLATFORM: base_vultr.DOMAIN,
+            })
+        with pytest.raises(vol.Invalid):  # Bad monitored_conditions
+            vultr.PLATFORM_SCHEMA({
+                CONF_PLATFORM: base_vultr.DOMAIN,
+                CONF_SUBS: ['12345', '56789'],
+                CONF_MONITORED_CONDITIONS: [
+                    'non-existent-condition',
+                ]
+            })
+
     @requests_mock.Mocker()
-    def test_invalid_sensor(self, mock):
-        """Test the Vultr sensor class and methods."""
-        mock.get('https://api.vultr.com/v1/account/info',
-                 text="{}")
+    def test_invalid_sensors(self, mock):
+        """Test the VultrSensor fails."""
+        mock.get(
+            'https://api.vultr.com/v1/account/info?api_key=ABCDEFG1234567',
+            text=load_fixture('vultr_account_info.json'))
 
-        mock.get('https://api.vultr.com/v1/server/list')
+        mock.get(
+            'https://api.vultr.com/v1/server/list?api_key=ABCDEFG1234567',
+            text=load_fixture('vultr_server_list.json'))
 
-        self.assertFalse(
-            base_vultr.setup(self.hass, {"vultr": {}}))
+        base_vultr.setup(self.hass, VALID_CONFIG)
 
-        setup = vultr.setup_platform(self.hass,
-                                     self.config,
-                                     self.add_devices,
-                                     None)
+        bad_conf = {
+            CONF_MONITORED_CONDITIONS: vultr.MONITORED_CONDITIONS,
+        }  # No subs at all
 
-        self.assertFalse(setup)
+        no_sub_setup = vultr.setup_platform(self.hass,
+                                            bad_conf,
+                                            self.add_devices,
+                                            None)
+
+        self.assertFalse(no_sub_setup)
+        self.assertEqual(0, len(self.DEVICES))
+
+        bad_conf = {
+            CONF_SUBS: ['123456', '56789'],
+            CONF_MONITORED_CONDITIONS: ['bad-condition',
+                                        'non-existing-condition'],
+        }  # Invalid monitored_conditions
+
+        bad_conditions_setup = vultr.setup_platform(self.hass,
+                                                    bad_conf,
+                                                    self.add_devices,
+                                                    None)
+
+        self.assertFalse(bad_conditions_setup)
+        self.assertEqual(0, len(self.DEVICES))
