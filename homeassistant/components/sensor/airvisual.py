0482ae93855a01c8789ab7253da6717a33575660
Aaron Bach <bachya1208@gmail.com>
2017-10-22 18:20:42 -0600

    Fixes AirVisual bug regarding incorrect location data

--- homeassistant/components/sensor/airvisual.py
+++ homeassistant/components/sensor/airvisual.py
@@ -39,22 +39,57 @@ VOLUME_MICROGRAMS_PER_CUBIC_METER = 'µg/m3'
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=10)
 
-POLLUTANT_LEVEL_MAPPING = [
-    {'label': 'Good', 'minimum': 0, 'maximum': 50},
-    {'label': 'Moderate', 'minimum': 51, 'maximum': 100},
-    {'label': 'Unhealthy for sensitive group', 'minimum': 101, 'maximum': 150},
-    {'label': 'Unhealthy', 'minimum': 151, 'maximum': 200},
-    {'label': 'Very Unhealthy', 'minimum': 201, 'maximum': 300},
-    {'label': 'Hazardous', 'minimum': 301, 'maximum': 10000}
-]
+POLLUTANT_LEVEL_MAPPING = [{
+    'label': 'Good',
+    'minimum': 0,
+    'maximum': 50
+}, {
+    'label': 'Moderate',
+    'minimum': 51,
+    'maximum': 100
+}, {
+    'label': 'Unhealthy for sensitive group',
+    'minimum': 101,
+    'maximum': 150
+}, {
+    'label': 'Unhealthy',
+    'minimum': 151,
+    'maximum': 200
+}, {
+    'label': 'Very Unhealthy',
+    'minimum': 201,
+    'maximum': 300
+}, {
+    'label': 'Hazardous',
+    'minimum': 301,
+    'maximum': 10000
+}]
 
 POLLUTANT_MAPPING = {
-    'co': {'label': 'Carbon Monoxide', 'unit': MASS_PARTS_PER_MILLION},
-    'n2': {'label': 'Nitrogen Dioxide', 'unit': MASS_PARTS_PER_BILLION},
-    'o3': {'label': 'Ozone', 'unit': MASS_PARTS_PER_BILLION},
-    'p1': {'label': 'PM10', 'unit': VOLUME_MICROGRAMS_PER_CUBIC_METER},
-    'p2': {'label': 'PM2.5', 'unit': VOLUME_MICROGRAMS_PER_CUBIC_METER},
-    's2': {'label': 'Sulfur Dioxide', 'unit': MASS_PARTS_PER_BILLION},
+    'co': {
+        'label': 'Carbon Monoxide',
+        'unit': MASS_PARTS_PER_MILLION
+    },
+    'n2': {
+        'label': 'Nitrogen Dioxide',
+        'unit': MASS_PARTS_PER_BILLION
+    },
+    'o3': {
+        'label': 'Ozone',
+        'unit': MASS_PARTS_PER_BILLION
+    },
+    'p1': {
+        'label': 'PM10',
+        'unit': VOLUME_MICROGRAMS_PER_CUBIC_METER
+    },
+    'p2': {
+        'label': 'PM2.5',
+        'unit': VOLUME_MICROGRAMS_PER_CUBIC_METER
+    },
+    's2': {
+        'label': 'Sulfur Dioxide',
+        'unit': MASS_PARTS_PER_BILLION
+    },
 }
 
 SENSOR_LOCALES = {'cn': 'Chinese', 'us': 'U.S.'}
@@ -66,8 +101,8 @@ SENSOR_TYPES = [
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
-    vol.Required(CONF_MONITORED_CONDITIONS):
-        vol.All(cv.ensure_list, [vol.In(SENSOR_LOCALES)]),
+    vol.Required(CONF_MONITORED_CONDITIONS): vol.All(
+        cv.ensure_list, [vol.In(SENSOR_LOCALES)]),
     vol.Optional(CONF_CITY): cv.string,
     vol.Optional(CONF_COUNTRY): cv.string,
     vol.Optional(CONF_LATITUDE): cv.latitude,
@@ -93,17 +128,23 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     show_on_map = config.get(CONF_SHOW_ON_MAP)
 
     if city and state and country:
-        _LOGGER.debug(
-            "Using city, state, and country: %s, %s, %s", city, state, country)
+        _LOGGER.debug("Using city, state, and country: %s, %s, %s", city,
+                      state, country)
         data = AirVisualData(
-            pav.Client(api_key), city=city, state=state, country=country,
+            pav.Client(api_key),
+            city=city,
+            state=state,
+            country=country,
             show_on_map=show_on_map)
     else:
-        _LOGGER.debug(
-            "Using latitude and longitude: %s, %s", latitude, longitude)
+        _LOGGER.debug("Using latitude and longitude: %s, %s", latitude,
+                      longitude)
         data = AirVisualData(
-            pav.Client(api_key), latitude=latitude, longitude=longitude,
-            radius=radius, show_on_map=show_on_map)
+            pav.Client(api_key),
+            latitude=latitude,
+            longitude=longitude,
+            radius=radius,
+            show_on_map=show_on_map)
 
     data.update()
     sensors = []
@@ -126,7 +167,7 @@ class AirVisualBaseSensor(Entity):
 
     def __init__(self, data, name, icon, locale):
         """Initialize the sensor."""
-        self._data = data
+        self.data = data
         self._icon = icon
         self._locale = locale
         self._name = name
@@ -136,20 +177,17 @@ class AirVisualBaseSensor(Entity):
     @property
     def device_state_attributes(self):
         """Return the device state attributes."""
-        attrs = {
+        attrs = merge_two_dicts({
             ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
-            ATTR_CITY: self._data.city,
-            ATTR_COUNTRY: self._data.country,
-            ATTR_REGION: self._data.state,
-            ATTR_TIMESTAMP: self._data.pollution_info.get('ts')
-        }
+            ATTR_TIMESTAMP: self.data.pollution_info.get('ts')
+        }, self.data.attrs)
 
-        if self._data.show_on_map:
-            attrs[ATTR_LATITUDE] = self._data.latitude
-            attrs[ATTR_LONGITUDE] = self._data.longitude
+        if self.data.show_on_map:
+            attrs[ATTR_LATITUDE] = self.data.latitude
+            attrs[ATTR_LONGITUDE] = self.data.longitude
         else:
-            attrs['lati'] = self._data.latitude
-            attrs['long'] = self._data.longitude
+            attrs['lati'] = self.data.latitude
+            attrs['long'] = self.data.longitude
 
         return attrs
 
@@ -174,9 +212,9 @@ class AirPollutionLevelSensor(AirVisualBaseSensor):
 
     def update(self):
         """Update the status of the sensor."""
-        self._data.update()
+        self.data.update()
 
-        aqi = self._data.pollution_info.get('aqi{0}'.format(self._locale))
+        aqi = self.data.pollution_info.get('aqi{0}'.format(self._locale))
         try:
             [level] = [
                 i for i in POLLUTANT_LEVEL_MAPPING
@@ -199,9 +237,9 @@ class AirQualityIndexSensor(AirVisualBaseSensor):
 
     def update(self):
         """Update the status of the sensor."""
-        self._data.update()
+        self.data.update()
 
-        self._state = self._data.pollution_info.get(
+        self._state = self.data.pollution_info.get(
             'aqi{0}'.format(self._locale))
 
 
@@ -224,9 +262,9 @@ class MainPollutantSensor(AirVisualBaseSensor):
 
     def update(self):
         """Update the status of the sensor."""
-        self._data.update()
+        self.data.update()
 
-        symbol = self._data.pollution_info.get('main{0}'.format(self._locale))
+        symbol = self.data.pollution_info.get('main{0}'.format(self._locale))
         pollution_info = POLLUTANT_MAPPING.get(symbol, {})
         self._state = pollution_info.get('label')
         self._unit = pollution_info.get('unit')
@@ -239,6 +277,7 @@ class AirVisualData(object):
     def __init__(self, client, **kwargs):
         """Initialize the AirVisual data element."""
         self._client = client
+        self.attrs = {}
         self.pollution_info = None
 
         self.city = kwargs.get(CONF_CITY)
@@ -258,19 +297,22 @@ class AirVisualData(object):
 
         try:
             if self.city and self.state and self.country:
-                resp = self._client.city(
-                    self.city, self.state, self.country).get('data')
+                resp = self._client.city(self.city, self.state,
+                                         self.country).get('data')
+                self.longitude, self.latitude = resp.get('location').get(
+                    'coordinates')
             else:
-                resp = self._client.nearest_city(
-                    self.latitude, self.longitude, self._radius).get('data')
+                resp = self._client.nearest_city(self.latitude, self.longitude,
+                                                 self._radius).get('data')
             _LOGGER.debug("New data retrieved: %s", resp)
 
-            self.city = resp.get('city')
-            self.state = resp.get('state')
-            self.country = resp.get('country')
-            self.longitude, self.latitude = resp.get('location').get(
-                'coordinates')
             self.pollution_info = resp.get('current', {}).get('pollution', {})
+
+            self.attrs = {
+                ATTR_CITY: resp.get('city'),
+                ATTR_REGION: resp.get('state'),
+                ATTR_COUNTRY: resp.get('country')
+            }
         except exceptions.HTTPError as exc_info:
             _LOGGER.error("Unable to retrieve data on this location: %s",
                           self.__dict__)
