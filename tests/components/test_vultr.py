d99cb931f031313c3d4daae645a3655d475b51fb
Adam Cooper <adam@genericstudent.com>
2017-10-23 19:51:42 +0100

    Resolved vultr platform test affecting subsequent vultr tests

--- tests/components/test_vultr.py
+++ tests/components/test_vultr.py
@@ -2,7 +2,7 @@
 import unittest
 import requests_mock
 
-from copy import copy
+from copy import deepcopy
 from homeassistant import setup
 import components.vultr as vultr
 
@@ -10,8 +10,8 @@ from tests.common import (
     get_test_home_assistant, load_fixture)
 
 VALID_CONFIG = {
-    "vultr": {
-        "api_key": "ABCDEFG1234567"
+    'vultr': {
+        'api_key': 'ABCDEFG1234567'
     }
 }
 
@@ -22,7 +22,7 @@ class TestVultr(unittest.TestCase):
     def setUp(self):
         """Initialize values for this test case class."""
         self.hass = get_test_home_assistant()
-        self.config = copy(VALID_CONFIG)
+        self.config = VALID_CONFIG
 
     def tearDown(self):  # pylint: disable=invalid-name
         """Stop everything that we started."""
@@ -34,17 +34,15 @@ class TestVultr(unittest.TestCase):
         mock.get(
             'https://api.vultr.com/v1/account/info?api_key=ABCDEFG1234567',
             text=load_fixture('vultr_account_info.json'))
-
         mock.get(
             'https://api.vultr.com/v1/server/list?api_key=ABCDEFG1234567',
             text=load_fixture('vultr_server_list.json'))
 
         response = vultr.setup(self.hass, self.config)
-
         self.assertTrue(response)
 
-    @requests_mock.Mocker()
-    def test_setup_no_api_key(self, mock):
+    def test_setup_no_api_key(self):
         """Test failed setup with missing API Key."""
-        assert not setup.setup_component(self.hass, vultr.DOMAIN,
-                                         {"vultr": {}})
+        conf = deepcopy(self.config)
+        del conf['vultr']['api_key']
+        assert not setup.setup_component(self.hass, vultr.DOMAIN, conf)
