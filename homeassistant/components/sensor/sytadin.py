d8c89ca9f4d9218ac3587132c93adfd30b856701
gautric <gautric@redhat.com>
2017-10-24 17:35:48 +0200

    implements @fabaff and @pvizeli comments

--- homeassistant/components/sensor/sytadin.py
+++ homeassistant/components/sensor/sytadin.py
@@ -7,11 +7,13 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.sytadin/
 """
 import logging
+import requests
+import re
 
 from datetime import timedelta
 import voluptuous as vol
 
-from homeassistant.const import (LENGTH_KILOMETERS, CONF_NAME)
+from homeassistant.const import (LENGTH_KILOMETERS, CONF_NAME, CONF_MONITORED_CONDITIONS)
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -27,26 +29,29 @@ TRAFFIC_JAM_XPATH = '//*[@id="main_content"]/div[1]/div/span[3]/text()'
 MEAN_VELOCITY_XPATH = '//*[@id="main_content"]/div[2]/div/span[3]/text()'
 CONGESTION_XPATH = '//*[@id="main_content"]/div[3]/div/span[3]/text()'
 
-TRAFFIC_JAM_REGEX = '([0-9]+)'
-MEAN_VELOCITY_REGEX = '([0-9]+)'
-CONGESTION_REGEX = '([0-9]+.[0-9]+)'
+SYSTADIN_REGEX = '(\d*\.\d+|\d+)'
 
-CONF_MONITORED_CONDITION = 'monitored_condition'
 OPTION_TRAFFIC_JAM = 'traffic_jam'
 OPTION_MEAN_VELOCITY = 'mean_velocity'
 OPTION_CONGESTION = 'congestion'
 CONF_UPDATE_INTERVAL = 'update_interval'
 
 SENSOR_TYPES = {
-    OPTION_TRAFFIC_JAM: ('Sytadin Traffic Jam', LENGTH_KILOMETERS),
-    OPTION_MEAN_VELOCITY: ('Sytadin Mean Velocity', LENGTH_KILOMETERS+'/h'),
-    OPTION_CONGESTION: ('Sytadin Congestion', '')
+    OPTION_TRAFFIC_JAM: ('Sytadin Traffic Jam',
+                            LENGTH_KILOMETERS,
+                            TRAFFIC_JAM_XPATH),
+    OPTION_MEAN_VELOCITY: ('Sytadin Mean Velocity',
+                            LENGTH_KILOMETERS+'/h',
+                            MEAN_VELOCITY_XPATH),
+    OPTION_CONGESTION: ('Sytadin Congestion',
+                            '',
+                            CONGESTION_XPATH)
 }
 
 TIMEOUT = 10
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_MONITORED_CONDITION): vol.In(SENSOR_TYPES.keys()),
+    vol.Required(CONF_MONITORED_CONDITIONS): vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
     vol.Optional(CONF_NAME): cv.string,
     vol.Optional(CONF_UPDATE_INTERVAL, default=timedelta(seconds=300)): (
         vol.All(cv.time_period, cv.positive_timedelta)),
@@ -55,39 +60,20 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
     options = [
-        it.strip() for it in config.get(CONF_MONITORED_CONDITION).split(',')
+        it.strip() for it in config.get(CONF_MONITORED_CONDITIONS)
     ]
 
-    name = config.get(CONF_NAME)
+    for option in options:
+        _LOGGER.debug('add new device: ' + option)
 
-    if OPTION_TRAFFIC_JAM in options:
-        if not name:
-            name = SENSOR_TYPES.get(OPTION_TRAFFIC_JAM)[0]
         add_devices([
-            SytadinSensor(name, SYSTADIN, TRAFFIC_JAM_XPATH,
-                          TRAFFIC_JAM_REGEX,
-                          SENSOR_TYPES.get(OPTION_TRAFFIC_JAM)[1],
-                          config.get(CONF_UPDATE_INTERVAL))
+            SytadinSensor(SENSOR_TYPES.get(option)[0],
+                            SYSTADIN,
+                            SENSOR_TYPES.get(option)[2],
+                            SYSTADIN_REGEX,
+                            SENSOR_TYPES.get(option)[1],
+                            config.get(CONF_UPDATE_INTERVAL))
             ])
-    if OPTION_MEAN_VELOCITY in options:
-        if not name:
-            name = SENSOR_TYPES.get(OPTION_MEAN_VELOCITY)[0]
-        add_devices([
-            SytadinSensor(name, SYSTADIN,
-                          MEAN_VELOCITY_XPATH, MEAN_VELOCITY_REGEX,
-                          SENSOR_TYPES.get(OPTION_MEAN_VELOCITY)[1]+'/h',
-                          config.get(CONF_UPDATE_INTERVAL))
-            ])
-    if OPTION_CONGESTION in options:
-        if not name:
-            name = SENSOR_TYPES.get(OPTION_CONGESTION)[0]
-        add_devices([
-            SytadinSensor(name, SYSTADIN, CONGESTION_XPATH,
-                          CONGESTION_REGEX,
-                          SENSOR_TYPES.get(OPTION_CONGESTION)[1],
-                          config.get(CONF_UPDATE_INTERVAL))
-            ])
-
 
 class SytadinSensor(Entity):
     """Sytadin Sensor."""
@@ -119,8 +105,7 @@ class SytadinSensor(Entity):
 
     def _update(self):
         """Fetch new state data for the sensor."""
-        import requests
-        import re
+
         from lxml import etree
 
         html = requests.get(self._url, timeout=TIMEOUT)
