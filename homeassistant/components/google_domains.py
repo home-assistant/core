d1e3d63c1ea40d5827d405c9bb96ae034dbc423f
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-10-25 09:33:54 +0200

    Update google_domains.py

--- homeassistant/components/google_domains.py
+++ homeassistant/components/google_domains.py
@@ -61,7 +61,7 @@ def async_setup(hass, config):
     hass.helpers.event.async_track_time_interval(
         update_domain_interval, INTERVAL)
 
-    return true
+    return True
 
 
 @asyncio.coroutine
