a6f02da5734ba628afd43f8aabebd30d58d3712a
Fabian Affolter <mail@fabian-affolter.ch>
2017-10-29 21:00:07 +0100

    Remove reference to binary sensor

--- homeassistant/components/switch/gc100.py
+++ homeassistant/components/switch/gc100.py
@@ -1,19 +1,16 @@
 """
-Support for binary sensor using GC100.
+Support for switches using GC100.
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/binary_sensor.gc100/
+https://home-assistant.io/components/switch.gc100/
 """
-
 import voluptuous as vol
 
-from homeassistant.components.gc100 import DATA_GC100
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.gc100 import DATA_GC100, CONF_PORTS
 from homeassistant.components.switch import (PLATFORM_SCHEMA)
 from homeassistant.helpers.entity import ToggleEntity
 from homeassistant.const import DEVICE_DEFAULT_NAME
-import homeassistant.helpers.config_validation as cv
-
-CONF_PORTS = 'ports'
 
 DEPENDENCIES = ['gc100']
 
@@ -39,7 +36,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 
 class GC100Switch(ToggleEntity):
-    """Represent a switch / relay from GC100."""
+    """Represent a switch/relay from GC100."""
 
     def __init__(self, name, port_addr, gc100):
         """Initialize the GC100 switch."""
@@ -47,17 +44,11 @@ class GC100Switch(ToggleEntity):
         self._name = name or DEVICE_DEFAULT_NAME
         self._port_addr = port_addr
         self._gc100 = gc100
-
         self._state = None
 
     @property
-    def should_poll(self):
-        """Should poll."""
-        return True
-
-    @property
     def name(self):
-        """Return the name of the sensor."""
+        """Return the name of the switch."""
         return self._name
 
     @property
@@ -78,6 +69,6 @@ class GC100Switch(ToggleEntity):
         self._gc100.read_sensor(self._port_addr, self.set_state)
 
     def set_state(self, state):
-        """Tell home-assistant about the current state."""
+        """Set the current state."""
         self._state = state == 1
         self.schedule_update_ha_state()
