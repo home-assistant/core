29895f38a3cdceeaaf650730b4d1836274931ee4
Heiko Thiery <heiko.thiery@gmail.com>
2017-10-29 06:52:51 +0100

    add pylacrosse version to REQUIREMENTS
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- homeassistant/components/sensor/lacrosse.py
+++ homeassistant/components/sensor/lacrosse.py
@@ -17,7 +17,7 @@ from homeassistant.helpers.entity import Entity, async_generate_entity_id
 from homeassistant.helpers.event import async_track_point_in_utc_time
 from homeassistant.util import dt as dt_util
 
-REQUIREMENTS = ['pylacrosse']
+REQUIREMENTS = ['pylacrosse==0.2.7']
 
 _LOGGER = logging.getLogger(__name__)
 
