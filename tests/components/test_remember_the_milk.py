1ad096b2cd18322e9e89d57d4f5c25c1a73e9678
Christian Kühnel <christian.kuehnel@gmail.com>
2017-10-29 16:06:18 +0100

    fixed static analysis findings and failing test case

--- tests/components/test_remember_the_milk.py
+++ tests/components/test_remember_the_milk.py
@@ -1,7 +1,6 @@
 """Tests for the Remember The Milk component."""
 
 import logging
-import os
 import unittest
 from unittest.mock import patch, mock_open, Mock
 
@@ -18,35 +17,33 @@ class TestConfiguration(unittest.TestCase):
     def setUp(self):
         """Set up test home assistant main loop."""
         self.hass = get_test_home_assistant()
+        self.profile = "myprofile"
+        self.token = "mytoken"
+        self.json_string = '{"myprofile": {"token": "mytoken"}}'
 
     def tearDown(self):
         """Exit home assistant."""
         self.hass.stop()
 
     def test_create_new(self):
-        """Test creating a new config file and reading data back."""
-        profile = "myprofile"
-        token = "mytoken"
-        json_string = '{"myprofile": {"token": "mytoken"}}'
-
+        """Test creating a new config file."""
         with patch("builtins.open", mock_open()), \
                 patch("os.path.isfile", Mock(return_value=False)):
             config = rtm.RememberTheMilkConfiguration(self.hass)
-            config.set_token(profile, token)
-        self.assertEqual(config.get_token(profile), token)
-        self.assertTrue(os.path.exists(rtm.CONFIG_FILE_NAME))
+            config.set_token(self.profile, self.token)
+        self.assertEqual(config.get_token(self.profile), self.token)
 
-        del config
-
-        with patch("builtins.open", mock_open(read_data=json_string)), \
+    def test_load_config(self):
+        """Test loading an existing token from the file."""
+        with patch("builtins.open", mock_open(read_data=self.json_string)), \
                 patch("os.path.isfile", Mock(return_value=True)):
             config = rtm.RememberTheMilkConfiguration(self.hass)
-        self.assertEqual(config.get_token(profile), token)
+        self.assertEqual(config.get_token(self.profile), self.token)
 
     def test_invalid_data(self):
         """Test starts with invalid data and should not raise an exception."""
-
-        with patch("builtins.open", mock_open(read_data='random charachters')),\
+        with patch("builtins.open",
+                   mock_open(read_data='random charachters')),\
                 patch("os.path.isfile", Mock(return_value=True)):
             config = rtm.RememberTheMilkConfiguration(self.hass)
         self.assertIsNotNone(config)
