2863deadf5671c4014bb608bf0bc7f2974287d15
Fabian Affolter <fabian@affolter-engineering.ch>
2017-10-30 07:37:42 +0100

    Add initial tests

---
+++ tests/components/system_monitoring/test_system_monitoring.py
@@ -0,0 +1,46 @@
+"""The tests for the System Monitoring component."""
+import unittest
+
+from homeassistant.components import system_monitoring
+from homeassistant.components.weather import (
+    ATTR_WEATHER_ATTRIBUTION, ATTR_WEATHER_HUMIDITY, ATTR_WEATHER_OZONE,
+    ATTR_WEATHER_PRESSURE, ATTR_WEATHER_TEMPERATURE, ATTR_WEATHER_WIND_BEARING,
+    ATTR_WEATHER_WIND_SPEED, ATTR_FORECAST, ATTR_FORECAST_TEMP)
+from homeassistant.util.unit_system import METRIC_SYSTEM
+from homeassistant.setup import setup_component
+
+from tests.common import get_test_home_assistant
+
+
+class TestSystemMonitoring(unittest.TestCase):
+    """Test the System Monitoring component."""
+
+    def setUp(self):
+        """Setup things to be run when tests are started."""
+        self.hass = get_test_home_assistant()
+        self.hass.config.units = METRIC_SYSTEM
+        self.assertTrue(setup_component(self.hass, system_monitoring.DOMAIN, {
+            'system_monitoring': {
+                'platform': 'demo',
+            }
+        }))
+
+    def tearDown(self):
+        """Stop down everything that was started."""
+        self.hass.stop()
+
+    def test_value_to_state(self):
+        """Test system monitoring resource."""
+        state = self.hass.states.get('system_monitoring.cpu_speed')
+        assert state is not None
+
+        assert float(state.state) == 1.3
+
+        data = state.attributes
+        assert data.get('unit_of_measurement') == 'GHz'
+
+    def test_naming_with_system(self):
+        """Test system monitoring resource."""
+        state = self.hass.states.get(
+            'system_monitoring.server_average_load_15m')
+        assert state is not None
