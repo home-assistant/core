dee3e6c54c20476a79e29a3d8758a9901967f4f1
Matt White <m.w.white@gmail.com>
2017-10-31 20:08:31 -0600

    Update entity filter helper to return function

--- homeassistant/helpers/entityfilter.py
+++ homeassistant/helpers/entityfilter.py
@@ -3,62 +3,76 @@
 from homeassistant.core import split_entity_id
 
 
-class EntityFilter():
-    """Class that implements an include/exclude filter for entities/domains."""
+def generate_filter(include_domains, include_entities,
+                    exclude_domains, exclude_entities):
+    """Set up the filter."""
+    include_d = set(include_domains)
+    include_e = set(include_entities)
+    exclude_d = set(exclude_domains)
+    exclude_e = set(exclude_entities)
 
-    def __init__(self, include_domains, include_entities,
-                 exclude_domains, exclude_entities):
-        """Set up the filter."""
-        self.include_d = set(include_domains)
-        self.include_e = set(include_entities)
-        self.exclude_d = set(exclude_domains)
-        self.exclude_e = set(exclude_entities)
+    have_exclude = bool(exclude_e or exclude_d)
+    have_include = bool(include_e or include_d)
 
-        self.have_exclude = bool(self.exclude_e or self.exclude_d)
-        self.have_include = bool(self.include_e or self.include_d)
+    # Case 1 - no includes or excludes - pass all entities
+    if not have_include and not have_exclude:
+        return lambda entity_id: True
 
-    def check_entity(self, entity_id):
-        """Check if a given entity_id should be filtered."""
-        domain = split_entity_id(entity_id)[0]
+    # Case 2 - includes, no excludes - only include specified entities
+    if have_include and not have_exclude:
+        def entity_filter_2(entity_id):
+            """Return filter function for case 2."""
+            domain = split_entity_id(entity_id)[0]
+            return (entity_id in include_e or
+                    domain in include_d)
 
-        # Case 1 - no includes or excludes - pass all entities
-        if not self.have_include and not self.have_exclude:
-            return True
+        return entity_filter_2
 
-        # Case 2 - includes, no excludes - only include specified entities
-        if self.have_include and not self.have_exclude:
-            return entity_id in self.include_e or \
-                        domain in self.include_d
+    # Case 3 - excludes, no includes - only exclude specified entities
+    if not have_include and have_exclude:
+        def entity_filter_3(entity_id):
+            """Return filter function for case 3."""
+            domain = split_entity_id(entity_id)[0]
+            return (entity_id not in exclude_e and
+                    domain not in exclude_d)
 
-        # Case 3 - excludes, no includes - only exclude specified entities
-        if not self.have_include and self.have_exclude:
-            return entity_id not in self.exclude_e and \
-                        domain not in self.exclude_d
+        return entity_filter_3
 
-        # Case 4 - both includes and excludes specified
-        # Case 4a - include domain specified
-        #  - if domain is included, and entity not excluded, pass
-        #  - if domain is not included, and entity not included, fail
-        # note: if both include and exclude domains specified,
-        #   the exclude domains are ignored
-        if self.include_d:
-            if domain in self.include_d:
-                return entity_id not in self.exclude_e
+    # Case 4 - both includes and excludes specified
+    # Case 4a - include domain specified
+    #  - if domain is included, and entity not excluded, pass
+    #  - if domain is not included, and entity not included, fail
+    # note: if both include and exclude domains specified,
+    #   the exclude domains are ignored
+    if include_d:
+        def entity_filter_4a(entity_id):
+            """Return filter function for case 4a."""
+            domain = split_entity_id(entity_id)[0]
+            if domain in include_d:
+                return entity_id not in exclude_e
             else:
-                return entity_id in self.include_e
+                return entity_id in include_e
 
-        # Case 4b - exclude domain specified
-        #  - if domain is excluded, and entity not included, fail
-        #  - if domain is not excluded, and entity not excluded, pass
-        if self.exclude_d:
-            if domain in self.exclude_d:
-                return entity_id in self.include_e
+        return entity_filter_4a
+
+    # Case 4b - exclude domain specified
+    #  - if domain is excluded, and entity not included, fail
+    #  - if domain is not excluded, and entity not excluded, pass
+    if exclude_d:
+        def entity_filter_4b(entity_id):
+            """Return filter function for case 4b."""
+            domain = split_entity_id(entity_id)[0]
+            if domain in exclude_d:
+                return entity_id in include_e
             else:
-                return entity_id not in self.exclude_e
+                return entity_id not in exclude_e
 
-        # Case 4c - neither include or exclude domain specified
-        #  - Only pass if entity is included.  Ignore entity excludes.
-        if entity_id in self.include_e:
-            return True
+        return entity_filter_4b
 
-        return False
+    # Case 4c - neither include or exclude domain specified
+    #  - Only pass if entity is included.  Ignore entity excludes.
+    def entity_filter_4c(entity_id):
+        """Return filter function for case 4c."""
+        return entity_id in include_e
+
+    return entity_filter_4c
--- tests/helpers/test_entityfilter.py
+++ tests/helpers/test_entityfilter.py
@@ -1,5 +1,5 @@
 """The tests for the EntityFitler component."""
-from homeassistant.helpers.entityfilter import EntityFilter
+from homeassistant.helpers.entityfilter import generate_filter
 
 
 def test_no_filters_case_1():
@@ -8,10 +8,10 @@ def test_no_filters_case_1():
     incl_ent = {}
     excl_dom = {}
     excl_ent = {}
-    testfilter = EntityFilter(incl_dom, incl_ent, excl_dom, excl_ent)
+    testfilter = generate_filter(incl_dom, incl_ent, excl_dom, excl_ent)
 
     for value in ("sensor.test", "sun.sun", "light.test"):
-        assert testfilter.check_entity(value)
+        assert testfilter(value)
 
 
 def test_includes_only_case_2():
@@ -20,13 +20,13 @@ def test_includes_only_case_2():
     incl_ent = {'binary_sensor.working'}
     excl_dom = {}
     excl_ent = {}
-    testfilter = EntityFilter(incl_dom, incl_ent, excl_dom, excl_ent)
+    testfilter = generate_filter(incl_dom, incl_ent, excl_dom, excl_ent)
 
-    assert testfilter.check_entity("sensor.test")
-    assert testfilter.check_entity("light.test")
-    assert testfilter.check_entity("binary_sensor.working")
-    assert testfilter.check_entity("binary_sensor.notworking") is False
-    assert testfilter.check_entity("sun.sun") is False
+    assert testfilter("sensor.test")
+    assert testfilter("light.test")
+    assert testfilter("binary_sensor.working")
+    assert testfilter("binary_sensor.notworking") is False
+    assert testfilter("sun.sun") is False
 
 
 def test_excludes_only_case_3():
@@ -35,13 +35,13 @@ def test_excludes_only_case_3():
     incl_ent = {}
     excl_dom = {'light', 'sensor'}
     excl_ent = {'binary_sensor.working'}
-    testfilter = EntityFilter(incl_dom, incl_ent, excl_dom, excl_ent)
+    testfilter = generate_filter(incl_dom, incl_ent, excl_dom, excl_ent)
 
-    assert testfilter.check_entity("sensor.test") is False
-    assert testfilter.check_entity("light.test") is False
-    assert testfilter.check_entity("binary_sensor.working") is False
-    assert testfilter.check_entity("binary_sensor.another")
-    assert testfilter.check_entity("sun.sun") is True
+    assert testfilter("sensor.test") is False
+    assert testfilter("light.test") is False
+    assert testfilter("binary_sensor.working") is False
+    assert testfilter("binary_sensor.another")
+    assert testfilter("sun.sun") is True
 
 
 def test_with_include_domain_case4a():
@@ -50,15 +50,15 @@ def test_with_include_domain_case4a():
     incl_ent = {'binary_sensor.working'}
     excl_dom = {}
     excl_ent = {'light.ignoreme', 'sensor.notworking'}
-    testfilter = EntityFilter(incl_dom, incl_ent, excl_dom, excl_ent)
+    testfilter = generate_filter(incl_dom, incl_ent, excl_dom, excl_ent)
 
-    assert testfilter.check_entity("sensor.test")
-    assert testfilter.check_entity("sensor.notworking") is False
-    assert testfilter.check_entity("light.test")
-    assert testfilter.check_entity("light.ignoreme") is False
-    assert testfilter.check_entity("binary_sensor.working")
-    assert testfilter.check_entity("binary_sensor.another") is False
-    assert testfilter.check_entity("sun.sun") is False
+    assert testfilter("sensor.test")
+    assert testfilter("sensor.notworking") is False
+    assert testfilter("light.test")
+    assert testfilter("light.ignoreme") is False
+    assert testfilter("binary_sensor.working")
+    assert testfilter("binary_sensor.another") is False
+    assert testfilter("sun.sun") is False
 
 
 def test_exclude_domain_case4b():
@@ -67,15 +67,15 @@ def test_exclude_domain_case4b():
     incl_ent = {'binary_sensor.working'}
     excl_dom = {'binary_sensor'}
     excl_ent = {'light.ignoreme', 'sensor.notworking'}
-    testfilter = EntityFilter(incl_dom, incl_ent, excl_dom, excl_ent)
+    testfilter = generate_filter(incl_dom, incl_ent, excl_dom, excl_ent)
 
-    assert testfilter.check_entity("sensor.test")
-    assert testfilter.check_entity("sensor.notworking") is False
-    assert testfilter.check_entity("light.test")
-    assert testfilter.check_entity("light.ignoreme") is False
-    assert testfilter.check_entity("binary_sensor.working")
-    assert testfilter.check_entity("binary_sensor.another") is False
-    assert testfilter.check_entity("sun.sun") is True
+    assert testfilter("sensor.test")
+    assert testfilter("sensor.notworking") is False
+    assert testfilter("light.test")
+    assert testfilter("light.ignoreme") is False
+    assert testfilter("binary_sensor.working")
+    assert testfilter("binary_sensor.another") is False
+    assert testfilter("sun.sun") is True
 
 
 def testno_domain_case4c():
@@ -84,12 +84,12 @@ def testno_domain_case4c():
     incl_ent = {'binary_sensor.working'}
     excl_dom = {}
     excl_ent = {'light.ignoreme', 'sensor.notworking'}
-    testfilter = EntityFilter(incl_dom, incl_ent, excl_dom, excl_ent)
+    testfilter = generate_filter(incl_dom, incl_ent, excl_dom, excl_ent)
 
-    assert testfilter.check_entity("sensor.test") is False
-    assert testfilter.check_entity("sensor.notworking") is False
-    assert testfilter.check_entity("light.test") is False
-    assert testfilter.check_entity("light.ignoreme") is False
-    assert testfilter.check_entity("binary_sensor.working")
-    assert testfilter.check_entity("binary_sensor.another") is False
-    assert testfilter.check_entity("sun.sun") is False
+    assert testfilter("sensor.test") is False
+    assert testfilter("sensor.notworking") is False
+    assert testfilter("light.test") is False
+    assert testfilter("light.ignoreme") is False
+    assert testfilter("binary_sensor.working")
+    assert testfilter("binary_sensor.another") is False
+    assert testfilter("sun.sun") is False
