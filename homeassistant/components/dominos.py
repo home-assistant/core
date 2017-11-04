64adc0148b8607c7f2ad4d64796868594729eecb
Craig J. Ward <ward.craig.j@gmail.com>
2017-11-04 14:57:51 -0500

    component fixes

--- homeassistant/components/dominos.py
+++ homeassistant/components/dominos.py
@@ -5,6 +5,8 @@ import homeassistant.helpers.config_validation as cv
 from pizzapi import Customer, Address, Order
 import jsonpickle
 from homeassistant.helpers.entity import Entity
+import unicodedata
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,7 +25,8 @@ ATTR_DUMP_MENU = 'dump_menu'
 ATTR_ORDER_ENTITY = 'order_entity_id'
 
 COMMIT = 'ae140bd640d64ce5fdb638d5b11cfc8dae6c6587'
-REQUIREMENTS = ['https://github.com/wardcraigj/pizzapi/archive/%s.zip#pizzapi==0.0.2' % COMMIT, 'jsonpickle']
+REQUIREMENTS = [
+    'https://github.com/wardcraigj/pizzapi/archive/%s.zip#pizzapi==0.0.2' % COMMIT, 'jsonpickle']
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -33,8 +36,8 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(ATTR_EMAIL): cv.string,
         vol.Required(ATTR_PHONE): cv.string,
         vol.Required(ATTR_ADDRESS): cv.string,
-        vol.Optional(ATTR_ORDERS) : cv.ensure_list,
-        vol.Optional(ATTR_DUMP_MENU) : cv.boolean,
+        vol.Optional(ATTR_ORDERS): cv.ensure_list,
+        vol.Optional(ATTR_DUMP_MENU): cv.boolean,
     }),
 }, extra=vol.ALLOW_EXTRA)
 
@@ -46,62 +49,60 @@ def setup(hass, config):
     hass.data[DOMAIN] = []
 
     if config[DOMAIN].get(ATTR_DUMP_MENU):
-        dominos.create_product_entities(hass)
+        dominos.dump_menu(hass)
 
     for order in config[DOMAIN].get(ATTR_ORDERS):
         # _LOGGER.INFO('Creating Order')
-        hass.states.set('dominos.' + order['name'].replace(" ", "_"), json.dumps(order['codes']))
+        hass.states.set(
+            'dominos.' + order['name'].replace(" ", "_"), json.dumps(order['codes']))
     hass.services.register(DOMAIN, 'order', dominos.handle_order)
-
 
     # Return boolean to indicate that initialization was successfully.
     return True
+
 
 class Dominos():
 
     def __init__(self, hass, config):
         self.hass = hass
         self.customer = Customer(config[DOMAIN].get(ATTR_FIRST_NAME), config[DOMAIN].get(
-        ATTR_LAST_NAME), config[DOMAIN].get(ATTR_EMAIL), config[DOMAIN].get(ATTR_PHONE), config[DOMAIN].get(ATTR_ADDRESS))
-        self.address = Address(*self.customer.address.split(','), country=config[DOMAIN].get(ATTR_COUNTRY))
-        self.store = self.address.closest_store()
-        self.country = config[DOMAIN].get(ATTR_COUNTRY) 
+            ATTR_LAST_NAME), config[DOMAIN].get(ATTR_EMAIL), config[DOMAIN].get(ATTR_PHONE), config[DOMAIN].get(ATTR_ADDRESS))
+        self.address = Address(
+            *self.customer.address.split(','), country=config[DOMAIN].get(ATTR_COUNTRY))
+        self.country = config[DOMAIN].get(ATTR_COUNTRY)
 
     def handle_order(self, call=None):
         """Handle ordering pizza."""
+
+        store = self.address.closest_store()
 
         order_key = call.data.get(ATTR_ORDER_ENTITY)
         state = self.hass.states._states[order_key]
         order_codes = json.loads(state.state)
 
-        order = Order(self.store, self.customer,self.address, self.country)
+        order = Order(store, self.customer, self.address, self.country)
 
         for code in order_codes:
             order.add_item(code['code'])
 
         order.place()
 
-        _LOGGER.warning('here')
+    def dump_menu(self, hass):
 
-    def create_product_entities(self, hass):
-        menu = self.store.get_menu()
+        store = self.address.closest_store()
+        menu = store.get_menu()
         for product in menu.products:
-            _LOGGER.warning('name: ' + product.name + ' variants: ' + ''.join(product.menu_data['Variants']))
+            if isinstance(product.menu_data['Variants'], list):
+                variants = ', '.join(product.menu_data['Variants'])
+            else:
+                variants = product.menu_data['Variants']
+            message = 'name: ' + product.name + ' variants: ' + variants
+            try:
+                
+                _LOGGER.warning(unicodedata.normalize('NFKC', message).encode('ascii','ignore').decode('ascii'))
+            except Exception:
+                _LOGGER.warn('here')
 
-        _LOGGER.warning('here')
-
-
-
-class DominosProduct(Entity):
-
-    def __init__(self, menu_item):
-        self._name = menu_item.name
-        self._variants = menu_item.menu_data['Variants']
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def variants(self):
-        return self._variants
+        # except Exception:
+        #     _LOGGER.error(
+        #         'Tried to dump menu but an error occurred. Check your config and make sure stores are open.')
