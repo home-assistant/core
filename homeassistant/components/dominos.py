18bbbcaf30d4b125da29125e84c3f2e7debb2a36
Craig J. Ward <ward.craig.j@gmail.com>
2017-11-05 12:03:56 -0600

    fix hound errors

--- homeassistant/components/dominos.py
+++ homeassistant/components/dominos.py
@@ -3,7 +3,6 @@ import logging
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from pizzapi import Customer, Address, Order
-from homeassistant.helpers.entity import Entity
 import unicodedata
 
 
@@ -23,9 +22,9 @@ ATTR_ORDERS = 'orders'
 ATTR_DUMP_MENU = 'dump_menu'
 ATTR_ORDER_ENTITY = 'order_entity_id'
 
-COMMIT = '183d0f5522e7fa99776e8c6c692b56e69f0e6b8f'
+REV = '183d0f5522e7fa99776e8c6c692b56e69f0e6b8f'
 REQUIREMENTS = [
-    'https://github.com/wardcraigj/pizzapi/archive/%s.zip#pizzapi==0.0.2' % COMMIT]
+    'https://github.com/wardcraigj/pizzapi/archive/%s.zip#pizzapi==0.0.2' % REV]
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -53,7 +52,8 @@ def setup(hass, config):
     for order in config[DOMAIN].get(ATTR_ORDERS):
         # _LOGGER.INFO('Creating Order')
         hass.states.set(
-            'dominos.' + order['name'].replace(" ", "_"), json.dumps(order['codes']))
+            'dominos.' + order['name'].replace(" ", "_"),
+            json.dumps(order['codes']))
     hass.services.register(DOMAIN, 'order', dominos.handle_order)
 
     # Return boolean to indicate that initialization was successfully.
@@ -64,10 +64,13 @@ class Dominos():
 
     def __init__(self, hass, config):
         self.hass = hass
-        self.customer = Customer(config[DOMAIN].get(ATTR_FIRST_NAME), config[DOMAIN].get(
-            ATTR_LAST_NAME), config[DOMAIN].get(ATTR_EMAIL), config[DOMAIN].get(ATTR_PHONE), config[DOMAIN].get(ATTR_ADDRESS))
+        self.customer = Customer(config[DOMAIN].get(
+            ATTR_FIRST_NAME), config[DOMAIN].get(
+            ATTR_LAST_NAME), config[DOMAIN].get(ATTR_EMAIL), config[DOMAIN].get(
+            ATTR_PHONE), config[DOMAIN].get(ATTR_ADDRESS))
         self.address = Address(
-            *self.customer.address.split(','), country=config[DOMAIN].get(ATTR_COUNTRY))
+            *self.customer.address.split(','),
+            country=config[DOMAIN].get(ATTR_COUNTRY))
         self.country = config[DOMAIN].get(ATTR_COUNTRY)
 
     def handle_order(self, call=None):
@@ -95,8 +98,10 @@ class Dominos():
                 variants = ', '.join(product.menu_data['Variants'])
             else:
                 variants = product.menu_data['Variants']
-            
+
             message = 'name: ' + product.name + ' variants: ' + variants
-            
-            #We get some weird product names sometimes, so clobber this to make it logger safe
-            _LOGGER.warning(unicodedata.normalize('NFKC', message).encode('ascii','ignore').decode('ascii'))
+
+            # We get some weird product names sometimes,
+            # so clobber this to make it logger safe
+            _LOGGER.warning(unicodedata.normalize('NFKC', message)
+                .encode('ascii','ignore').decode('ascii'))
