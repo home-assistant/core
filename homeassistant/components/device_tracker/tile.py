222e528a55d3d27dd832a488445b90a15dcd1f95
Aaron Bach <bachya1208@gmail.com>
2017-11-07 21:44:06 -0700

    Initial work in place

---
+++ homeassistant/components/device_tracker/tile.py
@@ -0,0 +1,82 @@
+"""
+Support for Tile® Bluetooth trackers
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/device_tracker.tile/
+"""
+import logging
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.device_tracker import (
+    PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.const import (
+    CONF_EMAIL, CONF_MONITORED_VARIABLES, CONF_PASSWORD)
+from homeassistant.helpers.event import track_utc_time_change
+from homeassistant.util import slugify
+
+_LOGGER = logging.getLogger(__name__)
+
+REQUIREMENTS = ['pytile==1.0.0']
+
+DEVICE_TYPES = ['PHONE', 'TILE']
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_EMAIL): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_MONITORED_VARIABLES):
+        vol.All(cv.ensure_list, [vol.In(DEVICE_TYPES)]),
+})
+
+
+def setup_scanner(hass, config: dict, see, discovery_info=None):
+    """Validate the configuration and return a Tile scanner."""
+    TileDeviceScanner(hass, config, see)
+    return True
+
+
+class TileDeviceScanner(DeviceScanner):
+    """Define a device scanner for Tiles."""
+
+    def __init__(self, hass, config, see):
+        """Initialize."""
+        from pytile import Client as TileAPI
+
+        self._client = TileAPI(
+            config[CONF_EMAIL],
+            config[CONF_PASSWORD])
+        self._types = config.get(CONF_MONITORED_VARIABLES)
+
+        self.devices = {}
+        self.hass = hass
+        self.see = see
+
+        track_utc_time_change(
+            self.hass, self._update_info, second=range(0, 60, 30))
+
+    def _update_info(self, now=None) -> None:
+        """Update the device info."""
+        device_data = self._client.get_tiles(type_whitelist=self._types)
+
+        try:
+            self.devices = device_data['result']
+        except KeyError:
+            _LOGGER.warning('No Tiles found')
+            return False
+
+        for _, info in self.devices.items():
+            dev_id = 'tile_{0}'.format(slugify(info['name']))
+            lat = info['tileState']['latitude']
+            lon = info['tileState']['longitude']
+
+            attrs = {
+                'altitude': info['tileState']['altitude'],
+                'is_lost': info['tileState']['is_lost'],
+                'last_seen': info['tileState']['timestamp'],
+                'last_updated': device_data['timestamp_ms'],
+            }
+
+            self.see(
+                dev_id=dev_id, gps=(lat, lon), attributes=attrs
+            )
