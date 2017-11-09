c173eee093e828e3031f8c406ae1cbeaeb30fc37
Anders Melchiorsen <amelchio@nogoto.net>
2017-11-09 17:57:41 +0100

    Remove lag from Harmony remote platform (#10218)
    * Simplify kwargs handling
    
    * Move Harmony remote to a persistent connection with push feedback
    
    * Make default delay_secs configurable on the harmony platform
    
    * Remove lint
    
    * Fix delay_secs with discovery
    
    * Temporary location for updated pyharmony
    
    * Remove lint
    
    * Update pyharmony to 1.0.17
    
    * Remove lint
    
    * Return an Optional marker
    
    * Update pyharmony to 1.0.18

--- homeassistant/components/remote/__init__.py
+++ homeassistant/components/remote/__init__.py
@@ -61,8 +61,7 @@ REMOTE_SERVICE_SEND_COMMAND_SCHEMA = REMOTE_SERVICE_SCHEMA.extend({
     vol.Optional(ATTR_DEVICE): cv.string,
     vol.Optional(
         ATTR_NUM_REPEATS, default=DEFAULT_NUM_REPEATS): cv.positive_int,
-    vol.Optional(
-        ATTR_DELAY_SECS, default=DEFAULT_DELAY_SECS): vol.Coerce(float)
+    vol.Optional(ATTR_DELAY_SECS): vol.Coerce(float),
 })
 
 
@@ -141,25 +140,18 @@ def async_setup(hass, config):
     def async_handle_remote_service(service):
         """Handle calls to the remote services."""
         target_remotes = component.async_extract_from_service(service)
-
-        activity_id = service.data.get(ATTR_ACTIVITY)
-        device = service.data.get(ATTR_DEVICE)
-        command = service.data.get(ATTR_COMMAND)
-        num_repeats = service.data.get(ATTR_NUM_REPEATS)
-        delay_secs = service.data.get(ATTR_DELAY_SECS)
+        kwargs = service.data.copy()
 
         update_tasks = []
         for remote in target_remotes:
             if service.service == SERVICE_TURN_ON:
-                yield from remote.async_turn_on(activity=activity_id)
+                yield from remote.async_turn_on(**kwargs)
             elif service.service == SERVICE_TOGGLE:
-                yield from remote.async_toggle(activity=activity_id)
+                yield from remote.async_toggle(**kwargs)
             elif service.service == SERVICE_SEND_COMMAND:
-                yield from remote.async_send_command(
-                    device=device, command=command,
-                    num_repeats=num_repeats, delay_secs=delay_secs)
+                yield from remote.async_send_command(**kwargs)
             else:
-                yield from remote.async_turn_off(activity=activity_id)
+                yield from remote.async_turn_off(**kwargs)
 
             if not remote.should_poll:
                 continue
