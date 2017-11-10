da54023ede7f01a8f1076fe4141e8d7b3155a2c3
carstenschroeder <carsten.schroeder@eskill.de>
2017-11-10 17:48:57 +0100

    Enable non dimmable lights

--- homeassistant/components/light/ads.py
+++ homeassistant/components/light/ads.py
@@ -15,7 +15,6 @@ _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = ['ads']
 DEFAULT_NAME = 'ADS Light'
 CONF_ADSVAR_BRIGHTNESS = 'adsvar_brightness'
-SUPPORT_ADS = SUPPORT_BRIGHTNESS
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_ADSVAR): cv.string,
     vol.Optional(CONF_ADSVAR_BRIGHTNESS): cv.string,
@@ -43,12 +42,11 @@ class AdsLight(Light):
     def __init__(self, ads_hub, varname_enable, varname_brightness, devname):
         """Initialize AdsLight entity."""
         self._ads_hub = ads_hub
-        self._on_state = False
-        self._brightness = 50
+        self._on_state = None
+        self._brightness = 255
         self._devname = devname
         self.varname_enable = varname_enable
         self.varname_brightness = varname_brightness
-        self.stype = 'dimmer'
 
     @property
     def name(self):
@@ -57,13 +55,7 @@ class AdsLight(Light):
 
     @property
     def brightness(self):
-        """
-        Brightness of the light.
-
-        This method is optional. Removing it indicates to Home Assistant
-        that brightness is not supported for this light.
-
-        """
+        """Return the brightness of this light between 0..255."""
         return self._brightness
 
     @property
@@ -74,7 +66,8 @@ class AdsLight(Light):
     @property
     def supported_features(self):
         """Flag supported features."""
-        return SUPPORT_ADS
+        if self.varname_brightness is not None:
+            return SUPPORT_BRIGHTNESS  
 
     def turn_on(self, **kwargs):
         """Turn the light on or set a specific dimmer value."""
