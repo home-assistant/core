379e50aa088d29fcc1ad114a285744ee837933f5
Derek Brooks <derek@broox.com>
2017-11-11 00:22:37 -0600

    first stab at the nuheat components

---
+++ homeassistant/components/climate/nuheat.py
@@ -0,0 +1,208 @@
+"""
+Support for NuHeat thermostats.
+
+For more details about this platform, please refer to the documentation at
+"""
+import logging
+from datetime import timedelta
+
+from homeassistant.components.climate import (
+    ClimateDevice,
+    STATE_HEAT,
+    STATE_IDLE)
+from homeassistant.const import (
+    ATTR_TEMPERATURE,
+    TEMP_CELSIUS,
+    TEMP_FAHRENHEIT)
+from homeassistant.util import Throttle
+
+DEPENDENCIES = ["nuheat"]
+
+_LOGGER = logging.getLogger(__name__)
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
+
+MODE_AUTO = "auto"  # Run device schedule
+MODE_AWAY = "away"
+MODE_HOLD_TEMPERATURE = "temperature"
+MODE_TEMPORARY_HOLD = "temporary_temperature"
+# TODO: offline?
+
+OPERATION_LIST = [STATE_HEAT, STATE_IDLE]
+
+SCHEDULE_HOLD = 3
+SCHEDULE_RUN = 1
+SCHEDULE_TEMPORARY_HOLD = 2
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    if discovery_info is None:
+        return
+
+    _LOGGER.info("Loading NuHeat thermostat component")
+
+    temperature_unit = hass.config.units.temperature_unit
+    _LOGGER.debug("temp_unit is %s", temperature_unit)
+    api, serial_numbers = hass.data[DATA_NUHEAT]
+
+    thermostats = [
+        NuHeatThermostat(api, serial_number, temperature_unit)
+        for serial_number in serial_numbers
+    ]
+    add_devices(thermostats, True)
+
+
+class NuHeatThermostat(ClimateDevice):
+    """Representation of a NuHeat Thermostat."""
+    def __init__(self, api, serial_number, temperature_unit):
+        self._thermostat = api.get_thermostat(serial_number)
+        self._temperature_unit = temperature_unit
+        self._force_update = False
+
+    @property
+    def name(self):
+        """Return the name of the thermostat."""
+        return self._thermostat.room
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        if self._temperature_unit == "C":
+            return TEMP_CELSIUS
+
+        return TEMP_FAHRENHEIT
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        if self._temperature_unit == "C":
+            return self._thermostat.celsius
+        
+        return self._thermostat.fahrenheit
+
+    @property
+    def current_operation(self):
+        """Return current operation. ie. heat, idle."""
+        if self._thermostat.heating:
+            return STATE_HEAT
+
+        return STATE_IDLE
+
+    @property
+    def min_temp(self):
+        """Return the minimum supported temperature for the thermostat."""
+        if self._temperature_unit == "C":
+            return self._thermostat.min_celsius
+
+        return self._thermostat.min_fahrenheit
+
+    @property
+    def max_temp(self):
+        """Return the maximum supported temperature for the thermostat."""
+        if self._temperature_unit == "C":
+            return self._thermostat.max_celsius
+
+        return self._thermostat.max_fahrenheit
+
+    @property
+    def target_temperature(self):
+        """Return the currently programmed temperature."""
+        if self._temperature_unit == "C":
+            return self._thermostat.target_celsius
+
+        return self._thermostat.target_fahrenheit
+
+    @property
+    def target_temperature_low(self):
+        """Return the lower bound temperature we try to reach."""
+        return self.target_temperature
+
+    @property
+    def target_temperature_high(self):
+        """Return the upper bound temperature we try to reach."""
+        return self.target_temperature
+
+    @property
+    def current_hold_mode(self):
+        """Return current hold mode."""
+        if self.is_away_mode_on:
+            return MODE_AWAY
+
+        schedule_mode = self._thermostat.schedule_mode
+        if schedule_mode == SCHEDULE_RUN:
+            return MODE_AUTO
+
+        if schedule_mode == SCHEDULE_HOLD:
+            return MODE_HOLD_TEMPERATURE
+
+        if schedule_mode == SCHEDULE_TEMPORARY_HOLD:
+            return MODE_TEMPORARY_HOLD
+
+        return MODE_AUTO
+
+    @property
+    def operation_list(self):
+        """Return list of possible operation modes."""
+        return OPERATION_LIST
+
+    @property
+    def is_away_mode_on(self):
+        """
+        Return true if away mode is on.
+
+        Away mode is determined by setting and HOLDing the target temperature
+        to the minimum temperature supported.
+        """
+        if self._thermostat.target_celsius > self._thermostat.min_celsius:
+            return False
+
+        if self._thermostat.schedule_mode != SCHEDULE_HOLD:
+            return False
+        
+        return True
+
+    def turn_away_mode_on(self):
+        """Turn away mode on."""
+        if self.is_away_mode_on:
+            return
+
+        kwargs = {}
+        kwargs[ATTR_TEMPERATURE] = self.min_temp
+
+        self.set_temperature(**kwargs)
+        self._force_update = True
+
+    def turn_away_mode_off(self):
+        """Turn away mode off."""
+        if not self.is_away_mode_on:
+            return
+
+        self._thermostat.resume_schedule()
+        self._force_update = True
+
+    def set_temperature(self, **kwargs):
+        """Set a new target temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if self._temperature_unit == "C":
+            self._thermostat.target_celsius = temperature
+        else:
+            self._thermostat.target_fahrenheit = temperature
+
+        _LOGGER.info(
+            "Setting NuHeat thermostat temperature to %s %s",
+            temperature, self.temperature_unit)
+
+        self._force_update = True
+
+    def update(self):
+        """Get the latest state from the thermostat."""
+        if self._force_update:
+            self._throttled_update(no_throttle=True)
+            self._force_update = False
+        else:
+            self._throttled_update()
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def _throttled_update(self):
+        """Get the latest state from the thermostat... but throttled!"""
+        self._thermostat.get_data()
