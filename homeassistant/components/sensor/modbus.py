c49271bda8cad7c61ca0f0caee2ed4e0d001c6d6
ziotibia81 <ziotibia1@libero.it>
2017-11-16 00:17:17 +0100

    Fix ValueError exception (#10596)
    * Fix ValueError exception
    
    structure = '>{:c}'.format(data_types[register.get(CONF_DATA_TYPE)][register.get(CONF_COUNT)])
    give:
    ValueError: Unknown format code 'c' for object of type 'str'
    
    * Minor typo

--- homeassistant/components/sensor/modbus.py
+++ homeassistant/components/sensor/modbus.py
@@ -70,7 +70,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         structure = '>i'
         if register.get(CONF_DATA_TYPE) != DATA_TYPE_CUSTOM:
             try:
-                structure = '>{:c}'.format(data_types[
+                structure = '>{}'.format(data_types[
                     register.get(CONF_DATA_TYPE)][register.get(CONF_COUNT)])
             except KeyError:
                 _LOGGER.error("Unable to detect data type for %s sensor, "
@@ -165,7 +165,7 @@ class ModbusRegisterSensor(Entity):
             if self._reverse_order:
                 registers.reverse()
         except AttributeError:
-            _LOGGER.error("No response from modbus slave %s register %s",
+            _LOGGER.error("No response from modbus slave %s, register %s",
                           self._slave, self._register)
             return
         byte_string = b''.join(
