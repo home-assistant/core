1d53d930df1c1dd7fe6f5ed5c13081f24c78cc9b
Robbie Trencheny <me@robbiet.us>
2017-11-17 00:54:30 -0800

    Add lock test

--- tests/components/alexa/test_smart_home.py
+++ tests/components/alexa/test_smart_home.py
@@ -132,12 +132,15 @@ def test_discovery_request(hass):
             'speed_list': ['low', 'medium', 'high']
         })
 
+    hass.states.async_set(
+        'lock.test', 'off', {'friendly_name': "Test lock"})
+
     msg = yield from smart_home.async_handle_message(hass, request)
 
     assert 'event' in msg
     msg = msg['event']
 
-    assert len(msg['payload']['endpoints']) == 9
+    assert len(msg['payload']['endpoints']) == 10
     assert msg['header']['name'] == 'Discover.Response'
     assert msg['header']['namespace'] == 'Alexa.Discovery'
 
@@ -231,6 +234,14 @@ def test_discovery_request(hass):
 
             assert 'Alexa.PercentageController' in caps
             assert 'Alexa.PowerController' in caps
+            continue
+
+        if appliance['endpointId'] == 'lock#test':
+            assert appliance['displayCategories'][0] == "SMARTLOCK"
+            assert appliance['friendlyName'] == "Test lock"
+            assert len(appliance['capabilities']) == 1
+            assert appliance['capabilities'][-1]['interface'] == \
+                'Alexa.LockController'
             continue
 
         raise AssertionError("Unknown appliance!")
@@ -604,4 +615,29 @@ def test_api_adjust_percentage(hass, result, adjust):
     assert len(call_fan) == 1
     assert call_fan[0].data['entity_id'] == 'fan.test_2'
     assert call_fan[0].data['speed'] == result
+    assert msg['header']['name'] == 'Response'
+
+
+@asyncio.coroutine
+@pytest.mark.parametrize("domain", ['lock'])
+def test_api_lock(hass, domain):
+    """Test api lock process."""
+    request = get_new_request(
+        'Alexa.LockController', 'Lock', '{}#test'.format(domain))
+
+    # settup test devices
+    hass.states.async_set(
+        '{}.test'.format(domain), 'off', {
+            'friendly_name': "Test {}".format(domain)
+        })
+
+    call = async_mock_service(hass, domain, 'lock')
+
+    msg = yield from smart_home.async_handle_message(hass, request)
+
+    assert 'event' in msg
+    msg = msg['event']
+
+    assert len(call) == 1
+    assert call[0].data['entity_id'] == '{}.test'.format(domain)
     assert msg['header']['name'] == 'Response'
