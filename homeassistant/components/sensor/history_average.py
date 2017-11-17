6e6ac6d6de9682b839acf9b35e12263a2c564bdf
Kevin Woley <kmwoley@users.noreply.github.com>
2017-11-17 08:47:26 -0800

    rewrite as async sensor, move state tracking in memory (reduce db reads)
    - update to core.py to ordering of States in a list via bisect
    - known issue: tests don't trigger the async update of the sensor (reading from history is fine)

--- homeassistant/components/sensor/history_average.py
+++ homeassistant/components/sensor/history_average.py
@@ -4,22 +4,25 @@ Calculates the weighted average of a sensor's historical numeric values.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.history_average/
 """
+import asyncio
+import bisect
 from collections import defaultdict
 import logging
 import math
-
 import voluptuous as vol
 
+from homeassistant.core import callback
 import homeassistant.components.history as history
-import homeassistant.helpers.config_validation as cv
-import homeassistant.util.dt as dt_util
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
     CONF_NAME, CONF_ENTITY_ID, CONF_UNIT_OF_MEASUREMENT,
     EVENT_HOMEASSISTANT_START)
 from homeassistant.exceptions import TemplateError
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.event import track_state_change
+from homeassistant.helpers.event import async_track_state_change
+from homeassistant.helpers.restore_state import async_get_last_state
+import homeassistant.util.dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -61,9 +64,11 @@ PLATFORM_SCHEMA = vol.All(PLATFORM_SCHEMA.extend({
 }), exactly_two_period_keys)
 
 
-# noinspection PyUnusedLocal
-def setup_platform(hass, config, add_devices, discovery_info=None):
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_devices, 
+                         discovery_info=None):
     """Set up the HistoryAverage sensor."""
+    print("async_setup_platform!")
     entity_id = config.get(CONF_ENTITY_ID)
     start = config.get(CONF_START)
     end = config.get(CONF_END)
@@ -75,14 +80,56 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         if template is not None:
             template.hass = hass
 
-    add_devices([HistoryAverageSensor(hass, entity_id, start, end,
-                                      duration, name, unit)])
-
+    async_add_devices(
+        [HistoryAverageSensor(hass, entity_id, start, end, duration, 
+            name, unit)])
     return True
 
 
 class HistoryAverageSensor(Entity):
-    """Representation of a HistoryAverage sensor."""
+    """HistoryAverage sensor which calculates time weighted averages."""
+
+    @asyncio.coroutine
+    def async_get_history(self):
+        """Pull states from recorder history."""
+        yield from self.async_update_period()
+        start, end = self._period
+
+        self._history = []
+
+        # get state changes history between start and now
+        states = history.state_changes_during_period(
+            self._hass, start, None, self._entity_id)
+        states = states.get(self._entity_id)
+        if(states):
+            self._history.extend(states)
+
+        print("async_get_history: ", self._history)
+
+    @asyncio.coroutine
+    def asysnc_trim_history(self):
+        """Remove items which are not relevent to the current period"""
+
+        yield from self.async_update_period()
+        start, end = self._period
+        start_timestamp = dt_util.as_timestamp(start)
+
+        def after_start(item, timestamp):
+            return item.last_changed.timestamp() >= timestamp 
+
+        # remove items before the current period's starting point,
+        # keeping the first item before the starting period to enable
+        # calculation of the state between start -> next state change
+        for index, x in enumerate(self._history[:]):
+            if after_start(x, start_timestamp):
+                break
+            else:
+                # remove if the next item is also before start
+                next_index = index + 1
+                if ((next_index < len(self._history)) and 
+                    (not after_start(self._history[next_index], 
+                        start_timestamp))):
+                    del self._history[index]
 
     def __init__(
             self, hass, entity_id, start, end, duration,
@@ -96,18 +143,39 @@ class HistoryAverageSensor(Entity):
         self._name = name
         self._unit_of_measurement = unit
 
-        self._period = (dt_util.utcnow(), dt_util.utcnow())
-        self.value = 0
+        # set defaults at init
+        now = dt_util.utcnow()
+        self._period = (now, now)
+        self._history = []
+        self._state = 0
 
-        def force_refresh(*args):
-            """Force the component to refresh."""
-            self.schedule_update_ha_state(True)
+    @asyncio.coroutine
+    def async_added_to_hass(self):
+        """Initialize data and register callbacks."""
 
-        # Update value when home assistant starts
-        hass.bus.listen_once(EVENT_HOMEASSISTANT_START, force_refresh)
+        # load data from recorder and do initial calculation
+        yield from self.async_get_history()
+        yield from self.async_update()
 
-        # Update value when tracked entity changes its state
-        track_state_change(hass, entity_id, force_refresh)
+        print("async_added_to_hass")
+
+        @callback
+        def state_listener(entity, old_state, new_state):
+            """Handle the sensor state changes."""
+            bisect.insort(self._history, new_state)
+            self._hass.async_add_job(self.async_update_ha_state(True))
+
+        @callback
+        def sensor_startup(event):
+            """Update on startup."""
+            print("sensor_startup")
+            async_track_state_change(
+                self._hass, self._entity_id, state_listener)
+            
+            self._hass.async_add_job(self.async_update_ha_state(True))
+
+        self._hass.bus.async_listen_once(
+            EVENT_HOMEASSISTANT_START, sensor_startup)
 
     @property
     def name(self):
@@ -117,16 +185,18 @@ class HistoryAverageSensor(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        return round(self.value, 2)
+        return self._state
 
     @property
     def unit_of_measurement(self):
-        """Return the unit the value is expressed in."""
+        """Return the unit the state is expressed in."""
         return self._unit_of_measurement
 
     @property
     def should_poll(self):
         """Return the polling state."""
+        # Since the values can/do change with time, not just with
+        # state changes, the sensor should poll
         return True
 
     @property
@@ -142,53 +212,48 @@ class HistoryAverageSensor(Entity):
         """Return the icon to use in the frontend, if any."""
         return ICON
 
-    def update(self):
-        """Get the latest data and updates the states."""
-        # Get previous values of start and end
-        p_start, p_end = self._period
+    @asyncio.coroutine
+    def async_update(self):
+        """Update the sensor's state."""
 
-        # Parse templates
-        self.update_period()
+        print("UPDATING....")
+
+        # clean up history & update period
+        yield from self.asysnc_trim_history()
         start, end = self._period
 
-        # Convert times to UTC
-        start = dt_util.as_utc(start)
-        end = dt_util.as_utc(end)
-        p_start = dt_util.as_utc(p_start)
-        p_end = dt_util.as_utc(p_end)
-        now = dt_util.utcnow()
+        print(" - period start: ", start)
+        print(" - period end  : ", end)
+
+        now = HistoryAverageHelper.utcnow()
 
         # Compute timestamps
         start_timestamp = dt_util.as_timestamp(start)
         end_timestamp = dt_util.as_timestamp(end)
-        p_start_timestamp = dt_util.as_timestamp(p_start)
-        p_end_timestamp = dt_util.as_timestamp(p_end)
         now_timestamp = dt_util.as_timestamp(now)
-
-        # Only update value every second, and when the period has changed
-        if math.floor(start_timestamp) == math.floor(p_start_timestamp) and \
-            math.floor(end_timestamp) == math.floor(p_end_timestamp) and \
-                math.floor(end_timestamp) <= math.floor(now_timestamp):
-            # Don't compute anything as the value cannot have changed by much
-            return
-
-        # Get history between start and end
-        history_list = history.state_changes_during_period(
-            self._hass, start, end, str(self._entity_id))
 
         last_state = None
         last_time = None
         total_elapsed = 0
         intervals = defaultdict(float)
 
-        # Calculations
-        for item in history_list.get(self._entity_id) or []:
+        # Sum time spent in each state
+        for item in self._history:
             current_state = item.state
             current_time = item.last_changed.timestamp()
 
-            # average only over valid states
+            print("item: ", item)
+
+            # don't include values not within the period
+            if(end_timestamp <= current_time):
+                break
+
+            # Average over valid states
             if last_state is not None:
+                if(last_time < start_timestamp):
+                    last_time = start_timestamp
                 elapsed = current_time - last_time
+                print("  - adding elapsed time: ", elapsed)
                 total_elapsed += elapsed
                 intervals[float(last_state)] += elapsed
 
@@ -197,19 +262,27 @@ class HistoryAverageSensor(Entity):
 
         # Count time elapsed between last history state and end of measure
         if last_state is not None:
+            print("end          : ", dt_util.utc_from_timestamp(end_timestamp))
+            print("now          : ", dt_util.utc_from_timestamp(now_timestamp))
+            print("last_time    : ", dt_util.utc_from_timestamp(last_time))
+            print("last_state   : ", last_state)
             measure_end = min(end_timestamp, now_timestamp)
             elapsed = measure_end - last_time
+            print("  - adding elapsed time: ", elapsed)
             total_elapsed += elapsed
             intervals[float(last_state)] += elapsed
 
         # Calculate the weighted average
-        value = 0 if intervals else None
+        updated_state = 0
         for state in intervals:
-            value += float(state) * (intervals[state] / total_elapsed)
+            print('interval: ' + str(state) + ' duration: ' + str(intervals[state]))
+            updated_state += float(state) * (intervals[state] / total_elapsed)
 
-        self.value = value
+        print("updated state: " + str(round(updated_state,2)))
+        self._state = round(updated_state, 2)
 
-    def update_period(self):
+    @asyncio.coroutine
+    def async_update_period(self):
         """Parse the templates and store a datetime tuple in _period."""
         start = None
         end = None
@@ -217,11 +290,12 @@ class HistoryAverageSensor(Entity):
         # Parse start
         if self._start is not None:
             try:
-                start_rendered = self._start.render()
+                start_rendered = self._start.async_render()
             except (TemplateError, TypeError) as ex:
                 HistoryAverageHelper.handle_template_exception(ex, 'start')
                 return
             start = dt_util.parse_datetime(start_rendered)
+
             if start is None:
                 try:
                     start = dt_util.utc_from_timestamp(float(start_rendered))
@@ -233,14 +307,13 @@ class HistoryAverageSensor(Entity):
         # Parse end
         if self._end is not None:
             try:
-                end_rendered = self._end.render()
+                end_rendered = self._end.async_render()
             except (TemplateError, TypeError) as ex:
                 HistoryAverageHelper.handle_template_exception(ex, 'end')
                 return
             end = dt_util.parse_datetime(end_rendered)
             if end is None:
                 try:
-                    print(float(end_rendered))
                     end = dt_util.utc_from_timestamp(float(end_rendered))
                 except ValueError:
                     _LOGGER.error("Parsing error: end must be a datetime "
@@ -253,11 +326,19 @@ class HistoryAverageSensor(Entity):
         if end is None:
             end = start + self._duration
 
-        self._period = start, end
+        # Convert times to UTC
+        start = dt_util.as_utc(start)
+        end = dt_util.as_utc(end)
 
+        self._period = start, end
 
 class HistoryAverageHelper:
     """Static methods to make the HistoryAverageSensor code lighter."""
+
+    @staticmethod
+    def utcnow():
+        """To test, enables patching now separately from dt_util"""
+        return dt_util.utcnow()
 
     @staticmethod
     def period_in_seconds(period):
--- tests/components/sensor/test_history_average.py
+++ tests/components/sensor/test_history_average.py
@@ -1,16 +1,17 @@
 """The test for the History Average sensor platform."""
+import asyncio
 from datetime import timedelta
 import unittest
 from unittest.mock import patch
 
-from homeassistant.components import recorder
+from homeassistant.components.recorder.const import DATA_INSTANCE
 from homeassistant.components.sensor.history_average\
      import HistoryAverageSensor
+import homeassistant.core as ha
 from homeassistant.helpers.template import Template
 from homeassistant.setup import setup_component
 import homeassistant.util.dt as dt_util
-from tests.common import init_recorder_component, get_test_home_assistant
-
+from tests.common import init_recorder_component, mock_state_change_event, get_test_home_assistant
 
 class TestHistoryAverageSensor(unittest.TestCase):
     """Test the History Average sensor."""
@@ -18,6 +19,10 @@ class TestHistoryAverageSensor(unittest.TestCase):
     def setUp(self):
         """Set up things to be run when tests are started."""
         self.hass = get_test_home_assistant()
+        init_recorder_component(self.hass)
+        # self.hass.config.components |= set(['history', 'recorder'])
+        self.hass.start()
+        self.wait_recording_done()
 
     def tearDown(self):
         """Stop everything that was started."""
@@ -25,14 +30,11 @@ class TestHistoryAverageSensor(unittest.TestCase):
 
     def init_recorder(self):
         """Initialize the recorder."""
-        init_recorder_component(self.hass)
-        self.hass.start()
-        self.wait_recording_done()
 
     def wait_recording_done(self):
         """Block till recording is done."""
         self.hass.block_till_done()
-        self.hass.data[recorder.DATA_INSTANCE].block_till_done()
+        self.hass.data[DATA_INSTANCE].block_till_done()
 
     def test_setup(self):
         """Test the history average sensor setup."""
@@ -65,9 +67,9 @@ class TestHistoryAverageSensor(unittest.TestCase):
         sensor2 = HistoryAverageSensor(
             self.hass, 'test', None, today, duration, 'Test', '')
 
-        sensor1.update_period()
+        yield from sensor1.async_update_period()
         sensor1_start, sensor1_end = sensor1._period
-        sensor2.update_period()
+        yield from sensor2.async_update_period()
         sensor2_start, sensor2_end = sensor2._period
 
         # Start = 00:00:00
@@ -90,78 +92,197 @@ class TestHistoryAverageSensor(unittest.TestCase):
         self.assertEqual(sensor2_end.minute, 0)
         self.assertEqual(sensor2_end.second, 0)
 
-    def _add_test_states(self, now):
+    def _add_test_states(self, entity_id, now):
         """Add multiple states to history for testing."""
-        self.init_recorder()
 
-        def set_state(entity_id, state, timestamp):
+        # TODO - look at test_template; I don't think this should be called
+        # here; I think it needs to be at the top of each test fn (or split up for hass start?), since
+        # the _add_test_states gets put in random places in each test fn
+        # self.init_recorder()
+
+        def set_state(entity_id, state, now, timestamp):
             """Set the state."""
-            with patch('homeassistant.components.recorder.dt_util.utcnow',
-                       return_value=timestamp):
-                self.hass.states.set(entity_id, state)
-                self.wait_recording_done()
+            with patch('homeassistant.components.sensor.history_average.HistoryAverageHelper.utcnow',
+                       return_value=now):
+                with patch('homeassistant.components.recorder.dt_util.utcnow',
+                        return_value=timestamp):
+                    print("set_state: " + str(state) + " @ ", timestamp)
+                    self.hass.states.set(entity_id, state)
+                    # state = ha.State(entity_id, state)
+                        #  state = ha.State(entity_id, state, attributes, last_changed,
+                        #  last_updated).as_dict()
+                    # mock_state_change_event(self.hass, state)
+                    self.wait_recording_done()
 
-        # Start     t0        t1        t2        End
+        # Start     t0        t1        t2        End (now)
         # |--20min--|--20min--|--10min--|--10min--|
         # |----?----|----1----|---10----|---100---|
 
         time0 = now - timedelta(minutes=40)
-        set_state('binary_sensor.test_id', 1, time0)
+        set_state(entity_id, 1, now, time0)
 
         time1 = now - timedelta(minutes=20)
-        set_state('binary_sensor.test_id', 10, time1)
+        set_state(entity_id, 10, now, time1)
 
         time2 = now - timedelta(minutes=10)
-        set_state('binary_sensor.test_id', 100, time2)
+        set_state(entity_id, 100, now, time2)
 
-    def test_measure(self):
-        """Test the history average sensor measurements."""
-        # TODO: removing the "+ timedelta" breaks the recorder, and I don't
-        #      know why - filing a bug in HA to track.
-        now = dt_util.utcnow() + timedelta(hours=1)
-        self._add_test_states(now)
+    def _setup_sensor(self, sensor, sensor_source, now, start_offset, end_offset):
+        """Setup sensor."""
         now_string = str(dt_util.as_timestamp(now))
+        start = '{{ ' + now_string + ' - ' + start_offset + ' }}'
+        end = '{{ ' + now_string + ' - ' + end_offset + ' }}'
 
-        # range: (t0 - 1 second) to End
-        start = Template('{{ ' + now_string + ' - 2401 }}', self.hass)
-        end = Template('{{ ' + now_string + ' }}', self.hass)
-        sensor1 = HistoryAverageSensor(
-            self.hass, 'binary_sensor.test_id', start, end, None, 'Test', '%')
+        with patch('homeassistant.components.sensor.history_average.HistoryAverageHelper.utcnow',
+            return_value=now):
+            assert setup_component(self.hass, 'sensor', {
+                'history': {
+                },
+                'sensor': {
+                    'platform': 'history_average',
+                    'name': sensor,
+                    'entity_id': sensor_source,
+                    'start': start,
+                    'end': end,
+                }
+            })
 
-        # range: full range, including unknown values at the start
-        start = Template('{{ ' + now_string + ' - 3600 }}', self.hass)
-        end = Template('{{ ' + now_string + ' }}', self.hass)
-        sensor2 = HistoryAverageSensor(
-            self.hass, 'binary_sensor.test_id', start, end, None, 'Test', '$')
+    def _get_sensor_state(self, sensor, now):
+        """Return current value of sensor"""
+        with patch('homeassistant.components.sensor.history_average.HistoryAverageHelper.utcnow',
+                    return_value=now):
+            with patch(
+                'homeassistant.components.sensor.history_average.dt_util.utcnow',
+                return_value=now):
+                state = self.hass.states.get('sensor.' + sensor)
+        return state
 
-        # range: (t1 - 1 second) to (t2 + 1 second)
-        start = Template('{{ ' + now_string + ' - 1201 }}', self.hass)
-        end = Template('{{ ' + now_string + ' - 599 }}', self.hass)
-        sensor3 = HistoryAverageSensor(
-            self.hass, 'binary_sensor.test_id', start, end, None, 'Test', '')
+    def test_history_loading(self):
+        """Test the loading of historical data on sensor startup."""
+        iteration = 1
+        while(iteration < 100):
+            print("Iteration: ", iteration)
+            iteration+=1
+            self.init_recorder()
 
-        # range: (t2 + 1 second) to End
-        start = Template('{{ ' + now_string + ' - 599 }}', self.hass)
-        end = Template('{{ ' + now_string + ' }}', self.hass)
-        sensor4 = HistoryAverageSensor(
-            self.hass, 'binary_sensor.test_id', start, end, None, 'Test', '')
+            now = dt_util.utcnow() + timedelta(hours=24)
+            sensor = 'sensor_history'
+            sensor_source = 'sensor.source_history'
 
-        with patch(
-             'homeassistant.components.sensor.history_average.dt_util.utcnow',
-             return_value=now):
-            sensor1.update()
-            sensor2.update()
-            sensor3.update()
-            sensor4.update()
+            self._add_test_states(sensor_source, now)
 
-        self.assertEqual(sensor1.state, 28)
-        self.assertEqual(sensor2.state, 28)
-        self.assertEqual(sensor3.state, 10.13)
-        self.assertEqual(sensor4.state, 100)
+            self._setup_sensor(sensor, sensor_source, now, '3000', '0')
+            state = self._get_sensor_state(sensor, now)
+            self.assertEqual(float(state.state), 28)
 
-        self.assertEqual(sensor1._unit_of_measurement, '%')
-        self.assertEqual(sensor2._unit_of_measurement, '$')
-        self.assertEqual(sensor3._unit_of_measurement, '')
+            # self.teardown_method(None)
+            # self.setup_method(None)
+
+    def test_history_loading_at_end(self):
+        """Test the loading of historical data, reading off end."""
+        self.init_recorder()
+
+        now = dt_util.utcnow() + timedelta(hours=24)
+        sensor = 'sensor_end'
+        sensor_source = 'sensor.source_end'
+
+        self._add_test_states(sensor_source, now)
+
+        self._setup_sensor(sensor, sensor_source, now, '1', '0')
+        state = self._get_sensor_state(sensor, now)
+        self.assertEqual(float(state.state), 100)
+
+    def test_state_changes(self):
+        """Test updates to source data on sensor."""
+        self.init_recorder()
+
+        now = dt_util.utcnow() + timedelta(hours=24)
+        sensor = 'sensor_state'
+        sensor_source = 'sensor.source_state'
+
+        self._setup_sensor(sensor, sensor_source, now, '3600', '0')
+        state = self._get_sensor_state(sensor, now)
+        self.assertEqual(float(state.state), 0)
+        
+        self._add_test_states(sensor_source, now)
+
+        state = self._get_sensor_state(sensor, now)
+        self.assertEqual(float(state.state), 28)
+
+    def test_range_1(self):
+        """Test range: (t0 - 1 second) to End"""
+        self.init_recorder()
+
+        now = dt_util.utcnow() + timedelta(hours=24)
+        sensor = 'sensor1'
+        sensor_source = 'sensor.source1'
+
+        self._add_test_states(sensor_source, now)
+
+        self._setup_sensor(sensor, sensor_source, now, '2401', '0')
+        state = self._get_sensor_state(sensor, now)
+        self.assertEqual(float(state.state), 28)
+
+    # TODO: add a version of this that's not updates, but history only?
+    def test_range_2(self):
+        """Test range: (t1 - 1 second) to (t2 + 1 second)"""
+        self.init_recorder()
+
+        now = dt_util.utcnow() + timedelta(hours=24)
+        sensor = 'sensor2'
+        sensor_source = 'sensor.source2'
+        self._setup_sensor(sensor, sensor_source, now, '1201', '599')
+
+        self._add_test_states(sensor_source, now)
+
+        state = self._get_sensor_state(sensor, now)
+        self.assertEqual(float(state.state), 10.13)
+
+    def test_range_3(self):
+        """Test range: (t2 + 1 second) to End"""
+        self.init_recorder()
+
+        now = dt_util.utcnow() + timedelta(hours=24)
+        sensor = 'sensor3'
+        sensor_source = 'sensor.source3'
+        self._setup_sensor(sensor, sensor_source, now, '599', '0')
+
+        self._add_test_states(sensor_source, now)
+
+        state = self._get_sensor_state(sensor, now)
+        self.assertEqual(float(state.state), 100)
+
+    def test_range_4_from_history(self):
+        """Test range: (t0 + 1 second) to End (loaded from history)"""
+        self.init_recorder()
+
+        now = dt_util.utcnow() + timedelta(hours=24)
+        sensor = 'sensor4_history'
+        sensor_source = 'sensor.source4_history'
+
+        self._add_test_states(sensor_source, now)
+
+        self._setup_sensor(sensor, sensor_source, now, '2399', '0')
+        state = self._get_sensor_state(sensor, now)
+        self.assertEqual(float(state.state), 28.01)
+
+    def test_range_4_from_updates(self):
+        """Test range: (t0 + 1 second) to End (async updates)"""
+        self.init_recorder()
+
+        now = dt_util.utcnow() + timedelta(hours=24)
+        sensor = 'sensor4_updates'
+        sensor_source = 'sensor.source4_updates'
+        self._setup_sensor(sensor, sensor_source, now, '2399', '0')
+
+        self._add_test_states(sensor_source, now)
+        state = self._get_sensor_state(sensor, now)
+        self.assertEqual(float(state.state), 28.01)
+
+
+        # self.assertEqual(sensor1._unit_of_measurement, '%')
+        # self.assertEqual(sensor2._unit_of_measurement, '$')
+        # self.assertEqual(sensor3._unit_of_measurement, '')
 
     def test_wrong_date(self):
         """Test when start or end value is not a timestamp or a date."""
@@ -176,8 +297,8 @@ class TestHistoryAverageSensor(unittest.TestCase):
         before_update1 = sensor1._period
         before_update2 = sensor2._period
 
-        sensor1.update_period()
-        sensor2.update_period()
+        yield from sensor1.async_update_period()
+        yield from sensor2.async_update_period()
 
         self.assertEqual(before_update1, sensor1._period)
         self.assertEqual(before_update2, sensor2._period)
@@ -189,7 +310,7 @@ class TestHistoryAverageSensor(unittest.TestCase):
             },
             'sensor': {
                 'platform': 'history_average',
-                'entity_id': 'binary_sensor.test_id',
+                'entity_id': 'sensor.source',
                 'name': 'Test',
                 'start': '{{ now() }}',
                 'duration': 'TEST',
@@ -214,8 +335,8 @@ class TestHistoryAverageSensor(unittest.TestCase):
         before_update1 = sensor1._period
         before_update2 = sensor2._period
 
-        sensor1.update_period()
-        sensor2.update_period()
+        yield from sensor1.async_update_period()
+        yield from sensor2.async_update_period()
 
         self.assertEqual(before_update1, sensor1._period)
         self.assertEqual(before_update2, sensor2._period)
@@ -227,7 +348,7 @@ class TestHistoryAverageSensor(unittest.TestCase):
             },
             'sensor': {
                 'platform': 'history_average',
-                'entity_id': 'binary_sensor.test_id',
+                'entity_id': 'sensor.source',
                 'name': 'Test',
                 'start': '{{ now() }}',
             }
@@ -245,7 +366,7 @@ class TestHistoryAverageSensor(unittest.TestCase):
             },
             'sensor': {
                 'platform': 'history_average',
-                'entity_id': 'binary_sensor.test_id',
+                'entity_id': 'sensor.source',
                 'name': 'Test',
                 'start': '{{ as_timestamp(now()) - 3600 }}',
                 'end': '{{ now() }}',
