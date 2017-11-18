ac0cd59c1a8082849aaac6412677aca6398cb2c9
Robert Svensson <kane610@me.com>
2017-11-18 21:25:31 +0100

    Bump requirements to v14

--- homeassistant/components/deconz.py
+++ homeassistant/components/deconz.py
@@ -17,7 +17,7 @@ from homeassistant.helpers import discovery
 from homeassistant.util import slugify
 from homeassistant.util.json import load_json, save_json
 
-REQUIREMENTS = ['pydeconz==13']
+REQUIREMENTS = ['pydeconz==14']
 
 _LOGGER = logging.getLogger(__name__)
 
