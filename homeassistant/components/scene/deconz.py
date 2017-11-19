85ed7d6b839c46e85bd17f74921060468a7d5d70
Robert Svensson <kane610@me.com>
2017-11-19 19:26:19 +0100

    Fix review comments

--- homeassistant/components/scene/deconz.py
+++ homeassistant/components/scene/deconz.py
@@ -15,37 +15,33 @@ DEPENDENCIES = [DOMAIN]
 
 _LOGGER = logging.getLogger(__name__)
 
+
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up scenes for the deCONZ platform."""
-    if DECONZ_DATA in hass.data:
-        scenes = hass.data[DECONZ_DATA].scenes
-    print('deconz scenes', scenes)
+    scenes = hass.data[DECONZ_DATA].scenes
     for _, scene in scenes.items():
         async_add_devices([DeconzScene(scene)])
-    print('scene set up done')
 
 
 class DeconzScene(Scene):
-    """Representation of a single LiteJet scene."""
+    """Representation of a single deCONZ scene."""
 
     def __init__(self, scene):
-        """Setup sensor and add update callback to get data from websocket."""
+        """Setup scene."""
         self._scene = scene
-
-    @property
-    def name(self):
-        """Return the name of the scene."""
-        return self._scene._group_name + '_' + self._scene.name
-        # return self._name
-
-    @property
-    def should_poll(self):
-        """Return that polling is not necessary."""
-        return False
 
     @asyncio.coroutine
     def async_activate(self, **kwargs):
         """Activate the scene."""
-        print('scene active kwargs', kwargs)
         yield from self._scene.set_state({})
+
+    @property
+    def name(self):
+        """Return the name of the scene."""
+        return self._scene._group_name + ' ' + self._scene.name
+
+    @property
+    def should_poll(self):
+        """No polling needed."""
+        return False
