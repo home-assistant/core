da0a0b949deb3e8443ba3f2a21c2ac3cf5eddc92
anoras <anders.noras@andersnoras.com>
2017-11-23 12:55:32 +0100

    Support for generic receivers by delegating actions to user configured scripts.

--- homeassistant/components/media_player/delegate_media_player.py
+++ homeassistant/components/media_player/delegate_media_player.py
@@ -1,5 +1,5 @@
 """
-Support for generic receivers by delegating actions to user configured scripts.
+Support for generic receivers by delegating actions to user configured scripts. 
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/media_player.delegate_media_player/
