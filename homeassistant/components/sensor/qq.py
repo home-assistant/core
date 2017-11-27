53eea134184cc7901b5cb83dc0ad9b62b11ad303
Mylife4air <1071484952@qq.com>
2017-11-27 22:06:01 +0800

    Update qq.py

--- homeassistant/components/sensor/qq.py
+++ homeassistant/components/sensor/qq.py
@@ -6,7 +6,6 @@ import threading
 import os
 
 DOMAIN = 'qq'
-# 配置文件的样式
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
@@ -20,24 +19,24 @@ REQUIREMENTS = ['qqbot==2.3.7']
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up the Demo sensors."""
-    o = DemoSensor()
+    """Set up the Qqsensors."""
+    o = Qqsensor()
     add_devices([o])
     thread1 = QQ(config['qq'])
     thread1.start()
 
 
-class DemoSensor(Entity):
-    """Representation of a Demo sensor."""
+class Qqsensor(Entity):
+    """Representation of a Qqsensor."""
     def __init__(self):
         """Initialize the sensor."""
-        self._state = '未知'
+        self._state = 'NULL'
         self._name = DOMAIN
         self._mutex = threading.Lock()
 
     @property
     def should_poll(self):
-        """No polling needed for a demo sensor."""
+        """need polling"""
         return True
 
     @property
@@ -47,10 +46,11 @@ class DemoSensor(Entity):
 
     @property
     def state(self):
-        """Return the state of the sensor."""
+        """Return the message of the sensor."""
         return self._name
 
     def update(self):
+        """get message from file"""
         path = os.path.expanduser('~') + '/.homeassistant'
         path += '/msg.txt'
         with open(path, 'r') as fs:
