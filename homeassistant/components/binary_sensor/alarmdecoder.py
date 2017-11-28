3d04a3176d721f20eecaa277ba23f1421d734ded
PhracturedBlue <rc2012@pblue.org>
2017-11-28 10:36:36 -0800

    Store raw state of RF sensors from alarmdecoder

--- homeassistant/components/binary_sensor/alarmdecoder.py
+++ homeassistant/components/binary_sensor/alarmdecoder.py
@@ -8,13 +8,20 @@ import asyncio
 import logging
 
 from homeassistant.components.binary_sensor import BinarySensorDevice
-from homeassistant.components.alarmdecoder import (
-    ZONE_SCHEMA, CONF_ZONES, CONF_ZONE_NAME, CONF_ZONE_TYPE,
-    SIGNAL_ZONE_FAULT, SIGNAL_ZONE_RESTORE)
+from homeassistant.components.alarmdecoder import (ZONE_SCHEMA,
+                                                   CONF_ZONES,
+                                                   CONF_ZONE_NAME,
+                                                   CONF_ZONE_TYPE,
+                                                   CONF_ZONE_RFID,
+                                                   SIGNAL_RFX_MESSAGE,
+                                                   SIGNAL_ZONE_FAULT,
+                                                   SIGNAL_ZONE_RESTORE)
 
 DEPENDENCIES = ['alarmdecoder']
 
 _LOGGER = logging.getLogger(__name__)
+
+ATTR_RF_STATE = 'rfstate'
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -26,7 +33,9 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         device_config_data = ZONE_SCHEMA(configured_zones[zone_num])
         zone_type = device_config_data[CONF_ZONE_TYPE]
         zone_name = device_config_data[CONF_ZONE_NAME]
-        device = AlarmDecoderBinarySensor(zone_num, zone_name, zone_type)
+        zone_rfid = device_config_data[CONF_ZONE_RFID]
+        device = AlarmDecoderBinarySensor(
+            zone_num, zone_name, zone_type, zone_rfid)
         devices.append(device)
 
     add_devices(devices)
@@ -37,13 +46,15 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class AlarmDecoderBinarySensor(BinarySensorDevice):
     """Representation of an AlarmDecoder binary sensor."""
 
-    def __init__(self, zone_number, zone_name, zone_type):
+    def __init__(self, zone_number, zone_name, zone_type, zone_rfid):
         """Initialize the binary_sensor."""
         self._zone_number = zone_number
         self._zone_type = zone_type
         self._state = 0
         self._name = zone_name
         self._type = zone_type
+        self._rfid = zone_rfid
+        self._rfstate = 0
 
     @asyncio.coroutine
     def async_added_to_hass(self):
@@ -53,6 +64,9 @@ class AlarmDecoderBinarySensor(BinarySensorDevice):
 
         self.hass.helpers.dispatcher.async_dispatcher_connect(
             SIGNAL_ZONE_RESTORE, self._restore_callback)
+
+        self.hass.helpers.dispatcher.async_dispatcher_connect(
+            self.hass, SIGNAL_RFX_MESSAGE, self._rfx_message_callback)
 
     @property
     def name(self):
@@ -76,6 +90,14 @@ class AlarmDecoderBinarySensor(BinarySensorDevice):
         return False
 
     @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        attr = {}
+        if self._rfid:
+            attr[ATTR_RF_STATE] = self._rfstate
+        return attr
+
+    @property
     def is_on(self):
         """Return true if sensor is on."""
         return self._state == 1
@@ -95,4 +117,10 @@ class AlarmDecoderBinarySensor(BinarySensorDevice):
         """Update the zone's state, if needed."""
         if zone is None or int(zone) == self._zone_number:
             self._state = 0
+            self.schedule_update_ha_state()
+
+    def _rfx_message_callback(self, message):
+        """Update RF state."""
+        if self._rfid and message and message.serial_number == self._rfid:
+            self._rfstate = message.value
             self.schedule_update_ha_state()
