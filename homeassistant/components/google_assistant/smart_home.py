029e574e746d5aba277b284c7bffe4be7601cd91
Zach Brown <git@prozach.org>
2017-11-27 19:26:50 -0500

    Change group to switch

--- homeassistant/components/google_assistant/smart_home.py
+++ homeassistant/components/google_assistant/smart_home.py
@@ -39,7 +39,7 @@ _LOGGER = logging.getLogger(__name__)
 # Mapping is [actions schema, primary trait, optional features]
 # optional is SUPPORT_* = (trait, command)
 MAPPING_COMPONENT = {
-    group.DOMAIN: [TYPE_LIGHT, TRAIT_ONOFF, None],
+    group.DOMAIN: [TYPE_SWITCH, TRAIT_ONOFF, None],
     scene.DOMAIN: [TYPE_SCENE, TRAIT_SCENE, None],
     script.DOMAIN: [TYPE_SCENE, TRAIT_SCENE, None],
     switch.DOMAIN: [TYPE_SWITCH, TRAIT_ONOFF, None],
