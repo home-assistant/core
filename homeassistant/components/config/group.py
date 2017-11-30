52fb6149dc4cd98cdaebec92c26d70cef84b7c3d
andrey-git <andrey-git@users.noreply.github.com>
2017-11-30 21:16:00 +0200

    Reload groups after saving a change via config API

--- homeassistant/components/config/group.py
+++ homeassistant/components/config/group.py
@@ -1,8 +1,8 @@
 """Provide configuration end points for Groups."""
 import asyncio
-
+from homeassistant.const import SERVICE_RELOAD
 from homeassistant.components.config import EditKeyBasedConfigView
-from homeassistant.components.group import GROUP_SCHEMA
+from homeassistant.components.group import DOMAIN, GROUP_SCHEMA
 import homeassistant.helpers.config_validation as cv
 
 
@@ -12,7 +12,13 @@ CONFIG_PATH = 'groups.yaml'
 @asyncio.coroutine
 def async_setup(hass):
     """Set up the Group config API."""
+    @asyncio.coroutine
+    def hook(hass):
+        """post_write_hook for Config View that reloads groups."""
+        yield from hass.services.async_call(DOMAIN, SERVICE_RELOAD)
+
     hass.http.register_view(EditKeyBasedConfigView(
-        'group', 'config', CONFIG_PATH, cv.slug, GROUP_SCHEMA
+        'group', 'config', CONFIG_PATH, cv.slug, GROUP_SCHEMA,
+        post_write_hook=hook
     ))
     return True
