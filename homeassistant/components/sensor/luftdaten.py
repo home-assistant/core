53208687796524030a295fcfae982e17411ebbde
Fabian Affolter <fabian@affolter-engineering.ch>
2017-12-06 10:08:16 +0100

    Refactoring

--- homeassistant/components/sensor/luftdaten.py
+++ homeassistant/components/sensor/luftdaten.py
@@ -60,19 +60,26 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the Luftdaten sensor."""
+    from luftdaten import Luftdaten
+
     name = config.get(CONF_NAME)
     sensor_id = config.get(CONF_SENSORID)
 
-    data = LuftdatenData(hass, sensor_id)
+    session = async_get_clientsession(hass)
+    luftdaten = LuftdatenData(Luftdaten(sensor_id, hass.loop, session))
 
-    yield from data.async_get()
+    yield from luftdaten.async_get()
+
+    if luftdaten.data is None:
+        _LOGGER.error("Sensor is not available: %s", sensor_id)
+        return
 
     devices = []
     for variable in config[CONF_MONITORED_CONDITIONS]:
-        if data.luftdaten.values[variable] is None:
+        if luftdaten.data.values[variable] is None:
             _LOGGER.warning("It might be that sensor %s is not providing "
                             "measurements for %s", sensor_id, variable)
-        devices.append(LuftdatenSensor(data, name, variable, sensor_id))
+        devices.append(LuftdatenSensor(luftdaten, name, variable, sensor_id))
 
     async_add_devices(devices)
 
@@ -80,9 +87,9 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
 class LuftdatenSensor(Entity):
     """Implementation of a Luftdaten sensor."""
 
-    def __init__(self, data, name, sensor_type, sensor_id):
+    def __init__(self, luftdaten, name, sensor_type, sensor_id):
         """Initialize the Luftdaten sensor."""
-        self.data = data
+        self.luftdaten = luftdaten
         self._name = name
         self._state = None
         self._sensor_id = sensor_id
@@ -97,7 +104,10 @@ class LuftdatenSensor(Entity):
     @property
     def state(self):
         """Return the state of the device."""
-        return self.data.luftdaten.values[self.sensor_type]
+        try:
+            return self.luftdaten.data.values[self.sensor_type]
+        except AttributeError:
+            pass
 
     @property
     def unit_of_measurement(self):
@@ -107,14 +117,14 @@ class LuftdatenSensor(Entity):
     @property
     def device_state_attributes(self):
         """Return the state attributes."""
-        if self.data is None:
+        if self.luftdaten is None:
             return
 
         attr = {
             ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
             ATTR_SENSOR_ID: self._sensor_id,
-            'lat': self.data.luftdaten.meta['latitude'],
-            'long': self.data.luftdaten.meta['longitude'],
+            'lat': self.luftdaten.data.meta['latitude'],
+            'long': self.luftdaten.data.meta['longitude'],
         }
         return attr
 
@@ -122,7 +132,7 @@ class LuftdatenSensor(Entity):
     def async_update(self):
         """Get the latest data from luftdaten.info and update the state."""
         try:
-            yield from self.data.async_get()
+            yield from self.luftdaten.async_get()
         except TypeError:
             pass
 
@@ -130,14 +140,9 @@ class LuftdatenSensor(Entity):
 class LuftdatenData(object):
     """Class for handling the data retrieval."""
 
-    def __init__(self, hass, sensor_id):
+    def __init__(self, data):
         """Initialize the data object."""
-        from luftdaten import Luftdaten
-
-        self.hass = hass
-        self._websession = async_get_clientsession(self.hass)
-        self.luftdaten = Luftdaten(
-            sensor_id, self.hass.loop, self._websession)
+        self.data = data
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     @asyncio.coroutine
@@ -146,7 +151,7 @@ class LuftdatenData(object):
         from luftdaten.exceptions import LuftdatenError
 
         try:
-            yield from self.luftdaten.async_get_data()
+            yield from self.data.async_get_data()
         except LuftdatenError:
             _LOGGER.error("Unable to retrieve data from luftdaten.info")
-            self.luftdaten = None
+            self.data = None
