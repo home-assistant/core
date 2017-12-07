db9f98b31d820beb603f9caf654034d19f78700e
Derek Brooks <derek@broox.com>
2017-12-06 22:34:13 -0600

    remove nuheat away functionality. :(

--- homeassistant/components/nuheat.py
+++ homeassistant/components/nuheat.py
@@ -20,15 +20,12 @@ DATA_NUHEAT = "nuheat"
 
 DOMAIN = "nuheat"
 
-CONF_MIN_AWAY_TEMP = 'min_away_temp'
-
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_USERNAME): cv.string,
         vol.Required(CONF_PASSWORD): cv.string,
         vol.Required(CONF_DEVICES, default=[]):
             vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_MIN_AWAY_TEMP): cv.string,
     }),
 }, extra=vol.ALLOW_EXTRA)
 
@@ -42,20 +39,9 @@ def setup(hass, config):
     password = conf.get(CONF_PASSWORD)
     devices = conf.get(CONF_DEVICES)
 
-    min_away_temp = None
-    _min_away_temp = conf.get(CONF_MIN_AWAY_TEMP)
-    if _min_away_temp:
-        try:
-            min_away_temp = int(_min_away_temp)
-        except ValueError:
-            _LOGGER.error(
-                "Configuration error. %s.%s=%s is invalid. Please provide a "
-                "numeric value.", DATA_NUHEAT, CONF_MIN_AWAY_TEMP,
-                _min_away_temp)
-
     api = nuheat.NuHeat(username, password)
     api.authenticate()
-    hass.data[DATA_NUHEAT] = (api, devices, min_away_temp)
+    hass.data[DATA_NUHEAT] = (api, devices)
 
     discovery.load_platform(hass, "climate", DOMAIN, {}, config)
     return True
