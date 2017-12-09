2543a0d7b590f278b448449e79fedd9b7aba85f3
Aksel Bondoe <aksel.bondoe@gmail.com>
2017-12-09 21:55:18 +0100

    Add target temperature

--- homeassistant/components/climate/touchline.py
+++ homeassistant/components/climate/touchline.py
@@ -15,10 +15,10 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components.climate import ClimateDevice, PLATFORM_SCHEMA
-from homeassistant.const import CONF_HOST, TEMP_CELSIUS
+from homeassistant.const import CONF_HOST, TEMP_CELSIUS, ATTR_TEMPERATURE
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pytouchline==0.5']
+REQUIREMENTS = ['pytouchline==0.6']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -48,12 +48,14 @@ class Touchline(ClimateDevice):
         self.unit.update()
         self._name = self.unit.get_name()
         self._current_temperature = self.unit.get_current_temperature()
+        self._target_temperature = self.unit.get_target_temperature()
 
     def update(self):
         """Update unit attributes."""
         self.unit.update()
         self._name = self.unit.get_name()
         self._current_temperature = self.unit.get_current_temperature()
+        self._target_temperature = self.unit.get_target_temperature()
 
     @property
     def should_poll(self):
@@ -74,3 +76,14 @@ class Touchline(ClimateDevice):
     def current_temperature(self):
         """Return the current temperature."""
         return self._current_temperature
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return self._target_temperature
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        if kwargs.get(ATTR_TEMPERATURE) is not None:
+            self._target_temperature = kwargs.get(ATTR_TEMPERATURE)
+        self.unit.set_target_temperature(self._target_temperature)
