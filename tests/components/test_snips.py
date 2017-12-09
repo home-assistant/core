b3affaf49aba0e9ccb25239755ec93a7e145fbe0
tschmidty69 <tschmidty@yahoo.com>
2017-12-09 12:52:45 -0500

    Stop trying to one line the json assertion

--- tests/components/test_snips.py
+++ tests/components/test_snips.py
@@ -41,8 +41,16 @@ def test_snips_call_action(hass, mqtt_mock):
     intent = intents[0]
     assert intent.platform == 'snips'
     assert intent.intent_type == 'Lights'
-    assert intent.slots == {'slots': {'light_color': {'slotName':
-                            'light_color', 'value': {'kind': 'Custom',
-                                'value': 'green'}}}, 'light_color':
-                                    {'value': 'green'}}
+    assert intent.slots == {
+        'slots': {
+            'light_color': {
+                'slotName': 'light_color',
+                'value': {
+                    'kind': 'Custom',
+                    'value': 'green'
+                }
+            }
+        },
+            'light_color': {'value': 'green'}
+    }
     assert intent.text_input == 'turn the lights green'
