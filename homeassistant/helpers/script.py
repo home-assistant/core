7e246af0dac1b06bacf1d19c51f3e3647d86ad5b
tschmidty69 <tschmidty@yahoo.com>
2017-12-14 08:35:07 -0500

    Removed stray logger statement

--- homeassistant/helpers/script.py
+++ homeassistant/helpers/script.py
@@ -192,7 +192,6 @@ class Script():
             try:
                 event_data.update(template.render_complex(
                     action[CONF_EVENT_DATA_TEMPLATE], variables))
-                _LOGGER.error('event data: %s' % event_data)
             except TemplateError as ex:
                 _LOGGER.error('Error rendering event data template: %s', ex)
 
