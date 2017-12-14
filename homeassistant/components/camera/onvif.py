8e286068070ab9e6299d7422ce58ad6e2696a5f6
matt <matthilton2005@gmail.com>
2017-12-14 21:26:12 +0000

    removed unnecessary profile token

--- homeassistant/components/camera/onvif.py
+++ homeassistant/components/camera/onvif.py
@@ -66,11 +66,10 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
                 config.get(CONF_HOST), config.get(CONF_PORT),
                 config.get(CONF_USERNAME), config.get(CONF_PASSWORD)
             ).create_media_service()
-            profiletoken = (media_service.GetProfiles())[0]._token
             stream_uri = media_service.GetStreamUri(
                 {'StreamSetup': {
                     'Stream': 'RTP-Unicast', 'Transport': 'RTSP'
-                    }, 'ProfileToken': profiletoken}
+                    }}
                 )
             self._input = stream_uri.Uri.replace(
                 'rtsp://', 'rtsp://{}:{}@'.format(
