15393094fba284bff0ee90eb0f7375d20d28567a
Fabian Affolter <mail@fabian-affolter.ch>
2017-12-18 18:20:52 +0100

    Move client setup to setup()

--- homeassistant/components/sensor/discogs.py
+++ homeassistant/components/sensor/discogs.py
@@ -1,62 +1,72 @@
 """
-Shows the amount of records in a user's Discogs collection.
+Show the amount of records in a user's Discogs collection.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.discogs/
 """
 import asyncio
-import logging
 from datetime import timedelta
+import logging
 
 import voluptuous as vol
 
-import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (CONF_TOKEN)
-from homeassistant.helpers.entity import Entity
+from homeassistant.const import ATTR_ATTRIBUTION, CONF_NAME, CONF_TOKEN
 from homeassistant.helpers.aiohttp_client import SERVER_SOFTWARE
-
-_LOGGER = logging.getLogger(__name__)
-
-ICON = 'mdi:album'
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_TOKEN): cv.string,
-})
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
 
 REQUIREMENTS = ['discogs_client==2.2.1']
 
+_LOGGER = logging.getLogger(__name__)
+
+ATTR_IDENTITY = 'identity'
+
+CONF_ATTRIBUTION = "Data provided by Discogs"
+
+DEFAULT_NAME = 'Discogs'
+
+ICON = 'mdi:album'
+
 SCAN_INTERVAL = timedelta(hours=2)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_TOKEN): cv.string,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+})
 
 
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
-    """Set up the discogs sensor."""
+    """Set up the Discogs sensor."""
+    import discogs_client
+
+    name = config.get(CONF_NAME)
     token = config.get(CONF_TOKEN)
 
-    async_add_devices([DiscogsCollection(token)], True)
-    return True
+    try:
+        discogs = discogs_client.Client(SERVER_SOFTWARE, user_token=token)
+        identity = discogs.identity()
+    except discogs_client.exceptions.HTTPError:
+        _LOGGER.error("API token is not valid")
+        return
+
+    async_add_devices([DiscogsSensor(identity, name)], True)
 
 
-class DiscogsCollection(Entity):
+class DiscogsSensor(Entity):
     """Get a user's number of records in collection."""
 
-    def __init__(self, token):
-        """Initialize the sensor."""
-        import discogs_client
-        discogs = discogs_client.Client(SERVER_SOFTWARE, user_token=token)
-
-        self._ds_user = discogs.identity()
+    def __init__(self, identity, name):
+        """Initialize the Discogs sensor."""
+        self._identity = identity
+        self._name = name
         self._state = None
-
-        # Don't setup platform if token is invalid
-        if self._ds_user is None:
-            return False
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Discogs'
+        return self._name
 
     @property
     def state(self):
@@ -73,7 +83,15 @@ class DiscogsCollection(Entity):
         """Return the unit this state is expressed in."""
         return 'records'
 
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the sensor."""
+        return {
+            ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
+            ATTR_IDENTITY: self._identity.name,
+        }
+
     @asyncio.coroutine
     def async_update(self):
         """Set state to the amount of records in user's collection."""
-        self._state = self._ds_user.num_collection
+        self._state = self._identity.num_collection
