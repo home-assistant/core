2b5e0821a5bb89f334ab9ad552e4ca5e38562fb3
Greg Laabs <OverloadUT@gmail.com>
2017-12-20 12:43:34 -0800

    Fix detection of if a negative node is in use
    Fix a problem where every negative node gets detected as in-use. Code was not checking the correct property.

--- homeassistant/components/binary_sensor/isy994.py
+++ homeassistant/components/binary_sensor/isy994.py
@@ -165,7 +165,7 @@ class ISYBinarySensorDevice(isy.ISYDevice, BinarySensorDevice):
         """
         self._negative_node = child
 
-        if not _is_val_unknown(self._negative_node):
+        if not _is_val_unknown(self._negative_node.status._val):
             # If the negative node has a value, it means the negative node is
             # in use for this device. Therefore, we cannot determine the state
             # of the sensor until we receive our first ON event.
