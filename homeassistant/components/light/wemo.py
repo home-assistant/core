adac4fd2de8431d58f18e53a65efe7b82af15bc0
Martin Hjelmare <marhje52@kth.se>
2017-12-21 12:25:26 +0100

    Move subscription to async_added_to_hass
    * Move subscription.
    * Clean up.

--- homeassistant/components/light/wemo.py
+++ homeassistant/components/light/wemo.py
@@ -4,6 +4,7 @@ Support for Belkin WeMo lights.
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/light.wemo/
 """
+import asyncio
 import logging
 from datetime import timedelta
 
@@ -25,12 +26,7 @@ _LOGGER = logging.getLogger(__name__)
 SUPPORT_WEMO = (SUPPORT_BRIGHTNESS | SUPPORT_COLOR_TEMP | SUPPORT_RGB_COLOR |
                 SUPPORT_TRANSITION | SUPPORT_XY_COLOR)
 
-WEMO_ON = 1
-WEMO_OFF = 0
-WEMO_STANDBY = 8
 
-
-# pylint: disable=unused-argument, too-many-function-args
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up discovered WeMo switches."""
     import pywemo.discovery as discovery
@@ -158,21 +154,21 @@ class WemoDimmer(Light):
         self.wemo = device
         self._brightness = None
         self._state = None
-        # look up model name once as it incurs network traffic
-        self._model_name = self.wemo.model_name
 
+    @asyncio.coroutine
+    def async_added_to_hass(self):
+        """Register update callback."""
         wemo = get_component('wemo')
-        wemo.SUBSCRIPTION_REGISTRY.register(self.wemo)
+        # The register method uses a threading condition, so call via executor.
+        self.hass.async_add_job(wemo.SUBSCRIPTION_REGISTRY.register, self.wemo)
+        # The on method just appends to a defaultdict list.
         wemo.SUBSCRIPTION_REGISTRY.on(self.wemo, None, self._update_callback)
 
     def _update_callback(self, _device, _type, _params):
         """Update the state by the Wemo device."""
-        _LOGGER.info("Subscription update for  %s", _device)
+        _LOGGER.debug("Subscription update for  %s", _device)
         updated = self.wemo.subscription_update(_type, _params)
         self._update(force_update=(not updated))
-
-        if not hasattr(self, 'hass'):
-            return
         self.schedule_update_ha_state()
 
     @property
@@ -198,8 +194,7 @@ class WemoDimmer(Light):
     @property
     def brightness(self):
         """Return the brightness of this light between 1 and 100."""
-        wemobrightness = int(self.wemo.get_brightness())
-        return int((wemobrightness * 255) / 100)
+        return self._brightness
 
     @property
     def is_on(self):
@@ -218,7 +213,6 @@ class WemoDimmer(Light):
 
     def turn_on(self, **kwargs):
         """Turn the dimmer on."""
-        self._state = WEMO_ON
         self.wemo.on()
 
         # Wemo dimmer switches use a range of [0, 100] to control
@@ -232,5 +226,4 @@ class WemoDimmer(Light):
 
     def turn_off(self, **kwargs):
         """Turn the dimmer off."""
-        self._state = WEMO_OFF
         self.wemo.off()
