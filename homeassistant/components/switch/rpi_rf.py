155255108729d932feac4e1deacfb8751ab11dda
Ulrich Dobramysl <uli-do@gmx.at>
2017-12-23 22:48:50 +0000

    Address hound complaints about code style

--- homeassistant/components/switch/rpi_rf.py
+++ homeassistant/components/switch/rpi_rf.py
@@ -43,29 +43,31 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_SWITCHES): vol.Schema({cv.string: SWITCH_SCHEMA}),
 })
 
+
 class RFDeviceWrapper:
     def __init__(self, gpio):
         import rpi_rf
         import queue
         import threading
-        
+
         self._rfdevice = rpi_rf.RFDevice(gpio)
         self._tx_queue = queue.Queue()
+
         def txsender():
             while True:
-                code_list, protocol, pulselength, repetitions = self._tx_queue.get()
-                self._rfdevice._tx_repeat = repetitions
-                for code in code_list:
+                codes, protocol, pulselength, repeats = self._tx_queue.get()
+                self._rfdevice._tx_repeat = repeats
+                for code in codes:
                     self._rfdevice.tx_code(code, protocol, pulselength)
         self._tx_sender = threading.Thread(target=txsender, daemon=True)
         self._tx_sender.start()
-        
+
     def send_code(self, code_list, protocol, pulselength, repetitions):
         self._tx_queue.put((code_list, protocol, pulselength, repetitions))
-            
+
     def enable_tx(self):
         self._rfdevice.enable_tx()
-        
+
     def cleanup(self):
         self._rfdevice.cleanup()
 
@@ -134,7 +136,8 @@ class RPiRFSwitch(SwitchDevice):
     def _send_code(self, code_list, protocol, pulselength):
         """Send the code(s) with a specified pulselength."""
         _LOGGER.info("Sending code(s): %s", code_list)
-        self._rfdevice.send_code(code_list, protocol, pulselength, self._repetitions)
+        self._rfdevice.send_code(code_list, protocol, pulselength,
+                                 self._repetitions)
         return True
 
     def turn_on(self):
