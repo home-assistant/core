c60d4caa8cbb2fc6b1e087fae7e6228803d4deb8
Felix Konstantin Maurer <maufl@maufl.de>
2018-01-03 20:11:51 +0100

    Start adding support for MAX! devices via a CUL

---
+++ homeassistant/components/climate/maxcul.py
@@ -0,0 +1,127 @@
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+from homeassistant.components.climate import (
+    ClimateDevice, STATE_AUTO, STATE_UNKNOWN, SUPPORT_TARGET_TEMPERATURE,
+    SUPPORT_OPERATION_MODE)
+from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE
+
+from homeassistant.components.maxcul import DATA_MAXCUL, DATA_DEVICES, EVENT_THERMOSTAT_UPDATE
+
+REQUIREMENTS = ['pymaxcul']
+DEPENDS = ['maxcul']
+
+DEFAULT_TEMPERATURE = 12
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    thermostat_id = discovery_info.get('device_id', None)
+    if thermostat_id is None:
+        return
+
+    device = MaxCulClimate( hass, thermostat_id )
+    add_devices([device])
+
+
+
+class MaxCulClimate(ClimateDevice):
+    """MAX! CUL climate device"""
+
+    def __init__(self, hass, thermostat_id, current_temperature = None, target_temperature = None, mode = None, battery_low = None):
+        self.entity_id = "climate.maxcul_thermostat_{:x}".format(thermostat_id)
+        self._name = "Thermostat {:x}".format(thermostat_id)
+        self._thermostat_id = thermostat_id
+        self._maxcul_handle = hass.data[DATA_MAXCUL]
+        self._current_temperature = current_temperature
+        self._target_temperature = target_temperature
+        self._mode = mode
+        self._battery_low = battery_low
+
+        def update(event):
+            thermostat_id = event.data.get('device_id')
+            if thermostat_id != self._thermostat_id:
+                return
+
+            current_temperature = event.data.get('measured_temperature')
+            target_temperature = event.data.get('desired_temperature')
+            mode = event.data.get('mode')
+            battery_low = event.data.get('battery_low')
+
+            if current_temperature is not None:
+                self._current_temperature = current_temperature
+            if target_temperature is not None:
+                self._target_temperature = target_temperature
+            if mode is not None:
+                self._mode = mode
+            if battery_low is not None:
+                self._battery_low = battery_low
+
+            self.async_schedule_update_ha_state()
+
+        hass.bus.listen(EVENT_THERMOSTAT_UPDATE, update)
+
+        self._maxcul_handle.wakeup(self._thermostat_id)
+
+    @property
+    def supported_features(self):
+        return SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE
+
+    @property
+    def should_poll(self):
+        return False
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def max_temp(self):
+        from maxcul import MAX_TEMPERATURE
+        return MAX_TEMPERATURE
+
+    @property
+    def min_temp(self):
+        from maxcul import MIN_TEMPERATURE
+        return MIN_TEMPERATURE
+
+    @property
+    def temperature_unit(self):
+        return TEMP_CELSIUS
+
+    @property
+    def current_temperature(self):
+        return self._current_temperature
+
+    @property
+    def target_temperature(self):
+        return self._target_temperature
+
+    @property
+    def current_operation(self):
+        return self._mode
+
+    @property
+    def operation_list(self):
+        from maxcul import MODE_AUTO, MODE_MANUAL, MODE_TEMPORARY, MODE_BOOST
+        return [MODE_AUTO, MODE_MANUAL, MODE_TEMPORARY, MODE_BOOST]
+
+    def set_temperature(self, **kwargs):
+        from maxcul import MODE_MANUAL
+        target_temperature = kwargs.get(ATTR_TEMPERATURE)
+        if target_temperature is None:
+            return False
+
+        try:
+            self._maxcul_handle.set_temperature(self._thermostat_id, target_temperature, self._mode or MODE_MANUAL)
+        except Exception as e:
+            LOGGER.error("Failed to set target temperature: {}".format(e))
+            return False
+
+    def set_operation_mode(self, operation_mode):
+        try:
+            self._maxcul_handle.set_temperature(self._thermostat_id, self._target_temperature or DEFAULT_TEMPERATURE, operation_mode)
+        except Exception as e:
+            LOGGER.error("Failed to set operation mode: {}".format(e))
+            return False
+
---
+++ homeassistant/components/maxcul.py
@@ -0,0 +1,111 @@
+import logging
+import os
+
+LOGGER = logging.getLogger(__name__)
+
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
+from homeassistant.config import load_yaml_config_file
+from homeassistant.helpers import discovery
+from homeassistant.util.yaml import load_yaml, dump as dump_yaml
+
+DOMAIN = 'maxcul'
+
+REQUIREMENTS = [ 'pymaxcul' ]
+
+CONF_DEVICE_PATH = 'device_path'
+CONF_DEVICE_BAUD_RATE = 'device_baud_rate'
+
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_DEVICE_PATH): cv.string,
+        vol.Required(CONF_DEVICE_BAUD_RATE): cv.positive_int
+    })
+},extra=vol.ALLOW_EXTRA)
+
+DATA_MAXCUL = 'maxcul'
+DATA_DEVICES = 'maxcul_devices'
+
+EVENT_THERMOSTAT_UPDATE = 'maxcul.thermostat_update'
+
+YAML_DEVICES = 'maxcul_paired_devices.yaml'
+
+SERIVCE_ENABLE_PAIRING = 'enable_pairing'
+
+SCHEMA_SERVICE_ENABLE_PAIRING = vol.Schema({
+    vol.Optional('duration', default=30): cv.positive_int,
+})
+
+DESCRIPTION_SERVICE_ENABLE_PAIRING = {
+    'description': 'Enable pairing for a given duration',
+    'fields': {
+        'duration': {
+            'description': 'Duration for which pairing is possible in seconds',
+            'example': 30
+        }
+    }
+}
+
+ATTR_DEVICE_ID = 'device_id'
+
+def read_paired_devices(path):
+    if not os.path.isfile(path):
+        return []
+    paired_devices = load_yaml(path)
+    if not isinstance(paired_devices, list):
+        LOGGER.warn("Configuration file {} did not contain a list".format(path))
+        return []
+    return paired_devices
+
+def write_paired_devices(path, devices):
+    fd = os.open(path, os.O_WRONLY | os.O_CREAT)
+    os.write(fd, dump_yaml(devices).encode())
+    os.close(fd)
+
+def setup(hass, config):
+    import maxcul
+    conf = config[DOMAIN]
+    path = conf[CONF_DEVICE_PATH]
+    baud = conf[CONF_DEVICE_BAUD_RATE]
+
+    paired_devices_path = hass.config.path(YAML_DEVICES)
+    hass.data[DATA_DEVICES] = read_paired_devices(paired_devices_path)
+
+    def callback(event, payload):
+        if event == maxcul.EVENT_THERMOSTAT_UPDATE:
+            hass.bus.fire(EVENT_THERMOSTAT_UPDATE, payload)
+
+        elif event == maxcul.EVENT_DEVICE_PAIRED or event == maxcul.EVENT_DEVICE_REPAIRED:
+            device_id = payload.get(ATTR_DEVICE_ID)
+            if device_id is not None and device_id not in hass.data[DATA_DEVICES]:
+                hass.data[DATA_DEVICES].append(device_id)
+                discovery.load_platform(hass, 'climate', DOMAIN, payload, config)
+                write_paired_devices(paired_devices_path, hass.data[DATA_DEVICES])
+
+    maxconn = hass.data[DATA_MAXCUL] = maxcul.MaxConnection(
+        device_path = path,
+        baudrate = baud,
+        paired_devices = list(hass.data[DATA_DEVICES]),
+        callback = callback
+    )
+    maxconn.start()
+
+    for device_id in hass.data[DATA_DEVICES]:
+        discovery.load_platform(hass, 'climate', DOMAIN, { ATTR_DEVICE_ID: device_id }, config)
+
+    def _service_enable_pairing(service):
+        duration = service.data.get('duration')
+        maxconn.enable_pairing(duration)
+
+    hass.services.register(DOMAIN, SERIVCE_ENABLE_PAIRING, _service_enable_pairing,
+                          DESCRIPTION_SERVICE_ENABLE_PAIRING, schema=SCHEMA_SERVICE_ENABLE_PAIRING)
+
+    # Stops server when HASS is shutting down
+    # TODO this leads to an exception, I don't know how to handle a separate thread :(
+    #hass.bus.listen_once(
+    #    EVENT_HOMEASSISTANT_STOP, hass.data[DATA_MAXCUL].stop)
+
+
+    return True
