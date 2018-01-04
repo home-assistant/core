4a4654ac7dc9db867c0fe397315e53c9060fcbd8
tschmidty69 <tschmidty@yahoo.com>
2018-01-04 14:34:25 -0500

    added unittests

--- homeassistant/components/snips.py
+++ homeassistant/components/snips.py
@@ -7,8 +7,9 @@ https://home-assistant.io/components/snips/
 import asyncio
 import json
 import logging
+from datetime import timedelta
 import voluptuous as vol
-from homeassistant.helpers import intent, template, config_validation as cv
+from homeassistant.helpers import intent, config_validation as cv
 import homeassistant.components.mqtt as mqtt
 
 DOMAIN = 'snips'
@@ -60,7 +61,7 @@ def async_setup(hass, config):
             _LOGGER.error('Intent has invalid schema: %s. %s', err, request)
             return
 
-        snips_response = SnipsResponse(hass, request.get('siteId', 'default'))
+        snips_response = None
 
         if request['intent']['intentName'].startswith('user'):
             intent_type = request['intent']['intentName'].split('__')[-1]
@@ -68,62 +69,46 @@ def async_setup(hass, config):
             intent_type = request['intent']['intentName'].split(':')[-1]
         slots = {}
         for slot in request.get('slots', []):
-            if 'value' in slot['value']:
-                slots[slot['slotName']] = {'value': slot['value']['value']}
-            else:
-                slots[slot['slotName']] = {'value': slot['rawValue']}
+            slots[slot['slotName']] = {'value': resolve_slot_values(slot)}
 
         try:
             intent_response = yield from intent.async_handle(
                 hass, DOMAIN, intent_type, slots, request['input'])
+            if 'plain' in intent_response.speech:
+                snips_response = intent_response.speech['plain']['speech']
         except intent.UnknownIntent as err:
             _LOGGER.warning("Received unknown intent %s",
                             request['intent']['intentName'])
-            snips_response.add_speech(
-                "This intent is not yet configured within Home Assistant.")
+            snips_response = "Unknown Intent"
         except intent.IntentError:
             _LOGGER.exception("Error while handling intent: %s.", intent_type)
+            snips_response = "Error while handling intent"
 
-        if 'plain' in intent_response.speech:
-            snips_response.add_speech(
-                intent_response.speech['plain']['speech'])
+        notification = {'sessionId': request.get('sessionId', 'default'),
+                        'text': snips_response}
 
-        if intent_response.speech:
-            snips_response.send_response()
+        _LOGGER.debug("send_response %s", json.dumps(notification))
+        mqtt.async_publish(hass, 'hermes/dialogueManager/endSession',
+                           json.dumps(notification))
 
     yield from hass.components.mqtt.async_subscribe(
         INTENT_TOPIC, message_received)
 
     return True
 
+def resolve_slot_values(slot):
+    """Convert snips builtin types to useable values."""
+    if 'value' in slot['value']:
+        value = slot['value']['value']
+    else:
+        value = slot['rawValue']
 
-class SnipsResponse(object):
-    """Help generating the response for Snips."""
+    if slot.get('entity') == "snips/duration":
+        delta = timedelta(weeks=slot['value']['weeks'],
+                          days=slot['value']['days'],
+                          hours=slot['value']['hours'],
+                          minutes=slot['value']['minutes'],
+                          seconds=slot['value']['seconds'])
+        value = delta.seconds
 
-    def __init__(self, hass, siteid):
-        """Initialize the Snips response."""
-        self.hass = hass
-        self.text = None
-        self.siteid = siteid
-
-    def add_speech(self, text):
-        """Add speech to the response."""
-        assert self.text is None
-
-        if isinstance(text, template.Template):
-            text = text.async_render()
-
-        self.text = text
-
-    def as_dict(self):
-        """Return response in a Snips valid dictionary."""
-        return {
-            'siteId': self.siteid,
-            'init': {'type': 'notification', 'text': self.text}
-        }
-
-    def send_response(self):
-        """ Send response as TTS. """
-        _LOGGER.debug("send_response %s", json.dumps(self.as_dict()))
-        mqtt.async_publish(self.hass, 'hermes/dialogueManager/startSession',
-                           json.dumps(self.as_dict()))
+    return value
