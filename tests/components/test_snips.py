704516eaa08295d13cc226e570c1f97b1b621e2d
tschmidty69 <tschmidty@yahoo.com>
2018-01-05 14:19:16 -0500

    Update test_snips.py

--- tests/components/test_snips.py
+++ tests/components/test_snips.py
@@ -129,7 +129,7 @@ class TestSnips(unittest.TestCase):
             fire_mqtt_message(self.hass,
                               'hermes/intent/SetTimer', payload)
             self.hass.block_till_done()
-            self.assertIn("\"sessionId\": \"abcdef1234567890\",
+            self.assertIn("\"sessionId\": \"abcdef1234567890\"",
                           test_handle.output[1])
             self.assertIn("\"text\": \"Timer set 300\"}",
                           test_handle.output[1])
@@ -164,7 +164,7 @@ class TestSnips(unittest.TestCase):
             fire_mqtt_message(self.hass,
                               'hermes/intent/CallServiceIntent', payload)
             self.hass.block_till_done()
-            self.assertIn("\"sessionId\": \"abcdef1234567890\",
+            self.assertIn("\"sessionId\": \"abcdef1234567890\"",
                           test_handle.output[1])
             self.assertIn("\"text\": \"Service called\"}",
                           test_handle.output[1])
