b7c70418734b079213afdf8f3ba6ab716598b425
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-01-04 21:40:18 -0800

    Add some tests to the cloud component (#11460)

--- homeassistant/components/cloud/iot.py
+++ homeassistant/components/cloud/iot.py
@@ -212,8 +212,8 @@ def async_handle_alexa(hass, cloud, payload):
 
 @HANDLERS.register('google_actions')
 @asyncio.coroutine
-def async_handle_google_assistant(hass, cloud, payload):
-    """Handle an incoming IoT message for Google Assistant."""
+def async_handle_google_actions(hass, cloud, payload):
+    """Handle an incoming IoT message for Google Actions."""
     result = yield from ga.async_handle_message(hass, cloud.gass_config,
                                                 payload)
     return result
