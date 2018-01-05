862d0a6c25a1dee12968f82e3fbbc370299c3681
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-01-05 11:28:39 -0800

    Version bump to 0.60.1

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -2,7 +2,7 @@
 """Constants used by Home Assistant components."""
 MAJOR_VERSION = 0
 MINOR_VERSION = 60
-PATCH_VERSION = '0'
+PATCH_VERSION = '1'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 REQUIRED_PYTHON_VER = (3, 4, 2)
