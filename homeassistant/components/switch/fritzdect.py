7fcac3551b753b1e43675a478623fca20c753eec
Wolf-Bastian Pöttner <bastian@poettner.de>
2018-01-06 16:54:41 +0000

    Update to fritzhome 1.0.4

--- homeassistant/components/switch/fritzdect.py
+++ homeassistant/components/switch/fritzdect.py
@@ -14,7 +14,7 @@ from homeassistant.const import (
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE, STATE_UNKNOWN
 
-REQUIREMENTS = ['fritzhome==1.0.3']
+REQUIREMENTS = ['fritzhome==1.0.4']
 
 _LOGGER = logging.getLogger(__name__)
 
