5d5365fc73661273a0032bfdbe4dbc83d3798408
tschmidty69 <tschmidty@yahoo.com>
2018-01-07 13:25:54 -0500

    still broken

--- tests/components/test_snips.py
+++ tests/components/test_snips.py
@@ -1,227 +1,120 @@
-"""The tests for the Dialogflow component."""
-import unittest
+"""Test the Snips component."""
+import asyncio
 
 from homeassistant.core import callback
-from homeassistant.components import snips
-from homeassistant.setup import async_setup_component, setup_component
-from tests.common import (get_test_home_assistant, fire_mqtt_message,
-                          mock_mqtt_component, mock_component, mock_service)
+from homeassistant.bootstrap import async_setup_component
+from tests.common import async_fire_mqtt_message, async_mock_intent
 
 
-class TestSnips(unittest.TestCase):
-    """Test Snips."""
+@asyncio.coroutine
+def test_snips_call_action(hass, mqtt_mock):
+    """Test calling action via Snips."""
+    result = yield from async_setup_component(hass, "snips", {
+        "snips": {},
+    })
+    assert result
+    payload = """
+    {
+        "input": "turn the lights green",
+        "intent": {
+            "intentName": "Lights",
+            "probability": 1
+        },
+        "slots": [
+            {
+                "slotName": "light_color",
+                "value": {
+                    "kind": "Custom",
+                    "value": "green"
+                }
+            }
+        ]
+    }
+    """
 
-    def setUp(self):
-        """Setup things to be run when tests are started."""
-        self.hass = get_test_home_assistant()
-        self.calls = []
+    intents = async_mock_intent(hass, 'Lights')
 
-        mock_mqtt_component(self.hass)
-        assert async_setup_component(self.hass, snips.DOMAIN, {
-            "snips": {},
-        })
-        self.hass.start()
+    async_fire_mqtt_message(hass, 'hermes/intent/Lights',
+                            payload)
+    yield from hass.async_block_till_done()
+    assert len(intents) == 1
+    intent = intents[0]
+    assert intent.platform == 'snips'
+    assert intent.intent_type == 'Lights'
+    assert intent.slots == {'light_color': {'value': 'green'}}
+    assert intent.text_input == 'turn the lights green'
 
-    def tearDown(self):  # pylint: disable=invalid-name
-        """Stop everything that was started."""
-        self.hass.stop()
+
+@asyncio.coroutine
+def test_snips_unknown_intent(hass, mqtt_mock):
+    """Test calling action via Snips."""
+    result = yield from async_setup_component(hass, "snips", {
+        "snips": {},
+    })
+    assert result
+    payload = """
+    {
+        "input": "what to do",
+        "intent": {
+            "intentName": "unknownIntent"
+        },
+        "slots": []
+    }
+    """
+    intents = async_mock_intent(hass, 'knownIntent')
+
+    async_fire_mqtt_message(hass, 'hermes/intent/unknownIntent',
+                            payload)
+    yield from hass.async_block_till_done()
+    assert len(intents) == 1
+    intent = intents[0]
+    assert intent.platform == 'snips'
+    assert intent.intent_type == 'unknownIntent'
+    assert intent.text_input == 'what to do'
+
+
+@asyncio.coroutine
+def test_intent_response(hass, mqtt_mock):
+    """Test intent response."""
+    event = 'call_service'
+    events = []
 
     @callback
-    def record_calls(self, *args):
-        """Helper for recording calls."""
-        self.calls.append(args)
+    def record_event(event):
+        """Add recorded event to set."""
+        events.append(event)
 
-    def test_intent_call_service(self):
-        """Test intent calling a service."""
-        mock_component(self.hass, 'mqtt')
-        setup_component(self.hass, snips.DOMAIN, {
-            snips.DOMAIN: {},
-        })
-        setup_component(self.hass, "intent_script", {
-            "intent_script": {
-                "CallServiceIntent": {
-                    "speech": {
-                        "type": "plain",
-                        "text": "Service called",
-                    },
-                    "action": {
-                        "service": "test.snips",
-                        "data_template": {
-                            "zodiac_sign": "{{ ZodiacSign }}"
-                        },
-                        "entity_id": "switch.test",
-                    }
+    result = yield from async_setup_component(hass, "snips", {
+        "snips": {},
+    })
+    assert result
+    result = yield from async_setup_component(hass, "intent_script", {
+        "intent_script": {
+            "spokenIntent": {
+                "speech": {
+                    "type": "plain",
+                    "text": "Hello",
                 }
             }
-        })
-
-        service_calls = mock_service(self.hass, 'test', 'snips')
-        payload = """
-        {
-            "input": "zodiac forecast for virgo",
-            "sessionId": "abcdef1234567890",
-            "intent": {
-                "intentName": "CallServiceIntent"
-            },
-            "slots": [
-                {
-                    "slotName": "ZodiacSign",
-                    "rawValue": "virgo",
-                    "value": {
-                        "kind": "custom",
-                        "value": "virgo"
-                    }
-                }
-            ]
         }
-        """
-        fire_mqtt_message(self.hass, 'hermes/intent/CallServiceIntent',
-                          payload)
-        self.hass.block_till_done()
-        self.assertEqual(1, len(service_calls))
-        self.assertEqual('virgo', service_calls[0].data['zodiac_sign'])
-        self.assertEqual('switch.test', service_calls[0].data['entity_id'][0])
-
-    def test_intent_with_snips_duration(self):
-        """Test intent with snips duration."""
-        mock_component(self.hass, 'mqtt')
-        setup_component(self.hass, snips.DOMAIN, {
-            snips.DOMAIN: {},
-        })
-        setup_component(self.hass, "intent_script", {
-            "intent_script": {
-                "SetTimer": {
-                    "speech": {
-                        "type": "plain",
-                        "text": "Timer set {{ timer_duration }}",
-                    },
-                }
-            }
-        })
-        payload = """
-        {
-            "input": "set a timer of five minutes",
-            "intent": {
-                "intentName": "SetTimer"
-            },
-            "slots": [
-                {
-                    "rawValue": "five minutes",
-                    "value": {
-                        "kind": "Duration",
-                        "years": 0,
-                        "quarters": 0,
-                        "months": 0,
-                        "weeks": 0,
-                        "days": 0,
-                        "hours": 0,
-                        "minutes": 5,
-                        "seconds": 0,
-                        "precision": "Exact"
-                    },
-                    "entity": "snips/duration",
-                    "slotName": "timer_duration"
-                }
-            ]
-        }
-        """
-        with self.assertLogs(level='INFO') as test_handle:
-            fire_mqtt_message(self.hass,
-                              'hermes/intent/SetTimer', payload)
-            self.hass.block_till_done()
-            self.assertIn("\"sessionId\": \"default\"",
-                          test_handle.output[1])
-            self.assertIn("\"text\": \"Timer set 300\"}",
-                          test_handle.output[1])
-
-    def test_intent_response(self):
-        """Test intent response."""
-        mock_component(self.hass, 'mqtt')
-        setup_component(self.hass, snips.DOMAIN, {
-            snips.DOMAIN: {},
-        })
-        setup_component(self.hass, "intent_script", {
-            "intent_script": {
-                "spokenIntent": {
-                    "speech": {
-                        "type": "plain",
-                        "text": "Service called",
-                    }
-                }
-            }
-        })
-        payload = """
-        {
-            "input": "speak to me",
-            "sessionId": "abcdef1234567890",
-            "intent": {
-                "intentName": "spokenIntent"
-            },
-            "slots": []
-        }
-        """
-        with self.assertLogs(level='INFO') as test_handle:
-            fire_mqtt_message(self.hass,
-                              'hermes/intent/CallServiceIntent', payload)
-            self.hass.block_till_done()
-            self.assertIn("\"sessionId\": \"abcdef1234567890\"",
-                          test_handle.output[1])
-            self.assertIn("\"text\": \"Service called\"}",
-                          test_handle.output[1])
-
-    def test_unknown_intent(self):
-        """Test unknown intent."""
-        mock_mqtt_component(self.hass)
-        setup_component(self.hass, snips.DOMAIN, {
-            snips.DOMAIN: {},
-        })
-        payload = """
-        {
-            "input": "I don't know what I am supposed to do",
-            "sessionId": "abcdef1234567890",
-            "intent": {
-                "intentName": "unknownIntent"
-            },
-            "slots": []
-        }
-        """
-        with self.assertLogs(level='WARNING') as test_handle:
-            fire_mqtt_message(self.hass,
-                              'hermes/intent/unknownIntent', payload)
-            self.hass.block_till_done()
-            self.assertIn("WARNING:homeassistant.components.snips:"
-                          "Received unknown intent unknownIntent",
-                          test_handle.output[0])
-
-    def test_intent_invalid_confg(self):
-        """Test when intent has invalid config."""
-        setup_component(self.hass, snips.DOMAIN, {
-            "snips": {},
-        })
-        payload = """
-        {
-            "input": "turn the lights green",
-            "sessionId": "abcdef1234567890",
-            "intent": {
-                "intentName": "activateLights",
-                "probability": 1
-            },
-            "slots": [
-                {
-                    "rawValue": "green",
-                    "value": {
-                        "value": "green"
-                    },
-                    "entity": "light_color",
-                    "slotName": "light_color"
-                }
-            ]
-        }
-        """
-        with self.assertLogs(level='ERROR') as test_handle:
-            fire_mqtt_message(self.hass,
-                              'hermes/intent/activateLights', payload)
-            self.hass.block_till_done()
-            self.assertIn("ERROR:homeassistant.components.snips:"
-                          "Intent has invalid schema",
-                          test_handle.output[0])
+    })
+    assert result
+    payload = """
+    {
+        "input": "speak to me",
+        "intent": {
+            "intentName": "spokenIntent"
+        },
+        "slots": []
+    }
+    """
+    async_fire_mqtt_message(hass, 'hermes/intent/spokenIntent',
+                            payload)
+    yield from hass.async_block_till_done()
+    hass.bus.listen(event, record_event)
+    # assert len(intents) == 1
+    # intent = intents[0]
+    # assert intent.platform == 'snips'
+    # assert intent.intent_type == 'spokenIntent'
+    # assert intent.text_input == 'speak to me'
+    # assert len(events) == 0
