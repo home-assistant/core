b46bb390f1f1a957003625a6082e02d4ba9f5959
starkillerOG <starkiller.og@gmail.com>
2018-01-09 00:54:34 +0100

    Hyperion: Add brightness, HDMI and effect support
    - fixed invalid variable "A"

--- homeassistant/components/light/hyperion.py
+++ homeassistant/components/light/hyperion.py
@@ -221,10 +221,10 @@ class Hyperion(Light):
                     self._rgb_color = [175, 0, 255]
                     self._icon = 'mdi:lava-lamp'
                     try:
-                        A = response['info']['activeEffects'][0]["script"]
-                        A = A.split('/')[-1][:-3].split("-")[0]
+                        s_name = response['info']['activeEffects'][0]["script"]
+                        s_name = s_name.split('/')[-1][:-3].split("-")[0]
                         self._effect = [x for x in self._effect_list
-                                        if (A.lower() in x.lower())][0]
+                                        if (s_name.lower() in x.lower())][0]
                     except:
                         self._effect = 'none'
                 # Bulb off state
