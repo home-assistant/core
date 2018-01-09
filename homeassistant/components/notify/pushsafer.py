7d0368b6f1a82a08c6f01375249cee97af081a57
Kevin Siml <appzer@users.noreply.github.com>
2018-01-09 10:43:54 +0100

    Update pushsafer.py

--- homeassistant/components/notify/pushsafer.py
+++ homeassistant/components/notify/pushsafer.py
@@ -15,6 +15,7 @@ from homeassistant.components.notify import (
     ATTR_TITLE, ATTR_TITLE_DEFAULT, ATTR_TARGET, ATTR_DATA,
     PLATFORM_SCHEMA, BaseNotificationService)
 import homeassistant.helpers.config_validation as cv
+
 _LOGGER = logging.getLogger(__name__)
 _RESOURCE = 'https://www.pushsafer.com/api'
 _ALLOWED_IMAGES = ['image/gif', 'image/jpeg', 'image/png']
@@ -85,12 +86,24 @@ class PushsaferNotificationService(BaseNotificationService):
         picture1 = data.get(ATTR_PICTURE1)
         picture1_encoded = ""
         if picture1 is not None:
-            picture1_encoded = self.loadfromfile(
-                url=picture1.get(ATTR_PICTURE1_URL),
-                local_path=picture1.get(ATTR_PICTURE1_PATH),
-                username=picture1.get(ATTR_PICTURE1_USERNAME),
-                password=picture1.get(ATTR_PICTURE1_PASSWORD),
-                auth=picture1.get(ATTR_PICTURE1_AUTH))
+            _LOGGER.debug("picture1 is available")
+            url = picture1.get(ATTR_PICTURE1_URL, None)
+            local_path = picture1.get(ATTR_PICTURE1_PATH, None)
+            username = picture1.get(ATTR_PICTURE1_USERNAME)
+            password = picture1.get(ATTR_PICTURE1_PASSWORD)
+            auth = picture1.get(ATTR_PICTURE1_AUTH)
+
+            if url is not None:
+                _LOGGER.debug("Loading image from url %s", url)
+                picture1_encoded = self.loadfromurl(url, username, password,
+                                                    auth)
+            elif local_path is not None:
+                _LOGGER.debug("Loading image from file %s", local_path)
+                picture1_encoded = self.loadfromfile(local_path)
+            else:
+                _LOGGER.warning("missing url or local_path for picture1")
+        else:
+            _LOGGER.debug("picture1 is not specified")
 
         payload = {
             'k': self._private_key,
@@ -112,13 +125,13 @@ class PushsaferNotificationService(BaseNotificationService):
             payload['d'] = target
             response = requests.post(_RESOURCE, data=payload,
                                      timeout=CONF_TIMEOUT)
-            if response.status_code != '200':
+            if response.status_code != 200:
                 _LOGGER.error("Pushsafer failed with: %s", response.text)
             else:
                 _LOGGER.debug("Push send: %s", response.json())
 
     @classmethod
-    def convertbase64string(cls, filebyte, mimetype):
+    def getbase64(cls, filebyte, mimetype):
         """Convert the image to the expected base64 string of pushsafer."""
         if mimetype not in _ALLOWED_IMAGES:
             _LOGGER.warning("%s is a not supported mimetype for images",
@@ -127,40 +140,41 @@ class PushsaferNotificationService(BaseNotificationService):
         else:
             if filebyte is not None:
                 base64_image = base64.b64encode(filebyte).decode('utf8')
-                return "data:"+mimetype+";base64,"+base64_image
+                return "data:" + mimetype + ";base64," + base64_image
             else:
                 _LOGGER.warning("Base64 encode no image passed")
 
-    def loadfromfile(self, url=None, local_path=None, username=None,
-                     password=None, auth=None):
-        """Load image/document/etc from a local path or URL."""
-        try:
-            if url is not None:
-                _LOGGER.debug("Downloading image from %s", url)
-                if username is not None and password is not None:
-                    auth_ = HTTPBasicAuth(username, password)
-                    response = requests.get(url, auth=auth_,
-                                            timeout=CONF_TIMEOUT)
-                else:
-                    response = requests.get(url, timeout=CONF_TIMEOUT)
-                    return self.convertbase64string(
-                        response.content,
-                        response.headers['content-type'])
+    def loadfromurl(self, url=None, username=None, password=None, auth=None):
+        """Load image/document/etc from URL."""
+        if url is not None:
+            _LOGGER.debug("Downloading image from %s", url)
+            if username is not None and password is not None:
+                auth_ = HTTPBasicAuth(username, password)
+                response = requests.get(url, auth=auth_,
+                                        timeout=CONF_TIMEOUT)
+            else:
+                response = requests.get(url, timeout=CONF_TIMEOUT)
+            return self.getbase64(response.content,
+                                    response.headers['content-type'])
+        else:
+            _LOGGER.warning("url not found in param")
 
-            elif local_path is not None:
+        return None
+
+    def loadfromfile(self, local_path=None):
+        """Load image/document/etc from a local path."""
+        try:
+            if local_path is not None:
                 _LOGGER.debug("Loading image from local path")
                 if self.is_allowed_path(local_path):
                     file_mimetype = mimetypes.guess_type(local_path)
                     _LOGGER.debug("Detected mimetype %s", file_mimetype)
                     with open(local_path, "rb") as binary_file:
                         data = binary_file.read()
-                    return self.convertbase64string(data, file_mimetype[0])
-                _LOGGER.warning("'%s' is not secure to load data from!",
-                                local_path)
+                    return self.getbase64(data, file_mimetype[0])
             else:
-                _LOGGER.warning("Neither URL nor local path found in params!")
-
+                _LOGGER.warning("Local path not found in params!")
         except OSError as error:
-            _LOGGER.error("Can't load from url or local path: %s", error)
+            _LOGGER.error("Can't load from local path: %s", error)
 
         return None
