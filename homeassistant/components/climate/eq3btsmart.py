08a285e3b7e92ec511d949bf07e090f2043fbbf1
Karol Ksionek <karlkar@tlen.pl>
2018-01-09 21:45:39 +0100

    Fixed trailing whitespaces

--- homeassistant/components/climate/eq3btsmart.py
+++ homeassistant/components/climate/eq3btsmart.py
@@ -179,11 +179,11 @@ class EQ3BTSmartThermostat(ClimateDevice):
         from bluepy.btle import BTLEException
         try:
             self._thermostat.update()
-            if (self._target_temperature and 
-                self._thermostat.target_temperature 
-                  != self._target_temperature):
+            if (self._target_temperature and
+                self._thermostat.target_temperature
+                != self._target_temperature):
                 self.set_temperature(temperature=self._target_temperature)
-            if (self._target_mode and 
+            if (self._target_mode and
                 self.modes[self._thermostat.mode] != self._target_mode):
                 self.set_operation_mode(operation_mode=self._target_mode)
             else:
