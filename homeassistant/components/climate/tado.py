d2b666088193a65f4f9e3664344aa34241af06ab
Josh Anderson <andersonshatch@gmail.com>
2018-01-11 09:49:41 +0000

    Tado improvements - hot water zone sensors and climate precision (#11521)
    * Add tado hot water zone sensors
    
    * Set precision to match tado app/website

--- homeassistant/components/climate/tado.py
+++ homeassistant/components/climate/tado.py
@@ -6,7 +6,7 @@ https://home-assistant.io/components/climate.tado/
 """
 import logging
 
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import (PRECISION_TENTHS, TEMP_CELSIUS)
 from homeassistant.components.climate import (
     ClimateDevice, SUPPORT_TARGET_TEMPERATURE, SUPPORT_OPERATION_MODE)
 from homeassistant.const import ATTR_TEMPERATURE
@@ -191,6 +191,11 @@ class TadoClimate(ClimateDevice):
     def is_away_mode_on(self):
         """Return true if away mode is on."""
         return self._is_away
+
+    @property
+    def target_temperature_step(self):
+        """Return the supported step of target temperature."""
+        return PRECISION_TENTHS
 
     @property
     def target_temperature(self):
