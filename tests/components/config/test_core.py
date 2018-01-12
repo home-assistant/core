be81b9b39aa90b5bf05bca6b3372bccc9494b763
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-01-12 09:19:06 +0100

    Update test_core.py

--- tests/components/config/test_core.py
+++ tests/components/config/test_core.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 
 from homeassistant.bootstrap import async_setup_component
 from homeassistant.components import config
-from tests.common import mock_coro, mock_component
+from tests.common import mock_coro
 
 
 @asyncio.coroutine
@@ -29,38 +29,6 @@ def test_validate_config_ok(hass, test_client):
 
     with patch(
         'homeassistant.components.config.core.async_check_ha_config_file',
-            return_value=mock_coro('beer')):
-        resp = yield from client.post('/api/config/core/check_config')
-
-    assert resp.status == 200
-    result = yield from resp.json()
-    assert result['result'] == 'invalid'
-    assert result['errors'] == 'beer'
-
-
-@asyncio.coroutine
-def test_validate_config_ok_hassio(hass, test_client):
-    """Test checking config."""
-    mock_component(hass, 'hassio')
-    with patch.object(config, 'SECTIONS', ['core']):
-        yield from async_setup_component(hass, 'config', {})
-
-    yield from asyncio.sleep(0.1, loop=hass.loop)
-
-    client = yield from test_client(hass.http.app)
-
-    with patch(
-        'homeassistant.components.hassio.async_check_config',
-            return_value=mock_coro()):
-        resp = yield from client.post('/api/config/core/check_config')
-
-    assert resp.status == 200
-    result = yield from resp.json()
-    assert result['result'] == 'valid'
-    assert result['errors'] is None
-
-    with patch(
-        'homeassistant.components.hassio.async_check_config',
             return_value=mock_coro('beer')):
         resp = yield from client.post('/api/config/core/check_config')
 
