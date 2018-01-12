0c9a207db1e8841e33840b95056de9ce4179301e
kbickar <trumpetgod@gmail.com>
2018-01-12 14:36:00 -0500

    Moved import in function

--- homeassistant/components/sensor/sense.py
+++ homeassistant/components/sensor/sense.py
@@ -14,7 +14,6 @@ from homeassistant.const import (CONF_EMAIL, CONF_PASSWORD)
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
-from sense_energy import Senseable
 
 REQUIREMENTS = ['sense_energy==0.2.2']
 
@@ -41,6 +40,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Sense sensor."""
+	from sense_energy import Senseable
+	
     username = config.get(CONF_EMAIL)
     password = config.get(CONF_PASSWORD)
 
