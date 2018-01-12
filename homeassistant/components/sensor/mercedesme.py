a92ca5498cbaf330448b7029293fdb2e2eedbc68
Rene Nulsch <ReneNulschDE.github@Nulsch.de>
2018-01-13 00:04:20 +0100

    Add Mercedes me component

---
+++ homeassistant/components/sensor/mercedesme.py
@@ -0,0 +1,103 @@
+"""
+Support for Mercedes cars with Mercedes ME.
+
+For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/mercedesme/
+"""
+import logging
+import datetime
+
+from homeassistant.helpers.entity import Entity
+
+DEPENDENCIES = ['mercedesme']
+
+DATA_MME = 'mercedesme'
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the sensor platform."""
+    if discovery_info is None:
+        return
+
+    controller = hass.data[DATA_MME]['controller']
+
+    if not controller.cars:
+        return False
+
+    for car in controller.cars:
+        add_devices([Sensor('fuelLevelPercent', car, controller, "%")], True)
+        add_devices([Sensor('fuelRangeKm', car, controller, "Km")], True)
+        add_devices([Sensor('serviceIntervalDays',
+                            car, controller, "days")], True)
+        add_devices([Sensor('odometerKm', car, controller, "Km")], True)
+        add_devices([Sensor('latestTrip', car, controller, None)], True)
+
+    return True
+
+
+class Sensor(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self, SensorName, Car, Controller, Unit):
+        """Initialize the sensor."""
+        self._state = None
+        self.__name = SensorName
+        self.__car = Car
+        self.controller = Controller
+        self.__unit = Unit
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self.__name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return self.__unit
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        if self.__name == "latestTrip":
+            return {
+                "durationSeconds":
+                    self.__car["latestTrip"]["durationSeconds"],
+                "distanceTraveledKm":
+                    self.__car["latestTrip"]["distanceTraveledKm"],
+                "startedAt": datetime.datetime.fromtimestamp(
+                    self.__car["latestTrip"]["startedAt"]
+                    ).strftime('%Y-%m-%d %H:%M:%S'),
+                "averageSpeedKmPerHr":
+                    self.__car["latestTrip"]["averageSpeedKmPerHr"],
+                "finished": self.__car["latestTrip"]["finished"],
+                "lastUpdate": datetime.datetime.fromtimestamp(
+                    self.__car["lastUpdate"]
+                    ).strftime('%Y-%m-%d %H:%M:%S'),
+                "car": self.__car["license"]
+            }
+
+        return {
+            "lastUpdate": datetime.datetime.fromtimestamp(
+                self.__car["lastUpdate"]).strftime('%Y-%m-%d %H:%M:%S'),
+            "car": self.__car["license"]
+        }
+
+    def update(self):
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self.controller.update()
+
+        if self.__name == "latestTrip":
+            self._state = self.__car["latestTrip"]["id"]
+        else:
+            self._state = self.__car[self.__name]
