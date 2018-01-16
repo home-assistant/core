13141cf8334f28395bed59a13adefff7a8ea0e7e
arcsur <arcsur@users.noreply.github.com>
2018-01-16 19:26:49 +0800

    Added Pylint exception for import error of system specific library

--- homeassistant/components/sensor/bme680.py
+++ homeassistant/components/sensor/bme680.py
@@ -92,6 +92,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
+# pylint: disable=import-error
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the BME680 sensor."""
