b56d39a99ddb78ba88ca91b0cd8df115b243651c
Rene Nulsch <ReneNulschDE.github@Nulsch.de>
2018-01-18 18:24:59 +0100

    Return statements added again

--- homeassistant/components/device_tracker/mercedesme.py
+++ homeassistant/components/device_tracker/mercedesme.py
@@ -15,14 +15,16 @@ DATA_MME = 'mercedesme'
 def setup_scanner(hass, config, see, discovery_info=None):
     """Set up the Mercedes ME tracker."""
     if discovery_info is None:
-        return
+        return False
 
     controller = hass.data[DATA_MME]['controller']
 
     if not controller.cars:
-        return
+        return False
 
     MercedesMEDeviceTracker(hass, config, see, controller)
+
+    return True
 
 
 class MercedesMEDeviceTracker(object):
