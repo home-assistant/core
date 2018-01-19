7cdcb40665a565e9efc7a4d866a57e3d2ca2f5b5
William Scanlon <wjs.scanlon@gmail.com>
2018-01-18 21:41:34 -0500

    Support for performance mode on Rheem water heaters.

--- homeassistant/components/climate/econet.py
+++ homeassistant/components/climate/econet.py
@@ -17,6 +17,7 @@ from homeassistant.components.climate import (
     STATE_HEAT_PUMP, STATE_HIGH_DEMAND,
     STATE_OFF, SUPPORT_TARGET_TEMPERATURE,
     SUPPORT_OPERATION_MODE,
+    STATE_PERFORMANCE,
     ClimateDevice)
 from homeassistant.config import load_yaml_config_file
 from homeassistant.const import (ATTR_ENTITY_ID,
@@ -61,6 +62,7 @@ HA_STATE_TO_ECONET = {
     STATE_GAS: 'gas',
     STATE_HIGH_DEMAND: 'High Demand',
     STATE_OFF: 'Off',
+    STATE_PERFORMANCE: 'Performance'
 }
 
 ECONET_STATE_TO_HA = {value: key for key, value in HA_STATE_TO_ECONET.items()}
