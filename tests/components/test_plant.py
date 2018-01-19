b84e551aea52fb9898c67d051ae0905b275e2a5a
ChristianKuehnel <christian.kuehnel@gmail.com>
2018-01-19 08:46:12 +0100

    plant - check history for min_brightness (#9534)
    * Added option to create a group for the plant and all of it's sensors so that they appear together in the UI
    
    * fixed warnings from the hound
    
    * added check for min_brightness over several days
    
    * fixed hound complaints
    
    * 1) added missing dependency on recorder
    2) using group.Group instead of hass.states.async_set as requested by @pvizeli
    
    * fixed pylint error in docstring
    
    * changed the way the groups are created
    
    * fixed requirements issue
    
    * Changed the way the groups are implemented. This is proposal number 4...
    
    * Data read from recorder only on startup.
    
    We now only store one data point per day. If a recorder is configured, this data is initialized from the database. If not the list is empty on startup.
    
    * added missing documentation
    
    * fixed typo in comment
    
    * removed group fature
    
    * added group dependency since it's still needed
    
    * fixed bug: now "None" is no longer added to the DailyHistory
    
    * now also outputting unit of measurement if defined by the sensors
    
    * removed iconss
    
    * fixed line length
    
    * Implemented changes requested in code reviews.
    
    These changes affect the interface to the UI:
    * renamed attribute for units of measurement to "unit_of_measurement_dict"
    * renamed attribute for maximum in brightness history to "max_brightness"
    
    * only loading the history if a brightness sensor was configured
    
    * fixed testcase
    
    * fixed stupid bug in check of brightness history
    
    Also added test for this bug
    
    * added missing docstring
    
    * Fixed sporadic failure in test case.
    
    Sometimes the component was created before the data was stored in the history. This lead to an empty history being read.
    
    * removed unused import statement in testcase
    
    * reverted change to test case
    
    * Changed startup behavior of the component.
    
    No failed tests after 20 local test runs.
    
    * added missing docstring
    
    * fixed tests
    
    * added hass.start() to Setup
    * fixed call parameters in constructor
    * added time.sleep
    
    * removed sleep
    
    * fixed typo in variable name
    
    * disabled loading from database as it's not stable at the moment and nobody knows why :(
    
    * fixed flake8
    
    * now using pytest.mark.skipif to skip test

--- tests/components/test_plant.py
+++ tests/components/test_plant.py
@@ -1,7 +1,16 @@
 """Unit tests for platform/plant.py."""
 import asyncio
+import unittest
+import pytest
+from datetime import datetime, timedelta
 
+from homeassistant.const import (ATTR_UNIT_OF_MEASUREMENT, STATE_UNKNOWN,
+                                 STATE_PROBLEM, STATE_OK)
+from homeassistant.components import recorder
 import homeassistant.components.plant as plant
+from homeassistant.setup import setup_component
+
+from tests.common import get_test_home_assistant, init_recorder_component
 
 
 GOOD_DATA = {
@@ -12,19 +21,23 @@ GOOD_DATA = {
     'brightness': 987,
 }
 
+BRIGHTNESS_ENTITY = 'sensor.mqtt_plant_brightness'
+MOISTURE_ENTITY = 'sensor.mqtt_plant_moisture'
+
 GOOD_CONFIG = {
     'sensors': {
-        'moisture': 'sensor.mqtt_plant_moisture',
+        'moisture': MOISTURE_ENTITY,
         'battery': 'sensor.mqtt_plant_battery',
         'temperature': 'sensor.mqtt_plant_temperature',
         'conductivity': 'sensor.mqtt_plant_conductivity',
-        'brightness': 'sensor.mqtt_plant_brightness',
+        'brightness': BRIGHTNESS_ENTITY,
     },
     'min_moisture': 20,
     'max_moisture': 60,
     'min_battery': 17,
     'min_conductivity': 500,
     'min_temperature': 15,
+    'min_brightness': 500,
 }
 
 
@@ -34,30 +47,152 @@ class _MockState(object):
         self.state = state
 
 
-@asyncio.coroutine
-def test_valid_data(hass):
-    """Test processing valid data."""
-    sensor = plant.Plant('my plant', GOOD_CONFIG)
-    sensor.hass = hass
-    for reading, value in GOOD_DATA.items():
-        sensor.state_changed(
-            GOOD_CONFIG['sensors'][reading], None,
-            _MockState(value))
-    assert sensor.state == 'ok'
-    attrib = sensor.state_attributes
-    for reading, value in GOOD_DATA.items():
-        # battery level has a different name in
-        # the JSON format than in hass
-        assert attrib[reading] == value
+class TestPlant(unittest.TestCase):
+    """Tests for component "plant"."""
+
+    def setUp(self):
+        """Create test instance of home assistant."""
+        self.hass = get_test_home_assistant()
+        self.hass.start()
+
+    def tearDown(self):
+        """Stop everything that was started."""
+        self.hass.stop()
+
+    @asyncio.coroutine
+    def test_valid_data(self):
+        """Test processing valid data."""
+        sensor = plant.Plant('my plant', GOOD_CONFIG)
+        sensor.hass = self.hass
+        for reading, value in GOOD_DATA.items():
+            sensor.state_changed(
+                GOOD_CONFIG['sensors'][reading], None,
+                _MockState(value))
+        assert sensor.state == 'ok'
+        attrib = sensor.state_attributes
+        for reading, value in GOOD_DATA.items():
+            # battery level has a different name in
+            # the JSON format than in hass
+            assert attrib[reading] == value
+
+    @asyncio.coroutine
+    def test_low_battery(self):
+        """Test processing with low battery data and limit set."""
+        sensor = plant.Plant('other plant', GOOD_CONFIG)
+        sensor.hass = self.hass
+        assert sensor.state_attributes['problem'] == 'none'
+        sensor.state_changed('sensor.mqtt_plant_battery',
+                             _MockState(45), _MockState(10))
+        assert sensor.state == 'problem'
+        assert sensor.state_attributes['problem'] == 'battery low'
+
+    def test_update_states(self):
+        """Test updating the state of a sensor.
+
+        Make sure that plant processes this correctly.
+        """
+        plant_name = 'some_plant'
+        assert setup_component(self.hass, plant.DOMAIN, {
+            plant.DOMAIN: {
+                plant_name: GOOD_CONFIG
+            }
+        })
+        self.hass.states.set(MOISTURE_ENTITY, 5,
+                             {ATTR_UNIT_OF_MEASUREMENT: 'us/cm'})
+        self.hass.block_till_done()
+        state = self.hass.states.get('plant.'+plant_name)
+        self.assertEquals(STATE_PROBLEM, state.state)
+        self.assertEquals(5, state.attributes[plant.READING_MOISTURE])
+
+    @pytest.mark.skipif(plant.ENABLE_LOAD_HISTORY is False,
+                        reason="tests for loading from DB are instable, thus"
+                               "this feature is turned of until tests become"
+                               "stable")
+    def test_load_from_db(self):
+        """Test bootstrapping the brightness history from the database.
+
+        This test can should only be executed if the loading of the history
+        is enabled via plant.ENABLE_LOAD_HISTORY.
+        """
+        init_recorder_component(self.hass)
+        plant_name = 'wise_plant'
+        for value in [20, 30, 10]:
+
+            self.hass.states.set(BRIGHTNESS_ENTITY, value,
+                                 {ATTR_UNIT_OF_MEASUREMENT: 'Lux'})
+            self.hass.block_till_done()
+        # wait for the recorder to really store the data
+        self.hass.data[recorder.DATA_INSTANCE].block_till_done()
+
+        assert setup_component(self.hass, plant.DOMAIN, {
+            plant.DOMAIN: {
+                plant_name: GOOD_CONFIG
+            }
+        })
+        self.hass.block_till_done()
+
+        state = self.hass.states.get('plant.'+plant_name)
+        self.assertEquals(STATE_UNKNOWN, state.state)
+        max_brightness = state.attributes.get(
+            plant.ATTR_MAX_BRIGHTNESS_HISTORY)
+        self.assertEquals(30, max_brightness)
+
+    def test_brightness_history(self):
+        """Test the min_brightness check."""
+        plant_name = 'some_plant'
+        assert setup_component(self.hass, plant.DOMAIN, {
+            plant.DOMAIN: {
+                plant_name: GOOD_CONFIG
+            }
+        })
+        self.hass.states.set(BRIGHTNESS_ENTITY, 100,
+                             {ATTR_UNIT_OF_MEASUREMENT: 'lux'})
+        self.hass.block_till_done()
+        state = self.hass.states.get('plant.'+plant_name)
+        self.assertEquals(STATE_PROBLEM, state.state)
+
+        self.hass.states.set(BRIGHTNESS_ENTITY, 600,
+                             {ATTR_UNIT_OF_MEASUREMENT: 'lux'})
+        self.hass.block_till_done()
+        state = self.hass.states.get('plant.'+plant_name)
+        self.assertEquals(STATE_OK, state.state)
+
+        self.hass.states.set(BRIGHTNESS_ENTITY, 100,
+                             {ATTR_UNIT_OF_MEASUREMENT: 'lux'})
+        self.hass.block_till_done()
+        state = self.hass.states.get('plant.'+plant_name)
+        self.assertEquals(STATE_OK, state.state)
 
 
-@asyncio.coroutine
-def test_low_battery(hass):
-    """Test processing with low battery data and limit set."""
-    sensor = plant.Plant(hass, GOOD_CONFIG)
-    sensor.hass = hass
-    assert sensor.state_attributes['problem'] == 'none'
-    sensor.state_changed('sensor.mqtt_plant_battery',
-                         _MockState(45), _MockState(10))
-    assert sensor.state == 'problem'
-    assert sensor.state_attributes['problem'] == 'battery low'
+class TestDailyHistory(unittest.TestCase):
+    """Test the DailyHistory helper class."""
+
+    def test_no_data(self):
+        """Test with empty history."""
+        dh = plant.DailyHistory(3)
+        self.assertIsNone(dh.max)
+
+    def test_one_day(self):
+        """Test storing data for the same day."""
+        dh = plant.DailyHistory(3)
+        values = [-2, 10, 0, 5, 20]
+        for i in range(len(values)):
+            dh.add_measurement(values[i])
+            max_value = max(values[0:i+1])
+            self.assertEqual(1, len(dh._days))
+            self.assertEqual(dh.max, max_value)
+
+    def test_multiple_days(self):
+        """Test storing data for different days."""
+        dh = plant.DailyHistory(3)
+        today = datetime.now()
+        today_minus_1 = today - timedelta(days=1)
+        today_minus_2 = today_minus_1 - timedelta(days=1)
+        today_minus_3 = today_minus_2 - timedelta(days=1)
+        days = [today_minus_3, today_minus_2, today_minus_1, today]
+        values = [10, 1, 7, 3]
+        max_values = [10, 10, 10, 7]
+
+        for i in range(len(days)):
+            dh.add_measurement(values[i], days[i])
+            self.assertEquals(max_values[i], dh.max)
