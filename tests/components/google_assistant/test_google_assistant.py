81df9aa73df3a3d9b24afa8699810028fd7785b1
andrewdolphin <31809425+andrewdolphin@users.noreply.github.com>
2018-01-19 10:05:14 +0000

    Update test_google_assistant.py

--- tests/components/google_assistant/test_google_assistant.py
+++ tests/components/google_assistant/test_google_assistant.py
@@ -238,7 +238,7 @@ def test_query_climate_request(hass_fixture, assistant_client):
         'climate.ecobee': {
             'thermostatTemperatureSetpointHigh': 24,
             'thermostatTemperatureAmbient': 23,
-            'thermostatMode': 'on',
+            'thermostatMode': 'heat',
             'thermostatTemperatureSetpointLow': 21
         },
         'climate.hvac': {
@@ -286,7 +286,7 @@ def test_query_climate_request_f(hass_fixture, assistant_client):
         'climate.ecobee': {
             'thermostatTemperatureSetpointHigh': -4.4,
             'thermostatTemperatureAmbient': -5,
-            'thermostatMode': 'on',
+            'thermostatMode': 'heat',
             'thermostatTemperatureSetpointLow': -6.1,
         },
         'climate.hvac': {
