81ed87434c090b0dc47cfbb853aacfd539c06f0d
Sean Olson <seandolson654@gmail.com>
2018-01-23 16:52:30 -0800

    Update tuya.py

--- homeassistant/components/switch/tuya.py
+++ homeassistant/components/switch/tuya.py
@@ -9,7 +9,7 @@ from homeassistant.components.switch import SwitchDevice, PLATFORM_SCHEMA
 from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_ID)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pytuya==3.0']
+REQUIREMENTS = ['pytuya==4.0']
 
 CONF_DEVICE_ID = 'device_id'
 CONF_LOCAL_KEY = 'local_key'
