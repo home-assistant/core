92908289582e23900cbd1e6e61d1ce78bc02da54
Fabian Affolter <mail@fabian-affolter.ch>
2018-01-25 18:31:15 +0100

    Fix ordering and docstring

--- homeassistant/components/iota.py
+++ homeassistant/components/iota.py
@@ -13,19 +13,21 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import load_platform
 from homeassistant.helpers.entity import Entity
 
-DOMAIN = 'iota'
-
 REQUIREMENTS = ['pyota==2.0.3']
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_IRI = 'iri'
+CONF_TESTNET = 'testnet'
+CONF_WALLET_NAME = 'name'
+CONF_WALLET_SEED = 'seed'
+CONF_WALLETS = 'wallets'
+
+DOMAIN = 'iota'
 
 IOTA_PLATFORMS = ['sensor']
 
 SCAN_INTERVAL = timedelta(minutes=10)
-
-CONF_IRI = 'iri'
-CONF_TESTNET = 'testnet'
-CONF_WALLETS = 'wallets'
-CONF_WALLET_NAME = 'name'
-CONF_WALLET_SEED = 'seed'
 
 WALLET_CONFIG = vol.Schema({
     vol.Required(CONF_WALLET_NAME): cv.string,
@@ -36,17 +38,14 @@ CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_IRI): cv.string,
         vol.Optional(CONF_TESTNET, default=False): cv.boolean,
-        vol.Required(CONF_WALLETS): vol.All(cv.ensure_list, [WALLET_CONFIG])
+        vol.Required(CONF_WALLETS): vol.All(cv.ensure_list, [WALLET_CONFIG]),
     })
 }, extra=vol.ALLOW_EXTRA)
 
-_LOGGER = logging.getLogger(__name__)
-
 
 def setup(hass, config):
-    """Setup IOTA component."""
+    """Set up the IOTA component."""
 
-    # Load platforms
     iota_config = config[DOMAIN]
     for platform in IOTA_PLATFORMS:
         load_platform(hass, platform, DOMAIN, iota_config, config)
