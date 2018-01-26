174da22ec1a7514d8509e7777d319e3dc692f3d0
c727 <crusader727@gmx.de>
2018-01-26 16:48:56 +0100

    Update based on feedback

--- homeassistant/components/custom_card.py
+++ homeassistant/components/custom_card.py
@@ -1,5 +1,5 @@
 """
-Show custom ha-cards and sate-cards on Home Assistant frontend
+Show custom full-cards (ha-cards) and sate-cards on Home Assistant frontend
 
 For more details about this component, please refer to the documentation
 at https://home-assistant.io/components/custom_card/
@@ -9,18 +9,15 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.entity_component import EntityComponent
+from aiohttp import web
+from homeassistant.components import http
 import homeassistant.helpers.config_validation as cv
 
 DOMAIN = 'custom_card'
 
-ENTITY_ID_FORMAT_HA_CARD = 'custom_ha_card' + '.{}'
-ENTITY_ID_FORMAT_STATE_CARD = 'custom_state_card' + '.{}'
-
 _LOGGER = logging.getLogger(__name__)
 
-CONF_HA_CARD = 'ha_card'
+CONF_FULL_CARD = 'full_card'
 CONF_STATE_CARD = 'state_card'
 CONF_MORE_INFO_CARD = 'more_info_card'
 CONF_CONFIG = 'config'
@@ -28,7 +25,7 @@ CONF_CONFIG = 'config'
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         cv.slug: vol.Any({
-            vol.Optional(CONF_HA_CARD): cv.string,
+            vol.Optional(CONF_FULL_CARD): cv.string,
             vol.Optional(CONF_STATE_CARD): cv.string,
             vol.Optional(CONF_MORE_INFO_CARD): cv.string,
             vol.Optional(CONF_CONFIG): cv.match_all,
@@ -40,63 +37,75 @@ CONFIG_SCHEMA = vol.Schema({
 @asyncio.coroutine
 def async_setup(hass, config):
     """Initialize custom card."""
-    component = EntityComponent(_LOGGER, DOMAIN, hass)
 
-    entities = []
+    card_configs = {}
 
     for object_id, cfg in config[DOMAIN].items():
         if not cfg:
             cfg = {}
 
-        ha_card = cfg.get(CONF_HA_CARD)
+        full_card = cfg.get(CONF_FULL_CARD)
         state_card = cfg.get(CONF_STATE_CARD)
         more_info_card = cfg.get(CONF_MORE_INFO_CARD)
         config = cfg.get(CONF_CONFIG)
 
-        if ha_card is None and state_card is None:
-            _LOGGER.error("Entity config must contain ha_card " +
+        if full_card is None and state_card is None:
+            _LOGGER.error("Entity config must contain full_card " +
                           "and/or state_card (%s)", object_id)
             return False
 
-        entities.append(CustomCard(object_id, ha_card, state_card,
-                        more_info_card, config))
+        if full_card:
+            entity_id = 'custom_full_card.{}'.format(object_id)
+            state = full_card
+        else:
+            entity_id = 'custom_sate_card.{}'.format(object_id)
+            state = state_card
 
-    if not entities:
-        return False
+        attributes = {}
+        if full_card:
+            attributes['full_card'] = full_card
+        if state_card:
+            attributes['state_card'] = state_card
+        if more_info_card:
+            attributes['more_info_card'] = more_info_card
 
-    yield from component.async_add_entities(entities)
+        if config:
+            card_configs[entity_id] = config
+
+        hass.states.async_set(entity_id, state, attributes)
+        
+    hass.http.register_view(CustomCardView(card_configs))
 
     return True
 
 
-class CustomCard(Entity):
-    """Representation of a custom card."""
+class CustomCardView(http.HomeAssistantView):
+    """API to request card config."""
 
-    def __init__(self, object_id, ha_card, state_card, more_info_card, config):
-        """Initialize a custom card."""
-        if ha_card:
-            self.entity_id = ENTITY_ID_FORMAT_HA_CARD.format(object_id)
-            self._state = ha_card
+    url = '/api/custom_card'
+    name = 'api:custom_card'
+
+    def __init__(self, card_configs):
+        self._card_configs = card_configs
+
+    @http.RequestDataValidator(vol.Schema({
+        vol.Required('entity_id'): str,
+    }))
+
+    @asyncio.coroutine
+    def post(self, request, data):
+        """Handle a config request."""
+        try:
+            config = self._card_configs[data['entity_id']]
+        except KeyError:
+            res = { "config": None }
+            state = 400
         else:
-            self.entity_id = ENTITY_ID_FORMAT_STATE_CARD.format(object_id)
-            self._state = state_card
+            res = { "config": config }
+            state = 200
 
-        self._attributes = {}
-        if ha_card:
-            self._attributes['ha_card'] = ha_card
-        if state_card:
-            self._attributes['state_card'] = state_card
-        if more_info_card:
-            self._attributes['more_info_card'] = more_info_card
-        if config:
-            self._attributes['config'] = config
-
-    @property
-    def state(self):
-        """Return card as state."""
-        return self._state
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes."""
-        return self._attributes
+        return web.Response(
+            body = str(res),
+            status = state,
+            content_type = 'application/json',
+        )
