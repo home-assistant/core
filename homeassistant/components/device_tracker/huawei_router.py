a6398292353d5bf93db08e558a15a4d87dd77694
Ville Skyttä <ville.skytta@iki.fi>
2018-01-27 17:33:33 +0200

    huawei_router: Fix documentation link (#11961)

--- homeassistant/components/device_tracker/huawei_router.py
+++ homeassistant/components/device_tracker/huawei_router.py
@@ -2,7 +2,7 @@
 Support for HUAWEI routers.
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/device_tracker.huawei/
+https://home-assistant.io/components/device_tracker.huawei_router/
 """
 import base64
 import logging
