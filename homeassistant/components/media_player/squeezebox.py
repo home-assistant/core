5bfbb5e5d1ad54064c6ad6b98b79e438d7b116d0
smoldaner <smoldaner@users.noreply.github.com>
2018-01-28 19:11:46 +0100

    Fix parameter escaping
    From rfc3986: The characters slash ("/") and question mark ("?") may represent data within the query component
    
    See https://tools.ietf.org/html/rfc3986#section-3.4

--- homeassistant/components/media_player/squeezebox.py
+++ homeassistant/components/media_player/squeezebox.py
@@ -494,5 +494,5 @@ class SqueezeBoxDevice(MediaPlayerDevice):
         all_params = [command]
         if parameters:
             for parameter in parameters:
-                all_params.append(urllib.parse.quote(parameter, safe=':='))
+                all_params.append(urllib.parse.quote(parameter, safe=':=/?'))
         return self.async_query(*all_params)
