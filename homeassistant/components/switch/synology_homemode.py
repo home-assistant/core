2e6a709c7a2075e4cb2165706ec93e13d3814e36
Philip Kleimeyer <philip.kleimeyer@googlemail.com>
2018-01-31 16:06:10 +0100

    update finish syno home mode switch

--- homeassistant/components/switch/synology_homemode.py
+++ homeassistant/components/switch/synology_homemode.py
@@ -1,5 +1,5 @@
 """
-Allows to configure a switch using RPi GPIO.
+Allows to configure a switch using Synology Home Mode.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/switch.synology_homemode/
@@ -18,8 +18,6 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-DEPENDENCIES = ['']
-
 DEFAULT_NAME = "Home Mode"
 DEFAULT_TIMEOUT = 5
 
@@ -34,53 +32,27 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up the Raspberry PI GPIO devices."""
+    """Set up the Synology Home Mode switch."""
     _timeout = config.get(CONF_TIMEOUT)
     _url = config.get(CONF_URL)
     _username = config.get(CONF_USERNAME)
     _password = config.get(CONF_PASSWORD)
-
-    try:
-
-        self._login(_url, _username, _password)
-
-    except (requests.exceptions.RequestException, ValueError):
-        _LOGGER.exception("Error when initializing SurveillanceStation")
-        return False
-    
-    add_devices(SynologyHomeModeSwitch(DEFAULT_NAME, ))
-
-def _login(host, username, password):
-    """Login request"""
-    url = '{}/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=3' + \
-          '&account={}&passwd={}!&session=SurveillanceStation' + \
-          '&format=sid'.format(host, username, password)
-
-    res = requests.post(url, data={}, timeout=5)
-
-"""
-pi@raspberrypi ~ $ curl -L "http://192.168.1.101:5000/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=3&account=XXXXXXX&passwd=XXXXXXXX&session=SurveillanceStation&format=sid"
-{"data":{"sid":"Gj.tXLURyrKZg1510MPN674502"},"success":true}
-
-
-Home Mode on
-
-pi@raspberrypi ~ $ curl -L "http://192.168.1.101:5000/webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=Switch&on=true&_sid=Gj.tXLURyrKZg1510MPN674502"
-
-
-Home Mode OFF
-
-{"success":true}pi@raspberrypi ~ $ curl -L "http://192.168.1.101:5000/webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=Switch&on=false&_sid=Gj.tXLURyrKZg1510MPN674502"
-"""
+    add_devices([SynologyHomeModeSwitch(DEFAULT_NAME, _url, _username, _password)])
 
 
 class SynologyHomeModeSwitch(ToggleEntity):
-    """Representation of a  Raspberry Pi GPIO."""
+    """Representation of Synology Home Mode Switch."""
 
-    def __init__(self, name):
-        """Initialize the pin."""
-        self._name = name or DEFAULT_NAME
-        
+    def __init__(self, name, host, username, password):
+        """Initialize the switch."""
+        self._name = name
+        self._sid = ""
+        self._host = host
+        self._username = username
+        self._password = password
+        self._state = None
+        self._login()
+
     @property
     def name(self):
         """Return the name of the switch."""
@@ -89,7 +61,10 @@ class SynologyHomeModeSwitch(ToggleEntity):
     @property
     def should_poll(self):
         """No polling needed."""
-        return False
+        return True
+
+    def update(self):
+        self._get_info()
 
     @property
     def is_on(self):
@@ -99,9 +74,61 @@ class SynologyHomeModeSwitch(ToggleEntity):
     def turn_on(self, **kwargs):
         """Turn the device on."""
         self._state = True
-        self.schedule_update_ha_state()
+        self._set_state("true")
+        #self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs):
         """Turn the device off."""
         self._state = False
-        self.schedule_update_ha_state()
+        self._set_state("false")
+        #self.schedule_update_ha_state()
+
+
+    def _login(self):
+        """Login request"""
+        try:
+            url = '{0}/webapi/auth.cgi?api=SYNO.API.Auth&method=Login' + \
+                '&version=3' + \
+                '&account={1}&passwd={2}&session=SurveillanceStation' + \
+                '&format=sid'
+
+            url = url.format(self._host, self._username, self._password)
+            res = requests.get(url, data={}, timeout=15).json()
+
+            if res['success'] == True:
+                self._sid = res['data']['sid']
+                return True
+            else:
+                return False
+        except (requests.exceptions.RequestException, ValueError):
+            _LOGGER.exception("Error when initializing SurveillanceStation")
+            return False
+
+    def _get_info(self):
+        """"Get info of Homemode"""
+        try:
+            url = '{0}/webapi/entry.cgi?' + \
+                'api=SYNO.SurveillanceStation.HomeMode' + \
+                '&version=1&method=GetInfo' + \
+                '&_sid={1}'
+
+            url = url.format(self._host, self._sid)
+
+            res = requests.get(url, data={}, timeout=15).json()
+
+            self._state = res['data']['on']
+        except (requests.exceptions.RequestException, ValueError):
+            self._state = False
+
+    def _set_state(self, mode):
+        """"Set state of Homemode"""
+        try:
+            url = '{0}/webapi/entry.cgi?' + \
+                'api=SYNO.SurveillanceStation.HomeMode' + \
+                '&version=1&method=Switch&on={1}' + \
+                '&_sid={2}'
+
+            url = url.format(self._host, mode, self._sid)
+            requests.get(url, data={}, timeout=15).json()
+        except (requests.exceptions.RequestException, ValueError):
+            self._state = False
