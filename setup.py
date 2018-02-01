be37bb14b7a2d7bf92d47d098cc4219286b45903
Alok Saboo <arsaboo@users.noreply.github.com>
2018-02-01 13:21:15 -0500

    Update jinja2 to 2.10 (#12118)

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ REQUIRES = [
     'pyyaml>=3.11,<4',
     'pytz>=2017.02',
     'pip>=8.0.3',
-    'jinja2>=2.9.6',
+    'jinja2>=2.10',
     'voluptuous==0.10.5',
     'typing>=3,<4',
     'aiohttp==2.3.7',   # If updated, check if yarl also needs an update!
