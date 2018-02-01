d8cb313df6400eaba2b26ff8e6fdd3545bd617b5
Qxlkdr <qxlkdr@gmail.com>
2018-02-01 10:51:35 +0100

    Added weather temperature information (air and road temperature) by Trafikverket API in Sweden

--- homeassistant/components/sensor/trafikverket_weatherstation.py
+++ homeassistant/components/sensor/trafikverket_weatherstation.py
@@ -1,17 +1,8 @@
 """
-Showing weather information for air and road provided by Trafikverket in Sweden.
+Weather information for air and road temperature, provided by Trafikverket
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/sensor.trafikverket_weatherstations/
-
-EXAMPLE
--------
-sensor:
-  - platform: trafikverket_weatherstations
-    name: Trafikverket WeatherStation Lanna
-    api: eXXcbXXXacXXXXc39XX3aXXX4aXX46XX
-    station: Lanna
-    type: road
+https://home-assistant.io/components/sensor.trafikverket_weatherstation/
 """
 import logging
 from datetime import timedelta
@@ -20,8 +11,8 @@ import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME, ATTR_ATTRIBUTION
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import (
+    CONF_NAME, ATTR_ATTRIBUTION, TEMP_CELSIUS)
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 
@@ -33,7 +24,8 @@ CONF_API = 'api'
 CONF_STATION = 'station'
 CONF_TYPE = 'type'
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=1)
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
+SCAN_INTERVAL = timedelta(seconds=300)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
@@ -42,6 +34,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_TYPE): vol.In(['air', 'road']),
 })
 
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
     sensor_name = config.get(CONF_NAME)
@@ -49,7 +42,9 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     sensor_station = config.get(CONF_STATION)
     sensor_type = config.get(CONF_TYPE)
 
-    add_devices([TrafikverketWeatherStation(sensor_name, sensor_api, sensor_station, sensor_type)])
+    add_devices([TrafikverketWeatherStation(
+        sensor_name, sensor_api, sensor_station, sensor_type)], True)
+
 
 class TrafikverketWeatherStation(Entity):
     """Representation of a Sensor."""
@@ -91,12 +86,11 @@ class TrafikverketWeatherStation(Entity):
 
         This is the only method that should fetch new data for Home Assistant.
         """
-
         import requests
         import json
 
-        url = 'http://api.trafikinfo.trafikverket.se/v1.3/data.json' # Set destination URL here
-        
+        url = 'http://api.trafikinfo.trafikverket.se/v1.3/data.json'
+
         if self._type == 'air':
             xml = """
             <REQUEST>
@@ -121,11 +115,15 @@ class TrafikverketWeatherStation(Entity):
             </REQUEST>"""
 
         post = requests.post(url, data=xml.encode('utf-8'))
-        loaded_json = json.loads(post.text)
 
+        # lj = loaded json
+        lj = json.loads(post.text)
+
+        # mm = measurement
+        mm = lj["RESPONSE"]["RESULT"][0]["WeatherStation"][0]["Measurement"]
         if self._type == 'air':
-            result = loaded_json["RESPONSE"]["RESULT"][0]["WeatherStation"][0]["Measurement"]["Air"]["Temp"]
+            result = mm["Air"]["Temp"]
         else:
-            result = loaded_json["RESPONSE"]["RESULT"][0]["WeatherStation"][0]["Measurement"]["Road"]["Temp"]
-        
+            result = mm["Road"]["Temp"]
+
         self._state = result
