0ed1bf1a3532d54ab2474fb15c0a636e95bdcfba
Marc Egli <marcegli@google.com>
2018-02-04 20:35:55 +0100

    Replace wake on lan with turn on script for philips TV

--- homeassistant/components/media_player/philips_js.py
+++ homeassistant/components/media_player/philips_js.py
@@ -15,10 +15,11 @@ from homeassistant.components.media_player import (
     SUPPORT_SELECT_SOURCE, SUPPORT_TURN_OFF, SUPPORT_TURN_ON,
     SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_STEP, SUPPORT_PLAY, MediaPlayerDevice)
 from homeassistant.const import (
-    CONF_HOST, CONF_NAME, CONF_MAC, STATE_OFF, STATE_ON, STATE_UNKNOWN)
+    CONF_HOST, CONF_NAME, STATE_OFF, STATE_ON, STATE_UNKNOWN)
+from homeassistant.helpers.script import Script
 from homeassistant.util import Throttle
 
-REQUIREMENTS = ['ha-philipsjs==0.0.1', 'wakeonlan==0.2.2']
+REQUIREMENTS = ['ha-philipsjs==0.0.1']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,6 +31,8 @@ SUPPORT_PHILIPS_JS = SUPPORT_TURN_OFF | SUPPORT_VOLUME_STEP | \
 SUPPORT_PHILIPS_JS_TV = SUPPORT_PHILIPS_JS | SUPPORT_NEXT_TRACK | \
                         SUPPORT_PREVIOUS_TRACK | SUPPORT_PLAY
 
+CONF_ON_ACTION = 'turn_on_action'
+
 DEFAULT_DEVICE = 'default'
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_NAME = 'Philips TV'
@@ -37,7 +40,7 @@ DEFAULT_NAME = 'Philips TV'
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST, default=DEFAULT_HOST): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_MAC): cv.string,
+    vol.Optional(CONF_ON_ACTION): cv.SCRIPT_SCHEMA,
 })
 
 
@@ -48,21 +51,21 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     name = config.get(CONF_NAME)
     host = config.get(CONF_HOST)
-    mac = config.get(CONF_MAC)
+    turn_on_action = config.get(CONF_ON_ACTION)
 
     tvapi = haphilipsjs.PhilipsTV(host)
+    on_script = Script(hass, turn_on_action) if turn_on_action else None
 
-    add_devices([PhilipsTV(tvapi, name, mac)])
+    add_devices([PhilipsTV(tvapi, name, on_script)])
 
 
 class PhilipsTV(MediaPlayerDevice):
     """Representation of a Philips TV exposing the JointSpace API."""
 
-    def __init__(self, tv, name, mac):
+    def __init__(self, tv, name, on_script):
         """Initialize the Philips TV."""
         self._tv = tv
         self._name = name
-        self._mac = mac
         self._state = STATE_UNKNOWN
         self._min_volume = None
         self._max_volume = None
@@ -76,6 +79,7 @@ class PhilipsTV(MediaPlayerDevice):
         self._source_mapping = {}
         self._watching_tv = None
         self._channel_name = None
+        self._on_script = on_script
 
     @property
     def name(self):
@@ -90,7 +94,7 @@ class PhilipsTV(MediaPlayerDevice):
     @property
     def supported_features(self):
         """Flag media player features that are supported."""
-        is_supporting_turn_on = SUPPORT_TURN_ON if self._mac else 0
+        is_supporting_turn_on = SUPPORT_TURN_ON if self._on_script else 0
         if self._watching_tv:
             return SUPPORT_PHILIPS_JS_TV | is_supporting_turn_on
         return SUPPORT_PHILIPS_JS | is_supporting_turn_on
@@ -130,9 +134,9 @@ class PhilipsTV(MediaPlayerDevice):
         return self._muted
 
     def turn_on(self):
-        """Turn off the device."""
-        from wakeonlan import wol
-        wol.send_magic_packet(self._mac)
+        """Turn on the device."""
+        if self._on_script:
+            self._on_script.run()
 
     def turn_off(self):
         """Turn off the device."""
