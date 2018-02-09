c0c1936d198d214a3aa422d1f8ce9aa14b038c61
escoand <escoand@users.noreply.github.com>
2018-02-09 23:02:25 +0100

    add not matching wildcard tests

--- tests/components/device_tracker/test_mqtt_json.py
+++ tests/components/device_tracker/test_mqtt_json.py
@@ -161,3 +161,41 @@ class TestComponentsDeviceTrackerJSONMQTT(unittest.TestCase):
         state = self.hass.states.get('device_tracker.zanzito')
         self.assertEqual(state.attributes.get('latitude'), 2.0)
         self.assertEqual(state.attributes.get('longitude'), 1.0)
+
+    def test_single_level_wildcard_topic_not_matching(self):
+        """Test not matching single level wildcard topic."""
+        dev_id = 'zanzito'
+        subscription = 'location/+/zanzito'
+        topic = 'location/zanzito'
+        location = json.dumps(LOCATION_MESSAGE)
+
+        assert setup_component(self.hass, device_tracker.DOMAIN, {
+            device_tracker.DOMAIN: {
+                CONF_PLATFORM: 'mqtt_json',
+                'devices': {dev_id: subscription}
+            }
+        })
+        fire_mqtt_message(self.hass, topic, location)
+        self.hass.block_till_done()
+        state = self.hass.states.get('device_tracker.zanzito')
+        self.assertNotEqual(state.attributes.get('latitude'), 2.0)
+        self.assertNotEqual(state.attributes.get('longitude'), 1.0)
+
+    def test_multi_level_wildcard_topic_not_matching(self):
+        """Test not matching multi level wildcard topic."""
+        dev_id = 'zanzito'
+        subscription = 'location/#'
+        topic = 'somewhere/zanzito'
+        location = json.dumps(LOCATION_MESSAGE)
+
+        assert setup_component(self.hass, device_tracker.DOMAIN, {
+            device_tracker.DOMAIN: {
+                CONF_PLATFORM: 'mqtt_json',
+                'devices': {dev_id: subscription}
+            }
+        })
+        fire_mqtt_message(self.hass, topic, location)
+        self.hass.block_till_done()
+        state = self.hass.states.get('device_tracker.zanzito')
+        self.assertNotEqual(state.attributes.get('latitude'), 2.0)
+        self.assertNotEqual(state.attributes.get('longitude'), 1.0)
