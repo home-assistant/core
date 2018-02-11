cb8a1f11e72437749cea478f80ef54b4d40ce77e
Igor Bernstein <igorbernstein@google.com>
2018-02-10 21:19:01 -0500

    wip: initial control

--- homeassistant/components/zha/const.py
+++ homeassistant/components/zha/const.py
@@ -39,6 +39,7 @@ def populate_data():
         zcl.clusters.general.OnOff: 'switch',
         zcl.clusters.measurement.TemperatureMeasurement: 'sensor',
         zcl.clusters.security.IasZone: 'binary_sensor',
+        zcl.clusters.hvac.Fan: 'fan',
     })
 
     # A map of hass components to all Zigbee clusters it could use
