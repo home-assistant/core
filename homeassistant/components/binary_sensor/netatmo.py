2e3524147cf1c20a7f043dfad94b9e52b359abb6
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-02-11 19:33:37 -0800

    Remove unique ID from netatmo (#12317)
    * Remove unique ID from netatmo
    
    * Shame platform in error message

--- homeassistant/components/binary_sensor/netatmo.py
+++ homeassistant/components/binary_sensor/netatmo.py
@@ -131,8 +131,6 @@ class NetatmoBinarySensor(BinarySensorDevice):
             self._name += ' / ' + module_name
         self._sensor_name = sensor
         self._name += ' ' + sensor
-        self._unique_id = data.camera_data.cameraByName(
-            camera=camera_name, home=home)['id']
         self._cameratype = camera_type
         self._state = None
 
@@ -140,11 +138,6 @@ class NetatmoBinarySensor(BinarySensorDevice):
     def name(self):
         """Return the name of the Netatmo device and this sensor."""
         return self._name
-
-    @property
-    def unique_id(self):
-        """Return the unique ID for this sensor."""
-        return self._unique_id
 
     @property
     def device_class(self):
