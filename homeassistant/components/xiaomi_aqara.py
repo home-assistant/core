b6a83fafd12b1ad81801540f37ff53cd2b83b9e8
Lukas Barth <lukas.barth@kit.edu>
2018-02-13 17:02:38 +0100

    Re-add removed attributes

--- homeassistant/components/xiaomi_aqara.py
+++ homeassistant/components/xiaomi_aqara.py
@@ -214,6 +214,7 @@ class XiaomiDevice(Entity):
         self._name = '{}_{}'.format(name, self._sid)
         self._write_to_hub = xiaomi_hub.write_to_hub
         self._get_from_hub = xiaomi_hub.get_from_hub
+        self._device_state_attributes = {}
         self._unique_id = DOMAIN + "_" + slugify(self._name)
         self._remove_unavailability_tracker = None
         xiaomi_hub.callbacks[self._sid].append(self._add_push_data_job)
