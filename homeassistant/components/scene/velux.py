143478029ad99df2240aec96a887ec05dc0f0604
Julius Mittenzwei <julius@mittenzwei.com>
2018-02-14 08:51:27 +0100

    fixed review comments

--- homeassistant/components/scene/velux.py
+++ homeassistant/components/scene/velux.py
@@ -5,6 +5,7 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/scene.velux/
 """
 import asyncio
+
 from homeassistant.components.scene import Scene
 from homeassistant.components.velux import _LOGGER, DATA_VELUX
 
@@ -12,22 +13,23 @@ from homeassistant.components.velux import _LOGGER, DATA_VELUX
 DEPENDENCIES = ['velux']
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_devices,
+                         discovery_info=None):
     """Set up the scenes for velux platform."""
     entities = []
     for scene in hass.data[DATA_VELUX].pyvlx.scenes:
-        entities.append(VeluxScene(hass, scene))
-    add_devices(entities)
+        entities.append(VeluxScene(scene))
+    async_add_devices(entities)
     return True
 
 
 class VeluxScene(Scene):
     """Representation of a velux scene."""
 
-    def __init__(self, hass, scene):
+    def __init__(self, scene):
         """Init velux scene."""
         _LOGGER.info("Adding VELUX scene: %s", scene)
-        self.hass = hass
         self.scene = scene
 
     @property
