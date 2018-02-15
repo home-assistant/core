HwangWonYo/home-assistant

Introduce Config Entries

homeassistant/setup.py
@@ -123,7 +123,7 @@ def _async_setup_component(hass: core.HomeAssistant,
         return False
 
     try:
-        yield from _process_deps_reqs(hass, config, domain, component)
+        yield from async_process_deps_reqs(hass, config, domain, component)
     except HomeAssistantError as err:
         log_error(str(err))
         return False
@@ -164,6 +164,9 @@ def _async_setup_component(hass: core.HomeAssistant,
                   "Disabling component.")
         loader.set_component(domain, None)
         return False
+
+    for entry in hass.config_entries.async_entries(domain):
+        yield from entry.async_setup(hass, component=component)
 
     hass.config.components.add(component.DOMAIN)
 
@@ -206,7 +209,8 @@ def async_prepare_setup_platform(hass: core.HomeAssistant, config, domain: str,
         return platform
 
     try:
-        yield from _process_deps_reqs(hass, config, platform_name, platform)
+        yield from async_process_deps_reqs(
+            hass, config, platform_name, platform)
     except HomeAssistantError as err:
         log_error(str(err))
         return None
@@ -215,7 +219,7 @@ def async_prepare_setup_platform(hass: core.HomeAssistant, config, domain: str,
 
 
 @asyncio.coroutine
-def _process_deps_reqs(hass, config, name, module):
+def async_process_deps_reqs(hass, config, name, module):
     """Process all dependencies and requirements for a module.
 
     Module is a Python module of either a component or platform.

home-assistant/core

homeassistant/setup.py
@@ -123,7 +123,7 @@ def _async_setup_component(hass: core.HomeAssistant,
         return False
 
     try:
-        yield from _process_deps_reqs(hass, config, domain, component)
+        yield from async_process_deps_reqs(hass, config, domain, component)
     except HomeAssistantError as err:
         log_error(str(err))
         return False
@@ -164,6 +164,9 @@ def _async_setup_component(hass: core.HomeAssistant,
                   "Disabling component.")
         loader.set_component(domain, None)
         return False
+
+    for entry in hass.config_entries.async_entries(domain):
+        yield from entry.async_setup(hass, component=component)
 
     hass.config.components.add(component.DOMAIN)
 
@@ -206,7 +209,8 @@ def async_prepare_setup_platform(hass: core.HomeAssistant, config, domain: str,
         return platform
 
     try:
-        yield from _process_deps_reqs(hass, config, platform_path, platform)
+        yield from async_process_deps_reqs(
+            hass, config, platform_path, platform)
     except HomeAssistantError as err:
         log_error(str(err))
         return None
@@ -215,7 +219,7 @@ def async_prepare_setup_platform(hass: core.HomeAssistant, config, domain: str,
 
 
 @asyncio.coroutine
-def _process_deps_reqs(hass, config, name, module):
+def async_process_deps_reqs(hass, config, name, module):
     """Process all dependencies and requirements for a module.
 
     Module is a Python module of either a component or platform.