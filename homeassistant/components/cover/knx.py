37d22b37e8f946dab74105594eabb4042f34b69d
Julius Mittenzwei <jm@grin.com>
2018-02-15 07:10:12 +0100

    Cleanup of knx component (#12408)

--- homeassistant/components/cover/knx.py
+++ homeassistant/components/cover/knx.py
@@ -53,9 +53,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up cover(s) for KNX platform."""
-    if DATA_KNX not in hass.data or not hass.data[DATA_KNX].initialized:
-        return
-
     if discovery_info is not None:
         async_add_devices_discovery(hass, discovery_info, async_add_devices)
     else:
