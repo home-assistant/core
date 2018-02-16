7558402d7cc8ae043f80cb2329255a414b353e83
nielstron <n.muendler@web.de>
2018-02-16 11:41:43 +0100

    Added pysyncthru to the requirements

--- homeassistant/components/sensor/syncthru.py
+++ homeassistant/components/sensor/syncthru.py
@@ -13,6 +13,10 @@ from homeassistant.const import (
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 
+REQUIREMENTS = [
+    'pysyncthru'  # for obvious reasons
+    ]
+
 _LOGGER = logging.getLogger(__name__)
 
 FRIENDLY_NAME = 'friendly_name'
@@ -24,13 +28,13 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the BLNET component"""
+
     resource = config.get(CONF_RESOURCE)
 
     if test_syncthru(resource) is None:
         _LOGGER.error("No SyncThru Printer reached under given resource")
         return False
 
-    
     sync_comp = SyncThruSensor(hass, SyncThru(resource))
     add_devices([sync_comp])
 
@@ -57,20 +61,20 @@ class SyncThruSensor(Entity):
     def state(self):
         """Return the state of the device."""
         return self._state
-    
+
     @property
     def icon(self):
         """Return the state of the device."""
         return self._icon
-    
+
     def update(self):
         """Get the latest data from SyncThru and update the state."""
         syncthru = self.syncthru
-        
+
         syncthru.update()
         self._state = syncthru.deviceState()
         self._friendly_name = syncthru.model()
-        
+
         self._attributes['output_tray'] = syncthru.outputTrayStatus()
         for key, value in syncthru.systemState().items():
             self._attributes[key] = value
@@ -80,12 +84,10 @@ class SyncThruSensor(Entity):
             self._attributes['drum_' + key] = value
         for key, value in syncthru.inputTrayStatus().items():
             self._attributes['input_tray_' + key] = value
-        
-        
+
     @property
     def device_state_attributes(self):
         """Return the state attributes of the device."""
 
         _attributes[FRIENDLY_NAME] = self._friendly_name
         return _attributes
-
