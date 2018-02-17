819855bd74c0854075c8a0be5ab4a4c05f2ca4d2
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-02-17 01:29:14 -0800

    Update voluputous (#12463)
    * Update voluputous
    
    * Fix http config
    
    * Fix optional with default=None
    
    * Optional, default=none
    
    * Fix defaults in voluptuous schemas
    
    * Fix tests
    
    * Fix update error
    
    * Lint

--- homeassistant/components/device_tracker/automatic.py
+++ homeassistant/components/device_tracker/automatic.py
@@ -49,8 +49,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_CLIENT_ID): cv.string,
     vol.Required(CONF_SECRET): cv.string,
     vol.Optional(CONF_CURRENT_LOCATION, default=False): cv.boolean,
-    vol.Optional(CONF_DEVICES, default=None):
-        vol.All(cv.ensure_list, [cv.string]),
+    vol.Optional(CONF_DEVICES): vol.All(cv.ensure_list, [cv.string]),
 })
 
 
@@ -109,7 +108,7 @@ def async_setup_scanner(hass, config, async_see, discovery_info=None):
             _write_refresh_token_to_file, hass, filename,
             session.refresh_token)
         data = AutomaticData(
-            hass, client, session, config[CONF_DEVICES], async_see)
+            hass, client, session, config.get(CONF_DEVICES), async_see)
 
         # Load the initial vehicle data
         vehicles = yield from session.get_vehicles()
--- homeassistant/components/xiaomi_aqara.py
+++ homeassistant/components/xiaomi_aqara.py
@@ -68,7 +68,7 @@ SERVICE_SCHEMA_REMOVE_DEVICE = vol.Schema({
 
 GATEWAY_CONFIG = vol.Schema({
     vol.Optional(CONF_MAC, default=None): vol.Any(GW_MAC, None),
-    vol.Optional(CONF_KEY, default=None):
+    vol.Optional(CONF_KEY):
         vol.All(cv.string, vol.Length(min=16, max=16)),
     vol.Optional(CONF_HOST): cv.string,
     vol.Optional(CONF_PORT, default=9898): cv.port,
@@ -90,11 +90,9 @@ def _fix_conf_defaults(config):
     return config
 
 
-DEFAULT_GATEWAY_CONFIG = [{CONF_MAC: None, CONF_KEY: None}]
-
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-        vol.Optional(CONF_GATEWAYS, default=DEFAULT_GATEWAY_CONFIG):
+        vol.Optional(CONF_GATEWAYS, default={}):
             vol.All(cv.ensure_list, [GATEWAY_CONFIG], [_fix_conf_defaults]),
         vol.Optional(CONF_INTERFACE, default='any'): cv.string,
         vol.Optional(CONF_DISCOVERY_RETRY, default=3): cv.positive_int
