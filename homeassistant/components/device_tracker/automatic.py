20447cd13e10e438af27a2bd647a324a2347e9ca
Adam Mills <adam@armills.info>
2018-02-17 12:04:22 -0500

    Bump aioautomatic to 0.6.5 for voluptuous 0.11

--- homeassistant/components/device_tracker/automatic.py
+++ homeassistant/components/device_tracker/automatic.py
@@ -23,7 +23,7 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import async_track_time_interval
 
-REQUIREMENTS = ['aioautomatic==0.6.4']
+REQUIREMENTS = ['aioautomatic==0.6.5']
 
 _LOGGER = logging.getLogger(__name__)
 
