6ab47f019cd392415dbe72025071004706f15ff8
Robin <robmarkcole@users.noreply.github.com>
2018-02-17 14:12:13 +0000

    Update test_filesize.py

--- tests/components/sensor/test_filesize.py
+++ tests/components/sensor/test_filesize.py
@@ -4,14 +4,9 @@ from unittest.mock import Mock, patch
 
 from homeassistant.components.sensor.filesize import CONF_FILE_PATHS
 from homeassistant.setup import setup_component
-from tests.common import get_test_home_assistant
+from tests.common import get_test_home_assistant, mock_registry
 
 PATH = '/Users/robincole/.homeassistant/home-assistant_v2.db'
-
-VALID_CONFIG = {
-    'platform': 'filesize',
-    CONF_FILE_PATHS: [PATH]
-    }
 
 
 class FakeObj():
@@ -26,21 +21,30 @@ FAKE = FakeObj(37990000, 1518126363.5514238, 33188)
 
 
 class TestFileSensor(unittest.TestCase):
-    """Test the File sensor."""
+    """Test the filesize sensor."""
 
     def setup_method(self, method):
         """Set up things to be run when tests are started."""
         self.hass = get_test_home_assistant()
-        self.config = VALID_CONFIG
+        mock_registry(self.hass)
 
     def teardown_method(self, method):
         """Stop everything that was started."""
         self.hass.stop()
 
+    @patch('os.path.isfile', Mock(return_value=True))
+    @patch('os.access', Mock(return_value=True))
     @patch('os.stat', Mock(return_value=FAKE))
     def test_filesize_class(self):
+
+        config = {
+            'sensor': {
+                'platform': 'filesize',
+                CONF_FILE_PATHS: [PATH]}
+        }
+
         self.assertTrue(
-            setup_component(self.hass, 'sensor', {'sensor': self.config}))
+            setup_component(self.hass, 'sensor', config))
 
         state = self.hass.states.get('sensor.homeassistant_v2db')
 
