08565342ec902473c0c43eabe17298ea25e27e44
cdce8p <30130371+cdce8p@users.noreply.github.com>
2018-02-17 22:54:13 +0100

    Code refactored
    * Added class HomeAccessory(Accessory)
    * Added class HomeBridge(Bridge)
    * Changed homekit imports to relative, to enable use in custom_components
    * Updated requirements
    * Added docs
    * Other smaller changes

--- homeassistant/components/homekit/__init__.py
+++ homeassistant/components/homekit/__init__.py
@@ -1,24 +1,26 @@
+"""Support for Apple Homekit.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/homekit/
+"""
 import asyncio
 import logging
 
 import voluptuous as vol
 
+from pyhap.accessory_driver import AccessoryDriver
+
 from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import (
-    CONF_NAME, CONF_IP_ADDRESS, CONF_PORT, CONF_ENTITIES, CONF_TYPE,
-    ATTR_FRIENDLY_NAME, 
+    CONF_NAME, CONF_IP_ADDRESS, CONF_PORT,
     EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP)
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_component import EntityComponent
 
-from homeassistant.components.homekit.const import (
-    NAME_TEMPERATURE, TYPE_SENSOR_TEMPERATURE, NAME_COVER, TYPE_COVER)
-from homeassistant.components.homekit.cover import Window
-from homeassistant.components.homekit.sensor import TemperatureSensor
-
-from pyhap.accessory import Bridge
-from pyhap.accessory_driver import AccessoryDriver
+from .accessories import HomeBridge
+from .covers import Window
+from .sensors import TemperatureSensor
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -26,7 +28,32 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'homekit'
 REQUIREMENTS = ['HAP-python==1.1.2']
 
+ATTR_CLASS = 'class'
+ATTR_NAME = 'name'
+
 CONF_PIN_CODE = 'pincode'
+CONF_TYPES = 'types'
+
+ALL_TYPES = {
+    'sensor_temperature': {ATTR_NAME: 'Temperature',
+                           ATTR_CLASS: TemperatureSensor},
+    'cover': {ATTR_NAME: 'Cover',
+              ATTR_CLASS: Window},
+}
+
+
+def valid_typ(value):
+    """Check if typ is valid."""
+    if value.lower() in ALL_TYPES.keys():
+        return value.lower()
+    raise vol.Invalid('Typ {} is an invalid typ.'.format(value))
+
+
+def valid_string(string):
+    """Check if name string is valid. An empty string is allowed."""
+    if string is None:
+        return None
+    return str(string)
 
 
 CONFIG_SCHEMA = vol.Schema({
@@ -35,73 +62,78 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Optional(CONF_IP_ADDRESS): cv.string,
         vol.Optional(CONF_PORT): vol.Coerce(int),
         vol.Optional(CONF_PIN_CODE): cv.string,
-        vol.Optional(CONF_ENTITIES): vol.Schema({
-            vol.Required(cv.entity_id): vol.All({
-                vol.Required(CONF_TYPE): cv.string,
-                vol.Optional(CONF_NAME): cv.string,
-            }),
-        }),
+        vol.Required(CONF_TYPES): vol.Schema({
+            vol.Required(valid_typ): vol.Schema({
+                vol.Required(cv.entity_id): valid_string,
+            })
+        })
     })
 }, extra=vol.ALLOW_EXTRA)
 
+
 @asyncio.coroutine
 def async_setup(hass, config):
+    """Setup the homekit component."""
     component = EntityComponent(_LOGGER, DOMAIN, hass)
 
     conf = config[DOMAIN]
     name = conf.get(CONF_NAME, 'homeassistant')
-    ip = conf.get(CONF_IP_ADDRESS, '127.0.0.1')
+    ip_address = conf.get(CONF_IP_ADDRESS, '127.0.0.1')
     port = conf.get(CONF_PORT, 51826)
     pin = conf.get(CONF_PIN_CODE, '123-45-678')
-    entities = conf.get(CONF_ENTITIES)
+    types = conf.get(CONF_TYPES)
 
-    homekit_bridge = []
-    homekit_bridge.append(HomekitBridge(hass, name, ip, port, pin, entities))
-
-    yield from component.async_add_entities(homekit_bridge)
+    yield from component.async_add_entities([
+        Homekit(hass, name, ip_address, port, pin, types)])
     return True
 
 
-class HomekitBridge(Entity):
+class Homekit(Entity):
+    """Class to handle all actions between homekit and Home Assistant."""
 
-    def __init__(self, hass, name, ip, port, pin, entities):
+    def __init__(self, hass, name, ip_address, port, pin, types):
+        """Initialize a homekit entity."""
         self._hass = hass
         self._name = name
-        self._ip = ip
+        self._ip_address = ip_address
         self._port = port
         self._pin = str.encode(pin)
-        self._entities = entities
+        self._types = types
         self.path = self._hass.config.path('accessory.state')
-        self.bridge = Bridge(self._name, pincode=self._pin)
+        self.bridge = None
         self.driver = None
 
-    def setup_accessories(self):
-        for entity_id, desc in self._entities.items():
-            if desc[CONF_TYPE] == TYPE_SENSOR_TEMPERATURE:
-                name = desc.get(CONF_NAME, NAME_TEMPERATURE)
-                acc = TemperatureSensor(self._hass, _LOGGER, entity_id,
-                                        display_name=name)
-            elif desc[CONF_TYPE] == TYPE_COVER:
-                name = desc.get(CONF_NAME, NAME_COVER)
-                acc = Window(self._hass, _LOGGER, entity_id, display_name=name)
-            else:
-                continue
-            self.bridge.add_accessory(acc)
+    def setup_bridge(self):
+        """Setup the bridge component to track all accessories."""
+        self.bridge = HomeBridge(self._name, pincode=self._pin)
+        self.bridge.set_accessory_info('homekit.bridge')
 
+    def setup_accessories(self):
+        """Setup all accessories to be available in homekit."""
+        for typ, entities in self._types.items():
+            for entity_id, name in entities.items():
+                if name is None:
+                    name = ALL_TYPES[typ][ATTR_NAME]
+                acc = ALL_TYPES[typ][ATTR_CLASS](
+                    self.hass, entity_id, display_name=name)
+                self.bridge.add_accessory(acc)
 
     @asyncio.coroutine
     def async_added_to_hass(self):
-
+        """Register callbacks."""
         @callback
         def start_driver(event):
+            """Start the accessory driver after HA start is called."""
+            self.setup_bridge()
             self.setup_accessories()
             _LOGGER.debug('Driver start')
             self.driver = AccessoryDriver(self.bridge, self._port,
-                                          self._ip, self.path)
+                                          self._ip_address, self.path)
             self.driver.start()
 
         @callback
         def homeassistant_stop(event):
+            """Stop the accessory drive after HA stop is called."""
             _LOGGER.debug('Driver stop')
             self.driver.stop()
 
--- homeassistant/components/homekit/covers.py
+++ homeassistant/components/homekit/covers.py
@@ -1,26 +1,34 @@
-import time
+"""Class to hold all cover accessories."""
+import logging
 
 from homeassistant.core import callback
 from homeassistant.helpers.event import async_track_state_change
 
-from homeassistant.components.homekit.const import (
-    SERVICES_ACCESSORY_INFO, SERVICES_WINDOW_COVERING,
-    CHAR_MODEL, CHAR_MANUFACTURER, CHAR_SERIAL_NUMBER,
-    CHAR_CURRENT_POSITION, CHAR_TARGET_POSITION, CHAR_POSITION_STATE,
-    HOMEASSISTANT)
-
-from pyhap.loader import get_serv_loader
-from pyhap.accessory import Accessory, Category
+from .accessories import HomeAccessory
+from .const import (
+    SERVICES_WINDOW_COVERING, CHAR_CURRENT_POSITION,
+    CHAR_TARGET_POSITION, CHAR_POSITION_STATE)
 
 
-class Window(Accessory):
-    category = Category.WINDOW
+_LOGGER = logging.getLogger(__name__)
 
-    def __init__(self, hass, _LOGGER, entity_id, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+
+class Window(HomeAccessory):
+    """Generate a Window accessory for a cover entity.
+
+    The cover entity must support: set_cover_position.
+    """
+
+    def __init__(self, hass, entity_id, display_name):
+        """Initialize a Window accessory object."""
+        super().__init__(display_name)
+        self.set_category(self.ALL_CATEGORIES.WINDOW)
+        self.set_services(SERVICES_WINDOW_COVERING)
+        self.set_accessory_info(entity_id)
+
         self._hass = hass
-        self._LOGGER = _LOGGER
         self._entity_id = entity_id
+
         self.current_position = None
         self.homekit_target = None
 
@@ -34,22 +42,8 @@ class Window(Accessory):
 
         self.char_target_position.setter_callback = self.move_cover
 
-    def _set_services(self):
-        super()._set_services()
-        self.add_service(get_serv_loader().get(SERVICES_WINDOW_COVERING))
-
-    def set_default_info(self):
-        service_info = self.get_service(SERVICES_ACCESSORY_INFO)
-        service_info.get_characteristic(CHAR_MODEL) \
-            .set_value(self._entity_id)
-        service_info.get_characteristic(CHAR_MANUFACTURER) \
-            .set_value(HOMEASSISTANT)
-        service_info.get_characteristic(CHAR_SERIAL_NUMBER) \
-            .set_value('0000')
-
     def run(self):
-        self.set_default_info()
-
+        """Method called be object after driver is started."""
         state = self._hass.states.get(self._entity_id)
         self.update_cover_position(new_state=state)
 
@@ -59,29 +53,32 @@ class Window(Accessory):
         # self.debug_characteristics()
 
     def debug_characteristics(self):
+        """Method to debug characteristics."""
         while not self.run_sentinel.wait(5):
-            self._LOGGER.debug("%s: Target: %d", self._entity_id,
-                self.char_target_position.get_value())
-            self._LOGGER.debug("%s: Current: %d", self._entity_id,
-                self.char_current_position.get_value())
-            self._LOGGER.debug("%s: PositionState: %d", self._entity_id,
-                self.char_position_state.get_value())
+            _LOGGER.debug("%s: Target: %d", self._entity_id,
+                          self.char_target_position.get_value())
+            _LOGGER.debug("%s: Current: %d", self._entity_id,
+                          self.char_current_position.get_value())
+            _LOGGER.debug("%s: PositionState: %d", self._entity_id,
+                          self.char_position_state.get_value())
 
     def move_cover(self, value):
+        """Move cover to value if call came from homekit."""
         if value != self.current_position:
-            self._LOGGER.debug("%s: Set position to %d", self._entity_id, value)
+            _LOGGER.debug("%s: Set position to %d", self._entity_id, value)
             self.homekit_target = value
             if value > self.current_position:
                 self.char_position_state.set_value(1)
             elif value < self.current_position:
                 self.char_position_state.set_value(0)
             self._hass.services.call(
-                'cover', 'set_cover_position', 
+                'cover', 'set_cover_position',
                 {'entity_id': self._entity_id, 'position': value})
 
     @callback
     def update_cover_position(self, entity_id=None, old_state=None,
                               new_state=None):
+        """Update cover position after state changed."""
         if new_state is None:
             return
 
@@ -89,7 +86,7 @@ class Window(Accessory):
         self.char_current_position.set_value(self.current_position)
 
         if self.homekit_target is None or \
-            abs(self.current_position - self.homekit_target) < 6:
+                abs(self.current_position - self.homekit_target) < 6:
             self.char_target_position.set_value(self.current_position)
             self.char_position_state.set_value(2)
             self.homekit_target = None
