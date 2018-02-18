6ff5c0ee1486c7443de7ac54435be199849b52a7
Gerrit Beine <gerrit.beine@gmx.de>
2018-02-18 11:26:54 +0100

    Fixed docstring (https://github.com/home-assistant/home-assistant/pull/11446#discussion_r165776934)

--- homeassistant/components/sensor/fronius.py
+++ homeassistant/components/sensor/fronius.py
@@ -1,9 +1,9 @@
-'''
+"""
 Support for Fronius devices.
 
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/sensor.fronius/
-'''
+"""
 import asyncio
 import logging
 from datetime import timedelta
