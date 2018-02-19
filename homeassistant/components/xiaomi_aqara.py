d86eebb1989eb78a1b7886b92de76fc03b32bcbe
Lukas Barth <lukas.barth@kit.edu>
2018-02-19 12:19:44 +0100

    Use type or data key

--- homeassistant/components/xiaomi_aqara.py
+++ homeassistant/components/xiaomi_aqara.py
@@ -206,20 +206,26 @@ def setup(hass, config):
 class XiaomiDevice(Entity):
     """Representation a base Xiaomi device."""
 
-    def __init__(self, device, name, xiaomi_hub):
+    def __init__(self, device, device_type, xiaomi_hub):
         """Initialize the Xiaomi device."""
         self._state = None
         self._is_available = True
         self._sid = device['sid']
-        self._name = '{}_{}'.format(name, self._sid)
+        self._name = '{}_{}'.format(device_type, self._sid)
+        self._type = device_type
         self._write_to_hub = xiaomi_hub.write_to_hub
         self._get_from_hub = xiaomi_hub.get_from_hub
         self._device_state_attributes = {}
-        self._unique_id = slugify(self._name)
         self._remove_unavailability_tracker = None
         xiaomi_hub.callbacks[self._sid].append(self._add_push_data_job)
         self.parse_data(device['data'], device['raw_data'])
         self.parse_voltage(device['data'])
+
+        if hasattr(self, '_data_key'):
+            self._unique_id = slugify("{}-{}".format(self._data_key,
+                                                     self._sid))
+        else:
+            self._unique_id = slugify("{}-{}".format(self._type, self._sid))
 
     def _add_push_data_job(self, *args):
         self.hass.add_job(self.push_data, *args)
