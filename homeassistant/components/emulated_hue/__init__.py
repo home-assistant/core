0271a8628422d781046a71387c7b413cce96451b
Marcus <marcus@phase.net>
2018-02-21 01:00:22 +1100

    Update __init__.py
    add HueGroupView for logitech pop

--- homeassistant/components/emulated_hue/__init__.py
+++ homeassistant/components/emulated_hue/__init__.py
@@ -21,7 +21,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.util.json import load_json, save_json
 from .hue_api import (
     HueUsernameView, HueAllLightsStateView, HueOneLightStateView,
-    HueOneLightChangeView)
+    HueOneLightChangeView, HueGroupView)
 from .upnp import DescriptionXmlView, UPNPResponderThread
 
 DOMAIN = 'emulated_hue'
@@ -105,7 +105,8 @@ def setup(hass, yaml_config):
     server.register_view(HueAllLightsStateView(config))
     server.register_view(HueOneLightStateView(config))
     server.register_view(HueOneLightChangeView(config))
-
+    server.register_view(HueGroupView(config))
+    
     upnp_listener = UPNPResponderThread(
         config.host_ip_addr, config.listen_port,
         config.upnp_bind_multicast, config.advertise_ip,
