02afe5e53df48eee35fcfd08af6280eab739454e
Diogo Gomes <dgomes@av.it.pt>
2018-02-20 19:29:44 +0000

    Removing asyncio.coroutine syntax

--- homeassistant/components/sensor/filter.py
+++ homeassistant/components/sensor/filter.py
@@ -61,8 +61,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-@asyncio.coroutine
-def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the Filter sensor."""
     entity_id = config.get(CONF_ENTITY_ID)
     filter_name = config.get(CONF_FILTER_NAME)
