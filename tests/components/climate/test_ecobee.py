80b3aa8c77cdd8c534cd863f7fe30bd61e90a8f8
uchagani <umair.chagani@manheim.com>
2018-02-22 17:43:17 -0500

    more hounds

--- tests/components/climate/test_ecobee.py
+++ tests/components/climate/test_ecobee.py
@@ -463,4 +463,3 @@ class TestEcobee(unittest.TestCase):
         self.thermostat.set_fan_mode('auto')
         self.data.ecobee.set_fan_mode.assert_has_calls(
             [mock.call(1, 'auto', 20, 40, 'nextTransition')])
-        
