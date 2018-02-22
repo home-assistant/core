97fdcbc108b9c201bef5ab5b8fc565cc9d1f37c4
Gerrit Beine <gerrit.beine@gmx.de>
2018-02-22 09:36:37 +0100

    Remove config logging (https://github.com/home-assistant/home-assistant/pull/11446#discussion_r168968748)

--- homeassistant/components/sensor/fronius.py
+++ homeassistant/components/sensor/fronius.py
@@ -51,7 +51,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up of Fronius platform."""
-    _LOGGER.debug(config)
     import pyfronius
 
     session = async_get_clientsession(hass)
