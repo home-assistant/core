30617a928fd18fae4b269d7636cbd9109c489efc
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-02-22 23:29:42 -0800

    Fix voluptuous breaking change things (#12611)
    * Fix voluptuous breaking change things
    
    * Change xiaomi aqara back

--- homeassistant/components/media_player/braviatv_psk.py
+++ homeassistant/components/media_player/braviatv_psk.py
@@ -42,7 +42,7 @@ TV_NO_INFO = 'No info: TV resumed after pause'
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
     vol.Required(CONF_PSK): cv.string,
-    vol.Optional(CONF_MAC, default=None): cv.string,
+    vol.Optional(CONF_MAC): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_AMP, default=False): cv.boolean,
     vol.Optional(CONF_ANDROID, default=True): cv.boolean,
