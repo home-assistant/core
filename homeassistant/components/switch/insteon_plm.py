39046c9ac86fa748b59be33fc6c2ad104910adc2
Tom Harris <tom@harrisnj.net>
2018-02-23 14:23:27 -0500

    Pylint clean up

--- homeassistant/components/switch/insteon_plm.py
+++ homeassistant/components/switch/insteon_plm.py
@@ -7,9 +7,8 @@ https://home-assistant.io/components/switch.insteon_plm/
 import asyncio
 import logging
 
-from homeassistant.core import callback
-from homeassistant.components.switch import (SwitchDevice)
-from homeassistant.loader import get_component
+from homeassistant.components.insteon_plm import InsteonPLMEntity
+from homeassistant.components.switch import SwitchDevice
 
 DEPENDENCIES = ['insteon_plm']
 
@@ -19,10 +18,7 @@ _LOGGER = logging.getLogger(__name__)
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the INSTEON PLM device class for the hass platform."""
-    entities = []
     plm = hass.data['insteon_plm']
-    _LOGGER.debug("Got here switch")
-    _LOGGER.debug(discovery_info)
 
     address = discovery_info['address']
     device = plm.devices[address]
@@ -30,66 +26,29 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
 
     state_name = device.states[state_key].name
 
-    _LOGGER.debug('Adding device %s with state name %s to Switch platform.', 
+    _LOGGER.debug('Adding device %s with state name %s to Switch platform.',
                   device.address.hex, device.states[state_key].name)
 
+    new_entity = None
     if state_name in ['lightOnOff', 'outletTopOnOff', 'outletBottomOnOff']:
-        entities.append(InsteonPLMSwitchDevice(device, state_key))
+        new_entity = InsteonPLMSwitchDevice(device, state_key)
     elif state_name == 'openClosedRelay':
-        entities.append(InsteonPLMOpenClosedDevice(device, state_key))
+        new_entity = InsteonPLMOpenClosedDevice(device, state_key)
 
-    async_add_devices(entities)
+    _LOGGER.debug('Created Switch device with address %s', new_entity.address)
+
+    if new_entity is not None:
+        async_add_devices([new_entity])
 
 
-class InsteonPLMSwitchDevice(SwitchDevice):
+class InsteonPLMSwitchDevice(InsteonPLMEntity, SwitchDevice):
     """A Class for an Insteon device."""
-
-    def __init__(self, device, state_key):
-        """Initialize the switch."""
-        self._insteon_device_state = device.states[state_key]
-        self._insteon_device = device
-
-    @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
-
-    @property
-    def address(self):
-        """Return the address of the node."""
-        return self._insteon_device.address.human
-
-    def group(self):
-        """Return the INSTEON group that the entity responds to."""
-        return self._insteon_device_state.group
-
-    @property
-    def name(self):
-        """Return the name of the node (used for Entity_ID)."""
-        name = ''
-        if self._insteon_device_state.group == 0x01:
-            name = self._insteon_device.id
-        else:
-            name = '{:s}_{:d}'.format(self._insteon_device.id,
-                                      self._insteon_device_state.group)
-        return name
 
     @property
     def is_on(self):
         """Return the boolean response if the node is on."""
         onlevel = self._insteon_device_state.value
         return bool(onlevel)
-
-    @property
-    def device_state_attributes(self):
-        """Provide attributes for display on device card."""
-        insteon_plm = get_component('insteon_plm')
-        return insteon_plm.common_attributes(self)
-
-    @callback
-    def async_switch_update(self, deviceid, statename, val):
-        """Receive notification from transport that new data exists."""
-        self.async_schedule_update_ha_state()
 
     @asyncio.coroutine
     def async_turn_on(self, **kwargs):
@@ -101,58 +60,9 @@ class InsteonPLMSwitchDevice(SwitchDevice):
         """Turn device off."""
         self._insteon_device_state.off()
 
-    @asyncio.coroutine
-    def async_added_to_hass(self):
-        """Register INSTEON update events."""
-        self._insteon_device_state.register_updates(self.async_switch_update)
 
-
-class InsteonPLMOpenClosedDevice(SwitchDevice):
+class InsteonPLMOpenClosedDevice(InsteonPLMEntity, SwitchDevice):
     """A Class for an Insteon device."""
-
-    def __init__(self, device, state_key):
-        """Initialize the switch."""
-        self._insteon_device_state = device.states[state_key]
-        self._insteon_device = device
-
-    @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
-
-    @property
-    def address(self):
-        """Return the address of the node."""
-        return self._insteon_device.address.human
-
-    @property
-    def name(self):
-        """Return the name of the node (used for Entity_ID)."""
-        name = ''
-        if self._insteon_device_state.group == 0x01:
-            name = self._insteon_device.id
-        else:
-            name = '{:s}_{:d}'.format(self._insteon_device.id,
-                                      self._insteon_device_state.group)
-        return name
-
-    @property
-    def is_on(self):
-        """Return the boolean response if the node is on."""
-        onlevel = self._insteon_device_state.value
-        return bool(onlevel)
-
-    @property
-    def device_state_attributes(self):
-        """Provide attributes for display on device card."""
-        insteon_plm = get_component('insteon_plm')
-        return insteon_plm.common_attributes(self._insteon_device,
-                                             self._insteon_device_state)
-
-    @callback
-    def async_relay_update(self, deviceid, statename, val):
-        """Receive notification from transport that new data exists."""
-        self.async_schedule_update_ha_state()
 
     @asyncio.coroutine
     def async_turn_on(self, **kwargs):
@@ -163,8 +73,3 @@ class InsteonPLMOpenClosedDevice(SwitchDevice):
     def async_turn_off(self, **kwargs):
         """Turn device off."""
         self._insteon_device_state.close()
-
-    @asyncio.coroutine
-    def async_added_to_hass(self):
-        """Register INSTEON update events."""
-        self._insteon_device_state.register_updates(self.async_relay_update)
