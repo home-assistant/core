851bd7819b541e03b7cf2ac2d20e9e3c35319387
Teemu Rytilahti <tpr@iki.fi>
2018-02-23 22:11:36 +0100

    use async defs and awaits

--- homeassistant/components/media_player/songpal.py
+++ homeassistant/components/media_player/songpal.py
@@ -5,7 +5,6 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/media_player.songpal/
 """
 import logging
-import asyncio
 
 import voluptuous as vol
 from typing import List  # noqa: F401
@@ -39,8 +38,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-@asyncio.coroutine
-def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the Songpal platform."""
     devices = []  # type: List[SongpalDevice]
     if discovery_info is not None:
@@ -54,8 +52,7 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
 
     async_add_devices(devices, True)
 
-    @asyncio.coroutine
-    def async_service_handler(service):
+    async def async_service_handler(service):
         params = {key: value for key, value in service.data.items()
                   if key != ATTR_ENTITY_ID}
         entity_id = service.data.get("entity_id", None)
@@ -64,8 +61,8 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
 
         for device in devices:
             if entity_id is None or device.entity_id == entity_id:
-                yield from device.async_set_sound_setting(params[PARAM_NAME],
-                                                          params[PARAM_VALUE])
+                await device.async_set_sound_setting(params[PARAM_NAME],
+                                                    params[PARAM_VALUE])
 
     schema = vol.Schema({vol.Required(PARAM_NAME): cv.string,
                          vol.Required(PARAM_VALUE): cv.string})
@@ -116,28 +113,26 @@ class SongpalDevice(MediaPlayerDevice):
         """Property for accessing the device handle."""
         return self._dev
 
-    @asyncio.coroutine
-    def async_set_sound_setting(self, name, value):
+    async def async_set_sound_setting(self, name, value):
         """Change a setting on the device."""
-        res = yield from self.dev.set_sound_settings(name, value)
+        res = await self.dev.set_sound_settings(name, value)
 
         return res
 
-    @asyncio.coroutine
-    def async_update(self):
+    async def async_update(self):
         """Fetch updates from the device."""
         from songpal import SongpalException
         if not self._initialized:
             try:
-                yield from self._dev.get_supported_methods()
-                self._sysinfo = yield from self.dev.get_system_info()
+                await self._dev.get_supported_methods()
+                self._sysinfo = await self.dev.get_system_info()
                 self._initialized = True
             except SongpalException as ex:
                 _LOGGER.error("Unable to get methods from songpal: %s", ex)
                 raise PlatformNotReady
 
         try:
-            volumes = yield from self.dev.get_volume_information()
+            volumes = await self.dev.get_volume_information()
             if len(volumes) == 0:
                 _LOGGER.error("Got no volume controls, bailing out")
                 return
@@ -154,11 +149,11 @@ class SongpalDevice(MediaPlayerDevice):
             self._volume_control = vol
             self._is_muted = self._volume_control.is_muted
 
-            status = yield from self.dev.get_power()
+            status = await self.dev.get_power()
             self._state = status.status
             _LOGGER.debug("Got state: %s" % status)
 
-            inputs = yield from self.dev.get_inputs()
+            inputs = await self.dev.get_inputs()
             _LOGGER.debug("Got ins: %s" % inputs)
             self._sources = inputs
 
@@ -169,12 +164,11 @@ class SongpalDevice(MediaPlayerDevice):
                 _LOGGER.error("Got an exception %s", ex, exc_info=True)
             self._available = False
 
-    @asyncio.coroutine
-    def async_select_source(self, source):
+    async def async_select_source(self, source):
         """Select source."""
         for out in self._sources:
             if out.title == source:
-                yield from out.activate()
+                await out.activate()
                 return
 
         _LOGGER.error("Unable to find output: %s" % source)
@@ -208,38 +202,32 @@ class SongpalDevice(MediaPlayerDevice):
         _LOGGER.debug("Current volume: %s" % volume)
         return volume
 
-    @asyncio.coroutine
-    def async_set_volume_level(self, volume):
+    async def async_set_volume_level(self, volume):
         """Set volume level."""
         vol = int(volume * self._volume_max)
         _LOGGER.debug("Setting volume to %s" % vol)
-        return self._volume_control.set_volume(vol)
+        return await self._volume_control.set_volume(vol)
 
-    @asyncio.coroutine
-    def async_volume_up(self):
+    async def async_volume_up(self):
         """Set volume up."""
         return self._volume_control.set_volume("+1")
 
-    @asyncio.coroutine
-    def async_volume_down(self):
+    async def async_volume_down(self):
         """Set volume down."""
         return self._volume_control.set_volume("-1")
 
-    @asyncio.coroutine
-    def async_turn_on(self):
+    async def async_turn_on(self):
         """Turn the device on."""
-        return self.dev.set_power(True)
+        return await self.dev.set_power(True)
 
-    @asyncio.coroutine
-    def async_turn_off(self):
+    async def async_turn_off(self):
         """Turn the device off."""
-        return self.dev.set_power(False)
+        return await self.dev.set_power(False)
 
-    @asyncio.coroutine
-    def async_mute_volume(self, mute):
+    async def async_mute_volume(self, mute):
         """Mute or unmute the device."""
         _LOGGER.debug("Set mute: %s" % mute)
-        return self._volume_control.set_mute(mute)
+        return await self._volume_control.set_mute(mute)
 
     @property
     def is_volume_muted(self):
