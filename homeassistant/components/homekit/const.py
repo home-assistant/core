a4f6dd753ea160658c31510573b6076f1321c7e7
cdce8p <30130371+cdce8p@users.noreply.github.com>
2018-02-26 04:27:40 +0100

    Homekit Update, Support for TempSensor (°F) (#12676)
    * Changed version of "HAP-python" to "v1.1.7"
    
    * Updated acc file to simplify init calls
    
    * Code refactored and '°F' temp Sensors added
    
    * Changed call to 'HomeAccessory' and 'HomeBridge'
    * Extended function of 'add_preload_service' to add additional characteristics
    * Added function to override characteristic property values
    
    * TemperatureSensor
      * Added unit
      * Added calc_temperature
    
    * Updated tests

--- homeassistant/components/homekit/const.py
+++ homeassistant/components/homekit/const.py
@@ -2,17 +2,20 @@
 MANUFACTURER = 'HomeAssistant'
 
 # Service: AccessoryInfomation
-SERVICES_ACCESSORY_INFO = 'AccessoryInformation'
+SERV_ACCESSORY_INFO = 'AccessoryInformation'
 CHAR_MODEL = 'Model'
 CHAR_MANUFACTURER = 'Manufacturer'
 CHAR_SERIAL_NUMBER = 'SerialNumber'
 
 # Service: TemperatureSensor
-SERVICES_TEMPERATURE_SENSOR = 'TemperatureSensor'
+SERV_TEMPERATURE_SENSOR = 'TemperatureSensor'
 CHAR_CURRENT_TEMPERATURE = 'CurrentTemperature'
 
 # Service: WindowCovering
-SERVICES_WINDOW_COVERING = 'WindowCovering'
+SERV_WINDOW_COVERING = 'WindowCovering'
 CHAR_CURRENT_POSITION = 'CurrentPosition'
 CHAR_TARGET_POSITION = 'TargetPosition'
 CHAR_POSITION_STATE = 'PositionState'
+
+# Properties
+PROP_CELSIUS = {'minValue': -273, 'maxValue': 999}
