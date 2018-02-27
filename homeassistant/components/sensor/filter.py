7bf3edac4aec933bb823775ee01c2b58f0010d80
Diogo Gomes <dgomes@av.it.pt>
2018-02-27 22:35:48 +0000

    Revert "Precision handling"
    This reverts commit f4abdd37021380d090b93a5c6b6f9e39cd59bc46.

--- homeassistant/components/sensor/filter.py
+++ homeassistant/components/sensor/filter.py
@@ -232,10 +232,7 @@ class Filter(object):
         """Implement a common interface for filters."""
         filtered = self._filter_state(new_state)
         if self.precision is not None:
-            if self.precision < 1:
-                filtered = round(filtered / self.precision) * self.precision
-            else:
-                filtered = round(filtered, self.precision)
+            filtered = round(filtered, self.precision)
         self.states.append(filtered)
         return filtered
 
