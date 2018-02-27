dbba32f8c92bae5ec52c5378ebfa124b44a1ed13
Sebastian Muszynski <s.muszynski@constructiva.de>
2018-02-27 08:51:54 +0100

    Use a unique data key per domain.

--- homeassistant/components/fan/xiaomi_miio.py
+++ homeassistant/components/fan/xiaomi_miio.py
@@ -21,7 +21,7 @@ import homeassistant.helpers.config_validation as cv
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = 'Xiaomi Miio Device'
-PLATFORM = 'xiaomi_miio'
+DATA_KEY = 'fan.xiaomi_miio'
 
 CONF_MODEL = 'model'
 MODEL_AIRPURIFIER_PRO = 'zhimi.airpurifier.v6'
@@ -202,8 +202,8 @@ SERVICE_TO_METHOD = {
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the miio fan device from config."""
     from miio import Device, DeviceException
-    if PLATFORM not in hass.data:
-        hass.data[PLATFORM] = {}
+    if DATA_KEY not in hass.data:
+        hass.data[DATA_KEY] = {}
 
     host = config.get(CONF_HOST)
     name = config.get(CONF_NAME)
@@ -239,7 +239,7 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
             'and provide the following data: %s', model)
         return False
 
-    hass.data[PLATFORM][host] = device
+    hass.data[DATA_KEY][host] = device
     async_add_devices([device], update_before_add=True)
 
     @asyncio.coroutine
@@ -250,10 +250,10 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
                   if key != ATTR_ENTITY_ID}
         entity_ids = service.data.get(ATTR_ENTITY_ID)
         if entity_ids:
-            devices = [device for device in hass.data[PLATFORM].values() if
+            devices = [device for device in hass.data[DATA_KEY].values() if
                        device.entity_id in entity_ids]
         else:
-            devices = hass.data[PLATFORM].values()
+            devices = hass.data[DATA_KEY].values()
 
         update_tasks = []
         for device in devices:
