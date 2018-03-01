944194bf9244efb8aaba94ab358b59031dae107a
cdce8p <30130371+cdce8p@users.noreply.github.com>
2018-03-02 00:20:02 +0100

    Improved Homekit tests (#12800)
    * Added test for temperature fahrenheit
    
    * Restructured tests to use more mocks
    
    * Rearanged homekit constants
    
    * Improved 'test_homekit_class'
    
    * Added import statements
    
    * Fix Pylint Test errors

--- homeassistant/components/homekit/const.py
+++ homeassistant/components/homekit/const.py
@@ -1,21 +1,24 @@
 """Constants used be the HomeKit component."""
 MANUFACTURER = 'HomeAssistant'
 
-# Service: AccessoryInfomation
+# Services
 SERV_ACCESSORY_INFO = 'AccessoryInformation'
-CHAR_MODEL = 'Model'
-CHAR_MANUFACTURER = 'Manufacturer'
-CHAR_SERIAL_NUMBER = 'SerialNumber'
-
-# Service: TemperatureSensor
+SERV_BRIDGING_STATE = 'BridgingState'
 SERV_TEMPERATURE_SENSOR = 'TemperatureSensor'
-CHAR_CURRENT_TEMPERATURE = 'CurrentTemperature'
-
-# Service: WindowCovering
 SERV_WINDOW_COVERING = 'WindowCovering'
+
+# Characteristics
+CHAR_ACC_IDENTIFIER = 'AccessoryIdentifier'
+CHAR_CATEGORY = 'Category'
 CHAR_CURRENT_POSITION = 'CurrentPosition'
-CHAR_TARGET_POSITION = 'TargetPosition'
+CHAR_CURRENT_TEMPERATURE = 'CurrentTemperature'
+CHAR_LINK_QUALITY = 'LinkQuality'
+CHAR_MANUFACTURER = 'Manufacturer'
+CHAR_MODEL = 'Model'
 CHAR_POSITION_STATE = 'PositionState'
+CHAR_REACHABLE = 'Reachable'
+CHAR_SERIAL_NUMBER = 'SerialNumber'
+CHAR_TARGET_POSITION = 'TargetPosition'
 
 # Properties
 PROP_CELSIUS = {'minValue': -273, 'maxValue': 999}
