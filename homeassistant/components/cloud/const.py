780f85e898ebaedc3e7e96570d1a3f159a3d7ea5
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-03-02 09:57:03 -0800

    Handle expired refresh token better

--- homeassistant/components/cloud/const.py
+++ homeassistant/components/cloud/const.py
@@ -16,3 +16,9 @@ MESSAGE_EXPIRATION = """
 It looks like your Home Assistant Cloud subscription has expired. Please check
 your [account page](/config/cloud/account) to continue using the service.
 """
+
+MESSAGE_AUTH_FAIL = """
+You have been logged out of Home Assistant Cloud because we have been unable
+to verify your credentials. Please [log in](/config/cloud) again to continue
+using the service.
+"""
