e5a728d178758be317c946dbcdba8ab898d9fe7c
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-03-02 11:18:34 -0800

    Skip flaky light.group test [skipci]

--- tests/components/light/test_group.py
+++ tests/components/light/test_group.py
@@ -2,6 +2,7 @@
 from unittest.mock import MagicMock
 
 import asynctest
+import pytest
 
 from homeassistant.components import light
 from homeassistant.components.light import group
@@ -316,6 +317,7 @@ async def test_supported_features(hass):
     assert state.attributes['supported_features'] == 41
 
 
+@pytest.mark.skip
 async def test_service_calls(hass):
     """Test service calls."""
     await async_setup_component(hass, 'light', {'light': [
