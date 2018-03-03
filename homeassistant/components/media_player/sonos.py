96249dde91a1b62666b44dac1659a34265b682f9
Anders Melchiorsen <amelchio@nogoto.net>
2018-03-03 23:17:20 +0100

    Additional radio schemes for sonos

--- homeassistant/components/media_player/sonos.py
+++ homeassistant/components/media_player/sonos.py
@@ -323,8 +323,10 @@ def _timespan_secs(timespan):
 
 def _is_radio_uri(uri):
     """Return whether the URI is a radio stream."""
-    return uri.startswith('x-rincon-mp3radio:') or \
-        uri.startswith('x-sonosapi-stream:')
+    radio_schemes = (
+        'x-rincon-mp3radio:', 'x-sonosapi-stream:', 'x-sonosapi-radio:',
+        'hls-radio:')
+    return uri.startswith(radio_schemes)
 
 
 class SonosDevice(MediaPlayerDevice):
