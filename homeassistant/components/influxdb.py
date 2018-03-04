d418355d4d24e2592920847ac61e1cbaf3d6501e
Anders Melchiorsen <amelchio@nogoto.net>
2018-03-04 21:01:16 +0100

    InfluxDB cleanups (#12903)
    * Close influxdb on shutdown
    
    * Ignore inf as an influxdb value
    
    * Remove deprecated CONF_RETRY_QUEUE

--- homeassistant/components/influxdb.py
+++ homeassistant/components/influxdb.py
@@ -35,7 +35,6 @@ CONF_COMPONENT_CONFIG = 'component_config'
 CONF_COMPONENT_CONFIG_GLOB = 'component_config_glob'
 CONF_COMPONENT_CONFIG_DOMAIN = 'component_config_domain'
 CONF_RETRY_COUNT = 'max_retries'
-CONF_RETRY_QUEUE = 'retry_queue_limit'
 
 DEFAULT_DATABASE = 'home_assistant'
 DEFAULT_VERIFY_SSL = True
@@ -53,7 +52,7 @@ COMPONENT_CONFIG_SCHEMA_ENTRY = vol.Schema({
 })
 
 CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.All(cv.deprecated(CONF_RETRY_QUEUE), vol.Schema({
+    DOMAIN: vol.All(vol.Schema({
         vol.Optional(CONF_HOST): cv.string,
         vol.Inclusive(CONF_USERNAME, 'authentication'): cv.string,
         vol.Inclusive(CONF_PASSWORD, 'authentication'): cv.string,
@@ -71,7 +70,6 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Optional(CONF_PORT): cv.port,
         vol.Optional(CONF_SSL): cv.boolean,
         vol.Optional(CONF_RETRY_COUNT, default=0): cv.positive_int,
-        vol.Optional(CONF_RETRY_QUEUE, default=20): cv.positive_int,
         vol.Optional(CONF_DEFAULT_MEASUREMENT): cv.string,
         vol.Optional(CONF_OVERRIDE_MEASUREMENT): cv.string,
         vol.Optional(CONF_TAGS, default={}):
@@ -222,6 +220,10 @@ def setup(hass, config):
                         json['fields'][key] = float(
                             RE_DECIMAL.sub('', new_value))
 
+                # Infinity is not a valid float in InfluxDB
+                if (key, float("inf")) in json['fields'].items():
+                    del json['fields'][key]
+
         json['tags'].update(tags)
 
         return json
@@ -234,6 +236,7 @@ def setup(hass, config):
         """Shut down the thread."""
         instance.queue.put(None)
         instance.join()
+        influx.close()
 
     hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, shutdown)
 
