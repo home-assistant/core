299748b8369e25e7058f82d9b57d775d194f8007
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-03-07 19:46:19 -0800

    Lint

--- homeassistant/components/google_assistant/smart_home.py
+++ homeassistant/components/google_assistant/smart_home.py
@@ -199,7 +199,7 @@ async def _process(hass, config, message):
             'requestId': request_id,
             'payload': {'errorCode': err.code}
         }
-    except Exception as err:  # pylint: disable=bare-except
+    except Exception as err:  # pylint: disable=broad-except
         _LOGGER.exception('Unexpected error')
         return {
             'requestId': request_id,
