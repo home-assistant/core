9502f0728c3a3a4b3c5bd4835d8660f051ffe369
Anders Melchiorsen <amelchio@nogoto.net>
2018-03-09 00:49:55 +0100

    Do temperature conversion on a Kelvin scale

--- homeassistant/components/light/limitlessled.py
+++ homeassistant/components/light/limitlessled.py
@@ -17,6 +17,7 @@ from homeassistant.components.light import (
     SUPPORT_BRIGHTNESS, SUPPORT_COLOR_TEMP, SUPPORT_EFFECT, SUPPORT_FLASH,
     SUPPORT_RGB_COLOR, SUPPORT_TRANSITION, Light, PLATFORM_SCHEMA)
 import homeassistant.helpers.config_validation as cv
+from homeassistant.util.color import color_temperature_mired_to_kelvin
 from homeassistant.helpers.restore_state import async_get_last_state
 
 REQUIREMENTS = ['limitlessled==1.1.0']
@@ -320,8 +321,11 @@ class LimitlessLEDGroup(Light):
 
     def limitlessled_temperature(self):
         """Convert Home Assistant color temperature units to percentage."""
-        width = self.max_mireds - self.min_mireds
-        temperature = 1 - (self._temperature - self.min_mireds) / width
+        max_kelvin = color_temperature_mired_to_kelvin(self.min_mireds)
+        min_kelvin = color_temperature_mired_to_kelvin(self.max_mireds)
+        width = max_kelvin - min_kelvin
+        kelvin = color_temperature_mired_to_kelvin(self._temperature)
+        temperature = (kelvin - min_kelvin) / width
         return max(0, min(1, temperature))
 
     def limitlessled_brightness(self):
