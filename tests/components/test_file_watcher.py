8c015998daf6e81c8e2419aa4c35f72da0269daf
Robin <robmarkcole@users.noreply.github.com>
2018-03-11 08:13:58 +0000

    Create test_file_watcher.py

---
+++ tests/components/test_file_watcher.py
@@ -0,0 +1,49 @@
+"""The tests for the folder_watcher component."""
+import unittest
+import os
+
+from homeassistant.components.folder_watcher import (
+    DOMAIN, CONF_FOLDER, CONF_WATCHERS)
+from homeassistant.setup import setup_component
+from tests.common import get_test_home_assistant
+
+
+CWD = os.path.join(os.path.dirname(__file__))
+TEST_FOLDER = 'test_folder'
+TEST_DIR = os.path.join(CWD, TEST_FOLDER)
+TEST_TXT = 'mock_test_folder.txt'
+TEST_FILE = os.path.join(TEST_DIR, TEST_TXT)
+
+
+def create_file(path):
+    """Create a test file."""
+    with open(path, 'w') as test_file:
+        test_file.write("test")
+
+
+class TestFolderWatcher(unittest.TestCase):
+    """Test the filesize sensor."""
+
+    def setup_method(self, method):
+        """Set up things to be run when tests are started."""
+        self.hass = get_test_home_assistant()
+        if not os.path.isdir(TEST_DIR):
+            os.mkdir(TEST_DIR)
+        self.hass.config.whitelist_external_dirs = set((TEST_DIR))
+
+    def teardown_method(self, method):
+        """Stop everything that was started."""
+        if os.path.isfile(TEST_FILE):
+            os.remove(TEST_FILE)
+            os.rmdir(TEST_DIR)
+        self.hass.stop()
+
+    def test_path(self):
+        """Test that a valid path is setup."""
+        config = {
+            DOMAIN: {
+                CONF_WATCHERS: [{CONF_FOLDER: TEST_DIR}]
+                }
+        }
+        self.assertTrue(
+            setup_component(self.hass, DOMAIN, config))
