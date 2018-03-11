b4bfac48076c74898a25c3c68f5c805bf54b2941
Robin <robmarkcole@users.noreply.github.com>
2018-03-11 12:00:53 +0000

    Run observer.schedule() on EVENT_HOMEASSISTANT_START

--- homeassistant/components/folder_watcher.py
+++ homeassistant/components/folder_watcher.py
@@ -1,53 +1,117 @@
-"""The tests for the folder_watcher component."""
-import unittest
+"""
+Component for monitoring activity on a folder.
+
+For more details about this platform, refer to the documentation at
+https://home-assistant.io/components/folder_watcher/
+"""
 import os
+import logging
+import voluptuous as vol
+from homeassistant.helpers.entity import Entity
+from homeassistant.const import (
+    EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP)
+import homeassistant.helpers.config_validation as cv
 
-from homeassistant.components.folder_watcher import (
-    DOMAIN, CONF_FOLDER)
-from homeassistant.setup import setup_component
-from tests.common import get_test_home_assistant
+REQUIREMENTS = ['watchdog==0.8.3']
+_LOGGER = logging.getLogger(__name__)
+
+CONF_FOLDER = 'folder'
+CONF_PATTERNS = 'patterns'
+CONF_WATCHERS = 'watchers'
+DEFAULT_PATTERN = '*'
+DOMAIN = "folder_watcher"
+EVENT_TYPE = "event_type"
+FILE = 'file'
+FOLDER = 'folder'
+
+WATCHER_CONFIG_SCHEMA = vol.Schema([{
+    vol.Required(CONF_FOLDER): cv.isdir,
+    vol.Optional(CONF_PATTERNS, default=[DEFAULT_PATTERN]): vol.All(
+        cv.ensure_list, [cv.string]),
+}])
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: WATCHER_CONFIG_SCHEMA,
+    }, extra=vol.ALLOW_EXTRA)
 
 
-CWD = os.path.join(os.path.dirname(__file__))
-TEST_FOLDER = 'test_folder'
-TEST_DIR = os.path.join(CWD, TEST_FOLDER)
-TEST_TXT = 'mock_test_folder.txt'
-TEST_FILE = os.path.join(TEST_DIR, TEST_TXT)
+def setup(hass, config):
+    """Set up the folder watcher."""
+    conf = config[DOMAIN]
+    for watcher in conf:
+        path = watcher[CONF_FOLDER]
+        patterns = watcher[CONF_PATTERNS]
+        if not hass.config.is_allowed_path(path):
+            _LOGGER.error("folder %s is not valid or allowed", path)
+            continue
+        Watcher(path, patterns, hass)
+
+    return True
 
 
-def create_file(path):
-    """Create a test file."""
-    with open(path, 'w') as test_file:
-        test_file.write("test")
+def create_event_handler(patterns, hass):
+    """"Return the Watchdog EventHandler object."""
+    from watchdog.events import PatternMatchingEventHandler
+
+    class EventHandler(PatternMatchingEventHandler):
+        """Class for handling Watcher events."""
+
+        def __init__(self, patterns, hass):
+            """Initialise the EventHandler."""
+            super().__init__(patterns)
+            self.hass = hass
+
+        def process(self, event):
+            """On Watcher event, fire HA event."""
+            if not event.is_directory:
+                folder_path, file_name = os.path.split(event.src_path)
+                self.hass.bus.fire(
+                    DOMAIN, {
+                        EVENT_TYPE: event.event_type,
+                        FILE: file_name,
+                        FOLDER: folder_path
+                        })
+
+        def on_modified(self, event):
+            """File modified."""
+            self.process(event)
+
+        def on_moved(self, event):
+            """File moved."""
+            self.process(event)
+
+        def on_created(self, event):
+            """File created."""
+            self.process(event)
+
+        def on_deleted(self, event):
+            """File deleted."""
+            self.process(event)
+
+    return EventHandler(patterns, hass)
 
 
-class TestFolderWatcher(unittest.TestCase):
-    """Test the file_watcher component."""
+class Watcher(Entity):
+    """Class for starting Watchdog."""
 
-    def setup_method(self, method):
-        """Set up things to be run when tests are started."""
-        self.hass = get_test_home_assistant()
-        if not os.path.isdir(TEST_DIR):
-            os.mkdir(TEST_DIR)
-        self.hass.config.whitelist_external_dirs = set((TEST_DIR))
+    def __init__(self, path, patterns, hass):
+        """Initialise the Watchdog oberver."""
+        from watchdog.observers import Observer
+        self._path = path
+        self._patterns = patterns
+        self.hass = hass
+        self._observer = Observer()
+        hass.bus.listen_once(EVENT_HOMEASSISTANT_START, self.startup)
+        hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, self.shutdown)
 
-    def teardown_method(self, method):
-        """Stop everything that was started."""
-        if os.path.isfile(TEST_FILE):
-            os.remove(TEST_FILE)
-            os.rmdir(TEST_DIR)
-        self.hass.stop()
+    def startup(self, event):
+        """Start the watcher."""
+        self._observer.schedule(
+            create_event_handler(self._patterns, self.hass),
+            self._path,
+            recursive=True)
+        self._observer.start()
 
-    def test_path(self):
-        """Test that a valid path is setup."""
-        config = {
-            DOMAIN: [{CONF_FOLDER: TEST_DIR}]}
-        self.assertTrue(
-            setup_component(self.hass, DOMAIN, config))
-
-    def test_invalid_path(self):
-        """Test that a valid path is setup."""
-        config = {
-            DOMAIN: [{CONF_FOLDER: 'invalid_path'}]}
-        self.assertFalse(
-            setup_component(self.hass, DOMAIN, config))
+    def shutdown(self, event):
+        """Shutdown the watcher."""
+        self._observer.stop()
