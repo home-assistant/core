ee729cedb0e87dbad2d7f882a30af87339712020
Jon Maddox <jon@jonmaddox.com>
2018-03-13 16:21:35 -0400

    Revert ":lipstick:"
    This reverts commit 81c08bb7323ec1242ee733bea8421c42243f6f63.

--- homeassistant/components/media_player/channels.py
+++ homeassistant/components/media_player/channels.py
@@ -33,7 +33,8 @@ FEATURE_SUPPORT = SUPPORT_PLAY | SUPPORT_PAUSE | SUPPORT_STOP | \
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string})
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string}
+)
 
 SERVICE_SEEK_FORWARD = 'channels_seek_forward'
 SERVICE_SEEK_BACKWARD = 'channels_seek_backward'
