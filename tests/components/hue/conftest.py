b3e81dcc479c162a3407611ec304c992180e5bea
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-03-13 23:54:06 -0700

    Fix tests

--- tests/components/hue/conftest.py
+++ tests/components/hue/conftest.py
@@ -9,8 +9,8 @@ from tests.common import mock_coro_func
 @pytest.fixture
 def mock_bridge():
     """Mock the HueBridge from initializing."""
-    patch('homeassistant.components.hue._find_username_from_config',
-          return_value=None), \
+    with patch('homeassistant.components.hue._find_username_from_config',
+               return_value=None), \
             patch('homeassistant.components.hue.HueBridge') as mock_bridge:
         mock_bridge().async_setup = mock_coro_func()
         mock_bridge.reset_mock()
