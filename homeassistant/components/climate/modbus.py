ccdf35dec12fbe02ca88a1297e33a241dc7e29ff
Lincoln Kirchoff <lkirchoff@watlow.com>
2018-03-15 11:07:28 -0500

    Fixed lint formatting error

--- homeassistant/components/climate/modbus.py
+++ homeassistant/components/climate/modbus.py
@@ -130,7 +130,7 @@ class ModbusThermostat(ClimateDevice):
             _LOGGER.error(ex)
 
     def read_register(self, register):
-        """Reads holding registers using the modbus hub slave."""
+        """Read holding register using the modbus hub slave."""
         try:
             result = modbus.HUB.read_holding_registers(self._slave, register,
                                                        self._count)
@@ -143,5 +143,5 @@ class ModbusThermostat(ClimateDevice):
         return register_value
 
     def write_register(self, register, value):
-        """Writes registers using the modbus hub slave."""
+        """Write register using the modbus hub slave."""
         modbus.HUB.write_registers(self._slave, register, [value, 0])
