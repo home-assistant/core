4915163fa671bfb25f8920577365c67acbed57e0
Daniel Høyer Iversen <mail@dahoiv.net>
2018-03-17 09:27:59 +0100

    remove unused import

--- homeassistant/components/sensor/xiaomi_miio.py
+++ homeassistant/components/sensor/xiaomi_miio.py
@@ -4,7 +4,6 @@ Support for Xiaomi Mi Air Quality Monitor (PM2.5).
 For more details about this platform, please refer to the documentation
 https://home-assistant.io/components/sensor.xiaomi_miio/
 """
-from functools import partial
 import logging
 
 import voluptuous as vol
