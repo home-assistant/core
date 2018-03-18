cffcd1312ad44e2fe42d2a3517c2600769ba3e69
turbokongen <turbokongen@hotmail.com>
2018-03-18 09:14:23 +0100

    Update tests

--- tests/components/zwave/test_init.py
+++ tests/components/zwave/test_init.py
@@ -1014,8 +1014,14 @@ class TestZWaveServices(unittest.TestCase):
             type=const.TYPE_LIST,
             data_items=['item1', 'item2', 'item3'],
         )
+        value_button = MockValue(
+            index=14,
+            command_class=const.COMMAND_CLASS_CONFIGURATION,
+            type=const.TYPE_BUTTON,
+        )
         node = MockNode(node_id=14)
-        node.get_values.return_value = {12: value, 13: value_list}
+        node.get_values.return_value = {12: value, 13: value_list,
+                                        14: value_button}
         self.zwave_network.nodes = {14: node}
 
         self.hass.services.call('zwave', 'set_config_parameter', {
@@ -1035,6 +1041,16 @@ class TestZWaveServices(unittest.TestCase):
         self.hass.block_till_done()
 
         assert value.data == 7
+
+        self.hass.services.call('zwave', 'set_config_parameter', {
+            const.ATTR_NODE_ID: 14,
+            const.ATTR_CONFIG_PARAMETER: 14,
+            const.ATTR_CONFIG_VALUE: True,
+        })
+        self.hass.block_till_done()
+
+        assert self.zwave_network.manager.pressButton.called
+        assert self.zwave_network.manager.releaseButton.called
 
         self.hass.services.call('zwave', 'set_config_parameter', {
             const.ATTR_NODE_ID: 14,
