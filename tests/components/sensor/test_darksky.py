e8c79f89e91c5eac2caed9857ceb48fc331c5073
Tobias Sauerwein <cgtobi@gmail.com>
2018-03-21 16:22:03 +0000

    Rework tests to cover language setting.

--- tests/components/sensor/test_darksky.py
+++ tests/components/sensor/test_darksky.py
@@ -1,4 +1,5 @@
 """The tests for the Dark Sky platform."""
+import json
 import re
 import unittest
 from unittest.mock import MagicMock, patch
@@ -11,8 +12,56 @@ from datetime import timedelta
 from homeassistant.components.sensor import darksky
 from homeassistant.setup import setup_component
 
-from tests.common import load_fixture, get_test_home_assistant
+from tests.common import (load_fixture, get_test_home_assistant,
+                          assert_setup_component, MockDependency)
 
+VALID_CONFIG_MINIMAL = {
+    'sensor': {
+        'platform': 'darksky',
+        'api_key': 'foo',
+        'forecast': [1, 2],
+        'monitored_conditions': ['summary', 'icon', 'temperature_max'],
+        'update_interval': timedelta(seconds=120),
+    }
+}
+
+INVALID_CONFIG_MINIMAL = {
+    'sensor': {
+        'platform': 'darksky',
+        'api_key': 'foo',
+        'forecast': [1, 2],
+        'monitored_conditions': ['sumary', 'iocn', 'temperature_max'],
+        'update_interval': timedelta(seconds=120),
+    }
+}
+
+VALID_CONFIG_LANG_DE = {
+    'sensor': {
+        'platform': 'darksky',
+        'api_key': 'foo',
+        'forecast': [1, 2],
+        'lang': 'de',
+        'monitored_conditions': ['summary', 'icon', 'temperature_max'],
+        'update_interval': timedelta(seconds=120),
+    }
+}
+
+INVALID_CONFIG_LANG = {
+    'sensor': {
+        'platform': 'darksky',
+        'api_key': 'foo',
+        'forecast': [1, 2],
+        'lang': 'yz',
+        'monitored_conditions': ['summary', 'icon', 'temperature_max'],
+        'update_interval': timedelta(seconds=120),
+    }
+}
+
+
+def load_forecastMock(key, lat, lon,
+                      units, lang):  # pylint: disable=invalid-name
+    """Mock darksky forecast loading."""
+    return ('')
 
 class TestDarkSkySetup(unittest.TestCase):
     """Test the Dark Sky platform."""
@@ -30,12 +79,6 @@ class TestDarkSkySetup(unittest.TestCase):
         """Initialize values for this testcase class."""
         self.hass = get_test_home_assistant()
         self.key = 'foo'
-        self.config = {
-            'api_key': 'foo',
-            'forecast': [1, 2],
-            'monitored_conditions': ['summary', 'icon', 'temperature_max'],
-            'update_interval': timedelta(seconds=120),
-        }
         self.lat = self.hass.config.latitude = 37.8267
         self.lon = self.hass.config.longitude = -122.423
         self.entities = []
@@ -44,10 +87,41 @@ class TestDarkSkySetup(unittest.TestCase):
         """Stop everything that was started."""
         self.hass.stop()
 
-    def test_setup_with_config(self):
+    @MockDependency('forecastio')
+    @patch('forecastio.load_forecast', new=load_forecastMock)
+    def test_setup_with_config(self, mock_forecastio):
         """Test the platform setup with configuration."""
-        self.assertTrue(
-            setup_component(self.hass, 'sensor', {'darksky': self.config}))
+        assert setup_component(self.hass, 'sensor', VALID_CONFIG_MINIMAL)
+
+        state = self.hass.states.get('sensor.dark_sky_summary')
+        assert state is not None
+
+    @MockDependency('forecastio')
+    @patch('forecastio.load_forecast', new=load_forecastMock)
+    def test_setup_with_invalid_config(self, mock_forecastio):
+        """Test the platform setup with invalid configuration."""
+        assert setup_component(self.hass, 'sensor', INVALID_CONFIG_MINIMAL)
+
+        state = self.hass.states.get('sensor.dark_sky_summary')
+        assert state is None
+
+    @MockDependency('forecastio')
+    @patch('forecastio.load_forecast', new=load_forecastMock)
+    def test_setup_with_language_config(self, mock_forecastio):
+        """Test the platform setup with language configuration."""
+        assert setup_component(self.hass, 'sensor', VALID_CONFIG_LANG_DE)
+
+        state = self.hass.states.get('sensor.dark_sky_summary')
+        assert state is not None
+
+    @MockDependency('forecastio')
+    @patch('forecastio.load_forecast', new=load_forecastMock)
+    def test_setup_with_invalid_language_config(self, mock_forecastio):
+        """Test the platform setup with language configuration."""
+        assert setup_component(self.hass, 'sensor', INVALID_CONFIG_LANG)
+
+        state = self.hass.states.get('sensor.dark_sky_summary')
+        assert state is None
 
     @patch('forecastio.api.get_forecast')
     def test_setup_bad_api_key(self, mock_get_forecast):
@@ -60,7 +134,8 @@ class TestDarkSkySetup(unittest.TestCase):
         msg = '400 Client Error: Bad Request for url: {}'.format(url)
         mock_get_forecast.side_effect = HTTPError(msg,)
 
-        response = darksky.setup_platform(self.hass, self.config, MagicMock())
+        response = darksky.setup_platform(self.hass, VALID_CONFIG_MINIMAL,
+                                          MagicMock())
         self.assertFalse(response)
 
     @requests_mock.Mocker()
@@ -69,9 +144,16 @@ class TestDarkSkySetup(unittest.TestCase):
         """Test for successfully setting up the forecast.io platform."""
         uri = (r'https://api.(darksky.net|forecast.io)\/forecast\/(\w+)\/'
                r'(-?\d+\.?\d*),(-?\d+\.?\d*)')
-        mock_req.get(re.compile(uri),
-                     text=load_fixture('darksky.json'))
-        darksky.setup_platform(self.hass, self.config, self.add_entities)
+        mock_req.get(re.compile(uri), text=load_fixture('darksky.json'))
+
+        assert setup_component(self.hass, 'sensor', VALID_CONFIG_MINIMAL)
+
         self.assertTrue(mock_get_forecast.called)
         self.assertEqual(mock_get_forecast.call_count, 1)
-        self.assertEqual(len(self.entities), 7)
+        self.assertEqual(len(self.hass.states.entity_ids()), 7)
+
+        state = self.hass.states.get('sensor.dark_sky_summary')
+        assert state is not None
+        self.assertEqual(state.state, 'Clear')
+        self.assertEqual(state.attributes.get('friendly_name'),
+                          'Dark Sky Summary')
