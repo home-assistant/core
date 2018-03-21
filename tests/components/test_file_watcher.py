ef2f6e4fbfdd5ef151cb4c662a5de3678928c975
Robin <robmarkcole@users.noreply.github.com>
2018-03-21 06:16:24 +0000

    Add event

--- tests/components/test_file_watcher.py
+++ tests/components/test_file_watcher.py
@@ -1,21 +1,26 @@
 """The tests for the folder_watcher component."""
 import unittest
-from unittest.mock import patch, MagicMock  # , Mock
+from unittest.mock import patch, MagicMock
 import os
 
 import pytest
 
-from homeassistant.components.folder_watcher import (
-    DOMAIN, CONF_FOLDER, DEFAULT_PATTERN, create_event_handler)
+from homeassistant.components import folder_watcher
+from homeassistant.core import callback
 from homeassistant.setup import setup_component
 from tests.common import get_test_home_assistant
 
 CWD = os.path.join(os.path.dirname(__file__))
+EVENT_TYPE = 'deleted'
+SRC_PATH = 'test/file.txt'
+FILE = 'file.txt'
+FOLDER = 'test'
 
 
-def get_fake_event(src_path='test/file.txt', event_type='deleted'):
+def get_fake_event(src_path=SRC_PATH, event_type=EVENT_TYPE):
     """Generate a Fake watchdog event object with the specified arguments."""
-    return MagicMock(src_path=src_path, event_type=event_type)
+    return MagicMock(
+        src_path=src_path, event_type=event_type, is_directory=False)
 
 
 @pytest.fixture(autouse=True)
@@ -36,6 +41,14 @@ class TestFolderWatcher(unittest.TestCase):
         """Set up things to be run when tests are started."""
         self.hass = get_test_home_assistant()
         self.hass.config.whitelist_external_dirs = set((CWD))
+        self.events = []
+
+        @callback
+        def record_event(event):
+            """Record HASS event."""
+            self.events.append(event)
+
+        self.hass.bus.listen(folder_watcher.DOMAIN, record_event)
 
     def teardown_method(self, method):
         """Stop everything that was started."""
@@ -44,21 +57,32 @@ class TestFolderWatcher(unittest.TestCase):
     def test_invalid_path_setup(self):
         """Test that a invalid path is not setup."""
         config = {
-            DOMAIN: [{CONF_FOLDER: 'invalid_path'}]
+            folder_watcher.DOMAIN: [{
+                folder_watcher.CONF_FOLDER: 'invalid_path'
+                }]
         }
         self.assertFalse(
-            setup_component(self.hass, DOMAIN, config))
+            setup_component(self.hass, folder_watcher.DOMAIN, config))
 
     def test_valid_path_setup(self):
         """Test that a valid path is setup."""
         config = {
-            DOMAIN: [{CONF_FOLDER: CWD}]
+            folder_watcher.DOMAIN: [{folder_watcher.CONF_FOLDER: CWD}]
         }
 
-        self.assertTrue(setup_component(self.hass, DOMAIN, config))
+        self.assertTrue(setup_component(
+            self.hass, folder_watcher.DOMAIN, config))
 
     def test_event(self):
         """Check that HASS events are fired correctly on watchdog event."""
-        event_handler = create_event_handler([DEFAULT_PATTERN], self.hass)
+        event_handler = folder_watcher.create_event_handler(
+            [folder_watcher.DEFAULT_PATTERN], self.hass)
         fake_event = get_fake_event()
         event_handler.process(fake_event)
+
+        self.hass.block_till_done()
+        last_event = self.events[-1]
+        self.assertEqual(last_event.data["event_type"], EVENT_TYPE)
+        self.assertEqual(last_event.data['path'], SRC_PATH)
+        self.assertEqual(last_event.data['file'], FILE)
+        self.assertEqual(last_event.data['folder'], FOLDER)
