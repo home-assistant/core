973e157f9a43cbed2a9e8c6dbf45b578d26037b6
Nik Klever <nik.klever@hs-augsburg.de>
2018-03-22 15:02:10 +0100

    onewire.py
    Based on file components/sensors/onewire.py I wrote this file components/switch/onewire.py for all onewire switch sensors listed on [owfs family site](http://owfs.sourceforge.net/family.html). It's only tested with family 3A (DS2413) switch, but according to the [owfs DS24xx man pages](http://owfs.org/index.php?page=man-pages-ds24xx) all of the DS24xx switches are similar. This file is working only on an installed [OWFS](http://owfs.org/) filesystem. 
    
    A onewire switch must be configured by **platform: onewire** and **mount_dir: /path/to/owfs-path** in a switch part of the configuration.

---
+++ homeassistant/components/switch/onewire.py
@@ -0,0 +1,139 @@
+"""
+Support for 1-Wire environment switches.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/switch.onewire/
+"""
+import os
+import time
+import logging
+from glob import glob
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.switch import SwitchDevice
+from homeassistant.helpers.config_validation import PLATFORM_SCHEMA  # noqa
+from homeassistant.const import DEVICE_DEFAULT_NAME
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_MOUNT_DIR = 'mount_dir'
+CONF_NAMES = 'names'
+
+DEFAULT_MOUNT_DIR = '/sys/bus/w1/devices/'
+DEVICE_SWITCHES = {'05': {'A': 'PIO'},
+                   '12': {'A': 'PIO.A',
+                          'B': 'PIO.B'},
+                   '29': {'0': 'PIO.0',
+                          '1': 'PIO.1',
+                          '2': 'PIO.2',
+                          '3': 'PIO.3',
+                          '4': 'PIO.4',
+                          '5': 'PIO.5',
+                          '6': 'PIO.6',
+                          '7': 'PIO.7'},
+                   '3A': {'A': 'PIO.A',
+                          'B': 'PIO.B'}}
+
+DEVICE_SWITCH_ON = "1"
+DEVICE_SWITCH_OFF = "0"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAMES): {cv.string: cv.string},
+    vol.Optional(CONF_MOUNT_DIR, default=DEFAULT_MOUNT_DIR): cv.string,
+})
+
+
+# pylint: disable=unused-argument
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the one wire Switches."""
+    base_dir = config.get(CONF_MOUNT_DIR)
+    devs = []
+    device_names = {}
+    if 'names' in config:
+        if isinstance(config['names'], dict):
+            device_names = config['names']
+
+    for family_file_path in glob(os.path.join(base_dir, '*', 'family')):
+        with open(family_file_path, "r") as family_file:
+            family = family_file.read()
+        if family in DEVICE_SWITCHES:
+           switch_id = os.path.split(
+               os.path.split(family_file_path)[0])[1]
+           device_files = {}
+           for switch_key, switch_value in DEVICE_SWITCHES[family].items():
+               switch_id_complete = switch_id + "_" + switch_key
+               device_file = os.path.join(
+                    os.path.split(family_file_path)[0], switch_value)
+               devs.append(OneWireSwitch(device_names.get(switch_id_complete,
+                                                          switch_id_complete),
+                                         device_file))
+
+    if devs == []:
+        _LOGGER.error("No onewire sensor found. Check if dtoverlay=w1-gpio "
+                      "is in your /boot/config.txt. "
+                      "Check the mount_dir parameter if it's defined")
+        return
+
+    add_devices(devs, True)
+
+
+class OneWireSwitch(SwitchDevice):
+    """Representation of a OneWire switch."""
+
+    def __init__(self, name, device_file):
+        """Initialize the OneWire switch."""
+        self._name = name or DEVICE_DEFAULT_NAME
+        self._device_file = device_file
+        self._state = self._readState()
+
+    def _readState(self):
+        state = None
+        try:
+            state = self._read_value_raw() == DEVICE_SWITCH_ON
+        except ValueError:
+            _LOGGER.warning("Invalid value read from %s", self._device_file)
+        except FileNotFoundError:
+            _LOGGER.warning("OneWire Switch file not found: %s", self._device_file)
+        except:
+            _LOGGER.warning("Other error in using switch file: %s", self._device_file)
+            raise
+        return state
+
+    def _read_value_raw(self):
+        """Read the value as it is returned by the switch."""
+        with open(self._device_file, 'r') as ds_device_file:
+            content = ds_device_file.read()
+        return content
+
+    @property
+    def should_poll(self):
+        """Currently easyer - if control of OneWire Switch is only done by hass"""
+        return False
+
+    @property
+    def name(self):
+        """Return the name of the device if any."""
+        return self._name
+
+    @property
+    def is_on(self):
+        """Return true if switch is on."""
+        if self._state == None:
+            self._state = self._readState()
+        return self._state
+
+    def turn_on(self, **kwargs):
+        """Turn the switch on."""
+        with open(self._device_file, 'w') as ds_device_file:
+            ds_device_file.write("1")
+        self._state = True
+        self.schedule_update_ha_state()
+
+    def turn_off(self, **kwargs):
+        """Turn the device off."""
+        with open(self._device_file, 'w') as ds_device_file:
+            ds_device_file.write("0")
+        self._state = False
+        self.schedule_update_ha_state()
