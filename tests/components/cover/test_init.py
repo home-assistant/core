1fcdb554171b96ea8530653a2155f3695a1539e6
tschmidty69 <tschmidty@yahoo.com>
2018-03-23 15:28:01 -0400

    Async fixes

--- tests/components/cover/test_init.py
+++ tests/components/cover/test_init.py
@@ -1,6 +1,4 @@
 """The tests for the cover platform."""
-import asyncio
-
 from homeassistant.components.cover import (SERVICE_OPEN_COVER,
                                             SERVICE_CLOSE_COVER)
 from homeassistant.components import intent
@@ -8,19 +6,18 @@ import homeassistant.components as comps
 from tests.common import async_mock_service
 
 
-@asyncio.coroutine
-def test_open_cover_intent(hass):
+async def test_open_cover_intent(hass):
     """Test HassOpenCover intent."""
-    result = yield from comps.cover.async_setup(hass, {})
+    result = await comps.cover.async_setup(hass, {})
     assert result
 
     hass.states.async_set('cover.garage_door', 'closed')
     calls = async_mock_service(hass, 'cover', SERVICE_OPEN_COVER)
 
-    response = yield from intent.async_handle(
+    response = await intent.async_handle(
         hass, 'test', 'HassOpenCover', {'name': {'value': 'garage door'}}
     )
-    yield from hass.async_block_till_done()
+    await hass.async_block_till_done()
 
     assert response.speech['plain']['speech'] == 'Opened garage door'
     assert len(calls) == 1
@@ -30,19 +27,18 @@ def test_open_cover_intent(hass):
     assert call.data == {'entity_id': 'cover.garage_door'}
 
 
-@asyncio.coroutine
-def test_close_cover_intent(hass):
+async def test_close_cover_intent(hass):
     """Test HassCloseCover intent."""
-    result = yield from comps.cover.async_setup(hass, {})
+    result = await comps.cover.async_setup(hass, {})
     assert result
 
     hass.states.async_set('cover.garage_door', 'open')
     calls = async_mock_service(hass, 'cover', SERVICE_CLOSE_COVER)
 
-    response = yield from intent.async_handle(
+    response = await intent.async_handle(
         hass, 'test', 'HassCloseCover', {'name': {'value': 'garage door'}}
     )
-    yield from hass.async_block_till_done()
+    await hass.async_block_till_done()
 
     assert response.speech['plain']['speech'] == 'Closed garage door'
     assert len(calls) == 1
