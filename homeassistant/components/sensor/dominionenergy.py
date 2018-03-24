539f0c51acd9d0a7c65af8c95e5d2ea118eebbf9
Mohamad <mhed.t91@gmail.com>
2018-03-23 20:07:31 -0400

    Add Exception Handeling if login failed, add PLATFORM_SCHEMA

--- homeassistant/components/sensor/dominionenergy.py
+++ homeassistant/components/sensor/dominionenergy.py
@@ -1,12 +1,35 @@
-"""Support for Washington Dominion Energy."""
+"""
+Support for Washington Dominion Energy.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.dominion_energy/
+"""
+
+import voluptuous as vol
+import logging
 
 from homeassistant.util import Throttle
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (CONF_NAME, CONF_PASSWORD, CONF_USERNAME)
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity import Entity
+
+_LOGGER = logging.getLogger(__name__)
+
+REQUIREMENTS = ['selenium==3.11.0']
+
+DEFAULT_NAME = "Dominion Energy"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
+})
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setting the platform in HASS and getting the username and password."""
-    add_devices([DominionEnergySensor(config['username'], config['password'])])
+    add_devices([DominionEnergySensor(config[CONF_NAME], config[CONF_USERNAME], config[CONF_PASSWORD])])
 
 
 class DominionEnergySensor(Entity):
@@ -17,33 +40,39 @@ class DominionEnergySensor(Entity):
     CURRENT_BILL_SELECTOR = str("#homepageContent > div:nth-child(3) >" +
                                 " div:nth-child(2) > p > span")
 
-    def __init__(self, username, password):
+    def __init__(self, name, username, password):
         """Initialize the sensor."""
         self._state = None
         self._password = password
         self._username = username
+        self._name = name
 
     @property
     def name(self):
-        """Name of the Sensor: DominionEnergy Sensor."""
-        return "Dominion Energy"
+        """Return the name of the sensor."""
+        return self._name
 
     @property
     def state(self):
         """Returning the State of DominionEnergy Sensor."""
         return self._state
 
+
     @Throttle(HOURS_TO_UPDATE)
     def update(self):
         """Using Selenium to access Dominion website and fetch data."""
         from selenium import webdriver
-        driver = webdriver.PhantomJS()
-        driver.set_window_size(1120, 550)
-        driver.get("https://www.dominionenergy.com/sign-in")
-        driver.find_element_by_id('user').send_keys(self._username)
-        driver.find_element_by_id('password').send_keys(self._password)
-        driver.find_element_by_id('SignIn').click()
-        driver.implicitly_wait(1)
-        state = str(driver.find_element_by_css_selector(
-            self.CURRENT_BILL_SELECTOR).text)
-        self._state = state
+        from selenium.common.exceptions import NoSuchElementException
+        try:
+            driver = webdriver.PhantomJS()
+            driver.set_window_size(1120, 550)
+            driver.get("https://www.dominionenergy.com/sign-in")
+            driver.find_element_by_id('user').send_keys(self._username)
+            driver.find_element_by_id('password').send_keys(self._password)
+            driver.find_element_by_id('SignIn').click()
+            driver.implicitly_wait(1)
+            self._state = str(driver.find_element_by_css_selector(
+                self.CURRENT_BILL_SELECTOR).text)
+        except NoSuchElementException :
+            _LOGGER.error("Update Dominion Energy Failed. check if your password changed")
+
