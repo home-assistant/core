e312ffa0b25a0a7156a135596ebbc27def9aa48a
Fabian Affolter <mail@fabian-affolter.ch>
2018-03-25 14:23:53 +0200

    Upgrade keyring to 12.0.0 and keyrings.alt to 3.0 (#13452)

--- homeassistant/scripts/keyring.py
+++ homeassistant/scripts/keyring.py
@@ -5,7 +5,7 @@ import os
 
 from homeassistant.util.yaml import _SECRET_NAMESPACE
 
-REQUIREMENTS = ['keyring==11.0.0', 'keyrings.alt==2.3']
+REQUIREMENTS = ['keyring==12.0.0', 'keyrings.alt==3.0']
 
 
 def run(args):
