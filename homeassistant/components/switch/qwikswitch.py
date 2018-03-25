4d2c84a47504f815d546f91e119c40b6af3d0679
Johann Kellerman <kellerza@gmail.com>
2018-03-25 23:32:13 +0200

    Qwikswitch async & updates (#12641)

--- homeassistant/components/switch/qwikswitch.py
+++ homeassistant/components/switch/qwikswitch.py
@@ -6,19 +6,16 @@ https://home-assistant.io/components/switch.qwikswitch/
 """
 import logging
 
-import homeassistant.components.qwikswitch as qwikswitch
-
-_LOGGER = logging.getLogger(__name__)
-
 DEPENDENCIES = ['qwikswitch']
 
 
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Add switched from the main Qwikswitch component."""
+    """Add switches from the main Qwikswitch component."""
     if discovery_info is None:
-        _LOGGER.error("Configure Qwikswitch component")
+        logging.getLogger(__name__).error(
+            "Configure Qwikswitch Switch component failed")
         return False
 
-    add_devices(qwikswitch.QSUSB['switch'])
+    add_devices(hass.data['qwikswitch']['switch'])
     return True
