6ee703734b16b11f2bb16a682b1983fdc0913eea
Robert Svensson <kane610@me.com>
2018-03-26 21:24:36 +0200

    Bump requirement to v35 Add pydeconz to list of test components

--- homeassistant/components/deconz/__init__.py
+++ homeassistant/components/deconz/__init__.py
@@ -18,7 +18,7 @@ from homeassistant.helpers import discovery, aiohttp_client
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.util.json import load_json, save_json
 
-REQUIREMENTS = ['pydeconz==33']
+REQUIREMENTS = ['pydeconz==35']
 
 _LOGGER = logging.getLogger(__name__)
 
