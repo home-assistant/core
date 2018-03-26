8eb26fa2e54a9892d54213c67d218f500de5db0a
Mohamad <mhed.t91@gmail.com>
2018-03-26 17:53:21 -0400

    Checking the username/password while setup

--- homeassistant/components/sensor/dominionenergy.py
+++ homeassistant/components/sensor/dominionenergy.py
@@ -6,8 +6,8 @@ https://home-assistant.io/components/sensor.dominion_energy/
 """
 
 import logging
+from datetime import timedelta
 import voluptuous as vol
-
 
 from homeassistant.util import Throttle
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -17,6 +17,10 @@ from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
 
+
+HOURS_TO_UPDATE = timedelta(hours=24)
+CURRENT_BILL_SELECTOR = str("#homepageContent > div:nth-child(3) >"
+                            " div:nth-child(2) > p > span")
 REQUIREMENTS = ['selenium==3.11.0']
 
 DEFAULT_NAME = "Dominion Energy"
@@ -30,17 +34,31 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setting the platform in HASS and getting the username and password."""
+    from selenium import webdriver
+    from selenium.common.exceptions import NoSuchElementException
+
     add_devices([DominionEnergySensor(config[CONF_NAME], config[CONF_USERNAME],
                                       config[CONF_PASSWORD])])
-
+    try:
+        driver = webdriver.PhantomJS()
+        driver.set_window_size(1120, 550)
+        driver.get("https://www.dominionenergy.com/sign-in")
+        driver.find_element_by_id('user').send_keys(config[CONF_USERNAME])
+        driver.find_element_by_id('password').send_keys(config[CONF_PASSWORD])
+        driver.find_element_by_id('SignIn').click()
+        driver.implicitly_wait(1)
+        driver.find_element_by_css_selector(
+            CURRENT_BILL_SELECTOR)
+    except NoSuchElementException:
+        _LOGGER.error("Setup DominionEnergy Fail"
+                      " check if your username/password changed")
+        return
+    return
 
 class DominionEnergySensor(Entity):
     """Washington Dominion Energy Sensor will check the bill on daily basis."""
 
-    from datetime import timedelta
-    HOURS_TO_UPDATE = timedelta(hours=24)
-    CURRENT_BILL_SELECTOR = str("#homepageContent > div:nth-child(3) >" +
-                                " div:nth-child(2) > p > span")
+
 
     def __init__(self, name, username, password):
         """Initialize the sensor."""
@@ -73,7 +91,7 @@ class DominionEnergySensor(Entity):
             driver.find_element_by_id('SignIn').click()
             driver.implicitly_wait(1)
             self._state = str(driver.find_element_by_css_selector(
-                self.CURRENT_BILL_SELECTOR).text)
+                CURRENT_BILL_SELECTOR).text)
         except NoSuchElementException:
             _LOGGER.error("Update Dominion Energy Failed."
                           " check if your password changed")
