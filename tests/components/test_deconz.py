010ebe97e63116b1afea36c8a97f9a3572f433b7
Robert Svensson <kane610@me.com>
2018-03-26 21:24:17 +0200

    Add test for no key

--- tests/components/test_deconz.py
+++ tests/components/test_deconz.py
@@ -32,7 +32,7 @@ async def test_flow_works(hass, aioclient_mock):
 
 
 async def test_flow_already_registered_bridge(hass, aioclient_mock):
-    """Test config flow don't more than one bridge to be registered."""
+    """Test config flow don't allow more than one bridge to be registered."""
     flow = deconz.DeconzFlowHandler()
     flow.hass = hass
     flow.hass.data[deconz.DOMAIN] = True
@@ -82,3 +82,16 @@ async def test_flow_two_bridges_discovered(hass, aioclient_mock):
 
     result['data_schema']({'host': '1.2.3.4'})
     result['data_schema']({'host': '5.6.7.8'})
+
+
+async def test_flow_no_api_key(hass, aioclient_mock):
+    """Test config flow discovers no bridges."""
+    aioclient_mock.post('http://1.2.3.4:80/api', json=[])
+    flow = deconz.DeconzFlowHandler()
+    flow.hass = hass
+    flow.deconz_config = {'host': '1.2.3.4', 'port': 80}
+
+    result = await flow.async_step_link(user_input={})
+    assert result['type'] == 'form'
+    assert result['step_id'] == 'link'
+    assert result['errors'] == {'base': 'no_key'}
