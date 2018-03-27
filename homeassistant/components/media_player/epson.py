6f16199e4117bfdfad34bf547eb980db6ce20673
pszafer <pszafer@gmail.com>
2018-03-27 12:13:16 +0200

    deleted timedelta

--- homeassistant/components/media_player/epson.py
+++ homeassistant/components/media_player/epson.py
@@ -5,7 +5,6 @@ For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/media_player.epson/
 """
 import asyncio
-from datetime import timedelta
 import logging
 
 import aiohttp
