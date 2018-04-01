26f29f9f2c6486e62e6bc7c01dcd099dc2cccd57
Lukas Satin <luke.satin@gmail.com>
2018-04-01 21:53:56 +0200

    LG WebOS Play Youtube URL using Launch App Params

--- homeassistant/components/media_player/webostv.py
+++ homeassistant/components/media_player/webostv.py
@@ -357,11 +357,10 @@ class LgWebOSDevice(MediaPlayerDevice):
         """
         Examples:
 
-        - http://youtu.be/SA2iWivDJiE
-        - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
-        - http://www.youtube.com/embed/SA2iWivDJiE
-        - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
-        
+        http://youtu.be/SA2iWivDJiE
+        http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+        http://www.youtube.com/embed/SA2iWivDJiE
+        http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
         """
         query = urlparse(value)
         if query.hostname == 'youtu.be':
