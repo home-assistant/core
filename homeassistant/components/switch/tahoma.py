6a7e6a7519462694226efd4d358cb7fc253f3d6d
Olivier LE TIEC <olivier.letiec@gmail.com>
2018-04-02 14:48:35 +0200

    fixing indents and so on

--- homeassistant/components/switch/tahoma.py
+++ homeassistant/components/switch/tahoma.py
@@ -1,6 +1,7 @@
 """
 Support for Tahoma Switch - those are push buttons for garage door etc.
-Those buttons are implemented as switchs that are never on. They only receive the turn_on action, perform the relay click, and stay in OFF state
+Those buttons are implemented as switchs that are never on. They only
+receive the turn_on action, perform the relay click, and stay in OFF state
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/switch.tahoma/
@@ -15,6 +16,7 @@ DEPENDENCIES = ['tahoma']
 
 _LOGGER = logging.getLogger(__name__)
 
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up Tahoma switchs."""
     controller = hass.data[TAHOMA_DOMAIN]['controller']
@@ -22,7 +24,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     for switch in hass.data[TAHOMA_DOMAIN]['devices']['switch']:
         devices.append(TahomaSwitch(switch, controller))
     add_devices(devices, True)
-	
+
+
 class TahomaSwitch(TahomaDevice, SwitchDevice):
     """Representation a Tahoma Switch."""
 
@@ -36,7 +39,7 @@ class TahomaSwitch(TahomaDevice, SwitchDevice):
     def turn_on(self, **kwargs):
         """Send the on command."""
         self.toggle()
-		
+
     def toggle(self, **kwargs):
         """Click the switch."""
         self.apply_action('cycle')
@@ -45,4 +48,3 @@ class TahomaSwitch(TahomaDevice, SwitchDevice):
     def is_on(self):
         """Get whether the switch is in on state."""
         return False
-
