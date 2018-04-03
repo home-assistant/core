89f5a938c7940fde3d1d96cecb988a4ca5d538bb
Fabian Affolter <mail@fabian-affolter.ch>
2018-04-03 18:27:08 +0200

    Upgrade youtube_dl to 2018.04.03 (#13647)

--- homeassistant/components/media_extractor.py
+++ homeassistant/components/media_extractor.py
@@ -14,7 +14,7 @@ from homeassistant.components.media_player import (
     SERVICE_PLAY_MEDIA)
 from homeassistant.helpers import config_validation as cv
 
-REQUIREMENTS = ['youtube_dl==2018.03.10']
+REQUIREMENTS = ['youtube_dl==2018.04.03']
 
 _LOGGER = logging.getLogger(__name__)
 
