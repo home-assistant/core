9a9ab251009074bd33c57d3c720522d655a065da
Mathias Thorsen <mathias@phithor.no>
2018-04-10 09:43:02 +0200

    first attempt at adding dB volume

--- homeassistant/components/media_player/nadtcp.py
+++ homeassistant/components/media_player/nadtcp.py
@@ -68,7 +68,7 @@ class NADtcp(MediaPlayerDevice):
         self._volume = None
         self._source = None
         self._source_list = self.nad_device.available_sources()
-		self._nad_volume_db = None
+        self._nad_volume_db = None
 
     @property
     def name(self):
@@ -98,7 +98,7 @@ class NADtcp(MediaPlayerDevice):
         # Update current volume
         self._volume = self.nad_vol_to_internal_vol(nad_status['volume'])
         self._nad_volume = nad_status['volume']
-		self._nad_volume_db = nad_vol_to_db_vol(nad_status['volume'])
+        self._nad_volume_db = nad_vol_to_db_vol(nad_status['volume'])
 
         # Update muted state
         self._mute = nad_status['muted']
