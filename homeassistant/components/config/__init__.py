b6856cad99e8c4a00584091686f7a17d29941c41
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-04-14 14:53:12 -0400

    Fix race condition for component loaded before listening

--- homeassistant/components/config/__init__.py
+++ homeassistant/components/config/__init__.py
@@ -40,15 +40,6 @@ def async_setup(hass, config):
             hass.bus.async_fire(EVENT_COMPONENT_LOADED, {ATTR_COMPONENT: key})
             hass.config.components.add(key)
 
-    tasks = [setup_panel(panel_name) for panel_name in SECTIONS]
-
-    for panel_name in ON_DEMAND:
-        if panel_name in hass.config.components:
-            tasks.append(setup_panel(panel_name))
-
-    if tasks:
-        yield from asyncio.wait(tasks, loop=hass.loop)
-
     @callback
     def component_loaded(event):
         """Respond to components being loaded."""
@@ -57,6 +48,15 @@ def async_setup(hass, config):
             hass.async_add_job(setup_panel(panel_name))
 
     hass.bus.async_listen(EVENT_COMPONENT_LOADED, component_loaded)
+
+    tasks = [setup_panel(panel_name) for panel_name in SECTIONS]
+
+    for panel_name in ON_DEMAND:
+        if panel_name in hass.config.components:
+            tasks.append(setup_panel(panel_name))
+
+    if tasks:
+        yield from asyncio.wait(tasks, loop=hass.loop)
 
     return True
 
