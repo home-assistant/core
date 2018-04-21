23577f3191f93d1b83605310174132990355f6b6
rafale77 <anhmantran@yahoo.com>
2018-04-21 09:49:25 -0700

    Update amcrest.py

--- homeassistant/components/switch/amcrest.py
+++ homeassistant/components/switch/amcrest.py
@@ -2,8 +2,8 @@
 Support for toggling Amcrest IP camera settings.
 
 Latest change may change switch entity_id. 
-Please check your automation and change the identity-id of the switch using the new one obtained after the update
-if you have assigned a name to your camera as it appends the name of the camera to the switch. 
+Please check your automation and update the identity-id if you have previously assigned 
+a name to your camera as it appends the name of the camera to the switch. 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/switch.amcrest/
 """
