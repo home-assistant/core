576d71aa3fd24ab494bb32c749fe4917bffc2d8e
Robert Svensson <kane610@me.com>
2018-04-22 22:03:50 +0200

    Add test for light platform

--- tests/components/binary_sensor/test_deconz.py
+++ tests/components/binary_sensor/test_deconz.py
@@ -9,18 +9,16 @@ from tests.common import mock_coro
 
 SENSOR = {
     "1": {
-        "id": "Sensor id",
-        "name": "Sensor name",
+        "id": "Sensor 1 id",
+        "name": "Sensor 1 name",
         "type": "ZHAPresence",
         "state": {"presence": False},
         "config": {}
     }
 }
 
-DATA = {"sensors": SENSOR}
 
-
-async def setup_bridge(hass):
+async def setup_bridge(hass, data):
     """Load the deCONZ binary sensor platform."""
     from pydeconz import DeconzSession
     loop = Mock()
@@ -29,20 +27,29 @@ async def setup_bridge(hass):
     entry.data = {'host': '1.2.3.4', 'port': 80, 'api_key': '1234567890ABCDEF'}
     bridge = DeconzSession(loop, session, **entry.data)
     with patch('pydeconz.DeconzSession.async_get_state',
-               return_value=mock_coro(DATA)):
+               return_value=mock_coro(data)):
         await bridge.async_load_parameters()
     hass.data[deconz.DOMAIN] = bridge
     hass.data[deconz.DATA_DECONZ_ID] = {}
-    config_entry = config_entries.ConfigEntry(1, deconz.DOMAIN, 'Mock Title', {
-        'host': 'mock-host'
-    }, 'test')
-    await hass.config_entries.async_forward_entry_setup(config_entry, 'binary_sensor')
+    config_entry = config_entries.ConfigEntry(
+        1, deconz.DOMAIN, 'Mock Title', {'host': 'mock-host'}, 'test')
+    await hass.config_entries.async_forward_entry_setup(
+        config_entry, 'binary_sensor')
     # To flush out the service call to update the group
     await hass.async_block_till_done()
 
 
-async def test_scene(hass):
+async def test_no_binary_sensors(hass):
     """Test the update_lights function with some lights."""
-    await setup_bridge(hass)
-    assert next(iter(hass.data[deconz.DATA_DECONZ_ID])) == \
-        "binary_sensor.sensor_name"
+    data = {}
+    await setup_bridge(hass, data)
+    assert len(hass.data[deconz.DATA_DECONZ_ID]) == 0
+    assert len(hass.states.async_all()) == 0
+
+
+async def test_binary_sensors(hass):
+    """Test the update_lights function with some lights."""
+    data = {"sensors": SENSOR}
+    await setup_bridge(hass, data)
+    assert "binary_sensor.sensor_1_name" in hass.data[deconz.DATA_DECONZ_ID]
+    assert len(hass.states.async_all()) == 1
--- tests/components/light/test_deconz.py
+++ tests/components/light/test_deconz.py
@@ -1,4 +1,4 @@
-"""deCONZ scenes platform tests."""
+"""deCONZ light platform tests."""
 from unittest.mock import Mock, patch
 
 from homeassistant import config_entries
@@ -7,22 +7,38 @@ from homeassistant.components import deconz
 from tests.common import mock_coro
 
 
+LIGHT = {
+    "1": {
+        "id": "Light 1 id",
+        "name": "Light 1 name",
+        "state": {}
+    }
+}
+
 GROUP = {
     "1": {
         "id": "Group 1 id",
         "name": "Group 1 name",
         "state": {},
         "action": {},
-        "scenes": [{
-            "id": "1",
-            "name": "Scene 1"
-        }],
-    }
+        "scenes": [],
+        "lights": [
+            "1",
+            "2"
+        ]
+    },
+    "2": {
+        "id": "Group 2 id",
+        "name": "Group 2 name",
+        "state": {},
+        "action": {},
+        "scenes": []
+    },
 }
 
 
 async def setup_bridge(hass, data):
-    """Load the deCONZ scene platform."""
+    """Load the deCONZ binary sensor platform."""
     from pydeconz import DeconzSession
     loop = Mock()
     session = Mock()
@@ -36,22 +52,22 @@ async def setup_bridge(hass, data):
     hass.data[deconz.DATA_DECONZ_ID] = {}
     config_entry = config_entries.ConfigEntry(
         1, deconz.DOMAIN, 'Mock Title', {'host': 'mock-host'}, 'test')
-    await hass.config_entries.async_forward_entry_setup(config_entry, 'scene')
+    await hass.config_entries.async_forward_entry_setup(config_entry, 'light')
     # To flush out the service call to update the group
     await hass.async_block_till_done()
 
 
-async def test_no_scenes(hass):
+async def test_no_lights_or_groups(hass):
     """Test the update_lights function with some lights."""
     data = {}
     await setup_bridge(hass, data)
     assert len(hass.data[deconz.DATA_DECONZ_ID]) == 0
     assert len(hass.states.async_all()) == 0
 
-
-async def test_scenes(hass):
+async def test_lights_and_groups(hass):
     """Test the update_lights function with some lights."""
-    data = {"groups": GROUP}
-    await setup_bridge(hass, data)
-    assert "scene.group_1_name_scene_1" in hass.data[deconz.DATA_DECONZ_ID]
-    assert len(hass.states.async_all()) == 1
+    await setup_bridge(hass, {"lights": LIGHT, "groups": GROUP})
+    assert "light.light_1_name" in hass.data[deconz.DATA_DECONZ_ID]
+    assert "light.group_1_name" in hass.data[deconz.DATA_DECONZ_ID]
+    assert "light.group_2_name" not in hass.data[deconz.DATA_DECONZ_ID]
+    assert len(hass.states.async_all()) == 3
