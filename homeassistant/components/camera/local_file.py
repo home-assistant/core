99f0cecd848bfde880e4572d08f10bee54ef2ff6
Robin <robmarkcole@gmail.com>
2018-04-23 07:24:13 +0100

    Adds file_path to device_state_attributes

--- homeassistant/components/camera/local_file.py
+++ homeassistant/components/camera/local_file.py
@@ -91,3 +91,12 @@ class LocalFile(Camera):
     def name(self):
         """Return the name of this camera."""
         return self._name
+
+    @property
+    def device_state_attributes(self):
+        """Return the camera state attributes."""
+        attr = {
+            'access_token': self.access_tokens[-1],
+            'file_path': self._file_path
+        }
+        return attr
