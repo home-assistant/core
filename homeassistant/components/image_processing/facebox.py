fe179975b9e415c958ecb0ede3c7585a6384602b
robmarkcole <robmarkcole@gmail.com>
2018-04-29 06:19:16 +0100

    Update to ImageProcessingFaceEntity
    Use ImageProcessingFaceEntity

--- homeassistant/components/image_processing/facebox.py
+++ homeassistant/components/image_processing/facebox.py
@@ -14,7 +14,9 @@ from homeassistant.core import split_entity_id
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.image_processing import (
     PLATFORM_SCHEMA, CONF_SOURCE, CONF_ENTITY_ID,
-    CONF_NAME, ImageProcessingEntity)
+    CONF_NAME, CONF_CONFIDENCE, DEFAULT_CONFIDENCE)
+from homeassistant.components.image_processing.microsoft_face_identify import (
+    ImageProcessingFaceEntity)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,6 +25,8 @@ ROUNDING_DECIMALS = 2
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_ENDPOINT): cv.string,
+    vol.Optional(CONF_CONFIDENCE, default=DEFAULT_CONFIDENCE):
+        vol.All(vol.Coerce(float), vol.Range(min=0, max=100))
 })
 
 
@@ -34,14 +38,15 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
             camera.get(CONF_NAME),
             config[CONF_ENDPOINT],
             camera[CONF_ENTITY_ID],
+            config[CONF_CONFIDENCE],
         ))
     add_devices(entities)
 
 
-class Facebox(ImageProcessingEntity):
+class Facebox(ImageProcessingFaceEntity):
     """Perform a classification via a Facebox."""
 
-    def __init__(self, name, endpoint, camera_entity):
+    def __init__(self, name, endpoint, camera_entity, confidence):
         """Init with the API key and model id"""
         super().__init__()
         if name:  # Since name is optional.
@@ -50,39 +55,41 @@ class Facebox(ImageProcessingEntity):
             self._name = "Facebox {0}".format(
                 split_entity_id(camera_entity)[1])
         self._camera = camera_entity
+        self._confidence = confidence
         self._url = "http://{}/facebox/check".format(endpoint)
-        self._state = "No_processing_performed"
-        self._faces = {}
+        self.total_faces = 0
+        self.faces = []
         self._response_time = None
 
     def process_image(self, image):
         """Process an image."""
         timer_start = time.perf_counter()
-        response = requests.post(
-            self._url,
-            json=self.encode_image(image)
-            ).json()
+        response = {}
+        try:
+            response = requests.post(
+                self._url,
+                json=self.encode_image(image),
+                timeout=30
+                ).json()
+        except requests.exceptions.ConnectionError:
+            _LOGGER.error("ConnectionError: Is Facebox running?")
+            response['success'] = False
 
         if response['success']:
             elapsed_time = time.perf_counter() - timer_start
             self._response_time = "{} seconds".format(
                     str(round(elapsed_time, 1)))
-            self._state = int(response['facesCount'])
-            self._faces = self.process_faces(response)
+            self.total_faces = response['facesCount']  # An int.
+            self.faces = response['faces']
+
         else:
-            self._state = "Processing_not_successful"
-            self._faces = {}
+            self.total_faces = "Request_failed"
+            self.faces = []
 
     def encode_image(self, image):
         """base64 encode an image stream."""
         base64_img = base64.b64encode(image).decode('ascii')
         return {"base64": base64_img}
-
-    def process_faces(self, response):
-        """Process face data from facebox response."""
-        faces = {face['name']: round(face['confidence'], ROUNDING_DECIMALS)
-                 for face in response['faces'] if face['matched']}
-        return faces
 
     @property
     def camera_entity(self):
@@ -90,30 +97,18 @@ class Facebox(ImageProcessingEntity):
         return self._camera
 
     @property
+    def confidence(self):
+        """Return minimum confidence for send events."""
+        return self._confidence
+
+    @property
     def name(self):
         """Return the name of the sensor."""
         return self._name
 
     @property
-    def state(self):
-        """Return the state of the entity."""
-        return self._state
-
-    @property
     def device_state_attributes(self):
-        """Return other details about the sensor state."""
-        attr = self._faces
-        attr.update({'response_time': self._response_time})
-        return attr
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        if isinstance(self._state, int):
-            if self._state == 1:
-                unit_of_measurement = "face"
-            else:
-                unit_of_measurement = "faces"
-        else:
-            unit_of_measurement = None
-        return unit_of_measurement
+        """Return the other state attributes."""
+        return {
+            'response_time': self._response_time,
+        }
