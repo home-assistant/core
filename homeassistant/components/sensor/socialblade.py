c9693a022e25166cab555392a12877bd41ae5a17
Fabian Affolter <mail@fabian-affolter.ch>
2018-05-01 21:47:25 +0200

    Fix remaining issues

--- homeassistant/components/sensor/socialblade.py
+++ homeassistant/components/sensor/socialblade.py
@@ -1,58 +1,57 @@
 """
-Support for Social Blade .
+Support for Social Blade.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.socialblade/
 """
-
-import logging
 from datetime import timedelta
+import logging
+
 import voluptuous as vol
 
-from homeassistant.const import CONF_NAME
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-
+from homeassistant.const import CONF_NAME
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity import Entity
-
 from homeassistant.util import Throttle
 
 _LOGGER = logging.getLogger(__name__)
 
 REQUIREMENTS = ['socialbladeclient==0.2']
 
-DEFAULT_NAME = "Social Blade"
-
 CHANNEL_ID = 'channel_id'
+
+DEFAULT_NAME = "Social Blade"
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(hours=2)
 
+SUBSCRIBERS = 'subscribers'
+
+TOTAL_VIEWS = 'total_views'
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Required(CHANNEL_ID): cv.string,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
 })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setting the platform in HASS and Channel Information."""
-    social_blade = SocialBladeSensor(config[CHANNEL_ID],
-                                     config[CONF_NAME])
+    """Set up the Social Blade sensor."""
+    social_blade = SocialBladeSensor(
+        config[CHANNEL_ID], config[CONF_NAME])
+
     social_blade.update()
-    if social_blade.valid_channel_id:
-        add_devices([social_blade])
-    else:
-        _LOGGER.error("Setup Social Blade Sensor Fail"
-                      " check if your channel ID is valid")
+    if social_blade.valid_channel_id is False:
+        return
+
+    add_devices([social_blade])
 
 
 class SocialBladeSensor(Entity):
     """Representation of a Social Blade Sensor."""
 
-    SUBSCRIBERS = "subscribers"
-    TOTAL_VIEWS = "total_views"
-
     def __init__(self, case, name):
-        """Initialize the sensor."""
+        """Initialize the Social Blade sensor."""
         self._state = None
         self.channel_id = case
         self._attributes = None
@@ -72,21 +71,20 @@ class SocialBladeSensor(Entity):
     @property
     def device_state_attributes(self):
         """Return the state attributes."""
-        return self._attributes
+        if self._attributes:
+            return self._attributes
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
-        """Using Request to access SocialBlade website and fetch data."""
+        """Get the latest data from Social Blade."""
         import socialbladeclient
         try:
             data = socialbladeclient.get_data(self.channel_id)
-            self._attributes = {
-                self.TOTAL_VIEWS: data[self.TOTAL_VIEWS]
-            }
-            self._state = data[self.SUBSCRIBERS]
+            self._attributes = {TOTAL_VIEWS: data[TOTAL_VIEWS]}
+            self._state = data[SUBSCRIBERS]
             self.valid_channel_id = True
 
-        except ValueError:
-            _LOGGER.exception("SocialBlade setup failed : Please check that \
-                                you have valid channel id")
+        except (ValueError, IndexError):
+            _LOGGER.error("Unable to find valid channel ID")
             self.valid_channel_id = False
+            self._attributes = None
