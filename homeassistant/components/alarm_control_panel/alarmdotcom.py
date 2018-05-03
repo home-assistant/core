df804a5217335c417c4cfab9113cb84c0659b313
Jesse Newland <jesse@jnewland.com>
2018-05-02 20:53:00 -0500

    missed a spot

--- homeassistant/components/alarm_control_panel/alarmdotcom.py
+++ homeassistant/components/alarm_control_panel/alarmdotcom.py
@@ -17,7 +17,7 @@ from homeassistant.const import (
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pyalarmdotcom==0.3.1']
+REQUIREMENTS = ['pyalarmdotcom==0.3.2']
 
 _LOGGER = logging.getLogger(__name__)
 
