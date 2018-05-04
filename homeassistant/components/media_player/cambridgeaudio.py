8622e82ed92e17ef634fff3a4621650d41bd5bc5
sebk-666 <seb.kaps@zoho.com>
2018-05-04 21:13:35 +0200

    More formatting fixes.

--- homeassistant/components/media_player/cambridgeaudio.py
+++ homeassistant/components/media_player/cambridgeaudio.py
@@ -40,8 +40,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_COMMAND_OFF, default=DEFAULT_PWROFF_CMD): cv.string
 })
 
-# TODO: split up into two items, one for when the device is configured
-# as pre-amp and one for when it's not.
 SUPPORT_CAMBRIDGE = SUPPORT_VOLUME_MUTE | SUPPORT_TURN_OFF | SUPPORT_TURN_ON |\
                     SUPPORT_PLAY | SUPPORT_SELECT_SOURCE |\
                     SUPPORT_CLEAR_PLAYLIST | SUPPORT_NEXT_TRACK |\
@@ -49,7 +47,7 @@ SUPPORT_CAMBRIDGE = SUPPORT_VOLUME_MUTE | SUPPORT_TURN_OFF | SUPPORT_TURN_ON |\
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up the Cambridge Audio platform"""
+    """Set up the Cambridge Audio platform."""
     from stream_magic import device as cadevice
     from stream_magic import discovery as ca
 
@@ -109,11 +107,10 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 
 class CADevice(MediaPlayerDevice):
-    """Representation of a Cambridge Audio Network Audio Player device"""
+    """Representation of a Cambridge Audio Network Audio Player device."""
 
     def __init__(self, smdevice, sources, poweroff_command, name=None):
-        """initialize the UPnP-AV control point"""
-
+        """Initialization."""
         self._smdevice = smdevice
         self._name = name
         self._pwstate = STATE_UNKNOWN
@@ -138,32 +135,32 @@ class CADevice(MediaPlayerDevice):
 
     @property
     def name(self):
-        """Return the name of the media_player"""
+        """Return the name of the media_player."""
         return self._name
 
     @property
     def state(self):
-        """ Return the state of the media_player. """
+        """Return the state of the media_player."""
         return self._state
 
     @property
     def is_volume_muted(self):
-        """ Return the state of the muting function. """
+        """Return the state of the muting function."""
         return self._muted
 
     @property
     def source(self):
-        """ Return currently selected input source. """
+        """Return currently selected input source."""
         return self._audio_source
 
     @property
     def source_list(self):
-        """ Return a list of available Presets. """
+        """Return a list of available Presets."""
         return self._source_list
 
     @property
     def supported_features(self):
-        """ return supported features """
+        """Return supported features."""
         return SUPPORT_CAMBRIDGE
 
     @property
@@ -173,49 +170,50 @@ class CADevice(MediaPlayerDevice):
 
     @property
     def media_image_url(self):
-        """Image url of current playing media."""
+        """Return image URI of current playing media."""
         return self._album_art
 
     @property
     def media_artist(self):
-        """Artist of current playing media, music track only."""
+        """Return artist of current playing media."""
         return self._artist
 
     @property
     def media_album(self):
-        """Artist of current playing media, music track only."""
+        """Return album of current playing media."""
         return self._album
 
     @property
     def media_track(self):
-        """Track number of current playing media, music track only."""
+        """Return track number of current playing media."""
         return self._trackno
 
     @property
     def media_title(self):
-        """Title of current playing media."""
+        """Return title of current playing media."""
         return self._title
 
     @property
     def media_position(self):
-        """Position of current playing media in seconds."""
+        """Return position of current playing media in seconds."""
         pass
 
     def clear_playlist(self):
+        """Clear the playlist."""
         pass
 
     def media_next_track(self):
-        """ Skip to the next track, when in media player mode. """
+        """Skip to the next track, when in media player mode."""
         if self._audio_source == "media player":
             self._smdevice.trnsprt_next()
 
     def media_previous_track(self):
-        """ Skip to the previous track, when in media player mode. """
+        """Skip to the previous track, when in media player mode."""
         if self._audio_source == "media player":
             self._smdevice.trnsprt_prev()
 
     def media_pause(self):
-        """ Pause playing the current media """
+        """Pause playing the current media."""
         # there is no "pause" with internet radio streams
         if self._audio_source == "media player":
             self._smdevice.trnsprt_pause()
@@ -225,51 +223,57 @@ class CADevice(MediaPlayerDevice):
             self._state = STATE_IDLE
 
     def media_seek(self, position):
+        """Jump to the specified position within the current track."""
         pass
 
     def media_stop(self):
-        """ Stop playing the current media. """
+        """Stop playing the current media."""
         self._smdevice.trnsprt_stop()
         self._state = STATE_IDLE
 
     def media_play(self):
-        """ Start playing the current media. """
+        """Start playing the current media."""
         self._smdevice.trnsprt_play()
         self._state = STATE_PLAYING
 
-    def mute_volume(self, state):
-        """ Mute the volume.
+    def mute_volume(self, mute):
+        """Mute the volume.
             This will only work if the device is set up as Pre-Amp.
         """
-        self._smdevice.volume_mute(state)
+        self._smdevice.volume_mute(mute)
 
     def play_media(self, media_type, media_id, **kwargs):
+        """Play the specified media."""
         pass
 
     def select_source(self, source):
-        """ Switch to the selected internet radio preset. """
+        """Switch to the selected internet radio preset."""
         self._smdevice.play_preset(self._sources_reverse[source])
 
     def set_shuffle(self, shuffle):
+        """Enable/disable shuffle play."""
         pass
 
     def set_volume_level(self, volume):
+        """Set the current volume level.
+            This will only work if the device is set up as Pre-Amp.
+        """
         pass
 
     def turn_off(self):
-        """ Switch the device OFF (default) or into IDLE mode.  """
+        """Switch the device OFF (default) or into IDLE mode."""
         _LOGGER.debug("Powering off (%s)", self._power_off_cmd)
         self._smdevice.power_off(power_state=self._power_off_cmd)
         self._pwstate = STATE_OFF
 
     def turn_on(self):
-        """ Turn the device on if it's in IDLE (network standby) mode. """
+        """Turn the device on if it's in IDLE (network standby) mode."""
         if self._smdevice.get_power_state() == 'idle':
             self._smdevice.power_on()
             self._pwstate = STATE_ON
 
     def update(self):
-        """Fetch new state data from the media_player"""
+        """Fetch new state data from the media_player."""
         dev = self._smdevice
 
         pwstate = dev.get_power_state()
@@ -278,11 +282,10 @@ class CADevice(MediaPlayerDevice):
             return
         self._pwstate = STATE_ON
         self._state = {
-                        'PLAYING': STATE_PLAYING,
-                        'PAUSED_PLAYBACK': STATE_PAUSED,
-                        'STOPPED': STATE_IDLE,
-                        'TRANSITIONING': STATE_PLAYING
-                      }.get(dev.get_transport_state())
+            'PLAYING': STATE_PLAYING,
+            'PAUSED_PLAYBACK': STATE_PAUSED,
+            'STOPPED': STATE_IDLE,
+            'TRANSITIONING': STATE_PLAYING}.get(dev.get_transport_state())
         self._audio_source = dev.get_audio_source()
         self._source = dev.get_current_preset()['name']
         self._muted = dev.get_mute_state()
