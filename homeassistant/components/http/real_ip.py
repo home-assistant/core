21c4827ef6a5bb0ae899892bc2f4389deeba630d
mvn23 <schopdiedwaas@gmail.com>
2018-05-10 18:12:46 +0200

    Improved handling of x-forwarded-for header to prevent spoofing attacks when using use_x_forwarded_for and trusted_networks. Updated tests to reflect changes in the http component.

--- homeassistant/components/http/real_ip.py
+++ homeassistant/components/http/real_ip.py
@@ -11,15 +11,23 @@ from .const import KEY_REAL_IP
 
 
 @callback
-def setup_real_ip(app, use_x_forwarded_for):
+def setup_real_ip(app, use_x_forwarded_for, trusted_networks):
     """Create IP Ban middleware for the app."""
     @middleware
     async def real_ip_middleware(request, handler):
         """Real IP middleware."""
         if (use_x_forwarded_for and
                 X_FORWARDED_FOR in request.headers):
-            request[KEY_REAL_IP] = ip_address(
-                request.headers.get(X_FORWARDED_FOR).split(',')[0])
+            forwarded_for = request.headers.get(X_FORWARDED_FOR).split(', ')
+            for ip_addr in forwarded_for:
+                if (any(ip_address(ip_addr) in trusted_network
+                        for trusted_network in trusted_networks)):
+                    continue
+                else:
+                    request[KEY_REAL_IP] = ip_address(ip_addr)
+                    break
+            if request.get(KEY_REAL_IP) is None:
+                request[KEY_REAL_IP] = ip_address(forwarded_for[0])
         else:
             request[KEY_REAL_IP] = \
                 ip_address(request.transport.get_extra_info('peername')[0])
--- tests/components/http/test_real_ip.py
+++ tests/components/http/test_real_ip.py
@@ -5,6 +5,7 @@ from aiohttp.hdrs import X_FORWARDED_FOR
 from homeassistant.components.http.real_ip import setup_real_ip
 from homeassistant.components.http.const import KEY_REAL_IP
 
+from .test_auth import TRUSTED_NETWORKS
 
 async def mock_handler(request):
     """Handler that returns the real IP as text."""
@@ -15,7 +16,7 @@ async def test_ignore_x_forwarded_for(aiohttp_client):
     """Test that we get the IP from the transport."""
     app = web.Application()
     app.router.add_get('/', mock_handler)
-    setup_real_ip(app, False)
+    setup_real_ip(app, False, TRUSTED_NETWORKS)
 
     mock_api_client = await aiohttp_client(app)
 
@@ -31,7 +32,7 @@ async def test_use_x_forwarded_for(aiohttp_client):
     """Test that we get the IP from the transport."""
     app = web.Application()
     app.router.add_get('/', mock_handler)
-    setup_real_ip(app, True)
+    setup_real_ip(app, True, TRUSTED_NETWORKS)
 
     mock_api_client = await aiohttp_client(app)
 
