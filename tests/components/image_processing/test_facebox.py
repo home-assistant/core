b9fbff29fa520e16740cb06deb221254237f42d2
Robin <robmarkcole@gmail.com>
2018-05-10 07:00:11 +0100

    Add event test

--- tests/components/image_processing/test_facebox.py
+++ tests/components/image_processing/test_facebox.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 import pytest
 import requests_mock
 
+from homeassistant.core import callback
 from homeassistant.const import (
     ATTR_ENTITY_ID, CONF_IP_ADDRESS, CONF_PORT)
 from homeassistant.setup import async_setup_component
@@ -58,6 +59,15 @@ async def test_process_image(hass, mock_image):
     await async_setup_component(hass, ip.DOMAIN, VALID_CONFIG)
     assert hass.states.get(VALID_ENTITY_ID)
 
+    face_events = []
+
+    @callback
+    def mock_face_event(event):
+        """Mock event."""
+        face_events.append(event)
+
+    hass.bus.async_listen('image_processing.detect_face', mock_face_event)
+
     with requests_mock.Mocker() as mock_req:
         url = "http://{}:{}/facebox/check".format(MOCK_IP, MOCK_PORT)
         mock_req.post(url, json=MOCK_JSON)
@@ -69,7 +79,12 @@ async def test_process_image(hass, mock_image):
 
     state = hass.states.get(VALID_ENTITY_ID)
     assert state.state == '1'
-    assert state.attributes.get('matched_faces') == {'John Lennon': 0.58}
+    assert state.attributes.get('matched_faces') == {MOCK_FACES['name']: 0.58}
 
     MOCK_FACES[ATTR_ENTITY_ID] = VALID_ENTITY_ID  # Update.
     assert state.attributes.get('faces') == [MOCK_FACES]
+
+    assert len(face_events) == 1
+    assert face_events[0].data['name'] == MOCK_FACES['name']
+    assert face_events[0].data['confidence'] == MOCK_FACES['confidence']
+    assert face_events[0].data['entity_id'] == VALID_ENTITY_ID
