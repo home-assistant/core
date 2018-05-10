1a436718c7f8b66d3cdce391072160f0d1e90eb9
damarco <schmidt.d@aon.at>
2018-05-10 19:59:23 +0200

    Fix binary_sensor async_update (#14376)

--- homeassistant/components/zha/__init__.py
+++ homeassistant/components/zha/__init__.py
@@ -410,7 +410,7 @@ def get_discovery_info(hass, discovery_info):
     return all_discovery_info.get(discovery_key, None)
 
 
-async def safe_read(cluster, attributes):
+async def safe_read(cluster, attributes, allow_cache=True):
     """Swallow all exceptions from network read.
 
     If we throw during initialization, setup fails. Rather have an entity that
@@ -420,7 +420,7 @@ async def safe_read(cluster, attributes):
     try:
         result, _ = await cluster.read_attributes(
             attributes,
-            allow_cache=True,
+            allow_cache=allow_cache,
         )
         return result
     except Exception:  # pylint: disable=broad-except
