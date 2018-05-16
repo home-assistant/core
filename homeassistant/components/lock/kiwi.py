c4f4a00465d3b2f079b2c26f409a0b9550049895
Christoph <christoph@gerneth.info>
2018-05-16 12:16:21 +0200

    created pypi package for door lock library

--- homeassistant/components/lock/kiwi.py
+++ homeassistant/components/lock/kiwi.py
@@ -5,16 +5,15 @@ For more details about this platform, please refer to the documentation
 https://home-assistant.io/components/lock.kiwi/
 """
 import logging
-import requests
 
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
-from homeassistant.util.dt import (utcnow, parse_datetime)
 from homeassistant.components.lock import (LockDevice, PLATFORM_SCHEMA)
 from homeassistant.const import (
     CONF_PASSWORD, CONF_USERNAME, ATTR_ID, ATTR_LONGITUDE, ATTR_LATITUDE)
 
+REQUIREMENTS = ['kiwiki-client==0.1']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,91 +26,12 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_PASSWORD): cv.string
 })
 
-BASE_URL = 'https://api.kiwi.ki'
-API_AUTH_URL = BASE_URL + '/pre/session/'
-API_LIST_DOOR_URL = BASE_URL + '/pre/sensors/'
-API_OPEN_DOOR_URL = BASE_URL + '/pre/sensors/{}/act/open'
-
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the KIWI lock platform."""
+    from kiwiki import KiwiClient
     kiwi = KiwiClient(config.get(CONF_USERNAME), config.get(CONF_PASSWORD))
     add_devices([KiwiLock(lock, kiwi) for lock in kiwi.get_locks()], True)
-
-
-class KiwiClient:
-    """Client for KIWI service."""
-
-    def __init__(self, username, password):
-        """Initiale the client.
-
-        :param username: valid KIWI username. Hint: your signup email address.
-        :param password: your KIWI account password.
-        """
-        self.__username = username
-        self.__password = password
-        self.__session_key = None
-        self.__session_expires = None
-
-        # get a new session token on client startup
-        self._renew_sessionkey()
-
-    def _with_valid_session(self):
-        """Check if the session is valid; renew if necessary."""
-        if not self.__session_expires or (utcnow() >= self.__session_expires):
-            _LOGGER.debug("no valid session found - renewing session key")
-            self._renew_sessionkey()
-
-    def _renew_sessionkey(self):
-        """Update the clients session key."""
-        _LOGGER.info(
-            "authentication for user %s started.",
-            self.__username)
-
-        auth_response = requests.post(
-            API_AUTH_URL,
-            json={
-                "username": self.__username,
-                "password": self.__password
-            },
-            headers={"Accept": "application/json"}
-        )
-
-        if not auth_response.ok:
-            _LOGGER.error(
-                "could not authenticate at KIWI:\n%s",
-                auth_response.json())
-
-            raise ValueError("authentication failed")
-
-        self.__session_key = auth_response.json()['result']['session_key']
-        self.__session_expires = parse_datetime(
-            auth_response.json()['result']['session']['expires'])
-
-    def get_locks(self):
-        """Return a list of kiwi locks."""
-        self._with_valid_session()
-        sensor_list = requests.get(
-            API_LIST_DOOR_URL,
-            params={"session_key": self.__session_key},
-            headers={"Accept": "application/json"}
-        )
-        if not sensor_list.ok:
-            _LOGGER.error("could not get your KIWI doors.")
-            return []
-
-        doors = sensor_list.json()['result']['sensors']
-        return doors
-
-    def open_door(self, door_id):
-        """Open the kiwi door lock."""
-        self._with_valid_session()
-        open_response = requests.post(
-            API_OPEN_DOOR_URL.format(door_id),
-            headers={"Accept": "application/json"},
-            params={"session_key": self.__session_key}
-        )
-        return True if open_response.ok else False
 
 
 class KiwiLock(LockDevice):
@@ -159,5 +79,8 @@ class KiwiLock(LockDevice):
 
     def unlock(self, **kwargs):
         """Unlock the device."""
-        if not self._client.open_door(self.lock_id):
+        from kiwiki import KiwiException
+        try:
+            self._client.open_door(self.lock_id)
+        except KiwiException:
             _LOGGER.error("failed to open door")
