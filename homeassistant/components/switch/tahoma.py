6b86684f375e4a9f440c4efbd4dabfa31e36320c
fucm <fucm@users.noreply.github.com>
2018-05-24 22:33:44 +0200

    Add support for 2 cover types, a smoke detector and a light switch: - 'io:VerticalExteriorAwningIOComponent': 'cover' - 'io:HorizontalAwningIOComponent': 'cover' - 'io:OnOffLightIOComponent': 'switch' - 'rtds:RTDSSmokeSensor': 'smoke'

--- homeassistant/components/switch/tahoma.py
+++ homeassistant/components/switch/tahoma.py
@@ -12,6 +12,7 @@ import logging
 from homeassistant.components.switch import SwitchDevice
 from homeassistant.components.tahoma import (
     DOMAIN as TAHOMA_DOMAIN, TahomaDevice)
+from homeassistant.const import (STATE_OFF, STATE_ON)
 
 DEPENDENCIES = ['tahoma']
 
@@ -30,6 +31,20 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class TahomaSwitch(TahomaDevice, SwitchDevice):
     """Representation a Tahoma Switch."""
 
+    def __init__(self, tahoma_device, controller):
+        """Initialize the switch."""
+        self._state = STATE_OFF
+        super().__init__(tahoma_device, controller)
+
+    def update(self):
+        """Update method."""
+        self.controller.get_states([self.tahoma_device])
+        if self.tahoma_device.type == 'io:OnOffLightIOComponent':
+            if self.tahoma_device.active_states['core:OnOffState'] == 'on':
+                self._state = STATE_ON
+            else:
+                self._state = STATE_OFF
+
     @property
     def device_class(self):
         """Return the class of the device."""
@@ -39,7 +54,25 @@ class TahomaSwitch(TahomaDevice, SwitchDevice):
 
     def turn_on(self, **kwargs):
         """Send the on command."""
-        self.toggle()
+        _LOGGER.debug("Turn on: %s", self._name)
+        if self.tahoma_device.type == 'rts:GarageDoor4TRTSComponent':
+            self.toggle()
+        else:
+            self.apply_action('on')
+            # FIXME: State immediately overwritten by update function.
+            # How to deferre update, till REST request is done?
+            self._state = STATE_ON
+
+    def turn_off(self, **kwargs):
+        """Send the off command."""
+        _LOGGER.debug("Turn off: %s", self._name)
+        if self.tahoma_device.type == 'rts:GarageDoor4TRTSComponent':
+            return
+        else:
+            self.apply_action('off')
+            # FIXME: State immediately overwritten by update function.
+            # How to deferre update, till REST request is done?
+            self._state = STATE_OFF
 
     def toggle(self, **kwargs):
         """Click the switch."""
@@ -48,4 +81,7 @@ class TahomaSwitch(TahomaDevice, SwitchDevice):
     @property
     def is_on(self):
         """Get whether the switch is in on state."""
-        return False
+        if self.tahoma_device.type == 'rts:GarageDoor4TRTSComponent':
+            return False
+        _LOGGER.debug("Is on (%s): %s", self._name, self._state)
+        return bool(self._state == STATE_ON)
