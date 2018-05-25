c5237b8dfd25ab1960c5a16706df82cd5e3dee5e
Nik Klever <nik.klever@hs-augsburg.de>
2018-05-25 10:29:20 +0200

    Adding illumination sensor (#14615)
    * Adding illumination sensor
    
    Adding Illumination sensor of 1wire device DS2438 (DEVICE_SENSOR type 26) according to [OWFS API](http://owfs.org/index.php?page=ds2438)
    
    * Correcting typo illumination -> illuminance

--- homeassistant/components/sensor/onewire.py
+++ homeassistant/components/sensor/onewire.py
@@ -28,7 +28,8 @@ DEVICE_SENSORS = {'10': {'temperature': 'temperature'},
                   '22': {'temperature': 'temperature'},
                   '26': {'temperature': 'temperature',
                          'humidity': 'humidity',
-                         'pressure': 'B1-R1-A/pressure'},
+                         'pressure': 'B1-R1-A/pressure',
+                         'illuminance': 'S3-R1-A/illuminance'},
                   '28': {'temperature': 'temperature'},
                   '3B': {'temperature': 'temperature'},
                   '42': {'temperature': 'temperature'}}
@@ -37,6 +38,7 @@ SENSOR_TYPES = {
     'temperature': ['temperature', TEMP_CELSIUS],
     'humidity': ['humidity', '%'],
     'pressure': ['pressure', 'mb'],
+    'illuminance': ['illuminance', 'lux'],
 }
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
