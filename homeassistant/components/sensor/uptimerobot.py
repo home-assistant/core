e6637376aed0e9e46d69c73e900927fc3a1bd90f
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-05-26 13:19:01 +0200

    Added changes form @cdce8p

--- homeassistant/components/sensor/uptimerobot.py
+++ homeassistant/components/sensor/uptimerobot.py
@@ -1,85 +1,62 @@
-"""
-A component to monitor Uptime Robot monitors.
+"""A component to monitor Uptime Robot monitors.
 
 For more details about this component, please refer to the documentation at
 https://www.home-assistant.io/components/sensor.uptimerobot
 """
 import logging
-from datetime import timedelta
+
 import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
+
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import CONF_API_KEY
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
 REQUIREMENTS = ['pyuptimerobot==0.0.4']
 
+_LOGGER = logging.getLogger(__name__)
+
 ATTR_TARGET = 'target'
-ATTR_MONITORID = 'monitor id'
-ATTR_COMPONENT = 'component'
-ATTR_COMPONENT_VERSION = 'component_version'
-
-SCAN_INTERVAL = timedelta(seconds=30)
-
-ICON = 'mdi:server'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
 })
 
-_LOGGER = logging.getLogger(__name__)
 
-
-def setup_platform(hass, config, add_devices_callback, discovery_info=None):
+def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Uptime Robot sensors."""
     from pyuptimerobot import UptimeRobot
-    uptimerobot = UptimeRobot()
+
+    up_robot = UptimeRobot()
     apikey = config.get(CONF_API_KEY)
-    monitors = uptimerobot.getMonitors(apikey)
-    _LOGGER.debug(monitors)
-    dev = []
-    if not monitors:
-        _LOGGER.critical("Something terrible happend :(")
-        retval = False
-    else:
-        for monitor in monitors['monitors']:
-            _LOGGER.debug(monitor)
-            monitorid = monitor['id']
-            name = monitor['friendly_name']
-            target = monitor['url']
-            dev.append(UptimeRobotSensor(apikey, monitorid, name, target))
-        add_devices_callback(dev, True)
-        retval = True
-    return retval
+    monitors = up_robot.getMonitors(apikey)
+
+    devices = []
+    if not monitors or monitors.get('stat') != 'ok':
+        error = monitors.get('error', {})
+        _LOGGER.error(error.get('message', 'Something terrible happend :('))
+        return False
+
+    for monitor in monitors['monitors']:
+        devices.append(UptimeRobotSensor(
+            apikey, up_robot, monitorid=monitor['id'],
+            name=monitor['friendly_name'], target=monitor['url']))
+
+    add_devices(devices, True)
+    return True
 
 
 class UptimeRobotSensor(Entity):
     """Representation of a Uptime Robot sensor."""
 
-    def __init__(self, apikey, monitorid, name, target):
+    def __init__(self, apikey, up_robot, monitorid, name, target):
         """Initialize the sensor."""
-        self._name = name
-        self._monitorid = monitorid
         self._apikey = apikey
+        self._monitorid = str(monitorid)
+        self._name = name
         self._target = target
-        self.update()
-
-    def update(self):
-        """Get the latest state of the sensor."""
-        from pyuptimerobot import UptimeRobot
-        uptimerobot = UptimeRobot()
-
-        monitor = uptimerobot.getMonitors(self._apikey, str(self._monitorid))
-        if not monitor:
-            _LOGGER.debug("Failed to get new state, trying again later.")
-            return False
-        else:
-            monitorinfo = monitor['monitors'][0]
-            state = monitorinfo['status']
-            if state == 2:
-                self._state = 'Online'
-            else:
-                self._state = 'Offline'
+        self._up_robot = up_robot
+        self._state = None
 
     @property
     def name(self):
@@ -94,12 +71,21 @@ class UptimeRobotSensor(Entity):
     @property
     def icon(self):
         """Return the icon to use in the frontend."""
-        return ICON
+        return 'mdi:server'
 
     @property
     def device_state_attributes(self):
         """Return the state attributes of the sensor."""
         return {
             ATTR_TARGET: self._target,
-            ATTR_MONITORID: self._monitorid,
         }
+
+    def update(self):
+        """Get the latest state of the sensor."""
+        monitor = self._up_robot.getMonitors(self._apikey, self._monitorid)
+        if not monitor or monitor.get('stat') != 'ok':
+            _LOGGER.debug("Failed to get new state, trying again later.")
+            return False
+
+        status = monitor['monitors'][0]['status']
+        self._state = 'Online' if status == 2 else 'Offline'
