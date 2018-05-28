ed7b933337d7e4f63e81b9a6527100d1904173bd
Jason Hu <awaregit@gmail.com>
2018-05-28 16:22:25 -0700

    Remove unused import

--- homeassistant/components/binary_sensor/nest.py
+++ homeassistant/components/binary_sensor/nest.py
@@ -8,10 +8,9 @@ from itertools import chain
 import logging
 
 from homeassistant.components.binary_sensor import (BinarySensorDevice)
-from homeassistant.components.nest import DATA_NEST, SIGNAL_NEST_UPDATE
+from homeassistant.components.nest import DATA_NEST
 from homeassistant.components.sensor.nest import NestSensor
 from homeassistant.const import CONF_MONITORED_CONDITIONS
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
 DEPENDENCIES = ['nest']
 
