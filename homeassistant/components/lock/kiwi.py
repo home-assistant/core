41422b368f900e5a1684f6af44d6c59ca32c2aa4
Christoph <christoph@gerneth.info>
2018-05-29 12:39:24 +0200

    added async lock state reset for locking state

--- homeassistant/components/lock/kiwi.py
+++ homeassistant/components/lock/kiwi.py
@@ -11,7 +11,10 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.lock import (LockDevice, PLATFORM_SCHEMA)
 from homeassistant.const import (
-    CONF_PASSWORD, CONF_USERNAME, ATTR_ID, ATTR_LONGITUDE, ATTR_LATITUDE)
+    CONF_PASSWORD, CONF_USERNAME, ATTR_ID, ATTR_LONGITUDE, ATTR_LATITUDE,
+    STATE_LOCKED, STATE_UNLOCKED)
+from homeassistant.helpers.event import async_call_later
+from homeassistant.core import callback
 
 REQUIREMENTS = ['kiwiki-client==0.1']
 
@@ -20,6 +23,8 @@ _LOGGER = logging.getLogger(__name__)
 ATTR_TYPE = 'hardware_type'
 ATTR_PERMISSION = 'permission'
 ATTR_CAN_INVITE = 'can_invite_others'
+
+UNLOCK_MAINTAIN_TIME = 5
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string,
@@ -42,6 +47,7 @@ class KiwiLock(LockDevice):
         self._sensor = kiwi_lock
         self._client = client
         self.lock_id = kiwi_lock['sensor_id']
+        self._state = None
 
         address = kiwi_lock.get('address')
         lat = address.pop('lat', None)
@@ -69,12 +75,19 @@ class KiwiLock(LockDevice):
     @property
     def is_locked(self):
         """Return true if lock is locked."""
-        return True
+        if self._state is not None:
+            return self._state == STATE_LOCKED
 
     @property
     def device_state_attributes(self):
         """Return the device specific state attributes."""
         return self._device_attrs
+
+    @callback
+    def clear_unlock_state(self, _):
+        """Clear unlock state automatically."""
+        self._state = STATE_LOCKED
+        self.async_schedule_update_ha_state()
 
     def unlock(self, **kwargs):
         """Unlock the device."""
@@ -83,3 +96,7 @@ class KiwiLock(LockDevice):
             self._client.open_door(self.lock_id)
         except KiwiException:
             _LOGGER.error("failed to open door")
+        else:
+            self._state = STATE_UNLOCKED
+            async_call_later(self.hass, UNLOCK_MAINTAIN_TIME,
+                             self.clear_unlock_state)
