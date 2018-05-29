4fee6171707c0b5fa0d781bf1f89b091b975f8fa
cdce8p <30130371+cdce8p@users.noreply.github.com>
2018-05-29 22:43:26 +0200

    Update HAP-python to 2.2.2 (#14674)
    * Pass driver to accessory
    * Added 'hk_driver' fixture for tests

---
+++ tests/components/homekit/conftest.py
@@ -0,0 +1,16 @@
+"""HomeKit session fixtures."""
+from unittest.mock import patch
+
+import pytest
+
+from pyhap.accessory_driver import AccessoryDriver
+
+
+@pytest.fixture(scope='session')
+def hk_driver():
+    """Return a custom AccessoryDriver instance for HomeKit accessory init."""
+    with patch('pyhap.accessory_driver.Zeroconf'), \
+        patch('pyhap.accessory_driver.AccessoryEncoder'), \
+        patch('pyhap.accessory_driver.HAPServer'), \
+            patch('pyhap.accessory_driver.AccessoryDriver.publish'):
+        return AccessoryDriver(pincode=b'123-45-678')
