638d4a7460821f4c4292abc1b03c4a4cc70535ca
Robert Svensson <Kane610@users.noreply.github.com>
2018-05-29 16:09:53 +0200

    deCONZ - Option to load or not to load clip sensors on start (#14480)
    * Option to load or not to load clip sensors on start
    
    * Full flow
    
    * Fix config flow and add tests
    
    * Fix attribute dark reporting properly
    
    * Imported and properly configured deCONZ shouldn't need extra input to create config entry

--- homeassistant/components/deconz/const.py
+++ homeassistant/components/deconz/const.py
@@ -8,3 +8,5 @@ CONFIG_FILE = 'deconz.conf'
 DATA_DECONZ_EVENT = 'deconz_events'
 DATA_DECONZ_ID = 'deconz_entities'
 DATA_DECONZ_UNSUB = 'deconz_dispatchers'
+
+CONF_ALLOW_CLIP_SENSOR = 'allow_clip_sensor'
