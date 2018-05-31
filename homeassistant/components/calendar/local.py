4deb516bab729d20eae891bfc306b802b38c2f73
Thibault Cohen <titilambert@gmail.com>
2018-05-31 00:14:09 -0400

    Remove useless API endpoint

--- homeassistant/components/calendar/local.py
+++ homeassistant/components/calendar/local.py
@@ -3,7 +3,6 @@ import asyncio
 import logging
 
 from homeassistant.components import http
-from homeassistant.components.calendar import DOMAIN
 
 SUBDOMAIN = 'local'
 DEPENDENCIES = ['http']
@@ -14,7 +13,6 @@ EVENT = 'calendar_updated'
 @asyncio.coroutine
 def async_setup_platform(hass, config, add_devices, discovery_info=None):
     """Initialize the calendar."""
-    hass.http.register_view(CalendarView)
     hass.http.register_view(CalendarListView)
 
     yield from hass.components.frontend.async_register_built_in_panel(
@@ -23,31 +21,11 @@ def async_setup_platform(hass, config, add_devices, discovery_info=None):
     return True
 
 
-class CalendarView(http.HomeAssistantView):
-    """View to retrieve calendar content."""
-
-    url = '/api/calendar'
-    name = "api:calendar"
-    extra_urls = ['/api/calendar/']
-
-    async def post(self, request):
-        """Retrieve calendar items."""
-        data = await request.json()
-        if data.get("calendars"):
-            ret = []
-            for calendar_name in data.get("calendars"):
-                if calendar_name in request.app['hass'].data[DOMAIN]:
-                    ret.extend(request.app['hass'].
-                               data[DOMAIN][calendar_name].items)
-            return self.json(ret)
-        return self.json([])
-
-
 class CalendarListView(http.HomeAssistantView):
     """View to retrieve calendar list."""
 
-    url = '/api/calendar-list'
-    name = "api:calendar-list"
+    url = '/api/calendars'
+    name = "api:calendars"
 
     async def get(self, request):
         """Retrieve calendar list."""
