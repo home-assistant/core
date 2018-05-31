a746d35eb0575851343cbb6a09bf3d673dceefff
Diogo Gomes <dgomes@av.it.pt>
2018-05-31 16:16:03 +0100

    lint

--- tests/components/weather/test_ipma.py
+++ tests/components/weather/test_ipma.py
@@ -1,10 +1,7 @@
 """The tests for the IPMA weather component."""
-import re
 import unittest
 from unittest.mock import patch
 from collections import namedtuple
-
-import pyipma 
 
 from homeassistant.components import weather
 from homeassistant.components.weather import (
@@ -13,14 +10,14 @@ from homeassistant.components.weather import (
 from homeassistant.util.unit_system import METRIC_SYSTEM
 from homeassistant.setup import setup_component
 
-from tests.common import load_fixture, get_test_home_assistant, mock_coro
+from tests.common import get_test_home_assistant, mock_coro
 
 MockStation = namedtuple('Station', ['latitude', 'longitude',
-                                         'idAreaAviso', 'idConselho',
-                                         'idDistrito', 'idRegiao',
-                                         'globalIdLocal', 'local'])
+                                     'idAreaAviso', 'idConselho',
+                                     'idDistrito', 'idRegiao',
+                                     'globalIdLocal', 'local'])
 
-mockStation = MockStation(40.61, -8.64, 'AAA', 1, 1, 1, 100000, 'HomeTown')
+MOCK_STATION = MockStation(40.61, -8.64, 'AAA', 1, 1, 1, 100000, 'HomeTown')
 
 Forecast = namedtuple('Forecast', ['precipitaProb', 'tMin', 'tMax',
                                    'predWindDir', 'idWeatherType',
@@ -28,20 +25,23 @@ Forecast = namedtuple('Forecast', ['precipitaProb', 'tMin', 'tMax',
                                    'forecastDate', 'classPrecInt',
                                    'latitude', 'description'])
 
-mockForecast = Forecast(73.0, 13.7, 18.7, 'NW', 6, 2, -8.64,
-                        '2018-05-31', 2, 40.61,
-                        'Aguaceiros, com vento Moderado de Noroeste')
+MOCK_FORECAST = Forecast(73.0, 13.7, 18.7, 'NW', 6, 2, -8.64,
+                         '2018-05-31', 2, 40.61,
+                         'Aguaceiros, com vento Moderado de Noroeste')
 
 Observation = namedtuple('Observation', ['temperature', 'humidity',
                                          'windspeed', 'winddirection',
                                          'precipitation', 'pressure',
                                          'description'])
 
-Station = namedtuple('ObservationStation', ['latitude', 'longitude', 'stationID',
-                                         'stationName', 'currentObs'])
+Station = namedtuple('ObservationStation', ['latitude', 'longitude',
+                                            'stationID', 'stationName',
+                                            'currentObs'])
 
-mockObservation = Observation(18, 71.0, 3.94, 'NW', 0, 1000.0, '---')
-mockObservationStation = Station(40.61, -8.64, 1, 'HomeTown', mockObservation)
+MOCK_OBSERVATION = Observation(18, 71.0, 3.94, 'NW', 0, 1000.0, '---')
+MOCK_OBSERVATION_STATION = Station(40.61, -8.64, 1, 'HomeTown',
+                                   MOCK_OBSERVATION)
+
 
 class TestIPMA(unittest.TestCase):
     """Test the IPMA weather component."""
@@ -50,21 +50,21 @@ class TestIPMA(unittest.TestCase):
         """Setup things to be run when tests are started."""
         self.hass = get_test_home_assistant()
         self.hass.config.units = METRIC_SYSTEM
-        self.lat = self.hass.config.latitude = 40.00 
+        self.lat = self.hass.config.latitude = 40.00
         self.lon = self.hass.config.longitude = -8.00
 
     def tearDown(self):
         """Stop down everything that was started."""
         self.hass.stop()
 
-    @patch("pyipma.api.IPMA_API.stations", return_value=mock_coro([mockStation]))
+    @patch("pyipma.api.IPMA_API.stations",
+           return_value=mock_coro([MOCK_STATION]))
     @patch("pyipma.api.IPMA_API.forecast",
-        return_value=mock_coro([mockForecast]))
+           return_value=mock_coro([MOCK_FORECAST]))
     @patch("pyipma.api.IPMA_API.observations",
-        return_value=mock_coro([mockObservationStation]))
+           return_value=mock_coro([MOCK_OBSERVATION_STATION]))
     def test_setup(self, mock_observation, mock_forecast, mock_stations):
         """Test for successfully setting up the IPMA platform."""
-
         self.assertTrue(setup_component(self.hass, weather.DOMAIN, {
             'weather': {
                 'name': 'HomeTown',
