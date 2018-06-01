eef87cc9f3dbaa6ba7b12ee962d56051e0741c75
michaeldavie <michael.davie@gmail.com>
2018-05-31 22:59:57 -0400

    Added battery attribute

--- homeassistant/components/climate/sensibo.py
+++ homeassistant/components/climate/sensibo.py
@@ -154,7 +154,8 @@ class SensiboClimate(ClimateDevice):
     @property
     def device_state_attributes(self):
         """Return the state attributes."""
-        return {ATTR_CURRENT_HUMIDITY: self.current_humidity}
+        return {ATTR_CURRENT_HUMIDITY: self.current_humidity,
+                'battery': self.current_battery}
 
     @property
     def temperature_unit(self):
@@ -190,6 +191,14 @@ class SensiboClimate(ClimateDevice):
     def current_humidity(self):
         """Return the current humidity."""
         return self._measurements['humidity']
+
+    @property
+    def current_battery(self):
+        """Return the current battery voltage."""
+        if 'batteryVoltage' in self._measurements:
+            return self._measurements['batteryVoltage']
+        else:
+            return None
 
     @property
     def current_temperature(self):
