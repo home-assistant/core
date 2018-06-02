7107c3f2de00142f49ba9d78c404a8cf5e93c59f
Christoph <christoph@gerneth.info>
2018-06-02 14:00:02 +0200

    added more check in setup_platform

--- homeassistant/components/lock/kiwi.py
+++ homeassistant/components/lock/kiwi.py
@@ -40,7 +40,12 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     except KiwiException as e:
         _LOGGER.error(e.msg)
         return False
-    add_devices([KiwiLock(lock, kiwi) for lock in kiwi.get_locks()], True)
+    available_locks = kiwi.get_locks()
+    if not available_locks:
+        # No locks found; abort setup routine.
+        _LOGGER.info("No KIWI locks found in your account.")
+        return False
+    add_devices([KiwiLock(lock, kiwi) for lock in available_locks], True)
 
 
 class KiwiLock(LockDevice):
