b82131a9dba2b869e2c91b56e7de0c9642c415df
quthla <quthla@users.noreply.github.com>
2018-06-03 15:26:23 +0200

    Add Kodi OnResume event (#14790)

--- homeassistant/components/media_player/kodi.py
+++ homeassistant/components/media_player/kodi.py
@@ -294,6 +294,7 @@ class KodiDevice(MediaPlayerDevice):
             # Register notification listeners
             self._ws_server.Player.OnPause = self.async_on_speed_event
             self._ws_server.Player.OnPlay = self.async_on_speed_event
+            self._ws_server.Player.OnResume = self.async_on_speed_event
             self._ws_server.Player.OnSpeedChanged = self.async_on_speed_event
             self._ws_server.Player.OnStop = self.async_on_stop
             self._ws_server.Application.OnVolumeChanged = \
