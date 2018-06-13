77111dd7eeaa0abadc7aa56218183910e919f852
Giuseppe Lo Presti <glopresti@gmail.com>
2018-06-13 22:09:42 +0200

    Incorporated the advanced Throttle class to support adaptive throttling, as opposed to integrating it in the core framework.
    Following code review, it was suggested to implement the
    specialised Throttle class in this platform instead of making a
    change in the general util package. Except that the required change
    (about 4 LoC) is part of the only relevant piece of code of that
    class, therefore this commit includes a full copy of the Throttle
    class from homeassistant.util, plus the extra feature to support
    adaptive throttling.

--- homeassistant/components/sensor/netatmo.py
+++ homeassistant/components/sensor/netatmo.py
@@ -7,6 +7,9 @@ https://home-assistant.io/components/sensor.netatmo/
 import logging
 from datetime import timedelta
 from time import time
+from functools import wraps
+import asyncio
+import threading
 
 import voluptuous as vol
 
@@ -15,8 +18,8 @@ from homeassistant.const import (
     TEMP_CELSIUS, DEVICE_CLASS_HUMIDITY, DEVICE_CLASS_TEMPERATURE,
     STATE_UNKNOWN)
 from homeassistant.helpers.entity import Entity
-from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
+from homeassistant.util.dt import utcnow
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -333,7 +336,116 @@ class NetAtmoData(object):
                 # Never hammer the NetAtmo API more than
                 # twice per update interval
                 newinterval = NETATMO_UPDATE_INTERVAL / 2
-            _LOGGER.info("NetAtmo refresh interval reset to %d seconds",
-                         newinterval)
+            _LOGGER.warning("NetAtmo refresh interval reset to %d seconds",
+                            newinterval)
             self.update = Throttle(timedelta(seconds=newinterval),
                                    immediate_throttle=True)(self._update)
+
+
+class Throttle(object):
+    """A class for throttling the execution of tasks.
+
+    This class is a clone of `homeassistant.util.Throttle`, except for adding
+    the immediate_throttle capability described below. If more sensors wish
+    to benefit from adaptive throttling, this class should be merged in the
+    util package.
+
+    This method decorator adds a cooldown to a method to prevent it from being
+    called more then 1 time within the timedelta interval `min_time` after it
+    returned its result.
+
+    Calling a method a second time during the interval will return None.
+
+    Pass keyword argument `no_throttle=True` to the wrapped method to make
+    the call not throttled.
+
+    Initialize the decorator with the optional `immediate_throttle=True`
+    argument to ensure that the wrapped method is not called until the
+    first `min_time` interval has passed. This option overrides the above
+    `no_throttle` argument.
+
+    Decorator takes in an optional second timedelta interval to throttle the
+    'no_throttle' calls.
+
+    Adds a datetime attribute `last_call` to the method.
+    """
+
+    def __init__(self, min_time, limit_no_throttle=None,
+                 immediate_throttle=False):
+        """Initialize the throttle."""
+        self.min_time = min_time
+        self.limit_no_throttle = limit_no_throttle
+        self.immediate_throttle = immediate_throttle
+
+    def __call__(self, method):
+        """Caller for the throttle."""
+        # Make sure we return a coroutine if the method is async.
+        if asyncio.iscoroutinefunction(method):
+            async def throttled_value():
+                """Stand-in function for when real func is being throttled."""
+                return None
+        else:
+            def throttled_value():
+                """Stand-in function for when real func is being throttled."""
+                return None
+
+        if self.limit_no_throttle is not None:
+            method = Throttle(self.limit_no_throttle)(method)
+
+        # Different methods that can be passed in:
+        #  - a function
+        #  - an unbound function on a class
+        #  - a method (bound function on a class)
+
+        # We want to be able to differentiate between function and unbound
+        # methods (which are considered functions).
+        # All methods have the classname in their qualname separated by a '.'
+        # Functions have a '.' in their qualname if defined inline, but will
+        # be prefixed by '.<locals>.' so we strip that out.
+        is_func = (not hasattr(method, '__self__') and
+                   '.' not in method.__qualname__.split('.<locals>.')[-1])
+
+        @wraps(method)
+        def wrapper(*args, **kwargs):
+            """Wrap that allows wrapped to be called only once per min_time.
+
+            If we cannot acquire the lock, it is running so return None.
+            """
+            # pylint: disable=protected-access
+            if hasattr(method, '__self__'):
+                host = method.__self__
+            elif is_func:
+                host = wrapper
+            else:
+                host = args[0] if args else wrapper
+
+            if not hasattr(host, '_throttle'):
+                host._throttle = {}
+
+            if id(self) not in host._throttle:
+                host._throttle[id(self)] = [threading.Lock(), None]
+            throttle = host._throttle[id(self)]
+
+            if not throttle[0].acquire(False):
+                return throttled_value()
+
+            try:
+                if self.immediate_throttle:
+                    # This only applies the very first time
+                    throttle[1] = utcnow()
+                    self.immediate_throttle = False
+                    return throttled_value()
+
+                # Check if method is never called or no_throttle is given
+                force = kwargs.pop('no_throttle', False) or not throttle[1]
+
+                if force or utcnow() - throttle[1] > self.min_time:
+                    result = method(*args, **kwargs)
+                    throttle[1] = utcnow()
+                    return result
+                return throttled_value()
+
+            finally:
+                throttle[0].release()
+
+        return wrapper
