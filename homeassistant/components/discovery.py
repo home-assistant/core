0d5dc9b53ccd4041fe15007da293dbda604ef88c
Fabian Affolter <mail@fabian-affolter.ch>
2018-06-17 15:38:00 +0200

    Upgrade netdisco to 1.5.0 (#15016)

--- homeassistant/components/discovery.py
+++ homeassistant/components/discovery.py
@@ -21,7 +21,7 @@ from homeassistant.helpers.event import async_track_point_in_utc_time
 from homeassistant.helpers.discovery import async_load_platform, async_discover
 import homeassistant.util.dt as dt_util
 
-REQUIREMENTS = ['netdisco==1.4.1']
+REQUIREMENTS = ['netdisco==1.5.0']
 
 DOMAIN = 'discovery'
 
