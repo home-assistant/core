7f138a3b54cf00467ff05243b5dddd36b4cf9118
Mattias Welponer <mattias@welponer.net>
2018-06-19 23:36:58 +0200

    Remove config_flow tests

--- tests/components/homematicip_cloud/test_config_flow.py
+++ tests/components/homematicip_cloud/test_config_flow.py
@@ -1,17 +1,2 @@
 """Tests for HomematicIP Cloud config flow."""
-import asyncio
-from unittest.mock import Mock, patch
 
-import aiohue
-import pytest
-import voluptuous as vol
-
-from homeassistant.components.homematicip_cloud import config_flow, const
-
-from tests.common import MockConfigEntry, mock_coro
-
-async def test_flow_works(hass, aioclient_mock):
-    """Test config flow ."""
-    flow = config_flow.HomematicipCloudFlowHandler()
-    flow.hass = hass
-    await flow.async_step_init()
