49845d9398f642ec20e77c62a7487724b050a9bf
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-06-20 15:13:08 -0400

    Rename experimental UI to lovelace (#15065)
    * Rename experimental UI to lovelace
    
    * Bump frontend to 20180620.0

--- homeassistant/components/frontend/__init__.py
+++ homeassistant/components/frontend/__init__.py
@@ -21,11 +21,12 @@ from homeassistant.components import websocket_api
 from homeassistant.config import find_config_file, load_yaml_config_file
 from homeassistant.const import CONF_NAME, EVENT_THEMES_UPDATED
 from homeassistant.core import callback
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.translation import async_get_translations
 from homeassistant.loader import bind_hass
 from homeassistant.util.yaml import load_yaml
 
-REQUIREMENTS = ['home-assistant-frontend==20180619.0']
+REQUIREMENTS = ['home-assistant-frontend==20180620.0']
 
 DOMAIN = 'frontend'
 DEPENDENCIES = ['api', 'websocket_api', 'http', 'system_log']
@@ -106,9 +107,9 @@ SCHEMA_GET_TRANSLATIONS = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend({
     vol.Required('type'): WS_TYPE_GET_TRANSLATIONS,
     vol.Required('language'): str,
 })
-WS_TYPE_GET_EXPERIMENTAL_UI = 'frontend/experimental_ui'
-SCHEMA_GET_EXPERIMENTAL_UI = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend({
-    vol.Required('type'): WS_TYPE_GET_EXPERIMENTAL_UI,
+WS_TYPE_GET_LOVELACE_UI = 'frontend/lovelace_config'
+SCHEMA_GET_LOVELACE_UI = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend({
+    vol.Required('type'): WS_TYPE_GET_LOVELACE_UI,
 })
 
 
@@ -216,8 +217,8 @@ async def async_setup(hass, config):
         WS_TYPE_GET_TRANSLATIONS, websocket_get_translations,
         SCHEMA_GET_TRANSLATIONS)
     hass.components.websocket_api.async_register_command(
-        WS_TYPE_GET_EXPERIMENTAL_UI, websocket_experimental_config,
-        SCHEMA_GET_EXPERIMENTAL_UI)
+        WS_TYPE_GET_LOVELACE_UI, websocket_lovelace_config,
+        SCHEMA_GET_LOVELACE_UI)
     hass.http.register_view(ManifestJSONView)
 
     conf = config.get(DOMAIN, {})
@@ -265,7 +266,7 @@ async def async_setup(hass, config):
     await asyncio.wait(
         [async_register_built_in_panel(hass, panel) for panel in (
             'dev-event', 'dev-info', 'dev-service', 'dev-state',
-            'dev-template', 'dev-mqtt', 'kiosk', 'experimental-ui')],
+            'dev-template', 'dev-mqtt', 'kiosk', 'lovelace')],
         loop=hass.loop)
 
     hass.data[DATA_FINALIZE_PANEL] = async_finalize_panel
@@ -499,15 +500,26 @@ def websocket_get_translations(hass, connection, msg):
     hass.async_add_job(send_translations())
 
 
-def websocket_experimental_config(hass, connection, msg):
-    """Send experimental UI config over websocket config."""
+def websocket_lovelace_config(hass, connection, msg):
+    """Send lovelace UI config over websocket config."""
     async def send_exp_config():
-        """Send experimental frontend config."""
-        config = await hass.async_add_job(
-            load_yaml, hass.config.path('experimental-ui.yaml'))
+        """Send lovelace frontend config."""
+        error = None
+        try:
+            config = await hass.async_add_job(
+                load_yaml, hass.config.path('ui-lovelace.yaml'))
+            message = websocket_api.result_message(
+                msg['id'], config
+            )
+        except FileNotFoundError:
+            error = ('file_not_found',
+                     'Could not find ui-lovelace.yaml in your config dir.')
+        except HomeAssistantError as err:
+            error = 'load_error', str(err)
 
-        connection.send_message_outside(websocket_api.result_message(
-            msg['id'], config
-        ))
+        if error is not None:
+            message = websocket_api.error_message(msg['id'], *error)
+
+        connection.send_message_outside(message)
 
     hass.async_add_job(send_exp_config())
