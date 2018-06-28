2698cd4ebc1831327d4d6c2a22a79c8863e5249a
Eduard van Valkenburg <edvan@microsoft.com>
2018-06-28 23:43:10 +0200

    fixed rebasing issues

--- homeassistant/components/media_player/cast.py
+++ homeassistant/components/media_player/cast.py
@@ -4,14 +4,7 @@ Provide functionality to interact with Cast devices on the network.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/media_player.cast/
 """
-<<<<<<< HEAD
-<<<<<<< HEAD
 import asyncio
-=======
->>>>>>> Lint cleanup (#15103)
-=======
-import asyncio
->>>>>>> Fix cast config (#15143)
 import logging
 import threading
 from typing import Optional, Tuple
