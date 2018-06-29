1ba2b84daeeabc03693aafd2562b9af6edfc9ff3
Sriram Vaidyanathan <vaidyasr@gmail.com>
2018-06-29 19:53:14 +0530

    Fixed Indentation error (#15210)
    * Fixed Indentation error
    
    * Update xiaomi.py

--- homeassistant/components/camera/xiaomi.py
+++ homeassistant/components/camera/xiaomi.py
@@ -113,14 +113,16 @@ class XiaomiCamera(Camera):
         except error_perm as exc:
             _LOGGER.error('Unable to find path: %s - %s', first_dir, exc)
             return False
+
         if self._model == MODEL_XIAOFANG:
             dirs = [d for d in ftp.nlst() if '.' not in d]
             if not dirs:
                 _LOGGER.warning("There don't appear to be any uploaded videos")
                 return False
 
-        latest_dir = dirs[-1]
-        ftp.cwd(latest_dir)
+            latest_dir = dirs[-1]
+            ftp.cwd(latest_dir)
+
         videos = [v for v in ftp.nlst() if '.tmp' not in v]
         if not videos:
             _LOGGER.info('Video folder "%s" is empty; delaying', latest_dir)
