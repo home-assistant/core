11e3cecf68f8e371d88e96e262dd436657b04e20
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-07-03 13:20:42 -0400

    Disable the calendar panel (#15282)

--- homeassistant/components/calendar/__init__.py
+++ homeassistant/components/calendar/__init__.py
@@ -41,8 +41,9 @@ async def async_setup(hass, config):
     hass.http.register_view(CalendarListView(component))
     hass.http.register_view(CalendarEventView(component))
 
-    await hass.components.frontend.async_register_built_in_panel(
-        'calendar', 'calendar', 'hass:calendar')
+    # Doesn't work in prod builds of the frontend: home-assistant-polymer#1289
+    # await hass.components.frontend.async_register_built_in_panel(
+    #     'calendar', 'calendar', 'hass:calendar')
 
     await component.async_setup(config)
     return True
