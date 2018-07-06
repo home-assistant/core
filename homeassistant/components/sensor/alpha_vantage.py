fbc1b185d8017b78e79d799cd737cd0ed8956e2c
Robert van Bregt <robert@robertvanbregt.nl>
2018-07-06 22:39:35 +0200

    Import time

--- homeassistant/components/sensor/alpha_vantage.py
+++ homeassistant/components/sensor/alpha_vantage.py
@@ -5,6 +5,7 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.alpha_vantage/
 """
 from datetime import timedelta
+import time
 import logging
 
 import voluptuous as vol
