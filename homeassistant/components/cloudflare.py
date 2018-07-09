a502eeb687c54f0c92fa23d179be322b4fb0ee4e
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-07-09 20:38:26 +0200

    Changed records to be required

--- homeassistant/components/cloudflare.py
+++ homeassistant/components/cloudflare.py
@@ -24,7 +24,7 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(CONF_EMAIL): cv.string,
         vol.Required(CONF_API_KEY): cv.string,
         vol.Required(CONF_ZONE): cv.string,
-        vol.Optional(CONF_RECORDS, default='None'):
+        vol.Required(CONF_RECORDS, default='None'):
             vol.All(cv.ensure_list, [cv.string]),
     })
 }, extra=vol.ALLOW_EXTRA)
