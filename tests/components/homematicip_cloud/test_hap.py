6bbc23c0de46b31a01d06dfd2704b6ff8fc4d37e
Mattias Welponer <mattias@welponer.net>
2018-07-13 03:57:41 +0200

    Add HomematicIP alarm control panel (#15342)
    * Add HomematicIP security zone
    
    * Update access point tests
    
    * Fix state if not armed and coments
    
    * Add comment for the empty state_attributes
    
    * Fix comment
    
    * Fix spelling

--- tests/components/homematicip_cloud/test_hap.py
+++ tests/components/homematicip_cloud/test_hap.py
@@ -65,8 +65,10 @@ async def test_hap_setup_works(aioclient_mock):
         assert await hap.async_setup() is True
 
     assert hap.home is home
-    assert len(hass.config_entries.async_forward_entry_setup.mock_calls) == 5
+    assert len(hass.config_entries.async_forward_entry_setup.mock_calls) == 6
     assert hass.config_entries.async_forward_entry_setup.mock_calls[0][1] == \
+        (entry, 'alarm_control_panel')
+    assert hass.config_entries.async_forward_entry_setup.mock_calls[1][1] == \
         (entry, 'binary_sensor')
 
 
@@ -104,10 +106,10 @@ async def test_hap_reset_unloads_entry_if_setup():
 
     assert hap.home is home
     assert len(hass.services.async_register.mock_calls) == 0
-    assert len(hass.config_entries.async_forward_entry_setup.mock_calls) == 5
+    assert len(hass.config_entries.async_forward_entry_setup.mock_calls) == 6
 
     hass.config_entries.async_forward_entry_unload.return_value = \
         mock_coro(True)
     await hap.async_reset()
 
-    assert len(hass.config_entries.async_forward_entry_unload.mock_calls) == 5
+    assert len(hass.config_entries.async_forward_entry_unload.mock_calls) == 6
