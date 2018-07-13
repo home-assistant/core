66bd88ba085a9f733dbfc8d19750269ca084f9f7
CrazYoshi <mar862002@hotmail.com>
2018-07-13 14:27:06 +0200

    ignore E504 error
    ignore E504 pep8 errors

--- homeassistant/components/sensor/ebusd.py
+++ homeassistant/components/sensor/ebusd.py
@@ -1,4 +1,3 @@
-
 """
 Support for Ebusd daemon for communication with eBUS heating systems.
 For more details about ebusd, please refer to the documentation at
@@ -33,70 +32,38 @@ MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=15)
 
 # SensorTypes: 0='decimal', 1='time-schedule', 2='switch', 3='op-mode'
 SENSOR_TYPES = {
-    'ActualFlowTemperatureDesired':
-    ['Hc1ActualFlowTempDesired', '°C', 'mdi:thermometer', 0],
-    'MaxFlowTemperatureDesired':
-    ['Hc1MaxFlowTempDesired', '°C', 'mdi:thermometer', 0],
-    'MinFlowTemperatureDesired':
-    ['Hc1MinFlowTempDesired', '°C', 'mdi:thermometer', 0],
-    'PumpStatus':
-    ['Hc1PumpStatus', None, 'mdi:toggle-switch', 2],
-    'Hc1SummerTemperatureLimit':
-    ['Hc1SummerTempLimit', '°C', 'mdi:weather-sunny', 0],
-    'HolidayTemperature':
-    ['HolidayTemp', '°C', 'mdi:thermometer', 0],
-    'HWTemperatureDesired':
-    ['HwcTempDesired', '°C', 'mdi:thermometer', 0],
-    'HWTimerMonday':
-    ['hwcTimer.Monday', None, 'mdi:timer', 1],
-    'HWTimerTuesday':
-    ['hwcTimer.Tuesday', None, 'mdi:timer', 1],
-    'HWTimerWednesday':
-    ['hwcTimer.Wednesday', None, 'mdi:timer', 1],
-    'HWTimerThursday':
-    ['hwcTimer.Thursday', None, 'mdi:timer', 1],
-    'HWTimerFriday':
-    ['hwcTimer.Friday', None, 'mdi:timer', 1],
-    'HWTimerSaturday':
-    ['hwcTimer.Saturday', None, 'mdi:timer', 1],
-    'HWTimerSunday':
-    ['hwcTimer.Sunday', None, 'mdi:timer', 1],
-    'WaterPressure':
-    ['WaterPressure', 'bar', 'mdi:water-pump', 0],
-    'Zone1RoomZoneMapping':
-    ['z1RoomZoneMapping', None, 'mdi:label', 0],
-    'Zone1NightTemperature':
-    ['z1NightTemp', '°C', 'mdi:weather-night', 0],
-    'Zone1DayTemperature':
-    ['z1DayTemp', '°C', 'mdi:weather-sunny', 0],
-    'Zone1HolidayTemperature':
-    ['z1HolidayTemp', '°C', 'mdi:thermometer', 0],
-    'Zone1RoomTemperature':
-    ['z1RoomTemp', '°C', 'mdi:thermometer', 0],
-    'Zone1ActualRoomTemperatureDesired':
-    ['z1ActualRoomTempDesired', '°C', 'mdi:thermometer', 0],
-    'Zone1TimerMonday':
-    ['z1Timer.Monday', None, 'mdi:timer', 1],
-    'Zone1TimerTuesday':
-    ['z1Timer.Tuesday', None, 'mdi:timer', 1],
-    'Zone1TimerWednesday':
-    ['z1Timer.Wednesday', None, 'mdi:timer', 1],
-    'Zone1TimerThursday':
-    ['z1Timer.Thursday', None, 'mdi:timer', 1],
-    'Zone1TimerFriday':
-    ['z1Timer.Friday', None, 'mdi:timer', 1],
-    'Zone1TimerSaturday':
-    ['z1Timer.Saturday', None, 'mdi:timer', 1],
-    'Zone1TimerSunday':
-    ['z1Timer.Sunday', None, 'mdi:timer', 1],
-    'Zone1OperativeMode':
-    ['z1OpMode', None, 'mdi:math-compass', 3],
-    'ContinuosHeating':
-    ['ContinuosHeating', '°C', 'mdi:weather-snowy', 0],
-    'PowerEnergyConsumptionLastMonth':
-    ['PrEnergySumHcLastMonth', 'kWh', 'mdi:flash', 0],
-    'PowerEnergyConsumptionThisMonth':
-    ['PrEnergySumHcThisMonth', 'kWh', 'mdi:flash', 0]
+    'ActualFlowTemperatureDesired': ['Hc1ActualFlowTempDesired', '°C', 'mdi:thermometer', 0],
+    'MaxFlowTemperatureDesired': ['Hc1MaxFlowTempDesired', '°C', 'mdi:thermometer', 0],
+    'MinFlowTemperatureDesired': ['Hc1MinFlowTempDesired', '°C', 'mdi:thermometer', 0],
+    'PumpStatus': ['Hc1PumpStatus', None, 'mdi:toggle-switch', 2],
+    'Hc1SummerTemperatureLimit': ['Hc1SummerTempLimit', '°C', 'mdi:weather-sunny', 0],
+    'HolidayTemperature': ['HolidayTemp', '°C', 'mdi:thermometer', 0],
+    'HWTemperatureDesired': ['HwcTempDesired', '°C', 'mdi:thermometer', 0],
+    'HWTimerMonday': ['hwcTimer.Monday', None, 'mdi:timer', 1],
+    'HWTimerTuesday': ['hwcTimer.Tuesday', None, 'mdi:timer', 1],
+    'HWTimerWednesday': ['hwcTimer.Wednesday', None, 'mdi:timer', 1],
+    'HWTimerThursday': ['hwcTimer.Thursday', None, 'mdi:timer', 1],
+    'HWTimerFriday': ['hwcTimer.Friday', None, 'mdi:timer', 1],
+    'HWTimerSaturday': ['hwcTimer.Saturday', None, 'mdi:timer', 1],
+    'HWTimerSunday': ['hwcTimer.Sunday', None, 'mdi:timer', 1],
+    'WaterPressure': ['WaterPressure', 'bar', 'mdi:water-pump', 0],
+    'Zone1RoomZoneMapping': ['z1RoomZoneMapping', None, 'mdi:label', 0],
+    'Zone1NightTemperature': ['z1NightTemp', '°C', 'mdi:weather-night', 0],
+    'Zone1DayTemperature': ['z1DayTemp', '°C', 'mdi:weather-sunny', 0],
+    'Zone1HolidayTemperature': ['z1HolidayTemp', '°C', 'mdi:thermometer', 0],
+    'Zone1RoomTemperature': ['z1RoomTemp', '°C', 'mdi:thermometer', 0],
+    'Zone1ActualRoomTemperatureDesired': ['z1ActualRoomTempDesired', '°C', 'mdi:thermometer', 0],
+    'Zone1TimerMonday': ['z1Timer.Monday', None, 'mdi:timer', 1],
+    'Zone1TimerTuesday': ['z1Timer.Tuesday', None, 'mdi:timer', 1],
+    'Zone1TimerWednesday': ['z1Timer.Wednesday', None, 'mdi:timer', 1],
+    'Zone1TimerThursday': ['z1Timer.Thursday', None, 'mdi:timer', 1],
+    'Zone1TimerFriday': ['z1Timer.Friday', None, 'mdi:timer', 1],
+    'Zone1TimerSaturday': ['z1Timer.Saturday', None, 'mdi:timer', 1],
+    'Zone1TimerSunday': ['z1Timer.Sunday', None, 'mdi:timer', 1],
+    'Zone1OperativeMode': ['z1OpMode', None, 'mdi:math-compass', 3],
+    'ContinuosHeating': ['ContinuosHeating', '°C', 'mdi:weather-snowy', 0],
+    'PowerEnergyConsumptionLastMonth': ['PrEnergySumHcLastMonth', 'kWh', 'mdi:flash', 0],
+    'PowerEnergyConsumptionThisMonth': ['PrEnergySumHcThisMonth', 'kWh', 'mdi:flash', 0]
 }
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -246,14 +213,11 @@ class Ebusd(Entity):
             self.data.update(self._name)
             if self._name in self.data.value:
                 if self._type == 0:
-                    self._state =
-                    format(float(self.data.value[self._name]), '.1f')
+                    self._state = format(float(self.data.value[self._name]), '.1f')
                 elif self._type == 1:
-                    self._state =
-                    timer_format(self.data.value[self._name])
+                    self._state = timer_format(self.data.value[self._name])
                 elif self._type == 2:
-                    self._state =
-                    STATE_ON if self.data.value[self._name] == 1 else STATE_OFF
+                    self._state = STATE_ON if self.data.value[self._name] == 1 else STATE_OFF
                 elif self._type == 3:
                     self._state = self.data.value[self._name]
         except RuntimeError:
