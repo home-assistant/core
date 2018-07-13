a662d63ca810bd56cfb537bab1cca54041a28709
turbokongen <turbokongen@hotmail.com>
2018-07-13 19:21:24 +0200

    Reversed generic type name

--- homeassistant/components/zwave/discovery_schemas.py
+++ homeassistant/components/zwave/discovery_schemas.py
@@ -39,7 +39,7 @@ DISCOVERY_SCHEMAS = [
     {const.DISC_COMPONENT: 'climate',
      const.DISC_GENERIC_DEVICE_CLASS: [
          const.GENERIC_TYPE_THERMOSTAT,
-         const.GENERIC_TYPE_MULTILEVEL_SENSOR],
+         const.GENERIC_TYPE_SENSOR_MULTILEVEL],
      const.DISC_VALUES: dict(DEFAULT_VALUES_SCHEMA, **{
          const.DISC_PRIMARY: {
              const.DISC_COMMAND_CLASS: [
