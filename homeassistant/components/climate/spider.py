2548beb40d300c4437cdaf255a9d4bc0ced94158
Peter Nijssen <peter@peternijssen.nl>
2018-07-16 23:51:25 +0200

    add spider thermostats

---
+++ homeassistant/components/climate/spider.py
@@ -0,0 +1,143 @@
+"""
+Support for Spider thermostats.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/climate.spider/
+"""
+
+import logging
+
+from homeassistant.components.climate import (
+    ATTR_TEMPERATURE, STATE_COOL, STATE_HEAT, STATE_IDLE,
+    SUPPORT_TARGET_TEMPERATURE, SUPPORT_OPERATION_MODE, ClimateDevice)
+from homeassistant.components.spider import DOMAIN
+from homeassistant.const import TEMP_CELSIUS
+
+DEPENDENCIES = ['spider']
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the Spider thermostat."""
+    thermostats = hass.data[DOMAIN]['thermostats']
+    api = hass.data[DOMAIN]['controller']
+    for thermostat in thermostats:
+        add_devices(
+            [SpiderThermostat(api, thermostat['id'], thermostat['name'])],
+            True
+        )
+
+
+class SpiderThermostat(ClimateDevice):
+    """Representation of a thermostat."""
+
+    def __init__(self, client, thermostat_id, name):
+        """Initialize the thermostat."""
+        self.client = client
+        self._id = thermostat_id
+        self._name = name
+        self._master = False
+        self._thermostat = None
+        self._current_temperature = None
+        self._target_temperature = None
+        self._min_temp = None
+        self._max_temp = None
+        self._operation = STATE_IDLE
+        self._operation_list = [
+            STATE_HEAT,
+            STATE_COOL,
+        ]
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        supports = SUPPORT_TARGET_TEMPERATURE
+
+        if self._master is True:
+            supports = supports | SUPPORT_OPERATION_MODE
+
+        return supports
+
+    @property
+    def name(self):
+        """Return the name of the thermostat, if any."""
+        return self._name
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        return TEMP_CELSIUS
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        return self._current_temperature
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return self._target_temperature
+
+    @property
+    def min_temp(self):
+        """Return the minimum temperature."""
+        return self._min_temp
+
+    @property
+    def max_temp(self):
+        """Return the maximum temperature."""
+        return self._max_temp
+
+    @property
+    def current_operation(self: ClimateDevice) -> str:
+        """Return current operation ie. heat, cool, idle."""
+        return self._operation
+
+    @property
+    def operation_list(self):
+        """Return the list of available operation modes."""
+        return self._operation_list
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if temperature is None:
+            return
+
+        self.client.set_temperature(self._thermostat, temperature)
+
+    def set_operation_mode(self, operation_mode):
+        """Set new target operation mode."""
+        self.client.set_operation_mode(self._thermostat, operation_mode)
+
+    def update(self):
+        """Get the latest data."""
+        try:
+            # Only let the master thermostat refresh
+            # and let the others use the cache
+            thermostats = self.client.get_thermostats(
+                force_refresh=self._master)
+            for thermostat in thermostats:
+                if thermostat['id'] == self._id:
+                    self._thermostat = thermostat
+
+        except StopIteration:
+            _LOGGER.error("No data from the Itho Daalderop API")
+            return
+
+        for prop in self._thermostat['properties']:
+            if prop['id'] == 'AmbientTemperature':
+                self._current_temperature = float(prop['status'])
+            if prop['id'] == 'SetpointTemperature':
+                self._target_temperature = float(prop['status'])
+                self._min_temp = float(prop['min'])
+                self._max_temp = float(prop['max'])
+            if prop['id'] == 'OperationMode':
+                self._master = True
+                if prop['status'] == 'Heat':
+                    self._operation = STATE_HEAT
+                elif prop['status'] == 'Cool':
+                    self._operation = STATE_COOL
+                else:
+                    self._operation = STATE_IDLE
---
+++ homeassistant/components/spider.py
@@ -0,0 +1,50 @@
+"""
+Support for Spider Smart devices.
+
+For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/spider/
+"""
+import logging
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD)
+
+REQUIREMENTS = ['itho_daalderop_api==1.0.4']
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = 'spider'
+
+SPIDER_COMPONENTS = [
+    'climate'
+]
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_PASSWORD): cv.string,
+        vol.Required(CONF_USERNAME): cv.string,
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+
+def setup(hass, config):
+    """Set up Spider Component."""
+    from itho_daalderop_api import IthoDaalderop_API, UnauthorizedException
+
+    username = config[DOMAIN][CONF_USERNAME]
+    password = config[DOMAIN][CONF_PASSWORD]
+
+    try:
+        api = IthoDaalderop_API(username, password)
+
+        hass.data[DOMAIN] = {
+            'controller': api,
+            'thermostats': api.get_thermostats()
+        }
+        _LOGGER.debug("Connection with Itho Daalderop API succeeded")
+        return True
+    except UnauthorizedException:
+        _LOGGER.error("Can't connect to the Itho Daalderop API")
+        return False
