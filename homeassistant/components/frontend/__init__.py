056d298ec48d27eb542a57a5fa2155f9fd58baf2
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-07-17 09:24:51 +0200

    Add current user WS command (#15485)

--- homeassistant/components/frontend/__init__.py
+++ homeassistant/components/frontend/__init__.py
@@ -257,7 +257,7 @@ async def async_setup(hass, config):
     await asyncio.wait(
         [async_register_built_in_panel(hass, panel) for panel in (
             'dev-event', 'dev-info', 'dev-service', 'dev-state',
-            'dev-template', 'dev-mqtt', 'kiosk', 'lovelace')],
+            'dev-template', 'dev-mqtt', 'kiosk', 'lovelace', 'profile')],
         loop=hass.loop)
 
     hass.data[DATA_FINALIZE_PANEL] = async_finalize_panel
