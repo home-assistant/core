6fbda30668bcb7d76d236d0f96533deca4963814
Steven Looman <steven.looman@gmail.com>
2018-07-17 21:49:01 +0200

    Use our own HTTP server to listen for events.

--- homeassistant/components/media_player/dlna_dmr.py
+++ homeassistant/components/media_player/dlna_dmr.py
@@ -8,6 +8,7 @@ https://home-assistant.io/components/media_player.dlna_dmr/
 
 import asyncio
 import logging
+import socket
 import urllib.parse
 from datetime import datetime
 
@@ -16,6 +17,7 @@ import async_timeout
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
+from homeassistant.components.http import HomeAssistantHTTP
 from homeassistant.components.http.view import (
     request_handler_factory, HomeAssistantView)
 from homeassistant.components.media_player import (
@@ -34,6 +36,8 @@ from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 
+DOMAIN = 'dlna_dmr'
+
 REQUIREMENTS = [
     'async-upnp-client==0.12.0',
 ]
@@ -41,9 +45,15 @@ REQUIREMENTS = [
 DEPENDENCIES = ['http']
 
 DEFAULT_NAME = 'DLNA Digital Media Renderer'
+DEFAULT_LISTEN_PORT = 8301
+
+CONF_LISTEN_IP = 'listen_ip'
+CONF_LISTEN_PORT = 'listen_port'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_URL): cv.string,
+    vol.Optional(CONF_LISTEN_IP): cv.string,
+    vol.Optional(CONF_LISTEN_PORT, default=DEFAULT_LISTEN_PORT): cv.port,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
 })
 
@@ -61,7 +71,6 @@ HOME_ASSISTANT_UPNP_CLASS_MAPPING = {
     'channel': 'object.item.videoItem',
     'playlist': 'object.item.playlist',
 }
-
 HOME_ASSISTANT_UPNP_MIME_TYPE_MAPPING = {
     'music': 'audio/*',
     'tvshow': 'video/*',
@@ -70,25 +79,78 @@ HOME_ASSISTANT_UPNP_MIME_TYPE_MAPPING = {
     'channel': 'video/*',
     'playlist': 'playlist/*',
 }
-
-UPNP_DEVICE_MEDIA_RENDERER = 'urn:schemas-upnp-org:device:MediaRenderer:1'
+UPNP_DEVICE_MEDIA_RENDERER = [
+    'urn:schemas-upnp-org:device:MediaRenderer:1',
+    'urn:schemas-upnp-org:device:MediaRenderer:2',
+    'urn:schemas-upnp-org:device:MediaRenderer:3',
+]
 
 _LOGGER = logging.getLogger(__name__)
 
+SETUP_LOCK = asyncio.Lock()
 
-def start_notify_view(hass, requester):
+
+def determine_listen_ip(target_url, config):
+    """
+    Determine the IP and port to listen on.
+
+    If specified in config, use config.
+    Otherwise try to figure it out.
+    """
+    server_host = config.get(CONF_LISTEN_IP)
+    server_port = config.get(CONF_LISTEN_PORT)
+
+    if server_host is None:
+        # determine server_host by opening a UDP socket to target,
+        # but don't actually send anything
+        parsed = urllib.parse.urlparse(target_url)
+        target_host = parsed.hostname
+        target_port = parsed.port
+        temp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        temp_sock.connect((target_host, target_port))
+        server_host = temp_sock.getsockname()[0]
+        temp_sock.close()
+
+    return server_host, server_port
+
+
+async def async_start_notify_view(hass, server_host, server_port, requester):
     """Register notify view."""
-    hass_data = hass.data[__name__]
-    name = 'notify_view'
-    if name in hass_data:
-        return
+    hass_data = hass.data[DOMAIN]
+    if 'notify_view' in hass_data:
+        return hass_data['notify_view']
 
-    view = UpnpNotifyView(hass)
-    hass.http.register_view(view)
-    hass_data[name] = view
+    # fire up a HTTP server
+    server = HomeAssistantHTTP(
+        hass,
+        server_host=server_host,
+        server_port=server_port,
+        api_password=None,
+        ssl_certificate=None,
+        ssl_peer_certificate=None,
+        ssl_key=None,
+        cors_origins=None,
+        use_x_forwarded_for=False,
+        trusted_proxies=[],
+        trusted_networks=[],
+        login_threshold=0,
+        is_ban_enabled=False
+    )
+    hass.bus.async_listen_once(
+        EVENT_HOMEASSISTANT_STOP, server.stop)
+    hass_data['notify_server'] = server
+
+    base_url = 'http://%s:%s/' % (server_host, server_port)
+    view = UpnpNotifyView(base_url)
+    server.register_view(view)
+    hass_data['notify_view'] = view
 
     from async_upnp_client import UpnpEventHandler
     view.event_handler = UpnpEventHandler(view.callback_url, requester)
+
+    await server.start()
+    _LOGGER.debug('started notify server on: %s', base_url)
+    return view
 
 
 async def async_setup_platform(hass: HomeAssistant,
@@ -99,7 +161,7 @@ async def async_setup_platform(hass: HomeAssistant,
     # ensure this is a DLNA DMR device, if found via discovery
     if discovery_info and \
        'upnp_device_type' in discovery_info and \
-       discovery_info['upnp_device_type'] != UPNP_DEVICE_MEDIA_RENDERER:
+       discovery_info['upnp_device_type'] not in UPNP_DEVICE_MEDIA_RENDERER:
         _LOGGER.debug('Device is not a MediaRenderer: %s, device_type: %s',
                       discovery_info.get('ssdp_description'),
                       discovery_info['upnp_device_type'])
@@ -112,16 +174,21 @@ async def async_setup_platform(hass: HomeAssistant,
         url = discovery_info['ssdp_description']
         name = discovery_info['name']
 
-    if __name__ not in hass.data:
-        hass.data[__name__] = {}
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
 
     requester = build_requester(hass)
 
     # ensure view has been started
-    hass.async_run_job(start_notify_view, hass, requester)
-    while 'notify_view' not in hass.data[__name__]:
-        asyncio.sleep(0.1)
-    notify_view = hass.data[__name__]['notify_view']
+    with (await SETUP_LOCK):
+        # discovered components don't get default values in config
+        config[CONF_LISTEN_PORT] = config.get(CONF_LISTEN_PORT,
+                                              DEFAULT_LISTEN_PORT)
+        server_host, server_port = determine_listen_ip(url, config)
+        notify_view = await async_start_notify_view(hass,
+                                                    server_host,
+                                                    server_port,
+                                                    requester)
 
     # create device
     from async_upnp_client import UpnpFactory
@@ -146,9 +213,9 @@ class UpnpNotifyView(HomeAssistantView):
     name = 'api:dlna_dmr:notify'
     requires_auth = False
 
-    def __init__(self, hass):
+    def __init__(self, base_url):
         """Initializer."""
-        self.hass = hass
+        self.base_url = base_url
         self.event_handler = None
         self._registered_services = {}
         self._backlog = {}
@@ -168,8 +235,7 @@ class UpnpNotifyView(HomeAssistantView):
     @property
     def callback_url(self):
         """Full URL to be called by device/service."""
-        base_url = self.hass.config.api.base_url
-        return urllib.parse.urljoin(base_url, self.url)
+        return urllib.parse.urljoin(self.base_url, self.url)
 
 
 def build_requester(hass):
@@ -226,8 +292,13 @@ class DlnaDmrDevice(MediaPlayerDevice):
         self._on_stop_unsubscriber = None
         self._subscription_renew_time = None
 
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP,
-                                   self._async_on_hass_stop)
+    async def async_added_to_hass(self):
+        """Callback when added."""
+        bus = self.hass.bus
+
+        # register unsubscribe on stop
+        bus.async_listen_once(EVENT_HOMEASSISTANT_STOP,
+                              self._async_on_hass_stop)
 
     @property
     def udn(self):
@@ -252,7 +323,7 @@ class DlnaDmrDevice(MediaPlayerDevice):
             self._available = True
         except (asyncio.TimeoutError, aiohttp.ClientError):
             self._available = False
-            _LOGGER.debug("Device unavailable 1")
+            _LOGGER.debug("Device unavailable")
             raise
 
         # do we need to (re-)subscribe?
@@ -266,7 +337,7 @@ class DlnaDmrDevice(MediaPlayerDevice):
                 self._subscription_renew_time = datetime.now() + timeout / 2
             except (asyncio.TimeoutError, aiohttp.ClientError):
                 self._available = False
-                _LOGGER.debug("Device unavailable 2")
+                _LOGGER.debug("Could not (re)subscribe")
                 raise
 
     def _on_event(self, service, state_variables):
