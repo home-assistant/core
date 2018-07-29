01482082d098b9c05befeb712b5d1d4647560681
Fabian Affolter <mail@fabian-affolter.ch>
2018-07-29 23:37:38 +0200

    Upgrade pyowm to 2.9.0 (#15736)

--- homeassistant/components/weather/openweathermap.py
+++ homeassistant/components/weather/openweathermap.py
@@ -18,7 +18,7 @@ from homeassistant.const import (
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util import Throttle
 
-REQUIREMENTS = ['pyowm==2.8.0']
+REQUIREMENTS = ['pyowm==2.9.0']
 
 _LOGGER = logging.getLogger(__name__)
 
