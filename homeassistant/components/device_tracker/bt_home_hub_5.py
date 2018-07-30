cfd9ea479501fa47c03b259c2d21ff865c475659
ahobsonsayers <32173585+ahobsonsayers@users.noreply.github.com>
2018-07-30 21:40:02 +0100

    Moved part of code to a published PyPi library

--- homeassistant/components/device_tracker/bt_home_hub_5.py
+++ homeassistant/components/device_tracker/bt_home_hub_5.py
@@ -1,20 +1,22 @@
 """
 Support for BT Home Hub 5.
-
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/device_tracker.bt_home_hub_5/
 """
+
 import logging
 import re
-from html.parser import HTMLParser
 
 import requests
-import voluptuous as vol
+from html_table_extractor import HTMLParser
 
 import homeassistant.helpers.config_validation as cv
-from homeassistant.components.device_tracker import (
-    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+import voluptuous as vol
+from homeassistant.components.device_tracker import (DOMAIN, PLATFORM_SCHEMA,
+                                                     DeviceScanner)
 from homeassistant.const import CONF_HOST
+
+REQUIREMENTS = ['html-table-parser-python3==0.1.2']
 
 _LOGGER = logging.getLogger(__name__)
 _MAC_REGEX = re.compile(r'(([0-9A-Fa-f]{1,2}:){5}[0-9A-Fa-f]{1,2})')
@@ -25,7 +27,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 # pylint: disable=unused-argument
-def get_scanner(hass, config):
+def get_scanner(config):
     """Return a BT Home Hub 5 scanner if successful."""
     scanner = BTHomeHub5DeviceScanner(config[DOMAIN])
 
@@ -65,7 +67,6 @@ class BTHomeHub5DeviceScanner(DeviceScanner):
 
     def _update_info(self):
         """Ensure the information from the BT Home Hub 5 is up to date.
-
         Return boolean if scanning successful.
         """
         if not self.success_init:
@@ -95,72 +96,6 @@ def _get_homehub_data(url):
         return _parse_homehub_response(response.text)
     else:
         _LOGGER.error("Invalid response from Home Hub: %s", response)
-
-
-class HTMLTableParser(HTMLParser):
-    """ This class serves as a html table parser. It is able to parse multiple
-    tables which you feed in. You can access the result per .tables field.
-    """
-
-    def __init__(
-            self,
-            decode_html_entities=False,
-            data_separator=' ',
-    ):
-
-        HTMLParser.__init__(self)
-
-        self._parse_html_entities = decode_html_entities
-        self._data_separator = data_separator
-
-        self._in_td = False
-        self._in_th = False
-        self._current_table = []
-        self._current_row = []
-        self._current_cell = []
-        self.tables = []
-
-    def handle_starttag(self, tag, attrs):
-        """ We need to remember the opening point for the content of interest.
-        The other tags (<table>, <tr>) are only handled at the closing point.
-        """
-        if tag == 'td':
-            self._in_td = True
-        if tag == 'th':
-            self._in_th = True
-
-    def handle_data(self, data):
-        """ This is where we save content to a cell. """
-        if self._in_td or self._in_th:
-            self._current_cell.append(data.strip())
-
-    def handle_charref(self, name):
-        """ Handle HTML encoded characters. """
-
-        if self._parse_html_entities:
-            self.handle_data(self.html.unescape('&#{};'.format(name)))
-
-    def handle_endtag(self, tag):
-        """ Here we exit the tags. If the closing tag is </tr>, we know that we
-        can save our currently parsed cells to the current table as a row and
-        prepare for a new row. If the closing tag is </table>, we save the
-        current table and prepare for a new one.
-        """
-        if tag == 'td':
-            self._in_td = False
-        elif tag == 'th':
-            self._in_th = False
-
-        if tag in ['td', 'th']:
-            final_cell = self._data_separator.join(self._current_cell).strip()
-            self._current_row.append(final_cell)
-            self._current_cell = []
-        elif tag == 'tr':
-            self._current_table.append(self._current_row)
-            self._current_row = []
-        elif tag == 'table':
-            self.tables.append(self._current_table)
-            self._current_table = []
 
 
 def _parse_homehub_response(data_str):
