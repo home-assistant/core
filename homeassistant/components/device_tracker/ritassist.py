e146c6712bd62339d77df2a70ecd4759b5b7b800
depl0y <wim@wim.me>
2018-07-31 16:38:54 +0200

    Remove authentication caching, can be done by external module Clean up unused variables

--- homeassistant/components/device_tracker/ritassist.py
+++ homeassistant/components/device_tracker/ritassist.py
@@ -7,7 +7,6 @@ https://home-assistant.io/components/device_tracker.ritassist/
 import logging
 import voluptuous as vol
 
-from ritassist import (API, Authentication)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.device_tracker import PLATFORM_SCHEMA
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
@@ -16,12 +15,9 @@ REQUIREMENTS = ['ritassist==0.3']
 
 _LOGGER = logging.getLogger(__name__)
 
-CLIENT_UUID_CONFIG_FILE = '.ritassist.conf'
-
 CONF_CLIENT_ID = 'client_id'
 CONF_CLIENT_SECRET = 'client_secret'
 CONF_INCLUDE = 'include'
-CONF_INTERVAL = 'interval'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string,
@@ -45,41 +41,30 @@ class RitAssistDeviceScanner:
     def __init__(self, hass, config, see, discovery_info):
         """Initialize RitAssistDeviceScanner."""
         from homeassistant.helpers.event import track_utc_time_change
+        from ritassist import API
 
-        self._discovery_info = discovery_info
-        self._hass = hass
-        self._devices = []
-        self._config = config
+        self._include = config.get(CONF_INCLUDE)
         self._see = see
 
-        self._file = self._hass.config.path(CLIENT_UUID_CONFIG_FILE)
-        self._authentication_info = self.load_authentication(self._file)
+        self._api = API(config.get(CONF_CLIENT_ID),
+                        config.get(CONF_CLIENT_SECRET),
+                        config.get(CONF_USERNAME),
+                        config.get(CONF_PASSWORD))
 
-        self._api = API(self._config.get(CONF_CLIENT_ID),
-                        self._config.get(CONF_CLIENT_SECRET),
-                        self._config.get(CONF_USERNAME),
-                        self._config.get(CONF_PASSWORD))
-
-        track_utc_time_change(self._hass,
+        track_utc_time_change(hass,
                               lambda now: self._refresh(),
                               second=range(0, 60, 30))
         self._refresh()
-
-    @property
-    def devices(self):
-        """Return the devices detected."""
-        return self._devices
 
     def _refresh(self) -> None:
         """Refresh device information from the platform."""
         import requests
 
         try:
-            include = self._config.get(CONF_INCLUDE)
-            self._devices = self._api.get_devices()
+            devices = self._api.get_devices()
 
-            for device in self._devices:
-                if (not include or device.license_plate in include):
+            for device in devices:
+                if (not self._include or device.license_plate in self._include):
                     self._see(dev_id=device.plate_as_id,
                               gps=(device.latitude, device.longitude),
                               attributes=device.state_attributes,
@@ -87,31 +72,3 @@ class RitAssistDeviceScanner:
 
         except requests.exceptions.ConnectionError:
             _LOGGER.error('ConnectionError: Could not connect to RitAssist')
-
-    def save_authentication(self, filename):
-        """Save the authentication information to a file for caching."""
-        from homeassistant.util.json import save_json
-
-        json = {
-            'access_token': self._authentication_info.access_token,
-            'refresh_token': self._authentication_info.refresh_token,
-            'expires_in': self._authentication_info.expires_in,
-            'authenticated': self._authentication_info.authenticated
-        }
-        if not save_json(filename, json):
-            _LOGGER.error("Failed to save configuration file")
-
-    def load_authentication(self, filename):
-        """Load the authentication information from a file for caching."""
-        from homeassistant.util.json import load_json
-
-        data = load_json(filename)
-        if data:
-            result = Authentication()
-            result.set_json(data)
-            if not result.is_valid():
-                return None
-
-            return result
-        else:
-            return None
