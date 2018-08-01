890a4a8d937a25cc9ace564b413407f451d7f778
Robert Svensson <Kane610@users.noreply.github.com>
2018-08-01 11:03:08 +0200

    deCONZ - support for power plugs (#15752)
    * Initial commit for deCONZ switch support
    
    * Fix hound comment
    
    * Fix martins comment; platforms shouldn't depend on another platform
    
    * Fix existing tests
    
    * New tests
    
    * Clean up unnecessary methods
    
    * Bump requirement to v43
    
    * Added device state attributes to light

--- tests/components/deconz/test_init.py
+++ tests/components/deconz/test_init.py
@@ -99,8 +99,8 @@ async def test_setup_entry_successful(hass):
     assert hass.data[deconz.DOMAIN]
     assert hass.data[deconz.DATA_DECONZ_ID] == {}
     assert len(hass.data[deconz.DATA_DECONZ_UNSUB]) == 1
-    assert len(mock_add_job.mock_calls) == 4
-    assert len(mock_config_entries.async_forward_entry_setup.mock_calls) == 4
+    assert len(mock_add_job.mock_calls) == 5
+    assert len(mock_config_entries.async_forward_entry_setup.mock_calls) == 5
     assert mock_config_entries.async_forward_entry_setup.mock_calls[0][1] == \
         (entry, 'binary_sensor')
     assert mock_config_entries.async_forward_entry_setup.mock_calls[1][1] == \
@@ -109,6 +109,8 @@ async def test_setup_entry_successful(hass):
         (entry, 'scene')
     assert mock_config_entries.async_forward_entry_setup.mock_calls[3][1] == \
         (entry, 'sensor')
+    assert mock_config_entries.async_forward_entry_setup.mock_calls[4][1] == \
+        (entry, 'switch')
 
 
 async def test_unload_entry(hass):
