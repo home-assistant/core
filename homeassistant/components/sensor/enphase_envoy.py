d90bd63de2de81e8846604a922e070f24db8d2c9
jesserizzo <jesse.rizzo@gmail.com>
2018-07-31 19:07:32 -0500

    Refactor

--- homeassistant/components/sensor/enphase_envoy.py
+++ homeassistant/components/sensor/enphase_envoy.py
@@ -4,70 +4,59 @@ Support for Enphase Envoy solar energy monitor.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.enphase_envoy/
 """
-
 import logging
 import voluptuous as vol
+
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 import homeassistant.helpers.config_validation as cv
+from homeassistant.const import (CONF_IP_ADDRESS)
+from homeassistant.const import (CONF_MONITORED_CONDITIONS)
 
 
 REQUIREMENTS = ['envoy_reader==0.1']
 _LOGGER = logging.getLogger(__name__)
 
-CONF_IP_ADDRESS = 'ip'
-CONF_MONITORED_CONDITIONS = 'monitored_conditions'
-DEFAULT_NAMES = [
-    "Envoy Current Energy Production",
-    "Envoy Today's Energy Production",
-    "Envoy Last Seven Days Energy Production",
-    "Envoy Lifetime Energy Production",
-    "Envoy Current Energy Consumption",
-    "Envoy Today's Energy Consumption",
-    "Envoy Last Seven Days Energy Consumption",
-    "Envoy Lifetime Energy Consumption"]
+SENSORS = {
+    "production": ("Envoy Current Energy Production", 'W'),
+    "daily_production": ("Envoy Today's Energy Production", "Wh"),
+    "7_days_production": ("Envoy Last Seven Days Energy Production", "Wh"),
+    "lifetime_production": ("Envoy Lifetime Energy Production", "Wh"),
+    "consumption": ("Envoy Current Energy Consumption", "W"),
+    "daily_consumption": ("Envoy Today's Energy Consumption", "Wh"),
+    "7_days_consumption": ("Envoy Last Seven Days Energy Consumption", "Wh"),
+    "lifetime_consumption": ("Envoy Lifetime Energy Consumption", "Wh")
+    }
 
-SENSOR_TYPES = [
-    "production", "daily_production", "7_days_production",
-    "lifetime_production", "consumption", "daily_consumption",
-    "7_days_consumption", "lifetime_consumption"]
 
 ICON = 'mdi:flash'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_IP_ADDRESS): cv.string,
-    vol.Optional(CONF_MONITORED_CONDITIONS): vol.All(cv.ensure_list)
-
-})
+    vol.Optional(CONF_MONITORED_CONDITIONS, default=list(SENSORS)):
+        vol.All(cv.ensure_list, [vol.In(list(SENSORS))])
+        })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Enphase Envoy sensor."""
-    ip_address = config.get(CONF_IP_ADDRESS)
-    monitored_conditions = config.get(CONF_MONITORED_CONDITIONS, {})
+    ip_address = config[CONF_IP_ADDRESS]
+    monitored_conditions = config[CONF_MONITORED_CONDITIONS]
 
-# Iterate through the list of sensors, adding it if either monitored
-# conditions has been left out of the config, or that the given sensor
-# is in the monitored conditions list
-    for i in range(8):
-        if monitored_conditions == {} or \
-                    SENSOR_TYPES[i] in monitored_conditions:
-            add_devices([Envoy(ip_address, DEFAULT_NAMES[i],
-                               SENSOR_TYPES[i])], True)
+# Iterate through the list of sensors
+    for condition in monitored_conditions:
+            add_devices([Envoy(ip_address, condition, SENSORS[condition][0],
+                        SENSORS[condition][1])], True)
 
 
 class Envoy(Entity):
     """Implementation of the Enphase Envoy sensors."""
 
-    def __init__(self, ip_address, name, sensor_type):
+    def __init__(self, ip_address, sensor_type, name, unit):
         """Initialize the sensor."""
         self._ip_address = ip_address
         self._name = name
-        if sensor_type == 'production' or sensor_type == 'consumption':
-            self._unit_of_measurement = 'W'
-        else:
-            self._unit_of_measurement = "Wh"
-
+        self._unit_of_measurement = unit
         self._type = sensor_type
         self._state = None
 
@@ -110,10 +99,10 @@ class Envoy(Entity):
             self._state = int(envoy_reader.consumption(self._ip_address))
         elif self._type == "daily_consumption":
             self._state = int(envoy_reader.daily_consumption(
-                              self._ip_address))
+                self._ip_address))
         elif self._type == "7_days_consumption":
             self._state = int(envoy_reader.seven_days_consumption(
-                              self._ip_address))
+                self._ip_address))
         elif self._type == "lifetime_consumption":
             self._state = int(envoy_reader.lifetime_consumption(
-                              self._ip_address))
+                self._ip_address))
