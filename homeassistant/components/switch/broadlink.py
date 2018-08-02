c94124f5d027440f87a794e3959891404f2096e2
cnk700i <39141120+cnk700i@users.noreply.github.com>
2018-08-02 09:11:43 +0800

    Problem:toggling MP1, switch's state will reset
    I think I have found the reason why MP1's state change abnormal after toggling. After turn_on/turn_off operation，HA will call switch.async_update_ha_state(True), which is defined in/components/switch/__init__.py to update data from MP1. However, the "@Throttle(TIME_BETWEEN_UPDATES)" method  using in _update() function, prevents HA from getting newest data of state, so HA updates state using a previous state. Remove "@Throttle(TIME_BETWEEN_UPDATES)" will solve the problem. By the way, HA has it's own cycle tasks to synchronize MP1' state  every "scan_interval" time.

--- homeassistant/components/switch/broadlink.py
+++ homeassistant/components/switch/broadlink.py
@@ -360,7 +360,7 @@ class BroadlinkMP1Switch(object):
         """Get status of outlet from cached status list."""
         return self._states['s{}'.format(slot)]
 
-    @Throttle(TIME_BETWEEN_UPDATES)
+    
     def update(self):
         """Fetch new state data for this device."""
         self._update()
