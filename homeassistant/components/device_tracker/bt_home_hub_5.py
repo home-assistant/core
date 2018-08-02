c3696b9fda5575166e54d3522022941601ac5570
ahobsonsayers <32173585+ahobsonsayers@users.noreply.github.com>
2018-08-03 00:08:28 +0100

    Moved API Specific Code to PyPi Repository

--- homeassistant/components/device_tracker/bt_home_hub_5.py
+++ homeassistant/components/device_tracker/bt_home_hub_5.py
@@ -7,7 +7,6 @@ https://home-assistant.io/components/device_tracker.bt_home_hub_5/
 
 import logging
 import re
-import requests
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
@@ -15,7 +14,7 @@ from homeassistant.components.device_tracker import (DOMAIN, PLATFORM_SCHEMA,
                                                      DeviceScanner)
 from homeassistant.const import CONF_HOST
 
-REQUIREMENTS = ['html-table-parser-python3==0.1.3']
+REQUIREMENTS = ['bthomehub5-devicelist==0.1.1']
 
 _LOGGER = logging.getLogger(__name__)
 _MAC_REGEX = re.compile(r'(([0-9A-Fa-f]{1,2}:){5}[0-9A-Fa-f]{1,2})')
@@ -36,19 +35,20 @@ class BTHomeHub5DeviceScanner(DeviceScanner):
     """This class queries a BT Home Hub 5."""
 
     def __init__(self, config):
+        import bthomehub5_devicelist
+
         """Initialise the scanner."""
         _LOGGER.info("Initialising BT Home Hub 5")
         self.host = config.get(CONF_HOST, '192.168.1.254')
         self.last_results = {}
-        self.url = 'http://{}/'.format(self.host)
 
         # Test the router is accessible
-        data = _get_homehub_data(self.url)
+        data = bthomehub5_devicelist.get_devicelist(self.host)
         self.success_init = data is not None
 
     def scan_devices(self):
         """Scan for new devices and return a list with found device IDs."""
-        self._update_info()
+        self.update_info()
 
         return (device for device in self.last_results)
 
@@ -56,23 +56,26 @@ class BTHomeHub5DeviceScanner(DeviceScanner):
         """Return the name of the given device or None if we don't know."""
         # If not initialised and not already scanned and not found.
         if device not in self.last_results:
-            self._update_info()
+            self.update_info()
 
             if not self.last_results:
                 return None
 
         return self.last_results.get(device)
 
-    def _update_info(self):
+    def update_info(self):
         """Ensure the information from the BT Home Hub 5 is up to date.
         Return boolean if scanning successful.
         """
+
+        import bthomehub5_devicelist
+
         if not self.success_init:
             return False
 
         _LOGGER.info("Scanning")
 
-        data = _get_homehub_data(self.url)
+        data = bthomehub5_devicelist.get_devicelist(self.host)
 
         if not data:
             _LOGGER.warning("Error scanning devices")
@@ -81,35 +84,3 @@ class BTHomeHub5DeviceScanner(DeviceScanner):
         self.last_results = data
 
         return True
-
-
-def _get_homehub_data(url):
-    """Retrieve data from BT Home Hub 5 and return parsed result."""
-    try:
-        response = requests.get(url, timeout=5)
-    except requests.exceptions.Timeout:
-        _LOGGER.exception("Connection to the router timed out")
-        return
-    if response.status_code == 200:
-        return _parse_homehub_response(response.text)
-    else:
-        _LOGGER.error("Invalid response from Home Hub: %s", response)
-
-
-def _parse_homehub_response(data_str):
-    """Parse the BT Home Hub 5 data format."""
-
-    from html_table_parser_python3 import HTMLTableParser
-
-    p = HTMLTableParser()
-    p.feed(data_str)
-
-    known_devices = p.tables[9]
-
-    devices = {}
-
-    for device in known_devices:
-        if len(device) == 5 and device[2] != '':
-            devices[device[2]] = device[1]
-
-    return devices
