df89bf2ea8c666b46d795719dbd0ab5a56b5587d
Adam Kemepnich <skiieraj@gmail.com>
2018-08-03 16:03:06 -0500

    Update ecoplug.py
    Updated changes requested by MartinHjelmare.

--- homeassistant/components/switch/ecoplug.py
+++ homeassistant/components/switch/ecoplug.py
@@ -2,7 +2,7 @@
 EcoPlugs Component for aut-discovering Wion and EcoPlugs automatically.
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/ecoplug/
+https://home-assistant.io/components/switch.ecoplug/
 """
 
 import logging
@@ -14,10 +14,34 @@ from homeassistant.components.switch import SwitchDevice
 
 DEFAULT_INVERT_LOGIC = False
 
-DOMAIN = 'ecoplug'
 REQUIREMENTS = ['pyecoplug==0.0.5']
 _LOGGER = logging.getLogger(__name__)
 SCAN_INTERVAL = timedelta(seconds=5)
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up EcoPlug switches."""
+    from pyecoplug import EcoDiscovery
+    discovered = hass.data
+
+    def add(plug):
+        """Find switches on the network."""
+        if plug.name not in discovered:
+            add_devices([EcoPlugSwitch(plug)])
+            discovered[plug.name] = plug
+
+    def remove(plug):
+        """Remove switch from the list."""
+        async_remove([EcoPlugSwitch(plug)], True)
+
+    disco = EcoDiscovery(add, remove)
+    disco.start()
+
+    def stop_disco(event):
+        """Stop device discovery."""
+        disco.stop()
+
+    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, stop_disco)
 
 
 class EcoPlugSwitch(SwitchDevice):
@@ -27,12 +51,7 @@ class EcoPlugSwitch(SwitchDevice):
         """Initialize the switch."""
         self._plug = plug
         self._name = plug.name or DEVICE_DEFAULT_NAME
-        self._state = self._plug.is_on()
-
-    @property
-    def should_poll(self):
-        """Return the polling state."""
-        return True
+        self._state = None
 
     @property
     def name(self):
@@ -47,39 +66,11 @@ class EcoPlugSwitch(SwitchDevice):
     def turn_on(self, **kwargs):
         """Turn the switch on."""
         self._plug.turn_on()
-        self.update()
 
     def turn_off(self, **kwargs):
         """Turn the switch off."""
         self._plug.turn_off()
-        self.update()
 
     def update(self):
         """Update the switch's status."""
-        _LOGGER.info('update')
         self._state = self._plug.is_on()
-
-
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up EcoPlug switches."""
-    from pyecoplug import EcoDiscovery
-    discovered = {}
-
-    def add(plug):
-        """Find switches on the network."""
-        if plug.name not in discovered:
-            add_devices([EcoPlugSwitch(plug)])
-            discovered[plug.name] = plug
-
-    def remove(plug):
-        """Unsure if there is a way to remove devices."""
-        pass
-
-    disco = EcoDiscovery(add, remove)
-    disco.start()
-
-    def stop_disco(event):
-        """Stop device discovery."""
-        disco.stop()
-
-    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, stop_disco)
