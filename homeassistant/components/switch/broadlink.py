2319e6d862bca9028de1e1fdc3478ea70b1a1f21
cnk700i <39141120+cnk700i@users.noreply.github.com>
2018-08-03 16:44:31 +0800

    Update broadlink.py

--- homeassistant/components/switch/broadlink.py
+++ homeassistant/components/switch/broadlink.py
@@ -360,7 +360,6 @@ class BroadlinkMP1Switch:
         """Get status of outlet from cached status list."""
         return self._states['s{}'.format(slot)]
 
-    
     def update(self):
         """Fetch new state data for this device."""
         self._update()
