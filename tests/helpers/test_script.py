91ce7af8b18ed2bbdff9df283d3a1725ca5e91c8
Luke Hovington <lukeh@hovo.id.au>
2018-08-05 20:52:56 +1000

    Fixing file permissions, adding test for new code

--- tests/helpers/test_script.py
+++ tests/helpers/test_script.py
@@ -412,6 +412,44 @@ class TestScriptHelper(unittest.TestCase):
         assert not script_obj.is_running
         assert len(events) == 1
 
+    def test_wait_template_timeout_proceed(self):
+        """Test the wait template with proceed."""
+        event = 'test_event'
+        events = []
+
+        @callback
+        def record_event(event):
+            """Add recorded event to set."""
+            events.append(event)
+
+        self.hass.bus.listen(event, record_event)
+
+        self.hass.states.set('switch.test', 'on')
+
+        script_obj = script.Script(self.hass, cv.SCRIPT_SCHEMA([
+            {'event': event},
+            {
+                'wait_template': "{{states.switch.test.state == 'off'}}",
+                'timeout': 5,
+                'proceed': True
+            },
+            {'event': event}]))
+
+        script_obj.run()
+        self.hass.block_till_done()
+
+        assert script_obj.is_running
+        assert script_obj.can_cancel
+        assert script_obj.last_action == event
+        assert len(events) == 1
+
+        future = dt_util.utcnow() + timedelta(seconds=5)
+        fire_time_changed(self.hass, future)
+        self.hass.block_till_done()
+
+        assert not script_obj.is_running
+        assert len(events) == 2
+
     def test_wait_template_variables(self):
         """Test the wait template with variables."""
         event = 'test_event'
