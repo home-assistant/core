0c120c99c3f470759c2ed2ccee23c1d31df96ff1
Ryan Davies <ryan@ryandavies.co.nz>
2018-08-05 12:07:06 +1200

    Google Maps - Add max_gps_accuracy option

--- homeassistant/components/device_tracker/google_maps.py
+++ homeassistant/components/device_tracker/google_maps.py
@@ -21,6 +21,7 @@ REQUIREMENTS = ['locationsharinglib==2.0.7']
 
 _LOGGER = logging.getLogger(__name__)
 
+CONF_MAX_GPS_ACCURACY = 'max_gps_accuracy'
 ATTR_ADDRESS = 'address'
 ATTR_FULL_NAME = 'full_name'
 ATTR_LAST_SEEN = 'last_seen'
@@ -31,6 +32,7 @@ CREDENTIALS_FILE = '.google_maps_location_sharing.cookies'
 MIN_TIME_BETWEEN_SCANS = timedelta(seconds=30)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_MAX_GPS_ACCURACY, default=100000): vol.Coerce(float),
     vol.Required(CONF_USERNAME): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
 })
@@ -53,6 +55,7 @@ class GoogleMapsScanner:
         self.see = see
         self.username = config[CONF_USERNAME]
         self.password = config[CONF_PASSWORD]
+        self.max_gps_accuracy = config[CONF_MAX_GPS_ACCURACY]
 
         try:
             self.service = Service(self.username, self.password,
@@ -76,18 +79,25 @@ class GoogleMapsScanner:
                 _LOGGER.warning("No location(s) shared with this account")
                 return
 
-            attrs = {
-                ATTR_ADDRESS: person.address,
-                ATTR_FULL_NAME: person.full_name,
-                ATTR_ID: person.id,
-                ATTR_LAST_SEEN: person.datetime,
-                ATTR_NICKNAME: person.nickname,
-            }
-            self.see(
-                dev_id=dev_id,
-                gps=(person.latitude, person.longitude),
-                picture=person.picture_url,
-                source_type=SOURCE_TYPE_GPS,
-                gps_accuracy=person.accuracy,
-                attributes=attrs,
-            )
+            if self.max_gps_accuracy is not None and \
+                    person.accuracy > self.max_gps_accuracy:
+                _LOGGER.info("Ignoring %s update because expected GPS "
+                             "accuracy %s is not met: %s",
+                             person.nickname, self.max_gps_accuracy,
+                             person.accuracy)
+            else:
+                attrs = {
+                    ATTR_ADDRESS: person.address,
+                    ATTR_FULL_NAME: person.full_name,
+                    ATTR_ID: person.id,
+                    ATTR_LAST_SEEN: person.datetime,
+                    ATTR_NICKNAME: person.nickname,
+                }
+                self.see(
+                    dev_id=dev_id,
+                    gps=(person.latitude, person.longitude),
+                    picture=person.picture_url,
+                    source_type=SOURCE_TYPE_GPS,
+                    gps_accuracy=person.accuracy,
+                    attributes=attrs,
+                )
