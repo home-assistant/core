dc0ff61fa912946c8301ab0a1b5d8bd28287f9a5
JC Connell <jamescarltonconnell@gmail.com>
2018-08-06 15:50:56 -0400

    Use py_noaa for request and data parsing.

--- homeassistant/components/sensor/noaa_tides.py
+++ homeassistant/components/sensor/noaa_tides.py
@@ -6,7 +6,7 @@ https://home-assistant.io/components/sensor.noaa_tides/
 """
 import logging
 import time
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 import requests
 import voluptuous as vol
@@ -15,19 +15,21 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (ATTR_ATTRIBUTION, CONF_NAME, STATE_UNKNOWN)
 from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+
+REQUIREMENTS = ['py_noaa==0.2.2']
 
 _LOGGER = logging.getLogger(__name__)
 
 API_URL = 'https://tidesandcurrents.noaa.gov/api/datagetter'
+CONF_STA_ID = 'station_id'
+CONF_TZ = 'timezone'
+CONF_UNITS = 'units'
 DEFAULT_ATTRIBUTION = "Data provided by NOAA"
 DEFAULT_NAME = 'NOAA Tides'
 DEFAULT_TZ = 'lst_ldt'
 
-CONF_STA_ID = 'station_id'
-CONF_TZ = 'timezone'
-CONF_UNITS = 'units'
-
-SCAN_INTERVAL = timedelta(seconds=3600)
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=60)
 
 TZS = ['gmt', 'lst', 'lst_ldt']
 UNITS = ['english', 'metric']
@@ -76,52 +78,52 @@ class NOAATidesAndCurrentsSensor(Entity):
     def device_state_attributes(self):
         """Return the state attributes of this device."""
         attr = {ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION}
-        predictions = self.data['predictions']
-        if "H" in predictions[0]['type']:
-            attr['high_tide_time'] = predictions[0]['t']
-            attr['high_tide_height'] = predictions[0]['v']
-            attr['low_tide_time'] = predictions[1]['t']
-            attr['low_tide_height'] = predictions[1]['v']
-        elif "L" in predictions[0]['type']:
-            attr['high_tide_time'] = predictions[1]['t']
-            attr['high_tide_height'] = predictions[1]['v']
-            attr['low_tide_time'] = predictions[0]['t']
-            attr['low_tide_height'] = predictions[0]['v']
+        if "H" == self.data['hi_lo'][1]:
+            attr['high_tide_time'] = self.data.index[1].strftime('%I:%M %p')
+            attr['high_tide_height'] = self.data['predicted_wl'][1]
+            attr['low_tide_time'] = self.data.index[2].strftime('%I:%M %p')
+            attr['low_tide_height'] = self.data['predicted_wl'][2]
+        elif "L" == self.data['hi_lo'][1]:
+            attr['low_tide_time'] = self.data.index[1].strftime('%I:%M %p')
+            attr['low_tide_height'] = self.data['predicted_wl'][1]
+            attr['high_tide_time'] = self.data.index[2].strftime('%I:%M %p')
+            attr['high_tide_height'] = self.data['predicted_wl'][2]
         return attr
 
     @property
     def state(self):
         """Return the state of the device."""
-        if self.data:
-            predictions = self.data['predictions']
-            api_time = time.strptime(predictions[0]['t'], "%Y-%m-%d %H:%M")
-            if "H" in predictions[0]['type']:
-                tidetime = time.strftime('%I:%M %p', api_time)
+        if self.data is not None:
+            api_time = self.data.index[0]
+            if "H" == self.data['hi_lo'][0]:
+                tidetime = api_time.strftime('%I:%M %p')
                 return "High tide at %s" % (tidetime)
-            if "L" in predictions[0]['type']:
-                tidetime = time.strftime('%I:%M %p', api_time)
+            if "L" == self.data['hi_lo'][0]:
+                tidetime = api_time.strftime('%I:%M %p')
                 return "Low tide at %s" % (tidetime)
             return STATE_UNKNOWN
         return STATE_UNKNOWN
 
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """Get the latest data from NOAA Tides and Currents API."""
-        begin = time.strftime("%Y%m%d %H:%M")
-        params = {'station': self._sta,
-                  'product': 'predictions',
-                  'application': 'HOMEASSISTANT',
-                  'begin_date': begin,
-                  'range': '72',
-                  'datum': 'MLLW',
-                  'time_zone': self._tz,
-                  'units': self._units,
-                  'interval': 'hilo',
-                  'format': 'json',
-                  }
+        from py_noaa import coops
+        begin = datetime.now()
+        delta = timedelta(days=2)
+        end = begin + delta
         try:
-            self.data = requests.get(API_URL, params=params, timeout=10).json()
+            df = coops.get_data(
+                begin_date=begin.strftime("%Y%m%d %H:%M"),
+                end_date=end.strftime("%Y%m%d %H:%M"),
+                stationid=self._sta,
+                product="predictions",
+                datum="MLLW",
+                interval="hilo",
+                units=self._units,
+                time_zone=self._tz)
+            self.data = df.head()
             _LOGGER.debug("Data = %s", self.data)
-            _LOGGER.info("Recent Tide data queried with start time set to")
+            _LOGGER.info("Recent Tide data queried with start time set to %s", begin.strftime("%m-%d-%Y %H:%M"))
         except ValueError as err:
             _LOGGER.error("Check NOAA Tides and Currents %s", err.args)
             self.data = None
