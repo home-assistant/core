47fa92842575ef6d5b3d49351cb519977ee9feb7
Paulus Schoutsen <balloob@gmail.com>
2018-08-06 13:01:32 +0200

    Bumped version to 0.76.0.dev0

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 """Constants used by Home Assistant components."""
 MAJOR_VERSION = 0
-MINOR_VERSION = 75
-PATCH_VERSION = '2'
+MINOR_VERSION = 76
+PATCH_VERSION = '0.dev0'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 REQUIRED_PYTHON_VER = (3, 5, 3)
