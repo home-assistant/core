15cf623356ff453b3d6250dced2563412ee6c6f2
JC Connell <jamescarltonconnell@gmail.com>
2018-08-07 14:57:54 -0400

    Incorporate @MartinHjelmare.

--- homeassistant/components/sensor/noaa_tides.py
+++ homeassistant/components/sensor/noaa_tides.py
@@ -6,19 +6,18 @@ https://home-assistant.io/components/sensor.noaa_tides/
 """
 import logging
 from datetime import datetime, timedelta
+
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (ATTR_ATTRIBUTION, CONF_NAME, STATE_UNKNOWN)
+from homeassistant.const import (ATTR_ATTRIBUTION, CONF_NAME)
 from homeassistant.helpers.entity import Entity
-from homeassistant.util import Throttle
 
 REQUIREMENTS = ['py_noaa==0.2.2']
 
 _LOGGER = logging.getLogger(__name__)
 
-API_URL = 'https://tidesandcurrents.noaa.gov/api/datagetter'
 CONF_STA_ID = 'station_id'
 CONF_TZ = 'timezone'
 CONF_UNITS = 'units'
@@ -26,7 +25,7 @@ DEFAULT_ATTRIBUTION = "Data provided by NOAA"
 DEFAULT_NAME = 'NOAA Tides'
 DEFAULT_TZ = 'lst_ldt'
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=60)
+SCAN_INTERVAL = timedelta(minutes=60)
 
 TZS = ['gmt', 'lst', 'lst_ldt']
 UNITS = ['english', 'metric']
@@ -46,7 +45,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     t_z = config.get(CONF_TZ)
 
     if CONF_UNITS in config:
-        units = config.get(CONF_UNITS)
+        units = config[CONF_UNITS]
     elif hass.config.units.is_metric:
         units = UNITS[1]
     else:
@@ -75,16 +74,19 @@ class NOAATidesAndCurrentsSensor(Entity):
     def device_state_attributes(self):
         """Return the state attributes of this device."""
         attr = {ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION}
-        if self.data['hi_lo'][1] == 'H':
-            attr['high_tide_time'] = self.data.index[1].strftime('%I:%M %p')
-            attr['high_tide_height'] = self.data['predicted_wl'][1]
-            attr['low_tide_time'] = self.data.index[2].strftime('%I:%M %p')
-            attr['low_tide_height'] = self.data['predicted_wl'][2]
-        elif self.data['hi_lo'][1] == 'L':
-            attr['low_tide_time'] = self.data.index[1].strftime('%I:%M %p')
-            attr['low_tide_height'] = self.data['predicted_wl'][1]
-            attr['high_tide_time'] = self.data.index[2].strftime('%I:%M %p')
-            attr['high_tide_height'] = self.data['predicted_wl'][2]
+        if self.data is not None:
+            if self.data['hi_lo'][1] == 'H':
+                attr['high_tide_time'] = \
+                    self.data.index[1].strftime('%I:%M %p')
+                attr['high_tide_height'] = self.data['predicted_wl'][1]
+                attr['low_tide_time'] = self.data.index[2].strftime('%I:%M %p')
+                attr['low_tide_height'] = self.data['predicted_wl'][2]
+            elif self.data['hi_lo'][1] == 'L':
+                attr['low_tide_time'] = self.data.index[1].strftime('%I:%M %p')
+                attr['low_tide_height'] = self.data['predicted_wl'][1]
+                attr['high_tide_time'] = \
+                    self.data.index[2].strftime('%I:%M %p')
+                attr['high_tide_height'] = self.data['predicted_wl'][2]
         return attr
 
     @property
@@ -94,14 +96,13 @@ class NOAATidesAndCurrentsSensor(Entity):
             api_time = self.data.index[0]
             if self.data['hi_lo'][0] == 'H':
                 tidetime = api_time.strftime('%I:%M %p')
-                return "High tide at %s" % (tidetime)
+                return "High tide at {}".format(tidetime)
             if self.data['hi_lo'][0] == 'L':
                 tidetime = api_time.strftime('%I:%M %p')
-                return "Low tide at %s" % (tidetime)
-            return STATE_UNKNOWN
-        return STATE_UNKNOWN
+                return "Low tide at {}".format(tidetime)
+            return None
+        return None
 
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """Get the latest data from NOAA Tides and Currents API."""
         from py_noaa import coops
@@ -120,9 +121,8 @@ class NOAATidesAndCurrentsSensor(Entity):
                 time_zone=self._tz)
             self.data = df_predictions.head()
             _LOGGER.debug("Data = %s", self.data)
-            _LOGGER.info("Recent Tide data queried with start time set to %s",
-                         begin.strftime("%m-%d-%Y %H:%M"))
+            _LOGGER.debug("Recent Tide data queried with start time set to %s",
+                          begin.strftime("%m-%d-%Y %H:%M"))
         except ValueError as err:
             _LOGGER.error("Check NOAA Tides and Currents %s", err.args)
             self.data = None
-            raise
