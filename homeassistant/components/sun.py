5019b6e4c1b0625ecb99534593e7d9a002066cff
Phil <36015321+pnbruckner@users.noreply.github.com>
2018-08-13 15:17:00 -0500

    Fix some lint and pylint failures

--- homeassistant/components/sun.py
+++ homeassistant/components/sun.py
@@ -9,8 +9,8 @@ import logging
 from datetime import timedelta
 import voluptuous as vol
 
-from homeassistant.const import (CONF_ELEVATION, CONF_MONITORED_CONDITIONS,
-        CONF_SCAN_INTERVAL)
+from homeassistant.const import (
+    CONF_ELEVATION, CONF_MONITORED_CONDITIONS, CONF_SCAN_INTERVAL)
 from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
@@ -45,12 +45,13 @@ STATE_ATTR_SUNSET = 'sunset'
 STATE_ATTR_DAYLIGHT = 'daylight'
 STATE_ATTR_PREV_DAYLIGHT = 'prev_daylight'
 STATE_ATTR_NEXT_DAYLIGHT = 'next_daylight'
-DEFAULT_STATE_ATTRS = [STATE_ATTR_AZIMUTH, STATE_ATTR_ELEVATION,
-        STATE_ATTR_NEXT_DAWN, STATE_ATTR_NEXT_DUSK, STATE_ATTR_NEXT_MIDNIGHT,
-        STATE_ATTR_NEXT_NOON, STATE_ATTR_NEXT_RISING, STATE_ATTR_NEXT_SETTING]
-OPTIONAL_STATE_ATTRS = [STATE_ATTR_SUNRISE, STATE_ATTR_SUNSET,
-        STATE_ATTR_DAYLIGHT, STATE_ATTR_PREV_DAYLIGHT,
-        STATE_ATTR_NEXT_DAYLIGHT]
+DEFAULT_STATE_ATTRS = [
+    STATE_ATTR_AZIMUTH, STATE_ATTR_ELEVATION, STATE_ATTR_NEXT_DAWN,
+    STATE_ATTR_NEXT_DUSK, STATE_ATTR_NEXT_MIDNIGHT, STATE_ATTR_NEXT_NOON,
+    STATE_ATTR_NEXT_RISING, STATE_ATTR_NEXT_SETTING]
+OPTIONAL_STATE_ATTRS = [
+    STATE_ATTR_SUNRISE, STATE_ATTR_SUNSET, STATE_ATTR_DAYLIGHT,
+    STATE_ATTR_PREV_DAYLIGHT, STATE_ATTR_NEXT_DAYLIGHT]
 STATE_ATTRS = DEFAULT_STATE_ATTRS + OPTIONAL_STATE_ATTRS
 
 CONFIG_SCHEMA = vol.Schema({
@@ -58,8 +59,9 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Optional(CONF_MONITORED_CONDITIONS, default=DEFAULT_STATE_ATTRS):
             vol.All(cv.ensure_list, [vol.In(STATE_ATTRS)]),
         vol.Optional(CONF_SCAN_INTERVAL):
-            vol.All(cv.time_period, vol.Clamp(min=MIN_SCAN_INTERVAL))})},
-    extra=vol.ALLOW_EXTRA)
+            vol.All(cv.time_period, vol.Clamp(min=MIN_SCAN_INTERVAL))
+        })
+}, extra=vol.ALLOW_EXTRA)
 
 
 @asyncio.coroutine
@@ -88,8 +90,8 @@ class Sun(Entity):
         self.hass = hass
         self.location = location
         # Initialize values for attributes to be reported.
-        for a in self._monitored_condtions:
-            setattr(self, a, None)
+        for attr in self._monitored_condtions:
+            setattr(self, attr, None)
         # Make sure STATE_ATTR_NEXT_RISING & STATE_ATTR_NEXT_SETTING are
         # defined since they are used to calculate state. Initialize them.
         setattr(self, STATE_ATTR_NEXT_RISING, None)
@@ -127,25 +129,25 @@ class Sun(Entity):
     def state_attributes(self):
         """Return the state attributes of the sun."""
         attrs = {}
-        for a in [STATE_ATTR_NEXT_DAWN,
-                  STATE_ATTR_NEXT_DUSK,
-                  STATE_ATTR_NEXT_MIDNIGHT,
-                  STATE_ATTR_NEXT_NOON,
-                  STATE_ATTR_NEXT_RISING,
-                  STATE_ATTR_NEXT_SETTING,
-                  STATE_ATTR_SUNRISE,
-                  STATE_ATTR_SUNSET]:
-            if a in self._monitored_condtions:
-                attrs[a] = getattr(self, a).isoformat()
-        for a in [STATE_ATTR_ELEVATION,
-                  STATE_ATTR_AZIMUTH]:
-            if a in self._monitored_condtions:
-                attrs[a] = round(getattr(self, a), 2)
-        for a in [STATE_ATTR_DAYLIGHT,
-                  STATE_ATTR_PREV_DAYLIGHT,
-                  STATE_ATTR_NEXT_DAYLIGHT]:
-            if a in self._monitored_condtions:
-                attrs[a] = getattr(self, a)
+        for attr in [STATE_ATTR_NEXT_DAWN,
+                     STATE_ATTR_NEXT_DUSK,
+                     STATE_ATTR_NEXT_MIDNIGHT,
+                     STATE_ATTR_NEXT_NOON,
+                     STATE_ATTR_NEXT_RISING,
+                     STATE_ATTR_NEXT_SETTING,
+                     STATE_ATTR_SUNRISE,
+                     STATE_ATTR_SUNSET]:
+            if attr in self._monitored_condtions:
+                attrs[attr] = getattr(self, attr).isoformat()
+        for attr in [STATE_ATTR_ELEVATION,
+                     STATE_ATTR_AZIMUTH]:
+            if attr in self._monitored_condtions:
+                attrs[attr] = round(getattr(self, attr), 2)
+        for attr in [STATE_ATTR_DAYLIGHT,
+                     STATE_ATTR_PREV_DAYLIGHT,
+                     STATE_ATTR_NEXT_DAYLIGHT]:
+            if attr in self._monitored_condtions:
+                attrs[attr] = getattr(self, attr)
         return attrs
 
     @property
@@ -157,21 +159,21 @@ class Sun(Entity):
                        getattr(self, STATE_ATTR_NEXT_SETTING)]
         # Only need to update remaining properties if they will be reported
         # in attributes.
-        for a in [STATE_ATTR_NEXT_DAWN,
-                  STATE_ATTR_NEXT_DUSK,
-                  STATE_ATTR_NEXT_MIDNIGHT,
-                  STATE_ATTR_NEXT_NOON]:
-            if a in self._monitored_condtions:
-                next_events.append(getattr(self, a))
+        for attr in [STATE_ATTR_NEXT_DAWN,
+                     STATE_ATTR_NEXT_DUSK,
+                     STATE_ATTR_NEXT_MIDNIGHT,
+                     STATE_ATTR_NEXT_NOON]:
+            if attr in self._monitored_condtions:
+                next_events.append(getattr(self, attr))
         # For sunrise, sunset and daylights, update at next "real" midnight
         # (as opposed to next_midnight, which is solar midnight.) But subtract
         # one second because point_in_time_listener() will add one.
-        if any(a in self._monitored_condtions for a in
-                (STATE_ATTR_SUNRISE, STATE_ATTR_SUNSET,
-                        STATE_ATTR_DAYLIGHT, STATE_ATTR_PREV_DAYLIGHT,
-                        STATE_ATTR_NEXT_DAYLIGHT)):
+        if any(attr in self._monitored_condtions for attr in
+               [STATE_ATTR_SUNRISE, STATE_ATTR_SUNSET,
+                STATE_ATTR_DAYLIGHT, STATE_ATTR_PREV_DAYLIGHT,
+                STATE_ATTR_NEXT_DAYLIGHT]):
             midnight = dt_util.start_of_local_day(
-                    dt_util.now() + timedelta(days=1))
+                dt_util.now() + timedelta(days=1))
             next_events.append(dt_util.as_utc(midnight) - timedelta(seconds=1))
         return min(next_events)
 
@@ -180,28 +182,31 @@ class Sun(Entity):
         """Update the attributes containing solar events."""
         # Always need to update next_rising and next_setting so state can be
         # determined.
-        for a, e in [(STATE_ATTR_NEXT_RISING, 'sunrise'),
-                     (STATE_ATTR_NEXT_SETTING, 'sunset')]:
-            setattr(self, a, get_astral_event_next(self.hass, e, utc_time))
+        for attr, event in [(STATE_ATTR_NEXT_RISING, 'sunrise'),
+                            (STATE_ATTR_NEXT_SETTING, 'sunset')]:
+            setattr(self, attr,
+                    get_astral_event_next(self.hass, event, utc_time))
         # Only need to update remaining properties if they will be reported
         # in attributes.
-        for a, e in [(STATE_ATTR_NEXT_DAWN, 'dawn'),
-                     (STATE_ATTR_NEXT_DUSK, 'dusk'),
-                     (STATE_ATTR_NEXT_MIDNIGHT, 'solar_midnight'),
-                     (STATE_ATTR_NEXT_NOON, 'solar_noon')]:
-            if a in self._monitored_condtions:
-                setattr(self, a, get_astral_event_next(self.hass, e, utc_time))
-        for a, e in [(STATE_ATTR_SUNRISE, 'sunrise'),
-                     (STATE_ATTR_SUNSET, 'sunset')]:
-            if a in self._monitored_condtions:
-                setattr(self, a, get_astral_event_date(self.hass, e, utc_time))
-        for a, d in [(STATE_ATTR_DAYLIGHT, 0),
-                     (STATE_ATTR_PREV_DAYLIGHT, -1),
-                     (STATE_ATTR_NEXT_DAYLIGHT, 1)]:
-            if a in self._monitored_condtions:
-                dl = get_astral_event_date(self.hass, 'daylight',
-                        utc_time+timedelta(days=d))
-                setattr(self, a, (dl[1]-dl[0]).total_seconds())
+        for attr, event in [(STATE_ATTR_NEXT_DAWN, 'dawn'),
+                            (STATE_ATTR_NEXT_DUSK, 'dusk'),
+                            (STATE_ATTR_NEXT_MIDNIGHT, 'solar_midnight'),
+                            (STATE_ATTR_NEXT_NOON, 'solar_noon')]:
+            if attr in self._monitored_condtions:
+                setattr(self, attr,
+                        get_astral_event_next(self.hass, event, utc_time))
+        for attr, event in [(STATE_ATTR_SUNRISE, 'sunrise'),
+                            (STATE_ATTR_SUNSET, 'sunset')]:
+            if attr in self._monitored_condtions:
+                setattr(self, attr,
+                        get_astral_event_date(self.hass, event, utc_time))
+        for attr, delta in [(STATE_ATTR_DAYLIGHT, 0),
+                            (STATE_ATTR_PREV_DAYLIGHT, -1),
+                            (STATE_ATTR_NEXT_DAYLIGHT, 1)]:
+            if attr in self._monitored_condtions:
+                daylight = get_astral_event_date(
+                    self.hass, 'daylight', utc_time+timedelta(days=delta))
+                setattr(self, attr, (daylight[1]-daylight[0]).total_seconds())
 
     @callback
     def update_sun_position(self, utc_time):
