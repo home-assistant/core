7bb53449428b0787cca1d0c986c93a458401e6fb
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-08-21 15:49:58 +0200

    Remove homeassistant.remote (#16099)
    * Remove homeassistant.remote
    
    * Use direct import for API
    
    * Fix docstring

--- tests/components/device_tracker/test_init.py
+++ tests/components/device_tracker/test_init.py
@@ -20,7 +20,7 @@ from homeassistant.const import (
     STATE_HOME, STATE_NOT_HOME, CONF_PLATFORM, ATTR_ICON)
 import homeassistant.components.device_tracker as device_tracker
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.remote import JSONEncoder
+from homeassistant.helpers.json import JSONEncoder
 
 from tests.common import (
     get_test_home_assistant, fire_time_changed,
