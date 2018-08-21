27ff53e3d570033920c03d26b4d02bc49df81044
Paulus Schoutsen <balloob@gmail.com>
2018-08-21 14:52:01 +0200

    Use direct import for API

--- homeassistant/components/api.py
+++ homeassistant/components/api.py
@@ -24,6 +24,7 @@ from homeassistant.exceptions import TemplateError
 from homeassistant.helpers import template
 from homeassistant.helpers.service import async_get_all_descriptions
 from homeassistant.helpers.state import AsyncTrackStates
+from homeassistant.helpers.json import JSONEncoder
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -101,7 +102,7 @@ class APIEventStream(HomeAssistantView):
             if event.event_type == EVENT_HOMEASSISTANT_STOP:
                 data = stop_obj
             else:
-                data = json.dumps(event, cls=hass.helpers.json.JSONEncoder)
+                data = json.dumps(event, cls=JSONEncoder)
 
             await to_write.put(data)
 
