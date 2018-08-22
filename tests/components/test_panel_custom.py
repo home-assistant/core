8d77eb599a0b7831d5b1fcbc52262f3cbf00b576
Bálint Villányi <vilanyib@gmail.com>
2018-08-22 18:54:14 +0200

    Final check

--- tests/components/test_panel_custom.py
+++ tests/components/test_panel_custom.py
@@ -167,32 +167,17 @@ async def test_url_conflict_config(hass):
         {'panel_custom': {
             'name': 'todo-mvc',
             'module_url': '/local/bla.js',
+            'js_url': '/local/bla.js',
         }
         }, {'panel_custom': {
             'name': 'todo-mvc',
             'webcomponent_path': '/local/bla.html',
             'js_url': '/local/bla.js',
-            'sidebar_title': 'Sidebar Title',
-            'sidebar_icon': 'mdi:iconicon',
-            'url_path': 'nice_url',
-            'config': {
-                'hello': 'world',
-            },
-            'embed_iframe': True,
-            'trust_external_script': True
         }}, {'panel_custom': {
             'name': 'todo-mvc',
             'webcomponent_path': '/local/bla.html',
             'module_url': '/local/bla.js',
             'js_url': '/local/bla.js',
-            'sidebar_title': 'Sidebar Title',
-            'sidebar_icon': 'mdi:iconicon',
-            'url_path': 'nice_url',
-            'config': {
-                'hello': 'world',
-            },
-            'embed_iframe': True,
-            'trust_external_script': True
         }}
     ]
 
