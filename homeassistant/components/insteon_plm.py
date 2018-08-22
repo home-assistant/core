349457ddc1ac5e3762b6bcd854083b8f2078ef3c
Tom Harris <tharris@harrisnj.net>
2018-08-22 03:09:04 -0400

    Merge insteon_plm and insteon_local to insteon component (#16102)
    * Implement X10
    
    * Add X10 after add_device_callback
    
    * Ref device by id not hex and add x10OnOffSwitch name
    
    * X10 services and add sensor device
    
    * Correctly reference X10_HOUSECODE_SCHEMA
    
    * Log adding of X10 devices
    
    * Add X10 All Units Off, All Lights On and All Lights Off devices
    
    * Correct ref to X10 states vs devices
    
    * Add X10 All Units Off, All Lights On and All Lights Off devices
    
    * Correct X10 config
    
    * Debug x10 device additions
    
    * Config x10 from bool to housecode char
    
    * Pass PLM to X10 device create
    
    * Remove PLM to call to add_x10_device
    
    * Unconfuse x10 config and method names
    
    * Correct spelling of x10_all_lights_off_housecode
    
    * Bump insteonplm to 0.10.0 to support X10
    
    * Add host to config options
    
    * Add username and password to config for hub connectivity
    
    * Add username and password to config for hub
    
    * Convert port to int if host is defined
    
    * Add KeypadLinc
    
    * Update config schema to require either port or host
    
    * Solidify Hub and PLM configuration to ensure proper settings
    
    * Update hub schema
    
    * Bump insteonplm version
    
    * Fix pylint and flake issues
    
    * Bump insteonplm to 0.12.1
    
    * Merge insteon_plm and insteon_local to insteon
    
    * Rename insteon_plm to insteon
    
    * Bump insteonplm to 0.12.2
    
    * Flake8 cleanup
    
    * Update .coveragerc for insteon_plm, insteon_local and insteon changes
    
    * Add persistent notification
    
    * Fix reference to insteon_plm
    
    * Fix indentation
    
    * Shorten message and fix grammer
    
    * Add comment to remove in release 0.90
    
    * Hound fix

--- homeassistant/components/insteon_plm.py
+++ homeassistant/components/insteon_plm.py
@@ -1,28 +1,30 @@
 """
-Local support for Insteon.
+Support for INSTEON PowerLinc Modem.
 
 For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/insteon_local/
+https://home-assistant.io/components/insteon_plm/
 """
+import asyncio
 import logging
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def setup(hass, config):
-    """Setup the insteon_local component.
+@asyncio.coroutine
+def async_setup(hass, config):
+    """Setup the insteon_plm component.
 
     This component is depreciated as of release 0.77 and should be removed in
     release 0.90.
     """
-    _LOGGER.warning('The insteon_local comonent has been replaced by '
+    _LOGGER.warning('The insteon_plm comonent has been replaced by '
                     'the insteon component')
     _LOGGER.warning('Please see https://home-assistant.io/components/insteon')
 
     hass.components.persistent_notification.create(
-        'insteon_local has been replaced by the insteon component.<br />'
+        'insteon_plm has been replaced by the insteon component.<br />'
         'Please see https://home-assistant.io/components/insteon',
-        title='insteon_local Component Deactivated',
-        notification_id='insteon_local')
+        title='insteon_plm Component Deactivated',
+        notification_id='insteon_plm')
 
     return False
