cbb45d54f05e04bb5a3d8d216c1de073de07a3c5
Tom Harris <tharris@harrisnj.net>
2018-08-26 13:43:26 -0400

    Bump insteonplm to 0.13.0

--- homeassistant/components/insteon/__init__.py
+++ homeassistant/components/insteon/__init__.py
@@ -18,7 +18,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers import discovery
 from homeassistant.helpers.entity import Entity
 
-REQUIREMENTS = ['insteonplm==0.12.3']
+REQUIREMENTS = ['insteonplm==0.13.0']
 
 _LOGGER = logging.getLogger(__name__)
 
