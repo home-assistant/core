ff31c04505563bd6ac03ba3c21edb4b74a767b47
Jesse Rizzo <jesse.rizzo@gmail.com>
2018-08-27 18:59:05 -0500

    Change some strings to constants, use getattr to call function

--- homeassistant/components/sensor/enphase_envoy.py
+++ homeassistant/components/sensor/enphase_envoy.py
@@ -20,32 +20,34 @@ _LOGGER = logging.getLogger(__name__)
 SENSORS = {
     "production": ("Envoy Current Energy Production", 'W'),
     "daily_production": ("Envoy Today's Energy Production", "Wh"),
-    "7_days_production": ("Envoy Last Seven Days Energy Production", "Wh"),
+    "seven_days_production": ("Envoy Last Seven Days Energy Production", "Wh"),
     "lifetime_production": ("Envoy Lifetime Energy Production", "Wh"),
     "consumption": ("Envoy Current Energy Consumption", "W"),
     "daily_consumption": ("Envoy Today's Energy Consumption", "Wh"),
-    "7_days_consumption": ("Envoy Last Seven Days Energy Consumption", "Wh"),
+    "seven_days_consumption": ("Envoy Last Seven Days Energy Consumption",
+                               "Wh"),
     "lifetime_consumption": ("Envoy Lifetime Energy Consumption", "Wh")
     }
 
 
 ICON = 'mdi:flash'
+CONST_DEFAULT_HOST = "envoy"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_IP_ADDRESS, default="envoy"): cv.string,
+    vol.Optional(CONF_IP_ADDRESS, default=CONST_DEFAULT_HOST): cv.string,
     vol.Optional(CONF_MONITORED_CONDITIONS, default=list(SENSORS)):
         vol.All(cv.ensure_list, [vol.In(list(SENSORS))])})
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Enphase Envoy sensor."""
     ip_address = config[CONF_IP_ADDRESS]
     monitored_conditions = config[CONF_MONITORED_CONDITIONS]
 
     # Iterate through the list of sensors
     for condition in monitored_conditions:
-        add_devices([Envoy(ip_address, condition, SENSORS[condition][0],
-                           SENSORS[condition][1])], True)
+        add_entities([Envoy(ip_address, condition, SENSORS[condition][0],
+                            SENSORS[condition][1])], True)
 
 
 class Envoy(Entity):
@@ -83,21 +85,4 @@ class Envoy(Entity):
         """Get the energy production data from the Enphase Envoy."""
         from envoy_reader import EnvoyReader
 
-        if self._type == "production":
-            self._state = EnvoyReader(self._ip_address).production()
-        elif self._type == "daily_production":
-            self._state = EnvoyReader(self._ip_address).daily_production()
-        elif self._type == "7_days_production":
-            self._state = EnvoyReader(self._ip_address).seven_days_production()
-        elif self._type == "lifetime_production":
-            self._state = EnvoyReader(self._ip_address).lifetime_production()
-
-        elif self._type == "consumption":
-            self._state = EnvoyReader(self._ip_address).consumption()
-        elif self._type == "daily_consumption":
-            self._state = EnvoyReader(self._ip_address).daily_consumption()
-        elif self._type == "7_days_consumption":
-            self._state = (EnvoyReader(self._ip_address)
-                           .seven_days_consumption())
-        elif self._type == "lifetime_consumption":
-            self._state = EnvoyReader(self._ip_address).lifetime_consumption()
+        self._state = getattr(EnvoyReader(self._ip_address), self._type)()
