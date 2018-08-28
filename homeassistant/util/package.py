68c64a003c619ab366bf27b64bb5e4151336a850
Paulus Schoutsen <balloob@gmail.com>
2018-08-28 10:39:48 +0200

    Improve package loadable

--- homeassistant/util/package.py
+++ homeassistant/util/package.py
@@ -4,16 +4,10 @@ import logging
 import os
 from subprocess import PIPE, Popen
 import sys
-import threading
-from urllib.parse import urlparse
 
 from typing import Optional
 
-import pkg_resources
-
 _LOGGER = logging.getLogger(__name__)
-
-INSTALL_LOCK = threading.Lock()
 
 
 def is_virtual_env() -> bool:
@@ -31,34 +25,30 @@ def install_package(package: str, upgrade: bool = True,
     Return boolean if install successful.
     """
     # Not using 'import pip; pip.main([])' because it breaks the logger
-    with INSTALL_LOCK:
-        if PACKAGES.loadable(package):
-            return True
+    _LOGGER.info('Attempting install of %s', package)
+    env = os.environ.copy()
+    args = [sys.executable, '-m', 'pip', 'install', '--quiet', package]
+    if upgrade:
+        args.append('--upgrade')
+    if constraints is not None:
+        args += ['--constraint', constraints]
+    if target:
+        assert not is_virtual_env()
+        # This only works if not running in venv
+        args += ['--user']
+        env['PYTHONUSERBASE'] = os.path.abspath(target)
+        if sys.platform != 'win32':
+            # Workaround for incompatible prefix setting
+            # See http://stackoverflow.com/a/4495175
+            args += ['--prefix=']
+    process = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE, env=env)
+    _, stderr = process.communicate()
+    if process.returncode != 0:
+        _LOGGER.error("Unable to install package %s: %s",
+                      package, stderr.decode('utf-8').lstrip().strip())
+        return False
 
-        _LOGGER.info('Attempting install of %s', package)
-        env = os.environ.copy()
-        args = [sys.executable, '-m', 'pip', 'install', '--quiet', package]
-        if upgrade:
-            args.append('--upgrade')
-        if constraints is not None:
-            args += ['--constraint', constraints]
-        if target:
-            assert not is_virtual_env()
-            # This only works if not running in venv
-            args += ['--user']
-            env['PYTHONUSERBASE'] = os.path.abspath(target)
-            if sys.platform != 'win32':
-                # Workaround for incompatible prefix setting
-                # See http://stackoverflow.com/a/4495175
-                args += ['--prefix=']
-        process = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE, env=env)
-        _, stderr = process.communicate()
-        if process.returncode != 0:
-            _LOGGER.error("Unable to install package %s: %s",
-                          package, stderr.decode('utf-8').lstrip().strip())
-            return False
-
-        return True
+    return True
 
 
 async def async_get_user_site(deps_dir: str) -> str:
@@ -76,47 +66,3 @@ async def async_get_user_site(deps_dir: str) -> str:
     stdout, _ = await process.communicate()
     lib_dir = stdout.decode().strip()
     return lib_dir
-
-
-class PackageLoadable:
-    """Class to check if a package is loadable, with built-in cache."""
-
-    def __init__(self):
-        """Initialize the PackageLoadable class."""
-        self.dist_cache = {}  # type: Dict[str, any]
-
-    def loadable(self, package: str) -> bool:
-        """Check if a package is what will be loaded when we import it.
-
-        Returns True when the requirement is met.
-        Returns False when the package is not installed or doesn't meet req.
-        """
-        dist_cache = self.dist_cache
-
-        try:
-            req = pkg_resources.Requirement.parse(package)
-        except ValueError:
-            # This is a zip file. We no longer use this in Home Assistant,
-            # leaving it in for custom components.
-            req = pkg_resources.Requirement.parse(urlparse(package).fragment)
-
-        req_proj_name = req.project_name.lower()
-        dist = dist_cache.get(req_proj_name)
-
-        if dist is not None:
-            return dist in req
-
-        for path in sys.path:
-            # We read the whole mount point as we're already here
-            # Caching it on first call makes subsequent calls a lot faster.
-            for dist in pkg_resources.find_distributions(path):
-                dist_cache.setdefault(dist.project_name.lower(), dist)
-
-            dist = dist_cache.get(req_proj_name)
-            if dist is not None:
-                return dist in req
-
-        return False
-
-
-PACKAGES = PackageLoadable()
