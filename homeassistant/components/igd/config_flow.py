f3f5c7f588d7a4ba18364a7ee317147cf2b35bf2
Steven Looman <steven.looman@gmail.com>
2018-08-29 21:19:04 +0200

    Working on IGD

--- homeassistant/components/igd/config_flow.py
+++ homeassistant/components/igd/config_flow.py
@@ -1,40 +1,19 @@
 """Config flow for IGD."""
 from homeassistant import config_entries, data_entry_flow
 from homeassistant.core import callback
-from homeassistant.helpers import config_validation as cv
 
 import voluptuous as vol
 
 from .const import DOMAIN
 from .const import LOGGER as _LOGGER
 
-
 @callback
-def configured_hosts(hass):
-    """Return a set of the configured hosts."""
-    return set(entry.data['ssdp_url']
-               for entry in hass.config_entries.async_entries(DOMAIN))
-
-
-async def _get_igd_device(hass, ssdp_url):
-    """."""
-    # build requester
-    from async_upnp_client.aiohttp import AiohttpSessionRequester
-    session = async_get_clientsession(hass)
-    requester = AiohttpSessionRequester(session, True)
-
-    # create upnp device
-    from async_upnp_client import UpnpFactory
-    factory = UpnpFactory(requester, disable_state_variable_validation=True)
-    try:
-        upnp_device = await factory.async_create_device(ssdp_url)
-    except (asyncio.TimeoutError, aiohttp.ClientError):
-        raise PlatformNotReady()
-
-    # wrap with IgdDevice
-    from async_upnp_client.igd import IgdDevice
-    igd_device = IgdDevice(upnp_device, None)
-    return igd_device
+def configured_udns(hass):
+    """Get all configured UDNs."""
+    return [
+        entry.data['udn']
+        for entry in hass.config_entries.async_entries(DOMAIN)
+    ]
 
 
 @config_entries.HANDLERS.register(DOMAIN)
@@ -43,61 +22,114 @@ class IgdFlowHandler(data_entry_flow.FlowHandler):
 
     VERSION = 1
 
-    # def __init__(self):
-    #     """Initializer."""
-    #     self.host = None
+    def __init__(self):
+        """Initializer."""
+        pass
 
-    # flow: 1. detection/user adding
-    #       2. question: port forward? sensors?
-    #       3. add it!
+    @property
+    def _discovereds(self):
+        """Get all discovered entries."""
+        if DOMAIN not in self.hass.data:
+            _LOGGER.debug('DOMAIN not in hass.data')
+        if 'discovered' not in self.hass.data.get(DOMAIN, {}):
+            _LOGGER.debug('discovered not in hass.data[DOMAIN]')
 
-    async def async_step_user(self, user_input=None):
-        _LOGGER.debug('async_step_user: %s', user_input)
-        return await self.async_abort(reason='todo')
+        return self.hass.data.get(DOMAIN, {}).get('discovered', {})
+
+    def _store_discovery_info(self, discovery_info):
+        """Add discovery info."""
+        udn = discovery_info['udn']
+        if DOMAIN not in self.hass.data:
+            _LOGGER.debug('DOMAIN not in hass.data')
+        self.hass.data[DOMAIN] = self.hass.data.get(DOMAIN, {})
+        if 'discovered' not in self.hass.data[DOMAIN]:
+            _LOGGER.debug('Creating new discovered: %s', self.hass.data[DOMAIN])
+        self.hass.data[DOMAIN]['discovered'] = self.hass.data[DOMAIN].get('discovered', {})
+        self.hass.data[DOMAIN]['discovered'][udn] = discovery_info
 
     async def async_step_discovery(self, discovery_info):
-        """Handle a discovered IGD.
+        """
+        Handle a discovered IGD.
 
         This flow is triggered by the discovery component. It will check if the
         host is already configured and delegate to the import step if not.
         """
-        _LOGGER.debug('async_step_discovery: %s', discovery_info)
+        _LOGGER.debug('async_step_discovery %s: %s', id(self), discovery_info)
 
-        ssdp_url = discovery_info['ssdp_description']
-        return await self.async_step_options({
-            'ssdp_url': ssdp_url,
-        })
+        # ensure not already discovered/configured
+        udn = discovery_info['udn']
+        if udn in configured_udns(self.hass):
+            _LOGGER.debug('Already configured: %s', discovery_info)
+            return self.async_abort(reason='already_configured')
 
-    async def async_step_options(self, user_options):
-        """."""
-        _LOGGER.debug('async_step_options: %s', user_options)
-        if user_options and \
-           'sensors' in user_options and \
-           'port_forward' in user_options:
-            return await self.async_step_import(user_options)
+        # store discovered device
+        self._store_discovery_info(discovery_info)
+
+        # abort --> not showing up in discovered things
+        # return self.async_abort(reason='user_input_required')
+
+        # user -> showing up in discovered things
+        return await self.async_step_user()
+
+    async def async_step_user(self, user_input=None):
+        """Manual set up."""
+        _LOGGER.debug('async_step_user %s: %s', id(self), user_input)
+
+        # if user input given, handle it
+        user_input = user_input or {}
+        if 'igd_host' in user_input:
+            if not user_input['sensors'] and not user_input['port_forward']:
+                _LOGGER.debug('Aborting, no sensors and no portforward')
+                return self.async_abort(reason='no_sensors_or_port_forward')
+
+            configured_hosts = [
+                entry['host']
+                for entry in self._discovereds.values()
+                if entry['udn'] in configured_udns(self.hass)
+            ]
+            if user_input['igd_host'] in configured_hosts:
+                return self.async_abort(reason='already_configured')
+
+            return await self._async_save(user_input)
+
+        # let user choose from all discovered IGDs
+        _LOGGER.debug('Discovered devices: %s', self._discovereds)
+        igd_hosts = [
+            entry['host']
+            for entry in self._discovereds.values()
+            if entry['udn'] not in configured_udns(self.hass)
+        ]
+        if not igd_hosts:
+            return self.async_abort(reason='no_devices_discovered')
 
         return self.async_show_form(
-            step_id='options',
+            step_id='user',
             data_schema=vol.Schema({
-                vol.Required('sensors'): cv.boolean,
-                vol.Required('port_forward'): cv.boolean,
-                # vol.Optional('ssdp_url', default=user_options['ssdp_url']): cv.url,
+                vol.Required('igd_host'): vol.In(igd_hosts),
+                vol.Required('sensors'): bool,
+                vol.Required('port_forward'): bool,
             })
         )
 
-    async def async_step_import(self, import_info):
-        """Import a IGD as new entry."""
-        _LOGGER.debug('async_step_import: %s', import_info)
+    async def _async_save(self, import_info):
+        """Store IGD as new entry."""
+        _LOGGER.debug('async_step_import %s: %s', id(self), import_info)
 
-        ssdp_url = import_info['ssdp_url']
-        try:
-            igd_device = await _get_igd_device(self.hass, ssdp_url)  # try it to see if it works
-        except:
-            pass
+        # ensure we know the host
+        igd_host = import_info['igd_host']
+        discovery_infos = [info
+                           for info in self._discovereds.values()
+                           if info['host'] == igd_host]
+        if not discovery_infos:
+            return self.async_abort(reason='host_not_found')
+        discovery_info = discovery_infos[0]
+
         return self.async_create_entry(
-            title=igd_device.name,
+            title=discovery_info['name'],
             data={
-                'ssdp_url': ssdp_url,
-                'udn': igd_device.udn,
-            }
+                'ssdp_description': discovery_info['ssdp_description'],
+                'udn': discovery_info['udn'],
+                'sensors': import_info['sensors'],
+                'port_forward': import_info['port_forward'],
+            },
         )
