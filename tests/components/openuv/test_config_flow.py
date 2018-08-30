c09d45a83165e17d2557935925e64a6abf9f22d5
Aaron Bach <bachya1208@gmail.com>
2018-08-30 15:01:23 -0600

    Updated tests

--- tests/components/openuv/test_config_flow.py
+++ tests/components/openuv/test_config_flow.py
@@ -1,10 +1,12 @@
 """Define tests for the OpenUV config flow."""
+from unittest.mock import patch
 
+from homeassistant import data_entry_flow
 from homeassistant.components.openuv import DOMAIN, config_flow
 from homeassistant.const import (
     CONF_API_KEY, CONF_ELEVATION, CONF_LATITUDE, CONF_LONGITUDE)
 
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, mock_coro
 
 
 async def test_duplicate_error(hass):
@@ -24,17 +26,35 @@ async def test_duplicate_error(hass):
     assert result['errors'] == {'base': 'identifier_exists'}
 
 
+@patch('pyopenuv.util.validate_api_key', return_value=mock_coro(False))
+async def test_invalid_api_key(hass):
+    """Test that an invalid API key throws an error."""
+    conf = {
+        CONF_API_KEY: '12345abcde',
+        CONF_ELEVATION: 59.1234,
+        CONF_LATITUDE: 39.128712,
+        CONF_LONGITUDE: -104.9812612,
+    }
+
+    flow = config_flow.OpenUvFlowHandler()
+    flow.hass = hass
+
+    result = await flow.async_step_user(user_input=conf)
+    assert result['errors'] == {'base': 'invalid_api_key'}
+
+
 async def test_show_form(hass):
     """Test that the form is served with no input."""
     flow = config_flow.OpenUvFlowHandler()
     flow.hass = hass
 
-    result = await flow.async_step_import(import_config=None)
+    result = await flow.async_step_user(user_input=None)
 
-    assert result['type'] == 'form'
+    assert result['type'] == data_entry_flow.RESULT_TYPE_FORM
     assert result['step_id'] == 'user'
 
 
+@patch('pyopenuv.util.validate_api_key', return_value=mock_coro(True))
 async def test_step_import(hass):
     """Test that the import step works."""
     conf = {
@@ -46,12 +66,13 @@ async def test_step_import(hass):
 
     result = await flow.async_step_import(import_config=conf)
 
-    assert result['type'] == 'create_entry'
+    assert result['type'] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result['title'] == '{0}, {1}'.format(
         hass.config.latitude, hass.config.longitude)
     assert result['data'] == conf
 
 
+@patch('pyopenuv.util.validate_api_key', return_value=mock_coro(True))
 async def test_step_user(hass):
     """Test that the user step works."""
     conf = {
@@ -66,7 +87,7 @@ async def test_step_user(hass):
 
     result = await flow.async_step_user(user_input=conf)
 
-    assert result['type'] == 'create_entry'
+    assert result['type'] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result['title'] == '{0}, {1}'.format(
         conf[CONF_LATITUDE], conf[CONF_LONGITUDE])
     assert result['data'] == conf
