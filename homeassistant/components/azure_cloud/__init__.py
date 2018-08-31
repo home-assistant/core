09dd6149cd2d541f888ed77bba775c810a2d5846
Vijayshinva Karnure <vijayshinva@gmail.com>
2018-08-31 17:48:45 +0530

    Added Azure VM sensor feature

---
+++ homeassistant/components/azure_cloud/__init__.py
@@ -0,0 +1,65 @@
+"""
+Azure component that handles Home Assistant interaction with Microsoft Azure Cloud.
+
+For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/azure/
+"""
+import asyncio
+import logging
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from azure.common.credentials import ServicePrincipalCredentials
+
+_LOGGER = logging.getLogger(__name__)
+
+REQUIREMENTS = ['azure-mgmt-compute==2.0.0']
+DEPENDENCIES = []
+DOMAIN = 'azure_cloud'
+
+CONF_TENANT_ID = 'tenant_id'
+CONF_SUBSCRIPTION_ID = 'subscription_id'
+CONF_CLIENT_ID = 'client_id'
+CONF_CLIENT_SECRET = 'client_secret'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_TENANT_ID): cv.string,
+        vol.Required(CONF_SUBSCRIPTION_ID): cv.string,
+        vol.Required(CONF_CLIENT_ID): cv.string,
+        vol.Required(CONF_CLIENT_SECRET): cv.string,
+    }),
+}, extra=vol.ALLOW_EXTRA)
+
+
+@asyncio.coroutine
+def async_setup(hass, config):
+    """Set up the azure component."""
+    conf = config.get(DOMAIN, {})
+    hass.data[DOMAIN] = AzureSubscription(hass, conf)
+    return True
+
+
+class AzureSubscription:
+    """Representation of an azure subscription."""
+
+    def __init__(self, hass, config):
+        """Initialize the azure subscription Entity."""
+        self._tenant_id = config.get(CONF_TENANT_ID)
+        self._subscription_id = config.get(CONF_SUBSCRIPTION_ID)
+        self._client_id = config.get(CONF_CLIENT_ID)
+        self._client_secret = config.get(CONF_CLIENT_SECRET)
+        self._credentials = ServicePrincipalCredentials(
+            client_id=self._client_id, secret=self._client_secret, tenant=self._tenant_id)
+        _LOGGER.debug(
+            "Azure subscription entity initialized for subscription %s.", self._subscription_id)
+
+    @property
+    def credentials(self):
+        """Return the credentials of the azure subscription."""
+        return self._credentials
+
+    @property
+    def subscription_id(self):
+        """Return the ID of the azure subscription."""
+        return self._subscription_id
---
+++ homeassistant/components/sensor/azure_vm.py
@@ -0,0 +1,85 @@
+"""
+Sensor platform for Azure Virtual Machine.
+
+For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/azure/
+"""
+
+from datetime import timedelta
+import asyncio
+import logging
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.util import Throttle
+from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
+from homeassistant.helpers.entity import Entity
+from homeassistant.components.azure_cloud import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+DEPENDENCIES = ['azure_cloud']
+REQUIREMENTS = ['azure-mgmt-compute==2.0.0']
+
+CONF_NAME = 'name'
+CONG_RESOURCE_GROUP = 'resource_group'
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=1)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONG_RESOURCE_GROUP): cv.string,
+})
+
+
+@asyncio.coroutine
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Setup sensor"""
+    async_add_entities([AzureVmSensor(hass, config)])
+
+
+class AzureVmSensor(Entity):
+    """Representation of an Azure virutal machine"""
+
+    def __init__(self, hass, config):
+        """Initialize the sensor."""
+        from azure.mgmt.compute import ComputeManagementClient
+
+        self._azure_compute_client = ComputeManagementClient(
+            hass.data[DOMAIN].credentials, hass.data[DOMAIN].subscription_id)
+        self._name = config.get(CONF_NAME)
+        self._resource_group = config.get(CONG_RESOURCE_GROUP)
+        self._attributes = None
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the azure vm."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the azure vm."""
+        return self._state
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend."""
+        if self._state == "VM running":
+            return 'mdi:server'
+        return 'mdi:server-off'
+
+    @property
+    def device_state_attributes(self):
+        """Return the attributes of the azure vm."""
+        return self._attributes
+
+    @asyncio.coroutine
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    async def async_update(self):
+        """Retrieve latest state."""
+        virtual_machine = self._azure_compute_client.virtual_machines.get(
+            self._resource_group, self._name, expand='instanceView')
+        _LOGGER.debug("Status of azure vm %s is %s ", self._name,
+                      virtual_machine.instance_view.statuses[1].display_status)
+        self._state = virtual_machine.instance_view.statuses[1].display_status
