5349385c79d69f2a6d27ac1586423c589934e5f8
Daniel Høyer Iversen <mail@dahoiv.net>
2018-09-02 14:08:46 +0200

    Make switchmate a bit more robust

--- homeassistant/components/switch/switchmate.py
+++ homeassistant/components/switch/switchmate.py
@@ -48,12 +48,37 @@ class Switchmate(SwitchDevice):
         self._state = False
         self._name = name
         self._mac = mac
+        self._device = None
+        if not self._connect():
+            raise PlatformNotReady()
+        
+    def _connect(self) -> bool:
+        import bluepy
+        if self._device:
+            try:
+                self._device.disconnect()
+            except bluepy.btle.BTLEException:
+                pass
         try:
             self._device = bluepy.btle.Peripheral(self._mac,
                                                   bluepy.btle.ADDR_TYPE_RANDOM)
         except bluepy.btle.BTLEException:
-            _LOGGER.error("Failed to set up switchmate")
-            raise PlatformNotReady()
+            _LOGGER.error("Failed to connect to switchmate")
+            return False
+        return True
+
+    def _sendpacket(self, key, retry=2) -> bool:
+        import bluepy
+        try:
+            self._device.writeCharacteristic(HANDLE, key, True)
+        except bluepy.btle.BTLEException:
+            _LOGGER.error("Cannot connect to switchmate. Retrying")
+            if retry < 1:
+                return False
+            if not self._connect():
+                return False
+            self._send_packet(retry-1)
+        return True
 
     @property
     def unique_id(self) -> str:
@@ -67,7 +92,11 @@ class Switchmate(SwitchDevice):
 
     def update(self) -> None:
         """Synchronize state with switch."""
-        self._state = self._device.readCharacteristic(HANDLE) == ON_KEY
+        import bluepy
+        try:
+            self._state = self._device.readCharacteristic(HANDLE) == ON_KEY
+        except bluepy.btle.BTLEException:
+            self._connect()
 
     @property
     def is_on(self) -> bool:
@@ -76,8 +105,8 @@ class Switchmate(SwitchDevice):
 
     def turn_on(self, **kwargs) -> None:
         """Turn the switch on."""
-        self._device.writeCharacteristic(HANDLE, ON_KEY, True)
+        self._sendpacket(ON_KEY)
 
     def turn_off(self, **kwargs) -> None:
         """Turn the switch off."""
-        self._device.writeCharacteristic(HANDLE, OFF_KEY, True)
+        self._sendpacket(OFF_KEY)
