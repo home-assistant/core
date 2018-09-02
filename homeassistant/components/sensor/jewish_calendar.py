0ef3c86b56b2f3bb0f80f28ddf5b9ca031410092
Tsvi Mostovicz <ttmost@gmail.com>
2018-09-02 18:38:39 +0300

    Initial commit for  jewish calendar sensor

---
+++ homeassistant/components/sensor/jewish_calendar.py
@@ -0,0 +1,60 @@
+"""
+Platform to retrieve Jewish calendar information for Home Assistant.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.jewish_calendar/
+"""
+import logging
+
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_NAME
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_NAME = 'Jewish Calendar'
+
+ICON = 'mdi:clock'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+})
+
+
+async def async_setup_platform(
+        hass, config, async_add_entities, discovery_info=None):
+    """Set up the Jewish calendar sensor platform."""
+    name = config.get(CONF_NAME)
+
+    async_add_entities([JewishCalendarSensor(name)], True)
+
+
+class JewishCalendarSensor(Entity):
+    """Representation of an Jewish calendar sensor."""
+
+    def __init__(self, name):
+        """Initialize the Jewish calendar sensor."""
+        self._name = name
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def icon(self):
+        """Icon to display in the front end."""
+        return ICON
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    async def async_update(self):
+        """Update the state of the sensor."""
+        _LOGGER.debug("New value: bla")
