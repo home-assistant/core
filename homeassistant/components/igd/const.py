ddbc4c12e92a9d99d20711579ad069f988ff120c
Steven Looman <steven.looman@gmail.com>
2018-09-02 16:40:39 +0200

    Move ensure_domain_data

--- homeassistant/components/igd/const.py
+++ homeassistant/components/igd/const.py
@@ -8,16 +8,3 @@ CONF_ENABLE_PORT_MAPPING = 'port_forward'
 CONF_ENABLE_SENSORS = 'sensors'
 CONF_UDN = 'udn'
 CONF_SSDP_DESCRIPTION = 'ssdp_description'
-
-
-def ensure_domain_data(hass):
-    """Ensure hass.data is filled properly."""
-    hass.data[DOMAIN] = hass.data.get(DOMAIN, {})
-    hass.data[DOMAIN]['devices'] = hass.data[DOMAIN].get('devices', {})
-    hass.data[DOMAIN]['sensors'] = hass.data[DOMAIN].get('sensors', {})
-    hass.data[DOMAIN]['discovered'] = hass.data[DOMAIN].get('discovered', {})
-    hass.data[DOMAIN]['auto_config'] = hass.data[DOMAIN].get('auto_config', {
-        'active': False,
-        'port_forward': False,
-        'sensors': False,
-    })
