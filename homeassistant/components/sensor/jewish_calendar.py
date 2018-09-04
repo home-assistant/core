afababcb452e88507a9ddb4b7f6b4d8e8581262e
Tsvi Mostovicz <ttmost@gmail.com>
2018-09-04 20:12:28 +0300

    Add multiple sensors

--- homeassistant/components/sensor/jewish_calendar.py
+++ homeassistant/components/sensor/jewish_calendar.py
@@ -5,12 +5,12 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.jewish_calendar/
 """
 import logging
-from datetime import datetime as dt
+from datetime import date
 
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
@@ -19,16 +19,33 @@ REQUIREMENTS = ['hdate==0.6.1']
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_LANGUAGE = 'english'
+SENSOR_TYPES = {
+    'date': ['Date', 'mdi:judaism'],
+    'weekly_portion': ['Parshat Hashavua', 'mdi:book-open-variant'],
+    'holiday_name': ['Holiday', 'mdi:calendar-star'],
+    'holyness': ['Holyness', 'mdi:counter'],
+    'first_light': ['Alot Hashachar', 'mdi:weather-sunset-up'],
+    'gra_end_shma': ['Latest time for Shm"a GR"A', 'mdi:calendar-clock'],
+    'mga_end_shma': ['Latest time for Shm"a MG"A', 'mdi:calendar-clock'],
+    'plag_mincha': ['Plag Hamincha', 'mdi:weather-sunset-down'],
+    'first_stars': ['T\'set Hakochavim', 'mdi:weather-night'],
+}
+
+CONF_DIASPORA = 'diaspora'
+CONF_LANGUAGE = 'language'
+CONF_SENSORS = 'sensors'
 
 DEFAULT_NAME = 'Jewish Calendar'
 
-ICON = 'mdi:calendar-today'
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_LANGUAGE, default='english'): vol.In(
-        ['hebrew', 'english'])
+    vol.Optional(CONF_DIASPORA, default=False): cv.boolean,
+    vol.Optional(CONF_LATITUDE): cv.latitude,
+    vol.Optional(CONF_LONGITUDE): cv.longitude,
+    vol.Optional(CONF_LANGUAGE, default='english'):
+        vol.In(['hebrew', 'english']),
+    vol.Optional(CONF_SENSORS, default=['date']):
+        vol.All(cv.ensure_list, vol.Length(min=1), [vol.In(SENSOR_TYPES)]),
 })
 
 
@@ -37,25 +54,43 @@ async def async_setup_platform(
     """Set up the Jewish calendar sensor platform."""
     language = config.get(CONF_LANGUAGE)
     name = config.get(CONF_NAME)
+    latitude = config.get(CONF_LATITUDE, hass.config.latitude)
+    longitude = config.get(CONF_LONGITUDE, hass.config.longitude)
+    diaspora = config.get(CONF_DIASPORA)
 
-    async_add_entities([JewishCalSensor(name, language)])
+    if None in (latitude, longitude):
+        _LOGGER.error("Latitude or longitude not set in Home Assistant config")
+        return False
+
+    dev = []
+    for sensor_type in config[CONF_SENSORS]:
+        dev.append(JewishCalSensor(
+            name, language, sensor_type, latitude, longitude, diaspora))
+    async_add_entities(dev)
 
 
 class JewishCalSensor(Entity):
     """Representation of an Jewish calendar sensor."""
 
-    def __init__(self, name, language):
+    def __init__(self, name, language, sensor_type,
+                 latitude, longitude, diaspora):
         """Initialize the Jewish calendar sensor."""
-        self._name = name
-        self._date = dt.today()
+        self.client_name = name
+        self._name = SENSOR_TYPES[sensor_type][0]
+        self.type = sensor_type
+        self._date = date.today()
         self._hebrew = (language == 'hebrew')
         self._state = None
-        _LOGGER.debug("Sensor initialized with date %s", self._date)
+        self.latitude = latitude
+        self.longitude = longitude
+        self.diaspora = diaspora
+        _LOGGER.debug("Sensor %s initialized with date %s",
+                      self.type, self._date)
 
     @property
     def icon(self):
         """Icon to display in the front end."""
-        return ICON
+        return SENSOR_TYPES[self.type][1]
 
     @property
     def state(self):
@@ -66,5 +101,23 @@ class JewishCalSensor(Entity):
         """Update the state of the sensor."""
         import hdate
 
-        self._state = str(hdate.HDate(self._date, hebrew=self._hebrew))
+        date = hdate.HDate(
+            self._date, diaspora=self.diaspora, hebrew=self._hebrew)
+
+        if self.type == 'date':
+            self._state = hdate.hdate_string.get_hebrew_date(
+                date.h_day, date.h_month, date.h_year, hebrew=self._hebrew)
+        elif self.type == 'weekly_portion':
+            self._state = date.get_reading(self.diaspora)
+        elif self.type == 'holiday_name':
+            self._state = date.get_holyday_name()
+        elif self.type == 'holyness':
+            self._state = hdate.get_holyday_type(date.get_holyday())
+        else:
+            times = hdate.Zmanim(
+                date=self._date,
+                latitude=self.latitude, longitude=self.longitude,
+                hebrew=self._hebrew).zmanim
+            self._state = times[self.type]
+
         _LOGGER.debug("New value: %s", self._state)
