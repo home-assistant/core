c1c403a6d2e780f279b2b3f710a101f3e95a7218
Jacob Mansfield <cyberjacob@gmail.com>
2018-09-04 22:48:46 +0100

    add Asterisk component

---
+++ homeassistant/components/asterisk_ami.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""Asterisk Phone System"""
+import logging
+
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_USERNAME, CONF_PASSWORD
+from homeassistant.helpers.discovery import load_platform
+
+DEFAULT_HOST = "127.0.0.1"
+DEFAULT_PORT = 5038
+DEFAULT_USERNAME = "manager"
+DEFAULT_PASSWORD = "manager"
+CONF_MONITOR = "monitor"
+
+DOMAIN = "asterisk_ami"
+REQUIREMENTS = ['pyst2==0.5.0']
+DATA_ASTERISK = 'asterisk'
+DATA_MONITOR = 'asterisk-monitor'
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_HOST): cv.string,
+        vol.Optional(CONF_PORT): cv.port,
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string,
+        vol.Optional(CONF_MONITOR): cv.ensure_list,
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup(hass, config):
+    """Your controller/hub specific code."""
+
+    # Needs to be done here because hass needs to load the file to know that the library needs installing
+    import asterisk.manager
+    manager = asterisk.manager.Manager()
+
+    host = config[DOMAIN].get(CONF_HOST, DEFAULT_HOST)
+    port = config[DOMAIN].get(CONF_PORT, DEFAULT_PORT)
+    username = config[DOMAIN].get(CONF_USERNAME, DEFAULT_USERNAME)
+    password = config[DOMAIN].get(CONF_PASSWORD, DEFAULT_PASSWORD)
+
+    try:
+        manager.connect(host, port)
+        login_status = manager.login(username=username, secret=password).get_header("Response")
+    except asterisk.manager.ManagerException as e:
+        _LOGGER.error("Error connecting to Asterisk: %s", e.args[1])
+        return False
+
+    if "Success" not in login_status:
+        _LOGGER.error("Could not authenticate: %s", login_status)
+
+    hass.data[DATA_ASTERISK] = manager
+    hass.data[DATA_MONITOR] = config[DOMAIN].get(CONF_MONITOR, [])
+
+    def handle_peer_status_message(event, manager):
+        """Handle PeerStatus events from Asterisk"""
+        hass.states.set(DOMAIN+'.PeerStatus_'+event['Peer'], event['PeerStatus'])
+
+    def handle_extension_status_message(event, manager):
+        """Handle ExtensionStatus events from Asterisk"""
+        hass.states.set(DOMAIN + '.ExtensionStatus_' + event['Exten'], event['StatusText'])
+
+    def handle_device_state_change_message(event, manager):
+        """Handle DeviceState events from Asterisk"""
+        hass.states.set(DOMAIN + '.DeviceStateChange_' + event['Device'], event['State'])
+
+    def handle_newstate_message(event, manager):
+        """Handle NewState events from Asterisk"""
+        hass.bus.fire('asterisk.new_call_state', event)
+
+    def handle_hangup_message(event, manager):
+        """Handle Hangup events from Asterisk"""
+        hass.bus.fire('asterisk.hangup', event)
+
+    manager.register_event('PeerStatus', handle_peer_status_message)
+    manager.register_event('ExtensionStatus', handle_extension_status_message)
+    manager.register_event('DeviceStateChange', handle_device_state_change_message)
+
+    manager.register_event('Newstate', handle_newstate_message)
+    manager.register_event('Hangup', handle_hangup_message)
+
+    load_platform(hass, 'sensor', DOMAIN)
+
+    return True
---
+++ homeassistant/components/sensor/asterisk_ami.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+"""Asterisk Sensors"""
+import logging
+
+from homeassistant.const import STATE_UNKNOWN
+from homeassistant.helpers.entity import Entity
+
+from .. import asterisk_ami as ami_platform
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Sensor setup
+       Loads configuration and creates devices for extensions and mailboxes
+    """
+    add_devices([AsteriskSensor(hass)])
+    for mailbox in hass.data[ami_platform.DATA_MAILBOX]:
+        add_devices([AsteriskMailbox(hass, mailbox)])
+
+
+class AsteriskSensor(Entity):
+    """Asterisk Server connection status sensor."""
+
+    def __init__(self, hass):
+        """Initialize the sensor."""
+        self._hass = hass
+        self._state = STATE_UNKNOWN
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Asterisk Connection'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def should_poll(self):
+        return True
+
+    def update(self):
+        """Check the connection status and update the internal state"""
+        if self._hass.data[ami_platform.DATA_ASTERISK].connected():
+            self._state = 'connected'
+        else:
+            self._state = 'disconnected'
+
+            ami_platform.setup(self._hass, self._hass.config)
+
+
+class AsteriskMailbox(Entity):
+    """Voicemail Mailbox Sensor."""
+    def __init__(self, hass, mailbox):
+        self._hass = hass
+        self._mailbox = mailbox
+        self._state = STATE_UNKNOWN
+
+    @property
+    def name(self):
+        return "Asterisk Mailbox " + self._mailbox
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def should_poll(self):
+        return True
+
+    def update(self):
+        """Check the mailbox status and update the state"""
+        cdict = {'Action': 'MailboxStatus'}
+        cdict['Peer'] = self._mailbox
+        response = self._hass.data[DATA_ASTERISK].send_action(cdict)
+        _LOGGER.info(response)
+        _LOGGER.info(response.headers)
+        if response.get_header('Response') == 'Error':
+            raise Exception(response.get_header('Message'))
+        self._state = response.get_header('Waiting', STATE_UNKNOWN)
