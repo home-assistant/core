1767e47b313262e317076e2570140dc8aead2a5f
Mike Rosseel <mike.rosseel@gmail.com>
2018-09-05 22:59:46 +0200

    fix linting errors

--- homeassistant/components/sensor/qnap.py
+++ homeassistant/components/sensor/qnap.py
@@ -13,11 +13,11 @@ from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 from homeassistant.const import (
     CONF_HOST, CONF_USERNAME, CONF_PASSWORD, CONF_PORT, CONF_SSL, ATTR_NAME,
-    CONF_VERIFY_SSL, CONF_TIMEOUT, CONF_MONITORED_CONDITIONS, TEMP_CELSIUS, CONF_ALLOW_UNREACHABLE)
+    CONF_VERIFY_SSL, CONF_TIMEOUT, CONF_MONITORED_CONDITIONS, TEMP_CELSIUS,
+    CONF_ALLOW_UNREACHABLE)
 from homeassistant.util import Throttle
 from homeassistant.exceptions import PlatformNotReady
 import homeassistant.helpers.config_validation as cv
-
 
 REQUIREMENTS = ['qnapstats==0.2.7']
 
@@ -117,11 +117,11 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
             raise PlatformNotReady
         else:
             hass.components.persistent_notification.create(
-            'Error: Failed to set up QNAP sensor.<br />'
-            'Check the logs for additional information. '
-            'You will need to restart hass after fixing.',
-            title=NOTIFICATION_TITLE,
-            notification_id=NOTIFICATION_ID)
+                'Error: Failed to set up QNAP sensor.<br />'
+                'Check the logs for additional information. '
+                'You will need to restart hass after fixing.',
+                title=NOTIFICATION_TITLE,
+                notification_id=NOTIFICATION_ID)
         return False
 
     sensors = []
