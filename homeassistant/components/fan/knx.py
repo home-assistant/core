800a17e170aaec856267f339e32803124038324c
itineric <itineric@gmail.com>
2018-09-05 13:19:37 +0200

    Add KNX fan component

--- homeassistant/components/fan/knx.py
+++ homeassistant/components/fan/knx.py
@@ -8,13 +8,14 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components.knx import DATA_KNX, ATTR_DISCOVER_DEVICES
-from homeassistant.components.fan import PLATFORM_SCHEMA, FanEntity, SUPPORT_SET_SPEED, ATTR_SPEED
+from homeassistant.components.fan import (
+    PLATFORM_SCHEMA, FanEntity, SUPPORT_SET_SPEED)
 from homeassistant.const import CONF_NAME
 from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 
-CONF_ADDRESS = 'speed_address'
-CONF_STATE_ADDRESS = 'speed_state_address'
+CONF_SPEED_ADDRESS = 'speed_address'
+CONF_SPEED_STATE_ADDRESS = 'speed_state_address'
 
 DEFAULT_NAME = 'KNX Fan'
 DEPENDENCIES = ['knx']
@@ -22,14 +23,16 @@ DEPENDENCIES = ['knx']
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_ADDRESS): cv.string,
+    vol.Required(CONF_SPEED_ADDRESS): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_STATE_ADDRESS): cv.string,
+    vol.Optional(CONF_SPEED_STATE_ADDRESS): cv.string,
 })
 
 
-async def async_setup_platform(hass, config, async_add_devices,
-                         discovery_info=None):
+async def async_setup_platform(hass,
+                               config,
+                               async_add_devices,
+                               discovery_info=None):
     """Set up fan(s) for KNX platform."""
     if discovery_info is not None:
         async_add_devices_discovery(hass, discovery_info, async_add_devices)
@@ -54,8 +57,8 @@ def async_add_devices_config(hass, config, async_add_devices):
     fan = xknx.devices.Fan(
         hass.data[DATA_KNX].xknx,
         name=config.get(CONF_NAME),
-        group_address_speed=config.get(CONF_ADDRESS),
-        group_address_speed_state=config.get(CONF_STATE_ADDRESS))
+        group_address_speed=config.get(CONF_SPEED_ADDRESS),
+        group_address_speed_state=config.get(CONF_SPEED_STATE_ADDRESS))
     hass.data[DATA_KNX].xknx.devices.add(fan)
     async_add_devices([KNXFan(hass, fan)])
 
@@ -73,7 +76,7 @@ class KNXFan(FanEntity):
     def async_register_callbacks(self):
         """Register callbacks to update hass after device was changed."""
         async def after_update_callback(device):
-            """Callback after device was updated."""
+            """Call after device was updated."""
             # pylint: disable=unused-argument
             await self.async_update_ha_state()
         self.device.register_device_updated_cb(after_update_callback)
@@ -106,22 +109,20 @@ class KNXFan(FanEntity):
         """Get the list of available speeds."""
         return [0, 25, 50, 75, 100]
 
-    async def async_turn_on(self, **kwargs):
+    async def async_turn_on(self, speed: str = None, **kwargs):
         """Turn on the entity."""
-        if ATTR_SPEED in kwargs:
-            speed = int(kwargs[ATTR_SPEED])
+        if speed:
+            await self._internal_set_speed(int(speed))
         else:
-            speed = 50
-        await self._internal_set_speed(speed)
+            await self._internal_set_speed(50)
 
     async def async_turn_off(self, **kwargs):
         """Turn off the entity."""
-        self.oscillate(False)
         await self._internal_set_speed(0)
 
-    async def async_set_speed(self, **kwargs):
+    async def async_set_speed(self, speed: str):
         """Set the speed of the fan."""
-        await self._internal_set_speed(int(kwargs[ATTR_SPEED]))
+        await self._internal_set_speed(int(speed))
 
     async def _internal_set_speed(self, speed: int):
         await self.device.set_speed(speed)
