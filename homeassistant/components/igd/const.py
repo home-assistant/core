0df0810079ac7ef909099318805982afa8b109a2
Steven Looman <steven.looman@gmail.com>
2018-09-08 00:11:23 +0200

    Working on igd

--- homeassistant/components/igd/const.py
+++ homeassistant/components/igd/const.py
@@ -2,9 +2,12 @@
 import logging
 
 
-DOMAIN = 'igd'
-LOGGER = logging.getLogger('homeassistant.components.igd')
 CONF_ENABLE_PORT_MAPPING = 'port_forward'
 CONF_ENABLE_SENSORS = 'sensors'
-CONF_UDN = 'udn'
+CONF_HASS = 'hass'
+CONF_LOCAL_IP = 'local_ip'
+CONF_PORTS = 'ports'
 CONF_SSDP_DESCRIPTION = 'ssdp_description'
+CONF_UDN = 'udn'
+DOMAIN = 'igd'
+LOGGER = logging.getLogger('homeassistant.components.igd')
