dd24988104f7545aded03b82d24e1ea9810f4d5c
Adam Dullage <spam@dullage.com>
2018-09-09 09:11:50 +0100

    Added support for Starling Bank

---
+++ homeassistant/components/sensor/starlingbank.py
@@ -0,0 +1,97 @@
+"""
+Support for balance data via the Starling Bank API.
+
+For more details about this platform, please refer to the documentation at
+https://www.home-assistant.io/components/sensor.starlingbank/
+"""
+import voluptuous as vol
+
+from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+    CONF_ACCESS_TOKEN, CONF_NAME
+)
+import homeassistant.helpers.config_validation as cv
+
+REQUIREMENTS = ['starlingbank==1.1']
+
+BALANCE_TYPES = ['cleared_balance', 'effective_balance']
+DEFAULT_ACCOUNT_NAME = 'Starling'
+ICON = 'mdi:currency-gbp'
+
+CONF_ACCOUNTS = 'accounts'
+CONF_BALANCE_TYPES = 'balance_types'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_ACCOUNTS): vol.All([{
+            vol.Optional(CONF_NAME, default=DEFAULT_ACCOUNT_NAME): cv.string,
+            vol.Required(CONF_ACCESS_TOKEN): cv.string,
+            vol.Optional(
+                CONF_BALANCE_TYPES, default=BALANCE_TYPES
+                ): vol.All(
+                    cv.ensure_list,  # Wraps value in a list if it is not one.
+                    [vol.In(BALANCE_TYPES)]
+                )
+        }], cv.ensure_list)
+    }
+)
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the sensor platform."""
+    accounts = config.get(CONF_ACCOUNTS)
+
+    sensors = []
+    for account in accounts:
+        for balance_type in account[CONF_BALANCE_TYPES]:
+            sensors.append(StarlingBalanceSensor(
+                account[CONF_ACCESS_TOKEN],
+                account[CONF_NAME],
+                balance_type
+            ))
+    add_devices(sensors, True)  # True forces an update upon platform setup.
+
+
+class StarlingBalanceSensor(Entity):
+    """Representation of a Starling balance sensor."""
+
+    def __init__(self, token, account_name, balance_data_type):
+        """Initialize the sensor."""
+        from starlingbank import StarlingAccount
+
+        self._starling_account = StarlingAccount(token)
+        self._balance_data_type = balance_data_type
+        self._state = None
+        self._account_name = account_name
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        if self._balance_data_type == 'cleared_balance':
+            return "{0} Cleared Balance".format(self._account_name)
+        elif self._balance_data_type == 'effective_balance':
+            return "{0} Effective Balance".format(self._account_name)
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return self._starling_account.currency
+
+    @property
+    def icon(self):
+        """Return the entity icon."""
+        return ICON
+
+    def update(self):
+        """Fetch new state data for the sensor."""
+        self._starling_account.balance.update()
+        if self._balance_data_type == 'cleared_balance':
+            self._state = self._starling_account.balance.cleared_balance
+        elif self._balance_data_type == 'effective_balance':
+            self._state = self._starling_account.balance.effective_balance
