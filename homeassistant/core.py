7f081d64865b0b32ced644559d26ed32ada9ad05
Ville Skyttä <ville.skytta@iki.fi>
2018-09-11 12:21:48 +0300

    Isort preparations (#16555)
    * Don't treat typing as an "in-between" module for import order
    
    That was a < 3.5 era thing.
    
    * Tighten scope of some pylint unused-import disables
    
    To avoid isort moving a top level one around, undesirably broadening its
    scope.

--- homeassistant/core.py
+++ homeassistant/core.py
@@ -18,8 +18,7 @@ from time import monotonic
 import uuid
 
 from types import MappingProxyType
-# pylint: disable=unused-import
-from typing import (  # NOQA
+from typing import (  # noqa: F401 pylint: disable=unused-import
     Optional, Any, Callable, List, TypeVar, Dict, Coroutine, Set,
     TYPE_CHECKING, Awaitable, Iterator)
 
