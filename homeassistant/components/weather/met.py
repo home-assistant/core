84c081ee89e750b4207c1ccc484560a8d3df8908
Daniel Iversen <Daniel.Iversen@tibber.com>
2018-09-15 14:31:36 +0200

    use lib

--- homeassistant/components/weather/met.py
+++ homeassistant/components/weather/met.py
@@ -4,26 +4,21 @@ Support for Met.no weather service.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/weather.met/
 """
-import asyncio
 import logging
 from datetime import timedelta
-
 from random import randrange
-from xml.parsers.expat import ExpatError
 
-import aiohttp
-import async_timeout
 import voluptuous as vol
 
 from homeassistant.components.weather import PLATFORM_SCHEMA, WeatherEntity
 from homeassistant.const import (CONF_ELEVATION, CONF_LATITUDE, CONF_LONGITUDE,
                                  CONF_NAME, TEMP_CELSIUS)
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.event import async_call_later
 from homeassistant.util import dt as dt_util, Throttle
 
-REQUIREMENTS = ['xmltodict==0.11.0']
+REQUIREMENTS = ['pyMetno==0.2.0']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -104,19 +99,22 @@ async def async_setup_platform(hass, config, async_add_entities,
         'msl': str(elevation),
     }
 
-    async_add_entities([MetWeather(name, coordinates)])
+    async_add_entities([MetWeather(name, coordinates, async_get_clientsession(hass))])
 
 
 class MetWeather(WeatherEntity):
     """Implementation of a Met.no weather condition."""
 
-    def __init__(self, name, coordinates):
+    def __init__(self, name, coordinates, clientsession):
         """Initialise the platform with a data instance and site."""
+        import metno
         self._name = name
         self._urlparams = coordinates
-        self._weather_data = None
-        self._url = 'https://aa015h6buqvih86i1.api.met.no/'\
-            'weatherapi/locationforecast/1.9/'
+        self._weather_data = metno.MetWeatherData(coordinates,
+                                                  clientsession,
+                                                  'https://aa015h6buqvih86i1.api.met.no/'
+                                                  'weatherapi/locationforecast/1.9/'
+                                                  )
         self._temperature = None
         self._condition = None
         self._pressure = None
@@ -130,31 +128,12 @@ class MetWeather(WeatherEntity):
 
     async def _fetching_data(self, *_):
         """Get the latest data from met.no."""
-        import xmltodict
 
-        def try_again(err: str):
-            """Retry in 15 to 20 minutes."""
+        if not await self._weather_data.fetching_data():
+            # Retry in 15 to 20 minutes.
             minutes = 15 + randrange(6)
-            _LOGGER.error("Retrying in %i minutes: %s", minutes, err)
+            _LOGGER.error("Retrying in %i minutes", minutes)
             async_call_later(self.hass, minutes*60, self._fetching_data)
-        try:
-            websession = async_get_clientsession(self.hass)
-            with async_timeout.timeout(10, loop=self.hass.loop):
-                resp = await websession.get(
-                    self._url, params=self._urlparams)
-            if resp.status != 200:
-                try_again('{} returned {}'.format(resp.url, resp.status))
-                return
-            text = await resp.text()
-
-        except (asyncio.TimeoutError, aiohttp.ClientError) as err:
-            try_again(err)
-            return
-
-        try:
-            self._weather_data = xmltodict.parse(text)['weatherdata']
-        except (ExpatError, IndexError) as err:
-            try_again(err)
             return
 
         async_call_later(self.hass, 60*60, self._fetching_data)
@@ -162,13 +141,14 @@ class MetWeather(WeatherEntity):
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """Get the latest data from Met.no."""
+        import metno
         if self._weather_data is None:
             return
 
         now = dt_util.utcnow()
 
         ordered_entries = []
-        for time_entry in self._weather_data['product']['time']:
+        for time_entry in self._weather_data.data['product']['time']:
             valid_from = dt_util.parse_datetime(time_entry['@from'])
             valid_to = dt_util.parse_datetime(time_entry['@to'])
 
@@ -185,13 +165,13 @@ class MetWeather(WeatherEntity):
             return
         ordered_entries.sort(key=lambda item: item[0])
 
-        self._temperature = get_forecast('temperature', ordered_entries)
-        self._condition = CONDITIONS.get(get_forecast('symbol',
-                                                      ordered_entries))
-        self._pressure = get_forecast('pressure', ordered_entries)
-        self._humidity = get_forecast('humidity', ordered_entries)
-        self._wind_speed = get_forecast('windSpeed', ordered_entries)
-        self._wind_bearing = get_forecast('windDirection', ordered_entries)
+        self._temperature = metno.get_forecast('temperature', ordered_entries)
+        self._condition = CONDITIONS.get(metno.get_forecast('symbol',
+                                                            ordered_entries))
+        self._pressure = metno.get_forecast('pressure', ordered_entries)
+        self._humidity = metno.get_forecast('humidity', ordered_entries)
+        self._wind_speed = metno.get_forecast('windSpeed', ordered_entries)
+        self._wind_bearing = metno.get_forecast('windDirection', ordered_entries)
 
     @property
     def name(self):
@@ -237,31 +217,3 @@ class MetWeather(WeatherEntity):
     def attribution(self):
         """Return the attribution."""
         return CONF_ATTRIBUTION
-
-
-def get_forecast(param, data):
-    """Retrieve forecast parameter."""
-    try:
-        for (_, selected_time_entry) in data:
-            loc_data = selected_time_entry['location']
-
-            if param not in loc_data:
-                continue
-
-            if param == 'precipitation':
-                new_state = loc_data[param]['@value']
-            elif param == 'symbol':
-                new_state = int(float(loc_data[param]['@number']))
-            elif param in ('temperature', 'pressure', 'humidity',
-                           'dewpointTemperature'):
-                new_state = round(float(loc_data[param]['@value']), 1)
-            elif param in ('windSpeed', 'windGust'):
-                new_state = round(float(loc_data[param]['@mps']) * 3.6, 1)
-            elif param == 'windDirection':
-                new_state = round(float(loc_data[param]['@deg']), 1)
-            elif param in ('fog', 'cloudiness', 'lowClouds',
-                           'mediumClouds', 'highClouds'):
-                new_state = round(float(loc_data[param]['@percent']), 1)
-            return new_state
-    except (ValueError, IndexError, KeyError):
-        return None
