fc6ee20c61d2b64745de47605a4758b353126e01
Anders Melchiorsen <amelchio@nogoto.net>
2018-09-20 23:50:11 +0200

    Use pysonos for Sonos media player (#16753)

--- tests/components/sonos/test_init.py
+++ tests/components/sonos/test_init.py
@@ -12,7 +12,7 @@ async def test_creating_entry_sets_up_media_player(hass):
     """Test setting up Sonos loads the media player."""
     with patch('homeassistant.components.media_player.sonos.async_setup_entry',
                return_value=mock_coro(True)) as mock_setup, \
-            patch('soco.discover', return_value=True):
+            patch('pysonos.discover', return_value=True):
         result = await hass.config_entries.flow.async_init(
             sonos.DOMAIN, context={'source': config_entries.SOURCE_USER})
         assert result['type'] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
@@ -26,7 +26,7 @@ async def test_configuring_sonos_creates_entry(hass):
     """Test that specifying config will create an entry."""
     with patch('homeassistant.components.sonos.async_setup_entry',
                return_value=mock_coro(True)) as mock_setup, \
-            patch('soco.discover', return_value=True):
+            patch('pysonos.discover', return_value=True):
         await async_setup_component(hass, sonos.DOMAIN, {
             'sonos': {
                 'some_config': 'to_trigger_import'
@@ -41,7 +41,7 @@ async def test_not_configuring_sonos_not_creates_entry(hass):
     """Test that no config will not create an entry."""
     with patch('homeassistant.components.sonos.async_setup_entry',
                return_value=mock_coro(True)) as mock_setup, \
-            patch('soco.discover', return_value=True):
+            patch('pysonos.discover', return_value=True):
         await async_setup_component(hass, sonos.DOMAIN, {})
         await hass.async_block_till_done()
 
