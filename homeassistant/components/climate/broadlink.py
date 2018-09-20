9e8663d41859a511a927292dffdd744d5d120d3e
Clément TALVARD <c.talvard@gmail.com>
2018-09-20 21:43:20 +0200

    fix

--- homeassistant/components/climate/broadlink.py
+++ homeassistant/components/climate/broadlink.py
@@ -1,5 +1,5 @@
 """
-Support for Chinese wifi thermostats (Floureon, Beok, Beca Energy).
+Support for Chinese wifi thermostats (Floureon, Beok, Beca Energy)
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/climate.broadlink/
@@ -25,8 +25,10 @@ _LOGGER = logging.getLogger(__name__)
 REQUIREMENTS = ['broadlink==0.9.0', 'BroadlinkWifiThermostat==1.0.1']
 
 DEFAULT_NAME = 'broadlink'
-
-OPERATION_LIST = [STATE_AUTO, STATE_HEAT, STATE_OFF]
+POWER_ON = 1
+POWER_OFF = 0
+AUTO = 1
+MANUAL = 0
 CONF_MODE_LIST = 'modes'
 CONF_MIN_TEMP = 'min_temp'
 CONF_MAX_TEMP = 'max_temp'
@@ -43,9 +45,42 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
     vol.Optional(CONF_MIN_TEMP, default=5): cv.positive_int,
     vol.Optional(CONF_MAX_TEMP, default=35): cv.positive_int,
-    vol.Optional(CONF_ADVANCED_CONFIG): cv.string,
-    vol.Optional(CONF_SCHEDULE_WEEKDAY): cv.string,
-    vol.Optional(CONF_SCHEDULE_WEEKEND): cv.string
+    vol.Optional(CONF_ADVANCED_CONFIG,
+                 default='{"loop_mode": 0, '
+                         '"sen": 0, '
+                         '"osv": 42, '
+                         '"dif": 2, '
+                         '"svh": 35, '
+                         '"svl": 5, '
+                         '"adj": 0, '
+                         '"fre": 1, '
+                         '"pon": 0}'): cv.string,
+    vol.Optional(CONF_SCHEDULE_WEEKDAY,
+                 default='[{"start_hour":6, '
+                         '"start_minute":30, '
+                         '"temp":20}, '
+                         '{"start_hour":9, '
+                         '"start_minute":0, '
+                         '"temp":17}, '
+                         '{"start_hour":12, '
+                         '"start_minute":0, '
+                         '"temp":20}, '
+                         '{"start_hour":14, '
+                         '"start_minute":0, '
+                         '"temp":17}, '
+                         '{"start_hour":18, '
+                         '"start_minute":0, '
+                         '"temp":20}, '
+                         '{"start_hour":22, '
+                         '"start_minute":30, '
+                         '"temp":17}]'): cv.string,
+    vol.Optional(CONF_SCHEDULE_WEEKEND,
+                 default='[{"start_hour":8, '
+                         '"start_minute":30, '
+                         '"temp":20}, '
+                         '{"start_hour":23, '
+                         '"start_minute":0, '
+                         '"temp":17}]'): cv.string
 })
 
 SET_SCHEDULE_SCHEMA = vol.Schema({
@@ -151,13 +186,18 @@ class BroadlinkThermostat(ClimateDevice):
 
     @property
     def state(self):
-        """Return the current state."""
         return self._device.current_operation
 
     @property
     def supported_features(self):
         """Return the list of supported features."""
-        return SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE
+        return SUPPORT_TARGET_TEMPERATURE \
+               | SUPPORT_OPERATION_MODE
+
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return True
 
     @property
     def name(self):
@@ -197,31 +237,34 @@ class BroadlinkThermostat(ClimateDevice):
     @property
     def operation_list(self):
         """List of available operation modes."""
-        return OPERATION_LIST
+        return [STATE_AUTO, STATE_HEAT, STATE_OFF]
 
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
-        current_temp = kwargs.get(ATTR_TEMPERATURE)
-        if current_temp is not None:
-            self._device.set_temperature(current_temp)
+        if kwargs.get(ATTR_TEMPERATURE) is not None:
+            self._device.set_temperature(kwargs.get(ATTR_TEMPERATURE))
+        self.schedule_update_ha_state()
 
     def set_operation_mode(self, operation_mode):
         """Set operation mode."""
         self._device.set_operation_mode(operation_mode)
+        self.schedule_update_ha_state()
 
     @property
     def is_on(self):
         """Return true if the device is on."""
-        return self._device.current_operation != STATE_OFF
+        return False if self._device.current_operation == STATE_OFF else True
 
-    def set_advanced_config(self, config_json):
-        """Set the thermostat advanced config."""
+    def set_advance_config(self, config_json):
+        """Set the thermostat advanced config"""
         self._device.set_advanced_config(json.loads(config_json))
+        self.schedule_update_ha_state()
 
     def set_schedule(self, schedule_json):
-        """Set the thermostat schedule."""
+        """Set the thermostat schedule"""
         self._device.set_schedule(json.loads(schedule_json))
+        self.schedule_update_ha_state()
 
     def update(self):
-        """Update component data."""
+        """Update component data"""
         self._device.read_status()
