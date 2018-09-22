7bd5bd14faba9715cf2f949f4b6f4984b41bdfea
Malte Franken <exxamalte@users.noreply.github.com>
2018-09-22 16:13:07 +1000

    improved test coverage

--- tests/components/geo_location/test_nsw_rural_fire_service_feed.py
+++ tests/components/geo_location/test_nsw_rural_fire_service_feed.py
@@ -1,14 +1,17 @@
 """The tests for the geojson platform."""
+import datetime
 import unittest
 from unittest import mock
 from unittest.mock import patch, MagicMock
 
 from homeassistant.components import geo_location
 from homeassistant.components.geo_location.nsw_rural_fire_service_feed import \
-    ATTR_EXTERNAL_ID, SCAN_INTERVAL, ATTR_CATEGORY, ATTR_FIRE
+    ATTR_EXTERNAL_ID, SCAN_INTERVAL, ATTR_CATEGORY, ATTR_FIRE, ATTR_LOCATION, \
+    ATTR_COUNCIL_AREA, ATTR_STATUS, ATTR_TYPE, ATTR_SIZE, \
+    ATTR_RESPONSIBLE_AGENCY, ATTR_PUBLICATION_DATE
 from homeassistant.const import CONF_URL, EVENT_HOMEASSISTANT_START, \
     CONF_RADIUS, ATTR_LATITUDE, ATTR_LONGITUDE, ATTR_FRIENDLY_NAME, \
-    ATTR_UNIT_OF_MEASUREMENT
+    ATTR_UNIT_OF_MEASUREMENT, ATTR_ATTRIBUTION
 from homeassistant.setup import setup_component
 from tests.common import get_test_home_assistant, assert_setup_component, \
     fire_time_changed
@@ -41,8 +44,8 @@ class TestGeoJsonPlatform(unittest.TestCase):
     def _generate_mock_feed_entry(external_id, title, distance_to_home,
                                   coordinates, category=None, location=None,
                                   attribution=None, publication_date=None,
-                                  council_area=None, status=None, type=None,
-                                  fire=True, size=None,
+                                  council_area=None, status=None,
+                                  entry_type=None, fire=True, size=None,
                                   responsible_agency=None):
         """Construct a mock feed entry for testing purposes."""
         feed_entry = MagicMock()
@@ -56,7 +59,7 @@ class TestGeoJsonPlatform(unittest.TestCase):
         feed_entry.publication_date = publication_date
         feed_entry.council_area = council_area
         feed_entry.status = status
-        feed_entry.type = type
+        feed_entry.type = entry_type
         feed_entry.fire = fire
         feed_entry.size = size
         feed_entry.responsible_agency = responsible_agency
@@ -67,9 +70,13 @@ class TestGeoJsonPlatform(unittest.TestCase):
     def test_setup(self, mock_feed):
         """Test the general setup of the platform."""
         # Set up some mock feed entries for this test.
-        mock_entry_1 = self._generate_mock_feed_entry('1234', 'Title 1', 15.5,
-                                                      (-31.0, 150.0),
-                                                      'Category 1')
+        mock_entry_1 = self._generate_mock_feed_entry(
+            '1234', 'Title 1', 15.5, (-31.0, 150.0), category='Category 1',
+            location='Location 1', attribution='Attribution 1',
+            publication_date=datetime.datetime(2018, 9, 22, 8, 0,
+                                               tzinfo=datetime.timezone.utc),
+            council_area='Council Area 1', status='Status 1',
+            entry_type='Type 1', size='Size 1', responsible_agency='Agency 1')
         mock_entry_2 = self._generate_mock_feed_entry('2345', 'Title 2', 20.5,
                                                       (-31.1, 150.1))
         mock_entry_3 = self._generate_mock_feed_entry('3456', 'Title 3', 25.5,
@@ -97,10 +104,19 @@ class TestGeoJsonPlatform(unittest.TestCase):
                 state = self.hass.states.get("geo_location.title_1")
                 self.assertIsNotNone(state)
                 assert state.name == "Title 1"
+                print(state.attributes)
                 assert state.attributes == {
                     ATTR_EXTERNAL_ID: "1234", ATTR_LATITUDE: -31.0,
                     ATTR_LONGITUDE: 150.0, ATTR_FRIENDLY_NAME: "Title 1",
-                    ATTR_CATEGORY: "Category 1", ATTR_FIRE: True,
+                    ATTR_CATEGORY: "Category 1", ATTR_LOCATION: "Location 1",
+                    ATTR_ATTRIBUTION: "Attribution 1",
+                    ATTR_PUBLICATION_DATE:
+                        datetime.datetime(2018, 9, 22, 8, 0,
+                                          tzinfo=datetime.timezone.utc),
+                    ATTR_FIRE: True,
+                    ATTR_COUNCIL_AREA: 'Council Area 1',
+                    ATTR_STATUS: 'Status 1', ATTR_TYPE: 'Type 1',
+                    ATTR_SIZE: 'Size 1', ATTR_RESPONSIBLE_AGENCY: 'Agency 1',
                     ATTR_UNIT_OF_MEASUREMENT: "km"}
                 self.assertAlmostEqual(float(state.state), 15.5)
 
