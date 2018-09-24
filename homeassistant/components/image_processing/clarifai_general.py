91fbfa622976b06173e814de21bc35f77c889b5e
robmarkcole <robmarkcole@gmail.com>
2018-09-24 08:14:47 +0100

    Adds clarifai

---
+++ homeassistant/components/image_processing/clarifai_general.py
@@ -0,0 +1,143 @@
+"""
+Component that will perform image classification via Clarifai general model.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/image_processing/clarifai_general
+"""
+import base64
+import json
+import logging
+
+import voluptuous as vol
+
+from homeassistant.core import split_entity_id
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.image_processing import (
+    PLATFORM_SCHEMA, ImageProcessingEntity, CONF_SOURCE, CONF_ENTITY_ID,
+    CONF_NAME)
+
+_LOGGER = logging.getLogger(__name__)
+
+CLASSIFIER = 'Clarifai'
+CONF_API_KEY = 'api_key'
+CONF_CONCEPTS = 'concepts'
+DEFAULT_CONCEPTS = 'None'
+EVENT_MODEL_PREDICTION = 'image_processing.model_prediction'
+
+REQUIREMENTS = ['clarifai==2.2.3']
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_API_KEY): cv.string,
+    vol.Optional(CONF_CONCEPTS, default=[DEFAULT_CONCEPTS]):
+        vol.All(cv.ensure_list, [cv.string]),
+})
+
+
+def encode_image(image):
+    """base64 encode an image stream."""
+    base64_img = base64.b64encode(image)
+    return base64_img
+
+
+def parse_concepts(api_concepts):
+    """Parse the API concepts data."""
+    return {concept['name']: round(100.0*concept['value'], 2)
+            for concept in api_concepts}
+
+
+def validate_api_key(api_key):
+    """Check that an API key is valid, if yes return the app."""
+    try:
+        from clarifai.rest import ClarifaiApp, ApiError
+        app = ClarifaiApp(api_key=api_key)
+        return app
+    except ApiError as exc:
+        error = json.loads(exc.response.content)
+        _LOGGER.error(
+            "%s error: %s", CLASSIFIER, error['status']['description'])
+        return None
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the Clarifai classifier."""
+    api_key = config[CONF_API_KEY]
+    app = validate_api_key(api_key)
+    if app is None:
+        return
+
+    entities = []
+    for camera in config[CONF_SOURCE]:
+        entities.append(ClarifaiClassifier(
+            app,
+            config[CONF_CONCEPTS],
+            camera[CONF_ENTITY_ID],
+            camera.get(CONF_NAME),
+        ))
+    add_devices(entities)
+
+
+class ClarifaiClassifier(ImageProcessingEntity):
+    """Perform a classification via Clarifai."""
+
+    def __init__(self, app, concepts, camera_entity, name=None):
+        """Init with the API key."""
+        model = 'general-v1.3'
+        self.model = app.models.get(model)
+        if name:  # Since name is optional.
+            self._name = name
+        else:
+            entity_name = split_entity_id(camera_entity)[1]
+            self._name = "{} {}".format(CLASSIFIER, entity_name)
+        self._concepts = concepts
+        self._camera_entity = camera_entity
+        self._classifications = {}  # The dict of classifications.
+        self._state = None  # The most likely classification.
+
+    def process_image(self, image):
+        """Perform classification of a single image."""
+        response = self.model.predict_by_base64(encode_image(image))
+
+        if response['status']['description'] == 'Ok':
+            api_concepts = response['outputs'][0]['data']['concepts']
+            self._classifications = parse_concepts(api_concepts)
+            self._state = next(iter(self._classifications))
+            self.fire_concept_events()
+        else:
+            self._state = "Request_failed"
+            self._classifications = {}
+
+    def fire_concept_events(self):
+        """Fire an event for each concept identified."""
+        identified_concepts = self._classifications.keys() & self._concepts
+        for concept in identified_concepts:
+            self.hass.bus.fire(
+                EVENT_MODEL_PREDICTION, {
+                    CONF_ENTITY_ID: self._camera_entity,
+                    'concept': concept,
+                    })
+
+    @property
+    def device_class(self):
+        """Return the class of this device, from component DEVICE_CLASSES."""
+        return 'class'
+
+    @property
+    def camera_entity(self):
+        """Return camera entity id from process pictures."""
+        return self._camera_entity
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        return self._state
+
+    @property
+    def state_attributes(self):
+        """Return device specific state attributes."""
+        attr = self._classifications
+        return attr
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
---
+++ tests/components/image_processing/test_clarifai_general.py
@@ -0,0 +1,123 @@
+"""The tests for the clarifai_general component."""
+from unittest.mock import patch
+
+import pytest
+
+from homeassistant.core import callback
+from homeassistant.const import ATTR_ENTITY_ID, CONF_FRIENDLY_NAME
+from homeassistant.setup import async_setup_component
+import homeassistant.components.image_processing as ip
+import homeassistant.components.image_processing.clarifai_general as cg
+
+
+MOCK_NAME = 'mock_name'
+MOCK_API_KEY = '12345'
+MOCK_KEY_ERROR = {'status': {'description': 'API key not found'}}
+MOCK_RESPONSE = {'status': {'description': 'Ok'},
+                 'outputs': [{'data': {'concepts': [{'name': 'dog',
+                                                     'value': 0.85432},
+                                                    {'name': 'cat',
+                                                     'value': 0.14568}]}}]}
+
+PARSED_CONCEPTS = {'cat': 14.57, 'dog': 85.43}
+
+VALID_ENTITY_ID = 'image_processing.clarifai_demo_camera'
+VALID_CONFIG = {
+    ip.DOMAIN: {
+        'platform': 'clarifai_general',
+        cg.CONF_API_KEY: MOCK_API_KEY,
+        ip.CONF_SOURCE: {
+            ip.CONF_ENTITY_ID: 'camera.demo_camera'},
+        cg.CONF_CONCEPTS: ['dog'],
+    },
+    'camera': {
+        'platform': 'demo'
+        }
+    }
+
+
+@pytest.fixture
+def mock_app():
+    """Return a mock ClarifaiApp object."""
+    with patch('clarifai.rest.ClarifaiApp') as _mock_app:
+        yield _mock_app
+
+
+@pytest.fixture
+def mock_image():
+    """Return a mock camera image."""
+    with patch('homeassistant.components.camera.demo.DemoCamera.camera_image',
+               return_value=b'Test') as image:
+        yield image
+
+
+@pytest.fixture
+def mock_response():
+    """Return a mock response from Clarifai."""
+    with patch('clarifai.rest.ClarifaiApp.model.predict_by_base64',
+               return_value=MOCK_RESPONSE) as _mock_response:
+        yield _mock_response
+
+
+def test_encode_image():
+    """Test that binary data is encoded correctly."""
+    assert cg.encode_image(b'test') == b'dGVzdA=='
+
+
+def test_parse_data():
+    """Test parsing of raw face data, and generation of matched_faces."""
+    raw_concepts = MOCK_RESPONSE['outputs'][0]['data']['concepts']
+    assert cg.parse_concepts(raw_concepts) == PARSED_CONCEPTS
+
+
+def test_valid_api_key(mock_app):
+    """Test that the api key is validated."""
+    cg.validate_api_key(MOCK_API_KEY)
+    mock_app.assert_called_with(api_key=MOCK_API_KEY)
+
+
+def test_invalid_api_key(caplog, mock_app):
+    """Test that an invalid api key is caught."""
+    with patch('clarifai.rest.ClarifaiApp', side_effect=Exception):
+        cg.validate_api_key(MOCK_API_KEY)
+        assert "Clarifai error: API Key not found" in caplog.text
+
+
+async def test_setup_platform(hass, mock_app, mock_image):
+    """Set up platform with one entity."""
+    await async_setup_component(hass, ip.DOMAIN, VALID_CONFIG)
+    assert hass.states.get(VALID_ENTITY_ID)
+
+
+async def test_process_image(hass, mock_app, mock_image, mock_response):
+    """Test successful processing of an image."""
+    await async_setup_component(hass, ip.DOMAIN, VALID_CONFIG)
+    assert hass.states.get(VALID_ENTITY_ID)
+
+    events = []
+
+    @callback
+    def mock_event(event):
+        """Mock event."""
+        events.append(event)
+
+    hass.bus.async_listen('image_processing.model_prediction', mock_event)
+    data = {ATTR_ENTITY_ID: VALID_ENTITY_ID}
+    await hass.services.async_call(ip.DOMAIN,
+                                   ip.SERVICE_SCAN,
+                                   service_data=data)
+    await hass.async_block_till_done()
+
+
+async def test_setup_platform_with_name(hass, mock_app):
+    """Set up platform with one entity and a name."""
+    named_entity_id = 'image_processing.{}'.format(MOCK_NAME)
+
+    valid_config_named = VALID_CONFIG.copy()
+    valid_config_named[ip.DOMAIN][ip.CONF_SOURCE][ip.CONF_NAME] = MOCK_NAME
+
+    await async_setup_component(hass, ip.DOMAIN, valid_config_named)
+    assert hass.states.get(named_entity_id)
+    state = hass.states.get(named_entity_id)
+    assert state.attributes.get(CONF_FRIENDLY_NAME) == MOCK_NAME
+
