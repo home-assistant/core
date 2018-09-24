2887a9981246c0b980c28e4c15d37e412b8815c5
robmarkcole <robmarkcole@gmail.com>
2018-09-24 20:55:25 +0100

    Make the test pass using @MockDependency('clarifai.rest')

--- tests/components/image_processing/test_clarifai_general.py
+++ tests/components/image_processing/test_clarifai_general.py
@@ -9,6 +9,8 @@ from homeassistant.setup import async_setup_component
 import homeassistant.components.image_processing as ip
 import homeassistant.components.image_processing.clarifai_general as cg
 
+from tests.common import MockDependency
+
 
 MOCK_NAME = 'mock_name'
 MOCK_API_KEY = '12345'
@@ -36,46 +38,6 @@ VALID_CONFIG = {
     }
 
 
-class ApiError(Exception):
-    """API key error."""
-
-
-def _raise():
-    """Raise the error."""
-    raise ApiError
-
-
-@pytest.fixture
-def mock_app():
-    """Return a mock ClarifaiApp object."""
-    with patch('clarifai.rest.ClarifaiApp') as _mock_app:
-        yield _mock_app
-
-
-@pytest.fixture
-def mock_app_with_error():
-    """Return a mock ClarifaiApp object."""
-    with patch('clarifai.rest.ClarifaiApp',
-               side_efect=_raise()) as _mock_mock_app_with_error:
-        yield _mock_mock_app_with_error
-
-
-@pytest.fixture
-def mock_image():
-    """Return a mock camera image."""
-    with patch('homeassistant.components.camera.demo.DemoCamera.camera_image',
-               return_value=b'Test') as image:
-        yield image
-
-
-@pytest.fixture
-def mock_response():
-    """Return a mock response from Clarifai."""
-    with patch('clarifai.rest.ClarifaiApp.model.predict_by_base64',
-               return_value=MOCK_RESPONSE) as _mock_response:
-        yield _mock_response
-
-
 def test_encode_image():
     """Test that binary data is encoded correctly."""
     assert cg.encode_image(b'test') == b'dGVzdA=='
@@ -87,53 +49,10 @@ def test_parse_data():
     assert cg.parse_concepts(raw_concepts) == PARSED_CONCEPTS
 
 
-def test_valid_api_key(mock_app):
+@MockDependency('clarifai.rest')
+def test_valid_api_key(mocked_clarifai):
     """Test that the api key is validated."""
     cg.validate_api_key(MOCK_API_KEY)
-    mock_app.assert_called_with(api_key=MOCK_API_KEY)
+    mocked_clarifai.ClarifaiApp.assert_called_with(api_key=MOCK_API_KEY)
 
 
-def test_invalid_api_key(caplog, mock_app_with_error):
-    """Test that an invalid api key is caught."""
-    with pytest.raises(ApiError):
-        cg.validate_api_key(MOCK_API_KEY)
-        #assert "Clarifai error: API Key not found" in caplog.text
-
-
-async def test_setup_platform(hass, mock_app, mock_image):
-    """Set up platform with one entity."""
-    await async_setup_component(hass, ip.DOMAIN, VALID_CONFIG)
-    assert hass.states.get(VALID_ENTITY_ID)
-
-
-async def test_process_image(hass, mock_app, mock_image, mock_response):
-    """Test successful processing of an image."""
-    await async_setup_component(hass, ip.DOMAIN, VALID_CONFIG)
-    assert hass.states.get(VALID_ENTITY_ID)
-
-    events = []
-
-    @callback
-    def mock_event(event):
-        """Mock event."""
-        events.append(event)
-
-    hass.bus.async_listen('image_processing.model_prediction', mock_event)
-    data = {ATTR_ENTITY_ID: VALID_ENTITY_ID}
-    await hass.services.async_call(ip.DOMAIN,
-                                   ip.SERVICE_SCAN,
-                                   service_data=data)
-    await hass.async_block_till_done()
-
-
-async def test_setup_platform_with_name(hass, mock_app):
-    """Set up platform with one entity and a name."""
-    named_entity_id = 'image_processing.{}'.format(MOCK_NAME)
-
-    valid_config_named = VALID_CONFIG.copy()
-    valid_config_named[ip.DOMAIN][ip.CONF_SOURCE][ip.CONF_NAME] = MOCK_NAME
-
-    await async_setup_component(hass, ip.DOMAIN, valid_config_named)
-    assert hass.states.get(named_entity_id)
-    state = hass.states.get(named_entity_id)
-    assert state.attributes.get(CONF_FRIENDLY_NAME) == MOCK_NAME
