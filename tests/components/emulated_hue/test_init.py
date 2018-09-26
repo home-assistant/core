3d9599fb464a763b513990d397a6c845407eafda
Anders Melchiorsen <amelchio@nogoto.net>
2018-09-27 00:21:29 +0200

    Remove unused import (#16909)
    * Remove unused import
    
    * Revert test usage

--- tests/components/emulated_hue/test_init.py
+++ tests/components/emulated_hue/test_init.py
@@ -16,25 +16,24 @@ def test_config_google_home_entity_id_to_number():
     handle = mop()
 
     with patch('homeassistant.util.json.open', mop, create=True):
-        with patch('homeassistant.util.json.os.open', return_value=0):
-            number = conf.entity_id_to_number('light.test')
-            assert number == '2'
-            assert handle.write.call_count == 1
-            assert json.loads(handle.write.mock_calls[0][1][0]) == {
-                '1': 'light.test2',
-                '2': 'light.test',
-            }
+        number = conf.entity_id_to_number('light.test')
+        assert number == '2'
+        assert handle.write.call_count == 1
+        assert json.loads(handle.write.mock_calls[0][1][0]) == {
+            '1': 'light.test2',
+            '2': 'light.test',
+        }
 
-            number = conf.entity_id_to_number('light.test')
-            assert number == '2'
-            assert handle.write.call_count == 1
+        number = conf.entity_id_to_number('light.test')
+        assert number == '2'
+        assert handle.write.call_count == 1
 
-            number = conf.entity_id_to_number('light.test2')
-            assert number == '1'
-            assert handle.write.call_count == 1
+        number = conf.entity_id_to_number('light.test2')
+        assert number == '1'
+        assert handle.write.call_count == 1
 
-            entity_id = conf.number_to_entity_id('1')
-            assert entity_id == 'light.test2'
+        entity_id = conf.number_to_entity_id('1')
+        assert entity_id == 'light.test2'
 
 
 def test_config_google_home_entity_id_to_number_altered():
@@ -47,25 +46,24 @@ def test_config_google_home_entity_id_to_number_altered():
     handle = mop()
 
     with patch('homeassistant.util.json.open', mop, create=True):
-        with patch('homeassistant.util.json.os.open', return_value=0):
-            number = conf.entity_id_to_number('light.test')
-            assert number == '22'
-            assert handle.write.call_count == 1
-            assert json.loads(handle.write.mock_calls[0][1][0]) == {
-                '21': 'light.test2',
-                '22': 'light.test',
-            }
+        number = conf.entity_id_to_number('light.test')
+        assert number == '22'
+        assert handle.write.call_count == 1
+        assert json.loads(handle.write.mock_calls[0][1][0]) == {
+            '21': 'light.test2',
+            '22': 'light.test',
+        }
 
-            number = conf.entity_id_to_number('light.test')
-            assert number == '22'
-            assert handle.write.call_count == 1
+        number = conf.entity_id_to_number('light.test')
+        assert number == '22'
+        assert handle.write.call_count == 1
 
-            number = conf.entity_id_to_number('light.test2')
-            assert number == '21'
-            assert handle.write.call_count == 1
+        number = conf.entity_id_to_number('light.test2')
+        assert number == '21'
+        assert handle.write.call_count == 1
 
-            entity_id = conf.number_to_entity_id('21')
-            assert entity_id == 'light.test2'
+        entity_id = conf.number_to_entity_id('21')
+        assert entity_id == 'light.test2'
 
 
 def test_config_google_home_entity_id_to_number_empty():
@@ -78,24 +76,23 @@ def test_config_google_home_entity_id_to_number_empty():
     handle = mop()
 
     with patch('homeassistant.util.json.open', mop, create=True):
-        with patch('homeassistant.util.json.os.open', return_value=0):
-            number = conf.entity_id_to_number('light.test')
-            assert number == '1'
-            assert handle.write.call_count == 1
-            assert json.loads(handle.write.mock_calls[0][1][0]) == {
-                '1': 'light.test',
-            }
+        number = conf.entity_id_to_number('light.test')
+        assert number == '1'
+        assert handle.write.call_count == 1
+        assert json.loads(handle.write.mock_calls[0][1][0]) == {
+            '1': 'light.test',
+        }
 
-            number = conf.entity_id_to_number('light.test')
-            assert number == '1'
-            assert handle.write.call_count == 1
+        number = conf.entity_id_to_number('light.test')
+        assert number == '1'
+        assert handle.write.call_count == 1
 
-            number = conf.entity_id_to_number('light.test2')
-            assert number == '2'
-            assert handle.write.call_count == 2
+        number = conf.entity_id_to_number('light.test2')
+        assert number == '2'
+        assert handle.write.call_count == 2
 
-            entity_id = conf.number_to_entity_id('2')
-            assert entity_id == 'light.test2'
+        entity_id = conf.number_to_entity_id('2')
+        assert entity_id == 'light.test2'
 
 
 def test_config_alexa_entity_id_to_number():
