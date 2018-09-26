472eb3fb69c896f670e54d9b3f0f35e8f62613db
Paulus Schoutsen <paulus@home-assistant.io>
2018-09-26 18:03:25 +0200

    Prevent discovered Tradfri while already configured (#16891)
    * Prevent discovered Tradfri while already configured
    
    * Lint

--- homeassistant/components/tradfri/config_flow.py
+++ homeassistant/components/tradfri/config_flow.py
@@ -77,6 +77,12 @@ class FlowHandler(config_entries.ConfigFlow):
 
     async def async_step_discovery(self, user_input):
         """Handle discovery."""
+        for entry in self._async_current_entries():
+            if entry.data[CONF_HOST] == user_input['host']:
+                return self.async_abort(
+                    reason='already_configured'
+                )
+
         self._host = user_input['host']
         return await self.async_step_auth()
 
