7a78501baee828f8338960909bebfe176209d797
cdce8p <30130371+cdce8p@users.noreply.github.com>
2018-09-27 16:50:31 +0200

    Fix config/script hook

--- homeassistant/components/config/script.py
+++ homeassistant/components/config/script.py
@@ -2,30 +2,23 @@
 import asyncio
 
 from homeassistant.components.config import EditKeyBasedConfigView
-from homeassistant.components.script import (
-    DOMAIN, SCRIPT_ENTRY_SCHEMA)
+from homeassistant.components.script import DOMAIN, SCRIPT_ENTRY_SCHEMA
 from homeassistant.const import SERVICE_RELOAD
 import homeassistant.helpers.config_validation as cv
-from homeassistant.loader import bind_hass
 
 
 CONFIG_PATH = 'scripts.yaml'
 
 
-@bind_hass
-def async_reload(hass):
-    """Reload the scripts from config.
-
-    Returns a coroutine object.
-    """
-    return hass.services.async_call(DOMAIN, SERVICE_RELOAD)
-
-
 @asyncio.coroutine
 def async_setup(hass):
     """Set up the script config API."""
+    async def hook(hass):
+        """post_write_hook for Config View that reloads scripts."""
+        await hass.services.async_call(DOMAIN, SERVICE_RELOAD)
+
     hass.http.register_view(EditKeyBasedConfigView(
         'script', 'config', CONFIG_PATH, cv.slug, SCRIPT_ENTRY_SCHEMA,
-        post_write_hook=async_reload
+        post_write_hook=hook
     ))
     return True
