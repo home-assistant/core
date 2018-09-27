7b33ca2bb7fe27b0732ca37304e8433cf5683043
Martin Mois <martin.mois@gmail.com>
2018-09-27 23:06:03 +0200

    Add notify.homematic_signalgen

---
+++ homeassistant/components/notify/homematic_signalgen.py
@@ -0,0 +1,72 @@
+"""
+Support for the signal generator from Homematic (HM-OU-CFM-TW).
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/notify.homematic_signalgen/
+"""
+import logging
+
+import voluptuous as vol
+
+from homeassistant.const import (CONF_ADDRESS)
+from homeassistant.components.notify import (
+    BaseNotificationService, PLATFORM_SCHEMA, ATTR_DATA)
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.homematic import (DOMAIN, SERVICE_SET_DEVICE_VALUE,
+    ATTR_ADDRESS, ATTR_CHANNEL, ATTR_PARAM, ATTR_VALUE)
+import homeassistant.helpers.template as template
+
+_LOGGER = logging.getLogger(__name__)
+DEPENDENCIES = ["homematic"]
+
+CONF_VALUE = 'value'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_ADDRESS): cv.string,
+    vol.Optional(CONF_VALUE): cv.string,
+})
+
+
+def get_service(hass, config, discovery_info=None):
+    """Get the Homematic Signal Generator notification service."""
+    address = config[CONF_ADDRESS]
+    value = None
+    if CONF_VALUE in config:
+        value = config[CONF_VALUE]
+
+    return HomematicSignalGeneratorNotificationService(hass, address, value)
+
+
+class HomematicSignalGeneratorNotificationService(BaseNotificationService):
+    """Implement the notification service for the Command Line service."""
+
+    def __init__(self, hass, address, value):
+        """Initialize the service."""
+        self.hass = hass
+        self.address = address
+        self.value = value
+
+    def send_message(self, message="", **kwargs):
+        """Send a notification to the signal generator."""
+        valueForSigGen = self.value
+
+        dataFromMsg = kwargs.get(ATTR_DATA)
+        if dataFromMsg is not None:
+            if 'value' in dataFromMsg:
+                valueForSigGen = dataFromMsg['value']
+
+        templ = template.Template(valueForSigGen, self.hass)
+        valueForSigGen = template.render_complex(templ, None)
+
+        if valueForSigGen is None:
+            _LOGGER.error("Value is null: Not invoking homematic signal generator.")
+
+        data = {
+            ATTR_ADDRESS: self.address,
+            ATTR_CHANNEL: 2,
+            ATTR_PARAM: "SUBMIT",
+            ATTR_VALUE: valueForSigGen
+        }
+        _LOGGER.debug('Calling service: domain=' + DOMAIN + ';service=' + SERVICE_SET_DEVICE_VALUE +
+                      ';data=' + str(data))
+        self.hass.services.call(DOMAIN, SERVICE_SET_DEVICE_VALUE, data)
---
+++ tests/components/notify/test_homematic_signalgen.py
@@ -0,0 +1,74 @@
+"""The tests for the Homematic Signal Generator notification platform."""
+
+import unittest
+
+from homeassistant.setup import setup_component
+import homeassistant.components.notify as notify
+from tests.common import assert_setup_component, get_test_home_assistant
+import homeassistant.components.homematic
+
+
+class TestHomematicSignalGen(unittest.TestCase):
+    """Test the homematic_signalgen notifications."""
+
+    def setUp(self):  # pylint: disable=invalid-name
+        """Set up things to be run when tests are started."""
+        self.hass = get_test_home_assistant()
+
+    def tearDown(self):  # pylint: disable=invalid-name
+        """Stop down everything that was started."""
+        self.hass.stop()
+
+    def test_setup_full(self):
+        """Test valid configuration"""
+        setup_component(self.hass, 'homematic', {
+            'homematic': {
+                'hosts': {
+                    'ccu2': {
+                        'host': '127.0.0.1'
+                    }
+                }
+            }
+        })
+        with assert_setup_component(1) as handle_config:
+            assert setup_component(self.hass, 'notify', {
+                'notify': {
+                    'name': 'test',
+                    'platform': 'homematic_signalgen',
+                    'address': 'NEQXXXXXXX',
+                    'value': '1,1,108000,2'}
+            })
+        assert handle_config[notify.DOMAIN]
+
+    def test_setup_without_optional(self):
+        """Test valid configuration"""
+        setup_component(self.hass, 'homematic', {
+            'homematic': {
+                'hosts': {
+                    'ccu2': {
+                        'host': '127.0.0.1'
+                    }
+                }
+            }
+        })
+        with assert_setup_component(1) as handle_config:
+            assert setup_component(self.hass, 'notify', {
+                'notify': {
+                    'name': 'test',
+                    'platform': 'homematic_signalgen',
+                    'address': 'NEQXXXXXXX'}
+            })
+        assert handle_config[notify.DOMAIN]
+
+    def test_bad_config(self):
+        """Test invalid configuration"""
+        config = {
+            notify.DOMAIN: {
+                'name': 'test',
+                'platform': 'homematic_signalgen',
+            }
+        }
+        with assert_setup_component(0) as handle_config:
+            assert setup_component(self.hass, notify.DOMAIN, config)
+        assert not handle_config[notify.DOMAIN]
+
