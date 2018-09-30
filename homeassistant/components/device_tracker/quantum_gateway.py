d1c65c672291b2cbcaedc6b4b40a9f8f7294ac04
Colby Rome <cdr013@bucknell.edu>
2018-09-30 18:22:33 -0400

    fixed linting errors, added docstrings

--- homeassistant/components/device_tracker/quantum_gateway.py
+++ homeassistant/components/device_tracker/quantum_gateway.py
@@ -1,9 +1,11 @@
+"""
+Support for Verizon FiOS Quantum Gatewaysself.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/device_tracker.quantum_gateway/
+"""
 import logging
 
-import hashlib
-from http.cookies import SimpleCookie
-import json
-import requests
 import voluptuous as vol
 
 from homeassistant.components.device_tracker import (DOMAIN, PLATFORM_SCHEMA,
@@ -23,29 +25,35 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def get_scanner(hass, config):
+    """Validate the configuration and return a Quantum Gateway scanner."""
     scanner = QuantumGatewayDeviceScanner(config[DOMAIN])
 
     return scanner if scanner.success_init else None
 
 
 class QuantumGatewayDeviceScanner(DeviceScanner):
+    """This class queries a Quantum Gateway."""
 
     def __init__(self, config):
+        """Initialize the scanner."""
         from quantum_gateway import QuantumGatewayScanner
-        
+
         self.host = config[CONF_HOST]
         self.password = config[CONF_PASSWORD]
-        _LOGGER.info("Initializing")
+        _LOGGER.debug("Initializing")
 
         self.quantum = QuantumGatewayScanner(self.host, self.password)
 
         self.success_init = self.quantum.success_init
 
         if not self.success_init:
-            _LOGGER.error("Unable to login to gateway. Check password and host.")
+            _LOGGER.error("Unable to login to gateway. Check password and "
+                          "host.")
 
     def scan_devices(self):
+        """Scan for new devices and return a list of found MACs."""
         return self.quantum.scan_devices()
 
     def get_device_name(self, device):
+        """Return the name of the given device or None if we don't know."""
         return self.quantum.get_device_name(device)
