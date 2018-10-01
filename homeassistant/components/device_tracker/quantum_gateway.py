dbbccdd294456c8d0758db3b12db86d7c9b9407a
Colby Rome <cdr013@bucknell.edu>
2018-10-01 19:26:44 -0400

    added error handling to prevent stacktrace

--- homeassistant/components/device_tracker/quantum_gateway.py
+++ homeassistant/components/device_tracker/quantum_gateway.py
@@ -13,7 +13,7 @@ from homeassistant.components.device_tracker import (DOMAIN, PLATFORM_SCHEMA,
 from homeassistant.const import (CONF_HOST, CONF_PASSWORD)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['quantum-gateway==0.0.2']
+REQUIREMENTS = ['quantum-gateway==0.0.3']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -42,9 +42,13 @@ class QuantumGatewayDeviceScanner(DeviceScanner):
         self.password = config[CONF_PASSWORD]
         _LOGGER.debug('Initializing')
 
-        self.quantum = QuantumGatewayScanner(self.host, self.password)
+        try:
+            self.quantum = QuantumGatewayScanner(self.host, self.password)
+            self.success_init = self.quantum.success_init
+        except:
+            self.success_init = False
+            _LOGGER.error("Unable to connect to gateway. Check host.")
 
-        self.success_init = self.quantum.success_init
 
         if not self.success_init:
             _LOGGER.error("Unable to login to gateway. Check password and "
@@ -52,7 +56,13 @@ class QuantumGatewayDeviceScanner(DeviceScanner):
 
     def scan_devices(self):
         """Scan for new devices and return a list of found MACs."""
-        return self.quantum.scan_devices()
+        connected_devices = []
+        try:
+            connected_devices = self.quantum.scan_devices()
+        except:
+            _LOGGER.error("Unable to scan devices. Check connection to router")
+        return connected_devices
+
 
     def get_device_name(self, device):
         """Return the name of the given device or None if we don't know."""
