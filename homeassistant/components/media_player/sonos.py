e29a6ab0412364d7e43e6372d435c23c03fcc46c
Anders Melchiorsen <amelchio@nogoto.net>
2018-10-01 17:58:04 +0200

    Remove error logging when Sonos shuffle_set is not available (#16921)
    Error on set_shuffle with UPnP Error 712 received: Play mode not supported from 10.23.2.16

--- homeassistant/components/media_player/sonos.py
+++ homeassistant/components/media_player/sonos.py
@@ -69,7 +69,7 @@ ATTR_SPEECH_ENHANCE = 'speech_enhance'
 
 ATTR_SONOS_GROUP = 'sonos_group'
 
-UPNP_ERRORS_TO_IGNORE = ['701', '711']
+UPNP_ERRORS_TO_IGNORE = ['701', '711', '712']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_ADVERTISE_ADDR): cv.string,
@@ -831,7 +831,7 @@ class SonosDevice(MediaPlayerDevice):
         """Set volume level, range 0..1."""
         self.soco.volume = str(int(volume * 100))
 
-    @soco_error()
+    @soco_error(UPNP_ERRORS_TO_IGNORE)
     @soco_coordinator
     def set_shuffle(self, shuffle):
         """Enable/Disable shuffle mode."""
