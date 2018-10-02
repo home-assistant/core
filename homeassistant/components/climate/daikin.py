6a192ba739190f7697e70c28c3cf413c7f0f18f8
MatteGary <MatteGary@users.noreply.github.com>
2018-10-02 12:39:49 +0200

    Whitespace fix

--- homeassistant/components/climate/daikin.py
+++ homeassistant/components/climate/daikin.py
@@ -143,8 +143,9 @@ class DaikinClimate(ClimateDevice):
         elif key == ATTR_OPERATION_MODE:
             # Daikin can return also internal states auto-1 or auto-7
             # and we need to translate them as AUTO
-            daikin_mode = re.sub('[^a-z]', '',
-               self._api.device.represent(daikin_attr)[1])
+            daikin_mode = re.sub(
+                '[^a-z]', '',
+                self._api.device.represent(daikin_attr)[1])
             ha_mode = DAIKIN_TO_HA_STATE.get(daikin_mode)
             value = ha_mode
 
