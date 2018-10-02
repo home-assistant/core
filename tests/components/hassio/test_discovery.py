bfb92aa439781afa989403ffda08b6f2f3e9a2c8
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-10-02 15:27:31 +0200

    Update test_discovery.py

--- tests/components/hassio/test_discovery.py
+++ tests/components/hassio/test_discovery.py
@@ -2,9 +2,8 @@
 from unittest.mock import patch
 
 
-patch('homeassistant.components.mqtt.config_flow.async_step_hassio')
-async def test_hassio_discovery_startup(hass, hassio_client, 
-                                        aioclient_mock, mock_mqtt):
+async def test_hassio_discovery_startup(hass, hassio_client,
+                                        aioclient_mock):
     """Test startup and discovery after event."""
     aioclient_mock.get(
         "http://127.0.0.1/discovery", json={
@@ -26,11 +25,13 @@ async def test_hassio_discovery_startup(hass, hassio_client,
             'result': 'ok', 'data': {'name': "Mosquitto Test"}
         })
 
-    await hass.async_start()
+    with patch('homeassistant.components.mqtt.'
+               'config_flow.async_step_hassio') as mock_mqtt:
+        await hass.async_start()
 
-    assert aioclient_mock.call_count == 5
-    assert mock_mqtt.called
-    assert mock_mqtt.assert_called_with(input={
-        'broker': 'mock-broker', 'port': 1883, 'username': 'mock-user',
-        'password': 'mock-pass', 'protocol': '3.1.1'
-    })
+        assert aioclient_mock.call_count == 5
+        assert mock_mqtt.called
+        assert mock_mqtt.assert_called_with(input={
+            'broker': 'mock-broker', 'port': 1883, 'username': 'mock-user',
+            'password': 'mock-pass', 'protocol': '3.1.1'
+        })
