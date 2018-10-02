ae9c3070d799354f20cb74b739236b6ba66b3d50
Malte Franken <exxamalte@users.noreply.github.com>
2018-10-03 01:41:17 +0930

    simplified feed update code

--- homeassistant/components/geo_location/nsw_rural_fire_service_feed.py
+++ homeassistant/components/geo_location/nsw_rural_fire_service_feed.py
@@ -124,18 +124,17 @@ class NswRuralFireServiceFeedManager:
         managed_entities = self._managed_entities.copy()
         entries_lookup = {entry.external_id: entry for entry in feed_entries}
         for entity in managed_entities:
-            if entity.external_id in entries_lookup:
-                entry = entries_lookup.get(entity.external_id)
-                # Existing entity - update details.
-                _LOGGER.debug("Existing entity found %s", entity)
-                feed_entries.remove(entry)
-                del entries_lookup[entity.external_id]
-                entity.schedule_update_ha_state(True)
-            else:
+            entry = entries_lookup.pop(entity.external_id, None)
+            if entry is None:
                 # Remove obsolete entity.
                 _LOGGER.debug("Entity not current anymore %s", entity)
                 self._managed_entities.remove(entity)
                 self._hass.add_job(entity.async_remove())
+                continue
+            # Existing entity - update details.
+            _LOGGER.debug("Existing entity found %s", entity)
+            feed_entries.remove(entry)
+            entity.schedule_update_ha_state(True)
         # Return the remaining entries that new entities must be created for.
         return feed_entries
 
