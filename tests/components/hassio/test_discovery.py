e0db534978cf01cd5fc51e4455117f8054d5e31e
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-10-02 14:09:46 +0200

    Update test_discovery.py

--- tests/components/hassio/test_discovery.py
+++ tests/components/hassio/test_discovery.py
@@ -6,14 +6,14 @@ import pytest
 
 patch('homeassistant.components.mqtt.config_flow.async_step_hassio')
 async def test_hassio_discovery_startup(hass, hassio_client, 
-                                        aiohttp_client, mock_mqtt):
+                                        aioclient_mock, mock_mqtt):
     """Test startup and discovery after event."""
     aioclient_mock.get(
         "http://127.0.0.1/discovery", json={
             'result': 'ok', 'data': {'discovery': [
                 {
                     "service": "mqtt", "uuid": "test",
-                    "addon", "mosquitto", "config": 
+                    "addon", "mosquitto", "config":
                     {
                         'broker': 'mock-broker',
                         'port': 1883,
@@ -27,6 +27,12 @@ async def test_hassio_discovery_startup(hass, hassio_client,
         "http://127.0.0.1/addons/mosquitto/info", json={
             'result': 'ok', 'data': {'name': "Mosquitto Test"}
         })
-    
+
     await hass.async_start()
-    
+
+    assert aioclient_mock.call_count == 5
+    assert mock_mqtt.called
+    assert mock_mqtt.assert_called_with(input={
+        'broker': 'mock-broker', 'port': 1883, 'username': 'mock-user',
+        'password': 'mock-pass', 'protocol': '3.1.1'
+    })
