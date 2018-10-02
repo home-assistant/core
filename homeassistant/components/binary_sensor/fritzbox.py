a3ed0906ba5e13758e9654aac898738b46df10ef
Heiko Thiery <heiko.thiery@gmail.com>
2018-10-02 20:57:03 +0200

    add blank line to keep imports ordered
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- homeassistant/components/binary_sensor/fritzbox.py
+++ homeassistant/components/binary_sensor/fritzbox.py
@@ -5,6 +5,7 @@ For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/binary_sensor.fritzbox/
 """
 import logging
+
 import requests
 
 from homeassistant.components.binary_sensor import BinarySensorDevice
