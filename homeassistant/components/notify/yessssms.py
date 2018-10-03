7b6c8fa05c748417aebe847d9fb244d4cc1d0116
Florian Klien <flowolf@klienux.org>
2018-10-03 19:04:00 +0200

    fixing tests, new upstream version 0.2.3
    fixing tests based on requested changes, coverage

--- homeassistant/components/notify/yessssms.py
+++ homeassistant/components/notify/yessssms.py
@@ -14,7 +14,7 @@ from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_RECIPIENT
 import homeassistant.helpers.config_validation as cv
 
 
-REQUIREMENTS = ['YesssSMS==0.2.1']
+REQUIREMENTS = ['YesssSMS==0.2.3']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -39,24 +39,24 @@ class YesssSMSNotificationService(BaseNotificationService):
         from YesssSMS import YesssSMS
         self.yesss = YesssSMS(username, password)
         self._recipient = recipient
-        # pylint: disable=protected-access
         _LOGGER.debug(
-            "initialized; library version: %s", self.yesss._version)
+            "initialized; library version: %s", self.yesss.version())
 
     def send_message(self, message="", **kwargs):
         """Send a SMS message via Yesss.at's website."""
         if self.yesss.account_is_suspended():
             # only retry to login after HASS was restarted with (hopefully)
             # new login data.
-            _LOGGER.critical(
+            _LOGGER.error(
                 "Account is suspended, cannot send SMS. "
-                "Check your login data!")
+                "Check your login data and restart Home Assistant")
             return
         try:
             self.yesss.send(self._recipient, message)
         except self.yesss.NoRecipientError as ex:
             _LOGGER.error(
-                "You need to provide a recipient for SMS notification: %s", ex)
+                "You need to provide a recipient for SMS notification.",
+                exc_info=ex)
         except self.yesss.EmptyMessageError as ex:
             _LOGGER.error(
                 "Cannot send empty SMS message: %s", ex)
@@ -66,7 +66,9 @@ class YesssSMSNotificationService(BaseNotificationService):
             _LOGGER.error(
                 "YesssSMS: unable to connect to yesss.at server: %s", ex)
         except self.yesss.AccountSuspendedError as ex:
-            _LOGGER.critical("Really wrong login credentials!! %s", ex)
+            _LOGGER.error("Wrong login credentials!! "
+                          "Verify correct credentials and restart "
+                          "Home Assistant: %s", ex)
         except self.yesss.LoginError as ex:
             _LOGGER.error("Wrong login credentials: %s", ex)
         else:
