4a63024ae80f2fb79c9f0056d1ef2091c7a32cd2
Daniel Høyer Iversen <Daniel.Iversen@tibber.com>
2018-10-04 11:16:31 +0200

    Update xiaomi lib

--- homeassistant/components/xiaomi_aqara.py
+++ homeassistant/components/xiaomi_aqara.py
@@ -22,7 +22,7 @@ from homeassistant.helpers.event import async_track_point_in_utc_time
 from homeassistant.util.dt import utcnow
 from homeassistant.util import slugify
 
-REQUIREMENTS = ['PyXiaomiGateway==0.10.0']
+REQUIREMENTS = ['PyXiaomiGateway==0.11.0']
 
 _LOGGER = logging.getLogger(__name__)
 
