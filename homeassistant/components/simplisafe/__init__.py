3820b5a13627c011ef5db69812b0cd48e43dfb1d
Aaron Bach <bachya1208@gmail.com>
2018-10-03 21:22:52 -0600

    More file placement

--- homeassistant/components/simplisafe/__init__.py
+++ homeassistant/components/simplisafe/__init__.py
@@ -9,6 +9,7 @@ import re
 
 import voluptuous as vol
 
+from homeassistant.config_entries import SOURCE_IMPORT
 from homeassistant.components.alarm_control_panel import (
     PLATFORM_SCHEMA, AlarmControlPanel)
 from homeassistant.const import (
@@ -17,13 +18,17 @@ from homeassistant.const import (
 from homeassistant.helpers import aiohttp_client, config_validation as cv
 from homeassistant.util.json import load_json, save_json
 
-REQUIREMENTS = ['simplisafe-python==3.1.2']
+from .config_flow import configured_instances
+from .const import DATA_SIMPLISAFE_CLIENT, DATA_SIMPLISAFE_LISTENER, DOMAIN
+
+REQUIREMENTS = ['simplisafe-python==3.1.1']
 
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_ALARM_ACTIVE = "alarm_active"
 ATTR_TEMPERATURE = "temperature"
 
+DATA_SIMPLISAFE_LISTENER = 'data_simpli'
 DATA_FILE = '.simplisafe'
 
 DEFAULT_NAME = 'SimpliSafe'
@@ -36,47 +41,118 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-async def async_setup_platform(
-        hass, config, async_add_entities, discovery_info=None):
-    """Set up the SimpliSafe platform."""
-    from simplipy import API
-    from simplipy.errors import SimplipyError
+async def async_setup(hass, config):
+    """Set up the SimpliSafe component."""
+    hass.data[DOMAIN] = {}
+    hass.data[DOMAIN][DATA_SIMPLISAFE_CLIENT] = {}
+    hass.data[DOMAIN][DATA_SIMPLISAFE_LISTENER] = {}
 
-    username = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
-    name = config.get(CONF_NAME)
-    code = config.get(CONF_CODE)
+    if DOMAIN not in config:
+        return True
 
-    websession = aiohttp_client.async_get_clientsession(hass)
+    conf = config[DOMAIN]
 
-    config_data = await hass.async_add_executor_job(
-        load_json, hass.config.path(DATA_FILE))
+    if conf[CONF_USERNAME] not in configured_instances(hass):
+        hass.async_add_job(
+            hass.config_entries.flow.async_init(
+                DOMAIN,
+                context={'source': SOURCE_IMPORT},
+                data={
+                    CONF_USERNAME: conf[CONF_USERNAME],
+                    CONF_PASSWORD: conf[CONF_PASSWORD],
+                    CONF_NAME: conf[CONF_NAME],
+                    CONF_CODE: conf[CONF_CODE],
+                }))
 
-    try:
-        if config_data:
-            try:
-                simplisafe = await API.login_via_token(
-                    config_data['refresh_token'], websession)
-                _LOGGER.debug('Logging in with refresh token')
-            except SimplipyError:
-                _LOGGER.info('Refresh token expired; attempting credentials')
-                simplisafe = await API.login_via_credentials(
-                    username, password, websession)
-        else:
-            simplisafe = await API.login_via_credentials(
-                username, password, websession)
-            _LOGGER.debug('Logging in with credentials')
-    except SimplipyError as err:
-        _LOGGER.error("There was an error during setup: %s", err)
-        return
+    return True
 
-    config_data = {'refresh_token': simplisafe.refresh_token}
-    await hass.async_add_executor_job(
-        save_json, hass.config.path(DATA_FILE), config_data)
 
-    systems = await simplisafe.get_systems()
-    async_add_entities(
-        [SimpliSafeAlarm(system, name, code) for system in systems], True)
+async def async_setup_entry(hass, config_entry):
+    """Set up OpenUV as config entry."""
+    # from simplipy import API
+    # from simplipy.errors import SimplipyError
+
+    # try:
+    #     websession = aiohttp_client.async_get_clientsession(hass)
+    #     openuv = OpenUV(
+    #         Client(
+    #             config_entry.data[CONF_API_KEY],
+    #             config_entry.data.get(CONF_LATITUDE, hass.config.latitude),
+    #             config_entry.data.get(CONF_LONGITUDE, hass.config.longitude),
+    #             websession,
+    #             altitude=config_entry.data.get(
+    #                 CONF_ELEVATION, hass.config.elevation)),
+    #         config_entry.data.get(CONF_BINARY_SENSORS, {}).get(
+    #             CONF_MONITORED_CONDITIONS, list(BINARY_SENSORS)),
+    #         config_entry.data.get(CONF_SENSORS, {}).get(
+    #             CONF_MONITORED_CONDITIONS, list(SENSORS)))
+    #     await openuv.async_update()
+    #     hass.data[DOMAIN][DATA_OPENUV_CLIENT][config_entry.entry_id] = openuv
+    # except OpenUvError as err:
+    #     _LOGGER.error('An error occurred: %s', str(err))
+    #     hass.components.persistent_notification.create(
+    #         'Error: {0}<br />'
+    #         'You will need to restart hass after fixing.'
+    #         ''.format(err),
+    #         title=NOTIFICATION_TITLE,
+    #         notification_id=NOTIFICATION_ID)
+    #     return False
+
+    for component in ('alarm_control_panel'):
+        hass.async_create_task(hass.config_entries.async_forward_entry_setup(
+            config_entry, component))
+
+    async def refresh_sensors(event_time):
+        """Refresh OpenUV data."""
+        _LOGGER.debug('Refreshing OpenUV data')
+        await openuv.async_update()
+        async_dispatcher_send(hass, TOPIC_UPDATE)
+
+    hass.data[DOMAIN][DATA_OPENUV_LISTENER][
+        config_entry.entry_id] = async_track_time_interval(
+            hass, refresh_sensors,
+            hass.data[DOMAIN].get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL))
+
+    return True
+# async def async_setup_platform(
+#         hass, config, async_add_entities, discovery_info=None):
+#     """Set up the SimpliSafe platform."""
+
+#     username = config[CONF_USERNAME]
+#     password = config[CONF_PASSWORD]
+#     name = config.get(CONF_NAME)
+#     code = config.get(CONF_CODE)
+
+#     websession = aiohttp_client.async_get_clientsession(hass)
+
+#     config_data = await hass.async_add_executor_job(
+#         load_json, hass.config.path(DATA_FILE))
+
+#     try:
+#         if config_data:
+#             try:
+#                 simplisafe = await API.login_via_token(
+#                     config_data['refresh_token'], websession)
+#                 _LOGGER.debug('Logging in with refresh token')
+#             except SimplipyError:
+#                 _LOGGER.info('Refresh token expired; attempting credentials')
+#                 simplisafe = await API.login_via_credentials(
+#                     username, password, websession)
+#         else:
+#             simplisafe = await API.login_via_credentials(
+#                 username, password, websession)
+#             _LOGGER.debug('Logging in with credentials')
+#     except SimplipyError as err:
+#         _LOGGER.error("There was an error during setup: %s", err)
+#         return
+
+#     config_data = {'refresh_token': simplisafe.refresh_token}
+#     await hass.async_add_executor_job(
+#         save_json, hass.config.path(DATA_FILE), config_data)
+
+#     systems = await simplisafe.get_systems()
+#     async_add_entities(
+#         [SimpliSafeAlarm(system, name, code) for system in systems], True)
 
 
 class SimpliSafeAlarm(AlarmControlPanel):
---
+++ homeassistant/components/simplisafe/const.py
@@ -0,0 +1,5 @@
+"""Define constants for the SimpliSafe component."""
+
+DOMAIN = 'simplisafe'
+
+DATA_SIMPLISAFE_CLIENT = 'simplisafe_client'
