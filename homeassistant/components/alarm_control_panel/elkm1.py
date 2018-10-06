e882ea9c197fd88bf805b10341cd38a4ff3058db
Glenn Waters <gwwaters@gmail.com>
2018-10-06 00:12:17 -0400

    Address PR comments

--- homeassistant/components/alarm_control_panel/elkm1.py
+++ homeassistant/components/alarm_control_panel/elkm1.py
@@ -6,30 +6,21 @@ https://home-assistant.io/components/alarm_control_panel.elkm1/
 """
 
 import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
 import homeassistant.components.alarm_control_panel as alarm
-from homeassistant.const import (ATTR_ENTITY_ID, STATE_ALARM_ARMED_AWAY,
-                                 STATE_ALARM_ARMED_HOME,
-                                 STATE_ALARM_ARMED_NIGHT, STATE_ALARM_ARMING,
-                                 STATE_ALARM_DISARMED, STATE_ALARM_PENDING,
-                                 STATE_ALARM_TRIGGERED, STATE_UNKNOWN)
+from homeassistant.const import (
+    ATTR_ENTITY_ID, STATE_ALARM_ARMED_AWAY, STATE_ALARM_ARMED_HOME,
+    STATE_ALARM_ARMED_NIGHT, STATE_ALARM_ARMING, STATE_ALARM_DISARMED,
+    STATE_ALARM_PENDING, STATE_ALARM_TRIGGERED, STATE_UNKNOWN)
+from homeassistant.components.elkm1 import (
+    DOMAIN as ELK_DOMAIN, create_elk_entities, ElkDeviceBase)
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.dispatcher import (
+    async_dispatcher_connect, async_dispatcher_send)
 
-from homeassistant.components.elkm1 import (DOMAIN, create_elk_devices,
-                                            ElkDeviceBase,
-                                            register_elk_service)
-from elkm1_lib.const import AlarmState, ArmedStatus, ArmLevel, ArmUpState
+DEPENDENCIES = [ELK_DOMAIN]
 
-DEPENDENCIES = [DOMAIN]
-
-STATE_ALARM_ARMED_VACATION = 'armed_vacation'
-STATE_ALARM_ARMED_HOME_INSTANT = 'armed_home_instant'
-STATE_ALARM_ARMED_NIGHT_INSTANT = 'armed_night_instant'
-
-SERVICE_TO_ELK = {
-    'alarm_arm_vacation': 'async_alarm_arm_vacation',
-    'alarm_arm_home_instant': 'async_alarm_arm_home_instant',
-    'alarm_arm_night_instant': 'async_alarm_arm_night_instant',
-}
+SIGNAL_ARM_ENTITY = 'elkm1_arm'
+SIGNAL_DISPLAY_MESSAGE = 'elkm1_display_message'
 
 DISPLAY_MESSAGE_SERVICE_SCHEMA = vol.Schema({
     vol.Optional(ATTR_ENTITY_ID): cv.entity_ids,
@@ -40,55 +31,70 @@ DISPLAY_MESSAGE_SERVICE_SCHEMA = vol.Schema({
     vol.Optional('line2', default=''): cv.string,
 })
 
-ELK_STATE_TO_HASS_STATE = {
-    ArmedStatus.DISARMED.value:               STATE_ALARM_DISARMED,
-    ArmedStatus.ARMED_AWAY.value:             STATE_ALARM_ARMED_AWAY,
-    ArmedStatus.ARMED_STAY.value:             STATE_ALARM_ARMED_HOME,
-    ArmedStatus.ARMED_STAY_INSTANT.value:     STATE_ALARM_ARMED_HOME,
-    ArmedStatus.ARMED_TO_NIGHT.value:         STATE_ALARM_ARMED_NIGHT,
-    ArmedStatus.ARMED_TO_NIGHT_INSTANT.value: STATE_ALARM_ARMED_NIGHT,
-    ArmedStatus.ARMED_TO_VACATION.value:      STATE_ALARM_ARMED_AWAY,
-}
 
-
-# pylint: disable=unused-argument
-async def async_setup_platform(hass, config, async_add_devices,
+async def async_setup_platform(hass, config, async_add_entities,
                                discovery_info):
     """Setup the ElkM1 alarm platform."""
 
-    elk = hass.data[DOMAIN]['elk']
-    devices = create_elk_devices(hass, elk.areas, 'area', ElkArea, [])
-    async_add_devices(devices, True)
+    elk = hass.data[ELK_DOMAIN]['elk']
+    entities = create_elk_entities(hass, elk.areas, 'area', ElkArea, [])
+    async_add_entities(entities, True)
 
-    for service, method in SERVICE_TO_ELK.items():
-        register_elk_service(
-            hass, alarm.DOMAIN, service, alarm.ALARM_SERVICE_SCHEMA, method)
+    def _arm_service(service):
+        async_dispatcher_send(hass, SIGNAL_ARM_ENTITY, service)
 
-    register_elk_service(
-        hass, alarm.DOMAIN, 'alarm_display_message',
-        DISPLAY_MESSAGE_SERVICE_SCHEMA, 'async_alarm_display_message')
+    for service in _arm_services():
+        hass.services.async_register(
+            ELK_DOMAIN, service, _arm_service, alarm.ALARM_SERVICE_SCHEMA)
+
+    def _display_message_service(service):
+        data = service.data
+        args = (data['clear'], data['beep'], data['timeout'],
+                data['line1'], data['line2'])
+        async_dispatcher_send(hass, SIGNAL_DISPLAY_MESSAGE, *args)
+
+    hass.services.async_register(
+        ELK_DOMAIN, 'alarm_display_message',
+        _display_message_service, DISPLAY_MESSAGE_SERVICE_SCHEMA)
 
     return True
+
+
+def _arm_services():
+    from elkm1_lib.const import ArmLevel
+
+    ARM_SERVICES = {
+        'alarm_arm_vacation': ArmLevel.ARMED_VACATION.value,
+        'alarm_arm_home_instant': ArmLevel.ARMED_STAY_INSTANT.value,
+        'alarm_arm_night_instant': ArmLevel.ARMED_NIGHT_INSTANT.value,
+    }
+    return ARM_SERVICES
 
 
 class ElkArea(ElkDeviceBase, alarm.AlarmControlPanel):
     """Representation of an Area / Partition within the ElkM1 alarm panel."""
 
-    def __init__(self, device, hass, config):
+    def __init__(self, element, elk, elk_data):
         """Initialize Area as Alarm Control Panel."""
-        ElkDeviceBase.__init__(self, 'alarm_control_panel', device,
-                               hass, config)
+        super().__init__('alarm_control_panel', element, elk, elk_data)
         self._changed_by_entity_id = ''
 
+    async def async_added_to_hass(self):
+        """Register callback for ElkM1 changes."""
+        await super().async_added_to_hass()
         for keypad in self._elk.keypads:
             keypad.add_callback(self._watch_keypad)
+        async_dispatcher_connect(
+            self.hass, SIGNAL_ARM_ENTITY, self._arm_service)
+        async_dispatcher_connect(
+            self.hass, SIGNAL_DISPLAY_MESSAGE, self._display_message)
 
     def _watch_keypad(self, keypad, changeset):
         if keypad.area != self._element.index:
             return
         if changeset.get('last_user') is not None:
-            self._changed_by_entity_id = self._hass.data[
-                DOMAIN]['keypads'].get(keypad.index, '')
+            self._changed_by_entity_id = self.hass.data[
+                ELK_DOMAIN]['keypads'].get(keypad.index, '')
             self.async_schedule_update_ha_state(True)
 
     @property
@@ -97,24 +103,43 @@ class ElkArea(ElkDeviceBase, alarm.AlarmControlPanel):
         return '^[0-9]{4}([0-9]{2})?$'
 
     @property
+    def state(self):
+        """The state of the element."""
+        return self._state
+
+    @property
     def device_state_attributes(self):
         """Attributes of the area."""
+        from elkm1_lib.const import AlarmState, ArmedStatus, ArmUpState
+
         attrs = self.initial_attrs()
         elmt = self._element
         attrs['is_exit'] = elmt.is_exit
         attrs['timer1'] = elmt.timer1
         attrs['timer2'] = elmt.timer2
-        attrs['armed_status'] = STATE_UNKNOWN if elmt.armed_status is None \
-            else ArmedStatus(elmt.armed_status).name.lower()
-        attrs['arm_up_state'] = STATE_UNKNOWN if elmt.arm_up_state is None \
-            else ArmUpState(elmt.arm_up_state).name.lower()
-        attrs['alarm_state'] = STATE_UNKNOWN if elmt.alarm_state is None \
-            else AlarmState(elmt.alarm_state).name.lower()
+        if elmt.armed_status is not None:
+            attrs['armed_status'] = \
+                ArmedStatus(elmt.armed_status).name.lower()
+        if elmt.arm_up_state is not None:
+            attrs['arm_up_state'] = ArmUpState(elmt.arm_up_state).name.lower()
+        if elmt.alarm_state is not None:
+            attrs['alarm_state'] = AlarmState(elmt.alarm_state).name.lower()
         attrs['changed_by_entity_id'] = self._changed_by_entity_id
         return attrs
 
-    # pylint: disable=unused-argument
     def _element_changed(self, element, changeset):
+        from elkm1_lib.const import ArmedStatus
+
+        ELK_STATE_TO_HASS_STATE = {
+            ArmedStatus.DISARMED.value:               STATE_ALARM_DISARMED,
+            ArmedStatus.ARMED_AWAY.value:             STATE_ALARM_ARMED_AWAY,
+            ArmedStatus.ARMED_STAY.value:             STATE_ALARM_ARMED_HOME,
+            ArmedStatus.ARMED_STAY_INSTANT.value:     STATE_ALARM_ARMED_HOME,
+            ArmedStatus.ARMED_TO_NIGHT.value:         STATE_ALARM_ARMED_NIGHT,
+            ArmedStatus.ARMED_TO_NIGHT_INSTANT.value: STATE_ALARM_ARMED_NIGHT,
+            ArmedStatus.ARMED_TO_VACATION.value:      STATE_ALARM_ARMED_AWAY,
+        }
+
         if self._element.alarm_state is None:
             self._state = STATE_UNKNOWN
         elif self._area_is_in_alarm_state():
@@ -129,6 +154,8 @@ class ElkArea(ElkDeviceBase, alarm.AlarmControlPanel):
         return self._element.timer1 > 0 or self._element.timer2 > 0
 
     def _area_is_in_alarm_state(self):
+        from elkm1_lib.const import AlarmState
+
         return self._element.alarm_state >= AlarmState.FIRE_ALARM.value
 
     async def async_alarm_disarm(self, code=None):
@@ -137,29 +164,29 @@ class ElkArea(ElkDeviceBase, alarm.AlarmControlPanel):
 
     async def async_alarm_arm_home(self, code=None):
         """Send arm home command."""
+        from elkm1_lib.const import ArmLevel
+
         self._element.arm(ArmLevel.ARMED_STAY.value, int(code))
 
     async def async_alarm_arm_away(self, code=None):
         """Send arm away command."""
+        from elkm1_lib.const import ArmLevel
+
         self._element.arm(ArmLevel.ARMED_AWAY.value, int(code))
 
     async def async_alarm_arm_night(self, code=None):
-        """Send arm away command."""
+        """Send arm night command."""
+        from elkm1_lib.const import ArmLevel
+
         self._element.arm(ArmLevel.ARMED_NIGHT.value, int(code))
 
-    async def async_alarm_arm_home_instant(self, code):
-        """Send arm vacation command."""
-        self._element.arm(ArmLevel.ARMED_STAY_INSTANT.value, int(code))
+    async def _arm_service(self, service):
+        if self.entity_id in service.data.get(ATTR_ENTITY_ID, []):
+            arm_level = _arm_services().get(service.service)
+            code = service.data.get('code')
+            if arm_level and code:
+                self._element.arm(arm_level, int(code))
 
-    async def async_alarm_arm_night_instant(self, code):
-        """Send arm vacation command."""
-        self._element.arm(ArmLevel.ARMED_VACATION.value, int(code))
-
-    async def async_alarm_arm_vacation(self, code):
-        """Send arm vacation command."""
-        self._element.arm(ArmLevel.ARMED_VACATION.value, int(code))
-
-    async def async_alarm_display_message(
-            self, clear, beep, timeout, line1, line2):
+    async def _display_message(self, clear, beep, timeout, line1, line2):
         """Display a message on all keypads for the area."""
         self._element.display_message(clear, beep, timeout, line1, line2)
--- homeassistant/components/elkm1/__init__.py
+++ homeassistant/components/elkm1/__init__.py
@@ -10,10 +10,9 @@ https://home-assistant.io/components/elkm1/
 import logging
 
 import voluptuous as vol
-from homeassistant.const import (ATTR_ENTITY_ID, CONF_EXCLUDE,
-                                 CONF_HOST, CONF_INCLUDE, CONF_PASSWORD,
-                                 CONF_TEMPERATURE_UNIT, CONF_USERNAME,
-                                 STATE_UNKNOWN, TEMP_CELSIUS, TEMP_FAHRENHEIT)
+from homeassistant.const import (
+    ATTR_ENTITY_ID, CONF_EXCLUDE, CONF_HOST, CONF_INCLUDE, CONF_PASSWORD,
+    CONF_TEMPERATURE_UNIT, CONF_USERNAME, STATE_UNKNOWN, TEMP_FAHRENHEIT)
 from homeassistant.core import HomeAssistant, callback  # noqa
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers import discovery
@@ -33,10 +32,10 @@ CONF_TASK = 'task'
 CONF_THERMOSTAT = 'thermostat'
 CONF_USER = 'user'
 CONF_PANEL = 'panel'
-CONF_PLC = 'plc'  # Not light because HASS complains about this
+CONF_PLC = 'plc'
 CONF_ZONE = 'zone'
 
-CONF_ENABLED = 'enabled'    # True to enable subdomain
+CONF_ENABLED = 'enabled'
 CONF_HIDE = 'hide'
 CONF_SHOW = 'show'
 
@@ -45,9 +44,7 @@ _LOGGER = logging.getLogger(__name__)
 CONFIG_SCHEMA_SUBDOMAIN = vol.Schema({
     vol.Optional(CONF_ENABLED, default=True): cv.boolean,
     vol.Optional(CONF_INCLUDE): list,
-    vol.Optional(CONF_EXCLUDE): list,
-    vol.Optional(CONF_HIDE): list,
-    vol.Optional(CONF_SHOW): list,
+    vol.Optional(CONF_EXCLUDE): list
     })
 
 CONFIG_SCHEMA = vol.Schema({
@@ -55,8 +52,8 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(CONF_HOST): cv.string,
         vol.Optional(CONF_USERNAME): cv.string,
         vol.Optional(CONF_PASSWORD): cv.string,
-        vol.Optional(CONF_TEMPERATURE_UNIT, default='fahrenheit'):
-            vol.In(['celsius', 'fahrenheit']),
+        vol.Optional(CONF_TEMPERATURE_UNIT, default=TEMP_FAHRENHEIT):
+            cv.temperature_unit,
         vol.Optional(CONF_AREA): CONFIG_SCHEMA_SUBDOMAIN,
         vol.Optional(CONF_COUNTER): CONFIG_SCHEMA_SUBDOMAIN,
         vol.Optional(CONF_KEYPAD): CONFIG_SCHEMA_SUBDOMAIN,
@@ -70,8 +67,7 @@ CONFIG_SCHEMA = vol.Schema({
     })
 }, extra=vol.ALLOW_EXTRA)
 
-SUPPORTED_DOMAINS = ['sensor', 'switch', 'alarm_control_panel',
-                     'climate', 'light']
+SUPPORTED_DOMAINS = ['alarm_control_panel',]
 
 
 async def async_setup(hass: HomeAssistant, hass_config: ConfigType) -> bool:
@@ -99,7 +95,7 @@ async def async_setup(hass: HomeAssistant, hass_config: ConfigType) -> bool:
         """Parse a value as an int or housecode."""
         i = int(val) if val.isdigit() else (housecode_to_index(val) + 1)
         if i < 1 or i > max_:
-            raise ValueError('Value not in range 1 to %d: "%s"' % (max_, val))
+            raise vol.Invalid('Value not in range 1-%d: "%s"' % (max_, val))
         return i
 
     def parse_range(config, item, set_to, values, max_):
@@ -118,22 +114,18 @@ async def async_setup(hass: HomeAssistant, hass_config: ConfigType) -> bool:
     def parse_config(item, max_):
         """Parse a config for an element type such as: zones, plc, etc."""
         if item not in config_raw:
-            return (True, [True] * max_, [True] * max_)
+            return (True, [True] * max_)
 
         conf = config_raw[item]
 
         if CONF_ENABLED in conf and not conf[CONF_ENABLED]:
-            return (False, [False] * max_, [False] * max_)
+            return (False, [False] * max_)
 
         included = [CONF_INCLUDE not in conf] * max_
         parse_range(conf, CONF_INCLUDE, True, included, max_)
         parse_range(conf, CONF_EXCLUDE, False, included, max_)
 
-        shown = [None] * max_
-        parse_range(conf, CONF_SHOW, True, shown, max_)
-        parse_range(conf, CONF_HIDE, False, shown, max_)
-
-        return (True, included, shown)
+        return (True, included)
 
     config_raw = hass_config.get(DOMAIN)
     config = {}
@@ -141,19 +133,13 @@ async def async_setup(hass: HomeAssistant, hass_config: ConfigType) -> bool:
     host = config_raw[CONF_HOST]
     username = config_raw.get(CONF_USERNAME)
     password = config_raw.get(CONF_PASSWORD)
-    if host.startswith('elks:'):
-        if username is None or password is None:
-            _LOGGER.error('Specify username & password for secure connection')
-            return False
+    if host.startswith('elks:') and (username is None or password is None): 
+        raise vol.Invalid("Specify username & password for elks://")
 
     for item, max_ in configs.items():
         config[item] = {}
-        try:
-            (config[item]['enabled'], config[item]['included'],
-             config[item]['shown']) = parse_config(item, max_)
-        except ValueError as err:
-            _LOGGER.error("Config item: %s; %s", item, err)
-            return False
+        (config[item]['enabled'], config[item]['included']) = \
+                parse_config(item, max_)
 
     config['temperature_unit'] = config_raw[CONF_TEMPERATURE_UNIT]
 
@@ -163,54 +149,30 @@ async def async_setup(hass: HomeAssistant, hass_config: ConfigType) -> bool:
     hass.data[DOMAIN] = {'elk': elk, 'config': config,
                          'entities': {}, 'keypads': {}}
     for component in SUPPORTED_DOMAINS:
-        hass.async_add_job(
-            discovery.async_load_platform(hass, component, DOMAIN, None, None))
+        hass.async_create_task(
+            discovery.async_load_platform(hass, component, DOMAIN))
     return True
 
 
-def create_elk_devices(hass, elk_elements, element_type, class_, devices):
+def create_elk_entities(hass, elk_elements, element_type, class_, entities):
     """Helper to create the ElkM1 devices of a particular class."""
-    config = hass.data[DOMAIN]['config']
+    elk_data = hass.data[DOMAIN]
+    elk = elk_data['elk']
     for element in elk_elements:
-        if config[element_type]['included'][element.index]:
-            devices.append(class_(element, hass, config[element_type]))
-    return devices
-
-
-def register_elk_service(hass, domain, service_name, schema, service_handler):
-    """Map services to methods."""
-    async def async_service_handler(service):
-        for entity_id in service.data.get(ATTR_ENTITY_ID, []):
-            entity = hass.data[DOMAIN]['entities'].get(entity_id)
-            if not entity:
-                continue
-
-            handler = getattr(entity, service_handler)
-            if not handler:
-                continue
-
-            kwargs = {key: val for key, val in service.data.items()
-                      if key != ATTR_ENTITY_ID}
-            await handler(**kwargs)
-
-    hass.services.async_register(
-        domain, service_name, async_service_handler, schema=schema)
+        if elk_data['config'][element_type]['included'][element.index]:
+            entities.append(class_(element, elk, elk_data))
+    return entities
 
 
 class ElkDeviceBase(Entity):
     """Sensor devices on the Elk."""
-    def __init__(self, platform, element, hass, config):
-        self._elk = hass.data[DOMAIN]['elk']
+    def __init__(self, platform, element, elk, elk_data):
+        self._elk = elk
         self._element = element
-        self._hass = hass
-        self._show_override = config['shown'][element.index]
-        self._hidden = False
-        self._state = STATE_UNKNOWN
-        self._temperature_unit = TEMP_CELSIUS if hass.data[DOMAIN][
-            'config']['temperature_unit'] == 'celsius' else TEMP_FAHRENHEIT
-        self._unique_id = platform + '.elkm1_' + \
-            self._element.default_name('_').lower()
-        self.entity_id = self._unique_id
+        self._state = None
+        self._temperature_unit = elk_data['config']['temperature_unit']
+        self._unique_id = 'elkm1_{}'.format(
+            self._element.default_name('_').lower())
 
     @property
     def name(self):
@@ -223,32 +185,24 @@ class ElkDeviceBase(Entity):
         return self._unique_id
 
     @property
-    def state(self):
-        """The state of the element."""
-        return self._state
-
-    @property
     def should_poll(self) -> bool:
         """Don't poll this device."""
         return False
-
-    @property
-    def hidden(self):
-        """Return if the element is hidden."""
-        if self._show_override is None:
-            return self._hidden
-        return not self._show_override
 
     @property
     def device_state_attributes(self):
         """Default attributes of the element, if not overridden."""
         return {**self._element.as_dict(), **self.initial_attrs()}
 
+    @property
+    def available(self):
+        """Is the entity available to be updated."""
+        return self._elk.is_connected()
+
     def initial_attrs(self):
         """The underlying element's attributes as a dict."""
         attrs = {}
         attrs['index'] = self._element.index + 1
-        attrs['state'] = self._state
         return attrs
 
     def _element_changed(self, element, changeset):
@@ -264,4 +218,3 @@ class ElkDeviceBase(Entity):
         """Register callback for ElkM1 changes and update entity state."""
         self._element.add_callback(self._element_callback)
         self._element_callback(self._element, {})
-        self._hass.data[DOMAIN]['entities'][self.entity_id] = self
