afdd4139849e35b1d3ff5580971456e87f4ec1bb
mvn23 <schopdiedwaas@gmail.com>
2018-10-06 14:41:19 +0200

    Add OpenTherm Gateway sensor platform.

--- homeassistant/components/opentherm_gw.py
+++ homeassistant/components/opentherm_gw.py
@@ -8,14 +8,16 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.const import (CONF_DEVICE, CONF_NAME, PRECISION_HALVES,
-                                 PRECISION_TENTHS, PRECISION_WHOLE)
 from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.helpers.dispatcher import async_dispatcher_send
-
+from homeassistant.const import (CONF_DEVICE, CONF_MONITORED_VARIABLES,
+                                 CONF_NAME, PRECISION_HALVES, PRECISION_TENTHS,
+                                 PRECISION_WHOLE)
 import homeassistant.helpers.config_validation as cv
 
 DOMAIN = 'opentherm_gw'
+
+COMP_SENSOR = 'sensor'
 
 CONF_CLIMATE = 'climate'
 CONF_FLOOR_TEMP = 'floor_temperature'
@@ -38,6 +40,7 @@ CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_DEVICE): cv.string,
         vol.Optional(CONF_CLIMATE, default={}): CLIMATE_SCHEMA,
+        vol.Optional(CONF_MONITORED_VARIABLES, default=[]): cv.ensure_list,
     }),
 }, extra=vol.ALLOW_EXTRA)
 
@@ -59,6 +62,8 @@ async def async_setup(hass, config):
         hass, conf[CONF_DEVICE], gateway))
     hass.async_create_task(async_load_platform(
         hass, 'climate', DOMAIN, conf.get(CONF_CLIMATE)))
+    hass.async_create_task(setup_monitored_vars(
+        hass, conf.get(CONF_MONITORED_VARIABLES)))
     return True
 
 
@@ -72,3 +77,68 @@ async def connect_and_subscribe(hass, device_path, gateway):
         _LOGGER.debug("Received report: %s", status)
         async_dispatcher_send(hass, SIGNAL_OPENTHERM_GW_UPDATE, status)
     gateway.subscribe(handle_report)
+
+
+async def setup_monitored_vars(hass, monitored_vars):
+    """Setup requested sensors."""
+    gw_vars = hass.data[DATA_OPENTHERM_GW][DATA_GW_VARS]
+    # Use dict to prepare for binary sensor support.
+    sensor_type_map = {
+        COMP_SENSOR: [
+            gw_vars.DATA_CONTROL_SETPOINT,
+            gw_vars.DATA_MASTER_MEMBERID,
+            gw_vars.DATA_SLAVE_MEMBERID,
+            gw_vars.DATA_SLAVE_OEM_FAULT,
+            gw_vars.DATA_COOLING_CONTROL,
+            gw_vars.DATA_CONTROL_SETPOINT_2,
+            gw_vars.DATA_ROOM_SETPOINT_OVRD,
+            gw_vars.DATA_SLAVE_MAX_RELATIVE_MOD,
+            gw_vars.DATA_SLAVE_MAX_CAPACITY,
+            gw_vars.DATA_SLAVE_MIN_MOD_LEVEL,
+            gw_vars.DATA_ROOM_SETPOINT,
+            gw_vars.DATA_REL_MOD_LEVEL,
+            gw_vars.DATA_CH_WATER_PRESS,
+            gw_vars.DATA_DHW_FLOW_RATE,
+            gw_vars.DATA_ROOM_SETPOINT_2,
+            gw_vars.DATA_ROOM_TEMP,
+            gw_vars.DATA_CH_WATER_TEMP,
+            gw_vars.DATA_DHW_TEMP,
+            gw_vars.DATA_OUTSIDE_TEMP,
+            gw_vars.DATA_RETURN_WATER_TEMP,
+            gw_vars.DATA_SOLAR_STORAGE_TEMP,
+            gw_vars.DATA_SOLAR_COLL_TEMP,
+            gw_vars.DATA_CH_WATER_TEMP_2,
+            gw_vars.DATA_DHW_TEMP_2,
+            gw_vars.DATA_EXHAUST_TEMP,
+            gw_vars.DATA_SLAVE_DHW_MAX_SETP,
+            gw_vars.DATA_SLAVE_DHW_MIN_SETP,
+            gw_vars.DATA_SLAVE_CH_MAX_SETP,
+            gw_vars.DATA_SLAVE_CH_MIN_SETP,
+            gw_vars.DATA_DHW_SETPOINT,
+            gw_vars.DATA_MAX_CH_SETPOINT,
+            gw_vars.DATA_OEM_DIAG,
+            gw_vars.DATA_CH_BURNER_STARTS,
+            gw_vars.DATA_CH_PUMP_STARTS,
+            gw_vars.DATA_DHW_PUMP_STARTS,
+            gw_vars.DATA_DHW_BURNER_STARTS,
+            gw_vars.DATA_CH_BURNER_HOURS,
+            gw_vars.DATA_CH_PUMP_HOURS,
+            gw_vars.DATA_DHW_PUMP_HOURS,
+            gw_vars.DATA_DHW_BURNER_HOURS,
+            gw_vars.DATA_MASTER_OT_VERSION,
+            gw_vars.DATA_SLAVE_OT_VERSION,
+            gw_vars.DATA_MASTER_PRODUCT_TYPE,
+            gw_vars.DATA_MASTER_PRODUCT_VERSION,
+            gw_vars.DATA_SLAVE_PRODUCT_TYPE,
+            gw_vars.DATA_SLAVE_PRODUCT_VERSION,
+        ]
+    }
+    sensors = []
+    for var in monitored_vars:
+        if var in sensor_type_map[COMP_SENSOR]:
+            sensors.append(var)
+        else:
+            _LOGGER.error("Monitored variable not supported: %s", var)
+    if sensors:
+        hass.async_create_task(
+            async_load_platform(hass, COMP_SENSOR, DOMAIN, sensors))
