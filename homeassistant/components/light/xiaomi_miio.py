eac8384cc2ee4e526e79a594d24f6548cfd12d11
MiguelGil <miguelgilmartinez@gmail.com>
2018-10-07 11:42:50 +0200

    Xiaomi Philips Eyecare 2: Fixed "translate" method when "value" is None

--- homeassistant/components/light/xiaomi_miio.py
+++ homeassistant/components/light/xiaomi_miio.py
@@ -505,6 +505,8 @@ class XiaomiPhilipsBulb(XiaomiPhilipsGenericLight):
         """Map a value from left span to right span."""
         left_span = left_max - left_min
         right_span = right_max - right_min
+        if value is None:
+            value = 0
         value_scaled = float(value - left_min) / float(left_span)
         return int(right_min + (value_scaled * right_span))
 
