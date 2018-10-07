e1c11c574523db221ef4d1c0ea9ed1bef0656358
mvn23 <schopdiedwaas@gmail.com>
2018-10-07 15:02:36 +0200

    Remove import from platform, use hass.data instead. Update .coveragerc Update docstrings Update requirements_all.txt General code cleanup

--- homeassistant/components/opentherm_gw.py
+++ homeassistant/components/opentherm_gw.py
@@ -1,13 +1,17 @@
-import asyncio
+"""
+Support for OpenTherm Gateway devices.
+
+For more details about this component, please refer to the documentation at
+http://home-assistant.io/components/opentherm_gw/
+"""
+import logging
 import voluptuous as vol
 
-from homeassistant.helpers.discovery import (async_load_platform,
-                                             async_listen_platform)
-from homeassistant.helpers.dispatcher import async_dispatcher_send
-from homeassistant.const import (CONF_BINARY_SENSORS, CONF_DEVICE,
-                                 CONF_MONITORED_VARIABLES, CONF_NAME,
-                                 CONF_SENSORS, PRECISION_HALVES,
+from homeassistant.const import (CONF_DEVICE, CONF_NAME, PRECISION_HALVES,
                                  PRECISION_TENTHS, PRECISION_WHOLE)
+from homeassistant.helpers.discovery import async_load_platform
+from homeassistant.helpers.dispatcher import async_dispatcher_send
+
 import homeassistant.helpers.config_validation as cv
 
 DOMAIN = 'opentherm_gw'
@@ -15,6 +19,10 @@ DOMAIN = 'opentherm_gw'
 CONF_CLIMATE = 'climate'
 CONF_FLOOR_TEMP = 'floor_temperature'
 CONF_PRECISION = 'precision'
+
+DATA_DEVICE = 'device'
+DATA_GW_VARS = 'gw_vars'
+DATA_OPENTHERM_GW = 'opentherm_gw'
 
 SIGNAL_OPENTHERM_GW_UPDATE = 'opentherm_gw_update'
 
@@ -25,22 +33,18 @@ CLIMATE_SCHEMA = vol.Schema({
     vol.Optional(CONF_FLOOR_TEMP, default=False): cv.boolean,
 })
 
-SENSOR_SCHEMA = vol.Schema({
-    vol.Optional(CONF_MONITORED_VARIABLES, default=[]): cv.ensure_list,
-})
-
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_DEVICE): cv.string,
         vol.Optional(CONF_CLIMATE, default=CLIMATE_SCHEMA({})):
             CLIMATE_SCHEMA,
-        vol.Optional(CONF_SENSORS, default=SENSOR_SCHEMA({})): SENSOR_SCHEMA,
-        vol.Optional(CONF_BINARY_SENSORS, default=SENSOR_SCHEMA({})):
-            SENSOR_SCHEMA,
     }),
-}, extra = vol.ALLOW_EXTRA)
+}, extra=vol.ALLOW_EXTRA)
 
 REQUIREMENTS = ['pyotgw==0.1b0']
+
+_LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup(hass, config):
     """Set up the OpenTherm Gateway component."""
@@ -48,27 +52,25 @@ async def async_setup(hass, config):
     if conf is None:
         return True
 
+    import pyotgw
+    hass.data[DATA_OPENTHERM_GW] = {
+        DATA_DEVICE: pyotgw.pyotgw(),
+        DATA_GW_VARS: pyotgw.vars,
+    }
     hass.async_add_job(connect_and_subscribe, hass, conf)
     hass.async_create_task(async_load_platform(
         hass, 'climate', DOMAIN, conf.get(CONF_CLIMATE)))
-    #hass.async_create_task(async_load_platform(
-    #    hass, 'sensor', DOMAIN, conf.get(CONF_MONITORED_VARIABLES)))
-    #hass.async_create_task(async_load_platform(
-    #    hass, 'binary_sensor', DOMAIN, conf.get(CONF_MONITORED_VARIABLES)))
     return True
+
 
 async def connect_and_subscribe(hass, conf):
     """Connect to serial device and subscribe report handler."""
-    import pyotgw
-    gateway = pyotgw.pyotgw()
+    gateway = hass.data[DATA_OPENTHERM_GW][DATA_DEVICE]
     await gateway.connect(hass.loop, conf[CONF_DEVICE])
+    _LOGGER.debug("Connected to OpenTherm Gateway at %s", conf[CONF_DEVICE])
 
     async def handle_report(status):
         """Handle reports from the OpenTherm Gateway."""
+        _LOGGER.debug("Received report: %s", status)
         async_dispatcher_send(hass, SIGNAL_OPENTHERM_GW_UPDATE, status)
     gateway.subscribe(handle_report)
-
-
-# class OpenThermSensor(Entity):
-#     """Basic functionality for opentherm_gw sensor."""
-#     
