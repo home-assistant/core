1ed1677827c74c628e166bca93d01225d46cf31a
Charles Garwood <cgarwood@gmail.com>
2018-10-09 14:45:26 -0400

    Add device_registry support for sensor and switch domains

--- homeassistant/components/switch/zwave.py
+++ homeassistant/components/switch/zwave.py
@@ -6,11 +6,28 @@ https://home-assistant.io/components/switch.zwave/
 """
 import logging
 import time
+from homeassistant.core import callback
 from homeassistant.components.switch import DOMAIN, SwitchDevice
 from homeassistant.components import zwave
-from homeassistant.components.zwave import workaround, async_setup_platform  # noqa pylint: disable=unused-import
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
 _LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_platform(hass, config, async_add_entities,
+                               discovery_info=None):
+    """Old method of setting up Z-Wave switches."""
+    pass
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up Z-Wave Switch from Config Entry."""
+    @callback
+    def async_add_switch(switch):
+        """Add Z-Wave Switch."""
+        async_add_entities([switch])
+
+    async_dispatcher_connect(hass, 'zwave_new_switch', async_add_switch)
 
 
 def get_device(values, **kwargs):
@@ -25,8 +42,8 @@ class ZwaveSwitch(zwave.ZWaveDeviceEntity, SwitchDevice):
         """Initialize the Z-Wave switch device."""
         zwave.ZWaveDeviceEntity.__init__(self, values, DOMAIN)
         self.refresh_on_update = (
-            workaround.get_device_mapping(values.primary) ==
-            workaround.WORKAROUND_REFRESH_NODE_ON_UPDATE)
+            zwave.workaround.get_device_mapping(values.primary) ==
+            zwave.workaround.WORKAROUND_REFRESH_NODE_ON_UPDATE)
         self.last_update = time.perf_counter()
         self._state = self.values.primary.data
 
