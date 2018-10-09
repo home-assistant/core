7bac1004013ecebd10756d37108c977e70495f69
Glenn Waters <gwwaters@gmail.com>
2018-10-08 20:20:41 -0400

    Fix comment.

--- homeassistant/components/switch/elkm1.py
+++ homeassistant/components/switch/elkm1.py
@@ -1,5 +1,5 @@
 """
-Support for control of ElkM1 outputs (relays) and tasks ("macros).
+Support for control of ElkM1 outputs (relays).
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/switch.elkm1/
