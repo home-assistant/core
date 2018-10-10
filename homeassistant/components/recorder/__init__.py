33b7428cc90877340f8a715a250c63d6198b8e7a
Ross Dargan <rossdargan@users.noreply.github.com>
2018-10-10 12:51:35 +0100

    Update __init__.py
    add in missing space

--- homeassistant/components/recorder/__init__.py
+++ homeassistant/components/recorder/__init__.py
@@ -34,7 +34,7 @@ from . import migration, purge
 from .const import DATA_INSTANCE
 from .util import session_scope
 
-REQUIREMENTS = ['sqlalchemy==1.2.11','pymssql==2.1.4']
+REQUIREMENTS = ['sqlalchemy==1.2.11', 'pymssql==2.1.4']
 
 _LOGGER = logging.getLogger(__name__)
 
