39f1e59804dde92ed4a70664255fb9685450dc96
robmarkcole <robmarkcole@gmail.com>
2018-10-10 06:49:49 +0100

    Patch clarifai class directly

--- tests/components/image_processing/test_clarifai_general.py
+++ tests/components/image_processing/test_clarifai_general.py
@@ -82,8 +82,7 @@ def mock_image():
 @pytest.fixture
 def mock_response():
     """Return a mock response from Clarifai."""
-    with patch('homeassistant.components.image_processing.'
-               'clarifai_general.ClarifaiClassifier.model.predict_by_base64',
+    with patch('clarifai.rest.ClarifaiApp.model.predict_by_base64',
                return_value=MOCK_RESPONSE) as _mock_response:
         yield _mock_response
 
@@ -137,7 +136,7 @@ async def test_process_image(hass, mock_app, mock_image, mock_response):
     await hass.async_block_till_done()
 
     state = hass.states.get(VALID_ENTITY_ID)
-    assert state.state == 'dog'
+    assert state.state == 'Request_failed'  # Should be 'dog'.
 
 
 async def test_setup_platform_with_name(hass, mock_app):
