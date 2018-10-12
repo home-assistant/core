154217feb7b7af551f680c80a2871338f2bc6741
Aaron Bach <bachya1208@gmail.com>
2018-10-12 11:07:47 -0600

    Add config entry for SimpliSafe (#17148)
    * Initial move into component
    
    * Base functionality in place
    
    * Starting tests in place
    
    * All config entry tests in place
    
    * Made default scan interval more obvious and removed extra logging
    
    * Inherit default scan interval from alarm_control_panel
    
    * Updated coveragerc and CODEOWNERS
    
    * Member-requested changes
    
    * Hound
    
    * Updated requirements
    
    * Updated tests
    
    * Member-requested changes
    
    * Owner-requested changes
    
    * Constant cleanup
    
    * Fixed config flow test
    
    * Owner-requested updates
    
    * Updated requirements
    
    * Using async_will_remove_from_hass
    
    * Corrected scan interval logic
    
    * Fixed tests
    
    * Owner-requested changes
    
    * Additional logging
    
    * Owner-requested changes

--- homeassistant/components/alarm_control_panel/simplisafe.py
+++ homeassistant/components/alarm_control_panel/simplisafe.py
@@ -1,5 +1,5 @@
 """
-Interfaces with SimpliSafe alarm control panel.
+This platform provides alarm control functionality for SimpliSafe.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/alarm_control_panel.simplisafe/
@@ -7,86 +7,44 @@ https://home-assistant.io/components/alarm_control_panel.simplisafe/
 import logging
 import re
 
-import voluptuous as vol
-
-from homeassistant.components.alarm_control_panel import (
-    PLATFORM_SCHEMA, AlarmControlPanel)
+from homeassistant.components.alarm_control_panel import AlarmControlPanel
+from homeassistant.components.simplisafe.const import (
+    DATA_CLIENT, DOMAIN, TOPIC_UPDATE)
 from homeassistant.const import (
-    CONF_CODE, CONF_NAME, CONF_PASSWORD, CONF_USERNAME, STATE_ALARM_ARMED_AWAY,
-    STATE_ALARM_ARMED_HOME, STATE_ALARM_DISARMED)
-from homeassistant.helpers import aiohttp_client, config_validation as cv
-from homeassistant.util.json import load_json, save_json
-
-REQUIREMENTS = ['simplisafe-python==3.1.2']
+    CONF_CODE, STATE_ALARM_ARMED_AWAY, STATE_ALARM_ARMED_HOME,
+    STATE_ALARM_DISARMED)
+from homeassistant.core import callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_ALARM_ACTIVE = "alarm_active"
-ATTR_TEMPERATURE = "temperature"
-
-DATA_FILE = '.simplisafe'
-
-DEFAULT_NAME = 'SimpliSafe'
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_USERNAME): cv.string,
-    vol.Required(CONF_PASSWORD): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_CODE): cv.string,
-})
+ATTR_ALARM_ACTIVE = 'alarm_active'
+ATTR_TEMPERATURE = 'temperature'
 
 
 async def async_setup_platform(
         hass, config, async_add_entities, discovery_info=None):
-    """Set up the SimpliSafe platform."""
-    from simplipy import API
-    from simplipy.errors import SimplipyError
+    """Set up a SimpliSafe alarm control panel based on existing config."""
+    pass
 
-    username = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
-    name = config.get(CONF_NAME)
-    code = config.get(CONF_CODE)
 
-    websession = aiohttp_client.async_get_clientsession(hass)
-
-    config_data = await hass.async_add_executor_job(
-        load_json, hass.config.path(DATA_FILE))
-
-    try:
-        if config_data:
-            try:
-                simplisafe = await API.login_via_token(
-                    config_data['refresh_token'], websession)
-                _LOGGER.debug('Logging in with refresh token')
-            except SimplipyError:
-                _LOGGER.info('Refresh token expired; attempting credentials')
-                simplisafe = await API.login_via_credentials(
-                    username, password, websession)
-        else:
-            simplisafe = await API.login_via_credentials(
-                username, password, websession)
-            _LOGGER.debug('Logging in with credentials')
-    except SimplipyError as err:
-        _LOGGER.error("There was an error during setup: %s", err)
-        return
-
-    config_data = {'refresh_token': simplisafe.refresh_token}
-    await hass.async_add_executor_job(
-        save_json, hass.config.path(DATA_FILE), config_data)
-
-    systems = await simplisafe.get_systems()
-    async_add_entities(
-        [SimpliSafeAlarm(system, name, code) for system in systems], True)
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up a SimpliSafe alarm control panel based on a config entry."""
+    systems = hass.data[DOMAIN][DATA_CLIENT][entry.entry_id]
+    async_add_entities([
+        SimpliSafeAlarm(system, entry.data.get(CONF_CODE))
+        for system in systems
+    ], True)
 
 
 class SimpliSafeAlarm(AlarmControlPanel):
     """Representation of a SimpliSafe alarm."""
 
-    def __init__(self, system, name, code):
+    def __init__(self, system, code):
         """Initialize the SimpliSafe alarm."""
+        self._async_unsub_dispatcher_connect = None
         self._attrs = {}
-        self._code = str(code) if code else None
-        self._name = name
+        self._code = code
         self._system = system
         self._state = None
 
@@ -98,9 +56,7 @@ class SimpliSafeAlarm(AlarmControlPanel):
     @property
     def name(self):
         """Return the name of the device."""
-        if self._name:
-            return self._name
-        return 'Alarm {}'.format(self._system.system_id)
+        return self._system.address
 
     @property
     def code_format(self):
@@ -128,6 +84,21 @@ class SimpliSafeAlarm(AlarmControlPanel):
             _LOGGER.warning("Wrong code entered for %s", state)
         return check
 
+    async def async_added_to_hass(self):
+        """Register callbacks."""
+        @callback
+        def update():
+            """Update the state."""
+            self.async_schedule_update_ha_state(True)
+
+        self._async_unsub_dispatcher_connect = async_dispatcher_connect(
+            self.hass, TOPIC_UPDATE, update)
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Disconnect dispatcher listener when removed."""
+        if self._async_unsub_dispatcher_connect:
+            self._async_unsub_dispatcher_connect()
+
     async def async_alarm_disarm(self, code=None):
         """Send disarm command."""
         if not self._validate_code(code, 'disarming'):
@@ -151,22 +122,24 @@ class SimpliSafeAlarm(AlarmControlPanel):
 
     async def async_update(self):
         """Update alarm status."""
-        await self._system.update()
+        from simplipy.system import SystemStates
 
-        if self._system.state == self._system.SystemStates.off:
-            self._state = STATE_ALARM_DISARMED
-        elif self._system.state in (
-                self._system.SystemStates.home,
-                self._system.SystemStates.home_count):
-            self._state = STATE_ALARM_ARMED_HOME
-        elif self._system.state in (
-                self._system.SystemStates.away,
-                self._system.SystemStates.away_count,
-                self._system.SystemStates.exit_delay):
-            self._state = STATE_ALARM_ARMED_AWAY
-        else:
-            self._state = None
+        await self._system.update()
 
         self._attrs[ATTR_ALARM_ACTIVE] = self._system.alarm_going_off
         if self._system.temperature:
             self._attrs[ATTR_TEMPERATURE] = self._system.temperature
+
+        if self._system.state == SystemStates.error:
+            return
+
+        if self._system.state == SystemStates.off:
+            self._state = STATE_ALARM_DISARMED
+        elif self._system.state in (SystemStates.home,
+                                    SystemStates.home_count):
+            self._state = STATE_ALARM_ARMED_HOME
+        elif self._system.state in (SystemStates.away, SystemStates.away_count,
+                                    SystemStates.exit_delay):
+            self._state = STATE_ALARM_ARMED_AWAY
+        else:
+            self._state = None
