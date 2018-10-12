b558f92a36af29a3bf9e431f9de494e4bf26c46f
shred86 <shred86@icloud.com>
2018-10-11 19:11:23 -0800

    Update line length

--- homeassistant/components/light/abode.py
+++ homeassistant/components/light/abode.py
@@ -48,7 +48,8 @@ class AbodeLight(AbodeDevice, Light):
         """Turn on the light."""
         if ATTR_COLOR_TEMP in kwargs and self._device.is_color_capable:
             self._device.set_color_temp(
-                int(color_temperature_mired_to_kelvin(kwargs[ATTR_COLOR_TEMP])))
+                int(color_temperature_mired_to_kelvin(
+                    kwargs[ATTR_COLOR_TEMP])))
 
         if ATTR_HS_COLOR in kwargs and self._device.is_color_capable:
             self._device.set_color(kwargs[ATTR_HS_COLOR])
