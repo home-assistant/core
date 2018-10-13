493606056b242db1e44f207f2e0312237520c40b
Malte Franken <exxamalte@users.noreply.github.com>
2018-10-13 12:48:50 +1100

    small refactorings

--- homeassistant/components/automation/geo_location.py
+++ homeassistant/components/automation/geo_location.py
@@ -13,6 +13,7 @@ from homeassistant.const import (
     CONF_EVENT, CONF_PLATFORM, CONF_SOURCE, CONF_ZONE, EVENT_STATE_CHANGED)
 from homeassistant.helpers import (
     condition, config_validation as cv)
+from homeassistant.helpers.config_validation import entity_domain
 
 EVENT_ENTER = 'enter'
 EVENT_LEAVE = 'leave'
@@ -21,10 +22,15 @@ DEFAULT_EVENT = EVENT_ENTER
 TRIGGER_SCHEMA = vol.Schema({
     vol.Required(CONF_PLATFORM): 'geo_location',
     vol.Required(CONF_SOURCE): cv.string,
-    vol.Required(CONF_ZONE): cv.entity_id,
+    vol.Required(CONF_ZONE): entity_domain('zone'),
     vol.Required(CONF_EVENT, default=DEFAULT_EVENT):
         vol.Any(EVENT_ENTER, EVENT_LEAVE),
 })
+
+
+def source_match(state, source):
+    """Check if the state matches the provided source."""
+    return state and state.attributes.get('source') == source
 
 
 async def async_trigger(hass, config, action):
@@ -32,10 +38,6 @@ async def async_trigger(hass, config, action):
     source = config.get(CONF_SOURCE).lower()
     zone_entity_id = config.get(CONF_ZONE)
     trigger_event = config.get(CONF_EVENT)
-
-    def source_match(state):
-        """Check if the state matches the configured source."""
-        return state and state.attributes.get('source') == source
 
     @callback
     def state_change_listener(event):
@@ -46,7 +48,8 @@ async def async_trigger(hass, config, action):
         # Skip if the event's source does not match the trigger's source.
         from_state = event.data.get('old_state')
         to_state = event.data.get('new_state')
-        if not source_match(from_state) and not source_match(to_state):
+        if not source_match(from_state, source) \
+           and not source_match(to_state, source):
             return
 
         zone_state = hass.states.get(zone_entity_id)
@@ -66,6 +69,6 @@ async def async_trigger(hass, config, action):
                     'zone': zone_state,
                     'event': trigger_event,
                 },
-            }, context=None if not to_state else to_state.context))
+            }, context=event.context))
 
     return hass.bus.async_listen(EVENT_STATE_CHANGED, state_change_listener)
