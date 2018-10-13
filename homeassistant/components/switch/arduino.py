b23c2d57eaa4884d66d298950ec625dbbc475d44
Daniel Ingram <ingramds@appstate.edu>
2018-10-13 09:54:10 -0400

    Remove unnecessary callback functionality from ArduinoSwitch

--- homeassistant/components/switch/arduino.py
+++ homeassistant/components/switch/arduino.py
@@ -54,13 +54,12 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class ArduinoSwitch(SwitchDevice):
     """Representation of an Arduino switch."""
 
-    def __init__(self, pin, options, callback=None):
+    def __init__(self, pin, options):
         """Initialize the Pin."""
         self._pin = pin
         self._name = options.get(CONF_NAME)
         self.pin_type = CONF_TYPE
         self.direction = 'out'
-        self._callback = callback
 
         self._state = options.get(CONF_INITIAL)
 
@@ -71,8 +70,7 @@ class ArduinoSwitch(SwitchDevice):
             self.turn_on_handler = arduino.BOARD.set_digital_out_high
             self.turn_off_handler = arduino.BOARD.set_digital_out_low
 
-        arduino.BOARD.set_mode(
-            self._pin, self.direction, self.pin_type, callback=callback)
+        arduino.BOARD.set_mode(self._pin, self.direction, self.pin_type)
         (self.turn_on_handler if self._state else self.turn_off_handler)(pin)
 
     @property
