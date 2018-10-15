281b4ca58bbba2f5922f7193d8e3909c932cb5b8
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-10-15 12:21:18 +0200

    Update homematic.py

--- homeassistant/components/notify/homematic.py
+++ homeassistant/components/notify/homematic.py
@@ -4,7 +4,6 @@ Notification support for Homematic.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/notify.homematic/
 """
-import copy
 import logging
 
 import voluptuous as vol
@@ -53,8 +52,7 @@ class HomematicNotificationService(BaseNotificationService):
 
     def send_message(self, message="", **kwargs):
         """Send a notification to the device."""
-        data = copy.deepcopy(self.data)
-        data.update(kwargs.get(ATTR_DATA, {}))
+        data = {**self.data, **kwargs.get(ATTR_DATA, {})}
 
         if data.get(ATTR_VALUE) is not None:
             templ = template_helper.Template(self.data[ATTR_VALUE], self.hass)
