bd838268befa4edf94f41d26d90fa75bf7b74271
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2018-10-15 16:43:15 +0300

    Xiaomi Plug UI fix #17422

--- homeassistant/components/switch/xiaomi_aqara.py
+++ homeassistant/components/switch/xiaomi_aqara.py
@@ -137,7 +137,7 @@ class XiaomiGenericSwitch(XiaomiDevice, SwitchDevice):
             self._load_power = round(float(data[LOAD_POWER]), 2)
 
         value = data.get(self._data_key)
-        if value is None:
+        if value is None or value == 'unknown':
             return False
 
         state = value == 'on'
