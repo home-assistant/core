48537388ca626e2cf292d898ce4c8dd35ac0e673
Erik Hendrix <hendrix_erik@hotmail.com>
2018-10-16 15:18:11 -0600

    Enhancements for DirecTV media player
    Following enhancements have been made:
    
    1. Added debug logging
    2. Added ability to change channel using select_source service of the remote platform.
    3. State will now show paused if a recorded program is paused, for live TV playing will always be returned.
    4. Added the following attributes:
        a. media_position: current position of the media (in seconds)
        b. media_position_updated_at: timestamp when media_position was updated.
       c. source: current source (channel).
       d. media_isbeingrecorded: if current media is being recorded or not.
       e. media_rating: TV/Movie rating of the media
       f. media_recorded: if current media is recorded or live TV
       g. media_starttime: Timestamp media was aired
    
    Reordered properties to follow same order as how they are in __init__.py of remote platform.

--- homeassistant/components/media_player/directv.py
+++ homeassistant/components/media_player/directv.py
@@ -23,10 +23,17 @@ REQUIREMENTS = ['directpy==0.5']
 
 _LOGGER = logging.getLogger(__name__)
 
+<<<<<<< HEAD
 ATTR_MEDIA_CURRENTLY_RECORDING = 'media_currently_recording'
 ATTR_MEDIA_RATING = 'media_rating'
 ATTR_MEDIA_RECORDED = 'media_recorded'
 ATTR_MEDIA_START_TIME = 'media_start_time'
+=======
+ATTR_MEDIA_CURRENTLYRECORDING = 'media_currently_recording'
+ATTR_MEDIA_RATING = 'media_rating'
+ATTR_MEDIA_RECORDED = 'media_recorded'
+ATTR_MEDIA_STARTTIME = 'media_starttime'
+>>>>>>> Enhancements for DirecTV media player
 
 DEFAULT_DEVICE = '0'
 DEFAULT_NAME = "DirecTV Receiver"
@@ -250,7 +257,7 @@ class DirecTvDevice(MediaPlayerDevice):
         if self._is_standby:
             return None
 
-        return self._last_position
+       return self._last_position
 
     @property
     def media_position_updated_at(self):
@@ -278,6 +285,23 @@ class DirecTvDevice(MediaPlayerDevice):
             return None
 
         return self._current.get('episodeTitle')
+
+    @property
+    def media_channel(self):
+        """Return the channel current playing media."""
+        if self._is_standby:
+            return None
+
+        return "{} ({})".format(
+            self._current['callsign'], self._current['major'])
+
+    @property
+    def source(self):
+        """Name of the current input source."""
+        if self._is_standby:
+            return None
+
+        return self._current['major']
 
     @property
     def media_channel(self):
