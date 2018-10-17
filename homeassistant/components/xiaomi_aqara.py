df83affca55943cdf3a1d126e17e75b3d6f8477e
Daniel Høyer Iversen <mail@dahoiv.net>
2018-10-17 11:07:23 +0200

    Fix unique id for xiaomi cover

--- homeassistant/components/xiaomi_aqara.py
+++ homeassistant/components/xiaomi_aqara.py
@@ -221,6 +221,7 @@ class XiaomiDevice(Entity):
         self.parse_voltage(device['data'])
 
         if hasattr(self, '_data_key') \
+                and isinstance(self._data_key, str) \
                 and self._data_key:  # pylint: disable=no-member
             self._unique_id = slugify("{}-{}".format(
                 self._data_key,  # pylint: disable=no-member
