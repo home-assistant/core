47fbb125bd9d2dc12524aaec5e58caa975ca532e
Antoine GRÉA <grea09@users.noreply.github.com>
2018-10-19 13:59:09 +0200

    Removing update interval alltogether

--- tests/components/sensor/test_fail2ban.py
+++ tests/components/sensor/test_fail2ban.py
@@ -103,7 +103,7 @@ class TestBanSensor(unittest.TestCase):
 
     def test_single_ban(self):
         """Test that log is parsed correctly for single ban."""
-        log_parser = BanLogParser(-1, '/tmp')
+        log_parser = BanLogParser('/tmp')
         sensor = BanSensor('fail2ban', 'jail_one', log_parser)
         self.assertEqual(sensor.name, 'fail2ban jail_one')
         mock_fh = MockOpen(read_data=fake_log('single_ban'))
@@ -124,7 +124,7 @@ class TestBanSensor(unittest.TestCase):
 
     def test_ipv6_ban(self):
         """Test that log is parsed correctly for single ban."""
-        log_parser = BanLogParser(-1, '/tmp')
+        log_parser = BanLogParser('/tmp')
         sensor = BanSensor('fail2ban', 'jail_one', log_parser)
         self.assertEqual(sensor.name, 'fail2ban jail_one')
         mock_fh = MockOpen(read_data=fake_log('ipv6_ban'))
@@ -146,7 +146,7 @@ class TestBanSensor(unittest.TestCase):
 
     def test_multiple_ban(self):
         """Test that log is parsed correctly for multiple ban."""
-        log_parser = BanLogParser(-1, '/tmp')
+        log_parser = BanLogParser('/tmp')
         sensor = BanSensor('fail2ban', 'jail_one', log_parser)
         self.assertEqual(sensor.name, 'fail2ban jail_one')
         mock_fh = MockOpen(read_data=fake_log('multi_ban'))
@@ -169,7 +169,7 @@ class TestBanSensor(unittest.TestCase):
 
     def test_unban_all(self):
         """Test that log is parsed correctly when unbanning."""
-        log_parser = BanLogParser(-1, '/tmp')
+        log_parser = BanLogParser('/tmp')
         sensor = BanSensor('fail2ban', 'jail_one', log_parser)
         self.assertEqual(sensor.name, 'fail2ban jail_one')
         mock_fh = MockOpen(read_data=fake_log('unban_all'))
@@ -186,7 +186,7 @@ class TestBanSensor(unittest.TestCase):
 
     def test_unban_one(self):
         """Test that log is parsed correctly when unbanning one ip."""
-        log_parser = BanLogParser(-1, '/tmp')
+        log_parser = BanLogParser('/tmp')
         sensor = BanSensor('fail2ban', 'jail_one', log_parser)
         self.assertEqual(sensor.name, 'fail2ban jail_one')
         mock_fh = MockOpen(read_data=fake_log('unban_one'))
@@ -206,7 +206,7 @@ class TestBanSensor(unittest.TestCase):
 
     def test_multi_jail(self):
         """Test that log is parsed correctly when using multiple jails."""
-        log_parser = BanLogParser(-1, '/tmp')
+        log_parser = BanLogParser('/tmp')
         sensor1 = BanSensor('fail2ban', 'jail_one', log_parser)
         sensor2 = BanSensor('fail2ban', 'jail_two', log_parser)
         self.assertEqual(sensor1.name, 'fail2ban jail_one')
@@ -240,7 +240,7 @@ class TestBanSensor(unittest.TestCase):
 
     def test_ban_active_after_update(self):
         """Test that ban persists after subsequent update."""
-        log_parser = BanLogParser(-1, '/tmp')
+        log_parser = BanLogParser('/tmp')
         sensor = BanSensor('fail2ban', 'jail_one', log_parser)
         self.assertEqual(sensor.name, 'fail2ban jail_one')
         mock_fh = MockOpen(read_data=fake_log('single_ban'))
