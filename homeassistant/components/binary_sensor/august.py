a1e194943bb7b19d7483dec212059eed159f24f9
ehendrix23 <hendrix_erik@hotmail.com>
2018-10-19 01:37:02 -0600

    Add doorsense sensor for August 3rd Gen Smart Lock Pro (#17299)
    * Add doorsense sensor for August 3rd Gen Smart Lock Pro
    
    Add a binary sensor to August for the August 3rd Gen Smart Lock Pro doorsense sensor.
    
    This is a re-do from PR 17116 https://github.com/home-assistant/home-assistant/pull/17116 that I closed due to rebase issue on my end.
    
    * Changed to use snjoetw provided code
    
    Going through the py-august I found that snjoetw had provided updated versions for the august component (august.py and binary_sensor/august.py) to include DoorSense sensor.
    Changed what I did to to what snjoetw provided instead as he split it into 2 classes; much cleaner I think.
    
    I modified his coding with:
       Fixes that were done to the August component and not part of the coding snjoetw provided.
       Added the debug logging improvement I had done in the code.
    
    Note, fix I committed earlier for lock atribute (lock/august.py) is thus still the same.
    
    * Reverted change from add_device to add_entities
    
    Missed an item when merging snjoetw's code with current. Fixed.
    
    * Updated call from add_devices to add_entities as well
    
    Updated the call from add_devices to add_entities.
    
    * Fixed permissions on files
    
    Fixed permissions on components/august.py and binary_snesor/august.py
    
    * Changed if/else to if/continue
    
    Changed logic so that if the door sensor state is unknown during initalization the debug log is written and then continue the loop instead of using if/else logic.
    
    * Added available property for Door Sensor
    
    Added the available property for the Door Sensor and setting it to False if a status unknown is received.
    
    * Updated setting self._available
    
    Changed line for setting self._available to what Martin provided. Much  more efficient to read. :-)

--- homeassistant/components/binary_sensor/august.py
+++ homeassistant/components/binary_sensor/august.py
@@ -4,14 +4,24 @@ Support for August binary sensors.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.august/
 """
+import logging
 from datetime import timedelta, datetime
 
 from homeassistant.components.august import DATA_AUGUST
 from homeassistant.components.binary_sensor import (BinarySensorDevice)
 
+_LOGGER = logging.getLogger(__name__)
+
 DEPENDENCIES = ['august']
 
 SCAN_INTERVAL = timedelta(seconds=5)
+
+
+def _retrieve_door_state(data, lock):
+    """Get the latest state of the DoorSense sensor."""
+    from august.lock import LockDoorStatus
+    doorstate = data.get_door_state(lock.device_id)
+    return doorstate == LockDoorStatus.OPEN
 
 
 def _retrieve_online_state(data, doorbell):
@@ -46,7 +56,11 @@ def _activity_time_based_state(data, doorbell, activity_types):
 
 
 # Sensor types: Name, device_class, state_provider
-SENSOR_TYPES = {
+SENSOR_TYPES_DOOR = {
+    'door_open': ['Open', 'door', _retrieve_door_state],
+}
+
+SENSOR_TYPES_DOORBELL = {
     'doorbell_ding': ['Ding', 'occupancy', _retrieve_ding_state],
     'doorbell_motion': ['Motion', 'motion', _retrieve_motion_state],
     'doorbell_online': ['Online', 'connectivity', _retrieve_online_state],
@@ -58,14 +72,78 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     data = hass.data[DATA_AUGUST]
     devices = []
 
+    from august.lock import LockDoorStatus
+    for door in data.locks:
+        for sensor_type in SENSOR_TYPES_DOOR:
+            state_provider = SENSOR_TYPES_DOOR[sensor_type][2]
+            if state_provider(data, door) is LockDoorStatus.UNKNOWN:
+                _LOGGER.debug(
+                    "Not adding sensor class %s for lock %s ",
+                    SENSOR_TYPES_DOOR[sensor_type][1], door.device_name
+                )
+                continue
+
+            _LOGGER.debug(
+                "Adding sensor class %s for %s",
+                SENSOR_TYPES_DOOR[sensor_type][1], door.device_name
+            )
+            devices.append(AugustDoorBinarySensor(data, sensor_type, door))
+
     for doorbell in data.doorbells:
-        for sensor_type in SENSOR_TYPES:
-            devices.append(AugustBinarySensor(data, sensor_type, doorbell))
+        for sensor_type in SENSOR_TYPES_DOORBELL:
+            _LOGGER.debug("Adding doorbell sensor class %s for %s",
+                          SENSOR_TYPES_DOORBELL[sensor_type][1],
+                          doorbell.device_name)
+            devices.append(
+                AugustDoorbellBinarySensor(data, sensor_type,
+                                           doorbell)
+            )
 
     add_entities(devices, True)
 
 
-class AugustBinarySensor(BinarySensorDevice):
+class AugustDoorBinarySensor(BinarySensorDevice):
+    """Representation of an August Door binary sensor."""
+
+    def __init__(self, data, sensor_type, door):
+        """Initialize the sensor."""
+        self._data = data
+        self._sensor_type = sensor_type
+        self._door = door
+        self._state = None
+        self._available = False
+
+    @property
+    def available(self):
+        """Return the availability of this sensor."""
+        return self._available
+
+    @property
+    def is_on(self):
+        """Return true if the binary sensor is on."""
+        return self._state
+
+    @property
+    def device_class(self):
+        """Return the class of this device, from component DEVICE_CLASSES."""
+        return SENSOR_TYPES_DOOR[self._sensor_type][1]
+
+    @property
+    def name(self):
+        """Return the name of the binary sensor."""
+        return "{} {}".format(self._door.device_name,
+                              SENSOR_TYPES_DOOR[self._sensor_type][0])
+
+    def update(self):
+        """Get the latest state of the sensor."""
+        state_provider = SENSOR_TYPES_DOOR[self._sensor_type][2]
+        self._state = state_provider(self._data, self._door)
+
+        from august.lock import LockDoorStatus
+        self._available = self._state != LockDoorStatus.UNKNOWN
+
+
+class AugustDoorbellBinarySensor(BinarySensorDevice):
     """Representation of an August binary sensor."""
 
     def __init__(self, data, sensor_type, doorbell):
@@ -83,15 +161,15 @@ class AugustBinarySensor(BinarySensorDevice):
     @property
     def device_class(self):
         """Return the class of this device, from component DEVICE_CLASSES."""
-        return SENSOR_TYPES[self._sensor_type][1]
+        return SENSOR_TYPES_DOORBELL[self._sensor_type][1]
 
     @property
     def name(self):
         """Return the name of the binary sensor."""
         return "{} {}".format(self._doorbell.device_name,
-                              SENSOR_TYPES[self._sensor_type][0])
+                              SENSOR_TYPES_DOORBELL[self._sensor_type][0])
 
     def update(self):
         """Get the latest state of the sensor."""
-        state_provider = SENSOR_TYPES[self._sensor_type][2]
+        state_provider = SENSOR_TYPES_DOORBELL[self._sensor_type][2]
         self._state = state_provider(self._data, self._doorbell)
