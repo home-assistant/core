a7f5bdd93886def5fd55efc1e692a2ed6851f321
mvn23 <schopdiedwaas@gmail.com>
2018-10-19 07:31:19 +0200

    Add sensor support to opentherm_gw (#17314)
    * Add OpenTherm Gateway sensor platform.
    
    * Add OTGW_ variables to list of supported sensors.
    
    * Order imports.
    
    * Add OpenTherm Gateway binary sensor support.
    
    * Revert "Add OpenTherm Gateway binary sensor support."
    
    This reverts commit 115acaa912f4f9ff47408a04f4a770bebd071f26.
    
    * Import COMP_SENSOR from sensor component rather than defining it.
    
    * Update opentherm_gw sensor platform docs url.
    
    * Update dependency to v0.2b1
    Old version had incorrect variable names for some of the sensors
    
    * Update requirements_all.txt
    
    * Address review findings.
    
    * Update .coveragerc

--- homeassistant/components/opentherm_gw.py
+++ homeassistant/components/opentherm_gw.py
@@ -8,8 +8,10 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.const import (CONF_DEVICE, CONF_NAME, PRECISION_HALVES,
-                                 PRECISION_TENTHS, PRECISION_WHOLE)
+from homeassistant.components.sensor import DOMAIN as COMP_SENSOR
+from homeassistant.const import (CONF_DEVICE, CONF_MONITORED_VARIABLES,
+                                 CONF_NAME, PRECISION_HALVES, PRECISION_TENTHS,
+                                 PRECISION_WHOLE)
 from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 
@@ -38,10 +40,12 @@ CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_DEVICE): cv.string,
         vol.Optional(CONF_CLIMATE, default={}): CLIMATE_SCHEMA,
+        vol.Optional(CONF_MONITORED_VARIABLES, default=[]): vol.All(
+            cv.ensure_list, [cv.string]),
     }),
 }, extra=vol.ALLOW_EXTRA)
 
-REQUIREMENTS = ['pyotgw==0.1b0']
+REQUIREMENTS = ['pyotgw==0.2b1']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -59,6 +63,8 @@ async def async_setup(hass, config):
         hass, conf[CONF_DEVICE], gateway))
     hass.async_create_task(async_load_platform(
         hass, 'climate', DOMAIN, conf.get(CONF_CLIMATE)))
+    hass.async_create_task(setup_monitored_vars(
+        hass, conf.get(CONF_MONITORED_VARIABLES)))
     return True
 
 
@@ -72,3 +78,85 @@ async def connect_and_subscribe(hass, device_path, gateway):
         _LOGGER.debug("Received report: %s", status)
         async_dispatcher_send(hass, SIGNAL_OPENTHERM_GW_UPDATE, status)
     gateway.subscribe(handle_report)
+
+
+async def setup_monitored_vars(hass, monitored_vars):
+    """Set up requested sensors."""
+    gw_vars = hass.data[DATA_OPENTHERM_GW][DATA_GW_VARS]
+    # Use dict to prepare for binary sensor support.
+    sensor_type_map = {
+        COMP_SENSOR: [
+            gw_vars.DATA_CONTROL_SETPOINT,
+            gw_vars.DATA_MASTER_MEMBERID,
+            gw_vars.DATA_SLAVE_MEMBERID,
+            gw_vars.DATA_SLAVE_OEM_FAULT,
+            gw_vars.DATA_COOLING_CONTROL,
+            gw_vars.DATA_CONTROL_SETPOINT_2,
+            gw_vars.DATA_ROOM_SETPOINT_OVRD,
+            gw_vars.DATA_SLAVE_MAX_RELATIVE_MOD,
+            gw_vars.DATA_SLAVE_MAX_CAPACITY,
+            gw_vars.DATA_SLAVE_MIN_MOD_LEVEL,
+            gw_vars.DATA_ROOM_SETPOINT,
+            gw_vars.DATA_REL_MOD_LEVEL,
+            gw_vars.DATA_CH_WATER_PRESS,
+            gw_vars.DATA_DHW_FLOW_RATE,
+            gw_vars.DATA_ROOM_SETPOINT_2,
+            gw_vars.DATA_ROOM_TEMP,
+            gw_vars.DATA_CH_WATER_TEMP,
+            gw_vars.DATA_DHW_TEMP,
+            gw_vars.DATA_OUTSIDE_TEMP,
+            gw_vars.DATA_RETURN_WATER_TEMP,
+            gw_vars.DATA_SOLAR_STORAGE_TEMP,
+            gw_vars.DATA_SOLAR_COLL_TEMP,
+            gw_vars.DATA_CH_WATER_TEMP_2,
+            gw_vars.DATA_DHW_TEMP_2,
+            gw_vars.DATA_EXHAUST_TEMP,
+            gw_vars.DATA_SLAVE_DHW_MAX_SETP,
+            gw_vars.DATA_SLAVE_DHW_MIN_SETP,
+            gw_vars.DATA_SLAVE_CH_MAX_SETP,
+            gw_vars.DATA_SLAVE_CH_MIN_SETP,
+            gw_vars.DATA_DHW_SETPOINT,
+            gw_vars.DATA_MAX_CH_SETPOINT,
+            gw_vars.DATA_OEM_DIAG,
+            gw_vars.DATA_TOTAL_BURNER_STARTS,
+            gw_vars.DATA_CH_PUMP_STARTS,
+            gw_vars.DATA_DHW_PUMP_STARTS,
+            gw_vars.DATA_DHW_BURNER_STARTS,
+            gw_vars.DATA_TOTAL_BURNER_HOURS,
+            gw_vars.DATA_CH_PUMP_HOURS,
+            gw_vars.DATA_DHW_PUMP_HOURS,
+            gw_vars.DATA_DHW_BURNER_HOURS,
+            gw_vars.DATA_MASTER_OT_VERSION,
+            gw_vars.DATA_SLAVE_OT_VERSION,
+            gw_vars.DATA_MASTER_PRODUCT_TYPE,
+            gw_vars.DATA_MASTER_PRODUCT_VERSION,
+            gw_vars.DATA_SLAVE_PRODUCT_TYPE,
+            gw_vars.DATA_SLAVE_PRODUCT_VERSION,
+            gw_vars.OTGW_MODE,
+            gw_vars.OTGW_DHW_OVRD,
+            gw_vars.OTGW_ABOUT,
+            gw_vars.OTGW_BUILD,
+            gw_vars.OTGW_CLOCKMHZ,
+            gw_vars.OTGW_LED_A,
+            gw_vars.OTGW_LED_B,
+            gw_vars.OTGW_LED_C,
+            gw_vars.OTGW_LED_D,
+            gw_vars.OTGW_LED_E,
+            gw_vars.OTGW_LED_F,
+            gw_vars.OTGW_GPIO_A,
+            gw_vars.OTGW_GPIO_B,
+            gw_vars.OTGW_SB_TEMP,
+            gw_vars.OTGW_SETP_OVRD_MODE,
+            gw_vars.OTGW_SMART_PWR,
+            gw_vars.OTGW_THRM_DETECT,
+            gw_vars.OTGW_VREF,
+        ]
+    }
+    sensors = []
+    for var in monitored_vars:
+        if var in sensor_type_map[COMP_SENSOR]:
+            sensors.append(var)
+        else:
+            _LOGGER.error("Monitored variable not supported: %s", var)
+    if sensors:
+        await async_load_platform(hass, COMP_SENSOR, DOMAIN, sensors)
