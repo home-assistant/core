975da399ea7aaa1e4ca8e841baf3271f3c5029fd
Julien Debaru <Debaru@users.noreply.github.com>
2018-10-20 15:30:10 +0200

    Fix linky sensor login error (#17110)
    * Fix linky sensor login error
    
    * Make platform fail-safe
    
    Adding following enhancements:
    * Make sure the platform loads correctly by making the first API request in setup_platform.
    * Close the session after each API call.
    * Use timeout parameter everywhere.
    
    * Fix Hound CI error: line too long.
    
    * Update pylinky library
    
    * Remove LinkyClient from update()

--- homeassistant/components/sensor/linky.py
+++ homeassistant/components/sensor/linky.py
@@ -16,7 +16,7 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pylinky==0.1.6']
+REQUIREMENTS = ['pylinky==0.1.8']
 _LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(minutes=10)
@@ -37,11 +37,11 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     from pylinky.client import LinkyClient, PyLinkyError
     client = LinkyClient(username, password, None, timeout)
-
     try:
         client.fetch_data()
     except PyLinkyError as exp:
         _LOGGER.error(exp)
+        client.close_session()
         return
 
     devices = [LinkySensor('Linky', client)]
@@ -80,6 +80,7 @@ class LinkySensor(Entity):
             self._client.fetch_data()
         except PyLinkyError as exp:
             _LOGGER.error(exp)
+            self._client.close_session()
             return
 
         _LOGGER.debug(json.dumps(self._client.get_data(), indent=2))
