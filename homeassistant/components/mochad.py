c0ec3e354a7a3e8cfe5044e5042759be9941c0f4
Alex Osadchyy <aosadchyy@outlook.com>
2018-10-21 21:21:27 -0700

    From synch to asynch. Read out the receiving mochad buffer for stability and responsiveness

--- homeassistant/components/mochad.py
+++ homeassistant/components/mochad.py
@@ -6,7 +6,8 @@ https://home-assistant.io/components/mochad/
 """
 import logging
 import threading
-
+import asyncio
+import time
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
@@ -34,7 +35,8 @@ CONFIG_SCHEMA = vol.Schema({
 }, extra=vol.ALLOW_EXTRA)
 
 
-def setup(hass, config):
+@asyncio.coroutine
+def async_setup(hass, config):
     """Set up the mochad component."""
     conf = config[DOMAIN]
     host = conf.get(CONF_HOST)
@@ -55,13 +57,16 @@ def setup(hass, config):
 
     def start_mochad(event):
         """Start the Mochad service."""
-        hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, stop_mochad)
-    hass.bus.listen_once(EVENT_HOMEASSISTANT_START, start_mochad)
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, stop_mochad)
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, start_mochad)
 
-    return True
+    """Start listening thread for Mochad data."""
+    CONTROLLER.start()
+
+    return True 
 
 
-class MochadCtrl:
+class MochadCtrl(threading.Thread):
     """Mochad controller."""
 
     def __init__(self, host, port):
@@ -73,7 +78,11 @@ class MochadCtrl:
         from pymochad import controller
 
         self.ctrl = controller.PyMochad(server=self._host, port=self._port)
-
+        super(MochadCtrl, self).__init__()
+    
+    def run(self):
+        self._ws_listen()
+    
     @property
     def host(self):
         """Return the server where mochad is running."""
@@ -87,3 +96,33 @@ class MochadCtrl:
     def disconnect(self):
         """Close the connection to the mochad socket."""
         self.ctrl.socket.close()
+
+    def _ws_listen(self):
+        """Read the data in a loop to exhaust receiving buffer of Mochad."""
+        _LOGGER.info("Entering Mochad event listening loop")
+        try:
+            # READ FROM NETWORK LOOP
+            retry_count=0
+            while True:
+                try:
+                    content = self.ctrl.read_data()
+                    if content:
+                        retry_count=0
+                except Exception as e:
+                    _LOGGER.error(
+                        "Failed to read from the socket. {}".format(e))
+                    if retry_count>=300:
+                        raise Exception(
+                            "Retry attempts exceeded. Failed to read for the socket.")
+                    else:
+                        retry_count+=1
+                    time.sleep(10)
+                    content=""
+
+        except Exception as e:
+            _LOGGER.error("Failed to read from the socket. {}".format(e))
+        finally:
+            _LOGGER.error(
+                "Loop exited. No more X10 msgs will be received from Mochad.")
+            if self.ctrl.socket:
+                self.disconnect()
