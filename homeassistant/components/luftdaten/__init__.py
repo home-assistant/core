f4b04842c6b9cb1c2dd7fb17d9f0ddb7e144863a
Fabian Affolter <mail@fabian-affolter.ch>
2018-10-22 23:20:36 +0200

    Enable config flow

--- homeassistant/components/luftdaten/__init__.py
+++ homeassistant/components/luftdaten/__init__.py
@@ -1,155 +1,200 @@
 """
-Support for Luftdaten sensors.
+Support for Luftdaten stations.
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/sensor.luftdaten/
+https://home-assistant.io/components/luftdaten/
 """
-from datetime import timedelta
 import logging
 
 import voluptuous as vol
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.config_entries import SOURCE_IMPORT
 from homeassistant.const import (
-    ATTR_ATTRIBUTION, ATTR_LATITUDE, ATTR_LONGITUDE, CONF_MONITORED_CONDITIONS,
-    CONF_NAME, CONF_SHOW_ON_MAP, TEMP_CELSIUS)
+    ATTR_ATTRIBUTION, CONF_MONITORED_CONDITIONS, CONF_SCAN_INTERVAL,
+    CONF_SENSORS, CONF_SHOW_ON_MAP, TEMP_CELSIUS)
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.dispatcher import async_dispatcher_send
 from homeassistant.helpers.entity import Entity
-from homeassistant.util import Throttle
+from homeassistant.helpers.event import async_track_time_interval
 
-REQUIREMENTS = ['luftdaten==0.2.0']
+from .config_flow import configured_sensors
+from .const import CONF_SENSOR_ID, DEFAULT_SCAN_INTERVAL, DOMAIN
+
+REQUIREMENTS = ['luftdaten==0.3.4']
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_SENSOR_ID = 'sensor_id'
+DATA_LUFTDATEN = 'luftdaten'
+DATA_LUFTDATEN_CLIENT = 'data_luftdaten_client'
+DATA_LUFTDATEN_LISTENER = 'data_luftdaten_listener'
+DEFAULT_ATTRIBUTION = "Data provided by luftdaten.info"
 
-CONF_ATTRIBUTION = "Data provided by luftdaten.info"
+NOTIFICATION_ID = 'luftdaten_notification'
+NOTIFICATION_TITLE = 'Luftdaten Component Setup'
 
-
-VOLUME_MICROGRAMS_PER_CUBIC_METER = 'µg/m3'
-
-SENSOR_TEMPERATURE = 'temperature'
 SENSOR_HUMIDITY = 'humidity'
 SENSOR_PM10 = 'P1'
 SENSOR_PM2_5 = 'P2'
 SENSOR_PRESSURE = 'pressure'
+SENSOR_TEMPERATURE = 'temperature'
 
-SENSOR_TYPES = {
-    SENSOR_TEMPERATURE: ['Temperature', TEMP_CELSIUS],
-    SENSOR_HUMIDITY: ['Humidity', '%'],
-    SENSOR_PRESSURE: ['Pressure', 'Pa'],
-    SENSOR_PM10: ['PM10', VOLUME_MICROGRAMS_PER_CUBIC_METER],
-    SENSOR_PM2_5: ['PM2.5', VOLUME_MICROGRAMS_PER_CUBIC_METER]
+TOPIC_UPDATE = '{0}_data_update'.format(DOMAIN)
+
+VOLUME_MICROGRAMS_PER_CUBIC_METER = 'µg/m3'
+
+SENSORS = {
+    SENSOR_TEMPERATURE: ['Temperature', 'mdi:thermometer', TEMP_CELSIUS],
+    SENSOR_HUMIDITY: ['Humidity', 'mdi:water-percent', '%'],
+    SENSOR_PRESSURE: ['Pressure', 'mdi:arrow-down-bold', 'Pa'],
+    SENSOR_PM10: ['PM10', 'mdi:thought-bubble',
+                  VOLUME_MICROGRAMS_PER_CUBIC_METER],
+    SENSOR_PM2_5: ['PM2.5', 'mdi:thought-bubble-outline',
+                   VOLUME_MICROGRAMS_PER_CUBIC_METER]
 }
 
-DEFAULT_NAME = 'Luftdaten'
-
-CONF_SENSORID = 'sensorid'
-
-MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_SENSORID): cv.positive_int,
-    vol.Required(CONF_MONITORED_CONDITIONS):
-        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_SHOW_ON_MAP, default=False): cv.boolean,
+SENSOR_SCHEMA = vol.Schema({
+    vol.Optional(CONF_MONITORED_CONDITIONS, default=list(SENSORS)):
+        vol.All(cv.ensure_list, [vol.In(SENSORS)])
 })
 
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN:
+        vol.Schema({
+            vol.Required(CONF_SENSOR_ID): cv.positive_int,
+            vol.Optional(CONF_SENSORS, default={}): SENSOR_SCHEMA,
+            vol.Optional(CONF_SHOW_ON_MAP, default=False): cv.boolean,
+            vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL):
+                cv.time_period,
+        })
+}, extra=vol.ALLOW_EXTRA)
 
-async def async_setup_platform(
-        hass, config, async_add_entities, discovery_info=None):
-    """Set up the Luftdaten sensor."""
+
+async def async_setup(hass, config):
+    """Set up the Luftdaten component."""
+    hass.data[DOMAIN] = {}
+    hass.data[DOMAIN][DATA_LUFTDATEN_CLIENT] = {}
+    hass.data[DOMAIN][DATA_LUFTDATEN_LISTENER] = {}
+
+    if DOMAIN not in config:
+        return True
+
+    conf = config[DOMAIN]
+    station_id = conf.get(CONF_SENSOR_ID)
+
+    if station_id not in configured_sensors(hass):
+        hass.async_add_job(
+            hass.config_entries.flow.async_init(
+                DOMAIN,
+                context={'source': SOURCE_IMPORT},
+                data={
+                    CONF_SENSORS: conf[CONF_SENSORS],
+                    CONF_SENSOR_ID: conf[CONF_SENSOR_ID],
+                    CONF_SHOW_ON_MAP: conf[CONF_SHOW_ON_MAP],
+                }
+            )
+        )
+
+    hass.data[DOMAIN][CONF_SCAN_INTERVAL] = conf[CONF_SCAN_INTERVAL]
+
+    return True
+
+
+async def async_setup_entry(hass, config_entry):
+    """Set up Luftdaten as config entry."""
     from luftdaten import Luftdaten
-
-    name = config.get(CONF_NAME)
-    show_on_map = config.get(CONF_SHOW_ON_MAP)
-    sensor_id = config.get(CONF_SENSORID)
+    from luftdaten.exceptions import LuftdatenError
 
     session = async_get_clientsession(hass)
-    luftdaten = LuftdatenData(Luftdaten(sensor_id, hass.loop, session))
 
-    await luftdaten.async_update()
+    try:
+        luftdaten = LuftDatenData(
+            Luftdaten(
+                config_entry.data[CONF_SENSOR_ID], hass.loop, session),
+            config_entry.data.get(CONF_SENSORS, {}).get(
+                    CONF_MONITORED_CONDITIONS, list(SENSORS)))
+        await luftdaten.async_update()
+        hass.data[DOMAIN][DATA_LUFTDATEN_CLIENT][config_entry.entry_id] = \
+            luftdaten
+    except LuftdatenError as err:
+        _LOGGER.error("An error occurred: %s", str(err))
+        hass.components.persistent_notification.create(
+            'Error: {0}<br />'
+            'You will need to restart Home Assistant after fixing.'
+            ''.format(err),
+            title=NOTIFICATION_TITLE,
+            notification_id=NOTIFICATION_ID)
+        return False
 
-    if luftdaten.data is None:
-        _LOGGER.error("Sensor is not available: %s", sensor_id)
-        return
+    hass.async_create_task(hass.config_entries.async_forward_entry_setup(
+        config_entry, 'sensor'))
 
-    devices = []
-    for variable in config[CONF_MONITORED_CONDITIONS]:
-        if luftdaten.data.values[variable] is None:
-            _LOGGER.warning("It might be that sensor %s is not providing "
-                            "measurements for %s", sensor_id, variable)
-        devices.append(
-            LuftdatenSensor(luftdaten, name, variable, sensor_id, show_on_map))
+    async def refresh_sensors(event_time):
+        """Refresh Luftdaten data."""
+        await luftdaten.async_update()
+        async_dispatcher_send(hass, TOPIC_UPDATE)
 
-    async_add_entities(devices)
+    hass.data[DOMAIN][DATA_LUFTDATEN_LISTENER][
+        config_entry.entry_id] = async_track_time_interval(
+            hass, refresh_sensors,
+            hass.data[DOMAIN].get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL))
+
+    return True
 
 
-class LuftdatenSensor(Entity):
-    """Implementation of a Luftdaten sensor."""
+async def async_unload_entry(hass, config_entry):
+    """Unload an Luftdaten config entry."""
+    for component in ('sensor', ):
+        await hass.config_entries.async_forward_entry_unload(
+            config_entry, component)
 
-    def __init__(self, luftdaten, name, sensor_type, sensor_id, show):
-        """Initialize the Luftdaten sensor."""
+    hass.data[DOMAIN][DATA_LUFTDATEN_CLIENT].pop(config_entry.entry_id)
+
+    remove_listener = hass.data[DOMAIN][DATA_LUFTDATEN_LISTENER].pop(
+        config_entry.entry_id)
+    remove_listener()
+
+    return True
+
+
+class LuftDatenData:
+    """Define a generic Luftdaten object."""
+
+    def __init__(self, client, sensor_conditions):
+        """Initialize the Luftdata object."""
+        self.client = client
+        self.data = {}
+        self.sensor_conditions = sensor_conditions
+
+    async def async_update(self):
+        """Update sensor/binary sensor data."""
+        from luftdaten.exceptions import LuftdatenError
+
+        try:
+            await self.client.get_data()
+
+            self.data[DATA_LUFTDATEN] = self.client.values
+            self.data[DATA_LUFTDATEN].update(self.client.meta)
+
+        except LuftdatenError:
+            _LOGGER.error("Unable to retrieve data from luftdaten.info")
+
+
+class LuftDatenEntity(Entity):
+    """Define a generic Luftdaten entity."""
+
+    def __init__(self, luftdaten):
+        """Initialize an Luftdaten entity."""
+        self._attrs = {ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION}
+        self._name = None
         self.luftdaten = luftdaten
-        self._name = name
-        self._state = None
-        self._sensor_id = sensor_id
-        self.sensor_type = sensor_type
-        self._unit_of_measurement = SENSOR_TYPES[sensor_type][1]
-        self._show_on_map = show
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return '{} {}'.format(self._name, SENSOR_TYPES[self.sensor_type][0])
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return self.luftdaten.data.values[self.sensor_type]
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return self._unit_of_measurement
 
     @property
     def device_state_attributes(self):
         """Return the state attributes."""
-        onmap = ATTR_LATITUDE, ATTR_LONGITUDE
-        nomap = 'lat', 'long'
-        lat_format, lon_format = onmap if self._show_on_map else nomap
-        try:
-            attr = {
-                ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
-                ATTR_SENSOR_ID: self._sensor_id,
-                lat_format: self.luftdaten.data.meta['latitude'],
-                lon_format: self.luftdaten.data.meta['longitude'],
-            }
-            return attr
-        except KeyError:
-            return
+        return self._attrs
 
-    async def async_update(self):
-        """Get the latest data from luftdaten.info and update the state."""
-        await self.luftdaten.async_update()
-
-
-class LuftdatenData:
-    """Class for handling the data retrieval."""
-
-    def __init__(self, data):
-        """Initialize the data object."""
-        self.data = data
-
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    async def async_update(self):
-        """Get the latest data from luftdaten.info."""
-        from luftdaten.exceptions import LuftdatenError
-
-        try:
-            await self.data.async_get_data()
-        except LuftdatenError:
-            _LOGGER.error("Unable to retrieve data from luftdaten.info")
+    @property
+    def name(self):
+        """Return the name of the entity."""
+        return self._name
---
+++ homeassistant/components/luftdaten/config_flow.py
@@ -0,0 +1,80 @@
+"""Config flow to configure the Luftdaten component."""
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_SCAN_INTERVAL, CONF_SHOW_ON_MAP
+from homeassistant.core import callback
+from homeassistant.helpers import aiohttp_client
+
+from .const import CONF_SENSOR_ID, DEFAULT_SCAN_INTERVAL, DOMAIN
+
+
+@callback
+def configured_sensors(hass):
+    """Return a set of configured Luftdaten sensors."""
+    return set(
+        '{0}'.format(entry.data[CONF_SENSOR_ID])
+        for entry in hass.config_entries.async_entries(DOMAIN))
+
+
+@config_entries.HANDLERS.register(DOMAIN)
+class LuftDatenFlowHandler(config_entries.ConfigFlow):
+    """Handle a Luftdaten config flow."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    def __init__(self):
+        """Initialize the config flow."""
+        pass
+
+    async def _show_form(self, errors=None):
+        """Show the form to the user."""
+        data_schema = vol.Schema({
+            vol.Required(CONF_SENSOR_ID): str,
+        })
+
+        return self.async_show_form(
+            step_id='user',
+            data_schema=data_schema,
+            errors=errors if errors else {},
+        )
+
+    async def async_step_import(self, import_config):
+        """Import a config entry from configuration.yaml."""
+        return await self.async_step_user(import_config)
+
+    async def async_step_user(self, user_input=None):
+        """Handle the start of the config flow."""
+        from luftdaten import Luftdaten, exceptions
+
+        if not user_input:
+            return await self._show_form()
+
+        sensor_id = user_input[CONF_SENSOR_ID]
+
+        if sensor_id in configured_sensors(self.hass):
+            return await self._show_form({CONF_SENSOR_ID: 'sensor_exists'})
+
+        session = aiohttp_client.async_get_clientsession(self.hass)
+        luftdaten = Luftdaten(
+            user_input[CONF_SENSOR_ID], self.hass.loop, session)
+        try:
+            await luftdaten.get_data()
+            valid = await luftdaten.validate_sensor()
+        except exceptions.LuftdatenConnectionError:
+            return await self._show_form(
+                {CONF_SENSOR_ID: 'communication_error'})
+
+        if not valid:
+            return await self._show_form({CONF_SENSOR_ID: 'invalid_sensor'})
+
+        scan_interval = user_input.get(
+            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
+        user_input.update({
+            CONF_SCAN_INTERVAL: scan_interval.seconds,
+            CONF_SHOW_ON_MAP: False,
+            CONF_SENSOR_ID: sensor_id,
+        })
+
+        return self.async_create_entry(title=sensor_id, data=user_input)
