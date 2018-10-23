3aa7cd0f7d04e82d38a06bba4640d3a807f7495a
Rohan Kapoor <rohan@rohankapoor.com>
2018-10-23 02:14:46 -0700

    Migrate Mailgun to use the webhook component (#17464)
    * Switch mailgun to use webhook api
    
    * Generalize webhook_config_entry_flow
    
    * Add tests for webhook_config_entry_flow
    
    * Add tests for mailgun
    
    * Remove old mailgun file from .coveragerc
    
    * Refactor WebhookFlowHandler into config_entry_flow
    
    * Remove test of helper func from IFTTT
    
    * Lint

--- tests/components/mailgun/test_init.py
+++ tests/components/mailgun/test_init.py
@@ -1,15 +1,16 @@
-"""Test the init file of IFTTT."""
+"""Test the init file of Mailgun."""
 from unittest.mock import patch
 
 from homeassistant import data_entry_flow
+from homeassistant.components import mailgun
+
 from homeassistant.core import callback
-from homeassistant.components import ifttt
 
 
 async def test_config_flow_registers_webhook(hass, aiohttp_client):
-    """Test setting up IFTTT and sending webhook."""
+    """Test setting up Mailgun and sending webhook."""
     with patch('homeassistant.util.get_local_ip', return_value='example.com'):
-        result = await hass.config_entries.flow.async_init('ifttt', context={
+        result = await hass.config_entries.flow.async_init('mailgun', context={
             'source': 'user'
         })
     assert result['type'] == data_entry_flow.RESULT_TYPE_FORM, result
@@ -19,20 +20,20 @@ async def test_config_flow_registers_webhook(hass, aiohttp_client):
     assert result['type'] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     webhook_id = result['result'].data['webhook_id']
 
-    ifttt_events = []
+    mailgun_events = []
 
     @callback
     def handle_event(event):
-        """Handle IFTTT event."""
-        ifttt_events.append(event)
+        """Handle Mailgun event."""
+        mailgun_events.append(event)
 
-    hass.bus.async_listen(ifttt.EVENT_RECEIVED, handle_event)
+    hass.bus.async_listen(mailgun.MESSAGE_RECEIVED, handle_event)
 
     client = await aiohttp_client(hass.http.app)
-    await client.post('/api/webhook/{}'.format(webhook_id), json={
-        'hello': 'ifttt'
+    await client.post('/api/webhook/{}'.format(webhook_id), data={
+        'hello': 'mailgun'
     })
 
-    assert len(ifttt_events) == 1
-    assert ifttt_events[0].data['webhook_id'] == webhook_id
-    assert ifttt_events[0].data['hello'] == 'ifttt'
+    assert len(mailgun_events) == 1
+    assert mailgun_events[0].data['webhook_id'] == webhook_id
+    assert mailgun_events[0].data['hello'] == 'mailgun'
