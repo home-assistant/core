edf85ec1b82b67968a3cf0141df51e6f6aa0c1c1
Jaxom Nutt <40261038+JaxomCS@users.noreply.github.com>
2018-10-23 14:20:30 +0800

    Amendment
    Changed it to use 'hass' as the default instead of defaulting to the recipient which is the array. Would have worked if users set their own name but users who were using the default were experiencing the issue.

--- homeassistant/components/notify/clicksend.py
+++ homeassistant/components/notify/clicksend.py
@@ -29,7 +29,7 @@ def validate_sender(config):
     """Set the optional sender name if sender name is not provided."""
     if CONF_SENDER in config:
         return config
-    config[CONF_SENDER] = config[CONF_RECIPIENT]
+    config[CONF_SENDER] = 'hass'
     return config
 
 
@@ -61,7 +61,7 @@ class ClicksendNotificationService(BaseNotificationService):
         self.username = config.get(CONF_USERNAME)
         self.api_key = config.get(CONF_API_KEY)
         self.recipients = config.get(CONF_RECIPIENT)
-        self.sender = config.get(CONF_SENDER, CONF_RECIPIENT)
+        self.sender = config.get(CONF_SENDER)
 
     def send_message(self, message="", **kwargs):
         """Send a message to a user."""
@@ -69,7 +69,7 @@ class ClicksendNotificationService(BaseNotificationService):
         for recipient in self.recipients:
             data["messages"].append({
                 'source': 'hass.notify',
-                'from': 'hass',
+                'from': self.sender,
                 'to': recipient,
                 'body': message,
             })
