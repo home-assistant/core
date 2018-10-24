0c5cd724e8b7e8fcd6a3092168cfdc7ef958e901
Charles Garwood <cgarwood@newdealmultimedia.com>
2018-10-24 05:53:45 -0400

    Add cover to supported platforms (#17725)

--- homeassistant/components/zwave/__init__.py
+++ homeassistant/components/zwave/__init__.py
@@ -66,7 +66,7 @@ DEFAULT_CONF_INVERT_OPENCLOSE_BUTTONS = False
 DEFAULT_CONF_REFRESH_VALUE = False
 DEFAULT_CONF_REFRESH_DELAY = 5
 
-SUPPORTED_PLATFORMS = ['binary_sensor', 'climate', 'fan',
+SUPPORTED_PLATFORMS = ['binary_sensor', 'climate', 'cover', 'fan',
                        'light', 'sensor', 'switch']
 
 RENAME_NODE_SCHEMA = vol.Schema({
