b2a4b23df5e279e34b9574b1df101a33165fbe37
François LEGAL <francois@DevLinux>
2018-10-24 15:21:10 +0200

    Tested by simulation, awaiting real test

---
+++ tests/components/test_legrandinone.py
@@ -0,0 +1,232 @@
+"""Common functions for legrandinone component tests and generic platform tests."""
+
+import asyncio
+from collections import defaultdict
+from typing import Any, Dict, cast
+from unittest.mock import Mock
+
+from homeassistant.bootstrap import async_setup_component
+from homeassistant.components.legrandinone import (
+    CONF_RECONNECT_INTERVAL, SERVICE_SEND_COMMAND)
+from homeassistant.const import (
+    ATTR_ENTITY_ID, SERVICE_OPEN_COVER, SERVICE_STOP_COVER)
+
+
+@asyncio.coroutine
+def mock_legrandinone(hass, config, domain, monkeypatch, failures=None):
+    """Create mock legrandinone asyncio protocol, test component setup."""
+    transport, protocol = (Mock(), Mock())
+
+    @asyncio.coroutine
+    def send_command(*command):
+        return True
+    protocol.send_command = Mock(wraps=send_command)
+
+    @asyncio.coroutine
+    def create_iobl_connection(*args, **kwargs):
+        """Return mocked transport and protocol."""
+        # failures can be a list of booleans indicating in which sequence
+        # creating a connection should success or fail
+        if failures:
+            fail = failures.pop()
+        else:
+            fail = False
+
+        if fail:
+            raise ConnectionRefusedError
+        else:
+            return transport, protocol
+
+    mock_create = Mock(wraps=create_iobl_connection)
+    monkeypatch.setattr(
+        'iobl.protocol.create_iobl_connection',
+        mock_create)
+
+    yield from async_setup_component(hass, domain, config)
+
+    # hook into mock config for injecting events
+    event_callback = mock_create.call_args_list[0][1]['event_callback']
+    assert event_callback
+
+    disconnect_callback = mock_create.call_args_list[0][1]['disconnect_callback']
+
+    return event_callback, mock_create, protocol, disconnect_callback
+
+
+@asyncio.coroutine
+def test_send_command(hass, monkeypatch):
+    """Test command sending."""
+    domain = 'cover'
+    config = {
+        'legrandinone': {
+            'port': '/dev/ttyABC0',
+        },
+        domain: {
+            'platform': 'legrandinone',
+            'devices': {
+                '123456': {
+                        'name': 'test',
+                },
+            },
+        },
+    }
+
+    # setup mocking iobl module
+    _, _, protocol, _ = yield from mock_legrandinone(
+        hass, config, domain, monkeypatch)
+
+    hass.async_add_job(
+        hass.services.async_call(domain, SERVICE_OPEN_COVER,
+                                 {ATTR_ENTITY_ID: 'cover.test'}))
+    yield from hass.async_block_till_done()
+
+    data = cast(Dict[str, Any], {
+    'type': 'bus_command',
+    'legrand_id' : '123456',
+    'who' : 'automation',
+    'mode': 'unicast',
+    'media': 'plc',
+    'unit': '2',
+    'what': 'move_up'
+    })
+
+    assert protocol.send_packet.call_args_list[0][0][0] == data
+
+@asyncio.coroutine
+def test_send_command_invalid_arguments(hass, monkeypatch):
+    """Test send_command service."""
+    domain = 'legrandinone'
+    config = {
+        'legrandinone': {
+            'port': '/dev/ttyABC0',
+        },
+    }
+
+    # setup mocking iobl module
+    _, _, protocol, _ = yield from mock_legrandinone(
+        hass, config, domain, monkeypatch)
+
+    # one argument missing
+    data = cast(Dict[str, Any], {
+    'type': 'bus_command',
+    'who' : 'automation',
+    'mode': 'unicast',
+    'media': 'plc',
+    'unit': '2',
+    'what': 'move_up'
+    })
+
+    hass.async_add_job(
+        hass.services.async_call(domain, SERVICE_SEND_COMMAND,
+                                 data))
+    # no arguments
+    hass.async_add_job(
+        hass.services.async_call(domain, SERVICE_SEND_COMMAND, {}))
+    yield from hass.async_block_till_done()
+    assert protocol.send_packet.call_args_list == []
+
+
+@asyncio.coroutine
+def test_reconnecting_after_disconnect(hass, monkeypatch):
+    """An unexpected disconnect should cause a reconnect."""
+    domain = 'cover'
+    config = {
+        'legrandinone': {
+            'port': '/dev/ttyABC0',
+             CONF_RECONNECT_INTERVAL: 0,
+       },
+        domain: {
+            'platform': 'legrandinone',
+            'devices': {
+                '123456': {
+                        'name': 'test',
+                },
+            },
+        },
+    }
+
+    # setup mocking iobl module
+    _, mock_create, _, disconnect_callback = yield from mock_legrandinone(
+        hass, config, domain, monkeypatch)
+
+    assert disconnect_callback, 'disconnect callback not passed to iobl'
+
+    # iobl initiated disconnect
+    disconnect_callback(None)
+
+    yield from hass.async_block_till_done()
+
+    # we expect 2 call, the initial and reconnect
+    assert mock_create.call_count == 2
+
+
+@asyncio.coroutine
+def test_reconnecting_after_failure(hass, monkeypatch):
+    """A failure to reconnect should be retried."""
+    domain = 'cover'
+    config = {
+        'legrandinone': {
+            'port': '/dev/ttyABC0',
+            CONF_RECONNECT_INTERVAL: 0,
+        },
+        domain: {
+            'platform': 'legrandinone',
+            'devices': {
+                '123456': {
+                        'name': 'test',
+                },
+            },
+        },
+    }
+
+    # success first time but fail second
+    failures = [False, True, False]
+
+    # setup mocking iobl module
+    _, mock_create, _, disconnect_callback = yield from mock_legrandinone(
+        hass, config, domain, monkeypatch, failures=failures)
+
+    # iobl initiated disconnect
+    disconnect_callback(None)
+
+    # wait for reconnects to have happened
+    yield from hass.async_block_till_done()
+    yield from hass.async_block_till_done()
+
+    # we expect 3 calls, the initial and 2 reconnects
+    assert mock_create.call_count == 3
+
+
+@asyncio.coroutine
+def test_error_when_not_connected(hass, monkeypatch):
+    """Sending command should error when not connected."""
+    domain = 'cover'
+    config = {
+        'legrandinone': {
+            'port': '/dev/ttyABC0',
+        },
+        domain: {
+            'platform': 'legrandinone',
+            'devices': {
+                '123456': {
+                        'name': 'test',
+                },
+            },
+        },
+    }
+
+    # success first time but fail second
+    failures = [False, True, False]
+
+    # setup mocking iobl module
+    _, mock_create, _, disconnect_callback = yield from mock_legrandinone(
+        hass, config, domain, monkeypatch, failures=failures)
+
+    # rflink initiated disconnect
+    disconnect_callback(None)
+
+    yield from asyncio.sleep(0, loop=hass.loop)
+
+    success = yield from hass.services.async_call(
+        domain, SERVICE_OPEN_COVER, {ATTR_ENTITY_ID: 'cover.test'})
+    assert not success, 'changing state should not succeed when disconnected'
