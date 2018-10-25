525456a16cc9a008262c3e5fb08cf30030c504e5
Paulus Schoutsen <balloob@gmail.com>
2018-10-25 20:26:53 +0200

    Convert automation tests to async

--- tests/components/automation/common.py
+++ tests/components/automation/common.py
@@ -11,43 +11,34 @@ from homeassistant.loader import bind_hass
 
 
 @bind_hass
-def turn_on(hass, entity_id=None):
+async def async_turn_on(hass, entity_id=None):
     """Turn on specified automation or all."""
     data = {ATTR_ENTITY_ID: entity_id} if entity_id else {}
-    hass.services.call(DOMAIN, SERVICE_TURN_ON, data)
+    await hass.services.async_call(DOMAIN, SERVICE_TURN_ON, data)
 
 
 @bind_hass
-def turn_off(hass, entity_id=None):
+async def async_turn_off(hass, entity_id=None):
     """Turn off specified automation or all."""
     data = {ATTR_ENTITY_ID: entity_id} if entity_id else {}
-    hass.services.call(DOMAIN, SERVICE_TURN_OFF, data)
+    await hass.services.async_call(DOMAIN, SERVICE_TURN_OFF, data)
 
 
 @bind_hass
-def toggle(hass, entity_id=None):
+async def async_toggle(hass, entity_id=None):
     """Toggle specified automation or all."""
     data = {ATTR_ENTITY_ID: entity_id} if entity_id else {}
-    hass.services.call(DOMAIN, SERVICE_TOGGLE, data)
+    await hass.services.async_call(DOMAIN, SERVICE_TOGGLE, data)
 
 
 @bind_hass
-def trigger(hass, entity_id=None):
+async def async_trigger(hass, entity_id=None):
     """Trigger specified automation or all."""
     data = {ATTR_ENTITY_ID: entity_id} if entity_id else {}
-    hass.services.call(DOMAIN, SERVICE_TRIGGER, data)
+    await hass.services.async_call(DOMAIN, SERVICE_TRIGGER, data)
 
 
 @bind_hass
-def reload(hass):
+async def async_reload(hass):
     """Reload the automation from config."""
-    hass.services.call(DOMAIN, SERVICE_RELOAD)
-
-
-@bind_hass
-def async_reload(hass):
-    """Reload the automation from config.
-
-    Returns a coroutine object.
-    """
-    return hass.services.async_call(DOMAIN, SERVICE_RELOAD)
+    await hass.services.async_call(DOMAIN, SERVICE_RELOAD)
