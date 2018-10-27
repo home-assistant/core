3309f0b20fa353573d97823e97688edb4c73fbd1
Vasiley <25254807+Vasiley@users.noreply.github.com>
2018-10-27 10:21:58 -0500

    Update unifi __init__.py
    Allow for retries to cloud key

--- homeassistant/components/unifi/__init__.py
+++ homeassistant/components/unifi/__init__.py
@@ -40,11 +40,11 @@ async def async_setup_entry(hass, config_entry):
         host=config_entry.data[CONF_CONTROLLER][CONF_HOST],
         site=config_entry.data[CONF_CONTROLLER][CONF_SITE_ID]
     )
+    
+    hass.data[DOMAIN][controller_id] = controller
 
     if not await controller.async_setup():
         return False
-
-    hass.data[DOMAIN][controller_id] = controller
 
     if controller.mac is None:
         return True
