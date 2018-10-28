87c0f372f0b8dc18f7e998db462ca1d7eab74469
Rohan Kapoor <rohan@rohankapoor.com>
2018-10-28 11:51:12 -0700

    Switch mailgun webhooks to the webhook api

--- tests/components/mailgun/test_init.py
+++ tests/components/mailgun/test_init.py
@@ -1,26 +1,42 @@
 """Test the init file of Mailgun."""
-from unittest.mock import patch
+import hashlib
+import hmac
+from unittest.mock import patch, Mock
+
+import pytest
 
 from homeassistant import data_entry_flow
 from homeassistant.components import mailgun
+from homeassistant.const import CONF_API_KEY, CONF_DOMAIN
 
 from homeassistant.core import callback
+from homeassistant.setup import async_setup_component
+
+API_KEY = 'abc123'
+mailgun_events = []
 
 
-async def test_config_flow_registers_webhook(hass, aiohttp_client):
-    """Test setting up Mailgun and sending webhook."""
-    with patch('homeassistant.util.get_local_ip', return_value='example.com'):
-        result = await hass.config_entries.flow.async_init('mailgun', context={
-            'source': 'user'
-        })
+@pytest.fixture
+async def fixture(hass, aiohttp_client):
+    """Initialize a Home Assistant Server for testing this module."""
+
+    await async_setup_component(hass, mailgun.DOMAIN, {
+        mailgun.DOMAIN: {
+            CONF_API_KEY: API_KEY,
+            CONF_DOMAIN: 'example.com'
+        },
+    })
+
+    hass.config.api = Mock(base_url='http://example.com')
+    result = await hass.config_entries.flow.async_init('mailgun', context={
+        'source': 'user'
+    })
     assert result['type'] == data_entry_flow.RESULT_TYPE_FORM, result
 
     result = await hass.config_entries.flow.async_configure(
         result['flow_id'], {})
     assert result['type'] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     webhook_id = result['result'].data['webhook_id']
-
-    mailgun_events = []
 
     @callback
     def handle_event(event):
@@ -29,11 +45,73 @@ async def test_config_flow_registers_webhook(hass, aiohttp_client):
 
     hass.bus.async_listen(mailgun.MESSAGE_RECEIVED, handle_event)
 
-    client = await aiohttp_client(hass.http.app)
-    await client.post('/api/webhook/{}'.format(webhook_id), data={
-        'hello': 'mailgun'
+    return await aiohttp_client(hass.http.app), webhook_id
+
+
+async def test_mailgun_webhook_with_missing_signature(fixture):
+    client, webhook_id = fixture
+
+    event_count = len(mailgun_events)
+
+    await client.post('/api/webhook/{}'.format(webhook_id), json={
+        'hello': 'mailgun',
+        'signature': {}
     })
 
-    assert len(mailgun_events) == 1
-    assert mailgun_events[0].data['webhook_id'] == webhook_id
-    assert mailgun_events[0].data['hello'] == 'mailgun'
+    assert len(mailgun_events) == event_count
+
+    await client.post('/api/webhook/{}'.format(webhook_id), json={
+        'hello': 'mailgun',
+    })
+
+    assert len(mailgun_events) == event_count
+
+
+async def test_mailgun_webhook_with_different_api_key(fixture):
+    client, webhook_id = fixture
+
+    timestamp = '1529006854'
+    token = 'a8ce0edb2dd8301dee6c2405235584e45aa91d1e9f979f3de0'
+
+    event_count = len(mailgun_events)
+
+    await client.post('/api/webhook/{}'.format(webhook_id), json={
+        'hello': 'mailgun',
+        'signature': {
+            'signature': hmac.new(
+                key=b'random_api_key',
+                msg=bytes('{}{}'.format(timestamp, token), 'utf-8'),
+                digestmod=hashlib.sha256
+            ).hexdigest(),
+            'timestamp': timestamp,
+            'token': token
+        }
+    })
+
+    assert len(mailgun_events) == event_count
+
+
+async def test_mailgun_webhook_with_correct_api_key(fixture):
+    client, webhook_id = fixture
+
+    timestamp = '1529006854'
+    token = 'a8ce0edb2dd8301dee6c2405235584e45aa91d1e9f979f3de0'
+
+    event_count = len(mailgun_events)
+
+    await client.post('/api/webhook/{}'.format(webhook_id), json={
+        'hello': 'mailgun',
+        'signature': {
+            'signature': hmac.new(
+                key=bytes(API_KEY, 'utf-8'),
+                msg=bytes('{}{}'.format(timestamp, token), 'utf-8'),
+                digestmod=hashlib.sha256
+            ).hexdigest(),
+            'timestamp': timestamp,
+            'token': token
+        }
+    })
+
+    assert len(mailgun_events) == event_count + 1
+    assert mailgun_events[-1].data['webhook_id'] == webhook_id
+    assert mailgun_events[-1].data['hello'] == 'mailgun'
