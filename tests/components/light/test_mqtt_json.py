3a8891d9ac59cd0cbd54ab92afc66b067868609c
Paulus Schoutsen <paulus@home-assistant.io>
2018-10-29 19:21:21 +0100

    Pass hass_config to load_platform (#17952)
    * Pass hass_config to load_platform
    
    * Fix tests
    
    * Lint

--- tests/components/light/test_mqtt_json.py
+++ tests/components/light/test_mqtt_json.py
@@ -674,7 +674,7 @@ class TestLightMQTTJSON(unittest.TestCase):
 
 async def test_discovery_removal(hass, mqtt_mock, caplog):
     """Test removal of discovered mqtt_json lights."""
-    await async_start(hass, 'homeassistant', {})
+    await async_start(hass, 'homeassistant', {'mqtt': {}})
     data = (
         '{ "name": "Beer",'
         '  "platform": "mqtt_json",'
