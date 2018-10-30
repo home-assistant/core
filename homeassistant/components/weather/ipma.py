8390154ef60d037ae0f061fe9ec0dd17f7545b83
Diogo Gomes <dgomes@ua.pt>
2018-10-30 13:37:09 +0000

    shorten debug messages

--- homeassistant/components/weather/ipma.py
+++ homeassistant/components/weather/ipma.py
@@ -71,7 +71,7 @@ async def async_setup_platform(hass, config, async_add_entities,
         station = await Station.get(websession, float(latitude),
                                     float(longitude))
 
-    _LOGGER.debug("Initializing ipma weather: coordinates %s, %s -> station %s",
+    _LOGGER.debug("Initializing for coordinates %s, %s -> station %s",
                   latitude, longitude, station.local)
 
     async_add_entities([IPMAWeather(station, config)], True)
@@ -93,7 +93,8 @@ class IPMAWeather(WeatherEntity):
         """Update Condition and Forecast."""
         with async_timeout.timeout(10, loop=self.hass.loop):
             self._condition = await self._station.observation()
-            _LOGGER.debug("Updating ipma weather condition and forecast: station %s, condition %s", self._station.local, self._condition)
+            _LOGGER.debug("Updating station %s, condition %s",
+                          self._station.local, self._condition)
             self._forecast = await self._station.forecast()
             self._description = self._forecast[0].description
 
