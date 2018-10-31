26d800d1fb1571a1563bb39cd2407d1fc1dffa91
François LEGAL <devel@thom.fr.eu.org>
2018-10-31 09:19:52 +0100

    Add legrandinone platform to communicate with legrand PLC devices

---
+++ homeassistant/components/light/legrandinone.py
@@ -0,0 +1,133 @@
+"""
+Support for IOBL lights.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/light.iobl/
+"""
+import logging
+
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS, PLATFORM_SCHEMA, SUPPORT_BRIGHTNESS, Light)
+from homeassistant.components.legrandinone import (
+    CONF_AUTOMATIC_ADD, CONF_DEVICE_DEFAULTS,
+    CONF_DEVICES, CONF_FIRE_EVENT, DATA_DEVICE_REGISTER,
+    DEVICE_DEFAULTS_SCHEMA, CONF_MEDIA, CONF_COMM_MODE,
+    EVENT_KEY_COMMAND, EVENT_KEY_ID, DEVICE_TYPE_LIGHT,
+    SwitchableLegrandInOneDevice, cv,
+    vol)
+from homeassistant.const import (CONF_NAME, CONF_TYPE)
+
+DEPENDENCIES = ['legrandinone']
+
+_LOGGER = logging.getLogger(__name__)
+
+TYPE_DIMMABLE = 'dimmable'
+TYPE_SWITCHABLE = 'switchable'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_DEVICE_DEFAULTS, default=DEVICE_DEFAULTS_SCHEMA({})):
+        DEVICE_DEFAULTS_SCHEMA,
+    vol.Optional(CONF_AUTOMATIC_ADD, default=True): cv.boolean,
+    vol.Optional(CONF_DEVICES, default={}): {
+        cv.string: vol.Schema({
+            vol.Optional(CONF_NAME): cv.string,
+            vol.Optional(CONF_TYPE):
+                vol.Any(TYPE_DIMMABLE, TYPE_SWITCHABLE),
+            vol.Optional(CONF_FIRE_EVENT): cv.boolean,
+            vol.Optional(CONF_MEDIA, default='plc'): cv.string,
+            vol.Optional(CONF_COMM_MODE, default='unicast'): cv.string,
+        })
+    },
+}, extra=vol.ALLOW_EXTRA)
+
+
+def entity_class_for_type(entity_type):
+    """Translate entity type to entity class.
+
+    Async friendly.
+    """
+    entity_device_mapping = {
+        # sends 'dim', 'off' and 'on' command to support both
+        # dimmers and on/off switches.
+        TYPE_DIMMABLE: DimmableLegrandInOneLight,
+        # sends only 'on/off' commands not advices with dimmers
+        TYPE_SWITCHABLE: LegrandInOneLight,
+    }
+
+    return entity_device_mapping.get(entity_type, LegrandInOneLight)
+
+
+def devices_from_config(domain_config):
+    """Parse configuration and add IOBL light devices."""
+    devices = []
+    for device_id, config in domain_config[CONF_DEVICES].items():
+        # Determine which kind of entity to create
+        if CONF_TYPE in config:
+            # Remove type from config to not pass it as and argument to entity
+            # instantiation
+            entity_type = config.pop(CONF_TYPE)
+        else:
+            # When no type is specified, defaults to switchable.
+            entity_type = TYPE_SWITCHABLE
+
+        entity_class = entity_class_for_type(entity_type)
+
+        device_config = dict(domain_config[CONF_DEVICE_DEFAULTS], **config)
+
+        device = entity_class(device_id, **device_config)
+        devices.append(device)
+
+    return devices
+
+
+async def async_setup_platform(hass, config, async_add_entities,
+                               discovery_info=None):
+    """Set up the IOBL light platform."""
+    async_add_entities(devices_from_config(config))
+
+    async def add_new_device(event):
+        """Check if device is known, otherwise add to list of known devices."""
+        device_id = event[EVENT_KEY_ID]
+
+    # Added devices from detection defaults to SWITCHABLE.
+        entity_type = TYPE_SWITCHABLE
+        entity_class = entity_class_for_type(entity_type)
+
+        device_config = config[CONF_DEVICE_DEFAULTS]
+        device = entity_class(device_id, initial_event=event, **device_config)
+        async_add_entities([device])
+
+    if config[CONF_AUTOMATIC_ADD]:
+        hass.data[DATA_DEVICE_REGISTER][EVENT_KEY_COMMAND][
+            DEVICE_TYPE_LIGHT] = add_new_device
+
+
+class LegrandInOneLight(SwitchableLegrandInOneDevice, Light):
+    """Representation of an IOBL light."""
+
+    pass
+
+
+class DimmableLegrandInOneLight(SwitchableLegrandInOneDevice, Light):
+    """IOBL light device that support dimming."""
+
+    _brightness = 255
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the device on."""
+        if ATTR_BRIGHTNESS in kwargs:
+            self._brightness = int(kwargs[ATTR_BRIGHTNESS])
+            # Turn on light at the requested dim level
+            await self._async_handle_command('dim', self._brightness)
+        else:
+            await self._async_handle_command('turn_on')
+
+    @property
+    def brightness(self):
+        """Return the brightness of this light between 0..255."""
+        return self._brightness
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        return SUPPORT_BRIGHTNESS
---
+++ tests/components/light/test_legrandinone.py
@@ -0,0 +1,233 @@
+"""Test for IOBL light components.
+
+Test setup of IOBL lights component/platform. State tracking and
+control of IOBL switch devices.
+
+"""
+
+import asyncio
+
+from homeassistant.components.light import ATTR_BRIGHTNESS
+from homeassistant.components.legrandinone import EVENT_BUTTON_PRESSED
+from homeassistant.const import (
+    ATTR_ENTITY_ID, SERVICE_TURN_OFF, SERVICE_TURN_ON)
+from homeassistant.core import callback
+
+from ..test_legrandinone import mock_legrandinone
+
+DOMAIN = 'light'
+
+CONFIG = {
+    'legrandinone': {
+        'port': '/dev/ttyABC0',
+    },
+    DOMAIN: {
+        'platform': 'legrandinone',
+        'devices': {
+            123456: {
+                'name': 'test',
+            },
+            234567: {
+                'name': 'dim_test',
+                'type': 'dimmable',
+            },
+            345678: {
+                'name': 'switch_test',
+                'type': 'switchable',
+            }
+        },
+    },
+}
+
+
+@asyncio.coroutine
+def test_default_setup(hass, monkeypatch):
+    """Test all basic functionality of the IOBL switch component."""
+    # setup mocking rflink module
+    event_callback, create, protocol, _ = yield from mock_legrandinone(
+        hass, CONFIG, DOMAIN, monkeypatch)
+
+    # test default state of light loaded from config
+    light_initial = hass.states.get(DOMAIN + '.test')
+    assert light_initial.state == 'off'
+    assert light_initial.attributes['assumed_state']
+
+    # light should follow state of the hardware device by interpreting
+    # incoming events for its name and aliases
+
+    # mock incoming command event for this device
+    event_callback({
+        'type': 'bus_command',
+        'legrand_id': '123456',
+        'what': 'on',
+    })
+    yield from hass.async_block_till_done()
+
+    light_after_first_command = hass.states.get(DOMAIN + '.test')
+    assert light_after_first_command.state == 'on'
+    # also after receiving first command state not longer has to be assumed
+    assert not light_after_first_command.attributes.get('assumed_state')
+
+    # mock incoming command event for this device
+    event_callback({
+        'type': 'bus_command',
+        'legrand_id': '123456',
+        'what': 'off',
+    })
+    yield from hass.async_block_till_done()
+
+    assert hass.states.get(DOMAIN + '.test').state == 'off'
+
+    # test event for new unconfigured sensor
+    event_callback({
+        'type': 'bus_command',
+        'legrand_id': '666666',
+        'who': 'light',
+        'what': 'on',
+    })
+    yield from hass.async_block_till_done()
+
+    assert hass.states.get(DOMAIN + '.666666').state == 'on'
+
+    # test changing state from HA propagates to IOBL
+    hass.async_add_job(
+        hass.services.async_call(DOMAIN, SERVICE_TURN_OFF,
+                                 {ATTR_ENTITY_ID: DOMAIN + '.test'}))
+    yield from hass.async_block_till_done()
+    assert hass.states.get(DOMAIN + '.test').state == 'off'
+    assert protocol.send_packet.call_args_list[0][0][0] == {
+        'type': 'bus_command',
+        'legrand_id': '123456',
+        'who': 'light',
+        'mode': 'unicast',
+        'media': 'plc',
+        'unit': '0',
+        'what': 'off'
+        }
+
+    hass.async_add_job(
+        hass.services.async_call(DOMAIN, SERVICE_TURN_ON,
+                                 {ATTR_ENTITY_ID: DOMAIN + '.test'}))
+    yield from hass.async_block_till_done()
+    assert hass.states.get(DOMAIN + '.test').state == 'on'
+    assert protocol.send_packet.call_args_list[1][0][0] == {
+        'type': 'bus_command',
+        'legrand_id': '123456',
+        'who': 'light',
+        'mode': 'unicast',
+        'media': 'plc',
+        'unit': '0',
+        'what': 'on'
+        }
+
+    # protocols supporting dimming and on/off should create diming light entity
+    event_callback({
+        'type': 'bus_command',
+        'legrand_id': '234567',
+        'what': 'off',
+    })
+    yield from hass.async_block_till_done()
+    hass.async_add_job(
+        hass.services.async_call(DOMAIN, SERVICE_TURN_ON,
+                                 {
+                                     ATTR_ENTITY_ID: DOMAIN + '.dim_test',
+                                     ATTR_BRIGHTNESS: 128,
+                                 }))
+    yield from hass.async_block_till_done()
+
+    assert protocol.send_packet.call_args_list[2][0][0] == {
+        'type': 'set_dimension',
+        'legrand_id': '234567',
+        'who': 'light',
+        'mode': 'unicast',
+        'media': 'plc',
+        'unit': '0',
+        'dimension': 'go_to_level_time',
+        'values': ['50']
+        }
+
+    hass.async_add_job(
+        hass.services.async_call(DOMAIN, SERVICE_TURN_ON,
+                                 {
+                                     ATTR_ENTITY_ID: DOMAIN + '.dim_test',
+                                 }))
+    yield from hass.async_block_till_done()
+
+    assert protocol.send_packet.call_args_list[3][0][0] == {
+        'type': 'bus_command',
+        'legrand_id': '234567',
+        'who': 'light',
+        'mode': 'unicast',
+        'media': 'plc',
+        'unit': '0',
+        'what': 'on'
+        }
+
+
+@asyncio.coroutine
+def test_firing_bus_event(hass, monkeypatch):
+    """Incoming iobl command events should be put on the HA event bus."""
+    config = {
+        'legrandinone': {
+            'port': '/dev/ttyABC0',
+        },
+        DOMAIN: {
+            'platform': 'legrandinone',
+            'devices': {
+                '123456': {
+                    'name': 'test',
+                    'fire_event': True,
+                },
+            },
+        },
+    }
+
+    # setup mocking rflink module
+    event_callback, _, _, _ = yield from mock_legrandinone(
+        hass, config, DOMAIN, monkeypatch)
+
+    calls = []
+
+    @callback
+    def listener(event):
+        calls.append(event)
+    hass.bus.async_listen_once(EVENT_BUTTON_PRESSED, listener)
+
+    # test event for firing event
+    event_callback({
+        'type': 'bus_command',
+        'legrand_id': '123456',
+        'what': 'off',
+    })
+    yield from hass.async_block_till_done()
+
+    assert calls[0].data == {'state': 'off', 'entity_id': DOMAIN + '.test'}
+
+
+@asyncio.coroutine
+def test_disable_automatic_add(hass, monkeypatch):
+    """If disabled new devices should not be automatically added."""
+    config = {
+        'legrandinone': {
+            'port': '/dev/ttyABC0',
+        },
+        DOMAIN: {
+            'platform': 'legrandinone',
+            'automatic_add': False,
+        },
+    }
+
+    # setup mocking rflink module
+    event_callback, _, _, _ = yield from mock_legrandinone(
+        hass, config, DOMAIN, monkeypatch)
+
+    # test event for new unconfigured sensor
+    event_callback({
+        'type': 'bus_command',
+        'legrand_id': '123456',
+        'what': 'off',
+    })
+    yield from hass.async_block_till_done()
+
+    # make sure new device is not added
+    assert not hass.states.get(DOMAIN + '.123456')
