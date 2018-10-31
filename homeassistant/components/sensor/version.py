ee5b9072114ccf782fb90e7668ec92710aa17028
Joakim Sørensen <joasoe@gmail.com>
2018-10-31 21:43:07 +0100

    Add missing blank line.

--- homeassistant/components/sensor/version.py
+++ homeassistant/components/sensor/version.py
@@ -40,6 +40,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
                                                                cv.string),
 })
 
+
 async def async_setup_platform(
         hass, config, async_add_entities, discovery_info=None):
     """Set up the Version sensor platform."""
