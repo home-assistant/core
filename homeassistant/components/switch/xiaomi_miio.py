c75c00d568af86f20c54378f5d285e8f92e6c253
Sebastian Muszynski <basti@linkt.de>
2018-11-01 17:53:48 +0100

    Bump python-miio version (#18095)

--- homeassistant/components/switch/xiaomi_miio.py
+++ homeassistant/components/switch/xiaomi_miio.py
@@ -17,7 +17,7 @@ from homeassistant.const import (
 from homeassistant.exceptions import PlatformNotReady
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['python-miio==0.4.2', 'construct==2.9.45']
+REQUIREMENTS = ['python-miio==0.4.3', 'construct==2.9.45']
 
 _LOGGER = logging.getLogger(__name__)
 
