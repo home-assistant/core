cbfe70a59be79d917e234c4b2220e8244505fa19
Jongerius, Gerben <g.jongerius@jong-soft.com>
2018-11-02 08:33:16 +0100

    Add changes after review of pull request

--- homeassistant/components/sensor/youless.py
+++ homeassistant/components/sensor/youless.py
@@ -1,18 +1,18 @@
 """
-@ Author      : Gerben Jongerius
-@ Date        : 04/29/2018
-@ Description : Youless Sensor - Monitor power consumption. This component will add the following sensors
-                   - Current power consumption (in W)
-                   - Current tick count, since the Youless meter started running (in W)
+Support for YouLess device.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.youless/
 """
 import json
-import logging
 from datetime import timedelta
 from urllib.request import urlopen
 
 import voluptuous as vol
+
 import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_MONITORED_VARIABLES
+from homeassistant.const import CONF_HOST, CONF_MONITORED_CONDITIONS
+from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 
@@ -22,22 +22,6 @@ SENSOR_P_HIGH = 'p2'
 SENSOR_P_LOW = 'p1'
 SENSOR_GAS = 'gas'
 
-DOMAIN = 'youless'
-CONF_HOST = "host"
-CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Optional(CONF_HOST): cv.string,
-        vol.Optional(CONF_MONITORED_VARIABLES,
-                     default=[SENSOR_POWER, SENSOR_POWER_NET]): vol.All(
-            cv.ensure_list, vol.Length(min=1),
-            [vol.In([SENSOR_POWER, SENSOR_POWER_NET, SENSOR_P_LOW,
-                     SENSOR_P_HIGH, SENSOR_GAS])])
-    })
-}, extra=vol.ALLOW_EXTRA)
-
-SENSOR_PREFIX = 'youless_'
-_LOGGER = logging.getLogger(__name__)
-
 SENSOR_TYPES = {
     SENSOR_POWER: ['Usage', 'usage', 'W', 'mdi:flash'],
     SENSOR_POWER_NET: ['Power', 'meter', 'W', 'mdi:gauge'],
@@ -46,10 +30,21 @@ SENSOR_TYPES = {
     SENSOR_GAS: ['Gas', 'gas', 'm3', 'mdi:fire']
 }
 
+CONFIG_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_HOST): cv.string,
+    vol.Optional(CONF_MONITORED_CONDITIONS,
+                 default=[SENSOR_POWER, SENSOR_POWER_NET]): vol.All(
+        cv.ensure_list, vol.Length(min=1),
+        [vol.In(SENSOR_TYPES)])
+})
+
+SENSOR_PREFIX = 'youless_'
+
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the platform based on the configuration"""
     host = config.get(CONF_HOST)
-    sensors = config.get(CONF_MONITORED_VARIABLES)
+    sensors = config.get(CONF_MONITORED_CONDITIONS)
     data_bridge = YoulessDataBridge(host)
 
     devices = []
@@ -63,23 +58,29 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 
 class YoulessDataBridge(object):
+    """A helper class responsible for fetching data"""
 
     def __init__(self, host):
+        """Initialize the helper class"""
         self._url = 'http://' + host + '/e'
         self._data = None
 
     def data(self):
+        """Return the values obtained during the last fetch"""
         return self._data
 
     @Throttle(timedelta(seconds=1))
     def update(self):
+        """Update the inner values by calling the YouLess API"""
         raw_res = urlopen(self._url)
         self._data = json.loads(raw_res.read().decode('utf-8'))[0]
 
 
 class YoulessSensor(Entity):
+    """The sensor implementation for YouLess"""
 
     def __init__(self, data_bridge, name, variable, sensor_id, uom, icon):
+        """Setup the sensor"""
         self._state = None
         self._name = name
         self._property = variable
@@ -91,28 +92,34 @@ class YoulessSensor(Entity):
 
     @property
     def name(self):
+        """Return the name of the sensor"""
         return self._name
 
     @property
     def icon(self):
+        """Return the icon of the sensor depending on the type selected"""
         return self._icon
 
     @property
     def unit_of_measurement(self):
+        """Return the unit of measure for the created sensor"""
         return self._uom
 
     @property
     def state(self):
+        """Return the value stored during the fetching of the data"""
         return self._state
 
     @property
     def state_attributes(self):
+        """return the timestamp that the last measurement was done"""
         if self._raw is not None:
             return {
                 'timestamp': self._raw['tm']
             }
 
     def update(self):
+        """Update the internal state of the sensor using the data fetcher"""
         self._data_bridge.update()
         self._raw = self._data_bridge.data()
         if self._raw is not None:
