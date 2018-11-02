513d887731e733bb920bb6d05f239af75a3a62e3
Fabian Affolter <mail@fabian-affolter.ch>
2018-11-02 13:26:21 +0100

    Fix typos and ordering

--- homeassistant/components/climate/velbus.py
+++ homeassistant/components/climate/velbus.py
@@ -1,46 +1,47 @@
 """
-Velbus thermostat.
+Support for Velbus thermostat.
 
 For more details about this platform, please refer to the documentation
 https://home-assistant.io/components/climate.velbus/
 """
 import logging
 
+from homeassistant.components.climate import (
+    SUPPORT_OPERATION_MODE, SUPPORT_TARGET_TEMPERATURE, ClimateDevice)
 from homeassistant.components.velbus import (
     DOMAIN as VELBUS_DOMAIN, VelbusEntity)
-from homeassistant.components.climate import (
-    ClimateDevice, SUPPORT_TARGET_TEMPERATURE,
-    SUPPORT_OPERATION_MODE)
-from homeassistant.const import (
-    ATTR_TEMPERATURE)
+from homeassistant.const import ATTR_TEMPERATURE
 
 _LOGGER = logging.getLogger(__name__)
 
 DEPENDENCIES = ['velbus']
 
-SUPPORT_FLAGS = (SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE)
 OPERATION_LIST = ['comfort', 'day', 'night', 'safe']
 
+SUPPORT_FLAGS = (SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE)
 
-async def async_setup_platform(hass, config, async_add_entities,
-                               discovery_info=None):
-    """Set up the Velbus temp sensor platform."""
+
+async def async_setup_platform(
+        hass, config, async_add_entities, discovery_info=None):
+    """Set up the Velbus thermostat platform."""
     if discovery_info is None:
         return
+
     sensors = []
     for sensor in discovery_info:
         module = hass.data[VELBUS_DOMAIN].get_module(sensor[0])
         channel = sensor[1]
         sensors.append(VelbusClimate(module, channel))
+
     async_add_entities(sensors)
 
 
 class VelbusClimate(VelbusEntity, ClimateDevice):
-    """Representation of a sensor."""
+    """Representation of a Velbus thermostat."""
 
     @property
     def supported_features(self):
-        """Retur the list off supported featuress."""
+        """Return the list off supported features."""
         return SUPPORT_FLAGS
 
     @property
