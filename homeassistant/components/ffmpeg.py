cea91103092340f435d843952694d526d7bd44f6
Paulus Schoutsen <paulus@home-assistant.io>
2018-11-03 11:05:00 +0100

    Update ffmpeg.py

--- homeassistant/components/ffmpeg.py
+++ homeassistant/components/ffmpeg.py
@@ -40,12 +40,11 @@ CONF_OUTPUT = 'output'
 CONF_RUN_TEST = 'run_test'
 
 DEFAULT_BINARY = 'ffmpeg'
-DEFAULT_RUN_TEST = True
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Optional(CONF_FFMPEG_BIN, default=DEFAULT_BINARY): cv.string,
-        vol.Optional(CONF_RUN_TEST, default=DEFAULT_RUN_TEST): cv.boolean,
+        vol.Optional(CONF_RUN_TEST): cv.boolean,
     }),
 }, extra=vol.ALLOW_EXTRA)
 
