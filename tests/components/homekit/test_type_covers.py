ae00b148470a09780889693e3ac83dc47f29f163
cdce8p <30130371+cdce8p@users.noreply.github.com>
2018-11-05 16:42:19 +0100

    Ignore duplicate state changes GarageDoor HomeKit (#18149)
    * Ignore duplicate state changes GarageDoor HomeKit
    
    * Don't ignore service_call

--- tests/components/homekit/test_type_covers.py
+++ tests/components/homekit/test_type_covers.py
@@ -80,13 +80,30 @@ async def test_garage_door_open_close(hass, hk_driver, cls, events):
     hass.states.async_set(entity_id, STATE_CLOSED)
     await hass.async_block_till_done()
 
+    await hass.async_add_job(acc.char_target_state.client_update_value, 1)
+    await hass.async_block_till_done()
+    assert acc.char_current_state.value == 1
+    assert acc.char_target_state.value == 1
+    assert len(events) == 2
+    assert events[-1].data[ATTR_VALUE] is None
+
     await hass.async_add_job(acc.char_target_state.client_update_value, 0)
     await hass.async_block_till_done()
     assert call_open_cover
     assert call_open_cover[0].data[ATTR_ENTITY_ID] == entity_id
     assert acc.char_current_state.value == 3
     assert acc.char_target_state.value == 0
-    assert len(events) == 2
+    assert len(events) == 3
+    assert events[-1].data[ATTR_VALUE] is None
+
+    hass.states.async_set(entity_id, STATE_OPEN)
+    await hass.async_block_till_done()
+
+    await hass.async_add_job(acc.char_target_state.client_update_value, 0)
+    await hass.async_block_till_done()
+    assert acc.char_current_state.value == 0
+    assert acc.char_target_state.value == 0
+    assert len(events) == 4
     assert events[-1].data[ATTR_VALUE] is None
 
 
