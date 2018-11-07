16c7af59e1ef2f5d41f0b838153501b4393a39a2
Joakim Sørensen <joasoe@gmail.com>
2018-11-07 19:58:21 +0100

    Adds data class and throtle, reuse aiohttp session.

--- homeassistant/components/sensor/launch.py
+++ homeassistant/components/sensor/launch.py
@@ -4,6 +4,7 @@ A sensor platform that give you information about the next space launch.
 For more details about this platform, please refer to the documentation at
 https://www.home-assistant.io/components/sensor.launch/
 """
+from datetime import timedelta
 import logging
 
 import voluptuous as vol
@@ -12,14 +13,18 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import ATTR_ATTRIBUTION, CONF_NAME
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.util import Throttle
 
-REQUIREMENTS = ['pylaunches==0.0.2']
+REQUIREMENTS = ['pylaunches==0.1.1']
 
 _LOGGER = logging.getLogger(__name__)
 
 CONF_ATTRIBUTION = "Data provided by Launch Library."
 
 DEFAULT_NAME = 'Launch'
+
+TIME_BETWEEN_UPDATES = timedelta(hours=1)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
@@ -33,7 +38,8 @@ async def async_setup_platform(
 
     name = config[CONF_NAME]
 
-    launches = Launches(hass.loop)
+    session = async_get_clientsession(hass)
+    launches = LaunchData(Launches(hass.loop, session))
     sensor = [LaunchSensor(launches, name)]
     async_add_entities(sensor, True)
 
@@ -50,12 +56,12 @@ class LaunchSensor(Entity):
 
     async def async_update(self):
         """Get the latest data."""
-        await self.launches.get_launches()
-        if self.launches.launches is None:
+        await self.launches.async_update()
+        if self.launches.api.launches is None:
             _LOGGER.error("No data recieved.")
             return
         try:
-            data = self.launches.launches[0]
+            data = self.launches.api.launches[0]
             self._state = data['name']
             self._attributes['launch_time'] = data['start']
             self._attributes['agency'] = data['agency']
@@ -85,3 +91,15 @@ class LaunchSensor(Entity):
     def device_state_attributes(self):
         """Return attributes for the sensor."""
         return self._attributes
+
+class LaunchData():
+    """Get the latest data and update the states."""
+
+    def __init__(self, api):
+        """Initialize the data object."""
+        self.api = api
+
+    @Throttle(TIME_BETWEEN_UPDATES)
+    async def async_update(self):
+        """Get the latest launch data."""
+        await self.api.get_launches()
