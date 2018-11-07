cf108cf561177e1f5e5531339e3eb5a458aec135
Adam Mills <adam@armills.info>
2018-11-07 03:56:24 -0500

    Update manual ACP tests to async (#18289)

--- tests/components/alarm_control_panel/common.py
+++ tests/components/alarm_control_panel/common.py
@@ -9,6 +9,21 @@ from homeassistant.const import (
     SERVICE_ALARM_DISARM, SERVICE_ALARM_ARM_HOME, SERVICE_ALARM_ARM_AWAY,
     SERVICE_ALARM_ARM_NIGHT, SERVICE_ALARM_ARM_CUSTOM_BYPASS)
 from homeassistant.loader import bind_hass
+from homeassistant.core import callback
+
+
+@callback
+@bind_hass
+def async_alarm_disarm(hass, code=None, entity_id=None):
+    """Send the alarm the command for disarm."""
+    data = {}
+    if code:
+        data[ATTR_CODE] = code
+    if entity_id:
+        data[ATTR_ENTITY_ID] = entity_id
+
+    hass.async_create_task(
+        hass.services.async_call(DOMAIN, SERVICE_ALARM_DISARM, data))
 
 
 @bind_hass
@@ -23,6 +38,20 @@ def alarm_disarm(hass, code=None, entity_id=None):
     hass.services.call(DOMAIN, SERVICE_ALARM_DISARM, data)
 
 
+@callback
+@bind_hass
+def async_alarm_arm_home(hass, code=None, entity_id=None):
+    """Send the alarm the command for disarm."""
+    data = {}
+    if code:
+        data[ATTR_CODE] = code
+    if entity_id:
+        data[ATTR_ENTITY_ID] = entity_id
+
+    hass.async_create_task(
+        hass.services.async_call(DOMAIN, SERVICE_ALARM_ARM_HOME, data))
+
+
 @bind_hass
 def alarm_arm_home(hass, code=None, entity_id=None):
     """Send the alarm the command for arm home."""
@@ -33,6 +62,20 @@ def alarm_arm_home(hass, code=None, entity_id=None):
         data[ATTR_ENTITY_ID] = entity_id
 
     hass.services.call(DOMAIN, SERVICE_ALARM_ARM_HOME, data)
+
+
+@callback
+@bind_hass
+def async_alarm_arm_away(hass, code=None, entity_id=None):
+    """Send the alarm the command for disarm."""
+    data = {}
+    if code:
+        data[ATTR_CODE] = code
+    if entity_id:
+        data[ATTR_ENTITY_ID] = entity_id
+
+    hass.async_create_task(
+        hass.services.async_call(DOMAIN, SERVICE_ALARM_ARM_AWAY, data))
 
 
 @bind_hass
@@ -47,6 +90,20 @@ def alarm_arm_away(hass, code=None, entity_id=None):
     hass.services.call(DOMAIN, SERVICE_ALARM_ARM_AWAY, data)
 
 
+@callback
+@bind_hass
+def async_alarm_arm_night(hass, code=None, entity_id=None):
+    """Send the alarm the command for disarm."""
+    data = {}
+    if code:
+        data[ATTR_CODE] = code
+    if entity_id:
+        data[ATTR_ENTITY_ID] = entity_id
+
+    hass.async_create_task(
+        hass.services.async_call(DOMAIN, SERVICE_ALARM_ARM_NIGHT, data))
+
+
 @bind_hass
 def alarm_arm_night(hass, code=None, entity_id=None):
     """Send the alarm the command for arm night."""
@@ -59,6 +116,20 @@ def alarm_arm_night(hass, code=None, entity_id=None):
     hass.services.call(DOMAIN, SERVICE_ALARM_ARM_NIGHT, data)
 
 
+@callback
+@bind_hass
+def async_alarm_trigger(hass, code=None, entity_id=None):
+    """Send the alarm the command for disarm."""
+    data = {}
+    if code:
+        data[ATTR_CODE] = code
+    if entity_id:
+        data[ATTR_ENTITY_ID] = entity_id
+
+    hass.async_create_task(
+        hass.services.async_call(DOMAIN, SERVICE_ALARM_TRIGGER, data))
+
+
 @bind_hass
 def alarm_trigger(hass, code=None, entity_id=None):
     """Send the alarm the command for trigger."""
@@ -69,6 +140,21 @@ def alarm_trigger(hass, code=None, entity_id=None):
         data[ATTR_ENTITY_ID] = entity_id
 
     hass.services.call(DOMAIN, SERVICE_ALARM_TRIGGER, data)
+
+
+@callback
+@bind_hass
+def async_alarm_arm_custom_bypass(hass, code=None, entity_id=None):
+    """Send the alarm the command for disarm."""
+    data = {}
+    if code:
+        data[ATTR_CODE] = code
+    if entity_id:
+        data[ATTR_ENTITY_ID] = entity_id
+
+    hass.async_create_task(
+        hass.services.async_call(
+            DOMAIN, SERVICE_ALARM_ARM_CUSTOM_BYPASS, data))
 
 
 @bind_hass
