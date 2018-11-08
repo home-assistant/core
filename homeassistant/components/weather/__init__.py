bd852dc3e73642f59fa166043a3a952a0955343a
victorcerutti <victor.cerutti@gmail.com>
2018-11-08 10:31:04 +0100

    new weather component for meteofrance

--- homeassistant/components/weather/__init__.py
+++ homeassistant/components/weather/__init__.py
@@ -36,6 +36,8 @@ ATTR_WEATHER_TEMPERATURE = 'temperature'
 ATTR_WEATHER_VISIBILITY = 'visibility'
 ATTR_WEATHER_WIND_BEARING = 'wind_bearing'
 ATTR_WEATHER_WIND_SPEED = 'wind_speed'
+ATTR_WEATHER_RAIN_FORECAST = 'rain_forecast'
+ATTR_WEATHER_CONDITION_ICON = 'condition_icon'
 
 
 async def async_setup(hass, config):
@@ -104,6 +106,16 @@ class WeatherEntity(Entity):
         return None
 
     @property
+    def rain_forecast(self):
+        """Return the rain forecast."""
+        return None
+
+    @property
+    def condition_icon(self):
+        """Return the condition icon."""
+        return self.condition
+
+    @property
     def forecast(self):
         """Return the forecast."""
         return None
@@ -146,6 +158,14 @@ class WeatherEntity(Entity):
         visibility = self.visibility
         if visibility is not None:
             data[ATTR_WEATHER_VISIBILITY] = visibility
+
+        rain_forecast = self.rain_forecast
+        if rain_forecast is not None:
+            data[ATTR_WEATHER_RAIN_FORECAST] = rain_forecast
+
+        condition_icon = self.condition_icon
+        if condition_icon is not None:
+            data[ATTR_WEATHER_CONDITION_ICON] = condition_icon
 
         attribution = self.attribution
         if attribution is not None:
