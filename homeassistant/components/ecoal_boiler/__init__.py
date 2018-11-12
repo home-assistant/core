a1abe5c6b7ae5bbcb2b3fa8383095fdbca2fdda2
Mateusz Korniak <matkorgithubcom@ant.gliwice.pl>
2018-11-12 12:16:21 +0100

    Temp sensors seems be working.

--- homeassistant/components/ecoal_boiler/__init__.py
+++ homeassistant/components/ecoal_boiler/__init__.py
@@ -1,26 +1,61 @@
 """
 Support for ecoal/esterownik.pl coal/wood boiler controller
 """
-
+import logging
 
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 
-from homeassistant.const import CONF_HOST
+## from homeassistant.const import CONF_HOST
 
-from homeassistant.components.light import PLATFORM_SCHEMA
+## from homeassistant.components.light import PLATFORM_SCHEMA
 
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = 'ecoal_boiler'
+
+CONF_HOSTNAME = 'host'
 CONF_LOGIN = "login"
 DEFAULT_LOGIN = "admin"
 CONF_PASSWORD = "password"
 DEFAULT_PASSWORD = "admin"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string,
-    vol.Optional(CONF_LOGIN,
-                default=DEFAULT_LOGIN): cv.string,
-    vol.Optional(CONF_PASSWORD,
-                default=DEFAULT_PASSWORD): cv.string,
 
-})
+##CONFIG_SCHEMA = vol.Schema({
+##    DOMAIN: vol.Schema({
+##        vol.Required(CONF_HOSTNAME): cv.string,
+##        vol.Optional(CONF_LOGIN,
+##                default=DEFAULT_LOGIN): cv.string,
+##        vol.Optional(CONF_PASSWORD,
+##                default=DEFAULT_PASSWORD): cv.string,
+##    })
+##})
+# TODO: Fails with:
+# [homeassistant.config] Invalid config for [ecoal_boiler]: [homeassistant] is an invalid option for [ecoal_boiler]. Check: ecoal_boiler->homeassistant. (See /home/matkor/src/home-assistant/homeassistant-i2c_expanders.venv/./config/configuration.yaml, line 74). Please check the docs at https://home-assistant.io/components/ecoal_boiler/
+#  [homeassistant.setup] Setup failed for ecoal_boiler: Invalid config.
+
+
+##CONFIG_SCHEMA = vol.Schema({
+##    DOMAIN: vol.Schema({
+##    })
+##})
+
+g_ecoal_contr = None
+
+async def async_setup(hass, config):
+    global g_ecoal_contr
+    _LOGGER.debug("async_setup(): config: %r",  config)
+    from .http_iface import ECoalControler
+    # hass.states.set('hello.world', 'Paulus')_LOGGE
+    conf = config.get(DOMAIN)
+    _LOGGER.debug("async_setup(): conf: %r  conf.keys(): %r",  conf, conf.keys())
+
+    host = conf.get(CONF_HOSTNAME)
+    login = conf.get(CONF_LOGIN)
+    passwd = conf.get(CONF_PASSWORD)
+    _LOGGER.debug("async_setup(): host: %r login: %r passwd: %r",  host, login, passwd)
+    g_ecoal_contr = ECoalControler(host, login, passwd)
+    _LOGGER.debug("async_setup(): g_ecoal_contr: %r",  g_ecoal_contr)
+
+    return True
---
+++ homeassistant/components/sensor/ecoal_boiler.py
@@ -0,0 +1,61 @@
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.helpers.entity import Entity
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the ecoal sensors ."""
+    from ..ecoal_boiler import g_ecoal_contr
+    ## contr = ECoalControler("")
+
+    add_devices([
+
+            EcoalTempSensor(g_ecoal_contr,"Outside", 'external_temp'),
+            EcoalTempSensor(g_ecoal_contr,"Inside", 'internal_temp'),
+            EcoalTempSensor(g_ecoal_contr,"2nd inside", 'internal2_temp'),
+
+            EcoalTempSensor(g_ecoal_contr,"Domestic water", 'domestic_hot_water_temp'),
+            EcoalTempSensor(g_ecoal_contr,"Domestic water target", 'target_domestic_hot_water_temp'),
+
+            EcoalTempSensor(g_ecoal_contr,"Feedwater in", 'feedwater_in_temp'),
+            EcoalTempSensor(g_ecoal_contr,"Feedwater out", 'feedwater_out_temp'),
+            EcoalTempSensor(g_ecoal_contr,"Feedwater target", 'target_feedwater_temp'),
+
+            EcoalTempSensor(g_ecoal_contr,"Fuel feeder", 'coal_feeder_temp'),
+            EcoalTempSensor(g_ecoal_contr,"Exhaust gas", 'chimney_temp'),
+        ])
+
+
+
+class EcoalTempSensor(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self, ecoal_contr, name, status_attr):
+        """Initialize the sensor."""
+        self._ecoal_contr = ecoal_contr
+        self._name = name
+        self._status_attr = status_attr
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return TEMP_CELSIUS
+
+    def update(self):
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        ## self._state = 23
+        status = self._ecoal_contr.get_cached_status(0.5)  # Old values read 0.5 back can still be used
+        self._state = getattr(status, self._status_attr)
