e5ad352b72b1ca3fa2d3b0b895fc9fa083fc2f19
Mateusz Korniak <matkorgithubcom@ant.gliwice.pl>
2018-11-12 22:29:12 +0100

    Optional enabling pumps via config.

--- homeassistant/components/switch/ecoal_boiler.py
+++ homeassistant/components/switch/ecoal_boiler.py
@@ -1,20 +1,41 @@
 """
 Allows to configure ecoal (esterownik.pl) solid fuel boiler controller as set of switches operating over pumps
+Example configuration:
+
+switch:
+  - platform: ecoal_boiler
+    enable:
+      central_heating_pump: Wall radiators
+      central_heating_pump2: Floor radiators
+      # domestic_hot_water_pump: N/A
+
+sets names for two pumps (central_heating_pump,central_heating_pump2).
+Missing grom config 3rd pump ( domestic_hot_water_pump ) is not used
 
 """
 import logging
 from typing import Optional
 
-# import voluptuous as vol
+import voluptuous as vol
 
-# from homeassistant.components.switch import PLATFORM_SCHEMA
+from homeassistant.components.switch import PLATFORM_SCHEMA
 
-## from homeassistant.const import DEVICE_DEFAULT_NAME
 from homeassistant.helpers.entity import ToggleEntity
-## import homeassistant.helpers.config_validation as cv
-
+import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
+
+# Available ids
+PUMP_IDNAMES = ('central_heating_pump', 'domestic_hot_water_pump', 'central_heating_pump2', )
+
+ENABLED_SCHEMA = {}
+for pump_id in PUMP_IDNAMES:
+    ENABLED_SCHEMA[vol.Optional(pump_id)] = cv.string
+CONF_ENABLE = 'enable'
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_ENABLE): ENABLED_SCHEMA,
+})
+
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
@@ -23,10 +44,13 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     ## invert_logic = config.get(CONF_INVERT_LOGIC)
 
+    config_enable = config.get(CONF_ENABLE, {})
+    #_LOGGER.debug("config_enable: %r", config_enable)
     switches = []
-    switches.append(EcoalSwitch(g_ecoal_contr, 'Central heating pump', 'central_heating_pump'))
-    switches.append(EcoalSwitch(g_ecoal_contr, 'Domestic home water pump', 'domestic_hot_water_pump'))
-    switches.append(EcoalSwitch(g_ecoal_contr, 'Central heating pump #2', 'central_heating_pump2'))
+    for pump_id in PUMP_IDNAMES:
+        name = config_enable.get(pump_id)
+        if name:
+            switches.append(EcoalSwitch(g_ecoal_contr, name, pump_id))
     add_entities(switches)
 
 
@@ -38,13 +62,9 @@ class EcoalSwitch(ToggleEntity):
         self._ecoal_contr = ecoal_contr
         self._name = name
         self._state_attr = state_attr
-
-
         # NOTE: Convetion set_<attr> and status.<attr> is held inside ecoal_boiler.http_interface
         self._contr_set_fun = getattr(self._ecoal_contr, 'set_'+state_attr)
-
         # No setting value, read instead
-        # self._contr_set_fun(0)  # self.turn_off() ?
         self._state = self.is_on
 
 
@@ -70,14 +90,17 @@ class EcoalSwitch(ToggleEntity):
         """Turn the device on."""
         # self._ecoal_contr.set_domestic_hot_water_pump(1)
         self._contr_set_fun(1)
-        # TODO: Rearead state witout caching here ?
-        self._state = True
+        # Reread state without using cache here
+        self._ecoal_contr.get_status()
+        self.is_on
         self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs) -> None:
         """Turn the device off."""
         ## self._ecoal_contr.set_domestic_hot_water_pump(0)
         self._contr_set_fun(0)
-        self._state = False
+        # Reread state without using cache here
+        self._ecoal_contr.get_status()
+        self.is_on
         self.schedule_update_ha_state()
 
