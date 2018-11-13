8e1a75df6d79b6d98aa3b0faaaa4a51b5cb1a6c7
Erik <erik@montnemery.com>
2018-11-13 16:44:52 +0100

    Avoid race in entity_platform.async_add_entities() This avoids a race between multiple concurrent calls to entity_platform.async_add_entities() which may cause entities to be created with non-unique entity_id

--- homeassistant/helpers/entity_registry.py
+++ homeassistant/helpers/entity_registry.py
@@ -95,7 +95,8 @@ class EntityRegistry:
         return None
 
     @callback
-    def async_generate_entity_id(self, domain, suggested_object_id):
+    def async_generate_entity_id(self, domain, suggested_object_id,
+                                 known_object_ids=None):
         """Generate an entity ID that does not conflict.
 
         Conflicts checked against registered and currently existing entities.
@@ -103,7 +104,8 @@ class EntityRegistry:
         return ensure_unique_string(
             '{}.{}'.format(domain, slugify(suggested_object_id)),
             chain(self.entities.keys(),
-                  self.hass.states.async_entity_ids(domain))
+                  self.hass.states.async_entity_ids(domain),
+                  known_object_ids if known_object_ids else [])
         )
 
     @callback
