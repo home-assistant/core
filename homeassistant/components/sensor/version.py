8f0b7f5d50e4ee743f020b218191d833cb08e22c
Joakim Sørensen <joasoe@gmail.com>
2018-11-13 21:00:21 +0100

    Fixes issue for returning the correct docker version. (#18439)

--- homeassistant/components/sensor/version.py
+++ homeassistant/components/sensor/version.py
@@ -16,7 +16,7 @@ from homeassistant.const import CONF_NAME, CONF_SOURCE
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 
-REQUIREMENTS = ['pyhaversion==2.0.2']
+REQUIREMENTS = ['pyhaversion==2.0.3']
 
 _LOGGER = logging.getLogger(__name__)
 
