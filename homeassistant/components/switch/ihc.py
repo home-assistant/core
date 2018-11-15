566278fa17916218b0e3c2132fa2398ca099ae55
mopolus <43782170+mopolus@users.noreply.github.com>
2018-11-15 10:22:30 +0100

    Update ihc.py

--- homeassistant/components/switch/ihc.py
+++ homeassistant/components/switch/ihc.py
@@ -6,10 +6,10 @@ https://home-assistant.io/components/switch.ihc/
 import voluptuous as vol
 
 from homeassistant.components.ihc import (
-    validate_name, IHC_DATA, IHC_CONTROLLER, CONTROLLER_ID, IHC_INFO)
+    validate_name, IHC_DATA, IHC_CONTROLLER, IHC_INFO)
 from homeassistant.components.ihc.ihcdevice import IHCDevice
 from homeassistant.components.ihc.const import (
-    CONF_SECONDARY)
+    CONF_CONTROLLER)
 from homeassistant.components.switch import SwitchDevice, PLATFORM_SCHEMA
 from homeassistant.const import CONF_ID, CONF_NAME, CONF_SWITCHES
 import homeassistant.helpers.config_validation as cv
@@ -21,7 +21,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
         vol.All(cv.ensure_list, [
             vol.All({
                 vol.Required(CONF_ID): cv.positive_int,
-                vol.Optional(CONF_SECONDARY, default=False): cv.boolean,
+                vol.Optional(CONF_CONTROLLER, default="0"): cv.string,
                 vol.Optional(CONF_NAME): cv.string,
             }, validate_name)
         ])
@@ -47,13 +47,11 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         switches = config[CONF_SWITCHES]
         for switch in switches:
             ihc_id = switch[CONF_ID]
-            # Get controller id
-            ihc_secondary = bool(switch[CONF_SECONDARY])
-            ihc_key = IHC_DATA.format(CONTROLLER_ID[ihc_secondary])
-            ihc_controller = hass.data[ihc_key][IHC_CONTROLLER]
-
-            info = hass.data[ihc_key][IHC_INFO]
+            controller_id = switch[CONF_CONTROLLER]
+            ihc_controller = hass.data[controller_id][IHC_CONTROLLER]
+            info = hass.data[controller_id][IHC_INFO]            
             name = switch[CONF_NAME]
+
             sensor = IHCSwitch(ihc_controller, name, ihc_id, info)
             devices.append(sensor)
 
