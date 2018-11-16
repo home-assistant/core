20cc167cd89c0b23970e8c9e01abb6fa80ee2341
mopolus <43782170+mopolus@users.noreply.github.com>
2018-11-16 22:49:06 +0100

    Update ihc.py

--- homeassistant/components/sensor/ihc.py
+++ homeassistant/components/sensor/ihc.py
@@ -15,21 +15,22 @@ DEPENDENCIES = ['ihc']
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the IHC sensor platform."""
+    if discover_info is None:
+        return
     devices = []
-    if discovery_info:
-        for name, device in discovery_info.items():
-            ihc_id = device['ihc_id']
-            product_cfg = device['product_cfg']
-            product = device['product']
-            # Find controller that corresponds with device id
-            ctrl_id = device['ctrl_id']
-            ihc_key = IHC_DATA.format(ctrl_id)
-            info = hass.data[ihc_key][IHC_INFO]
-            ihc_controller = hass.data[ihc_key][IHC_CONTROLLER]
-            unit = product_cfg[CONF_UNIT_OF_MEASUREMENT]
-            sensor = IHCSensor(ihc_controller, name, ihc_id, info,
-                               unit, product)
-            devices.append(sensor)
+    for name, device in discovery_info.items():
+        ihc_id = device['ihc_id']
+        product_cfg = device['product_cfg']
+        product = device['product']
+        # Find controller that corresponds with device id
+        ctrl_id = device['ctrl_id']
+        ihc_key = IHC_DATA.format(ctrl_id)
+        info = hass.data[ihc_key][IHC_INFO]
+        ihc_controller = hass.data[ihc_key][IHC_CONTROLLER]
+        unit = product_cfg[CONF_UNIT_OF_MEASUREMENT]
+        sensor = IHCSensor(ihc_controller, name, ihc_id, info,
+                           unit, product)
+        devices.append(sensor)
     add_entities(devices)
 
 
