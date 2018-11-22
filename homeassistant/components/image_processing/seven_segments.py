f28d84b1e727bd18508c044852e4e678306696cb
jumpkick <ironstorm@gmail.com>
2018-11-22 03:23:53 -0500

    Update seven_segments.py

--- homeassistant/components/image_processing/seven_segments.py
+++ homeassistant/components/image_processing/seven_segments.py
@@ -69,7 +69,7 @@ class ImageProcessingSsocr(ImageProcessingEntity):
                 split_entity_id(camera_entity)[1])
         self._state = None
 
-        self.filepath = os.path.join(self.hass.config.config_dir, 'ssocr-' + 
+        self.filepath = os.path.join(self.hass.config.config_dir, 'ssocr-' +
                                      self._name.replace(' ', '_') + '.png')
         crop = ['crop', str(config[CONF_X_POS]), str(config[CONF_Y_POS]),
                 str(config[CONF_WIDTH]), str(config[CONF_HEIGHT])]
