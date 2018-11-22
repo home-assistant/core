1662bbeef6485ed1ec0c352a9237c20c1b60767e
Eliseo Martelli <me@eliseomartelli.it>
2018-11-22 09:59:10 +0100

    removed white space

--- homeassistant/components/sensor/qbittorrent.py
+++ homeassistant/components/sensor/qbittorrent.py
@@ -34,7 +34,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-async def async_setup_platform(hass, config, add_entities, 
+async def async_setup_platform(hass, config, add_entities,
                                discovery_info=None):
     """Set up the qbittorrent sensors."""
     from qbittorrent import Client
