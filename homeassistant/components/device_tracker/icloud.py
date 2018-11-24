18f1301ee95827b55027c0931ac41d4670428bde
melhoj <martin@elhoj.dk>
2018-11-24 20:25:28 +0000

    Updated based in flake8 and pylint

--- homeassistant/components/device_tracker/icloud.py
+++ homeassistant/components/device_tracker/icloud.py
@@ -443,7 +443,8 @@ class Icloud(DeviceScanner):
         self.api.authenticate()
 
         for device in self.api.devices:
-            if devicename is None or device == self.devices[devicename]:
+            if devicename is None or \
+               str(device) == str(self.devices[devicename]):
                 device.play_sound()
 
     def update_icloud(self, devicename=None):
