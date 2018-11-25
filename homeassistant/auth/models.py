24aa162a3d842f364234b9bb9433098cfea3b25b
Paulus Schoutsen <paulus@home-assistant.io>
2018-11-25 18:04:48 +0100

    Add permission checks to Rest API (#18639)
    * Add permission checks to Rest API
    
    * Clean up unnecessary method
    
    * Remove all the tuple stuff from entity check
    
    * Simplify perms
    
    * Correct param name for owner permission
    
    * Hass.io make/update user to be admin
    
    * Types

--- homeassistant/auth/models.py
+++ homeassistant/auth/models.py
@@ -8,6 +8,7 @@ import attr
 from homeassistant.util import dt as dt_util
 
 from . import permissions as perm_mdl
+from .const import GROUP_ID_ADMIN
 from .util import generate_secret
 
 TOKEN_TYPE_NORMAL = 'normal'
@@ -48,7 +49,7 @@ class User:
     )  # type: Dict[str, RefreshToken]
 
     _permissions = attr.ib(
-        type=perm_mdl.PolicyPermissions,
+        type=Optional[perm_mdl.PolicyPermissions],
         init=False,
         cmp=False,
         default=None,
@@ -68,6 +69,19 @@ class User:
                 group.policy for group in self.groups]))
 
         return self._permissions
+
+    @property
+    def is_admin(self) -> bool:
+        """Return if user is part of the admin group."""
+        if self.is_owner:
+            return True
+
+        return self.is_active and any(
+            gr.id == GROUP_ID_ADMIN for gr in self.groups)
+
+    def invalidate_permission_cache(self) -> None:
+        """Invalidate permission cache."""
+        self._permissions = None
 
 
 @attr.s(slots=True)
