8f24aaddd68dab250e31e06a032d5c149e19f81a
Mateusz Korniak <matkorgithubcom@ant.gliwice.pl>
2018-11-26 21:46:37 +0100

    Removed debug/comments.

--- homeassistant/components/ecoal_boiler/__init__.py
+++ homeassistant/components/ecoal_boiler/__init__.py
@@ -1,5 +1,5 @@
 """
-Support for ecoal/esterownik.pl coal/wood boiler controller.
+Provides functionality allowing control ecoal/esterownik.pl coal/wood boiler controller.
 
 Allows read various readings available in controller
 and set very basic switches.
@@ -10,12 +10,10 @@ import logging
 # import homeassistant.helpers.config_validation as cv
 from homeassistant.const import CONF_HOST, CONF_PASSWORD
 
-
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "ecoal_boiler"
 DATA_ECOAL_BOILER = 'data_' + DOMAIN
-
 
 CONF_USERNAME = "username"
 DEFAULT_USERNAME = "admin"
@@ -34,26 +32,15 @@ DEFAULT_PASSWORD = "admin"
 # Invalid config for [ecoal_boiler]:
 #   [homeassistant] is an invalid option for [ecoal_boiler].
 
-# In fact it is variable, set during setup
-## ECOAL_CONTR = None
-
 
 async def async_setup(hass, config):
     """Set up global ECoalControler instance same for sensors and switches."""
-    _LOGGER.debug("async_setup(): config: %r", config)
     from .http_iface import ECoalControler
 
     conf = config.get(DOMAIN)
-    _LOGGER.debug(
-        "async_setup(): conf: %r  conf.keys(): %r", conf, conf.keys()
-    )
-
     host = conf.get(CONF_HOST)
     username = conf.get(CONF_USERNAME)
     passwd = conf.get(CONF_PASSWORD)
-    _LOGGER.debug(
-        "async_setup(): host: %r username: %r passwd: %r", host, username, passwd
-    )
     ecoal_contr = ECoalControler(host, username, passwd)
     hass.data[DATA_ECOAL_BOILER] = ecoal_contr
     return True
