956acb32c1b0caebad57a2242033ca1fc4713397
Mateusz Korniak <matkorgithubcom@ant.gliwice.pl>
2018-11-26 18:34:05 +0100

    Using configuration consts from homeassistant.const

--- homeassistant/components/ecoal_boiler/__init__.py
+++ homeassistant/components/ecoal_boiler/__init__.py
@@ -8,24 +8,23 @@ import logging
 
 # import voluptuous as vol
 # import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_HOST, CONF_PASSWORD
 
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "ecoal_boiler"
 
-CONF_HOSTNAME = "host"
-CONF_LOGIN = "login"
-DEFAULT_LOGIN = "admin"
-CONF_PASSWORD = "password"
+CONF_USERNAME = "username"
+DEFULT_USERNAME = "admin"
 DEFAULT_PASSWORD = "admin"
 
 
 # CONFIG_SCHEMA = vol.Schema({
 #    DOMAIN: vol.Schema({
-#        vol.Required(CONF_HOSTNAME): cv.string,
-#        vol.Optional(CONF_LOGIN,
-#                        default=DEFAULT_LOGIN): cv.string,
+#        vol.Required(CONF_HOST): cv.string,
+#        vol.Optional(CONF_USERNAME,
+#                        default=DEFULT_USERNAME): cv.string,
 #        vol.Optional(CONF_PASSWORD,
 #                        default=DEFAULT_PASSWORD): cv.string,
 #    })
@@ -50,12 +49,12 @@ async def async_setup(hass, config):
         "async_setup(): conf: %r  conf.keys(): %r", conf, conf.keys()
     )
 
-    host = conf.get(CONF_HOSTNAME)
-    login = conf.get(CONF_LOGIN)
+    host = conf.get(CONF_HOST)
+    username = conf.get(CONF_USERNAME)
     passwd = conf.get(CONF_PASSWORD)
     _LOGGER.debug(
-        "async_setup(): host: %r login: %r passwd: %r", host, login, passwd
+        "async_setup(): host: %r username: %r passwd: %r", host, username, passwd
     )
-    ECOAL_CONTR = ECoalControler(host, login, passwd)
+    ECOAL_CONTR = ECoalControler(host, username, passwd)
     # _LOGGER.debug("async_setup(): ECOAL_CONTR: %r", ECOAL_CONTR)
     return True
