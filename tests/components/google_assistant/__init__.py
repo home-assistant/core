c21b280afa5780d13d085de3293b3462e12ada7c
Bryan York <bryan.york@gmail.com>
2018-11-27 08:11:55 -0800

    Enable Google Assistant OnOffTrait for climate devices that support them (#18544)
    * Enable Google Assistant OnOffTrait for climate devices that support them
    
    This commit enables the OnOffTrait for climate devices that have the SUPPORT_ON_OFF feature. I have tested this locally with a Sensibo device which supports ON_OFF and a nest device that does not.
    
    * Update trait.py
    
    * Add tests for onoff_climate
    
    * Add OnOff trait to climate.heatpump
    
    * Add on status to heatpump in google_assistant tests

--- tests/components/google_assistant/__init__.py
+++ tests/components/google_assistant/__init__.py
@@ -225,7 +225,10 @@ DEMO_DEVICES = [{
     'name': {
         'name': 'HeatPump'
     },
-    'traits': ['action.devices.traits.TemperatureSetting'],
+    'traits': [
+        'action.devices.traits.OnOff',
+        'action.devices.traits.TemperatureSetting'
+    ],
     'type': 'action.devices.types.THERMOSTAT',
     'willReportState': False
 }, {
