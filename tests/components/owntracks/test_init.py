a6a1407fbd0e947e3b2b19e4ddb74211f1f6086c
Paulus Schoutsen <paulus@home-assistant.io>
2018-11-28 22:20:13 +0100

    OwnTracks Config Entry (#18759)
    * OwnTracks Config Entry
    
    * Fix test
    
    * Fix headers
    
    * Lint
    
    * Username for android only
    
    * Update translations
    
    * Tweak translation
    
    * Create config entry if not there
    
    * Update reqs
    
    * Types
    
    * Lint

--- tests/components/owntracks/test_init.py
+++ tests/components/owntracks/test_init.py
@@ -1,14 +1,11 @@
 """Test the owntracks_http platform."""
 import asyncio
-from unittest.mock import patch
 
-import os
 import pytest
 
-from homeassistant.components import device_tracker
 from homeassistant.setup import async_setup_component
 
-from tests.common import mock_component, mock_coro
+from tests.common import mock_component, MockConfigEntry
 
 MINIMAL_LOCATION_MESSAGE = {
     '_type': 'location',
@@ -36,38 +33,33 @@ LOCATION_MESSAGE = {
 }
 
 
-@pytest.fixture(autouse=True)
-def owntracks_http_cleanup(hass):
-    """Remove known_devices.yaml."""
-    try:
-        os.remove(hass.config.path(device_tracker.YAML_DEVICES))
-    except OSError:
-        pass
-
-
 @pytest.fixture
 def mock_client(hass, aiohttp_client):
     """Start the Hass HTTP component."""
     mock_component(hass, 'group')
     mock_component(hass, 'zone')
-    with patch('homeassistant.components.device_tracker.async_load_config',
-               return_value=mock_coro([])):
-        hass.loop.run_until_complete(
-            async_setup_component(hass, 'device_tracker', {
-                'device_tracker': {
-                    'platform': 'owntracks_http',
-                    'webhook_id': 'owntracks_test'
-                }
-            }))
+    mock_component(hass, 'device_tracker')
+
+    MockConfigEntry(domain='owntracks', data={
+        'webhook_id': 'owntracks_test',
+        'secret': 'abcd',
+    }).add_to_hass(hass)
+    hass.loop.run_until_complete(async_setup_component(hass, 'owntracks', {}))
+
     return hass.loop.run_until_complete(aiohttp_client(hass.http.app))
 
 
 @asyncio.coroutine
 def test_handle_valid_message(mock_client):
     """Test that we forward messages correctly to OwnTracks."""
-    resp = yield from mock_client.post('/api/webhook/owntracks_test?'
-                                       'u=test&d=test',
-                                       json=LOCATION_MESSAGE)
+    resp = yield from mock_client.post(
+        '/api/webhook/owntracks_test',
+        json=LOCATION_MESSAGE,
+        headers={
+            'X-Limit-u': 'Paulus',
+            'X-Limit-d': 'Pixel',
+        }
+    )
 
     assert resp.status == 200
 
@@ -78,9 +70,14 @@ def test_handle_valid_message(mock_client):
 @asyncio.coroutine
 def test_handle_valid_minimal_message(mock_client):
     """Test that we forward messages correctly to OwnTracks."""
-    resp = yield from mock_client.post('/api/webhook/owntracks_test?'
-                                       'u=test&d=test',
-                                       json=MINIMAL_LOCATION_MESSAGE)
+    resp = yield from mock_client.post(
+        '/api/webhook/owntracks_test',
+        json=MINIMAL_LOCATION_MESSAGE,
+        headers={
+            'X-Limit-u': 'Paulus',
+            'X-Limit-d': 'Pixel',
+        }
+    )
 
     assert resp.status == 200
 
@@ -91,8 +88,14 @@ def test_handle_valid_minimal_message(mock_client):
 @asyncio.coroutine
 def test_handle_value_error(mock_client):
     """Test we don't disclose that this is a valid webhook."""
-    resp = yield from mock_client.post('/api/webhook/owntracks_test'
-                                       '?u=test&d=test', json='')
+    resp = yield from mock_client.post(
+        '/api/webhook/owntracks_test',
+        json='',
+        headers={
+            'X-Limit-u': 'Paulus',
+            'X-Limit-d': 'Pixel',
+        }
+    )
 
     assert resp.status == 200
 
@@ -103,10 +106,15 @@ def test_handle_value_error(mock_client):
 @asyncio.coroutine
 def test_returns_error_missing_username(mock_client):
     """Test that an error is returned when username is missing."""
-    resp = yield from mock_client.post('/api/webhook/owntracks_test?d=test',
-                                       json=LOCATION_MESSAGE)
+    resp = yield from mock_client.post(
+        '/api/webhook/owntracks_test',
+        json=LOCATION_MESSAGE,
+        headers={
+            'X-Limit-d': 'Pixel',
+        }
+    )
 
-    assert resp.status == 200
+    assert resp.status == 400
 
     json = yield from resp.json()
     assert json == {'error': 'You need to supply username.'}
@@ -115,10 +123,27 @@ def test_returns_error_missing_username(mock_client):
 @asyncio.coroutine
 def test_returns_error_missing_device(mock_client):
     """Test that an error is returned when device name is missing."""
-    resp = yield from mock_client.post('/api/webhook/owntracks_test?u=test',
-                                       json=LOCATION_MESSAGE)
+    resp = yield from mock_client.post(
+        '/api/webhook/owntracks_test',
+        json=LOCATION_MESSAGE,
+        headers={
+            'X-Limit-u': 'Paulus',
+        }
+    )
 
     assert resp.status == 200
 
     json = yield from resp.json()
-    assert json == {'error': 'You need to supply device name.'}
+    assert json == []
+
+
+async def test_config_flow_import(hass):
+    """Test that we automatically create a config flow."""
+    assert not hass.config_entries.async_entries('owntracks')
+    assert await async_setup_component(hass, 'owntracks', {
+        'owntracks': {
+
+        }
+    })
+    await hass.async_block_till_done()
+    assert hass.config_entries.async_entries('owntracks')
