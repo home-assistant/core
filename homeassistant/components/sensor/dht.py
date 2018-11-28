c37101a3c2c3a0194e28450c715ec9ad1ff188be
Fabian Affolter <mail@fabian-affolter.ch>
2018-11-28 13:38:26 +0100

    Upgrade Adafruit-DHT to 1.4.0 (fixes #15847) (#18614)

--- homeassistant/components/sensor/dht.py
+++ homeassistant/components/sensor/dht.py
@@ -17,7 +17,7 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 from homeassistant.util.temperature import celsius_to_fahrenheit
 
-REQUIREMENTS = ['Adafruit-DHT==1.3.4']
+REQUIREMENTS = ['Adafruit-DHT==1.4.0']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -57,9 +57,9 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     SENSOR_TYPES[SENSOR_TEMPERATURE][1] = hass.config.units.temperature_unit
     available_sensors = {
+        "AM2302": Adafruit_DHT.AM2302,
         "DHT11": Adafruit_DHT.DHT11,
         "DHT22": Adafruit_DHT.DHT22,
-        "AM2302": Adafruit_DHT.AM2302
     }
     sensor = available_sensors.get(config.get(CONF_SENSOR))
     pin = config.get(CONF_PIN)
