8841485f63bcda90de4cc6bd2505d585cfd255a8
Daniel Høyer Iversen <mail@dahoiv.net>
2018-11-30 14:45:40 +0100

    update netatmo library (#18823)

--- homeassistant/components/netatmo.py
+++ homeassistant/components/netatmo.py
@@ -16,7 +16,7 @@ from homeassistant.helpers import discovery
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util import Throttle
 
-REQUIREMENTS = ['pyatmo==1.3']
+REQUIREMENTS = ['pyatmo==1.4']
 
 _LOGGER = logging.getLogger(__name__)
 
