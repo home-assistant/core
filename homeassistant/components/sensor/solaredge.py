3b4b13d1d492aa0aa6c7f1128166739bc8638c2e
Gido Hakvoort <gido@hakvoort.it>
2018-11-30 13:39:34 +0100

    Update solaredge to pass lint and flake8

--- homeassistant/components/sensor/solaredge.py
+++ homeassistant/components/sensor/solaredge.py
@@ -1,13 +1,13 @@
 """
-Support for SolarEdge Monitoring API
+Support for SolarEdge Monitoring API.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.solaredge/
 """
 
 import asyncio
-from datetime import datetime, timedelta
 import json
+from datetime import timedelta
 import logging
 
 import requests
@@ -24,47 +24,46 @@ CONF_APIKEY = "api_key"
 CONF_SITEID = "site_id"
 
 DELAY_OK = 10
-DELAY_NOT_OKAY = 20
+DELAY_NOT_OK = 20
 
 # Supported sensor types:
 # Key: ['name', unit, icon]
 SENSOR_TYPES = {
-  'lifeTimeData': ["Lifetime energy", 'Wh', 'mdi:solar-power'],
-  'lastYearData': ["Energy this year", 'Wh', 'mdi:solar-power'],
-  'lastMonthData' : ["Energy this month", 'Wh', 'mdi:solar-power'],
-  'lastDayData' : ["Energy today", 'Wh', 'mdi:solar-power'],
-  'currentPower' : ["Current Power", 'W', 'mdi:solar-power']
+    'lifeTimeData': ["Lifetime energy", 'Wh', 'mdi:solar-power'],
+    'lastYearData': ["Energy this year", 'Wh', 'mdi:solar-power'],
+    'lastMonthData': ["Energy this month", 'Wh', 'mdi:solar-power'],
+    'lastDayData': ["Energy today", 'Wh', 'mdi:solar-power'],
+    'currentPower': ["Current Power", 'W', 'mdi:solar-power']
 }
 
 _LOGGER = logging.getLogger(__name__)
 
 # Request parameters will be set during platform setup.
-url = 'https://monitoringapi.solaredge.com/site/{siteId}/overview'
-params = {}
+URL = 'https://monitoringapi.solaredge.com/site/{siteId}/overview'
+
 
 @asyncio.coroutine
-def async_setup_platform(hass, config, async_add_entities, 
-        discovery_info=None):
-    global url, params
+def async_setup_platform(hass, config, async_add_entities,
+                         discovery_info=None):
+    """Create the SolarEdge Monitoring API sensor."""
+    api_key = config.get(CONF_APIKEY, None)
+    site_id = config.get(CONF_SITEID, None)
 
-    apiKey = config.get(CONF_APIKEY, None)
-    siteId = config.get(CONF_SITEID, None)
-
-    if None in (apiKey, siteId):
-       _LOGGER.error("api_key or site_id not set in Home Assistant config")
-       return False
+    if None in (api_key, site_id):
+        _LOGGER.error("api_key or site_id not set in Home Assistant config")
+        return False
 
     # Setup request url and parameters.
-    url = url.format(siteId=siteId)
-    params['api_key'] = apiKey
-    
+    url = URL.format(siteId=site_id)
+    params = {'api_key': api_key}
+
     # Create solaredge data service which will retrieve and update the data.
-    data = SolarEdgeData(hass)
-   
+    data = SolarEdgeData(hass, url, params)
+
     # Create a new sensor for each sensor type.
     entities = []
-    for sensorType in config[CONF_MONITORED_VARIABLES]:
-        sensor = SolarEdgeSensor(sensorType, data)
+    for sensor_type in config[CONF_MONITORED_VARIABLES]:
+        sensor = SolarEdgeSensor(sensor_type, data)
         entities.append(sensor)
 
     async_add_entities(entities, True)
@@ -72,9 +71,12 @@ def async_setup_platform(hass, config, async_add_entities,
     # Schedule first data service update straight away.
     async_track_point_in_utc_time(hass, data.async_update, dt_util.utcnow())
 
+
 class SolarEdgeSensor(Entity):
-    
+    """Representation of an SolarEdge Monitoring API sensor."""
+
     def __init__(self, sensorType, data):
+        """Initialize the sensor."""
         self.type = sensorType
         self.data = data
 
@@ -83,49 +85,58 @@ class SolarEdgeSensor(Entity):
 
     @property
     def name(self):
+        """Return the name."""
         return 'solaredge_' + self.type
-    
+
     @property
     def unit_of_measurement(self):
+        """Return the unit of measurement."""
         return self._unit_of_measurement
 
     @property
     def icon(self):
+        """Return the sensor icon."""
         return SENSOR_TYPES[self.type][2]
 
     @property
     def state(self):
+        """Return the state of the sensor."""
         if self.type in self.data.data:
             return self.data.data.get(self.type)
-        else:
-            return 0
+        return 0
+
 
 class SolarEdgeData:
+    """Get and update the latest data."""
 
-    def __init__(self, hass):
+    def __init__(self, hass, url, params):
+        """Initialize the data object."""
         self.hass = hass
         self.data = {}
+        self.url = url
+        self.params = params
 
     @asyncio.coroutine
     def schedule_update(self, minutes):
+        """Schedule an update after minute minutes."""
         nxt = dt_util.utcnow() + timedelta(minutes=minutes)
         _LOGGER.debug("Scheduling next SolarEdge update in %s minutes",
-                minutes)
+                      minutes)
         async_track_point_in_utc_time(self.hass, self.async_update, nxt)
-    
+
     @asyncio.coroutine
     def async_update(self, *_):
+        """Update the data from the SolarEdge Monitoring API."""
+        response = requests.get(self.url, params=self.params)
 
-        response = requests.get(url, params=params)
-        
         if response.status_code != requests.codes.ok:
-            _LOGGER.debug("failed to retrieve data from SolarEdge API, 
+            _LOGGER.debug("failed to retrieve data from SolarEdge API, \
                     delaying next update")
             yield from self.schedule_update(DELAY_NOT_OK)
             return
 
         data = json.loads(response.text)
-        
+
         if 'overview' not in data:
             _LOGGER.debug("Missing overview data, delaying next update")
             yield from self.schedule_update(DELAY_NOT_OK)
@@ -134,14 +145,14 @@ class SolarEdgeData:
         overview = data['overview']
 
         self.data = {}
-        
+
         for item in overview:
             value = overview[item]
             if 'energy' in value:
-              self.data[item] = value['energy']
+                self.data[item] = value['energy']
             elif 'power' in value:
-              self.data[item] = value['power']
-        
+                self.data[item] = value['power']
+
         _LOGGER.debug("Updated SolarEdge overview data: %s", self.data)
 
         yield from self.schedule_update(DELAY_OK)
