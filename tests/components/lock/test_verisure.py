192ad068d815233f1cc1fef22211547ab10f496a
Andrew Hayworth <ahayworth@gmail.com>
2018-12-01 00:49:40 -0600

    Allow verisure locks to be configured with a default code

---
+++ tests/components/lock/test_verisure.py
@@ -0,0 +1,62 @@
+"""Tests for the Verisure platform."""
+
+from unittest.mock import Mock, patch
+from homeassistant.const import STATE_LOCKED, STATE_UNLOCKED
+
+
+NO_DEFAULT_LOCK_CODE_CONFIG = {
+    'locks': True,
+}
+
+DEFAULT_LOCK_CODE_CONFIG = {
+    'locks': True,
+    'default_lock_code': '9999',
+}
+
+
+async def test_verisure_no_default_code(hass):
+    """Test configs without a default lock code."""
+
+    with patch('homeassistant.components.lock.verisure.hub') as hub:
+        hub.config = NO_DEFAULT_LOCK_CODE_CONFIG
+
+        from homeassistant.components.lock.verisure import VerisureDoorlock
+        lock = VerisureDoorlock("lock")
+        tests = [
+            {'method': lock.lock, 'state': STATE_LOCKED},
+            {'method': lock.unlock, 'state': STATE_UNLOCKED},
+        ]
+
+        for test in tests:
+            lock.set_lock_state = Mock(name='set_lock_state',
+                                       return_value=None)
+
+            test['method']()
+            lock.set_lock_state.assert_not_called()
+
+            test['method'](code='12345')
+            lock.set_lock_state.assert_called_with('12345', test['state'])
+
+
+async def test_verisure_default_code(hass):
+    """Test configs with a default lock code."""
+
+    with patch('homeassistant.components.lock.verisure.hub') as hub:
+        hub.config = DEFAULT_LOCK_CODE_CONFIG
+
+        from homeassistant.components.lock.verisure import VerisureDoorlock
+        lock = VerisureDoorlock("lock")
+        tests = [
+            {'method': lock.lock, 'state': STATE_LOCKED},
+            {'method': lock.unlock, 'state': STATE_UNLOCKED},
+        ]
+
+        for test in tests:
+            lock.set_lock_state = Mock(name='set_lock_state',
+                                       return_value=None)
+
+            test['method']()
+            lock.set_lock_state.assert_called_with('9999', test['state'])
+
+            test['method'](code='12345')
+            lock.set_lock_state.assert_called_with('12345', test['state'])
