363984f0770479f08e13a7ad0e5ff57d272f75a9
Peter Balogh <peter.balogh2@gmail.com>
2018-12-02 21:09:33 +0100

    Removed unused functions

--- homeassistant/components/scene/fibaro.py
+++ homeassistant/components/scene/fibaro.py
@@ -7,7 +7,7 @@ https://home-assistant.io/components/scene.fibaro/
 import logging
 
 from homeassistant.components.scene import (
-    DOMAIN, Scene)
+    Scene)
 from homeassistant.components.fibaro import (
     FIBARO_CONTROLLER, FIBARO_DEVICES, FibaroDevice)
 
@@ -28,15 +28,6 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
 class FibaroScene(FibaroDevice, Scene):
     """Representation of a Fibaro scene entity."""
-
-    def __init__(self, fibaro_device, controller):
-        """Initialize the scene."""
-        super().__init__(fibaro_device, controller)
-
-    def update(self):
-        """Update the scene status."""
-        pass
-
     async def async_activate(self):
         """Activate the scene."""
         self.fibaro_device.start()
@@ -45,8 +36,3 @@ class FibaroScene(FibaroDevice, Scene):
     def name(self):
         """Return the name of the scene."""
         return self._name
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes of the scene."""
-        return None
