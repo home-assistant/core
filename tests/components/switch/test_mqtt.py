368f2b29ca4938ba48d020040fbf9eb0ff2d3b8d
emontnemery <erik@montnemery.com>
2018-12-02 16:16:46 +0100

    Reconfigure MQTT switch component if discovery info is changed (#18179)

--- tests/components/switch/test_mqtt.py
+++ tests/components/switch/test_mqtt.py
@@ -361,6 +361,42 @@ async def test_discovery_removal_switch(hass, mqtt_mock, caplog):
     assert state is None
 
 
+async def test_discovery_update_switch(hass, mqtt_mock, caplog):
+    """Test expansion of discovered switch."""
+    entry = MockConfigEntry(domain=mqtt.DOMAIN)
+    await async_start(hass, 'homeassistant', {}, entry)
+
+    data1 = (
+        '{ "name": "Beer",'
+        '  "status_topic": "test_topic",'
+        '  "command_topic": "test_topic" }'
+    )
+    data2 = (
+        '{ "name": "Milk",'
+        '  "status_topic": "test_topic",'
+        '  "command_topic": "test_topic" }'
+    )
+
+    async_fire_mqtt_message(hass, 'homeassistant/switch/bla/config',
+                            data1)
+    await hass.async_block_till_done()
+
+    state = hass.states.get('switch.beer')
+    assert state is not None
+    assert state.name == 'Beer'
+
+    async_fire_mqtt_message(hass, 'homeassistant/switch/bla/config',
+                            data2)
+    await hass.async_block_till_done()
+    await hass.async_block_till_done()
+
+    state = hass.states.get('switch.beer')
+    assert state is not None
+    assert state.name == 'Milk'
+    state = hass.states.get('switch.milk')
+    assert state is None
+
+
 async def test_entity_device_info_with_identifier(hass, mqtt_mock):
     """Test MQTT switch device registry integration."""
     entry = MockConfigEntry(domain=mqtt.DOMAIN)
