4d767e205945d5b739983dc8f1313095edf511fa
Jason Hunter <hunterjm@gmail.com>
2018-12-04 03:45:17 -0500

    New Events and Context Fixes (#18765)
    * Add new events for automation trigger and script run, fix context for image processing, add tests to ensure same context
    
    * remove custom logbook entry for automation and add new automation event to logbook
    
    * code review updates

--- homeassistant/components/image_processing/__init__.py
+++ homeassistant/components/image_processing/__init__.py
@@ -76,10 +76,14 @@ async def async_setup(hass, config):
         """Service handler for scan."""
         image_entities = component.async_extract_from_service(service)
 
-        update_task = [entity.async_update_ha_state(True) for
-                       entity in image_entities]
-        if update_task:
-            await asyncio.wait(update_task, loop=hass.loop)
+        update_tasks = []
+        for entity in image_entities:
+            entity.async_set_context(service.context)
+            update_tasks.append(
+                entity.async_update_ha_state(True))
+
+        if update_tasks:
+            await asyncio.wait(update_tasks, loop=hass.loop)
 
     hass.services.async_register(
         DOMAIN, SERVICE_SCAN, async_scan_service,
