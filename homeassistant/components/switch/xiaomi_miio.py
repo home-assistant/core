c4a8b347a041d0e8d2b938dc27d7b97790619a15
Sebastian Muszynski <basti@linkt.de>
2018-12-06 20:21:04 +0100

    Add next generation of the Xiaomi Mi Smart Plug (chuangmi.plug.hmi205)

--- homeassistant/components/switch/xiaomi_miio.py
+++ homeassistant/components/switch/xiaomi_miio.py
@@ -39,6 +39,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
          'chuangmi.plug.m1',
          'chuangmi.plug.v2',
          'chuangmi.plug.v3',
+         'chuangmi.plug.hmi205',
          ]),
 })
 
@@ -146,7 +147,7 @@ async def async_setup_platform(hass, config, async_add_entities,
         device = XiaomiPowerStripSwitch(name, plug, model, unique_id)
         devices.append(device)
         hass.data[DATA_KEY][host] = device
-    elif model in ['chuangmi.plug.m1', 'chuangmi.plug.v2']:
+    elif model in ['chuangmi.plug.m1', 'chuangmi.plug.v2', 'chuangmi.plug.hmi205']:
         from miio import ChuangmiPlug
         plug = ChuangmiPlug(host, token, model=model)
         device = XiaomiPlugGenericSwitch(name, plug, model, unique_id)
