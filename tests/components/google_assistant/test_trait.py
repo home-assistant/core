7f3d21ac08bb29d356302084baa7da4a3ddf7eb3
phnx <eanagley@gmail.com>
2018-12-07 01:15:04 -0500

    home-assistant/home-assistant#18645: Fix climate mode mapping.

--- tests/components/google_assistant/test_trait.py
+++ tests/components/google_assistant/test_trait.py
@@ -679,11 +679,11 @@ async def test_temperature_setting_climate_range(hass):
             climate.ATTR_MAX_TEMP: 80
         }), BASIC_CONFIG)
     assert trt.sync_attributes() == {
-        'availableThermostatModes': 'off,cool,heat,heatcool',
+        'availableThermostatModes': 'off,cool,heat,auto',
         'thermostatTemperatureUnit': 'F',
     }
     assert trt.query_attributes() == {
-        'thermostatMode': 'heatcool',
+        'thermostatMode': 'auto',
         'thermostatTemperatureAmbient': 21.1,
         'thermostatHumidityAmbient': 25,
         'thermostatTemperatureSetpointLow': 18.3,
@@ -709,7 +709,7 @@ async def test_temperature_setting_climate_range(hass):
     calls = async_mock_service(
         hass, climate.DOMAIN, climate.SERVICE_SET_OPERATION_MODE)
     await trt.execute(trait.COMMAND_THERMOSTAT_SET_MODE, {
-        'thermostatMode': 'heatcool',
+        'thermostatMode': 'auto',
     })
     assert len(calls) == 1
     assert calls[0].data == {
