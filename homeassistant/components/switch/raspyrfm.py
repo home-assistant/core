07ed96b6140af1953ca385ded9eb1f2c54515821
Markus Ressel <mail@markusressel.de>
2018-12-08 20:26:52 +0100

    change config to allow the definition of multiple switches without redefining the gateway

--- homeassistant/components/switch/raspyrfm.py
+++ homeassistant/components/switch/raspyrfm.py
@@ -11,7 +11,7 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.switch import SwitchDevice
 from homeassistant.const import CONF_NAME, DEVICE_DEFAULT_NAME, CONF_HOST, \
-    CONF_PORT, CONF_PLATFORM
+    CONF_PORT, CONF_PLATFORM, CONF_SWITCHES
 
 REQUIREMENTS = ['raspyrfm-client==1.2.8']
 _LOGGER = logging.getLogger(__name__)
@@ -26,15 +26,17 @@ DEFAULT_HOST = '127.0.0.1'
 # define configuration parameters
 PLATFORM_SCHEMA = vol.Schema({
     vol.Required(CONF_PLATFORM): 'raspyrfm',
-    vol.Optional(CONF_NAME, default=DEVICE_DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_GATEWAY_MANUFACTURER): cv.string,
-    vol.Optional(CONF_GATEWAY_MODEL): cv.string,
-    vol.Required(CONF_CONTROLUNIT_MANUFACTURER): cv.string,
-    vol.Required(CONF_CONTROLUNIT_MODEL): cv.string,
-    vol.Required(CONF_CHANNEL_CONFIG): cv.match_all,
     vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
     vol.Optional(CONF_PORT, default=None):
         vol.All(vol.Coerce(int), vol.Range(min=1, max=65535)),
+    vol.Optional(CONF_GATEWAY_MANUFACTURER): cv.string,
+    vol.Optional(CONF_GATEWAY_MODEL): cv.string,
+    vol.Required(CONF_SWITCHES, default={}): vol.Schema([{
+        vol.Optional(CONF_NAME, default=DEVICE_DEFAULT_NAME): cv.string,
+        vol.Required(CONF_CONTROLUNIT_MANUFACTURER): cv.string,
+        vol.Required(CONF_CONTROLUNIT_MODEL): cv.string,
+        vol.Required(CONF_CHANNEL_CONFIG): cv.match_all,
+    }])
 }, extra=vol.ALLOW_EXTRA)
 
 
@@ -51,51 +53,49 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         import Manufacturer
 
     # read configuration
-    name = config.get(CONF_NAME)
     gateway_manufacturer = config.get(CONF_GATEWAY_MANUFACTURER,
                                       Manufacturer.SEEGEL_SYSTEME.value)
     gateway_model = config.get(CONF_GATEWAY_MODEL, GatewayModel.RASPYRFM.value)
-    controlunit_manufacturer = config.get(CONF_CONTROLUNIT_MANUFACTURER)
-    controlunit_model = config.get(CONF_CONTROLUNIT_MODEL)
-    channel_config = config.get(CONF_CHANNEL_CONFIG)
     host = config.get(CONF_HOST, DEFAULT_HOST)
     port = config.get(CONF_PORT)
 
+    switches = config.get(CONF_SWITCHES)
+
     # create raspyrfm client
     raspyrfm_client = RaspyRFMClient()
-
-    # TODO: automatically detect clients
-    #
-    # if host is None:
-    #    # search for gateways on local network
-    #    gateways = raspyrfm_client.search()
-    #    for gateway in gateways:
-    #        if gateway.get_manufacturer() is Manufacturer(gateway_manufacturer)
-    #
-    # else:
-    #    pass
 
     # try to get controlunit from client
     gateway = raspyrfm_client.get_gateway(Manufacturer(gateway_manufacturer),
                                           GatewayModel(gateway_model), host,
                                           port)
-    controlunit = raspyrfm_client.get_controlunit(
-        Manufacturer(controlunit_manufacturer),
-        ControlUnitModel(controlunit_model))
 
-    # convert any key thats not a string into a string (needed for api)
-    channel = {}
-    for key in channel_config:
-        channel[str(key)] = channel_config[key]
+    switch_entities = []
+    for switch in switches:
+        name = config.get(CONF_NAME)
+        controlunit_manufacturer = switch.get(CONF_CONTROLUNIT_MANUFACTURER)
+        controlunit_model = switch.get(CONF_CONTROLUNIT_MODEL)
+        channel_config = switch.get(CONF_CHANNEL_CONFIG)
 
-    # setup channel
-    controlunit.set_channel_config(**channel)
+        controlunit = raspyrfm_client.get_controlunit(
+            Manufacturer(controlunit_manufacturer),
+            ControlUnitModel(controlunit_model))
 
-    # create switch object
-    switch = RaspyRFMSwitch(raspyrfm_client, name, gateway, controlunit)
+        # convert any key thats not a string into a string (needed for api)
+        channel = {}
+        for key in channel_config:
+            channel[str(key)] = channel_config[key]
+
+        # setup channel
+        controlunit.set_channel_config(**channel)
+
+        # create switch object
+        switch = RaspyRFMSwitch(raspyrfm_client, name, gateway, controlunit)
+        switch_entities.append(switch)
 
     # add it to home assistant
-    add_devices([switch])
+    add_devices(switch_entities)
+
+    return True
 
 
 class RaspyRFMSwitch(SwitchDevice):
