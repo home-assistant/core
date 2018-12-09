38c5dac79cccab072707cdb2984d264122dd8f52
Krzysztof M <c-soft@users.noreply.github.com>
2018-12-09 18:25:37 +0100

    Added support for output monitoring. Added initial status check for the alarm.

--- homeassistant/components/satel_integra.py
+++ homeassistant/components/satel_integra.py
@@ -40,6 +40,7 @@ CONF_ARM_HOME_MODE = 'arm_home_mode'
 CONF_ZONE_NAME = 'name'
 CONF_ZONE_TYPE = 'type'
 CONF_ZONES = 'zones'
+CONF_OUTPUTS = 'outputs'
 
 ZONES = 'zones'
 
@@ -49,6 +50,7 @@ SIGNAL_PANEL_ARM_HOME = 'satel_integra.panel_arm_home'
 SIGNAL_PANEL_DISARM = 'satel_integra.panel_disarm'
 
 SIGNAL_ZONES_UPDATED = 'satel_integra.zones_updated'
+SIGNAL_OUTPUTS_UPDATED = 'satel_integra.outputs_updated'
 
 ZONE_SCHEMA = vol.Schema({
     vol.Required(CONF_ZONE_NAME): cv.string,
@@ -63,6 +65,7 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Optional(CONF_ARM_HOME_MODE,
                      default=DEFAULT_CONF_ARM_HOME_MODE): vol.In([1, 2, 3]),
         vol.Optional(CONF_ZONES): {vol.Coerce(int): ZONE_SCHEMA},
+        vol.Optional(CONF_OUTPUTS): {vol.Coerce(int): ZONE_SCHEMA},
     }),
 }, extra=vol.ALLOW_EXTRA)
 
@@ -71,14 +74,16 @@ async def async_setup(hass, config):
     """Set up the Satel Integra component."""
     conf = config.get(DOMAIN)
 
-    zones = conf.get(CONF_ZONES)
+    zones = conf.get(CONF_ZONES) if conf.get(CONF_ZONES) is not None else []
+    outputs = \
+        conf.get(CONF_OUTPUTS) if conf.get(CONF_OUTPUTS) is not None else []
     host = conf.get(CONF_DEVICE_HOST)
     port = conf.get(CONF_DEVICE_PORT)
     partition = conf.get(CONF_DEVICE_PARTITION)
 
     from satel_integra.satel_integra import AsyncSatel, AlarmState
 
-    controller = AsyncSatel(host, port, hass.loop, zones, [], partition)
+    controller = AsyncSatel(host, port, hass.loop, zones, outputs, partition)
 
     hass.data[DATA_SATEL] = controller
 
@@ -101,7 +106,8 @@ async def async_setup(hass, config):
 
     task_zones = hass.async_create_task(
         async_load_platform(hass, 'binary_sensor', DOMAIN,
-                            {CONF_ZONES: zones}, config))
+                            {CONF_ZONES: zones, CONF_OUTPUTS: outputs}, config)
+        )
 
     await asyncio.wait([task_control_panel, task_zones], loop=hass.loop)
 
@@ -134,8 +140,14 @@ async def async_setup(hass, config):
     @callback
     def zones_update_callback(status):
         """Update zone objects as per notification from the alarm."""
-        _LOGGER.debug("Zones callback , status: %s", status)
+        _LOGGER.debug("Zones callback, status: %s", status)
         async_dispatcher_send(hass, SIGNAL_ZONES_UPDATED, status[ZONES])
+
+    @callback
+    def outputs_update_callback(status):
+        """Update zone objects as per notification from the alarm."""
+        _LOGGER.debug("Outputs updated callback , status: %s", status)
+        async_dispatcher_send(hass, SIGNAL_OUTPUTS_UPDATED, status["outputs"])
 
     # Create a task instead of adding a tracking job, since this task will
     # run until the connection to satel_integra is closed.
@@ -143,7 +155,8 @@ async def async_setup(hass, config):
     hass.loop.create_task(
         controller.monitor_status(
             alarm_status_update_callback,
-            zones_update_callback)
+            zones_update_callback,
+            outputs_update_callback)
     )
 
     return True
