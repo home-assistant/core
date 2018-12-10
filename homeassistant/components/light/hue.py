29c96de8dc3e93cd4a629dcb2a3e135178457307
starkillerOG <starkiller.og@gmail.com>
2018-12-10 22:54:38 +0100

    docstring

--- homeassistant/components/light/hue.py
+++ homeassistant/components/light/hue.py
@@ -215,9 +215,9 @@ class HueLight(Light):
     """Representation of a Hue light."""
 
     def __init__(self, light, request_bridge_update, bridge, is_group=False):
+        """Initialize the light."""
         from rgbxy import Converter, GamutA, GamutB, GamutC
 
-        """Initialize the light."""
         self.light = light
         self.async_request_bridge_update = request_bridge_update
         self.bridge = bridge
