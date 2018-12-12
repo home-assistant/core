a4fc4a4b2c679e688256b3c7e489064d6a910f9e
tmd224 <tmdisanto@gmail.com>
2018-12-11 19:49:04 -0500

    Updating link to documentation in doc-string.  File already named in doc repo

--- homeassistant/components/sensor/ambient_station.py
+++ homeassistant/components/sensor/ambient_station.py
@@ -2,7 +2,7 @@
 Support for Ambient Weather Station Service.
 
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/sensor.ambstation/
+https://home-assistant.io/components/sensor.ambient_station/
 """
 
 import asyncio
