865b130b94cc49084e46136c33b69709818f35c3
Paulus Schoutsen <balloob@gmail.com>
2018-12-13 15:30:20 +0100

    Check admin permission before able to manage config entries

--- homeassistant/exceptions.py
+++ homeassistant/exceptions.py
@@ -47,12 +47,18 @@ class Unauthorized(HomeAssistantError):
     def __init__(self, context: Optional['Context'] = None,
                  user_id: Optional[str] = None,
                  entity_id: Optional[str] = None,
+                 config_entry_id: Optional[str] = None,
+                 perm_category: Optional[str] = None,
                  permission: Optional[Tuple[str]] = None) -> None:
         """Unauthorized error."""
         super().__init__(self.__class__.__name__)
         self.context = context
         self.user_id = user_id
         self.entity_id = entity_id
+        self.config_entry_id = config_entry_id
+        # Not all actions have an ID (like adding config entry)
+        # We then use this fallback to know what category was unauth
+        self.perm_category = perm_category
         self.permission = permission
 
 
