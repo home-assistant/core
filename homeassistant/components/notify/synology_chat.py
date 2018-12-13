f517da9b4c14f0a4edb01c06e58a286c897747b8
bremor <34525505+bremor@users.noreply.github.com>
2018-12-14 08:48:48 +1100

    Added verify_ssl option to notify.synology_chat
    Python requests will verify ssl by default, this configuration options allows the user to specify if they want to verify ssl or not. Non breaking change, default is True - do verify ssl.

--- homeassistant/components/notify/synology_chat.py
+++ homeassistant/components/notify/synology_chat.py
@@ -1,5 +1,6 @@
 """
 SynologyChat platform for notify component.
+
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/notify.synology_chat/
 """
