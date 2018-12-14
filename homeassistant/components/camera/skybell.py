5a09628c5cad83fc40d079ad130f2144b2f02a73
nhorvath <nhorvath@gmail.com>
2018-12-14 18:12:34 -0500

    add camera selection config to skybell camera

--- homeassistant/components/camera/skybell.py
+++ homeassistant/components/camera/skybell.py
@@ -8,6 +8,11 @@ from datetime import timedelta
 import logging
 
 import requests
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_MONITORED_CONDITIONS
+import homeassistant.helpers.config_validation as cv
 
 from homeassistant.components.camera import Camera
 from homeassistant.components.skybell import (
@@ -19,14 +24,32 @@ _LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(seconds=90)
 
+IMAGE_AVATAR = 'avatar'
+IMAGE_ACTIVITY = 'activity'
+
+CONF_ACTIVITY_NAME = 'activity_name'
+CONF_AVATAR_NAME = 'avatar_name'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_MONITORED_CONDITIONS, default=[IMAGE_AVATAR]):
+        vol.All(cv.ensure_list, [vol.In([IMAGE_AVATAR, IMAGE_ACTIVITY])]),
+    vol.Optional(CONF_ACTIVITY_NAME, default=""): cv.string,
+    vol.Optional(CONF_AVATAR_NAME, default=""): cv.string,
+})
+
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the platform for a Skybell device."""
+    cond = config[CONF_MONITORED_CONDITIONS]
+    names = {}
+    names[IMAGE_ACTIVITY] = config[CONF_ACTIVITY_NAME]
+    names[IMAGE_AVATAR] = config[CONF_AVATAR_NAME]
     skybell = hass.data.get(SKYBELL_DOMAIN)
 
     sensors = []
     for device in skybell.get_devices():
-        sensors.append(SkybellCamera(device))
+        for camera_type in cond:
+            sensors.append(SkybellCamera(device, type, names[camera_type]))
 
     add_entities(sensors, True)
 
@@ -34,11 +57,15 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class SkybellCamera(SkybellDevice, Camera):
     """A camera implementation for Skybell devices."""
 
-    def __init__(self, device):
+    def __init__(self, device, camera_type, name=""):
         """Initialize a camera for a Skybell device."""
+        self._type = camera_type
         SkybellDevice.__init__(self, device)
         Camera.__init__(self)
-        self._name = self._device.name
+        if name != "":
+            self._name = self._device.name + " " + name
+        else:
+            self._name = self._device.name
         self._url = None
         self._response = None
 
@@ -47,12 +74,19 @@ class SkybellCamera(SkybellDevice, Camera):
         """Return the name of the sensor."""
         return self._name
 
+    @property
+    def image_url(self):
+        """Get the camera image url based on type."""
+        if self._type == IMAGE_ACTIVITY:
+            return self._device.activity_image
+        return self._device.image
+
     def camera_image(self):
         """Get the latest camera image."""
         super().update()
 
-        if self._url != self._device.image:
-            self._url = self._device.image
+        if self._url != self.image_url:
+            self._url = self.image_url
 
             try:
                 self._response = requests.get(
