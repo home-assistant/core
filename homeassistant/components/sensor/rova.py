a95feed43967d1d3196fd7b56fbc6882d0c0a782
Gido <gido@hakvoort.it>
2018-12-15 15:58:30 +0100

    Update debug to error messages Change CONF_MONITORED_VARIABLES to CONF_MONITORED_CONDITIONS Update async update call to normal update

--- homeassistant/components/sensor/rova.py
+++ homeassistant/components/sensor/rova.py
@@ -10,8 +10,8 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.components.light import PLATFORM_SCHEMA
-from homeassistant.const import CONF_MONITORED_VARIABLES, CONF_NAME
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_MONITORED_CONDITIONS, CONF_NAME
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
@@ -22,7 +22,8 @@ REQUIREMENTS = ['rova==0.0.1']
 CONF_ZIP_CODE = 'zip_code'
 CONF_HOUSE_NUMBER = 'house_number'
 
-UPDATE_DELAY = timedelta(days=1)
+UPDATE_DELAY = timedelta(hours=12)
+SCAN_INTERVAL = timedelta(hours=12)
 
 # Supported sensor types:
 SENSOR_TYPES = {
@@ -35,23 +36,20 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_ZIP_CODE): cv.string,
     vol.Required(CONF_HOUSE_NUMBER): cv.string,
     vol.Optional(CONF_NAME, default='Rova'): cv.string,
-    vol.Optional(CONF_MONITORED_VARIABLES, default=['gft']):
+    vol.Optional(CONF_MONITORED_CONDITIONS, default=['gft']):
     vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)])
 })
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_platform(hass, config, async_add_entities,
-                               discovery_info=None):
+def setup_platform(hass, config, add_entities, discovery_info=None):
     """Create the Rova data service and sensors."""
     import rova.rova as r
 
     zip_code = config[CONF_ZIP_CODE]
     house_number = config[CONF_HOUSE_NUMBER]
     name = config[CONF_NAME]
-
-    _LOGGER.debug("Setting up Rova API")
 
     # Create new Rova object to  retrieve data
     api = r.Rova(zip_code, house_number)
@@ -61,11 +59,11 @@ async def async_setup_platform(hass, config, async_add_entities,
 
     # Create a new sensor for each garbage type.
     entities = []
-    for sensor_type in config[CONF_MONITORED_VARIABLES]:
+    for sensor_type in config[CONF_MONITORED_CONDITIONS]:
         sensor = RovaSensor(name, sensor_type, data)
         entities.append(sensor)
 
-    async_add_entities(entities, True)
+    add_entities(entities, True)
 
 
 class RovaSensor(Entity):
@@ -94,9 +92,9 @@ class RovaSensor(Entity):
         """Return the state of the sensor."""
         return self._state
 
-    async def async_update(self):
+    def update(self):
         """Get the latest data from the sensor and update the state."""
-        await self.hass.async_add_job(self.data.update)
+        self.data.update()
         pickup_date = self.data.data.get(self.code)
         if isinstance(pickup_date, datetime):
             self._state = pickup_date.isoformat()
@@ -119,7 +117,7 @@ class RovaData:
         try:
             items = self.api.get_calendar_items()
         except (ConnectTimeout, HTTPError):
-            _LOGGER.debug("Could not retrieve data, retry again tomorrow")
+            _LOGGER.error("Could not retrieve data, retry again later")
             return
 
         _LOGGER.debug(items)
