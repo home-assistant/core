69e422e19a3f90dfad53279ef2cde3c5309b75dd
Vijayshinva Karnure <vijayshinva@gmail.com>
2018-12-15 14:43:44 +0530

    rename sensor to azure

--- homeassistant/components/sensor/azure.py
+++ homeassistant/components/sensor/azure.py
@@ -2,7 +2,7 @@
 Sensor platform representing an Azure Virtual Machine.
 
 For more details about this sensor platform, please refer to the documentation
-at https://home-assistant.io/components/sensor.azure_vm/
+at https://home-assistant.io/components/sensor.azure/
 """
 
 from datetime import timedelta
@@ -14,17 +14,17 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.azure import DOMAIN as AZURE_DOMAIN
-from homeassistant.const import (CONF_NAME)
 
 _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = ['azure']
 
+CONF_VIRTUAL_MACHINE = 'virtual_machine'
 CONF_RESOURCE_GROUP = 'resource_group'
 
 SCAN_INTERVAL = timedelta(minutes=1)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONF_VIRTUAL_MACHINE): cv.string,
     vol.Required(CONF_RESOURCE_GROUP): cv.string,
 })
 
@@ -43,7 +43,7 @@ class AzureVmSensor(Entity):
 
         self._azure_compute_client = ComputeManagementClient(
             azure_subscription.credentials, azure_subscription.subscription_id)
-        self._name = config[CONF_NAME]
+        self._name = config[CONF_VIRTUAL_MACHINE]
         self._resource_group = config[CONF_RESOURCE_GROUP]
         self._state = None
 
