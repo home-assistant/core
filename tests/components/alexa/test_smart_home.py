9c6c351c6f208d009b643ae26d45b41ae8987984
abmantis <amfcalt@gmail.com>
2018-12-16 00:46:54 +0000

    add tests for auth and token requests

--- tests/components/alexa/test_smart_home.py
+++ tests/components/alexa/test_smart_home.py
@@ -11,6 +11,7 @@ from homeassistant.const import (
 from homeassistant.setup import async_setup_component
 from homeassistant.components import alexa
 from homeassistant.components.alexa import smart_home
+from homeassistant.components.alexa.auth import Auth
 from homeassistant.helpers import entityfilter
 
 from tests.common import async_mock_service
@@ -22,6 +23,7 @@ async def get_access_token():
 
 
 TEST_URL = "https://api.amazonalexa.com/v3/events"
+TEST_TOKEN_URL = "https://api.amazon.com/auth/o2/token"
 
 DEFAULT_CONFIG = smart_home.Config(
     endpoint=TEST_URL,
@@ -1684,3 +1686,67 @@ async def test_report_state(hass, aioclient_mock):
                "value"] == "NOT_DETECTED"
     assert call_json["event"]["endpoint"][
                "endpointId"] == "binary_sensor#test_contact"
+
+
+async def run_auth_get_access_token(hass, aioclient_mock, expires_in,
+                                    client_id, client_secret,
+                                    accept_grant_code, refresh_token):
+    """Helper to do auth and request a new token tests."""
+    aioclient_mock.post(TEST_TOKEN_URL,
+                        json={'access_token': 'the_access_token',
+                              'refresh_token': refresh_token,
+                              'expires_in': expires_in})
+
+    auth = Auth(hass, client_id, client_secret)
+    await auth.async_do_auth(accept_grant_code)
+    await auth.async_get_access_token()
+
+
+async def test_auth_get_access_token_expired(hass, aioclient_mock):
+    """Test the auth get access token function."""
+    client_id = "client123"
+    client_secret = "shhhhh"
+    accept_grant_code = "abcdefg"
+    refresh_token = "refresher"
+
+    await run_auth_get_access_token(hass, aioclient_mock, -5,
+                                    client_id, client_secret,
+                                    accept_grant_code, refresh_token)
+
+    assert len(aioclient_mock.mock_calls) == 2
+    calls = aioclient_mock.mock_calls
+
+    auth_call_json = calls[0][2]
+    token_call_json = calls[1][2]
+
+    assert auth_call_json["grant_type"] == "authorization_code"
+    assert auth_call_json["code"] == accept_grant_code
+    assert auth_call_json["client_id"] == client_id
+    assert auth_call_json["client_secret"] == client_secret
+
+    assert token_call_json["grant_type"] == "refresh_token"
+    assert token_call_json["refresh_token"] == refresh_token
+    assert token_call_json["client_id"] == client_id
+    assert token_call_json["client_secret"] == client_secret
+
+
+async def test_auth_get_access_token_not_expired(hass, aioclient_mock):
+    """Test the auth get access token function."""
+    client_id = "client123"
+    client_secret = "shhhhh"
+    accept_grant_code = "abcdefg"
+    refresh_token = "refresher"
+
+    await run_auth_get_access_token(hass, aioclient_mock, 555,
+                                    client_id, client_secret,
+                                    accept_grant_code, refresh_token)
+
+    assert len(aioclient_mock.mock_calls) == 1
+    call = aioclient_mock.mock_calls
+
+    auth_call_json = call[0][2]
+
+    assert auth_call_json["grant_type"] == "authorization_code"
+    assert auth_call_json["code"] == accept_grant_code
+    assert auth_call_json["client_id"] == client_id
+    assert auth_call_json["client_secret"] == client_secret
