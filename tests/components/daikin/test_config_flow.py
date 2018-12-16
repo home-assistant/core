7196384d11df7000175edace3769dc32ae7fa088
Fredrik Erlandsson <fredrik.e@gmail.com>
2018-12-16 14:07:32 +0100

    tox test fixes

--- tests/components/daikin/test_config_flow.py
+++ tests/components/daikin/test_config_flow.py
@@ -6,12 +6,16 @@ from unittest.mock import Mock, patch
 import pytest
 
 from homeassistant import data_entry_flow
-from homeassistant.components.daikin.const import (KEY_HOST, KEY_IP, KEY_MAC)
 from homeassistant.components.daikin import config_flow
-from tests.common import MockDependency
+from homeassistant.components.daikin.const import KEY_HOST, KEY_IP, KEY_MAC
+
+from tests.common import MockConfigEntry, MockDependency
+
+mac = 'AABBCCDDEEFF'
+host = '127.0.0.1'
 
 
-def init_config_flow(hass, side_effect=None):
+def init_config_flow(hass):
     """Init a configuration flow."""
     flow = config_flow.FlowHandler()
     flow.hass = hass
@@ -34,23 +38,19 @@ async def test_user(hass, mock_daikin):
     assert result['type'] == data_entry_flow.RESULT_TYPE_FORM
     assert result['step_id'] == 'user'
 
-    result = await flow.async_step_user({KEY_HOST: '127.0.0.1'})
+    result = await flow.async_step_user({KEY_HOST: host})
     assert result['type'] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result['title'] == '127.0.0.1'
-    assert result['data'][KEY_HOST] == '127.0.0.1'
-    assert result['data'][KEY_MAC] == 'AABBCCDDEEFF'
+    assert result['title'] == host
+    assert result['data'][KEY_HOST] == host
+    assert result['data'][KEY_MAC] == mac
 
 
 async def test_abort_if_already_setup(hass, mock_daikin):
     """Test we abort if Daikin is already setup."""
     flow = init_config_flow(hass)
+    MockConfigEntry(domain='daikin', data={KEY_MAC: mac}).add_to_hass(hass)
 
-    entry = Mock()
-    entry.data = {KEY_MAC: 'AABBCCDDEEFF'}
-    with patch.object(
-            hass.config_entries, 'async_entries', return_value=[entry]):
-        result = await flow.async_step_user({KEY_HOST: '127.0.0.1'})
-
+    result = await flow.async_step_user({KEY_HOST: host})
     assert result['type'] == data_entry_flow.RESULT_TYPE_ABORT
     assert result['reason'] == 'already_configured'
 
@@ -63,40 +63,32 @@ async def test_import(hass, mock_daikin):
     assert result['type'] == data_entry_flow.RESULT_TYPE_FORM
     assert result['step_id'] == 'user'
 
-    result = await flow.async_step_import({KEY_HOST: '127.0.0.1'})
+    result = await flow.async_step_import({KEY_HOST: host})
     assert result['type'] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result['title'] == '127.0.0.1'
-    assert result['data'][KEY_HOST] == '127.0.0.1'
-    assert result['data'][KEY_MAC] == 'AABBCCDDEEFF'
+    assert result['title'] == host
+    assert result['data'][KEY_HOST] == host
+    assert result['data'][KEY_MAC] == mac
 
 
 async def test_discovery(hass, mock_daikin):
     """Test discovery step."""
     flow = init_config_flow(hass)
 
-    result = await flow.async_step_discovery({
-        KEY_IP: '127.0.0.1',
-        KEY_MAC: 'AABBCCDDEEFF'
-    })
+    result = await flow.async_step_discovery({KEY_IP: host, KEY_MAC: mac})
     assert result['type'] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result['title'] == '127.0.0.1'
-    assert result['data'][KEY_HOST] == '127.0.0.1'
-    assert result['data'][KEY_MAC] == 'AABBCCDDEEFF'
+    assert result['title'] == host
+    assert result['data'][KEY_HOST] == host
+    assert result['data'][KEY_MAC] == mac
 
 
-async def test_device_abort(hass, mock_daikin):
+@pytest.mark.parametrize('s_effect,reason',
+                         [(asyncio.TimeoutError, 'device_timeout'),
+                          (Exception, 'device_fail')])
+async def test_device_abort(hass, mock_daikin, s_effect, reason):
     """Test device abort."""
     flow = init_config_flow(hass)
+    mock_daikin.Appliance.side_effect = s_effect
 
-    with patch.object(
-            flow.hass, 'async_add_executor_job',
-            side_effect=asyncio.TimeoutError):
-        result = await flow.async_step_user({KEY_HOST: '127.0.0.1'})
+    result = await flow.async_step_user({KEY_HOST: host})
     assert result['type'] == data_entry_flow.RESULT_TYPE_ABORT
-    assert result['reason'] == 'device_timeout'
-
-    with patch.object(
-            flow.hass, 'async_add_executor_job', side_effect=Exception):
-        result = await flow.async_step_user({KEY_HOST: '127.0.0.1'})
-    assert result['type'] == data_entry_flow.RESULT_TYPE_ABORT
-    assert result['reason'] == 'device_fail'
+    assert result['reason'] == reason
