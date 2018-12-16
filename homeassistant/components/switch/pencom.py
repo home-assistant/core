06bfb3e23f9892a4792ef2bd4646a9c08a524dee
Michael Dubno <michael@dubno.com>
2018-12-16 18:37:36 -0500

    Cleaned up for submission.

--- homeassistant/components/switch/pencom.py
+++ homeassistant/components/switch/pencom.py
@@ -1,28 +1,25 @@
 """Pencom relay control.
 
 For more details about this component, please refer to the documentation at
-http://home-assistant.io/components/pencom
+http://home-assistant.io/components/switch.pencom
 """
 import logging
 
 import voluptuous as vol
 
-# Import the device class from the component that you want to support
 from homeassistant.components.switch import SwitchDevice, PLATFORM_SCHEMA
 from homeassistant.const import CONF_HOST, CONF_PORT, CONF_NAME
+from homeassistant.exceptions import PlatformNotReady
 import homeassistant.helpers.config_validation as cv
 
-# Home Assistant depends on 3rd party packages for API specific code.
 REQUIREMENTS = ['pencompy==0.0.3']
 
 _LOGGER = logging.getLogger(__name__)
 
-# Number of boards connected to the serial port
 CONF_BOARDS = 'boards'
 CONF_BOARD = 'board'
 CONF_ADDR = 'addr'
 CONF_RELAYS = 'relays'
-CONF_RELAY = 'relay'
 
 RELAY_SCHEMA = vol.Schema({
     vol.Required(CONF_NAME): cv.string,
@@ -30,7 +27,6 @@ RELAY_SCHEMA = vol.Schema({
     vol.Optional(CONF_BOARD, default=0): cv.positive_int,
 })
 
-# Validation of the user's configuration
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
     vol.Required(CONF_PORT): cv.port,
@@ -44,26 +40,25 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     from pencompy.pencompy import Pencompy
 
     # Assign configuration variables.
-    host = config.get(CONF_HOST)
-    port = config.get(CONF_PORT)
-    boards = config.get(CONF_BOARDS)
+    host = config[CONF_HOST]
+    port = config[CONF_PORT]
+    boards = config[CONF_BOARDS]
 
-    # Setup connection with devices/cloud.
+    # Setup connection
     try:
         hub = Pencompy(host, port, boards=boards)
     except OSError as error:
         _LOGGER.error("Could not connect to pencompy: %s", error)
-        return False
+        raise PlatformNotReady
 
     # Add devices.
     devs = []
-    for relay in config.get(CONF_RELAYS):
-        name = relay.get(CONF_NAME)
-        board = relay.get(CONF_BOARD)
-        addr = relay.get(CONF_ADDR)
+    for relay in config[CONF_RELAYS]:
+        name = relay[CONF_NAME]
+        board = relay[CONF_BOARD]
+        addr = relay[CONF_ADDR]
         devs.append(PencomRelay(hub, board, addr, name))
     add_devices(devs, True)
-    return True
 
 
 class PencomRelay(SwitchDevice):
@@ -74,8 +69,8 @@ class PencomRelay(SwitchDevice):
         self._hub = hub
         self._board = board
         self._addr = addr
-        self._state = None
         self._name = name
+        self._state = None
 
     @property
     def name(self):
