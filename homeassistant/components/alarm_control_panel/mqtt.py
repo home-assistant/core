20b2c86062a82dafc9af9aa558fe4c1cb24261f0
Erik <erik@montnemery.com>
2018-12-17 20:59:15 +0100

    Correct calls to subscription.async_unsubscribe_topics

--- homeassistant/components/alarm_control_panel/mqtt.py
+++ homeassistant/components/alarm_control_panel/mqtt.py
@@ -127,7 +127,8 @@ class MqttAlarm(MqttAvailability, MqttDiscoveryUpdate,
 
     async def async_will_remove_from_hass(self):
         """Unsubscribe when removed."""
-        await subscription.async_unsubscribe_topics(self.hass, self._sub_state)
+        self._sub_state = await subscription.async_unsubscribe_topics(
+            self.hass, self._sub_state)
         await MqttAvailability.async_will_remove_from_hass(self)
 
     @property
