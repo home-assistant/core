0f619bfcf14c3a84bb84838e259c10bd49ee1a24
Fabian Affolter <mail@fabian-affolter.ch>
2018-12-18 17:29:38 +0100

    Upgrade requests to 2.21.0 (#19385)

--- setup.py
+++ setup.py
@@ -38,8 +38,6 @@ REQUIRES = [
     'attrs==18.2.0',
     'bcrypt==3.1.4',
     'certifi>=2018.04.16',
-    # Dec 5, 2018: Idna released 2.8, requests caps idna at <2.8, CI fails
-    'idna==2.7',
     'jinja2>=2.10',
     'PyJWT==1.6.4',
     # PyJWT has loose dependency. We want the latest one.
@@ -48,7 +46,7 @@ REQUIRES = [
     'python-slugify==1.2.6',
     'pytz>=2018.07',
     'pyyaml>=3.13,<4',
-    'requests==2.20.1',
+    'requests==2.21.0',
     'ruamel.yaml==0.15.81',
     'voluptuous==0.11.5',
     'voluptuous-serialize==2.0.0',
