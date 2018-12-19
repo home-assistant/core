ec5e674af65e1483de21a8f8e04376533e373e9f
Martin Hjelmare <marhje52@kth.se>
2018-12-20 00:34:59 +0100

    Validate child values using set message

--- homeassistant/components/mysensors/helpers.py
+++ homeassistant/components/mysensors/helpers.py
@@ -24,17 +24,15 @@ def discover_mysensors_platform(hass, hass_config, platform, new_devices):
     return task
 
 
-def validate_child(gateway, node_id, child):
-    """Validate that a child has the correct values according to schema.
+def validate_set(msg):
+    """Validate a set message."""
+    child = msg.gateway.sensors[msg.node_id].children[msg.child_id]
+    return validate_child(msg.gateway, msg.node_id, child, msg.sub_type)
 
-    Return a dict of platform with a list of device ids for validated devices.
-    """
+
+def validate_child(gateway, node_id, child, value_type=None):
+    """Validate a child."""
     validated = defaultdict(list)
-
-    if not child.values:
-        _LOGGER.debug(
-            "No child values for node %s child %s", node_id, child.id)
-        return validated
     if gateway.sensors[node_id].sketch_name is None:
         _LOGGER.debug("Node %s is missing sketch name", node_id)
         return validated
@@ -42,12 +40,11 @@ def validate_child(gateway, node_id, child):
     set_req = gateway.const.SetReq
     s_name = next(
         (member.name for member in pres if member.value == child.type), None)
-    if s_name not in MYSENSORS_CONST_SCHEMA:
+    child_schemas = MYSENSORS_CONST_SCHEMA.get(s_name, [])
+    if not child_schemas:
         _LOGGER.warning("Child type %s is not supported", s_name)
-        return validated
-    child_schemas = MYSENSORS_CONST_SCHEMA[s_name]
 
-    def msg(name):
+    def invalid_msg(name):
         """Return a message for an invalid schema."""
         return "{} requires value_type {}".format(
             pres(child.type).name, set_req[name].name)
@@ -55,27 +52,24 @@ def validate_child(gateway, node_id, child):
     for schema in child_schemas:
         platform = schema[PLATFORM]
         v_name = schema[TYPE]
-        value_type = next(
-            (member.value for member in set_req if member.name == v_name),
-            None)
-        if value_type is None:
-            continue
         _child_schema = child.get_schema(gateway.protocol_version)
         vol_schema = _child_schema.extend(
-            {vol.Required(set_req[key].value, msg=msg(key)):
+            {vol.Required(set_req[key].value, msg=invalid_msg(key)):
              _child_schema.schema.get(set_req[key].value, val)
              for key, val in schema.get(SCHEMA, {v_name: cv.string}).items()},
             extra=vol.ALLOW_EXTRA)
+        if (value_type and value_type not in vol_schema.schema
+                or not any(
+                    child_value_type in vol_schema.schema
+                    for child_value_type in child.values)):
+            continue
         try:
             vol_schema(child.values)
         except vol.Invalid as exc:
-            level = (logging.WARNING if value_type in child.values
-                     else logging.DEBUG)
-            _LOGGER.log(
-                level,
+            _LOGGER.warning(
                 "Invalid values: %s: %s platform: node %s child %s: %s",
                 child.values, platform, node_id, child.id, exc)
             continue
-        dev_id = id(gateway), node_id, child.id, value_type
+        dev_id = id(gateway), node_id, child.id, set_req[v_name].value
         validated[platform].append(dev_id)
     return validated
