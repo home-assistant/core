df7bbe5c0dc509cb4bb47a61ea16e72ba5e7cb6f
Antoine GRÉA <grea09@users.noreply.github.com>
2018-12-22 13:35:07 +0100

    Commit to try to fix the CLA ?

--- homeassistant/components/sensor/fail2ban.py
+++ homeassistant/components/sensor/fail2ban.py
@@ -3,6 +3,7 @@ Support for displaying IPs banned by fail2ban.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.fail2ban/
+
 """
 import os
 import logging
