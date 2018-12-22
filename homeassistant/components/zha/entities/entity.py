4bdb21a87100d275e0f50a124c7265e1bf1ccc2d
David F. Mulcahey <david.mulcahey@icloud.com>
2018-12-22 12:18:48 -0500

    Update ZHA entity state on ZigBee zdo device announce (#19208)
    * call async_update if defined on device_announce
    
    * lint
    
    * change update method
    
    * remove unneeded listener

--- homeassistant/components/zha/entities/entity.py
+++ homeassistant/components/zha/entities/entity.py
@@ -77,6 +77,8 @@ class ZhaEntity(entity.Entity):
         for cluster_id, cluster in self._out_clusters.items():
             cluster.add_listener(self._out_listeners.get(cluster_id, self))
 
+        self._endpoint.device.zdo.add_listener(self)
+
         self._initialized = True
 
     async def async_configure(self):
@@ -170,6 +172,16 @@ class ZhaEntity(entity.Entity):
     @callback
     def zdo_command(self, tsn, command_id, args):
         """Handle a ZDO command received on this cluster."""
+        pass
+
+    @callback
+    def device_announce(self, device):
+        """Handle device_announce zdo event."""
+        self.async_schedule_update_ha_state(force_refresh=True)
+
+    @callback
+    def permit_duration(self, permit_duration):
+        """Handle permit_duration zdo event."""
         pass
 
     @property
