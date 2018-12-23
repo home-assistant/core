c1a3131c72a8fba27fbdc6d19056f367ee9dc3ea
Thibaut ETIENNE <thibaut@etienne.pw>
2018-12-23 20:25:12 +0100

    ADD support for tilt position

--- homeassistant/components/cover/somfy.py
+++ homeassistant/components/cover/somfy.py
@@ -4,10 +4,12 @@ Support for Somfy Covers.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/cover.somfy/
 """
+from pymfy.api.devices.blind import Blind
 from pymfy.api.devices.category import Category
 from pymfy.api.devices.roller_shutter import RollerShutter
 
-from homeassistant.components.cover import CoverDevice, ATTR_POSITION
+from homeassistant.components.cover import CoverDevice, ATTR_POSITION, \
+    ATTR_TILT_POSITION
 from homeassistant.components.somfy import DOMAIN, SomfyEntity, DEVICES
 
 DEPENDENCIES = ['somfy']
@@ -53,3 +55,33 @@ class SomfyCover(SomfyEntity, CoverDevice):
     def is_closed(self):
         """Return if the cover is closed."""
         return RollerShutter(self.device, self.api).is_closed()
+
+    @property
+    def current_cover_tilt_position(self):
+        """Return current position of cover tilt.
+
+        None is unknown, 0 is closed, 100 is fully open.
+        """
+        orientation = None
+        try:
+            orientation = Blind(self.device, self.api).orientation
+        except StopIteration:
+            pass
+        return orientation
+
+    def set_cover_tilt_position(self, **kwargs):
+        """Move the cover tilt to a specific position."""
+        orientation = kwargs.get(ATTR_TILT_POSITION)
+        Blind(self.device, self.api).orientation = orientation
+
+    def open_cover_tilt(self, **kwargs):
+        """Open the cover tilt."""
+        Blind(self.device, self.api).orientation = 100
+
+    def close_cover_tilt(self, **kwargs):
+        """Close the cover tilt."""
+        Blind(self.device, self.api).orientation = 0
+
+    def stop_cover_tilt(self, **kwargs):
+        """Stop the cover."""
+        Blind(self.device, self.api).stop()
