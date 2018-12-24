1a5af8ff4870256b82b87d436569f8cdc6450444
Thibaut ETIENNE <thibaut@etienne.pw>
2018-12-24 17:17:20 +0100

    FIX import locally 3rd party library

--- homeassistant/components/cover/somfy.py
+++ homeassistant/components/cover/somfy.py
@@ -4,9 +4,6 @@ Support for Somfy Covers.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/cover.somfy/
 """
-from pymfy.api.devices.blind import Blind
-from pymfy.api.devices.category import Category
-from pymfy.api.devices.roller_shutter import RollerShutter
 
 from homeassistant.components.cover import CoverDevice, ATTR_POSITION, \
     ATTR_TILT_POSITION
@@ -14,15 +11,17 @@ from homeassistant.components.somfy import DOMAIN, SomfyEntity, DEVICES
 
 DEPENDENCIES = ['somfy']
 
-CATEGORIES = {Category.ROLLER_SHUTTER.value, Category.INTERIOR_BLIND.value,
-              Category.EXTERIOR_BLIND.value}
-
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Somfy cover platform."""
+
+    from pymfy.api.devices.category import Category
+    categories = {Category.ROLLER_SHUTTER.value, Category.INTERIOR_BLIND.value,
+                  Category.EXTERIOR_BLIND.value}
+
     devices = hass.data[DOMAIN][DEVICES]
     for cover in devices:
-        if CATEGORIES & set(cover.categories):
+        if categories & set(cover.categories):
             add_entities([SomfyCover(cover, hass)])
 
 
@@ -31,19 +30,23 @@ class SomfyCover(SomfyEntity, CoverDevice):
 
     def close_cover(self, **kwargs):
         """Close the cover."""
+        from pymfy.api.devices.roller_shutter import RollerShutter
         RollerShutter(self.device, self.api).close()
 
     def open_cover(self, **kwargs):
         """Open the cover."""
+        from pymfy.api.devices.roller_shutter import RollerShutter
         RollerShutter(self.device, self.api).open()
 
     def stop_cover(self, **kwargs):
         """Stop the cover"""
+        from pymfy.api.devices.roller_shutter import RollerShutter
         RollerShutter(self.device, self.api).stop()
 
     def set_cover_position(self, **kwargs):
         """Move the cover shutter to a specific position."""
         position = kwargs.get(ATTR_POSITION)
+        from pymfy.api.devices.roller_shutter import RollerShutter
         RollerShutter(self.device, self.api).set_position(100 - position)
 
     @property
@@ -51,6 +54,7 @@ class SomfyCover(SomfyEntity, CoverDevice):
         """Return the current position of cover shutter."""
         position = None
         try:
+            from pymfy.api.devices.roller_shutter import RollerShutter
             shutter = RollerShutter(self.device, self.api)
             position = 100 - shutter.get_position()
         except StopIteration:
@@ -62,6 +66,7 @@ class SomfyCover(SomfyEntity, CoverDevice):
         """Return if the cover is closed."""
         is_closed = None
         try:
+            from pymfy.api.devices.roller_shutter import RollerShutter
             is_closed = RollerShutter(self.device, self.api).is_closed()
         except StopIteration:
             pass
@@ -75,6 +80,7 @@ class SomfyCover(SomfyEntity, CoverDevice):
         """
         orientation = None
         try:
+            from pymfy.api.devices.blind import Blind
             orientation = Blind(self.device, self.api).orientation
         except StopIteration:
             pass
@@ -83,16 +89,20 @@ class SomfyCover(SomfyEntity, CoverDevice):
     def set_cover_tilt_position(self, **kwargs):
         """Move the cover tilt to a specific position."""
         orientation = kwargs.get(ATTR_TILT_POSITION)
+        from pymfy.api.devices.blind import Blind
         Blind(self.device, self.api).orientation = orientation
 
     def open_cover_tilt(self, **kwargs):
         """Open the cover tilt."""
+        from pymfy.api.devices.blind import Blind
         Blind(self.device, self.api).orientation = 100
 
     def close_cover_tilt(self, **kwargs):
         """Close the cover tilt."""
+        from pymfy.api.devices.blind import Blind
         Blind(self.device, self.api).orientation = 0
 
     def stop_cover_tilt(self, **kwargs):
         """Stop the cover."""
+        from pymfy.api.devices.blind import Blind
         Blind(self.device, self.api).stop()
