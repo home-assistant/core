5d2d2af29a7dd78236f8cf7d90efa85cdc3c0555
SNoof85 <snoof85@gmail.com>
2018-12-25 21:54:13 +0100

    Fix URL

--- homeassistant/components/sensor/freebox.py
+++ homeassistant/components/sensor/freebox.py
@@ -2,7 +2,7 @@
 Support for Freebox devices (Freebox v6 and Freebox mini 4K).
 
 For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/sensor/freebox/
+https://home-assistant.io/components/sensor.freebox/
 """
 import logging
 
