3235481b63b6fdefcd5156174b44fd31b9786d03
Jonathan Keljo <jkeljo@users.noreply.github.com>
2018-12-25 23:07:30 -0500

    Upgrade greeneye_monitor to 1.0
    This is a breaking change; it causes the `serial_number` field in
    configuration to be treated as the full 8-digit serial number rather
    than the last 5 digits as was previously done, which results in the unique
    identifiers for the sensors being different. (Fixing them up in
    `config/.storage/core.entity_registry` before rebooting into the updated
    version seems to prevent any weirdness.)
    
    The last-5-digits behavior was a result of me misunderstanding the packet
    format docs and not realizing that the true serial number was split across
    two fields. In addition to being confusing (see
    https://community.home-assistant.io/t/brultech-greeneye-issues/86852), it
    was technically incorrect. The `greeneye_monitor` platform was just introduced
    in 0.82, so it seems like the kind of thing that's best to fix now while
    adoption is relatively low rather than later when somebody runs into it
    as more than just a point of confusion.

--- homeassistant/components/greeneye_monitor.py
+++ homeassistant/components/greeneye_monitor.py
@@ -16,7 +16,7 @@ from homeassistant.const import (
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import async_load_platform
 
-REQUIREMENTS = ['greeneye_monitor==0.1']
+REQUIREMENTS = ['greeneye_monitor==1.0']
 
 _LOGGER = logging.getLogger(__name__)
 
