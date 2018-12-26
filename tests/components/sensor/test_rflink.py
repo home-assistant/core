28dc568ec53c63b0331ecbb4ab1fb57caade00f3
javicalle <javicalle@gmail.com>
2018-12-26 23:16:50 +0100

    Correct tests failures

--- tests/components/sensor/test_rflink.py
+++ tests/components/sensor/test_rflink.py
@@ -133,3 +133,47 @@ async def test_entity_availability(hass, monkeypatch):
 
     # Entities should be available again
     assert hass.states.get('sensor.test').state == STATE_UNKNOWN
+
+
+async def test_aliasses(hass, monkeypatch):
+    """Validate the response to sensor's alias
+    (and the remove_deprecated function)."""
+    config = {
+        'rflink': {
+            'port': '/dev/ttyABC0',
+        },
+        DOMAIN: {
+            'platform': 'rflink',
+            'devices': {
+                'test_02': {
+                    'name': 'test_02',
+                    'sensor_type': 'humidity',
+                    'aliasses': ['test_alias_02_0'],
+                },
+            },
+        },
+    }
+
+    # setup mocking rflink module
+    event_callback, _, _, _ = await mock_rflink(
+        hass, config, DOMAIN, monkeypatch)
+
+    # test default state of sensor loaded from config
+    config_sensor = hass.states.get('sensor.test_02')
+    assert config_sensor
+    assert config_sensor.state == 'unknown'
+
+    # test event for config sensor
+    event_callback({
+        'id': 'test_alias_02_0',
+        'sensor': 'humidity',
+        'value': 65,
+        'unit': '%',
+    })
+    await hass.async_block_till_done()
+
+    # test  state of new sensor
+    updated_sensor = hass.states.get('sensor.test_02')
+    assert updated_sensor
+    assert updated_sensor.state == '65'
+    assert updated_sensor.attributes['unit_of_measurement'] == '%'
