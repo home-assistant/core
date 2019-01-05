f61f2f33bb7b4da15ed6a51ce4cd3f646548dae6
Jonathan Keljo <jkeljo@users.noreply.github.com>
2019-01-04 20:50:12 -0800

    - `sensor` -> `binary_sensor` - `state_attributes` -> `device_state_attributes` - remove unnecessary `str` - try to connect in `setup_platform`

--- homeassistant/components/binary_sensor/webdav.py
+++ homeassistant/components/binary_sensor/webdav.py
@@ -5,16 +5,16 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensors.webdav/
 """
 import logging
-from urllib.parse import quote
 
 import voluptuous as vol
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.components.binary_sensor import (
+    BinarySensorDevice, PLATFORM_SCHEMA
+)
 from homeassistant.const import (
     CONF_HOST, CONF_USERNAME, CONF_PASSWORD, CONF_PATH, CONF_NAME, CONF_TOKEN
 )
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -22,6 +22,7 @@ REQUIREMENTS = ['webdavclient3==0.9']
 
 CONF_CERTIFICATE_PATH = "ssl_client_certificate"
 CONF_KEY_PATH = "ssl_client_key"
+DEVICE_CLASS = "connectivity"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
@@ -37,34 +38,44 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up a single WebDAV share."""
-    add_entities([WebDAVSensor(hass, config)])
+    from webdav3.client import Client
+    from webdav3.exceptions import WebDavException
+
+    name = config.get(CONF_NAME)
+    host = config.get(CONF_HOST)
+    path = config.get(CONF_PATH)
+    username = config.get(CONF_USERNAME)
+    password = config.get(CONF_PASSWORD)
+    token = config.get(CONF_TOKEN)
+    cert_path = config.get(CONF_CERTIFICATE_PATH)
+    key_path = config.get(CONF_KEY_PATH)
+    client = Client({
+        "webdav_hostname": host,
+        "webdav_login": username,
+        "webdav_password": password,
+        "webdav_token": token,
+        "webdav_cert_path": cert_path,
+        "webdav_key_path": key_path,
+        "webdav_root": path,
+    })
+
+    try:
+        client.list('/')  # This will throw if we can't access the share
+        add_entities([WebDAVSensor(name, client)], update_before_add=True)
+    except WebDavException as exception:
+        _LOGGER.error(
+            "Failed to connect to %s: %s",
+            client.get_url(""),
+            exception)
 
 
-class WebDAVSensor(Entity):
+class WebDAVSensor(BinarySensorDevice):
     """A sensor that monitors a WebDAV share."""
 
-    def __init__(self, hass, config):
+    def __init__(self, name, client):
         """Create the sensor."""
-        self._name = config.get(CONF_NAME)
-        self._host = config.get(CONF_HOST)
-        username = config.get(CONF_USERNAME)
-        password = config.get(CONF_PASSWORD)
-        token = config.get(CONF_TOKEN)
-        cert_path = config.get(CONF_CERTIFICATE_PATH)
-        key_path = config.get(CONF_KEY_PATH)
-        self._path = config.get(CONF_PATH)
-
-        from webdav3.client import Client
-
-        self._client = Client({
-            "webdav_hostname": self._host,
-            "webdav_login": username,
-            "webdav_password": password,
-            "webdav_token": token,
-            "webdav_cert_path": cert_path,
-            "webdav_key_path": key_path,
-            "webdav_root": self._path,
-        })
+        self._name = name
+        self._client = client
 
         self._files = []
         self._available = False
@@ -74,7 +85,7 @@ class WebDAVSensor(Entity):
         from webdav3.exceptions import WebDavException
         try:
             self._files = [
-                self._get_url(path) for path in self._client.list('/')
+                self._client.get_url(path) for path in self._client.list('/')
                 if not path.endswith('/')]
             self._available = True
         except WebDavException as exception:
@@ -83,7 +94,7 @@ class WebDAVSensor(Entity):
             _LOGGER.debug(
                 "Could not open share %s. Message: %s",
                 self.unique_id,
-                str(exception))
+                exception)
 
     @property
     def name(self):
@@ -93,21 +104,21 @@ class WebDAVSensor(Entity):
     @property
     def unique_id(self):
         """Return the URL for the share."""
-        return self._get_url("")
+        return self._client.get_url("")
 
     @property
-    def state(self):
+    def is_on(self):
         """Return true if the share is currently available."""
         return self._available
 
     @property
-    def state_attributes(self):
+    def device_class(self):
+        """Return the device class of this sensor."""
+        return DEVICE_CLASS
+
+    @property
+    def device_state_attributes(self):
         """Return the list of files on the share."""
         return {
             "files": self._files,
         }
-
-    def _get_url(self, path):
-        return "{host}{root}/{path}".format(host=self._host,
-                                            root=quote(self._path),
-                                            path=quote(path))
