be2e07241936c695d253e76684d9e6298efc6bd5
emontnemery <erik@montnemery.com>
2019-01-05 13:48:09 +0100

    Clear ALREADY_DISCOVERED list with helper

--- homeassistant/components/cover/mqtt.py
+++ homeassistant/components/cover/mqtt.py
@@ -25,7 +25,7 @@ from homeassistant.components.mqtt import (
     CONF_QOS, CONF_RETAIN, valid_publish_topic, valid_subscribe_topic,
     MqttAvailability, MqttDiscoveryUpdate, MqttEntityDeviceInfo, subscription)
 from homeassistant.components.mqtt.discovery import (
-    ALREADY_DISCOVERED, MQTT_DISCOVERY_NEW)
+    MQTT_DISCOVERY_NEW, clear_discovery_hash)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.typing import HomeAssistantType, ConfigType
@@ -144,7 +144,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                                       discovery_hash)
         except Exception:
             if discovery_hash:
-                del hass.data[ALREADY_DISCOVERED][discovery_hash]
+                clear_discovery_hash(hass, discovery_hash)
             raise
 
     async_dispatcher_connect(
