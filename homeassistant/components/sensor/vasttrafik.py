20afba047c529252c69342c39bc2b2ea74109941
magnusknutas <magnus@thefarm.se>
2019-01-05 13:36:34 +0100

    This makes the vasttrafik platform stop spamming the logs with warrnings

--- homeassistant/components/sensor/vasttrafik.py
+++ homeassistant/components/sensor/vasttrafik.py
@@ -116,7 +116,7 @@ class VasttrafikDepartureSensor(Entity):
     def state(self):
         """Return the next departure time."""
         if not self._departureboard:
-            _LOGGER.warning(
+            _LOGGER.debug(
                 "No departures from %s heading %s",
                 self._departure['name'],
                 self._heading['name'] if self._heading else 'ANY')
@@ -143,5 +143,5 @@ class VasttrafikDepartureSensor(Entity):
                 direction=self._heading['id'] if self._heading else None,
                 date=datetime.now()+self._delay)
         except self._vasttrafik.Error:
-            _LOGGER.warning("Unable to read departure board, updating token")
+            _LOGGER.debug("Unable to read departure board, updating token")
             self._planner.update_token()
