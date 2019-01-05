6b0451c5614824e01884901efcd3aa500e74db86
magnusknutas <magnus@thefarm.se>
2019-01-05 20:39:59 +0100

    Forcing build

--- homeassistant/components/sensor/vasttrafik.py
+++ homeassistant/components/sensor/vasttrafik.py
@@ -59,6 +59,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     planner = vasttrafik.JournyPlanner(
         config.get(CONF_KEY), config.get(CONF_SECRET))
     sensors = []
+
     for departure in config.get(CONF_DEPARTURES):
         sensors.append(
             VasttrafikDepartureSensor(
