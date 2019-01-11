872cc99a6f6b3277812e00114d5e30f983b668a5
Andrew Chatham <chatham@waymo.com>
2019-01-11 14:34:32 -0800

    Fix the anthemav component by removing a debugging line. (#19979)
    This lane ended up calling vars(transport) on an asyncio Transport
    object. In a standard setup, that's a python object provided by syncio,
    and it works. Home Assistant injects uvloop into asyncio, which makes this
    a Python C object, and those don't support vars().

--- homeassistant/components/media_player/anthemav.py
+++ homeassistant/components/media_player/anthemav.py
@@ -59,7 +59,6 @@ async def async_setup_platform(hass, config, async_add_entities,
 
     _LOGGER.debug("dump_devicedata: %s", device.dump_avrdata)
     _LOGGER.debug("dump_conndata: %s", avr.dump_conndata)
-    _LOGGER.debug("dump_rawdata: %s", avr.protocol.dump_rawdata)
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, device.avr.close)
     async_add_entities([device])
