4a0b7637dbd38668ce9bff7a8354e0c7a4035099
Paulus Schoutsen <balloob@gmail.com>
2019-01-11 11:48:32 -0800

    Updated frontend to 20190109.1

--- homeassistant/components/frontend/__init__.py
+++ homeassistant/components/frontend/__init__.py
@@ -24,7 +24,7 @@ from homeassistant.core import callback
 from homeassistant.helpers.translation import async_get_translations
 from homeassistant.loader import bind_hass
 
-REQUIREMENTS = ['home-assistant-frontend==20190109.0']
+REQUIREMENTS = ['home-assistant-frontend==20190109.1']
 
 DOMAIN = 'frontend'
 DEPENDENCIES = ['api', 'websocket_api', 'http', 'system_log',
