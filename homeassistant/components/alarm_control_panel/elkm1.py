41367cb4e583d0ef9a4c72260f5189f223fe9591
arsaboo <arsaboo@gmx.com>
2019-01-12 18:29:54 -0500

    Change code format

--- homeassistant/components/alarm_control_panel/elkm1.py
+++ homeassistant/components/alarm_control_panel/elkm1.py
@@ -21,6 +21,7 @@ DEPENDENCIES = [ELK_DOMAIN]
 
 SIGNAL_ARM_ENTITY = 'elkm1_arm'
 SIGNAL_DISPLAY_MESSAGE = 'elkm1_display_message'
+FORMAT_NUMBER = 'number'
 
 ELK_ALARM_SERVICE_SCHEMA = vol.Schema({
     vol.Required(ATTR_ENTITY_ID, default=[]): cv.entity_ids,
@@ -116,7 +117,7 @@ class ElkArea(ElkEntity, alarm.AlarmControlPanel):
     @property
     def code_format(self):
         """Return the alarm code format."""
-        return '^[0-9]{4}([0-9]{2})?$'
+        return FORMAT_NUMBER
 
     @property
     def state(self):
