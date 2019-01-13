8cb7c3808ad6a69fd667ce535a184381eb775412
arsaboo <arsaboo@gmx.com>
2019-01-13 10:43:56 -0500

    Change return text code for manual MQTT alarm

--- homeassistant/components/alarm_control_panel/manual_mqtt.py
+++ homeassistant/components/alarm_control_panel/manual_mqtt.py
@@ -242,7 +242,7 @@ class ManualMQTTAlarm(alarm.AlarmControlPanel):
             return None
         if isinstance(self._code, str) and re.search('^\\d+$', self._code):
             return alarm.FORMAT_NUMBER
-        return 'Any'
+        return alarm.FORMAT_TEXT
 
     def alarm_disarm(self, code=None):
         """Send disarm command."""
