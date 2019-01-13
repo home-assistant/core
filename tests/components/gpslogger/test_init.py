f62ea4eb17a74c4f8764839f6f35920c0664e6fc
Rohan Kapoor <rohan@rohankapoor.com>
2019-01-13 11:16:40 -0800

    Increase test coverage

--- tests/components/gpslogger/test_init.py
+++ tests/components/gpslogger/test_init.py
@@ -7,8 +7,9 @@ from homeassistant.components import zone
 from homeassistant.components.device_tracker import \
     DOMAIN as DEVICE_TRACKER_DOMAIN
 from homeassistant.components.gpslogger import URL, DOMAIN
+from homeassistant.components.http import CONF_API_PASSWORD
 from homeassistant.const import HTTP_OK, HTTP_UNPROCESSABLE_ENTITY, \
-    STATE_HOME, STATE_NOT_HOME
+    STATE_HOME, STATE_NOT_HOME, HTTP_UNAUTHORIZED, CONF_PASSWORD
 from homeassistant.setup import async_setup_component
 
 HOME_LATITUDE = 37.239622
@@ -30,8 +31,8 @@ def mock_dev_track(mock_device_tracker_conf):
 
 
 @pytest.fixture
-def gpslogger_client(loop, hass, hass_client):
-    """Locative mock client."""
+def authenticated_gpslogger_client(loop, hass, hass_client):
+    """Locative mock client (authenticated)."""
     assert loop.run_until_complete(async_setup_component(
         hass, DOMAIN, {
             DOMAIN: {}
@@ -39,6 +40,23 @@ def gpslogger_client(loop, hass, hass_client):
 
     with patch('homeassistant.components.device_tracker.update_config'):
         yield loop.run_until_complete(hass_client())
+
+
+@pytest.fixture
+def unauthenticated_gpslogger_client(loop, hass, aiohttp_client):
+    """Locative mock client (unauthenticated)."""
+    assert loop.run_until_complete(async_setup_component(
+        hass, 'persistent_notification', {}))
+
+    assert loop.run_until_complete(async_setup_component(
+        hass, DOMAIN, {
+            DOMAIN: {
+                CONF_PASSWORD: 'test'
+            }
+        }))
+
+    with patch('homeassistant.components.device_tracker.update_config'):
+        yield loop.run_until_complete(aiohttp_client(hass.http.app))
 
 
 @pytest.fixture(autouse=True)
@@ -54,33 +72,58 @@ def setup_zones(loop, hass):
             }}))
 
 
-async def test_missing_data(gpslogger_client):
+async def test_authentication(hass, unauthenticated_gpslogger_client):
     """Test missing data."""
     data = {
         'latitude': 1.0,
         'longitude': 1.1,
         'device': '123',
-        'id': 'Home',
+        CONF_API_PASSWORD: 'test'
+    }
+
+    # No auth
+    req = await unauthenticated_gpslogger_client.get(_url({}))
+    await hass.async_block_till_done()
+    assert req.status == HTTP_UNAUTHORIZED
+
+    # Authenticated
+    req = await unauthenticated_gpslogger_client.get(_url(data))
+    await hass.async_block_till_done()
+    assert req.status == HTTP_OK
+    state_name = hass.states.get('{}.{}'.format(DEVICE_TRACKER_DOMAIN,
+                                                data['device'])).state
+    assert STATE_NOT_HOME == state_name
+
+
+async def test_missing_data(hass, authenticated_gpslogger_client):
+    """Test missing data."""
+    data = {
+        'latitude': 1.0,
+        'longitude': 1.1,
+        'device': '123',
     }
 
     # No data
-    req = await gpslogger_client.get(_url({}))
+    req = await authenticated_gpslogger_client.get(_url({}))
+    await hass.async_block_till_done()
     assert req.status == HTTP_UNPROCESSABLE_ENTITY
 
     # No latitude
     copy = data.copy()
     del copy['latitude']
-    req = await gpslogger_client.get(_url(copy))
+    req = await authenticated_gpslogger_client.get(_url(copy))
+    await hass.async_block_till_done()
     assert req.status == HTTP_UNPROCESSABLE_ENTITY
 
     # No device
     copy = data.copy()
     del copy['device']
-    req = await gpslogger_client.get(_url(copy))
+    req = await authenticated_gpslogger_client.get(_url(copy))
+    await hass.async_block_till_done()
     assert req.status == HTTP_UNPROCESSABLE_ENTITY
 
 
-async def test_enter_and_exit(hass, gpslogger_client):
+async def test_enter_and_exit(hass, authenticated_gpslogger_client):
     """Test when there is a known zone."""
     data = {
         'latitude': HOME_LATITUDE,
@@ -89,7 +132,7 @@ async def test_enter_and_exit(hass, gpslogger_client):
     }
 
     # Enter the Home
-    req = await gpslogger_client.get(_url(data))
+    req = await authenticated_gpslogger_client.get(_url(data))
     await hass.async_block_till_done()
     assert req.status == HTTP_OK
     state_name = hass.states.get('{}.{}'.format(DEVICE_TRACKER_DOMAIN,
@@ -97,7 +140,7 @@ async def test_enter_and_exit(hass, gpslogger_client):
     assert STATE_HOME == state_name
 
     # Enter Home again
-    req = await gpslogger_client.get(_url(data))
+    req = await authenticated_gpslogger_client.get(_url(data))
     await hass.async_block_till_done()
     assert req.status == HTTP_OK
     state_name = hass.states.get('{}.{}'.format(DEVICE_TRACKER_DOMAIN,
@@ -108,9 +151,39 @@ async def test_enter_and_exit(hass, gpslogger_client):
     data['latitude'] = 0
 
     # Enter Somewhere else
-    req = await gpslogger_client.get(_url(data))
+    req = await authenticated_gpslogger_client.get(_url(data))
     await hass.async_block_till_done()
     assert req.status == HTTP_OK
     state_name = hass.states.get('{}.{}'.format(DEVICE_TRACKER_DOMAIN,
                                                 data['device'])).state
     assert STATE_NOT_HOME == state_name
+
+
+async def test_enter_with_attrs(hass, authenticated_gpslogger_client):
+    """Test when additional attributes are present."""
+    data = {
+        'latitude': 1.0,
+        'longitude': 1.1,
+        'device': '123',
+        'accuracy': 10.5,
+        'battery': 10,
+        'speed': 100,
+        'direction': 105.32,
+        'altitude': 102,
+        'provider': 'gps',
+        'activity': 'running'
+    }
+
+    req = await authenticated_gpslogger_client.get(_url(data))
+    await hass.async_block_till_done()
+    assert req.status == HTTP_OK
+    state = hass.states.get('{}.{}'.format(DEVICE_TRACKER_DOMAIN,
+                                           data['device']))
+    assert STATE_NOT_HOME == state.state
+    assert 10 == state.attributes['gps_accuracy']
+    assert 10.0 == state.attributes['battery']
+    assert 100.0 == state.attributes['speed']
+    assert 105.32 == state.attributes['direction']
+    assert 102.0 == state.attributes['altitude']
+    assert 'gps' == state.attributes['provider']
+    assert 'running' == state.attributes['activity']
