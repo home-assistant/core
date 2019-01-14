32ec374bc6739b117616b8ea0cc873b8c3608f75
ludeeus <joasoe@gmail.com>
2019-01-14 16:06:18 +0100

    Validate device_types

--- homeassistant/components/googlehome.py
+++ homeassistant/components/googlehome.py
@@ -31,7 +31,8 @@ DEFAULT_RSSI_THRESHOLD = -70
 DEVICE_CONFIG = vol.Schema({
     vol.Required(CONF_HOST): cv.string,
     vol.Optional(CONF_DEVICE_TYPES,
-                 default=DEFAULT_DEVICE_TYPES): cv.ensure_list,
+                 default=DEFAULT_DEVICE_TYPES):
+                 vol.All(cv.ensure_list, [vol.In(DEFAULT_DEVICE_TYPES)]),
     vol.Optional(CONF_RSSI_THRESHOLD,
                  default=DEFAULT_RSSI_THRESHOLD): vol.Coerce(int),
 })
