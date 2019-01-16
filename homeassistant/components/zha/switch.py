dc7bc444955849fb62c297ddf108894155b0da15
David F. Mulcahey <david.mulcahey@icloud.com>
2019-01-16 14:06:22 -0500

    Make imports relative in ZHA component (#20020)
    * make imports relative
    
    * remove cyclic import

--- homeassistant/components/zha/switch.py
+++ homeassistant/components/zha/switch.py
@@ -7,11 +7,11 @@ at https://home-assistant.io/components/switch.zha/
 import logging
 
 from homeassistant.components.switch import DOMAIN, SwitchDevice
-from homeassistant.components.zha import helpers
-from homeassistant.components.zha.const import (
-    DATA_ZHA, DATA_ZHA_DISPATCHERS, REPORT_CONFIG_IMMEDIATE, ZHA_DISCOVERY_NEW)
-from homeassistant.components.zha.entities import ZhaEntity
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from . import helpers
+from .const import (
+    DATA_ZHA, DATA_ZHA_DISPATCHERS, REPORT_CONFIG_IMMEDIATE, ZHA_DISCOVERY_NEW)
+from .entities import ZhaEntity
 
 _LOGGER = logging.getLogger(__name__)
 
