8946c8383904f7d3a1244be5c59c32a46c73d3e7
Aaron Bach <bachya1208@gmail.com>
2019-01-16 20:05:17 -0700

    Contributor comments

--- homeassistant/components/sensor/ambient_station.py
+++ homeassistant/components/sensor/ambient_station.py
@@ -68,7 +68,7 @@ SENSOR_TYPES = {
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_APP_KEY): cv.string,
     vol.Required(CONF_API_KEY): cv.string,
-    vol.Required(CONF_MONITORED_CONDITIONS, default=list(SENSOR_TYPES)):
+    vol.Optional(CONF_MONITORED_CONDITIONS, default=list(SENSOR_TYPES)):
         vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
     vol.Optional(CONF_UNITS): vol.In([UNITS_SI, UNITS_US]),
 })
