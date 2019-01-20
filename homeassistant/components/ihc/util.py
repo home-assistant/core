a46cb56ffda65cd71c3c986b014da564ad7bcee7
Michael Svinth <msvinth@yahoo.com>
2019-01-20 08:33:06 +0100

    Lint fix

--- homeassistant/components/ihc/util.py
+++ homeassistant/components/ihc/util.py
@@ -1,9 +1,10 @@
-"""Utility methods for IHC"""
+"""Useful functions for the IHC component."""
 
 import time
 
 
 def pulse(ihc_controller, ihc_id: int):
+    """Send a short on/off pulse to an IHC controller resource."""
     ihc_controller.set_runtime_value_bool(ihc_id, True)
     time.sleep(0.1)
     ihc_controller.set_runtime_value_bool(ihc_id, False)
