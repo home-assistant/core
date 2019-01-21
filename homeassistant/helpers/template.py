935e5c67a3e9892f9bc3611c8ee4efa629bedea4
Phil Bruckner <pnbruckner@gmail.com>
2019-01-20 18:46:14 -0600

    Handle non-string values in JSON renderer (#20233)
    Handle the case of async_render_with_possible_json_value's value argument
    being something other than a string. This can happen, e.g., when using the
    SQL sensor to extract a datetime column such as last_changed and also using
    its value_template to convert that datetime to another format. This was
    causing a TypeError from json.loads, but async_render_with_possible_json_value
    was only catching ValueError's.

--- homeassistant/helpers/template.py
+++ homeassistant/helpers/template.py
@@ -167,7 +167,7 @@ class Template:
 
         try:
             variables['value_json'] = json.loads(value)
-        except ValueError:
+        except (ValueError, TypeError):
             pass
 
         try:
