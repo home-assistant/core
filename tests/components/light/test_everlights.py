4581665ce720a37c06ae7baf487649789f979b74
Jon Caruana <jon@joncaruana.com>
2019-01-21 00:01:21 +0000

    Style fixes for hound.

--- tests/components/light/test_everlights.py
+++ tests/components/light/test_everlights.py
@@ -1,6 +1,4 @@
 """The tests for the everlights component."""
-import unittest
-
 from homeassistant.components.light import everlights
 
 
@@ -9,6 +7,7 @@ def test_color_rgb_to_int():
     assert everlights.color_rgb_to_int(0x00, 0x00, 0x00) == 0x000000
     assert everlights.color_rgb_to_int(0xff, 0xff, 0xff) == 0xffffff
     assert everlights.color_rgb_to_int(0x12, 0x34, 0x56) == 0x123456
+
 
 def test_int_to_rgb():
     """Test integer to RGB conversion."""
