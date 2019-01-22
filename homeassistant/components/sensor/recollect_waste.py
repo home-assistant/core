9833990da9ba1fa6c70a78727490cb8905b3813c
Warren Brown <wwb@warrenb.ca>
2019-01-22 15:39:02 -0800

    Added true to add_entities. Created constant for default time interval. Used different pylint exclusion comment.

--- homeassistant/components/sensor/recollect_waste.py
+++ homeassistant/components/sensor/recollect_waste.py
@@ -8,9 +8,10 @@ import logging
 from datetime import timedelta
 
 import voluptuous as vol
+
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME
+from homeassistant.const import (CONF_NAME)
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 
@@ -19,18 +20,20 @@ REQUIREMENTS = ['recollect-waste==1.0.1']
 _LOGGER = logging.getLogger(__name__)
 ATTR_PICKUP_TYPES = 'pickup_types'
 ATTR_AREA_NAME = 'area_name'
-CONF_UPDATE_INTERVAL = 'update_interval'
 CONF_PLACE_ID = 'place_id'
 CONF_SERVICE_ID = 'service_id'
+CONF_UPDATE_INTERVAL = 'update_interval'
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=86400)
 DOMAIN = 'recollect_waste'
 ICON = 'mdi:trash-can-outline'
+
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_PLACE_ID): cv.string,
     vol.Required(CONF_SERVICE_ID): cv.string,
-    vol.Optional(CONF_NAME): cv.string,
-    vol.Optional(CONF_UPDATE_INTERVAL, default=timedelta(days=1)): (
-        vol.All(cv.time_period, cv.positive_timedelta)),
+    vol.Optional(CONF_NAME, default=DOMAIN): cv.string,
+    vol.Optional(CONF_UPDATE_INTERVAL, default=DEFAULT_SCAN_INTERVAL): (
+        vol.All(cv.time_period, cv.positive_timedelta))
 })
 
 
@@ -40,7 +43,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         config.get(CONF_NAME),
         config[CONF_PLACE_ID],
         config[CONF_SERVICE_ID],
-        config.get(CONF_UPDATE_INTERVAL))])
+        config.get(CONF_UPDATE_INTERVAL))], True)
 
 
 class RecollectWasteSensor(Entity):
@@ -48,18 +51,18 @@ class RecollectWasteSensor(Entity):
 
     def __init__(self, name, place_id, service_id, interval):
         """Initialize the sensor."""
+        self._attributes = {}
         self._name = name
+        self._state = None
+        self._unique_id = "{}{}".format(place_id, service_id)
         self.place_id = place_id
         self.service_id = service_id
-        self._attributes = {}
-        self._state = None
         self.update = Throttle(interval)(self._update)
-        self._unique_id = "{}{}".format(place_id, service_id)
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._name or DOMAIN
+        return self._name
 
     @property
     def unique_id(self) -> str:
@@ -86,7 +89,7 @@ class RecollectWasteSensor(Entity):
         import recollect_waste
 
         try:
-            # pylint:disable=E1101
+            # pylint: disable=no-member
             client = recollect_waste.RecollectWasteClient(self.place_id,
                                                           self.service_id)
             pickup_event = client.get_next_pickup()
