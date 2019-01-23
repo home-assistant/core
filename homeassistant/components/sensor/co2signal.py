343e5377b2f1131f7e00f9fcd06bcb8c1e8beb67
Daniels Jef <danielsjf@users.noreply.github.com>
2019-01-23 22:47:56 +0100

    Update co2signal.py
    Group imports;  remove empty lines; refactor use of location_type; remove logging messages; remove unused functions; add global variables

--- homeassistant/components/sensor/co2signal.py
+++ homeassistant/components/sensor/co2signal.py
@@ -3,7 +3,9 @@ Support for the CO2signal platform.
 For more details about this platform, please refer to the documentation
 """
 import logging
+
 import voluptuous as vol
+
 import homeassistant.helpers.config_validation as cv
 
 from homeassistant.const import (
@@ -19,42 +21,33 @@ _LOGGER = logging.getLogger(__name__)
 
 ATTRIBUTION = 'Data provided by CO2signal'
 
-msg_location = "Please use either coordinates or the country code. " \
+MSG_LOCATION = "Please use either coordinates or the country code. " \
                "For the coordinates, " \
                "you need to use both latitude and longitude."
+CO2_INTENSITY_UNIT = "CO2eq/kWh"
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_TOKEN): cv.string,
-    vol.Inclusive(CONF_LATITUDE, 'coords', msg=msg_location): cv.latitude,
-    vol.Inclusive(CONF_LONGITUDE, 'coords', msg=msg_location): cv.longitude,
-    vol.Optional(CONF_COUNTRY_CODE, default='None'): cv.string,
+    vol.Inclusive(CONF_LATITUDE, 'coords', msg=MSG_LOCATION): cv.latitude,
+    vol.Inclusive(CONF_LONGITUDE, 'coords', msg=MSG_LOCATION): cv.longitude,
+    vol.Optional(CONF_COUNTRY_CODE): cv.string,
 })
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the CO2signal sensor."""
-
-    _LOGGER.debug("Setting up the CO2signal platform")
-
     token = config.get(CONF_TOKEN)
     lat = config.get(CONF_LATITUDE, hass.config.latitude)
     lon = config.get(CONF_LONGITUDE, hass.config.longitude)
     country_code = config.get(CONF_COUNTRY_CODE)
 
-    if country_code != 'None':
-        location_type = 'country_code'
-    else:
-        location_type = 'coordinates'
-
-    _LOGGER.debug("Setting up the sensor using the %s", location_type)
+    _LOGGER.debug("Setting up the sensor using the %s", country_code)
 
     devs = []
 
     devs.append(CO2Sensor(token,
                           country_code,
                           lat,
-                          lon,
-                          location_type))
-
+                          lon))
     add_entities(devs, True)
 
 
@@ -64,15 +57,13 @@ class CO2Sensor(Entity):
     def __init__(self, token, country_code, lat, lon,
                  location_type):
         """Initialize the sensor."""
-
         self._token = token
         self._country_code = country_code
         self._latitude = lat
         self._longitude = lon
         self._location_type = location_type
-        self._unit = 'CO2eq/kWh'
 
-        if self._location_type == 'country_code':
+        if country_code is not None:
             self._device_name = country_code
         else:
             self._device_name = '{lat}/{lon}'\
@@ -80,8 +71,6 @@ class CO2Sensor(Entity):
                         lon=round(self._longitude, 2))
 
         self._friendly_name = 'CO2 intensity - {}'.format(self._device_name)
-
-        _LOGGER.debug("Initialise %s", self._friendly_name)
 
     @property
     def name(self):
@@ -94,20 +83,14 @@ class CO2Sensor(Entity):
         return 'mdi:periodic-table-co2'
 
     @property
-    def device_class(self):
-        """Return the device class."""
-        return None
-
-    @property
     def state(self):
         """Return the state of the device."""
-        _LOGGER.debug("Get state for %s", self._friendly_name)
         return self._data
 
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement of this entity, if any."""
-        return self._unit
+        return CO2_INTENSITY_UNIT
 
     def update(self):
         """Get the latest data and updates the states."""
@@ -115,17 +98,14 @@ class CO2Sensor(Entity):
 
         _LOGGER.debug("Update data for %s", self._friendly_name)
 
-        if self._location_type == 'country_code':
+        if self._country_code is not None:
             self._data = CO2Signal\
                 .get_latest_carbon_intensity(self._token,
                                              country_code=self._country_code)
-        elif self._location_type == 'coordinates':
+        else:
             self._data = CO2Signal\
                 .get_latest_carbon_intensity(self._token,
                                              latitude=self._latitude,
                                              longitude=self._longitude)
-        else:
-            raise ValueError("Unknown location type: {location_type}"
-                             .format(location_type=self._location_type))
 
         self._data = round(self._data, 2)
