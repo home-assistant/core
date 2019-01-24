a7fc08111bdd43b1939f73867e619c1d5ef0445f
Fréderic Kinnaer <frederic@censored.be>
2019-01-24 12:55:39 +0100

    SongPal: do not crash if active_source is not (yet) available - fixes #20343 (#20344)
    * SongPal: error handling if active_source can't be detected
    
    * sonpal: Add comment to the use of getattr() for property source
    
    * songpal: make comment single-line

--- homeassistant/components/media_player/songpal.py
+++ homeassistant/components/media_player/songpal.py
@@ -288,7 +288,8 @@ class SongpalDevice(MediaPlayerDevice):
     @property
     def source(self):
         """Return currently active source."""
-        return self._active_source.title
+        # Avoid a KeyError when _active_source is not (yet) populated
+        return getattr(self._active_source, 'title', None)
 
     @property
     def volume_level(self):
