288222892fdc1e7bc3bf669c9a5d422b89cc5810
Steven Rollason <2099542+gadgetchnnel@users.noreply.github.com>
2019-01-25 15:13:09 +0000

    Line length fixes
    Line length fixes

--- homeassistant/components/binary_sensor/template.py
+++ homeassistant/components/binary_sensor/template.py
@@ -222,7 +222,8 @@ class BinarySensorTemplate(BinarySensorDevice):
 
         if self._attribute_templates is not None:
             templates.update(dict(('_attributes.' + key, value)
-                             for key, value in self._attribute_templates.items()))
+                             for key, value
+                             in self._attribute_templates.items()))
 
         for property_name, template in templates.items():
             if template is None:
