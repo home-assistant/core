912276db6f03224c7aa39610044d027fd525d168
John Carr <john.carr@unrouted.co.uk>
2019-01-26 18:57:29 +0000

    Neater fixture use per review

--- tests/components/homekit_controller/conftest.py
+++ tests/components/homekit_controller/conftest.py
@@ -9,7 +9,6 @@ import pytest
 def utcnow(request):
     """Freeze time at a known point."""
     start_dt = datetime.datetime(2019, 1, 1, 0, 0, 0)
-    patcher = mock.patch('homeassistant.util.dt.utcnow', return_value=start_dt)
-    patcher.start()
-    request.addfinalizer(patcher.stop)
-    return start_dt
+    with mock.patch('homeassistant.util.dt.utcnow') as dt_utcnow:
+        dt_utcnow.return_value = start_dt
+        yield dt_utcnow
