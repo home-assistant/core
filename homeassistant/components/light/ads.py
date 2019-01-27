a4181efb7f58ee290d6b4d001d5f6155437ccfbf
Carsten Schröder <carsten@schroeder.net>
2019-01-27 15:43:21 +0100

    Add friendly_name option

--- homeassistant/components/light/ads.py
+++ homeassistant/components/light/ads.py
@@ -8,11 +8,12 @@ https://home-assistant.io/components/light.ads/
 import logging
 import voluptuous as vol
 from homeassistant.components.light import Light, ATTR_BRIGHTNESS, \
-    SUPPORT_BRIGHTNESS, PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME
+    SUPPORT_BRIGHTNESS, PLATFORM_SCHEMA, ENTITY_ID_FORMAT
+from homeassistant.const import CONF_NAME, CONF_FRIENDLY_NAME
 from homeassistant.components.ads import DATA_ADS, CONF_ADS_VAR, \
     CONF_ADS_VAR_BRIGHTNESS
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import async_generate_entity_id
 
 _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = ['ads']
@@ -21,7 +22,8 @@ CONF_ADSVAR_BRIGHTNESS = 'adsvar_brightness'
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_ADS_VAR): cv.string,
     vol.Optional(CONF_ADS_VAR_BRIGHTNESS): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_FRIENDLY_NAME): cv.string,
 })
 
 
@@ -32,20 +34,26 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     ads_var_enable = config.get(CONF_ADS_VAR)
     ads_var_brightness = config.get(CONF_ADS_VAR_BRIGHTNESS)
     name = config.get(CONF_NAME)
+    friendly_name = config.get(CONF_FRIENDLY_NAME) 
 
-    add_entities([AdsLight(ads_hub, ads_var_enable, ads_var_brightness,
-                           name)], True)
+    add_entities([AdsLight(hass, ads_hub, ads_var_enable, ads_var_brightness,
+                           name, friendly_name)], True)
 
 
 class AdsLight(Light):
     """Representation of ADS light."""
 
-    def __init__(self, ads_hub, ads_var_enable, ads_var_brightness, name):
+    def __init__(self, hass, ads_hub, ads_var_enable, ads_var_brightness, name, friendly_name):
         """Initialize AdsLight entity."""
         self._ads_hub = ads_hub
         self._on_state = False
         self._brightness = None
-        self._name = name
+        if friendly_name is not None:
+            self.entity_id = async_generate_entity_id(
+                ENTITY_ID_FORMAT, name, hass=hass)
+            self._name = friendly_name
+        else:
+            self._name = name
         self.ads_var_enable = ads_var_enable
         self.ads_var_brightness = ads_var_brightness
 
