63265128be4dfc1e012fa4b909c43875d3a6cad7
Rohan Kapoor <rohan@rohankapoor.com>
2019-01-27 15:55:57 -0800

    Correctly subscribe to state changes

--- homeassistant/components/templated_automation/__init__.py
+++ homeassistant/components/templated_automation/__init__.py
@@ -4,6 +4,8 @@ Support for Templated Automation.
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/templated_automation/
 """
+import logging
+
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
@@ -14,8 +16,10 @@ from homeassistant.components.input_boolean import \
 from homeassistant.components.light import DOMAIN as LIGHT_DOMAIN
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.const import CONF_SOURCE, STATE_OFF, \
-    SERVICE_TURN_OFF, SERVICE_TURN_ON, ATTR_ENTITY_ID
+    SERVICE_TURN_OFF, SERVICE_TURN_ON, ATTR_ENTITY_ID, STATE_ON
 from homeassistant.helpers.event import async_track_state_change
+
+_LOGGER = logging.getLogger(__name__)
 
 CONF_BIDIRECTIONAL = 'bidirectional'
 CONF_INVERTED = 'inverted'
@@ -51,17 +55,29 @@ CONFIG_SCHEMA = vol.Schema({
 
 ATTR_NEW_STATE = 'new_state'
 
+STATE_TO_SERVICE = {
+    STATE_OFF: SERVICE_TURN_OFF,
+    STATE_ON: SERVICE_TURN_ON
+}
+
+INVERTED_STATE_TO_SERVICE = {
+    STATE_OFF: SERVICE_TURN_ON,
+    STATE_ON: SERVICE_TURN_OFF
+}
+
 
 async def async_setup(hass, config):
     """Set up the Templated Automation component."""
     automations = {}
     for conf in config[DOMAIN]:
         source = conf[CONF_SOURCE]
-        target = conf[CONF_TARGET]
-        inverse = conf.get(CONF_INVERTED)
-        automation = TemplatedAutomation(hass, source, target, inverse)
-        await automation._init()
-        automations[source] = automation
+        automations[source] = TemplatedAutomation(
+            hass,
+            source,
+            conf[CONF_TARGET],
+            conf[CONF_BIDIRECTIONAL],
+            conf.get(CONF_INVERTED)
+        )
 
     hass.data[DOMAIN] = automations
     return True
@@ -70,24 +86,44 @@ async def async_setup(hass, config):
 class TemplatedAutomation:
     """Represents a Templated Automation."""
 
-    def __init__(self, hass, source, target, inverse):
+    def __init__(self, hass, source, target, bidirectional, inverted):
         """Initialize a Templated Automation object."""
         self._hass = hass
         self._source = source
         self._target = target
-        self._inverse = inverse
-        self._unsub = None
+        self._bidirectional = bidirectional
+        self._inverted = inverted
+        self._unsub = []
 
-    async def _init(self):
-        """Add the callback for state changes."""
-        self._unsub = await async_track_state_change(
-            self._hass, self._source, self.handle_state_change
+        self._unsub.append(
+            async_track_state_change(
+                self._hass, self._source, self.handle_state_change
+            )
         )
+
+        if self._bidirectional:
+            self._unsub.append(
+                async_track_state_change(
+                    self._hass, self._target, self.handle_state_change
+                )
+            )
 
     async def handle_state_change(self, entity_id, old_state, new_state):
         """Listen for state changes from source and apply to target."""
-        service = SERVICE_TURN_OFF if new_state == STATE_OFF \
-            and not self._inverse else SERVICE_TURN_ON
-        await self._hass.services.async_call(
-            'homeassistant', service, {ATTR_ENTITY_ID: self._target}
-        )
+        _LOGGER.info('Called handle_state_change on %s', entity_id)
+
+        if self._inverted:
+            service = INVERTED_STATE_TO_SERVICE[new_state.state]
+        else:
+            service = STATE_TO_SERVICE[new_state.state]
+
+        target = None
+        if entity_id == self._source:
+            target = self._target
+        elif entity_id == self._target and self._bidirectional:
+            target = self._source
+
+        if target:
+            await self._hass.services.async_call(
+                'homeassistant', service, {ATTR_ENTITY_ID: target}
+            )
