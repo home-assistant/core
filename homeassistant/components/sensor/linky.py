25135f120b5bdfe550510fe8be1bd6a1633d10b9
Antoine GRÉA <grea09@users.noreply.github.com>
2019-01-28 14:37:52 +0100

    Format 8 !

--- homeassistant/components/sensor/linky.py
+++ homeassistant/components/sensor/linky.py
@@ -138,7 +138,7 @@ class LinkySensor(Entity):
                                                     ["peak_hours"]
                                                     * 100) /
         (self._attributes["peak_hours"] +
-             self._attributes["offpeak_hours"])
+         self._attributes["offpeak_hours"])
         self._attributes["daily_cost"] = self
         ._peak_hours_cost *
         self._attributes["peak_hours"] +
