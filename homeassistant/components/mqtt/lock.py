bc6f6a61ea202a153f70d29fd041c9a7393f96fc
emontnemery <erik@montnemery.com>
2019-01-29 18:29:02 +0100

    Disable extra=vol.ALLOW_EXTRA for MQTT platforms. (#20562)

--- homeassistant/components/mqtt/lock.py
+++ homeassistant/components/mqtt/lock.py
@@ -58,7 +58,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     async def async_discover(discovery_payload):
         """Discover and add an MQTT lock."""
         try:
-            discovery_hash = discovery_payload[ATTR_DISCOVERY_HASH]
+            discovery_hash = discovery_payload.pop(ATTR_DISCOVERY_HASH)
             config = PLATFORM_SCHEMA(discovery_payload)
             await _async_setup_entity(config, async_add_entities, config_entry,
                                       discovery_hash)
