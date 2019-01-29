efba5db9c75599801bc293678adde6bfb0d70438
Eliseo Martelli <me@eliseomartelli.it>
2019-01-29 20:44:32 +0100

    google home alarm

---
+++ homeassistant/components/sensor/googlehome.py
@@ -0,0 +1,101 @@
+"""
+Support for Google Home alarm sensor.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.googlehome/
+"""
+import logging
+from datetime import timedelta
+
+from homeassistant.components.googlehome import (
+    CLIENT, DOMAIN as GOOGLEHOME_DOMAIN, NAME)
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.util import slugify
+import homeassistant.util.dt as dt_util
+
+
+DEPENDENCIES = ['googlehome']
+
+SCAN_INTERVAL = timedelta(seconds=10)
+
+_LOGGER = logging.getLogger(__name__)
+
+ICON = 'mdi:alarm'
+
+SENSOR_TYPES = {
+    'timer': "Timer",
+    'alarm': "Alarm",
+}
+
+
+async def async_setup_platform(
+        hass, config, async_add_entities, discovery_info=None):
+        if discovery_info is None:
+            _LOGGER.warning(
+                "To use this you need to configure the 'googlehome' component")
+            return False
+
+        devices = []
+        for condition in SENSOR_TYPES:
+            device = GoogleHomeAlarm(hass, hass.data[CLIENT], condition, discovery_info)
+            await device.async_init()
+            devices.append(device)
+
+        async_add_entities(devices, True)
+
+class GoogleHomeAlarm(Entity):
+
+    def __init__(self, hass, client, condition, config):
+        self._hass = hass
+        self._host = config['host']
+        self._client = client
+        self._condition = condition
+        self._name = None
+        self._state = None
+        self._available = True
+
+    async def async_init(self):
+        data = self._hass.data[GOOGLEHOME_DOMAIN][self._host]
+        info = data.get('info', {})
+        if info is None:
+            return
+        self._name = "{} {}".format(info.get('name', NAME), SENSOR_TYPES[self._condition])
+
+    async def async_update(self):
+        await self._client.update_data(self._host)
+        data = self._hass.data[GOOGLEHOME_DOMAIN][self._host]
+
+        alarms = data.get('alarms')[self._condition]
+        if alarms is None or not len(alarms):
+            self._available = False
+            return
+        self._available = True
+        time_date = dt_util.utc_from_timestamp(min(
+                element['fire_time'] for element in alarms) / 1000)
+        self._state = time_date.isoformat()
+
+    @property
+    def state(self):
+        """Return the state."""
+        return self._state
+
+    @property
+    def name(self):
+        """Return the name."""
+        return self._name
+
+    @property
+    def device_class(self):
+        """Return the device class."""
+        return 'timestamp'
+
+    @property
+    def available(self):
+        """Return the availability state."""
+        return self._available
+
+    @property
+    def icon(self):
+        """Return the icon."""
+        return ICON
