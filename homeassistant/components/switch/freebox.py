6b995eaf79211986a5f5bf280f695f541857a634
SNoof85 <snoof85@gmail.com>
2019-01-30 14:08:59 +0100

    small fix: miss blank line

--- homeassistant/components/switch/freebox.py
+++ homeassistant/components/switch/freebox.py
@@ -13,6 +13,7 @@ DEPENDENCIES = ['freebox']
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_platform(
         hass, config, async_add_entities, discovery_info=None):
     """Set up the sensors."""
