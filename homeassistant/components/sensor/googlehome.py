90cfa26d61af6bf40c6e3f8307203fa3e45f5004
Eliseo Martelli <me@eliseomartelli.it>
2019-01-31 23:01:31 +0100

    fixed indentation

--- homeassistant/components/sensor/googlehome.py
+++ homeassistant/components/sensor/googlehome.py
@@ -78,8 +78,8 @@ class GoogleHomeAlarm(Entity):
             self._available = False
             return
         self._available = True
-        time_date = dt_util.utc_from_timestamp(min(
-                element['fire_time'] for element in alarms) / 1000)
+        time_date = dt_util.utc_from_timestamp(min(element['fire_time'] 
+                                               for element in alarms) / 1000)
         self._state = time_date.isoformat()
 
     @property
