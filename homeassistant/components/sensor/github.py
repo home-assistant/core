117c37ffc6cdcc8079eb9666831fc4dae9b9cbec
Timmo <contact@timmo.xyz>
2019-02-01 15:30:01 +0000

    :shirt: Remove unused json import

--- homeassistant/components/sensor/github.py
+++ homeassistant/components/sensor/github.py
@@ -5,7 +5,6 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.github/
 """
 from datetime import timedelta
-import json
 import logging
 import voluptuous as vol
 
