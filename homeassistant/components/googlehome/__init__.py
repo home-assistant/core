1e65404aedbd90ed00f8ad62752f03887095da6d
Eliseo Martelli <me@eliseomartelli.it>
2019-02-03 09:46:29 +0100

    fix linting

--- homeassistant/components/googlehome/__init__.py
+++ homeassistant/components/googlehome/__init__.py
@@ -61,7 +61,7 @@ async def async_setup(hass, config):
         if device.get(CONF_TRACK_ALARMS):
             hass.async_create_task(
                 discovery.async_load_platform(
-                    hass, 'sensor', DOMAIN, device, config)            )
+                    hass, 'sensor', DOMAIN, device, config))
 
     return True
 
