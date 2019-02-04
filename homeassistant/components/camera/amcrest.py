207a050dbaad95957a25752593b312081ea09fa2
Jason Hu <awarecan@users.noreply.github.com>
2019-02-04 09:57:22 -0800

    Fix ffmpeg v4 stream issue (#20314)
    * Add ffmpeg version
    
    * Add ffmpeg stream content type
    
    * Change ffmpeg camera stream content type
    
    * Change ffmpeg stream content type
    
    * Lint
    
    * Add a none guard
    
    * Fix
    
    * Fix
    
    * Update onvif.py
    
    * Fix version match regrex
    
    * Fix regrex
    
    * Upgrade ha-ffmpeg to 1.11
    
    * Lint
    
    * Get ffmpeg version in ffmpeg component setup

--- homeassistant/components/camera/amcrest.py
+++ homeassistant/components/camera/amcrest.py
@@ -82,7 +82,7 @@ class AmcrestCam(Camera):
         try:
             return await async_aiohttp_proxy_stream(
                 self.hass, request, stream,
-                'multipart/x-mixed-replace;boundary=ffserver')
+                self._ffmpeg.ffmpeg_stream_content_type)
         finally:
             await stream.close()
 
