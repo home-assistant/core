1b1be4e547ee776ca23235dec8347f5ecb1707c8
Diogo Gomes <dgomes@av.it.pt>
2019-02-05 00:25:56 +0000

    new tests

--- tests/components/ipma/test_ipma.py
+++ tests/components/ipma/test_ipma.py
@@ -1,17 +1,23 @@
 """The tests for the IPMA weather component."""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from collections import namedtuple
 
 from homeassistant.components import weather
 from homeassistant.components.weather import (
     ATTR_WEATHER_HUMIDITY, ATTR_WEATHER_PRESSURE, ATTR_WEATHER_TEMPERATURE,
-    ATTR_WEATHER_WIND_BEARING, ATTR_WEATHER_WIND_SPEED)
+    ATTR_WEATHER_WIND_BEARING, ATTR_WEATHER_WIND_SPEED,
+    DOMAIN as WEATHER_DOMAIN)
 from homeassistant.util.unit_system import METRIC_SYSTEM
 from homeassistant.setup import setup_component
 
-from tests.common import get_test_home_assistant, MockDependency
+from tests.common import load_fixture, MockDependency, MockConfigEntry
 
+TEST_CONFIG = {
+    "name": "HomeTown",
+    "latitude": "40.00",
+    "longitude": "-8.00",
+}
 
 class MockStation():
     """Mock Station from pyipma."""
@@ -58,38 +64,19 @@ class MockStation():
         return 0
 
 
-class TestIPMA(unittest.TestCase):
-    """Test the IPMA weather component."""
+@patch("pyipma.Station", new=MockStation)
+async def test_setup_hass(hass):
+    """Test for successfully setting up the IPMA platform."""
+    entry = MockConfigEntry(domain='ipma', data=TEST_CONFIG)
+    await hass.config_entries.async_forward_entry_setup(entry, WEATHER_DOMAIN)
+    
+    state = hass.states.get('weather.hometown')
+    assert state.state == 'rainy'
 
-    def setUp(self):
-        """Set up things to be run when tests are started."""
-        self.hass = get_test_home_assistant()
-        self.hass.config.units = METRIC_SYSTEM
-        self.lat = self.hass.config.latitude = 40.00
-        self.lon = self.hass.config.longitude = -8.00
-
-    def tearDown(self):
-        """Stop down everything that was started."""
-        self.hass.stop()
-
-    @MockDependency("pyipma")
-    @patch("pyipma.Station", new=MockStation)
-    def test_setup(self, mock_pyipma):
-        """Test for successfully setting up the IPMA platform."""
-        assert setup_component(self.hass, weather.DOMAIN, {
-            'weather': {
-                'name': 'HomeTown',
-                'platform': 'ipma',
-            }
-        })
-
-        state = self.hass.states.get('weather.hometown')
-        assert state.state == 'rainy'
-
-        data = state.attributes
-        assert data.get(ATTR_WEATHER_TEMPERATURE) == 18.0
-        assert data.get(ATTR_WEATHER_HUMIDITY) == 71
-        assert data.get(ATTR_WEATHER_PRESSURE) == 1000.0
-        assert data.get(ATTR_WEATHER_WIND_SPEED) == 3.94
-        assert data.get(ATTR_WEATHER_WIND_BEARING) == 'NW'
-        assert state.attributes.get('friendly_name') == 'HomeTown'
+    data = state.attributes
+    assert data.get(ATTR_WEATHER_TEMPERATURE) == 18.0
+    assert data.get(ATTR_WEATHER_HUMIDITY) == 71
+    assert data.get(ATTR_WEATHER_PRESSURE) == 1000.0
+    assert data.get(ATTR_WEATHER_WIND_SPEED) == 3.94
+    assert data.get(ATTR_WEATHER_WIND_BEARING) == 'NW'
+    assert state.attributes.get('friendly_name') == 'HomeTown'
