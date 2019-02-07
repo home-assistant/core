7f710c980a52addd472e40887d1e67e8cb24b5ff
Fabio Testa <fabtesta@gmail.com>
2019-02-07 14:34:33 +0100

    Added new component optional parameter "caller". If defined is used to override default logic that uses recipient phone number in "from" API call parameter

--- homeassistant/components/notify/clicksend_tts.py
+++ homeassistant/components/notify/clicksend_tts.py
@@ -27,9 +27,11 @@ HEADERS = {CONTENT_TYPE: CONTENT_TYPE_JSON}
 
 CONF_LANGUAGE = 'language'
 CONF_VOICE = 'voice'
+CONF_CALLER = 'caller'
 
 DEFAULT_LANGUAGE = 'en-us'
 DEFAULT_VOICE = 'female'
+DEFAULT_CALLER = ''
 TIMEOUT = 5
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -38,6 +40,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_RECIPIENT): cv.string,
     vol.Optional(CONF_LANGUAGE, default=DEFAULT_LANGUAGE): cv.string,
     vol.Optional(CONF_VOICE, default=DEFAULT_VOICE): cv.string,
+    vol.Optional(CONF_CALLER, default=DEFAULT_CALLER): cv.string,
 })
 
 
@@ -60,10 +63,13 @@ class ClicksendNotificationService(BaseNotificationService):
         self.recipient = config.get(CONF_RECIPIENT)
         self.language = config.get(CONF_LANGUAGE)
         self.voice = config.get(CONF_VOICE)
+        self.caller = config.get(CONF_CALLER)
+        if self.caller == '':
+            self.caller = self.recipient
 
     def send_message(self, message="", **kwargs):
         """Send a voice call to a user."""
-        data = ({'messages': [{'source': 'hass.notify', 
+        data = ({'messages': [{'source': 'hass.notify', 'from': self.caller,
                                'to': self.recipient, 'body': message,
                                'lang': self.language, 'voice': self.voice}]})
         api_url = "{}/voice/send".format(BASE_API_URL)
