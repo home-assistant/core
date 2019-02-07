6f2c16d07d384f40d5de441eb9ea5ee7f81470f4
David F. Mulcahey <david.mulcahey@icloud.com>
2019-02-07 03:14:19 -0500

    Enable the available property for zha entities (#20788)

--- tests/components/zha/test_switch.py
+++ tests/components/zha/test_switch.py
@@ -1,11 +1,11 @@
 """Test zha switch."""
 from unittest.mock import call, patch
 from homeassistant.components.switch import DOMAIN
-from homeassistant.const import STATE_ON, STATE_OFF
+from homeassistant.const import STATE_ON, STATE_OFF, STATE_UNAVAILABLE
 from tests.common import mock_coro
 from .common import (
     async_init_zigpy_device, make_attribute, make_entity_id,
-    async_test_device_join
+    async_test_device_join, async_enable_traffic
 )
 
 ON = 1
@@ -29,6 +29,13 @@ async def test_switch(hass, config_entry, zha_gateway):
 
     cluster = zigpy_device.endpoints.get(1).on_off
     entity_id = make_entity_id(DOMAIN, zigpy_device, cluster)
+    zha_device = zha_gateway.get_device(str(zigpy_device.ieee))
+
+    # test that the switch was created and that its state is unavailable
+    assert hass.states.get(entity_id).state == STATE_UNAVAILABLE
+
+    # allow traffic to flow through the gateway and device
+    await async_enable_traffic(hass, zha_gateway, [zha_device])
 
     # test that the state has changed from unavailable to off
     assert hass.states.get(entity_id).state == STATE_OFF
@@ -71,4 +78,4 @@ async def test_switch(hass, config_entry, zha_gateway):
 
     # test joining a new switch to the network and HA
     await async_test_device_join(
-        hass, zha_gateway, OnOff.cluster_id, DOMAIN, expected_state=STATE_OFF)
+        hass, zha_gateway, OnOff.cluster_id, DOMAIN)
