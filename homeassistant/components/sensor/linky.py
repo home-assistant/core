81cf61adc2ee3ef0cc6ca9b2fd02ce9c6083faab
Antoine GRÉA <grea09@users.noreply.github.com>
2019-02-08 10:38:37 +0100

    Format one too many

--- homeassistant/components/sensor/linky.py
+++ homeassistant/components/sensor/linky.py
@@ -219,8 +219,8 @@ class LinkyData:
             _LOGGER.info("Same month last year (from 1st to same day): %s",
                          str(self.compare_month))
         except PyLinkyError as exp:
-            maintenance = "(maybe due to night maintenance downtime schedule):"
-            _LOGGER.warning("Unable to fetch Linky data %s %s", maintenance, exp)
+            reason = "(maybe due to night maintenance downtime schedule):"
+            _LOGGER.warning("Unable to fetch Linky data %s %s", reason, exp)
             return False
         return True
 
