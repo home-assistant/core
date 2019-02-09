50f0a58f4843ac28cfeddb4af891dafae42488f5
David Mulcahey <david.mulcahey@icloud.com>
2019-02-09 10:36:43 -0500

    review comments

--- homeassistant/components/zha/core/helpers.py
+++ homeassistant/components/zha/core/helpers.py
@@ -6,7 +6,6 @@ https://home-assistant.io/components/zha/
 """
 import asyncio
 import logging
-from concurrent.futures._base import TimeoutError as Timeout
 
 from .const import (
     DEFAULT_BAUDRATE, REPORT_CONFIG_MAX_INT, REPORT_CONFIG_MIN_INT,
@@ -49,7 +48,7 @@ async def bind_cluster(entity_id, cluster):
         _LOGGER.debug(
             "%s: bound  '%s' cluster: %s", entity_id, cluster_name, res[0]
         )
-    except (DeliveryError, Timeout) as ex:
+    except (DeliveryError, TimeoutError) as ex:
         _LOGGER.debug(
             "%s: Failed to bind '%s' cluster: %s",
             entity_id, cluster_name, str(ex)
@@ -69,8 +68,10 @@ async def configure_reporting(entity_id, cluster, attr,
     from zigpy.exceptions import DeliveryError
 
     attr_name = cluster.attributes.get(attr, [attr])[0]
-    attr_id = (attr, get_attr_id_by_name(cluster, attr_name))[
-        isinstance(attr, str)]
+    attr_id = (
+        attr,
+        get_attr_id_by_name(cluster, attr_name)
+        )[isinstance(attr, str)]
     cluster_name = cluster.ep_attribute
     kwargs = {}
     if manufacturer:
@@ -84,7 +85,7 @@ async def configure_reporting(entity_id, cluster, attr,
             entity_id, attr_name, cluster_name, min_report, max_report,
             reportable_change, res
         )
-    except (DeliveryError, Timeout) as ex:
+    except (DeliveryError, TimeoutError) as ex:
         _LOGGER.debug(
             "%s: failed to set reporting for '%s' attr on '%s' cluster: %s",
             entity_id, attr_name, cluster_name, str(ex)
