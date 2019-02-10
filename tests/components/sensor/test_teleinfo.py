32e50350b138815fb5fba9f70cf86a3c07a80021
Guillaume DELVIT <guiguid@free.fr>
2019-02-10 12:10:07 +0100

    fix flake8 unused import

--- tests/components/sensor/test_teleinfo.py
+++ tests/components/sensor/test_teleinfo.py
@@ -1,15 +1,5 @@
 """The tests for the teleinfo platform."""
 
-import json
-import unittest
-# from unittest import mock
-from unittest.mock import patch
-
-from homeassistant.setup import setup_component
-
-from tests.common import (get_test_home_assistant, load_fixture)
-
-
 VALID_CONFIG_MINIMAL = {
     'sensor': {
         'platform': 'teleinfo',
