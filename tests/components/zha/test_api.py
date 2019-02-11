fcefb04225c2a65db8735a6b9e23bd82b36b6fc9
David Mulcahey <david.mulcahey@icloud.com>
2019-02-10 23:07:00 -0500

    update test

--- tests/components/zha/test_api.py
+++ tests/components/zha/test_api.py
@@ -1,16 +1,15 @@
 """Test ZHA API."""
 from unittest.mock import Mock
 import pytest
-from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.components.switch import DOMAIN
 from homeassistant.components.zha.api import (
-    async_load_api, WS_ENTITIES_BY_IEEE, WS_ENTITY_CLUSTERS, ATTR_IEEE, TYPE,
-    ID, WS_ENTITY_CLUSTER_ATTRIBUTES, WS_ENTITY_CLUSTER_COMMANDS,
+    async_load_api, WS_DEVICE_CLUSTERS, ATTR_IEEE, TYPE,
+    ID, WS_DEVICE_CLUSTER_ATTRIBUTES, WS_DEVICE_CLUSTER_COMMANDS,
     WS_DEVICES
 )
 from homeassistant.components.zha.core.const import (
     ATTR_CLUSTER_ID, ATTR_CLUSTER_TYPE, IN, IEEE, MODEL, NAME, QUIRK_APPLIED,
-    ATTR_MANUFACTURER
+    ATTR_MANUFACTURER, ATTR_ENDPOINT_ID
 )
 from .common import async_init_zigpy_device
 
@@ -35,25 +34,11 @@ async def zha_client(hass, config_entry, zha_gateway, hass_ws_client):
     return await hass_ws_client(hass)
 
 
-async def test_entities_by_ieee(hass, config_entry, zha_gateway, zha_client):
-    """Test getting entity refs by ieee address."""
+async def test_device_clusters(hass, config_entry, zha_gateway, zha_client):
+    """Test getting device cluster info."""
     await zha_client.send_json({
         ID: 5,
-        TYPE: WS_ENTITIES_BY_IEEE,
-    })
-
-    msg = await zha_client.receive_json()
-
-    assert '00:0d:6f:00:0a:90:69:e7' in msg['result']
-    assert len(msg['result']['00:0d:6f:00:0a:90:69:e7']) == 2
-
-
-async def test_entity_clusters(hass, config_entry, zha_gateway, zha_client):
-    """Test getting entity cluster info."""
-    await zha_client.send_json({
-        ID: 5,
-        TYPE: WS_ENTITY_CLUSTERS,
-        ATTR_ENTITY_ID: 'switch.fakemanufacturer_fakemodel_0a9069e7_1_6',
+        TYPE: WS_DEVICE_CLUSTERS,
         ATTR_IEEE: '00:0d:6f:00:0a:90:69:e7'
     })
 
@@ -68,13 +53,13 @@ async def test_entity_clusters(hass, config_entry, zha_gateway, zha_client):
     assert cluster_info[NAME] == 'OnOff'
 
 
-async def test_entity_cluster_attributes(
+async def test_device_cluster_attributes(
         hass, config_entry, zha_gateway, zha_client):
-    """Test getting entity cluster attributes."""
+    """Test getting device cluster attributes."""
     await zha_client.send_json({
         ID: 5,
-        TYPE: WS_ENTITY_CLUSTER_ATTRIBUTES,
-        ATTR_ENTITY_ID: 'switch.fakemanufacturer_fakemodel_0a9069e7_1_6',
+        TYPE: WS_DEVICE_CLUSTER_ATTRIBUTES,
+        ATTR_ENDPOINT_ID: 1,
         ATTR_IEEE: '00:0d:6f:00:0a:90:69:e7',
         ATTR_CLUSTER_ID: 6,
         ATTR_CLUSTER_TYPE: IN
@@ -90,13 +75,13 @@ async def test_entity_cluster_attributes(
         assert attribute[NAME] is not None
 
 
-async def test_entity_cluster_commands(
+async def test_device_cluster_commands(
         hass, config_entry, zha_gateway, zha_client):
-    """Test getting entity cluster commands."""
+    """Test getting device cluster commands."""
     await zha_client.send_json({
         ID: 5,
-        TYPE: WS_ENTITY_CLUSTER_COMMANDS,
-        ATTR_ENTITY_ID: 'switch.fakemanufacturer_fakemodel_0a9069e7_1_6',
+        TYPE: WS_DEVICE_CLUSTER_COMMANDS,
+        ATTR_ENDPOINT_ID: 1,
         ATTR_IEEE: '00:0d:6f:00:0a:90:69:e7',
         ATTR_CLUSTER_ID: 6,
         ATTR_CLUSTER_TYPE: IN
