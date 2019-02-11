f96a37944211703fe3c5994c367d7cc15b14da6e
GeoffAtHome <geoff@soord.org.uk>
2019-02-11 18:39:23 +0000

    Add support to register/de-register from Lightwave WiFi Link Hub

--- homeassistant/components/lightwave/switch.py
+++ homeassistant/components/lightwave/switch.py
@@ -55,11 +55,17 @@ class LWRFSwitch(SwitchDevice):
     async def async_turn_on(self, **kwargs):
         """Turn the LightWave switch on."""
         self._state = True
-        self._lwlink.turn_on_switch(self._device_id, self._name)
+        if self._device_id == "registration":
+            self._lwlink.register()
+        else:
+            self._lwlink.turn_on_switch(self._device_id, self._name)
         self.async_schedule_update_ha_state()
 
     async def async_turn_off(self, **kwargs):
         """Turn the LightWave switch off."""
         self._state = False
-        self._lwlink.turn_off(self._device_id, self._name)
+        if self._device_id == "registration":
+            self._lwlink.deregister_all()
+        else:
+            self._lwlink.turn_off(self._device_id, self._name)
         self.async_schedule_update_ha_state()
