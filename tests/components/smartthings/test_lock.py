c2f63041d25da169103da1a6ef96a305d98a7fd8
Ben Dews <contact@bendews.com>
2019-02-13 00:49:18 +1100

    Updated tests to use new setup_platform

--- tests/components/smartthings/test_lock.py
+++ tests/components/smartthings/test_lock.py
@@ -6,28 +6,13 @@ real HTTP calls are not initiated during testing.
 """
 from pysmartthings import Attribute, Capability
 
-from homeassistant.components.smartthings import DeviceBroker, lock
+from homeassistant.components.lock import DOMAIN as LOCK_DOMAIN
+from homeassistant.components.smartthings import lock
 from homeassistant.components.smartthings.const import (
-    DATA_BROKERS, DOMAIN, SIGNAL_SMARTTHINGS_UPDATE)
-from homeassistant.config_entries import (
-    CONN_CLASS_CLOUD_PUSH, SOURCE_USER, ConfigEntry)
+    DOMAIN, SIGNAL_SMARTTHINGS_UPDATE)
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 
-
-async def _setup_platform(hass, *devices):
-    """Set up the SmartThings lock platform and prerequisites."""
-    hass.config.components.add(DOMAIN)
-    broker = DeviceBroker(hass, devices, '')
-    config_entry = ConfigEntry("1", DOMAIN, "Test", {},
-                               SOURCE_USER, CONN_CLASS_CLOUD_PUSH)
-    hass.data[DOMAIN] = {
-        DATA_BROKERS: {
-            config_entry.entry_id: broker
-        }
-    }
-    await hass.config_entries.async_forward_entry_setup(config_entry, 'lock')
-    await hass.async_block_till_done()
-    return config_entry
+from .conftest import setup_platform
 
 
 async def test_async_setup_platform():
@@ -36,7 +21,7 @@ async def test_async_setup_platform():
 
 
 def test_is_lock(device_factory):
-    """Test lockes are correctly identified."""
+    """Test locks are correctly identified."""
     lock_device = device_factory('Lock', [Capability.lock])
     assert lock.is_lock(lock_device)
 
@@ -49,7 +34,7 @@ async def test_entity_and_device_attributes(hass, device_factory):
     entity_registry = await hass.helpers.entity_registry.async_get_registry()
     device_registry = await hass.helpers.device_registry.async_get_registry()
     # Act
-    await _setup_platform(hass, device)
+    await setup_platform(hass, LOCK_DOMAIN, device)
     # Assert
     entry = entity_registry.async_get('lock.lock_1')
     assert entry
@@ -68,7 +53,7 @@ async def test_lock(hass, device_factory):
     # Arrange
     device = device_factory('Lock_1', [Capability.lock],
                             {Attribute.lock: 'unlocked'})
-    await _setup_platform(hass, device)
+    await setup_platform(hass, LOCK_DOMAIN, device)
     # Act
     await hass.services.async_call(
         'lock', 'lock', {'entity_id': 'lock.lock_1'},
@@ -84,7 +69,7 @@ async def test_unlock(hass, device_factory):
     # Arrange
     device = device_factory('Lock_1', [Capability.lock],
                             {Attribute.lock: 'locked'})
-    await _setup_platform(hass, device)
+    await setup_platform(hass, LOCK_DOMAIN, device)
     # Act
     await hass.services.async_call(
         'lock', 'unlock', {'entity_id': 'lock.lock_1'},
@@ -100,7 +85,7 @@ async def test_update_from_signal(hass, device_factory):
     # Arrange
     device = device_factory('Lock_1', [Capability.lock],
                             {Attribute.lock: 'unlocked'})
-    await _setup_platform(hass, device)
+    await setup_platform(hass, LOCK_DOMAIN, device)
     await device.lock(True)
     # Act
     async_dispatcher_send(hass, SIGNAL_SMARTTHINGS_UPDATE,
@@ -110,16 +95,3 @@ async def test_update_from_signal(hass, device_factory):
     state = hass.states.get('lock.lock_1')
     assert state is not None
     assert state.state == 'locked'
-
-
-async def test_unload_config_entry(hass, device_factory):
-    """Test the lock is removed when the config entry is unloaded."""
-    # Arrange
-    device = device_factory('Lock', [Capability.lock],
-                            {Attribute.lock: 'locked'})
-    config_entry = await _setup_platform(hass, device)
-    # Act
-    await hass.config_entries.async_forward_entry_unload(
-        config_entry, 'lock')
-    # Assert
-    assert not hass.states.get('lock.lock_1')
