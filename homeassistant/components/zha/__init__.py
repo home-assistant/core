433ffbf3230258a984b4e03c179ffb8dbe2d41a1
David Mulcahey <david.mulcahey@icloud.com>
2019-02-11 23:05:13 -0500

    update available handling

--- homeassistant/components/zha/__init__.py
+++ homeassistant/components/zha/__init__.py
@@ -152,12 +152,15 @@ async def async_setup_entry(hass, config_entry):
     def handle_message(sender, is_reply, profile, cluster,
                        src_ep, dst_ep, tsn, command_id, args):
         """Handle message from a device."""
-        hass.async_create_task(
-            zha_gateway.async_handle_zigbee_message(
-                sender, is_reply, profile, cluster, src_ep, dst_ep, tsn,
-                command_id, args
+        if (sender.last_seen is None and not sender.initializing) or \
+                (sender.ieee in zha_gateway.devices and not
+                 zha_gateway.devices[sender.ieee].available):
+            hass.async_create_task(
+                zha_gateway.async_device_became_available(
+                    sender, is_reply, profile, cluster, src_ep, dst_ep, tsn,
+                    command_id, args
+                )
             )
-        )
         return sender.handle_message(
             is_reply, profile, cluster, src_ep, dst_ep, tsn, command_id, args)
 
