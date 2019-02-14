2e6f83ae99f3fb8bfcc5f3ad0560754acce24072
Jason Hu <awaregit@gmail.com>
2019-02-14 01:41:53 -0800

    Simplify http.CachingStaticResource implementation

--- homeassistant/components/http/static.py
+++ homeassistant/components/http/static.py
@@ -5,6 +5,9 @@ from aiohttp.web_exceptions import HTTPNotFound
 from aiohttp.web_urldispatcher import StaticResource
 from yarl import URL
 
+CACHE_TIME = 31 * 86400  # = 1 month
+CACHE_HEADERS = {hdrs.CACHE_CONTROL: "public, max-age={}".format(CACHE_TIME)}
+
 
 class CachingStaticResource(StaticResource):
     """Static Resource handler that will add cache headers."""
@@ -27,27 +30,6 @@ class CachingStaticResource(StaticResource):
         if filepath.is_dir():
             return await super()._handle(request)
         if filepath.is_file():
-            return CachingFileResponse(filepath, chunk_size=self._chunk_size)
+            return FileResponse(
+                filepath, chunk_size=self._chunk_size, headers=CACHE_HEADERS)
         raise HTTPNotFound
-
-
-# pylint: disable=too-many-ancestors
-class CachingFileResponse(FileResponse):
-    """FileSender class that caches output if not in dev mode."""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize the hass file sender."""
-        super().__init__(*args, **kwargs)
-
-        orig_sendfile = self._sendfile
-
-        async def sendfile(request, fobj, count):
-            """Sendfile that includes a cache header."""
-            cache_time = 31 * 86400  # = 1 month
-            self.headers[hdrs.CACHE_CONTROL] = "public, max-age={}".format(
-                cache_time)
-
-            await orig_sendfile(request, fobj, count)
-
-        # Overwriting like this because __init__ can change implementation.
-        self._sendfile = sendfile
