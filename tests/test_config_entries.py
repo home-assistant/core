1b1cec1acc2d09de9333f485923b1891b15a491b
Robert Svensson <kane610@me.com>
2019-02-15 18:08:04 +0100

    Fix typing

--- tests/test_config_entries.py
+++ tests/test_config_entries.py
@@ -579,8 +579,7 @@ async def test_update_entry_no_change_does_not_call_save(manager):
     with patch('homeassistant.config_entries'
                '.ConfigEntries._async_schedule_save') as mock_save:
         manager.async_update_entry(entry)
-
-    assert not mock_save.mock_calls
+        assert not mock_save.mock_calls
 
 
 async def test_setup_raise_not_ready(hass, caplog):
