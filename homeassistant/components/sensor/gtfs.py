33b8dbe73afc59cee77ead7409271fd7ef1e9151
René-Marc Simard <renemarc@users.noreply.github.com>
2019-02-17 05:46:08 -0500

    Return None if no GTFS departures found (#20919)

--- homeassistant/components/sensor/gtfs.py
+++ homeassistant/components/sensor/gtfs.py
@@ -205,7 +205,7 @@ class GTFSDepartureSensor(Entity):
         self._icon = ICON
         self._name = ''
         self._unit_of_measurement = 'min'
-        self._state = 0
+        self._state = None
         self._attributes = {}
         self.lock = threading.Lock()
         self.update()
@@ -241,7 +241,7 @@ class GTFSDepartureSensor(Entity):
             self._departure = get_next_departure(
                 self._pygtfs, self.origin, self.destination, self._offset)
             if not self._departure:
-                self._state = 0
+                self._state = None
                 self._attributes = {'Info': 'No more departures today'}
                 if self._name == '':
                     self._name = (self._custom_name or DEFAULT_NAME)
