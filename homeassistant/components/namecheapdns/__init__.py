586512b1788c1eea584c50e90b31f1e973a24c0c
Antoine GRÉA <grea09@users.noreply.github.com>
2019-02-18 16:14:16 +0100

    Allowing to update several hosts.

--- homeassistant/components/namecheapdns/__init__.py
+++ homeassistant/components/namecheapdns/__init__.py
@@ -15,37 +15,49 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'namecheapdns'
 
+CONF_HOSTS = CONF_HOST + 's' #FIXME Put in homeassistant.const ?
+
 INTERVAL = timedelta(minutes=5)
 
 UPDATE_URL = 'https://dynamicdns.park-your-domain.com/update'
+
+DEFAULT_HOST = '@'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_DOMAIN): cv.string,
         vol.Required(CONF_PASSWORD): cv.string,
-        vol.Optional(CONF_HOST, default='@'): cv.string,
+        vol.Optional(CONF_HOST, default='@'): cv.deprecated(replacement_key=CONF_HOSTS),
+        vol.Optional(CONF_HOSTS): cv.ensure_list,
     })
 }, extra=vol.ALLOW_EXTRA)
 
 
 async def async_setup(hass, config):
     """Initialize the namecheap DNS component."""
-    host = config[DOMAIN][CONF_HOST]
+    _host = config[DOMAIN][CONF_HOST]
+    hosts = config[DOMAIN][CONF_HOSTS]
     domain = config[DOMAIN][CONF_DOMAIN]
     password = config[DOMAIN][CONF_PASSWORD]
 
     session = async_get_clientsession(hass)
+    
+    if not hosts and _host is not None:
+        hosts = [_host];
+    
+    result = False
+    
+    for host in hosts :
+        result |= await _update_namecheapdns(session, host, domain, password)
 
-    result = await _update_namecheapdns(session, host, domain, password)
+        if not result:
+            return False
 
-    if not result:
-        return False
+        async def update_domain_interval(now):
+            """Update the namecheap DNS entry."""
+            await _update_namecheapdns(session, host, domain, password)
 
-    async def update_domain_interval(now):
-        """Update the namecheap DNS entry."""
-        await _update_namecheapdns(session, host, domain, password)
-
-    async_track_time_interval(hass, update_domain_interval, INTERVAL)
+        async_track_time_interval(hass, update_domain_interval, INTERVAL)
 
     return result
 
