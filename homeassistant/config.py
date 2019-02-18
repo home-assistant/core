fbb0da65b7fedac0c8a598fe76adc92f6a3898b0
Jason Hu <awaregit@gmail.com>
2019-02-18 10:31:55 -0800

    Add some log information

--- homeassistant/config.py
+++ homeassistant/config.py
@@ -444,17 +444,24 @@ async def async_process_ha_core_config(
             auth_conf = [
                 {'type': 'homeassistant'}
             ]
+            _LOGGER.info('A Homeassistant auth provider is automatic'
+                         ' configured')
             if has_api_password:
                 auth_conf.append({'type': 'legacy_api_password'})
+                _LOGGER.info('A Legacy API Password auth provider is automatic'
+                             ' configured using http.api_password config')
             if trusted_networks:
                 auth_conf.append({
                     'type': 'trusted_networks',
                     'trusted_networks': trusted_networks,
                 })
+                _LOGGER.info('A Trusted Networks auth provider is automatic'
+                             ' configured using http.trusted_networks config')
 
         mfa_conf = config.get(CONF_AUTH_MFA_MODULES, [
             {'type': 'totp', 'id': 'totp', 'name': 'Authenticator app'},
         ])
+        _LOGGER.info('A Authenticator app MFA module is automatic configured')
 
         setattr(hass, 'auth', await auth.auth_manager_from_config(
             hass,
