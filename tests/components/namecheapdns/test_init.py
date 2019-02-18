ee64941b45a9ff5d28eebdf8bce461adc9d6788b
Antoine GRÉA <grea09@users.noreply.github.com>
2019-02-18 16:18:49 +0100

    Updating the test to handle several hosts

--- tests/components/namecheapdns/test_init.py
+++ tests/components/namecheapdns/test_init.py
@@ -11,12 +11,13 @@ from homeassistant.util.dt import utcnow
 from tests.common import async_fire_time_changed
 
 HOST = 'test'
+HOSTS = ['test1', 'test2']
 DOMAIN = 'bla'
 PASSWORD = 'abcdefgh'
 
 
 @pytest.fixture
-def setup_namecheapdns(hass, aioclient_mock):
+def setup_namecheapdns_deprecated(hass, aioclient_mock):
     """Fixture that sets up NamecheapDNS."""
     aioclient_mock.get(namecheapdns.UPDATE_URL, params={
         'host': HOST,
@@ -35,7 +36,7 @@ def setup_namecheapdns(hass, aioclient_mock):
 
 
 @asyncio.coroutine
-def test_setup(hass, aioclient_mock):
+def test_setup_deprecated(hass, aioclient_mock):
     """Test setup works if update passes."""
     aioclient_mock.get(namecheapdns.UPDATE_URL, params={
         'host': HOST,
@@ -56,6 +57,68 @@ def test_setup(hass, aioclient_mock):
     async_fire_time_changed(hass, utcnow() + timedelta(minutes=5))
     yield from hass.async_block_till_done()
     assert aioclient_mock.call_count == 2
+
+
+@asyncio.coroutine
+def test_setup_fails_if_update_fails_deprecated(hass, aioclient_mock):
+    """Test setup fails if first update fails."""
+    aioclient_mock.get(namecheapdns.UPDATE_URL, params={
+        'host': HOST,
+        'domain': DOMAIN,
+        'password': PASSWORD,
+    }, text='<interface-response><ErrCount>1</ErrCount></interface-response>')
+
+    result = yield from async_setup_component(hass, namecheapdns.DOMAIN, {
+        'namecheapdns': {
+            'host': HOST,
+            'domain': DOMAIN,
+            'password': PASSWORD,
+        }
+    })
+    assert not result
+    assert aioclient_mock.call_count == 1
+
+@pytest.fixture
+def setup_namecheapdns(hass, aioclient_mock):
+    """Fixture that sets up NamecheapDNS."""
+    aioclient_mock.get(namecheapdns.UPDATE_URL, params={
+        'hosts': HOSTS,
+        'domain': DOMAIN,
+        'password': PASSWORD,
+    }, text='<interface-response><ErrCount>0</ErrCount></interface-response>')
+
+    hass.loop.run_until_complete(async_setup_component(
+        hass, namecheapdns.DOMAIN, {
+            'namecheapdns': {
+                'hosts': HOSTS,
+                'domain': DOMAIN,
+                'password': PASSWORD,
+            }
+        }))
+
+
+@asyncio.coroutine
+def test_setup(hass, aioclient_mock):
+    """Test setup works if update passes."""
+    aioclient_mock.get(namecheapdns.UPDATE_URL, params={
+        'hosts': HOSTS,
+        'domain': DOMAIN,
+        'password': PASSWORD
+    }, text='<interface-response><ErrCount>0</ErrCount></interface-response>')
+
+    result = yield from async_setup_component(hass, namecheapdns.DOMAIN, {
+        'namecheapdns': {
+            'hosts': HOSTS,
+            'domain': DOMAIN,
+            'password': PASSWORD,
+        }
+    })
+    assert result
+    assert aioclient_mock.call_count == 2
+
+    async_fire_time_changed(hass, utcnow() + timedelta(minutes=5))
+    yield from hass.async_block_till_done()
+    assert aioclient_mock.call_count == 4
 
 
 @asyncio.coroutine
