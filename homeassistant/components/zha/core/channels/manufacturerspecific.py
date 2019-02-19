40922e90ab1ef674d500a0a84dce55f14289b5be
David F. Mulcahey <david.mulcahey@icloud.com>
2019-02-19 12:58:22 -0500

    Refactor ZHA listeners into channels (#21196)
    * refactor listeners to channels
    
    * update coveragerc

--- homeassistant/components/zha/core/channels/manufacturerspecific.py
+++ homeassistant/components/zha/core/channels/manufacturerspecific.py
@@ -1,10 +1,9 @@
 """
-Core module for Zigbee Home Automation.
+Manufacturer specific channels module for Zigbee Home Automation.
 
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/zha/
 """
+import logging
 
-# flake8: noqa
-from .device import ZHADevice
-from .gateway import ZHAGateway
+_LOGGER = logging.getLogger(__name__)
