521ad98e344dac007e43a8ddd159d2b3c548210c
Finbarr Brady <finbarr.brady@gmail.com>
2019-02-20 11:50:09 +0000

    Updates based on review comments
    Updates based on review comments

--- homeassistant/components/device_tracker/luci.py
+++ homeassistant/components/device_tracker/luci.py
@@ -5,9 +5,7 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/device_tracker.luci/
 """
 from collections import namedtuple
-from datetime import timedelta
 import logging
-
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
@@ -15,14 +13,10 @@ from homeassistant.components.device_tracker import (
     DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
 from homeassistant.const import (
     CONF_HOST, CONF_USERNAME, CONF_PASSWORD, CONF_SSL)
-from homeassistant.util import Throttle
-import homeassistant.util.dt as dt_util
 
 REQUIREMENTS = ['openwrt-luci-rpc==0.3.0']
 
 _LOGGER = logging.getLogger(__name__)
-
-MIN_TIME_BETWEEN_SCANS = timedelta(seconds=60)
 
 DEFAULT_SSL = False
 
@@ -41,7 +35,7 @@ def get_scanner(hass, config):
     return scanner if scanner.success_init else None
 
 
-Device = namedtuple('Device', ['mac', 'name', 'ip', 'last_update'])
+Device = namedtuple('Device', ['mac', 'name'])
 
 
 class LuciDeviceScanner(DeviceScanner):
@@ -59,7 +53,6 @@ class LuciDeviceScanner(DeviceScanner):
                                  config[CONF_USERNAME],
                                  config[CONF_PASSWORD])
 
-        """Initialize the scanner."""
         self.last_results = []  # type: List[Device]
         self.success_init = self.router.is_logged_in()
 
@@ -71,32 +64,25 @@ class LuciDeviceScanner(DeviceScanner):
 
     def get_device_name(self, device):
         """Return the name of the given device or None if we don't know."""
-        filter_named = [result.name for result in self.last_results if
-                        result.mac == device]
+        name = next((
+            result.name for result in self.last_results
+            if result.mac == device), None)
+        return name
 
-        if filter_named:
-            return filter_named[0]
-        return None
-
-    @Throttle(MIN_TIME_BETWEEN_SCANS)
     def _update_info(self):
         """Check the Luci router for devices
 
         Returns a boolean. True if scan was successful.
         """
-        _LOGGER.info("Scanning...")
-
         result = self.router.get_all_connected_devices(
             only_reachable=True)
 
-        now = dt_util.now()
+        _LOGGER.debug("Luci get_all_connected_devices returned:"
+                      " %s" % result)
+
         last_results = []
         for device in result:
             last_results.append(
-                Device(device['macaddress'], device['hostname'],
-                       device['ipaddress'], now))
+                Device(device['macaddress'], device['hostname']))
 
         self.last_results = last_results
-
-        _LOGGER.info("Scan successful")
-        return True
