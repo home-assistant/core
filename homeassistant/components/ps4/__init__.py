a9ab8ab501204ea9b54d5ceace86266890941e66
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-02-21 03:37:52 -0700

    bump 0.3.4

--- homeassistant/components/ps4/__init__.py
+++ homeassistant/components/ps4/__init__.py
@@ -11,7 +11,7 @@ from homeassistant.components.ps4.const import DOMAIN  # noqa: pylint: disable=u
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['pyps4-homeassistant==0.3.3']
+REQUIREMENTS = ['pyps4-homeassistant==0.3.4']
 
 
 async def async_setup(hass, config):
