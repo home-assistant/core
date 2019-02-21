5df89821cb3adfa4ebb58c03805f977e5d0924d5
Finbarr Brady <fbradyirl@users.noreply.github.com>
2019-02-21 22:05:48 +0000

    Update homeassistant/components/media_player/enigma2.py

--- homeassistant/components/media_player/enigma2.py
+++ homeassistant/components/media_player/enigma2.py
@@ -40,7 +40,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 # pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices, discovery_info=None):
-"""Set up of an enigma2 media player."""
+    """Set up of an enigma2 media player."""
     name = config[CONF_NAME]
 
     # Generate a configuration for the Enigma2 library
