bccd519ec3a06c2a8f8278c924761dc6cb53a151
Leonardo Merza <ljmerza@gmail.com>
2019-02-22 09:25:47 -0500

    removed comments

--- homeassistant/components/sensor/reddit.py
+++ homeassistant/components/sensor/reddit.py
@@ -1,10 +1,3 @@
-"""
-Support for Reddit.
-
-For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/sensor.reddit/
-"""
-
 from datetime import timedelta
 import logging
 import voluptuous as vol
