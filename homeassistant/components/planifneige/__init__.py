e1afa37506fa27069a73461fdab1bb681f165b75
kkr16 <21132051+kkr16@users.noreply.github.com>
2019-02-22 20:36:27 -0500

    Adding new PlanifNeige platform/sensor

---
+++ homeassistant/components/planifneige/__init__.py
@@ -0,0 +1,114 @@
+"""
+Platform for the City of Montreal's Planif-Neige snow removal APIs.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.planifneige/
+"""
+import logging
+from datetime import timedelta
+
+import voluptuous as vol
+
+from homeassistant.helpers.discovery import async_load_platform
+from homeassistant.helpers.dispatcher import dispatcher_send
+from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import (CONF_NAME, CONF_API_KEY,
+                                 CONF_UPDATE_INTERVAL, CONF_SCAN_INTERVAL,
+                                 CONF_UPDATE_INTERVAL_INVALIDATION_VERSION)
+
+DOMAIN = 'planifneige'
+
+DATA_UPDATED = '{}_data_updated'.format(DOMAIN)
+
+REQUIREMENTS = ['planif-neige-client==0.0.5']
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_STREETID = 'streetid'
+CONF_STREETS = 'streets'
+CONF_DBPATH = 'database_path'
+
+ATTR_SENSOR = 'sensor'
+ATTR_STATES = 'states'
+
+DEFAULT_INTERVAL = timedelta(minutes=5)
+
+_STREET_SCHEME = vol.Schema({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONF_STREETID): cv.positive_int
+})
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.All(
+        vol.Schema({
+            vol.Required(CONF_API_KEY): cv.string,
+            vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_INTERVAL):
+                vol.All(cv.time_period, cv.positive_timedelta),
+            vol.Required(CONF_DBPATH): cv.string,
+            vol.Required(CONF_STREETS): [_STREET_SCHEME]
+        }),
+        cv.deprecated(
+            CONF_UPDATE_INTERVAL,
+            replacement_key=CONF_SCAN_INTERVAL,
+            invalidation_version=CONF_UPDATE_INTERVAL_INVALIDATION_VERSION,
+            default=DEFAULT_INTERVAL
+        )
+    )
+}, extra=vol.ALLOW_EXTRA)
+
+
+async def async_setup(hass, config):
+    """Set up the PlanifNeige component."""
+    conf = config[DOMAIN]
+    data = hass.data[DOMAIN] = PlanifNeigeData(hass, conf.get(CONF_API_KEY),
+                                               conf.get(CONF_DBPATH),
+                                               conf.get(CONF_STREETS))
+
+    async_track_time_interval(
+        hass, data.update, conf[CONF_SCAN_INTERVAL]
+        )
+
+    def update(call=None):
+        """Service call to manually update the data."""
+        data.update()
+
+    hass.services.async_register(DOMAIN, 'planifneige', update)
+
+    hass.async_create_task(
+        async_load_platform(
+            hass,
+            SENSOR_DOMAIN,
+            DOMAIN,
+            conf[CONF_STREETS],
+            config
+        )
+    )
+
+    return True
+
+
+class PlanifNeigeData:
+    """Get the latest data from PlanifNeige."""
+
+    def __init__(self, hass, api_key, db_path, streets):
+        """Initialize the data object."""
+        self.data = []
+        self._hass = hass
+        self._streets = streets
+        self._api_key = api_key
+        self._db_path = db_path
+
+    def update(self, now=None):
+        """Get the latest data from PlanifNeige."""
+        from planif_neige_client import PlanifNeigeClient
+        _pn = PlanifNeigeClient.PlanifNeigeClient(self._api_key,
+                                                  self._db_path)
+        _pn.get_planification_for_date()
+
+        for street in self._streets:
+            self.data.append(
+                _pn.get_planification_for_street(street['streetid']))
+
+        dispatcher_send(self._hass, DATA_UPDATED)
---
+++ homeassistant/components/planifneige/sensor.py
@@ -0,0 +1,132 @@
+"""
+Sensor for the City of Montreal's Planif-Neige snow removal APIs.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.planifneige/
+"""
+
+import logging
+
+from homeassistant.const import ATTR_ATTRIBUTION
+from homeassistant.helpers.restore_state import RestoreEntity
+
+DEPENDENCIES = ['planifneige']
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_STREETID = 'streetid'
+CONF_STREETS = 'streets'
+CONF_DBPATH = 'database_path'
+
+ATTR_SENSOR = 'sensor'
+ATTR_STATES = 'states'
+
+DOMAIN = 'planifneige'
+
+STREET_STATE = {
+    '0': ['Snowed', 'mdi:snowflake'],  # street is snowed in
+    '1': ['Clear', 'mdi:road'],  # street is clear
+    '2': ['Planned', 'mdi:clock-outline'],  # street is planned for clearing
+    '3': ['Planned', 'mdi:clock-outline'],  # clearing planned with new date
+    '4': ['Snowed', 'mdi:snowflake'],  # to be planned, still snowed in
+    '5': ['Clearing', 'mdi:bulldozer'],  # trucks loading, still clearing
+    '10': ['Ploughed', 'mdi:road']  # street cleared, snow not loaded
+}
+
+PLANIFNEIGE_ATTRIBUTION = "Information provided by the City of Montreal "\
+    "(http://donnees.ville.montreal.qc.ca/dataset/deneigement)"
+
+
+async def async_setup_platform(hass, config, async_add_entities,
+                               discovery_info=None):
+    """Set up the PlanifNeige platform."""
+    data = hass.data[DOMAIN]
+    async_add_entities(
+        [PlanifNeigeSensor(data, sensor) for sensor in discovery_info]
+    )
+
+
+class PlanifNeigeSensor(RestoreEntity):
+    """PlanifNeige sensor."""
+
+    def __init__(self, data, sensor):
+        """Initialize the sensor."""
+        self._data = data.data
+        self._state = None
+        self._name = sensor['name']
+        self._streetid = sensor['streetid']
+        self._icon = ""
+        self._start_plan_date = None
+        self.__end_plan_date = None
+        self._start_replan_date = None
+        self._end_replan_date = None
+        self._date_updated = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def streetid(self):
+        """Return the name of the sensor."""
+        return self._streetid
+
+    @property
+    def start_plan_date(self):
+        """Return the name of the sensor."""
+        return self._start_plan_date
+
+    @property
+    def _end_plan_date(self):
+        """Return the name of the sensor."""
+        return self.__end_plan_date
+
+    @property
+    def start_replan_date(self):
+        """Return the name of the sensor."""
+        return self._start_replan_date
+
+    @property
+    def end_replan_date(self):
+        """Return the name of the sensor."""
+        return self._end_replan_date
+
+    @property
+    def date_updated(self):
+        """Return the name of the sensor."""
+        return self._date_updated
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Update device state."""
+        for street in self._data:
+            if street[1] == self._streetid:
+                self._state = STREET_STATE[str(street[2])][0]
+                self._icon = STREET_STATE[str(street[2])][1]
+                self._start_plan_date = street[3]
+                self.__end_plan_date = street[4]
+                self._start_replan_date = street[5]
+                self._end_replan_date = street[6]
+                self._date_updated = street[7]
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return {
+            'startPlanDate': self._start_plan_date,
+            'endPlanDate': self.__end_plan_date,
+            'startReplanDate': self._start_replan_date,
+            'endReplanDate': self._end_replan_date,
+            'dateUpdated': self._date_updated,
+            ATTR_ATTRIBUTION: PLANIFNEIGE_ATTRIBUTION
+        }
+
+    @property
+    def icon(self):
+        """Return the icon."""
+        return self._icon
