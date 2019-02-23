db2420e6a277e13925e4efaafb8005ed18dd8f2c
Jean-Paul van Ravensberg <14926452+Cloudenius@users.noreply.github.com>
2019-02-23 19:07:52 +0100

    Initial version of Azure DNS Component

---
+++ homeassistant/components/azuredns/__init__.py
@@ -0,0 +1,125 @@
+"""Support for Azure DNS services."""
+import logging
+from datetime import timedelta
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_HOST, CONF_DOMAIN, CONF_URL
+from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+REQUIREMENTS = ['adal==1.2.1']
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = 'azuredns'
+
+INTERVAL = timedelta(minutes=5)
+
+AUTHORITYHOSTURL = 'https://login.microsoftonline.com'
+RESOURCE = 'https://management.azure.com/'
+
+CONF_CLIENTID = 'clientid'
+CONF_CLIENTSECRET = 'clientsecret'
+CONF_TENANT = 'tenant'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_DOMAIN): cv.string,
+        vol.Optional(CONF_HOST, default='@'): cv.string,
+        vol.Required(CONF_TENANT): cv.string,
+        vol.Required(CONF_CLIENTID): cv.string,
+        vol.Required(CONF_CLIENTSECRET): cv.string,
+        vol.Required(CONF_URL): cv.string,
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+
+async def async_setup(hass, config):
+    """Initialize the Azure DNS component."""
+    domain = config[DOMAIN][CONF_DOMAIN]
+    host = config[DOMAIN][CONF_HOST]
+    resource = (RESOURCE)
+    tenant = config[DOMAIN][CONF_TENANT]
+    clientid = config[DOMAIN][CONF_CLIENTID]
+    clientsecret = config[DOMAIN][CONF_CLIENTSECRET]
+    authority_url = (AUTHORITYHOSTURL + '/' + config[DOMAIN][CONF_TENANT])
+    url = config[DOMAIN][CONF_URL]
+
+    session = async_get_clientsession(hass)
+
+    result = await _update_azuredns(session, domain, host, resource, tenant, clientid, clientsecret, authority_url, url)
+
+    if not result:
+        return False
+
+    async def update_domain_interval(now):
+        """Update the Azure DNS entry."""
+        await _update_azuredns(session, domain, host, resource, tenant, clientid, clientsecret, authority_url, url)
+
+    async_track_time_interval(hass, update_domain_interval, INTERVAL)
+
+    return result
+
+
+async def _update_azuredns(session, domain, host, resource, tenant, clientid, clientsecret, authority_url, url):
+    import adal
+    import json
+
+    params = {
+        'domain': domain,
+        'host': host,
+        'resource': resource,
+        'tenant': tenant,
+        'clientid': clientid,
+        'clientsecret': clientsecret,
+        'url': url,
+    }
+
+    """ Requesting the Azure AD App Token by using ADAL """
+    context = adal.AuthenticationContext(
+        authority_url, validate_authority=['tenant'] != 'adfs',
+    )
+
+    token = context.acquire_token_with_client_credentials(
+        params['resource'],
+        params['clientid'],
+        params['clientsecret'])
+
+    access_token = token.get('accessToken')
+
+    _LOGGER.debug("Azure AD App Token is:\n" + json.dumps(token, indent=2))
+
+    """ Update the DNS entry by using the Azure REST API """
+    import requests
+    from datetime import datetime
+
+    ipv4Address = requests.get('https://api.ipify.org').text
+    
+    _LOGGER.debug("External IP address is: " + ipv4Address)
+
+    headers = {"Authorization": 'Bearer ' + access_token, "Content-Type": 'application/json'}
+    url = params['url']
+    payload = {
+        "properties": {
+            "metadata": {
+                "Last_changed_by_Home_Assistant": str(datetime.now())
+            },
+            "TTL": 60,
+            "ARecords": [
+                {
+                    "ipv4Address": ipv4Address
+                }
+            ]
+        }
+    }
+
+    try:
+        requests.patch(url, json=payload, headers=headers).json()
+
+    except requests.exceptions.RequestException as errormessage:
+        _LOGGER.error(errormessage)
+        return False
+
+    return True
