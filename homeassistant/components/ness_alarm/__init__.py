934ca660362a09a4b936a71135e2299da5b9bc02
Nick Whyte <nick@nickwhyte.com>
2019-02-25 08:09:27 +1100

    ness_alarm: Allow configuration of update_interval

--- homeassistant/components/ness_alarm/__init__.py
+++ homeassistant/components/ness_alarm/__init__.py
@@ -1,6 +1,7 @@
 """Support for Ness D8X/D16X devices."""
-from collections import namedtuple
+import datetime
 import logging
+from collections import namedtuple
 
 import voluptuous as vol
 
@@ -10,7 +11,7 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 
-REQUIREMENTS = ['nessclient==0.9.10']
+REQUIREMENTS = ['nessclient==0.9.11']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -19,12 +20,14 @@ DATA_NESS = 'ness_alarm'
 
 CONF_DEVICE_HOST = 'host'
 CONF_DEVICE_PORT = 'port'
+CONF_UPDATE_INTERVAL = 'update_interval'
 CONF_ZONES = 'zones'
 CONF_ZONE_NAME = 'name'
 CONF_ZONE_TYPE = 'type'
 CONF_ZONE_ID = 'id'
 ATTR_OUTPUT_ID = 'output_id'
 DEFAULT_ZONES = []
+DEFAULT_UPDATE_INTERVAL = datetime.timedelta(minutes=1)
 
 SIGNAL_ZONE_CHANGED = 'ness_alarm.zone_changed'
 SIGNAL_ARMING_STATE_CHANGED = 'ness_alarm.arming_state_changed'
@@ -42,6 +45,8 @@ CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_DEVICE_HOST): cv.string,
         vol.Required(CONF_DEVICE_PORT): cv.port,
+        vol.Optional(CONF_UPDATE_INTERVAL, default=DEFAULT_UPDATE_INTERVAL):
+            cv.time_period,
         vol.Optional(CONF_ZONES, default=DEFAULT_ZONES):
             vol.All(cv.ensure_list, [ZONE_SCHEMA]),
     }),
@@ -67,8 +72,10 @@ async def async_setup(hass, config):
     zones = conf[CONF_ZONES]
     host = conf[CONF_DEVICE_HOST]
     port = conf[CONF_DEVICE_PORT]
+    update_interval = conf[CONF_UPDATE_INTERVAL]
 
-    client = Client(host=host, port=port, loop=hass.loop)
+    client = Client(host=host, port=port, loop=hass.loop,
+                    update_interval=update_interval.total_seconds())
     hass.data[DATA_NESS] = client
 
     async def _close(event):
