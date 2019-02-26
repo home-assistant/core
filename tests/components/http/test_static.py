f4a9583229ba366f82ce6d8d55aff40152a2e563
Jason Hu <awaregit@gmail.com>
2019-02-26 13:33:23 -0800

    Use one test file

--- tests/components/http/test_static.py
+++ tests/components/http/test_static.py
@@ -15,16 +15,17 @@ async def test_register_static_path(hass, hass_client, aiohttp_client):
     hass.http.app._router.freeze = lambda: None
     client = await hass_client()
 
+    # we are using same test file to save I/O
     with TemporaryDirectory() as tmp_dir:
         # test default setting is cache-able and no auth need
-        default_dir = os.path.join(tmp_dir, 'default')
-        os.mkdir(default_dir)
+        test_dir = os.path.join(tmp_dir, 'static_test')
+        os.mkdir(test_dir)
 
-        test_file_path = os.path.join(default_dir, 'test.jpg')
+        test_file_path = os.path.join(test_dir, 'test.jpg')
         with open(test_file_path, 'w') as tmp_file:
             tmp_file.write('test')
 
-        hass.http.register_static_path('/default', default_dir)
+        hass.http.register_static_path('/default', test_dir)
 
         # no auth
         resp = await client.get('default/test.jpg',
@@ -35,81 +36,59 @@ async def test_register_static_path(hass, hass_client, aiohttp_client):
         assert await resp.text() == 'test'
 
         # test dynamic content prefer not be cached
-        no_cache_dir = os.path.join(tmp_dir, 'no-cache')
-        os.mkdir(no_cache_dir)
+        hass.http.register_static_path('/no-cache', test_dir, False)
 
-        test_file_path = os.path.join(no_cache_dir, 'dynamic.mp3')
-        with open(test_file_path, 'w') as tmp_file:
-            tmp_file.write('no-cache')
-
-        hass.http.register_static_path('/no-cache', no_cache_dir, False)
-
-        resp = await client.get('no-cache/dynamic.mp3')
+        resp = await client.get('no-cache/test.jpg')
         assert resp.status == 200
         assert 'Cache-Control' not in resp.headers
-        assert resp.headers['Content-Type'] == 'audio/mpeg'
-        assert await resp.text() == 'no-cache'
+        assert resp.headers['Content-Type'] == 'image/jpeg'
+        assert await resp.text() == 'test'
 
         # test single file register with no cache control
-        test_file_path = os.path.join(default_dir, 'dynamic.mp4')
-        with open(test_file_path, 'w') as tmp_file:
-            tmp_file.write('no-cache')
-
         hass.http.register_static_path('/nc/d.m', test_file_path, False)
 
         resp = await client.get('nc/d.m')
         assert resp.status == 200
         assert 'Cache-Control' not in resp.headers
-        assert resp.headers['Content-Type'] == 'video/mp4'
-        assert await resp.text() == 'no-cache'
+        assert resp.headers['Content-Type'] == 'image/jpeg'
+        assert await resp.text() == 'test'
 
         # this file still can be access from cached url
-        resp = await client.get('default/dynamic.mp4')
+        resp = await client.get('default/test.jpg')
         assert resp.status == 200
         assert resp.headers['Cache-Control'] == 'public, max-age=2678400'
-        assert resp.headers['Content-Type'] == 'video/mp4'
-        assert await resp.text() == 'no-cache'
+        assert resp.headers['Content-Type'] == 'image/jpeg'
+        assert await resp.text() == 'test'
 
         # test single file register with cache control
-        test_file_path = os.path.join(no_cache_dir, 'cache.txt')
-        with open(test_file_path, 'w') as tmp_file:
-            tmp_file.write('cached')
-
         hass.http.register_static_path('/cache.me', test_file_path, True)
 
         resp = await client.get('cache.me')
         assert resp.status == 200
         assert resp.headers['Cache-Control'] == 'public, max-age=2678400'
-        assert resp.headers['Content-Type'] == 'text/plain'
-        assert await resp.text() == 'cached'
+        assert resp.headers['Content-Type'] == 'image/jpeg'
+        assert await resp.text() == 'test'
 
         # this file still can be access from no-cache url
-        resp = await client.get('no-cache/cache.txt')
+        resp = await client.get('no-cache/test.jpg')
         assert resp.status == 200
         assert 'Cache-Control' not in resp.headers
-        assert resp.headers['Content-Type'] == 'text/plain'
-        assert await resp.text() == 'cached'
+        assert resp.headers['Content-Type'] == 'image/jpeg'
+        assert await resp.text() == 'test'
 
         # test secure feature
-        secure_dir = os.path.join(tmp_dir, 'secure')
-        os.mkdir(secure_dir)
-
-        test_file_path = os.path.join(secure_dir, 'pass.html')
-        with open(test_file_path, 'w') as tmp_file:
-            tmp_file.write('<html>pass</html>')
-
-        hass.http.register_static_path('/secure', secure_dir, True,
+        hass.http.register_static_path('/secure', test_dir, True,
                                        requires_auth=True)
 
-        resp = await client.get('secure/pass.html')
+        resp = await client.get('secure/test.jpg')
         assert resp.status == 200
         # secure file is not cache-able, cache-headers parameter is ignored
         assert 'Cache-Control' not in resp.headers
-        assert resp.headers['Content-Type'] == 'text/html'
-        assert await resp.text() == '<html>pass</html>'
+        assert resp.headers['Content-Type'] == 'image/jpeg'
+        assert await resp.text() == 'test'
 
         # negative test: no auth
-        resp = await client.get('secure/pass.html',
+        resp = await client.get('secure/test.jpg',
                                 headers={'Authorization': ''})
         assert resp.status == 401
         resp = await client.get('secure/',
@@ -127,7 +106,7 @@ async def test_register_static_path(hass, hass_client, aiohttp_client):
         # negative register test
         with pytest.raises(ValueError):
             hass.http.register_static_path(
-                '/invalid', secure_dir + '/pass.html', False, True)
+                '/invalid', test_dir + '/pass.html', False, True)
         with pytest.raises(ValueError):
             hass.http.register_static_path(
-                '/invalid', secure_dir + '/not-exist', False, True)
+                '/invalid', test_dir + '/not-exist', False, True)
