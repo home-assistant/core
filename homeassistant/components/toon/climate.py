2cf9c61cc4f8fefb5464b0ab9829ade4b31a3998
Franck Nijhof <frenck@frenck.nl>
2019-02-26 19:18:09 +0100

    Rewrite of Toon component (#21186)
    * :tractor: Rewrite of Toon component
    
    * :fire: Removed manual state from list
    
    * :shirt: Addresses code review comments
    
    * :fire: Removes a log line that should not have been left behind
    
    * :shirt: Addresses linting warnings
    
    * :shirt: Addresses Hound CI warning
    
    * :shirt: Fixes small code styling issues
    
    * :sparkles: Sets an appropriate SCAN_INTERVAL
    
    * :sparkles: Sets min/max temperature for climate platform
    
    * :shirt: Makes imports more consistent with codebase
    
    * :ambulance: Fixes incorrect SCAN_INTERVAL value in climate platform
    
    * :ambulance: Uses OrderedDict for config_flow schema
    
    * :shirt: Adds return types for min/max temp
    
    * :tractor: Refactors entities into their actual devices
    
    * :arrow_up: Updates toonapilib to 3.0.7
    
    * :tractor: Refactors binary sensor state inversion
    
    * :ambulance: Fixes states of OpenTherm connection and Hot Tap Water
    
    * :sparkles: Adds Boiler Preheat binary sensor
    
    * :sparkles: Adds Toon Thermostat Program binary sensor
    
    * :sparkles: Adds Boiler Modulation Level sensor
    
    * :sparkles: Adds Daily Power Cost sensor
    
    * :fire: Cleanup of Toon Thermostat climate attributes
    
    * :tractor: Adjusts config_flow with Tenant selection
    
    * :raising_hand: Adds myself to codeowners file as maintainer
    
    * :arrow_up: Gen requirements
    
    * :arrow_up: Updates toonapilib to 3.0.9
    
    * :umbrella: Adds config_flow tests

--- homeassistant/components/toon/climate.py
+++ homeassistant/components/toon/climate.py
@@ -1,12 +1,28 @@
-"""Support for Toon van Eneco Thermostats."""
+"""Support for Toon thermostat."""
+
+from datetime import timedelta
+import logging
+from typing import Any, Dict, List
+
 from homeassistant.components.climate import ClimateDevice
 from homeassistant.components.climate.const import (
-    STATE_COOL, STATE_ECO, STATE_HEAT, STATE_AUTO,
-    SUPPORT_OPERATION_MODE, SUPPORT_TARGET_TEMPERATURE)
-import homeassistant.components.toon as toon_main
+    STATE_AUTO, STATE_COOL, STATE_ECO, STATE_HEAT, SUPPORT_OPERATION_MODE,
+    SUPPORT_TARGET_TEMPERATURE)
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
+from homeassistant.helpers.typing import HomeAssistantType
+
+from . import ToonDisplayDeviceEntity
+from .const import DATA_TOON_CLIENT, DEFAULT_MAX_TEMP, DEFAULT_MIN_TEMP, DOMAIN
+
+DEPENDENCIES = ['toon']
+
+_LOGGER = logging.getLogger(__name__)
 
 SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=5)
+SCAN_INTERVAL = timedelta(seconds=30)
 
 HA_TOON = {
     STATE_AUTO: 'Comfort',
@@ -14,77 +30,100 @@ HA_TOON = {
     STATE_ECO: 'Away',
     STATE_COOL: 'Sleep',
 }
+
 TOON_HA = {value: key for key, value in HA_TOON.items()}
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Toon climate device."""
-    add_entities([ThermostatDevice(hass)], True)
+async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry,
+                            async_add_entities) -> None:
+    """Set up a Toon binary sensors based on a config entry."""
+    toon = hass.data[DATA_TOON_CLIENT][entry.entry_id]
+    async_add_entities([ToonThermostatDevice(toon)], True)
 
 
-class ThermostatDevice(ClimateDevice):
+class ToonThermostatDevice(ToonDisplayDeviceEntity, ClimateDevice):
     """Representation of a Toon climate device."""
 
-    def __init__(self, hass):
+    def __init__(self, toon) -> None:
         """Initialize the Toon climate device."""
-        self._name = 'Toon van Eneco'
-        self.hass = hass
-        self.thermos = hass.data[toon_main.TOON_HANDLE]
-
         self._state = None
-        self._temperature = None
-        self._setpoint = None
-        self._operation_list = [
-            STATE_AUTO,
-            STATE_HEAT,
-            STATE_ECO,
-            STATE_COOL,
-        ]
+
+        self._current_temperature = None
+        self._target_temperature = None
+        self._next_target_temperature = None
+
+        self._heating_type = None
+
+        super().__init__(toon, "Toon Thermostat", 'mdi:thermostat')
 
     @property
-    def supported_features(self):
+    def unique_id(self) -> str:
+        """Return the unique ID for this thermostat."""
+        return '_'.join([DOMAIN, self.toon.agreement.id, 'climate'])
+
+    @property
+    def supported_features(self) -> int:
         """Return the list of supported features."""
         return SUPPORT_FLAGS
 
     @property
-    def name(self):
-        """Return the name of this thermostat."""
-        return self._name
-
-    @property
-    def temperature_unit(self):
-        """Return the unit of measurement used by the platform."""
+    def temperature_unit(self) -> str:
+        """Return the unit of measurement."""
         return TEMP_CELSIUS
 
     @property
-    def current_operation(self):
+    def current_operation(self) -> str:
         """Return current operation i.e. comfort, home, away."""
-        return TOON_HA.get(self.thermos.get_data('state'))
+        return TOON_HA.get(self._state)
 
     @property
-    def operation_list(self):
+    def operation_list(self) -> List[str]:
         """Return a list of available operation modes."""
-        return self._operation_list
+        return list(HA_TOON.keys())
 
     @property
-    def current_temperature(self):
+    def current_temperature(self) -> float:
         """Return the current temperature."""
-        return self.thermos.get_data('temp')
+        return self._current_temperature
 
     @property
-    def target_temperature(self):
+    def target_temperature(self) -> float:
         """Return the temperature we try to reach."""
-        return self.thermos.get_data('setpoint')
+        return self._target_temperature
 
-    def set_temperature(self, **kwargs):
+    @property
+    def min_temp(self) -> float:
+        """Return the minimum temperature."""
+        return DEFAULT_MIN_TEMP
+
+    @property
+    def max_temp(self) -> float:
+        """Return the maximum temperature."""
+        return DEFAULT_MAX_TEMP
+
+    @property
+    def device_state_attributes(self) -> Dict[str, Any]:
+        """Return the current state of the burner."""
+        return {
+            'heating_type': self._heating_type,
+        }
+
+    def set_temperature(self, **kwargs) -> None:
         """Change the setpoint of the thermostat."""
-        temp = kwargs.get(ATTR_TEMPERATURE)
-        self.thermos.set_temp(temp)
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        self.toon.thermostat = temperature
 
-    def set_operation_mode(self, operation_mode):
+    def set_operation_mode(self, operation_mode: str) -> None:
         """Set new operation mode."""
-        self.thermos.set_state(HA_TOON[operation_mode])
+        self.toon.thermostat_state = HA_TOON[operation_mode]
 
-    def update(self):
+    async def async_update(self) -> None:
         """Update local state."""
-        self.thermos.update()
+        if self.toon.thermostat_state is None:
+            self._state = None
+        else:
+            self._state = self.toon.thermostat_state.name
+
+        self._current_temperature = self.toon.temperature
+        self._target_temperature = self.toon.thermostat
+        self._heating_type = self.toon.agreement.heating_type
