ff9ee96bfd6a54ca7adfbad611156b3295d2b70a
roger-rainey <34807548+roger-rainey@users.noreply.github.com>
2019-02-25 22:25:29 -0800

    remove whitespace

--- homeassistant/components/notify/nfandroidtv.py
+++ homeassistant/components/notify/nfandroidtv.py
@@ -249,11 +249,11 @@ class NFAndroidTVNotificationService(BaseNotificationService):
                         auth_ = HTTPBasicAuth(username, password)
                     # Load file from URL with authentication
                     req = requests.get(
-                        url, auth=auth_, timeout=DEFAULT_TIMEOUT, 
+                        url, auth=auth_, timeout=DEFAULT_TIMEOUT,
                         verify=bool(int(verify_ssl_)))
                 else:
                     # Load file from URL without authentication
-                    req = requests.get(url, timeout=DEFAULT_TIMEOUT, 
+                    req = requests.get(url, timeout=DEFAULT_TIMEOUT,
                                        verify=bool(int(verify_ssl_)))
                 return req.content
 
