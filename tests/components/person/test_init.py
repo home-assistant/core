13691239fdb7b9be8cb8ce560ba6b5e7ed553b57
Victor Vostrikov <1998617+gorynychzmey@users.noreply.github.com>
2019-02-27 19:49:58 +0100

    Code style cleanup

--- tests/components/person/test_init.py
+++ tests/components/person/test_init.py
@@ -170,7 +170,8 @@ async def test_setup_two_trackers(hass, hass_admin_user):
 
     hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
     await hass.async_block_till_done()
-    hass.states.async_set(DEVICE_TRACKER, 'home',
+    hass.states.async_set(
+        DEVICE_TRACKER, 'home',
         {ATTR_SOURCE_TYPE: SOURCE_TYPE_ROUTER})
     await hass.async_block_till_done()
 
@@ -212,7 +213,8 @@ async def test_setup_two_trackers(hass, hass_admin_user):
     assert state.state == 'zone1'
     assert state.attributes.get(ATTR_SOURCE) == DEVICE_TRACKER_2
 
-    hass.states.async_set(DEVICE_TRACKER, 'home',
+    hass.states.async_set(
+        DEVICE_TRACKER, 'home',
         {ATTR_SOURCE_TYPE: SOURCE_TYPE_ROUTER})
     await hass.async_block_till_done()
     hass.states.async_set(
