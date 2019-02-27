2717dcfe1b5030f4785e82cb062cedfe8eb9ba49
Paulus Schoutsen <paulus@home-assistant.io>
2019-02-27 14:54:19 -0800

    Remove syntax error.

--- homeassistant/components/sensor/starlingbank.py
+++ homeassistant/components/sensor/starlingbank.py
@@ -44,7 +44,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Sterling Bank sensor platform."""
-    from starlingbank import StarlingAccount  # pylint: disable=syntax-error
+    from starlingbank import StarlingAccount
 
     sensors = []
     for account in config[CONF_ACCOUNTS]:
