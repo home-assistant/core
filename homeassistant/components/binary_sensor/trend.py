3e8e998078be75ae793152ea3ec38f2b50b3ea22
Fabian Affolter <mail@fabian-affolter.ch>
2019-02-28 17:46:21 +0530

    Upgrade numpy to 1.16.2 (#21525)

--- homeassistant/components/binary_sensor/trend.py
+++ homeassistant/components/binary_sensor/trend.py
@@ -17,7 +17,7 @@ from homeassistant.helpers.entity import generate_entity_id
 from homeassistant.helpers.event import async_track_state_change
 from homeassistant.util import utcnow
 
-REQUIREMENTS = ['numpy==1.16.1']
+REQUIREMENTS = ['numpy==1.16.2']
 
 _LOGGER = logging.getLogger(__name__)
 
