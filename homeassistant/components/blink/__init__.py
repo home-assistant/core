d48a11734565e467204fef765c9cae0ad78d590a
Kevin Fronczak <kfronczak@gmail.com>
2019-03-01 22:06:57 -0500

    Upgrade blinkpy with new api endpoint

--- homeassistant/components/blink/__init__.py
+++ homeassistant/components/blink/__init__.py
@@ -10,7 +10,7 @@ from homeassistant.const import (
     CONF_BINARY_SENSORS, CONF_SENSORS, CONF_FILENAME,
     CONF_MONITORED_CONDITIONS, TEMP_FAHRENHEIT)
 
-REQUIREMENTS = ['blinkpy==0.12.1']
+REQUIREMENTS = ['blinkpy==0.13.1']
 
 _LOGGER = logging.getLogger(__name__)
 
