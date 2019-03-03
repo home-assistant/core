0502c946605309c16e2e95c52e5fc2abe25047ee
sriram <638940+srirams@users.noreply.github.com>
2019-03-03 00:38:05 -0600

    make sender name configurable

--- homeassistant/components/notify/sendgrid.py
+++ homeassistant/components/notify/sendgrid.py
@@ -18,11 +18,14 @@ REQUIREMENTS = ['sendgrid==5.6.0']
 
 _LOGGER = logging.getLogger(__name__)
 
+CONF_SENDER_NAME = 'sender_name'
+
 # pylint: disable=no-value-for-parameter
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
     vol.Required(CONF_SENDER): vol.Email(),
     vol.Required(CONF_RECIPIENT): vol.Email(),
+    vol.Optional(CONF_SENDER_NAME, default='Home Assistant'): cv.string,
 })
 
 
@@ -31,19 +34,21 @@ def get_service(hass, config, discovery_info=None):
     api_key = config.get(CONF_API_KEY)
     sender = config.get(CONF_SENDER)
     recipient = config.get(CONF_RECIPIENT)
+    sender_name = config.get(CONF_SENDER_NAME)
 
-    return SendgridNotificationService(api_key, sender, recipient)
+    return SendgridNotificationService(api_key, sender, recipient, sender_name)
 
 
 class SendgridNotificationService(BaseNotificationService):
     """Implementation the notification service for email via Sendgrid."""
 
-    def __init__(self, api_key, sender, recipient):
+    def __init__(self, api_key, sender, recipient, sender_name):
         """Initialize the service."""
         from sendgrid import SendGridAPIClient
 
         self.api_key = api_key
         self.sender = sender
+        self.sender_name = sender_name
         self.recipient = recipient
 
         self._sg = SendGridAPIClient(apikey=self.api_key)
@@ -65,7 +70,7 @@ class SendgridNotificationService(BaseNotificationService):
             ],
             "from": {
                 "email": self.sender,
-                "name": "Home Assistant"
+                "name": self.sender_name
             },
             "content": [
                 {
