a778cd117f6165837a59906803efd4fea96cc198
Franck Nijhof <frenck@frenck.nl>
2019-03-05 00:51:15 +0100

    Upgrade toonapilib to 3.1.0 (#21661)

--- homeassistant/components/toon/__init__.py
+++ homeassistant/components/toon/__init__.py
@@ -16,7 +16,7 @@ from .const import (
     CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_DISPLAY, CONF_TENANT,
     DATA_TOON_CLIENT, DATA_TOON_CONFIG, DOMAIN)
 
-REQUIREMENTS = ['toonapilib==3.0.9']
+REQUIREMENTS = ['toonapilib==3.1.0']
 
 _LOGGER = logging.getLogger(__name__)
 
