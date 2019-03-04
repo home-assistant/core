fe3fa8c533bb29a1618c9de9c391cdaecf33f185
Robbie Trencheny <me@robbiet.us>
2019-03-04 05:06:59 -0800

    Add a few helper functions for custom logic components to use

--- homeassistant/components/mobile_app/__init__.py
+++ homeassistant/components/mobile_app/__init__.py
@@ -1,9 +1,10 @@
 """Integrates Native Apps to Home Assistant."""
-from homeassistant.helpers.typing import (ConfigType,
-                                          HomeAssistantType)
+from homeassistant.helpers.typing import ConfigType, HomeAssistantType
+from homeassistant.loader import bind_hass
 
-from .const import (ATTR_DELETED_IDS, ATTR_REGISTRATIONS, ATTR_STORE,
-                    DOMAIN, STORAGE_KEY, STORAGE_VERSION)
+from .const import (ATTR_APP_COMPONENT, ATTR_APP_DATA, ATTR_DELETED_IDS,
+                    ATTR_REGISTRATIONS, ATTR_STORE, DOMAIN, STORAGE_KEY,
+                    STORAGE_VERSION)
 
 from .http_api import register_http_handlers
 from .webhook import register_device_webhook
@@ -35,3 +36,45 @@ async def async_setup(hass: HomeAssistantType, config: ConfigType):
     register_websocket_handlers(hass)
 
     return True
+
+
+@bind_hass
+def async_devices(hass: HomeAssistantType, component: str) -> list:
+    """Return all devices with the given domain set for app_component."""
+    devices = []
+
+    if DOMAIN not in hass.data:
+        return devices
+
+    for device in hass.data[DOMAIN][ATTR_REGISTRATIONS].values():
+        if device.get(ATTR_APP_COMPONENT) == component:
+            devices.append(device)
+
+    return devices
+
+
+@bind_hass
+def async_devices_with_key(hass: HomeAssistantType, component: str,
+                           key: str) -> list:
+    """Return devices for the given component that has the key in app_data."""
+    devices = []
+
+    if DOMAIN not in hass.data:
+        return devices
+
+    for device in hass.data[DOMAIN][ATTR_REGISTRATIONS].values():
+        if (ATTR_APP_COMPONENT in device and key in device[ATTR_APP_DATA] and
+                device[ATTR_APP_COMPONENT] == component):
+            devices.append(device)
+
+    return devices
+
+
+@bind_hass
+def async_device_for_webhook_id(hass: HomeAssistantType,
+                                webhook_id: str) -> dict:
+    """Return devices for the given webhook ID."""
+    if DOMAIN not in hass.data:
+        return None
+
+    return hass.data[DOMAIN][ATTR_REGISTRATIONS].get(webhook_id)
