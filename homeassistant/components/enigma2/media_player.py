2fbe78f015376ebc5f8e212e2276045d940025e8
Finbarr Brady <finbarr.brady@gmail.com>
2019-03-05 17:12:45 +0000

    Bump pip

--- homeassistant/components/enigma2/media_player.py
+++ homeassistant/components/enigma2/media_player.py
@@ -14,7 +14,7 @@ from homeassistant.const import (
     STATE_OFF, STATE_ON, STATE_PLAYING, CONF_PORT)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['openwebifpy==1.1.7']
+REQUIREMENTS = ['openwebifpy==1.1.8']
 
 _LOGGER = logging.getLogger(__name__)
 
