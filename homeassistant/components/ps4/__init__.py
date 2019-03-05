c7fd2c8f8f5c6b84e03bde35160fd5fb9113c294
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-04 17:48:25 -0700

    Add support for multiple devices for PS4 component (#21302)
    * Support multiple devices.
    
    * Revert "Support multiple devices."
    
    This reverts commit 3f5d4462a98da13ebb1ab1c07d341dbd7020e6cc.
    
    * Support multiple devices
    
    * Bump to 0.3.3
    
    * bump 0.3.4
    
    * Add tests for multiple devices.
    
    * Update Requirements
    
    * Update config_flow.py
    
    * Update config_flow.py
    
    * fixed typo
    
    * Reordered functions
    
    * Added multiple flow implementation test.
    
    * fix
    
    * typo
    
    * fix tests
    
    * bump 0.4.0
    
    * Bump 0.4.0
    
    * 0.4.0
    
    * bump version
    
    * bump version
    
    * bump version
    
    * Add keep alive feature with multiple devices
    
    * bump version
    
    * bump version
    
    * bump version
    
    * bump 0.4.7
    
    * bump 0.4.7
    
    * bump 0.4.7
    
    * Edited tests.
    
    * bump/pylint
    
    * pylint
    
    * bump/pylint
    
    * bump/pylint
    
    * Change to add additional entry
    
    * Changed to multiple entries
    
    * pylint
    
    * Corrections to manage multiple devices.
    
    * lint
    
    * comments
    
    * Removed redundant for loop
    
    * Shorthand correction
    
    * Remove reference to private object
    
    * Test fix
    
    * Revert changes. Test failure.
    
    * Test fix
    
    * test fix
    
    * unindent assertions
    
    * pylint

--- homeassistant/components/ps4/__init__.py
+++ homeassistant/components/ps4/__init__.py
@@ -11,7 +11,7 @@ from homeassistant.components.ps4.const import DOMAIN  # noqa: pylint: disable=u
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['pyps4-homeassistant==0.3.0']
+REQUIREMENTS = ['pyps4-homeassistant==0.4.8']
 
 
 async def async_setup(hass, config):
