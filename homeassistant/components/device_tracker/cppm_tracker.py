93d9242cf1d566690d0a6945cbad0f9d4856968a
zemerick1 <zemerick@emerickcc.com>
2019-03-08 10:42:08 -0600

    Implemented suggested changes partially.

--- homeassistant/components/device_tracker/cppm_tracker.py
+++ homeassistant/components/device_tracker/cppm_tracker.py
@@ -5,6 +5,7 @@ Allows tracking devices with CPPM.
 """
 import logging
 from datetime import timedelta
+
 import voluptuous as vol
 from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
@@ -17,11 +18,15 @@ from homeassistant.const import (
 
 REQUIREMENTS = ['clearpasspy==1.0.2']
 
-SCAN_INTERVAL = timedelta(seconds=120)
+CONF_SCAN_INTERVAL = timedelta(seconds=120)
+
+CLIENT_ID = 'client_id'
+
+GRANT_TYPE = 'client_credentials'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
-    vol.Required('client_id'): cv.string,
+    vol.Required(CLIENT_ID): cv.string,
     vol.Required(CONF_API_KEY): cv.string,
 })
 
@@ -30,8 +35,7 @@ _LOGGER = logging.getLogger(__name__)
 
 def get_scanner(hass, config):
     """Initialize Scanner."""
-    grant_type = 'client_credentials'
-    new_scan = CPPMDeviceScanner(config[DOMAIN], grant_type)
+    new_scan = CPPMDeviceScanner(config[DOMAIN], GRANT_TYPE)
     return new_scan if new_scan.success_init else None
 
 
@@ -40,29 +44,25 @@ class CPPMDeviceScanner(DeviceScanner):
 
     def __init__(self, config, grant_type):
         """Initialize class."""
-        _LOGGER.debug("-------------INIT CALLED--------------")
         self._cppm_host = config[CONF_HOST]
         self._api_key = config[CONF_API_KEY]
         self._grant_type = grant_type
-        self._client_id = config['client_id']
+        self._client_id = config[CLIENT_ID]
         self.success_init = self.get_cppm_data()
 
     async def async_scan_devices(self):
         """Initialize scanner."""
-        _LOGGER.debug("------ SCAN DEVICES CALLED. ------------")
         self.get_cppm_data()
         return [device['mac'] for device in self.results]
 
     async def async_get_device_name(self, device):
         """Retrieve device name."""
-        _LOGGER.debug("------ RESOLVING DEVICE NAME ----")
         return [device['name'] for device in self.results]
 
     @Throttle(CONF_SCAN_INTERVAL)
     def get_cppm_data(self):
         """Retrieve data from Aruba Clearpass and return parsed result."""
         from clearpasspy import ClearPass
-        _LOGGER.debug("--------- GET CPPM DATA CALLED------------")
         data = {
             'server': self._cppm_host,
             'grant_type': self._grant_type,
@@ -83,6 +83,5 @@ class CPPMDeviceScanner(DeviceScanner):
                 devices.append(device)
             else:
                 continue
-        _LOGGER.debug("-----------Update successful!-----------")
         self.results = devices
         return True
