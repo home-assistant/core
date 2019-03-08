6dd647d57979aacfe0b0b0d974bd00c3730ab5ec
Paulus Schoutsen <balloob@gmail.com>
2019-03-08 15:11:31 -0800

    Add user group

--- tests/auth/test_auth_store.py
+++ tests/auth/test_auth_store.py
@@ -60,7 +60,7 @@ async def test_loading_no_group_data_format(hass, hass_storage):
 
     store = auth_store.AuthStore(hass)
     groups = await store.async_get_groups()
-    assert len(groups) == 2
+    assert len(groups) == 3
     admin_group = groups[0]
     assert admin_group.name == auth_store.GROUP_NAME_ADMIN
     assert admin_group.system_generated
@@ -69,6 +69,10 @@ async def test_loading_no_group_data_format(hass, hass_storage):
     assert read_group.name == auth_store.GROUP_NAME_READ_ONLY
     assert read_group.system_generated
     assert read_group.id == auth_store.GROUP_ID_READ_ONLY
+    user_group = groups[2]
+    assert user_group.name == auth_store.GROUP_NAME_USER
+    assert user_group.system_generated
+    assert user_group.id == auth_store.GROUP_ID_USER
 
     users = await store.async_get_users()
     assert len(users) == 2
@@ -153,7 +157,7 @@ async def test_loading_all_access_group_data_format(hass, hass_storage):
 
     store = auth_store.AuthStore(hass)
     groups = await store.async_get_groups()
-    assert len(groups) == 2
+    assert len(groups) == 3
     admin_group = groups[0]
     assert admin_group.name == auth_store.GROUP_NAME_ADMIN
     assert admin_group.system_generated
@@ -162,6 +166,10 @@ async def test_loading_all_access_group_data_format(hass, hass_storage):
     assert read_group.name == auth_store.GROUP_NAME_READ_ONLY
     assert read_group.system_generated
     assert read_group.id == auth_store.GROUP_ID_READ_ONLY
+    user_group = groups[2]
+    assert user_group.name == auth_store.GROUP_NAME_USER
+    assert user_group.system_generated
+    assert user_group.id == auth_store.GROUP_ID_USER
 
     users = await store.async_get_users()
     assert len(users) == 2
@@ -185,12 +193,16 @@ async def test_loading_empty_data(hass, hass_storage):
     """Test we correctly load with no existing data."""
     store = auth_store.AuthStore(hass)
     groups = await store.async_get_groups()
-    assert len(groups) == 2
+    assert len(groups) == 3
     admin_group = groups[0]
     assert admin_group.name == auth_store.GROUP_NAME_ADMIN
     assert admin_group.system_generated
     assert admin_group.id == auth_store.GROUP_ID_ADMIN
-    read_group = groups[1]
+    user_group = groups[1]
+    assert user_group.name == auth_store.GROUP_NAME_USER
+    assert user_group.system_generated
+    assert user_group.id == auth_store.GROUP_ID_USER
+    read_group = groups[2]
     assert read_group.name == auth_store.GROUP_NAME_READ_ONLY
     assert read_group.system_generated
     assert read_group.id == auth_store.GROUP_ID_READ_ONLY
@@ -212,6 +224,10 @@ async def test_system_groups_store_id_and_name(hass, hass_storage):
         {
             'id': auth_store.GROUP_ID_ADMIN,
             'name': auth_store.GROUP_NAME_ADMIN,
+        },
+        {
+            'id': auth_store.GROUP_ID_USER,
+            'name': auth_store.GROUP_NAME_USER,
         },
         {
             'id': auth_store.GROUP_ID_READ_ONLY,
