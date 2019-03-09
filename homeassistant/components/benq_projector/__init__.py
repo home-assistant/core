760fd02cea439176c40bcad73e029fdf1df48534
Carsten Schröder <carsten@schroeder.net>
2019-03-09 16:33:06 +0100

    fix pylint 3

--- homeassistant/components/benq_projector/__init__.py
+++ homeassistant/components/benq_projector/__init__.py
@@ -1,3 +1,6 @@
 """
-Obtain and set state of a Benq projector over telnet-to-serial gateway
+Obtain and set state of a Benq projector over telnet-to-serial gateway.
+
+For more details about this component, please refer to the documentation
+at https://home-assistant.io/components/switch.benq_projector/
 """
--- homeassistant/components/benq_projector/switch.py
+++ homeassistant/components/benq_projector/switch.py
@@ -1,7 +1,9 @@
 """
 Obtain and set state of a Benq projector over telnet-to-serial gateway.
-"""
 
+For more details about this component, please refer to the documentation
+at https://home-assistant.io/components/switch.benq_projector/
+"""
 import logging
 import threading
 
