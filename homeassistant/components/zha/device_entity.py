551c5e4a668d15ba7a0895de1628d5b53354899f
David Mulcahey <david.mulcahey@me.com>
2019-03-09 10:34:52 -0500

    move inside guard

--- homeassistant/components/zha/device_entity.py
+++ homeassistant/components/zha/device_entity.py
@@ -146,7 +146,7 @@ class ZhaDeviceEntity(ZhaEntity):
         """Get the latest battery reading from channels cache."""
         battery = await self._battery_channel.get_attribute_value(
             'battery_percentage_remaining')
-        # per zcl specs battery percent is reported at 200% ¯\_(ツ)_/¯
-        battery = battery / 2
         if battery is not None:
+            # per zcl specs battery percent is reported at 200% ¯\_(ツ)_/¯
+            battery = battery / 2
             self._device_state_attributes['battery_level'] = battery
