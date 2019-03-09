a69c0bafd2c604d87e2ba4be7953ff8f95301378
karlkar <karlkar@tlen.pl>
2019-03-09 01:16:28 +0100

    Check if mac is set when more than 2 gateways
    When more than 2 gateways mac is required for each of them. Now voluptuous will require it.

--- homeassistant/components/xiaomi_aqara/__init__.py
+++ homeassistant/components/xiaomi_aqara/__init__.py
@@ -62,7 +62,6 @@ SERVICE_SCHEMA_REMOVE_DEVICE = vol.Schema({
 
 
 GATEWAY_CONFIG = vol.Schema({
-    vol.Optional(CONF_MAC, default=None): vol.Any(GW_MAC, None),
     vol.Optional(CONF_KEY):
         vol.All(cv.string, vol.Length(min=16, max=16)),
     vol.Optional(CONF_HOST): cv.string,
@@ -89,7 +88,10 @@ def _fix_conf_defaults(config):
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Optional(CONF_GATEWAYS, default={}):
-            vol.All(cv.ensure_list, [GATEWAY_CONFIG], [_fix_conf_defaults]),
+            vol.All(cv.ensure_list, vol.Any(
+                vol.All([{vol.Optional(CONF_MAC, default=None): vol.Any(GW_MAC, None)}], vol.Length(max=1)),
+                vol.All([{vol.Required(CONF_MAC): GW_MAC}], vol.Length(min=2))
+            ), [GATEWAY_CONFIG], [_fix_conf_defaults]),
         vol.Optional(CONF_INTERFACE, default='any'): cv.string,
         vol.Optional(CONF_DISCOVERY_RETRY, default=3): cv.positive_int
     })
