785fd273e31078fa279a68aa8ef22424ae8d4549
Robbie Trencheny <me@robbiet.us>
2019-03-11 05:34:58 -0700

    If registration supports encryption then return encrypted payloads (#21853)

--- tests/components/mobile_app/test_http_api.py
+++ tests/components/mobile_app/test_http_api.py
@@ -56,4 +56,10 @@ async def test_registration(hass_client, authed_api_client):  # noqa: F811
     assert resp.status == 200
 
     webhook_json = await resp.json()
-    assert webhook_json == {'rendered': 'Hello world'}
+    assert 'encrypted_data' in webhook_json
+
+    decrypted_data = SecretBox(key).decrypt(webhook_json['encrypted_data'],
+                                            encoder=Base64Encoder)
+    decrypted_data = decrypted_data.decode("utf-8")
+
+    assert json.loads(decrypted_data) == {'rendered': 'Hello world'}
