3b659449128fdf8e82582934dba0ff2c22387155
Jon Maddox <jon@jonmaddox.com>
2019-03-12 23:07:35 -0400

    THIS ONE TOO

--- homeassistant/components/tesla/switch.py
+++ homeassistant/components/tesla/switch.py
@@ -44,9 +44,7 @@ class ChargerSwitch(TeslaDevice, SwitchDevice):
     @property
     def unique_id(self) -> str:
         """Return a unique ID."""
-        s = "-"
-        parts = ["switch", 'tesla', self.tesla_id]
-        return s.join(parts)
+        return self.tesla_id
 
     @property
     def is_on(self):
