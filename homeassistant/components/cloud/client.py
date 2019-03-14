29f4754982bac3aafc5ed12130b3bd33d100974e
Pascal Vizeli <pvizeli@syshack.ch>
2019-03-14 18:47:16 +0100

    respell

--- homeassistant/components/cloud/client.py
+++ homeassistant/components/cloud/client.py
@@ -136,15 +136,15 @@ class CloudClient(Interface):
         if not self._prefs.google_enabled:
             return ga.turned_off_response(payload)
 
-        result = await ga.async_handle_message(
+        answer = await ga.async_handle_message(
             self._hass, self.google_config, self.prefs.cloud_user, payload
         )
 
         # Fix AgentUserId
         cloud = self._hass[DOMAIN]
-        result['payload']['agentUserId'] = cloud.claims['cognito:username']
+        answer['payload']['agentUserId'] = cloud.claims['cognito:username']
 
-        return result
+        return answer
 
     async def async_webhook_message(
             self, payload: Dict[Any, Any]) -> Dict[Any, Any]:
