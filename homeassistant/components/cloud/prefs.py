87ec1deb36174fb09ed86db1dd3c82b60500284c
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-03-15 00:18:31 +0100

    Fix Google Assistant User with Cloud (#22042)
    * Fix Google Assistant User with Cloud
    
    * Fix User Agent ID
    
    * respell
    
    * Fix object
    
    * Fix tests
    
    * fix lint
    
    * Fix lint

--- homeassistant/components/cloud/prefs.py
+++ homeassistant/components/cloud/prefs.py
@@ -1,7 +1,7 @@
 """Preference management for cloud."""
 from .const import (
     DOMAIN, PREF_ENABLE_ALEXA, PREF_ENABLE_GOOGLE, PREF_ENABLE_REMOTE,
-    PREF_GOOGLE_ALLOW_UNLOCK, PREF_CLOUDHOOKS)
+    PREF_GOOGLE_ALLOW_UNLOCK, PREF_CLOUDHOOKS, PREF_CLOUD_USER)
 
 STORAGE_KEY = DOMAIN
 STORAGE_VERSION = 1
@@ -26,14 +26,16 @@ class CloudPreferences:
                 PREF_ENABLE_GOOGLE: True,
                 PREF_ENABLE_REMOTE: False,
                 PREF_GOOGLE_ALLOW_UNLOCK: False,
-                PREF_CLOUDHOOKS: {}
+                PREF_CLOUDHOOKS: {},
+                PREF_CLOUD_USER: None,
             }
 
         self._prefs = prefs
 
     async def async_update(self, *, google_enabled=_UNDEF,
                            alexa_enabled=_UNDEF, remote_enabled=_UNDEF,
-                           google_allow_unlock=_UNDEF, cloudhooks=_UNDEF):
+                           google_allow_unlock=_UNDEF, cloudhooks=_UNDEF,
+                           cloud_user=_UNDEF):
         """Update user preferences."""
         for key, value in (
                 (PREF_ENABLE_GOOGLE, google_enabled),
@@ -41,6 +43,7 @@ class CloudPreferences:
                 (PREF_ENABLE_REMOTE, remote_enabled),
                 (PREF_GOOGLE_ALLOW_UNLOCK, google_allow_unlock),
                 (PREF_CLOUDHOOKS, cloudhooks),
+                (PREF_CLOUD_USER, cloud_user),
         ):
             if value is not _UNDEF:
                 self._prefs[key] = value
@@ -75,3 +78,8 @@ class CloudPreferences:
     def cloudhooks(self):
         """Return the published cloud webhooks."""
         return self._prefs.get(PREF_CLOUDHOOKS, {})
+
+    @property
+    def cloud_user(self) -> str:
+        """Return ID from Home Assistant Cloud system user."""
+        return self._prefs.get(PREF_CLOUD_USER)
