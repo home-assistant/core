a7738bc94b2d16820548ff8ae8cffc7bb7a04d62
Swamp-Ig <github@ninjateaparty.com>
2019-03-16 20:05:07 +0800

    Moved media_player components with tests into platform dirs.

---
+++ homeassistant/components/blackbird/__init__.py
@@ -0,0 +1,6 @@
+"""The blackbird component.
+
+    For more details about this component, please refer to the documentation at
+    https://home-assistant.io/components/blackbird/
+    """
+    
---
+++ homeassistant/components/monoprice/__init__.py
@@ -0,0 +1,6 @@
+"""The monoprice component.
+
+    For more details about this component, please refer to the documentation at
+    https://home-assistant.io/components/monoprice/
+    """
+    
