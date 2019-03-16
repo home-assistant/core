f6443b8fb905fa9a37d5fd4d47babf4964f7e14f
Swamp-Ig <github@ninjateaparty.com>
2019-03-16 20:05:00 +0800

    Moved light components with tests into platform dirs.

---
+++ tests/components/everlights/__init__.py
@@ -0,0 +1,6 @@
+"""Tests for the everlights component.
+
+    For more details about this component, please refer to the documentation at
+    https://home-assistant.io/components/everlights/
+    """
+    
