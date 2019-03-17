f95c977d3aa07dcfba40a3153eb80780cd3317b9
CV <dagobert@users.noreply.github.com>
2019-03-17 23:07:52 +0100

    Fix breaking on HTML email without images
    If using html emails with no images the code breaks since it is not tested for empty (uninitialized) key images.

--- homeassistant/components/notify/smtp.py
+++ homeassistant/components/notify/smtp.py
@@ -241,14 +241,15 @@ def _build_html_msg(text, html, images):
     alternative.attach(MIMEText(html, ATTR_HTML, _charset='utf-8'))
     msg.attach(alternative)
 
-    for atch_num, atch_name in enumerate(images):
-        name = os.path.basename(atch_name)
-        try:
-            with open(atch_name, 'rb') as attachment_file:
-                attachment = MIMEImage(attachment_file.read(), filename=name)
-            msg.attach(attachment)
-            attachment.add_header('Content-ID', '<{}>'.format(name))
-        except FileNotFoundError:
-            _LOGGER.warning("Attachment %s [#%s] not found. Skipping",
-                            atch_name, atch_num)
+    if images is not None:
+        for atch_num, atch_name in enumerate(images):
+            name = os.path.basename(atch_name)
+            try:
+                with open(atch_name, 'rb') as attachment_file:
+                    attachment = MIMEImage(attachment_file.read(), filename=name)
+                msg.attach(attachment)
+                attachment.add_header('Content-ID', '<{}>'.format(name))
+            except FileNotFoundError:
+                _LOGGER.warning("Attachment %s [#%s] not found. Skipping",
+                                atch_name, atch_num)
     return msg
