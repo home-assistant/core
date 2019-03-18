96ba837290adf430e624a28d14a7e828ef6c0a00
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-18 12:23:52 -0700

    Update const.py

--- homeassistant/components/ps4/const.py
+++ homeassistant/components/ps4/const.py
@@ -3,5 +3,5 @@ DEFAULT_NAME = "PlayStation 4"
 DEFAULT_REGION = "United States"
 DOMAIN = 'ps4'
 
-# Deprecated used for logger only
+# Deprecated used for logger/backwards compatibility from 0.89
 REGIONS = ['R1', 'R2', 'R3', 'R4', 'R5']
