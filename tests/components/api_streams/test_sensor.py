8c4904aca7c755da81828d746790dd9629c88946
Swamp-Ig <github@ninjateaparty.com>
2019-03-18 08:33:04 +0800

    There seems to be a thread race condition. I suspect the logger stuff is running in another thread, which means waiting until the aio loop is done is missing the log messages. Used sleep instead because that allows the logger thread to run. I think the api_streams sensor might not be thread safe.

--- tests/components/api_streams/test_sensor.py
+++ tests/components/api_streams/test_sensor.py
@@ -6,13 +6,12 @@ from homeassistant.bootstrap import async_setup_component
 from tests.common import assert_setup_component
 
 
-@asyncio.coroutine
-def test_api_streams(hass):
+async def test_api_streams(hass):
     """Test API streams."""
     log = logging.getLogger('homeassistant.components.api')
 
     with assert_setup_component(1):
-        yield from async_setup_component(hass, 'sensor', {
+        await async_setup_component(hass, 'sensor', {
             'sensor': {
                 'platform': 'api_streams',
             }
@@ -22,31 +21,30 @@ def test_api_streams(hass):
     assert state.state == '0'
 
     log.debug('STREAM 1 ATTACHED')
-    yield from hass.async_block_till_done()
+    await asyncio.sleep(0.1)
 
     state = hass.states.get('sensor.connected_clients')
     assert state.state == '1'
 
     log.debug('STREAM 1 ATTACHED')
-    yield from hass.async_block_till_done()
+    await asyncio.sleep(0.1)
 
     state = hass.states.get('sensor.connected_clients')
     assert state.state == '2'
 
     log.debug('STREAM 1 RESPONSE CLOSED')
-    yield from hass.async_block_till_done()
+    await asyncio.sleep(0.1)
 
     state = hass.states.get('sensor.connected_clients')
     assert state.state == '1'
 
 
-@asyncio.coroutine
-def test_websocket_api(hass):
+async def test_websocket_api(hass):
     """Test API streams."""
     log = logging.getLogger('homeassistant.components.websocket_api')
 
     with assert_setup_component(1):
-        yield from async_setup_component(hass, 'sensor', {
+        await async_setup_component(hass, 'sensor', {
             'sensor': {
                 'platform': 'api_streams',
             }
@@ -56,19 +54,19 @@ def test_websocket_api(hass):
     assert state.state == '0'
 
     log.debug('WS %s: %s', id(log), 'Connected')
-    yield from hass.async_block_till_done()
+    await asyncio.sleep(0.1)
 
     state = hass.states.get('sensor.connected_clients')
     assert state.state == '1'
 
     log.debug('WS %s: %s', id(log), 'Connected')
-    yield from hass.async_block_till_done()
+    await asyncio.sleep(0.1)
 
     state = hass.states.get('sensor.connected_clients')
     assert state.state == '2'
 
     log.debug('WS %s: %s', id(log), 'Closed connection')
-    yield from hass.async_block_till_done()
+    await asyncio.sleep(0.1)
 
     state = hass.states.get('sensor.connected_clients')
     assert state.state == '1'
