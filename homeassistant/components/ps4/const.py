6b1d37d6d46a4c25ef6052916ebba751ea39c6bd
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-18 12:22:11 -0700

    Update const.py

--- homeassistant/components/ps4/const.py
+++ homeassistant/components/ps4/const.py
@@ -1,4 +1,7 @@
 """Constants for PlayStation 4."""
 DEFAULT_NAME = "PlayStation 4"
+DEFAULT_REGION = "United States"
 DOMAIN = 'ps4'
-REGIONS = ('R1', 'R2', 'R3', 'R4', 'R5')
+
+# Deprecated
+REGIONS = ['R1', 'R2', 'R3', 'R4', 'R5']
