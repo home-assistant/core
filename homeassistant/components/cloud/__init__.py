90c07767cb45484bd64877c86cb77416a3d3ceb5
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-03-19 15:10:30 +0100

    Update Hass-NabuCasa 0.8 (#22177)

--- homeassistant/components/cloud/__init__.py
+++ homeassistant/components/cloud/__init__.py
@@ -24,7 +24,7 @@ from .const import (
     CONF_USER_POOL_ID, DOMAIN, MODE_DEV, MODE_PROD)
 from .prefs import CloudPreferences
 
-REQUIREMENTS = ['hass-nabucasa==0.7']
+REQUIREMENTS = ['hass-nabucasa==0.8']
 DEPENDENCIES = ['http']
 
 _LOGGER = logging.getLogger(__name__)
