7fbee4d652262d339fe79bca9e7ba08fd7423bd6
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-03-20 02:17:54 -0700

    Add migration for v1 entry

--- homeassistant/components/ps4/__init__.py
+++ homeassistant/components/ps4/__init__.py
@@ -32,3 +32,40 @@ async def async_unload_entry(hass, entry):
     await hass.config_entries.async_forward_entry_unload(
         entry, 'media_player')
     return True
+
+
+async def async_migrate_entry(hass, entry):
+    """Migrate old entry."""
+    from pyps4_homeassistant.media_art import COUNTRIES
+
+    config_entries = hass.config_entries
+    data = entry.data
+    version = entry.version
+
+    reason = {1: "Region codes have changed"}
+
+    # Migrate Version 1 -> Version 2
+    if version == 1:
+        loc = location.detect_location_info()
+        if loc:
+            country = loc.country_name
+            if country in COUNTRIES:
+                for device in data['devices']:
+                    device[CONF_REGION] = country
+                entry.version = 2
+                config_entries.async_update_entry(entry, data=data)
+                _LOGGER.info(
+                    "PlayStation 4 Config Updated: \
+                    Region changed to: %s", country)
+                return True
+
+    msg = """{} for the PlayStation 4 Integration.
+            Please remove the PS4 Integration and re-configure
+            [here](/config/integrations).""".format(reason[version])
+
+    hass.components.persistent_notification.async_create(
+        title="PlayStation 4 Integration Configuration Requires Update",
+        message=msg,
+        notification_id='config_entry_migration'
+    )
+    return False
