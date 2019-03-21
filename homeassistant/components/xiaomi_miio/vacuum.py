0c58cca8d4188138aa758de8f384f5994b9a946f
Sebastian Muszynski <basti@linkt.de>
2019-03-21 08:56:36 +0100

    Bump python-miio version (#22202)
    * Bump python-miio version
    
    * Rename speed property to motor_speed
    
    * Enable set_led service of the Air Humidifier
    
    * Allow a favorite level in [0...17]
    
    * Allow a scene in [0...6]

--- homeassistant/components/xiaomi_miio/vacuum.py
+++ homeassistant/components/xiaomi_miio/vacuum.py
@@ -16,7 +16,7 @@ from homeassistant.const import (
     ATTR_ENTITY_ID, CONF_HOST, CONF_NAME, CONF_TOKEN, STATE_OFF, STATE_ON)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['python-miio==0.4.4', 'construct==2.9.45']
+REQUIREMENTS = ['python-miio==0.4.5', 'construct==2.9.45']
 
 _LOGGER = logging.getLogger(__name__)
 
