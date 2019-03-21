ddb90d7f0df1872271b94d4ec6adee862ec694be
Steven Looman <steven.looman@gmail.com>
2019-03-21 08:58:04 +0100

    Upgrade to async_upnp_client==0.14.6 (#22223)
    Upgrade to async_upnp_client to 0.14.6

--- homeassistant/components/dlna_dmr/media_player.py
+++ homeassistant/components/dlna_dmr/media_player.py
@@ -29,7 +29,7 @@ from homeassistant.helpers.typing import HomeAssistantType
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util import get_local_ip
 
-REQUIREMENTS = ['async-upnp-client==0.14.5']
+REQUIREMENTS = ['async-upnp-client==0.14.6']
 
 _LOGGER = logging.getLogger(__name__)
 
