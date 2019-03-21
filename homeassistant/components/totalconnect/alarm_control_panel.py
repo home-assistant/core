6743ffbe61fed8640ac1d8e4a6c82fdd852f7278
Karim Roukoz <roukoz@gmail.com>
2019-03-21 12:39:30 -0400

    Bump total-connect-client to 0.25, fixing issue with Total Connect (#22230)
    * Bump total-connect-client to 0.25
    
    * Bump version in requirements_all.txt

--- homeassistant/components/totalconnect/alarm_control_panel.py
+++ homeassistant/components/totalconnect/alarm_control_panel.py
@@ -18,7 +18,7 @@ from homeassistant.const import (
     STATE_ALARM_ARMED_CUSTOM_BYPASS)
 
 
-REQUIREMENTS = ['total_connect_client==0.24']
+REQUIREMENTS = ['total_connect_client==0.25']
 
 _LOGGER = logging.getLogger(__name__)
 
