7a91dc1c2ddbaa89aac405baab552719d31d1632
fucm <fucm@users.noreply.github.com>
2019-03-21 22:08:14 +0100

    Prepare component for sensor platform

--- homeassistant/components/stiebel_eltron/__init__.py
+++ homeassistant/components/stiebel_eltron/__init__.py
@@ -1,1 +1,77 @@
-"""The stiebel_eltron component."""
+"""
+The component for STIEBEL ELTRON heat pumps with ISGWeb Modbus module.
+
+Example configuration:
+
+modbus:
+  type: tcp
+  host: YOUR_ISGWEB_IP
+  port: 502
+
+stiebel_eltron:
+  name: LWZ504e
+
+For more details about this component, please refer to the documentation
+https://home-assistant.io/components/stiebel_eltron/
+"""
+from datetime import timedelta
+import logging
+
+import voluptuous as vol
+
+from homeassistant.components.modbus import (
+    CONF_HUB, DEFAULT_HUB, DOMAIN as MODBUS_DOMAIN)
+from homeassistant.const import CONF_NAME, DEVICE_DEFAULT_NAME
+from homeassistant.helpers import discovery
+import homeassistant.helpers.config_validation as cv
+from homeassistant.util import Throttle
+
+REQUIREMENTS = ['pystiebeleltron==0.0.1.dev2']
+DEPENDENCIES = ['modbus']
+
+DOMAIN = 'stiebel_eltron'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Optional(CONF_NAME, default=DEVICE_DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_HUB, default=DEFAULT_HUB): cv.string,
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+_LOGGER = logging.getLogger(__name__)
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
+
+
+class StiebelEltronData:
+    """Get the latest data and update the states."""
+
+    def __init__(self, name, modbus_client):
+        """Init the STIEBEL ELTRON data object."""
+        from pystiebeleltron import pystiebeleltron
+        self.api = pystiebeleltron.StiebelEltronAPI(modbus_client, 1)
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        """Update unit data."""
+        if not self.api.update():
+            _LOGGER.warning("Modbus read failed")
+
+        _LOGGER.debug("Data updated successfully")
+
+
+def setup(hass, config):
+    """Set up the STIEBEL ELTRON unit.
+
+    Will automatically load climat platform.
+    """
+    name = config[DOMAIN].get(CONF_NAME, DEVICE_DEFAULT_NAME)
+    modbus_client = hass.data[MODBUS_DOMAIN][config[DOMAIN].get(CONF_HUB)]
+
+    hass.data[DOMAIN] = {
+        'name': name,
+        'ste_data': StiebelEltronData(name, modbus_client)
+    }
+
+    discovery.load_platform(hass, 'climate', DOMAIN, {}, config)
+    return True
--- homeassistant/components/stiebel_eltron/climate.py
+++ homeassistant/components/stiebel_eltron/climate.py
@@ -1,44 +1,14 @@
-"""
-Platform for STIEBEL ELTRON heat pumps with ISGWeb Modbus module.
-
-Example configuration:
-
-modbus:
-  type: tcp
-  host: YOUR_ISGWEB_IP
-  port: 502
-
-climate:
-  - platform: stiebel_eltron
-    name: LWZ504e
-
-For more details about this platform, please refer to the documentation
-https://home-assistant.io/components/climate.stiebel_eltron/
-"""
+"""Support for stiebel_eltron climat platform."""
 import logging
-
-import voluptuous as vol
 
 from homeassistant.components.climate import ClimateDevice
 from homeassistant.components.climate.const import (
     ATTR_CURRENT_HUMIDITY, STATE_AUTO, STATE_IDLE, STATE_MANUAL,
     SUPPORT_OPERATION_MODE, SUPPORT_TARGET_TEMPERATURE)
-from homeassistant.components.modbus import (
-    CONF_HUB, DEFAULT_HUB, DOMAIN as MODBUS_DOMAIN)
-from homeassistant.const import (
-    ATTR_TEMPERATURE, CONF_NAME, DEVICE_DEFAULT_NAME, TEMP_CELSIUS)
-import homeassistant.helpers.config_validation as cv
+from homeassistant.components.stiebel_eltron import DOMAIN as STE_DOMAIN
+from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
 
-REQUIREMENTS = ['pystiebeleltron==0.0.1.dev2']
-DEPENDENCIES = ['modbus']
-
-
-DEFAULT_SLAVE = 1
-
-PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default=DEVICE_DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_HUB, default=DEFAULT_HUB): cv.string,
-})
+DEPENDENCIES = ['stiebel_eltron']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -65,22 +35,19 @@ HA_TO_STE_STATE = {value: key for key, value in STE_TO_HA_STATE.items()}
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the StiebelEltron platform."""
-    name = config.get(CONF_NAME, DEVICE_DEFAULT_NAME)
-    modbus_client = hass.data[MODBUS_DOMAIN][config.get(CONF_HUB)]
+    name = hass.data[STE_DOMAIN]['name']
+    ste_data = hass.data[STE_DOMAIN]['ste_data']
 
-    add_devices([StiebelEltron(name, modbus_client, DEFAULT_SLAVE)], True)
+    add_devices([StiebelEltron(name, ste_data)], True)
     return True
 
 
 class StiebelEltron(ClimateDevice):
     """Representation of a STIEBEL ELTRON heat pump."""
 
-    def __init__(self, name, modbus_client, modbus_slave):
+    def __init__(self, name, ste_data):
         """Initialize the unit."""
-        from pystiebeleltron import pystiebeleltron
         self._name = name
-        self._modbus_client = modbus_client
-        self._modbus_slave = modbus_slave
 
         self._target_temperature = None
         self._current_temperature = None
@@ -88,9 +55,9 @@ class StiebelEltron(ClimateDevice):
         self._operation_modes = [STATE_AUTO, STATE_IDLE, STATE_DHW]
         self._current_operation = None
         self._filter_alarm = None
-        self.unit = pystiebeleltron.StiebelEltronAPI(self._modbus_client,
-                                                     self._modbus_slave)
-        _LOGGER.debug("Initialized STIEBEL ELTRON climat component.")
+        self._ste_data = ste_data
+
+        _LOGGER.debug("Initialized climate platform")
 
     @property
     def supported_features(self):
@@ -99,14 +66,13 @@ class StiebelEltron(ClimateDevice):
 
     def update(self):
         """Update unit attributes."""
-        if not self.unit.update():
-            _LOGGER.warning("Modbus read failed")
+        self._ste_data.update()
 
-        self._target_temperature = self.unit.get_target_temp()
-        self._current_temperature = self.unit.get_current_temp()
-        self._current_humidity = self.unit.get_current_humidity()
-        self._filter_alarm = self.unit.get_filter_alarm_status()
-        self._current_operation = self.unit.get_operation()
+        self._target_temperature = self._ste_data.api.get_target_temp()
+        self._current_temperature = self._ste_data.api.get_current_temp()
+        self._current_humidity = self._ste_data.api.get_current_humidity()
+        self._filter_alarm = self._ste_data.api.get_filter_alarm_status()
+        self._current_operation = self._ste_data.api.get_operation()
 
         _LOGGER.debug("Update %s, current temp: %s", self._name,
                       self._current_temperature)
@@ -116,7 +82,7 @@ class StiebelEltron(ClimateDevice):
         """Return device specific state attributes."""
         return {
             ATTR_CURRENT_HUMIDITY: self._current_humidity,
-            'filter_alarm':     self._filter_alarm
+            'filter_alarm': self._filter_alarm
         }
 
     @property
@@ -165,7 +131,7 @@ class StiebelEltron(ClimateDevice):
         if kwargs.get(ATTR_TEMPERATURE) is not None:
             self._target_temperature = kwargs.get(ATTR_TEMPERATURE)
         _LOGGER.debug("set_temperature: %s", self._target_temperature)
-        self.unit.set_target_temp(self._target_temperature)
+        self._ste_data.api.set_target_temp(self._target_temperature)
 
     # Handle SUPPORT_OPERATION_MODE
     @property
@@ -184,4 +150,4 @@ class StiebelEltron(ClimateDevice):
         _LOGGER.debug("set_operation_mode: %s -> %s", self._current_operation,
                       new_mode)
         self._current_operation = new_mode
-        self.unit.set_operation(new_mode)
+        self._ste_data.api.set_operation(new_mode)
