f0d321e68ddaea10c68be5ade3dd20703fa7a759
Jason Hu <awaregit@gmail.com>
2019-03-21 00:10:10 -0700

    Add aws component

---
+++ homeassistant/components/aws/const.py
@@ -0,0 +1,11 @@
+"""Constant for AWS component."""
+DOMAIN = "aws"
+DATA_KEY = DOMAIN
+DATA_CONFIG = "aws_config"
+DATA_SESSIONS = "aws_sessions"
+
+CONF_REGION = "region_name"
+CONF_ACCESS_KEY_ID = "aws_access_key_id"
+CONF_SECRET_ACCESS_KEY = "aws_secret_access_key"
+CONF_PROFILE_NAME = "profile_name"
+CONF_CREDENTIAL_NAME = "credential_name"
