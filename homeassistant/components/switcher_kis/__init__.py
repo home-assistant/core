fbbbcedbec293aef2cd11b90fab650c9f2783f2d
TomerFi <tomer.figenblat@gmail.com>
2019-03-23 13:48:40 +0200

    Added component named switcher_kis switcher water heater integration.

---
+++ homeassistant/components/switcher_kis/__init__.py
@@ -0,0 +1,105 @@
+"""Home Assistant Switcher Component."""
+
+from asyncio import TimeoutError as Asyncio_TimeoutError, wait_for
+from datetime import datetime
+from logging import getLogger
+from typing import Dict, Optional
+
+import voluptuous as vol
+
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
+from homeassistant.const import CONF_ICON, CONF_NAME, EVENT_HOMEASSISTANT_STOP
+from homeassistant.core import Event, HomeAssistant
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.discovery import async_load_platform
+from homeassistant.helpers.event import async_call_later
+
+_LOGGER = getLogger(__name__)
+
+REQUIREMENTS = ['aioswitcher==2019.3.21']
+
+DOMAIN = 'switcher_kis'
+
+ENTITY_ID_FORMAT = DOMAIN + '_{}'
+
+CONF_DEVICE_ID = 'device_id'
+CONF_DEVICE_PASSWORD = 'device_password'
+CONF_PHONE_ID = 'phone_id'
+
+DEFAULT_NAME = 'boiler'
+
+DISCOVERY_CONFIG = 'config'
+DISCOVERY_DEVICE = 'device'
+
+EVENT_SWITCHER_DEVICE_UPDATED = 'switcher_device_update'
+UPDATED_DEVICE = 'updated_device'
+
+ATTR_AUTO_OFF_SET = 'auto_off_set'
+ATTR_DEVICE_NAME = 'device_name'
+ATTR_ELECTRIC_CURRNET = 'electric_current'
+ATTR_IP_ADDRESS = 'ip_address'
+ATTR_LAST_DATA_UPDATE = 'last_data_update'
+ATTR_LAST_STATE_CHANGE = 'last_state_change'
+ATTR_REMAINING_TIME = 'remaining_time'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_PHONE_ID): cv.string,
+        vol.Required(CONF_DEVICE_ID): cv.string,
+        vol.Required(CONF_DEVICE_PASSWORD): cv.string,
+        vol.Optional(CONF_NAME,
+                     default=DEFAULT_NAME): cv.slugify,
+        vol.Optional(CONF_ICON): cv.icon
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+
+async def async_setup(hass: HomeAssistant, config: Dict) -> bool:
+    """Set up the switcher component."""
+    from aioswitcher.bridge import SwitcherV2Bridge
+
+    phone_id = config[DOMAIN][CONF_PHONE_ID]
+    device_id = config[DOMAIN][CONF_DEVICE_ID]
+    device_password = config[DOMAIN][CONF_DEVICE_PASSWORD]
+
+    v2bridge = SwitcherV2Bridge(
+        hass.loop, phone_id, device_id, device_password)
+
+    await v2bridge.start()
+
+    async def async_stop_bridge(event: Event) -> None:
+        """On homeassistant stop, gracefully stop the bridge if running."""
+        await v2bridge.stop()
+        return None
+
+    hass.async_add_job(hass.bus.async_listen_once(
+        EVENT_HOMEASSISTANT_STOP, async_stop_bridge))
+
+    try:
+        device_data = await wait_for(
+            v2bridge.queue.get(), timeout=5.0, loop=hass.loop)
+    except (Asyncio_TimeoutError, RuntimeError):
+        _LOGGER.exception("failed to get response from device")
+        return False
+
+    hass.data[DOMAIN] = {
+        DISCOVERY_CONFIG: config[DOMAIN],
+        DISCOVERY_DEVICE: device_data
+    }
+
+    await hass.async_create_task(async_load_platform(
+        hass, SWITCH_DOMAIN, DOMAIN, None, config))
+
+    async def device_updates(timestamp: Optional[datetime]) -> None:
+        """Use for updating the device data from the queue."""
+        if v2bridge.running:
+            device_new_data = await v2bridge.queue.get()
+            if device_new_data:
+                hass.bus.async_fire(EVENT_SWITCHER_DEVICE_UPDATED,
+                                    {UPDATED_DEVICE: device_new_data})
+            async_call_later(hass, 3, device_updates)
+        return None
+
+    async_call_later(hass, 3, device_updates)
+
+    return True
---
+++ homeassistant/components/switcher_kis/switch.py
@@ -0,0 +1,230 @@
+"""Home Assistant Switcher Component Switch platform."""
+
+from datetime import datetime
+from logging import getLogger
+from typing import Callable, cast, Dict, Optional
+
+from homeassistant.components.switch import (
+    ATTR_CURRENT_POWER_W, ENTITY_ID_FORMAT as SWITCH_FORMAT, SwitchDevice)
+from homeassistant.const import CONF_ICON, CONF_NAME
+from homeassistant.core import HomeAssistant, Event
+from homeassistant.exceptions import PlatformNotReady
+
+from . import (
+    ATTR_AUTO_OFF_SET, ATTR_DEVICE_NAME, ATTR_ELECTRIC_CURRNET,
+    ATTR_IP_ADDRESS, ATTR_LAST_DATA_UPDATE, ATTR_LAST_STATE_CHANGE,
+    ATTR_REMAINING_TIME, DISCOVERY_CONFIG, DISCOVERY_DEVICE, DOMAIN,
+    ENTITY_ID_FORMAT as SWITCHER_KIS_FORMAT, EVENT_SWITCHER_DEVICE_UPDATED,
+    UPDATED_DEVICE)
+
+_LOGGER = getLogger(__name__)
+
+DEPENDENCIES = ['switcher_kis']
+
+ENTITY_ID_FORMAT = SWITCH_FORMAT.format(SWITCHER_KIS_FORMAT)
+
+PROPERTIES_TO_ATTRIBUTES = {
+    'current_power_w': ATTR_CURRENT_POWER_W,
+    'device_ip_addr': ATTR_IP_ADDRESS,
+    'electric_current': ATTR_ELECTRIC_CURRNET,
+    'remaining_time': ATTR_REMAINING_TIME,
+    'auto_off_set': ATTR_AUTO_OFF_SET,
+    'last_data_update': ATTR_LAST_DATA_UPDATE,
+    'last_state_change': ATTR_LAST_STATE_CHANGE,
+    'device_name': ATTR_DEVICE_NAME
+}
+
+
+async def async_setup_platform(hass: HomeAssistant, config: Dict,
+                               async_add_entities: Callable,
+                               discovery_info: Dict) -> None:
+    """Set up the switcher platform for the switch component."""
+    if hass.data[DOMAIN][DISCOVERY_CONFIG]:
+        name = hass.data[DOMAIN][DISCOVERY_CONFIG][CONF_NAME].title()
+        icon = hass.data[DOMAIN][DISCOVERY_CONFIG].get(CONF_ICON)
+    else:
+        raise PlatformNotReady("No config data found")
+
+    if hass.data[DOMAIN].get(DISCOVERY_DEVICE):
+        switcher_entity = SwitcherControl(
+            hass, name, icon, hass.data[DOMAIN].get(DISCOVERY_DEVICE))
+    else:
+        raise PlatformNotReady("No device data discoverd")
+
+    async_add_entities([switcher_entity], False)
+
+    return None
+
+
+class SwitcherControl(SwitchDevice):
+    """Home Assistant switch entity."""
+
+    from aioswitcher.devices import SwitcherV2Device
+
+    def __init__(self, hass: HomeAssistant, name: str, icon: Optional[str],
+                 device_data: SwitcherV2Device) -> None:
+        """Initialize the entity."""
+        self._hass = hass
+        self._name = name
+        self._icon = icon
+        self._self_initiated = False
+
+        self._device_data = device_data
+        self._state = device_data.state
+        hass.bus.async_listen(EVENT_SWITCHER_DEVICE_UPDATED,
+                              self.async_update_data)
+
+    @property
+    def device_ip_addr(self) -> str:
+        """Return the device's ip address."""
+        return cast(str, self._device_data.ip_addr)
+
+    @property
+    def electric_current(self) -> float:
+        """Return the electric current."""
+        return cast(float, self._device_data.electric_current)
+
+    @property
+    def remaining_time(self) -> Optional[str]:
+        """Return the remaining time to off command."""
+        return self._device_data.remaining_time \
+            if isinstance(self._device_data.remaining_time, str) \
+            else None
+
+    @property
+    def auto_off_set(self) -> str:
+        """Return the auto off configuration set."""
+        return cast(str, self._device_data.auto_off_set)
+
+    @property
+    def last_data_update(self) -> datetime:
+        """Return the datetime for the last update received by the device."""
+        return cast(datetime, self._device_data.last_data_update)
+
+    @property
+    def last_state_change(self) -> datetime:
+        """Return the datetime for the last state change."""
+        return cast(datetime, self._device_data.last_state_change)
+
+    @property
+    def device_name(self) -> str:
+        """Return the device's name."""
+        return cast(str, self._device_data.name)
+
+    @property
+    def should_poll(self) -> bool:
+        """Return False, entity pushes its state to HA."""
+        return False
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique ID."""
+        return "{}-{}".format(
+            self._device_data.device_id, self._device_data.mac_addr)
+
+    @property
+    def name(self) -> str:
+        """Return the name of the entity."""
+        return self._name
+
+    @property
+    def is_on(self) -> bool:
+        """Return True if entity is on."""
+        from aioswitcher.consts import STATE_ON as SWITCHER_STATE_ON
+        return cast(bool, self._state == SWITCHER_STATE_ON)
+
+    @property
+    def current_power_w(self) -> int:
+        """Return the current power usage in W."""
+        return cast(int, self._device_data.power_consumption)
+
+    @property
+    def device_state_attributes(self) -> Dict:
+        """Return the optional state attributes."""
+        from aioswitcher.consts import WAITING_TEXT
+
+        attribs = {}
+
+        for prop, attr in PROPERTIES_TO_ATTRIBUTES.items():
+            value = getattr(self, prop)
+            if value and value is not WAITING_TEXT:
+                attribs[attr] = value
+
+        return attribs
+
+    @property
+    def icon(self) -> Optional[str]:
+        """Return the icon to use in the frontend, if any."""
+        return self._icon
+
+    @property
+    def available(self) -> bool:
+        """Return True if entity is available."""
+        from aioswitcher.consts import (STATE_OFF as SWITCHER_STATE_OFF,
+                                        STATE_ON as SWITCHER_STATE_ON)
+        return self._state in [SWITCHER_STATE_ON, SWITCHER_STATE_OFF]
+
+    async def async_update_data(self, event: Event) -> None:
+        """Update the entity data."""
+        device_data = event.data.get(UPDATED_DEVICE)
+        if device_data:
+            if self._self_initiated:
+                self._self_initiated = False
+            else:
+                self._device_data = device_data
+                self._state = self._device_data.state
+                self.async_schedule_update_ha_state()
+
+        return None
+
+    async def async_turn_on(self, **kwargs: Dict) -> None:
+        """Turn the entity on.
+
+        This method must be run in the event loop and returns a coroutine.
+        """
+        from aioswitcher.api import SwitcherV2Api
+        # pylint: disable=unused-import
+        from aioswitcher.api.messages import (  # noqa F401
+            SwitcherV2ControlResponseMSG)
+        # pylint: enable=unused-import
+        from aioswitcher.consts import (COMMAND_ON,
+                                        STATE_ON as SWITCHER_STATE_ON)
+        response = None  # type: SwitcherV2ControlResponseMSG
+        async with SwitcherV2Api(
+                self._hass.loop, self.device_ip_addr,
+                self._device_data.phone_id, self._device_data.device_id,
+                self._device_data.device_password) as swapi:
+            response = await swapi.control_device(COMMAND_ON)
+
+        if response and response.successful:
+            self._self_initiated = True
+            self._state = SWITCHER_STATE_ON
+            self.async_schedule_update_ha_state()
+
+        return None
+
+    async def async_turn_off(self, **kwargs: Dict) -> None:
+        """Turn the entity off.
+
+        This method must be run in the event loop and returns a coroutine.
+        """
+        from aioswitcher.api import SwitcherV2Api
+        # pylint: disable=unused-import
+        from aioswitcher.api.messages import (  # noqa F401
+            SwitcherV2ControlResponseMSG)
+        # pylint: enable=unused-import
+        from aioswitcher.consts import (COMMAND_OFF,
+                                        STATE_OFF as SWITCHER_STATE_OFF)
+        response = None  # type: SwitcherV2ControlResponseMSG
+        async with SwitcherV2Api(
+                self._hass.loop, self.device_ip_addr,
+                self._device_data.phone_id, self._device_data.device_id,
+                self._device_data.device_password) as swapi:
+            response = await swapi.control_device(COMMAND_OFF)
+
+        if response and response.successful:
+            self._self_initiated = True
+            self._state = SWITCHER_STATE_OFF
+            self.async_schedule_update_ha_state()
+
+        return None
