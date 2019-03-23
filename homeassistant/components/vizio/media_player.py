de734a74fa3232f74b91693326c34a3389368969
Raman Gupta <7243222+raman325@users.noreply.github.com>
2019-03-22 21:40:01 -0400

    Added check for auth token not being set for tv device_class

--- homeassistant/components/vizio/media_player.py
+++ homeassistant/components/vizio/media_player.py
@@ -94,6 +94,12 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         _LOGGER.error("Failed to set up Vizio platform, "
                       "please check if host and API key are correct")
         return
+    elif (token is None or token == "") and device_type == "tv":
+        _LOGGER.error("Failed to set up Vizio platform, "
+                      "if device_class is 'tv' then an auth_token needs "
+                      "to be provided, otherwise if device_class is 'soundbar' "
+                      "then add the right device_class to config")
+        return
 
     if config.get(CONF_SUPPRESS_WARNING):
         from requests.packages import urllib3
