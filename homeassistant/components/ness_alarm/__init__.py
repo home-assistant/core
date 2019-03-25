c6143bf858d523e80d99c6a367b8f3a569289e41
Nick Whyte <nick@nickwhyte.com>
2019-03-25 22:29:43 +1100

    Add infer_arming_state option to ness alarm

--- homeassistant/components/ness_alarm/__init__.py
+++ homeassistant/components/ness_alarm/__init__.py
@@ -13,7 +13,7 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 
-REQUIREMENTS = ['nessclient==0.9.14']
+REQUIREMENTS = ['nessclient==0.9.15']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -22,6 +22,7 @@ DATA_NESS = 'ness_alarm'
 
 CONF_DEVICE_HOST = 'host'
 CONF_DEVICE_PORT = 'port'
+CONF_INFER_ARMING_STATE = 'infer_arming_state'
 CONF_ZONES = 'zones'
 CONF_ZONE_NAME = 'name'
 CONF_ZONE_TYPE = 'type'
@@ -29,6 +30,7 @@ CONF_ZONE_ID = 'id'
 ATTR_OUTPUT_ID = 'output_id'
 DEFAULT_ZONES = []
 DEFAULT_SCAN_INTERVAL = datetime.timedelta(minutes=1)
+DEFAULT_INFER_ARMING_STATE = False
 
 SIGNAL_ZONE_CHANGED = 'ness_alarm.zone_changed'
 SIGNAL_ARMING_STATE_CHANGED = 'ness_alarm.arming_state_changed'
@@ -50,6 +52,9 @@ CONFIG_SCHEMA = vol.Schema({
             vol.All(cv.time_period, cv.positive_timedelta),
         vol.Optional(CONF_ZONES, default=DEFAULT_ZONES):
             vol.All(cv.ensure_list, [ZONE_SCHEMA]),
+        vol.Optional(CONF_INFER_ARMING_STATE,
+                     default=DEFAULT_INFER_ARMING_STATE):
+            cv.boolean
     }),
 }, extra=vol.ALLOW_EXTRA)
 
