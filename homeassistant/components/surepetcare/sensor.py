343f7c6ea6522490bf0d39b78713bf09b3b7e638
Ben Lebherz <git@benleb.de>
2019-03-25 19:58:59 +0100

    update init fuction, small fixes

--- homeassistant/components/surepetcare/sensor.py
+++ homeassistant/components/surepetcare/sensor.py
@@ -8,7 +8,7 @@ from homeassistant.helpers.entity import Entity
 from .const import (BATTERY_DEFAULT_DEVICE_CLASS, BATTERY_DEFAULT_ICON,
                     CONF_HOUSEHOLD_ID, DATA_SURE_PETCARE,
                     SURE_BATT_VOLTAGE_DIFF, SURE_BATT_VOLTAGE_LOW, SURE_IDS,
-                    SureProductID, SureThingType)
+                    SureProductID, SureThingTypeID)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -17,7 +17,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     """Set up Sure PetCare Flaps sensors based on a config entry."""
     hass.data[DATA_SURE_PETCARE][CONF_HOUSEHOLD_ID] = entry.data[
         CONF_HOUSEHOLD_ID]
-    hass.data[DATA_SURE_PETCARE][SureThingType.FLAP.name] = dict()
+    hass.data[DATA_SURE_PETCARE][SureThingTypeID.FLAP.name] = dict()
 
     entities = list()
 
@@ -25,7 +25,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
         sure_id = thing[CONF_ID]
         sure_type = thing[CONF_TYPE]
 
-        if sure_type != SureThingType.FLAP.name:
+        if sure_type != SureThingTypeID.FLAP.name:
             continue
 
         if sure_id not in hass.data[DATA_SURE_PETCARE][sure_type]:
@@ -39,16 +39,19 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class FlapBattery(Entity):
     """Sure Petcare Flap."""
 
-    def __init__(self, _id: int, name: int, hass=None):
+    def __init__(self, _id: int, name: str, hass=None):
+        """Initialize a Sure Petcare Flap battery sensor."""
+        self._id = _id
+        self._hass = hass
+        self._name = f"Flap {name.capitalize()} Battery Level"
+        self._unit_of_measurement = "%"
         self._icon = BATTERY_DEFAULT_ICON
         self._device_class = BATTERY_DEFAULT_DEVICE_CLASS
-        self._id = _id
-        self._name = f"Flap {name.capitalize()} Battery Level"
-        self._hass = hass
+
         self._household_id = hass.data[DATA_SURE_PETCARE][CONF_HOUSEHOLD_ID]
+
         self._state = dict()
-        self._data = hass.data[DATA_SURE_PETCARE][SureThingType.FLAP.name]
-        self._unit_of_measurement = "%"
+        self._data = hass.data[DATA_SURE_PETCARE][SureThingTypeID.FLAP.name]
 
     @property
     def name(self):
