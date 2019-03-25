dfe5cf37d2ce780da1e54e61bf611c676fb13636
Paulus Schoutsen <paulus@home-assistant.io>
2019-03-25 10:04:35 -0700

    Frontend indicate require admin (#22272)
    * Allow panels to indicate they are meant for admins
    
    * Panels to indicate when they require admin access
    
    * Do not return admin-only panels to non-admin users
    
    * Fix flake8

--- tests/components/hassio/test_init.py
+++ tests/components/hassio/test_init.py
@@ -8,6 +8,7 @@ import pytest
 from homeassistant.auth.const import GROUP_ID_ADMIN
 from homeassistant.setup import async_setup_component
 from homeassistant.components.hassio import STORAGE_KEY
+from homeassistant.components import frontend
 
 from tests.common import mock_coro
 
@@ -42,6 +43,28 @@ def test_setup_api_ping(hass, aioclient_mock):
     assert aioclient_mock.call_count == 3
     assert hass.components.hassio.get_homeassistant_version() == "10.0"
     assert hass.components.hassio.is_hassio()
+
+
+async def test_setup_api_panel(hass, aioclient_mock):
+    """Test setup with API ping."""
+    assert await async_setup_component(hass, 'frontend', {})
+    with patch.dict(os.environ, MOCK_ENVIRON):
+        result = await async_setup_component(hass, 'hassio', {})
+        assert result
+
+    panels = hass.data[frontend.DATA_PANELS]
+
+    assert panels.get('hassio').to_response() == {
+        'component_name': 'custom',
+        'icon': 'hass:home-assistant',
+        'title': 'Hass.io',
+        'url_path': 'hassio',
+        'require_admin': True,
+        'config': {'_panel_custom': {'embed_iframe': True,
+                                     'js_url': '/api/hassio/app/entrypoint.js',
+                                     'name': 'hassio-main',
+                                     'trust_external': False}},
+    }
 
 
 @asyncio.coroutine
