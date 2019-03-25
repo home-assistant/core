6996bed8e1b625717e589ce82fdd757431474341
Jason Hu <awarecan@users.noreply.github.com>
2019-03-24 17:58:20 -0700

    Add aws component and consolidate aws notify platform  (#22240)
    * Add aws component
    
    * Move notify config under aws component
    
    * Add basic tests for aws component
    
    * Add deprecated warning for notify.aws_*
    
    * Add more tests

---
+++ homeassistant/components/aws/const.py
@@ -0,0 +1,13 @@
+"""Constant for AWS component."""
+DOMAIN = "aws"
+DATA_KEY = DOMAIN
+DATA_CONFIG = "aws_config"
+DATA_HASS_CONFIG = "aws_hass_config"
+DATA_SESSIONS = "aws_sessions"
+
+CONF_REGION = "region_name"
+CONF_ACCESS_KEY_ID = "aws_access_key_id"
+CONF_SECRET_ACCESS_KEY = "aws_secret_access_key"
+CONF_PROFILE_NAME = "profile_name"
+CONF_CREDENTIAL_NAME = "credential_name"
+CONF_NOTIFY = "notify"
