fa89bb7b06f6daa8c70877caca7f09fb5d8da10b
Leonardo Merza <ljmerza@gmail.com>
2019-03-26 09:59:11 -0400

    Add humanize error for websocket invalid vol error

--- tests/components/websocket_api/test_connection.py
+++
@@ -1,5 +0,0 @@
-"""Test connection of websocket API."""
-
-from homeassistant.components.websocket.connection import ActiveConnection
-
-def test_auth_via_msg(no_auth_websocket_client, legacy_auth):
