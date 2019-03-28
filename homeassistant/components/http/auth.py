928fb1c47510f524b74ca94458dde39b6104dc74
Jason Hu <awarecan@users.noreply.github.com>
2019-03-27 23:13:49 -0700

    Update auth.py

--- homeassistant/components/http/auth.py
+++ homeassistant/components/http/auth.py
@@ -192,7 +192,7 @@ def setup_auth(hass, app):
               await async_validate_trusted_networks(request)):
             _LOGGER.warning(
                 'Using trusted networks to bypass authentication is going to'
-                ' deprecate after 0.95 release. You need to use a bearer token'
+                ' removed after 0.95 release. You need to use a bearer token'
                 ' to access %s from %s',
                 request.path, request[KEY_REAL_IP])
             authenticated = True
