23f75f5f3447f1f2695064feed3a4d99c1b77696
Reinder Brouwer <reinder@brouwer.pro>
2019-03-28 17:11:27 +0400

    fixes an error with certain versions of Python

--- homeassistant/util/__init__.py
+++ homeassistant/util/__init__.py
@@ -87,7 +87,7 @@ def interface_ip(interface):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(
         fcntl.ioctl(
-            sock.fileno(), 0x8915, struct.pack('256s', interface[:15])
+            sock.fileno(), 0x8915, struct.pack('256s', interface[:15].encode('utf-8'))
         )[20:24]
     )
     # Explanation:
