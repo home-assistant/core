821a90fa5482638fba973f249816e2bf11a94f0f
Jason Hu <awarecan@users.noreply.github.com>
2019-03-28 14:37:44 -0700

    Remove botocore dependency from credstash script (#22511)
    * Remove botocore dependency from credstash script
    
    * Update requirements_all.txt
    
    * Update pylintrc
    
    * Update credstash.py

--- homeassistant/scripts/credstash.py
+++ homeassistant/scripts/credstash.py
@@ -4,7 +4,7 @@ import getpass
 
 from homeassistant.util.yaml import _SECRET_NAMESPACE
 
-REQUIREMENTS = ['credstash==1.15.0', 'botocore==1.7.34']
+REQUIREMENTS = ['credstash==1.15.0']
 
 
 def run(args):
@@ -24,16 +24,15 @@ def run(args):
         'value', help="The value to save when putting a secret",
         nargs='?', default=None)
 
-    # pylint: disable=import-error, no-member
+    # pylint: disable=no-member
     import credstash
-    import botocore
 
     args = parser.parse_args(args)
     table = _SECRET_NAMESPACE
 
     try:
         credstash.listSecrets(table=table)
-    except botocore.errorfactory.ClientError:
+    except Exception:  # pylint: disable=broad-except
         credstash.createDdbTable(table=table)
 
     if args.action == 'list':
