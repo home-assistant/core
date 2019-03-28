3c2649358dbf05541de1f292b376dde28deabbb2
Pascal Vizeli <pvizeli@syshack.ch>
2019-03-28 18:16:47 +0100

    Fix API stream of snapshot / Add ingress

--- homeassistant/components/hassio/http.py
+++ homeassistant/components/hassio/http.py
@@ -3,6 +3,7 @@ import asyncio
 import logging
 import os
 import re
+from typing import Dict, Union
 
 import aiohttp
 from aiohttp import web
@@ -20,7 +21,8 @@ _LOGGER = logging.getLogger(__name__)
 NO_TIMEOUT = re.compile(
     r'^(?:'
     r'|homeassistant/update'
-    r'|host/update'
+    r'|hassos/update'
+    r'|hassos/update/cli'
     r'|supervisor/update'
     r'|addons/[^/]+/(?:update|install|rebuild)'
     r'|snapshots/.+/full'
@@ -36,6 +38,12 @@ NO_AUTH = re.compile(
     r')$'
 )
 
+STREAM_RESPONSE = re.compile(
+    r'^(?:'
+    r'|snapshots/[^/]+/(?:upload|download)'
+    r')$'
+)
+
 
 class HassIOView(HomeAssistantView):
     """Hass.io view to handle base part."""
@@ -44,25 +52,29 @@ class HassIOView(HomeAssistantView):
     url = "/api/hassio/{path:.+}"
     requires_auth = False
 
-    def __init__(self, host, websession):
+    def __init__(self, host: str, websession: aiohttp.ClientSession):
         """Initialize a Hass.io base view."""
         self._host = host
         self._websession = websession
 
-    async def _handle(self, request, path):
+    async def _handle(
+            self, request: web.Request, path: str
+    ) -> Union[web.Response, web.StreamResponse]:
         """Route data to Hass.io."""
         if _need_auth(path) and not request[KEY_AUTHENTICATED]:
             return web.Response(status=401)
 
-        client = await self._command_proxy(path, request)
-
-        data = await client.read()
-        return _create_response(client, data)
+        # Prepare the right proxy
+        if _is_stream(path):
+            return await self._stream_proxy(path, request)
+        return await self._command_proxy(path, request)
 
     get = _handle
     post = _handle
 
-    async def _command_proxy(self, path, request):
+    async def _command_proxy(
+            self, path: str, request: web.Request
+    ) -> web.Response:
         """Return a client request with proxy origin for Hass.io supervisor.
 
         This method is a coroutine.
@@ -71,13 +83,7 @@ class HassIOView(HomeAssistantView):
         hass = request.app['hass']
 
         data = None
-        headers = {
-            X_HASSIO: os.environ.get('HASSIO_TOKEN', ""),
-        }
-        user = request.get('hass_user')
-        if user is not None:
-            headers[X_HASS_USER_ID] = request['hass_user'].id
-            headers[X_HASS_IS_ADMIN] = str(int(request['hass_user'].is_admin))
+        headers = _init_header(request)
 
         try:
             with async_timeout.timeout(10, loop=hass.loop):
@@ -93,7 +99,52 @@ class HassIOView(HomeAssistantView):
                 headers=headers, timeout=read_timeout
             )
 
-            return client
+            data = await client.read()
+            return web.Response(
+                body=data,
+                status=client.status,
+                content_type=client.content_type
+            )
+
+        except aiohttp.ClientError as err:
+            _LOGGER.error("Client error on api %s request %s", path, err)
+
+        except asyncio.TimeoutError:
+            _LOGGER.error("Client timeout error on API request %s", path)
+
+        raise HTTPBadGateway()
+
+    async def _stream_proxy(
+            self, path: str, request: web.Request
+    ) -> web.StreamResponse:
+        """Return a client stream with proxy origin for Hass.io supervisor.
+
+        This method is a coroutine.
+        """
+        read_timeout = _get_timeout(path)
+        hass = request.app['hass']
+
+        data = None
+        headers = _init_header(request)
+
+        try:
+            method = getattr(self._websession, request.method.lower())
+            client = await method(
+                "http://{}/{}".format(self._host, path), data=data,
+                headers=headers, timeout=read_timeout
+            )
+
+            response = web.StreamResponse()
+            response.content_type = request.content_type
+            try:
+                await response.prepare(request)
+                async for data in client.content:
+                    await response.write(data)
+
+            except (aiohttp.ClientError, aiohttp.ClientPayloadError):
+                pass
+
+            return response
 
         except aiohttp.ClientError as err:
             _LOGGER.error("Client error on api %s request %s", path, err)
@@ -104,24 +155,38 @@ class HassIOView(HomeAssistantView):
         raise HTTPBadGateway()
 
 
-def _create_response(client, data):
-    """Convert a response from client request."""
-    return web.Response(
-        body=data,
-        status=client.status,
-        content_type=client.content_type,
-    )
+def _init_header(request: web.Request) -> Dict[str, str]:
+    """Create initial header."""
+    headers = {
+        X_HASSIO: os.environ.get('HASSIO_TOKEN', ""),
+        CONTENT_TYPE: request.content_type,
+    }
+
+    # Add user data
+    user = request.get('hass_user')
+    if user is not None:
+        headers[X_HASS_USER_ID] = request['hass_user'].id
+        headers[X_HASS_IS_ADMIN] = str(int(request['hass_user'].is_admin))
+
+    return headers
 
 
-def _get_timeout(path):
+def _get_timeout(path: str) -> int:
     """Return timeout for a URL path."""
     if NO_TIMEOUT.match(path):
         return 0
     return 300
 
 
-def _need_auth(path):
+def _need_auth(path: str) -> bool:
     """Return if a path need authentication."""
     if NO_AUTH.match(path):
         return False
     return True
+
+
+def _is_stream(path: str) -> bool:
+    """Return if a path need stream response."""
+    if STREAM_RESPONSE.match(path):
+        return True
+    return False
---
+++ homeassistant/components/hassio/ingress.py
@@ -0,0 +1,127 @@
+"""Hass.io Add-on ingress service."""
+import asyncio
+from typing import Dict, Union
+
+import aiohttp
+from aiohttp import web
+
+from homeassistant.components.http import HomeAssistantView
+
+
+class HassIOIngressView(HomeAssistantView):
+    """Hass.io view to handle base part."""
+
+    name = "api:hassio:ingress"
+    url = "/api/hassio_ingress/{addon}/{path:.+}"
+    requires_auth = False
+
+    def __init__(self, host: str, websession: aiohttp.ClientSession):
+        """Initialize a Hass.io ingress view."""
+        self._host = host
+        self._connector = websession.connector
+
+    def _create_url(self, addon: str, path: str) -> str:
+        """Create URL to service."""
+        return "http://{}/addons/{}/web/{}".format(self._host, addon, path)
+
+    async def _handle(
+            self, request: web.Request, addon: str, path: str
+    ) -> Union[web.Response, web.StreamResponse, web.WebSocketResponse]:
+        """Route data to Hass.io ingress service."""
+        header = request.headers
+
+        # Create websession and aims cookies
+        client = aiohttp.ClientSession(
+            cookies=request.cookies, connector=self._connector
+        )
+
+        try:
+            # Websocket
+            if header["connection"] == "Upgrade" and\
+                    header["upgrade"] == "websocket":
+                return await self._handle_websocket(
+                    client, request, addon, path
+                )
+
+            # Stream
+            if header["transfer-encoding"] == "chunked":
+                return await self._handle_stream(client, request, addon, path)
+
+            # Request
+            return await self._handle_request(client, request, addon, path)
+
+        finally:
+            client.detach()
+
+    get = _handle
+    post = _handle
+    put = _handle
+    delete = _handle
+
+    async def _handle_websocket(
+            self, client: aiohttp.ClientSession,
+            request: web.Request, addon: str, path: str
+    ) -> web.WebSocketResponse:
+        """Ingress route for websocket."""
+        ws_server = web.WebSocketResponse()
+        await ws_server.prepare(request)
+
+        # Start proxy
+        url = self._create_url(addon, path)
+        async with client.ws_connect(url) as ws_client:
+            # Proxy requests
+            await asyncio.wait(
+                [
+                    _websocket_forward(ws_server, ws_client),
+                    _websocket_forward(ws_client, ws_server),
+                ],
+                return_when=asyncio.FIRST_COMPLETED
+            )
+
+        return ws_server
+
+    async def _handle_stream(
+            self, client: aiohttp.ClientSession,
+            request: web.Request, addon: str, path: str
+    ) -> web.StreamResponse:
+        """Ingress route for stream."""
+
+    async def _handle_request(
+            self, client: aiohttp.ClientSession,
+            request: web.Request, addon: str, path: str
+    ) -> web.Response:
+        """Ingress route for request."""
+
+        url = self._create_url(addon, path)
+        data = request.read()
+        header = request.headers.copy()
+
+        async with client.request(
+            request.method, url, headers=header, data=data
+        ) as result:
+            headers = result.headers.copy()
+            del headers['content-length']
+
+            body = await result.read()
+
+            # Return Response
+            return web.Response(
+                headers=headers,
+                status=result.status,
+                body=body
+            )
+
+
+async def _websocket_forward(ws_from, ws_to):
+    """Handle websocket message directly."""
+    async for msg in ws_from:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            await ws_to.send_str(msg.data)
+        elif msg.type == aiohttp.WSMsgType.BINARY:
+            await ws_to.send_bytes(msg.data)
+        elif msg.type == aiohttp.WSMsgType.PING:
+            await ws_to.ping()
+        elif msg.type == aiohttp.WSMsgType.PONG:
+            await ws_to.pong()
+        elif ws_to.closed:
+            await ws_to.close(code=ws_to.close_code, message=msg.extra)
