0d85a82864ac070232d1d5054348f78da3eacc46
David Bonnes <david@bonnes.me>
2019-03-29 18:50:30 +0000

    delint unused import

--- homeassistant/components/honeywell/climate.py
+++ homeassistant/components/honeywell/climate.py
@@ -5,7 +5,6 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/climate.honeywell/
 """
 import logging
-import socket
 import datetime
 
 import requests
