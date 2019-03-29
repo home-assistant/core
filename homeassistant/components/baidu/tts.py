7720f09923d912eb9389b7d126b6b2baa547d68c
Jason Hu <awarecan@users.noreply.github.com>
2019-03-29 11:45:02 -0700

    Fix tts Great Migration issue (#22539)

--- homeassistant/components/baidu/tts.py
+++ homeassistant/components/baidu/tts.py
@@ -9,10 +9,9 @@ import logging
 
 import voluptuous as vol
 
+from homeassistant.components.tts import CONF_LANG, PLATFORM_SCHEMA, Provider
 from homeassistant.const import CONF_API_KEY
 import homeassistant.helpers.config_validation as cv
-
-from . import CONF_LANG, PLATFORM_SCHEMA, Provider
 
 REQUIREMENTS = ["baidu-aip==1.6.6"]
 
