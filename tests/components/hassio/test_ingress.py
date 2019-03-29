c226d33b40824de4764dc93ce33f0e5442f692c2
Pascal Vizeli <pvizeli@syshack.ch>
2019-03-29 23:38:23 +0100

    Add more tests

--- tests/components/hassio/test_ingress.py
+++ tests/components/hassio/test_ingress.py
@@ -31,3 +31,90 @@ async def test_ingress_request_get(
     assert len(aioclient_mock.mock_calls) == 1
     assert aioclient_mock.mock_calls[-1][3]["X-HASSIO-KEY"] == "123456"
     assert aioclient_mock.mock_calls[-1][3]["x-test-header"] == "beer"
+
+
+@pytest.mark.parametrize(
+    'build_type', [
+        ("a3_vl", "test/beer/ping?index=1"), ("core", "index.html"),
+        ("local", "panel/config"), ("jk_921", "editor.php?idx=3&ping=5")
+    ])
+async def test_ingress_request_post(
+        hassio_client, build_type, aioclient_mock):
+    """Test no auth needed for ."""
+    aioclient_mock.post(
+        "http://127.0.0.1/addons/{}/web/{}".format(build_type[0], build_type[1]),
+        text="test"
+    )
+
+    resp = await hassio_client.post(
+        '/api/hassio_ingress/{}/{}'.format(build_type[0], build_type[1]),
+        headers={"x-test-header": "beer"}
+    )
+
+    # Check we got right response
+    assert resp.status == 200
+    body = await resp.text()
+    assert body == "test"
+
+    # Check we forwarded command
+    assert len(aioclient_mock.mock_calls) == 1
+    assert aioclient_mock.mock_calls[-1][3]["X-HASSIO-KEY"] == "123456"
+    assert aioclient_mock.mock_calls[-1][3]["x-test-header"] == "beer"
+
+
+@pytest.mark.parametrize(
+    'build_type', [
+        ("a3_vl", "test/beer/ping?index=1"), ("core", "index.html"),
+        ("local", "panel/config"), ("jk_921", "editor.php?idx=3&ping=5")
+    ])
+async def test_ingress_request_put(
+        hassio_client, build_type, aioclient_mock):
+    """Test no auth needed for ."""
+    aioclient_mock.put(
+        "http://127.0.0.1/addons/{}/web/{}".format(build_type[0], build_type[1]),
+        text="test"
+    )
+
+    resp = await hassio_client.put(
+        '/api/hassio_ingress/{}/{}'.format(build_type[0], build_type[1]),
+        headers={"x-test-header": "beer"}
+    )
+
+    # Check we got right response
+    assert resp.status == 200
+    body = await resp.text()
+    assert body == "test"
+
+    # Check we forwarded command
+    assert len(aioclient_mock.mock_calls) == 1
+    assert aioclient_mock.mock_calls[-1][3]["X-HASSIO-KEY"] == "123456"
+    assert aioclient_mock.mock_calls[-1][3]["x-test-header"] == "beer"
+
+
+@pytest.mark.parametrize(
+    'build_type', [
+        ("a3_vl", "test/beer/ping?index=1"), ("core", "index.html"),
+        ("local", "panel/config"), ("jk_921", "editor.php?idx=3&ping=5")
+    ])
+async def test_ingress_request_delete(
+        hassio_client, build_type, aioclient_mock):
+    """Test no auth needed for ."""
+    aioclient_mock.delete(
+        "http://127.0.0.1/addons/{}/web/{}".format(build_type[0], build_type[1]),
+        text="test"
+    )
+
+    resp = await hassio_client.delete(
+        '/api/hassio_ingress/{}/{}'.format(build_type[0], build_type[1]),
+        headers={"x-test-header": "beer"}
+    )
+
+    # Check we got right response
+    assert resp.status == 200
+    body = await resp.text()
+    assert body == "test"
+
+    # Check we forwarded command
+    assert len(aioclient_mock.mock_calls) == 1
+    assert aioclient_mock.mock_calls[-1][3]["X-HASSIO-KEY"] == "123456"
+    assert aioclient_mock.mock_calls[-1][3]["x-test-header"] == "beer"
