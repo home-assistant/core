f908239670724e5446141e3aa86c7db0e8f29741
Kyle Niewiada <kniewia@gmail.com>
2019-03-30 05:32:16 -0400

    modify test to handle exception

--- tests/components/mqtt/test_init.py
+++ tests/components/mqtt/test_init.py
@@ -12,6 +12,7 @@ from homeassistant.const import (
     ATTR_DOMAIN, ATTR_SERVICE, EVENT_CALL_SERVICE, EVENT_HOMEASSISTANT_STOP)
 from homeassistant.core import callback
 from homeassistant.setup import async_setup_component
+from homeassistant.exceptions import ConfigEntryNotReady
 
 from tests.common import (
     MockConfigEntry, async_fire_mqtt_message, async_mock_mqtt_component,
@@ -610,7 +611,7 @@ def test_setup_embedded_with_embedded(hass):
         assert _start.call_count == 1
 
 
-async def test_setup_fails_if_no_connect_broker(hass):
+async def test_setup_throws_ConfigEntryNotReady_if_no_connect_broker(hass):
     """Test for setup failure if connection to broker is missing."""
     entry = MockConfigEntry(domain=mqtt.DOMAIN, data={
         mqtt.CONF_BROKER: 'test-broker'
@@ -618,7 +619,12 @@ async def test_setup_fails_if_no_connect_broker(hass):
 
     with mock.patch('paho.mqtt.client.Client') as mock_client:
         mock_client().connect = lambda *args: 1
-        assert not await mqtt.async_setup_entry(hass, entry)
+        try:
+            await mqtt.async_setup_entry(hass, entry)
+        except ConfigEntryNotReady:
+            pass
+        finally:
+            assert False, 'ConfigEntryNotReady not raised'
 
 
 async def test_setup_uses_certificate_on_certificate_set_to_auto(
