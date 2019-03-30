5bf05dcd926d76cb2721c53cba9a3fe58fee55b4
jamiewalters <linuxjamie@gmail.com>
2019-03-30 06:41:23 +0100

    Update climate.py

--- homeassistant/components/verisure/climate.py
+++ homeassistant/components/verisure/climate.py
@@ -6,8 +6,7 @@ from homeassistant.components.climate.const import (
     SUPPORT_FAN_MODE, SUPPORT_OPERATION_MODE,
     SUPPORT_SWING_MODE, SUPPORT_ON_OFF)
 from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE
-from homeassistant.components.verisure import HUB as hub
-from homeassistant.components.verisure import CONF_CLIMATE
+from homeassistant.components.verisure import CONF_CLIMATE, HUB as hub
 
 _LOGGER = logging.getLogger(__name__)
 HEAT_PUMPS = None
