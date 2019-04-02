home-assistant/core

commit 091c4f20a3df244a52cf036a72fe4a1a1bfc23d7

Remove get_serial helper that is no longer needed. (#22368)

homeassistant/components/homekit_controller/__init__.py
@@ -11,8 +11,7 @@ from homeassistant.helpers.event import call_later
 
 from .connection import get_accessory_information
 from .const import (
-    CONTROLLER, DOMAIN, HOMEKIT_ACCESSORY_DISPATCH, KNOWN_ACCESSORIES,
-    KNOWN_DEVICES
+    CONTROLLER, DOMAIN, HOMEKIT_ACCESSORY_DISPATCH, KNOWN_DEVICES
 )
 
 
@@ -31,25 +30,6 @@ _LOGGER = logging.getLogger(__name__)
 RETRY_INTERVAL = 60  # seconds
 
 PAIRING_FILE = "pairing.json"
-
-
-def get_serial(accessory):
-    """Obtain the serial number of a HomeKit device."""
-    # pylint: disable=import-error
-    from homekit.model.services import ServicesTypes
-    from homekit.model.characteristics import CharacteristicsTypes
-
-    for service in accessory['services']:
-        if ServicesTypes.get_short(service['type']) != \
-           'accessory-information':
-            continue
-        for characteristic in service['characteristics']:
-            ctype = CharacteristicsTypes.get_short(
-                characteristic['type'])
-            if ctype != 'serial-number':
-                continue
-            return characteristic['value']
-    return None
 
 
 def escape_characteristic_name(char_name):
@@ -74,6 +54,10 @@ class HKDevice():
         self.config = config
         self.configurator = hass.components.configurator
         self._connection_warning_logged = False
+
+        # This just tracks aid/iid pairs so we know if a HK service has been
+        # mapped to a HA entity.
+        self.entities = []
 
         self.pairing_lock = asyncio.Lock(loop=hass.loop)
 
@@ -100,15 +84,16 @@ class HKDevice():
                 self.hass, RETRY_INTERVAL, lambda _: self.accessory_setup())
             return
         for accessory in data:
-            serial = get_serial(accessory)
-            if serial in self.hass.data[KNOWN_ACCESSORIES]:
-                continue
-            self.hass.data[KNOWN_ACCESSORIES][serial] = self
             aid = accessory['aid']
             for service in accessory['services']:
+                iid = service['iid']
+                if (aid, iid) in self.entities:
+                    # Don't add the same entity again
+                    continue
+
                 devtype = ServicesTypes.get_short(service['type'])
                 _LOGGER.debug("Found %s", devtype)
-                service_info = {'serial': serial,
+                service_info = {'serial': self.hkid,
                                 'aid': aid,
                                 'iid': service['iid'],
                                 'model': self.model,
@@ -381,7 +366,6 @@ def setup(hass, config):
         device = HKDevice(hass, host, port, model, hkid, config_num, config)
         hass.data[KNOWN_DEVICES][hkid] = device
 
-    hass.data[KNOWN_ACCESSORIES] = {}
     hass.data[KNOWN_DEVICES] = {}
     discovery.listen(hass, SERVICE_HOMEKIT, discovery_dispatch)
     return True

homeassistant/components/homekit_controller/alarm_control_panel.py
@@ -6,7 +6,7 @@ from homeassistant.const import (
     ATTR_BATTERY_LEVEL, STATE_ALARM_ARMED_AWAY, STATE_ALARM_ARMED_HOME,
     STATE_ALARM_ARMED_NIGHT, STATE_ALARM_DISARMED, STATE_ALARM_TRIGGERED)
 
-from . import KNOWN_ACCESSORIES, HomeKitEntity
+from . import KNOWN_DEVICES, HomeKitEntity
 
 DEPENDENCIES = ['homekit_controller']
 
@@ -34,7 +34,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up Homekit Alarm Control Panel support."""
     if discovery_info is None:
         return
-    accessory = hass.data[KNOWN_ACCESSORIES][discovery_info['serial']]
+    accessory = hass.data[KNOWN_DEVICES][discovery_info['serial']]
     add_entities([HomeKitAlarmControlPanel(accessory, discovery_info)],
                  True)
 

homeassistant/components/homekit_controller/binary_sensor.py
@@ -3,7 +3,7 @@ import logging
 
 from homeassistant.components.binary_sensor import BinarySensorDevice
 
-from . import KNOWN_ACCESSORIES, HomeKitEntity
+from . import KNOWN_DEVICES, HomeKitEntity
 
 DEPENDENCIES = ['homekit_controller']
 
@@ -13,7 +13,7 @@ _LOGGER = logging.getLogger(__name__)
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up Homekit motion sensor support."""
     if discovery_info is not None:
-        accessory = hass.data[KNOWN_ACCESSORIES][discovery_info['serial']]
+        accessory = hass.data[KNOWN_DEVICES][discovery_info['serial']]
         add_entities([HomeKitMotionSensor(accessory, discovery_info)], True)
 
 

homeassistant/components/homekit_controller/climate.py
@@ -7,7 +7,7 @@ from homeassistant.components.climate.const import (
     SUPPORT_TARGET_TEMPERATURE)
 from homeassistant.const import ATTR_TEMPERATURE, STATE_OFF, TEMP_CELSIUS
 
-from . import KNOWN_ACCESSORIES, HomeKitEntity
+from . import KNOWN_DEVICES, HomeKitEntity
 
 DEPENDENCIES = ['homekit_controller']
 
@@ -29,7 +29,7 @@ DEFAULT_VALID_MODES = list(MODE_HOMEKIT_TO_HASS)
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up Homekit climate."""
     if discovery_info is not None:
-        accessory = hass.data[KNOWN_ACCESSORIES][discovery_info['serial']]
+        accessory = hass.data[KNOWN_DEVICES][discovery_info['serial']]
         add_entities([HomeKitClimateDevice(accessory, discovery_info)], True)
 
 

homeassistant/components/homekit_controller/const.py
@@ -1,7 +1,6 @@
 """Constants for the homekit_controller component."""
 DOMAIN = 'homekit_controller'
 
-KNOWN_ACCESSORIES = "{}-accessories".format(DOMAIN)
 KNOWN_DEVICES = "{}-devices".format(DOMAIN)
 CONTROLLER = "{}-controller".format(DOMAIN)
 

homeassistant/components/homekit_controller/cover.py
@@ -8,7 +8,7 @@ from homeassistant.components.cover import (
 from homeassistant.const import (
     STATE_CLOSED, STATE_CLOSING, STATE_OPEN, STATE_OPENING)
 
-from . import KNOWN_ACCESSORIES, HomeKitEntity
+from . import KNOWN_DEVICES, HomeKitEntity
 
 STATE_STOPPED = 'stopped'
 
@@ -41,7 +41,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up HomeKit Cover support."""
     if discovery_info is None:
         return
-    accessory = hass.data[KNOWN_ACCESSORIES][discovery_info['serial']]
+    accessory = hass.data[KNOWN_DEVICES][discovery_info['serial']]
 
     if discovery_info['device-type'] == 'garage-door-opener':
         add_entities([HomeKitGarageDoorCover(accessory, discovery_info)],

homeassistant/components/homekit_controller/light.py
@@ -5,7 +5,7 @@ from homeassistant.components.light import (
     ATTR_BRIGHTNESS, ATTR_COLOR_TEMP, ATTR_HS_COLOR, SUPPORT_BRIGHTNESS,
     SUPPORT_COLOR, SUPPORT_COLOR_TEMP, Light)
 
-from . import KNOWN_ACCESSORIES, HomeKitEntity
+from . import KNOWN_DEVICES, HomeKitEntity
 
 DEPENDENCIES = ['homekit_controller']
 
@@ -15,7 +15,7 @@ _LOGGER = logging.getLogger(__name__)
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up Homekit lighting."""
     if discovery_info is not None:
-        accessory = hass.data[KNOWN_ACCESSORIES][discovery_info['serial']]
+        accessory = hass.data[KNOWN_DEVICES][discovery_info['serial']]
         add_entities([HomeKitLight(accessory, discovery_info)], True)
 
 

homeassistant/components/homekit_controller/lock.py
@@ -5,7 +5,7 @@ from homeassistant.components.lock import LockDevice
 from homeassistant.const import (
     ATTR_BATTERY_LEVEL, STATE_LOCKED, STATE_UNLOCKED)
 
-from . import KNOWN_ACCESSORIES, HomeKitEntity
+from . import KNOWN_DEVICES, HomeKitEntity
 
 DEPENDENCIES = ['homekit_controller']
 
@@ -30,7 +30,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up Homekit Lock support."""
     if discovery_info is None:
         return
-    accessory = hass.data[KNOWN_ACCESSORIES][discovery_info['serial']]
+    accessory = hass.data[KNOWN_DEVICES][discovery_info['serial']]
     add_entities([HomeKitLock(accessory, discovery_info)], True)
 
 

commit 11c00e9d14d58ac144ef2d2dcd883c4c84c8f379

Fix racy homekit_controller platform setup caused by #22368

homeassistant/components/homekit_controller/__init__.py
@@ -191,8 +191,7 @@ def setup(hass, config):
             return
 
         _LOGGER.debug('Discovered unique device %s', hkid)
-        device = HKDevice(hass, host, port, model, hkid, config_num, config)
-        hass.data[KNOWN_DEVICES][hkid] = device
+        HKDevice(hass, host, port, model, hkid, config_num, config)
 
     hass.data[KNOWN_DEVICES] = {}
     discovery.listen(hass, SERVICE_HOMEKIT, discovery_dispatch)

homeassistant/components/homekit_controller/connection.py
@@ -7,7 +7,8 @@ from homeassistant.helpers import discovery
 from homeassistant.helpers.event import call_later
 
 from .const import (
-    CONTROLLER, DOMAIN, HOMEKIT_ACCESSORY_DISPATCH, PAIRING_FILE, HOMEKIT_DIR
+    CONTROLLER, DOMAIN, HOMEKIT_ACCESSORY_DISPATCH, KNOWN_DEVICES,
+    PAIRING_FILE, HOMEKIT_DIR
 )
 
 
@@ -75,6 +76,8 @@ class HKDevice():
         self.pairing_lock = asyncio.Lock(loop=hass.loop)
 
         self.pairing = self.controller.pairings.get(hkid)
+
+        hass.data[KNOWN_DEVICES][hkid] = self
 
         if self.pairing is not None:
             self.accessory_setup()