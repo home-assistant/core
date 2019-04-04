309699ca543d277ce0b5a3990d30ce6ece485e05
Paulus Schoutsen <balloob@gmail.com>
2019-04-03 21:29:42 -0700

    Update manifest_helper.py

--- script/manifest/manifest_helper.py
+++ script/manifest/manifest_helper.py
@@ -1,3 +1,4 @@
+"""Helpers to deal with manifests."""
 import json
 import pathlib
 
@@ -6,6 +7,7 @@ component_dir = pathlib.Path('homeassistant/components')
 
 
 def iter_manifests():
+    """Iterate over all available manifests."""
     manifests = [
         json.loads(fil.read_text())
         for fil in component_dir.glob('*/manifest.json')
