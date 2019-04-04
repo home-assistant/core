0aa3db5d0cef960ba51fadaa98c9cb8159a7d12c
Rohan Kapoor <rohan@rohankapoor.com>
2019-04-04 10:09:53 -0700

    Correct path to manifest validate script?

--- script/manifest/requirements.py
+++ script/manifest/requirements.py
@@ -8,14 +8,15 @@ def gather_requirements_from_manifests(process_requirements, errors, reqs):
         if manifest.get('domain') is None:
             errors.append(manifest)
             errors.append(
-                'An invalid manifest exists. Please run script/validate.py'
+                'An invalid manifest exists. Please run'
+                'script/manifest/validate.py'
             )
             continue
 
         if manifest.get('requirements') is None:
             errors.append(
                 'The manifest for component {} is invalid. Please run'
-                'script/validate.py'.format(manifest['domain'])
+                'script/manifest/validate.py'.format(manifest['domain'])
             )
             continue
 
