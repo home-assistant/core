ea98fc6c987ed584bd1b4969b8cccd2adf58e79f
Paul Madden <maddenp@colorado.edu>
2019-04-05 00:14:26 -0600

    Work on PR comments

--- homeassistant/components/bomradarcam/bomradarcam.py
+++ homeassistant/components/bomradarcam/bomradarcam.py
@@ -41,25 +41,24 @@ PLATFORM_SCHEMA = vol.All(PLATFORM_SCHEMA.extend({
 }), _validate_schema)
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up BOM radar-loop camera component."""
     location = config.get(CONF_LOCATION) or "ID {}".format(config.get(CONF_ID))
     name = config.get(CONF_NAME) or "BOM Radar Loop - {}".format(location)
     args = [config.get(x) for x in (CONF_LOCATION, CONF_ID, CONF_DELTA, CONF_FRAMES,
                                     CONF_OUTFILE)]
-    add_devices([BOMRadarCam(hass, name, *args)])
+    add_entities([BOMRadarCam(name, *args)])
 
 
 class BOMRadarCam(Camera):
     """A camera component producing animated BOM radar-imagery GIFs."""
 
-    def __init__(self, hass, name, location, radar_id, delta, frames, outfile):
+    def __init__(self, name, location, radar_id, delta, frames, outfile):
         """Initialize the component."""
         from bomradarloop import BOMRadarLoop
         super().__init__()
         self._name = name
-        self._cam = BOMRadarLoop(location, radar_id, delta, frames, outfile,
-                                 LOGGER)
+        self._cam = BOMRadarLoop(location, radar_id, delta, frames, outfile)
 
     def camera_image(self):
         """Return the current BOM radar-loop image."""
