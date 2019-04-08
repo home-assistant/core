0dce10fed5e56cb4c708366da305c096b130fa0e
araczkowski <araczkowski@gmail.com>
2019-04-08 15:57:40 +0200

    step 2 in adding devices

--- homeassistant/components/ais_dom_device/config_flow.py
+++ homeassistant/components/ais_dom_device/config_flow.py
@@ -3,12 +3,25 @@
 from homeassistant import config_entries
 from homeassistant.core import callback
 from .const import DOMAIN
+import asyncio
+import logging
 G_AUTH_URL = None
+_LOGGER = logging.getLogger(__name__)
 
 
 def setUrl(url):
     global G_AUTH_URL
     G_AUTH_URL = url
+
+
+def check_scanning_resoult(hass, loop) -> int:
+    import time
+    _LOGGER.info('loop: ' + str(loop))
+    iot_dev = hass.states.get('input_select.ais_iot_devices_in_network')
+    iot_dev_to_add = len(iot_dev.attributes['options'])
+    time.sleep(5)
+    return iot_dev_to_add
+
 
 @callback
 def configured_service(hass):
@@ -63,19 +76,27 @@ class AisDomDeviceFlowHandler(config_entries.ConfigFlow):
     async def async_step_three(self, user_input=None):
         """Handle a flow start."""
         errors = {}
-        if user_input is not None:
-            return await self.async_step_init(user_input=None)
-        return self._async_in_progress(
-            step_id='three',
+        for x in range(0, 5):
+            result = await self.hass.async_add_executor_job(check_scanning_resoult, self.hass, x)
+            _LOGGER.info(str(result))
+            if result > 1:
+                return await self.async_step_init(user_input=None)
+            else:
+                errors = {'devices': 'search_failed'}
+
+        user_input['devices'] = ' '
+        return self.async_show_form(
+            step_id='two',
             errors=errors,
-        )
+            data=user_input
+        )   
 
     async def async_step_init(self, user_input=None):
         """Handle a flow start."""
         errors = {}
         if user_input is not None:
             return self.async_create_entry(
-                title="Dostęp do Servisu Asystent Google",
+                title="Dodano nowe urządzenie",
                 data=user_input
             )
 
