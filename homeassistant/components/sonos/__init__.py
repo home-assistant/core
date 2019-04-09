0d4502369cdaf8daade9283d5bb44c9cbc2851a2
Anders Melchiorsen <amelchio@nogoto.net>
2019-04-09 02:32:56 +0200

    Fix Sonos handling of unsupported favorites (#22906)

--- homeassistant/components/sonos/__init__.py
+++ homeassistant/components/sonos/__init__.py
@@ -4,7 +4,7 @@ from homeassistant.helpers import config_entry_flow
 
 
 DOMAIN = 'sonos'
-REQUIREMENTS = ['pysonos==0.0.8']
+REQUIREMENTS = ['pysonos==0.0.9']
 
 
 async def async_setup(hass, config):
