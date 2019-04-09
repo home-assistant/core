58ec77b017b9fa4daee1dcc76135c4f08fa3b310
Anders Melchiorsen <amelchio@nogoto.net>
2019-04-09 20:28:20 +0200

    Binary sensors for netgear_lte (#22902)
    * Binary sensors for netgear_lte
    
    * Move LTEEntity to component
    
    * Revert unrelated manifest changes
    
    * Address review comments
    
    * Remove unused import

--- homeassistant/components/netgear_lte/sensor_types.py
+++ homeassistant/components/netgear_lte/sensor_types.py
@@ -1,5 +1,7 @@
 """Define possible sensor types."""
 
+from homeassistant.components.binary_sensor import DEVICE_CLASS_CONNECTIVITY
+
 SENSOR_SMS = 'sms'
 SENSOR_USAGE = 'usage'
 
@@ -10,17 +12,24 @@ SENSOR_UNITS = {
     'rx_level': 'dBm',
     'tx_level': 'dBm',
     'upstream': None,
-    'wire_connected': None,
-    'mobile_connected': None,
     'connection_text': None,
     'connection_type': None,
     'current_ps_service_type': None,
     'register_network_display': None,
-    'roaming': None,
     'current_band': None,
     'cell_id': None,
 }
 
-ALL = list(SENSOR_UNITS)
+BINARY_SENSOR_MOBILE_CONNECTED = 'mobile_connected'
 
-DEFAULT = [SENSOR_USAGE]
+BINARY_SENSOR_CLASSES = {
+    'roaming': None,
+    'wire_connected': DEVICE_CLASS_CONNECTIVITY,
+    BINARY_SENSOR_MOBILE_CONNECTED: DEVICE_CLASS_CONNECTIVITY,
+}
+
+ALL_SENSORS = list(SENSOR_UNITS)
+DEFAULT_SENSORS = [SENSOR_USAGE]
+
+ALL_BINARY_SENSORS = list(BINARY_SENSOR_CLASSES)
+DEFAULT_BINARY_SENSORS = [BINARY_SENSOR_MOBILE_CONNECTED]
