7cf4e25f6a75cac7d3484b90a0d7d970a285c672
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-04-10 11:59:27 -0700

    add test

--- tests/components/ps4/test_config_flow.py
+++ tests/components/ps4/test_config_flow.py
@@ -50,8 +50,7 @@ MOCK_MANUAL = {"Config Mode": 'Manual Entry', CONF_IP_ADDRESS: MOCK_HOST}
 MOCK_LOCATION = {'country_name': DEFAULT_REGION}
 
 
-@patch('homeassistant.util.location.detect_location_info',
-       return_value=MOCK_LOCATION)
+@patch('homeassistant.util.location.detect_location_info', MOCK_LOCATION)
 async def test_full_flow_implementation(hass):
     """Test registering an implementation and flow works."""
     flow = ps4.PlayStation4FlowHandler()
@@ -317,6 +316,18 @@ async def test_credential_abort(hass):
         result = await flow.async_step_creds({})
     assert result['type'] == data_entry_flow.RESULT_TYPE_ABORT
     assert result['reason'] == 'credential_error'
+
+
+async def test_credential_timeout(hass):
+    """Test that Credential Timeout shows error."""
+    flow = ps4.PlayStation4FlowHandler()
+    flow.hass = hass
+
+    with patch('pyps4_homeassistant.Helper.get_creds',
+               side_effect=UnboundLocalError):
+        result = await flow.async_step_creds({})
+    assert result['type'] == data_entry_flow.RESULT_TYPE_FORM
+    assert result['errors'] == {'base': 'credential_timeout'}
 
 
 @patch('homeassistant.util.location.detect_location_info', MOCK_LOCATION)
