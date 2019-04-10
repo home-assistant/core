645c6acbe708e08f32cb50f0874e56e094bafbe7
Swamp-Ig <github@ninjateaparty.com>
2019-04-10 21:15:28 +0800

    Sensor

--- homeassistant/components/template/sensor.py
+++ homeassistant/components/template/sensor.py
@@ -14,13 +14,13 @@ from homeassistant.components.sensor import ENTITY_ID_FORMAT, \
     PLATFORM_SCHEMA, DEVICE_CLASSES_SCHEMA
 from homeassistant.const import (
     ATTR_FRIENDLY_NAME, ATTR_UNIT_OF_MEASUREMENT, CONF_VALUE_TEMPLATE,
-    CONF_ICON_TEMPLATE, CONF_ENTITY_PICTURE_TEMPLATE, ATTR_ENTITY_ID,
-    CONF_SENSORS, EVENT_HOMEASSISTANT_START, CONF_FRIENDLY_NAME_TEMPLATE,
-    MATCH_ALL, CONF_DEVICE_CLASS)
+    CONF_ICON_TEMPLATE, CONF_ENTITY_PICTURE_TEMPLATE,
+    CONF_SENSORS, CONF_FRIENDLY_NAME_TEMPLATE,
+    CONF_DEVICE_CLASS)
 from homeassistant.exceptions import TemplateError
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity, async_generate_entity_id
-from homeassistant.helpers.event import async_track_state_change
+from homeassistant.helpers.event import async_track_template_result
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -31,8 +31,7 @@ SENSOR_SCHEMA = vol.Schema({
     vol.Optional(CONF_FRIENDLY_NAME_TEMPLATE): cv.template,
     vol.Optional(ATTR_FRIENDLY_NAME): cv.string,
     vol.Optional(ATTR_UNIT_OF_MEASUREMENT): cv.string,
-    vol.Optional(CONF_DEVICE_CLASS): DEVICE_CLASSES_SCHEMA,
-    vol.Optional(ATTR_ENTITY_ID): cv.entity_ids
+    vol.Optional(CONF_DEVICE_CLASS): DEVICE_CLASSES_SCHEMA
 })
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -55,42 +54,15 @@ async def async_setup_platform(hass, config, async_add_entities,
         unit_of_measurement = device_config.get(ATTR_UNIT_OF_MEASUREMENT)
         device_class = device_config.get(CONF_DEVICE_CLASS)
 
-        entity_ids = set()
-        manual_entity_ids = device_config.get(ATTR_ENTITY_ID)
-        invalid_templates = []
-
-        for tpl_name, template in (
-                (CONF_VALUE_TEMPLATE, state_template),
-                (CONF_ICON_TEMPLATE, icon_template),
-                (CONF_ENTITY_PICTURE_TEMPLATE, entity_picture_template),
-                (CONF_FRIENDLY_NAME_TEMPLATE, friendly_name_template),
+        for template in (
+                state_template,
+                icon_template,
+                entity_picture_template,
+                friendly_name_template,
         ):
             if template is None:
                 continue
             template.hass = hass
-
-            if manual_entity_ids is not None:
-                continue
-
-            template_entity_ids = template.extract_entities()
-            if template_entity_ids == MATCH_ALL:
-                entity_ids = MATCH_ALL
-                # Cut off _template from name
-                invalid_templates.append(tpl_name[:-9])
-            elif entity_ids != MATCH_ALL:
-                entity_ids |= set(template_entity_ids)
-
-        if invalid_templates:
-            _LOGGER.warning(
-                'Template sensor %s has no entity ids configured to track nor'
-                ' were we able to extract the entities to track from the %s '
-                'template(s). This entity will only be able to be updated '
-                'manually.', device, ', '.join(invalid_templates))
-
-        if manual_entity_ids is not None:
-            entity_ids = manual_entity_ids
-        elif entity_ids != MATCH_ALL:
-            entity_ids = list(entity_ids)
 
         sensors.append(
             SensorTemplate(
@@ -102,7 +74,6 @@ async def async_setup_platform(hass, config, async_add_entities,
                 state_template,
                 icon_template,
                 entity_picture_template,
-                entity_ids,
                 device_class)
             )
     if not sensors:
@@ -118,42 +89,58 @@ class SensorTemplate(Entity):
 
     def __init__(self, hass, device_id, friendly_name, friendly_name_template,
                  unit_of_measurement, state_template, icon_template,
-                 entity_picture_template, entity_ids, device_class):
+                 entity_picture_template, device_class):
         """Initialize the sensor."""
         self.hass = hass
         self.entity_id = async_generate_entity_id(ENTITY_ID_FORMAT, device_id,
                                                   hass=hass)
-        self._name = friendly_name
-        self._friendly_name_template = friendly_name_template
         self._unit_of_measurement = unit_of_measurement
-        self._template = state_template
-        self._state = None
-        self._icon_template = icon_template
-        self._entity_picture_template = entity_picture_template
-        self._icon = None
-        self._entity_picture = None
-        self._entities = entity_ids
         self._device_class = device_class
+
+        self._state = None
+        self._templates = {
+            state_template: '_state',
+        }
+        self._name = friendly_name
+        if friendly_name_template is not None:
+            self._templates[friendly_name_template] = '_name'
+        self._icon = None
+        if icon_template is not None:
+            self._templates[icon_template] = '_icon'
+        self._entity_picture = None
+        if entity_picture_template is not None:
+            self._templates[entity_picture_template] = '_entity_picture'
+        self._callbacks = []
+
+    def _template_error(self, prop, error):
+        friendly_property_name = prop[1:].replace('_', ' ')
+        if error.args and error.args[0].startswith(
+                "UndefinedError: 'None' has no attribute"):
+            # Common during HA startup - so just a warning
+            _LOGGER.warning('Could not render %s template %s,'
+                            ' the state is unknown.',
+                            friendly_property_name, self._name)
+        else:
+            _LOGGER.error('Could not render %s template %s',
+                          friendly_property_name, self._name,
+                          exc_info=error)
+
+    @callback
+    def _template_changed(self, event, template, last_result, result):
+        prop = self._templates[template]
+        if isinstance(result, TemplateError):
+            self._template_error(prop, result)
+            return
+        setattr(self, prop, result)
+        self.async_schedule_update_ha_state()
 
     async def async_added_to_hass(self):
         """Register callbacks."""
-        @callback
-        def template_sensor_state_listener(entity, old_state, new_state):
-            """Handle device state changes."""
-            self.async_schedule_update_ha_state(True)
-
-        @callback
-        def template_sensor_startup(event):
-            """Update template on startup."""
-            if self._entities != MATCH_ALL:
-                # Track state change only for valid templates
-                async_track_state_change(
-                    self.hass, self._entities, template_sensor_state_listener)
-
-            self.async_schedule_update_ha_state(True)
-
-        self.hass.bus.async_listen_once(
-            EVENT_HOMEASSISTANT_START, template_sensor_startup)
+        for template in self._templates:
+            info = async_track_template_result(
+                self.hass, template,
+                self._template_changed)
+            self._callbacks.append(info)
 
     @property
     def name(self):
@@ -191,41 +178,6 @@ class SensorTemplate(Entity):
         return False
 
     async def async_update(self):
-        """Update the state from the template."""
-        try:
-            self._state = self._template.async_render()
-        except TemplateError as ex:
-            if ex.args and ex.args[0].startswith(
-                    "UndefinedError: 'None' has no attribute"):
-                # Common during HA startup - so just a warning
-                _LOGGER.warning('Could not render template %s,'
-                                ' the state is unknown.', self._name)
-            else:
-                self._state = None
-                _LOGGER.error('Could not render template %s: %s', self._name,
-                              ex)
-        for property_name, template in (
-                ('_icon', self._icon_template),
-                ('_entity_picture', self._entity_picture_template),
-                ('_name', self._friendly_name_template)):
-            if template is None:
-                continue
-
-            try:
-                setattr(self, property_name, template.async_render())
-            except TemplateError as ex:
-                friendly_property_name = property_name[1:].replace('_', ' ')
-                if ex.args and ex.args[0].startswith(
-                        "UndefinedError: 'None' has no attribute"):
-                    # Common during HA startup - so just a warning
-                    _LOGGER.warning('Could not render %s template %s,'
-                                    ' the state is unknown.',
-                                    friendly_property_name, self._name)
-                    continue
-
-                try:
-                    setattr(self, property_name,
-                            getattr(super(), property_name))
-                except AttributeError:
-                    _LOGGER.error('Could not render %s template %s: %s',
-                                  friendly_property_name, self._name, ex)
+        """Force update of the state from the template."""
+        for call in self._callbacks:
+            call.async_refresh()
