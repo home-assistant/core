84eb18f29ac5776be104c90a899b709022909b32
Martin Hjelmare <marhje52@kth.se>
2019-04-11 02:00:14 +0200

    Return list of MAC addresses, not dict
    Co-Authored-By: mzdrale <mzdrale@gmail.com>

--- homeassistant/components/ubee/device_tracker.py
+++ homeassistant/components/ubee/device_tracker.py
@@ -54,7 +54,7 @@ class UbeeDeviceScanner(DeviceScanner):
         """Scan for new devices and return a list with found device IDs."""
         devices = self._get_connected_devices()
         self._mac2name = devices
-        return devices
+        return list(devices)
 
     def get_device_name(self, device):
         """Return the name of the given device or None if we don't know."""
