972d8346c86a321ecc6f970856a19a2b17ca0890
Jason Hu <awaregit@gmail.com>
2019-04-11 02:41:46 -0700

    Allow aws credential skip validation

--- homeassistant/components/aws/__init__.py
+++ homeassistant/components/aws/__init__.py
@@ -20,6 +20,7 @@ from .const import (
     CONF_REGION,
     CONF_SECRET_ACCESS_KEY,
     CONF_SERVICE,
+    CONF_VALIDATE,
     DATA_CONFIG,
     DATA_HASS_CONFIG,
     DATA_SESSIONS,
@@ -36,6 +37,7 @@ AWS_CREDENTIAL_SCHEMA = vol.Schema(
         vol.Inclusive(CONF_ACCESS_KEY_ID, ATTR_CREDENTIALS): cv.string,
         vol.Inclusive(CONF_SECRET_ACCESS_KEY, ATTR_CREDENTIALS): cv.string,
         vol.Exclusive(CONF_PROFILE_NAME, ATTR_CREDENTIALS): cv.string,
+        vol.Optional(CONF_VALIDATE, default=True): cv.boolean,
     }
 )
 
@@ -170,7 +172,8 @@ async def _validate_aws_credentials(hass, credential):
     else:
         session = aiobotocore.AioSession(loop=hass.loop)
 
-    async with session.create_client("iam", **aws_config) as client:
-        await client.get_user()
+    if credential[CONF_VALIDATE]:
+        async with session.create_client("iam", **aws_config) as client:
+            await client.get_user()
 
     return session
