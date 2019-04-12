331674f2adba3721160388daae22b6f9dfb152d6
Paulus Schoutsen <balloob@gmail.com>
2019-04-11 17:09:50 -0700

    Fix bloomsky

--- homeassistant/components/bloomsky/sensor.py
+++ homeassistant/components/bloomsky/sensor.py
@@ -8,7 +8,9 @@ from homeassistant.const import (TEMP_FAHRENHEIT, CONF_MONITORED_CONDITIONS)
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 
-_LOGGER = logging.getLogger(__name__)
+from . import BLOOMSKY
+
+LOGGER = logging.getLogger(__name__)
 
 DEPENDENCIES = ['bloomsky']
 
@@ -38,14 +40,13 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the available BloomSky weather sensors."""
-    bloomsky = hass.components.bloomsky
     # Default needed in case of discovery
     sensors = config.get(CONF_MONITORED_CONDITIONS, SENSOR_TYPES)
 
-    for device in bloomsky.BLOOMSKY.devices.values():
+    for device in BLOOMSKY.devices.values():
         for variable in sensors:
             add_entities(
-                [BloomSkySensor(bloomsky.BLOOMSKY, device, variable)], True)
+                [BloomSkySensor(BLOOMSKY, device, variable)], True)
 
 
 class BloomSkySensor(Entity):
