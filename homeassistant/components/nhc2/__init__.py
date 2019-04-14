0a11e188ac12bb408a8091b8305377e3a16b454c
Filip Van Ham <filipvanham@gmail.com>
2019-04-14 21:10:34 +0200

    Proposal for Niko Home Control II

---
+++ homeassistant/components/nhc2/__init__.py
@@ -0,0 +1,100 @@
+"""Support for Niko Home Control II - CoCo"""
+import logging
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_USERNAME, CONF_PASSWORD
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
+import homeassistant.helpers.config_validation as cv
+
+import voluptuous as vol
+
+from .helpers import extract_versions
+from .config_flow import Nhc2FlowHandler
+from .const import DOMAIN, KEY_GATEWAY
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = DOMAIN
+KEY_GATEWAY = KEY_GATEWAY
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_PORT): cv.port,
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+
+async def async_setup(hass, config):
+    """Set up the NHC2 CoCo component."""
+    conf = config.get(DOMAIN)
+
+    if conf is None:
+        return True
+
+    host = conf.get(CONF_HOST)
+    port = conf.get(CONF_PORT)
+    username = conf.get(CONF_USERNAME)
+    password = conf.get(CONF_PASSWORD)
+
+    hass.async_create_task(hass.config_entries.flow.async_init(
+        DOMAIN, context={'source': config_entries.SOURCE_IMPORT},
+        data={CONF_HOST: host, CONF_PORT: port,
+              CONF_USERNAME: username,
+              CONF_PASSWORD: password}
+    ))
+
+    return True
+
+
+async def async_setup_entry(hass, entry):
+    """Create a NHC2 gateway."""
+    from nhc2_coco import CoCo
+
+    coco = CoCo(
+        entry.data[CONF_HOST],
+        entry.data[CONF_USERNAME],
+        entry.data[CONF_PASSWORD],
+        entry.data[CONF_PORT]
+    )
+
+    async def on_hass_stop(event):
+        """Close connection when hass stops."""
+        coco.disconnect()
+
+    def get_process_sysinfo(dev_reg):
+        def process_sysinfo(nhc2_sysinfo):
+            coco_image, nhc_version = extract_versions(nhc2_sysinfo)
+            _LOGGER.debug('Sysinfo: NhcVersion %s - CocoImage %s', nhc_version, coco_image)
+            dev_reg.async_get_or_create(
+                config_entry_id=entry.entry_id,
+                connections=set(),
+                identifiers={
+                    (DOMAIN, entry.data[CONF_USERNAME])
+                },
+                manufacturer='Niko',
+                name='Home Control II',
+                model='Connected controller',
+                sw_version=nhc_version + ' - CoCo Image: ' + coco_image,
+            )
+
+            hass.async_create_task(hass.config_entries.async_forward_entry_setup(
+                entry, 'light'
+            ))
+
+            hass.async_create_task(hass.config_entries.async_forward_entry_setup(
+                entry, 'switch'
+            ))
+        return process_sysinfo
+
+    hass.data.setdefault(KEY_GATEWAY, {})[entry.entry_id] = coco
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, on_hass_stop)
+
+    _LOGGER.debug('Connecting to %s:%s', entry.data[CONF_HOST], str(entry.data[CONF_PORT]))
+    coco.connect()
+    dev_reg = await hass.helpers.device_registry.async_get_registry()
+    coco.get_systeminfo(get_process_sysinfo(dev_reg))
+
+    return True
---
+++ homeassistant/components/nhc2/config_flow.py
@@ -0,0 +1,65 @@
+"""Config flow to configure component."""
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_USERNAME, CONF_PASSWORD
+import homeassistant.helpers.config_validation as cv
+
+from .const import DOMAIN
+
+
+@config_entries.HANDLERS.register(DOMAIN)
+class Nhc2FlowHandler(config_entries.ConfigFlow):
+    """Config flow for NHC2 platform."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
+
+    def __init__(self):
+        """Init NHC2FlowHandler."""
+        self._errors = {}
+
+    async def async_step_import(self, user_input):
+        """Import a config entry."""
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        return self.async_create_entry(
+            title="configuration.yaml", data=user_input
+        )
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        self._errors = {}
+
+        if user_input is not None:
+            if user_input[CONF_USERNAME] not in\
+                    self.hass.config_entries.async_entries(DOMAIN):
+                return self.async_create_entry(
+                    title=user_input[CONF_USERNAME],
+                    data=user_input,
+                )
+
+            self._errors[CONF_USERNAME] = 'name_exists'
+
+        # default location is set hass configuration
+        return await self._show_config_form(
+            host=None,
+            port=8883,
+            username=None,
+            password=None)
+
+    async def _show_config_form(self, host=None, port=None,
+                                username=None,
+                                password=None):
+        """Show the configuration form to edit NHC2 data."""
+        return self.async_show_form(
+            step_id='user',
+            data_schema=vol.Schema({
+                vol.Required(CONF_HOST, default=host): str,
+                vol.Required(CONF_PORT, default=port): cv.port,
+                vol.Required(CONF_USERNAME, default=username): str,
+                vol.Required(CONF_PASSWORD, default=password): str
+            }),
+            errors=self._errors,
+        )
