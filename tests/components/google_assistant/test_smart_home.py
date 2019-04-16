bcc521139f2fc079b66167f4c2f783bbe1db9c0a
Joakim Plate <elupus@ecce.se>
2019-04-16 19:52:50 +0200

    Drop remnant unused import

--- tests/components/google_assistant/test_smart_home.py
+++ tests/components/google_assistant/test_smart_home.py
@@ -14,7 +14,6 @@ from homeassistant.components.google_assistant import (
     const, trait, helpers, smart_home as sh,
     EVENT_COMMAND_RECEIVED, EVENT_QUERY_RECEIVED, EVENT_SYNC_RECEIVED)
 from homeassistant.components.demo.light import DemoLight
-from homeassistant.components.demo.cover import DemoCover
 from homeassistant.components.demo.switch import DemoSwitch
 
 from homeassistant.helpers import device_registry
