d29d882a62f2a2320d94e174441cd218e6514713
David Bonnes <david@bonnes.me>
2019-04-17 09:16:31 +0100

    configuration for hub tokens are now separate from host addresses/credentials

--- homeassistant/components/geniushub/__init__.py
+++ homeassistant/components/geniushub/__init__.py
@@ -4,7 +4,7 @@ import logging
 import voluptuous as vol
 
 from homeassistant.const import (
-    CONF_HOST, CONF_PASSWORD, CONF_USERNAME)
+    CONF_HOST, CONF_PASSWORD, CONF_TOKEN, CONF_USERNAME)
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.discovery import async_load_platform
@@ -13,12 +13,21 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'geniushub'
 
+_V1_API_SCHEMA = vol.Schema({
+    vol.Required(CONF_TOKEN): cv.string,
+})
+
+_V3_API_SCHEMA = vol.Schema({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+})
+
 CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Optional(CONF_USERNAME): cv.string,
-        vol.Optional(CONF_PASSWORD): cv.string,
-        vol.Required(CONF_HOST): cv.string,
-    }),
+    DOMAIN: vol.Any(
+        _V3_API_SCHEMA,
+        _V1_API_SCHEMA,
+    )
 }, extra=vol.ALLOW_EXTRA)
 
 
@@ -26,11 +35,15 @@ async def async_setup(hass, hass_config):
     """Create a Genius Hub system."""
     from geniushubclient import GeniusHubClient  # noqa; pylint: disable=no-name-in-module
 
-    host = hass_config[DOMAIN].get(CONF_HOST)
-    username = hass_config[DOMAIN].get(CONF_USERNAME)
-    password = hass_config[DOMAIN].get(CONF_PASSWORD)
-
     geniushub_data = hass.data[DOMAIN] = {}
+
+    if hass_config[DOMAIN].get(CONF_HOST):
+        host = hass_config[DOMAIN].get(CONF_HOST)
+        username = hass_config[DOMAIN].get(CONF_USERNAME)
+        password = hass_config[DOMAIN].get(CONF_PASSWORD)
+    else:
+        host = hass_config[DOMAIN].get(CONF_TOKEN)
+        username = password = None
 
     try:
         client = geniushub_data['client'] = GeniusHubClient(
