38d23ba0af5fbbe3be8d3c488afe69a8e7f8d39c
David F. Mulcahey <david.mulcahey@icloud.com>
2019-04-18 12:24:02 -0400

    Misc. ZHA changes (#23190)
    * handle the off part of on with timed off command
    
    * use correct var
    
    * only bind / configure cluster once
    
    * clean up channel configuration
    
    * additional debug logging
    
    * add guard
    
    * prevent multiple discoveries for a device
    
    * cleanup and still configure on rejoin

--- homeassistant/components/zha/core/channels/general.py
+++ homeassistant/components/zha/core/channels/general.py
@@ -7,6 +7,7 @@ https://home-assistant.io/components/zha/
 import logging
 from homeassistant.core import callback
 from homeassistant.helpers.dispatcher import async_dispatcher_send
+from homeassistant.helpers.event import async_call_later
 from . import ZigbeeChannel, parse_and_log_command, MAINS_POWERED
 from ..helpers import get_attr_id_by_name
 from ..const import (
@@ -40,10 +41,27 @@ class OnOffChannel(ZigbeeChannel):
 
         if cmd in ('off', 'off_with_effect'):
             self.attribute_updated(self.ON_OFF, False)
-        elif cmd in ('on', 'on_with_recall_global_scene', 'on_with_timed_off'):
+        elif cmd in ('on', 'on_with_recall_global_scene'):
             self.attribute_updated(self.ON_OFF, True)
+        elif cmd == 'on_with_timed_off':
+            should_accept = args[0]
+            on_time = args[1]
+            # 0 is always accept 1 is only accept when already on
+            if should_accept == 0 or (should_accept == 1 and self._state):
+                self.attribute_updated(self.ON_OFF, True)
+                if on_time > 0:
+                    async_call_later(
+                        self.device.hass,
+                        (on_time / 10),  # value is in 10ths of a second
+                        self.set_to_off
+                    )
         elif cmd == 'toggle':
             self.attribute_updated(self.ON_OFF, not bool(self._state))
+
+    @callback
+    def set_to_off(self, *_):
+        """Set the state to off."""
+        self.attribute_updated(self.ON_OFF, False)
 
     @callback
     def attribute_updated(self, attrid, value):
