a2b5e5c61c1b23716aaf12b5da8144d5a0b113af
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-04-19 09:43:47 +0200

    Hass.io Add-on panel support for Ingress (#23185)
    * Hass.io Add-on panel support for Ingress
    
    * Revert part of discovery startup handling
    
    * Add type
    
    * Fix tests
    
    * Add tests
    
    * Fix lint
    
    * Fix lint on test

--- homeassistant/components/hassio/const.py
+++ homeassistant/components/hassio/const.py
@@ -1,5 +1,6 @@
 """Hass.io const variables."""
 
+ATTR_ADDONS = 'addons'
 ATTR_DISCOVERY = 'discovery'
 ATTR_ADDON = 'addon'
 ATTR_NAME = 'name'
@@ -8,6 +9,11 @@ ATTR_CONFIG = 'config'
 ATTR_UUID = 'uuid'
 ATTR_USERNAME = 'username'
 ATTR_PASSWORD = 'password'
+ATTR_PANELS = 'panels'
+ATTR_ENABLE = 'enable'
+ATTR_TITLE = 'title'
+ATTR_ICON = 'icon'
+ATTR_ADMIN = 'admin'
 
 X_HASSIO = 'X-Hassio-Key'
 X_INGRESS_PATH = "X-Ingress-Path"
