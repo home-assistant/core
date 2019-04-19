96ebb3c14a3d733aeaca343af96358b9082db6e4
Kim Frellsen <kim@frellsen.se>
2019-04-19 18:20:48 +0200

    Update device_tracker.py
    circle ci, blank line required

--- homeassistant/components/fortios/device_tracker.py
+++ homeassistant/components/fortios/device_tracker.py
@@ -1,5 +1,6 @@
 """
 Support to use FortiOS device like FortiGate as device tracker.
+
 This component is part of the device_tracker platform.
 
 For more details about this platform, please refer to the documentation at
