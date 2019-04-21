6c7126069e09029417ea988864d432f58a8c2f17
Tomas Kislan <kislan.tomas@gmail.com>
2019-04-21 10:50:02 +0200

    Add minio implementation

---
+++ homeassistant/components/minio/__init__.py
@@ -0,0 +1,178 @@
+import logging
+import os
+import threading
+from queue import Queue
+
+import voluptuous as vol
+from homeassistant.const import EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = 'minio'
+CONF_HOST = 'host'
+CONF_PORT = 'port'
+CONF_ACCESS_KEY = 'access_key'
+CONF_SECRET_KEY = 'secret_key'
+CONF_SECURE = 'secure'
+CONF_LISTEN = 'listen'
+CONF_LISTEN_BUCKET = 'bucket'
+CONF_LISTEN_PREFIX = 'prefix'
+CONF_LISTEN_SUFFIX = 'suffix'
+CONF_LISTEN_EVENTS = 'events'
+
+CONF_LISTEN_PREFIX_DEFAULT = ''
+CONF_LISTEN_SUFFIX_DEFAULT = '.*'
+CONF_LISTEN_EVENTS_DEFAULT = 's3:ObjectCreated:*'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_PORT): cv.port,
+        vol.Required(CONF_ACCESS_KEY): cv.string,
+        vol.Required(CONF_SECRET_KEY): cv.string,
+        vol.Required(CONF_SECURE): cv.boolean,
+        vol.Optional(CONF_LISTEN): vol.All(cv.ensure_list, [vol.Schema({
+            vol.Required(CONF_LISTEN_BUCKET): cv.string,
+            vol.Optional(CONF_LISTEN_PREFIX, default=CONF_LISTEN_PREFIX_DEFAULT): cv.string,
+            vol.Optional(CONF_LISTEN_SUFFIX, default=CONF_LISTEN_SUFFIX_DEFAULT): cv.string,
+            vol.Optional(CONF_LISTEN_EVENTS, default=CONF_LISTEN_EVENTS_DEFAULT): cv.string,
+        })])
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+BUCKET_KEY_SCHEMA = vol.Schema({
+    vol.Required('bucket'): cv.template,
+    vol.Required('key'): cv.template,
+})
+
+BUCKET_KEY_FILE_SCHEMA = BUCKET_KEY_SCHEMA.extend({
+    vol.Required('file_path'): cv.template,
+})
+
+
+class QueueListener(threading.Thread):
+    def __init__(self, hass):
+        super().__init__()
+        self.__hass = hass
+        self.__q = Queue()
+
+    def run(self):
+        _LOGGER.info('Running QueueListener')
+        while True:
+            event = self.__q.get()
+            if event is None:
+                break
+
+            _, file_name = os.path.split(event['key'])
+
+            _LOGGER.debug('Sending event %s, %s, %s', event['event_name'], event['bucket'], event['key'])
+            self.__hass.bus.fire(DOMAIN, {
+                'file_name': file_name,
+                **event,
+            })
+
+    @property
+    def q(self):
+        return self.__q
+
+    def stop(self):
+        _LOGGER.info('Stopping QueueListener')
+        self.__q.put(None)
+        self.join()
+        _LOGGER.info('Stopped QueueListener')
+
+    def start_handler(self, _):
+        self.start()
+
+    def stop_handler(self, _):
+        self.stop()
+
+
+class MinioListener:
+    def __init__(self, *args):
+        self.__args = args
+        self.__minio_event_thread = None
+
+    def start_handler(self, _):
+        from .minio_helper import MinioEventThread
+
+        self.__minio_event_thread = MinioEventThread(*self.__args)
+        self.__minio_event_thread.start()
+
+    def stop_handler(self, _):
+        if self.__minio_event_thread is not None:
+            self.__minio_event_thread.stop()
+
+
+def setup(hass, config):
+    conf = config[DOMAIN]
+
+    host = conf[CONF_HOST]
+    port = conf[CONF_PORT]
+    access_key = conf[CONF_ACCESS_KEY]
+    secret_key = conf[CONF_SECRET_KEY]
+    secure = conf[CONF_SECURE]
+
+    queue_listener = QueueListener(hass)
+    q = queue_listener.q
+
+    hass.bus.listen_once(EVENT_HOMEASSISTANT_START, queue_listener.start_handler)
+    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, queue_listener.stop_handler)
+
+    def _setup_listener(c):
+        bucket = c[CONF_LISTEN_BUCKET]
+        prefix = c[CONF_LISTEN_PREFIX]
+        suffix = c[CONF_LISTEN_SUFFIX]
+        events = c[CONF_LISTEN_EVENTS]
+
+        minio_listener = MinioListener(q, f'{host}:{str(port)}', access_key, secret_key, secure, bucket, prefix, suffix, events)
+
+        hass.bus.listen_once(EVENT_HOMEASSISTANT_START, minio_listener.start_handler)
+        hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, minio_listener.stop_handler)
+
+    for listen_conf in conf.get(CONF_LISTEN, []):
+        _setup_listener(listen_conf)
+
+    from minio import Minio
+
+    mc = Minio(f'{host}:{str(port)}', access_key, secret_key, secure)
+
+    def _render_service_value(service, key):
+        value = service.data.get(key)
+        value.hass = hass
+        return value.async_render()
+
+    def put_file(service):
+        bucket = _render_service_value(service, 'bucket')
+        key = _render_service_value(service, 'key')
+        file_path = _render_service_value(service, 'file_path')
+
+        if not hass.config.is_allowed_path(file_path):
+            _LOGGER.error('Invalid file_path %s', file_path)
+            return
+
+        mc.fput_object(bucket, key, file_path)
+
+    def get_file(service):
+        bucket = _render_service_value(service, 'bucket')
+        key = _render_service_value(service, 'key')
+        file_path = _render_service_value(service, 'file_path')
+
+        if not hass.config.is_allowed_path(file_path):
+            _LOGGER.error('Invalid file_path %s', file_path)
+            return
+
+        mc.fget_object(bucket, key, file_path)
+
+    def remove_file(service):
+        bucket = _render_service_value(service, 'bucket')
+        key = _render_service_value(service, 'key')
+
+        mc.remove_object(bucket, key)
+
+    hass.services.register(DOMAIN, 'put', put_file, schema=BUCKET_KEY_FILE_SCHEMA)
+    hass.services.register(DOMAIN, 'get', get_file, schema=BUCKET_KEY_FILE_SCHEMA)
+    hass.services.register(DOMAIN, 'remove', remove_file, schema=BUCKET_KEY_SCHEMA)
+
+    return True
---
+++ homeassistant/components/minio/minio_helper.py
@@ -0,0 +1,139 @@
+import collections
+import json
+import logging
+import re
+import threading
+from queue import Queue
+from typing import Iterator, List
+from urllib.parse import unquote
+
+from minio import Minio
+
+_LOGGER = logging.getLogger(__name__)
+
+
+_METADATA_RE = re.compile('x-amz-meta-(.*)', re.IGNORECASE)
+
+
+def normalize_metadata(metadata: dict) -> dict:
+    new_metadata = {}
+    for meta_key in metadata.keys():
+        m = _METADATA_RE.match(meta_key)
+        if not m:
+            continue
+
+        new_metadata[m.group(1).lower()] = metadata[meta_key]
+
+    return new_metadata
+
+
+def get_minio_notification_response(mc, bucket_name: str, prefix: str, suffix: str, events: List[str]):
+    query = {
+        'prefix': prefix,
+        'suffix': suffix,
+        'events': events,
+    }
+    # noinspection PyProtectedMember
+    return mc._url_open('GET', bucket_name=bucket_name, query=query, preload_content=False)
+
+
+class MinioEventStreamIterator(collections.Iterable):
+    def __iter__(self) -> Iterator:
+        return self
+
+    def __init__(self, response):
+        self.__response = response
+        self.__stream = response.stream()
+
+    def __next__(self):
+        while True:
+            line = next(self.__stream)
+            if line.strip():
+                event = json.loads(line.decode('utf-8'))
+                if event['Records'] is not None:
+                    return event
+
+    def close(self):
+        self.__response.close()
+
+
+class MinioEventThread(threading.Thread):
+    def __init__(self, q: Queue, endpoint: str, access_key: str, secret_key: str, secure: bool, bucket_name: str, prefix: str, suffix: str, events: List[str]):
+        super().__init__()
+        self.__q = q
+        self.__endpoint = endpoint
+        self.__access_key = access_key
+        self.__secret_key = secret_key
+        self.__secure = secure
+        self.__bucket_name = bucket_name
+        self.__prefix = prefix
+        self.__suffix = suffix
+        self.__events = events
+        self.__event_stream_it = None
+
+    def __enter__(self):
+        self.start()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+
+    def run(self):
+        _LOGGER.info('Running MinioEventThread')
+
+        mc = Minio(self.__endpoint, self.__access_key, self.__secret_key, self.__secure)
+
+        while True:
+            _LOGGER.info('Connecting to minio event stream')
+            response = get_minio_notification_response(mc, self.__bucket_name, self.__prefix, self.__suffix, self.__events)
+            self.__event_stream_it = MinioEventStreamIterator(response)
+
+            try:
+                for event in self.__event_stream_it:
+                    for event_name, bucket, key, metadata in iterate_objects(event):
+                        presigned_url = ''
+                        try:
+                            presigned_url = mc.presigned_get_object(bucket, key)
+                        except Exception as error:
+                            _LOGGER.error('Failed to generate presigned url: %s', error)
+
+                        queue_entry = {
+                            "event_name": event_name,
+                            "bucket": bucket,
+                            "key": key,
+                            "presigned_url": presigned_url,
+                            "metadata": metadata,
+                        }
+                        _LOGGER.debug('Queue entry, %s', queue_entry)
+                        self.__q.put(queue_entry)
+            except json.JSONDecodeError:
+                response.close()
+            except AttributeError:
+                break
+
+    def stop(self):
+        _LOGGER.info('Stopping event thread')
+        if self.__event_stream_it is not None:
+            self.__event_stream_it.close()
+            self.__event_stream_it = None
+
+        _LOGGER.info('Joining event thread')
+        self.join()
+        _LOGGER.info('Event thread joined')
+
+
+def iterate_objects(event):
+    records = event.get('Records', [])
+
+    for record in records:
+        event_name = record.get('eventName')
+        bucket = record.get('s3', {}).get('bucket', {}).get('name')
+        key = record.get('s3', {}).get('object', {}).get('key')
+        metadata = normalize_metadata(record.get('s3', {}).get('object', {}).get('userMetadata', {}))
+
+        if not bucket or not key:
+            _LOGGER.info('Invalid bucket and/or key', bucket, key)
+            continue
+
+        key = unquote(key)
+
+        yield event_name, bucket, key, metadata
