b7862732f1b2005d8da1d2361ad344bfce8806e9
Ties de Kock <ties@tiesdekock.nl>
2019-04-24 21:06:31 +0200

    Use asyncio.Conditions instead of asyncio.Lock

--- homeassistant/components/buienradar/camera.py
+++ homeassistant/components/buienradar/camera.py
@@ -9,10 +9,12 @@ from typing import Optional
 
 from homeassistant.components.camera import PLATFORM_SCHEMA, Camera
 from homeassistant.const import CONF_ID, CONF_NAME
+
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from homeassistant.util import dt as dt_util
+from homeassistant.util.async_ import run_coroutine_threadsafe
 
 CONF_DIMENSION = 'dimension'
 CONF_INTERVAL = 'interval'
@@ -22,9 +24,12 @@ RADAR_MAP_URL_TEMPLATE = ('https://api.buienradar.nl/image/1.0/'
 
 _LOG = logging.getLogger(__name__)
 
+# Maximum range according to docs
+DIM_RANGE = vol.All(vol.Coerce(int), vol.Range(min=120, max=700))
+
 PLATFORM_SCHEMA = vol.All(
     PLATFORM_SCHEMA.extend({
-        vol.Optional(CONF_DIMENSION): cv.positive_int,
+        vol.Optional(CONF_DIMENSION): DIM_RANGE,
         vol.Optional(CONF_INTERVAL): cv.positive_int,
         vol.Optional(CONF_NAME): cv.string,
     }))
@@ -36,7 +41,7 @@ async def async_setup_platform(hass, config, async_add_entities,
     c_id = config.get(CONF_ID)
     dimension = config.get(CONF_DIMENSION) or 512
     interval = config.get(CONF_INTERVAL) or 600 
-    name = config.get(CONF_NAME) or "Buienradar Radar Loop"
+    name = config.get(CONF_NAME) or "Buienradar loop"
 
     async_add_entities([BuienradarCam(hass, name, c_id, dimension, interval)])
 
@@ -47,10 +52,19 @@ class BuienradarCam(Camera):
     _deadline: Optional[int]
     _name: str
     _interval: Optional[int]
+    _condition: asyncio.Condition
+    """ Loading status """
+    _loading: bool
+
+    _last_image: Optional[bytes]
+    """ last modified HTTP response header"""
+    _last_modified: Optional[str]
     """
     A camera component producing animated buienradar radar-imagery GIFs.
 
     Image URL taken from https://www.buienradar.nl/overbuienradar/gratis-weerdata
+
+
     """
 
     def __init__(self, hass, name: str, c_id: Optional[str], dimension: int, interval: Optional[int]):
@@ -62,9 +76,11 @@ class BuienradarCam(Camera):
         self._dimension = dimension
         self._interval = interval
 
-        self._lock = asyncio.Lock(loop=hass.loop)
+        self._condition = asyncio.Condition(loop=hass.loop)
 
         self._last_image = None
+        self._last_modified = None
+        self._loading = False
         # deadline for image to be refreshed
         self._deadline = None
 
@@ -81,45 +97,80 @@ class BuienradarCam(Camera):
 
     def __needs_refresh(self) -> bool:
         if not (self._interval and self._deadline and self._last_image):
-            _LOG.info("refresh due to preconditions")
             return True
 
-        if dt_util.utcnow() > self._deadline:
-            _LOG.info("refresh due to refresh interval")
         return dt_util.utcnow() > self._deadline
 
+    async def __retrieve_radar_image(self) -> bool:
+        """
+        Retrieve new radar image and return whether this succeeded.
+        """
+        session: asyncio.ClientSession = async_get_clientsession(self._hass)
+
+        url = RADAR_MAP_URL_TEMPLATE.format(w=self._dimension,
+                                            h=self._dimension)
+
+        if self._last_modified:
+            headers = {'If-Modified-Since': self._last_modified}
+        else:
+            headers = {}
+
+        try:
+            async with session.get(url, timeout=5, headers=headers) as res:
+                if res.status == 304:
+                    _LOG.debug("HTTP 304 - success")
+                    return True
+                elif res.status != 200:
+                    _LOG.error("HTTP %s - failure", res.status)
+                    return False
+                else:
+                    last_modified = res.headers.get('Last-Modified', None)
+                    if last_modified:
+                        self._last_modified = last_modified
+
+                    self._last_image = await res.read()
+                    _LOG.debug("HTTP 200 - Last-Modified: %s", last_modified)
+
+                    return True
+        except (asyncio.TimeoutError, aiohttp.ClientError) as e:
+            _LOG.error("Failed to fetch image, %s", type(e))
+            return False
+
+
     async def async_camera_image(self):
-        """Return a still image response from the camera."""
+        """
+        Return a still image response from the camera.
+
+        Uses ayncio conditions to make sure only one task enters the critical
+        section at the same time. Othertiwse, two http requests would start
+        when two tabs with home assistant are open.
+
+        An additional boolean (_loading) is used to indicate the loading status
+        instead of _last_image since that is initialized to None.
+        """
         if not self.__needs_refresh():
-            _LOG.info(f"r: cached image {self._deadline}, {self._interval}")
             return self._last_image
 
-        async with self._lock:
-            # check if image was loaded while locked
-            if not self.__needs_refresh():
-                _LOG.info(f"return fresh cached image {self._deadline}, "
-                         f"{self._interval}")
+        # get lock, check iff loading, await notification if loading
+        async with self._condition:
+            if self._loading:
+                _LOG.debug("already loading - waiting for notification")
+                await self._condition.wait()
                 return self._last_image
 
-            _LOG.info(f"getting new image.")
+            # Set loading status **while holding lock**, makes other tasks wait
+            self._loading = True
 
-            # deadline has passed, get shared ClientSession to load new image.
-            # note that this session dies when another component (i.e. aiohue)
-            # throws.
-            session: asyncio.ClientSession = async_get_clientsession(self._hass)
-
+        try:
             now = dt_util.utcnow()
-            url = RADAR_MAP_URL_TEMPLATE.format(w=self._dimension,
-                                                h=self._dimension)
-            try:
-                async with session.get(url, timeout=5) as res:
-                    if res.status != 200:
-                        _LOG.error("Failed to fetch image, %s", res.status)
-                    else:
-                        self._last_image = await res.read()
-                        self._deadline = now + timedelta(seconds=self._interval)
-                        _LOG.info(f"return newly loaded image, deadline: {self._deadline}, {self._interval}")
-            except (asyncio.TimeoutError, aiohttp.ClientError) as e:
-                _LOG.error("Failed to fetch image, %s", type(e))
+            res = await self.__retrieve_radar_image()
+            # successful response? update deadline to time before loading
+            if res:
+                self._deadline = now + timedelta(seconds=self._interval)
 
             return self._last_image
+        finally:
+            # get lock, unset loading status, notify all waiting tasks
+            async with self._condition:
+                self._loading = False
+                self._condition.notify_all()
