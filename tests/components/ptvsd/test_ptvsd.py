2025ac9035e40e4ef41afec9d4b0d14493058499
Swamp-Ig <github@ninjateaparty.com>
2019-04-25 18:43:20 +0800

    Get the test working.

--- tests/components/ptvsd/test_ptvsd.py
+++ tests/components/ptvsd/test_ptvsd.py
@@ -1,41 +1,10 @@
 """Tests for PTVSD Debugger."""
 
-
 from unittest.mock import patch
-
-import ptvsd  # noqa: F401
+from asynctest import CoroutineMock
 
 import homeassistant.components.ptvsd as ptvsd_component
-from homeassistant.setup import async_setup_component
 from homeassistant.bootstrap import _async_set_up_integrations
-
-
-async def test_ptvsd(hass):
-    """Test loading ptvsd component."""
-    with patch('ptvsd.enable_attach') as attach:
-        with patch('ptvsd.wait_for_attach') as wait:
-            assert await async_setup_component(
-                hass, ptvsd_component.DOMAIN, {
-                    ptvsd_component.DOMAIN: {}
-                })
-
-            attach.assert_called_once_with(('0.0.0.0', 5678))
-            assert wait.call_count == 0
-
-
-async def test_ptvsd_wait(hass):
-    """Test loading ptvsd component with wait."""
-    with patch('ptvsd.enable_attach') as attach:
-        with patch('ptvsd.wait_for_attach') as wait:
-            assert await async_setup_component(
-                hass, ptvsd_component.DOMAIN, {
-                    ptvsd_component.DOMAIN: {
-                        ptvsd_component.CONF_WAIT: True
-                    }
-                })
-
-            attach.assert_called_once_with(('0.0.0.0', 5678))
-            assert wait.call_count == 1
 
 
 async def test_ptvsd_bootstrap(hass):
@@ -46,9 +15,9 @@ async def test_ptvsd_bootstrap(hass):
         }
     }
 
-    with patch('ptvsd.enable_attach') as attach:
-        with patch('ptvsd.wait_for_attach') as wait:
-            await _async_set_up_integrations(hass, config)
+    with patch(
+            'homeassistant.components.ptvsd.async_setup',
+            CoroutineMock()) as setup_mock:
+        await _async_set_up_integrations(hass, config)
 
-            attach.assert_called_once_with(('0.0.0.0', 5678))
-            assert wait.call_count == 1
+        setup_mock.assert_called_once()
