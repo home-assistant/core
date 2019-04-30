5da64234fab6cde2ddcd47ce5b20bddef000899b
Austin Drummond <adrum0x7@gmail.com>
2019-04-29 22:47:22 -0400

    dont validate TelevisionMediaPlayer accessory via config features

--- homeassistant/components/homekit/util.py
+++ homeassistant/components/homekit/util.py
@@ -37,8 +37,7 @@ CODE_SCHEMA = BASIC_INFO_SCHEMA.extend({
 MEDIA_PLAYER_SCHEMA = vol.Schema({
     vol.Required(CONF_FEATURE): vol.All(
         cv.string, vol.In((FEATURE_ON_OFF, FEATURE_PLAY_PAUSE,
-                           FEATURE_PLAY_STOP, FEATURE_TOGGLE_MUTE,
-                           FEATURE_SELECT_SOURCE, FEATURE_VOLUME_STEP))),
+                           FEATURE_PLAY_STOP, FEATURE_TOGGLE_MUTE))),
 })
 
 SWITCH_TYPE_SCHEMA = BASIC_INFO_SCHEMA.extend({
@@ -104,10 +103,6 @@ def validate_media_player_features(state, feature_list):
         supported_modes.append(FEATURE_PLAY_STOP)
     if features & media_player.const.SUPPORT_VOLUME_MUTE:
         supported_modes.append(FEATURE_TOGGLE_MUTE)
-    if features & media_player.const.SUPPORT_VOLUME_STEP:
-        supported_modes.append(FEATURE_VOLUME_STEP)
-    if features & media_player.const.SUPPORT_SELECT_SOURCE:
-        supported_modes.append(FEATURE_SELECT_SOURCE)
 
     error_list = []
     for feature in feature_list:
