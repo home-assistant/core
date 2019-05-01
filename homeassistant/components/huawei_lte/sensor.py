90a9210b6e851b7d52a4a26fbf83466473e7a1ee
Ville Skyttä <ville.skytta@iki.fi>
2019-05-01 10:37:49 +0300

    Clean up redundant same-package ..package imports

--- homeassistant/components/huawei_lte/sensor.py
+++ homeassistant/components/huawei_lte/sensor.py
@@ -12,7 +12,7 @@ from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 
-from ..huawei_lte import DATA_KEY, RouterData
+from . import DATA_KEY, RouterData
 
 _LOGGER = logging.getLogger(__name__)
 
