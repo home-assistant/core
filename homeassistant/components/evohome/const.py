3bdfa1a6891499b7c6d798579753a25181931aa2
David Bonnes <david@bonnes.me>
2019-05-03 00:43:19 +0100

    Refactor evohome to prepare for water_heater (#23489)
    * refactor - add const.py, change order of propertys, methods
    
    * import client at top of file
    
    * remove debug line
    
    * de-lint
    
    * delint
    
    * add me as CODEOWNER
    
    * remove lint hint
    
    * delint

---
+++ homeassistant/components/evohome/const.py
@@ -0,0 +1,9 @@
+"""Provides the constants needed for evohome."""
+
+DOMAIN = 'evohome'
+DATA_EVOHOME = 'data_' + DOMAIN
+DISPATCHER_EVOHOME = 'dispatcher_' + DOMAIN
+
+# These are used only to help prevent E501 (line too long) violations.
+GWS = 'gateways'
+TCS = 'temperatureControlSystems'
