4a4d00e2d3d8096bf2c959f38400513fb869ec10
Eduard van Valkenburg <edvan@microsoft.com>
2019-05-03 11:17:51 +0200

    updates to EH

--- homeassistant/components/azure_eventhub/__init__.py
+++ homeassistant/components/azure_eventhub/__init__.py
@@ -1,9 +1,7 @@
-"""Support for Azure Event Hubs"""
+"""Support for Azure Event Hubs."""
 import datetime
 import json
 import logging
-import os
-import asyncio
 from typing import Any, Dict
 
 import voluptuous as vol
@@ -16,13 +14,11 @@ from homeassistant.helpers.entityfilter import FILTER_SCHEMA
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'azure'
+DOMAIN = 'azure_eventhub'
 
 CONF_EVENT_HUB_ADDRESS = 'event_hub_address'
 CONF_EVENT_HUB_SAS_POLICY = 'event_hub_sas_policy'
 CONF_EVENT_HUB_SAS_KEY = 'event_hub_sas_key'
-CONF_EVENT_HUB_PARTITIONED = 'event_hub_partitioned'
-CONF_EVENT_HUB_PARTITION_KEY = 'event_hub_partition_key'
 CONF_FILTER = 'filter'
 
 CONFIG_SCHEMA = vol.Schema({
@@ -30,8 +26,6 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(CONF_EVENT_HUB_ADDRESS): cv.string,
         vol.Required(CONF_EVENT_HUB_SAS_POLICY): cv.string,
         vol.Required(CONF_EVENT_HUB_SAS_KEY): cv.string,
-        vol.Optional(CONF_EVENT_HUB_PARTITIONED, default=False): cv.boolean,
-        vol.Optional(CONF_EVENT_HUB_PARTITION_KEY, default='homeassistant'): cv.string,
         vol.Required(CONF_FILTER): FILTER_SCHEMA,
     }),
 }, extra=vol.ALLOW_EXTRA)
@@ -45,9 +39,7 @@ def setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
     event_hub_address = config[CONF_EVENT_HUB_ADDRESS]
     event_hub_sas_policy = config[CONF_EVENT_HUB_SAS_POLICY]
     event_hub_sas_key = config[CONF_EVENT_HUB_SAS_KEY]
-    event_hub_partitioned = config[CONF_EVENT_HUB_PARTITIONED]
-    event_hub_partition_key = config[CONF_EVENT_HUB_PARTITION_KEY]
-    
+
     entities_filter = config[CONF_FILTER]
 
     client = EventHubClientAsync(
@@ -67,18 +59,14 @@ def setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
                 or not entities_filter(state.entity_id)):
             return
 
-        as_dict = state.as_dict()
-        event_data = json.dumps(
-            obj=as_dict,
-            default=encoder.encode
-        ).encode('utf-8')
+        event_data = EventData(
+            json.dumps(
+                obj=state.as_dict(),
+                default=encoder.encode
+            ).encode('utf-8')
+        )
 
-        data = EventData(event_data)
-        if event_hub_partitioned:
-            data.partition_key = b'homeassistant'
-        else:
-            data.partition_key = b'homeassistant'
-        await sender.send(data)
+        await sender.send(event_data)
 
     hass.bus.listen(EVENT_STATE_CHANGED, send_to_eventhub)
     return True
