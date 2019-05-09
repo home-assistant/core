cd27d19c779448ebe98a8d4f226611c81c573730
Alex Sleeis <alex-github@diverman.com>
2019-05-09 08:57:21 -0700

    Revert "Adding filter support for splunk component."  Attempting to redo commits.
    This reverts commit bf57a600232362cc44770ff3e49d96cf45dcc0ea.

--- homeassistant/components/splunk/__init__.py
+++ homeassistant/components/splunk/__init__.py
@@ -11,12 +11,10 @@ from homeassistant.const import (
     CONF_PORT, CONF_TOKEN, EVENT_STATE_CHANGED)
 from homeassistant.helpers import state as state_helper
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entityfilter import FILTER_SCHEMA
 from homeassistant.helpers.json import JSONEncoder
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_FILTER = 'filter'
 DOMAIN = 'splunk'
 
 DEFAULT_HOST = 'localhost'
@@ -32,7 +30,6 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Optional(CONF_SSL, default=False): cv.boolean,
         vol.Optional(CONF_VERIFY_SSL, default=True): cv.boolean,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Optional(CONF_FILTER, default={}): FILTER_SCHEMA,
     }),
 }, extra=vol.ALLOW_EXTRA)
 
@@ -47,8 +44,6 @@ def setup(hass, config):
     verify_ssl = conf.get(CONF_VERIFY_SSL)
     name = conf.get(CONF_NAME)
 
-    entity_filter = conf[CONF_FILTER]
-
     if use_ssl:
         uri_scheme = 'https://'
     else:
@@ -62,8 +57,7 @@ def setup(hass, config):
         """Listen for new messages on the bus and sends them to Splunk."""
         state = event.data.get('new_state')
 
-        """Skip empty states, and exclude filtered domains/entities."""
-        if state is None or not entity_filter(state.entity_id):
+        if state is None:
             return
 
         try:
