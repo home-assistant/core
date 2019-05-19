e360235ac8e218bf6999d626f612e855f5fe21a9
RB28Z2 <kmarasinghe1@gmail.com>
2019-05-18 19:09:03 -0600

    pulling and displaying data from jlrpy is working. very basic functionality as of now.

--- homeassistant/components/jlrincontrol/__init__.py
+++ homeassistant/components/jlrincontrol/__init__.py
@@ -9,13 +9,21 @@ from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_NAME
 from homeassistant.helpers.dispatcher import (
     async_dispatcher_send
 )
+from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import async_track_point_in_utc_time
 from homeassistant.util.dt import utcnow
 
 __LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'jlrincontrol'
+SIGNAL_VEHICLE_SEEN = '{}.vehicle_seen'.format(DOMAIN)
 DATA_KEY = DOMAIN
+CONF_MUTABLE = 'mutable'
+
+RESOURCES = {
+    'FUEL_LEVEL_PERC': ('sensor', 'Fuel level', 'mdi:fuel', '%'),
+    'DISTANCE_TO_EMPTY_FUEL': ('sensor', 'Range', 'mdi:car', 'km')
+}
 
 SIGNAL_STATE_UPDATED = '{}.updated'.format(DOMAIN)
 
@@ -23,6 +31,8 @@ CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_USERNAME): cv.string,
         vol.Required(CONF_PASSWORD): cv.string,
+        vol.Required(CONF_NAME, default={}): vol.Schema(
+            {cv.slug: cv.string}),
     })
 }, extra=vol.ALLOW_EXTRA)
 
@@ -34,7 +44,7 @@ async def async_setup(hass, config):
     username = config[DOMAIN].get(CONF_USERNAME)
     password = config[DOMAIN].get(CONF_PASSWORD)
 
-    data = hass.data[DATA_KEY] = JLRData(config)
+    state = hass.data[DATA_KEY] = JLRData(config)
 
     connection = jlrpy.Connection(username, password)
 
@@ -50,11 +60,28 @@ async def async_setup(hass, config):
         info = vehicle.get_status()
         vehicle_status = format_nicely(info.get('vehicleStatus'))
 
-        hass.states.async_set('jlrtest.fuel_level', vehicle_status.get('FUEL_LEVEL_PERC'))
+        # hass.states.async_set('jlrtest.fuel_level', vehicle_status.get('FUEL_LEVEL_PERC'))
 
     def discover_vehicle(vehicle):
-        data.vehicles.add(vehicle.vin)
-        get_info(vehicle)
+        state.entities[vehicle.vin] = []
+
+        for attr, (component, *_) in RESOURCES.items():
+            hass.async_create_task(
+                hass.helpers.discovery.async_load_platform(
+                    'sensor', DOMAIN, (vehicle.vin, attr), config
+                )
+            )
+
+    async def update_vehicle(vehicle):
+        """Update information on vehicle"""
+        state.vehicles[vehicle.vin] = vehicle
+        if vehicle.vin not in state.entities:
+            discover_vehicle(vehicle)
+
+        for entity in state.entities[vehicle.vin]:
+            entity.schedule_update_ha_state()
+
+        async_dispatcher_send(hass, SIGNAL_VEHICLE_SEEN, vehicle)
 
 
     async def update(now):
@@ -65,10 +92,7 @@ async def async_setup(hass, config):
                 return False
 
             for vehicle in connection.vehicles:
-                if vehicle.vin not in data.vehicles:
-                    discover_vehicle(vehicle)
-
-            async_dispatcher_send(hass, SIGNAL_STATE_UPDATED)
+                await update_vehicle(vehicle)
 
             return True
         finally:
@@ -85,31 +109,68 @@ class JLRData:
 
     def __init__(self, config):
         """Initialize the component state."""
-        self.vehicles = set()
-        self.instruments = set()
+        self.entities = {}
+        self.vehicles = {}
         self.config = config[DOMAIN]
         self.names = self.config.get(CONF_NAME)
 
-    def instrument(self, vin, component, attr):
-        """Return corresponding instrument."""
-        return next((instrument
-                     for instrument in self.instruments
-                     if instrument.vehicle.vin == vin and
-                     instrument.component == component and
-                     instrument.attr == attr), None)
-
     def vehicle_name(self, vehicle):
         """Provide a friendly name for a vehicle."""
-        if (self._registration_number(vehicle) and
-            self._registration_number(vehicle).lower()) in self.names:
-            return self.names[self._registration_number(vehicle).lower()]
-        if vehicle.vin and vehicle.vin.lower() in self.names:
+        if (vehicle.vin and vehicle.vin.lower() in self.names):
             return self.names[vehicle.vin.lower()]
-        if self._registration_number(vehicle):
-            return self._registration_number(vehicle)
-        if vehicle.vin:
+        elif vehicle.vin:
             return vehicle.vin
-        return ''
+        else:
+            return ''
 
-    def _registration_number(self, vehicle):
-        return vehicle.get_attributes().get('registrationNumber')
+
+class JLREntity(Entity):
+    """Base class for all JLR Vehicle entities."""
+
+    def __init__(self, hass, vin, attribute):
+        """Initialize the entity."""
+        self._hass = hass
+        self._vin = vin
+        self._attribute = attribute
+        self._state.entities[self._vin].append(self)
+
+    @property
+    def _state(self):
+        return self._hass.data[DATA_KEY]
+
+    @property
+    def vehicle(self):
+        """Return vehicle."""
+        return self._state.vehicles[self._vin]
+
+    @property
+    def _entity_name(self):
+        return RESOURCES[self._attribute][1]
+
+    @property
+    def _vehicle_name(self):
+        return self._state.vehicle_name(self.vehicle)
+
+    @property
+    def name(self):
+        """Return full name of the entity."""
+        return '{} {}'.format(
+            self._vehicle_name,
+            self._entity_name)
+
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return False
+
+    @property
+    def assumed_state(self):
+        """Return true if unable to access real state of entity."""
+        return True
+
+    @property
+    def device_state_attributes(self):
+        """Return device specific state attributes."""
+        vehicle_attr = self.vehicle.get_attributes()
+        return dict(model='{} {} {}'.format(vehicle_attr['modelYear'], vehicle_attr['vehicleBrand'],
+                                            vehicle_attr['vehicleType']))
---
+++ homeassistant/components/jlrincontrol/sensor.py
@@ -0,0 +1,51 @@
+"""Support for JLR InControl sensors"""
+import logging
+
+from . import JLREntity, RESOURCES
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the Volkswagen sensors."""
+    if discovery_info is None:
+        return
+    async_add_entities([JLRSensor(hass, *discovery_info)])
+
+
+class JLRSensor(JLREntity):
+    """Representation of a JLR Sensor."""
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        _LOGGER.debug('Getting state of %s sensor' % self._attribute)
+
+        val = self._get_vehicle_status(self.vehicle)[self._attribute]
+        if val is None:
+            return val
+        if self._attribute in ['last_connected', 'service_inspection', 'oil_inspection']:
+            return str(val)
+        else:
+            return int(float(val))
+
+    def _get_vehicle_status(self, vehicle):
+        dict_only = {}
+        for el in vehicle.get_status().get('vehicleStatus'):
+            dict_only[el.get('key')] = el.get('value')
+        return dict_only
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return RESOURCES[self._attribute][3]
+
+    @property
+    def icon(self):
+        """Return the icon."""
+        return RESOURCES[self._attribute][2]
+
+    @property
+    def available(self):
+        """Return True if entity is available."""
+        return True
