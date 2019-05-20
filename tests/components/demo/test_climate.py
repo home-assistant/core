0a07c0c56c8d922fb5236d76531557d056aed24e
Pascal Vizeli <pvizeli@syshack.ch>
2019-05-20 16:10:54 +0200

    Fix tests

--- tests/components/demo/test_climate.py
+++ tests/components/demo/test_climate.py
@@ -6,12 +6,12 @@ import voluptuous as vol
 
 from homeassistant.components.climate.const import (
     ATTR_AUX_HEAT, ATTR_CURRENT_HUMIDITY, ATTR_CURRENT_HVAC,
-    ATTR_CURRENT_TEMPERATURE, ATTR_FAN_MODE, ATTR_HUMIDITY, ATTR_MAX_HUMIDITY,
-    ATTR_MAX_TEMP, ATTR_MIN_HUMIDITY, ATTR_MIN_TEMP, ATTR_PRESET_MODE,
-    ATTR_SWING_MODE, ATTR_TARGET_TEMP_HIGH, ATTR_TARGET_TEMP_LOW,
-    ATTR_TARGET_TEMP_STEP, CURRENT_HVAC_COOL, DOMAIN, HVAC_MODE_COOL)
-from homeassistant.const import (
-    ATTR_ENTITY_ID, ATTR_TEMPERATURE, STATE_OFF, STATE_ON)
+    ATTR_CURRENT_TEMPERATURE, ATTR_FAN_MODE, ATTR_HUMIDITY, ATTR_HVAC_MODES,
+    ATTR_MAX_HUMIDITY, ATTR_MAX_TEMP, ATTR_MIN_HUMIDITY, ATTR_MIN_TEMP,
+    ATTR_PRESET_MODE, ATTR_SWING_MODE, ATTR_TARGET_TEMP_HIGH,
+    ATTR_TARGET_TEMP_LOW, ATTR_TARGET_TEMP_STEP, CURRENT_HVAC_COOL, DOMAIN,
+    HVAC_MODE_COOL, HVAC_MODE_HEAT, HVAC_MODES, PRESET_AWAY, PRESET_ECO)
+from homeassistant.const import ATTR_TEMPERATURE, STATE_OFF, STATE_ON
 from homeassistant.setup import async_setup_component
 from homeassistant.util.unit_system import METRIC_SYSTEM
 
@@ -45,6 +45,7 @@ def test_setup_params(hass):
     assert "Off" == state.attributes.get(ATTR_SWING_MODE)
     assert STATE_OFF == state.attributes.get(ATTR_AUX_HEAT)
     assert 5 == state.attributes.get(ATTR_TARGET_TEMP_STEP)
+    assert state.attributes.get(ATTR_HVAC_MODES) ==HVAC_MODES
 
 
 def test_default_setup_params(hass):
@@ -57,135 +58,155 @@ def test_default_setup_params(hass):
     assert 5 == state.attributes.get(ATTR_TARGET_TEMP_STEP)
 
 
-def test_set_only_target_temp_bad_attr(hass):
+async def test_set_only_target_temp_bad_attr(hass):
     """Test setting the target temperature without required attribute."""
     state = hass.states.get(ENTITY_CLIMATE)
     assert 21 == state.attributes.get(ATTR_TEMPERATURE)
 
     with pytest.raises(vol.Invalid):
-        common.set_temperature(hass, None, ENTITY_CLIMATE)
+        await common.async_set_temperature(hass, None, ENTITY_CLIMATE)
 
-    hass.block_till_done()
+    await hass.async_block_till_done()
     assert 21 == state.attributes.get(ATTR_TEMPERATURE)
 
 
-def test_set_only_target_temp(hass):
+async def test_set_only_target_temp(hass):
     """Test the setting of the target temperature."""
     state = hass.states.get(ENTITY_CLIMATE)
     assert 21 == state.attributes.get(ATTR_TEMPERATURE)
-    common.set_temperature(hass, 30, ENTITY_CLIMATE)
-    hass.block_till_done()
+
+    await common.async_set_temperature(hass, 30, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
     assert 30.0 == state.attributes.get(ATTR_TEMPERATURE)
 
 
-def test_set_only_target_temp_with_convert(hass):
+async def test_set_only_target_temp_with_convert(hass):
     """Test the setting of the target temperature."""
     state = hass.states.get(ENTITY_HEATPUMP)
     assert 20 == state.attributes.get(ATTR_TEMPERATURE)
-    common.set_temperature(hass, 21, ENTITY_HEATPUMP)
-    hass.block_till_done()
+
+    await common.async_set_temperature(hass, 21, ENTITY_HEATPUMP)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_HEATPUMP)
     assert 21.0 == state.attributes.get(ATTR_TEMPERATURE)
 
 
-def test_set_target_temp_range(hass):
+async def test_set_target_temp_range(hass):
     """Test the setting of the target temperature with range."""
     state = hass.states.get(ENTITY_ECOBEE)
     assert state.attributes.get(ATTR_TEMPERATURE) is None
     assert 21.0 == state.attributes.get(ATTR_TARGET_TEMP_LOW)
     assert 24.0 == state.attributes.get(ATTR_TARGET_TEMP_HIGH)
-    common.set_temperature(
+
+    await common.async_set_temperature(
         hass, target_temp_high=25, target_temp_low=20, entity_id=ENTITY_ECOBEE)
-    hass.block_till_done()
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_ECOBEE)
     assert state.attributes.get(ATTR_TEMPERATURE) is None
     assert 20.0 == state.attributes.get(ATTR_TARGET_TEMP_LOW)
     assert 25.0 == state.attributes.get(ATTR_TARGET_TEMP_HIGH)
 
 
-def test_set_target_temp_range_bad_attr(hass):
+async def test_set_target_temp_range_bad_attr(hass):
     """Test setting the target temperature range without attribute."""
     state = hass.states.get(ENTITY_ECOBEE)
     assert state.attributes.get(ATTR_TEMPERATURE) is None
     assert 21.0 == state.attributes.get(ATTR_TARGET_TEMP_LOW)
     assert 24.0 == state.attributes.get(ATTR_TARGET_TEMP_HIGH)
+
     with pytest.raises(vol.Invalid):
-        common.set_temperature(
+        await common.async_set_temperature(
             hass, temperature=None, entity_id=ENTITY_ECOBEE,
             target_temp_low=None, target_temp_high=None)
-    hass.block_till_done()
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_ECOBEE)
     assert state.attributes.get(ATTR_TEMPERATURE) is None
     assert 21.0 == state.attributes.get(ATTR_TARGET_TEMP_LOW)
     assert 24.0 == state.attributes.get(ATTR_TARGET_TEMP_HIGH)
 
 
-def test_set_target_humidity_bad_attr(hass):
+async def test_set_target_humidity_bad_attr(hass):
     """Test setting the target humidity without required attribute."""
     state = hass.states.get(ENTITY_CLIMATE)
     assert 67 == state.attributes.get(ATTR_HUMIDITY)
+
     with pytest.raises(vol.Invalid):
-        common.set_humidity(hass, None, ENTITY_CLIMATE)
-    hass.block_till_done()
+        await common.async_set_humidity(hass, None, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
     assert 67 == state.attributes.get(ATTR_HUMIDITY)
 
 
-def test_set_target_humidity(hass):
+async def test_set_target_humidity(hass):
     """Test the setting of the target humidity."""
     state = hass.states.get(ENTITY_CLIMATE)
     assert 67 == state.attributes.get(ATTR_HUMIDITY)
-    common.set_humidity(hass, 64, ENTITY_CLIMATE)
-    hass.block_till_done()
+
+    await common.async_set_humidity(hass, 64, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
     assert 64.0 == state.attributes.get(ATTR_HUMIDITY)
 
 
-def test_set_fan_mode_bad_attr(hass):
+async def test_set_fan_mode_bad_attr(hass):
     """Test setting fan mode without required attribute."""
     state = hass.states.get(ENTITY_CLIMATE)
     assert "On High" == state.attributes.get(ATTR_FAN_MODE)
+
     with pytest.raises(vol.Invalid):
-        common.set_fan_mode(hass, None, ENTITY_CLIMATE)
-    hass.block_till_done()
+        await common.async_set_fan_mode(hass, None, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
     assert "On High" == state.attributes.get(ATTR_FAN_MODE)
 
 
-def test_set_fan_mode(hass):
+async def test_set_fan_mode(hass):
     """Test setting of new fan mode."""
     state = hass.states.get(ENTITY_CLIMATE)
     assert "On High" == state.attributes.get(ATTR_FAN_MODE)
-    common.set_fan_mode(hass, "On Low", ENTITY_CLIMATE)
-    hass.block_till_done()
+
+    await common.async_set_fan_mode(hass, "On Low", ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
     assert "On Low" == state.attributes.get(ATTR_FAN_MODE)
 
 
-def test_set_swing_mode_bad_attr(hass):
+async def test_set_swing_mode_bad_attr(hass):
     """Test setting swing mode without required attribute."""
     state = hass.states.get(ENTITY_CLIMATE)
     assert "Off" == state.attributes.get(ATTR_SWING_MODE)
+
     with pytest.raises(vol.Invalid):
-        common.set_swing_mode(hass, None, ENTITY_CLIMATE)
-    hass.block_till_done()
+        await common.async_set_swing_mode(hass, None, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
     assert "Off" == state.attributes.get(ATTR_SWING_MODE)
 
 
-def test_set_swing(hass):
+async def test_set_swing(hass):
     """Test setting of new swing mode."""
     state = hass.states.get(ENTITY_CLIMATE)
     assert "Off" == state.attributes.get(ATTR_SWING_MODE)
-    common.set_swing_mode(hass, "Auto", ENTITY_CLIMATE)
-    hass.block_till_done()
+
+    await common.async_set_swing_mode(hass, "Auto", ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
     assert "Auto" == state.attributes.get(ATTR_SWING_MODE)
 
 
-def test_set_operation_bad_attr_and_state(hass):
-    """Test setting operation mode without required attribute.
+async def test_set_hvac_bad_attr_and_state(hass):
+    """Test setting hvac mode without required attribute.
 
     Also check the state.
     """
@@ -194,95 +215,68 @@ def test_set_operation_bad_attr_and_state(hass):
     assert state.state == HVAC_MODE_COOL
 
     with pytest.raises(vol.Invalid):
-        common.set_hvac_mode(hass, None, ENTITY_CLIMATE)
-    hass.block_till_done()
+        await common.async_set_hvac_mode(hass, None, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
 
     state = hass.states.get(ENTITY_CLIMATE)
     assert state.attributes.get(ATTR_CURRENT_HVAC) == CURRENT_HVAC_COOL
     assert state.state == HVAC_MODE_COOL
 
 
-def test_set_operation(hass):
-    """Test setting of new operation mode."""
+async def test_set_hvac(hass):
+    """Test setting of new hvac mode."""
     state = hass.states.get(ENTITY_CLIMATE)
-    assert "cool" == state.attributes.get('operation_mode')
-    assert "cool" == state.state
-    common.set_operation_mode(hass, "heat", ENTITY_CLIMATE)
-    hass.block_till_done()
+    assert state.state == HVAC_MODE_COOL
+
+    await common.async_set_hvac_mode(hass, HVAC_MODE_HEAT, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
-    assert "heat" == state.attributes.get('operation_mode')
-    assert "heat" == state.state
+    assert state.state == HVAC_MODE_HEAT
 
 
-def test_set_away_mode_bad_attr(hass):
-    """Test setting the away mode without required attribute."""
-    state = hass.states.get(ENTITY_CLIMATE)
-    assert 'on' == state.attributes.get('away_mode')
-    common.set_away_mode(hass, None, ENTITY_CLIMATE)
-    hass.block_till_done()
-    assert 'on' == state.attributes.get('away_mode')
-
-
-def test_set_away_mode_on(hass):
-    """Test setting the away mode on/true."""
-    common.set_away_mode(hass, True, ENTITY_CLIMATE)
-    hass.block_till_done()
-    state = hass.states.get(ENTITY_CLIMATE)
-    assert 'on' == state.attributes.get('away_mode')
-
-
-def test_set_away_mode_off(hass):
-    """Test setting the away mode off/false."""
-    common.set_away_mode(hass, False, ENTITY_CLIMATE)
-    hass.block_till_done()
-    state = hass.states.get(ENTITY_CLIMATE)
-    assert 'off' == state.attributes.get('away_mode')
-
-
-def test_set_hold_mode_home(hass):
-    """Test setting the hold mode home."""
-    common.set_hold_mode(hass, 'home', ENTITY_ECOBEE)
-    hass.block_till_done()
-    state = hass.states.get(ENTITY_ECOBEE)
-    assert 'home' == state.attributes.get(ATTR_PRESET_MODE)
-
-
-def test_set_hold_mode_away(hass):
+async def test_set_hold_mode_away(hass):
     """Test setting the hold mode away."""
-    common.set_hold_mode(hass, 'away', ENTITY_ECOBEE)
-    hass.block_till_done()
+    await common.async_set_preset_mode(hass, PRESET_AWAY, ENTITY_ECOBEE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_ECOBEE)
-    assert 'away' == state.attributes.get(ATTR_PRESET_MODE)
+    assert state.attributes.get(ATTR_PRESET_MODE) == PRESET_AWAY
 
 
-def test_set_hold_mode_none(hass):
-    """Test setting the hold mode off/false."""
-    common.set_hold_mode(hass, 'off', ENTITY_ECOBEE)
-    hass.block_till_done()
+async def test_set_hold_mode_eco(hass):
+    """Test setting the hold mode eco."""
+    await common.async_set_preset_mode(hass, PRESET_ECO, ENTITY_ECOBEE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_ECOBEE)
-    assert 'off' == state.attributes.get(ATTR_PRESET_MODE)
+    assert state.attributes.get(ATTR_PRESET_MODE) == PRESET_ECO
 
 
-def test_set_aux_heat_bad_attr(hass):
+async def test_set_aux_heat_bad_attr(hass):
     """Test setting the auxiliary heater without required attribute."""
     state = hass.states.get(ENTITY_CLIMATE)
-    assert 'off' == state.attributes.get(ATTR_AUX_HEAT)
-    common.set_aux_heat(hass, None, ENTITY_CLIMATE)
-    hass.block_till_done()
-    assert 'off' == state.attributes.get(ATTR_AUX_HEAT)
+    assert state.attributes.get(ATTR_AUX_HEAT) == STATE_OFF
+
+    await common.async_set_aux_heat(hass, None, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
+    assert state.attributes.get(ATTR_AUX_HEAT) == STATE_OFF
 
 
-def test_set_aux_heat_on(hass):
+async def test_set_aux_heat_on(hass):
     """Test setting the axillary heater on/true."""
-    common.set_aux_heat(hass, True, ENTITY_CLIMATE)
-    hass.block_till_done()
+    await common.async_set_aux_heat(hass, True, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
-    assert 'on' == state.attributes.get(ATTR_AUX_HEAT)
+    assert state.attributes.get(ATTR_AUX_HEAT) == STATE_ON
 
 
-def test_set_aux_heat_off(hass):
+async def test_set_aux_heat_off(hass):
     """Test setting the auxiliary heater off/false."""
-    common.set_aux_heat(hass, False, ENTITY_CLIMATE)
-    hass.block_till_done()
+    await common.async_set_aux_heat(hass, False, ENTITY_CLIMATE)
+    await hass.async_block_till_done()
+
     state = hass.states.get(ENTITY_CLIMATE)
-    assert 'off' == state.attributes.get(ATTR_AUX_HEAT)
+    assert state.attributes.get(ATTR_AUX_HEAT) == STATE_OFF
