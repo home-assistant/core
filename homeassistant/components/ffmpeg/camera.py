1de0a0bbb95e177f7f109099bb63d785b070f922
Paulus Schoutsen <balloob@gmail.com>
2019-05-23 09:45:30 -0700

    Convert stream source to method (#23905)
    * Convert stream source to method
    
    * Use async with

--- homeassistant/components/ffmpeg/camera.py
+++ homeassistant/components/ffmpeg/camera.py
@@ -47,8 +47,7 @@ class FFmpegCamera(Camera):
         """Return supported features."""
         return SUPPORT_STREAM
 
-    @property
-    def stream_source(self):
+    async def stream_source(self):
         """Return the stream source."""
         return self._input.split(' ')[-1]
 
