79879c6d744b56c36da9ea4b5cacc17182ee5e73
zombielinux <will@sutton-family.org>
2019-05-24 20:30:40 -0400

    Fixed static build errors for PR 24086

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -1,13 +1,8 @@
 # coding: utf-8
 """Constants used by Home Assistant components."""
 MAJOR_VERSION = 0
-<<<<<<< HEAD
 MINOR_VERSION = 94
 PATCH_VERSION = '0.dev0'
-=======
-MINOR_VERSION = 93
-PATCH_VERSION = '2'
->>>>>>> upstream/master
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 REQUIRED_PYTHON_VER = (3, 5, 3)
