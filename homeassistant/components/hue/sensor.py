daaee5e19c3577a0ad1add33930097485f79d537
Cyr-ius <cyr-ius@ipocus.net>
2019-05-26 18:29:34 +0200

    Add dark & daylight attributs for lightlevel sensor

--- homeassistant/components/hue/sensor.py
+++ homeassistant/components/hue/sensor.py
@@ -49,6 +49,8 @@ class HueLightLevel(GenericHueGaugeSensorEntity):
         attributes.update({
             "threshold_dark": self.sensor.tholddark,
             "threshold_offset": self.sensor.tholdoffset,
+            "dark": self.sensor.dark,
+            "daylight": self.sensor.daylight,
         })
         return attributes
 
