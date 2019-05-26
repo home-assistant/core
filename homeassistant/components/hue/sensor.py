ca1bb7658701e7d901dec724bcfe3515e9214ed1
Cyr-ius <cyr-ius@ipocus.net>
2019-05-26 18:04:45 +0200

    Add dark & daylight attribut for Hue LightLevel sensor

--- homeassistant/components/hue/sensor.py
+++ homeassistant/components/hue/sensor.py
@@ -8,7 +8,6 @@ from homeassistant.components.hue.sensor_base import (
 
 LIGHT_LEVEL_NAME_FORMAT = "{} light level"
 TEMPERATURE_NAME_FORMAT = "{} temperature"
-BUTTON_EVENT_NAME_FORMAT = "{}"
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -41,7 +40,7 @@ class HueLightLevel(GenericHueGaugeSensorEntity):
         # scale used because the human eye adjusts to light levels and small
         # changes at low lux levels are more noticeable than at high lux
         # levels.
-        return self.sensor.lightlevel
+        return 10 ** ((self.sensor.lightlevel - 1) / 10000)
 
     @property
     def device_state_attributes(self):
@@ -69,23 +68,3 @@ class HueTemperature(GenericHueGaugeSensorEntity):
             return None
 
         return self.sensor.temperature / 100
-
-class HueSwitch(GenericHueGaugeSensorEntity):
-    """The dimmer sensor entity for a Hue sensor device."""
-
-    @property
-    def state(self):
-        BUTTONS = {
-                    34: "1_click", 16: "2_click", 17: "3_click", 18: "4_click",
-                    1000: "1_click", 2000: "2_click", 3000: "3_click", 4000: "4_click",
-                    1001: "1_hold", 2001: "2_hold", 3001: "3_hold", 4001: "4_hold",
-                    1002: "1_click_up", 2002: "2_click_up", 3002: "3_click_up", 4002: "4_click_up",
-                    1003: "1_hold_up", 2003: "2_hold_up", 3003: "3_hold_up", 4003: "4_hold_up",
-        }
-        """Return the state of the device."""
-        return BUTTONS[self.sensor.buttonevent]
-
-    @property
-    def icon(self) -> str:
-        """Icon to use in the frontend, if any."""
-        return 'mdi:remote'
