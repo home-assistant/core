052658f7b71f4022df8c7eefc89713ac2615cc8d
araczkowski <araczkowski@gmail.com>
2019-05-27 23:45:48 +0200

    20190514.5

--- homeassistant/components/ais_wifi_service/config_flow.py
+++ homeassistant/components/ais_wifi_service/config_flow.py
@@ -13,9 +13,9 @@ G_WIFI_NETWORKS = []
 _LOGGER = logging.getLogger(__name__)
 
 
-def scan_for_new_device(hass, loop) -> []:
+def scan_for_wifi(hass, loop) -> []:
     global G_WIFI_NETWORKS
-    _LOGGER.info('scan_for_new_device, no of try: ' + str(loop))
+    _LOGGER.info('scan_for_wifi, no of try: ' + str(loop))
     # send scan request to frame
     if loop == 0:
         # reset the current status
@@ -23,26 +23,32 @@ def scan_for_new_device(hass, loop) -> []:
     # wait
     time.sleep(4)
     # and check the answer
-    iot_dev = hass.states.get('input_select.ais_android_wifi_network')
-    G_WIFI_NETWORKS = iot_dev.attributes['options']
+    wifi_networks = hass.states.get('input_select.ais_android_wifi_network')
+    G_WIFI_NETWORKS = wifi_networks.attributes['options']
     return G_WIFI_NETWORKS
 
 
-def add_new_device(device, name, network, password, secure_android_id, set_option_30, ais_req_id) -> str:
+def connect_to_wifi(ssid, password) -> str:
     import requests
     from homeassistant.components import ais_ai_service as ais_ai_service
     # send add request to frame
     url = ais_ai_service.G_HTTP_REST_SERVICE_BASE_URL.format("127.0.0.1")
+    _LOGGER.info("connect_to_wifi: " + ssid + " pass: " + password)
     try:
-        requests.post(
-            url + '/command',
-            json={"WifiConnectTheDevice": device, "ip": "127.0.0.1", "WifiNetworkPass": password,
-                  "WifiNetworkSsid": network, "IotName": name, "bsssid": network,
-                  "SecureAndroidId": secure_android_id, "SetOption30": set_option_30, "AisReqId": ais_req_id},
-            timeout=5)
+        requests.post(url + '/command', json={"WifiConnectToSid": ssid, "WifiNetworkPass": password}, timeout=5)
     except Exception as e:
-        _LOGGER.error("add_new_device: " + str(e))
+        _LOGGER.error("connect_to_wifi: " + str(e))
 
+
+def check_wifi_connection(hass, loop) -> []:
+    global G_WIFI_NETWORKS
+    _LOGGER.info('check_wifi_connection, no of try: ' + str(loop))
+    # wait
+    time.sleep(4)
+    # and check the answer
+    net_info = hass.states.get('sensor.ais_android_current_network_info')
+    ssid = net_info.attributes.get('ssid', '')
+    return ssid
 
 @callback
 def configured_service(hass):
@@ -75,7 +81,7 @@ class AisWiFilowHandler(config_entries.ConfigFlow):
         )
 
     async def async_step_one(self, user_input=None):
-        """Step one - plug in the device to power"""
+        """Step one"""
         errors = {}
         if user_input is not None:
             return await self.async_step_search_wifi(user_input=None)
@@ -85,10 +91,10 @@ class AisWiFilowHandler(config_entries.ConfigFlow):
         )
 
     async def async_step_search_wifi(self, user_input=None):
-        """Step three - search the new device in network"""
+        """Step - scan the wifi"""
         errors = {}
         for x in range(0, 7):
-            result = await self.hass.async_add_executor_job(scan_for_new_device, self.hass, x)
+            result = await self.hass.async_add_executor_job(scan_for_wifi, self.hass, x)
             _LOGGER.info("Szukam sieci WiFi: " + str(result))
             if len(result) > 1:
                 return await self.async_step_connect_to_wifi(user_input=None)
@@ -103,24 +109,10 @@ class AisWiFilowHandler(config_entries.ConfigFlow):
     async def async_step_connect_to_wifi(self, user_input=None):
         """Step four - connect to wifi"""
         wifi_network = self.hass.states.get('input_select.ais_android_wifi_network')
-        all_networks = wifi_network.attributes['options']
-        all_24_networks = []
-        for wifi in all_networks:
-            if wifi != ais_global.G_EMPTY_OPTION:
-                # check the frequency
-                wifi_frequency_mhz = wifi.split(';')[-2]
-                if wifi_frequency_mhz.strip().startswith("2"):
-                    all_24_networks.append(wifi)
-
-        # curr network info
-        curr_network = []
-        if ais_global.GLOBAL_MY_WIFI_SSID is not None:
-            curr_network = [ais_global.GLOBAL_MY_WIFI_SSID + " (Twoje aktualne połączenie WiFi)"]
-
-        networks = curr_network + all_24_networks
-        curr_pass = ais_global.GLOBAL_MY_WIFI_PASS
-        if curr_pass is None:
-            curr_pass = ""
+        networks = wifi_network.attributes['options']
+        # remove empty option
+        if networks[0] == ais_global.G_EMPTY_OPTION:
+            networks.pop(0)
 
         errors = {}
 
@@ -134,41 +126,38 @@ class AisWiFilowHandler(config_entries.ConfigFlow):
         if user_input is None:
             data_schema = vol.Schema({
                 vol.Required('networks', default=networks[0]): vol.In(list(networks)),
-                vol.Optional(CONF_PASSWORD, default=curr_pass): str,
-                vol.Required('confirm_key'): bool,
+                vol.Optional(CONF_PASSWORD): str,
             })
 
         else:
-            # validations
-            if not user_input['confirm_key']:
-                errors['confirm_key'] = 'confirm_error'
-
+            password = ''
+            if CONF_PASSWORD in user_input:
+                password = user_input[CONF_PASSWORD]
             data_schema = vol.Schema({
                 vol.Required('networks', default=user_input['networks']): vol.In(list(networks)),
-                vol.Optional(CONF_PASSWORD, default=user_input[CONF_PASSWORD]): str,
-                vol.Required('confirm_key', default=user_input['confirm_key']): bool,
+                vol.Optional(CONF_PASSWORD, default=password): str,
             })
 
             # try to connect
             if errors == {}:
-                # relays are announced as a switch and PWM as a light (default)
-
-                # unique req_id
-                ais_req_id = int(round(time.time() * 1000))
-                _LOGGER.info("add_new_device -> ais_req_id: " + str(ais_req_id))
-                ais_global.set_ais_gate_req(str(ais_req_id), None)
                 # send a request to frame to add the new device
-                network = user_input['networks'].replace('(Twoje aktualne połączenie WiFi)', "").strip()
+                network = user_input['networks']
                 await self.hass.async_add_executor_job(
-                    add_new_device, G_WIFI_NETWORKS[1].split(';')[0], user_input[CONF_NAME], network.split(';')[0],
-                    user_input[CONF_PASSWORD], ais_global.G_AIS_SECURE_ANDROID_ID_DOM, set_option_30, ais_req_id)
+                    connect_to_wifi, network.split(';')[0], password)
                 # request was correctly send, now check and wait for the answer
-                return self.async_abort(reason='add_executed')
+                for x in range(0, 7):
+                    result = await self.hass.async_add_executor_job(check_wifi_connection, self.hass, x)
+                    _LOGGER.info("Spawdzam połączenie z siecią WiFi: " + str(result))
+                    if len(result) > 1:
+                        # return await self.async_step_connect_to_wifi(user_input=None)
+                        return self.async_create_entry(
+                            title="Połączenie z WiFi",
+                            data=user_input,
+                        )
+                    else:
+                        errors = {'base': 'conn_failed'}
 
         return self.async_show_form(
-            step_id='add_device',
+            step_id='connect_to_wifi',
             errors=errors if errors else {},
-            data_schema=data_schema,
-            description_placeholders={
-                'gate_id': ais_global.G_AIS_SECURE_ANDROID_ID_DOM, 'device_id': G_WIFI_NETWORKS[1].split(';')[0]},
-        )
+            data_schema=data_schema)
