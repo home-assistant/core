ce5eaf2e4fe0b93f13b7b5d32a17fdfbf54e9f28
Robert Svensson <kane610@me.com>
2019-05-28 23:10:32 +0200

    Fix tests

--- tests/components/unifi/test_device_tracker.py
+++ tests/components/unifi/test_device_tracker.py
@@ -1,8 +1,6 @@
 """The tests for the Unifi WAP device tracker platform."""
 from unittest import mock
 from datetime import datetime, timedelta
-from pyunifi.controller import APIError
-
 
 import pytest
 import voluptuous as vol
@@ -13,13 +11,20 @@ import homeassistant.components.unifi.device_tracker as unifi
 from homeassistant.const import (CONF_HOST, CONF_USERNAME, CONF_PASSWORD,
                                  CONF_PLATFORM, CONF_VERIFY_SSL,
                                  CONF_MONITORED_CONDITIONS)
+
+from tests.common import mock_coro
+from asynctest import CoroutineMock
+from aiounifi.clients import Clients
+
 DEFAULT_DETECTION_TIME = timedelta(seconds=300)
 
 
 @pytest.fixture
 def mock_ctrl():
     """Mock pyunifi."""
-    with mock.patch('pyunifi.controller.Controller') as mock_control:
+    with mock.patch('aiounifi.Controller') as mock_control:
+        mock_control.return_value.login.return_value = mock_coro()
+        mock_control.return_value.initialize.return_value = mock_coro()
         yield mock_control
 
 
@@ -33,7 +38,7 @@ def mock_scanner():
 
 @mock.patch('os.access', return_value=True)
 @mock.patch('os.path.isfile', mock.Mock(return_value=True))
-def test_config_valid_verify_ssl(hass, mock_scanner, mock_ctrl):
+async def test_config_valid_verify_ssl(hass, mock_scanner, mock_ctrl):
     """Test the setup with a string for ssl_verify.
 
     Representing the absolute path to a CA certificate bundle.
@@ -46,12 +51,9 @@ def test_config_valid_verify_ssl(hass, mock_scanner, mock_ctrl):
             CONF_VERIFY_SSL: "/tmp/unifi.crt"
         })
     }
-    result = unifi.get_scanner(hass, config)
+    result = await unifi.async_get_scanner(hass, config)
     assert mock_scanner.return_value == result
     assert mock_ctrl.call_count == 1
-    assert mock_ctrl.mock_calls[0] == \
-        mock.call('localhost', 'foo', 'password', 8443,
-                  version='v4', site_id='default', ssl_verify="/tmp/unifi.crt")
 
     assert mock_scanner.call_count == 1
     assert mock_scanner.call_args == mock.call(mock_ctrl.return_value,
@@ -59,7 +61,7 @@ def test_config_valid_verify_ssl(hass, mock_scanner, mock_ctrl):
                                                None, None)
 
 
-def test_config_minimal(hass, mock_scanner, mock_ctrl):
+async def test_config_minimal(hass, mock_scanner, mock_ctrl):
     """Test the setup with minimal configuration."""
     config = {
         DOMAIN: unifi.PLATFORM_SCHEMA({
@@ -68,12 +70,10 @@ def test_config_minimal(hass, mock_scanner, mock_ctrl):
             CONF_PASSWORD: 'password',
         })
     }
-    result = unifi.get_scanner(hass, config)
+
+    result = await unifi.async_get_scanner(hass, config)
     assert mock_scanner.return_value == result
     assert mock_ctrl.call_count == 1
-    assert mock_ctrl.mock_calls[0] == \
-        mock.call('localhost', 'foo', 'password', 8443,
-                  version='v4', site_id='default', ssl_verify=True)
 
     assert mock_scanner.call_count == 1
     assert mock_scanner.call_args == mock.call(mock_ctrl.return_value,
@@ -81,7 +81,7 @@ def test_config_minimal(hass, mock_scanner, mock_ctrl):
                                                None, None)
 
 
-def test_config_full(hass, mock_scanner, mock_ctrl):
+async def test_config_full(hass, mock_scanner, mock_ctrl):
     """Test the setup with full configuration."""
     config = {
         DOMAIN: unifi.PLATFORM_SCHEMA({
@@ -96,12 +96,9 @@ def test_config_full(hass, mock_scanner, mock_ctrl):
             'detection_time': 300,
         })
     }
-    result = unifi.get_scanner(hass, config)
+    result = await unifi.async_get_scanner(hass, config)
     assert mock_scanner.return_value == result
     assert mock_ctrl.call_count == 1
-    assert mock_ctrl.call_args == \
-        mock.call('myhost', 'foo', 'password', 123,
-                  version='v4', site_id='abcdef01', ssl_verify=False)
 
     assert mock_scanner.call_count == 1
     assert mock_scanner.call_args == mock.call(
@@ -137,7 +134,7 @@ def test_config_error():
         })
 
 
-def test_config_controller_failed(hass, mock_ctrl, mock_scanner):
+async def test_config_controller_failed(hass, mock_ctrl, mock_scanner):
     """Test for controller failure."""
     config = {
         'device_tracker': {
@@ -146,13 +143,12 @@ def test_config_controller_failed(hass, mock_ctrl, mock_scanner):
             CONF_PASSWORD: 'password',
         }
     }
-    mock_ctrl.side_effect = APIError(
-        '/', 500, 'foo', {}, None)
-    result = unifi.get_scanner(hass, config)
+    mock_ctrl.side_effect = unifi.CannotConnect
+    result = await unifi.async_get_scanner(hass, config)
     assert result is False
 
 
-def test_scanner_update():
+async def test_scanner_update():
     """Test the scanner update."""
     ctrl = mock.MagicMock()
     fake_clients = [
@@ -161,21 +157,20 @@ def test_scanner_update():
         {'mac': '234', 'essid': 'barnet',
          'last_seen': dt_util.as_timestamp(dt_util.utcnow())},
     ]
-    ctrl.get_clients.return_value = fake_clients
-    unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, None, None)
-    assert ctrl.get_clients.call_count == 1
-    assert ctrl.get_clients.call_args == mock.call()
+    ctrl.clients = Clients([], CoroutineMock(return_value=fake_clients))
+    scnr = unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, None, None)
+    await scnr.async_update()
+    assert len(scnr._clients) == 2
 
 
 def test_scanner_update_error():
     """Test the scanner update for error."""
     ctrl = mock.MagicMock()
-    ctrl.get_clients.side_effect = APIError(
-        '/', 500, 'foo', {}, None)
+    ctrl.get_clients.side_effect = unifi.aiounifi.AiounifiException
     unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, None, None)
 
 
-def test_scan_devices():
+async def test_scan_devices():
     """Test the scanning for devices."""
     ctrl = mock.MagicMock()
     fake_clients = [
@@ -184,12 +179,13 @@ def test_scan_devices():
         {'mac': '234', 'essid': 'barnet',
          'last_seen': dt_util.as_timestamp(dt_util.utcnow())},
     ]
-    ctrl.get_clients.return_value = fake_clients
-    scanner = unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, None, None)
-    assert set(scanner.scan_devices()) == set(['123', '234'])
+    ctrl.clients = Clients([], CoroutineMock(return_value=fake_clients))
+    scnr = unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, None, None)
+    await scnr.async_update()
+    assert set(await scnr.async_scan_devices()) == set(['123', '234'])
 
 
-def test_scan_devices_filtered():
+async def test_scan_devices_filtered():
     """Test the scanning for devices based on SSID."""
     ctrl = mock.MagicMock()
     fake_clients = [
@@ -204,13 +200,13 @@ def test_scan_devices_filtered():
     ]
 
     ssid_filter = ['foonet', 'barnet']
-    ctrl.get_clients.return_value = fake_clients
-    scanner = unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, ssid_filter,
-                                 None)
-    assert set(scanner.scan_devices()) == set(['123', '234', '890'])
+    ctrl.clients = Clients([], CoroutineMock(return_value=fake_clients))
+    scnr = unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, ssid_filter, None)
+    await scnr.async_update()
+    assert set(await scnr.async_scan_devices()) == set(['123', '234', '890'])
 
 
-def test_get_device_name():
+async def test_get_device_name():
     """Test the getting of device names."""
     ctrl = mock.MagicMock()
     fake_clients = [
@@ -226,15 +222,16 @@ def test_get_device_name():
          'essid': 'barnet',
          'last_seen': '1504786810'},
     ]
-    ctrl.get_clients.return_value = fake_clients
-    scanner = unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, None, None)
-    assert scanner.get_device_name('123') == 'foobar'
-    assert scanner.get_device_name('234') == 'Nice Name'
-    assert scanner.get_device_name('456') is None
-    assert scanner.get_device_name('unknown') is None
+    ctrl.clients = Clients([], CoroutineMock(return_value=fake_clients))
+    scnr = unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, None, None)
+    await scnr.async_update()
+    assert scnr.get_device_name('123') == 'foobar'
+    assert scnr.get_device_name('234') == 'Nice Name'
+    assert scnr.get_device_name('456') is None
+    assert scnr.get_device_name('unknown') is None
 
 
-def test_monitored_conditions():
+async def test_monitored_conditions():
     """Test the filtering of attributes."""
     ctrl = mock.MagicMock()
     fake_clients = [
@@ -254,16 +251,17 @@ def test_monitored_conditions():
          'essid': 'barnet',
          'last_seen': dt_util.as_timestamp(dt_util.utcnow())},
     ]
-    ctrl.get_clients.return_value = fake_clients
-    scanner = unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, None,
-                                 ['essid', 'signal', 'latest_assoc_time'])
-    assert scanner.get_extra_attributes('123') == {
+    ctrl.clients = Clients([], CoroutineMock(return_value=fake_clients))
+    scnr = unifi.UnifiScanner(ctrl, DEFAULT_DETECTION_TIME, None,
+                              ['essid', 'signal', 'latest_assoc_time'])
+    await scnr.async_update()
+    assert scnr.get_extra_attributes('123') == {
         'essid': 'barnet',
         'signal': -60,
         'latest_assoc_time': datetime(2000, 1, 1, 0, 0, tzinfo=dt_util.UTC)
     }
-    assert scanner.get_extra_attributes('234') == {
+    assert scnr.get_extra_attributes('234') == {
         'essid': 'barnet',
         'signal': -42
     }
-    assert scanner.get_extra_attributes('456') == {'essid': 'barnet'}
+    assert scnr.get_extra_attributes('456') == {'essid': 'barnet'}
