bd4f0a0f45c3fa236629ae7d7604733e91b1d41a
David Roberts <drob@drob.net>
2019-05-27 22:21:49 -0400

    remve unneded date/time

--- homeassistant/components/solaredge_local/sensor.py
+++ homeassistant/components/solaredge_local/sensor.py
@@ -5,7 +5,6 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.solaredge_local/
 """
 
-from datetime import timedelta
 import logging
 
 import voluptuous as vol
