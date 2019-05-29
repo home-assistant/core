ca1a727035d4a2bdc2fb7ae7ae96c163efaeff44
Paulus Schoutsen <paulus@home-assistant.io>
2019-05-29 08:39:12 -0700

    Cloud: Websocket API to manage Google assistant entity config (#24153)
    * Extract exposed devices function
    
    * Add might_2fa info to trait
    
    * Do not filter with should_expose in Google helper func
    
    * Cloud: allow setting if Google entity is exposed
    
    * Allow disabling 2FA via config
    
    * Cloud: allow disabling 2FA
    
    * Lint
    
    * More changes
    
    * Fix typing

--- homeassistant/components/cloud/prefs.py
+++ homeassistant/components/cloud/prefs.py
@@ -4,6 +4,8 @@ from ipaddress import ip_address
 from .const import (
     DOMAIN, PREF_ENABLE_ALEXA, PREF_ENABLE_GOOGLE, PREF_ENABLE_REMOTE,
     PREF_GOOGLE_SECURE_DEVICES_PIN, PREF_CLOUDHOOKS, PREF_CLOUD_USER,
+    PREF_GOOGLE_ENTITY_CONFIGS, PREF_OVERRIDE_NAME, PREF_DISABLE_2FA,
+    PREF_ALIASES, PREF_SHOULD_EXPOSE,
     InvalidTrustedNetworks)
 
 STORAGE_KEY = DOMAIN
@@ -30,6 +32,7 @@ class CloudPreferences:
                 PREF_ENABLE_GOOGLE: True,
                 PREF_ENABLE_REMOTE: False,
                 PREF_GOOGLE_SECURE_DEVICES_PIN: None,
+                PREF_GOOGLE_ENTITY_CONFIGS: {},
                 PREF_CLOUDHOOKS: {},
                 PREF_CLOUD_USER: None,
             }
@@ -39,7 +42,7 @@ class CloudPreferences:
     async def async_update(self, *, google_enabled=_UNDEF,
                            alexa_enabled=_UNDEF, remote_enabled=_UNDEF,
                            google_secure_devices_pin=_UNDEF, cloudhooks=_UNDEF,
-                           cloud_user=_UNDEF):
+                           cloud_user=_UNDEF, google_entity_configs=_UNDEF):
         """Update user preferences."""
         for key, value in (
                 (PREF_ENABLE_GOOGLE, google_enabled),
@@ -48,6 +51,7 @@ class CloudPreferences:
                 (PREF_GOOGLE_SECURE_DEVICES_PIN, google_secure_devices_pin),
                 (PREF_CLOUDHOOKS, cloudhooks),
                 (PREF_CLOUD_USER, cloud_user),
+                (PREF_GOOGLE_ENTITY_CONFIGS, google_entity_configs),
         ):
             if value is not _UNDEF:
                 self._prefs[key] = value
@@ -57,9 +61,48 @@ class CloudPreferences:
 
         await self._store.async_save(self._prefs)
 
+    async def async_update_google_entity_config(
+            self, *, entity_id, override_name=_UNDEF, disable_2fa=_UNDEF,
+            aliases=_UNDEF, should_expose=_UNDEF):
+        """Update config for a Google entity."""
+        entities = self.google_entity_configs
+        entity = entities.get(entity_id, {})
+
+        changes = {}
+        for key, value in (
+                (PREF_OVERRIDE_NAME, override_name),
+                (PREF_DISABLE_2FA, disable_2fa),
+                (PREF_ALIASES, aliases),
+                (PREF_SHOULD_EXPOSE, should_expose),
+        ):
+            if value is not _UNDEF:
+                changes[key] = value
+
+        if not changes:
+            return
+
+        updated_entity = {
+            **entity,
+            **changes,
+        }
+
+        updated_entities = {
+            **entities,
+            entity_id: updated_entity,
+        }
+        await self.async_update(google_entity_configs=updated_entities)
+
     def as_dict(self):
         """Return dictionary version."""
-        return self._prefs
+        return {
+            PREF_ENABLE_ALEXA: self.alexa_enabled,
+            PREF_ENABLE_GOOGLE: self.google_enabled,
+            PREF_ENABLE_REMOTE: self.remote_enabled,
+            PREF_GOOGLE_SECURE_DEVICES_PIN: self.google_secure_devices_pin,
+            PREF_GOOGLE_ENTITY_CONFIGS: self.google_entity_configs,
+            PREF_CLOUDHOOKS: self.cloudhooks,
+            PREF_CLOUD_USER: self.cloud_user,
+        }
 
     @property
     def remote_enabled(self):
@@ -88,6 +131,11 @@ class CloudPreferences:
     def google_secure_devices_pin(self):
         """Return if Google is allowed to unlock locks."""
         return self._prefs.get(PREF_GOOGLE_SECURE_DEVICES_PIN)
+
+    @property
+    def google_entity_configs(self):
+        """Return Google Entity configurations."""
+        return self._prefs.get(PREF_GOOGLE_ENTITY_CONFIGS, {})
 
     @property
     def cloudhooks(self):
