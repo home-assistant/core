e6665ab697f30af4a773f0d2c3fee770e75300cc
Robbie Trencheny <me@robbiet.us>
2019-05-29 09:09:25 -0700

    Fix calling notify.notify with mobile_app targets in play. Fixes #24064 (#24156)

--- homeassistant/components/mobile_app/notify.py
+++ homeassistant/components/mobile_app/notify.py
@@ -89,13 +89,12 @@ class MobileAppNotificationService(BaseNotificationService):
         targets = kwargs.get(ATTR_TARGET)
 
         if not targets:
-            targets = push_registrations(self.hass)
+            targets = push_registrations(self.hass).values()
 
         if kwargs.get(ATTR_DATA) is not None:
             data[ATTR_DATA] = kwargs.get(ATTR_DATA)
 
         for target in targets:
-
             entry = self.hass.data[DOMAIN][DATA_CONFIG_ENTRIES][target]
             entry_data = entry.data
 
