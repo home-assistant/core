07b4dc7a2a23fe9b676ef751b91dfa95dfb3e1b1
Paulus Schoutsen <balloob@gmail.com>
2019-06-02 09:30:18 -0700

    Add restore state to OwnTracks device tracker

--- tests/components/owntracks/test_device_tracker.py
+++ tests/components/owntracks/test_device_tracker.py
@@ -4,13 +4,14 @@ import json
 from asynctest import patch
 import pytest
 
-from homeassistant.components import owntracks
+from homeassistant.components import owntracks, device_tracker
 from homeassistant.const import STATE_NOT_HOME
 from homeassistant.setup import async_setup_component
+from homeassistant.helpers import entity_registry
 
 from tests.common import (
     MockConfigEntry, async_fire_mqtt_message, async_mock_mqtt_component,
-    mock_coro)
+    mock_coro, mock_registry)
 
 USER = 'greg'
 DEVICE = 'phone'
@@ -1491,3 +1492,47 @@ async def test_region_mapping(hass, setup_comp):
 
     await send_message(hass, EVENT_TOPIC, message)
     assert_location_state(hass, 'inner')
+
+
+async def test_restore_state(hass, hass_client):
+    """Test that we can restore state."""
+    entry = MockConfigEntry(domain='owntracks', data={
+        'webhook_id': 'owntracks_test',
+        'secret': 'abcd',
+    })
+    entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    client = await hass_client()
+    resp = await client.post(
+        '/api/webhook/owntracks_test',
+        json=LOCATION_MESSAGE,
+        headers={
+            'X-Limit-u': 'Paulus',
+            'X-Limit-d': 'Pixel',
+        }
+    )
+    assert resp.status == 200
+    await hass.async_block_till_done()
+
+    state_1 = hass.states.get('device_tracker.paulus_pixel')
+    assert state_1 is not None
+
+    await hass.config_entries.async_reload(entry.entry_id)
+    await hass.async_block_till_done()
+
+    state_2 = hass.states.get('device_tracker.paulus_pixel')
+    assert state_2 is not None
+
+    assert state_1 is not state_2
+
+    assert state_1.state == state_2.state
+    assert state_1.name == state_2.name
+    assert state_1.attributes['latitude'] == state_2.attributes['latitude']
+    assert state_1.attributes['longitude'] == state_2.attributes['longitude']
+    assert state_1.attributes['battery_level'] == \
+        state_2.attributes['battery_level']
+    assert state_1.attributes['source_type'] == \
+        state_2.attributes['source_type']
