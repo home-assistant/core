d8c1d7edde4d9a7ff8c10ec38e6aae108b3ff034
GoNzCiD <gonza_cid@hotmail.com>
2019-06-03 22:19:46 +0200

    Update homeassistant/components/traccar/device_tracker.py
    Use [] syntax for keys that are in the validated data.
    
    Co-Authored-By: Otto Winter <otto@otto-winter.com>

--- homeassistant/components/traccar/device_tracker.py
+++ homeassistant/components/traccar/device_tracker.py
@@ -76,7 +76,7 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
     scanner = TraccarScanner(
         api, hass, async_see,
         config.get(CONF_SCAN_INTERVAL, SCAN_INTERVAL),
-        config.get(CONF_MAX_ACCURACY), config[CONF_SKIP_ACCURACY_ON],
+        config[CONF_MAX_ACCURACY], config[CONF_SKIP_ACCURACY_ON],
         config[CONF_MONITORED_CONDITIONS], config[CONF_EVENT])
 
     return await scanner.async_init()
