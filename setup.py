5f025e59808737516b1cfd241b10e417a28cc439
Paulus Schoutsen <balloob@gmail.com>
2019-06-06 13:06:40 -0700

    Fix req

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ REQUIRES = [
     'pytz>=2019.01',
     'pyyaml>=3.13,<4',
     'requests==2.22.0',
-    '   ',
+    'ruamel.yaml==0.15.97',
     'voluptuous==0.11.5',
     'voluptuous-serialize==2.1.0',
 ]
