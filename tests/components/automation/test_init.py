bc91ff0cc3e730d0d74b7bf30bd5cec3f82c841e
Franck Nijhof <frenck@addons.community>
2019-06-08 09:18:24 +0200

    Restore automation last_triggered with initial_state override

--- tests/components/automation/test_init.py
+++ tests/components/automation/test_init.py
@@ -894,3 +894,57 @@ async def test_automation_with_error_in_script(hass, caplog):
     hass.bus.async_fire('test_event')
     await hass.async_block_till_done()
     assert 'Service not found' in caplog.text
+
+@asyncio.coroutine
+def test_automation_restore_last_triggered_with_initial_state(hass):
+    """Ensure last_triggered is restored, even when initial state is set."""
+    time = dt_util.utcnow()
+
+    mock_restore_cache(hass, (
+        State('automation.hello', STATE_ON),
+        State('automation.bye', STATE_ON, {'last_triggered': time}),
+        State('automation.solong', STATE_OFF, {'last_triggered': time}),
+    ))
+
+    config = {automation.DOMAIN: [{
+        'alias': 'hello',
+        'initial_state': 'off',
+        'trigger': {
+            'platform': 'event',
+            'event_type': 'test_event',
+        },
+        'action': {'service': 'test.automation'}
+    }, {
+        'alias': 'bye',
+        'initial_state': 'off',
+        'trigger': {
+            'platform': 'event',
+            'event_type': 'test_event',
+        },
+        'action': {'service': 'test.automation'}
+    }, {
+        'alias': 'solong',
+        'initial_state': 'on',
+        'trigger': {
+            'platform': 'event',
+            'event_type': 'test_event',
+        },
+        'action': {'service': 'test.automation'}
+    }]}
+
+    assert (yield from async_setup_component(hass, automation.DOMAIN, config))
+
+    state = hass.states.get('automation.hello')
+    assert state
+    assert state.state == STATE_OFF
+    assert state.attributes.get('last_triggered') is None
+
+    state = hass.states.get('automation.bye')
+    assert state
+    assert state.state == STATE_OFF
+    assert state.attributes.get('last_triggered') == time
+
+    state = hass.states.get('automation.solong')
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes.get('last_triggered') == time
