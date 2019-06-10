1ef8a52d2d2685dc8c40b3150833ea034e1d6235
Ben Lebherz <git@benleb.de>
2019-06-10 13:15:40 +0200

    fix bug which prevented updating the sensors

--- homeassistant/components/surepetcare/__init__.py
+++ homeassistant/components/surepetcare/__init__.py
@@ -7,10 +7,12 @@ from homeassistant.const import (CONF_DEVICE_CLASS, CONF_ICON, CONF_ID,
                                  CONF_TYPE, CONF_USERNAME)
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.dispatcher import async_dispatcher_send
+from homeassistant.helpers.event import async_track_time_interval
 
 from .const import (CONF_FLAPS, CONF_HOUSEHOLD_ID, CONF_PETS,
                     DATA_SURE_PETCARE, DATA_SUREPY, DEFAULT_SCAN_INTERVAL,
-                    DOMAIN, SURE_IDS, SureThingID)
+                    DOMAIN, SURE_IDS, TOPIC_UPDATE, SureThingID)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -58,7 +60,10 @@ async def async_setup(hass, config):
 
     # shared data
     hass.data[DOMAIN] = {}
+    hass.data[DOMAIN][CONF_SCAN_INTERVAL] = conf[CONF_SCAN_INTERVAL]
     hass.data[DATA_SURE_PETCARE] = {}
+    hass.data[DATA_SURE_PETCARE][CONF_USERNAME] = CONF_USERNAME
+    hass.data[DATA_SURE_PETCARE][CONF_PASSWORD] = CONF_PASSWORD
     hass.data[DATA_SURE_PETCARE][CONF_HOUSEHOLD_ID] = conf[CONF_HOUSEHOLD_ID]
     hass.data[DATA_SURE_PETCARE][SureThingID.FLAP.name] = dict()
     hass.data[DATA_SURE_PETCARE][SureThingID.PET.name] = dict()
@@ -85,6 +90,10 @@ async def async_setup(hass, config):
             CONF_TYPE: SureThingID.PET.name,
         } for pet in conf[CONF_PETS]])
 
+    spc_api = SurePetcareAPI(hass)
+
+    async_track_time_interval(hass, spc_api.async_update, SCAN_INTERVAL)
+
     # load platforms
     await hass.helpers.discovery.async_load_platform(
         'binary_sensor', DOMAIN, {}, config)
@@ -92,3 +101,49 @@ async def async_setup(hass, config):
         'sensor', DOMAIN, {}, config)
 
     return True
+
+
+class SurePetcareAPI:
+    """Define a generic Sure Petcare object."""
+
+    def __init__(self, hass):
+        """Initialize the Sure Petcare object."""
+        self._hass = hass
+
+    async def async_update(self, args):
+        """Refresh Sure Petcare data."""
+        from surepy import SurePetcareError
+
+        if self._hass.data[DATA_SURE_PETCARE][DATA_SUREPY]:
+            surepy = self._hass.data[DATA_SURE_PETCARE][DATA_SUREPY]
+        else:
+            from surepy import SurePetcare
+            surepy = SurePetcare(
+                self._hass.data[DATA_SURE_PETCARE][CONF_USERNAME],
+                self._hass.data[DATA_SURE_PETCARE][CONF_PASSWORD],
+                self._hass.data[DATA_SURE_PETCARE][CONF_HOUSEHOLD_ID],
+                self._hass.loop, async_get_clientsession(self._hass),
+                debug=True)
+
+        if SureThingID.FLAP.name not in self._hass.data[DATA_SURE_PETCARE]:
+            self._hass.data[DATA_SURE_PETCARE][SureThingID.FLAP.name] = dict()
+
+        if SureThingID.PET.name not in self._hass.data[DATA_SURE_PETCARE]:
+            self._hass.data[DATA_SURE_PETCARE][SureThingID.PET.name] = dict()
+
+        for thing in self._hass.data[DATA_SURE_PETCARE][SURE_IDS]:
+            sure_id = thing[CONF_ID]
+            sure_type = thing[CONF_TYPE]
+
+            try:
+                if sure_type == SureThingID.FLAP.name:
+                    self._hass.data[DATA_SURE_PETCARE][sure_type][
+                        sure_id] = await surepy.get_flap_data(sure_id)
+                elif sure_type == SureThingID.PET.name:
+                    self._hass.data[DATA_SURE_PETCARE][sure_type][
+                        sure_id] = await surepy.get_pet_data(sure_id)
+            except SurePetcareError as error:
+                _LOGGER.debug(
+                    "Unable to retrieve data from surepetcare.io: %s", error)
+
+        async_dispatcher_send(self._hass, TOPIC_UPDATE)
