jurriaan/home-assistant

Support ZLO device types
Support the device types that are added in https://github.com/zigpy/zigpy/pull/176
so newer Zigbee devices can be supported.

homeassistant/components/zha/core/registries.py
@@ -121,6 +121,13 @@ def establish_device_mappings():
         zha.DeviceType.ON_OFF_LIGHT_SWITCH: BINARY_SENSOR,
         zha.DeviceType.DIMMER_SWITCH: BINARY_SENSOR,
         zha.DeviceType.COLOR_DIMMER_SWITCH: BINARY_SENSOR,
+        zha.DeviceType.ON_OFF_BALLAST: SWITCH,
+        zha.DeviceType.DIMMABLE_BALLAST: LIGHT,
+        zha.DeviceType.ON_OFF_PLUG_IN_UNIT: SWITCH,
+        zha.DeviceType.DIMMABLE_PLUG_IN_UNIT: LIGHT,
+        zha.DeviceType.COLOR_TEMPERATURE_LIGHT: LIGHT,
+        zha.DeviceType.EXTENDED_COLOR_LIGHT: LIGHT,
+        zha.DeviceType.ON_OFF_SENSOR: BINARY_SENSOR,
     })
 
     DEVICE_CLASS[zll.PROFILE_ID].update({

home-assistant/core

homeassistant/components/zha/core/registries.py
@@ -115,7 +115,17 @@ def establish_device_mappings():
         zha.DeviceType.LEVEL_CONTROLLABLE_OUTPUT: LIGHT,
         zha.DeviceType.ON_OFF_LIGHT: LIGHT,
         zha.DeviceType.DIMMABLE_LIGHT: LIGHT,
-        zha.DeviceType.COLOR_DIMMABLE_LIGHT: LIGHT
+        zha.DeviceType.COLOR_DIMMABLE_LIGHT: LIGHT,
+        zha.DeviceType.ON_OFF_LIGHT_SWITCH: BINARY_SENSOR,
+        zha.DeviceType.DIMMER_SWITCH: BINARY_SENSOR,
+        zha.DeviceType.COLOR_DIMMER_SWITCH: BINARY_SENSOR,
+        zha.DeviceType.ON_OFF_BALLAST: SWITCH,
+        zha.DeviceType.DIMMABLE_BALLAST: LIGHT,
+        zha.DeviceType.ON_OFF_PLUG_IN_UNIT: SWITCH,
+        zha.DeviceType.DIMMABLE_PLUG_IN_UNIT: LIGHT,
+        zha.DeviceType.COLOR_TEMPERATURE_LIGHT: LIGHT,
+        zha.DeviceType.EXTENDED_COLOR_LIGHT: LIGHT,
+        zha.DeviceType.ON_OFF_SENSOR: BINARY_SENSOR
     })
 
     DEVICE_CLASS[zll.PROFILE_ID].update({