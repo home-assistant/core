d48a3813ad6cc913f54fb90cbe0b72a1023e7395
David Bonnes <david@bonnes.me>
2019-06-15 19:32:41 +0100

    refactor binary_sensor to simplify

--- homeassistant/components/incomfort/binary_sensor.py
+++ homeassistant/components/incomfort/binary_sensor.py
@@ -9,25 +9,19 @@ from . import DOMAIN
 async def async_setup_platform(hass, config, async_add_entities,
                                discovery_info=None):
     """Set up an InComfort/InTouch binary_sensor device."""
-    client = hass.data[DOMAIN]['client']
-    heater = hass.data[DOMAIN]['heater']
-
     async_add_entities([
-        IncomfortFailed(client, heater)
+        IncomfortFailed(hass.data[DOMAIN]['client'],
+                        hass.data[DOMAIN]['heater'])
     ])
 
 
-class IncomfortBinarySensor(BinarySensorDevice):
-    """Representation of an InComfort/InTouch binary_sensor device."""
+class IncomfortFailed(IncomfortBinarySensor):
+    """Representation of an InComfort Failed sensor."""
 
     def __init__(self, client, boiler):
         """Initialize the binary sensor."""
         self._client = client
         self._boiler = boiler
-
-        self._name = None
-        self._is_on_key = None
-        self._other_key = None
 
     async def async_added_to_hass(self):
         """Set up a listener when this entity is added to HA."""
@@ -40,32 +34,20 @@ class IncomfortBinarySensor(BinarySensorDevice):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._name
+        return 'Fault state'
 
     @property
     def is_on(self):
         """Return the status of the sensor."""
-        return self._boiler.status[self._is_on_key]
+        return self._boiler.status['is_failed']
 
     @property
     def device_state_attributes(self):
         """Return the device state attributes."""
-        value = self._boiler.status[self._other_key] if self.is_on else None
-        return {self._other_key: value}
+        return {'fault_code': self._boiler.status['fault_code']}
 
     @property
     def should_poll(self) -> bool:
         """Return False as this device should never be polled."""
         return False
 
-
-class IncomfortFailed(IncomfortBinarySensor):
-    """Representation of an InComfort Failed sensor."""
-
-    def __init__(self, client, boiler):
-        """Initialize the binary sensor."""
-        super().__init__(client, boiler)
-
-        self._name = 'Failed'
-        self._is_on_key = 'is_failed'
-        self._other_key = 'fault_code'
