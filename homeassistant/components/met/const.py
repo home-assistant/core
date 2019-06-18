f4e8ff61dd1d8d948f2f473133bbfc0cf71856f1
Aaron Bach <bachya1208@gmail.com>
2019-06-18 16:10:02 -0600

    Add config entry for Met.no

---
+++ homeassistant/components/met/const.py
@@ -0,0 +1,14 @@
+"""Constants for Met component."""
+import logging
+
+from homeassistant.components.weather import DOMAIN as WEATHER_DOMAIN
+
+DOMAIN = 'met'
+
+HOME_LOCATION_NAME = 'Home'
+
+ENTITY_ID_SENSOR_FORMAT = WEATHER_DOMAIN + ".met_{}"
+ENTITY_ID_SENSOR_FORMAT_HOME = ENTITY_ID_SENSOR_FORMAT.format(
+    HOME_LOCATION_NAME)
+
+_LOGGER = logging.getLogger('.')
