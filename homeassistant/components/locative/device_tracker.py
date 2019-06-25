726e8ac8622b09ab156dc81760bcc8042776d9a3
Martin Hjelmare <marhje52@kth.se>
2019-06-25 05:00:28 +0200

    Fix locative device update (#24744)
    * Add a test for two devices
    
    * Fix locative updating all devices
    
    * Add a guard clause that checks if correct device is passed.

--- homeassistant/components/locative/device_tracker.py
+++ homeassistant/components/locative/device_tracker.py
@@ -85,6 +85,8 @@ class LocativeEntity(DeviceTrackerEntity):
     @callback
     def _async_receive_data(self, device, location, location_name):
         """Update device data."""
+        if device != self._name:
+            return
         self._location_name = location_name
         self._location = location
         self.async_write_ha_state()
