c12a278a54845fb55c99745cbe37480fd4de2dd8
David Mulcahey <david.mulcahey@me.com>
2019-07-01 14:03:32 -0400

    go back to signals and no hard entity references

--- homeassistant/components/zha/core/const.py
+++ homeassistant/components/zha/core/const.py
@@ -103,6 +103,7 @@ SIGNAL_MOVE_LEVEL = "move_level"
 SIGNAL_SET_LEVEL = "set_level"
 SIGNAL_STATE_ATTR = "update_state_attribute"
 SIGNAL_AVAILABLE = 'available'
+SIGNAL_REMOVE = 'remove'
 
 QUIRK_APPLIED = 'quirk_applied'
 QUIRK_CLASS = 'quirk_class'
