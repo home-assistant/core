47c56d3bcaf52d53853fd9073508a31bfa0877dd
Aaron Bach <bachya1208@gmail.com>
2019-07-06 23:07:44 -0600

    Add support for World Wide Lightning Location Network

---
+++ homeassistant/components/wwlln/const.py
@@ -0,0 +1,6 @@
+"""Define constants for the WWLLN integration."""
+DOMAIN = 'wwlln'
+
+DATA_CLIENT = 'client'
+
+DEFAULT_RADIUS = 25
--- homeassistant/generated/config_flows.py
+++ homeassistant/generated/config_flows.py
@@ -56,6 +56,7 @@ FLOWS = [
     "unifi",
     "upnp",
     "wemo",
+    "wwlln",
     "zha",
     "zone",
     "zwave"
