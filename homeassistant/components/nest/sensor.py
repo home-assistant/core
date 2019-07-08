6e53b09e7cdfd2f03f264953a00f8921b8640cc0
Paulus Schoutsen <balloob@gmail.com>
2019-07-08 14:12:02 -0700

    Fix Nest sensor (#25023)

--- homeassistant/components/nest/sensor.py
+++ homeassistant/components/nest/sensor.py
@@ -7,7 +7,7 @@ from homeassistant.const import (
 
 from . import CONF_SENSORS, DATA_NEST, DATA_NEST_CONFIG, NestSensorDevice
 
-SENSOR_TYPES = ['humidity', 'operation_mode', 'hvac_mode']
+SENSOR_TYPES = ['humidity', 'operation_mode', 'hvac_state']
 
 TEMP_SENSOR_TYPES = ['temperature', 'target']
 
@@ -36,7 +36,7 @@ SENSOR_DEVICE_CLASSES = {'humidity': DEVICE_CLASS_HUMIDITY}
 VARIABLE_NAME_MAPPING = {'eta': 'eta_begin', 'operation_mode': 'mode'}
 
 VALUE_MAPPING = {
-    'hvac_mode': {
+    'hvac_state': {
         'heating': STATE_HEAT, 'cooling': STATE_COOL, 'off': STATE_OFF}}
 
 SENSOR_TYPES_DEPRECATED = ['last_ip',
