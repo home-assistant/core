191f5df31418a46e779c08d3be3293bab9c6e145
Unknown <rendili@outlook.com>
2019-07-08 21:26:20 +0100

    Update water_heater

--- homeassistant/components/hive/water_heater.py
+++ homeassistant/components/hive/water_heater.py
@@ -1,4 +1,5 @@
 """Support for hive water heaters."""
+from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS, TEMP_FAHRENHEIT
 
 from homeassistant.components.water_heater import (
     STATE_ECO, STATE_ON, STATE_OFF, SUPPORT_OPERATION_MODE, WaterHeaterDevice)
@@ -9,13 +10,13 @@ SUPPORT_FLAGS_HEATER = (SUPPORT_OPERATION_MODE)
 
 HIVE_TO_HASS_STATE = {
     'SCHEDULE': STATE_ECO,
-    'MANUAL': STATE_ON,
+    'ON': STATE_ON,
     'OFF': STATE_OFF,
 }
 
 HASS_TO_HIVE_STATE = {
     STATE_ECO: 'SCHEDULE',
-    STATE_ON: 'MANUAL',
+    STATE_ON: 'ON',
     STATE_OFF: 'OFF',
 }
 
@@ -26,7 +27,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Wink water heater devices."""
     if discovery_info is None:
         return
-    if discovery_info["HA_DeviceType"] != "Hotwater":
+    if discovery_info["HA_DeviceType"] != "HotWater":
         return
 
     session = hass.data.get(DATA_HIVE)
@@ -48,6 +49,7 @@ class HiveWaterHeater(WaterHeaterDevice):
         self.data_updatesource = '{}.{}'.format(
             self.device_type, self.node_id)
         self._unique_id = '{}-{}'.format(self.node_id, self.device_type)
+        self._unit_of_measurement = TEMP_CELSIUS
         self.session.entities.append(self)
 
     @property
@@ -78,12 +80,19 @@ class HiveWaterHeater(WaterHeaterDevice):
     @property
     def name(self):
         """Return the name of the water heater """
-        return 'HotWater'
+        if self.node_name is None:
+            self.node_name = "Hot Water"
+        return self.node_name
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        return self._unit_of_measurement
 
     @property
     def current_operation(self):
         """ Return current operation. """
-        return HIVE_TO_HASS_STATE[self.session.hotwater.get_state(self.node_id)]
+        return HIVE_TO_HASS_STATE[self.session.hotwater.get_mode(self.node_id)]
 
     @property
     def operation_list(self):
