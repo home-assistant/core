83b80b68b0b25394f08d749b86a58d3fe7420e90
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-07-12 01:34:34 -0700

    add COMMANDS to const

--- homeassistant/components/ps4/const.py
+++ homeassistant/components/ps4/const.py
@@ -5,5 +5,8 @@ DEFAULT_ALIAS = 'Home-Assistant'
 DOMAIN = 'ps4'
 PS4_DATA = 'ps4_data'
 
+COMMANDS = (
+    'up', 'down', 'right', 'left', 'enter', 'back', 'option', 'ps')
+
 # Deprecated used for logger/backwards compatibility from 0.89
 REGIONS = ['R1', 'R2', 'R3', 'R4', 'R5']
