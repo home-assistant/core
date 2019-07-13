62763502b688036bba53d88a5d7f8d6ca335fc03
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-07-12 18:46:49 -0700

    Blank line

--- homeassistant/components/ps4/media_player.py
+++ homeassistant/components/ps4/media_player.py
@@ -32,6 +32,7 @@ MEDIA_IMAGE_DEFAULT = None
 
 DEFAULT_RETRIES = 2
 
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up PS4 from a config entry."""
     config = config_entry
