ba8f45979bd017d554f77ddef12896e9ad53ac97
Paulus Schoutsen <paulus@home-assistant.io>
2019-07-17 15:15:29 -0700

    Move call to async_added_to_hass

--- homeassistant/components/hive/climate.py
+++ homeassistant/components/hive/climate.py
@@ -51,8 +51,7 @@ class HiveClimateEntity(ClimateDevice):
         self.data_updatesource = '{}.{}'.format(
             self.device_type, self.node_id)
         self._unique_id = '{}-{}'.format(self.node_id, self.device_type)
-        self.session.entities.append(self)
-
+        
     @property
     def unique_id(self):
         """Return unique ID of entity."""
@@ -143,6 +142,11 @@ class HiveClimateEntity(ClimateDevice):
     def preset_modes(self):
         """Return a list of available preset modes."""
         return SUPPORT_PRESET
+
+    async def async_added_to_hass(self):
+        """When entity is added to Home Assistant."""
+        await super().async_added_to_hass()
+        self.session.entities.append(self)
 
     def set_hvac_mode(self, hvac_mode):
         """Set new target hvac mode."""
