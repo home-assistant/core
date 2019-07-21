e6bbe21dd616fbe538ea15a6cdf8ca251804fe6d
Pierre Ståhl <pierre.staahl@gmail.com>
2019-07-21 22:31:11 +0200

    Add add_torrent service to Transmission (#25144)
    * Add add_torrent service to Transmission
    
    * Fix services.yaml format
    
    * Verify that torrent is whitelisted
    
    * Add logging if adding failed
    
    * Change warn to warning

--- homeassistant/components/transmission/__init__.py
+++ homeassistant/components/transmission/__init__.py
@@ -34,6 +34,14 @@ SENSOR_TYPES = {
 
 DEFAULT_SCAN_INTERVAL = timedelta(seconds=120)
 
+ATTR_TORRENT = 'torrent'
+
+SERVICE_ADD_TORRENT = 'add_torrent'
+
+SERVICE_ADD_TORRENT_SCHEMA = vol.Schema({
+    vol.Required(ATTR_TORRENT): cv.string,
+})
+
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_HOST): cv.string,
@@ -81,6 +89,19 @@ def setup(hass, config):
         tm_data.update()
 
     track_time_interval(hass, refresh, scan_interval)
+
+    def add_torrent(service):
+        """Add new torrent to download."""
+        torrent = service.data[ATTR_TORRENT]
+        if torrent.startswith(('http', 'ftp:', 'magnet:')) or \
+           hass.config.is_allowed_path(torrent):
+            api.add_torrent(torrent)
+        else:
+            _LOGGER.warning('Could not add torrent: '
+                            'unsupported type or no permission')
+
+    hass.services.register(DOMAIN, SERVICE_ADD_TORRENT, add_torrent,
+                           schema=SERVICE_ADD_TORRENT_SCHEMA)
 
     sensorconfig = {
         'sensors': config[DOMAIN][CONF_MONITORED_CONDITIONS],
