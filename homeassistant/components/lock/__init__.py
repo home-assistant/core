6d84d017db606e50f2815e99458da9b7dc22e330
Aaron Bach <bachya1208@gmail.com>
2019-07-23 12:06:07 -0600

    extend

--- homeassistant/components/lock/__init__.py
+++ homeassistant/components/lock/__init__.py
@@ -28,7 +28,7 @@ GROUP_NAME_ALL_LOCKS = 'all locks'
 
 MIN_TIME_BETWEEN_SCANS = timedelta(seconds=10)
 
-LOCK_SERVICE_SCHEMA = ENTITY_SERVICE_SCHEMA({
+LOCK_SERVICE_SCHEMA = ENTITY_SERVICE_SCHEMA.extend({
     vol.Optional(ATTR_CODE): cv.string,
 })
 
