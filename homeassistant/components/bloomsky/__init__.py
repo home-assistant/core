10b120f11f9f001aaefeb3a60f92df77e1bc4721
Santobert <tobhaase@gmail.com>
2019-07-24 19:37:36 +0200

    Fix bloomsky unit system (#25460)
    * initial commit - fix bloomsky unit system
    
    * Add another warning
    
    * Fix linting error
    
    * Include metric sensor units
    
    * Shorten a too long line

--- homeassistant/components/bloomsky/__init__.py
+++ homeassistant/components/bloomsky/__init__.py
@@ -35,7 +35,7 @@ def setup(hass, config):
 
     global BLOOMSKY
     try:
-        BLOOMSKY = BloomSky(api_key)
+        BLOOMSKY = BloomSky(api_key, hass.config.units.is_metric)
     except RuntimeError:
         return False
 
@@ -51,10 +51,12 @@ class BloomSky:
     # API documentation at http://weatherlution.com/bloomsky-api/
     API_URL = 'http://api.bloomsky.com/api/skydata'
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, is_metric):
         """Initialize the BookSky."""
         self._api_key = api_key
+        self._endpoint_argument = 'unit=intl' if is_metric else ''
         self.devices = {}
+        self.is_metric = is_metric
         _LOGGER.debug("Initial BloomSky device load...")
         self.refresh_devices()
 
@@ -63,9 +65,13 @@ class BloomSky:
         """Use the API to retrieve a list of devices."""
         _LOGGER.debug("Fetching BloomSky update")
         response = requests.get(
-            self.API_URL, headers={AUTHORIZATION: self._api_key}, timeout=10)
+            "{}?{}".format(self.API_URL, self._endpoint_argument),
+            headers={AUTHORIZATION: self._api_key}, timeout=10)
         if response.status_code == 401:
             raise RuntimeError("Invalid API_KEY")
+        if response.status_code == 405:
+            _LOGGER.error("You have no bloomsky devices configured")
+            return
         if response.status_code != 200:
             _LOGGER.error("Invalid HTTP response: %s", response.status_code)
             return
