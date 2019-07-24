e254987fd9cbae39f12da085b0bbd25e1cc32118
Tobias Haase <tobhaase@gmail.com>
2019-07-24 15:01:54 +0200

    initial commit - fix bloomsky unit system

--- homeassistant/components/bloomsky/__init__.py
+++ homeassistant/components/bloomsky/__init__.py
@@ -35,7 +35,7 @@ def setup(hass, config):
 
     global BLOOMSKY
     try:
-        BLOOMSKY = BloomSky(api_key)
+        BLOOMSKY = BloomSky(api_key, hass.config.units.is_metric)
     except RuntimeError:
         return False
 
@@ -51,9 +51,10 @@ class BloomSky:
     # API documentation at http://weatherlution.com/bloomsky-api/
     API_URL = 'http://api.bloomsky.com/api/skydata'
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, is_metric):
         """Initialize the BookSky."""
         self._api_key = api_key
+        self.endpoint_argument = 'unit=intl' if is_metric else ''
         self.devices = {}
         _LOGGER.debug("Initial BloomSky device load...")
         self.refresh_devices()
@@ -63,10 +64,11 @@ class BloomSky:
         """Use the API to retrieve a list of devices."""
         _LOGGER.debug("Fetching BloomSky update")
         response = requests.get(
-            self.API_URL, headers={AUTHORIZATION: self._api_key}, timeout=10)
+            "{}?{}".format(self.API_URL, self.endpoint_argument),
+            headers={AUTHORIZATION: self._api_key}, timeout=10)
         if response.status_code == 401:
             raise RuntimeError("Invalid API_KEY")
-        if response.status_code != 200:
+        elif response.status_code != 200:
             _LOGGER.error("Invalid HTTP response: %s", response.status_code)
             return
         # Create dictionary keyed off of the device unique id
