3de3fa7b695ce35def60eef4a43e077e0e7f6a54
Aaron Bach <bachya1208@gmail.com>
2019-07-24 00:29:08 -0600

    Add area support to vacuum service schemas (#25443)
    * Add area support to vacuum service schemas
    
    * Fixed tests
    
    * De-couple platform schemas

--- homeassistant/components/vacuum/__init__.py
+++ homeassistant/components/vacuum/__init__.py
@@ -7,12 +7,12 @@ import voluptuous as vol
 
 from homeassistant.components import group
 from homeassistant.const import (
-    ATTR_BATTERY_LEVEL, ATTR_COMMAND, ATTR_ENTITY_ID, SERVICE_TOGGLE,
-    SERVICE_TURN_OFF, SERVICE_TURN_ON, STATE_ON, STATE_PAUSED, STATE_IDLE)
+    ATTR_BATTERY_LEVEL, ATTR_COMMAND, SERVICE_TOGGLE, SERVICE_TURN_OFF,
+    SERVICE_TURN_ON, STATE_ON, STATE_PAUSED, STATE_IDLE)
 from homeassistant.loader import bind_hass
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.config_validation import (  # noqa
-    PLATFORM_SCHEMA, PLATFORM_SCHEMA_BASE)
+    ENTITY_SERVICE_SCHEMA, PLATFORM_SCHEMA, PLATFORM_SCHEMA_BASE)
 from homeassistant.helpers.entity_component import EntityComponent
 from homeassistant.helpers.entity import (ToggleEntity, Entity)
 from homeassistant.helpers.icon import icon_for_battery_level
@@ -42,15 +42,11 @@ SERVICE_START = 'start'
 SERVICE_PAUSE = 'pause'
 SERVICE_STOP = 'stop'
 
-VACUUM_SERVICE_SCHEMA = vol.Schema({
-    vol.Optional(ATTR_ENTITY_ID): cv.comp_entity_ids,
-})
-
-VACUUM_SET_FAN_SPEED_SERVICE_SCHEMA = VACUUM_SERVICE_SCHEMA.extend({
+VACUUM_SET_FAN_SPEED_SERVICE_SCHEMA = ENTITY_SERVICE_SCHEMA.extend({
     vol.Required(ATTR_FAN_SPEED): cv.string,
 })
 
-VACUUM_SEND_COMMAND_SERVICE_SCHEMA = VACUUM_SERVICE_SCHEMA.extend({
+VACUUM_SEND_COMMAND_SERVICE_SCHEMA = ENTITY_SERVICE_SCHEMA.extend({
     vol.Required(ATTR_COMMAND): cv.string,
     vol.Optional(ATTR_PARAMS): vol.Any(dict, cv.ensure_list),
 })
@@ -95,43 +91,43 @@ async def async_setup(hass, config):
     await component.async_setup(config)
 
     component.async_register_entity_service(
-        SERVICE_TURN_ON, VACUUM_SERVICE_SCHEMA,
+        SERVICE_TURN_ON, ENTITY_SERVICE_SCHEMA,
         'async_turn_on'
     )
     component.async_register_entity_service(
-        SERVICE_TURN_OFF, VACUUM_SERVICE_SCHEMA,
+        SERVICE_TURN_OFF, ENTITY_SERVICE_SCHEMA,
         'async_turn_off'
     )
     component.async_register_entity_service(
-        SERVICE_TOGGLE, VACUUM_SERVICE_SCHEMA,
+        SERVICE_TOGGLE, ENTITY_SERVICE_SCHEMA,
         'async_toggle'
     )
     component.async_register_entity_service(
-        SERVICE_START_PAUSE, VACUUM_SERVICE_SCHEMA,
+        SERVICE_START_PAUSE, ENTITY_SERVICE_SCHEMA,
         'async_start_pause'
     )
     component.async_register_entity_service(
-        SERVICE_START, VACUUM_SERVICE_SCHEMA,
+        SERVICE_START, ENTITY_SERVICE_SCHEMA,
         'async_start'
     )
     component.async_register_entity_service(
-        SERVICE_PAUSE, VACUUM_SERVICE_SCHEMA,
+        SERVICE_PAUSE, ENTITY_SERVICE_SCHEMA,
         'async_pause'
     )
     component.async_register_entity_service(
-        SERVICE_RETURN_TO_BASE, VACUUM_SERVICE_SCHEMA,
+        SERVICE_RETURN_TO_BASE, ENTITY_SERVICE_SCHEMA,
         'async_return_to_base'
     )
     component.async_register_entity_service(
-        SERVICE_CLEAN_SPOT, VACUUM_SERVICE_SCHEMA,
+        SERVICE_CLEAN_SPOT, ENTITY_SERVICE_SCHEMA,
         'async_clean_spot'
     )
     component.async_register_entity_service(
-        SERVICE_LOCATE, VACUUM_SERVICE_SCHEMA,
+        SERVICE_LOCATE, ENTITY_SERVICE_SCHEMA,
         'async_locate'
     )
     component.async_register_entity_service(
-        SERVICE_STOP, VACUUM_SERVICE_SCHEMA,
+        SERVICE_STOP, ENTITY_SERVICE_SCHEMA,
         'async_stop'
     )
     component.async_register_entity_service(
