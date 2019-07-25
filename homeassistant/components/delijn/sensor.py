cc58f0511c0140aaa00f845448aabefd78406cda
Bram Goolaerts <dev@bollewolle.be>
2019-07-25 12:35:37 +0200

    changes to variable naming, setting attribution and states, plus some smaller optimizations

--- homeassistant/components/delijn/sensor.py
+++ homeassistant/components/delijn/sensor.py
@@ -1,54 +1,64 @@
 """Support for De Lijn (Flemish public transport) information."""
-import asyncio
 import logging
 
+from pydelijn.api import Passages
+import pytz
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.exceptions import PlatformNotReady
+from homeassistant.const import (
+    ATTR_ATTRIBUTION, CONF_API_KEY, DEVICE_CLASS_TIMESTAMP)
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
+import homeassistant.util.dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_NEXT_PASSAGE = 'nextpassage'
+ATTRIBUTION = "Data provided by data.delijn.be"
+
+CONF_NEXT_DEPARTURE = 'next_departure'
 CONF_STOP_ID = 'stop_id'
 CONF_API_KEY = 'api_key'
-CONF_MAX_PASSAGES = 'max_passages'
+CONF_NUMBER_OF_DEPARTURES = 'number_of_departures'
 
 DEFAULT_NAME = 'De Lijn'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
-    vol.Required(CONF_NEXT_PASSAGE): [{
+    vol.Required(CONF_NEXT_DEPARTURE): [{
         vol.Required(CONF_STOP_ID): cv.string,
-        vol.Optional(CONF_MAX_PASSAGES, default=5): cv.positive_int}]
+        vol.Optional(CONF_NUMBER_OF_DEPARTURES, default=5): cv.positive_int}]
 })
+
+
+def due_in_minutes(timestamp):
+    """Get the time in minutes from a timestamp."""
+    if timestamp is None:
+        return None
+    diff = timestamp - dt_util.now()
+    return int(diff.total_seconds() / 60)
 
 
 async def async_setup_platform(
         hass, config, async_add_entities, discovery_info=None):
     """Create the sensor."""
-    from pydelijn.api import Passages
 
-    api_key = config.get(CONF_API_KEY)
+    api_key = config[CONF_API_KEY]
     name = DEFAULT_NAME
 
     session = async_get_clientsession(hass)
 
     sensors = []
-    for nextpassage in config.get(CONF_NEXT_PASSAGE):
+    for nextpassage in config[CONF_NEXT_DEPARTURE]:
         stop_id = nextpassage[CONF_STOP_ID]
-        max_passages = nextpassage[CONF_MAX_PASSAGES]
-        line = Passages(hass.loop, stop_id, max_passages, api_key, session)
+        number_of_departures = nextpassage[CONF_NUMBER_OF_DEPARTURES]
+        line = Passages(hass.loop, stop_id, number_of_departures, api_key, session)
+        await line.get_passages()
+        if line.passages is None:
+            _LOGGER.error("No data recieved from De Lijn")
+            return
         sensors.append(DeLijnPublicTransportSensor(line, name))
-
-    tasks = [sensor.async_update() for sensor in sensors]
-    if tasks:
-        await asyncio.wait(tasks)
-    if not all(sensor._attributes for sensor in sensors):
-        raise PlatformNotReady
 
     async_add_entities(sensors, True)
 
@@ -72,19 +82,31 @@ class DeLijnPublicTransportSensor(Entity):
         try:
             attributes = {}
             first = self.line.passages[0]
-            self._state = first['due_in_min']
+            local_timezone = pytz.timezone("Europe/Brussels")
+            if first['due_at_rt'] is not None:
+                first_passage_naive = dt_util.parse_datetime(first['due_at_rt'])
+            else:
+                first_passage_naive = dt_util.parse_datetime(first['due_at_sch'])
+            first_passage_local = local_timezone.localize(first_passage_naive)
+            first_passage_utc = dt_util.as_utc(first_passage_local)
+            self._state = first_passage_utc.isoformat()
             self._name = first['stopname']
             attributes['stopname'] = first['stopname']
             attributes['line_number_public'] = first['line_number_public']
             attributes['line_transport_type'] = first['line_transport_type']
             attributes['final_destination'] = first['final_destination']
-            attributes['due_at_sch'] = first['due_at_sch']
-            attributes['due_at_rt'] = first['due_at_rt']
+            attributes['due_at_schedule'] = first['due_at_sch']
+            attributes['due_at_realtime'] = first['due_at_rt']
             attributes['due_in_min'] = first['due_in_min']
             attributes['next_passages'] = self.line.passages
             self._attributes = attributes
         except (KeyError, IndexError) as error:
-            _LOGGER.debug("Error getting data from De Lijn, %s", error)
+            _LOGGER.debug("Error getting data from De Lijn, {}".format(error))
+
+    @property
+    def device_class(self):
+        """Return the device class."""
+        return DEVICE_CLASS_TIMESTAMP
 
     @property
     def name(self):
@@ -104,4 +126,5 @@ class DeLijnPublicTransportSensor(Entity):
     @property
     def device_state_attributes(self):
         """Return attributes for the sensor."""
+        self._attributes[ATTR_ATTRIBUTION] = ATTRIBUTION
         return self._attributes
