5aa35b52cc7ed6a225ca2f40dd8c1f93827a47b5
Alexei Chetroi <lexoid@gmail.com>
2019-07-30 15:19:24 -0400

    ZHA log helper (#25543)
    * Logging helper.
    * Use log helper for ZHA entities.
    * Use log helper for ZHA core device.
    * Log helper for ZHA core channels.
    * Lint
    * ZHA fixture fix.

--- homeassistant/components/zha/core/helpers.py
+++ homeassistant/components/zha/core/helpers.py
@@ -207,3 +207,27 @@ def async_is_bindable_target(source_zha_device, target_zha_device):
             if any(bindable in bindables for bindable in matches):
                 return True
     return False
+
+
+class LogMixin:
+    """Log helper."""
+
+    def log(self, level, msg, *args):
+        """Log with level."""
+        raise NotImplementedError
+
+    def debug(self, msg, *args):
+        """Debug level log."""
+        return self.log(logging.DEBUG, msg, *args)
+
+    def info(self, msg, *args):
+        """Info level log."""
+        return self.log(logging.INFO, msg, *args)
+
+    def warning(self, msg, *args):
+        """Warning method log."""
+        return self.log(logging.WARNING, msg, *args)
+
+    def error(self, msg, *args):
+        """Error level log."""
+        return self.log(logging.ERROR, msg, *args)
