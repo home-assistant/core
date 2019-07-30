39b8102ce641e817aec997beaca6b6abbaa979ff
Paulus Schoutsen <balloob@gmail.com>
2019-07-30 16:44:39 -0700

    Bump Python support to min Python 3.6.0 (#25582)
    * Bump Python support to min Python 3.6.0
    
    * Fix type

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -5,7 +5,7 @@ MINOR_VERSION = 97
 PATCH_VERSION = '0.dev0'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
-REQUIRED_PYTHON_VER = (3, 5, 3)
+REQUIRED_PYTHON_VER = (3, 6, 0)
 
 # Format for platform files
 PLATFORM_FORMAT = '{platform}.{domain}'
