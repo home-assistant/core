1e90a07e94865140522baf49f6865bf10db0f3c6
Robert Dunmire III <slackr31337@gmail.com>
2019-07-30 20:43:56 -0400

    Fix validation error

--- homeassistant/components/mikrotik/device_tracker.py
+++ homeassistant/components/mikrotik/device_tracker.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 import logging
 
 from homeassistant.helpers.event import async_track_time_interval
-from homeassistant.components.device_tracker DeviceScanner
+from homeassistant.components.device_tracker import DeviceScanner
 from homeassistant.const import CONF_HOST
 from . import CLIENT, DOMAIN as MIKROTIK_DOMAIN, DEVICE_TRACKER
 
