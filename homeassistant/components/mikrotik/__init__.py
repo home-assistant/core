22ac31c5bd61ee773d20f030ce0b859300032e3c
Robert Dunmire III <slackr31337@gmail.com>
2019-07-31 10:47:27 -0400

    Move constants to const.py

--- homeassistant/components/mikrotik/__init__.py
+++ homeassistant/components/mikrotik/__init__.py
@@ -14,144 +14,27 @@ from homeassistant.components.device_tracker import (
 from homeassistant.components.sensor import DOMAIN as SENSOR
 from homeassistant.components.binary_sensor import (
     DOMAIN as BINARY_SENSOR, DEVICE_CLASS_CONNECTIVITY)
+from .const import (CLIENT, CONNECTING, CONNECTED, IDENTITY, MEGA,
+                    CONF_ARP_PING, CONF_WAN_PORT, CONF_TRACK_DEVICES,
+                    CONF_LOGIN_METHOD, CONF_ENCODING, DEFAULT_ENCODING,
+                    ARP, DHCP, WIRELESS, CAPSMAN, MIKROTIK_SERVICES,
+                    SENSOR_SYSINFO, SENSOR_CPU, SENSOR_MEMORY, 
+                    SENSOR_DISK, SENSOR_DOWNLOAD_SPEED, 
+                    SENSOR_UPLOAD_SPEED, SENSORS, BINARY_SENSOR_NETWATCH,
+                    BINARY_SENSOR_INTERNET, BINARY_SENSORS, 
+                    ATTRIB_DEVICE_TRACKER)
 
 _LOGGER = logging.getLogger(__name__)
 
 NAME = 'Mikrotik'
 DOMAIN = 'mikrotik'
 MIKROTIK = DOMAIN
-CLIENT = 'mikrotik_client'
-
-CONF_ARP_PING = 'arp_ping'
-CONF_WAN_PORT = 'wan_port'
-CONF_TRACK_DEVICES = 'track_devices'
-CONF_LOGIN_METHOD = 'login_method'
-CONF_ENCODING = 'encoding'
-DEFAULT_ENCODING = 'utf-8'
-
-CONNECTING = 'connected'
-CONNECTED = 'connected'
 
 MTK_DEFAULT_WAN = 'ether1'
 MTK_DEFAULT_API_PORT = '8728'
 MTK_DEFAULT_API_SSL_PORT = '8729'
 MTK_LOGIN_PLAIN = 'plain'
 MTK_LOGIN_TOKEN = 'token'
-
-IDENTITY = 'identity'
-ARP = 'arp'
-DHCP = 'dhcp'
-WIRELESS = 'wireless'
-CAPSMAN = 'capsman'
-
-MIKROTIK_SERVICES = {
-    IDENTITY: '/system/identity/getall',
-    ARP: '/ip/arp/getall',
-    DHCP: '/ip/dhcp-server/lease/getall',
-    WIRELESS: '/interface/wireless/registration-table/getall',
-    CAPSMAN: '/caps-man/registration-table/getall'
-}
-
-SENSOR_SYSINFO = 'sysinfo'
-SENSOR_CPU = 'cpu'
-SENSOR_MEMORY = 'memory'
-SENSOR_DISK = 'disk'
-SENSOR_DOWNLOAD_SPEED = 'download_speed'
-SENSOR_UPLOAD_SPEED = 'upload_speed'
-
-ATTRIB_SYSINFO = ['board-name', 'serial-number',
-                  'version', 'factory-firmware',
-                  'firmware-type', 'current-firmware',
-                  'upgrade-frimware', 'routerboard',
-                  'cpu', 'total-memory',
-                  'architecture-name']
-ATTRIB_CPU = ['cpu',
-              'cpu-frequency',
-              'cpu-count']
-ATTRIB_MEMORY = ['free-memory',
-                 'total-memory']
-ATTRIB_DISK = ['free-hdd-space',
-               'total-hdd-space']
-ATTRIB_DOWNLOAD_SPEED = ['name',
-                         'rx-packets-per-second',
-                         'rx-bits-per-second',
-                         'fp-rx-packets-per-second',
-                         'fp-rx-bits-per-second',
-                         'rx-drops-per-second',
-                         'rx-errors-per-second']
-ATTRIB_UPLOAD_SPEED = ['name',
-                       'tx-packets-per-second',
-                       'tx-bits-per-second',
-                       'fp-tx-packets-per-second',
-                       'fp-tx-bits-per-second',
-                       'tx-drops-per-second',
-                       'tx-queue-drops-per-second',
-                       'tx-errors-per-second']
-ATTRIB_DOWNLOAD = ['name', 'tx-bytes']
-ATTRIB_UPLOAD = ['name', 'rx-bytes']
-
-PARAM_SPEED = {'interface': MTK_DEFAULT_WAN,
-               'duration': '1s'}
-MEGA = 1048576
-
-# Sensor types are defined like:
-# Name, units, icon, state item, api cmd(s), attributes
-SENSORS = {
-    SENSOR_SYSINFO: ['System Info', None, 'mdi:switch',
-                     'board-name',
-                     ['/system/routerboard/getall',
-                      '/system/resource/getall'],
-                     ATTRIB_SYSINFO, None, None],
-    SENSOR_CPU: ['CPU Load', '%', 'mdi:chip',
-                 'cpu-load',
-                 ['/system/resource/getall'],
-                 ATTRIB_CPU, None],
-    SENSOR_MEMORY: ['Memory Free', 'Mbytes',
-                    'mdi:memory', 'free-memory',
-                    ['/system/resource/getall'],
-                    ATTRIB_MEMORY, None],
-    SENSOR_DISK: ['Disk Free', 'Mbytes', 'mdi:harddisk',
-                  'free-hdd-space',
-                  ['/system/resource/getall'],
-                  ATTRIB_DISK, None],
-    SENSOR_DOWNLOAD_SPEED: ['Download Speed', 'Mbps',
-                            'mdi:download-network',
-                            'rx-bits-per-second',
-                            ['/interface/monitor-traffic'],
-                            ATTRIB_DOWNLOAD_SPEED,
-                            PARAM_SPEED],
-    SENSOR_UPLOAD_SPEED: ['Upload Speed', 'Mbps',
-                          'mdi:upload-network',
-                          'tx-bits-per-second',
-                          ['/interface/monitor-traffic'],
-                          ATTRIB_UPLOAD_SPEED, PARAM_SPEED],
-}
-
-BINARY_SENSOR_NETWATCH = 'netwatch'
-BINARY_SENSOR_INTERNET = 'internet'
-ATTRIB_NETWATCH = ['host', 'interval',
-                   'timeout', 'since', 'disabled', 'comment']
-ATTRIB_INTERNET = ['name', 'cloud-rtt', 'state-change-time']
-
-# Binary Sensors: Name, Class, icon, state, api cmd, attributes, state hash
-BINARY_SENSORS = {
-    BINARY_SENSOR_NETWATCH: ['Netwatch', DEVICE_CLASS_CONNECTIVITY,
-                             'mdi:lan-connect', '/tool/netwatch/getall',
-                             'status', ATTRIB_NETWATCH,
-                             {'up': True, 'down': False},
-                             'host'],
-    BINARY_SENSOR_INTERNET: ['Internet', DEVICE_CLASS_CONNECTIVITY,
-                             'mdi:wan',
-                             '/interface/detect-internet/state/getall',
-                             'state',
-                             ATTRIB_INTERNET,
-                             {'internet': True, 'unknown': False},
-                             'name'],
-}
-
-ATTRIB_DEVICE_TRACKER = ['mac-address', 'rx-signal', 'ssid', 'interface',
-                         'comment', 'host-name', 'address', 'uptime',
-                         'rx-rate', 'tx-rate', 'last-seen']
 
 MIKROTIK_SCHEMA = vol.All(
     vol.Schema({
