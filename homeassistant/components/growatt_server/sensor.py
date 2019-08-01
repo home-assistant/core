7941d8d80b9aab14a2203e73983c031007c247f9
Indy Koning <indykoning@hotmail.com>
2019-08-01 21:52:42 +0200

    Ran black formatter

--- homeassistant/components/growatt_server/sensor.py
+++ homeassistant/components/growatt_server/sensor.py
@@ -1,37 +1,35 @@
 """Read status of growatt inverters."""
-import logging
-import voluptuous as vol
-import growattServer
 import datetime
+import logging
+import growattServer
+import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (CONF_NAME)
+from homeassistant.const import CONF_NAME
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
-"""Start the logger"""
+
 _LOGGER = logging.getLogger(__name__)
 
-plant_id = 0
-"""configuration for accessing the Unifi Controller"""
-CONF_USERNAME = 'username'
-CONF_PASSWORD = 'password'
-CONF_PLANT_ID = 'plant_id'
-DEFAULT_UNIT = 'kW'
-DEFAULT_NAME = 'Growatt'
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_PLANT_ID = "plant_id"
+DEFAULT_UNIT = "kW"
+DEFAULT_NAME = "Growatt"
 SCAN_INTERVAL = datetime.timedelta(minutes=5)
 
-"""Define the schema for the Sensor Platform"""
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_PLANT_ID, default='0'): cv.string,
-    vol.Required(CONF_USERNAME): cv.string,
-    vol.Required(CONF_PASSWORD): cv.string
-})
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_PLANT_ID, default="0"): cv.string,
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string,
+    }
+)
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Growatt sensor."""
-    """get all the parameters passed by the user to access the controller"""
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
     plant_id = config.get(CONF_PLANT_ID)
@@ -39,22 +37,35 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     api = growattServer.GrowattApi()
 
-    """Log in to api and fetch first plant if no plant id is defined"""
-    user_id = api.login(username, password)['userId']
-    if plant_id == '0':
+    # Log in to api and fetch first plant if no plant id is defined.
+    user_id = api.login(username, password)["userId"]
+    if plant_id == "0":
         plant_info = api.plant_list(user_id)
-        plant_id = plant_info['data'][0]['plantId']
+        plant_id = plant_info["data"][0]["plantId"]
         pass
 
-    """Get a list of inverters for specified plant to add sensors for."""
+    # Get a list of inverters for specified plant to add sensors for.
     inverters = api.inverter_list(plant_id)
 
-    """Add a sensor for the total of the whole plant"""
-    add_devices([GrowattInverter(api, name + '_Total', plant_id, username, password)], True)
+    # Add a sensor for the total of the whole plant.
+    add_devices(
+        [GrowattInverter(api, name + "_Total", plant_id, username, password)], True
+    )
 
-    """Add sensors for each inverter in the specified plant"""
+    # Add sensors for each inverter in the specified plant.
     for inverter in inverters:
-        add_devices([GrowattInverter(api, name + '_' + inverter['deviceAilas'], inverter['deviceSn'], username, password)], True)
+        add_devices(
+            [
+                GrowattInverter(
+                    api,
+                    name + "_" + inverter["deviceAilas"],
+                    inverter["deviceSn"],
+                    username,
+                    password,
+                )
+            ],
+            True,
+        )
 
 
 class GrowattInverter(Entity):
@@ -67,10 +78,10 @@ class GrowattInverter(Entity):
         self.inverter_id = inverter_id
         self.username = u
         self.password = p
-        self.isTotal = 'Total' in name and inverter_id.isdigit()
+        self.is_total = "Total" in name and inverter_id.isdigit()
         self._state = None
         self.attributes = {}
-        self._unit_of_measurement = 'W'
+        self._unit_of_measurement = "W"
 
     @property
     def name(self):
@@ -91,23 +102,22 @@ class GrowattInverter(Entity):
     def device_state_attributes(self):
         """Return the state attributes of the monitored installation."""
         attributes = self.attributes
-        attributes['icon'] = 'mdi:solar-power'
+        attributes["icon"] = "mdi:solar-power"
         return attributes
 
     def update(self):
         """Get the latest data from the Growat API and updates the state."""
         try:
             self.api.login(self.username, self.password)
-            if self.isTotal:
+            if self.is_total:
                 total_info = self.api.plant_info(self.inverter_id)
-                del total_info['deviceList']
-                self._state = total_info['invTodayPpv']
+                del total_info["deviceList"]
+                self._state = total_info["invTodayPpv"]
                 self.attributes = total_info
             else:
                 inverter_info = self.api.inverter_detail(self.inverter_id)
 
-                self.attributes = inverter_info['data']
-                self._state = inverter_info['data']['pac']
+                self.attributes = inverter_info["data"]
+                self._state = inverter_info["data"]["pac"]
         except TypeError:
-            _LOGGER.error(
-                "Unable to fetch data from Growatt server. %s")
+            _LOGGER.error("Unable to fetch data from Growatt server. %s")
