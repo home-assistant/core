8cfe91e74e8c71ebd7d3d6a939670245ac13d42f
tleegaard <tleegaard@gmail.com>
2019-08-01 09:21:15 +0200

    black/balloob

--- tests/components/homekit_controller/test_sensor.py
+++ tests/components/homekit_controller/test_sensor.py
@@ -1,18 +1,18 @@
 """Basic checks for HomeKit sensor."""
-from tests.components.homekit_controller.common import (
-    FakeService, setup_test_component)
+from tests.components.homekit_controller.common import FakeService, setup_test_component
 
-TEMPERATURE = ('temperature', 'temperature.current')
-HUMIDITY = ('humidity', 'relative-humidity.current')
-LIGHT_LEVEL = ('light', 'light-level.current')
-CARBON_DIOXIDE_LEVEL = ('carbon-dioxide', 'carbon-dioxide.level')
+TEMPERATURE = ("temperature", "temperature.current")
+HUMIDITY = ("humidity", "relative-humidity.current")
+LIGHT_LEVEL = ("light", "light-level.current")
+CARBON_DIOXIDE_LEVEL = ("carbon-dioxide", "carbon-dioxide.level")
 
 
 def create_temperature_sensor_service():
     """Define temperature characteristics."""
-    service = FakeService('public.hap.service.sensor.temperature')
+    service = FakeService("public.hap.service.sensor.temperature")
 
-    cur_state = service.add_characteristic('temperature.current')
+    cur_state = service.add_characteristic("temperature.current")
+    
     cur_state.value = 0
 
     return service
@@ -20,9 +20,9 @@ def create_temperature_sensor_service():
 
 def create_humidity_sensor_service():
     """Define humidity characteristics."""
-    service = FakeService('public.hap.service.sensor.humidity')
+    service = FakeService("public.hap.service.sensor.humidity")
 
-    cur_state = service.add_characteristic('relative-humidity.current')
+    cur_state = service.add_characteristic("relative-humidity.current")
     cur_state.value = 0
 
     return service
@@ -30,9 +30,9 @@ def create_humidity_sensor_service():
 
 def create_light_level_sensor_service():
     """Define light level characteristics."""
-    service = FakeService('public.hap.service.sensor.light')
+    service = FakeService("public.hap.service.sensor.light")
 
-    cur_state = service.add_characteristic('light-level.current')
+    cur_state = service.add_characteristic("light-level.current")
     cur_state.value = 0
 
     return service
@@ -40,9 +40,9 @@ def create_light_level_sensor_service():
 
 def create_carbon_dioxide_level_sensor_service():
     """Define carbon dioxide level characteristics."""
-    service = FakeService('public.hap.service.sensor.carbon-dioxide')
+    service = FakeService("public.hap.service.sensor.carbon-dioxide")
 
-    cur_state = service.add_characteristic('carbon-dioxide.level')
+    cur_state = service.add_characteristic("carbon-dioxide.level")
     cur_state.value = 0
 
     return service
@@ -55,11 +55,11 @@ async def test_temperature_sensor_read_state(hass, utcnow):
 
     helper.characteristics[TEMPERATURE].value = 10
     state = await helper.poll_and_get_state()
-    assert state.state == '10'
+    assert state.state == "10"
 
     helper.characteristics[TEMPERATURE].value = 20
     state = await helper.poll_and_get_state()
-    assert state.state == '20'
+    assert state.state == "20"
 
 
 async def test_humidity_sensor_read_state(hass, utcnow):
@@ -69,11 +69,11 @@ async def test_humidity_sensor_read_state(hass, utcnow):
 
     helper.characteristics[HUMIDITY].value = 10
     state = await helper.poll_and_get_state()
-    assert state.state == '10'
+    assert state.state == "10"
 
     helper.characteristics[HUMIDITY].value = 20
     state = await helper.poll_and_get_state()
-    assert state.state == '20'
+    assert state.state == "20"
 
 
 async def test_light_level_sensor_read_state(hass, utcnow):
@@ -83,11 +83,11 @@ async def test_light_level_sensor_read_state(hass, utcnow):
 
     helper.characteristics[LIGHT_LEVEL].value = 10
     state = await helper.poll_and_get_state()
-    assert state.state == '10'
+    assert state.state == "10"
 
     helper.characteristics[LIGHT_LEVEL].value = 20
     state = await helper.poll_and_get_state()
-    assert state.state == '20'
+    assert state.state == "20"
 
 
 async def test_carbon_dioxide_level_sensor_read_state(hass, utcnow):
@@ -98,8 +98,8 @@ async def test_carbon_dioxide_level_sensor_read_state(hass, utcnow):
 
     helper.characteristics[CARBON_DIOXIDE_LEVEL].value = 10
     state = await helper.poll_and_get_state()
-    assert state.state == '10'
+    assert state.state == "10"
 
     helper.characteristics[CARBON_DIOXIDE_LEVEL].value = 20
     state = await helper.poll_and_get_state()
-    assert state.state == '20'
+    assert state.state == "20"
