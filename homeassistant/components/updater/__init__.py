345cb10a1bbc9522968ba75c350894501d8afeda
Tobias Haase <tobhaase@gmail.com>
2019-08-01 23:03:06 +0200

    Make flake happy

--- homeassistant/components/updater/__init__.py
+++ homeassistant/components/updater/__init__.py
@@ -53,7 +53,7 @@ class Updater:
     """Updater class for data exchange."""
 
     def __init__(self, update_available: bool, newest_version: str, release_notes: str):
-        """Updater constructor."""
+        """Setup Updater."""
         self.update_available = update_available
         self.release_notes = release_notes
         self.newest_version = newest_version
