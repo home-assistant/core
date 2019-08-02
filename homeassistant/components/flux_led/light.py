103047f2887554983ca00ec50185a355f51b07fd
Anders Melchiorsen <amelchio@nogoto.net>
2019-08-02 09:52:39 +0200

    Use mode detection for scanned bulbs

--- homeassistant/components/flux_led/light.py
+++ homeassistant/components/flux_led/light.py
@@ -164,7 +164,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         if ipaddr in light_ips:
             continue
         device["name"] = "{} {}".format(device["id"], ipaddr)
-        device[ATTR_MODE] = MODE_RGBW
+        device[ATTR_MODE] = None
         device[CONF_PROTOCOL] = None
         device[CONF_CUSTOM_EFFECT] = None
         light = FluxLight(device)
