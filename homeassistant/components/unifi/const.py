7a71669027f9667552c403c938b7015012560bbc
Robert Svensson <Kane610@users.noreply.github.com>
2019-08-02 23:51:06 +0200

    Options to not track wired clients (#25669)

--- homeassistant/components/unifi/const.py
+++ homeassistant/components/unifi/const.py
@@ -15,6 +15,7 @@ CONF_BLOCK_CLIENT = "block_client"
 CONF_DETECTION_TIME = "detection_time"
 CONF_DONT_TRACK_CLIENTS = "dont_track_clients"
 CONF_DONT_TRACK_DEVICES = "dont_track_devices"
+CONF_DONT_TRACK_WIRED_CLIENTS = "dont_track_wired_clients"
 CONF_SSID_FILTER = "ssid_filter"
 
 ATTR_MANUFACTURER = "Ubiquiti Networks"
