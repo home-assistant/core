b6a8de99f835bc1ac1dbc213a363302819933fe1
Charles Garwood <cgarwood@gmail.com>
2019-08-04 19:21:37 -0400

    Add usb_path to Z-Wave network_status websocket response (#25617)
    * Add usb stick path to zwave network_status websocket response
    
    * Move to separate websocket command
    
    * Return additional config options
    
    * add tests

--- homeassistant/components/zwave/websocket_api.py
+++ homeassistant/components/zwave/websocket_api.py
@@ -7,7 +7,14 @@ import voluptuous as vol
 from homeassistant.components import websocket_api
 from homeassistant.core import callback
 
-from .const import DATA_NETWORK
+from .const import (
+    CONF_AUTOHEAL,
+    CONF_DEBUG,
+    CONF_POLLING_INTERVAL,
+    CONF_USB_STICK_PATH,
+    DATA_NETWORK,
+    DATA_ZWAVE_CONFIG,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,7 +30,24 @@ def websocket_network_status(hass, connection, msg):
     connection.send_result(msg[ID], {"state": network.state})
 
 
+@websocket_api.require_admin
+@websocket_api.websocket_command({vol.Required(TYPE): "zwave/get_config"})
+def websocket_get_config(hass, connection, msg):
+    """Get Z-Wave configuration."""
+    config = hass.data[DATA_ZWAVE_CONFIG]
+    connection.send_result(
+        msg[ID],
+        {
+            CONF_AUTOHEAL: config[CONF_AUTOHEAL],
+            CONF_DEBUG: config[CONF_DEBUG],
+            CONF_POLLING_INTERVAL: config[CONF_POLLING_INTERVAL],
+            CONF_USB_STICK_PATH: config[CONF_USB_STICK_PATH],
+        },
+    )
+
+
 @callback
 def async_load_websocket_api(hass):
     """Set up the web socket API."""
     websocket_api.async_register_command(hass, websocket_network_status)
+    websocket_api.async_register_command(hass, websocket_get_config)
