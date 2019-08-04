662cee7b20ff129eaf9aea90b3d1ff8dcb27d19b
ktnrg45 <38207570+ktnrg45@users.noreply.github.com>
2019-08-04 05:33:01 -0700

    Merge async_setup_platform into async_setup_entry

--- homeassistant/components/ps4/media_player.py
+++ homeassistant/components/ps4/media_player.py
@@ -58,11 +58,6 @@ DEFAULT_RETRIES = 2
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up PS4 from a config entry."""
     config = config_entry
-    await async_setup_platform(hass, config, async_add_entities, discovery_info=None)
-
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up PS4 Platform."""
     creds = config.data[CONF_TOKEN]
     device_list = []
     for device in config.data["devices"]:
