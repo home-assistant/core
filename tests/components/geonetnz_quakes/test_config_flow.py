4815a6618e04a7cce472224f7b28824db1f0e0fb
Malte Franken <exxamalte@users.noreply.github.com>
2019-08-05 16:56:44 +1000

    added test case for config flow

---
+++ tests/components/geonetnz_quakes/test_config_flow.py
@@ -0,0 +1,144 @@
+"""Define tests for the GeoNet NZ Quakes config flow."""
+from datetime import timedelta
+
+import pytest
+from asynctest import patch
+
+from homeassistant import data_entry_flow
+from homeassistant.components.geonetnz_quakes import (
+    async_setup_entry,
+    config_flow,
+    CONF_MMI,
+    CONF_MINIMUM_MAGNITUDE,
+    DOMAIN,
+)
+from homeassistant.const import (
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+    CONF_RADIUS,
+    CONF_UNIT_SYSTEM,
+    CONF_SCAN_INTERVAL,
+)
+from tests.common import MockConfigEntry
+
+
+@pytest.fixture
+def config_entry():
+    """Create a mock WWLLN config entry."""
+    return MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            CONF_LATITUDE: -41.2,
+            CONF_LONGITUDE: 174.7,
+            CONF_RADIUS: 25,
+            CONF_UNIT_SYSTEM: "metric",
+            CONF_SCAN_INTERVAL: 300.0,
+        },
+        title="-41.2, 174.7",
+    )
+
+
+async def test_duplicate_error(hass, config_entry):
+    """Test that errors are shown when duplicates are added."""
+    conf = {CONF_LATITUDE: -41.2, CONF_LONGITUDE: 174.7, CONF_RADIUS: 25}
+
+    config_entry.add_to_hass(hass)
+    flow = config_flow.GeonetnzQuakesFlowHandler()
+    flow.hass = hass
+
+    result = await flow.async_step_user(user_input=conf)
+    assert result["errors"] == {"base": "identifier_exists"}
+
+
+async def test_show_form(hass):
+    """Test that the form is served with no input."""
+    flow = config_flow.GeonetnzQuakesFlowHandler()
+    flow.hass = hass
+
+    result = await flow.async_step_user(user_input=None)
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "user"
+
+
+async def test_step_import(hass):
+    """Test that the import step works."""
+    conf = {
+        CONF_LATITUDE: -41.2,
+        CONF_LONGITUDE: 174.7,
+        CONF_RADIUS: 25,
+        CONF_UNIT_SYSTEM: "metric",
+        CONF_MMI: 2,
+    }
+
+    flow = config_flow.GeonetnzQuakesFlowHandler()
+    flow.hass = hass
+
+    result = await flow.async_step_import(import_config=conf)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == "-41.2, 174.7"
+    assert result["data"] == {
+        CONF_LATITUDE: -41.2,
+        CONF_LONGITUDE: 174.7,
+        CONF_RADIUS: 25,
+        CONF_MMI: 2,
+        CONF_UNIT_SYSTEM: "metric",
+        CONF_SCAN_INTERVAL: 300.0,
+    }
+
+
+async def test_step_user(hass):
+    """Test that the user step works."""
+    conf = {CONF_LATITUDE: -41.2, CONF_LONGITUDE: 174.7, CONF_RADIUS: 25, CONF_MMI: 3}
+
+    flow = config_flow.GeonetnzQuakesFlowHandler()
+    flow.hass = hass
+
+    result = await flow.async_step_user(user_input=conf)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == "-41.2, 174.7"
+    assert result["data"] == {
+        CONF_LATITUDE: -41.2,
+        CONF_LONGITUDE: 174.7,
+        CONF_RADIUS: 25,
+        CONF_MMI: 3,
+        CONF_UNIT_SYSTEM: "metric",
+        CONF_SCAN_INTERVAL: 300.0,
+    }
+
+
+async def test_custom_minimum_magnitude(hass):
+    """Test that a custom minimum magnitude is stored correctly."""
+    conf = {
+        CONF_LATITUDE: -41.2,
+        CONF_LONGITUDE: 174.7,
+        CONF_RADIUS: 25,
+        CONF_MINIMUM_MAGNITUDE: 8.0,
+        CONF_MMI: 2,
+    }
+
+    flow = config_flow.GeonetnzQuakesFlowHandler()
+    flow.hass = hass
+
+    result = await flow.async_step_user(user_input=conf)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == "-41.2, 174.7"
+    assert result["data"] == {
+        CONF_LATITUDE: -41.2,
+        CONF_LONGITUDE: 174.7,
+        CONF_RADIUS: 25,
+        CONF_MMI: 2,
+        CONF_UNIT_SYSTEM: "metric",
+        CONF_MINIMUM_MAGNITUDE: 8.0,
+        CONF_SCAN_INTERVAL: 300.0,
+    }
+
+
+async def test_component_load_config_entry(hass, config_entry):
+    """Test that loading an existing config entry yields a client."""
+    config_entry.add_to_hass(hass)
+    with patch.object(hass.config_entries, "async_forward_entry_setup") as forward_mock:
+        assert await async_setup_entry(hass, config_entry)
+
+        await hass.async_block_till_done()
+        assert forward_mock.call_count == 1
