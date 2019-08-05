3e57e21a65ed61e2fa5308ba102f6a925ef010c3
Tobias Sauerwein <cgtobi@gmail.com>
2019-08-05 09:54:29 +0200

    Remove old code

--- tests/components/rmvtransport/test_config_flow.py
+++ tests/components/rmvtransport/test_config_flow.py
@@ -1,27 +1,14 @@
 """Tests for rmvtransport config flow."""
 from unittest.mock import patch
 
-import asyncio
-
 from homeassistant.components.rmvtransport import config_flow
-from tests.common import MockConfigEntry, mock_coro
+from tests.common import mock_coro
 
 from .test_sensor import get_departures_mock, search_station_mock
 
 
 async def test_flow_works(hass, aioclient_mock):
     """Test that config flow works."""
-    # aioclient_mock.get(
-    #     pydeconz.utils.URL_DISCOVER,
-    #     json=[{"id": "id", "internalipaddress": "1.2.3.4", "internalport": 80}],
-    #     headers={"content-type": "application/json"},
-    # )
-    # aioclient_mock.post(
-    #     "http://1.2.3.4:80/api",
-    #     json=[{"success": {"username": "1234567890ABCDEF"}}],
-    #     headers={"content-type": "application/json"},
-    # )
-
     result = await hass.config_entries.flow.async_init(
         config_flow.DOMAIN, context={"source": "user"}
     )
