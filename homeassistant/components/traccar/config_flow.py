f38b333414a1a0127682f8763f77e59ca94932ef
escoand <escoand@users.noreply.github.com>
2019-08-08 21:10:17 +0200

    rename config_flow

--- homeassistant/components/traccar/config_flow.py
+++ homeassistant/components/traccar/config_flow.py
@@ -5,7 +5,7 @@ from .const import DOMAIN
 
 config_entry_flow.register_webhook_flow(
     DOMAIN,
-    'Traccar / Osmand Webhook',
+    'Traccar Webhook',
     {
         'docs_url': 'https://www.home-assistant.io/components/traccar/'
     }
