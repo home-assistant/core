135cb97b2f54d3e94d3dad23bc27755e38d2bd85
Thomas Lovén <thomasloven@gmail.com>
2019-08-09 22:21:08 +0200

    Add error handling to !include command in yaml (#25801)
    * Catch errors if !include file is not found
    
    * Address review comments
    
    * Add line number to error message

--- homeassistant/util/yaml/loader.py
+++ homeassistant/util/yaml/loader.py
@@ -116,7 +116,10 @@ def _include_yaml(loader: SafeLineLoader, node: yaml.nodes.Node) -> JSON_TYPE:
 
     """
     fname = os.path.join(os.path.dirname(loader.name), node.value)
-    return _add_reference(load_yaml(fname), loader, node)
+    try:
+        return _add_reference(load_yaml(fname), loader, node)
+    except FileNotFoundError:
+        raise HomeAssistantError(f"{node.start_mark}: Unable to read file {fname}.")
 
 
 def _is_file_valid(name: str) -> bool:
