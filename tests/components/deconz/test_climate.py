4715aee05952888dfd111f834fab1565c7485405
Nikolay Vasilchuk <Anonym.tsk@gmail.com>
2019-08-09 20:31:58 +0300

    Fix deconz allow_clip_sensor and allow_deconz_groups options (#25811)

--- tests/components/deconz/test_climate.py
+++ tests/components/deconz/test_climate.py
@@ -39,12 +39,15 @@ SENSOR = {
 }
 
 ENTRY_CONFIG = {
-    deconz.const.CONF_ALLOW_CLIP_SENSOR: True,
-    deconz.const.CONF_ALLOW_DECONZ_GROUPS: True,
     deconz.config_flow.CONF_API_KEY: "ABCDEF",
     deconz.config_flow.CONF_BRIDGEID: "0123456789",
     deconz.config_flow.CONF_HOST: "1.2.3.4",
     deconz.config_flow.CONF_PORT: 80,
+}
+
+ENTRY_OPTIONS = {
+    deconz.const.CONF_ALLOW_CLIP_SENSOR: True,
+    deconz.const.CONF_ALLOW_DECONZ_GROUPS: True,
 }
 
 
@@ -59,7 +62,7 @@ async def setup_gateway(hass, data, allow_clip_sensor=True):
 
     session = Mock(put=asynctest.CoroutineMock(return_value=response))
 
-    ENTRY_CONFIG[deconz.const.CONF_ALLOW_CLIP_SENSOR] = allow_clip_sensor
+    ENTRY_OPTIONS[deconz.const.CONF_ALLOW_CLIP_SENSOR] = allow_clip_sensor
 
     config_entry = config_entries.ConfigEntry(
         1,
@@ -68,6 +71,7 @@ async def setup_gateway(hass, data, allow_clip_sensor=True):
         ENTRY_CONFIG,
         "test",
         config_entries.CONN_CLASS_LOCAL_PUSH,
+        ENTRY_OPTIONS,
     )
     gateway = deconz.DeconzGateway(hass, config_entry)
     gateway.api = DeconzSession(hass.loop, session, **config_entry.data)
