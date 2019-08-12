14453604b1d860de50ec4f48ea8dd352b2b69334
Gil Peeters <gil@cancas.com>
2019-08-12 06:38:01 +0000

    Added 'availability_template' to template vacuum

--- tests/components/template/test_vacuum.py
+++ tests/components/template/test_vacuum.py
@@ -3,7 +3,7 @@ import logging
 import pytest
 
 from homeassistant import setup
-from homeassistant.const import STATE_ON, STATE_UNKNOWN
+from homeassistant.const import STATE_ON, STATE_UNKNOWN, STATE_UNAVAILABLE
 from homeassistant.components.vacuum import (
     ATTR_BATTERY_LEVEL,
     STATE_CLEANING,
@@ -208,6 +208,51 @@ async def test_invalid_templates(hass, calls):
     await hass.async_block_till_done()
 
     _verify(hass, STATE_UNKNOWN, None)
+
+
+async def test_available_template_with_entities(hass, calls):
+    """Test availability templates with values from other entities."""
+    availability_template = """
+        {% if is_state('availability_boolean.state', 'True') %}
+            {{ 'true' }}
+        {% else %}
+            {{ 'false' }}
+        {% endif %}
+    """
+    with assert_setup_component(1, "vacuum"):
+        assert await setup.async_setup_component(
+            hass,
+            "vacuum",
+            {
+                "vacuum": {
+                    "platform": "template",
+                    "vacuums": {
+                        "test_template_vacuum": {
+                            "availability_template": availability_template,
+                            "start": {"service": "script.vacuum_start"},
+                        }
+                    },
+                }
+            },
+        )
+    await hass.async_start()
+    await hass.async_block_till_done()
+
+    # When template returns true..
+    hass.states.async_set("availability_boolean.state", True)
+    await hass.async_block_till_done()
+
+    # Device State should not be unavailable
+    state = hass.states.get("vacuum.test_template_vacuum")
+    assert state.state != STATE_UNAVAILABLE
+
+    # When Availability template returns false
+    hass.states.async_set("availability_boolean.state", False)
+    await hass.async_block_till_done()
+
+    # device state should be unavailable
+    state = hass.states.get("vacuum.test_template_vacuum")
+    assert state.state == STATE_UNAVAILABLE
 
 
 # End of template tests #
