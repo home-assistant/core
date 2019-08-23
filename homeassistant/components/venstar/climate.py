a0ff2ca60a8ab1a219461fbdc2e0b051ad00935f
Tyler Page <iamtpage@users.noreply.github.com>
2019-08-23 01:23:19 -0700

    Venstar: define success for all branches of set_temperature() (#26148)
    * define success for all branches
    
    * add operation_mode to error when unexpected value
    
    * fix black linting
    
    * black linting
    
    * fix weird black linting result

--- homeassistant/components/venstar/climate.py
+++ homeassistant/components/venstar/climate.py
@@ -265,9 +265,11 @@ class VenstarThermostat(ClimateDevice):
             elif operation_mode == self._client.MODE_AUTO:
                 success = self._client.set_setpoints(temp_low, temp_high)
             else:
+                success = False
                 _LOGGER.error(
                     "The thermostat is currently not in a mode "
-                    "that supports target temperature"
+                    "that supports target temperature: %s",
+                    operation_mode,
                 )
 
             if not success:
