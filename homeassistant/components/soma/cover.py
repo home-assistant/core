f5b30d0b8d8739e7afc14447e0883f473df21976
Tiit Rätsep <tiit@wazombi.com>
2019-08-27 08:12:41 +0100

    Style fixes

--- homeassistant/components/soma/cover.py
+++ homeassistant/components/soma/cover.py
@@ -13,6 +13,7 @@ from homeassistant.exceptions import PlatformNotReady
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Soma cover platform."""
     def get_covers():
@@ -38,10 +39,6 @@ async def async_setup_platform(hass, config, async_add_entities,
 class SomaCover(SomaEntity, CoverDevice):
     """Representation of a Soma cover device."""
 
-    # def __init__(self, device, api):
-    #    """Initialize the Soma device."""
-    #    super().__init__(device, api)
-
     async def async_update(self):
         """Update the device with the latest data."""
         await super().async_update()
@@ -61,40 +58,20 @@ class SomaCover(SomaEntity, CoverDevice):
 
     def set_cover_position(self, **kwargs):
         """Move the cover shutter to a specific position."""
-        _LOGGER.warning(str(kwargs[ATTR_POSITION]));
-        position = kwargs[ATTR_POSITION]
-        if self.api.set_shade_position(self.device['mac'], kwargs[ATTR_POSITION]) != 'success':
+        self.current_position = kwargs[ATTR_POSITION]
+        if self.api.set_shade_position(
+                self.device['mac'], kwargs[ATTR_POSITION]) != 'success':
             raise PlatformNotReady()
 
     @property
     def current_cover_position(self):
         """Return the current position of cover shutter."""
-        position = self.current_position
-        return position
+        return self.current_position
 
     @property
     def is_closed(self):
         """Return if the cover is closed."""
-        is_closed = None
+        is_closed = False
+        if self.current_position == 100:
+            is_closed = True
         return is_closed
-
-    @property
-    def current_cover_tilt_position(self):
-        """Return current position of cover tilt.
-
-        None is unknown, 0 is closed, 100 is fully open.
-        """
-        orientation = None
-        return orientation
-
-    def set_cover_tilt_position(self, **kwargs):
-        """Move the cover tilt to a specific position."""
-
-    def open_cover_tilt(self, **kwargs):
-        """Open the cover tilt."""
-
-    def close_cover_tilt(self, **kwargs):
-        """Close the cover tilt."""
-
-    def stop_cover_tilt(self, **kwargs):
-        """Stop the cover."""
