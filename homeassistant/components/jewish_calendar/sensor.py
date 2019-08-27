3bbf625309752c6d92cf73b2a113d79d28fd4bfd
Tsvi Mostovicz <ttmost@gmail.com>
2019-08-27 15:19:33 +0300

    Cleanup log messages

--- homeassistant/components/jewish_calendar/sensor.py
+++ homeassistant/components/jewish_calendar/sensor.py
@@ -13,8 +13,6 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Jewish calendar sensor platform."""
-    _LOGGER.debug("Configuration loaded: %s", config)
-
     if discovery_info is None:
         return
 
@@ -65,6 +63,7 @@ class JewishCalendarSensor(Entity):
         import hdate
 
         now = dt_util.now()
+        _LOGGER.debug("Now: %s Timezone = %s", now, now.tzinfo)
 
         today = now.date()
         sunset = dt_util.as_local(
