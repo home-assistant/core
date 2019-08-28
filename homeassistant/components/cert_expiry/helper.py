ce7c284d46d11d623401b1dbca0fc3bc3d7ad593
Maikel Punie <maikel.punie@gmail.com>
2019-08-28 15:56:07 +0200

    Removed the catch

--- homeassistant/components/cert_expiry/helper.py
+++ homeassistant/components/cert_expiry/helper.py
@@ -8,12 +8,8 @@ from .const import TIMEOUT
 def get_cert(host, port):
     """Get the ssl certificate for the host and port combination."""
     ctx = ssl.create_default_context()
-    try:
-        address = (host, port)
-        with socket.create_connection(address, timeout=TIMEOUT) as sock:
-            with ctx.wrap_socket(sock, server_hostname=address[0]) as ssock:
-                cert = ssock.getpeercert()
-                return cert
-    except Exception as excep:
-        raise excep
-    return None
+    address = (host, port)
+    with socket.create_connection(address, timeout=TIMEOUT) as sock:
+        with ctx.wrap_socket(sock, server_hostname=address[0]) as ssock:
+            cert = ssock.getpeercert()
+            return cert
