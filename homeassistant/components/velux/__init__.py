326e3c0915bc3ebf53727ca5627538217eb30907
Elo Rasmussen <elr@seges.dk>
2019-08-29 16:43:11 +0200

    velux KLF200 device did not disconnect properly when rebooting the hass device. disconnect is now being called on the 'EVENT_HOMEASSISTANT_STOP' event

--- homeassistant/components/velux/__init__.py
+++ homeassistant/components/velux/__init__.py
@@ -5,7 +5,7 @@ import voluptuous as vol
 
 from homeassistant.helpers import discovery
 import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_HOST, CONF_PASSWORD
+from homeassistant.const import (CONF_HOST, CONF_PASSWORD, EVENT_HOMEASSISTANT_STOP)
 
 DOMAIN = "velux"
 DATA_VELUX = "data_velux"
@@ -40,17 +40,25 @@ async def async_setup(hass, config):
         )
     return True
 
-
 class VeluxModule:
     """Abstraction for velux component."""
 
     def __init__(self, hass, config):
         """Initialize for velux component."""
+        
+        async def on_hass_stop(event):
+            """Close connection when hass stops."""
+            _LOGGER.info("velux interface terminated")
+            await self.pyvlx.disconnect()                    
+        
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, on_hass_stop)
+        #from custom_components.pyvlx import PyVLX                
         from pyvlx import PyVLX
-
         host = config[DOMAIN].get(CONF_HOST)
         password = config[DOMAIN].get(CONF_PASSWORD)
-        self.pyvlx = PyVLX(host=host, password=password)
+        self.pyvlx = PyVLX(
+            host=host,
+            password=password)
 
     async def async_start(self):
         """Start velux component."""
