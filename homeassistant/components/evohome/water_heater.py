6f51937dd9902eccdcbfa8e3aec838cc0aaca521
David Bonnes <david@bonnes.me>
2019-09-01 11:45:41 +0100

    Change evohome to asyncio client (#26042)
    * fully async now
    * add convergence (call update() 2 seconds after client API call) (issue#25400)
    * handle dead TRVs (e.g. flat battery)

--- homeassistant/components/evohome/water_heater.py
+++ homeassistant/components/evohome/water_heater.py
@@ -2,9 +2,6 @@
 import logging
 from typing import List
 
-import requests.exceptions
-import evohomeclient2
-
 from homeassistant.components.water_heater import (
     SUPPORT_OPERATION_MODE,
     WaterHeaterDevice,
@@ -12,7 +9,7 @@ from homeassistant.components.water_heater import (
 from homeassistant.const import PRECISION_WHOLE, STATE_OFF, STATE_ON
 from homeassistant.util.dt import parse_datetime
 
-from . import _handle_exception, EvoDevice
+from . import EvoDevice
 from .const import DOMAIN, EVO_STRFTIME, EVO_FOLLOW, EVO_TEMPOVER, EVO_PERMOVER
 
 _LOGGER = logging.getLogger(__name__)
@@ -23,8 +20,13 @@ EVO_STATE_TO_HA = {v: k for k, v in HA_STATE_TO_EVO.items()}
 HA_OPMODE_TO_DHW = {STATE_ON: EVO_FOLLOW, STATE_OFF: EVO_PERMOVER}
 
 
-def setup_platform(hass, hass_config, add_entities, discovery_info=None) -> None:
+async def async_setup_platform(
+    hass, config, async_add_entities, discovery_info=None
+) -> None:
     """Create the DHW controller."""
+    if discovery_info is None:
+        return
+
     broker = hass.data[DOMAIN]["broker"]
 
     _LOGGER.debug(
@@ -33,7 +35,7 @@ def setup_platform(hass, hass_config, add_entities, discovery_info=None) -> None
 
     evo_dhw = EvoDHW(broker, broker.tcs.hotwater)
 
-    add_entities([evo_dhw], update_before_add=True)
+    async_add_entities([evo_dhw], update_before_add=True)
 
 
 class EvoDHW(EvoDevice, WaterHeaterDevice):
@@ -59,6 +61,11 @@ class EvoDHW(EvoDevice, WaterHeaterDevice):
         self._operation_list = list(HA_OPMODE_TO_DHW)
 
     @property
+    def available(self) -> bool:
+        """Return True if entity is available."""
+        return self._evo_device.temperatureStatus.get("isAvailable", False)
+
+    @property
     def current_operation(self) -> str:
         """Return the current operating mode (On, or Off)."""
         return EVO_STATE_TO_HA[self._evo_device.stateStatus["state"]]
@@ -73,7 +80,7 @@ class EvoDHW(EvoDevice, WaterHeaterDevice):
         """Return the current temperature."""
         return self._evo_device.temperatureStatus["temperature"]
 
-    def set_operation_mode(self, operation_mode: str) -> None:
+    async def async_set_operation_mode(self, operation_mode: str) -> None:
         """Set new operation mode for a DHW controller."""
         op_mode = HA_OPMODE_TO_DHW[operation_mode]
 
@@ -81,17 +88,13 @@ class EvoDHW(EvoDevice, WaterHeaterDevice):
         until = None  # EVO_FOLLOW, EVO_PERMOVER
 
         if op_mode == EVO_TEMPOVER and self._schedule["DailySchedules"]:
-            self._update_schedule()
+            await self._update_schedule()
             if self._schedule["DailySchedules"]:
                 until = parse_datetime(self.setpoints["next"]["from"])
                 until = until.strftime(EVO_STRFTIME)
 
         data = {"Mode": op_mode, "State": state, "UntilTime": until}
 
-        try:
+        await self._call_client_api(
             self._evo_device._set_dhw(data)  # pylint: disable=protected-access
-        except (
-            requests.exceptions.RequestException,
-            evohomeclient2.AuthenticationError,
-        ) as err:
-            _handle_exception(err)
+        )
