8e251ce34e96f2e89a0b5a7d7d14adbb7e3aa994
Erik <erik@montnemery.com>
2019-09-02 19:18:52 +0200

    Add support for device actions, with light as example.

--- tests/components/light/test_device_automation.py
+++ tests/components/light/test_device_automation.py
@@ -46,6 +46,44 @@ def _same_lists(a, b):
     return True
 
 
+async def test_get_actions(hass, device_reg, entity_reg):
+    """Test we get the expected actions from a light."""
+    config_entry = MockConfigEntry(domain="test", data={})
+    config_entry.add_to_hass(hass)
+    device_entry = device_reg.async_get_or_create(
+        config_entry_id=config_entry.entry_id,
+        connections={(device_registry.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
+    )
+    entity_reg.async_get_or_create("light", "test", "5678", device_id=device_entry.id)
+    expected_actions = [
+        {
+            "device": None,
+            "domain": "light",
+            "type": "turn_off",
+            "device_id": device_entry.id,
+            "entity_id": "light.test_5678",
+        },
+        {
+            "device": None,
+            "domain": "light",
+            "type": "turn_on",
+            "device_id": device_entry.id,
+            "entity_id": "light.test_5678",
+        },
+        {
+            "device": None,
+            "domain": "light",
+            "type": "toggle",
+            "device_id": device_entry.id,
+            "entity_id": "light.test_5678",
+        },
+    ]
+    actions = await async_get_device_automations(
+        hass, "async_get_actions", device_entry.id
+    )
+    assert _same_lists(actions, expected_actions)
+
+
 async def test_get_conditions(hass, device_reg, entity_reg):
     """Test we get the expected conditions from a light."""
     config_entry = MockConfigEntry(domain="test", data={})
@@ -263,3 +301,78 @@ async def test_if_state(hass, calls):
     await hass.async_block_till_done()
     assert len(calls) == 2
     assert calls[1].data["some"] == "is_off event - test_event2"
+
+
+async def test_action(hass, calls):
+    """Test for turn_on and turn_off actions."""
+    platform = getattr(hass.components, "test.light")
+
+    platform.init()
+    assert await async_setup_component(
+        hass, light.DOMAIN, {light.DOMAIN: {CONF_PLATFORM: "test"}}
+    )
+
+    dev1, dev2, dev3 = platform.DEVICES
+
+    assert await async_setup_component(
+        hass,
+        automation.DOMAIN,
+        {
+            automation.DOMAIN: [
+                {
+                    "trigger": {"platform": "event", "event_type": "test_event1"},
+                    "action": {
+                        "device": None,
+                        "domain": "light",
+                        "entity_id": dev1.entity_id,
+                        "type": "turn_off",
+                    },
+                },
+                {
+                    "trigger": {"platform": "event", "event_type": "test_event2"},
+                    "action": {
+                        "device": None,
+                        "domain": "light",
+                        "entity_id": dev1.entity_id,
+                        "type": "turn_on",
+                    },
+                },
+                {
+                    "trigger": {"platform": "event", "event_type": "test_event3"},
+                    "action": {
+                        "device": None,
+                        "domain": "light",
+                        "entity_id": dev1.entity_id,
+                        "type": "toggle",
+                    },
+                },
+            ]
+        },
+    )
+    await hass.async_block_till_done()
+    assert hass.states.get(dev1.entity_id).state == STATE_ON
+    assert len(calls) == 0
+
+    hass.bus.async_fire("test_event1")
+    await hass.async_block_till_done()
+    assert hass.states.get(dev1.entity_id).state == STATE_OFF
+
+    hass.bus.async_fire("test_event1")
+    await hass.async_block_till_done()
+    assert hass.states.get(dev1.entity_id).state == STATE_OFF
+
+    hass.bus.async_fire("test_event2")
+    await hass.async_block_till_done()
+    assert hass.states.get(dev1.entity_id).state == STATE_ON
+
+    hass.bus.async_fire("test_event2")
+    await hass.async_block_till_done()
+    assert hass.states.get(dev1.entity_id).state == STATE_ON
+
+    hass.bus.async_fire("test_event3")
+    await hass.async_block_till_done()
+    assert hass.states.get(dev1.entity_id).state == STATE_OFF
+
+    hass.bus.async_fire("test_event3")
+    await hass.async_block_till_done()
+    assert hass.states.get(dev1.entity_id).state == STATE_ON
