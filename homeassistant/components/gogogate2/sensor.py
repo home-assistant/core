9b7ecef22a0d4089700d2ec17ec64b83ea423e6e
Kyle Rotte <krotte1@yahoo.com>
2019-09-02 02:37:59 -0600

    Updated comments, ran isort and black on code

--- homeassistant/components/gogogate2/sensor.py
+++ homeassistant/components/gogogate2/sensor.py
@@ -4,17 +4,15 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-
 from homeassistant.const import (
-    CONF_USERNAME,
-    CONF_PASSWORD,
     CONF_IP_ADDRESS,
     CONF_NAME,
+    CONF_PASSWORD,
+    CONF_USERNAME,
     TEMP_CELSIUS,
 )
-from homeassistant.helpers.entity import Entity
-
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -38,7 +36,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     from pygogogate2 import Gogogate2API as pygogogate2
 
     temp_unit = hass.config.units.temperature_unit
-    
+
     ip_address = config.get(CONF_IP_ADDRESS)
     name = config.get(CONF_NAME)
     password = config.get(CONF_PASSWORD)
@@ -51,7 +49,9 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         if devices is False:
             raise ValueError("Username or Password is incorrect or no devices found")
 
-        add_entities(MyGogogate2Thermostat(mygogogate2, door, temp_unit) for door in devices)
+        add_entities(
+            MyGogogate2Thermostat(mygogogate2, door, temp_unit) for door in devices
+        )
 
     except (TypeError, KeyError, NameError, ValueError) as ex:
         _LOGGER.error("%s", ex)
@@ -63,7 +63,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
             notification_id=NOTIFICATION_ID,
         )
 
-    
+
 class MyGogogate2Sensor(Entity):
     """Representation of a Gogogate2 temperature sensor."""
 
@@ -73,7 +73,7 @@ class MyGogogate2Sensor(Entity):
         self.device_id = device["door"]
         self._name = device["name"]
         self._temperature = device["temperature"]
-        self._temp_unit=temp_unit
+        self._temp_unit = temp_unit
 
     @property
     def name(self):
@@ -93,11 +93,11 @@ class MyGogogate2Sensor(Entity):
     def update(self):
         """Cache value from pygogogate2"""
         try:
-            # mygogogate2 always returns the temperature in fahrenheit
-            self._temperature=float(self.mygogogate2.get_temperature(self.device_id))
+            # mygogogate2 always returns the temperature in fahrenheit.
+            self._temperature = float(self.mygogogate2.get_temperature(self.device_id))
             if self._temp_unit == TEMP_CELSIUS:
-                # Convert output to celcius
-                self._temperature=(self._temperature-32.0)*5.0/9.0
+                # Convert the output to celcius.
+                self._temperature = (self._temperature - 32.0) * 5.0 / 9.0
         except (TypeError, KeyError, NameError, ValueError) as ex:
             _LOGGER.error("%s", ex)
             self._temperature = None
