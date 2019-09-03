0fccaddded4583cba715b5e24ba7f76bd2b70260
Taylor Silva <taylorsilva@outlook.com>
2019-09-03 09:41:03 -0400

    Update homeassistant/components/isy994/__init__.py
    Co-Authored-By: Martin Hjelmare <marhje52@kth.se>

--- homeassistant/components/isy994/__init__.py
+++ homeassistant/components/isy994/__init__.py
@@ -182,7 +182,7 @@ def _check_for_node_def(hass: HomeAssistant, node, single_domain: str = None) ->
             hass.data[ISY994_NODES][domain].append(node)
             return True
 
-    _LOGGER.warning("unsupported node: {node.name}, type: {node.type}")
+    _LOGGER.warning("Unsupported node: %s, type: %s", node.name, node.type)
     return False
 
 
