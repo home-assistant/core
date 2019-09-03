4ae8bbcb72765c9af541c9f7f96b89eef836dc52
Florent Thoumie <ft@fb.com>
2019-09-03 20:25:09 +0000

    Fix stale doctstrings and add unittest.

--- tests/components/iaqualink/test_config_flow.py
+++ tests/components/iaqualink/test_config_flow.py
@@ -12,7 +12,7 @@ DATA = {"username": "test@example.com", "password": "pass"}
 
 @pytest.mark.parametrize("step", ["import", "user"])
 async def test_already_configured(hass, step):
-    """Test if a HomeKit discovered bridge has already been configured."""
+    """Test config flow when iaqualink component is already setup."""
     MockConfigEntry(domain="iaqualink", data=DATA).add_to_hass(hass)
 
     flow = config_flow.AqualinkFlowHandler()
@@ -27,8 +27,24 @@ async def test_already_configured(hass, step):
 
 
 @pytest.mark.parametrize("step", ["import", "user"])
+async def test_without_config(hass, step):
+    """Test with no configuration."""
+    flow = config_flow.AqualinkFlowHandler()
+    flow.hass = hass
+    flow.context = {}
+
+    fname = f"async_step_{step}"
+    func = getattr(flow, fname)
+    result = await func()
+
+    assert result["type"] == "form"
+    assert result["step_id"] == "user"
+    assert result["errors"] == {}
+
+
+@pytest.mark.parametrize("step", ["import", "user"])
 async def test_with_invalid_credentials(hass, step):
-    """Test config flow ."""
+    """Test config flow with invalid username and/or password."""
     flow = config_flow.AqualinkFlowHandler()
     flow.hass = hass
 
@@ -46,7 +62,7 @@ async def test_with_invalid_credentials(hass, step):
 
 @pytest.mark.parametrize("step", ["import", "user"])
 async def test_with_existing_config(hass, step):
-    """Test importing a host with an existing config file."""
+    """Test with existing configuration."""
     flow = config_flow.AqualinkFlowHandler()
     flow.hass = hass
     flow.context = {}
