15d59d45e52fe51a61de26fea108f0aeeb5f92c2
gibman <gibman.dk@gmail.com>
2019-09-04 20:38:39 +0200

    logging casing fixed. code moved from init.

--- homeassistant/components/velux/__init__.py
+++ homeassistant/components/velux/__init__.py
@@ -28,6 +28,7 @@ async def async_setup(hass, config):
 
     try:
         hass.data[DATA_VELUX] = VeluxModule(hass, config)
+        hass.data[DATA_VELUX].setup()
         await hass.data[DATA_VELUX].async_start()
 
     except PyVLXException as ex:
@@ -40,24 +41,35 @@ async def async_setup(hass, config):
         )
     return True
 
+
 class VeluxModule:
     """Abstraction for velux component."""
 
     def __init__(self, hass, config):
         """Initialize for velux component."""
+        self._hass = hass
+        self._config = config
         
+
+    def setup(self):
         async def on_hass_stop(event):
             """Close connection when hass stops."""
-            _LOGGER.info("velux interface terminated")
-            await self.pyvlx.disconnect()                    
-        
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, on_hass_stop)                   
+            _LOGGER.info("Velux interface terminated")
+            await self.pyvlx.disconnect()
+            
+        self._hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, on_hass_stop)     
         from pyvlx import PyVLX
-        host = config[DOMAIN].get(CONF_HOST)
-        password = config[DOMAIN].get(CONF_PASSWORD)
+        host = self._config[DOMAIN].get(CONF_HOST)
+        password = self._config[DOMAIN].get(CONF_PASSWORD)
         self.pyvlx = PyVLX(host=host, password=password)
+
 
     async def async_start(self):
         """Start velux component."""
+        _LOGGER.info("Velux interface started")        
         await self.pyvlx.load_scenes()
         await self.pyvlx.load_nodes()
+                        
+        for node in self.pyvlx.nodes:
+          _LOGGER.info("Stopping node: " + str(node))
+          await node.stop(wait_for_completion=False)
