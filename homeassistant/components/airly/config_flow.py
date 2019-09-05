9c70f81e620e415b790be9504882ffdb6ba46476
bieniu <maciej.bieniek@gmail.com>
2019-09-05 09:26:47 +0200

    Change latitude and longitude to HA defaults

--- homeassistant/components/airly/config_flow.py
+++ homeassistant/components/airly/config_flow.py
@@ -85,8 +85,12 @@ class AirlyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {
                     vol.Required(CONF_API_KEY, default=api_key): str,
-                    vol.Required(CONF_LATITUDE, default=latitude): cv.latitude,
-                    vol.Required(CONF_LONGITUDE, default=longitude): cv.longitude,
+                    vol.Optional(
+                        CONF_LATITUDE, default=self.hass.config.latitude
+                    ): cv.latitude,
+                    vol.Optional(
+                        CONF_LONGITUDE, default=self.hass.config.longitude
+                    ): cv.longitude,
                     vol.Optional(CONF_NAME, default=name): str,
                     vol.Optional(CONF_LANGUAGE, default=language): str,
                 }
