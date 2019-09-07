c182902071dd20ec799c6120c0f317a667996d04
Indy Koning <indykoning@hotmail.com>
2019-09-07 20:23:50 +0200

    Added dynamic device classes.

--- homeassistant/components/growatt_server/sensor.py
+++ homeassistant/components/growatt_server/sensor.py
@@ -5,8 +5,8 @@ import logging
 import datetime
 
 import growattServer
-
 import voluptuous as vol
+
 from homeassistant.util import Throttle
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
@@ -21,32 +21,32 @@ DEFAULT_NAME = "Growatt"
 SCAN_INTERVAL = datetime.timedelta(minutes=5)
 
 TOTAL_SENSOR_TYPES = {
-    "total_money_today": ("Total money today", "€", "plantMoneyText"),
-    "total_money_total": ("Money lifetime", "€", "totalMoneyText"),
-    "total_energy_today": ("Energy Today", "kWh", "todayEnergy"),
-    "total_output_power": ("Output Power", "W", "invTodayPpv"),
-    "total_energy_output": ("Lifetime energy output", "kWh", "totalEnergy"),
-    "total_maximum_output": ("Maximum power", "W", "nominalPower"),
+    "total_money_today": ("Total money today", "€", "plantMoneyText", None),
+    "total_money_total": ("Money lifetime", "€", "totalMoneyText", None),
+    "total_energy_today": ("Energy Today", "kWh", "todayEnergy", "power"),
+    "total_output_power": ("Output Power", "W", "invTodayPpv", "power"),
+    "total_energy_output": ("Lifetime energy output", "kWh", "totalEnergy", "power"),
+    "total_maximum_output": ("Maximum power", "W", "nominalPower", "power"),
 }
 
 INVERTER_SENSOR_TYPES = {
-    "inverter_energy_today": ("Energy today", "kWh", "e_today"),
-    "inverter_energy_total": ("Lifetime energy output", "kWh", "e_total"),
-    "inverter_voltage_input_1": ("Input 1 voltage", "V", "vpv1"),
-    "inverter_amperage_input_1": ("Input 1 Amperage", "A", "ipv1"),
-    "inverter_wattage_input_1": ("Input 1 Wattage", "W", "ppv1"),
-    "inverter_voltage_input_2": ("Input 2 voltage", "V", "vpv2"),
-    "inverter_amperage_input_2": ("Input 2 Amperage", "A", "ipv2"),
-    "inverter_wattage_input_2": ("Input 2 Wattage", "W", "ppv2"),
-    "inverter_voltage_input_3": ("Input 3 voltage", "V", "vpv3"),
-    "inverter_amperage_input_3": ("Input 3 Amperage", "A", "ipv3"),
-    "inverter_wattage_input_3": ("Input 3 Wattage", "W", "ppv3"),
-    "inverter_internal_wattage": ("Internal wattage", "W", "ppv"),
-    "inverter_reactive_voltage": ("Reactive voltage", "V", "vacr"),
-    "inverter_inverter_reactive_amperage": ("Reactive amperage", "A", "iacr"),
-    "inverter_frequency": ("AC frequency", "Hz", "fac"),
-    "inverter_current_wattage": ("Output power", "W", "pac"),
-    "inverter_current_reactive_wattage": ("Reactive wattage", "W", "pacr"),
+    "inverter_energy_today": ("Energy today", "kWh", "e_today", "power"),
+    "inverter_energy_total": ("Lifetime energy output", "kWh", "e_total", "power"),
+    "inverter_voltage_input_1": ("Input 1 voltage", "V", "vpv1", None),
+    "inverter_amperage_input_1": ("Input 1 Amperage", "A", "ipv1", None),
+    "inverter_wattage_input_1": ("Input 1 Wattage", "W", "ppv1", "power"),
+    "inverter_voltage_input_2": ("Input 2 voltage", "V", "vpv2", None),
+    "inverter_amperage_input_2": ("Input 2 Amperage", "A", "ipv2", None),
+    "inverter_wattage_input_2": ("Input 2 Wattage", "W", "ppv2", "power"),
+    "inverter_voltage_input_3": ("Input 3 voltage", "V", "vpv3", None),
+    "inverter_amperage_input_3": ("Input 3 Amperage", "A", "ipv3", None),
+    "inverter_wattage_input_3": ("Input 3 Wattage", "W", "ppv3", "power"),
+    "inverter_internal_wattage": ("Internal wattage", "W", "ppv", "power"),
+    "inverter_reactive_voltage": ("Reactive voltage", "V", "vacr", None),
+    "inverter_inverter_reactive_amperage": ("Reactive amperage", "A", "iacr", None),
+    "inverter_frequency": ("AC frequency", "Hz", "fac", None),
+    "inverter_current_wattage": ("Output power", "W", "pac", "power"),
+    "inverter_current_reactive_wattage": ("Reactive wattage", "W", "pacr", "power"),
 }
 
 SENSOR_TYPES = {**TOTAL_SENSOR_TYPES, **INVERTER_SENSOR_TYPES}
@@ -139,7 +139,7 @@ class GrowattInverter(Entity):
     @property
     def device_class(self):
         """Return the device class of the sensor."""
-        return "power"
+        return SENSOR_TYPES[self.sensor][3]
 
     @property
     def unit_of_measurement(self):
