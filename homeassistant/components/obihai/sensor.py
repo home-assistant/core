07ae7aa70bc38d448508d4d5e096e6246223fe2b
Daniel Shokouhi <dshokouhi@gmail.com>
2019-09-09 16:35:20 -0700

    Review comments

--- homeassistant/components/obihai/sensor.py
+++ homeassistant/components/obihai/sensor.py
@@ -1,41 +1,46 @@
-"""Support for Obihai Sensors."""
+"""
+Support for Obihai Sensors.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/obihai/
+"""
 import logging
 
-from datetime import timedelta
 import voluptuous as vol
-
-from pyobihai import PyObihai
+from datetime import timedelta
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import STATE_UNKNOWN
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
 
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 
+from .pyobihai import PyObihai
 
 _LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(seconds=5)
 
-OBIHAI = "Obihai"
+DOMAIN = "Obihai"
 DEFAULT_USERNAME = "admin"
 DEFAULT_PASSWORD = "admin"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Required(CONF_HOST): cv.string,
+        vol.Optional(CONF_HOST): cv.string,
         vol.Optional(CONF_USERNAME, default=DEFAULT_USERNAME): cv.string,
         vol.Optional(CONF_PASSWORD, default=DEFAULT_PASSWORD): cv.string,
     }
 )
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Obihai sensor platform."""
 
-    username = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
-    host = config[CONF_HOST]
+    username = config.get(CONF_USERNAME, None)
+    password = config.get(CONF_PASSWORD, None)
+    host = config.get(CONF_HOST, None)
 
     sensors = []
 
@@ -46,26 +51,26 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     line_services = pyobihai.get_line_state(host, username, password)
 
     for key in services:
-        sensors.append(ObihaiServiceSensors(pyobihai, host, username, password, key))
+        sensors.append(ObihaiServiceSensors(host, username, password, key))
 
     for key in line_services:
-        sensors.append(ObihaiServiceSensors(pyobihai, host, username, password, key))
+        sensors.append(ObihaiServiceSensors(host, username, password, key))
 
-    add_entities(sensors)
+    add_devices(sensors)
 
 
 class ObihaiServiceSensors(Entity):
     """Get the status of each Obihai Lines."""
 
-    def __init__(self, pyobihai, host, username, password, service_name):
+    def __init__(self, host, username, password, service_name):
         """Initialize monitor sensor."""
         self._host = host
         self._username = username
         self._password = password
         self._service_name = service_name
         self._state = None
-        self._name = f"{OBIHAI} {self._service_name}"
-        self._pyobihai = pyobihai
+        self._name = "{} {}".format(DOMAIN, self._service_name)
+        self._pyobihai = PyObihai()
 
     @property
     def name(self):
@@ -77,13 +82,19 @@ class ObihaiServiceSensors(Entity):
         """Return the state of the sensor."""
         return self._state
 
+    @property
+    def device_class(self):
+        """Return the device class for uptime sensor."""
+        if self._service_name == "Last Reboot":
+            return "timestamp"
+
     def update(self):
         """Update the sensor."""
         services = self._pyobihai.get_state(self._host, self._username, self._password)
 
         if self._service_name in services:
             if services[self._service_name] is None:
-                self._state = None
+                self._state = STATE_UNKNOWN
             else:
                 self._state = services[self._service_name]
 
@@ -93,6 +104,6 @@ class ObihaiServiceSensors(Entity):
 
         if self._service_name in services:
             if services[self._service_name] is None:
-                self._state = None
+                self._state = STATE_UNKNOWN
             else:
                 self._state = services[self._service_name]
