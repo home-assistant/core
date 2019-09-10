12d789d0bd94bd04c5010e32346fd9234c81fd77
Robert Svensson <kane610@me.com>
2019-09-10 19:42:55 +0200

    See if device automation test passes in azure

--- tests/components/deconz/test_device_automation.py
+++ tests/components/deconz/test_device_automation.py
@@ -86,7 +86,6 @@ async def setup_deconz(hass, options):
 
 async def test_get_triggers(hass):
     """Test triggers work."""
-    return
     gateway = await setup_deconz(hass, options={})
     device_id = gateway.events[0].device_id
     triggers = await async_get_device_automations(hass, "async_get_triggers", device_id)
