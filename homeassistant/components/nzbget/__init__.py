deb771b17059858429ed5264827c618f49b40d8a
Chris Clark <chriscla@gmail.com>
2019-09-10 22:11:33 -0700

    Using pynzbgetapi

--- homeassistant/components/nzbget/__init__.py
+++ homeassistant/components/nzbget/__init__.py
@@ -15,7 +15,6 @@ from homeassistant.const import (
     CONF_SCAN_INTERVAL,
     CONF_SSL,
     CONF_USERNAME,
-    CONTENT_TYPE_JSON,
 )
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.dispatcher import dispatcher_send
@@ -70,24 +69,19 @@ CONFIG_SCHEMA = vol.Schema(
 def setup(hass, config):
     """Set up the NZBGet sensors."""
     host = config[DOMAIN][CONF_HOST]
-    port = config[DOMAIN].get(CONF_PORT)
-    ssl = "s" if config[DOMAIN].get(CONF_SSL) else ""
+    port = config[DOMAIN][CONF_PORT]
+    ssl = "s" if config[DOMAIN][CONF_SSL] else ""
     name = config[DOMAIN][CONF_NAME]
     username = config[DOMAIN].get(CONF_USERNAME)
     password = config[DOMAIN].get(CONF_PASSWORD)
     monitored_types = config[DOMAIN].get(CONF_MONITORED_VARIABLES)
     scan_interval = config[DOMAIN].get(CONF_SCAN_INTERVAL)
 
-    url = f"http{ssl}://{host}:{port}/jsonrpc"
-
     try:
-        nzbget_api = NZBGetAPI(api_url=url, username=username, password=password)
-        nzbget_api.status()
+        nzbget_api = pynzbgetapi.NZBGetAPI(host, username, password, ssl, ssl, port)
+        nzbget_api.version()
         _LOGGER.debug("Successfully validated NZBGet API connection.")
-    except (
-        requests.exceptions.ConnectionError,
-        requests.exceptions.HTTPError,
-    ) as conn_err:
+    except pynzbgetapi.NZBGetAPIException as conn_err:
         _LOGGER.error("Error setting up NZBGet API: %s", conn_err)
         return False
 
@@ -125,45 +119,3 @@ class NZBGetData:
         except requests.exceptions.ConnectionError:
             self.available = False
             _LOGGER.error("Unable to refresh NZBGet data")
-
-
-class NZBGetAPI:
-    """Simple JSON-RPC wrapper for NZBGet's API."""
-
-    def __init__(self, api_url, username=None, password=None):
-        """Initialize NZBGet API and set headers needed later."""
-        self.api_url = api_url
-        self.headers = {CONTENT_TYPE: CONTENT_TYPE_JSON}
-
-        if username is not None and password is not None:
-            self.auth = (username, password)
-        else:
-            self.auth = None
-
-    def post(self, method, params=None):
-        """Send a POST request and return the response as a dict."""
-        payload = {"method": method}
-
-        if params:
-            payload["params"] = params
-        try:
-            response = requests.post(
-                self.api_url,
-                json=payload,
-                auth=self.auth,
-                headers=self.headers,
-                timeout=5,
-            )
-            response.raise_for_status()
-            return response.json()
-        except requests.exceptions.ConnectionError as conn_exc:
-            _LOGGER.error(
-                "Failed to update NZBGet status from %s. Error: %s",
-                self.api_url,
-                conn_exc,
-            )
-            raise
-
-    def status(self):
-        """Update cached response."""
-        return self.post("status")["result"]
