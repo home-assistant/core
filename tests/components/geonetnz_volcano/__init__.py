a57f82cae3d86a632f353760c723071294b86bcf
Malte Franken <exxamalte@users.noreply.github.com>
2019-09-11 18:18:10 +1000

    adopted unit tests

--- tests/components/geonetnz_volcano/__init__.py
+++ tests/components/geonetnz_volcano/__init__.py
@@ -5,10 +5,10 @@ from unittest.mock import MagicMock
 def _generate_mock_feed_entry(
     external_id,
     title,
+    alert_level,
     distance_to_home,
     coordinates,
     attribution=None,
-    alert_level=None,
     activity=None,
     hazards=None,
 ):
@@ -16,10 +16,10 @@ def _generate_mock_feed_entry(
     feed_entry = MagicMock()
     feed_entry.external_id = external_id
     feed_entry.title = title
+    feed_entry.alert_level = alert_level
     feed_entry.distance_to_home = distance_to_home
     feed_entry.coordinates = coordinates
     feed_entry.attribution = attribution
-    feed_entry.alert_level = alert_level
     feed_entry.activity = activity
     feed_entry.hazards = hazards
     return feed_entry
--- tests/components/geonetnz_volcano/test_sensor.py
+++ tests/components/geonetnz_volcano/test_sensor.py
@@ -2,22 +2,25 @@
 from asynctest import patch, CoroutineMock
 
 from homeassistant.components import geonetnz_volcano
+from homeassistant.components.geo_location import ATTR_DISTANCE
 from homeassistant.components.geonetnz_volcano import DEFAULT_SCAN_INTERVAL
-from homeassistant.components.geonetnz_volcano.sensor import (
-    ATTR_STATUS,
-    ATTR_LAST_UPDATE,
-    ATTR_CREATED,
-    ATTR_UPDATED,
-    ATTR_REMOVED,
-    ATTR_LAST_UPDATE_SUCCESSFUL,
+from homeassistant.components.geonetnz_volcano.const import (
+    ATTR_ACTIVITY,
+    ATTR_EXTERNAL_ID,
+    ATTR_HAZARDS,
 )
 from homeassistant.const import (
     EVENT_HOMEASSISTANT_START,
     CONF_RADIUS,
+    ATTR_LATITUDE,
+    ATTR_LONGITUDE,
+    ATTR_FRIENDLY_NAME,
     ATTR_UNIT_OF_MEASUREMENT,
+    ATTR_ATTRIBUTION,
     ATTR_ICON,
 )
 from homeassistant.setup import async_setup_component
+from homeassistant.util.unit_system import IMPERIAL_SYSTEM
 from tests.common import async_fire_time_changed
 import homeassistant.util.dt as dt_util
 from tests.components.geonetnz_volcano import _generate_mock_feed_entry
@@ -31,16 +34,16 @@ async def test_setup(hass):
     mock_entry_1 = _generate_mock_feed_entry(
         "1234",
         "Title 1",
+        1,
         15.5,
         (38.0, -3.0),
         attribution="Attribution 1",
-        alert_level="Alert Level 1",
         activity="Activity 1",
         hazards="Hazards 1",
     )
-    mock_entry_2 = _generate_mock_feed_entry("2345", "Title 2", 20.5, (38.1, -3.1))
-    mock_entry_3 = _generate_mock_feed_entry("3456", "Title 3", 25.5, (38.2, -3.2))
-    mock_entry_4 = _generate_mock_feed_entry("4567", "Title 4", 12.5, (38.3, -3.3))
+    mock_entry_2 = _generate_mock_feed_entry("2345", "Title 2", 0, 20.5, (38.1, -3.1))
+    mock_entry_3 = _generate_mock_feed_entry("3456", "Title 3", 2, 25.5, (38.2, -3.2))
+    mock_entry_4 = _generate_mock_feed_entry("4567", "Title 4", 1, 12.5, (38.3, -3.3))
 
     # Patching 'utcnow' to gain more control over the timed update.
     utcnow = dt_util.utcnow()
@@ -55,20 +58,42 @@ async def test_setup(hass):
 
         all_states = hass.states.async_all()
         # 3 geolocation and 1 sensor entities
-        assert len(all_states) == 4
+        assert len(all_states) == 3
 
-        state = hass.states.get("sensor.geonet_nz_volcano_32_87336_117_22743")
+        state = hass.states.get("sensor.volcano_title_1")
         assert state is not None
-        assert int(state.state) == 3
-        assert state.name == "GeoNet NZ Volcano (32.87336, -117.22743)"
-        attributes = state.attributes
-        assert attributes[ATTR_STATUS] == "OK"
-        assert attributes[ATTR_CREATED] == 3
-        assert attributes[ATTR_LAST_UPDATE].tzinfo == dt_util.UTC
-        assert attributes[ATTR_LAST_UPDATE_SUCCESSFUL].tzinfo == dt_util.UTC
-        assert attributes[ATTR_LAST_UPDATE] == attributes[ATTR_LAST_UPDATE_SUCCESSFUL]
-        assert attributes[ATTR_UNIT_OF_MEASUREMENT] == "volcanos"
-        assert attributes[ATTR_ICON] == "mdi:mountain"
+        assert state.name == "Volcano Title 1"
+        assert int(state.state) == 1
+        assert state.attributes[ATTR_EXTERNAL_ID] == "1234"
+        assert state.attributes[ATTR_LATITUDE] == 38.0
+        assert state.attributes[ATTR_LONGITUDE] == -3.0
+        assert state.attributes[ATTR_DISTANCE] == 15.5
+        assert state.attributes[ATTR_FRIENDLY_NAME] == "Volcano Title 1"
+        assert state.attributes[ATTR_ATTRIBUTION] == "Attribution 1"
+        assert state.attributes[ATTR_ACTIVITY] == "Activity 1"
+        assert state.attributes[ATTR_HAZARDS] == "Hazards 1"
+        assert state.attributes[ATTR_UNIT_OF_MEASUREMENT] == "alert level"
+        assert state.attributes[ATTR_ICON] == "mdi:image-filter-hdr"
+
+        state = hass.states.get("sensor.volcano_title_2")
+        assert state is not None
+        assert state.name == "Volcano Title 2"
+        assert int(state.state) == 0
+        assert state.attributes[ATTR_EXTERNAL_ID] == "2345"
+        assert state.attributes[ATTR_LATITUDE] == 38.1
+        assert state.attributes[ATTR_LONGITUDE] == -3.1
+        assert state.attributes[ATTR_DISTANCE] == 20.5
+        assert state.attributes[ATTR_FRIENDLY_NAME] == "Volcano Title 2"
+
+        state = hass.states.get("sensor.volcano_title_3")
+        assert state is not None
+        assert state.name == "Volcano Title 3"
+        assert int(state.state) == 2
+        assert state.attributes[ATTR_EXTERNAL_ID] == "3456"
+        assert state.attributes[ATTR_LATITUDE] == 38.2
+        assert state.attributes[ATTR_LONGITUDE] == -3.2
+        assert state.attributes[ATTR_DISTANCE] == 25.5
+        assert state.attributes[ATTR_FRIENDLY_NAME] == "Volcano Title 3"
 
         # Simulate an update - two existing, one new entry, one outdated entry
         mock_feed_update.return_value = "OK", [mock_entry_1, mock_entry_4, mock_entry_3]
@@ -76,13 +101,7 @@ async def test_setup(hass):
         await hass.async_block_till_done()
 
         all_states = hass.states.async_all()
-        assert len(all_states) == 4
-
-        state = hass.states.get("sensor.geonet_nz_volcano_32_87336_117_22743")
-        attributes = state.attributes
-        assert attributes[ATTR_CREATED] == 1
-        assert attributes[ATTR_UPDATED] == 2
-        assert attributes[ATTR_REMOVED] == 1
+        assert len(all_states) == 3
 
         # Simulate an update - empty data, but successful update,
         # so no changes to entities.
@@ -91,7 +110,7 @@ async def test_setup(hass):
         await hass.async_block_till_done()
 
         all_states = hass.states.async_all()
-        assert len(all_states) == 4
+        assert len(all_states) == 3
 
         # Simulate an update - empty data, removes all entities
         mock_feed_update.return_value = "ERROR", None
@@ -99,8 +118,45 @@ async def test_setup(hass):
         await hass.async_block_till_done()
 
         all_states = hass.states.async_all()
+        assert len(all_states) == 0
+
+
+async def test_setup_imperial(hass):
+    """Test the setup of the integration using imperial unit system."""
+    hass.config.units = IMPERIAL_SYSTEM
+    # Set up some mock feed entries for this test.
+    mock_entry_1 = _generate_mock_feed_entry("1234", "Title 1", 1, 15.5, (38.0, -3.0))
+
+    # Patching 'utcnow' to gain more control over the timed update.
+    utcnow = dt_util.utcnow()
+    with patch("homeassistant.util.dt.utcnow", return_value=utcnow), patch(
+        "aio_geojson_client.feed.GeoJsonFeed.update", new_callable=CoroutineMock
+    ) as mock_feed_update, patch(
+        "aio_geojson_client.feed.GeoJsonFeed.__init__",
+        new_callable=CoroutineMock,
+        create=True,
+    ) as mock_feed_init:
+        mock_feed_update.return_value = "OK", [mock_entry_1]
+        assert await async_setup_component(hass, geonetnz_volcano.DOMAIN, CONFIG)
+        # Artificially trigger update and collect events.
+        hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
+        await hass.async_block_till_done()
+
+        all_states = hass.states.async_all()
         assert len(all_states) == 1
 
-        state = hass.states.get("sensor.geonet_nz_volcano_32_87336_117_22743")
-        attributes = state.attributes
-        assert attributes[ATTR_REMOVED] == 3
+        # Test conversion of 200 miles to kilometers.
+        assert mock_feed_init.call_args[1].get("filter_radius") == 321.8688
+
+        state = hass.states.get("sensor.volcano_title_1")
+        assert state is not None
+        assert state.name == "Volcano Title 1"
+        assert int(state.state) == 1
+        assert state.attributes[ATTR_EXTERNAL_ID] == "1234"
+        assert state.attributes[ATTR_LATITUDE] == 38.0
+        assert state.attributes[ATTR_LONGITUDE] == -3.0
+        assert state.attributes[ATTR_DISTANCE] == 9.6
+        assert state.attributes[ATTR_FRIENDLY_NAME] == "Volcano Title 1"
+        # assert state.attributes[ATTR_ATTRIBUTION] == "Attribution 1"
+        assert state.attributes[ATTR_UNIT_OF_MEASUREMENT] == "alert level"
+        assert state.attributes[ATTR_ICON] == "mdi:image-filter-hdr"
