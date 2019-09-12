95f707057eefb4cc5ed7ed4ad0ea7e3d0b164061
Mark Coombes <mark@markcoombes.ca>
2019-09-12 16:12:41 -0400

    Update constants for ecobee

--- homeassistant/components/ecobee/const.py
+++ homeassistant/components/ecobee/const.py
@@ -1,6 +1,14 @@
 """Constants for the ecobee integration."""
+import logging
 
 DOMAIN = "ecobee"
-ECOBEE_CONFIG_FILE = "ecobee.conf"
+DATA_ECOBEE_CONFIG = "ecobee_config"
 
 CONF_HOLD_TEMP = "hold_temp"
+DEFAULT_HOLD_TEMP = False
+
+CONF_REFRESH_TOKEN = "refresh_token"
+
+ECOBEE_PLATFORMS = ["binary_sensor", "climate", "sensor", "weather"]
+
+_LOGGER = logging.getLogger(__package__)
