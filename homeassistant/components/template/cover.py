57b983d0f2641e51d5a5cdce4479df8ea9e7b4e4
Gil Peeters <gil@cancas.com>
2019-09-14 03:02:18 +0000

    Fix import order (pylint)

--- homeassistant/components/template/cover.py
+++ homeassistant/components/template/cover.py
@@ -33,12 +33,12 @@ from homeassistant.const import (
     STATE_OPEN,
     STATE_CLOSED,
 )
-from .const import CONF_AVAILABILITY_TEMPLATE
 from homeassistant.exceptions import TemplateError
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import async_generate_entity_id
 from homeassistant.helpers.event import async_track_state_change
 from homeassistant.helpers.script import Script
+from .const import CONF_AVAILABILITY_TEMPLATE
 
 _LOGGER = logging.getLogger(__name__)
 _VALID_STATES = [STATE_OPEN, STATE_CLOSED, "true", "false"]
