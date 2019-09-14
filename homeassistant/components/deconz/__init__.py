6825117de605ac85404a3709e3ec622f42ba53cf
Robert Svensson <kane610@me.com>
2019-09-14 20:05:01 +0200

    Remove mechanisms to import a configuration from configuration.yaml

--- homeassistant/components/deconz/__init__.py
+++ homeassistant/components/deconz/__init__.py
@@ -1,48 +1,20 @@
 """Support for deCONZ devices."""
 import voluptuous as vol
 
-from homeassistant import config_entries
-from homeassistant.const import (
-    CONF_API_KEY,
-    CONF_HOST,
-    CONF_PORT,
-    EVENT_HOMEASSISTANT_STOP,
-)
-from homeassistant.helpers import config_validation as cv
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 
 from .config_flow import get_master_gateway
-from .const import CONF_BRIDGEID, CONF_MASTER_GATEWAY, DEFAULT_PORT, DOMAIN
+from .const import CONF_BRIDGEID, CONF_MASTER_GATEWAY, DOMAIN
 from .gateway import DeconzGateway
 from .services import async_setup_services, async_unload_services
 
 CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Optional(CONF_API_KEY): cv.string,
-                vol.Optional(CONF_HOST): cv.string,
-                vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
+    {DOMAIN: vol.Schema({}, extra=vol.ALLOW_EXTRA)}, extra=vol.ALLOW_EXTRA
 )
 
 
 async def async_setup(hass, config):
-    """Load configuration for deCONZ component.
-
-    Discovery has loaded the component if DOMAIN is not present in config.
-    """
-    if not hass.config_entries.async_entries(DOMAIN) and DOMAIN in config:
-        deconz_config = config[DOMAIN]
-        hass.async_create_task(
-            hass.config_entries.flow.async_init(
-                DOMAIN,
-                context={"source": config_entries.SOURCE_IMPORT},
-                data=deconz_config,
-            )
-        )
+    """Old way of setting up deCONZ integrations."""
     return True
 
 
