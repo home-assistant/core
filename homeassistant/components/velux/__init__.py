dbd0392cf0c145a05b9007e9ed9fc736422aa326
gibman <gibman.dk@gmail.com>
2019-09-16 08:47:52 +0200

    D400 First line should end with a period

--- homeassistant/components/velux/__init__.py
+++ homeassistant/components/velux/__init__.py
@@ -51,7 +51,7 @@ class VeluxModule:
         self._domain_config = domain_config
 
     def setup(self):
-        """Velux component setup"""
+        """Velux component setup."""
         async def on_hass_stop(event):
             """Close connection when hass stops."""
             _LOGGER.debug("Velux interface terminated")
