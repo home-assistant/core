5b0cbad953090e3a787d9308862d41b33426ad14
Maikel Punie <maikel.punie@gmail.com>
2019-09-17 22:39:46 +0200

    Fix cert expiry config flow check and update (#26638)
    * Fix typo in translations
    
    * Work on bug #26619
    
    * readd the homeassistant.start event
    
    * Remove the callback
    
    * Added the executor_job for _test_connection
    
    * Update test_config_flow.py

--- homeassistant/components/cert_expiry/__init__.py
+++ homeassistant/components/cert_expiry/__init__.py
@@ -1,7 +1,5 @@
 """The cert_expiry component."""
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import EVENT_HOMEASSISTANT_START
-from homeassistant.core import callback
 from homeassistant.helpers.typing import HomeAssistantType
 
 
@@ -13,13 +11,7 @@ async def async_setup(hass, config):
 async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
     """Load the saved entities."""
 
-    @callback
-    def async_start(_):
-        """Load the entry after the start event."""
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, "sensor")
-        )
-
-    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, async_start)
-
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
