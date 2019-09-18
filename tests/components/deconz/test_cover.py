8a39924b37486bf806467eafefaa654c33ada45b
Robert Svensson <Kane610@users.noreply.github.com>
2019-09-18 12:47:26 +0200

    deCONZ improve light tests (#26697)
    * Improve light tests
    * Small improvements on light and group classes

--- tests/components/deconz/test_cover.py
+++ tests/components/deconz/test_cover.py
@@ -1,82 +1,83 @@
 """deCONZ cover platform tests."""
-from unittest.mock import Mock, patch
+from copy import deepcopy
+
+from asynctest import patch
 
 from homeassistant import config_entries
 from homeassistant.components import deconz
-from homeassistant.components.deconz.const import COVER_TYPES
-from homeassistant.helpers.dispatcher import async_dispatcher_send
 from homeassistant.setup import async_setup_component
 
 import homeassistant.components.cover as cover
 
-from tests.common import mock_coro
-
-SUPPORTED_COVERS = {
+COVERS = {
     "1": {
-        "id": "Cover 1 id",
-        "name": "Cover 1 name",
+        "id": "Level controllable cover id",
+        "name": "Level controllable cover",
         "type": "Level controllable output",
         "state": {"bri": 255, "on": False, "reachable": True},
         "modelid": "Not zigbee spec",
         "uniqueid": "00:00:00:00:00:00:00:00-00",
     },
     "2": {
-        "id": "Cover 2 id",
-        "name": "Cover 2 name",
+        "id": "Window covering device id",
+        "name": "Window covering device",
         "type": "Window covering device",
         "state": {"bri": 255, "on": True, "reachable": True},
         "modelid": "lumi.curtain",
+        "uniqueid": "00:00:00:00:00:00:00:01-00",
+    },
+    "3": {
+        "id": "Unsupported cover id",
+        "name": "Unsupported cover",
+        "type": "Not a cover",
+        "state": {"reachable": True},
+        "uniqueid": "00:00:00:00:00:00:00:02-00",
     },
 }
 
-UNSUPPORTED_COVER = {
-    "1": {
-        "id": "Cover id",
-        "name": "Unsupported switch",
-        "type": "Not a cover",
-        "state": {},
-    }
-}
-
+BRIDGEID = "0123456789"
 
 ENTRY_CONFIG = {
-    deconz.const.CONF_ALLOW_CLIP_SENSOR: True,
-    deconz.const.CONF_ALLOW_DECONZ_GROUPS: True,
     deconz.config_flow.CONF_API_KEY: "ABCDEF",
-    deconz.config_flow.CONF_BRIDGEID: "0123456789",
+    deconz.config_flow.CONF_BRIDGEID: BRIDGEID,
     deconz.config_flow.CONF_HOST: "1.2.3.4",
     deconz.config_flow.CONF_PORT: 80,
 }
 
+DECONZ_CONFIG = {
+    "bridgeid": BRIDGEID,
+    "mac": "00:11:22:33:44:55",
+    "name": "deCONZ mock gateway",
+    "sw_version": "2.05.69",
+    "websocketport": 1234,
+}
 
-async def setup_gateway(hass, data):
-    """Load the deCONZ cover platform."""
-    from pydeconz import DeconzSession
+DECONZ_WEB_REQUEST = {"config": DECONZ_CONFIG}
 
-    loop = Mock()
-    session = Mock()
 
+async def setup_deconz_integration(hass, config, options, get_state_response):
+    """Create the deCONZ gateway."""
     config_entry = config_entries.ConfigEntry(
-        1,
-        deconz.DOMAIN,
-        "Mock Title",
-        ENTRY_CONFIG,
-        "test",
-        config_entries.CONN_CLASS_LOCAL_PUSH,
+        version=1,
+        domain=deconz.DOMAIN,
+        title="Mock Title",
+        data=config,
+        source="test",
+        connection_class=config_entries.CONN_CLASS_LOCAL_PUSH,
         system_options={},
+        options=options,
+        entry_id="1",
     )
-    gateway = deconz.DeconzGateway(hass, config_entry)
-    gateway.api = DeconzSession(loop, session, **config_entry.data)
-    gateway.api.config = Mock()
-    hass.data[deconz.DOMAIN] = {gateway.bridgeid: gateway}
 
-    with patch("pydeconz.DeconzSession.async_get_state", return_value=mock_coro(data)):
-        await gateway.api.async_load_parameters()
-
-    await hass.config_entries.async_forward_entry_setup(config_entry, "cover")
-    # To flush out the service call to update the group
+    with patch(
+        "pydeconz.DeconzSession.async_get_state", return_value=get_state_response
+    ), patch("pydeconz.DeconzSession.start", return_value=True):
+        await deconz.async_setup_entry(hass, config_entry)
     await hass.async_block_till_done()
-    return gateway
+
+    hass.config_entries._entries.append(config_entry)
+
+    return hass.data[deconz.DOMAIN][config[deconz.CONF_BRIDGEID]]
 
 
 async def test_platform_manually_configured(hass):
@@ -92,64 +93,69 @@ async def test_platform_manually_configured(hass):
 
 async def test_no_covers(hass):
     """Test that no cover entities are created."""
-    gateway = await setup_gateway(hass, {})
-    assert not hass.data[deconz.DOMAIN][gateway.bridgeid].deconz_ids
+    data = deepcopy(DECONZ_WEB_REQUEST)
+    gateway = await setup_deconz_integration(
+        hass, ENTRY_CONFIG, options={}, get_state_response=data
+    )
+    assert len(gateway.deconz_ids) == 0
     assert len(hass.states.async_all()) == 0
 
 
 async def test_cover(hass):
     """Test that all supported cover entities are created."""
-    with patch("pydeconz.DeconzSession.async_put_state", return_value=mock_coro(True)):
-        gateway = await setup_gateway(hass, {"lights": SUPPORTED_COVERS})
-    assert "cover.cover_1_name" in gateway.deconz_ids
-    assert len(SUPPORTED_COVERS) == len(COVER_TYPES)
-    assert len(hass.states.async_all()) == 3
-
-    cover_1 = hass.states.get("cover.cover_1_name")
-    assert cover_1 is not None
-    assert cover_1.state == "open"
-
-    gateway.api.lights["1"].async_update({})
-
-    await hass.services.async_call(
-        "cover", "open_cover", {"entity_id": "cover.cover_1_name"}, blocking=True
+    data = deepcopy(DECONZ_WEB_REQUEST)
+    data["lights"] = deepcopy(COVERS)
+    gateway = await setup_deconz_integration(
+        hass, ENTRY_CONFIG, options={}, get_state_response=data
     )
-    await hass.services.async_call(
-        "cover", "close_cover", {"entity_id": "cover.cover_1_name"}, blocking=True
-    )
-    await hass.services.async_call(
-        "cover", "stop_cover", {"entity_id": "cover.cover_1_name"}, blocking=True
-    )
+    assert "cover.level_controllable_cover" in gateway.deconz_ids
+    assert "cover.window_covering_device" in gateway.deconz_ids
+    assert "cover.unsupported_cover" not in gateway.deconz_ids
+    assert len(hass.states.async_all()) == 5
 
-    await hass.services.async_call(
-        "cover", "close_cover", {"entity_id": "cover.cover_2_name"}, blocking=True
-    )
+    level_controllable_cover = hass.states.get("cover.level_controllable_cover")
+    assert level_controllable_cover.state == "open"
 
+    level_controllable_cover_device = gateway.api.lights["1"]
 
-async def test_add_new_cover(hass):
-    """Test successful creation of cover entity."""
-    data = {}
-    gateway = await setup_gateway(hass, data)
-    cover = Mock()
-    cover.name = "name"
-    cover.type = "Level controllable output"
-    cover.uniqueid = "1"
-    cover.register_async_callback = Mock()
-    async_dispatcher_send(hass, gateway.async_signal_new_device("light"), [cover])
+    level_controllable_cover_device.async_update({"state": {"on": True}})
     await hass.async_block_till_done()
-    assert "cover.name" in gateway.deconz_ids
 
+    level_controllable_cover = hass.states.get("cover.level_controllable_cover")
+    assert level_controllable_cover.state == "closed"
 
-async def test_unsupported_cover(hass):
-    """Test that unsupported covers are not created."""
-    await setup_gateway(hass, {"lights": UNSUPPORTED_COVER})
-    assert len(hass.states.async_all()) == 0
+    with patch.object(
+        level_controllable_cover_device, "_async_set_callback", return_value=True
+    ) as set_callback:
+        await hass.services.async_call(
+            cover.DOMAIN,
+            cover.SERVICE_OPEN_COVER,
+            {"entity_id": "cover.level_controllable_cover"},
+            blocking=True,
+        )
+        await hass.async_block_till_done()
+        set_callback.assert_called_with("/lights/1/state", {"on": False})
 
+    with patch.object(
+        level_controllable_cover_device, "_async_set_callback", return_value=True
+    ) as set_callback:
+        await hass.services.async_call(
+            cover.DOMAIN,
+            cover.SERVICE_CLOSE_COVER,
+            {"entity_id": "cover.level_controllable_cover"},
+            blocking=True,
+        )
+        await hass.async_block_till_done()
+        set_callback.assert_called_with("/lights/1/state", {"on": True, "bri": 255})
 
-async def test_unload_cover(hass):
-    """Test that it works to unload switch entities."""
-    gateway = await setup_gateway(hass, {"lights": SUPPORTED_COVERS})
-
-    await gateway.async_reset()
-
-    assert len(hass.states.async_all()) == 1
+    with patch.object(
+        level_controllable_cover_device, "_async_set_callback", return_value=True
+    ) as set_callback:
+        await hass.services.async_call(
+            cover.DOMAIN,
+            cover.SERVICE_STOP_COVER,
+            {"entity_id": "cover.level_controllable_cover"},
+            blocking=True,
+        )
+        await hass.async_block_till_done()
+        set_callback.assert_called_with("/lights/1/state", {"bri_inc": 0})
