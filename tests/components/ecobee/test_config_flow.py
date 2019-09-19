c4a4adfdc345a594867a08b19d974698f3880133
Mark Coombes <mark@markcoombes.ca>
2019-09-19 18:11:37 -0400

    Test full implementation

--- tests/components/ecobee/test_config_flow.py
+++ tests/components/ecobee/test_config_flow.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 from homeassistant import data_entry_flow
 from homeassistant.components.ecobee import config_flow
+from homeassistant.components.ecobee.const import DATA_ECOBEE_CONFIG
 from homeassistant.const import CONF_API_KEY
 
 from homeassistant.components.ecobee.const import CONF_REFRESH_TOKEN
@@ -24,26 +25,28 @@ async def test_full_flow_implementation(hass):
     """Test full ecobee flow works."""
     flow = config_flow.EcobeeFlowHandler()
     flow.hass = hass
+    flow.hass.data[DATA_ECOBEE_CONFIG] = {}
 
     result = await flow.async_step_user()
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
 
-    with patch("config_flow.Ecobee") as mock_ecobee:
-        mock_ecobee.get_pin.return_value = True
-        mock_ecobee.pin = "test_pin"
+    with patch("homeassistant.components.ecobee.config_flow.Ecobee") as mock_ecobee:
+        mock_ecobee.request_pin.return_value = True
+        mock_ecobee.get_tokens.return_value = True
+        mock_ecobee.return_value.pin = "test_pin"
+        mock_ecobee.return_value.refresh_token = "reftoken"
+        mock_ecobee.return_value.api_key = "abcdef123456"
+
         result = await flow.async_step_user(user_input={CONF_API_KEY: "abcdef123456"})
 
         assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
         assert result["step_id"] == "authorize"
         assert result["description_placeholders"] == {"pin": "test_pin"}
 
-        mock_ecobee.get_tokens.return_value = True
-        mock_ecobee.refresh_token = "reftoken"
-
         result = await flow.async_step_authorize(user_input={})
 
         assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-        assert result["data"]["title"] == "ecobee"
+        assert result["title"] == "ecobee"
         assert result["data"][CONF_API_KEY] == "abcdef123456"
         assert result["data"][CONF_REFRESH_TOKEN] == "reftoken"
