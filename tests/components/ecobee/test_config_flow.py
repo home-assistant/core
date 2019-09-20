04eeb1c6623c31fb27152cc82cc07c54d17d2c95
Mark Coombes <mark@markcoombes.ca>
2019-09-19 23:04:23 -0400

    Update test_config_flow.py

--- tests/components/ecobee/test_config_flow.py
+++ tests/components/ecobee/test_config_flow.py
@@ -1,12 +1,10 @@
 """Tests for the ecobee config flow."""
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from homeassistant import data_entry_flow
 from homeassistant.components.ecobee import config_flow
 from homeassistant.components.ecobee.const import DATA_ECOBEE_CONFIG
 from homeassistant.const import CONF_API_KEY
-
-from homeassistant.components.ecobee.const import CONF_REFRESH_TOKEN
 
 
 async def test_abort_if_already_setup(hass):
@@ -21,8 +19,8 @@ async def test_abort_if_already_setup(hass):
     assert result["reason"] == "one_instance_only"
 
 
-async def test_full_flow_implementation(hass):
-    """Test full ecobee flow works."""
+async def test_user_step_without_user_input(hass):
+    """Test expected result if user step is called."""
     flow = config_flow.EcobeeFlowHandler()
     flow.hass = hass
     flow.hass.data[DATA_ECOBEE_CONFIG] = {}
@@ -31,22 +29,37 @@ async def test_full_flow_implementation(hass):
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
 
-    with patch("homeassistant.components.ecobee.config_flow.Ecobee") as mock_ecobee:
-        mock_ecobee.request_pin.return_value = True
-        mock_ecobee.get_tokens.return_value = True
-        mock_ecobee.return_value.pin = "test_pin"
-        mock_ecobee.return_value.refresh_token = "reftoken"
-        mock_ecobee.return_value.api_key = "abcdef123456"
 
-        result = await flow.async_step_user(user_input={CONF_API_KEY: "abcdef123456"})
+async def test_pin_request_succeeds(hass):
+    """Test expected result if pin request succeeds."""
+    flow = config_flow.EcobeeFlowHandler()
+    flow.hass = hass
+    flow.hass.data[DATA_ECOBEE_CONFIG] = {}
 
-        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-        assert result["step_id"] == "authorize"
-        assert result["description_placeholders"] == {"pin": "test_pin"}
+    mock_ecobee = Mock()
+    mock_ecobee.request_pin.return_value = True
+    mock_ecobee.return_value.pin = "test-pin"
+    config_flow.Ecobee = mock_ecobee
 
-        result = await flow.async_step_authorize(user_input={})
+    result = await flow.async_step_user(user_input={CONF_API_KEY: "api-key"})
 
-        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-        assert result["title"] == "ecobee"
-        assert result["data"][CONF_API_KEY] == "abcdef123456"
-        assert result["data"][CONF_REFRESH_TOKEN] == "reftoken"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "authorize"
+    assert result["description_placeholders"] == {"pin": "test-pin"}
+
+
+async def test_pin_request_fails(hass):
+    """Test expected result if pin request fails."""
+    flow = config_flow.EcobeeFlowHandler()
+    flow.hass = hass
+    flow.hass.data[DATA_ECOBEE_CONFIG] = {}
+
+    mock_ecobee = Mock()
+    mock_ecobee.request_pin.return_value = False
+    config_flow.Ecobee = mock_ecobee
+
+    result = await flow.async_step_user(user_input={CONF_API_KEY: "api-key"})
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "user"
+    assert result["errors"]["base"] == "pin_request_failed"
