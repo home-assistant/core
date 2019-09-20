064948a362dcfc23f26e4831fc5feb9076bbeb6f
scheric <38077357+scheric@users.noreply.github.com>
2019-09-20 15:40:39 +0200

    Update units

--- homeassistant/components/solaredge_local/sensor.py
+++ homeassistant/components/solaredge_local/sensor.py
@@ -8,7 +8,7 @@ from solaredge_local import SolarEdge
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_IP_ADDRESS, CONF_NAME, POWER_WATT, ENERGY_WATT_HOUR
+from homeassistant.const import CONF_IP_ADDRESS, CONF_NAME, POWER_WATT, ENERGY_WATT_HOUR, TEMP_CELSIUS 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
@@ -41,7 +41,7 @@ SENSOR_TYPES = {
     "inverter_temperature": [
         "invertertemperature",
         "Inverter Temperature",
-        "C",
+        TEMP_CELSIUS,
         "mdi:thermometer",
     ],
     "lifetime_energy": [
@@ -65,7 +65,7 @@ SENSOR_TYPES = {
     "optimizer_temperature": [
         "optimizertemperature",
         "Avrage Optimizer Temperature",
-        "C",
+        TEMP_CELSIUS ,
         "mdi:solar-panel",
     ],
     "optimizer_voltage": [
