8d5c117d7b55a905a0740ae09b4994e5b6b25860
konikvranik <petr@vranik.name>
2019-09-21 16:16:46 +0200

    simplified time calculation

--- homeassistant/components/rainbird/__init__.py
+++ homeassistant/components/rainbird/__init__.py
@@ -34,11 +34,7 @@ SENSOR_TYPES = {
 
 TRIGGER_TIME_SCHEMA = vol.Any(
     cv.positive_int,
-    vol.All(
-        cv.time_period,
-        cv.positive_timedelta,
-        lambda td: (td.days * 24 * 60) + td.seconds // 60,
-    ),
+    vol.All(cv.time_period, cv.positive_timedelta, lambda td: td.total_seconds // 60),
 )
 
 ZONE_SCHEMA = vol.Schema(
