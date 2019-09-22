5ffcd36ae66c09963edc4af5c3adb81c93bcc6a0
Paulus Schoutsen <balloob@gmail.com>
2019-09-22 11:21:08 -0700

    Add scaffolding split

--- script/scaffold/model.py
+++ script/scaffold/model.py
@@ -1,5 +1,10 @@
 """Models for scaffolding."""
+import json
+from pathlib import Path
+
 import attr
+
+from .const import COMPONENT_DIR, TESTS_DIR
 
 
 @attr.s
@@ -8,5 +13,31 @@ class Info:
 
     domain: str = attr.ib()
     name: str = attr.ib()
-    codeowner: str = attr.ib()
-    requirement: str = attr.ib()
+    codeowner: str = attr.ib(default=None)
+    requirement: str = attr.ib(default=None)
+
+    @property
+    def integration_dir(self) -> Path:
+        """Return directory if integration."""
+        return COMPONENT_DIR / self.domain
+
+    @property
+    def tests_dir(self) -> Path:
+        """Return test directory."""
+        return TESTS_DIR / self.domain
+
+    @property
+    def manifest_path(self) -> Path:
+        """Path to the manifest."""
+        return COMPONENT_DIR / self.domain / "manifest.json"
+
+    def manifest(self) -> dict:
+        """Return integration manifest."""
+        return json.loads(self.manifest_path.read_text())
+
+    def update_manifest(self, **kwargs) -> None:
+        """Update the integration manifest."""
+        print(f"Updating {self.domain} manifest: {kwargs}")
+        self.manifest_path.write_text(
+            json.dumps({**self.manifest(), **kwargs}, indent=2)
+        )
