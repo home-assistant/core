8833f789b77d1574540fb8ef6c4b41160b43d885
David Mulcahey <david.mulcahey@me.com>
2019-09-22 19:06:53 -0400

    add event trigger test

--- tests/components/zha/test_device_automation.py
+++ tests/components/zha/test_device_automation.py
@@ -1,11 +1,18 @@
 """ZHA device automation tests."""
+import pytest
+
+import homeassistant.components.automation as automation
 from homeassistant.components.device_automation import (
     _async_get_device_automations as async_get_device_automations,
 )
 from homeassistant.components.switch import DOMAIN
+from homeassistant.components.zha.core.const import CHANNEL_ON_OFF
 from homeassistant.helpers.device_registry import async_get_registry
+from homeassistant.setup import async_setup_component
 
-from .common import async_init_zigpy_device
+from .common import async_enable_traffic, async_init_zigpy_device
+
+from tests.common import async_mock_service
 
 ON = 1
 OFF = 0
@@ -29,6 +36,12 @@ def _same_lists(list_a, list_b):
         if item not in list_b:
             return False
     return True
+
+
+@pytest.fixture
+def calls(hass):
+    """Track calls to a mock serivce."""
+    return async_mock_service(hass, "test", "automation")
 
 
 async def test_switch(hass, config_entry, zha_gateway):
@@ -100,3 +113,65 @@ async def test_switch(hass, config_entry, zha_gateway):
         },
     ]
     assert _same_lists(triggers, expected_triggers)
+
+
+async def test_if_fires_on_state_change(hass, config_entry, zha_gateway, calls):
+    """Test for on and off triggers firing."""
+    from zigpy.zcl.clusters.general import OnOff, Basic
+
+    # create zigpy device
+    zigpy_device = await async_init_zigpy_device(
+        hass, [Basic.cluster_id], [OnOff.cluster_id], None, zha_gateway
+    )
+
+    zigpy_device.device_automation_triggers = {
+        (SHAKEN, SHAKEN): {COMMAND: COMMAND_SHAKE},
+        (DOUBLE_PRESS, DOUBLE_PRESS): {COMMAND: COMMAND_DOUBLE},
+        (SHORT_PRESS, SHORT_PRESS): {COMMAND: COMMAND_SINGLE},
+        (LONG_PRESS, LONG_PRESS): {COMMAND: COMMAND_HOLD},
+        (LONG_RELEASE, LONG_RELEASE): {COMMAND: COMMAND_HOLD},
+    }
+
+    await hass.config_entries.async_forward_entry_setup(config_entry, DOMAIN)
+    await hass.async_block_till_done()
+    hass.config_entries._entries.append(config_entry)
+
+    zha_device = zha_gateway.get_device(zigpy_device.ieee)
+
+    # allow traffic to flow through the gateway and device
+    await async_enable_traffic(hass, zha_gateway, [zha_device])
+
+    ieee_address = str(zha_device.ieee)
+    ha_device_registry = await async_get_registry(hass)
+    reg_device = ha_device_registry.async_get_device({("zha", ieee_address)}, set())
+
+    assert await async_setup_component(
+        hass,
+        automation.DOMAIN,
+        {
+            automation.DOMAIN: [
+                {
+                    "trigger": {
+                        "device_id": reg_device.id,
+                        "domain": "zha",
+                        "platform": "device",
+                        "type": SHORT_PRESS,
+                        "subtype": SHORT_PRESS,
+                    },
+                    "action": {
+                        "service": "test.automation",
+                        "data": {"message": "service called"},
+                    },
+                }
+            ]
+        },
+    )
+
+    await hass.async_block_till_done()
+
+    on_off_channel = zha_device.cluster_channels[CHANNEL_ON_OFF]
+    on_off_channel.zha_send_event(on_off_channel.cluster, COMMAND_SINGLE, [])
+    await hass.async_block_till_done()
+
+    assert len(calls) == 1
+    assert calls[0].data["message"] == "service called"
