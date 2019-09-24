45e74406e30276a238992c745d30cd9ceda975f9
Neil Renaud <neil.renaud@gmail.com>
2019-09-24 21:32:39 +0100

    Address PR comment - don't need the current_version as a state attribute.

--- homeassistant/components/updater/binary_sensor.py
+++ homeassistant/components/updater/binary_sensor.py
@@ -5,6 +5,7 @@ from homeassistant.components.binary_sensor import BinarySensorDevice
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
 from . import ATTR_NEWEST_VERSION, ATTR_RELEASE_NOTES, ATTR_CURRENT_VERSION, DISPATCHER_REMOTE_UPDATE, Updater
+from homeassistant.const import __version__ as current_version
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
@@ -20,7 +21,6 @@ class UpdaterBinary(BinarySensorDevice):
         self._update_available = None
         self._release_notes = None
         self._newest_version = None
-        self._current_version = None
         self._unsub_dispatcher = None
 
     @property
@@ -58,8 +58,7 @@ class UpdaterBinary(BinarySensorDevice):
             data[ATTR_RELEASE_NOTES] = self._release_notes
         if self._newest_version:
             data[ATTR_NEWEST_VERSION] = self._newest_version
-        if self._current_version:
-            data[ATTR_CURRENT_VERSION] = self._current_version
+        data[ATTR_CURRENT_VERSION] = current_version
         return data
 
     async def async_added_to_hass(self):
@@ -71,7 +70,6 @@ class UpdaterBinary(BinarySensorDevice):
             self._newest_version = updater.newest_version
             self._release_notes = updater.release_notes
             self._update_available = updater.update_available
-            self._current_version = updater.current_version
             self.async_schedule_update_ha_state()
 
         self._unsub_dispatcher = async_dispatcher_connect(
