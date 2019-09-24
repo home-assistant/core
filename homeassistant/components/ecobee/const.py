d5b7e75d3bf7e4958b4d54529947db37579a24c1
Mark Coombes <mark@markcoombes.ca>
2019-09-24 10:38:01 -0400

    Remove hold_temp constants

--- homeassistant/components/ecobee/const.py
+++ homeassistant/components/ecobee/const.py
@@ -6,10 +6,7 @@ _LOGGER = logging.getLogger(__package__)
 DOMAIN = "ecobee"
 DATA_ECOBEE_CONFIG = "ecobee_config"
 
-CONF_HOLD_TEMP = "hold_temp"
 CONF_INDEX = "index"
 CONF_REFRESH_TOKEN = "refresh_token"
-
-DEFAULT_HOLD_TEMP = False
 
 ECOBEE_PLATFORMS = ["binary_sensor", "climate", "sensor", "weather"]
