31dfcb18303e8ed54c0a9894d42a19fe0aae84f8
David F. Mulcahey <david.mulcahey@me.com>
2019-09-27 12:57:47 -0400

    Add device action support for ZHA (#26903)
    * start implementing device actions
    
    * rename file
    
    * cleanup and add tests
    
    * fix docstrings
    
    * sort imports

--- homeassistant/components/zha/device_trigger.py
+++ homeassistant/components/zha/device_trigger.py
@@ -9,8 +9,7 @@ from homeassistant.const import CONF_DEVICE_ID, CONF_DOMAIN, CONF_PLATFORM, CONF
 from homeassistant.components.device_automation import TRIGGER_BASE_SCHEMA
 
 from . import DOMAIN
-from .core.const import DATA_ZHA, DATA_ZHA_GATEWAY
-from .core.helpers import convert_ieee
+from .core.helpers import async_get_zha_device
 
 CONF_SUBTYPE = "subtype"
 DEVICE = "device"
@@ -26,7 +25,7 @@ async def async_attach_trigger(hass, config, action, automation_info):
     """Listen for state changes based on configuration."""
     config = TRIGGER_SCHEMA(config)
     trigger = (config[CONF_TYPE], config[CONF_SUBTYPE])
-    zha_device = await _async_get_zha_device(hass, config[CONF_DEVICE_ID])
+    zha_device = await async_get_zha_device(hass, config[CONF_DEVICE_ID])
 
     if (
         zha_device.device_automation_triggers is None
@@ -52,7 +51,7 @@ async def async_get_triggers(hass, device_id):
     Make sure the device supports device automations and
     if it does return the trigger list.
     """
-    zha_device = await _async_get_zha_device(hass, device_id)
+    zha_device = await async_get_zha_device(hass, device_id)
 
     if not zha_device.device_automation_triggers:
         return
@@ -70,15 +69,3 @@ async def async_get_triggers(hass, device_id):
         )
 
     return triggers
-
-
-async def _async_get_zha_device(hass, device_id):
-    device_registry = await hass.helpers.device_registry.async_get_registry()
-    registry_device = device_registry.async_get(device_id)
-    zha_gateway = hass.data[DATA_ZHA][DATA_ZHA_GATEWAY]
-    ieee_address = list(list(registry_device.identifiers)[0])[1]
-    ieee = convert_ieee(ieee_address)
-    zha_device = zha_gateway.devices[ieee]
-    if not zha_device:
-        raise InvalidDeviceAutomationConfig
-    return zha_device
