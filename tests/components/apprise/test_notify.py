693667a5e3e1e5dad55de4be8283ad1912d8f254
Chris Caron <lead2gold@gmail.com>
2019-09-27 19:52:28 -0400

    more test coverage

--- tests/components/apprise/test_notify.py
+++ tests/components/apprise/test_notify.py
@@ -1,9 +1,11 @@
 """The tests for the apprise notification platform."""
+import os
 import unittest
 from unittest.mock import patch
+from tempfile import mkdtemp
 
 from apprise import Apprise
-import requests_mock
+from apprise import AppriseConfig
 
 from homeassistant.setup import setup_component
 import homeassistant.components.notify as notify
@@ -13,17 +15,44 @@ from tests.common import assert_setup_component, get_test_home_assistant
 class TestApprise(unittest.TestCase):
     """Tests the Apprise Component."""
 
+    tmp_dir = None
+
     def setUp(self):
         """Initialize values for this test case class."""
         self.hass = get_test_home_assistant()
+        self.tmp_dir = mkdtemp()
 
     def tearDown(self):  # pylint: disable=invalid-name
         """Stop everything that we started."""
         self.hass.stop()
+        for fname in os.listdir(self.tmp_dir):
+            os.remove(os.path.join(self.tmp_dir, fname))
+        os.rmdir(self.tmp_dir)
 
-    @patch.object(Apprise, "notify", return_value=True)
-    def test_apprise_config(self, mock__get_data):
-        """Test setup."""
+    @patch.object(AppriseConfig, "add", return_value=False)
+    def test_apprise_config_bad_conf_add(self, __mockcfg_add):
+        """Test set up the platform with bad/missing file configuration."""
+        config = {
+            notify.DOMAIN: {"name": "test", "platform": "apprise", "config": "/path/"}
+        }
+        with assert_setup_component(1) as handle_config:
+            assert setup_component(self.hass, notify.DOMAIN, config)
+        assert handle_config[notify.DOMAIN]
+
+    @patch.object(Apprise, "add", return_value=False)
+    @patch.object(AppriseConfig, "add", return_value=True)
+    def test_apprise_config_bad_conf_save(self, __mock_add, __mockcfg_add):
+        """Test set up the platform failing to save config data."""
+        config = {
+            notify.DOMAIN: {"name": "test", "platform": "apprise", "config": "/path/"}
+        }
+        with assert_setup_component(1) as handle_config:
+            assert setup_component(self.hass, notify.DOMAIN, config)
+        assert handle_config[notify.DOMAIN]
+
+    @patch.object(Apprise, "add", return_value=False)
+    def test_apprise_bad_url(self, __mock_notify):
+        """Test set up the platform with bad/missing configuration."""
         config = {
             notify.DOMAIN: {"name": "test", "platform": "apprise", "url": "dbus://"}
         }
@@ -31,13 +60,13 @@ class TestApprise(unittest.TestCase):
             assert setup_component(self.hass, notify.DOMAIN, config)
         assert handle_config[notify.DOMAIN]
 
-    def test_apprise_config_bad(self):
-        """Test set up the platform with bad/missing configuration."""
+    def test_apprise_config_bad_url(self):
+        """Test set up the platform with bad/missing url configuration."""
         config = {
             notify.DOMAIN: {
                 "name": "test",
                 "platform": "apprise",
-                # Expects a list
+                # Expects an actual URL
                 "url": 1,
             }
         }
@@ -45,13 +74,31 @@ class TestApprise(unittest.TestCase):
             assert setup_component(self.hass, notify.DOMAIN, config)
         assert not handle_config[notify.DOMAIN]
 
-    @requests_mock.Mocker()
     @patch.object(Apprise, "notify", return_value=True)
-    def test_apprise_push_default(self, mock, mock__get_data):
-        """Test simple apprise push."""
+    def test_apprise_url(self, __mock_notify):
+        """Test simple apprise push using URL config."""
         config = {
             notify.DOMAIN: {"name": "test", "platform": "apprise", "url": "windows://"}
         }
+        with assert_setup_component(1) as handle_config:
+            assert setup_component(self.hass, notify.DOMAIN, config)
+        assert handle_config[notify.DOMAIN]
+        data = {"title": "Test Title", "message": "Test Message"}
+        assert not self.hass.services.call(notify.DOMAIN, "test", data)
+        self.hass.block_till_done()
+
+    @patch.object(Apprise, "notify", return_value=True)
+    def test_apprise_config(self, __mock_notify):
+        """Test simple apprise push using AppriseConfig."""
+        tmp_cfg = os.path.join(self.tmp_dir, "apprise.txt")
+        config = {
+            notify.DOMAIN: {"name": "test", "platform": "apprise", "config": tmp_cfg}
+        }
+
+        with open(tmp_cfg, "w+") as f:
+            # Write a simple text based configuration file
+            f.write("windows://")
+
         with assert_setup_component(1) as handle_config:
             assert setup_component(self.hass, notify.DOMAIN, config)
         assert handle_config[notify.DOMAIN]
