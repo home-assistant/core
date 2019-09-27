d47919bf9e53cf8740e501d43b7ff6813133639a
David Mulcahey <david.mulcahey@me.com>
2019-09-27 10:22:39 -0400

    cleanup and add tests

--- homeassistant/components/zha/device_action.py
+++ homeassistant/components/zha/device_action.py
@@ -5,8 +5,7 @@ import voluptuous as vol
 
 from homeassistant.const import CONF_DEVICE_ID, CONF_DOMAIN, CONF_TYPE
 from homeassistant.core import Context, HomeAssistant
-from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers import service
+from homeassistant.helpers import config_validation as cv, service
 from homeassistant.helpers.typing import ConfigType, TemplateVarsType
 
 from . import DOMAIN
@@ -16,20 +15,31 @@ from .core.helpers import async_get_zha_device
 
 ACTION_SQUAWK = "squawk"
 ACTION_WARN = "warn"
-ATTR_IEEE = "ieee"
 ATTR_DATA = "data"
+ATTR_IEEE = "ieee"
+CONF_ZHA_ACTION_TYPE = "zha_action_type"
+ZHA_ACTION_TYPE_SERVICE_CALL = "service_call"
+
+ACTION_SCHEMA = cv.DEVICE_ACTION_BASE_SCHEMA.extend(
+    {vol.Required(CONF_DOMAIN): DOMAIN, vol.Required(CONF_TYPE): str}
+)
+
+DEVICE_ACTIONS = {
+    CHANNEL_IAS_WD: [
+        {CONF_TYPE: ACTION_SQUAWK, CONF_DOMAIN: DOMAIN},
+        {CONF_TYPE: ACTION_WARN, CONF_DOMAIN: DOMAIN},
+    ]
+}
+
+DEVICE_ACTION_TYPES = {
+    ACTION_SQUAWK: ZHA_ACTION_TYPE_SERVICE_CALL,
+    ACTION_WARN: ZHA_ACTION_TYPE_SERVICE_CALL,
+}
 
 SERVICE_NAMES = {
     ACTION_SQUAWK: SERVICE_WARNING_DEVICE_SQUAWK,
     ACTION_WARN: SERVICE_WARNING_DEVICE_WARN,
 }
-
-ACTION_SCHEMA = cv.DEVICE_ACTION_BASE_SCHEMA.extend(
-    {
-        vol.Required(CONF_DOMAIN): DOMAIN,
-        vol.Required(CONF_TYPE): vol.In([ACTION_SQUAWK, ACTION_WARN]),
-    }
-)
 
 
 async def async_call_action_from_config(
@@ -40,6 +50,31 @@ async def async_call_action_from_config(
 ) -> None:
     """Perform an action based on configuration."""
     config = ACTION_SCHEMA(config)
+    await ZHA_ACTION_TYPES[DEVICE_ACTION_TYPES[config[CONF_TYPE]]](
+        hass, config, variables, context
+    )
+
+
+async def async_get_actions(hass: HomeAssistant, device_id: str) -> List[dict]:
+    """List device actions."""
+    zha_device = await async_get_zha_device(hass, device_id)
+    actions = [
+        action
+        for channel in DEVICE_ACTIONS
+        for action in DEVICE_ACTIONS[channel]
+        if channel in zha_device.cluster_channels
+    ]
+    for action in actions:
+        action[CONF_DEVICE_ID] = device_id
+    return actions
+
+
+async def _execute_service_based_action(
+    hass: HomeAssistant,
+    config: ACTION_SCHEMA,
+    variables: TemplateVarsType,
+    context: Context,
+) -> None:
     action_type = config[CONF_TYPE]
     service_name = SERVICE_NAMES[action_type]
     zha_device = await async_get_zha_device(hass, config[CONF_DEVICE_ID])
@@ -54,12 +89,4 @@ async def async_call_action_from_config(
     )
 
 
-async def async_get_actions(hass: HomeAssistant, device_id: str) -> List[dict]:
-    """List device actions."""
-    zha_device = await async_get_zha_device(hass, device_id)
-    if CHANNEL_IAS_WD in zha_device.cluster_channels:
-        return [
-            {CONF_TYPE: ACTION_SQUAWK, CONF_DEVICE_ID: device_id, CONF_DOMAIN: DOMAIN},
-            {CONF_TYPE: ACTION_WARN, CONF_DEVICE_ID: device_id, CONF_DOMAIN: DOMAIN},
-        ]
-    return []
+ZHA_ACTION_TYPES = {ZHA_ACTION_TYPE_SERVICE_CALL: _execute_service_based_action}
