96ec9b802c461bac15b0843106da77560bdafc91
Sebastian Kügler <sebas@vizZzion.org>
2019-09-27 18:40:01 +0200

    only render url once

--- homeassistant/components/rest_command/__init__.py
+++ homeassistant/components/rest_command/__init__.py
@@ -94,10 +94,11 @@ async def async_setup(hass, config):
                     template_payload.async_render(variables=service.data), "utf-8"
                 )
 
+            request_url = template_url.async_render(variables=service.data)
             try:
                 with async_timeout.timeout(timeout):
                     request = await getattr(websession, method)(
-                        template_url.async_render(variables=service.data),
+                        request_url,
                         data=payload,
                         auth=auth,
                         headers=headers,
@@ -112,10 +113,7 @@ async def async_setup(hass, config):
                 _LOGGER.warning("Timeout call %s.", request.url)
 
             except aiohttp.ClientError:
-                _LOGGER.error(
-                    "Client error %s.",
-                    template_url.async_render(variables=service.data),
-                )
+                _LOGGER.error("Client error %s.", request_url)
 
         # register services
         hass.services.async_register(DOMAIN, name, async_service_handler)
