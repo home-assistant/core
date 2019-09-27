95d32de6f77ec549f1d3663e02eaea9fdffa60ff
Mark Coombes <mark@markcoombes.ca>
2019-09-27 16:36:01 -0400

    Revise tests as standalone functions

--- tests/components/ecobee/test_util.py
+++ tests/components/ecobee/test_util.py
@@ -1,34 +1,35 @@
 """Tests for the ecobee.util module."""
-import unittest
+import pytest
 import voluptuous as vol
+
 from homeassistant.components.ecobee.util import ecobee_date, ecobee_time
 
 
-class TestUtil(unittest.TestCase):
-    """Tests for the ecobee.util functions."""
+def test_ecobee_date_with_valid_input():
+    """Test that the date function returns the expected result."""
+    test_input = "2019-09-27"
 
-    def test_ecobee_date_with_valid_input(self):
-        """Test that the date function returns the expected result."""
-        test_input = "2019-09-27"
+    assert ecobee_date(test_input) == test_input
 
-        assert ecobee_date(test_input) == test_input
 
-    def test_ecobee_date_with_invalid_input(self):
-        """Test that the date function raises the expected exception."""
-        test_input = "20190927"
+def test_ecobee_date_with_invalid_input():
+    """Test that the date function raises the expected exception."""
+    test_input = "20190927"
 
-        with self.assertRaises(vol.Invalid):
-            ecobee_date(test_input)
+    with pytest.raises(vol.Invalid):
+        ecobee_date(test_input)
 
-    def test_ecobee_time_with_valid_input(self):
-        """Test that the time function returns the expected result."""
-        test_input = "20:55:15"
 
-        assert ecobee_time(test_input) == test_input
+def test_ecobee_time_with_valid_input():
+    """Test that the time function returns the expected result."""
+    test_input = "20:55:15"
 
-    def test_ecobee_time_with_invalid_input(self):
-        """Test that the time function raises the expected exception."""
-        test_input = "20:55"
+    assert ecobee_time(test_input) == test_input
 
-        with self.assertRaises(vol.Invalid):
-            ecobee_time(test_input)
+
+def test_ecobee_time_with_invalid_input():
+    """Test that the time function raises the expected exception."""
+    test_input = "20:55"
+
+    with pytest.raises(vol.Invalid):
+        ecobee_time(test_input)
