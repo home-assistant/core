fb3db325fdbb13b3f2e2396c8f6e0401efd5bba6
Timmo <contact@timmo.xyz>
2019-09-28 15:42:22 +0000

    :hammer: Dont update before adding entities

--- homeassistant/components/lyric/sensor.py
+++ homeassistant/components/lyric/sensor.py
@@ -123,7 +123,7 @@ async def async_setup_entry(
                 )
             )
 
-    async_add_entities(devices, True)
+    async_add_entities(devices)
 
 
 class LyricSensor(LyricDeviceEntity):
