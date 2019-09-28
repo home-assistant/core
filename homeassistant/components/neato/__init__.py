fa61829897814ce2d403682e077b1df493aaebb4
Tobias Haase <tobhaase@gmail.com>
2019-09-28 22:02:30 +0200

    Move IO out of constructur

--- homeassistant/components/neato/__init__.py
+++ homeassistant/components/neato/__init__.py
@@ -81,21 +81,20 @@ async def async_setup_entry(hass, entry):
     from pybotvac import Account, Neato, Vorwerk
 
     if entry.data[CONF_VENDOR] == "neato":
-        hass.data[NEATO_LOGIN] = await hass.async_add_executor_job(
-            NeatoHub, hass, entry.data, Account, Neato
-        )
+        hass.data[NEATO_LOGIN] = NeatoHub(hass, entry.data, Account, Neato)
     elif entry.data[CONF_VENDOR] == "vorwerk":
-        hass.data[NEATO_LOGIN] = await hass.async_add_executor_job(
-            NeatoHub, hass, entry.data, Account, Vorwerk
-        )
+        hass.data[NEATO_LOGIN] = NeatoHub(hass, entry.data, Account, Vorwerk)
+    else:
+        _LOGGER.debug("Invalid Vendor")
+        return False
 
     hub = hass.data[NEATO_LOGIN]
+    await hass.async_add_executor_job(hub.login)
     if not hub.logged_in:
         _LOGGER.debug("Failed to login to Neato API")
         return False
 
     await hass.async_add_executor_job(hub.update_robots)
-
     for component in ("camera", "vacuum", "switch"):
         hass.async_add_job(
             hass.config_entries.async_forward_entry_setup(entry, component)
@@ -131,12 +130,7 @@ class NeatoHub:
         self._vendor = vendor
 
         self.my_neato = None
-        self.logged_in = self.login()
-
-        if self.logged_in:
-            self._hass.data[NEATO_ROBOTS] = self.my_neato.robots
-            self._hass.data[NEATO_PERSISTENT_MAPS] = self.my_neato.persistent_maps
-            self._hass.data[NEATO_MAP_DATA] = self.my_neato.maps
+        self.logged_in = False
 
     def login(self):
         """Login to My Neato."""
@@ -145,10 +139,15 @@ class NeatoHub:
             self.my_neato = self._neato(
                 self.config[CONF_USERNAME], self.config[CONF_PASSWORD], self._vendor
             )
-            return True
+            self.logged_in = True
         except (HTTPError, ConnError):
             _LOGGER.error("Unable to connect to Neato API")
-            return False
+            self.logged_in = False
+
+        _LOGGER.debug("Successfully connected to Neato API")
+        self._hass.data[NEATO_ROBOTS] = self.my_neato.robots
+        self._hass.data[NEATO_PERSISTENT_MAPS] = self.my_neato.persistent_maps
+        self._hass.data[NEATO_MAP_DATA] = self.my_neato.maps
 
     @Throttle(timedelta(seconds=300))
     def update_robots(self):
