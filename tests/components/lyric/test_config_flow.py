e84fe74dc7a14e5f2e18d074b70c32b45de2dcd8
Timmo <contact@timmo.xyz>
2019-09-28 16:33:48 +0000

    :shirt: Formatting

--- tests/components/lyric/test_config_flow.py
+++ tests/components/lyric/test_config_flow.py
@@ -5,38 +5,34 @@ import pytest
 
 from homeassistant import data_entry_flow
 from homeassistant.components.lyric import config_flow
-from homeassistant.components.lyric.const import (DATA_LYRIC_CONFIG, DOMAIN,
-                                                  CONF_CLIENT_ID,
-                                                  CONF_CLIENT_SECRET)
+from homeassistant.components.lyric.const import (
+    DATA_LYRIC_CONFIG,
+    DOMAIN,
+    CONF_CLIENT_ID,
+    CONF_CLIENT_SECRET,
+)
 from homeassistant.setup import async_setup_component
 
 from tests.common import MockDependency
 
 FIXTURE_APP = {
-    DOMAIN: {
-        CONF_CLIENT_ID: '1234567890abcdef',
-        CONF_CLIENT_SECRET: '1234567890abcdef',
-    }
+    DOMAIN: {CONF_CLIENT_ID: "1234567890abcdef", CONF_CLIENT_SECRET: "1234567890abcdef"}
 }
 
-FIXTURE_API = {
-    'api': {
-        'base_url': 'http://localhost:8123',
-    }
-}
+FIXTURE_API = {"api": {"base_url": "http://localhost:8123"}}
 
 
 @pytest.fixture
 def mock_lyriclib():
     """Mock lyric."""
-    with MockDependency('lyric') as mock_lyriclib_:
+    with MockDependency("lyric") as mock_lyriclib_:
         yield mock_lyriclib_
 
 
 async def setup_component(hass):
     """Set up Honeywell Lyric component."""
-    with patch('os.path.isfile', return_value=False):
-        assert await async_setup_component(hass, 'api', FIXTURE_API)
+    with patch("os.path.isfile", return_value=False):
+        assert await async_setup_component(hass, "api", FIXTURE_API)
         assert await async_setup_component(hass, DOMAIN, FIXTURE_APP)
         await hass.async_block_till_done()
 
@@ -51,8 +47,8 @@ async def test_lyric_abort(hass, mock_lyriclib):
     flow.hass.data[DATA_LYRIC_CONFIG] = None
 
     result = await flow.async_step_user()
-    assert result['type'] == data_entry_flow.RESULT_TYPE_ABORT
-    assert result['reason'] == 'no_config'
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "no_config"
 
 
 async def test_lyric_setup(hass, mock_lyriclib):
@@ -62,7 +58,7 @@ async def test_lyric_setup(hass, mock_lyriclib):
     flow = config_flow.LyricFlowHandler()
     flow.hass = hass
 
-    flow.flow_id = '1234567890abcdef'
+    flow.flow_id = "1234567890abcdef"
 
     result = await flow.async_step_user()
-    assert result['type'] == data_entry_flow.RESULT_TYPE_EXTERNAL_STEP
+    assert result["type"] == data_entry_flow.RESULT_TYPE_EXTERNAL_STEP
