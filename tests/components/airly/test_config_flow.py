e475e02959e394b728ec76b4a03f37059d078759
bieniu <maciej.bieniek@gmail.com>
2019-10-02 09:03:14 +0200

    Add tests

--- tests/components/airly/test_config_flow.py
+++ tests/components/airly/test_config_flow.py
@@ -1,27 +1,22 @@
 """Define tests for the Airly config flow."""
-import pytest
+import json
+
+from asynctest import patch
 
 from homeassistant import data_entry_flow
 from homeassistant.const import CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
 from homeassistant.components.airly import config_flow
 from homeassistant.components.airly.const import CONF_LANGUAGE, DOMAIN
 
-from tests.common import MockConfigEntry, MockDependency
+from tests.common import MockConfigEntry, load_fixture
 
 CONFIG = {
     CONF_NAME: "abcd",
     CONF_API_KEY: "foo",
-    CONF_LATITUDE: 0,
-    CONF_LONGITUDE: 0,
+    CONF_LATITUDE: 123,
+    CONF_LONGITUDE: 456,
     CONF_LANGUAGE: "en",
 }
-
-
-@pytest.fixture
-def mock_airly():
-    """Mock the airly library."""
-    with MockDependency("airly") as mock_airly_:
-        yield mock_airly_
 
 
 async def test_show_form(hass):
@@ -46,7 +41,7 @@ async def test_show_form_with_input(hass):
     assert result["step_id"] == "user"
 
 
-async def test_invalid_api_key(hass, mock_airly):
+async def test_invalid_api_key(hass):
     """Test that an invalid API_KEY throws an error."""
     flow = config_flow.AirlyFlowHandler()
     flow.hass = hass
@@ -55,11 +50,48 @@ async def test_invalid_api_key(hass, mock_airly):
     assert result["errors"] == {"base": "auth"}
 
 
-async def test_duplicate_error(hass, mock_airly):
-    """Test that errors are shown when duplicates are added."""
-    MockConfigEntry(domain=DOMAIN, data=CONFIG).add_to_hass(hass)
-    flow = config_flow.AirlyFlowHandler()
-    flow.hass = hass
+async def test_invalid_location(hass):
+    """Test that an invalid location throws an error."""
+    with patch(
+        "airly._private._RequestsHandler" ".get",
+        return_value=json.loads(load_fixture("airly_no_station.json")),
+    ):
+        flow = config_flow.AirlyFlowHandler()
+        flow.hass = hass
 
-    result = await flow.async_step_user(user_input=CONFIG)
-    assert result["errors"] == {CONF_NAME: "name_exists", "base": "auth"}
+        result = await flow.async_step_user(user_input=CONFIG)
+        assert result["errors"] == {"base": "wrong_location"}
+
+
+async def test_duplicate_error(hass):
+    """Test that errors are shown when duplicates are added."""
+
+    with patch(
+        "airly._private._RequestsHandler" ".get",
+        return_value=json.loads(load_fixture("airly_valid_station.json")),
+    ):
+        MockConfigEntry(domain=DOMAIN, data=CONFIG).add_to_hass(hass)
+        flow = config_flow.AirlyFlowHandler()
+        flow.hass = hass
+
+        result = await flow.async_step_user(user_input=CONFIG)
+        assert result["errors"] == {CONF_NAME: "name_exists"}
+
+
+async def test_create_entry(hass):
+    """Test that the user step works."""
+
+    with patch(
+        "airly._private._RequestsHandler" ".get",
+        return_value=json.loads(load_fixture("airly_valid_station.json")),
+    ):
+        flow = config_flow.AirlyFlowHandler()
+        flow.hass = hass
+
+        result = await flow.async_step_user(user_input=CONFIG)
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result["title"] == CONFIG[CONF_NAME]
+        assert result["data"][CONF_LATITUDE] == CONFIG[CONF_LATITUDE]
+        assert result["data"][CONF_LONGITUDE] == CONFIG[CONF_LONGITUDE]
+        assert result["data"][CONF_API_KEY] == CONFIG[CONF_API_KEY]
+        assert result["data"][CONF_LANGUAGE] == CONFIG[CONF_LANGUAGE]
