de11b6656b4e496b68e2b5835513ad21b5d8cdfd
Raman Gupta <7243222+raman325@users.noreply.github.com>
2019-10-03 02:46:46 -0400

    add ability for MQTT device tracker to map nondefault topic payloads to zones

--- homeassistant/components/mqtt/device_tracker.py
+++ homeassistant/components/mqtt/device_tracker.py
@@ -13,8 +13,13 @@ from . import CONF_QOS
 
 _LOGGER = logging.getLogger(__name__)
 
+CONF_CUSTOM_PAYLOAD = "custom_payload"
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(mqtt.SCHEMA_BASE).extend(
-    {vol.Required(CONF_DEVICES): {cv.string: mqtt.valid_subscribe_topic}}
+    {
+        vol.Required(CONF_DEVICES): {cv.string: mqtt.valid_subscribe_topic},
+        vol.Optional(CONF_CUSTOM_PAYLOAD): {cv.string: cv.string},
+    }
 )
 
 
@@ -22,13 +27,21 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
     """Set up the MQTT tracker."""
     devices = config[CONF_DEVICES]
     qos = config[CONF_QOS]
+    custom_payload = config.get(CONF_CUSTOM_PAYLOAD)
 
     for dev_id, topic in devices.items():
 
         @callback
         def async_message_received(msg, dev_id=dev_id):
             """Handle received MQTT message."""
-            hass.async_create_task(async_see(dev_id=dev_id, location_name=msg.payload))
+            if custom_payload and msg.payload in custom_payload:
+                hass.async_create_task(
+                    async_see(dev_id=dev_id, location_name=custom_payload[msg.payload])
+                )
+            else:
+                hass.async_create_task(
+                    async_see(dev_id=dev_id, location_name=msg.payload)
+                )
 
         await mqtt.async_subscribe(hass, topic, async_message_received, qos)
 
