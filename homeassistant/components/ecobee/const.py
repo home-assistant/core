18baa12a1815c15dfd7f298c2d0481788853cd34
Mark Coombes <mark@markcoombes.ca>
2019-10-04 02:31:45 -0400

    Add device registry support to ecobee integration (#27109)
    * Add manufacturer const
    
    * Add device_info to binary sensor
    
    * Add device info to climate
    
    * Add device info to sensor
    
    * Add device info to weather
    
    * Add constant for device info
    
    * Fix log messages
    
    * Use guard clauses

--- homeassistant/components/ecobee/const.py
+++ homeassistant/components/ecobee/const.py
@@ -9,4 +9,18 @@ DATA_ECOBEE_CONFIG = "ecobee_config"
 CONF_INDEX = "index"
 CONF_REFRESH_TOKEN = "refresh_token"
 
+ECOBEE_MODEL_TO_NAME = {
+    "idtSmart": "ecobee Smart",
+    "idtEms": "ecobee Smart EMS",
+    "siSmart": "ecobee Si Smart",
+    "siEms": "ecobee Si EMS",
+    "athenaSmart": "ecobee3 Smart",
+    "athenaEms": "ecobee3 EMS",
+    "corSmart": "Carrier/Bryant Cor",
+    "nikeSmart": "ecobee3 lite Smart",
+    "nikeEms": "ecobee3 lite EMS",
+}
+
 ECOBEE_PLATFORMS = ["binary_sensor", "climate", "sensor", "weather"]
+
+MANUFACTURER = "ecobee"
