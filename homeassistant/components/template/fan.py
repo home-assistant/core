758150adc11288b23acd928beef5aa86b7b18b84
Paulus Schoutsen <paulus@home-assistant.io>
2019-10-04 08:55:28 -0700

    Format

--- homeassistant/components/template/fan.py
+++ homeassistant/components/template/fan.py
@@ -269,8 +269,7 @@ class TemplateFan(FanEntity):
 
     async def async_turn_on(self, speed: str = None) -> None:
         """Turn on the fan."""
-        await self._on_script.async_run(
-            {ATTR_SPEED: speed}, context=self._context)
+        await self._on_script.async_run({ATTR_SPEED: speed}, context=self._context)
         self._state = STATE_ON
 
         if speed is not None:
