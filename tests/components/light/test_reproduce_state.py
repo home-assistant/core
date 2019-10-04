8cbb908d8ab5e991f5aac0260c71c3b513358dba
Tobias Haase <tobhaase@gmail.com>
2019-10-04 21:05:24 +0200

    Another try

--- tests/components/light/test_reproduce_state.py
+++ tests/components/light/test_reproduce_state.py
@@ -90,56 +90,67 @@ async def test_reproducing_states(hass, caplog):
 
     assert len(turn_on_calls) == 12
 
-    for i in range(0, 12):
-        assert turn_on_calls[i].domain == "light"
+    expected_calls = []
 
     expected_off = VALID_BRIGHTNESS
     expected_off["entity_id"] = "light.entity_off"
-    assert turn_on_calls[0].data == expected_off
+    expected_calls.append(expected_off)
 
     expected_bright = VALID_WHITE_VALUE
     expected_bright["entity_id"] = "light.entity_bright"
-    assert turn_on_calls[1].data == expected_bright
+    expected_calls.append(expected_bright)
 
     expected_white = VALID_FLASH
     expected_white["entity_id"] = "light.entity_white"
-    assert turn_on_calls[2].data == expected_white
+    expected_calls.append(expected_white)
 
     expected_flash = VALID_EFFECT
     expected_flash["entity_id"] = "light.entity_flash"
-    assert turn_on_calls[3].data == expected_flash
+    expected_calls.append(expected_flash)
 
     expected_effect = VALID_TRANSITION
     expected_effect["entity_id"] = "light.entity_effect"
-    assert turn_on_calls[4].data == expected_effect
+    expected_calls.append(expected_effect)
 
     expected_trans = VALID_COLOR_NAME
     expected_trans["entity_id"] = "light.entity_trans"
-    assert turn_on_calls[5].data == expected_trans
+    expected_calls.append(expected_trans)
 
     expected_name = VALID_COLOR_TEMP
     expected_name["entity_id"] = "light.entity_name"
-    assert turn_on_calls[6].data == expected_name
+    expected_calls.append(expected_name)
 
     expected_temp = VALID_HS_COLOR
     expected_temp["entity_id"] = "light.entity_temp"
-    assert turn_on_calls[7].data == expected_temp
+    expected_calls.append(expected_temp)
 
     expected_hs = VALID_KELVIN
     expected_hs["entity_id"] = "light.entity_hs"
-    assert turn_on_calls[8].data == expected_hs
+    expected_calls.append(expected_hs)
 
     expected_kelvin = VALID_PROFILE
     expected_kelvin["entity_id"] = "light.entity_kelvin"
-    assert turn_on_calls[9].data == expected_kelvin
+    expected_calls.append(expected_kelvin)
 
     expected_profile = VALID_RGB_COLOR
     expected_profile["entity_id"] = "light.entity_profile"
-    assert turn_on_calls[10].data == expected_profile
+    expected_calls.append(expected_profile)
 
     expected_rgb = VALID_XY_COLOR
     expected_rgb["entity_id"] = "light.entity_rgb"
-    assert turn_on_calls[11].data == expected_rgb
+    expected_calls.append(expected_rgb)
+
+    for call in turn_on_calls:
+        assert call.domain == "light"
+        found = False
+        for expected in expected_calls:
+            if call.data["entity_id"] == expected["entity_id"]:
+                # We found the matching entry
+                assert call.data == expected
+                found = True
+                break
+        # No entry found
+        assert found
 
     assert len(turn_off_calls) == 1
     assert turn_off_calls[0].domain == "light"
