606d6ccf31e145c0e7f7ad2b22825aa91ee3704e
Raman Gupta <7243222+raman325@users.noreply.github.com>
2019-10-03 22:12:03 -0400

    support for payload_home, payload_not_home, and payload_custom

--- homeassistant/components/mqtt/device_tracker.py
+++ homeassistant/components/mqtt/device_tracker.py
@@ -14,11 +14,20 @@ from . import CONF_QOS
 _LOGGER = logging.getLogger(__name__)
 
 CONF_PAYLOAD_CUSTOM = "payload_custom"
+CONF_PAYLOAD_HOME = "payload_home"
+CONF_PAYLOAD_NOT_HOME = "payload_not_home"
+
+DEFAULT_PAYLOAD_HOME = "home"
+DEFAULT_PAYLOAD_NOT_HOME = "not_home"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(mqtt.SCHEMA_BASE).extend(
     {
         vol.Required(CONF_DEVICES): {cv.string: mqtt.valid_subscribe_topic},
         vol.Optional(CONF_PAYLOAD_CUSTOM): {cv.string: cv.string},
+        vol.Optional(CONF_PAYLOAD_HOME, default=DEFAULT_PAYLOAD_HOME): cv.string,
+        vol.Optional(
+            CONF_PAYLOAD_NOT_HOME, default=DEFAULT_PAYLOAD_NOT_HOME
+        ): cv.string,
     }
 )
 
@@ -28,20 +37,26 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
     devices = config[CONF_DEVICES]
     qos = config[CONF_QOS]
     payload_custom = config.get(CONF_PAYLOAD_CUSTOM)
+    payload_home = config[CONF_PAYLOAD_HOME]
+    payload_not_home = config[CONF_PAYLOAD_NOT_HOME]
 
     for dev_id, topic in devices.items():
 
         @callback
         def async_message_received(msg, dev_id=dev_id):
             """Handle received MQTT message."""
-            if payload_custom and msg.payload in payload_custom:
-                hass.async_create_task(
-                    async_see(dev_id=dev_id, location_name=payload_custom[msg.payload])
-                )
+            if msg.payload == payload_home:
+                location_name = DEFAULT_PAYLOAD_HOME
+            elif msg.payload == payload_not_home:
+                location_name = DEFAULT_PAYLOAD_NOT_HOME
+            elif payload_custom and msg.payload in payload_custom:
+                location_name = payload_custom[msg.payload]
             else:
-                hass.async_create_task(
-                    async_see(dev_id=dev_id, location_name=msg.payload)
-                )
+                location_name = msg.payload
+
+            hass.async_create_task(
+                async_see(dev_id=dev_id, location_name=location_name)
+            )
 
         await mqtt.async_subscribe(hass, topic, async_message_received, qos)
 
