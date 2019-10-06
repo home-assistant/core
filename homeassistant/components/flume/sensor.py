a30f1ad857e5cef27c28fbf119aeed4a7528060a
ChrisMandich <mandichc@gmail.com>
2019-10-06 12:58:43 -0700

    Update sensor.py
    Update sensor to not required Device ID for configuration. Now loops through all available Type 2 devices and adds them as an entity.

--- homeassistant/components/flume/sensor.py
+++ homeassistant/components/flume/sensor.py
@@ -19,7 +19,6 @@ DEFAULT_NAME = "Flume Sensor"
 
 CONF_CLIENT_ID = "client_id"
 CONF_CLIENT_SECRET = "client_secret"
-CONF_DEVICE_ID = "device_id"
 CONF_TIME_ZONE = "time_zone"
 
 SCAN_INTERVAL = timedelta(minutes=1)
@@ -31,7 +30,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Required(CONF_PASSWORD): cv.string,
         vol.Required(CONF_CLIENT_ID): cv.string,
         vol.Required(CONF_CLIENT_SECRET): cv.string,
-        vol.Required(CONF_DEVICE_ID): cv.string,
         vol.Required(CONF_TIME_ZONE): cv.string,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     }
@@ -44,26 +42,28 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     password = config.get(CONF_PASSWORD)
     client_id = config.get(CONF_CLIENT_ID)
     client_secret = config.get(CONF_CLIENT_SECRET)
-    device_id = config.get(CONF_DEVICE_ID)
     time_zone = config.get(CONF_TIME_ZONE)
     name = config.get(CONF_NAME)
 
-    Flume = FlumeData(
-        username,
-        password,
-        client_id,
-        client_secret,
-        device_id,
-        time_zone,
-        SCAN_INTERVAL,
-    )
-    try:
-        Flume.update()
-    except Exception as error:
-        _LOGGER.error("Unable to update: %s", error)
-        return False
+    FlumeDeviceList = FlumeDevice(username, password, client_id, client_secret)
 
-    add_entities([FlumeSensor(Flume, name)], True)
+    try:
+        for device in FlumeDeviceList._devices:
+            if device["type"] == 2:
+                Flume = FlumeData(
+                    username,
+                    password,
+                    client_id,
+                    client_secret,
+                    device["id"],
+                    time_zone,
+                    SCAN_INTERVAL,
+                )
+                sensor_name = name + " " + device["id"]
+                add_entities([FlumeSensor(Flume, sensor_name)], True)
+    except Exception as error:
+        _LOGGER.error("Unable to setup Flume Devices: %s", error)
+        return False
 
 
 class FlumeSensor(Entity):
@@ -95,6 +95,80 @@ class FlumeSensor(Entity):
         """Get the latest data and updates the states."""
         self.Flume.update()
         self._state = self.Flume.value
+
+
+class FlumeDevice:
+    """Get the latest data and update the states."""
+
+    def __init__(self, username, password, client_id, client_secret):
+        """Initialize the data object."""
+        self._username = username
+        self._password = password
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._token = self.getToken()
+        self._user_id = self.getUserId()
+        self._bearer = self.getBearer()
+        self._devices = self.getDevices()
+
+    def getToken(self):
+        """Return authorization token for session."""
+        url = "https://api.flumetech.com/oauth/token"
+        payload = (
+            '{"grant_type":"password","client_id":"'
+            + self._client_id
+            + '","client_secret":"'
+            + self._client_secret
+            + '","username":"'
+            + self._username
+            + '","password":"'
+            + self._password
+            + '"}'
+        )
+        headers = {"content-type": "application/json"}
+
+        response = requests.request("POST", url, data=payload, headers=headers)
+
+        _LOGGER.debug("Token Payload: %s", payload)
+        _LOGGER.debug("Token Response: %s", response.text)
+
+        if response.status_code == 200:
+            return json.loads(response.text)["data"]
+        else:
+            raise Exception(
+                "getToken Response Code not Successful. Returned {}".format(
+                    response.status_code
+                )
+            )
+
+    def getUserId(self):
+        """Return User ID for authorized user."""
+        json_token_data = self._token[0]
+        return json.loads(
+            base64.b64decode(json_token_data["access_token"].split(".")[1])
+        )["user_id"]
+
+    def getBearer(self):
+        """Return Bearer for Authorized session."""
+        return self._token[0]["access_token"]
+
+    def getDevices(self):
+        """Return all available devices from Flume API."""
+        url = "https://api.flumetech.com/users/" + str(self._user_id) + "/devices"
+        querystring = {"user": "false", "location": "false"}
+        headers = {"authorization": "Bearer " + self._bearer + ""}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        _LOGGER.debug("getDevices Response: %s", response.text)
+
+        if response.status_code == 200:
+            return json.loads(response.text)["data"]
+        else:
+            raise Exception(
+                "getDevices Response Code not Successful. Returned {}".format(
+                    response.status_code
+                )
+            )
 
 
 class FlumeData:
