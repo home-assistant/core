6f8b108faee032597cfd56335e907a3f3c65ee7a
Bruno Furtado <bfurtado@google.com>
2019-10-07 02:36:07 +0100

    Only auth on enter_learning in response to errors.

--- homeassistant/components/broadlink/__init__.py
+++ homeassistant/components/broadlink/__init__.py
@@ -47,16 +47,16 @@ def async_setup_service(hass, host, device):
             """Learn a packet from remote."""
             device = hass.data[DOMAIN][call.data[CONF_HOST]]
 
-            try:
-                auth = await hass.async_add_executor_job(device.auth)
-            except socket.timeout:
-                _LOGGER.error("Failed to connect to device, timeout")
-                return
-            if not auth:
-                _LOGGER.error("Failed to connect to device")
-                return
+            for retry in range(DEFAULT_RETRY):
+                try:
+                    await hass.async_add_executor_job(device.enter_learning)
+                except (socket.timeout, ValueError):
+                    try:
+                        await hass.async_add_executor_job(device.auth)
+                    except socket.timeout:
+                        if retry == DEFAULT_RETRY - 1:
+                            _LOGGEr.error("Failed to enter learning mode")
 
-            await hass.async_add_executor_job(device.enter_learning)
 
             _LOGGER.info("Press the key you want Home Assistant to learn")
             start_time = utcnow()
