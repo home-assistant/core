d1f90ad618e19463985a6b2134dc9913ec21a39b
Paulus Schoutsen <balloob@gmail.com>
2019-10-09 12:43:44 -0700

    Fix upnp tests

--- homeassistant/components/upnp/device.py
+++ homeassistant/components/upnp/device.py
@@ -3,6 +3,7 @@ import asyncio
 from ipaddress import IPv4Address
 
 import aiohttp
+from async_upnp_client.profiles.igd import IgdDevice
 
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.typing import HomeAssistantType
@@ -28,9 +29,6 @@ class Device:
             local_ip = hass.data[DOMAIN]["config"].get(CONF_LOCAL_IP)
         if local_ip:
             local_ip = IPv4Address(local_ip)
-
-        # discover devices
-        from async_upnp_client.profiles.igd import IgdDevice
 
         discovery_infos = await IgdDevice.async_search(source_ip=local_ip, timeout=10)
 
