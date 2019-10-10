ffa285eedfc2a5ce04edc10567ab265e35806444
Raman Gupta <7243222+raman325@users.noreply.github.com>
2019-10-10 00:16:45 -0400

    add empty dict as default for payload_custom

--- homeassistant/components/mqtt/device_tracker.py
+++ homeassistant/components/mqtt/device_tracker.py
@@ -20,7 +20,7 @@ CONF_PAYLOAD_NOT_HOME = "payload_not_home"
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(mqtt.SCHEMA_BASE).extend(
     {
         vol.Required(CONF_DEVICES): {cv.string: mqtt.valid_subscribe_topic},
-        vol.Optional(CONF_PAYLOAD_CUSTOM): {cv.string: cv.string},
+        vol.Optional(CONF_PAYLOAD_CUSTOM, default=dict()): {cv.string: cv.string},
         vol.Optional(CONF_PAYLOAD_HOME, default=STATE_HOME): cv.string,
         vol.Optional(CONF_PAYLOAD_NOT_HOME, default=STATE_NOT_HOME): cv.string,
     }
@@ -31,7 +31,7 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
     """Set up the MQTT tracker."""
     devices = config[CONF_DEVICES]
     qos = config[CONF_QOS]
-    payload_custom = config.get(CONF_PAYLOAD_CUSTOM)
+    payload_custom = config[CONF_PAYLOAD_CUSTOM]
     payload_home = config[CONF_PAYLOAD_HOME]
     payload_not_home = config[CONF_PAYLOAD_NOT_HOME]
 
@@ -44,7 +44,7 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
                 location_name = STATE_HOME
             elif msg.payload == payload_not_home:
                 location_name = STATE_NOT_HOME
-            elif payload_custom and msg.payload in payload_custom:
+            elif msg.payload in payload_custom:
                 location_name = payload_custom[msg.payload]
             else:
                 location_name = msg.payload
