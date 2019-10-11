bc1abf4d562c60606a74b71ef15312d433c7c345
Bogdan Vlaicu <bogdanvlaicu@yahoo.com>
2019-10-10 20:01:38 -0400

    lint fixes, set icon, other small refactorings

--- homeassistant/components/oru/sensor.py
+++ homeassistant/components/oru/sensor.py
@@ -1,36 +1,47 @@
 """Platform for sensor integration."""
 import logging
+
 from oru import Meter
 from homeassistant.const import ENERGY_WATT_HOUR
 from homeassistant.helpers.entity import Entity
 
-
 _LOGGER = logging.getLogger(__name__)
 
 CONF_METER = "meter"
+
+COMPONENT_NAME = "Real Time Energy Usage"
+COMPONENT_ICON = "mdi:counter"
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
     global meter_id
 
-    add_entities([RealTimeEnergyUsageSensor()])
-
     meter_id = str(config.get(CONF_METER, None))
+    meter = Meter(meter_id)
+
+    add_entities([RealTimeEnergyUsageSensor(meter)])
+
     _LOGGER.debug("meter_id = %s", meter_id)
 
 
 class RealTimeEnergyUsageSensor(Entity):
     """Representation of the sensor."""
 
-    def __init__(self):
+    def __init__(self, meter):
         """Initialize the sensor."""
         self._state = None
+        self.meter = meter
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return "Real Time Energy Usage"
+        return COMPONENT_NAME
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return COMPONENT_ICON
 
     @property
     def state(self):
@@ -44,10 +55,8 @@ class RealTimeEnergyUsageSensor(Entity):
 
     def update(self):
         """Fetch new state data for the sensor."""
+        last_read = self.meter.last_read()
 
-        meter = Meter(meter_id)
-        last_read = meter.last_read()
-
-        _LOGGER.info("last_read = %s %s", last_read, self.unit_of_measurement)
+        _LOGGER.info("Oru meter last_read = %s %s", last_read, self.unit_of_measurement)
 
         self._state = last_read
