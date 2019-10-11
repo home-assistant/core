79fe29a20fee986ee1fdcdb448adfe1ea544ae6d
Erik <erik@montnemery.com>
2019-10-11 19:10:50 +0200

    Restore ZHA changes but add check to make sure ZHA is loaded

--- homeassistant/components/zha/device_trigger.py
+++ homeassistant/components/zha/device_trigger.py
@@ -21,6 +21,23 @@ TRIGGER_SCHEMA = TRIGGER_BASE_SCHEMA.extend(
 )
 
 
+async def async_validate_trigger_config(hass, config):
+    """Validate config."""
+    config = TRIGGER_SCHEMA(config)
+
+    trigger = (config[CONF_TYPE], config[CONF_SUBTYPE])
+    zha_device = await async_get_zha_device(hass, config[CONF_DEVICE_ID])
+
+    if "zha" in hass.config.components:
+        if (
+            zha_device.device_automation_triggers is None
+            or trigger not in zha_device.device_automation_triggers
+        ):
+            raise InvalidDeviceAutomationConfig
+
+    return config
+
+
 async def async_attach_trigger(hass, config, action, automation_info):
     """Listen for state changes based on configuration."""
     trigger = (config[CONF_TYPE], config[CONF_SUBTYPE])
