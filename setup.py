0c8e208fd850c1b13e60c407f5d999dc7dca174b
quthla <quthla@users.noreply.github.com>
2019-10-11 09:27:07 +0200

    Bump python-slugify to 3.0.6 (#27430)
    * Bump python-slugify to 3.0.6
    
    * Bump python-slugify to 3.0.6
    
    * Bump python-slugify to 3.0.6

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ REQUIRES = [
     # PyJWT has loose dependency. We want the latest one.
     "cryptography==2.7",
     "pip>=8.0.3",
-    "python-slugify==3.0.4",
+    "python-slugify==3.0.6",
     "pytz>=2019.03",
     "pyyaml==5.1.2",
     "requests==2.22.0",
