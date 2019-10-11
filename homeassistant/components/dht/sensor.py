d279be2ce6008b21bd20146f28f0cbb6407ac437
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-10-11 15:22:08 +0200

    Move imports in dht component

--- homeassistant/components/dht/sensor.py
+++ homeassistant/components/dht/sensor.py
@@ -2,6 +2,7 @@
 import logging
 from datetime import timedelta
 
+import Adafruit_DHT  # pylint: disable=import-error
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -10,6 +11,7 @@ from homeassistant.const import TEMP_FAHRENHEIT, CONF_NAME, CONF_MONITORED_CONDI
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 from homeassistant.util.temperature import celsius_to_fahrenheit
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -50,7 +52,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the DHT sensor."""
-    import Adafruit_DHT  # pylint: disable=import-error
 
     SENSOR_TYPES[SENSOR_TEMPERATURE][1] = hass.config.units.temperature_unit
     available_sensors = {
