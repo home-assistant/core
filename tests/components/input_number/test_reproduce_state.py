b831662ab14e4a99f998ff70783fd8979e6ae526
Rolf K <abstractstar@gmail.com>
2019-10-12 11:48:34 +0200

    Use snake case for variable names in test_reproduce_state.

--- tests/components/input_number/test_reproduce_state.py
+++ tests/components/input_number/test_reproduce_state.py
@@ -11,8 +11,8 @@ async def test_reproducing_states(hass, caplog):
         "input_number",
         {
             "input_number": {
-                "testint": {"min": "0", "max": "100", "initial": "19"},
-                "testfloat": {"min": "0", "max": "100", "initial": "5.77"},
+                "test_int": {"min": "5", "max": "100", "initial": "19"},
+                "test_float": {"min": "5", "max": "100", "initial": "5.77"},
             }
         },
     )
@@ -20,37 +20,50 @@ async def test_reproducing_states(hass, caplog):
     # These calls should do nothing as entities already in desired state
     await hass.helpers.state.async_reproduce_state(
         [
-            State("input_number.testint", "19"),
-            State("input_number.testfloat", "5.77"),
+            State("input_number.test_int", "19"),
+            State("input_number.test_float", "5.77"),
             # Should not raise
             State("input_number.non_existing", "234"),
         ],
         blocking=True,
     )
 
-    assert hass.states.get("input_number.testint").state == "19.0"
-    assert hass.states.get("input_number.testfloat").state == "5.77"
+    assert hass.states.get("input_number.test_int").state == "19.0"
+    assert hass.states.get("input_number.test_float").state == "5.77"
 
     # Test reproducing with different state
     await hass.helpers.state.async_reproduce_state(
         [
-            State("input_number.testint", "18"),
-            State("input_number.testfloat", "7.55"),
+            State("input_number.test_int", "18"),
+            State("input_number.test_float", "7.55"),
             # Should not raise
             State("input_number.non_existing", "234"),
         ],
         blocking=True,
     )
 
-    assert hass.states.get("input_number.testint").state == "18.0"
-    assert hass.states.get("input_number.testfloat").state == "7.55"
+    assert hass.states.get("input_number.test_int").state == "18.0"
+    assert hass.states.get("input_number.test_float").state == "7.55"
+
+    # Test setting state to number out of range
+    await hass.helpers.state.async_reproduce_state(
+        [
+            State("input_number.test_int", "150"),
+            State("input_number.test_float", "1.23"),
+        ],
+        blocking=True,
+    )
+
+    # The entity states should be unchanged after trying to set them to out-of-range number
+    assert hass.states.get("input_number.test_int").state == "18.0"
+    assert hass.states.get("input_number.test_float").state == "7.55"
 
     await hass.helpers.state.async_reproduce_state(
         [
             # Test invalid state
-            State("input_number.testint", "invalid_state"),
+            State("input_number.test_int", "invalid_state"),
             # Set to state it already is.
-            State("input_number.testint", "18.0"),
+            State("input_number.test_int", "18.0"),
         ],
         blocking=True,
     )
