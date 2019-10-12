a5fec27e4772c3eef52db1b648779a3efd9b940d
Peter Nijssen <peter@peternijssen.nl>
2019-10-12 19:29:40 +0200

    Updated PostNL to also contain distributions and letters

--- homeassistant/components/postnl/sensor.py
+++ homeassistant/components/postnl/sensor.py
@@ -44,21 +44,28 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     try:
         api = PostNL_API(username, password)
+        _LOGGER.debug("Connection with PostNL API succeeded")
 
     except UnauthorizedException:
         _LOGGER.exception("Can't connect to the PostNL webservice")
         return
 
-    add_entities([PostNLSensor(api, name)], True)
+    add_entities([PostNLDelivery(api, name)], True)
+    add_entities([PostNLDistribution(api, name)], True)
+    add_entities([PostNLLetter(api, name)], True)
 
 
-class PostNLSensor(Entity):
-    """Representation of a PostNL sensor."""
+class PostNLDelivery(Entity):
+    """Delivery Packages."""
 
     def __init__(self, api, name):
         """Initialize the PostNL sensor."""
-        self._name = name
-        self._attributes = {ATTR_ATTRIBUTION: ATTRIBUTION, "shipments": []}
+        self._name = name + "_delivery"
+        self._attributes = {
+            ATTR_ATTRIBUTION: ATTRIBUTION,
+            'enroute': [],
+            'delivered': [],
+        }
         self._state = None
         self._api = api
 
@@ -75,7 +82,7 @@ class PostNLSensor(Entity):
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement of this entity, if any."""
-        return "packages"
+        return 'packages'
 
     @property
     def device_state_attributes(self):
@@ -85,16 +92,130 @@ class PostNLSensor(Entity):
     @property
     def icon(self):
         """Icon to use in the frontend."""
-        return ICON
+        return "mdi:package-variant-closed"
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """Update device state."""
-        shipments = self._api.get_relevant_deliveries()
+        shipments = self._api.get_deliveries()
 
-        self._attributes["shipments"] = []
+        self._attributes['enroute'] = []
+        self._attributes['delivered'] = []
 
         for shipment in shipments:
-            self._attributes["shipments"].append(vars(shipment))
+            if shipment.delivery_date is None:
+                self._attributes['enroute'].append(vars(shipment))
+            else:
+                self._attributes['delivered'].append(vars(shipment))
 
-        self._state = len(shipments)
+        self._state = len(self._attributes['enroute'])
+
+
+class PostNLDistribution(Entity):
+    """Distribution Packages."""
+
+    def __init__(self, api, name):
+        """Initialize the PostNL sensor."""
+        self._name = name + "_distribution"
+        self._attributes = {
+            ATTR_ATTRIBUTION: ATTRIBUTION,
+            'enroute': [],
+            'delivered': [],
+        }
+        self._state = None
+        self._api = api
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return 'packages'
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend."""
+        return "mdi:package-variant-closed"
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        """Update device state."""
+        shipments = self._api.get_distributions()
+
+        self._attributes['enroute'] = []
+        self._attributes['delivered'] = []
+
+        for shipment in shipments:
+            if shipment.delivery_date is None:
+                self._attributes['enroute'].append(vars(shipment))
+            else:
+                self._attributes['delivered'].append(vars(shipment))
+
+        self._state = len(self._attributes['enroute'])
+
+
+class PostNLLetter(Entity):
+    """Letters."""
+
+    def __init__(self, api, name):
+        """Initialize the PostNL sensor."""
+        self._name = name + "_letters"
+        self._attributes = {
+            ATTR_ATTRIBUTION: ATTRIBUTION,
+            'letters': [],
+            'enabled': False,
+        }
+        self._state = None
+        self._api = api
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return 'letters'
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend."""
+        return "mdi:email"
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        """Update device state."""
+        self._attributes['enabled'] = self._api.is_letters_activated
+
+        if self._attributes['enabled']:
+            letters = self._api.get_letters()
+
+            self._attributes['letters'] = []
+
+            for letter in letters:
+                self._attributes['letters'].append(vars(letter))
+            self._state = len(letters)
