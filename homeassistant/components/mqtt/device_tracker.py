7f82c924d0e8a5dc35c3787fa0175b71a0e15c10
Raman Gupta <7243222+raman325@users.noreply.github.com>
2019-10-13 09:48:43 -0400

    change parameter name for custom mapping of payloads to non-home zones to be more descriptive

--- homeassistant/components/mqtt/device_tracker.py
+++ homeassistant/components/mqtt/device_tracker.py
@@ -13,14 +13,14 @@ from . import CONF_QOS
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_PAYLOAD_CUSTOM = "payload_custom"
+CONF_PAYLOAD_OTHER_ZONES = "payload_other_zones"
 CONF_PAYLOAD_HOME = "payload_home"
 CONF_PAYLOAD_NOT_HOME = "payload_not_home"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(mqtt.SCHEMA_BASE).extend(
     {
         vol.Required(CONF_DEVICES): {cv.string: mqtt.valid_subscribe_topic},
-        vol.Optional(CONF_PAYLOAD_CUSTOM, default=dict()): {cv.string: cv.string},
+        vol.Optional(CONF_PAYLOAD_OTHER_ZONES, default=dict()): {cv.string: cv.string},
         vol.Optional(CONF_PAYLOAD_HOME, default=STATE_HOME): cv.string,
         vol.Optional(CONF_PAYLOAD_NOT_HOME, default=STATE_NOT_HOME): cv.string,
     }
@@ -31,7 +31,7 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
     """Set up the MQTT tracker."""
     devices = config[CONF_DEVICES]
     qos = config[CONF_QOS]
-    payload_custom = config[CONF_PAYLOAD_CUSTOM]
+    payload_other_zones = config[CONF_PAYLOAD_OTHER_ZONES]
     payload_home = config[CONF_PAYLOAD_HOME]
     payload_not_home = config[CONF_PAYLOAD_NOT_HOME]
 
@@ -44,8 +44,8 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
                 location_name = STATE_HOME
             elif msg.payload == payload_not_home:
                 location_name = STATE_NOT_HOME
-            elif msg.payload in payload_custom:
-                location_name = payload_custom[msg.payload]
+            elif msg.payload in payload_other_zones:
+                location_name = payload_other_zones[msg.payload]
             else:
                 location_name = msg.payload
 
