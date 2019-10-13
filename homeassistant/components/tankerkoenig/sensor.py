deba66d485c06fe1a24b16f3c4a1326d832ccd43
Guillem Pagès <git@guillempages.cat>
2019-10-14 00:34:23 +0200

    Update values regularly
    The tankerkoenig values get updated regularly with real data.

--- homeassistant/components/tankerkoenig/sensor.py
+++ homeassistant/components/tankerkoenig/sensor.py
@@ -1,22 +1,23 @@
 """Platform for tankerkoenig sensor integration."""
 import logging
 
+from datetime import timedelta
 import pytankerkoenig
 
 import voluptuous as vol
-
-from datetime import timedelta
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
     ATTR_ATTRIBUTION,
-    ATTR_LOCATION,
+    ATTR_LATITUDE,
+    ATTR_LONGITUDE,
     CONF_API_KEY,
     CONF_LATITUDE,
     CONF_LONGITUDE,
     CONF_RADIUS,
+    CONF_SCAN_INTERVAL,
     STATE_CLOSED,
     STATE_OPEN,
 )
@@ -24,6 +25,7 @@ from .const import NAME
 
 _LOGGER = logging.getLogger(__name__)
 
+ATTR_ADDRESS = "address"
 ATTR_BRAND = "brand"
 ATTR_FUEL = "fuel_type"
 ATTR_IS_OPEN = "state"
@@ -51,6 +53,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
             CONF_LONGITUDE, "coordinates", "Latitude and longitude must exist together"
         ): cv.longitude,
         vol.Optional(CONF_RADIUS, DEFAULT_RADIUS): cv.positive_int,
+        vol.Optional(CONF_SCAN_INTERVAL, SCAN_INTERVAL): cv.time_period,
     }
 )
 
@@ -76,11 +79,15 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
             api_key, latitude, longitude, radius, fuel, "dist"
         )
 
-        if len(data["stations"]) <= 0:
-            _LOGGER.error("Could not find any station in range")
-        else:
-            station = data["stations"][0]
-            entities.append(FuelPriceSensor(fuel, station))
+        for station in data["stations"]:
+            if len(data["stations"]) <= 0:
+                _LOGGER.error("Could not find any station in range")
+            else:
+                entities.append(
+                    FuelPriceSensor(
+                        api_key, fuel, station, f"{NAME}_{station['name']}_{fuel}"
+                    )
+                )
 
     async_add_entities(entities, True)
 
@@ -88,24 +95,24 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 class FuelPriceSensor(Entity):
     """Contains prices for fuels in the given station."""
 
-    def __init__(self, fuel_type, station):
+    def __init__(self, api_key, fuel_type, station, name=NAME):
         """Initialize the sensor."""
-        self._data = station["price"]
+        self._api_key = api_key
+        self._station = station
         self._fuel_type = fuel_type
-        self._name = NAME
-        self._station_name = station["name"]
-        self._station_id = station["id"]
+        self._name = name
+        self._latitude = station["lat"]
+        self._longitude = station["lng"]
         if station["isOpen"]:
             self._is_open = STATE_OPEN
         else:
             self._is_open = STATE_CLOSED
         self._address = f"{station['street']} {station['houseNumber']}, {station['postCode']} {station['place']}"
-        self._brand = station["brand"]
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return "Tankerkoenig"
+        return self._name
 
     @property
     def icon(self):
@@ -120,21 +127,29 @@ class FuelPriceSensor(Entity):
     @property
     def state(self):
         """Return the state of the device."""
-        return self._data
+        return self._station["price"]
 
     @property
     def device_state_attributes(self):
         """Return the attributes of the device."""
         attrs = {
             ATTR_ATTRIBUTION: ATTRIBUTION,
-            ATTR_BRAND: self._brand,
+            ATTR_BRAND: self._station["brand"],
             ATTR_FUEL: self._fuel_type,
-            ATTR_STATION_NAME: self._station_name,
-            ATTR_LOCATION: self._address,
+            ATTR_STATION_NAME: self._station["name"],
+            ATTR_ADDRESS: self._address,
+            ATTR_LATITUDE: self._latitude,
+            ATTR_LONGITUDE: self._longitude,
             ATTR_IS_OPEN: self._is_open,
         }
         return attrs
 
     async def async_update(self):
         """Fetch new prices."""
-        self._data = 1.429
+        self._station = pytankerkoenig.getNearbyStations(
+            self._api_key, self._latitude, self._longitude, 1, self._fuel_type, "dist"
+        )["stations"][0]
+        if self._station["isOpen"]:
+            self._is_open = STATE_OPEN
+        else:
+            self._is_open = STATE_CLOSED
