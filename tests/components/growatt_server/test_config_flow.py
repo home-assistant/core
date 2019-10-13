cc234346719393871eabb9940ce58d0183f2a7f2
Indy Koning <indykoning@hotmail.com>
2019-10-13 14:48:30 +0200

    Ran black formatter for tests.

--- tests/components/growatt_server/test_config_flow.py
+++ tests/components/growatt_server/test_config_flow.py
@@ -10,32 +10,33 @@ FIXTURE_USER_INPUT = {
     CONF_NAME: DEFAULT_NAME,
     CONF_PLANT_ID: None,
     CONF_USERNAME: "username",
-    CONF_PASSWORD: "password"
+    CONF_PASSWORD: "password",
 }
 
 GROWATT_PLANT_LIST_RESPONSE = {
-    'data': [
-    {
-        'plantMoneyText': '474.9 (€)',
-        'plantName': 'Plant name',
-        'plantId': '123456',
-        'isHaveStorage': 'false',
-        'todayEnergy': '2.6 kWh',
-        'totalEnergy': '2.37 MWh',
-        'currentPower': '628.8 W'
-    }
+    "data": [
+        {
+            "plantMoneyText": "474.9 (€)",
+            "plantName": "Plant name",
+            "plantId": "123456",
+            "isHaveStorage": "false",
+            "todayEnergy": "2.6 kWh",
+            "totalEnergy": "2.37 MWh",
+            "currentPower": "628.8 W",
+        }
     ],
-    'totalData': {
-        'currentPowerSum': '628.8 W',
-        'CO2Sum': '2.37 KT',
-        'isHaveStorage': 'false',
-        'eTotalMoneyText': '474.9 (€)',
-        'todayEnergySum': '2.6 kWh',
-        'totalEnergySum': '2.37 MWh'
+    "totalData": {
+        "currentPowerSum": "628.8 W",
+        "CO2Sum": "2.37 KT",
+        "isHaveStorage": "false",
+        "eTotalMoneyText": "474.9 (€)",
+        "todayEnergySum": "2.6 kWh",
+        "totalEnergySum": "2.37 MWh",
     },
-    'success': True
+    "success": True,
 }
-GROWATT_LOGIN_RESPONSE = {'userId': 123456, 'userLevel': 1, 'success': True}
+GROWATT_LOGIN_RESPONSE = {"userId": 123456, "userLevel": 1, "success": True}
+
 
 async def test_show_authenticate_form(hass):
     """Test that the setup form is served."""
@@ -46,31 +47,45 @@ async def test_show_authenticate_form(hass):
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "init"
 
+
 async def test_incorrect_username(hass):
     """Test that it shows the appropriate error when an incorrect username is entered."""
     flow = config_flow.GrowattServerConfigFlow()
     flow.hass = hass
-    with patch('growattServer.GrowattApi.login', return_value={'errCode': '102', 'success': False}):
+    with patch(
+        "growattServer.GrowattApi.login",
+        return_value={"errCode": "102", "success": False},
+    ):
         result = await flow.async_step_user(user_input=FIXTURE_USER_INPUT)
         assert result["errors"] == {"base": "auth_error"}
+
 
 async def test_multiple_plants(hass):
     """Test that it shows an error when no plant_id is entered and multiple plants are available on the account."""
     flow = config_flow.GrowattServerConfigFlow()
     flow.hass = hass
     growatt_list_response_more_plants = GROWATT_PLANT_LIST_RESPONSE
-    growatt_list_response_more_plants["data"].append(growatt_list_response_more_plants["data"][0])
-    with patch('growattServer.GrowattApi.login', return_value=GROWATT_LOGIN_RESPONSE):
-        with patch('growattServer.GrowattApi.plant_list', return_value=GROWATT_PLANT_LIST_RESPONSE):
+    growatt_list_response_more_plants["data"].append(
+        growatt_list_response_more_plants["data"][0]
+    )
+    with patch("growattServer.GrowattApi.login", return_value=GROWATT_LOGIN_RESPONSE):
+        with patch(
+            "growattServer.GrowattApi.plant_list",
+            return_value=GROWATT_PLANT_LIST_RESPONSE,
+        ):
             result = await flow.async_step_user(user_input=FIXTURE_USER_INPUT)
             assert result["errors"] == {"base": "multiple_plants"}
+
 
 async def test_full_flow_implementation(hass):
     """Test registering an integration and finishing flow works."""
     flow = config_flow.GrowattServerConfigFlow()
     flow.hass = hass
-    with patch('growattServer.GrowattApi.login', return_value=GROWATT_LOGIN_RESPONSE):
-        with patch('growattServer.GrowattApi.plant_list', return_value=GROWATT_PLANT_LIST_RESPONSE):
+    with patch("growattServer.GrowattApi.login", return_value=GROWATT_LOGIN_RESPONSE):
+        with patch(
+            "growattServer.GrowattApi.plant_list",
+            return_value=GROWATT_PLANT_LIST_RESPONSE,
+        ):
             result = await flow.async_step_user(user_input=None)
             assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
             assert result["step_id"] == "init"
@@ -79,7 +94,9 @@ async def test_full_flow_implementation(hass):
             assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
             assert result["title"] == FIXTURE_USER_INPUT[CONF_NAME]
             assert result["data"][CONF_NAME] == FIXTURE_USER_INPUT[CONF_NAME]
-            assert result["data"][CONF_PLANT_ID] == GROWATT_PLANT_LIST_RESPONSE["data"][0]["plantId"]
+            assert (
+                result["data"][CONF_PLANT_ID]
+                == GROWATT_PLANT_LIST_RESPONSE["data"][0]["plantId"]
+            )
             assert result["data"][CONF_USERNAME] == FIXTURE_USER_INPUT[CONF_USERNAME]
             assert result["data"][CONF_PASSWORD] == FIXTURE_USER_INPUT[CONF_PASSWORD]
-
