690aaf6abde7da1b41f1028e75015f113a424da3
ziv1234 <16467659+ziv1234@users.noreply.github.com>
2019-10-14 00:29:53 +0300

    Mostly doc changes and fixing enums

--- homeassistant/components/dynalite/const.py
+++ homeassistant/components/dynalite/const.py
@@ -3,17 +3,14 @@ import logging
 
 LOGGER = logging.getLogger(__package__)
 DOMAIN = "dynalite"
+DATA_CONFIGS = "dynalite_configs"
 
-
-CONF_BRIDGES = "bridges"
 CONF_AREACREATE = "areacreate"
 CONF_AREA_CREATE_MANUAL = "manual"
 CONF_AREA_CREATE_ASSIGN = "assign"
 CONF_AREA_CREATE_AUTO = "auto"
+CONF_BRIDGES = "bridges"
 
-DATA_CONFIGS = "dynalite_configs"
 ENTITY_CATEGORIES = ["light", "switch", "cover"]
-
 DEFAULT_NAME = "dynalite"
 DEFAULT_PORT = 12345
-DEFAULT_LOGGING = "info"
