55037e38300288b40d73c840f4a86a43d5777496
Steven D. Lander <stevendlander@gmail.com>
2019-10-13 19:16:22 -0400

    Fixed black formatting

--- homeassistant/components/tensorflow/image_processing.py
+++ homeassistant/components/tensorflow/image_processing.py
@@ -14,7 +14,7 @@ except ImportError:
 
 try:
     # Verify that the TensorFlow Object Detection API is pre-installed
-    import tensorflow as tf # noqa
+    import tensorflow as tf  # noqa
     from object_detection.utils import label_map_util  # noqa
 except ImportError:
     label_map_util = None
