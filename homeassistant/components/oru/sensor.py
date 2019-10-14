3b0acb80f472c676e418942ed61d4d4088dbd122
Bogdan Vlaicu <bogdanvlaicu@yahoo.com>
2019-10-14 19:49:37 -0400

    Added PLATFORM_SCHEMA Adde unique_id property Changed logger level from info to debug when printing the updated sensor value Set the SCAN_INTERVAL to 15 mins Added exception handling durin init when creating the oru meter instance

--- homeassistant/components/oru/sensor.py
+++ homeassistant/components/oru/sensor.py
@@ -1,8 +1,14 @@
 """Platform for sensor integration."""
+from datetime import timedelta
 import logging
+
+import voluptuous as vol
 
 from oru import Meter
 from oru import MeterError
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
 from homeassistant.const import ENERGY_KILO_WATT_HOUR
 from homeassistant.helpers.entity import Entity
 
@@ -10,15 +16,25 @@ _LOGGER = logging.getLogger(__name__)
 
 CONF_METER_NUMBER = "meter_number"
 
+SCAN_INTERVAL = timedelta(minutes=15)
+
 SENSOR_NAME = "ORU Current Energy Usage"
 SENSOR_ICON = "mdi:counter"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({vol.Required(CONF_METER_NUMBER): cv.string})
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
 
     meter_number = str(config.get(CONF_METER_NUMBER, None))
-    meter = Meter(meter_number)
+
+    try:
+        meter = Meter(meter_number)
+
+    except MemoryError:
+        _LOGGER.error("Unable to create Oru meter")
+        return
 
     add_entities([CurrentEnergyUsageSensor(meter)])
 
@@ -33,6 +49,11 @@ class CurrentEnergyUsageSensor(Entity):
         self._state = None
         self._available = None
         self.meter = meter
+
+    @property
+    def unique_id(self):
+        """Return a unique, HASS-friendly identifier for this entity."""
+        return self.meter.meter_id
 
     @property
     def name(self):
@@ -62,7 +83,9 @@ class CurrentEnergyUsageSensor(Entity):
             self._state = last_read
             self._available = True
 
-            _LOGGER.info("%s = %s %s", self.name, self._state, self.unit_of_measurement)
+            _LOGGER.debug(
+                "%s = %s %s", self.name, self._state, self.unit_of_measurement
+            )
         except MeterError as err:
             self._available = False
 
