88f21ffade8bf47a1f1cdde37c661b7dd78fcd1b
Rolf K <abstractstar@gmail.com>
2019-10-15 11:49:23 +0200

    Add support for set_options
    Allows defining the options for an input_select in a scene.

--- homeassistant/components/input_select/reproduce_state.py
+++ homeassistant/components/input_select/reproduce_state.py
@@ -1,13 +1,22 @@
-"""Reproduce an Input selec state."""
+"""Reproduce an Input select state."""
 import asyncio
 import logging
+from types import MappingProxyType
 from typing import Iterable, Optional
 
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import Context, State
 from homeassistant.helpers.typing import HomeAssistantType
 
-from . import DOMAIN, SERVICE_SELECT_OPTION, ATTR_OPTION
+from . import (
+    DOMAIN,
+    SERVICE_SELECT_OPTION,
+    SERVICE_SET_OPTIONS,
+    ATTR_OPTION,
+    ATTR_OPTIONS,
+)
+
+ATTR_GROUP = [ATTR_OPTION, ATTR_OPTIONS]
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,12 +33,30 @@ async def _async_reproduce_state(
         return
 
     # Return if we are already at the right state.
-    if cur_state.state == state.state:
+    if cur_state.state == state.state and all(
+        check_attr_equal(cur_state.attributes, state.attributes, attr)
+        for attr in ATTR_GROUP
+    ):
         return
 
-    # Call service
+    # Set service data
+    service_data = {ATTR_ENTITY_ID: state.entity_id}
+
+    # If options are specified, call SERVICE_SET_OPTIONS
+    if ATTR_OPTIONS in state.attributes:
+        service = SERVICE_SET_OPTIONS
+        service_data[ATTR_OPTIONS] = state.attributes[ATTR_OPTIONS]
+
+        await hass.services.async_call(
+            DOMAIN, service, service_data, context=context, blocking=True
+        )
+
+        # Remove ATTR_OPTIONS from service_data so we can reuse service_data in next call
+        del service_data[ATTR_OPTIONS]
+
+    # Call SERVICE_SELECT_OPTION
     service = SERVICE_SELECT_OPTION
-    service_data = {ATTR_ENTITY_ID: state.entity_id, ATTR_OPTION: state.state}
+    service_data[ATTR_OPTION] = state.state
 
     await hass.services.async_call(
         DOMAIN, service, service_data, context=context, blocking=True
@@ -39,8 +66,15 @@ async def _async_reproduce_state(
 async def async_reproduce_states(
     hass: HomeAssistantType, states: Iterable[State], context: Optional[Context] = None
 ) -> None:
-    """Reproduce Input selec states."""
+    """Reproduce Input select states."""
     # Reproduce states in parallel.
     await asyncio.gather(
         *(_async_reproduce_state(hass, state, context) for state in states)
     )
+
+
+def check_attr_equal(
+    attr1: MappingProxyType, attr2: MappingProxyType, attr_str: str
+) -> bool:
+    """Return true if the given attributes are equal."""
+    return attr1.get(attr_str) == attr2.get(attr_str)
