7f422458a66fb20e57116cf685a7dd5b27d788aa
Rolf K <abstractstar@gmail.com>
2019-10-15 13:01:57 +0200

    Add tests for set_options in test_reproduce_state

--- tests/components/input_select/test_reproduce_state.py
+++ tests/components/input_select/test_reproduce_state.py
@@ -1,12 +1,18 @@
 """Test reproduce state for Input select."""
 from homeassistant.core import State
 from homeassistant.setup import async_setup_component
+from tests.common import async_mock_service
 
 VALID_OPTION1 = "Option A"
 VALID_OPTION2 = "Option B"
-VALID_OPTION3 = "Option X"
-INVALID_OPTION = "Option Y"
-OPTIONS = [VALID_OPTION1, VALID_OPTION2, VALID_OPTION3]
+VALID_OPTION3 = "Option C"
+VALID_OPTION4 = "Option D"
+VALID_OPTION5 = "Option E"
+VALID_OPTION6 = "Option F"
+INVALID_OPTION = "Option X"
+VALID_OPTION_SET1 = [VALID_OPTION1, VALID_OPTION2, VALID_OPTION3]
+VALID_OPTION_SET2 = [VALID_OPTION4, VALID_OPTION5, VALID_OPTION6]
+ENTITY = "input_select.test_select"
 
 
 async def test_reproducing_states(hass, caplog):
@@ -18,7 +24,7 @@ async def test_reproducing_states(hass, caplog):
         "input_select",
         {
             "input_select": {
-                "test_select": {"options": OPTIONS, "initial": VALID_OPTION1}
+                "test_select": {"options": VALID_OPTION_SET1, "initial": VALID_OPTION1}
             }
         },
     )
@@ -26,7 +32,7 @@ async def test_reproducing_states(hass, caplog):
     # These calls should do nothing as entities already in desired state
     await hass.helpers.state.async_reproduce_state(
         [
-            State("input_select.test_select", VALID_OPTION1),
+            State(ENTITY, VALID_OPTION1),
             # Should not raise
             State("input_select.non_existing", VALID_OPTION1),
         ],
@@ -34,12 +40,12 @@ async def test_reproducing_states(hass, caplog):
     )
 
     # Test that entity is in desired state
-    assert hass.states.get("input_select.test_select").state == VALID_OPTION1
+    assert hass.states.get(ENTITY).state == VALID_OPTION1
 
     # Try reproducing with different state
     await hass.helpers.state.async_reproduce_state(
         [
-            State("input_select.test_select", VALID_OPTION3),
+            State(ENTITY, VALID_OPTION3),
             # Should not raise
             State("input_select.non_existing", VALID_OPTION3),
         ],
@@ -47,12 +53,27 @@ async def test_reproducing_states(hass, caplog):
     )
 
     # Test that we got the desired result
-    assert hass.states.get("input_select.test_select").state == VALID_OPTION3
+    assert hass.states.get(ENTITY).state == VALID_OPTION3
 
     # Test setting state to invalid state
     await hass.helpers.state.async_reproduce_state(
-        [State("input_select.test_select", INVALID_OPTION)], blocking=True
+        [State(ENTITY, INVALID_OPTION)], blocking=True
     )
 
     # The entity state should be unchanged
-    assert hass.states.get("input_select.test_select").state == VALID_OPTION3
+    assert hass.states.get(ENTITY).state == VALID_OPTION3
+
+    # Test setting a different set of options
+    set_options_calls = async_mock_service(hass, "input_select", "set_options")
+    select_option_calls = async_mock_service(hass, "input_select", "select_option")
+
+    await hass.helpers.state.async_reproduce_state(
+        [State(ENTITY, VALID_OPTION5, {"options": VALID_OPTION_SET2})], blocking=True
+    )
+
+    # Test that both set_options and select_option were called
+    assert len(select_option_calls) == 1
+    assert select_option_calls[0].data == {"entity_id": ENTITY, "option": VALID_OPTION5}
+
+    assert len(set_options_calls) == 1
+    assert set_options_calls[0].data == {"entity_id": ENTITY, "option": VALID_OPTION5}
