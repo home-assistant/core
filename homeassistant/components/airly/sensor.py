15eba834ca6b3fd45757b3f884b5b5c612831b8e
bieniu <maciej.bieniek@gmail.com>
2019-10-16 07:20:27 +0200

    Remove icons from sensors with device_class

--- homeassistant/components/airly/sensor.py
+++ homeassistant/components/airly/sensor.py
@@ -37,19 +37,19 @@ SENSOR_TYPES = {
     },
     ATTR_API_HUMIDITY: {
         ATTR_DEVICE_CLASS: DEVICE_CLASS_HUMIDITY,
-        ATTR_ICON: "mdi:water-percent",
+        ATTR_ICON: None,
         ATTR_LABEL: ATTR_API_HUMIDITY.capitalize(),
         ATTR_UNIT: HUMI_PERCENT,
     },
     ATTR_API_PRESSURE: {
         ATTR_DEVICE_CLASS: DEVICE_CLASS_PRESSURE,
-        ATTR_ICON: "mdi:gauge",
+        ATTR_ICON: None,
         ATTR_LABEL: ATTR_API_PRESSURE.capitalize(),
         ATTR_UNIT: PRESSURE_HPA,
     },
     ATTR_API_TEMPERATURE: {
         ATTR_DEVICE_CLASS: DEVICE_CLASS_TEMPERATURE,
-        ATTR_ICON: "mdi:thermometer",
+        ATTR_ICON: None,
         ATTR_LABEL: ATTR_API_TEMPERATURE.capitalize(),
         ATTR_UNIT: TEMP_CELSIUS,
     },
