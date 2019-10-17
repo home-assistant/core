d6be6fe615a2b624cb84d19232a0fd08baab99e0
Ernst Klamer <e.klamer@gmail.com>
2019-10-17 19:59:44 +0200

    Config flow added (__init__.py)

--- homeassistant/components/solarlog/__init__.py
+++ homeassistant/components/solarlog/__init__.py
@@ -1,1 +1,24 @@
-"""Solar-Log API integration."""
+"""Solar-Log integration."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import HomeAssistantType
+
+
+async def async_setup(hass, config):
+    """Component setup, do nothing."""
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
+    """Set up a config entry for solarlog."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    )
+    return True
