2c98a82af5d7512646002d01f29ca8910c71b9e2
Tobias Haase <tobhaase@gmail.com>
2019-10-18 18:57:43 +0200

    Improved process

--- homeassistant/components/vacuum/reproduce_state.py
+++ homeassistant/components/vacuum/reproduce_state.py
@@ -64,24 +64,27 @@ async def _async_reproduce_state(
 
     service_data = {ATTR_ENTITY_ID: state.entity_id}
 
-    if state.state == STATE_ON:
-        service = SERVICE_TURN_ON
-    elif state.state == STATE_OFF:
-        service = SERVICE_TURN_OFF
-    elif state.state == STATE_CLEANING:
-        service = SERVICE_START
-    elif state.state == STATE_DOCKED or state.state == STATE_RETURNING:
-        service = SERVICE_RETURN_TO_BASE
-    elif state.state == STATE_IDLE:
-        service = SERVICE_STOP
-    elif state.state == STATE_PAUSED:
-        service = SERVICE_PAUSE
+    if cur_state.state != state.state:
+        # Wrong state
+        if state.state == STATE_ON:
+            service = SERVICE_TURN_ON
+        elif state.state == STATE_OFF:
+            service = SERVICE_TURN_OFF
+        elif state.state == STATE_CLEANING:
+            service = SERVICE_START
+        elif state.state == STATE_DOCKED or state.state == STATE_RETURNING:
+            service = SERVICE_RETURN_TO_BASE
+        elif state.state == STATE_IDLE:
+            service = SERVICE_STOP
+        elif state.state == STATE_PAUSED:
+            service = SERVICE_PAUSE
 
-    await hass.services.async_call(
-        DOMAIN, service, service_data, context=context, blocking=True
-    )
+        await hass.services.async_call(
+            DOMAIN, service, service_data, context=context, blocking=True
+        )
 
-    if ATTR_FAN_SPEED in state.attributes:
+    if cur_state.attributes.get(ATTR_FAN_SPEED) != state.attributes.get(ATTR_FAN_SPEED):
+        # Wrong fan speed
         service_data["fan_speed"] = state.attributes[ATTR_FAN_SPEED]
         await hass.services.async_call(
             DOMAIN, SERVICE_SET_FAN_SPEED, service_data, context=context, blocking=True
