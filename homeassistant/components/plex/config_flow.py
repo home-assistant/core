b43b3dc8c87c9ae371af5f6e81515f0edebe2df6
Jason Lawrence <jjlawren@users.noreply.github.com>
2019-10-19 21:59:51 -0500

    Save client identifier from auth for future use

--- homeassistant/components/plex/config_flow.py
+++ homeassistant/components/plex/config_flow.py
@@ -51,7 +51,7 @@ def configured_servers(hass):
 class PlexFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a Plex config flow."""
 
-    VERSION = 1
+    VERSION = 2
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     @staticmethod
