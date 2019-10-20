03bf08a42f683d39214d429731b8397a34dd239e
Quentame <polletquentin74@me.com>
2019-10-20 12:15:46 +0200

    Fixing config_entries.async_forward_entry_unload calls (step 1) (#27857)

--- homeassistant/components/withings/__init__.py
+++ homeassistant/components/withings/__init__.py
@@ -92,8 +92,4 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
 
 async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry):
     """Unload Withings config entry."""
-    await hass.async_create_task(
-        hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    )
-
-    return True
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
