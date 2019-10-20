5d94c821751021251b5c6e1d2d185972de985f11
David Bonnes <zxdavb@bonnes.me>
2019-10-20 20:17:27 +0100

    isort the geniushub code (#27978)

--- homeassistant/components/geniushub/__init__.py
+++ homeassistant/components/geniushub/__init__.py
@@ -4,9 +4,8 @@ import logging
 from typing import Any, Dict, Optional
 
 import aiohttp
-import voluptuous as vol
-
 from geniushubclient import GeniusHub
+import voluptuous as vol
 
 from homeassistant.const import (
     ATTR_TEMPERATURE,
@@ -22,8 +21,8 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.helpers.dispatcher import (
-    async_dispatcher_send,
     async_dispatcher_connect,
+    async_dispatcher_send,
 )
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import async_track_time_interval
