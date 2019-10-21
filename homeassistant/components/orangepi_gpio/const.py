c0aee4033476d554c7cb94ca0a952eb82e2307f6
Pascal Roeleven <dev@pascalroeleven.nl>
2019-10-21 13:56:03 +0200

    Code cleanup for orangepi_gpio (#27958)
    * Code cleanup for orangepi_gpio
    
    * Move constants to const.py
    
    * Use async wherever possible
    
    * Remove obsolute functions
    
    * Use relative package integration imports
    
    * Move callbacks to async_added_to_hass
    
    * Avoid side effects in init
    
    * Prevent blocking I/O in coroutines
    
    * Make sure entity is setup before added

--- homeassistant/components/orangepi_gpio/const.py
+++ homeassistant/components/orangepi_gpio/const.py
@@ -1,14 +1,14 @@
 """Constants for Orange Pi GPIO."""
+
 import voluptuous as vol
 
 from homeassistant.helpers import config_validation as cv
 
-from . import CONF_PIN_MODE, PIN_MODES
-
 CONF_INVERT_LOGIC = "invert_logic"
+CONF_PIN_MODE = "pin_mode"
 CONF_PORTS = "ports"
-
 DEFAULT_INVERT_LOGIC = False
+PIN_MODES = ["pc", "zeroplus", "zeroplus2", "deo", "neocore2"]
 
 _SENSORS_SCHEMA = vol.Schema({cv.positive_int: cv.string})
 
