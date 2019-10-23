aaa6318efd5cbe269372dc12286fdfebfc99bbd6
Raman Gupta <7243222+raman325@users.noreply.github.com>
2019-10-23 01:26:29 -0400

    Support custom source type for MQTT device tracker (#27838)
    * support custom source type for MQTT device tracker
    
    * fix typo
    
    * add abbreviation

--- homeassistant/components/mqtt/device_tracker.py
+++ homeassistant/components/mqtt/device_tracker.py
@@ -4,7 +4,7 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components import mqtt
-from homeassistant.components.device_tracker import PLATFORM_SCHEMA
+from homeassistant.components.device_tracker import PLATFORM_SCHEMA, SOURCE_TYPES
 from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import CONF_DEVICES, STATE_NOT_HOME, STATE_HOME
@@ -15,12 +15,14 @@ _LOGGER = logging.getLogger(__name__)
 
 CONF_PAYLOAD_HOME = "payload_home"
 CONF_PAYLOAD_NOT_HOME = "payload_not_home"
+CONF_SOURCE_TYPE = "source_type"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(mqtt.SCHEMA_BASE).extend(
     {
         vol.Required(CONF_DEVICES): {cv.string: mqtt.valid_subscribe_topic},
         vol.Optional(CONF_PAYLOAD_HOME, default=STATE_HOME): cv.string,
         vol.Optional(CONF_PAYLOAD_NOT_HOME, default=STATE_NOT_HOME): cv.string,
+        vol.Optional(CONF_SOURCE_TYPE): vol.In(SOURCE_TYPES),
     }
 )
 
@@ -31,6 +33,7 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
     qos = config[CONF_QOS]
     payload_home = config[CONF_PAYLOAD_HOME]
     payload_not_home = config[CONF_PAYLOAD_NOT_HOME]
+    source_type = config.get(CONF_SOURCE_TYPE)
 
     for dev_id, topic in devices.items():
 
@@ -44,9 +47,11 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
             else:
                 location_name = msg.payload
 
-            hass.async_create_task(
-                async_see(dev_id=dev_id, location_name=location_name)
-            )
+            see_args = {"dev_id": dev_id, "location_name": location_name}
+            if source_type:
+                see_args["source_type"] = source_type
+
+            hass.async_create_task(async_see(**see_args))
 
         await mqtt.async_subscribe(hass, topic, async_message_received, qos)
 
