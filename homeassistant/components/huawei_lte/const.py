feaa3824c6e1a346e3928e02de6a4c9c7c3a4f67
Ville Skyttä <ville.skytta@iki.fi>
2019-10-25 02:42:54 +0300

    Huawei LTE sensor metadata update (#28187)

--- homeassistant/components/huawei_lte/const.py
+++ homeassistant/components/huawei_lte/const.py
@@ -7,6 +7,9 @@ DEFAULT_DEVICE_NAME = "LTE"
 UPDATE_SIGNAL = f"{DOMAIN}_update"
 UPDATE_OPTIONS_SIGNAL = f"{DOMAIN}_options_update"
 
+UNIT_BYTES = "B"
+UNIT_SECONDS = "s"
+
 KEY_DEVICE_BASIC_INFORMATION = "device_basic_information"
 KEY_DEVICE_INFORMATION = "device_information"
 KEY_DEVICE_SIGNAL = "device_signal"
