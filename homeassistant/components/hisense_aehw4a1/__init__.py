c3afe97c1121eb8afe8b3766bc63a7d95bfdd4c8
Davide Varricchio <davide.varricchio@gmail.com>
2019-10-24 10:47:59 +0200

    First commit

--- homeassistant/components/hisense_aehw4a1/__init__.py
+++ homeassistant/components/hisense_aehw4a1/__init__.py
@@ -1,5 +1,6 @@
 """The Hisense AEH-W4A1 integration."""
 import voluptuous as vol
+<<<<<<< HEAD
 import homeassistant.helpers.config_validation as cv
 
 from homeassistant import config_entries
@@ -13,10 +14,18 @@ CONFIG_SCHEMA = vol.Schema(
     {DOMAIN: {CLIMA_DOMAIN: vol.Schema(vol.All(cv.ensure_list, [INTERFACE_SCHEMA]))}},
     extra=vol.ALLOW_EXTRA,
 )
+=======
+
+from .const import DOMAIN
+
+
+CONFIG_SCHEMA = vol.Schema({vol.Optional(DOMAIN): {}})
+>>>>>>> First commit
 
 
 async def async_setup(hass, config):
     """Set up the Hisense AEH-W4A1 integration."""
+<<<<<<< HEAD
     conf = config.get(DOMAIN)
 
     hass.data[DOMAIN] = conf or {}
@@ -28,10 +37,13 @@ async def async_setup(hass, config):
             )
         )
 
+=======
+>>>>>>> First commit
     return True
 
 
 async def async_setup_entry(hass, entry):
+<<<<<<< HEAD
     """Set up a config entry for Hisense AEH-W4A1."""
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, CLIMA_DOMAIN)
@@ -43,3 +55,12 @@ async def async_setup_entry(hass, entry):
 async def async_unload_entry(hass, entry):
     """Unload a config entry."""
     return await hass.config_entries.async_forward_entry_unload(entry, CLIMA_DOMAIN)
+=======
+    """Set up a config entry for NEW_NAME."""
+    # TODO forward the entry for each platform that you want to set up.
+    # hass.async_create_task(
+    #     hass.config_entries.async_forward_entry_setup(entry, "media_player")
+    # )
+
+    return True
+>>>>>>> First commit
--- homeassistant/components/hisense_aehw4a1/config_flow.py
+++ homeassistant/components/hisense_aehw4a1/config_flow.py
@@ -1,6 +1,7 @@
 """Config flow for Hisense AEH-W4A1 integration."""
 import logging
 
+<<<<<<< HEAD
 from pyaehw4a1.aehw4a1 import AehW4a1
 
 from homeassistant import config_entries
@@ -21,3 +22,66 @@ async def _async_has_devices(hass):
 config_entry_flow.register_discovery_flow(
     DOMAIN, "Hisense AEH-W4A1", _async_has_devices, config_entries.CONN_CLASS_LOCAL_POLL
 )
+=======
+import voluptuous as vol
+
+from homeassistant import core, config_entries, exceptions
+
+from .const import DOMAIN  # pylint:disable=unused-import
+
+_LOGGER = logging.getLogger(__name__)
+
+# TODO adjust the data schema to the data that you need
+DATA_SCHEMA = vol.Schema({"host": str, "username": str, "password": str})
+
+
+async def validate_input(hass: core.HomeAssistant, data):
+    """Validate the user input allows us to connect.
+
+    Data has the keys from DATA_SCHEMA with values provided by the user.
+    """
+    # TODO validate the data can be used to set up a connection.
+    # If you cannot connect:
+    # throw CannotConnect
+    # If the authentication is wrong:
+    # InvalidAuth
+
+    # Return some info we want to store in the config entry.
+    return {"title": "Name of the device"}
+
+
+class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Hisense AEH-W4A1."""
+
+    VERSION = 1
+    # TODO pick one of the available connection classes in homeassistant/config_entries.py
+    CONNECTION_CLASS = config_entries.CONN_CLASS_UNKNOWN
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            try:
+                info = await validate_input(self.hass, user_input)
+
+                return self.async_create_entry(title=info["title"], data=user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
+
+
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(exceptions.HomeAssistantError):
+    """Error to indicate there is invalid auth."""
+>>>>>>> First commit
