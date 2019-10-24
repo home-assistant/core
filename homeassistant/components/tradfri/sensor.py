67cf7c26da2b0b7a0dffc1d397cd1396bb6da29a
Joakim Sørensen <joasoe@gmail.com>
2019-10-24 23:25:47 +0200

    Removes unwanted tradfri battery sensor (#28181)

--- homeassistant/components/tradfri/sensor.py
+++ homeassistant/components/tradfri/sensor.py
@@ -19,6 +19,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         if not dev.has_light_control
         and not dev.has_socket_control
         and not dev.has_blind_control
+        and not dev.has_signal_repeater_control
     )
     if devices:
         async_add_entities(TradfriSensor(device, api, gateway_id) for device in devices)
