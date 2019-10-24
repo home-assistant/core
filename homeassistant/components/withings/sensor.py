3ea72e6a08e34dd3ddaf466c9eff0d7543726766
Robert Van Gorkom <vangorra@users.noreply.github.com>
2019-10-24 09:41:04 -0700

    Use latest withings_api module (#27817)
    * Using latest winthings_api module.
    Drastically reduced complexity of tests.
    
    * Removing import source.
    
    * Fixing test requirements.
    
    * Using requests_mock instead of responses module.
    
    * Updating file formatting.
    
    * Removing unused method.
    
    * Adding support for new OAuth2 config flow.
    
    * Addressing PR feedback.
    Removing unecessary base_url from config, this is a potential breaking change.
    
    * Addressing PR feedback.

--- homeassistant/components/withings/sensor.py
+++ homeassistant/components/withings/sensor.py
@@ -1,10 +1,22 @@
 """Sensors flow for Withings."""
-import typing as types
+from typing import Callable, List, Union
+
+from withings_api.common import (
+    MeasureType,
+    GetSleepSummaryField,
+    MeasureGetMeasResponse,
+    SleepGetResponse,
+    SleepGetSummaryResponse,
+    get_measure_value,
+    MeasureGroupAttribs,
+    SleepState,
+)
 
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.util import slugify
+from homeassistant.helpers import config_entry_oauth2_flow
 
 from . import const
 from .common import _LOGGER, WithingsDataManager, get_data_manager
@@ -16,55 +28,20 @@ PARALLEL_UPDATES = 1
 
 
 async def async_setup_entry(
-    hass: HomeAssistantType,
+    hass: HomeAssistant,
     entry: ConfigEntry,
-    async_add_entities: types.Callable[[types.List[Entity], bool], None],
-):
+    async_add_entities: Callable[[List[Entity], bool], None],
+) -> None:
     """Set up the sensor config entry."""
-    data_manager = get_data_manager(hass, entry)
-    entities = create_sensor_entities(data_manager)
+    implementation = await config_entry_oauth2_flow.async_get_config_entry_implementation(
+        hass, entry
+    )
+
+    data_manager = get_data_manager(hass, entry, implementation)
+    user_id = entry.data["token"]["userid"]
+
+    entities = create_sensor_entities(data_manager, user_id)
     async_add_entities(entities, True)
-
-
-def get_measures():
-    """Get all the measures.
-
-    This function exists to be easily mockable so we can test
-    one measure at a time. This becomes necessary when integration
-    testing throttle functionality in the data manager.
-    """
-    return list(WITHINGS_MEASUREMENTS_MAP)
-
-
-def create_sensor_entities(data_manager: WithingsDataManager):
-    """Create sensor entities."""
-    entities = []
-
-    measures = get_measures()
-
-    for attribute in WITHINGS_ATTRIBUTES:
-        if attribute.measurement not in measures:
-            _LOGGER.debug(
-                "Skipping measurement %s as it is not in the"
-                "list of measurements to use",
-                attribute.measurement,
-            )
-            continue
-
-        _LOGGER.debug(
-            "Creating entity for measurement: %s, measure_type: %s,"
-            "friendly_name: %s, unit_of_measurement: %s",
-            attribute.measurement,
-            attribute.measure_type,
-            attribute.friendly_name,
-            attribute.unit_of_measurement,
-        )
-
-        entity = WithingsHealthSensor(data_manager, attribute)
-
-        entities.append(entity)
-
-    return entities
 
 
 class WithingsAttribute:
@@ -107,104 +84,104 @@ class WithingsSleepSummaryAttribute(WithingsAttribute):
 WITHINGS_ATTRIBUTES = [
     WithingsMeasureAttribute(
         const.MEAS_WEIGHT_KG,
-        const.MEASURE_TYPE_WEIGHT,
+        MeasureType.WEIGHT,
         "Weight",
         const.UOM_MASS_KG,
         "mdi:weight-kilogram",
     ),
     WithingsMeasureAttribute(
         const.MEAS_FAT_MASS_KG,
-        const.MEASURE_TYPE_FAT_MASS,
+        MeasureType.FAT_MASS_WEIGHT,
         "Fat Mass",
         const.UOM_MASS_KG,
         "mdi:weight-kilogram",
     ),
     WithingsMeasureAttribute(
         const.MEAS_FAT_FREE_MASS_KG,
-        const.MEASURE_TYPE_FAT_MASS_FREE,
+        MeasureType.FAT_FREE_MASS,
         "Fat Free Mass",
         const.UOM_MASS_KG,
         "mdi:weight-kilogram",
     ),
     WithingsMeasureAttribute(
         const.MEAS_MUSCLE_MASS_KG,
-        const.MEASURE_TYPE_MUSCLE_MASS,
+        MeasureType.MUSCLE_MASS,
         "Muscle Mass",
         const.UOM_MASS_KG,
         "mdi:weight-kilogram",
     ),
     WithingsMeasureAttribute(
         const.MEAS_BONE_MASS_KG,
-        const.MEASURE_TYPE_BONE_MASS,
+        MeasureType.BONE_MASS,
         "Bone Mass",
         const.UOM_MASS_KG,
         "mdi:weight-kilogram",
     ),
     WithingsMeasureAttribute(
         const.MEAS_HEIGHT_M,
-        const.MEASURE_TYPE_HEIGHT,
+        MeasureType.HEIGHT,
         "Height",
         const.UOM_LENGTH_M,
         "mdi:ruler",
     ),
     WithingsMeasureAttribute(
         const.MEAS_TEMP_C,
-        const.MEASURE_TYPE_TEMP,
+        MeasureType.TEMPERATURE,
         "Temperature",
         const.UOM_TEMP_C,
         "mdi:thermometer",
     ),
     WithingsMeasureAttribute(
         const.MEAS_BODY_TEMP_C,
-        const.MEASURE_TYPE_BODY_TEMP,
+        MeasureType.BODY_TEMPERATURE,
         "Body Temperature",
         const.UOM_TEMP_C,
         "mdi:thermometer",
     ),
     WithingsMeasureAttribute(
         const.MEAS_SKIN_TEMP_C,
-        const.MEASURE_TYPE_SKIN_TEMP,
+        MeasureType.SKIN_TEMPERATURE,
         "Skin Temperature",
         const.UOM_TEMP_C,
         "mdi:thermometer",
     ),
     WithingsMeasureAttribute(
         const.MEAS_FAT_RATIO_PCT,
-        const.MEASURE_TYPE_FAT_RATIO,
+        MeasureType.FAT_RATIO,
         "Fat Ratio",
         const.UOM_PERCENT,
         None,
     ),
     WithingsMeasureAttribute(
         const.MEAS_DIASTOLIC_MMHG,
-        const.MEASURE_TYPE_DIASTOLIC_BP,
+        MeasureType.DIASTOLIC_BLOOD_PRESSURE,
         "Diastolic Blood Pressure",
         const.UOM_MMHG,
         None,
     ),
     WithingsMeasureAttribute(
         const.MEAS_SYSTOLIC_MMGH,
-        const.MEASURE_TYPE_SYSTOLIC_BP,
+        MeasureType.SYSTOLIC_BLOOD_PRESSURE,
         "Systolic Blood Pressure",
         const.UOM_MMHG,
         None,
     ),
     WithingsMeasureAttribute(
         const.MEAS_HEART_PULSE_BPM,
-        const.MEASURE_TYPE_HEART_PULSE,
+        MeasureType.HEART_RATE,
         "Heart Pulse",
         const.UOM_BEATS_PER_MINUTE,
         "mdi:heart-pulse",
     ),
     WithingsMeasureAttribute(
-        const.MEAS_SPO2_PCT, const.MEASURE_TYPE_SPO2, "SP02", const.UOM_PERCENT, None
+        const.MEAS_SPO2_PCT, MeasureType.SP02, "SP02", const.UOM_PERCENT, None
     ),
     WithingsMeasureAttribute(
-        const.MEAS_HYDRATION, const.MEASURE_TYPE_HYDRATION, "Hydration", "", "mdi:water"
+        const.MEAS_HYDRATION, MeasureType.HYDRATION, "Hydration", "", "mdi:water"
     ),
     WithingsMeasureAttribute(
         const.MEAS_PWV,
-        const.MEASURE_TYPE_PWV,
+        MeasureType.PULSE_WAVE_VELOCITY,
         "Pulse Wave Velocity",
         const.UOM_METERS_PER_SECOND,
         None,
@@ -214,91 +191,91 @@ WITHINGS_ATTRIBUTES = [
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_WAKEUP_DURATION_SECONDS,
-        const.MEASURE_TYPE_SLEEP_WAKEUP_DURATION,
+        GetSleepSummaryField.WAKEUP_DURATION.value,
         "Wakeup time",
         const.UOM_SECONDS,
         "mdi:sleep-off",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_LIGHT_DURATION_SECONDS,
-        const.MEASURE_TYPE_SLEEP_LIGHT_DURATION,
+        GetSleepSummaryField.LIGHT_SLEEP_DURATION.value,
         "Light sleep",
         const.UOM_SECONDS,
         "mdi:sleep",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_DEEP_DURATION_SECONDS,
-        const.MEASURE_TYPE_SLEEP_DEEP_DURATION,
+        GetSleepSummaryField.DEEP_SLEEP_DURATION.value,
         "Deep sleep",
         const.UOM_SECONDS,
         "mdi:sleep",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_REM_DURATION_SECONDS,
-        const.MEASURE_TYPE_SLEEP_REM_DURATION,
+        GetSleepSummaryField.REM_SLEEP_DURATION.value,
         "REM sleep",
         const.UOM_SECONDS,
         "mdi:sleep",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_WAKEUP_COUNT,
-        const.MEASURE_TYPE_SLEEP_WAKUP_COUNT,
+        GetSleepSummaryField.WAKEUP_COUNT.value,
         "Wakeup count",
         const.UOM_FREQUENCY,
         "mdi:sleep-off",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_TOSLEEP_DURATION_SECONDS,
-        const.MEASURE_TYPE_SLEEP_TOSLEEP_DURATION,
+        GetSleepSummaryField.DURATION_TO_SLEEP.value,
         "Time to sleep",
         const.UOM_SECONDS,
         "mdi:sleep",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_TOWAKEUP_DURATION_SECONDS,
-        const.MEASURE_TYPE_SLEEP_TOWAKEUP_DURATION,
+        GetSleepSummaryField.DURATION_TO_WAKEUP.value,
         "Time to wakeup",
         const.UOM_SECONDS,
         "mdi:sleep-off",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_HEART_RATE_AVERAGE,
-        const.MEASURE_TYPE_SLEEP_HEART_RATE_AVERAGE,
+        GetSleepSummaryField.HR_AVERAGE.value,
         "Average heart rate",
         const.UOM_BEATS_PER_MINUTE,
         "mdi:heart-pulse",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_HEART_RATE_MIN,
-        const.MEASURE_TYPE_SLEEP_HEART_RATE_MIN,
+        GetSleepSummaryField.HR_MIN.value,
         "Minimum heart rate",
         const.UOM_BEATS_PER_MINUTE,
         "mdi:heart-pulse",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_HEART_RATE_MAX,
-        const.MEASURE_TYPE_SLEEP_HEART_RATE_MAX,
+        GetSleepSummaryField.HR_MAX.value,
         "Maximum heart rate",
         const.UOM_BEATS_PER_MINUTE,
         "mdi:heart-pulse",
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_RESPIRATORY_RATE_AVERAGE,
-        const.MEASURE_TYPE_SLEEP_RESPIRATORY_RATE_AVERAGE,
+        GetSleepSummaryField.RR_AVERAGE.value,
         "Average respiratory rate",
         const.UOM_BREATHS_PER_MINUTE,
         None,
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_RESPIRATORY_RATE_MIN,
-        const.MEASURE_TYPE_SLEEP_RESPIRATORY_RATE_MIN,
+        GetSleepSummaryField.RR_MIN.value,
         "Minimum respiratory rate",
         const.UOM_BREATHS_PER_MINUTE,
         None,
     ),
     WithingsSleepSummaryAttribute(
         const.MEAS_SLEEP_RESPIRATORY_RATE_MAX,
-        const.MEASURE_TYPE_SLEEP_RESPIRATORY_RATE_MAX,
+        GetSleepSummaryField.RR_MAX.value,
         "Maximum respiratory rate",
         const.UOM_BREATHS_PER_MINUTE,
         None,
@@ -312,7 +289,10 @@ class WithingsHealthSensor(Entity):
     """Implementation of a Withings sensor."""
 
     def __init__(
-        self, data_manager: WithingsDataManager, attribute: WithingsAttribute
+        self,
+        data_manager: WithingsDataManager,
+        attribute: WithingsAttribute,
+        user_id: str,
     ) -> None:
         """Initialize the Withings sensor."""
         self._data_manager = data_manager
@@ -320,7 +300,7 @@ class WithingsHealthSensor(Entity):
         self._state = None
 
         self._slug = self._data_manager.slug
-        self._user_id = self._data_manager.api.get_credentials().user_id
+        self._user_id = user_id
 
     @property
     def name(self) -> str:
@@ -335,7 +315,7 @@ class WithingsHealthSensor(Entity):
         )
 
     @property
-    def state(self):
+    def state(self) -> Union[str, int, float, None]:
         """Return the state of the sensor."""
         return self._state
 
@@ -350,7 +330,7 @@ class WithingsHealthSensor(Entity):
         return self._attribute.icon
 
     @property
-    def device_state_attributes(self):
+    def device_state_attributes(self) -> None:
         """Get withings attributes."""
         return self._attribute.__dict__
 
@@ -378,71 +358,45 @@ class WithingsHealthSensor(Entity):
             await self._data_manager.update_sleep_summary()
             await self.async_update_sleep_summary(self._data_manager.sleep_summary)
 
-    async def async_update_measure(self, data) -> None:
+    async def async_update_measure(self, data: MeasureGetMeasResponse) -> None:
         """Update the measures data."""
-        if data is None:
-            _LOGGER.error("Provided data is None. Setting state to %s", None)
-            self._state = None
-            return
-
         measure_type = self._attribute.measure_type
 
         _LOGGER.debug(
             "Finding the unambiguous measure group with measure_type: %s", measure_type
         )
-        measure_groups = [
-            g
-            for g in data
-            if (not g.is_ambiguous() and g.get_measure(measure_type) is not None)
-        ]
 
-        if not measure_groups:
-            _LOGGER.debug("No measure groups found, setting state to %s", None)
+        value = get_measure_value(data, measure_type, MeasureGroupAttribs.UNAMBIGUOUS)
+
+        if value is None:
+            _LOGGER.debug("Could not find a value, setting state to %s", None)
             self._state = None
             return
 
-        _LOGGER.debug(
-            "Sorting list of %s measure groups by date created (DESC)",
-            len(measure_groups),
-        )
-        measure_groups.sort(key=(lambda g: g.created), reverse=True)
+        self._state = round(value, 2)
 
-        self._state = round(measure_groups[0].get_measure(measure_type), 4)
-
-    async def async_update_sleep_state(self, data) -> None:
+    async def async_update_sleep_state(self, data: SleepGetResponse) -> None:
         """Update the sleep state data."""
-        if data is None:
-            _LOGGER.error("Provided data is None. Setting state to %s", None)
-            self._state = None
-            return
-
         if not data.series:
             _LOGGER.debug("No sleep data, setting state to %s", None)
             self._state = None
             return
 
-        series = sorted(data.series, key=lambda o: o.enddate, reverse=True)
+        serie = data.series[len(data.series) - 1]
+        state = None
+        if serie.state == SleepState.AWAKE:
+            state = const.STATE_AWAKE
+        elif serie.state == SleepState.LIGHT:
+            state = const.STATE_LIGHT
+        elif serie.state == SleepState.DEEP:
+            state = const.STATE_DEEP
+        elif serie.state == SleepState.REM:
+            state = const.STATE_REM
 
-        serie = series[0]
+        self._state = state
 
-        if serie.state == const.MEASURE_TYPE_SLEEP_STATE_AWAKE:
-            self._state = const.STATE_AWAKE
-        elif serie.state == const.MEASURE_TYPE_SLEEP_STATE_LIGHT:
-            self._state = const.STATE_LIGHT
-        elif serie.state == const.MEASURE_TYPE_SLEEP_STATE_DEEP:
-            self._state = const.STATE_DEEP
-        elif serie.state == const.MEASURE_TYPE_SLEEP_STATE_REM:
-            self._state = const.STATE_REM
-        else:
-            self._state = None
-
-    async def async_update_sleep_summary(self, data) -> None:
+    async def async_update_sleep_summary(self, data: SleepGetSummaryResponse) -> None:
         """Update the sleep summary data."""
-        if data is None:
-            _LOGGER.error("Provided data is None. Setting state to %s", None)
-            self._state = None
-            return
-
         if not data.series:
             _LOGGER.debug("Sleep data has no series, setting state to %s", None)
             self._state = None
@@ -454,7 +408,59 @@ class WithingsHealthSensor(Entity):
         _LOGGER.debug("Determining total value for: %s", measurement)
         total = 0
         for serie in data.series:
-            if hasattr(serie, measure_type):
-                total += getattr(serie, measure_type)
+            data = serie.data
+            value = 0
+            if measure_type == GetSleepSummaryField.REM_SLEEP_DURATION.value:
+                value = data.remsleepduration
+            elif measure_type == GetSleepSummaryField.WAKEUP_DURATION.value:
+                value = data.wakeupduration
+            elif measure_type == GetSleepSummaryField.LIGHT_SLEEP_DURATION.value:
+                value = data.lightsleepduration
+            elif measure_type == GetSleepSummaryField.DEEP_SLEEP_DURATION.value:
+                value = data.deepsleepduration
+            elif measure_type == GetSleepSummaryField.WAKEUP_COUNT.value:
+                value = data.wakeupcount
+            elif measure_type == GetSleepSummaryField.DURATION_TO_SLEEP.value:
+                value = data.durationtosleep
+            elif measure_type == GetSleepSummaryField.DURATION_TO_WAKEUP.value:
+                value = data.durationtowakeup
+            elif measure_type == GetSleepSummaryField.HR_AVERAGE.value:
+                value = data.hr_average
+            elif measure_type == GetSleepSummaryField.HR_MIN.value:
+                value = data.hr_min
+            elif measure_type == GetSleepSummaryField.HR_MAX.value:
+                value = data.hr_max
+            elif measure_type == GetSleepSummaryField.RR_AVERAGE.value:
+                value = data.rr_average
+            elif measure_type == GetSleepSummaryField.RR_MIN.value:
+                value = data.rr_min
+            elif measure_type == GetSleepSummaryField.RR_MAX.value:
+                value = data.rr_max
+
+            # Sometimes a None is provided for value, default to 0.
+            total += value or 0
 
         self._state = round(total, 4)
+
+
+def create_sensor_entities(
+    data_manager: WithingsDataManager, user_id: str
+) -> List[WithingsHealthSensor]:
+    """Create sensor entities."""
+    entities = []
+
+    for attribute in WITHINGS_ATTRIBUTES:
+        _LOGGER.debug(
+            "Creating entity for measurement: %s, measure_type: %s,"
+            "friendly_name: %s, unit_of_measurement: %s",
+            attribute.measurement,
+            attribute.measure_type,
+            attribute.friendly_name,
+            attribute.unit_of_measurement,
+        )
+
+        entity = WithingsHealthSensor(data_manager, attribute, user_id)
+
+        entities.append(entity)
+
+    return entities
