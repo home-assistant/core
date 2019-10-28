059259253fcad851771734e87cfa536b094c273b
Tsvi Mostovicz <ttmost@gmail.com>
2019-10-28 21:28:22 +0200

    Remove unreachable exception

--- homeassistant/components/jewish_calendar/config_flow.py
+++ homeassistant/components/jewish_calendar/config_flow.py
@@ -1,6 +1,4 @@
 """Config flow for Jewish calendar integration."""
-import logging
-
 from homeassistant import config_entries
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
 
@@ -14,8 +12,6 @@ from .const import (
     DATA_SCHEMA,
 )
 
-_LOGGER = logging.getLogger(__name__)
-
 
 class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Jewish calendar."""
@@ -25,32 +21,23 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        errors = {}
         if user_input is not None:
-            try:
-                return self.async_create_entry(
-                    title=user_input.get(CONF_NAME, DEFAULT_NAME),
-                    data={
-                        CONF_DIASPORA: user_input[CONF_DIASPORA],
-                        CONF_LANGUAGE: user_input[CONF_LANGUAGE],
-                        CONF_CANDLE_LIGHT_MINUTES: user_input[
-                            CONF_CANDLE_LIGHT_MINUTES
-                        ],
-                        CONF_HAVDALAH_OFFSET_MINUTES: user_input[
-                            CONF_HAVDALAH_OFFSET_MINUTES
-                        ],
-                        CONF_LATITUDE: user_input.get(
-                            CONF_LATITUDE, self.hass.config.latitude
-                        ),
-                        CONF_LONGITUDE: user_input.get(
-                            CONF_LONGITUDE, self.hass.config.longitude
-                        ),
-                    },
-                )
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+            return self.async_create_entry(
+                title=user_input.get(CONF_NAME, DEFAULT_NAME),
+                data={
+                    CONF_DIASPORA: user_input[CONF_DIASPORA],
+                    CONF_LANGUAGE: user_input[CONF_LANGUAGE],
+                    CONF_CANDLE_LIGHT_MINUTES: user_input[CONF_CANDLE_LIGHT_MINUTES],
+                    CONF_HAVDALAH_OFFSET_MINUTES: user_input[
+                        CONF_HAVDALAH_OFFSET_MINUTES
+                    ],
+                    CONF_LATITUDE: user_input.get(
+                        CONF_LATITUDE, self.hass.config.latitude
+                    ),
+                    CONF_LONGITUDE: user_input.get(
+                        CONF_LONGITUDE, self.hass.config.longitude
+                    ),
+                },
+            )
 
-        return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
-        )
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors={})
