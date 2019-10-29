50c2edd90092f589d7651b436edd7575060a82a2
ochlocracy <5885236+ochlocracy@users.noreply.github.com>
2019-10-29 12:31:01 -0400

    Split out media_player SeekController tests and added error test.

--- tests/components/alexa/test_smart_home.py
+++ tests/components/alexa/test_smart_home.py
@@ -712,7 +712,6 @@ async def test_media_player(hass):
             | SUPPORT_PLAY
             | SUPPORT_PLAY_MEDIA
             | SUPPORT_PREVIOUS_TRACK
-            | SUPPORT_SEEK
             | SUPPORT_SELECT_SOURCE
             | SUPPORT_STOP
             | SUPPORT_TURN_OFF
@@ -720,7 +719,6 @@ async def test_media_player(hass):
             | SUPPORT_VOLUME_MUTE
             | SUPPORT_VOLUME_SET,
             "volume_level": 0.75,
-            "media_position": 300,
         },
     )
     appliance = await discovery_test(device, hass)
@@ -737,7 +735,6 @@ async def test_media_player(hass):
         "Alexa.PlaybackController",
         "Alexa.PlaybackStateReporter",
         "Alexa.PowerController",
-        "Alexa.SeekController",
         "Alexa.Speaker",
         "Alexa.StepSpeaker",
     )
@@ -934,54 +931,6 @@ async def test_media_player(hass):
         payload={"channelCount": -1},
     )
 
-    # Test seek forward 30 seconds.
-    call, msg = await assert_request_calls_service(
-        "Alexa.SeekController",
-        "AdjustSeekPosition",
-        "media_player#test",
-        "media_player.media_seek",
-        hass,
-        response_type="StateReport",
-        payload={"deltaPositionMilliseconds": 30000},
-    )
-    assert call.data["seek_position"] == 330
-
-    assert "properties" in msg["event"]["payload"]
-    properties = msg["event"]["payload"]["properties"]
-    assert {"name": "positionMilliseconds", "value": 330000} in properties
-
-    # Test seek reverse 30 seconds.
-    call, msg = await assert_request_calls_service(
-        "Alexa.SeekController",
-        "AdjustSeekPosition",
-        "media_player#test",
-        "media_player.media_seek",
-        hass,
-        response_type="StateReport",
-        payload={"deltaPositionMilliseconds": -30000},
-    )
-    assert call.data["seek_position"] == 270
-
-    assert "properties" in msg["event"]["payload"]
-    properties = msg["event"]["payload"]["properties"]
-    assert {"name": "positionMilliseconds", "value": 270000} in properties
-
-    # Test seek more than current position result = 0.
-    call, msg = await assert_request_calls_service(
-        "Alexa.SeekController",
-        "AdjustSeekPosition",
-        "media_player#test",
-        "media_player.media_seek",
-        hass,
-        response_type="StateReport",
-        payload={"deltaPositionMilliseconds": -500000},
-    )
-    assert call.data["seek_position"] == 0
-
-    assert "properties" in msg["event"]["payload"]
-    properties = msg["event"]["payload"]["properties"]
-    assert {"name": "positionMilliseconds", "value": 0} in properties
-
 
 async def test_media_player_power(hass):
     """Test media player discovery with mapped on/off."""
@@ -1047,6 +996,120 @@ async def test_media_player_speaker(hass):
     assert appliance["endpointId"] == "media_player#test"
     assert appliance["displayCategories"][0] == "SPEAKER"
     assert appliance["friendlyName"] == "Test media player"
+
+
+async def test_media_player_seek(hass):
+    """Test media player seek capability."""
+    device = (
+        "media_player.test_seek",
+        "playing",
+        {
+            "friendly_name": "Test media player seek",
+            "supported_features": SUPPORT_SEEK,
+            "media_position": 300,  # 5min
+            "media_duration": 600,  # 10min
+        },
+    )
+    appliance = await discovery_test(device, hass)
+
+    assert appliance["endpointId"] == "media_player#test_seek"
+    assert appliance["displayCategories"][0] == "TV"
+    assert appliance["friendlyName"] == "Test media player seek"
+
+    assert_endpoint_capabilities(
+        appliance,
+        "Alexa.EndpointHealth",
+        "Alexa.PowerController",
+        "Alexa.SeekController",
+    )
+
+    # Test seek forward 30 seconds.
+    call, msg = await assert_request_calls_service(
+        "Alexa.SeekController",
+        "AdjustSeekPosition",
+        "media_player#test_seek",
+        "media_player.media_seek",
+        hass,
+        response_type="StateReport",
+        payload={"deltaPositionMilliseconds": 30000},
+    )
+    assert call.data["seek_position"] == 330
+    assert "properties" in msg["event"]["payload"]
+    properties = msg["event"]["payload"]["properties"]
+    assert {"name": "positionMilliseconds", "value": 330000} in properties
+
+    # Test seek reverse 30 seconds.
+    call, msg = await assert_request_calls_service(
+        "Alexa.SeekController",
+        "AdjustSeekPosition",
+        "media_player#test_seek",
+        "media_player.media_seek",
+        hass,
+        response_type="StateReport",
+        payload={"deltaPositionMilliseconds": -30000},
+    )
+    assert call.data["seek_position"] == 270
+    assert "properties" in msg["event"]["payload"]
+    properties = msg["event"]["payload"]["properties"]
+    assert {"name": "positionMilliseconds", "value": 270000} in properties
+
+    # Test seek backwards more than current position (5 min.) result = 0.
+    call, msg = await assert_request_calls_service(
+        "Alexa.SeekController",
+        "AdjustSeekPosition",
+        "media_player#test_seek",
+        "media_player.media_seek",
+        hass,
+        response_type="StateReport",
+        payload={"deltaPositionMilliseconds": -500000},
+    )
+    assert call.data["seek_position"] == 0
+    assert "properties" in msg["event"]["payload"]
+    properties = msg["event"]["payload"]["properties"]
+    assert {"name": "positionMilliseconds", "value": 0} in properties
+
+    # Test seek forward more than current duration (10 min.) result = 600 sec.
+    call, msg = await assert_request_calls_service(
+        "Alexa.SeekController",
+        "AdjustSeekPosition",
+        "media_player#test_seek",
+        "media_player.media_seek",
+        hass,
+        response_type="StateReport",
+        payload={"deltaPositionMilliseconds": 800000},
+    )
+    assert call.data["seek_position"] == 600
+    assert "properties" in msg["event"]["payload"]
+    properties = msg["event"]["payload"]["properties"]
+    assert {"name": "positionMilliseconds", "value": 600000} in properties
+
+
+async def test_media_player_seek_error(hass):
+    """Test media player seek capability for media_position Error."""
+    device = (
+        "media_player.test_seek",
+        "playing",
+        {"friendly_name": "Test media player seek", "supported_features": SUPPORT_SEEK},
+    )
+    await discovery_test(device, hass)
+
+    # Test for media_position error.
+    with pytest.raises(AssertionError):
+        call, msg = await assert_request_calls_service(
+            "Alexa.SeekController",
+            "AdjustSeekPosition",
+            "media_player#test_seek",
+            "media_player.media_seek",
+            hass,
+            response_type="StateReport",
+            payload={"deltaPositionMilliseconds": 30000},
+        )
+
+        assert "event" in msg
+        msg = msg["event"]
+        assert msg["header"]["name"] == "ErrorResponse"
+        assert msg["header"]["namespace"] == "Alexa.Video"
+        assert msg["payload"]["type"] == "ACTION_NOT_PERMITTED_FOR_CONTENT"
 
 
 async def test_alert(hass):
