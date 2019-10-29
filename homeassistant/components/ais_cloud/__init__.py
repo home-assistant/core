030746768d42af570ea67770869b9751218108d4
Sebastian Grzyś <sgrzys@gmail.com>
2019-10-29 12:48:09 +0100

    Don't say voice name on startup

--- homeassistant/components/ais_cloud/__init__.py
+++ homeassistant/components/ais_cloud/__init__.py
@@ -226,7 +226,8 @@ def async_setup(hass, config):
             else:
                 ais_global.GLOBAL_TTS_VOICE = "pl-pl-x-oda-local"
             # publish to frame
-            hass.services.call("ais_ai_service", "say_it", {"text": new_voice})
+            if ais_global.G_AIS_START_IS_DONE: 
+                hass.services.call("ais_ai_service", "say_it", {"text": new_voice})
             hass.services.call(
                 "ais_ai_service",
                 "publish_command_to_frame",
