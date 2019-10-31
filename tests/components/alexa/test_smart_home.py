8fb6e631aff65c70653f86686869dd0bf95859ba
ochlocracy <5885236+ochlocracy@users.noreply.github.com>
2019-10-31 08:47:23 -0400

    Updated test for recently added SeekController.

--- tests/components/alexa/test_smart_home.py
+++ tests/components/alexa/test_smart_home.py
@@ -1035,6 +1035,7 @@ async def test_media_player_seek(hass):
 
     assert_endpoint_capabilities(
         appliance,
+        "Alexa",
         "Alexa.EndpointHealth",
         "Alexa.PowerController",
         "Alexa.SeekController",
