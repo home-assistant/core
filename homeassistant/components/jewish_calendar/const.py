2166ae99a280256893045f8d5efadac606f5609c
Tsvi Mostovicz <ttmost@gmail.com>
2019-10-31 03:30:31 +0200

    Make coordinates optional
    When set to Inclusive, the UI won't allow you to skip them.
    Also reorder the settings logically

--- homeassistant/components/jewish_calendar/const.py
+++ homeassistant/components/jewish_calendar/const.py
@@ -18,10 +18,10 @@ DOMAIN = "jewish_calendar"
 DATA_SCHEMA = {
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
     vol.Optional(CONF_DIASPORA, default=False): bool,
-    vol.Inclusive(CONF_LATITUDE, "coordinates"): cv.latitude,
-    vol.Inclusive(CONF_LONGITUDE, "coordinates"): cv.longitude,
     vol.Optional(CONF_LANGUAGE, default="english"): vol.In(["hebrew", "english"]),
     vol.Optional(CONF_CANDLE_LIGHT_MINUTES, default=CANDLE_LIGHT_DEFAULT): int,
     # Default of 0 means use 8.5 degrees / 'three_stars' time.
     vol.Optional(CONF_HAVDALAH_OFFSET_MINUTES, default=0): int,
+    vol.Optional(CONF_LATITUDE): cv.latitude,
+    vol.Optional(CONF_LONGITUDE): cv.longitude,
 }
