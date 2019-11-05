0eca44e96985e7f9b65d70b68e20ca086cf20b5b
Franck Nijhof <frenck@addons.community>
2019-11-05 19:57:34 +0100

    Restructure config flow code

--- homeassistant/components/wled/config_flow.py
+++ homeassistant/components/wled/config_flow.py
@@ -5,7 +5,7 @@ from typing import Any, Dict, Optional
 import voluptuous as vol
 from wled import WLED, WLEDConnectionError
 
-from homeassistant.components.wled.const import DOMAIN
+from homeassistant.components.wled.const import DOMAIN  # pylint: disable=W0611
 from homeassistant.config_entries import (
     CONN_CLASS_LOCAL_POLL,
     SOURCE_ZEROCONF,
@@ -24,34 +24,52 @@ class WLEDFlowHandler(ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = CONN_CLASS_LOCAL_POLL
 
-    def _show_setup_form(self, errors: Optional[Dict] = None) -> Dict[str, Any]:
-        """Show the setup form to the user."""
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({vol.Required(CONF_HOST): str}),
-            errors=errors or {},
-        )
-
-    def _show_confirm_dialog(self, errors: Optional[Dict] = None) -> Dict[str, Any]:
-        """Show the setup form to the user."""
-        # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
-        name = self.context.get(CONF_NAME)
-        return self.async_show_form(
-            step_id="zeroconf_confirm",
-            description_placeholders={"name": name},
-            errors=errors or {},
-        )
-
-    async def _handle_config_flow(
+    async def async_step_user(
         self, user_input: Optional[ConfigType] = None
     ) -> Dict[str, Any]:
+        """Handle a flow initiated by the user."""
+        return await self._handle_config_flow(user_input)
+
+    async def async_step_zeroconf(
+        self, user_input: Optional[ConfigType] = None
+    ) -> Dict[str, Any]:
+        """Handle zeroconf discovery."""
+        if user_input is None:
+            return self.async_abort(reason="connection_error")
+
+        # Hostname is format: wled-livingroom.local.
+        host = user_input["hostname"].rstrip(".")
+        name, _ = host.rsplit(".")
+
+        # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
+        self.context.update(
+            {CONF_HOST: host, CONF_NAME: name, "title_placeholders": {"name": name}}
+        )
+
+        # Prepare configuration flow
+        return await self._handle_config_flow(user_input, True)
+
+    async def async_step_zeroconf_confirm(
+        self, user_input: ConfigType = None
+    ) -> Dict[str, Any]:
+        """Handle a flow initiated by zeroconf."""
+        return await self._handle_config_flow(user_input)
+
+    async def _handle_config_flow(
+        self, user_input: Optional[ConfigType] = None, prepare: bool = False
+    ) -> Dict[str, Any]:
+        """Config flow handler for WLED."""
         # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
         source = self.context.get("source")
 
-        if user_input is None:
+        # Request user input, unless we are preparing discovery flow
+        if user_input is None and not prepare:
             if source == SOURCE_ZEROCONF:
                 return self._show_confirm_dialog()
             return self._show_setup_form()
+
+        if user_input is None:
+            user_input = {}
 
         if source == SOURCE_ZEROCONF:
             # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
@@ -81,50 +99,27 @@ class WLEDFlowHandler(ConfigFlow, domain=DOMAIN):
             # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
             title = self.context.get(CONF_NAME)
 
+        if prepare:
+            return await self.async_step_zeroconf_confirm()
+
         return self.async_create_entry(
             title=title, data={CONF_HOST: user_input[CONF_HOST], CONF_MAC: mac_address}
         )
 
-    async def async_step_user(
-        self, user_input: Optional[ConfigType] = None
-    ) -> Dict[str, Any]:
-        """Handle a flow initiated by the user."""
-        return await self._handle_config_flow(user_input)
-
-    async def async_step_zeroconf_confirm(
-        self, user_input: ConfigType = None
-    ) -> Dict[str, Any]:
-        """Handle a flow initiated by zeroconf."""
-        return await self._handle_config_flow(user_input)
-
-    async def async_step_zeroconf(
-        self, user_input: Optional[ConfigType] = None
-    ) -> Dict[str, Any]:
-        """Handle zeroconf discovery."""
-        if user_input is None:
-            return self.async_abort(reason="connection_error")
-
-        # Hostname is format: wled-livingroom.local.
-        host = user_input["hostname"].rstrip(".")
-        name, _ = host.rsplit(".")
-
-        # Set up connection to WLED to find its unique identifier
-        session = async_get_clientsession(self.hass)
-        wled = WLED(host, loop=self.hass.loop, session=session)
-        try:
-            device = await wled.update()
-        except WLEDConnectionError:
-            return self.async_abort(reason="connection_error")
-
-        # Check if already configured
-        mac_address = device.info.mac_address
-        for entry in self._async_current_entries():
-            if entry.data[CONF_MAC] == mac_address:
-                return self.async_abort(reason="already_configured")
-
-        # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
-        self.context.update(
-            {CONF_HOST: host, CONF_NAME: name, "title_placeholders": {"name": name}}
+    def _show_setup_form(self, errors: Optional[Dict] = None) -> Dict[str, Any]:
+        """Show the setup form to the user."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({vol.Required(CONF_HOST): str}),
+            errors=errors or {},
         )
 
-        return await self.async_step_zeroconf_confirm()
+    def _show_confirm_dialog(self, errors: Optional[Dict] = None) -> Dict[str, Any]:
+        """Show the confirm dialog to the user."""
+        # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
+        name = self.context.get(CONF_NAME)
+        return self.async_show_form(
+            step_id="zeroconf_confirm",
+            description_placeholders={"name": name},
+            errors=errors or {},
+        )
