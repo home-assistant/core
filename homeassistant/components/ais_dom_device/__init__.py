31ff44b3525c5c9ad6604359831f2fb5d9de50df
tomasz75 <tomek@sviete.pl>
2019-11-05 17:03:01 +0100

    RF

--- homeassistant/components/ais_dom_device/__init__.py
+++ homeassistant/components/ais_dom_device/__init__.py
@@ -7,11 +7,29 @@ https://sviete.github.io/AIS-docs
 import logging
 import asyncio
 from .config_flow import configured_service
-
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
 
 @asyncio.coroutine
 def async_setup(hass, config):
     """Set up the Ais Dom devices platform."""
     _LOGGER.info("async_setup Ais Dom devices platform.")
+
+    # register services
+    def convert_rf_code_b1_to_b0(call):
+        if "code" not in call.data:
+            _LOGGER.error("No code to convert")
+            return
+        _convert_rf_code_b1_to_b0(call.data["code"])
+
+    hass.services.async_register(
+        DOMAIN, "convert_rf_code_b1_to_b0", convert_rf_code_b1_to_b0
+    )
+
+    return True
+
+
+def _convert_rf_code_b1_to_b0(code):
+    return "123" + str(code)
