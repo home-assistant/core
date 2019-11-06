9e5b7b038d12aa258ec4611245a2c79fe5988220
tomasz75 <tomek@sviete.pl>
2019-11-06 19:09:06 +0100

    beta 0.101.3b0

--- homeassistant/components/ais_dom_device/__init__.py
+++ homeassistant/components/ais_dom_device/__init__.py
@@ -8,6 +8,7 @@ import logging
 import asyncio
 from .config_flow import configured_service
 from .const import DOMAIN
+from homeassistant.helpers import device_registry as dr
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,12 +25,33 @@ def async_setup(hass, config):
             return
         _convert_rf_code_b1_to_b0(call.data["code"])
 
+    def add_new_rf433_switch(call):
+        if "name" not in call.data:
+            return
+        if "codes" not in call.data:
+            return
+        _add_new_rf433_switch(hass, call.data["name"], call.data["codes"])
+
     hass.services.async_register(
         DOMAIN, "convert_rf_code_b1_to_b0", convert_rf_code_b1_to_b0
     )
+    hass.services.async_register(DOMAIN, "add_new_rf433_switch", add_new_rf433_switch)
 
     return True
 
 
 def _convert_rf_code_b1_to_b0(code):
-    return "123" + str(code)
+    _LOGGER.info("TODO 123" + str(code))
+
+
+def _add_new_rf433_switch(hass, name, codes):
+    # 1. select the best code and convert it to B0
+
+    # 2. save this code and his mane in json
+
+    # 3. execute the discovery
+    yield from hass.services.async_call(
+        "mqtt", "publish", {"topic": "dom/cmnd/SetOption19", "payload": 1}
+    )
+
+    pass
