d67ddc9112d5152c7b09da492fece7d38de92946
Aaron Bach <bachya1208@gmail.com>
2019-11-09 15:36:16 -0700

    Final pieces for PR

--- homeassistant/components/simplisafe/lock.py
+++ homeassistant/components/simplisafe/lock.py
@@ -1,12 +1,21 @@
 """Support for SimpliSafe locks."""
 import logging
 
+from simplipy.lock import LockStates
+
 from homeassistant.components.lock import LockDevice
+from homeassistant.const import STATE_LOCKED, STATE_UNLOCKED
 
 from . import SimpliSafeEntity
 from .const import DATA_CLIENT, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
+ATTR_LOCK_LOW_BATTERY = "lock_low_battery"
+ATTR_JAMMED = "jammed"
+ATTR_PIN_PAD_LOW_BATTERY = "pin_pad_low_battery"
+
+STATE_MAP = {LockStates.locked: STATE_LOCKED, LockStates.unlocked: STATE_UNLOCKED}
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -29,6 +38,29 @@ class SimpliSafeLock(SimpliSafeEntity, LockDevice):
         super().__init__(system, lock.name, serial=lock.serial)
         self._lock = lock
 
+    @property
+    def is_locked(self):
+        """Return true if the lock is locked."""
+        return STATE_MAP[self._lock.state] == STATE_LOCKED
+
+    async def async_lock(self, **kwargs):
+        """Lock the lock."""
+        await self._lock.lock()
+
+    async def async_unlock(self, **kwargs):
+        """Unlock the lock."""
+        await self._lock.unlock()
+
     async def async_update(self):
         """Update lock status."""
-        self._attrs.update({})
+        if self._lock.disabled:
+            self._online = False
+            return
+
+        self._attrs.update(
+            {
+                ATTR_LOCK_LOW_BATTERY: self._lock.lock_low_battery,
+                ATTR_JAMMED: self._lock.state == LockStates.jammed,
+                ATTR_PIN_PAD_LOW_BATTERY: self._lock.pin_pad_low_battery,
+            }
+        )
