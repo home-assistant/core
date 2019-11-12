a2eadedcfc33fa968d61f004d121231722a58084
Andrew Bobulsky <andrew.bobulsky@buyerquest.com>
2019-11-12 14:55:12 -0500

    Monkeypatch imghdr bug
    Imghdr has an unresolved bug from 7 years ago where it cannot properly 
    identify all possible JPEG images. This code extends the imghdr tests to 
    fix this problem.

--- homeassistant/components/smtp/notify.py
+++ homeassistant/components/smtp/notify.py
@@ -65,6 +65,33 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     }
 )
 
+# Monkeypatch bug in imagehdr
+# https://stackoverflow.com/a/57693121/7969681 for details
+from imghdr import tests
+
+def test_jpeg1(h, f):
+    """JPEG data in JFIF format"""
+    if b'JFIF' in h[:23]:
+        return 'jpeg'
+
+
+JPEG_MARK = b'\xff\xd8\xff\xdb\x00C\x00\x08\x06\x06' \
+            b'\x07\x06\x05\x08\x07\x07\x07\t\t\x08\n\x0c\x14\r\x0c\x0b\x0b\x0c\x19\x12\x13\x0f'
+
+def test_jpeg2(h, f):
+    """JPEG with small header"""
+    if len(h) >= 32 and 67 == h[5] and h[:32] == JPEG_MARK:
+        return 'jpeg'
+
+
+def test_jpeg3(h, f):
+    """JPEG data in JFIF or Exif format"""
+    if h[6:10] in (b'JFIF', b'Exif') or h[:2] == b'\xff\xd8':
+        return 'jpeg'
+
+tests.append(test_jpeg1)
+tests.append(test_jpeg2)
+tests.append(test_jpeg3)
 
 def get_service(hass, config, discovery_info=None):
     """Get the mail notification service."""
