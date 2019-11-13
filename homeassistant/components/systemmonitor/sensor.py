b4cec23adde3d7f90f4670f3bcae1176d2949fac
Fabian Affolter <mail@fabian-affolter.ch>
2019-11-13 08:43:14 +0100

    Upgrade psutil to 5.6.5 (#28717)

--- homeassistant/components/systemmonitor/sensor.py
+++ homeassistant/components/systemmonitor/sensor.py
@@ -7,11 +7,10 @@ import psutil
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_RESOURCES, STATE_OFF, STATE_ON, CONF_TYPE
-from homeassistant.helpers.entity import Entity
+from homeassistant.const import CONF_RESOURCES, CONF_TYPE, STATE_OFF, STATE_ON
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
 import homeassistant.util.dt as dt_util
-
 
 # mypy: allow-untyped-defs, no-check-untyped-defs
 
