15e6278a2e40b839142a702d40751c1fd651ecad
Bram Kragten <mail@bramkragten.nl>
2019-11-13 16:37:31 +0100

    Add config entry and device support to Demo (#28702)
    * Add config entry and device support to Demo
    
    * Some more devices
    
    * Fix tests using demo
    
    * Review comments
    
    * Update config_flow.py
    
    * Revert
    
    * Disable pylint

--- homeassistant/components/demo/alarm_control_panel.py
+++ homeassistant/components/demo/alarm_control_panel.py
@@ -57,3 +57,8 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
             )
         ]
     )
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Demo config entry."""
+    await async_setup_platform(hass, {}, async_add_entities)
