0e1478e7a8641d0e5fea3bdfab106099fc3a7c80
tomasz75 <tomek@sviete.pl>
2019-11-18 16:53:22 +0100

    fix in scripts

--- homeassistant/components/ais_updater/scripts/release_script.sh
+++ homeassistant/components/ais_updater/scripts/release_script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 apt update 
 apt upgrade -y 
-echo "19.10.18" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
+apt install pkg-config -y 
+echo "19.11.06" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
