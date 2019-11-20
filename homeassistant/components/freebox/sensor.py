faf0b046594d0707c97c11887e83d91a7c4e2a68
SNoof85 <snoof85@gmail.com>
2019-11-20 21:37:26 +0100

    bump API version and add sensors

--- homeassistant/components/freebox/sensor.py
+++ homeassistant/components/freebox/sensor.py
@@ -11,10 +11,51 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the sensors."""
     fbx = hass.data[DATA_FREEBOX]
-    async_add_entities([FbxRXSensor(fbx), FbxTXSensor(fbx)], True)
+    sys_config = await fbx.system.get_config()
+    fbx_sensors = []
+    for temp_sensor_id in sys_config["sensors"]:
+        fbx_sensors.append(FbxTempSensor(fbx, temp_sensor_id["id"]))
+
+    fbx_sensors.extend([FbxRXSensor(fbx), FbxTXSensor(fbx)])
+    async_add_entities(fbx_sensors, True)
 
 
-class FbxSensor(Entity):
+class FbxTempSensor(Entity):
+    """Representation of a temperature sensor."""
+
+    def __init__(self, fbx, sensor_id):
+        """Initialize the sensor."""
+        self._fbx = fbx
+        self._datas = None
+        self._state = None
+        self._sensor_id = sensor_id
+        self._unit_of_measurement = "°C"
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of the sensor."""
+        return self._unit_of_measurement
+
+    async def async_update(self):
+        """Update freebox temperature sensors."""
+        self._datas = await self._fbx.system.get_config()
+        for temp_sensor in self._datas["sensors"]:
+            if temp_sensor["id"] == self._sensor_id:
+                self._name = "freebox_" + str(temp_sensor["name"])
+                self._state = temp_sensor["value"]
+
+
+class FbxConnSensor(Entity):
     """Representation of a freebox sensor."""
 
     _name = "generic"
@@ -40,7 +81,7 @@ class FbxSensor(Entity):
         self._datas = await self._fbx.connection.get_status()
 
 
-class FbxRXSensor(FbxSensor):
+class FbxRXSensor(FbxConnSensor):
     """Update the Freebox RxSensor."""
 
     _name = "Freebox download speed"
@@ -58,7 +99,7 @@ class FbxRXSensor(FbxSensor):
             self._state = round(self._datas["rate_down"] / 1000, 2)
 
 
-class FbxTXSensor(FbxSensor):
+class FbxTXSensor(FbxConnSensor):
     """Update the Freebox TxSensor."""
 
     _name = "Freebox upload speed"
