ab90412c6443f51638655f5e2b184449b86c4a8a
E.A. van Valkenburg <edvan@microsoft.com>
2019-11-21 14:57:42 +0100

    rebuilt of AEH integration

--- homeassistant/components/azure_event_hub/__init__.py
+++ homeassistant/components/azure_event_hub/__init__.py
@@ -5,7 +5,7 @@ import queue
 import threading
 import time
 from typing import Any, Dict
-
+import asyncio
 import voluptuous as vol
 
 from azure.eventhub import (
@@ -13,7 +13,6 @@ from azure.eventhub import (
     EventDataBatch,
     EventHubProducerClient,
     EventHubSharedKeyCredential,
-    EventHubSASTokenCredential,
     EventHubError,
 )
 
@@ -22,33 +21,30 @@ from homeassistant.const import (
     EVENT_STATE_CHANGED,
     STATE_UNAVAILABLE,
     STATE_UNKNOWN,
+    MATCH_ALL,
 )
-from homeassistant.core import Event, HomeAssistant
+
+from homeassistant.core import Event, HomeAssistant, callback
+from homeassistant.config_entries import ConfigEntry
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entityfilter import FILTER_SCHEMA
 from homeassistant.helpers.json import JSONEncoder
 
+from .const import (
+    DOMAIN,
+    CONF_EVENT_HUB_CON_STRING,
+    CONF_EVENT_HUB_INSTANCE_NAME,
+    CONF_EVENT_HUB_NAMESPACE,
+    CONF_EVENT_HUB_SAS_KEY,
+    CONF_EVENT_HUB_SAS_POLICY,
+    CONF_FILTER,
+)
+
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "azure_event_hub"
-
-CONF_EVENT_HUB_NAMESPACE = "event_hub_namespace"
-CONF_EVENT_HUB_INSTANCE_NAME = "event_hub_instance_name"
-CONF_EVENT_HUB_SAS_POLICY = "event_hub_sas_policy"
-CONF_EVENT_HUB_SAS_KEY = "event_hub_sas_key"
-CONF_EVENT_HUB_CON_STRING = "event_hub_connection_string"
-CONF_IOT_HUB_CON_STRING = "iot_hub_connection_string"
-CONF_FILTER = "filter"
-
-TIMEOUT = 5
-RETRY_DELAY = 20
 QUEUE_BACKLOG_SECONDS = 30
-RETRY_INTERVAL = 60  # seconds
-KEEP_ALIVE_INTERVAL = 300
 PAUSE_WHEN_EMPTY = 5
-MAX_TRIES = 3
 BATCH_TIMEOUT = 1
-BATCH_BUFFER_SIZE = 100
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -74,13 +70,26 @@ CONFIG_SCHEMA = vol.Schema(
 )
 
 
-def setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
+async def async_setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
     """Activate Azure EH component."""
-    config = yaml_config[DOMAIN]
+    _LOGGER.info("Setting up through setup")
+    if DOMAIN not in yaml_config:
+        return True
+    return await _setup(hass, yaml_config[DOMAIN])
 
-    entities_filter = config[CONF_FILTER]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Azure Event Hub from a config entry."""
+    _LOGGER.info("Setting up through config flow")
+    return await _setup(hass, entry.data)
+
+
+async def _setup(hass: HomeAssistant, config: Dict[str, Any]):
+    """Set up Azure Event Hub from a config entry."""
+    entities_filter = config.get(CONF_FILTER, {})
     if CONF_EVENT_HUB_CON_STRING in config:
         client_args = {"conn_str": config[CONF_EVENT_HUB_CON_STRING]}
+        conn_str_client = True
     else:
         client_args = {
             "host": f"{config[CONF_EVENT_HUB_NAMESPACE]}.servicebus.windows.net",
@@ -90,8 +99,8 @@ def setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
             ),
             "event_hub_path": config[CONF_EVENT_HUB_INSTANCE_NAME],
         }
+        conn_str_client = False
 
-    # encoder = JSONEncoder()
     _LOGGER.debug("    Client created.")
 
     def event_to_event_data(event: Event):
@@ -103,29 +112,34 @@ def setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
             or not entities_filter(state.entity_id)
         ):
             return
-        event_data = EventData(
-            json.dumps(obj=state, cls=JSONEncoder).encode("utf-8")
-            # json.dumps(obj=state.as_dict(), default=encoder.default).encode("utf-8")
-        )
+        event_data = EventData(json.dumps(obj=state, cls=JSONEncoder).encode("utf-8"))
         return event_data
 
-    instance = hass.data[DOMAIN] = AEHThread(hass, client_args, event_to_event_data)
+    instance = hass.data[DOMAIN] = AEHThread(
+        hass, client_args, event_to_event_data, conn_str_client
+    )
     _LOGGER.debug("    Instance created.")
+    instance.async_initialize()
     instance.start()
     _LOGGER.info("    Instance started.")
 
-    # async_sender = client.add_async_sender()
-    # await client.run_async()
-
-    def shutdown(event: Event):
+    async def shutdown(event: Event):
         """Shut down the thread and client."""
         _LOGGER.info("    Shutting down.")
         instance.queue.put(None)
         instance.join()
-        # client.close()
 
-    # hass.bus.async_listen(EVENT_STATE_CHANGED, async_send_to_event_hub)
-    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, shutdown)
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, shutdown)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    _LOGGER.info("    Shutting down.")
+    instance = hass.data[DOMAIN]
+    instance.queue.put(None)
+    instance.join()
 
     return True
 
@@ -133,21 +147,26 @@ def setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
 class AEHThread(threading.Thread):
     """A threaded event handler class."""
 
-    def __init__(self, hass, client_args, event_to_event_data):
+    def __init__(self, hass, client_args, event_to_event_data, conn_str_client):
         """Initialize the listener."""
         _LOGGER.debug("    Creating Thread.")
         threading.Thread.__init__(self, name="AzureEventHub")
         _LOGGER.info("    Thread Created.")
+        self.hass = hass
         self.queue = queue.Queue()
-        self.client_args = client_args
-        # self.client = aeh_client
+        self._client_args = client_args
+        self._conn_str_client = conn_str_client
         self.event_to_event_data = event_to_event_data
         self.write_errors = 0
         self.shutdown = False
-        _LOGGER.debug("    Adding event listener to hass bus.")
-        hass.bus.listen(EVENT_STATE_CHANGED, self._event_listener)
         _LOGGER.info("    Initialized AEH.")
 
+    @callback
+    def async_initialize(self):
+        """Initialize the recorder."""
+        self.hass.bus.async_listen(MATCH_ALL, self._event_listener)
+
+    @callback
     def _event_listener(self, event):
         """Listen for new messages on the bus and queue them for AEH."""
         item = (time.monotonic(), event)
@@ -158,15 +177,10 @@ class AEHThread(threading.Thread):
         """Return number of seconds to wait for more events."""
         return BATCH_TIMEOUT
 
-    def create_batch(self, client):
+    def create_batch(self, event_data_batch):
         """Return a batch of events formatted for writing."""
-        # _LOGGER.info("    Creating batch of events.")
-        queue_seconds = QUEUE_BACKLOG_SECONDS  # + self.max_tries * RETRY_DELAY
-        # _LOGGER.info("    Queue size:" + str(self.queue.qsize()))
-        event_data_batch = client.create_batch(max_size=10000)
         count = 0
         dropped = 0
-        # if not self.queue.empty():
         _LOGGER.debug("    Queue size: %s", str(self.queue.qsize()))
         can_add = True
         try:
@@ -177,10 +191,9 @@ class AEHThread(threading.Thread):
                     self.shutdown = True
                 else:
                     timestamp, event = item
-                    # _LOGGER.debug("      Event data item: " + str(event))
                     age = time.monotonic() - timestamp
                     try:
-                        if age < queue_seconds:
+                        if age < QUEUE_BACKLOG_SECONDS:
                             event_data = self.event_to_event_data(event)
                             _LOGGER.debug("      Event data json: %s", str(event_data))
                             if event_data:
@@ -199,67 +212,19 @@ class AEHThread(threading.Thread):
 
     def send(self):
         """Write preprocessed events to eventhub, with retry."""
-        if "conn_str" in self.client_args:
-            client = EventHubProducerClient.from_connection_string(**self.client_args)
+        if self._conn_str_client:
+            client = EventHubProducerClient.from_connection_string(**self._client_args)
         else:
-            client = EventHubProducerClient(**self.client_args)
+            client = EventHubProducerClient(**self._client_args)
+        event_data_batch = client.create_batch(max_size=10000)
 
-        count, batch_data = self.create_batch(client)
+        count, batch_data = self.create_batch(event_data_batch)
         if count > 0:
             _LOGGER.debug("    Sending messages count: %s", str(count))
             with client:
                 client.send(batch_data)
                 client.close()
         return count
-
-    # def get_events_json(self):
-    #     """Return a batch of events formatted for writing."""
-    #     queue_seconds = QUEUE_BACKLOG_SECONDS + self.max_tries * RETRY_DELAY
-
-    #     count = 0
-    #     events = []
-
-    #     dropped = 0
-
-    #     try:
-    #         while len(events) < BATCH_BUFFER_SIZE and not self.shutdown:
-    #             timeout = None if count == 0 else self.batch_timeout()
-    #             item = self.queue.get(timeout=timeout)
-    #             count += 1
-
-    #             if item is None:
-    #                 self.shutdown = True
-    #             else:
-    #                 timestamp, event = item
-    #                 age = time.monotonic() - timestamp
-
-    #                 if age < queue_seconds:
-    #                     event_json = self.event_to_json(event)
-    #                     if event_json:
-    #                         events.append(event_json)
-    #                 else:
-    #                     dropped += 1
-
-    #     except queue.Empty:
-    #         pass
-
-    #     if dropped:
-    #         _LOGGER.warning("Catching up, dropped %d old events", dropped)
-
-    #     return count, events
-
-    # async def write_to_eventhub(self, events):
-    #     """Write preprocessed events to eventhub, with retry."""
-    #     for retry in range(self.max_tries + 1):
-    #         try:
-    #             await self.async_sender.async_send_to_event_hub(events)
-    #         except AttributeError as err:
-    #             if retry < self.max_tries:
-    #                 time.sleep(RETRY_DELAY)
-    #                 await self.async_sender.reconnect_async()
-    #                 await self.async_sender.async_send_to_event_hub(events)
-    #             else:
-    #                 _LOGGER.error("Send error: %s", err)
 
     def run(self):
         """Process incoming events."""
--- homeassistant/components/azure_event_hub/config_flow.py
+++ homeassistant/components/azure_event_hub/config_flow.py
@@ -1,115 +1,122 @@
-"""Config flow to configure the Event Hub integration."""
+"""Config flow for Azure Event Hub integration."""
 import logging
 
 import voluptuous as vol
-from azure.eventhub import EventData, EventHubClientAsync
 
-from homeassistant import config_entries
-from homeassistant.core import Event, HomeAssistant
-from homeassistant.config_entries import ConfigFlow
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entityfilter import FILTER_SCHEMA
-from homeassistant.helpers.json import JSONEncoder
-from homeassistant.const import (
-    EVENT_HOMEASSISTANT_STOP,
-    EVENT_STATE_CHANGED,
-    STATE_UNAVAILABLE,
-    STATE_UNKNOWN,
+from azure.eventhub import (
+    EventHubProducerClient,
+    EventHubSharedKeyCredential,
+    ConnectError,
 )
 
-from homeassistant.components.azure_event_hub import (
-    DOMAIN,
-    CONFIG_SCHEMA,
-    CONF_EVENT_HUB_NAMESPACE,
-    CONF_EVENT_HUB_INSTANCE_NAME,
-    CONF_EVENT_HUB_SAS_POLICY,
-    CONF_EVENT_HUB_SAS_KEY,
+from homeassistant import core, config_entries, exceptions
+from homeassistant.helpers.entityfilter import FILTER_SCHEMA
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+from .const import DOMAIN  # pylint:disable=unused-import
+from .const import (
     CONF_EVENT_HUB_CON_STRING,
-    CONF_IOT_HUB_CON_STRING,
+    CONF_EVENT_HUB_INSTANCE_NAME,
+    CONF_EVENT_HUB_NAMESPACE,
+    CONF_EVENT_HUB_SAS_KEY,
+    CONF_EVENT_HUB_SAS_POLICY,
     CONF_FILTER,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_EVENT_HUB_NAMESPACE): str,
+        vol.Required(CONF_EVENT_HUB_INSTANCE_NAME): str,
+        vol.Required(CONF_EVENT_HUB_SAS_POLICY): str,
+        vol.Required(CONF_EVENT_HUB_SAS_KEY): str,
+    }
+)
+
+
+@callback
+def configured_instances(hass):
+    """Return a set of configured AEH instances."""
+    _LOGGER.info("Configured Instances")
+    return set(entry.title for entry in hass.config_entries.async_entries(DOMAIN))
+
+
+async def validate_input(hass: core.HomeAssistant, data, config=True):
+    """Validate the user input allows us to connect.
+
+    Data has the keys from DATA_SCHEMA with values provided by the user.
+    """
+    _LOGGER.info("Validating input from %s.", "config" if config else "yaml")
+    _LOGGER.debug("Config: %s", data)
+    if config:
+        client_args = {
+            "host": f"{data[CONF_EVENT_HUB_NAMESPACE]}.servicebus.windows.net",
+            "credential": EventHubSharedKeyCredential(
+                policy=data[CONF_EVENT_HUB_SAS_POLICY], key=data[CONF_EVENT_HUB_SAS_KEY]
+            ),
+            "event_hub_path": data[CONF_EVENT_HUB_INSTANCE_NAME],
+        }
+        conn_str_client = False
+    else:
+        data = data[DOMAIN]
+        if CONF_EVENT_HUB_CON_STRING in data:
+            client_args = {"conn_str": data[CONF_EVENT_HUB_CON_STRING]}
+            conn_str_client = True
+        else:
+            client_args = {
+                "host": f"{data[CONF_EVENT_HUB_NAMESPACE]}.servicebus.windows.net",
+                "credential": EventHubSharedKeyCredential(
+                    policy=data[CONF_EVENT_HUB_SAS_POLICY],
+                    key=data[CONF_EVENT_HUB_SAS_KEY],
+                ),
+                "event_hub_path": data[CONF_EVENT_HUB_INSTANCE_NAME],
+            }
+            conn_str_client = False
+    try:
+        if conn_str_client:
+            client = EventHubProducerClient.from_connection_string(**client_args)
+        else:
+            client = EventHubProducerClient(**client_args)
+        client.close()
+        return {"title": data[CONF_EVENT_HUB_NAMESPACE]}
+    except ConnectError:
+        raise CannotConnect
+
 
 @config_entries.HANDLERS.register(DOMAIN)
-class EventHubFlowHandler(ConfigFlow):
-    """Handle a Event Hub config flow."""
+class AzureEventHubConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Azure Event Hub."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_PUSH
 
-    _hassio_discovery = None
+    async def async_step_import(self, import_config):
+        """Import a config entry from configuration.yaml."""
+        return await self.async_step_user(import_config, config=True)
 
-    def __init__(self):
-        """Initialize Event Hub flow."""
-        pass
-
-    async def _show_setup_form(self, errors=None):
-        """Show the setup form to the user."""
-        return self.async_show_form(step_id="user", data_schema=CONFIG_SCHEMA)
-
-    async def async_step_user(self, user_input=None):
-        """Handle a flow initiated by the user."""
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
-
-        if user_input is None:
-            return await self._show_setup_form(user_input)
-
+    async def async_step_user(self, user_input=None, config=True):
+        """Handle the initial step."""
+        _LOGGER.info("Setting up steps.")
         errors = {}
-
-        entities_filter = user_input[CONF_FILTER]
-        if user_input[CONF_IOT_HUB_CON_STRING]:
-            client = EventHubClientAsync.from_iot_connection_string(
-                user_input[CONF_IOT_HUB_CON_STRING]
-            )
-        elif user_input[CONF_EVENT_HUB_CON_STRING]:
-            client = EventHubClientAsync.from_connection_string(
-                user_input[CONF_EVENT_HUB_CON_STRING]
-            )
-        else:
-            event_hub_address = "amqps://{}.servicebus.windows.net/{}".format(
-                user_input[CONF_EVENT_HUB_NAMESPACE],
-                user_input[CONF_EVENT_HUB_INSTANCE_NAME],
-            )
-            client = EventHubClientAsync(
-                event_hub_address,
-                username=user_input[CONF_EVENT_HUB_SAS_POLICY],
-                password=user_input[CONF_EVENT_HUB_SAS_KEY],
-            )
-
-        async_sender = client.add_async_sender()
-        await client.run_async()
-
-        # encoder = JSONEncoder()
-
-        async def async_send_to_event_hub(event: Event):
-            """Send states to Event Hub."""
-            state = event.data.get("new_state")
-            if (
-                state is None
-                or state.state in (STATE_UNKNOWN, "", STATE_UNAVAILABLE)
-                or not entities_filter(state.entity_id)
-            ):
-                return
-
-            event_data = EventData(
-                json.dumps(obj=state, cls=JSONEncoder).encode("utf-8")
-                # json.dumps(obj=state.as_dict(), default=encoder.default).encode("utf-8")
-            )
+        if user_input is not None:
             try:
-                await async_sender.send(event_data)
-            except AttributeError:
-                await async_sender.reconnect_async()
-                await async_sender.send(event_data)
+                info = await validate_input(self.hass, user_input, config)
+                return self.async_create_entry(title=info["title"], data=user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
 
-        async def async_shutdown(event: Event):
-            """Shut down the client."""
-            await client.stop_async()
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
 
-        hass.bus.async_listen(EVENT_STATE_CHANGED, async_send_to_event_hub)
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, async_shutdown)
 
-        return True
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(exceptions.HomeAssistantError):
+    """Error to indicate there is invalid auth."""
