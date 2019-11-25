86fa011c2d1b173c43b19dffc51a3adf893e56c6
Otto Winter <otto@otto-winter.com>
2019-11-25 14:13:19 +0100

    Add climate hvac_action for ESPHome (#28993)
    * Add climate action
    
    * Bump aioesphomeapi

--- homeassistant/components/esphome/climate.py
+++ homeassistant/components/esphome/climate.py
@@ -3,10 +3,11 @@ import logging
 from typing import List, Optional
 
 from aioesphomeapi import (
+    ClimateAction,
+    ClimateFanMode,
     ClimateInfo,
     ClimateMode,
     ClimateState,
-    ClimateFanMode,
     ClimateSwingMode,
 )
 
@@ -15,32 +16,38 @@ from homeassistant.components.climate.const import (
     ATTR_HVAC_MODE,
     ATTR_TARGET_TEMP_HIGH,
     ATTR_TARGET_TEMP_LOW,
-    HVAC_MODE_HEAT_COOL,
-    HVAC_MODE_COOL,
-    HVAC_MODE_HEAT,
-    HVAC_MODE_FAN_ONLY,
-    HVAC_MODE_DRY,
-    HVAC_MODE_OFF,
-    SUPPORT_TARGET_TEMPERATURE,
-    SUPPORT_PRESET_MODE,
-    SUPPORT_TARGET_TEMPERATURE_RANGE,
-    SUPPORT_FAN_MODE,
-    SUPPORT_SWING_MODE,
-    PRESET_AWAY,
-    PRESET_HOME,
-    FAN_ON,
-    FAN_OFF,
+    CURRENT_HVAC_COOL,
+    CURRENT_HVAC_DRY,
+    CURRENT_HVAC_FAN,
+    CURRENT_HVAC_HEAT,
+    CURRENT_HVAC_OFF,
+    CURRENT_HVAC_IDLE,
     FAN_AUTO,
+    FAN_DIFFUSE,
+    FAN_FOCUS,
+    FAN_HIGH,
     FAN_LOW,
     FAN_MEDIUM,
-    FAN_HIGH,
     FAN_MIDDLE,
-    FAN_FOCUS,
-    FAN_DIFFUSE,
-    SWING_OFF,
+    FAN_OFF,
+    FAN_ON,
+    HVAC_MODE_COOL,
+    HVAC_MODE_DRY,
+    HVAC_MODE_FAN_ONLY,
+    HVAC_MODE_HEAT,
+    HVAC_MODE_HEAT_COOL,
+    HVAC_MODE_OFF,
+    PRESET_AWAY,
+    PRESET_HOME,
+    SUPPORT_FAN_MODE,
+    SUPPORT_PRESET_MODE,
+    SUPPORT_SWING_MODE,
+    SUPPORT_TARGET_TEMPERATURE,
+    SUPPORT_TARGET_TEMPERATURE_RANGE,
     SWING_BOTH,
-    SWING_VERTICAL,
     SWING_HORIZONTAL,
+    SWING_OFF,
+    SWING_VERTICAL,
 )
 from homeassistant.const import (
     ATTR_TEMPERATURE,
@@ -82,6 +89,18 @@ def _climate_modes():
         ClimateMode.HEAT: HVAC_MODE_HEAT,
         ClimateMode.FAN_ONLY: HVAC_MODE_FAN_ONLY,
         ClimateMode.DRY: HVAC_MODE_DRY,
+    }
+
+
+@esphome_map_enum
+def _climate_actions():
+    return {
+        ClimateAction.OFF: CURRENT_HVAC_OFF,
+        ClimateAction.COOLING: CURRENT_HVAC_COOL,
+        ClimateAction.HEATING: CURRENT_HVAC_HEAT,
+        ClimateAction.IDLE: CURRENT_HVAC_IDLE,
+        ClimateAction.DRYING: CURRENT_HVAC_DRY,
+        ClimateAction.FAN: CURRENT_HVAC_FAN,
     }
 
 
@@ -204,6 +223,14 @@ class EsphomeClimateDevice(EsphomeEntity, ClimateDevice):
     def hvac_mode(self) -> Optional[str]:
         """Return current operation ie. heat, cool, idle."""
         return _climate_modes.from_esphome(self._state.mode)
+
+    @esphome_state_property
+    def hvac_action(self) -> Optional[str]:
+        """Return current action."""
+        # HA has no support feature field for hvac_action
+        if not self._static_info.supports_action:
+            return None
+        return _climate_actions.from_esphome(self._state.action)
 
     @esphome_state_property
     def fan_mode(self):
