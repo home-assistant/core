e93c7458eec6cafb7a93988140173989fa2383ad
Aaron Bach <bachya1208@gmail.com>
2019-11-25 15:28:50 -0700

    Activation handlers

--- homeassistant/components/scheduler/__init__.py
+++ homeassistant/components/scheduler/__init__.py
@@ -7,8 +7,9 @@ import voluptuous as vol
 
 from homeassistant.components import websocket_api
 from homeassistant.const import CONF_ENTITY_ID, CONF_ID
-from homeassistant.core import callback
+from homeassistant.core import Context, callback
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.event import async_track_point_in_time
 import homeassistant.util.dt as dt_util
 from homeassistant.util.json import load_json, save_json
 
@@ -19,58 +20,32 @@ PERSISTENCE = ".scheduler.json"
 
 EVENT = "scheduler_updated"
 
+CONF_ACTIVATION_CONTEXT_ID = "activation_context_id"
 CONF_END_DATETIME = "end_datetime"
 CONF_SCHEDULE_ID = "schedule_id"
 CONF_START_DATETIME = "start_datetime"
-CONF_ENABLED = "enabled"
 
 ITEM_UPDATE_SCHEMA = vol.Schema(
-    {
-        CONF_ENTITY_ID: cv.entity_id,
-        CONF_START_DATETIME: str,
-        CONF_END_DATETIME: str,
-        CONF_ENABLED: bool,
-    }
+    {CONF_ENTITY_ID: cv.entity_id, CONF_START_DATETIME: str, CONF_END_DATETIME: str}
 )
 
-WS_TYPE_SCHEDULER_CLEAR_INACTIVE_SCHEDULES = "scheduler/items/clear"
 WS_TYPE_SCHEDULER_CREATE_SCHEDULE = "scheduler/items/create"
 WS_TYPE_SCHEDULER_DELETE_SCHEDULE = "scheduler/items/delete"
-WS_TYPE_SCHEDULER_DISABLE_SCHEDULE = "scheduler/items/disable"
-WS_TYPE_SCHEDULER_ENABLE_SCHEDULE = "scheduler/items/enable"
 WS_TYPE_SCHEDULER_SCHEDULES = "scheduler/items"
 WS_TYPE_SCHEDULER_UPDATE_SCHEDULE = "scheduler/items/update"
-
-SCHEMA_WEBSOCKET_CLEAR_INACTIVE_SCHEDULES = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
-    {vol.Required("type"): WS_TYPE_SCHEDULER_CLEAR_INACTIVE_SCHEDULES}
-)
 
 SCHEMA_WEBSOCKET_CREATE_SCHEDULE = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
     {
         vol.Required("type"): WS_TYPE_SCHEDULER_CREATE_SCHEDULE,
         vol.Required(CONF_ENTITY_ID): cv.entity_id,
         vol.Required(CONF_START_DATETIME): str,
-        vol.Required(CONF_END_DATETIME): str,
+        vol.Optional(CONF_END_DATETIME): str,
     }
 )
 
 SCHEMA_WEBSOCKET_DELETE_SCHEDULE = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
     {
         vol.Required("type"): WS_TYPE_SCHEDULER_DELETE_SCHEDULE,
-        vol.Required(CONF_SCHEDULE_ID): str,
-    }
-)
-
-SCHEMA_WEBSOCKET_DISABLE_SCHEDULE = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
-    {
-        vol.Required("type"): WS_TYPE_SCHEDULER_DISABLE_SCHEDULE,
-        vol.Required(CONF_SCHEDULE_ID): str,
-    }
-)
-
-SCHEMA_WEBSOCKET_ENABLE_SCHEDULE = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
-    {
-        vol.Required("type"): WS_TYPE_SCHEDULER_ENABLE_SCHEDULE,
         vol.Required(CONF_SCHEDULE_ID): str,
     }
 )
@@ -90,25 +65,6 @@ SCHEMA_WEBSOCKET_UPDATE_SCHEDULE = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.ext
 )
 
 
-def raise_on_invalid_dates(start_datetime, end_datetime):
-    """Raise an error if either the start or end datetimes are invalid."""
-    now = datetime.now()
-
-    # Since it's possible to pass None for both parameters, check that case and return
-    # quickly if needed:
-    if not start_datetime and not end_datetime:
-        return
-
-    if start_datetime:
-        start_dt = dt_util.parse_datetime(start_datetime)
-
-    if end_datetime:
-        end_dt = dt_util.parse_datetime(end_datetime)
-
-    if (start_datetime and start_dt < now) or (end_datetime and end_dt < now):
-        raise ValueError
-
-
 async def async_setup(hass, config):
     """Set up the scheduler."""
     if DOMAIN not in config:
@@ -118,11 +74,6 @@ async def async_setup(hass, config):
     await scheduler.async_load()
 
     hass.components.websocket_api.async_register_command(
-        WS_TYPE_SCHEDULER_CLEAR_INACTIVE_SCHEDULES,
-        websocket_handle_clear,
-        SCHEMA_WEBSOCKET_CLEAR_INACTIVE_SCHEDULES,
-    )
-    hass.components.websocket_api.async_register_command(
         WS_TYPE_SCHEDULER_CREATE_SCHEDULE,
         websocket_handle_create,
         SCHEMA_WEBSOCKET_CREATE_SCHEDULE,
@@ -131,16 +82,6 @@ async def async_setup(hass, config):
         WS_TYPE_SCHEDULER_DELETE_SCHEDULE,
         websocket_handle_delete,
         SCHEMA_WEBSOCKET_DELETE_SCHEDULE,
-    )
-    hass.components.websocket_api.async_register_command(
-        WS_TYPE_SCHEDULER_DISABLE_SCHEDULE,
-        websocket_handle_disable,
-        SCHEMA_WEBSOCKET_DISABLE_SCHEDULE,
-    )
-    hass.components.websocket_api.async_register_command(
-        WS_TYPE_SCHEDULER_ENABLE_SCHEDULE,
-        websocket_handle_enable,
-        SCHEMA_WEBSOCKET_ENABLE_SCHEDULE,
     )
     hass.components.websocket_api.async_register_command(
         WS_TYPE_SCHEDULER_SCHEDULES, websocket_handle_items, SCHEMA_WEBSOCKET_SCHEDULES
@@ -154,50 +95,81 @@ async def async_setup(hass, config):
     return True
 
 
+def raise_on_invalid_dates(start_datetime, end_datetime):
+    """Raise an error if either the start or end datetimes are invalid."""
+    now = datetime.now()
+    if (start_datetime and start_datetime < now) or (
+        end_datetime and end_datetime < now
+    ):
+        raise ValueError("Dates in the past are not allowed")
+
+
 class Scheduler:
     """A class to handle scene schedules."""
 
     def __init__(self, hass):
         """Initialize."""
         self._hass = hass
+        self._async_activation_listeners = {}
+        self._async_deactivation_listeners = {}
         self.items = {}
 
     @callback
-    def _async_set_enabled(self, schedule_id, enabled):
-        """Enable/disable a schedule."""
-        if schedule_id not in self.items:
-            raise KeyError
-
-        item = self.items[schedule_id]
-        item[CONF_ENABLED] = enabled
-        self.async_save()
-        return item
-
-    @callback
-    def async_clear_disabled(self):
-        """Clear disabled/inactive scheduler items."""
-        self.items = {
-            schedule_id: item
-            for schedule_id, item in self.items.items()
-            if item[CONF_ENABLED]
-        }
-        self.async_save()
-
-    @callback
-    def async_create(self, entity_id, start_datetime, end_datetime=None):
+    def async_create(
+        self,
+        entity_id,
+        start_datetime,
+        end_datetime=None,
+        schedule_id=None,
+        activation_context=None,
+    ):
         """Add a scheduler item."""
-        raise_on_invalid_dates(start_datetime, end_datetime)
+        start = dt_util.parse_datetime(start_datetime)
+        try:
+            end = dt_util.parse_datetime(end_datetime)
+        except TypeError:
+            end = None
 
-        schedule_id = uuid4().hex
+        raise_on_invalid_dates(start, end)
+
+        if not schedule_id:
+            schedule_id = uuid4().hex
+        if not activation_context:
+            activation_context = Context()
+
         item = {
             CONF_ID: schedule_id,
             CONF_ENTITY_ID: entity_id,
             CONF_START_DATETIME: start_datetime,
             CONF_END_DATETIME: end_datetime,
-            CONF_ENABLED: True,
+            CONF_ACTIVATION_CONTEXT_ID: activation_context.id,
         }
+
+        @callback
+        def activate(call):
+            """Activate the scene."""
+            self._hass.async_create_task(
+                self._hass.services.async_call(
+                    "scene",
+                    "turn_on",
+                    service_data={"entity_id": entity_id},
+                    context=activation_context,
+                )
+            )
+
+            _LOGGER.info("Scheduler activated scene: %s", entity_id)
+
+            self._async_activation_listeners.pop(schedule_id)
+            self.items.pop(schedule_id)
+            self.async_save()
+
         self.items[schedule_id] = item
         self.async_save()
+
+        self._async_activation_listeners[schedule_id] = async_track_point_in_time(
+            self._hass, activate, start
+        )
+
         return item
 
     @callback
@@ -206,19 +178,17 @@ class Scheduler:
         if schedule_id not in self.items:
             raise KeyError
 
+        for listener_repo in (
+            self._async_activation_listeners,
+            self._async_deactivation_listeners,
+        ):
+            if schedule_id in listener_repo:
+                cancel = listener_repo(schedule_id)
+                cancel()
+
         item = self.items.pop(schedule_id)
         self.async_save()
         return item
-
-    @callback
-    def async_disable(self, schedule_id):
-        """Disable a scheduler item."""
-        return self._async_set_enabled(schedule_id, False)
-
-    @callback
-    def async_enable(self, schedule_id):
-        """Enable a scheduler item."""
-        return self._async_set_enabled(schedule_id, True)
 
     async def async_load(self):
         """Load scheduler items."""
@@ -250,11 +220,17 @@ class Scheduler:
             data.get(CONF_START_DATETIME), data.get(CONF_END_DATETIME)
         )
 
-        item = self.items[schedule_id]
         data = ITEM_UPDATE_SCHEMA(data)
+        item = self.async_delete(schedule_id)
         item.update(data)
-        self.async_save()
-        return item
+
+        return self.async_create(
+            item[CONF_ENTITY_ID],
+            item[CONF_START_DATETIME],
+            end_datetime=item[CONF_END_DATETIME],
+            schedule_id=item[CONF_SCHEDULE_ID],
+            activation_context=item[CONF_ACTIVATION_CONTEXT_ID],
+        )
 
 
 @callback
@@ -262,23 +238,14 @@ def websocket_handle_create(hass, connection, msg):
     """Handle creating a schedule."""
     try:
         item = hass.data[DOMAIN].async_create(
-            msg[CONF_ENTITY_ID], msg[CONF_START_DATETIME], msg[CONF_END_DATETIME]
+            msg[CONF_ENTITY_ID], msg[CONF_START_DATETIME], msg.get(CONF_END_DATETIME)
         )
-    except ValueError:
+    except ValueError as err:
         connection.send_message(
-            websocket_api.error_message(
-                msg[CONF_ID], "date_in_past", "Can't have a date in the past"
-            )
+            websocket_api.error_message(msg[CONF_ID], "invalid_parameter", err)
         )
     else:
         connection.send_message(websocket_api.result_message(msg[CONF_ID], item))
-
-
-@callback
-def websocket_handle_clear(hass, connection, msg):
-    """Handle clearing disabled schedules from the scheduler."""
-    hass.data[DOMAIN].async_clear_inactive()
-    connection.send_message(websocket_api.result_message(msg[CONF_ID]))
 
 
 @callback
@@ -286,36 +253,6 @@ def websocket_handle_delete(hass, connection, msg):
     """Handle deleting a schedule."""
     try:
         item = hass.data[DOMAIN].async_delete(msg[CONF_SCHEDULE_ID])
-    except KeyError:
-        connection.send_message(
-            websocket_api.error_message(
-                msg[CONF_ID], "schedule_not_found", "Schedule not found"
-            )
-        )
-    else:
-        connection.send_message(websocket_api.result_message(msg[CONF_ID], item))
-
-
-@callback
-def websocket_handle_disable(hass, connection, msg):
-    """Handle disabling a schedule."""
-    try:
-        item = hass.data[DOMAIN].async_disable(msg[CONF_SCHEDULE_ID])
-    except KeyError:
-        connection.send_message(
-            websocket_api.error_message(
-                msg[CONF_ID], "schedule_not_found", "Schedule not found"
-            )
-        )
-    else:
-        connection.send_message(websocket_api.result_message(msg[CONF_ID], item))
-
-
-@callback
-def websocket_handle_enable(hass, connection, msg):
-    """Handle enabling a schedule."""
-    try:
-        item = hass.data[DOMAIN].async_enable(msg[CONF_SCHEDULE_ID])
     except KeyError:
         connection.send_message(
             websocket_api.error_message(
@@ -350,11 +287,9 @@ def websocket_handle_update(hass, connection, msg):
                 msg_id, "schedule_not_found", "Schedule not found"
             )
         )
-    except ValueError:
+    except ValueError as err:
         connection.send_message(
-            websocket_api.error_message(
-                msg_id, "date_in_past", "Can't have a date in the past"
-            )
+            websocket_api.error_message(msg_id, "invalid_parameter", err)
         )
     else:
         connection.send_message(websocket_api.result_message(msg_id, item))
