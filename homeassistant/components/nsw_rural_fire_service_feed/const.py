34cf0ef99014f54537ce280c23acc1a4007e1af4
Malte Franken <exxamalte@users.noreply.github.com>
2019-11-25 13:44:22 +1100

    removed unused constants

--- homeassistant/components/nsw_rural_fire_service_feed/const.py
+++ homeassistant/components/nsw_rural_fire_service_feed/const.py
@@ -1,9 +1,4 @@
 """Define constants for the NSW Rural Fire Service Feeds integration."""
-
-PLATFORMS = "geo_location"
-
-FEED = "feed"
-
 SIGNAL_DELETE_ENTITY = "nsw_rural_fire_service_feed_delete_{}"
 SIGNAL_UPDATE_ENTITY = "nsw_rural_fire_service_feed_update_{}"
 
