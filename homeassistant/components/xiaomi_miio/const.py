2eda053754c234e05ea7abdd9b0ee7705d75e3f6
Raman Gupta <7243222+raman325@users.noreply.github.com>
2019-11-26 22:53:05 -0500

    move service constants to const.py, move all custom xiaomi_miio services to xiaomi_miio domain

---
+++ homeassistant/components/xiaomi_miio/const.py
@@ -0,0 +1,48 @@
+"""Constants for the Xiaomi Miio component."""
+DOMAIN = "xiaomi_miio"
+
+# Fan Services
+SERVICE_SET_BUZZER_ON = "set_buzzer_on"
+SERVICE_SET_BUZZER_OFF = "set_buzzer_off"
+SERVICE_SET_LED_ON = "set_led_on"
+SERVICE_SET_LED_OFF = "set_led_off"
+SERVICE_SET_CHILD_LOCK_ON = "set_child_lock_on"
+SERVICE_SET_CHILD_LOCK_OFF = "set_child_lock_off"
+SERVICE_SET_LED_BRIGHTNESS = "set_led_brightness"
+SERVICE_SET_FAVORITE_LEVEL = "set_favorite_level"
+SERVICE_SET_AUTO_DETECT_ON = "set_auto_detect_on"
+SERVICE_SET_AUTO_DETECT_OFF = "set_auto_detect_off"
+SERVICE_SET_LEARN_MODE_ON = "set_learn_mode_on"
+SERVICE_SET_LEARN_MODE_OFF = "set_learn_mode_off"
+SERVICE_SET_VOLUME = "set_volume"
+SERVICE_RESET_FILTER = "reset_filter"
+SERVICE_SET_EXTRA_FEATURES = "set_extra_features"
+SERVICE_SET_TARGET_HUMIDITY = "set_target_humidity"
+SERVICE_SET_DRY_ON = "set_dry_on"
+SERVICE_SET_DRY_OFF = "set_dry_off"
+
+# Light Services
+SERVICE_SET_SCENE = "set_scene"
+SERVICE_SET_DELAYED_TURN_OFF = "set_delayed_turn_off"
+SERVICE_REMINDER_ON = "reminder_on"
+SERVICE_REMINDER_OFF = "reminder_off"
+SERVICE_NIGHT_LIGHT_MODE_ON = "night_light_mode_on"
+SERVICE_NIGHT_LIGHT_MODE_OFF = "night_light_mode_off"
+SERVICE_EYECARE_MODE_ON = "eyecare_mode_on"
+SERVICE_EYECARE_MODE_OFF = "eyecare_mode_off"
+
+# Remote Services
+SERVICE_LEARN = "learn_command"
+
+# Switch Services
+SERVICE_SET_WIFI_LED_ON = "set_wifi_led_on"
+SERVICE_SET_WIFI_LED_OFF = "set_wifi_led_off"
+SERVICE_SET_POWER_MODE = "set_power_mode"
+SERVICE_SET_POWER_PRICE = "set_power_price"
+
+# Vacuum Services
+SERVICE_MOVE_REMOTE_CONTROL = "control_move"
+SERVICE_MOVE_REMOTE_CONTROL_STEP = "control_move_step"
+SERVICE_START_REMOTE_CONTROL = "control_start"
+SERVICE_STOP_REMOTE_CONTROL = "control_stop"
+SERVICE_CLEAN_ZONE = "clean_zone"
