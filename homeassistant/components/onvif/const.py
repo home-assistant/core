d3fdbffdbc63f487a99b2c63b8175af5f16eb09b
Kolja windeler <Kolja.windeler@gmail.com>
2019-11-28 21:47:34 +0100

    moved add const to dedicated file

---
+++ homeassistant/components/onvif/const.py
@@ -0,0 +1,29 @@
+"""Constants for the ONVIF Camera component."""
+DOMAIN = "onvif"
+
+DEFAULT_NAME = "ONVIF Camera"
+DEFAULT_PORT = 5000
+DEFAULT_USERNAME = "admin"
+DEFAULT_PASSWORD = "888888"
+DEFAULT_ARGUMENTS = "-pred 1"
+DEFAULT_PROFILE = 0
+
+CONF_PROFILE = "profile"
+
+ATTR_PAN = "pan"
+ATTR_TILT = "tilt"
+ATTR_ZOOM = "zoom"
+
+DIR_UP = "UP"
+DIR_DOWN = "DOWN"
+DIR_LEFT = "LEFT"
+DIR_RIGHT = "RIGHT"
+ZOOM_OUT = "ZOOM_OUT"
+ZOOM_IN = "ZOOM_IN"
+PTZ_NONE = "NONE"
+
+SERVICE_PTZ = "ptz"
+SERVICE_REBOOT = "reboot"
+
+ONVIF_DATA = "onvif"
+ENTITIES = "entities"
