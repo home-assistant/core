c6ae483660e78dfaf7b9d6d5b3d33f7a8bc81021
Wim Haanstra <wim@wim.me>
2019-11-29 06:33:28 +0100

    Fix review issues

--- homeassistant/components/dsmr_reader/sensor.py
+++ homeassistant/components/dsmr_reader/sensor.py
@@ -8,8 +8,6 @@ from homeassistant.util import slugify
 
 from .definitions import DEFINITIONS
 
-_LOGGER = logging.getLogger(__name__)
-
 DOMAIN = "dsmr_reader"
 
 
@@ -35,14 +33,9 @@ class DSMRSensor(Entity):
         self._topic = topic
 
         self._name = self._definition["name"]
-        self._unit_of_measurement = (
-            self._definition["unit"] if "unit" in self._definition else ""
-        )
-        self._icon = self._definition["icon"] if "icon" in self._definition else None
-        self._transform = (
-            self._definition["transform"] if "transform" in self._definition else None
-        )
-
+        self._unit_of_measurement = self._definition.get("unit", None)
+        self._icon = self._definition.get("icon", None)
+        self._transform = self._definition.get("transform", None)
         self._state = None
 
     async def async_added_to_hass(self):
@@ -59,7 +52,7 @@ class DSMRSensor(Entity):
 
             self.async_schedule_update_ha_state()
 
-        return await mqtt.async_subscribe(self.hass, self._topic, message_received, 1)
+        await mqtt.async_subscribe(self.hass, self._topic, message_received, 1)
 
     @property
     def name(self):
