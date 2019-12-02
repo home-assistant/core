aa7ab7e22639c3098bd66c359a2b117c84765c53
springstan <floorb@gmx.de>
2019-12-02 23:24:25 +0100

    Move imports to top for qbittorrent

--- homeassistant/components/qbittorrent/sensor.py
+++ homeassistant/components/qbittorrent/sensor.py
@@ -1,9 +1,9 @@
 """Support for monitoring the qBittorrent API."""
 import logging
 
-import voluptuous as vol
-
+from qbittorrent.client import Client, LoginRequired
 from requests.exceptions import RequestException
+import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
@@ -13,9 +13,9 @@ from homeassistant.const import (
     CONF_USERNAME,
     STATE_IDLE,
 )
-from homeassistant.helpers.entity import Entity
-import homeassistant.helpers.config_validation as cv
 from homeassistant.exceptions import PlatformNotReady
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -43,7 +43,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the qBittorrent sensors."""
-    from qbittorrent.client import Client, LoginRequired
 
     try:
         client = Client(config[CONF_URL])
