4fcd4986b9e80313974f4aefcc555fa32c20fe86
Shulyaka <Shulyaka@gmail.com>
2019-12-02 23:37:09 +0200

    make setup_platform async
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/demo/humidifier.py
+++ homeassistant/components/demo/humidifier.py
@@ -17,9 +17,9 @@ from homeassistant.components.humidifier.const import (
 SUPPORT_FLAGS = 0
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Demo humidifier devices."""
-    add_entities(
+    async_add_entities(
         [
             DemoHumidifier(
                 name="Humidifier",
