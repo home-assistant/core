0e223662a96f2fa8b510e85a27398edaea946729
springstan <46536646+springstan@users.noreply.github.com>
2019-12-04 00:45:46 +0100

    Move imports to top for nanoleaf (#29359)

--- homeassistant/components/nanoleaf/light.py
+++ homeassistant/components/nanoleaf/light.py
@@ -1,6 +1,7 @@
 """Support for Nanoleaf Lights."""
 import logging
 
+from pynanoleaf import Nanoleaf, Unavailable
 import voluptuous as vol
 
 from homeassistant.components.light import (
@@ -54,7 +55,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Nanoleaf light."""
-    from pynanoleaf import Nanoleaf, Unavailable
 
     if DATA_NANOLEAF not in hass.data:
         hass.data[DATA_NANOLEAF] = dict()
@@ -222,7 +222,6 @@ class NanoleafLight(Light):
 
     def update(self):
         """Fetch new state data for this light."""
-        from pynanoleaf import Unavailable
 
         try:
             self._available = self._light.available
