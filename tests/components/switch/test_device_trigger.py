fbdefafa52165f9c36112b284a2a343a6e359c29
Bas Nijholt <basnijholt@gmail.com>
2019-12-08 18:50:17 +0100

    Sort imports according to PEP8 for switch (#29654)

--- tests/components/switch/test_device_trigger.py
+++ tests/components/switch/test_device_trigger.py
@@ -1,22 +1,23 @@
 """The test for switch device automation."""
 from datetime import timedelta
+
 import pytest
 
-from homeassistant.components.switch import DOMAIN
-from homeassistant.const import STATE_ON, STATE_OFF, CONF_PLATFORM
-from homeassistant.setup import async_setup_component
 import homeassistant.components.automation as automation
+from homeassistant.components.switch import DOMAIN
+from homeassistant.const import CONF_PLATFORM, STATE_OFF, STATE_ON
 from homeassistant.helpers import device_registry
+from homeassistant.setup import async_setup_component
 import homeassistant.util.dt as dt_util
 
 from tests.common import (
     MockConfigEntry,
     async_fire_time_changed,
+    async_get_device_automation_capabilities,
+    async_get_device_automations,
     async_mock_service,
     mock_device_registry,
     mock_registry,
-    async_get_device_automations,
-    async_get_device_automation_capabilities,
 )
 
 
