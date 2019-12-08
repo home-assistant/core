b04e9f277aea2adbd10a839b2c35fc6d2e5b077c
springstan <floorb@gmx.de>
2019-12-08 18:03:18 +0100

    Make name customizable for nmbs live sensors

--- homeassistant/components/nmbs/sensor.py
+++ homeassistant/components/nmbs/sensor.py
@@ -80,7 +80,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     ]
 
     if station_live is not None:
-        sensors.append(NMBSLiveBoard(api_client, station_live))
+        sensors.append(NMBSLiveBoard(name, api_client, station_live))
 
     add_entities(sensors, True)
 
@@ -88,8 +88,9 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class NMBSLiveBoard(Entity):
     """Get the next train from a station's liveboard."""
 
-    def __init__(self, api_client, live_station):
+    def __init__(self, name, api_client, live_station):
         """Initialize the sensor for getting liveboard data."""
+        self._name = name
         self._station = live_station
         self._api_client = api_client
 
@@ -99,7 +100,7 @@ class NMBSLiveBoard(Entity):
     @property
     def name(self):
         """Return the sensor default name."""
-        return "NMBS Live"
+        return "Live {}".format(self._name)
 
     @property
     def icon(self):
