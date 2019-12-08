55559f3e306d9b50e5a02b9cbbd7f61ab07b4e58
Bas Nijholt <basnijholt@gmail.com>
2019-12-08 21:09:48 +0100

    Sort imports according to PEP8 for starline (#29653)

--- homeassistant/components/starline/sensor.py
+++ homeassistant/components/starline/sensor.py
@@ -2,6 +2,7 @@
 from homeassistant.components.sensor import DEVICE_CLASS_TEMPERATURE
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.icon import icon_for_battery_level, icon_for_signal_level
+
 from .account import StarlineAccount, StarlineDevice
 from .const import DOMAIN
 from .entity import StarlineEntity
