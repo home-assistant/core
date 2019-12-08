4aa830ebe65ba8162a87aec0f8366aca1e4f460e
Bas Nijholt <basnijholt@gmail.com>
2019-12-08 02:21:27 +0100

    use isort to sort imports according to PEP8 for wemo

--- homeassistant/components/wemo/light.py
+++ homeassistant/components/wemo/light.py
@@ -1,7 +1,7 @@
 """Support for Belkin WeMo lights."""
 import asyncio
-import logging
 from datetime import timedelta
+import logging
 
 import async_timeout
 from pywemo import discovery
@@ -9,15 +9,15 @@ import requests
 
 from homeassistant import util
 from homeassistant.components.light import (
-    Light,
     ATTR_BRIGHTNESS,
     ATTR_COLOR_TEMP,
     ATTR_HS_COLOR,
     ATTR_TRANSITION,
     SUPPORT_BRIGHTNESS,
-    SUPPORT_COLOR_TEMP,
     SUPPORT_COLOR,
+    SUPPORT_COLOR_TEMP,
     SUPPORT_TRANSITION,
+    Light,
 )
 from homeassistant.exceptions import PlatformNotReady
 import homeassistant.util.color as color_util
