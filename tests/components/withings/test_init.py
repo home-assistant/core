b2c530b7e8e6ad0fa8b1361d1b50dc2bb68d9954
Robbie Van Gorkom <robbie.van.gorkom@protonmail.com>
2019-12-08 11:34:51 -0800

    Fixing formatting.

--- tests/components/withings/test_init.py
+++ tests/components/withings/test_init.py
@@ -10,26 +10,26 @@ from withings_api.common import SleepModel, SleepState
 
 import homeassistant.components.http as http
 from homeassistant.components.withings import (
+    CONFIG_SCHEMA,
     async_setup,
     async_setup_entry,
     const,
-    CONFIG_SCHEMA,
 )
 from homeassistant.const import STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
 
 from .common import (
-    assert_state_equals,
-    configure_integration,
-    setup_hass,
     WITHINGS_GET_DEVICE_RESPONSE,
     WITHINGS_GET_DEVICE_RESPONSE_EMPTY,
+    WITHINGS_MEASURES_RESPONSE,
+    WITHINGS_MEASURES_RESPONSE_EMPTY,
     WITHINGS_SLEEP_RESPONSE,
     WITHINGS_SLEEP_RESPONSE_EMPTY,
     WITHINGS_SLEEP_SUMMARY_RESPONSE,
     WITHINGS_SLEEP_SUMMARY_RESPONSE_EMPTY,
-    WITHINGS_MEASURES_RESPONSE,
-    WITHINGS_MEASURES_RESPONSE_EMPTY,
+    assert_state_equals,
+    configure_integration,
+    setup_hass,
 )
 
 
@@ -314,7 +314,7 @@ async def test_full_setup(hass: HomeAssistant, aiohttp_client, aioclient_mock) -
                         "startdate": "2019-02-01 01:00:00",
                         "enddate": "2019-02-01 02:00:00",
                         "state": SleepState.AWAKE.real,
-                    }
+                    },
                 ],
             },
         },
@@ -343,7 +343,7 @@ async def test_full_setup(hass: HomeAssistant, aiohttp_client, aioclient_mock) -
                         "startdate": "2019-02-01 00:00:00",
                         "enddate": "2019-02-01 01:00:00",
                         "state": SleepState.REM.real,
-                    }
+                    },
                 ],
             },
         },
@@ -377,7 +377,7 @@ async def test_full_setup(hass: HomeAssistant, aiohttp_client, aioclient_mock) -
                         "startdate": "2019-02-01 01:00:00",
                         "enddate": "2019-02-01 02:00:00",
                         "state": SleepState.AWAKE.real,
-                    }
+                    },
                 ],
             },
         },
