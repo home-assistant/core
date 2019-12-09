1ab18083074e7388bddec59d4baad4202685b19f
Bas Nijholt <basnijholt@gmail.com>
2019-12-09 14:46:24 +0100

    Sort imports according to PEP8 for components starting with "N" (#29773)

--- tests/components/nsw_fuel_station/test_sensor.py
+++ tests/components/nsw_fuel_station/test_sensor.py
@@ -4,7 +4,8 @@ from unittest.mock import patch
 
 from homeassistant.components import sensor
 from homeassistant.setup import setup_component
-from tests.common import get_test_home_assistant, assert_setup_component
+
+from tests.common import assert_setup_component, get_test_home_assistant
 
 VALID_CONFIG = {
     "platform": "nsw_fuel_station",
