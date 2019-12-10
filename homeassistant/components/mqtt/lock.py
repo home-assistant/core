6fbcbf072d6b4fc16e16910a8520494a81962fba
Iulius <iulius@gutberlet.eu>
2019-12-10 01:02:16 +0100

    Formatting updated after black run

--- homeassistant/components/mqtt/lock.py
+++ homeassistant/components/mqtt/lock.py
@@ -57,7 +57,9 @@ PLATFORM_SCHEMA = (
                 CONF_PAYLOAD_UNLOCK, default=DEFAULT_PAYLOAD_UNLOCK
             ): cv.string,
             vol.Optional(CONF_STATE_LOCKED, default=DEFAULT_STATE_LOCKED): cv.string,
-            vol.Optional(CONF_STATE_UNLOCKED, default=DEFAULT_STATE_UNLOCKED): cv.string,
+            vol.Optional(
+                CONF_STATE_UNLOCKED, default=DEFAULT_STATE_UNLOCKED
+            ): cv.string,
             vol.Optional(CONF_UNIQUE_ID): cv.string,
         }
     )
