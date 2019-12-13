3c721fa7c82827c2843ac4fa5ddb8a737b1747b5
Franck Nijhof <git@frenck.dev>
2019-12-13 12:29:24 +0100

    Fix incorrect file format yr test fixure (#29910)

--- tests/components/yr/test_sensor.py
+++ tests/components/yr/test_sensor.py
@@ -16,7 +16,7 @@ def test_default_setup(hass, aioclient_mock):
     """Test the default setup."""
     aioclient_mock.get(
         "https://aa015h6buqvih86i1.api.met.no/" "weatherapi/locationforecast/1.9/",
-        text=load_fixture("yr.no.json"),
+        text=load_fixture("yr.no.xml"),
     )
     config = {"platform": "yr", "elevation": 0}
     hass.allow_pool = True
@@ -36,7 +36,7 @@ def test_custom_setup(hass, aioclient_mock):
     """Test a custom setup."""
     aioclient_mock.get(
         "https://aa015h6buqvih86i1.api.met.no/" "weatherapi/locationforecast/1.9/",
-        text=load_fixture("yr.no.json"),
+        text=load_fixture("yr.no.xml"),
     )
 
     config = {
@@ -82,7 +82,7 @@ def test_forecast_setup(hass, aioclient_mock):
     """Test a custom setup with 24h forecast."""
     aioclient_mock.get(
         "https://aa015h6buqvih86i1.api.met.no/" "weatherapi/locationforecast/1.9/",
-        text=load_fixture("yr.no.json"),
+        text=load_fixture("yr.no.xml"),
     )
 
     config = {
