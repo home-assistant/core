94eaea4968cf33041e6fd797e95a7af99dcd0077
Marc Egli <marcegli@google.com>
2019-12-17 20:34:09 +0100

    fix broken test import

--- tests/components/google_stackdriver/conftest.py
+++ tests/components/google_stackdriver/conftest.py
@@ -1,10 +1,10 @@
 """Configuration for Google Stackdriver tests."""
 import pytest
 
-from homeassistant.components.google_stackdriver import CONF_KEYFILE, DOMAIN
+from homeassistant.components.google_stackdriver import CONF_KEY_FILE, DOMAIN
 
 
 @pytest.fixture(name="config")
 def config_fixture():
     """Create hass config fixture."""
-    return {DOMAIN: {CONF_KEYFILE: "google_valid_service_account.json"}}
+    return {DOMAIN: {CONF_KEY_FILE: "google_valid_service_account.json"}}
