2fc37521d396bfaf95484cc564cec58e8ba2d237
Paulus Schoutsen <balloob@gmail.com>
2019-12-28 21:20:18 +0100

    Whitelist Android/iOS auth callbacks (#30082)
    * Whitelist Android/iOS
    
    * Add iOS alternate flavor URLs
    
    * Update indieauth.py
    
    Co-authored-by: Robbie Trencheny <me@robbiet.us>

--- homeassistant/components/auth/indieauth.py
+++ homeassistant/components/auth/indieauth.py
@@ -30,6 +30,14 @@ async def verify_redirect_uri(hass, client_id, redirect_uri):
     if is_valid:
         return True
 
+    # Whitelist the iOS and Android callbacks so that people can link apps
+    # without being connected to the internet.
+    if redirect_uri == "homeassistant://auth-callback" and client_id in (
+        "https://home-assistant.io/android",
+        "https://home-assistant.io/iOS",
+    ):
+        return True
+
     # IndieAuth 4.2.2 allows for redirect_uri to be on different domain
     # but needs to be specified in link tag when fetching `client_id`.
     redirect_uris = await fetch_redirect_uris(hass, client_id)
