833852dfebfcf117ff04840d81592f3403db8c8e
bendavid <joshbendavid@gmail.com>
2019-12-29 22:04:15 +0100

    update unit tests

--- tests/components/denonavr/test_media_player.py
+++ tests/components/denonavr/test_media_player.py
@@ -1,17 +1,39 @@
 """The tests for the denonavr media player platform."""
-import unittest
-from unittest import mock
+from unittest.mock import MagicMock
 
-from homeassistant.components.denonavr import media_player as denonavr
+from homeassistant.components import media_player
+from homeassistant.components.demo import media_player as demo
+import homeassistant.components.denonavr as denonavr
+from homeassistant.components.denonavr import ATTR_COMMAND, DOMAIN, SERVICE_GET_COMMAND
+from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.setup import async_setup_component
 
 
-class TestDenonDevice(unittest.TestCase):
-    """Test the DenonDevice class."""
+async def test_setup_demo_platform(hass):
+    """Test setup."""
+    mock = MagicMock()
+    add_entities = mock.MagicMock()
+    await demo.async_setup_platform(hass, {}, add_entities)
+    assert add_entities.call_count == 1
 
-    def setUp(self):
-        """Configure a fake device for each test."""
-        self.device = denonavr.DenonDevice(mock.MagicMock())
 
-    def test_get_command(self):
-        """Test generic command functionality."""
-        self.device.get_command("/goform/formiPhoneAppDirect.xml?RCKSK0410370")
+async def test_get_command(hass):
+    """Test generic command functionality."""
+
+    await hass.async_add_executor_job(denonavr.setup, hass, {})
+
+    assert await async_setup_component(
+        hass,
+        media_player.DOMAIN,
+        {"media_player": {"platform": "manual", "name": "test"}},
+    )
+
+    entity_id = "media_player.test"
+    command = "/goform/formiPhoneAppDirect.xml?RCKSK0410370"
+
+    data = {
+        ATTR_ENTITY_ID: entity_id,
+        ATTR_COMMAND: command,
+    }
+
+    await hass.services.async_call(DOMAIN, SERVICE_GET_COMMAND, data, blocking=True)
