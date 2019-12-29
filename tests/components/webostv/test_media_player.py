0d67f650cc0927b0182678e25789155b06887cd9
bendavid <joshbendavid@gmail.com>
2019-12-29 08:45:48 +0100

    make webostv unit tests work with python 3.7

--- tests/components/webostv/test_media_player.py
+++ tests/components/webostv/test_media_player.py
@@ -1,45 +1,34 @@
 """The tests for the LG webOS media player platform."""
-import unittest
 from unittest import mock
 
 from homeassistant.components.webostv import media_player as webostv
 
 
-class TestLgWebOSMediaPlayerEntity(unittest.IsolatedAsyncioTestCase):
-    """Test the LgWebOSMediaPlayerEntity class."""
+async def test_select_source_with_empty_source_list():
+    """Ensure we don't call client methods when we don't have sources."""
+    device = webostv.LgWebOSMediaPlayerEntity(mock.AsyncMock(), "fake_device")
+    await device.async_select_source("nonexistent")
+    assert 0 == device._client.launch_app.call_count
+    assert 0 == device._client.set_input.call_count
 
-    def setUp(self):
-        """Configure a fake device for each test."""
-        self.device = webostv.LgWebOSMediaPlayerEntity(mock.AsyncMock(), "fake_device")
 
-    async def test_select_source_with_empty_source_list(self):
-        """Ensure we don't call client methods when we don't have sources."""
-        await self.device.async_select_source("nonexistent")
-        assert 0 == self.device._client.launch_app.call_count
-        assert 0 == self.device._client.set_input.call_count
+async def test_select_source_with_titled_entry():
+    """Test that a titled source is treated as an app."""
+    device = webostv.LgWebOSMediaPlayerEntity(mock.AsyncMock(), "fake_device")
+    device._source_list = {"existent": {"id": "existent_id", "title": "existent_title"}}
 
-    async def test_select_source_with_titled_entry(self):
-        """Test that a titled source is treated as an app."""
-        self.device._source_list = {
-            "existent": {"id": "existent_id", "title": "existent_title"}
-        }
+    await device.async_select_source("existent")
 
-        await self.device.async_select_source("existent")
+    assert "existent_title" == device._current_source
+    assert [mock.call("existent_id")] == (device._client.launch_app.call_args_list)
 
-        assert "existent_title" == self.device._current_source
-        assert [mock.call("existent_id")] == (
-            self.device._client.launch_app.call_args_list
-        )
 
-    async def test_select_source_with_labelled_entry(self):
-        """Test that a labelled source is treated as an input source."""
-        self.device._source_list = {
-            "existent": {"id": "existent_id", "label": "existent_label"}
-        }
+async def test_select_source_with_labelled_entry():
+    """Test that a labelled source is treated as an input source."""
+    device = webostv.LgWebOSMediaPlayerEntity(mock.AsyncMock(), "fake_device")
+    device._source_list = {"existent": {"id": "existent_id", "label": "existent_label"}}
 
-        await self.device.async_select_source("existent")
+    await device.async_select_source("existent")
 
-        assert "existent_label" == self.device._current_source
-        assert [mock.call("existent_id")] == (
-            self.device._client.set_input.call_args_list
-        )
+    assert "existent_label" == device._current_source
+    assert [mock.call("existent_id")] == (device._client.set_input.call_args_list)
