5da7d9b20577450a501694232471e2e347d6d1f3
Paolo Tuninetto <paolo.tuninetto@gmail.com>
2019-12-30 16:51:21 +0100

    Trying codecov fix

--- homeassistant/components/samsungtv/media_player.py
+++ homeassistant/components/samsungtv/media_player.py
@@ -100,7 +100,7 @@ class SamsungTVDevice(MediaPlayerDevice):
         """Update state of device."""
         self.send_key(None)
 
-    def get_remote(self, clean=False):
+    def get_remote(self):
         """Create or return a remote control instance."""
         if self._remote is None:
             # We need to create a new instance to reconnect.
@@ -118,11 +118,11 @@ class SamsungTVDevice(MediaPlayerDevice):
             retry_count = 1
             for _ in range(retry_count + 1):
                 try:
-                    #                    if key !:
+                    # if key:
                     self.get_remote().control(key)
-                    #                    else:
+                    # else:
                     # clean existing connection (used only to test is TV is on)
-                    #                        self.get_remote(True)
+                    #   self.get_remote(True)
                     break
                 except (
                     samsung_exceptions.ConnectionClosed,
