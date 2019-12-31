1ed1efbcf8e92eed32287e6d4b10dbb01042a6cc
Alan Tse <alandtse@users.noreply.github.com>
2019-12-31 07:27:39 -0800

    Simplify Tesla icon get code (#30301)
    * Simplify icon get code
    
    * Remove extraneous None

--- homeassistant/components/tesla/__init__.py
+++ homeassistant/components/tesla/__init__.py
@@ -26,7 +26,7 @@ from .config_flow import (
     configured_instances,
     validate_input,
 )
-from .const import DATA_LISTENER, DOMAIN, SENSOR_ICONS, TESLA_COMPONENTS
+from .const import DATA_LISTENER, DOMAIN, ICONS, TESLA_COMPONENTS
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -186,11 +186,7 @@ class TeslaDevice(Entity):
         self._name = self.tesla_device.name
         self.tesla_id = slugify(self.tesla_device.uniq_name)
         self._attributes = {}
-        self._icon = (
-            SENSOR_ICONS[self.tesla_device.type]
-            if self.tesla_device.type and self.tesla_device.type in SENSOR_ICONS.keys()
-            else None
-        )
+        self._icon = ICONS.get(self.tesla_device.type)
 
     @property
     def name(self):
