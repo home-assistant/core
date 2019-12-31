e0b4660c76252ccd0f3598a4942f1c3cd6f68605
Issac Goldstand <margol@beamartyr.net>
2019-12-31 08:27:11 +0000

    Remove unneeded code from config_flow

--- homeassistant/components/local_ip/config_flow.py
+++ homeassistant/components/local_ip/config_flow.py
@@ -18,21 +18,11 @@ class SimpleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        for entry in self._async_current_entries():
-            _LOGGER.warning(entry)
-
-        errors = {}
         if user_input is not None:
-            try:
-                return self.async_create_entry(
-                    title=user_input["name"], data=user_input
-                )
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+            return self.async_create_entry(title=user_input["name"], data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({vol.Optional("name", default=DOMAIN): str}),
-            errors=errors,
+            errors={},
         )
