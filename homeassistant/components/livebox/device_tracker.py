fdfeccd123f6a498eb7d547cc895ee4fb266cecb
Cyr-ius <cyr-ius@ipocus.net>
2020-01-03 16:59:14 +0100

    Remove dispatcher

--- homeassistant/components/livebox/device_tracker.py
+++ homeassistant/components/livebox/device_tracker.py
@@ -1,15 +1,16 @@
 """Support for the Livebox platform."""
+from datetime import timedelta
 import logging
 
 from homeassistant.components.device_tracker import SOURCE_TYPE_ROUTER
 from homeassistant.components.device_tracker.config_entry import ScannerEntity
-from homeassistant.core import callback
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.util import Throttle
 
 from . import DATA_LIVEBOX, DOMAIN, ID_BOX
 
-TRACKER_UPDATE = "{}_tracker_update".format(DOMAIN)
 _LOGGER = logging.getLogger(__name__)
+
+SCAN_INTERVAL = timedelta(minutes=1)
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -34,7 +35,6 @@ class LiveboxDeviceScannerEntity(ScannerEntity):
         """Initialize the device tracker."""
         self._bridge_id = id
         self._device = device
-        self._unsubs = []
         self._session = session
 
     @property
@@ -46,6 +46,16 @@ class LiveboxDeviceScannerEntity(ScannerEntity):
     def unique_id(self) -> str:
         """Return a unique ID."""
         return self._device["PhysAddress"]
+
+    @property
+    def is_connected(self):
+        """Return true if the device is connected to the network."""
+        return self._device["Active"] is True
+
+    @property
+    def source_type(self):
+        """Return the source type, eg gps or router, of the device."""
+        return SOURCE_TYPE_ROUTER
 
     @property
     def device_info(self):
@@ -66,36 +76,8 @@ class LiveboxDeviceScannerEntity(ScannerEntity):
         }
         return _attributs
 
-    async def async_added_to_hass(self):
-        """Register state update callback."""
-        await super().async_added_to_hass()
-        self._unsubs = async_dispatcher_connect(
-            self.hass,
-            TRACKER_UPDATE,
-            self._async_receive_data(self._bridge_id, self._device, self._session),
-        )
-
-    async def async_will_remove_from_hass(self) -> None:
-        """Disconnect entity object when removed."""
-        await super().async_will_remove_from_hass()
-        self._unsubs()
-
+    @Throttle(SCAN_INTERVAL)
     async def async_update(self):
         """Handle polling."""
         _LOGGER.debug(f"Update {self.name} - {self.unique_id} - {self.is_connected}")
         self._device = await self._session.async_get_device(self.unique_id)
-
-    @property
-    def is_connected(self):
-        """Return true if the device is connected to the network."""
-        return self._device["Active"] is True
-
-    @property
-    def source_type(self):
-        """Return the source type, eg gps or router, of the device."""
-        return SOURCE_TYPE_ROUTER
-
-    @callback
-    async def _async_receive_data(self, bridge_id, device, session):
-        if await self.async_update():
-            self.async_write_ha_state()
