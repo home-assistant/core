54538dfea9ca0a9da99d2745c0bb8c66f28ce19c
brefra <frank_van_breugel@hotmail.com>
2020-01-03 23:06:45 +0100

    Fix async call inside sync context

--- homeassistant/components/velbus/__init__.py
+++ homeassistant/components/velbus/__init__.py
@@ -67,7 +67,7 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
         hass.data[DOMAIN][entry.entry_id] = discovery_info
 
         for category in COMPONENT_TYPES:
-            hass.async_create_task(
+            hass.add_job(
                 hass.config_entries.async_forward_entry_setup(entry, category)
             )
 
