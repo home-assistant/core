4729d4781184b3fa01f27fcf0dfa93e37763f370
brefra <frank_van_breugel@hotmail.com>
2020-01-03 17:06:25 +0100

    Add LED control

--- homeassistant/components/velbus/light.py
+++ homeassistant/components/velbus/light.py
@@ -5,8 +5,12 @@ from velbus.util import VelbusException
 
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
+    ATTR_FLASH,
     ATTR_TRANSITION,
+    FLASH_LONG,
+    FLASH_SHORT,
     SUPPORT_BRIGHTNESS,
+    SUPPORT_FLASH,
     SUPPORT_TRANSITION,
     Light,
 )
@@ -37,9 +41,28 @@ class VelbusLight(VelbusEntity, Light):
     """Representation of a Velbus light."""
 
     @property
+    def name(self):
+        """Return the display name of this entity."""
+        if self._module.light_is_buttonled(self._channel):
+            return "LED " + self._module.get_name(self._channel)
+        else:
+            return self._module.get_name(self._channel)
+
+    @property
     def supported_features(self):
         """Flag supported features."""
-        return SUPPORT_BRIGHTNESS | SUPPORT_TRANSITION
+        if self._module.light_is_buttonled(self._channel):
+            return SUPPORT_FLASH
+        else:
+            return SUPPORT_BRIGHTNESS | SUPPORT_TRANSITION
+
+    @property
+    def entity_registry_enabled_default(self):
+        """Disable Button LEDs by default."""
+        if self._module.light_is_buttonled(self._channel):
+            return False
+        else:
+            return True
 
     @property
     def is_on(self):
@@ -53,25 +76,45 @@ class VelbusLight(VelbusEntity, Light):
 
     def turn_on(self, **kwargs):
         """Instruct the Velbus light to turn on."""
-        try:
-            if ATTR_BRIGHTNESS in kwargs:
-                self._module.set_dimmer_state(
-                    self._channel,
-                    kwargs[ATTR_BRIGHTNESS],
-                    kwargs.get(ATTR_TRANSITION, 0),
-                )
+        if self._module.light_is_buttonled(self._channel):
+            if ATTR_FLASH in kwargs:
+                if kwargs[ATTR_FLASH] == FLASH_LONG:
+                    action = "slow"
+                elif kwargs[ATTR_FLASH] == FLASH_SHORT:
+                    action = "fast"
+                else:
+                    action = "on"
             else:
-                self._module.restore_dimmer_state(
-                    self._channel, kwargs.get(ATTR_TRANSITION, 0),
-                )
-        except VelbusException as err:
-            _LOGGER.error("A Velbus error occurred: %s", err)
+                action = "on"
+            try:
+                self._module.set_led_state(self._channel, action)
+            except VelbusException as err:
+                _LOGGER.error("A Velbus error occurred: %s", err)
+        else:
+            try:
+                if ATTR_BRIGHTNESS in kwargs:
+                    self._module.set_dimmer_state(
+                        self._channel, kwargs[ATTR_BRIGHTNESS],
+                        kwargs.get(ATTR_TRANSITION, 0),
+                    )
+                else:
+                    self._module.restore_dimmer_state(
+                        self._channel, kwargs.get(ATTR_TRANSITION, 0),
+                    )
+            except VelbusException as err:
+                _LOGGER.error("A Velbus error occurred: %s", err)
 
     def turn_off(self, **kwargs):
         """Instruct the velbus light to turn off."""
-        try:
-            self._module.set_dimmer_state(
-                self._channel, 0, kwargs.get(ATTR_TRANSITION, 0),
-            )
-        except VelbusException as err:
-            _LOGGER.error("A Velbus error occurred: %s", err)
+        if self._module.light_is_buttonled(self._channel):
+            try:
+                self._module.set_led_state(self._channel, "off")
+            except VelbusException as err:
+                _LOGGER.error("A Velbus error occurred: %s", err)
+        else:
+            try:
+                self._module.set_dimmer_state(
+                    self._channel, 0, kwargs.get(ATTR_TRANSITION, 0),
+                )
+            except VelbusException as err:
+                _LOGGER.error("A Velbus error occurred: %s", err)
