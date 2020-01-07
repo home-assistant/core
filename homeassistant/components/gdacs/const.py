7d04943aa7db8887a1288d91d2f4becac5875738
Malte Franken <exxamalte@users.noreply.github.com>
2020-01-07 22:05:54 +1100

    initial version of gdacs integration

---
+++ homeassistant/components/gdacs/const.py
@@ -0,0 +1,18 @@
+"""Define constants for the GDACS integration."""
+from datetime import timedelta
+
+DOMAIN = "gdacs"
+
+PLATFORMS = ("sensor", "geo_location")
+
+FEED = "feed"
+
+DEFAULT_ICON = "mdi:alert"
+DEFAULT_RADIUS = 500.0
+DEFAULT_SCAN_INTERVAL = timedelta(minutes=5)
+
+SIGNAL_DELETE_ENTITY = "gdacs_delete_{}"
+SIGNAL_UPDATE_ENTITY = "gdacs_update_{}"
+SIGNAL_STATUS = "gdacs_status_{}"
+
+SIGNAL_NEW_GEOLOCATION = "gdacs_new_geolocation_{}"
