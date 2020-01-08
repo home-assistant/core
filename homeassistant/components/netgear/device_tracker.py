d29cf959bef4a6c76179de7de795fdc78a756c77
Daniel Lashua <daniel@inklog.net>
2020-01-08 07:57:29 -0600

    check link_rate to ensure device is connected

--- homeassistant/components/netgear/device_tracker.py
+++ homeassistant/components/netgear/device_tracker.py
@@ -31,9 +31,15 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_USERNAME, default=""): cv.string,
         vol.Required(CONF_PASSWORD): cv.string,
         vol.Optional(CONF_PORT, default=None): vol.Any(None, cv.port),
-        vol.Optional(CONF_DEVICES, default=[]): vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_EXCLUDE, default=[]): vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_APS, default=[]): vol.All(cv.ensure_list, [cv.string]),
+        vol.Optional(CONF_DEVICES, default=[]): vol.All(
+            cv.ensure_list, [cv.string]
+        ),
+        vol.Optional(CONF_EXCLUDE, default=[]): vol.All(
+            cv.ensure_list, [cv.string]
+        ),
+        vol.Optional(CONF_APS, default=[]): vol.All(
+            cv.ensure_list, [cv.string]
+        ),
     }
 )
 
@@ -51,7 +57,14 @@ def get_scanner(hass, config):
     accesspoints = info.get(CONF_APS)
 
     scanner = NetgearDeviceScanner(
-        host, ssl, username, password, port, devices, excluded_devices, accesspoints
+        host,
+        ssl,
+        username,
+        password,
+        port,
+        devices,
+        excluded_devices,
+        accesspoints,
     )
 
     return scanner if scanner.success_init else None
@@ -110,7 +123,10 @@ class NetgearDeviceScanner(DeviceScanner):
                     or dev.name in self.excluded_devices
                 )
             )
-            if tracked:
+
+            # when link_rate is None this means the router still knows about
+            # the device, but it is not in range.
+            if tracked and dev.link_rate is not None:
                 devices.append(dev.mac)
                 if (
                     self.tracked_accesspoints
