fcd85f279f872792c89174188e355eae9b0e2cfa
Adam Michaleski <adam@prairieapps.com>
2020-01-09 21:03:06 -0600

    Add integration for Schluter New integration for Schluter DITRA-HEAT thermostats. Interacts with Schluter API via py-schluter to implement climate platform.

---
+++ homeassistant/components/schluter/__init__.py
@@ -0,0 +1,184 @@
+"""The Schluter DITRA-HEAT integration."""
+from datetime import timedelta
+import logging
+
+import voluptuous as vol
+from schluter.api import Api
+from schluter.authenticator import AuthenticationState, Authenticator
+from .const import DOMAIN
+from requests import RequestException, Session
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import discovery
+from homeassistant.util import Throttle
+import homeassistant.helpers.config_validation as cv
+
+from homeassistant.const import (
+    CONF_PASSWORD,
+    CONF_TIMEOUT,
+    CONF_USERNAME,
+    EVENT_HOMEASSISTANT_STOP,
+)
+
+_LOGGER = logging.getLogger(__name__)
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=5)
+_CONFIGURING = {}
+
+DATA_SCHLUTER = "schluter"
+DOMAIN = "schluter"
+PLATFORMS = ["climate"]
+DEFAULT_TIMEOUT = 10
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=5)
+SCHLUTER_CONFIG_FILE = ".schluter.conf"
+NOTIFICATION_ID = "schluter_notification"
+NOTIFICATION_TITLE = "Schluter Setup"
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_USERNAME): cv.string,
+                vol.Required(CONF_PASSWORD): cv.string,
+                vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+def setup_schluter(hass, config, api, authenticator):
+    """Set up the Schluter component."""
+
+    authentication = None
+    try:
+        authentication = authenticator.authenticate()
+    except RequestException as ex:
+        _LOGGER.error("Unable to connect to Schluter service: %s", str(ex))
+
+        hass.components.persistent_notification.create(
+            "Error: {}<br />"
+            "You will need to restart hass after fixing."
+            "".format(ex),
+            title=NOTIFICATION_TITLE,
+            notification_id=NOTIFICATION_ID,
+        )
+
+    state = authentication.state
+
+    if state == AuthenticationState.AUTHENTICATED:
+        if DOMAIN in _CONFIGURING:
+            hass.components.configurator.request_done(_CONFIGURING.pop(DOMAIN))
+
+        hass.data[DATA_SCHLUTER] = SchluterData(hass, api, authentication.session_id)
+
+        for component in PLATFORMS:
+            discovery.load_platform(hass, component, DOMAIN, {}, config)
+
+        return True
+    if state == AuthenticationState.BAD_PASSWORD:
+        _LOGGER.error("Invalid password provided")
+        return False
+    if state == AuthenticationState.BAD_EMAIL:
+        _LOGGER.error("Invalid email provided")
+        return False
+
+    return False
+
+
+def setup(hass, config):
+    """Set up the Schluter component."""
+    _LOGGER.debug("Starting setup of schluter")
+    conf = config[DOMAIN]
+    api_http_session = None
+    try:
+        api_http_session = Session()
+    except RequestException as ex:
+        _LOGGER.warning("Creating HTTP session failed with: %s", str(ex))
+
+    api = Api(timeout=conf.get(CONF_TIMEOUT), http_session=api_http_session)
+
+    authenticator = Authenticator(
+        api,
+        conf.get(CONF_USERNAME),
+        conf.get(CONF_PASSWORD),
+        session_id_cache_file=hass.config.path(SCHLUTER_CONFIG_FILE),
+    )
+
+    def close_http_session(event):
+        """Close API sessions used to connect to Schluter."""
+        _LOGGER.debug("Closing HTTP sessions")
+        if api_http_session:
+            try:
+                api_http_session.close()
+            except RequestException:
+                pass
+
+        _LOGGER.debug("HTTP session closed.")
+
+    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, close_http_session)
+    _LOGGER.debug("Registered for Home Assistant stop event")
+
+    return setup_schluter(hass, config, api, authenticator)
+
+
+class SchluterData:
+    """Schluter data object."""
+
+    def __init__(self, hass, api, session_id):
+        self._hass = hass
+        self._api = api
+        self._session_id = session_id
+        self._thermostats = self._api.get_thermostats(self._session_id) or []
+
+        self._thermostat_temp_by_id = {}
+        self._thermostat_set_temp_by_id = {}
+        self._thermostat_is_heating_by_id = {}
+
+    @property
+    def thermostats(self):
+        """Get thermostats."""
+        return self._thermostats
+
+    def get_thermostat_temp(self, serial_number):
+        """Get thermostat current temperatures."""
+        self._update_thermostats()
+        return self._thermostat_temp_by_id.get(serial_number)
+
+    def get_thermostat_set_temp(self, serial_number):
+        """Get thermostat set temperatures."""
+        self._update_thermostats()
+        return self._thermostat_set_temp_by_id.get(serial_number)
+
+    def get_thermostat_heating_status(self, serial_number):
+        """Get thermostat heating statuses."""
+        self._update_thermostats()
+        return self._thermostat_is_heating_by_id.get(serial_number)
+
+    def set_thermostat_temp(self, serial_number, temperature):
+        """Set thermostat temperature.."""
+        result = self._api.set_temperature(self._session_id, serial_number, temperature)
+        return result
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def _update_thermostats(self):
+        temp_by_id = {}
+        set_temp_by_id = {}
+        is_heating_by_id = {}
+
+        try:
+            _LOGGER.debug("Updating thermostats from API")
+            self._thermostats = self._api.get_thermostats(self._session_id)
+
+            for thermostat in self._thermostats:
+                temp_by_id[thermostat.serial_number] = thermostat.temperature
+                set_temp_by_id[thermostat.serial_number] = thermostat.set_point_temp
+                is_heating_by_id[thermostat.serial_number] = thermostat.is_heating
+        except RequestException as ex:
+            _LOGGER.error("Request error trying to retrieve thermostats. %s", ex)
+
+        _LOGGER.debug("Successfully retrieved thermostats from API")
+        self._thermostat_temp_by_id = temp_by_id
+        self._thermostat_set_temp_by_id = set_temp_by_id
+        self._thermostat_is_heating_by_id = is_heating_by_id
---
+++ homeassistant/components/schluter/climate.py
@@ -0,0 +1,169 @@
+"""Support for Schluter thermostats."""
+import logging
+import voluptuous as vol
+from requests import RequestException
+from datetime import timedelta
+
+from homeassistant.components.climate import PLATFORM_SCHEMA, ClimateDevice
+from homeassistant.components.climate.const import (
+    HVAC_MODE_HEAT,
+    HVAC_MODE_OFF,
+    SUPPORT_TARGET_TEMPERATURE,
+)
+from homeassistant.const import ATTR_TEMPERATURE, CONF_SCAN_INTERVAL, TEMP_CELSIUS
+
+from . import DATA_SCHLUTER, DOMAIN as SCHLUTER_DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+SCAN_INTERVAL = timedelta(seconds=5)
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {vol.Optional(CONF_SCAN_INTERVAL): vol.All(vol.Coerce(int), vol.Range(min=1))}
+)
+
+MODE_SCHLUTER_TO_HASS = {
+    False: HVAC_MODE_OFF,
+    True: HVAC_MODE_HEAT,
+}
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Schluter thermostats."""
+    data = hass.data[DATA_SCHLUTER]
+    devices = []
+    temp_unit = hass.config.units.temperature_unit
+
+    for thermostat in data.thermostats:
+        devices.append(SchluterThermostat(thermostat, temp_unit, data))
+        _LOGGER.debug("Adding thermostat: %s", thermostat.name)
+
+    add_entities(devices, True)
+
+
+class SchluterThermostat(ClimateDevice):
+    """Representation of a Schluter thermostat."""
+
+    def __init__(self, device, temp_unit, data):
+        """Initialize the thermostat."""
+        self._unit = temp_unit
+        self.device = device
+        self.data = data
+
+        # Set the default supported features
+        self._support_flags = SUPPORT_TARGET_TEMPERATURE
+
+        # data attributes
+        self._serial_number = None
+        self._group = None
+        self._name = None
+        self._target_temperature = None
+        self._temperature = None
+        self._temperature_scale = None
+        self._is_heating = None
+        self._action = None
+        self._min_temperature = None
+        self._max_temperature = None
+
+    @property
+    def should_poll(self):
+        return True
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return self._support_flags
+
+    @property
+    def unique_id(self):
+        """Return unique ID for this device."""
+        return self.device.serial_number
+
+    @property
+    def device_info(self):
+        """Return information about the device."""
+        return {
+            "identifiers": {(SCHLUTER_DOMAIN, self.device.serial_number)},
+            "name": self.device.name,
+            "manufacturer": "Schluter",
+            "model": "Thermostat",
+            "sw_version": self.device.sw_version,
+        }
+
+    @property
+    def name(self):
+        """Return the name of the thermostat."""
+        return self._name
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        return self._unit
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        return self._temperature
+
+    @property
+    def hvac_mode(self):
+        """Return current operation ie. heat, idle."""
+        return MODE_SCHLUTER_TO_HASS[self._is_heating]
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return self._target_temperature
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        temp = None
+        temp = kwargs.get(ATTR_TEMPERATURE)
+        _LOGGER.debug("Setting thermostat temperature: %s", temp)
+
+        try:
+            if temp is not None:
+                self.device.target = temp
+                self.data.set_thermostat_temp(self._serial_number, temp)
+        except RequestException as ex:
+            _LOGGER.error("An error occurred while setting temperature: %s", ex)
+            self.schedule_update_ha_state(True)
+
+    @property
+    def hvac_modes(self):
+        """List of available operation modes."""
+        return None
+
+    @property
+    def fan_mode(self):
+        """Return whether the fan is on."""
+        # No Fan available
+        return None
+
+    @property
+    def fan_modes(self):
+        """List of available fan modes."""
+        return None
+
+    @property
+    def min_temp(self):
+        """Identify min_temp in Schluter API."""
+        return self._min_temperature
+
+    @property
+    def max_temp(self):
+        """Identify max_temp in Schluter API."""
+        return self._max_temperature
+
+    def update(self):
+        """Cache value from py-schluter."""
+        self._serial_number = self.device.serial_number
+        self._group = self.device.group_name
+        self._name = self.device.name
+        self._min_temperature = self.device.min_temp
+        self._max_temperature = self.device.max_temp
+        self._temperature_scale = TEMP_CELSIUS
+
+        self._temperature = self.data.get_thermostat_temp(self._serial_number)
+        self._is_heating = self.data.get_thermostat_heating_status(self._serial_number)
+        self._target_temperature = self.data.get_thermostat_set_temp(
+            self._serial_number
+        )
