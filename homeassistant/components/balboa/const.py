75c1624780b0a4d80bcebe29d19ddfa5402ef6dc
garbled1 <root@garbled.net>
2020-01-11 05:36:59 -0700

    Remove everything except climate per request for first import.

--- homeassistant/components/balboa/const.py
+++ homeassistant/components/balboa/const.py
@@ -7,10 +7,8 @@ from homeassistant.components.climate.const import (
     HVAC_MODE_HEAT,
     HVAC_MODE_OFF,
 )
-from homeassistant.components.fan import SPEED_HIGH, SPEED_LOW, SPEED_OFF
 
 DOMAIN = "balboa"
-BALBOA_PLATFORMS = ["climate", "switch", "binary_sensor", "fan"]
+BALBOA_PLATFORMS = ["climate"]
 CLIMATE_SUPPORTED_MODES = [HVAC_MODE_HEAT, HVAC_MODE_OFF]
 CLIMATE_SUPPORTED_FANSTATES = [FAN_OFF, FAN_LOW, FAN_MEDIUM, FAN_HIGH]
-FAN_SUPPORTED_SPEEDS = [SPEED_OFF, SPEED_LOW, SPEED_HIGH]
