4064d6a0ceeb554557d9721aec91e2bd554712d8
Paulus Schoutsen <balloob@gmail.com>
2020-01-11 07:36:53 -0800

    Fix test

--- tests/components/ring/test_config_flow.py
+++ tests/components/ring/test_config_flow.py
@@ -50,7 +50,8 @@ async def test_form_invalid_auth(hass):
     )
 
     with patch(
-        "homeassistant.components.ring.config_flow.Ring", side_effect=InvalidAuth,
+        "homeassistant.components.ring.config_flow.Auth.fetch_token",
+        side_effect=InvalidAuth,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
