b1e311fb858ba80b15928fd6d8852f7af7413ff7
Stefan Agner <stefan@agner.ch>
2020-01-12 20:41:27 +0100

    Add stationboard type sensor for swiss public transport
    The Swiss public transport API supports stationboard type requests to
    get all departing connections from a given station. Add this type of
    sensor to the swiss_public_transport integration. This is especially
    useful for smaller train or bus station to just display when the next
    departure is and whether the departure is delayed.

--- homeassistant/components/swiss_public_transport/sensor.py
+++ homeassistant/components/swiss_public_transport/sensor.py
@@ -2,7 +2,7 @@
 from datetime import timedelta
 import logging
 
-from opendata_transport import OpendataTransport
+from opendata_transport import OpendataTransport, OpendataTransportStationboard
 from opendata_transport.exceptions import OpendataTransportError
 import voluptuous as vol
 
@@ -26,11 +26,13 @@ ATTR_TRAIN_NUMBER = "train_number"
 ATTR_TRANSFERS = "transfers"
 ATTR_DELAY = "delay"
 ATTR_CONNECTIONS = "connections"
+ATTR_DEPARTURES = "departures"
 
 ATTRIBUTION = "Data provided by transport.opendata.ch"
 
 CONF_DESTINATION = "to"
 CONF_START = "from"
+CONF_STATIONBOARD = "stationboard"
 CONF_LIMIT = "limit"
 
 DEFAULT_NAME = "Next Departure"
@@ -41,12 +43,28 @@ SCAN_INTERVAL = timedelta(seconds=90)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Required(CONF_DESTINATION): cv.string,
-        vol.Required(CONF_START): cv.string,
+        vol.Optional(CONF_DESTINATION): cv.string,
+        vol.Optional(CONF_START): cv.string,
+        vol.Optional(CONF_STATIONBOARD): vol.All(cv.ensure_list, [cv.string]),
         vol.Optional(CONF_LIMIT): cv.positive_int,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     }
 )
+
+
+async def test_opendata(opendata):
+    """Test wheather the current configuration allows to get data."""
+
+    try:
+        data = await opendata.async_get_data()
+        _LOGGER.debug(data)
+    except OpendataTransportError:
+        _LOGGER.error(
+            "Check at http://transport.opendata.ch/examples/stationboard.html "
+            "if your station names are valid"
+        )
+        return False
+    return True
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
@@ -55,21 +73,84 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     name = config.get(CONF_NAME)
     start = config.get(CONF_START)
     destination = config.get(CONF_DESTINATION)
+    stationboard = config.get(CONF_STATIONBOARD)
     limit = config.get(CONF_LIMIT, 5)
 
     session = async_get_clientsession(hass)
-    opendata = OpendataTransport(start, destination, hass.loop, session, limit)
 
-    try:
-        await opendata.async_get_data()
-    except OpendataTransportError:
-        _LOGGER.error(
-            "Check at http://transport.opendata.ch/examples/stationboard.html "
-            "if your station names are valid"
+    entities = []
+
+    if start is not None and destination is not None:
+        opendata = OpendataTransport(start, destination, hass.loop, session, limit)
+        if await test_opendata(opendata):
+            entities.append(SwissPublicTransportSensor(opendata, name))
+
+    if stationboard is not None:
+        opendata = OpendataTransportStationboard(
+            stationboard, hass.loop, session, limit
         )
-        return
+        if await test_opendata(opendata):
+            entities.append(SwissPublicTransportStationboardSensor(opendata, name))
 
-    async_add_entities([SwissPublicTransportSensor(opendata, name)])
+    async_add_entities(entities)
+
+
+class SwissPublicTransportStationboardSensor(Entity):
+    """Implementation of an Swiss public transport stationboard sensor."""
+
+    def __init__(self, opendata, name):
+        """Initialize the sensor."""
+        self._opendata = opendata
+        self._name = name
+        self._remaining_time = ""
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return (
+            self._opendata.journeys[0]["departure"]
+            if self._opendata is not None and len(self._opendata.journeys) > 0
+            else None
+        )
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        if self._opendata is None or len(self._opendata.journeys) < 1:
+            return
+
+        self._remaining_time = dt_util.parse_datetime(
+            self._opendata.journeys[0]["departure"]
+        ) - dt_util.as_local(dt_util.utcnow())
+
+        attr = {
+            ATTR_DEPARTURES: self._opendata.journeys,
+            ATTR_TRAIN_NUMBER: self._opendata.journeys[0]["number"],
+            ATTR_PLATFORM: self._opendata.journeys[0]["platform"],
+            ATTR_REMAINING_TIME: f"{self._remaining_time}",
+            ATTR_ATTRIBUTION: ATTRIBUTION,
+        }
+        return attr
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return ICON
+
+    async def async_update(self):
+        """Get the latest data from opendata.ch and update the states."""
+        _LOGGER.info("async_update called")
+        try:
+            if self._remaining_time.total_seconds() < 0:
+                self._opendata.journeys = []
+                await self._opendata.async_get_data()
+        except OpendataTransportError:
+            _LOGGER.error("Unable to retrieve data from transport.opendata.ch")
 
 
 class SwissPublicTransportSensor(Entity):
