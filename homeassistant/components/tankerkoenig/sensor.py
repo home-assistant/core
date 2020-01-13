5fc7e3d1d14f482587c404943ecb52e26323c688
Guillem Pagès <git@guillempages.cat>
2020-01-13 21:25:05 +0100

    Separate address into different fields

--- homeassistant/components/tankerkoenig/sensor.py
+++ homeassistant/components/tankerkoenig/sensor.py
@@ -16,11 +16,14 @@ from .const import DOMAIN, NAME
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_ADDRESS = "address"
 ATTR_BRAND = "brand"
+ATTR_CITY = "city"
 ATTR_FUEL_TYPE = "fuel_type"
+ATTR_HOUSE_NUMBER = "house_number"
 ATTR_IS_OPEN = "is_open"
+ATTR_POSTCODE = "postcode"
 ATTR_STATION_NAME = "station_name"
+ATTR_STREET = "street"
 ATTRIBUTION = "Data provided by https://creativecommons.tankerkoenig.de"
 
 ICON = "mdi:gas-station"
@@ -45,7 +48,10 @@ class FuelPriceSensor(Entity):
         self._latitude = station["lat"]
         self._longitude = station["lng"]
         self._is_open = STATE_OPEN if station["isOpen"] else STATE_CLOSED
-        self._address = f"{station['street']} {station['houseNumber']}, {station['postCode']} {station['place']}"
+        self._city = station["place"]
+        self._house_number = station["houseNumber"]
+        self._postcode = station["postCode"]
+        self._street = station["street"]
         _LOGGER.debug("Setup sensor %s", name)
         self._price = station[fuel_type]
 
@@ -77,7 +83,10 @@ class FuelPriceSensor(Entity):
             ATTR_BRAND: self._station["brand"],
             ATTR_FUEL_TYPE: self._fuel_type,
             ATTR_STATION_NAME: self._station["name"],
-            ATTR_ADDRESS: self._address,
+            ATTR_STREET: self._street,
+            ATTR_HOUSE_NUMBER: self._house_number,
+            ATTR_POSTCODE: self._postcode,
+            ATTR_CITY: self._city,
             ATTR_LATITUDE: self._latitude,
             ATTR_LONGITUDE: self._longitude,
             ATTR_IS_OPEN: self._is_open,
