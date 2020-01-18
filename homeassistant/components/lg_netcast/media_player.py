e75969f8757251c4487157162caa287073e9c80c
Laszlo Jakab <6196882+laszlojakab@users.noreply.github.com>
2020-01-18 08:53:18 +0100

    add timestamp to media_image_url to update image correctly

--- homeassistant/components/lg_netcast/media_player.py
+++ homeassistant/components/lg_netcast/media_player.py
@@ -1,5 +1,5 @@
 """Support for LG TV running on NetCast 3 or 4."""
-from datetime import timedelta
+from datetime import timedelta, datetime
 import logging
 
 from pylgnetcast import LgNetCastClient, LgNetCastError
@@ -185,7 +185,7 @@ class LgTVDevice(MediaPlayerDevice):
     @property
     def media_image_url(self):
         """URL for obtaining a screen capture."""
-        return self._client.url + "data?target=screen_image"
+        return self._client.url + "data?target=screen_image&_=" + str(int(datetime.now().timestamp()))
 
     def turn_off(self):
         """Turn off media player."""
