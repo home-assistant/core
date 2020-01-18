2880a963d6b859fce16acd47b7181859e5fd7ab8
Ron Klinkien <ron@cyberjunky.nl>
2020-01-18 17:23:01 +0000

    Async and ConfigFlow code

--- homeassistant/components/garmin_connect/__init__.py
+++ homeassistant/components/garmin_connect/__init__.py
@@ -1,1 +1,122 @@
 """The Garmin Connect integration."""
+import asyncio
+import logging
+import voluptuous as vol
+from datetime import date
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.util import Throttle
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_ID, CONF_NAME
+
+from .const import DOMAIN, MIN_TIME_BETWEEN_UPDATES
+
+from garminconnect import (
+    Garmin,
+    GarminConnectConnectionError,
+    GarminConnectTooManyRequestsError,
+    GarminConnectAuthenticationError,
+)
+
+_LOGGER = logging.getLogger(__name__)
+PLATFORMS = ["sensor"]
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Garmin Connect component."""
+
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Garmin Connect from a config entry."""
+    username = entry.data[CONF_USERNAME]
+    password = entry.data[CONF_PASSWORD]
+
+    try:
+        garmin_client = Garmin(username, password)
+    except GarminConnectConnectionError:
+        _LOGGER.error("Connection error occured during Garmin Connect Client init")
+        return False
+    except GarminConnectAuthenticationError:
+        _LOGGER.error("Authentication error occured during Garmin Connect Client init")
+        return False
+    except GarminConnectTooManyRequestsError:
+        _LOGGER.error(
+            "Too many requests error occured during Garmin Connect Client init"
+        )
+        return False
+    except Exception:  # pylint: disable=broad-except
+        _LOGGER.exception("Unknown error occured during Garmin Connect Client init")
+        return False
+
+    garmin_data = GarminConnectClient(garmin_client)
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = garmin_data
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    await garmin_data.async_update()
+    # async def _interval_update(now=None) -> None:
+    #     """Update Twente Milieu data."""
+    #     await _update_twentemilieu(hass, unique_id)
+
+    # async_track_time_interval(hass, _interval_update, SCAN_INTERVAL)
+
+    # if hass.config.units.name == CONF_UNIT_SYSTEM_IMPERIAL:
+    #     unit_system = CONF_UNIT_SYSTEM_IMPERIAL
+    # else:
+    #     unit_system = CONF_UNIT_SYSTEM_METRIC
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
+
+class GarminConnectClient:
+    """Set up the Garmin Connect client."""
+
+    def __init__(self, client):
+        """Initialize the client."""
+        self.client = client
+        self.data = None
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    async def async_update(self):
+        """Fetch the latest data."""
+        today = date.today()
+        try:
+            self.data = self.client.fetch_stats(today.isoformat())
+        except GarminConnectConnectionError:
+            _LOGGER.error("Connection error occured during Garmin Connect Client init")
+            return
+        except GarminConnectAuthenticationError:
+            _LOGGER.error(
+                "Authentication error occured during Garmin Connect Client init"
+            )
+            return
+        except GarminConnectTooManyRequestsError:
+            _LOGGER.error(
+                "Too many requests error occured during Garmin Connect Client init"
+            )
+            return
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unknown error occured during Garmin Connect Client init")
+            return
---
+++ homeassistant/components/garmin_connect/config_flow.py
@@ -0,0 +1,94 @@
+"""Config flow for Garmin Connect integration."""
+import logging
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.const import (
+    CONF_ID,
+    CONF_PASSWORD,
+    CONF_USERNAME,
+    CONF_NAME,
+)
+from .const import DOMAIN
+from garminconnect import (
+    Garmin,
+    GarminConnectConnectionError,
+    GarminConnectTooManyRequestsError,
+    GarminConnectAuthenticationError,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@config_entries.HANDLERS.register(DOMAIN)
+class GarminConnectConfigFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Garmin Connect."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    async def _show_setup_form(self, errors=None):
+        """Show the setup form to the user."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                    vol.Optional(CONF_NAME): str,
+                }
+            ),
+            errors=errors or {},
+        )
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        if user_input is None:
+            return await self._show_setup_form(user_input)
+
+        errors = {}
+        try:
+            garmin = Garmin(user_input[CONF_USERNAME], user_input[CONF_PASSWORD],)
+        except GarminConnectConnectionError:
+            errors["base"] = "cannot_connect"
+            return await self._show_setup_form(errors)
+        except GarminConnectAuthenticationError:
+            errors["base"] = "invalid_auth"
+            return await self._show_setup_form(errors)
+        except GarminConnectTooManyRequestsError:
+            errors["base"] = "too_many_requests"
+            return await self._show_setup_form(errors)
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+            return await self._show_setup_form(errors)
+
+        try:
+            unique_id = garmin.unique_id()
+        except GarminConnectConnectionError:
+            errors["base"] = "cannot_connect"
+            return await self._show_setup_form(errors)
+        except GarminConnectAuthenticationError:
+            errors["base"] = "invalid_auth"
+            return await self._show_setup_form(errors)
+        except GarminConnectTooManyRequestsError:
+            errors["base"] = "too_many_requests"
+            return await self._show_setup_form(errors)
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+            return await self._show_setup_form(errors)
+
+        entries = self._async_current_entries()
+        for entry in entries:
+            if entry.data[CONF_ID] == unique_id:
+                return self.async_abort(reason="already_setup")
+
+        return self.async_create_entry(
+            title=unique_id,
+            data={
+                CONF_ID: unique_id,
+                CONF_USERNAME: user_input[CONF_USERNAME],
+                CONF_PASSWORD: user_input[CONF_PASSWORD],
+                CONF_NAME: user_input[CONF_NAME],
+            },
+        )
