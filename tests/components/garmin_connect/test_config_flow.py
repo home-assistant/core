db3f8569e7559de338e4382ca4f48fb631d0178d
Ron Klinkien <ron@cyberjunky.nl>
2020-01-20 19:43:55 +0000

    Start working test file

--- tests/components/garmin_connect/test_config_flow.py
+++ tests/components/garmin_connect/test_config_flow.py
@@ -1,89 +1,14 @@
 """Test the Garmin Connect config flow."""
-from asynctest import patch
-
-from homeassistant import config_entries, setup
-from homeassistant.components.garmin_connect.config_flow import (
-    CannotConnect,
-    InvalidAuth,
-)
-from homeassistant.components.garmin_connect.const import DOMAIN
+from homeassistant import data_entry_flow
+from homeassistant.components.garmin_connect import config_flow
 
 
-async def test_form(hass):
-    """Test we get the form."""
-    await setup.async_setup_component(hass, "persistent_notification", {})
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert result["type"] == "form"
-    assert result["errors"] == {}
+async def test_show_form(hass):
+    """Test that the form is served with no input."""
+    flow = config_flow.GarminConnectConfigFlowHandler()
+    flow.hass = hass
 
-    with patch(
-        "homeassistant.components.garmin_connect.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.garmin_connect.async_setup", return_value=True
-    ) as mock_setup, patch(
-        "homeassistant.components.garmin_connect.async_setup_entry", return_value=True,
-    ) as mock_setup_entry:
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
+    result = await flow.async_step_user(user_input=None)
 
-    assert result2["type"] == "create_entry"
-    assert result2["title"] == "Garmin Connect"
-    assert result2["data"] == {
-        "username": "test-username",
-        "password": "test-password",
-    }
-    await hass.async_block_till_done()
-    assert len(mock_setup.mock_calls) == 1
-    assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_invalid_auth(hass):
-    """Test we handle invalid auth."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.garmin_connect.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_auth"}
-
-
-async def test_form_cannot_connect(hass):
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.garmin_connect.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "cannot_connect"}
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "user"
