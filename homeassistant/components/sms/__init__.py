c8493ff6377106dd2787ac72c0c7c2aa29bd3a8c
Oscar Calvo <oscar@calvonet.com>
2020-01-21 09:12:23 -0800

    Update doc comments

--- homeassistant/components/sms/__init__.py
+++ homeassistant/components/sms/__init__.py
@@ -1,19 +1,23 @@
 """The sms component."""
+import logging
+
+import gammu
+
+from homeassistant.const import CONF_DEVICE
 
 DOMAIN = "sms"
 
-import logging
-import gammu
-from homeassistant.const import CONF_DEVICE
 
 _LOGGER = logging.getLogger(__name__)
 
 STATE_MACHINE = gammu.StateMachine()
 CONF_PHONE_NUMBER = "phone_number"
 
+
 async def async_setup(hass, config):
+    """Configure Gammu state machine."""
     conf = config[DOMAIN]
     device = conf.get(CONF_DEVICE)
-    STATE_MACHINE.SetConfig(0,dict(Device = device, Connection = 'at'))
+    STATE_MACHINE.SetConfig(0, dict(Device=device, Connection="at"))
     STATE_MACHINE.Init()
     return True
--- homeassistant/components/sms/notify.py
+++ homeassistant/components/sms/notify.py
@@ -1,8 +1,5 @@
 """Support for SMS notification services."""
 import logging
-import gammu
-from . import STATE_MACHINE
-from . import CONF_PHONE_NUMBER
 
 import voluptuous as vol
 
@@ -10,16 +7,20 @@ from homeassistant.components.notify import PLATFORM_SCHEMA, BaseNotificationSer
 from homeassistant.const import CONF_NAME
 import homeassistant.helpers.config_validation as cv
 
+from . import CONF_PHONE_NUMBER, STATE_MACHINE
+
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {vol.Required(CONF_PHONE_NUMBER): cv.string, vol.Optional(CONF_NAME): cv.string}
 )
 
+
 def get_service(hass, config, discovery_info=None):
     """Get the SMS notification service."""
     number = config[CONF_PHONE_NUMBER]
     return SMSNotificationService(number)
+
 
 class SMSNotificationService(BaseNotificationService):
     """Implement the notification service for SMS."""
@@ -33,8 +34,8 @@ class SMSNotificationService(BaseNotificationService):
         # Prepare message data
         # We tell that we want to use first SMSC number stored in phone
         gammu_message = {
-            'Text': message,
-            'SMSC': {'Location': 1},
-            'Number': self.number,
+            "Text": message,
+            "SMSC": {"Location": 1},
+            "Number": self.number,
         }
         STATE_MACHINE.SendSMS(gammu_message)
