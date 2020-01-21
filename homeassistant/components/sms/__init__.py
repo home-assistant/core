b804a5f8378a776d1135803e7760750983616de8
Oscar Calvo <oscar@calvonet.com>
2020-01-20 16:26:23 -0800

    Remove comment

--- homeassistant/components/sms/__init__.py
+++ homeassistant/components/sms/__init__.py
@@ -12,8 +12,6 @@ STATE_MACHINE = gammu.StateMachine()
 CONF_PHONE_NUMBER = "phone_number"
 
 async def async_setup(hass, config):
-    # Create object for talking with phone
-    #device = '/dev/ttyUSB2'
     conf = config[DOMAIN]
     device = conf.get(CONF_DEVICE)
     STATE_MACHINE.SetConfig(0,dict(Device = device, Connection = 'at'))
