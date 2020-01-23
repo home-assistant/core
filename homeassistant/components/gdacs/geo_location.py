5d930193cec69b47042d8fe878df05dc353800fc
Malte Franken <exxamalte@users.noreply.github.com>
2020-01-24 08:52:49 +1100

    improved entity name for earthquakes

--- homeassistant/components/gdacs/geo_location.py
+++ homeassistant/components/gdacs/geo_location.py
@@ -137,7 +137,11 @@ class GdacsEvent(GeolocationEvent):
 
     def _update_from_feed(self, feed_entry):
         """Update the internal state from the provided feed entry."""
-        self._title = f"{feed_entry.event_type}: {feed_entry.event_name}"
+        event_name = feed_entry.event_name
+        if not event_name:
+            # Earthquakes usually don't have an event name.
+            event_name = f"{feed_entry.country} ({feed_entry.event_id})"
+        self._title = f"{feed_entry.event_type}: {event_name}"
         # Convert distance if not metric system.
         if self._unit_system == CONF_UNIT_SYSTEM_IMPERIAL:
             self._distance = IMPERIAL_SYSTEM.length(
