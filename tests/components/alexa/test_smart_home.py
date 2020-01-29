910148a0adea2d2c72aff8a4dc9154dc70eb25b3
ochlocracy <5885236+ochlocracy@users.noreply.github.com>
2020-01-28 21:58:39 -0500

    Update Comment.

--- tests/components/alexa/test_smart_home.py
+++ tests/components/alexa/test_smart_home.py
@@ -2520,7 +2520,7 @@ async def test_mode_unsupported_domain(hass):
 
 
 async def test_cover(hass):
-    """Test cover discovery and powerController """
+    """Test garage cover discovery and powerController."""
     device = (
         "cover.test",
         "off",
