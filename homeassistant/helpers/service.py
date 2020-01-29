0185f8af3a7b089f35f416aa8482f375155ff876
Paulus Schoutsen <balloob@gmail.com>
2020-01-29 13:38:54 -0800

    Fix tests

--- homeassistant/helpers/service.py
+++ homeassistant/helpers/service.py
@@ -376,7 +376,7 @@ async def _handle_service_platform_call(
 
         if asyncio.iscoroutine(result):
             _LOGGER.error(
-                "Service %s for %s incorrectly returns a coroutine object. Await result instead in service handler.",
+                "Service %s for %s incorrectly returns a coroutine object. Await result instead in service handler. Report bug to component author.",
                 func,
                 entity.entity_id,
             )
