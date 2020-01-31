3bb44827740b87eb2ef0d423f14d8eac4892b3d6
Malte Franken <exxamalte@users.noreply.github.com>
2020-01-31 21:22:52 +1100

    simplified test code

--- tests/components/gdacs/test_init.py
+++ tests/components/gdacs/test_init.py
@@ -1,12 +1,7 @@
 """Define tests for the GDACS general setup."""
 from asynctest import CoroutineMock, patch
 
-from homeassistant.components.gdacs import (
-    DOMAIN,
-    FEED,
-    async_setup_entry,
-    async_unload_entry,
-)
+from homeassistant.components.gdacs import DOMAIN, FEED
 
 
 async def test_component_unload_config_entry(hass, config_entry):
@@ -16,11 +11,11 @@ async def test_component_unload_config_entry(hass, config_entry):
         "aio_georss_gdacs.GdacsFeedManager.update", new_callable=CoroutineMock
     ) as mock_feed_manager_update:
         # Load config entry.
-        assert await async_setup_entry(hass, config_entry)
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
         assert mock_feed_manager_update.call_count == 1
         assert hass.data[DOMAIN][FEED][config_entry.entry_id] is not None
         # Unload config entry.
-        assert await async_unload_entry(hass, config_entry)
+        assert await hass.config_entries.async_unload(config_entry.entry_id)
         await hass.async_block_till_done()
         assert hass.data[DOMAIN][FEED].get(config_entry.entry_id) is None
