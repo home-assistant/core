0c922045979f448f5633a9580bb9e91602d46d4c
Malte Franken <exxamalte@users.noreply.github.com>
2020-01-31 22:18:18 +1100

    changed code structure

--- homeassistant/components/gdacs/sensor.py
+++ homeassistant/components/gdacs/sensor.py
@@ -89,11 +89,10 @@ class GdacsSensor(Entity):
         self._last_update = (
             dt.as_utc(status_info.last_update) if status_info.last_update else None
         )
-        self._last_update_successful = (
-            dt.as_utc(status_info.last_update_successful)
-            if status_info.last_update_successful
-            else None
-        )
+        if status_info.last_update_successful:
+            self._last_update_successful = dt.as_utc(status_info.last_update_successful)
+        else:
+            self._last_update_successful = None
         self._last_timestamp = status_info.last_timestamp
         self._total = status_info.total
         self._created = status_info.created
