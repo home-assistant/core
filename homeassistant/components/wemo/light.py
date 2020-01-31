66baf8b6e38175bdad6ba44f13260dac9b15ebe4
Paulus Schoutsen <balloob@gmail.com>
2020-01-31 14:01:25 -0800

    Fix wemo device types for lights (#31360)

--- homeassistant/components/wemo/light.py
+++ homeassistant/components/wemo/light.py
@@ -114,7 +114,7 @@ class WemoLight(Light):
         return {
             "name": self.wemo.name,
             "identifiers": {(WEMO_DOMAIN, self.wemo.uniqueID)},
-            "model": self.wemo.model_name,
+            "model": self.wemo.device_type,
             "manufacturer": "Belkin",
         }
 
