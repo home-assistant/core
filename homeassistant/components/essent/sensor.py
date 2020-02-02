dc0bffadcc3da3de61a83adcbac9e9aa36853fd6
Arjan van Balken <arjanvanbalken@gmail.com>
2020-02-02 13:28:15 +0100

    Fix mix-up of sensor entities and their values

--- homeassistant/components/essent/sensor.py
+++ homeassistant/components/essent/sensor.py
@@ -1,5 +1,6 @@
 """Support for Essent API."""
 from datetime import timedelta
+from typing import Optional
 
 from pyessent import PyEssent
 import voluptuous as vol
@@ -91,6 +92,11 @@ class EssentMeter(Entity):
         self._type = meter_type
         self._tariff = tariff
         self._unit = unit
+
+    @property
+    def unique_id(self) -> Optional[str]:
+        """Return a unique ID."""
+        return f"{self._meter}-{self._type}-{self._tariff}"
 
     @property
     def name(self):
