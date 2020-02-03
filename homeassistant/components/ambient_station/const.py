ed810e0ac17e5639c3772b1d63f4a5aa967146f8
Aaron Bach <bachya1208@gmail.com>
2020-02-03 11:13:06 -0700

    Correct over-broad Ambient PWS data updates

--- homeassistant/components/ambient_station/const.py
+++ homeassistant/components/ambient_station/const.py
@@ -1,4 +1,6 @@
 """Define constants for the Ambient PWS component."""
+from functools import partial
+
 DOMAIN = "ambient_station"
 
 ATTR_LAST_DATA = "last_data"
@@ -8,7 +10,7 @@ CONF_APP_KEY = "app_key"
 
 DATA_CLIENT = "data_client"
 
-TOPIC_UPDATE = "update"
+TOPIC_UPDATE = partial("{domain}_update_{mac_address}", domain=DOMAIN)
 
 TYPE_BINARY_SENSOR = "binary_sensor"
 TYPE_SENSOR = "sensor"
