3f9fcac10e2b02d3cfff84fc9bd9b15c2e02125e
Bas Nijholt <basnijholt@gmail.com>
2020-02-03 18:26:44 +0100

    fix test_dataSet6

--- tests/components/derivative/test_sensor.py
+++ tests/components/derivative/test_sensor.py
@@ -43,7 +43,6 @@ async def _setup_sensor(hass, config):
         "platform": "derivative",
         "name": "power",
         "source": "sensor.energy",
-        "unit_time": "s",
         "round": 2,
     }
 
@@ -79,19 +78,21 @@ async def setup_tests(hass, config, times, values, expected_state):
 async def test_dataSet1(hass):
     """Test derivative sensor state."""
     times, values = zip(*[(20, 10), (30, 30), (40, 5), (50, 0)])
-    await setup_tests(hass, {}, times, values, expected_state=-0.5)
+    await setup_tests(hass, {"unit_time": "s"}, times, values, expected_state=-0.5)
 
 
 async def test_dataSet2(hass):
     """Test derivative sensor state."""
     times, values = zip(*[(20, 5), (30, 0)])
-    await setup_tests(hass, {}, times, values, expected_state=-0.5)
+    await setup_tests(hass, {"unit_time": "s"}, times, values, expected_state=-0.5)
 
 
 async def test_dataSet3(hass):
     """Test derivative sensor state."""
     times, values = zip(*[(20, 5), (30, 10)])
-    state = await setup_tests(hass, {}, times, values, expected_state=0.5)
+    state = await setup_tests(
+        hass, {"unit_time": "s"}, times, values, expected_state=0.5
+    )
 
     assert state.attributes.get("unit_of_measurement") == "/s"
 
@@ -99,18 +100,18 @@ async def test_dataSet3(hass):
 async def test_dataSet4(hass):
     """Test derivative sensor state."""
     times, values = zip(*[(20, 5), (30, 5)])
-    await setup_tests(hass, {}, times, values, expected_state=0)
+    await setup_tests(hass, {"unit_time": "s"}, times, values, expected_state=0)
 
 
 async def test_dataSet5(hass):
     """Test derivative sensor state."""
     times, values = zip(*[(20, 10), (30, -10)])
-    await setup_tests(hass, {}, times, values, expected_state=-2)
+    await setup_tests(hass, {"unit_time": "s"}, times, values, expected_state=-2)
 
 
 async def test_dataSet6(hass):
     """Test derivative sensor state."""
-    times, values = zip(*[(20, 0), (30, 36000)])
+    times, values = zip(*[(0, 0), (60, 1 / 60)])
     await setup_tests(hass, {}, times, values, expected_state=1)
 
 
@@ -122,9 +123,10 @@ async def test_data_moving_average_for_discrete_sensor(hass):
     # the temperature rounded down to an integer value.
 
     temperature_values = []
-    for minute in range(60):
-        temperature_values += [minute] * 60
+    for temperature in range(60):
+        temperature_values += [temperature] * 60
     time_window = 600
+
     times = list(range(len(temperature_values)))
     config, entity_id = await _setup_sensor(
         hass, {"time_window": {"seconds": time_window}, "unit_time": "min", "round": 1}
@@ -136,10 +138,11 @@ async def test_data_moving_average_for_discrete_sensor(hass):
             hass.states.async_set(entity_id, value, {}, force_update=True)
             await hass.async_block_till_done()
 
-        if time_window < time < len(times) - time_window:
+        if time_window < time < times[-1] - time_window:
             state = hass.states.get("sensor.power")
             derivative = round(float(state.state), config["sensor"]["round"])
-            # Test that the error is never more than 10%
+            # Test that the error is never more than
+            # (time_window_in_minutes / true_derivative * 100) =10%
             assert abs(1 - derivative) <= 0.1
 
 
