8297fdc77c7aebfa4c27e79ee2d0b5a330420602
Quentin POLLET <polletquentin74@me.com>
2020-02-04 12:47:25 +0100

    Review 4

--- tests/components/meteo_france/test_config_flow.py
+++ tests/components/meteo_france/test_config_flow.py
@@ -42,7 +42,7 @@ def mock_controller_client_2():
 async def test_user(hass, client_1):
     """Test user config."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}, data=None,
+        DOMAIN, context={"source": SOURCE_USER}
     )
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
