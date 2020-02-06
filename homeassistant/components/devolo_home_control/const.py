d7cf5bd7f16c6cc064c12716d0f4cc86de0d05aa
Markus Bong <2Fake1987@gmail.com>
2020-02-06 15:07:03 +0100

    moved information to const

--- homeassistant/components/devolo_home_control/const.py
+++ homeassistant/components/devolo_home_control/const.py
@@ -1,3 +1,6 @@
 """Constants for the devolo_home_control integration."""
 
 DOMAIN = "devolo_home_control"
+DEFAULT_MYDEVOLO = "https://www.mydevolo.com"
+DEFAULT_MPRM = "https://homecontrol.mydevolo.com"
+PLATFORMS = ["switch"]
