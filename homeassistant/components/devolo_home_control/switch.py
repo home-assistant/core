bf2ee50aa427a02d817005ddbaee20da8fa3bf89
Markus Bong <Markus.Bong@devolo.de>
2020-02-07 11:06:42 +0100

    configure domain

--- homeassistant/components/devolo_home_control/switch.py
+++ homeassistant/components/devolo_home_control/switch.py
@@ -1,6 +1,8 @@
 """Platform for light integration."""
 import logging
 
+from devolo_home_control_api.mprm_rest import get_sub_device_uid_from_element_uid
+
 from homeassistant.components.switch import SwitchDevice
 
 from .const import DOMAIN
@@ -15,37 +17,59 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     devices_list = []
     for device in devices:
-        for binary_switch_property in device.binary_switch_property:
+        if (
+            get_sub_device_uid_from_element_uid([*device.binary_switch_property][0])
+            is None
+        ):
+            # Normal binary switch device with one binary switch
             devices_list.append(
-                DevoloSwitch(
-                    device_instance=device,
-                    binary_switch_property=binary_switch_property,
-                    mprm=hass.data[DOMAIN]["mprm"],
-                )
+                DevoloSwitch(hass=hass, device_instance=device, sub_uid=None)
             )
+        else:
+            # Device with more than one binary switch
+            for i in range(1, len(device.binary_switch_property) + 1):
+                devices_list.append(
+                    DevoloSwitch(hass=hass, device_instance=device, sub_uid=i)
+                )
     add_entities(devices_list, False)
 
 
 class DevoloSwitch(SwitchDevice):
     """Representation of an Awesome Light."""
 
-    def __init__(self, device_instance, binary_switch_property, mprm):
+    def __init__(self, hass, device_instance, sub_uid):
         """Initialize an devolo Switch."""
         self._device_instance = device_instance
-        self._unique_id = binary_switch_property
-        self._mprm = mprm
-        self._name = device_instance.name
+        if sub_uid is not None:
+            binary_switch = (
+                "devolo.BinarySwitch:"
+                + self._device_instance.device_uid
+                + "#"
+                + str(sub_uid)
+            )
+            consumption_property = (
+                "devolo.Meter:" + self._device_instance.device_uid + "#" + str(sub_uid)
+            )
+        else:
+            binary_switch = "devolo.BinarySwitch:" + self._device_instance.device_uid
+            consumption_property = "devolo.Meter:" + self._device_instance.device_uid
+        self._unique_id = binary_switch
+        self._mprm = hass.data[DOMAIN]["mprm"]
+        self._name = self._device_instance.name
         self._binary_switch_property = self._device_instance.binary_switch_property.get(
-            binary_switch_property
+            binary_switch
         )
         self._is_on = self._binary_switch_property.state
-        try:
-            self._consumption = [*self._device_instance.consumption_property][0].current
-        except (IndexError, AttributeError):
-            # Not every binary switch device has a consumption
+        if hasattr(self._device_instance, "consumption_property"):
+            self._consumption = self._device_instance.consumption_property.get(
+                consumption_property
+            ).current
+        else:
             self._consumption = None
-        self.subscriber = Subscriber(self._device_instance.name, device=self)
-        self._mprm.publisher.register(self._device_instance.device_uid, self.subscriber)
+        self._subscriber = Subscriber(self._device_instance.name, device=self)
+        self._mprm.publisher.register(
+            self._device_instance.device_uid, self._subscriber
+        )
 
     @property
     def unique_id(self):
