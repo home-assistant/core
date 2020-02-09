f68795ea50407244aeba1a989e890f1741c2a051
shred86 <shred86@icloud.com>
2020-02-08 17:51:46 -0800

    Add test to test_switch.py

--- tests/components/abode/test_switch.py
+++ tests/components/abode/test_switch.py
@@ -1,7 +1,13 @@
 """Test for the Abode switch device."""
+import abodepy.helpers.constants as CONST
+
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 
 from .common import setup_platform
+
+from tests.common import load_fixture
+
+AUTOMATION_ID = "47fae27488f74f55b964a81a066c3a01"
 
 
 async def test_entity_registry(hass, requests_mock):
@@ -10,13 +16,30 @@ async def test_entity_registry(hass, requests_mock):
     entity_registry = await hass.helpers.entity_registry.async_get_registry()
 
     entry = entity_registry.async_get("switch.test_automation")
-    assert entry.unique_id == "47fae27488f74f55b964a81a066c3a01"
+    assert entry.unique_id == AUTOMATION_ID
 
 
-async def test_attributes(hass, requests_mock):
-    """Test the binary sensor attributes are correct."""
+async def test_automation_attributes(hass, requests_mock):
+    """Test the automation attributes are correct."""
     await setup_platform(hass, SWITCH_DOMAIN)
 
     state = hass.states.get("switch.test_automation")
     # State is set based on "enabled" key in automation JSON.
     assert state.state == "on"
+
+
+async def test_turn_automation_off(hass, requests_mock):
+    """Test the automation can be turned off."""
+    await setup_platform(hass, SWITCH_DOMAIN)
+    requests_mock.patch(
+        str.replace(CONST.AUTOMATION_ID_URL, "$AUTOMATIONID$", AUTOMATION_ID),
+        text=load_fixture("abode_automation_changed.json"),
+    )
+
+    await hass.services.async_call(
+        "switch", "turn_off", {"entity_id": "switch.test_automation"}, blocking=True
+    )
+    await hass.async_block_till_done()
+
+    state = hass.states.get("switch.test_automation")
+    assert state.state == "off"
