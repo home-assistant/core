6586d0d8cec8152e83b3629dd043356f34d45245
Marcus Terasa <>
2020-02-10 13:07:38 +0100

    Rewrite pushover notify to use pushover_complete library

--- homeassistant/components/pushover/notify.py
+++ homeassistant/components/pushover/notify.py
@@ -1,7 +1,7 @@
 """Pushover platform for notify component."""
 import logging
 
-from pushover import Pushover, RequestError
+from pushover_complete import PushoverAPI
 import requests
 import voluptuous as vol
 
@@ -19,6 +19,15 @@ import homeassistant.helpers.config_validation as cv
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_ATTACHMENT = "attachment"
+ATTR_URL = "url"
+ATTR_URL_TITLE = "url_title"
+ATTR_PRIORITY = "priority"
+ATTR_RETRY = "retry"
+ATTR_SOUND = "sound"
+ATTR_HTML = "html"
+ATTR_CALLBACK_URL = "callback_url"
+ATTR_EXPIRE = "expire"
+ATTR_TIMESTAMP = "timestamp"
 
 CONF_USER_KEY = "user_key"
 
@@ -42,26 +51,36 @@ class PushoverNotificationService(BaseNotificationService):
         self._hass = hass
         self._user_key = user_key
         self._api_token = api_token
-        self.pushover = Pushover(self._api_token)
+        self.pushover = PushoverAPI(self._api_token)
 
     def send_message(self, message="", **kwargs):
         """Send a message to a user."""
-        # Make a copy and use empty dict if necessary
+
+        # Extract params from data dict
+        title = kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT)
         data = dict(kwargs.get(ATTR_DATA) or {})
+        url = data.get(ATTR_URL, None)
+        url_title = data.get(ATTR_URL_TITLE, None)
+        priority = data.get(ATTR_PRIORITY, None)
+        retry = data.get(ATTR_PRIORITY, None)
+        expire = data.get(ATTR_EXPIRE, None)
+        callback_url = data.get(ATTR_CALLBACK_URL, None)
+        timestamp = data.get(ATTR_TIMESTAMP, None)
+        sound = data.get(ATTR_SOUND, None)
+        html = 1 if data.get(ATTR_HTML, False) else 0
 
-        data["title"] = kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT)
-
-        # Check for attachment.
-        if ATTR_ATTACHMENT in data:
+        image = data.get(ATTR_ATTACHMENT, None)
+        # Check for attachment
+        if image is not None:
             # If attachment is a URL, use requests to open it as a stream.
-            if data[ATTR_ATTACHMENT].startswith("http"):
+            if image.startswith("http"):
                 try:
                     response = requests.get(
                         data[ATTR_ATTACHMENT], stream=True, timeout=5
                     )
                     if response.status_code == 200:
                         # Replace the attachment identifier with file object.
-                        data[ATTR_ATTACHMENT] = response.content
+                        image = response.content
                     else:
                         _LOGGER.error(
                             "Failed to download image %s, response code: %d",
@@ -69,11 +88,11 @@ class PushoverNotificationService(BaseNotificationService):
                             response.status_code,
                         )
                         # Remove attachment key to send without attachment.
-                        del data[ATTR_ATTACHMENT]
+                        image = None
                 except requests.exceptions.RequestException as ex_val:
                     _LOGGER.error(ex_val)
-                    # Remove attachment key to try sending without attachment
-                    del data[ATTR_ATTACHMENT]
+                    # Remove attachment key to send without attachment.
+                    image = None
             else:
                 # Not a URL, check valid path first
                 if self._hass.config.is_allowed_path(data[ATTR_ATTACHMENT]):
@@ -81,15 +100,15 @@ class PushoverNotificationService(BaseNotificationService):
                     try:
                         file_handle = open(data[ATTR_ATTACHMENT], "rb")
                         # Replace the attachment identifier with file object.
-                        data[ATTR_ATTACHMENT] = file_handle
+                        image = file_handle
                     except OSError as ex_val:
                         _LOGGER.error(ex_val)
                         # Remove attachment key to send without attachment.
-                        del data[ATTR_ATTACHMENT]
+                        image = None
                 else:
                     _LOGGER.error("Path is not whitelisted")
                     # Remove attachment key to send without attachment.
-                    del data[ATTR_ATTACHMENT]
+                    image = None
 
         targets = kwargs.get(ATTR_TARGET)
 
@@ -97,12 +116,22 @@ class PushoverNotificationService(BaseNotificationService):
             targets = [targets]
 
         for target in targets:
-            if target is not None:
-                data["device"] = target
-
             try:
-                self.pushover.message(self._user_key, message, **data)
+                self.pushover.send_message(
+                    self._user_key,
+                    message,
+                    target,
+                    title,
+                    url,
+                    url_title,
+                    image,
+                    priority,
+                    retry,
+                    expire,
+                    callback_url,
+                    timestamp,
+                    sound,
+                    html,
+                )
             except ValueError as val_err:
                 _LOGGER.error(val_err)
-            except RequestError:
-                _LOGGER.exception("Could not send pushover notification")
