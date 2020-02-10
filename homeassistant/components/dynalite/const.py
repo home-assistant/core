7710925440fb88a72c20cb7ebfe235dc2ef174ae
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-02-10 23:16:04 +0200

    Dynalite Integration (#27841)
    * Initial commit
    
    * ran hassfest and gen_requirements_all scripts
    
    * fixes per request from Paulus Schoutsen
    
    * ran gen_requirements_all
    
    * updated library version - removed some debug leftover
    
    * get_requirements again...
    
    * added documentation URL
    
    * ran isort
    
    * changed storage in hass.data[DOMAIN] to use entry_id instead of host
    
    * adopted unit tests to latest fix
    
    * Update const.py
    
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

---
+++ homeassistant/components/dynalite/const.py
@@ -0,0 +1,11 @@
+"""Constants for the Dynalite component."""
+import logging
+
+LOGGER = logging.getLogger(__package__)
+DOMAIN = "dynalite"
+DATA_CONFIGS = "dynalite_configs"
+
+CONF_BRIDGES = "bridges"
+
+DEFAULT_NAME = "dynalite"
+DEFAULT_PORT = 12345
