760edcbeac5337a331e8b80859ed9af8fee6d6a4
Quentin POLLET <polletquentin74@me.com>
2020-02-10 13:46:03 +0100

    Review

--- tests/components/meteo_france/conftest.py
+++ tests/components/meteo_france/conftest.py
@@ -7,6 +7,10 @@ import pytest
 @pytest.fixture(autouse=True)
 def patch_requests():
     """Stub out services that makes requests."""
-    with patch("homeassistant.components.meteo_france.meteofranceClient"):
-        with patch("homeassistant.components.meteo_france.VigilanceMeteoFranceProxy"):
-            yield
+    patch_client = patch("homeassistant.components.meteo_france.meteofranceClient")
+    patch_weather_alert = patch(
+        "homeassistant.components.meteo_france.VigilanceMeteoFranceProxy"
+    )
+
+    with patch_client, patch_weather_alert:
+        yield
