ed99d82deb906ca7fb8b09d99b891d394ea1d632
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-02-14 11:49:41 +0000

    moved test_light to also use the core interfaces

--- tests/components/dynalite/test_light.py
+++ tests/components/dynalite/test_light.py
@@ -1,81 +1,84 @@
 """Test Dynalite light."""
-from unittest.mock import Mock, call, patch
+from unittest.mock import Mock, patch
 
-from homeassistant.components.dynalite import DOMAIN
-from homeassistant.components.dynalite.light import DynaliteLight, async_setup_entry
+import pytest
+
+from homeassistant.components import dynalite
 from homeassistant.components.light import SUPPORT_BRIGHTNESS
+from homeassistant.setup import async_setup_component
 
 from tests.common import mock_coro
 
 
-async def test_light_setup():
-    """Test a successful setup."""
-    hass = Mock()
-    entry = Mock()
-    async_add = Mock()
-    bridge = Mock()
-    hass.data = {DOMAIN: {entry.entry_id: bridge}}
-    await async_setup_entry(hass, entry, async_add)
-    bridge.register_add_devices.assert_called_once()
-    internal_func = bridge.register_add_devices.mock_calls[0][1][0]
+@pytest.fixture
+def mock_device(hass):
+    """Mock a Dynalite device."""
     device = Mock()
     device.category = "light"
-    internal_func([device])
-    async_add.assert_called_once()
-    assert async_add.mock_calls[0][1][0][0].device == device
+    device.unique_id = "UNIQUE"
+    device.name = "NAME"
+    device.device_info = {
+        "identifiers": {(dynalite.DOMAIN, device.unique_id)},
+        "name": device.name,
+        "manufacturer": "Dynalite",
+    }
+    return device
 
 
-async def test_light():
-    """Test the light entity."""
-    device = Mock()
-    device.async_turn_on = Mock(return_value=mock_coro(Mock()))
-    device.async_turn_off = Mock(return_value=mock_coro(Mock()))
-    bridge = Mock()
-    dyn_light = DynaliteLight(device, bridge)
-    assert dyn_light.name is device.name
-    assert dyn_light.unique_id is device.unique_id
-    assert dyn_light.available is device.available
-    assert dyn_light.hidden is device.hidden
-    await dyn_light.async_update()  # does nothing
-    assert dyn_light.device_info is device.device_info
-    assert dyn_light.brightness is device.brightness
-    assert dyn_light.is_on is device.is_on
-    await dyn_light.async_turn_on(aaa="bbb")
-    assert device.async_turn_on.mock_calls[0] == call(aaa="bbb")
-    await dyn_light.async_turn_off(ccc="ddd")
-    assert device.async_turn_off.mock_calls[0] == call(ccc="ddd")
-
-
-async def test_supported_features():
-    """Test supported feaures didn't change."""
-    device = Mock()
-    bridge = Mock()
-    dyn_light = DynaliteLight(device, bridge)
-    assert dyn_light.supported_features == SUPPORT_BRIGHTNESS
-
-
-async def test_added_to_ha():
-    """Test registration to dispatch when added."""
-
-    def temp_signal(device=None):
-        if device:
-            return "yes"
-        return "no"
-
-    device = Mock()
-    bridge = Mock()
-    bridge.update_signal = temp_signal
-    dyn_light = DynaliteLight(device, bridge)
-    async_dispatch = Mock()
-    with patch(
-        "homeassistant.components.dynalite.light.async_dispatcher_connect",
-        async_dispatch,
+async def create_light_from_device(hass, device):
+    """Set up the component and platform and create a light based on the device provided."""
+    host = "1.2.3.4"
+    with patch.object(
+        dynalite.bridge.DynaliteDevices, "async_setup", return_value=mock_coro(True)
     ):
-        await dyn_light.async_added_to_hass()
-        assert async_dispatch.call_count == 2
-        assert async_dispatch.mock_calls[0] == call(
-            dyn_light.hass, temp_signal("aaa"), dyn_light.async_schedule_update_ha_state
+        assert (
+            await async_setup_component(
+                hass,
+                dynalite.DOMAIN,
+                {
+                    dynalite.DOMAIN: {
+                        dynalite.CONF_BRIDGES: [{dynalite.CONF_HOST: host}]
+                    }
+                },
+            )
+            is True
         )
-        assert async_dispatch.mock_calls[1] == call(
-            dyn_light.hass, temp_signal(), dyn_light.async_schedule_update_ha_state
-        )
+    await hass.async_block_till_done()
+    # Find the bridge
+    bridge = None
+    for index in hass.data[dynalite.DOMAIN]:
+        if index != dynalite.DATA_CONFIGS:
+            bridge = hass.data[dynalite.DOMAIN][index]
+    bridge.dynalite_devices.newDeviceFunc([device])
+    await hass.async_block_till_done()
+
+
+async def test_light_setup(hass, mock_device):
+    """Test a successful setup."""
+    await create_light_from_device(hass, mock_device)
+    entity_state = hass.states.get("light.name")
+    assert entity_state.attributes["hidden"] == mock_device.hidden
+    assert entity_state.attributes["brightness"] == mock_device.brightness
+    assert entity_state.attributes["supported_features"] == SUPPORT_BRIGHTNESS
+
+
+async def test_turn_on(hass, mock_device):
+    """Test turning a light on."""
+    mock_device.async_turn_on.return_value = mock_coro(True)
+    await create_light_from_device(hass, mock_device)
+    await hass.services.async_call(
+        "light", "turn_on", {"entity_id": "light.name"}, blocking=True
+    )
+    await hass.async_block_till_done()
+    mock_device.async_turn_on.assert_called_once()
+
+
+async def test_turn_off(hass, mock_device):
+    """Test turning a light off."""
+    mock_device.async_turn_off.return_value = mock_coro(True)
+    await create_light_from_device(hass, mock_device)
+    await hass.services.async_call(
+        "light", "turn_off", {"entity_id": "light.name"}, blocking=True
+    )
+    await hass.async_block_till_done()
+    mock_device.async_turn_off.assert_called_once()
