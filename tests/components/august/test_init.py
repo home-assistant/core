63ead83e698a826140a06755880870a888a3c790
J. Nick Koston <nick@koston.org>
2020-02-15 15:35:12 +0000

    Do not mock the lock

--- tests/components/august/test_init.py
+++ tests/components/august/test_init.py
@@ -1,4 +1,5 @@
 """The tests for the august platform."""
+import asyncio
 from unittest.mock import MagicMock
 
 from homeassistant.components import august
@@ -13,12 +14,10 @@ async def test__refresh_access_token(hass):
     """Set up things to be run when tests are started."""
     authentication = _mock_august_authentication("original_token", 1234)
     authenticator = _mock_august_authenticator()
+    token_refresh_lock = asyncio.Lock()
+
     data = august.AugustData(
-        hass,
-        MagicMock(name="api"),
-        authentication,
-        authenticator,
-        MagicMock(name="token_refresh_lock"),
+        hass, MagicMock(name="api"), authentication, authenticator, token_refresh_lock
     )
     await data._async_refresh_access_token_if_needed()
     authenticator.refresh_access_token.assert_not_called()
