4c966712460d7519d2a174b9cb97309ec60d3990
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-02-15 00:00:12 +0000

    added break in loop

--- tests/components/dynalite/test_light.py
+++ tests/components/dynalite/test_light.py
@@ -9,7 +9,6 @@ from homeassistant.components.light import SUPPORT_BRIGHTNESS
 from homeassistant.setup import async_setup_component
 
 
-
 @pytest.fixture
 def mock_device(hass):
     """Mock a Dynalite device."""
@@ -29,19 +28,18 @@ async def create_light_from_device(hass, device):
     """Set up the component and platform and create a light based on the device provided."""
     host = "1.2.3.4"
     with patch(
-        "homeassistant.components.dynalite.bridge.DynaliteDevices.async_setup", return_value=True
+        "homeassistant.components.dynalite.bridge.DynaliteDevices.async_setup",
+        return_value=True,
     ):
         with patch.object(dynalite.DynaliteBridge, "try_connection", return_value=True):
-            assert (
-                await async_setup_component(
-                    hass,
-                    dynalite.DOMAIN,
-                    {
-                        dynalite.DOMAIN: {
-                            dynalite.CONF_BRIDGES: [{dynalite.CONF_HOST: host}]
-                        }
-                    },
-                )
+            assert await async_setup_component(
+                hass,
+                dynalite.DOMAIN,
+                {
+                    dynalite.DOMAIN: {
+                        dynalite.CONF_BRIDGES: [{dynalite.CONF_HOST: host}]
+                    }
+                },
             )
     await hass.async_block_till_done()
     # Find the bridge
@@ -49,6 +47,7 @@ async def create_light_from_device(hass, device):
     for index in hass.data[dynalite.DOMAIN]:
         if index != dynalite.DATA_CONFIGS:
             bridge = hass.data[dynalite.DOMAIN][index]
+            break
     bridge.dynalite_devices.newDeviceFunc([device])
     await hass.async_block_till_done()
 
