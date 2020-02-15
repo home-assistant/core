f98f232de2ba9c4c13a4366616a659110bb891f4
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-02-15 18:16:37 +0000

    moved CONFIG_SCHEMA into component

--- homeassistant/components/dynalite/__init__.py
+++ homeassistant/components/dynalite/__init__.py
@@ -1,5 +1,19 @@
 """Support for the Dynalite networks."""
-from dynalite_devices_lib import BRIDGE_CONFIG_SCHEMA
+from dynalite_devices_lib import (
+    CONF_ACTIVE,
+    CONF_AREA,
+    CONF_AUTODISCOVER,
+    CONF_CHANNEL,
+    CONF_DEFAULT,
+    CONF_FADE,
+    CONF_HIDDENENTITY,
+    CONF_LOGLEVEL,
+    CONF_NAME,
+    CONF_POLLTIMER,
+    CONF_PORT,
+    DEFAULT_NAME,
+    DEFAULT_PORT,
+)
 import voluptuous as vol
 
 from homeassistant import config_entries
@@ -10,14 +24,61 @@ from homeassistant.helpers import config_validation as cv
 from .bridge import DynaliteBridge
 from .const import CONF_BRIDGES, CONF_NOWAIT, DATA_CONFIGS, DOMAIN, LOGGER
 
-EXT_BRIDGE_SCHEMA = BRIDGE_CONFIG_SCHEMA.extend(
-    {vol.Optional(CONF_NOWAIT): vol.Coerce(bool)}
+
+def num_string(value):
+    """Test if value is a string of digits, aka an integer."""
+    new_value = str(value)
+    if new_value.isdigit():
+        return new_value
+    raise vol.Invalid("Not a string with numbers")
+
+
+CHANNEL_DATA_SCHEMA = vol.Schema(
+    vol.All(
+        {
+            vol.Optional(CONF_NAME): str,
+            vol.Optional(CONF_FADE): vol.Coerce(float),
+            vol.Optional(CONF_HIDDENENTITY, default=False): vol.Coerce(bool),
+        }
+    )
+)
+
+CHANNEL_SCHEMA = vol.Schema({num_string: vol.Any(CHANNEL_DATA_SCHEMA, None)})
+
+AREA_DATA_SCHEMA = vol.Schema(
+    vol.All(
+        {
+            vol.Required(CONF_NAME): str,
+            vol.Optional(CONF_FADE): vol.Coerce(float),
+            vol.Optional(CONF_CHANNEL): CHANNEL_SCHEMA,
+        },
+    )
+)
+
+AREA_SCHEMA = vol.Schema({num_string: vol.Any(AREA_DATA_SCHEMA, None)})
+
+PLATFORM_DEFAULTS_SCHEMA = vol.Schema({vol.Optional(CONF_FADE): vol.Coerce(float)})
+
+
+BRIDGE_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
+        vol.Required(CONF_HOST): str,
+        vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
+        vol.Optional(CONF_LOGLEVEL): str,
+        vol.Optional(CONF_AUTODISCOVER, default=False): vol.Coerce(bool),
+        vol.Optional(CONF_POLLTIMER, default=1.0): vol.Coerce(float),
+        vol.Optional(CONF_AREA): AREA_SCHEMA,
+        vol.Optional(CONF_DEFAULT): PLATFORM_DEFAULTS_SCHEMA,
+        vol.Optional(CONF_ACTIVE, default=False): vol.Coerce(bool),
+        vol.Optional(CONF_NOWAIT): vol.Coerce(bool),
+    }
 )
 
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
-            {vol.Optional(CONF_BRIDGES): vol.All(cv.ensure_list, [EXT_BRIDGE_SCHEMA])}
+            {vol.Optional(CONF_BRIDGES): vol.All(cv.ensure_list, [BRIDGE_SCHEMA])}
         )
     },
     extra=vol.ALLOW_EXTRA,
