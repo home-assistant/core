8e7667f4ec26898b61727c9e617ba2cee45176d4
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-02-17 17:32:22 +0200

    Update tests/components/dynalite/test_light.py
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/dynalite/test_light.py
+++ tests/components/dynalite/test_light.py
@@ -30,7 +30,7 @@ async def create_light_from_device(hass, device):
     with patch(
         "homeassistant.components.dynalite.bridge.DynaliteDevices.async_setup",
         return_value=True,
-    ), patch.object(dynalite.DynaliteBridge, "try_connection", return_value=True):
+    ), patch("homeassistant.components.dynalite.bridge.DynaliteDevices.available", True):
         assert await async_setup_component(
             hass,
             dynalite.DOMAIN,
