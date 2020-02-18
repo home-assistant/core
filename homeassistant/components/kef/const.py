be91da78569a1030924a6df971bfda452c835eee
Bas Nijholt <basnijholt@gmail.com>
2020-02-18 23:40:54 +0100

    add homeassistant/components/kef/const.py

---
+++ homeassistant/components/kef/const.py
@@ -0,0 +1,10 @@
+"""Constants for the KEF component."""
+DOMAIN = "kef"
+
+SERVICE_MODE = "set_mode"
+SERVICE_DESK_DB = "set_desk_db"
+SERVICE_WALL_DB = "set_wall_db"
+SERVICE_TREBLE_DB = "set_treble_db"
+SERVICE_HIGH_HZ = "set_high_hz"
+SERVICE_LOW_HZ = "set_low_hz"
+SERVICE_SUB_DB = "set_sub_db"
