565cc761d17003dd40e9089e83079cfbfca7fb75
J. Nick Koston <nick@koston.org>
2020-02-18 00:21:15 +0000

    Audit state handling of august bridges and sensors
    This addresses issue #29980
    
    * Prevent setting up august locks that do not have a bridge as they will never work
    
    * Prevent locks showing available when their bridge is offline
    
    * Prevent door sensors from showing available when their bridge is offline
    
    * Prevent creating door sensors for locks that do not have them
    
    * Prevent doorbells showing unavailable when they are in standby mode
    
    * Set SCAN_INTERVAL for binary_sensors to 5 seconds as
      data comes in from the activity endpoint more frequently

--- tests/components/august/test_init.py
+++ tests/components/august/test_init.py
@@ -1,5 +1,6 @@
 """The tests for the august platform."""
 import asyncio
+import unittest.mock
 from unittest.mock import MagicMock
 
 from homeassistant.components import august
@@ -11,6 +12,10 @@ from tests.components.august.mocks import (
     _mock_august_authentication,
     _mock_august_authenticator,
     _mock_august_lock,
+    _mock_doorsense_enabled_august_lock_detail,
+    _mock_doorsense_missing_august_lock_detail,
+    _mock_inoperative_august_lock_detail,
+    _mock_operative_august_lock_detail,
 )
 
 
@@ -19,7 +24,7 @@ def test_get_lock_name():
     data = MockAugustComponentData(last_lock_status_update_timestamp=1)
     lock = _mock_august_lock()
     data.set_mocked_locks([lock])
-    assert data.get_lock_name("mockdeviceid1") == "Mocked Lock 1"
+    assert data.get_lock_name("mocklockid1") == "mocklockid1 Name"
 
 
 def test_unlock_throws_august_api_http_error():
@@ -29,11 +34,12 @@ def test_unlock_throws_august_api_http_error():
     data.set_mocked_locks([lock])
     last_err = None
     try:
-        data.unlock("mockdeviceid1")
+        data.unlock("mocklockid1")
     except HomeAssistantError as err:
         last_err = err
     assert (
-        str(last_err) == "Mocked Lock 1: This should bubble up as its user consumable"
+        str(last_err)
+        == "mocklockid1 Name: This should bubble up as its user consumable"
     )
 
 
@@ -44,12 +50,39 @@ def test_lock_throws_august_api_http_error():
     data.set_mocked_locks([lock])
     last_err = None
     try:
-        data.unlock("mockdeviceid1")
+        data.unlock("mocklockid1")
     except HomeAssistantError as err:
         last_err = err
     assert (
-        str(last_err) == "Mocked Lock 1: This should bubble up as its user consumable"
+        str(last_err)
+        == "mocklockid1 Name: This should bubble up as its user consumable"
     )
+
+
+class TestAugustData(unittest.TestCase):
+    """Tests for AugustData."""
+
+    def test_inoperative_locks_are_filtered_out(self):
+        """Ensure inoperative locks do not get setup."""
+        august_operative_lock = _mock_operative_august_lock_detail("oplockid1")
+        data = _create_august_data_with_lock_details(
+            [august_operative_lock, _mock_inoperative_august_lock_detail("inoplockid1")]
+        )
+
+        self.assertEqual(len(data.locks), 1)
+        self.assertEqual(data.locks[0].device_id, "oplockid1")
+
+    def test_lock_has_doorsense(self):
+        """Check to see if a lock has doorsense."""
+        data = _create_august_data_with_lock_details(
+            [
+                _mock_doorsense_enabled_august_lock_detail("doorsenselock1"),
+                _mock_doorsense_missing_august_lock_detail("nodoorsenselock1"),
+            ]
+        )
+
+        self.assertTrue(data.lock_has_doorsense("doorsenselock1"))
+        self.assertFalse(data.lock_has_doorsense("nodoorsenselock1"))
 
 
 async def test__refresh_access_token(hass):
@@ -72,3 +105,20 @@ async def test__refresh_access_token(hass):
     authenticator.refresh_access_token.assert_called()
     assert data._access_token == "new_token"
     assert data._access_token_expires == 5678
+
+
+def _create_august_data_with_lock_details(lock_details):
+    locks = []
+    for lock in lock_details:
+        locks.append(_mock_august_lock(lock.device_id))
+    authentication = _mock_august_authentication("original_token", 1234)
+    authenticator = _mock_august_authenticator()
+    token_refresh_lock = MagicMock()
+    api = MagicMock()
+    api.get_lock_status = MagicMock(return_value=(MagicMock(), MagicMock()))
+    api.get_lock_detail = MagicMock(side_effect=lock_details)
+    api.get_operable_locks = MagicMock(return_value=locks)
+    api.get_doorbells = MagicMock(return_value=[])
+    return august.AugustData(
+        MagicMock(), api, authentication, authenticator, token_refresh_lock
+    )
