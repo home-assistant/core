5e48880f7ae7063a2b0619b0e9e9b0bf32b328c6
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-02-22 00:29:59 +0200

    Enhance Dynalite Integration after review (#31760)
    * fixes per Martin Hjelmare
    
    * pylint fix
    
    * final fixes per request
    
    * fixed unit tests for new config flow
    
    * Added unit-tests to increase coverage. at 97% now
    
    * Added unit tests for 100% coverage of component
    
    * removed configured_host function and updated config_flow unit tests
    
    * added a pylint directive since it tells me by mistake DOMAIN is not used
    
    * fixed path (removed __init__)
    
    * Update homeassistant/components/dynalite/light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update homeassistant/components/dynalite/light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * fixed the test as we moved from schedule_update_... to async_schedule
    
    * Update homeassistant/components/dynalite/bridge.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * removed context from config_flow
    changed test_init to use the core methods
    
    * moved test_light to also use the core interfaces
    
    * moved to config_entries.async_unload
    
    * additional fixes for the tests
    
    * pylint fix and removed unnecessary code
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * added break in loop
    
    * removed last mock_coro reference
    pylint fix
    
    * added coverage for try_connect
    
    * added check for a successful connection before bridge.async_setup succeeds
    also added a "nowait" config option (default False) that avoids this check
    
    * changed log level
    
    * fixed accidental chmod I did
    
    * fixed accidental change
    
    * not storing config in bridge
    
    * not patching asyncio
    
    * moved CONFIG_SCHEMA into component
    
    * moved all logs to start capitalized (and revised some of them)
    
    * moved test_config_flow to not patch the DynaliteBridge
    
    * also took DynaliteBridge patching out of test_init
    
    * removed NO_WAIT
    
    * fixes to SCHEMA
    
    * changed _ in multi-word CONF
    moved imports to component const.py
    
    * removed tries
    
    * removed redundant tests
    
    * fixed some small change i broke in the library. only version update
    
    * fixed rewuirements
    
    * Update tests/components/dynalite/test_config_flow.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_light.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/dynalite/test_config_flow.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * removed HIDDEN_ENTITY
    removed hass in test fixture
    
    * black fixes
    
    * removed final piece of hidden_entity from light
    fix in the library
    updated config flow so if the entry is already set but with a different config, calls async_update_entry
    
    * removed DATA_CONFIGS - no longer necessary
    
    * pylint fixes
    
    * added coverage
    
    * use abort in config_flow
    
    * test update
    
    * removed logs
    
    * test that update actually updates the entry
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/dynalite/test_light.py
+++ tests/components/dynalite/test_light.py
@@ -1,44 +1,78 @@
 """Test Dynalite light."""
-from unittest.mock import Mock, call, patch
+from unittest.mock import Mock
 
-from homeassistant.components.dynalite import DOMAIN
-from homeassistant.components.dynalite.light import DynaliteLight, async_setup_entry
+from asynctest import CoroutineMock, patch
+import pytest
 
-from tests.common import mock_coro
-
-
-async def test_light_setup():
-    """Test a successful setup."""
-    hass = Mock()
-    entry = Mock()
-    async_add = Mock()
-    bridge = Mock()
-    hass.data = {DOMAIN: {entry.entry_id: bridge}}
-    await async_setup_entry(hass, entry, async_add)
-    bridge.register_add_entities.assert_called_once()
-    assert bridge.register_add_entities.mock_calls[0] == call(async_add)
+from homeassistant.components import dynalite
+from homeassistant.components.light import SUPPORT_BRIGHTNESS
+from homeassistant.setup import async_setup_component
 
 
-async def test_light():
-    """Test the light entity."""
+@pytest.fixture
+def mock_device():
+    """Mock a Dynalite device."""
     device = Mock()
-    device.async_turn_on = Mock(return_value=mock_coro(Mock()))
-    device.async_turn_off = Mock(return_value=mock_coro(Mock()))
-    bridge = Mock()
-    dyn_light = DynaliteLight(device, bridge)
-    assert dyn_light.name is device.name
-    assert dyn_light.unique_id is device.unique_id
-    assert dyn_light.available is device.available
-    assert dyn_light.hidden is device.hidden
-    await dyn_light.async_update()  # does nothing
-    assert dyn_light.device_info is device.device_info
-    assert dyn_light.brightness is device.brightness
-    assert dyn_light.is_on is device.is_on
-    await dyn_light.async_turn_on(aaa="bbb")
-    assert device.async_turn_on.mock_calls[0] == call(aaa="bbb")
-    await dyn_light.async_turn_off(ccc="ddd")
-    assert device.async_turn_off.mock_calls[0] == call(ccc="ddd")
-    with patch.object(dyn_light, "hass"):
-        with patch.object(dyn_light, "schedule_update_ha_state") as update_ha:
-            dyn_light.try_schedule_ha()
-            update_ha.assert_called_once()
+    device.category = "light"
+    device.unique_id = "UNIQUE"
+    device.name = "NAME"
+    device.device_info = {
+        "identifiers": {(dynalite.DOMAIN, device.unique_id)},
+        "name": device.name,
+        "manufacturer": "Dynalite",
+    }
+    return device
+
+
+async def create_light_from_device(hass, device):
+    """Set up the component and platform and create a light based on the device provided."""
+    host = "1.2.3.4"
+    with patch(
+        "homeassistant.components.dynalite.bridge.DynaliteDevices.async_setup",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.dynalite.bridge.DynaliteDevices.available", True
+    ):
+        assert await async_setup_component(
+            hass,
+            dynalite.DOMAIN,
+            {dynalite.DOMAIN: {dynalite.CONF_BRIDGES: [{dynalite.CONF_HOST: host}]}},
+        )
+    await hass.async_block_till_done()
+    # Find the bridge
+    bridge = None
+    assert len(hass.data[dynalite.DOMAIN]) == 1
+    key = next(iter(hass.data[dynalite.DOMAIN]))
+    bridge = hass.data[dynalite.DOMAIN][key]
+    bridge.dynalite_devices.newDeviceFunc([device])
+    await hass.async_block_till_done()
+
+
+async def test_light_setup(hass, mock_device):
+    """Test a successful setup."""
+    await create_light_from_device(hass, mock_device)
+    entity_state = hass.states.get("light.name")
+    assert entity_state.attributes["brightness"] == mock_device.brightness
+    assert entity_state.attributes["supported_features"] == SUPPORT_BRIGHTNESS
+
+
+async def test_turn_on(hass, mock_device):
+    """Test turning a light on."""
+    mock_device.async_turn_on = CoroutineMock(return_value=True)
+    await create_light_from_device(hass, mock_device)
+    await hass.services.async_call(
+        "light", "turn_on", {"entity_id": "light.name"}, blocking=True
+    )
+    await hass.async_block_till_done()
+    mock_device.async_turn_on.assert_awaited_once()
+
+
+async def test_turn_off(hass, mock_device):
+    """Test turning a light off."""
+    mock_device.async_turn_off = CoroutineMock(return_value=True)
+    await create_light_from_device(hass, mock_device)
+    await hass.services.async_call(
+        "light", "turn_off", {"entity_id": "light.name"}, blocking=True
+    )
+    await hass.async_block_till_done()
+    mock_device.async_turn_off.assert_awaited_once()
