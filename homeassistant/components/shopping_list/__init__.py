eb10938f5d0e5acf5158af583c6a7aa0d5c48b83
Mike <22148848+thegame3202@users.noreply.github.com>
2020-02-21 21:02:30 -0600

    Update __init__.py

--- homeassistant/components/shopping_list/__init__.py
+++ homeassistant/components/shopping_list/__init__.py
@@ -5,24 +5,19 @@ import uuid
 
 import voluptuous as vol
 
-from homeassistant.const import HTTP_NOT_FOUND, HTTP_BAD_REQUEST
-from homeassistant.core import callback
-from homeassistant.components import http
+from homeassistant.components import http, websocket_api
 from homeassistant.components.http.data_validator import RequestDataValidator
-from homeassistant.helpers import intent
+from homeassistant.const import HTTP_BAD_REQUEST, HTTP_NOT_FOUND
+from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util.json import load_json, save_json
-from homeassistant.components import websocket_api
 
 ATTR_NAME = "name"
 
 DOMAIN = "shopping_list"
 _LOGGER = logging.getLogger(__name__)
 CONFIG_SCHEMA = vol.Schema({DOMAIN: {}}, extra=vol.ALLOW_EXTRA)
-EVENT_UPDATE = "shopping_list_updated"
-EVENT_ITEM_ADD = "shopping_list_item_added"
-INTENT_ADD_ITEM = "HassShoppingListAddItem"
-INTENT_LAST_ITEMS = "HassShoppingListLastItems"
+EVENT = "shopping_list_updated"
 ITEM_UPDATE_SCHEMA = vol.Schema({"complete": bool, ATTR_NAME: str})
 PERSISTENCE = ".shopping_list.json"
 
@@ -86,9 +81,6 @@ def async_setup(hass, config):
 
     data = hass.data[DOMAIN] = ShoppingData(hass)
     yield from data.async_load()
-
-    intent.async_register(hass, AddItemIntent())
-    intent.async_register(hass, ListTopItemsIntent())
 
     hass.services.async_register(
         DOMAIN, SERVICE_ADD_ITEM, add_item_service, schema=SERVICE_ITEM_SCHEMA
@@ -161,62 +153,18 @@ class ShoppingData:
         self.items = [itm for itm in self.items if not itm["complete"]]
         self.hass.async_add_job(self.save)
 
-    @asyncio.coroutine
-    def async_load(self):
+    async def async_load(self):
         """Load items."""
 
         def load():
             """Load the items synchronously."""
             return load_json(self.hass.config.path(PERSISTENCE), default=[])
 
-        self.items = yield from self.hass.async_add_job(load)
+        self.items = await self.hass.async_add_executor_job(load)
 
     def save(self):
         """Save the items."""
         save_json(self.hass.config.path(PERSISTENCE), self.items)
-
-
-class AddItemIntent(intent.IntentHandler):
-    """Handle AddItem intents."""
-
-    intent_type = INTENT_ADD_ITEM
-    slot_schema = {"item": cv.string}
-
-    @asyncio.coroutine
-    def async_handle(self, intent_obj):
-        """Handle the intent."""
-        slots = self.async_validate_slots(intent_obj.slots)
-        item = slots["item"]["value"]
-        intent_obj.hass.data[DOMAIN].async_add(item)
-
-        response = intent_obj.create_response()
-        response.async_set_speech(f"I've added {item} to your shopping list")
-        intent_obj.hass.bus.async_fire(EVENT_ITEM_ADD, {"item_added": item})
-        return response
-
-
-class ListTopItemsIntent(intent.IntentHandler):
-    """Handle AddItem intents."""
-
-    intent_type = INTENT_LAST_ITEMS
-    slot_schema = {"item": cv.string}
-
-    @asyncio.coroutine
-    def async_handle(self, intent_obj):
-        """Handle the intent."""
-        items = intent_obj.hass.data[DOMAIN].items[-5:]
-        response = intent_obj.create_response()
-
-        if not items:
-            response.async_set_speech("There are no items on your shopping list")
-        else:
-            response.async_set_speech(
-                "These are the top {} items on your shopping list: {}".format(
-                    min(len(items), 5),
-                    ", ".join(itm["name"] for itm in reversed(items)),
-                )
-            )
-        return response
 
 
 class ShoppingListView(http.HomeAssistantView):
@@ -243,7 +191,7 @@ class UpdateShoppingListItemView(http.HomeAssistantView):
 
         try:
             item = request.app["hass"].data[DOMAIN].async_update(item_id, data)
-            request.app["hass"].bus.async_fire(EVENT_UPDATE)
+            request.app["hass"].bus.async_fire(EVENT)
             return self.json(item)
         except KeyError:
             return self.json_message("Item not found", HTTP_NOT_FOUND)
@@ -262,7 +210,7 @@ class CreateShoppingListItemView(http.HomeAssistantView):
     def post(self, request, data):
         """Create a new shopping list item."""
         item = request.app["hass"].data[DOMAIN].async_add(data["name"])
-        request.app["hass"].bus.async_fire(EVENT_UPDATE)
+        request.app["hass"].bus.async_fire(EVENT,{"action":"item_added", "item":item})
         return self.json(item)
 
 
@@ -277,7 +225,7 @@ class ClearCompletedItemsView(http.HomeAssistantView):
         """Retrieve if API is running."""
         hass = request.app["hass"]
         hass.data[DOMAIN].async_clear_completed()
-        hass.bus.async_fire(EVENT_UPDATE)
+        hass.bus.async_fire(EVENT,{"action":"cleared_completed"})
         return self.json_message("Cleared completed items.")
 
 
@@ -293,7 +241,7 @@ def websocket_handle_items(hass, connection, msg):
 def websocket_handle_add(hass, connection, msg):
     """Handle add item to shopping_list."""
     item = hass.data[DOMAIN].async_add(msg["name"])
-    hass.bus.async_fire(EVENT_ITEM_ADD, {"item_added": item})
+    hass.bus.async_fire(EVENT,{"action":"item_added", "item":item})
     connection.send_message(websocket_api.result_message(msg["id"], item))
 
 
@@ -307,7 +255,7 @@ async def websocket_handle_update(hass, connection, msg):
 
     try:
         item = hass.data[DOMAIN].async_update(item_id, data)
-        hass.bus.async_fire(EVENT_UPDATE)
+        hass.bus.async_fire(EVENT,{"action":"item_updated", "item":item})
         connection.send_message(websocket_api.result_message(msg_id, item))
     except KeyError:
         connection.send_message(
@@ -319,5 +267,5 @@ async def websocket_handle_update(hass, connection, msg):
 def websocket_handle_clear(hass, connection, msg):
     """Handle clearing shopping_list items."""
     hass.data[DOMAIN].async_clear_completed()
-    hass.bus.async_fire(EVENT_UPDATE)
+    hass.bus.async_fire(EVENT,{"action":"cleared_completed"})
     connection.send_message(websocket_api.result_message(msg["id"]))
