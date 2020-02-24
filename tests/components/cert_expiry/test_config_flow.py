bfa2ffb296f47748cfe3d940c486e9fc9aa89074
Jason Lawrence <jjlawren@users.noreply.github.com>
2020-02-23 23:44:20 -0600

    Rewrite tests to use config flow core interfaces, validate created sensors

--- tests/components/cert_expiry/test_config_flow.py
+++ tests/components/cert_expiry/test_config_flow.py
@@ -6,10 +6,8 @@ from unittest.mock import patch
 from asynctest import patch as asyncpatch
 
 from homeassistant import data_entry_flow
-from homeassistant.components.cert_expiry import config_flow
-from homeassistant.components.cert_expiry.const import DEFAULT_PORT
-from homeassistant.config_entries import SOURCE_IMPORT, SOURCE_USER
-from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
+from homeassistant.components.cert_expiry.const import DEFAULT_PORT, DOMAIN
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT, STATE_UNAVAILABLE
 
 from tests.common import MockConfigEntry
 
@@ -19,95 +17,173 @@ GET_CERT_METHOD = "homeassistant.components.cert_expiry.helper.get_cert"
 GET_CERT_TIME_METHOD = (
     "homeassistant.components.cert_expiry.config_flow.get_cert_time_to_expiry"
 )
-
-
-def init_config_flow(hass, source=SOURCE_USER):
-    """Init a configuration flow."""
-    flow = config_flow.CertexpiryConfigFlow()
-    flow.hass = hass
-    flow.context = {"source": source}
-    return flow
+GET_SENSOR_CERT_TIME_METHOD = (
+    "homeassistant.components.cert_expiry.sensor.get_cert_time_to_expiry"
+)
 
 
 async def test_user(hass):
     """Test user config."""
-    flow = init_config_flow(hass)
-
-    result = await flow.async_step_user()
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": "user"}
+    )
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
 
-    # test with all provided
-    with asyncpatch(GET_CERT_TIME_METHOD, return_value=1):
-        result = await flow.async_step_user({CONF_HOST: HOST, CONF_PORT: PORT})
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == HOST
-    assert result["data"][CONF_HOST] == HOST
-    assert result["data"][CONF_PORT] == PORT
-
-
-async def test_user_with_bad_cert(hass):
-    """Test user config."""
-    flow = init_config_flow(hass)
-
-    result = await flow.async_step_user()
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "user"
-
-    # import with certification validation error
-    with patch(GET_CERT_METHOD, side_effect=ssl.SSLError("some error")):
-        result = await flow.async_step_user({CONF_HOST: HOST, CONF_PORT: PORT})
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == HOST
-    assert result["data"][CONF_HOST] == HOST
-    assert result["data"][CONF_PORT] == PORT
-
-
-async def test_import(hass):
-    """Test import step."""
-    flow = init_config_flow(hass, source=SOURCE_IMPORT)
-
-    # import with only host
-    with asyncpatch(GET_CERT_TIME_METHOD, return_value=1):
-        result = await flow.async_step_import({CONF_HOST: HOST})
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == HOST
-    assert result["data"][CONF_HOST] == HOST
-    assert result["data"][CONF_PORT] == DEFAULT_PORT
-
-    # import with host and port
-    with asyncpatch(GET_CERT_TIME_METHOD, return_value=1):
-        result = await flow.async_step_import({CONF_HOST: HOST, CONF_PORT: PORT})
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == HOST
-    assert result["data"][CONF_HOST] == HOST
-    assert result["data"][CONF_PORT] == PORT
-
-    # import with host and non-default port
-    with asyncpatch(GET_CERT_TIME_METHOD, return_value=1):
-        result = await flow.async_step_import({CONF_HOST: HOST, CONF_PORT: 888})
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == f"{HOST}:888"
-    assert result["data"][CONF_HOST] == HOST
-    assert result["data"][CONF_PORT] == 888
-
-    # import legacy config with name
-    with asyncpatch(GET_CERT_TIME_METHOD, return_value=1):
-        result = await flow.async_step_import(
-            {CONF_NAME: "legacy", CONF_HOST: HOST, CONF_PORT: PORT}
+    with asyncpatch(GET_CERT_TIME_METHOD):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input={CONF_HOST: HOST, CONF_PORT: PORT}
         )
     assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result["title"] == HOST
     assert result["data"][CONF_HOST] == HOST
     assert result["data"][CONF_PORT] == PORT
+    assert result["result"].unique_id == f"{HOST}:{PORT}"
+
+    with asyncpatch(GET_SENSOR_CERT_TIME_METHOD, return_value=100):
+        await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.cert_expiry_example_com")
+    assert state is not None
+    assert state.state != STATE_UNAVAILABLE
+    assert state.state == "100"
+    assert state.attributes.get("error") == "None"
+    assert state.attributes.get("is_valid")
+
+
+async def test_user_with_bad_cert(hass):
+    """Test user config with bad certificate."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": "user"}
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "user"
+
+    with patch(GET_CERT_METHOD, side_effect=ssl.SSLError("some error")):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input={CONF_HOST: HOST, CONF_PORT: PORT}
+        )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == HOST
+    assert result["data"][CONF_HOST] == HOST
+    assert result["data"][CONF_PORT] == PORT
+    assert result["result"].unique_id == f"{HOST}:{PORT}"
+
+    with patch(GET_CERT_METHOD, side_effect=ssl.SSLError("some error")):
+        await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.cert_expiry_example_com")
+    assert state is not None
+    assert state.state != STATE_UNAVAILABLE
+    assert state.state == "0"
+    assert state.attributes.get("error") == "some error"
+    assert not state.attributes.get("is_valid")
+
+
+async def test_import_host_only(hass):
+    """Test import with host only."""
+    with asyncpatch(GET_CERT_TIME_METHOD, return_value=1):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": "import"}, data={CONF_HOST: HOST}
+        )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == HOST
+    assert result["data"][CONF_HOST] == HOST
+    assert result["data"][CONF_PORT] == DEFAULT_PORT
+    assert result["result"].unique_id == f"{HOST}:{DEFAULT_PORT}"
+
+    with asyncpatch(GET_SENSOR_CERT_TIME_METHOD, return_value=100):
+        await hass.async_block_till_done()
+    state = hass.states.get("sensor.cert_expiry_example_com")
+    assert state is not None
+    assert state.state != STATE_UNAVAILABLE
+    assert state.attributes.get("error") == "None"
+    assert state.attributes.get("is_valid")
+    assert state.state == "100"
+
+
+async def test_import_host_and_port(hass):
+    """Test import with host and port."""
+    with asyncpatch(GET_CERT_TIME_METHOD, return_value=1):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": "import"},
+            data={CONF_HOST: HOST, CONF_PORT: PORT},
+        )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == HOST
+    assert result["data"][CONF_HOST] == HOST
+    assert result["data"][CONF_PORT] == PORT
+    assert result["result"].unique_id == f"{HOST}:{PORT}"
+
+    with asyncpatch(GET_SENSOR_CERT_TIME_METHOD, return_value=100):
+        await hass.async_block_till_done()
+    state = hass.states.get("sensor.cert_expiry_example_com")
+    assert state is not None
+    assert state.state != STATE_UNAVAILABLE
+    assert state.attributes.get("error") == "None"
+    assert state.attributes.get("is_valid")
+    assert state.state == "100"
+
+
+async def test_import_non_default_port(hass):
+    """Test import with host and non-default port."""
+    with asyncpatch(GET_CERT_TIME_METHOD):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": "import"}, data={CONF_HOST: HOST, CONF_PORT: 888}
+        )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == f"{HOST}:888"
+    assert result["data"][CONF_HOST] == HOST
+    assert result["data"][CONF_PORT] == 888
+    assert result["result"].unique_id == f"{HOST}:888"
+
+    with asyncpatch(GET_SENSOR_CERT_TIME_METHOD, return_value=100):
+        await hass.async_block_till_done()
+    state = hass.states.get("sensor.cert_expiry_example_com_888")
+    assert state is not None
+    assert state.state != STATE_UNAVAILABLE
+    assert state.attributes.get("error") == "None"
+    assert state.attributes.get("is_valid")
+    assert state.state == "100"
+
+
+async def test_import_with_name(hass):
+    """Test import with name (deprecated)."""
+    with asyncpatch(GET_CERT_TIME_METHOD, return_value=1):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": "import"},
+            data={CONF_NAME: "legacy", CONF_HOST: HOST, CONF_PORT: PORT},
+        )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == HOST
+    assert result["data"][CONF_HOST] == HOST
+    assert result["data"][CONF_PORT] == PORT
+    assert result["result"].unique_id == f"{HOST}:{PORT}"
+
+    with asyncpatch(GET_SENSOR_CERT_TIME_METHOD, return_value=100):
+        await hass.async_block_till_done()
+    state = hass.states.get("sensor.cert_expiry_example_com")
+    assert state is not None
+    assert state.state != STATE_UNAVAILABLE
+    assert state.attributes.get("error") == "None"
+    assert state.attributes.get("is_valid")
+    assert state.state == "100"
 
 
 async def test_bad_import(hass):
     """Test import step."""
-    flow = init_config_flow(hass, source=SOURCE_IMPORT)
-
     with patch(GET_CERT_METHOD, side_effect=ConnectionRefusedError()):
-        result = await flow.async_step_import({CONF_HOST: HOST})
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": "import"}, data={CONF_HOST: HOST}
+        )
+
     assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
     assert result["reason"] == "import_failed"
 
@@ -115,45 +191,47 @@ async def test_bad_import(hass):
 async def test_abort_if_already_setup(hass):
     """Test we abort if the cert is already setup."""
     MockConfigEntry(
-        domain="cert_expiry", data={CONF_PORT: DEFAULT_PORT, CONF_HOST: HOST},
+        domain="cert_expiry",
+        data={CONF_HOST: HOST, CONF_PORT: PORT},
+        unique_id=f"{HOST}:{PORT}",
     ).add_to_hass(hass)
 
-    # Test with import source
-    flow = init_config_flow(hass, source=SOURCE_IMPORT)
-
-    # Should fail, same HOST and PORT (default)
-    result = await flow.async_step_import({CONF_HOST: HOST, CONF_PORT: DEFAULT_PORT})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": "import"}, data={CONF_HOST: HOST, CONF_PORT: PORT}
+    )
     assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
-    assert result["reason"] == "host_port_exists"
+    assert result["reason"] == "already_configured"
 
-    # Test with user source
-    flow = init_config_flow(hass)
-
-    result = await flow.async_step_user()
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "user"
-
-    # Should fail, same HOST and PORT (default)
-    result = await flow.async_step_user({CONF_HOST: HOST, CONF_PORT: DEFAULT_PORT})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": "user"}, data={CONF_HOST: HOST, CONF_PORT: PORT}
+    )
     assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
-    assert result["reason"] == "host_port_exists"
+    assert result["reason"] == "already_configured"
 
 
 async def test_abort_on_socket_failed(hass):
     """Test we abort of we have errors during socket creation."""
-    flow = init_config_flow(hass)
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": "user"}
+    )
 
     with patch(GET_CERT_METHOD, side_effect=socket.gaierror()):
-        result = await flow.async_step_user({CONF_HOST: HOST})
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input={CONF_HOST: HOST}
+        )
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["errors"] == {CONF_HOST: "resolve_failed"}
 
     with patch(GET_CERT_METHOD, side_effect=socket.timeout()):
-        result = await flow.async_step_user({CONF_HOST: HOST})
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input={CONF_HOST: HOST}
+        )
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["errors"] == {CONF_HOST: "connection_timeout"}
 
     with patch(GET_CERT_METHOD, side_effect=ConnectionRefusedError):
-        result = await flow.async_step_user({CONF_HOST: HOST})
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input={CONF_HOST: HOST}
+        )
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["errors"] == {CONF_HOST: "connection_refused"}
