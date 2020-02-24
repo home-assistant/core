97a2c94d5671f775936e18226151e7241ccb2a92
Aaron Bach <bachya1208@gmail.com>
2020-02-23 21:01:16 -0700

    Clean up RainMachine config entry data

--- tests/components/rainmachine/test_config_flow.py
+++ tests/components/rainmachine/test_config_flow.py
@@ -71,6 +71,7 @@ async def test_step_import(hass):
         CONF_PASSWORD: "password",
         CONF_PORT: 8080,
         CONF_SSL: True,
+        CONF_SCAN_INTERVAL: 60,
     }
 
     flow = config_flow.RainMachineFlowHandler()
@@ -100,6 +101,7 @@ async def test_step_user(hass):
         CONF_PASSWORD: "password",
         CONF_PORT: 8080,
         CONF_SSL: True,
+        CONF_SCAN_INTERVAL: 60,
     }
 
     flow = config_flow.RainMachineFlowHandler()
