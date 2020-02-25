75c80ead8508c8d41c6143c0c7941fb2e93ccd01
springstan <46536646+springstan@users.noreply.github.com>
2020-02-25 21:26:13 +0100

    Apply suggestions from code review

--- homeassistant/components/emoncms/sensor.py
+++ homeassistant/components/emoncms/sensor.py
@@ -108,7 +108,9 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
         unit = elem.get("unit")
         if unit:
-            unit_of_measurement = config.get(CONF_UNIT_OF_MEASUREMENT)
+            unit_of_measurement = unit
+else:
+    unit_of_measurement = config.get(CONF_UNIT_OF_MEASUREMENT)
 
         sensors.append(
             EmonCmsSensor(
