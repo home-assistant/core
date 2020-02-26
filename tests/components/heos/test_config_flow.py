5ce79283e8528977588a66345334c29426212bcb
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2020-02-26 22:43:39 +0900

    fix for the patch

--- tests/components/heos/test_config_flow.py
+++ tests/components/heos/test_config_flow.py
@@ -10,6 +10,8 @@ from homeassistant.components.heos.config_flow import HeosFlowHandler
 from homeassistant.components.heos.const import DATA_DISCOVERED_HOSTS
 from homeassistant.const import CONF_HOST
 
+from tests.common import mock_coro
+
 
 async def test_flow_aborts_already_setup(hass, config_entry):
     """Test flow aborts when entry already setup."""
@@ -49,14 +51,13 @@ async def test_cannot_connect_shows_error_form(hass, controller):
 async def test_create_entry_when_host_valid(hass, controller):
     """Test result type is create entry when host is valid."""
     data = {CONF_HOST: "127.0.0.1"}
-    result = await hass.config_entries.flow.async_init(
-        heos.DOMAIN, context={"source": "user"}, data=data
-    )
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == "Controller (127.0.0.1)"
-    assert result["data"] == data
-    await heos.async_setup_entry(hass, result)
-    with patch("homeassistant.components.hue.async_setup_entry"):
+    with patch("homeassistant.components.hue.async_setup_entry", return_value=mock_coro(True)):
+        result = await hass.config_entries.flow.async_init(
+            heos.DOMAIN, context={"source": "user"}, data=data
+        )
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result["title"] == "Controller (127.0.0.1)"
+        assert result["data"] == data
         assert controller.connect.call_count == 1
         assert controller.disconnect.call_count == 1
 
@@ -65,16 +66,16 @@ async def test_create_entry_when_friendly_name_valid(hass, controller):
     """Test result type is create entry when friendly name is valid."""
     hass.data[DATA_DISCOVERED_HOSTS] = {"Office (127.0.0.1)": "127.0.0.1"}
     data = {CONF_HOST: "Office (127.0.0.1)"}
-    result = await hass.config_entries.flow.async_init(
-        heos.DOMAIN, context={"source": "user"}, data=data
-    )
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == "Controller (127.0.0.1)"
-    assert result["data"] == {CONF_HOST: "127.0.0.1"}
-    with patch("homeassistant.components.hue.async_setup_entry"):
+    with patch("homeassistant.components.hue.async_setup_entry", return_value=mock_coro(True)):
+        result = await hass.config_entries.flow.async_init(
+            heos.DOMAIN, context={"source": "user"}, data=data
+        )
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result["title"] == "Controller (127.0.0.1)"
+        assert result["data"] == {CONF_HOST: "127.0.0.1"}
         assert controller.connect.call_count == 1
         assert controller.disconnect.call_count == 1
-    assert DATA_DISCOVERED_HOSTS not in hass.data
+        assert DATA_DISCOVERED_HOSTS not in hass.data
 
 
 async def test_discovery_shows_create_form(hass, controller, discovery_data):
