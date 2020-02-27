fcc368e7483347e7f1bc928a4a45bad821455e41
J. Nick Koston <nick@cpanel.net>
2020-02-26 16:48:44 -1000

    Centralize august activity updates (#32197)
    * Reduce August doorbell detail updates
    
    * Doorbell images now get updates from the activity feed
    
    * Tests for activity updates
    
    * py-august now provides bridge_is_online for available state
    
    * py-august now provides is_standby for available state
    
    * py-august now provides get_doorbell_image (eliminate requests)
    
    * remove debug
    
    * black after merge conflict
    
    * Centralize august activity updates
    
    * Updates appear significantly more responsive
    
    * Should address the community complaints about "lag"
    
    * Reduce detail updates (device end points) to one hour interval
    
    * Signal entities to update via dispatcher when new activity arrives
    
    * Resolves out of sync state (skipped test is now unskipped)
    
    * pylint
    
    * fix merge conflict
    
    * review comments
    
    * Remove stray
    
    * Address review items that can be done without refactor

--- homeassistant/components/august/const.py
+++ homeassistant/components/august/const.py
@@ -8,6 +8,8 @@ CONF_ACCESS_TOKEN_CACHE_FILE = "access_token_cache_file"
 CONF_LOGIN_METHOD = "login_method"
 CONF_INSTALL_ID = "install_id"
 
+AUGUST_DEVICE_UPDATE = "august_devices_update"
+
 VERIFICATION_CODE_KEY = "verification_code"
 
 NOTIFICATION_ID = "august_notification"
@@ -20,16 +22,14 @@ DATA_AUGUST = "data_august"
 DEFAULT_NAME = "August"
 DOMAIN = "august"
 
-# Limit battery, online, and hardware updates to 1800 seconds
+# Limit battery, online, and hardware updates to hourly
 # in order to reduce the number of api requests and
 # avoid hitting rate limits
-MIN_TIME_BETWEEN_DETAIL_UPDATES = timedelta(seconds=1800)
+MIN_TIME_BETWEEN_DETAIL_UPDATES = timedelta(hours=1)
 
 # Activity needs to be checked more frequently as the
 # doorbell motion and rings are included here
-MIN_TIME_BETWEEN_ACTIVITY_UPDATES = timedelta(seconds=10)
-
-DEFAULT_SCAN_INTERVAL = timedelta(seconds=10)
+ACTIVITY_UPDATE_INTERVAL = timedelta(seconds=10)
 
 LOGIN_METHODS = ["phone", "email"]
 
