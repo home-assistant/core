80b0d0f948a9f271d116598a2bc2553cfb151ddb
Paulus Schoutsen <balloob@gmail.com>
2020-02-26 16:43:34 -0800

    Lovelace resource mgmt (#32224)
    * Add websockets commands for resource management
    
    * Add LL resource management
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- tests/components/lovelace/test_resources.py
+++ tests/components/lovelace/test_resources.py
@@ -90,6 +90,67 @@ async def test_storage_resources_import(hass, hass_ws_client, hass_storage):
         not in hass_storage[dashboard.CONFIG_STORAGE_KEY_DEFAULT]["data"]["config"]
     )
 
+    # Add a resource
+    await client.send_json(
+        {
+            "id": 6,
+            "type": "lovelace/resources/create",
+            "res_type": "module",
+            "url": "/local/yo.js",
+        }
+    )
+    response = await client.receive_json()
+    assert response["success"]
+
+    await client.send_json({"id": 7, "type": "lovelace/resources"})
+    response = await client.receive_json()
+    assert response["success"]
+
+    last_item = response["result"][-1]
+    assert last_item["type"] == "module"
+    assert last_item["url"] == "/local/yo.js"
+
+    # Update a resource
+    first_item = response["result"][0]
+
+    await client.send_json(
+        {
+            "id": 8,
+            "type": "lovelace/resources/update",
+            "resource_id": first_item["id"],
+            "res_type": "css",
+            "url": "/local/updated.css",
+        }
+    )
+    response = await client.receive_json()
+    assert response["success"]
+
+    await client.send_json({"id": 9, "type": "lovelace/resources"})
+    response = await client.receive_json()
+    assert response["success"]
+
+    first_item = response["result"][0]
+    assert first_item["type"] == "css"
+    assert first_item["url"] == "/local/updated.css"
+
+    # Delete resources
+    await client.send_json(
+        {
+            "id": 10,
+            "type": "lovelace/resources/delete",
+            "resource_id": first_item["id"],
+        }
+    )
+    response = await client.receive_json()
+    assert response["success"]
+
+    await client.send_json({"id": 11, "type": "lovelace/resources"})
+    response = await client.receive_json()
+    assert response["success"]
+
+    assert len(response["result"]) == 2
+    assert first_item["id"] not in (item["id"] for item in response["result"])
+
 
 async def test_storage_resources_import_invalid(hass, hass_ws_client, hass_storage):
     """Test importing resources from storage config."""
