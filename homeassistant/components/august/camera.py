463096faac43bcd41663ae3c92e6828fb09c6051
J. Nick Koston <nick@cpanel.net>
2020-02-27 17:44:23 -1000

    Coordinate all august detail and activity updates (#32249)
    * Removing polling from august
    
    * Now using subscribers to the detail and activity
    
    * Fix hash to list keys
    
    * continue to the next house if one fails
    
    * Add async_signal_device_id_update
    
    * Fix double initial update
    
    * Handle self.hass not being available until after async_added_to_hass
    
    * Remove not needed await
    
    * Fix regression with device name

--- homeassistant/components/august/camera.py
+++ homeassistant/components/august/camera.py
@@ -5,18 +5,9 @@ from august.util import update_doorbell_image_from_activity
 
 from homeassistant.components.camera import Camera
 from homeassistant.core import callback
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
-from .const import (
-    AUGUST_DEVICE_UPDATE,
-    DATA_AUGUST,
-    DEFAULT_NAME,
-    DEFAULT_TIMEOUT,
-    DOMAIN,
-    MIN_TIME_BETWEEN_DETAIL_UPDATES,
-)
-
-SCAN_INTERVAL = MIN_TIME_BETWEEN_DETAIL_UPDATES
+from .const import DATA_AUGUST, DEFAULT_NAME, DEFAULT_TIMEOUT, DOMAIN
+from .entity import AugustEntityMixin
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -30,31 +21,27 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     async_add_entities(devices, True)
 
 
-class AugustCamera(Camera):
+class AugustCamera(AugustEntityMixin, Camera):
     """An implementation of a August security camera."""
 
-    def __init__(self, data, doorbell, timeout):
+    def __init__(self, data, device, timeout):
         """Initialize a August security camera."""
-        super().__init__()
-        self._undo_dispatch_subscription = None
+        super().__init__(data, device)
         self._data = data
-        self._doorbell = doorbell
-        self._doorbell_detail = None
+        self._device = device
         self._timeout = timeout
         self._image_url = None
         self._image_content = None
-        self._firmware_version = None
-        self._model = None
 
     @property
     def name(self):
         """Return the name of this device."""
-        return self._doorbell.device_name
+        return f"{self._device.device_name} Camera"
 
     @property
     def is_recording(self):
         """Return true if the device is recording."""
-        return self._doorbell.has_subscription
+        return self._device.has_subscription
 
     @property
     def motion_detection_enabled(self):
@@ -69,77 +56,34 @@ class AugustCamera(Camera):
     @property
     def model(self):
         """Return the camera model."""
-        return self._model
+        return self._detail.model
 
-    async def async_camera_image(self):
-        """Return bytes of camera image."""
-        self._doorbell_detail = await self._data.async_get_doorbell_detail(
-            self._doorbell.device_id
-        )
-        doorbell_activity = self._data.activity_stream.async_get_latest_device_activity(
-            self._doorbell.device_id, [ActivityType.DOORBELL_MOTION]
+    @callback
+    def _update_from_data(self):
+        """Get the latest state of the sensor."""
+        doorbell_activity = self._data.activity_stream.get_latest_device_activity(
+            self._device_id, [ActivityType.DOORBELL_MOTION]
         )
 
         if doorbell_activity is not None:
-            update_doorbell_image_from_activity(
-                self._doorbell_detail, doorbell_activity
-            )
+            update_doorbell_image_from_activity(self._detail, doorbell_activity)
 
-        if self._doorbell_detail is None:
-            return None
+    async def async_camera_image(self):
+        """Return bytes of camera image."""
+        self._update_from_data()
 
-        if self._image_url is not self._doorbell_detail.image_url:
-            self._image_url = self._doorbell_detail.image_url
+        if self._image_url is not self._detail.image_url:
+            self._image_url = self._detail.image_url
             self._image_content = await self.hass.async_add_executor_job(
                 self._camera_image
             )
         return self._image_content
 
-    async def async_update(self):
-        """Update camera data."""
-        self._doorbell_detail = await self._data.async_get_doorbell_detail(
-            self._doorbell.device_id
-        )
-
-        if self._doorbell_detail is None:
-            return None
-
-        self._firmware_version = self._doorbell_detail.firmware_version
-        self._model = self._doorbell_detail.model
-
     def _camera_image(self):
         """Return bytes of camera image."""
-        return self._doorbell_detail.get_doorbell_image(timeout=self._timeout)
+        return self._detail.get_doorbell_image(timeout=self._timeout)
 
     @property
     def unique_id(self) -> str:
         """Get the unique id of the camera."""
-        return f"{self._doorbell.device_id:s}_camera"
-
-    @property
-    def device_info(self):
-        """Return the device_info of the device."""
-        return {
-            "identifiers": {(DOMAIN, self._doorbell.device_id)},
-            "name": self._doorbell.device_name + " Camera",
-            "manufacturer": DEFAULT_NAME,
-            "sw_version": self._firmware_version,
-            "model": self._model,
-        }
-
-    async def async_added_to_hass(self):
-        """Register callbacks."""
-
-        @callback
-        def update():
-            """Update the state."""
-            self.async_schedule_update_ha_state(True)
-
-        self._undo_dispatch_subscription = async_dispatcher_connect(
-            self.hass, f"{AUGUST_DEVICE_UPDATE}-{self._doorbell.device_id}", update
-        )
-
-    async def async_will_remove_from_hass(self):
-        """Undo subscription."""
-        if self._undo_dispatch_subscription:
-            self._undo_dispatch_subscription()
+        return f"{self._device_id:s}_camera"
