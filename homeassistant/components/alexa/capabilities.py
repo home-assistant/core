6d23070608e67a0b1d0e56b61da61b8a15eb3296
Dave Nielsen <dcnielsen90@gmail.com>
2020-02-28 02:13:49 +0000

    alexa/capabilities.py: Fix TypeError Exception
        - Remove division by zero try/catch -- there is no division
        - Handle TypeError exception when current_volume = None
        - Simplify math and return logic

--- homeassistant/components/alexa/capabilities.py
+++ homeassistant/components/alexa/capabilities.py
@@ -1,6 +1,5 @@
 """Alexa capabilities."""
 import logging
-import math
 
 from homeassistant.components import (
     cover,
@@ -669,11 +668,8 @@ class AlexaSpeaker(AlexaCapability):
             current_level = self.entity.attributes.get(
                 media_player.ATTR_MEDIA_VOLUME_LEVEL
             )
-            try:
-                current = math.floor(int(current_level * 100))
-            except ZeroDivisionError:
-                current = 0
-            return current
+            if current_level is not None:
+                return round(float(current_level) * 100)
 
         if name == "muted":
             return bool(
