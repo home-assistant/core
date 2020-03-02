181d34ed8a8a6712ab99352d07a308fa879cbd18
Tom Schneider <tom.schneider-github@sutomaji.net>
2020-03-02 07:03:19 +0100

    Use _abort_if_unique_id_configured

--- homeassistant/components/shopping_list/config_flow.py
+++ homeassistant/components/shopping_list/config_flow.py
@@ -17,8 +17,7 @@ class ShoppingListFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
+        self._abort_if_unique_id_configured()
 
         self._errors = {}
 
