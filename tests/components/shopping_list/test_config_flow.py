bb1268b1a50346ac5a72078b6b7a67d333772831
Tom Schneider <tom.schneider-github@sutomaji.net>
2020-03-02 07:35:45 +0100

    Fix tests

--- tests/components/shopping_list/test_config_flow.py
+++ tests/components/shopping_list/test_config_flow.py
@@ -4,29 +4,16 @@ from homeassistant.components.shopping_list.const import DOMAIN
 from homeassistant.const import CONF_TYPE
 from homeassistant.setup import async_setup_component
 
-from tests.common import MockConfigEntry
-
 
 async def test_manual_config_set(hass):
-    """Test we ignore entry if manual config available."""
+    """Test entry will be imported."""
     assert await async_setup_component(hass, DOMAIN, {})
     await hass.async_block_till_done()
 
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": "user"}
+        DOMAIN, data={}, context={"source": "user"}
     )
-    assert result[CONF_TYPE] == "abort"
-
-
-async def test_user_single_instance(hass):
-    """Test we only allow a single config flow."""
-    MockConfigEntry(domain=DOMAIN).add_to_hass(hass)
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": "user"}
-    )
-    assert result[CONF_TYPE] == "abort"
-    assert result["reason"] == "single_instance_allowed"
+    assert result[CONF_TYPE] == "create_entry"
 
 
 async def test_user(hass):
