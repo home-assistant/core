67da271c8dedbbebeda246ce75a63a346b83980b
Tom Schneider <tom.schneider-github@sutomaji.net>
2020-03-02 19:50:19 +0100

    Use consts in test

--- tests/components/shopping_list/test_config_flow.py
+++ tests/components/shopping_list/test_config_flow.py
@@ -1,6 +1,8 @@
 """Test config flow."""
 
+from homeassistant import data_entry_flow
 from homeassistant.components.shopping_list.const import DOMAIN
+from homeassistant.config_entries import SOURCE_USER
 from homeassistant.setup import async_setup_component
 
 
@@ -10,19 +12,19 @@ async def test_manual_config_set(hass):
     await hass.async_block_till_done()
 
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, data={}, context={"source": "user"}
+        DOMAIN, data={}, context={"source": SOURCE_USER}
     )
-    assert result["type"] == "create_entry"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
 
 
 async def test_user(hass):
     """Test we can start a config flow."""
 
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, data=None, context={"source": "user"}
+        DOMAIN, data=None, context={"source": SOURCE_USER}
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
 
 
@@ -30,8 +32,8 @@ async def test_user_confirm(hass):
     """Test we can finish a config flow."""
 
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, data={}, context={"source": "user"}
+        DOMAIN, data={}, context={"source": SOURCE_USER}
     )
 
-    assert result["type"] == "create_entry"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result["result"].data == {}
