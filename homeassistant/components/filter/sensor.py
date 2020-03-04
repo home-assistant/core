7dc0557d4e4e2829a9c71ffc3cac2f7fc59a26c5
Diogo Gomes <dgomes@av.it.pt>
2020-03-04 19:57:05 +0000

    only_numbers flag

--- homeassistant/components/filter/sensor.py
+++ homeassistant/components/filter/sensor.py
@@ -364,6 +364,7 @@ class Filter:
         self._skip_processing = False
         self._window_size = window_size
         self._store_raw = False
+        self._only_numbers = True
 
     @property
     def window_size(self):
@@ -386,7 +387,11 @@ class Filter:
 
     def filter_state(self, new_state):
         """Implement a common interface for filters."""
-        filtered = self._filter_state(FilterState(new_state))
+        fstate = FilterState(new_state)
+        if self._only_numbers and not isinstance(fstate.state, Number):
+            raise ValueError
+
+        filtered = self._filter_state(fstate)
         filtered.set_precision(self.precision)
         if self._store_raw:
             self.states.append(copy(FilterState(new_state)))
@@ -423,8 +428,6 @@ class RangeFilter(Filter):
 
     def _filter_state(self, new_state):
         """Implement the range filter."""
-        if not isinstance(new_state.state, Number):
-            raise ValueError
 
         if self._upper_bound is not None and new_state.state > self._upper_bound:
 
@@ -472,8 +475,6 @@ class OutlierFilter(Filter):
 
     def _filter_state(self, new_state):
         """Implement the outlier filter."""
-        if not isinstance(new_state.state, Number):
-            raise ValueError
 
         median = statistics.median([s.state for s in self.states]) if self.states else 0
         if (
@@ -504,8 +505,6 @@ class LowPassFilter(Filter):
 
     def _filter_state(self, new_state):
         """Implement the low pass filter."""
-        if not isinstance(new_state.state, Number):
-            raise ValueError
 
         if not self.states:
             return new_state
@@ -548,8 +547,6 @@ class TimeSMAFilter(Filter):
 
     def _filter_state(self, new_state):
         """Implement the Simple Moving Average filter."""
-        if not isinstance(new_state.state, Number):
-            raise ValueError
 
         self._leak(new_state.timestamp)
         self.queue.append(copy(new_state))
@@ -577,6 +574,7 @@ class ThrottleFilter(Filter):
     def __init__(self, window_size, precision, entity):
         """Initialize Filter."""
         super().__init__(FILTER_NAME_THROTTLE, window_size, precision, entity)
+        self._only_numbers = False
 
     def _filter_state(self, new_state):
         """Implement the throttle filter."""
@@ -601,6 +599,7 @@ class TimeThrottleFilter(Filter):
         super().__init__(FILTER_NAME_TIME_THROTTLE, window_size, precision, entity)
         self._time_window = window_size
         self._last_emitted_at = None
+        self._only_numbers = False
 
     def _filter_state(self, new_state):
         """Implement the filter."""
