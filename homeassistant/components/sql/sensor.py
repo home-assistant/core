53fb4230150912c46cd1b32051a0fb36511a1d8e
Ken Cote <kennethcote@LIVE.COM>
2020-03-04 21:52:20 -0500

    Added async, templating to query, MSSQL support

--- homeassistant/components/sql/sensor.py
+++ homeassistant/components/sql/sensor.py
@@ -7,9 +7,20 @@ import sqlalchemy
 from sqlalchemy.orm import scoped_session, sessionmaker
 import voluptuous as vol
 
-from homeassistant.components.recorder import CONF_DB_URL, DEFAULT_DB_FILE, DEFAULT_URL
+from homeassistant.components.recorder import (
+    CONF_DB_URL, 
+    DEFAULT_DB_FILE, 
+    DEFAULT_URL
+)
+
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME, CONF_UNIT_OF_MEASUREMENT, CONF_VALUE_TEMPLATE
+
+from homeassistant.const import (
+    CONF_NAME, 
+    CONF_UNIT_OF_MEASUREMENT, 
+    CONF_VALUE_TEMPLATE,
+)
+
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
@@ -18,22 +29,22 @@ _LOGGER = logging.getLogger(__name__)
 CONF_COLUMN_NAME = "column"
 CONF_QUERIES = "queries"
 CONF_QUERY = "query"
-
+CONF_QUERY_TEMPLATE = "query_template"
 
 def validate_sql_select(value):
     """Validate that value is a SQL SELECT query."""
-    if not value.lstrip().lower().startswith("select"):
-        raise vol.Invalid("Only SELECT queries allowed")
+    if not value.lstrip().lower().startswith("select") and not value.lstrip().lower().startswith("exec"):
+         raise vol.Invalid("Only SELECT or EXEC queries allowed")
     return value
-
 
 _QUERY_SCHEME = vol.Schema(
     {
         vol.Required(CONF_COLUMN_NAME): cv.string,
         vol.Required(CONF_NAME): cv.string,
-        vol.Required(CONF_QUERY): vol.All(cv.string, validate_sql_select),
+        vol.Optional(CONF_QUERY): cv.string,
         vol.Optional(CONF_UNIT_OF_MEASUREMENT): cv.string,
         vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+        vol.Optional(CONF_QUERY_TEMPLATE): cv.template,
     }
 )
 
@@ -41,10 +52,10 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {vol.Required(CONF_QUERIES): [_QUERY_SCHEME], vol.Optional(CONF_DB_URL): cv.string}
 )
 
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the SQL sensor platform."""
     db_url = config.get(CONF_DB_URL, None)
+
     if not db_url:
         db_url = DEFAULT_URL.format(hass_config_path=hass.config.path(DEFAULT_DB_FILE))
 
@@ -70,34 +81,60 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         unit = query.get(CONF_UNIT_OF_MEASUREMENT)
         value_template = query.get(CONF_VALUE_TEMPLATE)
         column_name = query.get(CONF_COLUMN_NAME)
+        query_template = query.get(CONF_QUERY_TEMPLATE)
 
+        if query_str and query_template:
+            raise Exception("Both query and query_template are defined. Choose one.")
+ 
         if value_template is not None:
             value_template.hass = hass
 
+        if query_template is not None:
+            query_template.hass = hass
+
         sensor = SQLSensor(
-            name, sessmaker, query_str, column_name, unit, value_template
+            hass, name, sessmaker, query_str, column_name, unit, value_template, query_template
         )
         queries.append(sensor)
 
-    add_entities(queries, True)
+    async_add_entities(queries)
 
+    return True
 
 class SQLSensor(Entity):
     """Representation of an SQL sensor."""
 
-    def __init__(self, name, sessmaker, query, column, unit, value_template):
+    def __init__(
+        self,
+        hass, 
+        name, 
+        sessmaker, 
+        query, 
+        column, 
+        unit, 
+        value_template, 
+        query_template
+    ):
+
         """Initialize the SQL sensor."""
+        self.hass = hass
         self._name = name
-        if "LIMIT" in query:
-            self._query = query
+        self._query_template = query_template
+
+        if query is not None:
+            if "LIMIT" in query:
+                self._query = query
+            else:
+                self._query = query.replace(";", " LIMIT 1;")
         else:
-            self._query = query.replace(";", " LIMIT 1;")
+            self._query = None
+
         self._unit_of_measurement = unit
         self._template = value_template
         self._column_name = column
         self.sessionmaker = sessmaker
         self._state = None
-        self._attributes = None
+        self._attributes = {}
 
     @property
     def name(self):
@@ -119,12 +156,30 @@ class SQLSensor(Entity):
         """Return the state attributes."""
         return self._attributes
 
-    def update(self):
+    async def async_update(self):
         """Retrieve sensor data from the query."""
+        
+        if self._query is not None:
+            sql_command = self._query
 
-        try:
+        if self._query_template is not None:
+            try:
+                sql_command = self._query_template.async_render()
+            except TemplateError as ex:
+                if ex.args and ex.args[0].startswith(
+                    "UndefinedError: 'None' has no attribute"
+                ):
+                    # Common during HA startup - so just a warning
+                    _LOGGER.warning(
+                        "Could not render template %s, the state is unknown.", self._name
+                    )
+                else:
+                    self._state = None
+                    _LOGGER.error("Could not render template %s: %s", self._name, ex)
+
+        try:            
             sess = self.sessionmaker()
-            result = sess.execute(self._query)
+            result = sess.execute(sql_command)
             self._attributes = {}
 
             if not result.returns_rows or result.rowcount == 0:
