9f5c3a7ffe08940fd2b69471caea863fd9acf4a5
Paulus Schoutsen <balloob@gmail.com>
2020-03-04 18:08:59 -0800

    Type

--- homeassistant/components/remote/__init__.py
+++ homeassistant/components/remote/__init__.py
@@ -2,7 +2,7 @@
 from datetime import timedelta
 import functools as ft
 import logging
-from typing import Any, Iterable
+from typing import Any, Iterable, cast
 
 import voluptuous as vol
 
@@ -114,12 +114,14 @@ async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
 
 async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool:
     """Set up a config entry."""
-    return await hass.data[DOMAIN].async_setup_entry(entry)
+    return cast(
+        bool, await cast(EntityComponent, hass.data[DOMAIN]).async_setup_entry(entry)
+    )
 
 
 async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.data[DOMAIN].async_unload_entry(entry)
+    return await cast(EntityComponent, hass.data[DOMAIN]).async_unload_entry(entry)
 
 
 class RemoteDevice(ToggleEntity):
