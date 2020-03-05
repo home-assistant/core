521cc7247dadf37cfc96498863b356219e3fad10
Paulus Schoutsen <balloob@gmail.com>
2020-03-04 22:05:39 -0800

    Add Dynalite switch platform (#32389)
    * added presets for switch devices
    
    * added channel type to __init and const
    
    * ran pylint on library so needed a few changes in names
    
    * removed callback
    
    * bool -> cv.boolean

--- homeassistant/components/dynalite/light.py
+++ homeassistant/components/dynalite/light.py
@@ -1,6 +1,5 @@
 """Support for Dynalite channels as lights."""
 from homeassistant.components.light import SUPPORT_BRIGHTNESS, Light
-from homeassistant.core import callback
 
 from .dynalitebase import DynaliteBase, async_setup_entry_base
 
@@ -8,12 +7,8 @@ from .dynalitebase import DynaliteBase, async_setup_entry_base
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Record the async_add_entities function to add them later when received from Dynalite."""
 
-    @callback
-    def light_from_device(device, bridge):
-        return DynaliteLight(device, bridge)
-
     async_setup_entry_base(
-        hass, config_entry, async_add_entities, "light", light_from_device
+        hass, config_entry, async_add_entities, "light", DynaliteLight
     )
 
 
