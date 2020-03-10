ca9bba715fc8517888376692b8b190f2aa3101bc
J. Nick Koston <nick@koston.org>
2020-03-10 23:13:15 +0000

    Fix revert that come back with the conflict

--- homeassistant/components/august/lock.py
+++ homeassistant/components/august/lock.py
@@ -38,9 +38,6 @@ class AugustLock(AugustEntityMixin, RestoreEntity, LockDevice):
         self._device = device
         self._lock_status = None
         self._changed_by = None
-        self._operated_remote = None
-        self._operated_keypad = None
-        self._operated_autorelock = None
         self._available = False
         self._update_from_data()
 
@@ -80,12 +77,8 @@ class AugustLock(AugustEntityMixin, RestoreEntity, LockDevice):
         )
 
         if lock_activity is not None:
-            update_lock_detail_from_activity(self._detail, lock_activity)
-
             self._changed_by = lock_activity.operated_by
-            self._operated_remote = lock_activity.operated_remote
-            self._operated_keypad = lock_activity.operated_keypad
-            self._operated_autorelock = lock_activity.operated_autorelock
+            update_lock_detail_from_activity(self._detail, lock_activity)
 
         self._update_lock_status_from_detail()
 
@@ -118,12 +111,6 @@ class AugustLock(AugustEntityMixin, RestoreEntity, LockDevice):
 
         if self._detail.keypad is not None:
             attributes["keypad_battery_level"] = self._detail.keypad.battery_level
-        if self._operated_remote is not None:
-            attributes["operated_remote"] = self._operated_remote
-        if self._operated_keypad is not None:
-            attributes["operated_keypad"] = self._operated_keypad
-        if self._operated_autorelock is not None:
-            attributes["operated_autorelock"] = self._operated_autorelock
 
         return attributes
 
@@ -137,12 +124,6 @@ class AugustLock(AugustEntityMixin, RestoreEntity, LockDevice):
 
         if ATTR_CHANGED_BY in last_state.attributes:
             self._changed_by = last_state.attributes[ATTR_CHANGED_BY]
-        if "operated_remote" in last_state.attributes:
-            self._operated_remote = last_state.attributes["operated_remote"]
-        if "operated_keypad" in last_state.attributes:
-            self._operated_keypad = last_state.attributes["operated_keypad"]
-        if "operated_autorelock" in last_state.attributes:
-            self._operated_autorelock = last_state.attributes["operated_autorelock"]
 
     @property
     def unique_id(self) -> str:
