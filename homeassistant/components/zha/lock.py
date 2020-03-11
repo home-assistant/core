4248893007bf194e7df505524c6c7e1beb9ce78f
David F. Mulcahey <david.mulcahey@me.com>
2020-03-11 07:17:53 -0400

    Clean up custom polling in ZHA device and light (#32653)
    * cleanup timer handle when device is removed
    
    * separate unavailable times for mains vs battery
    
    * better name
    
    * remove light refresh handle when removing light
    
    * remove unused parallel updates
    
    * don't steal HA const for different purpose
    
    * don't flood network every hour for lights
    
    * update test to test both intervals
    
    * add test for light refresh interval
    
    * fix tests
    
    * update test
    
    * put parallel updates back for now...
    
    * fix async_schedule_update_ha_state usage
    
    * review comment
    
    * review comment
    
    * update test - review conversation
    
    * review comments
    
    * await count not call count
    
    * flip some state

--- homeassistant/components/zha/lock.py
+++ homeassistant/components/zha/lock.py
@@ -87,7 +87,7 @@ class ZhaDoorLock(ZhaEntity, LockDevice):
         if not isinstance(result, list) or result[0] is not Status.SUCCESS:
             self.error("Error with lock_door: %s", result)
             return
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_unlock(self, **kwargs):
         """Unlock the lock."""
@@ -95,7 +95,7 @@ class ZhaDoorLock(ZhaEntity, LockDevice):
         if not isinstance(result, list) or result[0] is not Status.SUCCESS:
             self.error("Error with unlock_door: %s", result)
             return
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_update(self):
         """Attempt to retrieve state from the lock."""
@@ -106,7 +106,7 @@ class ZhaDoorLock(ZhaEntity, LockDevice):
     def async_set_state(self, attr_id, attr_name, value):
         """Handle state update from channel."""
         self._state = VALUE_TO_STATE.get(value, self._state)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_get_state(self, from_cache=True):
         """Attempt to retrieve state from the lock."""
