c5cc28347e64c6f588ab8ee35fc3aa8a4049ab4f
Aaron Bach <bachya1208@gmail.com>
2020-03-11 14:12:07 -0600

    Code review

--- homeassistant/components/iqvia/__init__.py
+++ homeassistant/components/iqvia/__init__.py
@@ -299,7 +299,7 @@ class IQVIAEntity(Entity):
         @callback
         def update():
             """Update the state."""
-            self.async_schedule_update_ha_state(True)
+            self.async_write_ha_state()
 
         self._async_unsub_dispatcher_connect = async_dispatcher_connect(
             self.hass, TOPIC_DATA_UPDATE, update
