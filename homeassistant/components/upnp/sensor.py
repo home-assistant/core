c00e1fc126b9842dea774c8ef22437c3c26fb11a
Steven Looman <steven.looman@gmail.com>
2020-03-12 21:39:09 +0100

    Throttle updates to max once per 30s

--- homeassistant/components/upnp/sensor.py
+++ homeassistant/components/upnp/sensor.py
@@ -1,11 +1,14 @@
 """Support for UPnP/IGD Sensors."""
 import logging
 
+from datetime import timedelta
+
 from homeassistant.core import callback
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.typing import HomeAssistantType
+from homeassistant.util import Throttle
 import homeassistant.util.dt as dt_util
 
 from .const import DOMAIN as DOMAIN_UPNP, SIGNAL_REMOVE_SENSOR
@@ -27,6 +30,8 @@ SENSOR_TYPES = {
 IN = "received"
 OUT = "sent"
 KBYTE = 1024
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
 
 
 async def async_setup_platform(
@@ -141,6 +146,7 @@ class RawUPnPIGDSensor(UpnpSensor):
         """Return the unit of measurement of this entity, if any."""
         return self._type["unit"]
 
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
     async def async_update(self):
         """Get the latest information from the IGD."""
         if self._type_name == BYTES_RECEIVED:
