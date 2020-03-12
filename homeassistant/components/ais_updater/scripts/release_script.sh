549d2877d977d71521ba4364bfbdc03cacefce3d
tomasz75 <tomek@sviete.pl>
2020-03-12 17:35:50 +0100

    fix in remove drive

--- homeassistant/components/ais_updater/scripts/release_script.sh
+++ homeassistant/components/ais_updater/scripts/release_script.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+apt update 
+apt upgrade -y 
+curl -o "/data/data/pl.sviete.dom/files/home/.bash_profile" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/.bash_profile 
+chmod +x /data/data/pl.sviete.dom/files/home/.bash_profile 
 curl https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/remove_deps.sh | bash 
 echo "20.02.24" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
