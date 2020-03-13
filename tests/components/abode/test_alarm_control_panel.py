562792adc7ff7c84e2f2f9ca3481144aa921498b
shred86 <shred86@icloud.com>
2020-03-12 18:23:43 -0700

    Add more complete test for alarms

--- tests/components/abode/test_alarm_control_panel.py
+++ tests/components/abode/test_alarm_control_panel.py
@@ -1,6 +1,8 @@
 """Tests for the Abode alarm control panel device."""
 from unittest.mock import patch
 
+import abodepy.helpers.constants as CONST
+
 from homeassistant.components.abode import ATTR_DEVICE_ID
 from homeassistant.components.alarm_control_panel import DOMAIN as ALARM_DOMAIN
 from homeassistant.const import (
@@ -10,10 +12,14 @@ from homeassistant.const import (
     SERVICE_ALARM_ARM_AWAY,
     SERVICE_ALARM_ARM_HOME,
     SERVICE_ALARM_DISARM,
+    STATE_ALARM_ARMED_AWAY,
+    STATE_ALARM_ARMED_HOME,
     STATE_ALARM_DISARMED,
 )
 
 from .common import setup_platform
+
+DEVICE_ID = "alarm_control_panel.abode_alarm"
 
 
 async def test_entity_registry(hass):
@@ -21,7 +27,7 @@ async def test_entity_registry(hass):
     await setup_platform(hass, ALARM_DOMAIN)
     entity_registry = await hass.helpers.entity_registry.async_get_registry()
 
-    entry = entity_registry.async_get("alarm_control_panel.abode_alarm")
+    entry = entity_registry.async_get(DEVICE_ID)
     # Abode alarm device unique_id is the MAC address
     assert entry.unique_id == "001122334455"
 
@@ -30,7 +36,7 @@ async def test_automation_attributes(hass):
     """Test the alarm control panel attributes are correct."""
     await setup_platform(hass, ALARM_DOMAIN)
 
-    state = hass.states.get("alarm_control_panel.abode_alarm")
+    state = hass.states.get(DEVICE_ID)
     assert state.state == STATE_ALARM_DISARMED
     assert state.attributes.get(ATTR_DEVICE_ID) == "area_1"
     assert not state.attributes.get("battery_backup")
@@ -39,46 +45,77 @@ async def test_automation_attributes(hass):
     assert state.attributes.get(ATTR_SUPPORTED_FEATURES) == 3
 
 
-async def test_set_alarm_away(hass):
+async def test_set_alarm_away(hass, requests_mock):
     """Test the alarm control panel can be set to away."""
-    await setup_platform(hass, ALARM_DOMAIN)
+    requests_mock.put(
+        CONST.get_panel_mode_url("1", CONST.MODE_AWAY),
+        json={"area": "1", "mode": CONST.MODE_AWAY},
+    )
 
-    with patch("abodepy.ALARM.AbodeAlarm.set_away") as mock_set_away:
+    with patch("abodepy.AbodeEventController.add_device_callback") as mock_callback:
+        await setup_platform(hass, ALARM_DOMAIN)
+
         await hass.services.async_call(
-            "alarm_control_panel",
+            ALARM_DOMAIN,
             SERVICE_ALARM_ARM_AWAY,
-            {ATTR_ENTITY_ID: "alarm_control_panel.abode_alarm"},
+            {ATTR_ENTITY_ID: DEVICE_ID},
             blocking=True,
         )
         await hass.async_block_till_done()
-        mock_set_away.assert_called_once()
+
+        update_callback = mock_callback.call_args[0][1]
+        await hass.async_add_executor_job(update_callback, "area_1")
+        await hass.async_block_till_done()
+
+        state = hass.states.get(DEVICE_ID)
+        assert state.state == STATE_ALARM_ARMED_AWAY
 
 
-async def test_set_alarm_home(hass):
+async def test_set_alarm_home(hass, requests_mock):
     """Test the alarm control panel can be set to home."""
-    await setup_platform(hass, ALARM_DOMAIN)
+    requests_mock.put(
+        CONST.get_panel_mode_url("1", CONST.MODE_HOME),
+        json={"area": "1", "mode": CONST.MODE_HOME},
+    )
 
-    with patch("abodepy.ALARM.AbodeAlarm.set_home") as mock_set_home:
+    with patch("abodepy.AbodeEventController.add_device_callback") as mock_callback:
+        await setup_platform(hass, ALARM_DOMAIN)
         await hass.services.async_call(
             ALARM_DOMAIN,
             SERVICE_ALARM_ARM_HOME,
-            {ATTR_ENTITY_ID: "alarm_control_panel.abode_alarm"},
+            {ATTR_ENTITY_ID: DEVICE_ID},
             blocking=True,
         )
         await hass.async_block_till_done()
-        mock_set_home.assert_called_once()
+
+        update_callback = mock_callback.call_args[0][1]
+        await hass.async_add_executor_job(update_callback, "area_1")
+        await hass.async_block_till_done()
+
+        state = hass.states.get(DEVICE_ID)
+        assert state.state == STATE_ALARM_ARMED_HOME
 
 
-async def test_set_alarm_standby(hass):
+async def test_set_alarm_standby(hass, requests_mock):
     """Test the alarm control panel can be set to standby."""
-    await setup_platform(hass, ALARM_DOMAIN)
+    requests_mock.put(
+        CONST.get_panel_mode_url("1", CONST.MODE_STANDBY),
+        json={"area": "1", "mode": CONST.MODE_STANDBY},
+    )
 
-    with patch("abodepy.ALARM.AbodeAlarm.set_standby") as mock_set_standby:
+    with patch("abodepy.AbodeEventController.add_device_callback") as mock_callback:
+        await setup_platform(hass, ALARM_DOMAIN)
         await hass.services.async_call(
             ALARM_DOMAIN,
             SERVICE_ALARM_DISARM,
-            {ATTR_ENTITY_ID: "alarm_control_panel.abode_alarm"},
+            {ATTR_ENTITY_ID: DEVICE_ID},
             blocking=True,
         )
         await hass.async_block_till_done()
-        mock_set_standby.assert_called_once()
+
+        update_callback = mock_callback.call_args[0][1]
+        await hass.async_add_executor_job(update_callback, "area_1")
+        await hass.async_block_till_done()
+
+        state = hass.states.get(DEVICE_ID)
+        assert state.state == STATE_ALARM_DISARMED
