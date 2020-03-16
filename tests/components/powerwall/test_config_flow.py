a5f134b797232d9e9233850927020fa502239441
J. Nick Koston <nick@koston.org>
2020-03-16 02:27:02 +0000

    Add test for import

--- tests/components/powerwall/test_config_flow.py
+++ tests/components/powerwall/test_config_flow.py
@@ -10,8 +10,8 @@ from homeassistant.const import CONF_IP_ADDRESS
 from .mocks import _mock_powerwall_return_value, _mock_powerwall_side_effect
 
 
-async def test_form(hass):
-    """Test we get the form."""
+async def test_form_source_user(hass):
+    """Test we get config flow setup form as a user."""
     await setup.async_setup_component(hass, "persistent_notification", {})
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -38,6 +38,36 @@ async def test_form(hass):
     assert result2["type"] == "create_entry"
     assert result2["title"] == "My site"
     assert result2["data"] == {CONF_IP_ADDRESS: "1.2.3.4"}
+    await hass.async_block_till_done()
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_form_source_import(hass):
+    """Test we setup the config entry via import."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+
+    mock_powerwall = _mock_powerwall_return_value(
+        site_info={POWERWALL_SITE_NAME: "Imported site"}
+    )
+
+    with patch(
+        "homeassistant.components.powerwall.config_flow.PowerWall",
+        return_value=mock_powerwall,
+    ), patch(
+        "homeassistant.components.powerwall.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.powerwall.async_setup_entry", return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_IMPORT},
+            data={CONF_IP_ADDRESS: "1.2.3.4"},
+        )
+
+    assert result["type"] == "create_entry"
+    assert result["title"] == "Imported site"
+    assert result["data"] == {CONF_IP_ADDRESS: "1.2.3.4"}
     await hass.async_block_till_done()
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
