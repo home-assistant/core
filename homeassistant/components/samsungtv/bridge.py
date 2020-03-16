a355447a7853e64a4cc28c99ccbfa07490f5324b
Paolo Tuninetto <paolo.tuninetto@gmail.com>
2020-03-17 00:37:10 +0100

    Add default port to samsung tv (#32820)
    * Default port for websocket tv
    
    * Update config entry
    
    * move bridge creation
    
    * fix indent
    
    * remove loop

--- homeassistant/components/samsungtv/bridge.py
+++ homeassistant/components/samsungtv/bridge.py
@@ -46,6 +46,7 @@ class SamsungTVBridge(ABC):
         self.method = method
         self.host = host
         self.token = None
+        self.default_port = None
         self._remote = None
         self._callback = None
 
@@ -191,6 +192,7 @@ class SamsungTVWSBridge(SamsungTVBridge):
         """Initialize Bridge."""
         super().__init__(method, host, port)
         self.token = token
+        self.default_port = 8001
 
     def try_connect(self):
         """Try to connect to the Websocket TV."""
