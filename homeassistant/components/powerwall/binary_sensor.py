6d7c791ba9f4bea45c9d27c7a1aaced37886ffac
J. Nick Koston <nick@cpanel.net>
2020-03-15 20:39:10 -0500

    Update homeassistant/components/powerwall/binary_sensor.py
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/powerwall/binary_sensor.py
+++ homeassistant/components/powerwall/binary_sensor.py
@@ -85,9 +85,6 @@ class PowerWallRunningSensor(PowerWallBinarySensor):
     def __init__(self, coordinator, site_info, ip_address):
         """Initialize the sensor."""
         super().__init__(coordinator, site_info, ip_address)
-        self._coordinator = coordinator
-        self._site_info = site_info
-        self._ip_address = ip_address
 
     @property
     def name(self):
