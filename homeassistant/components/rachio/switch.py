f622e037c392b1430f4fb9ef28fb5a2d6cb31efb
J. Nick Koston <nick@cpanel.net>
2020-03-15 20:06:49 -0500

    Update homeassistant/components/rachio/switch.py
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/rachio/switch.py
+++ homeassistant/components/rachio/switch.py
@@ -68,7 +68,6 @@ class RachioSwitch(RachioDeviceInfoProvider, SwitchDevice):
     def __init__(self, controller, poll=True):
         """Initialize a new Rachio switch."""
         super().__init__(controller)
-        self._controller = controller
 
         if poll:
             self._state = self._poll_update()
