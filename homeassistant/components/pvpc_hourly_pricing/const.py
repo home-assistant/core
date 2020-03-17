1e5324b6d0eb0cdfb3b97f807bd458b0a8990a49
Eugenio Panadero <eugenio.panadero@gmail.com>
2020-03-17 11:34:50 +0100

    Fix lint

--- homeassistant/components/pvpc_hourly_pricing/const.py
+++ homeassistant/components/pvpc_hourly_pricing/const.py
@@ -1,9 +1,9 @@
 """Constant values for pvpc_hourly_pricing."""
-from aiopvpc import DEFAULT_TIMEOUT, TARIFFS  # noqa: F401
+from aiopvpc import TARIFFS
 
 DOMAIN = "pvpc_hourly_pricing"
 PLATFORM = "sensor"
 ATTR_TARIFF = "tariff"
 DEFAULT_NAME = "PVPC"
-
 DEFAULT_TARIFF = TARIFFS[1]
+DEFAULT_TIMEOUT = 5
