f8fcebc020d9788d78235dd091974956e15659e5
Aaron Bach <bachya1208@gmail.com>
2020-03-16 23:29:41 -0600

    Add cleanup to SimpliSafe

--- homeassistant/components/simplisafe/__init__.py
+++ homeassistant/components/simplisafe/__init__.py
@@ -642,13 +642,17 @@ class SimpliSafeEntity(Entity):
         @callback
         def update():
             """Update the state."""
-            self.async_schedule_update_ha_state(True)
+            self.update_from_latest_data()
+            self.async_write_ha_state()
 
         self._async_unsub_dispatcher_connect = async_dispatcher_connect(
             self.hass, TOPIC_UPDATE.format(self._system.system_id), update
         )
 
-    async def async_update(self):
+        self.update_from_latest_data()
+
+    @callback
+    def update_from_latest_data(self):
         """Update the entity."""
         self.async_update_from_rest_api()
 
