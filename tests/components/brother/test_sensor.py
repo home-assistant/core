96325ebea7280f797fe2de0f0995de64f86d9a80
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-03-18 12:31:50 +0100

    Tests improvement

--- tests/components/brother/test_sensor.py
+++ tests/components/brother/test_sensor.py
@@ -4,14 +4,24 @@ import json
 
 from asynctest import patch
 
-from homeassistant.components.brother.const import DOMAIN
-from homeassistant.const import CONF_HOST, CONF_TYPE, STATE_UNAVAILABLE
+from homeassistant.components.brother.const import DOMAIN, UNIT_PAGES
+from homeassistant.const import (
+    ATTR_ICON,
+    ATTR_UNIT_OF_MEASUREMENT,
+    CONF_HOST,
+    CONF_TYPE,
+    STATE_UNAVAILABLE,
+    UNIT_PERCENTAGE,
+)
 from homeassistant.util.dt import utcnow
 
 from tests.common import MockConfigEntry, async_fire_time_changed, load_fixture
 
+ATTR_REMAINING_PAGES = "remaining_pages"
+ATTR_COUNTER = "counter"
 
-async def test_states(hass):
+
+async def test_sensors(hass):
     """Test states of the sensors."""
     entry = MockConfigEntry(
         domain=DOMAIN,
@@ -27,24 +37,28 @@ async def test_states(hass):
         await hass.async_block_till_done()
 
         state = hass.states.get("sensor.hl_l2340dw_status")
+        assert state
+        assert state.attributes.get(ATTR_ICON) == "mdi:printer"
         assert state.state == "waiting"
 
         state = hass.states.get("sensor.hl_l2340dw_black_toner_remaining")
+        assert state
+        assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
+        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
         assert state.state == "75"
 
-        state = hass.states.get("sensor.hl_l2340dw_cyan_drum_remaining_life")
-        assert state.state == "92"
-
-        state = hass.states.get("sensor.hl_l2340dw_magenta_drum_remaining_life")
-        assert state.state == "92"
-
-        state = hass.states.get("sensor.hl_l2340dw_yellow_drum_remaining_life")
-        assert state.state == "92"
-
         state = hass.states.get("sensor.hl_l2340dw_black_drum_remaining_life")
+        assert state
+        assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
+        assert state.attributes.get(ATTR_REMAINING_PAGES) == 16389
+        assert state.attributes.get(ATTR_COUNTER) == 1611
+        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
         assert state.state == "92"
 
         state = hass.states.get("sensor.hl_l2340dw_page_counter")
+        assert state
+        assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
+        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
         assert state.state == "986"
 
 
