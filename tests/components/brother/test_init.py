867a90cc4bbc2bfda95d6391c84af483fec7d85f
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-03-18 17:02:58 +0100

    Simplify tests

--- tests/components/brother/test_init.py
+++ tests/components/brother/test_init.py
@@ -1,36 +1,22 @@
 """Test init of Brother integration."""
-import json
-
 from asynctest import patch
 
 from homeassistant.components.brother.const import DOMAIN
 from homeassistant.config_entries import ENTRY_STATE_SETUP_RETRY
 from homeassistant.const import CONF_HOST, CONF_TYPE, STATE_UNAVAILABLE
 
-from tests.common import MockConfigEntry, load_fixture
+from tests.common import MockConfigEntry
+from tests.components.brother import init_integration
 
 
 async def test_async_setup_entry(hass):
     """Test a successful setup entry."""
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        title="HL-L2340DW 0123456789",
-        unique_id="0123456789",
-        data={CONF_HOST: "localhost", CONF_TYPE: "laser"},
-    )
+    await init_integration(hass)
 
-    with patch(
-        "brother.Brother._get_data",
-        return_value=json.loads(load_fixture("brother_printer_data.json")),
-    ):
-        entry.add_to_hass(hass)
-        assert await hass.config_entries.async_setup(entry.entry_id)
-        await hass.async_block_till_done()
-
-        state = hass.states.get("sensor.hl_l2340dw_status")
-        assert state is not None
-        assert state.state != STATE_UNAVAILABLE
-        assert state.state == "waiting"
+    state = hass.states.get("sensor.hl_l2340dw_status")
+    assert state is not None
+    assert state.state != STATE_UNAVAILABLE
+    assert state.state == "waiting"
 
 
 async def test_config_not_ready(hass):
@@ -50,22 +36,9 @@ async def test_config_not_ready(hass):
 
 async def test_unload_entry(hass):
     """Test successful unload of entry."""
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        title="HL-L2340DW 0123456789",
-        unique_id="0123456789",
-        data={CONF_HOST: "localhost", CONF_TYPE: "laser"},
-    )
+    entry = await init_integration(hass)
 
-    with patch(
-        "brother.Brother._get_data",
-        return_value=json.loads(load_fixture("brother_printer_data.json")),
-    ):
-        entry.add_to_hass(hass)
-        assert await hass.config_entries.async_setup(entry.entry_id)
-        await hass.async_block_till_done()
+    assert hass.data[DOMAIN][entry.entry_id]
 
-        assert hass.data[DOMAIN][entry.entry_id]
-
-        assert await hass.config_entries.async_unload(entry.entry_id)
-        assert not hass.data[DOMAIN]
+    assert await hass.config_entries.async_unload(entry.entry_id)
+    assert not hass.data[DOMAIN]
--- tests/components/brother/test_sensor.py
+++ tests/components/brother/test_sensor.py
@@ -4,19 +4,18 @@ import json
 
 from asynctest import patch
 
-from homeassistant.components.brother.const import DOMAIN, UNIT_PAGES
+from homeassistant.components.brother.const import UNIT_PAGES
 from homeassistant.const import (
     ATTR_ICON,
     ATTR_UNIT_OF_MEASUREMENT,
-    CONF_HOST,
-    CONF_TYPE,
     STATE_UNAVAILABLE,
     TIME_DAYS,
     UNIT_PERCENTAGE,
 )
 from homeassistant.util.dt import utcnow
 
-from tests.common import MockConfigEntry, async_fire_time_changed, load_fixture
+from tests.common import async_fire_time_changed, load_fixture
+from tests.components.brother import init_integration
 
 ATTR_REMAINING_PAGES = "remaining_pages"
 ATTR_COUNTER = "counter"
@@ -24,156 +23,134 @@ ATTR_COUNTER = "counter"
 
 async def test_sensors(hass):
     """Test states of the sensors."""
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        title="HL-L2340DW 0123456789",
-        data={CONF_HOST: "localhost", CONF_TYPE: "laser"},
-    )
-    with patch(
-        "brother.Brother._get_data",
-        return_value=json.loads(load_fixture("brother_printer_data.json")),
-    ):
-        entry.add_to_hass(hass)
-        assert await hass.config_entries.async_setup(entry.entry_id)
-        await hass.async_block_till_done()
+    await init_integration(hass)
 
-        state = hass.states.get("sensor.hl_l2340dw_status")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:printer"
-        assert state.state == "waiting"
+    state = hass.states.get("sensor.hl_l2340dw_status")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:printer"
+    assert state.state == "waiting"
 
-        state = hass.states.get("sensor.hl_l2340dw_black_toner_remaining")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "75"
+    state = hass.states.get("sensor.hl_l2340dw_black_toner_remaining")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "75"
 
-        state = hass.states.get("sensor.hl_l2340dw_cyan_toner_remaining")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "10"
+    state = hass.states.get("sensor.hl_l2340dw_cyan_toner_remaining")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "10"
 
-        state = hass.states.get("sensor.hl_l2340dw_magenta_toner_remaining")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "8"
+    state = hass.states.get("sensor.hl_l2340dw_magenta_toner_remaining")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "8"
 
-        state = hass.states.get("sensor.hl_l2340dw_yellow_toner_remaining")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "2"
+    state = hass.states.get("sensor.hl_l2340dw_yellow_toner_remaining")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "2"
 
-        state = hass.states.get("sensor.hl_l2340dw_drum_remaining_life")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
-        assert state.attributes.get(ATTR_REMAINING_PAGES) == 11014
-        assert state.attributes.get(ATTR_COUNTER) == 986
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "92"
+    state = hass.states.get("sensor.hl_l2340dw_drum_remaining_life")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
+    assert state.attributes.get(ATTR_REMAINING_PAGES) == 11014
+    assert state.attributes.get(ATTR_COUNTER) == 986
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "92"
 
-        state = hass.states.get("sensor.hl_l2340dw_black_drum_remaining_life")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
-        assert state.attributes.get(ATTR_REMAINING_PAGES) == 16389
-        assert state.attributes.get(ATTR_COUNTER) == 1611
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "92"
+    state = hass.states.get("sensor.hl_l2340dw_black_drum_remaining_life")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
+    assert state.attributes.get(ATTR_REMAINING_PAGES) == 16389
+    assert state.attributes.get(ATTR_COUNTER) == 1611
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "92"
 
-        state = hass.states.get("sensor.hl_l2340dw_cyan_drum_remaining_life")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
-        assert state.attributes.get(ATTR_REMAINING_PAGES) == 16389
-        assert state.attributes.get(ATTR_COUNTER) == 1611
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "92"
+    state = hass.states.get("sensor.hl_l2340dw_cyan_drum_remaining_life")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
+    assert state.attributes.get(ATTR_REMAINING_PAGES) == 16389
+    assert state.attributes.get(ATTR_COUNTER) == 1611
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "92"
 
-        state = hass.states.get("sensor.hl_l2340dw_magenta_drum_remaining_life")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
-        assert state.attributes.get(ATTR_REMAINING_PAGES) == 16389
-        assert state.attributes.get(ATTR_COUNTER) == 1611
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "92"
+    state = hass.states.get("sensor.hl_l2340dw_magenta_drum_remaining_life")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
+    assert state.attributes.get(ATTR_REMAINING_PAGES) == 16389
+    assert state.attributes.get(ATTR_COUNTER) == 1611
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "92"
 
-        state = hass.states.get("sensor.hl_l2340dw_yellow_drum_remaining_life")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
-        assert state.attributes.get(ATTR_REMAINING_PAGES) == 16389
-        assert state.attributes.get(ATTR_COUNTER) == 1611
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "92"
+    state = hass.states.get("sensor.hl_l2340dw_yellow_drum_remaining_life")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
+    assert state.attributes.get(ATTR_REMAINING_PAGES) == 16389
+    assert state.attributes.get(ATTR_COUNTER) == 1611
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "92"
 
-        state = hass.states.get("sensor.hl_l2340dw_fuser_remaining_life")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:water-outline"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "97"
+    state = hass.states.get("sensor.hl_l2340dw_fuser_remaining_life")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:water-outline"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "97"
 
-        state = hass.states.get("sensor.hl_l2340dw_belt_unit_remaining_life")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:current-ac"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "97"
+    state = hass.states.get("sensor.hl_l2340dw_belt_unit_remaining_life")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:current-ac"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "97"
 
-        state = hass.states.get("sensor.hl_l2340dw_pf_kit_1_remaining_life")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
-        assert state.state == "98"
+    state = hass.states.get("sensor.hl_l2340dw_pf_kit_1_remaining_life")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
+    assert state.state == "98"
 
-        state = hass.states.get("sensor.hl_l2340dw_page_counter")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
-        assert state.state == "986"
+    state = hass.states.get("sensor.hl_l2340dw_page_counter")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
+    assert state.state == "986"
 
-        state = hass.states.get("sensor.hl_l2340dw_duplex_unit_pages_counter")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
-        assert state.state == "538"
+    state = hass.states.get("sensor.hl_l2340dw_duplex_unit_pages_counter")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
+    assert state.state == "538"
 
-        state = hass.states.get("sensor.hl_l2340dw_b_w_counter")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
-        assert state.state == "709"
+    state = hass.states.get("sensor.hl_l2340dw_b_w_counter")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
+    assert state.state == "709"
 
-        state = hass.states.get("sensor.hl_l2340dw_color_counter")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
-        assert state.state == "902"
+    state = hass.states.get("sensor.hl_l2340dw_color_counter")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
+    assert state.state == "902"
 
-        state = hass.states.get("sensor.hl_l2340dw_uptime")
-        assert state
-        assert state.attributes.get(ATTR_ICON) == "mdi:timer"
-        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == TIME_DAYS
-        assert state.state == "48"
+    state = hass.states.get("sensor.hl_l2340dw_uptime")
+    assert state
+    assert state.attributes.get(ATTR_ICON) == "mdi:timer"
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == TIME_DAYS
+    assert state.state == "48"
 
 
 async def test_availability(hass):
     """Ensure that we mark the entities unavailable correctly when device is offline."""
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        title="HL-L2340DW 0123456789",
-        data={CONF_HOST: "localhost", CONF_TYPE: "laser"},
-    )
-    with patch(
-        "brother.Brother._get_data",
-        return_value=json.loads(load_fixture("brother_printer_data.json")),
-    ):
-        entry.add_to_hass(hass)
-        assert await hass.config_entries.async_setup(entry.entry_id)
-        await hass.async_block_till_done()
+    await init_integration(hass)
 
-        state = hass.states.get("sensor.hl_l2340dw_status")
-        assert state
-        assert state.state != STATE_UNAVAILABLE
-        assert state.state == "waiting"
+    state = hass.states.get("sensor.hl_l2340dw_status")
+    assert state
+    assert state.state != STATE_UNAVAILABLE
+    assert state.state == "waiting"
 
     future = utcnow() + timedelta(minutes=5)
     with patch("brother.Brother._get_data", side_effect=ConnectionError()):
