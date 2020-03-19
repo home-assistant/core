68ac7c6e542a74442124e326da1ebfa6b9e6fe35
Markus Bong <2Fake1987@gmail.com>
2020-03-19 14:13:46 +0100

    fetch upstream

--- homeassistant/components/devolo_home_control/config_flow.py
+++ homeassistant/components/devolo_home_control/config_flow.py
@@ -1,49 +1,85 @@
 """Config flow to configure the devolo home control integration."""
 import logging
 
+from devolo_home_control_api.mydevolo import Mydevolo, WrongCredentialsError
 import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import callback
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 
+def _login_data_valid(user, password):
+    """
+    Validate the given login data.
+
+    Raise WrongCredentialsError if data is wrong.
+    """
+    mydevolo = Mydevolo.get_instance()
+    mydevolo.user = user
+    mydevolo.password = password
+    mydevolo.uuid
+
+
 @config_entries.HANDLERS.register(DOMAIN)
 class DevoloHomeControlFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a iCloud config flow."""
+    """Handle a devolo HomeControl config flow."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     def __init__(self):
         """Initialize devolo Home Control flow."""
-        pass
+        self.data_schema = {
+            vol.Required(CONF_USERNAME, default=""): str,
+            vol.Required(CONF_PASSWORD): str,
+        }
 
     async def _show_setup_form(self, errors=None):
         """Show the setup form to the user."""
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {vol.Required(CONF_USERNAME): str, vol.Required(CONF_PASSWORD): str}
-            ),
+            data_schema=vol.Schema(self.data_schema),
             errors=errors or {},
         )
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initiated by the user."""
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
+        try:
+            if user_input is None:
+                return await self._show_setup_form(user_input)
+            user = user_input.get(CONF_USERNAME)
+            password = user_input.get(CONF_PASSWORD)
+            _login_data_valid(user=user, password=password)
+            _LOGGER.debug("Credentials valid")
+            return self.async_create_entry(
+                title="devolo Home Control",
+                data={
+                    CONF_PASSWORD: user_input.get(CONF_PASSWORD),
+                    CONF_USERNAME: user_input.get(CONF_USERNAME),
+                },
+            )
+        except WrongCredentialsError:
+            return self._show_form({"base": "invalid_credentials"})
 
-        if user_input is None:
-            return await self._show_setup_form(user_input)
-
-        return self.async_create_entry(
-            title="devolo Home Control",
-            data={
-                CONF_PASSWORD: user_input.get(CONF_PASSWORD),
-                CONF_USERNAME: user_input.get(CONF_USERNAME),
-            },
+    @callback
+    def _show_form(self, errors=None):
+        """Show the form to the user."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(self.data_schema),
+            errors=errors if errors else {},
         )
+
+
+def create_config_flow(hass):
+    """Start a config flow."""
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_IMPORT}
+        )
+    )
