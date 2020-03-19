895c78dd823dd0c02b3bb79bba7d2f108199e54a
Markus Bong <2Fake1987@gmail.com>
2020-03-19 14:32:07 +0100

    update branch

--- homeassistant/components/devolo_home_control/__init__.py
+++ homeassistant/components/devolo_home_control/__init__.py
@@ -9,13 +9,12 @@ import voluptuous as vol
 
 from homeassistant.components import switch as ha_switch
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, EVENT_HOMEASSISTANT_STOP
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.typing import HomeAssistantType
 
+from .config_flow import create_config_flow
 from .const import DEFAULT_MPRM, DEFAULT_MYDEVOLO, DOMAIN, PLATFORMS
-
-CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
 SUPPORTED_PLATFORMS = [ha_switch.DOMAIN]
 
@@ -37,6 +36,7 @@ async def async_setup(hass, config):
     """Get all devices and add them to hass."""
     mydevolo = Mydevolo()
     try:
+        # We need this because of staging purposes, but don't want to show this on the UI
         mydevolo.url = config.get(DOMAIN).get("mydevolo")
         mydevolo.mprm = config.get(DOMAIN).get("mprm")
     except AttributeError:
@@ -54,8 +54,8 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
         mydevolo = Mydevolo.get_instance()
         mydevolo.user = conf.get(CONF_USERNAME)
         mydevolo.password = conf.get(CONF_PASSWORD)
-        # mydevolo.url = conf.get("mydevolo")
     except (WrongCredentialsError, WrongUrlError):
+        create_config_flow(hass=hass)
         return False
 
     if mydevolo.maintenance:
@@ -76,4 +76,20 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, platform)
         )
+
+    # Listen when EVENT_HOMEASSISTANT_STOP is fired
+    hass.bus.async_listen_once(
+        EVENT_HOMEASSISTANT_STOP, hass.data[DOMAIN]["homecontrol"].websocket_disconnect
+    )
+
+    return True
+
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(config_entry, "switch")
+
+    if hass.data[DOMAIN]:
+        hass.data[DOMAIN]["homecontrol"].websocket_disconnect()
+        del hass.data[DOMAIN]["homecontrol"]
     return True
