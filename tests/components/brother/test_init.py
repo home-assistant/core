aa784ecaf47f5abe61db50e1f2109c8a80080847
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-03-19 11:10:04 +0100

    Test improvement

--- tests/components/brother/test_init.py
+++ tests/components/brother/test_init.py
@@ -2,7 +2,11 @@
 from asynctest import patch
 
 from homeassistant.components.brother.const import DOMAIN
-from homeassistant.config_entries import ENTRY_STATE_SETUP_RETRY
+from homeassistant.config_entries import (
+    ENTRY_STATE_LOADED,
+    ENTRY_STATE_NOT_LOADED,
+    ENTRY_STATE_SETUP_RETRY,
+)
 from homeassistant.const import CONF_HOST, CONF_TYPE, STATE_UNAVAILABLE
 
 from tests.common import MockConfigEntry
@@ -38,7 +42,11 @@ async def test_unload_entry(hass):
     """Test successful unload of entry."""
     entry = await init_integration(hass)
 
-    assert hass.data[DOMAIN][entry.entry_id]
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+    assert entry.state == ENTRY_STATE_LOADED
 
     assert await hass.config_entries.async_unload(entry.entry_id)
-    assert not hass.data[DOMAIN]
+    await hass.async_block_till_done()
+
+    assert entry.state == ENTRY_STATE_NOT_LOADED
+    assert not hass.data.get(DOMAIN)
--- tests/components/brother/test_sensor.py
+++ tests/components/brother/test_sensor.py
@@ -4,14 +4,17 @@ import json
 
 from asynctest import patch
 
+from homeassistant.components.brother import BrotherDataUpdateCoordinator
 from homeassistant.components.brother.const import UNIT_PAGES
 from homeassistant.const import (
+    ATTR_ENTITY_ID,
     ATTR_ICON,
     ATTR_UNIT_OF_MEASUREMENT,
     STATE_UNAVAILABLE,
     TIME_DAYS,
     UNIT_PERCENTAGE,
 )
+from homeassistant.setup import async_setup_component
 from homeassistant.util.dt import utcnow
 
 from tests.common import async_fire_time_changed, load_fixture
@@ -24,11 +27,16 @@ ATTR_COUNTER = "counter"
 async def test_sensors(hass):
     """Test states of the sensors."""
     await init_integration(hass)
+    registry = await hass.helpers.entity_registry.async_get_registry()
 
     state = hass.states.get("sensor.hl_l2340dw_status")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:printer"
     assert state.state == "waiting"
+
+    entry = registry.async_get("sensor.hl_l2340dw_status")
+    assert entry
+    assert entry.unique_id == "0123456789_status"
 
     state = hass.states.get("sensor.hl_l2340dw_black_toner_remaining")
     assert state
@@ -36,11 +44,19 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "75"
 
+    entry = registry.async_get("sensor.hl_l2340dw_black_toner_remaining")
+    assert entry
+    assert entry.unique_id == "0123456789_black_toner_remaining"
+
     state = hass.states.get("sensor.hl_l2340dw_cyan_toner_remaining")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "10"
+
+    entry = registry.async_get("sensor.hl_l2340dw_cyan_toner_remaining")
+    assert entry
+    assert entry.unique_id == "0123456789_cyan_toner_remaining"
 
     state = hass.states.get("sensor.hl_l2340dw_magenta_toner_remaining")
     assert state
@@ -48,11 +64,19 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "8"
 
+    entry = registry.async_get("sensor.hl_l2340dw_magenta_toner_remaining")
+    assert entry
+    assert entry.unique_id == "0123456789_magenta_toner_remaining"
+
     state = hass.states.get("sensor.hl_l2340dw_yellow_toner_remaining")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d-nozzle"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "2"
+
+    entry = registry.async_get("sensor.hl_l2340dw_yellow_toner_remaining")
+    assert entry
+    assert entry.unique_id == "0123456789_yellow_toner_remaining"
 
     state = hass.states.get("sensor.hl_l2340dw_drum_remaining_life")
     assert state
@@ -62,6 +86,10 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "92"
 
+    entry = registry.async_get("sensor.hl_l2340dw_drum_remaining_life")
+    assert entry
+    assert entry.unique_id == "0123456789_drum_remaining_life"
+
     state = hass.states.get("sensor.hl_l2340dw_black_drum_remaining_life")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
@@ -69,6 +97,10 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_COUNTER) == 1611
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "92"
+
+    entry = registry.async_get("sensor.hl_l2340dw_black_drum_remaining_life")
+    assert entry
+    assert entry.unique_id == "0123456789_black_drum_remaining_life"
 
     state = hass.states.get("sensor.hl_l2340dw_cyan_drum_remaining_life")
     assert state
@@ -78,6 +110,10 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "92"
 
+    entry = registry.async_get("sensor.hl_l2340dw_cyan_drum_remaining_life")
+    assert entry
+    assert entry.unique_id == "0123456789_cyan_drum_remaining_life"
+
     state = hass.states.get("sensor.hl_l2340dw_magenta_drum_remaining_life")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:chart-donut"
@@ -85,6 +121,10 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_COUNTER) == 1611
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "92"
+
+    entry = registry.async_get("sensor.hl_l2340dw_magenta_drum_remaining_life")
+    assert entry
+    assert entry.unique_id == "0123456789_magenta_drum_remaining_life"
 
     state = hass.states.get("sensor.hl_l2340dw_yellow_drum_remaining_life")
     assert state
@@ -94,11 +134,19 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "92"
 
+    entry = registry.async_get("sensor.hl_l2340dw_yellow_drum_remaining_life")
+    assert entry
+    assert entry.unique_id == "0123456789_yellow_drum_remaining_life"
+
     state = hass.states.get("sensor.hl_l2340dw_fuser_remaining_life")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:water-outline"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "97"
+
+    entry = registry.async_get("sensor.hl_l2340dw_fuser_remaining_life")
+    assert entry
+    assert entry.unique_id == "0123456789_fuser_remaining_life"
 
     state = hass.states.get("sensor.hl_l2340dw_belt_unit_remaining_life")
     assert state
@@ -106,11 +154,19 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "97"
 
+    entry = registry.async_get("sensor.hl_l2340dw_belt_unit_remaining_life")
+    assert entry
+    assert entry.unique_id == "0123456789_belt_unit_remaining_life"
+
     state = hass.states.get("sensor.hl_l2340dw_pf_kit_1_remaining_life")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:printer-3d"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PERCENTAGE
     assert state.state == "98"
+
+    entry = registry.async_get("sensor.hl_l2340dw_pf_kit_1_remaining_life")
+    assert entry
+    assert entry.unique_id == "0123456789_pf_kit_1_remaining_life"
 
     state = hass.states.get("sensor.hl_l2340dw_page_counter")
     assert state
@@ -118,11 +174,19 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
     assert state.state == "986"
 
+    entry = registry.async_get("sensor.hl_l2340dw_page_counter")
+    assert entry
+    assert entry.unique_id == "0123456789_page_counter"
+
     state = hass.states.get("sensor.hl_l2340dw_duplex_unit_pages_counter")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
     assert state.state == "538"
+
+    entry = registry.async_get("sensor.hl_l2340dw_duplex_unit_pages_counter")
+    assert entry
+    assert entry.unique_id == "0123456789_duplex_unit_pages_counter"
 
     state = hass.states.get("sensor.hl_l2340dw_b_w_counter")
     assert state
@@ -130,17 +194,29 @@ async def test_sensors(hass):
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
     assert state.state == "709"
 
+    entry = registry.async_get("sensor.hl_l2340dw_b_w_counter")
+    assert entry
+    assert entry.unique_id == "0123456789_b/w_counter"
+
     state = hass.states.get("sensor.hl_l2340dw_color_counter")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:file-document-outline"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UNIT_PAGES
     assert state.state == "902"
 
+    entry = registry.async_get("sensor.hl_l2340dw_color_counter")
+    assert entry
+    assert entry.unique_id == "0123456789_color_counter"
+
     state = hass.states.get("sensor.hl_l2340dw_uptime")
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:timer"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == TIME_DAYS
     assert state.state == "48"
+
+    entry = registry.async_get("sensor.hl_l2340dw_uptime")
+    assert entry
+    assert entry.unique_id == "0123456789_uptime"
 
 
 async def test_availability(hass):
@@ -173,3 +249,21 @@ async def test_availability(hass):
         assert state
         assert state.state != STATE_UNAVAILABLE
         assert state.state == "waiting"
+
+
+async def test_manual_update_entity(hass):
+    """Test manual update entity via service homeasasistant/update_entity."""
+    await init_integration(hass)
+
+    await async_setup_component(hass, "homeassistant", {})
+    with patch.object(
+        BrotherDataUpdateCoordinator, "_async_update_data", return_value=True
+    ) as mock_update:
+        await hass.services.async_call(
+            "homeassistant",
+            "update_entity",
+            {ATTR_ENTITY_ID: ["sensor.hl_l2340dw_status"]},
+            blocking=True,
+        )
+
+        assert len(mock_update.mock_calls) == 1
