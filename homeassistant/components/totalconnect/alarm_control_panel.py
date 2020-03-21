4d69a406fc91754795d6656f67372dc13ad071ba
Austin Mroczek <austin@mroczek.org>
2020-03-21 11:45:07 -0700

    Remove period from loggging message

--- homeassistant/components/totalconnect/alarm_control_panel.py
+++ homeassistant/components/totalconnect/alarm_control_panel.py
@@ -108,7 +108,7 @@ class TotalConnectAlarm(alarm.AlarmControlPanel):
             state = STATE_ALARM_TRIGGERED
             attr["triggered_source"] = "Carbon Monoxide"
         else:
-            logging.info("Total Connect Client returned unknown status.")
+            logging.info("Total Connect Client returned unknown status")
             state = None
 
         self._state = state
