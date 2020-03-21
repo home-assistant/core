ce9eb33375d0416edc13b0f4ceacdb34bfb4029b
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-03-21 10:32:30 +0100

    Remove debug file

--- bravia.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-from bravia_tv import BraviaRC
-
-host = argv[1]
-pin = argv[2]
-braviarc = BraviaRC(host)
-braviarc.connect(str(pin), "HomeAssistant", "Home Assistant")
-print(braviarc.get_system_info())
