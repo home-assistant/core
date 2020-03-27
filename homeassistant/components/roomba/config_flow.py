bc6d357af931428274d7438f1d9d4cd3771bdcb9
cyr-ius <cyr-ius@ipocus.net>
2020-03-27 08:23:09 +0100

    Fix options in config flow

--- homeassistant/components/roomba/config_flow.py
+++ homeassistant/components/roomba/config_flow.py
@@ -121,15 +121,11 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
-        return await self.async_step_options()
-
-    async def async_step_options(self, user_input=None):
-        """Manage the options."""
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
-            step_id="options",
+            step_id="init",
             data_schema=vol.Schema(
                 {
                     vol.Optional(
