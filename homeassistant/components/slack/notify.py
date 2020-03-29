af454681d9da1a32add463c6a4a3a7309f24fa25
Aaron Bach <bachya1208@gmail.com>
2020-03-29 14:54:39 -0600

    Remove remote file option

--- homeassistant/components/slack/notify.py
+++ homeassistant/components/slack/notify.py
@@ -4,9 +4,6 @@ import logging
 import os
 from urllib.parse import urlparse
 
-import requests
-from requests.auth import HTTPBasicAuth, HTTPDigestAuth
-from requests.exceptions import RequestException
 from slack import WebClient
 from slack.errors import SlackApiError
 import voluptuous as vol
@@ -18,26 +15,15 @@ from homeassistant.components.notify import (
     PLATFORM_SCHEMA,
     BaseNotificationService,
 )
-from homeassistant.const import (
-    CONF_API_KEY,
-    CONF_ICON,
-    CONF_USERNAME,
-    HTTP_BASIC_AUTHENTICATION,
-    HTTP_DIGEST_AUTHENTICATION,
-)
+from homeassistant.const import CONF_API_KEY, CONF_ICON, CONF_USERNAME
 from homeassistant.core import callback
 from homeassistant.helpers import aiohttp_client, config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_ATTACHMENTS = "attachments"
-ATTR_AUTH_TYPE = "auth"
 ATTR_BLOCKS = "blocks"
 ATTR_FILE = "file"
-ATTR_FILE_URL = "url"
-ATTR_FILE_PATH = "path"
-ATTR_PASSWORD = "password"
-ATTR_USERNAME = "username"
 
 CONF_DEFAULT_CHANNEL = "default_channel"
 
@@ -72,31 +58,6 @@ async def async_get_service(hass, config, discovery_info=None):
         username=config.get(CONF_USERNAME),
         icon=config.get(CONF_ICON),
     )
-
-
-def _get_remote_file_contents(url, username=None, password=None, auth_type=None):
-    """Retrieve a remote file via URL and return its binary content.
-
-    Note that we use a sync method (and the requests library) because aiohttp does not
-    currently have a helper for digest authentication.
-    """
-    if auth_type == HTTP_BASIC_AUTHENTICATION:
-        resp = requests.get(
-            url,
-            auth=HTTPBasicAuth(username, password),
-            timeout=DEFAULT_TIMEOUT_SECONDS,
-        )
-    elif auth_type == HTTP_DIGEST_AUTHENTICATION:
-        resp = requests.get(
-            url,
-            auth=HTTPDigestAuth(username, password),
-            timeout=DEFAULT_TIMEOUT_SECONDS,
-        )
-    else:
-        resp = requests.get(url, timeout=DEFAULT_TIMEOUT_SECONDS)
-
-    resp.raise_for_status()
-    return resp.content
 
 
 @callback
@@ -148,7 +109,7 @@ class SlackNotificationService(BaseNotificationService):
         except SlackApiError as err:
             _LOGGER.error("Error while uploading file-based message: %s", err)
 
-    async def _async_send_regular_message(
+    async def _async_send_text_only_message(
         self, targets, message, title, attachments, blocks
     ):
         """Send a text-only message."""
@@ -174,31 +135,6 @@ class SlackNotificationService(BaseNotificationService):
                     result,
                 )
 
-    async def _async_send_remote_file_message(
-        self, url, targets, message, title, username, password, auth_type
-    ):
-        """Upload a remote file (with message) to Slack."""
-        try:
-            file_as_bytes = await self._hass.async_add_executor_job(
-                _get_remote_file_contents, url, username, password, auth_type
-            )
-        except RequestException as err:
-            _LOGGER.error("Error while retrieving %s: %s", url, err)
-            return
-
-        filename = _async_get_filename_from_url(url)
-
-        try:
-            await self._client.files_upload(
-                channels=",".join(targets),
-                file=file_as_bytes,
-                filename=filename,
-                initial_comment=message,
-                title=title or filename,
-            )
-        except SlackApiError as err:
-            _LOGGER.error("Error while uploading file-based message: %s", err)
-
     async def async_send_message(self, message, **kwargs):
         """Send a message to Slack."""
         data = kwargs[ATTR_DATA] or {}
@@ -207,37 +143,20 @@ class SlackNotificationService(BaseNotificationService):
             kwargs.get(ATTR_TARGET, [self._default_channel])
         )
 
-        if ATTR_FILE not in data:
-            attachments = data.get(ATTR_ATTACHMENTS, {})
-            if attachments:
-                _LOGGER.warning(
-                    "Attachments are now part of Slack's legacy API; "
-                    "in most cases, Blocks should be used instead: "
-                    "https://www.home-assistant.io/integrations/slack/"
-                )
-            blocks = data.get(ATTR_BLOCKS, {})
-
-            return await self._async_send_regular_message(
-                targets, message, title, attachments, blocks
-            )
-
-        file_data = data[ATTR_FILE]
-
-        if ATTR_FILE_PATH not in file_data and ATTR_FILE_URL not in file_data:
-            _LOGGER.error('A file path/URL must be provided when using the "file" key')
-            return
-
-        if ATTR_FILE_PATH in file_data:
+        if ATTR_FILE in data:
             return await self._async_send_local_file_message(
-                file_data[ATTR_FILE_PATH], targets, message, title
+                data[ATTR_FILE], targets, message, title
             )
 
-        return await self._async_send_remote_file_message(
-            file_data[ATTR_FILE_URL],
-            targets,
-            message,
-            title,
-            data.get(ATTR_USERNAME),
-            data.get(ATTR_PASSWORD),
-            data.get(ATTR_AUTH_TYPE),
+        attachments = data.get(ATTR_ATTACHMENTS, {})
+        if attachments:
+            _LOGGER.warning(
+                "Attachments are now part of Slack's legacy API; "
+                "in most cases, Blocks should be used instead: "
+                "https://www.home-assistant.io/integrations/slack/"
+            )
+        blocks = data.get(ATTR_BLOCKS, {})
+
+        return await self._async_send_text_only_message(
+            targets, message, title, attachments, blocks
         )
