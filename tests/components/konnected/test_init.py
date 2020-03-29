253fdf1718ff3987c907ef129c05c3356a47da30
Kit Klein <kit@kleinlabs.org>
2020-03-29 14:41:18 -0400

    add test to for importing multiple output settings

--- tests/components/konnected/test_init.py
+++ tests/components/konnected/test_init.py
@@ -124,7 +124,7 @@ async def test_config_schema(hass):
         }
     }
 
-    # check pin to zone
+    # check pin to zone and multiple output
     config = {
         konnected.DOMAIN: {
             konnected.CONF_ACCESS_TOKEN: "abcdefgh",
@@ -134,6 +134,22 @@ async def test_config_schema(hass):
                     "binary_sensors": [
                         {"pin": 2, "type": "door"},
                         {"zone": 1, "type": "door"},
+                    ],
+                    "switches": [
+                        {
+                            "zone": 3,
+                            "name": "Beep Beep",
+                            "momentary": 65,
+                            "pause": 55,
+                            "repeat": 4,
+                        },
+                        {
+                            "zone": 3,
+                            "name": "Warning",
+                            "momentary": 100,
+                            "pause": 100,
+                            "repeat": -1,
+                        },
                     ],
                 }
             ],
@@ -153,7 +169,7 @@ async def test_config_schema(hass):
                             "11": "Disabled",
                             "12": "Disabled",
                             "2": "Binary Sensor",
-                            "3": "Disabled",
+                            "3": "Switchable Output",
                             "4": "Disabled",
                             "5": "Disabled",
                             "6": "Disabled",
@@ -168,6 +184,24 @@ async def test_config_schema(hass):
                         "binary_sensors": [
                             {"inverse": False, "type": "door", "zone": "2"},
                             {"inverse": False, "type": "door", "zone": "1"},
+                        ],
+                        "switches": [
+                            {
+                                "zone": "3",
+                                "activation": "high",
+                                "name": "Beep Beep",
+                                "momentary": 65,
+                                "pause": 55,
+                                "repeat": 4,
+                            },
+                            {
+                                "zone": "3",
+                                "activation": "high",
+                                "name": "Warning",
+                                "momentary": 100,
+                                "pause": 100,
+                                "repeat": -1,
+                            },
                         ],
                     },
                     "id": "aabbccddeeff",
