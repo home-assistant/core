1410e0dd59b9dd855d9efff2f6cc5e8f4ccc6d4a
Chris Talkington <chris@talkingtontech.com>
2020-03-29 17:15:04 -0500

    Update const.py

--- homeassistant/components/ipp/const.py
+++ homeassistant/components/ipp/const.py
@@ -4,20 +4,20 @@
 DOMAIN = "ipp"
 
 # Attributes
+ATTR_COMMAND_SET = "command_set"
 ATTR_IDENTIFIERS = "identifiers"
+ATTR_INFO = "info"
+ATTR_LOCATION = "location"
 ATTR_MANUFACTURER = "manufacturer"
 ATTR_MARKER_TYPE = "marker_type"
 ATTR_MARKER_LOW_LEVEL = "marker_low_level"
 ATTR_MARKER_HIGH_LEVEL = "marker_high_level"
 ATTR_MODEL = "model"
-ATTR_PRINTER_COMMAND_SET = "printer_command_set"
-ATTR_PRINTER_INFO = "printer_info"
-ATTR_PRINTER_LOCATION = "printer_location"
-ATTR_PRINTER_SERIAL = "printer_serial"
-ATTR_PRINTER_STATE_MESSAGE = "printer_state_message"
-ATTR_PRINTER_STATE_REASON = "printer_state_reason"
-ATTR_PRINTER_URI_SUPPORTED = "printer_uri_supported"
+ATTR_SERIAL = "serial"
 ATTR_SOFTWARE_VERSION = "sw_version"
+ATTR_STATE_MESSAGE = "state_message"
+ATTR_STATE_REASON = "state_reason"
+ATTR_URI_SUPPORTED = "uri_supported"
 
 # Config Keys
 CONF_BASE_PATH = "base_path"
