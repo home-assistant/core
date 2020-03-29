f44a8c66ebe3cd0828134b8514a00817cafd148c
FlavorFx <michael.schwaer@gmx.de>
2020-03-29 23:16:47 +0200

    Update homeassistant/components/tankerkoenig/sensor.py
    Co-Authored-By: guillempages <guillempages@users.noreply.github.com>

--- homeassistant/components/tankerkoenig/sensor.py
+++ homeassistant/components/tankerkoenig/sensor.py
@@ -59,7 +59,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
                 )
                 continue
             sensor = FuelPriceSensor(
-                fuel, station, coordinator, f"{NAME}_{station['name']}_{fuel}", show
+                fuel, station, coordinator, f"{NAME}_{station['name']}_{fuel}", tankerkoenig.show_on_map
             )
             entities.append(sensor)
     _LOGGER.debug("Added sensors %s", entities)
