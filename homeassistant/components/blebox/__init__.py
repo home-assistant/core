94cd788abceec3d037a421661544a567d627065b
gadgetmobile <57815233+gadgetmobile@users.noreply.github.com>
2020-03-30 21:11:11 +0200

    Update homeassistant/components/blebox/__init__.py
    require CONF_HOST in config
    
    Co-Authored-By: J. Nick Koston <nick@koston.org>

--- homeassistant/components/blebox/__init__.py
+++ homeassistant/components/blebox/__init__.py
@@ -55,7 +55,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
 
 async def async_add_blebox(klass, method, hass, config, async_add, exception):
     """Add a BleBox device from the given config."""
-    host = config.get(CONF_HOST)
+    host = config[CONF_HOST]
     port = config.get(CONF_PORT)
     # timeout = config.get(CONF_TIMEOUT)
     timeout = 3
