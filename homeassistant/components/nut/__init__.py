bed35c3f9897d6dfc18898d00cf71a6ba0353ffb
J. Nick Koston <nick@koston.org>
2020-03-31 03:13:14 +0000

    Convert nut to config flow

--- homeassistant/components/nut/__init__.py
+++ homeassistant/components/nut/__init__.py
@@ -1,1 +1,159 @@
 """The nut component."""
+import asyncio
+import logging
+
+from pynut2.nut2 import PyNUTClient, PyNUTError
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    CONF_ALIAS,
+    CONF_HOST,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_RESOURCES,
+    CONF_USERNAME,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+
+from .const import DOMAIN, PLATFORMS, PYNUT_DATA, PYNUT_STATUS, PYNUT_UNIQUE_ID
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Network UPS Tools (NUT) component."""
+    hass.data.setdefault(DOMAIN, {})
+
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Network UPS Tools (NUT) from a config entry."""
+
+    config = entry.data
+    host = config[CONF_HOST]
+    port = config[CONF_PORT]
+
+    alias = config.get(CONF_ALIAS)
+    username = config.get(CONF_USERNAME)
+    password = config.get(CONF_PASSWORD)
+
+    data = PyNUTData(host, port, alias, username, password)
+
+    status = await hass.async_add_executor_job(pynutdata_status, data)
+
+    if not status:
+        _LOGGER.error("NUT Sensor has no data, unable to set up")
+        raise ConfigEntryNotReady
+
+    _LOGGER.debug("NUT Sensors Available: %s", status)
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        PYNUT_DATA: data,
+        PYNUT_STATUS: status,
+        PYNUT_UNIQUE_ID: unique_id_from_status(status, host, port),
+    }
+
+    entry.add_update_listener(_async_update_listener)
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    return True
+
+
+async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
+
+
+def unique_id_from_status(status, host, port):
+    """Find the best unique id from the status."""
+    # The serial number should always exist, however some off brand
+    # UPSs do not send one so we use the next best thing
+    return status.get("device.serial") or status.get("ups.serial") or f"{host}:{port}"
+
+
+def find_resources_in_config_entry(config_entry):
+    """Find the configured resources in the config entry."""
+    if CONF_RESOURCES in config_entry.options:
+        return config_entry.options[CONF_RESOURCES]
+    return config_entry.data[CONF_RESOURCES]
+
+
+def pynutdata_status(data):
+    """Wrap for data update as a callable."""
+    return data.status
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
+
+class PyNUTData:
+    """Stores the data retrieved from NUT.
+
+    For each entity to use, acts as the single point responsible for fetching
+    updates from the server.
+    """
+
+    def __init__(self, host, port, alias, username, password):
+        """Initialize the data object."""
+
+        self._host = host
+        self._port = port
+        self._alias = alias
+        self._username = username
+        self._password = password
+
+        self.pynuterror = PyNUTError
+        # Establish client with persistent=False to open/close connection on
+        # each update call.  This is more reliable with async.
+        self._client = PyNUTClient(
+            self._host, self._port, self._username, self._password, 5, False
+        )
+        self._status = None
+
+    @property
+    def status(self):
+        """Get latest update if throttle allows. Return status."""
+        self.update()
+        return self._status
+
+    def _get_alias(self):
+        """Get the ups alias from NUT."""
+        try:
+            return next(iter(self._client.list_ups()))
+        except self.pynuterror as err:
+            _LOGGER.error("Failure getting NUT ups alias, %s", err)
+            return None
+
+    def _get_status(self):
+        """Get the ups status from NUT."""
+        if self._alias is None:
+            self._alias = self._get_alias()
+
+        try:
+            return self._client.list_vars(self._alias)
+        except (self.pynuterror, ConnectionResetError) as err:
+            _LOGGER.debug("Error getting NUT vars for host %s: %s", self._host, err)
+            return None
+
+    def update(self, **kwargs):
+        """Fetch the latest status from NUT."""
+        self._status = self._get_status()
---
+++ homeassistant/components/nut/config_flow.py
@@ -0,0 +1,133 @@
+"""Config flow for Network UPS Tools (NUT) integration."""
+import logging
+
+import voluptuous as vol
+
+from homeassistant import config_entries, core, exceptions
+from homeassistant.const import (
+    CONF_ALIAS,
+    CONF_HOST,
+    CONF_NAME,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_RESOURCES,
+    CONF_USERNAME,
+)
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+
+from . import (
+    PyNUTData,
+    find_resources_in_config_entry,
+    pynutdata_status,
+    unique_id_from_status,
+)
+from .const import DEFAULT_HOST, DEFAULT_NAME, DEFAULT_PORT, SENSOR_TYPES
+from .const import DOMAIN  # pylint:disable=unused-import
+
+_LOGGER = logging.getLogger(__name__)
+
+
+sensor_dict = {sensor_id: SENSOR_TYPES[sensor_id][0] for sensor_id in SENSOR_TYPES}
+
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
+        vol.Required(CONF_RESOURCES): cv.multi_select(sensor_dict),
+        vol.Optional(CONF_HOST, default=DEFAULT_HOST): str,
+        vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
+        vol.Optional(CONF_ALIAS): str,
+        vol.Optional(CONF_USERNAME): str,
+        vol.Optional(CONF_PASSWORD): str,
+    }
+)
+
+
+async def validate_input(hass: core.HomeAssistant, data):
+    """Validate the user input allows us to connect.
+
+    Data has the keys from DATA_SCHEMA with values provided by the user.
+    """
+
+    host = data[CONF_HOST]
+    port = data[CONF_PORT]
+    alias = data.get(CONF_ALIAS)
+    username = data.get(CONF_USERNAME)
+    password = data.get(CONF_PASSWORD)
+
+    data = PyNUTData(host, port, alias, username, password)
+
+    status = await hass.async_add_executor_job(pynutdata_status, data)
+
+    if not status:
+        raise CannotConnect
+
+    unique_id = unique_id_from_status(status, host, port)
+
+    return {"title": unique_id}
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Network UPS Tools (NUT)."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            try:
+                info = await validate_input(self.hass, user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+
+            if "base" not in errors:
+                await self.async_set_unique_id(info["title"])
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(title=info["title"], data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_import(self, user_input):
+        """Handle import."""
+        return await self.async_step_user(user_input)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return OptionsFlowHandler(config_entry)
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle a option flow for nut."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Handle options flow."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        resources = find_resources_in_config_entry(self.config_entry)
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_RESOURCES, default=resources): cv.multi_select(
+                    sensor_dict
+                ),
+            }
+        )
+        return self.async_show_form(step_id="init", data_schema=data_schema)
+
+
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
