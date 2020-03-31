60ec771bc3bc156ca9ca01340db434bbdc8ad0ae
David Mulcahey <david.mulcahey@me.com>
2020-03-31 19:05:48 -0400

    review comment

--- homeassistant/helpers/storage.py
+++ homeassistant/helpers/storage.py
@@ -149,6 +149,7 @@ class Store:
         self._async_cleanup_final_write_listener()
 
         if self.hass.state == CoreState.stopping:
+            self._async_ensure_final_write_listener()
             return
 
         self._unsub_delay_listener = async_call_later(
