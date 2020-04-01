861f786fb43055cb20ba2abdbd92cd763d72362d
Alistair Galbraith <alistair@alistairs.net>
2020-04-01 11:30:28 -0700

    Removed assumption that template render may not be a string

--- homeassistant/components/template/light.py
+++ homeassistant/components/template/light.py
@@ -378,7 +378,7 @@ class LightTemplate(Light):
             return
         try:
             brightness = self._level_template.async_render()
-            if brightness is None or brightness == "None" or brightness == "":
+            if brightness == "None" or brightness == "":
                 self._brightness = None
                 return
             if 0 <= int(brightness) <= 255:
@@ -419,7 +419,7 @@ class LightTemplate(Light):
             return
         try:
             render = self._temperature_template.async_render()
-            if render is None or render == "None" or render == "":
+            if render == "None" or render == "":
                 return
             temperature = int(render)
             if self.min_mireds <= temperature <= self.max_mireds:
@@ -445,7 +445,7 @@ class LightTemplate(Light):
 
         try:
             render = self._color_template.async_render()
-            if render is None or render == "None" or render == "":
+            if render == "None" or render == "":
                 return
             h_str, s_str = map(
                 float, render.replace("(", "").replace(")", "").split(",", 1)
