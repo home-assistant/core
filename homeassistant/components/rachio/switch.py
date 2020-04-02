4a9bdf1d15f036d210be24a82e0416aa4c0921d2
brg468 <brg468@hotmail.com>
2020-04-02 13:13:57 -0400

    Black formatting

--- homeassistant/components/rachio/switch.py
+++ homeassistant/components/rachio/switch.py
@@ -339,9 +339,7 @@ class RachioSchedule(RachioSwitch):
 
         self._controller.rachio.schedulerule.start(self._schedule_id)
         _LOGGER.debug(
-            "Schedule %s started on %s",
-            self.name,
-            self._controller.name,
+            "Schedule %s started on %s", self.name, self._controller.name,
         )
 
     def turn_off(self, **kwargs) -> None:
