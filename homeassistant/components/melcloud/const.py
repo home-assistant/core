9f5c8db760a03c2521e1728fc96f0eb0a9d68b51
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2020-04-02 22:56:41 +0300

    Add melcloud AtaDevice vane control (#32672)
    * Add melcloud AtaDevice vane control
    
    * Return empty dict when no vane states available
    
    Co-Authored-By: springstan <46536646+springstan@users.noreply.github.com>
    
    * Use constants for services and conf
    
    * Split state attribute assignment and fix suggested changes
    
    * Log valid positions when called with an invalid position
    
    * Improve service description
    
    Co-Authored-By: springstan <46536646+springstan@users.noreply.github.com>
    
    Co-authored-by: springstan <46536646+springstan@users.noreply.github.com>

--- homeassistant/components/melcloud/const.py
+++ homeassistant/components/melcloud/const.py
@@ -5,7 +5,16 @@ from homeassistant.const import TEMP_CELSIUS, TEMP_FAHRENHEIT
 
 DOMAIN = "melcloud"
 
+CONF_POSITION = "position"
+
 ATTR_STATUS = "status"
+ATTR_VANE_HORIZONTAL = "vane_horizontal"
+ATTR_VANE_HORIZONTAL_POSITIONS = "vane_horizontal_positions"
+ATTR_VANE_VERTICAL = "vane_vertical"
+ATTR_VANE_VERTICAL_POSITIONS = "vane_vertical_positions"
+
+SERVICE_SET_VANE_HORIZONTAL = "set_vane_horizontal"
+SERVICE_SET_VANE_VERTICAL = "set_vane_vertical"
 
 TEMP_UNIT_LOOKUP = {
     UNIT_TEMP_CELSIUS: TEMP_CELSIUS,
