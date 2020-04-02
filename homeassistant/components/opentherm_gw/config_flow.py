ac7f6bd875d7a4ceb071b18bdc771b0ff245d2ff
mvn23 <schopdiedwaas@gmail.com>
2020-04-02 14:03:33 +0200

    Update pyotgw to 0.6b1

--- homeassistant/components/opentherm_gw/config_flow.py
+++ homeassistant/components/opentherm_gw/config_flow.py
@@ -2,6 +2,7 @@
 import asyncio
 
 import pyotgw
+from pyotgw import vars as gw_vars
 from serial import SerialException
 import voluptuous as vol
 
@@ -53,7 +54,7 @@ class OpenThermGwConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 otgw = pyotgw.pyotgw()
                 status = await otgw.connect(self.hass.loop, device)
                 await otgw.disconnect()
-                return status.get(pyotgw.OTGW_ABOUT)
+                return status.get(gw_vars.OTGW_ABOUT)
 
             try:
                 res = await asyncio.wait_for(test_connection(), timeout=10)
