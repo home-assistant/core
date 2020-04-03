6b6cb9772825bbf84fefe41c61cc9d4f74c64f3c
ollo69 <60491700+ollo69@users.noreply.github.com>
2020-04-03 13:48:09 +0200

    Format correction

--- tests/components/asuswrt/test_device_tracker.py
+++ tests/components/asuswrt/test_device_tracker.py
@@ -27,7 +27,7 @@ async def test_password_or_pub_key_required(hass):
 async def test_network_unreachable(hass):
     """Test creating an AsusWRT scanner without a pass or pubkey."""
     with patch("homeassistant.components.asuswrt.AsusWrt") as AsusWrt:
-        AsusWrt().connection.async_connect = mock_coro_func(exception = OSError)
+        AsusWrt().connection.async_connect = mock_coro_func(exception=OSError)
         AsusWrt().is_connected = False
         result = await async_setup_component(
             hass, DOMAIN, {DOMAIN: {CONF_HOST: "fake_host", CONF_USERNAME: "fake_user"}}
