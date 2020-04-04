c63ac21d2d4d9ddde6c1376e8a130e003d0ad914
Mathieu Pasquet <mathieu@pasquet.co>
2020-04-04 23:06:54 +0100

    Add zones support

--- homeassistant/components/onkyo/media_player.py
+++ homeassistant/components/onkyo/media_player.py
@@ -12,6 +12,8 @@ from homeassistant.components.media_player.const import (
     SUPPORT_VOLUME_MUTE,
     SUPPORT_VOLUME_SET,
     SUPPORT_VOLUME_STEP,
+    SUPPORT_PLAY,
+    SUPPORT_PLAY_MEDIA,
 )
 from homeassistant.const import (
     CONF_HOST,
@@ -30,12 +32,51 @@ DOMAIN = "onkyo"
 
 DEFAULT_PORT = 60128
 
-SUPPORT_ANTHEMAV = (
+CONF_SOURCES = "sources"
+CONF_MAX_VOLUME = "max_volume"
+CONF_RECEIVER_MAX_VOLUME = "receiver_max_volume"
+CONF_ZONES = "zones"
+
+DEFAULT_NAME = "Onkyo Receiver"
+SUPPORTED_MAX_VOLUME = 100
+DEFAULT_RECEIVER_MAX_VOLUME = 80
+ZONES = ["zone1", "zone2", "zone3", "zone4"]
+
+DEFAULT_SOURCES = {
+    "tv": "TV",
+    "bd": "Bluray",
+    "game": "Game",
+    "aux1": "Aux1",
+    "video1": "Video 1",
+    "video2": "Video 2",
+    "video3": "Video 3",
+    "video4": "Video 4",
+    "video5": "Video 5",
+    "video6": "Video 6",
+    "video7": "Video 7",
+    "fm": "Radio",
+}
+
+SUPPORT_ONKYO = (
     SUPPORT_VOLUME_SET
+    | SUPPORT_VOLUME_STEP
     | SUPPORT_VOLUME_MUTE
     | SUPPORT_TURN_ON
     | SUPPORT_TURN_OFF
     | SUPPORT_SELECT_SOURCE
+    | SUPPORT_PLAY
+    | SUPPORT_PLAY_MEDIA
+)
+
+SUPPORT_ONKYO_WO_VOLUME = (
+    SUPPORT_VOLUME_SET
+    | SUPPORT_VOLUME_STEP
+    | SUPPORT_VOLUME_MUTE
+    | SUPPORT_TURN_ON
+    | SUPPORT_TURN_OFF
+    | SUPPORT_SELECT_SOURCE
+    | SUPPORT_PLAY
+    | SUPPORT_PLAY_MEDIA
 )
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
@@ -43,10 +84,18 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Required(CONF_HOST): cv.string,
         vol.Optional(CONF_NAME): cv.string,
         vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+        vol.Optional(CONF_MAX_VOLUME, default=SUPPORTED_MAX_VOLUME): vol.All(
+            vol.Coerce(int), vol.Range(min=1, max=100)
+        ),
+        vol.Optional(
+            CONF_RECEIVER_MAX_VOLUME, default=DEFAULT_RECEIVER_MAX_VOLUME
+        ): vol.All(vol.Coerce(int), vol.Range(min=0, max=100)),
+        vol.Optional(CONF_SOURCES, default=DEFAULT_SOURCES): {cv.string: cv.string},
+        vol.Optional(CONF_ZONES, default=[]): vol.All(cv.ensure_list, [vol.In(ZONES)]),
     }
 )
 
-DEVICE = None
+ATTR_PRESET = "preset"
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
@@ -54,8 +103,11 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
     host = config.get(CONF_HOST)
     port = config.get(CONF_PORT)
-    name = config.get(CONF_NAME)
-    device = None
+    name = config.get(CONF_NAME) or "Onkyo Reciever"
+    max_volume = config.get(CONF_MAX_VOLUME)
+    receiver_max_volume = config.get(CONF_RECEIVER_MAX_VOLUME)
+    zones = config.get(CONF_ZONES)
+    sources = config.get(CONF_SOURCES)
 
     _LOGGER.info("Provisioning Anthem AVR device at %s:%d", host, port)
 
@@ -63,44 +115,101 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     def async_anthemav_update_callback(message):
         """Receive notification from transport that new data exists."""
         _LOGGER.debug("Received update callback from AVR: %s", message)
-        device.process_update(message)
-        hass.async_create_task(device.async_update_ha_state())
+        zone, _, _ = message
+        if zone in active_zones.keys():
+            active_zones[zone].process_update(message)
+            hass.async_create_task(active_zones[zone].async_update_ha_state())
 
     avr = await pyeiscp.Connection.create(
         host=host, port=port, update_callback=async_anthemav_update_callback
     )
 
-    device = AnthemAVR(avr, name)
+    active_zones = {}
 
-    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, device.avr.close)
-    async_add_entities([device])
+    for zone in ["main"] + zones:
+        active_zones[zone] = OnkyoAVR(
+            avr, name, sources, zone, max_volume, receiver_max_volume
+        )
+
+    for zone in active_zones.values():
+        zone.backfill_state()
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, zone.avr.close)
+
+    async_add_entities(active_zones.values())
 
 
-class AnthemAVR(MediaPlayerDevice):
+class OnkyoAVR(MediaPlayerDevice):
     """Entity reading values from Anthem AVR protocol."""
 
-    def __init__(self, avr, name):
+    def __init__(self, avr, name, sources, zone, max_volume, receiver_max_volume):
         """Initialize entity with transport."""
         super().__init__()
         self.avr = avr
         self._name = name
+        self._zone = zone
         self._volume = 0
-        self._powerstate = STATE_ON
+        self._supports_volume = False
         self._muted = False
+        self._max_volume = max_volume
+        self._receiver_max_volume = receiver_max_volume
+        self._powerstate = STATE_ON
         self._source = None
+        self._source_list = list(sources.values())
+        self._source_mapping = sources
+        self._reverse_mapping = {value: key for key, value in sources.items()}
+        self._attributes = {}
 
     def process_update(self, update):
-        zone, command, value = update
-        _LOGGER.info("recieved update about %s" % command)
-        if command in ["volume", "master-volume"]:
-            self._volume = value / 90
-        elif command == "power":
-            self._powerstate = value == "on"
+        """Stores relevant updates so they can be queried later."""
+        _, command, value = update
+        if command in ["system-power", "power"]:
+            if value == "on":
+                self._powerstate = STATE_ON
+            else:
+                self._powerstate = STATE_OFF
+        elif command in ["volume", "master-volume"]:
+            self._supports_volume = True
+            self._volume = value / self._receiver_max_volume * (self._max_volume / 100)
+        elif command == "audio-muting":
+            self._muted = bool(value == "on")
+        elif command == "input-selector":
+            # eiscp can return string or tuple. Make everything tuples.
+            if isinstance(value, str):
+                current_source_tuples = (command, (value,))
+            else:
+                current_source_tuples = (command, value)
+
+            for source in current_source_tuples[1]:
+                if source in self._source_mapping:
+                    self._source = self._source_mapping[source]
+                    break
+                self._source = "_".join(current_source_tuples[1])
+        elif command == "hdmi-output-selector":
+            self._attributes["video_out"] = ",".join(value)
+        elif command == "preset":
+            if self._source.lower() == "radio":
+                self._attributes[ATTR_PRESET] = value
+            elif ATTR_PRESET in self._attributes:
+                del self._attributes[ATTR_PRESET]
+
+    def backfill_state(self):
+        """Get the reciever to send all the info we care about.
+
+        Usually run only on connect, as we can otherwise rely on the
+        reciever to keep us informed of changes."""
+        self._query_avr("power")
+        self._query_avr("volume")
+        self._query_avr("audio-muting")
+        self._query_avr("input-selector")
+        self._query_avr("hdmi-output-selector")
+        self._query_avr("preset")
 
     @property
     def supported_features(self):
         """Flag media player features that are supported."""
-        return SUPPORT_ANTHEMAV
+        if self._supports_volume:
+            return SUPPORT_ONKYO
+        return SUPPORT_ONKYO_WO_VOLUME
 
     @property
     def should_poll(self):
@@ -135,10 +244,12 @@ class AnthemAVR(MediaPlayerDevice):
     @property
     def source_list(self):
         """Return all active, configured inputs."""
-        return ["unknown"]
+        return self._source_list
 
     async def async_select_source(self, source):
         """Change AVR to the designated source (by name)."""
+        if source in self._source_list:
+            source = self._reverse_mapping[source]
         self._update_avr("input_name", source)
 
     async def async_turn_off(self):
@@ -151,7 +262,8 @@ class AnthemAVR(MediaPlayerDevice):
 
     async def async_volume_up(self):
         """Increment volume by 1"""
-        self._update_avr("volume", "level-up")
+        if self._volume < self._max_volume:
+            self._update_avr("volume", "level-up")
 
     async def async_volume_down(self):
         """Decrement volume by 1"""
@@ -159,7 +271,9 @@ class AnthemAVR(MediaPlayerDevice):
 
     async def async_set_volume_level(self, volume):
         """Set AVR volume (0 to 1)."""
-        self._update_avr("volume", round(volume * 90))
+        self._update_avr(
+            "volume", int(volume * (self._max_volume / 100) * self._receiver_max_volume)
+        )
 
     async def async_mute_volume(self, mute):
         """Engage AVR mute."""
@@ -167,5 +281,8 @@ class AnthemAVR(MediaPlayerDevice):
 
     def _update_avr(self, propname, value):
         """Update a property in the AVR."""
-        _LOGGER.info("Sending command to AVR: set %s to %s", propname, str(value))
-        self.avr.send(f"{propname}={value}")
+        self.avr.send(f"{self._zone}.{propname}={value}")
+
+    def _query_avr(self, propname):
+        """Cause the AVR to send an update about _propname_"""
+        self.avr.send(f"{self._zone}.{propname}=query")
