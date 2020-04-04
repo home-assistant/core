c0e1bae718dafda7d03744c87cb7b4926394f0fb
cyr-ius <cyr-ius@ipocus.net>
2020-04-04 09:18:37 +0200

    Add options reload

--- homeassistant/components/roomba/__init__.py
+++ homeassistant/components/roomba/__init__.py
@@ -15,6 +15,8 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
     """Set up the roomba environment."""
+    hass.data.setdefault(DOMAIN, {})
+
     if DOMAIN not in config:
         return True
 
@@ -29,6 +31,7 @@ async def async_setup(hass, config):
 async def async_setup_entry(hass, config_entry):
     """Set the config entry up."""
     # Set up roomba platforms with config entry
+    _LOGGER.debug(config_entry.entry_id)
     if config_entry.data is None:
         return False
 
@@ -41,22 +44,21 @@ async def async_setup_entry(hass, config_entry):
             },
         )
 
-    if DOMAIN not in hass.data:
-        hass.data[DOMAIN] = {}
-
     if "roomba" not in hass.data[DOMAIN]:
-
         roomba = Roomba(
             address=config_entry.data[CONF_HOST],
             blid=config_entry.data[CONF_USERNAME],
             password=config_entry.data[CONF_PASSWORD],
-            cert_name=config_entry.options[CONF_CERT],
+            cert_name=config_entry.data[CONF_CERT],
             continuous=config_entry.options[CONF_CONTINUOUS],
             delay=config_entry.options[CONF_DELAY],
         )
 
-        if not await async_connect_or_timeout(hass, roomba):
-            return False
+        try:
+            if not await async_connect_or_timeout(hass, roomba):
+                return False
+        except CannotConnect:
+            raise exceptions.ConfigEntryNotReady
 
     for component in COMPONENTS:
         hass.async_create_task(
@@ -74,23 +76,20 @@ async def async_connect_or_timeout(hass, roomba):
         hass.data[DOMAIN]["name"] = name = None
         with async_timeout.timeout(10):
             await hass.async_add_job(roomba.connect)
+            _LOGGER.debug("Initialize to connect to vacuum")
             while not roomba.roomba_connected or name is None:
                 # Waiting for connection and check datas ready
-                name = (
-                    roomba.master_state.get("state", {})
-                    .get("reported", {})
-                    .get("name", None)
-                )
-                await asyncio.sleep(0.5)
-            hass.data[DOMAIN]["roomba"] = roomba
-            hass.data[DOMAIN]["name"] = name
+                name = roomba_reported_state(roomba).get("name", None)
+                hass.data[DOMAIN]["roomba"] = roomba
+                hass.data[DOMAIN]["name"] = name
+                await asyncio.sleep(1)
     except RoombaConnectionError:
         _LOGGER.error("Error to connect to vacuum")
         raise CannotConnect
     except asyncio.TimeoutError:
         # api looping if user or password incorrect and roomba exist
         await async_disconnect_or_timeout(hass, roomba)
-        _LOGGER.exception("Timeout expired, user or password incorrect")
+        _LOGGER.exception("Timeout expired")
         raise CannotConnect
 
     return True
@@ -98,7 +97,9 @@ async def async_connect_or_timeout(hass, roomba):
 
 async def async_disconnect_or_timeout(hass, roomba):
     """Disconnect to vacuum."""
-    await hass.async_add_job(roomba.disconnect)
+    with async_timeout.timeout(3):
+        await hass.async_add_job(roomba.disconnect)
+        hass.data[DOMAIN].pop("roomba", None)
     await asyncio.sleep(1)
     return True
 
@@ -106,23 +107,24 @@ async def async_disconnect_or_timeout(hass, roomba):
 async def async_update_options(hass, config_entry):
     """Update options."""
     await hass.config_entries.async_reload(config_entry.entry_id)
-    await async_disconnect_or_timeout(hass, roomba)
-    await async_connect_or_timeout(hass, roomba)
 
 
 async def async_unload_entry(hass, config_entry):
     """Unload a config entry."""
-    for component in COMPONENTS:
-        hass.async_create_task(
+    _LOGGER.debug("Unload Entries")
+    await asyncio.gather(
+        *[
             hass.config_entries.async_forward_entry_unload(config_entry, component)
-        )
-    roomba = hass.data[DOMAIN]["roomba"]
-    return await async_disconnect_or_timeout(hass, roomba)
+            for component in COMPONENTS
+        ]
+    )
+    return await async_disconnect_or_timeout(hass, roomba=hass.data[DOMAIN]["roomba"])
+
+
+def roomba_reported_state(roomba):
+    """Roomba report."""
+    return roomba.master_state.get("state", {}).get("reported", {})
 
 
 class CannotConnect(exceptions.HomeAssistantError):
     """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(exceptions.HomeAssistantError):
-    """Error to indicate there is invalid auth."""
