6f7c9191c545d32ad0c1ab1e1b75691e39e98a6b
Alan Murray <github@atmurray.net>
2020-04-05 17:28:23 +1000

    Remove unused constants.

--- homeassistant/components/acmeda/const.py
+++ homeassistant/components/acmeda/const.py
@@ -1,10 +1,6 @@
 """Constants for the Rollease Acmeda Automate integration."""
-from datetime import timedelta
 import logging
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__package__)
 DOMAIN = "acmeda"
-
-DEFAULT_PORT = 12416
-DEFAULT_SCAN_INTERVAL = timedelta(seconds=60)
