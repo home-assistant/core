d8dfb79bc7ee2c8e8a4af11d432a2c2c3cc8a1e4
springstan <floorb@gmx.de>
2020-04-05 12:56:56 +0200

    Revert changes in gen_requirements_all.py

--- script/gen_requirements_all.py
+++ script/gen_requirements_all.py
@@ -127,7 +127,7 @@ def gather_recursive_requirements(domain, seen=None):
     seen.add(domain)
     integration = Integration(Path(f"homeassistant/components/{domain}"))
     integration.load_manifest()
-    reqs = {integration.requirements}
+    reqs = set(integration.requirements)
     for dep_domain in integration.dependencies:
         reqs.update(gather_recursive_requirements(dep_domain, seen))
     return reqs
