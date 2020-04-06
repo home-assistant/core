b3f23128a4547eb1b36cd5dfef0ab06404956174
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-04-06 10:46:06 +0000

    fixed missing mock values that were causing exceptions

--- tests/ignore_uncaught_exceptions.py
+++ tests/ignore_uncaught_exceptions.py
@@ -1,9 +1,5 @@
 """List of modules that have uncaught exceptions today. Will be shrunk over time."""
 IGNORE_UNCAUGHT_EXCEPTIONS = [
-    ("tests.components.cast.test_media_player", "test_start_discovery_called_once"),
-    ("tests.components.cast.test_media_player", "test_entry_setup_single_config"),
-    ("tests.components.cast.test_media_player", "test_entry_setup_list_config"),
-    ("tests.components.cast.test_media_player", "test_entry_setup_platform_not_ready"),
     ("tests.components.demo.test_init", "test_setting_up_demo"),
     ("tests.components.dyson.test_air_quality", "test_purecool_aiq_attributes"),
     ("tests.components.dyson.test_air_quality", "test_purecool_aiq_update_state"),
