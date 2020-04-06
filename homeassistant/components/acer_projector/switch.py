cd408c813e14b81a313bcb3804b6b8e76596673f
springstan <floorb@gmx.de>
2020-04-06 23:04:58 +0200

    Clean up access to config in various integrations

--- homeassistant/components/acer_projector/switch.py
+++ homeassistant/components/acer_projector/switch.py
@@ -61,10 +61,10 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Connect with serial port and return Acer Projector."""
-    serial_port = config.get(CONF_FILENAME)
-    name = config.get(CONF_NAME)
-    timeout = config.get(CONF_TIMEOUT)
-    write_timeout = config.get(CONF_WRITE_TIMEOUT)
+    serial_port = config[CONF_FILENAME]
+    name = config[CONF_NAME]
+    timeout = config[CONF_TIMEOUT]
+    write_timeout = config[CONF_WRITE_TIMEOUT]
 
     add_entities([AcerSwitch(serial_port, name, timeout, write_timeout)], True)
 
