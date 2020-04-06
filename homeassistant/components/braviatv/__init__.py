777ac04ca320d1ec4430c193ad597ed69a5e6ffa
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-04-06 16:36:59 +0200

    Fix FlowOptions update listener

--- homeassistant/components/braviatv/__init__.py
+++ homeassistant/components/braviatv/__init__.py
@@ -1,6 +1,5 @@
 """The Bravia TV component."""
 import asyncio
-import logging
 
 from bravia_tv import BraviaRC
 
@@ -11,18 +10,16 @@ from .const import CLIENTID_PREFIX, DOMAIN, NICKNAME
 
 PLATFORMS = ["media_player"]
 
-_LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup(hass, config):
     """Set up the Bravia TV component."""
     return True
 
 
-async def async_setup_entry(hass, entry):
+async def async_setup_entry(hass, config_entry):
     """Set up a config entry."""
-    host = entry.data[CONF_HOST]
-    pin = entry.data[CONF_PIN]
+    host = config_entry.data[CONF_HOST]
+    pin = config_entry.data[CONF_PIN]
 
     braviarc = BraviaRC(host)
 
@@ -32,34 +29,36 @@ async def async_setup_entry(hass, entry):
         raise ConfigEntryNotReady
 
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = braviarc
+    hass.data[DOMAIN][config_entry.entry_id] = braviarc
+
+    config_entry.add_update_listener(update_listener)
 
     for component in PLATFORMS:
         hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
+            hass.config_entries.async_forward_entry_setup(config_entry, component)
         )
 
     return True
 
 
-async def async_unload_entry(hass, entry):
+async def async_unload_entry(hass, config_entry):
     """Unload a config entry."""
     unload_ok = all(
         await asyncio.gather(
             *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
+                hass.config_entries.async_forward_entry_unload(config_entry, component)
                 for component in PLATFORMS
             ]
         )
     )
     if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+        hass.data[DOMAIN].pop(config_entry.entry_id)
 
     return unload_ok
 
 
 async def update_listener(hass, config_entry):
-    """Update listener."""
+    """Handle an options update."""
     for component in PLATFORMS:
         await hass.config_entries.async_forward_entry_unload(config_entry, component)
     hass.async_add_job(async_setup_entry(hass, config_entry))
