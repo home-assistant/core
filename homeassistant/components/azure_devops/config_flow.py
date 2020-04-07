344a894004f6e0651e7d77aef36085fcde649739
Timmo <contact@timmo.xyz>
2020-04-07 17:14:27 +0100

    Error to user

--- homeassistant/components/azure_devops/config_flow.py
+++ homeassistant/components/azure_devops/config_flow.py
@@ -55,17 +55,13 @@ class AzureDevOpsFlowHandler(ConfigFlow):
         )
         try:
             core_client = connection.clients.get_core_client()
-
             project = core_client.get_project(user_input.get(CONF_PROJECT))
-
             if not project:
-                errors[
-                    "base"
-                ] = f"Could not get project '{user_input.get(CONF_PROJECT)}' from Azure DevOps."
+                errors["base"] = "project_error"
                 return await self._show_setup_form(errors)
         except ClientRequestError as exception:
-            _LOGGER.warning(exception)
-            errors["base"] = f"Could not connect to Azure DevOps."
+            _LOGGER.warning("Could not connect to Azure DevOps: %s", exception)
+            errors["base"] = "connection_error"
             return await self._show_setup_form(errors)
 
         return self.async_create_entry(
