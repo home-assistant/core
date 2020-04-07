bee742994eafb560eedfaf43aa0e98a02b33e90a
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-04-07 19:34:13 +0300

    Fix uncaught exceptions for discovery, unify_direct, spotify,… (#33735)
    * used coroutinemock to avoid exception
    
    * added spec to mock to remove exception
    
    * added the current_user return value so it doesnt throw exception
    
    * fix the mocks so properties do not need .return_value
    
    * fixed missing mock values that were causing exceptions
    
    * moved patch to asynctest so no need to define m_init
    
    * fixed black error

--- tests/ignore_uncaught_exceptions.py
+++ tests/ignore_uncaught_exceptions.py
@@ -1,11 +1,6 @@
 """List of modules that have uncaught exceptions today. Will be shrunk over time."""
 IGNORE_UNCAUGHT_EXCEPTIONS = [
-    ("tests.components.cast.test_media_player", "test_start_discovery_called_once"),
-    ("tests.components.cast.test_media_player", "test_entry_setup_single_config"),
-    ("tests.components.cast.test_media_player", "test_entry_setup_list_config"),
-    ("tests.components.cast.test_media_player", "test_entry_setup_platform_not_ready"),
     ("tests.components.demo.test_init", "test_setting_up_demo"),
-    ("tests.components.discovery.test_init", "test_discover_config_flow"),
     ("tests.components.dyson.test_air_quality", "test_purecool_aiq_attributes"),
     ("tests.components.dyson.test_air_quality", "test_purecool_aiq_update_state"),
     (
@@ -42,18 +37,6 @@ IGNORE_UNCAUGHT_EXCEPTIONS = [
     ("tests.components.qwikswitch.test_init", "test_binary_sensor_device"),
     ("tests.components.qwikswitch.test_init", "test_sensor_device"),
     ("tests.components.rflink.test_init", "test_send_command_invalid_arguments"),
-    ("tests.components.unifi_direct.test_device_tracker", "test_get_scanner"),
 ]
 
-IGNORE_UNCAUGHT_JSON_EXCEPTIONS = [
-    ("tests.components.spotify.test_config_flow", "test_full_flow"),
-    ("tests.components.smartthings.test_init", "test_config_entry_loads_platforms"),
-    (
-        "tests.components.smartthings.test_init",
-        "test_scenes_unauthorized_loads_platforms",
-    ),
-    (
-        "tests.components.smartthings.test_init",
-        "test_config_entry_loads_unconnected_cloud",
-    ),
-]
+IGNORE_UNCAUGHT_JSON_EXCEPTIONS = []
