26db2db25389bd539a7a3906196a74945a326375
J. Nick Koston <nick@koston.org>
2020-04-08 11:45:45 -0500

    Convert nut to a multi step config flow (#33803)
    * Convert nut to a multi step config flow
    
    * Users can now choose the ups they want in
    step 2 (or skipped if only one)
    
    * Users can now select the resources they want
    to monitor based on what is actually available
    on the device
    
    * CONF_NAME has been removed as we now get the
    name from NUT
    
    * Device classes have been added which allows
    the battery charge state to be seen in the
    devices UI
    
    * Remove update_interval as its for a followup PR
    
    * explict
    
    * reduce
    
    * fix bug, add tests for options flow
    
    * Test for dupe import
    
    * Test for dupe import
    
    * Call step directly
    
    * Update homeassistant/components/nut/config_flow.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/nut/config_flow.py
+++ homeassistant/components/nut/config_flow.py
@@ -7,7 +7,6 @@ from homeassistant import config_entries, core, exceptions
 from homeassistant.const import (
     CONF_ALIAS,
     CONF_HOST,
-    CONF_NAME,
     CONF_PASSWORD,
     CONF_PORT,
     CONF_RESOURCES,
@@ -17,7 +16,7 @@ from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 
 from . import PyNUTData, find_resources_in_config_entry, pynutdata_status
-from .const import DEFAULT_HOST, DEFAULT_NAME, DEFAULT_PORT, SENSOR_TYPES
+from .const import DEFAULT_HOST, DEFAULT_PORT, SENSOR_TYPES
 from .const import DOMAIN  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
@@ -27,15 +26,37 @@ SENSOR_DICT = {sensor_id: SENSOR_TYPES[sensor_id][0] for sensor_id in SENSOR_TYP
 
 DATA_SCHEMA = vol.Schema(
     {
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
-        vol.Required(CONF_RESOURCES): cv.multi_select(SENSOR_DICT),
         vol.Optional(CONF_HOST, default=DEFAULT_HOST): str,
         vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
-        vol.Optional(CONF_ALIAS): str,
         vol.Optional(CONF_USERNAME): str,
         vol.Optional(CONF_PASSWORD): str,
     }
 )
+
+
+def _resource_schema(available_resources, selected_resources):
+    """Resource selection schema."""
+
+    known_available_resources = {
+        sensor_id: sensor[0]
+        for sensor_id, sensor in SENSOR_TYPES.items()
+        if sensor_id in available_resources
+    }
+
+    return vol.Schema(
+        {
+            vol.Required(CONF_RESOURCES, default=selected_resources): cv.multi_select(
+                known_available_resources
+            )
+        }
+    )
+
+
+def _ups_schema(ups_list):
+    """UPS selection schema."""
+    ups_map = {ups: ups for ups in ups_list}
+
+    return vol.Schema({vol.Required(CONF_ALIAS): vol.In(ups_map)})
 
 
 async def validate_input(hass: core.HomeAssistant, data):
@@ -52,16 +73,22 @@ async def validate_input(hass: core.HomeAssistant, data):
 
     data = PyNUTData(host, port, alias, username, password)
 
-    status = await hass.async_add_executor_job(pynutdata_status, data)
+    ups_list = await hass.async_add_executor_job(data.list_ups)
+    if not ups_list:
+        raise CannotConnect
 
+    status = await hass.async_add_executor_job(pynutdata_status, data)
     if not status:
         raise CannotConnect
 
-    return {"title": _format_host_port_alias(host, port, alias)}
+    return {"ups_list": ups_list, "available_resources": status}
 
 
-def _format_host_port_alias(host, port, alias):
+def _format_host_port_alias(user_input):
     """Format a host, port, and alias so it can be used for comparison or display."""
+    host = user_input[CONF_HOST]
+    port = user_input[CONF_PORT]
+    alias = user_input.get(CONF_ALIAS)
     if alias:
         return f"{alias}@{host}:{port}"
     return f"{host}:{port}"
@@ -73,40 +100,96 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
+    def __init__(self):
+        """Initialize the nut config flow."""
+        self.nut_config = {}
+        self.available_resources = {}
+        self.ups_list = None
+        self.title = None
+
+    async def async_step_import(self, user_input=None):
+        """Handle the import."""
         errors = {}
         if user_input is not None:
-            if self._host_port_alias_already_configured(
-                user_input[CONF_HOST], user_input[CONF_PORT], user_input.get(CONF_ALIAS)
-            ):
+            if self._host_port_alias_already_configured(user_input):
                 return self.async_abort(reason="already_configured")
-            try:
-                info = await validate_input(self.hass, user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+            _, errors = await self._async_validate_or_error(user_input)
 
-            if "base" not in errors:
-                return self.async_create_entry(title=info["title"], data=user_input)
+            if not errors:
+                title = _format_host_port_alias(user_input)
+                return self.async_create_entry(title=title, data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
 
-    def _host_port_alias_already_configured(self, host, port, alias):
+    async def async_step_user(self, user_input=None):
+        """Handle the user input."""
+        errors = {}
+        if user_input is not None:
+            info, errors = await self._async_validate_or_error(user_input)
+
+            if not errors:
+                self.nut_config.update(user_input)
+                if len(info["ups_list"]) > 1:
+                    self.ups_list = info["ups_list"]
+                    return await self.async_step_ups()
+
+                self.available_resources.update(info["available_resources"])
+                return await self.async_step_resources()
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_ups(self, user_input=None):
+        """Handle the picking the ups."""
+        errors = {}
+
+        if user_input is not None:
+            self.nut_config.update(user_input)
+            if self._host_port_alias_already_configured(self.nut_config):
+                return self.async_abort(reason="already_configured")
+            info, errors = await self._async_validate_or_error(self.nut_config)
+            if not errors:
+                self.available_resources.update(info["available_resources"])
+                return await self.async_step_resources()
+
+        return self.async_show_form(
+            step_id="ups", data_schema=_ups_schema(self.ups_list), errors=errors,
+        )
+
+    async def async_step_resources(self, user_input=None):
+        """Handle the picking the resources."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="resources",
+                data_schema=_resource_schema(self.available_resources, []),
+            )
+
+        self.nut_config.update(user_input)
+        title = _format_host_port_alias(self.nut_config)
+        return self.async_create_entry(title=title, data=self.nut_config)
+
+    def _host_port_alias_already_configured(self, user_input):
         """See if we already have a nut entry matching user input configured."""
         existing_host_port_aliases = {
-            _format_host_port_alias(host, port, alias)
+            _format_host_port_alias(entry.data)
             for entry in self._async_current_entries()
         }
-        return _format_host_port_alias(host, port, alias) in existing_host_port_aliases
+        return _format_host_port_alias(user_input) in existing_host_port_aliases
 
-    async def async_step_import(self, user_input):
-        """Handle import."""
-        return await self.async_step_user(user_input)
+    async def _async_validate_or_error(self, config):
+        errors = {}
+        info = {}
+        try:
+            info = await validate_input(self.hass, config)
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        return info, errors
 
     @staticmethod
     @callback
@@ -129,14 +212,12 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
 
         resources = find_resources_in_config_entry(self.config_entry)
 
-        data_schema = vol.Schema(
-            {
-                vol.Required(CONF_RESOURCES, default=resources): cv.multi_select(
-                    SENSOR_DICT
-                ),
-            }
+        info = await validate_input(self.hass, self.config_entry.data)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=_resource_schema(info["available_resources"], resources),
         )
-        return self.async_show_form(step_id="init", data_schema=data_schema)
 
 
 class CannotConnect(exceptions.HomeAssistantError):
--- tests/components/nut/test_config_flow.py
+++ tests/components/nut/test_config_flow.py
@@ -1,18 +1,28 @@
 """Test the Network UPS Tools (NUT) config flow."""
 from asynctest import MagicMock, patch
 
-from homeassistant import config_entries, setup
+from homeassistant import config_entries, data_entry_flow, setup
 from homeassistant.components.nut.const import DOMAIN
+from homeassistant.const import CONF_RESOURCES
+
+from tests.common import MockConfigEntry
+
+VALID_CONFIG = {
+    "host": "localhost",
+    "port": 123,
+    "name": "name",
+    "resources": ["battery.charge"],
+}
 
 
-def _get_mock_pynutclient(list_vars=None):
+def _get_mock_pynutclient(list_vars=None, list_ups=None):
     pynutclient = MagicMock()
-    type(pynutclient).list_ups = MagicMock(return_value=["ups1"])
+    type(pynutclient).list_ups = MagicMock(return_value=list_ups)
     type(pynutclient).list_vars = MagicMock(return_value=list_vars)
     return pynutclient
 
 
-async def test_form(hass):
+async def test_form_user_one_ups(hass):
     """Test we get the form."""
     await setup.async_setup_component(hass, "persistent_notification", {})
     result = await hass.config_entries.flow.async_init(
@@ -21,7 +31,25 @@ async def test_form(hass):
     assert result["type"] == "form"
     assert result["errors"] == {}
 
-    mock_pynut = _get_mock_pynutclient(list_vars={"battery.voltage": "voltage"})
+    mock_pynut = _get_mock_pynutclient(
+        list_vars={"battery.voltage": "voltage"}, list_ups=["ups1"]
+    )
+
+    with patch(
+        "homeassistant.components.nut.PyNUTClient", return_value=mock_pynut,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "host": "1.1.1.1",
+                "username": "test-username",
+                "password": "test-password",
+                "port": 2222,
+            },
+        )
+
+    assert result2["step_id"] == "resources"
+    assert result2["type"] == "form"
 
     with patch(
         "homeassistant.components.nut.PyNUTClient", return_value=mock_pynut,
@@ -30,6 +58,40 @@ async def test_form(hass):
     ) as mock_setup, patch(
         "homeassistant.components.nut.async_setup_entry", return_value=True,
     ) as mock_setup_entry:
+        result3 = await hass.config_entries.flow.async_configure(
+            result2["flow_id"], {"resources": ["battery.voltage"]},
+        )
+
+    assert result3["type"] == "create_entry"
+    assert result3["title"] == "1.1.1.1:2222"
+    assert result3["data"] == {
+        "host": "1.1.1.1",
+        "password": "test-password",
+        "port": 2222,
+        "resources": ["battery.voltage"],
+        "username": "test-username",
+    }
+    await hass.async_block_till_done()
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_form_user_multiple_ups(hass):
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == "form"
+    assert result["errors"] == {}
+
+    mock_pynut = _get_mock_pynutclient(
+        list_vars={"battery.voltage": "voltage"}, list_ups=["ups1", "ups2"]
+    )
+
+    with patch(
+        "homeassistant.components.nut.PyNUTClient", return_value=mock_pynut,
+    ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
@@ -37,20 +99,41 @@ async def test_form(hass):
                 "username": "test-username",
                 "password": "test-password",
                 "port": 2222,
-                "alias": "ups1",
-                "resources": ["battery.charge"],
             },
         )
 
-    assert result2["type"] == "create_entry"
-    assert result2["title"] == "ups1@1.1.1.1:2222"
-    assert result2["data"] == {
-        "alias": "ups1",
+    assert result2["step_id"] == "ups"
+    assert result2["type"] == "form"
+
+    with patch(
+        "homeassistant.components.nut.PyNUTClient", return_value=mock_pynut,
+    ):
+        result3 = await hass.config_entries.flow.async_configure(
+            result2["flow_id"], {"alias": "ups2"},
+        )
+
+    assert result3["step_id"] == "resources"
+    assert result3["type"] == "form"
+
+    with patch(
+        "homeassistant.components.nut.PyNUTClient", return_value=mock_pynut,
+    ), patch(
+        "homeassistant.components.nut.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.nut.async_setup_entry", return_value=True,
+    ) as mock_setup_entry:
+        result4 = await hass.config_entries.flow.async_configure(
+            result3["flow_id"], {"resources": ["battery.voltage"]},
+        )
+
+    assert result4["type"] == "create_entry"
+    assert result4["title"] == "ups2@1.1.1.1:2222"
+    assert result4["data"] == {
         "host": "1.1.1.1",
-        "name": "NUT UPS",
         "password": "test-password",
+        "alias": "ups2",
         "port": 2222,
-        "resources": ["battery.charge"],
+        "resources": ["battery.voltage"],
         "username": "test-username",
     }
     await hass.async_block_till_done()
@@ -62,7 +145,9 @@ async def test_form_import(hass):
     """Test we get the form with import source."""
     await setup.async_setup_component(hass, "persistent_notification", {})
 
-    mock_pynut = _get_mock_pynutclient(list_vars={"battery.voltage": "serial"})
+    mock_pynut = _get_mock_pynutclient(
+        list_vars={"battery.voltage": "serial"}, list_ups=["ups1"]
+    )
 
     with patch(
         "homeassistant.components.nut.PyNUTClient", return_value=mock_pynut,
@@ -95,6 +180,20 @@ async def test_form_import(hass):
     assert len(mock_setup_entry.mock_calls) == 1
 
 
+async def test_form_import_dupe(hass):
+    """Test we get abort on duplicate import."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+
+    entry = MockConfigEntry(domain=DOMAIN, data=VALID_CONFIG)
+    entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": "import"}, data=VALID_CONFIG
+    )
+    assert result["type"] == "abort"
+    assert result["reason"] == "already_configured"
+
+
 async def test_form_cannot_connect(hass):
     """Test we handle cannot connect error."""
     result = await hass.config_entries.flow.async_init(
@@ -113,10 +212,39 @@ async def test_form_cannot_connect(hass):
                 "username": "test-username",
                 "password": "test-password",
                 "port": 2222,
-                "alias": "ups1",
-                "resources": ["battery.charge"],
             },
         )
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_options_flow(hass):
+    """Test config flow options."""
+
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="abcde12345",
+        data=VALID_CONFIG,
+        options={CONF_RESOURCES: ["battery.charge"]},
+    )
+    config_entry.add_to_hass(hass)
+
+    mock_pynut = _get_mock_pynutclient(
+        list_vars={"battery.voltage": "voltage"}, list_ups=["ups1"]
+    )
+
+    with patch(
+        "homeassistant.components.nut.PyNUTClient", return_value=mock_pynut,
+    ), patch("homeassistant.components.nut.async_setup_entry", return_value=True):
+        result = await hass.config_entries.options.async_init(config_entry.entry_id)
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["step_id"] == "init"
+
+        result = await hass.config_entries.options.async_configure(
+            result["flow_id"], user_input={CONF_RESOURCES: ["battery.voltage"]}
+        )
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert config_entry.options == {CONF_RESOURCES: ["battery.voltage"]}
