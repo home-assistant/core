425c97626a4e92c20ac47b1a5792e70fb441b1b4
Paulus Schoutsen <balloob@gmail.com>
2020-04-09 14:13:20 -0700

    Consolidate translation script (#33911)
    * Consolidate translation script
    
    * Remove commented code

---
+++ script/translations/__main__.py
@@ -0,0 +1,40 @@
+"""Validate manifests."""
+import argparse
+from pathlib import Path
+import sys
+
+from . import download, error
+
+
+def get_arguments() -> argparse.Namespace:
+    """Get parsed passed in arguments."""
+    parser = argparse.ArgumentParser(description="Home Assistant Scaffolder")
+    parser.add_argument("action", type=str, choices=["download"])
+    parser.add_argument("--debug", action="store_true", help="Enable log output")
+
+    arguments = parser.parse_args()
+
+    return arguments
+
+
+def main():
+    """Run a translation script."""
+    if not Path("requirements_all.txt").is_file():
+        print("Run from project root")
+        return 1
+
+    args = get_arguments()
+
+    if args.action == "download":
+        download.run(args)
+
+    return 0
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except error.ExitApp as err:
+        print()
+        print(f"Fatal Error: {err.reason}")
+        sys.exit(err.exit_code)
