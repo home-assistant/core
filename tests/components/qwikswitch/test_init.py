2d354b55bde88db8b83d5bdeb5c99a635c5aa87a
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-04-09 13:40:39 +0000

    passing json instead of the binary string

--- tests/components/qwikswitch/test_init.py
+++ tests/components/qwikswitch/test_init.py
@@ -2,30 +2,49 @@
 import asyncio
 import logging
 
-from homeassistant.bootstrap import async_setup_component
 from homeassistant.components.qwikswitch import DOMAIN as QWIKSWITCH
 from homeassistant.const import EVENT_HOMEASSISTANT_START
+from homeassistant.setup import async_setup_component
 
 _LOGGER = logging.getLogger(__name__)
 
 
-DEVICES = """[
-        {"id":"@000001","name":"Switch 1","type":"rel","val":"OFF",
-        "time":"1522777506","rssi":"51%"},
-        {"id":"@000002","name":"Light 2","type":"rel","val":"ON",
-        "time":"1522777507","rssi":"45%"},
-        {"id":"@000003","name":"Dim 3","type":"dim","val":"280c00",
-        "time":"1522777544","rssi":"62%"}]"""
+DEVICES = [
+    {
+        "id": "@000001",
+        "name": "Switch 1",
+        "type": "rel",
+        "val": "OFF",
+        "time": "1522777506",
+        "rssi": "51%",
+    },
+    {
+        "id": "@000002",
+        "name": "Light 2",
+        "type": "rel",
+        "val": "ON",
+        "time": "1522777507",
+        "rssi": "45%",
+    },
+    {
+        "id": "@000003",
+        "name": "Dim 3",
+        "type": "dim",
+        "val": "280c00",
+        "time": "1522777544",
+        "rssi": "62%",
+    },
+]
 
 
-async def test_binary_sensor_device(hass, aioclient_mock):  # noqa: F811
+async def test_binary_sensor_device(hass, aioclient_mock):
     """Test a binary sensor device."""
     config = {
         "qwikswitch": {
             "sensors": {"name": "s1", "id": "@a00001", "channel": 1, "type": "imod"}
         }
     }
-    aioclient_mock.get("http://127.0.0.1:2020/&device", text=DEVICES)
+    aioclient_mock.get("http://127.0.0.1:2020/&device", json=DEVICES)
     aioclient_mock.get("http://127.0.0.1:2020/&listen", long_poll=True)
     await async_setup_component(hass, QWIKSWITCH, config)
     hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
@@ -55,7 +74,7 @@ async def test_binary_sensor_device(hass, aioclient_mock):  # noqa: F811
     assert state_obj.state == "off"
 
 
-async def test_sensor_device(hass, aioclient_mock):  # noqa: F811
+async def test_sensor_device(hass, aioclient_mock):
     """Test a sensor device."""
     config = {
         "qwikswitch": {
@@ -67,7 +86,7 @@ async def test_sensor_device(hass, aioclient_mock):  # noqa: F811
             }
         }
     }
-    aioclient_mock.get("http://127.0.0.1:2020/&device", text=DEVICES)
+    aioclient_mock.get("http://127.0.0.1:2020/&device", json=DEVICES)
     aioclient_mock.get("http://127.0.0.1:2020/&listen", long_poll=True)
     await async_setup_component(hass, QWIKSWITCH, config)
     hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
