a0dc7603fec1f1361511ad40b3337cad1d159ebe
jan iversen <jancasacondor@gmail.com>
2020-04-09 22:15:20 +0200

    Rename domain import in modbus (#33906)

--- tests/components/modbus/conftest.py
+++ tests/components/modbus/conftest.py
@@ -11,7 +11,7 @@ from homeassistant.components.modbus.const import (
     CONF_REGISTER_TYPE,
     CONF_REGISTERS,
     DEFAULT_HUB,
-    MODBUS_DOMAIN,
+    MODBUS_DOMAIN as DOMAIN,
 )
 from homeassistant.const import CONF_NAME, CONF_PLATFORM, CONF_SCAN_INTERVAL
 from homeassistant.setup import async_setup_component
@@ -25,10 +25,10 @@ _LOGGER = logging.getLogger(__name__)
 @pytest.fixture()
 def mock_hub(hass):
     """Mock hub."""
-    mock_integration(hass, MockModule(MODBUS_DOMAIN))
+    mock_integration(hass, MockModule(DOMAIN))
     hub = mock.MagicMock()
     hub.name = "hub"
-    hass.data[MODBUS_DOMAIN] = {DEFAULT_HUB: hub}
+    hass.data[DOMAIN] = {DEFAULT_HUB: hub}
     return hub
 
 
