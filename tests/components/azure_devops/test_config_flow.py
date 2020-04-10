29328908f8a9b1ffc6cadb90b789b50a676413d5
Timmo <contact@timmo.xyz>
2020-04-10 17:00:24 +0100

    Add real test

--- tests/components/azure_devops/test_config_flow.py
+++ tests/components/azure_devops/test_config_flow.py
@@ -3,11 +3,13 @@ from homeassistant import data_entry_flow
 from homeassistant.components.azure_devops import config_flow
 from homeassistant.components.azure_devops.const import CONF_ORG, CONF_PAT, CONF_PROJECT
 
-FIXTURE_USER_INPUT = {
+USER_INPUT_BAD = {
     CONF_ORG: "example",
-    CONF_PROJECT: "Something",
+    CONF_PROJECT: "something",
     CONF_PAT: "abcdef",
 }
+
+USER_INPUT = {CONF_ORG: "ms", CONF_PROJECT: "calculator"}
 
 
 async def test_show_form(hass):
@@ -24,8 +26,24 @@ async def test_authorization_error(hass):
     """Test we show user form on Azure DevOps connection error."""
     flow = config_flow.AzureDevOpsFlowHandler()
     flow.hass = hass
-    result = await flow.async_step_user(user_input=FIXTURE_USER_INPUT)
+    result = await flow.async_step_user(user_input=USER_INPUT_BAD)
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
     assert result["errors"] == {"base": "authorization_error"}
+
+
+async def test_full_flow_implementation(hass):
+    """Test registering an integration and finishing flow works."""
+    flow = config_flow.AzureDevOpsFlowHandler()
+    flow.hass = hass
+
+    result = await flow.async_step_user(user_input=None)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "user"
+
+    result = await flow.async_step_user(user_input=USER_INPUT)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == f"{USER_INPUT[CONF_ORG]}/{USER_INPUT[CONF_PROJECT]}"
+    assert result["data"][CONF_ORG] == USER_INPUT[CONF_ORG]
+    assert result["data"][CONF_PROJECT] == USER_INPUT[CONF_PROJECT]
