6d9844baaab2991427763e5343b6264f0620ef7e
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-04-11 19:18:26 +0000

    qwikswitch exceptions were already fixed

--- tests/ignore_uncaught_exceptions.py
+++ tests/ignore_uncaught_exceptions.py
@@ -34,8 +34,6 @@ IGNORE_UNCAUGHT_EXCEPTIONS = [
     ("tests.components.ios.test_init", "test_creating_entry_sets_up_sensor"),
     ("tests.components.ios.test_init", "test_not_configuring_ios_not_creates_entry"),
     ("tests.components.local_file.test_camera", "test_file_not_readable"),
-    ("tests.components.qwikswitch.test_init", "test_binary_sensor_device"),
-    ("tests.components.qwikswitch.test_init", "test_sensor_device"),
 ]
 
 IGNORE_UNCAUGHT_JSON_EXCEPTIONS = []
