a4dac75156dc2945978eb90813cb978206422477
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-04-11 07:43:16 +0000

    added test for switch

--- tests/components/qwikswitch/test_init.py
+++ tests/components/qwikswitch/test_init.py
@@ -2,18 +2,21 @@
 import asyncio
 import logging
 
+from asynctest import patch
+from yarl import URL
+
 from homeassistant.components.qwikswitch import DOMAIN as QWIKSWITCH
 from homeassistant.const import EVENT_HOMEASSISTANT_START
 from homeassistant.setup import async_setup_component
 
-from tests.test_util.aiohttp import MockLongPollSideEffect
+from tests.test_util.aiohttp import AiohttpClientMockResponse, MockLongPollSideEffect
 
 _LOGGER = logging.getLogger(__name__)
 
 
 DEVICES = [
     {
-        "id": "@000001",
+        "id": "@a00001",
         "name": "Switch 1",
         "type": "rel",
         "val": "OFF",
@@ -21,7 +24,7 @@ DEVICES = [
         "rssi": "51%",
     },
     {
-        "id": "@000002",
+        "id": "@a00002",
         "name": "Light 2",
         "type": "rel",
         "val": "ON",
@@ -29,7 +32,7 @@ DEVICES = [
         "rssi": "45%",
     },
     {
-        "id": "@000003",
+        "id": "@a00003",
         "name": "Dim 3",
         "type": "dim",
         "val": "280c00",
@@ -104,5 +107,58 @@ async def test_sensor_device(hass, aioclient_mock):
     await hass.async_block_till_done()
     state_obj = hass.states.get("sensor.ss1")
     assert state_obj.state == "416"
+
+    listen_mock.stop()
+
+
+async def test_switch_device(hass, aioclient_mock):
+    """Test a switch device."""
+
+    async def get_devices_json(method, url, data):
+        return AiohttpClientMockResponse(method=method, url=url, json=DEVICES)
+
+    config = {"qwikswitch": {"switches": ["@a00001"]}}
+    aioclient_mock.get("http://127.0.0.1:2020/&device", side_effect=get_devices_json)
+    listen_mock = MockLongPollSideEffect()
+    aioclient_mock.get("http://127.0.0.1:2020/&listen", side_effect=listen_mock)
+    await async_setup_component(hass, QWIKSWITCH, config)
+    hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
+    await hass.async_block_till_done()
+
+    state_obj = hass.states.get("switch.switch_1")
+    assert state_obj.state == "off"
+
+    aioclient_mock.mock_calls.clear()
+    aioclient_mock.get("http://127.0.0.1:2020/@a00001=100")
+    await hass.services.async_call(
+        "switch", "turn_on", {"entity_id": "switch.switch_1"}, blocking=True
+    )
+    await asyncio.sleep(0.01)
+    assert (
+        "GET",
+        URL("http://127.0.0.1:2020/@a00001=100"),
+        None,
+        None,
+    ) in aioclient_mock.mock_calls
+
+    with patch.dict(DEVICES[0], {"val": "ON"}):
+        state_obj = hass.states.get("switch.switch_1")
+        assert state_obj.state == "off"
+        listen_mock.queue_response(json={"id": "@a00001", "cmd": ""},)
+        await hass.async_block_till_done()
+        state_obj = hass.states.get("switch.switch_1")
+        assert state_obj.state == "on"
+
+        aioclient_mock.mock_calls.clear()
+        aioclient_mock.get("http://127.0.0.1:2020/@a00001=0")
+        await hass.services.async_call(
+            "switch", "turn_off", {"entity_id": "switch.switch_1"}, blocking=True
+        )
+        assert (
+            "GET",
+            URL("http://127.0.0.1:2020/@a00001=0"),
+            None,
+            None,
+        ) in aioclient_mock.mock_calls
 
     listen_mock.stop()
