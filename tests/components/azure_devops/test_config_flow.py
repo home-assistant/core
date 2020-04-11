6b42e07bf7f6d15b143d467c10c1be21f2ca7b8c
Timmo <contact@timmo.xyz>
2020-04-11 23:57:04 +0100

    Convert to async_init

--- tests/components/azure_devops/test_config_flow.py
+++ tests/components/azure_devops/test_config_flow.py
@@ -1,9 +1,13 @@
 """Test the Azure DevOps config flow."""
 from unittest.mock import patch
 
-from homeassistant import data_entry_flow
-from homeassistant.components.azure_devops import config_flow
-from homeassistant.components.azure_devops.const import CONF_ORG, CONF_PAT, CONF_PROJECT
+from homeassistant import config_entries, data_entry_flow
+from homeassistant.components.azure_devops.const import (
+    CONF_ORG,
+    CONF_PAT,
+    CONF_PROJECT,
+    DOMAIN,
+)
 from homeassistant.core import HomeAssistant
 
 from tests.common import mock_coro
@@ -19,9 +23,9 @@ FIXTURE_USER_INPUT = {CONF_ORG: "ms", CONF_PROJECT: "calculator"}
 
 async def test_show_form(hass: HomeAssistant) -> None:
     """Test that the setup form is served."""
-    flow = config_flow.AzureDevOpsFlowHandler()
-    flow.hass = hass
-    result = await flow.async_step_user(user_input=None)
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
@@ -29,45 +33,54 @@ async def test_show_form(hass: HomeAssistant) -> None:
 
 async def test_authorization_error(hass: HomeAssistant) -> None:
     """Test we show user form on Azure DevOps authorization error."""
-    flow = config_flow.AzureDevOpsFlowHandler()
-    flow.hass = hass
-    result = await flow.async_step_user(user_input=FIXTURE_USER_INPUT_BAD)
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
 
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "user"
-    assert result["errors"] == {"base": "authorization_error"}
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"], FIXTURE_USER_INPUT_BAD,
+    )
+
+    assert result2["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result2["step_id"] == "user"
+    assert result2["errors"] == {"base": "authorization_error"}
 
 
 async def test_connection_error(hass: HomeAssistant) -> None:
     """Test we show user form on Azure DevOps connection error."""
-    flow = config_flow.AzureDevOpsFlowHandler()
-    flow.hass = hass
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
 
     with patch(
         "homeassistant.components.azure_devops.config_flow.AzureDevOpsFlowHandler._test_connection",
         return_value=mock_coro(return_value="connection_error",),
     ):
-        result = await flow.async_step_user(user_input=FIXTURE_USER_INPUT)
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"], FIXTURE_USER_INPUT,
+        )
 
-        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-        assert result["step_id"] == "user"
-        assert result["errors"] == {"base": "connection_error"}
+        assert result2["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result2["step_id"] == "user"
+        assert result2["errors"] == {"base": "connection_error"}
 
 
 async def test_full_flow_implementation(hass: HomeAssistant) -> None:
     """Test registering an integration and finishing flow works."""
-    flow = config_flow.AzureDevOpsFlowHandler()
-    flow.hass = hass
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
 
-    result = await flow.async_step_user(user_input=None)
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
 
-    result = await flow.async_step_user(user_input=FIXTURE_USER_INPUT)
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"], FIXTURE_USER_INPUT,
+    )
+    assert result2["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert (
-        result["title"]
+        result2["title"]
         == f"{FIXTURE_USER_INPUT[CONF_ORG]}/{FIXTURE_USER_INPUT[CONF_PROJECT]}"
     )
-    assert result["data"][CONF_ORG] == FIXTURE_USER_INPUT[CONF_ORG]
-    assert result["data"][CONF_PROJECT] == FIXTURE_USER_INPUT[CONF_PROJECT]
+    assert result2["data"][CONF_ORG] == FIXTURE_USER_INPUT[CONF_ORG]
+    assert result2["data"][CONF_PROJECT] == FIXTURE_USER_INPUT[CONF_PROJECT]
