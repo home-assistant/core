d454c6a43d06e991f24194ecdb939c1cc46b68b9
springstan <46536646+springstan@users.noreply.github.com>
2020-04-12 19:45:50 +0200

    Use MASS_KILOGRAMS constant (#34052)

--- homeassistant/components/garmin_connect/const.py
+++ homeassistant/components/garmin_connect/const.py
@@ -1,5 +1,10 @@
 """Constants for the Garmin Connect integration."""
-from homeassistant.const import DEVICE_CLASS_TIMESTAMP, TIME_MINUTES, UNIT_PERCENTAGE
+from homeassistant.const import (
+    DEVICE_CLASS_TIMESTAMP,
+    MASS_KILOGRAMS,
+    TIME_MINUTES,
+    UNIT_PERCENTAGE,
+)
 
 DOMAIN = "garmin_connect"
 ATTRIBUTION = "Data provided by garmin.com"
@@ -309,12 +314,12 @@ GARMIN_ENTITY_LIST = {
         DEVICE_CLASS_TIMESTAMP,
         False,
     ],
-    "weight": ["Weight", "kg", "mdi:weight-kilogram", None, False],
+    "weight": ["Weight", MASS_KILOGRAMS, "mdi:weight-kilogram", None, False],
     "bmi": ["BMI", "", "mdi:food", None, False],
     "bodyFat": ["Body Fat", UNIT_PERCENTAGE, "mdi:food", None, False],
     "bodyWater": ["Body Water", UNIT_PERCENTAGE, "mdi:water-percent", None, False],
-    "bodyMass": ["Body Mass", "kg", "mdi:food", None, False],
-    "muscleMass": ["Muscle Mass", "kg", "mdi:dumbbell", None, False],
+    "bodyMass": ["Body Mass", MASS_KILOGRAMS, "mdi:food", None, False],
+    "muscleMass": ["Muscle Mass", MASS_KILOGRAMS, "mdi:dumbbell", None, False],
     "physiqueRating": ["Physique Rating", "", "mdi:numeric", None, False],
     "visceralFat": ["Visceral Fat", "", "mdi:food", None, False],
     "metabolicAge": ["Metabolic Age", "", "mdi:calendar-heart", None, False],
