824502b2602e1c8ba047f94009ee7ab11daba57c
Colin Robbins <ColinRobbins@users.noreply.github.com>
2020-04-12 17:01:28 +0100

    Update homeassistant/components/lightwave/sensor.py
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/lightwave/sensor.py
+++ homeassistant/components/lightwave/sensor.py
@@ -11,7 +11,7 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Find and return battery."""
-    if not discovery_info:
+    if discovery_info is None:
         return
 
     batteries = []
