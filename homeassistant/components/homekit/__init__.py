005f28a483308a04880ab7c8d8645230ccf479d5
J. Nick Koston <nick@koston.org>
2020-04-12 08:47:39 -0500

    Update homeassistant/components/homekit/__init__.py
    Co-Authored-By: springstan <46536646+springstan@users.noreply.github.com>

--- homeassistant/components/homekit/__init__.py
+++ homeassistant/components/homekit/__init__.py
@@ -127,9 +127,7 @@ async def async_setup(hass, config):
     safe_mode = conf[CONF_SAFE_MODE]
     entity_filter = conf[CONF_FILTER]
     entity_config = conf[CONF_ENTITY_CONFIG]
-    interface_choice = None
-    if config.get(CONF_ZEROCONF_DEFAULT_INTERFACE):
-        interface_choice = InterfaceChoice.Default
+    interface_choice = InterfaceChoice.Default if config.get(CONF_ZEROCONF_DEFAULT_INTERFACE) else None
 
     homekit = HomeKit(
         hass,
