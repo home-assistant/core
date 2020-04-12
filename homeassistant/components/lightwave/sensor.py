46f12df09e07b6e7283ec3a7ae0097e431e65b7f
Colin Robbins <ColinRobbins@users.noreply.github.com>
2020-04-12 17:03:02 +0100

    Update homeassistant/components/lightwave/sensor.py
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/lightwave/sensor.py
+++ homeassistant/components/lightwave/sensor.py
@@ -6,8 +6,6 @@ from homeassistant.helpers.entity import Entity
 
 from . import CONF_SERIAL, LIGHTWAVE_LINK
 
-_LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Find and return battery."""
