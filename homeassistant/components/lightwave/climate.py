e1f54bbd9fc0463d6b0b821482b010881cf427d0
Colin Robbins <ColinRobbins@users.noreply.github.com>
2020-04-12 17:02:10 +0100

    Update homeassistant/components/lightwave/climate.py
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/lightwave/climate.py
+++ homeassistant/components/lightwave/climate.py
@@ -17,8 +17,6 @@ from homeassistant.const import (
 
 from . import CONF_SERIAL, LIGHTWAVE_LINK
 
-_LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Find and return LightWave lights."""
