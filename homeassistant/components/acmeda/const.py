e97cf4b9885d1b2932c3dde3c86e38be4baa1d75
Alan Murray <github@atmurray.net>
2020-04-13 17:11:22 +1000

    Bumped aiopulse module to 0.3.2. Reduced verbosity of aiopulse module.

--- homeassistant/components/acmeda/const.py
+++ homeassistant/components/acmeda/const.py
@@ -1,7 +1,6 @@
 """Constants for the Rollease Acmeda Automate integration."""
 import logging
 
-logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__package__)
 DOMAIN = "acmeda"
 
