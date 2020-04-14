5a9970e63ccad5ee84e73593a0edcdc4509833a6
Paulus Schoutsen <balloob@gmail.com>
2020-04-13 17:41:01 -0700

    Dump states in event handler for HA_Stop (#33974)
    * Dump states in event handler for HA_Stop
    
    * Fix type

--- homeassistant/helpers/restore_state.py
+++ homeassistant/helpers/restore_state.py
@@ -171,14 +171,13 @@ class RestoreStateData:
     def async_setup_dump(self, *args: Any) -> None:
         """Set up the restore state listeners."""
 
-        @callback
-        def _async_dump_states(*_: Any) -> None:
-            self.hass.async_create_task(self.async_dump_states())
+        async def _async_dump_states(*_: Any) -> None:
+            await self.async_dump_states()
 
         # Dump the initial states now. This helps minimize the risk of having
         # old states loaded by overwriting the last states once Home Assistant
         # has started and the old states have been read.
-        _async_dump_states()
+        self.hass.async_create_task(_async_dump_states())
 
         # Dump states periodically
         async_track_time_interval(self.hass, _async_dump_states, STATE_DUMP_INTERVAL)
