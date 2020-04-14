226e8973d41ecef28d4cdae53909b4b995c56021
J. Nick Koston <nick@koston.org>
2020-04-14 19:27:02 +0000

    Convert homekit fans to use service callbacks
    * Convert homekit fans to use service callbacks
    
    Service callbacks allow us ensure that we call
    fan services in the correct order.

--- tests/components/homekit/common.py
+++ tests/components/homekit/common.py
@@ -1,6 +1,9 @@
 """Collection of fixtures and functions for the HomeKit tests."""
 from unittest.mock import patch
 
+from pyhap.accessory_driver import AccessoryDriver
+import pytest
+
 
 def patch_debounce():
     """Return patch for debounce method."""
@@ -8,3 +11,12 @@ def patch_debounce():
         "homeassistant.components.homekit.accessories.debounce",
         lambda f: lambda *args, **kwargs: f(*args, **kwargs),
     )
+
+
+@pytest.fixture
+def driver():
+    """Patch AccessoryDriver without zeroconf or HAPServer."""
+    with patch("pyhap.accessory_driver.HAPServer"), patch(
+        "pyhap.accessory_driver.Zeroconf"
+    ), patch("pyhap.accessory_driver.AccessoryDriver.persist"):
+        yield AccessoryDriver()
