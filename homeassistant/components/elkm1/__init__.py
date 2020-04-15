ffbb4a355920cd0d5e58cca9d72e6992925e1cb1
J. Nick Koston <nick@koston.org>
2020-04-15 23:48:59 +0000

    Handle cv.temperature_unit mutate

--- homeassistant/components/elkm1/__init__.py
+++ homeassistant/components/elkm1/__init__.py
@@ -25,6 +25,8 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.typing import ConfigType
 
 from .const import (
+    BARE_TEMP_CELSIUS,
+    BARE_TEMP_FAHRENHEIT,
     CONF_AREA,
     CONF_AUTO_CONFIGURE,
     CONF_COUNTER,
@@ -121,8 +123,9 @@ DEVICE_SCHEMA = vol.Schema(
         vol.Optional(CONF_USERNAME, default=""): cv.string,
         vol.Optional(CONF_PASSWORD, default=""): cv.string,
         vol.Optional(CONF_AUTO_CONFIGURE, default=False): cv.boolean,
+        # cv.temperature_unit will mutate 'C' -> '°C' and 'F' -> '°F'
         vol.Optional(
-            CONF_TEMPERATURE_UNIT, default=TEMP_FAHRENHEIT
+            CONF_TEMPERATURE_UNIT, default=BARE_TEMP_FAHRENHEIT
         ): cv.temperature_unit,
         vol.Optional(CONF_AREA, default={}): DEVICE_SCHEMA_SUBDOMAIN,
         vol.Optional(CONF_COUNTER, default={}): DEVICE_SCHEMA_SUBDOMAIN,
@@ -192,7 +195,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     _LOGGER.debug("Setting up elkm1 %s", conf["host"])
 
     temperature_unit = TEMP_FAHRENHEIT
-    if conf[CONF_TEMPERATURE_UNIT] in ("C", TEMP_CELSIUS):
+    if conf[CONF_TEMPERATURE_UNIT] in (BARE_TEMP_CELSIUS, TEMP_CELSIUS):
         temperature_unit = TEMP_CELSIUS
 
     config = {"temperature_unit": temperature_unit}
