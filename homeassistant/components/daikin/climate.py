c2b676995973d57b4780a3b1e7c33aa59a025f51
Fredrik Erlandsson <fredrik.e@gmail.com>
2020-04-15 12:40:47 +0200

    Add daikin update_before_add (#34248)

--- homeassistant/components/daikin/climate.py
+++ homeassistant/components/daikin/climate.py
@@ -83,7 +83,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up Daikin climate based on config_entry."""
     daikin_api = hass.data[DAIKIN_DOMAIN].get(entry.entry_id)
-    async_add_entities([DaikinClimate(daikin_api)])
+    async_add_entities([DaikinClimate(daikin_api)], update_before_add=True)
 
 
 class DaikinClimate(ClimateDevice):
