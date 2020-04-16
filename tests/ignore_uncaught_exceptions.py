374987d650d4b5b5b9a3d61f08370bf51f1d8eb9
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-04-16 21:45:39 +0000

    remove ignore from localfile

--- tests/ignore_uncaught_exceptions.py
+++ tests/ignore_uncaught_exceptions.py
@@ -1,6 +1,4 @@
 """List of modules that have uncaught exceptions today. Will be shrunk over time."""
-IGNORE_UNCAUGHT_EXCEPTIONS = [
-    ("tests.components.local_file.test_camera", "test_file_not_readable"),
-]
+IGNORE_UNCAUGHT_EXCEPTIONS = []
 
 IGNORE_UNCAUGHT_JSON_EXCEPTIONS = []
