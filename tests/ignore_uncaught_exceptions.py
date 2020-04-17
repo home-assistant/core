b4d9e346c9591b3ae16218696ac83748cdea6bf0
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-04-17 11:25:31 +0300

    Fix uncaught exceptions in ios (#34119)
    * verify that the config in hass is not empty
    
    * changed to use MockConfigEntry
    
    * Update tests/components/ios/test_init.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * Update tests/components/ios/test_init.py
    
    Co-Authored-By: Martin Hjelmare <marhje52@gmail.com>
    
    * changed the test per suggestions
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/ignore_uncaught_exceptions.py
+++ tests/ignore_uncaught_exceptions.py
@@ -1,7 +1,5 @@
 """List of modules that have uncaught exceptions today. Will be shrunk over time."""
 IGNORE_UNCAUGHT_EXCEPTIONS = [
-    ("tests.components.ios.test_init", "test_creating_entry_sets_up_sensor"),
-    ("tests.components.ios.test_init", "test_not_configuring_ios_not_creates_entry"),
     ("tests.components.local_file.test_camera", "test_file_not_readable"),
 ]
 
