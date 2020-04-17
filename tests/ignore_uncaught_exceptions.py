656c865c766f7f15345f1c1046ad8dfb5b01b817
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-04-17 14:10:48 +0300

    Fix uncaught exception in local_file (#34312)
    * remove ignore from localfile
    
    * added mock_registry
    
    * since there are no more exceptions, infra to ignore them is redundant

--- tests/ignore_uncaught_exceptions.py
+++
@@ -1,6 +0,0 @@
-"""List of modules that have uncaught exceptions today. Will be shrunk over time."""
-IGNORE_UNCAUGHT_EXCEPTIONS = [
-    ("tests.components.local_file.test_camera", "test_file_not_readable"),
-]
-
-IGNORE_UNCAUGHT_JSON_EXCEPTIONS = []
