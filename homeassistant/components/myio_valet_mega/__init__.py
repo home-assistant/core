a6d1cb87904d98d1d04493c7313f41a28e4c9b8f
Smarthome Ninja <49280231+SmarthomeNinja@users.noreply.github.com>
2020-04-18 00:54:34 +0200

    Update __init__.py

--- homeassistant/components/myio_valet_mega/__init__.py
+++ homeassistant/components/myio_valet_mega/__init__.py
@@ -60,7 +60,7 @@ async def async_setup_entry(hass, config_entry):
     hass.states.async_set(f"{_server_name}.state", _server_status)
     try:
         _refresh_timer = config_entry.options[CONF_REFRESH_TIME]
-    except (ValueError, Exception):
+    except (ValueError, Exception): # pylint: disable=broad-except
         _refresh_timer = 4
 
     def server_status():
