aa19d5f7e7318d36691bd896e5f5872b09e7f1f1
Thomas Delaet <thomas@delaet.org>
2020-04-18 17:37:44 +0200

    handle unavailable state in template fan
    fixes issue #31170

--- homeassistant/components/template/fan.py
+++ homeassistant/components/template/fan.py
@@ -26,6 +26,7 @@ from homeassistant.const import (
     STATE_OFF,
     STATE_ON,
     STATE_UNKNOWN,
+    STATE_UNAVAILABLE,
 )
 from homeassistant.core import callback
 from homeassistant.exceptions import TemplateError
@@ -341,7 +342,7 @@ class TemplateFan(FanEntity):
         # Validate state
         if state in _VALID_STATES:
             self._state = state
-        elif state == STATE_UNKNOWN:
+        elif state in [STATE_UNKNOWN, STATE_UNAVAILABLE]:
             self._state = None
         else:
             _LOGGER.error(
@@ -363,7 +364,7 @@ class TemplateFan(FanEntity):
             # Validate speed
             if speed in self._speed_list:
                 self._speed = speed
-            elif speed == STATE_UNKNOWN:
+            elif speed in [STATE_UNKNOWN, STATE_UNAVAILABLE]:
                 self._speed = None
             else:
                 _LOGGER.error(
@@ -385,7 +386,7 @@ class TemplateFan(FanEntity):
                 self._oscillating = True
             elif oscillating == "False" or oscillating is False:
                 self._oscillating = False
-            elif oscillating == STATE_UNKNOWN:
+            elif oscillating in [STATE_UNKNOWN, STATE_UNAVAILABLE]:
                 self._oscillating = None
             else:
                 _LOGGER.error(
@@ -406,7 +407,7 @@ class TemplateFan(FanEntity):
             # Validate speed
             if direction in _VALID_DIRECTIONS:
                 self._direction = direction
-            elif direction == STATE_UNKNOWN:
+            elif direction in [STATE_UNKNOWN, STATE_UNAVAILABLE]:
                 self._direction = None
             else:
                 _LOGGER.error(
