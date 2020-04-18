4f3202d58e20635e0959160472e6f6e22f1b0b0a
starkillerOG <starkiller.og@gmail.com>
2020-04-18 19:38:21 +0200

    Implement Xiaomi Miio Config Flow

--- homeassistant/components/xiaomi_miio/__init__.py
+++ homeassistant/components/xiaomi_miio/__init__.py
@@ -1,81 +1,83 @@
 """Support for Xiaomi Miio."""
 import logging
-
-import voluptuous as vol
-
+import asyncio
 from homeassistant.const import (
     CONF_HOST,
-    CONF_NAME,
     CONF_TOKEN,
 )
 
 from miio import DeviceException, gateway
 
-from homeassistant.helpers import discovery
-import homeassistant.helpers.config_validation as cv
+from homeassistant import config_entries, core
+from homeassistant.helpers import config_validation as cv, device_registry as dr
+
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'xiaomi_miio'
+GATEWAY_PLATFORMS = ["alarm_control_panel"]
 
-CONF_GATEWAYS = "gateways"
-KEY_GATEWAY_DEVICE = "device"
-KEY_GATEWAY_INFO = "info"
 
-DEFAULT_ALARM_NAME = "Xiaomi Gateway Alarm"
-
-GATEWAY_CONFIG = vol.Schema(
-    {
-        vol.Required(CONF_HOST): cv.string,
-        vol.Required(CONF_TOKEN): vol.All(cv.string, vol.Length(min=32, max=32)),
-        vol.Optional(CONF_NAME, default=DEFAULT_ALARM_NAME): cv.string,
-    }
-)
-
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Optional(CONF_GATEWAYS, default={}): vol.All(
-                    cv.ensure_list,
-                    vol.All([GATEWAY_CONFIG]),
-                )
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
-
-def setup(hass, config):
+async def async_setup(hass: core.HomeAssistant, config: dict):
     """Set up the Xiaomi Miio component."""
-    gateways = []
-    hass.data[DOMAIN] = {
-        CONF_GATEWAYS: []
-    }
-    if DOMAIN in config:
-        gateways = config[DOMAIN][CONF_GATEWAYS]
+    conf = config.get(DOMAIN)
+    hass.data.setdefault(DOMAIN, {})
 
-    for gw_device in gateways:
-        host = gw_device.get(CONF_HOST)
-        token = gw_device.get(CONF_TOKEN)
-        name = gw_device.get(CONF_NAME)
-        _LOGGER.info("Initializing with host %s (token %s...)", host, token[:5])
+    if not conf:
+        return True
 
-        try:
-            gateway_device = gateway.Gateway(host, token)
-            gateway_info = gateway_device.info()
-            _LOGGER.info(
-                "%s %s %s detected",
-                gateway_info.model,
-                gateway_info.firmware_version,
-                gateway_info.hardware_version,
-            )
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_IMPORT}, data=conf
+        )
+    )
+    return True
 
-            hass.data[DOMAIN][CONF_GATEWAYS].append({CONF_NAME: name, KEY_GATEWAY_DEVICE: gateway_device, KEY_GATEWAY_INFO: gateway_info})
-        except DeviceException:
-            _LOGGER.error("DeviceException during setup of xiaomi gateway with host %s", host)
 
-    for component in ["alarm_control_panel"]:
-        discovery.load_platform(hass, component, DOMAIN, {}, config)
+async def async_setup_entry(
+    hass: core.HomeAssistant, entry: config_entries.ConfigEntry
+):
+    """Set up the Xiaomi Miio component from a config entry."""
+    host = entry.data.get(CONF_HOST)
+    token = entry.data.get(CONF_TOKEN)
+    name = entry.title
+    gateway_id = entry.data.get("gateway_id")
+    _LOGGER.info("Initializing with host %s (token %s...)", host, token[:5])
+
+    # Connect to gateway
+    try:
+        gateway_device = gateway.Gateway(host, token)
+        gateway_info = gateway_device.info()
+        _LOGGER.info(
+            "%s %s %s detected",
+            gateway_info.model,
+            gateway_info.firmware_version,
+            gateway_info.hardware_version,
+        )
+    except DeviceException:
+        _LOGGER.error(
+            "DeviceException during setup of xiaomi gateway with host %s", host
+        )
+        return False
+
+    hass.data[DOMAIN][entry.entry_id] = gateway_device
+
+    gateway_model = "{}-{}".format(gateway_info.model, gateway_info.hardware_version)
+
+    device_registry = await dr.async_get_registry(hass)
+    device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        connections={(dr.CONNECTION_NETWORK_MAC, gateway_info.mac_address)},
+        identifiers={(DOMAIN, gateway_id)},
+        manufacturer="Xiaomi",
+        name=name,
+        model=gateway_model,
+        sw_version=gateway_info.firmware_version,
+    )
+
+    for component in GATEWAY_PLATFORMS:
+        hass.async_add_job(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
 
     return True
