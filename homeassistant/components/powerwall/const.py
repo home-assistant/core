34fe4a679beecb0d9c5c090fb96f28cdcc829f34
J. Nick Koston <nick@koston.org>
2020-04-19 19:51:15 +0000

    Powerwall charging margin of error is much higher than expected

--- homeassistant/components/powerwall/const.py
+++ homeassistant/components/powerwall/const.py
@@ -45,9 +45,9 @@ POWERWALL_RUNNING_KEY = "running"
 POWERWALL_BATTERY_METER = "battery"
 
 # We only declare charging if they are getting
-# at least 12W incoming as measuring the fields
+# at least 40W incoming as measuring the fields
 # is not an exact science because of interference
-CHARGING_MARGIN_OF_ERROR = -12
+CHARGING_MARGIN_OF_ERROR = -40
 
 MODEL = "PowerWall 2"
 MANUFACTURER = "Tesla"
