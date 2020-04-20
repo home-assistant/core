5d5230df2ba7baf435967a2b8b1b884e1607bc50
Rami Mosleh <engrbm87@gmail.com>
2020-04-20 15:22:48 +0300

    Add Islamic Prayer Times config flow (#31590)
    * Add Islamic Prayer Times config_flow
    
    * Add Islamic Prayer Times config_flow
    
    * handle options update and fix tests
    
    * fix sensor update handling
    
    * fix pylint
    
    * fix scheduled update and add test
    
    * update test_init
    
    * update flow options to show drop list
    
    * clean up code
    
    * async scheduling and revert state to timestamp
    
    * fix update retry method
    
    * update strings
    
    * keep title as root key

--- tests/components/islamic_prayer_times/__init__.py
+++ tests/components/islamic_prayer_times/__init__.py
@@ -1,1 +1,45 @@
 """Tests for the islamic_prayer_times component."""
+
+from datetime import datetime
+
+PRAYER_TIMES = {
+    "Fajr": "06:10",
+    "Sunrise": "07:25",
+    "Dhuhr": "12:30",
+    "Asr": "15:32",
+    "Maghrib": "17:35",
+    "Isha": "18:53",
+    "Midnight": "00:45",
+}
+
+PRAYER_TIMES_TIMESTAMPS = {
+    "Fajr": datetime(2020, 1, 1, 6, 10, 0),
+    "Sunrise": datetime(2020, 1, 1, 7, 25, 0),
+    "Dhuhr": datetime(2020, 1, 1, 12, 30, 0),
+    "Asr": datetime(2020, 1, 1, 15, 32, 0),
+    "Maghrib": datetime(2020, 1, 1, 17, 35, 0),
+    "Isha": datetime(2020, 1, 1, 18, 53, 0),
+    "Midnight": datetime(2020, 1, 1, 00, 45, 0),
+}
+
+NEW_PRAYER_TIMES = {
+    "Fajr": "06:00",
+    "Sunrise": "07:25",
+    "Dhuhr": "12:30",
+    "Asr": "15:32",
+    "Maghrib": "17:45",
+    "Isha": "18:53",
+    "Midnight": "00:43",
+}
+
+NEW_PRAYER_TIMES_TIMESTAMPS = {
+    "Fajr": datetime(2020, 1, 1, 6, 00, 0),
+    "Sunrise": datetime(2020, 1, 1, 7, 25, 0),
+    "Dhuhr": datetime(2020, 1, 1, 12, 30, 0),
+    "Asr": datetime(2020, 1, 1, 15, 32, 0),
+    "Maghrib": datetime(2020, 1, 1, 17, 45, 0),
+    "Isha": datetime(2020, 1, 1, 18, 53, 0),
+    "Midnight": datetime(2020, 1, 1, 00, 43, 0),
+}
+
+NOW = datetime(2020, 1, 1, 00, 00, 0)
