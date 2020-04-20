dee554e6585b75332dc5f0cda8ba55e4b86674f4
Jacob Adlers <jacob.adlers@gmail.com>
2020-04-20 13:53:17 +0200

    Add jenkins url to config

--- homeassistant/components/jenkins/__init__.py
+++ homeassistant/components/jenkins/__init__.py
@@ -2,18 +2,11 @@
 
 import logging
 
+from jenkinsapi.jenkins import Jenkins
 import voluptuous as vol
 
-# from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_URL
 import homeassistant.helpers.config_validation as cv
-
-# from homeassistant.const import (
-#     ATTR_ATTRIBUTION,
-#     CONF_API_KEY,
-#     CONF_MONITORED_CONDITIONS,
-#     CONF_SCAN_INTERVAL,
-#     TIME_SECONDS,
-# )
 
 DOMAIN = "jenkins"
 _LOGGER = logging.getLogger(__name__)
@@ -23,16 +16,16 @@ CONF_REPOSITORY = "repository"
 CONF_BRANCH = "branch"
 
 # Default values
-DEFAULT_BRANCH_NAME = "master"
-
+DEFAULT_BRANCH = "master"
 
 # Validating configuration
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
             {
+                vol.Required(CONF_URL): cv.string,
                 vol.Required(CONF_REPOSITORY): cv.string,
-                vol.Optional(CONF_BRANCH, default=DEFAULT_BRANCH_NAME): cv.string,
+                vol.Optional(CONF_BRANCH, default=DEFAULT_BRANCH): cv.string,
             }
         )
     },
@@ -43,11 +36,15 @@ CONFIG_SCHEMA = vol.Schema(
 def setup(hass, config):
     """Set up the Jenkins integration."""
 
-    hass.states.set("jenkins.dummy", "Jacob")
     hass.states.set("jenkins.repo", config[DOMAIN].get(CONF_REPOSITORY))
     hass.states.set("jenkins.branch", config[DOMAIN].get(CONF_BRANCH))
 
-    _LOGGER.debug("repo: %s" % (config[DOMAIN].get(CONF_REPOSITORY)))
-    _LOGGER.debug("branch: %s" % (config[DOMAIN].get(CONF_BRANCH)))
+    _LOGGER.debug(f"repo: {config[DOMAIN].get(CONF_URL)}")
+    _LOGGER.debug(f"repo: {config[DOMAIN].get(CONF_REPOSITORY)}")
+    _LOGGER.debug(f"branch: {config[DOMAIN].get(CONF_BRANCH)}")
+
+    server = Jenkins(config[DOMAIN].get(CONF_URL))
+    for (job_name, job) in server.get_jobs():
+        _LOGGER.debug(f"Job name: {job_name}")
 
     return True
