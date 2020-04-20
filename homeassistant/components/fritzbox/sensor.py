c87ecf0ff639bb8f8b33da1bb1b4c16aeed39c90
escoand <escoand@users.noreply.github.com>
2020-04-20 15:00:07 +0200

    Add config flow and device registry to fritzbox integration (#31240)
    * add config flow
    
    * fix pylint
    
    * update lib
    
    * Update config_flow.py
    
    * remote devices layer in config
    
    * add default host
    
    * avoid double setups of entities
    
    * remove async_setup_platform
    
    * store entities in hass.data
    
    * pass fritz connection together with config_entry
    
    * fritz connections try no4 (or is it even more)
    
    * fix comments
    
    * add unloading
    
    * fixed comments
    
    * Update config_flow.py
    
    * Update const.py
    
    * Update config_flow.py
    
    * Update __init__.py
    
    * Update config_flow.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update config_flow.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update config_flow.py
    
    * add init tests
    
    * test unloading
    
    * add switch tests
    
    * add sensor tests
    
    * add climate tests
    
    * test target temperature
    
    * mock config to package
    
    * comments
    
    * test binary sensor state
    
    * add config flow tests
    
    * comments
    
    * add missing tests
    
    * minor
    
    * remove string title
    
    * deprecate yaml
    
    * don't change yaml
    
    * get devices async
    
    * minor
    
    * add devices again
    
    * comments fixed
    
    * unique_id fixes
    
    * fix patches
    
    * Fix schema
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/fritzbox/sensor.py
+++ homeassistant/components/fritzbox/sensor.py
@@ -1,33 +1,35 @@
 """Support for AVM Fritz!Box smarthome temperature sensor only devices."""
-import logging
-
 import requests
 
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import CONF_DEVICES, TEMP_CELSIUS
 from homeassistant.helpers.entity import Entity
 
-from . import ATTR_STATE_DEVICE_LOCKED, ATTR_STATE_LOCKED, DOMAIN as FRITZBOX_DOMAIN
+from .const import (
+    ATTR_STATE_DEVICE_LOCKED,
+    ATTR_STATE_LOCKED,
+    CONF_CONNECTIONS,
+    DOMAIN as FRITZBOX_DOMAIN,
+    LOGGER,
+)
 
-_LOGGER = logging.getLogger(__name__)
 
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Fritzbox smarthome sensor from config_entry."""
+    entities = []
+    devices = hass.data[FRITZBOX_DOMAIN][CONF_DEVICES]
+    fritz = hass.data[FRITZBOX_DOMAIN][CONF_CONNECTIONS][config_entry.entry_id]
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Fritzbox smarthome sensor platform."""
-    _LOGGER.debug("Initializing fritzbox temperature sensors")
-    devices = []
-    fritz_list = hass.data[FRITZBOX_DOMAIN]
+    for device in await hass.async_add_executor_job(fritz.get_devices):
+        if (
+            device.has_temperature_sensor
+            and not device.has_switch
+            and not device.has_thermostat
+            and device.ain not in devices
+        ):
+            entities.append(FritzBoxTempSensor(device, fritz))
+            devices.add(device.ain)
 
-    for fritz in fritz_list:
-        device_list = fritz.get_devices()
-        for device in device_list:
-            if (
-                device.has_temperature_sensor
-                and not device.has_switch
-                and not device.has_thermostat
-            ):
-                devices.append(FritzBoxTempSensor(device, fritz))
-
-    add_entities(devices)
+    async_add_entities(entities)
 
 
 class FritzBoxTempSensor(Entity):
@@ -37,6 +39,22 @@ class FritzBoxTempSensor(Entity):
         """Initialize the switch."""
         self._device = device
         self._fritz = fritz
+
+    @property
+    def device_info(self):
+        """Return device specific attributes."""
+        return {
+            "name": self.name,
+            "identifiers": {(FRITZBOX_DOMAIN, self._device.ain)},
+            "manufacturer": self._device.manufacturer,
+            "model": self._device.productname,
+            "sw_version": self._device.fw_version,
+        }
+
+    @property
+    def unique_id(self):
+        """Return the unique ID of the device."""
+        return self._device.ain
 
     @property
     def name(self):
@@ -58,7 +76,7 @@ class FritzBoxTempSensor(Entity):
         try:
             self._device.update()
         except requests.exceptions.HTTPError as ex:
-            _LOGGER.warning("Fritzhome connection error: %s", ex)
+            LOGGER.warning("Fritzhome connection error: %s", ex)
             self._fritz.login()
 
     @property
--- homeassistant/components/fritzbox/switch.py
+++ homeassistant/components/fritzbox/switch.py
@@ -1,34 +1,40 @@
 """Support for AVM Fritz!Box smarthome switch devices."""
-import logging
-
 import requests
 
 from homeassistant.components.switch import SwitchDevice
-from homeassistant.const import ATTR_TEMPERATURE, ENERGY_KILO_WATT_HOUR, TEMP_CELSIUS
+from homeassistant.const import (
+    ATTR_TEMPERATURE,
+    CONF_DEVICES,
+    ENERGY_KILO_WATT_HOUR,
+    TEMP_CELSIUS,
+)
 
-from . import ATTR_STATE_DEVICE_LOCKED, ATTR_STATE_LOCKED, DOMAIN as FRITZBOX_DOMAIN
+from .const import (
+    ATTR_STATE_DEVICE_LOCKED,
+    ATTR_STATE_LOCKED,
+    ATTR_TEMPERATURE_UNIT,
+    ATTR_TOTAL_CONSUMPTION,
+    ATTR_TOTAL_CONSUMPTION_UNIT,
+    CONF_CONNECTIONS,
+    DOMAIN as FRITZBOX_DOMAIN,
+    LOGGER,
+)
 
-_LOGGER = logging.getLogger(__name__)
-
-ATTR_TOTAL_CONSUMPTION = "total_consumption"
-ATTR_TOTAL_CONSUMPTION_UNIT = "total_consumption_unit"
 ATTR_TOTAL_CONSUMPTION_UNIT_VALUE = ENERGY_KILO_WATT_HOUR
 
-ATTR_TEMPERATURE_UNIT = "temperature_unit"
 
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Fritzbox smarthome switch from config_entry."""
+    entities = []
+    devices = hass.data[FRITZBOX_DOMAIN][CONF_DEVICES]
+    fritz = hass.data[FRITZBOX_DOMAIN][CONF_CONNECTIONS][config_entry.entry_id]
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Fritzbox smarthome switch platform."""
-    devices = []
-    fritz_list = hass.data[FRITZBOX_DOMAIN]
+    for device in await hass.async_add_executor_job(fritz.get_devices):
+        if device.has_switch and device.ain not in devices:
+            entities.append(FritzboxSwitch(device, fritz))
+            devices.add(device.ain)
 
-    for fritz in fritz_list:
-        device_list = fritz.get_devices()
-        for device in device_list:
-            if device.has_switch:
-                devices.append(FritzboxSwitch(device, fritz))
-
-    add_entities(devices)
+    async_add_entities(entities)
 
 
 class FritzboxSwitch(SwitchDevice):
@@ -38,6 +44,22 @@ class FritzboxSwitch(SwitchDevice):
         """Initialize the switch."""
         self._device = device
         self._fritz = fritz
+
+    @property
+    def device_info(self):
+        """Return device specific attributes."""
+        return {
+            "name": self.name,
+            "identifiers": {(FRITZBOX_DOMAIN, self._device.ain)},
+            "manufacturer": self._device.manufacturer,
+            "model": self._device.productname,
+            "sw_version": self._device.fw_version,
+        }
+
+    @property
+    def unique_id(self):
+        """Return the unique ID of the device."""
+        return self._device.ain
 
     @property
     def available(self):
@@ -67,7 +89,7 @@ class FritzboxSwitch(SwitchDevice):
         try:
             self._device.update()
         except requests.exceptions.HTTPError as ex:
-            _LOGGER.warning("Fritzhome connection error: %s", ex)
+            LOGGER.warning("Fritzhome connection error: %s", ex)
             self._fritz.login()
 
     @property
