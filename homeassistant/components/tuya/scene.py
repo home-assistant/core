192c2206b0cf447d4ebdf7c533797bf1d30498e4
Franck Nijhof <git@frenck.dev>
2020-04-21 03:07:50 +0200

    Add transition support to scenes, cleanup blocking parameter (#34434)

--- homeassistant/components/tuya/scene.py
+++ homeassistant/components/tuya/scene.py
@@ -1,4 +1,6 @@
 """Support for the Tuya scenes."""
+from typing import Any
+
 from homeassistant.components.scene import DOMAIN, Scene
 
 from . import DATA_TUYA, TuyaDevice
@@ -29,6 +31,6 @@ class TuyaScene(TuyaDevice, Scene):
         super().__init__(tuya)
         self.entity_id = ENTITY_ID_FORMAT.format(tuya.object_id())
 
-    def activate(self):
+    def activate(self, **kwargs: Any) -> None:
         """Activate the scene."""
         self.tuya.activate()
