defec50f0bc221f568074337b5775000be8d2092
ludeeus <joasoe@gmail.com>
2020-04-21 11:13:08 +0200

    cleanup

--- script/hassfest/translations.py
+++ script/hassfest/translations.py
@@ -44,7 +44,6 @@ def check_translations_directory_name(config: Config, integration: Integration) 
 
     if translations.is_dir():
         # No action required
-        os.rename(str(translations), str(legacy_translations))
         return
 
     if legacy_translations.is_dir():
