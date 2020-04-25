3244a633d086e1d146a8cfd20c0828f4e4a5bbaa
J. Nick Koston <nick@koston.org>
2020-04-25 02:39:14 +0000

    update test

--- tests/components/rachio/test_config_flow.py
+++ tests/components/rachio/test_config_flow.py
@@ -117,6 +117,8 @@ async def test_form_homekit(hass):
     )
     assert result["type"] == "form"
     assert result["errors"] == {}
+    flows_in_progress = hass.config_entries.flow.async_progress()
+    assert flows_in_progress[0]["context"]["unique_id"] == "AA:BB:CC:DD:EE:FF"
 
     entry = MockConfigEntry(domain=DOMAIN, data={CONF_API_KEY: "api_key"})
     entry.add_to_hass(hass)
