9b2b7f28b5f0ab529b8b0cf7dcd9c02adac5926a
escoand <escoand@users.noreply.github.com>
2020-04-26 00:40:26 +0200

    Fix fritzbox integration errors (#34639)

--- homeassistant/components/fritzbox/config_flow.py
+++ homeassistant/components/fritzbox/config_flow.py
@@ -83,7 +83,7 @@ class FritzboxConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
 
             for entry in self.hass.config_entries.async_entries(DOMAIN):
-                if entry.data[CONF_HOST] == user_input[CONF_HOST]:
+                if entry.data.get(CONF_HOST) == user_input[CONF_HOST]:
                     if entry.data != user_input:
                         self.hass.config_entries.async_update_entry(
                             entry, data=user_input
@@ -117,7 +117,7 @@ class FritzboxConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 return self.async_abort(reason="already_in_progress")
 
         for entry in self.hass.config_entries.async_entries(DOMAIN):
-            if entry.data[CONF_HOST] == host:
+            if entry.data.get(CONF_HOST) == host:
                 if entry.data != user_input:
                     self.hass.config_entries.async_update_entry(entry, data=user_input)
                 return self.async_abort(reason="already_configured")
