2bf8d35a340ad6c5076c41cdb44cf2a6dcca03f6
alxrdn <alxrdn@users.noreply.github.com>
2020-04-25 13:37:00 +0000

    Refactor to set config yaml section under the top level integration domain key

--- homeassistant/components/rpi_camera/camera.py
+++ homeassistant/components/rpi_camera/camera.py
@@ -5,62 +5,27 @@ import shutil
 import subprocess
 from tempfile import NamedTemporaryFile
 
-import voluptuous as vol
-
-from homeassistant.components.camera import PLATFORM_SCHEMA, Camera
+from homeassistant.components.camera import Camera
 from homeassistant.const import CONF_FILE_PATH, CONF_NAME, EVENT_HOMEASSISTANT_STOP
-from homeassistant.helpers import config_validation as cv
+
+from .const import (
+    CONF_HORIZONTAL_FLIP,
+    CONF_IMAGE_HEIGHT,
+    CONF_IMAGE_QUALITY,
+    CONF_IMAGE_ROTATION,
+    CONF_IMAGE_WIDTH,
+    CONF_OVERLAY_METADATA,
+    CONF_OVERLAY_TIMESTAMP,
+    CONF_TIMELAPSE,
+    CONF_VERTICAL_FLIP,
+    DOMAIN,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_HORIZONTAL_FLIP = "horizontal_flip"
-CONF_IMAGE_HEIGHT = "image_height"
-CONF_IMAGE_QUALITY = "image_quality"
-CONF_IMAGE_ROTATION = "image_rotation"
-CONF_IMAGE_WIDTH = "image_width"
-CONF_TIMELAPSE = "timelapse"
-CONF_VERTICAL_FLIP = "vertical_flip"
-CONF_OVERLAY_METADATA = "overlay_metadata"
-CONF_OVERLAY_TIMESTAMP = "overlay_timestamp"
-
-DEFAULT_HORIZONTAL_FLIP = 0
-DEFAULT_IMAGE_HEIGHT = 480
-DEFAULT_IMAGE_QUALITY = 7
-DEFAULT_IMAGE_ROTATION = 0
-DEFAULT_IMAGE_WIDTH = 640
-DEFAULT_NAME = "Raspberry Pi Camera"
-DEFAULT_TIMELAPSE = 1000
-DEFAULT_VERTICAL_FLIP = 0
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Optional(CONF_FILE_PATH): cv.isfile,
-        vol.Optional(CONF_HORIZONTAL_FLIP, default=DEFAULT_HORIZONTAL_FLIP): vol.All(
-            vol.Coerce(int), vol.Range(min=0, max=1)
-        ),
-        vol.Optional(CONF_IMAGE_HEIGHT, default=DEFAULT_IMAGE_HEIGHT): vol.Coerce(int),
-        vol.Optional(CONF_IMAGE_QUALITY, default=DEFAULT_IMAGE_QUALITY): vol.All(
-            vol.Coerce(int), vol.Range(min=0, max=100)
-        ),
-        vol.Optional(CONF_IMAGE_ROTATION, default=DEFAULT_IMAGE_ROTATION): vol.All(
-            vol.Coerce(int), vol.Range(min=0, max=359)
-        ),
-        vol.Optional(CONF_IMAGE_WIDTH, default=DEFAULT_IMAGE_WIDTH): vol.Coerce(int),
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Optional(CONF_TIMELAPSE, default=1000): vol.Coerce(int),
-        vol.Optional(CONF_VERTICAL_FLIP, default=DEFAULT_VERTICAL_FLIP): vol.All(
-            vol.Coerce(int), vol.Range(min=0, max=1)
-        ),
-        vol.Optional(CONF_OVERLAY_METADATA): vol.All(
-            vol.Coerce(int), vol.Range(min=4, max=2056)
-        ),
-        vol.Optional(CONF_OVERLAY_TIMESTAMP): cv.string,
-    }
-)
-
-
 def kill_raspistill(*args):
     """Kill any previously running raspistill process.."""
+    _LOGGER.debug("killall raspistill")
     subprocess.Popen(
         ["killall", "raspistill"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT
     )
@@ -68,27 +33,16 @@ def kill_raspistill(*args):
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Raspberry Camera."""
+    _LOGGER.debug("setup_platform")
     if shutil.which("raspistill") is None:
         _LOGGER.error("'raspistill' was not found")
         return False
 
-    setup_config = {
-        CONF_NAME: config.get(CONF_NAME),
-        CONF_IMAGE_WIDTH: config.get(CONF_IMAGE_WIDTH),
-        CONF_IMAGE_HEIGHT: config.get(CONF_IMAGE_HEIGHT),
-        CONF_IMAGE_QUALITY: config.get(CONF_IMAGE_QUALITY),
-        CONF_IMAGE_ROTATION: config.get(CONF_IMAGE_ROTATION),
-        CONF_TIMELAPSE: config.get(CONF_TIMELAPSE),
-        CONF_HORIZONTAL_FLIP: config.get(CONF_HORIZONTAL_FLIP),
-        CONF_VERTICAL_FLIP: config.get(CONF_VERTICAL_FLIP),
-        CONF_FILE_PATH: config.get(CONF_FILE_PATH),
-        CONF_OVERLAY_METADATA: config.get(CONF_OVERLAY_METADATA),
-        CONF_OVERLAY_TIMESTAMP: config.get(CONF_OVERLAY_TIMESTAMP),
-    }
+    _LOGGER.debug("hass.data[%s] = %s", DOMAIN, hass.data[DOMAIN])
 
     hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, kill_raspistill)
 
-    file_path = setup_config[CONF_FILE_PATH]
+    file_path = hass.data[DOMAIN][CONF_FILE_PATH]
 
     def delete_temp_file(*args):
         """Delete the temporary file to prevent saving multiple temp images.
@@ -102,15 +56,16 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         temp_file = NamedTemporaryFile(suffix=".jpg", delete=False)
         temp_file.close()
         file_path = temp_file.name
-        setup_config[CONF_FILE_PATH] = file_path
+        hass.data[DOMAIN][CONF_FILE_PATH] = file_path
         hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, delete_temp_file)
+        _LOGGER.debug("hass.data[%s][%s] = %s", DOMAIN, CONF_FILE_PATH, hass.data[DOMAIN][CONF_FILE_PATH])
 
     # Check whether the file path has been whitelisted
     elif not hass.config.is_allowed_path(file_path):
         _LOGGER.error("'%s' is not a whitelisted directory", file_path)
         return False
 
-    add_entities([RaspberryCamera(setup_config)])
+    add_entities([RaspberryCamera(hass.data[DOMAIN])])
 
 
 class RaspberryCamera(Camera):
@@ -119,7 +74,9 @@ class RaspberryCamera(Camera):
     def __init__(self, device_info):
         """Initialize Raspberry Pi camera component."""
         super().__init__()
+        _LOGGER.debug("RaspberryCamera::init")
 
+        _LOGGER.debug("device_info = %s", device_info)
         self._name = device_info[CONF_NAME]
         self._config = device_info
 
@@ -160,10 +117,14 @@ class RaspberryCamera(Camera):
             cmd_args.append("-a")
             cmd_args.append(str(device_info[CONF_OVERLAY_TIMESTAMP]))
 
+        _LOGGER.debug("cmd_args = %s", cmd_args)
+
         subprocess.Popen(cmd_args, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
     def camera_image(self):
         """Return raspistill image response."""
+        _LOGGER.debug("returning image from %s", self._config[CONF_FILE_PATH])
+
         with open(self._config[CONF_FILE_PATH], "rb") as file:
             return file.read()
 
@@ -171,3 +132,9 @@ class RaspberryCamera(Camera):
     def name(self):
         """Return the name of this camera."""
         return self._name
+
+    @property
+    def frame_interval(self):
+        """Return the interval between frames of the stream."""
+        return self._config[CONF_TIMELAPSE]/1000
+
