64a28ae6768e0e1615503be3b9884f9ec0a3f1a2
Jesse Hills <jesse@springload.co.nz>
2020-04-27 13:33:44 +1200

    Black

--- homeassistant/components/juicenet/entity.py
+++ homeassistant/components/juicenet/entity.py
@@ -34,7 +34,9 @@ class JuiceNetDevice(Entity):
 
     async def async_added_to_hass(self):
         """Subscribe to updates."""
-        self.async_on_remove(self.coordinator.async_add_listener(self.async_write_ha_state))
+        self.async_on_remove(
+            self.coordinator.async_add_listener(self.async_write_ha_state)
+        )
 
     @property
     def unique_id(self):
