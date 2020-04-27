bc857ca149c48b703928507257ffb1ddbc682b7b
Martin Hjelmare <marhje52@gmail.com>
2020-04-27 02:38:26 +0200

    Clean wrong pin error

--- tests/components/ps4/test_config_flow.py
+++ tests/components/ps4/test_config_flow.py
@@ -452,14 +452,31 @@ async def test_credential_timeout(hass):
 
 async def test_wrong_pin_error(hass):
     """Test that incorrect pin throws an error."""
-    flow = ps4.PlayStation4FlowHandler()
-    flow.hass = hass
-    flow.location = MOCK_LOCATION
+    with patch("pyps4_2ndscreen.Helper.port_bind", return_value=None):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": "user"}
+        )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "creds"
 
-    with patch("pyps4_2ndscreen.Helper.link", return_value=(True, False)), patch(
+    with patch("pyps4_2ndscreen.Helper.get_creds", return_value=MOCK_CREDS):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input={}
+        )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "mode"
+
+    with patch(
         "pyps4_2ndscreen.Helper.has_devices", return_value=[{"host-ip": MOCK_HOST}]
     ):
-        result = await flow.async_step_link(MOCK_CONFIG)
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input=MOCK_AUTO
+        )
+
+    with patch("pyps4_2ndscreen.Helper.link", return_value=(True, False)):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input=MOCK_CONFIG
+        )
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "link"
     assert result["errors"] == {"base": "login_failed"}
