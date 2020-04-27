454f499c1c9b02ae3aeebcd81de5719b62855fe5
Markus Bong <Markus.Bong@devolo.de>
2020-04-27 07:37:42 +0200

    use lowercase snake

--- homeassistant/components/devolo_home_control/const.py
+++ homeassistant/components/devolo_home_control/const.py
@@ -4,5 +4,5 @@ DOMAIN = "devolo_home_control"
 DEFAULT_MYDEVOLO = "https://www.mydevolo.com"
 DEFAULT_MPRM = "https://homecontrol.mydevolo.com"
 PLATFORMS = ["switch"]
-CONF_MYDEVOLO = "Mydevolo_URL"
-CONF_HOMECONTROL = "Home_Control_URL"
+CONF_MYDEVOLO = "mydevolo_url"
+CONF_HOMECONTROL = "home_control_url"
