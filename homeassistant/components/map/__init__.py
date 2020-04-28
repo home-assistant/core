d1fe52f54d9b3154922bf237a26d8dd273ebdab8
Hmmbob <33529490+hmmbob@users.noreply.github.com>
2020-04-28 21:24:12 +0200

    Fix CheckFormat

--- homeassistant/components/map/__init__.py
+++ homeassistant/components/map/__init__.py
@@ -10,9 +10,7 @@ DOMAIN = "map"
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
-            {
-                vol.Optional(CONF_REQUIRE_ADMIN, default=False): cv.boolean,
-            }
+            {vol.Optional(CONF_REQUIRE_ADMIN, default=False): cv.boolean,}
         )
     },
     extra=vol.ALLOW_EXTRA,
@@ -22,9 +20,6 @@ CONFIG_SCHEMA = vol.Schema(
 async def async_setup(hass, config):
     """Register the built-in map panel."""
     hass.components.frontend.async_register_built_in_panel(
-        "map",
-        "map",
-        "hass:tooltip-account",
-        require_admin=CONF_REQUIRE_ADMIN,
+        "map", "map", "hass:tooltip-account", require_admin=CONF_REQUIRE_ADMIN,
     )
     return True
