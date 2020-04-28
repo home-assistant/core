7c94968386db44ee13313a92493742fe958e8356
Jacob Adlers <jacob.adlers@gmail.com>
2020-04-28 16:53:43 +0200

    Add error messages in config flow

--- homeassistant/components/jenkins/config_flow.py
+++ homeassistant/components/jenkins/config_flow.py
@@ -1,6 +1,6 @@
 """Config flow to configure the Jenkins integration."""
 from jenkinsapi.jenkins import Jenkins
-from requests.exceptions import HTTPError
+from requests.exceptions import HTTPError, MissingSchema
 import voluptuous as vol
 
 from homeassistant import config_entries
@@ -18,9 +18,15 @@ class JenkinsFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle a flow initiated by the user."""
 
-        if user_input is not None:
-            _LOGGER.debug(f"Step 1. Got user_input: {user_input}")
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_URL): str,
+                vol.Optional(CONF_USERNAME): str,
+                vol.Optional(CONF_TOKEN): str,
+            }
+        )
 
+        if user_input is not None:
             self.url = user_input[CONF_URL]
             self.username = (
                 user_input[CONF_USERNAME] if CONF_USERNAME in user_input else None
@@ -31,30 +37,41 @@ class JenkinsFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 self.server = await self.hass.async_add_executor_job(
                     Jenkins, self.url, self.username, self.token
                 )
+                _LOGGER.debug(f"Successfully connected to host: {self.url}")
                 return await self.async_step_job()
-            except HTTPError as err:
-                _LOGGER.error(err)
-                # return await self.async_step_userLogin()
+            except MissingSchema:
+                return self.async_show_form(
+                    step_id="user",
+                    data_schema=data_schema,
+                    errors={"base": "missing_schema"},
+                )
+            except HTTPError as error:
+                if error.response.status_code == 403 and self.username is None:
+                    return self.async_show_form(
+                        step_id="user",
+                        data_schema=data_schema,
+                        errors={"base": "credentials_needed"},
+                    )
+                elif error.response.status_code == 403:
+                    return self.async_show_form(
+                        step_id="user",
+                        data_schema=data_schema,
+                        errors={"base": "invalid_credentials"},
+                    )
+                elif error.response.status_code == 404:
+                    return self.async_show_form(
+                        step_id="user",
+                        data_schema=data_schema,
+                        errors={"base": "invalid_host"},
+                    )
 
-        _LOGGER.debug(f"Setting up jenkins")
-
-        data_schema = {
-            vol.Required(CONF_URL): str,
-            vol.Optional(CONF_USERNAME): str,
-            vol.Optional(CONF_TOKEN): str,
-        }
-
-        return self.async_show_form(step_id="user", data_schema=vol.Schema(data_schema))
+        return self.async_show_form(step_id="user", data_schema=data_schema)
 
     async def async_step_job(self, user_input=None):
         """Handle selecting which job to add a sensor for."""
-        if self.server is None:
-            _LOGGER.error("Got to step 2 without an server connection")
-
         if user_input is not None:
-            _LOGGER.debug(f"User decided on the following: {user_input}")
-
             self.job_name = user_input[CONF_JOB_NAME]
+            _LOGGER.debug(f"Creating entry for {self.job_name}.")
 
             return self.async_create_entry(
                 title=user_input[CONF_JOB_NAME],
