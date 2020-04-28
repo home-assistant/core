0e907737a9e37de74c1f9e29971e521b3c1a3371
Hmmbob <33529490+hmmbob@users.noreply.github.com>
2020-04-28 20:35:54 +0200

    Enable require_admin for lovelace panel

--- homeassistant/components/map/__init__.py
+++ homeassistant/components/map/__init__.py
@@ -1,10 +1,29 @@
 """Support for showing device locations."""
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+
+CONF_REQUIRE_ADMIN = "require_admin"
+
 DOMAIN = "map"
 
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Optional(CONF_REQUIRE_ADMIN, default=False): cv.boolean,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
 async def async_setup(hass, config):
     """Register the built-in map panel."""
     hass.components.frontend.async_register_built_in_panel(
-        "map", "map", "hass:tooltip-account"
+        "map",
+        "map",
+        "hass:tooltip-account",
+        require_admin=CONF_REQUIRE_ADMIN,
     )
     return True
