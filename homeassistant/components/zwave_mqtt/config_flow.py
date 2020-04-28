1884d33fc13921c0ca03dc5f7ba6c194816a99ad
Martin Hjelmare <marhje52@gmail.com>
2020-04-28 17:25:56 +0200

    Improve config flow and add test

--- homeassistant/components/zwave_mqtt/config_flow.py
+++ homeassistant/components/zwave_mqtt/config_flow.py
@@ -1,11 +1,7 @@
 """Config flow for zwave_mqtt integration."""
-import logging
-
 from homeassistant import config_entries
 
 from .const import DOMAIN  # pylint:disable=unused-import
-
-_LOGGER = logging.getLogger(__name__)
 
 TITLE = "Z-Wave MQTT"
 
@@ -18,4 +14,7 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        return self.async_create_entry(title=TITLE, data={})
+        if user_input is not None:
+            return self.async_create_entry(title=TITLE, data={})
+
+        return self.async_show_form(step_id="user")
---
+++ tests/components/zwave_mqtt/test_config_flow.py
@@ -0,0 +1,31 @@
+"""Test the Z-Wave over MQTT config flow."""
+from asynctest import patch
+
+from homeassistant import config_entries, setup
+from homeassistant.components.zwave_mqtt.config_flow import TITLE
+from homeassistant.components.zwave_mqtt.const import DOMAIN
+
+
+async def test_form(hass):
+    """Test we get the form."""
+    hass.config.components.add("mqtt")
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == "form"
+    assert result["errors"] is None
+
+    with patch(
+        "homeassistant.components.zwave_mqtt.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.zwave_mqtt.async_setup_entry", return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(result["flow_id"], {})
+
+    assert result2["type"] == "create_entry"
+    assert result2["title"] == TITLE
+    assert result2["data"] == {}
+    await hass.async_block_till_done()
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
