1f27ec955568b8f49dbc70b71496d9935aafac75
Guillaume DELVIT <guiguid@free.fr>
2020-04-29 19:22:00 +0200

    Add all configuration parameter with default choice

--- homeassistant/components/serial/sensor.py
+++ homeassistant/components/serial/sensor.py
@@ -17,9 +17,36 @@ _LOGGER = logging.getLogger(__name__)
 
 CONF_SERIAL_PORT = "serial_port"
 CONF_BAUDRATE = "baudrate"
+CONF_BYTESIZE = "bytesize"
+CONF_PARITY = "parity"
+CONF_STOPBITS = "stopbits"
+CONF_XONXOFF = "xonxoff"
+CONF_RTSCTS = "rtscts"
+CONF_DSRDTR = "dsrdtr"
 
 DEFAULT_NAME = "Serial Sensor"
 DEFAULT_BAUDRATE = 9600
+DEFAULT_BYTESIZE = serial_asyncio.serial.EIGHTBITS
+DEFAULT_PARITY = serial_asyncio.serial.PARITY_NONE
+DEFAULT_STOPBITS = serial_asyncio.serial.STOPBITS_ONE
+DEFAULT_XONXOFF = False
+DEFAULT_RTSCTS = False
+DEFAULT_DSRDTR = False
+
+"""
+ __init__(port=None, baudrate=9600, bytesize=EIGHTBITS, parity=PARITY_NONE, stopbits=STOPBITS_ONE, timeout=None, xonxoff=False, rtscts=False, write_timeout=None, dsrdtr=False, inter_byte_timeout=None, exclusive=None)
+    Parameters:	
+
+        port – Device name or None.
+        baudrate (int) – Baud rate such as 9600 or 115200 etc.
+        bytesize – Number of data bits. Possible values: FIVEBITS, SIXBITS, SEVENBITS, EIGHTBITS
+        parity – Enable parity checking. Possible values: PARITY_NONE, PARITY_EVEN, PARITY_ODD PARITY_MARK, PARITY_SPACE
+        stopbits – Number of stop bits. Possible values: STOPBITS_ONE, STOPBITS_ONE_POINT_FIVE, STOPBITS_TWO
+        timeout (float) – Set a read timeout value.
+        xonxoff (bool) – Enable software flow control.
+        rtscts (bool) – Enable hardware (RTS/CTS) flow control.
+        dsrdtr (bool) – Enable hardware (DSR/DTR) flow control.
+"""
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
@@ -27,6 +54,12 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_BAUDRATE, default=DEFAULT_BAUDRATE): cv.positive_int,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+        vol.Optional(CONF_BYTESIZE, default=DEFAULT_BYTESIZE): cv.positive_int,
+        vol.Optional(CONF_PARITY, default=DEFAULT_PARITY): cv.string,
+        vol.Optional(CONF_STOPBITS, default=DEFAULT_STOPBITS): cv.positive_int,
+        vol.Optional(CONF_XONXOFF, default=DEFAULT_XONXOFF): cv.boolean,
+        vol.Optional(CONF_RTSCTS, default=DEFAULT_RTSCTS): cv.boolean,
+        vol.Optional(CONF_DSRDTR, default=DEFAULT_DSRDTR): cv.boolean,
     }
 )
 
@@ -36,12 +69,29 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     name = config.get(CONF_NAME)
     port = config.get(CONF_SERIAL_PORT)
     baudrate = config.get(CONF_BAUDRATE)
+    bytesize = config.get(CONF_BYTESIZE)
+    parity = config.get(CONF_PARITY)
+    stopbits = config.get(CONF_STOPBITS)
+    xonxoff = config.get(CONF_XONXOFF)
+    rtscts = config.get(CONF_RTSCTS)
+    dsrdtr = config.get(CONF_DSRDTR)
 
     value_template = config.get(CONF_VALUE_TEMPLATE)
     if value_template is not None:
         value_template.hass = hass
 
-    sensor = SerialSensor(name, port, baudrate, value_template)
+    sensor = SerialSensor(
+        name,
+        port,
+        baudrate,
+        bytesize,
+        parity,
+        stopbits,
+        xonxoff,
+        rtscts,
+        dsrdtr,
+        value_template,
+    )
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, sensor.stop_serial_read)
     async_add_entities([sensor], True)
@@ -50,12 +100,30 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 class SerialSensor(Entity):
     """Representation of a Serial sensor."""
 
-    def __init__(self, name, port, baudrate, value_template):
+    def __init__(
+        self,
+        name,
+        port,
+        baudrate,
+        bytesize,
+        parity,
+        stopbits,
+        xonxoff,
+        rtscts,
+        dsrdtr,
+        value_template,
+    ):
         """Initialize the Serial sensor."""
         self._name = name
         self._state = None
         self._port = port
         self._baudrate = baudrate
+        self._bytesize = bytesize
+        self._parity = parity
+        self._stopbits = stopbits
+        self._xonxoff = xonxoff
+        self._rtscts = rtscts
+        self._dsrdtr = dsrdtr
         self._serial_loop_task = None
         self._template = value_template
         self._attributes = None
@@ -63,17 +131,45 @@ class SerialSensor(Entity):
     async def async_added_to_hass(self):
         """Handle when an entity is about to be added to Home Assistant."""
         self._serial_loop_task = self.hass.loop.create_task(
-            self.serial_read(self._port, self._baudrate)
+            self.serial_read(
+                self._port,
+                self._baudrate,
+                self._bytesize,
+                self._parity,
+                self._stopbits,
+                self._xonxoff,
+                self._rtscts,
+                self._dsrdtr,
+            )
         )
 
-    async def serial_read(self, device, rate, **kwargs):
+    async def serial_read(
+        self,
+        device,
+        baudrate,
+        bytesize,
+        parity,
+        stopbits,
+        xonxoff,
+        rtscts,
+        dsrdtr,
+        **kwargs,
+    ):
         """Read the data from the port."""
         logged_error = False
         while True:
             try:
                 reader, _ = await serial_asyncio.open_serial_connection(
-                    url=device, baudrate=rate, **kwargs
+                    url=device,
+                    baudrate=baudrate,
+                    bytesize=bytesize,
+                    parity=parity,
+                    stopbits=stopbits,
+                    xonxoff=xonxoff,
+                    rtscts=rtscts,
+                    dsrdtr=dsrdtr ** kwargs,
                 )
+
             except SerialException as exc:
                 if not logged_error:
                     _LOGGER.exception(
