ad34ecb39dd1328e18bb471c236b7aa2b2248a6c
Paulus Schoutsen <balloob@gmail.com>
2020-04-29 16:52:04 -0700

    Fix on py37 imports

--- tests/async_mock.py
+++ tests/async_mock.py
@@ -2,8 +2,7 @@
 import sys
 
 if sys.version_info[:2] < (3, 8):
-    from asynctest import *  # noqa
-
-    AsyncMock = CoroutineMock  # noqa
+    from asynctest.mock import *  # noqa
+    from asynctest.mock import CoroutineMock as AsyncMock  # noqa
 else:
     from unittest.mock import *  # noqa
