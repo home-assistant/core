eb4ea5fa7db25f95680df5c74ee26bd61fa99bcf
Ian Harcombe <ian.harcombe@gmail.com>
2020-05-01 15:02:38 +0100

    - Code changes following reviewer comments on the PR - Not all sensor entities are now enabled by default - Changes to the test cases to only therefore test sensor entities that *are* enabled

--- tests/components/metoffice/conftest.py
+++ tests/components/metoffice/conftest.py
@@ -1,17 +1,8 @@
 """Fixtures for Met Office weather integration tests."""
-<<<<<<< HEAD
 from datapoint.exceptions import APIException
 import pytest
 
 from tests.async_mock import patch
-=======
-from unittest.mock import patch
-
-from datapoint.exceptions import APIException
-import pytest
-
-from tests.common import mock_coro
->>>>>>> Added further test cases to improve patch coverage. Now tests more negative cases, as well as the positive cases.
 
 
 @pytest.fixture()
@@ -19,15 +10,8 @@ def mock_simple_manager_fail():
     """Mock datapoint Manager with default values for testing in config_flow."""
     with patch("datapoint.Manager") as mock_manager:
         instance = mock_manager.return_value
-<<<<<<< HEAD
         instance.get_nearest_forecast_site.side_effect = APIException()
         instance.get_forecast_for_site.side_effect = APIException()
-=======
-        instance.get_nearest_forecast_site.return_value = mock_coro(
-            exception=APIException
-        )
-        instance.get_forecast_for_site.return_value = mock_coro(exception=APIException)
->>>>>>> Added further test cases to improve patch coverage. Now tests more negative cases, as well as the positive cases.
         instance.latitude = None
         instance.longitude = None
         instance.site = None
