9be141f0635ad126bf800dcebb1df8eef9737bb4
Joakim Plate <elupus@ecce.se>
2020-05-01 23:35:12 +0200

    Replace yaml with config entry

--- homeassistant/components/arcam_fmj/config_flow.py
+++ homeassistant/components/arcam_fmj/config_flow.py
@@ -1,12 +1,63 @@
 """Config flow to configure the Arcam FMJ component."""
-from operator import itemgetter
+import asyncio
+import logging
+from urllib.parse import urlparse
+
+import aiohttp
+from defusedxml import ElementTree
+import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.components.ssdp import ATTR_SSDP_LOCATION, ATTR_UPNP_UDN
 from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.data_entry_flow import AbortFlow
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN
+from .const import DEFAULT_NAME, DEFAULT_PORT, DOMAIN, DOMAIN_DATA_ENTRIES
 
-_GETKEY = itemgetter(CONF_HOST, CONF_PORT)
+_LOGGER = logging.getLogger(__name__)
+
+
+def _strip_uuid_prefix(data):
+    return data[5:].split("-")[4]
+
+
+def _log_exception(msg, *args):
+    """Log an error and turn on traceback if debug is on."""
+    _LOGGER.error(msg, *args, exc_info=_LOGGER.getEffectiveLevel() == logging.DEBUG)
+
+
+async def _get_uniqueid_from_device_description(hass, url):
+    """Retrieve and extract unique id from url."""
+    try:
+        session = async_get_clientsession(hass)
+        async with session.get(url) as req:
+            req.raise_for_status()
+            data = await req.text()
+    except (aiohttp.ClientError, asyncio.TimeoutError):
+        _log_exception("Unable to get device description from %s", url)
+        return None
+
+    try:
+        xml = ElementTree.fromstring(data)
+    except ElementTree.ParseError:
+        _log_exception("Unable to parse xml from %s", url)
+        return None
+
+    udn = xml.findtext("d:device/d:UDN", None, {"d": "urn:schemas-upnp-org:device-1-0"})
+    return _strip_uuid_prefix(udn)
+
+
+async def _get_uniqueid_from_host(hass, host):
+    """Try to deduce a unique id from a host based on ssdp/upnp."""
+    return await _get_uniqueid_from_device_description(
+        hass, f"http://{host}:8080/dd.xml"
+    )
+
+
+def get_entry_client(hass, entry):
+    """Retrieve client associated with a config entry."""
+    return hass.data[DOMAIN_DATA_ENTRIES][entry.entry_id]
 
 
 @config_entries.HANDLERS.register(DOMAIN)
@@ -16,12 +67,75 @@ class ArcamFmjFlowHandler(config_entries.ConfigFlow):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
-    async def async_step_import(self, import_config):
-        """Import a config entry from configuration.yaml."""
-        entries = self.hass.config_entries.async_entries(DOMAIN)
-        import_key = _GETKEY(import_config)
-        for entry in entries:
-            if _GETKEY(entry.data) == import_key:
-                return self.async_abort(reason="already_setup")
+    async def _async_set_unique_id_and_update(self, host, port, uuid):
+        config = {
+            CONF_HOST: host,
+            CONF_PORT: port,
+        }
+        entry = await self.async_set_unique_id(uuid)
+        if not entry:
+            return
 
-        return self.async_create_entry(title="Arcam FMJ", data=import_config)
+        if config == entry.data:
+            raise AbortFlow("already_setup")
+        self.hass.config_entries.async_update_entry(entry, data=config)
+        raise AbortFlow("updated_instance")
+
+    async def _async_create_entry(self, host, port):
+        return self.async_create_entry(
+            title=f"{DEFAULT_NAME} ({host})", data={CONF_HOST: host, CONF_PORT: port},
+        )
+
+    async def async_step_user(self, user_info):
+        """Handle a discovered device."""
+        errors = {}
+
+        if user_info is not None:
+            uuid = await _get_uniqueid_from_host(self.hass, user_info[CONF_HOST])
+            if uuid:
+                await self._async_set_unique_id_and_update(
+                    user_info[CONF_HOST], user_info[CONF_PORT], uuid
+                )
+
+            return await self._async_create_entry(
+                user_info[CONF_HOST], user_info[CONF_PORT]
+            )
+
+        fields = {
+            vol.Required(CONF_HOST): str,
+            vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
+        }
+
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema(fields), errors=errors
+        )
+
+    async def async_step_confirm(self, user_input=None):
+        """Handle user-confirmation of discovered node."""
+        context = self.context  # pylint: disable=no-member
+        placeholders = {
+            "host": context[CONF_HOST],
+        }
+        context["title_placeholders"] = placeholders
+
+        if user_input is not None:
+            return await self._async_create_entry(
+                context[CONF_HOST], context[CONF_PORT]
+            )
+
+        return self.async_show_form(
+            step_id="confirm", description_placeholders=placeholders
+        )
+
+    async def async_step_ssdp(self, discovery_info):
+        """Handle a discovered device."""
+        host = urlparse(discovery_info[ATTR_SSDP_LOCATION]).hostname
+        port = DEFAULT_PORT
+        uuid = _strip_uuid_prefix(discovery_info[ATTR_UPNP_UDN])
+
+        await self._async_set_unique_id_and_update(host, port, uuid)
+
+        context = self.context  # pylint: disable=no-member
+        context[CONF_HOST] = host
+        context[CONF_PORT] = DEFAULT_PORT
+        return await self.async_step_confirm(None)
--- tests/components/arcam_fmj/test_config_flow.py
+++ tests/components/arcam_fmj/test_config_flow.py
@@ -3,35 +3,149 @@
 import pytest
 
 from homeassistant import data_entry_flow
-from homeassistant.components.arcam_fmj.config_flow import ArcamFmjFlowHandler
-from homeassistant.components.arcam_fmj.const import DOMAIN
+from homeassistant.components import ssdp
+from homeassistant.components.arcam_fmj.config_flow import (
+    ArcamFmjFlowHandler,
+    get_entry_client,
+)
+from homeassistant.components.arcam_fmj.const import DOMAIN, DOMAIN_DATA_ENTRIES
+from homeassistant.const import CONF_HOST, CONF_PORT
 
-from .conftest import MOCK_CONFIG, MOCK_NAME
+from .conftest import (
+    MOCK_CONFIG_ENTRY,
+    MOCK_HOST,
+    MOCK_NAME,
+    MOCK_PORT,
+    MOCK_UDN,
+    MOCK_UUID,
+)
 
 from tests.common import MockConfigEntry
 
+MOCK_UPNP_DEVICE = f"""
+<root xmlns="urn:schemas-upnp-org:device-1-0">
+  <device>
+    <UDN>{MOCK_UDN}</UDN>
+  </device>
+</root>
+"""
 
-@pytest.fixture(name="config_entry")
-def config_entry_fixture():
-    """Create a mock Arcam config entry."""
-    return MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, title=MOCK_NAME)
+MOCK_UPNP_LOCATION = f"http://{MOCK_HOST}:8080/dd.xml"
+
+MOCK_DISCOVER = {
+    ssdp.ATTR_UPNP_MANUFACTURER: "ARCAM",
+    ssdp.ATTR_UPNP_MODEL_NAME: " ",
+    ssdp.ATTR_UPNP_MODEL_NUMBER: "AVR450, AVR750",
+    ssdp.ATTR_UPNP_FRIENDLY_NAME: f"Arcam media client {MOCK_UUID}",
+    ssdp.ATTR_UPNP_SERIAL: "12343",
+    ssdp.ATTR_SSDP_LOCATION: f"http://{MOCK_HOST}:8080/dd.xml",
+    ssdp.ATTR_UPNP_UDN: MOCK_UDN,
+    ssdp.ATTR_UPNP_DEVICE_TYPE: "urn:schemas-upnp-org:device:MediaRenderer:1",
+}
 
 
-async def test_single_import_only(hass, config_entry):
-    """Test form is shown when host not provided."""
-    config_entry.add_to_hass(hass)
+@pytest.fixture(name="flow")
+def flow_fixture(hass):
+    """Create a mock flow for use in tests."""
     flow = ArcamFmjFlowHandler()
     flow.hass = hass
-    result = await flow.async_step_import(MOCK_CONFIG)
-    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
-    assert result["reason"] == "already_setup"
+    flow.context = {}
+    return flow
 
 
-async def test_import(hass):
-    """Test form is shown when host not provided."""
-    flow = ArcamFmjFlowHandler()
-    flow.hass = hass
-    result = await flow.async_step_import(MOCK_CONFIG)
+async def test_ssdp(hass, flow):
+    """Test a ssdp import flow."""
+    result = await flow.async_step_ssdp(MOCK_DISCOVER)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "confirm"
+    assert flow.context["host"] == MOCK_HOST
+    assert flow.unique_id == MOCK_UUID
+
+    result = await flow.async_step_confirm({})
     assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == "Arcam FMJ"
-    assert result["data"] == MOCK_CONFIG
+    assert result["title"] == f"Arcam FMJ ({MOCK_HOST})"
+    assert result["data"] == MOCK_CONFIG_ENTRY
+
+
+async def test_ssdp_abort(hass, flow):
+    """Test a ssdp import flow."""
+    entry = MockConfigEntry(
+        domain=DOMAIN, data=MOCK_CONFIG_ENTRY, title=MOCK_NAME, unique_id=MOCK_UUID
+    )
+    entry.add_to_hass(hass)
+
+    with pytest.raises(data_entry_flow.AbortFlow, match="Flow aborted: already_setup"):
+        await flow.async_step_ssdp(MOCK_DISCOVER)
+
+
+async def test_ssdp_update(hass, flow):
+    """Test a ssdp import flow."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_HOST: "old_host", CONF_PORT: MOCK_PORT},
+        title=MOCK_NAME,
+        unique_id=MOCK_UUID,
+    )
+    entry.add_to_hass(hass)
+
+    with pytest.raises(
+        data_entry_flow.AbortFlow, match="Flow aborted: updated_instance"
+    ):
+        await flow.async_step_ssdp(MOCK_DISCOVER)
+
+
+async def test_user(hass, flow, aioclient_mock):
+    """Test a manual user configuration flow."""
+    result = await flow.async_step_user(None)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "user"
+
+    user_input = {
+        CONF_HOST: MOCK_HOST,
+        CONF_PORT: MOCK_PORT,
+    }
+
+    aioclient_mock.get(MOCK_UPNP_LOCATION, text=MOCK_UPNP_DEVICE)
+    result = await flow.async_step_user(user_input)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == f"Arcam FMJ ({MOCK_HOST})"
+    assert result["data"] == MOCK_CONFIG_ENTRY
+    assert flow.unique_id == MOCK_UUID
+
+
+async def test_invalid_ssdp(hass, flow, aioclient_mock):
+    """Test a manual user configuration flow."""
+    user_input = {
+        CONF_HOST: MOCK_HOST,
+        CONF_PORT: MOCK_PORT,
+    }
+
+    aioclient_mock.get(MOCK_UPNP_LOCATION, text="")
+    result = await flow.async_step_user(user_input)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == f"Arcam FMJ ({MOCK_HOST})"
+    assert result["data"] == MOCK_CONFIG_ENTRY
+    assert flow.unique_id is None
+
+
+async def test_user_wrong(hass, flow, aioclient_mock):
+    """Test a manual user configuration flow with no ssdp response."""
+    user_input = {
+        CONF_HOST: MOCK_HOST,
+        CONF_PORT: MOCK_PORT,
+    }
+
+    aioclient_mock.get(MOCK_UPNP_LOCATION, status=404)
+    result = await flow.async_step_user(user_input)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == f"Arcam FMJ ({MOCK_HOST})"
+    assert flow.unique_id is None
+
+
+async def test_get_entry_client(hass):
+    """Test helper for configuration."""
+    entry = MockConfigEntry(
+        domain=DOMAIN, data=MOCK_CONFIG_ENTRY, title=MOCK_NAME, unique_id=MOCK_UUID
+    )
+    hass.data[DOMAIN_DATA_ENTRIES] = {entry.entry_id: "dummy"}
+    assert get_entry_client(hass, entry) == "dummy"
