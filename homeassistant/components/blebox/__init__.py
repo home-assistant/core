1e67f8d24d4c0f76641fcd93e486aec4def0a181
Gadget Mobile <the_gadget_mobile@yahoo.com>
2020-05-03 00:30:44 +0200

    stop sharing logger with blebox_uniapi

--- homeassistant/components/blebox/__init__.py
+++ homeassistant/components/blebox/__init__.py
@@ -36,7 +36,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     port = entry.data[CONF_PORT]
     timeout = DEFAULT_SETUP_TIMEOUT
 
-    api_host = ApiHost(host, port, timeout, websession, hass.loop, _LOGGER)
+    api_host = ApiHost(host, port, timeout, websession, hass.loop)
 
     try:
         product = await Products.async_from_host(api_host)
