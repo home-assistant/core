f445a81a4d1d870808c4c2868129b8e7555381a0
Robert Svensson <Kane610@users.noreply.github.com>
2020-05-02 23:16:45 +0200

    UniFi - Catch controllers running on UniFi OS that don't have a local user configured (#35060)

--- homeassistant/components/unifi/config_flow.py
+++ homeassistant/components/unifi/config_flow.py
@@ -32,7 +32,12 @@ from .const import (
     LOGGER,
 )
 from .controller import get_controller
-from .errors import AlreadyConfigured, AuthenticationRequired, CannotConnect
+from .errors import (
+    AlreadyConfigured,
+    AuthenticationRequired,
+    CannotConnect,
+    NoLocalUser,
+)
 
 DEFAULT_PORT = 8443
 DEFAULT_SITE_ID = "default"
@@ -135,6 +140,8 @@ class UnifiFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
                 for site in self.sites.values():
                     if desc == site["desc"]:
+                        if "role" not in site:
+                            raise NoLocalUser
                         self.config[CONF_SITE_ID] = site["name"]
                         break
 
@@ -152,6 +159,9 @@ class UnifiFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
             except AlreadyConfigured:
                 return self.async_abort(reason="already_configured")
+
+            except NoLocalUser:
+                return self.async_abort(reason="no_local_user")
 
         if len(self.sites) == 1:
             self.desc = next(iter(self.sites.values()))["desc"]
