9cde7b020c0423bd522dd2279fac94dac9b9a08e
Gadget Mobile <the_gadget_mobile@yahoo.com>
2020-05-03 18:54:58 +0200

    assure that async_update is initially properly mocked

--- tests/components/blebox/conftest.py
+++ tests/components/blebox/conftest.py
@@ -51,6 +51,7 @@ def mock_only_feature(spec, **kwargs):
 def mock_feature(category, spec, **kwargs):
     """Mock a feature along with whole product setup."""
     feature_mock = mock_only_feature(spec, **kwargs)
+    feature_mock.async_update = AsyncMock()
     product = setup_product_mock(category, [feature_mock])
 
     type(feature_mock.product).name = PropertyMock(return_value="Some name")
