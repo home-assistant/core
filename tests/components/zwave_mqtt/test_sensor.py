24bdd516ef6f0ed608a3e41a7cceb8182194ef10
Charles Garwood <cgarwood@gmail.com>
2020-05-03 21:52:14 -0400

    Add test for disabled entities

--- tests/components/zwave_mqtt/test_sensor.py
+++ tests/components/zwave_mqtt/test_sensor.py
@@ -11,3 +11,21 @@ async def test_sensor(hass, generic_data):
     assert state is not None
     assert state.state == "123.9"
     assert state.attributes["unit_of_measurement"] == "V"
+
+    # Test ZWaveListSensor disabled by default
+    registry = await hass.helpers.entity_registry.async_get_registry()
+    entity_id = "sensor.water_sensor_6_instance_1_water"
+    state = hass.states.get(entity_id)
+    assert state is None
+
+    entry = registry.async_get(entity_id)
+    assert entry
+    assert entry.disabled
+    assert entry.disabled_by == "integration"
+
+    # Test enabling entity
+    updated_entry = registry.async_update_entity(
+        entry.entity_id, **{"disabled_by": None}
+    )
+    assert updated_entry != entry
+    assert updated_entry.disabled is False
