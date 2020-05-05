5d06cab2f5ad5eba3aee483f9d8d7fb3c408d075
starkillerOG <starkiller.og@gmail.com>
2020-05-05 14:39:22 +0200

    black + use fixture to reduce duplication

--- tests/components/denonavr/test_config_flow.py
+++ tests/components/denonavr/test_config_flow.py
@@ -1,10 +1,22 @@
 """Test the DenonAVR config flow."""
+from unittest.mock import patch
+
+import pytest
+
 from homeassistant import config_entries
 from homeassistant.components import ssdp
-from homeassistant.components.denonavr.config_flow import CONF_RECEIVER_ID, CONF_SHOW_ALL_SOURCES, CONF_ZONE2, CONF_ZONE3, DEFAULT_SHOW_SOURCES, DEFAULT_TIMEOUT, DEFAULT_ZONE2, DEFAULT_ZONE3, DOMAIN
+from homeassistant.components.denonavr.config_flow import (
+    CONF_RECEIVER_ID,
+    CONF_SHOW_ALL_SOURCES,
+    CONF_ZONE2,
+    CONF_ZONE3,
+    DEFAULT_SHOW_SOURCES,
+    DEFAULT_TIMEOUT,
+    DEFAULT_ZONE2,
+    DEFAULT_ZONE3,
+    DOMAIN,
+)
 from homeassistant.const import CONF_HOST, CONF_TIMEOUT
-
-from unittest.mock import patch
 
 TEST_HOST = "1.2.3.4"
 TEST_HOST2 = "5.6.7.8"
@@ -16,6 +28,42 @@ TEST_SSDP_LOCATION = f"http://{TEST_HOST}/"
 TEST_UNIQUE_ID = f"{TEST_MODEL}-{TEST_SERIALNUMBER}"
 TEST_DISCOVER_1_RECEIVER = [{CONF_HOST: TEST_HOST}]
 TEST_DISCOVER_2_RECEIVER = [{CONF_HOST: TEST_HOST}, {CONF_HOST: TEST_HOST2}]
+
+
+@pytest.fixture(name="denonavr_connect", autouse=True)
+def denonavr_connect_fixture():
+    """Mock denonavr connection and entry setup."""
+    with patch(
+        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_input_func_list",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_receiver_name",
+        return_value=TEST_NAME,
+    ), patch(
+        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_support_sound_mode",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr_2016",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.get_device_info",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.name", TEST_NAME,
+    ), patch(
+        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.model_name",
+        TEST_MODEL,
+    ), patch(
+        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.serial_number",
+        TEST_SERIALNUMBER,
+    ), patch(
+        "homeassistant.components.denonavr.async_setup_entry", return_value=True
+    ):
+        yield
+
 
 async def test_config_flow_manual_host_success(hass):
     """Test a successful config flow manualy initialized by the user with the host specified."""
@@ -33,37 +81,8 @@ async def test_config_flow_manual_host_success(hass):
 
     assert result["type"] == "form"
     assert result["step_id"] == "settings"
-    
-    with patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_input_func_list",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_receiver_name",
-        return_value=TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_support_sound_mode",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr_2016",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.get_device_info",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.name", TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.model_name", TEST_MODEL,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.serial_number", TEST_SERIALNUMBER,
-    ), patch(
-        "homeassistant.components.denonavr.async_setup_entry", return_value=True
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {},
-        )
+
+    result = await hass.config_entries.flow.async_configure(result["flow_id"], {},)
 
     assert result["type"] == "create_entry"
     assert result["title"] == TEST_NAME
@@ -75,6 +94,7 @@ async def test_config_flow_manual_host_success(hass):
         CONF_ZONE3: DEFAULT_ZONE3,
         CONF_RECEIVER_ID: TEST_UNIQUE_ID,
     }
+
 
 async def test_config_flow_manual_discover_1_success(hass):
     """Test a successful config flow manualy initialized by the user without the host specified and 1 receiver discovered."""
@@ -87,46 +107,15 @@ async def test_config_flow_manual_discover_1_success(hass):
     assert result["errors"] == {}
 
     with patch(
-            "homeassistant.components.denonavr.config_flow.denonavr.ssdp.identify_denonavr_receivers",
-            return_value=TEST_DISCOVER_1_RECEIVER,
+        "homeassistant.components.denonavr.config_flow.denonavr.ssdp.identify_denonavr_receivers",
+        return_value=TEST_DISCOVER_1_RECEIVER,
     ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {},
-        )
+        result = await hass.config_entries.flow.async_configure(result["flow_id"], {},)
 
     assert result["type"] == "form"
     assert result["step_id"] == "settings"
-    
-    with patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_input_func_list",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_receiver_name",
-        return_value=TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_support_sound_mode",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr_2016",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.get_device_info",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.name", TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.model_name", TEST_MODEL,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.serial_number", TEST_SERIALNUMBER,
-    ), patch(
-        "homeassistant.components.denonavr.async_setup_entry", return_value=True
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {},
-        )
+
+    result = await hass.config_entries.flow.async_configure(result["flow_id"], {},)
 
     assert result["type"] == "create_entry"
     assert result["title"] == TEST_NAME
@@ -139,6 +128,7 @@ async def test_config_flow_manual_discover_1_success(hass):
         CONF_RECEIVER_ID: TEST_UNIQUE_ID,
     }
 
+
 async def test_config_flow_manual_discover_2_success(hass):
     """Test a successful config flow manualy initialized by the user without the host specified and 2 receivers discovered."""
     result = await hass.config_entries.flow.async_init(
@@ -150,54 +140,23 @@ async def test_config_flow_manual_discover_2_success(hass):
     assert result["errors"] == {}
 
     with patch(
-            "homeassistant.components.denonavr.config_flow.denonavr.ssdp.identify_denonavr_receivers",
-            return_value=TEST_DISCOVER_2_RECEIVER,
+        "homeassistant.components.denonavr.config_flow.denonavr.ssdp.identify_denonavr_receivers",
+        return_value=TEST_DISCOVER_2_RECEIVER,
     ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {},
-        )
+        result = await hass.config_entries.flow.async_configure(result["flow_id"], {},)
 
     assert result["type"] == "form"
     assert result["step_id"] == "select"
     assert result["errors"] == {}
-    
+
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"], {"select_host": TEST_HOST2},
     )
-    
+
     assert result["type"] == "form"
     assert result["step_id"] == "settings"
-    
-    with patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_input_func_list",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_receiver_name",
-        return_value=TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_support_sound_mode",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr_2016",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.get_device_info",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.name", TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.model_name", TEST_MODEL,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.serial_number", TEST_SERIALNUMBER,
-    ), patch(
-        "homeassistant.components.denonavr.async_setup_entry", return_value=True
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {},
-        )
+
+    result = await hass.config_entries.flow.async_configure(result["flow_id"], {},)
 
     assert result["type"] == "create_entry"
     assert result["title"] == TEST_NAME
@@ -210,6 +169,7 @@ async def test_config_flow_manual_discover_2_success(hass):
         CONF_RECEIVER_ID: TEST_UNIQUE_ID,
     }
 
+
 async def test_config_flow_manual_discover_error(hass):
     """Test a failed config flow manualy initialized by the user without the host specified and no receivers discovered."""
     result = await hass.config_entries.flow.async_init(
@@ -221,17 +181,16 @@ async def test_config_flow_manual_discover_error(hass):
     assert result["errors"] == {}
 
     with patch(
-            "homeassistant.components.denonavr.config_flow.denonavr.ssdp.identify_denonavr_receivers",
-            return_value=[],
+        "homeassistant.components.denonavr.config_flow.denonavr.ssdp.identify_denonavr_receivers",
+        return_value=[],
     ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {},
-        )
+        result = await hass.config_entries.flow.async_configure(result["flow_id"], {},)
 
     assert result["type"] == "form"
     assert result["step_id"] == "user"
-    assert result["errors"] == {'base': 'discovery_error'}
-    
+    assert result["errors"] == {"base": "discovery_error"}
+
+
 async def test_config_flow_settings(hass):
     """Test a succesfull config flow manualy initialized by the user with the host specified and non default settings specified."""
     result = await hass.config_entries.flow.async_init(
@@ -248,37 +207,16 @@ async def test_config_flow_settings(hass):
 
     assert result["type"] == "form"
     assert result["step_id"] == "settings"
-    
-    with patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_input_func_list",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_receiver_name",
-        return_value=TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_support_sound_mode",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr_2016",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.get_device_info",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.name", TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.model_name", TEST_MODEL,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.serial_number", TEST_SERIALNUMBER,
-    ), patch(
-        "homeassistant.components.denonavr.async_setup_entry", return_value=True
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {CONF_TIMEOUT: 5, CONF_SHOW_ALL_SOURCES: True, CONF_ZONE2: True, CONF_ZONE3: True},
-        )
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_TIMEOUT: 5,
+            CONF_SHOW_ALL_SOURCES: True,
+            CONF_ZONE2: True,
+            CONF_ZONE3: True,
+        },
+    )
 
     assert result["type"] == "create_entry"
     assert result["title"] == TEST_NAME
@@ -290,6 +228,7 @@ async def test_config_flow_settings(hass):
         CONF_ZONE3: True,
         CONF_RECEIVER_ID: TEST_UNIQUE_ID,
     }
+
 
 async def test_config_flow_manual_host_connection_error(hass):
     """Test a failed config flow manualy initialized by the user with the host specified and a connection error."""
@@ -307,32 +246,16 @@ async def test_config_flow_manual_host_connection_error(hass):
 
     assert result["type"] == "form"
     assert result["step_id"] == "settings"
-    
+
     with patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_input_func_list",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_receiver_name",
-        return_value=TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_support_sound_mode",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr_2016",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr",
-        return_value=True,
-    ), patch(
         "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.get_device_info",
         side_effect=ConnectionError,
     ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {},
-        )
+        result = await hass.config_entries.flow.async_configure(result["flow_id"], {},)
 
     assert result["type"] == "abort"
     assert result["reason"] == "connection_error"
+
 
 async def test_config_flow_ssdp(hass):
     """Test a succesfull config flow initialized by ssdp discovery."""
@@ -344,42 +267,13 @@ async def test_config_flow_ssdp(hass):
             ssdp.ATTR_UPNP_MODEL_NAME: TEST_MODEL,
             ssdp.ATTR_UPNP_SERIAL: TEST_SERIALNUMBER,
             ssdp.ATTR_SSDP_LOCATION: TEST_SSDP_LOCATION,
-        }
+        },
     )
 
     assert result["type"] == "form"
     assert result["step_id"] == "settings"
-    
-    with patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_input_func_list",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_receiver_name",
-        return_value=TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._get_support_sound_mode",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr_2016",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR._update_avr",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.get_device_info",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.name", TEST_NAME,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.model_name", TEST_MODEL,
-    ), patch(
-        "homeassistant.components.denonavr.receiver.denonavr.DenonAVR.serial_number", TEST_SERIALNUMBER,
-    ), patch(
-        "homeassistant.components.denonavr.async_setup_entry", return_value=True
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {},
-        )
+
+    result = await hass.config_entries.flow.async_configure(result["flow_id"], {},)
 
     assert result["type"] == "create_entry"
     assert result["title"] == TEST_NAME
@@ -392,6 +286,7 @@ async def test_config_flow_ssdp(hass):
         CONF_RECEIVER_ID: TEST_UNIQUE_ID,
     }
 
+
 async def test_config_flow_ssdp_not_denon(hass):
     """Test a failed config flow initialized by ssdp discovery with a not supported manufacturer."""
     result = await hass.config_entries.flow.async_init(
@@ -402,11 +297,12 @@ async def test_config_flow_ssdp_not_denon(hass):
             ssdp.ATTR_UPNP_MODEL_NAME: TEST_MODEL,
             ssdp.ATTR_UPNP_SERIAL: TEST_SERIALNUMBER,
             ssdp.ATTR_SSDP_LOCATION: TEST_SSDP_LOCATION,
-        }
+        },
     )
 
     assert result["type"] == "abort"
     assert result["reason"] == "not_denonavr_manufacturer"
+
 
 async def test_config_flow_ssdp_missing_info(hass):
     """Test a failed config flow initialized by ssdp discovery with missing information."""
@@ -416,7 +312,7 @@ async def test_config_flow_ssdp_missing_info(hass):
         data={
             ssdp.ATTR_UPNP_MANUFACTURER: TEST_MANUFACTURER,
             ssdp.ATTR_SSDP_LOCATION: TEST_SSDP_LOCATION,
-        }
+        },
     )
 
     assert result["type"] == "abort"
