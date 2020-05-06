a0607b584e43094e0f8ff12e269782f9c633ad52
Nicolas <contact@nicolasberthel.fr>
2020-05-06 14:26:05 +0200

    Add new component for becker covers integration using centronic usb stick

---
+++ homeassistant/components/becker/const.py
@@ -0,0 +1,13 @@
+"""Becker Cover Constants."""
+
+DOMAIN = "becker"
+MANUFACTURER = "Becker"
+
+DEVICE_CLASS = "shutter"
+
+CONF_CHANNEL = "channel"
+CONF_UNIT = "unit"
+
+DEFAULT_CONF_USB_STICK_PATH = (
+    "/dev/serial/by-id/usb-BECKER-ANTRIEBE_GmbH_CDC_RS232_v125_Centronic-if00"
+)
