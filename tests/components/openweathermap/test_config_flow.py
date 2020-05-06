f9c3422a5638a9e6770b5bbb3c48d6d0739cf2a4
freekode <efrolov.app@gmail.com>
2020-05-06 16:47:24 +0200

    options test

--- tests/components/openweathermap/test_config_flow.py
+++ tests/components/openweathermap/test_config_flow.py
@@ -18,6 +18,8 @@ from homeassistant.const import (
     CONF_NAME,
 )
 
+from tests.common import MockConfigEntry
+
 CONFIG = {
     CONF_NAME: "openweathermap",
     CONF_API_KEY: "foo",
@@ -30,7 +32,7 @@ CONFIG = {
 
 
 async def test_form(hass):
-    """Test that the form is served with no input."""
+    """Test that the form is served with valid input."""
     mocked_owm = _create_mocked_owm(True)
 
     with patch(
@@ -45,15 +47,46 @@ async def test_form(hass):
         assert result["step_id"] == SOURCE_USER
         assert result["errors"] == {}
 
-        result2 = await hass.config_entries.flow.async_init(
+        result = await hass.config_entries.flow.async_init(
             DOMAIN, context={"source": SOURCE_USER}, data=CONFIG
         )
 
-        assert result2["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-        assert result2["title"] == CONFIG[CONF_NAME]
-        assert result2["data"][CONF_LATITUDE] == CONFIG[CONF_LATITUDE]
-        assert result2["data"][CONF_LONGITUDE] == CONFIG[CONF_LONGITUDE]
-        assert result2["data"][CONF_API_KEY] == CONFIG[CONF_API_KEY]
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result["title"] == CONFIG[CONF_NAME]
+        assert result["data"][CONF_LATITUDE] == CONFIG[CONF_LATITUDE]
+        assert result["data"][CONF_LONGITUDE] == CONFIG[CONF_LONGITUDE]
+        assert result["data"][CONF_API_KEY] == CONFIG[CONF_API_KEY]
+
+
+async def test_options_form(hass):
+    """Test that the options form."""
+
+    config_entry = MockConfigEntry(
+        domain=DOMAIN, unique_id="openweathermap_unique_id", data=CONFIG
+    )
+    config_entry.add_to_hass(hass)
+
+    mocked_owm = _create_mocked_owm(True)
+
+    with patch(
+        "homeassistant.components.openweathermap.config_flow.OWM",
+        return_value=mocked_owm,
+    ):
+        result = await hass.config_entries.options.async_init(config_entry.entry_id)
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["step_id"] == "init"
+
+        result = await hass.config_entries.options.async_configure(
+            result["flow_id"], user_input={CONF_MODE: "daily"}
+        )
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert config_entry.options == {
+            CONF_MODE: "daily",
+            CONF_MONITORED_CONDITIONS: "",
+            CONF_LANGUAGE: DEFAULT_LANGUAGE,
+        }
 
 
 async def test_invalid_api_key(hass):
