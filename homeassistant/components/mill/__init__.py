37d9546ee9a2d9155f61fb128e9e81c6c479c59a
Daniel Høyer Iversen <mail@dahoiv.net>
2020-05-08 07:59:52 +0200

    Apply suggestions from code review
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/mill/__init__.py
+++ homeassistant/components/mill/__init__.py
@@ -3,15 +3,9 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.components.climate import PLATFORM_SCHEMA
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
-from homeassistant.helpers import config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {vol.Required(CONF_USERNAME): cv.string, vol.Required(CONF_PASSWORD): cv.string}
-)
 
 
 async def async_setup(hass, config):
