26497937a6828cc95194413806275d92bcf15fa7
Kevin Fronczak <kfronczak@gmail.com>
2020-05-08 11:04:29 -0400

    Migrate to dual yaml/ui config, add 2FA support

---
+++ homeassistant/components/blink/config_flow.py
@@ -0,0 +1,93 @@
+"""Config flow to configure Blink."""
+import logging
+
+from blinkpy import blinkpy
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import (
+    CONF_PASSWORD,
+    CONF_PIN,
+    CONF_SCAN_INTERVAL,
+    CONF_USERNAME,
+)
+from homeassistant.helpers import config_validation as cv
+
+from .const import DEFAULT_OFFSET, DEFAULT_SCAN_INTERVAL, DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class BlinkConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a Blink config flow."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    def __init__(self):
+        """Initialize the blink flow."""
+        self.blink = None
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initiated by the user."""
+        if self._async_current_entries():
+            return self.async_abort(reason="one_instance_only")
+
+        stored_username = ""
+        stored_password = ""
+        stored_interval = DEFAULT_SCAN_INTERVAL
+
+        if DOMAIN in self.hass.data:
+            stored_username = self.hass.data[DOMAIN].get(CONF_USERNAME)
+            stored_password = self.hass.data[DOMAIN].get(CONF_PASSWORD)
+            stored_interval = self.hass.data[DOMAIN].get(CONF_SCAN_INTERVAL)
+
+        if user_input is not None:
+            await self.async_set_unique_id(user_input[CONF_USERNAME])
+            self.blink = blinkpy.Blink(
+                username=user_input[CONF_USERNAME],
+                password=user_input[CONF_PASSWORD],
+                motion_interval=DEFAULT_OFFSET,
+                legacy_subdomain=False,
+                no_prompt=True,
+                device_id="Home Assistant",
+            )
+            self.blink.refresh_rate = user_input[CONF_SCAN_INTERVAL].total_seconds()
+
+            await self.hass.async_add_executor_job(self._blink.start())
+
+            if not self.blink.key_required:
+                # No key required, we're good
+                return await self.async_create_entry(
+                    title=DOMAIN,
+                    data={CONF_USERNAME: self.blink.login_handler.data["username"]},
+                )
+            else:
+                return await self.async_step_2fa()
+
+            return self.async_show_form(
+                step_id="user",
+                data_schema=vol.Schema(
+                    {
+                        vol.Required(CONF_USERNAME, default=stored_username): cv.string,
+                        vol.Required(CONF_PASSWORD, default=stored_password): cv.string,
+                        vol.Optional(
+                            CONF_SCAN_INTERVAL, default=stored_interval
+                        ): cv.time_period,
+                    }
+                ),
+                errors={},
+            )
+
+    async def async_step_2fa(self, user_input=None):
+        """Handle 2FA step."""
+        if user_input:
+            await self.blink.setup_post_verify()
+            return await self.async_create_entry(
+                title=DOMAIN,
+                data={CONF_USERNAME: self.blink.login_handler.data["username"]},
+            )
+
+        return self.async_show_form(
+            step_id="2FA", data_schema=vol.Schema({CONF_PIN: str})
+        )
---
+++ homeassistant/components/blink/const.py
@@ -0,0 +1,26 @@
+"""Constants for Blink."""
+from datetime import timedelta
+
+DOMAIN = "blink"
+BLINK_CONFIG = "blink"
+
+CONF_CAMERA = "camera"
+CONF_ALARM_CONTROL_PANEL = "alarm_control_panel"
+
+DEFAULT_BRAND = "Blink"
+DEFAULT_ATTRIBUTION = "Data provided by immedia-semi.com"
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=300)
+DEFAULT_OFFSET = 1
+SIGNAL_UPDATE_BLINK = "blink_update"
+
+TYPE_CAMERA_ARMED = "motion_enabled"
+TYPE_MOTION_DETECTED = "motion_detected"
+TYPE_TEMPERATURE = "temperature"
+TYPE_BATTERY = "battery"
+TYPE_WIFI_STRENGTH = "wifi_strength"
+
+SERVICE_REFRESH = "blink_update"
+SERVICE_TRIGGER = "trigger_camera"
+SERVICE_SAVE_VIDEO = "save_video"
+
+PLATFORMS = ("alarm_control_panel", "binary_sensor", "camera", "sensor")
