a890882bb37a9f795dc0b8ce7aff8c65b4494526
zewelor <zewelor@gmail.com>
2020-05-10 21:47:56 +0200

    Comment fix

--- homeassistant/components/yeelight/__init__.py
+++ homeassistant/components/yeelight/__init__.py
@@ -236,7 +236,7 @@ class YeelightDevice:
         """Return configured device model.
 
         Model method returns declared model, and if not, it tries to get it from
-        capabilities call. This way even if model is not defined, we can get real model
+        cached capabilities. This way even if model is not defined, we can get real model
         here. Useful for correct color temp range etc.
         """
         return self._bulb_device.model
