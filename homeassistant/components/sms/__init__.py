37a9739b4206fba109a187f8e2be92bb1e0481c7
Paul Annekov <paul.annekov@gmail.com>
2020-05-11 22:48:18 +0300

    draft

--- homeassistant/components/sms/__init__.py
+++ homeassistant/components/sms/__init__.py
@@ -18,16 +18,58 @@ CONFIG_SCHEMA = vol.Schema(
 
 
 def setup(hass, config):
-    """Configure Gammu state machine."""
+    """Configure Gateway."""
     conf = config[DOMAIN]
     device = conf.get(CONF_DEVICE)
-    gateway = gammu.StateMachine()  # pylint: disable=no-member
-    try:
-        gateway.SetConfig(0, dict(Device=device, Connection="at"))
-        gateway.Init()
-    except gammu.GSMError as exc:  # pylint: disable=no-member
-        _LOGGER.error("Failed to initialize, error %s", exc)
-        return False
-    else:
+    gateway = Gateway(hass, device)
+    result = gateway.init()
+    if result:
         hass.data[DOMAIN] = gateway
         return True
+    else:
+        return False
+
+
+class Gateway:
+    """Wrapper for Gammu State Machine."""
+
+    def __init__(self, hass, device, scan_interval):
+        """Initialize the Gateway object, poll as per scan interval."""
+        self._state_machine = gammu.StateMachine()
+        self._state_machine.SetConfig(0, dict(Device=device, Connection="at"))
+        # TODO: make configurable?
+        self._scan_interval = timedelta(seconds=1)
+        self._hass = hass
+        # TODO: do we need this?
+        hass.bus.listen_once(EVENT_HOMEASSISTANT_START, lambda _: self._update())
+        self._init_regular_updates(hass)
+
+    def init(self):
+        try:
+            self._state_machine.Init()
+        except gammu.GSMError as exc:  # pylint: disable=no-member
+            _LOGGER.error("Failed to initialize, error %s", exc)
+            return False
+        else:
+            return True
+
+    def _init_regular_updates(self, hass):
+        """Schedule regular updates at the top of the clock."""
+        # TODO: what if previous update didn't end?
+        track_time_interval(hass, lambda now: self._update(), self._scan_interval)
+
+    def _update(self):
+        """Get pending incoming sms and publish them to the event bus."""
+        _LOGGER.debug("Fetching new sms")
+
+        while True:
+            sms = self._state_machine.GetNextSMS(0, True)
+            if sms is not None:
+                _LOGGER.debug("Got new sms from %s", sms)
+                self._hass.bus.fire("%s.incoming_sms" % DOMAIN, sms)
+                self._state_machine.DeleteSMS(0, sms[0]['Location'])
+            else:
+                _LOGGER.debug("No new sms")
+                break
+        
+        _LOGGER.info("Fetching completed")
