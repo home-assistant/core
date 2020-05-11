bf5dfeef871dba95abbf5ab619e3cb36f6265782
matsnl <mats.nelissen@gmail.com>
2020-05-11 10:55:00 +0000

    rewrite library

--- homeassistant/components/atag/sensor.py
+++ homeassistant/components/atag/sensor.py
@@ -1,14 +1,31 @@
 """Initialization of ATAG One sensor platform."""
-from homeassistant.const import ATTR_STATE
+from homeassistant.const import (
+    DEVICE_CLASS_PRESSURE,
+    DEVICE_CLASS_TEMPERATURE,
+    PRESSURE_BAR,
+    TEMP_CELSIUS,
+    TEMP_FAHRENHEIT,
+)
 
-from . import DOMAIN, ENTITY_TYPES, SENSOR, AtagEntity
+from . import DOMAIN, AtagEntity
+
+SENSORS = [
+    "Burning Hours",
+    "Outside Temperature",
+    "Flame",
+    "Average Outside Temperature",
+    "Weather Status",
+    "CH Return Temperature",
+    "CH Water Pressure",
+    "CH Water Temperature",
+]
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Initialize sensor platform from config entry."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
     entities = []
-    for sensor in ENTITY_TYPES[SENSOR]:
+    for sensor in SENSORS:
         entities.append(AtagSensor(coordinator, sensor))
     async_add_entities(entities)
 
@@ -16,7 +33,39 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 class AtagSensor(AtagEntity):
     """Representation of a AtagOne Sensor."""
 
+    def __init__(self, coordinator, sensor):
+        """Initialize Atag sensor."""
+        self.coordinator = coordinator
+        self._id = sensor
+        self._name = sensor
+
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self.coordinator.data[self._id][ATTR_STATE]
+        return self.coordinator.data[self._id].state
+
+    @property
+    def icon(self):
+        """Return icon."""
+        return self.coordinator.data[self._id].icon
+
+    @property
+    def device_class(self):
+        """Return deviceclass."""
+        if self.coordinator.data[self._id].sensorclass in [
+            DEVICE_CLASS_PRESSURE,
+            DEVICE_CLASS_TEMPERATURE,
+        ]:
+            return self.coordinator.data[self._id].sensorclass
+        return None
+
+    @property
+    def unit_of_measurement(self):
+        """Return measure."""
+        if self.coordinator.data[self._id].measure in [
+            PRESSURE_BAR,
+            TEMP_CELSIUS,
+            TEMP_FAHRENHEIT,
+        ]:
+            return self.coordinator.data[self._id].measure
+        return None
