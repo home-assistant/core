0f3c6772147ee4d3e4a0db8f13c3583806597393
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-05-12 16:23:53 +0200

    add test for enabling a legacy binary sensor

--- tests/components/zwave_mqtt/test_binary_sensor.py
+++ tests/components/zwave_mqtt/test_binary_sensor.py
@@ -1,5 +1,9 @@
 """Test Z-Wave Sensors."""
-from homeassistant.components.binary_sensor import DEVICE_CLASS_MOTION
+from homeassistant.components.binary_sensor import (
+    DEVICE_CLASS_MOTION,
+    DOMAIN as BINARY_SENSOR_DOMAIN,
+)
+from homeassistant.components.zwave_mqtt.const import DOMAIN
 from homeassistant.const import ATTR_DEVICE_CLASS
 
 from .common import setup_zwave
@@ -36,4 +40,27 @@ async def test_binary_sensor(hass, generic_data, binary_sensor_msg):
     receive_msg(binary_sensor_msg)
     await hass.async_block_till_done()
     state = hass.states.get("binary_sensor.trisensor_home_security_motion_detected")
+    assert state.state == "on"
+
+
+async def test_sensor_enabled(hass, generic_data, binary_sensor_alt_msg):
+    """Test enabling a legacy binary_sensor."""
+
+    registry = await hass.helpers.entity_registry.async_get_registry()
+
+    entry = registry.async_get_or_create(
+        BINARY_SENSOR_DOMAIN,
+        DOMAIN,
+        "1-37-625737744",
+        suggested_object_id="trisensor_sensor_instance_1_sensor",
+        disabled_by=None,
+    )
+    assert entry.disabled is False
+
+    receive_msg = await setup_zwave(hass, fixture=generic_data)
+    receive_msg(binary_sensor_alt_msg)
+    await hass.async_block_till_done()
+
+    state = hass.states.get(entry.entity_id)
+    assert state is not None
     assert state.state == "on"
