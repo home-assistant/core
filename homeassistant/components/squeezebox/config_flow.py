721aec4b3ebdd1579f0a451e3b299a39cb7d5f25
Raj Laud <raj.laud@gmail.com>
2020-05-12 21:40:55 -0500

    Tests for user and edit steps in config flow

--- homeassistant/components/squeezebox/config_flow.py
+++ homeassistant/components/squeezebox/config_flow.py
@@ -32,7 +32,8 @@ TIMEOUT = 5
 
 
 async def validate_input(hass: core.HomeAssistant, data):
-    """Validate the user input allows us to connect.
+    """
+    Validate the user input allows us to connect.
 
     Data has the keys from DATA_SCHEMA with values provided by the user.
     """
@@ -61,7 +62,6 @@ class SqueezeboxConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     def __init__(self):
         """Initialize an instance of the squeezebox config flow."""
-        # a private version of the schema allows us to update suggested values
         self.data_schema = DATA_SCHEMA
         self.data = None
 
@@ -106,14 +106,13 @@ class SqueezeboxConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                         "uuid": server.uuid,
                     }
                     _LOGGER.debug("Discovered server: %s", self.data)
-                return None
 
             discovery_task = self.hass.async_create_task(
                 async_discover(_discovery_callback)
             )
             while not self.data:
                 await asyncio.sleep(1)
-            discovery_task.cancel()  # stop searching for LMS servers
+            discovery_task.cancel()  # stop searching as soon as we find a server
             return self.data
 
         try:
@@ -140,7 +139,7 @@ class SqueezeboxConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="no_server_found")
 
     async def async_step_edit(self, user_input=None):
-        """Edit a discovered or manually inputted configuration."""
+        """Edit a discovered or manually inputted server."""
         errors = {}
         if user_input:
             try:
@@ -180,47 +179,43 @@ class SqueezeboxConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_discovery(self, discovery_info):
         """Handle discovery."""
         _LOGGER.debug("Reached discovery flow with info: %s", discovery_info)
-        try:
-            if "uuid" not in discovery_info:
-                DATA_SCHEMA(discovery_info)
+        if "uuid" not in discovery_info:
+            DATA_SCHEMA(discovery_info)
+            try:
                 info = await validate_input(self.hass, discovery_info)
                 discovery_info.update(info)
-            if "uuid" in discovery_info:
-                try:
-                    await self.async_set_unique_id(discovery_info["uuid"])
-                    self._abort_if_unique_id_configured()
-                except data_entry_flow.AbortFlow:
-                    # ignore already discovered servers
-                    return self.async_abort(reason="already_configured")
+            except CannotConnect:
+                return self.async_abort(reason="cannot_connect")
+            except InvalidAuth:
+                return self.async_abort(reason="invalid_auth")
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                return self.async_abort(reason="unknown")
+        if "uuid" in discovery_info:
+            try:
+                await self.async_set_unique_id(discovery_info["uuid"])
+                self._abort_if_unique_id_configured()
+            except data_entry_flow.AbortFlow:
+                # ignore already discovered servers
+                return self.async_abort(reason="already_configured")
 
-                # update with suggested values from discovery
-                self.data_schema = vol.Schema(
-                    {
-                        vol.Required(
-                            CONF_HOST,
-                            description={
-                                "suggested_value": discovery_info.get(CONF_HOST)
-                            },
-                        ): str,
-                        vol.Required(
-                            CONF_PORT,
-                            default=DEFAULT_PORT,
-                            description={
-                                "suggested_value": discovery_info.get(CONF_PORT)
-                            },
-                        ): int,
-                        vol.Optional(CONF_USERNAME): str,
-                        vol.Optional(CONF_PASSWORD): str,
-                    }
-                )
-                return await self.async_step_edit()
-        except CannotConnect:
-            return self.async_abort(reason="cannot_connect")
-        except InvalidAuth:
-            return self.async_abort(reason="invalid_auth")
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception")
-            return self.async_abort(reason="unknown")
+            # update schema with suggested values from discovery
+            self.data_schema = vol.Schema(
+                {
+                    vol.Required(
+                        CONF_HOST,
+                        description={"suggested_value": discovery_info.get(CONF_HOST)},
+                    ): str,
+                    vol.Required(
+                        CONF_PORT,
+                        default=DEFAULT_PORT,
+                        description={"suggested_value": discovery_info.get(CONF_PORT)},
+                    ): int,
+                    vol.Optional(CONF_USERNAME): str,
+                    vol.Optional(CONF_PASSWORD): str,
+                }
+            )
+            return await self.async_step_edit()
 
 
 class CannotConnect(exceptions.HomeAssistantError):
--- tests/components/squeezebox/test_config_flow.py
+++ tests/components/squeezebox/test_config_flow.py
@@ -4,83 +4,103 @@ from asynctest import patch
 from homeassistant import config_entries, setup
 from homeassistant.components.squeezebox.config_flow import CannotConnect, InvalidAuth
 from homeassistant.components.squeezebox.const import DOMAIN
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_PORT, CONF_USERNAME
 
 
 async def test_form(hass):
-    """Test we get the form."""
+    """Test user-initiated flow."""
     await setup.async_setup_component(hass, "persistent_notification", {})
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     assert result["type"] == "form"
-    assert result["errors"] == {}
+    assert result["step_id"] == "user"
 
     with patch(
         "homeassistant.components.squeezebox.config_flow.validate_input",
-        return_value={"uuid": "test-uuid", "ip": "1.1.1.1"},
+        return_value={
+            "uuid": "test-uuid",
+            CONF_HOST: "1.1.1.1",
+            CONF_PORT: 9000,
+            "ip": "1.1.1.1",
+        },
     ), patch(
         "homeassistant.components.squeezebox.async_setup", return_value=True
     ) as mock_setup, patch(
         "homeassistant.components.squeezebox.async_setup_entry", return_value=True,
     ) as mock_setup_entry:
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {"host": "1.1.1.1", "port": 9000, "username": "", "password": ""},
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {CONF_HOST: "1.1.1.1"}
         )
-    assert result2["type"] == "create_entry"
-    assert result2["title"] == "1.1.1.1"
-    assert result2["data"] == {
-        "host": "1.1.1.1",
-        "port": 9000,
-        "username": "",
-        "password": "",
-    }
-    await hass.async_block_till_done()
-    assert len(mock_setup.mock_calls) == 1
-    assert len(mock_setup_entry.mock_calls) == 1
+        assert result["type"] == "form"
+        assert result["step_id"] == "edit"
+
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_HOST: "1.1.1.1",
+                CONF_PORT: 9000,
+                CONF_USERNAME: "",
+                CONF_PASSWORD: "",
+            },
+        )
+        assert result["type"] == "create_entry"
+        assert result["title"] == "1.1.1.1"
+        assert result["data"] == {
+            CONF_HOST: "1.1.1.1",
+            CONF_PORT: 9000,
+            CONF_USERNAME: "",
+            CONF_PASSWORD: "",
+        }
+
+        await hass.async_block_till_done()
+        assert len(mock_setup.mock_calls) == 1
+        assert len(mock_setup_entry.mock_calls) == 1
 
 
 async def test_form_invalid_auth(hass):
     """Test we handle invalid auth."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": "edit"}
     )
 
     with patch(
         "homeassistant.components.squeezebox.config_flow.validate_input",
         side_effect=InvalidAuth,
     ):
-        result2 = await hass.config_entries.flow.async_configure(
+        result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
+                CONF_HOST: "1.1.1.1",
+                CONF_PORT: 9000,
+                CONF_USERNAME: "test-username",
+                CONF_PASSWORD: "test-password",
             },
         )
 
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_auth"}
+    assert result["type"] == "form"
+    assert result["errors"] == {"base": "invalid_auth"}
 
 
 async def test_form_cannot_connect(hass):
     """Test we handle cannot connect error."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": "edit"}
     )
 
     with patch(
         "homeassistant.components.squeezebox.config_flow.validate_input",
         side_effect=CannotConnect,
     ):
-        result2 = await hass.config_entries.flow.async_configure(
+        result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
+                CONF_HOST: "1.1.1.1",
+                CONF_PORT: 9000,
+                CONF_USERNAME: "test-username",
+                CONF_PASSWORD: "test-password",
             },
         )
 
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "cannot_connect"}
+    assert result["type"] == "form"
+    assert result["errors"] == {"base": "cannot_connect"}
