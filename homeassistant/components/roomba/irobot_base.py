1bc7a12a9f99e94ee6c72c4f9133d1a118084d03
Xiaonan Shen <s@sxn.dev>
2020-05-14 23:18:05 -0700

    Fix lint

--- homeassistant/components/roomba/irobot_base.py
+++ homeassistant/components/roomba/irobot_base.py
@@ -121,7 +121,7 @@ class IRobotEntity(Entity):
         """Register callback function."""
         self.vacuum.register_on_message_callback(self.on_message)
 
-    def new_state_filter(self, new_state):
+    def new_state_filter(self, new_state):  # pylint: disable=no-self-use
         """Filter out wifi state messages."""
         return len(new_state) > 1 or "signal" not in new_state
 
