23676c3e8a59cd3aad59d14c9009f35b3e34e5db
G-Two <g_two_github@fastmail.com>
2020-05-18 02:09:05 -0400

    Fixed config flow tests.

--- tests/components/subaru/test_config_flow.py
+++ tests/components/subaru/test_config_flow.py
@@ -1,8 +1,9 @@
 """Test the Subaru config flow."""
 from unittest.mock import patch
 
+from subarulink.exceptions import SubaruException
+
 from homeassistant import config_entries, setup
-from homeassistant.components.subaru.config_flow import CannotConnect, InvalidAuth
 from homeassistant.components.subaru.const import DOMAIN
 from homeassistant.const import CONF_DEVICE_ID, CONF_PASSWORD, CONF_PIN, CONF_USERNAME
 
@@ -12,7 +13,6 @@ TEST_USERNAME = "test@fake.com"
 TEST_TITLE = TEST_USERNAME
 TEST_PASSWORD = "test-password"
 TEST_PIN = "1234"
-TEST_DEVICE_ID = "Test Device ID"
 
 
 async def test_form(hass):
@@ -25,16 +25,8 @@ async def test_form(hass):
     assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.subaru.config_flow.validate_input",
-        return_value=mock_coro(
-            {
-                "title": TEST_TITLE,
-                CONF_USERNAME: TEST_USERNAME,
-                CONF_PASSWORD: TEST_PASSWORD,
-                CONF_PIN: TEST_PIN,
-                CONF_DEVICE_ID: "test",
-            }
-        ),
+        "homeassistant.components.subaru.config_flow.SubaruAPI.connect",
+        return_value=mock_coro(True),
     ), patch(
         "homeassistant.components.subaru.async_setup", return_value=mock_coro(True)
     ) as mock_setup, patch(
@@ -52,13 +44,11 @@ async def test_form(hass):
 
     assert result2["type"] == "create_entry"
     assert result2["title"] == TEST_TITLE
-    assert result2["data"] == {
-        "title": TEST_TITLE,
-        CONF_USERNAME: TEST_USERNAME,
-        CONF_PASSWORD: TEST_PASSWORD,
-        CONF_PIN: TEST_PIN,
-        CONF_DEVICE_ID: "test",
-    }
+    assert result2["data"][CONF_USERNAME] == TEST_USERNAME
+    assert result2["data"][CONF_PASSWORD] == TEST_PASSWORD
+    assert result2["data"][CONF_PIN] == TEST_PIN
+    assert result2["data"][CONF_DEVICE_ID] > 1000
+
     await hass.async_block_till_done()
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
@@ -71,8 +61,8 @@ async def test_form_invalid_auth(hass):
     )
 
     with patch(
-        "homeassistant.components.subaru.config_flow.validate_input",
-        side_effect=InvalidAuth,
+        "homeassistant.components.subaru.config_flow.SubaruAPI.connect",
+        side_effect=SubaruException("invalidAccount"),
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -94,8 +84,8 @@ async def test_form_cannot_connect(hass):
     )
 
     with patch(
-        "homeassistant.components.subaru.config_flow.validate_input",
-        side_effect=CannotConnect,
+        "homeassistant.components.subaru.config_flow.SubaruAPI.connect",
+        side_effect=SubaruException(None),
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
