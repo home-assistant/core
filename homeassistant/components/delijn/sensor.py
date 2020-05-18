a05df48e13135a91266c5d8f99cb67600d1d9c9e
Emil Vanherp <emil@vanherp.me>
2020-05-18 21:43:28 +0200

    fix Delijn sensor naming
    fixes #32290

--- homeassistant/components/delijn/sensor.py
+++ homeassistant/components/delijn/sensor.py
@@ -2,10 +2,15 @@
 import logging
 
 from pydelijn.api import Passages
+from pydelijn.common import HttpException
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import ATTR_ATTRIBUTION, DEVICE_CLASS_TIMESTAMP
+from homeassistant.const import (
+    ATTR_ATTRIBUTION,
+    DEVICE_CLASS_TIMESTAMP,
+    STATE_UNAVAILABLE,
+)
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
@@ -37,22 +42,23 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Create the sensor."""
     api_key = config[CONF_API_KEY]
-    name = DEFAULT_NAME
 
     session = async_get_clientsession(hass)
 
     sensors = []
     for nextpassage in config[CONF_NEXT_DEPARTURE]:
-        stop_id = nextpassage[CONF_STOP_ID]
-        number_of_departures = nextpassage[CONF_NUMBER_OF_DEPARTURES]
-        line = Passages(
-            hass.loop, stop_id, number_of_departures, api_key, session, True
+        sensors.append(
+            DeLijnPublicTransportSensor(
+                Passages(
+                    hass.loop,
+                    nextpassage[CONF_STOP_ID],
+                    nextpassage[CONF_NUMBER_OF_DEPARTURES],
+                    api_key,
+                    session,
+                    True,
+                )
+            )
         )
-        await line.get_passages()
-        if line.passages is None:
-            _LOGGER.warning("No data received from De Lijn")
-            return
-        sensors.append(DeLijnPublicTransportSensor(line, name))
 
     async_add_entities(sensors, True)
 
@@ -60,44 +66,42 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 class DeLijnPublicTransportSensor(Entity):
     """Representation of a Ruter sensor."""
 
-    def __init__(self, line, name):
+    def __init__(self, line):
         """Initialize the sensor."""
         self.line = line
         self._attributes = {ATTR_ATTRIBUTION: ATTRIBUTION}
-        self._name = name
+        self._name = None
         self._state = None
-        self._available = False
 
     async def async_update(self):
         """Get the latest data from the De Lijn API."""
-        await self.line.get_passages()
-        if self.line.passages is None:
-            _LOGGER.warning("No data received from De Lijn")
-            return
         try:
+            _LOGGER.debug("updating delijn sensor")
+            await self.line.get_passages()
+
+            self._name = await self.line.get_stopname()
+            _LOGGER.debug("stop name: %s", self._name)
             first = self.line.passages[0]
             if first["due_at_realtime"] is not None:
                 first_passage = first["due_at_realtime"]
             else:
                 first_passage = first["due_at_schedule"]
             self._state = first_passage
-            self._name = first["stopname"]
-            self._attributes["stopname"] = first["stopname"]
+            _LOGGER.debug("state: %s", self._state)
+            self._attributes["is_realtime"] = first["is_realtime"]
             self._attributes["line_number_public"] = first["line_number_public"]
             self._attributes["line_transport_type"] = first["line_transport_type"]
             self._attributes["final_destination"] = first["final_destination"]
             self._attributes["due_at_schedule"] = first["due_at_schedule"]
             self._attributes["due_at_realtime"] = first["due_at_realtime"]
             self._attributes["next_passages"] = self.line.passages
-            self._available = True
-        except (KeyError, IndexError) as error:
-            _LOGGER.debug("Error getting data from De Lijn: %s", error)
-            self._available = False
+        except HttpException:
+            self._state = STATE_UNAVAILABLE
+            _LOGGER.error("Delijn http error")
 
-    @property
-    def available(self):
-        """Return True if entity is available."""
-        return self._available
+        except (KeyError, IndexError):
+            _LOGGER.error("Invalid data received from De Lijn")
+            self._state = STATE_UNAVAILABLE
 
     @property
     def device_class(self):
