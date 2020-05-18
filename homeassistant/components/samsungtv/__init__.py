6b1b832d49cdb76bfeb3014963ec0a79c00f741f
escoand <escoand@users.noreply.github.com>
2020-05-18 22:55:52 +0200

    Update __init__.py

--- homeassistant/components/samsungtv/__init__.py
+++ homeassistant/components/samsungtv/__init__.py
@@ -19,27 +19,24 @@ def ensure_unique_hosts(value):
 
 
 CONFIG_SCHEMA = vol.Schema(
-    vol.All(
-        cv.deprecated(DOMAIN),
-        {
-            DOMAIN: vol.All(
-                cv.ensure_list,
-                [
-                    cv.deprecated(CONF_PORT),
-                    vol.Schema(
-                        {
-                            vol.Required(CONF_HOST): cv.string,
-                            vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-                            vol.Optional(CONF_PORT): cv.port,
-                            vol.Optional(CONF_ON_ACTION): cv.SCRIPT_SCHEMA,
-                        }
-                    ),
-                ],
-                ensure_unique_hosts,
-            )
-        },
-        extra=vol.ALLOW_EXTRA,
-    )
+    {
+        DOMAIN: vol.All(
+            cv.ensure_list,
+            [
+                cv.deprecated(CONF_PORT),
+                vol.Schema(
+                    {
+                        vol.Required(CONF_HOST): cv.string,
+                        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+                        vol.Optional(CONF_PORT): cv.port,
+                        vol.Optional(CONF_ON_ACTION): cv.SCRIPT_SCHEMA,
+                    }
+                ),
+            ],
+            ensure_unique_hosts,
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
 )
 
 
