b953c62fb2f481e08b0c7e8aee158b803bd6a4ce
thomkaufmann <thom.kaufmann@gmail.com>
2020-05-19 18:10:05 +0200

    Update requirements_all; Run isort

--- homeassistant/components/nuki/lock.py
+++ homeassistant/components/nuki/lock.py
@@ -7,11 +7,7 @@ from pynuki import NukiBridge
 from requests.exceptions import RequestException
 import voluptuous as vol
 
-from homeassistant.components.lock import (
-    PLATFORM_SCHEMA,
-    SUPPORT_OPEN,
-    LockEntity,
-)
+from homeassistant.components.lock import PLATFORM_SCHEMA, SUPPORT_OPEN, LockEntity
 from homeassistant.const import ATTR_ENTITY_ID, CONF_HOST, CONF_PORT, CONF_TOKEN
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.service import extract_entity_ids
