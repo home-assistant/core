78464cdd21ec1c5a2e033e1377a1aa23a643e5c2
Justin Wong <46082645+uvjustin@users.noreply.github.com>
2020-05-19 12:06:13 +0000

    Add zeroconf tests for invalid versions

--- tests/components/forked_daapd/test_config_flow.py
+++ tests/components/forked_daapd/test_config_flow.py
@@ -129,7 +129,30 @@ async def test_config_flow_no_websocket(hass, config_entry):
 
 async def test_config_flow_zeroconf_invalid(hass):
     """Test that an invalid zeroconf entry doesn't work."""
+    # test with no discovery properties
     discovery_info = {"host": "127.0.0.1", "port": 23}
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_ZEROCONF}, data=discovery_info
+    )  # doesn't create the entry, tries to show form but gets abort
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "not_forked_daapd"
+    # test with forked-daapd version < 27
+    discovery_info = {
+        "host": "127.0.0.1",
+        "port": 23,
+        "properties": {"mtd-version": "26.3", "Machine Name": "forked-daapd"},
+    }
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_ZEROCONF}, data=discovery_info
+    )  # doesn't create the entry, tries to show form but gets abort
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "not_forked_daapd"
+    # test with verbose mtd-version from Firefly
+    discovery_info = {
+        "host": "127.0.0.1",
+        "port": 23,
+        "properties": {"mtd-version": "0.2.4.1", "Machine Name": "firefly"},
+    }
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": SOURCE_ZEROCONF}, data=discovery_info
     )  # doesn't create the entry, tries to show form but gets abort
