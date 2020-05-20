756486c53bcd340c5778f6e7fa33c9ef7e3338bd
Tom Scholten <tom@sue.nl>
2020-05-20 20:42:28 +0200

    Show only when multiple

--- homeassistant/components/plugwise/climate.py
+++ homeassistant/components/plugwise/climate.py
@@ -150,7 +150,8 @@ class PwThermostat(SmileGateway, ClimateEntity):
         """Return the device specific state attributes."""
         attributes = {}
         if self._schema_names:
-            attributes["available_schemas"] = self._schema_names
+            if len(self._schema_names) > 1:
+                attributes["available_schemas"] = self._schema_names
         if self._selected_schema:
             attributes["selected_schema"] = self._selected_schema
         return attributes
