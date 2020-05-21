home-assistant/core

commit c132adbbdd570bd2dafe67d08062de078ad32e9e

Clean up ssl usage

homeassistant/components/zabbix/__init__.py
@@ -40,12 +40,9 @@ def setup(hass, config):
     """Set up the Zabbix component."""
 
     conf = config[DOMAIN]
-    if conf[CONF_SSL]:
-        schema = "https"
-    else:
-        schema = "http"
+    protocol = "https" if config[CONF_SSL] else "http"
 
-    url = urljoin("{}://{}".format(schema, conf[CONF_HOST]), conf[CONF_PATH])
+    url = urljoin(f"{protocol}://{conf[CONF_HOST]}", conf[CONF_PATH])
     username = conf.get(CONF_USERNAME)
     password = conf.get(CONF_PASSWORD)
 

homeassistant/components/homematic/__init__.py
@@ -222,7 +222,7 @@ def setup(hass, config):
             "password": rconfig.get(CONF_PASSWORD),
             "callbackip": rconfig.get(CONF_CALLBACK_IP),
             "callbackport": rconfig.get(CONF_CALLBACK_PORT),
-            "ssl": rconfig.get(CONF_SSL),
+            "ssl": rconfig[CONF_SSL],
             "verify_ssl": rconfig.get(CONF_VERIFY_SSL),
             "connect": True,
         }

homeassistant/components/octoprint/__init__.py
@@ -144,9 +144,9 @@ def setup(hass, config):
 
     for printer in config[DOMAIN]:
         name = printer[CONF_NAME]
-        ssl = "s" if printer[CONF_SSL] else ""
+        protocol = "https" if printer[CONF_SSL] else "http"
         base_url = (
-            f"http{ssl}://{printer[CONF_HOST]}:{printer[CONF_PORT]}"
+            f"{protocol}://{printer[CONF_HOST]}:{printer[CONF_PORT]}"
             f"{printer[CONF_PATH]}api/"
         )
         api_key = printer[CONF_API_KEY]

homeassistant/components/plex/__init__.py
@@ -100,10 +100,10 @@ def _async_setup_plex(hass, config):
     if MP_DOMAIN in server_config:
         hass.data.setdefault(PLEX_MEDIA_PLAYER_OPTIONS, server_config.pop(MP_DOMAIN))
     if CONF_HOST in server_config:
-        prefix = "https" if server_config.pop(CONF_SSL) else "http"
+        protocol = "https" if server_config.pop(CONF_SSL) else "http"
         server_config[
             CONF_URL
-        ] = f"{prefix}://{server_config.pop(CONF_HOST)}:{server_config.pop(CONF_PORT)}"
+        ] = f"{protocol}://{server_config.pop(CONF_HOST)}:{server_config.pop(CONF_PORT)}"
     hass.async_create_task(
         hass.config_entries.flow.async_init(
             PLEX_DOMAIN,

homeassistant/components/sabnzbd/__init__.py
@@ -134,7 +134,7 @@ async def async_setup(hass, config):
 
     conf = config.get(DOMAIN)
     if conf is not None:
-        use_ssl = conf.get(CONF_SSL)
+        use_ssl = conf[CONF_SSL]
         name = conf.get(CONF_NAME)
         api_key = conf.get(CONF_API_KEY)
         await async_configure_sabnzbd(hass, conf, use_ssl, name, api_key)

homeassistant/components/splunk/__init__.py
@@ -70,7 +70,7 @@ def setup(hass, config):
     host = conf.get(CONF_HOST)
     port = conf.get(CONF_PORT)
     token = conf.get(CONF_TOKEN)
-    use_ssl = conf.get(CONF_SSL)
+    use_ssl = conf[CONF_SSL]
     verify_ssl = conf.get(CONF_VERIFY_SSL)
     name = conf.get(CONF_NAME)
     entity_filter = conf[CONF_FILTER]

homeassistant/components/zoneminder/__init__.py
@@ -58,13 +58,10 @@ def setup(hass, config):
     success = True
 
     for conf in config[DOMAIN]:
-        if conf[CONF_SSL]:
-            schema = "https"
-        else:
-            schema = "http"
+        protocol = "https" if config[CONF_SSL] else "http"
 
         host_name = conf[CONF_HOST]
-        server_origin = f"{schema}://{host_name}"
+        server_origin = f"{protocol}://{host_name}"
         zm_client = ZoneMinder(
             server_origin,
             conf.get(CONF_USERNAME),

homeassistant/components/cisco_mobility_express/device_tracker.py
@@ -43,7 +43,7 @@ def get_scanner(hass, config):
         config[CONF_HOST],
         config[CONF_USERNAME],
         config[CONF_PASSWORD],
-        config.get(CONF_SSL),
+        config[CONF_SSL],
         config.get(CONF_VERIFY_SSL),
     )
     if not controller.is_logged_in():

commit feed095e5bb4be0d31991530378fe48fcafbbf9c

Fix typo in conf[CONF_SSL]
Caused by c132adb

homeassistant/components/zabbix/__init__.py
@@ -40,7 +40,7 @@ def setup(hass, config):
     """Set up the Zabbix component."""
 
     conf = config[DOMAIN]
-    protocol = "https" if config[CONF_SSL] else "http"
+    protocol = "https" if conf[CONF_SSL] else "http"
 
     url = urljoin(f"{protocol}://{conf[CONF_HOST]}", conf[CONF_PATH])
     username = conf.get(CONF_USERNAME)