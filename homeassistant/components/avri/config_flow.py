898be16dd18160484feeaccf6bc61d18835f6e80
Tim van Cann <timvancann@gmail.com>
2020-05-22 20:26:50 +0200

    Fix import order

--- homeassistant/components/avri/config_flow.py
+++ homeassistant/components/avri/config_flow.py
@@ -5,8 +5,6 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_ID
 
-from .const import DOMAIN  # pylint:disable=unused-import
-
 from .const import (
     CONF_COUNTRY_CODE,
     CONF_HOUSE_NUMBER,
@@ -14,6 +12,7 @@ from .const import (
     CONF_ZIP_CODE,
     DEFAULT_COUNTRY_CODE,
 )
+from .const import DOMAIN  # pylint:disable=unused-import
 
 DATA_SCHEMA = vol.Schema(
     {
