8753fda50835ea4f0e001afb2210a40945120706
Jason Hunter <hunterjm@gmail.com>
2020-05-24 17:23:53 +0000

    add comments around why weird things are being done

--- homeassistant/components/onvif/base.py
+++ homeassistant/components/onvif/base.py
@@ -24,12 +24,20 @@ class ONVIFBaseEntity(Entity):
     def device_info(self):
         """Return a device description for device registry."""
         device_info = {
-            "identifiers": {(DOMAIN, self.device.info.serial_number)},
             "manufacturer": self.device.info.manufacturer,
             "model": self.device.info.model,
             "name": self.device.name,
             "sw_version": self.device.info.fw_version,
         }
+
+        # MAC address is not always available, and given the number
+        # of non-conformant ONVIF devices we have historically supported,
+        # we can not guarantee serial number either.  Due to this, we have
+        # adopted an either/or approach in the config entry setup, and can
+        # guarantee that one or the other will be populated.
+        # See: https://github.com/home-assistant/core/issues/35883
+        if self.device.info.serial_number:
+            device_info["identifiers"] = {(DOMAIN, self.device.info.serial_number)}
 
         if self.device.info.mac:
             device_info["connections"] = {
