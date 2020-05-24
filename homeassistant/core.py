84461e9ecf9eb9f4bf2ee2abd575a61c18371eda
J. Nick Koston <nick@koston.org>
2020-05-24 23:40:18 +0000

    adj

--- homeassistant/core.py
+++ homeassistant/core.py
@@ -408,6 +408,7 @@ class HomeAssistant:
         await asyncio.sleep(0)
 
         while self._pending_tasks:
+            _LOGGER.debug("Pending Tasks: %s", self._pending_tasks)
             pending = [task for task in self._pending_tasks if not task.done()]
             self._pending_tasks.clear()
             if pending:
