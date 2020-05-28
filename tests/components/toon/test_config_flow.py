ebbe5e768f7c651477b9c77ee3a058bf1fb80fda
Quentin POLLET <polletquentin74@me.com>
2020-05-28 23:42:13 +0200

    Fix pylint

--- tests/components/toon/test_config_flow.py
+++ tests/components/toon/test_config_flow.py
@@ -10,14 +10,13 @@ from toonapilib.toonapilibexceptions import (
 
 from homeassistant import data_entry_flow
 from homeassistant.components.toon import config_flow
-from homeassistant.components.toon.const import (
+from homeassistant.components.toon.const import CONF_DISPLAY, CONF_TENANT, DOMAIN
+from homeassistant.const import (
     CONF_CLIENT_ID,
     CONF_CLIENT_SECRET,
-    CONF_DISPLAY,
-    CONF_TENANT,
-    DOMAIN,
+    CONF_PASSWORD,
+    CONF_USERNAME,
 )
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.setup import async_setup_component
 
 from tests.async_mock import patch
