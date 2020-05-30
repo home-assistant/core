a7012f1908cca39e1a827cbd188d56c8a0a6232d
Fredrik Erlandsson <fredrik.e@gmail.com>
2020-05-30 07:11:19 +0200

    Update tests and use CONF_HOST as hostname

--- homeassistant/components/daikin/config_flow.py
+++ homeassistant/components/daikin/config_flow.py
@@ -128,7 +128,7 @@ class FlowHandler(config_entries.ConfigFlow):
     async def async_step_zeroconf(self, discovery_info):
         """Prepare configuration for a discovered Daikin device."""
         _LOGGER.debug("Zeroconf discovery_info: %s", discovery_info)
-        await self.async_set_unique_id(discovery_info.get(KEY_HOSTNAME))
+        await self.async_set_unique_id(discovery_info[KEY_HOSTNAME])
         self._abort_if_unique_id_configured()
-        self.host = discovery_info.get(KEY_HOSTNAME)
+        self.host = discovery_info[CONF_HOST]
         return await self.async_step_user()
