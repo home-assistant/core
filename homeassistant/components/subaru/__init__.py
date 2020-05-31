6b587fa8f8f7672059f688b55b33aaf7ec1ae23d
G-Two <g_two_github@fastmail.com>
2020-05-31 15:26:41 -0400

    Removes DOMAIN if all subaru entries are removed.

--- homeassistant/components/subaru/__init__.py
+++ homeassistant/components/subaru/__init__.py
@@ -98,6 +98,8 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     )
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
+    if len(hass.data[DOMAIN]) == 0:
+        hass.data.pop(DOMAIN)
 
     return unload_ok
 
