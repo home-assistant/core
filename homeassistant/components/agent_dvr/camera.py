38bd8f6479b5b39bbd59787f23909b7c0b6709e1
Timmo <contact@timmo.xyz>
2020-06-03 20:46:54 +0100

    Use updated package

--- homeassistant/components/agent_dvr/camera.py
+++ homeassistant/components/agent_dvr/camera.py
@@ -74,8 +74,8 @@ class AgentCamera(MjpegCamera):
 
         device_info = {
             CONF_NAME: device.name,
-            CONF_MJPEG_URL: f"{self.server_url}{device.mjpeg_image_url}&size={device.raw_result['data']['width']}x{device.raw_result['data']['height']}",
-            CONF_STILL_IMAGE_URL: f"{self.server_url}{device.still_image_url}&size={device.raw_result['data']['width']}x{device.raw_result['data']['height']}",
+            CONF_MJPEG_URL: f"{self.server_url}{device.mjpeg_image_url}&size={device.width}x{device.height}",
+            CONF_STILL_IMAGE_URL: f"{self.server_url}{device.still_image_url}&size={device.width}x{device.height}",
         }
         self.device = device
         self._removed = False
