168d92c279a743714f54c1817ad9d88a42b21c02
Paul Woelfel <paul.woelfel@cloudpilots.com>
2020-06-04 20:17:10 +0000

    Update remote_rpi_gpio, Add tests

--- homeassistant/components/remote_rpi_gpio/__init__.py
+++ homeassistant/components/remote_rpi_gpio/__init__.py
@@ -1,9 +1,6 @@
 """Support for controlling GPIO pins of a Raspberry Pi."""
 import logging
 
-from gpiozero import LED, Button
-from gpiozero.pins.pigpio import PiGPIOFactory
-
 _LOGGER = logging.getLogger(__name__)
 
 CONF_BOUNCETIME = "bouncetime"
@@ -14,50 +11,14 @@ DEFAULT_BOUNCETIME = 50
 DEFAULT_INVERT_LOGIC = False
 DEFAULT_PULL_MODE = "UP"
 
+# The domain of your component. Should be equal to the name of your component.
 DOMAIN = "remote_rpi_gpio"
+
+# List of integration names (string) your integration depends upon.
+DEPENDENCIES = []
 
 
 def setup(hass, config):
-    """Set up the Raspberry Pi Remote GPIO component."""
+    """Set up remote_rpi_gpio."""
+    _LOGGER.info("loading %s completed.", DOMAIN)
     return True
-
-
-def setup_output(address, port, invert_logic):
-    """Set up a GPIO as output."""
-
-    try:
-        return LED(port, active_high=invert_logic, pin_factory=PiGPIOFactory(address))
-    except (ValueError, IndexError, KeyError):
-        return None
-
-
-def setup_input(address, port, pull_mode, bouncetime):
-    """Set up a GPIO as input."""
-
-    if pull_mode == "UP":
-        pull_gpio_up = True
-    elif pull_mode == "DOWN":
-        pull_gpio_up = False
-
-    try:
-        return Button(
-            port,
-            pull_up=pull_gpio_up,
-            bounce_time=bouncetime,
-            pin_factory=PiGPIOFactory(address),
-        )
-    except (ValueError, IndexError, KeyError, OSError):
-        return None
-
-
-def write_output(switch, value):
-    """Write a value to a GPIO."""
-    if value == 1:
-        switch.on()
-    if value == 0:
-        switch.off()
-
-
-def read_input(button):
-    """Read a value from a GPIO."""
-    return button.is_pressed
