966989437fc389f1a0ede8d53db4f6c7f35b4cf3
Bram Devooght <b.devooght@smappee.com>
2020-06-05 13:40:41 +0200

    Restrict initial integration to sensor entities

--- homeassistant/components/smappee_official/const.py
+++ homeassistant/components/smappee_official/const.py
@@ -7,6 +7,6 @@ DEFAULT_NAME = "Smappee Official"
 DOMAIN = "smappee_official"
 DATA_CLIENT = "smappee_data"
 
-SMAPPEE_COMPONENTS = ["sensor", "binary_sensor", "switch"]
+SMAPPEE_COMPONENTS = ["sensor"]
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
