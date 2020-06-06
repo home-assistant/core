3dde8062575322166a8e53145b2997617dff5d1d
Quentin POLLET <polletquentin74@me.com>
2020-06-06 16:18:45 +0200

    WIP: sensors

--- homeassistant/components/meteo_france/sensor.py
+++ homeassistant/components/meteo_france/sensor.py
@@ -1,73 +1,35 @@
 """Support for Meteo-France raining forecast sensor."""
 import logging
 
-from meteofrance.client import meteofranceClient
-from vigilancemeteo import DepartmentWeatherAlert, VigilanceMeteoFranceProxy
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_ATTRIBUTION
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.typing import HomeAssistantType
 
+from . import MeteoFranceDataUpdateCoordinator
 from .const import (
     ATTRIBUTION,
-    CONF_CITY,
     DOMAIN,
-    SENSOR_TYPE_CLASS,
-    SENSOR_TYPE_ICON,
-    SENSOR_TYPE_NAME,
-    SENSOR_TYPE_UNIT,
+    ENTITY_API_DATA_PATH,
+    ENTITY_CLASS,
+    ENTITY_ENABLE,
+    ENTITY_ICON,
+    ENTITY_NAME,
+    ENTITY_UNIT,
     SENSOR_TYPES,
 )
 
 _LOGGER = logging.getLogger(__name__)
-
-STATE_ATTR_FORECAST = "1h rain forecast"
-STATE_ATTR_BULLETIN_TIME = "Bulletin date"
 
 
 async def async_setup_entry(
     hass: HomeAssistantType, entry: ConfigEntry, async_add_entities
 ) -> None:
     """Set up the Meteo-France sensor platform."""
-    city = entry.data[CONF_CITY]
-    client = hass.data[DOMAIN][city]
-    weather_alert_client = hass.data[DOMAIN]["weather_alert_client"]
-
-    alert_watcher = None
-    datas = client.update()
-    # Check if a department code is available for this city.
-    if "dept" in datas:
-        try:
-            # If yes create the watcher DepartmentWeatherAlert object.
-            alert_watcher = await hass.async_add_executor_job(
-                DepartmentWeatherAlert, datas["dept"], weather_alert_client
-            )
-            _LOGGER.info(
-                "Weather alert watcher added for %s in department %s",
-                city,
-                datas["dept"],
-            )
-        except ValueError as exp:
-            _LOGGER.error(
-                "Unexpected error when creating the weather alert sensor for %s in department %s: %s",
-                city,
-                datas["dept"],
-                exp,
-            )
-    else:
-        _LOGGER.warning(
-            "No 'dept' key found for '%s'. So weather alert information won't be available",
-            city,
-        )
-        # Exit and don't create the sensor if no department code available.
-        return
+    coordinator = hass.data[DOMAIN][entry.entry_id]
 
     async_add_entities(
-        [
-            MeteoFranceSensor(sensor_type, client, alert_watcher)
-            for sensor_type in SENSOR_TYPES
-        ],
+        [MeteoFranceSensor(sensor_type, coordinator) for sensor_type in SENSOR_TYPES],
         True,
     )
 
@@ -75,50 +37,64 @@ async def async_setup_entry(
 class MeteoFranceSensor(Entity):
     """Representation of a Meteo-France sensor."""
 
-    def __init__(
-        self,
-        sensor_type: str,
-        client: meteofranceClient,
-        alert_watcher: VigilanceMeteoFranceProxy,
-    ):
+    def __init__(self, sensor_type: str, coordinator: MeteoFranceDataUpdateCoordinator):
         """Initialize the Meteo-France sensor."""
         self._type = sensor_type
-        self._client = client
-        self._alert_watcher = alert_watcher
-        self._state = None
-        self._data = {}
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return f"{self._data['name']} {SENSOR_TYPES[self._type][SENSOR_TYPE_NAME]}"
+        self.coordinator = coordinator
+        city_name = self.coordinator.data.position["name"]
+        self._name = f"{city_name} {SENSOR_TYPES[self._type][ENTITY_NAME]}"
 
     @property
     def unique_id(self):
-        """Return the unique id of the sensor."""
-        return self.name
+        """Return the unique id."""
+        return self._name
+
+    @property
+    def name(self):
+        """Return the name."""
+        return self._name
 
     @property
     def state(self):
-        """Return the state of the sensor."""
-        return self._state
+        """Return the state."""
+        path = SENSOR_TYPES[self._type][ENTITY_API_DATA_PATH].split(":")
+        data = getattr(self.coordinator.data, path[0])
+        if path[0] == "forecast":
+            data = data[2]
+        else:
+            data = data[0]
+        # _LOGGER.info("-"*80)
+        # _LOGGER.warning(self._type)
+        # _LOGGER.error(path)
+        # _LOGGER.error(data)
+
+        # # if len(path) == 3:
+        # #     _LOGGER.warning(data[path[1]][path[2]])
+        # #     _LOGGER.warning("OK-2")
+        # # _LOGGER.warning(data[path[1]])
+        # # _LOGGER.warning("OK")
+
+        # _LOGGER.info("-"*80)
+        if len(path) == 3:
+            return data[path[1]][path[2]]
+        return data[path[1]]
 
     @property
     def device_state_attributes(self):
-        """Return the state attributes of the sensor."""
+        """Return the state attributes."""
         # Attributes for next_rain sensor.
-        if self._type == "next_rain" and "rain_forecast" in self._data:
+        if self._type == "next_rain":
             return {
-                **{STATE_ATTR_FORECAST: self._data["rain_forecast"]},
-                **self._data["next_rain_intervals"],
+                # **{STATE_ATTR_FORECAST: self._data["rain_forecast"]},
+                # **self._data["next_rain_intervals"],
                 **{ATTR_ATTRIBUTION: ATTRIBUTION},
             }
 
         # Attributes for weather_alert sensor.
-        if self._type == "weather_alert" and self._alert_watcher is not None:
+        if self._type == "weather_alert":
             return {
-                **{STATE_ATTR_BULLETIN_TIME: self._alert_watcher.bulletin_date},
-                **self._alert_watcher.alerts_list,
+                # **{STATE_ATTR_BULLETIN_TIME: self._alert_watcher.bulletin_date},
+                # **self._alert_watcher.alerts_list,
                 ATTR_ATTRIBUTION: ATTRIBUTION,
             }
 
@@ -128,41 +104,31 @@ class MeteoFranceSensor(Entity):
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement."""
-        return SENSOR_TYPES[self._type][SENSOR_TYPE_UNIT]
+        return SENSOR_TYPES[self._type][ENTITY_UNIT]
 
     @property
     def icon(self):
         """Return the icon."""
-        return SENSOR_TYPES[self._type][SENSOR_TYPE_ICON]
+        return SENSOR_TYPES[self._type][ENTITY_ICON]
 
     @property
     def device_class(self):
-        """Return the device class of the sensor."""
-        return SENSOR_TYPES[self._type][SENSOR_TYPE_CLASS]
+        """Return the device class."""
+        return SENSOR_TYPES[self._type][ENTITY_CLASS]
 
-    def update(self):
-        """Fetch new state data for the sensor."""
-        try:
-            self._client.update()
-            self._data = self._client.update()
+    @property
+    def entity_registry_enabled_default(self) -> bool:
+        """Return if the entity should be enabled when first added to the entity registry."""
+        return SENSOR_TYPES[self._type][ENTITY_ENABLE]
 
-            if self._type == "weather_alert":
-                if self._alert_watcher is not None:
-                    self._alert_watcher.update_department_status()
-                    self._state = self._alert_watcher.department_color
-                    _LOGGER.debug(
-                        "weather alert watcher for %s updated. Proxy have the status: %s",
-                        self._data["name"],
-                        self._alert_watcher.proxy.status,
-                    )
-                else:
-                    _LOGGER.warning(
-                        "No weather alert data for location %s", self._data["name"]
-                    )
-            else:
-                self._state = self._data[self._type]
-        except KeyError:
-            _LOGGER.error(
-                "No condition %s for location %s", self._type, self._data["name"]
-            )
-            self._state = None
+    @property
+    def should_poll(self) -> bool:
+        """No polling needed."""
+        return False
+
+    async def async_update(self):
+        """Only used by the generic entity update service."""
+        if not self.enabled:
+            return
+
+        await self.coordinator.async_request_refresh()
