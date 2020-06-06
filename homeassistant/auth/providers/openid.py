88d3274d7d81bc5e09c785caf3456d3313dc2468
Joakim Plate <elupus@ecce.se>
2020-06-06 12:19:10 +0200

    Use preferred_username then given_name then name, then email

--- homeassistant/auth/providers/openid.py
+++ homeassistant/auth/providers/openid.py
@@ -216,15 +216,21 @@ class OpenIdAuthProvider(AuthProvider):
 
         Will be used to populate info when creating a new user.
         """
-        email = credentials.data["email"]
-        if "name" in credentials.data:
+        if "preferred_username" in credentials.data:
+            name = credentials.data["preferred_username"]
+        elif "given_name" in credentials.data:
+            name = credentials.data["given_name"]
+        elif "name" in credentials.data:
             name = credentials.data["name"]
-        else:
+        elif "email" in credentials.data:
+            email = credentials.data["email"]
             match = re.match(r"[^@]+", email)
             if match:
                 name = str(match.group(0))
             else:
                 name = str(email)
+        else:
+            name = credentials.data["sub"]
 
         return UserMeta(name=name, is_active=True)
 
