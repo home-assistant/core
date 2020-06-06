7a429f54b5f0c67f8f2f8d88891233b3f0c16843
G-Two <g_two_github@fastmail.com>
2020-06-06 15:13:50 -0400

    Uses hass.config.units for sensor conversions
    Updated to subarulink==0.2.2

--- homeassistant/components/subaru/sensor.py
+++ homeassistant/components/subaru/sensor.py
@@ -5,16 +5,23 @@ from homeassistant.const import (
     LENGTH_KILOMETERS,
     LENGTH_METERS,
     LENGTH_MILES,
-    TEMP_CELSIUS,
-    TEMP_FAHRENHEIT,
+    TIME_MINUTES,
+    UNIT_PERCENTAGE,
+    VOLT,
+    VOLUME_GALLONS,
+    VOLUME_LITERS,
 )
 from homeassistant.helpers.entity import Entity
-from homeassistant.util.distance import convert
+from homeassistant.util.distance import convert as dist_convert
 from homeassistant.util.temperature import celsius_to_fahrenheit
+from homeassistant.util.unit_system import IMPERIAL_SYSTEM
+from homeassistant.util.volume import convert as vol_convert
 
 from . import DOMAIN as SUBARU_DOMAIN, SubaruDevice
 
 _LOGGER = logging.getLogger(__name__)
+L_PER_GAL = vol_convert(1, VOLUME_GALLONS, VOLUME_LITERS)
+KM_PER_MI = dist_convert(1, LENGTH_MILES, LENGTH_KILOMETERS)
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -22,17 +29,18 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     controller = hass.data[SUBARU_DOMAIN][config_entry.entry_id]["controller"]
     entities = []
     for device in hass.data[SUBARU_DOMAIN][config_entry.entry_id]["devices"]["sensor"]:
-        entities.append(SubaruSensor(device, controller, config_entry))
+        entities.append(SubaruSensor(device, controller, config_entry, hass))
     async_add_entities(entities, True)
 
 
 class SubaruSensor(SubaruDevice, Entity):
     """Representation of Subaru sensors."""
 
-    def __init__(self, subaru_device, controller, config_entry):
+    def __init__(self, subaru_device, controller, config_entry, hass):
         """Initialize of the sensor."""
         self.current_value = None
-        self.units = None
+        self.hass = hass
+        self._unit_of_measurement = None
         super().__init__(subaru_device, controller, config_entry)
 
     @property
@@ -43,56 +51,83 @@ class SubaruSensor(SubaruDevice, Entity):
     @property
     def unit_of_measurement(self):
         """Return the unit_of_measurement of the device."""
-        return self.units
+        return self._unit_of_measurement
 
     async def async_update(self):
         """Update the state from the sensor."""
         _LOGGER.debug("Updating sensor: %s", self._name)
         await super().async_update()
-        units = self.subaru_device.measurement
+        self.units = self.hass.config.units
+        self.current_value = self.subaru_device.get_value()
 
-        if self.subaru_device.type == "External Temp":
-            self.current_value = self.subaru_device.get_outside_temp()
-            if units == "F":
-                self.units = TEMP_FAHRENHEIT
-                self.current_value = round(celsius_to_fahrenheit(self.current_value), 2)
-            else:
-                self.units = TEMP_CELSIUS
-        elif self.subaru_device.type == "Odometer":
-            self.current_value = self.subaru_device.get_value()
-            if units == "LENGTH_MILES":
-                self.units = LENGTH_MILES
+        if self.current_value is None:
+            pass
+
+        elif self.subaru_device.type == "External Temp":  # C
+            if self.units == IMPERIAL_SYSTEM:
                 self.current_value = round(
-                    convert(self.current_value, LENGTH_METERS, LENGTH_MILES), 2
+                    celsius_to_fahrenheit(float(self.current_value)), 1
+                )
+            self._unit_of_measurement = self.units.temperature_unit
+
+        elif self.subaru_device.type == "Odometer":  # m
+            if self.units == IMPERIAL_SYSTEM:
+                self.current_value = round(
+                    dist_convert(int(self.current_value), LENGTH_METERS, LENGTH_MILES),
+                    1,
                 )
             else:
-                self.units = LENGTH_KILOMETERS
                 self.current_value = round(
-                    convert(self.current_value, LENGTH_METERS, LENGTH_KILOMETERS), 2
+                    dist_convert(
+                        int(self.current_value), LENGTH_METERS, LENGTH_KILOMETERS
+                    ),
+                    1,
                 )
-        elif self.subaru_device.type == "EV Range":
-            self.current_value = self.subaru_device.get_value()
-            if units == "LENGTH_MILES":
-                self.units = LENGTH_MILES
+            self._unit_of_measurement = self.units.length_unit
+
+        elif self.subaru_device.type == "EV Range":  # mi
+            if self.units == IMPERIAL_SYSTEM:
+                pass
             else:
-                self.units = LENGTH_KILOMETERS
                 self.current_value = round(
-                    convert(self.current_value, LENGTH_MILES, LENGTH_KILOMETERS), 2
+                    dist_convert(
+                        int(self.current_value), LENGTH_MILES, LENGTH_KILOMETERS
+                    ),
+                    1,
                 )
-        elif self.subaru_device.type == "Range":
-            self.current_value = self.subaru_device.get_value()
-            if units == "LENGTH_MILES":
-                self.units = LENGTH_MILES
+            self._unit_of_measurement = self.units.length_unit
+
+        elif self.subaru_device.type == "Range":  # km
+            if self.units == IMPERIAL_SYSTEM:
+                self.current_value = round(
+                    dist_convert(
+                        int(self.current_value), LENGTH_KILOMETERS, LENGTH_MILES
+                    ),
+                    0,
+                )
+            self._unit_of_measurement = self.units.length_unit
+
+        elif self.subaru_device.type == "EV Charge Rate":  # min
+            self._unit_of_measurement = TIME_MINUTES
+
+        elif self.subaru_device.type == "12V Battery Voltage":  # V
+            self._unit_of_measurement = VOLT
+
+        elif self.subaru_device.type == "Avg Fuel Consumption":  # L/10km
+            if self.units == IMPERIAL_SYSTEM:
+                self._unit_of_measurement = "mi/gal"
+                self.current_value = (1000.0 * L_PER_GAL) / (
+                    KM_PER_MI * float(self.current_value)
+                )
+                self.current_value = round(self.current_value, 1)
             else:
-                self.units = LENGTH_KILOMETERS
-                self.current_value = round(
-                    convert(self.current_value, LENGTH_MILES, LENGTH_KILOMETERS), 2
-                )
-        elif self.subaru_device.type == "EV Charge Rate":
-            self.units = units
-            self._attributes = {
-                "time_left": self.subaru_device.time_left,
-            }
+                self._unit_of_measurement = "L/100km"
+                self.current_value = float(self.current_value) / 10.0
+
+        elif self.subaru_device.type == "EV Battery Level":  # %
+            self._unit_of_measurement = UNIT_PERCENTAGE
+
         else:
-            self.current_value = self.subaru_device.get_value()
-            self.units = units
+            _LOGGER.warning(
+                "Unsupported Subaru Sensor Type %s" % self.subaru_device.type
+            )
