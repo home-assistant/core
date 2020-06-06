c62a145e08053c2a84353ef8c5d4cc56f2b27734
starkillerOG <starkiller.og@gmail.com>
2020-06-06 18:30:37 +0200

    remove unnecesary identifiers

--- homeassistant/components/denonavr/__init__.py
+++ homeassistant/components/denonavr/__init__.py
@@ -71,7 +71,6 @@ async def async_setup_entry(
     device_registry.async_get_or_create(
         config_entry_id=entry.entry_id,
         connections={(dr.CONNECTION_NETWORK_MAC, entry.data[CONF_MAC])},
-        identifiers={(DOMAIN, entry.unique_id)},
         manufacturer=entry.data[CONF_MANUFACTURER],
         name=entry.title,
         model=f"{entry.data[CONF_MODEL]}-{entry.data[CONF_TYPE]}",
