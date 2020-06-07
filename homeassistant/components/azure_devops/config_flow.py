bd1edf08ae6ff259566ae344db1ea59741b7bf8d
Timmo <contact@timmo.xyz>
2020-06-07 23:20:26 +0100

    Move checks into helper

--- homeassistant/components/azure_devops/config_flow.py
+++ homeassistant/components/azure_devops/config_flow.py
@@ -28,7 +28,6 @@ class AzureDevOpsFlowHandler(ConfigFlow, domain=DOMAIN):
         self._organization = None
         self._project = None
         self._pat = None
-        self._reauth = False
 
     async def _show_setup_form(self, errors=None):
         """Show the setup form to the user."""
@@ -55,6 +54,26 @@ class AzureDevOpsFlowHandler(ConfigFlow, domain=DOMAIN):
             errors=errors or {},
         )
 
+    async def _check_setup(self):
+        errors = {}
+
+        client = DevOpsClient()
+
+        try:
+            if self._pat is not None:
+                await client.authorize(self._pat, self._organization)
+                if not client.authorized:
+                    errors["base"] = "authorization_error"
+                    return errors
+            project_info = await client.get_project(self._organization, self._project)
+            if project_info is None:
+                errors["base"] = "authorization_error"
+                return errors
+        except aiohttp.ClientError:
+            errors["base"] = "connection_error"
+            return errors
+        return None
+
     async def async_step_user(self, user_input=None):
         """Handle a flow initiated by the user."""
         if user_input is None:
@@ -64,6 +83,12 @@ class AzureDevOpsFlowHandler(ConfigFlow, domain=DOMAIN):
         self._project = user_input[CONF_PROJECT]
         self._pat = user_input.get(CONF_PAT)
 
+        await self.async_set_unique_id(f"{self._organization}_{self._project}")
+        self._abort_if_unique_id_configured()
+
+        errors = await self._check_setup()
+        if errors is not None:
+            return await self._show_setup_form(errors)
         return await self.async_step_confirm()
 
     async def async_step_reauth(self, user_input=None):
@@ -75,7 +100,6 @@ class AzureDevOpsFlowHandler(ConfigFlow, domain=DOMAIN):
             self._organization = user_input[CONF_ORG]
             self._project = user_input[CONF_PROJECT]
         self._pat = user_input[CONF_PAT]
-        self._reauth = True
 
         # pylint: disable=no-member
         self.context["title_placeholders"] = {
@@ -84,38 +108,13 @@ class AzureDevOpsFlowHandler(ConfigFlow, domain=DOMAIN):
 
         await self.async_set_unique_id(f"{self._organization}_{self._project}")
 
+        errors = await self._check_setup()
+        if errors is not None:
+            return await self._show_reauth_form(errors)
         return await self.async_step_confirm()
 
     async def async_step_confirm(self):
         """Handle final configuration step."""
-        if not self._reauth:
-            await self.async_set_unique_id(f"{self._organization}_{self._project}")
-            self._abort_if_unique_id_configured()
-
-        errors = {}
-
-        client = DevOpsClient()
-
-        try:
-            if self._pat is not None:
-                await client.authorize(self._pat, self._organization)
-                if not client.authorized:
-                    errors["base"] = "authorization_error"
-                    if self._reauth:
-                        return await self._show_reauth_form(errors)
-                    return await self._show_setup_form(errors)
-            project_info = await client.get_project(self._organization, self._project)
-            if project_info is None:
-                errors["base"] = "authorization_error"
-                if self._reauth:
-                    return await self._show_reauth_form(errors)
-                return await self._show_setup_form(errors)
-        except aiohttp.ClientError:
-            errors["base"] = "connection_error"
-            if self._reauth:
-                return await self._show_reauth_form(errors)
-            return await self._show_setup_form(errors)
-
         return self.async_create_entry(
             title=f"{self._organization}/{self._project}",
             data={
