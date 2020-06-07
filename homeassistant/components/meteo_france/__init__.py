83c15dd9af999af66c336bbf5940aeda6321b5c9
Quentin POLLET <polletquentin74@me.com>
2020-06-07 02:40:10 +0200

    WIP: coordinator next_rain + alert

--- homeassistant/components/meteo_france/__init__.py
+++ homeassistant/components/meteo_france/__init__.py
@@ -3,7 +3,6 @@ import asyncio
 from datetime import timedelta
 import logging
 
-import async_timeout
 from meteofrance.auth import AuthMeteofrance
 from meteofrance.client import MeteofranceClient
 import voluptuous as vol
@@ -13,9 +12,16 @@ from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
 from homeassistant.exceptions import ConfigEntryNotReady
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.typing import ConfigType, HomeAssistantType
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from .const import CONF_CITY, DOMAIN, PLATFORMS
+from .const import (
+    CONF_CITY,
+    COORDINATOR_ALERT,
+    COORDINATOR_FORECAST,
+    COORDINATOR_RAIN,
+    DOMAIN,
+    PLATFORMS,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -52,12 +58,71 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
     latitude = entry.data[CONF_LATITUDE]
     longitude = entry.data[CONF_LONGITUDE]
 
-    coordinator = MeteoFranceDataUpdateCoordinator(hass, latitude, longitude)
-    await coordinator.async_refresh()
-    if not coordinator.last_update_success:
+    # coordinator = MeteoFranceDataUpdateCoordinator(hass, latitude, longitude)
+
+    auth = AuthMeteofrance()
+    client = MeteofranceClient(auth)
+
+    async def _async_update_data_forecast_forecast():
+        """Fetch data from API endpoint."""
+        return await hass.async_add_job(client.get_forecast, latitude, longitude)
+
+    async def _async_update_data_rain():
+        """Fetch data from API endpoint."""
+        return await hass.async_add_job(client.get_rain, latitude, longitude)
+
+    async def _async_update_data_alert():
+        """Fetch data from API endpoint."""
+        return await hass.async_add_job(
+            client.get_warning_current_phenomenoms, department
+        )
+
+    coordinator_forecast = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name=f"Météo-France forecast for city {entry.title}",
+        update_method=_async_update_data_forecast_forecast,
+        update_interval=SCAN_INTERVAL,
+    )
+
+    coordinator_rain = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name=f"Météo-France rain for city {entry.title}",
+        update_method=_async_update_data_rain,
+        update_interval=SCAN_INTERVAL,
+    )
+
+    # Fetch initial data so we have data when entities subscribe
+    await coordinator_forecast.async_refresh()
+    await coordinator_rain.async_refresh()
+
+    if (
+        not coordinator_forecast.last_update_success
+        or not coordinator_rain.last_update_success
+    ):
         raise ConfigEntryNotReady
 
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    department = coordinator_forecast.data.position["dept"]
+    if department:
+        coordinator_alert = DataUpdateCoordinator(
+            hass,
+            _LOGGER,
+            name=f"Météo-France alert for department {department}",
+            update_method=_async_update_data_alert,
+            update_interval=SCAN_INTERVAL,
+        )
+
+        await coordinator_alert.async_refresh()
+
+        if not coordinator_alert.last_update_success:
+            raise ConfigEntryNotReady
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        COORDINATOR_FORECAST: coordinator_forecast,
+        COORDINATOR_RAIN: coordinator_rain,
+        COORDINATOR_ALERT: coordinator_alert,
+    }
 
     for platform in PLATFORMS:
         hass.async_create_task(
@@ -83,25 +148,25 @@ async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry):
     return unload_ok
 
 
-class MeteoFranceDataUpdateCoordinator(DataUpdateCoordinator):
-    """Define an object to hold Meteo-France data."""
+# class MeteoFranceDataUpdateCoordinator(DataUpdateCoordinator):
+#     """Define an object to hold Meteo-France data."""
 
-    def __init__(self, hass, latitude, longitude):
-        """Initialize."""
-        self.latitude = latitude
-        self.longitude = longitude
+#     def __init__(self, hass, latitude, longitude):
+#         """Initialize."""
+#         self.latitude = latitude
+#         self.longitude = longitude
 
-        auth = AuthMeteofrance()
-        self.client = MeteofranceClient(auth)
+#         auth = AuthMeteofrance()
+#         self.client = MeteofranceClient(auth)
 
-        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=SCAN_INTERVAL)
+#         super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=SCAN_INTERVAL)
 
-    async def _async_update_data(self):
-        """Update data via library."""
-        with async_timeout.timeout(20):
-            try:
-                return await self.hass.async_add_executor_job(
-                    self.client.get_forecast, self.latitude, self.longitude
-                )
-            except Exception as exp:  # pylint: disable=broad-except
-                raise UpdateFailed(exp)
+#     async def _async_update_data(self):
+#         """Update data via library."""
+#         with async_timeout.timeout(20):
+#             try:
+#                 return await self.hass.async_add_executor_job(
+#                     self.client.get_forecast, self.latitude, self.longitude
+#                 )
+#             except Exception as exp:  # pylint: disable=broad-except
+#                 raise UpdateFailed(exp)
