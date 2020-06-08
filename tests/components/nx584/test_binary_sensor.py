a93cf27371884ed6d7c80b67e14a7c7048af4200
Mike Reiling <45905227+mreiling@users.noreply.github.com>
2020-06-08 17:29:35 +0000

    Changed order of test

--- tests/components/nx584/test_binary_sensor.py
+++ tests/components/nx584/test_binary_sensor.py
@@ -134,7 +134,7 @@ class TestNX584ZoneSensor(unittest.TestCase):
         assert "foo" == sensor.name
         assert not sensor.should_poll
         assert sensor.is_on
-        assert 1 == sensor.device_state_attributes["zone_number"]
+        assert sensor.device_state_attributes["zone_number"] == 1
 
         zone["state"] = False
         assert not sensor.is_on
