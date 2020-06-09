61aa19ee8b1e2510b9cdcda4414d3cbc50993771
Markus Bong <Markus.Bong@devolo.de>
2020-06-09 12:48:27 +0200

    fix R1719 in line 73:15

--- homeassistant/components/devolo_home_control/cover.py
+++ homeassistant/components/devolo_home_control/cover.py
@@ -55,6 +55,7 @@ class DevoloCoverDeviceEntity(DevoloDeviceEntity, CoverEntity):
         )
 
         self._position = self._multi_level_switch_property.value
+        print()
 
     @property
     def current_cover_position(self):
