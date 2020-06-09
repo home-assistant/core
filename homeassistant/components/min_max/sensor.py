4cba1d594de529cc533b90ecd459028e85a3b9aa
knudsvik <t@knudsvik.com>
2020-06-09 17:29:52 +0000

    adds median

--- homeassistant/components/min_max/sensor.py
+++ homeassistant/components/min_max/sensor.py
@@ -1,4 +1,4 @@
-"""Support for displaying the minimal and the maximal value."""
+"""Support for displaying minimal, maximal, mean or median values."""
 import logging
 
 import voluptuous as vol
@@ -24,6 +24,8 @@ ATTR_MAX_VALUE = "max_value"
 ATTR_MAX_ENTITY_ID = "max_entity_id"
 ATTR_COUNT_SENSORS = "count_sensors"
 ATTR_MEAN = "mean"
+ATTR_MEDIAN = "median"
+ATTR_MEDIAN_ENTITY_ID = "median_entity_id"
 ATTR_LAST = "last"
 ATTR_LAST_ENTITY_ID = "last_entity_id"
 
@@ -32,6 +34,8 @@ ATTR_TO_PROPERTY = [
     ATTR_MAX_VALUE,
     ATTR_MAX_ENTITY_ID,
     ATTR_MEAN,
+    ATTR_MEDIAN,
+    ATTR_MEDIAN_ENTITY_ID,
     ATTR_MIN_VALUE,
     ATTR_MIN_ENTITY_ID,
     ATTR_LAST,
@@ -47,6 +51,7 @@ SENSOR_TYPES = {
     ATTR_MIN_VALUE: "min",
     ATTR_MAX_VALUE: "max",
     ATTR_MEAN: "mean",
+    ATTR_MEDIAN: "median",
     ATTR_LAST: "last",
 }
 
@@ -110,6 +115,26 @@ def calc_mean(sensor_values, round_digits):
     return round(sensor_value_sum / count, round_digits)
 
 
+def calc_median(sensor_values, round_digits):
+    """Calculate median value, honoring unknown states."""
+    count = 0
+    sensor_value_list = []
+    for _, sensor_value in sensor_values:
+        if sensor_value != STATE_UNKNOWN:
+            count += 1
+            sensor_value_list.append(sensor_value)
+    if count == 0:
+        return None
+    sensor_value_list.sort()
+    if count % 2 == 0:
+        median1 = sensor_value_list[count // 2]
+        median2 = sensor_value_list[count // 2 - 1]
+        median = (median1 + median2) / 2
+    else:
+        median = sensor_value_list[count // 2]
+    return round(median, round_digits)
+
+
 class MinMaxSensor(Entity):
     """Representation of a min/max sensor."""
 
@@ -126,7 +151,7 @@ class MinMaxSensor(Entity):
             self._name = f"{next(v for k, v in SENSOR_TYPES.items() if self._sensor_type == v)} sensor".capitalize()
         self._unit_of_measurement = None
         self._unit_of_measurement_mismatch = False
-        self.min_value = self.max_value = self.mean = self.last = None
+        self.min_value = self.max_value = self.mean = self.last = self.median = None
         self.min_entity_id = self.max_entity_id = self.last_entity_id = None
         self.count_sensors = len(self._entity_ids)
         self.states = {}
@@ -219,3 +244,4 @@ class MinMaxSensor(Entity):
         self.min_entity_id, self.min_value = calc_min(sensor_values)
         self.max_entity_id, self.max_value = calc_max(sensor_values)
         self.mean = calc_mean(sensor_values, self._round_digits)
+        self.median = calc_median(sensor_values, self._round_digits)
