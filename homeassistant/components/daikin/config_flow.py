8dafe48aa7b3f592025880d41fdeb6aac1910319
Fredrik Erlandsson <fredrik.e@gmail.com>
2020-06-12 10:32:27 +0200

    Use mac from device

--- homeassistant/components/daikin/config_flow.py
+++ homeassistant/components/daikin/config_flow.py
@@ -5,8 +5,8 @@ from uuid import uuid4
 
 from aiohttp import ClientError, web_exceptions
 from async_timeout import timeout
-from getmac import get_mac_address
 from pydaikin.daikin_base import Appliance
+from pydaikin.discovery import Discovery
 import voluptuous as vol
 
 from homeassistant import config_entries
@@ -129,7 +129,8 @@ class FlowHandler(config_entries.ConfigFlow):
     async def async_step_zeroconf(self, discovery_info):
         """Prepare configuration for a discovered Daikin device."""
         _LOGGER.debug("Zeroconf discovery_info: %s", discovery_info)
-        await self.async_set_unique_id(get_mac_address(ip=discovery_info[CONF_HOST]))
+        devices = Discovery.poll(discovery_info[CONF_HOST])
+        await self.async_set_unique_id(next(iter(devices.values()))[KEY_MAC])
         self._abort_if_unique_id_configured()
         self.host = discovery_info[CONF_HOST]
         return await self.async_step_user()
