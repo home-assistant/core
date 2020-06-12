ca111003c227a13c2357c1f16f650928ba1c9a3b
Bram Devooght <b.devooght@smappee.com>
2020-06-12 11:25:29 +0200

    Review strings

--- homeassistant/components/smappee_official/config_flow.py
+++ homeassistant/components/smappee_official/config_flow.py
@@ -25,6 +25,6 @@ class SmappeeFlowHandler(
     async def async_step_user(self, user_input=None):
         """Handle a flow start."""
         if self.hass.config_entries.async_entries(DOMAIN):
-            return self.async_abort(reason="already_setup")
+            return self.async_abort(reason="single_instance_allowed")
 
         return await super().async_step_user(user_input)
