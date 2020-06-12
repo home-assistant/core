23ff543dc27fb2e20ac24adf9d1bb94cbe36a2d1
Bram Devooght <b.devooght@smappee.com>
2020-06-12 11:49:48 +0200

    add async_update

--- homeassistant/components/smappee_official/switch.py
+++ homeassistant/components/smappee_official/switch.py
@@ -176,6 +176,9 @@ class SmappeeActuator(SwitchEntity):
         }
 
     async def async_update(self):
+        """Get the latest data from Smappee and update the state."""
+        await self._smappee_base.async_update()
+
         """Update state value for this switch."""
         new_state = self._service_location.actuators.get(self._actuator_id).state
         if new_state != self._state:
