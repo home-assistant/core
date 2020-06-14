27f6310775fe6b011c60c256680fbe68f2d097a0
Jonas Thuresson <thuresson.j@gmail.com>
2020-06-14 18:22:56 +0200

    Added clean segment service to xiaomi_miio vaccum

--- homeassistant/components/xiaomi_miio/vacuum.py
+++ homeassistant/components/xiaomi_miio/vacuum.py
@@ -382,7 +382,6 @@ class MiroboVacuum(StateVacuumEntity):
 
     async def async_send_command(self, command, params=None, **kwargs):
         """Send raw command."""
-        _LOGGER.debug()
         await self._try_command(
             "Unable to send command to the vacuum: %s",
             self._vacuum.raw_command,
