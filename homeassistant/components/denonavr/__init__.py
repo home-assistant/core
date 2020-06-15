654872a5f19063c2d51f31175347cf67084afda0
starkillerOG <starkiller.og@gmail.com>
2020-06-15 19:21:22 +0200

    Properly remove zone2 and zone3

--- homeassistant/components/denonavr/__init__.py
+++ homeassistant/components/denonavr/__init__.py
@@ -1,10 +1,16 @@
 """The denonavr component."""
+import logging
+
 import voluptuous as vol
 
 from homeassistant import config_entries, core
 from homeassistant.const import ATTR_ENTITY_ID, CONF_HOST, CONF_MAC
 from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers import config_validation as cv, device_registry as dr
+from homeassistant.helpers import (
+    config_validation as cv,
+    device_registry as dr,
+    entity_registry as er,
+)
 from homeassistant.helpers.dispatcher import dispatcher_send
 
 from .config_flow import (
@@ -26,6 +32,8 @@ CONF_RECEIVER = "receiver"
 UNDO_UPDATE_LISTENER = "undo_update_listener"
 SERVICE_GET_COMMAND = "get_command"
 ATTR_COMMAND = "command"
+
+_LOGGER = logging.getLogger(__name__)
 
 CALL_SCHEMA = vol.Schema({vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids})
 
@@ -98,21 +106,36 @@ async def async_setup_entry(
 
 
 async def async_unload_entry(
-    hass: core.HomeAssistant, entry: config_entries.ConfigEntry
+    hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry
 ):
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_forward_entry_unload(
-        entry, "media_player"
+        config_entry, "media_player"
     )
 
-    hass.data[DOMAIN][entry.entry_id][UNDO_UPDATE_LISTENER]()
+    hass.data[DOMAIN][config_entry.entry_id][UNDO_UPDATE_LISTENER]()
+
+    # Remove zone2 and zone3 entities if needed
+    entity_registry = await er.async_get_registry(hass)
+    entries = er.async_entries_for_config_entry(entity_registry, config_entry.entry_id)
+    zone2_id = f"{config_entry.unique_id}-Zone2"
+    zone3_id = f"{config_entry.unique_id}-Zone3"
+    for entry in entries:
+        if entry.unique_id == zone2_id and not config_entry.options.get(CONF_ZONE2):
+            entity_registry.async_remove(entry.entity_id)
+            _LOGGER.info("removing zone2 from DenonAvr")
+        if entry.unique_id == zone3_id and not config_entry.options.get(CONF_ZONE3):
+            entity_registry.async_remove(entry.entity_id)
+            _LOGGER.info("removing zone3 from DenonAvr")
 
     if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+        hass.data[DOMAIN].pop(config_entry.entry_id)
 
     return unload_ok
 
 
-async def update_listener(hass: core.HomeAssistant, entry: config_entries.ConfigEntry):
+async def update_listener(
+    hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry
+):
     """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
+    await hass.config_entries.async_reload(config_entry.entry_id)
