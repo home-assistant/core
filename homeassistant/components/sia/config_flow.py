78b10c42f9d22a62bd3ca0e4ed71bfa23739dc96
E.A. van Valkenburg <edvan@microsoft.com>
2020-06-17 07:22:33 +0000

    refactored config and fix coverage of test

--- homeassistant/components/sia/config_flow.py
+++ homeassistant/components/sia/config_flow.py
@@ -1,8 +1,5 @@
 """Config flow for sia integration."""
-import logging
-
-from pysiaalarm.sia_account import SIAAccount
-from pysiaalarm.sia_errors import (
+from pysiaalarm import (
     InvalidAccountFormatError,
     InvalidAccountLengthError,
     InvalidKeyFormatError,
@@ -13,7 +10,8 @@ import voluptuous as vol
 from homeassistant import config_entries, exceptions
 from homeassistant.const import CONF_NAME, CONF_PORT
 
-from .const import (  # pylint: disable:unused-import
+from .const import (
+    ABORT_ALREADY_CONFIGURED,
     CONF_ACCOUNT,
     CONF_ACCOUNTS,
     CONF_ENCRYPTION_KEY,
@@ -22,10 +20,6 @@ from .const import (  # pylint: disable:unused-import
     DEFAULT_NAME,
     DOMAIN,
 )
-
-_LOGGER = logging.getLogger(__name__)
-
-CONF_ADDITIONAL_ACCOUNTS = "additional_account"
 
 HUB_SCHEMA = vol.Schema(
     {
@@ -51,8 +45,7 @@ ACCOUNT_SCHEMA = vol.Schema(
 
 def validate_input(data: dict) -> bool:
     """Validate the input by the user."""
-    SIAAccount(data[CONF_ACCOUNT], data.get(CONF_ENCRYPTION_KEY, None))
-
+    SIAAccount(data[CONF_ACCOUNT], data.get(CONF_ENCRYPTION_KEY))
     try:
         ping = int(data[CONF_PING_INTERVAL])
         assert 1 <= ping <= 1440
@@ -64,95 +57,78 @@ def validate_input(data: dict) -> bool:
     except AssertionError:
         raise InvalidZones
 
-    return True
-
 
 class SIAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for sia."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
-    data = None
-
-    async def async_step_add_account(self, user_input: dict = None):
-        """Handle the additional accounts steps."""
-        errors = {}
-        if user_input is not None:
-            try:
-                if validate_input(user_input):
-                    add_data = user_input.copy()
-                    add_data.pop(CONF_ADDITIONAL_ACCOUNTS)
-                    self.data[CONF_ACCOUNTS].append(add_data)
-                    if user_input[CONF_ADDITIONAL_ACCOUNTS]:
-                        return await self.async_step_add_account()
-            except InvalidKeyFormatError:
-                errors["base"] = "invalid_key_format"
-            except InvalidKeyLengthError:
-                errors["base"] = "invalid_key_length"
-            except InvalidAccountFormatError:
-                errors["base"] = "invalid_account_format"
-            except InvalidAccountLengthError:
-                errors["base"] = "invalid_account_length"
-            except InvalidPing:
-                errors["base"] = "invalid_ping"
-            except InvalidZones:
-                errors["base"] = "invalid_zones"
-
-        return self.async_show_form(
-            step_id="user", data_schema=ACCOUNT_SCHEMA, errors=errors,
-        )
+    data = {}
 
     async def async_step_user(self, user_input: dict = None):
         """Handle the initial step."""
         errors = {}
-        if user_input is not None:
+        if not user_input:
+            return self.async_show_form(
+                step_id="user", data_schema=HUB_SCHEMA, errors=errors
+            )
+
+        # validate and return with error
+        try:
+            validate_input(user_input)
+        except InvalidKeyFormatError:
+            errors["base"] = "invalid_key_format"
+        except InvalidKeyLengthError:
+            errors["base"] = "invalid_key_length"
+        except InvalidAccountFormatError:
+            errors["base"] = "invalid_account_format"
+        except InvalidAccountLengthError:
+            errors["base"] = "invalid_account_length"
+        except InvalidPing:
+            errors["base"] = "invalid_ping"
+        except InvalidZones:
+            errors["base"] = "invalid_zones"
+        except Exception:  # pylint: disable=broad-except
+            errors["base"] = "unknown"
+        if errors:
+            schema = HUB_SCHEMA
+            if self.data.get(CONF_PORT):
+                schema = ACCOUNT_SCHEMA
+            return self.async_show_form(
+                step_id="user", data_schema=schema, errors=errors
+            )
+
+        # create self.data or add to self.data
+        if self.data:
+            add_data = user_input.copy()
+            add_data.pop(CONF_ADDITIONAL_ACCOUNTS)
+            self.data[CONF_ACCOUNTS].append(add_data)
+        else:
+            # if the port is being set, check for uniqueness and abort if not unique
+            await self.async_set_unique_id(f"{DOMAIN}_{user_input[CONF_PORT]}")
             try:
-                if validate_input(user_input):
-                    if not self.data:
-                        self.data = {
-                            CONF_PORT: user_input[CONF_PORT],
-                            CONF_ACCOUNTS: [
-                                {
-                                    CONF_ACCOUNT: user_input[CONF_ACCOUNT],
-                                    CONF_ENCRYPTION_KEY: user_input.get(
-                                        CONF_ENCRYPTION_KEY, None
-                                    ),
-                                    CONF_PING_INTERVAL: user_input[CONF_PING_INTERVAL],
-                                    CONF_ZONES: user_input[CONF_ZONES],
-                                }
-                            ],
-                        }
-                    else:
-                        add_data = user_input.copy()
-                        add_data.pop(CONF_ADDITIONAL_ACCOUNTS)
-                        self.data[CONF_ACCOUNTS].append(add_data)
-                    await self.async_set_unique_id(self.data[CONF_PORT])
-                    self._abort_if_unique_id_configured()
+                self._abort_if_unique_id_configured()
+            except AbortFlow:
+                return self.async_abort(reason=ABORT_ALREADY_CONFIGURED)
+            self.data = {
+                CONF_PORT: user_input[CONF_PORT],
+                CONF_ACCOUNTS: [
+                    {
+                        CONF_ACCOUNT: user_input[CONF_ACCOUNT],
+                        CONF_ENCRYPTION_KEY: user_input.get(CONF_ENCRYPTION_KEY),
+                        CONF_PING_INTERVAL: user_input[CONF_PING_INTERVAL],
+                        CONF_ZONES: user_input[CONF_ZONES],
+                    }
+                ],
+            }
 
-                    if not user_input[CONF_ADDITIONAL_ACCOUNTS]:
-                        return self.async_create_entry(
-                            title=f"SIA Alarm on port {self.data[CONF_PORT]}",
-                            data=self.data,
-                        )
-                    return await self.async_step_add_account()
-            except InvalidKeyFormatError:
-                errors["base"] = "invalid_key_format"
-            except InvalidKeyLengthError:
-                errors["base"] = "invalid_key_length"
-            except InvalidAccountFormatError:
-                errors["base"] = "invalid_account_format"
-            except InvalidAccountLengthError:
-                errors["base"] = "invalid_account_length"
-            except InvalidPing:
-                errors["base"] = "invalid_ping"
-            except InvalidZones:
-                errors["base"] = "invalid_zones"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-
-        return self.async_show_form(
-            step_id="user", data_schema=HUB_SCHEMA, errors=errors
+        # if additional accounts need to be added, do that, otherwise, create entry
+        if user_input[CONF_ADDITIONAL_ACCOUNTS]:
+            return self.async_show_form(
+                step_id="user", data_schema=ACCOUNT_SCHEMA, errors=errors
+            )
+        return self.async_create_entry(
+            title=f"SIA Alarm on port {self.data[CONF_PORT]}", data=self.data
         )
 
 
--- tests/components/sia/test_config_flow.py
+++ tests/components/sia/test_config_flow.py
@@ -1,16 +1,27 @@
 """Test the sia config flow."""
 from asynctest import patch
-from pysiaalarm.sia_errors import (
-    InvalidAccountFormatError,
-    InvalidAccountLengthError,
-    InvalidKeyFormatError,
-    InvalidKeyLengthError,
-    PortInUseError,
-)
+import pytest
 
 from homeassistant import config_entries, setup
-from homeassistant.components.sia.config_flow import InvalidPing
-from homeassistant.components.sia.const import DOMAIN
+from homeassistant.components.sia.const import ABORT_ALREADY_CONFIGURED, DOMAIN
+from homeassistant.data_entry_flow import AbortFlow
+
+BASIC_CONFIG = {
+    "port": 7777,
+    "account": "ABCDEF",
+    "encryption_key": "AAAAAAAAAAAAAAAA",
+    "ping_interval": 10,
+    "zones": 1,
+    "additional_account": False,
+}
+
+ADDITIONAL_ACCOUNT = {
+    "account": "ACC2",
+    "encryption_key": "AAAAAAAAAAAAAAAA",
+    "ping_interval": 2,
+    "zones": 2,
+    "additional_account": False,
+}
 
 
 async def test_form(hass):
@@ -59,35 +70,49 @@ async def test_form_invalid_key_format(hass):
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
+    assert result["type"] == "form"
+    assert result["errors"] == {}
+    config = BASIC_CONFIG.copy()
+    config["additional_account"] = True
+    print(config)
+    result2 = await hass.config_entries.flow.async_configure(result["flow_id"], config)
+    assert result2["type"] == "form"
+    assert result2["errors"] == {}
 
     with patch(
-        "homeassistant.components.sia.config_flow.validate_input",
-        side_effect=InvalidKeyFormatError,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
+        "homeassistant.components.sia.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.sia.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        result3 = await hass.config_entries.flow.async_configure(
+            result2["flow_id"], ADDITIONAL_ACCOUNT
+        )
+    assert result3["type"] == "create_entry"
+    assert result3["title"] == "SIA Alarm on port 7777"
+    assert result3["data"] == {
+        "port": 7777,
+        "accounts": [
             {
-                "name": "SIA",
-                "port": 7777,
                 "account": "ABCDEF",
                 "encryption_key": "AAAAAAAAAAAAA",
                 "ping_interval": 10,
             },
-        )
+        ],
+    }
+    await hass.async_block_till_done()
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
 
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_key_format"}
 
-
-async def test_form_invalid_key_length(hass):
+async def test_abort_form(hass):
     """Test we handle invalid key."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
     with patch(
-        "homeassistant.components.sia.config_flow.validate_input",
-        side_effect=InvalidKeyLengthError,
+        "homeassistant.config_entries.ConfigFlow._abort_if_unique_id_configured",
+        side_effect=AbortFlow(reason=ABORT_ALREADY_CONFIGURED),
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -99,106 +124,53 @@ async def test_form_invalid_key_length(hass):
                 "ping_interval": 10,
             },
         )
+    assert result2["type"] == "abort"
+    assert result2["reason"] == ABORT_ALREADY_CONFIGURED
 
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_key_length"}
 
-
-async def test_form_invalid_account_format(hass):
-    """Test we handle invalid account."""
+@pytest.mark.parametrize(
+    "additional, field, value, error",
+    [
+        (False, "encryption_key", "AAAAAAAAAAAAAZZZ", "invalid_key_format"),
+        (True, "encryption_key", "AAAAAAAAAAAAAZZZ", "invalid_key_format"),
+        (False, "encryption_key", "AAAAAAAAAAAAA", "invalid_key_length"),
+        (True, "encryption_key", "AAAAAAAAAAAAA", "invalid_key_length"),
+        (False, "account", "ZZZ", "invalid_account_format"),
+        (True, "account", "ZZZ", "invalid_account_format"),
+        (False, "account", "A", "invalid_account_length"),
+        (True, "account", "A", "invalid_account_length"),
+        (False, "ping_interval", 1500, "invalid_ping"),
+        (True, "ping_interval", 1500, "invalid_ping"),
+        (False, "zones", 0, "invalid_zones"),
+        (True, "zones", 0, "invalid_zones"),
+    ],
+)
+async def test_form_errors(hass, additional, field, value, error):
+    """Test we handle the different invalid inputs, both in the main and in the additional flow."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
-
-    with patch(
-        "homeassistant.components.sia.config_flow.validate_input",
-        side_effect=InvalidAccountFormatError,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "name": "SIA",
-                "port": 7777,
-                "account": "A",
-                "encryption_key": "AAAAAAAAAAAAAAAA",
-                "ping_interval": 10,
-            },
+    if not additional:
+        config = BASIC_CONFIG.copy()
+        config[field] = value
+    else:
+        add_config = BASIC_CONFIG.copy()
+        add_config["additional_account"] = True
+        result_add = await hass.config_entries.flow.async_configure(
+            result["flow_id"], add_config
         )
+        assert result_add["type"] == "form"
+        assert result_add["errors"] == {}
 
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_account_format"}
+        config = ADDITIONAL_ACCOUNT.copy()
+        config[field] = value
 
-
-async def test_form_invalid_account_length(hass):
-    """Test we handle invalid account."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.sia.config_flow.validate_input",
-        side_effect=InvalidAccountLengthError,
+    with patch("homeassistant.components.sia.async_setup", return_value=True), patch(
+        "homeassistant.components.sia.async_setup_entry", return_value=True
     ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "name": "SIA",
-                "port": 7777,
-                "account": "A",
-                "encryption_key": "AAAAAAAAAAAAAAAA",
-                "ping_interval": 10,
-            },
+        result_err = await hass.config_entries.flow.async_configure(
+            result["flow_id"], config
         )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_account_length"}
-
-
-async def test_form_invalid_ping(hass):
-    """Test we handle invalid ping."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.sia.config_flow.validate_input",
-        side_effect=InvalidPing,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "name": "SIA",
-                "port": 7777,
-                "account": "ABCDEF",
-                "encryption_key": "AAAAAAAAAAAAAAAA",
-                "ping_interval": 1500,
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_ping"}
-
-
-async def test_form_port_in_use(hass):
-    """Test we handle port in use."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.sia.config_flow.validate_input",
-        side_effect=PortInUseError,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "name": "SIA",
-                "port": 80,
-                "account": "ABCDEF",
-                "encryption_key": "AAAAAAAAAAAAAAAA",
-                "ping_interval": 10,
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "port_in_use"}
+    print(result_err)
+    assert result_err["type"] == "form"
+    assert result_err["errors"] == {"base": error}
