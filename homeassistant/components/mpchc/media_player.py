f93a6d1411023ced7ba22f06460a90f8ffcf833b
Petr Koutný <petr.k12@live.com>
2020-06-20 17:17:03 +0200

    mpchc - decode state by number, not localized text

--- homeassistant/components/mpchc/media_player.py
+++ homeassistant/components/mpchc/media_player.py
@@ -116,11 +116,13 @@ class MpcHcDevice(MediaPlayerEntity):
     @property
     def state(self):
         """Return the state of the device."""
-        state = self._player_variables.get("statestring", None)
+        state = self._player_variables.get("state", None)
 
         if state is None:
             return STATE_OFF
-        if state == "playing":
+        if state == "1":
+            return STATE_PAUSED
+        if state == "2":
             return STATE_PLAYING
         if state == "paused":
             return STATE_PAUSED
