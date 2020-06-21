e95ce67f6da86d4d3851f0105fedf420eee8f2bf
Aidan Timson <contact@timmo.xyz>
2020-06-21 16:15:40 +0100

    Reduce polling

--- homeassistant/components/ovo_energy/__init__.py
+++ homeassistant/components/ovo_energy/__init__.py
@@ -52,7 +52,7 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
         name="sensor",
         update_method=async_update_data,
         # Polling interval. Will only be polled if there are subscribers.
-        update_interval=timedelta(seconds=30),
+        update_interval=timedelta(seconds=300),
     )
 
     hass.data.setdefault(DOMAIN, {})
