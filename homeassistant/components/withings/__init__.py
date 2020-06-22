cf25aacfe9e628c440ad5d99be890bbbb84dc5ee
Robbie Van Gorkom <robbie.van.gorkom@protonmail.com>
2020-06-22 07:47:40 -0700

    Updating deprecated option.

--- homeassistant/components/withings/__init__.py
+++ homeassistant/components/withings/__init__.py
@@ -39,20 +39,23 @@ DOMAIN = const.DOMAIN
 
 CONFIG_SCHEMA = vol.Schema(
     {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_CLIENT_ID): vol.All(cv.string, vol.Length(min=1)),
-                vol.Required(CONF_CLIENT_SECRET): vol.All(cv.string, vol.Length(min=1)),
-                vol.Optional(const.CONF_USE_WEBHOOK, default=False): cv.boolean,
-                cv.deprecated(
-                    const.CONF_PROFILES, invalidation_version="0.114"
-                ): vol.All(
-                    cv.ensure_list,
-                    vol.Unique(),
-                    vol.Length(min=1),
-                    [vol.All(cv.string, vol.Length(min=1))],
-                ),
-            }
+        DOMAIN: vol.All(
+            cv.deprecated(const.CONF_PROFILES, invalidation_version="0.114"),
+            vol.Schema(
+                {
+                    vol.Required(CONF_CLIENT_ID): vol.All(cv.string, vol.Length(min=1)),
+                    vol.Required(CONF_CLIENT_SECRET): vol.All(
+                        cv.string, vol.Length(min=1)
+                    ),
+                    vol.Optional(const.CONF_USE_WEBHOOK, default=False): cv.boolean,
+                    vol.Optional(const.CONF_PROFILES): vol.All(
+                        cv.ensure_list,
+                        vol.Unique(),
+                        vol.Length(min=1),
+                        [vol.All(cv.string, vol.Length(min=1))],
+                    ),
+                }
+            ),
         )
     },
     extra=vol.ALLOW_EXTRA,
