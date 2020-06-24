07fc9516971a0cac39ac488181cefa1592129c13
Markus Bong <Markus.Bong@devolo.de>
2020-06-24 11:08:48 +0200

    initial commit

---
+++ homeassistant/components/devolo_home_control/sensor.py
@@ -0,0 +1,92 @@
+"""Platform for sensor integration."""
+import logging
+
+from homeassistant.components.sensor import (
+    DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_ILLUMINANCE,
+    DEVICE_CLASS_TEMPERATURE,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import HomeAssistantType
+
+from .const import DOMAIN
+from .devolo_device import DevoloDeviceEntity
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistantType, entry: ConfigEntry, async_add_entities
+) -> None:
+    """Get all sensor devices and setup them via config entry."""
+    entities = []
+
+    for device in hass.data[DOMAIN]["homecontrol"].multi_level_sensor_devices:
+        for multi_level_sensor in device.multi_level_sensor_property:
+            entities.append(
+                DevoloMultiLevelDeviceEntity(
+                    homecontrol=hass.data[DOMAIN]["homecontrol"],
+                    device_instance=device,
+                    element_uid=multi_level_sensor,
+                )
+            )
+    async_add_entities(entities, False)
+
+
+class DevoloMultiLevelDeviceEntity(DevoloDeviceEntity):
+    """Representation o a multi level sensor within devolo Home Control."""
+
+    def __init__(self, homecontrol, device_instance, element_uid):
+        """Initialize a devolo multi level sensor."""
+        super().__init__(
+            homecontrol=homecontrol,
+            device_instance=device_instance,
+            element_uid=element_uid,
+            name=f"{device_instance.itemName} {device_instance.multi_level_sensor_property.get(element_uid).sensor_type}",
+            sync=self._sync,
+        )
+
+        self._state = self._device_instance.multi_level_sensor_property.get(
+            element_uid
+        ).value
+
+        self._multi_level_sensor_property = device_instance.multi_level_sensor_property.get(
+            element_uid
+        )
+        if self._multi_level_sensor_property.sensor_type == "temperature":
+            self._device_class = DEVICE_CLASS_TEMPERATURE
+        elif self._multi_level_sensor_property.sensor_type == "light":
+            self._device_class = DEVICE_CLASS_ILLUMINANCE
+        elif self._multi_level_sensor_property.sensor_type == "humidity":
+            self._device_class = DEVICE_CLASS_HUMIDITY
+        else:
+            self._device_class = None
+            self._unit = None
+        self._unit = self._multi_level_sensor_property.unit
+
+    @property
+    def device_class(self) -> str:
+        """Return device class."""
+        return self._device_class
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity."""
+        return self._unit
+
+    def _sync(self, message=None):
+        """Update the multi level sensor state."""
+        if message[0].startswith("devolo.MultiLevelSensor"):
+            self._state = self._device_instance.multi_level_sensor_property[
+                message[0]
+            ].value
+        elif message[0].startswith("hdm"):
+            self._available = self._device_instance.is_online()
+        else:
+            _LOGGER.debug("No valid message received: %s", message)
+        self.schedule_update_ha_state()
