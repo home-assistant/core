2774b401fbb6807b11a29e8407fa26096eae1a9c
Haemish Kyd <haemish.kyd@gmail.com>
2020-06-24 07:23:43 +0200

    Removed space in icon return
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>

--- homeassistant/components/poolsense/sensor.py
+++ homeassistant/components/poolsense/sensor.py
@@ -147,7 +147,7 @@ class PoolSenseSensor(Entity):
         if self.info_type == "Chlorine Status":
             if self.coordinator.data[self.info_type] == "red":
                 return "mdi: thumb-down"
-            return "mdi: thumb-up"
+            return "mdi:thumb-up"
         return SENSORS[self.info_type]["icon"]
 
     @property
