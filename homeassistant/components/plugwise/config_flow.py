b0a550ae8406dfbdd5d544cf0dbe1048cbbb954b
Tom Scholten <tom@sue.nl>
2020-06-25 08:42:18 +0200

    Update to beta accordingly

--- homeassistant/components/plugwise/config_flow.py
+++ homeassistant/components/plugwise/config_flow.py
@@ -114,7 +114,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             if not errors:
                 await self.async_set_unique_id(api.gateway_id)
-                self._abort_if_unique_id_configured()
 
                 return self.async_create_entry(title=api.smile_name, data=user_input)
 
