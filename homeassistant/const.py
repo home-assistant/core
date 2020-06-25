7be13b07ff0c96b76e889bf797e3b51c4c9cdf66
tomek@sviete.pl <tomek@sviete.pl>
2020-06-25 10:09:11 +0200

    0.111.4b2

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 111
-PATCH_VERSION = "4b1"
+PATCH_VERSION = "4b2"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
 REQUIRED_PYTHON_VER = (3, 7, 0)
