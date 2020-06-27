355aae64d9f087827e650e43579e72f6908c5c4f
J. Nick Koston <nick@koston.org>
2020-06-27 14:26:53 +0000

    adj

--- tests/helpers/test_event.py
+++ tests/helpers/test_event.py
@@ -166,22 +166,22 @@ async def test_track_state_change(hass):
 
 async def test_async_track_state_change_event(hass):
     """Test async_track_state_change_event."""
-    single_runs = []
-    multiple_runs = []
+    single_entity_id_tracker = []
+    multiple_entity_id_tracker = []
 
     @ha.callback
     def single_run_callback(event):
         old_state = event.data.get("old_state")
         new_state = event.data.get("new_state")
 
-        single_runs.append((old_state, new_state))
+        single_entity_id_tracker.append((old_state, new_state))
 
     @ha.callback
     def multiple_run_callback(event):
         old_state = event.data.get("old_state")
         new_state = event.data.get("new_state")
 
-        multiple_runs.append((old_state, new_state))
+        multiple_entity_id_tracker.append((old_state, new_state))
 
     async_track_state_change_event(hass, ["light.Bowl"], single_run_callback)
     async_track_state_change_event(
@@ -191,51 +191,51 @@ async def test_async_track_state_change_event(hass):
     # Adding state to state machine
     hass.states.async_set("light.Bowl", "on")
     await hass.async_block_till_done()
-    assert len(single_runs) == 1
-    assert single_runs[-1][0] is None
-    assert single_runs[-1][1] is not None
-    assert len(multiple_runs) == 1
-    assert multiple_runs[-1][0] is None
-    assert multiple_runs[-1][1] is not None
+    assert len(single_entity_id_tracker) == 1
+    assert single_entity_id_tracker[-1][0] is None
+    assert single_entity_id_tracker[-1][1] is not None
+    assert len(multiple_entity_id_tracker) == 1
+    assert multiple_entity_id_tracker[-1][0] is None
+    assert multiple_entity_id_tracker[-1][1] is not None
 
     # Set same state should not trigger a state change/listener
     hass.states.async_set("light.Bowl", "on")
     await hass.async_block_till_done()
-    assert len(single_runs) == 1
-    assert len(multiple_runs) == 1
+    assert len(single_entity_id_tracker) == 1
+    assert len(multiple_entity_id_tracker) == 1
 
     # State change off -> on
     hass.states.async_set("light.Bowl", "off")
     await hass.async_block_till_done()
-    assert len(single_runs) == 2
-    assert len(multiple_runs) == 2
+    assert len(single_entity_id_tracker) == 2
+    assert len(multiple_entity_id_tracker) == 2
 
     # State change off -> off
     hass.states.async_set("light.Bowl", "off", {"some_attr": 1})
     await hass.async_block_till_done()
-    assert len(single_runs) == 3
-    assert len(multiple_runs) == 3
+    assert len(single_entity_id_tracker) == 3
+    assert len(multiple_entity_id_tracker) == 3
 
     # State change off -> on
     hass.states.async_set("light.Bowl", "on")
     await hass.async_block_till_done()
-    assert len(single_runs) == 4
-    assert len(multiple_runs) == 4
+    assert len(single_entity_id_tracker) == 4
+    assert len(multiple_entity_id_tracker) == 4
 
     hass.states.async_remove("light.bowl")
     await hass.async_block_till_done()
-    assert len(single_runs) == 5
-    assert single_runs[-1][0] is not None
-    assert single_runs[-1][1] is None
-    assert len(multiple_runs) == 5
-    assert multiple_runs[-1][0] is not None
-    assert multiple_runs[-1][1] is None
+    assert len(single_entity_id_tracker) == 5
+    assert single_entity_id_tracker[-1][0] is not None
+    assert single_entity_id_tracker[-1][1] is None
+    assert len(multiple_entity_id_tracker) == 5
+    assert multiple_entity_id_tracker[-1][0] is not None
+    assert multiple_entity_id_tracker[-1][1] is None
 
     # Set state for different entity id
     hass.states.async_set("switch.kitchen", "on")
     await hass.async_block_till_done()
-    assert len(single_runs) == 5
-    assert len(multiple_runs) == 6
+    assert len(single_entity_id_tracker) == 5
+    assert len(multiple_entity_id_tracker) == 6
 
 
 async def test_track_template(hass):
