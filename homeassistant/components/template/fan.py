26166d5b33bac88768a0a2d881477f23c404900a
Thomas Delaet <thomas@delaet.org>
2020-06-27 20:18:25 +0200

    if template state is unavailable, entity state is also unavailable

--- homeassistant/components/template/fan.py
+++ homeassistant/components/template/fan.py
@@ -51,7 +51,7 @@ CONF_SET_SPEED_ACTION = "set_speed"
 CONF_SET_OSCILLATING_ACTION = "set_oscillating"
 CONF_SET_DIRECTION_ACTION = "set_direction"
 
-_VALID_STATES = [STATE_ON, STATE_OFF]
+_VALID_STATES = [STATE_ON, STATE_OFF, STATE_UNAVAILABLE]
 _VALID_OSC = [True, False]
 _VALID_DIRECTIONS = [DIRECTION_FORWARD, DIRECTION_REVERSE]
 
@@ -345,7 +345,7 @@ class TemplateFan(FanEntity):
         # Validate state
         if state in _VALID_STATES:
             self._state = state
-        elif state in [STATE_UNKNOWN, STATE_UNAVAILABLE]:
+        elif state == STATE_UNKNOWN:
             self._state = None
         else:
             _LOGGER.error(
@@ -367,7 +367,9 @@ class TemplateFan(FanEntity):
             # Validate speed
             if speed in self._speed_list:
                 self._speed = speed
-            elif speed in [STATE_UNKNOWN, STATE_UNAVAILABLE]:
+            elif speed == STATE_UNAVAILABLE:
+                self._speed = STATE_UNAVAILABLE
+            elif speed == STATE_UNKNOWN:
                 self._speed = None
             else:
                 _LOGGER.error(
@@ -389,7 +391,9 @@ class TemplateFan(FanEntity):
                 self._oscillating = True
             elif oscillating == "False" or oscillating is False:
                 self._oscillating = False
-            elif oscillating in [STATE_UNKNOWN, STATE_UNAVAILABLE]:
+            elif oscillating == STATE_UNAVAILABLE:
+                self._oscillating = STATE_UNAVAILABLE
+            elif oscillating == STATE_UNKNOWN:
                 self._oscillating = None
             else:
                 _LOGGER.error(
@@ -410,7 +414,9 @@ class TemplateFan(FanEntity):
             # Validate speed
             if direction in _VALID_DIRECTIONS:
                 self._direction = direction
-            elif direction in [STATE_UNKNOWN, STATE_UNAVAILABLE]:
+            elif direction == STATE_UNAVAILABLE:
+                self._direction = STATE_UNAVAILABLE
+            elif direction == STATE_UNKNOWN:
                 self._direction = None
             else:
                 _LOGGER.error(
