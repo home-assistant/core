d1f81c176915b1cedcf217a4c32cf59985860751
J. Nick Koston <nick@koston.org>
2020-06-27 16:46:45 -0500

    Improve scalability of state change event routing (#37174)

--- homeassistant/helpers/event.py
+++ homeassistant/helpers/event.py
@@ -1,7 +1,7 @@
 """Helpers for listening to events."""
 from datetime import datetime, timedelta
 import functools as ft
-from typing import Any, Awaitable, Callable, Dict, Iterable, Optional, Union, cast
+from typing import Any, Awaitable, Callable, Dict, Iterable, Optional, Union
 
 import attr
 
@@ -20,6 +20,9 @@ from homeassistant.helpers.template import Template
 from homeassistant.loader import bind_hass
 from homeassistant.util import dt as dt_util
 from homeassistant.util.async_ import run_callback_threadsafe
+
+TRACK_STATE_CHANGE_CALLBACKS = "track_state_change_callbacks"
+TRACK_STATE_CHANGE_LISTENER = "track_state_change_listener"
 
 # PyLint does not like the use of threaded_listener_factory
 # pylint: disable=invalid-name
@@ -81,12 +84,6 @@ def async_track_state_change(
     @callback
     def state_change_listener(event: Event) -> None:
         """Handle specific state changes."""
-        if (
-            entity_ids != MATCH_ALL
-            and cast(str, event.data.get("entity_id")) not in entity_ids
-        ):
-            return
-
         old_state = event.data.get("old_state")
         if old_state is not None:
             old_state = old_state.state
@@ -103,10 +100,89 @@ def async_track_state_change(
                 event.data.get("new_state"),
             )
 
+    if entity_ids != MATCH_ALL:
+        # If we have a list of entity ids we use
+        # async_track_state_change_event to route
+        # by entity_id to avoid iterating though state change
+        # events and creating a jobs where the most
+        # common outcome is to return right away because
+        # the entity_id does not match since usually
+        # only one or two listeners want that specific
+        # entity_id.
+        return async_track_state_change_event(hass, entity_ids, state_change_listener)
+
     return hass.bus.async_listen(EVENT_STATE_CHANGED, state_change_listener)
 
 
 track_state_change = threaded_listener_factory(async_track_state_change)
+
+
+@bind_hass
+def async_track_state_change_event(
+    hass: HomeAssistant, entity_ids: Iterable[str], action: Callable[[Event], None]
+) -> Callable[[], None]:
+    """Track specific state change events indexed by entity_id.
+
+    Unlike async_track_state_change, async_track_state_change_event
+    passes the full event to the callback.
+
+    In order to avoid having to iterate a long list
+    of EVENT_STATE_CHANGED and fire and create a job
+    for each one, we keep a dict of entity ids that
+    care about the state change events so we can
+    do a fast dict lookup to route events.
+    """
+
+    entity_callbacks = hass.data.setdefault(TRACK_STATE_CHANGE_CALLBACKS, {})
+
+    if TRACK_STATE_CHANGE_LISTENER not in hass.data:
+
+        @callback
+        def _async_state_change_dispatcher(event: Event) -> None:
+            """Dispatch state changes by entity_id."""
+            entity_id = event.data.get("entity_id")
+
+            if entity_id not in entity_callbacks:
+                return
+
+            for action in entity_callbacks[entity_id]:
+                hass.async_run_job(action, event)
+
+        hass.data[TRACK_STATE_CHANGE_LISTENER] = hass.bus.async_listen(
+            EVENT_STATE_CHANGED, _async_state_change_dispatcher
+        )
+
+    entity_ids = [entity_id.lower() for entity_id in entity_ids]
+
+    for entity_id in entity_ids:
+        if entity_id not in entity_callbacks:
+            entity_callbacks[entity_id] = []
+
+        entity_callbacks[entity_id].append(action)
+
+    @callback
+    def remove_listener() -> None:
+        """Remove state change listener."""
+        _async_remove_state_change_listeners(hass, entity_ids, action)
+
+    return remove_listener
+
+
+@callback
+def _async_remove_state_change_listeners(
+    hass: HomeAssistant, entity_ids: Iterable[str], action: Callable[[Event], None]
+) -> None:
+    """Remove a listener."""
+    entity_callbacks = hass.data[TRACK_STATE_CHANGE_CALLBACKS]
+
+    for entity_id in entity_ids:
+        entity_callbacks[entity_id].remove(action)
+        if len(entity_callbacks[entity_id]) == 0:
+            del entity_callbacks[entity_id]
+
+    if not entity_callbacks:
+        hass.data[TRACK_STATE_CHANGE_LISTENER]()
+        del hass.data[TRACK_STATE_CHANGE_LISTENER]
 
 
 @callback
