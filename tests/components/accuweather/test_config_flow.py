3d8a41b8468a355f9e275fb2c8387a83bf299c4f
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-06-27 11:17:03 +0200

    More tests

--- tests/components/accuweather/test_config_flow.py
+++ tests/components/accuweather/test_config_flow.py
@@ -4,7 +4,7 @@ import json
 from accuweather import ApiError, InvalidApiKeyError, RequestsExceededError
 
 from homeassistant import data_entry_flow
-from homeassistant.components.accuweather.const import DOMAIN
+from homeassistant.components.accuweather.const import CONF_FORECAST, DOMAIN
 from homeassistant.config_entries import SOURCE_USER
 from homeassistant.const import CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
 
@@ -93,7 +93,7 @@ async def test_integration_already_exists(hass):
     """Test we only allow a single config flow."""
     with patch(
         "accuweather.AccuWeather._async_get_data",
-        return_value=json.loads(load_fixture("accuweather_location_data.json")),
+        return_value=json.loads(load_fixture("accuweather/location_data.json")),
     ):
         MockConfigEntry(
             domain=DOMAIN, unique_id="123456", data=VALID_CONFIG,
@@ -111,7 +111,7 @@ async def test_create_entry(hass):
     """Test that the user step works."""
     with patch(
         "accuweather.AccuWeather._async_get_data",
-        return_value=json.loads(load_fixture("accuweather_location_data.json")),
+        return_value=json.loads(load_fixture("accuweather/location_data.json")),
     ):
 
         result = await hass.config_entries.flow.async_init(
@@ -124,3 +124,35 @@ async def test_create_entry(hass):
         assert result["data"][CONF_LATITUDE] == 55.55
         assert result["data"][CONF_LONGITUDE] == 122.12
         assert result["data"][CONF_API_KEY] == "32-character-string-1234567890qw"
+
+
+async def test_options_flow(hass):
+    """Test config flow options."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN, unique_id="123456", data=VALID_CONFIG,
+    )
+    config_entry.add_to_hass(hass)
+
+    with patch(
+        "accuweather.AccuWeather._async_get_data",
+        return_value=json.loads(load_fixture("accuweather/location_data.json")),
+    ), patch(
+        "accuweather.AccuWeather.async_get_current_conditions",
+        return_value=json.loads(
+            load_fixture("accuweather/current_conditions_data.json")
+        ),
+    ):
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+        result = await hass.config_entries.options.async_init(config_entry.entry_id)
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["step_id"] == "user"
+
+        result = await hass.config_entries.options.async_configure(
+            result["flow_id"], user_input={CONF_FORECAST: True}
+        )
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert config_entry.options == {CONF_FORECAST: True}
