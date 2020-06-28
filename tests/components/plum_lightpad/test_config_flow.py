7bffe1562c29939e74cfa7a4f5db2e7c3638f973
Eugene Prystupa <eugene.prystupa@gmail.com>
2020-06-28 17:52:17 -0400

    add first unit test to config flow for Plum Lightpad

--- tests/components/plum_lightpad/test_config_flow.py
+++ tests/components/plum_lightpad/test_config_flow.py
@@ -8,6 +8,7 @@ from tests.async_mock import patch
 async def test_form(hass):
     """Test we get the form."""
     await setup.async_setup_component(hass, "persistent_notification", {})
+
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
@@ -33,3 +34,28 @@ async def test_form(hass):
     await hass.async_block_till_done()
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_import(hass):
+    """Test configuring the flow using configuration.yaml."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+
+    with patch("plumlightpad.Plum.loadCloudData"), patch(
+        "homeassistant.components.plum_lightpad.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.plum_lightpad.async_setup_entry", return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_IMPORT},
+            data={"username": "test-plum-username", "password": "test-plum-password"},
+        )
+        assert result["type"] == "create_entry"
+        assert result["title"] == "test-plum-username"
+        assert result["data"] == {
+            "username": "test-plum-username",
+            "password": "test-plum-password",
+        }
+        await hass.async_block_till_done()
+        assert len(mock_setup.mock_calls) == 1
+        assert len(mock_setup_entry.mock_calls) == 1
