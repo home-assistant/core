7297e852dd6fd0a6956c3500f1c69faa4e34bf42
Denis Shulyaka <Shulyaka@gmail.com>
2020-06-29 02:23:59 +0300

    converted from platform to integration

--- homeassistant/components/generic_hygrostat/humidifier.py
+++ homeassistant/components/generic_hygrostat/humidifier.py
@@ -25,64 +25,43 @@ from homeassistant.const import (
 )
 from homeassistant.core import DOMAIN as HA_DOMAIN, callback
 from homeassistant.helpers import condition
-import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import (
     async_track_state_change,
     async_track_time_interval,
 )
 from homeassistant.helpers.restore_state import RestoreEntity
 
-_LOGGER = logging.getLogger(__name__)
+from . import (
+    CONF_AWAY_FIXED,
+    CONF_AWAY_HUMIDITY,
+    CONF_DEVICE_CLASS,
+    CONF_DRY_TOLERANCE,
+    CONF_HUMIDIFIER,
+    CONF_INITIAL_STATE,
+    CONF_KEEP_ALIVE,
+    CONF_MAX_HUMIDITY,
+    CONF_MIN_DUR,
+    CONF_MIN_HUMIDITY,
+    CONF_SENSOR,
+    CONF_STALE_DURATION,
+    CONF_TARGET_HUMIDITY,
+    CONF_WET_TOLERANCE,
+    HYGROSTAT_SCHEMA,
+)
 
-DEFAULT_TOLERANCE = 0.3
-DEFAULT_NAME = "Generic Hygrostat"
+_LOGGER = logging.getLogger(__name__)
 
 ATTR_SAVED_HUMIDITY = "saved_humidity"
 
-CONF_HUMIDIFIER = "humidifier"
-CONF_SENSOR = "target_sensor"
-CONF_MIN_HUMIDITY = "min_humidity"
-CONF_MAX_HUMIDITY = "max_humidity"
-CONF_TARGET_HUMIDITY = "target_humidity"
-CONF_DEVICE_CLASS = "device_class"
-CONF_MIN_DUR = "min_cycle_duration"
-CONF_DRY_TOLERANCE = "dry_tolerance"
-CONF_WET_TOLERANCE = "wet_tolerance"
-CONF_KEEP_ALIVE = "keep_alive"
-CONF_INITIAL_STATE = "initial_state"
-CONF_AWAY_HUMIDITY = "away_humidity"
-CONF_AWAY_FIXED = "away_fixed"
-CONF_STALE_DURATION = "sensor_stale_duration"
-
 SUPPORT_FLAGS = 0
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Required(CONF_HUMIDIFIER): cv.entity_id,
-        vol.Required(CONF_SENSOR): cv.entity_id,
-        vol.Optional(CONF_DEVICE_CLASS): vol.In(
-            [DEVICE_CLASS_HUMIDIFIER, DEVICE_CLASS_DEHUMIDIFIER]
-        ),
-        vol.Optional(CONF_MAX_HUMIDITY): vol.Coerce(float),
-        vol.Optional(CONF_MIN_DUR): vol.All(cv.time_period, cv.positive_timedelta),
-        vol.Optional(CONF_MIN_HUMIDITY): vol.Coerce(float),
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Optional(CONF_DRY_TOLERANCE, default=DEFAULT_TOLERANCE): vol.Coerce(float),
-        vol.Optional(CONF_WET_TOLERANCE, default=DEFAULT_TOLERANCE): vol.Coerce(float),
-        vol.Optional(CONF_TARGET_HUMIDITY): vol.Coerce(float),
-        vol.Optional(CONF_KEEP_ALIVE): vol.All(cv.time_period, cv.positive_timedelta),
-        vol.Optional(CONF_INITIAL_STATE): cv.boolean,
-        vol.Optional(CONF_AWAY_HUMIDITY): vol.Coerce(float),
-        vol.Optional(CONF_AWAY_FIXED): cv.boolean,
-        vol.Optional(CONF_STALE_DURATION): vol.All(
-            cv.time_period, cv.positive_timedelta
-        ),
-    }
-)
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(HYGROSTAT_SCHEMA.schema)
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the generic hygrostat platform."""
+    if discovery_info:
+        config = PLATFORM_SCHEMA(discovery_info)
     name = config.get(CONF_NAME)
     switch_entity_id = config.get(CONF_HUMIDIFIER)
     sensor_entity_id = config.get(CONF_SENSOR)
