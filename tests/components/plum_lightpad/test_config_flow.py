cd8dccdcf570bb3f15f8ccb62b35043406d76a98
Eugene Prystupa <eugene.prystupa@gmail.com>
2020-06-29 09:19:10 -0400

    add first unit test to config flow for Plum Lightpad (add unit test for abort)

--- tests/components/plum_lightpad/test_config_flow.py
+++ tests/components/plum_lightpad/test_config_flow.py
@@ -5,6 +5,7 @@ from homeassistant import config_entries, setup
 from homeassistant.components.plum_lightpad.const import DOMAIN
 
 from tests.async_mock import patch
+from tests.common import MockConfigEntry
 
 
 async def test_form(hass):
@@ -57,6 +58,36 @@ async def test_form_cannot_connect(hass):
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_one_entry_per_email_allowed(hass):
+    """Test that only one entry allowed per Plum cloud email address."""
+    MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="test-plum-username",
+        data={"username": "test-plum-username", "password": "test-plum-password"},
+    ).add_to_hass(hass)
+
+    await setup.async_setup_component(hass, "persistent_notification", {})
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.plum_lightpad.utils.Plum.loadCloudData"
+    ), patch("homeassistant.components.plum_lightpad.async_setup") as mock_setup, patch(
+        "homeassistant.components.plum_lightpad.async_setup_entry"
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {"username": "test-plum-username", "password": "test-plum-password"},
+        )
+
+    assert result2["type"] == "abort"
+    await hass.async_block_till_done()
+    assert len(mock_setup.mock_calls) == 0
+    assert len(mock_setup_entry.mock_calls) == 0
 
 
 async def test_import(hass):
