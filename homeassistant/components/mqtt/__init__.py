9356bd54563dfd7271a82efe81d41f2d91a75812
definitio <37266727+definitio@users.noreply.github.com>
2020-06-29 21:13:44 +0400

    Don't save 'unavailable' state on disconnect

--- homeassistant/components/mqtt/__init__.py
+++ homeassistant/components/mqtt/__init__.py
@@ -1032,7 +1032,8 @@ class MqttAvailability(Entity):
     @callback
     def async_mqtt_connect(self):
         """Update state on connection/disconnection to MQTT broker."""
-        self.async_write_ha_state()
+        if self.hass.is_running:
+            self.async_write_ha_state()
 
     async def async_will_remove_from_hass(self):
         """Unsubscribe when removed."""
