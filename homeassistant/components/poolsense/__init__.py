ee4fa815a96ea8f05c7afa3d3eafbdabc7f87c3f
Chris Talkington <chris@talkingtontech.com>
2020-06-30 21:18:48 -0500

    Update homeassistant/components/poolsense/__init__.py

--- homeassistant/components/poolsense/__init__.py
+++ homeassistant/components/poolsense/__init__.py
@@ -68,6 +68,9 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
         )
     )
 
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
     return unload_ok
 
 
