35eeb5522bdeed9d22f41d5d1c364b3b5189f107
Shulyaka <Shulyaka@gmail.com>
2020-07-02 16:20:48 +0300

    Update homeassistant/components/homekit/type_humidifiers.py
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/homekit/type_humidifiers.py
+++ homeassistant/components/homekit/type_humidifiers.py
@@ -92,9 +92,8 @@ class HumidifierDehumidifier(HomeAccessory):
             CHAR_CURRENT_HUMIDIFIER_DEHUMIDIFIER, value=0
         )
         self.char_target_humidifier_dehumidifier = serv_humidifier_dehumidifier.configure_char(
-            CHAR_TARGET_HUMIDIFIER_DEHUMIDIFIER, value=self._hk_device_class,
-        )
-        self.char_target_humidifier_dehumidifier.override_properties(
+            CHAR_TARGET_HUMIDIFIER_DEHUMIDIFIER, 
+            value=self._hk_device_class,
             valid_values={
                 HC_HASS_TO_HOMEKIT_DEVICE_CLASS_NAME[
                     device_class
