34f7a4e31d63cc4e25cb4ca1f13f930c272c6d2d
tomek@sviete.pl <tomek@sviete.pl>
2020-07-02 11:15:34 +0200

    bump version info

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ REQUIRES = [
     "ruamel.yaml==0.15.100",
     "voluptuous==0.11.7",
     "voluptuous-serialize==2.3.0",
-    "ais-dom-frontend==20200701.1",
+    "ais-dom-frontend==20200701.2",
 ]
 
 MIN_PY_VERSION = ".".join(map(str, hass_const.REQUIRED_PYTHON_VER))
