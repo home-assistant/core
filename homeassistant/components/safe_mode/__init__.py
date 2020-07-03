60b53f30269c96ec98f2d3449dd3ac42baf9e643
tomasz75 <tomek@sviete.pl>
2020-07-03 08:20:45 +0200

    merge

--- homeassistant/components/safe_mode/__init__.py
+++ homeassistant/components/safe_mode/__init__.py
@@ -9,7 +9,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Safe Mode component."""
     persistent_notification.async_create(
         hass,
-        "Asystent domowy działa w trybie awaryjnym. Przejdź [do logów  systemu](/developer-tools/logs) i "
+        "Asystent domowy działa w trybie awaryjnym. Przejdź [do logów  systemu](/config/logs) i "
         "sprawdź, co poszło nie tak.",
         "Tryb Awaryjny",
     )
