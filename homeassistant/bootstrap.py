c871391e2181d892cfdca3c411adac9917c4bf51
Franck Nijhof <git@frenck.dev>
2020-07-04 21:48:19 +0200

    Break typehinting to test problem matcher

--- homeassistant/bootstrap.py
+++ homeassistant/bootstrap.py
@@ -158,7 +158,7 @@ async def async_setup_hass(
 
 async def async_from_config_dict(
     config: ConfigType, hass: core.HomeAssistant
-) -> Optional[core.HomeAssistant]:
+) -> core.HomeAssistant:
     """Try to configure Home Assistant from a configuration dictionary.
 
     Dynamically loads required components and its dependencies.
