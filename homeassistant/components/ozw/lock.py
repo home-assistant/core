9e55896145ed7b4c959ecb2372c5e46084c29c8c
firstof9 <firstof9@gmail.com>
2020-07-03 18:13:01 -0700

    Add usercodes support to ozw locks

--- homeassistant/components/ozw/lock.py
+++ homeassistant/components/ozw/lock.py
@@ -1,10 +1,25 @@
 """Representation of Z-Wave locks."""
+import logging
+
+from openzwavemqtt.const import CommandClass
+import voluptuous as vol
+
 from homeassistant.components.lock import DOMAIN as LOCK_DOMAIN, LockEntity
 from homeassistant.core import callback
+from homeassistant.helpers import config_validation as cv, entity_platform
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
 from .const import DATA_UNSUBSCRIBE, DOMAIN
 from .entity import ZWaveDeviceEntity
+
+ATTR_CODE_SLOT = "code_slot"
+ATTR_USERCODE = "usercode"
+
+SERVICE_SET_USERCODE = "set_usercode"
+SERVICE_GET_USERCODE = "get_usercode"
+SERVICE_CLEAR_USERCODE = "clear_usercode"
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -19,6 +34,29 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     hass.data[DOMAIN][config_entry.entry_id][DATA_UNSUBSCRIBE].append(
         async_dispatcher_connect(hass, f"{DOMAIN}_new_{LOCK_DOMAIN}", async_add_lock)
+    )
+
+    platform = entity_platform.current_platform.get()
+
+    platform.async_register_entity_service(
+        SERVICE_SET_USERCODE,
+        {
+            vol.Required(ATTR_CODE_SLOT): vol.Coerce(int),
+            vol.Required(ATTR_USERCODE): cv.string,
+        },
+        "set_usercode",
+    )
+
+    platform.async_register_entity_service(
+        SERVICE_GET_USERCODE,
+        {vol.Required(ATTR_CODE_SLOT): vol.Coerce(int)},
+        "get_usercode",
+    )
+
+    platform.async_register_entity_service(
+        SERVICE_CLEAR_USERCODE,
+        {vol.Required(ATTR_CODE_SLOT): vol.Coerce(int)},
+        "clear_usercode",
     )
 
 
@@ -37,3 +75,55 @@ class ZWaveLock(ZWaveDeviceEntity, LockEntity):
     async def async_unlock(self, **kwargs):
         """Unlock the lock."""
         self.values.primary.send_value(False)
+
+    def set_usercode(self, code_slot, usercode):
+        """Set the usercode to index X on the lock."""
+
+        lock_node = self.values()
+
+        _LOGGER.debug("Debug: %s", str(dir(self.values())))
+
+        for value in lock_node:
+            _LOGGER.debug("Debug: %s", str(value))
+            if (
+                value.command_class == CommandClass.USER_CODE
+                and value.index == code_slot
+            ):
+                if len(str(usercode)) < 4:
+                    _LOGGER.error(
+                        "Invalid code provided: (%s) user code must be at least 4 digits",
+                        usercode,
+                    )
+                    break
+                value.send_value(usercode)
+                break
+
+    def get_usercode(self, code_slot):
+        """Get a usercode at index X on the lock."""
+        # code_slot = service.data[ATTR_CODE_SLOT]
+
+        # lock_node = self._manager.get_instance(instance_id).get_node(node_id).values()
+
+        # for value in lock_node:
+        #     if (
+        #         value.command_class == CommandClass.USER_CODE
+        #         and value.index == code_slot
+        #     ):
+        #         _LOGGER.info("User code at slot %s is: %s", code_slot, value.value)
+        #         break
+
+    def clear_usercode(self, code_slot):
+        """Set usercode to slot X on the lock."""
+        # code_slot = service.data[ATTR_CODE_SLOT]
+
+        # lock_node = self._manager.get_instance(instance_id).get_node(node_id).values()
+
+        # for value in lock_node:
+        #     if (
+        #         value.command_class == CommandClass.USER_CODE
+        #         and value.label == "Remove User Code"
+        #     ):
+        #         value.send_value(code_slot)
+        #         value.send_value(code_slot)
+        #         _LOGGER.info("Usercode at slot %s is cleared", code_slot)
+        #         break
--- homeassistant/components/ozw/services.py
+++ homeassistant/components/ozw/services.py
@@ -1,16 +1,9 @@
 """Methods and classes related to executing Z-Wave commands and publishing these to hass."""
-import logging
-
-from openzwavemqtt.const import CommandClass
 import voluptuous as vol
 
-from homeassistant.components.lock import DOMAIN as LOCK_DOMAIN
 from homeassistant.core import callback
-import homeassistant.helpers.config_validation as cv
 
 from . import const
-
-_LOGGER = logging.getLogger(__name__)
 
 
 class ZWaveServices:
@@ -43,103 +36,6 @@ class ZWaveServices:
                 {vol.Optional(const.ATTR_INSTANCE_ID, default=1): vol.Coerce(int)}
             ),
         )
-
-        self._hass.services.async_register(
-            LOCK_DOMAIN,
-            const.SERVICE_SET_USERCODE,
-            self.set_usercode,
-            schema=vol.Schema(
-                {
-                    vol.Optional(const.ATTR_INSTANCE_ID, default=1): vol.Coerce(int),
-                    vol.Required(const.ATTR_NODE_ID): vol.Coerce(int),
-                    vol.Required(const.ATTR_CODE_SLOT): vol.Coerce(int),
-                    vol.Required(const.ATTR_USERCODE): cv.string,
-                }
-            ),
-        )
-
-        self._hass.services.async_register(
-            LOCK_DOMAIN,
-            const.SERVICE_GET_USERCODE,
-            self.get_usercode,
-            schema=vol.Schema(
-                {
-                    vol.Optional(const.ATTR_INSTANCE_ID, default=1): vol.Coerce(int),
-                    vol.Required(const.ATTR_NODE_ID): vol.Coerce(int),
-                    vol.Required(const.ATTR_CODE_SLOT): vol.Coerce(int),
-                }
-            ),
-        )
-
-        self._hass.services.async_register(
-            LOCK_DOMAIN,
-            const.SERVICE_CLEAR_USERCODE,
-            self.clear_usercode,
-            schema=vol.Schema(
-                {
-                    vol.Optional(const.ATTR_INSTANCE_ID, default=1): vol.Coerce(int),
-                    vol.Required(const.ATTR_NODE_ID): vol.Coerce(int),
-                    vol.Required(const.ATTR_CODE_SLOT): vol.Coerce(int),
-                }
-            ),
-        )
-
-    def set_usercode(self, service):
-        """Set the usercode to index X on the lock."""
-        instance_id = service.data[const.ATTR_INSTANCE_ID]
-        node_id = service.data[const.ATTR_NODE_ID]
-        code_slot = service.data[const.ATTR_CODE_SLOT]
-        usercode = service.data[const.ATTR_USERCODE]
-
-        lock_node = self._manager.get_instance(instance_id).get_node(node_id).values()
-
-        for value in lock_node:
-            if (
-                value.command_class == CommandClass.USER_CODE
-                and value.index == code_slot
-            ):
-                if len(str(usercode)) < 4:
-                    _LOGGER.error(
-                        "Invalid code provided: (%s) user code must be at least 4 digits",
-                        usercode,
-                    )
-                    break
-                value.send_value(usercode)
-                break
-
-    def get_usercode(self, service):
-        """Get a usercode at index X on the lock."""
-        instance_id = service.data[const.ATTR_INSTANCE_ID]
-        node_id = service.data[const.ATTR_NODE_ID]
-        code_slot = service.data[const.ATTR_CODE_SLOT]
-
-        lock_node = self._manager.get_instance(instance_id).get_node(node_id).values()
-
-        for value in lock_node:
-            if (
-                value.command_class == CommandClass.USER_CODE
-                and value.index == code_slot
-            ):
-                _LOGGER.info("User code at slot %s is: %s", code_slot, value.value)
-                break
-
-    def clear_usercode(self, service):
-        """Set usercode to slot X on the lock."""
-        instance_id = service.data[const.ATTR_INSTANCE_ID]
-        node_id = service.data[const.ATTR_NODE_ID]
-        code_slot = service.data[const.ATTR_CODE_SLOT]
-
-        lock_node = self._manager.get_instance(instance_id).get_node(node_id).values()
-
-        for value in lock_node:
-            if (
-                value.command_class == CommandClass.USER_CODE
-                and value.label == "Remove User Code"
-            ):
-                value.send_value(code_slot)
-                value.send_value(code_slot)
-                _LOGGER.info("Usercode at slot %s is cleared", code_slot)
-                break
 
     @callback
     def async_add_node(self, service):
