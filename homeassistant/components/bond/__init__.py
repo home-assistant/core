7b192e91b1ffa9fc27ed7d3f5c49fb78eb7bc874
Eugene Prystupa <eugene.prystupa@gmail.com>
2020-07-05 15:13:33 -0400

    Update homeassistant/components/bond/__init__.py
    adding async_unload_entry per PR review suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>

--- homeassistant/components/bond/__init__.py
+++ homeassistant/components/bond/__init__.py
@@ -33,3 +33,20 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         )
 
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
