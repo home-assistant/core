9b77e16ffc3e6b4398befc7ec640d72fe6d2797a
Eugene Prystupa <eugene.prystupa@gmail.com>
2020-07-05 21:17:53 -0400

    Add new integration for Bond hub (#37477)
    * create foundation for Bond integration
    
    * add Bond hub integration (fix lint)
    
    * Update homeassistant/components/bond/__init__.py
    
    adding async_unload_entry per PR review suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * add Bond hub integration (fix missing import after applying PR suggestion)
    
    * Update tests/components/bond/test_init.py
    
    add a unit for unloading per PR review suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update tests/components/bond/test_init.py
    
    add unit test for unload per PR review suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update tests/components/bond/test_init.py
    
    PR review suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update tests/components/bond/test_init.py
    
    PR review suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update tests/components/bond/test_init.py
    
    PR review suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * add Bond hub integration (fix formatting)
    
    * Update homeassistant/components/bond/manifest.json
    
    PR suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update homeassistant/components/bond/manifest.json
    
    PR suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update homeassistant/components/bond/manifest.json
    
    PR suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update homeassistant/components/bond/strings.json
    
    PR suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update homeassistant/components/bond/manifest.json
    
    PR suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update requirements_all.txt
    
    PR suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update homeassistant/components/bond/manifest.json
    
    PR suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update requirements_test_all.txt
    
    PR suggestion
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * add Bond hub integration (remove friendly name from config per PR suggestion)
    
    * Update homeassistant/components/bond/__init__.py
    
    add per PR review feedback
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update homeassistant/components/bond/__init__.py
    
    remove per PR review feedback
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    * Update tests/components/bond/test_init.py
    
    fix unit test
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>

---
+++ tests/components/bond/test_config_flow.py
@@ -0,0 +1,94 @@
+"""Test the Bond config flow."""
+from requests.exceptions import ConnectionError
+from simplejson import JSONDecodeError
+
+from homeassistant import config_entries, setup
+from homeassistant.components.bond.const import DOMAIN
+from homeassistant.const import CONF_ACCESS_TOKEN, CONF_HOST
+
+from tests.async_mock import patch
+
+
+async def test_form(hass):
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == "form"
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.bond.config_flow.Bond.getDeviceIds", return_value=[],
+    ), patch(
+        "homeassistant.components.bond.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.bond.async_setup_entry", return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {CONF_HOST: "1.1.1.1", CONF_ACCESS_TOKEN: "test-token"},
+        )
+
+    assert result2["type"] == "create_entry"
+    assert result2["title"] == "1.1.1.1"
+    assert result2["data"] == {
+        CONF_HOST: "1.1.1.1",
+        CONF_ACCESS_TOKEN: "test-token",
+    }
+    await hass.async_block_till_done()
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_form_invalid_auth(hass):
+    """Test we handle invalid auth."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.bond.config_flow.Bond.getDeviceIds",
+        side_effect=JSONDecodeError("test-message", "test-doc", 0),
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {CONF_HOST: "1.1.1.1", CONF_ACCESS_TOKEN: "test-token"},
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "invalid_auth"}
+
+
+async def test_form_cannot_connect(hass):
+    """Test we handle cannot connect error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.bond.config_flow.Bond.getDeviceIds",
+        side_effect=ConnectionError,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {CONF_HOST: "1.1.1.1", CONF_ACCESS_TOKEN: "test-token"},
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_unexpected_error(hass):
+    """Test we handle unexpected error gracefully."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.bond.config_flow.Bond.getDeviceIds",
+        side_effect=Exception,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {CONF_HOST: "1.1.1.1", CONF_ACCESS_TOKEN: "test-token"},
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "unknown"}
