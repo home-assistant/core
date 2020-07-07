5b2669c9167696623ab492af361c4fa8618fda81
Chris Talkington <chris@talkingtontech.com>
2020-07-06 20:56:33 -0500

    Update test_cover.py

--- tests/components/bond/test_cover.py
+++ tests/components/bond/test_cover.py
@@ -19,18 +19,13 @@ from tests.async_mock import patch
 _LOGGER = logging.getLogger(__name__)
 
 TEST_DEVICE_IDS = ["device-1"]
-TEST_DEVICE = {
-    "name": "name-1",
-    "type": BOND_DEVICE_TYPE_MOTORIZED_SHADES
-}
+TEST_DEVICE = {"name": "name-1", "type": BOND_DEVICE_TYPE_MOTORIZED_SHADES}
 
 
 async def test_entity_registry(hass):
     """Tests that the devices are registered in the entity registry."""
 
-    with patch(
-        "bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS
-    ), patch(
+    with patch("bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS), patch(
         "bond.Bond.getDevice", return_value=TEST_DEVICE
     ):
         await setup_platform(hass, COVER_DOMAIN)
@@ -42,9 +37,7 @@ async def test_entity_registry(hass):
 async def test_open_cover(hass):
     """Tests that open cover command delegates to API."""
 
-    with patch(
-        "bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS
-    ), patch(
+    with patch("bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS), patch(
         "bond.Bond.getDevice", return_value=TEST_DEVICE
     ):
         await setup_platform(hass, COVER_DOMAIN)
@@ -63,9 +56,7 @@ async def test_open_cover(hass):
 async def test_close_cover(hass):
     """Tests that close cover command delegates to API."""
 
-    with patch(
-        "bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS
-    ), patch(
+    with patch("bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS), patch(
         "bond.Bond.getDevice", return_value=TEST_DEVICE
     ):
         await setup_platform(hass, COVER_DOMAIN)
@@ -84,9 +75,7 @@ async def test_close_cover(hass):
 async def test_stop_cover(hass):
     """Tests that stop cover command delegates to API."""
 
-    with patch(
-        "bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS
-    ), patch(
+    with patch("bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS), patch(
         "bond.Bond.getDevice", return_value=TEST_DEVICE
     ):
         await setup_platform(hass, COVER_DOMAIN)
