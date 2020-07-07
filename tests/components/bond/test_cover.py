902b76594408a84cb740ef830f0eed114573deef
Chris Talkington <chris@talkingtontech.com>
2020-07-06 20:37:50 -0500

    Update test_cover.py

--- tests/components/bond/test_cover.py
+++ tests/components/bond/test_cover.py
@@ -19,18 +19,20 @@ from tests.async_mock import patch
 
 _LOGGER = logging.getLogger(__name__)
 
+TEST_DEVICE_IDS = ["device-1"]
+TEST_DEVICE = {
+    "name": "name-1",
+    "type": BOND_DEVICE_TYPE_MOTORIZED_SHADES
+}
+
 
 async def test_entity_registry(hass):
     """Tests that the devices are registered in the entity registry."""
 
     with patch(
-        "homeassistant.components.bond.utils.get_bond_devices",
-        return_value=[
-            BondDevice(
-                "device-1",
-                {"name": "name-1", "type": BOND_DEVICE_TYPE_MOTORIZED_SHADES},
-            )
-        ],
+        "bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS
+    ), patch(
+        "bond.Bond.getDevice", return_value=TEST_DEVICE
     ):
         await setup_platform(hass, COVER_DOMAIN)
 
@@ -42,13 +44,9 @@ async def test_open_cover(hass):
     """Tests that open cover command delegates to API."""
 
     with patch(
-        "homeassistant.components.bond.utils.get_bond_devices",
-        return_value=[
-            BondDevice(
-                "device-1",
-                {"name": "name-1", "type": BOND_DEVICE_TYPE_MOTORIZED_SHADES},
-            )
-        ],
+        "bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS
+    ), patch(
+        "bond.Bond.getDevice", return_value=TEST_DEVICE
     ):
         await setup_platform(hass, COVER_DOMAIN)
 
@@ -67,13 +65,9 @@ async def test_close_cover(hass):
     """Tests that close cover command delegates to API."""
 
     with patch(
-        "homeassistant.components.bond.utils.get_bond_devices",
-        return_value=[
-            BondDevice(
-                "device-1",
-                {"name": "name-1", "type": BOND_DEVICE_TYPE_MOTORIZED_SHADES},
-            )
-        ],
+        "bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS
+    ), patch(
+        "bond.Bond.getDevice", return_value=TEST_DEVICE
     ):
         await setup_platform(hass, COVER_DOMAIN)
 
@@ -92,10 +86,9 @@ async def test_stop_cover(hass):
     """Tests that stop cover command delegates to API."""
 
     with patch(
-        "bond.Bond.getDeviceIds", return_value=["device-1"],
+        "bond.Bond.getDeviceIds", return_value=TEST_DEVICE_IDS
     ), patch(
-        "bond.Bond.getDevice",
-        return_value={"name": "name-1", type: BOND_DEVICE_TYPE_MOTORIZED_SHADES}
+        "bond.Bond.getDevice", return_value=TEST_DEVICE
     ):
         await setup_platform(hass, COVER_DOMAIN)
 
