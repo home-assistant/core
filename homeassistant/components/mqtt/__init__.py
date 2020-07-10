ce7cef767d21cd86f9ffc73998e70b1337cd1197
Erik Montnemery <erik@montnemery.com>
2020-07-10 18:39:18 +0200

    Tweak MQTT availability (#37719)

--- homeassistant/components/mqtt/__init__.py
+++ homeassistant/components/mqtt/__init__.py
@@ -1045,10 +1045,13 @@ class MqttAvailability(Entity):
         """Subscribe MQTT events."""
         await super().async_added_to_hass()
         await self._availability_subscribe_topics()
-        async_dispatcher_connect(self.hass, MQTT_CONNECTED, self.async_mqtt_connect)
-        async_dispatcher_connect(self.hass, MQTT_DISCONNECTED, self.async_mqtt_connect)
         self.async_on_remove(
             async_dispatcher_connect(self.hass, MQTT_CONNECTED, self.async_mqtt_connect)
+        )
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass, MQTT_DISCONNECTED, self.async_mqtt_connect
+            )
         )
 
     async def availability_discovery_update(self, config: dict):
@@ -1116,7 +1119,7 @@ class MqttAvailability(Entity):
     @property
     def available(self) -> bool:
         """Return if the device is available."""
-        if not self.hass.data[DATA_MQTT].connected:
+        if not self.hass.data[DATA_MQTT].connected and not self.hass.is_stopping:
             return False
         return not self._avail_topics or self._available
 
