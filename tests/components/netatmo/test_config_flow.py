a769a11f93b01db05efac29ae01e34e6707d8aec
Tobias Sauerwein <cgtobi@gmail.com>
2020-07-10 06:42:39 +0200

    Fix tests

--- tests/components/netatmo/test_config_flow.py
+++ tests/components/netatmo/test_config_flow.py
@@ -31,7 +31,7 @@ async def test_abort_if_existing_entry(hass):
         "netatmo", context={"source": config_entries.SOURCE_USER}
     )
     assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
-    assert result["reason"] == "missing_configuration"
+    assert result["reason"] == "already_setup"
 
     result = await hass.config_entries.flow.async_init(
         "netatmo",
@@ -39,7 +39,7 @@ async def test_abort_if_existing_entry(hass):
         data={"host": "0.0.0.0", "properties": {"id": "aa:bb:cc:dd:ee:ff"}},
     )
     assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
-    assert result["reason"] == "missing_configuration"
+    assert result["reason"] == "already_setup"
 
 
 async def test_full_flow(hass, aiohttp_client, aioclient_mock):
