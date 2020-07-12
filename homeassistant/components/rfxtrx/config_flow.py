08309c8763a8b008a0cd4609fac1c5fbd0fb45bc
Joakim Plate <elupus@ecce.se>
2020-07-12 22:52:23 +0200

    Add minimal test for config flow

--- homeassistant/components/rfxtrx/config_flow.py
+++ homeassistant/components/rfxtrx/config_flow.py
@@ -2,17 +2,10 @@
 import logging
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_DEVICE, CONF_HOST, CONF_PORT
 
-from . import DOMAIN  # pylint:disable=unused-import
+from . import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
-
-
-def _get_key(config):
-    if CONF_PORT in config:
-        return config[CONF_HOST], config[CONF_PORT]
-    return config[CONF_DEVICE]
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -24,14 +17,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_import(self, import_config=None):
         """Handle the initial step."""
 
-        import_key = _get_key(import_config)
-        entries = self.hass.config_entries.async_entries(DOMAIN)
-        for entry in entries:
-            if _get_key(entry.data) == import_key:
-                if entry.data != import_config:
-                    self.hass.config_entries.async_update_entry(
-                        entry, data=import_config
-                    )
-                return self.async_abort(reason="already_configured")
-
+        await self.async_set_unique_id(DOMAIN)
+        self._abort_if_unique_id_configured(import_config)
         return self.async_create_entry(title="RFXTRX", data=import_config)
