00ac46730f44040a106c2abeb47ce1104002173b
J. Nick Koston <nick@koston.org>
2020-07-12 22:12:00 +0000

    adj

--- homeassistant/components/logbook/__init__.py
+++ homeassistant/components/logbook/__init__.py
@@ -198,16 +198,6 @@ class LogbookView(HomeAssistantView):
 
         hass = request.app["hass"]
 
-        import cProfile
-        import asyncio
-
-        pr = cProfile.Profile()
-        pr.enable()
-        await asyncio.sleep(300)
-        pr.disable()
-        pr.create_stats()
-        pr.dump_stats("300.cprof")
-
         def json_events():
             """Fetch events and generate JSON."""
             return self.json(
