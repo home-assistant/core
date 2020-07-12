ac28c3836541c4d7f1dca25da63a1dbbba21a276
Matt Zimmerman <mdz@alcor.net>
2020-07-11 22:07:23 -0700

    suppress pylint warning
    https://github.com/PyCQA/pylint/issues/1630

--- homeassistant/components/smarttub/config_flow.py
+++ homeassistant/components/smarttub/config_flow.py
@@ -8,7 +8,7 @@ from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, CONF_SCAN_INTERVAL
 from homeassistant.core import callback
 
 from .api import validate_credentials
-from .const import DEFAULT_SCAN_INTERVAL, DOMAIN
+from .const import DEFAULT_SCAN_INTERVAL, DOMAIN  # pylint: disable=unused-import
 
 DATA_SCHEMA = vol.Schema(
     {vol.Required(CONF_EMAIL): str, vol.Required(CONF_PASSWORD): str}
