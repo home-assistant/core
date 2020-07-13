3d4da38cabfeafe4080cf1c0fedaff0532f9c4c1
Matt Zimmerman <mdz@alcor.net>
2020-07-12 18:08:57 -0700

    pylint

--- homeassistant/components/smarttub/helpers.py
+++ homeassistant/components/smarttub/helpers.py
@@ -5,8 +5,10 @@ from homeassistant.config_entries import SOURCE_IMPORT
 from .const import DOMAIN
 
 
-def create_config_flow(hass, data={}):
+def create_config_flow(hass, data=None):
     """Create a new config flow to set up the SmartTub integration."""
+    if data is None:
+        data = {}
     hass.async_create_task(
         hass.config_entries.flow.async_init(
             DOMAIN, context={"source": SOURCE_IMPORT}, data=data,
