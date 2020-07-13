ab252b9b2050f88c3163702d5ab206e85ca91ded
Bram Devooght <b.devooght@smappee.com>
2020-07-13 12:52:14 +0200

    resolve test

--- homeassistant/components/smappee/config_flow.py
+++ homeassistant/components/smappee/config_flow.py
@@ -37,6 +37,9 @@ class SmappeeFlowHandler(
             # We currently only support Energy and Solar models (legacy)
             return self.async_abort(reason="invalid mDNS")
 
+        # pylint: E1101: Instance of 'SmappeeFlowHandler' has no 'context' member (no-member)
+        self.context = {"source": "zeroconf"}
+
         self.context.update(
             {
                 CONF_HOSTNAME: discovery_info[CONF_HOSTNAME],
