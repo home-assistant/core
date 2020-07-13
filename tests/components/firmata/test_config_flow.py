868b18c33dd22f692d541f7a7a918a42230c2c36
DaAwesomeP <DaAwesomeP@users.noreply.github.com>
2020-07-12 21:34:22 -0400

    firmata improve test mocks

--- tests/components/firmata/test_config_flow.py
+++ tests/components/firmata/test_config_flow.py
@@ -1,9 +1,10 @@
 """Test the Firmata config flow."""
 from homeassistant import config_entries, setup
 from homeassistant.components.firmata.const import CONF_SERIAL_PORT, DOMAIN
+from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
 
-from tests.async_mock import patch
+from tests.async_mock import AsyncMock, patch
 
 
 async def test_import_cannot_connect(hass: HomeAssistant) -> None:
@@ -11,7 +12,8 @@ async def test_import_cannot_connect(hass: HomeAssistant) -> None:
     await setup.async_setup_component(hass, "persistent_notification", {})
 
     with patch(
-        "homeassistant.components.firmata.board.get_board", side_effect=RuntimeError,
+        "homeassistant.components.firmata.board.PymataExpress.start_aio",
+        side_effect=RuntimeError,
     ):
         result = await hass.config_entries.flow.async_init(
             DOMAIN,
@@ -28,24 +30,28 @@ async def test_import(hass: HomeAssistant) -> None:
     """Test we create an entry from config."""
     await setup.async_setup_component(hass, "persistent_notification", {})
 
-    async def mock_get_board(config: dict):
-        return MockPymata()
-
-    class MockPymata:
-        """A fake test board instance."""
-
-        async def shutdown(self):
-            """Shut down the fake board."""
-
     with patch(
-        "homeassistant.components.firmata.config_flow.get_board",
-        side_effect=mock_get_board,
-    ):
+        "homeassistant.components.firmata.board.PymataExpress"
+    ) as PymataExpress, patch(  # pylint: disable=invalid-name
+        "homeassistant.components.firmata.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.firmata.async_setup_entry", return_value=True,
+    ) as mock_setup_entry:
+        PymataExpress().start_aio = AsyncMock()
+        PymataExpress().shutdown = AsyncMock()
+
         result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_IMPORT},
             data={CONF_SERIAL_PORT: "/dev/nonExistent"},
         )
 
-    assert result["type"] == "create_entry"
-    await hass.async_block_till_done()
+        assert result["type"] == "create_entry"
+        assert result["title"] == "serial-/dev/nonExistent"
+        assert result["data"] == {
+            CONF_NAME: "serial-/dev/nonExistent",
+            CONF_SERIAL_PORT: "/dev/nonExistent",
+        }
+        await hass.async_block_till_done()
+        assert len(mock_setup.mock_calls) == 1
+        assert len(mock_setup_entry.mock_calls) == 1
