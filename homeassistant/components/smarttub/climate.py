845a7f28f2994358c43b5341870426bdff86f063
Matt Zimmerman <mdz@alcor.net>
2020-07-12 17:37:04 -0700

    replace ValueError with NotImplementedError

--- homeassistant/components/smarttub/climate.py
+++ homeassistant/components/smarttub/climate.py
@@ -52,7 +52,7 @@ class SmartTubThermostat(SmartTubEntity, ClimateEntity):
             return CURRENT_HVAC_HEAT
         if heater_status == "OFF":
             return CURRENT_HVAC_IDLE
-        raise ValueError(f"unknown heater status: {heater_status}")
+        raise NotImplementedError(f"unknown heater status: {heater_status}")
 
     @property
     def hvac_modes(self):
