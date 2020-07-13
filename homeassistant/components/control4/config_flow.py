a05b46868ab04c06e0f459d70a5825861c1f29a3
lawtancool <26829131+lawtancool@users.noreply.github.com>
2020-07-13 05:40:53 +0000

    remove import from asyncio.exceptions apparently exceptions are supposed to be imported directly from asyncio

--- homeassistant/components/control4/config_flow.py
+++ homeassistant/components/control4/config_flow.py
@@ -1,5 +1,5 @@
 """Config flow for Control4 integration."""
-from asyncio import exceptions as asyncioExceptions
+from asyncio import TimeoutError as asyncioTimeoutError
 import logging
 
 from aiohttp.client_exceptions import ClientError
@@ -86,7 +86,7 @@ class Control4Validator:
             self.director = C4Director(self.host, self.director_bearer_token)
             await self.director.getAllItemInfo()
             return True
-        except (Unauthorized, ClientError, asyncioExceptions.TimeoutError) as exception:
+        except (Unauthorized, ClientError, asyncioTimeoutError) as exception:
             _LOGGER.error(exception)
             return False
 
