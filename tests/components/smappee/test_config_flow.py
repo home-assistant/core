ad68ffd6a4574154bc4d5f134bc407ebaa8a492a
Bram Devooght <b.devooght@smappee.com>
2020-07-14 11:30:39 +0200

    use core async_init methods

--- tests/components/smappee/test_config_flow.py
+++ tests/components/smappee/test_config_flow.py
@@ -1,16 +1,13 @@
 """Test the Smappee config flow."""
 from homeassistant import config_entries, data_entry_flow, setup
-from homeassistant.components.smappee import config_flow
 from homeassistant.components.smappee.const import (
     AUTHORIZE_URL,
     CONF_HOSTNAME,
-    CONF_SERIALNUMBER,
-    CONF_TITLE,
     DOMAIN,
     TOKEN_URL,
 )
 from homeassistant.config_entries import SOURCE_ZEROCONF
-from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_IP_ADDRESS
+from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET
 from homeassistant.helpers import config_entry_oauth2_flow
 
 from tests.async_mock import patch
@@ -21,13 +18,18 @@ CLIENT_SECRET = "5678"
 
 async def test_show_zeroconf_confirm_form(hass, aiohttp_client, aioclient_mock):
     """Test that the zeroconf confirmation form is served."""
-    flow = config_flow.SmappeeFlowHandler()
-    flow.hass = hass
-    flow.context = {
-        "source": SOURCE_ZEROCONF,
-        CONF_HOSTNAME: "Smappee1006000212.local.",
-    }
-    result = await flow.async_step_zeroconf_confirm()
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_ZEROCONF},
+        data={
+            "host": "1.2.3.4",
+            "port": 22,
+            CONF_HOSTNAME: "Smappee1006000212.local.",
+            "type": "_ssh._tcp.local.",
+            "name": "Smappee1006000212._ssh._tcp.local.",
+            "properties": {"_raw": {}},
+        },
+    )
 
     assert result["step_id"] == "zeroconf_confirm"
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
@@ -35,34 +37,28 @@ async def test_show_zeroconf_confirm_form(hass, aiohttp_client, aioclient_mock):
 
 async def test_show_zerconf_form(hass, aiohttp_client, aioclient_mock):
     """Test that the zeroconf confirmation form is served."""
-    flow = config_flow.SmappeeFlowHandler()
-    flow.hass = hass
-    flow.context = {"source": SOURCE_ZEROCONF}
-    result = await flow.async_step_zeroconf(
-        {
-            "host": "192.168.0.0",
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_ZEROCONF},
+        data={
+            "host": "1.2.3.4",
             "port": 22,
-            "hostname": "Smappee1006000212.local.",
+            CONF_HOSTNAME: "Smappee1006000212.local.",
             "type": "_ssh._tcp.local.",
             "name": "Smappee1006000212._ssh._tcp.local.",
             "properties": {"_raw": {}},
-        }
+        },
     )
 
-    assert flow.context[CONF_HOSTNAME] == "Smappee1006000212.local."
-    assert flow.context[CONF_IP_ADDRESS] == "192.168.0.0"
-    assert flow.context[CONF_TITLE] == "Smappee1006000212"
-    assert flow.context[CONF_SERIALNUMBER] == "1006000212"
     assert result["step_id"] == "zeroconf_confirm"
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
 
 
 async def test_zeroconf_no_data(hass, aiohttp_client, aioclient_mock):
     """Test we abort if zeroconf provides no data."""
-    flow = config_flow.SmappeeFlowHandler()
-    flow.hass = hass
-    flow.context = {"source": SOURCE_ZEROCONF}
-    result = await flow.async_step_zeroconf(None)
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_ZEROCONF}
+    )
 
     assert result["reason"] == "connection_error"
     assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
@@ -70,18 +66,17 @@ async def test_zeroconf_no_data(hass, aiohttp_client, aioclient_mock):
 
 async def test_zerconf_wrong_mdns(hass, aiohttp_client, aioclient_mock):
     """Test we abort if unsupported mDNS name is discovered."""
-    flow = config_flow.SmappeeFlowHandler()
-    flow.hass = hass
-    flow.context = {"source": SOURCE_ZEROCONF}
-    result = await flow.async_step_zeroconf(
-        {
-            "host": "192.168.0.0",
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_ZEROCONF},
+        data={
+            "host": "1.2.3.4",
             "port": 22,
-            "hostname": "example.local.",
+            CONF_HOSTNAME: "example.local.",
             "type": "_ssh._tcp.local.",
-            "name": "Example._ssh._tcp.local.",
+            "name": "example._ssh._tcp.local.",
             "properties": {"_raw": {}},
-        }
+        },
     )
 
     assert result["reason"] == "invalid_mdns"
