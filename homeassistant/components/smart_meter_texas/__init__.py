eef359639d3bf346effc0a98ce5c14358021d197
Graham Wetzler <graham@wetzler.dev>
2020-07-14 22:45:30 +0000

    Initial commit

--- homeassistant/components/smart_meter_texas/__init__.py
+++ homeassistant/components/smart_meter_texas/__init__.py
@@ -1,29 +1,80 @@
 """The Smart Meter Texas integration."""
 import asyncio
+from datetime import timedelta
+import logging
 
+from smart_meter_texas.async_api import Auth, Meter
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import aiohttp_client
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from .const import DOMAIN
+from .const import DOMAIN, UPDATE_INTERVAL
 
-CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
+_LOGGER = logging.getLogger(__name__)
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
-PLATFORMS = ["light"]
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_USERNAME): str,
+                vol.Required(CONF_PASSWORD): str,
+                vol.Required("meter"): str,
+                vol.Required("esiid"): str,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+PLATFORMS = ["sensor"]
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Smart Meter Texas component."""
+
+    hass.data.setdefault(DOMAIN, {})
+    conf = config.get(DOMAIN)
+
+    if not conf:
+        return True
+
+    hass.async_create_task(hass.config_entries.flow.async_init(DOMAIN))
+
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Smart Meter Texas from a config entry."""
-    # TODO Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
+
+    entry_data = entry.data
+    websession = aiohttp_client.async_get_clientsession(hass)
+
+    async def async_update_data():
+        auth = Auth(websession, entry_data[CONF_USERNAME], entry_data[CONF_PASSWORD])
+        await auth.authenticate()
+        meter = Meter(auth, entry_data["esiid"], entry_data["meter"])
+        await meter.async_read_meter()
+        return meter
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="Smart Meter Texas read meter",
+        update_method=async_update_data,
+        update_interval=timedelta(minutes=UPDATE_INTERVAL),
+    )
+
+    await coordinator.async_refresh()
+
+    if not coordinator.last_update_success:
+        raise ConfigEntryNotReady
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
 
     for component in PLATFORMS:
         hass.async_create_task(
---
+++ homeassistant/components/smart_meter_texas/sensor.py
@@ -0,0 +1,85 @@
+"""Support for Smart Meter Texas sensors."""
+import logging
+
+from homeassistant.const import ENERGY_KILO_WATT_HOUR
+from homeassistant.helpers.entity import Entity
+
+from .const import DOMAIN, LAST_UPDATED
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Smart Meter Texas sensors."""
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    meter_number = config_entry.data["meter"]
+
+    async_add_entities([SmartMeterTexasSensor(coordinator, meter_number)], True)
+
+
+class SmartMeterTexasSensor(Entity):
+    """Representation of an Smart Meter Texas sensor."""
+
+    def __init__(self, coordinator, meter_number):
+        """Initialize the sensor."""
+        self._coordinator = coordinator
+        self._meter_number = meter_number
+        self._last_updated = coordinator.data.reading_datetime
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return ENERGY_KILO_WATT_HOUR
+
+    @property
+    def name(self):
+        """Device Name."""
+        return f"Electric meter {self._meter_number}"
+
+    @property
+    def icon(self):
+        """Device Ice."""
+        return "mdi:counter"
+
+    @property
+    def unique_id(self):
+        """Device Uniqueid."""
+        return f"electric_meter_{self._meter_number}"
+
+    @property
+    def available(self):
+        """Return True if entity is available."""
+        return self._coordinator.last_update_success
+
+    @property
+    def state(self):
+        """Get the current price."""
+        return self._coordinator.data.reading
+
+    @property
+    def device_state_attributes(self):
+        """Return the device specific state attributes."""
+        attributes = {}
+
+        if self._last_updated is not None:
+            attributes[LAST_UPDATED] = self._last_updated
+
+        return attributes
+
+    @property
+    def should_poll(self):
+        """Return False, updates are controlled via coordinator."""
+        return False
+
+    async def async_update(self):
+        """Update the entity.
+
+        Only used by the generic entity update service.
+        """
+        await self._coordinator.async_request_refresh()
+
+    async def async_added_to_hass(self):
+        """Subscribe to updates."""
+        self.async_on_remove(
+            self._coordinator.async_add_listener(self.async_write_ha_state)
+        )
