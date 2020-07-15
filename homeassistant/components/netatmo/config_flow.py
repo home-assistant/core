9af83bec337bbb8d92ddb4be5245810837e2621f
Tobias Sauerwein <cgtobi@gmail.com>
2020-07-15 08:33:30 +0200

    Add docstring

--- homeassistant/components/netatmo/config_flow.py
+++ homeassistant/components/netatmo/config_flow.py
@@ -127,6 +127,7 @@ class NetatmoOptionsFlowHandler(config_entries.OptionsFlow):
         if user_input is not None and CONF_NEW_AREA not in user_input:
 
             def fix_coordinates(user_input):
+                """Ensure coordinates have acceptable length for the Netatmo API."""
                 for coordinate in [CONF_LAT_NE, CONF_LAT_SW, CONF_LON_NE, CONF_LON_SW]:
                     if len(str(user_input[coordinate]).split(".")[1]) < 7:
                         user_input[coordinate] = user_input[coordinate] + 0.0000001
