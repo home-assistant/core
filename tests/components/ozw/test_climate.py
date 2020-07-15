85d5ead0dc0eb73edfb599cdb7745f6ec262afd1
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-07-15 23:54:46 +0200

    update tests
    code coverage

--- tests/components/ozw/test_climate.py
+++ tests/components/ozw/test_climate.py
@@ -6,6 +6,7 @@ from homeassistant.components.climate.const import (
     ATTR_FAN_MODES,
     ATTR_HVAC_ACTION,
     ATTR_HVAC_MODES,
+    ATTR_PRESET_MODE,
     ATTR_PRESET_MODES,
     ATTR_TARGET_TEMP_HIGH,
     ATTR_TARGET_TEMP_LOW,
@@ -226,9 +227,12 @@ async def test_climate(hass, climate_data, sent_messages, climate_msg, caplog):
     assert state.attributes[ATTR_HVAC_MODES] == [
         HVAC_MODE_HEAT,
     ]
+    assert state.attributes.get(ATTR_CURRENT_TEMPERATURE) is None
     assert round(state.attributes[ATTR_TEMPERATURE], 0) == 21
     assert state.attributes.get(ATTR_TARGET_TEMP_LOW) is None
     assert state.attributes.get(ATTR_TARGET_TEMP_HIGH) is None
+    assert state.attributes.get(ATTR_PRESET_MODE) is None
+    assert state.attributes.get(ATTR_PRESET_MODES) is None
 
     # Test set target temperature
     await hass.services.async_call(
@@ -247,3 +251,15 @@ async def test_climate(hass, climate_data, sent_messages, climate_msg, caplog):
         "Value": 28.0,
         "ValueIDKey": 281475116220434,
     }
+
+    await hass.services.async_call(
+        "climate",
+        "set_hvac_mode",
+        {
+            "entity_id": "climate.danfoss_living_connect_z_v1_06_014g0013_heating_1",
+            "hvac_mode": HVAC_MODE_HEAT,
+        },
+        blocking=True,
+    )
+    assert len(sent_messages) == 9
+    assert "does not support setting a mode" in caplog.text
