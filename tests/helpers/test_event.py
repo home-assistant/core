5281dc87f26cc324caf8792100004de9a077f518
J. Nick Koston <nick@koston.org>
2020-07-16 23:54:59 +0000

    debug

--- tests/helpers/test_event.py
+++ tests/helpers/test_event.py
@@ -1013,7 +1013,7 @@ async def test_async_call_later(hass):
     assert remove is mock()
 
 
-async def test_track_state_change_event_chain(hass):
+async def test_track_state_change_event_chain_multple_entity(hass):
     """Test that adding a new state tracker inside a tracker does not fire right away."""
     tracker_called = []
     chained_tracker_called = []
@@ -1037,12 +1037,64 @@ async def test_track_state_change_event_chain(hass):
 
         chained_tracker_unsub.append(
             async_track_state_change_event(
-                hass, ["light.bowl"], chained_single_run_callback
+                hass, ["light.bowl", "light.top"], chained_single_run_callback
             )
         )
 
     tracker_unsub.append(
-        async_track_state_change_event(hass, ["light.bowl"], single_run_callback)
+        async_track_state_change_event(
+            hass, ["light.bowl", "light.top"], single_run_callback
+        )
+    )
+
+    hass.states.async_set("light.bowl", "on")
+    hass.states.async_set("light.top", "on")
+    await hass.async_block_till_done()
+
+    assert len(tracker_called) == 2
+    assert len(chained_tracker_called) == 1
+    assert len(tracker_unsub) == 1
+    assert len(chained_tracker_unsub) == 2
+
+    hass.states.async_set("light.bowl", "off")
+    await hass.async_block_till_done()
+
+    assert len(tracker_called) == 3
+    assert len(chained_tracker_called) == 3
+    assert len(tracker_unsub) == 1
+    assert len(chained_tracker_unsub) == 3
+
+
+async def test_track_state_change_event_chain_single_entity(hass):
+    """Test that adding a new state tracker inside a tracker does not fire right away."""
+    tracker_called = []
+    chained_tracker_called = []
+
+    chained_tracker_unsub = []
+    tracker_unsub = []
+
+    @ha.callback
+    def chained_single_run_callback(event):
+        old_state = event.data.get("old_state")
+        new_state = event.data.get("new_state")
+
+        chained_tracker_called.append((old_state, new_state))
+
+    @ha.callback
+    def single_run_callback(event):
+        old_state = event.data.get("old_state")
+        new_state = event.data.get("new_state")
+
+        tracker_called.append((old_state, new_state))
+
+        chained_tracker_unsub.append(
+            async_track_state_change_event(
+                hass, "light.bowl", chained_single_run_callback
+            )
+        )
+
+    tracker_unsub.append(
+        async_track_state_change_event(hass, "light.bowl", single_run_callback)
     )
 
     hass.states.async_set("light.bowl", "on")
