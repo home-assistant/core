03807665871bee4a61cfa5d20cc5d7d33019dd4b
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2020-07-19 16:23:10 +0200

    Get folder states

--- homeassistant/components/syncthing/__init__.py
+++ homeassistant/components/syncthing/__init__.py
@@ -1,6 +1,7 @@
 """The syncthing integration."""
 import asyncio
 
+import syncthing
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
@@ -20,8 +21,21 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up syncthing from a config entry."""
-    # TODO Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
+    data = entry.data
+
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        "system": syncthing.System(data["token"], host=data["host"], port=data["port"]),
+        "database": syncthing.Database(
+            data["token"], host=data["host"], port=data["port"]
+        ),
+        "statistics": syncthing.Statistics(
+            data["token"], host=data["host"], port=data["port"]
+        ),
+        "events": syncthing.Events(data["token"], host=data["host"], port=data["port"]),
+    }
 
     for component in PLATFORMS:
         hass.async_create_task(
--- homeassistant/components/syncthing/sensor.py
+++ homeassistant/components/syncthing/sensor.py
@@ -1,1 +1,82 @@
 """Support for monitoring the Syncthing instance."""
+
+import logging
+
+import syncthing
+
+from homeassistant.const import CONF_NAME
+from homeassistant.helpers.entity import Entity
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Syncthing sensors."""
+
+    syncthing = hass.data[DOMAIN][config_entry.entry_id]
+    system = syncthing["system"]
+    name = config_entry.data[CONF_NAME]
+
+    config = await hass.async_add_executor_job(system.config)
+
+    dev = []
+
+    for folder in config["folders"]:
+        dev.append(FolderSensor(hass, syncthing, name, folder))
+
+    async_add_entities(dev, True)
+
+
+class FolderSensor(Entity):
+    """A Syncthing folder sensor."""
+
+    def __init__(self, hass, syncthing, client_name, folder):
+        """Initialize the sensor."""
+        self._hass = hass
+        self._syncthing = syncthing
+        self._client_name = client_name
+        self._folder = folder
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return (
+            f"{DOMAIN} {self._client_name} {self._folder['id']} {self._folder['label']}"
+        )
+
+    @property
+    def unique_id(self):
+        """Return the unique id of the entity."""
+        return f"{DOMAIN}-{self._client_name}-{self._folder['id']}"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state["state"]
+
+    @property
+    def available(self):
+        """Could the device be accessed during the last update call."""
+        return self._state is not None
+
+    @property
+    def icon(self):
+        """Return the icon for this sensor."""
+        return "mdi:folder"
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return self._state
+
+    async def async_update(self):
+        """Update device state."""
+        try:
+            self._state = await self._hass.async_add_executor_job(
+                self._syncthing["database"].status, self._folder["id"]
+            )
+        except syncthing.SyncthingError:
+            self._state = None
