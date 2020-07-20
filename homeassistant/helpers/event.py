7dcf0e98cbd6f5908c94fb1d3ec2608cbfa4e162
J. Nick Koston <nick@koston.org>
2020-07-20 23:37:00 +0000

    Revert "simplify since the event loop time cannot move backwards"
    This reverts commit 3b13714ef489b5c5661ae0254b12f137d047fc26.

--- homeassistant/helpers/event.py
+++ homeassistant/helpers/event.py
@@ -594,31 +594,37 @@ def async_track_utc_time_change(
     matching_minutes = dt_util.parse_time_expression(minute, 0, 59)
     matching_hours = dt_util.parse_time_expression(hour, 0, 23)
 
-    def calculate_next(now: datetime) -> datetime:
+    last_now: datetime = dt_util.utcnow()
+    next_time: datetime = last_now
+
+    def calculate_next(now: datetime) -> None:
         """Calculate and set the next time the trigger should fire."""
+        nonlocal next_time
 
         localized_now = dt_util.as_local(now) if local else now
-        return dt_util.find_next_time_expression_time(
+        next_time = dt_util.find_next_time_expression_time(
             localized_now, matching_seconds, matching_minutes, matching_hours
         )
 
     # Make sure rolling back the clock doesn't prevent the timer from
     # triggering.
     cancel_callback: Optional[asyncio.TimerHandle] = None
-    next_time: datetime = calculate_next(dt_util.utcnow())
+    calculate_next(last_now)
 
     @callback
     def pattern_time_change_listener() -> None:
         """Listen for matching time_changed events."""
-        nonlocal next_time, cancel_callback
+        nonlocal next_time, last_now, cancel_callback
 
         now = pattern_utc_now()
         hass.async_run_job(action, dt_util.as_local(now) if local else now)
 
         if next_time <= now:
-            next_time = calculate_next(now + timedelta(seconds=1))
+            calculate_next(now + timedelta(seconds=1))
         else:
-            next_time = calculate_next(now)
+            calculate_next(now)
+
+        last_now = now
 
         cancel_callback = hass.loop.call_at(
             hass.loop.time() + next_time.timestamp() - time.time(),
