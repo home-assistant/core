496fb2e62a0d6ca122a954e070b274831b322fa2
David F. Mulcahey <david.mulcahey@me.com>
2020-07-20 10:04:57 -0400

    Cleanup async_accept_signal in ZHA (#38009)

--- homeassistant/components/zha/lock.py
+++ homeassistant/components/zha/lock.py
@@ -60,7 +60,7 @@ class ZhaDoorLock(ZhaEntity, LockEntity):
     async def async_added_to_hass(self):
         """Run when about to be added to hass."""
         await super().async_added_to_hass()
-        await self.async_accept_signal(
+        self.async_accept_signal(
             self._doorlock_channel, SIGNAL_ATTR_UPDATED, self.async_set_state
         )
 
