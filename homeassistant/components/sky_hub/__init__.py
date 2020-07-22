2716afc8617942b19bb0ffb93976a7694d1ef064
RogerSelwyn <rogers@selwyn.org.uk>
2020-07-22 14:03:34 +0100

    Fix comments from review

--- homeassistant/components/sky_hub/__init__.py
+++ homeassistant/components/sky_hub/__init__.py
@@ -1,20 +1,8 @@
 """The sky_hub component."""
-from pyskyqhub.skyq_hub import SkyQHub
-
-from homeassistant.components.device_tracker import DOMAIN
-from homeassistant.const import CONF_HOST
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 PLATFORMS = ["device_tracker"]
 
 
 async def async_setup(hass, config):
-    """Set up the sky)hub component."""
-    conf = config[DOMAIN][0]
-
-    host = conf.get(CONF_HOST, "192.168.1.254")
-    websession = async_get_clientsession(hass)
-    hub = SkyQHub(websession, host)
-    await hub.async_connect()
-
-    return hub.success_init
+    """Set up the sky_hub component."""
+    return True
--- homeassistant/components/sky_hub/device_tracker.py
+++ homeassistant/components/sky_hub/device_tracker.py
@@ -14,8 +14,6 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
-_ERROR = logging.ERROR
-_INFO = logging.INFO
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({vol.Optional(CONF_HOST): cv.string})
 
@@ -25,61 +23,40 @@ async def async_get_scanner(hass, config):
     host = config[DOMAIN].get(CONF_HOST, "192.168.1.254")
     websession = async_get_clientsession(hass)
     hub = SkyQHub(websession, host)
-    scanner = SkyHubDeviceScanner(config[DOMAIN], hub, host)
-    await scanner.async_connect(hass)
-    return scanner if scanner.success_init else None
+
+    _LOGGER.info("Initialising Sky Hub")
+    await hub.async_connect()
+    if hub.success_init:
+        scanner = SkyHubDeviceScanner(config[DOMAIN], hub)
+        return scanner
+    else:
+        return None
 
 
 class SkyHubDeviceScanner(DeviceScanner):
     """This class queries a Sky Hub router."""
 
-    def __init__(self, config, hub, host):
+    def __init__(self, config, hub):
         """Initialise the scanner."""
-        self._connection_failed = False
-        self._dataparse_failed = False
         self._hub = hub
-        self.host = host
         self.last_results = {}
-        self.success_init = False
-
-    async def async_connect(self, hass):
-        """Test the router is accessible."""
-        _LOGGER.info("Initialising Sky Hub")
-        await self._hub.async_connect()
-        self.success_init = self._hub.success_init
 
     async def async_scan_devices(self):
         """Scan for new devices and return a list with found device IDs."""
         await self._async_update_info()
-
-        return (device for device in self.last_results)
+        return list(device for device in self.last_results)
 
     async def async_get_device_name(self, device):
-        """Return the name of the given device or None if we don't know."""
-        # If not initialised and not already scanned and not found.
-        if device not in self.last_results:
-            await self._async_update_info()
-
-            if not self.last_results:
-                return None
-
+        """Return the name of the given device."""
         return self.last_results.get(device)
 
     async def _async_update_info(self):
-        """Ensure the information from the Sky Hub is up to date.
-
-        Return boolean if scanning successful.
-        """
-        if not self.success_init:
-            return False
-
+        """Ensure the information from the Sky Hub is up to date."""
         _LOGGER.info("Scanning")
 
         data = await self._hub.async_get_skyhub_data()
 
         if not data:
-            return False
+            return
 
         self.last_results = data
-
-        return True
