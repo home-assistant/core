bf57b0cafd5c1333e648dbcf99bf1ae9d200dce0
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2020-07-22 21:39:13 +0200

    Listen to syncthing events in a separate thread

--- homeassistant/components/syncthing/__init__.py
+++ homeassistant/components/syncthing/__init__.py
@@ -1,11 +1,19 @@
 """The syncthing integration."""
 import asyncio
+import logging
+import threading
 
 import syncthing
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, CONF_PORT, CONF_TOKEN
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_PORT,
+    CONF_TOKEN,
+    EVENT_HOMEASSISTANT_START,
+    EVENT_HOMEASSISTANT_STOP,
+)
 from homeassistant.core import HomeAssistant
 
 from .const import CONF_USE_HTTPS, DOMAIN
@@ -13,6 +21,8 @@ from .const import CONF_USE_HTTPS, DOMAIN
 CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
 PLATFORMS = ["sensor"]
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
@@ -27,17 +37,31 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     if DOMAIN not in hass.data:
         hass.data[DOMAIN] = {}
 
-    hass.data[DOMAIN][entry.entry_id] = syncthing.Syncthing(
+    client = syncthing.Syncthing(
         data[CONF_TOKEN],
         host=data[CONF_HOST],
         port=data[CONF_PORT],
         is_https=data[CONF_USE_HTTPS],
     )
 
+    hass.data[DOMAIN][entry.entry_id] = client
+
     for component in PLATFORMS:
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, component)
         )
+
+    event_thread = EventThread(hass, client)
+
+    def start_event_thread(_):
+        event_thread.start()
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, start_event_thread)
+
+    def stop_event_thread(_):
+        event_thread.stop()
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, stop_event_thread)
 
     return True
 
@@ -56,3 +80,33 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+class EventThread(threading.Thread):
+    """A threaded event handler class."""
+
+    def __init__(self, hass, client):
+        """Initialize the listener."""
+        super().__init__()
+        self._hass = hass
+        self._client = client
+        self._events_stream = None
+
+    def run(self):
+        """Listen to syncthing events."""
+        _LOGGER.info("Starting the syncthing event listener...")
+        self._events_stream = self._client.events()
+        for event in self._events_stream:
+            pass
+            _LOGGER.warn("EVENT ARRIVED!")
+
+    def stop(self):
+        """Stop listening to syncthing events."""
+        _LOGGER.info("Stopping the syncthing event listener...")
+        if self._events_stream is None:
+            _LOGGER.info("The syncthing event listener was not running.")
+            return
+
+        self._events_stream.stop()
+        self._events_stream = None
+        _LOGGER.info("The syncthing event listener stopped.")
