b15dad8c4bbec82817be8b6a8d8fb560e4078242
Franck Nijhof <git@frenck.dev>
2020-07-22 23:56:28 +0200

    Upgrade aiohttp to 3.6.2 (#38082)

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ PROJECT_URLS = {
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
 
 REQUIRES = [
-    "aiohttp==3.6.1",
+    "aiohttp==3.6.2",
     "astral==1.10.1",
     "async_timeout==3.0.1",
     "attrs==19.3.0",
