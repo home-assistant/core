fa4ac88602160c2f5d12a78576605f244acf6cd3
Eugene Prystupa <eugene.prystupa@gmail.com>
2020-07-22 21:38:18 -0400

    Refactor bond integration to centralize update state logic in single superclass

--- homeassistant/components/bond/entity.py
+++ homeassistant/components/bond/entity.py
@@ -1,4 +1,5 @@
 """An abstract class common to all Bond entities."""
+from abc import ABC, abstractmethod
 from typing import Any, Dict, Optional
 
 from homeassistant.const import ATTR_NAME
@@ -7,7 +8,7 @@ from .const import DOMAIN
 from .utils import BondDevice, BondHub
 
 
-class BondEntity:
+class BondEntity(ABC):
     """Generic Bond entity encapsulating common features of any Bond controlled device."""
 
     def __init__(self, hub: BondHub, device: BondDevice):
@@ -38,3 +39,12 @@ class BondEntity:
     def assumed_state(self) -> bool:
         """Let HA know this entity relies on an assumed state tracked by Bond."""
         return True
+
+    async def async_update(self):
+        """Fetch assumed state of the cover from the hub using API."""
+        state: dict = await self._hub.bond.device_state(self._device.device_id)
+        self._apply_state(state)
+
+    @abstractmethod
+    def _apply_state(self, state: dict):
+        raise NotImplementedError
