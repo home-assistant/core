d1d3df978c8332df430d58417c805465bc8cc285
Justin Wong <46082645+uvjustin@users.noreply.github.com>
2020-07-24 02:36:16 +0000

    Change find_box to generator

--- homeassistant/components/stream/hls.py
+++ homeassistant/components/stream/hls.py
@@ -155,51 +155,48 @@ class HlsStreamOutput(StreamOutput):
         return {"movflags": "empty_moov+default_base_moof"}
 
 
-def find_box(
-    segment: io.BytesIO, target_type: bytes, box_start: int = 0, nth_box: int = 1
-) -> int:
+def find_box(segment: io.BytesIO, target_type: bytes, box_start: int = 0) -> int:
     """Find location of first box (or sub_box if box_start provided) of given type."""
-    times_found = 0
     if box_start == 0:
-        segment_end = len(segment.getbuffer())
+        box_end = len(segment.getbuffer())
         index = 0
     else:
         segment.seek(box_start)
-        segment_end = box_start + int.from_bytes(segment.read(4), byteorder="big")
+        box_end = box_start + int.from_bytes(segment.read(4), byteorder="big")
         index = box_start + 8
     while 1:
-        if index > segment_end - 8:  # End of segment, not found
-            return None
+        if index > box_end - 8:  # End of box, not found
+            break
         segment.seek(index)
         box_header = segment.read(8)
         if box_header[4:8] == target_type:
-            times_found += 1
-        if times_found == nth_box:
-            return index
+            yield index
+            segment.seek(index)
         index += int.from_bytes(box_header[0:4], byteorder="big")
 
 
 def get_init(segment: io.BytesIO) -> bytes:
     """Get init section from fragmented mp4."""
-    moof_location = find_box(segment, b"moof")
+    moof_location = next(find_box(segment, b"moof"))
     segment.seek(0)
     return segment.read(moof_location)
 
 
 def get_m4s(segment: io.BytesIO, start_pts: tuple, sequence: int) -> bytes:
     """Get m4s section from fragmented mp4."""
-    moof_location = find_box(segment, b"moof")
-    mfra_location = find_box(segment, b"mfra")
+    moof_location = next(find_box(segment, b"moof"))
+    mfra_location = next(find_box(segment, b"mfra"))
     # adjust mfhd sequence number in moof
     view = segment.getbuffer()
     view[moof_location + 20 : moof_location + 24] = sequence.to_bytes(4, "big")
     # adjust tfdt in video traf
-    traf_location = find_box(segment, b"traf", moof_location)
-    tfdt_location = find_box(segment, b"tfdt", traf_location)
+    traf_finder = find_box(segment, b"traf", moof_location)
+    traf_location = next(traf_finder)
+    tfdt_location = next(find_box(segment, b"tfdt", traf_location))
     view[tfdt_location + 12 : tfdt_location + 20] = start_pts[0].to_bytes(8, "big")
     # adjust tfdt in audio traf
-    traf_location = find_box(segment, b"traf", moof_location, 2)
-    tfdt_location = find_box(segment, b"tfdt", traf_location)
+    traf_location = next(traf_finder)
+    tfdt_location = next(find_box(segment, b"tfdt", traf_location))
     view[tfdt_location + 12 : tfdt_location + 20] = start_pts[1].to_bytes(8, "big")
     # done adjusting
     segment.seek(moof_location)
