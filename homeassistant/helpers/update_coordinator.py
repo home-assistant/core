ea44beeb0a894d33c4fe8d9fdda52f6f3039cb39
Ville Skyttä <ville.skytta@iki.fi>
2020-07-26 08:40:19 +0300

    Make generic

--- homeassistant/helpers/update_coordinator.py
+++ homeassistant/helpers/update_coordinator.py
@@ -3,7 +3,7 @@ import asyncio
 from datetime import datetime, timedelta
 import logging
 from time import monotonic
-from typing import Any, Awaitable, Callable, List, Optional
+from typing import Awaitable, Callable, Generic, List, Optional, TypeVar
 
 import aiohttp
 
@@ -16,12 +16,14 @@ from .debounce import Debouncer
 REQUEST_REFRESH_DEFAULT_COOLDOWN = 10
 REQUEST_REFRESH_DEFAULT_IMMEDIATE = True
 
+T = TypeVar("T")
+
 
 class UpdateFailed(Exception):
     """Raised when an update has failed."""
 
 
-class DataUpdateCoordinator:
+class DataUpdateCoordinator(Generic[T]):
     """Class to manage fetching data from single endpoint."""
 
     def __init__(
@@ -31,7 +33,7 @@ class DataUpdateCoordinator:
         *,
         name: str,
         update_interval: Optional[timedelta] = None,
-        update_method: Optional[Callable[[], Awaitable]] = None,
+        update_method: Optional[Callable[[], Awaitable[T]]] = None,
         request_refresh_debouncer: Optional[Debouncer] = None,
     ):
         """Initialize global data updater."""
@@ -41,7 +43,7 @@ class DataUpdateCoordinator:
         self.update_method = update_method
         self.update_interval = update_interval
 
-        self.data: Optional[Any] = None
+        self.data: Optional[T] = None
 
         self._listeners: List[CALLBACK_TYPE] = []
         self._unsub_refresh: Optional[CALLBACK_TYPE] = None
@@ -120,7 +122,7 @@ class DataUpdateCoordinator:
         """
         await self._debounced_refresh.async_call()
 
-    async def _async_update_data(self) -> Optional[Any]:
+    async def _async_update_data(self) -> Optional[T]:
         """Fetch the latest data from the source."""
         if self.update_method is None:
             raise NotImplementedError("Update method not implemented")
