4c44f5934e57d11cfef1d8f4331c69a014c0d1cd
David Bonnes <zxdavb@bonnes.me>
2020-07-26 21:13:10 +0100

    fix issue #34559 (#38241)

--- homeassistant/components/evohome/__init__.py
+++ homeassistant/components/evohome/__init__.py
@@ -614,13 +614,12 @@ class EvoChild(EvoDevice):
     @property
     def current_temperature(self) -> Optional[float]:
         """Return the current temperature of a Zone."""
-        if not self._evo_device.temperatureStatus["isAvailable"]:
-            return None
-
         if self._evo_broker.temps:
-            return self._evo_broker.temps[self._evo_device.zoneId]
+            if self._evo_broker.temps[self._evo_device.zoneId] != 128:
+                return self._evo_broker.temps[self._evo_device.zoneId]
 
-        return self._evo_device.temperatureStatus["temperature"]
+        if self._evo_device.temperatureStatus["isAvailable"]:
+            return self._evo_device.temperatureStatus["temperature"]
 
     @property
     def setpoints(self) -> Dict[str, Any]:
