7314aeb69d30819a98f883482cfb6ac45fcc6b37
Frans Rampen <Frans.Rampen@yoda-ict.nl>
2020-07-27 17:23:26 +0200

    MQTT and Const update

--- homeassistant/components/mqtt/alarm_control_panel.py
+++ homeassistant/components/mqtt/alarm_control_panel.py
@@ -11,6 +11,7 @@ from homeassistant.components.alarm_control_panel.const import (
     SUPPORT_ALARM_ARM_CUSTOM_BYPASS,
     SUPPORT_ALARM_ARM_HOME,
     SUPPORT_ALARM_ARM_NIGHT,
+    SUPPORT_ALARM_ARM_VACATION,
 )
 from homeassistant.const import (
     CONF_CODE,
@@ -21,6 +22,7 @@ from homeassistant.const import (
     STATE_ALARM_ARMED_CUSTOM_BYPASS,
     STATE_ALARM_ARMED_HOME,
     STATE_ALARM_ARMED_NIGHT,
+    STATE_ALARM_ARMED_VACATION,
     STATE_ALARM_ARMING,
     STATE_ALARM_DISARMED,
     STATE_ALARM_DISARMING,
@@ -56,10 +58,12 @@ CONF_PAYLOAD_DISARM = "payload_disarm"
 CONF_PAYLOAD_ARM_HOME = "payload_arm_home"
 CONF_PAYLOAD_ARM_AWAY = "payload_arm_away"
 CONF_PAYLOAD_ARM_NIGHT = "payload_arm_night"
+CONF_PAYLOAD_ARM_VACATION = "payload_arm_vacation"
 CONF_PAYLOAD_ARM_CUSTOM_BYPASS = "payload_arm_custom_bypass"
 CONF_COMMAND_TEMPLATE = "command_template"
 
 DEFAULT_COMMAND_TEMPLATE = "{{action}}"
+DEFAULT_ARM_VACATION = "ARM_VACATION"
 DEFAULT_ARM_NIGHT = "ARM_NIGHT"
 DEFAULT_ARM_AWAY = "ARM_AWAY"
 DEFAULT_ARM_HOME = "ARM_HOME"
@@ -81,6 +85,9 @@ PLATFORM_SCHEMA = (
             vol.Optional(CONF_PAYLOAD_ARM_AWAY, default=DEFAULT_ARM_AWAY): cv.string,
             vol.Optional(CONF_PAYLOAD_ARM_HOME, default=DEFAULT_ARM_HOME): cv.string,
             vol.Optional(CONF_PAYLOAD_ARM_NIGHT, default=DEFAULT_ARM_NIGHT): cv.string,
+            vol.Optional(
+                CONF_PAYLOAD_ARM_VACATION, default=DEFAULT_ARM_VACATION
+            ): cv.string,
             vol.Optional(
                 CONF_PAYLOAD_ARM_CUSTOM_BYPASS, default=DEFAULT_ARM_CUSTOM_BYPASS
             ): cv.string,
@@ -190,6 +197,7 @@ class MqttAlarm(
                 STATE_ALARM_ARMED_HOME,
                 STATE_ALARM_ARMED_AWAY,
                 STATE_ALARM_ARMED_NIGHT,
+                STATE_ALARM_ARMED_VACATION,
                 STATE_ALARM_ARMED_CUSTOM_BYPASS,
                 STATE_ALARM_PENDING,
                 STATE_ALARM_ARMING,
@@ -249,6 +257,7 @@ class MqttAlarm(
             SUPPORT_ALARM_ARM_HOME
             | SUPPORT_ALARM_ARM_AWAY
             | SUPPORT_ALARM_ARM_NIGHT
+            | SUPPORT_ALARM_ARM_VACATION
             | SUPPORT_ALARM_ARM_CUSTOM_BYPASS
         )
 
@@ -310,6 +319,17 @@ class MqttAlarm(
         if code_required and not self._validate_code(code, "arming night"):
             return
         action = self._config[CONF_PAYLOAD_ARM_NIGHT]
+        self._publish(code, action)
+
+    async def async_alarm_arm_vacation(self, code=None):
+        """Send arm vacation command.
+
+        This method is a coroutine.
+        """
+        code_required = self._config[CONF_CODE_ARM_REQUIRED]
+        if code_required and not self._validate_code(code, "arming vacation"):
+            return
+        action = self._config[CONF_PAYLOAD_ARM_VACATION]
         self._publish(code, action)
 
     async def async_alarm_arm_custom_bypass(self, code=None):
