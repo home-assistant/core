cc4ca607f892adcaf0bf9c969b83e5bc82f4eabf
Erik Hendrix <hendrix_erik@hotmail.com>
2020-07-29 08:43:26 -0600

    Updated based on comments

--- script/run-in-env.sh
+++ script/run-in-env.sh
@@ -10,23 +10,23 @@ fi
 
 # other common virtualenvs
 my_path="."
-sed_cmd=`which sed`
+sed_cmd=$(which sed)
 if [ "${sed_cmd}" == "" ]; then
-	if [ -f "/usr/bin/sed" ]; then
-		sed_cmd="/usr/bin/sed"
-	fi
+  if [ -f "/usr/bin/sed" ]; then
+    sed_cmd="/usr/bin/sed"
+  fi
 fi
 if [ "${sed_cmd}" != "" ]; then
-	if [ "$0" != "" ]; then
-		my_path=`echo $0 | ${sed_cmd} -e 's;\/[^/]*$;;'`
-		my_path="${my_path}/.."
-	fi
+  if [ "$0" != "" ]; then
+    my_path=`echo $0 | ${sed_cmd} -e 's;\/[^/]*$;;'`
+    my_path="${my_path}/.."
+  fi
 fi
 
 for venv in venv .venv .; do
-    if [ -f ${my_path}/$venv/bin/activate ]; then
-        . ${my_path}/$venv/bin/activate
-    fi
+  if [ -f "${my_path}/${venv}/bin/activate" ]; then
+    . "${my_path}/${venv}/bin/activate"
+  fi
 done
 
 exec "$@"
