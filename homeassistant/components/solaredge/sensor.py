022c496b9edad734b8b0d76afb8cf08a48e68414
Markus Haack <markus.haack@gmail.com>
2020-07-30 23:07:27 +0200

    Add SolarEdge battery storage sensor

--- homeassistant/components/solaredge/sensor.py
+++ homeassistant/components/solaredge/sensor.py
@@ -6,9 +6,13 @@ from requests.exceptions import ConnectTimeout, HTTPError
 import solaredge
 from stringcase import snakecase
 
-from homeassistant.const import CONF_API_KEY
+from homeassistant.const import (
+    CONF_API_KEY,
+    ATTR_BATTERY_CHARGING,
+    DEVICE_CLASS_BATTERY,
+    DEVICE_CLASS_POWER,
+)
 from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.icon import icon_for_battery_level
 from homeassistant.util import Throttle
 
 from .const import (
@@ -83,6 +87,9 @@ class SolarEdgeSensorFactory:
 
         for key in ["power_consumption", "solar_power", "grid_power", "storage_power"]:
             self.services[key] = (SolarEdgePowerFlowSensor, flow)
+
+        for key in ["storage_level"]:
+            self.services[key] = (SolarEdgeStorageLevelSensor, flow)
 
         for key in [
             "purchased_power",
@@ -234,6 +241,11 @@ class SolarEdgePowerFlowSensor(SolarEdgeSensor):
         """Return the state attributes."""
         return self._attributes
 
+    @property
+    def device_class(self):
+        """Device Class."""
+        return DEVICE_CLASS_POWER
+
     def update(self):
         """Get the latest inventory data and update state and attributes."""
         self.data_service.update()
@@ -241,11 +253,34 @@ class SolarEdgePowerFlowSensor(SolarEdgeSensor):
         self._attributes = self.data_service.attributes.get(self._json_key)
         self._unit_of_measurement = self.data_service.unit
 
-        if self._json_key == "STORAGE" and "soc" in self._attributes:
-            charging = True if self._attributes["flow"] == "charge" else False
-            self._icon = icon_for_battery_level(
-                battery_level=self._attributes["soc"], charging=charging,
-            )
+
+class SolarEdgeStorageLevelSensor(SolarEdgeSensor):
+    """Representation of an SolarEdge Monitoring API storage level sensor."""
+
+    def __init__(self, platform_name, sensor_key, data_service):
+        """Initialize the storage level sensor."""
+        super().__init__(platform_name, sensor_key, data_service)
+
+        self._json_key = SENSOR_TYPES[self.sensor_key][0]
+
+        self._attributes = {}
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
+
+    @property
+    def device_class(self):
+        """Return the device_class of the device."""
+        return DEVICE_CLASS_BATTERY
+
+    def update(self):
+        """Get the latest inventory data and update state and attributes."""
+        self.data_service.update()
+        attr = self.data_service.attributes.get(self._json_key)
+        self._state = attr["soc"]
+        self._attributes[ATTR_BATTERY_CHARGING] = attr["flow"] != "discharge"
 
 
 class SolarEdgeDataService:
