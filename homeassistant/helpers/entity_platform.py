4c9ef2b7418e1031162318dedb241cf4c3c7bfc3
J. Nick Koston <nick@koston.org>
2020-07-31 11:12:08 -1000

    Update homeassistant/helpers/entity_platform.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/helpers/entity_platform.py
+++ homeassistant/helpers/entity_platform.py
@@ -440,9 +440,9 @@ class EntityPlatform:
             if entity.unique_id is not None:
                 msg = f"Platform {self.platform_name} does not generate unique IDs. "
                 if requested_entity_id:
-                    msg += f"({entity.unique_id}) is already used by {entity.entity_id} - ignoring {requested_entity_id}"
+                    msg += f"ID {entity.unique_id} is already used by {entity.entity_id} - ignoring {requested_entity_id}"
                 else:
-                    msg += f"({entity.unique_id}) already exists - ignoring {entity.entity_id}"
+                    msg += f"ID {entity.unique_id} already exists - ignoring {entity.entity_id}"
             else:
                 msg = f"Entity id already exists - ignoring: {entity.entity_id}"
             self.logger.error(msg)
