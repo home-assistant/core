8fffd53e36be465709c415be4280cf0551d9b376
Aidan Timson <contact@timmo.xyz>
2020-08-01 13:27:58 +0100

    Unique ids

--- homeassistant/components/github/config_flow.py
+++ homeassistant/components/github/config_flow.py
@@ -76,9 +76,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors["base"] = "cannot_find_repo"
             except InvalidAuth:
                 errors["base"] = "invalid_auth"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+
+        await self.async_set_unique_id(user_input[CONF_REPOSITORY])
+        self._abort_if_unique_id_configured()
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
@@ -107,6 +107,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self.context["title_placeholders"] = {
             "repository": user_input[CONF_REPOSITORY],
         }
+
+        await self.async_set_unique_id(self.repository)
+
         try:
             await validate_input(self.hass, user_input)
             for entry in self._async_current_entries():
@@ -121,9 +124,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             errors["base"] = "cannot_find_repo"
         except InvalidAuth:
             errors["base"] = "invalid_auth"
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
 
         return self.async_show_form(
             step_id="reauth", data_schema=REAUTH_SCHEMA, errors=errors
