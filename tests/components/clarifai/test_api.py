14b28c1db7037c9fc946ccb84b669b7dfd590d4c
DJ Bowers <dj@dj-bowers.com>
2020-07-31 18:55:55 -0700

    Add tests for Clarifai API request/response logic

--- tests/components/clarifai/test_api.py
+++ tests/components/clarifai/test_api.py
@@ -1,12 +1,18 @@
 """Tests for the Clarifai API."""
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2
-from clarifai_grpc.grpc.api.status import status_code_pb2
+from google.protobuf.json_format import Parse
 import pytest
 
-from homeassistant.components.clarifai.api import Clarifai, validate_response
+from homeassistant.components.clarifai.api import Clarifai
+from homeassistant.components.clarifai.const import CONCEPTS, DEFAULT, OUTPUTS
 from homeassistant.exceptions import HomeAssistantError
 
 from tests.async_mock import MagicMock, patch
+from tests.common import load_fixture
+
+DATA = "data"
+NAME = "name"
+VALUE = "value"
 
 
 class TestClarifaiAPI:
@@ -23,9 +29,9 @@ class TestClarifaiAPI:
             self.api = Clarifai(self.access_token)
 
     @patch(
-        "homeassistant.components.clarifai.api.validate_response", return_value=False,
+        "homeassistant.components.clarifai.api._validate_response", return_value=False,
     )
-    def test_post_workflow_results(self, mock_validation):
+    def test_post_workflow_results_request_logic(self, mock_validation):
         """Test the post_workflow_results method of the Clarifai API."""
         app_id = "12345678abcdef"
         workflow_id = "Face"
@@ -51,26 +57,71 @@ class TestClarifaiAPI:
             request, metadata=metadata
         )
 
+    def test_post_workflow_results_response_with_default_parser(self):
+        """Test the default response parsing methods of post_workflow_results."""
+        result_format = DEFAULT
 
-def test_validate_response_successful():
-    """Test the successful path of validate_response."""
-    response = MagicMock()
-    response.status.code = status_code_pb2.SUCCESS
+        message = Parse(
+            load_fixture("clarifai_response_success.json"),
+            service_pb2.PostWorkflowResultsResponse(),
+        )
 
-    assert validate_response(response) is True
+        self.api.stub.PostWorkflowResults.return_value = message
 
+        result = self.api.post_workflow_results(
+            "12345678abcdef", "General", result_format, b"Test"
+        )
 
-def test_validate_response_failure():
-    """Test the failure path of validate_response."""
-    response = MagicMock()
-    response.status.code = status_code_pb2.FAILURE
+        concept_name = "dog"
+        concept_value = 0.9979874
+        found_dog = False
 
-    with pytest.raises(HomeAssistantError):
-        validate_response(response)
+        assert OUTPUTS in result
+        for output in result[OUTPUTS]:
+            assert DATA in output
+            output_data = output[DATA]
+            if CONCEPTS in output_data:
+                for concept in output_data[CONCEPTS]:
+                    assert NAME in concept
+                    assert VALUE in concept
+                    if concept[NAME] == concept_name:
+                        found_dog = True
+                        assert concept[VALUE] == concept_value
+        assert found_dog
 
+    def test_post_workflow_results_response_with_concepts_parser(self):
+        """Test the concept parsing methods of post_workflow_results."""
+        result_format = CONCEPTS
 
-# def test_parse_workflow_response_default():
+        message = Parse(
+            load_fixture("clarifai_response_success.json"),
+            service_pb2.PostWorkflowResultsResponse(),
+        )
 
-# def test_parse_workflow_response_concepts():
+        self.api.stub.PostWorkflowResults.return_value = message
 
-# def test_parse_workflow_response_no_response():
+        result = self.api.post_workflow_results(
+            "12345678abcdef", "General", result_format, b"Test"
+        )
+
+        concept_name = "dog"
+        concept_value = 0.9979874
+
+        assert CONCEPTS in result
+        concepts = result[CONCEPTS]
+        assert concept_name in concepts
+        assert max(concepts[concept_name]) == concept_value
+
+    def test_post_workflow_results_response_failure(self):
+        """Test a failed response from post_workflow_results."""
+        message = Parse(
+            load_fixture("clarifai_response_failure.json"),
+            service_pb2.PostWorkflowResultsResponse(),
+        )
+
+        self.api.stub.PostWorkflowResults.return_value = message
+
+        with pytest.raises(HomeAssistantError):
+            self.api.post_workflow_results(
+                "12345678abcdef", "General", "default", b"Test"
+            )
