11994d207aaf1b682c40afe0ac36c2d599f156c4
Eugene Prystupa <eugene.prystupa@gmail.com>
2020-08-01 12:18:40 -0400

    Add zeroconf discovery for bond integration (#38448)
    * Add zeroconf discovery for bond integration
    
    * Add zeroconf discovery for bond integration (fix typo)
    
    * Add zeroconf discovery for bond integration (PR feedback)
    
    * Add zeroconf discovery for bond integration (PR feedback)
    
    * Add zeroconf discovery for bond integration (PR feedback)

--- homeassistant/components/bond/config_flow.py
+++ homeassistant/components/bond/config_flow.py
@@ -1,23 +1,26 @@
 """Config flow for Bond integration."""
 import logging
+from typing import Any, Dict, Optional
 
 from aiohttp import ClientConnectionError, ClientResponseError
 from bond_api import Bond
 import voluptuous as vol
 
 from homeassistant import config_entries, exceptions
-from homeassistant.const import CONF_ACCESS_TOKEN, CONF_HOST
+from homeassistant.const import CONF_ACCESS_TOKEN, CONF_HOST, CONF_NAME
 
+from .const import CONF_BOND_ID
 from .const import DOMAIN  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
 
-DATA_SCHEMA = vol.Schema(
+DATA_SCHEMA_USER = vol.Schema(
     {vol.Required(CONF_HOST): str, vol.Required(CONF_ACCESS_TOKEN): str}
 )
+DATA_SCHEMA_DISCOVERY = vol.Schema({vol.Required(CONF_ACCESS_TOKEN): str})
 
 
-async def validate_input(data) -> str:
+async def _validate_input(data: Dict[str, Any]) -> str:
     """Validate the user input allows us to connect."""
 
     try:
@@ -26,11 +29,14 @@ async def validate_input(data) -> str:
         # call to non-version API is needed to validate authentication
         await bond.devices()
     except ClientConnectionError:
-        raise CannotConnect
+        raise InputValidationError("cannot_connect")
     except ClientResponseError as error:
         if error.status == 401:
-            raise InvalidAuth
-        raise
+            raise InputValidationError("invalid_auth")
+        raise InputValidationError("unknown")
+    except Exception:
+        _LOGGER.exception("Unexpected exception")
+        raise InputValidationError("unknown")
 
     # Return unique ID from the hub to be stored in the config entry.
     return version["bondid"]
@@ -42,32 +48,73 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
+    _discovered: dict = None
+
+    async def async_step_zeroconf(
+        self, discovery_info: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        """Handle a flow initialized by zeroconf discovery."""
+        name: str = discovery_info[CONF_NAME]
+        host: str = discovery_info[CONF_HOST]
+        bond_id = name.partition(".")[0]
+        await self.async_set_unique_id(bond_id)
+        self._abort_if_unique_id_configured({CONF_HOST: host})
+
+        self._discovered = {
+            CONF_HOST: host,
+            CONF_BOND_ID: bond_id,
+        }
+        # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
+        self.context.update({"title_placeholders": self._discovered})
+
+        return await self.async_step_confirm()
+
+    async def async_step_confirm(
+        self, user_input: Dict[str, Any] = None
+    ) -> Dict[str, Any]:
+        """Handle confirmation flow for discovered bond hub."""
+        errors = {}
+        if user_input is not None:
+            data = user_input.copy()
+            data[CONF_HOST] = self._discovered[CONF_HOST]
+            try:
+                return await self._try_create_entry(data)
+            except InputValidationError as error:
+                errors["base"] = error.base
+
+        return self.async_show_form(
+            step_id="confirm",
+            data_schema=DATA_SCHEMA_DISCOVERY,
+            errors=errors,
+            description_placeholders=self._discovered,
+        )
+
+    async def async_step_user(
+        self, user_input: Dict[str, Any] = None
+    ) -> Dict[str, Any]:
+        """Handle a flow initialized by the user."""
         errors = {}
         if user_input is not None:
             try:
-                bond_id = await validate_input(user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
-                errors["base"] = "invalid_auth"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-            else:
-                await self.async_set_unique_id(bond_id)
-                self._abort_if_unique_id_configured()
-                return self.async_create_entry(title=bond_id, data=user_input)
+                return await self._try_create_entry(user_input)
+            except InputValidationError as error:
+                errors["base"] = error.base
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=DATA_SCHEMA_USER, errors=errors
         )
 
+    async def _try_create_entry(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        bond_id = await _validate_input(data)
+        await self.async_set_unique_id(bond_id)
+        self._abort_if_unique_id_configured()
+        return self.async_create_entry(title=bond_id, data=data)
 
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
 
+class InputValidationError(exceptions.HomeAssistantError):
+    """Error to indicate we cannot proceed due to invalid input."""
 
-class InvalidAuth(exceptions.HomeAssistantError):
-    """Error to indicate there is invalid auth."""
+    def __init__(self, base: str):
+        """Initialize with error base."""
+        super().__init__()
+        self.base = base
