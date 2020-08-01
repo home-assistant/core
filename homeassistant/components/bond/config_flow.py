81b1f327d5c429e93d875ba60dbae5a636b605f3
Eugene Prystupa <eugene.prystupa@gmail.com>
2020-08-01 11:39:52 -0400

    Add zeroconf discovery for bond integration (PR feedback)

--- homeassistant/components/bond/config_flow.py
+++ homeassistant/components/bond/config_flow.py
@@ -7,7 +7,7 @@ from bond_api import Bond
 import voluptuous as vol
 
 from homeassistant import config_entries, exceptions
-from homeassistant.const import CONF_ACCESS_TOKEN, CONF_HOST
+from homeassistant.const import CONF_ACCESS_TOKEN, CONF_HOST, CONF_NAME
 
 from .const import DOMAIN  # pylint:disable=unused-import
 
@@ -53,8 +53,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, discovery_info: Optional[Dict[str, Any]] = None
     ) -> Dict[str, Any]:
         """Handle a flow initialized by zeroconf discovery."""
-        name: str = discovery_info["name"]
-        host: str = discovery_info["host"]
+        name: str = discovery_info[CONF_NAME]
+        host: str = discovery_info[CONF_HOST]
         bond_id = name.partition(".")[0]
         await self.async_set_unique_id(bond_id)
         self._abort_if_unique_id_configured({CONF_HOST: host})
