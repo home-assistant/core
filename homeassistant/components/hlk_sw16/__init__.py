7173d65a41fd6e97f656dd3ef7b5743b5be2b646
James Hilliard <james.hilliard1@gmail.com>
2020-08-02 01:08:18 -0600

    Refactor unload

--- homeassistant/components/hlk_sw16/__init__.py
+++ homeassistant/components/hlk_sw16/__init__.py
@@ -86,11 +86,14 @@ async def async_unload_entry(hass, entry):
     """Unload a config entry."""
     client = hass.data[DOMAIN][entry.entry_id].pop(DATA_DEVICE_REGISTER)
     client.stop()
-    if hass.data[DOMAIN][entry.entry_id]:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    if not hass.data[DOMAIN]:
-        hass.data.pop(DOMAIN)
-    return await hass.config_entries.async_forward_entry_unload(entry, "switch")
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "switch")
+
+    if unload_ok:
+        if hass.data[DOMAIN][entry.entry_id]:
+            hass.data[DOMAIN].pop(entry.entry_id)
+        if not hass.data[DOMAIN]:
+            hass.data.pop(DOMAIN)
+    return unload_ok
 
 
 class SW16Device(Entity):
