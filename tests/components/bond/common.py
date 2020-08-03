4d0462863673dd32568de2a828ff9e3c5aca8615
Eugene Prystupa <eugene.prystupa@gmail.com>
2020-08-02 21:13:38 -0400

    Fix a bug when generated entity IDs are not guaranteed to be unique when there are multiple Bond hubs

--- tests/components/bond/common.py
+++ tests/components/bond/common.py
@@ -58,7 +58,7 @@ async def setup_platform(
     """Set up the specified Bond platform."""
     mock_entry = MockConfigEntry(
         domain=BOND_DOMAIN,
-        data={CONF_HOST: "1.1.1.1", CONF_ACCESS_TOKEN: "test-token"},
+        data={CONF_HOST: "some host", CONF_ACCESS_TOKEN: "test-token"},
     )
     mock_entry.add_to_hass(hass)
 
