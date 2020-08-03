0c7bc4517782f894edf09b91c59677b301f66a2f
Eugene Prystupa <eugene.prystupa@gmail.com>
2020-08-02 22:50:03 -0400

    Ensure bond unique ids are unique across hubs (#38496)

--- tests/components/bond/test_cover.py
+++ tests/components/bond/test_cover.py
@@ -34,10 +34,17 @@ def shades(name: str):
 
 async def test_entity_registry(hass: core.HomeAssistant):
     """Tests that the devices are registered in the entity registry."""
-    await setup_platform(hass, COVER_DOMAIN, shades("name-1"))
+    await setup_platform(
+        hass,
+        COVER_DOMAIN,
+        shades("name-1"),
+        bond_version={"bondid": "test-hub-id"},
+        bond_device_id="test-device-id",
+    )
 
     registry: EntityRegistry = await hass.helpers.entity_registry.async_get_registry()
-    assert [key for key in registry.entities] == ["cover.name_1"]
+    entity = registry.entities["cover.name_1"]
+    assert entity.unique_id == "test-hub-id_test-device-id"
 
 
 async def test_open_cover(hass: core.HomeAssistant):
