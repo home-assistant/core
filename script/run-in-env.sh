home-assistant/core

commit f6540e30023e9f1abe7f018bc93ac4b4e2954db9

Fix pre-commit hook issue on Windows (#31648)
* Make shebang compatable with Windows

* Prehook test

* Undo prehook test

script/run-in-env.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eu
+#!/usr/bin/env sh -eu
 
 # Activate pyenv and virtualenv if present, then run the specified command
 

commit f07867b2d4505c02a8a7f31733f45a10352700b4

Fix run-in-env.sh sh options
Shebang takes only one arg, regression in
f6540e30023e9f1abe7f018bc93ac4b4e2954db9

script/run-in-env.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh -eu
+#!/usr/bin/env sh
+set -eu
 
 # Activate pyenv and virtualenv if present, then run the specified command
 