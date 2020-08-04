936a8aaab3c7b482ae011509ac98349b317dd978
Bram Devooght <b.devooght@smappee.com>
2020-08-04 14:45:33 +0200

    we do not need a specific test for this

--- tests/components/smappee/test_config_flow.py
+++ tests/components/smappee/test_config_flow.py
@@ -301,6 +301,9 @@ async def test_full_zeroconf_flow(hass):
         assert result["title"] == "Smappee1006000212"
         assert len(hass.config_entries.async_entries(DOMAIN)) == 1
 
+        entry = hass.config_entries.async_entries(DOMAIN)[0]
+        assert entry.unique_id == "Smappee1006000212"
+
 
 async def test_full_user_local_flow(hass):
     """Test the full zeroconf flow."""
@@ -329,42 +332,6 @@ async def test_full_user_local_flow(hass):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"], {"host": "1.2.3.4"}
         )
-        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-        assert result["title"] == "Smappee1006000212"
-        assert len(hass.config_entries.async_entries(DOMAIN)) == 1
-
-
-async def test_setting_unique_id(hass):
-    """Test setting unique id."""
-    with patch("pysmappee.api.SmappeeLocalApi.logon", return_value={}), patch(
-        "pysmappee.api.SmappeeLocalApi.load_advanced_config",
-        return_value=[{"key": "mdnsHostName", "value": "Smappee1006000212"}],
-    ), patch(
-        "pysmappee.api.SmappeeLocalApi.load_command_control_config", return_value=[]
-    ), patch(
-        "pysmappee.api.SmappeeLocalApi.load_instantaneous",
-        return_value=[{"key": "phase0ActivePower", "value": 0}],
-    ):
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN,
-            context={"source": SOURCE_ZEROCONF},
-            data={
-                "host": "1.2.3.4",
-                "port": 22,
-                CONF_HOSTNAME: "Smappee1006000212.local.",
-                "type": "_ssh._tcp.local.",
-                "name": "Smappee1006000212._ssh._tcp.local.",
-                "properties": {"_raw": {}},
-            },
-        )
-        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-        assert result["step_id"] == "zeroconf_confirm"
-        assert result["description_placeholders"] == {CONF_SERIALNUMBER: "1006000212"}
-
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {"host": "1.2.3.4"}
-        )
-
         assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
         assert result["title"] == "Smappee1006000212"
         assert len(hass.config_entries.async_entries(DOMAIN)) == 1
