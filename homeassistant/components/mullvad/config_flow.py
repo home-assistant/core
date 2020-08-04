9c3924ff42650aa3bf07085f3d1a827af85901a8
meichthys <10717998+meichthys@users.noreply.github.com>
2020-08-03 22:37:55 -0400

    abort if already configured

--- homeassistant/components/mullvad/config_flow.py
+++ homeassistant/components/mullvad/config_flow.py
@@ -16,6 +16,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
+        if self.hass.config_entries.async_entries(DOMAIN):
+            return self.async_abort(reason="already_configured")
 
         if user_input is not None:
             return self.async_create_entry(title="Mullvad VPN", data=user_input)
