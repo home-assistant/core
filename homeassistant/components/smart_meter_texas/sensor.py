1c44fc770e493ec66c89a9557141ef77c911a6ab
Graham Wetzler <graham@wetzler.dev>
2020-08-05 19:48:54 +0000

    Allow multiple meters per account

--- homeassistant/components/smart_meter_texas/sensor.py
+++ homeassistant/components/smart_meter_texas/sensor.py
@@ -1,32 +1,33 @@
 """Support for Smart Meter Texas sensors."""
+from datetime import timedelta
 import logging
 
-from homeassistant.const import ENERGY_KILO_WATT_HOUR
+from smart_meter_texas import Client, Meter
+
+from homeassistant.const import CONF_ADDRESS, ENERGY_KILO_WATT_HOUR
 from homeassistant.helpers.entity import Entity
 
-from .const import DOMAIN, LAST_UPDATED
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
+SCAN_INTERVAL = timedelta(hours=1)
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Smart Meter Texas sensors."""
-    coordinator = hass.data[DOMAIN][config_entry.entry_id]
-    meter_number = config_entry.data["meter"]
-    esiid = config_entry.data["esiid"]
-
-    async_add_entities([SmartMeterTexasSensor(coordinator, meter_number, esiid)], True)
+    smartmetertexas = hass.data[DOMAIN][config_entry.entry_id]
+    for meter in smartmetertexas.meters:
+        async_add_entities([SmartMeterTexasSensor(meter, smartmetertexas.client)], True)
 
 
 class SmartMeterTexasSensor(Entity):
     """Representation of an Smart Meter Texas sensor."""
 
-    def __init__(self, coordinator, meter_number, esiid):
+    def __init__(self, meter: Meter, client: Client):
         """Initialize the sensor."""
-        self._coordinator = coordinator
-        self._meter_number = meter_number
-        self._esiid = esiid
-        self._last_updated = coordinator.data.reading_datetime
+        self.meter = meter
+        self.client = client
 
     @property
     def unit_of_measurement(self):
@@ -36,7 +37,7 @@ class SmartMeterTexasSensor(Entity):
     @property
     def name(self):
         """Device Name."""
-        return "Electric meter"
+        return f"Electric meter {self.meter.meter}"
 
     @property
     def icon(self):
@@ -46,45 +47,38 @@ class SmartMeterTexasSensor(Entity):
     @property
     def unique_id(self):
         """Device Uniqueid."""
-        return f"electric_meter_{self._meter_number}"
+        return f"electric_meter_{self.meter.meter}"
 
     @property
     def available(self):
         """Return True if entity is available."""
-        return self._coordinator.last_update_success
+        return True  # self._coordinator.last_update_success
 
     @property
     def state(self):
-        """Get the current price."""
-        return self._coordinator.data.reading
+        """Get the latest reading."""
+        return self.meter.reading
 
     @property
     def device_state_attributes(self):
         """Return the device specific state attributes."""
-        attributes = {}
-
-        if self._last_updated is not None:
-            attributes[LAST_UPDATED] = self._last_updated
-
-        attributes["meter"] = self._meter_number
-        attributes["esiid"] = self._esiid
-
+        attributes = {
+            "meter_number": self.meter.meter,
+            "electric_service_identifier": self.meter.esiid,
+            CONF_ADDRESS: self.meter.address,
+            "last_updated": self.meter.reading_datetime,
+        }
         return attributes
 
     @property
     def should_poll(self):
         """Return False, updates are controlled via coordinator."""
-        return False
+        return True
 
     async def async_update(self):
         """Update the entity.
 
         Only used by the generic entity update service.
         """
-        await self._coordinator.async_request_refresh()
-
-    async def async_added_to_hass(self):
-        """Subscribe to updates."""
-        self.async_on_remove(
-            self._coordinator.async_add_listener(self.async_write_ha_state)
-        )
+        _LOGGER.debug("Reading meter %s", self.meter.meter)
+        await self.meter.read_meter(self.client)
