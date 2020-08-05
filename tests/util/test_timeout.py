6db0c4e768f190b9fb3eec779836d69575a39c65
J. Nick Koston <nick@koston.org>
2020-08-05 05:50:01 +0000

    more coverage

--- tests/util/test_timeout.py
+++ tests/util/test_timeout.py
@@ -42,6 +42,78 @@ async def test_simple_global_timeout_freeze():
             await asyncio.sleep(0.3)
 
 
+async def test_simple_zone_timeout_freeze_inside_executor_job(hass):
+    """Test a simple zone timeout freeze inside an executor job."""
+    timeout = ZoneTimeout()
+
+    def _some_sync_work():
+        with timeout.freeze("recorder"):
+            time.sleep(0.3)
+
+    async with timeout.async_timeout(1.0):
+        async with timeout.async_timeout(0.2, zone_name="recorder"):
+            await hass.async_add_executor_job(_some_sync_work)
+
+
+async def test_simple_global_timeout_freeze_inside_executor_job(hass):
+    """Test a simple global timeout freeze inside an executor job."""
+    timeout = ZoneTimeout()
+
+    def _some_sync_work():
+        with timeout.freeze():
+            time.sleep(0.3)
+
+    async with timeout.async_timeout(0.2):
+        await hass.async_add_executor_job(_some_sync_work)
+
+
+async def test_mix_global_timeout_freeze_and_zone_freeze_inside_executor_job(hass):
+    """Test a simple global timeout freeze inside an executor job."""
+    timeout = ZoneTimeout()
+
+    def _some_sync_work():
+        with timeout.freeze("recorder"):
+            time.sleep(0.3)
+
+    async with timeout.async_timeout(0.1):
+        async with timeout.async_timeout(0.2, zone_name="recorder"):
+            await hass.async_add_executor_job(_some_sync_work)
+
+
+async def test_mix_global_timeout_freeze_and_zone_freeze_other_zone_inside_executor_job(
+    hass,
+):
+    """Test a simple global timeout freeze other zone inside an executor job."""
+    timeout = ZoneTimeout()
+
+    def _some_sync_work():
+        with timeout.freeze("not_recorder"):
+            time.sleep(0.3)
+
+    with pytest.raises(asyncio.TimeoutError):
+        async with timeout.async_timeout(0.1):
+            async with timeout.async_timeout(0.2, zone_name="recorder"):
+                async with timeout.async_timeout(0.2, zone_name="not_recorder"):
+                    await hass.async_add_executor_job(_some_sync_work)
+
+
+async def test_mix_global_timeout_freeze_and_zone_freeze_inside_executor_job_second_job_outside_zone_context(
+    hass,
+):
+    """Test a simple global timeout freeze inside an executor job with second job outside of zone context."""
+    timeout = ZoneTimeout()
+
+    def _some_sync_work():
+        with timeout.freeze("recorder"):
+            time.sleep(0.3)
+
+    with pytest.raises(asyncio.TimeoutError):
+        async with timeout.async_timeout(0.1):
+            async with timeout.async_timeout(0.2, zone_name="recorder"):
+                await hass.async_add_executor_job(_some_sync_work)
+            await hass.async_add_executor_job(lambda: time.sleep(0.2))
+
+
 async def test_simple_global_timeout_freeze_with_executor_job(hass):
     """Test a simple global timeout freeze with executor job."""
     timeout = ZoneTimeout()
