2f3c0b49fa26f7689a2e262826b51c2e64d53b0a
cgtobi <cgtobi@users.noreply.github.com>
2020-08-06 20:19:49 +0200

    Update homeassistant/components/netatmo/climate.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/netatmo/climate.py
+++ homeassistant/components/netatmo/climate.py
@@ -155,7 +155,6 @@ async def async_setup_entry(hass, entry, async_add_entities):
         platform.async_register_entity_service(
             SERVICE_SETSCHEDULE,
             {
-                vol.Required(ATTR_ENTITY_ID): cv.entity_domain(CLIMATE_DOMAIN),
                 vol.Required(ATTR_SCHEDULE_NAME): cv.string,
             },
             "_service_set_schedule",
