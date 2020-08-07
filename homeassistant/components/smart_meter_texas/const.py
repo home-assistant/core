6c93075d785c3c7d4aed1b400a309accafd86402
Graham Wetzler <graham@wetzler.dev>
2020-08-07 20:41:49 +0000

    Move strings to const.py

--- homeassistant/components/smart_meter_texas/const.py
+++ homeassistant/components/smart_meter_texas/const.py
@@ -1,8 +1,13 @@
 """Constants for the Smart Meter Texas integration."""
 from datetime import timedelta
 
-DOMAIN = "smart_meter_texas"
-LAST_UPDATED = "last_updated"
 SCAN_INTERVAL = timedelta(hours=1)
 DEBOUNCE_COOLDOWN = 1800  # Seconds
+
+DOMAIN = "smart_meter_texas"
+ICON = "mdi:counter"
+
 METER_NUMBER = "meter_number"
+ESIID = "electric_service_identifier"
+LAST_UPDATE = "last_updated"
+ELECTRIC_METER = "Electric Meter"
