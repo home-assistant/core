fa65d7c41544af6ca32590a61764c930df988c10
Bas Nijholt <basnijholt@gmail.com>
2020-08-07 17:56:34 +0200

    remaining changes

--- tests/async_mock.py
+++ tests/async_mock.py
@@ -3,6 +3,6 @@ import sys
 
 if sys.version_info[:2] < (3, 8):
     from asynctest.mock import *  # noqa
-    from asynctest.mock import CoroutineMock as AsyncMock
+    from asynctest.mock import CoroutineMock as AsyncMock  # noqa
 else:
     from unittest.mock import *  # noqa
