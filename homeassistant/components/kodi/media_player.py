0991a17db6ba431577d281652d81493d0a546ddd
On Freund <onfreund@gmail.com>
2020-08-09 22:09:23 +0300

    fix indentation

--- homeassistant/components/kodi/media_player.py
+++ homeassistant/components/kodi/media_player.py
@@ -218,8 +218,8 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     name = config_entry.data[CONF_NAME]
     uid = config_entry.data.get(CONF_ID)
 
-if uid is None:
-    uid = config_entry.entry_id
+    if uid is None:
+        uid = config_entry.entry_id
 
     entity = KodiEntity(connection, kodi, name, uid, version)
     async_add_entities([entity])
