462757cc43c4a2738d4e171a610aa508d48aab1d
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-08-09 12:43:41 +0300

    added tests for the new configuration options

--- tests/components/dynalite/test_init.py
+++ tests/components/dynalite/test_init.py
@@ -85,8 +85,7 @@ async def test_service_request_area_preset(hass):
         "homeassistant.components.dynalite.bridge.DynaliteDevices.async_setup",
         return_value=True,
     ), patch(
-        "homeassistant.components.dynalite.bridge.DynaliteDevices.request_area_preset",
-        return_value=True,
+        "dynalite_devices_lib.dynalite.Dynalite.request_area_preset", return_value=True,
     ) as mock_req_area_pres:
         assert await async_setup_component(
             hass,
@@ -94,8 +93,18 @@ async def test_service_request_area_preset(hass):
             {
                 dynalite.DOMAIN: {
                     dynalite.CONF_BRIDGES: [
-                        {CONF_HOST: "1.2.3.4", CONF_PORT: 1234},
-                        {CONF_HOST: "5.6.7.8", CONF_PORT: 5678},
+                        {
+                            CONF_HOST: "1.2.3.4",
+                            CONF_PORT: 1234,
+                            dynalite.CONF_AREA: {
+                                "7": {CONF_NAME: "test", dynalite.CONF_QUERY_CHANNEL: 8}
+                            },
+                        },
+                        {
+                            CONF_HOST: "5.6.7.8",
+                            CONF_PORT: 5678,
+                            dynalite.CONF_DEFAULT: {dynalite.CONF_QUERY_CHANNEL: 4},
+                        },
                     ]
                 }
             },
@@ -106,19 +115,19 @@ async def test_service_request_area_preset(hass):
             dynalite.DOMAIN, "request_area_preset", {"host": "1.2.3.4", "area": 2},
         )
         await hass.async_block_till_done()
-        mock_req_area_pres.assert_called_once_with(2, None)
+        mock_req_area_pres.assert_called_once_with(2, 1)
         mock_req_area_pres.reset_mock()
         await hass.services.async_call(
             dynalite.DOMAIN, "request_area_preset", {"host": "", "area": 3},
         )
         await hass.async_block_till_done()
-        assert mock_req_area_pres.mock_calls == [call(3, None), call(3, None)]
+        assert mock_req_area_pres.mock_calls == [call(3, 1), call(3, 4)]
         mock_req_area_pres.reset_mock()
         await hass.services.async_call(
             dynalite.DOMAIN, "request_area_preset", {"host": "5.6.7.8", "area": 4},
         )
         await hass.async_block_till_done()
-        mock_req_area_pres.assert_called_once_with(4, None)
+        mock_req_area_pres.assert_called_once_with(4, 4)
         mock_req_area_pres.reset_mock()
         await hass.services.async_call(
             dynalite.DOMAIN, "request_area_preset", {"host": "6.5.4.3", "area": 5},
@@ -133,6 +142,12 @@ async def test_service_request_area_preset(hass):
         )
         await hass.async_block_till_done()
         mock_req_area_pres.assert_called_once_with(6, 9)
+        mock_req_area_pres.reset_mock()
+        await hass.services.async_call(
+            dynalite.DOMAIN, "request_area_preset", {"host": "1.2.3.4", "area": 7},
+        )
+        await hass.async_block_till_done()
+        mock_req_area_pres.assert_called_once_with(7, 8)
 
 
 async def test_async_setup_bad_config1(hass):
