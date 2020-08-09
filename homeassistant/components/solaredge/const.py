3d308e0599a4c968bf19853ff6e33302f6cc623a
Markus Haack <developer.mhaack@gmail.com>
2020-08-09 23:28:45 +0200

    Add SolarEdge battery level and dynamic icon for storage sensor (#37826)
    * Add SolarEdge battery level and dynamic icon for storage sensor
    
    * Add SolarEdge battery storage sensor
    
    * Fix isort warning
    
    * Remove charging attribute
    
    * Fix isort warning
    
    * Apply suggestions from code review
    
    * Update homeassistant/components/solaredge/sensor.py
    
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>

--- homeassistant/components/solaredge/const.py
+++ homeassistant/components/solaredge/const.py
@@ -1,7 +1,7 @@
 """Constants for the SolarEdge Monitoring API."""
 from datetime import timedelta
 
-from homeassistant.const import ENERGY_WATT_HOUR, POWER_WATT
+from homeassistant.const import ENERGY_WATT_HOUR, POWER_WATT, UNIT_PERCENTAGE
 
 DOMAIN = "solaredge"
 
@@ -77,4 +77,5 @@ SENSOR_TYPES = {
         False,
     ],
     "feedin_power": ["FeedIn", "Exported Power", None, "mdi:flash", False],
+    "storage_level": ["STORAGE", "Storage Level", UNIT_PERCENTAGE, None, False],
 }
