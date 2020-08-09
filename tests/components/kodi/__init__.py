c25c895083e5f01cb737a2d8965a1d9ddd2e0baf
On Freund <onfreund@gmail.com>
2020-08-09 22:38:43 +0300

    Fix init test

--- tests/components/kodi/__init__.py
+++ tests/components/kodi/__init__.py
@@ -1,6 +1,12 @@
 """Tests for the Kodi integration."""
-from homeassistant.components.kodi.const import DOMAIN
-from homeassistant.const import CONF_HOST
+from homeassistant.components.kodi.const import CONF_WS_PORT, DOMAIN
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_SSL,
+    CONF_USERNAME,
+)
 
 from .util import MockConnection
 
@@ -12,11 +18,16 @@ async def init_integration(hass) -> MockConfigEntry:
     """Set up the Kodi integration in Home Assistant."""
     entry_data = {
         CONF_HOST: "1.1.1.1",
+        CONF_PORT: 8080,
+        CONF_WS_PORT: 9090,
+        CONF_USERNAME: "user",
+        CONF_PASSWORD: "pass",
+        CONF_SSL: False,
     }
     entry = MockConfigEntry(domain=DOMAIN, data=entry_data)
-    with patch(
-        "homeassistant.components.kodi.Kodi.ping",
-        return_value=True,
+    with patch("homeassistant.components.kodi.Kodi.ping", return_value=True,), patch(
+        "homeassistant.components.kodi.Kodi.get_application_properties",
+        return_value={"version": {"major": 18, "minor": 18}},
     ), patch(
         "homeassistant.components.kodi.Kodi.get_application_properties",
         return_value={"version": {"major": 1, "minor": 1}},
