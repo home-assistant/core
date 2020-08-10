2c612a630435c9444243fdfab93a03579b4a6de4
David Mulcahey <david.mulcahey@me.com>
2020-08-10 13:39:39 -0400

    additional review comments

--- homeassistant/components/flo/config_flow.py
+++ homeassistant/components/flo/config_flow.py
@@ -49,7 +49,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             try:
                 info = await validate_input(self.hass, user_input)
-
+                await self.async_set_unique_id(user_input[CONF_USERNAME])
+                self._abort_if_unique_id_configured()
                 return self.async_create_entry(title=info["title"], data=user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
