ce6ea04f9639851ef14da4933f3f983408b41ca4
Graham Wetzler <graham@wetzler.dev>
2020-08-10 21:07:16 +0000

    Update config flow tests to reach 100% coverage

--- tests/components/smart_meter_texas/test_config_flow.py
+++ tests/components/smart_meter_texas/test_config_flow.py
@@ -1,13 +1,19 @@
 """Test the Smart Meter Texas config flow."""
-from homeassistant import config_entries, setup
-from homeassistant.components.smart_meter_texas.config_flow import (
-    CannotConnect,
-    InvalidAuth,
+import asyncio
+
+from aiohttp import ClientError
+import pytest
+from smart_meter_texas.exceptions import (
+    SmartMeterTexasAPIError,
+    SmartMeterTexasAuthError,
 )
+
+from homeassistant import config_entries, setup
 from homeassistant.components.smart_meter_texas.const import DOMAIN
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 
 from tests.async_mock import patch
+from tests.common import MockConfigEntry
 
 TEST_LOGIN = {CONF_USERNAME: "test-username", CONF_PASSWORD: "test-password"}
 
@@ -46,7 +52,7 @@ async def test_form_invalid_auth(hass):
     )
 
     with patch(
-        "smart_meter_texas.Client.authenticate", side_effect=InvalidAuth,
+        "smart_meter_texas.Client.authenticate", side_effect=SmartMeterTexasAuthError,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"], TEST_LOGIN,
@@ -56,14 +62,17 @@ async def test_form_invalid_auth(hass):
     assert result2["errors"] == {"base": "invalid_auth"}
 
 
-async def test_form_cannot_connect(hass):
+@pytest.mark.parametrize(
+    "side_effect", [asyncio.TimeoutError, ClientError, SmartMeterTexasAPIError]
+)
+async def test_form_cannot_connect(hass, side_effect):
     """Test we handle cannot connect error."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
     with patch(
-        "smart_meter_texas.Client.authenticate", side_effect=CannotConnect,
+        "smart_meter_texas.Client.authenticate", side_effect=side_effect,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"], TEST_LOGIN
@@ -71,3 +80,41 @@ async def test_form_cannot_connect(hass):
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_unknown_exception(hass):
+    """Test base exception is handled."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "smart_meter_texas.Client.authenticate", side_effect=Exception,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"], TEST_LOGIN,
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "unknown"}
+
+
+async def test_form_duplicate_account(hass):
+    """Test that a duplicate account cannot be configured."""
+    MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="user123",
+        data={"username": "user123", "password": "password123"},
+    ).add_to_hass(hass)
+
+    with patch(
+        "smart_meter_texas.Client.authenticate", return_value=True,
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+            data={"username": "user123", "password": "password123"},
+        )
+
+    assert result["type"] == "abort"
+    assert result["reason"] == "already_configured"
