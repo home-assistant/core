68c0025283a35a190726af8c97f4285606aa5957
Franck Nijhof <git@frenck.dev>
2020-08-10 18:20:25 +0200

    Process review comments

--- homeassistant/components/http/forwarded.py
+++ homeassistant/components/http/forwarded.py
@@ -58,15 +58,15 @@ def setup_forwarded(app, trusted_proxies):
       - If an empty X-Forwarded-Proto is provided, or an empty element in the list,
         an HTTP 400 status code is thrown.
     """
-    #
+
     @middleware
     async def forwarded_middleware(request, handler):
         """Process forwarded data by a reverse proxy."""
         overrides = {}
 
         # Handle X-Forwarded-For
-        forwarded_for = request.headers.getall(X_FORWARDED_FOR, [])
-        if not forwarded_for:
+        forwarded_for_headers = request.headers.getall(X_FORWARDED_FOR, [])
+        if not forwarded_for_headers:
             # No forwarding headers, continue as normal
             return await handler(request)
 
@@ -75,50 +75,56 @@ def setup_forwarded(app, trusted_proxies):
         if not any(connected_ip in trusted_proxy for trusted_proxy in trusted_proxies):
             _LOGGER.warning(
                 "Received X-Forwarded-For header from untrusted proxy %s, headers not processed",
-                str(connected_ip),
+                connected_ip,
             )
             # Not trusted, continue as normal
             return await handler(request)
 
         # Multiple X-Forwarded-For headers
-        if len(forwarded_for) > 1:
-            _LOGGER.error("Too many headers for X-Forwarded-For: %s", forwarded_for)
+        if len(forwarded_for_headers) > 1:
+            _LOGGER.error(
+                "Too many headers for X-Forwarded-For: %s", forwarded_for_headers
+            )
             raise HTTPBadRequest
 
         # Process X-Forwarded-For from the right side (by reversing the list)
-        forwarded_for = list(reversed(forwarded_for[0].split(",")))
+        forwarded_for_split = list(reversed(forwarded_for_headers[0].split(",")))
         try:
-            forwarded_for = [ip_address(addr.strip()) for addr in forwarded_for]
+            forwarded_for = [ip_address(addr.strip()) for addr in forwarded_for_split]
         except ValueError:
-            _LOGGER.error("Invalid IP address in X-Forwarded-For: %s", forwarded_for)
+            _LOGGER.error(
+                "Invalid IP address in X-Forwarded-For: %s", forwarded_for_headers[0]
+            )
             raise HTTPBadRequest
 
         # Find the last trusted index in the X-Forwarded-For list
-        index = 0
+        forwarded_for_index = 0
         for forwarded_ip in forwarded_for:
             if any(forwarded_ip in trusted_proxy for trusted_proxy in trusted_proxies):
-                index += 1
+                forwarded_for_index += 1
                 continue
             overrides["remote"] = str(forwarded_ip)
             break
 
         # If all the IP addresses are from trusted networks, take the left-most.
         if "remote" not in overrides:
-            index = -1
+            forwarded_for_index = -1
             overrides["remote"] = str(forwarded_for[-1])
 
         # Handle X-Forwarded-Proto
-        forwarded_proto = list(reversed(request.headers.getall(X_FORWARDED_PROTO, [])))
-        if forwarded_proto:
-            if len(forwarded_proto) > 1:
+        forwarded_proto_headers = list(
+            reversed(request.headers.getall(X_FORWARDED_PROTO, []))
+        )
+        if forwarded_proto_headers:
+            if len(forwarded_proto_headers) > 1:
                 _LOGGER.error(
-                    "Too many headers for X-Forward-Proto: %s", forwarded_proto
+                    "Too many headers for X-Forward-Proto: %s", forwarded_proto_headers
                 )
                 raise HTTPBadRequest
 
             forwarded_proto = [
                 proto
-                for proto in (p.strip() for p in forwarded_proto[0].split(","))
+                for proto in (p.strip() for p in forwarded_proto_headers[0].split(","))
                 if proto
             ]
 
@@ -129,29 +135,31 @@ def setup_forwarded(app, trusted_proxies):
                     "Incorrect number of elements in X-Forward-Proto. Expected 1 or %d, got %d: %s",
                     len(forwarded_for),
                     len(forwarded_proto),
-                    forwarded_proto,
+                    forwarded_proto_headers[0],
                 )
                 raise HTTPBadRequest
 
             # Ideally this should take the scheme corresponding to the entry
             # in X-Forwarded-For that was chosen, but some proxies only retain
             # one element. In that case, use what we have.
-            if index != -1 and index >= len(forwarded_proto):
-                index = -1
+            if forwarded_for_index != -1 and forwarded_for_index >= len(
+                forwarded_proto
+            ):
+                forwarded_for_index = -1
 
-            overrides["scheme"] = forwarded_proto[index]
+            overrides["scheme"] = forwarded_proto[forwarded_for_index]
 
         # Handle X-Forwarded-Host
-        forwarded_host = request.headers.getall(X_FORWARDED_HOST, [])
-        if forwarded_host:
+        forwarded_host_headers = request.headers.getall(X_FORWARDED_HOST, [])
+        if forwarded_host_headers:
             # Multiple X-Forwarded-Host headers
-            if len(forwarded_host) > 1:
+            if len(forwarded_host_headers) > 1:
                 _LOGGER.error(
-                    "Too many headers for X-Forwarded-Host: %s", forwarded_host
+                    "Too many headers for X-Forwarded-Host: %s", forwarded_host_headers
                 )
                 raise HTTPBadRequest
 
-            forwarded_host = forwarded_host[0].strip()
+            forwarded_host = forwarded_host_headers[0].strip()
             if not forwarded_host:
                 _LOGGER.error("Empty value received in X-Forward-Host header")
                 raise HTTPBadRequest
