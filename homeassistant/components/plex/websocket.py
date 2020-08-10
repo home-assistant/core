8e05371a5e982e2715cc5e4eb2d742eebc767be7
Jason Lawrence <jjlawren@users.noreply.github.com>
2020-08-10 14:05:31 -0500

    Refactor to prepare for browse_media endpoint

--- homeassistant/components/plex/websocket.py
+++ homeassistant/components/plex/websocket.py
@@ -5,13 +5,20 @@ from homeassistant.components import websocket_api
 
 from .const import DOMAIN as PLEX_DOMAIN, SERVERS
 
+EXPANDABLES = ["album", "artist", "playlist", "season", "show"]
+
 
 @websocket_api.async_response
 @websocket_api.websocket_command(
     {
         vol.Required("type"): "plex/lookup_media",
         vol.Required("media_type"): str,
-        vol.Required("library_name"): str,
+        # Media ID lookup
+        vol.Optional("media_content_id"): int,
+        # Playlist field
+        vol.Optional("playlist_name"): str,
+        # Required for all searches below
+        vol.Optional("library_name"): str,
         # Music fields
         vol.Optional("artist_name"): str,
         vol.Optional("album_name"): str,
@@ -24,33 +31,94 @@ from .const import DOMAIN as PLEX_DOMAIN, SERVERS
         vol.Optional("episode_number"): int,
         # Movie/video field
         vol.Optional("video_name"): str,
-        # Playlist field
-        vol.Optional("playlist_name"): str,
     }
 )
 async def websocket_lookup_media(hass, connection, msg):
     """Lookup and return a media search."""
-    plex_server = list(hass.data[PLEX_DOMAIN][SERVERS].values())[0]
-
     payload = dict(msg)
     payload.pop("id")
     payload.pop("type")
-    media_type = payload.pop("media_type").lower()
+    if "media_content_id" in payload:
+        payload["plex_key"] = payload.pop("media_content_id")
 
-    def lookup_media():
-        """Wrap lookup call with kwarg payload."""
-        return plex_server.lookup_media(media_type, **payload)
+    plex_server = _lookup_plex_server(hass, connection, msg)
 
-    media = await hass.async_add_executor_job(lookup_media)
+    media_info = await hass.async_add_executor_job(
+        _build_item_response, plex_server, payload
+    )
 
-    if media:
-        media_info = {
-            "title": media.title,
-            "id": media.ratingKey,
-            "type": media.type,
-        }
+    if media_info:
         connection.send_result(msg["id"], media_info)
     else:
         connection.send_error(
             msg["id"], websocket_api.const.ERR_NOT_FOUND, "No results"
         )
+
+
+def _lookup_plex_server(hass, connection, msg):
+    """Return a valid Plex server or send websocket response."""
+    server_id = msg.get("server_id")
+
+    plex_servers = list(hass.data[PLEX_DOMAIN][SERVERS].values())
+    if len(plex_servers) == 1:
+        return plex_servers[0]
+    elif server_id is None:
+        response = []
+        for server in plex_servers:
+            response.append(
+                {
+                    "title": server.name,
+                    "id": server.machineIdentifier,
+                    "type": "server",
+                    "can_play": False,
+                    "can_expand": True,
+                }
+            )
+        connection.send_result(msg["id"], response)
+        return None
+    else:
+        try:
+            plex_server = next(
+                x for x in plex_servers if x.machineIdentifier == server_id
+            )
+        except StopIteration:
+            connection.send_error(
+                msg["id"],
+                websocket_api.const.ERR_NOT_FOUND,
+                f"Plex server with ID {server_id} not found",
+            )
+            return None
+        else:
+            return plex_server
+
+
+def _build_item_response(plex_server, payload):
+    """Build the response payload for the provided media query."""
+    media = plex_server.lookup_media(**payload)
+
+    if media is None:
+        return None
+
+    media_info = {
+        "title": media.title,
+        "media_content_id": media.ratingKey,
+        "media_content_type": "plex",
+        "type": media.type,
+        "can_play": True,
+    }
+    if media.type in EXPANDABLES:
+        media_info["can_expand"] = True
+        child_items = []
+        for item in media:
+            child_info = {
+                "title": item.title,
+                "media_content_id": item.ratingKey,
+                "media_content_type": "plex",
+                "type": item.type,
+                "can_play": True,
+            }
+            if item.type in EXPANDABLES:
+                child_info["can_expand"] = True
+            child_items.append(child_info)
+        media_info["contains"] = child_items
+    return media_info
