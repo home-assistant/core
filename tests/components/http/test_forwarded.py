50e4837242294a9cfb60b1ea46ec86f36347e9a5
Franck Nijhof <git@frenck.dev>
2020-08-10 08:48:26 +0200

    Adjust forwarded tests setup signature

--- tests/components/http/test_forwarded.py
+++ tests/components/http/test_forwarded.py
@@ -16,7 +16,7 @@ async def test_ignore_x_forwarded_for(aiohttp_client):
     """Test that we get the IP from the transport."""
     app = web.Application()
     app.router.add_get("/", mock_handler)
-    setup_forwarded(app, False, [])
+    setup_forwarded(app, [])
 
     mock_api_client = await aiohttp_client(app)
 
@@ -30,7 +30,7 @@ async def test_use_x_forwarded_for_without_trusted_proxy(aiohttp_client):
     """Test that we get the IP from the transport."""
     app = web.Application()
     app.router.add_get("/", mock_handler)
-    setup_forwarded(app, True, [])
+    setup_forwarded(app, [])
 
     mock_api_client = await aiohttp_client(app)
 
@@ -44,7 +44,7 @@ async def test_use_x_forwarded_for_with_trusted_proxy(aiohttp_client):
     """Test that we get the IP from the transport."""
     app = web.Application()
     app.router.add_get("/", mock_handler)
-    setup_forwarded(app, True, [ip_network("127.0.0.1")])
+    setup_forwarded(app, [ip_network("127.0.0.1")])
 
     mock_api_client = await aiohttp_client(app)
 
@@ -58,7 +58,7 @@ async def test_use_x_forwarded_for_with_untrusted_proxy(aiohttp_client):
     """Test that we get the IP from the transport."""
     app = web.Application()
     app.router.add_get("/", mock_handler)
-    setup_forwarded(app, True, [ip_network("1.1.1.1")])
+    setup_forwarded(app, [ip_network("1.1.1.1")])
 
     mock_api_client = await aiohttp_client(app)
 
@@ -72,7 +72,7 @@ async def test_use_x_forwarded_for_with_spoofed_header(aiohttp_client):
     """Test that we get the IP from the transport."""
     app = web.Application()
     app.router.add_get("/", mock_handler)
-    setup_forwarded(app, True, [ip_network("127.0.0.1")])
+    setup_forwarded(app, [ip_network("127.0.0.1")])
 
     mock_api_client = await aiohttp_client(app)
 
@@ -88,7 +88,7 @@ async def test_use_x_forwarded_for_with_nonsense_header(aiohttp_client):
     """Test that we get the IP from the transport."""
     app = web.Application()
     app.router.add_get("/", mock_handler)
-    setup_forwarded(app, True, [ip_network("127.0.0.1")])
+    setup_forwarded(app, [ip_network("127.0.0.1")])
 
     mock_api_client = await aiohttp_client(app)
 
