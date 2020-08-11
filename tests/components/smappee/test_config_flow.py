3b64dd1656bf12381f168f7ac90166c8df47dab2
Bram Devooght <b.devooght@smappee.com>
2020-08-11 15:02:29 +0200

    Series-2 devices are now supported

--- tests/components/smappee/test_config_flow.py
+++ tests/components/smappee/test_config_flow.py
@@ -119,7 +119,7 @@ async def test_full_user_wrong_mdns(hass):
     """Test we abort user flow if unsupported mDNS name got resolved."""
     with patch("pysmappee.api.SmappeeLocalApi.logon", return_value={}), patch(
         "pysmappee.api.SmappeeLocalApi.load_advanced_config",
-        return_value=[{"key": "mdnsHostName", "value": "Smappee2006000212"}],
+        return_value=[{"key": "mdnsHostName", "value": "Smappee5010000001"}],
     ), patch(
         "pysmappee.api.SmappeeLocalApi.load_command_control_config", return_value=[]
     ), patch(
