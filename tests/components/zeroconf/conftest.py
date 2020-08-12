6d78b9958f944b746a2a9a7b88214a5a5cf6affc
J. Nick Koston <nick@koston.org>
2020-08-12 13:22:12 +0000

    move conftest one level up

--- tests/components/zeroconf/conftest.py
+++ tests/components/zeroconf/conftest.py
@@ -1,14 +1,7 @@
 """conftest for zeroconf."""
 import pytest
 
-from homeassistant.components import zeroconf
-
 from tests.async_mock import patch
-
-zeroconf.orig_install_multiple_zeroconf_catcher = (
-    zeroconf.install_multiple_zeroconf_catcher
-)
-zeroconf.install_multiple_zeroconf_catcher = lambda zc: None
 
 
 @pytest.fixture
