86aa758ecd18a5ba7aac21e6e53390736829677c
David F. Mulcahey <david.mulcahey@me.com>
2020-08-13 07:26:47 -0400

    Add binary sensor support to the Flo integration (#38267)
    * update device
    
    * add binary sensor
    
    * updates post rebase
    
    * fix entity type post rebase
    
    * fix post rebase
    
    * fix add entities
    
    * fix name
    
    * review comments

--- homeassistant/components/flo/__init__.py
+++ homeassistant/components/flo/__init__.py
@@ -19,7 +19,7 @@ CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = ["sensor"]
+PLATFORMS = ["binary_sensor", "sensor"]
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
