1b6ced2322044950c850a23aaa351810844ed416
On Freund <onfreund@gmail.com>
2020-08-16 07:57:26 +0300

    Fall back to entry id

--- homeassistant/components/kodi/media_player.py
+++ homeassistant/components/kodi/media_player.py
@@ -216,6 +216,8 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     kodi = data[DATA_KODI]
     name = config_entry.data[CONF_NAME]
     uid = config_entry.unique_id
+    if uid is None:
+        uid = config_entry.entry_id
 
     entity = KodiEntity(connection, kodi, name, uid, version)
     async_add_entities([entity])
