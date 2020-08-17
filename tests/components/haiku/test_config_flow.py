f2b3c65a23166fd7560bd77d5b27c0f48e2e0b93
Josh <43808099+josh-codes@users.noreply.github.com>
2020-08-17 19:38:27 +0800

    Update test_config_flow.py

--- tests/components/haiku/test_config_flow.py
+++ tests/components/haiku/test_config_flow.py
@@ -1,14 +1,9 @@
 """Test config flow for Haiku."""
 import asyncio
-import concurrent.futures
 
 from haiku import discover
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    future = executor.submit(runondifthread)
-    result = future.result()
+result = asyncio.run(discover.discover())
 
-def runondifthread():
-    asyncio.run(discover.discover())
 if result is False:
     print("Socket Error")
