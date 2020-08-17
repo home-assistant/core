00b4c8cf1bddc597b3bfd2805c077a5536a0535c
Josh <43808099+josh-codes@users.noreply.github.com>
2020-08-17 19:51:28 +0800

    Formating

--- homeassistant/components/haiku/config_flow.py
+++ homeassistant/components/haiku/config_flow.py
@@ -2,19 +2,18 @@
 
 from homeassistant import config_entries
 from homeassistant.helpers import config_entry_flow
-from haiku.discover import *
+from haiku.discover import discover
 from .const import DOMAIN
 
 
 async def _async_has_devices(hass) -> bool:
     """Return if there are devices that can be discovered."""
     devices = await discover()
-    if devices == [[],[]]:
+    if devices == [[], []]:
         result = False
     else:
-        result =  True
+        result = True
     return result
-
 
 config_entry_flow.register_discovery_flow(
     DOMAIN, "Haiku", _async_has_devices, config_entries.CONN_CLASS_UNKNOWN
