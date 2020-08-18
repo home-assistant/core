b7ebd721bfcb7a63816b8c4cfbebeb942ab81501
oncleben31 <oncleben31@users.noreply.github.com>
2020-08-18 00:09:07 +0000

    Set the coordinator for 1 hour rain forecast in English

--- homeassistant/components/meteo_france/__init__.py
+++ homeassistant/components/meteo_france/__init__.py
@@ -85,7 +85,9 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
 
     async def _async_update_data_rain():
         """Fetch data from API endpoint."""
-        return await hass.async_add_executor_job(client.get_rain, latitude, longitude)
+        return await hass.async_add_executor_job(
+            client.get_rain, latitude, longitude, "en"
+        )
 
     async def _async_update_data_alert():
         """Fetch data from API endpoint."""
