4cc177617bdadde2827628b584b58b1c723989c2
oncleben31 <oncleben31@users.noreply.github.com>
2020-08-18 00:11:03 +0000

    Attribut dict keys shorten

--- homeassistant/components/meteo_france/sensor.py
+++ homeassistant/components/meteo_france/sensor.py
@@ -189,7 +189,7 @@ class MeteoFranceRainSensor(MeteoFranceSensor):
         return {
             ATTR_NEXT_RAIN_DT_REF: dt_util.utc_from_timestamp(reference_dt).isoformat(),
             ATTR_NEXT_RAIN_1_HOUR_FORECAST: {
-                "in {} min.".format(int((item["dt"] - reference_dt) / 60)): item["desc"]
+                "{} min".format(int((item["dt"] - reference_dt) / 60)): item["desc"]
                 for item in self.coordinator.data.forecast
             },
             ATTR_ATTRIBUTION: ATTRIBUTION,
