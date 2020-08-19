4b41b12f10aadf73dbbcd92e33127bf983d96363
Ron Klinkien <ron@cyberjunky.nl>
2020-08-19 18:08:42 +0000

    Fixed basic test

--- tests/components/fireservicerota/test_config_flow.py
+++ tests/components/fireservicerota/test_config_flow.py
@@ -1,31 +1,20 @@
 """Test the FireServiceRota config flow."""
-from homeassistant import config_entries, setup
-from pyfireservicerota import FireServiceRota, InvalidAuthError
-from homeassistant.const import CONF_PASSWORD, CONF_TOKEN, CONF_URL, CONF_USERNAME
+from pyfireservicerota import InvalidAuthError
+
 from homeassistant import data_entry_flow
-from homeassistant.components.fireservicerota.const import (
+from homeassistant.components.fireservicerota.const import (  # pylint: disable=unused-import
     DOMAIN,
-    URL_LIST,
-)  # pylint: disable=unused-import
+)
+from homeassistant.const import CONF_PASSWORD, CONF_URL, CONF_USERNAME
 
 from tests.async_mock import patch
-import pytest
+from tests.common import MockConfigEntry
 
 MOCK_CONF = {
     CONF_USERNAME: "my@email.address",
     CONF_PASSWORD: "mypassw0rd",
-    CONF_URL: "https://brandweerrooster.nl",
+    CONF_URL: "brandweerrooster.nl",
 }
-
-
-# @pytest.fixture(name="mock_fireservicerota")
-# def mock_fireservicerota():
-#     """Mock FireServiceRota."""
-#     with patch(
-#         "homeassistant.components.fireservicerota.config_flow.FireServiceRota",
-#     ) as fireservice:
-#         fireservice.return_value.get_full_name.return_value = MOCK_CONF[CONF_ID]
-#         yield fireservice.return_value
 
 
 async def test_show_form(hass):
@@ -37,60 +26,27 @@ async def test_show_form(hass):
     assert result["step_id"] == "user"
 
 
-async def test_step_user(hass, mock_fireservicerota):
-    """Test registering an integration and finishing flow works."""
+async def test_abort_if_already_setup(hass):
+    """Test abort if already setup."""
+    entry = MockConfigEntry(
+        domain=DOMAIN, data=MOCK_CONF, unique_id=MOCK_CONF[CONF_USERNAME]
+    )
+    entry.add_to_hass(hass)
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": "user"}, data=MOCK_CONF
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "already_configured"
+
+
+async def test_invalid_credentials(hass):
+    """Test that invalid credentials throws an error."""
 
     with patch(
-        "homeassistant.components.fireservicerota.async_setup_entry", return_value=True
-    ), patch("homeassistant.components.fireservicerota.async_setup", return_value=True):
+        "homeassistant.components.fireservicerota.FireServiceRota.request_tokens",
+        side_effect=InvalidAuthError,
+    ):
         result = await hass.config_entries.flow.async_init(
             DOMAIN, context={"source": "user"}, data=MOCK_CONF
         )
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["data"] == MOCK_CONF
-
-
-# async def test_full_flow(hass, aiohttp_client, aioclient_mock):
-#     """Check full flow."""
-#     assert await setup.async_setup_component(
-#         hass,
-#         "fireservicerota",
-#         {
-#             "fireservicerota": {"client_id": CLIENT_ID, "client_secret": CLIENT_SECRET},
-#             "http": {"base_url": "https://example.com"},
-#         },
-#     )
-
-#     result = await hass.config_entries.flow.async_init(
-#         "fireservicerota", context={"source": config_entries.SOURCE_USER}
-#     )
-#     state = config_entry_oauth2_flow._encode_jwt(hass, {"flow_id": result["flow_id"]})
-
-#     assert result["url"] == (
-#         f"{OAUTH2_AUTHORIZE}?response_type=code&client_id={CLIENT_ID}"
-#         "&redirect_uri=https://example.com/auth/external/callback"
-#         f"&state={state}"
-#     )
-
-#     client = await aiohttp_client(hass.http.app)
-#     resp = await client.get(f"/auth/external/callback?code=abcd&state={state}")
-#     assert resp.status == 200
-#     assert resp.headers["content-type"] == "text/html; charset=utf-8"
-
-#     aioclient_mock.post(
-#         OAUTH2_TOKEN,
-#         json={
-#             "refresh_token": "mock-refresh-token",
-#             "access_token": "mock-access-token",
-#             "type": "Bearer",
-#             "expires_in": 60,
-#         },
-#     )
-
-#     with patch(
-#         "homeassistant.components.fireservicerota.async_setup_entry", return_value=True
-#     ) as mock_setup:
-#         await hass.config_entries.flow.async_configure(result["flow_id"])
-
-#     assert len(hass.config_entries.async_entries(DOMAIN)) == 1
-#     assert len(mock_setup.mock_calls) == 1
+        assert result["errors"] == {"base": "invalid_credentials"}
