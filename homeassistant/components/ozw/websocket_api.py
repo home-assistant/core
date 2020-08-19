599e8cc00a282bb4a04a33186420e2b7a84722fe
Martin Hjelmare <marhje52@gmail.com>
2020-08-19 18:32:58 +0200

    Register migration command

--- homeassistant/components/ozw/websocket_api.py
+++ homeassistant/components/ozw/websocket_api.py
@@ -63,6 +63,7 @@ ATTR_NEIGHBORS = "neighbors"
 @callback
 def async_register_api(hass):
     """Register all of our api endpoints."""
+    websocket_api.async_register_command(hass, websocket_migrate_zwave)
     websocket_api.async_register_command(hass, websocket_get_instances)
     websocket_api.async_register_command(hass, websocket_get_nodes)
     websocket_api.async_register_command(hass, websocket_network_status)
@@ -180,7 +181,7 @@ def _get_config_params(node, *args):
         vol.Optional(DRY_RUN, default=True): vol.Coerce(bool),
     }
 )
-async def migrate_zwave(hass, connection, msg):
+async def websocket_migrate_zwave(hass, connection, msg):
     """Migrate the zwave integration device and entity data to ozw integration."""
     if "zwave" not in hass.config.components:
         _LOGGER.error("Can not migrate, zwave integration is not loaded")
