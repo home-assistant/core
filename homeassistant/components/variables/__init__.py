933db9fc829b1404a5362bd09d96b91207319b57
Aaron Bach <bachya1208@gmail.com>
2020-08-20 11:03:03 -0600

    Use a manager object

--- homeassistant/components/variables/__init__.py
+++ homeassistant/components/variables/__init__.py
@@ -1,5 +1,6 @@
 """The Variables integration."""
 import logging
+from typing import Union
 
 import voluptuous as vol
 
@@ -19,12 +20,13 @@ DOMAIN = "variables"
 
 STORAGE_KEY = DOMAIN
 STORAGE_VERSION = 1
+STORAGE_SAVE_DELAY = 10
 
 DATA_STORAGE = "storage"
 
-TOPIC_VARIABLE_SET = f"{DOMAIN}_set_{0}"
+TOPIC_VARIABLE_SET = "variables_set_{0}"
 
-UNIQUE_ID_TEMPLATE = f"{DOMAIN}_{0}"
+UNIQUE_ID_TEMPLATE = "variables_{0}"
 
 CONF_KEY = "key"
 CONF_VALUE = "value"
@@ -35,6 +37,56 @@ SET_VARIABLE_SCHEMA = vol.Schema(
 )
 
 CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Variables component."""
+    variable_manager = VariableManager(hass)
+    await variable_manager.async_init()
+
+    entity_component = EntityComponent(_LOGGER, DOMAIN, hass)
+
+    async def async_remove_variable(call: ServiceCall):
+        """Remove a variable."""
+        key = call.data[CONF_KEY]
+
+        value = variable_manager.async_get(key)
+        if not value:
+            _LOGGER.warning("Can't delete variable that doesn't exist: %s", key)
+            return
+
+        _LOGGER.debug("Removing variable: %s", key)
+
+        variable_manager.async_remove(key)
+
+        ent_reg = await entity_registry.async_get_registry(hass)
+        ent_to_remove = ent_reg.async_get_entity_id(
+            DOMAIN, DOMAIN, UNIQUE_ID_TEMPLATE.format(key)
+        )
+        if ent_to_remove is not None:
+            ent_reg.async_remove(ent_to_remove)
+
+    async def async_set_variable(call: ServiceCall):
+        """Set a variable."""
+        key = call.data[CONF_KEY]
+        value = call.data[CONF_VALUE]
+
+        if variable_manager.async_get(key):
+            _LOGGER.debug('Updating existing variable: %s ("%s")', key, value)
+            async_dispatcher_send(hass, TOPIC_VARIABLE_SET.format(key), value)
+        else:
+            _LOGGER.debug('Creating new variable: %s ("%s")', key, value)
+            await entity_component.async_add_entities([Variable(key, value)])
+
+        variable_manager.async_set(key, value)
+
+    for service, method, schema in [
+        ("remove", async_remove_variable, REMOVE_VARIABLE_SCHEMA),
+        ("set", async_set_variable, SET_VARIABLE_SCHEMA),
+    ]:
+        hass.services.async_register(DOMAIN, service, method, schema=schema)
+
+    return True
 
 
 class VariableStore(Store):
@@ -48,55 +100,41 @@ class VariableStore(Store):
         return old_data
 
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the Variables component."""
-    storage = VariableStore(hass, STORAGE_VERSION, STORAGE_KEY)
-    variables = await storage.async_load() or {}
+class VariableManager:
+    """Define an object to manage variables."""
 
-    entity_component = EntityComponent(_LOGGER, DOMAIN, hass)
+    def __init__(self, hass: HomeAssistant) -> None:
+        """Initialize."""
+        self._storage = VariableStore(hass, STORAGE_VERSION, STORAGE_KEY)
+        self._variables = {}
 
-    async def async_remove_variable(call: ServiceCall):
+    @callback
+    def _async_save(self) -> None:
+        """Save variables to storage."""
+        self._storage.async_delay_save(lambda: self._variables, STORAGE_SAVE_DELAY)
+
+    @callback
+    def async_get(self, key: str) -> Union[str, None]:
+        """Get a variable value. Returns None if the variable doesn't exist."""
+        return self._variables.get(key, None)
+
+    async def async_init(self) -> None:
+        """Set up the manager."""
+        variables = await self._storage.async_load()
+        if variables:
+            self._variables = variables
+
+    @callback
+    def async_set(self, key: str, value: str) -> None:
         """Remove a variable."""
-        key = call.data[CONF_KEY]
+        self._variables[key] = value
+        self._async_save()
 
-        if key not in variables:
-            _LOGGER.error("Can't remove non-existent variable: %s", key)
-            return
-
-        _LOGGER.debug("Removing variable: %s", key)
-
-        ent_reg = await entity_registry.async_get_registry(hass)
-        ent_to_remove = ent_reg.async_get_entity_id(
-            DOMAIN, DOMAIN, UNIQUE_ID_TEMPLATE.format(key)
-        )
-        if ent_to_remove is not None:
-            ent_reg.async_remove(ent_to_remove)
-
-        variables.pop(key)
-        hass.async_create_task(storage.async_save(variables))
-
-    async def async_set_variable(call: ServiceCall):
-        """Set a variable."""
-        key = call.data[CONF_KEY]
-        value = call.data[CONF_VALUE]
-
-        if key in variables:
-            _LOGGER.debug('Updating existing variable: %s ("%s")', key, value)
-            async_dispatcher_send(hass, TOPIC_VARIABLE_SET.format(key), value)
-        else:
-            _LOGGER.debug('Creating new variable: %s ("%s")', key, value)
-            await entity_component.async_add_entities([Variable(key, value)])
-
-        variables[key] = value
-        hass.async_create_task(storage.async_save(variables))
-
-    for service, method, schema in [
-        ("remove", async_remove_variable, REMOVE_VARIABLE_SCHEMA),
-        ("set", async_set_variable, SET_VARIABLE_SCHEMA),
-    ]:
-        hass.services.async_register(DOMAIN, service, method, schema=schema)
-
-    return True
+    @callback
+    def async_remove(self, key: str) -> None:
+        """Remove a variable."""
+        self._variables.pop(key)
+        self._async_save()
 
 
 class Variable(Entity):
