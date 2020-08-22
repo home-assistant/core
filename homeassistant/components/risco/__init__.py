867cffeb9afe0ed8f122b971504b9ae7cc52635a
On Freund <onfreund@gmail.com>
2020-08-22 20:15:03 +0300

    Add risco options flow (#39154)

--- homeassistant/components/risco/__init__.py
+++ homeassistant/components/risco/__init__.py
@@ -6,16 +6,21 @@ import logging
 from pyrisco import CannotConnectError, OperationError, RiscoAPI, UnauthorizedError
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_PASSWORD, CONF_PIN, CONF_USERNAME
+from homeassistant.const import (
+    CONF_PASSWORD,
+    CONF_PIN,
+    CONF_SCAN_INTERVAL,
+    CONF_USERNAME,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import DATA_COORDINATOR, DOMAIN
+from .const import DATA_COORDINATOR, DEFAULT_SCAN_INTERVAL, DOMAIN
 
 PLATFORMS = ["alarm_control_panel"]
-
+UNDO_UPDATE_LISTENER = "undo_update_listener"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -38,11 +43,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         _LOGGER.exception("Failed to login to Risco cloud")
         return False
 
-    coordinator = RiscoDataUpdateCoordinator(hass, risco)
+    scan_interval = entry.options.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
+    coordinator = RiscoDataUpdateCoordinator(hass, risco, scan_interval)
     await coordinator.async_refresh()
+
+    undo_listener = entry.add_update_listener(_update_listener)
 
     hass.data[DOMAIN][entry.entry_id] = {
         DATA_COORDINATOR: coordinator,
+        UNDO_UPDATE_LISTENER: undo_listener,
     }
 
     for component in PLATFORMS:
@@ -65,18 +74,24 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     )
 
     if unload_ok:
+        hass.data[DOMAIN][entry.entry_id][UNDO_UPDATE_LISTENER]()
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
 
 
+async def _update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
+
+
 class RiscoDataUpdateCoordinator(DataUpdateCoordinator):
     """Class to manage fetching risco data."""
 
-    def __init__(self, hass, risco):
+    def __init__(self, hass, risco, scan_interval):
         """Initialize global risco data updater."""
         self.risco = risco
-        interval = timedelta(seconds=30)
+        interval = timedelta(seconds=scan_interval)
         super().__init__(
             hass, _LOGGER, name=DOMAIN, update_interval=interval,
         )
