ad7f0f661acd6ac93af75e48538a3069ae83bd55
On Freund <onfreund@gmail.com>
2020-08-23 21:44:39 +0300

    Custom mapping of Risco states

--- homeassistant/components/risco/config_flow.py
+++ homeassistant/components/risco/config_flow.py
@@ -10,13 +10,20 @@ from homeassistant.const import (
     CONF_PIN,
     CONF_SCAN_INTERVAL,
     CONF_USERNAME,
+    STATE_ALARM_ARMED_AWAY,
+    STATE_ALARM_ARMED_CUSTOM_BYPASS,
+    STATE_ALARM_ARMED_HOME,
+    STATE_ALARM_ARMED_NIGHT,
 )
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import (
     CONF_CODE_ARM_REQUIRED,
     CONF_CODE_DISARM_REQUIRED,
-    DEFAULT_SCAN_INTERVAL,
+    CONF_HA_STATES_TO_RISCO,
+    CONF_RISCO_STATES_TO_HA,
+    DEFAULT_OPTIONS,
+    RISCO_STATES,
 )
 from .const import DOMAIN  # pylint:disable=unused-import
 
@@ -24,6 +31,12 @@ _LOGGER = logging.getLogger(__name__)
 
 
 DATA_SCHEMA = vol.Schema({CONF_USERNAME: str, CONF_PASSWORD: str, CONF_PIN: str})
+HA_STATES = [
+    STATE_ALARM_ARMED_AWAY,
+    STATE_ALARM_ARMED_HOME,
+    STATE_ALARM_ARMED_NIGHT,
+    STATE_ALARM_ARMED_CUSTOM_BYPASS,
+]
 
 
 async def validate_input(hass: core.HomeAssistant, data):
@@ -83,22 +96,20 @@ class RiscoOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         """Initialize."""
         self.config_entry = config_entry
+        self._data = {**DEFAULT_OPTIONS, **config_entry.options}
 
     def _options_schema(self):
-        scan_interval = self.config_entry.options.get(
-            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
-        )
-        code_arm_required = self.config_entry.options.get(CONF_CODE_ARM_REQUIRED, False)
-        code_disarm_required = self.config_entry.options.get(
-            CONF_CODE_DISARM_REQUIRED, False
-        )
-
         return vol.Schema(
             {
-                vol.Required(CONF_SCAN_INTERVAL, default=scan_interval): int,
-                vol.Required(CONF_CODE_ARM_REQUIRED, default=code_arm_required): bool,
                 vol.Required(
-                    CONF_CODE_DISARM_REQUIRED, default=code_disarm_required
+                    CONF_SCAN_INTERVAL, default=self._data[CONF_SCAN_INTERVAL]
+                ): int,
+                vol.Required(
+                    CONF_CODE_ARM_REQUIRED, default=self._data[CONF_CODE_ARM_REQUIRED]
+                ): bool,
+                vol.Required(
+                    CONF_CODE_DISARM_REQUIRED,
+                    default=self._data[CONF_CODE_DISARM_REQUIRED],
                 ): bool,
             }
         )
@@ -106,6 +117,53 @@ class RiscoOptionsFlowHandler(config_entries.OptionsFlow):
     async def async_step_init(self, user_input=None):
         """Manage the options."""
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            self._data = {**self._data, **user_input}
+            return await self.async_step_risco_to_ha()
 
         return self.async_show_form(step_id="init", data_schema=self._options_schema())
+
+    async def async_step_risco_to_ha(self, user_input=None):
+        """Map Risco states to HA states."""
+        if user_input is not None:
+            self._data[CONF_RISCO_STATES_TO_HA] = user_input
+            return await self.async_step_ha_to_risco()
+
+        risco_to_ha = self._data[CONF_RISCO_STATES_TO_HA]
+        options = vol.Schema(
+            {
+                vol.Required(risco_state, default=risco_to_ha[risco_state]): vol.In(
+                    HA_STATES
+                )
+                for risco_state in RISCO_STATES
+            }
+        )
+
+        return self.async_show_form(step_id="risco_to_ha", data_schema=options)
+
+    async def async_step_ha_to_risco(self, user_input=None):
+        """Map HA states to Risco states."""
+        if user_input is not None:
+            self._data[CONF_HA_STATES_TO_RISCO] = user_input
+            return self.async_create_entry(title="", data=self._data)
+
+        options = {}
+        risco_to_ha = self._data[CONF_RISCO_STATES_TO_HA]
+        # we iterate over HA_STATES, instead of set(self._risco_to_ha.values())
+        # to ensure a consistent order
+        for ha_state in HA_STATES:
+            if ha_state not in risco_to_ha.values():
+                continue
+
+            values = [
+                risco_state
+                for risco_state in RISCO_STATES
+                if risco_to_ha[risco_state] == ha_state
+            ]
+            current = self._data[CONF_HA_STATES_TO_RISCO].get(ha_state)
+            if current not in values:
+                current = values[0]
+            options[vol.Required(ha_state, default=current)] = vol.In(values)
+
+        return self.async_show_form(
+            step_id="ha_to_risco", data_schema=vol.Schema(options)
+        )
