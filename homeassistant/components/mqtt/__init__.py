e27f0f4583d1484b8a467d50adbb84ef36877c27
Erik <erik@montnemery.com>
2020-08-23 21:12:48 +0200

    Don't block HA startup

--- homeassistant/components/mqtt/__init__.py
+++ homeassistant/components/mqtt/__init__.py
@@ -911,7 +911,7 @@ class MQTT:
                 )
 
             birth_message = Message(**self.conf[CONF_BIRTH_MESSAGE])
-            self.hass.add_job(publish_birth_message(birth_message))
+            self.hass.create_task(publish_birth_message(birth_message))
 
     def _mqtt_on_message(self, _mqttc, _userdata, msg) -> None:
         """Message received callback."""
