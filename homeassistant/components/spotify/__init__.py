3ba7db1bc902a3f8ddd3fb9a6e570527a6ca972f
Franck Nijhof <frenck@frenck.nl>
2020-08-27 14:52:08 +0200

    Add Spotify re-authentication logic (#5)
    * Reauth
    
    * Add Spotify re-authentication logic
    
    * Delete stale debug lines
    
    * Reset supported features
    
    * Add persistent notification
    
    Co-authored-by: Tobias Sauerwein <cgtobi@gmail.com>

--- homeassistant/components/spotify/__init__.py
+++ homeassistant/components/spotify/__init__.py
@@ -61,6 +61,18 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 
+async def async_migrate_entry(hass, entry):
+    """Handle migration of a previous version config entry."""
+    if entry.version == 1:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN, context={"source": "reauth"}, data=entry.data,
+            )
+        )
+
+    return True
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Spotify from a config entry."""
     implementation = await async_get_config_entry_implementation(hass, entry)
