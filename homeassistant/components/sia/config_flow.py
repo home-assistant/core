f0ca2aa788b492021f3c0aee64f62e56bbd2837d
E.A. van Valkenburg <edvan@microsoft.com>
2020-08-28 14:41:24 +0000

    final comment and small legibility enhancements

--- homeassistant/components/sia/config_flow.py
+++ homeassistant/components/sia/config_flow.py
@@ -55,7 +55,7 @@ def validate_input(data: dict) -> dict:
     return errors
 
 
-def create_schema(step_id: str, user_input: dict, errors: dict) -> vol.Schema:
+def create_schema(step_id: str, data: dict) -> vol.Schema:
     """Create a schema for the next or first form, using the errors to delete erroneous input, but filling the other fields."""
     port_sch = vol.Required(CONF_PORT)
     account_sch = vol.Required(CONF_ACCOUNT)
@@ -64,25 +64,14 @@ def create_schema(step_id: str, user_input: dict, errors: dict) -> vol.Schema:
     zones_sch = vol.Required(CONF_ZONES, default=1)
     additional_sch = vol.Optional(CONF_ADDITIONAL_ACCOUNTS, default=False)
 
-    if user_input:
-        if not errors:
-            errors = {"base": None}
-
+    if data:
         if step_id == "user":
-            port_sch.default = vol.default_factory(user_input[CONF_PORT])
-        if not errors["base"] in [INVALID_ACCOUNT_FORMAT, INVALID_ACCOUNT_LENGTH]:
-            account_sch.default = vol.default_factory(user_input[CONF_ACCOUNT])
-        if (
-            not errors["base"] in [INVALID_KEY_FORMAT, INVALID_KEY_LENGTH]
-            and user_input.get(CONF_ENCRYPTION_KEY, None) is not None
-        ):
-            encryption_sch.default = vol.default_factory(
-                user_input[CONF_ENCRYPTION_KEY]
-            )
-        if errors["base"] != INVALID_PING:
-            ping_sch.default = vol.default_factory(user_input[CONF_PING_INTERVAL])
-        if errors["base"] != INVALID_ZONES:
-            zones_sch.default = vol.default_factory(user_input[CONF_ZONES])
+            port_sch.default = vol.default_factory(data[CONF_PORT])
+        account_sch.default = vol.default_factory(data[CONF_ACCOUNT])
+        if data.get(CONF_ENCRYPTION_KEY, None) is not None:
+            encryption_sch.default = vol.default_factory(data[CONF_ENCRYPTION_KEY])
+        ping_sch.default = vol.default_factory(data[CONF_PING_INTERVAL])
+        zones_sch.default = vol.default_factory(data[CONF_ZONES])
 
     if step_id == "user":
         return vol.Schema(
@@ -111,46 +100,42 @@ class SIAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
-    data = {}
+    DATA = {}
 
     async def async_step_additional_account(self, user_input: dict = None):
-        """Handle the additional account step."""
+        """Handle the additional account step, parse and store the input, get additional accounts if needed and create entry."""
         errors = validate_input(user_input)
-        # validate and return with error
         if not user_input or errors:
-            schema = create_schema("additional_account", user_input, errors)
+            schema = create_schema("additional_account", user_input)
             return self.async_show_form(
                 step_id="additional_account", data_schema=schema, errors=errors,
             )
 
-        # parse the user_input and store in self.data
         add_data = user_input.copy()
         add_data.pop(CONF_ADDITIONAL_ACCOUNTS)
-        self.data[CONF_ACCOUNTS].append(add_data)
+        self.DATA[CONF_ACCOUNTS].append(add_data)
 
-        # call additional accounts if necessary
         if user_input[CONF_ADDITIONAL_ACCOUNTS]:
             return await self.async_step_additional_account()
-        # done
+
+        _LOGGER.debug("Creating SIA entry with data: %s", self.DATA)
         return self.async_create_entry(
-            title=f"SIA Alarm on port {self.data[CONF_PORT]}", data=self.data
+            title=f"SIA Alarm on port {self.DATA[CONF_PORT]}", data=self.DATA
         )
 
     async def async_step_user(self, user_input: dict = None):
-        """Handle the initial step."""
+        """Handle the initial step, parse and store the input, get additional accounts if needed and create entry."""
         errors = validate_input(user_input)
         if not user_input or errors:
-            schema = create_schema("user", user_input, errors)
+            schema = create_schema("user", user_input)
             return self.async_show_form(
                 step_id="user", data_schema=schema, errors=errors,
             )
 
-        # check uniqueness of the setup
         await self.async_set_unique_id(user_input[CONF_PORT])
         self._abort_if_unique_id_configured()
 
-        # parse the user_input and store in self.data
-        self.data = {
+        self.DATA = {
             CONF_PORT: user_input[CONF_PORT],
             CONF_ACCOUNTS: [
                 {
@@ -162,11 +147,10 @@ class SIAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             ],
         }
 
-        # call additional accounts if necessary
         if user_input[CONF_ADDITIONAL_ACCOUNTS]:
             return await self.async_step_additional_account()
 
-        # done
+        _LOGGER.debug("Creating SIA entry with data: %s", self.DATA)
         return self.async_create_entry(
-            title=f"SIA Alarm on port {self.data[CONF_PORT]}", data=self.data
+            title=f"SIA Alarm on port {self.DATA[CONF_PORT]}", data=self.DATA
         )
