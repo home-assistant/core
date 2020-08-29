7d2306ce5ce64c36318d5c112a0334c409af8f8a
Chris Talkington <chris@talkingtontech.com>
2020-08-29 14:05:28 -0500

    Update __init__.py

--- homeassistant/components/nzbget/__init__.py
+++ homeassistant/components/nzbget/__init__.py
@@ -38,6 +38,7 @@ from .const import (
     SERVICE_RESUME,
     SERVICE_SET_SPEED,
 )
+from .coordinator import NZBGetDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -170,81 +171,6 @@ def _async_register_services(
 async def _async_update_listener(hass: HomeAssistantType, entry: ConfigEntry) -> None:
     """Handle options update."""
     await hass.config_entries.async_reload(entry.entry_id)
-
-
-class NZBGetDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching NZBGet data."""
-
-    def __init__(
-        self,
-        hass: HomeAssistantType,
-        *,
-        config: dict,
-        options: dict
-    ):
-        """Initialize global NZBGet data updater."""
-        self.nzbget = NZBGetAPI(
-            config[CONF_HOST],
-            config[CONF_USERNAME] if config[CONF_USERNAME] != "" else None,
-            config[CONF_PASSWORD] if config[CONF_PASSWORD] != "" else None,
-            config[CONF_SSL],
-            config[CONF_VERIFY_SSL],
-            config[CONF_PORT],
-        )
-
-        self._completed_downloads_init = False
-        self._completed_downloads = {}
-
-        update_interval = timedelta(seconds=options[CONF_SCAN_INTERVAL])
-
-        super().__init__(
-            hass, _LOGGER, name=DOMAIN, update_interval=update_interval,
-        )
-
-    def _check_completed_downloads(self, history):
-        """Check history for newly completed downloads."""
-        actual_completed_downloads = {
-            (x["Name"], x["Category"], x["Status"]) for x in history
-        }
-
-        if self._completed_downloads_init:
-            tmp_completed_downloads = list(
-                actual_completed_downloads.difference(self._completed_downloads)
-            )
-
-            for download in tmp_completed_downloads:
-                self.hass.bus.fire(
-                    "nzbget_download_complete",
-                    {
-                        "name": download[0],
-                        "category": download[1],
-                        "status": download[2],
-                    },
-                )
-
-        self._completed_downloads = actual_completed_downloads
-        self._completed_downloads_init = True
-
-    async def _async_update_data(self) -> dict:
-        """Fetch data from NZBGet."""
-
-        def _update_data() -> dict:
-            """Fetch data from NZBGet via sync functions."""
-            status = self.nzbget.status()
-            history = self.nzbget.history()
-
-            self._check_completed_downloads(history)
-
-            return {
-                "status": status,
-                "downloads": history,
-            }
-
-        try:
-            async with timeout(4):
-                return await self.hass.async_add_executor_job(_update_data)
-        except NZBGetAPIException as error:
-            raise UpdateFailed(f"Invalid response from API: {error}")
 
 
 class NZBGetEntity(Entity):
