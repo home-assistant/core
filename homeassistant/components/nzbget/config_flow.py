4f3d70990b8ba3e27bd99c97915def179307fe72
Chris Talkington <chris@talkingtontech.com>
2020-08-29 14:00:49 -0500

    Update config_flow.py

--- homeassistant/components/nzbget/config_flow.py
+++ homeassistant/components/nzbget/config_flow.py
@@ -113,7 +113,9 @@ class NZBGetConfigFlow(ConfigFlow, domain=DOMAIN):
             ] = bool
 
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema(data_schema), errors=errors or {},
+            step_id="user",
+            data_schema=vol.Schema(data_schema),
+            errors=errors or {},
         )
 
 
