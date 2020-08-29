b7b73fc7ddf9d123e8a55bf3c4966b0978350bf0
Ville Skyttä <ville.skytta@iki.fi>
2020-08-29 09:23:55 +0300

    Upgrade isort to 5.4.2 (#37939)

--- homeassistant/components/homekit/__init__.py
+++ homeassistant/components/homekit/__init__.py
@@ -606,6 +606,7 @@ class HomeKit:
             type_cameras,
             type_covers,
             type_fans,
+            type_humidifiers,
             type_lights,
             type_locks,
             type_media_players,
@@ -613,7 +614,6 @@ class HomeKit:
             type_sensors,
             type_switches,
             type_thermostats,
-            type_humidifiers,
         )
 
         for state in bridged_states:
