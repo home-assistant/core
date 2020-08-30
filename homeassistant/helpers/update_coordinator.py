bd34f01adbfb761da49a5f7e03ab2ae24ef20848
Paulus Schoutsen <balloob@gmail.com>
2020-08-30 12:30:39 +0000

    CoordinatorEntity to call super added_to_hass

--- homeassistant/helpers/update_coordinator.py
+++ homeassistant/helpers/update_coordinator.py
@@ -211,6 +211,7 @@ class CoordinatorEntity(entity.Entity):
 
     async def async_added_to_hass(self) -> None:
         """When entity is added to hass."""
+        await super().async_added_to_hass()
         self.async_on_remove(
             self.coordinator.async_add_listener(self.async_write_ha_state)
         )
