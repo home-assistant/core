aedc157131845f12b1f0cf894056eefd206fda63
Rob Bierbooms <mail@robbierbooms.nl>
2020-08-30 18:40:22 +0000

    Add config flow for import

---
+++ homeassistant/components/dsmr/config_flow.py
@@ -0,0 +1,27 @@
+"""Config flow for DSMR integration."""
+import logging
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_PORT
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class DSMRFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for DSMR."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
+
+    async def async_step_import(self, import_config=None):
+        """Handle the initial step."""
+        if CONF_HOST in import_config:
+            name = f"{import_config[CONF_HOST]}:{import_config[CONF_PORT]}"
+        else:
+            name = import_config[CONF_PORT]
+
+        await self.async_set_unique_id(name)
+        self._abort_if_unique_id_configured(import_config)
+        return self.async_create_entry(title=name, data=import_config)
