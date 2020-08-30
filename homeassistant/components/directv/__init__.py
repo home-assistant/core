3440469e0b17f6c23e8fe187c24ffb585ab49a7a
Chris Talkington <chris@talkingtontech.com>
2020-08-30 10:22:56 -0500

    Update __init__.py

--- homeassistant/components/directv/__init__.py
+++ homeassistant/components/directv/__init__.py
@@ -13,6 +13,7 @@ from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import (
     ATTR_IDENTIFIERS,
@@ -22,6 +23,7 @@ from .const import (
     ATTR_VIA_DEVICE,
     DOMAIN,
 )
+from .coordinator import DIRECTVDataUpdateCoordinator
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -55,14 +57,19 @@ async def async_setup(hass: HomeAssistant, config: Dict) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up DirecTV from a config entry."""
-    dtv = DIRECTV(entry.data[CONF_HOST], session=async_get_clientsession(hass))
+    coordinator = DIRECTVDataUpdateCoordinator(entry.data, entry.options)
 
     try:
-        await dtv.update()
+        await coordinator.dtv.update()
     except DIRECTVError as err:
         raise ConfigEntryNotReady from err
+    
+    await coordinator.async_refresh()
 
-    hass.data[DOMAIN][entry.entry_id] = dtv
+    if not coordinator.last_update_success:
+        raise ConfigEntryNotReady
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
 
     for component in PLATFORMS:
         hass.async_create_task(
@@ -89,16 +96,16 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return unload_ok
 
 
-class DIRECTVEntity(Entity):
+class DIRECTVEntity(CoordinatorEntity):
     """Defines a base DirecTV entity."""
 
-    def __init__(self, *, dtv: DIRECTV, name: str, address: str = "0") -> None:
+    def __init__(self, *, coordinator: DIRECTVDataUpdateCoordinator, name: str, address: str = "0") -> None:
         """Initialize the DirecTV entity."""
+        super().__init__(coordinator)
         self._address = address
         self._device_id = address if address != "0" else dtv.device.info.receiver_id
         self._is_client = address != "0"
         self._name = name
-        self.dtv = dtv
 
     @property
     def name(self) -> str:
@@ -111,8 +118,8 @@ class DIRECTVEntity(Entity):
         return {
             ATTR_IDENTIFIERS: {(DOMAIN, self._device_id)},
             ATTR_NAME: self.name,
-            ATTR_MANUFACTURER: self.dtv.device.info.brand,
+            ATTR_MANUFACTURER: self.coordinator.dtv.device.info.brand,
             ATTR_MODEL: None,
-            ATTR_SOFTWARE_VERSION: self.dtv.device.info.version,
-            ATTR_VIA_DEVICE: (DOMAIN, self.dtv.device.info.receiver_id),
+            ATTR_SOFTWARE_VERSION: self.coordinator.dtv.device.info.version,
+            ATTR_VIA_DEVICE: (DOMAIN, self.coordinator.dtv.device.info.receiver_id),
         }
