0c71415917311ceec14e225803ba909cee17ab1c
J. Nick Koston <nick@koston.org>
2020-08-30 11:21:04 -0500

    Prevent CoordinatorEntity from requesting updates on disabled entities

--- homeassistant/helpers/update_coordinator.py
+++ homeassistant/helpers/update_coordinator.py
@@ -221,4 +221,9 @@ class CoordinatorEntity(entity.Entity):
 
         Only used by the generic entity update service.
         """
+
+        # Ignore manual update requests if the entity is disabled
+        if not self.enabled:
+            return
+
         await self.coordinator.async_request_refresh()
