ad0d3b48480d6d2dcaa77e0d7f8ce8298b4621d1
Jeff Irion <JeffLIrion@users.noreply.github.com>
2020-08-29 19:56:25 -0700

    Improve handling of exceptions in Android TV (#39229)
    * Close the ADB connection when there is an exception
    
    * Add a test
    
    * Split a comment onto two lines
    
    * Fix test ('async_update' -> 'async_update_entity')
    
    * 'close' -> 'Close'

--- homeassistant/components/androidtv/media_player.py
+++ homeassistant/components/androidtv/media_player.py
@@ -376,6 +376,12 @@ def adb_decorator(override_available=False):
                 await self.aftv.adb_close()
                 self._available = False
                 return None
+            except Exception:
+                # An unforeseen exception occurred. Close the ADB connection so that
+                # it doesn't happen over and over again, then raise the exception.
+                await self.aftv.adb_close()
+                self._available = False  # pylint: disable=protected-access
+                raise
 
         return _adb_exception_catcher
 
@@ -421,10 +427,8 @@ class ADBDevice(MediaPlayerEntity):
             # Using "adb_shell" (Python ADB implementation)
             self.exceptions = (
                 AdbTimeoutError,
-                AttributeError,
                 BrokenPipeError,
                 ConnectionResetError,
-                TypeError,
                 ValueError,
                 InvalidChecksumError,
                 InvalidCommandError,
