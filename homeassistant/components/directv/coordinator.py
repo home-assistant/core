9f962e2ec19e2fe1ec2ebf2579a2d0ed33089421
Chris Talkington <chris@talkingtontech.com>
2020-08-30 10:13:53 -0500

    Update coordinator.py

--- homeassistant/components/directv/coordinator.py
+++ homeassistant/components/directv/coordinator.py
@@ -13,12 +13,29 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+SCAN_INTERVAL = timedelta(seconds=30)
 
-class DirecTVDataUpdateCoordinator(DataUpdateCoordinator[Device]):
-    """Class to manage fetching DirecTV data."""
+
+class DirecTVDataUpdateCoordinator(DataUpdateCoordinator):
+    """Class to manage fetching DirecTV receiver data."""
 
     def __init__(self, hass: HomeAssistantType, *, config: dict, options: dict):
         """Initialize global DirecTV data updater."""
         self.dtv = DIRECTV(config[CONF_HOST], session=async_get_clientsession(hass))
+
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=DOMAIN,
+            update_interval=SCAN_INTERVAL,
+        )
+
+    async def _async_update_data(self) -> dict:
+        """Fetch data from DirecTV receiver."""
+        try:
+            return await self.dtv.state()
+        except DIRECTVError as error:
+            raise UpdateFailed(f"Invalid response from API: {error}") from error
+
 
         
