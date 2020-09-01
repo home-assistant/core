9f7b99bc5badb34245ebb60bb823cdabecb0fe6d
Rob Bierbooms <mail@robbierbooms.nl>
2020-09-01 05:44:57 +0000

    Update tests

--- tests/components/dsmr/test_config_flow.py
+++ tests/components/dsmr/test_config_flow.py
@@ -71,11 +71,32 @@ async def test_import_update(hass):
     )
     entry.add_to_hass(hass)
 
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={"source": config_entries.SOURCE_IMPORT},
-        data=entry_data,
-    )
+    with patch(
+        "homeassistant.components.dsmr.async_setup_entry", return_value=True
+    ), patch("homeassistant.components.dsmr.async_unload_entry", return_value=True):
+        await hass.config_entries.async_setup(entry.entry_id)
+
+    await hass.async_block_till_done()
+
+    new_entry_data = {
+        "port": "/dev/ttyUSB0",
+        "dsmr_version": "2.2",
+        "precision": 3,
+        "reconnect_interval": 30,
+    }
+
+    with patch(
+        "homeassistant.components.dsmr.async_setup_entry", return_value=True
+    ), patch("homeassistant.components.dsmr.async_unload_entry", return_value=True):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_IMPORT},
+            data=new_entry_data,
+        )
+
+    await hass.async_block_till_done()
 
     assert result["type"] == "abort"
     assert result["reason"] == "already_configured"
+
+    assert entry.data["precision"] == 3
