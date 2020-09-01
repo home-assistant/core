9ce964a4a3912b2e0457bb639e53fde5dc93195c
Rob Bierbooms <mail@robbierbooms.nl>
2020-09-01 06:52:00 +0200

    Implement function to check on host/port already existing
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>

--- homeassistant/components/dsmr/config_flow.py
+++ homeassistant/components/dsmr/config_flow.py
@@ -15,6 +15,33 @@ class DSMRFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
+    def _abort_if_host_port_configured(
+        self,
+        host: str,
+        port: int,
+        updates: Optional[Dict[Any, Any]] = None,
+        reload_on_update: bool = True,
+    ):
+        """Test if host and port are already configured."""
+        for entry in self.hass.config_entries.async_entries(DOMAIN):
+            if (
+                entry.data[CONF_HOST] == host
+                and entry.data[CONF_PORT] == port
+            ):
+                if updates is not None:
+                    changed = self.hass.config_entries.async_update_entry(
+                        entry, data={**entry.data, **updates}
+                    )
+                    if (
+                        changed
+                        and reload_on_update
+                        and entry.state in (ENTRY_STATE_LOADED, ENTRY_STATE_SETUP_RETRY)
+                    ):
+                        self.hass.async_create_task(
+                            self.hass.config_entries.async_reload(entry.entry_id)
+                        )
+                return self.async_abort(reason="already_configured")
+
     async def async_step_import(self, import_config=None):
         """Handle the initial step."""
         if CONF_HOST in import_config:
