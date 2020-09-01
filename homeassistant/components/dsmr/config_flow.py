f49bbb69ff9e88ad527664994fe7d7a64606477e
Rob Bierbooms <mail@robbierbooms.nl>
2020-09-01 05:05:27 +0000

    Implement new method in import

--- homeassistant/components/dsmr/config_flow.py
+++ homeassistant/components/dsmr/config_flow.py
@@ -1,5 +1,6 @@
 """Config flow for DSMR integration."""
 import logging
+from typing import Any, Dict, Optional
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_PORT
@@ -17,17 +18,14 @@ class DSMRFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     def _abort_if_host_port_configured(
         self,
-        host: str,
-        port: int,
+        port: str,
+        host: str = None,
         updates: Optional[Dict[Any, Any]] = None,
         reload_on_update: bool = True,
     ):
         """Test if host and port are already configured."""
         for entry in self.hass.config_entries.async_entries(DOMAIN):
-            if (
-                entry.data[CONF_HOST] == host
-                and entry.data[CONF_PORT] == port
-            ):
+            if entry.data.get(CONF_HOST) == host and entry.data[CONF_PORT] == port:
                 if updates is not None:
                     changed = self.hass.config_entries.async_update_entry(
                         entry, data={**entry.data, **updates}
@@ -35,7 +33,11 @@ class DSMRFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                     if (
                         changed
                         and reload_on_update
-                        and entry.state in (ENTRY_STATE_LOADED, ENTRY_STATE_SETUP_RETRY)
+                        and entry.state
+                        in (
+                            config_entries.ENTRY_STATE_LOADED,
+                            config_entries.ENTRY_STATE_SETUP_RETRY,
+                        )
                     ):
                         self.hass.async_create_task(
                             self.hass.config_entries.async_reload(entry.entry_id)
@@ -44,11 +46,16 @@ class DSMRFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_import(self, import_config=None):
         """Handle the initial step."""
-        if CONF_HOST in import_config:
-            name = f"{import_config[CONF_HOST]}:{import_config[CONF_PORT]}"
-        else:
-            name = import_config[CONF_PORT]
+        host = import_config.get(CONF_HOST)
+        port = import_config[CONF_PORT]
 
-        await self.async_set_unique_id(name)
-        self._abort_if_unique_id_configured(import_config)
+        status = self._abort_if_host_port_configured(port, host, import_config)
+        if status is not None:
+            return status
+
+        if host is not None:
+            name = f"{host}:{port}"
+        else:
+            name = port
+
         return self.async_create_entry(title=name, data=import_config)
