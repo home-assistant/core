bb0817dc72e146f2d485737b0932f81f65a6f2a3
J. Nick Koston <nick@koston.org>
2020-09-02 13:47:49 -0500

    clenaup

--- homeassistant/components/notify/__init__.py
+++ homeassistant/components/notify/__init__.py
@@ -144,9 +144,13 @@ class BaseNotificationService:
         await self.async_send_message(**kwargs)
 
     async def async_setup(
-        self, service_name: str, target_service_name_prefix: str
+        self,
+        hass: HomeAssistantType,
+        service_name: str,
+        target_service_name_prefix: str,
     ) -> None:
         """Store the data for the notify service."""
+        self.hass = hass
         self._service_name = service_name
         self._target_service_name_prefix = target_service_name_prefix
         self._registered_targets = {}
@@ -258,8 +262,6 @@ async def async_setup(hass, config):
             _LOGGER.exception("Error setting up platform %s", integration_name)
             return
 
-        notify_service.hass = hass
-
         if discovery_info is None:
             discovery_info = {}
 
@@ -267,7 +269,7 @@ async def async_setup(hass, config):
         target_service_name_prefix = conf_name or integration_name
         service_name = slugify(conf_name or SERVICE_NOTIFY)
 
-        await notify_service.async_setup(service_name, target_service_name_prefix)
+        await notify_service.async_setup(hass, service_name, target_service_name_prefix)
         await notify_service.async_register_services()
 
         hass.data[NOTIFY_SERVICES].setdefault(integration_name, []).append(
