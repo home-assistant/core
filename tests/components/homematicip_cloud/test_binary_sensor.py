5855174f37ba0e9e665b3d7bdea8c58f2488b4f1
SukramJ <markus@mm-jankowski.de>
2020-09-03 17:23:42 +0200

    Add HmIP-STV to HomematicIP Cloud (#39518)
    * add general attribute for connection type
    
    * Add HmIP-STV to HomematicIP Cloud

--- tests/components/homematicip_cloud/test_binary_sensor.py
+++ tests/components/homematicip_cloud/test_binary_sensor.py
@@ -75,6 +75,42 @@ async def test_hmip_acceleration_sensor(hass, default_mock_hap_factory):
     assert len(hmip_device.mock_calls) == service_call_counter + 2
 
 
+async def test_hmip_tilt_vibration_sensor(hass, default_mock_hap_factory):
+    """Test HomematicipTiltVibrationSensor."""
+    entity_id = "binary_sensor.garage_neigungs_und_erschutterungssensor"
+    entity_name = "Garage Neigungs- und Erschütterungssensor"
+    device_model = "HmIP-STV"
+    mock_hap = await default_mock_hap_factory.async_get_mock_hap(
+        test_devices=[entity_name]
+    )
+
+    ha_state, hmip_device = get_and_check_entity_basics(
+        hass, mock_hap, entity_id, entity_name, device_model
+    )
+
+    assert ha_state.state == STATE_ON
+    assert ha_state.attributes[ATTR_ACCELERATION_SENSOR_MODE] == "FLAT_DECT"
+    assert (
+        ha_state.attributes[ATTR_ACCELERATION_SENSOR_SENSITIVITY] == "SENSOR_RANGE_2G"
+    )
+    assert ha_state.attributes[ATTR_ACCELERATION_SENSOR_TRIGGER_ANGLE] == 45
+    service_call_counter = len(hmip_device.mock_calls)
+
+    await async_manipulate_test_data(
+        hass, hmip_device, "accelerationSensorTriggered", False
+    )
+    ha_state = hass.states.get(entity_id)
+    assert ha_state.state == STATE_OFF
+    assert len(hmip_device.mock_calls) == service_call_counter + 1
+
+    await async_manipulate_test_data(
+        hass, hmip_device, "accelerationSensorTriggered", True
+    )
+    ha_state = hass.states.get(entity_id)
+    assert ha_state.state == STATE_ON
+    assert len(hmip_device.mock_calls) == service_call_counter + 2
+
+
 async def test_hmip_contact_interface(hass, default_mock_hap_factory):
     """Test HomematicipContactInterface."""
     entity_id = "binary_sensor.kontakt_schnittstelle_unterputz_1_fach"
