8c929c8db845fd44410eb6c70c4dfbb4630ef745
Rob Bierbooms <mail@robbierbooms.nl>
2020-09-04 18:35:49 +0000

    Implement DSMR Connection class

--- homeassistant/components/dsmr/config_flow.py
+++ homeassistant/components/dsmr/config_flow.py
@@ -1,13 +1,84 @@
 """Config flow for DSMR integration."""
+import asyncio
+from functools import partial
 import logging
 from typing import Any, Dict, Optional
 
-from homeassistant import config_entries
+from dsmr_parser import obis_references as obis_ref
+from dsmr_parser.clients.protocol import create_dsmr_reader, create_tcp_dsmr_reader
+import serial
+
+from homeassistant import config_entries, core
 from homeassistant.const import CONF_HOST, CONF_PORT
 
 from .const import DOMAIN  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
+
+
+class DSMRConnection:
+    """Test the connection to DSMR and receive telegram to read serial ids."""
+
+    def __init__(self, host, port, dsmr_version):
+        """Initialize."""
+        self._host = host
+        self._port = port
+        self._dsmr_version = dsmr_version
+        self._telegram = {}
+
+    def equipment_identifier(self):
+        """Equipment identifier."""
+        if obis_ref.EQUIPMENT_IDENTIFIER in self._telegram:
+            dsmr_object = self._telegram[obis_ref.EQUIPMENT_IDENTIFIER]
+            return getattr(dsmr_object, "value", None)
+
+    def equipment_identifier_gas(self):
+        """Equipment identifier gas."""
+        if obis_ref.EQUIPMENT_IDENTIFIER_GAS in self._telegram:
+            dsmr_object = self._telegram[obis_ref.EQUIPMENT_IDENTIFIER_GAS]
+            return getattr(dsmr_object, "value", None)
+
+    async def validate_connect(self, hass: core.HomeAssistant) -> bool:
+        """Test if we can validate connection with the device."""
+
+        def update_telegram(telegram):
+            self._telegram = telegram
+
+            transport.close()
+
+        if self._host is None:
+            reader_factory = partial(
+                create_dsmr_reader,
+                self._port,
+                self._dsmr_version,
+                update_telegram,
+                loop=hass.loop,
+            )
+        else:
+            reader_factory = partial(
+                create_tcp_dsmr_reader,
+                self._host,
+                self._port,
+                self._dsmr_version,
+                update_telegram,
+                loop=hass.loop,
+            )
+
+        try:
+            transport, protocol = await hass.loop.create_task(reader_factory())
+        except (
+            serial.serialutil.SerialException,
+            ConnectionRefusedError,
+            TimeoutError,
+            asyncio.TimeoutError,
+        ):
+            _LOGGER.exception("Error connecting to DSMR")
+            return False
+
+        if transport:
+            await protocol.wait_closed()
+
+        return True
 
 
 class DSMRFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
