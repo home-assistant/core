9eb87f2095d04a4025fdc0e6d22f4541be68f3d2
Keilin <trumpetgod@gmail.com>
2020-09-03 23:24:34 -0400

    More formatting

--- tests/components/emulated_kasa/test_server.py
+++ tests/components/emulated_kasa/test_server.py
@@ -361,9 +361,9 @@ async def test_sensor_state(hass):
     """Test a configuration using a sensor in a template."""
     config = CONFIG_SENSOR[DOMAIN][CONF_ENTITIES]
     assert await async_setup_component(
-        hass, 
-        SENSOR_DOMAIN, 
-        {SENSOR_DOMAIN: {"platform": "demo", "name": "power_sensor"}}
+        hass,
+        SENSOR_DOMAIN,
+        {SENSOR_DOMAIN: {"platform": "demo", "name": "power_sensor"}},
     )
     with patch(
         "sense_energy.SenseLink",
