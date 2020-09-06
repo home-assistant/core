3eaef348f3e961971a061a860c5b7cd0178d20a8
Franck Nijhof <git@frenck.dev>
2020-09-07 00:36:01 +0200

    State condition can also accept an input_* Entity ID as state value (#39691)

--- tests/helpers/test_condition.py
+++ tests/helpers/test_condition.py
@@ -443,6 +443,88 @@ async def test_state_attribute(hass):
     assert not test(hass)
 
 
+async def test_state_using_input_entities(hass):
+    """Test state conditions using input_* entities."""
+    await async_setup_component(
+        hass,
+        "input_text",
+        {
+            "input_text": {
+                "hello": {"initial": "goodbye"},
+            }
+        },
+    )
+
+    await async_setup_component(
+        hass,
+        "input_select",
+        {
+            "input_select": {
+                "hello": {"options": ["cya", "goodbye", "welcome"], "initial": "cya"},
+            }
+        },
+    )
+
+    test = await condition.async_from_config(
+        hass,
+        {
+            "condition": "and",
+            "conditions": [
+                {
+                    "condition": "state",
+                    "entity_id": "sensor.salut",
+                    "state": [
+                        "input_text.hello",
+                        "input_select.hello",
+                        "input_number.not_exist",
+                        "salut",
+                    ],
+                },
+            ],
+        },
+    )
+
+    hass.states.async_set("sensor.salut", "goodbye")
+    assert test(hass)
+
+    hass.states.async_set("sensor.salut", "salut")
+    assert test(hass)
+
+    hass.states.async_set("sensor.salut", "hello")
+    assert not test(hass)
+
+    await hass.services.async_call(
+        "input_text",
+        "set_value",
+        {
+            "entity_id": "input_text.hello",
+            "value": "hi",
+        },
+        blocking=True,
+    )
+    assert not test(hass)
+
+    hass.states.async_set("sensor.salut", "hi")
+    assert test(hass)
+
+    hass.states.async_set("sensor.salut", "cya")
+    assert test(hass)
+
+    await hass.services.async_call(
+        "input_select",
+        "select_option",
+        {
+            "entity_id": "input_select.hello",
+            "option": "welcome",
+        },
+        blocking=True,
+    )
+    assert not test(hass)
+
+    hass.states.async_set("sensor.salut", "welcome")
+    assert test(hass)
+
+
 async def test_numeric_state_multiple_entities(hass):
     """Test with multiple entities in condition."""
     test = await condition.async_from_config(
