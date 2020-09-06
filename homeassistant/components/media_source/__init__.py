450c683264442f0dbd07ecb8d37da5552fa7e70b
Philip Allgaier <mail@spacegaier.de>
2020-09-06 18:32:45 +0100

    Fix media browser panel title

--- homeassistant/components/media_source/__init__.py
+++ homeassistant/components/media_source/__init__.py
@@ -38,7 +38,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
     hass.components.websocket_api.async_register_command(websocket_browse_media)
     hass.components.websocket_api.async_register_command(websocket_resolve_media)
     hass.components.frontend.async_register_built_in_panel(
-        "media-browser", "media-browser", "hass:play-box-multiple"
+        "media-browser", "media_browser", "hass:play-box-multiple"
     )
     local_source.async_setup(hass)
     await async_process_integration_platforms(
