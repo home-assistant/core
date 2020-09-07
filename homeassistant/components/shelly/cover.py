d1400a961333bedce5a8d0f95b0bd506f0315105
Simone Chemelli <simone.chemelli@gmail.com>
2020-09-07 20:43:58 +0200

    Update homeassistant/components/shelly/cover.py
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/shelly/cover.py
+++ homeassistant/components/shelly/cover.py
@@ -33,10 +33,7 @@ class ShellyCover(ShellyBlockEntity, CoverEntity):
         """Initialize light."""
         super().__init__(wrapper, block)
         self.control_result = None
-        self._supported_features = 0
-        self._supported_features |= SUPPORT_OPEN
-        self._supported_features |= SUPPORT_CLOSE
-        self._supported_features |= SUPPORT_STOP
+        self._supported_features = SUPPORT_OPEN | SUPPORT_CLOSE | SUPPORT_STOP
         if self.wrapper.device.settings["rollers"][0]["positioning"]:
             self._supported_features |= SUPPORT_SET_POSITION
 
