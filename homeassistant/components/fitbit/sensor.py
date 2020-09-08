d63fb7ca9f0f8d70dce929282e7a1b8788f45e85
Colin Frei <git@colinfrei.com>
2020-09-08 23:07:47 +0200

    Use same host as currently used, not necessarily external

--- homeassistant/components/fitbit/sensor.py
+++ homeassistant/components/fitbit/sensor.py
@@ -185,7 +185,7 @@ def request_app_setup(hass, config, add_entities, config_path, discovery_info=No
         else:
             setup_platform(hass, config, add_entities, discovery_info)
 
-    start_url = f"{get_url(hass, prefer_external=True)}{FITBIT_AUTH_CALLBACK_PATH}"
+    start_url = f"{get_url(hass, require_current_request=True)}{FITBIT_AUTH_CALLBACK_PATH}"
 
     description = f"""Please create a Fitbit developer app at
                        https://dev.fitbit.com/apps/new.
@@ -220,7 +220,7 @@ def request_oauth_completion(hass):
     def fitbit_configuration_callback(callback_data):
         """Handle configuration updates."""
 
-    start_url = f"{get_url(hass, prefer_external=True)}{FITBIT_AUTH_START}"
+    start_url = f"{get_url(hass, require_current_request=True)}{FITBIT_AUTH_START}"
 
     description = f"Please authorize Fitbit by visiting {start_url}"
 
@@ -313,7 +313,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         )
 
         redirect_uri = (
-            f"{get_url(hass, prefer_external=True)}{FITBIT_AUTH_CALLBACK_PATH}"
+            f"{get_url(hass, require_current_request=True)}{FITBIT_AUTH_CALLBACK_PATH}"
         )
 
         fitbit_auth_start_url, _ = oauth.authorize_token_url(
@@ -360,7 +360,7 @@ class FitbitAuthCallbackView(HomeAssistantView):
         result = None
         if data.get("code") is not None:
             redirect_uri = (
-                f"{get_url(hass, prefer_external=True)}{FITBIT_AUTH_CALLBACK_PATH}"
+                f"{get_url(hass, require_current_request=True)}{FITBIT_AUTH_CALLBACK_PATH}"
             )
 
             try:
