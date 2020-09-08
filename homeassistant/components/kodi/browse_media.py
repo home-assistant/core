7beede0a48252b6f6e257416e1ef02ea0240650f
Tobias Sauerwein <cgtobi@gmail.com>
2020-09-08 23:26:31 +0200

    Clean up

--- homeassistant/components/kodi/browse_media.py
+++ homeassistant/components/kodi/browse_media.py
@@ -189,7 +189,7 @@ def item_payload(item, media_library):
         media_content_id = ""
 
     title = item["label"]
-    can_play = media_content_type in PLAYABLE_MEDIA_TYPES and bool(media_content_id)
+    can_play = media_content_type in PLAYABLE_MEDIA_TYPES and media_content_id
     can_expand = media_content_type in EXPANDABLE_MEDIA_TYPES
 
     thumbnail = item.get("thumbnail")
