e0d7e417dc6ac91623a15307e2c3d01e22c07dd2
Erik <erik@montnemery.com>
2020-09-08 22:22:17 +0200

    Remove debug prints, fix tests

--- homeassistant/components/mqtt/light/schema_basic.py
+++ homeassistant/components/mqtt/light/schema_basic.py
@@ -158,7 +158,8 @@ async def async_setup_entity_basic(
     hass, config, async_add_entities, config_entry, discovery_data=None
 ):
     """Set up a MQTT Light."""
-    config.setdefault(CONF_STATE_VALUE_TEMPLATE, config.get(CONF_VALUE_TEMPLATE))
+    if CONF_STATE_VALUE_TEMPLATE not in config and CONF_VALUE_TEMPLATE in config:
+        config[CONF_STATE_VALUE_TEMPLATE] = config[CONF_VALUE_TEMPLATE]
 
     async_add_entities([MqttLight(hass, config, config_entry, discovery_data)])
 
