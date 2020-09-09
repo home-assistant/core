5e2ec8f32951d398cff9dc7846e383183f7c11b8
Chris Talkington <chris@talkingtontech.com>
2020-09-08 20:33:56 -0500

    Update test_sensor.py

--- tests/components/nzbget/test_sensor.py
+++ tests/components/nzbget/test_sensor.py
@@ -12,6 +12,7 @@ from . import init_integration
 async def test_sensors(hass) -> None:
     """Test the creation and values of the sensors."""
     entry = await init_integration(hass)
+    registry = await hass.helpers.entity_registry.async_get_registry()
 
     sensors = {
         "article_cache": ("64", DATA_MEGABYTES, None),
