1ee405a73f7b242709d22b1f31a913094da6500d
Chris Talkington <chris@talkingtontech.com>
2020-09-08 21:17:04 -0500

    Update test_sensor.py

--- tests/components/nzbget/test_sensor.py
+++ tests/components/nzbget/test_sensor.py
@@ -18,7 +18,7 @@ async def test_sensors(hass) -> None:
     """Test the creation and values of the sensors."""
     now = dt_util.utcnow().replace(microsecond=0)
     with patch("homeassistant.util.dt.utcnow", return_value=now):
-.       entry = await init_integration(hass)
+        entry = await init_integration(hass)
 
     registry = await hass.helpers.entity_registry.async_get_registry()
 
