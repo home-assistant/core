b4bf3f8b1da35cbec6d76f3c6c9107a40601c3b7
Tim Empringham <tim.empringham@live.ca>
2020-09-10 14:42:29 -0400

    Updated test_config_flow to follow recommended pattern.

--- tests/components/omnilogic/test_config_flow.py
+++ tests/components/omnilogic/test_config_flow.py
@@ -2,7 +2,6 @@
 from omnilogic import LoginException
 
 from homeassistant import config_entries, setup
-from homeassistant.components.omnilogic import config_flow
 from homeassistant.components.omnilogic.const import DOMAIN
 
 from tests.async_mock import patch
@@ -46,31 +45,16 @@ async def test_already_configured(hass):
     """Test config flow when Omnilogic component is already setup."""
     MockConfigEntry(domain="omnilogic", data=DATA).add_to_hass(hass)
 
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-    flow.context = {}
-
-    fname = "async_step_user"
-    func = getattr(flow, fname)
-    result = await func(DATA)
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
 
     assert result["type"] == "abort"
 
 
 async def test_without_config(hass):
-    """Test config flow with no or incomplete configuration."""
-
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-    flow.context = {}
-
-    fname = "async_step_user"
-    func = getattr(flow, fname)
-    result = await func()
-
-    assert result["type"] == "form"
-    assert result["step_id"] == "user"
-    assert result["errors"] == {}
+    """Test config flow with polling interval not entered."""
 
     await setup.async_setup_component(hass, "persistent_notification", {})
     result = await hass.config_entries.flow.async_init(
@@ -101,21 +85,26 @@ async def test_without_config(hass):
 async def test_with_invalid_credentials(hass):
     """Test with invalid credentials."""
 
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-    flow.context = {}
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
 
-    fname = "async_step_user"
-    func = getattr(flow, fname)
     with patch("omnilogic.OmniLogic.connect", side_effect=LoginException):
-        result = await func(DATA)
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            DATA,
+        )
 
     assert result["type"] == "form"
     assert result["step_id"] == "user"
     assert result["errors"] == {"base": "cannot_connect"}
 
     with patch("omnilogic.OmniLogic.connect", side_effect=Exception):
-        result2 = await func(DATA)
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            DATA,
+        )
 
     assert result2["type"] == "form"
     assert result2["step_id"] == "user"
