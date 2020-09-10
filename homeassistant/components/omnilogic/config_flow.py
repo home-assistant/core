e387fbf25f65668f3cb98e7d6884098827cfe096
Tim Empringham <tim.empringham@live.ca>
2020-09-10 12:02:41 -0400

    Added config_flow tests and updated issue with config_flow on cannot_connect

--- homeassistant/components/omnilogic/config_flow.py
+++ homeassistant/components/omnilogic/config_flow.py
@@ -31,15 +31,18 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             username = user_input[CONF_USERNAME]
             password = user_input[CONF_PASSWORD]
 
+            errors = {"base": ""}
+
             try:
                 omni = OmniLogic(username, password)
                 await omni.connect()
             except LoginException:
                 errors["base"] = "cannot_connect"
 
-            await self.async_set_unique_id(user_input["username"])
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(title="Omnilogic", data=user_input)
+            if errors["base"] == "":
+                await self.async_set_unique_id(user_input["username"])
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(title="Omnilogic", data=user_input)
 
         return self.async_show_form(
             step_id="user",
