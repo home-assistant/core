ff7b4916dd134329cc4e4fb6c49100deb35dee6d
Erik Montnemery <erik@montnemery.com>
2020-09-10 20:52:23 +0200

    Fix discovery update of MQTT state templates (#39901)

--- tests/components/mqtt/test_sensor.py
+++ tests/components/mqtt/test_sensor.py
@@ -1,4 +1,5 @@
 """The tests for the MQTT sensor platform."""
+import copy
 from datetime import datetime, timedelta
 import json
 
@@ -430,12 +431,71 @@ async def test_discovery_removal_sensor(hass, mqtt_mock, caplog):
     await help_test_discovery_removal(hass, mqtt_mock, caplog, sensor.DOMAIN, data)
 
 
-async def test_discovery_update_sensor(hass, mqtt_mock, caplog):
+async def test_discovery_update_sensor_topic_template(hass, mqtt_mock, caplog):
     """Test update of discovered sensor."""
-    data1 = '{ "name": "Beer", "state_topic": "test_topic" }'
-    data2 = '{ "name": "Milk", "state_topic": "test_topic" }'
+    config = {"name": "test", "state_topic": "test_topic"}
+    config1 = copy.deepcopy(config)
+    config2 = copy.deepcopy(config)
+    config1["name"] = "Beer"
+    config2["name"] = "Milk"
+    config1["state_topic"] = "sensor/state1"
+    config2["state_topic"] = "sensor/state2"
+    config1["value_template"] = "{{ value_json.state | int }}"
+    config2["value_template"] = "{{ value_json.state | int * 2 }}"
+
+    state_data1 = [
+        ([("sensor/state1", '{"state":100}')], "100", None),
+    ]
+    state_data2 = [
+        ([("sensor/state1", '{"state":1000}')], "100", None),
+        ([("sensor/state1", '{"state":1000}')], "100", None),
+        ([("sensor/state2", '{"state":100}')], "200", None),
+    ]
+
+    data1 = json.dumps(config1)
+    data2 = json.dumps(config2)
     await help_test_discovery_update(
-        hass, mqtt_mock, caplog, sensor.DOMAIN, data1, data2
+        hass,
+        mqtt_mock,
+        caplog,
+        sensor.DOMAIN,
+        data1,
+        data2,
+        state_data1=state_data1,
+        state_data2=state_data2,
+    )
+
+
+async def test_discovery_update_sensor_template(hass, mqtt_mock, caplog):
+    """Test update of discovered sensor."""
+    config = {"name": "test", "state_topic": "test_topic"}
+    config1 = copy.deepcopy(config)
+    config2 = copy.deepcopy(config)
+    config1["name"] = "Beer"
+    config2["name"] = "Milk"
+    config1["state_topic"] = "sensor/state1"
+    config2["state_topic"] = "sensor/state1"
+    config1["value_template"] = "{{ value_json.state | int }}"
+    config2["value_template"] = "{{ value_json.state | int * 2 }}"
+
+    state_data1 = [
+        ([("sensor/state1", '{"state":100}')], "100", None),
+    ]
+    state_data2 = [
+        ([("sensor/state1", '{"state":100}')], "200", None),
+    ]
+
+    data1 = json.dumps(config1)
+    data2 = json.dumps(config2)
+    await help_test_discovery_update(
+        hass,
+        mqtt_mock,
+        caplog,
+        sensor.DOMAIN,
+        data1,
+        data2,
+        state_data1=state_data1,
+        state_data2=state_data2,
     )
 
 
