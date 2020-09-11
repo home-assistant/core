741487a1fc2b5f06b5be195748696fd0527067c0
J. Nick Koston <nick@koston.org>
2020-09-11 13:18:40 -0500

    Return the listeners with the template result for the websocket api (#39925)

--- homeassistant/helpers/event.py
+++ homeassistant/helpers/event.py
@@ -582,6 +582,15 @@ class _TrackTemplateResultInfo:
         self._create_listeners()
 
     @property
+    def listeners(self) -> Dict:
+        """State changes that will cause a re-render."""
+        return {
+            "all": self._all_listener is not None,
+            "entities": self._last_entities,
+            "domains": self._last_domains,
+        }
+
+    @property
     def _needs_all_listener(self) -> bool:
         for track_template_ in self._track_templates:
             template = track_template_.template
