cee96ae207cff58c99ff263c684e8abe843694a6
springstan <46536646+springstan@users.noreply.github.com>
2020-09-12 18:07:13 +0200

    Use opening and occupancy device class in various integrations (#39965)

--- homeassistant/components/ring/binary_sensor.py
+++ homeassistant/components/ring/binary_sensor.py
@@ -2,7 +2,10 @@
 from datetime import datetime
 import logging
 
-from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.components.binary_sensor import (
+    DEVICE_CLASS_OCCUPANCY,
+    BinarySensorEntity,
+)
 from homeassistant.core import callback
 
 from . import DOMAIN
@@ -12,7 +15,7 @@ _LOGGER = logging.getLogger(__name__)
 
 # Sensor types: Name, category, device_class
 SENSOR_TYPES = {
-    "ding": ["Ding", ["doorbots", "authorized_doorbots"], "occupancy"],
+    "ding": ["Ding", ["doorbots", "authorized_doorbots"], DEVICE_CLASS_OCCUPANCY],
     "motion": ["Motion", ["doorbots", "authorized_doorbots", "stickup_cams"], "motion"],
 }
 
--- homeassistant/components/smartthings/binary_sensor.py
+++ homeassistant/components/smartthings/binary_sensor.py
@@ -4,6 +4,7 @@ from typing import Optional, Sequence
 from pysmartthings import Attribute, Capability
 
 from homeassistant.components.binary_sensor import (
+    DEVICE_CLASS_OPENING,
     DEVICE_CLASS_SOUND,
     BinarySensorEntity,
 )
@@ -24,13 +25,13 @@ CAPABILITY_TO_ATTRIB = {
 }
 ATTRIB_TO_CLASS = {
     Attribute.acceleration: "moving",
-    Attribute.contact: "opening",
+    Attribute.contact: DEVICE_CLASS_OPENING,
     Attribute.filter_status: "problem",
     Attribute.motion: "motion",
     Attribute.presence: "presence",
     Attribute.sound: DEVICE_CLASS_SOUND,
     Attribute.tamper: "problem",
-    Attribute.valve: "opening",
+    Attribute.valve: DEVICE_CLASS_OPENING,
     Attribute.water: "moisture",
 }
 
