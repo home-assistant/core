1d821932910af70797409cd6e29a5550094d9ad6
chemelli74 <simone.chemelli@gmail.com>
2020-09-12 20:01:36 +0200

    Added attributes (ip, id) for easy identification of devices

--- homeassistant/components/shelly/binary_sensor.py
+++ homeassistant/components/shelly/binary_sensor.py
@@ -17,22 +17,52 @@ from .entity import (
 
 SENSORS = {
     ("device", "overtemp"): BlockAttributeDescription(
-        name="Overheating", device_class=DEVICE_CLASS_PROBLEM
+        name="Overheating",
+        device_class=DEVICE_CLASS_PROBLEM,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("device", "overpower"): BlockAttributeDescription(
-        name="Over Power", device_class=DEVICE_CLASS_PROBLEM
+        name="Over Power",
+        device_class=DEVICE_CLASS_PROBLEM,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("light", "overpower"): BlockAttributeDescription(
-        name="Over Power", device_class=DEVICE_CLASS_PROBLEM
+        name="Over Power",
+        device_class=DEVICE_CLASS_PROBLEM,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("relay", "overpower"): BlockAttributeDescription(
-        name="Over Power", device_class=DEVICE_CLASS_PROBLEM
+        name="Over Power",
+        device_class=DEVICE_CLASS_PROBLEM,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("sensor", "dwIsOpened"): BlockAttributeDescription(
-        name="Door", device_class=DEVICE_CLASS_OPENING
+        name="Door",
+        device_class=DEVICE_CLASS_OPENING,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("sensor", "flood"): BlockAttributeDescription(
-        name="flood", device_class=DEVICE_CLASS_MOISTURE
+        name="flood",
+        device_class=DEVICE_CLASS_MOISTURE,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("sensor", "gas"): BlockAttributeDescription(
         name="gas",
@@ -41,10 +71,20 @@ SENSORS = {
         device_state_attributes=lambda block: {"detected": block.gas},
     ),
     ("sensor", "smoke"): BlockAttributeDescription(
-        name="smoke", device_class=DEVICE_CLASS_SMOKE
+        name="smoke",
+        device_class=DEVICE_CLASS_SMOKE,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("sensor", "vibration"): BlockAttributeDescription(
-        name="vibration", device_class=DEVICE_CLASS_VIBRATION
+        name="vibration",
+        device_class=DEVICE_CLASS_VIBRATION,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
 }
 
--- homeassistant/components/shelly/sensor.py
+++ homeassistant/components/shelly/sensor.py
@@ -25,6 +25,10 @@ SENSORS = {
         unit=temperature_unit,
         value=lambda value: round(value, 1),
         device_class=sensor.DEVICE_CLASS_TEMPERATURE,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
         default_enabled=False,
     ),
     ("emeter", "current"): BlockAttributeDescription(
@@ -32,12 +36,20 @@ SENSORS = {
         unit=ELECTRICAL_CURRENT_AMPERE,
         value=lambda value: value,
         device_class=sensor.DEVICE_CLASS_CURRENT,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("light", "power"): BlockAttributeDescription(
         name="Power",
         unit=POWER_WATT,
         value=lambda value: round(value, 1),
         device_class=sensor.DEVICE_CLASS_POWER,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
         default_enabled=False,
     ),
     ("device", "power"): BlockAttributeDescription(
@@ -45,36 +57,60 @@ SENSORS = {
         unit=POWER_WATT,
         value=lambda value: round(value, 1),
         device_class=sensor.DEVICE_CLASS_POWER,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("emeter", "power"): BlockAttributeDescription(
         name="Power",
         unit=POWER_WATT,
         value=lambda value: round(value, 1),
         device_class=sensor.DEVICE_CLASS_POWER,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("relay", "power"): BlockAttributeDescription(
         name="Power",
         unit=POWER_WATT,
         value=lambda value: round(value, 1),
         device_class=sensor.DEVICE_CLASS_POWER,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("device", "energy"): BlockAttributeDescription(
         name="Energy",
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 60 / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("emeter", "energy"): BlockAttributeDescription(
         name="Energy",
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("light", "energy"): BlockAttributeDescription(
         name="Energy",
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 60 / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
         default_enabled=False,
     ),
     ("relay", "energy"): BlockAttributeDescription(
@@ -82,6 +118,10 @@ SENSORS = {
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 60 / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("sensor", "concentration"): BlockAttributeDescription(
         name="Gas Concentration",
@@ -89,25 +129,48 @@ SENSORS = {
         value=lambda value: value,
         # "sensorOp" is "normal" when the Shelly Gas is working properly and taking measurements.
         available=lambda block: block.sensorOp == "normal",
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("sensor", "extTemp"): BlockAttributeDescription(
         name="Temperature",
         unit=temperature_unit,
         value=lambda value: round(value, 1),
         device_class=sensor.DEVICE_CLASS_TEMPERATURE,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("sensor", "humidity"): BlockAttributeDescription(
         name="Humidity",
         unit=PERCENTAGE,
         value=lambda value: round(value, 1),
         device_class=sensor.DEVICE_CLASS_HUMIDITY,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
     ("sensor", "luminosity"): BlockAttributeDescription(
         name="Luminosity",
         unit="lx",
         device_class=sensor.DEVICE_CLASS_ILLUMINANCE,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
     ),
-    ("sensor", "tilt"): BlockAttributeDescription(name="tilt", unit=DEGREE),
+    ("sensor", "tilt"): BlockAttributeDescription(
+        name="tilt",
+        unit=DEGREE,
+        device_state_attributes=lambda wrapper: {
+            "ip address": wrapper.device.ip,
+            "Shelly id": wrapper.device.id,
+        },
+    ),
 }
 
 
