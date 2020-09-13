70f68d992cf689be8122f88b533492985ec598ab
AJ Schmidt <ajs4392@gmail.com>
2020-09-13 16:28:32 -0400

    rename devices -> entities

--- homeassistant/components/alarmdecoder/binary_sensor.py
+++ homeassistant/components/alarmdecoder/binary_sensor.py
@@ -40,7 +40,7 @@ async def async_setup_entry(
 
     zones = entry.options.get(OPTIONS_ZONES, DEFAULT_ZONE_OPTIONS)
 
-    devices = []
+    entities = []
     for zone_num in zones:
         zone_info = zones[zone_num]
         zone_type = zone_info[CONF_ZONE_TYPE]
@@ -52,10 +52,9 @@ async def async_setup_entry(
         device = AlarmDecoderBinarySensor(
             zone_num, zone_name, zone_type, zone_rfid, zone_loop, relay_addr, relay_chan
         )
-        devices.append(device)
+        entities.append(device)
 
-    async_add_entities(devices)
-    return True
+    async_add_entities(entities)
 
 
 class AlarmDecoderBinarySensor(BinarySensorEntity):
