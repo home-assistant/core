00acb180d618472d71e66fdc9f7a533ca4b5c75f
Chris Talkington <chris@talkingtontech.com>
2020-09-13 16:36:26 -0500

    Add canary alarm_control_panel tests (#40029)
    * add canary alarm_control_panel tests
    
    * Update .coveragerc
    
    * Create test_alarm_control_panel.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py
    
    * Update test_alarm_control_panel.py

--- tests/components/canary/__init__.py
+++ tests/components/canary/__init__.py
@@ -3,24 +3,6 @@ from unittest.mock import MagicMock, PropertyMock
 
 from canary.api import SensorType
 
-from homeassistant.components.homeassistant import (
-    DOMAIN as HA_DOMAIN,
-    SERVICE_UPDATE_ENTITY,
-)
-from homeassistant.const import ATTR_ENTITY_ID
-from homeassistant.core import HomeAssistant
-
-
-async def update_entity(hass: HomeAssistant, entity_id: str) -> None:
-    """Run an update action for an entity."""
-    await hass.services.async_call(
-        HA_DOMAIN,
-        SERVICE_UPDATE_ENTITY,
-        {ATTR_ENTITY_ID: entity_id},
-        blocking=True,
-    )
-    await hass.async_block_till_done()
-
 
 def mock_device(device_id, name, is_online=True, device_type_name=None):
     """Mock Canary Device class."""
@@ -34,13 +16,17 @@ def mock_device(device_id, name, is_online=True, device_type_name=None):
     return device
 
 
-def mock_location(location_id, name, is_celsius=True, devices=None):
+def mock_location(
+    location_id, name, is_celsius=True, devices=None, mode=None, is_private=False
+):
     """Mock Canary Location class."""
     location = MagicMock()
     type(location).location_id = PropertyMock(return_value=location_id)
     type(location).name = PropertyMock(return_value=name)
     type(location).is_celsius = PropertyMock(return_value=is_celsius)
+    type(location).is_private = PropertyMock(return_value=is_private)
     type(location).devices = PropertyMock(return_value=devices or [])
+    type(location).mode = PropertyMock(return_value=mode)
     return location
 
 
