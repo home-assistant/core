b73bb3a153fcab8f812e818a6de7480abb4cbc4e
Chris Talkington <chris@talkingtontech.com>
2020-09-12 22:05:09 -0500

    Update test_init.py

--- tests/components/canary/test_init.py
+++ tests/components/canary/test_init.py
@@ -1,4 +1,6 @@
 """The tests for the Canary component."""
+from requests import HTTPError
+
 from homeassistant.components.canary import DOMAIN
 from homeassistant.setup import async_setup_component
 
@@ -21,4 +23,25 @@ async def test_setup_with_valid_config(hass, canary) -> None:
         return_value=True,
     ):
         assert await async_setup_component(hass, DOMAIN, config)
+        await hass.async_block_till_done()
+
+
+async def test_setup_with_http_error(hass, canary) -> None:
+    """Test setup with HTTP error."""
+    await async_setup_component(hass, "persistent_notification", {})
+    config = {DOMAIN: {"username": "test-username", "password": "test-password"}}
+
+    canary.side_effect = HTTPError()
+
+    with patch(
+        "homeassistant.components.canary.alarm_control_panel.setup_platform",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.canary.camera.setup_platform",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.canary.sensor.setup_platform",
+        return_value=True,
+    ):
+        assert not await async_setup_component(hass, DOMAIN, config)
         await hass.async_block_till_done()
