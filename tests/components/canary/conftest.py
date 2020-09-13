7c68bd9a41240c4084d3a7f6e59d01152139b906
Chris Talkington <chris@talkingtontech.com>
2020-09-12 21:22:03 -0500

    Update conftest.py

--- tests/components/canary/conftest.py
+++ tests/components/canary/conftest.py
@@ -7,6 +7,20 @@ from pytest import fixture
 from tests.async_mock import MagicMock, patch
 
 
+def mock_canary_update(self, **kwargs):
+    """Get the latest data from py-canary."""
+    for location in self._api.get_locations():
+        location_id = location.location_id
+
+        self._locations_by_id[location_id] = location
+
+        for device in location.devices:
+            if device.is_online:
+                self._readings_by_device_id[
+                    device.device_id
+                ] = self._api.get_latest_readings(device.device_id)
+
+
 def mock_decorator(*args, **kwargs):
     """Mock decorator to patch unwanted decorators."""
 
@@ -24,6 +38,8 @@ def mock_decorator(*args, **kwargs):
 def canary(hass):
     """Mock the CanaryApi for easier testing."""
     with patch.object(Api, "login", return_value=True), patch(
+        "homeassistant.components.canary.CanaryData.update", mock_canary_update
+    ), patch(
         "homeassistant.components.canary.Api"
     ) as mock_canary:
         instance = mock_canary.return_value = Api(
@@ -44,4 +60,4 @@ def canary(hass):
         yield mock_canary
 
 
-patch("homeassistant.util.Throttle", mock_decorator).start()
+# patch("homeassistant.util.Throttle", mock_decorator).start()
