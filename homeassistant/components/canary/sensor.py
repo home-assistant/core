c77cad28cbe3d1a10d6ea79f1ba5ff125fc6cdce
Chris Talkington <chris@talkingtontech.com>
2020-09-13 22:39:11 -0500

    Update sensor.py

--- homeassistant/components/canary/sensor.py
+++ homeassistant/components/canary/sensor.py
@@ -65,7 +65,7 @@ class CanarySensor(Entity):
         self._data = data
         self._sensor_type = sensor_type
         self._device_name = device.name
-        self._device_type_name = device.device_type.name
+        self._device_type_name = device.device_type["name"]
         self._device_id = device.device_id
         self._sensor_value = None
 
