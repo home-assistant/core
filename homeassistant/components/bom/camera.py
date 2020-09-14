2653286ae80cec504b4268e11c1f4db4716c1235
Paul Madden <maddenp@colorado.edu>
2020-09-14 09:27:29 -0600

    Use bomradarloop v0.2.0

--- homeassistant/components/bom/camera.py
+++ homeassistant/components/bom/camera.py
@@ -6,8 +6,6 @@ from homeassistant.components.camera import PLATFORM_SCHEMA, Camera
 from homeassistant.const import CONF_ID, CONF_NAME
 from homeassistant.helpers import config_validation as cv
 
-CONF_DELTA = "delta"
-CONF_FRAMES = "frames"
 CONF_LOCATION = "location"
 CONF_OUTFILE = "filename"
 
@@ -72,15 +70,6 @@ LOCATIONS = [
 ]
 
 
-def _validate_schema(config):
-    if config.get(CONF_LOCATION) is None:
-        if not all(config.get(x) for x in (CONF_ID, CONF_DELTA, CONF_FRAMES)):
-            raise vol.Invalid(
-                f"Specify '{CONF_ID}', '{CONF_DELTA}' and '{CONF_FRAMES}' when '{CONF_LOCATION}' is unspecified"
-            )
-    return config
-
-
 LOCATIONS_MSG = f"Set '{CONF_LOCATION}' to one of: {', '.join(sorted(LOCATIONS))}"
 XOR_MSG = f"Specify exactly one of '{CONF_ID}' or '{CONF_LOCATION}'"
 
@@ -91,13 +80,10 @@ PLATFORM_SCHEMA = vol.All(
             vol.Exclusive(CONF_LOCATION, "xor", msg=XOR_MSG): vol.In(
                 LOCATIONS, msg=LOCATIONS_MSG
             ),
-            vol.Optional(CONF_DELTA): cv.positive_int,
-            vol.Optional(CONF_FRAMES): cv.positive_int,
             vol.Optional(CONF_NAME): cv.string,
             vol.Optional(CONF_OUTFILE): cv.string,
         }
     ),
-    _validate_schema,
 )
 
 
@@ -105,22 +91,19 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up BOM radar-loop camera component."""
     location = config.get(CONF_LOCATION) or f"ID {config.get(CONF_ID)}"
     name = config.get(CONF_NAME) or f"BOM Radar Loop - {location}"
-    args = [
-        config.get(x)
-        for x in (CONF_LOCATION, CONF_ID, CONF_DELTA, CONF_FRAMES, CONF_OUTFILE)
-    ]
+    args = [config.get(x) for x in (CONF_LOCATION, CONF_ID, CONF_OUTFILE)]
     add_entities([BOMRadarCam(name, *args)])
 
 
 class BOMRadarCam(Camera):
     """A camera component producing animated BOM radar-imagery GIFs."""
 
-    def __init__(self, name, location, radar_id, delta, frames, outfile):
+    def __init__(self, name, location, radar_id, outfile):
         """Initialize the component."""
 
         super().__init__()
         self._name = name
-        self._cam = BOMRadarLoop(location, radar_id, delta, frames, outfile)
+        self._cam = BOMRadarLoop(location, radar_id, outfile)
 
     def camera_image(self):
         """Return the current BOM radar-loop image."""
