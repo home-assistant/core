1d54f4e0a349c680ecfa2bd0fb1c359c6aedfc56
springstan <46536646+springstan@users.noreply.github.com>
2020-09-15 19:01:07 +0200

    Use http status constants more, add HTTP_ACCEPTED and HTTP_BAD_GATEWAY (#39993)
    * Use http status codes and add HTTP_BAD_GATEWAY constant
    
    * Address review comments:
     - using constants in tado integration
     - using constant in media_player init.py
    
    * Add and use HTTP_ACCEPTED constant

--- homeassistant/components/nest/local_auth.py
+++ homeassistant/components/nest/local_auth.py
@@ -4,6 +4,7 @@ from functools import partial
 
 from nest.nest import AUTHORIZE_URL, AuthorizationError, NestAuth
 
+from homeassistant.const import HTTP_UNAUTHORIZED
 from homeassistant.core import callback
 
 from . import config_flow
@@ -42,7 +43,7 @@ async def resolve_auth_code(hass, client_id, client_secret, code):
         await hass.async_add_job(auth.login)
         return await result
     except AuthorizationError as err:
-        if err.response.status_code == 401:
+        if err.response.status_code == HTTP_UNAUTHORIZED:
             raise config_flow.CodeInvalid()
         raise config_flow.NestAuthError(
             f"Unknown error: {err} ({err.response.status_code})"
