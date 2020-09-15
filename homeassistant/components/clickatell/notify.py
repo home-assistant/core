c6491fe390c8fb696c32cd5110536b9da06aed6b
Andrii Petrenko <aplsms@gmail.com>
2020-09-14 17:52:55 -0700

    fix clickatell API: added from field

--- homeassistant/components/clickatell/notify.py
+++ homeassistant/components/clickatell/notify.py
@@ -5,7 +5,7 @@ import requests
 import voluptuous as vol
 
 from homeassistant.components.notify import PLATFORM_SCHEMA, BaseNotificationService
-from homeassistant.const import CONF_API_KEY, CONF_RECIPIENT, HTTP_OK
+from homeassistant.const import CONF_API_KEY, CONF_RECIPIENT, CONF_SENDER, HTTP_OK
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
@@ -15,8 +15,7 @@ DEFAULT_NAME = "clickatell"
 BASE_API_URL = "https://platform.clickatell.com/messages/http/send"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {vol.Required(CONF_API_KEY): cv.string, vol.Required(CONF_RECIPIENT): cv.string}
-)
+    {vol.Required(CONF_API_KEY): cv.string, vol.Required(CONF_RECIPIENT): cv.string, vol.Required(CONF_SENDER): cv.string})
 
 
 def get_service(hass, config, discovery_info=None):
@@ -31,10 +30,11 @@ class ClickatellNotificationService(BaseNotificationService):
         """Initialize the service."""
         self.api_key = config[CONF_API_KEY]
         self.recipient = config[CONF_RECIPIENT]
+        self.sender = config[CONF_SENDER]
 
     def send_message(self, message="", **kwargs):
         """Send a message to a user."""
-        data = {"apiKey": self.api_key, "to": self.recipient, "content": message}
+        data = {"apiKey": self.api_key, "to": self.recipient, "from":self.sender, "content": message}
 
         resp = requests.get(BASE_API_URL, params=data, timeout=5)
         if (resp.status_code != HTTP_OK) or (resp.status_code != 202):
