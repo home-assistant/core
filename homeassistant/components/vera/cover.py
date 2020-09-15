9a1ad25a8f65cc0b0304e4c83adb82e8262f3113
Robert Van Gorkom <vangorra@users.noreply.github.com>
2020-09-14 20:06:52 -0700

    Add support for multiple vera controller hubs (#33613)

--- homeassistant/components/vera/cover.py
+++ homeassistant/components/vera/cover.py
@@ -13,7 +13,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
 
 from . import VeraDevice
-from .const import DOMAIN
+from .common import ControllerData, get_controller_data
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,10 +24,10 @@ async def async_setup_entry(
     async_add_entities: Callable[[List[Entity], bool], None],
 ) -> None:
     """Set up the sensor config entry."""
-    controller_data = hass.data[DOMAIN]
+    controller_data = get_controller_data(hass, entry)
     async_add_entities(
         [
-            VeraCover(device, controller_data.controller)
+            VeraCover(device, controller_data)
             for device in controller_data.devices.get(PLATFORM_DOMAIN)
         ]
     )
@@ -36,9 +36,9 @@ async def async_setup_entry(
 class VeraCover(VeraDevice, CoverEntity):
     """Representation a Vera Cover."""
 
-    def __init__(self, vera_device, controller):
+    def __init__(self, vera_device, controller_data: ControllerData):
         """Initialize the Vera device."""
-        VeraDevice.__init__(self, vera_device, controller)
+        VeraDevice.__init__(self, vera_device, controller_data)
         self.entity_id = ENTITY_ID_FORMAT.format(self.vera_id)
 
     @property
--- homeassistant/components/vera/sensor.py
+++ homeassistant/components/vera/sensor.py
@@ -13,7 +13,7 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.util import convert
 
 from . import VeraDevice
-from .const import DOMAIN
+from .common import ControllerData, get_controller_data
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -26,10 +26,10 @@ async def async_setup_entry(
     async_add_entities: Callable[[List[Entity], bool], None],
 ) -> None:
     """Set up the sensor config entry."""
-    controller_data = hass.data[DOMAIN]
+    controller_data = get_controller_data(hass, entry)
     async_add_entities(
         [
-            VeraSensor(device, controller_data.controller)
+            VeraSensor(device, controller_data)
             for device in controller_data.devices.get(PLATFORM_DOMAIN)
         ]
     )
@@ -38,12 +38,12 @@ async def async_setup_entry(
 class VeraSensor(VeraDevice, Entity):
     """Representation of a Vera Sensor."""
 
-    def __init__(self, vera_device, controller):
+    def __init__(self, vera_device, controller_data: ControllerData):
         """Initialize the sensor."""
         self.current_value = None
         self._temperature_units = None
         self.last_changed_time = None
-        VeraDevice.__init__(self, vera_device, controller)
+        VeraDevice.__init__(self, vera_device, controller_data)
         self.entity_id = ENTITY_ID_FORMAT.format(self.vera_id)
 
     @property
