14c3412c5b6b00c57b07ef7f9ff4b7f5bc1b0cdf
tomasz75 <tomek@sviete.pl>
2020-09-16 20:41:04 +0200

    bump frontend version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ REQUIRES = [
     "ruamel.yaml==0.15.100",
     "voluptuous==0.11.7",
     "voluptuous-serialize==2.4.0",
-    "ais-dom-frontend==20200915.0",
+    "ais-dom-frontend==20200915.1",
     "yarl==1.4.2",
 ]
 
