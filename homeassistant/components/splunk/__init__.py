3a5cf4f56c7e69bd8cfa6a68665a51c97e2036b7
Brett Adams <brett@ba.id.au>
2020-09-16 02:05:25 +0000

    Rebuilt Splunk using splunk_data_sender

--- homeassistant/components/splunk/__init__.py
+++ homeassistant/components/splunk/__init__.py
@@ -1,9 +1,10 @@
 """Support to send data to an Splunk instance."""
 import json
 import logging
+import time
 
-from aiohttp.hdrs import AUTHORIZATION
-import requests
+from requests.exceptions import ConnectionError, HTTPError, Timeout, TooManyRedirects
+from splunk_data_sender import SplunkSender
 import voluptuous as vol
 
 from homeassistant.const import (
@@ -13,7 +14,9 @@ from homeassistant.const import (
     CONF_SSL,
     CONF_TOKEN,
     CONF_VERIFY_SSL,
+    EVENT_HOMEASSISTANT_STOP,
     EVENT_STATE_CHANGED,
+    EVENT_TIME_CHANGED,
 )
 from homeassistant.helpers import state as state_helper
 import homeassistant.helpers.config_validation as cv
@@ -22,8 +25,9 @@ from homeassistant.helpers.json import JSONEncoder
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_FILTER = "filter"
 DOMAIN = "splunk"
+CONF_FILTER = "filter"
+SPLUNK_ENDPOINT = "collector/event"
 
 DEFAULT_HOST = "localhost"
 DEFAULT_PORT = 8088
@@ -48,22 +52,6 @@ CONFIG_SCHEMA = vol.Schema(
 )
 
 
-def post_request(event_collector, body, headers, verify_ssl):
-    """Post request to Splunk."""
-    try:
-        payload = {"host": event_collector, "event": body}
-        requests.post(
-            event_collector,
-            data=json.dumps(payload, cls=JSONEncoder),
-            headers=headers,
-            timeout=10,
-            verify=verify_ssl,
-        )
-
-    except requests.exceptions.RequestException as error:
-        _LOGGER.exception("Error saving event to Splunk: %s", error)
-
-
 def setup(hass, config):
     """Set up the Splunk component."""
     conf = config[DOMAIN]
@@ -75,16 +63,50 @@ def setup(hass, config):
     name = conf.get(CONF_NAME)
     entity_filter = conf[CONF_FILTER]
 
-    if use_ssl:
-        uri_scheme = "https://"
-    else:
-        uri_scheme = "http://"
+    if verify_ssl and not use_ssl:
+        _LOGGER.error("You cannot use verify_ssl without ssl")
+        return False
 
-    event_collector = f"{uri_scheme}{host}:{port}/services/collector/event"
-    headers = {AUTHORIZATION: f"Splunk {token}"}
+    event_collector = SplunkSender(
+        host=host,
+        port=port,
+        token=token,
+        hostname=name,
+        protocol=["http", "https"][use_ssl],
+        verify=verify_ssl,
+        api_url=SPLUNK_ENDPOINT,
+    )
+
+    batch = []
+
+    def queue(payload):
+        batch.append(json.dumps(payload, cls=JSONEncoder))
+
+    payload = {
+        "time": time.time(),
+        "host": name,
+        "event": {
+            "domain": DOMAIN,
+            "meta": "Home Assistant has started",
+            "value": 1,
+        },
+    }
+
+    try:
+        event_collector._send_to_splunk("send-event", json.dumps(payload))
+    except HTTPError as e:
+        if e.response.status_code in (401, 403):
+            _LOGGER.error("Invalid or disabled token")
+            return False
+        else:
+            _LOGGER.warn(e)
+    except (Timeout, ConnectionError, TooManyRedirects) as e:
+        _LOGGER.warn(e)
 
     def splunk_event_listener(event):
         """Listen for new messages on the bus and sends them to Splunk."""
+
+        nonlocal batch
         state = event.data.get("new_state")
 
         if state is None or not entity_filter(state.entity_id):
@@ -95,19 +117,47 @@ def setup(hass, config):
         except ValueError:
             _state = state.state
 
-        json_body = [
+        queue(
             {
-                "domain": state.domain,
-                "entity_id": state.object_id,
-                "attributes": dict(state.attributes),
-                "time": str(event.time_fired),
-                "value": _state,
+                "time": event.time_fired.timestamp(),
                 "host": name,
+                "event": {
+                    "domain": state.domain,
+                    "entity_id": state.object_id,
+                    "attributes": dict(state.attributes),
+                    "value": _state,
+                },
             }
-        ]
+        )
+        return True
 
-        post_request(event_collector, json_body, headers, verify_ssl)
+    def splunk_send(event=None):
+        """Send batched messages to Splunk every second."""
+        nonlocal batch
+        if len(batch):
+            events = batch
+            batch = []
+            try:
+                event_collector._send_to_splunk("send-event", "".join(events))
+            except (HTTPError, Timeout, ConnectionError, TooManyRedirects) as e:
+                _LOGGER.warn(e)
+
+    def splunk_stop(event):
+        queue(
+            {
+                "time": event.time_fired.timestamp(),
+                "host": name,
+                "event": {
+                    "domain": DOMAIN,
+                    "meta": "Home Assistant is stopping",
+                    "value": 0,
+                },
+            }
+        )
+        splunk_send()
 
     hass.bus.listen(EVENT_STATE_CHANGED, splunk_event_listener)
+    hass.bus.listen(EVENT_TIME_CHANGED, splunk_send)
+    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, splunk_stop)
 
     return True
