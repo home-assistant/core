0559d30225f655b05ecc1e29aead24cbb323e80a
Brett Adams <brett@ba.id.au>
2020-09-16 06:16:14 +0000

    Working Integration

--- homeassistant/components/advantage_air/cover.py
+++ homeassistant/components/advantage_air/cover.py
@@ -1,3 +1,5 @@
+"""Support for Advantage Air Zone Vent control."""
+
 from homeassistant.components.cover import (
     ATTR_POSITION,
     DEVICE_CLASS_DAMPER,
@@ -6,137 +8,164 @@ from homeassistant.components.cover import (
     SUPPORT_SET_POSITION,
     CoverEntity,
 )
-from homeassistant.const import STATE_CLOSED, STATE_OPEN
+from homeassistant.const import CONF_IP_ADDRESS, STATE_OPEN
 
-from .const import ADVANTAGE_AIR_ZONE_CLOSE, ADVANTAGE_AIR_ZONE_OPEN, DOMAIN
+from .const import DOMAIN, STATE_CLOSE
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Platform setup isnt required."""
     return True
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up MyAir cover platform."""
+    """Set up AdvantageAir cover platform."""
 
-    my = hass.data[DOMAIN][config_entry.data.get("url")]
+    instance = hass.data[DOMAIN][config_entry.data[CONF_IP_ADDRESS]]
 
     entities = []
-    for _, acx in enumerate(my["coordinator"].data["aircons"]):
-        for _, zx in enumerate(my["coordinator"].data["aircons"][acx]["zones"]):
-            # Only add zone damper controls when zone in damper control.
-            if my["coordinator"].data["aircons"][acx]["zones"][zx]["type"] == 0:
-                entities.append(MyAirZoneDamper(my, acx, zx))
+    for _, ac_index in enumerate(instance["coordinator"].data["aircons"]):
+        for _, zone_index in enumerate(
+            instance["coordinator"].data["aircons"][ac_index]["zones"]
+        ):
+            # Only add zone vent controls when zone in vent control mode.
+            if (
+                instance["coordinator"].data["aircons"][ac_index]["zones"][zone_index][
+                    "type"
+                ]
+                == 0
+            ):
+                entities.append(AdvantageAirZoneVent(instance, ac_index, zone_index))
     async_add_entities(entities)
     return True
 
 
-class MyAirZoneDamper(CoverEntity):
-    """MyAir Zone Damper"""
+class AdvantageAirZoneVent(CoverEntity):
+    """Representation of Advantage Air Zone Vent Cover."""
 
-    def __init__(self, my, acx, zx):
-        self.coordinator = my["coordinator"]
-        self.async_set_data = my["async_set_data"]
-        self.device = my["device"]
-        self.acx = acx
-        self.zx = zx
+    def __init__(self, instance, ac_index, zone_index):
+        """Initialize the Advantage Air Zone Vent Cover."""
+        self.coordinator = instance["coordinator"]
+        self.async_change = instance["async_change"]
+        self.device = instance["device"]
+        self.ac_index = ac_index
+        self.zone_index = zone_index
 
     @property
     def name(self):
-        return f"{self.coordinator.data['aircons'][self.acx]['zones'][self.zx]['name']} Vent"
+        """Return the name."""
+        return f"{self.coordinator.data['aircons'][self.ac_index]['zones'][self.zone_index]['name']} Vent"
 
     @property
     def unique_id(self):
-        return f"{self.coordinator.data['system']['rid']}-{self.acx}-{self.zx}-cover"
+        """Return a unique id."""
+        return f"{self.coordinator.data['system']['rid']}-{self.ac_index}-{self.zone_index}-cover"
 
     @property
     def device_class(self):
+        """Return the device class of the vent."""
         return DEVICE_CLASS_DAMPER
 
     @property
     def supported_features(self):
+        """Return the supported features."""
         return SUPPORT_OPEN | SUPPORT_CLOSE | SUPPORT_SET_POSITION
 
     @property
     def is_closed(self):
+        """Return if vent is fully closed."""
         return (
-            self.coordinator.data["aircons"][self.acx]["zones"][self.zx]["state"]
-            == ADVANTAGE_AIR_ZONE_CLOSE
+            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
+                "state"
+            ]
+            == STATE_CLOSE
         )
 
     @property
     def is_opening(self):
+        """Platform is unaware of opening state."""
         return False
 
     @property
     def is_closing(self):
+        """Platform is unaware of closing state."""
         return False
 
     @property
     def current_cover_position(self):
+        """Return vents current possition as a percentage."""
         if (
-            self.coordinator.data["aircons"][self.acx]["zones"][self.zx]["state"]
-            == ADVANTAGE_AIR_ZONE_OPEN
+            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
+                "state"
+            ]
+            == STATE_OPEN
         ):
-            return self.coordinator.data["aircons"][self.acx]["zones"][self.zx]["value"]
-        else:
-            return 0
+            return self.coordinator.data["aircons"][self.ac_index]["zones"][
+                self.zone_index
+            ]["value"]
+        return 0
 
     @property
     def icon(self):
+        """Return vent icon."""
         return ["mdi:fan-off", "mdi:fan"][
-            self.coordinator.data["aircons"][self.acx]["zones"][self.zx]["state"]
-            == ADVANTAGE_AIR_ZONE_OPEN
+            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
+                "state"
+            ]
+            == STATE_OPEN
         ]
 
     @property
     def should_poll(self):
+        """No polling needed."""
         return False
 
     @property
     def available(self):
+        """Return if platform is avaliable."""
         return self.coordinator.last_update_success
 
     @property
     def device_info(self):
+        """Return parent device information."""
         return self.device
 
     async def async_open_cover(self, **kwargs):
-        await self.async_set_data(
+        """Fully open zone vent."""
+        await self.async_change(
             {
-                self.acx: {
-                    "zones": {self.zx: {"state": ADVANTAGE_AIR_ZONE_OPEN, "value": 100}}
+                self.ac_index: {
+                    "zones": {self.zone_index: {"state": STATE_OPEN, "value": 100}}
                 }
             }
         )
-        # await self.coordinator.async_request_refresh()
 
     async def async_close_cover(self, **kwargs):
-        await self.async_set_data(
-            {self.acx: {"zones": {self.zx: {"state": ADVANTAGE_AIR_ZONE_CLOSE}}}}
+        """Fully close zone vent."""
+        await self.async_change(
+            {self.ac_index: {"zones": {self.zone_index: {"state": STATE_CLOSE}}}}
         )
-        # await self.coordinator.async_request_refresh()
 
     async def async_set_cover_position(self, **kwargs):
+        """Change vent position."""
         position = round(kwargs.get(ATTR_POSITION) / 5) * 5
         if position == 0:
-            await self.async_set_data(
-                {self.acx: {"zones": {self.zx: {"state": ADVANTAGE_AIR_ZONE_CLOSE}}}}
+            await self.async_change(
+                {self.ac_index: {"zones": {self.zone_index: {"state": STATE_CLOSE}}}}
             )
         else:
-            await self.async_set_data(
+            await self.async_change(
                 {
-                    self.acx: {
+                    self.ac_index: {
                         "zones": {
-                            self.zx: {
-                                "state": ADVANTAGE_AIR_ZONE_OPEN,
+                            self.zone_index: {
+                                "state": STATE_OPEN,
                                 "value": position,
                             }
                         }
                     }
                 }
             )
-
-        # await self.coordinator.async_request_refresh()
 
     async def async_added_to_hass(self):
         """When entity is added to hass."""
@@ -145,4 +174,5 @@ class MyAirZoneDamper(CoverEntity):
         )
 
     async def async_update(self):
+        """Request update."""
         await self.coordinator.async_request_refresh()
--- homeassistant/components/advantage_air/switch.py
+++ homeassistant/components/advantage_air/switch.py
@@ -1,59 +1,72 @@
+"""Switch platform for Advantage Air integration."""
+from homeassistant.const import CONF_IP_ADDRESS
 from homeassistant.helpers.entity import ToggleEntity
 
 from .const import DOMAIN
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-
+    """Platform setup isnt required."""
     return True
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up MyAir toggle platform."""
+    """Set up AdvantageAir toggle platform."""
 
-    my = hass.data[DOMAIN][config_entry.data.get("url")]
+    instance = hass.data[DOMAIN][config_entry.data[CONF_IP_ADDRESS]]
 
-    if "aircons" in my["coordinator"].data:
+    if "aircons" in instance["coordinator"].data:
         entities = []
-        for _, acx in enumerate(my["coordinator"].data["aircons"]):
+        for _, ac_index in enumerate(instance["coordinator"].data["aircons"]):
             if (
-                my["coordinator"].data["aircons"][acx]["info"]["freshAirStatus"]
+                instance["coordinator"].data["aircons"][ac_index]["info"][
+                    "freshAirStatus"
+                ]
                 != "none"
             ):
-                entities.append(MyAirZoneFreshAir(my, acx))
+                entities.append(AdvantageAirZoneFreshAir(instance, ac_index))
         async_add_entities(entities)
     return True
 
 
-class MyAirZoneFreshAir(ToggleEntity):
-    """MyAir Fresh Air Toggle"""
+class AdvantageAirZoneFreshAir(ToggleEntity):
+    """Representation of Advantage Air fresh air control."""
 
-    def __init__(self, my, acx):
-        self.coordinator = my["coordinator"]
-        self.async_set_data = my["async_set_data"]
-        self.device = my["device"]
-        self.acx = acx
+    def __init__(self, instance, ac_index):
+        """Initialize the Advantage Air Zone fresh air control."""
+        self.coordinator = instance["coordinator"]
+        self.async_change = instance["async_change"]
+        self.device = instance["device"]
+        self.ac_index = ac_index
 
     @property
     def name(self):
-        return f"{self.coordinator.data['aircons'][self.acx]['info']['name']} Fresh Air"
+        """Return the name."""
+        return f"{self.coordinator.data['aircons'][self.ac_index]['info']['name']} Fresh Air"
 
     @property
     def unique_id(self):
-        return f"{self.coordinator.data['system']['rid']}-{self.acx}-toggle:freshair"
+        """Return a unique id."""
+        return (
+            f"{self.coordinator.data['system']['rid']}-{self.ac_index}-toggle:freshair"
+        )
 
     @property
     def is_on(self):
+        """Return the fresh air status."""
         return (
-            self.coordinator.data["aircons"][self.acx]["info"]["freshAirStatus"] == "on"
+            self.coordinator.data["aircons"][self.ac_index]["info"]["freshAirStatus"]
+            == "on"
         )
 
     @property
     def should_poll(self):
+        """No polling needed."""
         return False
 
     @property
     def icon(self):
+        """Return a representative icon of the timer."""
         return "mdi:air-filter"
 
     @property
@@ -65,13 +78,15 @@ class MyAirZoneFreshAir(ToggleEntity):
         return self.device
 
     async def async_turn_on(self, **kwargs):
-        await self.async_set_data(
-            {self.acx: {"zones": {"info": {"freshAirStatus": "on"}}}}
+        """Turn Fresh Air On."""
+        await self.async_change(
+            {self.ac_index: {"zones": {"info": {"freshAirStatus": "on"}}}}
         )
 
     async def async_turn_off(self, **kwargs):
-        await self.async_set_data(
-            {self.acx: {"zones": {"info": {"freshAirStatus": "off"}}}}
+        """Turn Fresh Air Off."""
+        await self.async_change(
+            {self.ac_index: {"zones": {"info": {"freshAirStatus": "off"}}}}
         )
 
     async def async_added_to_hass(self):
@@ -81,4 +96,5 @@ class MyAirZoneFreshAir(ToggleEntity):
         )
 
     async def async_update(self):
+        """Request update."""
         await self.coordinator.async_request_refresh()
