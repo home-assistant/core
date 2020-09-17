33a712405d916b5be803b8bd2ac8374e26e4233f
chemelli74 <simone.chemelli@gmail.com>
2020-09-17 08:48:49 +0200

    Added calc for all devices channels number

--- homeassistant/components/shelly/entity.py
+++ homeassistant/components/shelly/entity.py
@@ -22,7 +22,6 @@ def temperature_unit(block_info: dict) -> str:
 
 def shelly_naming(self, block, entity_type: str):
     """Naming for switch and sensors."""
-
     if self.wrapper.model in ["SHDW-2", "SHHT-1", "SHWT-1"]:
         channels = 0
     else:
