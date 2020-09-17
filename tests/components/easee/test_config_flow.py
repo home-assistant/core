2db7db7e07bc31a695a3152b65cd67b241fc3480
Niklas Fondberg <niklas.fondberg@photowall.se>
2020-09-17 14:06:11 +0200

    Add basic tests

---
+++ tests/components/easee/test_config_flow.py
@@ -0,0 +1,63 @@
+"""Test the Easee EV Charger config flow."""
+from easee import AuthorizationFailedException
+
+from homeassistant import config_entries, setup
+from homeassistant.components.easee.const import DOMAIN
+
+from tests.async_mock import patch
+
+
+async def test_form(hass):
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == "form"
+    assert result["errors"] == {}
+
+    with patch("easee.Easee.connect", return_value=True,), patch(
+        "homeassistant.components.easee.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.easee.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
+
+    assert result2["type"] == "create_entry"
+
+    assert result2["data"] == {
+        "username": "test-username",
+        "password": "test-password",
+    }
+    await hass.async_block_till_done()
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_form_cannot_connect(hass):
+    """Test we handle cannot connect error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "easee.Easee.connect",
+        side_effect=AuthorizationFailedException,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "connection_failure"}
