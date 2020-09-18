fe9e657ce0399398fbab2d4112db623b28aeabfd
raman325 <7243222+raman325@users.noreply.github.com>
2020-09-18 11:43:23 -0400

    pylint

--- homeassistant/components/climacell/config_flow.py
+++ homeassistant/components/climacell/config_flow.py
@@ -112,6 +112,7 @@ class ClimaCellConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+    DOMAIN = DOMAIN
 
     @staticmethod
     @callback
