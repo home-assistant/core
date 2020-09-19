2171981c77b403e6b49144254bab2f2e74bc4b22
Florian Lagg <florian.lagg@gmail.com>
2020-09-19 20:02:34 +0000

    added brightness, color, color_temp + fixes

--- homeassistant/components/govee/light.py
+++ homeassistant/components/govee/light.py
@@ -11,7 +11,14 @@ from homeassistant import config_entries, core, exceptions
 from homeassistant.components.light import (
     LightEntity,
     SUPPORT_BRIGHTNESS,
-)  # add supports
+    SUPPORT_COLOR,
+    SUPPORT_COLOR_TEMP,
+    ATTR_BRIGHTNESS,
+    ATTR_RGB_COLOR,
+    ATTR_HS_COLOR,
+    ATTR_COLOR_TEMP,
+)
+from homeassistant.util import color
 
 _LOGGER = logging.getLogger(__name__)
 SCAN_INTERVAL = timedelta(seconds=10)
@@ -41,8 +48,6 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         [GoveeLightEntity(HUB, config_entry.title, device) for device in devices], True
     )
 
-    # TODO: check status every interval
-
 
 class GoveeLightEntity(LightEntity):
     """ representation of a stateful light entity """
@@ -52,36 +57,74 @@ class GoveeLightEntity(LightEntity):
         self._hub = hub
         self._title = title
         self._device = device
-        self._is_on = False
+        self._state = GoveeDeviceState(
+            device=device.device,
+            model=device.model,
+            online=False,
+            power_state=False,
+            brightness=0,
+            color=(0, 0, 0),
+        )
 
     @property
     def supported_features(self):
         """Flag supported features."""
-        return 0  # SUPPORT_BRIGHTNESS, ....
+        return SUPPORT_BRIGHTNESS | SUPPORT_COLOR | SUPPORT_COLOR_TEMP
 
     async def async_update(self):
         """ get state of the led strip """
-        _LOGGER.debug(f"async_turn_on for Govee light {self._device.device}")
         state, err = await self._hub.get_state(self._device)
         if state:
-            self._is_on = state.power_state
-            # TODO: other states
+            self._state = state
         else:
-            _LOGGER.warn(f"cannot get state for {self._device.device}")
+            _LOGGER.warn(f"cannot get state for {self._device.device}: {err}")
 
     async def async_turn_on(self, **kwargs):
         """Turn device on."""
-        _LOGGER.debug(f"async_turn_on for Govee light {self._device.device}")
-        success, err = await self._hub.turn_on(self._device)
-        if success:
-            self._is_on = True
+        _LOGGER.debug(
+            f"async_turn_on for Govee light {self._device.device}, kwargs: {kwargs}"
+        )
+        success = False
+        err = None
+        if ATTR_RGB_COLOR in kwargs:
+            col = kwargs[ATTR_RGB_COLOR]
+            success, err = await self._hub.set_color(self._device, col)
+            if success:
+                self._state.power_state = True
+                self._state.color = col
+        elif ATTR_HS_COLOR in kwargs:
+            hs_color = kwargs[ATTR_HS_COLOR]
+            col = color.color_hs_to_RGB(hs_color[0], hs_color[1])
+            success, err = await self._hub.set_color(self._device, col)
+            if success:
+                self._state.power_state = True
+                self._state.color = col
+        elif ATTR_BRIGHTNESS in kwargs:
+            brightness = kwargs[ATTR_BRIGHTNESS]
+            success, err = await self._hub.set_brightness(self._device, brightness)
+            if success:
+                self._state.power_state = True
+                self._state.brightness = brightness
+        elif ATTR_COLOR_TEMP in kwargs:
+            color_temp = kwargs[ATTR_COLOR_TEMP]
+            success, err = await self._hub.set_color_temp(self._device, color_temp)
+            # color_temp is not in state
+        else:
+            success, err = await self._hub.turn_on(self._device)
+            if success:
+                self._state.power_state = True
+        # warn on any error
+        if err:
+            _LOGGER.warn(
+                f"async_turn_on failed with '{err}' for {self._device.device}, kwargs: {kwargs}"
+            )
 
     async def async_turn_off(self, **kwargs):
         """Turn device off."""
         _LOGGER.debug(f"async_turn_off for Govee light {self._device.device}")
         success, err = await self._hub.turn_off(self._device)
         if success:
-            self._is_on = False
+            self._state.power_state = False
 
     @property
     def unique_id(self):
@@ -106,10 +149,63 @@ class GoveeLightEntity(LightEntity):
             "name": self.name,
             "manufacturer": "Govee",
             "model": self._device.model,
-            # "sw_version": self.light.raw["swversion"],
             "via_device": (DOMAIN, "Govee API (cloud)"),
         }
 
     @property
     def is_on(self):
-        return self._is_on
+        """ return true if device is on """
+        return self._state.power_state
+
+    @property
+    def available(self):
+        """Return if light is available."""
+        return self._state.online
+
+    @property
+    def hs_color(self):
+        """Return the hs color value."""
+        return color.color_RGB_to_hs(
+            self._state.color[0],
+            self._state.color[1],
+            self._state.color[2],
+        )
+
+    @property
+    def rgb_color(self):
+        """Return the rgb color value."""
+        return [
+            self._state.color[0],
+            self._state.color[1],
+            self._state.color[2],
+        ]
+
+    @property
+    def brightness(self):
+        """Return the rgb color value."""
+        return self._state.brightness
+
+    @property
+    def min_mireds(self):
+        """Return the coldest color_temp that this light supports."""
+        return 2000
+
+    @property
+    def max_mireds(self):
+        """Return the warmest color_temp that this light supports."""
+        return 9000
+
+    @property
+    def device_state_attributes(self):
+        """Return the device state attributes."""
+        return {
+            # rate limiting information on Govee API
+            "rate_limit_total": self._hub.rate_limit_total,
+            "rate_limit_remaining": self._hub.rate_limit_remaining,
+            "rate_limit_reset_seconds": self._hub.rate_limit_reset_seconds,
+            "rate_limit_reset": self._hub.rate_limit_reset,
+            "rate_limit_on": self._hub.rate_limit_on,
+            # general information
+            "manufacturer": "Govee",
+            "model": self._device.model,
+        }
