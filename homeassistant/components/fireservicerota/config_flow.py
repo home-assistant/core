3ab8d8eb9b97fd25b7e24ed3ea9b5d14ecbbeeb0
Ron Klinkien <ron@cyberjunky.nl>
2020-09-20 09:21:28 +0000

    Added constant and simplified show_form code

--- homeassistant/components/fireservicerota/config_flow.py
+++ homeassistant/components/fireservicerota/config_flow.py
@@ -7,32 +7,20 @@ from homeassistant.const import CONF_PASSWORD, CONF_TOKEN, CONF_URL, CONF_USERNA
 
 from .const import DOMAIN, URL_LIST  # pylint: disable=unused-import
 
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_USERNAME): str,
+        vol.Required(CONF_PASSWORD): str,
+        vol.Required(CONF_URL, default="www.brandweerrooster.nl"): vol.In(URL_LIST),
+    }
+)
+
 
 class FireServiceRotaFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a FireServiceRota config flow."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
-
-    def __init__(self):
-        """Initialize the config flow."""
-        self.data_schema = vol.Schema(
-            {
-                vol.Required(CONF_USERNAME): str,
-                vol.Required(CONF_PASSWORD): str,
-                vol.Required(CONF_URL, default="www.brandweerrooster.nl"): vol.In(
-                    URL_LIST
-                ),
-            }
-        )
-
-    async def _show_form(self, errors=None):
-        """Show the form to the user."""
-        return self.async_show_form(
-            step_id="user",
-            data_schema=self.data_schema,
-            errors=errors if errors else {},
-        )
 
     async def async_step_user(self, user_input=None):
         """Handle the start of the config flow."""
@@ -50,7 +38,11 @@ class FireServiceRotaFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 token_info = await self.hass.async_add_executor_job(api.request_tokens)
 
             except InvalidAuthError:
-                return await self._show_form(errors={"base": "invalid_credentials"})
+                return self.async_show_form(
+                    step_id="user",
+                    data_schema=DATA_SCHEMA,
+                    errors={"base": "invalid_credentials"},
+                )
 
             return self.async_create_entry(
                 title=user_input[CONF_USERNAME],
@@ -62,5 +54,5 @@ class FireServiceRotaFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             )
 
         return self.async_show_form(
-            step_id="user", data_schema=self.data_schema, errors=errors
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
