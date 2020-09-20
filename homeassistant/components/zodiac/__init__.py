4dbe6b46abddc86d9ac0deb74dd2995f82acb7ec
Chris Talkington <chris@talkingtontech.com>
2020-09-20 17:22:19 -0500

    Update __init__.py

--- homeassistant/components/zodiac/__init__.py
+++ homeassistant/components/zodiac/__init__.py
@@ -2,6 +2,7 @@
 import voluptuous as vol
 
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.discovery import async_load_platform
 
 from .const import DOMAIN
 
@@ -13,4 +14,8 @@ CONFIG_SCHEMA = vol.Schema(
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the zodiac component."""
+    hass.async_create_task(
+        async_load_platform(hass, "sensor", DOMAIN, {}, config)
+    )
+
     return True
