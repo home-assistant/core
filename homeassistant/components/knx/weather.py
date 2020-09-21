06c1a7d50fa0232ff8f5f0b4440d720df192ee62
Marvin Wichmann <marvin.wichmann@unic.com>
2020-09-21 18:08:35 +0200

    Use centralized KnxEntity for all KNX platforms (#40381)

--- homeassistant/components/knx/weather.py
+++ homeassistant/components/knx/weather.py
@@ -5,34 +5,30 @@ from xknx.devices import Weather as XknxWeather
 from homeassistant.components.weather import WeatherEntity
 from homeassistant.const import TEMP_CELSIUS
 
-from .const import DATA_KNX
+from .const import DOMAIN
+from .knx_entity import KnxEntity
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the scenes for KNX platform."""
     entities = []
-    for device in hass.data[DATA_KNX].xknx.devices:
+    for device in hass.data[DOMAIN].xknx.devices:
         if isinstance(device, XknxWeather):
             entities.append(KNXWeather(device))
     async_add_entities(entities)
 
 
-class KNXWeather(WeatherEntity):
+class KNXWeather(KnxEntity, WeatherEntity):
     """Representation of a KNX weather device."""
 
     def __init__(self, device: XknxWeather):
         """Initialize of a KNX sensor."""
-        self.device = device
-
-    @property
-    def name(self):
-        """Return the name of the weather device."""
-        return self.device.name
+        super().__init__(device)
 
     @property
     def temperature(self):
         """Return current temperature."""
-        return self.device.temperature
+        return self._device.temperature
 
     @property
     def temperature_unit(self):
@@ -44,25 +40,27 @@ class KNXWeather(WeatherEntity):
         """Return current air pressure."""
         # KNX returns pA - HA requires hPa
         return (
-            self.device.air_pressure / 100
-            if self.device.air_pressure is not None
+            self._device.air_pressure / 100
+            if self._device.air_pressure is not None
             else None
         )
 
     @property
     def condition(self):
         """Return current weather condition."""
-        return self.device.ha_current_state().value
+        return self._device.ha_current_state().value
 
     @property
     def humidity(self):
         """Return current humidity."""
-        return self.device.humidity if self.device.humidity is not None else None
+        return self._device.humidity if self._device.humidity is not None else None
 
     @property
     def wind_speed(self):
         """Return current wind speed in km/h."""
         # KNX only supports wind speed in m/s
         return (
-            self.device.wind_speed * 3.6 if self.device.wind_speed is not None else None
+            self._device.wind_speed * 3.6
+            if self._device.wind_speed is not None
+            else None
         )
