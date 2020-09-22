8bff25feddafc754109b712ac880a7c799d9f229
cagnulein <Cagnulein@gmail.com>
2020-09-22 10:53:46 +0200

    Fix luci device_tracker incorrectly reporting devices status (#40409)

--- homeassistant/components/luci/device_tracker.py
+++ homeassistant/components/luci/device_tracker.py
@@ -94,7 +94,11 @@ class LuciDeviceScanner(DeviceScanner):
 
         last_results = []
         for device in result:
-            if device.reachable:
+            if (
+                not hasattr(self.router.router.owrt_version, "release")
+                or self.router.router.owrt_version.release[0] < 19
+                or device.reachable
+            ):
                 last_results.append(device)
 
         self.last_results = last_results
