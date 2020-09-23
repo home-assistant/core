e968dc973e68400bed5198fd036d2f30b909f4fe
chemelli74 <simone.chemelli@gmail.com>
2020-09-24 01:05:36 +0200

    Fixed wrong import

--- homeassistant/components/canary/sensor.py
+++ homeassistant/components/canary/sensor.py
@@ -3,7 +3,6 @@ from typing import Callable, List
 
 from canary.api import SensorType
 
-from homeassistant.components.const import SIGNAL_STRENGTH_DECIBELS_MILLIWATT
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     DEVICE_CLASS_BATTERY,
