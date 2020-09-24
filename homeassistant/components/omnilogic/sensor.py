def1822372c28d19265b2f08dcfbfbb67632e48d
djtimca <60706061+djtimca@users.noreply.github.com>
2020-09-24 16:07:44 -0400

    Update homeassistant/components/omnilogic/sensor.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/omnilogic/sensor.py
+++ homeassistant/components/omnilogic/sensor.py
@@ -61,7 +61,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
                 entities.append(entity)
 
-    async_add_entities(entities, update_before_add=True)
+    async_add_entities(entities)
 
 
 class OmnilogicSensor(OmniLogicEntity):
