0b11559031e507a98893cbe8078f4ca76b375ac5
Guido Schmitz <Shutgun@users.noreply.github.com>
2020-09-24 17:57:52 +0200

    Improve devolo Home Control code quality (#40480)
    Co-authored-by: Markus Bong <2Fake1987@gmail.com>

--- homeassistant/components/devolo_home_control/sensor.py
+++ homeassistant/components/devolo_home_control/sensor.py
@@ -33,7 +33,7 @@ async def async_setup_entry(
     for device in hass.data[DOMAIN]["homecontrol"].multi_level_sensor_devices:
         for multi_level_sensor in device.multi_level_sensor_property:
             entities.append(
-                DevoloMultiLevelDeviceEntity(
+                DevoloGenericMultiLevelDeviceEntity(
                     homecontrol=hass.data[DOMAIN]["homecontrol"],
                     device_instance=device,
                     element_uid=multi_level_sensor,
@@ -55,44 +55,7 @@ async def async_setup_entry(
 
 
 class DevoloMultiLevelDeviceEntity(DevoloDeviceEntity):
-    """Representation of a multi level sensor within devolo Home Control."""
-
-    def __init__(
-        self,
-        homecontrol,
-        device_instance,
-        element_uid,
-        multi_level_sensor_property=None,
-        sync=None,
-    ):
-        """Initialize a devolo multi level sensor."""
-        if multi_level_sensor_property is None:
-            self._multi_level_sensor_property = (
-                device_instance.multi_level_sensor_property[element_uid]
-            )
-        else:
-            self._multi_level_sensor_property = multi_level_sensor_property
-
-        self._state = self._multi_level_sensor_property.value
-
-        self._device_class = DEVICE_CLASS_MAPPING.get(
-            self._multi_level_sensor_property.sensor_type
-        )
-
-        name = device_instance.item_name
-
-        if self._device_class is None:
-            name += f" {self._multi_level_sensor_property.sensor_type}"
-
-        self._unit = self._multi_level_sensor_property.unit
-
-        super().__init__(
-            homecontrol=homecontrol,
-            device_instance=device_instance,
-            element_uid=element_uid,
-            name=name,
-            sync=self._sync if sync is None else sync,
-        )
+    """Abstract representation of a multi level sensor within devolo Home Control."""
 
     @property
     def device_class(self) -> str:
@@ -102,24 +65,43 @@ class DevoloMultiLevelDeviceEntity(DevoloDeviceEntity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self._state
+        return self._value
 
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement of this entity."""
         return self._unit
 
-    def _sync(self, message=None):
-        """Update the multi level sensor state."""
-        if message[0] == self._multi_level_sensor_property.element_uid:
-            self._state = self._device_instance.multi_level_sensor_property[
-                message[0]
-            ].value
-        elif message[0].startswith("hdm"):
-            self._available = self._device_instance.is_online()
-        else:
-            _LOGGER.debug("No valid message received: %s", message)
-        self.schedule_update_ha_state()
+
+class DevoloGenericMultiLevelDeviceEntity(DevoloMultiLevelDeviceEntity):
+    """Representation of a generic multi level sensor within devolo Home Control."""
+
+    def __init__(
+        self,
+        homecontrol,
+        device_instance,
+        element_uid,
+    ):
+        """Initialize a devolo multi level sensor."""
+        self._multi_level_sensor_property = device_instance.multi_level_sensor_property[
+            element_uid
+        ]
+
+        super().__init__(
+            homecontrol=homecontrol,
+            device_instance=device_instance,
+            element_uid=element_uid,
+        )
+
+        self._device_class = DEVICE_CLASS_MAPPING.get(
+            self._multi_level_sensor_property.sensor_type
+        )
+
+        self._value = self._multi_level_sensor_property.value
+        self._unit = self._multi_level_sensor_property.unit
+
+        if self._device_class is None:
+            self._name += f" {self._multi_level_sensor_property.sensor_type}"
 
 
 class DevoloConsumptionEntity(DevoloMultiLevelDeviceEntity):
@@ -127,36 +109,36 @@ class DevoloConsumptionEntity(DevoloMultiLevelDeviceEntity):
 
     def __init__(self, homecontrol, device_instance, element_uid, consumption):
         """Initialize a devolo consumption sensor."""
-        self._device_instance = device_instance
-
-        self.value = getattr(
-            device_instance.consumption_property[element_uid], consumption
-        )
-        self.sensor_type = consumption
-        self.unit = getattr(
-            device_instance.consumption_property[element_uid], f"{consumption}_unit"
-        )
-        self.element_uid = element_uid
 
         super().__init__(
-            homecontrol,
-            device_instance,
-            element_uid,
-            multi_level_sensor_property=self,
-            sync=self._sync,
+            homecontrol=homecontrol,
+            device_instance=device_instance,
+            element_uid=element_uid,
         )
+
+        self._sensor_type = consumption
+        self._device_class = DEVICE_CLASS_MAPPING.get(consumption)
+
+        self._value = getattr(
+            device_instance.consumption_property[element_uid], consumption
+        )
+        self._unit = getattr(
+            device_instance.consumption_property[element_uid], f"{consumption}_unit"
+        )
+
+        self._name += f" {consumption}"
 
     @property
     def unique_id(self):
         """Return the unique ID of the entity."""
-        return f"{self._unique_id}_{self.sensor_type}"
+        return f"{self._unique_id}_{self._sensor_type}"
 
-    def _sync(self, message=None):
+    def _sync(self, message):
         """Update the consumption sensor state."""
-        if message[0] == self.element_uid:
-            self._state = getattr(
-                self._device_instance.consumption_property[self.element_uid],
-                self.sensor_type,
+        if message[0] == self._unique_id:
+            self._value = getattr(
+                self._device_instance.consumption_property[self._unique_id],
+                self._sensor_type,
             )
         elif message[0].startswith("hdm"):
             self._available = self._device_instance.is_online()
