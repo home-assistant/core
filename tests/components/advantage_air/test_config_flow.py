093dee5c9546fef3111a13f2536641c11eca2ead
Brett Adams <brett@ba.id.au>
2020-09-26 07:55:42 +0000

    Move api response to private function

--- tests/components/advantage_air/test_config_flow.py
+++ tests/components/advantage_air/test_config_flow.py
@@ -9,7 +9,7 @@ from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT
 API_DATA = '{"aircons":{"ac1":{"info":{"climateControlModeIsRunning":false,"countDownToOff":0,"countDownToOn":0,"fan":"high","filterCleanStatus":0,"freshAirStatus":"none","mode":"vent","myZone":0,"name":"AC","setTemp":24,"state":"off"},"zones":{"z01":{"error":0,"maxDamper":100,"measuredTemp":0,"minDamper":0,"motion":0,"motionConfig":1,"name":"Zone 1","number":1,"rssi":0,"setTemp":24,"state":"open","type":0,"value":100}}}},"system":{"hasAircons":true,"hasLights":false,"hasSensors":false,"hasThings":false,"hasThingsBOG":false,"hasThingsLight":false,"name":"testname","rid":"uniqueid","sysType":"e-zone","myAppRev":"testversion"}}'
 
 
-async def api_response(request):
+async def _api_response(request):
     """Advantage Air API response."""
     return web.Response(text=API_DATA)
 
@@ -26,7 +26,7 @@ async def test_form_success(hass, aiohttp_raw_server, aiohttp_unused_port):
     """Test that the setup can fully complete."""
 
     port = aiohttp_unused_port()
-    await aiohttp_raw_server(api_response, port=port)
+    await aiohttp_raw_server(_api_response, port=port)
 
     user_input = {
         CONF_IP_ADDRESS: "127.0.0.1",
