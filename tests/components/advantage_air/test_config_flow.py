7d06b54e508567d1a2dd1ba9e18a5efd4338e89e
Brett Adams <brett@ba.id.au>
2020-09-26 07:22:17 +0000

    Refixed tests

--- tests/components/advantage_air/test_config_flow.py
+++ tests/components/advantage_air/test_config_flow.py
@@ -1,13 +1,20 @@
 """Test the Advantage Air config flow."""
 
+from aiohttp import web
+
 from homeassistant import data_entry_flow
 from homeassistant.components.advantage_air import config_flow
 from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT
 
-API_DATA = b'{"aircons":{"ac1":{"info":{"climateControlModeIsRunning":false,"countDownToOff":0,"countDownToOn":0,"fan":"high","filterCleanStatus":0,"freshAirStatus":"none","mode":"vent","myZone":0,"name":"AC","setTemp":24,"state":"off"},"zones":{"z01":{"error":0,"maxDamper":100,"measuredTemp":0,"minDamper":0,"motion":0,"motionConfig":1,"name":"Zone 1","number":1,"rssi":0,"setTemp":24,"state":"open","type":0,"value":100}}}},"system":{"hasAircons":true,"hasLights":false,"hasSensors":false,"hasThings":false,"hasThingsBOG":false,"hasThingsLight":false,"name":"testname","rid":"uniqueid","sysType":"e-zone","tspModel":"tspnumbers"}}'
+API_DATA = '{"aircons":{"ac1":{"info":{"climateControlModeIsRunning":false,"countDownToOff":0,"countDownToOn":0,"fan":"high","filterCleanStatus":0,"freshAirStatus":"none","mode":"vent","myZone":0,"name":"AC","setTemp":24,"state":"off"},"zones":{"z01":{"error":0,"maxDamper":100,"measuredTemp":0,"minDamper":0,"motion":0,"motionConfig":1,"name":"Zone 1","number":1,"rssi":0,"setTemp":24,"state":"open","type":0,"value":100}}}},"system":{"hasAircons":true,"hasLights":false,"hasSensors":false,"hasThings":false,"hasThingsBOG":false,"hasThingsLight":false,"name":"testname","rid":"uniqueid","sysType":"e-zone","myAppRev":"testversion"}}'
 
 
-async def test_form(hass, httpserver):
+async def api_response(request):
+    """Advantage Air API response."""
+    return web.Response(text=API_DATA)
+
+
+async def test_form(hass):
     """Test that form shows up."""
     flow = config_flow.AdvantageAirConfigFlow()
     result = await flow.async_step_user(user_input=None)
@@ -15,13 +22,15 @@ async def test_form(hass, httpserver):
     assert result["step_id"] == "user"
 
 
-async def test_form_success(hass, httpserver):
+async def test_form_success(hass, aiohttp_raw_server, aiohttp_unused_port):
     """Test that the setup can fully complete."""
 
-    httpserver.serve_content(API_DATA)
+    port = aiohttp_unused_port()
+    await aiohttp_raw_server(api_response, port=port)
+
     user_input = {
         CONF_IP_ADDRESS: "127.0.0.1",
-        CONF_PORT: int(httpserver.url.split(":")[2]),
+        CONF_PORT: port,
     }
 
     flow = config_flow.AdvantageAirConfigFlow()
@@ -32,7 +41,7 @@ async def test_form_success(hass, httpserver):
     assert result["data"][CONF_PORT] == user_input[CONF_PORT]
 
 
-async def test_form_cannot_connect(hass, aioclient_mock):
+async def test_form_cannot_connect(hass):
     """Test we handle cannot connect error."""
 
     flow = config_flow.AdvantageAirConfigFlow()
