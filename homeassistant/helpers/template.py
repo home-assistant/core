0b2f207f874c6a1055605c87e79df90696e8f92b
J. Nick Koston <nick@koston.org>
2020-09-26 19:17:21 -0500

    merge

--- homeassistant/helpers/template.py
+++ homeassistant/helpers/template.py
@@ -14,7 +14,6 @@ from typing import Any, Generator, Iterable, List, Optional, Union
 from urllib.parse import urlencode as urllib_urlencode
 import weakref
 
-import async_timeout
 import jinja2
 from jinja2 import contextfilter, contextfunction
 from jinja2.sandbox import ImmutableSandboxedEnvironment
@@ -38,6 +37,7 @@ from homeassistant.helpers.typing import HomeAssistantType, TemplateVarsType
 from homeassistant.loader import bind_hass
 from homeassistant.util import convert, dt as dt_util, location as loc_util
 from homeassistant.util.async_ import run_callback_threadsafe
+from homeassistant.util.thread import ThreadWithException
 
 # mypy: allow-untyped-calls, allow-untyped-defs
 # mypy: no-check-untyped-defs, no-warn-return-any
@@ -337,13 +337,22 @@ class Template:
         if variables is not None:
             kwargs.update(variables)
 
+        finish_event = asyncio.Event()
+
+        def _render_template():
+            try:
+                compiled.render(kwargs)
+            except TimeoutError:
+                pass
+            run_callback_threadsafe(self.hass.loop, finish_event.set)
+
         try:
-            async with async_timeout.timeout(timeout):
-                await self.hass.async_add_executor_job(compiled.render, kwargs)
+            template_render_thread = ThreadWithException(target=_render_template)
+            template_render_thread.start()
+            await asyncio.wait_for(finish_event.wait(), timeout=timeout)
         except asyncio.TimeoutError:
+            template_render_thread.raise_exc(TimeoutError)
             return True
-        except jinja2.TemplateError:
-            pass
 
         return False
 
--- homeassistant/util/thread.py
+++ homeassistant/util/thread.py
@@ -1,4 +1,6 @@
 """Threading util helpers."""
+import ctypes
+import inspect
 import sys
 import threading
 from typing import Any
@@ -24,3 +26,33 @@ def fix_threading_exception_logging() -> None:
             sys.excepthook(*sys.exc_info())
 
     threading.Thread.run = run  # type: ignore
+
+
+def _async_raise(tid: int, exctype: Any) -> None:
+    """Raise an exception in the threads with id tid."""
+    if not inspect.isclass(exctype):
+        raise TypeError("Only types can be raised (not instances)")
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
+        ctypes.c_long(tid), ctypes.py_object(exctype)
+    )
+    if res == 0:
+        raise ValueError("Invalid thread id")
+    elif res != 1:
+        # "if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), None)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+
+
+class ThreadWithException(threading.Thread):
+    """A thread class that supports raising exception in the thread from another thread.
+
+    Based on
+    https://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread/49877671
+
+    """
+
+    def raise_exc(self, exctype: Any) -> None:
+        """Raise the given exception type in the context of this thread."""
+        assert self.ident
+        _async_raise(self.ident, exctype)
