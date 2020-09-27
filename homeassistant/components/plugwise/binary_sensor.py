dda777b2cfc0e81406d2ca28b2b4d466d78f0ba5
Tom Scholten <git@scholten.nu>
2020-09-27 17:40:37 +0200

    Revert guardings in line with -beta

--- homeassistant/components/plugwise/binary_sensor.py
+++ homeassistant/components/plugwise/binary_sensor.py
@@ -33,25 +33,23 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     all_devices = api.get_all_devices()
     for dev_id, device_properties in all_devices.items():
-        if device_properties["class"] not in ["heater_central", "gateway"]:
-            continue
+        if device_properties["class"] == "heater_central":
+            data = api.get_device_data(dev_id)
 
-        data = api.get_device_data(dev_id)
+            for binary_sensor, dummy in BINARY_SENSOR_MAP.items():
+                if binary_sensor not in data:
+                    continue
 
-        for binary_sensor, dummy in BINARY_SENSOR_MAP.items():
-            if binary_sensor not in data:
-                continue
-
-            entities.append(
-                PwBinarySensor(
-                    api,
-                    coordinator,
-                    device_properties["name"],
-                    dev_id,
-                    binary_sensor,
-                    device_properties["class"],
+                entities.append(
+                    PwBinarySensor(
+                        api,
+                        coordinator,
+                        device_properties["name"],
+                        dev_id,
+                        binary_sensor,
+                        device_properties["class"],
+                    )
                 )
-            )
 
     async_add_entities(entities, True)
 
