9c628e8c4fd6bb57e34b2dd2a5ebd4e49ae752a6
J. Nick Koston <nick@koston.org>
2020-09-26 23:41:38 -0500

    cover

--- tests/util/test_thread.py
+++ tests/util/test_thread.py
@@ -34,5 +34,22 @@ async def test_thread_not_started(hass):
         test_thread.raise_exc(TimeoutError)
 
 
+async def test_thread_fails_raise(hass):
+    """Test throwing after already ended."""
+
+    finish_event = asyncio.Event()
+
+    def _do_nothing(*_):
+        run_callback_threadsafe(hass.loop, finish_event.set)
+
+    test_thread = ThreadWithException(target=_do_nothing)
+    test_thread.start()
+    await asyncio.wait_for(finish_event.wait(), timeout=0.1)
+    test_thread.join()
+
+    with pytest.raises(SystemError):
+        test_thread.raise_exc(ValueError)
+
+
 class _EmptyClass:
     """An empty class."""
