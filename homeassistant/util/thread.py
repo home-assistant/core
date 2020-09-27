9b68bfb6d5c91aae142610990829ab4c762bba17
J. Nick Koston <nick@koston.org>
2020-09-26 21:15:47 -0500

    tweaks

--- homeassistant/util/thread.py
+++ homeassistant/util/thread.py
@@ -32,16 +32,20 @@ def _async_raise(tid: int, exctype: Any) -> None:
     """Raise an exception in the threads with id tid."""
     if not inspect.isclass(exctype):
         raise TypeError("Only types can be raised (not instances)")
+
     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
         ctypes.c_long(tid), ctypes.py_object(exctype)
     )
+
+    if res == 1:
+        return
     if res == 0:
         raise ValueError("Invalid thread id")
-    elif res != 1:
-        # "if it returns a number greater than one, you're in trouble,
-        # and you should call it again with exc=NULL to revert the effect"
-        ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), None)
-        raise SystemError("PyThreadState_SetAsyncExc failed")
+
+    # "if it returns a number greater than one, you're in trouble,
+    # and you should call it again with exc=NULL to revert the effect"
+    ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), None)
+    raise SystemError("PyThreadState_SetAsyncExc failed")
 
 
 class ThreadWithException(threading.Thread):
