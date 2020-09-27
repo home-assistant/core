c637e4df3a5ecdfb3484dd19bfddffd7f911de7d
Robert Svensson <Kane610@users.noreply.github.com>
2020-09-27 11:02:45 +0200

    deCONZ fix comments from #40265 (#40640)
    * Use set not list
    
    * Events are not entities
    
    * Don't await unload_events
    
    * Remove checks of entities content in tests
    
    * List to set comprehension
    
    * Why is it so hard to remember that sets arent parenthesis...

--- tests/components/deconz/test_binary_sensor.py
+++ tests/components/deconz/test_binary_sensor.py
@@ -67,7 +67,6 @@ async def test_no_binary_sensors(hass):
     """Test that no sensors in deconz results in no sensor entities."""
     gateway = await setup_deconz_integration(hass)
     assert len(gateway.deconz_ids) == 0
-    assert len(gateway.entities[binary_sensor.DOMAIN]) == 0
     assert len(hass.states.async_all()) == 0
 
 
@@ -81,7 +80,6 @@ async def test_binary_sensors(hass):
     assert "binary_sensor.clip_presence_sensor" not in gateway.deconz_ids
     assert "binary_sensor.vibration_sensor" in gateway.deconz_ids
     assert len(hass.states.async_all()) == 3
-    assert len(gateway.entities[binary_sensor.DOMAIN]) == 2
 
     presence_sensor = hass.states.get("binary_sensor.presence_sensor")
     assert presence_sensor.state == "off"
@@ -113,7 +111,6 @@ async def test_binary_sensors(hass):
     await gateway.async_reset()
 
     assert len(hass.states.async_all()) == 0
-    assert len(gateway.entities[binary_sensor.DOMAIN]) == 0
 
 
 async def test_allow_clip_sensor(hass):
@@ -130,7 +127,6 @@ async def test_allow_clip_sensor(hass):
     assert "binary_sensor.clip_presence_sensor" in gateway.deconz_ids
     assert "binary_sensor.vibration_sensor" in gateway.deconz_ids
     assert len(hass.states.async_all()) == 4
-    assert len(gateway.entities[binary_sensor.DOMAIN]) == 3
 
     presence_sensor = hass.states.get("binary_sensor.presence_sensor")
     assert presence_sensor.state == "off"
@@ -154,7 +150,6 @@ async def test_allow_clip_sensor(hass):
     assert "binary_sensor.clip_presence_sensor" not in gateway.deconz_ids
     assert "binary_sensor.vibration_sensor" in gateway.deconz_ids
     assert len(hass.states.async_all()) == 3
-    assert len(gateway.entities[binary_sensor.DOMAIN]) == 2
 
     hass.config_entries.async_update_entry(
         gateway.config_entry, options={deconz.gateway.CONF_ALLOW_CLIP_SENSOR: True}
@@ -166,14 +161,12 @@ async def test_allow_clip_sensor(hass):
     assert "binary_sensor.clip_presence_sensor" in gateway.deconz_ids
     assert "binary_sensor.vibration_sensor" in gateway.deconz_ids
     assert len(hass.states.async_all()) == 4
-    assert len(gateway.entities[binary_sensor.DOMAIN]) == 3
 
 
 async def test_add_new_binary_sensor(hass):
     """Test that adding a new binary sensor works."""
     gateway = await setup_deconz_integration(hass)
     assert len(gateway.deconz_ids) == 0
-    assert len(gateway.entities[binary_sensor.DOMAIN]) == 0
 
     state_added_event = {
         "t": "event",
@@ -189,4 +182,3 @@ async def test_add_new_binary_sensor(hass):
 
     presence_sensor = hass.states.get("binary_sensor.presence_sensor")
     assert presence_sensor.state == "off"
-    assert len(gateway.entities[binary_sensor.DOMAIN]) == 1
