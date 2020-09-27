cf085842fdde19fda6380099598d0b90262a5f51
Robert Svensson <kane610@me.com>
2020-09-27 11:36:43 +0200

    Add some inline comment to the service

--- homeassistant/components/deconz/services.py
+++ homeassistant/components/deconz/services.py
@@ -204,24 +204,32 @@ async def async_remove_orphaned_entries_service(hass, data):
         if gateway.config_entry.entry_id in entry.config_entries
     ]
 
+    # Don't remove the Gateway device
     if gateway.device_id in devices_to_be_removed:
         devices_to_be_removed.remove(gateway.device_id)
 
+    # Don't remove devices belonging to available events
     for event in gateway.events:
         if event.device_id in devices_to_be_removed:
             devices_to_be_removed.remove(event.device_id)
 
     for entry in entity_entries:
 
+        # Don't remove available entities
         if entry.unique_id in gateway.entities[entry.domain]:
+
+            # Don't remove devices with available entities
             if entry.device_id in devices_to_be_removed:
                 devices_to_be_removed.remove(entry.device_id)
             continue
+        # Remove entities that are not available
         entities_to_be_removed.append(entry.entity_id)
 
+    # Remove unavailable entities
     for entity_id in entities_to_be_removed:
         entity_registry.async_remove(entity_id)
 
+    # Remove devices that don't belong to any entity
     for device_id in devices_to_be_removed:
         if len(async_entries_for_device(entity_registry, device_id)) == 0:
             device_registry.async_remove_device(device_id)
