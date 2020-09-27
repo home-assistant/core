a500f76de9f72c9b8acaad2e5cc2f86aa403a519
joogps <joogps@gmail.com>
2020-09-27 02:44:45 +0000

    Fixing email address

--- homeassistant/components/panasonic_viera/config_flow.py
+++ homeassistant/components/panasonic_viera/config_flow.py
@@ -10,6 +10,9 @@ from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PIN, CONF_PORT
 
 from .const import (  # pylint: disable=unused-import
+    ATTR_DEVICE_INFO,
+    ATTR_FRIENDLY_NAME,
+    ATTR_UDN,
     CONF_APP_ID,
     CONF_ENCRYPTION_KEY,
     CONF_ON_ACTION,
@@ -38,6 +41,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             CONF_NAME: None,
             CONF_PORT: None,
             CONF_ON_ACTION: None,
+            ATTR_DEVICE_INFO: None,
         }
 
         self._remote = None
@@ -52,6 +56,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 self._remote = await self.hass.async_add_executor_job(
                     partial(RemoteControl, self._data[CONF_HOST], self._data[CONF_PORT])
                 )
+
+                self._data[ATTR_DEVICE_INFO] = await self.hass.async_add_executor_job(
+                    self._remote.get_device_info
+                )
             except (TimeoutError, URLError, SOAPError, OSError) as err:
                 _LOGGER.error("Could not establish remote connection: %s", err)
                 errors["base"] = ERROR_NOT_CONNECTED
@@ -60,6 +68,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 return self.async_abort(reason=REASON_UNKNOWN)
 
             if "base" not in errors:
+                print(self._data)
+                await self.async_set_unique_id(self._data[ATTR_DEVICE_INFO][ATTR_UDN])
+                self._abort_if_unique_id_configured()
+
+                if self._data[CONF_NAME] == DEFAULT_NAME:
+                    self._data[CONF_NAME] = self._data[ATTR_DEVICE_INFO][
+                        ATTR_FRIENDLY_NAME
+                    ].replace("_", " ")
+
                 if self._remote.type == TV_TYPE_ENCRYPTED:
                     return await self.async_step_pairing()
 
