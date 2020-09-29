b9d0b09b2cdeb2d6652954a512c23b50d9223980
Dermot Duffy <dermot.duffy@gmail.com>
2020-09-29 07:58:11 -0700

    Variable tidy.

--- homeassistant/components/hyperion/const.py
+++ homeassistant/components/hyperion/const.py
@@ -1,10 +1,10 @@
 """Constants for Hyperion integration."""
 DOMAIN = "hyperion"
-CONF_INSTANCE = "instance"
-CONF_CREATE_TOKEN = "create_token"
+
 DEFAULT_NAME = "Hyperion"
 DEFAULT_ORIGIN = "Home Assistant"
 
-# TODO: Need to be here vs config_flow?
-CONF_HYPERION_URL = "hyperion_url"
 CONF_AUTH_ID = "auth_id"
+CONF_CREATE_TOKEN = "create_token"
+CONF_HYPERION_URL = "hyperion_url"
+CONF_INSTANCE = "instance"
