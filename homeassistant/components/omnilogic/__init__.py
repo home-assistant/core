3e04502a53ac623085f3c2710a1074cc7a8d23d8
Tim Empringham <tim.empringham@live.ca>
2020-09-28 20:46:53 -0400

    Fixing black and flake issues after merge for PR.

--- homeassistant/components/omnilogic/__init__.py
+++ homeassistant/components/omnilogic/__init__.py
@@ -17,8 +17,10 @@ _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS = ["sensor", "binary_sensor"]
 
+
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Omnilogic component."""
+
     hass.data.setdefault(DOMAIN, {})
 
     return True
