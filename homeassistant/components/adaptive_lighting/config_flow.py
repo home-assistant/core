4cdf404b3719aa11e2348f65f1fd047c556d8edc
Bas Nijholt <bas@nijho.lt>
2020-09-29 13:09:32 +0200

    do not use old settings when reloading config

--- homeassistant/components/adaptive_lighting/config_flow.py
+++ homeassistant/components/adaptive_lighting/config_flow.py
@@ -45,9 +45,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         await self.async_set_unique_id(user_input["name"])
         for entry in self._async_current_entries():
             if entry.unique_id == self.unique_id:
-                self.hass.config_entries.async_update_entry(
-                    entry, data=dict(entry.data, **user_input)
-                )
+                self.hass.config_entries.async_update_entry(entry, data=user_input)
                 self._abort_if_unique_id_configured()
         return self.async_create_entry(title=user_input["name"], data=user_input)
 
