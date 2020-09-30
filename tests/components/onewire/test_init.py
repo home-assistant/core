f082c1b1ed964234adc4a0dda6ad0c2c91675038
epenet <6771947+epenet@users.noreply.github.com>
2020-09-30 20:18:59 +0200

    Improve test coverage

---
+++ tests/components/onewire/test_init.py
@@ -0,0 +1,36 @@
+"""Tests for 1-Wire config flow."""
+from homeassistant.components import onewire
+
+from homeassistant import config_entries
+from homeassistant.components.onewire.const import (
+    CONF_TYPE_SYSBUS,
+)
+
+from tests.common import MockConfigEntry
+
+
+async def setup_onewire_integration(hass):
+    """Create the 1-Wire integration."""
+    config_entry = MockConfigEntry(
+        domain=onewire.DOMAIN,
+        source="user",
+        data={
+            onewire.config_flow.CONF_TYPE: CONF_TYPE_SYSBUS,
+        },
+        connection_class=config_entries.CONN_CLASS_LOCAL_POLL,
+        options={},
+        entry_id="1",
+    )
+    config_entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    return config_entry
+
+
+async def test_unload_entry(hass):
+    """Test being able to unload an entry."""
+    config_entry = await setup_onewire_integration(hass)
+
+    assert await onewire.async_unload_entry(hass, config_entry)
