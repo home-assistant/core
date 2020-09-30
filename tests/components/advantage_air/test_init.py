4440cb285936476dc702c5ad2c7fed26f1fee15b
Brett Adams <brett@ba.id.au>
2020-09-30 09:21:06 +0000

    Combing reused test code

--- tests/components/advantage_air/test_init.py
+++ tests/components/advantage_air/test_init.py
@@ -9,28 +9,18 @@ from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from tests.common import MockConfigEntry
-from tests.components.advantage_air import api_response_without_sensor
+from tests.components.advantage_air import add_mock_config, api_response_without_sensor
 
 
 async def test_async_setup_entry(hass, aiohttp_raw_server, aiohttp_unused_port):
     """Test a successful setup entry."""
 
-    assert await async_setup(hass, {})
-    assert hass.data[DOMAIN] == {}
-
     port = aiohttp_unused_port()
     await aiohttp_raw_server(api_response_without_sensor, port=port)
 
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        title="test entry",
-        unique_id="0123456",
-        data={
-            CONF_IP_ADDRESS: "127.0.0.1",
-            CONF_PORT: port,
-        },
-    )
-    assert await async_setup_entry(hass, entry)
+    assert await async_setup(hass, {})
+    assert hass.data[DOMAIN] == {}
+    entry = await add_mock_config(hass, port)
 
     assert hass.data[DOMAIN][entry.entry_id]
     assert isinstance(
@@ -40,14 +30,10 @@ async def test_async_setup_entry(hass, aiohttp_raw_server, aiohttp_unused_port):
     assert isinstance(hass.data[DOMAIN][entry.entry_id]["device"], dict)
 
 
-async def test_async_setup_entry_failure(hass, aiohttp_raw_server, aiohttp_unused_port):
+async def test_async_setup_entry_failure(hass, aiohttp_unused_port):
     """Test a successful setup entry."""
 
-    assert await async_setup(hass, {})
-    assert hass.data[DOMAIN] == {}
-
     port = aiohttp_unused_port()
-
     entry = MockConfigEntry(
         domain=DOMAIN,
         title="test entry",
@@ -57,5 +43,7 @@ async def test_async_setup_entry_failure(hass, aiohttp_raw_server, aiohttp_unuse
             CONF_PORT: port,
         },
     )
+
+    await async_setup(hass, {})
     with pytest.raises(ConfigEntryNotReady):
         await async_setup_entry(hass, entry)
