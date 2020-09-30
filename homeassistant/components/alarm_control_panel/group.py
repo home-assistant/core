29c02f790e390753996fe687d2c544003a49f7c9
J. Nick Koston <nick@koston.org>
2020-09-30 08:36:55 -0500

    List to Set

--- homeassistant/components/alarm_control_panel/group.py
+++ homeassistant/components/alarm_control_panel/group.py
@@ -20,12 +20,12 @@ def async_describe_on_off_states(
 ) -> None:
     """Describe group on off states."""
     registry.on_off_states(
-        [
+        {
             STATE_ALARM_ARMED_AWAY,
             STATE_ALARM_ARMED_CUSTOM_BYPASS,
             STATE_ALARM_ARMED_HOME,
             STATE_ALARM_ARMED_NIGHT,
             STATE_ALARM_TRIGGERED,
-        ],
+        },
         STATE_OFF,
     )
