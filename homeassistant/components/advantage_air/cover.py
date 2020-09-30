2be7326d4c7ae3fea5ad6c85fc5b65d996f409be
Brett Adams <brett@ba.id.au>
2020-09-30 14:27:26 +0000

    Cover plaform improvements and tests

--- homeassistant/components/advantage_air/cover.py
+++ homeassistant/components/advantage_air/cover.py
@@ -8,7 +8,7 @@ from homeassistant.components.cover import (
     SUPPORT_SET_POSITION,
     CoverEntity,
 )
-from homeassistant.const import CONF_IP_ADDRESS, STATE_OPEN
+from homeassistant.const import STATE_OPEN
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN, STATE_CLOSE
@@ -16,27 +16,26 @@ from .const import DOMAIN, STATE_CLOSE
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Platform setup isn't required."""
-    return True
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up AdvantageAir cover platform."""
 
-    instance = hass.data[DOMAIN][config_entry.data[CONF_IP_ADDRESS]]
+    instance = hass.data[DOMAIN][config_entry.entry_id]
 
     entities = []
-    for _, ac_index in enumerate(instance["coordinator"].data["aircons"]):
-        for _, zone_index in enumerate(
-            instance["coordinator"].data["aircons"][ac_index]["zones"]
+    for _, ac_key in enumerate(instance["coordinator"].data["aircons"]):
+        for _, zone_key in enumerate(
+            instance["coordinator"].data["aircons"][ac_key]["zones"]
         ):
             # Only add zone vent controls when zone in vent control mode.
             if (
-                instance["coordinator"].data["aircons"][ac_index]["zones"][zone_index][
+                instance["coordinator"].data["aircons"][ac_key]["zones"][zone_key][
                     "type"
                 ]
                 == 0
             ):
-                entities.append(AdvantageAirZoneVent(instance, ac_index, zone_index))
+                entities.append(AdvantageAirZoneVent(instance, ac_key, zone_key))
     async_add_entities(entities)
     return True
 
@@ -44,23 +43,23 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 class AdvantageAirZoneVent(CoordinatorEntity, CoverEntity):
     """Advantage Air Climate Class."""
 
-    def __init__(self, instance, ac_index, zone_index):
+    def __init__(self, instance, ac_key, zone_key):
         """Initialize the Advantage Air Zone Vent cover entity."""
         super().__init__(instance["coordinator"])
         self.async_change = instance["async_change"]
         self.device = instance["device"]
-        self.ac_index = ac_index
-        self.zone_index = zone_index
+        self.ac_key = ac_key
+        self.zone_key = zone_key
 
     @property
     def name(self):
         """Return the name."""
-        return f'{self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index]["name"]} Vent'
+        return f'{self.coordinator.data["aircons"][self.ac_key]["zones"][self.zone_key]["name"]} Vent'
 
     @property
     def unique_id(self):
         """Return a unique id."""
-        return f'{self.coordinator.data["system"]["rid"]}-{self.ac_index}-{self.zone_index}-cover'
+        return f'{self.coordinator.data["system"]["rid"]}-{self.ac_key}-{self.zone_key}'
 
     @property
     def device_class(self):
@@ -76,7 +75,7 @@ class AdvantageAirZoneVent(CoordinatorEntity, CoverEntity):
     def is_closed(self):
         """Return if vent is fully closed."""
         return (
-            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
+            self.coordinator.data["aircons"][self.ac_key]["zones"][self.zone_key][
                 "state"
             ]
             == STATE_CLOSE
@@ -96,13 +95,13 @@ class AdvantageAirZoneVent(CoordinatorEntity, CoverEntity):
     def current_cover_position(self):
         """Return vents current position as a percentage."""
         if (
-            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
+            self.coordinator.data["aircons"][self.ac_key]["zones"][self.zone_key][
                 "state"
             ]
             == STATE_OPEN
         ):
-            return self.coordinator.data["aircons"][self.ac_index]["zones"][
-                self.zone_index
+            return self.coordinator.data["aircons"][self.ac_key]["zones"][
+                self.zone_key
             ]["value"]
         return 0
 
@@ -110,7 +109,7 @@ class AdvantageAirZoneVent(CoordinatorEntity, CoverEntity):
     def icon(self):
         """Return vent icon."""
         return ["mdi:fan-off", "mdi:fan"][
-            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
+            self.coordinator.data["aircons"][self.ac_key]["zones"][self.zone_key][
                 "state"
             ]
             == STATE_OPEN
@@ -125,8 +124,8 @@ class AdvantageAirZoneVent(CoordinatorEntity, CoverEntity):
         """Fully open zone vent."""
         await self.async_change(
             {
-                self.ac_index: {
-                    "zones": {self.zone_index: {"state": STATE_OPEN, "value": 100}}
+                self.ac_key: {
+                    "zones": {self.zone_key: {"state": STATE_OPEN, "value": 100}}
                 }
             }
         )
@@ -134,7 +133,7 @@ class AdvantageAirZoneVent(CoordinatorEntity, CoverEntity):
     async def async_close_cover(self, **kwargs):
         """Fully close zone vent."""
         await self.async_change(
-            {self.ac_index: {"zones": {self.zone_index: {"state": STATE_CLOSE}}}}
+            {self.ac_key: {"zones": {self.zone_key: {"state": STATE_CLOSE}}}}
         )
 
     async def async_set_cover_position(self, **kwargs):
@@ -142,14 +141,14 @@ class AdvantageAirZoneVent(CoordinatorEntity, CoverEntity):
         position = round(kwargs.get(ATTR_POSITION) / 5) * 5
         if position == 0:
             await self.async_change(
-                {self.ac_index: {"zones": {self.zone_index: {"state": STATE_CLOSE}}}}
+                {self.ac_key: {"zones": {self.zone_key: {"state": STATE_CLOSE}}}}
             )
         else:
             await self.async_change(
                 {
-                    self.ac_index: {
+                    self.ac_key: {
                         "zones": {
-                            self.zone_index: {
+                            self.zone_key: {
                                 "state": STATE_OPEN,
                                 "value": position,
                             }
---
+++ tests/components/advantage_air/test_cover.py
@@ -0,0 +1,53 @@
+"""Test the Advantage Air Cover Platform."""
+
+from homeassistant.components.cover import (
+    ATTR_POSITION,
+    DEVICE_CLASS_DAMPER,
+    DOMAIN as COVER_DOMAIN,
+    SERVICE_CLOSE_COVER,
+    SERVICE_OPEN_COVER,
+    SERVICE_SET_COVER_POSITION,
+)
+from homeassistant.const import ATTR_ENTITY_ID
+
+from tests.components.advantage_air import add_mock_config, api_response_without_sensor
+
+
+async def test_cover_async_setup_entry(hass, aiohttp_raw_server, aiohttp_unused_port):
+    """Test climate setup without sensors."""
+
+    port = aiohttp_unused_port()
+    await aiohttp_raw_server(api_response_without_sensor, port=port)
+    await add_mock_config(hass, port)
+
+    registry = await hass.helpers.entity_registry.async_get_registry()
+
+    # Test Cover Zone Entity
+    entity_id = "cover.testzone_vent"
+    state = hass.states.get(entity_id)
+    assert state
+    assert state.attributes.get("device_class") == DEVICE_CLASS_DAMPER
+    assert state.attributes.get("current_position") == 100
+
+    entry = registry.async_get(entity_id)
+    assert entry
+    assert entry.unique_id == "uniqueid-ac1-z01"
+
+    await hass.services.async_call(
+        COVER_DOMAIN,
+        SERVICE_CLOSE_COVER,
+        {ATTR_ENTITY_ID: [entity_id]},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        COVER_DOMAIN,
+        SERVICE_OPEN_COVER,
+        {ATTR_ENTITY_ID: [entity_id]},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        COVER_DOMAIN,
+        SERVICE_SET_COVER_POSITION,
+        {ATTR_ENTITY_ID: [entity_id], ATTR_POSITION: 50},
+        blocking=True,
+    )
