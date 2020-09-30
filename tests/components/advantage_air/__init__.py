cc11865df158eedb40150438dafb6cabf8b9fb94
Brett Adams <brett@ba.id.au>
2020-09-30 03:49:30 +0000

    Testing init

--- tests/components/advantage_air/__init__.py
+++ tests/components/advantage_air/__init__.py
@@ -1,1 +1,18 @@
 """Tests for the Advantage Air component."""
+
+from aiohttp import web
+
+from tests.common import load_fixture
+
+payload_without_sensor = load_fixture("advantage_air/payload_without_sensor.json")
+payload_with_sensor = load_fixture("advantage_air/payload_with_sensor.json")
+
+
+async def api_response_without_sensor(request):
+    """Advantage Air API response."""
+    return web.Response(body=payload_without_sensor)
+
+
+async def api_response_with_sensor(request):
+    """Advantage Air API response."""
+    return web.Response(body=payload_with_sensor)
