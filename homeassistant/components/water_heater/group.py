04cc5fb49254d53b3b377a5546b1f7aff6fd8ff6
J. Nick Koston <nick@koston.org>
2020-09-30 07:59:19 -0500

    Update homeassistant/components/water_heater/group.py
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/water_heater/group.py
+++ homeassistant/components/water_heater/group.py
@@ -22,13 +22,13 @@ def async_describe_on_off_states(
 ) -> None:
     """Describe group on off states."""
     registry.on_off_states(
-        [
+        {
             STATE_ECO,
             STATE_ELECTRIC,
             STATE_PERFORMANCE,
             STATE_HIGH_DEMAND,
             STATE_HEAT_PUMP,
             STATE_GAS,
-        ],
+        },
         STATE_OFF,
     )
