f85151f710c097f3e0dace396a788c0da394a387
docker <chris@talkingtontech.com>
2020-09-30 21:40:42 -0500

    work on updater

--- homeassistant/components/canary/__init__.py
+++ homeassistant/components/canary/__init__.py
@@ -13,7 +13,6 @@ from homeassistant.const import CONF_PASSWORD, CONF_TIMEOUT, CONF_USERNAME
 from homeassistant.exceptions import ConfigEntryNotReady
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.typing import HomeAssistantType
-from homeassistant.util import Throttle
 
 from .const import (
     CONF_FFMPEG_ARGUMENTS,
@@ -90,14 +89,12 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
         hass.config_entries.async_update_entry(entry, options=options)
 
     try:
-        canary_api = await hass.async_add_executor_job(
-            _get_canary_api_instance, entry
-        )
+        canary_api = await hass.async_add_executor_job(_get_canary_api_instance, entry)
     except (ConnectTimeout, HTTPError) as error:
         _LOGGER.error("Unable to connect to Canary service: %s", str(error))
         raise ConfigEntryNotReady from error
 
-    coordinator = CanaryDataUpdateCoordinator(api=canary_api)
+    coordinator = CanaryDataUpdateCoordinator(hass, api=canary_api)
     await coordinator.async_refresh()
 
     if not coordinator.last_update_success:
@@ -141,69 +138,7 @@ async def _async_update_listener(hass: HomeAssistantType, entry: ConfigEntry) ->
     await hass.config_entries.async_reload(entry.entry_id)
 
 
-class CanaryData:
-    """Manages the data retrieved from Canary API."""
-
-    def __init__(self, api: Api):
-        """Init the Canary data object."""
-        self._api = api
-        self._locations_by_id = {}
-        self._readings_by_device_id = {}
-
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self, **kwargs):
-        """Get the latest data from py-canary with a throttle."""
-        self._update(**kwargs)
-
-    def _update(self, **kwargs):
-        """Get the latest data from py-canary."""
-        for location in self._api.get_locations():
-            location_id = location.location_id
-
-            self._locations_by_id[location_id] = location
-
-            for device in location.devices:
-                if device.is_online:
-                    self._readings_by_device_id[
-                        device.device_id
-                    ] = self._api.get_latest_readings(device.device_id)
-
-    @property
-    def locations(self):
-        """Return a list of locations."""
-        return self._locations_by_id.values()
-
-    def get_location(self, location_id):
-        """Return a location based on location_id."""
-        return self._locations_by_id.get(location_id, [])
-
-    def get_readings(self, device_id):
-        """Return a list of readings based on device_id."""
-        return self._readings_by_device_id.get(device_id, [])
-
-    def get_reading(self, device_id, sensor_type):
-        """Return reading for device_id and sensor type."""
-        readings = self._readings_by_device_id.get(device_id, [])
-        return next(
-            (
-                reading.value
-                for reading in readings
-                if reading.sensor_type == sensor_type
-            ),
-            None,
-        )
-
-    def set_location_mode(self, location_id, mode_name, is_private=False):
-        """Set location mode."""
-        self._api.set_location_mode(location_id, mode_name, is_private)
-        self.update(no_throttle=True)
-
-    def get_live_stream_session(self, device):
-        """Return live stream session."""
-        return self._api.get_live_stream_session(device)
-
-
-def _get_canary_api_instance(entry: ConfigEntry) -> CanaryData:
+def _get_canary_api_instance(entry: ConfigEntry) -> Api:
     """Initialize a new instance of CanaryApi."""
     canary = Api(
         entry.data[CONF_USERNAME],
