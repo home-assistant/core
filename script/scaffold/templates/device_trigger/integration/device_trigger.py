e6b23ddc43b53810b928543b9e463683a3175f52
Daniel Hjelseth Høyer <mail@dahoiv.net>
2020-10-01 19:46:59 +0200

    import order

--- script/scaffold/templates/device_trigger/integration/device_trigger.py
+++ script/scaffold/templates/device_trigger/integration/device_trigger.py
@@ -4,8 +4,8 @@ from typing import List
 import voluptuous as vol
 
 from homeassistant.components.automation import AutomationActionType
-from homeassistant.components.homeassistant.triggers import state
 from homeassistant.components.device_automation import TRIGGER_BASE_SCHEMA
+from homeassistant.components.homeassistant.triggers import state
 from homeassistant.const import (
     CONF_DEVICE_ID,
     CONF_DOMAIN,
