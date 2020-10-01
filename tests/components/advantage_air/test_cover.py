edf6efd9203000af781fff34f712b74f950e8445
Brett Adams <brett@ba.id.au>
2020-10-01 08:41:29 +0000

    100% test code coverage of cover platform

--- tests/components/advantage_air/test_cover.py
+++ tests/components/advantage_air/test_cover.py
@@ -10,20 +10,20 @@ from homeassistant.components.cover import (
 )
 from homeassistant.const import ATTR_ENTITY_ID
 
-from tests.components.advantage_air import add_mock_config, api_response_without_sensor
+from tests.components.advantage_air import add_mock_config, api_response
 
 
 async def test_cover_async_setup_entry(hass, aiohttp_raw_server, aiohttp_unused_port):
     """Test climate setup without sensors."""
 
     port = aiohttp_unused_port()
-    await aiohttp_raw_server(api_response_without_sensor, port=port)
+    await aiohttp_raw_server(api_response, port=port)
     await add_mock_config(hass, port)
 
     registry = await hass.helpers.entity_registry.async_get_registry()
 
     # Test Cover Zone Entity
-    entity_id = "cover.testzone_vent"
+    entity_id = "cover.zone_open_without_sensor_vent"
     state = hass.states.get(entity_id)
     assert state
     assert state.attributes.get("device_class") == DEVICE_CLASS_DAMPER
@@ -31,7 +31,7 @@ async def test_cover_async_setup_entry(hass, aiohttp_raw_server, aiohttp_unused_
 
     entry = registry.async_get(entity_id)
     assert entry
-    assert entry.unique_id == "uniqueid-ac1-z01"
+    assert entry.unique_id == "uniqueid-ac2-z01"
 
     await hass.services.async_call(
         COVER_DOMAIN,
@@ -49,5 +49,11 @@ async def test_cover_async_setup_entry(hass, aiohttp_raw_server, aiohttp_unused_
         COVER_DOMAIN,
         SERVICE_SET_COVER_POSITION,
         {ATTR_ENTITY_ID: [entity_id], ATTR_POSITION: 50},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        COVER_DOMAIN,
+        SERVICE_SET_COVER_POSITION,
+        {ATTR_ENTITY_ID: [entity_id], ATTR_POSITION: 0},
         blocking=True,
     )
