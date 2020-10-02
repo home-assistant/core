46eae017fcbf38756202efa449f498232cd948aa
Melvin Blokhuijzen <melvin@blokhuijzen.nl>
2020-10-02 19:18:00 +0200

    Change invalid_credentials to invalid_auth key

--- homeassistant/components/neato/config_flow.py
+++ homeassistant/components/neato/config_flow.py
@@ -105,7 +105,7 @@ class NeatoConfigFlow(config_entries.ConfigFlow, domain=NEATO_DOMAIN):
         try:
             Account(username, password, this_vendor)
         except NeatoLoginException:
-            return "invalid_credentials"
+            return "invalid_auth"
         except NeatoRobotException:
             return "unknown"
 
