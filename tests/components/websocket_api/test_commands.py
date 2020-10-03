3cea2cf9b6637e2596e67d27a5d6aa7d77ce1bb2
J. Nick Koston <nick@koston.org>
2020-10-03 14:29:02 -0500

    websocketapi

--- tests/components/websocket_api/test_commands.py
+++ tests/components/websocket_api/test_commands.py
@@ -420,7 +420,12 @@ async def test_render_template_renders_template(hass, websocket_client):
     event = msg["event"]
     assert event == {
         "result": "State is: on",
-        "listeners": {"all": False, "domains": [], "entities": ["light.test"]},
+        "listeners": {
+            "all": False,
+            "domains": [],
+            "entities": ["light.test"],
+            "time_patterns": [],
+        },
     }
 
     hass.states.async_set("light.test", "off")
@@ -430,7 +435,12 @@ async def test_render_template_renders_template(hass, websocket_client):
     event = msg["event"]
     assert event == {
         "result": "State is: off",
-        "listeners": {"all": False, "domains": [], "entities": ["light.test"]},
+        "listeners": {
+            "all": False,
+            "domains": [],
+            "entities": ["light.test"],
+            "time_patterns": [],
+        },
     }
 
 
@@ -459,7 +469,12 @@ async def test_render_template_manual_entity_ids_no_longer_needed(
     event = msg["event"]
     assert event == {
         "result": "State is: on",
-        "listeners": {"all": False, "domains": [], "entities": ["light.test"]},
+        "listeners": {
+            "all": False,
+            "domains": [],
+            "entities": ["light.test"],
+            "time_patterns": [],
+        },
     }
 
     hass.states.async_set("light.test", "off")
@@ -469,7 +484,12 @@ async def test_render_template_manual_entity_ids_no_longer_needed(
     event = msg["event"]
     assert event == {
         "result": "State is: off",
-        "listeners": {"all": False, "domains": [], "entities": ["light.test"]},
+        "listeners": {
+            "all": False,
+            "domains": [],
+            "entities": ["light.test"],
+            "time_patterns": [],
+        },
     }
 
 
