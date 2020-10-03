20bacb69189a44e92863f9b2d01660dd908877c3
groot406 <a.de.groot@connectingthedots.nl>
2020-10-03 15:02:30 +0200

    Use reference strings for Zoneminder config flow (#41103)

--- tests/components/zoneminder/test_config_flow.py
+++ tests/components/zoneminder/test_config_flow.py
@@ -45,7 +45,7 @@ async def test_import(hass: HomeAssistant) -> None:
         )
         assert result
         assert result["type"] == "abort"
-        assert result["reason"] == "auth_fail"
+        assert result["reason"] == "invalid_auth"
 
         zm_client.login.return_value = True
         result = await hass.config_entries.flow.async_init(
