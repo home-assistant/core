d660ccc09906847dd50204a00e7b12f6ca0ad17d
epenet <6771947+epenet@users.noreply.github.com>
2020-10-04 11:33:19 +0200

    Remove OWFS from config_flow

--- homeassistant/components/onewire/config_flow.py
+++ homeassistant/components/onewire/config_flow.py
@@ -10,10 +10,8 @@ from homeassistant.const import CONF_HOST, CONF_PORT, CONF_TYPE
 
 from .const import (  # pylint: disable=unused-import
     CONF_MOUNT_DIR,
-    CONF_TYPE_OWFS,
     CONF_TYPE_OWSERVER,
     CONF_TYPE_SYSBUS,
-    DEFAULT_OWFS_MOUNT_DIR,
     DEFAULT_OWSERVER_HOST,
     DEFAULT_OWSERVER_PORT,
     DEFAULT_SYSBUS_MOUNT_DIR,
@@ -41,14 +39,16 @@ class OneWireFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
         if user_input is not None:
             self.onewire_config.update(user_input)
-            if CONF_TYPE_OWFS == user_input[CONF_TYPE]:
-                return await self.async_step_mount_dir()
             if CONF_TYPE_OWSERVER == user_input[CONF_TYPE]:
                 return await self.async_step_owserver()
             if CONF_TYPE_SYSBUS == user_input[CONF_TYPE]:
-                return await self.async_step_mount_dir()
+                if os.path.isdir(DEFAULT_SYSBUS_MOUNT_DIR):
+                    return self.async_create_entry(
+                        title=DEFAULT_SYSBUS_MOUNT_DIR, data=self.onewire_config
+                    )
+                errors["base"] = "invalid_path"
 
-        proxy_types = [CONF_TYPE_OWSERVER, CONF_TYPE_OWFS, CONF_TYPE_SYSBUS]
+        proxy_types = [CONF_TYPE_OWSERVER, CONF_TYPE_SYSBUS]
 
         return self.async_show_form(
             step_id="user",
@@ -78,33 +78,6 @@ class OneWireFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 {
                     vol.Required(CONF_HOST, default=DEFAULT_OWSERVER_HOST): str,
                     vol.Required(CONF_PORT, default=DEFAULT_OWSERVER_PORT): int,
-                }
-            ),
-            errors=errors,
-        )
-
-    async def async_step_mount_dir(self, user_input=None):
-        """Handle OWFS / SysBus configuration."""
-        errors = {}
-        if user_input:
-            self.onewire_config.update(user_input)
-            mount_dir = user_input.get(CONF_MOUNT_DIR)
-            if os.path.isdir(mount_dir):
-                title = mount_dir
-                if self.onewire_config[CONF_TYPE] == CONF_TYPE_SYSBUS:
-                    title = CONF_TYPE_SYSBUS
-                return self.async_create_entry(title=title, data=self.onewire_config)
-            errors["base"] = "invalid_path"
-
-        default_mount_dir = DEFAULT_OWFS_MOUNT_DIR
-        if self.onewire_config[CONF_TYPE] == CONF_TYPE_SYSBUS:
-            default_mount_dir = DEFAULT_SYSBUS_MOUNT_DIR
-
-        return self.async_show_form(
-            step_id="mount_dir",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_MOUNT_DIR, default=default_mount_dir): str,
                 }
             ),
             errors=errors,
--- tests/components/onewire/test_config_flow.py
+++ tests/components/onewire/test_config_flow.py
@@ -3,7 +3,6 @@ from pyownet import protocol
 
 from homeassistant.components.onewire.const import (
     CONF_MOUNT_DIR,
-    CONF_TYPE_OWFS,
     CONF_TYPE_OWSERVER,
     CONF_TYPE_SYSBUS,
     DOMAIN,
@@ -63,53 +62,6 @@ async def test_config_flow_owserver(hass):
             CONF_HOST: "1.2.3.4",
             CONF_PORT: 1234,
         }
-
-
-async def test_config_flow_owfs(hass):
-    """Test OWFS flow."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["errors"] == {}
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        user_input={CONF_TYPE: CONF_TYPE_OWFS},
-    )
-
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "mount_dir"
-    assert result["errors"] == {}
-
-    # Invalid path
-    with patch(
-        "homeassistant.components.onewire.config_flow.os.path.isdir", return_value=False
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            user_input={CONF_MOUNT_DIR: "/mount/invalid_directory"},
-        )
-
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "mount_dir"
-    assert result["errors"] == {"base": "invalid_path"}
-
-    # Valid path
-    with patch(
-        "homeassistant.components.onewire.config_flow.os.path.isdir", return_value=True
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            user_input={CONF_MOUNT_DIR: "/mount/directory"},
-        )
-
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == "/mount/directory"
-    assert result["data"] == {
-        CONF_TYPE: CONF_TYPE_OWFS,
-        CONF_MOUNT_DIR: "/mount/directory",
-    }
 
 
 async def test_config_flow_sysbus(hass):
