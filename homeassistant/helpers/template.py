9191c06a8b824f277cf29df2840a10086193d6d5
Franck Nijhof <git@frenck.dev>
2020-10-04 22:18:52 +0200

    Update homeassistant/helpers/template.py
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- homeassistant/helpers/template.py
+++ homeassistant/helpers/template.py
@@ -337,7 +337,7 @@ class Template:
         try:
             result = literal_eval(render_result)
         except (ValueError, SyntaxError, MemoryError):
-            return render_result.strip()
+            pass
 
         # If the literal_eval result is a string, use the original render
         if isinstance(result, str):
