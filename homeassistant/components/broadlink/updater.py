bcadebf1c896a6d37ec004da5fcc04edcff4a4b5
Felipe Martins Diel <felipe-diel@hotmail.com>
2020-10-05 19:15:23 -0300

    Set an appropriate SCAN_INTERVAL for Broadlink A1 sensor

--- homeassistant/components/broadlink/updater.py
+++ homeassistant/components/broadlink/updater.py
@@ -42,6 +42,8 @@ class BroadlinkUpdateManager(ABC):
     monitor device availability.
     """
 
+    SCAN_INTERVAL = timedelta(minutes=1)
+
     def __init__(self, device):
         """Initialize the update manager."""
         self.device = device
@@ -50,7 +52,7 @@ class BroadlinkUpdateManager(ABC):
             _LOGGER,
             name="device",
             update_method=self.async_update,
-            update_interval=timedelta(minutes=1),
+            update_interval=self.SCAN_INTERVAL,
         )
         self.available = None
         self.last_update = None
@@ -62,7 +64,7 @@ class BroadlinkUpdateManager(ABC):
 
         except (BroadlinkException, OSError) as err:
             if self.available and (
-                dt.utcnow() - self.last_update > timedelta(minutes=3)
+                dt.utcnow() - self.last_update > self.SCAN_INTERVAL * 3
                 or isinstance(err, (AuthorizationError, OSError))
             ):
                 self.available = False
@@ -87,6 +89,8 @@ class BroadlinkUpdateManager(ABC):
 
 class BroadlinkA1UpdateManager(BroadlinkUpdateManager):
     """Manages updates for Broadlink A1 devices."""
+
+    SCAN_INTERVAL = timedelta(seconds=10)
 
     async def async_fetch_data(self):
         """Fetch data from the device."""
