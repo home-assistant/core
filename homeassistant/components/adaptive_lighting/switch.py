a7d51a6d43903900e8466f6100820f9158ba1079
Bas Nijholt <bas@nijho.lt>
2020-10-05 12:16:34 +0200

    register service after async_add_entities

--- homeassistant/components/adaptive_lighting/switch.py
+++ homeassistant/components/adaptive_lighting/switch.py
@@ -150,6 +150,8 @@ async def async_setup_entry(hass, config_entry: ConfigEntry, async_add_entities:
     data[config_entry.entry_id]["sleep_mode_switch"] = sleep_mode_switch
     data[config_entry.entry_id][SWITCH_DOMAIN] = switch
 
+    async_add_entities([switch, sleep_mode_switch], update_before_add=True)
+
     # Register `apply` service
     platform = entity_platform.current_platform.get()
     platform.async_register_entity_service(
@@ -167,7 +169,6 @@ async def async_setup_entry(hass, config_entry: ConfigEntry, async_add_entities:
         },
         handle_apply,
     )
-    async_add_entities([switch, sleep_mode_switch], update_before_add=True)
 
 
 def validate(config_entry):
