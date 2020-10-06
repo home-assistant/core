7555a2e1b49e251f4d53ac1780594d6076116430
J. Nick Koston <nick@koston.org>
2020-10-06 17:30:08 -0500

    adj

--- homeassistant/components/profiler/__init__.py
+++ homeassistant/components/profiler/__init__.py
@@ -49,9 +49,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     @callback
     def _log_objects(*_):
-        #        _LOGGER.debug("Leaking objects: %s", objgraph.get_leaking_objects())
-        _LOGGER.debug("Most common types: %s", objgraph.most_common_types())
-        _LOGGER.debug("Growth: %s", objgraph.growth())
+        _LOGGER.debug("Most common types: %s", objgraph.most_common_types(limit=25))
+        _LOGGER.debug("Growth: %s", objgraph.growth(limit=25))
 
     async_track_time_interval(hass, _log_objects, timedelta(seconds=30))
 
