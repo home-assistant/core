13129cdb11da1472a1d5c91366516ea941295100
Ville Skyttä <ville.skytta@iki.fi>
2020-10-06 19:12:25 +0300

    Defer legacy platform setup errors to generic ones

--- homeassistant/components/upcloud/binary_sensor.py
+++ homeassistant/components/upcloud/binary_sensor.py
@@ -1,7 +1,5 @@
 """Support for monitoring the state of UpCloud servers."""
 
-import logging
-
 import voluptuous as vol
 
 from homeassistant.components.binary_sensor import PLATFORM_SCHEMA, BinarySensorEntity
@@ -10,20 +8,9 @@ import homeassistant.helpers.config_validation as cv
 
 from . import CONF_SERVERS, DATA_UPCLOUD, UpCloudServerEntity
 
-_LOGGER = logging.getLogger(__name__)
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {vol.Required(CONF_SERVERS): vol.All(cv.ensure_list, [cv.string])}
 )
-
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Legacy platform set up."""
-    _LOGGER.warning(
-        "Loading upcloud binary sensors via platform config is deprecated and no longer "
-        "necessary as of 0.117. Please remove it from binary_sensor YAML configuration."
-    )
-    return True
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
--- homeassistant/components/upcloud/switch.py
+++ homeassistant/components/upcloud/switch.py
@@ -1,5 +1,4 @@
 """Support for interacting with UpCloud servers."""
-import logging
 
 import voluptuous as vol
 
@@ -10,20 +9,9 @@ from homeassistant.helpers.dispatcher import dispatcher_send
 
 from . import CONF_SERVERS, DATA_UPCLOUD, SIGNAL_UPDATE_UPCLOUD, UpCloudServerEntity
 
-_LOGGER = logging.getLogger(__name__)
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {vol.Required(CONF_SERVERS): vol.All(cv.ensure_list, [cv.string])}
 )
-
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Legacy platform set up."""
-    _LOGGER.warning(
-        "Loading upcloud switches via platform config is deprecated and no longer "
-        "necessary as of 0.117. Please remove it from switch YAML configuration."
-    )
-    return True
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
