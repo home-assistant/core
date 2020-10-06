63506b5980fa2ad90fd8f68c6058e6e8cd09edb2
Nathan Tilley <nathan@tilley.xyz>
2020-10-06 11:44:25 -0400

    Flip reversed icons

--- homeassistant/components/faadelays/const.py
+++ homeassistant/components/faadelays/const.py
@@ -5,7 +5,7 @@ DOMAIN = "faadelays"
 FAA_BINARY_SENSORS = {
     "GROUND_DELAY": ("Ground Delay", "mdi:airport"),
     "GROUND_STOP": ("Ground Stop", "mdi:airport"),
-    "DEPART_DELAY": ("Departure Delay", "mdi:airplane-landing"),
-    "ARRIVE_DELAY": ("Arrival Delay", "mdi:airplane-takeoff"),
+    "DEPART_DELAY": ("Departure Delay", "mdi:airplane-takeoff"),
+    "ARRIVE_DELAY": ("Arrival Delay", "mdi:airplane-landing"),
     "CLOSURE": ("Closure", "mdi:airplane-off"),
 }
