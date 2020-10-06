356ebc112885302b5f4bdd8131ba238ed5468a6a
Charles Garwood <cgarwood@gmail.com>
2020-10-06 08:05:52 -0400

    Add cancel_command service for OpenZWave (#41285)
    * Add cancel_command service for OpenZWave
    
    * Raise ValueError if instance is None

--- homeassistant/components/ozw/services.py
+++ homeassistant/components/ozw/services.py
@@ -43,6 +43,14 @@ class ZWaveServices:
                 {vol.Optional(const.ATTR_INSTANCE_ID, default=1): vol.Coerce(int)}
             ),
         )
+        self._hass.services.async_register(
+            const.DOMAIN,
+            const.SERVICE_CANCEL_COMMAND,
+            self.async_cancel_command,
+            schema=vol.Schema(
+                {vol.Optional(const.ATTR_INSTANCE_ID, default=1): vol.Coerce(int)}
+            ),
+        )
 
         self._hass.services.async_register(
             const.DOMAIN,
@@ -111,3 +119,12 @@ class ZWaveServices:
         instance_id = service.data[const.ATTR_INSTANCE_ID]
         instance = self._manager.get_instance(instance_id)
         instance.remove_node()
+
+    @callback
+    def async_cancel_command(self, service):
+        """Tell the controller to cancel an add or remove command."""
+        instance_id = service.data[const.ATTR_INSTANCE_ID]
+        instance = self._manager.get_instance(instance_id)
+        if instance is None:
+            raise ValueError(f"No OpenZWave Instance with ID {instance_id}")
+        instance.cancel_controller_command()
