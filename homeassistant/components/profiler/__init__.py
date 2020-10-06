ad4275aee3f694d108a772ae154abc9511110018
J. Nick Koston <nick@koston.org>
2020-10-06 09:05:31 -0500

    leak

--- homeassistant/components/profiler/__init__.py
+++ homeassistant/components/profiler/__init__.py
@@ -1,14 +1,17 @@
 """The profiler integration."""
 import asyncio
 import cProfile
+from datetime import timedelta
 import logging
 import time
 
+import objgraph
 from pyprof2calltree import convert
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.core import HomeAssistant, ServiceCall, callback
+from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.helpers.service import async_register_admin_service
 from homeassistant.helpers.typing import ConfigType
 
@@ -43,6 +46,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
             {vol.Optional(CONF_SECONDS, default=60.0): vol.Coerce(float)}
         ),
     )
+
+    @callback
+    def _log_objects(*_):
+        _LOGGER.debug("Leaking objects: %s", objgraph.get_leaking_objects())
+        _LOGGER.debug("Most common types: %s", objgraph.most_common_types())
+        _LOGGER.debug("Growth: %s", objgraph.growth())
+
+    async_track_time_interval(hass, _log_objects, timedelta(seconds=30))
 
     return True
 
