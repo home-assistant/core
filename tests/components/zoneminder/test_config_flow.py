72df87752e0566533e1c4c9c0a67a2c671a5b24e
Martin Hjelmare <marhje52@gmail.com>
2020-10-07 14:24:09 +0200

    Revert "Use reference strings for Zoneminder config flow (#41103)"
    This reverts commit 20bacb69189a44e92863f9b2d01660dd908877c3.

--- tests/components/zoneminder/test_config_flow.py
+++ tests/components/zoneminder/test_config_flow.py
@@ -45,7 +45,7 @@ async def test_import(hass: HomeAssistant) -> None:
         )
         assert result
         assert result["type"] == "abort"
-        assert result["reason"] == "invalid_auth"
+        assert result["reason"] == "auth_fail"
 
         zm_client.login.return_value = True
         result = await hass.config_entries.flow.async_init(
