f63731f7fda110022310a22c1743917a33a6299a
docker <chris@talkingtontech.com>
2020-10-07 20:36:59 -0500

    work on flow.

--- homeassistant/components/cloudflare/config_flow.py
+++ homeassistant/components/cloudflare/config_flow.py
@@ -1,52 +1,83 @@
 """Config flow for Cloudflare integration."""
 import logging
+from typing import Dict, List, Optional
 
 from pycfdns import CloudflareUpdater
+from pycfdns.exceptions import (
+    CloudflareAuthenticationException,
+    CloudflareConnectionException,
+    CloudflareZoneException,
+)
 import voluptuous as vol
 
-from homeassistant.config_entries import (
-    CONN_CLASS_CLOUD_PUSH,
-    SOURCE_USER,
-    ConfigFlow,
-)
-from homeassistant.const import CONF_API_KEY, CONF_EMAIL, CONF_ZONE
+from homeassistant.components import persistent_notification
+from homeassistant.config_entries import CONN_CLASS_CLOUD_PUSH, ConfigFlow
+from homeassistant.const import CONF_API_TOKEN, CONF_ZONE
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
+from .const import CONF_RECORDS
 from .const import DOMAIN  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
 
 DATA_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_EMAIL): str, 
-        vol.Required(CONF_API_KEY): str,
-        vol.Required(CONF_ZONE): str
+        vol.Required(CONF_API_TOKEN): str,
     }
 )
 
 
-async def validate_input(hass: HomeAssistant, data):
+def _zone_schema(zones: Optional[List] = None):
+    """Zone selection schema."""
+    zones_list = []
+
+    if zones is not None:
+        zones_list = zones
+
+    return vol.Schema({vol.Required(CONF_ZONE): vol.In(zones_list)})
+
+
+def _records_schema(records: Optional[List] = None):
+    """Zone records selection schema."""
+    records_dict = {}
+
+    if records:
+        records_dict = {name: name for name in records}
+
+    return vol.Schema({vol.Required(CONF_RECORDS): cv.multi_select(records_dict)})
+
+
+async def validate_input(hass: HomeAssistant, data: Dict):
     """Validate the user input allows us to connect.
 
     Data has the keys from DATA_SCHEMA with values provided by the user.
     """
+    zone = data.get(CONF_ZONE)
+    records = None
+
     cfupdate = CloudflareUpdater(
-        async_get_clientsession(),
-        data[CONF_EMAIL],
-        data[CONF_TOKEN],
-        data[CONF_ZONE],
-        {},
+        async_get_clientsession(hass),
+        data[CONF_API_TOKEN],
+        zone,
+        [],
     )
 
     try:
-        await cfupdate.get_zone_id()
-    except CloudflareException as error:
+        zones = await cfupdate.get_zones()
+        if zone:
+            zone_id = await cfupdate.get_zone_id()
+            records = await cfupdate.get_zone_records(zone_id, "A")
+    except CloudflareConnectionException as error:
         raise CannotConnect from error
+    except CloudflareAuthenticationException as error:
+        raise InvalidAuth from error
+    except CloudflareZoneException as error:
+        raise InvalidZone from error
 
-    # Return info that you want to store in the config entry.
-    return {"title": data[CONF_ZONE]}
+    return {"zones": zones, "records": records}
 
 
 class CloudflareConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -55,24 +86,95 @@ class CloudflareConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = CONN_CLASS_CLOUD_PUSH
 
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
+    def __init__(self):
+        """Initialize the Cloudflare config flow."""
+        self.cloudflare_config = {}
+        self.zones = None
+        self.records = None
+
+    async def async_step_user(self, user_input: Optional[Dict] = None):
+        """Handle a flow initiated by the user."""
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        assert self.hass
+        persistent_notification.async_dismiss(self.hass, "cloudflare_setup")
+
         errors = {}
 
         if user_input is not None:
-            try:
-                info = await validate_input(self.hass, user_input)
-                return self.async_create_entry(title=info["title"], data=user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+            info, errors = await self._async_validate_or_error(user_input)
+
+            if not errors:
+                self.cloudflare_config.update(user_input)
+                self.zones = info["zones"]
+                return await self.async_step_zone()
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
 
+    async def async_step_zone(self, user_input: Optional[Dict] = None):
+        """Handle the picking the zone."""
+        errors = {}
+
+        if user_input is not None:
+            self.cloudflare_config.update(user_input)
+            info, errors = await self._async_validate_or_error(self.cloudflare_config)
+
+            if not errors:
+                await self.async_set_unique_id(user_input[CONF_ZONE])
+                self.records = info["records"]
+
+                return await self.async_step_records()
+
+        return self.async_show_form(
+            step_id="zone",
+            data_schema=vol.Schema(_zone_schema(self.zones)),
+            errors=errors,
+        )
+
+    async def async_step_records(self, user_input: Optional[Dict] = None):
+        """Handle the picking the zone records."""
+        errors = {}
+
+        if user_input is not None:
+            self.cloudflare_config.update(user_input)
+            title = self.cloudflare_config[CONF_ZONE]
+            return self.async_create_entry(title=title, data=self.cloudflare_config)
+
+        return self.async_show_form(
+            step_id="records",
+            data_schema=vol.Schema(_records_schema(self.records)),
+            errors=errors,
+        )
+
+    async def _async_validate_or_error(self, config):
+        errors = {}
+        info = {}
+
+        try:
+            info = await validate_input(self.hass, config)
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except InvalidZone:
+            errors["base"] = "invalid_zone"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+
+        return info, errors
+
 
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(HomeAssistantError):
+    """Error to indicate there is invalid auth."""
+
+
+class InvalidZone(HomeAssistantError):
+    """Error to indicate we cannot alidate zone exists in account."""
--- homeassistant/components/cloudflare/const.py
+++ homeassistant/components/cloudflare/const.py
@@ -1,3 +1,16 @@
 """Constants for Cloudflare."""
 
 DOMAIN = "cloudflare"
+
+# Config
+CONF_API_TOKEN = "api_token"
+CONF_RECORDS = "records"
+
+# Data
+DATA_UNDO_UPDATE_INTERVAL = "undo_update_interval"
+
+# Defaults
+DEFAULT_UPDATE_INTERVAL = 60  # in minutes
+
+# Services
+SERVICE_UPDATE_RECORDS = "update_records"
