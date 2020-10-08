7a56b3f8b90fe85305ddab38f344f33d2af56675
Sergio Conde Gomez <skgsergio@gmail.com>
2020-10-08 10:59:02 +0200

    Simplify config_flow steps

--- homeassistant/components/foscam/config_flow.py
+++ homeassistant/components/foscam/config_flow.py
@@ -3,15 +3,15 @@ from libpyfoscam import FoscamCamera
 from libpyfoscam.foscam import ERROR_FOSCAM_AUTH, ERROR_FOSCAM_UNAVAILABLE
 import voluptuous as vol
 
-from homeassistant import config_entries, core, exceptions
+from homeassistant import config_entries, exceptions
 from homeassistant.const import (
     CONF_HOST,
-    CONF_MAC,
     CONF_NAME,
     CONF_PASSWORD,
     CONF_PORT,
     CONF_USERNAME,
 )
+from homeassistant.data_entry_flow import AbortFlow
 
 from .const import CONF_STREAM, LOGGER
 from .const import DOMAIN  # pylint:disable=unused-import
@@ -32,36 +32,40 @@ DATA_SCHEMA = vol.Schema(
 )
 
 
-async def validate_input(hass: core.HomeAssistant, data):
-    """Validate the user input allows us to connect.
-
-    Data has the keys from DATA_SCHEMA with values provided by the user.
-    """
-    camera = FoscamCamera(
-        data[CONF_HOST],
-        data[CONF_PORT],
-        data[CONF_USERNAME],
-        data[CONF_PASSWORD],
-        verbose=False,
-    )
-
-    # Validate data by sending a request to the camera
-    ret, response = await hass.async_add_executor_job(camera.get_dev_info)
-
-    if ret == ERROR_FOSCAM_UNAVAILABLE:
-        raise CannotConnect
-
-    if ret == ERROR_FOSCAM_AUTH:
-        raise InvalidAuth
-
-    return {CONF_NAME: response["devName"], CONF_MAC: response["mac"]}
-
-
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for foscam."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+
+    async def _validate_and_create(self, data):
+        """Validate the user input allows us to connect.
+
+        Data has the keys from DATA_SCHEMA with values provided by the user.
+        """
+        camera = FoscamCamera(
+            data[CONF_HOST],
+            data[CONF_PORT],
+            data[CONF_USERNAME],
+            data[CONF_PASSWORD],
+            verbose=False,
+        )
+
+        # Validate data by sending a request to the camera
+        ret, response = await self.hass.async_add_executor_job(camera.get_dev_info)
+
+        if ret == ERROR_FOSCAM_UNAVAILABLE:
+            raise CannotConnect
+
+        if ret == ERROR_FOSCAM_AUTH:
+            raise InvalidAuth
+
+        await self.async_set_unique_id(response["mac"])
+        self._abort_if_unique_id_configured()
+
+        name = data.pop(CONF_NAME, response["devName"])
+
+        return self.async_create_entry(title=name, data=data)
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
@@ -69,7 +73,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             try:
-                info = await validate_input(self.hass, user_input)
+                return await self._validate_and_create(user_input)
 
             except CannotConnect:
                 errors["base"] = "cannot_connect"
@@ -77,15 +81,12 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except InvalidAuth:
                 errors["base"] = "invalid_auth"
 
+            except AbortFlow:
+                raise
+
             except Exception:  # pylint: disable=broad-except
                 LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
-
-            if not errors:
-                await self.async_set_unique_id(info[CONF_MAC])
-                self._abort_if_unique_id_configured()
-
-                return self.async_create_entry(title=info[CONF_NAME], data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
@@ -94,7 +95,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_import(self, import_config):
         """Handle config import from yaml."""
         try:
-            info = await validate_input(self.hass, import_config)
+            return await self._validate_and_create(import_config)
 
         except CannotConnect:
             LOGGER.error("Error importing foscam platform config: cannot connect.")
@@ -104,19 +105,14 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             LOGGER.error("Error importing foscam platform config: invalid auth.")
             return self.async_abort(reason="invalid_auth")
 
+        except AbortFlow:
+            raise
+
         except Exception:  # pylint: disable=broad-except
             LOGGER.exception(
-                "Error importing foscam platform config: unexpected exception"
+                "Error importing foscam platform config: unexpected exception."
             )
             return self.async_abort(reason="unknown")
-
-        await self.async_set_unique_id(info[CONF_MAC])
-        self._abort_if_unique_id_configured()
-
-        if CONF_NAME in import_config:
-            info[CONF_NAME] = import_config.pop(CONF_NAME)
-
-        return self.async_create_entry(title=info[CONF_NAME], data=import_config)
 
 
 class CannotConnect(exceptions.HomeAssistantError):
