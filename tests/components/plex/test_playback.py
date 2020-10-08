99e0b10d21f4dda61a5683ea08d77a17802b0f82
Jason Lawrence <jjlawren@users.noreply.github.com>
2020-10-08 13:43:26 -0500

    Add tests

--- tests/components/plex/test_playback.py
+++ tests/components/plex/test_playback.py
@@ -1,16 +1,26 @@
 """Tests for Plex player playback methods/services."""
 from plexapi.exceptions import NotFound
 
+from homeassistant.components.demo import DOMAIN as DEMO_DOMAIN
 from homeassistant.components.media_player.const import (
     ATTR_MEDIA_CONTENT_ID,
     ATTR_MEDIA_CONTENT_TYPE,
     MEDIA_TYPE_MUSIC,
 )
-from homeassistant.components.plex.const import DOMAIN, SERVERS, SERVICE_PLAY_ON_SONOS
+from homeassistant.components.plex.const import (
+    DOMAIN,
+    SERVERS,
+    SERVICE_PLAY_ON_OTHER,
+    SERVICE_PLAY_ON_SONOS,
+)
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.setup import async_setup_component
+
+from .const import DEFAULT_OPTIONS, SECOND_DATA
 
 from tests.async_mock import patch
+from tests.common import MockConfigEntry
 
 
 async def test_sonos_playback(hass, mock_plex_server):
@@ -119,3 +129,56 @@ async def test_sonos_playback(hass, mock_plex_server):
             },
             True,
         )
+
+
+async def test_playback_to_unsupported_integration(hass, caplog, setup_plex_server):
+    """Test playing media on an unsupported integration."""
+    assert await async_setup_component(
+        hass, "media_player", {"media_player": {"platform": "demo"}}
+    )
+    await hass.async_block_till_done()
+
+    # Set up after demo to allow `media_player` platform to load
+    await setup_plex_server()
+
+    assert await hass.services.async_call(
+        DOMAIN,
+        SERVICE_PLAY_ON_OTHER,
+        {
+            ATTR_ENTITY_ID: "media_player.bedroom",
+            ATTR_MEDIA_CONTENT_TYPE: MEDIA_TYPE_MUSIC,
+            ATTR_MEDIA_CONTENT_ID: '{"library_name": "Music", "artist_name": "Artist", "album_name": "Album"}',
+        },
+        True,
+    )
+    assert f"{DEMO_DOMAIN} is not a supported integration" in caplog.text
+
+
+async def test_playback_on_other_multiple_servers(hass, caplog, setup_plex_server):
+    """Test playing media on other integrations with multiple servers."""
+    assert await async_setup_component(
+        hass, "media_player", {"media_player": {"platform": "demo"}}
+    )
+    await hass.async_block_till_done()
+
+    second_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data=SECOND_DATA,
+        options=DEFAULT_OPTIONS,
+        unique_id=SECOND_DATA["server_id"],
+    )
+
+    await setup_plex_server()
+    await setup_plex_server(config_entry=second_entry)
+
+    assert await hass.services.async_call(
+        DOMAIN,
+        SERVICE_PLAY_ON_OTHER,
+        {
+            ATTR_ENTITY_ID: "media_player.bedroom",
+            ATTR_MEDIA_CONTENT_TYPE: MEDIA_TYPE_MUSIC,
+            ATTR_MEDIA_CONTENT_ID: '{"library_name": "Music", "artist_name": "Artist", "album_name": "Album"}',
+        },
+        True,
+    )
+    assert "Multiple Plex servers available" in caplog.text
