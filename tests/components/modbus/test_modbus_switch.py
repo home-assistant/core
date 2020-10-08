4d9ff1338459e10530eaabe109d3118774f1160f
jan iversen <jancasacondor@gmail.com>
2020-10-08 23:52:41 +0200

    Fix bool registers in modbus integration (#41506)
    When a register is of type bool modbus returns a whole byte, but ONLY the
    lowest bit determine true/false.

--- tests/components/modbus/test_modbus_switch.py
+++ tests/components/modbus/test_modbus_switch.py
@@ -13,39 +13,19 @@ from .conftest import run_base_read_test, setup_base_test
 _LOGGER = logging.getLogger(__name__)
 
 
-async def run_sensor_test(hass, use_mock_hub, value, expected):
-    """Run test for given config."""
-    switch_name = "modbus_test_switch"
-    scan_interval = 5
-    entity_id, now, device = await setup_base_test(
-        switch_name,
-        hass,
-        use_mock_hub,
-        {
-            CONF_COILS: [
-                {CONF_NAME: switch_name, CALL_TYPE_COIL: 1234, CONF_SLAVE: 1},
-            ]
-        },
-        SWITCH_DOMAIN,
-        scan_interval,
-    )
-
-    await run_base_read_test(
-        entity_id,
-        hass,
-        use_mock_hub,
-        CALL_TYPE_COIL,
-        value,
-        expected,
-        now + timedelta(seconds=scan_interval + 1),
-    )
-
-
 @pytest.mark.parametrize(
     "regs,expected",
     [
         (
             [0x00],
+            STATE_OFF,
+        ),
+        (
+            [0x80],
+            STATE_OFF,
+        ),
+        (
+            [0xFE],
             STATE_OFF,
         ),
         (
@@ -59,10 +39,28 @@ async def run_sensor_test(hass, use_mock_hub, value, expected):
     ],
 )
 async def test_coil_switch(hass, mock_hub, regs, expected):
-    """Test reading of switch coil."""
-    await run_sensor_test(
+    """Run test for given config."""
+    switch_name = "modbus_test_switch"
+    scan_interval = 5
+    entity_id, now, device = await setup_base_test(
+        switch_name,
         hass,
         mock_hub,
+        {
+            CONF_COILS: [
+                {CONF_NAME: switch_name, CALL_TYPE_COIL: 1234, CONF_SLAVE: 1},
+            ]
+        },
+        SWITCH_DOMAIN,
+        scan_interval,
+    )
+
+    await run_base_read_test(
+        entity_id,
+        hass,
+        mock_hub,
+        CALL_TYPE_COIL,
         regs,
         expected,
+        now + timedelta(seconds=scan_interval + 1),
     )
