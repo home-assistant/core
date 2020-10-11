0bb0392ba25789df9d5ea93b20304186fc96a9a4
Rob Bierbooms <mail@robbierbooms.nl>
2020-10-11 19:55:48 +0000

    Change config flow to setup camera or weather

--- homeassistant/components/buienradar/config_flow.py
+++ homeassistant/components/buienradar/config_flow.py
@@ -4,7 +4,7 @@ import logging
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
+from homeassistant.const import CONF_DOMAIN, CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
 from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 
@@ -14,6 +14,7 @@ from .const import (
     CONF_DIMENSION,
     CONF_SENSOR,
     CONF_WEATHER,
+    DEFAULT_DIMENSION,
     DOMAIN,
     HOME_LOCATION_NAME,
     SUPPORTED_COUNTRY_CODES,
@@ -46,6 +47,57 @@ class BuienradarFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
+        if user_input is not None:
+            if user_input[CONF_DOMAIN] == "Camera":
+                return await self.async_step_camera()
+            else:
+                return await self.async_step_weather()
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_DOMAIN): vol.In(["Camera", "Weather"]),
+            }
+        )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
+            errors={},
+        )
+
+    async def async_step_camera(self, user_input=None):
+        """Handle a flow to configure camera setup."""
+        errors = {}
+
+        if user_input is not None:
+            if (
+                f"{user_input.get(CONF_DIMENSION)}-{user_input.get(CONF_COUNTRY)}"
+                not in configured_instances(self.hass)
+            ):
+                return await self.async_create_buienradar_entry(user_input, True)
+
+            errors["base"] = "already_configured"
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_NAME, default=HOME_LOCATION_NAME): str,
+                vol.Required(CONF_DIMENSION, default=DEFAULT_DIMENSION): vol.All(
+                    vol.Coerce(int), vol.Range(min=120, max=700)
+                ),
+                vol.Required(CONF_COUNTRY, default="NL"): vol.All(
+                    vol.Coerce(str), vol.In(SUPPORTED_COUNTRY_CODES)
+                ),
+            }
+        )
+
+        return self.async_show_form(
+            step_id="camera",
+            data_schema=data_schema,
+            errors=errors,
+        )
+
+    async def async_step_weather(self, user_input=None):
+        """Handle a flow to configure weather setup."""
         errors = {}
 
         if user_input is not None:
@@ -53,9 +105,9 @@ class BuienradarFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 f"{user_input.get(CONF_LATITUDE)}-{user_input.get(CONF_LONGITUDE)}"
                 not in configured_instances(self.hass)
             ):
-                return await self.async_create_buienradar_entry(user_input)
+                return await self.async_create_buienradar_entry(user_input, False)
 
-            errors[CONF_NAME] = "name_exists"
+            errors["base"] = "already_configured"
 
         data_schema = vol.Schema(
             {
@@ -66,33 +118,33 @@ class BuienradarFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required(
                     CONF_LONGITUDE, default=self.hass.config.longitude
                 ): cv.longitude,
-                vol.Required(CONF_CAMERA, default=False): bool,
-                vol.Required(CONF_COUNTRY, default="NL"): vol.All(
-                    vol.Coerce(str), vol.In(SUPPORTED_COUNTRY_CODES)
-                ),
             }
         )
 
         return self.async_show_form(
-            step_id="user",
+            step_id="weather",
             data_schema=data_schema,
             errors=errors,
         )
 
-    async def async_create_buienradar_entry(self, input_data):
+    async def async_create_buienradar_entry(self, input_data, is_camera):
         """Create a config entry."""
         name = input_data[CONF_NAME]
 
         # Always setup sensor and weather platform, camera is optional
         data = {
             CONF_NAME: name,
-            CONF_LATITUDE: input_data[CONF_LATITUDE],
-            CONF_LONGITUDE: input_data[CONF_LONGITUDE],
-            CONF_CAMERA: input_data[CONF_CAMERA],
-            CONF_SENSOR: True,
-            CONF_WEATHER: True,
-            CONF_COUNTRY: input_data[CONF_COUNTRY],
+            CONF_CAMERA: is_camera,
+            CONF_SENSOR: (not is_camera),
+            CONF_WEATHER: (not is_camera),
         }
+
+        if is_camera:
+            data[CONF_COUNTRY] = input_data[CONF_COUNTRY]
+            data[CONF_DIMENSION] = input_data[CONF_DIMENSION]
+        else:
+            data[CONF_LATITUDE] = input_data[CONF_LATITUDE]
+            data[CONF_LONGITUDE] = input_data[CONF_LONGITUDE]
 
         return self.async_create_entry(title=name, data=data)
 
