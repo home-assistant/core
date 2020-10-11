43fb41228332afee10e455b62ccafb78c3f0163c
epenet <6771947+epenet@users.noreply.github.com>
2020-10-11 21:15:01 +0200

    Ajust unique_id for config entries

--- homeassistant/components/onewire/config_flow.py
+++ homeassistant/components/onewire/config_flow.py
@@ -43,14 +43,6 @@ class OneWireFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 return await self.async_step_owserver()
             if CONF_TYPE_SYSBUS == user_input[CONF_TYPE]:
                 return await self.async_step_mount_dir()
-                if os.path.isdir(DEFAULT_SYSBUS_MOUNT_DIR):
-                    self.onewire_config[CONF_MOUNT_DIR] = DEFAULT_SYSBUS_MOUNT_DIR
-                    await self.async_set_unique_id(CONF_TYPE_SYSBUS)
-                    self._abort_if_unique_id_configured()
-                    return self.async_create_entry(
-                        title=CONF_TYPE_SYSBUS, data=self.onewire_config
-                    )
-                errors["base"] = "invalid_path"
 
         proxy_types = [CONF_TYPE_OWSERVER, CONF_TYPE_SYSBUS]
 
@@ -69,7 +61,9 @@ class OneWireFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             owport = user_input.get(CONF_PORT)
             try:
                 await self.hass.async_add_executor_job(protocol.proxy, owhost, owport)
-                await self.async_set_unique_id(f"{owhost}:{owport}")
+                await self.async_set_unique_id(
+                    f"{CONF_TYPE_OWSERVER}:{owhost}:{owport}"
+                )
                 self._abort_if_unique_id_configured()
                 return self.async_create_entry(title=owhost, data=self.onewire_config)
             except (protocol.Error, protocol.ConnError) as exc:
@@ -96,10 +90,11 @@ class OneWireFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             self.onewire_config.update(user_input)
             mount_dir = user_input.get(CONF_MOUNT_DIR)
             if os.path.isdir(mount_dir):
-                title = mount_dir
-                if self.onewire_config[CONF_TYPE] == CONF_TYPE_SYSBUS:
-                    title = CONF_TYPE_SYSBUS
-                return self.async_create_entry(title=title, data=self.onewire_config)
+                await self.async_set_unique_id(f"{CONF_TYPE_SYSBUS}:{mount_dir}")
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(
+                    title=mount_dir, data=self.onewire_config
+                )
             errors["base"] = "invalid_path"
 
         return self.async_show_form(
