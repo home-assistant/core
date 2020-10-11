a35f0637674c056244b83443bfa6a9af67a1a199
Martin Hjelmare <marhje52@gmail.com>
2020-10-11 16:52:50 +0200

    Add supervisor step

--- homeassistant/components/ozw/config_flow.py
+++ homeassistant/components/ozw/config_flow.py
@@ -1,8 +1,17 @@
 """Config flow for ozw integration."""
+import voluptuous as vol
+
 from homeassistant import config_entries
 
 from .const import DOMAIN  # pylint:disable=unused-import
 
+ON_SUPERVISOR_SCHEMA = vol.Schema(
+    {
+        vol.Required("connection"): vol.In(
+            {"addon": "Add-on", "mqtt_integration": "MQTT integration"}
+        )
+    }
+)
 TITLE = "OpenZWave"
 
 
@@ -16,7 +25,11 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
-        return await self.async_step_use_mqtt_integration()
+
+        if not self.hass.components.hassio.is_hassio():
+            return await self.async_step_use_mqtt_integration()
+
+        return await self.async_step_on_supervisor()
 
     async def async_step_use_mqtt_integration(self, user_input=None):
         """Handle logic when using the MQTT integration."""
@@ -26,3 +39,19 @@ class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title=TITLE, data={})
 
         return self.async_show_form(step_id="use_mqtt_integration")
+
+    async def async_step_on_supervisor(self, user_input=None):
+        """Handle logic when on Supervisor host."""
+        if user_input is not None:
+            connection = user_input["connection"]
+            if connection == "addon":
+                return await self.async_step_use_addon()
+            return await self.async_step_use_mqtt_integration()
+
+        return self.async_show_form(
+            step_id="on_supervisor", data_schema=ON_SUPERVISOR_SCHEMA
+        )
+
+    async def async_step_use_addon(self, user_input=None):
+        """Handle logic when using the OpenZWave add-on."""
+        pass
