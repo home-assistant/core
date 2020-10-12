491305900203f79675a4cc3fd53789f05d0cb89c
Brett Adams <brett@ba.id.au>
2020-10-12 07:29:06 +0000

    Test for duplicate config flow

--- tests/components/advantage_air/test_config_flow.py
+++ tests/components/advantage_air/test_config_flow.py
@@ -1,6 +1,6 @@
 """Test the Advantage Air config flow."""
 
-from homeassistant import config_entries, data_entry_flow, setup
+from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.advantage_air.const import DOMAIN
 
 from tests.async_mock import patch
@@ -9,29 +9,25 @@ from tests.components.advantage_air import TEST_SYSTEM_DATA, TEST_SYSTEM_URL, US
 
 async def test_form(hass, aioclient_mock):
     """Test that form shows up."""
-    with patch(
-        "homeassistant.components.advantage_air.async_setup", return_value=True
-    ) as mock_setup:
-        await setup.async_setup_component(hass, DOMAIN, {})
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "user"
-    assert result["errors"] == {}
 
     aioclient_mock.get(
         TEST_SYSTEM_URL,
         text=TEST_SYSTEM_DATA,
     )
 
+    result1 = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result1["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result1["step_id"] == "user"
+    assert result1["errors"] == {}
+
     with patch(
         "homeassistant.components.advantage_air.async_setup_entry",
         return_value=True,
     ) as mock_setup_entry:
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
+            result1["flow_id"],
             USER_INPUT,
         )
 
@@ -40,8 +36,17 @@ async def test_form(hass, aioclient_mock):
     assert result2["title"] == "testname"
     assert result2["data"] == USER_INPUT
     await hass.async_block_till_done()
-    assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
+
+    # Test Duplicate Config Flow
+    result3 = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    result4 = await hass.config_entries.flow.async_configure(
+        result3["flow_id"],
+        USER_INPUT,
+    )
+    assert result4["type"] == data_entry_flow.RESULT_TYPE_ABORT
 
 
 async def test_form_cannot_connect(hass, aioclient_mock):
