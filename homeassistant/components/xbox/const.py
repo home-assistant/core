9877e8e25ba1c1a86599022d9cb9cbdd305de9ca
Jason Hunter <hunterjm@gmail.com>
2020-10-13 09:37:01 -0400

    Add Xbox Integration (#41697)
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

---
+++ homeassistant/components/xbox/const.py
@@ -0,0 +1,6 @@
+"""Constants for the xbox integration."""
+
+DOMAIN = "xbox"
+
+OAUTH2_AUTHORIZE = "https://login.live.com/oauth20_authorize.srf"
+OAUTH2_TOKEN = "https://login.live.com/oauth20_token.srf"
