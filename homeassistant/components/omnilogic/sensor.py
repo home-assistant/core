8fb7913f6f2d295fc1bf3fa8e76c9464f8e715a3
Tim Empringham <tim.empringham@live.ca>
2020-10-13 10:13:08 -0400

    Fixed salt sensor metric conversion, fixed SINGLE pump type for Filters (excluded).

--- homeassistant/components/omnilogic/sensor.py
+++ homeassistant/components/omnilogic/sensor.py
@@ -175,7 +175,7 @@ class OmniLogicSaltLevelSensor(OmnilogicSensor):
         unit_of_measurement = self._unit
 
         if self._unit_type == "Metric":
-            salt_return = round(salt_return / 1000, 2)
+            salt_return = round(int(salt_return) / 1000, 2)
             unit_of_measurement = f"{MASS_GRAMS}/{VOLUME_LITERS}"
 
         self._unit = unit_of_measurement
@@ -279,7 +279,7 @@ SENSOR_TYPES = {
             "icon": "mdi:speedometer",
             "unit": PERCENTAGE,
             "guard_condition": [
-                {"Type": "FMT_SINGLE_SPEED"},
+                {"Filter-Type": "FMT_SINGLE_SPEED"},
             ],
         },
     ],
