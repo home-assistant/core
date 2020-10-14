94e4a73a795c2e59db997dee5f055918796869a3
Clifford Roche <clifford.roche@gmail.com>
2020-10-14 09:15:56 -0400

    Add gree climate integration (#37498)
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

---
+++ homeassistant/components/gree/bridge.py
@@ -0,0 +1,38 @@
+"""Helper and wrapper classes for Gree module."""
+import logging
+from typing import List
+
+from greeclimate.device import Device, DeviceInfo
+from greeclimate.discovery import Discovery
+from greeclimate.exceptions import DeviceNotBoundError
+
+from homeassistant import exceptions
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class DeviceHelper:
+    """Device search and bind wrapper for Gree platform."""
+
+    @staticmethod
+    async def try_bind_device(device_info: DeviceInfo) -> Device:
+        """Try and bing with a discovered device.
+
+        Note the you must bind with the device very quickly after it is discovered, or the
+        process may not be completed correctly, raising a `CannotConnect` error.
+        """
+        device = Device(device_info)
+        try:
+            await device.bind()
+        except DeviceNotBoundError as exception:
+            raise CannotConnect from exception
+        return device
+
+    @staticmethod
+    async def find_devices() -> List[DeviceInfo]:
+        """Gather a list of device infos from the local network."""
+        return await Discovery.search_devices()
+
+
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
