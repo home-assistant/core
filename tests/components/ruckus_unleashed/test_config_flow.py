9f5c0c854ba85e97ac500841d9bca9c2c4262671
Gabe Cook <gabe565@gmail.com>
2020-10-14 02:16:30 -0500

    Update tests

--- tests/components/ruckus_unleashed/test_config_flow.py
+++ tests/components/ruckus_unleashed/test_config_flow.py
@@ -1,16 +1,19 @@
 """Test the Ruckus Unleashed config flow."""
+from datetime import timedelta
+
 from pyruckus.exceptions import AuthenticationError
 
-from homeassistant import config_entries, setup
+from homeassistant import config_entries
 from homeassistant.components.ruckus_unleashed.const import DOMAIN
+from homeassistant.util import utcnow
 
 from tests.async_mock import patch
-from tests.components.ruckus_unleashed import CONFIG, DEFAULT_TITLE
+from tests.common import async_fire_time_changed
+from tests.components.ruckus_unleashed import CONFIG, DEFAULT_SYSTEM_INFO, DEFAULT_TITLE
 
 
 async def test_form(hass):
     """Test we get the form."""
-    await setup.async_setup_component(hass, "persistent_notification", {})
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
@@ -23,6 +26,9 @@ async def test_form(hass):
     ), patch(
         "homeassistant.components.ruckus_unleashed.Ruckus.mesh_name",
         return_value=DEFAULT_TITLE,
+    ), patch(
+        "homeassistant.components.ruckus_unleashed.Ruckus.system_info",
+        return_value=DEFAULT_SYSTEM_INFO,
     ), patch(
         "homeassistant.components.ruckus_unleashed.async_setup", return_value=True
     ) as mock_setup, patch(
@@ -97,3 +103,70 @@ async def test_form_unknown_error(hass):
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "unknown"}
+
+
+async def test_form_cannot_connect_unknown_serial(hass):
+    """Test we handle cannot connect error on invalid serial number."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == "form"
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.ruckus_unleashed.Ruckus.connect",
+        return_value=None,
+    ), patch(
+        "homeassistant.components.ruckus_unleashed.Ruckus.mesh_name",
+        return_value=DEFAULT_TITLE,
+    ), patch(
+        "homeassistant.components.ruckus_unleashed.Ruckus.system_info",
+        return_value={},
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            CONFIG,
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_duplicate_error(hass):
+    """Test we handle duplicate error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.ruckus_unleashed.Ruckus.connect",
+        return_value=None,
+    ), patch(
+        "homeassistant.components.ruckus_unleashed.Ruckus.mesh_name",
+        return_value=DEFAULT_TITLE,
+    ), patch(
+        "homeassistant.components.ruckus_unleashed.Ruckus.system_info",
+        return_value=DEFAULT_SYSTEM_INFO,
+    ):
+        await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            CONFIG,
+        )
+
+        future = utcnow() + timedelta(minutes=60)
+        async_fire_time_changed(hass, future)
+        await hass.async_block_till_done()
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
+        assert result["type"] == "form"
+        assert result["errors"] == {}
+
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            CONFIG,
+        )
+
+    assert result2["type"] == "abort"
+    assert result2["reason"] == "already_configured"
