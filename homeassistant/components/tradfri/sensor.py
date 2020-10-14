ec2140c2c5a9572d20421ad9adab862235e8adf7
Thomas Barnekov <37448419+tbarnekov@users.noreply.github.com>
2020-10-14 20:42:13 +0000

    Show battery level for devices

--- homeassistant/components/tradfri/sensor.py
+++ homeassistant/components/tradfri/sensor.py
@@ -13,25 +13,21 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     api = tradfri_data[KEY_API]
     devices = tradfri_data[DEVICES]
 
-    sensors = (
-        dev
-        for dev in devices
-        if not dev.has_light_control
-        and not dev.has_socket_control
-        and not dev.has_blind_control
-        and not dev.has_signal_repeater_control
-    )
+    sensors = (dev for dev in devices if dev.device_info.power_source == 3)  # Battery
+
     if sensors:
-        async_add_entities(TradfriSensor(sensor, api, gateway_id) for sensor in sensors)
+        async_add_entities(
+            TradfriBatterySensor(sensor, api, gateway_id) for sensor in sensors
+        )
 
 
-class TradfriSensor(TradfriBaseDevice):
+class TradfriBatterySensor(TradfriBaseDevice):
     """The platform class required by Home Assistant."""
 
     def __init__(self, device, api, gateway_id):
-        """Initialize the device."""
+        """Initialize the battery sensor entity."""
         super().__init__(device, api, gateway_id)
-        self._unique_id = f"{gateway_id}-{device.id}"
+        self._unique_id = f"{gateway_id}-{device.id}-battery-sensor"
 
     @property
     def device_class(self):
@@ -47,3 +43,7 @@ class TradfriSensor(TradfriBaseDevice):
     def unit_of_measurement(self):
         """Return the unit_of_measurement of the device."""
         return PERCENTAGE
+
+    def _refresh(self, device):
+        super()._refresh(device)
+        self._name = f"{device.name} battery sensor"
