b9d27668808303fa7e77a5911bfe1cabb3a852c4
Raman Gupta <7243222+raman325@users.noreply.github.com>
2020-10-14 11:26:45 -0400

    Update homeassistant/config_entries.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -572,7 +572,7 @@ class ConfigEntriesFlowManager(data_entry_flow.FlowManager):
             self.hass.components.persistent_notification.async_create(
                 title="Devices require reconfiguration",
                 message=(
-                    "At least one of your devices requires reconfiguration to "
+                    "At least one of your integrations requires reconfiguration to "
                     "continue functioning. [Check it out](/config/integrations)"
                 ),
                 notification_id=RECONFIGURE_NOTIFICATION_ID,
