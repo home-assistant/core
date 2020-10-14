7d660eff3dccb5380665da2ea9e9611985f23984
Brett Adams <brett@ba.id.au>
2020-10-14 23:19:49 +0000

    Code quality improvements

--- homeassistant/components/advantage_air/sensor.py
+++ homeassistant/components/advantage_air/sensor.py
@@ -1,11 +1,11 @@
 """Sensor platform for Advantage Air integration."""
 import voluptuous as vol
 
-from homeassistant.const import CONF_IP_ADDRESS, PERCENTAGE
+from homeassistant.const import PERCENTAGE
 from homeassistant.helpers import config_validation as cv, entity_platform
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DOMAIN, ADVANTAGE_AIR_STATE_OPEN
+from .const import ADVANTAGE_AIR_STATE_OPEN, DOMAIN
 
 ADVANTAGE_AIR_SET_COUNTDOWN_VALUE = "minutes"
 ADVANTAGE_AIR_SET_COUNTDOWN_UNIT = "min"
@@ -14,29 +14,19 @@ ADVANTAGE_AIR_SET_COUNTDOWN_UNIT = "min"
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up AdvantageAir sensor platform."""
 
-    instance = hass.data[DOMAIN][config_entry.data[CONF_IP_ADDRESS]]
+    instance = hass.data[DOMAIN][config_entry.entry_id]
 
     entities = []
-    for ac_index in instance["coordinator"].data["aircons"]:
-        entities.append(AdvantageAirTimeTo(instance, ac_index, "On"))
-        entities.append(AdvantageAirTimeTo(instance, ac_index, "Off"))
-        for zone_index in instance["coordinator"].data["aircons"][ac_index]["zones"]:
+    for ac_key, ac_device in instance["coordinator"].data["aircons"].items():
+        entities.append(AdvantageAirTimeTo(instance, ac_key, "On"))
+        entities.append(AdvantageAirTimeTo(instance, ac_key, "Off"))
+        for zone_key, zone in ac_device["zones"].items():
             # Only show damper sensors when zone is in temperature control
-            if (
-                instance["coordinator"].data["aircons"][ac_index]["zones"][zone_index][
-                    "type"
-                ]
-                != 0
-            ):
-                entities.append(AdvantageAirZoneVent(instance, ac_index, zone_index))
+            if zone["type"] != 0:
+                entities.append(AdvantageAirZoneVent(instance, ac_key, zone_key))
             # Only show wireless signal strength sensors when using wireless sensors
-            if (
-                instance["coordinator"].data["aircons"][ac_index]["zones"][zone_index][
-                    "rssi"
-                ]
-                > 0
-            ):
-                entities.append(AdvantageAirZoneSignal(instance, ac_index, zone_index))
+            if zone["rssi"] > 0:
+                entities.append(AdvantageAirZoneSignal(instance, ac_key, zone_key))
     async_add_entities(entities)
 
     platform = entity_platform.current_platform.get()
@@ -48,29 +38,31 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 class AdvantageAirTimeTo(CoordinatorEntity):
     """Representation of Advantage Air timer control."""
 
-    def __init__(self, instance, ac_index, time_period):
+    def __init__(self, instance, ac_key, time_period):
         """Initialize the Advantage Air timer control."""
         super().__init__(instance["coordinator"])
         self.async_change = instance["async_change"]
-        self.ac_index = ac_index
+        self.ac_key = ac_key
         self.time_period = time_period
+
+    @property
+    def _ac(self):
+        return self.coordinator.data["aircons"][self.ac_key]["info"]
 
     @property
     def name(self):
         """Return the name."""
-        return f'{self.coordinator.data["aircons"][self.ac_index]["info"]["name"]} Time To {self.time_period}'
+        return f'{self._ac["name"]} Time To {self.time_period}'
 
     @property
     def unique_id(self):
         """Return a unique id."""
-        return f'{self.coordinator.data["system"]["rid"]}-{self.ac_index}-timeto{self.time_period}'
+        return f'{self.coordinator.data["system"]["rid"]}-{self.ac_key}-timeto{self.time_period}'
 
     @property
     def state(self):
         """Return the current value."""
-        return self.coordinator.data["aircons"][self.ac_index]["info"][
-            f"countDownTo{self.time_period}"
-        ]
+        return self._ac[f"countDownTo{self.time_period}"]
 
     @property
     def unit_of_measurement(self):
@@ -81,10 +73,7 @@ class AdvantageAirTimeTo(CoordinatorEntity):
     def icon(self):
         """Return a representative icon of the timer."""
         return ["mdi:timer-off-outline", "mdi:timer-outline"][
-            self.coordinator.data["aircons"][self.ac_index]["info"][
-                f"countDownTo{self.time_period}"
-            ]
-            > 0
+            self._ac[f"countDownTo{self.time_period}"] > 0
         ]
 
     @property
@@ -103,41 +92,40 @@ class AdvantageAirTimeTo(CoordinatorEntity):
         if ADVANTAGE_AIR_SET_COUNTDOWN_VALUE in kwargs:
             value = min(720, max(0, int(kwargs[ADVANTAGE_AIR_SET_COUNTDOWN_VALUE])))
             await self.async_change(
-                {self.ac_index: {"info": {f"countDownTo{self.time_period}": value}}}
+                {self.ac_key: {"info": {f"countDownTo{self.time_period}": value}}}
             )
 
 
 class AdvantageAirZoneVent(CoordinatorEntity):
     """Representation of Advantage Air Zone Vent Sensor."""
 
-    def __init__(self, instance, ac_index, zone_index):
+    def __init__(self, instance, ac_key, zone_key):
         """Initialize the Advantage Air Zone Vent Sensor."""
         super().__init__(instance["coordinator"])
         self.async_change = instance["async_change"]
-        self.ac_index = ac_index
-        self.zone_index = zone_index
+        self.ac_key = ac_key
+        self.zone_key = zone_key
+
+    @property
+    def _zone(self):
+        return self.coordinator.data["aircons"][self.ac_key]["zones"][self.zone_key]
 
     @property
     def name(self):
         """Return the name."""
-        return f'{self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index]["name"]} Vent'
+        return f'{self._zone["name"]} Vent'
 
     @property
     def unique_id(self):
         """Return a unique id."""
-        return f'{self.coordinator.data["system"]["rid"]}-{self.ac_index}-{self.zone_index}-sensor:vent'
+        return f'{self.coordinator.data["system"]["rid"]}-{self.ac_key}-{self.zone_key}-sensor:vent'
 
     @property
     def state(self):
         """Return the current value of the air vent."""
-        if (
-            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
-                "state"
-            ]
-            == ADVANTAGE_AIR_STATE_OPEN
-        ):
-            return self.coordinator.data["aircons"][self.ac_index]["zones"][
-                self.zone_index
+        if self._zone["state"] == ADVANTAGE_AIR_STATE_OPEN:
+            return self.coordinator.data["aircons"][self.ac_key]["zones"][
+                self.zone_key
             ]["value"]
         return 0
 
@@ -150,10 +138,7 @@ class AdvantageAirZoneVent(CoordinatorEntity):
     def icon(self):
         """Return a representative icon."""
         return ["mdi:fan-off", "mdi:fan"][
-            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
-                "state"
-            ]
-            == ADVANTAGE_AIR_STATE_OPEN
+            self._zone["state"] == ADVANTAGE_AIR_STATE_OPEN
         ]
 
     @property
@@ -171,30 +156,32 @@ class AdvantageAirZoneVent(CoordinatorEntity):
 class AdvantageAirZoneSignal(CoordinatorEntity):
     """Representation of Advantage Air Zone wireless signal sensor."""
 
-    def __init__(self, instance, ac_index, zone_index):
+    def __init__(self, instance, ac_key, zone_key):
         """Initialize the Advantage Air Zone wireless signal sensor."""
         super().__init__(instance["coordinator"])
         self.async_change = instance["async_change"]
         self.device = instance["device"]
-        self.ac_index = ac_index
-        self.zone_index = zone_index
+        self.ac_key = ac_key
+        self.zone_key = zone_key
+
+    @property
+    def _zone(self):
+        return self.coordinator.data["aircons"][self.ac_key]["zones"][self.zone_key]
 
     @property
     def name(self):
         """Return the name."""
-        return f'{self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index]["name"]} Signal'
+        return f'{self._zone["name"]} Signal'
 
     @property
     def unique_id(self):
         """Return a unique id."""
-        return f'{self.coordinator.data["system"]["rid"]}-{self.ac_index}-{self.zone_index}-signal'
+        return f'{self.coordinator.data["system"]["rid"]}-{self.ac_key}-{self.zone_key}-signal'
 
     @property
     def state(self):
         """Return the current value of the wireless signal."""
-        return self.coordinator.data["aircons"][self.ac_index]["zones"][
-            self.zone_index
-        ]["rssi"]
+        return self._zone["rssi"]
 
     @property
     def unit_of_measurement(self):
@@ -204,33 +191,13 @@ class AdvantageAirZoneSignal(CoordinatorEntity):
     @property
     def icon(self):
         """Return a representative icon."""
-        if (
-            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
-                "rssi"
-            ]
-            >= 80
-        ):
+        if self._zone["rssi"] >= 80:
             return "mdi:wifi-strength-4"
-        if (
-            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
-                "rssi"
-            ]
-            >= 60
-        ):
+        if self._zone["rssi"] >= 60:
             return "mdi:wifi-strength-3"
-        if (
-            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
-                "rssi"
-            ]
-            >= 40
-        ):
+        if self._zone["rssi"] >= 40:
             return "mdi:wifi-strength-2"
-        if (
-            self.coordinator.data["aircons"][self.ac_index]["zones"][self.zone_index][
-                "rssi"
-            ]
-            >= 20
-        ):
+        if self._zone["rssi"] >= 20:
             return "mdi:wifi-strength-1"
         return "mdi:wifi-strength-outline"
 
