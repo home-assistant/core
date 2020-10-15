932f394a8b40032fbffd96d2edb85bcfb5cccb81
abmantis <amfcalt@gmail.com>
2020-10-15 22:48:39 +0000

    Fix loading from cache condition

--- homeassistant/components/zha/core/channels/general.py
+++ homeassistant/components/zha/core/channels/general.py
@@ -89,7 +89,7 @@ class BasicChannel(ZigbeeChannel):
 
     async def async_initialize(self, from_cache):
         """Initialize channel."""
-        if not from_cache and not self._ch_pool.skip_configuration:
+        if not self._ch_pool.skip_configuration or from_cache:
             power_source = await self.get_attribute_value(
                 "power_source", from_cache=from_cache
             )
