141bde10e4cc707e78c81d6629e0799b680d9671
Ron Klinkien <ron@cyberjunky.nl>
2020-10-15 14:17:24 +0200

    Corrected static variable name
    Co-authored-by: Robert Svensson <Kane610@users.noreply.github.com>

--- homeassistant/components/fireservicerota/__init__.py
+++ homeassistant/components/fireservicerota/__init__.py
@@ -25,7 +25,7 @@ MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
 
 _LOGGER = logging.getLogger(__name__)
 
-ENTITY_COMPONENTS = {SENSOR_DOMAIN}
+SUPPORTED_PLATFORMS = {SENSOR_DOMAIN}
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
