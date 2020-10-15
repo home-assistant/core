fbae83a4058edd73f894c63d44ba313ea16ee26c
Gabe Cook <gabe565@gmail.com>
2020-10-15 05:49:43 -0500

    Ruckus Unleashed Requested Changes (#41824)

--- tests/components/ruckus_unleashed/test_device_tracker.py
+++ tests/components/ruckus_unleashed/test_device_tracker.py
@@ -1,15 +1,20 @@
 """The sensor tests for the Ruckus Unleashed platform."""
 from datetime import timedelta
 
-from homeassistant.components.ruckus_unleashed import DOMAIN
-from homeassistant.const import CONF_MAC, STATE_HOME, STATE_NOT_HOME, STATE_UNAVAILABLE
+from homeassistant.components.ruckus_unleashed import API_MAC, DOMAIN
+from homeassistant.components.ruckus_unleashed.const import API_AP, API_ID, API_NAME
+from homeassistant.const import STATE_HOME, STATE_NOT_HOME, STATE_UNAVAILABLE
 from homeassistant.helpers import entity_registry
+from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
 from homeassistant.util import utcnow
 
 from tests.async_mock import patch
 from tests.common import async_fire_time_changed
 from tests.components.ruckus_unleashed import (
+    DEFAULT_AP_INFO,
+    DEFAULT_SYSTEM_INFO,
     DEFAULT_TITLE,
+    DEFAULT_UNIQUE_ID,
     TEST_CLIENT,
     TEST_CLIENT_ENTITY_ID,
     init_integration,
@@ -23,17 +28,17 @@ async def test_client_connected(hass):
 
     future = utcnow() + timedelta(minutes=60)
     with patch(
-        "homeassistant.components.ruckus_unleashed.Ruckus.clients",
+        "homeassistant.components.ruckus_unleashed.RuckusUnleashedDataUpdateCoordinator._fetch_clients",
         return_value={
-            TEST_CLIENT[CONF_MAC]: TEST_CLIENT,
+            TEST_CLIENT[API_MAC]: TEST_CLIENT,
         },
     ):
         async_fire_time_changed(hass, future)
         await hass.async_block_till_done()
-
         await hass.helpers.entity_component.async_update_entity(TEST_CLIENT_ENTITY_ID)
-        test_client = hass.states.get(TEST_CLIENT_ENTITY_ID)
-        assert test_client.state == STATE_HOME
+
+    test_client = hass.states.get(TEST_CLIENT_ENTITY_ID)
+    assert test_client.state == STATE_HOME
 
 
 async def test_client_disconnected(hass):
@@ -42,7 +47,7 @@ async def test_client_disconnected(hass):
 
     future = utcnow() + timedelta(minutes=60)
     with patch(
-        "homeassistant.components.ruckus_unleashed.Ruckus.clients",
+        "homeassistant.components.ruckus_unleashed.RuckusUnleashedDataUpdateCoordinator._fetch_clients",
         return_value={},
     ):
         async_fire_time_changed(hass, future)
@@ -59,7 +64,7 @@ async def test_clients_update_failed(hass):
 
     future = utcnow() + timedelta(minutes=60)
     with patch(
-        "homeassistant.components.ruckus_unleashed.Ruckus.clients",
+        "homeassistant.components.ruckus_unleashed.RuckusUnleashedDataUpdateCoordinator._fetch_clients",
         side_effect=ConnectionError,
     ):
         async_fire_time_changed(hass, future)
@@ -79,7 +84,7 @@ async def test_restoring_clients(hass):
     registry.async_get_or_create(
         "device_tracker",
         DOMAIN,
-        TEST_CLIENT[CONF_MAC],
+        DEFAULT_UNIQUE_ID,
         suggested_object_id="ruckus_test_device",
         config_entry=entry,
     )
@@ -91,7 +96,13 @@ async def test_restoring_clients(hass):
         "homeassistant.components.ruckus_unleashed.Ruckus.mesh_name",
         return_value=DEFAULT_TITLE,
     ), patch(
-        "homeassistant.components.ruckus_unleashed.Ruckus.clients",
+        "homeassistant.components.ruckus_unleashed.Ruckus.system_info",
+        return_value=DEFAULT_SYSTEM_INFO,
+    ), patch(
+        "homeassistant.components.ruckus_unleashed.Ruckus.ap_info",
+        return_value=DEFAULT_AP_INFO,
+    ), patch(
+        "homeassistant.components.ruckus_unleashed.RuckusUnleashedDataUpdateCoordinator._fetch_clients",
         return_value={},
     ):
         entry.add_to_hass(hass)
@@ -101,3 +112,24 @@ async def test_restoring_clients(hass):
     device = hass.states.get(TEST_CLIENT_ENTITY_ID)
     assert device is not None
     assert device.state == STATE_NOT_HOME
+
+
+async def test_client_device_setup(hass):
+    """Test a client device is created."""
+    await init_integration(hass)
+
+    router_info = DEFAULT_AP_INFO[API_AP][API_ID]["1"]
+
+    device_registry = await hass.helpers.device_registry.async_get_registry()
+    client_device = device_registry.async_get_device(
+        identifiers={},
+        connections={(CONNECTION_NETWORK_MAC, TEST_CLIENT[API_MAC])},
+    )
+    router_device = device_registry.async_get_device(
+        identifiers={(CONNECTION_NETWORK_MAC, router_info[API_MAC])},
+        connections={(CONNECTION_NETWORK_MAC, router_info[API_MAC])},
+    )
+
+    assert client_device
+    assert client_device.name == TEST_CLIENT[API_NAME]
+    assert client_device.via_device_id == router_device.id
