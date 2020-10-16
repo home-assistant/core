74a1b75919ab6aef88119cf49dccab2046fc528e
Aaron Bach <bachya1208@gmail.com>
2020-10-16 15:35:13 -0600

    Deprecate YAML config for RainMachine (0.119 removal) (#41971)

--- homeassistant/components/rainmachine/config_flow.py
+++ homeassistant/components/rainmachine/config_flow.py
@@ -46,10 +46,6 @@ class RainMachineFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors if errors else {},
         )
 
-    async def async_step_import(self, import_config):
-        """Import a config entry from configuration.yaml."""
-        return await self.async_step_user(import_config)
-
     async def async_step_user(self, user_input=None):
         """Handle the start of the config flow."""
         if not user_input:
