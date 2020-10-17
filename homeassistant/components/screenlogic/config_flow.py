49f66ac05e74eefab3a092be8271751736a683c8
dieselrabbit <37058192+dieselrabbit@users.noreply.github.com>
2020-10-17 16:05:39 -0700

    Starting updates to scaffold

--- homeassistant/components/screenlogic/config_flow.py
+++ homeassistant/components/screenlogic/config_flow.py
@@ -1,5 +1,5 @@
 """Config flow for Screenlogic."""
-import my_pypi_dependency
+import screenlogicpy
 
 from homeassistant import config_entries
 from homeassistant.helpers import config_entry_flow
@@ -10,10 +10,14 @@ from .const import DOMAIN
 async def _async_has_devices(hass) -> bool:
     """Return if there are devices that can be discovered."""
     # TODO Check if there are any devices that can be discovered in the network.
-    devices = await hass.async_add_executor_job(my_pypi_dependency.discover)
+    devices = await hass.async_add_executor_job(screenlogicpy.discover)
     return len(devices) > 0
 
 
 config_entry_flow.register_discovery_flow(
-    DOMAIN, "Screenlogic", _async_has_devices, config_entries.CONN_CLASS_UNKNOWN
+    DOMAIN, "Screenlogic", _async_has_devices, config_entries.CONN_CLASS_LOCAL_POLL
 )
+
+
+class ScreenlogicConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Example config flow."""
