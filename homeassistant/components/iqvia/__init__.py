7fe6425767e7d2e2a463058dc620b6c675fb3528
Aaron Bach <bachya1208@gmail.com>
2020-10-16 18:13:09 -0600

    Better re-raise

--- homeassistant/components/iqvia/__init__.py
+++ homeassistant/components/iqvia/__init__.py
@@ -58,7 +58,7 @@ async def async_setup_entry(hass, entry):
         try:
             return await api_coro()
         except IQVIAError as err:
-            raise UpdateFailed(f"Retrieval of {api_coro} data failed") from err
+            raise UpdateFailed from err
 
     init_data_update_tasks = []
     for sensor_type, api_coro in [
