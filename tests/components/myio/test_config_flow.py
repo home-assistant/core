f91b2e2904874dc96b0cba3f2acadf0f6f644682
Smarthome Ninja <info@smarthome.ninja>
2020-10-17 17:01:54 +0200

    -test

--- tests/components/myio/test_config_flow.py
+++ tests/components/myio/test_config_flow.py
@@ -64,7 +64,7 @@ async def test_form_already_configured(hass):
         side_effect=AlreadyConfigured,
     ), patch(
         "homeassistant.components.myio.config_flow.MyIOHub.already_check",
-        test_false=False,
+        test_false=True,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"], TEST_DATA
