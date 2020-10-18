9f92c9e29eecdb0c8d3bcc6877d02975247cbc70
Aaron Bach <bachya1208@gmail.com>
2020-10-18 10:58:44 -0600

    Cleanup

--- homeassistant/components/simplisafe/lock.py
+++ homeassistant/components/simplisafe/lock.py
@@ -33,6 +33,7 @@ class SimpliSafeLock(SimpliSafeEntity, LockEntity):
         """Initialize."""
         super().__init__(simplisafe, system, lock.name, serial=lock.serial)
         self._lock = lock
+        self._is_locked = None
 
         for event_type in (EVENT_LOCK_LOCKED, EVENT_LOCK_UNLOCKED):
             self.websocket_events_to_listen_for.append(event_type)
@@ -50,8 +51,6 @@ class SimpliSafeLock(SimpliSafeEntity, LockEntity):
             LOGGER.error('Error while locking "%s": %s', self._lock.name, err)
             return
 
-        self._is_locked = True
-
     async def async_unlock(self, **kwargs):
         """Unlock the lock."""
         try:
@@ -59,8 +58,6 @@ class SimpliSafeLock(SimpliSafeEntity, LockEntity):
         except SimplipyError as err:
             LOGGER.error('Error while unlocking "%s": %s', self._lock.name, err)
             return
-
-        self._is_locked = False
 
     @callback
     def async_update_from_rest_api(self):
