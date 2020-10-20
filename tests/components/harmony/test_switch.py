c948d758885c38052b9ff2662ce5acda599ecc74
Mike Keesey <michael.w.keesey@gmail.com>
2020-10-20 06:02:07 -0600

    Additional tests for switch transitions

--- tests/components/harmony/test_switch.py
+++ tests/components/harmony/test_switch.py
@@ -2,10 +2,11 @@
 
 import logging
 
-from homeassistant.components.harmony.const import DOMAIN
+from homeassistant.components.harmony.const import DOMAIN, SIGNAL_UPDATE_ACTIVITY
 
 # from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN, SERVICE_TURN_OFF
 from homeassistant.const import CONF_HOST, CONF_NAME, STATE_OFF, STATE_ON
+from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 from tests.async_mock import patch
 from tests.common import MockConfigEntry
@@ -13,14 +14,11 @@ from tests.common import MockConfigEntry
 # TODO centralize
 from tests.components.harmony.test_config_flow import _get_mock_harmonyclient
 
-# from homeassistant.helpers.dispatcher import async_dispatcher_send
-
-
 _LOGGER = logging.getLogger(__name__)
 
 
 async def test_switch_state_transitions(hass):
-    """Test state transitions for harmony activity switches."""
+    """Test state transitions for harmony activity switches. These signals are sent by callbacks registered by the remote."""
     entry = MockConfigEntry(
         domain=DOMAIN, data={CONF_HOST: "192.0.2.0", CONF_NAME: "Guest Room"}
     )
@@ -39,15 +37,45 @@ async def test_switch_state_transitions(hass):
     assert hass.states.is_state("switch.guest_room_watch_tv", STATE_ON)
     assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
 
-    # _LOGGER.info("WAFFLES %s", hass.services.async_call)
-    # call = hass.services.async_call(
-    # SWITCH_DOMAIN,
-    # SERVICE_TURN_OFF,
-    # { ATTR_ENTITY_ID: "switch.guest_room_watch_tv"},
-    # blocking=True,
-    # )
-    # _LOGGER.info("WAFFLES %s", call)
-    # await call
+    # Harmony Hub is turned off
+    async_dispatcher_send(
+        hass,
+        f"{SIGNAL_UPDATE_ACTIVITY}-{entry.unique_id}",
+        {"current_activity": -1},
+    )
+    await hass.async_block_till_done()
 
-    # assert hass.states.is_state("switch.guest_room_watch_tv", STATE_OFF)
-    # assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
+    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_OFF)
+    assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
+    # Play Music is turned on
+    async_dispatcher_send(
+        hass,
+        f"{SIGNAL_UPDATE_ACTIVITY}-{entry.unique_id}",
+        {"current_activity": "Play Music"},
+    )
+    await hass.async_block_till_done()
+
+    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_OFF)
+    assert hass.states.is_state("switch.guest_room_play_music", STATE_ON)
+
+    # Watch TV is turned on
+    async_dispatcher_send(
+        hass,
+        f"{SIGNAL_UPDATE_ACTIVITY}-{entry.unique_id}",
+        {"current_activity": "Watch TV"},
+    )
+    await hass.async_block_till_done()
+
+    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_ON)
+    assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
+
+    # Unknown activity is turned on
+    async_dispatcher_send(
+        hass,
+        f"{SIGNAL_UPDATE_ACTIVITY}-{entry.unique_id}",
+        {"current_activity": "Some Other Activity"},
+    )
+    await hass.async_block_till_done()
+
+    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_OFF)
+    assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
