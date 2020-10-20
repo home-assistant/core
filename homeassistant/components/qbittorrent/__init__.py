856133e14454733d025c1ab18b4f9cef419bb27c
Geoffrey Lagaisse <geoffreylagaisse@gmail.com>
2020-10-20 21:33:20 +0000

    Cleaned up some constants, ready for the real work

--- homeassistant/components/qbittorrent/__init__.py
+++ homeassistant/components/qbittorrent/__init__.py
@@ -1,1 +1,38 @@
 """The qbittorrent component."""
+# import asyncio
+# import logging
+
+# from qbittorrent.client import Client, LoginRequired
+# from requests.exceptions import RequestException
+# import voluptuous as vol
+
+# from homeassistant.components.sensor import PLATFORM_SCHEMA
+# from homeassistant.const import (
+#     CONF_MONITORED_VARIABLES,
+#     CONF_NAME,
+#     CONF_PASSWORD,
+#     CONF_URL,
+#     CONF_USERNAME,
+#     DATA_RATE_KILOBYTES_PER_SECOND,
+#     STATE_IDLE,
+# )
+# from homeassistant.exceptions import PlatformNotReady
+# import homeassistant.helpers.config_validation as cv
+# from homeassistant.helpers.entity import Entity
+
+# from .const import (
+#     SENSOR_TYPE_CURRENT_STATUS,
+#     SENSOR_TYPE_DOWNLOAD_SPEED,
+#     SENSOR_TYPE_UPLOAD_SPEED,
+#     SENSOR_TYPE_TOTAL_TORRENTS,
+#     SENSOR_TYPE_ACTIVE_TORRENTS,
+#     SENSOR_TYPE_INACTIVE_TORRENTS,
+#     SENSOR_TYPE_DOWNLOADING_TORRENTS,
+#     SENSOR_TYPE_SEEDING_TORRENTS,
+#     SENSOR_TYPE_RESUMED_TORRENTS,
+#     SENSOR_TYPE_PAUSED_TORRENTS,
+#     SENSOR_TYPE_COMPLETED_TORRENTS,
+#     DEFAULT_NAME,
+#     TRIM_SIZE,
+#     CONF_CATEGORIES,
+# )
---
+++ homeassistant/components/qbittorrent/const.py
@@ -0,0 +1,21 @@
+"""Added constants for the qbittorrent component."""
+
+from datetime import timedelta
+
+SENSOR_TYPE_CURRENT_STATUS = "current_status"
+SENSOR_TYPE_DOWNLOAD_SPEED = "download_speed"
+SENSOR_TYPE_UPLOAD_SPEED = "upload_speed"
+SENSOR_TYPE_TOTAL_TORRENTS = "total_torrents"
+SENSOR_TYPE_ACTIVE_TORRENTS = "active_torrents"
+SENSOR_TYPE_INACTIVE_TORRENTS = "inactive_torrents"
+SENSOR_TYPE_DOWNLOADING_TORRENTS = "downloading_torrents"
+SENSOR_TYPE_SEEDING_TORRENTS = "seeding_torrents"
+SENSOR_TYPE_RESUMED_TORRENTS = "resumed_torrents"
+SENSOR_TYPE_PAUSED_TORRENTS = "paused_torrents"
+SENSOR_TYPE_COMPLETED_TORRENTS = "completed_torrents"
+
+DEFAULT_NAME = "qBittorrent"
+TRIM_SIZE = 35
+CONF_CATEGORIES = "categories"
+
+SCAN_INTERVAL = timedelta(minutes=1)
