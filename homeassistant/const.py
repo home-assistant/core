811d891c8241e8c63a6b42b37d0f0ac143d1e1fa
Franck Nijhof <git@frenck.dev>
2020-10-21 22:06:18 +0200

    Bump version to 0.118.0dev0 (#42178)

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -1,6 +1,6 @@
 """Constants used by Home Assistant components."""
 MAJOR_VERSION = 0
-MINOR_VERSION = 117
+MINOR_VERSION = 118
 PATCH_VERSION = "0.dev0"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
