5672f52aa20b24a2466b402cd11512882934a9f8
Guido Schmitz <guido.schmitz@fedaix.de>
2020-10-21 14:54:16 +0000

    Add first two testcases

--- tests/components/devolo_home_control/test_init.py
+++ tests/components/devolo_home_control/test_init.py
@@ -3,42 +3,50 @@ import pytest
 
 from homeassistant.components.devolo_home_control import async_setup_entry
 from homeassistant.components.devolo_home_control.const import DOMAIN
-from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
 from tests.async_mock import patch
-from tests.components.devolo_home_control import configure_integration
+from tests.common import MockConfigEntry
 
 
-async def test_setup_entry(hass: HomeAssistant):
-    """Test setup entry."""
-    entry = configure_integration(hass)
-    with patch("homeassistant.components.devolo_home_control.HomeControl"):
-        assert await hass.config_entries.async_setup(entry.entry_id)
-        assert hass.data[DOMAIN]
-
-
-@pytest.mark.credentials_invalid
-async def test_setup_entry_credentials_invalid(hass: HomeAssistant):
+async def test_setup_entry_credentials_invalid(hass):
     """Test setup entry fails if credentials are invalid."""
-    entry = configure_integration(hass)
-    assert not await hass.config_entries.async_setup(entry.entry_id)
+    config = {
+        "username": "test-username",
+        "password": "test-password",
+        "home_control_url": "https://test_url.test",
+        "mydevolo_url": "https://test_mydevolo_url.test",
+    }
+    entry = MockConfigEntry(domain=DOMAIN, data=config)
+    entry.add_to_hass(hass)
+
+    with patch(
+        "homeassistant.components.devolo_home_control.Mydevolo.credentials_valid",
+        return_value=False,
+    ):
+        await hass.config_entries.async_setup(entry.entry_id)
+
     assert not hass.data[DOMAIN]
 
 
-@pytest.mark.maintenance
-async def test_setup_entry_maintenance(hass: HomeAssistant):
+async def test_setup_entry_maintenance(hass):
     """Test setup entry fails if mydevolo is in maintenance mode."""
-    entry = configure_integration(hass)
-    with pytest.raises(ConfigEntryNotReady):
-        await async_setup_entry(hass, entry)
+    config = {
+        "username": "test-username",
+        "password": "test-password",
+        "home_control_url": "https://test_url.test",
+        "mydevolo_url": "https://test_mydevolo_url.test",
+    }
+    entry = MockConfigEntry(domain=DOMAIN, data=config)
+    entry.add_to_hass(hass)
 
-
-async def test_setup_connection_error(hass: HomeAssistant):
-    """Test setup entry fails on connection error."""
-    entry = configure_integration(hass)
     with patch(
-        "devolo_home_control_api.homecontrol.HomeControl.__init__",
-        side_effect=ConnectionError,
-    ), pytest.raises(ConfigEntryNotReady):
+        "homeassistant.components.devolo_home_control.Mydevolo.credentials_valid",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.devolo_home_control.Mydevolo.maintenance",
+        return_value=True,
+    ), pytest.raises(
+        ConfigEntryNotReady
+    ):
         await async_setup_entry(hass, entry)
