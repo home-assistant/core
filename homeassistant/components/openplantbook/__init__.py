0f8c735eca177451cc1833a0182630b4031d89e5
Ola Thoresen <ola@nlogic.no>
2020-10-23 09:56:46 +0200

    Moving API-calls to an external package (pyopenplantbook)

--- homeassistant/components/openplantbook/__init__.py
+++ homeassistant/components/openplantbook/__init__.py
@@ -1,14 +1,14 @@
 """The OpenPlantBook integration."""
 import logging
 
+from openplantbook import OpenPlantBookApi
 import voluptuous as vol
 
 from homeassistant import exceptions
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN, PLANTBOOK_BASEURL
+from .const import DOMAIN
 
 CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 _LOGGER = logging.getLogger(__name__)
@@ -23,10 +23,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up OpenPlantBook from a config entry."""
 
     if DOMAIN not in hass.data:
+        _LOGGER.debug("Creating domain in init")
         hass.data[DOMAIN] = {}
-
-    hass.data[DOMAIN]["PLANTBOOK_TOKEN"] = entry.data.get("token")
-    hass.data[DOMAIN]["API"] = OpenPlantBookApi(hass)
+    if "API" not in hass.data[DOMAIN]:
+        hass.data[DOMAIN]["API"] = OpenPlantBookApi(
+            entry.data.get("client_id"), entry.data.get("secret")
+        )
 
     return True
 
@@ -36,57 +38,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.pop(DOMAIN)
 
     return True
-
-
-class OpenPlantBookApi:
-    """Fetches data from the OpenPlantbook API."""
-
-    def __init__(self, hass):
-        """Initialize."""
-        self.hass = hass
-
-    async def get_plantbook_token(self, client_id, secret):
-        """Get the token from the openplantbook API."""
-        if not client_id or not secret:
-            return False
-        token = None
-        url = f"{PLANTBOOK_BASEURL}/token/"
-        data = {
-            "grant_type": "client_credentials",
-            "client_id": client_id,
-            "client_secret": secret,
-        }
-        try:
-            session = async_get_clientsession(self.hass)
-            async with session.post(url, data=data) as result:
-                token = await result.json()
-                if token.get("access_token"):
-                    _LOGGER.debug("Got token from %s", url)
-                    return token
-
-                raise InvalidAuth
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.error("Unable to connect to openplantbook")
-            raise
-
-    async def get_plantbook_data(self, species):
-        """Get information about the plant from the openplantbook API."""
-        if not self.hass.data[DOMAIN]["PLANTBOOK_TOKEN"]:
-            _LOGGER.debug("No plantbook token")
-            return
-        url = f"{PLANTBOOK_BASEURL}/plant/detail/{species}"
-        headers = {
-            "Authorization": f"Bearer {self.hass.data[DOMAIN]['PLANTBOOK_TOKEN'].get('access_token')}"
-        }
-        try:
-            session = async_get_clientsession(self.hass)
-            async with session.get(url, headers=headers) as result:
-                _LOGGER.debug("Fetched data from %s", url)
-                res = await result.json()
-                return res
-        except Exception as exception:  # pylint: disable=broad-except
-            _LOGGER.error("Unable to get plant from plantbook API: %s", str(exception))
-        return False
 
 
 class CannotConnect(exceptions.HomeAssistantError):
