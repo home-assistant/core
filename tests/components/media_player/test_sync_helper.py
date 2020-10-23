ee8d060e2cbb5f9d427b4e04b9b2bf3960f73627
Tim Werner <tim.werner@gmx.com>
2020-10-23 15:12:09 +0200

    Rewrite media_player unittest tests to pytest style test functions

--- tests/components/media_player/test_sync_helper.py
+++ tests/components/media_player/test_sync_helper.py
@@ -1,13 +1,19 @@
-"""The tests for the Async Media player helper functions."""
+"""The tests for the Sync Media player helper functions."""
 
 import pytest
 
 import homeassistant.components.media_player as mp
-from homeassistant.const import STATE_OFF, STATE_ON, STATE_PAUSED, STATE_PLAYING
+from homeassistant.const import (
+    STATE_IDLE,
+    STATE_OFF,
+    STATE_ON,
+    STATE_PAUSED,
+    STATE_PLAYING,
+)
 
 
-class AsyncMediaPlayer(mp.MediaPlayerEntity):
-    """Async media player test class."""
+class SyncMediaPlayer(mp.MediaPlayerEntity):
+    """Sync media player test class."""
 
     def __init__(self, hass):
         """Initialize the test media player."""
@@ -30,59 +36,82 @@ class AsyncMediaPlayer(mp.MediaPlayerEntity):
         """Flag media player features that are supported."""
         return (
             mp.const.SUPPORT_VOLUME_SET
+            | mp.const.SUPPORT_VOLUME_STEP
             | mp.const.SUPPORT_PLAY
             | mp.const.SUPPORT_PAUSE
             | mp.const.SUPPORT_TURN_OFF
             | mp.const.SUPPORT_TURN_ON
         )
 
-    async def async_set_volume_level(self, volume):
+    def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
         self._volume = volume
 
-    async def async_media_play(self):
-        """Send play command."""
-        self._state = STATE_PLAYING
+    def volume_up(self):
+        """Turn volume up for media player."""
+        if self.volume_level < 1:
+            self.set_volume_level(min(1, self.volume_level + 0.2))
 
-    async def async_media_pause(self):
-        """Send pause command."""
-        self._state = STATE_PAUSED
+    def volume_down(self):
+        """Turn volume down for media player."""
+        if self.volume_level > 0:
+            self.set_volume_level(max(0, self.volume_level - 0.2))
 
-    async def async_turn_on(self):
-        """Turn the media player on."""
-        self._state = STATE_ON
+    def media_play_pause(self):
+        """Play or pause the media player."""
+        if self._state == STATE_PLAYING:
+            self._state = STATE_PAUSED
+        else:
+            self._state = STATE_PLAYING
 
-    async def async_turn_off(self):
-        """Turn the media player off."""
-        self._state = STATE_OFF
+    def toggle(self):
+        """Toggle the power on the media player."""
+        if self._state in [STATE_OFF, STATE_IDLE]:
+            self._state = STATE_ON
+        else:
+            self._state = STATE_OFF
+
+    async def async_media_play_pause(self):
+        """Create a coroutine to wrap the future returned by ABC.
+
+        This allows the run_coroutine_threadsafe helper to be used.
+        """
+        await super().async_media_play_pause()
+
+    async def async_toggle(self):
+        """Create a coroutine to wrap the future returned by ABC.
+
+        This allows the run_coroutine_threadsafe helper to be used.
+        """
+        await super().async_toggle()
 
 
 @pytest.fixture
 def player(hass):
-    """Set up async player to be run when tests are started."""
-    return AsyncMediaPlayer(hass)
+    """Set up sync player to be run when tests are started."""
+    return SyncMediaPlayer(hass)
 
 
 async def test_volume_up(hass, player):
     """Test the volume_up helper function."""
     assert player.volume_level == 0
 
-    await player.async_set_volume_level(0.5)
+    player.set_volume_level(0.5)
     assert player.volume_level == 0.5
 
     await player.async_volume_up()
-    assert player.volume_level == 0.6
+    assert player.volume_level == 0.7
 
 
 async def test_volume_down(hass, player):
     """Test the volume_down helper function."""
     assert player.volume_level == 0
 
-    await player.async_set_volume_level(0.5)
+    player.set_volume_level(0.5)
     assert player.volume_level == 0.5
 
     await player.async_volume_down()
-    assert player.volume_level == 0.4
+    assert player.volume_level == 0.3
 
 
 async def test_media_play_pause(hass, player):
@@ -91,7 +120,6 @@ async def test_media_play_pause(hass, player):
 
     await player.async_media_play_pause()
     assert player.state == STATE_PLAYING
-
     await player.async_media_play_pause()
     assert player.state == STATE_PAUSED
 
