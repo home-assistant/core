fd2a1275ba329767d38af75054eb3e7926d6973a
cgtobi <cgtobi@users.noreply.github.com>
2020-10-24 14:37:28 +0200

    Update homeassistant/components/netatmo/sensor.py
    Co-authored-by: Chris Talkington <chris@talkingtontech.com>

--- homeassistant/components/netatmo/sensor.py
+++ homeassistant/components/netatmo/sensor.py
@@ -91,7 +91,7 @@ SENSOR_TYPES = {
     ],
     "reachable": ["Reachability", None, "mdi:signal", None, False],
     "rf_status": ["Radio", None, "mdi:signal", None, False],
-    "rf_status_lvl": ["Radio Level", "dBm", None, DEVICE_CLASS_SIGNAL_STRENGTH, False],
+    "rf_status_lvl": ["Radio Level", SIGNAL_STRENGTH_DECIBELS_MILLIWATT, None, DEVICE_CLASS_SIGNAL_STRENGTH, False],
     "wifi_status": ["Wifi", None, "mdi:wifi", None, False],
     "wifi_status_lvl": ["Wifi Level", "dBm", None, DEVICE_CLASS_SIGNAL_STRENGTH, False],
     "health_idx": ["Health", None, "mdi:cloud", None, True],
