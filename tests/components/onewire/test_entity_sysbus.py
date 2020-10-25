c157a582b85347d9684105e3f4bd46dac20e6142
epenet <6771947+epenet@users.noreply.github.com>
2020-10-25 18:05:02 +0100

    Add device_info to onewire sensors (#42309)
    * Add device info
    
    * Cleanup log

--- tests/components/onewire/test_entity_sysbus.py
+++ tests/components/onewire/test_entity_sysbus.py
@@ -8,7 +8,7 @@ from homeassistant.const import DEVICE_CLASS_TEMPERATURE, TEMP_CELSIUS
 from homeassistant.setup import async_setup_component
 
 from tests.async_mock import patch
-from tests.common import mock_registry
+from tests.common import mock_device_registry, mock_registry
 
 MOCK_CONFIG = {
     SENSOR_DOMAIN: {
@@ -23,6 +23,12 @@ MOCK_CONFIG = {
 MOCK_DEVICE_SENSORS = {
     "00-111111111111": {"sensors": []},
     "10-111111111111": {
+        "device_info": {
+            "identifiers": {(DOMAIN, "10-111111111111")},
+            "manufacturer": "Maxim Integrated",
+            "model": "10",
+            "name": "10-111111111111",
+        },
         "sensors": [
             {
                 "entity_id": "sensor.my_ds18b20_temperature",
@@ -32,11 +38,17 @@ MOCK_DEVICE_SENSORS = {
                 "unit": TEMP_CELSIUS,
                 "class": DEVICE_CLASS_TEMPERATURE,
             },
-        ]
+        ],
     },
     "12-111111111111": {"sensors": []},
     "1D-111111111111": {"sensors": []},
     "22-111111111111": {
+        "device_info": {
+            "identifiers": {(DOMAIN, "22-111111111111")},
+            "manufacturer": "Maxim Integrated",
+            "model": "22",
+            "name": "22-111111111111",
+        },
         "sensors": [
             {
                 "entity_id": "sensor.22_111111111111_temperature",
@@ -46,10 +58,16 @@ MOCK_DEVICE_SENSORS = {
                 "unit": TEMP_CELSIUS,
                 "class": DEVICE_CLASS_TEMPERATURE,
             },
-        ]
+        ],
     },
     "26-111111111111": {"sensors": []},
     "28-111111111111": {
+        "device_info": {
+            "identifiers": {(DOMAIN, "28-111111111111")},
+            "manufacturer": "Maxim Integrated",
+            "model": "28",
+            "name": "28-111111111111",
+        },
         "sensors": [
             {
                 "entity_id": "sensor.28_111111111111_temperature",
@@ -59,9 +77,15 @@ MOCK_DEVICE_SENSORS = {
                 "unit": TEMP_CELSIUS,
                 "class": DEVICE_CLASS_TEMPERATURE,
             },
-        ]
+        ],
     },
     "3B-111111111111": {
+        "device_info": {
+            "identifiers": {(DOMAIN, "3B-111111111111")},
+            "manufacturer": "Maxim Integrated",
+            "model": "3B",
+            "name": "3B-111111111111",
+        },
         "sensors": [
             {
                 "entity_id": "sensor.3b_111111111111_temperature",
@@ -71,9 +95,15 @@ MOCK_DEVICE_SENSORS = {
                 "unit": TEMP_CELSIUS,
                 "class": DEVICE_CLASS_TEMPERATURE,
             },
-        ]
+        ],
     },
     "42-111111111111": {
+        "device_info": {
+            "identifiers": {(DOMAIN, "42-111111111111")},
+            "manufacturer": "Maxim Integrated",
+            "model": "42",
+            "name": "42-111111111111",
+        },
         "sensors": [
             {
                 "entity_id": "sensor.42_111111111111_temperature",
@@ -83,7 +113,7 @@ MOCK_DEVICE_SENSORS = {
                 "unit": TEMP_CELSIUS,
                 "class": DEVICE_CLASS_TEMPERATURE,
             },
-        ]
+        ],
     },
     "EF-111111111111": {
         "sensors": [],
@@ -98,10 +128,13 @@ MOCK_DEVICE_SENSORS = {
 async def test_onewiredirect_setup_valid_device(hass, device_id):
     """Test that sysbus config entry works correctly."""
     entity_registry = mock_registry(hass)
+    device_registry = mock_device_registry(hass)
+
+    mock_device_sensor = MOCK_DEVICE_SENSORS[device_id]
 
     glob_result = [f"/{DEFAULT_SYSBUS_MOUNT_DIR}/{device_id}"]
     read_side_effect = []
-    expected_sensors = MOCK_DEVICE_SENSORS[device_id]["sensors"]
+    expected_sensors = mock_device_sensor["sensors"]
     for expected_sensor in expected_sensors:
         read_side_effect.append(expected_sensor["injected_value"])
 
@@ -118,6 +151,16 @@ async def test_onewiredirect_setup_valid_device(hass, device_id):
         await hass.async_block_till_done()
 
     assert len(entity_registry.entities) == len(expected_sensors)
+
+    if len(expected_sensors) > 0:
+        device_info = mock_device_sensor["device_info"]
+        assert len(device_registry.devices) == 1
+        registry_entry = device_registry.async_get_device({(DOMAIN, device_id)}, set())
+        assert registry_entry is not None
+        assert registry_entry.identifiers == {(DOMAIN, device_id)}
+        assert registry_entry.manufacturer == device_info["manufacturer"]
+        assert registry_entry.name == device_info["name"]
+        assert registry_entry.model == device_info["model"]
 
     for expected_sensor in expected_sensors:
         entity_id = expected_sensor["entity_id"]
