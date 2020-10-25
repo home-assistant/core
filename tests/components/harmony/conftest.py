81097f04d5dd82c81ec5dc31c3c04e4ed421204b
Mike Keesey <michael.w.keesey@gmail.com>
2020-10-24 18:12:04 -0600

    Testing switch state changes

--- tests/components/harmony/conftest.py
+++ tests/components/harmony/conftest.py
@@ -3,18 +3,45 @@ import pytest
 
 from tests.async_mock import AsyncMock, MagicMock, PropertyMock
 
+WATCH_TV_ACTIVITY_ID = 123
+PLAY_MUSIC_ACTIVITY_ID = 456
+
+ACTIVITIES_TO_IDS = {
+    "Watch TV": WATCH_TV_ACTIVITY_ID,
+    "Play Music": PLAY_MUSIC_ACTIVITY_ID,
+}
+
+# class FakeHarmonyStates:
+# def __init__(self):
+# self._activity_name = "Watch TV"
+
+# def get_activity_name(self, *args):
+# return self._activity_name
+
+# async def start_activity(self, *args, **kwargs):
+# print(args)
+# print(kwargs)
+# fut = asyncio.Future()
+# fut.return_value=True
+# return await fut
+
 
 @pytest.fixture()
 def mock_harmonyclient():
     """Create a mock HarmonyClient."""
+    # stateHandler = FakeHarmonyStates()
     harmonyclient_mock = MagicMock()
     type(harmonyclient_mock).connect = AsyncMock()
     type(harmonyclient_mock).close = AsyncMock()
+    # type(harmonyclient_mock).get_activity_name = MagicMock(side_effect=stateHandler.get_activity_name)
     type(harmonyclient_mock).get_activity_name = MagicMock(return_value="Watch TV")
+    type(harmonyclient_mock).get_activity_id = _get_activity_id
+    # type(harmonyclient_mock).start_activity = AsyncMock(side_effect=stateHandler.start_activity, return_value=True)
+    type(harmonyclient_mock).start_activity = AsyncMock()
     type(harmonyclient_mock.hub_config).activities = PropertyMock(
         return_value=[
-            {"name": "Watch TV", "id": 123},
-            {"name": "Play Music", "id": 456},
+            {"name": "Watch TV", "id": WATCH_TV_ACTIVITY_ID},
+            {"name": "Play Music", "id": PLAY_MUSIC_ACTIVITY_ID},
         ]
     )
     type(harmonyclient_mock.hub_config).devices = PropertyMock(
@@ -25,10 +52,14 @@ def mock_harmonyclient():
     type(harmonyclient_mock.hub_config).config = PropertyMock(
         return_value={
             "activity": [
-                {"id": 123, "label": "Watch TV"},
-                {"id": 456, "label": "Play Music"},
+                {"id": WATCH_TV_ACTIVITY_ID, "label": "Watch TV"},
+                {"id": PLAY_MUSIC_ACTIVITY_ID, "label": "Play Music"},
             ]
         }
     )
 
     yield harmonyclient_mock
+
+
+def _get_activity_id(_, activity_name):
+    return ACTIVITIES_TO_IDS.get(activity_name)
--- tests/components/harmony/test_switch.py
+++ tests/components/harmony/test_switch.py
@@ -7,9 +7,13 @@ from homeassistant.components.harmony.const import (
     DOMAIN,
     SIGNAL_UPDATE_ACTIVITY,
 )
-
-# from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN, SERVICE_TURN_OFF
+from homeassistant.components.switch import (
+    DOMAIN as SWITCH_DOMAIN,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+)
 from homeassistant.const import (
+    ATTR_ENTITY_ID,
     CONF_HOST,
     CONF_NAME,
     STATE_OFF,
@@ -20,8 +24,12 @@ from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 from tests.async_mock import patch
 from tests.common import MockConfigEntry
+from tests.components.harmony.conftest import PLAY_MUSIC_ACTIVITY_ID
 
 _LOGGER = logging.getLogger(__name__)
+
+ENTITY_WATCH_TV = "switch.guest_room_watch_tv"
+ENTITY_PLAY_MUSIC = "switch.guest_room_play_music"
 
 
 async def test_state_transitions(hass, mock_harmonyclient):
@@ -39,8 +47,8 @@ async def test_state_transitions(hass, mock_harmonyclient):
         await hass.async_block_till_done()
 
     # mocks start with current activity == Watch TV
-    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_ON)
-    assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
+    assert hass.states.is_state(ENTITY_WATCH_TV, STATE_ON)
+    assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_OFF)
 
     # Harmony Hub is turned off
     async_dispatcher_send(
@@ -50,8 +58,8 @@ async def test_state_transitions(hass, mock_harmonyclient):
     )
     await hass.async_block_till_done()
 
-    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_OFF)
-    assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
+    assert hass.states.is_state(ENTITY_WATCH_TV, STATE_OFF)
+    assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_OFF)
     # Play Music is turned on
     async_dispatcher_send(
         hass,
@@ -60,8 +68,8 @@ async def test_state_transitions(hass, mock_harmonyclient):
     )
     await hass.async_block_till_done()
 
-    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_OFF)
-    assert hass.states.is_state("switch.guest_room_play_music", STATE_ON)
+    assert hass.states.is_state(ENTITY_WATCH_TV, STATE_OFF)
+    assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_ON)
 
     # Watch TV is turned on
     async_dispatcher_send(
@@ -71,8 +79,8 @@ async def test_state_transitions(hass, mock_harmonyclient):
     )
     await hass.async_block_till_done()
 
-    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_ON)
-    assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
+    assert hass.states.is_state(ENTITY_WATCH_TV, STATE_ON)
+    assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_OFF)
 
     # Unknown activity is turned on
     async_dispatcher_send(
@@ -82,8 +90,8 @@ async def test_state_transitions(hass, mock_harmonyclient):
     )
     await hass.async_block_till_done()
 
-    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_OFF)
-    assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
+    assert hass.states.is_state(ENTITY_WATCH_TV, STATE_OFF)
+    assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_OFF)
 
 
 async def test_availability_transitions(hass, mock_harmonyclient):
@@ -101,8 +109,8 @@ async def test_availability_transitions(hass, mock_harmonyclient):
         await hass.async_block_till_done()
 
     # mocks start with current activity == Watch TV
-    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_ON)
-    assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
+    assert hass.states.is_state(ENTITY_WATCH_TV, STATE_ON)
+    assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_OFF)
 
     async_dispatcher_send(
         hass,
@@ -111,8 +119,8 @@ async def test_availability_transitions(hass, mock_harmonyclient):
     )
     await hass.async_block_till_done()
 
-    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_UNAVAILABLE)
-    assert hass.states.is_state("switch.guest_room_play_music", STATE_UNAVAILABLE)
+    assert hass.states.is_state(ENTITY_WATCH_TV, STATE_UNAVAILABLE)
+    assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_UNAVAILABLE)
 
     async_dispatcher_send(
         hass,
@@ -121,5 +129,52 @@ async def test_availability_transitions(hass, mock_harmonyclient):
     )
     await hass.async_block_till_done()
 
-    assert hass.states.is_state("switch.guest_room_watch_tv", STATE_ON)
-    assert hass.states.is_state("switch.guest_room_play_music", STATE_OFF)
+    assert hass.states.is_state(ENTITY_WATCH_TV, STATE_ON)
+    assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_OFF)
+
+
+async def test_switch_toggles(hass, mock_harmonyclient):
+    """Ensure calls to the switch call into the harmony client.
+
+    Note the harmonyclient mock is not currently set up to actually change states.
+    """
+    entry = MockConfigEntry(
+        domain=DOMAIN, data={CONF_HOST: "192.0.2.0", CONF_NAME: "Guest Room"}
+    )
+
+    with patch(
+        "aioharmony.harmonyapi.HarmonyClient",
+        return_value=mock_harmonyclient,
+    ), patch("homeassistant.components.harmony.remote.HarmonyRemote.write_config_file"):
+        entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        SERVICE_TURN_OFF,
+        {ATTR_ENTITY_ID: ENTITY_WATCH_TV},
+        blocking=True,
+    )
+
+    mock_harmonyclient.start_activity.assert_awaited_with(activity_id=-1)
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: ENTITY_PLAY_MUSIC},
+        blocking=True,
+    )
+
+    mock_harmonyclient.start_activity.assert_awaited_with(
+        activity_id=PLAY_MUSIC_ACTIVITY_ID
+    )
+
+    # TODO test turning on and off watch tv
+    # await hass.services.async_call(
+    # SWITCH_DOMAIN,
+    # SERVICE_TURN_OFF,
+    # {ATTR_ENTITY_ID: ENTITY_PLAY_MUSIC},
+    # blocking=True,
+    # )
+
+    # mock_harmonyclient.start_activity.assert_awaited_with(activity_id=PLAY_MUSIC_ACTIVITY_ID)
