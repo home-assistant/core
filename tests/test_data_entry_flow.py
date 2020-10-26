974c04a1f9b1aee3c4b20d9b2ba1d35e34c9853c
Martin Hjelmare <marhje52@gmail.com>
2020-10-26 18:32:44 +0100

    Add info step test

--- tests/test_data_entry_flow.py
+++ tests/test_data_entry_flow.py
@@ -285,6 +285,54 @@ async def test_external_step(hass, manager):
     assert result["title"] == "Hello"
 
 
+async def test_info_step(hass, manager):
+    """Test info step logic."""
+    manager.hass = hass
+
+    @manager.mock_reg_handler("test")
+    class TestFlow(data_entry_flow.FlowHandler):
+        VERSION = 5
+        data = None
+
+        async def async_step_init(self, user_input=None):
+            if not user_input:
+                return self.async_info_step(
+                    step_id="init",
+                )
+
+            self.data = user_input
+            return self.async_info_step_done(next_step_id="finish")
+
+        async def async_step_finish(self, user_input=None):
+            return self.async_create_entry(title=self.data["title"], data=self.data)
+
+    events = async_capture_events(
+        hass, data_entry_flow.EVENT_DATA_ENTRY_FLOW_PROGRESSED
+    )
+
+    result = await manager.async_init("test")
+    assert result["type"] == data_entry_flow.RESULT_TYPE_INFO_STEP
+    assert len(manager.async_progress()) == 1
+
+    # Mimic task finishing and continuing step
+    # Called by integrations: `hass.config_entries.flow.async_configure(…)`
+    result = await manager.async_configure(result["flow_id"], {"title": "Hello"})
+    assert result["type"] == data_entry_flow.RESULT_TYPE_INFO_STEP_DONE
+
+    await hass.async_block_till_done()
+    assert len(events) == 1
+    assert events[0].data == {
+        "handler": "test",
+        "flow_id": result["flow_id"],
+        "refresh": True,
+    }
+
+    # Frontend refreshses the flow
+    result = await manager.async_configure(result["flow_id"])
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == "Hello"
+
+
 async def test_abort_flow_exception(manager):
     """Test that the AbortFlow exception works."""
 
