d263198e5bb533871754d726932505e0f8b8923a
Guido Schmitz <guido.schmitz@fedaix.de>
2020-10-27 20:28:48 +0000

    Use markers

--- tests/components/devolo_home_control/test_init.py
+++ tests/components/devolo_home_control/test_init.py
@@ -11,69 +11,45 @@ from tests.components.devolo_home_control import configure_integration
 
 
 @pytest.mark.usefixtures("patch_mydevolo")
+@pytest.mark.credentials_valid(True)
+@pytest.mark.maintenance(False)
 async def test_setup_entry(hass: HomeAssistant):
     """Test setup entry."""
     entry = configure_integration(hass)
-
-    with patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.maintenance",
-        return_value=False,
-    ), patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.get_gateway_ids",
-        return_value=["1400000000000001", "1400000000000002"],
-    ), patch(
-        "homeassistant.components.devolo_home_control.HomeControl"
-    ):
+    with patch("homeassistant.components.devolo_home_control.HomeControl"):
         assert await hass.config_entries.async_setup(entry.entry_id)
         assert hass.data[DOMAIN]
 
 
+@pytest.mark.usefixtures("patch_mydevolo")
+@pytest.mark.credentials_valid(False)
+@pytest.mark.maintenance(False)
 async def test_setup_entry_credentials_invalid(hass: HomeAssistant):
     """Test setup entry fails if credentials are invalid."""
     entry = configure_integration(hass)
-
-    with patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.credentials_valid",
-        return_value=False,
-    ):
-        assert not await hass.config_entries.async_setup(entry.entry_id)
-        assert not hass.data[DOMAIN]
+    assert not await hass.config_entries.async_setup(entry.entry_id)
+    assert not hass.data[DOMAIN]
 
 
+@pytest.mark.usefixtures("patch_mydevolo")
+@pytest.mark.credentials_valid(True)
+@pytest.mark.maintenance(True)
 async def test_setup_entry_maintenance(hass: HomeAssistant):
     """Test setup entry fails if mydevolo is in maintenance mode."""
     entry = configure_integration(hass)
-
-    with patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.credentials_valid",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.maintenance",
-        return_value=True,
-    ), pytest.raises(
-        ConfigEntryNotReady
-    ):
+    with pytest.raises(ConfigEntryNotReady):
         await async_setup_entry(hass, entry)
 
 
+@pytest.mark.usefixtures("patch_mydevolo")
+@pytest.mark.credentials_valid(True)
+@pytest.mark.maintenance(False)
 async def test_setup_connection_error(hass: HomeAssistant):
     """Test setup entry fails on connection error."""
     entry = configure_integration(hass)
-
     with patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.credentials_valid",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.maintenance",
-        return_value=False,
-    ), patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.get_gateway_ids",
-        return_value=["1400000000000001", "1400000000000002"],
-    ), patch(
         "devolo_home_control_api.homecontrol.HomeControl.__init__",
         return_value=None,
         side_effect=ConnectionError,
-    ), pytest.raises(
-        ConfigEntryNotReady
-    ):
+    ), pytest.raises(ConfigEntryNotReady):
         await async_setup_entry(hass, entry)
