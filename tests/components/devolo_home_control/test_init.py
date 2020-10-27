7865a7b1cb691f053b8e65424339272336c4f85b
Guido Schmitz <guido.schmitz@fedaix.de>
2020-10-27 15:31:00 +0000

    First attempt to use fixtures

--- tests/components/devolo_home_control/test_init.py
+++ tests/components/devolo_home_control/test_init.py
@@ -10,26 +10,12 @@ from tests.async_mock import patch
 from tests.components.devolo_home_control import configure_integration
 
 
-async def test_setup_entry_credentials_invalid(hass: HomeAssistant):
-    """Test setup entry fails if credentials are invalid."""
+@pytest.mark.usefixtures("patch_mydevolo")
+async def test_setup_entry(hass: HomeAssistant):
+    """Test setup entry."""
     entry = configure_integration(hass)
 
     with patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.credentials_valid",
-        return_value=False,
-    ):
-        assert not await hass.config_entries.async_setup(entry.entry_id)
-        assert not hass.data[DOMAIN]
-
-
-async def test_setup_entry_credentials_valid(hass: HomeAssistant):
-    """Test setup entry is correct if credentials are valid."""
-    entry = configure_integration(hass)
-
-    with patch(
-        "homeassistant.components.devolo_home_control.Mydevolo.credentials_valid",
-        return_value=True,
-    ), patch(
         "homeassistant.components.devolo_home_control.Mydevolo.maintenance",
         return_value=False,
     ), patch(
@@ -40,6 +26,18 @@ async def test_setup_entry_credentials_valid(hass: HomeAssistant):
     ):
         assert await hass.config_entries.async_setup(entry.entry_id)
         assert hass.data[DOMAIN]
+
+
+async def test_setup_entry_credentials_invalid(hass: HomeAssistant):
+    """Test setup entry fails if credentials are invalid."""
+    entry = configure_integration(hass)
+
+    with patch(
+        "homeassistant.components.devolo_home_control.Mydevolo.credentials_valid",
+        return_value=False,
+    ):
+        assert not await hass.config_entries.async_setup(entry.entry_id)
+        assert not hass.data[DOMAIN]
 
 
 async def test_setup_entry_maintenance(hass: HomeAssistant):
