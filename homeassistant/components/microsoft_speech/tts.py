52206115a75ce995afc729e97a19ae859046792a
Jean-Paul van Ravensberg <14926452+Cloudenius@users.noreply.github.com>
2020-10-27 14:36:13 +0000

    Add support for voice selection

--- homeassistant/components/microsoft_speech/tts.py
+++ homeassistant/components/microsoft_speech/tts.py
@@ -13,27 +13,37 @@ from azure.cognitiveservices.speech import (
 import voluptuous as vol
 
 from homeassistant.components.tts import CONF_LANG, PLATFORM_SCHEMA, Provider
-from homeassistant.const import CONF_API_KEY
+from homeassistant.const import CONF_API_KEY, CONF_TYPE
 import homeassistant.helpers.config_validation as cv
+
+from .const import (
+    SUPPORTED_LANGUAGES,
+    SUPPORTED_OUTPUTS,
+    SUPPORTED_REGIONS,
+    SUPPORTED_TYPES,
+    SUPPORTED_VOICES,
+)
 
 CONF_OUTPUT = "output"
 CONF_REGION = "region"
 
 _LOGGER = logging.getLogger(__name__)
 
-SUPPORTED_LANGUAGES = [
-    "en-US",
-]
-
-DEFAULT_LANG = "en-us"
+DEFAULT_LANG = "en-US"
+DEFAULT_TYPE = "AriaNeural"
 DEFAULT_OUTPUT = "Audio16Khz128KBitRateMonoMp3"
 DEFAULT_REGION = "eastus"
+DEFAULT_VOICE = f"{DEFAULT_LANG}-{DEFAULT_TYPE}"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_API_KEY): cv.string,
+        vol.Optional(CONF_TYPE, default=DEFAULT_TYPE): cv.string,
         vol.Optional(CONF_REGION, default=DEFAULT_REGION): cv.string,
         vol.Optional(CONF_LANG, default=DEFAULT_LANG): vol.In(SUPPORTED_LANGUAGES),
+        vol.Optional(CONF_TYPE, default=DEFAULT_TYPE): vol.In(SUPPORTED_TYPES),
+        vol.Optional(CONF_OUTPUT, default=DEFAULT_OUTPUT): vol.In(SUPPORTED_OUTPUTS),
+        vol.Optional(CONF_REGION, default=DEFAULT_REGION): vol.In(SUPPORTED_REGIONS),
     }
 )
 
@@ -43,19 +53,23 @@ def get_engine(hass, config, discovery_info=None):
     return MicrosoftProvider(
         config[CONF_API_KEY],
         config[CONF_REGION],
+        config[CONF_OUTPUT],
         config[CONF_LANG],
+        config[CONF_TYPE],
     )
 
 
 class MicrosoftProvider(Provider):
     """The Microsoft Speech API provider."""
 
-    def __init__(self, apikey, region, lang):
+    def __init__(self, apikey, region, output, lang, ttype):
         """Init Microsoft TTS service."""
         self._apikey = apikey
         self._region = region
         self._lang = lang
-        self._output = DEFAULT_OUTPUT
+        self._type = ttype
+        self._voice = f"{lang}-{ttype}"
+        self._output = output
         self.name = "Microsoft Speech"
 
     @property
@@ -68,16 +82,46 @@ class MicrosoftProvider(Provider):
         """Return list of supported languages."""
         return SUPPORTED_LANGUAGES
 
+    @property
+    def supported_types(self):
+        """Return list of supported types."""
+        return SUPPORTED_TYPES
+
+    @property
+    def supported_voices(self):
+        """Return list of supported voices."""
+        return SUPPORTED_VOICES
+
+    @property
+    def supported_outputs(self):
+        """Return list of supported outputs."""
+        return SUPPORTED_OUTPUTS
+
+    @property
+    def supported_regions(self):
+        """Return list of supported regions."""
+        return SUPPORTED_REGIONS
+
     def get_tts_audio(self, message, language, options=None):
         """Load TTS from Microsoft Speech."""
         if language is None:
             language = self._lang
 
+        if self._voice in self.supported_voices:
+            pass
+        else:
+            _LOGGER.error(
+                f"The provided voice {self._voice} is not in the supported types list. Falling back to default voice {DEFAULT_VOICE}"
+            )
+            self._voice = DEFAULT_VOICE
+
         try:
             speech_config = SpeechConfig(subscription=self._apikey, region=self._region)
+
             speech_config.set_speech_synthesis_output_format(
                 SpeechSynthesisOutputFormat[self._output]
             )
+            speech_config.speech_synthesis_voice_name = self._voice
             synthesizer = SpeechSynthesizer(
                 speech_config=speech_config, audio_config=None
             )
