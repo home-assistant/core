61f90991c90c412013d95c2a33cd4af824a9ac68
Jean-Paul van Ravensberg <14926452+Cloudenius@users.noreply.github.com>
2020-10-28 17:44:17 +0000

    Add negative tests

--- tests/components/microsoft_speech/test_tts.py
+++ tests/components/microsoft_speech/test_tts.py
@@ -94,6 +94,90 @@ class TestTTSMicrosoftSpeechPlatform:
         assert calls[0].data[ATTR_MEDIA_CONTENT_ID].find(".wav") != -1
 
     @patch("gtts_token.gtts_token.Token.calculate_token", autospec=True, return_value=5)
+    def test_service_say_error_region(self, mock_calculate):
+        """Test service call say error on region."""
+        calls = mock_service(self.hass, DOMAIN_MP, SERVICE_PLAY_MEDIA)
+
+        config = {
+            tts.DOMAIN: {
+                "platform": "microsoft_speech",
+                "api_key": "123456789abcdefghijklmnopqrstuvwxyz",
+                "region": "nonexistingregion",
+            }
+        }
+
+        with assert_setup_component(1, tts.DOMAIN):
+            setup_component(self.hass, tts.DOMAIN, config)
+
+            self.hass.services.call(
+                tts.DOMAIN,
+                "microsoft_speech_say",
+                {
+                    "entity_id": "media_player.something",
+                    tts.ATTR_MESSAGE: "HomeAssistant",
+                },
+            )
+            self.hass.block_till_done()
+
+        assert len(calls) == 0
+
+    @patch("gtts_token.gtts_token.Token.calculate_token", autospec=True, return_value=5)
+    def test_service_say_error_language(self, mock_calculate):
+        """Test service call say error on language."""
+        calls = mock_service(self.hass, DOMAIN_MP, SERVICE_PLAY_MEDIA)
+
+        config = {
+            tts.DOMAIN: {
+                "platform": "microsoft_speech",
+                "api_key": "123456789abcdefghijklmnopqrstuvwxyz",
+                "language": "nonexistinglanguage",
+            }
+        }
+
+        with assert_setup_component(1, tts.DOMAIN):
+            setup_component(self.hass, tts.DOMAIN, config)
+
+            self.hass.services.call(
+                tts.DOMAIN,
+                "microsoft_speech_say",
+                {
+                    "entity_id": "media_player.something",
+                    tts.ATTR_MESSAGE: "HomeAssistant",
+                },
+            )
+            self.hass.block_till_done()
+
+        assert len(calls) == 0
+
+    @patch("gtts_token.gtts_token.Token.calculate_token", autospec=True, return_value=5)
+    def test_service_say_error_type(self, mock_calculate):
+        """Test service call say error on type."""
+        calls = mock_service(self.hass, DOMAIN_MP, SERVICE_PLAY_MEDIA)
+
+        config = {
+            tts.DOMAIN: {
+                "platform": "microsoft_speech",
+                "api_key": "123456789abcdefghijklmnopqrstuvwxyz",
+                "type": "nonexistingtype",
+            }
+        }
+
+        with assert_setup_component(1, tts.DOMAIN):
+            setup_component(self.hass, tts.DOMAIN, config)
+
+            self.hass.services.call(
+                tts.DOMAIN,
+                "microsoft_speech_say",
+                {
+                    "entity_id": "media_player.something",
+                    tts.ATTR_MESSAGE: "HomeAssistant",
+                },
+            )
+            self.hass.block_till_done()
+
+        assert len(calls) == 0
+
+    @patch("gtts_token.gtts_token.Token.calculate_token", autospec=True, return_value=5)
     def test_service_say_dutch(self, mock_calculate):
         """Test service call say in Dutch."""
         calls = mock_service(self.hass, DOMAIN_MP, SERVICE_PLAY_MEDIA)
