935dd8b293b0fa6b9fcd1ead64d54b1db4326573
Thomas Schamm <thomas@tschamm.de>
2020-10-28 21:50:29 +0100

    Fixed config_flow tests for bosch_shc.

--- tests/components/bosch_shc/test_config_flow.py
+++ tests/components/bosch_shc/test_config_flow.py
@@ -1,5 +1,6 @@
 """Test the Bosch SHC config flow."""
 from boschshcpy import SHCSession
+from boschshcpy.information import SHCInformation
 
 from homeassistant import config_entries, setup
 from homeassistant.components.bosch_shc.config_flow import CannotConnect, InvalidAuth
@@ -18,8 +19,8 @@ async def test_form(hass):
     assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.bosch_shc.config_flow.validate_input",
-        return_value={"title": "Bosch SHC"},
+        "homeassistant.components.bosch_shc.config_flow.SHCSession.information",
+        return_value=SHCInformation,
     ), patch(
         "homeassistant.components.bosch_shc.async_setup", return_value=True
     ) as mock_setup, patch(
@@ -65,6 +66,25 @@ async def test_form_invalid_auth(hass):
                 "ssl_key": "test-key.pem",
             },
         )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "invalid_auth"}
+
+
+async def test_form_invalid_auth_from_shc(hass):
+    """Test we handle invalid auth from SHCSession."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            "host": "1.1.1.1",
+            "ssl_certificate": "test-cert.pem",
+            "ssl_key": "test-key.pem",
+        },
+    )
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "invalid_auth"}
@@ -128,3 +148,17 @@ async def test_session_valid(hass):
     assert session.api._controller_ip == "1.1.1.1"
     assert session.api._certificate == "test-cert.pem"
     assert session.api._key == "test-key.pem"
+
+
+async def test_session_invalid(hass):
+    """Test we provide parameters to SHC Session valid."""
+    session = await hass.async_add_executor_job(
+        SHCSession,
+        "1.1.1.1",
+        "test-cert.pem",
+        "test-key.pem",
+        True,
+    )
+
+    status = session.information.version
+    assert status == "n/a"
