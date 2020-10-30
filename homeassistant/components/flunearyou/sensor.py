e9d8212ff57e918d5f6466d70f48b6b4a02c4f0e
Aaron Bach <bachya1208@gmail.com>
2020-10-30 10:36:05 -0600

    Remove overlap with CoordinatorEntity

--- homeassistant/components/flunearyou/sensor.py
+++ homeassistant/components/flunearyou/sensor.py
@@ -132,16 +132,15 @@ class FluNearYouSensor(CoordinatorEntity):
         """Return the unit the value is expressed in."""
         return self._unit
 
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self.update_from_latest_data()
+        self.async_write_ha_state()
+
     async def async_added_to_hass(self):
         """Register callbacks."""
-
-        @callback
-        def update():
-            """Update the state."""
-            self.update_from_latest_data()
-            self.async_write_ha_state()
-
-        self.async_on_remove(self.coordinator.async_add_listener(update))
+        await super().async_added_to_hass()
         self.update_from_latest_data()
 
     @callback
