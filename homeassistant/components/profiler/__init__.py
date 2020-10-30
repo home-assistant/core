4378b9c03fb56e6ac80f829252f1a076be9342cf
J. Nick Koston <nick@koston.org>
2020-10-30 07:28:01 -1000

    objgraph

--- homeassistant/components/profiler/__init__.py
+++ homeassistant/components/profiler/__init__.py
@@ -68,12 +68,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         _LOGGER.debug("Most common types: %s", objgraph.most_common_types(limit=100))
         _LOGGER.debug("Growth: %s", objgraph.growth(limit=100))
 
-    @callback
-    def _log_dicts(*_):
-        _LOGGER.debug("Dicts: %s", objgraph.by_type("dict"))
+    #    @callback
+    #    def _log_dicts(*_):
+    #        _LOGGER.debug("Dicts: %s", objgraph.by_type("dict"))
 
     async_track_time_interval(hass, _log_objects, timedelta(seconds=30))
-    async_track_time_interval(hass, _log_dicts, timedelta(seconds=60))
+    #    async_track_time_interval(hass, _log_dicts, timedelta(seconds=60))
 
     return True
 
