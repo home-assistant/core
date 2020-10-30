c10c2ed85740dea0225d64f759969f1025ca6a88
Allen Porter <allen@thebends.org>
2020-10-30 05:25:52 +0000

    Add missing config flow translation key
    Add missing config key for no_url_available when generating new oauth2
    config entry based integrations.  This was found in issue #42423 where
    the nest integration was missing a string when a user had a misconfiguration.

--- script/scaffold/generate.py
+++ script/scaffold/generate.py
@@ -165,6 +165,7 @@ def _custom_tasks(template, info) -> None:
                 "abort": {
                     "missing_configuration": "[%key:common::config_flow::abort::oauth2_missing_configuration%]",
                     "authorize_url_timeout": "[%key:common::config_flow::abort::oauth2_authorize_url_timeout%]",
+                    "no_url_available": "[%key:common::config_flow::abort::oauth2_no_url_available%]",
                 },
                 "create_entry": {
                     "default": "[%key:common::config_flow::create_entry::authenticated%]"
