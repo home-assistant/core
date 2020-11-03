5a2c28853a69449e6d2f5be482aaefa1e39a343a
chemelli74 <simone.chemelli@gmail.com>
2020-11-03 10:54:41 +0100

    Applied core review hints

--- homeassistant/components/shelly/utils.py
+++ homeassistant/components/shelly/utils.py
@@ -8,8 +8,8 @@ _LOGGER = logging.getLogger(__name__)
 async def async_remove_entity_by_domain(hass, domain, unique_id):
     """Remove entity by domain."""
     entity_reg = await hass.helpers.entity_registry.async_get_registry()
-    for entity in entity_reg.entities.values():
-        if entity.entity_id.startswith(domain + ".") and entity.unique_id == unique_id:
-            entity_reg.async_remove(entity.entity_id)
-            _LOGGER.debug("Removed %s domain for %s", domain, entity.original_name)
+    for entry in entity_reg.entities.values():
+        if entry.domain == domain and entry.unique_id == unique_id:
+            entity_reg.async_remove(entry.entity_id)
+            _LOGGER.debug("Removed %s domain for %s", domain, entry.original_name)
             break
