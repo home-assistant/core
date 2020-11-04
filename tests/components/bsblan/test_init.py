8dbc3a3c2443c9aa94fc21359ac0ab770717a2ac
Willem-Jan van Rootselaar <liudgervr@gmail.com>
2020-11-04 20:15:13 +0100

    Refactor init

--- tests/components/bsblan/test_init.py
+++ tests/components/bsblan/test_init.py
@@ -1,17 +1,11 @@
 """Tests for the BSBLan integration."""
 import aiohttp
 
-from homeassistant.components.bsblan.const import (
-    CONF_DEVICE_IDENT,
-    CONF_PASSKEY,
-    DOMAIN,
-)
+from homeassistant.components.bsblan.const import DOMAIN
 from homeassistant.config_entries import ENTRY_STATE_SETUP_RETRY
-from homeassistant.const import CONF_HOST, CONF_PORT
 from homeassistant.core import HomeAssistant
 
-from tests.common import MockConfigEntry
-from tests.components.bsblan import init_integration
+from tests.components.bsblan import init_integration, init_integration_without_auth
 from tests.test_util.aiohttp import AiohttpClientMocker
 
 
@@ -49,16 +43,7 @@ async def test_config_entry_no_authentication(
         exc=aiohttp.ClientError,
     )
 
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        unique_id="RVS21.831F/127",
-        data={
-            CONF_HOST: "example.local",
-            CONF_PASSKEY: "1234",
-            CONF_PORT: 80,
-            CONF_DEVICE_IDENT: "RVS21.831F/127",
-        },
-    )
+    entry = await init_integration_without_auth(hass, aioclient_mock)
 
     entry = await init_integration(hass, aioclient_mock)
     assert entry.state == ENTRY_STATE_SETUP_RETRY
