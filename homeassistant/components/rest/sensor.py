c7fc0a224b0c19aedee5469ed1688f80dd8852f2
J. Nick Koston <nick@koston.org>
2020-11-03 21:52:47 -1000

    Fix rest sensors with resource templates (#42818)

--- homeassistant/components/rest/sensor.py
+++ homeassistant/components/rest/sensor.py
@@ -103,7 +103,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
     if resource_template is not None:
         resource_template.hass = hass
-        resource = resource_template.render(parse_result=False)
+        resource = resource_template.async_render(parse_result=False)
 
     if username and password:
         if config.get(CONF_AUTHENTICATION) == HTTP_DIGEST_AUTHENTICATION:
@@ -202,7 +202,7 @@ class RestSensor(Entity):
     async def async_update(self):
         """Get the latest data from REST API and update the state."""
         if self._resource_template is not None:
-            self.rest.set_url(self._resource_template.render(parse_result=False))
+            self.rest.set_url(self._resource_template.async_render(parse_result=False))
 
         await self.rest.async_update()
 
