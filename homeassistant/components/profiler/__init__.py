ca57f708fdaae18a3df3444ee6ce193bde4fcb1d
J. Nick Koston <nick@koston.org>
2020-11-06 09:41:32 -1000

    tweaks

--- homeassistant/components/profiler/__init__.py
+++ homeassistant/components/profiler/__init__.py
@@ -70,7 +70,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     @callback
     def _log_loggers(*_):
-        _LOGGER.debug("HassLogger: %s", objgraph.by_type("HassLogger"))
+        _LOGGER.debug("Task: %s", objgraph.by_type("Task"))
 
     async_track_time_interval(hass, _log_objects, timedelta(seconds=30))
     async_track_time_interval(hass, _log_loggers, timedelta(seconds=60))
