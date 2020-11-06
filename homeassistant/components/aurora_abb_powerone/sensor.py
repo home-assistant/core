ce99702e005cd1da8354d7b2a47042a07251de3a
Dave T <17680170+davet2001@users.noreply.github.com>
2020-11-06 22:08:32 +0000

    Correct typo in log message and fix formatting.

--- homeassistant/components/aurora_abb_powerone/sensor.py
+++ homeassistant/components/aurora_abb_powerone/sensor.py
@@ -68,7 +68,7 @@ async def async_setup_entry(hass, config, async_add_entities) -> None:
             )
         else:
             _LOGGER.error("Unrecognised sensor parameter '%s'", sensor["parameter"])
-    _LOGGER.debug("async_setup_entry adding %d entitites", len(entities))
+    _LOGGER.debug("async_setup_entry adding %d entities", len(entities))
     async_add_entities(entities, True)
 
 
