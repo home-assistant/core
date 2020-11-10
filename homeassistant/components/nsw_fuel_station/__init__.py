867f7fe01a22a6ad62c39892ba226712e5580c66
Nick Whyte <nick@nickwhyte.com>
2020-11-10 21:13:30 +1100

    Further reduce request volume

--- homeassistant/components/nsw_fuel_station/__init__.py
+++ homeassistant/components/nsw_fuel_station/__init__.py
@@ -1,5 +1,6 @@
 """The nsw_fuel_station component."""
 import logging
+from typing import Optional
 
 from nsw_fuel import FuelCheckClient, FuelCheckError
 
@@ -15,46 +16,53 @@ _LOGGER = logging.getLogger(__name__)
 
 def setup(hass, config):
     client = FuelCheckClient()
+    fuel_check_data = FuelCheckData(client)
+    fuel_check_data.update()
 
-    reference_data = SharedReferenceData(client)
-    reference_data.update()
-
-    hass.data[DATA_NSW_FUEL_STATION] = {
-        DATA_ATTR_CLIENT: client,
-        DATA_ATTR_REFERENCE_DATA: reference_data,
-    }
+    hass.data[DATA_NSW_FUEL_STATION] = fuel_check_data
 
     return True
 
 
-class SharedReferenceData():
+class FuelCheckData:
     def __init__(self, client: FuelCheckClient):
+        """An object to fetch and cache the latest fuel check data."""
         self._client = client
-        self._data = None
+        self._stations = None
+        self._prices = None
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
-        if self._data is None:
-            try:
-                self._data = self._client.get_reference_data()
-            except FuelCheckError as exc:
-                _LOGGER.error(
-                    "Failed to fetch NSW Fuel station reference data. %s", exc
-                )
-                return
-
-    def get_station_name(self, station_id: int) -> str:
-        """Return the name of the station."""
-        name = None
-        if self._data is not None:
-            name = next(
-                (
-                    station.name
-                    for station in self._data.stations
-                    if station.code == station_id
-                ),
-                None,
+        """Update the internal data using the API client."""
+        try:
+            raw_price_data = self._client.get_fuel_prices()
+            # Store prices and station details indexed by station code for
+            # O(1) lookup
+            self._stations = {s.code: s for s in raw_price_data.stations}
+            self._prices = {(str(p.station_code), p.fuel_type): p.price for p
+                            in
+                            raw_price_data.prices}
+        except FuelCheckError as exc:
+            _LOGGER.error(
+                "Failed to fetch NSW Fuel station price data. %s", exc
             )
+            return
 
-        return name or f"station {station_id}"
+    def get_fuel_price(self, station_code: int, fuel_type: str) -> Optional[
+        float]:
+        """Return the price of the given fuel type."""
+        if self._prices is None:
+            return None
 
+        return self._prices.get((str(station_code), fuel_type))
+
+    def get_station_name(self, station_code: int) -> str:
+        """Return the name of the station for a given station code."""
+
+        name = None
+        if self._stations:
+            station_info = self._stations.get(station_code)
+            if station_info:
+                name = station_info.name
+
+        return name or f"station {station_code}"
--- homeassistant/components/nsw_fuel_station/sensor.py
+++ homeassistant/components/nsw_fuel_station/sensor.py
@@ -3,17 +3,14 @@ import logging
 from typing import Optional
 
 import voluptuous as vol
-from nsw_fuel import FuelCheckError, FuelCheckClient
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.nsw_fuel_station import (
     DATA_NSW_FUEL_STATION,
-    DATA_ATTR_CLIENT, DATA_ATTR_REFERENCE_DATA, SharedReferenceData,
-    MIN_TIME_BETWEEN_UPDATES)
+    FuelCheckData)
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import ATTR_ATTRIBUTION, CURRENCY_CENT, VOLUME_LITERS
 from homeassistant.helpers.entity import Entity
-from homeassistant.util import Throttle
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -54,81 +51,41 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     station_id = config[CONF_STATION_ID]
     fuel_types = config[CONF_FUEL_TYPES]
 
-    client = hass.data[DATA_NSW_FUEL_STATION][DATA_ATTR_CLIENT]
-    reference_data = hass.data[DATA_NSW_FUEL_STATION][DATA_ATTR_REFERENCE_DATA]
-
-    station_data = StationPriceData(client, station_id)
-    station_data.update()
+    fuel_check_data = hass.data[DATA_NSW_FUEL_STATION]
 
     add_entities(
-        [StationPriceSensor(station_id, reference_data, station_data,
-                            fuel_type) for
+        [StationPriceSensor(fuel_check_data, station_id, fuel_type) for
          fuel_type in fuel_types])
-
-
-class StationPriceData:
-    """An object to store and fetch the latest data for a given station."""
-
-    def __init__(self, client: FuelCheckClient, station_id: int) -> None:
-        """Initialize the sensor."""
-        self._station_id = station_id
-        self._client = client
-        self._data = None
-
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self):
-        """Update the internal data using the API client."""
-        try:
-            self._data = self._client.get_fuel_prices_for_station(
-                self._station_id)
-        except FuelCheckError as exc:
-            _LOGGER.error("Failed to fetch NSW Fuel station price data. %s", exc)
-
-    def for_fuel_type(self, fuel_type: str):
-        """Return the price of the given fuel type."""
-        if self._data is None:
-            return None
-        return next(
-            (price for price in self._data if price.fuel_type == fuel_type), None
-        )
-
-    def get_available_fuel_types(self):
-        """Return the available fuel types for the station."""
-        return [price.fuel_type for price in self._data]
 
 
 class StationPriceSensor(Entity):
     """Implementation of a sensor that reports the fuel price for a station."""
 
-    def __init__(self, station_id: int, reference_data: SharedReferenceData,
-                 station_data: StationPriceData, fuel_type: str):
+    def __init__(self, fuel_check_data: FuelCheckData, station_id: int,
+                 fuel_type: str):
         """Initialize the sensor."""
         self._station_id = station_id
-        self._reference_data = reference_data
-        self._station_data = station_data
         self._fuel_type = fuel_type
+        self._fuel_check_data = fuel_check_data
 
     @property
     def name(self) -> str:
         """Return the name of the sensor."""
-        station_name = self._reference_data.get_station_name(self._station_id)
+        station_name = self._fuel_check_data.get_station_name(self._station_id)
         return f"{station_name} {self._fuel_type}"
 
     @property
     def state(self) -> Optional[float]:
         """Return the state of the sensor."""
-        price_info = self._station_data.for_fuel_type(self._fuel_type)
-        if price_info:
-            return price_info.price
-
-        return None
+        return self._fuel_check_data.get_fuel_price(
+            self._station_id, self._fuel_type)
 
     @property
     def device_state_attributes(self) -> dict:
         """Return the state attributes of the device."""
         return {
             ATTR_STATION_ID: self._station_id,
-            ATTR_STATION_NAME: self._reference_data.get_station_name(
+            ATTR_STATION_NAME: self._fuel_check_data.get_station_name(
                 self._station_id),
             ATTR_ATTRIBUTION: ATTRIBUTION,
         }
@@ -140,5 +97,4 @@ class StationPriceSensor(Entity):
 
     def update(self):
         """Update current conditions."""
-        self._reference_data.update()
-        self._station_data.update()
+        self._fuel_check_data.update()
