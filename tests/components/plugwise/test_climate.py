7d24a7da4aa5fb86a2fec9ba85ad1345d4f8761b
Tom Scholten <git@scholten.nu>
2020-11-10 19:53:02 +0100

    Add tests for exceptions

--- tests/components/plugwise/test_climate.py
+++ tests/components/plugwise/test_climate.py
@@ -1,5 +1,7 @@
 """Tests for the Plugwise Climate integration."""
 
+from plugwise.exceptions import PlugwiseException
+
 from homeassistant.config_entries import ENTRY_STATE_LOADED
 
 from tests.components.plugwise.common import async_init_integration
@@ -38,6 +40,34 @@ async def test_adam_climate_entity_attributes(hass, mock_smile_adam):
     assert attrs["current_temperature"] == 17.2
     assert attrs["temperature"] == 15.0
 
+    assert attrs["preset_mode"] == "asleep"
+
+
+async def test_adam_climate_switch_negative_testing(hass, mock_smile_adam):
+    """Test exceptions of climate entities."""
+    mock_smile_adam.set_temperature.side_effect = PlugwiseException
+    mock_smile_adam.set_preset.side_effect = PlugwiseException
+    entry = await async_init_integration(hass, mock_smile_adam)
+    assert entry.state == ENTRY_STATE_LOADED
+
+    await hass.services.async_call(
+        "climate",
+        "set_temperature",
+        {"entity_id": "climate.zone_lisa_wk", "temperature": 25},
+        blocking=True,
+    )
+    state = hass.states.get("climate.zone_lisa_wk")
+    attrs = state.attributes
+    assert attrs["temperature"] == 21.5
+
+    await hass.services.async_call(
+        "climate",
+        "set_preset_mode",
+        {"entity_id": "climate.zone_thermostat_jessie", "preset_mode": "home"},
+        blocking=True,
+    )
+    state = hass.states.get("climate.zone_thermostat_jessie")
+    attrs = state.attributes
     assert attrs["preset_mode"] == "asleep"
 
 
