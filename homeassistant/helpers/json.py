14839dc5379eb494bd8964e19e6eaf30184625f5
J. Nick Koston <nick@koston.org>
2020-11-09 19:34:18 -1000

    Revert "adjust encoder"
    This reverts commit b89eb1a13010fcc122f612fa3574872c64675a6e.

--- homeassistant/helpers/json.py
+++ homeassistant/helpers/json.py
@@ -1,10 +1,10 @@
 """Helpers to help with encoding Home Assistant objects in JSON."""
 from datetime import datetime
-from json import JSONEncoder as DefaultJSONEncoder
+import json
 from typing import Any
 
 
-class JSONEncoder(DefaultJSONEncoder):
+class JSONEncoder(json.JSONEncoder):
     """JSONEncoder that supports Home Assistant objects."""
 
     def default(self, o: Any) -> Any:
@@ -12,13 +12,11 @@ class JSONEncoder(DefaultJSONEncoder):
 
         Hand other objects to the original method.
         """
-        try:
-            return DefaultJSONEncoder.default(self, o)
-        except TypeError:
-            if isinstance(o, datetime):
-                return o.isoformat()
-            if isinstance(o, set):
-                return list(o)
-            if hasattr(o, "as_dict"):
-                return o.as_dict()
-            raise
+        if isinstance(o, datetime):
+            return o.isoformat()
+        if isinstance(o, set):
+            return list(o)
+        if hasattr(o, "as_dict"):
+            return o.as_dict()
+
+        return json.JSONEncoder.default(self, o)
