14f3f68af0a311523b5d6e2f862889fa6c07b5eb
tomasz75 <tomek@sviete.pl>
2020-11-10 07:50:35 +0100

    improve auth flow

--- homeassistant/components/google/config_flow.py
+++ homeassistant/components/google/config_flow.py
@@ -1,12 +1,16 @@
 """Config flow to configure the AIS Google Calendar Service component."""
 
+import datetime
+import json
 import logging
 
 import aiohttp
-from oauth2client.file import Storage
+import yarl
 
 from homeassistant import config_entries
 from homeassistant.components import ais_cloud
+from homeassistant.components.ais_dom import ais_global
+from homeassistant.components.http import current_request
 from homeassistant.components.http.view import HomeAssistantView
 from homeassistant.const import CONF_NAME
 from homeassistant.core import callback
@@ -35,16 +39,17 @@ class AuthorizationCallbackView(HomeAssistantView):
     async def get(self, request):
         hass = request.app["hass"]
         flow_id = request.query["flow_id"]
-
-        # the call was from ais-dom finish the integration
-        hass.async_create_task(
-            hass.config_entries.flow.async_configure(flow_id=flow_id, user_input="ok")
-        )
-        js_text = (
-            "<script>window.location.href='/config/integrations/dashboard'</script>"
-        )
+        flow_id = flow_id.replace("google-calendar-", "")
+        conf = {
+            "client_id": "ASK_AIS_DOM",
+            "client_secret": "ASK_AIS_DOM",
+            CONF_TRACK_NEW: True,
+        }
+        # finish the integration
+        await hass.config_entries.flow.async_configure(flow_id=flow_id, user_input=conf)
         return aiohttp.web_response.Response(
-            headers={"content-type": "text/html"}, text=js_text
+            headers={"content-type": "text/html"},
+            text="<script>window.close()</script>Success! This window can be closed",
         )
 
 
@@ -66,82 +71,94 @@ class GoogleHomeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_confirm(self, user_input=None):
         """Handle a flow start."""
-        from homeassistant.components.ais_dom import ais_global
-
         errors = {}
         if user_input is not None:
-            try:
-                ais_dom = ais_cloud.AisCloudWS(self.hass)
-                json_ws_resp = ais_dom.key("google_calendar_web_client_id")
-                client_id = json_ws_resp["key"]
-                json_ws_resp = ais_dom.key("google_calendar_web_secret")
-                client_secret = json_ws_resp["key"]
-                gate_id = ais_global.get_sercure_android_id_dom()
-                verification_url = (
-                    "https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id="
-                    + client_id
-                    + "&redirect_uri=https://powiedz.co/ords/dom/auth/google_calendar_callback"
-                    "&response_type=code&scope=https://www.googleapis.com/auth/calendar"
-                    "&state=" + gate_id
-                )
-                description_placeholders = {
-                    "error_info": "",
-                    "verification_url": verification_url,
-                }
+            # register view
+            self.hass.http.register_view(AuthorizationCallbackView)
+            return self.async_show_form(
+                step_id="oauth",
+                errors=None,
+                description_placeholders=None,
+            )
 
-                return self.async_show_form(
-                    step_id="oauth",
-                    errors=None,
-                    description_placeholders=description_placeholders,
-                )
-
-            except Exception as e:
-                _LOGGER.error("Error calendar do_authentication: " + str(e))
-                errors = {CONF_OAUTH_INFO: "oauth_error"}
-                description_placeholders = {
-                    "error_info": f"Error: {e}",
-                    "verification_url": "",
-                    "user_code": "",
-                }
-                return self.async_show_form(
-                    step_id="oauth",
-                    errors=errors,
-                    description_placeholders=description_placeholders,
-                )
         return self.async_show_form(step_id="confirm", errors=errors)
 
     async def async_step_oauth(self, user_input=None):
-        """Handle a flow start."""
-        errors = {}
-        description_placeholders = {"error_info": ""}
-        if user_input is not None:
-            try:
-                credentials = "xxxx"
-                storage = Storage(self.hass.config.path(TOKEN_FILE))
-                storage.put(credentials)
-                conf = {
-                    "client_id": "ASK_AIS_DOM",
-                    "client_secret": "ASK_AIS_DOM",
-                    CONF_TRACK_NEW: True,
-                }
+        request = current_request.get()
+        url_host = yarl.URL(request.url).host
 
-                await async_do_setup(self.hass, self.hass.config.as_dict())
-
-                self.hass.components.frontend.async_register_built_in_panel(
-                    "calendar", "calendar", "hass:calendar", update=True
-                )
-
-                return self.async_create_entry(title="AIS Google Calendars", data=conf)
-            except Exception as err:
-                _LOGGER.error("Error calendar async_step_token: " + str(err))
-                description_placeholders = {"error_info": f"Error: {err}"}
-                return self.async_abort(
-                    reason="abort_by_error",
-                    description_placeholders=description_placeholders,
-                )
-
-        return self.async_show_form(
-            step_id="oauth",
-            errors=errors,
-            description_placeholders=description_placeholders,
+        """Handle flow external step."""
+        ais_dom = ais_cloud.AisCloudWS(self.hass)
+        json_ws_resp = ais_dom.key("google_calendar_web_client_id")
+        client_id = json_ws_resp["key"]
+        gate_id = ais_global.get_sercure_android_id_dom()
+        auth_url = (
+            "https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id="
+            + client_id
+            + "&redirect_uri=https://powiedz.co/ords/dom/auth/google_calendar_callback"
+            + "&response_type=code&scope=https://www.googleapis.com/auth/calendar"
+            + "&access_type=offline"
+            + "&state="
+            + gate_id
+            + "ais0dom"
+            + url_host
+            + "ais0domgoogle-calendar-"
+            + self.flow_id
         )
+        return self.async_external_step(step_id="obtain_token", url=auth_url)
+
+    async def async_step_obtain_token(self, user_input=None):
+        """Obtain token after external auth completed."""
+        # get token from cloud
+        try:
+            ais_dom = ais_cloud.AisCloudWS(self.hass)
+            json_ws_resp = await ais_dom.async_key("google_calendar_user_token")
+            calendar_token = json_ws_resp["key"]
+            json_ws_resp = await ais_dom.async_key("google_calendar_web_client_id")
+            client_id = json_ws_resp["key"]
+            json_ws_resp = await ais_dom.async_key("google_calendar_web_secret")
+            client_secret = json_ws_resp["key"]
+
+            token_response = json.loads(calendar_token)
+            json_token = json.loads(calendar_token)
+            json_token["token_response"] = token_response
+            json_token["_module"] = "oauth2client.client"
+            json_token["_class"] = "OAuth2Credentials"
+            json_token["client_id"] = client_id
+            json_token["client_secret"] = client_secret
+            delta = datetime.timedelta(seconds=token_response["expires_in"])
+            now = datetime.datetime.utcnow()
+            json_token["token_expiry"] = str(now + delta)
+            json_token["token_uri"] = "https://www.googleapis.com/oauth2/v4/token"
+            json_token["user_agent"] = None
+            json_token["invalid"] = False
+
+            with open(self.hass.config.path(TOKEN_FILE), "w") as json_file:
+                json.dump(json_token, json_file)
+
+        except Exception as e:
+            return self.async_external_step_done(
+                next_step_id="token_error", user_input=str(e)
+            )
+
+        return self.async_external_step_done(next_step_id="use_external_token")
+
+    async def async_step_token_error(self, user_input=None):
+        """Abort flow when time expires."""
+        return self.async_abort(
+            reason="abort_by_error", description_placeholders={"error_info": user_input}
+        )
+
+    async def async_step_use_external_token(self, user_input=None):
+        """Continue server validation with external token."""
+        await async_do_setup(self.hass, self.hass.config.as_dict())
+
+        self.hass.components.frontend.async_register_built_in_panel(
+            "calendar", "calendar", "hass:calendar", update=True
+        )
+        conf = {
+            "client_id": "ASK_AIS_DOM",
+            "client_secret": "ASK_AIS_DOM",
+            CONF_TRACK_NEW: True,
+        }
+        return self.async_create_entry(title="AIS Google Calendars", data=conf)
