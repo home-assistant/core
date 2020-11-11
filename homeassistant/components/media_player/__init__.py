13d4ac2e91db6b19457cb3e1fa4f5218799e657d
Chris Talkington <chris@talkingtontech.com>
2020-11-11 09:22:21 -0600

    Update __init__.py

--- homeassistant/components/media_player/__init__.py
+++ homeassistant/components/media_player/__init__.py
@@ -913,7 +913,7 @@ class MediaPlayerEntity(Entity):
 
         url_query = {"token": self.access_token}
         if media_image_id:
-            url_query.update({"media_image_id": media_image_id})
+            url_query["media_image_id"] = media_image_id
 
         return str(URL(url_path).with_query(url_query))
 
@@ -962,6 +962,7 @@ class MediaPlayerImageView(HomeAssistantView):
             data, content_type = await player.async_get_media_image()
 
         if data is None:
+            _LOGGER.warning("Error retrieving proxied image from %s", image_url)
             return web.Response(status=HTTP_INTERNAL_SERVER_ERROR)
 
         headers: LooseHeaders = {CACHE_CONTROL: "max-age=3600"}
