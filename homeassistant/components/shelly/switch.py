df54fea6c27c9780644609c6a602cd92d5461bb0
Shay Levy <levyshay1@gmail.com>
2020-11-12 11:12:56 +0200

    Make Appliance Type Case-insensitive (#43114)
    "appliance_type" is a free text parameter in the device settings, this fix will make the comparison case-insensitive

--- homeassistant/components/shelly/switch.py
+++ homeassistant/components/shelly/switch.py
@@ -23,18 +23,21 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     relay_blocks = []
     for block in wrapper.device.blocks:
-        if block.type == "relay" and (
-            wrapper.device.settings["relays"][int(block.channel)].get("appliance_type")
-            != "light"
-        ):
-            relay_blocks.append(block)
-            unique_id = f'{wrapper.device.shelly["mac"]}-{block.type}_{block.channel}'
-            await async_remove_entity_by_domain(
-                hass,
-                "light",
-                unique_id,
-                config_entry.entry_id,
+        if block.type == "relay":
+            appliance_type = wrapper.device.settings["relays"][int(block.channel)].get(
+                "appliance_type"
             )
+            if not appliance_type or appliance_type.lower() != "light":
+                relay_blocks.append(block)
+                unique_id = (
+                    f'{wrapper.device.shelly["mac"]}-{block.type}_{block.channel}'
+                )
+                await async_remove_entity_by_domain(
+                    hass,
+                    "light",
+                    unique_id,
+                    config_entry.entry_id,
+                )
 
     if not relay_blocks:
         return
