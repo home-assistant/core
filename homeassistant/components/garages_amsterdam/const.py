44b72b27f617bd5ac540bb0c78227fc81e840252
Klaas Schoute <klaas_schoute@hotmail.com>
2020-11-13 21:23:18 +0000

    Remove unused const

--- homeassistant/components/garages_amsterdam/const.py
+++ homeassistant/components/garages_amsterdam/const.py
@@ -1,5 +1,4 @@
 """Constants for the Garages Amsterdam integration."""
 
 DOMAIN = "garages_amsterdam"
-OPTION_ALL = "__hole Amsterdam"
 ATTRIBUTION = f'{"Data provided by municipality of Amsterdam"}'
