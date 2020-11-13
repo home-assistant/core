da989fe4b5aad6f4a52f6027de0220041ebe495e
Paul Daumlechner <paul.daumlechner@live.de>
2020-11-13 18:56:25 +0000

    reboot service added

--- homeassistant/components/velux/__init__.py
+++ homeassistant/components/velux/__init__.py
@@ -58,13 +58,16 @@ class VeluxModule:
             _LOGGER.debug("Velux interface terminated")
             await self.pyvlx.disconnect()
 
+        async def async_reboot_gateway(service_call):
+            await self.pyvlx.reboot_gateway()
+
         self._hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, on_hass_stop)
         host = self._domain_config.get(CONF_HOST)
         password = self._domain_config.get(CONF_PASSWORD)
         self.pyvlx = PyVLX(host=host, password=password)
 
         self._hass.services.async_register(
-            DOMAIN, "reboot_gateway", self.async_reboot_gateway
+            DOMAIN, "reboot_gateway", async_reboot_gateway
         )
 
     async def async_start(self):
@@ -72,6 +75,3 @@ class VeluxModule:
         _LOGGER.debug("Velux interface started")
         await self.pyvlx.load_scenes()
         await self.pyvlx.load_nodes()
-
-    async def async_reboot_gateway(self, service_call):
-        await self.pyvlx.reboot_gateway()
