9135d04049c3510f30170f5d6e3f61bea944e8dc
Klaas Schoute <klaas_schoute@hotmail.com>
2020-11-16 17:19:05 +0000

    Add support for Dutch (female) language

--- homeassistant/components/cloud/tts.py
+++ homeassistant/components/cloud/tts.py
@@ -10,7 +10,7 @@ from .const import DOMAIN
 
 CONF_GENDER = "gender"
 
-SUPPORT_LANGUAGES = ["en-US", "de-DE", "es-ES"]
+SUPPORT_LANGUAGES = ["en-US", "de-DE", "es-ES", "nl-NL"]
 SUPPORT_GENDER = ["male", "female"]
 
 DEFAULT_LANG = "en-US"
