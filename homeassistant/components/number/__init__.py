d4c7a1e5b89474ba7a741afc43f44dc51f2164e5
Shulyaka <Shulyaka@gmail.com>
2020-11-16 15:22:45 +0300

    Apply suggestions from code review
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/number/__init__.py
+++ homeassistant/components/number/__init__.py
@@ -51,12 +51,12 @@ async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
     return True
 
 
-async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool:
     """Set up a config entry."""
     return await hass.data[DOMAIN].async_setup_entry(entry)
 
 
-async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.data[DOMAIN].async_unload_entry(entry)
 
