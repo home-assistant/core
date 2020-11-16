56d036128f9a2a9a69d32ea243f79f52ccdb2f1e
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-11-16 11:44:53 +0100

    Add system health support

---
+++ homeassistant/components/gios/system_health.py
@@ -0,0 +1,20 @@
+"""Provide info to system health."""
+from gios import URL_STATIONS
+
+from homeassistant.components import system_health
+from homeassistant.core import HomeAssistant, callback
+
+
+@callback
+def async_register(
+    hass: HomeAssistant, register: system_health.SystemHealthRegistration
+) -> None:
+    """Register system health callbacks."""
+    register.async_register_info(system_health_info)
+
+
+async def system_health_info(hass):
+    """Get info for the info page."""
+    return {
+        "can_reach_server": system_health.async_check_can_reach_url(hass, URL_STATIONS)
+    }
---
+++ tests/components/gios/test_system_health.py
@@ -0,0 +1,41 @@
+"""Test cloud system health."""
+import asyncio
+
+from aiohttp import ClientError
+
+from homeassistant.components.gios.const import DOMAIN
+from homeassistant.setup import async_setup_component
+
+from tests.common import get_system_health_info
+
+
+async def test_gios_system_health(hass, aioclient_mock):
+    """Test gios system health."""
+    aioclient_mock.get("http://api.gios.gov.pl/pjp-api/rest/station/findAll", text="")
+    hass.config.components.add(DOMAIN)
+    assert await async_setup_component(hass, "system_health", {})
+
+    info = await get_system_health_info(hass, DOMAIN)
+
+    for key, val in info.items():
+        if asyncio.iscoroutine(val):
+            info[key] = await val
+
+    assert info == {"can_reach_server": "ok"}
+
+
+async def test_gios_system_health_fail(hass, aioclient_mock):
+    """Test gios system health."""
+    aioclient_mock.get(
+        "http://api.gios.gov.pl/pjp-api/rest/station/findAll", exc=ClientError
+    )
+    hass.config.components.add(DOMAIN)
+    assert await async_setup_component(hass, "system_health", {})
+
+    info = await get_system_health_info(hass, DOMAIN)
+
+    for key, val in info.items():
+        if asyncio.iscoroutine(val):
+            info[key] = await val
+
+    assert info == {"can_reach_server": {"type": "failed", "error": "unreachable"}}
