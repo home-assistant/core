85cb9e3597b71f7eb7f989522b8b9d52da0f468d
Aaron Bach <bachya1208@gmail.com>
2020-11-17 12:29:05 -0700

    Code review

--- homeassistant/components/tile/__init__.py
+++ homeassistant/components/tile/__init__.py
@@ -10,12 +10,14 @@ from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import aiohttp_client
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.util.async_ import gather_with_concurrency
 
 from .const import DATA_COORDINATOR, DATA_TILE, DOMAIN, LOGGER
 
 PLATFORMS = ["device_tracker"]
 DEVICE_TYPES = ["PHONE", "TILE"]
 
+DEFAULT_INIT_TASK_LIMIT = 2
 DEFAULT_UPDATE_INTERVAL = timedelta(minutes=2)
 
 CONF_SHOW_INACTIVE = "show_inactive"
@@ -42,7 +44,7 @@ async def async_setup_entry(hass, entry):
         )
         hass.data[DOMAIN][DATA_TILE][entry.entry_id] = await client.async_get_tiles()
     except TileError as err:
-        raise ConfigEntryNotReady(f"Error during integration setup: {err}")
+        raise ConfigEntryNotReady("Error during integration setup") from err
 
     async def async_update_tile(tile):
         """Update the Tile."""
@@ -67,7 +69,7 @@ async def async_setup_entry(hass, entry):
         )
         coordinator_init_tasks.append(coordinator.async_refresh())
 
-    await asyncio.gather(*coordinator_init_tasks)
+    await gather_with_concurrency(DEFAULT_INIT_TASK_LIMIT, *coordinator_init_tasks)
 
     for component in PLATFORMS:
         hass.async_create_task(
