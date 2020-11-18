65cfdc7f2244a6ccd85e354d9e45952a1a6acecf
Jamie Weston <jamie.weston@sage.com>
2020-11-18 18:51:30 +0000

    Removing msteams_presence component (junk)

--- homeassistant/components/msteams_presence/const.py
+++
@@ -1,6 +0,0 @@
-"""Constants for the Microsoft Teams Presence integration."""
-
-DOMAIN = "msteams_presence"
-
-OAUTH2_AUTHORIZE = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
-OAUTH2_TOKEN = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
