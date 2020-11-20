fb216d2af3f4499e90d2c61d8f9f5fc0c4ba4bcc
Aaron Bach <bachya1208@gmail.com>
2020-11-20 14:11:50 -0700

    Re-organize OpenUV constants

--- homeassistant/components/openuv/binary_sensor.py
+++ homeassistant/components/openuv/binary_sensor.py
@@ -1,19 +1,16 @@
 """Support for OpenUV binary sensors."""
-import logging
-
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.core import callback
 from homeassistant.util.dt import as_local, parse_datetime, utcnow
 
-from . import (
+from . import OpenUvEntity
+from .const import (
     DATA_OPENUV_CLIENT,
     DATA_PROTECTION_WINDOW,
     DOMAIN,
+    LOGGER,
     TYPE_PROTECTION_WINDOW,
-    OpenUvEntity,
 )
-
-_LOGGER = logging.getLogger(__name__)
 
 ATTR_PROTECTION_WINDOW_ENDING_TIME = "end_time"
 ATTR_PROTECTION_WINDOW_ENDING_UV = "end_uv"
@@ -86,7 +83,7 @@ class OpenUvBinarySensor(OpenUvEntity, BinarySensorEntity):
 
         for key in ("from_time", "to_time", "from_uv", "to_uv"):
             if not data.get(key):
-                _LOGGER.info("Skipping update due to missing data: %s", key)
+                LOGGER.info("Skipping update due to missing data: %s", key)
                 return
 
         if self._sensor_type == TYPE_PROTECTION_WINDOW:
