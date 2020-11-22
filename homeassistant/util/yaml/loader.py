2666922cb19141ac4619ec2f6e24adab7dff6671
Tom Parker-Shemilt <palfrey@tevp.net>
2020-11-22 14:41:19 +0000

    Redo check for secret_filename before getting secret_path

--- homeassistant/util/yaml/loader.py
+++ homeassistant/util/yaml/loader.py
@@ -272,11 +272,10 @@ def _load_secret_yaml(secret_path: str) -> JSON_TYPE:
 
 def secret_yaml(loader: SafeLineLoader, node: yaml.nodes.Node) -> JSON_TYPE:
     """Load secrets and embed it into the configuration YAML."""
-    secret_path = os.path.dirname(loader.name)
-    secret_filename = os.path.join(secret_path, SECRET_YAML)
-    if secret_filename == loader.name:
+    if os.path.filename(loader.name) == SECRET_YAML:
         _LOGGER.error("secrets.yaml: attempt to load secret from within secrets file")
         return ""
+    secret_path = os.path.dirname(loader.name)
     while True:
         secrets = _load_secret_yaml(secret_path)
 
