543749ec9ff7d304043fca1d9a2e1105a0bab3bb
Sillyfrog <sillyfrog@users.noreply.github.com>
2020-11-22 11:35:38 +1000

    Update to allow testing

--- homeassistant/components/automate/config_flow.py
+++ homeassistant/components/automate/config_flow.py
@@ -4,13 +4,24 @@ import logging
 import aiopulse2
 import voluptuous as vol
 
-from homeassistant import config_entries, exceptions
+from homeassistant import config_entries, core, exceptions
 
 from .const import DOMAIN  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
 
 DATA_SCHEMA = vol.Schema({vol.Required("host"): str})
+
+
+async def validate_input(hass: core.HomeAssistant, data) -> dict:
+    """Validate the user input allows us to connect to the hub.
+
+    Data has the keys from DATA_SCHEMA with values provided by the user.
+    Broken out into it's own function to facilitate tests.
+    """
+    hub = aiopulse2.Hub(data["host"])
+    await hub.test()
+    return {"title": hub.name}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -24,9 +35,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
         if user_input is not None:
             try:
-                hub = aiopulse2.Hub(user_input["host"])
-                await hub.test()
-                info = {"title": hub.name}
+                info = await validate_input(self.hass, user_input)
                 return self.async_create_entry(title=info["title"], data=user_input)
             except Exception:  # pylint: disable=broad-except
                 errors["base"] = "cannot_connect"
