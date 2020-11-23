4aabc3b689c4a114f8e02a2c78e9a8da9ab99b89
Aleix Murtra <alemurbcn@gmail.com>
2020-11-23 19:09:26 +0100

    Add TMB Planner feature

--- homeassistant/components/tmb/sensor.py
+++ homeassistant/components/tmb/sensor.py
@@ -3,29 +3,64 @@ from datetime import timedelta
 import logging
 
 from requests import HTTPError
-from tmb import IBus
+from tmb import IBus, Planner
 import voluptuous as vol
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
-from homeassistant.const import ATTR_ATTRIBUTION, CONF_NAME, TIME_MINUTES
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+    ATTR_ATTRIBUTION,
+    ATTR_NAME,
+    ATTR_SECONDS,
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+    CONF_NAME,
+    TIME_MINUTES,
+)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util import Throttle
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTRIBUTION = "Data provided by Transport Metropolitans de Barcelona"
+ATTRIBUTION = "Data provided by Transports Metropolitans de Barcelona - tmb.cat"
 
-ICON = "mdi:bus-clock"
+"""
+sensor:
+- platform: tmb
+  app_id: !secret tmb_app_id
+  app_key: !secret tmb_app_key
+  stops:
+    - line: V25
+      stop: 3258
+  routes:
+    - name: From home to work
+      from:
+        latitude: 41.3755204
+        longitude: 2.1498870
+      to:
+        latitude: 41.3878951
+        longitude: 2.1308587
+"""
 
+ATTR_BUS_STOP = "stop"
+ATTR_DESCRIPTION = "description"
+ATTR_IN_TRANSIT = "time in transit"
+ATTR_LINE = "line"
+ATTR_TRANSFERS = "num transfers"
+ATTR_WAITING = "time waiting"
+ATTR_WALK_DISTANCE = "walk distance"
 CONF_APP_ID = "app_id"
 CONF_APP_KEY = "app_key"
-CONF_LINE = "line"
 CONF_BUS_STOP = "stop"
 CONF_BUS_STOPS = "stops"
-ATTR_BUS_STOP = "stop"
-ATTR_LINE = "line"
+CONF_LINE = "line"
+CONF_ROUTES = "routes"
+CONF_ROUTE_FROM = "from"
+CONF_ROUTE_TO = "to"
+ICON_IBUS = "mdi:bus-clock"
+ICON_PLANNER = "mdi:map-marker-distance"
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
+MIN_TIME_BETWEEN_UPDATES_IBUS = timedelta(seconds=60)
+MIN_TIME_BETWEEN_UPDATES_PLANNER = timedelta(seconds=300)
 
 LINE_STOP_SCHEMA = vol.Schema(
     {
@@ -35,11 +70,30 @@ LINE_STOP_SCHEMA = vol.Schema(
     }
 )
 
+ROUTES_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME): cv.string,
+        vol.Required(CONF_ROUTE_FROM): vol.Schema(
+            {
+                vol.Required(CONF_LATITUDE): cv.latitude,
+                vol.Required(CONF_LONGITUDE): cv.longitude,
+            }
+        ),
+        vol.Required(CONF_ROUTE_TO): vol.Schema(
+            {
+                vol.Required(CONF_LATITUDE): cv.latitude,
+                vol.Required(CONF_LONGITUDE): cv.longitude,
+            }
+        ),
+    }
+)
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_APP_ID): cv.string,
         vol.Required(CONF_APP_KEY): cv.string,
-        vol.Required(CONF_BUS_STOPS): vol.All(cv.ensure_list, [LINE_STOP_SCHEMA]),
+        vol.Optional(CONF_BUS_STOPS): vol.All(cv.ensure_list, [LINE_STOP_SCHEMA]),
+        vol.Optional(CONF_ROUTES): vol.All(cv.ensure_list, [ROUTES_SCHEMA]),
     }
 )
 
@@ -47,22 +101,42 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensors."""
     ibus_client = IBus(config[CONF_APP_ID], config[CONF_APP_KEY])
+    planner_client = Planner(config[CONF_APP_ID], config[CONF_APP_KEY])
 
     sensors = []
 
-    for line_stop in config.get(CONF_BUS_STOPS):
-        line = line_stop[CONF_LINE]
-        stop = line_stop[CONF_BUS_STOP]
-        if line_stop.get(CONF_NAME):
-            name = f"{line} - {line_stop[CONF_NAME]} ({stop})"
-        else:
-            name = f"{line} - {stop}"
-        sensors.append(TMBSensor(ibus_client, stop, line, name))
+    # TMB iBus sensors
+    if config.get(CONF_BUS_STOPS) is not None:
+        for line_stop in config.get(CONF_BUS_STOPS):
+            line = line_stop[CONF_LINE]
+            stop = line_stop[CONF_BUS_STOP]
+            if line_stop.get(CONF_NAME):
+                name = f"{line} - {line_stop[CONF_NAME]} ({stop})"
+            else:
+                name = f"{line} - {stop}"
+
+            sensors.append(TMBIBusSensor(ibus_client, stop, line, name))
+
+    # TMB Planner sensors
+    if config.get(CONF_ROUTES) is not None:
+        for route in config.get(CONF_ROUTES):
+            from_lat = route[CONF_ROUTE_FROM][CONF_LATITUDE]
+            from_lon = route[CONF_ROUTE_FROM][CONF_LONGITUDE]
+            from_latlon = f"{from_lat},{from_lon}"
+
+            to_lat = route[CONF_ROUTE_TO][CONF_LATITUDE]
+            to_lon = route[CONF_ROUTE_TO][CONF_LONGITUDE]
+            to_latlon = f"{to_lat},{to_lon}"
+            name = route[CONF_NAME]
+
+            sensors.append(
+                TMBPlannerSensor(planner_client, from_latlon, to_latlon, name)
+            )
 
     add_entities(sensors, True)
 
 
-class TMBSensor(SensorEntity):
+class TMBIBusSensor(Entity):
     """Implementation of a TMB line/stop Sensor."""
 
     def __init__(self, ibus_client, stop, line, name):
@@ -82,7 +156,7 @@ class TMBSensor(SensorEntity):
     @property
     def icon(self):
         """Return the icon for the frontend."""
-        return ICON
+        return ICON_IBUS
 
     @property
     def unit_of_measurement(self):
@@ -108,7 +182,7 @@ class TMBSensor(SensorEntity):
             ATTR_LINE: self._line,
         }
 
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    @Throttle(MIN_TIME_BETWEEN_UPDATES_IBUS)
     def update(self):
         """Get the next bus information."""
         try:
@@ -116,4 +190,82 @@ class TMBSensor(SensorEntity):
         except HTTPError:
             _LOGGER.error(
                 "Unable to fetch data from TMB API. Please check your API keys are valid"
+            )
+
+
+class TMBPlannerSensor(Entity):
+    """Implementation of a TMB line/stop Sensor."""
+
+    def __init__(self, planner_client, from_coords, to_coords, name):
+        """Initialize the sensor."""
+        self._planner_client = planner_client
+        self._from_coords = from_coords
+        self._to_coords = to_coords
+        self._unit = TIME_MINUTES
+        self._name = name
+        self._attr_description = None
+        self._attr_durationInSeconds = None
+        self._attr_transitTime = None
+        self._attr_waitingTime = None
+        self._attr_walkDistance = None
+        self._attr_transfers = None
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def icon(self):
+        """Return the icon for the frontend."""
+        return ICON_PLANNER
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return self._unit
+
+    @property
+    def unique_id(self):
+        """Return a unique, HASS-friendly identifier for this entity."""
+        return f"tmb_planner_{self._name}"
+
+    @property
+    def state(self):
+        """Return the next departure time."""
+        return self._state
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the last update."""
+        return {
+            ATTR_ATTRIBUTION: ATTRIBUTION,
+            ATTR_NAME: self._name,
+            ATTR_DESCRIPTION: self._attr_description,
+            ATTR_SECONDS: self._attr_durationInSeconds,
+            ATTR_IN_TRANSIT: self._attr_transitTime,
+            ATTR_WAITING: self._attr_waitingTime,
+            ATTR_WALK_DISTANCE: self._attr_walkDistance,
+            ATTR_TRANSFERS: self._attr_transfers,
+        }
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES_PLANNER)
+    def update(self):
+        """Get the next bus information."""
+        try:
+            plan = self._planner_client.get_shortest_itinerary(
+                self._from_coords, self._to_coords
+            )
+            self._state = plan["durationInMinutes"]
+            self._attr_description = plan["description"]
+            self._attr_durationInSeconds = plan["durationInSeconds"]
+            self._attr_transitTime = plan["transitTime"]
+            self._attr_waitingTime = plan["waitingTime"]
+            self._attr_walkDistance = plan["walkDistance"]
+            self._attr_transfers = plan["transfers"]
+
+        except HTTPError:
+            _LOGGER.error(
+                "Unable to fetch data from TMB API. Please check your API keys."
             )
