593188fc4c044433117f29165c6795421b6bd2bd
Geoffrey Lagaisse <geoffreylagaisse@gmail.com>
2020-11-23 22:23:50 +0000

    Added some services

--- homeassistant/components/qbittorrent/services.py
+++ homeassistant/components/qbittorrent/services.py
@@ -1,28 +1,28 @@
 """Support for services the qBittorrent API."""
 import logging
+
 import voluptuous as vol
-
-from qbittorrent.client import LoginRequired
-from requests.exceptions import RequestException
-
-from homeassistant.helpers.dispatcher import async_dispatcher_send
-
-from homeassistant.helpers.entity import Entity
 
 from .const import (
     DATA_KEY_CLIENT,
-    DATA_KEY_NAME,
     DOMAIN,
     SERVICE_ADD_DOWNLOAD,
     SERVICE_REMOVE_DOWNLOAD,
 )
-from .wrapper_functions import get_main_data_client, retrieve_torrentdata, create_client
 
 DOWNLOAD_SCHEMA = vol.Schema(
     {
         vol.Optional("download_path"): str,
-        vol.Required("server_url"): str,
+        vol.Optional("server_url"): str,
         vol.Required("magnet_link"): str,
+    }
+)
+
+REMOVE_SCHEMA = vol.Schema(
+    {
+        vol.Optional("server_url"): str,
+        vol.Required("torrent_hash"): str,
+        vol.Optional("delete_permanent"): bool,
     }
 )
 
@@ -45,16 +45,65 @@ async def async_setup_services(hass):
         schema=DOWNLOAD_SCHEMA,
     )
     hass.services.async_register(
-        DOMAIN, SERVICE_REMOVE_DOWNLOAD, async_remove_download_service
+        DOMAIN,
+        SERVICE_REMOVE_DOWNLOAD,
+        async_remove_download_service,
+        schema=REMOVE_SCHEMA,
     )
 
     return True
 
 
 def add_download(hass, service_call):
-    qbit_data = hass.data[DOMAIN][service_call["server_url"]]
+    """Download a magnetlink."""
+    qbit_url = service_call.data.get("server_url")
+    downloadPath = service_call.data.get("download_path")
+    magnet = service_call.data.get("magnet_link")
+    client = get_qbitclient(hass, qbit_url)[DATA_KEY_CLIENT]
+
+    client.download_from_link(magnet, savepath=downloadPath)
     return True
 
 
+def get_qbitclient(hass, server_url=None):
+    """Retrieve a configured qbittorrent clients by name."""
+    qbittdata = hass.data[DOMAIN].values()
+
+    if server_url:
+        plex_server = next(
+            (x for x in qbittdata if x.friendly_name == server_url), None
+        )
+        if plex_server is not None:
+            return plex_server
+        _LOGGER.error(
+            "Requested Qbitclient '%s' not found in %s",
+            server_url,
+            [x.friendly_name for x in server_url],
+        )
+        return None
+
+    if len(qbittdata) == 1:
+        return next(iter(qbittdata))
+
+    _LOGGER.error(
+        "Multiple Qbittorrent clients configured, choose with 'server_url' key: %s",
+        [x.friendly_name for x in qbittdata],
+    )
+    return None
+
+
 def remove_download(hass, service_call):
+    """Remove a download from the download/seed list."""
+    qbit_url = service_call.data.get("server_url")
+    hash = service_call.data.get("torrent_hash")
+    delete_permanent = service_call.data.get("delete_permanent")
+    if delete_permanent is None:
+        delete_permanent = False
+
+    client = get_qbitclient(hass, qbit_url)[DATA_KEY_CLIENT]
+
+    if delete_permanent is True:
+        client.delete_permanently(hash)
+    else:
+        client.delete(hash)
     return True
