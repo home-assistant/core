37ec0b7ee9ce24864838c5d12a29d215ce87ca7c
jpeterson37 <joshuapeterson37@yahoo.com>
2020-11-24 22:00:54 +0000

    updater

--- homeassistant/components/sleepiq/__init__.py
+++ homeassistant/components/sleepiq/__init__.py
@@ -102,7 +102,6 @@ class SleepIQData:
 
     def set_new_sleep_number(self, bed_name: str, side: str, sleep_number):
         """Change the sleep number on a side to a new value."""
-
         # Sanity check: Sleep number has to be between 1-100.
         if 0 < int(sleep_number) <= 100 and int(sleep_number) % 5 == 0:
             self._set_sleep_number(bed_name, side, int(sleep_number))
