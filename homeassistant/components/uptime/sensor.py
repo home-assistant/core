8fd7c652d3f146ce8b7e59e48a8c46fe5cbafbf9
chemelli74 <simone.chemelli@gmail.com>
2020-11-25 09:46:17 +0100

    Moved from relative time to absolute time

--- homeassistant/components/uptime/sensor.py
+++ homeassistant/components/uptime/sensor.py
@@ -3,8 +3,8 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME, CONF_UNIT_OF_MEASUREMENT
+from homeassistant.components.sensor import DEVICE_CLASS_TIMESTAMP, PLATFORM_SCHEMA
+from homeassistant.const import CONF_NAME
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 import homeassistant.util.dt as dt_util
@@ -13,14 +13,9 @@ _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "Uptime"
 
-ICON = "mdi:clock"
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Optional(CONF_UNIT_OF_MEASUREMENT, default="days"): vol.All(
-            cv.string, vol.In(["minutes", "hours", "days", "seconds"])
-        ),
     }
 )
 
@@ -28,19 +23,17 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the uptime sensor platform."""
     name = config.get(CONF_NAME)
-    units = config.get(CONF_UNIT_OF_MEASUREMENT)
 
-    async_add_entities([UptimeSensor(name, units)], True)
+    async_add_entities([UptimeSensor(name)], True)
 
 
 class UptimeSensor(Entity):
     """Representation of an uptime sensor."""
 
-    def __init__(self, name, unit):
+    def __init__(self, name):
         """Initialize the uptime sensor."""
         self._name = name
-        self._unit = unit
-        self.initial = dt_util.now()
+        self.initial = dt_util.now().isoformat()
         self._state = None
 
     @property
@@ -49,14 +42,9 @@ class UptimeSensor(Entity):
         return self._name
 
     @property
-    def icon(self):
-        """Icon to display in the front end."""
-        return ICON
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement the value is expressed in."""
-        return self._unit
+    def device_class(self):
+        """Return device class."""
+        return DEVICE_CLASS_TIMESTAMP
 
     @property
     def state(self):
@@ -65,16 +53,4 @@ class UptimeSensor(Entity):
 
     async def async_update(self):
         """Update the state of the sensor."""
-        delta = dt_util.now() - self.initial
-        div_factor = 3600
-
-        if self.unit_of_measurement == "days":
-            div_factor *= 24
-        elif self.unit_of_measurement == "minutes":
-            div_factor /= 60
-        elif self.unit_of_measurement == "seconds":
-            div_factor /= 3600
-
-        delta = delta.total_seconds() / div_factor
-        self._state = round(delta, 2)
-        _LOGGER.debug("New value: %s", delta)
+        self._state = self.initial
