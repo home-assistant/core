6969c65cfc8b8edef4d7c36fa2214b4ccf020d4f
Bram Kragten <mail@bramkragten.nl>
2020-11-25 17:35:10 +0100

    Add more selectors

--- homeassistant/helpers/selector.py
+++ homeassistant/helpers/selector.py
@@ -3,6 +3,7 @@ from typing import Any, Callable, Dict, cast
 
 import voluptuous as vol
 
+from homeassistant.const import CONF_MODE, CONF_UNIT_OF_MEASUREMENT
 from homeassistant.util import decorator
 
 SELECTORS = decorator.Registry()
@@ -66,5 +67,41 @@ class DeviceSelector(Selector):
             vol.Optional("manufacturer"): str,
             # Model of device
             vol.Optional("model"): str,
+        }
+    )
+
+
+@SELECTORS.register("number")
+class NumberSelector(Selector):
+    """Selector of a numeric value."""
+
+    CONFIG_SCHEMA = vol.Schema(
+        {
+            vol.Required("min"): vol.Coerce(float),
+            vol.Required("max"): vol.Coerce(float),
+            vol.Optional("step", default=1): vol.All(
+                vol.Coerce(float), vol.Range(min=1e-3)
+            ),
+            vol.Optional(CONF_UNIT_OF_MEASUREMENT): str,
+            vol.Optional(CONF_MODE, default="slider"): vol.In(["box", "slider"]),
+        }
+    )
+
+
+@SELECTORS.register("boolean")
+class BooleanSelector(Selector):
+    """Selector of a boolean value."""
+
+    CONFIG_SCHEMA = vol.Schema({})
+
+
+@SELECTORS.register("datetime")
+class DateTimeSelector(Selector):
+    """Selector of a date and or time value."""
+
+    CONFIG_SCHEMA = vol.Schema(
+        {
+            vol.Optional("has_date", default=False): bool,
+            vol.Optional("has_time", default=False): bool,
         }
     )
