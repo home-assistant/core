a4c0b8ad07e6b78208bed89670a89ed498f851a8
brefra <frank_van_breugel@hotmail.com>
2020-11-26 22:28:54 +0100

    Remove unused import

--- homeassistant/components/plugwise/__init__.py
+++ homeassistant/components/plugwise/__init__.py
@@ -4,7 +4,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 
-from .const import CONF_USB_PATH, DOMAIN
+from .const import CONF_USB_PATH
 from .gateway import async_setup_entry_gw, async_unload_entry_gw
 from .usb import async_setup_entry_usb, async_unload_entry_usb
 
