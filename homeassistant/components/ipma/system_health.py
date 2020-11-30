8777427ce601bb6b4ae1bfe5dafa3dc185b2587d
Diogo Gomes <dgomes@ua.pt>
2020-11-30 12:15:15 +0000

    const

--- homeassistant/components/ipma/system_health.py
+++ homeassistant/components/ipma/system_health.py
@@ -2,6 +2,8 @@
 from homeassistant.components import system_health
 from homeassistant.core import HomeAssistant, callback
 
+IPMA_API_URL = "http://api.ipma.pt"
+
 
 @callback
 def async_register(
@@ -15,6 +17,6 @@ async def system_health_info(hass):
     """Get info for the info page."""
     return {
         "api_endpoint_reachable": system_health.async_check_can_reach_url(
-            hass, "https://api.ipma.pt"
+            hass, IPMA_API_URL
         )
     }
--- tests/components/ipma/test_system_health.py
+++ tests/components/ipma/test_system_health.py
@@ -1,6 +1,7 @@
 """Test ipma system health."""
 import asyncio
 
+from homeassistant.components.ipma.system_health import IPMA_API_URL
 from homeassistant.setup import async_setup_component
 
 from tests.common import get_system_health_info
@@ -8,7 +9,7 @@ from tests.common import get_system_health_info
 
 async def test_ipma_system_health(hass, aioclient_mock):
     """Test ipma system health."""
-    aioclient_mock.get("https://api.ipma.pt", json={"result": "ok", "data": {}})
+    aioclient_mock.get(IPMA_API_URL, json={"result": "ok", "data": {}})
 
     hass.config.components.add("ipma")
     assert await async_setup_component(hass, "system_health", {})
