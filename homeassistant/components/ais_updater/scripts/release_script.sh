e84dfc05c0bd5f248219cf2c796b488f62e7d06b
tomasz75 <tomek@sviete.pl>
2020-12-01 10:45:48 +0100

    0.118.4b1

--- homeassistant/components/ais_updater/scripts/release_script.sh
+++ homeassistant/components/ais_updater/scripts/release_script.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-echo "AIS START"; 
-echo "AIS remove data from tmp"; 
-su -c "rm -rf /data/data/pl.sviete.dom/files/usr/tmp/*" 
-echo "New zigbee frontend"; 
-curl https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/add_zigbee_frontend.sh | bash 
-echo "AIS DomApp-client-gate-release.apk"; 
+echo "AIS release script starting for 20.10.07 on chanel 2" 
 curl -o "/sdcard/AisClient.apk" -L https://powiedz.co/ota/android/AisDomApp-client-gate-release.apk && su -c "pm install -r /sdcard/AisClient.apk" 
-echo "20.10.07" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
+
+echo "20.11.24" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
