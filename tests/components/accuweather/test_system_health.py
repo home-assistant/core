4e80a25c80034effd7cb872374b1d5191633071d
Maciej Bieniek <bieniu@users.noreply.github.com>
2020-12-02 19:36:16 +0100

    Add support for system health to AccuWeather integration (#43277)

---
+++ tests/components/accuweather/test_system_health.py
@@ -0,0 +1,58 @@
+"""Test AccuWeather system health."""
+import asyncio
+
+from aiohttp import ClientError
+
+from homeassistant.components.accuweather.const import COORDINATOR, DOMAIN
+from homeassistant.setup import async_setup_component
+
+from tests.async_mock import Mock
+from tests.common import get_system_health_info
+
+
+async def test_accuweather_system_health(hass, aioclient_mock):
+    """Test AccuWeather system health."""
+    aioclient_mock.get("https://dataservice.accuweather.com/", text="")
+    hass.config.components.add(DOMAIN)
+    assert await async_setup_component(hass, "system_health", {})
+
+    hass.data[DOMAIN] = {}
+    hass.data[DOMAIN]["0123xyz"] = {}
+    hass.data[DOMAIN]["0123xyz"][COORDINATOR] = Mock(
+        accuweather=Mock(requests_remaining="42")
+    )
+
+    info = await get_system_health_info(hass, DOMAIN)
+
+    for key, val in info.items():
+        if asyncio.iscoroutine(val):
+            info[key] = await val
+
+    assert info == {
+        "can_reach_server": "ok",
+        "remaining_requests": "42",
+    }
+
+
+async def test_accuweather_system_health_fail(hass, aioclient_mock):
+    """Test AccuWeather system health."""
+    aioclient_mock.get("https://dataservice.accuweather.com/", exc=ClientError)
+    hass.config.components.add(DOMAIN)
+    assert await async_setup_component(hass, "system_health", {})
+
+    hass.data[DOMAIN] = {}
+    hass.data[DOMAIN]["0123xyz"] = {}
+    hass.data[DOMAIN]["0123xyz"][COORDINATOR] = Mock(
+        accuweather=Mock(requests_remaining="0")
+    )
+
+    info = await get_system_health_info(hass, DOMAIN)
+
+    for key, val in info.items():
+        if asyncio.iscoroutine(val):
+            info[key] = await val
+
+    assert info == {
+        "can_reach_server": {"type": "failed", "error": "unreachable"},
+        "remaining_requests": "0",
+    }
