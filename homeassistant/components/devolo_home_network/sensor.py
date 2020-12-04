6fee7ce244aef74ebab87018a40a4a13d469d1e4
Markus Bong <2Fake1987@gmail.com>
2020-12-04 11:40:50 +0100

    1st try to use two devices

--- homeassistant/components/devolo_home_network/sensor.py
+++ homeassistant/components/devolo_home_network/sensor.py
@@ -33,66 +33,69 @@ async def async_setup_entry(
 ) -> None:
     """Get all devices and sensors and setup them via config entry."""
     entities = []
+    device = hass.data[DOMAIN][entry.entry_id]
+    print(entry.entry_id)
 
-    for device in hass.data[DOMAIN][entry]["devices"]:
+    async def async_update_data():
+        """Fetch data from API endpoint.
 
-        async def async_update_data():
-            """Fetch data from API endpoint.
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        try:
+            async with async_timeout.timeout(60):
+                calls = await asyncio.gather(
+                    device.plcnet.async_get_network_overview(),
+                    device.device.async_get_wifi_connected_station(),
+                )
+                #  device.device.async_get_wifi_neighbor_access_points())
+                dict_call = {}
+                for call in calls:
+                    for e in call:
+                        dict_call[e] = call[e]
+                # We pre process our data - Think about it!
+                calls[0] = {
+                    "Home Network Devices": len(calls[0]["network"]["data_rates"]) // 2
+                }
+                calls[1] = {"Wifi Clients": len(calls[1]["connected_stations"])}
+                # calls[2] = {"Neighbor Wifi Networks": len(calls[2]["neighbor_aps"])}
+                return calls
+        except KeyError as err:
+            raise UpdateFailed(f"Error communicating with API: {err}")
 
-            This is the place to pre-process the data to lookup tables
-            so entities can quickly look up their data.
-            """
-            try:
-                async with async_timeout.timeout(60):
-                    calls = await asyncio.gather(
-                        device.plcnet.async_get_network_overview(),
-                        device.device.async_get_wifi_connected_station(),
-                    )
-                    #  device.device.async_get_wifi_neighbor_access_points())
-                    dict_call = {}
-                    for call in calls:
-                        for e in call:
-                            dict_call[e] = call[e]
-                    # We pre process our data - Think about it!
-                    calls[0] = {
-                        "Home Network Devices": len(calls[0]["network"]["data_rates"])
-                        // 2
-                    }
-                    calls[1] = {"Wifi Clients": len(calls[1]["connected_stations"])}
-                    # calls[2] = {"Neighbor Wifi Networks": len(calls[2]["neighbor_aps"])}
-                    return calls
-            except KeyError as err:
-                raise UpdateFailed(f"Error communicating with API: {err}")
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        # Name of the data. For logging purposes.
+        name="sensor",
+        update_method=async_update_data,
+        # Polling interval. Will only be polled if there are subscribers.
+        update_interval=timedelta(seconds=10),
+    )
 
-        coordinator = DataUpdateCoordinator(
-            hass,
-            _LOGGER,
-            # Name of the data. For logging purposes.
-            name="sensor",
-            update_method=async_update_data,
-            # Polling interval. Will only be polled if there are subscribers.
-            update_interval=timedelta(seconds=10),
-        )
-
-        await coordinator.async_refresh()
-
-        for idx, sensor in enumerate(SENSORS):
-            entities.append(DevoloDevice(device, coordinator, idx, sensor))
+    await coordinator.async_refresh()
+    print(device)
+    for idx, sensor in enumerate(SENSORS):
+        print(device.mac)
+        entities.append(DevoloDevice(device, coordinator, idx, sensor, entry.title))
     async_add_entities(entities, True)
 
 
 class DevoloDevice(CoordinatorEntity, Entity):
     """Representation of a devolo home network device."""
 
-    def __init__(self, device, coordinator, index, sensor):
+    def __init__(self, device, coordinator, index, sensor, device_name):
         """Initialize a devolo home network device."""
         super().__init__(coordinator)
+        print(device)
         self.device = device
         self._unique_id = device.mac
+        print(self._unique_id)
         self._state = ""
         self._index = index
-        self.device_name = self.device.hostname.split(".")[0]
-        self._name = sensor
+        self.device_name = device_name
+        self._name = f"{self.device_name}_{sensor}"
+        self._sensor = sensor
 
     @property
     def name(self):
@@ -117,7 +120,7 @@ class DevoloDevice(CoordinatorEntity, Entity):
         """Return the state of the device."""
         print(f"STATE {self._name} {self.device_name}")
         try:
-            return self.coordinator.data[self._index][self._name]
+            return self.coordinator.data[self._index][self._sensor]
         except KeyError:
             pass
         # return self._state
