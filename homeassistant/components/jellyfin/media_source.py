65fed4eb14fb37f93b470bfcce97acb8fc1ea121
Stienstra.Jan@gmail.com <Stienstra.Jan@gmail.com>
2020-12-06 19:46:55 +0000

    Further improvements. Browsing is working. Now need to work on proper stream URL

--- homeassistant/components/jellyfin/media_source.py
+++ homeassistant/components/jellyfin/media_source.py
@@ -1,6 +1,7 @@
 import logging
+from typing import Tuple, List
+from aiohttp import web
 import mimetypes
-from typing import Tuple
 
 from jellyfin_apiclient_python.client import JellyfinClient
 from jellyfin_apiclient_python.api import jellyfin_url
@@ -11,10 +12,7 @@ from homeassistant.components.media_player.const import (
     MEDIA_CLASS_DIRECTORY,
     MEDIA_CLASS_MUSIC,
     MEDIA_CLASS_VIDEO,
-    MEDIA_CLASS_IMAGE,
-    MEDIA_TYPE_MOVIE,
 )
-from homeassistant.components.media_source.const import MEDIA_CLASS_MAP
 
 from homeassistant.core import HomeAssistant
 
@@ -25,7 +23,14 @@ from homeassistant.components.media_source.models import (
     PlayMedia,
 )
 
-from .const import DATA_CLIENT, DOMAIN  # pylint:disable=unused-import
+from .const import (
+    SUPPORTED_COLLECTION_TYPES,
+    COLLECTION_TYPE_MOVIES,
+    COLLECTION_TYPE_TVSHOWS,
+    COLLECTION_TYPE_MUSIC,
+    DATA_CLIENT,
+    DOMAIN,
+)  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -36,7 +41,10 @@ async def async_get_media_source(hass: HomeAssistant):
     data = hass.data[DOMAIN][entry.entry_id]
     client = data[DATA_CLIENT]
 
-    return JellyfinSource(hass, client)
+    source = JellyfinSource(hass, client)
+    hass.http.register_view(JellyfinMediaView(hass, source))
+
+    return source
 
 
 def parse_identifier(item: MediaSourceItem) -> Tuple[str, str]:
@@ -46,25 +54,39 @@ def parse_identifier(item: MediaSourceItem) -> Tuple[str, str]:
     return tuple(items + start[len(items) :])
 
 
-def _media_mime_type(media_class: str) -> str:
-    if media_class == MEDIA_CLASS_VIDEO:
-        return "video"
-    elif media_class == MEDIA_CLASS_MUSIC:
-        return "audio"
-    elif media_class == MEDIA_CLASS_IMAGE:
-        return "image"
-    else:
-        raise BrowseError("Unsupported media mime type %s" % type)
+def _media_mime_type(media_item: dict) -> str:
+    media_source = media_item["MediaSources"][0]
+    path = media_source["Path"]
+    mime_type, _ = mimetypes.guess_type(path)
+    return mime_type
 
 
 def _media_class(collection_type: str) -> str:
     """ Takes a Jellyfin collection type and return the corresponding media class """
-    if collection_type == "movies" or collection_type == "tvshows":
+    if (
+        collection_type == COLLECTION_TYPE_MOVIES
+        or collection_type == COLLECTION_TYPE_TVSHOWS
+    ):
         return MEDIA_CLASS_VIDEO
-    elif collection_type == "music":
+    elif collection_type == COLLECTION_TYPE_MUSIC:
         return MEDIA_CLASS_MUSIC
     else:
-        raise BrowseError(f"Unsupported collection type {collection_type}")
+        raise BrowseError("Unsupported collection type %s" % collection_type)
+
+
+def _is_playable(item: dict) -> bool:
+    if item["IsFolder"]:
+        return False
+
+    media_sources = item["MediaSources"]
+    if len(media_sources) == 0:
+        return False
+
+    media_source = media_sources[0]
+    if media_source["Type"] != "Default":
+        return False
+
+    return True
 
 
 class JellyfinSource(MediaSource):
@@ -85,28 +107,33 @@ class JellyfinSource(MediaSource):
         self._name = client.auth.get_server_info(server_id)["Name"]
 
     async def async_resolve_media(self, item: MediaSourceItem) -> PlayMedia:
-        media_item = self.api.get_item(item.identifier)
-        path = media_item["MediaStreams"][0]["Path"]
-        mime_type, _ = mimetypes.guess_type(path)
+        _, item_id = parse_identifier(item)
 
-        return PlayMedia(f"{self.url}/Items/{item.identifier}", mime_type)
+        media_item = await self.hass.async_add_executor_job(self.api.get_item, item_id)
+
+        stream_url = self._get_stream_url(media_item)
+        mime_type = _media_mime_type(media_item)
+
+        print(stream_url, mime_type)
+        return PlayMedia(stream_url, mime_type)
 
     async def async_browse_media(self, item: MediaSourceItem) -> BrowseMediaSource:
-        library, item_id = parse_identifier(item)
+        library_id, item_id = parse_identifier(item)
 
-        if not library:
+        if not library_id:
             return await self._build_libraries()
 
+        library = await self.hass.async_add_executor_job(self.api.get_item, library_id)
+        media_class = _media_class(library["CollectionType"])
+
         if not item_id:
-            result = self.api.get_item(library)
-            return self._build_library(library, result["CollectionType"])
+            return await self._build_library(library, media_class)
 
-        library_item = self.api.get_item(library)
         media_item = self.api.get_item(item_id)
-        media_class = _media_class(library_item["CollectionType"])
-        return self._processMediaItem(library, media_item, media_class)
 
-    async def _build_libraries(self):
+        return self._processMediaItem(library_id, media_item, media_class)
+
+    async def _build_libraries(self) -> BrowseMediaSource:
         base = BrowseMediaSource(
             domain=DOMAIN,
             identifier="",
@@ -120,62 +147,68 @@ class JellyfinSource(MediaSource):
 
         libraries = await self._get_libraries()
 
-        base.children = [
-            await self._build_library(library["Id"], library["CollectionType"])
-            for library in libraries
-        ]
+        base.children = []
+
+        for library in libraries:
+            media_class = _media_class(library["CollectionType"])
+            base.children.append(await self._build_library(library, media_class))
 
         return base
 
     async def _get_libraries(self):
-        result = await self.hass.async_add_executor_job(self.api.get_media_folders)
-        return result["Items"]
+        response = await self.hass.async_add_executor_job(self.api.get_media_folders)
+        libraries = response["Items"]
+        result = []
+        for library in libraries:
+            if library["CollectionType"] in SUPPORTED_COLLECTION_TYPES:
+                result.append(library)
+        return result
 
     async def _build_library(
-        self, library: str, collection_type: str
+        self, library: dict, media_class: str
     ) -> BrowseMediaSource:
-        _LOGGER.info(
-            f"Bulding library {library} with collection type {collection_type}"
-        )
+        id = library["Id"]
+        name = library["Name"]
 
-        children_media_class = _media_class(collection_type)
+        _LOGGER.info(f"Bulding library {name} with media class {media_class}")
 
         librarySource = BrowseMediaSource(
             domain=DOMAIN,
-            identifier=library,
+            identifier=id,
             media_class=MEDIA_CLASS_DIRECTORY,
             media_content_type=None,
-            title=library,
+            title=name,
             can_play=False,
             can_expand=True,
-            children_media_class=children_media_class,
+            children_media_class=media_class,
         )
 
-        result = await self.hass.async_add_executor_job(
-            self.api.user_items, "", {"Recursive": "true", "ParentId": library}
-        )
-        items = result["Items"]
+        library_members = await self._get_library_members(id)
 
         librarySource.children = []
-        for item in items:
-            print(item)
-            if not item["IsFolder"]:
-                child = self._processMediaItem(library, item, children_media_class)
+        for library_member in library_members:
+            if _is_playable(library_member):
+                child = self._processMediaItem(id, library_member, media_class)
                 librarySource.children.append(child)
-
         return librarySource
 
-    def _processMediaItem(
-        self, library: str, item: dict, media_class: str
-    ) -> BrowseMediaSource:
-        item_id = item["Id"]
-        title = item["Name"]
+    async def _get_library_members(self, library_id: str) -> List[dict]:
+        params = {"Recursive": "true", "ParentId": library_id, "Fields": "MediaSources"}
 
-        mime_type = _media_mime_type(media_class)
+        result = await self.hass.async_add_executor_job(self.api.user_items, "", params)
+        return result["Items"]
+
+    def _processMediaItem(
+        self, library_id: str, item: dict, media_class: str
+    ) -> BrowseMediaSource:
+
+        id = item["Id"]
+        title = item["Name"]
+        mime_type = _media_mime_type(item)
 
         media = BrowseMediaSource(
             domain=DOMAIN,
-            identifier=f"{library}~~{self.url}/Items/{item_id}",
+            identifier=f"{library_id}~~{id}",
             media_class=media_class,
             media_content_type=mime_type,
             title=title,
@@ -185,6 +218,15 @@ class JellyfinSource(MediaSource):
 
         return media
 
+    def _get_stream_url(self, media_item: dict) -> str:
+        id = media_item["Id"]
+        media_type = media_item["MediaType"]
+
+        if media_type == "Video":
+            return f"{self.url}/Videos/{id}/stream"
+        else:
+            return f"{self.url}/Audio/{id}/stream"
+
 
 class JellyfinMediaView(HomeAssistantView):
     """
@@ -193,7 +235,14 @@ class JellyfinMediaView(HomeAssistantView):
     Returns media files in Jellyfin libraries for configured user
     """
 
+    name = "Jellyfin"
+    url = "/media/jellyfin/{item_id}"
+
     def __init__(self, hass: HomeAssistant, source: JellyfinSource):
         """Initialize the media view."""
         self.hass = hass
         self.source = source
+
+    async def get(self, request: web.Request, item_id: str) -> web.Response:
+        url = f"{self.source.url}/Items/{item_id}/Download"
+        return web.FileResponse(url)
