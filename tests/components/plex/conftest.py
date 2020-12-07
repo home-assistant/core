2d4c4a608d28a1fdc5c50249cf6b0c7bd0a21005
Jason Lawrence <jjlawren@users.noreply.github.com>
2020-12-07 13:09:14 -0600

    Convert Plex tests to use XML payloads

--- tests/components/plex/conftest.py
+++ tests/components/plex/conftest.py
@@ -3,13 +3,41 @@ from unittest.mock import patch
 
 import pytest
 
-from homeassistant.components.plex.const import DOMAIN
+from homeassistant.components.plex.const import DOMAIN, PLEX_SERVER_CONFIG, SERVERS
+from homeassistant.const import CONF_URL
 
-from .const import DEFAULT_DATA, DEFAULT_OPTIONS
+from .const import DEFAULT_DATA, DEFAULT_OPTIONS, PLEX_DIRECT_URL
 from .helpers import websocket_connected
-from .mock_classes import MockGDM, MockPlexAccount, MockPlexServer
+from .mock_classes import MockGDM
+from .payloads import (
+    EMPTY_LIBRARY,
+    EMPTY_PAYLOAD,
+    LIBRARY_MOVIES_SORT,
+    LIBRARY_MUSIC_SORT,
+    LIBRARY_TVSHOWS_SORT,
+    PLAYLISTS_PAYLOAD,
+    PLEX_SERVER_PAYLOAD,
+    PLEXTV_ACCOUNT_PAYLOAD,
+    PLEXTV_RESOURCES,
+    PMS_ACCOUNT_PAYLOAD,
+    PMS_CLIENTS,
+    PMS_LIBRARY_PAYLOAD,
+    PMS_LIBRARY_SECTIONS_PAYLOAD,
+    SECURITY_TOKEN,
+    fetch_children,
+    fetch_grandchildren,
+    fetch_media,
+    fetch_playlist,
+    generate_library_all,
+    generate_session,
+)
 
 from tests.common import MockConfigEntry
+
+
+def plex_server_url(entry):
+    """Return a protocol-less URL from a config entry."""
+    return entry.data[PLEX_SERVER_CONFIG][CONF_URL].split(":", 1)[-1]
 
 
 @pytest.fixture(name="entry")
@@ -24,14 +52,6 @@ def mock_config_entry():
 
 
 @pytest.fixture
-def mock_plex_account():
-    """Mock the PlexAccount class and return the used instance."""
-    plex_account = MockPlexAccount()
-    with patch("plexapi.myplex.MyPlexAccount", return_value=plex_account):
-        yield plex_account
-
-
-@pytest.fixture
 def mock_websocket():
     """Mock the PlexWebsocket class."""
     with patch("homeassistant.components.plex.PlexWebsocket", autospec=True) as ws:
@@ -39,15 +59,75 @@ def mock_websocket():
 
 
 @pytest.fixture
-def setup_plex_server(hass, entry, mock_plex_account, mock_websocket):
+def mock_plex_calls(entry, requests_mock):
+    """Mock Plex API calls."""
+    requests_mock.get("https://plex.tv/users/account", text=PLEXTV_ACCOUNT_PAYLOAD)
+    requests_mock.get("https://plex.tv/api/resources", text=PLEXTV_RESOURCES)
+
+    url = plex_server_url(entry)
+
+    for server in [url, PLEX_DIRECT_URL]:
+        requests_mock.get(server, text=PLEX_SERVER_PAYLOAD)
+        requests_mock.get(f"{server}/accounts", text=PMS_ACCOUNT_PAYLOAD)
+
+    requests_mock.get(f"{url}/clients", text=PMS_CLIENTS)
+    requests_mock.get(f"{url}/library", text=PMS_LIBRARY_PAYLOAD)
+    requests_mock.get(f"{url}/library/sections", text=PMS_LIBRARY_SECTIONS_PAYLOAD)
+
+    requests_mock.get(f"{url}/library/onDeck", text=EMPTY_LIBRARY)
+    requests_mock.get(f"{url}/library/sections/1/sorts", text=LIBRARY_MOVIES_SORT)
+    requests_mock.get(f"{url}/library/sections/2/sorts", text=LIBRARY_TVSHOWS_SORT)
+    requests_mock.get(f"{url}/library/sections/3/sorts", text=LIBRARY_MUSIC_SORT)
+
+    requests_mock.get(
+        f"{url}/library/sections/1/all", text=generate_library_all("movie")
+    )
+    requests_mock.get(
+        f"{url}/library/sections/2/all", text=generate_library_all("show")
+    )
+    requests_mock.get(
+        f"{url}/library/sections/3/all", text=generate_library_all("artist")
+    )
+
+    requests_mock.get(f"{url}/library/metadata/200/children", text=fetch_children(200))
+    requests_mock.get(f"{url}/library/metadata/300/children", text=fetch_children(300))
+    requests_mock.get(
+        f"{url}/library/metadata/300/allLeaves", text=fetch_grandchildren(300)
+    )
+
+    for media_key in [1, 30, 100, 200]:
+        requests_mock.get(
+            f"{url}/library/metadata/{media_key}", text=fetch_media(media_key)
+        )
+
+    requests_mock.get(f"{url}/library/metadata/20/children", text=fetch_children(20))
+    requests_mock.get(f"{url}/library/metadata/30/children", text=fetch_children(30))
+
+    requests_mock.get(f"{url}/playlists", text=PLAYLISTS_PAYLOAD)
+    requests_mock.get(f"{url}/playlists/500/items", text=fetch_playlist(500))
+    requests_mock.get(f"{url}/security/token", text=SECURITY_TOKEN)
+
+
+@pytest.fixture
+def setup_plex_server(hass, entry, mock_websocket, mock_plex_calls, requests_mock):
     """Set up and return a mocked Plex server instance."""
 
     async def _wrapper(**kwargs):
-        """Wrap the fixture to allow passing arguments to the MockPlexServer instance."""
+        """Wrap the fixture to allow passing arguments to the setup method."""
         config_entry = kwargs.get("config_entry", entry)
+        disable_clients = kwargs.pop("disable_clients", False)
         disable_gdm = kwargs.pop("disable_gdm", True)
-        plex_server = MockPlexServer(**kwargs)
-        with patch("plexapi.server.PlexServer", return_value=plex_server), patch(
+        client_type = kwargs.pop("client_type", "native")
+        session_type = kwargs.pop("session_type", "movie")
+
+        url = plex_server_url(entry)
+        session = generate_session(session_type, client_type)
+        requests_mock.get(f"{url}/status/sessions", text=session)
+
+        if disable_clients:
+            requests_mock.get(f"{url}/clients", text=EMPTY_PAYLOAD)
+
+        with patch(
             "homeassistant.components.plex.GDM",
             return_value=MockGDM(disabled=disable_gdm),
         ):
@@ -56,6 +136,8 @@ def setup_plex_server(hass, entry, mock_plex_account, mock_websocket):
             await hass.async_block_till_done()
             websocket_connected(mock_websocket)
             await hass.async_block_till_done()
+
+        plex_server = hass.data[DOMAIN][SERVERS][entry.unique_id]
         return plex_server
 
     return _wrapper
--- tests/components/plex/test_init.py
+++ tests/components/plex/test_init.py
@@ -1,6 +1,7 @@
 """Tests for Plex setup."""
 import copy
 from datetime import timedelta
+import logging
 import ssl
 from unittest.mock import patch
 
@@ -15,13 +16,22 @@ from homeassistant.config_entries import (
     ENTRY_STATE_SETUP_RETRY,
 )
 from homeassistant.const import CONF_TOKEN, CONF_URL, CONF_VERIFY_SSL, STATE_IDLE
+from homeassistant.setup import async_setup_component
 import homeassistant.util.dt as dt_util
 
-from .const import DEFAULT_DATA, DEFAULT_OPTIONS
+from .const import DEFAULT_DATA, DEFAULT_OPTIONS, PLEX_DIRECT_URL
 from .helpers import trigger_plex_update, wait_for_debouncer
-from .mock_classes import MockGDM, MockPlexAccount, MockPlexServer
+from .payloads import (
+    EMPTY_PAYLOAD,
+    PLEX_SERVER_PAYLOAD,
+    PLEXTV_ACCOUNT_PAYLOAD,
+    PLEXTV_RESOURCES,
+    PMS_ACCOUNT_PAYLOAD,
+)
 
 from tests.common import MockConfigEntry, async_fire_time_changed
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def test_set_config_entry_unique_id(hass, entry, mock_plex_server):
@@ -31,7 +41,7 @@ async def test_set_config_entry_unique_id(hass, entry, mock_plex_server):
 
     assert (
         hass.config_entries.async_entries(const.DOMAIN)[0].unique_id
-        == mock_plex_server.machineIdentifier
+        == mock_plex_server.machine_identifier
     )
 
 
@@ -79,9 +89,9 @@ async def test_unload_config_entry(hass, entry, mock_plex_server):
     assert entry is config_entries[0]
     assert entry.state == ENTRY_STATE_LOADED
 
-    server_id = mock_plex_server.machineIdentifier
+    server_id = mock_plex_server.machine_identifier
     loaded_server = hass.data[const.DOMAIN][const.SERVERS][server_id]
-    assert loaded_server.plex_server == mock_plex_server
+    assert loaded_server == mock_plex_server
 
     websocket = hass.data[const.DOMAIN][const.WEBSOCKETS][server_id]
     await hass.config_entries.async_unload(entry.entry_id)
@@ -89,7 +99,7 @@ async def test_unload_config_entry(hass, entry, mock_plex_server):
     assert entry.state == ENTRY_STATE_NOT_LOADED
 
 
-async def test_setup_with_photo_session(hass, entry, mock_websocket, setup_plex_server):
+async def test_setup_with_photo_session(hass, entry, setup_plex_server):
     """Test setup component with config."""
     await setup_plex_server(session_type="photo")
 
@@ -97,7 +107,9 @@ async def test_setup_with_photo_session(hass, entry, mock_websocket, setup_plex_
     assert entry.state == ENTRY_STATE_LOADED
     await hass.async_block_till_done()
 
-    media_player = hass.states.get("media_player.plex_product_title")
+    media_player = hass.states.get(
+        "media_player.plex_plex_for_android_tv_shield_android_tv"
+    )
     assert media_player.state == STATE_IDLE
 
     await wait_for_debouncer(hass)
@@ -106,10 +118,13 @@ async def test_setup_with_photo_session(hass, entry, mock_websocket, setup_plex_
     assert sensor.state == "0"
 
 
-async def test_setup_when_certificate_changed(hass, entry):
+async def test_setup_when_certificate_changed(hass, requests_mock):
     """Test setup component when the Plex certificate has changed."""
+    await async_setup_component(hass, "persistent_notification", {})
+    requests_mock.get("https://plex.tv/users/account", text=PLEXTV_ACCOUNT_PAYLOAD)
+    requests_mock.get("https://plex.tv/api/resources", text=PLEXTV_RESOURCES)
 
-    old_domain = "1-2-3-4.1234567890abcdef1234567890abcdef.plex.direct"
+    old_domain = "1-2-3-4.1111111111ffffff1111111111ffffff.plex.direct"
     old_url = f"https://{old_domain}:32400"
 
     OLD_HOSTNAME_DATA = copy.deepcopy(DEFAULT_DATA)
@@ -123,6 +138,8 @@ async def test_setup_when_certificate_changed(hass, entry):
                 f"hostname '{old_domain}' doesn't match"
             )
 
+    requests_mock.get(old_url, exc=WrongCertHostnameException)
+
     old_entry = MockConfigEntry(
         domain=const.DOMAIN,
         data=OLD_HOSTNAME_DATA,
@@ -131,45 +148,40 @@ async def test_setup_when_certificate_changed(hass, entry):
     )
 
     # Test with account failure
-    with patch(
-        "plexapi.server.PlexServer", side_effect=WrongCertHostnameException
-    ), patch(
-        "plexapi.myplex.MyPlexAccount", side_effect=plexapi.exceptions.Unauthorized
-    ):
-        old_entry.add_to_hass(hass)
-        assert await hass.config_entries.async_setup(old_entry.entry_id) is False
-        await hass.async_block_till_done()
+    requests_mock.get(f"{old_url}/accounts", status_code=401)
+    old_entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(old_entry.entry_id) is False
+    await hass.async_block_till_done()
 
     assert old_entry.state == ENTRY_STATE_SETUP_ERROR
     await hass.config_entries.async_unload(old_entry.entry_id)
 
     # Test with no servers found
-    with patch(
-        "plexapi.server.PlexServer", side_effect=WrongCertHostnameException
-    ), patch("plexapi.myplex.MyPlexAccount", return_value=MockPlexAccount(servers=0)):
-        assert await hass.config_entries.async_setup(old_entry.entry_id) is False
-        await hass.async_block_till_done()
+    requests_mock.get(f"{old_url}/accounts", text=PMS_ACCOUNT_PAYLOAD)
+    requests_mock.get("https://plex.tv/api/resources", text=EMPTY_PAYLOAD)
+
+    assert await hass.config_entries.async_setup(old_entry.entry_id) is False
+    await hass.async_block_till_done()
 
     assert old_entry.state == ENTRY_STATE_SETUP_ERROR
     await hass.config_entries.async_unload(old_entry.entry_id)
 
     # Test with success
-    with patch(
-        "plexapi.server.PlexServer", side_effect=WrongCertHostnameException
-    ), patch("plexapi.myplex.MyPlexAccount", return_value=MockPlexAccount()):
-        assert await hass.config_entries.async_setup(old_entry.entry_id)
-        await hass.async_block_till_done()
+    new_url = PLEX_DIRECT_URL
+    requests_mock.get("https://plex.tv/api/resources", text=PLEXTV_RESOURCES)
+    requests_mock.get(new_url, text=PLEX_SERVER_PAYLOAD)
+    requests_mock.get(f"{new_url}/accounts", text=PMS_ACCOUNT_PAYLOAD)
+
+    assert await hass.config_entries.async_setup(old_entry.entry_id)
+    await hass.async_block_till_done()
 
     assert len(hass.config_entries.async_entries(const.DOMAIN)) == 1
     assert old_entry.state == ENTRY_STATE_LOADED
 
-    assert (
-        old_entry.data[const.PLEX_SERVER_CONFIG][CONF_URL]
-        == entry.data[const.PLEX_SERVER_CONFIG][CONF_URL]
-    )
+    assert old_entry.data[const.PLEX_SERVER_CONFIG][CONF_URL] == new_url
 
 
-async def test_tokenless_server(hass, entry, mock_websocket, setup_plex_server):
+async def test_tokenless_server(entry, setup_plex_server):
     """Test setup with a server with token auth disabled."""
     TOKENLESS_DATA = copy.deepcopy(DEFAULT_DATA)
     TOKENLESS_DATA[const.PLEX_SERVER_CONFIG].pop(CONF_TOKEN, None)
@@ -179,18 +191,13 @@ async def test_tokenless_server(hass, entry, mock_websocket, setup_plex_server):
     assert entry.state == ENTRY_STATE_LOADED
 
 
-async def test_bad_token_with_tokenless_server(hass, entry):
+async def test_bad_token_with_tokenless_server(
+    hass, entry, mock_websocket, setup_plex_server, requests_mock
+):
     """Test setup with a bad token and a server with token auth disabled."""
-    with patch("plexapi.server.PlexServer", return_value=MockPlexServer()), patch(
-        "plexapi.myplex.MyPlexAccount", side_effect=plexapi.exceptions.Unauthorized
-    ), patch(
-        "homeassistant.components.plex.GDM", return_value=MockGDM(disabled=True)
-    ), patch(
-        "homeassistant.components.plex.PlexWebsocket", autospec=True
-    ) as mock_websocket:
-        entry.add_to_hass(hass)
-        assert await hass.config_entries.async_setup(entry.entry_id)
-        await hass.async_block_till_done()
+    requests_mock.get("https://plex.tv/users/account", status_code=401)
+
+    await setup_plex_server()
 
     assert entry.state == ENTRY_STATE_LOADED
 
