96795b84638c3408f11b73f9d1b6426501516124
Eric Severance <esev@esev.com>
2020-12-07 05:10:46 -0800

    Unregister updates when a Wemo entry is removed (#44005)

--- homeassistant/components/wemo/switch.py
+++ homeassistant/components/wemo/switch.py
@@ -220,6 +220,11 @@ class WemoSwitch(SwitchEntity):
         await self.hass.async_add_executor_job(registry.register, self.wemo)
         registry.on(self.wemo, None, self._subscription_callback)
 
+    async def async_will_remove_from_hass(self) -> None:
+        """Wemo switch removed from hass."""
+        registry = self.hass.data[WEMO_DOMAIN]["registry"]
+        await self.hass.async_add_executor_job(registry.unregister, self.wemo)
+
     async def async_update(self):
         """Update WeMo state.
 
