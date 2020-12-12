3d7c23650e07ea12f49fcf3acce4f69116f6e7a0
Eric Severance <esev@esev.com>
2020-12-12 10:41:20 -0800

    Test edge cases in wemo platform code (#44136)

--- tests/components/wemo/test_light_bridge.py
+++ tests/components/wemo/test_light_bridge.py
@@ -1,5 +1,4 @@
 """Tests for the Wemo light entity via the bridge."""
-
 import pytest
 import pywemo
 
@@ -7,10 +6,14 @@ from homeassistant.components.homeassistant import (
     DOMAIN as HA_DOMAIN,
     SERVICE_UPDATE_ENTITY,
 )
+from homeassistant.components.wemo.light import MIN_TIME_BETWEEN_SCANS
 from homeassistant.const import ATTR_ENTITY_ID, STATE_OFF, STATE_ON
 from homeassistant.setup import async_setup_component
+import homeassistant.util.dt as dt_util
 
-from tests.async_mock import PropertyMock, create_autospec
+from . import entity_test_helpers
+
+from tests.async_mock import create_autospec, patch
 
 
 @pytest.fixture
@@ -19,14 +22,69 @@ def pywemo_model():
     return "Bridge"
 
 
+# Note: The ordering of where the pywemo_bridge_light comes in test arguments matters.
+# In test methods, the pywemo_bridge_light fixture argument must come before the
+# wemo_entity fixture argument.
 @pytest.fixture(name="pywemo_bridge_light")
 def pywemo_bridge_light_fixture(pywemo_device):
     """Fixture for Bridge.Light WeMoDevice instances."""
-    light = create_autospec(pywemo.ouimeaux_device.bridge.Light)
+    light = create_autospec(pywemo.ouimeaux_device.bridge.Light, instance=True)
     light.uniqueID = pywemo_device.serialnumber
     light.name = pywemo_device.name
+    light.bridge = pywemo_device
+    light.state = {"onoff": 0}
     pywemo_device.Lights = {pywemo_device.serialnumber: light}
     return light
+
+
+def _bypass_throttling():
+    """Bypass the util.Throttle on the update_lights method."""
+    utcnow = dt_util.utcnow()
+
+    def increment_and_return_time():
+        nonlocal utcnow
+        utcnow += MIN_TIME_BETWEEN_SCANS
+        return utcnow
+
+    return patch("homeassistant.util.utcnow", side_effect=increment_and_return_time)
+
+
+async def test_async_update_locked_multiple_updates(
+    hass, pywemo_registry, pywemo_bridge_light, wemo_entity, pywemo_device
+):
+    """Test that two state updates do not proceed at the same time."""
+    pywemo_device.bridge_update.reset_mock()
+
+    with _bypass_throttling():
+        await entity_test_helpers.test_async_update_locked_multiple_updates(
+            hass,
+            pywemo_registry,
+            wemo_entity,
+            pywemo_device,
+            update_polling_method=pywemo_device.bridge_update,
+        )
+
+
+async def test_async_update_with_timeout_and_recovery(
+    hass, pywemo_bridge_light, wemo_entity, pywemo_device
+):
+    """Test that the entity becomes unavailable after a timeout, and that it recovers."""
+    await entity_test_helpers.test_async_update_with_timeout_and_recovery(
+        hass, wemo_entity, pywemo_device
+    )
+
+
+async def test_async_locked_update_with_exception(
+    hass, pywemo_bridge_light, wemo_entity, pywemo_device
+):
+    """Test that the entity becomes unavailable when communication is lost."""
+    with _bypass_throttling():
+        await entity_test_helpers.test_async_locked_update_with_exception(
+            hass,
+            wemo_entity,
+            pywemo_device,
+            update_polling_method=pywemo_device.bridge_update,
+        )
 
 
 async def test_light_update_entity(
@@ -36,7 +94,7 @@ async def test_light_update_entity(
     await async_setup_component(hass, HA_DOMAIN, {})
 
     # On state.
-    type(pywemo_bridge_light).state = PropertyMock(return_value={"onoff": 1})
+    pywemo_bridge_light.state = {"onoff": 1}
     await hass.services.async_call(
         HA_DOMAIN,
         SERVICE_UPDATE_ENTITY,
@@ -46,7 +104,7 @@ async def test_light_update_entity(
     assert hass.states.get(wemo_entity.entity_id).state == STATE_ON
 
     # Off state.
-    type(pywemo_bridge_light).state = PropertyMock(return_value={"onoff": 0})
+    pywemo_bridge_light.state = {"onoff": 0}
     await hass.services.async_call(
         HA_DOMAIN,
         SERVICE_UPDATE_ENTITY,
