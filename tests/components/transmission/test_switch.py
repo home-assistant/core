5f2e1cce7766d7d0bf8feaedc9f58b4b1b3c2587
J.P. Hutchins <jphutchins@gmail.com>
2020-12-12 06:15:45 +0000

    Add switch tests

---
+++ tests/components/transmission/test_switch.py
@@ -0,0 +1,50 @@
+"""Transmission service platform tests."""
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
+
+from tests.common import patch
+
+
+async def test_switches(hass, torrent_info):
+    """Test the Transmission switch platforms."""
+    transmission_switch = hass.states.get("switch.transmission_switch")
+    assert transmission_switch is not None
+    assert transmission_switch.state == "on"
+
+    with patch("transmissionrpc.Client.stop_torrent") as func:
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            "turn_off",
+            {"entity_id": "switch.transmission_switch"},
+            blocking=True,
+        )
+        func.assert_called_with([1, 2, 3, 4, 5])
+
+    with patch("transmissionrpc.Client.start_all") as func:
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            "turn_on",
+            {"entity_id": "switch.transmission_switch"},
+            blocking=True,
+        )
+        func.assert_called()
+
+    transmission_turtle_mode = hass.states.get("switch.transmission_turtle_mode")
+    assert transmission_turtle_mode is not None
+    assert transmission_turtle_mode.state == "on"
+
+    with patch("transmissionrpc.Client.set_session") as func:
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            "turn_off",
+            {"entity_id": "switch.transmission_turtle_mode"},
+            blocking=True,
+        )
+        func.assert_called_with(alt_speed_enabled=False)
+
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            "turn_on",
+            {"entity_id": "switch.transmission_turtle_mode"},
+            blocking=True,
+        )
+        func.assert_called_with(alt_speed_enabled=True)
