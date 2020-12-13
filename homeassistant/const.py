d06b7ae9ad76d26228576692214f164402b0c45b
Franck Nijhof <git@frenck.dev>
2020-12-13 22:13:58 +0100

    2020.12.0

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -1,6 +1,6 @@
 """Constants used by Home Assistant components."""
-MAJOR_VERSION = 1
-MINOR_VERSION = 0
+MAJOR_VERSION = 2020
+MINOR_VERSION = 12
 PATCH_VERSION = "0"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
