f79513e02d544408648ed6ba5e8d6f2d88f0670f
Tobias Haase <tobhaase@gmail.com>
2020-12-15 18:40:36 +0100

    Minor changes

--- homeassistant/components/neato/config_flow.py
+++ homeassistant/components/neato/config_flow.py
@@ -23,16 +23,16 @@ class OAuth2FlowHandler(
     DOMAIN = NEATO_DOMAIN
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
+    _reauth_flow: bool = False
+
     @property
     def logger(self) -> logging.Logger:
         """Return logger."""
         return logging.getLogger(__name__)
 
-    async def async_step_user(
-        self, user_input: Optional[dict] = None, reauth: bool = False
-    ) -> dict:
+    async def async_step_user(self, user_input: Optional[dict] = None) -> dict:
         """Create an entry for the flow."""
-        if reauth:
+        if self._reauth_flow:
             # Ongoig migration
             return self.async_show_form(step_id="user")
 
@@ -45,7 +45,8 @@ class OAuth2FlowHandler(
 
     async def async_step_reauth(self, config_entry: ConfigEntry) -> dict:
         """Perform reauth upon migration of old entries."""
-        return await self.async_step_user(reauth=True)
+        self._reauth_flow = True
+        return await self.async_step_user()
 
     async def async_oauth_create_entry(self, data: dict) -> dict:
         """Create an entry for the flow. Update an entry if one already exist."""
@@ -56,5 +57,6 @@ class OAuth2FlowHandler(
             self.hass.config_entries.async_update_entry(
                 current_entries[0], title=self.flow_impl.name, data=data
             )
+            self.hass.config_entries.async_reload(current_entries[0].entry_id)
             return self.async_abort(reason="reauth_successful")
         return self.async_create_entry(title=self.flow_impl.name, data=data)
