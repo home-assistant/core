5b50a18d01397a173c237eff1aa9088fb24c287f
Allen Porter <allen@thebends.org>
2020-12-16 14:20:48 +0000

    Remove unreachable code

--- homeassistant/components/nest/climate_sdm.py
+++ homeassistant/components/nest/climate_sdm.py
@@ -294,7 +294,6 @@ class ThermostatEntity(ClimateEntity):
         """Set new target hvac mode."""
         if hvac_mode not in self.hvac_modes:
             raise ValueError(f"Unsupported hvac_mode '{hvac_mode}'")
-            return
         if hvac_mode == HVAC_MODE_FAN_ONLY:
             await self.async_set_fan_mode(FAN_ON)
             return
