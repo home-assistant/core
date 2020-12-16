3f002ed4d5202a46af0b783f91cf51efce5cf5dd
Dermot Duffy <dermot.duffy@gmail.com>
2020-12-16 14:55:31 -0800

    Add reauth step to Hyperion config flow (#43797)
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/hyperion/const.py
+++ homeassistant/components/hyperion/const.py
@@ -16,8 +16,6 @@ CONF_ON_UNLOAD = "ON_UNLOAD"
 SIGNAL_INSTANCES_UPDATED = f"{DOMAIN}_instances_updated_signal." "{}"
 SIGNAL_INSTANCE_REMOVED = f"{DOMAIN}_instance_removed_signal." "{}"
 
-SOURCE_IMPORT = "import"
-
 HYPERION_VERSION_WARN_CUTOFF = "2.0.0-alpha.9"
 HYPERION_RELEASES_URL = "https://github.com/hyperion-project/hyperion.ng/releases"
 
