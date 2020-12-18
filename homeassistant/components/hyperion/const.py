c97b8a43d16eccf89e49949b6cb5df3419b45208
Dermot Duffy <dermot.duffy@gmail.com>
2020-12-17 23:03:54 -0800

    Alphabetize hyperion const.py (#44343)

--- homeassistant/components/hyperion/const.py
+++ homeassistant/components/hyperion/const.py
@@ -1,22 +1,22 @@
 """Constants for Hyperion integration."""
-DOMAIN = "hyperion"
+
+CONF_AUTH_ID = "auth_id"
+CONF_CREATE_TOKEN = "create_token"
+CONF_INSTANCE = "instance"
+CONF_ON_UNLOAD = "ON_UNLOAD"
+CONF_PRIORITY = "priority"
+CONF_ROOT_CLIENT = "ROOT_CLIENT"
 
 DEFAULT_NAME = "Hyperion"
 DEFAULT_ORIGIN = "Home Assistant"
 DEFAULT_PRIORITY = 128
 
-CONF_AUTH_ID = "auth_id"
-CONF_CREATE_TOKEN = "create_token"
-CONF_INSTANCE = "instance"
-CONF_PRIORITY = "priority"
+DOMAIN = "hyperion"
 
-CONF_ROOT_CLIENT = "ROOT_CLIENT"
-CONF_ON_UNLOAD = "ON_UNLOAD"
+HYPERION_RELEASES_URL = "https://github.com/hyperion-project/hyperion.ng/releases"
+HYPERION_VERSION_WARN_CUTOFF = "2.0.0-alpha.9"
 
 SIGNAL_INSTANCES_UPDATED = f"{DOMAIN}_instances_updated_signal." "{}"
 SIGNAL_INSTANCE_REMOVED = f"{DOMAIN}_instance_removed_signal." "{}"
-
-HYPERION_VERSION_WARN_CUTOFF = "2.0.0-alpha.9"
-HYPERION_RELEASES_URL = "https://github.com/hyperion-project/hyperion.ng/releases"
 
 TYPE_HYPERION_LIGHT = "hyperion_light"
