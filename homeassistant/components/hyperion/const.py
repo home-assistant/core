e9ce85cef274e96b1e83033e6dada34022c3a65c
Dermot Duffy <dermot.duffy@gmail.com>
2020-12-17 20:43:20 -0800

    Back out the cleanup -- will handle separately

--- homeassistant/components/hyperion/const.py
+++ homeassistant/components/hyperion/const.py
@@ -1,29 +1,30 @@
 """Constants for Hyperion integration."""
 COLOR_BLACK = (0, 0, 0)
 
-CONF_AUTH_ID = "auth_id"
-CONF_CREATE_TOKEN = "create_token"
-CONF_INSTANCE = "instance"
-CONF_PRIORITY = "priority"
-CONF_MODE_OFF = "mode_off"
-CONF_MODE_OFF_LED_DEVICE = "led_device"
-CONF_MODE_OFF_SET_BLACK = "set_black"
-CONF_ROOT_CLIENT = "ROOT_CLIENT"
-CONF_ON_UNLOAD = "ON_UNLOAD"
+DOMAIN = "hyperion"
 
-DEFAULT_MODE_OFF = CONF_MODE_OFF_LED_DEVICE
 DEFAULT_NAME = "Hyperion"
 DEFAULT_ORIGIN = "Home Assistant"
 DEFAULT_PRIORITY = 128
 
-DOMAIN = "hyperion"
+CONF_AUTH_ID = "auth_id"
+CONF_CREATE_TOKEN = "create_token"
+CONF_INSTANCE = "instance"
+CONF_MODE_OFF = "mode_off"
+CONF_MODE_OFF_LED_DEVICE = "led_device"
+CONF_MODE_OFF_SET_BLACK = "set_black"
+CONF_PRIORITY = "priority"
+DEFAULT_MODE_OFF = CONF_MODE_OFF_LED_DEVICE
 
-HYPERION_VERSION_WARN_CUTOFF = "2.0.0-alpha.9"
-HYPERION_RELEASES_URL = "https://github.com/hyperion-project/hyperion.ng/releases"
+CONF_ROOT_CLIENT = "ROOT_CLIENT"
+CONF_ON_UNLOAD = "ON_UNLOAD"
 
 SIGNAL_INSTANCES_UPDATED = f"{DOMAIN}_instances_updated_signal." "{}"
 SIGNAL_INSTANCE_REMOVED = f"{DOMAIN}_instance_removed_signal." "{}"
 
 SOURCE_IMPORT = "import"
+
+HYPERION_VERSION_WARN_CUTOFF = "2.0.0-alpha.9"
+HYPERION_RELEASES_URL = "https://github.com/hyperion-project/hyperion.ng/releases"
 
 TYPE_HYPERION_LIGHT = "hyperion_light"
