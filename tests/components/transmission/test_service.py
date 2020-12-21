2d7797beffc7781a6bb82f287d9a4f3ad6e7a727
J.P. Hutchins <jphutchins@gmail.com>
2020-12-21 06:25:47 +0000

    Remove helper function for call service, patch after import, use async_mock patch

--- tests/components/transmission/test_service.py
+++ tests/components/transmission/test_service.py
@@ -1,29 +1,37 @@
 """Transmission service platform tests."""
-from tests.common import patch
+from tests.async_mock import patch
 
 
 async def test_call_services(hass, torrent_info):
     """Test that the Transmission services are called."""
-
-    async def call_service(
-        hass,
-        ha_service_name,
-        additional_service_data=None,
+    with patch(
+        "homeassistant.components.transmission.transmissionrpc.Client.add_torrent"
     ):
-        """Test Transmission service."""
-        service_data = {"name": "Transmission"}
-        if additional_service_data:
-            service_data.update(additional_service_data)
-        assert await hass.services.async_call(
-            "transmission",
-            ha_service_name,
-            service_data=service_data,
-            blocking=True,
+        assert await (
+            hass.services.async_call(
+                "transmission",
+                "add_torrent",
+                service_data={"name": "Transmission", "torrent": "magnet:"},
+                blocking=True,
+            )
+        )
+        assert await (
+            hass.services.async_call(
+                "transmission",
+                "add_torrent",
+                service_data={"name": "Transmission", "torrent": "blumber"},
+                blocking=True,
+            )
         )
 
-    with patch("transmissionrpc.Client.add_torrent"):
-        await call_service(hass, "add_torrent", {"torrent": "magnet:"})
-        await call_service(hass, "add_torrent", {"torrent": "blumber"})
-
-    with patch("transmissionrpc.Client.remove_torrent"):
-        await call_service(hass, "remove_torrent", {"id": 1})
+    with patch(
+        "homeassistant.components.transmission.transmissionrpc.Client.remove_torrent"
+    ):
+        assert await (
+            hass.services.async_call(
+                "transmission",
+                "remove_torrent",
+                service_data={"name": "Transmission", "id": 1},
+                blocking=True,
+            )
+        )
