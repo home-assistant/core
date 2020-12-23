2348d0d3ef3ae691a8d3db9a6323bf6d827c2ed8
starkillerOG <starkiller.og@gmail.com>
2020-12-24 00:15:11 +0100

    Motion Blinds upgrade to local push (#44391)
    * Motion Blinds upgrade to local push

--- tests/components/motion_blinds/test_config_flow.py
+++ tests/components/motion_blinds/test_config_flow.py
@@ -8,10 +8,11 @@ from homeassistant.components.motion_blinds.config_flow import DEFAULT_GATEWAY_N
 from homeassistant.components.motion_blinds.const import DOMAIN
 from homeassistant.const import CONF_API_KEY, CONF_HOST
 
-from tests.async_mock import patch
+from tests.async_mock import Mock, patch
 
 TEST_HOST = "1.2.3.4"
 TEST_API_KEY = "12ab345c-d67e-8f"
+TEST_DEVICE_LIST = {"mac": Mock()}
 
 
 @pytest.fixture(name="motion_blinds_connect", autouse=True)
@@ -23,6 +24,9 @@ def motion_blinds_connect_fixture():
     ), patch(
         "homeassistant.components.motion_blinds.gateway.MotionGateway.Update",
         return_value=True,
+    ), patch(
+        "homeassistant.components.motion_blinds.gateway.MotionGateway.device_list",
+        TEST_DEVICE_LIST,
     ), patch(
         "homeassistant.components.motion_blinds.async_setup_entry", return_value=True
     ):
