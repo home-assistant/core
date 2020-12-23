a4e559db12836a4cf1da023f93c6fbb2cbea3e35
starkillerOG <starkiller.og@gmail.com>
2020-12-24 00:15:11 +0100

    Motion Blinds upgrade to local push (#44391)
    * Motion Blinds upgrade to local push

--- homeassistant/components/motion_blinds/const.py
+++ homeassistant/components/motion_blinds/const.py
@@ -1,6 +1,10 @@
 """Constants for the Motion Blinds component."""
 DOMAIN = "motion_blinds"
-MANUFACTURER = "Motion, Coulisse B.V."
+MANUFACTURER = "Motion Blinds, Coulisse B.V."
+DEFAULT_GATEWAY_NAME = "Motion Blinds Gateway"
+
+MOTION_PLATFORMS = ["cover", "sensor"]
 
 KEY_GATEWAY = "gateway"
 KEY_COORDINATOR = "coordinator"
+KEY_MULTICAST_LISTENER = "multicast_listener"
