4da3a4ea22b9ed6558fceb5e007244537789bf82
cyr-ius <cyr-ius@ipocus.net>
2020-12-27 19:32:34 +0100

    Clean code

--- homeassistant/components/time_date/const.py
+++ homeassistant/components/time_date/const.py
@@ -1,26 +1,14 @@
 """The Time & Date constants."""
 DOMAIN = "time_date"
 CONF_OPTIONS = "display_options"
-DISPLAY_OPTIONS = [
-    "time",
-    "date",
-    "date_time",
-    "date_time_utc",
-    "date_time_iso",
-    "time_date",
-    "time_utc",
-    "beat",
-]
-
 TIME_STR_FORMAT = "%H:%M"
-
 OPTION_TYPES = {
-    "time": "Time",
-    "date": "Date",
-    "date_time": "Date & Time",
-    "date_time_utc": "Date & Time (UTC)",
-    "date_time_iso": "Date & Time (ISO)",
-    "time_date": "Time & Date",
     "beat": "Internet Time",
+    "date_time_iso": "Date & Time (ISO)",
+    "date_time_utc": "Date & Time (UTC)",
+    "date_time": "Date & Time",
+    "date": "Date",
+    "time_date": "Time & Date",
     "time_utc": "Time (UTC)",
+    "time": "Time",
 }
