79734a1eb3fa26032dda527bd66e44a9506e23f1
djtimca <60706061+djtimca@users.noreply.github.com>
2020-12-27 16:21:33 -0500

    Refactor flux led and add config_flow (#44320)
    * Initial commit of updated flux_led component.
    
    * Updated to allow auto and manual configuration.
    
    * Added test cases for complete config_flow.
    
    * Added import from existing configuration.yaml with config_flow tests.
    
    * Added set_custom_effect service for lights.
    
    * Fix lint issues.
    
    * Added test coverage in test_config_flow.py.
    
    * Added test coverage to test_config_flow for errors/aborts.
    
    * Remove all but the config_flow from the current PR. Additional PR required to complete.
    
    * Handle entity setup from config_flow.
    
    * Resolve comments in PR.
    
    * Addressed PR comment re: iterating devices.
    
    * Wrapped bulb scan into async_add_executor_job.
    
    * Remove arguments from async_executor_job, not required.
    
    * Remove arguments from async_executor_job, not required.
    
    * Reworked config_flow for one flow for auto/manual. Device additions only so far.
    
    * Support removing bulbs through the options panel.
    
    * Added device config panel. Still need work on global options.
    
    * Fixed global options. Completed config_flow rework.
    
    * Added tests for base config_flow. Options tests to follow.
    
    * Fix lint issues.
    
    * Add import test cases. Fix other test cases. Options flow test cases pending.
    
    * Added tests for options flow.
    
    * Addressed comments from PR.
    
    * Updated last flagged issue - debug level and removed period.
    
    * Addressed issues in PR.
    
    * Include global effect speed if device speed has not been set.
    
    * Addressed PR comments.
    
    * Addressed PR comments - added hass.config_entries.async_setup
    
    * Add setup of integration to all options flow tests.

---
+++ homeassistant/components/flux_led/const.py
@@ -0,0 +1,27 @@
+"""Constants of the FluxLed/MagicHome Integration."""
+
+DOMAIN = "flux_led"
+
+BULB_COORDINATOR = "bulb_coordinator"
+COORDINATOR = "coordinator"
+
+API = "flux_api"
+
+ATTR_IDENTIFIERS = "identifiers"
+ATTR_MANUFACTURER = "manufacturer"
+ATTR_MODEL = "model"
+
+DEFAULT_NETWORK_SCAN_INTERVAL = 30
+DEFAULT_SCAN_INTERVAL = 500
+DEFAULT_EFFECT_SPEED = 50
+
+SIGNAL_ADD_DEVICE = "flux_led_add_device"
+SIGNAL_REMOVE_DEVICE = "flux_led_remove_device"
+
+DEFAULT_SPEED = 70
+
+CONF_AUTOMATIC_ADD = "automatic_add"
+CONF_EFFECT_SPEED = "effect_speed"
+CONF_DEVICES = "devices"
+CONF_CONFIGURE_DEVICE = "configure_device"
+CONF_REMOVE_DEVICE = "remove_device"
