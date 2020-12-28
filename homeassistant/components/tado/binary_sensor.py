c7e8ddba003674b423a4489b9aa9144455ad283b
Álvaro Fernández Rojas <noltari@gmail.com>
2020-12-28 12:02:12 +0100

    Tado: add zone binary sensors
    These should be binary sensors.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- homeassistant/components/tado/binary_sensor.py
+++ homeassistant/components/tado/binary_sensor.py
@@ -4,14 +4,25 @@ import logging
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_BATTERY,
     DEVICE_CLASS_CONNECTIVITY,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_WINDOW,
     BinarySensorEntity,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
-from .const import DATA, DOMAIN, SIGNAL_TADO_UPDATE_RECEIVED, TYPE_BATTERY, TYPE_POWER
-from .entity import TadoDeviceEntity
+from .const import (
+    DATA,
+    DOMAIN,
+    SIGNAL_TADO_UPDATE_RECEIVED,
+    TYPE_AIR_CONDITIONING,
+    TYPE_BATTERY,
+    TYPE_HEATING,
+    TYPE_HOT_WATER,
+    TYPE_POWER,
+)
+from .entity import TadoDeviceEntity, TadoZoneEntity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -25,6 +36,23 @@ DEVICE_SENSORS = {
     ],
 }
 
+ZONE_SENSORS = {
+    TYPE_HEATING: [
+        "power",
+        "link",
+        "overlay",
+        "early start",
+        "open window",
+    ],
+    TYPE_AIR_CONDITIONING: [
+        "power",
+        "link",
+        "overlay",
+        "open window",
+    ],
+    TYPE_HOT_WATER: ["power", "link", "overlay"],
+}
+
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities
@@ -33,6 +61,7 @@ async def async_setup_entry(
 
     tado = hass.data[DOMAIN][entry.entry_id][DATA]
     devices = tado.devices
+    zones = tado.zones
     entities = []
 
     # Create device sensors
@@ -44,8 +73,22 @@ async def async_setup_entry(
 
         entities.extend(
             [
-                TadoDeviceSensor(tado, device, variable)
+                TadoDeviceBinarySensor(tado, device, variable)
                 for variable in DEVICE_SENSORS[device_type]
+            ]
+        )
+
+    # Create zone sensors
+    for zone in zones:
+        zone_type = zone["type"]
+        if zone_type not in ZONE_SENSORS:
+            _LOGGER.warning("Unknown zone type skipped: %s", zone_type)
+            continue
+
+        entities.extend(
+            [
+                TadoZoneBinarySensor(tado, zone["name"], zone["id"], variable)
+                for variable in ZONE_SENSORS[zone_type]
             ]
         )
 
@@ -53,7 +96,7 @@ async def async_setup_entry(
         async_add_entities(entities, True)
 
 
-class TadoDeviceSensor(TadoDeviceEntity, BinarySensorEntity):
+class TadoDeviceBinarySensor(TadoDeviceEntity, BinarySensorEntity):
     """Representation of a tado Sensor."""
 
     def __init__(self, tado, device_info, device_variable):
@@ -124,4 +167,96 @@ class TadoDeviceSensor(TadoDeviceEntity, BinarySensorEntity):
         elif self.device_variable == "connection state":
             self._state = self._device_info.get("connectionState", {}).get(
                 "value", False
+            )
+
+
+class TadoZoneBinarySensor(TadoZoneEntity, BinarySensorEntity):
+    """Representation of a tado Sensor."""
+
+    def __init__(self, tado, zone_name, zone_id, zone_variable):
+        """Initialize of the Tado Sensor."""
+        self._tado = tado
+        super().__init__(zone_name, tado.home_id, zone_id)
+
+        self.zone_variable = zone_variable
+
+        self._unique_id = f"{zone_variable} {zone_id} {tado.home_id}"
+
+        self._state = None
+        self._tado_zone_data = None
+
+    async def async_added_to_hass(self):
+        """Register for sensor updates."""
+
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass,
+                SIGNAL_TADO_UPDATE_RECEIVED.format(
+                    self._tado.home_id, "zone", self.zone_id
+                ),
+                self._async_update_callback,
+            )
+        )
+        self._async_update_zone_data()
+
+    @property
+    def unique_id(self):
+        """Return the unique id."""
+        return self._unique_id
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{self.zone_name} {self.zone_variable}"
+
+    @property
+    def is_on(self):
+        """Return true if sensor is on."""
+        return self._state
+
+    @property
+    def device_class(self):
+        """Return the class of this sensor."""
+        if self.zone_variable == "early start":
+            return DEVICE_CLASS_POWER
+        if self.zone_variable == "link":
+            return DEVICE_CLASS_CONNECTIVITY
+        if self.zone_variable == "open window":
+            return DEVICE_CLASS_WINDOW
+        if self.zone_variable == "overlay":
+            return DEVICE_CLASS_POWER
+        if self.zone_variable == "power":
+            return DEVICE_CLASS_POWER
+        return None
+
+    @callback
+    def _async_update_callback(self):
+        """Update and write state."""
+        self._async_update_zone_data()
+        self.async_write_ha_state()
+
+    @callback
+    def _async_update_zone_data(self):
+        """Handle update callbacks."""
+        try:
+            self._tado_zone_data = self._tado.data["zone"][self.zone_id]
+        except KeyError:
+            return
+
+        if self.zone_variable == "power":
+            self._state = self._tado_zone_data.power
+
+        elif self.zone_variable == "link":
+            self._state = self._tado_zone_data.link
+
+        elif self.zone_variable == "overlay":
+            self._state = self._tado_zone_data.overlay_active
+
+        elif self.zone_variable == "early start":
+            self._state = self._tado_zone_data.preparation
+
+        elif self.zone_variable == "open window":
+            self._state = bool(
+                self._tado_zone_data.open_window
+                or self._tado_zone_data.open_window_detected
             )
