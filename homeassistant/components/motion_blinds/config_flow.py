8ea0ea283606a87f24174b672e8832bc85305e47
starkillerOG <starkiller.og@gmail.com>
2020-12-29 20:13:31 +0100

    Add discovery to Motion Blinds (#44615)
    * Add discovery to Motion Blinds
    
    * Update test_config_flow.py
    
    * ommit keys()
    
    Co-authored-by: Allen Porter <allen.porter@gmail.com>
    
    * use _ to indicate private variables
    
    * disregard changes to en.json
    
    * remove unused errors
    
    * clearify multicast=None
    
    * improve tests
    
    * make self._key a local variable
    
    * fix styling
    
    Co-authored-by: Allen Porter <allen.porter@gmail.com>

--- homeassistant/components/motion_blinds/config_flow.py
+++ homeassistant/components/motion_blinds/config_flow.py
@@ -1,6 +1,7 @@
 """Config flow to configure Motion Blinds using their WLAN API."""
 import logging
 
+from motionblinds import MotionDiscovery
 import voluptuous as vol
 
 from homeassistant import config_entries
@@ -15,7 +16,12 @@ _LOGGER = logging.getLogger(__name__)
 
 CONFIG_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_HOST): str,
+        vol.Optional(CONF_HOST): str,
+    }
+)
+
+CONFIG_SETTINGS = vol.Schema(
+    {
         vol.Required(CONF_API_KEY): vol.All(str, vol.Length(min=16, max=16)),
     }
 )
@@ -29,35 +35,64 @@ class MotionBlindsFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     def __init__(self):
         """Initialize the Motion Blinds flow."""
-        self.host = None
-        self.key = None
+        self._host = None
+        self._ips = []
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
         errors = {}
         if user_input is not None:
-            self.host = user_input[CONF_HOST]
-            self.key = user_input[CONF_API_KEY]
-            return await self.async_step_connect()
+            self._host = user_input.get(CONF_HOST)
+
+            if self._host is not None:
+                return await self.async_step_connect()
+
+            # Use MotionGateway discovery
+            discover_class = MotionDiscovery()
+            gateways = await self.hass.async_add_executor_job(discover_class.discover)
+            self._ips = list(gateways)
+
+            if len(self._ips) == 1:
+                self._host = self._ips[0]
+                return await self.async_step_connect()
+
+            if len(self._ips) > 1:
+                return await self.async_step_select()
+
+            errors["base"] = "discovery_error"
 
         return self.async_show_form(
             step_id="user", data_schema=CONFIG_SCHEMA, errors=errors
         )
 
+    async def async_step_select(self, user_input=None):
+        """Handle multiple motion gateways found."""
+        if user_input is not None:
+            self._host = user_input["select_ip"]
+            return await self.async_step_connect()
+
+        select_schema = vol.Schema({vol.Required("select_ip"): vol.In(self._ips)})
+
+        return self.async_show_form(step_id="select", data_schema=select_schema)
+
     async def async_step_connect(self, user_input=None):
         """Connect to the Motion Gateway."""
+        if user_input is not None:
+            key = user_input[CONF_API_KEY]
 
-        connect_gateway_class = ConnectMotionGateway(self.hass, None)
-        if not await connect_gateway_class.async_connect_gateway(self.host, self.key):
-            return self.async_abort(reason="connection_error")
-        motion_gateway = connect_gateway_class.gateway_device
+            connect_gateway_class = ConnectMotionGateway(self.hass, multicast=None)
+            if not await connect_gateway_class.async_connect_gateway(self._host, key):
+                return self.async_abort(reason="connection_error")
+            motion_gateway = connect_gateway_class.gateway_device
 
-        mac_address = motion_gateway.mac
+            mac_address = motion_gateway.mac
 
-        await self.async_set_unique_id(mac_address)
-        self._abort_if_unique_id_configured()
+            await self.async_set_unique_id(mac_address)
+            self._abort_if_unique_id_configured()
 
-        return self.async_create_entry(
-            title=DEFAULT_GATEWAY_NAME,
-            data={CONF_HOST: self.host, CONF_API_KEY: self.key},
-        )
+            return self.async_create_entry(
+                title=DEFAULT_GATEWAY_NAME,
+                data={CONF_HOST: self._host, CONF_API_KEY: key},
+            )
+
+        return self.async_show_form(step_id="connect", data_schema=CONFIG_SETTINGS)
