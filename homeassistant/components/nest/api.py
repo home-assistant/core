c0acc1eda19c003a0c839ea92ddabffc62284664
Allen Porter <allen@thebends.org>
2021-01-01 15:25:41 +0000

    Clarify purpose of each Auth API method

--- homeassistant/components/nest/api.py
+++ homeassistant/components/nest/api.py
@@ -30,13 +30,13 @@ class AsyncConfigEntryAuth(AbstractAuth):
         self._client_secret = client_secret
 
     async def async_get_access_token(self):
-        """Return a valid access token."""
+        """Return a valid access token for SDM API."""
         if not self._oauth_session.valid_token:
             await self._oauth_session.async_ensure_token_valid()
         return self._oauth_session.token["access_token"]
 
     async def async_get_creds(self):
-        """Return a minimal OAuth credential."""
+        """Return an OAuth credential for Pub/Sub Subscriber."""
         # We don't have a way for Home Assistant to refresh creds on behalf
         # of the google pub/sub subscriber. Instead, build a full
         # Credentials object with enough information for the subscriber to
