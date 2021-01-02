4605be4341053d97b7fd44c2bd78e6724895b500
posixx <2280400+posixx@users.noreply.github.com>
2021-01-02 14:40:23 +0100

    Update __init__.py

--- homeassistant/components/alarm_control_panel/__init__.py
+++ homeassistant/components/alarm_control_panel/__init__.py
@@ -102,9 +102,11 @@ async def async_setup_entry(hass, entry):
     """Set up a config entry."""
     return await hass.data[DOMAIN].async_setup_entry(entry)
 
+
 async def async_unload_entry(hass, entry):
     """Unload a config entry."""
     return await hass.data[DOMAIN].async_unload_entry(entry)
+
 
 class AlarmControlPanelEntity(Entity):
     """An abstract class for alarm control entities."""
