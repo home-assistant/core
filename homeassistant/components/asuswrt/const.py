066d256a2f1bfe3dc5f934eaccd4d76ec8de6602
ollo69 <60491700+ollo69@users.noreply.github.com>
2021-01-02 02:06:35 +0100

    Add Asuswrt config flow (#43948)

---
+++ homeassistant/components/asuswrt/const.py
@@ -0,0 +1,22 @@
+"""AsusWrt component constants."""
+DOMAIN = "asuswrt"
+
+CONF_DNSMASQ = "dnsmasq"
+CONF_INTERFACE = "interface"
+CONF_REQUIRE_IP = "require_ip"
+CONF_SSH_KEY = "ssh_key"
+
+DATA_ASUSWRT = DOMAIN
+
+DEFAULT_DNSMASQ = "/var/lib/misc"
+DEFAULT_INTERFACE = "eth0"
+DEFAULT_SSH_PORT = 22
+
+MODE_AP = "ap"
+MODE_ROUTER = "router"
+
+PROTOCOL_SSH = "ssh"
+PROTOCOL_TELNET = "telnet"
+
+# Sensor
+SENSOR_TYPES = ["devices", "upload_speed", "download_speed", "download", "upload"]
---
+++ homeassistant/components/asuswrt/router.py
@@ -0,0 +1,101 @@
+"""Represent the AsusWrt router."""
+from typing import Dict, Optional
+
+from aioasuswrt.asuswrt import AsusWrt
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_MODE,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_PROTOCOL,
+    CONF_USERNAME,
+)
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.typing import HomeAssistantType
+
+from .const import (
+    CONF_DNSMASQ,
+    CONF_INTERFACE,
+    CONF_REQUIRE_IP,
+    CONF_SSH_KEY,
+    DEFAULT_DNSMASQ,
+    DEFAULT_INTERFACE,
+    PROTOCOL_TELNET,
+)
+
+CONF_REQ_RELOAD = [CONF_DNSMASQ, CONF_INTERFACE, CONF_REQUIRE_IP]
+
+
+class AsusWrtRouter:
+    """Representation of a AsusWrt router."""
+
+    def __init__(self, hass: HomeAssistantType, entry: ConfigEntry) -> None:
+        """Initialize a AsusWrt router."""
+        self.hass = hass
+        self._entry = entry
+        self._api: AsusWrt = None
+        self._protocol = entry.data[CONF_PROTOCOL]
+
+        self._options = {
+            CONF_DNSMASQ: DEFAULT_DNSMASQ,
+            CONF_INTERFACE: DEFAULT_INTERFACE,
+            CONF_REQUIRE_IP: True,
+        }
+        self._options.update(entry.options)
+
+    async def setup(self) -> None:
+        """Set up a AsusWrt router."""
+        self._api = get_api(self._entry.data, self._options)
+
+        try:
+            await self._api.connection.async_connect()
+        except OSError as exp:
+            raise ConfigEntryNotReady from exp
+
+        if not self._api.is_connected:
+            raise ConfigEntryNotReady
+
+    async def close(self) -> None:
+        """Close the connection."""
+        if self._api is not None:
+            if self._protocol == PROTOCOL_TELNET:
+                await self._api.connection.disconnect()
+        self._api = None
+
+    def update_options(self, new_options: Dict) -> bool:
+        """Update router options."""
+        req_reload = False
+        for name, new_opt in new_options.items():
+            if name in (CONF_REQ_RELOAD):
+                old_opt = self._options.get(name)
+                if not old_opt or old_opt != new_opt:
+                    req_reload = True
+                    break
+
+        self._options.update(new_options)
+        return req_reload
+
+    @property
+    def api(self) -> AsusWrt:
+        """Return router API."""
+        return self._api
+
+
+def get_api(conf: Dict, options: Optional[Dict] = None) -> AsusWrt:
+    """Get the AsusWrt API."""
+    opt = options or {}
+
+    return AsusWrt(
+        conf[CONF_HOST],
+        conf[CONF_PORT],
+        conf[CONF_PROTOCOL] == PROTOCOL_TELNET,
+        conf[CONF_USERNAME],
+        conf.get(CONF_PASSWORD, ""),
+        conf.get(CONF_SSH_KEY, ""),
+        conf[CONF_MODE],
+        opt.get(CONF_REQUIRE_IP, True),
+        interface=opt.get(CONF_INTERFACE, DEFAULT_INTERFACE),
+        dnsmasq=opt.get(CONF_DNSMASQ, DEFAULT_DNSMASQ),
+    )
