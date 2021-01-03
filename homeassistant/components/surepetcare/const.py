0f90713ba9deae9ff142ebd9a05db6639cdeac09
Will Hughes <insertjokehere@users.noreply.github.com>
2021-01-04 01:43:16 +1300

    Add service to lock/unlock Sure Petcare pet flaps  (#44557)
    * Add service to lock/unlock Sure Petcare pet flaps
    
    Adds a service to the Sure Petcare pet flaps to allow lock, unlocking,
    locking in and locking out pets using the pet flap
    
    * Linting
    
    * Changes from code review

--- homeassistant/components/surepetcare/const.py
+++ homeassistant/components/surepetcare/const.py
@@ -31,3 +31,8 @@ BATTERY_ICON = "mdi:battery"
 SURE_BATT_VOLTAGE_FULL = 1.6  # voltage
 SURE_BATT_VOLTAGE_LOW = 1.25  # voltage
 SURE_BATT_VOLTAGE_DIFF = SURE_BATT_VOLTAGE_FULL - SURE_BATT_VOLTAGE_LOW
+
+# lock state service
+SERVICE_SET_LOCK_STATE = "set_lock_state"
+ATTR_FLAP_ID = "flap_id"
+ATTR_LOCK_STATE = "lock_state"
