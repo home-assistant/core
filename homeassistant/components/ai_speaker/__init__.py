b7084d8d8932c2f8dc64b826dd4a48fa35545aff
asystentka-jolka <jolka@sviete.pl>
2021-01-03 22:59:23 +0100

    fix publish_command service

--- homeassistant/components/ai_speaker/__init__.py
+++ homeassistant/components/ai_speaker/__init__.py
@@ -13,21 +13,21 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
     """Set up the initial domain configuration."""
+
+    async def async_command(service):
+        """Publish command to AI-Speaker WS."""
+        web_session = aiohttp_client.async_get_clientsession(hass)
+        ais_url = service.data["ais_url"]
+        ais_gate = AisWebService(hass.loop, web_session, ais_url)
+        await ais_gate.command(service.data["key"], service.data["val"])
+
+    hass.services.async_register(DOMAIN, "publish_command", async_command)
     return True
 
 
 async def async_setup_entry(hass, config_entry):
     """Set up the integration based on a configuration entry."""
     _LOGGER.debug("async_setup_entry %s", config_entry)
-    web_session = aiohttp_client.async_get_clientsession(hass)
-    ais_url = config_entry.data.get("ais_info")["ais_url"]
-    ais_gate = AisWebService(hass.loop, web_session, ais_url)
-
-    async def async_command(service):
-        """Publish command to AI-Speaker WS."""
-        await ais_gate.command(service.data["key"], service.data["val"])
-
-    hass.services.async_register(DOMAIN, "publish_command", async_command)
 
     for component in PLATFORMS:
         hass.async_create_task(
