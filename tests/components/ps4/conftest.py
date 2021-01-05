facd5ed271a88d1733b257a20b1298a4e81b125c
Paulus Schoutsen <balloob@gmail.com>
2021-01-05 21:12:14 +0100

    Avoid Ps4 doing I/O during tests (#44845)

--- tests/components/ps4/conftest.py
+++ tests/components/ps4/conftest.py
@@ -18,6 +18,13 @@ def patch_save_json():
         yield mock_save
 
 
+@pytest.fixture
+def patch_get_status():
+    """Prevent save JSON being used."""
+    with patch("pyps4_2ndscreen.ps4.get_status", return_value=None) as mock_get_status:
+        yield mock_get_status
+
+
 @pytest.fixture(autouse=True)
-def patch_io(patch_load_json, patch_save_json):
+def patch_io(patch_load_json, patch_save_json, patch_get_status):
     """Prevent PS4 doing I/O."""
