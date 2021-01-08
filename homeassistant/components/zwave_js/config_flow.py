7a9b3cf82c9ba4ae7bfe6fff919b64cccc8dde5f
Paulus Schoutsen <balloob@gmail.com>
2021-01-08 11:41:29 +0100

    Use zwave js server version check instead of fetching full state (#44943)
    * Use version check instead of fetching full state
    
    * Fix tests
    
    * Use 0.3.0
    
    * Also catch aiohttp client errors
    
    * Update docstring
    
    * Lint
    
    * Unignore zwave_js

--- homeassistant/components/zwave_js/config_flow.py
+++ homeassistant/components/zwave_js/config_flow.py
@@ -1,10 +1,15 @@
 """Config flow for Z-Wave JS integration."""
+import asyncio
 import logging
 
+import aiohttp
+from async_timeout import timeout
 import voluptuous as vol
+from zwave_js_server.version import VersionInfo, get_server_version
 
 from homeassistant import config_entries, core, exceptions
 from homeassistant.const import CONF_URL
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN, NAME  # pylint:disable=unused-import
 
@@ -13,18 +18,18 @@ _LOGGER = logging.getLogger(__name__)
 STEP_USER_DATA_SCHEMA = vol.Schema({CONF_URL: str})
 
 
-async def validate_input(hass: core.HomeAssistant, user_input: dict) -> bool:
+async def validate_input(hass: core.HomeAssistant, user_input: dict) -> VersionInfo:
     """Validate if the user input allows us to connect."""
     ws_address = user_input[CONF_URL]
 
-    # pylint: disable=fixme
-    # TODO: actually test server connection
-    if not ws_address.startswith("ws://") and not ws_address.startswith("wss://"):
-        raise CannotConnect
-    if not ws_address.endswith("/zjs"):
-        raise CannotConnect
+    if not ws_address.startswith(("ws://", "wss://")):
+        raise InvalidInput("invalid_ws_url")
 
-    return True
+    async with timeout(10):
+        try:
+            return await get_server_version(ws_address, async_get_clientsession(hass))
+        except (asyncio.TimeoutError, aiohttp.ClientError) as err:
+            raise InvalidInput("cannot_connect") from err
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -43,13 +48,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         try:
-            await validate_input(self.hass, user_input)
-        except CannotConnect:
-            errors["base"] = "cannot_connect"
+            version_info = await validate_input(self.hass, user_input)
+        except InvalidInput as err:
+            errors["base"] = err.error
         except Exception:  # pylint: disable=broad-except
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
         else:
+            await self.async_set_unique_id(version_info.home_id)
+            self._abort_if_unique_id_configured(user_input)
             return self.async_create_entry(title=NAME, data=user_input)
 
         return self.async_show_form(
@@ -57,5 +64,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
 
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
+class InvalidInput(exceptions.HomeAssistantError):
+    """Error to indicate input data is invalid."""
+
+    def __init__(self, error):
+        """Initialize error."""
+        super().__init__()
+        self.error = error
