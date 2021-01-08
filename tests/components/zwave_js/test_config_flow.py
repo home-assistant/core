ef681faa8ca184d6d44aacfe1dfd49661b87d58e
Paulus Schoutsen <balloob@gmail.com>
2021-01-08 09:45:09 +0000

    Fix tests

--- tests/components/zwave_js/test_config_flow.py
+++ tests/components/zwave_js/test_config_flow.py
@@ -1,14 +1,13 @@
 """Test the Z-Wave JS config flow."""
+import asyncio
 from unittest.mock import patch
 
-import pytest
+from zwave_js_server.version import VersionInfo
 
 from homeassistant import config_entries, setup
-from homeassistant.components.zwave_js.config_flow import CannotConnect, InvalidAuth
 from homeassistant.components.zwave_js.const import DOMAIN
 
 
-@pytest.mark.skip
 async def test_form(hass):
     """Test we get the form."""
     await setup.async_setup_component(hass, "persistent_notification", {})
@@ -16,11 +15,14 @@ async def test_form(hass):
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     assert result["type"] == "form"
-    assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.zwave_js.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
+        "homeassistant.components.zwave_js.config_flow.get_server_version",
+        return_value=VersionInfo(
+            driver_version="mock-driver-version",
+            server_version="mock-server-version",
+            home_id=1234,
+        ),
     ), patch(
         "homeassistant.components.zwave_js.async_setup", return_value=True
     ) as mock_setup, patch(
@@ -30,67 +32,47 @@ async def test_form(hass):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
+                "url": "ws://localhost:3000",
             },
         )
         await hass.async_block_till_done()
 
     assert result2["type"] == "create_entry"
-    assert result2["title"] == "Name of the device"
+    assert result2["title"] == "Z-Wave JS"
     assert result2["data"] == {
-        "host": "1.1.1.1",
-        "username": "test-username",
-        "password": "test-password",
+        "url": "ws://localhost:3000",
     }
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
+    assert result2["result"].unique_id == 1234
 
 
-@pytest.mark.skip
-async def test_form_invalid_auth(hass):
+async def test_form_invalid_input(hass):
     """Test we handle invalid auth."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
     with patch(
-        "homeassistant.components.zwave_js.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
+        "homeassistant.components.zwave_js.config_flow.get_server_version",
+        side_effect=asyncio.TimeoutError,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_auth"}
-
-
-@pytest.mark.skip
-async def test_form_cannot_connect(hass):
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.zwave_js.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
+                "url": "ws://localhost:3000",
             },
         )
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "cannot_connect"}
+
+    result3 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            "url": "not-ws-url",
+        },
+    )
+
+    assert result3["type"] == "form"
+    assert result3["errors"] == {"base": "invalid_ws_url"}
