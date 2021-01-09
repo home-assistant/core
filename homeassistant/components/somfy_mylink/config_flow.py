19eca2e983db5d0c6220df7019c8c5bd5ae9b51d
J. Nick Koston <nick@koston.org>
2021-01-08 16:49:19 -1000

    adjust

--- homeassistant/components/somfy_mylink/config_flow.py
+++ homeassistant/components/somfy_mylink/config_flow.py
@@ -163,7 +163,7 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
             entities_config.setdefault(self._entity_id, {})[CONF_REVERSE] = user_input[
                 CONF_REVERSE
             ]
-            self.options.setdefault(ENTITY_CONFIG_VERSION, 1)
+            self.options.setdefault(ENTITY_CONFIG_VERSION, 0)
             self.options[ENTITY_CONFIG_VERSION] += 1
             return await self.async_step_init()
         else:
