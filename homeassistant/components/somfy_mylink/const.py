aeda6e523d1280fbe2d79e34091131484eded162
J. Nick Koston <nick@koston.org>
2021-01-08 18:33:45 -1000

    Add config flow for somfy_mylink

---
+++ homeassistant/components/somfy_mylink/const.py
@@ -0,0 +1,14 @@
+"""Component for the Somfy MyLink device supporting the Synergy API."""
+
+CONF_ENTITY_CONFIG = "entity_config"
+CONF_SYSTEM_ID = "system_id"
+CONF_REVERSE = "reverse"
+CONF_DEFAULT_REVERSE = "default_reverse"
+DEFAULT_CONF_DEFAULT_REVERSE = False
+DATA_SOMFY_MYLINK = "somfy_mylink_data"
+MYLINK_STATUS = "mylink_status"
+MYLINK_ENTITY_IDS = "mylink_entity_ids"
+DOMAIN = "somfy_mylink"
+SOMFY_MYLINK_COMPONENTS = ["cover"]
+MANUFACTURER = "Somfy"
+DEFAULT_PORT = 44100
