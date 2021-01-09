5bd84edb791a9aeb4265bd020137ef51cb470ff1
J. Nick Koston <nick@koston.org>
2021-01-08 15:44:10 -1000

    fix

--- homeassistant/components/somfy_mylink/cover.py
+++ homeassistant/components/somfy_mylink/cover.py
@@ -8,6 +8,8 @@ from homeassistant.components.cover import (
     ENTITY_ID_FORMAT,
     CoverEntity,
 )
+from homeassistant.const import STATE_CLOSED, STATE_OPEN
+from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.util import slugify
 
 from .const import (
@@ -54,7 +56,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     async_add_entities(cover_list)
 
 
-class SomfyShade(CoverEntity):
+class SomfyShade(RestoreEntity, CoverEntity):
     """Object for controlling a Somfy cover."""
 
     def __init__(
@@ -70,7 +72,15 @@ class SomfyShade(CoverEntity):
         self._target_id = target_id
         self._name = name
         self._reverse = reverse
+        self._closed = None
+        self._is_opening = None
+        self._is_closing = None
         self._device_class = device_class
+
+    @property
+    def should_poll(self):
+        """No polling since assumed state."""
+        return False
 
     @property
     def unique_id(self):
@@ -83,11 +93,6 @@ class SomfyShade(CoverEntity):
         return self._name
 
     @property
-    def is_closed(self):
-        """Return if the cover is closed."""
-        return None
-
-    @property
     def assumed_state(self):
         """Let HA know the integration is assumed state."""
         return True
@@ -98,6 +103,21 @@ class SomfyShade(CoverEntity):
         return self._device_class
 
     @property
+    def is_opening(self):
+        """Return if the cover is opening."""
+        return self._is_opening
+
+    @property
+    def is_closing(self):
+        """Return if the cover is closing."""
+        return self._is_closing
+
+    @property
+    def is_closed(self) -> bool:
+        """Return if the cover is closed."""
+        return self._closed
+
+    @property
     def device_info(self):
         """Return the device_info of the device."""
         return {
@@ -106,20 +126,48 @@ class SomfyShade(CoverEntity):
             "manufacturer": MANUFACTURER,
         }
 
-    async def async_open_cover(self, **kwargs):
-        """Wrap Homeassistant calls to open the cover."""
-        if not self._reverse:
-            await self.somfy_mylink.move_up(self._target_id)
-        else:
-            await self.somfy_mylink.move_down(self._target_id)
-
     async def async_close_cover(self, **kwargs):
-        """Wrap Homeassistant calls to close the cover."""
-        if not self._reverse:
-            await self.somfy_mylink.move_down(self._target_id)
-        else:
-            await self.somfy_mylink.move_up(self._target_id)
+        """Close the cover."""
+        self._is_closing = True
+        self.async_write_ha_state()
+        try:
+            # Blocks until the close command is sent
+            if not self._reverse:
+                await self.somfy_mylink.move_down(self._target_id)
+            else:
+                await self.somfy_mylink.move_up(self._target_id)
+            self._closed = True
+        finally:
+            self._is_closing = None
+            self.async_write_ha_state()
+
+    async def async_open_cover(self, **kwargs):
+        """Open the cover."""
+        self._is_opening = True
+        self.async_write_ha_state()
+        try:
+            # Blocks until the open command is sent
+            if not self._reverse:
+                await self.somfy_mylink.move_up(self._target_id)
+            else:
+                await self.somfy_mylink.move_down(self._target_id)
+            self._closed = False
+        finally:
+            self._is_opening = None
+            self.async_write_ha_state()
 
     async def async_stop_cover(self, **kwargs):
         """Stop the cover."""
         await self.somfy_mylink.move_stop(self._target_id)
+
+    async def async_added_to_hass(self):
+        """Complete the initialization."""
+        await super().async_added_to_hass()
+        # Restore the last state
+        last_state = await self.async_get_last_state()
+
+        if last_state is not None and last_state.state in (
+            STATE_OPEN,
+            STATE_CLOSED,
+        ):
+            self._closed = last_state.state == STATE_CLOSED
