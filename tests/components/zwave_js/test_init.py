d489d9b727b62677cbb3795060edbe01199c29fb
Martin Hjelmare <marhje52@gmail.com>
2021-01-10 01:23:47 +0100

    Test on connect and on disconnect

--- tests/components/zwave_js/test_init.py
+++ tests/components/zwave_js/test_init.py
@@ -1,5 +1,8 @@
 """Test the Z-Wave JS init module."""
 from homeassistant.config_entries import ENTRY_STATE_LOADED, ENTRY_STATE_NOT_LOADED
+from homeassistant.const import STATE_UNAVAILABLE
+
+AIR_TEMPERATURE_SENSOR = "sensor.multisensor_6_air_temperature"
 
 
 async def test_entry_setup_unload(hass, client, integration):
@@ -26,3 +29,33 @@ async def test_home_assistant_stop(hass, client, integration):
     await hass.async_stop()
 
     assert client.disconnect.call_count == 1
+
+
+async def test_on_connect_disconnect(hass, client, multisensor_6, integration):
+    """Test we handle disconnect and reconnect."""
+    on_connect = client.register_on_connect.call_args[0][0]
+    on_disconnect = client.register_on_disconnect.call_args[0][0]
+    state = hass.states.get(AIR_TEMPERATURE_SENSOR)
+
+    assert state
+    assert state.state != STATE_UNAVAILABLE
+
+    client.connected = False
+
+    await on_disconnect()
+    await hass.async_block_till_done()
+
+    state = hass.states.get(AIR_TEMPERATURE_SENSOR)
+
+    assert state
+    assert state.state == STATE_UNAVAILABLE
+
+    client.connected = True
+
+    await on_connect()
+    await hass.async_block_till_done()
+
+    state = hass.states.get(AIR_TEMPERATURE_SENSOR)
+
+    assert state
+    assert state.state != STATE_UNAVAILABLE
