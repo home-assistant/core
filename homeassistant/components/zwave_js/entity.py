c3f2c2239443a8177259d3bf50cd19560c7b5378
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-01-10 02:02:22 +0100

    guard value updated log

--- homeassistant/components/zwave_js/entity.py
+++ homeassistant/components/zwave_js/entity.py
@@ -96,13 +96,14 @@ class ZWaveBaseEntity(Entity):
             value_id = event_data["value"].value_id
         if value_id in self.watched_value_ids:
             value = self.info.node.values[value_id]
-            LOGGER.debug(
-                "[%s] Value %s/%s changed to: %s",
-                self.entity_id,
-                value.property_,
-                value.property_key_name,
-                value.value,
-            )
+            if LOGGER.isEnabledFor(logging.DEBUG):
+                LOGGER.debug(
+                    "[%s] Value %s/%s changed to: %s",
+                    self.entity_id,
+                    value.property_,
+                    value.property_key_name,
+                    value.value,
+                )
             self.on_value_update()
             self.async_write_ha_state()
 
