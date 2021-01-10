282ef6a193296c6c1df7323217cc2cb92b5fe084
Martin Hjelmare <marhje52@gmail.com>
2021-01-10 20:20:26 +0100

    Add zwave_js test foundation (#44983)
    * Exclude text files from codespell
    
    * Add basic dump fixture
    
    * Add test foundation
    
    * Fix test after rebase
    
    * Exclude jsonl files from codespell
    
    * Rename fixture file type to jsonl
    
    * Update fixture path
    
    * Fix stale docstring
    
    * Add controller state json fixture
    
    * Add multisensor 6 state json fixture
    
    * Update fixtures
    
    * Remove basic dump fixture
    
    * Fix fixtures after library bump

---
+++ tests/components/zwave_js/test_sensor.py
@@ -0,0 +1,14 @@
+"""Test the Z-Wave JS sensor platform."""
+from homeassistant.const import DEVICE_CLASS_TEMPERATURE, TEMP_CELSIUS
+
+AIR_TEMPERATURE_SENSOR = "sensor.multisensor_6_air_temperature"
+
+
+async def test_numeric_sensor(hass, multisensor_6, integration):
+    """Test the numeric sensor."""
+    state = hass.states.get(AIR_TEMPERATURE_SENSOR)
+
+    assert state
+    assert state.state == "9.0"
+    assert state.attributes["unit_of_measurement"] == TEMP_CELSIUS
+    assert state.attributes["device_class"] == DEVICE_CLASS_TEMPERATURE
