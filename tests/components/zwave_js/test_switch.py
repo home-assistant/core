e6b1175b61ed1808d55954a171bc052749ec0839
Charles Garwood <cgarwood@gmail.com>
2021-01-11 18:09:02 -0500

    Test state change on value updated event

--- tests/components/zwave_js/test_switch.py
+++ tests/components/zwave_js/test_switch.py
@@ -1,11 +1,14 @@
 """Test the Z-Wave JS sensor platform."""
 
+from zwave_js_server.event import Event
+
 from .common import SWITCH_ENTITY
 
 
 async def test_switch(hass, hank_binary_switch, integration, client):
     """Test the switch."""
     state = hass.states.get(SWITCH_ENTITY)
+    node = client.driver.controller.nodes[32]
 
     assert state
     assert state.state == "off"
@@ -33,6 +36,29 @@ async def test_switch(hass, hank_binary_switch, integration, client):
         "value": False,
     }
     assert args["value"] is True
+
+    # Test state updates from value updated event
+    event = Event(
+        type="value updated",
+        data={
+            "source": "node",
+            "event": "value updated",
+            "nodeId": 32,
+            "args": {
+                "commandClassName": "Binary Switch",
+                "commandClass": 37,
+                "endpoint": 0,
+                "property": "currentValue",
+                "newValue": True,
+                "prevValue": False,
+                "propertyName": "currentValue",
+            },
+        },
+    )
+    node.receive_event(event)
+
+    state = hass.states.get(SWITCH_ENTITY)
+    assert state.state == "on"
 
     # Test turning off
     await hass.services.async_call(
