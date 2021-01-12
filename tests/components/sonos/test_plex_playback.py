48e6792e61dd9f8225bc01a1220cda794e4ca067
Jason Lawrence <jjlawren@users.noreply.github.com>
2021-01-12 12:34:43 -0600

    Additional Sonos->Plex tests

--- tests/components/sonos/test_plex_playback.py
+++ tests/components/sonos/test_plex_playback.py
@@ -1,4 +1,7 @@
 """Tests for the Sonos Media Player platform."""
+from unittest.mock import patch
+
+from plexapi.myplex import MyPlexAccount
 import pytest
 
 from homeassistant.components.media_player.const import (
@@ -15,8 +18,36 @@ from homeassistant.exceptions import HomeAssistantError
 from .test_media_player import setup_platform
 
 
-async def test_plex_play_media(hass, config_entry, config):
+async def test_plex_play_media(
+    hass,
+    config_entry,
+    config,
+    requests_mock,
+    plextv_account,
+    plex_empty_payload,
+    plex_sonos_resources,
+):
     """Test join/unjoin requires control access."""
+    requests_mock.get("https://plex.tv/users/account", text=plextv_account)
+    requests_mock.get("https://sonos.plex.tv/resources", text=plex_empty_payload)
+
+    class MockPlexServer:
+        """Mock a PlexServer instance."""
+
+        def __init__(self, has_media=False):
+            self.account = MyPlexAccount(token="token")
+            self.friendly_name = "plex"
+            if has_media:
+                self.media = "media"
+            else:
+                self.media = None
+
+        def create_playqueue(self, media, **kwargs):
+            pass
+
+        def lookup_media(self, content_type, **kwargs):
+            return self.media
+
     await setup_platform(hass, config_entry, config)
     hass.data[PLEX_DOMAIN] = {SERVERS: {}}
     media_player = "media_player.zone_a"
@@ -35,6 +66,9 @@ async def test_plex_play_media(hass, config_entry, config):
         )
     assert "No Plex servers available" in str(excinfo.value)
 
+    # Add a mocked Plex server with no media
+    hass.data[PLEX_DOMAIN][SERVERS] = {"plex": MockPlexServer()}
+
     # Test Plex service call with dict
     with pytest.raises(HomeAssistantError) as excinfo:
         assert await hass.services.async_call(
@@ -43,8 +77,44 @@ async def test_plex_play_media(hass, config_entry, config):
             {
                 ATTR_ENTITY_ID: media_player,
                 ATTR_MEDIA_CONTENT_TYPE: MEDIA_TYPE_MUSIC,
-                ATTR_MEDIA_CONTENT_ID: 'plex://{"library_name": "Music", "artist_name": "Artist", "album_name": "Album"}',
+                ATTR_MEDIA_CONTENT_ID: 'plex://{"library_name": "Music", "artist_name": "Artist"}',
             },
             True,
         )
-    assert "No Plex servers available" in str(excinfo.value)
+    assert "Plex media not found" in str(excinfo.value)
+
+    # Add a mocked Plex server
+    hass.data[PLEX_DOMAIN][SERVERS] = {"plex": MockPlexServer(has_media=True)}
+
+    # Test Plex service call with no Sonos speakers
+    requests_mock.get("https://sonos.plex.tv/resources", text=plex_empty_payload)
+    with pytest.raises(HomeAssistantError) as excinfo:
+        assert await hass.services.async_call(
+            MP_DOMAIN,
+            SERVICE_PLAY_MEDIA,
+            {
+                ATTR_ENTITY_ID: media_player,
+                ATTR_MEDIA_CONTENT_TYPE: MEDIA_TYPE_MUSIC,
+                ATTR_MEDIA_CONTENT_ID: 'plex://{"library_name": "Music", "artist_name": "Artist"}',
+            },
+            True,
+        )
+    assert "Sonos speaker 'Zone A' is not associated with" in str(excinfo.value)
+
+    # Test successful Plex service call
+    account = hass.data[PLEX_DOMAIN][SERVERS]["plex"].account
+    requests_mock.get("https://sonos.plex.tv/resources", text=plex_sonos_resources)
+
+    with patch.object(account, "_sonos_cache_timestamp", 0), patch(
+        "plexapi.sonos.PlexSonosClient.playMedia"
+    ):
+        assert await hass.services.async_call(
+            MP_DOMAIN,
+            SERVICE_PLAY_MEDIA,
+            {
+                ATTR_ENTITY_ID: media_player,
+                ATTR_MEDIA_CONTENT_TYPE: MEDIA_TYPE_MUSIC,
+                ATTR_MEDIA_CONTENT_ID: 'plex://{"plex_server": "plex", "library_name": "Music", "artist_name": "Artist", "album_name": "Album"}',
+            },
+            True,
+        )
