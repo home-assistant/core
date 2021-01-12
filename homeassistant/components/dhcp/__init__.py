2b661925d0c5df7c07856c271b7df638dbec3f0e
J. Nick Koston <nick@koston.org>
2021-01-12 07:30:57 -1000

    adj

--- homeassistant/components/dhcp/__init__.py
+++ homeassistant/components/dhcp/__init__.py
@@ -9,6 +9,7 @@ _LOGGER = logging.getLogger(__name__)
 from scapy.all import DHCP, AsyncSniffer, Ether
 
 from homeassistant.const import EVENT_HOMEASSISTANT_STARTED, EVENT_HOMEASSISTANT_STOP
+from homeassistant.helpers.device_registry import format_mac
 
 FILTER = "udp and (port 67 or 68)"
 REQUESTED_ADDR = "requested_addr"
@@ -19,8 +20,13 @@ async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the dhcp component."""
 
     async def _initialize(_):
-        dhcp_watcher = AsyncSniffer(filter=FILTER, prn=_handle_dhcp_packet)
-        dhcp_watcher.start()
+        try:
+            dhcp_watcher = AsyncSniffer(filter=FILTER, prn=_handle_dhcp_packet)
+            dhcp_watcher.start()
+        except Exception as ex:
+            _LOGGER.info("Cannot watch for dhcp packets: %s", ex)
+            return
+
         hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, dhcp_watcher.stop)
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STARTED, _initialize)
@@ -29,14 +35,11 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 def _decode_option(dhcp_options, key):
     """Extract and decode data from a packet option."""
-    try:
-        for i in dhcp_options:
-            if i[0] != key:
-                continue
-            # hostname is unicode
-            return i[1].decode() if key == HOSTNAME else i[i]
-    except Exception:  # pylint: disable=broad-except
-        pass
+    for i in dhcp_options:
+        if i[0] != key:
+            continue
+        # hostname is unicode
+        return i[1].decode() if key == HOSTNAME else i[i]
 
 
 def _handle_dhcp_packet(packet):
@@ -45,12 +48,16 @@ def _handle_dhcp_packet(packet):
         return
 
     # DHCP request
-    if packet[DHCP].options[0][1] == 3:
-        options = packet[DHCP].options
-        requested_addr = _decode_option(options, REQUESTED_ADDR)
-        hostname = _decode_option(options, HOSTNAME)
-        _LOGGER.warning(
-            f"Host {hostname} ({packet[Ether].src}) requested {requested_addr}"
-        )
+    try:
+        if packet[DHCP].options[0][1] == 3:
+            options = packet[DHCP].options
+            ip_address = _decode_option(options, REQUESTED_ADDR)
+            hostname = _decode_option(options, HOSTNAME)
+            mac = format_mac(packet[Ether].src)
+
+            _LOGGER.warning(f"Host {hostname} ({mac}) requested {ip_address}")
+    except Exception as ex:  # pylint: disable=broad-except
+        _LOGGER.debug("Error decoding DHCP packet: %s", ex)
+        pass
 
     return
