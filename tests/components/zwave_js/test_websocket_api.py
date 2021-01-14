757a6e916e0bef348958e583ddf8f1e0aa39d28a
Charles Garwood <cgarwood@gmail.com>
2021-01-14 14:51:08 -0500

    Adjust websocket client param name

--- tests/components/zwave_js/test_websocket_api.py
+++ tests/components/zwave_js/test_websocket_api.py
@@ -3,15 +3,15 @@
 from homeassistant.components.zwave_js.websocket_api import ENTRY_ID, ID, TYPE
 
 
-async def test_websocket_api(hass, client, integration, hass_ws_client):
+async def test_websocket_api(hass, integration, hass_ws_client):
     """Test the network_status websocket command."""
     entry = integration
-    client = await hass_ws_client(hass)
+    ws_client = await hass_ws_client(hass)
 
-    await client.send_json(
+    await ws_client.send_json(
         {ID: 2, TYPE: "zwave_js/network_status", ENTRY_ID: entry.entry_id}
     )
-    msg = await client.receive_json()
+    msg = await ws_client.receive_json()
     result = msg["result"]
 
     assert result["client"]["ws_server_url"] == "ws://test:3000/zjs"
