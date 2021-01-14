d204166e9cfeaf474782d71ea2c1bf0a3fbdd3e6
unaiur <unaiur@gmail.com>
2021-01-14 11:33:02 +0100

    Migrate to maxcube-api 0.3.0 version (#45126)
    Upgrade maxcube-api to solve bugs fixed in last 3 years.

--- homeassistant/components/maxcube/binary_sensor.py
+++ homeassistant/components/maxcube/binary_sensor.py
@@ -16,7 +16,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
             name = f"{cube.room_by_id(device.room_id).name} {device.name}"
 
             # Only add Window Shutters
-            if cube.is_windowshutter(device):
+            if device.is_windowshutter():
                 devices.append(MaxCubeShutter(handler, name, device.rf_address))
 
     if devices:
