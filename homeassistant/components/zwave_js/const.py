d02ecf753b9158fc513c47a6a8f013c067dd3d2a
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-01-15 15:15:03 +0100

    Add binary_sensor platform to zwave_js (#45081)
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/zwave_js/const.py
+++ homeassistant/components/zwave_js/const.py
@@ -3,7 +3,7 @@
 
 DOMAIN = "zwave_js"
 NAME = "Z-Wave JS"
-PLATFORMS = ["light", "sensor", "switch"]
+PLATFORMS = ["binary_sensor", "light", "sensor", "switch"]
 
 DATA_CLIENT = "client"
 DATA_UNSUBSCRIBE = "unsubs"
---
+++ tests/components/zwave_js/test_binary_sensor.py
@@ -0,0 +1,86 @@
+"""Test the Z-Wave JS binary sensor platform."""
+from zwave_js_server.event import Event
+
+from homeassistant.components.binary_sensor import DEVICE_CLASS_MOTION
+from homeassistant.const import DEVICE_CLASS_BATTERY, STATE_OFF, STATE_ON
+
+from .common import (
+    DISABLED_LEGACY_BINARY_SENSOR,
+    ENABLED_LEGACY_BINARY_SENSOR,
+    LOW_BATTERY_BINARY_SENSOR,
+    NOTIFICATION_MOTION_BINARY_SENSOR,
+)
+
+
+async def test_low_battery_sensor(hass, multisensor_6, integration):
+    """Test boolean binary sensor of type low battery."""
+    state = hass.states.get(LOW_BATTERY_BINARY_SENSOR)
+
+    assert state
+    assert state.state == STATE_OFF
+    assert state.attributes["device_class"] == DEVICE_CLASS_BATTERY
+
+
+async def test_enabled_legacy_sensor(hass, ecolink_door_sensor, integration):
+    """Test enabled legacy boolean binary sensor."""
+    node = ecolink_door_sensor
+    # this node has Notification CC not (fully) implemented
+    # so legacy binary sensor should be enabled
+
+    state = hass.states.get(ENABLED_LEGACY_BINARY_SENSOR)
+    assert state
+    assert state.state == STATE_OFF
+    assert state.attributes.get("device_class") is None
+
+    # Test state updates from value updated event
+    event = Event(
+        type="value updated",
+        data={
+            "source": "node",
+            "event": "value updated",
+            "nodeId": 53,
+            "args": {
+                "commandClassName": "Binary Sensor",
+                "commandClass": 48,
+                "endpoint": 0,
+                "property": "Any",
+                "newValue": True,
+                "prevValue": False,
+                "propertyName": "Any",
+            },
+        },
+    )
+    node.receive_event(event)
+
+    state = hass.states.get(ENABLED_LEGACY_BINARY_SENSOR)
+    assert state.state == STATE_ON
+
+
+async def test_disabled_legacy_sensor(hass, multisensor_6, integration):
+    """Test disabled legacy boolean binary sensor."""
+    # this node has Notification CC implemented so legacy binary sensor should be disabled
+
+    registry = await hass.helpers.entity_registry.async_get_registry()
+    entity_id = DISABLED_LEGACY_BINARY_SENSOR
+    state = hass.states.get(entity_id)
+    assert state is None
+    entry = registry.async_get(entity_id)
+    assert entry
+    assert entry.disabled
+    assert entry.disabled_by == "integration"
+
+    # Test enabling legacy entity
+    updated_entry = registry.async_update_entity(
+        entry.entity_id, **{"disabled_by": None}
+    )
+    assert updated_entry != entry
+    assert updated_entry.disabled is False
+
+
+async def test_notification_sensor(hass, multisensor_6, integration):
+    """Test binary sensor created from Notification CC."""
+    state = hass.states.get(NOTIFICATION_MOTION_BINARY_SENSOR)
+
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes["device_class"] == DEVICE_CLASS_MOTION
