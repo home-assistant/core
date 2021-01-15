7c8b261f927d2a0677357d8c958a996cf2672e77
tomasz75 <tomek@sviete.pl>
2021-01-15 16:37:31 +0100

    release script

--- homeassistant/components/ais_updater/scripts/release_script.sh
+++ homeassistant/components/ais_updater/scripts/release_script.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-echo "AIS release script starting for 20.11.24 on chanel 2" 
-curl -o "/sdcard/AisClient.apk" -L https://powiedz.co/ota/android/AisDomApp-client-gate-release.apk && su -c "pm install -r /sdcard/AisClient.apk" 
+echo "AIS release script starting for 21.01.06 on chanel 2" 
+apt update 
 
-echo "21.01.06" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
+apt upgrade  -y 
+
+echo "21.01.14" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
