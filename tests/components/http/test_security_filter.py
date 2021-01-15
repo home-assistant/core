a702b0e1799f6993e039a4239ae97da358b261ad
Franck Nijhof <git@frenck.dev>
2021-01-15 10:30:29 +0100

    Extend filter and filter tests (#45179)

--- tests/components/http/test_security_filter.py
+++ tests/components/http/test_security_filter.py
@@ -35,17 +35,26 @@ async def test_ok_requests(request_path, request_params, aiohttp_client):
 
 
 @pytest.mark.parametrize(
-    "request_path,request_params",
+    "request_path,request_params,fail_on_query_string",
     [
-        ("/proc/self/environ", {}),
-        ("/", {"test": "/test/../../api"}),
-        ("/", {"test": "test/../../api"}),
-        ("/", {"sql": ";UNION SELECT (a, b"}),
-        ("/", {"sql": "concat(..."}),
-        ("/", {"xss": "<script >"}),
+        ("/proc/self/environ", {}, False),
+        ("/", {"test": "/test/../../api"}, True),
+        ("/", {"test": "test/../../api"}, True),
+        ("/", {"test": "/test/%2E%2E%2f%2E%2E%2fapi"}, True),
+        ("/", {"test": "test/%2E%2E%2f%2E%2E%2fapi"}, True),
+        ("/test/%2E%2E%2f%2E%2E%2fapi", {}, False),
+        ("/", {"sql": ";UNION SELECT (a, b"}, True),
+        ("/", {"sql": "UNION%20SELECT%20%28a%2C%20b"}, True),
+        ("/UNION%20SELECT%20%28a%2C%20b", {}, False),
+        ("/", {"sql": "concat(..."}, True),
+        ("/", {"xss": "<script >"}, True),
+        ("/<script >", {"xss": ""}, False),
+        ("/%3Cscript%3E", {}, False),
     ],
 )
-async def test_bad_requests(request_path, request_params, aiohttp_client):
+async def test_bad_requests(
+    request_path, request_params, fail_on_query_string, aiohttp_client, caplog
+):
     """Test request paths that should be filtered."""
     app = web.Application()
     app.router.add_get("/{all:.*}", mock_handler)
@@ -56,3 +65,8 @@ async def test_bad_requests(request_path, request_params, aiohttp_client):
     resp = await mock_api_client.get(request_path, params=request_params)
 
     assert resp.status == 400
+
+    message = "Filtered a potential harmful request to:"
+    if fail_on_query_string:
+        message = "Filtered a request with a potential harmful query string:"
+    assert message in caplog.text
