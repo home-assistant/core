ff0d6b5ab769b8a76447e3688147ba5f743ccedf
Joshua Peterson <joshuapeterson37@yahoo.com>
2021-01-15 18:54:38 +0000

    Fix Test

--- tests/components/sleepiq/test_init.py
+++ tests/components/sleepiq/test_init.py
@@ -4,7 +4,6 @@ from unittest.mock import MagicMock, patch
 from homeassistant import setup
 import homeassistant.components.sleepiq as sleepiq
 
-# from tests.async_mock import MagicMock, patch
 from tests.common import load_fixture
 
 CONFIG = {"sleepiq": {"username": "foo", "password": "bar"}}
@@ -26,29 +25,28 @@ def mock_responses(mock, single=False):
     )
 
 
-# async def test_setup(hass, requests_mock):
-#     """Test the setup."""
-#     mock_responses(requests_mock)
+async def test_setup(hass, requests_mock):
+    """Test the setup."""
+    mock_responses(requests_mock)
 
-#     # We're mocking the load_platform discoveries or else the platforms
-#     # will be setup during tear down when blocking till done, but the mocks
-#     # are no longer active.
-#     with patch("homeassistant.helpers.discovery.load_platform", MagicMock()):
-#         response = sleepiq.setup(hass, CONFIG)
-#         assert response
+    # We're mocking the load_platform discoveries or else the platforms
+    # will be setup during tear down when blocking till done, but the mocks
+    # are no longer active.
+    with patch("homeassistant.helpers.discovery.load_platform", MagicMock()):
+        assert sleepiq.setup(hass, CONFIG)
 
 
-# async def test_setup_login_failed(hass, requests_mock):
-#     """Test the setup if a bad username or password is given."""
-#     mock_responses(requests_mock)
-#     requests_mock.put(
-#         "https://prod-api.sleepiq.sleepnumber.com/rest/login",
-#         status_code=401,
-#         json=load_fixture("sleepiq-login-failed.json"),
-#     )
+async def test_setup_login_failed(hass, requests_mock):
+    """Test the setup if a bad username or password is given."""
+    mock_responses(requests_mock)
+    requests_mock.put(
+        "https://prod-api.sleepiq.sleepnumber.com/rest/login",
+        status_code=401,
+        json=load_fixture("sleepiq-login-failed.json"),
+    )
 
-#     response = sleepiq.setup(hass, CONFIG)
-#     assert not response
+    response = sleepiq.setup(hass, CONFIG)
+    assert not response
 
 
 async def test_setup_component_no_login(hass):
