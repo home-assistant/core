562d30319b4e344a6336ee6663c4a6512f0b2423
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-01-16 10:43:25 -0600

    Add lock platform to zwave_js (#45175)
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/zwave_js/const.py
+++ homeassistant/components/zwave_js/const.py
@@ -3,7 +3,7 @@
 
 DOMAIN = "zwave_js"
 NAME = "Z-Wave JS"
-PLATFORMS = ["binary_sensor", "light", "sensor", "switch"]
+PLATFORMS = ["binary_sensor", "light", "lock", "sensor", "switch"]
 
 DATA_CLIENT = "client"
 DATA_UNSUBSCRIBE = "unsubs"
