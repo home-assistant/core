01ff8c58b0a5b3f9ecb54b55d646b05cff1adce6
Dennis Schroer <dev@dennisschroer.nl>
2021-01-16 15:46:09 +0100

    Apply suggestions from code review
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/huisbaasje/__init__.py
+++ homeassistant/components/huisbaasje/__init__.py
@@ -96,7 +96,7 @@ async def async_update_huisbaasje(huisbaasje):
         # handled by the data update coordinator.
         async with async_timeout.timeout(FETCH_TIMEOUT):
             if not huisbaasje.is_authenticated():
-                _LOGGER.warn("Huisbaasje is unauthenticated. Reauthenticating...")
+                _LOGGER.warning("Huisbaasje is unauthenticated. Reauthenticating...")
                 huisbaasje.authenticate()
 
             current_measurements = await huisbaasje.current_measurements()
