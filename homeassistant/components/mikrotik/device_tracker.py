340431662fc4a408229a3ceabd25c08966dd117a
J. Nick Koston <nick@koston.org>
2021-01-16 23:18:07 -1000

    Use standard device tracker attributes for "ip" and "mac" in Mikrotik (#45255)
    * Mikrotik now uses the device_tracker standard attributes for "ip" and "mac"
    
    The "mac_address" and "ip_address" attributes have been removed.
    
    * Update homeassistant/components/mikrotik/device_tracker.py

--- homeassistant/components/mikrotik/device_tracker.py
+++ homeassistant/components/mikrotik/device_tracker.py
@@ -16,6 +16,10 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+# These are normalized to ATTR_IP and ATTR_MAC to conform
+# to device_tracker
+FILTER_ATTRS = ("ip_address", "mac_address")
+
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up device tracker for Mikrotik component."""
@@ -122,7 +126,7 @@ class MikrotikHubTracker(ScannerEntity):
     def device_state_attributes(self):
         """Return the device state attributes."""
         if self.is_connected:
-            return self.device.attrs
+            return {k: v for k, v in self.device.attrs.items() if k not in FILTER_ATTRS}
         return None
 
     @property
