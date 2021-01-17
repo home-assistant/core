445594e8067f37a1c94e849e892ba6de9fca5b99
J. Nick Koston <nick@koston.org>
2021-01-16 16:09:04 -1000

    Prefill the ip address for powerwall discovery (#45209)

--- tests/components/powerwall/test_config_flow.py
+++ tests/components/powerwall/test_config_flow.py
@@ -5,10 +5,13 @@ from unittest.mock import patch
 from tesla_powerwall import MissingAttributeError, PowerwallUnreachableError
 
 from homeassistant import config_entries, setup
+from homeassistant.components.dhcp import HOSTNAME, IP_ADDRESS, MAC_ADDRESS
 from homeassistant.components.powerwall.const import DOMAIN
 from homeassistant.const import CONF_IP_ADDRESS
 
 from .mocks import _mock_powerwall_side_effect, _mock_powerwall_site_name
+
+from tests.common import MockConfigEntry
 
 
 async def test_form_source_user(hass):
@@ -93,6 +96,27 @@ async def test_form_cannot_connect(hass):
     assert result2["errors"] == {"base": "cannot_connect"}
 
 
+async def test_form_unknown_exeption(hass):
+    """Test we handle an unknown exception."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    mock_powerwall = _mock_powerwall_side_effect(site_info=ValueError)
+
+    with patch(
+        "homeassistant.components.powerwall.config_flow.Powerwall",
+        return_value=mock_powerwall,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_IP_ADDRESS: "1.2.3.4"},
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "unknown"}
+
+
 async def test_form_wrong_version(hass):
     """Test we can handle wrong version error."""
     result = await hass.config_entries.flow.async_init(
@@ -114,3 +138,84 @@ async def test_form_wrong_version(hass):
 
     assert result3["type"] == "form"
     assert result3["errors"] == {"base": "wrong_version"}
+
+
+async def test_already_configured(hass):
+    """Test we abort when already configured."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+
+    config_entry = MockConfigEntry(domain=DOMAIN, data={CONF_IP_ADDRESS: "1.1.1.1"})
+    config_entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": config_entries.SOURCE_DHCP},
+        data={
+            IP_ADDRESS: "1.1.1.1",
+            MAC_ADDRESS: "AA:BB:CC:DD:EE:FF",
+            HOSTNAME: "any",
+        },
+    )
+    assert result["type"] == "abort"
+    assert result["reason"] == "already_configured"
+
+
+async def test_already_configured_with_ignored(hass):
+    """Test ignored entries do not break checking for existing entries."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+
+    config_entry = MockConfigEntry(domain=DOMAIN, data={}, source="ignore")
+    config_entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": config_entries.SOURCE_DHCP},
+        data={
+            IP_ADDRESS: "1.1.1.1",
+            MAC_ADDRESS: "AA:BB:CC:DD:EE:FF",
+            HOSTNAME: "any",
+        },
+    )
+    assert result["type"] == "form"
+
+
+async def test_dhcp_discovery(hass):
+    """Test we can process the discovery from dhcp."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": config_entries.SOURCE_DHCP},
+        data={
+            IP_ADDRESS: "1.1.1.1",
+            MAC_ADDRESS: "AA:BB:CC:DD:EE:FF",
+            HOSTNAME: "any",
+        },
+    )
+    assert result["type"] == "form"
+    assert result["errors"] == {}
+
+    mock_powerwall = await _mock_powerwall_site_name(hass, "Some site")
+    with patch(
+        "homeassistant.components.powerwall.config_flow.Powerwall",
+        return_value=mock_powerwall,
+    ), patch(
+        "homeassistant.components.powerwall.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.powerwall.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_IP_ADDRESS: "1.1.1.1",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] == "create_entry"
+    assert result2["title"] == "Some site"
+    assert result2["data"] == {
+        CONF_IP_ADDRESS: "1.1.1.1",
+    }
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
