bae9004916dc011ed72a9bbb3893b74a30889f2a
Joakim Plate <elupus@ecce.se>
2021-01-19 20:08:04 +0100

    Add a test for missing credential

--- tests/components/onboarding/test_views.py
+++ tests/components/onboarding/test_views.py
@@ -288,6 +288,28 @@ async def test_onboarding_integration(hass, hass_storage, hass_client, hass_admi
         assert len(user.refresh_tokens) == 2, user
 
 
+async def test_onboarding_integration_missing_credential(
+    hass, hass_storage, hass_client, hass_access_token
+):
+    """Test that we fail integration step if user is missing credentials."""
+    mock_storage(hass_storage, {"done": [const.STEP_USER]})
+
+    assert await async_setup_component(hass, "onboarding", {})
+    await hass.async_block_till_done()
+
+    refresh_token = await hass.auth.async_validate_access_token(hass_access_token)
+    refresh_token.credential = None
+
+    client = await hass_client()
+
+    resp = await client.post(
+        "/api/onboarding/integration",
+        json={"client_id": CLIENT_ID, "redirect_uri": CLIENT_REDIRECT_URI},
+    )
+
+    assert resp.status == 403
+
+
 async def test_onboarding_integration_invalid_redirect_uri(
     hass, hass_storage, hass_client
 ):
