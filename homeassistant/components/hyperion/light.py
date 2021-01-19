a4e6a724ea5a91be1b4e9a24deb4267a9a8f391a
starkillerOG <starkiller.og@gmail.com>
2021-01-19 22:35:20 +0100

    Make icon lightbulb if it is off

--- homeassistant/components/hyperion/light.py
+++ homeassistant/components/hyperion/light.py
@@ -346,6 +346,9 @@ class HyperionLight(LightEntity):
     @property
     def icon(self) -> str:
         """Return state specific icon."""
+        if not self._client.is_on():
+            return ICON_LIGHTBULB
+
         return self._icon
 
     @property
