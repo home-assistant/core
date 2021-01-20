c354ebf66dcf695d9a2fe31e1007cf69267a48e6
Kevin Eifinger <k.eifinger@googlemail.com>
2021-01-20 22:07:27 +0100

    Increase Codecov

--- tests/components/kraken/test_config_flow.py
+++ tests/components/kraken/test_config_flow.py
@@ -11,17 +11,47 @@ from tests.common import MockConfigEntry
 
 async def test_config_flow(hass):
     """Test we can finish a config flow."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": "user"}
-    )
-    assert result["type"] == "form"
+    with patch(
+        "pykrakenapi.KrakenAPI.get_tradable_asset_pairs",
+        return_value=TRADEABLE_ASSET_PAIR_RESPONSE,
+    ), patch(
+        "pykrakenapi.KrakenAPI.get_ticker_information",
+        return_value=TICKER_INFORMATION_RESPONSE,
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": "user"}
+        )
+        assert result["type"] == "form"
 
-    result = await hass.config_entries.flow.async_configure(result["flow_id"], {})
-    assert result["type"] == "create_entry"
+        result = await hass.config_entries.flow.async_configure(result["flow_id"], {})
+        assert result["type"] == "create_entry"
 
-    await hass.async_block_till_done()
-    state = hass.states.get("sensor.xbt_usd_ask")
-    assert state
+        await hass.async_block_till_done()
+        state = hass.states.get("sensor.xbt_usd_ask")
+        assert state
+
+
+async def test_already_configured(hass):
+    """Test we can not add a second config flow."""
+    with patch(
+        "pykrakenapi.KrakenAPI.get_tradable_asset_pairs",
+        return_value=TRADEABLE_ASSET_PAIR_RESPONSE,
+    ), patch(
+        "pykrakenapi.KrakenAPI.get_ticker_information",
+        return_value=TICKER_INFORMATION_RESPONSE,
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": "user"}
+        )
+        assert result["type"] == "form"
+
+        result = await hass.config_entries.flow.async_configure(result["flow_id"], {})
+        assert result["type"] == "create_entry"
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": "user"}
+        )
+        assert result["type"] == "abort"
 
 
 async def test_options(hass):
--- tests/components/kraken/test_init.py
+++ tests/components/kraken/test_init.py
@@ -1,6 +1,8 @@
 """Tests for the kraken integration."""
 from unittest.mock import patch
 
+from pykrakenapi.pykrakenapi import CallRateLimitError, KrakenAPIError
+
 from homeassistant.components import kraken
 from homeassistant.components.kraken.const import DOMAIN
 
@@ -25,3 +27,40 @@ async def test_unload_entry(hass):
         await hass.async_block_till_done()
         assert await kraken.async_unload_entry(hass, entry)
         assert DOMAIN not in hass.data
+
+
+async def test_unkown_error(hass, caplog):
+    """Test unload for Kraken."""
+    with patch(
+        "pykrakenapi.KrakenAPI.get_tradable_asset_pairs",
+        return_value=TRADEABLE_ASSET_PAIR_RESPONSE,
+    ), patch(
+        "pykrakenapi.KrakenAPI.get_ticker_information",
+        side_effect=KrakenAPIError("EQuery: Error"),
+    ):
+        entry = MockConfigEntry(domain=DOMAIN)
+        entry.add_to_hass(hass)
+
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+        assert "Unable to fetch data from Kraken.com:" in caplog.text
+
+
+async def test_callrate_limit(hass, caplog):
+    """Test unload for Kraken."""
+    with patch(
+        "pykrakenapi.KrakenAPI.get_tradable_asset_pairs",
+        return_value=TRADEABLE_ASSET_PAIR_RESPONSE,
+    ), patch(
+        "pykrakenapi.KrakenAPI.get_ticker_information",
+        side_effect=CallRateLimitError(),
+    ):
+        entry = MockConfigEntry(domain=DOMAIN)
+        entry.add_to_hass(hass)
+
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+        assert (
+            "Exceeded the Kraken.com call rate limit. Increase the update interval to prevent this error."
+            in caplog.text
+        )
