fa4718d5f9208ecccd5392ee0b8cb68801b12ff7
J. Nick Koston <nick@koston.org>
2021-01-20 05:20:21 -1000

    Add periodic purge

--- homeassistant/helpers/device_registry.py
+++ homeassistant/helpers/device_registry.py
@@ -40,6 +40,8 @@ DELETED_DEVICE = "deleted"
 DISABLED_INTEGRATION = "integration"
 DISABLED_USER = "user"
 
+ORPHANED_DEVICE_KEEP_SECONDS = 86400 * 30
+
 
 @attr.s(slots=True, frozen=True)
 class DeviceEntry:
@@ -544,7 +546,7 @@ class DeviceRegistry:
     @callback
     def async_clear_config_entry(self, config_entry_id: str) -> None:
         """Clear config entry from registry entries."""
-        now = time.time()
+        now_time = time.time()
         for device in list(self.devices.values()):
             self._async_update_device(device.id, remove_config_entry_id=config_entry_id)
         for deleted_device in list(self.deleted_devices.values()):
@@ -555,7 +557,7 @@ class DeviceRegistry:
                 # Add a time stamp when the deleted device
                 # became orphaned
                 self.deleted_devices[deleted_device.id] = attr.evolve(
-                    deleted_device, orphaned_timestamp=now, config_entries=set()
+                    deleted_device, orphaned_timestamp=now_time, config_entries=set()
                 )
             else:
                 config_entries = config_entries - {config_entry_id}
@@ -565,6 +567,24 @@ class DeviceRegistry:
                     deleted_device, config_entries=config_entries
                 )
             self.async_schedule_save()
+
+    @callback
+    def async_purge_expired_orphaned_devices(self) -> None:
+        """Purge expired orphaned devices from the registry.
+
+        We need to purge these periodically to avoid the database
+        growing without bound.
+        """
+        now_time = time.time()
+        for deleted_device in list(self.deleted_devices.values()):
+            if deleted_device.orphaned_timestamp is None:
+                continue
+
+            if (
+                deleted_device.orphaned_timestamp + ORPHANED_DEVICE_KEEP_SECONDS
+                < now_time
+            ):
+                self._remove_device(deleted_device)
 
     @callback
     def async_clear_area_id(self, area_id: str) -> None:
@@ -633,7 +653,9 @@ def async_cleanup(
                     device.id, remove_config_entry_id=config_entry_id
                 )
 
-    # Todo: cleanup orphaned deleted devices older than 30 days
+    # Periodic purge of orphaned devices to avoid the registry
+    # growing without bounds when there are lots of deleted devices
+    dev_reg.async_purge_expired_orphaned_devices()
 
 
 @callback
