b83acd80ef0209aa0945897f52739e475cbd2126
J. Nick Koston <nick@koston.org>
2021-01-22 07:57:07 -1000

    try decorator

--- homeassistant/components/fan/__init__.py
+++ homeassistant/components/fan/__init__.py
@@ -1,8 +1,10 @@
 """Provides functionality to interact with fans."""
+import asyncio
 from datetime import timedelta
+import functools
 import functools as ft
 import logging
-from typing import Optional
+from typing import Callable, List, Optional
 
 import voluptuous as vol
 
@@ -361,3 +363,75 @@ class FanEntity(ToggleEntity):
     def supported_features(self) -> int:
         """Flag supported features."""
         return 0
+
+
+# Decorator
+def percentage_compat(func: Callable) -> Callable:
+    """Compaitiblity for fans that expect speed."""
+
+    # Check for partials to properly determine if coroutine function
+    check_func = func
+    while isinstance(check_func, functools.partial):
+        check_func = check_func.func
+
+    if asyncio.iscoroutinefunction(check_func):
+
+        @functools.wraps(func)
+        async def wrap_async_turn_on(
+            self, speed: str = None, percentage: int = None, **kwargs
+        ) -> None:
+            """Wrap async_turn_on to add percentage compatibility."""
+            if percentage is not None and speed is None:
+                speed = self.percentage_to_speed(percentage)
+
+            return await check_func(speed=speed, percentage=percentage, **kwargs)
+
+        return wrap_async_turn_on
+
+    @functools.wraps(func)
+    async def wrap_turn_on(
+        self, speed: str = None, percentage: int = None, **kwargs
+    ) -> None:
+        """Wrap turn_on to add percentage compatibility."""
+        if percentage is not None and speed is None:
+            speed = self.percentage_to_speed(percentage)
+
+        return check_func(speed=speed, percentage=percentage, **kwargs)
+
+    return wrap_turn_on
+
+
+# Decorator
+def speed_compat(func: Callable) -> Callable:
+    """Compaitiblity for fans that expect percentage."""
+
+    # Check for partials to properly determine if coroutine function
+    check_func = func
+    while isinstance(check_func, functools.partial):
+        check_func = check_func.func
+
+    if asyncio.iscoroutinefunction(check_func):
+
+        @functools.wraps(func)
+        async def wrap_async_turn_on(
+            self, speed: str = None, percentage: int = None, **kwargs
+        ) -> None:
+            """Wrap async_turn_on to add percentage compatibility."""
+            if speed is not None and percentage is None:
+                percentage = self.self.speed_to_percentage(speed)
+
+            return await check_func(speed=speed, percentage=percentage, **kwargs)
+
+        return wrap_async_turn_on
+
+    @functools.wraps(func)
+    async def wrap_turn_on(
+        self, speed: str = None, percentage: int = None, **kwargs
+    ) -> None:
+        """Wrap turn_on to add percentage compatibility."""
+        if speed is not None and percentage is None:
+            percentage = self.self.speed_to_percentage(speed)
+
+        return check_func(speed=speed, percentage=percentage, **kwargs)
+
+    return wrap_turn_on
