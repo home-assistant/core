f9ed190942e0d7697a4e59010d12a99c293e62bc
Robert Svensson <kane610@me.com>
2021-01-22 18:02:29 +0100

    Improve typing

--- homeassistant/components/deconz/logbook.py
+++ homeassistant/components/deconz/logbook.py
@@ -1,21 +1,27 @@
 """Describe deCONZ logbook events."""
 
+from typing import Callable, Optional
+
 from homeassistant.const import ATTR_DEVICE_ID, CONF_EVENT
-from homeassistant.core import callback
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.event import Event
 
 from .const import DOMAIN as DECONZ_DOMAIN
-from .deconz_event import CONF_DECONZ_EVENT
+from .deconz_event import CONF_DECONZ_EVENT, DeconzEvent
 from .device_trigger import _get_deconz_event_from_device_id
 
 
 @callback
-def async_describe_events(hass, async_describe_event):
+def async_describe_events(
+    hass: HomeAssistant,
+    async_describe_event: Callable[[str, str, Callable[[Event], dict]], None],
+) -> None:
     """Describe logbook events."""
 
     @callback
-    def async_describe_deconz_event(event):
+    def async_describe_deconz_event(event: Event) -> dict:
         """Describe deCONZ logbook event."""
-        deconz_event = _get_deconz_event_from_device_id(
+        deconz_event: Optional[DeconzEvent] = _get_deconz_event_from_device_id(
             hass, event.data[ATTR_DEVICE_ID]
         )
 
