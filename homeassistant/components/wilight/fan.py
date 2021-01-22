f93e26d908e386cb00178831f52a59701f688d0d
Leonardo Figueiro <leoagfig@gmail.com>
2021-01-22 15:28:52 -0300

    As MartinHjelmare requested

--- homeassistant/components/wilight/fan.py
+++ homeassistant/components/wilight/fan.py
@@ -1,6 +1,16 @@
 """Support for WiLight Fan."""
 
-from pywilight.const import DIRECTION_OFF, DOMAIN, FAN_V1, ITEM_FAN
+from pywilight.const import (
+    DOMAIN,
+    FAN_V1,
+    ITEM_FAN,
+    WL_DIRECTION_FORWARD,
+    WL_DIRECTION_OFF,
+    WL_DIRECTION_REVERSE,
+    WL_SPEED_HIGH,
+    WL_SPEED_LOW,
+    WL_SPEED_MEDIUM,
+)
 
 from homeassistant.components.fan import (
     DIRECTION_FORWARD,
@@ -21,22 +31,6 @@ SUPPORTED_SPEEDS = [SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH]
 SUPPORTED_FEATURES = SUPPORT_SET_SPEED | SUPPORT_DIRECTION
 
 
-def entities_from_discovered_wilight(hass, api_device):
-    """Parse configuration and add WiLight fan entities."""
-    entities = []
-    for item in api_device.items:
-        if item["type"] != ITEM_FAN:
-            continue
-        index = item["index"]
-        item_name = item["name"]
-        if item["sub_type"] != FAN_V1:
-            continue
-        entity = WiLightFan(api_device, index, item_name)
-        entities.append(entity)
-
-    return entities
-
-
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities
 ):
@@ -44,18 +38,28 @@ async def async_setup_entry(
     parent = hass.data[DOMAIN][entry.entry_id]
 
     # Handle a discovered WiLight device.
-    entities = entities_from_discovered_wilight(hass, parent.api)
+    entities = []
+    for item in parent.api.items:
+        if item["type"] != ITEM_FAN:
+            continue
+        index = item["index"]
+        item_name = item["name"]
+        if item["sub_type"] != FAN_V1:
+            continue
+        entity = WiLightFan(parent.api, index, item_name)
+        entities.append(entity)
+
     async_add_entities(entities)
 
 
 class WiLightFan(WiLightDevice, FanEntity):
     """Representation of a WiLights fan."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, api_device, index, item_name):
         """Initialize the device."""
-        super().__init__(self, *args, **kwargs)
+        super().__init__(self, api_device, index, item_name)
         # Initialize the WiLights fan.
-        self._direction = DIRECTION_FORWARD
+        self._direction = WL_DIRECTION_FORWARD
 
     @property
     def supported_features(self):
@@ -70,7 +74,7 @@ class WiLightFan(WiLightDevice, FanEntity):
     @property
     def is_on(self):
         """Return true if device is on."""
-        return self._status.get("direction", DIRECTION_OFF) != DIRECTION_OFF
+        return self._status.get("direction", WL_DIRECTION_OFF) != WL_DIRECTION_OFF
 
     @property
     def speed(self) -> str:
@@ -86,7 +90,7 @@ class WiLightFan(WiLightDevice, FanEntity):
     def current_direction(self) -> str:
         """Return the current direction of the fan."""
         if "direction" in self._status:
-            if self._status["direction"] != DIRECTION_OFF:
+            if self._status["direction"] != WL_DIRECTION_OFF:
                 self._direction = self._status["direction"]
         return self._direction
 
@@ -99,12 +103,20 @@ class WiLightFan(WiLightDevice, FanEntity):
 
     async def async_set_speed(self, speed: str):
         """Set the speed of the fan."""
-        await self._client.set_fan_speed(self._index, speed)
+        wl_speed = WL_SPEED_HIGH
+        if speed == SPEED_LOW:
+            wl_speed = WL_SPEED_LOW
+        if speed == SPEED_MEDIUM:
+            wl_speed = WL_SPEED_MEDIUM
+        await self._client.set_fan_speed(self._index, wl_speed)
 
     async def async_set_direction(self, direction: str):
         """Set the direction of the fan."""
-        await self._client.set_fan_direction(self._index, direction)
+        wl_direction = WL_DIRECTION_REVERSE
+        if direction == DIRECTION_FORWARD:
+            wl_direction = WL_DIRECTION_FORWARD
+        await self._client.set_fan_direction(self._index, wl_direction)
 
     async def async_turn_off(self, **kwargs):
         """Turn the fan off."""
-        await self._client.set_fan_direction(self._index, DIRECTION_OFF)
+        await self._client.set_fan_direction(self._index, WL_DIRECTION_OFF)
