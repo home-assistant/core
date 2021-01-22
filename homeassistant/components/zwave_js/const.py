3f0254589b0aba89de91d2600954effe167c9833
Chris <firstof9@gmail.com>
2021-01-22 09:51:39 -0700

    Add cover platform to zwave js (#45193)

--- homeassistant/components/zwave_js/const.py
+++ homeassistant/components/zwave_js/const.py
@@ -3,7 +3,7 @@
 
 DOMAIN = "zwave_js"
 NAME = "Z-Wave JS"
-PLATFORMS = ["binary_sensor", "climate", "light", "lock", "sensor", "switch"]
+PLATFORMS = ["binary_sensor", "climate", "cover", "light", "lock", "sensor", "switch"]
 
 DATA_CLIENT = "client"
 DATA_UNSUBSCRIBE = "unsubs"
