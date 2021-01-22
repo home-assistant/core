f0483487a4746746fdec93ddb00e0edec433660f
On Freund <onfreund@gmail.com>
2021-01-22 16:35:10 +0200

    Clear thumbnail cache on browse

--- homeassistant/components/volumio/media_player.py
+++ homeassistant/components/volumio/media_player.py
@@ -258,6 +258,7 @@ class Volumio(MediaPlayerEntity):
 
     async def async_browse_media(self, media_content_type=None, media_content_id=None):
         """Implement the websocket media browsing helper."""
+        self.thumbnail_cache = {}
         if media_content_type in [None, "library"]:
             return await browse_top_level(self._volumio)
 
