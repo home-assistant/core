03bdba2c4cd95031ab38044a7227c8f46be62e96
Paulus Schoutsen <balloob@gmail.com>
2021-01-23 18:28:57 +0100

    Avoid misuse sanitize_path, clarify docs (#45469)

--- tests/util/test_init.py
+++ tests/util/test_init.py
@@ -11,17 +11,17 @@ import homeassistant.util.dt as dt_util
 def test_sanitize_filename():
     """Test sanitize_filename."""
     assert util.sanitize_filename("test") == "test"
-    assert util.sanitize_filename("/test") == "test"
-    assert util.sanitize_filename("..test") == "test"
-    assert util.sanitize_filename("\\test") == "test"
-    assert util.sanitize_filename("\\../test") == "test"
+    assert util.sanitize_filename("/test") == ""
+    assert util.sanitize_filename("..test") == ""
+    assert util.sanitize_filename("\\test") == ""
+    assert util.sanitize_filename("\\../test") == ""
 
 
 def test_sanitize_path():
     """Test sanitize_path."""
     assert util.sanitize_path("test/path") == "test/path"
-    assert util.sanitize_path("~test/path") == "test/path"
-    assert util.sanitize_path("~/../test/path") == "//test/path"
+    assert util.sanitize_path("~test/path") == ""
+    assert util.sanitize_path("~/../test/path") == ""
 
 
 def test_slugify():
