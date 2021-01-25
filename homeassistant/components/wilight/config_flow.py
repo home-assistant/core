bd9d58ec6a580ada6dcf404cb22a52e4a4d6f999
Leonardo Figueiro <leoagfig@gmail.com>
2021-01-25 19:32:09 -0300

    Update config_flow.py

--- homeassistant/components/wilight/config_flow.py
+++ homeassistant/components/wilight/config_flow.py
@@ -2,12 +2,11 @@
 from urllib.parse import urlparse
 
 import pywilight
+from pywilight.const import DOMAIN  # pylint: disable=unused-import
 
 from homeassistant.components import ssdp
 from homeassistant.config_entries import CONN_CLASS_LOCAL_PUSH, ConfigFlow
 from homeassistant.const import CONF_HOST
-
-from .const import DOMAIN  # pylint: disable=unused-import
 
 CONF_SERIAL_NUMBER = "serial_number"
 CONF_MODEL_NAME = "model_name"
