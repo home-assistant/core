4eab4db2a2c7a5f95279dc67664dbe42748529a9
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-01-26 01:03:12 +0100

    Improve Slack notify component (#45479)
    * Add typing information
    
    * Small improvments
    
    * Use %r for exceptions
    * Added exception handlers for aiohttp.ClientError
    * Added testcase
    
    * Changes after review
    
    * Bugfixes

--- tests/components/slack/test_notify.py
+++ tests/components/slack/test_notify.py
@@ -1,7 +1,77 @@
 """Test slack notifications."""
-from unittest.mock import AsyncMock, Mock
+import logging
+from unittest.mock import AsyncMock, Mock, patch
 
-from homeassistant.components.slack.notify import SlackNotificationService
+from _pytest.logging import LogCaptureFixture
+import aiohttp
+from slack.errors import SlackApiError
+
+from homeassistant.components.slack.notify import (
+    CONF_DEFAULT_CHANNEL,
+    SlackNotificationService,
+    async_get_service,
+)
+from homeassistant.const import CONF_API_KEY, CONF_ICON, CONF_USERNAME
+from homeassistant.helpers.typing import HomeAssistantType
+
+MODULE_PATH = "homeassistant.components.slack.notify"
+
+
+async def test_get_service(hass: HomeAssistantType, caplog: LogCaptureFixture):
+    """Test async_get_service with exceptions."""
+    config = {
+        CONF_API_KEY: "12345",
+        CONF_DEFAULT_CHANNEL: "channel",
+    }
+
+    with patch(MODULE_PATH + ".aiohttp_client") as mock_session, patch(
+        MODULE_PATH + ".WebClient"
+    ) as mock_client, patch(
+        MODULE_PATH + ".SlackNotificationService"
+    ) as mock_slack_service:
+        mock_session.async_get_clientsession.return_value = session = Mock()
+        mock_client.return_value = client = AsyncMock()
+
+        # Normal setup
+        mock_slack_service.return_value = service = Mock()
+        assert await async_get_service(hass, config) == service
+        mock_slack_service.assert_called_once_with(
+            hass, client, "channel", username=None, icon=None
+        )
+        mock_client.assert_called_with(token="12345", run_async=True, session=session)
+        client.auth_test.assert_called_once_with()
+        mock_slack_service.assert_called_once_with(
+            hass, client, "channel", username=None, icon=None
+        )
+        mock_slack_service.reset_mock()
+
+        # aiohttp.ClientError
+        config.update({CONF_USERNAME: "user", CONF_ICON: "icon"})
+        mock_slack_service.reset_mock()
+        mock_slack_service.return_value = service = Mock()
+        client.auth_test.side_effect = [aiohttp.ClientError]
+        assert await async_get_service(hass, config) == service
+        assert len(caplog.records) == 1
+        record = caplog.records[0]
+        assert record.levelno == logging.WARNING
+        assert aiohttp.ClientError.__qualname__ in record.message
+        caplog.records.clear()
+        mock_slack_service.assert_called_once_with(
+            hass, client, "channel", username="user", icon="icon"
+        )
+        mock_slack_service.reset_mock()
+
+        # SlackApiError
+        err, level = SlackApiError("msg", "resp"), logging.ERROR
+        client.auth_test.side_effect = [err]
+        assert await async_get_service(hass, config) is None
+        assert len(caplog.records) == 1
+        record = caplog.records[0]
+        assert record.levelno == level
+        assert err.__class__.__qualname__ in record.message
+        caplog.records.clear()
+        mock_slack_service.assert_not_called()
+        mock_slack_service.reset_mock()
 
 
 async def test_message_includes_default_emoji():
