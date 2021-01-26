712a5a098d3cc787976f67628103adb32cb2d0ad
Paulus Schoutsen <balloob@gmail.com>
2021-01-26 21:45:09 +0100

    Add significant change filtering to Google (#45566)

--- tests/components/google_assistant/test_report_state.py
+++ tests/components/google_assistant/test_report_state.py
@@ -2,6 +2,7 @@
 from unittest.mock import AsyncMock, patch
 
 from homeassistant.components.google_assistant import error, report_state
+from homeassistant.setup import async_setup_component
 from homeassistant.util.dt import utcnow
 
 from . import BASIC_CONFIG
@@ -11,6 +12,7 @@ from tests.common import async_fire_time_changed
 
 async def test_report_state(hass, caplog, legacy_patchable_time):
     """Test report state works."""
+    assert await async_setup_component(hass, "switch", {})
     hass.states.async_set("light.ceiling", "off")
     hass.states.async_set("switch.ac", "on")
 
@@ -44,14 +46,11 @@ async def test_report_state(hass, caplog, legacy_patchable_time):
         "devices": {"states": {"light.kitchen": {"on": True, "online": True}}}
     }
 
-    # Test that state changes that change something that Google doesn't care about
-    # do not trigger a state report.
+    # Test that only significant state changes are reported
     with patch.object(
         BASIC_CONFIG, "async_report_state_all", AsyncMock()
     ) as mock_report:
-        hass.states.async_set(
-            "light.kitchen", "on", {"irrelevant": "should_be_ignored"}
-        )
+        hass.states.async_set("switch.ac", "on", {"something": "else"})
         await hass.async_block_till_done()
 
     assert len(mock_report.mock_calls) == 0
