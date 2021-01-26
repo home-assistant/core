40e57bec585435add444f5cf6b033f5e813c43dc
Leonardo Figueiro <leoagfig@gmail.com>
2021-01-25 22:36:43 -0300

    Update test_cover.py

--- tests/components/wilight/test_cover.py
+++ tests/components/wilight/test_cover.py
@@ -12,6 +12,7 @@ from homeassistant.const import (
     SERVICE_SET_COVER_POSITION,
     STATE_CLOSED,
     STATE_CLOSING,
+    STATE_OPEN,
     STATE_OPENING,
 )
 from homeassistant.helpers.typing import HomeAssistantType
@@ -113,5 +114,5 @@ async def test_open_close_cover_state(
     await hass.async_block_till_done()
     state = hass.states.get("cover.wl000000000099_1")
     assert state
-    assert state.state == STATE_CLOSED
+    assert state.state == STATE_OPEN
     assert state.attributes.get(ATTR_POSITION) == 50
