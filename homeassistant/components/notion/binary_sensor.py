357171cfd40b8c9b06f3d030fa49893eddb73f93
Aaron Bach <bachya1208@gmail.com>
2021-01-27 12:16:29 -0700

    Fix Notion binary sensor bug due to API changes

--- homeassistant/components/notion/binary_sensor.py
+++ homeassistant/components/notion/binary_sensor.py
@@ -16,6 +16,7 @@ from . import NotionEntity
 from .const import (
     DATA_COORDINATOR,
     DOMAIN,
+    LOGGER,
     SENSOR_BATTERY,
     SENSOR_DOOR,
     SENSOR_GARAGE_DOOR,
@@ -81,13 +82,19 @@ class NotionBinarySensor(NotionEntity, BinarySensorEntity):
 
         if "value" in task["status"]:
             self._state = task["status"]["value"]
-        elif task["task_type"] == SENSOR_BATTERY:
-            self._state = task["status"]["data"]["to_state"]
+        elif task["status"].get("insights", {}).get("primary"):
+            self._state = task["status"]["insights"]["primary"]["to_state"]
+        else:
+            LOGGER.warning("Unknown data payload: %s", task["status"])
+            self._state = None
 
     @property
     def is_on(self) -> bool:
         """Return whether the sensor is on or off."""
         task = self.coordinator.data["tasks"][self.task_id]
+
+        if not self._state:
+            return False
 
         if task["task_type"] == SENSOR_BATTERY:
             return self._state == "critical"
