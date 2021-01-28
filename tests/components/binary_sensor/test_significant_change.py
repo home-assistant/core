3222a6e04d3dba0c4b57b1d826203260d1f7c560
Aaron Bach <bachya1208@gmail.com>
2021-01-28 16:30:44 -0700

    Fix tests

--- tests/components/binary_sensor/test_significant_change.py
+++ tests/components/binary_sensor/test_significant_change.py
@@ -6,6 +6,13 @@ from homeassistant.components.binary_sensor.significant_change import (
 
 async def test_significant_change():
     """Detect Binary Sensor significant changes."""
-    attrs = {}
-    assert not async_check_significant_change(None, "on", attrs, "on", attrs)
-    assert async_check_significant_change(None, "on", attrs, "off", attrs)
+    old_attrs = {}
+    new_attrs = {"a": 1}
+
+    assert not async_check_significant_change(None, "on", old_attrs, "on", old_attrs)
+    assert (
+        async_check_significant_change(None, "on", old_attrs, "off", old_attrs) is True
+    )
+    assert (
+        async_check_significant_change(None, "on", old_attrs, "on", new_attrs) is False
+    )
