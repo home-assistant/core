8394089b8f7bccf470962c91b86dc4b023bcb8a5
J. Nick Koston <nick@koston.org>
2021-01-27 18:28:55 -1000

    Update zwave to use new fan entity model

--- tests/components/zwave/test_fan.py
+++ tests/components/zwave/test_fan.py
@@ -39,7 +39,10 @@ def test_fan_turn_on(mock_openzwave):
 
     node.reset_mock()
 
-    device.turn_on(speed=SPEED_OFF)
+    import pprint
+
+    pprint.pprint(device)
+    device.turn_on(percentage=0)
 
     assert node.set_dimmer.called
     value_id, brightness = node.set_dimmer.mock_calls[0][1]
@@ -49,7 +52,7 @@ def test_fan_turn_on(mock_openzwave):
 
     node.reset_mock()
 
-    device.turn_on(speed=SPEED_LOW)
+    device.turn_on(percentage=1)
 
     assert node.set_dimmer.called
     value_id, brightness = node.set_dimmer.mock_calls[0][1]
@@ -59,7 +62,7 @@ def test_fan_turn_on(mock_openzwave):
 
     node.reset_mock()
 
-    device.turn_on(speed=SPEED_MEDIUM)
+    device.turn_on(percentage=50)
 
     assert node.set_dimmer.called
     value_id, brightness = node.set_dimmer.mock_calls[0][1]
@@ -69,7 +72,7 @@ def test_fan_turn_on(mock_openzwave):
 
     node.reset_mock()
 
-    device.turn_on(speed=SPEED_HIGH)
+    device.turn_on(percentage=100)
 
     assert node.set_dimmer.called
     value_id, brightness = node.set_dimmer.mock_calls[0][1]
