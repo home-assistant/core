941a76c994116c310ea1570835af0c2d34dfdc9e
Pascal Reeb <github@schizopriest.de>
2021-01-29 11:05:13 +0100

    Add config flow for nuki (#45664)
    * implemented config_flow for nuki component
    
    * warn -> warning
    
    * exception handling & config_flow tests
    
    * gen_requirements_all
    
    * Update config_flow.py
    
    Co-authored-by: Pascal Vizeli <pascal.vizeli@syshack.ch>

---
+++ tests/components/nuki/test_config_flow.py
@@ -0,0 +1,93 @@
+"""Test the nuki config flow."""
+from unittest.mock import patch
+
+from homeassistant import config_entries, setup
+from homeassistant.components.nuki.config_flow import CannotConnect, InvalidAuth
+from homeassistant.components.nuki.const import DOMAIN
+
+
+async def test_form(hass):
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == "form"
+    assert result["errors"] == {}
+
+    mock_info = {"ids": {"hardwareId": "0001"}}
+
+    with patch(
+        "homeassistant.components.nuki.config_flow.NukiBridge.info",
+        return_value=mock_info,
+    ), patch(
+        "homeassistant.components.nuki.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.nuki.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "host": "1.1.1.1",
+                "port": 8080,
+                "token": "test-token",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] == "create_entry"
+    assert result2["title"] == "0001"
+    assert result2["data"] == {
+        "host": "1.1.1.1",
+        "port": 8080,
+        "token": "test-token",
+    }
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_form_invalid_auth(hass):
+    """Test we handle invalid auth."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.nuki.config_flow.NukiBridge.info",
+        side_effect=InvalidAuth,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "host": "1.1.1.1",
+                "port": 8080,
+                "token": "test-token",
+            },
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "invalid_auth"}
+
+
+async def test_form_cannot_connect(hass):
+    """Test we handle cannot connect error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.nuki.config_flow.NukiBridge.info",
+        side_effect=CannotConnect,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "host": "1.1.1.1",
+                "port": 8080,
+                "token": "test-token",
+            },
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "cannot_connect"}
