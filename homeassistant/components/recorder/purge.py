acede41beee3b4e6c2fb25d29f58ce0464f52c6d
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-01-29 15:15:21 +0100

    Add apply_filter option to recorder.purge service
    * Always delete states, even if event_id is None
    * Remove subsets
    * Set old_state_ids to Null before removing states

--- homeassistant/components/recorder/purge.py
+++ homeassistant/components/recorder/purge.py
@@ -8,21 +8,27 @@ from typing import TYPE_CHECKING
 
 from sqlalchemy.exc import OperationalError, SQLAlchemyError
 from sqlalchemy.orm.session import Session
+from sqlalchemy.sql.expression import distinct
 
+from homeassistant.const import EVENT_STATE_CHANGED
 import homeassistant.util.dt as dt_util
 
 from .const import MAX_ROWS_TO_PURGE
 from .models import Events, RecorderRuns, States
 from .repack import repack_database
-from .util import session_scope
+from .util import execute, session_scope
 
 if TYPE_CHECKING:
     from . import Recorder
 
 _LOGGER = logging.getLogger(__name__)
 
+DEBUG_DATETIME_FMT = r"%Y-%m-%d %H:%M UTC"
 
-def purge_old_data(instance: Recorder, purge_days: int, repack: bool) -> bool:
+
+def purge_old_data(
+    instance: Recorder, purge_days: int, repack: bool, apply_filter: bool = False
+) -> bool:
     """Purge events and states older than purge_days ago.
 
     Cleans up an timeframe of an hour, based on the oldest record.
@@ -45,6 +51,9 @@ def purge_old_data(instance: Recorder, purge_days: int, repack: bool) -> bool:
                 # return false, as we are not done yet.
                 _LOGGER.debug("Purging hasn't fully completed yet")
                 return False
+            if apply_filter:
+                if _purge_filtered_data(instance, session) is False:
+                    return False
             _purge_old_recorder_runs(instance, session, purge_before)
         if repack:
             repack_database(instance)
@@ -140,3 +149,193 @@ def _purge_old_recorder_runs(
         .delete(synchronize_session=False)
     )
     _LOGGER.debug("Deleted %s recorder_runs", deleted_rows)
+
+
+def _purge_filtered_data(
+    instance: Recorder,
+    session: Session,
+    *,
+    batch_size_hours: int = 1,
+) -> bool:
+    """Purge filtered states and events that shouldn't be in database."""
+
+    _LOGGER.debug("Purging filtered states and events")
+    utc_now = dt_util.utcnow()
+    batch_purge_before_states = utc_now
+    batch_purge_before_events = utc_now
+
+    # Check if excluded entity_ids are in database
+    excluded_entity_ids: list[str] = [
+        entity_id
+        for (entity_id,) in session.query(distinct(States.entity_id)).all()
+        if not instance.entity_filter(entity_id)
+    ]
+
+    if len(excluded_entity_ids) != 0:
+        batch_purge_before_states = _purge_filtered_states(
+            session,
+            excluded_entity_ids,
+            batch_purge_before_states,
+            batch_size_hours,
+        )
+
+    # Check if excluded event_types are in database
+    excluded_event_types: list[str] = [
+        event_type
+        for (event_type,) in session.query(distinct(Events.event_type)).all()
+        if event_type in instance.exclude_t
+    ]
+
+    if len(excluded_event_types) != 0:
+        batch_purge_before_events = _purge_filtered_events(
+            session,
+            excluded_event_types,
+            batch_purge_before_events,
+            batch_size_hours,
+        )
+
+    if batch_purge_before_states < utc_now or batch_purge_before_events < utc_now:
+        _LOGGER.debug("Purging filter hasn't fully completed yet")
+        return False
+
+    return True
+
+
+def _purge_filtered_states(
+    session: Session,
+    excluded_entity_ids: list[str],
+    batch_purge_before: datetime,
+    batch_size_hours: int,
+) -> datetime:
+    """Handle purging filtered states."""
+
+    query = (
+        session.query(States)
+        .filter(States.entity_id.in_(excluded_entity_ids))
+        .order_by(States.last_updated.asc())
+        .limit(1)
+    )
+    states = execute(query, to_native=True, validate_entity_ids=False)  # type: ignore
+    if states:
+        batch_purge_before = states[0].last_updated + timedelta(hours=batch_size_hours)
+
+    _LOGGER.debug(
+        "Purging states before %s that should be filtered",
+        batch_purge_before.strftime(DEBUG_DATETIME_FMT),
+    )
+
+    disconnected_rows = (
+        session.query(States)
+        .filter(
+            States.old_state_id.in_(
+                session.query(States.state_id)
+                .filter(States.last_updated < batch_purge_before)
+                .filter(States.entity_id.in_(excluded_entity_ids))
+                .subquery()
+            )
+        )
+        .update({"old_state_id": None}, synchronize_session=False)
+    )
+    _LOGGER.debug("Updated %s states to remove old_state_id", disconnected_rows)
+
+    event_ids: list[int] = [
+        event_id
+        for (event_id,) in session.query(States.event_id)
+        .filter(States.last_updated < batch_purge_before)
+        .filter(States.event_id is not None)
+        .filter(States.entity_id.in_(excluded_entity_ids))
+        .all()
+    ]
+
+    deleted_rows_states = (
+        session.query(States)
+        .filter(States.last_updated < batch_purge_before)
+        .filter(States.entity_id.in_(excluded_entity_ids))
+        .delete(synchronize_session=False)
+    )
+    _LOGGER.debug(
+        "Deleted %s states because entity_id is excluded", deleted_rows_states
+    )
+
+    if event_ids:
+        deleted_rows_events = (
+            session.query(Events)
+            .filter(Events.event_type == EVENT_STATE_CHANGED)
+            .filter(Events.time_fired < batch_purge_before)
+            .filter(Events.event_id.in_(event_ids))
+            .delete(synchronize_session=False)
+        )
+        _LOGGER.debug(
+            "Deleted %s events because entity_id is excluded", deleted_rows_events
+        )
+
+    return batch_purge_before
+
+
+def _purge_filtered_events(
+    session: Session,
+    excluded_event_types: list[str],
+    batch_purge_before: datetime,
+    batch_size_hours: int,
+) -> datetime:
+    """Handle purging filtered events."""
+
+    query = (
+        session.query(Events)
+        .filter(Events.event_type.in_(excluded_event_types))
+        .order_by(Events.time_fired.asc())
+        .limit(1)
+    )
+    events = execute(query, to_native=True, validate_entity_ids=False)  # type: ignore
+    if events:
+        batch_purge_before = events[0].time_fired + timedelta(hours=batch_size_hours)
+
+    _LOGGER.debug(
+        "Purging events before %s the should be filtered",
+        batch_purge_before.strftime(DEBUG_DATETIME_FMT),
+    )
+
+    event_ids: list[int] = [
+        event_id
+        for (event_id,) in session.query(Events.event_id)
+        .filter(Events.time_fired < batch_purge_before)
+        .join(States)
+        .filter(Events.event_type.in_(excluded_event_types))
+        .all()
+    ]
+
+    if event_ids:
+        disconnected_rows = (
+            session.query(States)
+            .filter(
+                States.old_state_id.in_(
+                    session.query(States.state_id)
+                    .filter(States.last_updated < batch_purge_before)
+                    .filter(States.event_id.in_(event_ids))
+                )
+            )
+            .update({"old_state_id": None}, synchronize_session=False)
+        )
+
+        deleted_rows_states = (
+            session.query(States)
+            .filter(States.last_updated < batch_purge_before)
+            .filter(States.event_id.in_(event_ids))
+            .delete(synchronize_session=False)
+        )
+        _LOGGER.debug("Updated %s states to remove old_state_id", disconnected_rows)
+        _LOGGER.debug(
+            "Deleted %s states because event_type is excluded", deleted_rows_states
+        )
+
+    deleted_rows_events = (
+        session.query(Events)
+        .filter(Events.time_fired < batch_purge_before)
+        .filter(Events.event_type.in_(excluded_event_types))
+        .delete(synchronize_session=False)
+    )
+    _LOGGER.debug(
+        "Deleted %s events because event_type is excluded", deleted_rows_events
+    )
+
+    return batch_purge_before
