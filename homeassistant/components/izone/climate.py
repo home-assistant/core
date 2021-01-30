69c994df67b388fbad5ce92ee0a8ed05935514ed
Nick Adams <4012017+Nick-Adams-AU@users.noreply.github.com>
2021-01-30 10:27:03 +1000

    Add range check to airflow input

--- homeassistant/components/izone/climate.py
+++ homeassistant/components/izone/climate.py
@@ -66,7 +66,9 @@ IZONE_SERVICE_AIRFLOW_MAX = "airflow_max"
 IZONE_SERVICE_AIRFLOW_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_ENTITY_ID): cv.entity_id,
-        vol.Required(ATTR_AIRFLOW): cv.positive_int,
+        vol.Required(ATTR_AIRFLOW): vol.All(
+            vol.Coerce(int), vol.Range(min=0, max=100), msg="invalid airflow"
+        ),
     }
 )
 
