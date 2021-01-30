42c76a1590b6460cb732c525f9104fda415d5be1
Timmo <contact@timmo.xyz>
2021-01-30 02:57:02 +0000

    Add timeout to initial check

--- homeassistant/components/lyric/__init__.py
+++ homeassistant/components/lyric/__init__.py
@@ -86,11 +86,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     client_id = hass.data[DOMAIN][CONF_CLIENT_ID]
     lyric: Lyric = Lyric(client, client_id)
 
-    try:
-        await lyric.get_locations()
-    except LYRIC_EXCEPTIONS as exception:
-        _LOGGER.warning(exception)
-        raise ConfigEntryNotReady from exception
+    async with async_timeout.timeout(60):
+        try:
+            await lyric.get_locations()
+        except LYRIC_EXCEPTIONS as exception:
+            _LOGGER.warning(exception)
+            raise ConfigEntryNotReady from exception
 
     async def async_update_data() -> Lyric:
         """Fetch data from Lyric."""
