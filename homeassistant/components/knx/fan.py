eaaf352d53b16a72fded91ae35a2e4745058d2be
crazyfx1 <pum.m@gmx.at>
2021-01-30 10:13:06 +0000

    Fan changes

--- homeassistant/components/knx/fan.py
+++ homeassistant/components/knx/fan.py
@@ -5,10 +5,6 @@ from typing import TYPE_CHECKING, Any, Iterator, Optional
 
 from homeassistant.components.fan import (
     FanEntity,
-    SPEED_OFF,
-    SPEED_LOW,
-    SPEED_MEDIUM,
-    SPEED_HIGH,
     SUPPORT_SET_SPEED,
 )
 
@@ -32,21 +28,15 @@ class KNXFan(KnxEntity, FanEntity):
     def __init__(self, device: XknxFan):
         """Initialize of KNX fan."""
         super().__init__(device)
-        # TODO: REMOVE
-        self._device = device
-        self._ha_to_knx_value = self._device.speed_mapping
-        self._knx_to_ha_value = {v: k for k, v in self._ha_to_knx_value.items()}
 
     @property
     def speed(self) -> Optional[str]:
         """Return the current speed."""
-        if self._device.current_speed in self._knx_to_ha_value:
-            return self._knx_to_ha_value[self._device.current_speed]
-        return None
+        return self._device.current_speed
 
     async def async_set_speed(self, speed: str):
         # await self._device.set_speed(self._ha_to_knx_value[speed])
-        await self._device.set_speed(2)
+        await self._device.set_speed(speed)
 
     @property
     def supported_features(self) -> int:
