291da037e2c39b2ac2f72b90be90c5ef0a59565e
Pascal Reeb <pascal@reeb.io>
2021-01-30 17:40:57 +0100

    added async_unload_entry

--- homeassistant/components/nuki/__init__.py
+++ homeassistant/components/nuki/__init__.py
@@ -1,5 +1,6 @@
 """The nuki component."""
 
+import asyncio
 from datetime import timedelta
 import logging
 
@@ -130,6 +131,22 @@ async def async_setup_entry(hass, entry):
         )
 
     return True
+
+
+async def async_unload_entry(hass, entry):
+    """Unload the Nuki entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, platform)
+                for platform in NUKI_PLATFORMS
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
 
 
 class NukiEntity(CoordinatorEntity):
