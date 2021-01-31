46fb1d54a4a60413a4cb6fd82422daa6793f3305
Brandon Rothweiler <brandonrothweiler@gmail.com>
2021-01-31 00:50:42 -0500

    Increase test coverage

--- tests/components/mazda/test_config_flow.py
+++ tests/components/mazda/test_config_flow.py
@@ -4,7 +4,10 @@ from unittest.mock import patch
 import aiohttp
 
 from homeassistant import config_entries, data_entry_flow, setup
-from homeassistant.components.mazda.config_flow import MazdaAuthenticationException
+from homeassistant.components.mazda.config_flow import (
+    MazdaAccountLockedException,
+    MazdaAuthenticationException,
+)
 from homeassistant.components.mazda.const import DOMAIN
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, CONF_REGION
 from homeassistant.core import HomeAssistant
@@ -79,6 +82,31 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
     assert result2["errors"] == {"base": "invalid_auth"}
 
 
+async def test_form_account_locked(hass: HomeAssistant) -> None:
+    """Test we handle account locked error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "user"
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.mazda.config_flow.MazdaAPI.validate_credentials",
+        side_effect=MazdaAccountLockedException("Account locked"),
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            FIXTURE_USER_INPUT,
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result2["step_id"] == "user"
+    assert result2["errors"] == {"base": "account_locked"}
+
+
 async def test_form_cannot_connect(hass):
     """Test we handle cannot connect error."""
     result = await hass.config_entries.flow.async_init(
@@ -98,6 +126,25 @@ async def test_form_cannot_connect(hass):
     assert result2["errors"] == {"base": "cannot_connect"}
 
 
+async def test_form_unknown_error(hass):
+    """Test we handle unknown error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.mazda.config_flow.MazdaAPI.validate_credentials",
+        side_effect=Exception,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            FIXTURE_USER_INPUT,
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "unknown"}
+
+
 async def test_reauth_flow(hass: HomeAssistant) -> None:
     """Test reauth works."""
     await setup.async_setup_component(hass, "persistent_notification", {})
@@ -112,6 +159,9 @@ async def test_reauth_flow(hass: HomeAssistant) -> None:
             data=FIXTURE_USER_INPUT,
         )
         mock_config.add_to_hass(hass)
+
+        await hass.config_entries.async_setup(mock_config.entry_id)
+        await hass.async_block_till_done()
 
         result = await hass.config_entries.flow.async_init(
             DOMAIN, context={"source": "reauth"}, data=FIXTURE_USER_INPUT
@@ -160,6 +210,30 @@ async def test_reauth_authorization_error(hass: HomeAssistant) -> None:
         assert result2["errors"] == {"base": "invalid_auth"}
 
 
+async def test_reauth_account_locked(hass: HomeAssistant) -> None:
+    """Test we show user form on account_locked error."""
+    with patch(
+        "homeassistant.components.mazda.config_flow.MazdaAPI.validate_credentials",
+        side_effect=MazdaAccountLockedException("Account locked"),
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": "reauth"}, data=FIXTURE_USER_INPUT
+        )
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["step_id"] == "reauth"
+
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            FIXTURE_USER_INPUT_REAUTH,
+        )
+        await hass.async_block_till_done()
+
+        assert result2["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result2["step_id"] == "reauth"
+        assert result2["errors"] == {"base": "account_locked"}
+
+
 async def test_reauth_connection_error(hass: HomeAssistant) -> None:
     """Test we show user form on connection error."""
     with patch(
@@ -182,3 +256,27 @@ async def test_reauth_connection_error(hass: HomeAssistant) -> None:
         assert result2["type"] == data_entry_flow.RESULT_TYPE_FORM
         assert result2["step_id"] == "reauth"
         assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_reauth_unknown_error(hass: HomeAssistant) -> None:
+    """Test we show user form on unknown error."""
+    with patch(
+        "homeassistant.components.mazda.config_flow.MazdaAPI.validate_credentials",
+        side_effect=Exception,
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": "reauth"}, data=FIXTURE_USER_INPUT
+        )
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["step_id"] == "reauth"
+
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            FIXTURE_USER_INPUT_REAUTH,
+        )
+        await hass.async_block_till_done()
+
+        assert result2["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result2["step_id"] == "reauth"
+        assert result2["errors"] == {"base": "unknown"}
