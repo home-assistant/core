9024f49bf94e28ffb819e49940673a0a3d6ab2b3
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2021-01-31 11:57:19 -0500

    Fix whitespace issue

--- homeassistant/components/hue/bridge.py
+++ homeassistant/components/hue/bridge.py
@@ -32,9 +32,9 @@ ATTR_SCENE_NAME = "scene_name"
 ATTR_TRANSITION = "transition"
 SCENE_SCHEMA = vol.Schema(
     {
-         vol.Required(ATTR_GROUP_NAME): cv.string,
-         vol.Required(ATTR_SCENE_NAME): cv.string,
-         vol.Optional(ATTR_TRANSITION, default=DEFAULT_SCENE_TRANSITION): cv.positive_int
+        vol.Required(ATTR_GROUP_NAME): cv.string,
+        vol.Required(ATTR_SCENE_NAME): cv.string,
+        vol.Optional(ATTR_TRANSITION, default=DEFAULT_SCENE_TRANSITION): cv.positive_int
     }
 )
 # How long should we sleep if the hub is busy
