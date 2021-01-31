53c24284b16c08882941fc2dce9f32bb659f3057
J. Nick Koston <nick@koston.org>
2021-01-31 07:27:05 -1000

    Revert "adj"
    This reverts commit de2bc62dc869a5e592d70c6f140e04ec2347597d.

--- tests/components/homekit/test_get_accessories.py
+++ tests/components/homekit/test_get_accessories.py
@@ -238,10 +238,10 @@ def test_type_sensors(type_name, entity_id, state, attrs):
         ("Switch", "script.test", "on", {}, {}),
         ("Switch", "switch.test", "on", {}, {}),
         ("Switch", "switch.test", "on", {}, {CONF_TYPE: TYPE_SWITCH}),
-        ("Faucet", "switch.test", "on", {}, {CONF_TYPE: TYPE_FAUCET}),
+        ("Valve", "switch.test", "on", {}, {CONF_TYPE: TYPE_FAUCET}),
         ("Valve", "switch.test", "on", {}, {CONF_TYPE: TYPE_VALVE}),
-        ("ShowerHead", "switch.test", "on", {}, {CONF_TYPE: TYPE_SHOWER}),
-        ("Sprinkler", "switch.test", "on", {}, {CONF_TYPE: TYPE_SPRINKLER}),
+        ("Valve", "switch.test", "on", {}, {CONF_TYPE: TYPE_SHOWER}),
+        ("Valve", "switch.test", "on", {}, {CONF_TYPE: TYPE_SPRINKLER}),
     ],
 )
 def test_type_switches(type_name, entity_id, state, attrs, config):
