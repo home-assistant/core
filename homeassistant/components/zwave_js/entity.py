548d32b92cf76177af6e664bbf4346baafb71aaf
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-02-02 19:57:08 +0100

    Guard for missing value (#45867)
    * guard for missing value
    
    * update comment

--- homeassistant/components/zwave_js/entity.py
+++ homeassistant/components/zwave_js/entity.py
@@ -92,7 +92,13 @@ class ZWaveBaseEntity(Entity):
     @property
     def available(self) -> bool:
         """Return entity availability."""
-        return self.client.connected and bool(self.info.node.ready)
+        return (
+            self.client.connected
+            and bool(self.info.node.ready)
+            # a None value indicates something wrong with the device,
+            # or the value is simply not yet there (it will arrive later).
+            and self.info.primary_value.value is not None
+        )
 
     @callback
     def _value_changed(self, event_data: dict) -> None:
