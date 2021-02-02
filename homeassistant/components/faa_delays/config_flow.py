93f7c0ae2284bfbf18dfb00fb276b30ea3d38be1
Nathan Tilley <nathan@tilley.xyz>
2021-02-01 21:06:26 -0500

    Change config flow order

--- homeassistant/components/faa_delays/config_flow.py
+++ homeassistant/components/faa_delays/config_flow.py
@@ -25,39 +25,37 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         errors = {}
-        if not user_input:
-            return self.async_show_form(
-                step_id="user", data_schema=DATA_SCHEMA, errors=errors
-            )
+        if user_input is not None:
 
-        await self.async_set_unique_id(user_input[CONF_ID])
-        self._abort_if_unique_id_configured()
+            await self.async_set_unique_id(user_input[CONF_ID])
+            self._abort_if_unique_id_configured()
 
-        websession = aiohttp_client.async_get_clientsession(self.hass)
+            websession = aiohttp_client.async_get_clientsession(self.hass)
 
-        try:
-            data = faadelays.Airport(user_input[CONF_ID], websession)
-            await data.update()
-            _LOGGER.debug(
-                "Creating entry with id: %s, name: %s", user_input[CONF_ID], data.name
-            )
-        except faadelays.InvalidAirport:
-            _LOGGER.error("Airport code %s is invalid", user_input[CONF_ID])
-            errors[CONF_ID] = "invalid_airport"
-            return self.async_show_form(
-                step_id="user", data_schema=DATA_SCHEMA, errors=errors
-            )
-        except ClientConnectionError:
-            _LOGGER.error("Error connecting to FAA API")
-            errors["base"] = "cannot_connect"
-            return self.async_show_form(
-                step_id="user", data_schema=DATA_SCHEMA, errors=errors
-            )
-        except Exception as error:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception: %s", error)
-            errors["base"] = "unknown"
-            return self.async_show_form(
-                step_id="user", data_schema=DATA_SCHEMA, errors=errors
-            )
+            try:
+                data = faadelays.Airport(user_input[CONF_ID], websession)
+                await data.update()
+                _LOGGER.debug(
+                    "Creating entry with id: %s, name: %s",
+                    user_input[CONF_ID],
+                    data.name,
+                )
 
-        return self.async_create_entry(title=data.name, data=user_input)
+            except faadelays.InvalidAirport:
+                _LOGGER.error("Airport code %s is invalid", user_input[CONF_ID])
+                errors[CONF_ID] = "invalid_airport"
+
+            except ClientConnectionError:
+                _LOGGER.error("Error connecting to FAA API")
+                errors["base"] = "cannot_connect"
+
+            except Exception as error:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception: %s", error)
+                errors["base"] = "unknown"
+
+            if not errors:
+                return self.async_create_entry(title=data.name, data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
