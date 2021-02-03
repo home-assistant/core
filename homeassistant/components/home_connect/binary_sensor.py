9662a323f7f700a90a1b6100aad3a7b9e755ddd1
badguy99 <61918526+badguy99@users.noreply.github.com>
2021-02-03 16:05:20 +0000

    Homeconnect remote states (#45610)
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/home_connect/binary_sensor.py
+++ homeassistant/components/home_connect/binary_sensor.py
@@ -3,7 +3,12 @@ import logging
 
 from homeassistant.components.binary_sensor import BinarySensorEntity
 
-from .const import BSH_DOOR_STATE, DOMAIN
+from .const import (
+    BSH_DOOR_STATE,
+    BSH_REMOTE_CONTROL_ACTIVATION_STATE,
+    BSH_REMOTE_START_ALLOWANCE_STATE,
+    DOMAIN,
+)
 from .entity import HomeConnectEntity
 
 _LOGGER = logging.getLogger(__name__)
@@ -26,11 +31,27 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 class HomeConnectBinarySensor(HomeConnectEntity, BinarySensorEntity):
     """Binary sensor for Home Connect."""
 
-    def __init__(self, device, desc, device_class):
+    def __init__(self, device, desc, sensor_type, device_class=None):
         """Initialize the entity."""
         super().__init__(device, desc)
-        self._device_class = device_class
         self._state = None
+        self._device_class = device_class
+        self._type = sensor_type
+        if self._type == "door":
+            self._update_key = BSH_DOOR_STATE
+            self._false_value_list = (
+                "BSH.Common.EnumType.DoorState.Closed",
+                "BSH.Common.EnumType.DoorState.Locked",
+            )
+            self._true_value_list = ["BSH.Common.EnumType.DoorState.Open"]
+        elif self._type == "remote_control":
+            self._update_key = BSH_REMOTE_CONTROL_ACTIVATION_STATE
+            self._false_value_list = [False]
+            self._true_value_list = [True]
+        elif self._type == "remote_start":
+            self._update_key = BSH_REMOTE_START_ALLOWANCE_STATE
+            self._false_value_list = [False]
+            self._true_value_list = [True]
 
     @property
     def is_on(self):
@@ -44,18 +65,17 @@ class HomeConnectBinarySensor(HomeConnectEntity, BinarySensorEntity):
 
     async def async_update(self):
         """Update the binary sensor's status."""
-        state = self.device.appliance.status.get(BSH_DOOR_STATE, {})
+        state = self.device.appliance.status.get(self._update_key, {})
         if not state:
             self._state = None
-        elif state.get("value") in [
-            "BSH.Common.EnumType.DoorState.Closed",
-            "BSH.Common.EnumType.DoorState.Locked",
-        ]:
+        elif state.get("value") in self._false_value_list:
             self._state = False
-        elif state.get("value") == "BSH.Common.EnumType.DoorState.Open":
+        elif state.get("value") in self._true_value_list:
             self._state = True
         else:
-            _LOGGER.warning("Unexpected value for HomeConnect door state: %s", state)
+            _LOGGER.warning(
+                "Unexpected value for HomeConnect %s state: %s", self._type, state
+            )
             self._state = None
         _LOGGER.debug("Updated, new state: %s", self._state)
 
--- homeassistant/components/home_connect/const.py
+++ homeassistant/components/home_connect/const.py
@@ -11,6 +11,8 @@ BSH_POWER_OFF = "BSH.Common.EnumType.PowerState.Off"
 BSH_POWER_STANDBY = "BSH.Common.EnumType.PowerState.Standby"
 BSH_ACTIVE_PROGRAM = "BSH.Common.Root.ActiveProgram"
 BSH_OPERATION_STATE = "BSH.Common.Status.OperationState"
+BSH_REMOTE_CONTROL_ACTIVATION_STATE = "BSH.Common.Status.RemoteControlActive"
+BSH_REMOTE_START_ALLOWANCE_STATE = "BSH.Common.Status.RemoteControlStartAllowed"
 
 COOKING_LIGHTING = "Cooking.Common.Setting.Lighting"
 COOKING_LIGHTING_BRIGHTNESS = "Cooking.Common.Setting.LightingBrightness"
