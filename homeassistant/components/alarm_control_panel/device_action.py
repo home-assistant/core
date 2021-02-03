f3fe2ff94c78ea805c190d9e30da58f20ee5d373
posixx <2280400+posixx@users.noreply.github.com>
2021-02-03 10:26:30 +0100

    Revert "Use constant as supposed"
    This reverts commit bb238a7737c603cb8609e7af520946ea6b1fba06.

--- homeassistant/components/alarm_control_panel/device_action.py
+++ homeassistant/components/alarm_control_panel/device_action.py
@@ -6,7 +6,6 @@ import voluptuous as vol
 from homeassistant.const import (
     ATTR_CODE,
     ATTR_ENTITY_ID,
-    ATTR_SUPPORTED_FEATURES,
     CONF_CODE,
     CONF_DEVICE_ID,
     CONF_DOMAIN,
@@ -66,7 +65,7 @@ async def async_get_actions(hass: HomeAssistant, device_id: str) -> List[dict]:
         if state is None:
             continue
 
-        supported_features = state.attributes[ATTR_SUPPORTED_FEATURES]
+        supported_features = state.attributes["supported_features"]
 
         # Add actions for each entity that belongs to this integration
         if supported_features & SUPPORT_ALARM_ARM_AWAY:
