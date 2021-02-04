83db73a2d418db9bc7d505b3b7f9342316a6c7ab
J. Nick Koston <nick@koston.org>
2021-02-03 21:31:43 -1000

    Add support for preset modes to fans

--- homeassistant/components/homekit/type_fans.py
+++ homeassistant/components/homekit/type_fans.py
@@ -7,13 +7,15 @@ from homeassistant.components.fan import (
     ATTR_DIRECTION,
     ATTR_OSCILLATING,
     ATTR_PERCENTAGE,
-    ATTR_PERCENTAGE_STEP,
+    ATTR_PRESET_MODE,
+    ATTR_PRESET_MODES,
     DIRECTION_FORWARD,
     DIRECTION_REVERSE,
     DOMAIN,
     SERVICE_OSCILLATE,
     SERVICE_SET_DIRECTION,
     SERVICE_SET_PERCENTAGE,
+    SERVICE_SET_PRESET_MODE,
     SUPPORT_DIRECTION,
     SUPPORT_OSCILLATE,
     SUPPORT_SET_SPEED,
@@ -31,11 +33,14 @@ from homeassistant.core import callback
 from .accessories import TYPES, HomeAccessory
 from .const import (
     CHAR_ACTIVE,
+    CHAR_NAME,
+    CHAR_ON,
     CHAR_ROTATION_DIRECTION,
     CHAR_ROTATION_SPEED,
     CHAR_SWING_MODE,
-    PROP_MIN_STEP,
+    CHAR_TARGET_FAN_STATE,
     SERV_FANV2,
+    SERV_SWITCH,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -64,11 +69,14 @@ class Fan(HomeAccessory):
             chars.append(CHAR_ROTATION_SPEED)
 
         serv_fan = self.add_preload_service(SERV_FANV2, chars)
+        self.set_primary_service(serv_fan)
         self.char_active = serv_fan.configure_char(CHAR_ACTIVE, value=0)
 
         self.char_direction = None
         self.char_speed = None
         self.char_swing = None
+        self.char_target_state = None
+        self.preset_mode_chars = []
 
         if CHAR_ROTATION_DIRECTION in chars:
             self.char_direction = serv_fan.configure_char(
@@ -79,12 +87,23 @@ class Fan(HomeAccessory):
             # Initial value is set to 100 because 0 is a special value (off). 100 is
             # an arbitrary non-zero value. It is updated immediately by async_update_state
             # to set to the correct initial value.
-            percentage_step = state.attributes.get(ATTR_PERCENTAGE_STEP, 1)
-            self.char_speed = serv_fan.configure_char(
-                CHAR_ROTATION_SPEED,
-                value=100,
-                properties={PROP_MIN_STEP: percentage_step},
-            )
+            self.char_speed = serv_fan.configure_char(CHAR_ROTATION_SPEED, value=100)
+
+            preset_modes = state.attributes.get(ATTR_PRESET_MODES, [])
+            for preset_mode in preset_modes:
+                preset_serv = self.add_preload_service(SERV_SWITCH, CHAR_NAME)
+                preset_serv.configure_char(
+                    CHAR_NAME, value=f"{self.display_name} {preset_mode}"
+                )
+                self.preset_mode_chars[preset_mode] = preset_serv.configure_char(
+                    CHAR_ON,
+                    value=False,
+                    setter_callback=lambda value: self.set_preset_mode(
+                        preset_mode, value
+                    ),
+                )
+            if preset_modes:
+                self.char_target_state = serv_fan.configure_char(CHAR_TARGET_FAN_STATE)
 
         if CHAR_SWING_MODE in chars:
             self.char_swing = serv_fan.configure_char(CHAR_SWING_MODE, value=0)
@@ -120,32 +139,44 @@ class Fan(HomeAccessory):
         if CHAR_ROTATION_SPEED in char_values:
             self.set_percentage(char_values[CHAR_ROTATION_SPEED])
 
+    def set_preset_mode(self, preset_mode, value):
+        """Set preset_mode if call came from HomeKit."""
+        _LOGGER.debug(
+            "%s: Set preset_mode %s to %d", self.entity_id, preset_mode, value
+        )
+        params = {ATTR_ENTITY_ID: self.entity_id}
+        if value:
+            params[ATTR_PRESET_MODE] = preset_mode
+            self.call_service(DOMAIN, SERVICE_SET_PRESET_MODE, params)
+        else:
+            self.call_service(DOMAIN, SERVICE_TURN_ON, params)
+
     def set_state(self, value):
         """Set state if call came from HomeKit."""
         _LOGGER.debug("%s: Set state to %d", self.entity_id, value)
         service = SERVICE_TURN_ON if value == 1 else SERVICE_TURN_OFF
         params = {ATTR_ENTITY_ID: self.entity_id}
-        self.async_call_service(DOMAIN, service, params)
+        self.call_service(DOMAIN, service, params)
 
     def set_direction(self, value):
         """Set state if call came from HomeKit."""
         _LOGGER.debug("%s: Set direction to %d", self.entity_id, value)
         direction = DIRECTION_REVERSE if value == 1 else DIRECTION_FORWARD
         params = {ATTR_ENTITY_ID: self.entity_id, ATTR_DIRECTION: direction}
-        self.async_call_service(DOMAIN, SERVICE_SET_DIRECTION, params, direction)
+        self.call_service(DOMAIN, SERVICE_SET_DIRECTION, params, direction)
 
     def set_oscillating(self, value):
         """Set state if call came from HomeKit."""
         _LOGGER.debug("%s: Set oscillating to %d", self.entity_id, value)
         oscillating = value == 1
         params = {ATTR_ENTITY_ID: self.entity_id, ATTR_OSCILLATING: oscillating}
-        self.async_call_service(DOMAIN, SERVICE_OSCILLATE, params, oscillating)
+        self.call_service(DOMAIN, SERVICE_OSCILLATE, params, oscillating)
 
     def set_percentage(self, value):
         """Set state if call came from HomeKit."""
         _LOGGER.debug("%s: Set speed to %d", self.entity_id, value)
         params = {ATTR_ENTITY_ID: self.entity_id, ATTR_PERCENTAGE: value}
-        self.async_call_service(DOMAIN, SERVICE_SET_PERCENTAGE, params, value)
+        self.call_service(DOMAIN, SERVICE_SET_PERCENTAGE, params, value)
 
     @callback
     def async_update_state(self, new_state):
@@ -193,3 +224,18 @@ class Fan(HomeAccessory):
                 hk_oscillating = 1 if oscillating else 0
                 if self.char_swing.value != hk_oscillating:
                     self.char_swing.set_value(hk_oscillating)
+
+        char_target_state_hk_value = 0
+        current_preset_mode = new_state.attributes.get(ATTR_PRESET_MODE)
+        for preset_mode, char in self.preset_mode_chars.items():
+            if preset_mode == current_preset_mode:
+                hk_value = 1
+                char_target_state_hk_value = 1
+            else:
+                hk_value = 0
+            if char.value != hk_value:
+                char.set_value(hk_value)
+
+        if self.char_target_state:
+            if self.char_target_state.value != char_target_state_hk_value:
+                self.char_target_state.set_value(char_target_state_hk_value)
