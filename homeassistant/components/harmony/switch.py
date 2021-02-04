964cdb1ddec1c0d81e150ba1c99607423b9825ee
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-02-04 14:16:09 +0100

    Fix entities device_info property in Harmony integration (#45964)

--- homeassistant/components/harmony/switch.py
+++ homeassistant/components/harmony/switch.py
@@ -53,6 +53,11 @@ class HarmonyActivitySwitch(ConnectionStateMixin, SwitchEntity):
         return self._data.device_info(DOMAIN)
 
     @property
+    def device_info(self):
+        """Return device info."""
+        return self._data.device_info(DOMAIN)
+
+    @property
     def is_on(self):
         """Return if the current activity is the one for this switch."""
         _, activity_name = self._data.current_activity
