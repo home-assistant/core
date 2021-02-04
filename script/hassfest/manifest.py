06e6005fbba2420f708bcf4046e21dbcb6dcd8fc
Joakim Sørensen <joasoe@gmail.com>
2021-02-04 09:59:41 +0100

    Add warning to custom integrations without version (#45919)
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- script/hassfest/manifest.py
+++ script/hassfest/manifest.py
@@ -2,10 +2,10 @@
 from typing import Dict
 from urllib.parse import urlparse
 
-from awesomeversion import AwesomeVersion
-from awesomeversion.strategy import AwesomeVersionStrategy
 import voluptuous as vol
 from voluptuous.humanize import humanize_error
+
+from homeassistant.loader import validate_custom_integration_version
 
 from .model import Integration
 
@@ -53,16 +53,9 @@ def verify_uppercase(value: str):
 
 def verify_version(value: str):
     """Verify the version."""
-    version = AwesomeVersion(value)
-    if version.strategy not in [
-        AwesomeVersionStrategy.CALVER,
-        AwesomeVersionStrategy.SEMVER,
-        AwesomeVersionStrategy.SIMPLEVER,
-        AwesomeVersionStrategy.BUILDVER,
-        AwesomeVersionStrategy.PEP440,
-    ]:
+    if not validate_custom_integration_version(value):
         raise vol.Invalid(
-            f"'{version}' is not a valid version. This will cause a future version of Home Assistant to block this integration.",
+            f"'{value}' is not a valid version. This will cause a future version of Home Assistant to block this integration.",
         )
     return value
 
