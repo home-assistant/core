5792b91e9824ad8d24455e7085d10f5d80920693
Nathan Spencer <natekspencer@gmail.com>
2021-02-05 02:48:47 -0700

    Fix zwave_js cover control for Up/Down and Open/Close (#45965)
    * Fix issue with control of cover when the target value is Up/Down instead of Open/Close
    
    * Adjust open/close/stop cover control to account for no Open/Up or Close/Down targets
    
    * Revert back to using values of 0/99 to close/open a cover since it is supported by all covers
    
    * Replace RELEASE_BUTTON with False and remove unused PRESS_BUTTON in zwave_js cover

--- tests/components/zwave_js/test_cover.py
+++ tests/components/zwave_js/test_cover.py
@@ -95,14 +95,16 @@ async def test_cover(hass, client, chain_actuator_zws12, integration):
         "commandClassName": "Multilevel Switch",
         "commandClass": 38,
         "endpoint": 0,
-        "property": "Open",
-        "propertyName": "Open",
+        "property": "targetValue",
+        "propertyName": "targetValue",
         "metadata": {
-            "type": "boolean",
+            "label": "Target value",
+            "max": 99,
+            "min": 0,
+            "type": "number",
             "readable": True,
             "writeable": True,
-            "label": "Perform a level change (Open)",
-            "ccSpecific": {"switchType": 3},
+            "label": "Target value",
         },
     }
     assert args["value"]
@@ -194,17 +196,19 @@ async def test_cover(hass, client, chain_actuator_zws12, integration):
         "commandClassName": "Multilevel Switch",
         "commandClass": 38,
         "endpoint": 0,
-        "property": "Close",
-        "propertyName": "Close",
+        "property": "targetValue",
+        "propertyName": "targetValue",
         "metadata": {
-            "type": "boolean",
+            "label": "Target value",
+            "max": 99,
+            "min": 0,
+            "type": "number",
             "readable": True,
             "writeable": True,
-            "label": "Perform a level change (Close)",
-            "ccSpecific": {"switchType": 3},
+            "label": "Target value",
         },
     }
-    assert args["value"]
+    assert args["value"] == 0
 
     client.async_send_command.reset_mock()
 
