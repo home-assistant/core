618fcda8211a66d0202c1c804839a1a5a12c7400
Robert Svensson <Kane610@users.noreply.github.com>
2021-02-06 21:32:18 +0100

    Simplify UniFi entry configuration data (#45759)
    * Simplify configuration structure by removing the controller key
    
    * Fix flake8
    
    * Fix review comments
    
    * Don't use migrate_entry mechanism to flatten configuration
    Keep legacy configuration when creating new entries as well

--- homeassistant/components/unifi/__init__.py
+++ homeassistant/components/unifi/__init__.py
@@ -5,6 +5,7 @@ from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
 
 from .const import (
     ATTR_MANUFACTURER,
+    CONF_CONTROLLER,
     DOMAIN as UNIFI_DOMAIN,
     LOGGER,
     UNIFI_WIRELESS_CLIENTS,
@@ -28,10 +29,14 @@ async def async_setup_entry(hass, config_entry):
     """Set up the UniFi component."""
     hass.data.setdefault(UNIFI_DOMAIN, {})
 
+    # Flat configuration was introduced with 2021.3
+    await async_flatten_entry_data(hass, config_entry)
+
     controller = UniFiController(hass, config_entry)
     if not await controller.async_setup():
         return False
 
+    # Unique ID was introduced with 2021.3
     if config_entry.unique_id is None:
         hass.config_entries.async_update_entry(
             config_entry, unique_id=controller.site_id
@@ -62,6 +67,17 @@ async def async_unload_entry(hass, config_entry):
     """Unload a config entry."""
     controller = hass.data[UNIFI_DOMAIN].pop(config_entry.entry_id)
     return await controller.async_reset()
+
+
+async def async_flatten_entry_data(hass, config_entry):
+    """Simpler configuration structure for entry data.
+
+    Keep controller key layer in case user rollbacks.
+    """
+
+    data: dict = {**config_entry.data, **config_entry.data[CONF_CONTROLLER]}
+    if config_entry.data != data:
+        hass.config_entries.async_update_entry(config_entry, data=data)
 
 
 class UnifiWirelessClients:
