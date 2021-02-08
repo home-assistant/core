0604902092dbbcb4811dfa7d54514f4fb164faa2
Paulus Schoutsen <balloob@gmail.com>
2021-02-08 10:45:46 +0100

    Mark entities as unavailable when they are removed but are still registered (#45528)
    * Mark entities as unavailable when they are removed but are still registered
    
    * Add sync_entity_lifecycle to collection helper
    
    * Remove debug print
    
    * Lint
    
    * Fix tests
    
    * Fix tests
    
    * Update zha
    
    * Update zone
    
    * Fix tests
    
    * Update hyperion
    
    * Update rfxtrx
    
    * Fix tests
    
    * Pass force_remove=True from integrations
    
    Co-authored-by: Erik <erik@montnemery.com>

--- homeassistant/helpers/entity.py
+++ homeassistant/helpers/entity.py
@@ -530,8 +530,16 @@ class Entity(ABC):
         await self.async_added_to_hass()
         self.async_write_ha_state()
 
-    async def async_remove(self) -> None:
-        """Remove entity from Home Assistant."""
+    async def async_remove(self, *, force_remove: bool = False) -> None:
+        """Remove entity from Home Assistant.
+
+        If the entity has a non disabled entry in the entity registry,
+        the entity's state will be set to unavailable, in the same way
+        as when the entity registry is loaded.
+
+        If the entity doesn't have a non disabled entry in the entity registry,
+        or if force_remove=True, its state will be removed.
+        """
         assert self.hass is not None
 
         if self.platform and not self._added:
@@ -548,7 +556,16 @@ class Entity(ABC):
         await self.async_internal_will_remove_from_hass()
         await self.async_will_remove_from_hass()
 
-        self.hass.states.async_remove(self.entity_id, context=self._context)
+        # Check if entry still exists in entity registry (e.g. unloading config entry)
+        if (
+            not force_remove
+            and self.registry_entry
+            and not self.registry_entry.disabled
+        ):
+            # Set the entity's state will to unavailable + ATTR_RESTORED: True
+            self.registry_entry.write_unavailable_state(self.hass)
+        else:
+            self.hass.states.async_remove(self.entity_id, context=self._context)
 
     async def async_added_to_hass(self) -> None:
         """Run when entity about to be added to hass.
@@ -606,6 +623,7 @@ class Entity(ABC):
         data = event.data
         if data["action"] == "remove":
             await self.async_removed_from_registry()
+            self.registry_entry = None
             await self.async_remove()
 
         if data["action"] != "update":
@@ -617,7 +635,7 @@ class Entity(ABC):
         self.registry_entry = ent_reg.async_get(data["entity_id"])
         assert self.registry_entry is not None
 
-        if self.registry_entry.disabled_by is not None:
+        if self.registry_entry.disabled:
             await self.async_remove()
             return
 
@@ -626,7 +644,7 @@ class Entity(ABC):
             self.async_write_ha_state()
             return
 
-        await self.async_remove()
+        await self.async_remove(force_remove=True)
 
         assert self.platform is not None
         self.entity_id = self.registry_entry.entity_id
--- tests/components/deconz/test_switch.py
+++ tests/components/deconz/test_switch.py
@@ -10,7 +10,7 @@ from homeassistant.components.switch import (
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
 )
-from homeassistant.const import ATTR_ENTITY_ID, STATE_OFF, STATE_ON
+from homeassistant.const import ATTR_ENTITY_ID, STATE_OFF, STATE_ON, STATE_UNAVAILABLE
 from homeassistant.setup import async_setup_component
 
 from .test_gateway import DECONZ_WEB_REQUEST, setup_deconz_integration
@@ -139,6 +139,13 @@ async def test_power_plugs(hass):
 
     await hass.config_entries.async_unload(config_entry.entry_id)
 
+    states = hass.states.async_all()
+    assert len(hass.states.async_all()) == 4
+    for state in states:
+        assert state.state == STATE_UNAVAILABLE
+
+    await hass.config_entries.async_remove(config_entry.entry_id)
+    await hass.async_block_till_done()
     assert len(hass.states.async_all()) == 0
 
 
@@ -202,4 +209,11 @@ async def test_sirens(hass):
 
     await hass.config_entries.async_unload(config_entry.entry_id)
 
+    states = hass.states.async_all()
+    assert len(hass.states.async_all()) == 2
+    for state in states:
+        assert state.state == STATE_UNAVAILABLE
+
+    await hass.config_entries.async_remove(config_entry.entry_id)
+    await hass.async_block_till_done()
     assert len(hass.states.async_all()) == 0
