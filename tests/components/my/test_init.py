381115a6db04942d0fe9bbf16ccb77438416086f
Bram Kragten <mail@bramkragten.nl>
2021-02-08 13:12:56 +0100

    Change test

--- tests/components/my/test_init.py
+++ tests/components/my/test_init.py
@@ -1,14 +1,18 @@
 """Test the my init."""
 
-from homeassistant.components.frontend import DATA_PANELS
+from unittest import mock
+
 from homeassistant.components.my import URL_PATH
 from homeassistant.setup import async_setup_component
 
 
 async def test_setup(hass):
     """Test setup."""
-    assert await async_setup_component(hass, "my", {"foo": "bar"})
-    panel = hass.data[DATA_PANELS][URL_PATH]
-    assert panel.component_name == "my"
-    assert panel.sidebar_title is None
-    assert not panel.require_admin
+    with mock.patch(
+        "homeassistant.components.frontend.async_register_built_in_panel",
+        return_value=True,
+    ) as mock_register_panel:
+        assert await async_setup_component(hass, "my", {"foo": "bar"})
+        assert mock_register_panel.call_args == mock.call(
+            hass, "my", frontend_url_path=URL_PATH
+        )
