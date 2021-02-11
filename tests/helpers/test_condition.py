1623472a3f5f1d6b70980277bb261e40ccab1d95
Anders Melchiorsen <amelchio@nogoto.net>
2021-02-11 17:29:17 +0100

    Raise ConditionError for time errors (#46250)

--- tests/helpers/test_condition.py
+++ tests/helpers/test_condition.py
@@ -334,8 +334,11 @@ async def test_time_using_input_datetime(hass):
             hass, after="input_datetime.pm", before="input_datetime.am"
         )
 
-    assert not condition.time(hass, after="input_datetime.not_existing")
-    assert not condition.time(hass, before="input_datetime.not_existing")
+    with pytest.raises(ConditionError):
+        condition.time(hass, after="input_datetime.not_existing")
+
+    with pytest.raises(ConditionError):
+        condition.time(hass, before="input_datetime.not_existing")
 
 
 async def test_if_numeric_state_raises_on_unavailable(hass, caplog):
