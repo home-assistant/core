cf9fafddf331b9fb3657fbe9fa9fca0d8937d8f3
tkdrob <tkdrob4390@yahoo.com>
2021-02-12 03:28:11 -0500

    Use core constants for recollect_waste (#46416)

--- homeassistant/components/recollect_waste/sensor.py
+++ homeassistant/components/recollect_waste/sensor.py
@@ -6,7 +6,7 @@ import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
-from homeassistant.const import ATTR_ATTRIBUTION, CONF_FRIENDLY_NAME
+from homeassistant.const import ATTR_ATTRIBUTION, CONF_FRIENDLY_NAME, CONF_NAME
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.update_coordinator import (
@@ -24,8 +24,6 @@ ATTR_NEXT_PICKUP_DATE = "next_pickup_date"
 DEFAULT_ATTRIBUTION = "Pickup data provided by ReCollect Waste"
 DEFAULT_NAME = "recollect_waste"
 DEFAULT_ICON = "mdi:trash-can-outline"
-
-CONF_NAME = "name"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
