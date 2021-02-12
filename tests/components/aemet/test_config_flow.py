f34cb20d5d7caeb0cba93e17f1e16510bbb5104d
Álvaro Fernández Rojas <noltari@gmail.com>
2021-02-12 18:01:05 +0100

    aemet: test: config_flow: check duplicated unique id
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- tests/components/aemet/test_config_flow.py
+++ tests/components/aemet/test_config_flow.py
@@ -6,11 +6,17 @@ import requests_mock
 
 from homeassistant import data_entry_flow
 from homeassistant.components.aemet.const import DOMAIN
-from homeassistant.config_entries import ENTRY_STATE_LOADED, SOURCE_USER
+from homeassistant.config_entries import (
+    ENTRY_STATE_LOADED,
+    ENTRY_STATE_NOT_LOADED,
+    SOURCE_USER,
+)
 from homeassistant.const import CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
 import homeassistant.util.dt as dt_util
 
 from .util import aemet_requests_mock
+
+from tests.common import MockConfigEntry
 
 CONFIG = {
     CONF_NAME: "aemet",
@@ -52,6 +58,32 @@ async def test_form(hass):
         assert result["data"][CONF_LATITUDE] == CONFIG[CONF_LATITUDE]
         assert result["data"][CONF_LONGITUDE] == CONFIG[CONF_LONGITUDE]
         assert result["data"][CONF_API_KEY] == CONFIG[CONF_API_KEY]
+
+
+async def test_form_duplicated_id(hass):
+    """Test that the options form."""
+
+    now = dt_util.parse_datetime("2021-01-09 12:00:00+00:00")
+    with patch("homeassistant.util.dt.now", return_value=now), patch(
+        "homeassistant.util.dt.utcnow", return_value=now
+    ), requests_mock.mock() as _m:
+        aemet_requests_mock(_m)
+
+        entry = MockConfigEntry(domain=DOMAIN, unique_id="aemet_unique_id", data=CONFIG)
+        entry.add_to_hass(hass)
+
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+        assert entry.state == ENTRY_STATE_LOADED
+
+        entry_dup = MockConfigEntry(
+            domain=DOMAIN, unique_id="aemet_unique_id", data=CONFIG
+        )
+        entry_dup.add_to_hass(hass)
+
+        await hass.config_entries.async_unload(entry_dup.entry_id)
+        await hass.async_block_till_done()
+        assert entry_dup.state == ENTRY_STATE_NOT_LOADED
 
 
 async def test_form_api_offline(hass):
