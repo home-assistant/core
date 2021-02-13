410fa4b67e332a96aae823b3ca07648a19a7b360
javicalle <javicalle@gmail.com>
2021-02-13 19:05:47 +0100

    Added handle_event for set_level command in dimmable devices

--- tests/components/rflink/test_light.py
+++ tests/components/rflink/test_light.py
@@ -360,6 +360,77 @@ async def test_type_toggle(hass, monkeypatch):
     assert hass.states.get(f"{DOMAIN}.toggle_test").state == "off"
 
 
+async def test_set_level_command(hass, monkeypatch):
+    """Test 'set_level=XX' events."""
+    config = {
+        "rflink": {"port": "/dev/ttyABC0"},
+        DOMAIN: {
+            "platform": "rflink",
+            "devices": {
+                "newkaku_12345678_0": {"name": "l1"},
+                "test_no_dimmable": {"name": "l2"},
+                "test_dimmable": {"name": "l3", "type": "dimmable"},
+                "test_hybrid": {"name": "l4", "type": "hybrid"},
+            },
+        },
+    }
+
+    # setup mocking rflink module
+    event_callback, _, _, _ = await mock_rflink(hass, config, DOMAIN, monkeypatch)
+
+    # test sending command to a newkaku device
+    event_callback({"id": "newkaku_12345678_0", "command": "set_level=10"})
+    await hass.async_block_till_done()
+    # should affect state
+    state = hass.states.get(f"{DOMAIN}.l1")
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes[ATTR_BRIGHTNESS] == 170
+
+    # test sending command to a no dimmable device
+    event_callback({"id": "test_no_dimmable", "command": "set_level=10"})
+    await hass.async_block_till_done()
+    # should NOT affect state
+    state = hass.states.get(f"{DOMAIN}.l2")
+    assert state
+    assert state.state == STATE_OFF
+    assert not state.attributes.get(ATTR_BRIGHTNESS)
+
+    # test sending command to a dimmable device
+    event_callback({"id": "test_dimmable", "command": "set_level=5"})
+    await hass.async_block_till_done()
+    # should affect state
+    state = hass.states.get(f"{DOMAIN}.l3")
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes[ATTR_BRIGHTNESS] == 85
+
+    # test sending command to a hybrid device
+    event_callback({"id": "test_hybrid", "command": "set_level=15"})
+    await hass.async_block_till_done()
+    # should affect state
+    state = hass.states.get(f"{DOMAIN}.l4")
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes[ATTR_BRIGHTNESS] == 255
+
+    event_callback({"id": "test_hybrid", "command": "off"})
+    await hass.async_block_till_done()
+    # should affect state
+    state = hass.states.get(f"{DOMAIN}.l4")
+    assert state
+    assert state.state == STATE_OFF
+    assert state.attributes[ATTR_BRIGHTNESS] == 255
+
+    event_callback({"id": "test_hybrid", "command": "set_level=0"})
+    await hass.async_block_till_done()
+    # should affect state
+    state = hass.states.get(f"{DOMAIN}.l4")
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes[ATTR_BRIGHTNESS] == 0
+
+
 async def test_group_alias(hass, monkeypatch):
     """Group aliases should only respond to group commands (allon/alloff)."""
     config = {
@@ -367,7 +438,12 @@ async def test_group_alias(hass, monkeypatch):
         DOMAIN: {
             "platform": "rflink",
             "devices": {
-                "protocol_0_0": {"name": "test", "group_aliases": ["test_group_0_0"]}
+                "protocol_0_0": {"name": "test", "group_aliases": ["test_group_0_0"]},
+                "protocol_0_1": {
+                    "name": "test2",
+                    "type": "dimmable",
+                    "group_aliases": ["test_group_0_0"],
+                },
             },
         },
     }
@@ -382,12 +458,14 @@ async def test_group_alias(hass, monkeypatch):
     await hass.async_block_till_done()
 
     assert hass.states.get(f"{DOMAIN}.test").state == "on"
+    assert hass.states.get(f"{DOMAIN}.test2").state == "on"
 
     # test sending group command to group alias
     event_callback({"id": "test_group_0_0", "command": "off"})
     await hass.async_block_till_done()
 
     assert hass.states.get(f"{DOMAIN}.test").state == "on"
+    assert hass.states.get(f"{DOMAIN}.test2").state == "on"
 
 
 async def test_nogroup_alias(hass, monkeypatch):
