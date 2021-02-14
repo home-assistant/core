5bf5ac3ee34fcb27883e6704badbe3118046f989
Mick Vleeshouwer <mick@imick.nl>
2021-02-14 19:35:35 +0100

    Fix device state bug

--- homeassistant/components/tahoma/tahoma_entity.py
+++ homeassistant/components/tahoma/tahoma_entity.py
@@ -67,6 +67,8 @@ class TahomaEntity(CoordinatorEntity, Entity):
     @property
     def device_state_attributes(self) -> Dict[str, Any]:
         """Return the state attributes of the device."""
+        attr = {}
+
         if self.has_state(CORE_RSSI_LEVEL_STATE):
             attr[ATTR_RSSI_LEVEL] = self.select_state(CORE_RSSI_LEVEL_STATE)
 
