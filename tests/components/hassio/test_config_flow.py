811ea706df33da66e64e4a6a8a2d77f96ec56105
raman325 <7243222+raman325@users.noreply.github.com>
2021-02-14 01:09:40 -0500

    fix config flow tests

--- tests/components/hassio/test_config_flow.py
+++ tests/components/hassio/test_config_flow.py
@@ -2,90 +2,35 @@
 from unittest.mock import patch
 
 from homeassistant import config_entries, setup
-from homeassistant.components.hassio.config_flow import CannotConnect, InvalidAuth
-from homeassistant.components.hassio.const import DOMAIN
+from homeassistant.components.hassio import DOMAIN
 
 
-async def test_form(hass):
+async def test_config_flow(hass):
     """Test we get the form."""
     await setup.async_setup_component(hass, "persistent_notification", {})
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert result["type"] == "form"
-    assert result["errors"] == {}
-
     with patch(
-        "homeassistant.components.hassio.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
-    ), patch(
         "homeassistant.components.hassio.async_setup", return_value=True
     ) as mock_setup, patch(
         "homeassistant.components.hassio.async_setup_entry",
         return_value=True,
     ) as mock_setup_entry:
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_HASSIO}
         )
+        assert result["type"] == "create_entry"
+        assert result["title"] == DOMAIN.title()
+        assert result["data"] == {}
         await hass.async_block_till_done()
 
-    assert result2["type"] == "create_entry"
-    assert result2["title"] == "Name of the device"
-    assert result2["data"] == {
-        "host": "1.1.1.1",
-        "username": "test-username",
-        "password": "test-password",
-    }
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
 
 
-async def test_form_invalid_auth(hass):
-    """Test we handle invalid auth."""
+async def test_multiple_entries(hass):
+    """Test creating multiple hassio entries."""
+    await test_config_flow(hass)
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": config_entries.SOURCE_HASSIO}
     )
-
-    with patch(
-        "homeassistant.components.hassio.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_auth"}
-
-
-async def test_form_cannot_connect(hass):
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.hassio.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "cannot_connect"}
+    assert result["type"] == "abort"
+    assert result["reason"] == "already_configured"
