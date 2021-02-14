2a673e8a396b593647af414a1deb947d829cb02f
cnico <>
2021-02-15 00:41:00 +0100

    Test correction with encrypted password

--- tests/components/flipr/test_config_flow.py
+++ tests/components/flipr/test_config_flow.py
@@ -30,7 +30,10 @@ async def test_valid_credentials(hass):
     ) as mock_setup, patch(
         "homeassistant.components.flipr.async_setup_entry",
         return_value=True,
-    ) as mock_setup_entry:
+    ) as mock_setup_entry, patch(
+        "homeassistant.components.flipr.config_flow.encrypt_data",
+        return_value="ENCRYPTED_DATA_ah_ah",
+    ) as mock_crypt_util:
         result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_USER},
@@ -42,13 +45,15 @@ async def test_valid_credentials(hass):
         )
         await hass.async_block_till_done()
 
+    assert len(mock_flipr_client.mock_calls) == 1
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
+    assert len(mock_crypt_util.mock_calls) == 1
+
     assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result["title"] == "Flipr device - flipid"
     assert result["data"] == {
         CONF_USERNAME: "dummylogin",
-        CONF_PASSWORD: "dummypass",
+        CONF_PASSWORD: "ENCRYPTED_DATA_ah_ah",
         CONF_FLIPR_ID: "flipid",
     }
-    assert len(mock_flipr_client.mock_calls) == 1
-    assert len(mock_setup.mock_calls) == 1
-    assert len(mock_setup_entry.mock_calls) == 1
