3a44177e7c641ce01410142a871731259fac589a
Nicolas Cazottes <nicolas@cazottes.net>
2021-02-14 23:06:40 +0000

    Gestion des cas 0 ou plusieurs flipr dans le config_flow

--- homeassistant/components/flipr/config_flow.py
+++ homeassistant/components/flipr/config_flow.py
@@ -1,5 +1,5 @@
 """Config flow for Flipr integration."""
-import logging
+from typing import List
 
 from flipr_api import FliprAPIRestClient
 from requests.exceptions import HTTPError
@@ -10,8 +10,6 @@ from homeassistant import config_entries
 from .const import CONF_FLIPR_ID, CONF_PASSWORD, CONF_USERNAME
 from .const import DOMAIN  # pylint:disable=unused-import
 from .crypt_util import encrypt_data
-
-_LOGGER = logging.getLogger(__name__)
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -25,27 +23,30 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self._username = None
         self._password = None
         self._flipr_id = None
+        self._possible_flipr_ids = None
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        _LOGGER.debug("Starting async_step_user")
-
         if user_input is None:
             return self._show_setup_form()
 
         self._username = user_input[CONF_USERNAME]
         self._password = user_input[CONF_PASSWORD]
 
-        errors = {}
-        try:
-            self._flipr_id = await self._authenticate_and_search_flipr()
-        except HTTPError:
-            errors["base"] = "invalid_auth"
-            return self._show_setup_form(errors)
-
         if not self._flipr_id:
-            # No Flipr found : asks the user to enter it.
-            return await self.async_step_flipr_id()
+            try:
+                flipr_ids = await self._authenticate_and_search_flipr()
+            except HTTPError:
+                errors = {}
+                errors["base"] = "invalid_auth"
+                return self._show_setup_form(errors)
+
+            if len(flipr_ids) == 1:
+                self._flipr_id = flipr_ids[0]
+            else:
+                # No Flipr or multiple found : asks the user to enter it or select it.
+                self._possible_flipr_ids = flipr_ids
+                return await self.async_step_flipr_id()
 
         # No check for data retrieval here.
 
@@ -75,32 +76,44 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
-    async def _authenticate_and_search_flipr(self):
+    async def _authenticate_and_search_flipr(self) -> List[str]:
         """Validate the username and password provided and searches for a flipr id."""
         client = await self.hass.async_add_executor_job(
             FliprAPIRestClient, self._username, self._password
         )
 
-        _LOGGER.debug("FliprAPIRestClient called. %s", str(client))
-
         flipr_ids = await self.hass.async_add_executor_job(client.search_flipr_ids)
-        _LOGGER.debug("Flipr_ids found = %s", str(flipr_ids))
 
-        if len(flipr_ids) > 0:
-            # Return the found flipr_id as a string
-            return flipr_ids[0]
-
-        return None
+        return flipr_ids
 
     async def async_step_flipr_id(self, user_input=None):
         """Handle the initial step."""
-        _LOGGER.debug("Starting async_step_flipr_id")
-
         if not user_input:
+            # Creation of a select with the proposal of flipr ids values found by API.
+            if len(self._possible_flipr_ids) > 1:
+                flipr_ids_for_form = {}
+                for flipr_id in self._possible_flipr_ids:
+                    flipr_ids_for_form[flipr_id] = f"{flipr_id}"
+
+                return self.async_show_form(
+                    step_id="flipr_id",
+                    data_schema=vol.Schema(
+                        {
+                            vol.Required(CONF_FLIPR_ID): vol.All(
+                                vol.Coerce(str), vol.In(flipr_ids_for_form)
+                            )
+                        }
+                    ),
+                )
+            # Else : no flipr_id found. Allow the user to enter it.
+            # It may cause no data found afterwards in metrics...
             return self.async_show_form(
                 step_id="flipr_id",
                 data_schema=vol.Schema({vol.Required(CONF_FLIPR_ID): str}),
             )
+
+        # Get chosen or entered flipr_id.
+        self._flipr_id = user_input[CONF_FLIPR_ID]
 
         return await self.async_step_user(
             {
