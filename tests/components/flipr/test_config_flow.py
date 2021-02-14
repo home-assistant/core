8a73f30c91d30a72184fa3ce59483005572705a9
cnico <>
2021-02-14 10:45:55 +0100

    passing test

--- tests/components/flipr/test_config_flow.py
+++ tests/components/flipr/test_config_flow.py
@@ -1,91 +1,54 @@
 """Test the Flipr config flow."""
 from unittest.mock import patch
 
-from homeassistant import config_entries, setup
-from homeassistant.components.flipr.config_flow import CannotConnect, InvalidAuth
-from homeassistant.components.flipr.const import DOMAIN
+from homeassistant import config_entries, data_entry_flow, setup
+from homeassistant.components.flipr.const import (
+    CONF_FLIPR_ID,
+    CONF_PASSWORD,
+    CONF_USERNAME,
+    DOMAIN,
+)
 
 
-async def test_form(hass):
+async def test_show_form(hass):
     """Test we get the form."""
     await setup.async_setup_component(hass, "persistent_notification", {})
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
-    assert result["type"] == "form"
-    assert result["errors"] == {}
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == config_entries.SOURCE_USER
 
+
+async def test_valid_credentials(hass):
+    """Test valid login form."""
     with patch(
-        "homeassistant.components.flipr.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
-    ), patch(
+        "flipr_api.FliprAPIRestClient.search_flipr_ids",
+        return_value=["flipid"],
+    ) as mock_flipr_client, patch(
         "homeassistant.components.flipr.async_setup", return_value=True
     ) as mock_setup, patch(
         "homeassistant.components.flipr.async_setup_entry",
         return_value=True,
     ) as mock_setup_entry:
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+            data={
+                CONF_USERNAME: "dummylogin",
+                CONF_PASSWORD: "dummypass",
+                CONF_FLIPR_ID: "flipid",
             },
         )
         await hass.async_block_till_done()
 
-    assert result2["type"] == "create_entry"
-    assert result2["title"] == "Name of the device"
-    assert result2["data"] == {
-        "host": "1.1.1.1",
-        "username": "test-username",
-        "password": "test-password",
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == "Flipr device - flipid"
+    assert result["data"] == {
+        CONF_USERNAME: "dummylogin",
+        CONF_PASSWORD: "dummypass",
+        CONF_FLIPR_ID: "flipid",
     }
+    assert len(mock_flipr_client.mock_calls) == 1
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_invalid_auth(hass):
-    """Test we handle invalid auth."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.flipr.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_auth"}
-
-
-async def test_form_cannot_connect(hass):
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.flipr.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "cannot_connect"}
