b4daae6e8c9a5bab46733c228cdcd64950944f79
Tom Parker-Shemilt <palfrey@tevp.net>
2021-02-14 12:35:29 +0000

    Update tests/util/yaml/test_init.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/util/yaml/test_init.py
+++ tests/util/yaml/test_init.py
@@ -466,9 +466,8 @@ def test_duplicate_key(caplog):
 def test_no_recursive_secrets(caplog):
     """Test that loading of secrets from the secrets file fails correctly."""
     files = {YAML_CONFIG_FILE: "key: !secret a", yaml.SECRET_YAML: "a: 1\nb: !secret a"}
-    with patch_yaml_files(files):
-        with pytest.raises(HomeAssistantError) as e:
-            load_yaml_config_file(YAML_CONFIG_FILE)
+    with patch_yaml_files(files), pytest.raises(HomeAssistantError) as e:
+        load_yaml_config_file(YAML_CONFIG_FILE)
         assert e.value.args == (
             "secrets.yaml: attempt to load secret from within secrets file",
         )
