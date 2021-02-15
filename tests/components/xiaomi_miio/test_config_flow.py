24c80cd80048c9255fe49d17ac19cd594147e226
starkillerOG <starkiller.og@gmail.com>
2021-02-15 08:59:56 +0100

    fix black

--- tests/components/xiaomi_miio/test_config_flow.py
+++ tests/components/xiaomi_miio/test_config_flow.py
@@ -6,7 +6,10 @@ from miio import DeviceException
 from homeassistant import config_entries
 from homeassistant.components import zeroconf
 from homeassistant.components.xiaomi_miio import const
-from homeassistant.components.xiaomi_miio.config_flow import DEFAULT_DEVICE_NAME, DEFAULT_GATEWAY_NAME
+from homeassistant.components.xiaomi_miio.config_flow import (
+    DEFAULT_DEVICE_NAME,
+    DEFAULT_GATEWAY_NAME,
+)
 from homeassistant.const import CONF_HOST, CONF_NAME, CONF_TOKEN
 
 ZEROCONF_NAME = "name"
