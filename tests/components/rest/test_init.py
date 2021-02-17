c1e3d3ee3f6e24177978585dfb93ca8fcea9f30b
J. Nick Koston <nick@koston.org>
2021-02-17 13:58:28 -1000

    tests

---
+++ tests/components/rest/test_init.py
@@ -0,0 +1,56 @@
+"""Tests for rest component."""
+
+import respx
+
+from homeassistant.components.rest.const import DOMAIN
+from homeassistant.const import DATA_MEGABYTES
+from homeassistant.setup import async_setup_component
+
+
+@respx.mock
+async def test_setup_minimum_resource_template(hass):
+    """Test setup with minimum configuration (resource_template)."""
+
+    respx.get("http://localhost").respond(
+        status_code=200,
+        json={
+            "sensor1": "1",
+            "sensor2": "2",
+            "binary_sensor1": "on",
+            "binary_sensor2": "off",
+        },
+    )
+    assert await async_setup_component(
+        hass,
+        DOMAIN,
+        {
+            "resource_template": "{% set url = 'http://localhost' %}{{ url }}",
+            "method": "GET",
+            "verify_ssl": "false",
+            "timeout": 30,
+            "sensor": [
+                {
+                    "unit_of_measurement": DATA_MEGABYTES,
+                    "name": "sensor1",
+                    "value_template": "{{ value_json.sensor1 }}",
+                },
+                {
+                    "unit_of_measurement": DATA_MEGABYTES,
+                    "name": "sensor2",
+                    "value_template": "{{ value_json.sensor2 }}",
+                },
+            ],
+            "binary_sensor": [
+                {
+                    "name": "binary_sensor1",
+                    "value_template": "{{ value_json.binary_sensor1 }}",
+                },
+                {
+                    "name": "binary_sensor2",
+                    "value_template": "{{ value_json.binary_sensor2 }}",
+                },
+            ],
+        },
+    )
+    await hass.async_block_till_done()
+    assert len(hass.states.async_all()) == 4
