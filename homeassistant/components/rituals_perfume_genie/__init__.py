50b993cea028e1a6b04ffd9e044305a1f59c841c
milanmeu <milan.meulemans@live.be>
2021-02-18 22:04:39 +0000

    Add a connectivity check during entry setup

--- homeassistant/components/rituals_perfume_genie/__init__.py
+++ homeassistant/components/rituals_perfume_genie/__init__.py
@@ -1,13 +1,18 @@
 """The Rituals Perfume Genie integration."""
 import asyncio
+import logging
 
+from aiohttp.client_exceptions import ClientConnectorError
 from pyrituals import Account
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import ACCOUNT_HASH, DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 EMPTY_CREDENTIALS = ""
 
@@ -24,6 +29,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     session = async_get_clientsession(hass)
     account = Account(EMPTY_CREDENTIALS, EMPTY_CREDENTIALS, session)
     account.data = {ACCOUNT_HASH: entry.data.get(ACCOUNT_HASH)}
+
+    try:
+        await account.get_devices()
+    except ClientConnectorError as ex:
+        raise ConfigEntryNotReady from ex
+
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = account
 
     for component in PLATFORMS:
