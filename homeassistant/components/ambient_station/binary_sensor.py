02955cf7d9f60e8c155dd375fbda8ae224639ffb
conflipper <68352908+conflipper@users.noreply.github.com>
2021-02-17 18:09:39 -0800

    Update binary_sensor.py

--- homeassistant/components/ambient_station/binary_sensor.py
+++ homeassistant/components/ambient_station/binary_sensor.py
@@ -70,10 +70,11 @@ class AmbientWeatherBinarySensor(AmbientWeatherEntity, BinarySensorEntity):
             TYPE_BATT7,
             TYPE_BATT8,
             TYPE_BATT9,
+            TYPE_BATTOUT,
             TYPE_PM25_CO2,
             TYPE_PM25_BATT,
             TYPE_PM25IN_BATT,
-            TYPE_BATTOUT,
+            
         ):
             return self._state == 0
 
