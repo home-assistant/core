b4a446bca927f681dc81615bd93edf9313b9f003
Matt Zimmerman <mdz@users.noreply.github.com>
2021-02-18 16:18:02 -0800

    Address late smarttub review (#46703)
    * _config -> config
    
    * remove unused string
    
    * remove entity tests
    
    * replace unit tests with integration tests using the core
    
    * refactor polling to use asyncio.gather
    
    * remove redundant component init
    
    * remove gather in favor of simple loop
    
    * use async_fire_time_changed instead of async_update_entity
    
    * use hass.config_entries.async_setup instead of calling smarttub.async_setup_entry directly
    
    * replace stray smarttub.async_setup_entry call
    
    * async_unload_entry -> hass.config_entries.async_unload
    
    * remove broken test

--- tests/components/smarttub/test_init.py
+++ tests/components/smarttub/test_init.py
@@ -1,48 +1,50 @@
 """Test smarttub setup process."""
 
 import asyncio
-from unittest.mock import patch
 
-import pytest
 from smarttub import LoginFailed
 
 from homeassistant.components import smarttub
-from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.config_entries import (
+    ENTRY_STATE_SETUP_ERROR,
+    ENTRY_STATE_SETUP_RETRY,
+)
 from homeassistant.setup import async_setup_component
 
 
-async def test_setup_with_no_config(hass):
+async def test_setup_with_no_config(setup_component, hass, smarttub_api):
     """Test that we do not discover anything."""
-    assert await async_setup_component(hass, smarttub.DOMAIN, {}) is True
 
     # No flows started
     assert len(hass.config_entries.flow.async_progress()) == 0
 
-    assert smarttub.const.SMARTTUB_CONTROLLER not in hass.data[smarttub.DOMAIN]
+    smarttub_api.login.assert_not_called()
 
 
-async def test_setup_entry_not_ready(hass, config_entry, smarttub_api):
+async def test_setup_entry_not_ready(setup_component, hass, config_entry, smarttub_api):
     """Test setup when the entry is not ready."""
-    assert await async_setup_component(hass, smarttub.DOMAIN, {}) is True
     smarttub_api.login.side_effect = asyncio.TimeoutError
 
-    with pytest.raises(ConfigEntryNotReady):
-        await smarttub.async_setup_entry(hass, config_entry)
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    assert config_entry.state == ENTRY_STATE_SETUP_RETRY
 
 
-async def test_setup_auth_failed(hass, config_entry, smarttub_api):
+async def test_setup_auth_failed(setup_component, hass, config_entry, smarttub_api):
     """Test setup when the credentials are invalid."""
-    assert await async_setup_component(hass, smarttub.DOMAIN, {}) is True
     smarttub_api.login.side_effect = LoginFailed
 
-    assert await smarttub.async_setup_entry(hass, config_entry) is False
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    assert config_entry.state == ENTRY_STATE_SETUP_ERROR
 
 
-async def test_config_passed_to_config_entry(hass, config_entry, config_data):
+async def test_config_passed_to_config_entry(
+    hass, config_entry, config_data, smarttub_api
+):
     """Test that configured options are loaded via config entry."""
     config_entry.add_to_hass(hass)
-    ret = await async_setup_component(hass, smarttub.DOMAIN, config_data)
-    assert ret is True
+    assert await async_setup_component(hass, smarttub.DOMAIN, config_data)
 
 
 async def test_unload_entry(hass, config_entry, smarttub_api):
@@ -51,10 +53,4 @@ async def test_unload_entry(hass, config_entry, smarttub_api):
 
     assert await async_setup_component(hass, smarttub.DOMAIN, {}) is True
 
-    assert await smarttub.async_unload_entry(hass, config_entry)
-
-    # test failure of platform unload
-    assert await async_setup_component(hass, smarttub.DOMAIN, {}) is True
-    with patch.object(hass.config_entries, "async_forward_entry_unload") as mock:
-        mock.return_value = False
-        assert await smarttub.async_unload_entry(hass, config_entry) is False
+    assert await hass.config_entries.async_unload(config_entry.entry_id)
