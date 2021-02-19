ed5338023f28cf527c19eccb0921febe64f04969
J. Nick Koston <nick@koston.org>
2021-02-19 09:19:46 -1000

    Typing fixes for fan speed count and steps

--- homeassistant/components/smartthings/fan.py
+++ homeassistant/components/smartthings/fan.py
@@ -81,7 +81,7 @@ class SmartThingsFan(SmartThingsEntity, FanEntity):
         return ranged_value_to_percentage(SPEED_RANGE, self._device.status.fan_speed)
 
     @property
-    def speed_count(self) -> Optional[int]:
+    def speed_count(self) -> int:
         """Return the number of speeds the fan supports."""
         return int_states_in_range(SPEED_RANGE)
 
