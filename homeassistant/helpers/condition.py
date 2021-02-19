d9abfdc1843691ad608ffbf19a6082967144f03b
Anders Melchiorsen <amelchio@nogoto.net>
2021-02-19 13:14:47 +0100

    Raise ConditionError for zone errors (#46253)
    * Raise ConditionError for zone errors
    
    * Do not test missing state
    
    * Handle multiple entities/zones

--- homeassistant/helpers/condition.py
+++ homeassistant/helpers/condition.py
@@ -588,23 +588,36 @@ def zone(
 
     Async friendly.
     """
+    if zone_ent is None:
+        raise ConditionError("No zone specified")
+
     if isinstance(zone_ent, str):
+        zone_ent_id = zone_ent
         zone_ent = hass.states.get(zone_ent)
 
-    if zone_ent is None:
-        return False
-
-    if isinstance(entity, str):
-        entity = hass.states.get(entity)
+        if zone_ent is None:
+            raise ConditionError(f"Unknown zone {zone_ent_id}")
 
     if entity is None:
-        return False
+        raise ConditionError("No entity specified")
+
+    if isinstance(entity, str):
+        entity_id = entity
+        entity = hass.states.get(entity)
+
+        if entity is None:
+            raise ConditionError(f"Unknown entity {entity_id}")
+    else:
+        entity_id = entity.entity_id
 
     latitude = entity.attributes.get(ATTR_LATITUDE)
     longitude = entity.attributes.get(ATTR_LONGITUDE)
 
-    if latitude is None or longitude is None:
-        return False
+    if latitude is None:
+        raise ConditionError(f"Entity {entity_id} has no 'latitude' attribute")
+
+    if longitude is None:
+        raise ConditionError(f"Entity {entity_id} has no 'longitude' attribute")
 
     return zone_cmp.in_zone(
         zone_ent, latitude, longitude, entity.attributes.get(ATTR_GPS_ACCURACY, 0)
@@ -622,13 +635,26 @@ def zone_from_config(
 
     def if_in_zone(hass: HomeAssistant, variables: TemplateVarsType = None) -> bool:
         """Test if condition."""
-        return all(
-            any(
-                zone(hass, zone_entity_id, entity_id)
-                for zone_entity_id in zone_entity_ids
-            )
-            for entity_id in entity_ids
-        )
+        errors = []
+
+        all_ok = True
+        for entity_id in entity_ids:
+            entity_ok = False
+            for zone_entity_id in zone_entity_ids:
+                try:
+                    if zone(hass, zone_entity_id, entity_id):
+                        entity_ok = True
+                except ConditionError as ex:
+                    errors.append(str(ex))
+
+            if not entity_ok:
+                all_ok = False
+
+        # Raise the errors only if no definitive result was found
+        if errors and not all_ok:
+            raise ConditionError("Error in 'zone' condition: " + ", ".join(errors))
+
+        return all_ok
 
     return if_in_zone
 
