8e3f81545a3c17bb2c1ad8a4cc1846fb58239695
Rob Bierbooms <mail@robbierbooms.nl>
2021-02-19 20:57:53 +0100

    Update homeassistant/components/buienradar/config_flow.py
    Co-authored-by: Ties de Kock <ties@tiesdekock.nl>

--- homeassistant/components/buienradar/config_flow.py
+++ homeassistant/components/buienradar/config_flow.py
@@ -40,7 +40,7 @@ def configured_instances(hass):
 
 
 class BuienradarFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for buienradar2."""
+    """Handle a config flow for buienradar."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
