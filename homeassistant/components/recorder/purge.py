148499ced72e7c5f042772127985b6c0be5a7129
J. Nick Koston <nick@koston.org>
2021-02-19 08:08:11 -1000

    Revert "Update homeassistant/components/recorder/purge.py"
    This reverts commit eaba6a8117b7fc122faa6522b795084dab198223.

--- homeassistant/components/recorder/purge.py
+++ homeassistant/components/recorder/purge.py
@@ -50,7 +50,6 @@ def purge_old_data(instance, purge_days: int, repack: bool) -> bool:
             # for us.
             disconnected_rows = (
                 session.query(States)
-                .filter(States.last_updated >= batch_purge_before)
                 .filter(
                     States.old_state_id.in_(
                         session.query(States.state_id)
