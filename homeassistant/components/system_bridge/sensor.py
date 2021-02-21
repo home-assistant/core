b2ad7c64ea590c5830f76667bdacccd20d3f506c
Aidan Timson <contact@timmo.xyz>
2021-02-21 13:53:08 +0000

    Add CPU sensors

--- homeassistant/components/system_bridge/sensor.py
+++ homeassistant/components/system_bridge/sensor.py
@@ -5,21 +5,34 @@ from systembridge import Bridge
 
 from homeassistant.components.zeroconf import ATTR_HOSTNAME
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_VOLTAGE, DEVICE_CLASS_BATTERY, PERCENTAGE
+from homeassistant.const import (
+    ATTR_VOLTAGE,
+    DEVICE_CLASS_BATTERY,
+    DEVICE_CLASS_TEMPERATURE,
+    FREQUENCY_GIGAHERTZ,
+    PERCENTAGE,
+    TEMP_CELSIUS,
+)
 from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from . import BridgeDeviceEntity
 from .const import DOMAIN
 
+# _LOGGER = logging.getLogger(__name__)
+
 ATTR_ARCH = "arch"
+ATTR_BRAND = "brand"
 ATTR_BUILD = "build"
 ATTR_CAPACITY = "capacity"
 ATTR_CAPACITY_MAX = "capacity max"
 ATTR_CHARGING = "charging"
 ATTR_CODENAME = "codename"
+ATTR_CORES = "cores"
+ATTR_CORES_PHYSICAL = "cores physical"
 ATTR_DISTRO = "distro"
 ATTR_FQDN = "fqdn"
+ATTR_GOVERNOR = "governor"
 ATTR_KERNEL = "kernel"
 ATTR_MANUFACTURER = "manufacturer"
 ATTR_MODEL = "model"
@@ -27,8 +40,15 @@ ATTR_PLATFORM = "platform"
 ATTR_RELEASE = "release"
 ATTR_SERIAL = "serial"
 ATTR_SERVICE_PACK = "service pack"
+ATTR_SPEED = "speed"
+ATTR_SPEED_CURRENT_MAX = "speed current max"
+ATTR_SPEED_CURRENT_MIN = "speed current min"
+ATTR_SPEED_MAX = "speed max"
+ATTR_SPEED_MIN = "speed min"
+ATTR_TEMPERATURE_MAX = "temperature max"
 ATTR_TIME_REMAINING = "time remaining"
 ATTR_TYPE = "type"
+ATTR_VENDOR = "vendor"
 
 
 async def async_setup_entry(
@@ -41,6 +61,8 @@ async def async_setup_entry(
     async_add_entities(
         [
             BridgeBatterySensor(coordinator, bridge),
+            BridgeCpuSpeedSensor(coordinator, bridge),
+            BridgeCpuTemperatureSensor(coordinator, bridge),
             BridgeOsSensor(coordinator, bridge),
         ],
         True,
@@ -112,6 +134,84 @@ class BridgeBatterySensor(BridgeSensor):
             ATTR_TIME_REMAINING: bridge.battery.timeRemaining,
             ATTR_TYPE: bridge.battery.type,
             ATTR_VOLTAGE: bridge.battery.voltage,
+        }
+
+
+class BridgeCpuSpeedSensor(BridgeSensor):
+    """Defines a CPU sensor."""
+
+    def __init__(self, coordinator: DataUpdateCoordinator, bridge: Bridge):
+        """Initialize System Bridge sensor."""
+        super().__init__(
+            coordinator,
+            bridge,
+            "cpu_speed",
+            "CPU Speed",
+            None,
+            None,
+            FREQUENCY_GIGAHERTZ,
+        )
+
+    @property
+    def state(self) -> str:
+        """Return the state of the sensor."""
+        bridge: Bridge = self.coordinator.data
+        return bridge.cpu.currentSpeed.avg
+
+    @property
+    def device_state_attributes(self) -> Optional[Dict[str, Any]]:
+        """Return the state attributes of the entity."""
+        bridge: Bridge = self.coordinator.data
+        return {
+            ATTR_BRAND: bridge.cpu.cpu.brand,
+            ATTR_CORES_PHYSICAL: bridge.cpu.cpu.physicalCores,
+            ATTR_CORES: bridge.cpu.cpu.cores,
+            ATTR_GOVERNOR: bridge.cpu.cpu.governor,
+            ATTR_MANUFACTURER: bridge.cpu.cpu.manufacturer,
+            ATTR_SPEED_CURRENT_MAX: bridge.cpu.currentSpeed.max,
+            ATTR_SPEED_CURRENT_MIN: bridge.cpu.currentSpeed.min,
+            ATTR_SPEED_MAX: bridge.cpu.cpu.speedMax,
+            ATTR_SPEED_MIN: bridge.cpu.cpu.speedMin,
+            ATTR_SPEED: bridge.cpu.cpu.speed,
+            ATTR_VENDOR: bridge.cpu.cpu.vendor,
+            ATTR_VOLTAGE: bridge.cpu.cpu.voltage,
+        }
+
+
+class BridgeCpuTemperatureSensor(BridgeSensor):
+    """Defines a CPU sensor."""
+
+    def __init__(self, coordinator: DataUpdateCoordinator, bridge: Bridge):
+        """Initialize System Bridge sensor."""
+        super().__init__(
+            coordinator,
+            bridge,
+            "cpu_temperature",
+            "CPU Temperature",
+            None,
+            DEVICE_CLASS_TEMPERATURE,
+            TEMP_CELSIUS,
+        )
+
+    @property
+    def state(self) -> str:
+        """Return the state of the sensor."""
+        bridge: Bridge = self.coordinator.data
+        return bridge.cpu.temperature.main
+
+    @property
+    def device_state_attributes(self) -> Optional[Dict[str, Any]]:
+        """Return the state attributes of the entity."""
+        bridge: Bridge = self.coordinator.data
+        return {
+            ATTR_BRAND: bridge.cpu.cpu.brand,
+            ATTR_CORES_PHYSICAL: bridge.cpu.cpu.physicalCores,
+            ATTR_CORES: bridge.cpu.cpu.cores,
+            ATTR_GOVERNOR: bridge.cpu.cpu.governor,
+            ATTR_MANUFACTURER: bridge.cpu.cpu.manufacturer,
+            ATTR_TEMPERATURE_MAX: bridge.cpu.temperature.max,
+            ATTR_VENDOR: bridge.cpu.cpu.vendor,
+            ATTR_VOLTAGE: bridge.cpu.cpu.voltage,
         }
 
 
