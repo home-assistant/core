c153d5519af90f8ce77039ea67473d151b4e5124
Matt Zimmerman <mdz@alcor.net>
2021-02-21 12:40:45 -0800

    title case entity names

--- homeassistant/components/smarttub/switch.py
+++ homeassistant/components/smarttub/switch.py
@@ -51,9 +51,9 @@ class SmartTubPump(SmartTubEntity, SwitchEntity):
         """Return a name for this pump entity."""
         spa_name = get_spa_name(self.spa)
         if self.pump_type == SpaPump.PumpType.CIRCULATION:
-            return f"{spa_name} circulation pump"
+            return f"{spa_name} Circulation Pump"
         if self.pump_type == SpaPump.PumpType.JET:
-            return f"{spa_name} jet {self.pump_id}"
+            return f"{spa_name} Jet {self.pump_id}"
         return f"{spa_name} pump {self.pump_id}"
 
     @property
