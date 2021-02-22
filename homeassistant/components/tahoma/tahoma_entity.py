1986f2dd9892f90325d312c82fa6d9f6c2e924ef
Mick Vleeshouwer <mick@imick.nl>
2021-02-22 13:49:48 +0100

    Simplify assumed states

--- homeassistant/components/tahoma/tahoma_entity.py
+++ homeassistant/components/tahoma/tahoma_entity.py
@@ -54,7 +54,7 @@ class TahomaEntity(CoordinatorEntity, Entity):
     @property
     def assumed_state(self) -> bool:
         """Return True if unable to access real state of the entity."""
-        return self.device.states is None or len(self.device.states) == 0
+        return not self.device.states
 
     @property
     def device_info(self) -> Dict[str, Any]:
