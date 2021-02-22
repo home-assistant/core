d2f2eaccbc217c39b63db0f647eb4054f847ae54
tomasz75 <tomek@sviete.pl>
2021-02-22 15:09:40 +0100

    bump yt

--- homeassistant/components/ais_updater/scripts/release_script.sh
+++ homeassistant/components/ais_updater/scripts/release_script.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-echo "AIS release script starting for 21.01.29 on chanel 2" 
-apt update 
+echo "AIS release script starting for 21.02.18 on chanel 2" 
+echo "21.02.19" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt  
 
-apt upgrade  -y 
-
-curl -k -o "/data/data/pl.sviete.dom/files/home/.bash_profile" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/.bash_profile 
-
-curl -k -o "/sdcard/AisClient.apk" -L https://powiedz.co/ota/android/AisPanelApp-gate-release.apk && su -c "pm install -r /sdcard/AisClient.apk" 
-
-echo "21.02.03" > /data/data/pl.sviete.dom/files/home/AIS/.ais_aptecho "21.02.03" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
