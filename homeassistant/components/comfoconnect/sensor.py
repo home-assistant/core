9fef1ad802ee6a2d82ad1fb1c2b8c7bcf00a2234
@tkdrob <tkdrob4390@yahoo.com>
2021-02-22 00:10:16 +0000

    Clean up constants

--- homeassistant/components/comfoconnect/sensor.py
+++ homeassistant/components/comfoconnect/sensor.py
@@ -29,6 +29,7 @@ import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
     ATTR_DEVICE_CLASS,
+    ATTR_ID,
     CONF_RESOURCES,
     DEVICE_CLASS_ENERGY,
     DEVICE_CLASS_HUMIDITY,
@@ -72,7 +73,6 @@ ATTR_SUPPLY_TEMPERATURE = "supply_temperature"
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_ICON = "icon"
-ATTR_ID = "id"
 ATTR_LABEL = "label"
 ATTR_MULTIPLIER = "multiplier"
 ATTR_UNIT = "unit"
