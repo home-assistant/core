d7f105ba797ed44320b33c1b4f89cb05245575da
Mick Vleeshouwer <mick@imick.nl>
2021-02-22 17:59:55 +0100

    Add cover platform
    Co-authored-by:Vincent Le Bourlot <vlebourl@gmail.com>
    Co-authored-by: Thibaut <thibaut@etienne.pw>

--- homeassistant/components/tahoma/const.py
+++ homeassistant/components/tahoma/const.py
@@ -1,5 +1,6 @@
 """Constants for the Somfy TaHoma integration."""
 from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR
+from homeassistant.components.cover import DOMAIN as COVER
 from homeassistant.components.light import DOMAIN as LIGHT
 from homeassistant.components.lock import DOMAIN as LOCK
 
@@ -15,17 +16,32 @@ IGNORED_TAHOMA_TYPES = [
 
 # Used to map the Somfy widget and ui_class to the Home Assistant platform
 TAHOMA_TYPES = {
+    "AdjustableSlatsRollerShutter": COVER,
     "AirFlowSensor": BINARY_SENSOR,  # widgetName, uiClass is AirSensor (sensor)
+    "Awning": COVER,
     "CarButtonSensor": BINARY_SENSOR,
     "ContactSensor": BINARY_SENSOR,
-    "Light": LIGHT,
+    "Curtain": COVER,
     "DoorLock": LOCK,
+    "ExteriorScreen": COVER,
+    "ExteriorVenetianBlind": COVER,
+    "GarageDoor": COVER,
+    "Gate": COVER,
+    "Light": LIGHT,
     "MotionSensor": BINARY_SENSOR,
+    "MyFoxSecurityCamera": COVER,  # widgetName, uiClass is Camera (not supported)
     "OccupancySensor": BINARY_SENSOR,
+    "Pergola": COVER,
     "RainSensor": BINARY_SENSOR,
+    "RollerShutter": COVER,
+    "Screen": COVER,
+    "Shutter": COVER,
     "SirenStatus": BINARY_SENSOR,  # widgetName, uiClass is Siren (switch)
     "SmokeSensor": BINARY_SENSOR,
+    "SwingingShutter": COVER,
+    "VenetianBlind": COVER,
     "WaterDetectionSensor": BINARY_SENSOR,  # widgetName, uiClass is HumiditySensor (sensor)
+    "Window": COVER,
     "WindowHandle": BINARY_SENSOR,
 }
 
