d36cd368102f14229a386b73ae9e504527864ca6
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-02-24 09:25:58 -0500

    Update homeassistant/components/hassio/__init__.py
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/hassio/__init__.py
+++ homeassistant/components/hassio/__init__.py
@@ -539,7 +539,7 @@ def register_addons_in_dev_reg(
             config_entry_id=entry_id,
             identifiers={(DOMAIN, addon[ATTR_SLUG])},
             manufacturer=addon.get(ATTR_REPOSITORY) or addon.get(ATTR_URL) or "unknown",
-            model="Hass.io Add-On",
+            model="Home Assistant Add-on",
             sw_version=addon[ATTR_VERSION],
             name=addon[ATTR_NAME],
             entry_type=ATTR_SERVICE,
