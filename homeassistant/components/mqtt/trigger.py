0eec1a3feccc2e08964ae82605f817fac87090d8
Erik <erik@montnemery.com>
2021-02-24 19:27:02 +0100

    Revert "Rename value_template to payload_template"
    This reverts commit 902094eefc6612e6b5c3bdb7440520af050c7f20.

--- homeassistant/components/mqtt/trigger.py
+++ homeassistant/components/mqtt/trigger.py
@@ -4,7 +4,7 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.const import CONF_PAYLOAD, CONF_PLATFORM
+from homeassistant.const import CONF_PAYLOAD, CONF_PLATFORM, CONF_VALUE_TEMPLATE
 from homeassistant.core import HassJob, callback
 from homeassistant.helpers import config_validation as cv, template
 
@@ -13,7 +13,6 @@ from .. import mqtt
 # mypy: allow-untyped-defs
 
 CONF_ENCODING = "encoding"
-CONF_PAYLOAD_TEMPLATE = "payload_template"
 CONF_QOS = "qos"
 CONF_TOPIC = "topic"
 DEFAULT_ENCODING = "utf-8"
@@ -24,7 +23,7 @@ TRIGGER_SCHEMA = vol.Schema(
         vol.Required(CONF_PLATFORM): mqtt.DOMAIN,
         vol.Required(CONF_TOPIC): mqtt.util.valid_subscribe_topic_template,
         vol.Optional(CONF_PAYLOAD): cv.template,
-        vol.Optional(CONF_PAYLOAD_TEMPLATE): cv.template,
+        vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
         vol.Optional(CONF_ENCODING, default=DEFAULT_ENCODING): cv.string,
         vol.Optional(CONF_QOS, default=DEFAULT_QOS): vol.All(
             vol.Coerce(int), vol.In([0, 1, 2])
@@ -39,7 +38,7 @@ async def async_attach_trigger(hass, config, action, automation_info):
     """Listen for state changes based on configuration."""
     topic = config[CONF_TOPIC]
     wanted_payload = config.get(CONF_PAYLOAD)
-    payload_template = config.get(CONF_PAYLOAD_TEMPLATE)
+    value_template = config.get(CONF_VALUE_TEMPLATE)
     encoding = config[CONF_ENCODING] or None
     qos = config[CONF_QOS]
     job = HassJob(action)
@@ -56,15 +55,15 @@ async def async_attach_trigger(hass, config, action, automation_info):
         topic = topic.async_render(variables, limited=True)
         topic = mqtt.util.valid_subscribe_topic(topic)
 
-    template.attach(hass, payload_template)
+    template.attach(hass, value_template)
 
     @callback
     def mqtt_automation_listener(mqttmsg):
         """Listen for MQTT messages."""
         payload = mqttmsg.payload
 
-        if payload_template is not None:
-            payload = payload_template.async_render_with_possible_json_value(
+        if value_template is not None:
+            payload = value_template.async_render_with_possible_json_value(
                 payload,
                 error_value=None,
             )
