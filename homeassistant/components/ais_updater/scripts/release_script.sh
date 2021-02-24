0ff4cc824f6ebdfd5c5bdaa46d3df16e22be88f2
tomasz75 <tomek@sviete.pl>
2021-02-24 16:01:44 +0100

    recorder.purge log warning

--- homeassistant/components/ais_updater/scripts/release_script.sh
+++ homeassistant/components/ais_updater/scripts/release_script.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-echo "AIS release script starting for 21.02.18 on chanel 2" 
-echo "21.02.19" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt  
+echo "AIS release script starting for 21.02.19 on chanel 2" 
 
