896b398f781e507fbd8cce5865a593bb59dd13f9
Nathan Spencer <natekspencer@gmail.com>
2021-02-25 18:53:10 +0000

    Patch pypi package instead of HA code

--- tests/components/litterrobot/conftest.py
+++ tests/components/litterrobot/conftest.py
@@ -34,7 +34,7 @@ def mock_account():
     return account
 
 
-async def setup_integration(hass, mock_account, platform_domain):
+async def setup_integration(hass, mock_account, platform_domain=None):
     """Load a Litter-Robot platform with the provided hub."""
     entry = MockConfigEntry(
         domain=litterrobot.DOMAIN,
@@ -43,7 +43,10 @@ async def setup_integration(hass, mock_account, platform_domain):
     entry.add_to_hass(hass)
 
     with patch("pylitterbot.Account", return_value=mock_account), patch(
-        "homeassistant.components.litterrobot.PLATFORMS", [platform_domain]
+        "homeassistant.components.litterrobot.PLATFORMS",
+        [platform_domain] if platform_domain else [],
     ):
         await hass.config_entries.async_setup(entry.entry_id)
         await hass.async_block_till_done()
+
+    return entry
--- tests/components/litterrobot/test_init.py
+++ tests/components/litterrobot/test_init.py
@@ -1,31 +1,32 @@
 """Test Litter-Robot setup process."""
 from unittest.mock import patch
 
-from pylitterbot.exceptions import LitterRobotException
+from pylitterbot.exceptions import LitterRobotException, LitterRobotLoginException
+import pytest
 
 from homeassistant.components import litterrobot
-from homeassistant.setup import async_setup_component
 
 from .common import CONFIG
+from .conftest import setup_integration
 
 from tests.common import MockConfigEntry
 
 
-async def test_unload_entry(hass):
+async def test_unload_entry(hass, mock_account):
     """Test being able to unload an entry."""
-    entry = MockConfigEntry(
-        domain=litterrobot.DOMAIN,
-        data=CONFIG[litterrobot.DOMAIN],
-    )
-    entry.add_to_hass(hass)
+    entry = await setup_integration(hass, mock_account)
 
-    assert await async_setup_component(hass, litterrobot.DOMAIN, {}) is True
-    assert await litterrobot.async_unload_entry(hass, entry)
+    assert await hass.config_entries.async_unload(entry.entry_id)
+    await hass.async_block_till_done()
     assert hass.data[litterrobot.DOMAIN] == {}
 
 
-async def test_not_ready(hass):
-    """Test being able to handle config entry not ready."""
+@pytest.mark.parametrize(
+    "exception",
+    (LitterRobotLoginException, LitterRobotException),
+)
+async def test_entry_not_setup(hass, exception):
+    """Test being able to handle config entry not setup."""
     entry = MockConfigEntry(
         domain=litterrobot.DOMAIN,
         data=CONFIG[litterrobot.DOMAIN],
@@ -33,7 +34,7 @@ async def test_not_ready(hass):
     entry.add_to_hass(hass)
 
     with patch(
-        "homeassistant.components.litterrobot.LitterRobotHub.login",
-        side_effect=LitterRobotException,
+        "pylitterbot.Account.connect",
+        side_effect=exception,
     ):
         assert await hass.config_entries.async_setup(entry.entry_id) is False
