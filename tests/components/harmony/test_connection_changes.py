68223d606d720e63ab91a55cb2fe35736417fb1e
Mike Keesey <michael.w.keesey@gmail.com>
2021-02-25 20:45:05 -0700

    Create shared fakeharmonyclient instance

--- tests/components/harmony/test_connection_changes.py
+++ tests/components/harmony/test_connection_changes.py
@@ -17,7 +17,9 @@ from .const import ENTITY_PLAY_MUSIC, ENTITY_REMOTE, ENTITY_WATCH_TV, HUB_NAME
 from tests.common import MockConfigEntry, async_fire_time_changed
 
 
-async def test_connection_state_changes(mock_hc, hass, mock_write_config):
+async def test_connection_state_changes(
+    harmony_client, mock_hc, hass, mock_write_config
+):
     """Ensure connection changes are reflected in the switch states."""
     entry = MockConfigEntry(
         domain=DOMAIN, data={CONF_HOST: "192.0.2.0", CONF_NAME: HUB_NAME}
@@ -27,14 +29,12 @@ async def test_connection_state_changes(mock_hc, hass, mock_write_config):
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
-    data = hass.data[DOMAIN][entry.entry_id]
-
     # mocks start with current activity == Watch TV
     assert hass.states.is_state(ENTITY_REMOTE, STATE_ON)
     assert hass.states.is_state(ENTITY_WATCH_TV, STATE_ON)
     assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_OFF)
 
-    data._disconnected()
+    harmony_client.mock_disconnection()
     await hass.async_block_till_done()
 
     # Entities do not immediately show as unavailable
@@ -49,15 +49,15 @@ async def test_connection_state_changes(mock_hc, hass, mock_write_config):
     assert hass.states.is_state(ENTITY_WATCH_TV, STATE_UNAVAILABLE)
     assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_UNAVAILABLE)
 
-    data._connected()
+    harmony_client.mock_reconnection()
     await hass.async_block_till_done()
 
     assert hass.states.is_state(ENTITY_REMOTE, STATE_ON)
     assert hass.states.is_state(ENTITY_WATCH_TV, STATE_ON)
     assert hass.states.is_state(ENTITY_PLAY_MUSIC, STATE_OFF)
 
-    data._disconnected()
-    data._connected()
+    harmony_client.mock_disconnection()
+    harmony_client.mock_reconnection()
     future_time = utcnow() + timedelta(seconds=10)
     async_fire_time_changed(hass, future_time)
 
