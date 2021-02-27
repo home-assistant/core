70e7e8f8304b52b1a28ab2415d043139f0bd527e
tomasz75 <tomek@sviete.pl>
2021-02-27 08:13:17 +0100

    fix service desc

--- homeassistant/components/ais_updater/scripts/release_script.sh
+++ homeassistant/components/ais_updater/scripts/release_script.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-echo "AIS release script starting for 21.02.19 on chanel 2" 
+echo "AIS release script starting for 21.02.03 on chanel 3" 
+echo "21.02.19" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt  
 
