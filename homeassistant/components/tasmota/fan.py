b22ddb433a537ae9629ceaf39b36d8b90cd4634c
Erik <erik@montnemery.com>
2021-02-28 17:18:14 +0100

    Tweak Tasmota fan typing

--- homeassistant/components/tasmota/fan.py
+++ homeassistant/components/tasmota/fan.py
@@ -1,7 +1,5 @@
 """Support for Tasmota fans."""
 
-from typing import Optional
-
 from hatasmota import const as tasmota_const
 
 from homeassistant.components import fan
@@ -59,7 +57,7 @@ class TasmotaFan(
         )
 
     @property
-    def speed_count(self) -> Optional[int]:
+    def speed_count(self) -> int:
         """Return the number of speeds the fan supports."""
         return len(ORDERED_NAMED_FAN_SPEEDS)
 
