d92c53fa8e39588196b25bace4f9d78fc232758f
Quentame <polletquentin74@me.com>
2021-03-02 21:43:59 +0100

    Uniformize platform setup (#47101)
    * A platform is not a component
    
    * Fix dynalite
    
    * SUPPORTED_PLATFORMS --> PLATFORMS
    
    * In tests
    
    * In tests 2
    
    * Fix SmartThings
    
    * Fix ZHA test
    
    * Fix Z-Wave
    
    * Revert Z-Wave
    
    * Use PLATFORMS const in ambient_station
    
    * Fix ihc comment

--- homeassistant/components/somfy_mylink/__init__.py
+++ homeassistant/components/somfy_mylink/__init__.py
@@ -23,7 +23,7 @@ from .const import (
     DEFAULT_PORT,
     DOMAIN,
     MYLINK_STATUS,
-    SOMFY_MYLINK_COMPONENTS,
+    PLATFORMS,
 )
 
 CONFIG_OPTIONS = (CONF_DEFAULT_REVERSE, CONF_ENTITY_CONFIG)
@@ -121,9 +121,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         UNDO_UPDATE_LISTENER: undo_listener,
     }
 
-    for component in SOMFY_MYLINK_COMPONENTS:
+    for platform in PLATFORMS:
         hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
+            hass.config_entries.async_forward_entry_setup(entry, platform)
         )
 
     return True
@@ -182,8 +182,8 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     unload_ok = all(
         await asyncio.gather(
             *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in SOMFY_MYLINK_COMPONENTS
+                hass.config_entries.async_forward_entry_unload(entry, platform)
+                for platform in PLATFORMS
             ]
         )
     )
