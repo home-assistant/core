55e528ad4beadedaf0465263ff7d76e090094d01
Chris <firstof9@gmail.com>
2021-03-03 16:32:37 -0700

    Add LZW36 device schema to zwave_js discovery (#47314)
    * Add LZW26 device schema to discovery
    
    Co-authored-by: @kpine
    
    * Update homeassistant/components/zwave_js/discovery.py
    
    Co-authored-by: kpine <keith.pine@gmail.com>
    
    * Add tests
    
    * Fix test
    
    Co-authored-by: kpine <keith.pine@gmail.com>

--- tests/components/zwave_js/test_discovery.py
+++ tests/components/zwave_js/test_discovery.py
@@ -23,3 +23,15 @@ async def test_ge_12730(hass, client, ge_12730, integration):
 
     state = hass.states.get("fan.in_wall_smart_fan_control")
     assert state
+
+
+async def test_inovelli_lzw36(hass, client, inovelli_lzw36, integration):
+    """Test LZW36 Fan Controller multilevel switch endpoint 2 is discovered as a fan."""
+    node = inovelli_lzw36
+    assert node.device_class.specific.label == "Unused"
+
+    state = hass.states.get("light.family_room_combo")
+    assert state.state == "off"
+
+    state = hass.states.get("fan.family_room_combo_2")
+    assert state
