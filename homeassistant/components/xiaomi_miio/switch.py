b58f9ce33aacd4aec41f1d5f37dcd09fc98003b3
starkillerOG <starkiller.og@gmail.com>
2021-03-04 21:53:09 +0100

    Fix Xiaomi Miio setup of switch entity for lumi.acpartner.v3 (#47345)

--- homeassistant/components/xiaomi_miio/switch.py
+++ homeassistant/components/xiaomi_miio/switch.py
@@ -21,6 +21,7 @@ import homeassistant.helpers.config_validation as cv
 from .const import (
     CONF_DEVICE,
     CONF_FLOW_TYPE,
+    CONF_GATEWAY,
     CONF_MODEL,
     DOMAIN,
     SERVICE_SET_POWER_MODE,
@@ -128,15 +129,18 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the switch from a config entry."""
     entities = []
 
-    if config_entry.data[CONF_FLOW_TYPE] == CONF_DEVICE:
+    host = config_entry.data[CONF_HOST]
+    token = config_entry.data[CONF_TOKEN]
+    name = config_entry.title
+    model = config_entry.data[CONF_MODEL]
+    unique_id = config_entry.unique_id
+
+    if config_entry.data[CONF_FLOW_TYPE] == CONF_DEVICE or (
+        config_entry.data[CONF_FLOW_TYPE] == CONF_GATEWAY
+        and model == "lumi.acpartner.v3"
+    ):
         if DATA_KEY not in hass.data:
             hass.data[DATA_KEY] = {}
-
-        host = config_entry.data[CONF_HOST]
-        token = config_entry.data[CONF_TOKEN]
-        name = config_entry.title
-        model = config_entry.data[CONF_MODEL]
-        unique_id = config_entry.unique_id
 
         _LOGGER.debug("Initializing with host %s (token %s...)", host, token[:5])
 
