6472460803818d007343327440df1f37568e1b91
lewei50 <service@lewei50.com>
2021-03-04 08:18:14 +0800

    Update homeassistant/components/iammeter/__init__.py
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/iammeter/__init__.py
+++ homeassistant/components/iammeter/__init__.py
@@ -20,9 +20,5 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[hass.config_entries.async_forward_entry_unload(entry, "sensor")]
-        )
-    )
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     return unload_ok
