e629ab91d684879d2512f6ad83c9ead7517848d0
Nick Touran <nick@partofthething.com>
2021-03-07 11:38:39 -0800

    Add turn on/off capabilities to VLC.
    The VLC component previously made a single instance that would connect
    to the sound backend once and for all. However, if the backend was
    restarted for any reason (e.g. pulseaudio work on the server) then any
    further action on the VLC component would result in an error like:
    
      vlcpulse audio output error: stream connection failure: Bad state
    
    By adding turn_on/turn_off services that release the instance and allow
    its recreation, we can begin working around these situations, at least
    by 'power cycling' the component.

--- homeassistant/components/vlc/media_player.py
+++ homeassistant/components/vlc/media_player.py
@@ -1,5 +1,6 @@
 """Provide functionality to interact with vlc devices on the network."""
 import logging
+from functools import wraps
 
 import vlc
 import voluptuous as vol
@@ -13,8 +14,16 @@ from homeassistant.components.media_player.const import (
     SUPPORT_STOP,
     SUPPORT_VOLUME_MUTE,
     SUPPORT_VOLUME_SET,
+    SUPPORT_TURN_ON,
+    SUPPORT_TURN_OFF,
 )
-from homeassistant.const import CONF_NAME, STATE_IDLE, STATE_PAUSED, STATE_PLAYING
+from homeassistant.const import (
+    CONF_NAME,
+    STATE_IDLE,
+    STATE_PAUSED,
+    STATE_PLAYING,
+    STATE_OFF,
+)
 import homeassistant.helpers.config_validation as cv
 import homeassistant.util.dt as dt_util
 
@@ -30,6 +39,8 @@ SUPPORT_VLC = (
     | SUPPORT_PLAY_MEDIA
     | SUPPORT_PLAY
     | SUPPORT_STOP
+    | SUPPORT_TURN_ON
+    | SUPPORT_TURN_OFF
 )
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
@@ -47,13 +58,41 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     )
 
 
+def cmd(func):
+    """Catch command exceptions."""
+
+    @wraps(func)
+    def wrapper(obj, *args, **kwargs):
+        """Wrap all command methods."""
+        try:
+            func(obj, *args, **kwargs)
+        except AttributeError as exc:
+            # If VLC is off, we expect calls to fail.
+            if obj.state == STATE_OFF:
+                _LOGGER.info(
+                    "Cannot call %s on entity %s because it is turned off: %r",
+                    func.__name__,
+                    obj.entity_id,
+                    exc,
+                )
+            else:
+                _LOGGER.error(
+                    "Error calling %s on entity %s: %r",
+                    func.__name__,
+                    obj.entity_id,
+                    exc,
+                )
+
+    return wrapper
+
+
 class VlcDevice(MediaPlayerEntity):
     """Representation of a vlc player."""
 
     def __init__(self, name, arguments):
         """Initialize the vlc device."""
-        self._instance = vlc.Instance(arguments)
-        self._vlc = self._instance.media_player_new()
+        self._instance = None
+        self._vlc = None
         self._name = name
         self._volume = None
         self._muted = None
@@ -61,24 +100,45 @@ class VlcDevice(MediaPlayerEntity):
         self._media_position_updated_at = None
         self._media_position = None
         self._media_duration = None
+        self._arguments = arguments
+        self.turn_on()
+
+    @property
+    def _vlc_is_off(self):
+        return self._vlc is None
+
+    def turn_on(self):
+        """Create a new instance of the vlc client."""
+        self._instance = vlc.Instance(self._arguments)
+        self._vlc = self._instance.media_player_new()
+
+    def turn_off(self):
+        """Turn off and destroy the current vlc client."""
+        self._instance.release()
+        self._instance = None
+        self._vlc = None
 
     def update(self):
         """Get the latest details from the device."""
-        status = self._vlc.get_state()
-        if status == vlc.State.Playing:
-            self._state = STATE_PLAYING
-        elif status == vlc.State.Paused:
-            self._state = STATE_PAUSED
+        if self._vlc_is_off:
+            self._state = STATE_OFF
+            self._media_duration = None
+            self._media_position = None
         else:
-            self._state = STATE_IDLE
-        self._media_duration = self._vlc.get_length() / 1000
-        position = self._vlc.get_position() * self._media_duration
-        if position != self._media_position:
-            self._media_position_updated_at = dt_util.utcnow()
-            self._media_position = position
+            status = self._vlc.get_state()
+            if status == vlc.State.Playing:
+                self._state = STATE_PLAYING
+            elif status == vlc.State.Paused:
+                self._state = STATE_PAUSED
+            else:
+                self._state = STATE_IDLE
+            self._media_duration = self._vlc.get_length() / 1000
+            position = self._vlc.get_position() * self._media_duration
+            if position != self._media_position:
+                self._media_position_updated_at = dt_util.utcnow()
 
-        self._volume = self._vlc.audio_get_volume() / 100
-        self._muted = self._vlc.audio_get_mute() == 1
+            self._volume = self._vlc.audio_get_volume() / 100
+            self._muted = self._vlc.audio_get_mute() == 1
 
         return True
 
@@ -127,36 +187,43 @@ class VlcDevice(MediaPlayerEntity):
         """When was the position of the current playing media valid."""
         return self._media_position_updated_at
 
+    @cmd
     def media_seek(self, position):
         """Seek the media to a specific location."""
         track_length = self._vlc.get_length() / 1000
         self._vlc.set_position(position / track_length)
 
+    @cmd
     def mute_volume(self, mute):
         """Mute the volume."""
         self._vlc.audio_set_mute(mute)
         self._muted = mute
 
+    @cmd
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
         self._vlc.audio_set_volume(int(volume * 100))
         self._volume = volume
 
+    @cmd
     def media_play(self):
         """Send play command."""
         self._vlc.play()
         self._state = STATE_PLAYING
 
+    @cmd
     def media_pause(self):
         """Send pause command."""
         self._vlc.pause()
         self._state = STATE_PAUSED
 
+    @cmd
     def media_stop(self):
         """Send stop command."""
         self._vlc.stop()
         self._state = STATE_IDLE
 
+    @cmd
     def play_media(self, media_type, media_id, **kwargs):
         """Play media from a URL or file."""
         if not media_type == MEDIA_TYPE_MUSIC:
