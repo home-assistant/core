aef98daa411861757ccde6bcc17f9e10268193a0
Oliver <scarface-4711@users.noreply.github.com>
2021-03-08 22:49:54 +0100

    Update attrs to 20.3.0 (#47642)

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ REQUIRES = [
     "aiohttp==3.7.4",
     "astral==1.10.1",
     "async_timeout==3.0.1",
-    "attrs==19.3.0",
+    "attrs==20.3.0",
     "awesomeversion==21.2.3",
     "bcrypt==3.1.7",
     "certifi>=2020.12.5",
