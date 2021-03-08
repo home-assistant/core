2242d5c73241c8a8e0778ed3164fe6a4e9bc8a8e
Erik Montnemery <erik@montnemery.com>
2021-03-08 22:48:36 +0100

    Include changed variables in automation trace (#47549)
    * Include changed variables in automation trace
    
    * Deduplicate some code
    
    * Tweak
    
    * Apply suggestions from code review
    
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>
    
    * Fix format
    
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/helpers/trace.py
+++ homeassistant/helpers/trace.py
@@ -16,7 +16,17 @@ class TraceElement:
         self._error: Optional[Exception] = None
         self._result: Optional[dict] = None
         self._timestamp = dt_util.utcnow()
-        self._variables = variables
+
+        if variables is None:
+            variables = {}
+        last_variables = variables_cv.get() or {}
+        variables_cv.set(dict(variables))
+        changed_variables = {
+            key: value
+            for key, value in variables.items()
+            if key not in last_variables or last_variables[key] != value
+        }
+        self._variables = changed_variables
 
     def __repr__(self) -> str:
         """Container for trace data."""
@@ -33,8 +43,8 @@ class TraceElement:
     def as_dict(self) -> Dict[str, Any]:
         """Return dictionary version of this TraceElement."""
         result: Dict[str, Any] = {"timestamp": self._timestamp}
-        # Commented out because we get too many copies of the same data
-        # result["variables"] = self._variables
+        if self._variables:
+            result["changed_variables"] = self._variables
         if self._error is not None:
             result["error"] = str(self._error)
         if self._result is not None:
@@ -55,6 +65,8 @@ trace_stack_cv: ContextVar[Optional[List[TraceElement]]] = ContextVar(
 trace_path_stack_cv: ContextVar[Optional[List[str]]] = ContextVar(
     "trace_path_stack_cv", default=None
 )
+# Copy of last variables
+variables_cv: ContextVar[Optional[Any]] = ContextVar("variables_cv", default=None)
 
 
 def trace_stack_push(trace_stack_var: ContextVar, node: Any) -> None:
@@ -128,6 +140,7 @@ def trace_clear() -> None:
     trace_cv.set({})
     trace_stack_cv.set(None)
     trace_path_stack_cv.set(None)
+    variables_cv.set(None)
 
 
 def trace_set_result(**kwargs: Any) -> None:
