784f37ac6b55650a663417f3a0ca515a27e1401d
Shay Levy <levyshay1@gmail.com>
2021-03-09 14:45:58 +0200

    Revert Shelly temperature sensor name change (#47664)

--- homeassistant/components/shelly/sensor.py
+++ homeassistant/components/shelly/sensor.py
@@ -133,7 +133,7 @@ SENSORS = {
         available=lambda block: block.sensorOp == "normal",
     ),
     ("sensor", "extTemp"): BlockAttributeDescription(
-        name="External Temperature",
+        name="Temperature",
         unit=temperature_unit,
         value=lambda value: round(value, 1),
         device_class=sensor.DEVICE_CLASS_TEMPERATURE,
