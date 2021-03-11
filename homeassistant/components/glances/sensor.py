a0e028978031792840c624767bbf3627a598e094
TheNetAdmin <zxwang42@gmail.com>
2021-03-11 13:07:08 -0800

    Revert long-line if statement

--- homeassistant/components/glances/sensor.py
+++ homeassistant/components/glances/sensor.py
@@ -174,8 +174,9 @@ class GlancesSensor(Entity):
                             self._state = sensor["value"]
             elif self.type == "temperature_hdd":
                 for sensor in value["sensors"]:
-                    if sensor["type"] == "temperature_hdd" and sensor["label"] == self._sensor_name_prefix:
-                        self._state = sensor["value"]
+                    if sensor["type"] == "temperature_hdd":
+                        if sensor["label"] == self._sensor_name_prefix:
+                            self._state = sensor["value"]
             elif self.type == "memory_use_percent":
                 self._state = value["mem"]["percent"]
             elif self.type == "memory_use":
