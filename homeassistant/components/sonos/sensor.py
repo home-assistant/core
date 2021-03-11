bed49e81bf684d3be1debe62ea4955d4f1b9ad64
Walter Huf <hufman@gmail.com>
2021-03-10 21:06:52 -0800

    Incorporate feedback

--- homeassistant/components/sonos/sensor.py
+++ homeassistant/components/sonos/sensor.py
@@ -1,18 +1,21 @@
 """Entity representing a Sonos Move battery level."""
 import datetime
-import logging
 from typing import TYPE_CHECKING, Any, Dict, Optional
 
 from pysonos.core import SoCo
 from pysonos.exceptions import SoCoException
 
+from homeassistant.const import (
+    DEVICE_CLASS_BATTERY,
+    PERCENTAGE,
+    STATE_UNKNOWN,
+)
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.icon import icon_for_battery_level
 
 if TYPE_CHECKING:  # Avoid recursive import loops
     from .media_player import SonosEntity
 
-_LOGGER = logging.getLogger(__name__)
 SCAN_INTERVAL = datetime.timedelta(seconds=10)
 
 ATTR_BATTERY_LEVEL = "battery_level"
@@ -31,7 +34,6 @@ class SonosBatteryEntity(Entity):
         self._parent = parent
         self._soco = soco
         self._battery_info = battery_info
-        self._timer = None
 
     @staticmethod
     def fetch(soco: SoCo) -> Optional[Dict[str, Any]]:
@@ -48,21 +50,21 @@ class SonosBatteryEntity(Entity):
 
     async def async_added_to_hass(self) -> None:
         """Register polling callback when added to hass."""
-        if self._timer is None:
-            self._timer = self._parent.hass.helpers.event.async_track_time_interval(
-                self.update, SCAN_INTERVAL
-            )
+        cancel_timer = self._parent.hass.helpers.event.async_track_time_interval(
+            self.update, SCAN_INTERVAL
+        )
+        self.async_on_remove(cancel_timer)
 
     # Identification of this Entity
     @property
     def unique_id(self) -> str:
         """Return the unique ID of the sensor."""
-        return self._parent.unique_id + "-battery"
+        return f"{self._parent.unique_id}-battery"
 
     @property
     def name(self) -> str:
         """Return the name of the sensor."""
-        return self._parent.name + " Battery"
+        return f"{self._parent.name} Battery"
 
     @property
     def device_info(self) -> Dict[str, Any]:
@@ -70,20 +72,25 @@ class SonosBatteryEntity(Entity):
         return self._parent.device_info
 
     @property
+    def device_class(self) -> str:
+        """Return the entity's device class."""
+        return DEVICE_CLASS_BATTERY
+
+    @property
     def unit_of_measurement(self) -> str:
         """Get the unit of measurement."""
-        return "%"
+        return PERCENTAGE
 
     # Update the current state
     def update(self, event=None):
         """Poll the device for the current state."""
-        if self.available:
-            # only poll if the Sonos device is online
-            _LOGGER.debug("Starting to check battery_info on %s", self._soco)
-            battery_info = SonosBatteryEntity.fetch(self._soco)
-            if battery_info is not None:
-                self._battery_info = battery_info
-                self.schedule_update_ha_state()
+        if not self.available:
+            # wait for the Sonos device to come back online
+            return
+        battery_info = SonosBatteryEntity.fetch(self._soco)
+        if battery_info is not None:
+            self._battery_info = battery_info
+            self.schedule_update_ha_state()
 
     # Current state
     @property
@@ -102,12 +109,12 @@ class SonosBatteryEntity(Entity):
 
         Observed to be either BATTERY or SONOS_CHARGING_RING or USB_POWER.
         """
-        return self._battery_info.get("PowerSource", "Unknown")
+        return self._battery_info.get("PowerSource", STATE_UNKNOWN)
 
     @property
     def charging(self) -> bool:
         """Return the charging status of this battery."""
-        return self.power_source != "BATTERY"
+        return self.power_source not in ("BATTERY", STATE_UNKNOWN)
 
     @property
     def icon(self) -> str:
@@ -115,16 +122,14 @@ class SonosBatteryEntity(Entity):
         return icon_for_battery_level(self.battery_level, self.charging)
 
     @property
-    def state(self) -> int:
+    def state(self) -> Optional[int]:
         """Return the state of the sensor."""
         return self._battery_info.get("Level")
 
     @property
     def device_state_attributes(self) -> Dict[str, Any]:
         """Return entity specific state attributes."""
-        attributes = {
+        return {
             ATTR_BATTERY_CHARGING: self.charging,
-            ATTR_BATTERY_LEVEL: self.battery_level,
             ATTR_BATTERY_POWERSOURCE: self.power_source,
         }
-        return attributes
