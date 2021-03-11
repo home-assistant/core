7ca5e969cc874258aa8cba2570818d269f3e2cd6
Chris <firstof9@gmail.com>
2021-03-11 16:28:38 -0700

    Fix zwave_js target_temp_low (#47762)

--- homeassistant/components/zwave_js/climate.py
+++ homeassistant/components/zwave_js/climate.py
@@ -287,7 +287,12 @@ class ZWaveClimate(ZWaveBaseEntity, ClimateEntity):
     @property
     def target_temperature_low(self) -> Optional[float]:
         """Return the lowbound target temperature we try to reach."""
-        return self.target_temperature
+        if self._current_mode and self._current_mode.value is None:
+            # guard missing value
+            return None
+        if len(self._current_mode_setpoint_enums) > 1:
+            return self.target_temperature
+        return None
 
     @property
     def preset_mode(self) -> Optional[str]:
