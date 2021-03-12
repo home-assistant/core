6f0adb7ab694b64834853e9e5214e2c81a6c66ec
Tobias Sauerwein <cgtobi@users.noreply.github.com>
2021-03-12 21:57:02 +0100

    Fix Netatmo event handling (#47792)

--- homeassistant/components/netatmo/webhook.py
+++ homeassistant/components/netatmo/webhook.py
@@ -76,16 +76,18 @@ def async_send_event(hass, event_type, data):
         {"type": event_type, "data": data},
     )
 
-    if event_type not in EVENT_ID_MAP:
-        return
+    event_data = {
+        "type": event_type,
+        "data": data,
+    }
 
-    data_device_id = data[EVENT_ID_MAP[event_type]]
+    if event_type in EVENT_ID_MAP:
+        data_device_id = data[EVENT_ID_MAP[event_type]]
+        event_data[ATTR_DEVICE_ID] = hass.data[DOMAIN][DATA_DEVICE_IDS].get(
+            data_device_id
+        )
 
     hass.bus.async_fire(
         event_type=NETATMO_EVENT,
-        event_data={
-            "type": event_type,
-            "data": data,
-            ATTR_DEVICE_ID: hass.data[DOMAIN][DATA_DEVICE_IDS].get(data_device_id),
-        },
+        event_data=event_data,
     )
