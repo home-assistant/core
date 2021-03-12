3b20b9d8644beec5c322020c682310b785192466
MatsNl <37705266+MatsNl@users.noreply.github.com>
2021-03-12 07:15:45 +0100

    Improve Atag integration and bump version to 0.3.5.3 (#47778)
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/atag/climate.py
+++ homeassistant/components/atag/climate.py
@@ -16,16 +16,14 @@ from homeassistant.const import ATTR_TEMPERATURE
 
 from . import CLIMATE, DOMAIN, AtagEntity
 
-PRESET_SCHEDULE = "Auto"
-PRESET_MANUAL = "Manual"
-PRESET_EXTEND = "Extend"
-SUPPORT_PRESET = [
-    PRESET_MANUAL,
-    PRESET_SCHEDULE,
-    PRESET_EXTEND,
-    PRESET_AWAY,
-    PRESET_BOOST,
-]
+PRESET_MAP = {
+    "Manual": "manual",
+    "Auto": "automatic",
+    "Extend": "extend",
+    PRESET_AWAY: "vacation",
+    PRESET_BOOST: "fireplace",
+}
+PRESET_INVERTED = {v: k for k, v in PRESET_MAP.items()}
 SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_PRESET_MODE
 HVAC_MODES = [HVAC_MODE_AUTO, HVAC_MODE_HEAT]
 
@@ -47,8 +45,8 @@ class AtagThermostat(AtagEntity, ClimateEntity):
     @property
     def hvac_mode(self) -> Optional[str]:
         """Return hvac operation ie. heat, cool mode."""
-        if self.coordinator.atag.climate.hvac_mode in HVAC_MODES:
-            return self.coordinator.atag.climate.hvac_mode
+        if self.coordinator.data.climate.hvac_mode in HVAC_MODES:
+            return self.coordinator.data.climate.hvac_mode
         return None
 
     @property
@@ -59,46 +57,46 @@ class AtagThermostat(AtagEntity, ClimateEntity):
     @property
     def hvac_action(self) -> Optional[str]:
         """Return the current running hvac operation."""
-        if self.coordinator.atag.climate.status:
-            return CURRENT_HVAC_HEAT
-        return CURRENT_HVAC_IDLE
+        is_active = self.coordinator.data.climate.status
+        return CURRENT_HVAC_HEAT if is_active else CURRENT_HVAC_IDLE
 
     @property
-    def temperature_unit(self):
+    def temperature_unit(self) -> Optional[str]:
         """Return the unit of measurement."""
-        return self.coordinator.atag.climate.temp_unit
+        return self.coordinator.data.climate.temp_unit
 
     @property
     def current_temperature(self) -> Optional[float]:
         """Return the current temperature."""
-        return self.coordinator.atag.climate.temperature
+        return self.coordinator.data.climate.temperature
 
     @property
     def target_temperature(self) -> Optional[float]:
         """Return the temperature we try to reach."""
-        return self.coordinator.atag.climate.target_temperature
+        return self.coordinator.data.climate.target_temperature
 
     @property
     def preset_mode(self) -> Optional[str]:
         """Return the current preset mode, e.g., auto, manual, fireplace, extend, etc."""
-        return self.coordinator.atag.climate.preset_mode
+        preset = self.coordinator.data.climate.preset_mode
+        return PRESET_INVERTED.get(preset)
 
     @property
     def preset_modes(self) -> Optional[List[str]]:
         """Return a list of available preset modes."""
-        return SUPPORT_PRESET
+        return list(PRESET_MAP.keys())
 
     async def async_set_temperature(self, **kwargs) -> None:
         """Set new target temperature."""
-        await self.coordinator.atag.climate.set_temp(kwargs.get(ATTR_TEMPERATURE))
+        await self.coordinator.data.climate.set_temp(kwargs.get(ATTR_TEMPERATURE))
         self.async_write_ha_state()
 
     async def async_set_hvac_mode(self, hvac_mode: str) -> None:
         """Set new target hvac mode."""
-        await self.coordinator.atag.climate.set_hvac_mode(hvac_mode)
+        await self.coordinator.data.climate.set_hvac_mode(hvac_mode)
         self.async_write_ha_state()
 
     async def async_set_preset_mode(self, preset_mode: str) -> None:
         """Set new preset mode."""
-        await self.coordinator.atag.climate.set_preset_mode(preset_mode)
+        await self.coordinator.data.climate.set_preset_mode(PRESET_MAP[preset_mode])
         self.async_write_ha_state()
