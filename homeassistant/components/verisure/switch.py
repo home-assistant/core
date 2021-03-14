90f61241dae056a51a0d56c082d5d8837aa2ffd6
Franck Nijhof <git@frenck.dev>
2021-03-14 19:56:45 +0100

    Add devices to Verisure integration

--- homeassistant/components/verisure/switch.py
+++ homeassistant/components/verisure/switch.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 from time import monotonic
-from typing import Callable, Iterable
+from typing import Any, Callable, Iterable
 
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
@@ -10,7 +10,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DOMAIN
+from .const import CONF_GIID, DOMAIN
 from .coordinator import VerisureDataUpdateCoordinator
 
 
@@ -50,6 +50,17 @@ class VerisureSmartplug(CoordinatorEntity, SwitchEntity):
     def unique_id(self) -> str:
         """Return the unique ID for this alarm control panel."""
         return self.serial_number
+
+    @property
+    def device_info(self) -> dict[str, Any]:
+        """Return device information about this entity."""
+        return {
+            "name": self.coordinator.data["smart_plugs"][self.serial_number]["area"],
+            "manufacturer": "Verisure",
+            "model": "SmartPlug",
+            "identifiers": {(DOMAIN, self.serial_number)},
+            "via_device": (DOMAIN, self.coordinator.entry.data[CONF_GIID]),
+        }
 
     @property
     def is_on(self) -> bool:
