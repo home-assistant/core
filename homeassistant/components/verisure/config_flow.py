5b490206064fcdf0ae37a7da298d2f45804e95dc
Franck Nijhof <git@frenck.dev>
2021-03-14 10:42:54 +0100

    Not Hue

--- homeassistant/components/verisure/config_flow.py
+++ homeassistant/components/verisure/config_flow.py
@@ -150,7 +150,7 @@ class VerisureOptionsFlowHandler(OptionsFlow):
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
     ) -> dict[str, Any]:
-        """Manage Hue options."""
+        """Manage Verisure options."""
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
