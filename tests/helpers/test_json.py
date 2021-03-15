cd41e698053f3573658d60a63b85a4a785d2af87
Erik <erik@montnemery.com>
2021-03-15 09:05:10 +0100

    Teach JSONEncoder how to serialize timedelta

--- tests/helpers/test_json.py
+++ tests/helpers/test_json.py
@@ -1,4 +1,6 @@
 """Test Home Assistant remote methods and classes."""
+from datetime import datetime, timedelta, timezone
+
 import pytest
 
 from homeassistant import core
@@ -11,6 +13,27 @@ def test_json_encoder(hass):
     ha_json_enc = JSONEncoder()
     state = core.State("test.test", "hello")
 
+    # Test serializing a datetime
+    data = datetime(2011, 11, 4, 0, 5, 23, 283000, tzinfo=timezone.utc)
+    assert ha_json_enc.default(data) == data.isoformat()
+
+    # Test serializing a timedelta
+    data = timedelta(
+        days=50,
+        seconds=27,
+        microseconds=10,
+        milliseconds=29000,
+        minutes=5,
+        hours=8,
+        weeks=2,
+    )
+    assert ha_json_enc.default(data) == str(data)
+
+    # Test serializing a set()
+    data = {"milk", "beer"}
+    assert sorted(ha_json_enc.default(data)) == sorted(list(data))
+
+    # Test serializong object which implements as_dict
     assert ha_json_enc.default(state) == state.as_dict()
 
     # Default method raises TypeError if non HA object
