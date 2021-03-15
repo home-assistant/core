09f46f09d1fc69104ce5dcf3695dc318ea8237cb
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2021-03-15 00:14:16 -0700

    Update homeassistant/components/screenlogic/config_flow.py
    Remove unnecessary debug logging.
    
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/screenlogic/config_flow.py
+++ homeassistant/components/screenlogic/config_flow.py
@@ -214,7 +214,6 @@ class ScreenLogicOptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry):
         """Init the screen logic options flow."""
-        _LOGGER.debug(config_entry)
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
