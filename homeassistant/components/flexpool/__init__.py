3b0e840a6824690f50ee4f7dcd0d88978a737279
DatDraggy <spamme@kieran.de>
2021-03-15 20:52:55 +0100

    Implement flexpool as an integration

---
+++ homeassistant/components/flexpool/__init__.py
@@ -0,0 +1,1 @@
+"""The flexpool component."""
---
+++ homeassistant/components/flexpool/const.py
@@ -0,0 +1,54 @@
+"""Constants for the Flexpool integration."""
+SENSOR_DICT = {
+    "flexpool_unpaid_balance": [
+        "Flexpool Unpaid Balance",
+        "mdi:wallet",
+    ],
+    "flexpool_current_reported": [
+        "Flexpool Current Reported Hashrate",
+        "mdi:pound",
+        "reported",
+    ],
+    "flexpool_current_effective": [
+        "Flexpool Current Effective Hashrate",
+        "mdi:pound",
+        "effective",
+    ],
+    "flexpool_daily_average": [
+        "Flexpool Daily Average Hashrate",
+        "mdi:pound",
+        "average",
+    ],
+    "flexpool_worker_reported": [
+        "Flexpool Worker Reported Hashrate",
+        "mdi:pound",
+        "reported",
+    ],
+    "flexpool_worker_effective": [
+        "Flexpool Worker Effective Hashrate",
+        "mdi:pound",
+        "effective",
+    ],
+    "flexpool_worker_daily_valid": [
+        "Flexpool Worker Daily Valid Shares",
+        "mdi:share-variant",
+        "valid",
+    ],
+    "flexpool_worker_daily_total": [
+        "Flexpool Worker Daily Valid Shares",
+        "mdi:share-variant",
+        "total",
+    ],
+    "flexpool_effective": [
+        "Flexpool Hashrate",
+        "mdi:pound",
+    ],
+    "flexpool_workers": [
+        "Flexpool Workers",
+        "mdi:pickaxe",
+    ],
+    "flexpool_luck": [
+        "Flexpool Current Luck",
+        "mdi:clover",
+    ],
+}
