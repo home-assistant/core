774af6f48da3d8f1091617e669f1d2503d68dab1
Erik <erik@montnemery.com>
2021-03-15 17:22:49 +0100

    Tweak

--- homeassistant/components/automation/trace.py
+++ homeassistant/components/automation/trace.py
@@ -224,4 +224,4 @@ class TraceJSONEncoder(json.JSONEncoder):
         if hasattr(o, "as_dict"):
             return o.as_dict()
 
-        return {"type": str(type(o)), "repr": repr(o)}
+        return {"__type": str(type(o)), "repr": repr(o)}
