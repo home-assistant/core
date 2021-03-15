0e4ebc311d8c8952508fc13093770c01061c287f
Rob Bierbooms <mail@robbierbooms.nl>
2021-03-15 22:29:02 +0000

    Add tests for option flow

--- tests/components/buienradar/test_config_flow.py
+++ tests/components/buienradar/test_config_flow.py
@@ -1,7 +1,7 @@
 """Test the buienradar2 config flow."""
 from unittest.mock import patch
 
-from homeassistant import config_entries
+from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.buienradar.const import DOMAIN
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
 
@@ -94,3 +94,37 @@ async def test_import_camera(hass):
 
     assert result["type"] == "abort"
     assert result["reason"] == "already_configured"
+
+
+async def test_options_flow(hass):
+    """Test options flow."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            CONF_LATITUDE: TEST_LATITUDE,
+            CONF_LONGITUDE: TEST_LONGITUDE,
+        },
+        unique_id=DOMAIN,
+    )
+    entry.add_to_hass(hass)
+
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+
+    assert result["type"] == "form"
+    assert result["step_id"] == "init"
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"],
+        user_input={"country_code": "BE", "delta": 450, "timeframe": 30},
+    )
+
+    with patch(
+        "homeassistant.components.buienradar.async_setup_entry", return_value=True
+    ), patch(
+        "homeassistant.components.buienradar.async_unload_entry", return_value=True
+    ):
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+
+        await hass.async_block_till_done()
+
+    assert entry.options == {"country_code": "BE", "delta": 450, "timeframe": 30}
