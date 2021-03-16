09335430f445e1605f66b39fe0319ab3d0a5c1f9
Paulus Schoutsen <balloob@gmail.com>
2021-03-16 18:02:45 +0000

    Just include context_id instead of run_id

--- homeassistant/components/automation/logbook.py
+++ homeassistant/components/automation/logbook.py
@@ -1,10 +1,9 @@
 """Describe logbook events."""
 from homeassistant.components.logbook import LazyEventPartialState
-from homeassistant.const import ATTR_ENTITY_ID, ATTR_NAME, CONF_ID
+from homeassistant.const import ATTR_ENTITY_ID, ATTR_NAME
 from homeassistant.core import HomeAssistant, callback
 
 from . import ATTR_SOURCE, DOMAIN, EVENT_AUTOMATION_TRIGGERED
-from .trace import DATA_AUTOMATION_TRACE
 
 
 @callback
@@ -19,24 +18,12 @@ def async_describe_events(hass: HomeAssistant, async_describe_event):  # type: i
         if ATTR_SOURCE in data:
             message = f"{message} by {data[ATTR_SOURCE]}"
 
-        run_id = None
-        if (
-            (entity_id := data.get(ATTR_ENTITY_ID))
-            and (cur_state := hass.states.get(entity_id))
-            and (automation_id := cur_state.attributes.get(CONF_ID))
-            and automation_id in hass.data[DATA_AUTOMATION_TRACE]
-        ):
-            for trace in hass.data[DATA_AUTOMATION_TRACE][automation_id].values():
-                if trace.context.id == event.context_id:
-                    run_id = trace.run_id
-                    break
-
         return {
             "name": data.get(ATTR_NAME),
             "message": message,
             "source": data.get(ATTR_SOURCE),
             "entity_id": data.get(ATTR_ENTITY_ID),
-            "run_id": run_id,
+            "context_id": event.context_id,
         }
 
     async_describe_event(
