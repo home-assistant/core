0577d6c59dd891da2d916cfbc0c50901fbea2de1
René Klomp <rene@klomp.ws>
2021-03-16 18:35:51 +0000

    Add tests for the sma config flow

---
+++ tests/components/sma/test_config_flow.py
@@ -0,0 +1,266 @@
+"""Test the sma config flow."""
+from unittest.mock import patch
+
+import aiohttp
+
+from homeassistant import setup
+from homeassistant.components.sma.const import DOMAIN
+from homeassistant.config_entries import SOURCE_IMPORT, SOURCE_USER
+from homeassistant.data_entry_flow import (
+    RESULT_TYPE_ABORT,
+    RESULT_TYPE_CREATE_ENTRY,
+    RESULT_TYPE_FORM,
+)
+
+from . import (
+    MOCK_CUSTOM_SENSOR,
+    MOCK_CUSTOM_SENSOR2,
+    MOCK_DEVICE,
+    MOCK_IMPORT,
+    MOCK_SETUP_DATA,
+    MOCK_USER_INPUT,
+    _patch_async_setup,
+    _patch_async_setup_entry,
+    _patch_validate_input,
+)
+
+
+async def test_form(hass, aioclient_mock):
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] == {}
+
+    aioclient_mock.get(
+        "https://1.1.1.1/data/l10n/en-US.json",
+        json={"461": MOCK_DEVICE["manufacturer"], "9402": MOCK_DEVICE["type"]},
+    )
+    aioclient_mock.post(
+        "https://1.1.1.1/dyn/login.json", json={"result": {"sid": "ABCD"}}
+    )
+    aioclient_mock.post("https://1.1.1.1/dyn/logout.json?sid=ABCD", json={})
+    aioclient_mock.post(
+        "https://1.1.1.1/dyn/getValues.json?sid=ABCD",
+        json={
+            "result": {
+                "0199-xxxxx385": {
+                    "6800_08822B00": {
+                        "1": [{"validVals": [461], "val": [{"tag": 461}]}]
+                    },
+                    "6800_00A21E00": {
+                        "1": [{"low": 0, "high": None, "val": MOCK_DEVICE["serial"]}]
+                    },
+                    "6800_08822000": {
+                        "1": [
+                            {
+                                "validVals": [9401, 9402, 9403, 9404, 9405],
+                                "val": [{"tag": 9402}],
+                            }
+                        ]
+                    },
+                    "6800_10821E00": {"1": [{"val": MOCK_DEVICE["name"]}]},
+                }
+            }
+        },
+    )
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        MOCK_USER_INPUT,
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["step_id"] == "sensors"
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            "add_custom": True,
+        },
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["step_id"] == "custom_sensor"
+    assert result["errors"] == {}
+
+    with _patch_async_setup() as mock_setup, _patch_async_setup_entry() as mock_setup_entry:
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], dict({"add_another": True}, **MOCK_CUSTOM_SENSOR)
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["step_id"] == "custom_sensor"
+    assert result["errors"] == {}
+
+    with _patch_async_setup() as mock_setup, _patch_async_setup_entry() as mock_setup_entry:
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], dict({"add_another": False}, **MOCK_CUSTOM_SENSOR2)
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] == RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == MOCK_USER_INPUT["host"]
+    assert result["data"] == MOCK_SETUP_DATA
+
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_form_cannot_connect(hass, aioclient_mock):
+    """Test we handle cannot connect error."""
+    aioclient_mock.get("https://1.1.1.1/data/l10n/en-US.json", exc=aiohttp.ClientError)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        MOCK_USER_INPUT,
+    )
+
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_invalid_auth(hass, aioclient_mock):
+    """Test we handle invalid auth error."""
+    aioclient_mock.get("https://1.1.1.1/data/l10n/en-US.json", json={})
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    with patch("pysma.SMA.new_session", return_value=False):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            MOCK_USER_INPUT,
+        )
+
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] == {"base": "invalid_auth"}
+
+
+async def test_form_cannot_retrieve_device_info(hass, aioclient_mock):
+    """Test we handle invalid auth error."""
+    aioclient_mock.get("https://1.1.1.1/data/l10n/en-US.json", json={})
+    aioclient_mock.get(
+        "https://1.1.1.1/dyn/getValues.json",
+        json={
+            "result": {
+                "0199-xxxxx385": {
+                    "6800_08822B00": {
+                        "1": [{"validVals": [461], "val": [{"tag": 461}]}]
+                    },
+                    "6800_00A21E00": {
+                        "1": [{"low": 0, "high": None, "val": 3006894981}]
+                    },
+                    "6800_08822000": {
+                        "1": [
+                            {
+                                "validVals": [9401, 9402, 9403, 9404, 9405],
+                                "val": [{"tag": 9402}],
+                            }
+                        ]
+                    },
+                    "6800_10821E00": {"1": [{"val": "SMA Device Name"}]},
+                }
+            }
+        },
+    )
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    with patch("pysma.SMA.new_session", return_value=True), patch(
+        "pysma.SMA.read", return_value=False
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            MOCK_USER_INPUT,
+        )
+
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] == {"base": "cannot_retrieve_device_info"}
+
+
+async def test_form_unexpected_exception(hass):
+    """Test we handle unexpected exception."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    with _patch_validate_input(side_effect=Exception):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            MOCK_USER_INPUT,
+        )
+
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] == {"base": "unknown"}
+
+
+async def test_form_already_configured(hass):
+    """Test starting a flow by user when already configured."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    with _patch_validate_input():
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            MOCK_USER_INPUT,
+        )
+
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["step_id"] == "sensors"
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            "add_custom": False,
+        },
+    )
+    assert result["type"] == RESULT_TYPE_CREATE_ENTRY
+    assert result["result"].unique_id == MOCK_DEVICE["serial"]
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    with _patch_validate_input():
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            MOCK_USER_INPUT,
+        )
+
+    assert result["type"] == RESULT_TYPE_ABORT
+    assert result["reason"] == "already_configured"
+
+
+async def test_import(hass):
+    """Test we can import."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+
+    with _patch_validate_input(), _patch_async_setup() as mock_setup, _patch_async_setup_entry() as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": SOURCE_IMPORT},
+            data=MOCK_IMPORT,
+        )
+
+    assert result["type"] == RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == MOCK_USER_INPUT["host"]
+    assert result["data"] == MOCK_IMPORT
+
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
