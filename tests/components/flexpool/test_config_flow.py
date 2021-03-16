03854da747afca86d5bc3e9fa4351fc5844fcc6b
DatDraggy <spamme@kieran.de>
2021-03-16 02:38:12 +0100

    Add test

--- tests/components/flexpool/test_config_flow.py
+++ tests/components/flexpool/test_config_flow.py
@@ -2,7 +2,6 @@
 from unittest.mock import patch
 
 from homeassistant import config_entries, setup
-from homeassistant.components.flexpool.config_flow import CannotConnect, InvalidAuth
 from homeassistant.components.flexpool.const import DOMAIN
 
 
@@ -16,9 +15,6 @@ async def test_form(hass):
     assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.flexpool.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
-    ), patch(
         "homeassistant.components.flexpool.async_setup", return_value=True
     ) as mock_setup, patch(
         "homeassistant.components.flexpool.async_setup_entry",
@@ -27,65 +23,15 @@ async def test_form(hass):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
+                "address": "0xf98bc863ad9d5dc6415360251ca6f793efc3c390",
             },
         )
         await hass.async_block_till_done()
 
     assert result2["type"] == "create_entry"
-    assert result2["title"] == "Name of the device"
+    assert result2["title"] == "0xf98bc863ad9d5dc6415360251ca6f793efc3c390"
     assert result2["data"] == {
-        "host": "1.1.1.1",
-        "username": "test-username",
-        "password": "test-password",
+        "address": "0xf98bc863ad9d5dc6415360251ca6f793efc3c390",
     }
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_invalid_auth(hass):
-    """Test we handle invalid auth."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.flexpool.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_auth"}
-
-
-async def test_form_cannot_connect(hass):
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.flexpool.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "cannot_connect"}
