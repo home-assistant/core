bef77ae805d21b6dd32345f5f84db6f5e9360828
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-03-17 18:01:13 +0100

    Update typing 05

--- homeassistant/components/device_tracker/device_trigger.py
+++ homeassistant/components/device_tracker/device_trigger.py
@@ -1,5 +1,5 @@
 """Provides device automations for Device Tracker."""
-from typing import List
+from __future__ import annotations
 
 import voluptuous as vol
 
@@ -32,7 +32,7 @@ TRIGGER_SCHEMA = TRIGGER_BASE_SCHEMA.extend(
 )
 
 
-async def async_get_triggers(hass: HomeAssistant, device_id: str) -> List[dict]:
+async def async_get_triggers(hass: HomeAssistant, device_id: str) -> list[dict]:
     """List device triggers for Device Tracker devices."""
     registry = await entity_registry.async_get_registry(hass)
     triggers = []
