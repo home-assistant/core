395912a957518a94453f8d58a312c07eaf9d81fc
DatDraggy <github.com@kieran.de>
2021-03-17 18:03:06 +0100

    Check for user input first, then both cases fall through to form
    Co-authored-by: Nathan Tilley <nathan@tilley.xyz>

--- homeassistant/components/flexpool/config_flow.py
+++ homeassistant/components/flexpool/config_flow.py
@@ -41,18 +41,16 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
-            )
-
         errors = {}
-
-        try:
-            info = await validate_input(self.hass, user_input)
-        except InvalidAddress:
-            errors["base"] = "invalid_address"
-        else:
+        if user_input is not None:
+          try:
+              info = await validate_input(self.hass, user_input)
+          except InvalidAddress:
+              errors["base"] = "invalid_address"
+          except Exception as error:  # pylint: disable=broad-except
+              _LOGGER.exception("Unexpected exception: %s", error)
+              errors["base"] = "unknown"
+          if not errors:
             return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
