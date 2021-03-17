2f3e95563f1a0c2c6db55affc318330ca23f3aa2
farmio <farmio@alphart.net>
2021-03-17 22:02:44 +0100

    type check scene

--- homeassistant/components/knx/scene.py
+++ homeassistant/components/knx/scene.py
@@ -1,15 +1,27 @@
 """Support for KNX scenes."""
-from typing import Any
+from __future__ import annotations
+
+from typing import Any, Callable
 
 from xknx.devices import Scene as XknxScene
 
 from homeassistant.components.scene import Scene
+from homeassistant.helpers.typing import (
+    ConfigType,
+    DiscoveryInfoType,
+    HomeAssistantType,
+)
 
 from .const import DOMAIN
 from .knx_entity import KnxEntity
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(
+    hass: HomeAssistantType,
+    config: ConfigType,
+    async_add_entities: Callable,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
     """Set up the scenes for KNX platform."""
     entities = []
     for device in hass.data[DOMAIN].xknx.devices:
@@ -23,6 +35,7 @@ class KNXScene(KnxEntity, Scene):
 
     def __init__(self, device: XknxScene):
         """Init KNX scene."""
+        self._device: XknxScene
         super().__init__(device)
 
     async def async_activate(self, **kwargs: Any) -> None:
