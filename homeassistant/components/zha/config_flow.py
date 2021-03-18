1375f259df7423fdd2d8df379bf4fb16da7f97f5
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-03-18 12:36:09 +0100

    Update typing 14 - manual

--- homeassistant/components/zha/config_flow.py
+++ homeassistant/components/zha/config_flow.py
@@ -1,6 +1,8 @@
 """Config flow for ZHA."""
+from __future__ import annotations
+
 import os
-from typing import Any, Dict, Optional
+from typing import Any
 
 import serial.tools.list_ports
 import voluptuous as vol
@@ -127,7 +129,7 @@ class ZhaFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
 
-async def detect_radios(dev_path: str) -> Optional[Dict[str, Any]]:
+async def detect_radios(dev_path: str) -> dict[str, Any] | None:
     """Probe all radio types on the device port."""
     for radio in RadioType:
         dev_config = radio.controller.SCHEMA_DEVICE({CONF_DEVICE_PATH: dev_path})
