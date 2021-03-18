c4c9255110ea716a899a0ead0f7672824dd7c491
posixx <2280400+posixx@users.noreply.github.com>
2021-03-18 11:23:43 +0100

    tryfix action test (changed order)

--- tests/components/alarm_control_panel/test_device_action.py
+++ tests/components/alarm_control_panel/test_device_action.py
@@ -313,18 +313,18 @@ async def test_action(hass):
         == STATE_ALARM_ARMED_HOME
     )
 
+    hass.bus.async_fire("test_event_arm_vacation")
+    await hass.async_block_till_done()
+    assert (
+        hass.states.get("alarm_control_panel.alarm_no_arm_code").state
+        == STATE_ALARM_ARMED_VACATION
+    )
+
     hass.bus.async_fire("test_event_arm_night")
     await hass.async_block_till_done()
     assert (
         hass.states.get("alarm_control_panel.alarm_no_arm_code").state
         == STATE_ALARM_ARMED_NIGHT
-    )
-
-    hass.bus.async_fire("test_event_arm_vacation")
-    await hass.async_block_till_done()
-    assert (
-        hass.states.get("alarm_control_panel.alarm_no_arm_code").state
-        != STATE_ALARM_ARMED_VACATION
     )
 
     hass.bus.async_fire("test_event_disarm")
