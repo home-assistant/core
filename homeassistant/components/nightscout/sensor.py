cd1a7f0c28010544830f2bebc5c265ff900bc701
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-03-18 13:21:46 +0100

    Update typing 11 (#48072)

--- homeassistant/components/nightscout/sensor.py
+++ homeassistant/components/nightscout/sensor.py
@@ -1,8 +1,10 @@
 """Support for Nightscout sensors."""
+from __future__ import annotations
+
 from asyncio import TimeoutError as AsyncIOTimeoutError
 from datetime import timedelta
 import logging
-from typing import Callable, List
+from typing import Callable
 
 from aiohttp import ClientError
 from py_nightscout import Api as NightscoutAPI
@@ -24,7 +26,7 @@ DEFAULT_NAME = "Blood Glucose"
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
-    async_add_entities: Callable[[List[Entity], bool], None],
+    async_add_entities: Callable[[list[Entity], bool], None],
 ) -> None:
     """Set up the Glucose Sensor."""
     api = hass.data[DOMAIN][entry.entry_id]
