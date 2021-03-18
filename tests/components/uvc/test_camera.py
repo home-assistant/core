cea6a15ccffbc2fe845fc07bc95b9647a70f4d7c
Martin Hjelmare <marhje52@gmail.com>
2021-03-18 22:02:05 +0100

    Test enable and disable motion detection

--- tests/components/uvc/test_camera.py
+++ tests/components/uvc/test_camera.py
@@ -8,6 +8,8 @@ from uvcclient import camera as camera, nvr
 
 from homeassistant.components.camera import (
     DEFAULT_CONTENT_TYPE,
+    SERVICE_DISABLE_MOTION,
+    SERVICE_ENABLE_MOTION,
     STATE_RECORDING,
     SUPPORT_STREAM,
     async_get_image,
@@ -523,3 +525,78 @@ async def test_camera_image_error(
         await async_get_image(hass, "camera.front")
 
     assert camera_v320.return_value.get_snapshot.call_count == snapshot_calls
+
+
+async def test_enable_disable_motion_detection(hass, mock_remote, camera_info):
+    """Test enable and disable motion detection."""
+
+    def set_recordmode(uuid, mode):
+        """Set record mode."""
+        motion_record_enabled = mode == "motion"
+        camera_info["recordingSettings"]["motionRecordEnabled"] = motion_record_enabled
+
+    mock_remote.return_value.set_recordmode.side_effect = set_recordmode
+    config = {"platform": "uvc", "nvr": "foo", "key": "secret"}
+    assert await async_setup_component(hass, "camera", {"camera": config})
+    await hass.async_block_till_done()
+
+    state = hass.states.get("camera.front")
+
+    assert state
+    assert "motion_detection" not in state.attributes
+
+    await hass.services.async_call(
+        "camera", SERVICE_ENABLE_MOTION, {"entity_id": "camera.front"}, True
+    )
+    await hass.async_block_till_done()
+
+    state = hass.states.get("camera.front")
+
+    assert state
+    assert state.attributes["motion_detection"]
+
+    await hass.services.async_call(
+        "camera", SERVICE_DISABLE_MOTION, {"entity_id": "camera.front"}, True
+    )
+    await hass.async_block_till_done()
+
+    state = hass.states.get("camera.front")
+
+    assert state
+    assert "motion_detection" not in state.attributes
+
+    mock_remote.return_value.set_recordmode.side_effect = nvr.NvrError
+
+    await hass.services.async_call(
+        "camera", SERVICE_ENABLE_MOTION, {"entity_id": "camera.front"}, True
+    )
+    await hass.async_block_till_done()
+
+    state = hass.states.get("camera.front")
+
+    assert state
+    assert "motion_detection" not in state.attributes
+
+    mock_remote.return_value.set_recordmode.side_effect = set_recordmode
+
+    await hass.services.async_call(
+        "camera", SERVICE_ENABLE_MOTION, {"entity_id": "camera.front"}, True
+    )
+    await hass.async_block_till_done()
+
+    state = hass.states.get("camera.front")
+
+    assert state
+    assert state.attributes["motion_detection"]
+
+    mock_remote.return_value.set_recordmode.side_effect = nvr.NvrError
+
+    await hass.services.async_call(
+        "camera", SERVICE_DISABLE_MOTION, {"entity_id": "camera.front"}, True
+    )
+    await hass.async_block_till_done()
+
+    state = hass.states.get("camera.front")
+
+    assert state
+    assert state.attributes["motion_detection"]
