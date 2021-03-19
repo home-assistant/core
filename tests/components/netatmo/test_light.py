49793fa781e06c62707e9e2a8752cf4332384176
Tobias Sauerwein <cgtobi@gmail.com>
2021-03-19 08:22:06 +0100

    Deduplicate webhook test data

--- tests/components/netatmo/test_light.py
+++ tests/components/netatmo/test_light.py
@@ -5,7 +5,6 @@ from homeassistant.components.light import (
     SERVICE_TURN_ON,
 )
 from homeassistant.const import ATTR_ENTITY_ID, CONF_WEBHOOK_ID
-from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 from .common import simulate_webhook
 
@@ -21,15 +20,9 @@ async def test_light_setup_and_services(hass, light_entry):
 
     # Fake webhook activation
     webhook_data = {
-        "user_id": "123",
-        "user": {"id": "123", "email": "foo@bar.com"},
         "push_type": "webhook_activation",
     }
-    async_dispatcher_send(
-        hass,
-        "signal-netatmo-webhook-None",
-        {"type": None, "data": webhook_data},
-    )
+    await simulate_webhook(hass, webhook_id, webhook_data)
     await hass.async_block_till_done()
     assert (
         hass.data["netatmo"][light_entry.entry_id]["netatmo_data_handler"].webhook
@@ -40,22 +33,24 @@ async def test_light_setup_and_services(hass, light_entry):
     assert hass.states.get(light_entity).state == "unavailable"
 
     # Trigger light mode change
-    response = (
-        b'{"user_id": "91763b24c43d3e344f424e8d","event_type": "light_mode",'
-        b'"device_id": "12:34:56:00:a5:a4","home_id": "91763b24c43d3e344f424e8b",'
-        b'"home_name": "MYHOME","camera_id": "12:34:56:00:a5:a4",'
-        b'"event_id": "601dce1560abca1ebad9b723","push_type": "NOC-light_mode","sub_type": "on"}'
-    )
+    response = {
+        "event_type": "light_mode",
+        "device_id": "12:34:56:00:a5:a4",
+        "camera_id": "12:34:56:00:a5:a4",
+        "event_id": "601dce1560abca1ebad9b723",
+        "push_type": "NOC-light_mode",
+        "sub_type": "on",
+    }
     await simulate_webhook(hass, webhook_id, response)
 
     assert hass.states.get(light_entity).state == "on"
 
     # Trigger light mode change with erroneous webhook data
-    response = (
-        b'{"user_id": "91763b24c43d3e344f424e8d","event_type": "light_mode",'
-        b'"device_id": "12:34:56:00:a5:a4","home_id": "91763b24c43d3e344f424e8b",'
-        b'"home_name": "MYHOME"}'
-    )
+    response = {
+        "user_id": "91763b24c43d3e344f424e8d",
+        "event_type": "light_mode",
+        "device_id": "12:34:56:00:a5:a4",
+    }
     await simulate_webhook(hass, webhook_id, response)
 
     # Test turning light off
