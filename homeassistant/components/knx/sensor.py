9a770a86ca87e6e78757964cee15797dee9fecb9
Matthias Alphart <farmio@alphart.net>
2021-03-19 10:22:18 +0100

    Type check KNX integration binary_sensor, sensor and switch (#48050)

--- homeassistant/components/knx/sensor.py
+++ homeassistant/components/knx/sensor.py
@@ -1,14 +1,29 @@
 """Support for KNX/IP sensors."""
+from __future__ import annotations
+
+from typing import Callable, Iterable
+
 from xknx.devices import Sensor as XknxSensor
 
 from homeassistant.components.sensor import DEVICE_CLASSES
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.typing import (
+    ConfigType,
+    DiscoveryInfoType,
+    HomeAssistantType,
+    StateType,
+)
 
 from .const import DOMAIN
 from .knx_entity import KnxEntity
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(
+    hass: HomeAssistantType,
+    config: ConfigType,
+    async_add_entities: Callable[[Iterable[Entity]], None],
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
     """Set up sensor(s) for KNX platform."""
     entities = []
     for device in hass.data[DOMAIN].xknx.devices:
@@ -20,22 +35,23 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 class KNXSensor(KnxEntity, Entity):
     """Representation of a KNX sensor."""
 
-    def __init__(self, device: XknxSensor):
+    def __init__(self, device: XknxSensor) -> None:
         """Initialize of a KNX sensor."""
+        self._device: XknxSensor
         super().__init__(device)
 
     @property
-    def state(self):
+    def state(self) -> StateType:
         """Return the state of the sensor."""
         return self._device.resolve_state()
 
     @property
-    def unit_of_measurement(self) -> str:
+    def unit_of_measurement(self) -> str | None:
         """Return the unit this state is expressed in."""
         return self._device.unit_of_measurement()
 
     @property
-    def device_class(self):
+    def device_class(self) -> str | None:
         """Return the device class of the sensor."""
         device_class = self._device.ha_device_class()
         if device_class in DEVICE_CLASSES:
