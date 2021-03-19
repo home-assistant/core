ab78189d634e5c6caac063a6a01b0699a10f5560
Es <hessel.vanes@kpn.com>
2021-03-19 20:28:47 +0100

    isort test const

--- tests/components/wallbox/const.py
+++ tests/components/wallbox/const.py
@@ -1,8 +1,6 @@
 """Constants for wallbox tests."""
-from homeassistant.components.wallbox.const import (
-    CONF_STATION,
-)
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
+from homeassistant.components.wallbox.const import CONF_STATION
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 
 # Mock config data to be used across multiple tests
 MOCK_CONFIG = {
