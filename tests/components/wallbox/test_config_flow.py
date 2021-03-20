156746110e3d7c926b8688cec259b8fd71c23fdf
Es <hessel.vanes@kpn.com>
2021-03-20 20:11:46 +0100

    add additional test for exceptions

--- tests/components/wallbox/test_config_flow.py
+++ tests/components/wallbox/test_config_flow.py
@@ -1,6 +1,9 @@
 """Test the Wallbox config flow."""
 from unittest.mock import patch
 
+import requests
+from voluptuous.schema_builder import raises
+
 from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.wallbox import config_flow
 from homeassistant.components.wallbox.config_flow import CannotConnect, InvalidAuth
@@ -87,7 +90,18 @@ def test_hub_class():
         "wallbox.Wallbox.getChargerStatus",
         side_effect=alternate_get_charger_status_method,
     ):
+        assert hub.authenticate() == True
+        assert hub.get_data()
+
+    with patch("wallbox.Wallbox.authenticate", side_effect=InvalidAuth,), patch(
+        "wallbox.Wallbox.getChargerStatus",
+        side_effect=CannotConnect,
+    ), raises(InvalidAuth):
         assert hub.authenticate()
+    with patch(
+        "wallbox.Wallbox.getChargerStatus",
+        side_effect=CannotConnect,
+    ), raises(ConnectionError):
         assert hub.get_data()
 
 
