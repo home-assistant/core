299d9530945e73701323b8412c9b57b2e862d4f8
Franck Nijhof <git@frenck.dev>
2021-03-20 14:33:11 +0100

    Black

--- tests/components/recorder/test_purge.py
+++ tests/components/recorder/test_purge.py
@@ -366,9 +366,7 @@ async def test_purge_filtered_states(
 
         assert session.query(States).get(72).old_state_id is None
         assert session.query(States).get(73).old_state_id is None
-        assert (
-            session.query(States).get(74).old_state_id == 62
-        )  # should have been kept
+        assert session.query(States).get(74).old_state_id == 62  # should have been kept
 
 
 async def test_purge_filtered_events(
@@ -554,9 +552,7 @@ async def test_purge_filtered_events_state_changed(
 
         assert session.query(States).get(61).old_state_id is None
         assert session.query(States).get(62).old_state_id is None
-        assert (
-            session.query(States).get(63).old_state_id == 62
-        )  # should have been kept
+        assert session.query(States).get(63).old_state_id == 62  # should have been kept
 
 
 async def _add_test_states(hass: HomeAssistantType, instance: recorder.Recorder):
