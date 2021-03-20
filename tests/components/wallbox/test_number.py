657dde349a4c4cc72de856a191e9c4eccfa5796a
Es <hessel.vanes@kpn.com>
2021-03-20 10:22:43 +0100

    add switch, number and lock test

---
+++ tests/components/wallbox/test_number.py
@@ -0,0 +1,29 @@
+from unittest.mock import MagicMock
+
+from homeassistant.components.wallbox import number
+
+
+def test_wallbox_lock_class():
+
+    wallbox = MagicMock()
+    coordinator = MagicMock(return_value="connected")
+    config = MagicMock(return_value="12345")
+    name = "wallbox_pause_tester"
+
+    wallbox_max_charging_current = number.WallboxMaxChargingCurrent(
+        name, config, coordinator, wallbox
+    )
+
+    assert wallbox_max_charging_current.name == "wallbox_pause_tester"
+    assert wallbox_max_charging_current.icon == "mdi:ev-station"
+    assert wallbox_max_charging_current.available
+
+    wallbox_max_charging_current.set_max_charging_current(25)
+
+
+def test_wallbox_updater():
+    station = "12345"
+
+    wallbox = MagicMock(return_value={"key1": 5, "key2": 4})
+    result = number.wallbox_updater(wallbox, station)
+    assert result
---
+++ tests/components/wallbox/test_switch.py
@@ -0,0 +1,28 @@
+from unittest.mock import MagicMock
+
+from homeassistant.components.wallbox import switch
+
+
+def test_wallbox_pause_class():
+
+    wallbox = MagicMock()
+    coordinator = MagicMock(return_value="connected")
+    config = MagicMock(return_value="12345")
+    name = "wallbox_pause_tester"
+
+    wallboxPause = switch.WallboxPause(name, config, coordinator, wallbox)
+
+    assert wallboxPause.name == "wallbox_pause_tester"
+    assert wallboxPause.icon == "mdi:power-plug-off-outline"
+    assert wallboxPause.available
+
+    wallboxPause.pause_charger(True)
+    wallboxPause.pause_charger(False)
+
+
+def test_wallbox_updater():
+    station = "12345"
+
+    wallbox = MagicMock(return_value={"key1": 5, "key2": 4})
+    result = switch.wallbox_updater(wallbox, station)
+    assert result
