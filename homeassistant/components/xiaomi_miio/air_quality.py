6d25ad657df9da140bfc257103e2a8d72f939d94
arturdobo <arturdobo@gmail.com>
2021-03-21 10:06:44 +0000

    Add cgllc.airm.cgdn1 support

--- homeassistant/components/xiaomi_miio/air_quality.py
+++ homeassistant/components/xiaomi_miio/air_quality.py
@@ -1,7 +1,7 @@
 """Support for Xiaomi Mi Air Quality Monitor (PM2.5)."""
 import logging
 
-from miio import AirQualityMonitor, DeviceException
+from miio import AirQualityMonitor, AirQualityMonitorCGDN1, DeviceException
 import voluptuous as vol
 
 from homeassistant.components.air_quality import PLATFORM_SCHEMA, AirQualityEntity
@@ -15,6 +15,7 @@ from .const import (
     CONF_MODEL,
     DOMAIN,
     MODEL_AIRQUALITYMONITOR_B1,
+    MODEL_AIRQUALITYMONITOR_CGDN1,
     MODEL_AIRQUALITYMONITOR_S1,
     MODEL_AIRQUALITYMONITOR_V1,
 )
@@ -43,6 +44,16 @@ PROP_TO_ATTR = {
     "temperature": ATTR_TEMP,
     "humidity": ATTR_HUM,
 }
+
+DEVICES = [
+    MODEL_AIRQUALITYMONITOR_B1,
+    MODEL_AIRQUALITYMONITOR_S1,
+    MODEL_AIRQUALITYMONITOR_V1,
+]
+
+MIOT_DEVICES = [
+    MODEL_AIRQUALITYMONITOR_CGDN1,
+]
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
@@ -73,14 +84,24 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
         _LOGGER.debug("Initializing with host %s (token %s...)", host, token[:5])
 
-        device = AirQualityMonitor(host, token, model=model)
-
-        if model == MODEL_AIRQUALITYMONITOR_S1:
-            entities.append(AirMonitorS1(name, device, config_entry, unique_id))
-        elif model == MODEL_AIRQUALITYMONITOR_B1:
-            entities.append(AirMonitorB1(name, device, config_entry, unique_id))
-        elif model == MODEL_AIRQUALITYMONITOR_V1:
-            entities.append(AirMonitorV1(name, device, config_entry, unique_id))
+        if model in DEVICES:
+            device = AirQualityMonitor(host, token, model=model)
+            if model == MODEL_AIRQUALITYMONITOR_S1:
+                entities.append(AirMonitorS1(name, device, config_entry, unique_id))
+            elif model == MODEL_AIRQUALITYMONITOR_B1:
+                entities.append(AirMonitorB1(name, device, config_entry, unique_id))
+            elif model == MODEL_AIRQUALITYMONITOR_V1:
+                entities.append(AirMonitorV1(name, device, config_entry, unique_id))
+        elif model in MIOT_DEVICES:
+            if model == MODEL_AIRQUALITYMONITOR_CGDN1:
+                entities.append(
+                    AirMonitorCGDN1(
+                        name,
+                        AirQualityMonitorCGDN1(host, token),
+                        config_entry,
+                        unique_id,
+                    )
+                )
         else:
             _LOGGER.warning("AirQualityMonitor model '%s' is not yet supported", model)
 
@@ -216,3 +237,55 @@ class AirMonitorV1(AirMonitorB1):
     def unit_of_measurement(self):
         """Return the unit of measurement."""
         return None
+
+
+class AirMonitorCGDN1(XiaomiMiioEntity, AirQualityEntity):
+    """Air Quality class for cgllc.airm.cgdn1 device."""
+
+    def __init__(self, name, device, entry, unique_id):
+        """Initialize the entity."""
+        super().__init__(name, device, entry, unique_id)
+
+        self._icon = "mdi:cloud"
+        self._available = None
+        self._carbon_dioxide = None
+        self._particulate_matter_2_5 = None
+        self._particulate_matter_10 = None
+
+    async def async_update(self):
+        """Fetch state from the miio device."""
+        try:
+            state = await self.hass.async_add_executor_job(self._device.status)
+            _LOGGER.debug("Got new state: %s", state)
+            self._carbon_dioxide = state.co2
+            self._particulate_matter_2_5 = round(state.pm25, 1)
+            self._particulate_matter_10 = round(state.pm10, 1)
+            self._available = True
+        except DeviceException as ex:
+            self._available = False
+            _LOGGER.error("Got exception while fetching the state: %s", ex)
+
+    @property
+    def icon(self):
+        """Return the icon to use for device if any."""
+        return self._icon
+
+    @property
+    def available(self):
+        """Return true when state is known."""
+        return self._available
+
+    @property
+    def carbon_dioxide(self):
+        """Return the CO2 (carbon dioxide) level."""
+        return self._carbon_dioxide
+
+    @property
+    def particulate_matter_2_5(self):
+        """Return the particulate matter 2.5 level."""
+        return self._particulate_matter_2_5
+
+    @property
+    def particulate_matter_10(self):
+        """Return the particulate matter 10 level."""
+        return self._particulate_matter_10
