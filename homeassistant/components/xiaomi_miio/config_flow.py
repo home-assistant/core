9ef08973ac6d5a3572be5a4f7288cf6062633f21
starkillerOG <starkiller.og@gmail.com>
2021-03-21 18:27:20 +0100

    CONF_HOST always available

--- homeassistant/components/xiaomi_miio/config_flow.py
+++ homeassistant/components/xiaomi_miio/config_flow.py
@@ -46,7 +46,7 @@ class XiaomiMiioFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_import(self, conf: dict):
         """Import a configuration from config.yaml."""
-        host = conf.get(CONF_HOST, "no host")
+        host = conf[CONF_HOST]
         self.context.update({"title_placeholders": {"name": f"YAML import {host}"}})
         return await self.async_step_device(user_input=conf)
 
