76a871fb479b51a10d7f9176feb2c1ca464a85a9
Dave T <17680170+davet2001@users.noreply.github.com>
2021-03-21 09:45:37 +0000

    Add sensor test

---
+++ tests/components/aurora_abb_powerone/test_sensor.py
@@ -0,0 +1,51 @@
+"""Test the Aurora ABB PowerOne Solar PV sensors."""
+from unittest.mock import patch
+
+from homeassistant.components.aurora_abb_powerone.const import (
+    DEFAULT_INTEGRATION_TITLE,
+    DOMAIN,
+)
+from homeassistant.config_entries import CONN_CLASS_LOCAL_POLL
+from homeassistant.const import CONF_ADDRESS, CONF_PORT
+
+from tests.common import MockConfigEntry
+
+
+async def test_sensors(hass):
+    """Test data coming back from inverter."""
+    mock_entry = MockConfigEntry(
+        version=1,
+        domain=DOMAIN,
+        title=DEFAULT_INTEGRATION_TITLE,
+        data={
+            CONF_PORT: "/dev/usb999",
+            CONF_ADDRESS: 3,
+        },
+        source="dummysource",
+        connection_class=CONN_CLASS_LOCAL_POLL,
+        system_options={},
+        entry_id="13579",
+    )
+
+    def _simulated_returns(index, global_measure=None):
+        returns = {
+            3: 45.678,  # power
+            21: 9.876,  # temperature
+        }
+        return returns[index]
+
+    with patch("aurorapy.client.AuroraSerialClient.connect", return_value=None), patch(
+        "aurorapy.client.AuroraSerialClient.measure",
+        side_effect=_simulated_returns,
+    ):
+        mock_entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(mock_entry.entry_id)
+        await hass.async_block_till_done()
+
+        power = hass.states.get("sensor.power_output")
+        assert power
+        assert power.state == "45.7"
+
+        temperature = hass.states.get("sensor.temperature")
+        assert temperature
+        assert temperature.state == "9.9"
