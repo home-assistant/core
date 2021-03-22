dc15f243e6913a0dc0049cd508e1a21dea97f168
Franck Nijhof <git@frenck.dev>
2021-03-22 13:29:39 +0100

    Upgrade pyupgrade to v2.11.0 (#48220)

--- homeassistant/auth/models.py
+++ homeassistant/auth/models.py
@@ -46,7 +46,7 @@ class User:
     credentials: list[Credentials] = attr.ib(factory=list, eq=False, order=False)
 
     # Tokens associated with a user.
-    refresh_tokens: dict[str, "RefreshToken"] = attr.ib(
+    refresh_tokens: dict[str, RefreshToken] = attr.ib(
         factory=dict, eq=False, order=False
     )
 
@@ -109,7 +109,7 @@ class RefreshToken:
     last_used_at: datetime | None = attr.ib(default=None)
     last_used_ip: str | None = attr.ib(default=None)
 
-    credential: "Credentials" | None = attr.ib(default=None)
+    credential: Credentials | None = attr.ib(default=None)
 
     version: str | None = attr.ib(default=__version__)
 
