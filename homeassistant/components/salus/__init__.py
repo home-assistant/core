9bbe3c4b55079bfd556d0b3bc7b467610486e693
Ciprian Florescu <ciprian.florescu@softwire.com>
2021-03-22 07:32:39 +0000

    Remove configuration.yaml support for salus integration

--- homeassistant/components/salus/__init__.py
+++ homeassistant/components/salus/__init__.py
@@ -2,29 +2,15 @@
 import logging
 
 from salus.api import Api
-import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_DEVICE, CONF_PASSWORD, CONF_USERNAME
-import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.typing import HomeAssistantType
 
 from .const import DOMAIN, PLATFORMS
 from .coordinator import SalusDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
-
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_USERNAME): cv.string,
-                vol.Required(CONF_PASSWORD): cv.string,
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
 
 
 async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
