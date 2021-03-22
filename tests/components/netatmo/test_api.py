0a6452a6e4bccc71dc1faa654de2690ca68612eb
Tobias Sauerwein <cgtobi@gmail.com>
2021-03-22 10:47:51 +0100

    Don't test implementation detail

--- tests/components/netatmo/test_api.py
+++ tests/components/netatmo/test_api.py
@@ -1,14 +1,17 @@
 """The tests for the Netatmo oauth2 api."""
 from unittest.mock import patch
 
-from homeassistant.components.netatmo import api
+from homeassistant import config_entries
+from homeassistant.setup import async_setup_component
 
 
 async def test_api(hass, config_entry):
     """Test auth instantiation."""
+    config_entry.add_to_hass(hass)
+
     with patch(
         "homeassistant.helpers.config_entry_oauth2_flow.async_get_config_entry_implementation",
-    ) as fake_implementation:
-        auth = api.ConfigEntryNetatmoAuth(hass, config_entry, fake_implementation)
+    ), patch("homeassistant.components.webhook.async_generate_url"):
+        assert await async_setup_component(hass, "netatmo", {})
 
-    assert isinstance(auth, api.ConfigEntryNetatmoAuth)
+    assert config_entry.state == config_entries.ENTRY_STATE_LOADED
