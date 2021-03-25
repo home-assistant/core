9217a2932577fb90a3425ed068a349b48adf8123
J. Nick Koston <nick@koston.org>
2021-03-24 20:31:41 -1000

    Add emonitor integration
    The SiteSage Emonitor allows local power monitoring on a per circuit basis via an [Emonitor](https://powerhousedynamics.com/solutions/sitesage/) device.

---
+++ homeassistant/components/emonitor/__init__.py
@@ -0,0 +1,77 @@
+"""The SiteSage Emonitor integration."""
+import asyncio
+from datetime import timedelta
+import logging
+
+from aioemonitor import Emonitor
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import aiohttp_client
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_UPDATE_RATE = 60
+
+PLATFORMS = ["sensor"]
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the SiteSage Emonitor component."""
+    hass.data[DOMAIN] = {}
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up SiteSage Emonitor from a config entry."""
+
+    conf = entry.data
+    host = conf[CONF_HOST]
+
+    session = aiohttp_client.async_get_clientsession(hass)
+    emonitor = Emonitor(host, session)
+
+    async def _async_update_data():
+        return await emonitor.async_get_status()
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="Emonitor power update",
+        update_method=_async_update_data,
+        update_interval=timedelta(seconds=DEFAULT_UPDATE_RATE),
+    )
+
+    await coordinator.async_refresh()
+    if not coordinator.last_update_success:
+        raise ConfigEntryNotReady
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    for platform in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, platform)
+                for platform in PLATFORMS
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
