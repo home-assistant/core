54e2ac45be501157e2899a6feb62bc23105660ab
J. Nick Koston <nick@koston.org>
2021-03-24 17:16:11 -1000

    adj

--- homeassistant/components/emonitor/__init__.py
+++ homeassistant/components/emonitor/__init__.py
@@ -23,6 +23,7 @@ PLATFORMS = ["sensor"]
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the SiteSage Emonitor component."""
+    hass.data[DOMAIN] = {}
     return True
 
 
