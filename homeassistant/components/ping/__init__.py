1a396cec9dfe47da3f9521a47338cb1ef07174fa
J. Nick Koston <nick@koston.org>
2021-03-25 13:33:32 -1000

    log it

--- homeassistant/components/ping/__init__.py
+++ homeassistant/components/ping/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from functools import lru_cache
+import logging
 
 from icmplib import SocketPermissionError, ping as icmp_ping
 
@@ -13,6 +14,8 @@ PLATFORMS = ["binary_sensor"]
 PING_ID = "ping_id"
 DEFAULT_START_ID = 129
 MAX_PING_ID = 65534
+
+_LOGGER = logging.getLogger(__name__)
 
 
 @callback
@@ -43,11 +46,13 @@ def can_use_icmp_lib_with_privilege() -> None | bool:
         try:
             icmp_ping("127.0.0.1", count=0, timeout=0, privileged=False)
         except SocketPermissionError:
-            # Cannot use icmplib even with privileged=False
+            _LOGGER.debug(
+                "Cannot use icmplib because privileges are insufficient to create the socket"
+            )
             return None
         else:
-            # Can use icmplib with privileged=False
+            _LOGGER.debug("Using icmplib in privileged=False mode")
             return False
     else:
-        # Can use icmplib privileged=True
+        _LOGGER.debug("Using icmplib in privileged=True mode")
         return True
