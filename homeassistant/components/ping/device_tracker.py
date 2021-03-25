6697928ca403e5ebe10451efa60edb38e7030253
J. Nick Koston <nick@koston.org>
2021-03-25 13:54:15 -1000

    multiping

--- homeassistant/components/ping/device_tracker.py
+++ homeassistant/components/ping/device_tracker.py
@@ -4,7 +4,7 @@ import logging
 import subprocess
 import sys
 
-from icmplib import NameLookupError, ping as icmp_ping
+from icmplib import multiping
 import voluptuous as vol
 
 from homeassistant import const, util
@@ -34,23 +34,15 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-class Host:
-    """A single host to track."""
+class HostSubProcess:
+    """Host object with ping detection."""
 
-    def __init__(self, ip_address, dev_id, hass, config):
+    def __init__(self, ip_address, dev_id, hass, config, privileged):
         """Initialize the Host pinger."""
         self.hass = hass
         self.ip_address = ip_address
         self.dev_id = dev_id
         self._count = config[CONF_PING_COUNT]
-
-
-class HostSubProcess(Host):
-    """Host object with ping detection."""
-
-    def __init__(self, ip_address, dev_id, hass, config, privileged):
-        """Initialize the Host pinger."""
-        super().__init__(ip_address, dev_id, hass, config)
         if sys.platform == "win32":
             self._ping_cmd = ["ping", "-n", "1", "-w", "1000", ip_address]
         else:
@@ -83,79 +75,60 @@ class HostSubProcess(Host):
         _LOGGER.debug("No response from %s failed=%d", self.ip_address, failed)
 
 
-class HostICMPLib(Host):
-    """Host object with ping detection."""
-
-    def __init__(self, ip_address, dev_id, hass, config, privileged):
-        """Initialize the Host pinger."""
-        super().__init__(ip_address, dev_id, hass, config)
-        self._privileged = privileged
-
-    def ping(self):
-        """Send an ICMP echo request and return True if success."""
-        next_id = run_callback_threadsafe(
-            self.hass.loop, async_get_next_ping_id, self.hass
-        ).result()
-
-        try:
-            data = icmp_ping(
-                self.ip_address,
-                count=PING_ATTEMPTS_COUNT,
-                timeout=ICMP_TIMEOUT,
-                privileged=self._privileged,
-                id=next_id,
-            )
-        except NameLookupError:
-            return False
-
-        return data.is_alive
-
-    def update(self, see):
-        """Update device state by sending one or more ping messages."""
-        if self.ping():
-            see(dev_id=self.dev_id, source_type=SOURCE_TYPE_ROUTER)
-            return True
-
-        _LOGGER.debug(
-            "No response from %s (%s) failed=%d",
-            self.ip_address,
-            self.dev_id,
-            PING_ATTEMPTS_COUNT,
-        )
-
-
 def setup_scanner(hass, config, see, discovery_info=None):
     """Set up the Host objects and return the update function."""
 
     privileged = can_use_icmp_lib_with_privilege()
-    if privileged is None:
-        host_cls = HostSubProcess
-    else:
-        host_cls = HostICMPLib
-
-    hosts = [
-        host_cls(ip, dev_id, hass, config, privileged)
-        for (dev_id, ip) in config[const.CONF_HOSTS].items()
-    ]
+    ip_to_dev_id = {ip: dev_id for (dev_id, ip) in config[const.CONF_HOSTS].items()}
     interval = config.get(
         CONF_SCAN_INTERVAL,
-        timedelta(seconds=len(hosts) * config[CONF_PING_COUNT]) + SCAN_INTERVAL,
+        timedelta(seconds=len(ip_to_dev_id) * config[CONF_PING_COUNT]) + SCAN_INTERVAL,
     )
     _LOGGER.debug(
         "Started ping tracker with interval=%s on hosts: %s",
         interval,
-        ",".join([host.ip_address for host in hosts]),
+        ",".join(ip_to_dev_id.keys()),
     )
 
-    def update_interval(now):
-        """Update all the hosts on every interval time."""
-        try:
+    if privileged is None:
+        hosts = [
+            HostSubProcess(ip, dev_id, hass, config, privileged)
+            for (dev_id, ip) in config[const.CONF_HOSTS].items()
+        ]
+
+        def update(now):
+            """Update all the hosts on every interval time."""
             for host in hosts:
                 host.update(see)
+
+    else:
+
+        def update(now):
+            """Update all the hosts on every interval time."""
+            next_id = run_callback_threadsafe(
+                hass.loop, async_get_next_ping_id, hass
+            ).result()
+            responses = multiping(
+                ip_to_dev_id.keys(),
+                count=PING_ATTEMPTS_COUNT,
+                timeout=ICMP_TIMEOUT,
+                privileged=privileged,
+                id=next_id,
+            )
+            for host in responses:
+                if host.is_alive:
+                    see(
+                        dev_id=ip_to_dev_id[host.address],
+                        source_type=SOURCE_TYPE_ROUTER,
+                    )
+
+    def _update_interval(now):
+        try:
+            update(now)
         finally:
             hass.helpers.event.track_point_in_utc_time(
-                update_interval, util.dt.utcnow() + interval
+                _update_interval, util.dt.utcnow() + interval
             )
 
-    update_interval(None)
+    _update_interval(None)
     return True
