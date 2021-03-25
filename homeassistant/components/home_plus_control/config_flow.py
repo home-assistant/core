eed4ee54edecb2b9b94f23463440ab6a25809fd7
chemaaa <187996+chemaaa@users.noreply.github.com>
2021-03-25 14:12:31 +0100

    Add Homepluscontrol integration (#46783)
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

---
+++ homeassistant/components/home_plus_control/config_flow.py
@@ -0,0 +1,32 @@
+"""Config flow for Legrand Home+ Control."""
+import logging
+
+from homeassistant import config_entries
+from homeassistant.helpers import config_entry_oauth2_flow
+
+from .const import DOMAIN
+
+
+class HomePlusControlFlowHandler(
+    config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN
+):
+    """Config flow to handle Home+ Control OAuth2 authentication."""
+
+    DOMAIN = DOMAIN
+
+    # Pick the Cloud Poll class
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    @property
+    def logger(self) -> logging.Logger:
+        """Return logger."""
+        return logging.getLogger(__name__)
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow start initiated by the user."""
+        await self.async_set_unique_id(DOMAIN)
+
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        return await super().async_step_user(user_input)
