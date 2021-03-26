82e78bd5be96becf948b1f37ee5b1baaede01efe
Erik Montnemery <erik@montnemery.com>
2021-03-26 08:46:26 +0100

    Validate device action schemas once (#48351)

--- homeassistant/components/fan/device_action.py
+++ homeassistant/components/fan/device_action.py
@@ -62,8 +62,6 @@ async def async_call_action_from_config(
     hass: HomeAssistant, config: dict, variables: dict, context: Context | None
 ) -> None:
     """Execute a device action."""
-    config = ACTION_SCHEMA(config)
-
     service_data = {ATTR_ENTITY_ID: config[CONF_ENTITY_ID]}
 
     if config[CONF_TYPE] == "turn_on":
