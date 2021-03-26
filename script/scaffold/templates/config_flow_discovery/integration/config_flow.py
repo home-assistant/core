d2d78d6205a1d7bba434ab975a842ed09c906f9c
Franck Nijhof <git@frenck.dev>
2021-03-26 15:19:44 +0100

    Extend typing on scaffold templates (#48232)

--- script/scaffold/templates/config_flow_discovery/integration/config_flow.py
+++ script/scaffold/templates/config_flow_discovery/integration/config_flow.py
@@ -2,12 +2,13 @@
 import my_pypi_dependency
 
 from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_entry_flow
 
 from .const import DOMAIN
 
 
-async def _async_has_devices(hass) -> bool:
+async def _async_has_devices(hass: HomeAssistant) -> bool:
     """Return if there are devices that can be discovered."""
     # TODO Check if there are any devices that can be discovered in the network.
     devices = await hass.async_add_executor_job(my_pypi_dependency.discover)
