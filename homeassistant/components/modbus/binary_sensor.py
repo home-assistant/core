33e34d4144797d5c40145fc6b3cb6652a04f9a40
jan iversen <jancasacondor@gmail.com>
2021-03-27 13:15:09 +0100

    Update homeassistant/components/modbus/binary_sensor.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/modbus/binary_sensor.py
+++ homeassistant/components/modbus/binary_sensor.py
@@ -86,7 +86,7 @@ async def async_setup_platform(
             "Binary_sensor configuration is deprecated, will be removed in a future release"
         )
         discovery_info = {
-            CONF_NAME: "noName",
+            CONF_NAME: "no name",
             CONF_BINARY_SENSORS: config[CONF_INPUTS],
         }
         config = None
