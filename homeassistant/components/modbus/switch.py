f73dc6ad894349c045af78bf13ec9c5d082b7965
jan iversen <jancasacondor@gmail.com>
2021-03-27 13:21:07 +0100

    Update homeassistant/components/modbus/switch.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/modbus/switch.py
+++ homeassistant/components/modbus/switch.py
@@ -100,7 +100,7 @@ async def async_setup_platform(
             "Switch configuration is deprecated, will be removed in a future release"
         )
         discovery_info = {
-            CONF_NAME: "noName",
+            CONF_NAME: "no name",
             CONF_SWITCHES: [],
         }
         if CONF_COILS in config:
