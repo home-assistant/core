4b66c2bc8f64fea59d901d8c7f5aa21c06987132
jugla <59493499+jugla@users.noreply.github.com>
2021-03-27 07:32:02 +0100

    AirVisual : update periodically interval request

--- homeassistant/components/airvisual/__init__.py
+++ homeassistant/components/airvisual/__init__.py
@@ -193,10 +193,10 @@ async def async_setup_entry(hass, config_entry):
 
         async def async_update_data():
             """reevaluate interval since all coordinators are created at same time on reboot (and so do not see each other), and so the 1rst evaluation may be wrong """
-            if  CONF_API_KEY in config_entry.data:
+            if CONF_API_KEY in config_entry.data:
                 async_sync_geo_coordinator_update_intervals(
                     hass, config_entry.data[CONF_API_KEY]
-                 )
+                )
                 
             """Get new data from the API."""
             if CONF_CITY in config_entry.data:
