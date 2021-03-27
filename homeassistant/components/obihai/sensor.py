21e69013c8cfed631a8751e1d13e1aa085dc8f43
Franck Nijhof <git@frenck.dev>
2021-03-27 10:38:57 +0100

    Merge of nested IF-IF cases - O-R (#48371)

--- homeassistant/components/obihai/sensor.py
+++ homeassistant/components/obihai/sensor.py
@@ -146,9 +146,8 @@ class ObihaiServiceSensors(SensorEntity):
 
         services = self._pyobihai.get_line_state()
 
-        if services is not None:
-            if self._service_name in services:
-                self._state = services.get(self._service_name)
+        if services is not None and self._service_name in services:
+            self._state = services.get(self._service_name)
 
         call_direction = self._pyobihai.get_call_direction()
 
