c454fa703b8acc073208467dedd230471f9dbc27
stefano055415 <stefano055415@gmail.com>
2021-03-29 10:33:56 +0200

    Update homeassistant/components/freedompro/const.py
    Co-authored-by: Milan Meulemans <milan.meulemans@live.be>

--- homeassistant/components/freedompro/const.py
+++ homeassistant/components/freedompro/const.py
@@ -5,6 +5,4 @@ COORDINATOR = "coordinator"
 DOMAIN = "freedompro"
 FREEDOMPRO_CONFIG = "freedompro_config"
 FREEDOMPRO_PARALLEL_UPDATES = 1
-FREEDOMPRO_SCAN_INTERVAL = timedelta(minutes=5)
-FREEDOMPRO_URL = "https://api.freedompro.eu/api/freedompro/accessories"
 UNDO_UPDATE_LISTENER = "undo_update_listener"
