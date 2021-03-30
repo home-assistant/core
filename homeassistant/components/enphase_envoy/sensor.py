6e920b1a2f8a230823b84be73846b1fca7427131
J. Nick Koston <nick@koston.org>
2021-03-30 08:55:47 -1000

    fixes

--- homeassistant/components/enphase_envoy/sensor.py
+++ homeassistant/components/enphase_envoy/sensor.py
@@ -78,6 +78,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                     )
                 )
         elif condition != "inverters":
+            data = coordinator.data.get(condition)
+            if data is not None and "not available" in data:
+                continue
+
             entity_name = f"{name} {SENSORS[condition][0]}"
             entities.append(
                 Envoy(
