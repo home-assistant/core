c1785728a903f3ef18016ede8eaa67c4ae99b1a9
Martidjen <martijn.wals@me.com>
2021-03-30 09:35:29 +0200

    Add opentherm_gw option for setpoint override mode (#48465)
    * Adding option for setpoint override mode
    
    Temporary setpoint override mode can now be set on or off. 'Constant' setpoint override mode will be used when Temporary is set to off
    
    * Changes after review comment
    
    Added default value for the setpoint override mode

--- homeassistant/components/opentherm_gw/config_flow.py
+++ homeassistant/components/opentherm_gw/config_flow.py
@@ -19,7 +19,12 @@ from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 
 from . import DOMAIN
-from .const import CONF_FLOOR_TEMP, CONF_READ_PRECISION, CONF_SET_PRECISION
+from .const import (
+    CONF_FLOOR_TEMP,
+    CONF_READ_PRECISION,
+    CONF_SET_PRECISION,
+    CONF_TEMPORARY_OVRD_MODE,
+)
 
 
 class OpenThermGwConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -138,6 +143,12 @@ class OpenThermGwOptionsFlow(config_entries.OptionsFlow):
                             [0, PRECISION_TENTHS, PRECISION_HALVES, PRECISION_WHOLE]
                         ),
                     ),
+                    vol.Optional(
+                        CONF_TEMPORARY_OVRD_MODE,
+                        default=self.config_entry.options.get(
+                            CONF_TEMPORARY_OVRD_MODE, True
+                        ),
+                    ): bool,
                     vol.Optional(
                         CONF_FLOOR_TEMP,
                         default=self.config_entry.options.get(CONF_FLOOR_TEMP, False),
