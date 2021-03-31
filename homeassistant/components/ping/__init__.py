c821c279849a7c3035bb504102a782a6bf68c5cb
J. Nick Koston <nick@koston.org>
2021-03-31 03:06:49 -1000

    Prevent ping integration from delaying startup (#43869)

--- homeassistant/components/ping/__init__.py
+++ homeassistant/components/ping/__init__.py
@@ -1,13 +1,26 @@
 """The ping component."""
+from __future__ import annotations
+
+import logging
+
+from icmplib import SocketPermissionError, ping as icmp_ping
 
 from homeassistant.core import callback
+from homeassistant.helpers.reload import async_setup_reload_service
 
-DOMAIN = "ping"
-PLATFORMS = ["binary_sensor"]
+from .const import DEFAULT_START_ID, DOMAIN, MAX_PING_ID, PING_ID, PING_PRIVS, PLATFORMS
 
-PING_ID = "ping_id"
-DEFAULT_START_ID = 129
-MAX_PING_ID = 65534
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass, config):
+    """Set up the template integration."""
+    await async_setup_reload_service(hass, DOMAIN, PLATFORMS)
+    hass.data[DOMAIN] = {
+        PING_PRIVS: await hass.async_add_executor_job(_can_use_icmp_lib_with_privilege),
+        PING_ID: DEFAULT_START_ID,
+    }
+    return True
 
 
 @callback
@@ -16,8 +29,7 @@ def async_get_next_ping_id(hass):
 
     Must be called in async
     """
-    current_id = hass.data.setdefault(DOMAIN, {}).get(PING_ID, DEFAULT_START_ID)
-
+    current_id = hass.data[DOMAIN][PING_ID]
     if current_id == MAX_PING_ID:
         next_id = DEFAULT_START_ID
     else:
@@ -26,3 +38,23 @@ def async_get_next_ping_id(hass):
     hass.data[DOMAIN][PING_ID] = next_id
 
     return next_id
+
+
+def _can_use_icmp_lib_with_privilege() -> None | bool:
+    """Verify we can create a raw socket."""
+    try:
+        icmp_ping("127.0.0.1", count=0, timeout=0, privileged=True)
+    except SocketPermissionError:
+        try:
+            icmp_ping("127.0.0.1", count=0, timeout=0, privileged=False)
+        except SocketPermissionError:
+            _LOGGER.debug(
+                "Cannot use icmplib because privileges are insufficient to create the socket"
+            )
+            return None
+        else:
+            _LOGGER.debug("Using icmplib in privileged=False mode")
+            return False
+    else:
+        _LOGGER.debug("Using icmplib in privileged=True mode")
+        return True
