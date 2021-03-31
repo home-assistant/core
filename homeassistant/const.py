910ebea80a0048fd4b2d0d9da35c39ed8b9ae7ff
Franck Nijhof <git@frenck.dev>
2021-03-31 23:16:50 +0200

    Bump version to 2021.5.0dev0 (#48559)

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -1,6 +1,6 @@
 """Constants used by Home Assistant components."""
 MAJOR_VERSION = 2021
-MINOR_VERSION = 4
+MINOR_VERSION = 5
 PATCH_VERSION = "0.dev0"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
