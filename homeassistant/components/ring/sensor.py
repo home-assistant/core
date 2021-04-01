bcd0065e02c74602e89d41449e0d3c2ad49963c4
Martin Hjelmare <marhje52@gmail.com>
2021-04-01 15:05:10 +0200

    Remove device class timestamp from device condition and trigger (#48431)
    * Remove unit from garmin connect
    
    * Remove unit from hvv departures
    
    * Remove device class timestamp from device condition and trigger
    
    * Remove unit from systemmonitor
    
    * Use device class constant for timestamp in ring

--- homeassistant/components/ring/sensor.py
+++ homeassistant/components/ring/sensor.py
@@ -1,6 +1,10 @@
 """This component provides HA sensor support for Ring Door Bell/Chimes."""
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import PERCENTAGE, SIGNAL_STRENGTH_DECIBELS_MILLIWATT
+from homeassistant.const import (
+    DEVICE_CLASS_TIMESTAMP,
+    PERCENTAGE,
+    SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+)
 from homeassistant.core import callback
 from homeassistant.helpers.icon import icon_for_battery_level
 
@@ -210,7 +214,7 @@ SENSOR_TYPES = {
         None,
         "history",
         None,
-        "timestamp",
+        DEVICE_CLASS_TIMESTAMP,
         HistoryRingSensor,
     ],
     "last_ding": [
@@ -219,7 +223,7 @@ SENSOR_TYPES = {
         None,
         "history",
         "ding",
-        "timestamp",
+        DEVICE_CLASS_TIMESTAMP,
         HistoryRingSensor,
     ],
     "last_motion": [
@@ -228,7 +232,7 @@ SENSOR_TYPES = {
         None,
         "history",
         "motion",
-        "timestamp",
+        DEVICE_CLASS_TIMESTAMP,
         HistoryRingSensor,
     ],
     "volume": [
--- tests/components/sensor/test_device_trigger.py
+++ tests/components/sensor/test_device_trigger.py
@@ -21,7 +21,10 @@ from tests.common import (
     mock_registry,
 )
 from tests.components.blueprint.conftest import stub_blueprint_populate  # noqa: F401
-from tests.testing_config.custom_components.test.sensor import DEVICE_CLASSES
+from tests.testing_config.custom_components.test.sensor import (
+    DEVICE_CLASSES,
+    UNITS_OF_MEASUREMENT,
+)
 
 
 @pytest.fixture
@@ -73,11 +76,12 @@ async def test_get_triggers(hass, device_reg, entity_reg):
             "entity_id": platform.ENTITIES[device_class].entity_id,
         }
         for device_class in DEVICE_CLASSES
+        if device_class in UNITS_OF_MEASUREMENT
         for trigger in ENTITY_TRIGGERS[device_class]
         if device_class != "none"
     ]
     triggers = await async_get_device_automations(hass, "trigger", device_entry.id)
-    assert len(triggers) == 14
+    assert len(triggers) == 13
     assert triggers == expected_triggers
 
 
