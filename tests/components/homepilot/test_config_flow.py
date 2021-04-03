d31cee3f85e2357688478556031020c2af8f6273
Nicolas <nicolas@gres.io>
2021-04-03 07:49:28 +0000

    Add missing config flow tests

--- tests/components/homepilot/test_config_flow.py
+++ tests/components/homepilot/test_config_flow.py
@@ -1,9 +1,39 @@
 """Test the Rademacher HomePilot config flow."""
 from unittest.mock import patch
 
+from pyhomepilot.auth import AuthError, ConnectionError
+from pytest import raises
+
 from homeassistant import config_entries, setup
-from homeassistant.components.homepilot.config_flow import CannotConnect, InvalidAuth
+from homeassistant.components.homepilot.config_flow import (
+    CannotConnect,
+    InvalidAuth,
+    validate_input,
+)
 from homeassistant.components.homepilot.const import DOMAIN
+
+TEST_USER_INPUT = {
+    "host": "192.168.1.16",
+    "password": "test-password",
+}
+
+
+async def test_validate_input_invalid_auth(hass):
+    """Test we receive an invalid auth error."""
+    with patch(
+        "pyhomepilot.auth.Auth.async_login",
+        side_effect=AuthError(None, OSError()),
+    ), raises(InvalidAuth):
+        await validate_input(hass, TEST_USER_INPUT)
+
+
+async def test_validate_input_cannot_connect(hass):
+    """Test we receive a cannot connect error."""
+    with patch(
+        "pyhomepilot.auth.Auth.async_login",
+        side_effect=ConnectionError(None, OSError()),
+    ), raises(CannotConnect):
+        await validate_input(hass, TEST_USER_INPUT)
 
 
 async def test_form(hass):
@@ -25,19 +55,13 @@ async def test_form(hass):
     ) as mock_setup_entry:
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "password": "test-password",
-            },
+            TEST_USER_INPUT,
         )
         await hass.async_block_till_done()
 
     assert result2["type"] == "create_entry"
     assert result2["title"] == "bridge"
-    assert result2["data"] == {
-        "host": "1.1.1.1",
-        "password": "test-password",
-    }
+    assert result2["data"] == TEST_USER_INPUT
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -49,15 +73,12 @@ async def test_form_invalid_auth(hass):
     )
 
     with patch(
-        "pyhomepilot.auth.Auth.async_login",
+        "homeassistant.components.homepilot.config_flow.validate_input",
         side_effect=InvalidAuth,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "password": "test-password",
-            },
+            TEST_USER_INPUT,
         )
 
     assert result2["type"] == "form"
@@ -71,16 +92,32 @@ async def test_form_cannot_connect(hass):
     )
 
     with patch(
-        "pyhomepilot.auth.Auth.async_login",
+        "homeassistant.components.homepilot.config_flow.validate_input",
         side_effect=CannotConnect,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "password": "test-password",
-            },
+            TEST_USER_INPUT,
         )
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_unknown_exception(hass):
+    """Test we handle unknown error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.homepilot.config_flow.validate_input",
+        side_effect=Exception,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            TEST_USER_INPUT,
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "unknown"}
