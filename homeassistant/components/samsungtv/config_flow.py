0e1c102255023b0eb758cd301e8ae7d2838315c2
chemelli74 <simone.chemelli@gmail.com>
2021-04-04 12:38:51 +0000

    Applied review comments

--- homeassistant/components/samsungtv/config_flow.py
+++ homeassistant/components/samsungtv/config_flow.py
@@ -48,8 +48,6 @@ class SamsungTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
-
     def __init__(self):
         """Initialize flow."""
         self._host = None
@@ -160,6 +158,7 @@ class SamsungTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             await self._get_and_check_device_info()
 
             await self.async_set_unique_id(self._id)
+            await self._abort_if_unique_id_configured()
 
             return self._get_entry()
 
