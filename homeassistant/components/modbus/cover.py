f77fa42bfbaf09208ada25bca7d70c7cf41a2468
jan Iversen <jancasacondor@gmail.com>
2021-04-04 20:02:08 +0200

    add pymodbus exception handling.

--- homeassistant/components/modbus/cover.py
+++ homeassistant/components/modbus/cover.py
@@ -4,9 +4,6 @@ from __future__ import annotations
 from datetime import timedelta
 from typing import Any
 
-from pymodbus.exceptions import ConnectionException, ModbusException
-from pymodbus.pdu import ExceptionResponse
-
 from homeassistant.components.cover import SUPPORT_CLOSE, SUPPORT_OPEN, CoverEntity
 from homeassistant.const import (
     CONF_COVERS,
@@ -179,22 +176,17 @@ class ModbusCover(CoverEntity, RestoreEntity):
 
     def _read_status_register(self) -> int | None:
         """Read status register using the Modbus hub slave."""
-        try:
-            if self._status_register_type == CALL_TYPE_REGISTER_INPUT:
-                result = self._hub.read_input_registers(
-                    self._slave, self._status_register, 1
-                )
-            else:
-                result = self._hub.read_holding_registers(
-                    self._slave, self._status_register, 1
-                )
-        except ConnectionException:
+        if self._status_register_type == CALL_TYPE_REGISTER_INPUT:
+            result = self._hub.read_input_registers(
+                self._slave, self._status_register, 1
+            )
+        else:
+            result = self._hub.read_holding_registers(
+                self._slave, self._status_register, 1
+            )
+        if result is None:
             self._available = False
-            return
-
-        if isinstance(result, (ModbusException, ExceptionResponse)):
-            self._available = False
-            return
+            return None
 
         value = int(result.registers[0])
         self._available = True
@@ -203,37 +195,18 @@ class ModbusCover(CoverEntity, RestoreEntity):
 
     def _write_register(self, value):
         """Write holding register using the Modbus hub slave."""
-        try:
-            self._hub.write_register(self._slave, self._register, value)
-        except ConnectionException:
-            self._available = False
-            return
-
-        self._available = True
+        self._available = self._hub.write_register(self._slave, self._register, value)
 
     def _read_coil(self) -> bool | None:
         """Read coil using the Modbus hub slave."""
-        try:
-            result = self._hub.read_coils(self._slave, self._coil, 1)
-        except ConnectionException:
+        result = self._hub.read_coils(self._slave, self._coil, 1)
+        if result is None:
             self._available = False
-            return
-
-        if isinstance(result, (ModbusException, ExceptionResponse)):
-            self._available = False
-            return
+            return None
 
         value = bool(result.bits[0] & 1)
-        self._available = True
-
         return value
 
     def _write_coil(self, value):
         """Write coil using the Modbus hub slave."""
-        try:
-            self._hub.write_coil(self._slave, self._coil, value)
-        except ConnectionException:
-            self._available = False
-            return
-
-        self._available = True
+        self._available = self._hub.write_coil(self._slave, self._coil, value)
