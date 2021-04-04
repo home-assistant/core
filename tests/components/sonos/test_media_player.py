42c92eea139937f24afd5fbd1d0d43c6c7c8efab
J. Nick Koston <nick@koston.org>
2021-04-04 12:37:39 -1000

    works better when the file is saved before commit

--- tests/components/sonos/test_media_player.py
+++ tests/components/sonos/test_media_player.py
@@ -72,5 +72,5 @@ async def test_entity_basic(hass, config_entry, discover):
     assert attributes["friendly_name"] == "Zone A"
     assert attributes["is_volume_muted"] is False
     assert attributes["night_sound"] is True
-    assert attributes["speech_enhance"] is False
+    assert attributes["speech_enhance"] is True
     assert attributes["volume_level"] == 0.19
