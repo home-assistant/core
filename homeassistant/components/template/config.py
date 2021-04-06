09635678bc6d475ce6c16cd9fa835de8d9d4cfcb
Paulus Schoutsen <balloob@gmail.com>
2021-04-06 12:10:39 -0700

    Allow reloading top-level template entities (#48733)

--- homeassistant/components/template/config.py
+++ homeassistant/components/template/config.py
@@ -36,7 +36,7 @@ from .const import (
 )
 from .sensor import SENSOR_SCHEMA as PLATFORM_SENSOR_SCHEMA
 
-CONVERSION_PLATFORM = {
+LEGACY_SENSOR = {
     CONF_ICON_TEMPLATE: CONF_ICON,
     CONF_ENTITY_PICTURE_TEMPLATE: CONF_PICTURE,
     CONF_AVAILABILITY_TEMPLATE: CONF_AVAILABILITY,
@@ -61,7 +61,7 @@ SENSOR_SCHEMA = vol.Schema(
     }
 )
 
-TRIGGER_ENTITY_SCHEMA = vol.Schema(
+CONFIG_SECTION_SCHEMA = vol.Schema(
     {
         vol.Optional(CONF_UNIQUE_ID): cv.string,
         vol.Required(CONF_TRIGGER): cv.TRIGGER_SCHEMA,
@@ -71,16 +71,43 @@ TRIGGER_ENTITY_SCHEMA = vol.Schema(
 )
 
 
+def _rewrite_legacy_to_modern_trigger_conf(cfg: dict):
+    """Rewrite a legacy to a modern trigger-basd conf."""
+    logging.getLogger(__name__).warning(
+        "The entity definition format under template: differs from the platform configuration format. See https://www.home-assistant.io/integrations/template#configuration-for-trigger-based-template-sensors"
+    )
+    sensor = list(cfg[SENSOR_DOMAIN]) if SENSOR_DOMAIN in cfg else []
+
+    for device_id, entity_cfg in cfg[CONF_SENSORS].items():
+        entity_cfg = {**entity_cfg}
+
+        for from_key, to_key in LEGACY_SENSOR.items():
+            if from_key not in entity_cfg or to_key in entity_cfg:
+                continue
+
+            val = entity_cfg.pop(from_key)
+            if isinstance(val, str):
+                val = template.Template(val)
+            entity_cfg[to_key] = val
+
+        if CONF_NAME not in entity_cfg:
+            entity_cfg[CONF_NAME] = template.Template(device_id)
+
+        sensor.append(entity_cfg)
+
+    return {**cfg, "sensor": sensor}
+
+
 async def async_validate_config(hass, config):
     """Validate config."""
     if DOMAIN not in config:
         return config
 
-    trigger_entity_configs = []
+    config_sections = []
 
     for cfg in cv.ensure_list(config[DOMAIN]):
         try:
-            cfg = TRIGGER_ENTITY_SCHEMA(cfg)
+            cfg = CONFIG_SECTION_SCHEMA(cfg)
             cfg[CONF_TRIGGER] = await async_validate_trigger_config(
                 hass, cfg[CONF_TRIGGER]
             )
@@ -88,39 +115,14 @@ async def async_validate_config(hass, config):
             async_log_exception(err, DOMAIN, cfg, hass)
             continue
 
-        if CONF_SENSORS not in cfg:
-            trigger_entity_configs.append(cfg)
-            continue
+        if CONF_TRIGGER in cfg and CONF_SENSORS in cfg:
+            cfg = _rewrite_legacy_to_modern_trigger_conf(cfg)
 
-        logging.getLogger(__name__).warning(
-            "The entity definition format under template: differs from the platform configuration format. See https://www.home-assistant.io/integrations/template#configuration-for-trigger-based-template-sensors"
-        )
-        sensor = list(cfg[SENSOR_DOMAIN]) if SENSOR_DOMAIN in cfg else []
-
-        for device_id, entity_cfg in cfg[CONF_SENSORS].items():
-            entity_cfg = {**entity_cfg}
-
-            for from_key, to_key in CONVERSION_PLATFORM.items():
-                if from_key not in entity_cfg or to_key in entity_cfg:
-                    continue
-
-                val = entity_cfg.pop(from_key)
-                if isinstance(val, str):
-                    val = template.Template(val)
-                entity_cfg[to_key] = val
-
-            if CONF_NAME not in entity_cfg:
-                entity_cfg[CONF_NAME] = template.Template(device_id)
-
-            sensor.append(entity_cfg)
-
-        cfg = {**cfg, "sensor": sensor}
-
-        trigger_entity_configs.append(cfg)
+        config_sections.append(cfg)
 
     # Create a copy of the configuration with all config for current
     # component removed and add validated config back in.
     config = config_without_domain(config, DOMAIN)
-    config[DOMAIN] = trigger_entity_configs
+    config[DOMAIN] = config_sections
 
     return config
