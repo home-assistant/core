bbdd91d00dd3f322ff61b17516c6ad428c650e82
Nathan Spencer <natekspencer@gmail.com>
2021-04-06 02:53:43 +0000

    Switch to entity services rather than using the 'send_command' on the vacuum entity

--- homeassistant/components/litterrobot/vacuum.py
+++ homeassistant/components/litterrobot/vacuum.py
@@ -4,13 +4,14 @@ from __future__ import annotations
 from typing import Any, Callable
 
 from pylitterbot.enums import LitterBoxStatus
+from pylitterbot.robot import VALID_WAIT_TIMES
+import voluptuous as vol
 
 from homeassistant.components.vacuum import (
     STATE_CLEANING,
     STATE_DOCKED,
     STATE_ERROR,
     STATE_PAUSED,
-    SUPPORT_SEND_COMMAND,
     SUPPORT_START,
     SUPPORT_STATE,
     SUPPORT_STATUS,
@@ -21,6 +22,7 @@ from homeassistant.components.vacuum import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import STATE_OFF
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv, entity_platform
 from homeassistant.helpers.entity import Entity
 
 from .const import DOMAIN
@@ -28,14 +30,13 @@ from .entity import LitterRobotControlEntity
 from .hub import LitterRobotHub
 
 SUPPORT_LITTERROBOT = (
-    SUPPORT_SEND_COMMAND
-    | SUPPORT_START
-    | SUPPORT_STATE
-    | SUPPORT_STATUS
-    | SUPPORT_TURN_OFF
-    | SUPPORT_TURN_ON
+    SUPPORT_START | SUPPORT_STATE | SUPPORT_STATUS | SUPPORT_TURN_OFF | SUPPORT_TURN_ON
 )
 TYPE_LITTER_BOX = "Litter Box"
+
+SERVICE_RESET_WASTE_DRAWER = "reset_waste_drawer"
+SERVICE_SET_SLEEP_MODE = "set_sleep_mode"
+SERVICE_SET_WAIT_TIME = "set_wait_time"
 
 
 async def async_setup_entry(
@@ -54,6 +55,26 @@ async def async_setup_entry(
 
     if entities:
         async_add_entities(entities, True)
+
+    platform = entity_platform.current_platform.get()
+    platform.async_register_entity_service(
+        SERVICE_RESET_WASTE_DRAWER,
+        {},
+        "async_reset_waste_drawer",
+    )
+    platform.async_register_entity_service(
+        SERVICE_SET_SLEEP_MODE,
+        {
+            vol.Required("enabled"): cv.boolean,
+            vol.Optional("start_time"): cv.time,
+        },
+        "async_set_sleep_mode",
+    )
+    platform.async_register_entity_service(
+        SERVICE_SET_WAIT_TIME,
+        {vol.Required("minutes"): vol.In(VALID_WAIT_TIMES)},
+        "async_set_wait_time",
+    )
 
 
 class LitterRobotCleaner(LitterRobotControlEntity, VacuumEntity):
@@ -100,42 +121,24 @@ class LitterRobotCleaner(LitterRobotControlEntity, VacuumEntity):
         """Start a clean cycle."""
         await self.perform_action_and_refresh(self.robot.start_cleaning)
 
-    async def async_send_command(
-        self, command: str, params: dict[str, Any] | None = None, **kwargs
+    async def async_reset_waste_drawer(self) -> None:
+        """Reset the waste drawer level."""
+        await self.robot.reset_waste_drawer()
+        self.coordinator.async_set_updated_data(True)
+
+    async def async_set_sleep_mode(
+        self, enabled: bool, start_time: str | None = None
     ) -> None:
-        """Send command.
+        """Set the sleep mode."""
+        await self.perform_action_and_refresh(
+            self.robot.set_sleep_mode,
+            enabled,
+            self.parse_time_at_default_timezone(start_time),
+        )
 
-        Available commands:
-          - reset_waste_drawer
-            * params: none
-          - set_sleep_mode
-            * params:
-              - enabled: bool
-              - sleep_time: str (optional)
-          - set_wait_time
-            * params:
-              - wait_time: int (one of [3,7,15])
-
-        """
-        if command == "reset_waste_drawer":
-            # Normally we need to request a refresh of data after a command is sent.
-            # However, the API for resetting the waste drawer returns a refreshed
-            # data set for the robot. Thus, we only need to tell hass to update the
-            # state of devices associated with this robot.
-            await self.robot.reset_waste_drawer()
-            self.coordinator.async_set_updated_data(True)
-        elif command == "set_sleep_mode":
-            await self.perform_action_and_refresh(
-                self.robot.set_sleep_mode,
-                params.get("enabled"),
-                self.parse_time_at_default_timezone(params.get("sleep_time")),
-            )
-        elif command == "set_wait_time":
-            await self.perform_action_and_refresh(
-                self.robot.set_wait_time, params.get("wait_time")
-            )
-        else:
-            raise NotImplementedError()
+    async def async_set_wait_time(self, minutes: int) -> None:
+        """Set the wait time."""
+        await self.perform_action_and_refresh(self.robot.set_wait_time, minutes)
 
     @property
     def extra_state_attributes(self) -> dict[str, Any]:
--- tests/components/litterrobot/test_vacuum.py
+++ tests/components/litterrobot/test_vacuum.py
@@ -3,42 +3,52 @@ from datetime import timedelta
 
 import pytest
 
+from homeassistant.components.litterrobot import DOMAIN
 from homeassistant.components.litterrobot.entity import REFRESH_WAIT_TIME_SECONDS
+from homeassistant.components.litterrobot.vacuum import (
+    SERVICE_RESET_WASTE_DRAWER,
+    SERVICE_SET_SLEEP_MODE,
+    SERVICE_SET_WAIT_TIME,
+)
 from homeassistant.components.vacuum import (
-    ATTR_PARAMS,
     DOMAIN as PLATFORM_DOMAIN,
-    SERVICE_SEND_COMMAND,
     SERVICE_START,
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
     STATE_DOCKED,
     STATE_ERROR,
 )
-from homeassistant.const import ATTR_COMMAND, ATTR_ENTITY_ID
+from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.core import HomeAssistant
 from homeassistant.util.dt import utcnow
 
+from .common import VACUUM_ENTITY_ID
 from .conftest import setup_integration
 
 from tests.common import async_fire_time_changed
 
-ENTITY_ID = "vacuum.test_litter_box"
+COMPONENT_SERVICE_DOMAIN = {
+    SERVICE_RESET_WASTE_DRAWER: DOMAIN,
+    SERVICE_SET_SLEEP_MODE: DOMAIN,
+    SERVICE_SET_WAIT_TIME: DOMAIN,
+}
 
 
-async def test_vacuum(hass, mock_account):
+async def test_vacuum(hass: HomeAssistant, mock_account):
     """Tests the vacuum entity was set up."""
     await setup_integration(hass, mock_account, PLATFORM_DOMAIN)
 
-    vacuum = hass.states.get(ENTITY_ID)
+    vacuum = hass.states.get(VACUUM_ENTITY_ID)
     assert vacuum
     assert vacuum.state == STATE_DOCKED
     assert vacuum.attributes["is_sleeping"] is False
 
 
-async def test_vacuum_with_error(hass, mock_account_with_error):
+async def test_vacuum_with_error(hass: HomeAssistant, mock_account_with_error):
     """Tests a vacuum entity with an error."""
     await setup_integration(hass, mock_account_with_error, PLATFORM_DOMAIN)
 
-    vacuum = hass.states.get(ENTITY_ID)
+    vacuum = hass.states.get(VACUUM_ENTITY_ID)
     assert vacuum
     assert vacuum.state == STATE_ERROR
 
@@ -50,58 +60,46 @@ async def test_vacuum_with_error(hass, mock_account_with_error):
         (SERVICE_TURN_OFF, "set_power_status", None),
         (SERVICE_TURN_ON, "set_power_status", None),
         (
-            SERVICE_SEND_COMMAND,
+            SERVICE_RESET_WASTE_DRAWER,
             "reset_waste_drawer",
-            {ATTR_COMMAND: "reset_waste_drawer"},
+            None,
         ),
         (
-            SERVICE_SEND_COMMAND,
+            SERVICE_SET_SLEEP_MODE,
             "set_sleep_mode",
-            {
-                ATTR_COMMAND: "set_sleep_mode",
-                ATTR_PARAMS: {"enabled": True, "sleep_time": "22:30"},
-            },
+            {"enabled": True, "start_time": "22:30"},
         ),
         (
-            SERVICE_SEND_COMMAND,
+            SERVICE_SET_SLEEP_MODE,
             "set_sleep_mode",
-            {
-                ATTR_COMMAND: "set_sleep_mode",
-                ATTR_PARAMS: {"enabled": True, "sleep_time": None},
-            },
+            {"enabled": True},
         ),
         (
-            SERVICE_SEND_COMMAND,
+            SERVICE_SET_SLEEP_MODE,
             "set_sleep_mode",
-            {
-                ATTR_COMMAND: "set_sleep_mode",
-                ATTR_PARAMS: {"enabled": False},
-            },
+            {"enabled": False},
         ),
         (
-            SERVICE_SEND_COMMAND,
+            SERVICE_SET_WAIT_TIME,
             "set_wait_time",
-            {
-                ATTR_COMMAND: "set_wait_time",
-                ATTR_PARAMS: {"wait_time": 3},
-            },
+            {"minutes": 3},
         ),
     ],
 )
-async def test_commands(hass, mock_account, service, command, extra):
+async def test_commands(hass: HomeAssistant, mock_account, service, command, extra):
     """Test sending commands to the vacuum."""
     await setup_integration(hass, mock_account, PLATFORM_DOMAIN)
 
-    vacuum = hass.states.get(ENTITY_ID)
+    vacuum = hass.states.get(VACUUM_ENTITY_ID)
     assert vacuum
     assert vacuum.state == STATE_DOCKED
 
-    data = {ATTR_ENTITY_ID: ENTITY_ID}
+    data = {ATTR_ENTITY_ID: VACUUM_ENTITY_ID}
     if extra:
         data.update(extra)
 
     await hass.services.async_call(
-        PLATFORM_DOMAIN,
+        COMPONENT_SERVICE_DOMAIN.get(service, PLATFORM_DOMAIN),
         service,
         data,
         blocking=True,
@@ -111,22 +109,20 @@ async def test_commands(hass, mock_account, service, command, extra):
     getattr(mock_account.robots[0], command).assert_called_once()
 
 
-async def test_invalid_commands(hass, caplog, mock_account_with_side_effects):
+async def test_invalid_commands(
+    hass: HomeAssistant, caplog, mock_account_with_side_effects
+):
     """Test sending invalid commands to the vacuum."""
     await setup_integration(hass, mock_account_with_side_effects, PLATFORM_DOMAIN)
 
-    vacuum = hass.states.get(ENTITY_ID)
+    vacuum = hass.states.get(VACUUM_ENTITY_ID)
     assert vacuum
     assert vacuum.state == STATE_DOCKED
 
     await hass.services.async_call(
-        PLATFORM_DOMAIN,
-        SERVICE_SEND_COMMAND,
-        {
-            ATTR_ENTITY_ID: ENTITY_ID,
-            ATTR_COMMAND: "set_wait_time",
-            ATTR_PARAMS: {"wait_time": 1},
-        },
+        DOMAIN,
+        SERVICE_SET_WAIT_TIME,
+        {ATTR_ENTITY_ID: VACUUM_ENTITY_ID, "minutes": 15},
         blocking=True,
     )
     mock_account_with_side_effects.robots[0].set_wait_time.assert_called_once()
