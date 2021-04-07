48fadbbccd9a5a70ce00f85910c04ade4a095794
Joakim Sørensen <joasoe@gmail.com>
2021-04-07 02:34:49 +0200

    Generate a seperate UUID for the analytics integration (#48742)

--- tests/components/analytics/test_init.py
+++ tests/components/analytics/test_init.py
@@ -1,6 +1,4 @@
 """The tests for the analytics ."""
-from unittest.mock import patch
-
 from homeassistant.components.analytics.const import ANALYTICS_ENDPOINT_URL, DOMAIN
 from homeassistant.setup import async_setup_component
 
@@ -22,11 +20,9 @@ async def test_websocket(hass, hass_ws_client, aioclient_mock):
     ws_client = await hass_ws_client(hass)
     await ws_client.send_json({"id": 1, "type": "analytics"})
 
-    with patch("homeassistant.helpers.instance_id.async_get", return_value="abcdef"):
-        response = await ws_client.receive_json()
+    response = await ws_client.receive_json()
 
     assert response["success"]
-    assert response["result"]["huuid"] == "abcdef"
 
     await ws_client.send_json(
         {"id": 2, "type": "analytics/preferences", "preferences": {"base": True}}
@@ -36,7 +32,5 @@ async def test_websocket(hass, hass_ws_client, aioclient_mock):
     assert response["result"]["preferences"]["base"]
 
     await ws_client.send_json({"id": 3, "type": "analytics"})
-    with patch("homeassistant.helpers.instance_id.async_get", return_value="abcdef"):
-        response = await ws_client.receive_json()
+    response = await ws_client.receive_json()
     assert response["result"]["preferences"]["base"]
-    assert response["result"]["huuid"] == "abcdef"
