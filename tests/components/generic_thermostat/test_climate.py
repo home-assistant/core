74add6559dbc88cd2f5dd6ce200048d9e129f8c9
Erik <erik@montnemery.com>
2021-04-07 12:57:05 +0200

    Reject nan, inf from generic_thermostat sensor

--- tests/components/generic_thermostat/test_climate.py
+++ tests/components/generic_thermostat/test_climate.py
@@ -331,7 +331,16 @@ async def test_sensor_bad_value(hass, setup_comp_2):
 
     _setup_sensor(hass, None)
     await hass.async_block_till_done()
+    state = hass.states.get(ENTITY)
+    assert temp == state.attributes.get("current_temperature")
 
+    _setup_sensor(hass, "inf")
+    await hass.async_block_till_done()
+    state = hass.states.get(ENTITY)
+    assert temp == state.attributes.get("current_temperature")
+
+    _setup_sensor(hass, "nan")
+    await hass.async_block_till_done()
     state = hass.states.get(ENTITY)
     assert temp == state.attributes.get("current_temperature")
 
