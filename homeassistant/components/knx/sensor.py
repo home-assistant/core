f5381cbfa0caf632f0b44b000d97697bdbb3b508
Marvin Wichmann <marvin.wichmann@unic.com>
2021-04-10 15:59:09 +0200

    Review: Add missing UTC conversion in sensor

--- homeassistant/components/knx/sensor.py
+++ homeassistant/components/knx/sensor.py
@@ -9,6 +9,7 @@ from homeassistant.components.sensor import DEVICE_CLASSES, SensorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType, StateType
+from homeassistant.util import dt
 
 from .const import ATTR_LAST_KNX_UPDATE, ATTR_SOURCE, DOMAIN
 from .knx_entity import KnxEntity
@@ -61,7 +62,9 @@ class KNXSensor(KnxEntity, SensorEntity):
 
         if self._device.last_telegram is not None:
             attr[ATTR_SOURCE] = str(self._device.last_telegram.source_address)
-            attr[ATTR_LAST_KNX_UPDATE] = str(self._device.last_telegram.timestamp)
+            attr[ATTR_LAST_KNX_UPDATE] = str(
+                dt.as_utc(self._device.last_telegram.timestamp)
+            )
         return attr
 
     @property
