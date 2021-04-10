f4ba12369b354b77c298a964bcd26fdbf0349439
J. Nick Koston <nick@koston.org>
2021-04-10 13:57:57 -1000

    Run database migrations in the background
    - Database migrations no longer block startup
    - Events are queued and processed when the migration is completed
    - There is a safety to start discarding events if more than 10000 happen before migration is completed
    - A notification is shown when the migration is in progress "The database is being upgraded. Integrations such as logbook and history that read the database may return inconsistent results until the migration completes."
    - The database recovery logic can now recover at point after setup including purge, migration, and event insert. In short we can _always_ (hopefully) recover and start a new db  without a restart.
    - The quick check is no longer performed on unclean shutdown since we can always recover live. The `db_integrity_check` option has been deprecated.

--- homeassistant/helpers/recorder.py
+++ homeassistant/helpers/recorder.py
@@ -1,15 +1,3 @@
 """Helpers to check the recorder."""
-from homeassistant.core import HomeAssistant
-from homeassistant.setup import async_start_setup
-
 DATA_INSTANCE = "recorder_instance"
 RECORDER_DOMAIN = "recorder"
-RECORDER_BASE_SETUP_TIMEOUT = 60
-
-
-async def async_wait_for_recorder_full_startup(hass: HomeAssistant) -> None:
-    """Ensure the recorder is ready if it was setup."""
-    if DATA_INSTANCE not in hass.data:
-        return
-    with async_start_setup(hass, [RECORDER_DOMAIN]):
-        await hass.data[DATA_INSTANCE].async_db_ready
