72e298181200a2cc8e4cbc5f08cf2c85fab9df85
Chris Talkington <chris@talkingtontech.com>
2021-04-10 14:39:21 -0500

    Update __init__.py

--- homeassistant/components/roku/__init__.py
+++ homeassistant/components/roku/__init__.py
@@ -53,7 +53,7 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
         hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await coordinator.async_config_entry_first_refresh()
-    
+
     for platform in PLATFORMS:
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, platform)
