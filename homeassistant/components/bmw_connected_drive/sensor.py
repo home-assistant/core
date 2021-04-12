d1ce19db4619014264213f1da22292b36d7b35f7
EddyK69 <eduard@kuijt-evers.net>
2021-04-12 15:42:33 +0200

    Changed date_utc to DEVICE_CLASS_TIMESTAMP

--- homeassistant/components/bmw_connected_drive/sensor.py
+++ homeassistant/components/bmw_connected_drive/sensor.py
@@ -4,10 +4,10 @@ import logging
 import homeassistant.util.dt as dt_util
 from bimmer_connected.const import SERVICE_LAST_TRIP, SERVICE_STATUS
 from bimmer_connected.state import ChargingState
-
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import (
     CONF_UNIT_SYSTEM_IMPERIAL,
+    DEVICE_CLASS_TIMESTAMP,
     ENERGY_KILO_WATT_HOUR,
     LENGTH_KILOMETERS,
     LENGTH_MILES,
@@ -17,7 +17,6 @@ from homeassistant.const import (
     VOLUME_GALLONS,
     VOLUME_LITERS,
 )
-
 from homeassistant.helpers.icon import icon_for_battery_level
 
 from . import DOMAIN as BMW_DOMAIN
@@ -27,70 +26,84 @@ from .const import CONF_ACCOUNT, DATA_ENTRIES
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_TO_HA_METRIC = {
-    "mileage": ["mdi:speedometer", LENGTH_KILOMETERS, True],
-    "remaining_range_total": ["mdi:map-marker-distance", LENGTH_KILOMETERS, True],
-    "remaining_range_electric": ["mdi:map-marker-distance", LENGTH_KILOMETERS, True],
-    "remaining_range_fuel": ["mdi:map-marker-distance", LENGTH_KILOMETERS, True],
-    "max_range_electric": ["mdi:map-marker-distance", LENGTH_KILOMETERS, True],
-    "remaining_fuel": ["mdi:gas-station", VOLUME_LITERS, True],
+    # "<ID>": [<MDI_ICON>, <DEVICE_CLASS>, <UNIT_OF_MEASUREMENT>, <ENABLED_BY_DEFAULT>],
+    "mileage": ["mdi:speedometer", None, LENGTH_KILOMETERS, True],
+    "remaining_range_total": ["mdi:map-marker-distance", None, LENGTH_KILOMETERS, True],
+    "remaining_range_electric": [
+        "mdi:map-marker-distance",
+        None,
+        LENGTH_KILOMETERS,
+        True,
+    ],
+    "remaining_range_fuel": ["mdi:map-marker-distance", None, LENGTH_KILOMETERS, True],
+    "max_range_electric": ["mdi:map-marker-distance", None, LENGTH_KILOMETERS, True],
+    "remaining_fuel": ["mdi:gas-station", None, VOLUME_LITERS, True],
     # LastTrip attributes
     "average_combined_consumption": [
         "mdi:flash",
+        None,
         f"{ENERGY_KILO_WATT_HOUR}/100{LENGTH_KILOMETERS}",
         True,
     ],
     "average_electric_consumption": [
         "mdi:power-plug-outline",
+        None,
         f"{ENERGY_KILO_WATT_HOUR}/100{LENGTH_KILOMETERS}",
         True,
     ],
     "average_recuperation": [
         "mdi:recycle-variant",
+        None,
         f"{ENERGY_KILO_WATT_HOUR}/100{LENGTH_KILOMETERS}",
         True,
     ],
-    "electric_distance": ["mdi:map-marker-distance", LENGTH_KILOMETERS, True],
-    "saved_fuel": ["mdi:fuel", VOLUME_LITERS, False],
-    "total_distance": ["mdi:map-marker-distance", LENGTH_KILOMETERS, True],
+    "electric_distance": ["mdi:map-marker-distance", None, LENGTH_KILOMETERS, True],
+    "saved_fuel": ["mdi:fuel", None, VOLUME_LITERS, False],
+    "total_distance": ["mdi:map-marker-distance", None, LENGTH_KILOMETERS, True],
 }
 
 ATTR_TO_HA_IMPERIAL = {
-    "mileage": ["mdi:speedometer", LENGTH_MILES, True],
-    "remaining_range_total": ["mdi:map-marker-distance", LENGTH_MILES, True],
-    "remaining_range_electric": ["mdi:map-marker-distance", LENGTH_MILES, True],
-    "remaining_range_fuel": ["mdi:map-marker-distance", LENGTH_MILES, True],
-    "max_range_electric": ["mdi:map-marker-distance", LENGTH_MILES, True],
-    "remaining_fuel": ["mdi:gas-station", VOLUME_GALLONS, True],
+    # "<ID>": [<MDI_ICON>, <DEVICE_CLASS>, <UNIT_OF_MEASUREMENT>, <ENABLED_BY_DEFAULT>],
+    "mileage": ["mdi:speedometer", None, LENGTH_MILES, True],
+    "remaining_range_total": ["mdi:map-marker-distance", None, LENGTH_MILES, True],
+    "remaining_range_electric": ["mdi:map-marker-distance", None, LENGTH_MILES, True],
+    "remaining_range_fuel": ["mdi:map-marker-distance", None, LENGTH_MILES, True],
+    "max_range_electric": ["mdi:map-marker-distance", None, LENGTH_MILES, True],
+    "remaining_fuel": ["mdi:gas-station", None, VOLUME_GALLONS, True],
     # LastTrip attributes
     "average_combined_consumption": [
         "mdi:flash",
+        None,
         f"{ENERGY_KILO_WATT_HOUR}/100{LENGTH_MILES}",
         True,
     ],
     "average_electric_consumption": [
         "mdi:power-plug-outline",
+        None,
         f"{ENERGY_KILO_WATT_HOUR}/100{LENGTH_MILES}",
         True,
     ],
     "average_recuperation": [
         "mdi:recycle-variant",
+        None,
         f"{ENERGY_KILO_WATT_HOUR}/100{LENGTH_MILES}",
         True,
     ],
-    "electric_distance": ["mdi:map-marker-distance", LENGTH_MILES, True],
-    "saved_fuel": ["mdi:fuel", VOLUME_GALLONS, False],
-    "total_distance": ["mdi:map-marker-distance", LENGTH_MILES, True],
+    "electric_distance": ["mdi:map-marker-distance", None, LENGTH_MILES, True],
+    "saved_fuel": ["mdi:fuel", None, VOLUME_GALLONS, False],
+    "total_distance": ["mdi:map-marker-distance", None, LENGTH_MILES, True],
 }
 
 ATTR_TO_HA_GENERIC = {
-    "charging_time_remaining": ["mdi:update", TIME_HOURS, True],
-    "charging_status": ["mdi:battery-charging", None, True],
+    # "<ID>": [<MDI_ICON>, <DEVICE_CLASS>, <UNIT_OF_MEASUREMENT>, <ENABLED_BY_DEFAULT>],
+    "charging_time_remaining": ["mdi:update", None, TIME_HOURS, True],
+    "charging_status": ["mdi:battery-charging", None, None, True],
     # No icon as this is dealt with directly as a special case in icon()
-    "charging_level_hv": [None, PERCENTAGE, True],
+    "charging_level_hv": [None, None, PERCENTAGE, True],
     # LastTrip attributes
-    "date_utc": ["mdi:calendar", None, True],
-    "duration": ["mdi:timer-outline", TIME_MINUTES, True],
-    "electric_distance_ratio": ["mdi:percent-outline", PERCENTAGE, False],
+    "date_utc": ["mdi:calendar", DEVICE_CLASS_TIMESTAMP, None, True],
+    "duration": ["mdi:timer-outline", None, TIME_MINUTES, True],
+    "electric_distance_ratio": ["mdi:percent-outline", None, PERCENTAGE, False],
 }
 
 ATTR_TO_HA_METRIC.update(ATTR_TO_HA_GENERIC)
@@ -178,15 +191,15 @@ class BMWConnectedDriveSensor(BMWConnectedDriveBaseEntity, SensorEntity):
             return icon_for_battery_level(
                 battery_level=vehicle_state.charging_level_hv, charging=charging_state
             )
-        icon = self._attribute_info.get(self._attribute, [None, None, None])[0]
+        icon = self._attribute_info.get(self._attribute, [None, None, None, None])[0]
         return icon
 
     @property
     def entity_registry_enabled_default(self) -> bool:
         """Return if the entity should be enabled when first added to the entity registry."""
-        enabled_default = self._attribute_info.get(self._attribute, [None, None, True])[
-            2
-        ]
+        enabled_default = self._attribute_info.get(
+            self._attribute, [None, None, None, True]
+        )[3]
         return enabled_default
 
     @property
@@ -199,9 +212,15 @@ class BMWConnectedDriveSensor(BMWConnectedDriveBaseEntity, SensorEntity):
         return self._state
 
     @property
+    def device_class(self) -> str:
+        """Get the device class."""
+        clss = self._attribute_info.get(self._attribute, [None, None, None, None])[1]
+        return clss
+
+    @property
     def unit_of_measurement(self) -> str:
         """Get the unit of measurement."""
-        unit = self._attribute_info.get(self._attribute, [None, None, None])[1]
+        unit = self._attribute_info.get(self._attribute, [None, None, None, None])[2]
         return unit
 
     def update(self) -> None:
@@ -224,6 +243,6 @@ class BMWConnectedDriveSensor(BMWConnectedDriveBaseEntity, SensorEntity):
         elif self._service == SERVICE_LAST_TRIP:
             if self._attribute == "date_utc":
                 date_str = getattr(vehicle_last_trip, "date")
-                self._state = dt_util.parse_datetime(date_str)
+                self._state = dt_util.parse_datetime(date_str).isoformat()
             else:
                 self._state = getattr(vehicle_last_trip, self._attribute)
