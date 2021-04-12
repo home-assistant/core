d687c62498f8add7e49942c395760b2c68e60a1a
tomasz75 <tomek@sviete.pl>
2021-04-12 18:22:11 +0200

    2021.4.3b0

--- homeassistant/components/ais_shell_command/__init__.py
+++ homeassistant/components/ais_shell_command/__init__.py
@@ -500,7 +500,11 @@ async def _disable_irda_remote(hass, call):
 
 
 async def _set_scaling_governor(hass, call):
-    if not ais_global.has_root() or multiprocessing.cpu_count() > 5:
+    if (
+        not ais_global.has_root()
+        or multiprocessing.cpu_count() > 5
+        or "4.9.113" in platform.uname()
+    ):
         return
     # /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
     scaling_available_governors = ["hotplug", "interactive", "performance"]
@@ -511,7 +515,6 @@ async def _set_scaling_governor(hass, call):
         scaling = call.data["scaling"]
     # default powersave freq
     # /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies
-    # scaling_available_frequencies = 100000 250000 500000 667000 1000000 1200000
     freq = "1000000"
     if scaling == "performance":
         freq = "1200000"
