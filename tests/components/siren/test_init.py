595ab94ad1eaec738a1411d580e1de8d923edd73
raman325 <7243222+raman325@users.noreply.github.com>
2021-04-13 00:13:58 -0400

    fix tests

--- tests/components/siren/test_init.py
+++ tests/components/siren/test_init.py
@@ -46,12 +46,23 @@ async def test_sync_set_volume_level(hass):
     assert siren.set_volume_level.called
 
 
-async def test_sync_set_active_tone(hass):
-    """Test if async set_active_tone calls sync set_active_tone."""
+async def test_sync_set_default_tone(hass):
+    """Test if async set_default_tone calls sync set_default_tone."""
     siren = MockSirenEntity()
     siren.hass = hass
 
-    siren.set_active_tone = MagicMock()
-    await siren.async_set_active_tone("fire")
+    siren.set_default_tone = MagicMock()
+    await siren.async_set_default_tone("fire")
 
-    assert siren.set_active_tone.called
+    assert siren.set_default_tone.called
+
+
+async def test_sync_set_default_duration(hass):
+    """Test if async set_default_duration calls sync set_default_duration."""
+    siren = MockSirenEntity()
+    siren.hass = hass
+
+    siren.set_default_duration = MagicMock()
+    await siren.async_set_default_duration(1)
+
+    assert siren.set_default_duration.called
