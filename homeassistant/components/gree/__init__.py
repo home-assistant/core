a68fdce65d1e09a0de8c7deed9c10a764805e33d
Clifford Roche <clifford.roche@gmail.com>
2021-04-13 05:54:03 -0400

    Improve the discovery process for Gree (#45449)
    * Add support for async device discovery
    
    * FIx missing dispatcher cleanup breaking integration reload
    
    * Update homeassistant/components/gree/climate.py
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    
    * Update homeassistant/components/gree/switch.py
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    
    * Update homeassistant/components/gree/bridge.py
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    
    * Working on feedback
    
    * Improving load/unload tests
    
    * Update homeassistant/components/gree/__init__.py
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    
    * Working on more feedback
    
    * Add tests covering async discovery scenarios
    
    * Remove unnecessary shutdown
    
    * Update homeassistant/components/gree/__init__.py
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    
    * Code refactor from reviews
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>

--- homeassistant/components/gree/__init__.py
+++ homeassistant/components/gree/__init__.py
@@ -1,14 +1,23 @@
 """The Gree Climate integration."""
 import asyncio
+from datetime import timedelta
 import logging
 
 from homeassistant.components.climate import DOMAIN as CLIMATE_DOMAIN
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.event import async_track_time_interval
 
-from .bridge import CannotConnect, DeviceDataUpdateCoordinator, DeviceHelper
-from .const import COORDINATOR, DOMAIN
+from .bridge import DiscoveryService
+from .const import (
+    COORDINATORS,
+    DATA_DISCOVERY_INTERVAL,
+    DATA_DISCOVERY_SERVICE,
+    DISCOVERY_SCAN_INTERVAL,
+    DISPATCHERS,
+    DOMAIN,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -21,31 +30,11 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Gree Climate from a config entry."""
-    devices = []
+    gree_discovery = DiscoveryService(hass)
+    hass.data[DATA_DISCOVERY_SERVICE] = gree_discovery
 
-    # First we'll grab as many devices as we can find on the network
-    # it's necessary to bind static devices anyway
-    _LOGGER.debug("Scanning network for Gree devices")
+    hass.data[DOMAIN].setdefault(DISPATCHERS, [])
 
-    for device_info in await DeviceHelper.find_devices():
-        try:
-            device = await DeviceHelper.try_bind_device(device_info)
-        except CannotConnect:
-            _LOGGER.error("Unable to bind to gree device: %s", device_info)
-            continue
-
-        _LOGGER.debug(
-            "Adding Gree device at %s:%i (%s)",
-            device.device_info.ip,
-            device.device_info.port,
-            device.device_info.name,
-        )
-        devices.append(device)
-
-    coordinators = [DeviceDataUpdateCoordinator(hass, d) for d in devices]
-    await asyncio.gather(*[x.async_refresh() for x in coordinators])
-
-    hass.data[DOMAIN][COORDINATOR] = coordinators
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, CLIMATE_DOMAIN)
     )
@@ -53,11 +42,31 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         hass.config_entries.async_forward_entry_setup(entry, SWITCH_DOMAIN)
     )
 
+    async def _async_scan_update(_=None):
+        await gree_discovery.discovery.scan()
+
+    _LOGGER.debug("Scanning network for Gree devices")
+    await _async_scan_update()
+
+    hass.data[DOMAIN][DATA_DISCOVERY_INTERVAL] = async_track_time_interval(
+        hass, _async_scan_update, timedelta(seconds=DISCOVERY_SCAN_INTERVAL)
+    )
+
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
+    if hass.data[DOMAIN].get(DISPATCHERS) is not None:
+        for cleanup in hass.data[DOMAIN][DISPATCHERS]:
+            cleanup()
+
+    if hass.data[DOMAIN].get(DATA_DISCOVERY_INTERVAL) is not None:
+        hass.data[DOMAIN].pop(DATA_DISCOVERY_INTERVAL)()
+
+    if hass.data.get(DATA_DISCOVERY_SERVICE) is not None:
+        hass.data.pop(DATA_DISCOVERY_SERVICE)
+
     results = asyncio.gather(
         hass.config_entries.async_forward_entry_unload(entry, CLIMATE_DOMAIN),
         hass.config_entries.async_forward_entry_unload(entry, SWITCH_DOMAIN),
@@ -65,8 +74,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     unload_ok = all(await results)
     if unload_ok:
-        hass.data[DOMAIN].pop("devices", None)
-        hass.data[DOMAIN].pop(CLIMATE_DOMAIN, None)
-        hass.data[DOMAIN].pop(SWITCH_DOMAIN, None)
+        hass.data[DOMAIN].pop(COORDINATORS, None)
+        hass.data[DOMAIN].pop(DISPATCHERS, None)
 
     return unload_ok
