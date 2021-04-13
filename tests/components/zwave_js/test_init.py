7db737df238583684312677db7bacf19f25ccfa5
raman325 <7243222+raman325@users.noreply.github.com>
2021-04-12 23:26:16 -0400

    test that we skip migration when multiple entities are found

--- tests/components/zwave_js/test_init.py
+++ tests/components/zwave_js/test_init.py
@@ -302,7 +302,7 @@ async def test_old_entity_migration(
     SENSOR_NAME = "sensor.smart_plug_with_two_usb_ports_value_electric_consumed"
     entity_name = SENSOR_NAME.split(".")[1]
 
-    # Create entity RegistryEntry using old unique ID format
+    # Create entity RegistryEntry using fake endpoint
     old_unique_id = f"{client.driver.controller.home_id}.32-50-1-value-66049"
     entity_entry = ent_reg.async_get_or_create(
         "sensor",
@@ -328,6 +328,63 @@ async def test_old_entity_migration(
         new_unique_id = f"{client.driver.controller.home_id}.32-50-0-value-66049"
         assert entity_entry.unique_id == new_unique_id
         assert ent_reg.async_get_entity_id("sensor", DOMAIN, old_unique_id) is None
+
+
+async def test_skip_old_entity_migration_for_multiiple(
+    hass, hank_binary_switch_state, client, integration
+):
+    """Test that multiple entities of the same value but on a different endpoint get skipped."""
+    node = Node(client, hank_binary_switch_state)
+
+    ent_reg = er.async_get(hass)
+    dev_reg = dr.async_get(hass)
+    device = dev_reg.async_get_or_create(
+        config_entry_id=integration.entry_id, identifiers={get_device_id(client, node)}
+    )
+
+    SENSOR_NAME = "sensor.smart_plug_with_two_usb_ports_value_electric_consumed"
+    entity_name = SENSOR_NAME.split(".")[1]
+
+    # Create two entity entrrys using different endpoints
+    old_unique_id_1 = f"{client.driver.controller.home_id}.32-50-1-value-66049"
+    entity_entry = ent_reg.async_get_or_create(
+        "sensor",
+        DOMAIN,
+        old_unique_id_1,
+        suggested_object_id=f"{entity_name}_1",
+        config_entry=integration,
+        original_name=f"{entity_name}_1",
+        device_id=device.id,
+    )
+    assert entity_entry.entity_id == f"{SENSOR_NAME}_1"
+    assert entity_entry.unique_id == old_unique_id_1
+
+    # Create two entity entrrys using different endpoints
+    old_unique_id_2 = f"{client.driver.controller.home_id}.32-50-2-value-66049"
+    entity_entry = ent_reg.async_get_or_create(
+        "sensor",
+        DOMAIN,
+        old_unique_id_2,
+        suggested_object_id=f"{entity_name}_2",
+        config_entry=integration,
+        original_name=f"{entity_name}_2",
+        device_id=device.id,
+    )
+    assert entity_entry.entity_id == f"{SENSOR_NAME}_2"
+    assert entity_entry.unique_id == old_unique_id_2
+    # Add a ready node, unique ID should be migrated
+    event = {"node": node}
+    client.driver.controller.emit("node added", event)
+    await hass.async_block_till_done()
+
+    # Check that new RegistryEntry is created using new unique ID format
+    entity_entry = ent_reg.async_get(SENSOR_NAME)
+    new_unique_id = f"{client.driver.controller.home_id}.32-50-0-value-66049"
+    assert entity_entry.unique_id == new_unique_id
+
+    # Check that the old entities stuck around because we skipped the migration step
+    assert ent_reg.async_get_entity_id("sensor", DOMAIN, old_unique_id_1)
+    assert ent_reg.async_get_entity_id("sensor", DOMAIN, old_unique_id_2)
 
 
 async def test_old_entity_migration_notification_binary_sensor(
