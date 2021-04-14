dd414c68d55b29b0b6eba476aff0c8ee9af4e2a4
David Mulcahey <david.mulcahey@me.com>
2021-04-14 07:31:30 -0400

    dynamically include alarm control panel config

--- homeassistant/components/zha/core/helpers.py
+++ homeassistant/components/zha/core/helpers.py
@@ -139,6 +139,18 @@ def async_get_zha_config_value(config_entry, section, config_key, default):
     )
 
 
+def async_input_cluster_exists(hass, cluster_id):
+    """Determine if a device containing the specified in cluster is paired."""
+    zha_gateway = hass.data[DATA_ZHA][DATA_ZHA_GATEWAY]
+    zha_devices = zha_gateway.devices.values()
+    for zha_device in zha_devices:
+        clusters_by_endpoint = zha_device.async_get_clusters()
+        for clusters in clusters_by_endpoint.values():
+            if cluster_id in clusters[CLUSTER_TYPE_IN]:
+                return True
+    return False
+
+
 async def async_get_zha_device(hass, device_id):
     """Get a ZHA device for the given device registry id."""
     device_registry = await hass.helpers.device_registry.async_get_registry()
