35f154c5262081075de8889c5ae71e205a0c53d6
Diogo Gomes <dgomes@ua.pt>
2021-04-14 01:00:51 +0100

    use contract as unique_id

--- homeassistant/components/prosegur/config_flow.py
+++ homeassistant/components/prosegur/config_flow.py
@@ -58,6 +58,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input:
             try:
                 info = await validate_input(self.hass, user_input)
+
+                await self.async_set_unique_id(info["contract"])
+                self._abort_if_unique_id_configured()
+
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except InvalidAuth:
