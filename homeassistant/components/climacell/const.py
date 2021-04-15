62fe44808cc42cec931ef84002d8c2097089a2b3
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-04-15 16:31:59 -0400

    Add sensors for other ClimaCell data (#49259)
    * Add sensors for other ClimaCell data
    
    * add tests and add rounding
    
    * docstrings
    
    * fix pressure
    
    * Update homeassistant/components/climacell/sensor.py
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * Update homeassistant/components/climacell/sensor.py
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * review comments
    
    * add another abstractmethod
    
    * use superscript
    
    * remove mypy ignore
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/climacell/const.py
+++ homeassistant/components/climacell/const.py
@@ -1,5 +1,13 @@
 """Constants for the ClimaCell integration."""
-from pyclimacell.const import DAILY, HOURLY, NOWCAST, WeatherCode
+from pyclimacell.const import (
+    DAILY,
+    HOURLY,
+    NOWCAST,
+    HealthConcernType,
+    PollenIndex,
+    PrimaryPollutantType,
+    WeatherCode,
+)
 
 from homeassistant.components.weather import (
     ATTR_CONDITION_CLEAR_NIGHT,
@@ -14,6 +22,15 @@ from homeassistant.components.weather import (
     ATTR_CONDITION_SNOWY_RAINY,
     ATTR_CONDITION_SUNNY,
     ATTR_CONDITION_WINDY,
+)
+from homeassistant.const import (
+    ATTR_NAME,
+    CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    CONCENTRATION_PARTS_PER_BILLION,
+    CONCENTRATION_PARTS_PER_MILLION,
+    CONF_UNIT_OF_MEASUREMENT,
+    CONF_UNIT_SYSTEM_IMPERIAL,
+    CONF_UNIT_SYSTEM_METRIC,
 )
 
 CONF_TIMESTEP = "timestep"
@@ -34,6 +51,12 @@ MAX_FORECASTS = {
     HOURLY: 24,
     NOWCAST: 30,
 }
+
+# Sensor type keys
+ATTR_FIELD = "field"
+ATTR_METRIC_CONVERSION = "metric_conversion"
+ATTR_VALUE_MAP = "value_map"
+ATTR_IS_METRIC_CHECK = "is_metric_check"
 
 # Additional attributes
 ATTR_WIND_GUST = "wind_gust"
@@ -68,6 +91,7 @@ CONDITIONS = {
     WeatherCode.PARTLY_CLOUDY: ATTR_CONDITION_PARTLYCLOUDY,
 }
 
+# Weather constants
 CC_ATTR_TIMESTAMP = "startTime"
 CC_ATTR_TEMPERATURE = "temperature"
 CC_ATTR_TEMPERATURE_HIGH = "temperatureMax"
@@ -84,6 +108,95 @@ CC_ATTR_PRECIPITATION_PROBABILITY = "precipitationProbability"
 CC_ATTR_WIND_GUST = "windGust"
 CC_ATTR_CLOUD_COVER = "cloudCover"
 CC_ATTR_PRECIPITATION_TYPE = "precipitationType"
+
+# Sensor attributes
+CC_ATTR_PARTICULATE_MATTER_25 = "particulateMatter25"
+CC_ATTR_PARTICULATE_MATTER_10 = "particulateMatter10"
+CC_ATTR_NITROGEN_DIOXIDE = "pollutantNO2"
+CC_ATTR_CARBON_MONOXIDE = "pollutantCO"
+CC_ATTR_SULFUR_DIOXIDE = "pollutantSO2"
+CC_ATTR_EPA_AQI = "epaIndex"
+CC_ATTR_EPA_PRIMARY_POLLUTANT = "epaPrimaryPollutant"
+CC_ATTR_EPA_HEALTH_CONCERN = "epaHealthConcern"
+CC_ATTR_CHINA_AQI = "mepIndex"
+CC_ATTR_CHINA_PRIMARY_POLLUTANT = "mepPrimaryPollutant"
+CC_ATTR_CHINA_HEALTH_CONCERN = "mepHealthConcern"
+CC_ATTR_POLLEN_TREE = "treeIndex"
+CC_ATTR_POLLEN_WEED = "weedIndex"
+CC_ATTR_POLLEN_GRASS = "grassIndex"
+CC_ATTR_FIRE_INDEX = "fireIndex"
+
+CC_SENSOR_TYPES = [
+    {
+        ATTR_FIELD: CC_ATTR_PARTICULATE_MATTER_25,
+        ATTR_NAME: "Particulate Matter < 2.5 μm",
+        CONF_UNIT_SYSTEM_IMPERIAL: "μg/ft³",
+        CONF_UNIT_SYSTEM_METRIC: CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+        ATTR_METRIC_CONVERSION: 3.2808399 ** 3,
+        ATTR_IS_METRIC_CHECK: True,
+    },
+    {
+        ATTR_FIELD: CC_ATTR_PARTICULATE_MATTER_10,
+        ATTR_NAME: "Particulate Matter < 10 μm",
+        CONF_UNIT_SYSTEM_IMPERIAL: "μg/ft³",
+        CONF_UNIT_SYSTEM_METRIC: CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+        ATTR_METRIC_CONVERSION: 3.2808399 ** 3,
+        ATTR_IS_METRIC_CHECK: True,
+    },
+    {
+        ATTR_FIELD: CC_ATTR_NITROGEN_DIOXIDE,
+        ATTR_NAME: "Nitrogen Dioxide",
+        CONF_UNIT_OF_MEASUREMENT: CONCENTRATION_PARTS_PER_BILLION,
+    },
+    {
+        ATTR_FIELD: CC_ATTR_CARBON_MONOXIDE,
+        ATTR_NAME: "Carbon Monoxide",
+        CONF_UNIT_OF_MEASUREMENT: CONCENTRATION_PARTS_PER_BILLION,
+    },
+    {
+        ATTR_FIELD: CC_ATTR_SULFUR_DIOXIDE,
+        ATTR_NAME: "Sulfur Dioxide",
+        CONF_UNIT_OF_MEASUREMENT: CONCENTRATION_PARTS_PER_BILLION,
+    },
+    {ATTR_FIELD: CC_ATTR_EPA_AQI, ATTR_NAME: "US EPA Air Quality Index"},
+    {
+        ATTR_FIELD: CC_ATTR_EPA_PRIMARY_POLLUTANT,
+        ATTR_NAME: "US EPA Primary Pollutant",
+        ATTR_VALUE_MAP: PrimaryPollutantType,
+    },
+    {
+        ATTR_FIELD: CC_ATTR_EPA_HEALTH_CONCERN,
+        ATTR_NAME: "US EPA Health Concern",
+        ATTR_VALUE_MAP: HealthConcernType,
+    },
+    {ATTR_FIELD: CC_ATTR_CHINA_AQI, ATTR_NAME: "China MEP Air Quality Index"},
+    {
+        ATTR_FIELD: CC_ATTR_CHINA_PRIMARY_POLLUTANT,
+        ATTR_NAME: "China MEP Primary Pollutant",
+        ATTR_VALUE_MAP: PrimaryPollutantType,
+    },
+    {
+        ATTR_FIELD: CC_ATTR_CHINA_HEALTH_CONCERN,
+        ATTR_NAME: "China MEP Health Concern",
+        ATTR_VALUE_MAP: HealthConcernType,
+    },
+    {
+        ATTR_FIELD: CC_ATTR_POLLEN_TREE,
+        ATTR_NAME: "Tree Pollen Index",
+        ATTR_VALUE_MAP: PollenIndex,
+    },
+    {
+        ATTR_FIELD: CC_ATTR_POLLEN_WEED,
+        ATTR_NAME: "Weed Pollen Index",
+        ATTR_VALUE_MAP: PollenIndex,
+    },
+    {
+        ATTR_FIELD: CC_ATTR_POLLEN_GRASS,
+        ATTR_NAME: "Grass Pollen Index",
+        ATTR_VALUE_MAP: PollenIndex,
+    },
+    {ATTR_FIELD: CC_ATTR_FIRE_INDEX, ATTR_NAME: "Fire Index"},
+]
 
 # V3 constants
 CONDITIONS_V3 = {
@@ -111,6 +224,7 @@ CONDITIONS_V3 = {
     "partly_cloudy": ATTR_CONDITION_PARTLYCLOUDY,
 }
 
+# Weather attributes
 CC_V3_ATTR_TIMESTAMP = "observation_time"
 CC_V3_ATTR_TEMPERATURE = "temp"
 CC_V3_ATTR_TEMPERATURE_HIGH = "max"
@@ -128,3 +242,73 @@ CC_V3_ATTR_PRECIPITATION_PROBABILITY = "precipitation_probability"
 CC_V3_ATTR_WIND_GUST = "wind_gust"
 CC_V3_ATTR_CLOUD_COVER = "cloud_cover"
 CC_V3_ATTR_PRECIPITATION_TYPE = "precipitation_type"
+
+# Sensor attributes
+CC_V3_ATTR_PARTICULATE_MATTER_25 = "pm25"
+CC_V3_ATTR_PARTICULATE_MATTER_10 = "pm10"
+CC_V3_ATTR_NITROGEN_DIOXIDE = "no2"
+CC_V3_ATTR_CARBON_MONOXIDE = "co"
+CC_V3_ATTR_SULFUR_DIOXIDE = "so2"
+CC_V3_ATTR_EPA_AQI = "epa_aqi"
+CC_V3_ATTR_EPA_PRIMARY_POLLUTANT = "epa_primary_pollutant"
+CC_V3_ATTR_EPA_HEALTH_CONCERN = "epa_health_concern"
+CC_V3_ATTR_CHINA_AQI = "china_aqi"
+CC_V3_ATTR_CHINA_PRIMARY_POLLUTANT = "china_primary_pollutant"
+CC_V3_ATTR_CHINA_HEALTH_CONCERN = "china_health_concern"
+CC_V3_ATTR_POLLEN_TREE = "pollen_tree"
+CC_V3_ATTR_POLLEN_WEED = "pollen_weed"
+CC_V3_ATTR_POLLEN_GRASS = "pollen_grass"
+CC_V3_ATTR_FIRE_INDEX = "fire_index"
+
+CC_V3_SENSOR_TYPES = [
+    {
+        ATTR_FIELD: CC_V3_ATTR_PARTICULATE_MATTER_25,
+        ATTR_NAME: "Particulate Matter < 2.5 μm",
+        CONF_UNIT_SYSTEM_IMPERIAL: "μg/ft³",
+        CONF_UNIT_SYSTEM_METRIC: CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+        ATTR_METRIC_CONVERSION: 1 / (3.2808399 ** 3),
+        ATTR_IS_METRIC_CHECK: False,
+    },
+    {
+        ATTR_FIELD: CC_V3_ATTR_PARTICULATE_MATTER_10,
+        ATTR_NAME: "Particulate Matter < 10 μm",
+        CONF_UNIT_SYSTEM_IMPERIAL: "μg/ft³",
+        CONF_UNIT_SYSTEM_METRIC: CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+        ATTR_METRIC_CONVERSION: 1 / (3.2808399 ** 3),
+        ATTR_IS_METRIC_CHECK: False,
+    },
+    {
+        ATTR_FIELD: CC_V3_ATTR_NITROGEN_DIOXIDE,
+        ATTR_NAME: "Nitrogen Dioxide",
+        CONF_UNIT_OF_MEASUREMENT: CONCENTRATION_PARTS_PER_BILLION,
+    },
+    {
+        ATTR_FIELD: CC_V3_ATTR_CARBON_MONOXIDE,
+        ATTR_NAME: "Carbon Monoxide",
+        CONF_UNIT_OF_MEASUREMENT: CONCENTRATION_PARTS_PER_MILLION,
+    },
+    {
+        ATTR_FIELD: CC_V3_ATTR_SULFUR_DIOXIDE,
+        ATTR_NAME: "Sulfur Dioxide",
+        CONF_UNIT_OF_MEASUREMENT: CONCENTRATION_PARTS_PER_BILLION,
+    },
+    {ATTR_FIELD: CC_V3_ATTR_EPA_AQI, ATTR_NAME: "US EPA Air Quality Index"},
+    {
+        ATTR_FIELD: CC_V3_ATTR_EPA_PRIMARY_POLLUTANT,
+        ATTR_NAME: "US EPA Primary Pollutant",
+    },
+    {ATTR_FIELD: CC_V3_ATTR_EPA_HEALTH_CONCERN, ATTR_NAME: "US EPA Health Concern"},
+    {ATTR_FIELD: CC_V3_ATTR_CHINA_AQI, ATTR_NAME: "China MEP Air Quality Index"},
+    {
+        ATTR_FIELD: CC_V3_ATTR_CHINA_PRIMARY_POLLUTANT,
+        ATTR_NAME: "China MEP Primary Pollutant",
+    },
+    {
+        ATTR_FIELD: CC_V3_ATTR_CHINA_HEALTH_CONCERN,
+        ATTR_NAME: "China MEP Health Concern",
+    },
+    {ATTR_FIELD: CC_V3_ATTR_POLLEN_TREE, ATTR_NAME: "Tree Pollen Index"},
+    {ATTR_FIELD: CC_V3_ATTR_POLLEN_WEED, ATTR_NAME: "Weed Pollen Index"},
+    {ATTR_FIELD: CC_V3_ATTR_POLLEN_GRASS, ATTR_NAME: "Grass Pollen Index"},
+    {ATTR_FIELD: CC_V3_ATTR_FIRE_INDEX, ATTR_NAME: "Fire Index"},
+]
---
+++ tests/components/climacell/test_sensor.py
@@ -0,0 +1,148 @@
+"""Tests for Climacell sensor entities."""
+from __future__ import annotations
+
+from datetime import datetime
+import logging
+from typing import Any
+from unittest.mock import patch
+
+import pytest
+import pytz
+
+from homeassistant.components.climacell.config_flow import (
+    _get_config_schema,
+    _get_unique_id,
+)
+from homeassistant.components.climacell.const import ATTRIBUTION, DOMAIN
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
+from homeassistant.const import ATTR_ATTRIBUTION
+from homeassistant.core import State, callback
+from homeassistant.helpers.entity_registry import async_get
+from homeassistant.helpers.typing import HomeAssistantType
+
+from .const import API_V3_ENTRY_DATA, API_V4_ENTRY_DATA
+
+from tests.common import MockConfigEntry
+
+_LOGGER = logging.getLogger(__name__)
+CC_SENSOR_ENTITY_ID = "sensor.climacell_{}"
+
+CO = "carbon_monoxide"
+NO2 = "nitrogen_dioxide"
+SO2 = "sulfur_dioxide"
+PM25 = "particulate_matter_2_5_mm"
+PM10 = "particulate_matter_10_mm"
+MEP_AQI = "china_mep_air_quality_index"
+MEP_HEALTH_CONCERN = "china_mep_health_concern"
+MEP_PRIMARY_POLLUTANT = "china_mep_primary_pollutant"
+EPA_AQI = "us_epa_air_quality_index"
+EPA_HEALTH_CONCERN = "us_epa_health_concern"
+EPA_PRIMARY_POLLUTANT = "us_epa_primary_pollutant"
+FIRE_INDEX = "fire_index"
+GRASS_POLLEN = "grass_pollen_index"
+WEED_POLLEN = "weed_pollen_index"
+TREE_POLLEN = "tree_pollen_index"
+
+
+@callback
+def _enable_entity(hass: HomeAssistantType, entity_name: str) -> None:
+    """Enable disabled entity."""
+    ent_reg = async_get(hass)
+    entry = ent_reg.async_get(entity_name)
+    updated_entry = ent_reg.async_update_entity(
+        entry.entity_id, **{"disabled_by": None}
+    )
+    assert updated_entry != entry
+    assert updated_entry.disabled is False
+
+
+async def _setup(hass: HomeAssistantType, config: dict[str, Any]) -> State:
+    """Set up entry and return entity state."""
+    with patch(
+        "homeassistant.util.dt.utcnow",
+        return_value=datetime(2021, 3, 6, 23, 59, 59, tzinfo=pytz.UTC),
+    ):
+        data = _get_config_schema(hass)(config)
+        config_entry = MockConfigEntry(
+            domain=DOMAIN,
+            data=data,
+            unique_id=_get_unique_id(hass, data),
+            version=1,
+        )
+        config_entry.add_to_hass(hass)
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+        for entity_name in (
+            CO,
+            NO2,
+            SO2,
+            PM25,
+            PM10,
+            MEP_AQI,
+            MEP_HEALTH_CONCERN,
+            MEP_PRIMARY_POLLUTANT,
+            EPA_AQI,
+            EPA_HEALTH_CONCERN,
+            EPA_PRIMARY_POLLUTANT,
+            FIRE_INDEX,
+            GRASS_POLLEN,
+            WEED_POLLEN,
+            TREE_POLLEN,
+        ):
+            _enable_entity(hass, CC_SENSOR_ENTITY_ID.format(entity_name))
+        await hass.async_block_till_done()
+        assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 15
+
+
+def check_sensor_state(hass: HomeAssistantType, entity_name: str, value: str):
+    """Check the state of a ClimaCell sensor."""
+    state = hass.states.get(CC_SENSOR_ENTITY_ID.format(entity_name))
+    assert state
+    assert state.state == value
+    assert state.attributes[ATTR_ATTRIBUTION] == ATTRIBUTION
+
+
+async def test_v3_sensor(
+    hass: HomeAssistantType,
+    climacell_config_entry_update: pytest.fixture,
+) -> None:
+    """Test v3 sensor data."""
+    await _setup(hass, API_V3_ENTRY_DATA)
+    check_sensor_state(hass, CO, "0.875")
+    check_sensor_state(hass, NO2, "14.1875")
+    check_sensor_state(hass, SO2, "2")
+    check_sensor_state(hass, PM25, "5.3125")
+    check_sensor_state(hass, PM10, "27")
+    check_sensor_state(hass, MEP_AQI, "27")
+    check_sensor_state(hass, MEP_HEALTH_CONCERN, "Good")
+    check_sensor_state(hass, MEP_PRIMARY_POLLUTANT, "pm10")
+    check_sensor_state(hass, EPA_AQI, "22.3125")
+    check_sensor_state(hass, EPA_HEALTH_CONCERN, "Good")
+    check_sensor_state(hass, EPA_PRIMARY_POLLUTANT, "pm25")
+    check_sensor_state(hass, FIRE_INDEX, "9")
+    check_sensor_state(hass, GRASS_POLLEN, "0")
+    check_sensor_state(hass, WEED_POLLEN, "0")
+    check_sensor_state(hass, TREE_POLLEN, "0")
+
+
+async def test_v4_sensor(
+    hass: HomeAssistantType,
+    climacell_config_entry_update: pytest.fixture,
+) -> None:
+    """Test v4 sensor data."""
+    await _setup(hass, API_V4_ENTRY_DATA)
+    check_sensor_state(hass, CO, "0.63")
+    check_sensor_state(hass, NO2, "10.67")
+    check_sensor_state(hass, SO2, "1.65")
+    check_sensor_state(hass, PM25, "5.2972")
+    check_sensor_state(hass, PM10, "20.1294")
+    check_sensor_state(hass, MEP_AQI, "23")
+    check_sensor_state(hass, MEP_HEALTH_CONCERN, "good")
+    check_sensor_state(hass, MEP_PRIMARY_POLLUTANT, "pm10")
+    check_sensor_state(hass, EPA_AQI, "24")
+    check_sensor_state(hass, EPA_HEALTH_CONCERN, "good")
+    check_sensor_state(hass, EPA_PRIMARY_POLLUTANT, "pm25")
+    check_sensor_state(hass, FIRE_INDEX, "10")
+    check_sensor_state(hass, GRASS_POLLEN, "none")
+    check_sensor_state(hass, WEED_POLLEN, "none")
+    check_sensor_state(hass, TREE_POLLEN, "none")
