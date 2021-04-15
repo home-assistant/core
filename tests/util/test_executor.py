758c4dbf1c2141d9aa50fbc530e6650c680fb92e
J. Nick Koston <nick@koston.org>
2021-04-14 16:26:26 -1000

    coverage

--- tests/util/test_executor.py
+++ tests/util/test_executor.py
@@ -30,3 +30,22 @@ async def test_executor_shutdown_can_interrupt_threads(caplog):
 
     assert "is still running at shutdown" in caplog.text
     assert "time.sleep(0.1)" in caplog.text
+
+
+async def test_executor_shutdown_without_interrupt(caplog):
+    """Test that the executor shutdown without interrupt."""
+
+    executor = InterruptibleThreadPoolExecutor()
+
+    def _loop_sleep_in_executor():
+        time.sleep(0.1)
+        return 50
+
+    future = executor.submit(_loop_sleep_in_executor)
+
+    executor.shutdown(cancel_futures=True, wait=True, interrupt=False)
+
+    assert future.result() == 50
+
+    assert "is still running at shutdown" not in caplog.text
+    assert "time.sleep(0.1)" not in caplog.text
