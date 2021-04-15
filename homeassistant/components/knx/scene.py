2c579d70525a44ef049eaeea58b2ace7dec1a68f
farmio <farmio@alphart.net>
2021-04-15 20:49:03 +0200

    create scene entities directly from config

--- homeassistant/components/knx/scene.py
+++ homeassistant/components/knx/scene.py
@@ -3,15 +3,18 @@ from __future__ import annotations
 
 from typing import Any, Callable, Iterable
 
+from xknx import XKNX
 from xknx.devices import Scene as XknxScene
 
 from homeassistant.components.scene import Scene
+from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-from .const import DOMAIN
+from .const import DOMAIN, KNX_ADDRESS
 from .knx_entity import KnxEntity
+from .schema import SceneSchema
 
 
 async def async_setup_platform(
@@ -21,20 +24,33 @@ async def async_setup_platform(
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the scenes for KNX platform."""
+    if not discovery_info or not discovery_info["platform_config"]:
+        return
+
+    platform_config = discovery_info["platform_config"]
+    xknx: XKNX = hass.data[DOMAIN].xknx
+
     entities = []
-    for device in hass.data[DOMAIN].xknx.devices:
-        if isinstance(device, XknxScene):
-            entities.append(KNXScene(device))
+    for entity_config in platform_config:
+        entities.append(KNXScene(xknx, entity_config))
+
     async_add_entities(entities)
 
 
 class KNXScene(KnxEntity, Scene):
     """Representation of a KNX scene."""
 
-    def __init__(self, device: XknxScene) -> None:
+    def __init__(self, xknx: XKNX, config: ConfigType) -> None:
         """Init KNX scene."""
         self._device: XknxScene
-        super().__init__(device)
+        super().__init__(
+            device=XknxScene(
+                xknx,
+                name=config[CONF_NAME],
+                group_address=config[KNX_ADDRESS],
+                scene_number=config[SceneSchema.CONF_SCENE_NUMBER],
+            )
+        )
 
     async def async_activate(self, **kwargs: Any) -> None:
         """Activate the scene."""
