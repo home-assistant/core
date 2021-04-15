89f992385c30cc4e0d7c53dbb243bec1d74a8027
J. Nick Koston <nick@koston.org>
2021-04-14 16:46:09 -1000

    tweaks

--- homeassistant/util/executor.py
+++ homeassistant/util/executor.py
@@ -10,6 +10,10 @@ from homeassistant.util.thread import async_raise
 
 _LOGGER = logging.getLogger(__name__)
 
+_JOIN_ATTEMPTS = 10
+
+START_LOG_ATTEMPT = 5
+
 
 def _log_thread_running_at_shutdown(name: str, ident: int) -> None:
     """Log the stack of a thread that was still running at shutdown."""
@@ -27,42 +31,26 @@ def _log_thread_running_at_shutdown(name: str, ident: int) -> None:
 class InterruptibleThreadPoolExecutor(ThreadPoolExecutor):
     """A ThreadPoolExecutor instance that will not deadlock on shutdown."""
 
-    def shutdown(
-        self,
-        wait: bool = True,
-        *,
-        cancel_futures: bool = False,
-        interrupt: bool = False,
-    ) -> None:
+    def logged_shutdown(self) -> None:
         """Shutdown backport from cpython 3.9 with interrupt support added."""
         with self._shutdown_lock:  # type: ignore[attr-defined]
             self._shutdown = True
-            if cancel_futures:
-                # Drain all work items from the queue, and then cancel their
-                # associated futures.
-                while True:
-                    try:
-                        work_item = self._work_queue.get_nowait()
-                    except queue.Empty:
-                        break
-                    if work_item is not None:
-                        work_item.future.cancel()
-
+            # Drain all work items from the queue, and then cancel their
+            # associated futures.
+            while True:
+                try:
+                    work_item = self._work_queue.get_nowait()
+                except queue.Empty:
+                    break
+                if work_item is not None:
+                    work_item.future.cancel()
             # Send a wake-up to prevent threads calling
             # _work_queue.get(block=True) from permanently blocking.
             self._work_queue.put(None)
-        if wait:
-            if interrupt:
-                self._join_or_interrupt_threads()
-            else:
-                for t in self._threads:  # type: ignore[attr-defined]
-                    t.join()
 
-    def _join_or_interrupt_threads(self) -> None:
-        """Join or interrupt threads."""
         remaining_threads = list(self._threads)  # type: ignore[attr-defined]
 
-        while True:
+        for attempt in range(1, _JOIN_ATTEMPTS + 1):
             joined = []
 
             for thread in remaining_threads:
@@ -71,7 +59,10 @@ class InterruptibleThreadPoolExecutor(ThreadPoolExecutor):
                 if not thread.is_alive() or ident is None:
                     joined.append(thread)
                     continue
-                _log_thread_running_at_shutdown(thread.name, ident)
+
+                if attempt >= START_LOG_ATTEMPT:
+                    _log_thread_running_at_shutdown(thread.name, ident)
+
                 async_raise(ident, SystemExit)
                 thread.join(timeout=1)
 
--- tests/util/test_executor.py
+++ tests/util/test_executor.py
@@ -2,16 +2,18 @@
 
 import concurrent.futures
 import time
+from unittest.mock import patch
 
 import pytest
 
+from homeassistant.util import executor
 from homeassistant.util.executor import InterruptibleThreadPoolExecutor
 
 
 async def test_executor_shutdown_can_interrupt_threads(caplog):
     """Test that the executor shutdown can interrupt threads."""
 
-    executor = InterruptibleThreadPoolExecutor()
+    iexecutor = InterruptibleThreadPoolExecutor()
 
     def _loop_sleep_in_executor():
         while True:
@@ -20,9 +22,10 @@ async def test_executor_shutdown_can_interrupt_threads(caplog):
     sleep_futures = []
 
     for _ in range(100):
-        sleep_futures.append(executor.submit(_loop_sleep_in_executor))
+        sleep_futures.append(iexecutor.submit(_loop_sleep_in_executor))
 
-    executor.shutdown(cancel_futures=True, wait=True, interrupt=True)
+    with patch.object(executor, "START_LOG_ATTEMPT", 1):
+        iexecutor.logged_shutdown()
 
     for future in sleep_futures:
         with pytest.raises((concurrent.futures.CancelledError, SystemExit)):
@@ -35,15 +38,16 @@ async def test_executor_shutdown_can_interrupt_threads(caplog):
 async def test_executor_shutdown_without_interrupt(caplog):
     """Test that the executor shutdown without interrupt."""
 
-    executor = InterruptibleThreadPoolExecutor()
+    iexecutor = InterruptibleThreadPoolExecutor()
 
     def _loop_sleep_in_executor():
         time.sleep(0.1)
         return 50
 
-    future = executor.submit(_loop_sleep_in_executor)
+    future = iexecutor.submit(_loop_sleep_in_executor)
 
-    executor.shutdown(cancel_futures=True, wait=True, interrupt=False)
+    with patch.object(executor, "START_LOG_ATTEMPT", 1):
+        iexecutor.logged_shutdown()
 
     assert future.result() == 50
 
