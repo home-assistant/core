ad967cfebb875cbedf1930490a49041d5a6a6cf7
Milan Meulemans <milan.meulemans@live.be>
2021-04-17 15:41:45 +0200

    Rituals Perfume Genie improvements (#49277)
    * Rituals Perfume Genie integration improvements
    
    * Add return type FlowResultDict to async_step_user
    
    * Rollback async_update_data
    
    * Add return type to DiffuserEntity init
    
    * check super().available too
    
    * Merge iterations
    
    * Use RitualsPerufmeGenieDataUpdateCoordinator

--- homeassistant/components/rituals_perfume_genie/switch.py
+++ homeassistant/components/rituals_perfume_genie/switch.py
@@ -1,20 +1,28 @@
 """Support for Rituals Perfume Genie switches."""
+from __future__ import annotations
+
+from typing import Any, Callable
+
+from pyrituals import Diffuser
+
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.core import callback
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import ATTRIBUTES, COORDINATORS, DEVICES, DOMAIN, HUB
 from .entity import DiffuserEntity
 
-STATUS = "status"
 FAN = "fanc"
 SPEED = "speedc"
 ROOM = "roomc"
 
 ON_STATE = "1"
-AVAILABLE_STATE = 1
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities: Callable
+) -> None:
     """Set up the diffuser switch."""
     diffusers = hass.data[DOMAIN][config_entry.entry_id][DEVICES]
     coordinators = hass.data[DOMAIN][config_entry.entry_id][COORDINATORS]
@@ -29,23 +37,18 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 class DiffuserSwitch(SwitchEntity, DiffuserEntity):
     """Representation of a diffuser switch."""
 
-    def __init__(self, diffuser, coordinator):
+    def __init__(self, diffuser: Diffuser, coordinator: CoordinatorEntity) -> None:
         """Initialize the diffuser switch."""
         super().__init__(diffuser, coordinator, "")
         self._is_on = self.coordinator.data[HUB][ATTRIBUTES][FAN] == ON_STATE
 
     @property
-    def available(self):
-        """Return if the device is available."""
-        return self.coordinator.data[HUB][STATUS] == AVAILABLE_STATE
-
-    @property
-    def icon(self):
+    def icon(self) -> str:
         """Return the icon of the device."""
         return "mdi:fan"
 
     @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict[str, Any]:
         """Return the device state attributes."""
         attributes = {
             "fan_speed": self.coordinator.data[HUB][ATTRIBUTES][SPEED],
@@ -54,24 +57,24 @@ class DiffuserSwitch(SwitchEntity, DiffuserEntity):
         return attributes
 
     @property
-    def is_on(self):
+    def is_on(self) -> bool:
         """If the device is currently on or off."""
         return self._is_on
 
-    async def async_turn_on(self, **kwargs):
+    async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the device on."""
         await self._diffuser.turn_on()
         self._is_on = True
-        self.schedule_update_ha_state()
+        self.async_write_ha_state()
 
-    async def async_turn_off(self, **kwargs):
+    async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the device off."""
         await self._diffuser.turn_off()
         self._is_on = False
-        self.schedule_update_ha_state()
+        self.async_write_ha_state()
 
     @callback
-    def _handle_coordinator_update(self):
+    def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
         self._is_on = self.coordinator.data[HUB][ATTRIBUTES][FAN] == ON_STATE
         self.async_write_ha_state()
