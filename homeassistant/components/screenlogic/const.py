32acf40a36f41908f2a8a8cce056fda56dc19485
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2021-04-19 17:12:42 +0000

    Refactor to single integration service with device_id

--- homeassistant/components/screenlogic/const.py
+++ homeassistant/components/screenlogic/const.py
@@ -7,25 +7,10 @@ DOMAIN = "screenlogic"
 DEFAULT_SCAN_INTERVAL = 30
 MIN_SCAN_INTERVAL = 10
 
+SERVICE_SET_COLOR_MODE = "set_color_mode"
 ATTR_COLOR_MODE = "color_mode"
 SUPPORTED_COLOR_MODES = {
     slugify(name): num for num, name in COLOR_MODE.NAME_FOR_NUM.items()
-}
-SET_COLOR_MODE_SERVICE_DESCRIPTION = {
-    "name": "Set Color Mode",
-    "description": "Sets the color mode for all color-capable lights attached to this ScreenLogic gateway.",
-    "fields": {
-        "color_mode": {
-            "name": "Color Mode",
-            "description": "The ScreenLogic color mode to set.",
-            "required": True,
-            "example": "romance",
-            "default": "all_off",
-            "selector": {
-                "select": {"options": [*SUPPORTED_COLOR_MODES]},
-            },
-        },
-    },
 }
 
 DISCOVERED_GATEWAYS = "_discovered_gateways"
