ee78e52e3e29c6a015293184206c4f530b94457a
jan Iversen <jan@casacondor.com>
2021-04-19 20:09:47 +0200

    Use pytest.fixture() for modbus_hub.

--- tests/components/modbus/test_modbus.py
+++ tests/components/modbus/test_modbus.py
@@ -158,7 +158,8 @@ async def test_config_multiple_modbus(hass):
     assert len(hass.data[DOMAIN]) == len(do_config)
 
 
-def _config_entry():
+@pytest.fixture()
+def modbus_hub():
     """Return class obj configured."""
 
     config_hub = {
@@ -174,254 +175,242 @@ def _config_entry():
     return hub
 
 
-async def test_class_ModbusHub_logger(hass, caplog):
+async def test_class_ModbusHub_logger(hass, caplog, modbus_hub):
     """Run general test of class modbusHub."""
 
-    hub = _config_entry()
     caplog.set_level(logging.DEBUG)
     e = ModbusException("test logger")
     caplog.clear()
-    hub._log_error(e)
+    modbus_hub._log_error(e)
     assert len(caplog.records) == 1
     assert caplog.records[0].levelname == "ERROR"
 
-    hub._log_error(e)
+    modbus_hub._log_error(e)
     assert len(caplog.records) == 2
     assert caplog.records[1].levelname == "DEBUG"
 
 
-async def test_pb_create_exception(hass):
+async def test_pb_create_exception(hass, modbus_hub):
     """Run general test of class modbusHub."""
 
-    hub = _config_entry()
     with mock.patch.object(modbus, "_LOGGER") as mock_logger, mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient"
     ) as mock_pb:
         mock_pb.side_effect = ModbusException("test no class")
-        hub.setup()
+        modbus_hub.setup()
         assert mock_logger.error.called
 
 
-async def test_pb_connect(hass):
+async def test_pb_connect(hass, modbus_hub):
     """Run general test of class modbusHub."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch.object(modbus, "_LOGGER") as mock_logger, mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
+        modbus_hub.setup()
         assert mock_pb.connect.called
         assert not mock_logger.error.called
 
         mock_pb.connect.side_effect = ModbusException("test failed connect()")
-        hub.setup()
+        modbus_hub.setup()
         assert mock_pb.connect.called
         assert mock_logger.error.called
 
 
-async def test_pb_close(hass):
+async def test_pb_close(hass, modbus_hub):
     """Run general test of class modbusHub."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch.object(modbus, "_LOGGER") as mock_logger, mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
-        hub.close()
+        modbus_hub.setup()
+        modbus_hub.close()
         assert mock_pb.close.called
         assert not mock_logger.error.called
 
         mock_pb.close.side_effect = ModbusException("test failed close()")
-        hub.setup()
-        hub.close()
+        modbus_hub.setup()
+        modbus_hub.close()
         assert mock_pb.close.called
         assert mock_logger.error.called
 
 
-async def test_pb_read_coils(hass):
+async def test_pb_read_coils(hass, modbus_hub):
     """Run test for pymodbus read_coils calls."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
+        modbus_hub.setup()
 
         data = [0x15]
         mock_pb.read_coils.return_value = ReadResult(data)
-        result = hub.read_coils(None, 17, 1)
+        result = modbus_hub.read_coils(None, 17, 1)
         assert result.registers == data
 
         data = [0x15, 0x16]
         mock_pb.read_coils.return_value = ReadResult(data)
-        result = hub.read_coils(None, 17, 2)
+        result = modbus_hub.read_coils(None, 17, 2)
         assert result.registers == data
 
         data = [0x15, 0x16]
         mock_pb.read_coils.side_effect = ModbusException("fail read_coils")
-        result = hub.read_coils(None, 17, 2)
+        result = modbus_hub.read_coils(None, 17, 2)
         assert result is None
 
 
-async def test_pb_read_discrete_inputs(hass):
+async def test_pb_read_discrete_inputs(hass, modbus_hub):
     """Run test for pymodbus read_coils calls."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
+        modbus_hub.setup()
 
         data = [0x15]
         mock_pb.read_discrete_inputs.return_value = ReadResult(data)
-        result = hub.read_discrete_inputs(None, 17, 1)
+        result = modbus_hub.read_discrete_inputs(None, 17, 1)
         assert result.registers == data
 
         data = [0x15, 0x16]
         mock_pb.read_discrete_inputs.return_value = ReadResult(data)
-        result = hub.read_discrete_inputs(None, 17, 2)
+        result = modbus_hub.read_discrete_inputs(None, 17, 2)
         assert result.registers == data
 
         data = [0x15, 0x16]
         mock_pb.read_discrete_inputs.side_effect = ModbusException("fail read_coils")
-        result = hub.read_discrete_inputs(None, 17, 2)
+        result = modbus_hub.read_discrete_inputs(None, 17, 2)
         assert result is None
 
 
-async def test_pb_read_input_registers(hass):
+async def test_pb_read_input_registers(hass, modbus_hub):
     """Run test for pymodbus read_input_registers calls."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
+        modbus_hub.setup()
 
         data = [0x15]
         mock_pb.read_input_registers.return_value = ReadResult(data)
-        result = hub.read_input_registers(None, 17, 1)
+        result = modbus_hub.read_input_registers(None, 17, 1)
         assert result.registers == data
 
         data = [0x15, 0x16]
         mock_pb.read_input_registers.return_value = ReadResult(data)
-        result = hub.read_input_registers(None, 17, 2)
+        result = modbus_hub.read_input_registers(None, 17, 2)
         assert result.registers == data
 
         data = [0x15, 0x16]
         mock_pb.read_input_registers.side_effect = ModbusException("fail read_coils")
-        result = hub.read_input_registers(None, 17, 2)
+        result = modbus_hub.read_input_registers(None, 17, 2)
         assert result is None
 
 
-async def test_pb_read_holding_registers(hass):
+async def test_pb_read_holding_registers(hass, modbus_hub):
     """Run test for pymodbus read_holding_registers calls."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
+        modbus_hub.setup()
 
         data = [0x15]
         mock_pb.read_holding_registers.return_value = ReadResult(data)
-        result = hub.read_holding_registers(None, 17, 1)
+        result = modbus_hub.read_holding_registers(None, 17, 1)
         assert result.registers == data
 
         data = [0x15, 0x16]
         mock_pb.read_holding_registers.return_value = ReadResult(data)
-        result = hub.read_holding_registers(None, 17, 2)
+        result = modbus_hub.read_holding_registers(None, 17, 2)
         assert result.registers == data
 
         data = [0x15, 0x16]
         mock_pb.read_holding_registers.side_effect = ModbusException("fail read_coils")
-        result = hub.read_holding_registers(None, 17, 2)
+        result = modbus_hub.read_holding_registers(None, 17, 2)
         assert result is None
 
 
-async def test_pb_write_coil(hass):
+async def test_pb_write_coil(hass, modbus_hub):
     """Run test for pymodbus write_coil calls."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
+        modbus_hub.setup()
 
         addr = 17
         data = 16
-        assert hub.write_coil(None, addr, data)
+        assert modbus_hub.write_coil(None, addr, data)
         assert mock_pb.write_coil.called
         assert mock_pb.write_coil.call_args[0] == (addr, data)
 
         mock_pb.write_coil.side_effect = ModbusException("fail write_coil")
-        assert not hub.write_coil(None, addr, data)
+        assert not modbus_hub.write_coil(None, addr, data)
 
 
-async def test_pb_write_coils(hass):
+async def test_pb_write_coils(hass, modbus_hub):
     """Run test for pymodbus write_coils calls."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
+        modbus_hub.setup()
 
         addr = 17
         data = 16
-        assert hub.write_coils(None, addr, data)
+        assert modbus_hub.write_coils(None, addr, data)
         assert mock_pb.write_coils.called
         assert mock_pb.write_coils.call_args[0] == (addr, data)
 
         mock_pb.write_coils.side_effect = ModbusException("fail write_coils")
-        assert not hub.write_coils(None, addr, data)
+        assert not modbus_hub.write_coils(None, addr, data)
 
 
-async def test_pb_write_register(hass):
+async def test_pb_write_register(hass, modbus_hub):
     """Run test for pymodbus write_register calls."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
+        modbus_hub.setup()
 
         addr = 17
         data = 16
-        assert hub.write_register(None, addr, data)
+        assert modbus_hub.write_register(None, addr, data)
         assert mock_pb.write_register.called
         assert mock_pb.write_register.call_args[0] == (addr, data)
 
         mock_pb.write_register.side_effect = ModbusException("fail write_")
-        assert not hub.write_register(None, addr, data)
+        assert not modbus_hub.write_register(None, addr, data)
 
 
-async def test_pb_write_registers(hass):
+async def test_pb_write_registers(hass, modbus_hub):
     """Run test for pymodbus write_registers calls."""
 
-    hub = _config_entry()
     mock_pb = mock.MagicMock()
     with mock.patch(
         "homeassistant.components.modbus.modbus.ModbusTcpClient", return_value=mock_pb
     ):
-        hub.setup()
+        modbus_hub.setup()
 
         addr = 17
         data = 16
-        assert hub.write_registers(None, addr, data)
+        assert modbus_hub.write_registers(None, addr, data)
         assert mock_pb.write_registers.called
         assert mock_pb.write_registers.call_args[0] == (addr, data)
 
         mock_pb.write_registers.side_effect = ModbusException("fail write_")
-        assert not hub.write_registers(None, addr, data)
+        assert not modbus_hub.write_registers(None, addr, data)
 
 
 async def test_pb_service_write_register(hass):
