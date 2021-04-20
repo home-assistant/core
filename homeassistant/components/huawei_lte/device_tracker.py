67690ed2f2df795b35a3397022ff3d303efa7d87
Ville Skyttä <ville.skytta@iki.fi>
2021-04-20 23:32:00 +0300

    Pick first IP address in case there are multiple

--- homeassistant/components/huawei_lte/device_tracker.py
+++ homeassistant/components/huawei_lte/device_tracker.py
@@ -238,7 +238,9 @@ class HuaweiLteScannerEntity(HuaweiLteBaseEntity, ScannerEntity):
         )
         self._is_connected = _is_connected(host)
         if host is not None:
-            self._ip_address = host.get("IpAddress")
+            # IpAddress can contain multiple semicolon separated addresses.
+            # Pick one for model sanity; e.g. the dhcp component to which it is fed, parses and expects to see just one.
+            self._ip_address = (host.get("IpAddress") or "").split(";", 2)[0] or None
             self._hostname = host.get("HostName")
             self._extra_state_attributes = {
                 _better_snakecase(k): v
