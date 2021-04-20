88f86d1c4442b6567c790ab01b417ce9383d2297
Robert Svensson <kane610@me.com>
2021-04-20 20:02:23 +0200

    Use config_entry.on_unload rather than local listener implementation

--- homeassistant/components/unifi/switch.py
+++ homeassistant/components/unifi/switch.py
@@ -86,7 +86,9 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
             add_dpi_entities(controller, async_add_entities, dpi_groups)
 
     for signal in (controller.signal_update, controller.signal_options_update):
-        controller.listeners.append(async_dispatcher_connect(hass, signal, items_added))
+        config_entry.async_on_unload(
+            async_dispatcher_connect(hass, signal, items_added)
+        )
 
     items_added()
     known_poe_clients.clear()
