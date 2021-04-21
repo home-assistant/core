af3dd4a4c9443c25bf21a9fdbf10e269da863eac
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2021-04-21 06:25:07 +0000

    Support multiple targets

--- homeassistant/components/screenlogic/services.py
+++ homeassistant/components/screenlogic/services.py
@@ -5,11 +5,10 @@ import logging
 from screenlogicpy import ScreenLogicError
 import voluptuous as vol
 
-from homeassistant.const import ATTR_DEVICE_ID
 from homeassistant.core import ServiceCall, callback
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers import device_registry as dr
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.service import async_extract_config_entry_ids
 from homeassistant.helpers.typing import HomeAssistantType
 
 from .const import (
@@ -23,11 +22,10 @@ _LOGGER = logging.getLogger(__name__)
 
 INTEGRATION_SERVICES = [SERVICE_SET_COLOR_MODE]
 
-SET_COLOR_MODE_SCHEMA = vol.Schema(
+SET_COLOR_MODE_SCHEMA = cv.make_entity_service_schema(
     {
-        vol.Required(ATTR_DEVICE_ID): cv.string,
         vol.Required(ATTR_COLOR_MODE): vol.In(SUPPORTED_COLOR_MODES),
-    }
+    },
 )
 
 
@@ -41,43 +39,44 @@ def async_load_screenlogic_services(hass: HomeAssistantType):
         # Integration-level services have already been added. Return.
         return
 
-    async def get_entry_id_for_device_id(device_id):
-        device_registry = await dr.async_get_registry(hass)
-        if (gateway_device_entry := device_registry.async_get(device_id)) is not None:
-            for entry_id in gateway_device_entry.config_entries:
-                config_entry = hass.config_entries.async_get_entry(entry_id)
-                if config_entry.domain == DOMAIN:
-                    return entry_id
-        return None
+    async def extract_screenlogic_config_entry_ids(service_call: ServiceCall):
+        screenlogic_entry_ids = []
+        for entry_id in await async_extract_config_entry_ids(hass, service_call):
+            config_entry = hass.config_entries.async_get_entry(entry_id)
+            if config_entry.domain == DOMAIN:
+                if entry_id not in screenlogic_entry_ids:
+                    screenlogic_entry_ids.append(entry_id)
+        return screenlogic_entry_ids
 
-    async def set_color_mode(service: ServiceCall):
-        device_id = service.data.get(ATTR_DEVICE_ID)
-        if (entry_id := await get_entry_id_for_device_id(device_id)) is None:
+    async def set_color_mode(service_call: ServiceCall):
+        if not (
+            screenlogic_entry_ids := await extract_screenlogic_config_entry_ids(
+                service_call
+            )
+        ):
             raise HomeAssistantError(
-                f"Failed to call service '{SERVICE_SET_COLOR_MODE}'. Config entry for Gateway device not found"
+                f"Failed to call service '{SERVICE_SET_COLOR_MODE}'. Config entry for target not found"
             )
-        if (coordinator := hass.data[DOMAIN][entry_id]["coordinator"]) is None:
-            raise HomeAssistantError(
-                f"Failed to call service '{SERVICE_SET_COLOR_MODE}'. Gateway coordinator not found"
-            )
-        if (color_mode := service.data.get(ATTR_COLOR_MODE)) is None:
-            raise ValueError(
-                f"Failed to call service '{SERVICE_SET_COLOR_MODE}'. color_mode not found"
-            )
-        try:
+        color_mode = service_call.data[ATTR_COLOR_MODE]
+        for entry_id in screenlogic_entry_ids:
+            coordinator = hass.data[DOMAIN][entry_id]["coordinator"]
             color_num = SUPPORTED_COLOR_MODES[color_mode]
             _LOGGER.debug(
-                "Service %s called with mode %s", SERVICE_SET_COLOR_MODE, color_num
+                "Service %s called on %s with mode %s",
+                SERVICE_SET_COLOR_MODE,
+                coordinator.gateway.name,
+                color_num,
             )
-            async with coordinator.api_lock:
-                if not await hass.async_add_executor_job(
-                    coordinator.gateway.set_color_lights, color_num
-                ):
-                    raise HomeAssistantError(
-                        f"Failed to call service '{SERVICE_SET_COLOR_MODE}'"
-                    )
-        except ScreenLogicError as error:
-            raise HomeAssistantError(error) from error
+            try:
+                async with coordinator.api_lock:
+                    if not await hass.async_add_executor_job(
+                        coordinator.gateway.set_color_lights, color_num
+                    ):
+                        raise HomeAssistantError(
+                            f"Failed to call service '{SERVICE_SET_COLOR_MODE}'"
+                        )
+            except ScreenLogicError as error:
+                raise HomeAssistantError(error) from error
 
     hass.services.async_register(
         DOMAIN, SERVICE_SET_COLOR_MODE, set_color_mode, SET_COLOR_MODE_SCHEMA
