121653ed62716fb661d2ad5ffe7ccb48a12e8133
Thomas Hollstegge <thomas.hollstegge@gmail.com>
2021-04-22 22:10:33 +0200

    Add test for closing cover

--- tests/components/emulated_hue/test_hue_api.py
+++ tests/components/emulated_hue/test_hue_api.py
@@ -926,6 +926,26 @@ async def test_open_cover_without_position(hass_hue, hue_client):
     assert cover_test_2.state == "open"
     assert cover_test_2.attributes.get("current_position") == 100
 
+    # Go through the API to turn it off
+    cover_result = await perform_put_light_state(hass_hue, hue_client, cover_id, False)
+
+    assert cover_result.status == HTTP_OK
+    assert CONTENT_TYPE_JSON in cover_result.headers["content-type"]
+
+    for _ in range(11):
+        future = dt_util.utcnow() + timedelta(seconds=1)
+        async_fire_time_changed(hass_hue, future)
+        await hass_hue.async_block_till_done()
+
+    cover_result_json = await cover_result.json()
+
+    assert len(cover_result_json) == 1
+
+    # Check to make sure the state changed
+    cover_test_2 = hass_hue.states.get(cover_id)
+    assert cover_test_2.state == "closed"
+    assert cover_test_2.attributes.get("current_position") == 0
+
 
 async def test_set_position_cover(hass_hue, hue_client):
     """Test setting position cover ."""
