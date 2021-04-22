022d65371f9944b71a6e33dd1284e968ca9f0f28
Erik <erik@montnemery.com>
2021-04-22 20:55:39 +0200

    Add support for RGBW color to blebox light

--- homeassistant/components/blebox/light.py
+++ homeassistant/components/blebox/light.py
@@ -5,19 +5,13 @@ from blebox_uniapi.error import BadOnValueError
 
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
-    ATTR_HS_COLOR,
-    ATTR_WHITE_VALUE,
-    SUPPORT_BRIGHTNESS,
-    SUPPORT_COLOR,
-    SUPPORT_WHITE_VALUE,
+    ATTR_RGBW_COLOR,
+    COLOR_MODE_BRIGHTNESS,
+    COLOR_MODE_ONOFF,
+    COLOR_MODE_RGBW,
     LightEntity,
 )
-from homeassistant.util.color import (
-    color_hs_to_RGB,
-    color_rgb_to_hex,
-    color_RGB_to_hs,
-    rgb_hex_to_rgb_list,
-)
+from homeassistant.util.color import color_rgb_to_hex, rgb_hex_to_rgb_list
 
 from . import BleBoxEntity, create_blebox_entities
 
@@ -36,12 +30,9 @@ class BleBoxLightEntity(BleBoxEntity, LightEntity):
     """Representation of BleBox lights."""
 
     @property
-    def supported_features(self):
-        """Return supported features."""
-        white = SUPPORT_WHITE_VALUE if self._feature.supports_white else 0
-        color = SUPPORT_COLOR if self._feature.supports_color else 0
-        brightness = SUPPORT_BRIGHTNESS if self._feature.supports_brightness else 0
-        return white | color | brightness
+    def supported_color_modes(self):
+        """Return supported color modes."""
+        return {self.color_mode}
 
     @property
     def is_on(self):
@@ -54,25 +45,27 @@ class BleBoxLightEntity(BleBoxEntity, LightEntity):
         return self._feature.brightness
 
     @property
-    def white_value(self):
-        """Return the white value."""
-        return self._feature.white_value
+    def color_mode(self):
+        """Return the color mode."""
+        if self._feature.supports_white and self._feature.supports_color:
+            return COLOR_MODE_RGBW
+        if self._feature.supports_brightness:
+            return COLOR_MODE_BRIGHTNESS
+        return COLOR_MODE_ONOFF
 
     @property
-    def hs_color(self):
+    def rgbw_color(self):
         """Return the hue and saturation."""
         rgbw_hex = self._feature.rgbw_hex
         if rgbw_hex is None:
             return None
 
-        rgb = rgb_hex_to_rgb_list(rgbw_hex)[0:3]
-        return color_RGB_to_hs(*rgb)
+        return tuple(rgb_hex_to_rgb_list(rgbw_hex)[0:4])
 
     async def async_turn_on(self, **kwargs):
         """Turn the light on."""
 
-        white = kwargs.get(ATTR_WHITE_VALUE)
-        hs_color = kwargs.get(ATTR_HS_COLOR)
+        rgbw = kwargs.get(ATTR_RGBW_COLOR)
         brightness = kwargs.get(ATTR_BRIGHTNESS)
 
         feature = self._feature
@@ -81,12 +74,9 @@ class BleBoxLightEntity(BleBoxEntity, LightEntity):
         if brightness is not None:
             value = feature.apply_brightness(value, brightness)
 
-        if white is not None:
-            value = feature.apply_white(value, white)
-
-        if hs_color is not None:
-            raw_rgb = color_rgb_to_hex(*color_hs_to_RGB(*hs_color))
-            value = feature.apply_color(value, raw_rgb)
+        if rgbw is not None:
+            value = feature.apply_white(value, rgbw[3])
+            value = feature.apply_color(value, color_rgb_to_hex(*rgbw[0:3]))
 
         try:
             await self._feature.async_on(value)
