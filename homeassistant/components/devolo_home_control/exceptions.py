7833d888b11f9fb885cdbf12b979a2ea5b2776f2
Guido Schmitz <guido.schmitz@fedaix.de>
2021-04-23 10:52:52 +0000

    Inherit from HomeAssistantError

--- homeassistant/components/devolo_home_control/exceptions.py
+++ homeassistant/components/devolo_home_control/exceptions.py
@@ -1,5 +1,6 @@
 """Custom exceptions for the devolo_home_control integration."""
+from homeassistant.exceptions import HomeAssistantError
 
 
-class CredentialsInvalid(Exception):
+class CredentialsInvalid(HomeAssistantError):
     """Given credentials are invalid."""
