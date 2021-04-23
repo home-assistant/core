92850057de2ca6be9ac7f552d50a50493c43b2fd
chemelli74 <simone.chemelli@gmail.com>
2021-04-23 12:14:36 +0000

    Black

--- homeassistant/components/samsungtv/media_player.py
+++ homeassistant/components/samsungtv/media_player.py
@@ -105,7 +105,9 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         """Stop SamsungTV bridge connection."""
         bridge.stop()
 
-    entry.async_on_unload(hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, stop_bridge))
+    config_entry.async_on_unload(
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, stop_bridge)
+    )
 
     async_add_entities([SamsungTVDevice(bridge, config_entry, on_script)])
 
