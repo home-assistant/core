1a08bd811188eb8c477a9b2aa4a43748eac75706
definitio <37266727+definitio@users.noreply.github.com>
2021-04-24 17:46:26 +0300

    Add more tests

--- tests/components/group/test_media_player.py
+++ tests/components/group/test_media_player.py
@@ -1,11 +1,10 @@
 """The tests for the Media group platform."""
-from os import path
-
 from homeassistant.components.group import DOMAIN
 from homeassistant.components.media_player import (
     ATTR_MEDIA_SHUFFLE,
     ATTR_MEDIA_VOLUME_LEVEL,
     DOMAIN as MEDIA_DOMAIN,
+    SERVICE_MEDIA_PAUSE,
     SERVICE_SHUFFLE_SET,
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
@@ -18,11 +17,23 @@ from homeassistant.components.media_player import (
     SUPPORT_VOLUME_SET,
     SUPPORT_VOLUME_STEP,
 )
+from homeassistant.components.media_player.const import (
+    ATTR_MEDIA_TRACK,
+    ATTR_MEDIA_VOLUME_MUTED,
+    SERVICE_CLEAR_PLAYLIST,
+)
 from homeassistant.const import (
     ATTR_ENTITY_ID,
     ATTR_SUPPORTED_FEATURES,
+    SERVICE_MEDIA_NEXT_TRACK,
+    SERVICE_MEDIA_PLAY,
+    SERVICE_MEDIA_PREVIOUS_TRACK,
+    SERVICE_VOLUME_DOWN,
+    SERVICE_VOLUME_MUTE,
+    SERVICE_VOLUME_UP,
     STATE_OFF,
     STATE_ON,
+    STATE_PAUSED,
     STATE_PLAYING,
     STATE_UNAVAILABLE,
     STATE_UNKNOWN,
@@ -87,7 +98,7 @@ async def test_state_reporting(hass):
     assert hass.states.get("media_player.media_group").state == STATE_ON
 
     hass.states.async_set("media_player.player_1", STATE_OFF)
-    hass.states.async_set("media_player.player_2", STATE_OFF)
+    hass.states.async_set("media_player.player_2", STATE_UNAVAILABLE)
     await hass.async_block_till_done()
     assert hass.states.get("media_player.media_group").state == STATE_OFF
 
@@ -202,6 +213,47 @@ async def test_service_calls(hass):
 
     await hass.services.async_call(
         MEDIA_DOMAIN,
+        SERVICE_MEDIA_PAUSE,
+        {ATTR_ENTITY_ID: "media_player.media_group"},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+    assert hass.states.get("media_player.bedroom").state == STATE_PAUSED
+    assert hass.states.get("media_player.kitchen").state == STATE_PAUSED
+    assert hass.states.get("media_player.living_room").state == STATE_PAUSED
+
+    await hass.services.async_call(
+        MEDIA_DOMAIN,
+        SERVICE_MEDIA_PLAY,
+        {ATTR_ENTITY_ID: "media_player.media_group"},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+    assert hass.states.get("media_player.bedroom").state == STATE_PLAYING
+    assert hass.states.get("media_player.kitchen").state == STATE_PLAYING
+    assert hass.states.get("media_player.living_room").state == STATE_PLAYING
+
+    assert hass.states.get("media_player.kitchen").attributes[ATTR_MEDIA_TRACK] == 1
+    await hass.services.async_call(
+        MEDIA_DOMAIN,
+        SERVICE_MEDIA_NEXT_TRACK,
+        {ATTR_ENTITY_ID: "media_player.media_group"},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+    assert hass.states.get("media_player.kitchen").attributes[ATTR_MEDIA_TRACK] == 2
+
+    await hass.services.async_call(
+        MEDIA_DOMAIN,
+        SERVICE_MEDIA_PREVIOUS_TRACK,
+        {ATTR_ENTITY_ID: "media_player.media_group"},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+    assert hass.states.get("media_player.kitchen").attributes[ATTR_MEDIA_TRACK] == 1
+
+    await hass.services.async_call(
+        MEDIA_DOMAIN,
         SERVICE_VOLUME_SET,
         {
             ATTR_ENTITY_ID: "media_player.media_group",
@@ -225,6 +277,66 @@ async def test_service_calls(hass):
 
     await hass.services.async_call(
         MEDIA_DOMAIN,
+        SERVICE_VOLUME_UP,
+        {ATTR_ENTITY_ID: "media_player.media_group"},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+    assert (
+        hass.states.get("media_player.bedroom").attributes[ATTR_MEDIA_VOLUME_LEVEL]
+        == 0.6
+    )
+    assert (
+        hass.states.get("media_player.kitchen").attributes[ATTR_MEDIA_VOLUME_LEVEL]
+        == 0.6
+    )
+    assert (
+        hass.states.get("media_player.living_room").attributes[ATTR_MEDIA_VOLUME_LEVEL]
+        == 0.6
+    )
+
+    await hass.services.async_call(
+        MEDIA_DOMAIN,
+        SERVICE_VOLUME_DOWN,
+        {ATTR_ENTITY_ID: "media_player.media_group"},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+    assert (
+        hass.states.get("media_player.bedroom").attributes[ATTR_MEDIA_VOLUME_LEVEL]
+        == 0.5
+    )
+    assert (
+        hass.states.get("media_player.kitchen").attributes[ATTR_MEDIA_VOLUME_LEVEL]
+        == 0.5
+    )
+    assert (
+        hass.states.get("media_player.living_room").attributes[ATTR_MEDIA_VOLUME_LEVEL]
+        == 0.5
+    )
+
+    await hass.services.async_call(
+        MEDIA_DOMAIN,
+        SERVICE_VOLUME_MUTE,
+        {ATTR_ENTITY_ID: "media_player.media_group", ATTR_MEDIA_VOLUME_MUTED: True},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+    assert (
+        hass.states.get("media_player.bedroom").attributes[ATTR_MEDIA_VOLUME_MUTED]
+        is True
+    )
+    assert (
+        hass.states.get("media_player.kitchen").attributes[ATTR_MEDIA_VOLUME_MUTED]
+        is True
+    )
+    assert (
+        hass.states.get("media_player.living_room").attributes[ATTR_MEDIA_VOLUME_MUTED]
+        is True
+    )
+
+    await hass.services.async_call(
+        MEDIA_DOMAIN,
         SERVICE_SHUFFLE_SET,
         {ATTR_ENTITY_ID: "media_player.media_group", ATTR_MEDIA_SHUFFLE: True},
         blocking=True,
@@ -240,6 +352,15 @@ async def test_service_calls(hass):
         hass.states.get("media_player.living_room").attributes[ATTR_MEDIA_SHUFFLE]
         is True
     )
+
+    await hass.services.async_call(
+        MEDIA_DOMAIN,
+        SERVICE_CLEAR_PLAYLIST,
+        {ATTR_ENTITY_ID: "media_player.media_group"},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+    assert hass.states.get("media_player.kitchen").state == STATE_OFF
 
 
 async def test_nested_group(hass):
@@ -279,7 +400,3 @@ async def test_nested_group(hass):
     assert state is not None
     assert state.state == STATE_ON
     assert state.attributes.get(ATTR_ENTITY_ID) == ["media_player.group_1"]
-
-
-def _get_fixtures_base_path():
-    return path.dirname(path.dirname(path.dirname(__file__)))
