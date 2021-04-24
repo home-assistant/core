51c75ba7fa8806e0fb017d89e27ded53ab1c3110
J. Nick Koston <nick@koston.org>
2021-04-23 20:11:02 -1000

    Enable mccabe complexity checks in flake8

--- script/lazytox.py
+++ script/lazytox.py
@@ -117,7 +117,9 @@ async def pylint(files):
 
 async def flake8(files):
     """Exec flake8."""
-    _, log = await async_exec("pre-commit", "run", "flake8", "--files", *files)
+    _, log = await async_exec(
+        "pre-commit", "run", "flake8", "--max-complexity", "10", "--files", *files
+    )
     res = []
     for line in log.splitlines():
         line = line.split(":")
@@ -154,7 +156,7 @@ async def lint(files):
     return lint_ok
 
 
-async def main():
+async def main():  # noqa: C901
     """Run the main loop."""
     # Ensure we are in the homeassistant root
     os.chdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
