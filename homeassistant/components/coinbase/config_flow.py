619214f7109b50bb63473cea3d6100cdf4a8d851
Tom Brien <tom@byth.co.uk>
2021-04-25 15:34:02 +0100

    Validate Options Flow

--- homeassistant/components/coinbase/config_flow.py
+++ homeassistant/components/coinbase/config_flow.py
@@ -32,8 +32,8 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 
-async def validate_input(hass: core.HomeAssistant, data):
-    """Validate the credentials and requested resources."""
+async def validate_api(hass: core.HomeAssistant, data):
+    """Validate the credentials."""
 
     for entry in hass.config_entries.async_entries(DOMAIN):
         if entry.data[CONF_API_KEY] == data[CONF_API_KEY]:
@@ -48,22 +48,32 @@ async def validate_input(hass: core.HomeAssistant, data):
     except ConnectionError as error:
         raise CannotConnect from error
 
+    return {"title": user["name"]}
+
+
+async def validate_options(
+    hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry, options
+):
+    """Validate the requested resources are provided by API."""
+
+    client = hass.data[DOMAIN][config_entry.entry_id].client
+
     accounts = await hass.async_add_executor_job(client.get_accounts)
     accounts_currencies = [
         account[API_ACCOUNT_CURRENCY] for account in accounts[API_ACCOUNTS_DATA]
     ]
     available_rates = await hass.async_add_executor_job(client.get_exchange_rates)
-    if CONF_CURRENCIES in data:
-        for currency in data[CONF_CURRENCIES]:
+    if CONF_CURRENCIES in options:
+        for currency in options[CONF_CURRENCIES]:
             if currency not in accounts_currencies:
                 raise CurrencyUnavaliable
 
-    if CONF_EXCHANGE_RATES in data:
-        for rate in data[CONF_EXCHANGE_RATES]:
+    if CONF_EXCHANGE_RATES in options:
+        for rate in options[CONF_EXCHANGE_RATES]:
             if rate not in available_rates[API_RATES]:
                 raise ExchangeRateUnavaliable
 
-    return {"title": user["name"]}
+    return True
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -81,17 +91,13 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             )
 
         try:
-            info = await validate_input(self.hass, user_input)
+            info = await validate_api(self.hass, user_input)
         except AlreadyConfigured:
             return self.async_abort(reason="already_configured")
         except CannotConnect:
             errors["base"] = "cannot_connect"
         except InvalidAuth:
             errors["base"] = "invalid_auth"
-        except CurrencyUnavaliable:
-            errors["base"] = "currency_unavaliable"
-        except ExchangeRateUnavaliable:
-            errors["base"] = "exchange_rate_unavaliable"
         except Exception:  # pylint: disable=broad-except
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
@@ -132,8 +138,30 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
+
+        errors = {}
+        default_currencies = self.config_entry.options.get(CONF_CURRENCIES)
+        default_exchange_rates = self.config_entry.options.get(CONF_EXCHANGE_RATES)
+
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            # Pass back user selected options, even if bad
+            if CONF_CURRENCIES in user_input:
+                default_currencies = user_input[CONF_CURRENCIES]
+
+            if CONF_EXCHANGE_RATES in user_input:
+                default_exchange_rates = user_input[CONF_EXCHANGE_RATES]
+
+            try:
+                await validate_options(self.hass, self.config_entry, user_input)
+            except CurrencyUnavaliable:
+                errors["base"] = "currency_unavaliable"
+            except ExchangeRateUnavaliable:
+                errors["base"] = "exchange_rate_unavaliable"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
             step_id="init",
@@ -141,14 +169,15 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
                 {
                     vol.Optional(
                         CONF_CURRENCIES,
-                        default=self.config_entry.options.get(CONF_CURRENCIES),
+                        default=default_currencies,
                     ): cv.multi_select(WALLETS),
                     vol.Optional(
                         CONF_EXCHANGE_RATES,
-                        default=self.config_entry.options.get(CONF_EXCHANGE_RATES),
+                        default=default_exchange_rates,
                     ): cv.multi_select(RATES),
                 }
             ),
+            errors=errors,
         )
 
 
