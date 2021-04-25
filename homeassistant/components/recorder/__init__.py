4996451e05b7e7b95911cb49a56a7897e36d8b33
J. Nick Koston <nick@koston.org>
2021-04-25 00:05:49 -1000

    Skip recorder commit if there is nothing to do (#49614)

--- homeassistant/components/recorder/__init__.py
+++ homeassistant/components/recorder/__init__.py
@@ -666,6 +666,9 @@ class Recorder(threading.Thread):
         return False
 
     def _commit_event_session_or_retry(self):
+        """Commit the event session if there is work to do."""
+        if not self.event_session.new and not self.event_session.dirty:
+            return
         tries = 1
         while tries <= self.db_max_retries:
             try:
