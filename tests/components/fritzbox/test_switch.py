9dd51ff6aca5e5878b055f6de3b7d3eb782b87f1
Michael <35783820+mib1185@users.noreply.github.com>
2021-04-25 21:49:08 +0200

    Remove yaml configuration from fritzbox (#49663)

--- tests/components/fritzbox/test_switch.py
+++ tests/components/fritzbox/test_switch.py
@@ -17,6 +17,7 @@ from homeassistant.const import (
     ATTR_ENTITY_ID,
     ATTR_FRIENDLY_NAME,
     ATTR_TEMPERATURE,
+    CONF_DEVICES,
     ENERGY_KILO_WATT_HOUR,
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
@@ -24,30 +25,23 @@ from homeassistant.const import (
     TEMP_CELSIUS,
 )
 from homeassistant.core import HomeAssistant
-from homeassistant.setup import async_setup_component
 import homeassistant.util.dt as dt_util
 
-from . import MOCK_CONFIG, FritzDeviceSwitchMock
+from . import MOCK_CONFIG, FritzDeviceSwitchMock, setup_config_entry
 
 from tests.common import async_fire_time_changed
 
 ENTITY_ID = f"{DOMAIN}.fake_name"
 
 
-async def setup_fritzbox(hass: HomeAssistant, config: dict):
-    """Set up mock AVM Fritz!Box."""
-    assert await async_setup_component(hass, FB_DOMAIN, config)
-    await hass.async_block_till_done()
-
-
 async def test_setup(hass: HomeAssistant, fritz: Mock):
     """Test setup of platform."""
     device = FritzDeviceSwitchMock()
-    fritz().get_devices.return_value = [device]
+    assert await setup_config_entry(
+        hass, MOCK_CONFIG[FB_DOMAIN][CONF_DEVICES][0], ENTITY_ID, device, fritz
+    )
 
-    await setup_fritzbox(hass, MOCK_CONFIG)
     state = hass.states.get(ENTITY_ID)
-
     assert state
     assert state.state == STATE_ON
     assert state.attributes[ATTR_CURRENT_POWER_W] == 5.678
@@ -63,9 +57,9 @@ async def test_setup(hass: HomeAssistant, fritz: Mock):
 async def test_turn_on(hass: HomeAssistant, fritz: Mock):
     """Test turn device on."""
     device = FritzDeviceSwitchMock()
-    fritz().get_devices.return_value = [device]
-
-    await setup_fritzbox(hass, MOCK_CONFIG)
+    assert await setup_config_entry(
+        hass, MOCK_CONFIG[FB_DOMAIN][CONF_DEVICES][0], ENTITY_ID, device, fritz
+    )
 
     assert await hass.services.async_call(
         DOMAIN, SERVICE_TURN_ON, {ATTR_ENTITY_ID: ENTITY_ID}, True
@@ -76,9 +70,9 @@ async def test_turn_on(hass: HomeAssistant, fritz: Mock):
 async def test_turn_off(hass: HomeAssistant, fritz: Mock):
     """Test turn device off."""
     device = FritzDeviceSwitchMock()
-    fritz().get_devices.return_value = [device]
-
-    await setup_fritzbox(hass, MOCK_CONFIG)
+    assert await setup_config_entry(
+        hass, MOCK_CONFIG[FB_DOMAIN][CONF_DEVICES][0], ENTITY_ID, device, fritz
+    )
 
     assert await hass.services.async_call(
         DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: ENTITY_ID}, True
@@ -89,9 +83,9 @@ async def test_turn_off(hass: HomeAssistant, fritz: Mock):
 async def test_update(hass: HomeAssistant, fritz: Mock):
     """Test update without error."""
     device = FritzDeviceSwitchMock()
-    fritz().get_devices.return_value = [device]
-
-    await setup_fritzbox(hass, MOCK_CONFIG)
+    assert await setup_config_entry(
+        hass, MOCK_CONFIG[FB_DOMAIN][CONF_DEVICES][0], ENTITY_ID, device, fritz
+    )
     assert device.update.call_count == 1
     assert fritz().login.call_count == 1
 
@@ -107,9 +101,9 @@ async def test_update_error(hass: HomeAssistant, fritz: Mock):
     """Test update with error."""
     device = FritzDeviceSwitchMock()
     device.update.side_effect = HTTPError("Boom")
-    fritz().get_devices.return_value = [device]
-
-    await setup_fritzbox(hass, MOCK_CONFIG)
+    assert not await setup_config_entry(
+        hass, MOCK_CONFIG[FB_DOMAIN][CONF_DEVICES][0], ENTITY_ID, device, fritz
+    )
     assert device.update.call_count == 1
     assert fritz().login.call_count == 1
 
