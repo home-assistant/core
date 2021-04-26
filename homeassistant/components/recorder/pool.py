9a70beb5602dc0045702b8cfede6c26620d7eb0f
J. Nick Koston <nick@koston.org>
2021-04-25 20:48:45 -1000

    Use single connection for recorder, and nullpoll for all other threads
    Since the recorder is constantly writing to the database it
    should keep the sqlite3 connection open for the life of the
    thread.  The default NullPool closes the database after
    each commit.  This is an expensive operation.  To solve this
    performance issue, RecorderPool has been created.  RecorderPool
    acts like StaticPool, maintaining a single connection to the
    database in the recorder thread, and NullPool in all other threads.
    This behavior ensures that threads do not share sqlite3
    connections for thread safety but still allows the recorder
    to use a single connection to the database that it does
    not close for the life of the run unless there is a problem.

---
+++ homeassistant/components/recorder/pool.py
@@ -0,0 +1,38 @@
+"""A pool for sqlite connections."""
+import threading
+
+from sqlalchemy.pool import NullPool, StaticPool
+
+
+class RecorderPool(StaticPool, NullPool):
+    """A hybird of NullPool and StaticPool.
+
+    When called from the creating thread acts like StaticPool
+    When called from any other thread, acts like NullPool
+    """
+
+    def __init__(self, *args, **kw):  # pylint: disable=super-init-not-called
+        """Create the pool."""
+        self._tid = threading.current_thread().ident
+        StaticPool.__init__(self, *args, **kw)
+
+    @property
+    def _is_recorder(self):
+        return threading.current_thread().ident == self._tid
+
+    def _do_return_conn(self, conn):
+        if self._is_recorder:
+            return super()._do_return_conn(conn)
+        conn.close()
+
+    def dispose(self):
+        """Dispose of the connection."""
+        if self._is_recorder:
+            return super().dispose()
+
+    def _do_get(self):
+        if self._is_recorder:
+            return super()._do_get()
+        return super(  # pylint: disable=bad-super-call
+            NullPool, self
+        )._create_connection()
