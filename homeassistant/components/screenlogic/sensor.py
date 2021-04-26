13371284bb05bae5c2d2c206a917ce250a1a60d3
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2021-04-26 03:32:19 +0000

    Add IntelliChem and SCG data

--- homeassistant/components/screenlogic/sensor.py
+++ homeassistant/components/screenlogic/sensor.py
@@ -1,7 +1,12 @@
 """Support for a ScreenLogic Sensor."""
 import logging
 
-from screenlogicpy.const import DATA as SL_DATA, DEVICE_TYPE
+from screenlogicpy.const import (
+    CHEM_DOSING_STATE,
+    DATA as SL_DATA,
+    DEVICE_TYPE,
+    EQUIPMENT,
+)
 
 from homeassistant.components.sensor import (
     DEVICE_CLASS_POWER,
@@ -14,7 +19,31 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-PUMP_SENSORS = ("currentWatts", "currentRPM", "currentGPM")
+SUPPORTED_CHEM_SENSORS = (
+    "calcium_harness",
+    "current_orp",
+    "current_ph",
+    "cya",
+    "orp_dosing_state",
+    "orp_last_dose_time",
+    "orp_last_dose_volume",
+    "orp_setpoint",
+    "ph_dosing_state",
+    "ph_last_dose_time",
+    "ph_last_dose_volume",
+    "ph_probe_water_temp",
+    "ph_setpoint",
+    "total_alkalinity",
+)
+
+SUPPORTED_SCG_SENSORS = (
+    "scg_level1",
+    "scg_level2",
+    "scg_salt_ppm",
+    "scg_super_chlor_timer",
+)
+
+SUPPORTED_PUMP_SENSORS = ("currentWatts", "currentRPM", "currentGPM")
 
 SL_DEVICE_TYPE_TO_HA_DEVICE_CLASS = {
     DEVICE_TYPE.TEMPERATURE: DEVICE_CLASS_TEMPERATURE,
@@ -40,8 +69,37 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
             coordinator.data[SL_DATA.KEY_PUMPS][pump]["data"] != 0
             and "currentWatts" in coordinator.data[SL_DATA.KEY_PUMPS][pump]
         ):
-            for pump_key in PUMP_SENSORS:
-                entities.append(ScreenLogicPumpSensor(coordinator, pump, pump_key))
+            entities.extend(
+                ScreenLogicPumpSensor(coordinator, pump, pump_key)
+                for pump_key in coordinator.data[SL_DATA.KEY_PUMPS][pump]
+                if pump_key in SUPPORTED_PUMP_SENSORS
+            )
+
+    # IntelliChem sensors
+    if (
+        coordinator.data[SL_DATA.KEY_CONFIG]["equipment_flags"]
+        & EQUIPMENT.FLAG_INTELLICHEM
+    ):
+        entities.extend(
+            [
+                ScreenLogicChemistrySensor(coordinator, chem_sensor)
+                for chem_sensor in coordinator.data[SL_DATA.KEY_CHEMISTRY]
+                if chem_sensor in SUPPORTED_CHEM_SENSORS
+            ]
+        )
+
+    # SCG sensors
+    if (
+        coordinator.data[SL_DATA.KEY_CONFIG]["equipment_flags"]
+        & EQUIPMENT.FLAG_CHLORINATOR
+    ):
+        entities.extend(
+            [
+                ScreenLogicSCGSensor(coordinator, scg_sensor)
+                for scg_sensor in coordinator.data[SL_DATA.KEY_SCG]
+                if scg_sensor in SUPPORTED_SCG_SENSORS
+            ]
+        )
 
     async_add_entities(entities)
 
@@ -90,3 +148,36 @@ class ScreenLogicPumpSensor(ScreenLogicSensor):
     def sensor(self):
         """Shortcut to access the pump sensor data."""
         return self.coordinator.data[SL_DATA.KEY_PUMPS][self._pump_id][self._key]
+
+
+class ScreenLogicChemistrySensor(ScreenLogicSensor):
+    """Representation of a ScreenLogic IntelliChem sensor entity."""
+
+    def __init__(self, coordinator, key):
+        """Initialize of the pump sensor."""
+        super().__init__(coordinator, f"chem_{key}")
+        self._key = key
+
+    @property
+    def state(self):
+        """State of the sensor."""
+        value = self.sensor["value"]
+        return (
+            CHEM_DOSING_STATE.NAME_FOR_NUM[value]
+            if "dosing_state" in self._key
+            else value
+        )
+
+    @property
+    def sensor(self):
+        """Shortcut to access the pump sensor data."""
+        return self.coordinator.data[SL_DATA.KEY_CHEMISTRY][self._key]
+
+
+class ScreenLogicSCGSensor(ScreenLogicSensor):
+    """Representation of ScreenLogic SCG sensor entity."""
+
+    @property
+    def sensor(self):
+        """Shortcut to access the pump sensor data."""
+        return self.coordinator.data[SL_DATA.KEY_SCG][self._data_key]
