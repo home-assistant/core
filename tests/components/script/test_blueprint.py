0aacae019b60c0577ec530b3051969122a293508
Franck Nijhof <git@frenck.dev>
2021-04-27 01:31:25 +0200

    Replace example with Confirmable notification script

--- tests/components/script/test_blueprint.py
+++ tests/components/script/test_blueprint.py
@@ -1,4 +1,5 @@
 """Test script blueprints."""
+import asyncio
 import contextlib
 import pathlib
 from typing import Iterator
@@ -37,22 +38,30 @@ def patch_blueprint(blueprint_path: str, data_path: str) -> Iterator[None]:
         yield
 
 
-async def test_random_color(hass: HomeAssistant) -> None:
-    """Test random color blueprint."""
+async def test_confirmable_notification(hass: HomeAssistant) -> None:
+    """Test confirmable notification blueprint."""
     with patch_blueprint(
-        "random_color.yaml",
-        BUILTIN_BLUEPRINT_FOLDER / "random_color.yaml",
+        "confirmable_notification.yaml",
+        BUILTIN_BLUEPRINT_FOLDER / "confirmable_notification.yaml",
     ):
         assert await async_setup_component(
             hass,
             script.DOMAIN,
             {
                 "script": {
-                    "roll_dice": {
+                    "confirm": {
                         "use_blueprint": {
-                            "path": "random_color.yaml",
+                            "path": "confirmable_notification.yaml",
                             "input": {
-                                "colors": ["red", "white", "blue"],
+                                "notify_device": "frodo",
+                                "title": "Lord of the things",
+                                "message": "Throw ring in mountain?",
+                                "confirm_action": [
+                                    {
+                                        "service": "homeassistant.turn_on",
+                                        "target": {"entity_id": "mount.doom"},
+                                    }
+                                ],
                             },
                         }
                     }
@@ -60,30 +69,46 @@ async def test_random_color(hass: HomeAssistant) -> None:
             },
         )
 
-    turn_on_calls = async_mock_service(hass, "light", "turn_on")
+    turn_on_calls = async_mock_service(hass, "homeassistant", "turn_on")
 
-    # Trigger random color
-    await hass.services.async_call(
-        script.DOMAIN,
-        "roll_dice",
-        {"target": {"entity_id": "light.kitchen"}},
-        blocking=True,
-    )
+    with patch(
+        "homeassistant.components.mobile_app.device_action.async_call_action_from_config"
+    ) as mock_call_action:
+
+        # Trigger script
+        await hass.services.async_call(script.DOMAIN, "confirm")
+
+        # Give script the time to attach the trigger.
+        await asyncio.sleep(0.1)
+
+    hass.bus.async_fire("mobile_app_notification_action", {"action": "CONFIRM"})
+    await hass.async_block_till_done()
+
+    assert len(mock_call_action.mock_calls) == 1
+    _hass, config, variables, _context = mock_call_action.mock_calls[0][1]
+
+    title_tpl = config.pop("title")
+    message_tpl = config.pop("message")
+    title_tpl.hass = hass
+    message_tpl.hass = hass
+
+    assert config == {
+        "alias": "Send notification",
+        "domain": "mobile_app",
+        "type": "notify",
+        "device_id": "frodo",
+        "data": {
+            "actions": [
+                {"action": "CONFIRM", "title": "Confirm"},
+                {"action": "DISMISS", "title": "Dismiss"},
+            ]
+        },
+    }
+
+    assert title_tpl.async_render(variables) == "Lord of the things"
+    assert message_tpl.async_render(variables) == "Throw ring in mountain?"
 
     assert len(turn_on_calls) == 1
-    call = turn_on_calls[0]
-    assert call.data["color_name"] in ["red", "white", "blue"]
-    assert call.data["entity_id"] == ["light.kitchen"]
-
-    # Trigger random color, override target
-    await hass.services.async_call(
-        script.DOMAIN,
-        "roll_dice",
-        {"target": {"entity_id": "light.living_room"}},
-        blocking=True,
-    )
-
-    assert len(turn_on_calls) == 2
-    call = turn_on_calls[1]
-    assert call.data["color_name"] in ["red", "white", "blue"]
-    assert call.data["entity_id"] == ["light.living_room"]
+    assert turn_on_calls[0].data == {
+        "entity_id": ["mount.doom"],
+    }
