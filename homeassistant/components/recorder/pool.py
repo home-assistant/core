601ec988cf4833cda6f04be2d22bbd93e285be86
J. Nick Koston <nick@koston.org>
2021-04-25 20:35:09 -1000

    adjust

--- homeassistant/components/recorder/pool.py
+++ homeassistant/components/recorder/pool.py
@@ -8,7 +8,6 @@ class RecorderPool(StaticPool, NullPool):
     """A hybird of NullPool and StaticPool.
 
     When called from the creating thread acts like StaticPool
-
     When called from any other thread, acts like NullPool
     """
 
@@ -20,20 +19,6 @@ class RecorderPool(StaticPool, NullPool):
     def status(self):
         """Status of the pool."""
         return "RecorderPool"
-
-    def recreate(self):
-        """Recreate the pool."""
-        self.logger.info("Pool recreating")
-        return self.__class__(
-            self._creator,
-            echo=self.echo,
-            pre_ping=self._pre_ping,
-            recycle=self._recycle,
-            reset_on_return=self._reset_on_return,
-            logging_name=self._orig_logging_name,
-            _dispatch=self.dispatch,
-            dialect=self._dialect,
-        )
 
     def _do_return_conn(self, conn):
         if threading.current_thread().ident == self._tid:
