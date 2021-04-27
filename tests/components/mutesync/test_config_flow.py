eba75b109a74a10980c377f74a006ac5e0919299
Franck Nijhof <git@frenck.dev>
2021-04-27 21:46:24 +0200

    Simplify tests, 100% coverage

--- tests/components/mutesync/test_config_flow.py
+++ tests/components/mutesync/test_config_flow.py
@@ -1,8 +1,11 @@
 """Test the mütesync config flow."""
+import asyncio
 from unittest.mock import patch
 
+import aiohttp
+import pytest
+
 from homeassistant import config_entries, setup
-from homeassistant.components.mutesync.config_flow import CannotConnect, InvalidAuth
 from homeassistant.components.mutesync.const import DOMAIN
 from homeassistant.core import HomeAssistant
 
@@ -37,49 +40,18 @@ async def test_form(hass: HomeAssistant) -> None:
     assert len(mock_setup_entry.mock_calls) == 1
 
 
-async def test_form_invalid_auth(hass: HomeAssistant) -> None:
-    """Test we handle invalid auth."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "mutesync.authenticate",
-        side_effect=InvalidAuth,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_auth"}
-
-
-async def test_form_cannot_connect(hass: HomeAssistant) -> None:
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "mutesync.authenticate",
-        side_effect=CannotConnect,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "cannot_connect"}
-
-
-async def test_form_unknown_error(hass: HomeAssistant) -> None:
+@pytest.mark.parametrize(
+    "side_effect,error",
+    [
+        (Exception, "unknown"),
+        (aiohttp.ClientResponseError(None, None, status=403), "invalid_auth"),
+        (aiohttp.ClientResponseError(None, None, status=500), "cannot_connect"),
+        (asyncio.TimeoutError, "cannot_connect"),
+    ],
+)
+async def test_form_unknown_error(
+    side_effect: Exception, error: str, hass: HomeAssistant
+) -> None:
     """Test we handle an unexpected/unknown error."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -87,7 +59,7 @@ async def test_form_unknown_error(hass: HomeAssistant) -> None:
 
     with patch(
         "mutesync.authenticate",
-        side_effect=Exception,
+        side_effect=side_effect,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -97,4 +69,4 @@ async def test_form_unknown_error(hass: HomeAssistant) -> None:
         )
 
     assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "unknown"}
+    assert result2["errors"] == {"base": error}
