19134ca5b2760af768575b5faa67ba4e169d061e
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-04-27 12:26:12 +0200

    Improve setup/unload entry

--- homeassistant/components/nam/__init__.py
+++ homeassistant/components/nam/__init__.py
@@ -1,7 +1,6 @@
 """The Nettigo Air Monitor component."""
 from __future__ import annotations
 
-import asyncio
 import logging
 from typing import Any
 
@@ -41,24 +40,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-            ]
-        )
-    )
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        del hass.data[DOMAIN]
 
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
