d72cfbe3a4ff1ad581583da9e9470c4e7982c7f4
J. Nick Koston <nick@koston.org>
2021-04-27 10:10:04 -1000

    Reduce config entry setup/unload boilerplate Q-S (#49778)

--- homeassistant/components/smhi/__init__.py
+++ homeassistant/components/smhi/__init__.py
@@ -8,16 +8,15 @@ from .const import DOMAIN  # noqa: F401
 
 DEFAULT_NAME = "smhi"
 
+PLATFORMS = ["weather"]
 
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up SMHI forecast as config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "weather")
-    )
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(config_entry, "weather")
-    return True
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
