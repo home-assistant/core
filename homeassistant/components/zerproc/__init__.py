ba048d56b6670069fb8241fc3a5b3ad3ad6b173c
J. Nick Koston <nick@koston.org>
2021-04-27 10:17:58 -1000

    Reduce config entry setup/unload boilerplate V-Z

--- homeassistant/components/zerproc/__init__.py
+++ homeassistant/components/zerproc/__init__.py
@@ -1,5 +1,4 @@
 """Zerproc lights integration."""
-import asyncio
 
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -25,10 +24,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     if DATA_ADDRESSES not in hass.data[DOMAIN]:
         hass.data[DOMAIN][DATA_ADDRESSES] = set()
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
@@ -42,11 +38,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data.pop(DOMAIN, None)
 
-    return all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-            ]
-        )
-    )
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
