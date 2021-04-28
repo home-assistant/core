e9c7908ad8aa20beecaf0a9f9e679f1c7b51fd73
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-04-28 04:22:54 -0400

    Add zwave_js discovery schema for Vision Security ZL7432 (#49510)
    * Add zwave_js discovery schema for Vision Security ZL7432
    
    * add tests
    
    * fix test
    
    * add correct fixture
    
    * Make discussed changes
    
    * fix tests
    
    * move event handler
    
    * fix logic to get entity ID
    
    * add test
    
    * make discovery test more explicit
    
    * remove domain from event data
    
    * always provide entity_id key to make automations easier and translate value if possible
    
    * formatting
    
    * comment
    
    * dont overwrite value

--- tests/components/zwave_js/test_events.py
+++ tests/components/zwave_js/test_events.py
@@ -194,3 +194,68 @@ async def test_notifications(hass, hank_binary_switch, integration, client):
     assert events[1].data["event_data"] == "555"
     assert events[1].data["command_class"] == CommandClass.ENTRY_CONTROL
     assert events[1].data["command_class_name"] == "Entry Control"
+
+
+async def test_value_updated(hass, vision_security_zl7432, integration, client):
+    """Test value updated events."""
+    node = vision_security_zl7432
+    events = async_capture_events(hass, "zwave_js_value_updated")
+
+    event = Event(
+        type="value updated",
+        data={
+            "source": "node",
+            "event": "value updated",
+            "nodeId": 7,
+            "args": {
+                "commandClassName": "Switch Binary",
+                "commandClass": 37,
+                "endpoint": 1,
+                "property": "currentValue",
+                "newValue": 1,
+                "prevValue": 0,
+                "propertyName": "currentValue",
+            },
+        },
+    )
+
+    node.receive_event(event)
+    # wait for the event
+    await hass.async_block_till_done()
+    assert len(events) == 1
+    assert events[0].data["home_id"] == client.driver.controller.home_id
+    assert events[0].data["node_id"] == 7
+    assert events[0].data["entity_id"] == "switch.in_wall_dual_relay_switch"
+    assert events[0].data["command_class"] == CommandClass.SWITCH_BINARY
+    assert events[0].data["command_class_name"] == "Switch Binary"
+    assert events[0].data["endpoint"] == 1
+    assert events[0].data["property_name"] == "currentValue"
+    assert events[0].data["property"] == "currentValue"
+    assert events[0].data["value"] == 1
+    assert events[0].data["value_raw"] == 1
+
+    # Try a value updated event on a value we aren't watching to make sure
+    # no event fires
+    event = Event(
+        type="value updated",
+        data={
+            "source": "node",
+            "event": "value updated",
+            "nodeId": 7,
+            "args": {
+                "commandClassName": "Basic",
+                "commandClass": 32,
+                "endpoint": 1,
+                "property": "currentValue",
+                "newValue": 1,
+                "prevValue": 0,
+                "propertyName": "currentValue",
+            },
+        },
+    )
+
+    node.receive_event(event)
+    # wait for the event
+    await hass.async_block_till_done()
+    # We should only still have captured one event
+    assert len(events) == 1
