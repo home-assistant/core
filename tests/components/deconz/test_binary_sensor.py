da03e49db9b0b0242d0f9b16132e0ef89a70d58b
Robert Svensson <Kane610@users.noreply.github.com>
2021-04-28 20:16:06 +0200

    Add tampering sensor to deCONZ integration (#49776)

--- tests/components/deconz/test_binary_sensor.py
+++ tests/components/deconz/test_binary_sensor.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_MOTION,
+    DEVICE_CLASS_PROBLEM,
     DEVICE_CLASS_VIBRATION,
 )
 from homeassistant.components.deconz.const import (
@@ -109,6 +110,52 @@ async def test_binary_sensors(hass, aioclient_mock, mock_deconz_websocket):
     await hass.config_entries.async_unload(config_entry.entry_id)
 
     assert hass.states.get("binary_sensor.presence_sensor").state == STATE_UNAVAILABLE
+
+    await hass.config_entries.async_remove(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert len(hass.states.async_all()) == 0
+
+
+async def test_tampering_sensor(hass, aioclient_mock, mock_deconz_websocket):
+    """Verify tampering sensor works."""
+    data = {
+        "sensors": {
+            "1": {
+                "name": "Presence sensor",
+                "type": "ZHAPresence",
+                "state": {"dark": False, "presence": False, "tampered": False},
+                "config": {"on": True, "reachable": True, "temperature": 10},
+                "uniqueid": "00:00:00:00:00:00:00:00-00",
+            },
+        }
+    }
+    with patch.dict(DECONZ_WEB_REQUEST, data):
+        config_entry = await setup_deconz_integration(hass, aioclient_mock)
+
+    assert len(hass.states.async_all()) == 3
+    presence_tamper = hass.states.get("binary_sensor.presence_sensor_tampered")
+    assert presence_tamper.state == STATE_OFF
+    assert presence_tamper.attributes[ATTR_DEVICE_CLASS] == DEVICE_CLASS_PROBLEM
+
+    event_changed_sensor = {
+        "t": "event",
+        "e": "changed",
+        "r": "sensors",
+        "id": "1",
+        "state": {"tampered": True},
+    }
+    await mock_deconz_websocket(data=event_changed_sensor)
+    await hass.async_block_till_done()
+
+    assert hass.states.get("binary_sensor.presence_sensor_tampered").state == STATE_ON
+
+    await hass.config_entries.async_unload(config_entry.entry_id)
+
+    assert (
+        hass.states.get("binary_sensor.presence_sensor_tampered").state
+        == STATE_UNAVAILABLE
+    )
 
     await hass.config_entries.async_remove(config_entry.entry_id)
     await hass.async_block_till_done()
