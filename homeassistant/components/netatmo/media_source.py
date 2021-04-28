d37294512b3419cb31052a99f7733261eae4c254
Tobias Sauerwein <cgtobi@gmail.com>
2021-04-28 14:07:04 +0000

    Fix media source bug

--- homeassistant/components/netatmo/media_source.py
+++ homeassistant/components/netatmo/media_source.py
@@ -159,7 +159,8 @@ def async_parse_identifier(
     item: MediaSourceItem,
 ) -> tuple[str, str, int | None]:
     """Parse identifier."""
-    if not item.identifier:
+    print(item.identifier)
+    if "/" not in item.identifier:
         return "events", "", None
 
     source, path = item.identifier.lstrip("/").split("/", 1)
