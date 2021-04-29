e496dbaf980cb9e493c423cabe02d86859a6994b
Antoni-Czaplicki <antekczaplicki@gmail.com>
2021-04-29 21:02:51 +0200

    Add more tests

--- tests/components/vulcan/test_config_flow.py
+++ tests/components/vulcan/test_config_flow.py
@@ -3,7 +3,7 @@ import shutil
 
 from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.vulcan import config_flow, const, register
-from homeassistant.const import CONF_PIN, CONF_REGION, CONF_TOKEN
+from homeassistant.const import CONF_PIN, CONF_REGION, CONF_SCAN_INTERVAL, CONF_TOKEN
 
 from tests.common import MockConfigEntry
 
@@ -25,7 +25,7 @@ async def test_config_flow_auth_success(hass):
         const.DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "auth"
     assert result["errors"] == {}
 
@@ -40,8 +40,28 @@ async def test_config_flow_auth_success(hass):
     shutil.rmtree(".vulcan")
 
 
+async def test_config_flow_reauth_success(hass):
+    """Test a successful config flow reauth."""
+    result = await hass.config_entries.flow.async_init(
+        const.DOMAIN, context={"source": config_entries.SOURCE_REAUTH}
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "reauth"
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_TOKEN: "FK10000", CONF_REGION: "powiatwulkanowy", CONF_PIN: "000000"},
+    )
+
+    assert result["type"] == "abort"
+    assert result["reason"] == "reauth_successful"
+    shutil.rmtree(".vulcan")
+
+
 async def test_multiple_config_entries(hass):
-    """Test a successful config flow initialized by the user."""
+    """Test a successful config flow for multiple config entries."""
     MockConfigEntry(
         domain=const.DOMAIN,
         unique_id="123456",
@@ -52,7 +72,7 @@ async def test_multiple_config_entries(hass):
         const.DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "add_next_config_entry"
     assert result["errors"] == {}
 
@@ -61,7 +81,7 @@ async def test_multiple_config_entries(hass):
         {"use_saved_credentials": False},
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "auth"
     assert result["errors"] == {}
 
@@ -77,7 +97,7 @@ async def test_multiple_config_entries(hass):
 
 
 async def test_multiple_config_entries_using_saved_credentials(hass):
-    """Test a successful config flow initialized by the user."""
+    """Test a successful config flow for multiple config entries using saved credentials."""
     MockConfigEntry(
         domain=const.DOMAIN,
         unique_id="123456",
@@ -90,7 +110,7 @@ async def test_multiple_config_entries_using_saved_credentials(hass):
         const.DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "add_next_config_entry"
     assert result["errors"] == {}
 
@@ -105,13 +125,82 @@ async def test_multiple_config_entries_using_saved_credentials(hass):
     shutil.rmtree(".vulcan")
 
 
-async def test_config_flow_auth_invalid_token(hass):
-    """Test a successful config flow initialized by the user."""
+async def test_student_already_exists(hass):
+    """Test config entry when student's entry already exists."""
+    MockConfigEntry(
+        domain=const.DOMAIN,
+        unique_id="111",
+        data={"student_id": "111", "login": "jan@fakelog.cf"},
+    ).add_to_hass(hass)
+
+    await register.register(hass, "FK10000", "powiatwulkanowy", "000000")
+
     result = await hass.config_entries.flow.async_init(
         const.DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "add_next_config_entry"
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"use_saved_credentials": True},
+    )
+
+    assert result["type"] == "abort"
+    assert result["reason"] == "all_student_already_configured"
+    shutil.rmtree(".vulcan")
+
+
+async def test_multiple_config_entries_with_selecting_saved_credentials(hass):
+    """Test a successful config flow for multiple config entries with selecting saved credentials."""
+    MockConfigEntry(
+        domain=const.DOMAIN,
+        unique_id="123456",
+        data={"student_id": "123456", "login": "example@mail.com"},
+    ).add_to_hass(hass)
+
+    await register.register(hass, "FK10000", "powiatwulkanowy", "000000")
+
+    open(".vulcan/keystore-test.json", "w")
+    open(".vulcan/account-test.json", "w")
+
+    result = await hass.config_entries.flow.async_init(
+        const.DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "add_next_config_entry"
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"use_saved_credentials": True},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "select_saved_credentials"
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"credentials": ".vulcan/account-jan@fakelog.cf.json"},
+    )
+
+    assert result["type"] == "create_entry"
+    assert result["title"] == "Jan Kowalski"
+    assert result["data"] == {"student_id": "111", "login": "jan@fakelog.cf"}
+    shutil.rmtree(".vulcan")
+
+
+async def test_config_flow_auth_invalid_token(hass):
+    """Test a config flow initialized by the user using invalid token."""
+    result = await hass.config_entries.flow.async_init(
+        const.DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "auth"
     assert result["errors"] == {}
 
@@ -120,18 +209,18 @@ async def test_config_flow_auth_invalid_token(hass):
         {CONF_TOKEN: "3S20000", CONF_REGION: "powiatwulkanowy", CONF_PIN: "000000"},
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "auth"
     assert result["errors"] == {"base": "invalid_token"}
 
 
 async def test_config_flow_auth_invalid_region(hass):
-    """Test a successful config flow initialized by the user."""
+    """Test a config flow initialized by the user using invalid region."""
     result = await hass.config_entries.flow.async_init(
         const.DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "auth"
     assert result["errors"] == {}
 
@@ -140,6 +229,37 @@ async def test_config_flow_auth_invalid_region(hass):
         {CONF_TOKEN: "3S10000", CONF_REGION: "invalid_region", CONF_PIN: "000000"},
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "auth"
     assert result["errors"] == {"base": "invalid_symbol"}
+
+
+async def test_options_flow(hass):
+    """Test config flow options."""
+    await register.register(hass, "FK10000", "powiatwulkanowy", "000000")
+    config_entry = MockConfigEntry(
+        domain=const.DOMAIN,
+        unique_id="111",
+        data={"student_id": "111", "login": "jan@fakelog.cf"},
+    )
+    config_entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    result = await hass.config_entries.options.async_init(config_entry.entry_id)
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "init"
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"], user_input={CONF_SCAN_INTERVAL: 2137}
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert config_entry.options == {CONF_SCAN_INTERVAL: 2137}
+
+    await hass.async_block_till_done()
+    assert await hass.config_entries.async_unload(config_entry.entry_id)
+    await hass.async_block_till_done()
+    shutil.rmtree(".vulcan")
