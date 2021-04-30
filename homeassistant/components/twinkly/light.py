751589cacff4ed5f0b415fd00c7955f8ab00af7f
Ville Skyttä <ville.skytta@iki.fi>
2021-05-01 00:21:39 +0300

    Make device info a TypedDict (#49670)

--- homeassistant/components/twinkly/light.py
+++ homeassistant/components/twinkly/light.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 import asyncio
 import logging
-from typing import Any
 
 from aiohttp import ClientError
 
@@ -14,6 +13,7 @@ from homeassistant.components.light import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
 
 from .const import (
     ATTR_HOST,
@@ -105,7 +105,7 @@ class TwinklyLight(LightEntity):
         return "mdi:string-lights"
 
     @property
-    def device_info(self) -> dict[str, Any] | None:
+    def device_info(self) -> DeviceInfo | None:
         """Get device specific attributes."""
         return (
             {
