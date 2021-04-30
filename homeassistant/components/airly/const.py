3e96c674e717aa4a0a850a816187fc596b066521
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-04-30 13:37:24 +0200

    Use typing.Final for consts

--- homeassistant/components/airly/const.py
+++ homeassistant/components/airly/const.py
@@ -1,4 +1,8 @@
 """Constants for Airly integration."""
+from __future__ import annotations
+
+from typing import Final
+
 from homeassistant.const import (
     CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
     DEVICE_CLASS_HUMIDITY,
@@ -11,30 +15,30 @@ from homeassistant.const import (
 
 from .model import SensorDescription
 
-ATTR_API_ADVICE = "ADVICE"
-ATTR_API_CAQI = "CAQI"
-ATTR_API_CAQI_DESCRIPTION = "DESCRIPTION"
-ATTR_API_CAQI_LEVEL = "LEVEL"
-ATTR_API_HUMIDITY = "HUMIDITY"
-ATTR_API_PM1 = "PM1"
-ATTR_API_PM10 = "PM10"
-ATTR_API_PM10_LIMIT = "PM10_LIMIT"
-ATTR_API_PM10_PERCENT = "PM10_PERCENT"
-ATTR_API_PM25 = "PM25"
-ATTR_API_PM25_LIMIT = "PM25_LIMIT"
-ATTR_API_PM25_PERCENT = "PM25_PERCENT"
-ATTR_API_PRESSURE = "PRESSURE"
-ATTR_API_TEMPERATURE = "TEMPERATURE"
+ATTR_API_ADVICE: Final = "ADVICE"
+ATTR_API_CAQI: Final = "CAQI"
+ATTR_API_CAQI_DESCRIPTION: Final = "DESCRIPTION"
+ATTR_API_CAQI_LEVEL: Final = "LEVEL"
+ATTR_API_HUMIDITY: Final = "HUMIDITY"
+ATTR_API_PM1: Final = "PM1"
+ATTR_API_PM10: Final = "PM10"
+ATTR_API_PM10_LIMIT: Final = "PM10_LIMIT"
+ATTR_API_PM10_PERCENT: Final = "PM10_PERCENT"
+ATTR_API_PM25: Final = "PM25"
+ATTR_API_PM25_LIMIT: Final = "PM25_LIMIT"
+ATTR_API_PM25_PERCENT: Final = "PM25_PERCENT"
+ATTR_API_PRESSURE: Final = "PRESSURE"
+ATTR_API_TEMPERATURE: Final = "TEMPERATURE"
 
-ATTRIBUTION = "Data provided by Airly"
-CONF_USE_NEAREST = "use_nearest"
-DEFAULT_NAME = "Airly"
-DOMAIN = "airly"
-LABEL_ADVICE = "advice"
-MANUFACTURER = "Airly sp. z o.o."
-MAX_UPDATE_INTERVAL = 90
-MIN_UPDATE_INTERVAL = 5
-NO_AIRLY_SENSORS = "There are no Airly sensors in this area yet."
+ATTRIBUTION: Final = "Data provided by Airly"
+CONF_USE_NEAREST: Final = "use_nearest"
+DEFAULT_NAME: Final = "Airly"
+DOMAIN: Final = "airly"
+LABEL_ADVICE: Final = "advice"
+MANUFACTURER: Final = "Airly sp. z o.o."
+MAX_UPDATE_INTERVAL: Final = 90
+MIN_UPDATE_INTERVAL: Final = 5
+NO_AIRLY_SENSORS: Final = "There are no Airly sensors in this area yet."
 
 SENSOR_TYPES: dict[str, SensorDescription] = {
     ATTR_API_PM1: {
