dc8ab0194c0f340b3346faad6b4be546b9070b85
Aidan Timson <contact@timmo.xyz>
2021-05-01 14:03:47 +0100

    Use hass.config_entries.async_unload_platforms

--- homeassistant/components/system_bridge/__init__.py
+++ homeassistant/components/system_bridge/__init__.py
@@ -184,14 +184,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
+    unload_ok = hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
