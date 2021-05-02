56b47536f242a3dda597d3366543583305a03e6d
lewei50 <service@lewei50.com>
2021-05-02 02:33:19 +0000

    Refactor config flow.

--- homeassistant/components/iammeter/__init__.py
+++ homeassistant/components/iammeter/__init__.py
@@ -2,10 +2,7 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Component setup, do nothing."""
-    return True
+PLATFORMS = ["sensor"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
@@ -13,7 +10,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     # hass.async_create_task(
     #     hass.config_entries.async_forward_entry_setup(entry, "sensor")
     # )
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
 
 
