662f25e9215e3b1b6dcde803e593c729755fc772
Tom Brien <tom@byth.co.uk>
2021-05-02 21:31:41 +0000

    Update tests

--- tests/components/coinbase/test_config_flow.py
+++ tests/components/coinbase/test_config_flow.py
@@ -54,20 +54,13 @@ async def test_form(hass):
             {
                 CONF_API_KEY: "123456",
                 CONF_API_TOKEN: "AbCDeF",
-                CONF_CURRENCIES: "BTC, USD",
-                CONF_EXCHANGE_RATES: "ATOM, BTC",
             },
         )
         await hass.async_block_till_done()
 
     assert result2["type"] == "create_entry"
     assert result2["title"] == "Test User"
-    assert result2["data"] == {
-        CONF_API_KEY: "123456",
-        CONF_API_TOKEN: "AbCDeF",
-        CONF_CURRENCIES: ["BTC", "USD"],
-        CONF_EXCHANGE_RATES: ["ATOM", "BTC"],
-    }
+    assert result2["data"] == {CONF_API_KEY: "123456", CONF_API_TOKEN: "AbCDeF"}
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -95,8 +88,6 @@ async def test_form_invalid_auth(hass):
             {
                 CONF_API_KEY: "123456",
                 CONF_API_TOKEN: "AbCDeF",
-                CONF_CURRENCIES: "BTC, USD",
-                CONF_EXCHANGE_RATES: "ATOM, BTC",
             },
         )
 
@@ -119,8 +110,6 @@ async def test_form_cannot_connect(hass):
             {
                 CONF_API_KEY: "123456",
                 CONF_API_TOKEN: "AbCDeF",
-                CONF_CURRENCIES: "BTC, USD",
-                CONF_EXCHANGE_RATES: "ATOM, BTC",
             },
         )
 
@@ -143,8 +132,6 @@ async def test_form_catch_all_exception(hass):
             {
                 CONF_API_KEY: "123456",
                 CONF_API_TOKEN: "AbCDeF",
-                CONF_CURRENCIES: "BTC, USD",
-                CONF_EXCHANGE_RATES: "ATOM, BTC",
             },
         )
 
@@ -152,11 +139,51 @@ async def test_form_catch_all_exception(hass):
     assert result2["errors"] == {"base": "unknown"}
 
 
-async def test_form_bad_account_currency(hass):
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+async def test_option_good_account_currency(hass):
+    """Test we handle a good wallet currency option."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="abcde12345",
+        title="Test User",
+        data={CONF_API_KEY: "123456", CONF_API_TOKEN: "AbCDeF"},
+        options={
+            CONF_CURRENCIES: [],
+            CONF_EXCHANGE_RATES: [],
+        },
     )
+    config_entry.add_to_hass(hass)
+
+    with patch(
+        "coinbase.wallet.client.Client.get_current_user",
+        return_value={"name": "Test User", "id": "123456789"},
+    ), patch(
+        "coinbase.wallet.client.Client.get_accounts",
+        return_value={
+            "data": [
+                {
+                    "currency": "BTC",
+                    "name": "BTC Wallet",
+                    "id": "abcd",
+                    "balance": {"amount": "1.00", "currency": "BTC"},
+                    "native_balance": {"amount": "0.01", "currency": "USD"},
+                },
+                {
+                    "currency": "USD",
+                    "name": "USD Wallet",
+                    "id": "efgh",
+                    "balance": {"amount": "9.90", "currency": "USD"},
+                    "native_balance": {"amount": "9.90", "currency": "USD"},
+                },
+            ]
+        },
+    ), patch(
+        "coinbase.wallet.client.Client.get_exchange_rates",
+        return_value={"currency": "USD", "rates": {"ATOM": "0.109", "BTC": "0.00002"}},
+    ):
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+        result = await hass.config_entries.options.async_init(config_entry.entry_id)
+        await hass.async_block_till_done()
 
     with patch(
         "coinbase.wallet.client.Client.get_current_user",
@@ -167,9 +194,17 @@ async def test_form_bad_account_currency(hass):
             "data": [
                 {
                     "currency": "BTC",
+                    "name": "BTC Wallet",
+                    "id": "abcd",
+                    "balance": {"amount": "1.00", "currency": "BTC"},
+                    "native_balance": {"amount": "0.01", "currency": "USD"},
                 },
                 {
                     "currency": "USD",
+                    "name": "USD Wallet",
+                    "id": "efgh",
+                    "balance": {"amount": "9.90", "currency": "USD"},
+                    "native_balance": {"amount": "9.90", "currency": "USD"},
                 },
             ]
         },
@@ -177,25 +212,229 @@ async def test_form_bad_account_currency(hass):
         "coinbase.wallet.client.Client.get_exchange_rates",
         return_value={"currency": "USD", "rates": {"ATOM": "0.109", "BTC": "0.00002"}},
     ):
-        result2 = await hass.config_entries.flow.async_configure(
+        result = await hass.config_entries.options.async_configure(
             result["flow_id"],
-            {
-                CONF_API_KEY: "123456",
-                CONF_API_TOKEN: "AbCDeF",
-                CONF_CURRENCIES: "NOT_A_CURRENCY",
-                CONF_EXCHANGE_RATES: "ATOM, BTC",
+            user_input={
+                CONF_CURRENCIES: ["BTC"],
+                CONF_EXCHANGE_RATES: [],
+            },
+        )
+    assert result["type"] == "create_entry"
+
+
+async def test_form_bad_account_currency(hass):
+    """Test we handle a bad currency option."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="abcde12345",
+        title="Test User",
+        data={CONF_API_KEY: "123456", CONF_API_TOKEN: "AbCDeF"},
+        options={
+            CONF_CURRENCIES: [],
+            CONF_EXCHANGE_RATES: [],
+        },
+    )
+    config_entry.add_to_hass(hass)
+
+    with patch(
+        "coinbase.wallet.client.Client.get_current_user",
+        return_value={"name": "Test User", "id": "123456789"},
+    ), patch(
+        "coinbase.wallet.client.Client.get_accounts",
+        return_value={
+            "data": [
+                {
+                    "currency": "BTC",
+                    "name": "BTC Wallet",
+                    "id": "abcd",
+                    "balance": {"amount": "1.00", "currency": "BTC"},
+                    "native_balance": {"amount": "0.01", "currency": "USD"},
+                },
+                {
+                    "currency": "USD",
+                    "name": "USD Wallet",
+                    "id": "efgh",
+                    "balance": {"amount": "9.90", "currency": "USD"},
+                    "native_balance": {"amount": "9.90", "currency": "USD"},
+                },
+            ]
+        },
+    ), patch(
+        "coinbase.wallet.client.Client.get_exchange_rates",
+        return_value={"currency": "USD", "rates": {"ATOM": "0.109", "BTC": "0.00002"}},
+    ):
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+        result = await hass.config_entries.options.async_init(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    with patch(
+        "coinbase.wallet.client.Client.get_current_user",
+        return_value={"name": "Test User"},
+    ), patch(
+        "coinbase.wallet.client.Client.get_accounts",
+        return_value={
+            "data": [
+                {
+                    "currency": "BTC",
+                    "name": "BTC Wallet",
+                    "id": "abcd",
+                    "balance": {"amount": "1.00", "currency": "BTC"},
+                    "native_balance": {"amount": "0.01", "currency": "USD"},
+                },
+                {
+                    "currency": "USD",
+                    "name": "USD Wallet",
+                    "id": "efgh",
+                    "balance": {"amount": "9.90", "currency": "USD"},
+                    "native_balance": {"amount": "9.90", "currency": "USD"},
+                },
+            ]
+        },
+    ), patch(
+        "coinbase.wallet.client.Client.get_exchange_rates",
+        return_value={"currency": "USD", "rates": {"ATOM": "0.109", "BTC": "0.00002"}},
+    ):
+        result = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_CURRENCIES: ["ETH"],
+                CONF_EXCHANGE_RATES: [],
             },
         )
 
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "currency_unavaliable"}
+    assert result["type"] == "form"
+    assert result["errors"] == {"base": "currency_unavaliable"}
+
+
+async def test_option_good_exchange_rate(hass):
+    """Test we handle a good exchange rate option."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="abcde12345",
+        title="Test User",
+        data={CONF_API_KEY: "123456", CONF_API_TOKEN: "AbCDeF"},
+        options={
+            CONF_CURRENCIES: [],
+            CONF_EXCHANGE_RATES: [],
+        },
+    )
+    config_entry.add_to_hass(hass)
+
+    with patch(
+        "coinbase.wallet.client.Client.get_current_user",
+        return_value={"name": "Test User", "id": "123456789"},
+    ), patch(
+        "coinbase.wallet.client.Client.get_accounts",
+        return_value={
+            "data": [
+                {
+                    "currency": "BTC",
+                    "name": "BTC Wallet",
+                    "id": "abcd",
+                    "balance": {"amount": "1.00", "currency": "BTC"},
+                    "native_balance": {"amount": "0.01", "currency": "USD"},
+                },
+                {
+                    "currency": "USD",
+                    "name": "USD Wallet",
+                    "id": "efgh",
+                    "balance": {"amount": "9.90", "currency": "USD"},
+                    "native_balance": {"amount": "9.90", "currency": "USD"},
+                },
+            ]
+        },
+    ), patch(
+        "coinbase.wallet.client.Client.get_exchange_rates",
+        return_value={"currency": "USD", "rates": {"ATOM": "0.109", "BTC": "0.00002"}},
+    ):
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+        result = await hass.config_entries.options.async_init(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    with patch(
+        "coinbase.wallet.client.Client.get_current_user",
+        return_value={"name": "Test User"},
+    ), patch(
+        "coinbase.wallet.client.Client.get_accounts",
+        return_value={
+            "data": [
+                {
+                    "currency": "BTC",
+                    "name": "BTC Wallet",
+                    "id": "abcd",
+                    "balance": {"amount": "1.00", "currency": "BTC"},
+                    "native_balance": {"amount": "0.01", "currency": "USD"},
+                },
+                {
+                    "currency": "USD",
+                    "name": "USD Wallet",
+                    "id": "efgh",
+                    "balance": {"amount": "9.90", "currency": "USD"},
+                    "native_balance": {"amount": "9.90", "currency": "USD"},
+                },
+            ]
+        },
+    ), patch(
+        "coinbase.wallet.client.Client.get_exchange_rates",
+        return_value={"currency": "USD", "rates": {"ATOM": "0.109", "BTC": "0.00002"}},
+    ):
+        result = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_CURRENCIES: [],
+                CONF_EXCHANGE_RATES: ["BTC"],
+            },
+        )
+    assert result["type"] == "create_entry"
 
 
 async def test_form_bad_exchange_rate(hass):
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    """Test we handle a bad currency option."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="abcde12345",
+        title="Test User",
+        data={CONF_API_KEY: "123456", CONF_API_TOKEN: "AbCDeF"},
+        options={
+            CONF_CURRENCIES: [],
+            CONF_EXCHANGE_RATES: [],
+        },
     )
+    config_entry.add_to_hass(hass)
+
+    with patch(
+        "coinbase.wallet.client.Client.get_current_user",
+        return_value={"name": "Test User", "id": "123456789"},
+    ), patch(
+        "coinbase.wallet.client.Client.get_accounts",
+        return_value={
+            "data": [
+                {
+                    "currency": "BTC",
+                    "name": "BTC Wallet",
+                    "id": "abcd",
+                    "balance": {"amount": "1.00", "currency": "BTC"},
+                    "native_balance": {"amount": "0.01", "currency": "USD"},
+                },
+                {
+                    "currency": "USD",
+                    "name": "USD Wallet",
+                    "id": "efgh",
+                    "balance": {"amount": "9.90", "currency": "USD"},
+                    "native_balance": {"amount": "9.90", "currency": "USD"},
+                },
+            ]
+        },
+    ), patch(
+        "coinbase.wallet.client.Client.get_exchange_rates",
+        return_value={"currency": "USD", "rates": {"ATOM": "0.109", "BTC": "0.00002"}},
+    ):
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+        result = await hass.config_entries.options.async_init(config_entry.entry_id)
+        await hass.async_block_till_done()
 
     with patch(
         "coinbase.wallet.client.Client.get_current_user",
@@ -206,9 +445,17 @@ async def test_form_bad_exchange_rate(hass):
             "data": [
                 {
                     "currency": "BTC",
+                    "name": "BTC Wallet",
+                    "id": "abcd",
+                    "balance": {"amount": "1.00", "currency": "BTC"},
+                    "native_balance": {"amount": "0.01", "currency": "USD"},
                 },
                 {
                     "currency": "USD",
+                    "name": "USD Wallet",
+                    "id": "efgh",
+                    "balance": {"amount": "9.90", "currency": "USD"},
+                    "native_balance": {"amount": "9.90", "currency": "USD"},
                 },
             ]
         },
@@ -216,18 +463,77 @@ async def test_form_bad_exchange_rate(hass):
         "coinbase.wallet.client.Client.get_exchange_rates",
         return_value={"currency": "USD", "rates": {"ATOM": "0.109", "BTC": "0.00002"}},
     ):
-        result2 = await hass.config_entries.flow.async_configure(
+        result = await hass.config_entries.options.async_configure(
             result["flow_id"],
-            {
-                CONF_API_KEY: "123456",
-                CONF_API_TOKEN: "AbCDeF",
-                CONF_CURRENCIES: "BTC, USD",
-                CONF_EXCHANGE_RATES: "NOT_A_RATE",
+            user_input={
+                CONF_CURRENCIES: [],
+                CONF_EXCHANGE_RATES: ["ETH"],
+            },
+        )
+    assert result["type"] == "form"
+    assert result["errors"] == {"base": "exchange_rate_unavaliable"}
+
+
+async def test_option_catch_all_exception(hass):
+    """Test we handle an unknown exception in the option flow."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="abcde12345",
+        title="Test User",
+        data={CONF_API_KEY: "123456", CONF_API_TOKEN: "AbCDeF"},
+        options={
+            CONF_CURRENCIES: [],
+            CONF_EXCHANGE_RATES: [],
+        },
+    )
+    config_entry.add_to_hass(hass)
+
+    with patch(
+        "coinbase.wallet.client.Client.get_current_user",
+        return_value={"name": "Test User", "id": "123456789"},
+    ), patch(
+        "coinbase.wallet.client.Client.get_accounts",
+        return_value={
+            "data": [
+                {
+                    "currency": "BTC",
+                    "name": "BTC Wallet",
+                    "id": "abcd",
+                    "balance": {"amount": "1.00", "currency": "BTC"},
+                    "native_balance": {"amount": "0.01", "currency": "USD"},
+                },
+                {
+                    "currency": "USD",
+                    "name": "USD Wallet",
+                    "id": "efgh",
+                    "balance": {"amount": "9.90", "currency": "USD"},
+                    "native_balance": {"amount": "9.90", "currency": "USD"},
+                },
+            ]
+        },
+    ), patch(
+        "coinbase.wallet.client.Client.get_exchange_rates",
+        return_value={"currency": "USD", "rates": {"ATOM": "0.109", "BTC": "0.00002"}},
+    ):
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+        result = await hass.config_entries.options.async_init(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    with patch(
+        "coinbase.wallet.client.Client.get_accounts",
+        side_effect=Exception,
+    ):
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_CURRENCIES: [],
+                CONF_EXCHANGE_RATES: ["ETH"],
             },
         )
 
     assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "exchange_rate_unavaliable"}
+    assert result2["errors"] == {"base": "unknown"}
 
 
 async def test_yaml_import(hass):
@@ -277,8 +583,6 @@ async def test_yaml_existing(hass):
         data={
             CONF_API_KEY: "123456",
             CONF_API_TOKEN: "AbCDeF",
-            CONF_CURRENCIES: ["BTC", "USD"],
-            CONF_EXCHANGE_RATES: ["ATOM", "BTC"],
         },
     ).add_to_hass(hass)
 
@@ -288,8 +592,6 @@ async def test_yaml_existing(hass):
         data={
             CONF_API_KEY: "123456",
             CONF_YAML_API_TOKEN: "AbCDeF",
-            CONF_CURRENCIES: ["BTC", "USD"],
-            CONF_EXCHANGE_RATES: ["ATOM", "BTC"],
         },
     )
 
