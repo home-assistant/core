dab9c1181606dea16a0a81821266d7a12daf7a16
marengaz <marengbj@gmail.com>
2021-05-03 18:36:02 +0100

    strict typing

--- homeassistant/components/rako/util.py
+++ homeassistant/components/rako/util.py
@@ -5,7 +5,7 @@ import hashlib
 from typing import Any
 
 
-def create_unique_id(bridge_id, room_id, channel_id) -> str:
+def create_unique_id(bridge_id: str, room_id: int, channel_id: int) -> str:
     """Create Unique ID for light."""
     return f"b:{bridge_id}r:{room_id}c:{channel_id}"
 
