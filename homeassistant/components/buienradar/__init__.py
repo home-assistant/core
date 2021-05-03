aaf549efc56a8090024a8a2b9cb712d81a8dc2d2
Rob Bierbooms <mail@robbierbooms.nl>
2021-05-03 07:26:55 +0000

    Fix issue with unload

--- homeassistant/components/buienradar/__init__.py
+++ homeassistant/components/buienradar/__init__.py
@@ -49,8 +49,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
 
