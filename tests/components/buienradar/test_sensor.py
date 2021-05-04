9aaf34e06cf57fc1782bc2cd4e9884b16e41ab77
Rob Bierbooms <mail@robbierbooms.nl>
2021-05-04 13:49:16 +0200

    Add configuration flow for Buienradar integration (#37796)
    * Add configuration flow for Buienradar integration
    
    * Update buienradar camera tests to work with config flow
    
    * Update buienradar weather tests to work with config flow
    
    * Update buienradar sensor tests to work with config flow
    
    * Remove buienradar config_flow tests to pass tests
    
    * Add config flow tests for buienradar integration
    
    * Increase test coverage for buienradar config_flow tests
    
    * Move data into domain
    
    * Remove forecast option
    
    * Move data to options
    
    * Remove options from config flow
    
    * Adjust tests
    
    * Adjust string
    
    * Fix pylint issues
    
    * Rework review comments
    
    * Handle import
    
    * Change config flow to setup camera or weather
    
    * Fix tests
    
    * Remove translated file
    
    * Fix pylint
    
    * Fix flake8
    
    * Fix unload
    
    * Minor name changes
    
    * Update homeassistant/components/buienradar/config_flow.py
    
    Co-authored-by: Ties de Kock <ties@tiesdekock.nl>
    
    * Remove asynctest
    
    * Add translation
    
    * Disable sensors by default
    
    * Remove integration name from translations
    
    * Remove import method
    
    * Drop  selection between platforms, disable camera by default
    
    * Minor fix in configured_instances
    
    * Bugfix in weather
    
    * Rework import
    
    * Change unique ids of camera
    
    * Fix in import
    
    * Fix camera tests
    
    * Fix sensor test
    
    * Fix sensor test 2
    
    * Fix config flow tests
    
    * Add option flow
    
    * Add tests for option flow
    
    * Add import tests
    
    * Some cleanups
    
    * Apply suggestions from code review
    
    Apply code suggestions
    
    Co-authored-by: Franck Nijhof <git@frenck.dev>
    
    * Fix isort,black,mypy
    
    * Small tweaks and added typing to new parts
    
    * Fix review comments (1)
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * Fix review comments (2)
    
    * Fix issues
    
    * Fix unique id
    
    * Improve tests
    
    * Extend tests
    
    * Fix issue with unload
    
    * Address review comments
    
    * Add warning when loading platform
    
    * Add load/unload test
    
    Co-authored-by: Ties de Kock <ties@tiesdekock.nl>
    Co-authored-by: Franck Nijhof <git@frenck.dev>
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/buienradar/test_sensor.py
+++ tests/components/buienradar/test_sensor.py
@@ -1,26 +1,29 @@
 """The tests for the Buienradar sensor platform."""
-from homeassistant.components import sensor
-from homeassistant.setup import async_setup_component
+from unittest.mock import patch
+
+from homeassistant.components.buienradar.const import DOMAIN
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
+
+from tests.common import MockConfigEntry
 
 CONDITIONS = ["stationname", "temperature"]
-BASE_CONFIG = {
-    "sensor": [
-        {
-            "platform": "buienradar",
-            "name": "volkel",
-            "latitude": 51.65,
-            "longitude": 5.7,
-            "monitored_conditions": CONDITIONS,
-        }
-    ]
-}
+TEST_CFG_DATA = {CONF_LATITUDE: 51.5288504, CONF_LONGITUDE: 5.4002156}
 
 
 async def test_smoke_test_setup_component(hass):
     """Smoke test for successfully set-up with default config."""
-    assert await async_setup_component(hass, sensor.DOMAIN, BASE_CONFIG)
-    await hass.async_block_till_done()
+    mock_entry = MockConfigEntry(domain=DOMAIN, unique_id="TEST_ID", data=TEST_CFG_DATA)
+
+    mock_entry.add_to_hass(hass)
+
+    with patch(
+        "homeassistant.components.buienradar.sensor.BrSensor.entity_registry_enabled_default"
+    ) as enabled_by_default_mock:
+        enabled_by_default_mock.return_value = True
+
+        await hass.config_entries.async_setup(mock_entry.entry_id)
+        await hass.async_block_till_done()
 
     for cond in CONDITIONS:
-        state = hass.states.get(f"sensor.volkel_{cond}")
+        state = hass.states.get(f"sensor.buienradar_{cond}")
         assert state.state == "unknown"
