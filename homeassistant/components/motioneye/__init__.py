9d2a610d37469231933fde5f571bd253c97490da
Dermot Duffy <dermot.duffy@gmail.com>
2021-05-03 23:19:41 -0700

    Enable mypy for motionEye (aye aye!) (#49738)

--- homeassistant/components/motioneye/__init__.py
+++ homeassistant/components/motioneye/__init__.py
@@ -65,10 +65,10 @@ def get_motioneye_entity_unique_id(
 
 
 def get_camera_from_cameras(
-    camera_id: int, data: dict[str, Any]
+    camera_id: int, data: dict[str, Any] | None
 ) -> dict[str, Any] | None:
     """Get an individual camera dict from a multiple cameras data response."""
-    for camera in data.get(KEY_CAMERAS) or []:
+    for camera in data.get(KEY_CAMERAS, []) if data else []:
         if camera.get(KEY_ID) == camera_id:
             val: dict[str, Any] = camera
             return val
@@ -105,7 +105,7 @@ def _add_camera(
     entry: ConfigEntry,
     camera_id: int,
     camera: dict[str, Any],
-    device_identifier: tuple[str, str, int],
+    device_identifier: tuple[str, str],
 ) -> None:
     """Add a motionEye camera to hass."""
 
@@ -164,14 +164,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         CONF_COORDINATOR: coordinator,
     }
 
-    current_cameras: set[tuple[str, str, int]] = set()
+    current_cameras: set[tuple[str, str]] = set()
     device_registry = await dr.async_get_registry(hass)
 
     @callback
     def _async_process_motioneye_cameras() -> None:
         """Process motionEye camera additions and removals."""
-        inbound_camera: set[tuple[str, str, int]] = set()
-        if KEY_CAMERAS not in coordinator.data:
+        inbound_camera: set[tuple[str, str]] = set()
+        if coordinator.data is None or KEY_CAMERAS not in coordinator.data:
             return
 
         for camera in coordinator.data[KEY_CAMERAS]:
