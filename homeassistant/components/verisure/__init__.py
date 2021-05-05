fc80acd9d71c8cbec174089fa36f12d70e24b4cf
Franck Nijhof <git@frenck.dev>
2021-05-05 09:51:05 +0200

    Remove YAML configuration from Verisure (#50076)

--- homeassistant/components/verisure/__init__.py
+++ homeassistant/components/verisure/__init__.py
@@ -3,9 +3,6 @@ from __future__ import annotations
 
 from contextlib import suppress
 import os
-from typing import Any
-
-import voluptuous as vol
 
 from homeassistant.components.alarm_control_panel import (
     DOMAIN as ALARM_CONTROL_PANEL_DOMAIN,
@@ -15,27 +12,14 @@ from homeassistant.components.camera import DOMAIN as CAMERA_DOMAIN
 from homeassistant.components.lock import DOMAIN as LOCK_DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
-from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
-from homeassistant.const import (
-    CONF_EMAIL,
-    CONF_PASSWORD,
-    CONF_USERNAME,
-    EVENT_HOMEASSISTANT_STOP,
-)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.storage import STORAGE_DIR
 
-from .const import (
-    CONF_CODE_DIGITS,
-    CONF_DEFAULT_LOCK_CODE,
-    CONF_GIID,
-    CONF_LOCK_CODE_DIGITS,
-    CONF_LOCK_DEFAULT_CODE,
-    DEFAULT_LOCK_CODE_DIGITS,
-    DOMAIN,
-)
+from .const import DOMAIN
 from .coordinator import VerisureDataUpdateCoordinator
 
 PLATFORMS = [
@@ -47,80 +31,11 @@ PLATFORMS = [
     SWITCH_DOMAIN,
 ]
 
-CONFIG_SCHEMA = vol.Schema(
-    vol.All(
-        cv.deprecated(DOMAIN),
-        {
-            DOMAIN: vol.Schema(
-                {
-                    vol.Required(CONF_PASSWORD): cv.string,
-                    vol.Required(CONF_USERNAME): cv.string,
-                    vol.Optional(CONF_CODE_DIGITS): cv.positive_int,
-                    vol.Optional(CONF_GIID): cv.string,
-                    vol.Optional(CONF_DEFAULT_LOCK_CODE): cv.string,
-                },
-                extra=vol.ALLOW_EXTRA,
-            )
-        },
-    ),
-    extra=vol.ALLOW_EXTRA,
-)
-
-
-async def async_setup(hass: HomeAssistant, config: dict[str, Any]) -> bool:
-    """Set up the Verisure integration."""
-    if DOMAIN in config:
-        hass.async_create_task(
-            hass.config_entries.flow.async_init(
-                DOMAIN,
-                context={"source": SOURCE_IMPORT},
-                data={
-                    CONF_EMAIL: config[DOMAIN][CONF_USERNAME],
-                    CONF_PASSWORD: config[DOMAIN][CONF_PASSWORD],
-                    CONF_GIID: config[DOMAIN].get(CONF_GIID),
-                    CONF_LOCK_CODE_DIGITS: config[DOMAIN].get(CONF_CODE_DIGITS),
-                    CONF_LOCK_DEFAULT_CODE: config[DOMAIN].get(CONF_LOCK_DEFAULT_CODE),
-                },
-            )
-        )
-
-    return True
+CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Verisure from a config entry."""
-    # Migrate old YAML settings (hidden in the config entry),
-    # to config entry options. Can be removed after YAML support is gone.
-    if CONF_LOCK_CODE_DIGITS in entry.data or CONF_DEFAULT_LOCK_CODE in entry.data:
-        options = entry.options.copy()
-
-        if (
-            CONF_LOCK_CODE_DIGITS in entry.data
-            and CONF_LOCK_CODE_DIGITS not in entry.options
-            and entry.data[CONF_LOCK_CODE_DIGITS] != DEFAULT_LOCK_CODE_DIGITS
-        ):
-            options.update(
-                {
-                    CONF_LOCK_CODE_DIGITS: entry.data[CONF_LOCK_CODE_DIGITS],
-                }
-            )
-
-        if (
-            CONF_DEFAULT_LOCK_CODE in entry.data
-            and CONF_DEFAULT_LOCK_CODE not in entry.options
-        ):
-            options.update(
-                {
-                    CONF_DEFAULT_LOCK_CODE: entry.data[CONF_DEFAULT_LOCK_CODE],
-                }
-            )
-
-        data = entry.data.copy()
-        data.pop(CONF_LOCK_CODE_DIGITS, None)
-        data.pop(CONF_DEFAULT_LOCK_CODE, None)
-        hass.config_entries.async_update_entry(entry, data=data, options=options)
-
-    # Continue as normal...
     coordinator = VerisureDataUpdateCoordinator(hass, entry=entry)
 
     if not await coordinator.async_login():
