b09df0c3eb343eceb274df490cc50e8e570790a5
Ludeeus <ludeeus@ludeeus.dev>
2021-05-05 13:14:06 +0000

    Address comments

--- homeassistant/components/hassio/__init__.py
+++ homeassistant/components/hassio/__init__.py
@@ -477,11 +477,12 @@ async def async_setup(hass: HomeAssistant, config: Config) -> bool:  # noqa: C90
             and await recorder.async_migration_in_progress(hass)
         ):
             _LOGGER.error(
-                "The system cannot %s while a database upgrade in progress",
+                "The system cannot %s while a database upgrade is in progress",
                 call.service,
             )
             raise HomeAssistantError(
-                f"The system cannot {call.service} while a database upgrade in progress."
+                f"The system cannot {call.service} "
+                "while a database upgrade is in progress."
             )
 
         if call.service == SERVICE_HOMEASSISTANT_STOP:
@@ -502,7 +503,8 @@ async def async_setup(hass: HomeAssistant, config: Config) -> bool:  # noqa: C90
                 f"{HASS_DOMAIN}.check_config",
             )
             raise HomeAssistantError(
-                f"The system cannot {call.service} because the configuration is not valid: {errors}"
+                f"The system cannot {call.service} "
+                f"because the configuration is not valid: {errors}"
             )
 
         if call.service == SERVICE_HOMEASSISTANT_RESTART:
