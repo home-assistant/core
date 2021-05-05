8c9648189b3caee985ac6f0e4e53820feffe0092
tomasz75 <tomek@sviete.pl>
2021-05-05 15:19:04 +0200

    2021.5.0b8

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 2021
 MINOR_VERSION = 5
-PATCH_VERSION = "0b7"
+PATCH_VERSION = "0b8"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
 REQUIRED_PYTHON_VER = (3, 8, 0)
