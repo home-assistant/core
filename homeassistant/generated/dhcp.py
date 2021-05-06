c3488d748f7f9d9f8a656f25a9ff24796448b8fe
J. Nick Koston <nick@koston.org>
2021-05-06 13:27:25 -1000

    Find tv by dhcp

--- homeassistant/generated/dhcp.py
+++ homeassistant/generated/dhcp.py
@@ -150,6 +150,10 @@ DHCP = [
         "macaddress": "80A589*"
     },
     {
+        "domain": "samsungtv",
+        "hostname": "tizen*"
+    },
+    {
         "domain": "screenlogic",
         "hostname": "pentair: *",
         "macaddress": "00C033*"
