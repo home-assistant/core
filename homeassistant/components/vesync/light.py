a8bf138f15172b9fc0a031471f1e7be1e8f78077
sdrapha <rapha.a.r@gmail.com>
2021-05-07 21:16:03 +0000

    Add Etekcity VeSync light bulbs to Homeassistant

--- homeassistant/components/vesync/light.py
+++ homeassistant/components/vesync/light.py
@@ -15,8 +15,10 @@ from .const import DOMAIN, VS_DISCOVERY, VS_DISPATCHERS, VS_LIGHTS
 _LOGGER = logging.getLogger(__name__)
 
 DEV_TYPE_TO_HA = {
-    "ESD16": "light",
-    "ESWD16": "light",
+    "ESD16": "dimmerswitch",
+    "ESWD16": "dimmerswitch",
+    "ESL100": "bulb",
+    "ESL100CW": "bulb",
 }
 
 
@@ -33,6 +35,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     hass.data[DOMAIN][VS_DISPATCHERS].append(disp)
 
     _async_setup_entities(hass.data[DOMAIN][VS_LIGHTS], async_add_entities)
+    return True
 
 
 @callback
@@ -40,10 +43,12 @@ def _async_setup_entities(devices, async_add_entities):
     """Check if device is online and add entity."""
     entities = []
     for dev in devices:
-        if DEV_TYPE_TO_HA.get(dev.device_type) == "light":
+        if DEV_TYPE_TO_HA.get(dev.device_type) == "dimmerswitch":
             entities.append(VeSyncDimmerHA(dev))
+        elif DEV_TYPE_TO_HA.get(dev.device_type) == "bulb":
+            entities.append(VeSyncBulbHA(dev))
         else:
-            _LOGGER.debug(
+            _LOGGER.warning(
                 "%s - Unknown device type - %s", dev.device_name, dev.device_type
             )
             continue
@@ -82,3 +87,36 @@ class VeSyncDimmerHA(VeSyncDevice, LightEntity):
     def brightness(self):
         """Get dimmer brightness."""
         return round((int(self.dimmer.brightness) / 100) * 255)
+
+
+class VeSyncBulbHA(VeSyncDevice, LightEntity):
+    """Representation of a VeSync bulb."""
+
+    def __init__(self, bulb):
+        """Initialize the VeSync bulb device."""
+        super().__init__(bulb)
+        self.bulb = bulb
+
+    def turn_on(self, **kwargs):
+        """Turn the device on."""
+        if ATTR_BRIGHTNESS in kwargs:
+            # get brightness from HA data
+            brightness = int(kwargs[ATTR_BRIGHTNESS])
+            # convert to percent that vesync api expects
+            brightness = round((brightness / 255) * 100)
+            # clamp to 1-100
+            brightness = max(1, min(brightness, 100))
+            self.bulb.set_brightness(brightness)
+        # Avoid turning device back on if this is just a brightness adjustment
+        if not self.is_on:
+            self.device.turn_on()
+
+    @property
+    def supported_features(self):
+        """Get supported features for this entity."""
+        return SUPPORT_BRIGHTNESS
+
+    @property
+    def brightness(self):
+        """Get bulb brightness."""
+        return round((int(self.bulb.brightness) / 100) * 255)
