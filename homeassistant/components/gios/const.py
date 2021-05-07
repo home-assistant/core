7a87846146a1f1b4e392fd78671b16ef50a2eee6
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-05-07 13:03:11 +0200

    Bump `gios` library (#50145)
    * Bump gios library
    
    * Use consts for API strings
    
    * Do not store data locally
    
    * Use API_TIMEOUT const

--- homeassistant/components/gios/const.py
+++ homeassistant/components/gios/const.py
@@ -1,6 +1,17 @@
 """Constants for GIOS integration."""
 from datetime import timedelta
 
+from homeassistant.components.air_quality import (
+    ATTR_CO,
+    ATTR_NO2,
+    ATTR_OZONE,
+    ATTR_PM_2_5,
+    ATTR_PM_10,
+    ATTR_SO2,
+)
+
+ATTRIBUTION = "Data provided by GIOŚ"
+
 ATTR_STATION = "station"
 CONF_STATION_ID = "station_id"
 DEFAULT_NAME = "GIOŚ"
@@ -8,6 +19,16 @@ DEFAULT_NAME = "GIOŚ"
 SCAN_INTERVAL = timedelta(minutes=30)
 DOMAIN = "gios"
 MANUFACTURER = "Główny Inspektorat Ochrony Środowiska"
+
+API_AQI = "aqi"
+API_CO = "co"
+API_NO2 = "no2"
+API_O3 = "o3"
+API_PM10 = "pm10"
+API_PM25 = "pm2.5"
+API_SO2 = "so2"
+
+API_TIMEOUT = 30
 
 AQI_GOOD = "dobry"
 AQI_MODERATE = "umiarkowany"
@@ -21,4 +42,13 @@ ICONS_MAP = {
     AQI_MODERATE: "mdi:emoticon-neutral",
     AQI_POOR: "mdi:emoticon-sad",
     AQI_VERY_POOR: "mdi:emoticon-dead",
+}
+
+SENSOR_MAP = {
+    API_CO: ATTR_CO,
+    API_NO2: ATTR_NO2,
+    API_O3: ATTR_OZONE,
+    API_PM10: ATTR_PM_10,
+    API_PM25: ATTR_PM_2_5,
+    API_SO2: ATTR_SO2,
 }
