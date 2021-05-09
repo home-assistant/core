bfb7d0aaaae122b0ecd2f195afc14af6c11edb12
Simone Chemelli <simone.chemelli@gmail.com>
2021-05-09 22:32:31 +0200

    Update homeassistant/components/fritz/sensor.py
    Co-authored-by: Maciej Bieniek <bieniu@users.noreply.github.com>

--- homeassistant/components/fritz/sensor.py
+++ homeassistant/components/fritz/sensor.py
@@ -59,7 +59,7 @@ SENSOR_DATA = {
 
 
 async def async_setup_entry(
-    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: Callable
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up entry."""
     _LOGGER.debug("Setting up FRITZ!Box sensors")
