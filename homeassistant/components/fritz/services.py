2e4470a39c2d06afdaf7cb63566990380cb90694
chemelli74 <simone.chemelli@gmail.com>
2021-05-09 07:10:45 +0000

    Switch to helper function

--- homeassistant/components/fritz/services.py
+++ homeassistant/components/fritz/services.py
@@ -1,16 +1,16 @@
 """Services for Fritz integration."""
 import logging
 
+from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.device_registry import async_get as async_get_dev_reg
-from homeassistant.helpers.entity_registry import async_get as async_get_ent_reg
+from homeassistant.helpers.service import async_extract_config_entry_ids
 
 from .const import DOMAIN, FRITZ_SERVICES, SERVICE_REBOOT, SERVICE_RECONNECT
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_services(hass):
+async def async_setup_services(hass: HomeAssistant):
     """Set up services for Fritz integration."""
     if hass.data.get(FRITZ_SERVICES, False):
         return
@@ -19,44 +19,38 @@ async def async_setup_services(hass):
 
     async def async_call_fritz_service(service_call):
         """Call correct Fritz service."""
-        fritz_tools = await _async_get_configured_fritz_tools(hass, service_call.data)
 
-        _LOGGER.debug("Executing service %s", service_call.service)
-        await fritz_tools.service_fritzbox(service_call.service)
+        if not (
+            fritzbox_entry_ids := await _async_get_configured_fritz_tools(
+                hass, service_call
+            )
+        ):
+            raise HomeAssistantError(
+                f"Failed to call service '{service_call.service}'. Config entry for target not found"
+            )
+
+        for entry in fritzbox_entry_ids:
+            _LOGGER.debug("Executing service %s", service_call.service)
+            fritz_tools = hass.data[DOMAIN].get(entry)
+            await fritz_tools.service_fritzbox(service_call.service)
 
     for service in [SERVICE_REBOOT, SERVICE_RECONNECT]:
         hass.services.async_register(DOMAIN, service, async_call_fritz_service)
 
 
-async def _async_get_configured_fritz_tools(hass, data):
+async def _async_get_configured_fritz_tools(
+    hass: HomeAssistant, service_call: ServiceCall
+):
     """Get FritzBoxTools class from config entry."""
 
-    entity_id = data.get("entity_id")
-    device_id = data.get("device_id")
-
-    if not entity_id and not device_id:
-        raise HomeAssistantError("Missing entity or device")
-
-    if entity_id:
-        try:
-            config_entry = (
-                async_get_ent_reg(hass).async_get(entity_id[0]).config_entry_id
-            )
-        except Exception as ex:  # pylint: disable=broad-except
-            raise HomeAssistantError("Specified FRITZ entity not found") from ex
-
-    if device_id:
-        try:
-            config_entry = list(
-                async_get_dev_reg(hass).async_get(device_id[0]).config_entries
-            )[0]
-        except Exception as ex:  # pylint: disable=broad-except
-            raise HomeAssistantError("Specified FRITZ!Box device not found") from ex
-
-    return hass.data[DOMAIN].get(config_entry)
+    return [
+        entry_id
+        for entry_id in await async_extract_config_entry_ids(hass, service_call)
+        if hass.config_entries.async_get_entry(entry_id).domain == DOMAIN
+    ]
 
 
-async def async_unload_services(hass):
+async def async_unload_services(hass: HomeAssistant):
     """Unload services for Fritz integration."""
 
     if not hass.data.get(FRITZ_SERVICES):
