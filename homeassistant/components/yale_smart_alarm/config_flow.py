d160b89eae0230cfe13bc8eaa9f151c2887b38bd
G Johansson <goran.johansson@shiftit.se>
2021-05-09 21:37:54 +0200

    Fix config_flow

--- homeassistant/components/yale_smart_alarm/config_flow.py
+++ homeassistant/components/yale_smart_alarm/config_flow.py
@@ -42,7 +42,7 @@ class YaleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Yale integration."""
 
     VERSION = 1
-    CONNif entry.data["username"] == username:SS_CLOUD_POLL
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
