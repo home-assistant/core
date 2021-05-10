2dbcf10a69f0a4622ebe6d069e509d191b0c7840
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-05-10 23:25:16 +0200

    Fix unique_id types

--- homeassistant/components/gios/__init__.py
+++ homeassistant/components/gios/__init__.py
@@ -12,6 +12,7 @@ from gios import ApiError, Gios, InvalidSensorsData, NoStationError
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.device_registry import async_get_registry
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import API_TIMEOUT, CONF_STATION_ID, DOMAIN, SCAN_INTERVAL
@@ -25,6 +26,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up GIOS as config entry."""
     station_id = entry.data[CONF_STATION_ID]
     _LOGGER.debug("Using station_id: %s", station_id)
+
+    if isinstance(entry.unique_id, int):  # type: ignore[unreachable]
+        hass.config_entries.async_update_entry(entry, unique_id=str(station_id))  # type: ignore[unreachable]
+
+    device_registry = await async_get_registry(hass)
+    old_ids = (DOMAIN, station_id)
+    device_entry = device_registry.async_get_device({old_ids})
+    if device_entry and entry.entry_id in device_entry.config_entries:
+        new_ids = (DOMAIN, str(station_id))
+        device_registry.async_update_device(device_entry.id, new_identifiers={new_ids})
 
     websession = async_get_clientsession(hass)
 
--- homeassistant/components/gios/air_quality.py
+++ homeassistant/components/gios/air_quality.py
@@ -1,12 +1,15 @@
 """Support for the GIOS service."""
 from __future__ import annotations
 
+from typing import Any, cast
+
 from homeassistant.components.air_quality import AirQualityEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity_registry import async_get_registry
 from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
@@ -39,17 +42,28 @@ async def async_setup_entry(
 
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
+    entity_registry = await async_get_registry(hass)
+    old_entity_id = entity_registry.async_get_entity_id(
+        "air_quality", DOMAIN, coordinator.gios.station_id
+    )
+    if old_entity_id is not None:
+        entity_registry.async_update_entity(
+            old_entity_id, new_unique_id=str(coordinator.gios.station_id)
+        )
+
     async_add_entities([GiosAirQuality(coordinator, name)], False)
 
 
 class GiosAirQuality(CoordinatorEntity, AirQualityEntity):
     """Define an GIOS sensor."""
 
-    def __init__(self, coordinator: GiosDataUpdateCoordinator, name: str) -> str:
+    coordinator: GiosDataUpdateCoordinator
+
+    def __init__(self, coordinator: GiosDataUpdateCoordinator, name: str) -> None:
         """Initialize."""
         super().__init__(coordinator)
         self._name = name
-        self._attrs = {}
+        self._attrs: dict[str, Any] = {}
 
     @property
     def name(self) -> str:
@@ -60,41 +74,41 @@ class GiosAirQuality(CoordinatorEntity, AirQualityEntity):
     def icon(self) -> str | None:
         """Return the icon."""
         if self.air_quality_index in ICONS_MAP:
-            return ICONS_MAP[self.air_quality_index]
+            return ICONS_MAP[cast(str, self.air_quality_index)]
         return "mdi:blur"
 
     @property
-    def air_quality_index(self) -> float:
+    def air_quality_index(self) -> StateType:
         """Return the air quality index."""
         return self._get_sensor_value(API_AQI)
 
     @property
-    def particulate_matter_2_5(self) -> float:
+    def particulate_matter_2_5(self) -> StateType:
         """Return the particulate matter 2.5 level."""
         return round_state(self._get_sensor_value(API_PM25))
 
     @property
-    def particulate_matter_10(self) -> float:
+    def particulate_matter_10(self) -> StateType:
         """Return the particulate matter 10 level."""
         return round_state(self._get_sensor_value(API_PM10))
 
     @property
-    def ozone(self) -> float:
+    def ozone(self) -> StateType:
         """Return the O3 (ozone) level."""
         return round_state(self._get_sensor_value(API_O3))
 
     @property
-    def carbon_monoxide(self) -> float:
+    def carbon_monoxide(self) -> StateType:
         """Return the CO (carbon monoxide) level."""
         return round_state(self._get_sensor_value(API_CO))
 
     @property
-    def sulphur_dioxide(self) -> float:
+    def sulphur_dioxide(self) -> StateType:
         """Return the SO2 (sulphur dioxide) level."""
         return round_state(self._get_sensor_value(API_SO2))
 
     @property
-    def nitrogen_dioxide(self) -> float:
+    def nitrogen_dioxide(self) -> StateType:
         """Return the NO2 (nitrogen dioxide) level."""
         return round_state(self._get_sensor_value(API_NO2))
 
@@ -106,13 +120,13 @@ class GiosAirQuality(CoordinatorEntity, AirQualityEntity):
     @property
     def unique_id(self) -> str:
         """Return a unique_id for this entity."""
-        return self.coordinator.gios.station_id
+        return str(self.coordinator.gios.station_id)
 
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device info."""
         return {
-            "identifiers": {(DOMAIN, self.coordinator.gios.station_id)},
+            "identifiers": {(DOMAIN, str(self.coordinator.gios.station_id))},
             "name": DEFAULT_NAME,
             "manufacturer": MANUFACTURER,
             "entry_type": "service",
@@ -138,7 +152,7 @@ class GiosAirQuality(CoordinatorEntity, AirQualityEntity):
         return None
 
 
-def round_state(state: float | None) -> float | None:
+def round_state(state: StateType) -> StateType:
     """Round state."""
     if isinstance(state, float):
         return round(state)
