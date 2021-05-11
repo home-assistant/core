5eb6ab54792720e7c0621a20befbc42d2b5451b1
J. Nick Koston <nick@koston.org>
2021-05-10 16:21:21 -1000

    Preserve default -F

--- homeassistant/components/nmap_tracker/const.py
+++ homeassistant/components/nmap_tracker/const.py
@@ -9,6 +9,6 @@ NMAP_TRACKED_DEVICES = "nmap_tracked_devices"
 # Interval in minutes to exclude devices from a scan while they are home
 CONF_HOME_INTERVAL = "home_interval"
 CONF_OPTIONS = "scan_options"
-DEFAULT_OPTIONS = "-sn --host-timeout 5s"
+DEFAULT_OPTIONS = "-F --host-timeout 5s"
 
 TRACKER_SCAN_INTERVAL = 120
