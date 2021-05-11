6c3af6f319df9eff0cbb37b5c9b7f7b78acbc5b4
Jason Lawrence <jjlawren@users.noreply.github.com>
2021-05-11 16:10:07 -0500

    Improve radio detection and handling on Sonos
    Use better URI for source detection
    Ignore playing state (addressed by #32712)
    Set artist from metadata if missing

--- homeassistant/components/sonos/speaker.py
+++ homeassistant/components/sonos/speaker.py
@@ -725,7 +725,7 @@ class SonosSpeaker:
 
         if variables and "transport_state" in variables:
             self.media.play_mode = variables["current_play_mode"]
-            track_uri = variables["current_track_uri"]
+            track_uri = variables["enqueued_transport_uri"]
             music_source = self.soco.music_source_from_uri(track_uri)
         else:
             self.media.play_mode = self.soco.play_mode
@@ -777,14 +777,18 @@ class SonosSpeaker:
         except (TypeError, KeyError, AttributeError):
             pass
 
-        # Non-playing radios will not have a current title. Radios without tagging
-        # can have part of the radio URI as title. In these cases we try to use the
-        # radio name instead.
+        if not self.media.artist:
+            try:
+                self.media.artist = variables["current_track_meta_data"].creator
+            except (KeyError, AttributeError):
+                pass
+
+        # Radios without tagging can have part of the radio URI as title.
+        # In this case we try to use the radio name instead.
         try:
             uri_meta_data = variables["enqueued_transport_uri_meta_data"]
             if isinstance(uri_meta_data, DidlAudioBroadcast) and (
-                self.media.playback_status != SONOS_STATE_PLAYING
-                or self.soco.music_source_from_uri(self.media.title) == MUSIC_SRC_RADIO
+                self.soco.music_source_from_uri(self.media.title) == MUSIC_SRC_RADIO
                 or (
                     isinstance(self.media.title, str)
                     and isinstance(self.media.uri, str)
