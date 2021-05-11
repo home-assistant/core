72d90c52d41d088af483121ef15c1f985e5e2b57
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-05-11 16:57:24 +0200

    Improve type annotations for GIOS integration (#50454)

--- homeassistant/components/gios/config_flow.py
+++ homeassistant/components/gios/config_flow.py
@@ -1,5 +1,8 @@
 """Adds config flow for GIOS."""
+from __future__ import annotations
+
 import asyncio
+from typing import Any
 
 from aiohttp.client_exceptions import ClientConnectorError
 from async_timeout import timeout
@@ -8,6 +11,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
+from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import API_TIMEOUT, CONF_STATION_ID, DEFAULT_NAME, DOMAIN
@@ -25,14 +29,16 @@ class GiosFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
         """Handle a flow initialized by the user."""
         errors = {}
 
         if user_input is not None:
             try:
                 await self.async_set_unique_id(
-                    user_input[CONF_STATION_ID], raise_on_progress=False
+                    str(user_input[CONF_STATION_ID]), raise_on_progress=False
                 )
                 self._abort_if_unique_id_configured()
 
--- homeassistant/components/gios/const.py
+++ homeassistant/components/gios/const.py
@@ -1,5 +1,8 @@
 """Constants for GIOS integration."""
+from __future__ import annotations
+
 from datetime import timedelta
+from typing import Final
 
 from homeassistant.components.air_quality import (
     ATTR_CO,
@@ -10,33 +13,33 @@ from homeassistant.components.air_quality import (
     ATTR_SO2,
 )
 
-ATTRIBUTION = "Data provided by GIOŚ"
+ATTRIBUTION: Final = "Data provided by GIOŚ"
 
-ATTR_STATION = "station"
-CONF_STATION_ID = "station_id"
-DEFAULT_NAME = "GIOŚ"
+ATTR_STATION: Final = "station"
+CONF_STATION_ID: Final = "station_id"
+DEFAULT_NAME: Final = "GIOŚ"
 # Term of service GIOŚ allow downloading data no more than twice an hour.
-SCAN_INTERVAL = timedelta(minutes=30)
-DOMAIN = "gios"
-MANUFACTURER = "Główny Inspektorat Ochrony Środowiska"
+SCAN_INTERVAL: Final = timedelta(minutes=30)
+DOMAIN: Final = "gios"
+MANUFACTURER: Final = "Główny Inspektorat Ochrony Środowiska"
 
-API_AQI = "aqi"
-API_CO = "co"
-API_NO2 = "no2"
-API_O3 = "o3"
-API_PM10 = "pm10"
-API_PM25 = "pm2.5"
-API_SO2 = "so2"
+API_AQI: Final = "aqi"
+API_CO: Final = "co"
+API_NO2: Final = "no2"
+API_O3: Final = "o3"
+API_PM10: Final = "pm10"
+API_PM25: Final = "pm2.5"
+API_SO2: Final = "so2"
 
-API_TIMEOUT = 30
+API_TIMEOUT: Final = 30
 
-AQI_GOOD = "dobry"
-AQI_MODERATE = "umiarkowany"
-AQI_POOR = "dostateczny"
-AQI_VERY_GOOD = "bardzo dobry"
-AQI_VERY_POOR = "zły"
+AQI_GOOD: Final = "dobry"
+AQI_MODERATE: Final = "umiarkowany"
+AQI_POOR: Final = "dostateczny"
+AQI_VERY_GOOD: Final = "bardzo dobry"
+AQI_VERY_POOR: Final = "zły"
 
-ICONS_MAP = {
+ICONS_MAP: Final[dict[str, str]] = {
     AQI_VERY_GOOD: "mdi:emoticon-excited",
     AQI_GOOD: "mdi:emoticon-happy",
     AQI_MODERATE: "mdi:emoticon-neutral",
@@ -44,7 +47,7 @@ ICONS_MAP = {
     AQI_VERY_POOR: "mdi:emoticon-dead",
 }
 
-SENSOR_MAP = {
+SENSOR_MAP: Final[dict[str, str]] = {
     API_CO: ATTR_CO,
     API_NO2: ATTR_NO2,
     API_O3: ATTR_OZONE,
