cef8ef6dc586992aa31a13d59327d7022c28f54c
Rudolf Offereins <r.offereins@gmail.com>
2021-05-12 12:05:38 +0000

    Refactor after userful PR review

--- homeassistant/components/geocaching/const.py
+++ homeassistant/components/geocaching/const.py
@@ -1,5 +1,11 @@
 """Constants for the Geocaching integration."""
+from __future__ import annotations
+
 from datetime import timedelta
+from typing import TYPE_CHECKING, Final
+
+if TYPE_CHECKING:
+    from .models import GeocachingSensorSettings
 
 DOMAIN = "geocaching"
 
@@ -8,3 +14,60 @@ OAUTH2_TOKEN_URL = "https://oauth-staging.geocaching.com/token"
 API_ENDPOINT_URL = "https://staging.api.groundspeak.com"
 
 UPDATE_INTERVAL = timedelta(hours=1)
+
+SENSOR_DATA: Final[dict[str, GeocachingSensorSettings]] = {
+    "username": {
+        "name": "Username",
+        "section": "user",
+        "state": "username",
+        "unit_of_measurement": None,
+        "device_class": None,
+        "icon": "mdi:account",
+        "default_enabled": False,
+    },
+    "find_count": {
+        "name": "Total finds",
+        "section": "user",
+        "state": "find_count",
+        "unit_of_measurement": "caches",
+        "device_class": None,
+        "icon": "mdi:notebook-edit-outline",
+        "default_enabled": True,
+    },
+    "hide_count": {
+        "name": "Total hides",
+        "section": "user",
+        "state": "hide_count",
+        "unit_of_measurement": "caches",
+        "device_class": None,
+        "icon": "mdi:eye-off-outline",
+        "default_enabled": True,
+    },
+    "favorite_points": {
+        "name": "Favorite points",
+        "section": "user",
+        "state": "favorite_points",
+        "unit_of_measurement": "points",
+        "device_class": None,
+        "icon": "mdi:heart-outline",
+        "default_enabled": True,
+    },
+    "souvenir_count": {
+        "name": "Total souvenirs",
+        "section": "user",
+        "state": "souvenir_count",
+        "unit_of_measurement": "souvenirs",
+        "device_class": None,
+        "icon": "mdi:license",
+        "default_enabled": True,
+    },
+    "awarded_favorite_points": {
+        "name": "Awarded favorite points",
+        "section": "user",
+        "state": "awarded_favorite_points",
+        "unit_of_measurement": "points",
+        "device_class": None,
+        "icon": "mdi:heart",
+        "default_enabled": True,
+    },
+}
--- homeassistant/components/geocaching/sensor.py
+++ homeassistant/components/geocaching/sensor.py
@@ -8,7 +8,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import HomeAssistantType
 
-from .const import DOMAIN
+from .const import DOMAIN, SENSOR_DATA
 from .coordinator import GeocachingDataUpdateCoordinator
 from .models import GeocachingEntity, GeocachingSensorSettings
 
@@ -18,68 +18,11 @@ async def async_setup_entry(
 ) -> None:
     """Set up a Geocaching sensor entry."""
 
-    sensor_data = {
-        "username": GeocachingSensorSettings(
-            name="Username",
-            section="user",
-            state="username",
-            unit_of_measurement=None,
-            device_class=None,
-            icon="mdi:account",
-            default_enabled=True,
-        ),
-        "find_count": GeocachingSensorSettings(
-            name="Total finds",
-            section="user",
-            state="find_count",
-            unit_of_measurement="caches",
-            device_class=None,
-            icon="mdi:notebook-edit-outline",
-            default_enabled=True,
-        ),
-        "hide_count": GeocachingSensorSettings(
-            name="Total hides",
-            section="user",
-            state="hide_count",
-            unit_of_measurement="caches",
-            device_class=None,
-            icon="mdi:eye-off-outline",
-            default_enabled=True,
-        ),
-        "favorite_points": GeocachingSensorSettings(
-            name="Favorite points",
-            section="user",
-            state="favorite_points",
-            unit_of_measurement="points",
-            device_class=None,
-            icon="mdi:heart-outline",
-            default_enabled=True,
-        ),
-        "souvenir_count": GeocachingSensorSettings(
-            name="Total souvenirs",
-            section="user",
-            state="souvenir_count",
-            unit_of_measurement="souvenirs",
-            device_class=None,
-            icon="mdi:license",
-            default_enabled=True,
-        ),
-        "awarded_favorite_points": GeocachingSensorSettings(
-            name="Awarded favorite points",
-            section="user",
-            state="awarded_favorite_points",
-            unit_of_measurement="points",
-            device_class=None,
-            icon="mdi:heart",
-            default_enabled=True,
-        ),
-    }
-
     coordinator = hass.data[DOMAIN][entry.entry_id]["COORD"]
     async_add_entities(
         [
-            GeocachingSensor(coordinator, key=key, settings=sensor_data[key])
-            for key in sensor_data
+            GeocachingSensor(coordinator, key=key, settings=SENSOR_DATA[key])
+            for key in SENSOR_DATA
         ]
     )
 
@@ -103,7 +46,7 @@ class GeocachingSensor(GeocachingEntity, SensorEntity):
             coordinator,
             enabled_default=settings["default_enabled"],
             icon=settings["icon"],
-            name=f"Geocaching {coordinator.data.user.reference_code} {settings['name']}",
+            name=f"Geocaching {coordinator.data.user.username} {settings['name']}",
         )
         self.settings = settings
         self.key = key
