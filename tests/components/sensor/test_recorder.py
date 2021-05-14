89a978894dff14c8873417c5edaedaabc0234dbc
Erik <erik@montnemery.com>
2021-05-14 12:21:46 +0200

    Improve tests

--- tests/components/sensor/test_recorder.py
+++ tests/components/sensor/test_recorder.py
@@ -9,6 +9,7 @@ from homeassistant.components.recorder import history
 from homeassistant.components.recorder.const import DATA_INSTANCE
 from homeassistant.components.recorder.models import process_timestamp_to_utc_isoformat
 from homeassistant.components.recorder.statistics import statistics_during_period
+from homeassistant.const import STATE_UNAVAILABLE
 from homeassistant.setup import setup_component
 import homeassistant.util.dt as dt_util
 
@@ -35,7 +36,6 @@ def hass_recorder():
 
 def test_compile_hourly_statistics(hass_recorder):
     """Test compiling hourly statistics."""
-    # TODO: Test what happens if state is unchanged, or if state is unavailable
     hass = hass_recorder()
     recorder = hass.data[DATA_INSTANCE]
     setup_component(hass, "sensor", {})
@@ -60,11 +60,77 @@ def test_compile_hourly_statistics(hass_recorder):
     }
 
 
+def test_compile_hourly_statistics_unchanged(hass_recorder):
+    """Test compiling hourly statistics, with no changes during the hour."""
+    hass = hass_recorder()
+    recorder = hass.data[DATA_INSTANCE]
+    setup_component(hass, "sensor", {})
+    zero, four, states = record_states(hass)
+    hist = history.get_significant_states(hass, zero, four)
+    assert dict(states) == dict(hist)
+
+    recorder.do_adhoc_statistics(period="hourly", start=four)
+    wait_recording_done(hass)
+    stats = statistics_during_period(hass, four)
+    assert stats == {
+        "sensor.test1": [
+            {
+                "statistic_id": "sensor.test1",
+                "period": "hourly",
+                "start": process_timestamp_to_utc_isoformat(four),
+                "mean": 20.0,
+                "min": 20.0,
+                "max": 20.0,
+            }
+        ]
+    }
+
+
+def test_compile_hourly_statistics_partially_unavailable(hass_recorder):
+    """Test compiling hourly statistics, with the sensor being partially unavailable."""
+    hass = hass_recorder()
+    recorder = hass.data[DATA_INSTANCE]
+    setup_component(hass, "sensor", {})
+    zero, four, states = record_states_partially_unavailable(hass)
+    hist = history.get_significant_states(hass, zero, four)
+    assert dict(states) == dict(hist)
+
+    recorder.do_adhoc_statistics(period="hourly", start=zero)
+    wait_recording_done(hass)
+    stats = statistics_during_period(hass, zero)
+    assert stats == {
+        "sensor.test1": [
+            {
+                "statistic_id": "sensor.test1",
+                "period": "hourly",
+                "start": process_timestamp_to_utc_isoformat(zero),
+                "mean": 17.5,
+                "min": 10.0,
+                "max": 25.0,
+            }
+        ]
+    }
+
+
+def test_compile_hourly_statistics_unavailable(hass_recorder):
+    """Test compiling hourly statistics, with the sensor being unavailable."""
+    hass = hass_recorder()
+    recorder = hass.data[DATA_INSTANCE]
+    setup_component(hass, "sensor", {})
+    zero, four, states = record_states_partially_unavailable(hass)
+    hist = history.get_significant_states(hass, zero, four)
+    assert dict(states) == dict(hist)
+
+    recorder.do_adhoc_statistics(period="hourly", start=four)
+    wait_recording_done(hass)
+    stats = statistics_during_period(hass, four)
+    assert stats == {}
+
+
 def record_states(hass):
     """Record some test states.
 
-    We inject a bunch of state updates from media player, zone and
-    thermostat.
+    We inject a bunch of state updates temperature sensors.
     """
     mp = "media_player.test"
     sns1 = "sensor.test1"
@@ -107,5 +173,55 @@ def record_states(hass):
         states[sns1].append(set_state(sns1, "20", attributes=sns1_attr))
         states[sns2].append(set_state(sns2, "20", attributes=sns2_attr))
         states[sns3].append(set_state(sns3, "20", attributes=sns3_attr))
+
+    return zero, four, states
+
+
+def record_states_partially_unavailable(hass):
+    """Record some test states.
+
+    We inject a bunch of state updates temperature sensors.
+    """
+    mp = "media_player.test"
+    sns1 = "sensor.test1"
+    sns2 = "sensor.test2"
+    sns3 = "sensor.test3"
+    sns1_attr = {"device_class": "temperature", "state_class": "measurement"}
+    sns2_attr = {"device_class": "temperature"}
+    sns3_attr = {}
+
+    def set_state(entity_id, state, **kwargs):
+        """Set the state."""
+        hass.states.set(entity_id, state, **kwargs)
+        wait_recording_done(hass)
+        return hass.states.get(entity_id)
+
+    zero = dt_util.utcnow()
+    one = zero + timedelta(minutes=1)
+    two = one + timedelta(minutes=15)
+    three = two + timedelta(minutes=30)
+    four = three + timedelta(minutes=15)
+
+    states = {mp: [], sns1: [], sns2: [], sns3: []}
+    with patch("homeassistant.components.recorder.dt_util.utcnow", return_value=one):
+        states[mp].append(
+            set_state(mp, "idle", attributes={"media_title": str(sentinel.mt1)})
+        )
+        states[mp].append(
+            set_state(mp, "YouTube", attributes={"media_title": str(sentinel.mt2)})
+        )
+        states[sns1].append(set_state(sns1, "10", attributes=sns1_attr))
+        states[sns2].append(set_state(sns2, "10", attributes=sns2_attr))
+        states[sns3].append(set_state(sns3, "10", attributes=sns3_attr))
+
+    with patch("homeassistant.components.recorder.dt_util.utcnow", return_value=two):
+        states[sns1].append(set_state(sns1, "25", attributes=sns1_attr))
+        states[sns2].append(set_state(sns2, "25", attributes=sns2_attr))
+        states[sns3].append(set_state(sns3, "25", attributes=sns3_attr))
+
+    with patch("homeassistant.components.recorder.dt_util.utcnow", return_value=three):
+        states[sns1].append(set_state(sns1, STATE_UNAVAILABLE, attributes=sns1_attr))
+        states[sns2].append(set_state(sns2, STATE_UNAVAILABLE, attributes=sns2_attr))
+        states[sns3].append(set_state(sns3, STATE_UNAVAILABLE, attributes=sns3_attr))
 
     return zero, four, states
