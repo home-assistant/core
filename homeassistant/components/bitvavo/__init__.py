53d434060bf42b842488ddcc3079e159fd1471b6
Ron Klinkien <ron@cyberjunky.nl>
2021-05-14 19:33:51 +0200

    Update homeassistant/components/bitvavo/__init__.py
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/bitvavo/__init__.py
+++ homeassistant/components/bitvavo/__init__.py
@@ -40,9 +40,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
