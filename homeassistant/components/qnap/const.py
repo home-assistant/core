f3f1be73463042dfa847d0e83cee49f643f9d207
cyr-ius <cyr-ius@ipocus.net>
2021-05-15 12:18:33 +0200

    Convert infos list to infos dict

--- homeassistant/components/qnap/const.py
+++ homeassistant/components/qnap/const.py
@@ -1,10 +1,29 @@
 """The Qnap constants."""
 from homeassistant.const import (
+    ATTR_ICON,
+    ATTR_NAME,
+    ATTR_UNIT_OF_MEASUREMENT,
     DATA_GIBIBYTES,
     DATA_RATE_MEBIBYTES_PER_SECOND,
     PERCENTAGE,
     TEMP_CELSIUS,
 )
+
+ATTR_DRIVE = "Drive"
+ATTR_ENABLED = "Sensor Enabled"
+ATTR_IP = "IP Address"
+ATTR_MAC = "MAC Address"
+ATTR_MASK = "Mask"
+ATTR_MAX_SPEED = "Max Speed"
+ATTR_MEMORY_SIZE = "Memory Size"
+ATTR_MODEL = "Model"
+ATTR_PACKETS_TX = "Packets (TX)"
+ATTR_PACKETS_RX = "Packets (RX)"
+ATTR_PACKETS_ERR = "Packets (Err)"
+ATTR_SERIAL = "Serial #"
+ATTR_TYPE = "Type"
+ATTR_UPTIME = "Uptime"
+ATTR_VOLUME_SIZE = "Volume Size"
 
 CONF_DRIVES = "drives"
 CONF_NICS = "nics"
@@ -28,52 +47,122 @@ NOTIFICATION_ID = "qnap_notification"
 NOTIFICATION_TITLE = "QNAP Sensor Setup"
 
 _SYSTEM_MON_COND = {
-    "status": ["Status", None, "mdi:checkbox-marked-circle-outline", True],
-    "system_temp": ["System Temperature", TEMP_CELSIUS, "mdi:thermometer", True],
+    "status": {
+        ATTR_NAME: "Status",
+        ATTR_UNIT_OF_MEASUREMENT: None,
+        ATTR_ICON: "mdi:checkbox-marked-circle-outline",
+        ATTR_ENABLED: True,
+    },
+    "system_temp": {
+        ATTR_NAME: "System Temperature",
+        ATTR_UNIT_OF_MEASUREMENT: TEMP_CELSIUS,
+        ATTR_ICON: "mdi:thermometer",
+        ATTR_ENABLED: True,
+    },
 }
 _CPU_MON_COND = {
-    "cpu_temp": ["CPU Temperature", TEMP_CELSIUS, "mdi:thermometer", False],
-    "cpu_usage": ["CPU Usage", PERCENTAGE, "mdi:chip", True],
+    "cpu_temp": {
+        ATTR_NAME: "CPU Temperature",
+        ATTR_UNIT_OF_MEASUREMENT: TEMP_CELSIUS,
+        ATTR_ICON: "mdi:thermometer",
+        ATTR_ENABLED: False,
+    },
+    "cpu_usage": {
+        ATTR_NAME: "CPU Usage",
+        ATTR_UNIT_OF_MEASUREMENT: PERCENTAGE,
+        ATTR_ICON: "mdi:chip",
+        ATTR_ENABLED: True,
+    },
 }
 _MEMORY_MON_COND = {
-    "memory_free": ["Memory Available", DATA_GIBIBYTES, "mdi:memory", False],
-    "memory_used": ["Memory Used", DATA_GIBIBYTES, "mdi:memory", False],
-    "memory_percent_used": ["Memory Usage", PERCENTAGE, "mdi:memory", True],
+    "memory_free": {
+        ATTR_NAME: "Memory Available",
+        ATTR_UNIT_OF_MEASUREMENT: DATA_GIBIBYTES,
+        ATTR_ICON: "mdi:memory",
+        ATTR_ENABLED: False,
+    },
+    "memory_used": {
+        ATTR_NAME: "Memory Used",
+        ATTR_UNIT_OF_MEASUREMENT: DATA_GIBIBYTES,
+        ATTR_ICON: "mdi:memory",
+        ATTR_ENABLED: False,
+    },
+    "memory_percent_used": {
+        ATTR_NAME: "Memory Usage",
+        ATTR_UNIT_OF_MEASUREMENT: PERCENTAGE,
+        ATTR_ICON: "mdi:memory",
+        ATTR_ENABLED: True,
+    },
 }
 _NETWORK_MON_COND = {
-    "network_link_status": [
-        "Network Link",
-        None,
-        "mdi:checkbox-marked-circle-outline",
-        True,
-    ],
-    "network_tx": ["Network Up", DATA_RATE_MEBIBYTES_PER_SECOND, "mdi:upload", False],
-    "network_rx": [
-        "Network Down",
-        DATA_RATE_MEBIBYTES_PER_SECOND,
-        "mdi:download",
-        False,
-    ],
+    "network_link_status": {
+        ATTR_NAME: "Network Link",
+        ATTR_UNIT_OF_MEASUREMENT: None,
+        ATTR_ICON: "mdi:checkbox-marked-circle-outline",
+        ATTR_ENABLED: True,
+    },
+    "network_tx": {
+        ATTR_NAME: "Network Up",
+        ATTR_UNIT_OF_MEASUREMENT: DATA_RATE_MEBIBYTES_PER_SECOND,
+        ATTR_ICON: "mdi:upload",
+        ATTR_ENABLED: False,
+    },
+    "network_rx": {
+        ATTR_NAME: "Network Down",
+        ATTR_UNIT_OF_MEASUREMENT: DATA_RATE_MEBIBYTES_PER_SECOND,
+        ATTR_ICON: "mdi:download",
+        ATTR_ENABLED: False,
+    },
 }
 _DRIVE_MON_COND = {
-    "drive_smart_status": [
-        "SMART Status",
-        None,
-        "mdi:checkbox-marked-circle-outline",
-        True,
-    ],
-    "drive_temp": ["Temperature", TEMP_CELSIUS, "mdi:thermometer", False],
+    "drive_smart_status": {
+        ATTR_NAME: "SMART Status",
+        ATTR_UNIT_OF_MEASUREMENT: None,
+        ATTR_ICON: "mdi:checkbox-marked-circle-outline",
+        ATTR_ENABLED: True,
+    },
+    "drive_temp": {
+        ATTR_NAME: "Temperature",
+        ATTR_UNIT_OF_MEASUREMENT: TEMP_CELSIUS,
+        ATTR_ICON: "mdi:thermometer",
+        ATTR_ENABLED: False,
+    },
 }
 
 _FOLDER_MON_COND = {
-    "folder_size_used": ["Used Space", DATA_GIBIBYTES, "mdi:chart-pie", False],
-    "folder_percentage_used": ["Folder Used", PERCENTAGE, "mdi:chart-pie", False],
+    "folder_size_used": {
+        ATTR_NAME: "Used Space",
+        ATTR_UNIT_OF_MEASUREMENT: DATA_GIBIBYTES,
+        ATTR_ICON: "mdi:chart-pie",
+        ATTR_ENABLED: False,
+    },
+    "folder_percentage_used": {
+        ATTR_NAME: "Folder Used",
+        ATTR_UNIT_OF_MEASUREMENT: PERCENTAGE,
+        ATTR_ICON: "mdi:chart-pie",
+        ATTR_ENABLED: False,
+    },
 }
 
 _VOLUME_MON_COND = {
-    "volume_size_used": ["Used Space", DATA_GIBIBYTES, "mdi:chart-pie", False],
-    "volume_size_free": ["Free Space", DATA_GIBIBYTES, "mdi:chart-pie", False],
-    "volume_percentage_used": ["Volume Used", PERCENTAGE, "mdi:chart-pie", True],
+    "volume_size_used": {
+        ATTR_NAME: "Used Space",
+        ATTR_UNIT_OF_MEASUREMENT: DATA_GIBIBYTES,
+        ATTR_ICON: "mdi:chart-pie",
+        ATTR_ENABLED: False,
+    },
+    "volume_size_free": {
+        ATTR_NAME: "Free Space",
+        ATTR_UNIT_OF_MEASUREMENT: DATA_GIBIBYTES,
+        ATTR_ICON: "mdi:chart-pie",
+        ATTR_ENABLED: False,
+    },
+    "volume_percentage_used": {
+        ATTR_NAME: "Volume Used",
+        ATTR_UNIT_OF_MEASUREMENT: PERCENTAGE,
+        ATTR_ICON: "mdi:chart-pie",
+        ATTR_ENABLED: True,
+    },
 }
 
 _MONITORED_CONDITIONS = (
