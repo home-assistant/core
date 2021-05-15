b9bb3cf093533e497c19f6f308bce1eec1c81649
Michael <35783820+mib1185@users.noreply.github.com>
2021-05-15 07:54:11 +0200

    Add strict typing to fritzbox (#50486)
    * enable strict typing
    
    * apply suggestions
    
    * set defaults for FritzboxConfigFlow
    
    * improvements and suggestions
    
    * another suggestion
    
    * tweaks
    
    * tweaks

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -69,7 +69,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.fortios.*",
     "homeassistant.components.foscam.*",
     "homeassistant.components.freebox.*",
-    "homeassistant.components.fritzbox.*",
     "homeassistant.components.garmin_connect.*",
     "homeassistant.components.geniushub.*",
     "homeassistant.components.glances.*",
