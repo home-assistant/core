053650944c10a9d1a4ced01f5c6796fd4c91b965
cyr-ius <cyr-ius@ipocus.net>
2021-05-15 14:54:03 +0200

    Remove connection_class

--- homeassistant/components/webostv/config_flow.py
+++ homeassistant/components/webostv/config_flow.py
@@ -43,7 +43,6 @@ class FlowHandler(config_entries.ConfigFlow):
     """WebosTV configuration flow."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
     def __init__(self):
         """Initialize workflow."""
