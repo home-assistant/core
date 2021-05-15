7ac307ca25dc0c457b8de8ce28509992ced66fd4
mib1185 <mail@mib85.de>
2021-05-15 13:00:47 +0000

    add strict type annotations

---
+++ homeassistant/components/actiontec/const.py
@@ -0,0 +1,24 @@
+"""Support for Actiontec MI424WR (Verizon FIOS) routers."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+import re
+from typing import Final
+
+LEASES_REGEX: Final[re.Pattern] = re.compile(
+    r"(?P<ip>([0-9]{1,3}[\.]){3}[0-9]{1,3})"
+    + r"\smac:\s(?P<mac>([0-9a-f]{2}[:-]){5}([0-9a-f]{2}))"
+    + r"\svalid\sfor:\s(?P<timevalid>(-?\d+))"
+    + r"\ssec"
+)
+
+UNKNOWN_DEVICE_NAME: Final = "unknown device name"
+
+
+@dataclass
+class Device:
+    """Actiontec device class."""
+
+    ip: str
+    mac: str
+    timevalid: int
