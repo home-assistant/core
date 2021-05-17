661d6f15d9d4e652bd8b6f2ac46bad5bba915f75
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-05-17 13:12:17 +0100

    Change Modbus error message to bytes (#50725)

--- homeassistant/components/modbus/sensor.py
+++ homeassistant/components/modbus/sensor.py
@@ -142,11 +142,13 @@ async def async_setup_platform(
             _LOGGER.error("Error in sensor %s structure: %s", entry[CONF_NAME], err)
             continue
 
-        if entry[CONF_COUNT] * 2 != size:
+        bytecount = entry[CONF_COUNT] * 2
+        if bytecount != size:
             _LOGGER.error(
-                "Structure size (%d bytes) mismatch registers count (%d words)",
+                "Structure request %d bytes, but %d registers have a size of %d bytes",
                 size,
                 entry[CONF_COUNT],
+                bytecount,
             )
             continue
 
