0b4080ab3476bdafd06e3f8c985e300785c79f79
Kevin Eifinger <k.eifinger@googlemail.com>
2021-05-17 09:12:04 +0200

    Add full typing to kraken (#50718)
    * Add full typing to kraken
    
    * Let device_info return DeviceInfo
    
    * Replace unsub_listeners with entry.async_on_unload
    
    * Raise TypeError on end of _try_get_state
    
    * Assert Coordinator is not none
    
    * Add class SensorType
    
    * Add strict typing to kraken
    
    * Add changes from code review
    
    * Revert typed dict creation

--- homeassistant/components/kraken/sensor.py
+++ homeassistant/components/kraken/sensor.py
@@ -8,6 +8,9 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers import device_registry
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from . import KrakenData
@@ -16,12 +19,17 @@ from .const import (
     DISPATCH_CONFIG_UPDATED,
     DOMAIN,
     SENSOR_TYPES,
+    SensorType,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
     """Add kraken entities from a config_entry."""
 
     @callback
@@ -59,7 +67,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     async_update_sensors(hass, config_entry)
 
-    hass.data[DOMAIN].unsub_listeners.append(
+    config_entry.async_on_unload(
         async_dispatcher_connect(
             hass,
             DISPATCH_CONFIG_UPDATED,
@@ -75,9 +83,10 @@ class KrakenSensor(CoordinatorEntity, SensorEntity):
         self,
         kraken_data: KrakenData,
         tracked_asset_pair: str,
-        sensor_type: dict[str, bool],
+        sensor_type: SensorType,
     ) -> None:
         """Initialize."""
+        assert kraken_data.coordinator is not None
         super().__init__(kraken_data.coordinator)
         self.tracked_asset_pair_wsname = kraken_data.tradable_asset_pairs[
             tracked_asset_pair
@@ -100,22 +109,22 @@ class KrakenSensor(CoordinatorEntity, SensorEntity):
         self._state = None
 
     @property
-    def entity_registry_enabled_default(self):
+    def entity_registry_enabled_default(self) -> bool:
         """Return if the entity should be enabled when first added to the entity registry."""
         return self._enabled_by_default
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name."""
         return self._name
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         """Set unique_id for sensor."""
         return self._name.lower()
 
     @property
-    def state(self):
+    def state(self) -> StateType:
         """Return the state."""
         return self._state
 
@@ -124,13 +133,76 @@ class KrakenSensor(CoordinatorEntity, SensorEntity):
         await super().async_added_to_hass()
         self._update_internal_state()
 
-    def _handle_coordinator_update(self):
+    def _handle_coordinator_update(self) -> None:
         self._update_internal_state()
         super()._handle_coordinator_update()
 
-    def _update_internal_state(self):
+    def _update_internal_state(self) -> None:
         try:
-            self._state = self._try_get_state()
+            if self._sensor_type == "last_trade_closed":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "last_trade_closed"
+                ][0]
+            if self._sensor_type == "ask":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "ask"
+                ][0]
+            if self._sensor_type == "ask_volume":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "ask"
+                ][1]
+            if self._sensor_type == "bid":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "bid"
+                ][0]
+            if self._sensor_type == "bid_volume":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "bid"
+                ][1]
+            if self._sensor_type == "volume_today":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "volume"
+                ][0]
+            if self._sensor_type == "volume_last_24h":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "volume"
+                ][1]
+            if self._sensor_type == "volume_weighted_average_today":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "volume_weighted_average"
+                ][0]
+            if self._sensor_type == "volume_weighted_average_last_24h":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "volume_weighted_average"
+                ][1]
+            if self._sensor_type == "number_of_trades_today":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "number_of_trades"
+                ][0]
+            if self._sensor_type == "number_of_trades_last_24h":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "number_of_trades"
+                ][1]
+            if self._sensor_type == "low_today":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "low"
+                ][0]
+            if self._sensor_type == "low_last_24h":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "low"
+                ][1]
+            if self._sensor_type == "high_today":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "high"
+                ][0]
+            if self._sensor_type == "high_last_24h":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "high"
+                ][1]
+            if self._sensor_type == "opening_price_today":
+                self._state = self.coordinator.data[self.tracked_asset_pair_wsname][
+                    "opening_price"
+                ]
             self._received_data_at_least_once = True  # Received data at least one time.
         except TypeError:
             if self._received_data_at_least_once:
@@ -141,55 +213,8 @@ class KrakenSensor(CoordinatorEntity, SensorEntity):
                     )
                     self._available = False
 
-    def _try_get_state(self) -> str:
-        """Try to get the state or return a TypeError."""
-        if self._sensor_type == "last_trade_closed":
-            return self.coordinator.data[self.tracked_asset_pair_wsname][
-                "last_trade_closed"
-            ][0]
-        if self._sensor_type == "ask":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["ask"][0]
-        if self._sensor_type == "ask_volume":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["ask"][1]
-        if self._sensor_type == "bid":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["bid"][0]
-        if self._sensor_type == "bid_volume":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["bid"][1]
-        if self._sensor_type == "volume_today":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["volume"][0]
-        if self._sensor_type == "volume_last_24h":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["volume"][1]
-        if self._sensor_type == "volume_weighted_average_today":
-            return self.coordinator.data[self.tracked_asset_pair_wsname][
-                "volume_weighted_average"
-            ][0]
-        if self._sensor_type == "volume_weighted_average_last_24h":
-            return self.coordinator.data[self.tracked_asset_pair_wsname][
-                "volume_weighted_average"
-            ][1]
-        if self._sensor_type == "number_of_trades_today":
-            return self.coordinator.data[self.tracked_asset_pair_wsname][
-                "number_of_trades"
-            ][0]
-        if self._sensor_type == "number_of_trades_last_24h":
-            return self.coordinator.data[self.tracked_asset_pair_wsname][
-                "number_of_trades"
-            ][1]
-        if self._sensor_type == "low_today":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["low"][0]
-        if self._sensor_type == "low_last_24h":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["low"][1]
-        if self._sensor_type == "high_today":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["high"][0]
-        if self._sensor_type == "high_last_24h":
-            return self.coordinator.data[self.tracked_asset_pair_wsname]["high"][1]
-        if self._sensor_type == "opening_price_today":
-            return self.coordinator.data[self.tracked_asset_pair_wsname][
-                "opening_price"
-            ]
-
     @property
-    def icon(self):
+    def icon(self) -> str:
         """Return the icon."""
         if self._target_asset == "EUR":
             return "mdi:currency-eur"
@@ -204,19 +229,19 @@ class KrakenSensor(CoordinatorEntity, SensorEntity):
         return "mdi:cash"
 
     @property
-    def unit_of_measurement(self):
+    def unit_of_measurement(self) -> str | None:
         """Return the unit the value is expressed in."""
         if "number_of" not in self._sensor_type:
             return self._unit_of_measurement
         return None
 
     @property
-    def available(self):
+    def available(self) -> bool:
         """Could the api be accessed during the last update call."""
         return self._available and self.coordinator.last_update_success
 
     @property
-    def device_info(self) -> dict:
+    def device_info(self) -> DeviceInfo:
         """Return a device description for device registry."""
 
         return {
