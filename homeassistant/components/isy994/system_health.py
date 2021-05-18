0d5292860872e15fa667c9b6e1b5aed3e716b081
shbatm <tim.j.bond@gmail.com>
2021-05-18 21:47:00 +0000

    Add tests to system_health.py

--- homeassistant/components/isy994/system_health.py
+++ homeassistant/components/isy994/system_health.py
@@ -5,7 +5,7 @@ from homeassistant.components import system_health
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant, callback
 
-from .const import DOMAIN, ISY994_ISY
+from .const import DOMAIN, ISY994_ISY, ISY_URL_POSTFIX
 
 
 @callback
@@ -27,7 +27,7 @@ async def system_health_info(hass):
 
     entry = hass.config_entries.async_get_entry(config_entry_id)
     health_info["host_reachable"] = await system_health.async_check_can_reach_url(
-        hass, f"{entry.data[CONF_HOST]}/desc"
+        hass, f"{entry.data[CONF_HOST]}{ISY_URL_POSTFIX}"
     )
     health_info["device_connected"] = isy.connected
     health_info["last_heartbeat"] = isy.websocket.last_heartbeat
