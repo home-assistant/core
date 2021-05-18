7681e7ed0490e99584f44239fd527d9adff9a747
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-05-18 23:11:27 +0200

    Add dymmy device tracker and TractiveClient

--- homeassistant/components/tractive/__init__.py
+++ homeassistant/components/tractive/__init__.py
@@ -1,22 +1,38 @@
 """The tractive integration."""
 from __future__ import annotations
 
+import aiotractive
+
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
-PLATFORMS = ["light"]
+PLATFORMS = ["device_tracker"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up tractive from a config entry."""
-    # TODO Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
+    data = entry.data
+
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+
+    client = aiotractive.Tractive(data[CONF_USERNAME], data[CONF_PASSWORD])
+
+    tractive = TractiveClient(hass, client)
+    tractive.subscribe()
+    hass.data[DOMAIN][entry.entry_id] = tractive
 
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+
+    async def cancel_listen_task(_):
+        await tractive.unsubscribe()
+
+    entry.async_on_unload(
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, cancel_listen_task)
+    )
 
     return True
 
@@ -25,6 +41,23 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
+        tractive = hass.data[DOMAIN].pop(entry.entry_id)
+        await tractive.unsubscribe()
     return unload_ok
+
+
+class TractiveClient:
+    """A Tractive client."""
+
+    def __init__(self, hass, client):
+        """Initialize the client."""
+        self._hass = hass
+        self._client = client
+
+    def subscribe(self):
+        """Start event listener coroutine."""
+        pass
+
+    async def unsubscribe(self):
+        """Stop event listener coroutine."""
+        await self._client.close()
---
+++ homeassistant/components/tractive/device_tracker.py
@@ -0,0 +1,6 @@
+"""Support for Tractive device trackers."""
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up Tractive device trackers."""
+    async_add_entities([])
