9247f2000edf6aeb3791787dc9a72f3c6ea4360e
René Klomp <rene@klomp.ws>
2021-05-18 10:57:58 +0200

    Updated tests

--- tests/components/sma/conftest.py
+++ tests/components/sma/conftest.py
@@ -1,6 +1,8 @@
 """Fixtures for sma tests."""
 from unittest.mock import patch
 
+from pysma import Sensors
+from pysma.const import DEVCLASS_INVERTER, SENSOR_MAP
 import pytest
 
 from homeassistant import config_entries
@@ -16,6 +18,7 @@ def mock_config_entry():
     """Return the default mocked config entry."""
     return MockConfigEntry(
         domain=DOMAIN,
+        version=2,
         title=MOCK_DEVICE["name"],
         unique_id=MOCK_DEVICE["serial"],
         data=MOCK_CUSTOM_SETUP_DATA,
@@ -28,7 +31,9 @@ async def init_integration(hass, mock_config_entry):
     """Create a fake SMA Config Entry."""
     mock_config_entry.add_to_hass(hass)
 
-    with patch("pysma.SMA.read"):
+    with patch("pysma.SMA.read"), patch(
+        "pysma.SMA.get_sensors", return_value=Sensors(SENSOR_MAP[DEVCLASS_INVERTER])
+    ):
         await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
     return mock_config_entry
