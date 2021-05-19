4f8cefea868c74414b2fcdff70979757b723fbf4
J. Nick Koston <nick@koston.org>
2021-05-19 08:58:32 -1000

    merge

--- homeassistant/components/network/const.py
+++ homeassistant/components/network/const.py
@@ -1,10 +1,6 @@
 """Constants for the network integration."""
 from __future__ import annotations
 
-import voluptuous as vol
-
-import homeassistant.helpers.config_validation as cv
-
 DOMAIN = "network"
 STORAGE_KEY = "core.network"
 STORAGE_VERSION = 1
@@ -12,11 +8,3 @@ STORAGE_VERSION = 1
 ATTR_ADAPTERS = "adapters"
 ATTR_CONFIGURED_ADAPTERS = "configured_adapters"
 DEFAULT_CONFIGURED_ADAPTERS: list[str] = []
-
-INTERFACES_SCHEMA = vol.Schema(
-    {
-        vol.Optional(
-            ATTR_CONFIGURED_ADAPTERS, default=DEFAULT_CONFIGURED_ADAPTERS
-        ): vol.Schema(vol.All(cv.ensure_list, [cv.string])),
-    }
-)
