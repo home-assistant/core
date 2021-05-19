c4f26ba90a83040e56a31333bf1560421e46f120
J. Nick Koston <nick@koston.org>
2021-05-19 12:35:33 -1000

    tweaks

--- homeassistant/components/network/const.py
+++ homeassistant/components/network/const.py
@@ -1,17 +1,22 @@
 """Constants for the network integration."""
 from __future__ import annotations
 
+from typing import Final
+
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 
-DOMAIN = "network"
-STORAGE_KEY = "core.network"
-STORAGE_VERSION = 1
+DOMAIN: Final = "network"
+STORAGE_KEY: Final = "core.network"
+STORAGE_VERSION: Final = 1
 
-ATTR_ADAPTERS = "adapters"
-ATTR_CONFIGURED_ADAPTERS = "configured_adapters"
+ATTR_ADAPTERS: Final = "adapters"
+ATTR_CONFIGURED_ADAPTERS: Final = "configured_adapters"
 DEFAULT_CONFIGURED_ADAPTERS: list[str] = []
+
+MDNS_TARGET_IP: Final = "224.0.0.251"
+
 
 NETWORK_CONFIG_SCHEMA = vol.Schema(
     {
