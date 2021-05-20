99750afe3a942cebf443df8c106c2152906772b9
Matthias Alphart <farmio@alphart.net>
2021-05-21 01:30:37 +0200

    Create KNX weather entity directly from config (#49640)
    * create climate entities directly from config
    
    * deprecate create_sensors
    
    * move create staticmethod to module level
    
    * add comment for deprecation date

--- homeassistant/components/knx/factory.py
+++ homeassistant/components/knx/factory.py
@@ -2,17 +2,13 @@
 from __future__ import annotations
 
 from xknx import XKNX
-from xknx.devices import (
-    Device as XknxDevice,
-    Sensor as XknxSensor,
-    Weather as XknxWeather,
-)
+from xknx.devices import Device as XknxDevice, Sensor as XknxSensor
 
 from homeassistant.const import CONF_NAME, CONF_TYPE
 from homeassistant.helpers.typing import ConfigType
 
 from .const import SupportedPlatforms
-from .schema import SensorSchema, WeatherSchema
+from .schema import SensorSchema
 
 
 def create_knx_device(
@@ -23,9 +19,6 @@ def create_knx_device(
     """Return the requested XKNX device."""
     if platform is SupportedPlatforms.SENSOR:
         return _create_sensor(knx_module, config)
-
-    if platform is SupportedPlatforms.WEATHER:
-        return _create_weather(knx_module, config)
 
     return None
 
@@ -39,41 +32,4 @@ def _create_sensor(knx_module: XKNX, config: ConfigType) -> XknxSensor:
         sync_state=config[SensorSchema.CONF_SYNC_STATE],
         always_callback=config[SensorSchema.CONF_ALWAYS_CALLBACK],
         value_type=config[CONF_TYPE],
-    )
-
-
-def _create_weather(knx_module: XKNX, config: ConfigType) -> XknxWeather:
-    """Return a KNX weather device to be used within XKNX."""
-    return XknxWeather(
-        knx_module,
-        name=config[CONF_NAME],
-        sync_state=config[WeatherSchema.CONF_SYNC_STATE],
-        create_sensors=config[WeatherSchema.CONF_KNX_CREATE_SENSORS],
-        group_address_temperature=config[WeatherSchema.CONF_KNX_TEMPERATURE_ADDRESS],
-        group_address_brightness_south=config.get(
-            WeatherSchema.CONF_KNX_BRIGHTNESS_SOUTH_ADDRESS
-        ),
-        group_address_brightness_east=config.get(
-            WeatherSchema.CONF_KNX_BRIGHTNESS_EAST_ADDRESS
-        ),
-        group_address_brightness_west=config.get(
-            WeatherSchema.CONF_KNX_BRIGHTNESS_WEST_ADDRESS
-        ),
-        group_address_brightness_north=config.get(
-            WeatherSchema.CONF_KNX_BRIGHTNESS_NORTH_ADDRESS
-        ),
-        group_address_wind_speed=config.get(WeatherSchema.CONF_KNX_WIND_SPEED_ADDRESS),
-        group_address_wind_bearing=config.get(
-            WeatherSchema.CONF_KNX_WIND_BEARING_ADDRESS
-        ),
-        group_address_rain_alarm=config.get(WeatherSchema.CONF_KNX_RAIN_ALARM_ADDRESS),
-        group_address_frost_alarm=config.get(
-            WeatherSchema.CONF_KNX_FROST_ALARM_ADDRESS
-        ),
-        group_address_wind_alarm=config.get(WeatherSchema.CONF_KNX_WIND_ALARM_ADDRESS),
-        group_address_day_night=config.get(WeatherSchema.CONF_KNX_DAY_NIGHT_ADDRESS),
-        group_address_air_pressure=config.get(
-            WeatherSchema.CONF_KNX_AIR_PRESSURE_ADDRESS
-        ),
-        group_address_humidity=config.get(WeatherSchema.CONF_KNX_HUMIDITY_ADDRESS),
     )
