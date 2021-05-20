ce87e8b6483be9182b1f52adecb322f21e7f67bd
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-05-20 13:18:57 +0200

    Add constructor return type in integrations L-N

--- homeassistant/components/meteo_france/config_flow.py
+++ homeassistant/components/meteo_france/config_flow.py
@@ -19,7 +19,7 @@ class MeteoFranceFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Init MeteoFranceFlowHandler."""
         self.places = []
 
@@ -113,7 +113,7 @@ class MeteoFranceFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 class MeteoFranceOptionsFlowHandler(config_entries.OptionsFlow):
     """Handle a option flow."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry):
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize options flow."""
         self.config_entry = config_entry
 
--- homeassistant/components/meteo_france/sensor.py
+++ homeassistant/components/meteo_france/sensor.py
@@ -194,7 +194,7 @@ class MeteoFranceRainSensor(MeteoFranceSensor):
 class MeteoFranceAlertSensor(MeteoFranceSensor):
     """Representation of a Meteo-France alert sensor."""
 
-    def __init__(self, sensor_type: str, coordinator: DataUpdateCoordinator):
+    def __init__(self, sensor_type: str, coordinator: DataUpdateCoordinator) -> None:
         """Initialize the Meteo-France sensor."""
         super().__init__(sensor_type, coordinator)
         dept_code = self.coordinator.data.domain_id
