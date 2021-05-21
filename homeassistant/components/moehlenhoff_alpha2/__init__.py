d9816ecf27b4c7e81b2aa74fcb1a6ae0ddec5a58
Jan Schneider <oss@janschneider.net>
2021-05-21 19:25:30 +0200

    Use async_unload_platforms

--- homeassistant/components/moehlenhoff_alpha2/__init__.py
+++ homeassistant/components/moehlenhoff_alpha2/__init__.py
@@ -44,14 +44,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
     if unload_ok and entry.entry_id in hass.data[DOMAIN]:
         hass.data[DOMAIN].pop(entry.entry_id)
 
