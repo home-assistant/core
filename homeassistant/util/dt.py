ec1dfa8fbb0137958e8698accc8f9569942b0de1
Erik <erik@montnemery.com>
2021-05-21 13:37:55 +0200

    Apply suggestions from code review

--- homeassistant/util/dt.py
+++ homeassistant/util/dt.py
@@ -81,8 +81,9 @@ def as_utc(dattim: dt.datetime) -> dt.datetime:
 
 def as_timestamp(dt_value: dt.datetime | str) -> float:
     """Convert a date/time into a unix time (seconds since 1970)."""
-    if hasattr(dt_value, "timestamp"):
-        parsed_dt: dt.datetime | None = cast(dt.datetime, dt_value)
+    parsed_dt: dt.datetime | None
+    if isinstance(dt_value, dt.datetime):
+        parsed_dt = dt_value
     else:
         parsed_dt = parse_datetime(str(dt_value))
     if parsed_dt is None:
