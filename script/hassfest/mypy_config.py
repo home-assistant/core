b5d6fada98e00eb815e310b1ed74c6dd689d05b8
Michael <35783820+mib1185@users.noreply.github.com>
2021-05-21 14:33:54 +0200

    Add strict type annotations to fitbit (#50740)
    * add strict type annotations
    
    * cast json_load()
    
    * apply suggestions
    
    * move SCAN_INTERVAL back to platform file
    
    * apply suggestion
    
    * apply suggestion
    
    * apply suggestions
    
    * rename to PARENT_PLATFORM_SCHEMA

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -63,7 +63,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.fints.*",
     "homeassistant.components.fireservicerota.*",
     "homeassistant.components.firmata.*",
-    "homeassistant.components.fitbit.*",
     "homeassistant.components.flo.*",
     "homeassistant.components.fortios.*",
     "homeassistant.components.foscam.*",
