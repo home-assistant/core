f4d3b884d04906153a7ced5ec992ccafb49e8f7a
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2021-05-22 03:53:58 +0000

    Cleanup __init__.py

--- homeassistant/components/switchbot/__init__.py
+++ homeassistant/components/switchbot/__init__.py
@@ -1,25 +1,18 @@
 """Support for Switchbot devices."""
-from datetime import timedelta
-import logging
-
 from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
-
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
-
-PLATFORM = ["switch"]
+PLATFORMS = ["switch"]
 
 
 async def async_setup_entry(hass, entry):
     """Set up Switchbot from a config entry."""
     hass.data.setdefault(DOMAIN, {})
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORM)
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
 
 async def async_unload_entry(hass, entry):
     """Unload a config entry."""
-    return True
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
