14f60615038ef5e78d61539e535bdef6145b180c
hesselonline <hessel@hesselonline.nl>
2021-05-23 13:59:25 +0200

    removed CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL

--- homeassistant/components/wallbox/config_flow.py
+++ homeassistant/components/wallbox/config_flow.py
@@ -34,9 +34,6 @@ async def validate_input(hass: core.HomeAssistant, data):
 class ConfigFlow(config_entries.ConfigFlow, domain=COMPONENT_DOMAIN):
     """Handle a config flow for Wallbox."""
 
-    VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
-
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is None:
