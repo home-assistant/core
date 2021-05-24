e6f0bb744bc852b82df6c499a904c1a99133b0dc
jan iversen <jancasacondor@gmail.com>
2021-05-24 14:03:44 +0200

    Add restore temperature to modbus climate (#50963)
    * Add restore temperature to climate.
    
    * please mypy.
    
    * Review 2.

--- tests/components/modbus/test_climate.py
+++ tests/components/modbus/test_climate.py
@@ -2,15 +2,24 @@
 import pytest
 
 from homeassistant.components.climate import DOMAIN as CLIMATE_DOMAIN
+from homeassistant.components.climate.const import HVAC_MODE_AUTO
 from homeassistant.components.modbus.const import (
     CONF_CLIMATES,
     CONF_CURRENT_TEMP,
     CONF_DATA_COUNT,
     CONF_TARGET_TEMP,
 )
-from homeassistant.const import CONF_NAME, CONF_SCAN_INTERVAL, CONF_SLAVE
+from homeassistant.const import (
+    ATTR_TEMPERATURE,
+    CONF_NAME,
+    CONF_SCAN_INTERVAL,
+    CONF_SLAVE,
+)
+from homeassistant.core import State
 
 from .conftest import ReadResult, base_config_test, base_test, prepare_service_update
+
+from tests.common import mock_restore_cache
 
 
 @pytest.mark.parametrize(
@@ -101,3 +110,34 @@ async def test_service_climate_update(hass, mock_pymodbus):
         "homeassistant", "update_entity", {"entity_id": entity_id}, blocking=True
     )
     assert hass.states.get(entity_id).state == "auto"
+
+
+async def test_restore_state_climate(hass):
+    """Run test for sensor restore state."""
+
+    climate_name = "test_climate"
+    test_temp = 37
+    entity_id = f"{CLIMATE_DOMAIN}.{climate_name}"
+    test_value = State(entity_id, 35)
+    test_value.attributes = {ATTR_TEMPERATURE: test_temp}
+    config_sensor = {
+        CONF_NAME: climate_name,
+        CONF_TARGET_TEMP: 117,
+        CONF_CURRENT_TEMP: 117,
+    }
+    mock_restore_cache(
+        hass,
+        (test_value,),
+    )
+    await base_config_test(
+        hass,
+        config_sensor,
+        climate_name,
+        CLIMATE_DOMAIN,
+        CONF_CLIMATES,
+        None,
+        method_discovery=True,
+    )
+    state = hass.states.get(entity_id)
+    assert state.state == HVAC_MODE_AUTO
+    assert state.attributes[ATTR_TEMPERATURE] == test_temp
