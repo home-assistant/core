360f12192f601237886d345e09f6eb8dd7b2823e
starkillerOG <starkiller.og@gmail.com>
2021-05-24 20:23:27 +0200

    fix tests

--- homeassistant/components/alexa/entities.py
+++ homeassistant/components/alexa/entities.py
@@ -618,10 +618,9 @@ class MediaPlayerCapabilities(AlexaEntity):
 
         # AlexaEqualizerController is disabled for denonavr
         # since it blocks alexa from discovering any devices.
-        domain = entity_sources(self.hass)[self.entity_id]["domain"]
-        if supported & media_player.const.SUPPORT_SELECT_SOUND_MODE and domain not in [
-            "denonavr"
-        ]:
+        if supported & media_player.const.SUPPORT_SELECT_SOUND_MODE and entity_sources(
+            self.hass
+        )[self.entity_id]["domain"] not in ["denonavr"]:
             inputs = AlexaEqualizerController.get_valid_inputs(
                 self.entity.attributes.get(media_player.const.ATTR_SOUND_MODE_LIST, [])
             )
