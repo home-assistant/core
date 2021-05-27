0520964e768801a73e281f0a8e2d160320cb7a3e
Aaron Schneider <aaron.david.schneider@gmail.com>
2021-05-27 12:56:53 +0200

    refactor async_added_to_hass

--- homeassistant/components/sonos/switch.py
+++ homeassistant/components/sonos/switch.py
@@ -43,11 +43,6 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                 entity = SonosAlarmEntity(alarm_id, speaker)
                 async_add_entities([entity])
                 configured_alarms.add(alarm_id)
-                config_entry.async_on_unload(
-                    async_dispatcher_connect(
-                        hass, SONOS_ALARM_UPDATE, entity.async_update
-                    )
-                )
 
     config_entry.async_on_unload(
         async_dispatcher_connect(hass, SONOS_CREATE_ALARM, _async_create_entity)
@@ -63,6 +58,17 @@ class SonosAlarmEntity(SonosEntity, SwitchEntity):
 
         self._alarm_id = alarm_id
         self.entity_id = ENTITY_ID_FORMAT.format(f"sonos_alarm_{self.alarm_id}")
+
+    async def async_added_to_hass(self) -> None:
+        """Handle switch setup when added to hass."""
+        await super().async_added_to_hass()
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass,
+                SONOS_ALARM_UPDATE,
+                self.async_update,
+            )
+        )
 
     @property
     def alarm(self):
