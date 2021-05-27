7b2ae5e470a6b3dd394f233320caa1891e2ba2da
Franck Nijhof <git@frenck.dev>
2021-05-27 10:13:44 +0200

    Commit missing change oops

--- homeassistant/components/dnsip/sensor.py
+++ homeassistant/components/dnsip/sensor.py
@@ -79,6 +79,6 @@ class WanIpSensor(SensorEntity):
             response = None
 
         if response:
-            self._state = response[0].host
+            self._attr_state = response[0].host
         else:
-            self._state = None
+            self._attr_state = None
