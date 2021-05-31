5ff8a7bb1bee417efdf9abf19b6cb34b0fa07575
Robert Hillis <tkdrob4390@yahoo.com>
2021-05-31 12:35:10 -0400

    Remove bad core change

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -910,7 +910,6 @@ class ConfigEntries:
         If an entry was not loaded, will just load.
         """
         entry = self.async_get_entry(entry_id)
-        _LOGGER.error(entry)
 
         if entry is None:
             raise UnknownEntry
