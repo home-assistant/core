d7ab219793ef726fdcee62c3a5dbe9678322a919
Franck Nijhof <git@frenck.dev>
2021-05-31 10:50:11 +0200

    Collection of changing entity properties to class attributes (#51248)
    * Collection of changing entity properties to class attributes
    
    * Apply suggestions from code review
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>

--- homeassistant/components/smarttub/binary_sensor.py
+++ homeassistant/components/smarttub/binary_sensor.py
@@ -63,6 +63,8 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class SmartTubOnline(SmartTubSensorBase, BinarySensorEntity):
     """A binary sensor indicating whether the spa is currently online (connected to the cloud)."""
 
+    _attr_device_class = DEVICE_CLASS_CONNECTIVITY
+
     def __init__(self, coordinator, spa):
         """Initialize the entity."""
         super().__init__(coordinator, spa, "Online", "online")
@@ -80,14 +82,11 @@ class SmartTubOnline(SmartTubSensorBase, BinarySensorEntity):
         """Return true if the binary sensor is on."""
         return self._state is True
 
-    @property
-    def device_class(self) -> str:
-        """Return the device class for this entity."""
-        return DEVICE_CLASS_CONNECTIVITY
-
 
 class SmartTubReminder(SmartTubEntity, BinarySensorEntity):
     """Reminders for maintenance actions."""
+
+    _attr_device_class = DEVICE_CLASS_PROBLEM
 
     def __init__(self, coordinator, spa, reminder):
         """Initialize the entity."""
@@ -120,11 +119,6 @@ class SmartTubReminder(SmartTubEntity, BinarySensorEntity):
             ATTR_REMINDER_SNOOZED: self.reminder.snoozed,
         }
 
-    @property
-    def device_class(self) -> str:
-        """Return the device class for this entity."""
-        return DEVICE_CLASS_PROBLEM
-
     async def async_snooze(self, days):
         """Snooze this reminder for the specified number of days."""
         await self.reminder.snooze(days)
@@ -136,6 +130,8 @@ class SmartTubError(SmartTubEntity, BinarySensorEntity):
 
     There may be 0 or more errors. If there are >0, we show the first one.
     """
+
+    _attr_device_class = DEVICE_CLASS_PROBLEM
 
     def __init__(self, coordinator, spa):
         """Initialize the entity."""
@@ -175,8 +171,3 @@ class SmartTubError(SmartTubEntity, BinarySensorEntity):
             ATTR_CREATED_AT: error.created_at.isoformat(),
             ATTR_UPDATED_AT: error.updated_at.isoformat(),
         }
-
-    @property
-    def device_class(self) -> str:
-        """Return the device class for this entity."""
-        return DEVICE_CLASS_PROBLEM
