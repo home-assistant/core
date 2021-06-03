849e2fc0e71924678c3cbab277045ee5957cfa98
Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
2021-06-03 08:39:44 +0200

    Add binary sensor platform to SIA integration (#51206)
    * add support for binary_sensor
    
    * added default enabled for binary sensors
    
    * fixed coverage and a import deleted
    
    * disable pylint for line
    
    * Apply suggestions from code review
    
    * split binary sensor and used more attr fields
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>

--- homeassistant/components/sia/const.py
+++ homeassistant/components/sia/const.py
@@ -2,13 +2,14 @@
 from homeassistant.components.alarm_control_panel import (
     DOMAIN as ALARM_CONTROL_PANEL_DOMAIN,
 )
+from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAIN
 
-PLATFORMS = [ALARM_CONTROL_PANEL_DOMAIN]
+PLATFORMS = [ALARM_CONTROL_PANEL_DOMAIN, BINARY_SENSOR_DOMAIN]
 
 DOMAIN = "sia"
 
 ATTR_CODE = "last_code"
-ATTR_ZONE = "zone"
+ATTR_ZONE = "last_zone"
 ATTR_MESSAGE = "last_message"
 ATTR_ID = "last_id"
 ATTR_TIMESTAMP = "last_timestamp"
@@ -24,5 +25,7 @@ CONF_ZONES = "zones"
 
 SIA_NAME_FORMAT = "{} - {} - zone {} - {}"
 SIA_UNIQUE_ID_FORMAT_ALARM = "{}_{}_{}"
+SIA_UNIQUE_ID_FORMAT_BINARY = "{}_{}_{}_{}"
+SIA_HUB_ZONE = 0
 
 SIA_EVENT = "sia_event_{}_{}"
