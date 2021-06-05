35e58e9b4463b65e8887950fad5de834d68e2aa3
Klaas Schoute <klaas_schoute@hotmail.com>
2021-06-05 18:48:52 +0000

    adjust settings after configuring option

--- homeassistant/components/forecast_solar/__init__.py
+++ homeassistant/components/forecast_solar/__init__.py
@@ -50,6 +50,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # Set up all platforms for this device/entry
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
+    entry.async_on_unload(entry.add_update_listener(async_update_options))
+
     return True
 
 
@@ -60,3 +62,8 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def async_update_options(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Update options."""
+    await hass.config_entries.async_reload(config_entry.entry_id)
--- homeassistant/components/forecast_solar/config_flow.py
+++ homeassistant/components/forecast_solar/config_flow.py
@@ -6,7 +6,9 @@ from typing import Any
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
+from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 import homeassistant.helpers.config_validation as cv
@@ -24,6 +26,14 @@ class ForecastSolarFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Forecast Solar."""
 
     VERSION = 1
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> ForecastSolarOptionFlowHandler:
+        """Get the options flow for this handler."""
+        return ForecastSolarOptionFlowHandler(config_entry)
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -50,6 +60,53 @@ class ForecastSolarFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                     vol.Required(CONF_AZIMUTH): vol.Coerce(int),
                     vol.Required(CONF_MODULES_POWER): vol.Coerce(float),
                     vol.Optional(CONF_DAMPING, default=0): vol.Coerce(int),
+                }
+            ),
+        )
+
+
+class ForecastSolarOptionFlowHandler(config_entries.OptionsFlow):
+    """Handle options."""
+
+    def __init__(self, entry: ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.entry = entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Manage the options."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_DECLINATION,
+                        default=self.entry.options.get(
+                            CONF_DECLINATION, self.entry.data.get(CONF_DECLINATION)
+                        ),
+                    ): vol.Coerce(int),
+                    vol.Required(
+                        CONF_AZIMUTH,
+                        default=self.entry.options.get(
+                            CONF_AZIMUTH, self.entry.data.get(CONF_AZIMUTH)
+                        ),
+                    ): vol.Coerce(int),
+                    vol.Required(
+                        CONF_MODULES_POWER,
+                        default=self.entry.options.get(
+                            CONF_MODULES_POWER, self.entry.data.get(CONF_MODULES_POWER)
+                        ),
+                    ): vol.Coerce(float),
+                    vol.Optional(
+                        CONF_DAMPING,
+                        default=self.entry.options.get(
+                            CONF_DAMPING, self.entry.data.get(CONF_DAMPING)
+                        ),
+                    ): vol.Coerce(int),
                 }
             ),
         )
