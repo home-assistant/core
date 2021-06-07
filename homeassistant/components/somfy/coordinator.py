fb62aa7f684306a18de265e0500fd98a3fda5e13
Thibaut Etienne <thibaut@etienne.pw>
2021-06-07 09:58:27 +0000

    Reduce call to /site entrypoint

--- homeassistant/components/somfy/coordinator.py
+++ homeassistant/components/somfy/coordinator.py
@@ -6,9 +6,10 @@ from typing import Dict, Optional
 
 from pymfy.api.model import Device
 from pymfy.api.somfy_api import SomfyApi
+from requests.exceptions import HTTPError
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 
 class SomfyDataUpdateCoordinator(DataUpdateCoordinator):
@@ -33,10 +34,27 @@ class SomfyDataUpdateCoordinator(DataUpdateCoordinator):
 
         self.data = {}
         self.client = client
+        self.site_ids = []
+        self.last_site_index = -1
 
     async def _async_update_data(self) -> Dict[str, Device]:
         """Fetch Somfy data."""
-        devices = await self.hass.async_add_executor_job(self.client.get_devices)
+        if not self.site_ids:
+            try:
+                sites = await self.hass.async_add_executor_job(self.client.get_sites)
+            except HTTPError:
+                sites = []
+            self.side_ids = [site.id for site in sites]
+            if not self.site_ids:
+                raise UpdateFailed("Somfy did not returned any site id.")
+
+        try:
+            devices = await self.hass.async_add_executor_job(
+                self.client.get_devices, self._site_id
+            )
+        except HTTPError:
+            devices = []
+
         previous_devices = self.data
         # Sometimes Somfy returns an empty list.
         if not devices and previous_devices:
@@ -45,3 +63,12 @@ class SomfyDataUpdateCoordinator(DataUpdateCoordinator):
             )
             return previous_devices
         return {dev.id: dev for dev in devices}
+
+    @property
+    def _site_id(self):
+        """Return the next site id to retrieve.
+
+        This tweak is required as Somfy does not allow to call the /site entrypoint more than once per minute.
+        """
+        self.last_site_index = (self.last_site_index + 1) % len(self.site_ids)
+        return self.site_ids[self.last_site_index]
