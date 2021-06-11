ffd1891a9d0cbe95ddcd74a5a08d2c1d4da5ac5e
Chris Talkington <chris@talkingtontech.com>
2021-06-10 22:11:11 -0500

    Update __init__.py

--- homeassistant/components/roku/__init__.py
+++ homeassistant/components/roku/__init__.py
@@ -15,10 +15,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.update_coordinator import (
-    DataUpdateCoordinator,
-    UpdateFailed,
-)
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 from homeassistant.util.dt import utcnow
 
 from .const import DOMAIN
