1352adc74d3cda8852641400653804b6285826b7
Franck Nijhof <git@frenck.dev>
2021-06-11 11:36:54 +0200

    Mock WLED in all WLED tests (#51724)
    * Mock WLED in all WLED tests
    
    * Update tests/components/wled/conftest.py
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    
    * Remove useless AsyncMock
    
    * Add missing asserts
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>

--- tests/components/wled/test_init.py
+++ tests/components/wled/test_init.py
@@ -1,5 +1,5 @@
 """Tests for the WLED integration."""
-from unittest.mock import MagicMock, patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 from wled import WLEDConnectionError
 
@@ -7,37 +7,44 @@ from homeassistant.components.wled.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
-from tests.components.wled import init_integration
-from tests.test_util.aiohttp import AiohttpClientMocker
+from tests.common import MockConfigEntry
 
 
-@patch(
-    "homeassistant.components.wled.coordinator.WLED.update",
-    side_effect=WLEDConnectionError,
-)
-async def test_config_entry_not_ready(
-    mock_update: MagicMock, hass: HomeAssistant, aioclient_mock: AiohttpClientMocker
-) -> None:
-    """Test the WLED configuration entry not ready."""
-    entry = await init_integration(hass, aioclient_mock)
-    assert entry.state is ConfigEntryState.SETUP_RETRY
-
-
-async def test_unload_config_entry(
-    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker
+async def test_load_unload_config_entry(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry, mock_wled: AsyncMock
 ) -> None:
     """Test the WLED configuration entry unloading."""
-    entry = await init_integration(hass, aioclient_mock)
-    assert hass.data[DOMAIN]
-
-    await hass.config_entries.async_unload(entry.entry_id)
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
+
+    assert mock_config_entry.state is ConfigEntryState.LOADED
+
+    await hass.config_entries.async_unload(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
     assert not hass.data.get(DOMAIN)
 
 
-async def test_setting_unique_id(hass, aioclient_mock):
-    """Test we set unique ID if not set yet."""
-    entry = await init_integration(hass, aioclient_mock)
+@patch(
+    "homeassistant.components.wled.coordinator.WLED.request",
+    side_effect=WLEDConnectionError,
+)
+async def test_config_entry_not_ready(
+    mock_request: MagicMock, hass: HomeAssistant, mock_config_entry: MockConfigEntry
+) -> None:
+    """Test the WLED configuration entry not ready."""
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
 
+    assert mock_request.call_count == 1
+    assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
+
+
+async def test_setting_unique_id(
+    hass: HomeAssistant, init_integration: MockConfigEntry
+) -> None:
+    """Test we set unique ID if not set yet."""
     assert hass.data[DOMAIN]
-    assert entry.unique_id == "aabbccddeeff"
+    assert init_integration.unique_id == "aabbccddeeff"
