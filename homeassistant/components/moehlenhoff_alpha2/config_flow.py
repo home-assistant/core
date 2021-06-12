e2847469775d1fa5dda517f565d3b0be711ef95c
Jan Schneider <oss@janschneider.net>
2021-06-12 10:16:52 +0000

    Remove CONNECTION_CLASS

--- homeassistant/components/moehlenhoff_alpha2/config_flow.py
+++ homeassistant/components/moehlenhoff_alpha2/config_flow.py
@@ -35,7 +35,6 @@ class Alpha2BaseConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Möhlenhoff Alpha2 config flow."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
