25df833b6644fba2a0d36b462152495691dea546
Hessel van Es <hessel@hesselonline.nl>
2021-06-12 09:12:10 +0000

    removed tests from test_init that calling methods directly

--- tests/components/wallbox/__init__.py
+++ tests/components/wallbox/__init__.py
@@ -10,7 +10,7 @@ from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from tests.common import MockConfigEntry
 
 test_response = json.loads(
-    '{"charging_power": 0,"max_available_power": 25,"charging_speed": 0,"added_range": 372,"added_energy": 44.697}'
+    '{"charging_power": 0,"max_available_power": "xx","charging_speed": 0,"added_range": "xx","added_energy": "44.697"}'
 )
 
 
@@ -26,6 +26,9 @@ async def setup_integration(hass):
         },
         entry_id="testEntry",
     )
+
+    entry.add_to_hass(hass)
+
     with requests_mock.Mocker() as mock_request:
         mock_request.get(
             "https://api.wall-box.com/auth/token/user",
