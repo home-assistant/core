d28ebca6fb539e404ba69895d31ec06fe6b09eb8
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2021-06-14 22:55:00 +0000

    Remove old code and simplify new statements.

--- homeassistant/components/screenlogic/sensor.py
+++ homeassistant/components/screenlogic/sensor.py
@@ -69,16 +69,15 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     for pump_num, pump_data in coordinator.data[SL_DATA.KEY_PUMPS].items():
         if pump_data["data"] != 0 and "currentWatts" in pump_data:
             for pump_key in pump_data:
-                enabled = True
-                if pump_data["pumpType"] == 1:  # Intelliflow VF
-                    if pump_key in ("currentRPM"):
-                        continue
-                elif pump_data["pumpType"] == 2:  # Intelliflow VS
-                    if pump_key in ("currentGPM"):
-                        continue
+                # Considerations for Intelliflow VF
+                if pump_data["pumpType"] == 1 and pump_key in ("currentRPM"):
+                    continue
+                # Considerations for Intelliflow VS
+                if pump_data["pumpType"] == 2 and pump_key in ("currentGPM"):
+                    continue
                 if pump_key in SUPPORTED_PUMP_SENSORS:
                     entities.append(
-                        ScreenLogicPumpSensor(coordinator, pump_num, pump_key, enabled)
+                        ScreenLogicPumpSensor(coordinator, pump_num, pump_key)
                     )
 
     # IntelliChem sensors
