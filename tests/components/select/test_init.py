35030bd4b28e3cbcc84d6a01ba67308e2fc1d1bd
Franck Nijhof <git@frenck.dev>
2021-06-14 19:18:06 +0200

    Slightly improve test coverage

--- tests/components/select/test_init.py
+++ tests/components/select/test_init.py
@@ -14,4 +14,15 @@ async def test_select(hass: HomeAssistant) -> None:
     """Test getting data from the mocked select entity."""
     select = MockSelectEntity()
     assert select.current_option == "option_one"
+    assert select.state == "option_one"
     assert select.options == ["option_one", "option_two", "option_three"]
+
+    # Test none selected
+    select._attr_current_option = None
+    assert select.current_option is None
+    assert select.state is None
+
+    # Test none existing selected
+    select._attr_current_option = "option_four"
+    assert select.current_option == "option_four"
+    assert select.state is None
