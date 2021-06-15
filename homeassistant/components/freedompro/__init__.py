5a772f52b7ae12613d78aca9ea79af2510ecfbc4
stefano055415 <stefano055415@gmail.com>
2021-06-15 12:11:59 +0200

    Update homeassistant/components/freedompro/__init__.py
    Co-authored-by: Maciej Bieniek <bieniu@users.noreply.github.com>

--- homeassistant/components/freedompro/__init__.py
+++ homeassistant/components/freedompro/__init__.py
@@ -40,14 +40,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-            ]
-        )
-    )
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS))
     if unload_ok:
         hass.data[DOMAIN][entry.entry_id] = {}
 
