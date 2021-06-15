60e916f2edf9ede7b3accf8ebaf02d05f7f0934a
sillyfrog <816454+sillyfrog@users.noreply.github.com>
2021-06-15 12:54:35 +1000

    Update homeassistant/components/automate/__init__.py
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/automate/__init__.py
+++ homeassistant/components/automate/__init__.py
@@ -20,10 +20,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = hub
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
