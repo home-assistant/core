599c8186097b64026c6dda01030e624ef3db19aa
hesselonline <hesselonline@users.noreply.github.com>
2021-06-17 13:17:01 +0200

    Update tests/components/wallbox/__init__.py
    Removed duplicate add_to_hass call
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/wallbox/__init__.py
+++ tests/components/wallbox/__init__.py
@@ -40,6 +40,5 @@ async def setup_integration(hass):
             json=test_response,
             status_code=200,
         )
-        entry.add_to_hass(hass)
         await hass.config_entries.async_setup(entry.entry_id)
         await hass.async_block_till_done()
