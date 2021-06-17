eddf64266180ec1e5f404368d02192a5ab4e3b7b
Matej Plavevski <matej.plavevski@gmail.com>
2021-06-17 12:00:10 +0200

    Update homeassistant/components/energenie/__init__.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/energenie/__init__.py
+++ homeassistant/components/energenie/__init__.py
@@ -6,7 +6,7 @@ from homeassistant.core import HomeAssistant
 PLATFORMS = ["switch"]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Energenie from a config entry."""
 
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
