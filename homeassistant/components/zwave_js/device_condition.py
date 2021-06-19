f8f9c3fd3365296e6506adeeb9b11417627e7ce1
raman325 <7243222+raman325@users.noreply.github.com>
2021-06-18 22:17:32 -0400

    Initial support for zwave_js device conditions

---
+++ homeassistant/components/zwave_js/device_condition.py
@@ -0,0 +1,82 @@
+"""Provide the device conditions for Z-Wave JS."""
+from __future__ import annotations
+
+import voluptuous as vol
+
+from homeassistant.const import (
+    CONF_CONDITION,
+    CONF_DEVICE_ID,
+    CONF_DOMAIN,
+    CONF_ENTITY_ID,
+    CONF_TYPE,
+)
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers import (
+    condition,
+    config_validation as cv,
+    device_registry,
+    entity_registry,
+)
+from homeassistant.helpers.config_validation import DEVICE_CONDITION_BASE_SCHEMA
+from homeassistant.helpers.typing import ConfigType, TemplateVarsType
+
+from . import DOMAIN
+from .helpers import async_get_node_status_sensor_entity_id
+
+CONDITION_TYPES = {"asleep", "awake", "dead", "alive"}
+
+CONDITION_SCHEMA = DEVICE_CONDITION_BASE_SCHEMA.extend(
+    {
+        vol.Required(CONF_TYPE): vol.In(CONDITION_TYPES),
+        vol.Required(CONF_ENTITY_ID): cv.entity_id,
+    }
+)
+
+
+async def async_get_conditions(
+    hass: HomeAssistant, device_id: str
+) -> list[dict[str, str]]:
+    """List device conditions for Z-Wave JS devices."""
+    base_condition = {
+        CONF_CONDITION: "device",
+        CONF_DEVICE_ID: device_id,
+        CONF_DOMAIN: DOMAIN,
+    }
+
+    ent_reg = entity_registry.async_get(hass)
+    dev_reg = device_registry.async_get(hass)
+    entity_id = async_get_node_status_sensor_entity_id(
+        hass, device_id, ent_reg, dev_reg
+    )
+
+    conditions = [
+        {**base_condition, CONF_ENTITY_ID: entity_id, CONF_TYPE: cond}
+        for cond in CONDITION_TYPES
+    ]
+
+    return conditions
+
+
+@callback
+def async_condition_from_config(
+    config: ConfigType, config_validation: bool
+) -> condition.ConditionCheckerType:
+    """Create a function to test a device condition."""
+    if config_validation:
+        config = CONDITION_SCHEMA(config)
+
+    state = config[CONF_TYPE]
+
+    @callback
+    def test_is_state(hass: HomeAssistant, variables: TemplateVarsType) -> bool:
+        """Test if an entity is a certain state."""
+        ent_reg = entity_registry.async_get(hass)
+        entity_id = config[CONF_ENTITY_ID]
+
+        return (
+            (entity := ent_reg.async_get(entity_id)) is not None
+            and not entity.disabled
+            and condition.state(hass, entity_id, state)
+        )
+
+    return test_is_state
---
+++ tests/components/zwave_js/test_device_condition.py
@@ -0,0 +1,229 @@
+"""The tests for Z-Wave JS device conditions."""
+from __future__ import annotations
+
+import pytest
+from zwave_js_server.event import Event
+
+from homeassistant.components import automation
+from homeassistant.components.zwave_js import DOMAIN
+from homeassistant.components.zwave_js.helpers import (
+    async_get_node_status_sensor_entity_id,
+)
+from homeassistant.helpers import device_registry, entity_registry
+from homeassistant.setup import async_setup_component
+
+from tests.common import async_get_device_automations, async_mock_service
+
+
+@pytest.fixture
+def calls(hass):
+    """Track calls to a mock service."""
+    return async_mock_service(hass, "test", "automation")
+
+
+async def test_get_conditions(hass, client, lock_schlage_be469, integration) -> None:
+    """Test we get the expected conditions from a zwave_js."""
+    dev_reg = device_registry.async_get(hass)
+    device = device_registry.async_entries_for_config_entry(
+        dev_reg, integration.entry_id
+    )[0]
+    node_sensor_entity_id = async_get_node_status_sensor_entity_id(
+        hass, device.id, dev_reg=dev_reg
+    )
+    expected_conditions = [
+        {
+            "condition": "device",
+            "domain": DOMAIN,
+            "type": "alive",
+            "device_id": device.id,
+            "entity_id": node_sensor_entity_id,
+        },
+        {
+            "condition": "device",
+            "domain": DOMAIN,
+            "type": "awake",
+            "device_id": device.id,
+            "entity_id": node_sensor_entity_id,
+        },
+        {
+            "condition": "device",
+            "domain": DOMAIN,
+            "type": "asleep",
+            "device_id": device.id,
+            "entity_id": node_sensor_entity_id,
+        },
+        {
+            "condition": "device",
+            "domain": DOMAIN,
+            "type": "dead",
+            "device_id": device.id,
+            "entity_id": node_sensor_entity_id,
+        },
+    ]
+    conditions = await async_get_device_automations(hass, "condition", device.id)
+    for condition in expected_conditions:
+        assert condition in conditions
+
+
+async def test_if_state(hass, client, lock_schlage_be469, integration, calls) -> None:
+    """Test for turn_on and turn_off conditions."""
+    dev_reg = device_registry.async_get(hass)
+    device = device_registry.async_entries_for_config_entry(
+        dev_reg, integration.entry_id
+    )[0]
+    node_sensor_entity_id = async_get_node_status_sensor_entity_id(
+        hass, device.id, dev_reg=dev_reg
+    )
+
+    ent_reg = entity_registry.async_get(hass)
+    ent_reg.async_update_entity(node_sensor_entity_id, **{"disabled_by": None})
+    await hass.config_entries.async_reload(integration.entry_id)
+
+    assert await async_setup_component(
+        hass,
+        automation.DOMAIN,
+        {
+            automation.DOMAIN: [
+                {
+                    "trigger": {"platform": "event", "event_type": "test_event1"},
+                    "condition": [
+                        {
+                            "condition": "device",
+                            "domain": DOMAIN,
+                            "device_id": device.id,
+                            "entity_id": node_sensor_entity_id,
+                            "type": "alive",
+                        }
+                    ],
+                    "action": {
+                        "service": "test.automation",
+                        "data_template": {
+                            "some": "alive - {{ trigger.platform }} - {{ trigger.event.event_type }}"
+                        },
+                    },
+                },
+                {
+                    "trigger": {"platform": "event", "event_type": "test_event2"},
+                    "condition": [
+                        {
+                            "condition": "device",
+                            "domain": DOMAIN,
+                            "device_id": device.id,
+                            "entity_id": node_sensor_entity_id,
+                            "type": "awake",
+                        }
+                    ],
+                    "action": {
+                        "service": "test.automation",
+                        "data_template": {
+                            "some": "awake - {{ trigger.platform }} - {{ trigger.event.event_type }}"
+                        },
+                    },
+                },
+                {
+                    "trigger": {"platform": "event", "event_type": "test_event3"},
+                    "condition": [
+                        {
+                            "condition": "device",
+                            "domain": DOMAIN,
+                            "device_id": device.id,
+                            "entity_id": node_sensor_entity_id,
+                            "type": "asleep",
+                        }
+                    ],
+                    "action": {
+                        "service": "test.automation",
+                        "data_template": {
+                            "some": "asleep - {{ trigger.platform }} - {{ trigger.event.event_type }}"
+                        },
+                    },
+                },
+                {
+                    "trigger": {"platform": "event", "event_type": "test_event4"},
+                    "condition": [
+                        {
+                            "condition": "device",
+                            "domain": DOMAIN,
+                            "device_id": device.id,
+                            "entity_id": node_sensor_entity_id,
+                            "type": "dead",
+                        }
+                    ],
+                    "action": {
+                        "service": "test.automation",
+                        "data_template": {
+                            "some": "dead - {{ trigger.platform }} - {{ trigger.event.event_type }}"
+                        },
+                    },
+                },
+            ]
+        },
+    )
+
+    hass.bus.async_fire("test_event1")
+    hass.bus.async_fire("test_event2")
+    hass.bus.async_fire("test_event3")
+    hass.bus.async_fire("test_event4")
+    await hass.async_block_till_done()
+    assert len(calls) == 1
+    assert calls[0].data["some"] == "alive - event - test_event1"
+
+    event = Event(
+        "wake up",
+        data={
+            "source": "node",
+            "event": "wake up",
+            "nodeId": lock_schlage_be469.node_id,
+        },
+    )
+    lock_schlage_be469.receive_event(event)
+    await hass.async_block_till_done()
+
+    hass.bus.async_fire("test_event1")
+    hass.bus.async_fire("test_event2")
+    hass.bus.async_fire("test_event3")
+    hass.bus.async_fire("test_event4")
+    await hass.async_block_till_done()
+    assert len(calls) == 2
+    assert calls[1].data["some"] == "awake - event - test_event2"
+
+    event = Event(
+        "sleep",
+        data={"source": "node", "event": "sleep", "nodeId": lock_schlage_be469.node_id},
+    )
+    lock_schlage_be469.receive_event(event)
+    await hass.async_block_till_done()
+
+    hass.bus.async_fire("test_event1")
+    hass.bus.async_fire("test_event2")
+    hass.bus.async_fire("test_event3")
+    hass.bus.async_fire("test_event4")
+    await hass.async_block_till_done()
+    assert len(calls) == 3
+    assert calls[2].data["some"] == "asleep - event - test_event3"
+
+    event = Event(
+        "dead",
+        data={"source": "node", "event": "dead", "nodeId": lock_schlage_be469.node_id},
+    )
+    lock_schlage_be469.receive_event(event)
+    await hass.async_block_till_done()
+
+    hass.bus.async_fire("test_event1")
+    hass.bus.async_fire("test_event2")
+    hass.bus.async_fire("test_event3")
+    hass.bus.async_fire("test_event4")
+    await hass.async_block_till_done()
+    assert len(calls) == 4
+    assert calls[3].data["some"] == "dead - event - test_event4"
+
+    event = Event(
+        "unknown",
+        data={
+            "source": "node",
+            "event": "unknown",
+            "nodeId": lock_schlage_be469.node_id,
+        },
+    )
+    lock_schlage_be469.receive_event(event)
+    await hass.async_block_till_done()
