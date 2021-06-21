4b043897c27fa2d67e31e566f301010913391b30
jbouwh <jan@jbsoft.nl>
2021-06-21 11:06:59 +0000

    Device name in XiaomiCoordinatedMiioEntity

--- homeassistant/components/xiaomi_miio/device.py
+++ homeassistant/components/xiaomi_miio/device.py
@@ -104,6 +104,7 @@ class XiaomiCoordinatedMiioEntity(CoordinatorEntity):
         self._model = entry.data[CONF_MODEL]
         self._mac = entry.data[CONF_MAC]
         self._device_id = entry.unique_id
+        self._device_name = entry.title
         self._unique_id = unique_id
         self._name = name
         self._available = None
