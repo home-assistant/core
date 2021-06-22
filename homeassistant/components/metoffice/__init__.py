c9c55bf65e1d722abbe2c48e008f002ea1a2ce34
Andrey Mityagin <andrey.mityagin@gmail.com>
2021-06-22 00:55:29 +0000

    Rewrote everything using CoordinatorEntity

--- homeassistant/components/metoffice/__init__.py
+++ homeassistant/components/metoffice/__init__.py
@@ -3,19 +3,24 @@
 import asyncio
 import logging
 
+import datapoint
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.util import utcnow
 
 from .const import (
     DEFAULT_SCAN_INTERVAL,
     DOMAIN,
+    METOFFICE_COORDINATES,
     METOFFICE_DAILY_COORDINATOR,
-    METOFFICE_DATA,
     METOFFICE_HOURLY_COORDINATOR,
     METOFFICE_NAME,
+    MODE_3HOURLY,
+    MODE_DAILY,
 )
 from .data import MetOfficeData
 
@@ -32,39 +37,52 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     api_key = entry.data[CONF_API_KEY]
     site_name = entry.data[CONF_NAME]
 
-    metoffice_data = MetOfficeData(hass, api_key, latitude, longitude)
-    await metoffice_data.async_update_site()
-    if metoffice_data.site_name is None:
+    connection = datapoint.connection(api_key=api_key)
+
+    site = await hass.async_add_executor_job(
+        _fetch_site, connection, latitude, longitude
+    )
+    if site is None:
         raise ConfigEntryNotReady()
 
-    metoffice_daily_coordinator = DataUpdateCoordinator(
-        hass,
-        _LOGGER,
-        name=f"MetOffice Daily Coordinator for {site_name}",
-        update_method=metoffice_data.async_update_daily,
-        update_interval=DEFAULT_SCAN_INTERVAL,
-    )
+    async def async_update_3hourly():
+        return await hass.async_add_executor_job(
+            _fetch_data, connection, site, MODE_3HOURLY
+        )
+
+    async def async_update_daily():
+        return await hass.async_add_executor_job(
+            _fetch_data, connection, site, MODE_DAILY
+        )
 
     metoffice_hourly_coordinator = DataUpdateCoordinator(
         hass,
         _LOGGER,
         name=f"MetOffice Hourly Coordinator for {site_name}",
-        update_method=metoffice_data.async_update_hourly,
+        update_method=async_update_3hourly,
+        update_interval=DEFAULT_SCAN_INTERVAL,
+    )
+
+    metoffice_daily_coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name=f"MetOffice Daily Coordinator for {site_name}",
+        update_method=async_update_daily,
         update_interval=DEFAULT_SCAN_INTERVAL,
     )
 
     metoffice_hass_data = hass.data.setdefault(DOMAIN, {})
     metoffice_hass_data[entry.entry_id] = {
-        METOFFICE_DATA: metoffice_data,
         METOFFICE_HOURLY_COORDINATOR: metoffice_hourly_coordinator,
         METOFFICE_DAILY_COORDINATOR: metoffice_daily_coordinator,
         METOFFICE_NAME: site_name,
+        METOFFICE_COORDINATES: f"{latitude}_{longitude}",
     }
 
     # Fetch initial data so we have data when entities subscribe
     await asyncio.gather(
-        metoffice_hourly_coordinator.async_refresh(),
-        metoffice_daily_coordinator.async_refresh(),
+        metoffice_hourly_coordinator.async_config_entry_first_refresh(),
+        metoffice_daily_coordinator.async_config_entry_first_refresh(),
     )
 
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
@@ -80,3 +98,33 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
         if not hass.data[DOMAIN]:
             hass.data.pop(DOMAIN)
     return unload_ok
+
+
+def _fetch_site(connection, latitude, longitude):
+    try:
+        return connection.get_nearest_forecast_site(
+            latitude=latitude, longitude=longitude
+        )
+    except datapoint.exceptions.APIException as err:
+        _LOGGER.error("Received error from Met Office Datapoint: %s", err)
+        return None
+
+
+def _fetch_data(connection, site, mode):
+    try:
+        forecast = connection.get_forecast_for_site(site.id, mode)
+    except (ValueError, datapoint.exceptions.APIException) as err:
+        _LOGGER.error("Check Met Office connection: %s", err.args)
+        raise UpdateFailed(err)
+    else:
+        time_now = utcnow()
+        return MetOfficeData(
+            forecast.now(),
+            [
+                timestep
+                for day in forecast.days
+                for timestep in day.timesteps
+                if timestep.date > time_now
+            ],
+            site,
+        )
--- homeassistant/components/metoffice/weather.py
+++ homeassistant/components/metoffice/weather.py
@@ -9,16 +9,17 @@ from homeassistant.components.weather import (
     WeatherEntity,
 )
 from homeassistant.const import LENGTH_KILOMETERS, TEMP_CELSIUS
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import (
     ATTRIBUTION,
     CONDITION_CLASSES,
     DEFAULT_NAME,
     DOMAIN,
+    METOFFICE_COORDINATES,
     METOFFICE_DAILY_COORDINATOR,
-    METOFFICE_DATA,
     METOFFICE_HOURLY_COORDINATOR,
     METOFFICE_NAME,
     MODE_3HOURLY_LABEL,
@@ -37,8 +38,8 @@ async def async_setup_entry(
 
     async_add_entities(
         [
-            MetOfficeWeather(entry.data, hass_data, True),
-            MetOfficeWeather(entry.data, hass_data, False),
+            MetOfficeWeather(hass_data[METOFFICE_HOURLY_COORDINATOR], hass_data, True),
+            MetOfficeWeather(hass_data[METOFFICE_DAILY_COORDINATOR], hass_data, False),
         ],
         False,
     )
@@ -67,26 +68,18 @@ def _get_weather_condition(metoffice_code):
     return None
 
 
-class MetOfficeWeather(WeatherEntity):
+class MetOfficeWeather(CoordinatorEntity, WeatherEntity):
     """Implementation of a Met Office weather condition."""
 
-    def __init__(self, entry_data, hass_data, use_3hourly):
+    def __init__(self, coordinator, hass_data, use_3hourly):
         """Initialise the platform with a data instance."""
-        self._data = hass_data[METOFFICE_DATA]
-        self._coordinator = (
-            hass_data[METOFFICE_HOURLY_COORDINATOR]
-            if use_3hourly
-            else hass_data[METOFFICE_DAILY_COORDINATOR]
-        )
+        super().__init__(coordinator)
 
-        self._name = f"{DEFAULT_NAME} {hass_data[METOFFICE_NAME]} {MODE_3HOURLY_LABEL if use_3hourly else MODE_DAILY_LABEL}"
-        self._unique_id = f"{self._data.latitude}_{self._data.longitude}"
+        mode_label = MODE_3HOURLY_LABEL if use_3hourly else MODE_DAILY_LABEL
+        self._name = f"{DEFAULT_NAME} {hass_data[METOFFICE_NAME]} {mode_label}"
+        self._unique_id = hass_data[METOFFICE_COORDINATES]
         if not use_3hourly:
             self._unique_id = f"{self._unique_id}_{MODE_DAILY}"
-
-        self.use_3hourly = use_3hourly
-        self.metoffice_now = None
-        self.metoffice_forecast = None
 
     @property
     def name(self):
@@ -101,15 +94,15 @@ class MetOfficeWeather(WeatherEntity):
     @property
     def condition(self):
         """Return the current condition."""
-        if self.metoffice_now:
-            return _get_weather_condition(self.metoffice_now.weather.value)
+        if self.coordinator.data.now:
+            return _get_weather_condition(self.coordinator.data.now.weather.value)
         return None
 
     @property
     def temperature(self):
         """Return the platform temperature."""
-        if self.metoffice_now and self.metoffice_now.temperature:
-            return self.metoffice_now.temperature.value
+        if self.coordinator.data.now.temperature:
+            return self.coordinator.data.now.temperature.value
         return None
 
     @property
@@ -121,8 +114,13 @@ class MetOfficeWeather(WeatherEntity):
     def visibility(self):
         """Return the platform visibility."""
         _visibility = None
-        if hasattr(self.metoffice_now, "visibility"):
-            _visibility = f"{VISIBILITY_CLASSES.get(self.metoffice_now.visibility.value)} - {VISIBILITY_DISTANCE_CLASSES.get(self.metoffice_now.visibility.value)}"
+        weather_now = self.coordinator.data.now
+        if hasattr(weather_now, "visibility"):
+            visibility_class = VISIBILITY_CLASSES.get(weather_now.visibility.value)
+            visibility_distance = VISIBILITY_DISTANCE_CLASSES.get(
+                weather_now.visibility.value
+            )
+            _visibility = f"{visibility_class} - {visibility_distance}"
         return _visibility
 
     @property
@@ -133,65 +131,46 @@ class MetOfficeWeather(WeatherEntity):
     @property
     def pressure(self):
         """Return the mean sea-level pressure."""
-        if self.metoffice_now and self.metoffice_now.pressure:
-            return self.metoffice_now.pressure.value
+        weather_now = self.coordinator.data.now
+        if weather_now and weather_now.pressure:
+            return weather_now.pressure.value
         return None
 
     @property
     def humidity(self):
         """Return the relative humidity."""
-        if self.metoffice_now and self.metoffice_now.humidity:
-            return self.metoffice_now.humidity.value
+        weather_now = self.coordinator.data.now
+        if weather_now and weather_now.humidity:
+            return weather_now.humidity.value
         return None
 
     @property
     def wind_speed(self):
         """Return the wind speed."""
-        if self.metoffice_now and self.metoffice_now.wind_speed:
-            return self.metoffice_now.wind_speed.value
+        weather_now = self.coordinator.data.now
+        if weather_now and weather_now.wind_speed:
+            return weather_now.wind_speed.value
         return None
 
     @property
     def wind_bearing(self):
         """Return the wind bearing."""
-        if self.metoffice_now and self.metoffice_now.wind_direction:
-            return self.metoffice_now.wind_direction.value
+        weather_now = self.coordinator.data.now
+        if weather_now and weather_now.wind_direction:
+            return weather_now.wind_direction.value
         return None
 
     @property
     def forecast(self):
         """Return the forecast array."""
-        return [_build_forecast_data(timestep) for timestep in self.metoffice_forecast]
+        if self.coordinator.data.forecast is None:
+            return None
+        return [
+            _build_forecast_data(timestep)
+            for timestep in self.coordinator.data.forecast
+        ]
 
     @property
     def attribution(self):
         """Return the attribution."""
         return ATTRIBUTION
-
-    async def async_added_to_hass(self) -> None:
-        """Set up a listener and load data."""
-        self.async_on_remove(
-            self._coordinator.async_add_listener(self._update_callback)
-        )
-        self._update_callback()
-
-    @callback
-    def _update_callback(self) -> None:
-        """Load data from integration."""
-        if self.use_3hourly:
-            self.metoffice_now = self._data.now_3hourly
-            self.metoffice_forecast = self._data.forecast_3hourly
-        else:
-            self.metoffice_now = self._data.now_daily
-            self.metoffice_forecast = self._data.forecast_daily
-        self.async_write_ha_state()
-
-    @property
-    def should_poll(self) -> bool:
-        """Entities do not individually poll."""
-        return False
-
-    @property
-    def available(self):
-        """Return if state is available."""
-        return self.metoffice_now is not None and self.metoffice_forecast is not None
