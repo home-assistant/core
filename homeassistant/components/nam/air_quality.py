44bbc5057a44ac044dcddbdc68ce08f5793b223d
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-06-22 11:52:04 +0200

    Bump nam library to version 1.0.0

--- homeassistant/components/nam/air_quality.py
+++ homeassistant/components/nam/air_quality.py
@@ -1,9 +1,13 @@
 """Support for the Nettigo Air Monitor air_quality service."""
 from __future__ import annotations
 
-from homeassistant.components.air_quality import AirQualityEntity
+import logging
+from typing import cast
+
+from homeassistant.components.air_quality import DOMAIN as PLATFORM, AirQualityEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
@@ -11,7 +15,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from . import NAMDataUpdateCoordinator
 from .const import (
     AIR_QUALITY_SENSORS,
-    ATTR_MHZ14A_CARBON_DIOXIDE,
+    ATTR_SDS011,
     DEFAULT_NAME,
     DOMAIN,
     SUFFIX_P1,
@@ -20,6 +24,8 @@ from .const import (
 
 PARALLEL_UPDATES = 1
 
+_LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
@@ -27,9 +33,23 @@ async def async_setup_entry(
     """Add a Nettigo Air Monitor entities from a config_entry."""
     coordinator: NAMDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
+    # Due to the change of the attribute name of one sensor, it is necessary to migrate
+    # the unique_id to the new name.
+    ent_reg = entity_registry.async_get(hass)
+    old_unique_id = f"{coordinator.unique_id}-sds"
+    new_unique_id = f"{coordinator.unique_id}-{ATTR_SDS011}"
+    if entity_id := ent_reg.async_get_entity_id(PLATFORM, DOMAIN, old_unique_id):
+        _LOGGER.debug(
+            "Migrating entity %s from old unique ID '%s' to new unique ID '%s'",
+            entity_id,
+            old_unique_id,
+            new_unique_id,
+        )
+        ent_reg.async_update_entity(entity_id, new_unique_id=new_unique_id)
+
     entities: list[NAMAirQuality] = []
     for sensor in AIR_QUALITY_SENSORS:
-        if f"{sensor}{SUFFIX_P1}" in coordinator.data:
+        if getattr(coordinator.data, f"{sensor}{SUFFIX_P1}") is not None:
             entities.append(NAMAirQuality(coordinator, sensor))
 
     async_add_entities(entities, False)
@@ -51,23 +71,21 @@ class NAMAirQuality(CoordinatorEntity, AirQualityEntity):
     @property
     def particulate_matter_2_5(self) -> StateType:
         """Return the particulate matter 2.5 level."""
-        return round_state(
-            getattr(self.coordinator.data, f"{self.sensor_type}{SUFFIX_P2}")
+        return cast(
+            StateType, getattr(self.coordinator.data, f"{self.sensor_type}{SUFFIX_P2}")
         )
 
     @property
     def particulate_matter_10(self) -> StateType:
         """Return the particulate matter 10 level."""
-        return round_state(
-            getattr(self.coordinator.data, f"{self.sensor_type}{SUFFIX_P1}")
+        return cast(
+            StateType, getattr(self.coordinator.data, f"{self.sensor_type}{SUFFIX_P1}")
         )
 
     @property
     def carbon_dioxide(self) -> StateType:
         """Return the particulate matter 10 level."""
-        return round_state(
-            getattr(self.coordinator.data, ATTR_MHZ14A_CARBON_DIOXIDE, None)
-        )
+        return cast(StateType, self.coordinator.data.mhz14a_carbon_dioxide)
 
     @property
     def available(self) -> bool:
@@ -77,14 +95,8 @@ class NAMAirQuality(CoordinatorEntity, AirQualityEntity):
         # For a short time after booting, the device does not return values for all
         # sensors. For this reason, we mark entities for which data is missing as
         # unavailable.
-        return available and bool(
-            getattr(self.coordinator.data, f"{self.sensor_type}{SUFFIX_P2}", None)
+        return (
+            available
+            and getattr(self.coordinator.data, f"{self.sensor_type}{SUFFIX_P2}")
+            is not None
         )
-
-
-def round_state(state: StateType) -> StateType:
-    """Round state."""
-    if isinstance(state, float):
-        return round(state)
-
-    return state
