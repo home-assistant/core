f613f335451d03ae11af527e4ed25bd6dfb7b84b
Pavel Pletenev <cpp.create@gmail.com>
2021-06-22 21:56:38 +0300

    Fix #52095
    Signed-off-by: delphi <cpp.create@gmail.com>

--- homeassistant/components/habitica/const.py
+++ homeassistant/components/habitica/const.py
@@ -1,6 +1,6 @@
 """Constants for the habitica integration."""
 
-from homeassistant.const import CONF_PATH
+from homeassistant.const import CONF_PATH, CONF_SERVICE_DATA
 
 CONF_API_USER = "api_user"
 
@@ -10,4 +10,5 @@ DOMAIN = "habitica"
 SERVICE_API_CALL = "api_call"
 ATTR_PATH = CONF_PATH
 ATTR_ARGS = "args"
+ATTR_DATA = CONF_SERVICE_DATA
 EVENT_API_CALL_SUCCESS = f"{DOMAIN}_{SERVICE_API_CALL}_success"
