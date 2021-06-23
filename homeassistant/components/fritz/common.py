84c9ed1b41a796cee0b2700787609413d08ec5a8
chemelli74 <simone.chemelli@gmail.com>
2021-06-23 19:03:13 +0000

    Typo

--- homeassistant/components/fritz/common.py
+++ homeassistant/components/fritz/common.py
@@ -95,7 +95,7 @@ class FritzBoxTools:
         self._model: str | None = None
         self._sw_version: str | None = None
 
-    async def async_setup(self) -> None
+    async def async_setup(self) -> None:
         """Wrap up FritzboxTools class setup."""
         await self.hass.async_add_executor_job(self.setup)
 
