a67ca08124d96764a3031d70907323fb5eb03d73
Franck Nijhof <git@frenck.dev>
2021-06-23 23:43:24 +0200

    Change dynamic segment handling of WLED (#52018)

--- homeassistant/components/wled/__init__.py
+++ homeassistant/components/wled/__init__.py
@@ -5,7 +5,6 @@ from homeassistant.components.light import DOMAIN as LIGHT_DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
@@ -16,7 +15,7 @@ PLATFORMS = (LIGHT_DOMAIN, SENSOR_DOMAIN, SWITCH_DOMAIN)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up WLED from a config entry."""
-    coordinator = WLEDDataUpdateCoordinator(hass, host=entry.data[CONF_HOST])
+    coordinator = WLEDDataUpdateCoordinator(hass, entry=entry)
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})
