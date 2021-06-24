48dfb92f1c57fd9f6f201c3286b42f858f090edd
Franck Nijhof <git@frenck.dev>
2021-06-24 18:48:51 +0200

    DSMR: Device/state classes, icons, less common disabled by default (#52159)

--- homeassistant/components/dsmr/models.py
+++ homeassistant/components/dsmr/models.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
@@ -11,6 +12,11 @@ class DSMRSensor:
     name: str
     obis_reference: str
 
+    device_class: str | None = None
     dsmr_versions: set[str] | None = None
+    entity_registry_enabled_default: bool = True
     force_update: bool = False
+    icon: str | None = None
     is_gas: bool = False
+    last_reset: datetime | None = None
+    state_class: str | None = None
