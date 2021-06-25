a995e2751868ddf4519cb49d5345fbe27d70899b
raman325 <7243222+raman325@users.noreply.github.com>
2021-06-25 17:59:47 -0400

    re-add type

--- homeassistant/components/device_automation/__init__.py
+++ homeassistant/components/device_automation/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import asyncio
 from collections.abc import MutableMapping
 from functools import wraps
+from types import ModuleType
 from typing import Any
 
 import voluptuous as vol
@@ -73,7 +74,7 @@ async def async_setup(hass, config):
 
 async def async_get_device_automation_platform(
     hass: HomeAssistant, domain: str, automation_type: str
-) -> Any:
+) -> ModuleType:
     """Load device automation platform for integration.
 
     Throws InvalidDeviceAutomationConfig if the integration is not found or does not support device automation.
