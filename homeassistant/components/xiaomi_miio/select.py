7ca3069a7c51c1c00828043504693fb6075515bd
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2021-06-26 18:01:47 +0200

    Update homeassistant/components/xiaomi_miio/select.py
    Remove default value
    
    Co-authored-by: Maciej Bieniek <bieniu@users.noreply.github.com>

--- homeassistant/components/xiaomi_miio/select.py
+++ homeassistant/components/xiaomi_miio/select.py
@@ -109,7 +109,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                     )
                 )
 
-    async_add_entities(entities, update_before_add=False)
+    async_add_entities(entities)
 
 
 class XiaomiSelector(XiaomiCoordinatedMiioEntity, SelectEntity):
