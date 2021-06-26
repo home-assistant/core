adf713564cd3e08d74820b24d37b8b96bd0eae12
jbouwh <jan@jbsoft.nl>
2021-06-26 20:32:25 +0000

    Remove default update_before_add

--- homeassistant/components/xiaomi_miio/switch.py
+++ homeassistant/components/xiaomi_miio/switch.py
@@ -362,7 +362,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                     )
                 )
 
-    async_add_entities(entities, update_before_add=False)
+    async_add_entities(entities)
 
 
 class XiaomiGenericCoordinatedSwitch(XiaomiCoordinatedMiioEntity, SwitchEntity):
