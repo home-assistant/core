361619d2d13432c6277d5ebf7451c4d5569c8be2
Matt Zimmerman <mdz@users.noreply.github.com>
2021-06-27 11:59:11 -0700

    Add service to reset SmartTub reminders (#51824)
    * Add service to reset SmartTub reminders
    
    * add test
    
    Co-authored-by: Franck Nijhof <git@frenck.dev>

--- homeassistant/components/smarttub/binary_sensor.py
+++ homeassistant/components/smarttub/binary_sensor.py
@@ -30,6 +30,11 @@ ATTR_UPDATED_AT = "updated_at"
 
 # how many days to snooze the reminder for
 ATTR_REMINDER_DAYS = "days"
+RESET_REMINDER_SCHEMA = {
+    vol.Required(ATTR_REMINDER_DAYS): vol.All(
+        vol.Coerce(int), vol.Range(min=30, max=365)
+    )
+}
 SNOOZE_REMINDER_SCHEMA = {
     vol.Required(ATTR_REMINDER_DAYS): vol.All(
         vol.Coerce(int), vol.Range(min=10, max=120)
@@ -59,6 +64,11 @@ async def async_setup_entry(hass, entry, async_add_entities):
         "snooze_reminder",
         SNOOZE_REMINDER_SCHEMA,
         "async_snooze",
+    )
+    platform.async_register_entity_service(
+        "reset_reminder",
+        RESET_REMINDER_SCHEMA,
+        "async_reset",
     )
 
 
@@ -125,6 +135,11 @@ class SmartTubReminder(SmartTubEntity, BinarySensorEntity):
     async def async_snooze(self, days):
         """Snooze this reminder for the specified number of days."""
         await self.reminder.snooze(days)
+        await self.coordinator.async_request_refresh()
+
+    async def async_reset(self, days):
+        """Dismiss this reminder, and reset it to the specified number of days."""
+        await self.reminder.reset(days)
         await self.coordinator.async_request_refresh()
 
 
