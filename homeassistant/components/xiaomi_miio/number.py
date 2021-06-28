e45a3e67b64fe5f89c586037c166931b70279c15
jbouwh <jan@jbsoft.nl>
2021-06-28 08:51:46 +0000

    Sort -remove checks feature and service mapping

--- homeassistant/components/xiaomi_miio/number.py
+++ homeassistant/components/xiaomi_miio/number.py
@@ -22,21 +22,12 @@ from .const import (
     MODEL_AIRHUMIDIFIER_CA4,
     MODEL_AIRHUMIDIFIER_CB1,
     MODELS_HUMIDIFIER,
-    SERVICE_SET_MOTOR_SPEED,
 )
 from .device import XiaomiCoordinatedMiioEntity
 
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_MOTOR_SPEED = "motor_speed"
-ATTR_ACTUAL_MOTOR_SPEED = "actual_speed"
-
-SERVICE_TO_METHOD = {
-    SERVICE_SET_MOTOR_SPEED: {
-        "method": "async_set_motor_speed",
-        "property": ATTR_MOTOR_SPEED,
-    },
-}
 
 
 @dataclass
@@ -51,7 +42,6 @@ class NumberType:
     min: float = None
     max: float = None
     step: float = None
-    service: str = None
     available_with_device_off: bool = True
 
 
@@ -64,7 +54,6 @@ NUMBER_TYPES = {
         min=200,
         max=2000,
         step=10,
-        service=SERVICE_SET_MOTOR_SPEED,
         available_with_device_off=False,
     ),
 }
@@ -112,13 +101,12 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     async_add_entities(entities)
 
 
-class XiaomiNumber(XiaomiCoordinatedMiioEntity, NumberEntity):
+class XiaomiAirHumidifierNumber(XiaomiCoordinatedMiioEntity, NumberEntity):
     """Representation of a generic Xiaomi attribute selector."""
 
     def __init__(self, name, device, entry, unique_id, number, coordinator):
         """Initialize the generic Xiaomi attribute selector."""
         super().__init__(name, device, entry, unique_id, coordinator)
-        self._state = None
         self._attr_icon = number.icon
         self._attr_unit_of_measurement = number.unit_of_measurement
         self._attr_min_value = number.min
@@ -128,9 +116,9 @@ class XiaomiNumber(XiaomiCoordinatedMiioEntity, NumberEntity):
         self._device_features = 0
         self._state_attrs = {}
         self._controller = number
-        self._enum_class = None
-        self._attributes = None
-        self._value = None
+        self._value = self._extract_value_from_attribute(
+            self.coordinator.data, self._controller.short_name
+        )
 
     @property
     def value(self):
@@ -174,27 +162,9 @@ class XiaomiNumber(XiaomiCoordinatedMiioEntity, NumberEntity):
                 self.max_value,
             )
             return
-        method = getattr(self, SERVICE_TO_METHOD[self._controller.service]["method"])
-        if await method(value):
+        if await self.async_set_motor_speed(value):
             self._value = value
             self.async_write_ha_state()
-
-
-class XiaomiAirHumidifierNumber(XiaomiNumber):
-    """Representation of a Xiaomi Air Humidifier selector."""
-
-    def __init__(self, name, device, entry, unique_id, controller, coordinator):
-        """Initialize the plug switch."""
-        super().__init__(name, device, entry, unique_id, controller, coordinator)
-        if self._model in [MODEL_AIRHUMIDIFIER_CA1, MODEL_AIRHUMIDIFIER_CB1]:
-            self._device_features = FEATURE_FLAGS_AIRHUMIDIFIER_CA_AND_CB
-        elif self._model in [MODEL_AIRHUMIDIFIER_CA4]:
-            self._device_features = FEATURE_FLAGS_AIRHUMIDIFIER_CA4
-        else:
-            self._device_features = FEATURE_FLAGS_AIRHUMIDIFIER
-        self._value = self._extract_value_from_attribute(
-            self.coordinator.data, self._controller.short_name
-        )
 
     @callback
     def _handle_coordinator_update(self):
