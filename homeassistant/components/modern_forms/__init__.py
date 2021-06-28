3472a733f750c7f6a58e144678313c131fed936b
Brian Towles <btowles@cloudera.com>
2021-06-28 02:47:41 -0500

    Modern forms switch platform (#52061)
    * Add switch platform to Modern Forms integration
    
    * Add reboot switch
    
    * Update  lib to catch status from switches
    
    * lint ignore
    
    * Removed reboot switch
    
    * bump aiomodernforms for dependency cleanup

--- homeassistant/components/modern_forms/__init__.py
+++ homeassistant/components/modern_forms/__init__.py
@@ -13,6 +13,7 @@ from aiomodernforms.models import Device as ModernFormsDeviceState
 
 from homeassistant.components.fan import DOMAIN as FAN_DOMAIN
 from homeassistant.components.light import DOMAIN as LIGHT_DOMAIN
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_MODEL, ATTR_NAME, ATTR_SW_VERSION, CONF_HOST
 from homeassistant.core import HomeAssistant
@@ -30,6 +31,7 @@ SCAN_INTERVAL = timedelta(seconds=5)
 PLATFORMS = [
     LIGHT_DOMAIN,
     FAN_DOMAIN,
+    SWITCH_DOMAIN,
 ]
 _LOGGER = logging.getLogger(__name__)
 
