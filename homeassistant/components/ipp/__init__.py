371a7bc8fccf8ec0a5e981ce2a86546fefdc9ebc
Chris Talkington <chris@talkingtontech.com>
2021-06-28 19:43:34 -0500

    Update __init__.py

--- homeassistant/components/ipp/__init__.py
+++ homeassistant/components/ipp/__init__.py
@@ -5,16 +5,11 @@ import logging
 
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (
-    CONF_HOST,
-    CONF_PORT,
-    CONF_SSL,
-    CONF_VERIFY_SSL,
-)
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_SSL, CONF_VERIFY_SSL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import CONF_BASE_PATH, DOMAIN,
+from .const import CONF_BASE_PATH, DOMAIN
 from .coordinator import IPPDataUpdateCoordinator
 
 PLATFORMS = [SENSOR_DOMAIN]
