adc39b9aabc641d1d726b24a9959fe2f0f58f3e0
Erik <erik@montnemery.com>
2021-06-29 15:49:11 +0200

    Support temperature conversion to and from K, improve tests

--- homeassistant/util/temperature.py
+++ homeassistant/util/temperature.py
@@ -2,6 +2,7 @@
 from homeassistant.const import (
     TEMP_CELSIUS,
     TEMP_FAHRENHEIT,
+    TEMP_KELVIN,
     TEMPERATURE,
     UNIT_NOT_RECOGNIZED_TEMPLATE,
 )
@@ -28,17 +29,41 @@ def celsius_to_fahrenheit(celsius: float, interval: bool = False) -> float:
     return celsius * 1.8 + 32.0
 
 
+def celsius_to_kelvin(celsius: float, interval: bool = False) -> float:
+    """Convert a temperature in Celsius to Fahrenheit."""
+    if interval:
+        return celsius
+    return celsius + 273.15
+
+
 def convert(
     temperature: float, from_unit: str, to_unit: str, interval: bool = False
 ) -> float:
     """Convert a temperature from one unit to another."""
-    if from_unit not in (TEMP_CELSIUS, TEMP_FAHRENHEIT):
+    if from_unit not in (TEMP_CELSIUS, TEMP_FAHRENHEIT, TEMP_KELVIN):
         raise ValueError(UNIT_NOT_RECOGNIZED_TEMPLATE.format(from_unit, TEMPERATURE))
-    if to_unit not in (TEMP_CELSIUS, TEMP_FAHRENHEIT):
+    if to_unit not in (TEMP_CELSIUS, TEMP_FAHRENHEIT, TEMP_KELVIN):
         raise ValueError(UNIT_NOT_RECOGNIZED_TEMPLATE.format(to_unit, TEMPERATURE))
 
     if from_unit == to_unit:
         return temperature
+
     if from_unit == TEMP_CELSIUS:
-        return celsius_to_fahrenheit(temperature, interval)
-    return fahrenheit_to_celsius(temperature, interval)
+        if to_unit == TEMP_FAHRENHEIT:
+            return celsius_to_fahrenheit(temperature, interval)
+        else:  # kelvin
+            return celsius_to_kelvin(temperature, interval)
+
+    if from_unit == TEMP_FAHRENHEIT:
+        if to_unit == TEMP_CELSIUS:
+            return fahrenheit_to_celsius(temperature, interval)
+        else:  # kelvin
+            return celsius_to_kelvin(
+                fahrenheit_to_celsius(temperature, interval), interval
+            )
+
+    # from_unit == kelvin
+    if to_unit == TEMP_CELSIUS:
+        return kelvin_to_celsius(temperature, interval)
+    else:  # fahrenheit
+        return celsius_to_fahrenheit(kelvin_to_celsius(temperature, interval), interval)
