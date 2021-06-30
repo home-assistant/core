099a0ab4edff0c044394551d00d00e7a9a43f25f
Aaron Bach <bachya1208@gmail.com>
2021-06-30 10:03:36 -0600

    Migrate AirVisual air_quality platform sensors

--- homeassistant/components/airvisual/__init__.py
+++ homeassistant/components/airvisual/__init__.py
@@ -42,7 +42,7 @@ from .const import (
     LOGGER,
 )
 
-PLATFORMS = ["air_quality", "sensor"]
+PLATFORMS = ["sensor"]
 
 DATA_LISTENER = "listener"
 
@@ -97,8 +97,8 @@ def async_get_cloud_coordinators_by_api_key(hass, api_key):
     """Get all DataUpdateCoordinator objects related to a particular API key."""
     coordinators = []
     for entry_id, coordinator in hass.data[DOMAIN][DATA_COORDINATOR].items():
-        config_entry = hass.config_entries.async_get_entry(entry_id)
-        if config_entry.data.get(CONF_API_KEY) == api_key:
+        entry = hass.config_entries.async_get_entry(entry_id)
+        if entry.data.get(CONF_API_KEY) == api_key:
             coordinators.append(coordinator)
     return coordinators
 
@@ -124,31 +124,25 @@ def async_sync_geo_coordinator_update_intervals(hass, api_key):
         coordinator.update_interval = update_interval
 
 
-async def async_setup(hass, config):
-    """Set up the AirVisual component."""
-    hass.data[DOMAIN] = {DATA_COORDINATOR: {}, DATA_LISTENER: {}}
-    return True
-
-
 @callback
-def _standardize_geography_config_entry(hass, config_entry):
+def _standardize_geography_entry(hass, entry):
     """Ensure that geography config entries have appropriate properties."""
     entry_updates = {}
 
-    if not config_entry.unique_id:
+    if not entry.unique_id:
         # If the config entry doesn't already have a unique ID, set one:
-        entry_updates["unique_id"] = config_entry.data[CONF_API_KEY]
-    if not config_entry.options:
+        entry_updates["unique_id"] = entry.data[CONF_API_KEY]
+    if not entry.options:
         # If the config entry doesn't already have any options set, set defaults:
         entry_updates["options"] = {CONF_SHOW_ON_MAP: True}
-    if config_entry.data.get(CONF_INTEGRATION_TYPE) not in [
+    if entry.data.get(CONF_INTEGRATION_TYPE) not in [
         INTEGRATION_TYPE_GEOGRAPHY_COORDS,
         INTEGRATION_TYPE_GEOGRAPHY_NAME,
     ]:
         # If the config entry data doesn't contain an integration type that we know
         # about, infer it from the data we have:
-        entry_updates["data"] = {**config_entry.data}
-        if CONF_CITY in config_entry.data:
+        entry_updates["data"] = {**entry.data}
+        if CONF_CITY in entry.data:
             entry_updates["data"][
                 CONF_INTEGRATION_TYPE
             ] = INTEGRATION_TYPE_GEOGRAPHY_NAME
@@ -160,47 +154,49 @@ def _standardize_geography_config_entry(hass, config_entry):
     if not entry_updates:
         return
 
-    hass.config_entries.async_update_entry(config_entry, **entry_updates)
+    hass.config_entries.async_update_entry(entry, **entry_updates)
 
 
 @callback
-def _standardize_node_pro_config_entry(hass, config_entry):
+def _standardize_node_pro_entry(hass, entry):
     """Ensure that Node/Pro config entries have appropriate properties."""
     entry_updates = {}
 
-    if CONF_INTEGRATION_TYPE not in config_entry.data:
+    if CONF_INTEGRATION_TYPE not in entry.data:
         # If the config entry data doesn't contain the integration type, add it:
         entry_updates["data"] = {
-            **config_entry.data,
+            **entry.data,
             CONF_INTEGRATION_TYPE: INTEGRATION_TYPE_NODE_PRO,
         }
 
     if not entry_updates:
         return
 
-    hass.config_entries.async_update_entry(config_entry, **entry_updates)
+    hass.config_entries.async_update_entry(entry, **entry_updates)
 
 
-async def async_setup_entry(hass, config_entry):
+async def async_setup_entry(hass, entry):
     """Set up AirVisual as config entry."""
-    if CONF_API_KEY in config_entry.data:
-        _standardize_geography_config_entry(hass, config_entry)
+    hass.data.setdefault(DOMAIN, {DATA_COORDINATOR: {}, DATA_LISTENER: {}})
+
+    if CONF_API_KEY in entry.data:
+        _standardize_geography_entry(hass, entry)
 
         websession = aiohttp_client.async_get_clientsession(hass)
-        cloud_api = CloudAPI(config_entry.data[CONF_API_KEY], session=websession)
+        cloud_api = CloudAPI(entry.data[CONF_API_KEY], session=websession)
 
         async def async_update_data():
             """Get new data from the API."""
-            if CONF_CITY in config_entry.data:
+            if CONF_CITY in entry.data:
                 api_coro = cloud_api.air_quality.city(
-                    config_entry.data[CONF_CITY],
-                    config_entry.data[CONF_STATE],
-                    config_entry.data[CONF_COUNTRY],
+                    entry.data[CONF_CITY],
+                    entry.data[CONF_STATE],
+                    entry.data[CONF_COUNTRY],
                 )
             else:
                 api_coro = cloud_api.air_quality.nearest_city(
-                    config_entry.data[CONF_LATITUDE],
-                    config_entry.data[CONF_LONGITUDE],
+                    entry.data[CONF_LATITUDE],
+                    entry.data[CONF_LONGITUDE],
                 )
 
             try:
@@ -213,7 +209,7 @@ async def async_setup_entry(hass, config_entry):
         coordinator = DataUpdateCoordinator(
             hass,
             LOGGER,
-            name=async_get_geography_id(config_entry.data),
+            name=async_get_geography_id(entry.data),
             # We give a placeholder update interval in order to create the coordinator;
             # then, below, we use the coordinator's presence (along with any other
             # coordinators using the same API key) to calculate an actual, leveled
@@ -223,17 +219,17 @@ async def async_setup_entry(hass, config_entry):
         )
 
         # Only geography-based entries have options:
-        hass.data[DOMAIN][DATA_LISTENER][
-            config_entry.entry_id
-        ] = config_entry.add_update_listener(async_reload_entry)
+        hass.data[DOMAIN][DATA_LISTENER][entry.entry_id] = entry.add_update_listener(
+            async_reload_entry
+        )
     else:
-        _standardize_node_pro_config_entry(hass, config_entry)
+        _standardize_node_pro_entry(hass, entry)
 
         async def async_update_data():
             """Get new data from the API."""
             try:
                 async with NodeSamba(
-                    config_entry.data[CONF_IP_ADDRESS], config_entry.data[CONF_PASSWORD]
+                    entry.data[CONF_IP_ADDRESS], entry.data[CONF_PASSWORD]
                 ) as node:
                     return await node.async_get_latest_measurements()
             except NodeProError as err:
@@ -249,40 +245,38 @@ async def async_setup_entry(hass, config_entry):
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data[DOMAIN][DATA_COORDINATOR][config_entry.entry_id] = coordinator
+    hass.data[DOMAIN][DATA_COORDINATOR][entry.entry_id] = coordinator
 
     # Reassess the interval between 2 server requests
-    if CONF_API_KEY in config_entry.data:
-        async_sync_geo_coordinator_update_intervals(
-            hass, config_entry.data[CONF_API_KEY]
-        )
+    if CONF_API_KEY in entry.data:
+        async_sync_geo_coordinator_update_intervals(hass, entry.data[CONF_API_KEY])
 
-    hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
 
-async def async_migrate_entry(hass, config_entry):
+async def async_migrate_entry(hass, entry):
     """Migrate an old config entry."""
-    version = config_entry.version
+    version = entry.version
 
     LOGGER.debug("Migrating from version %s", version)
 
     # 1 -> 2: One geography per config entry
     if version == 1:
-        version = config_entry.version = 2
+        version = entry.version = 2
 
         # Update the config entry to only include the first geography (there is always
         # guaranteed to be at least one):
-        geographies = list(config_entry.data[CONF_GEOGRAPHIES])
+        geographies = list(entry.data[CONF_GEOGRAPHIES])
         first_geography = geographies.pop(0)
         first_id = async_get_geography_id(first_geography)
 
         hass.config_entries.async_update_entry(
-            config_entry,
+            entry,
             unique_id=first_id,
             title=f"Cloud API ({first_id})",
-            data={CONF_API_KEY: config_entry.data[CONF_API_KEY], **first_geography},
+            data={CONF_API_KEY: entry.data[CONF_API_KEY], **first_geography},
         )
 
         # For any geographies that remain, create a new config entry for each one:
@@ -295,7 +289,7 @@ async def async_migrate_entry(hass, config_entry):
                 hass.config_entries.flow.async_init(
                     DOMAIN,
                     context={"source": source},
-                    data={CONF_API_KEY: config_entry.data[CONF_API_KEY], **geography},
+                    data={CONF_API_KEY: entry.data[CONF_API_KEY], **geography},
                 )
             )
 
@@ -304,30 +298,26 @@ async def async_migrate_entry(hass, config_entry):
     return True
 
 
-async def async_unload_entry(hass, config_entry):
+async def async_unload_entry(hass, entry):
     """Unload an AirVisual config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(
-        config_entry, PLATFORMS
-    )
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     if unload_ok:
-        hass.data[DOMAIN][DATA_COORDINATOR].pop(config_entry.entry_id)
-        remove_listener = hass.data[DOMAIN][DATA_LISTENER].pop(config_entry.entry_id)
+        hass.data[DOMAIN][DATA_COORDINATOR].pop(entry.entry_id)
+        remove_listener = hass.data[DOMAIN][DATA_LISTENER].pop(entry.entry_id)
         remove_listener()
 
-        if CONF_API_KEY in config_entry.data:
+        if CONF_API_KEY in entry.data:
             # Re-calculate the update interval period for any remaining consumers of
             # this API key:
-            async_sync_geo_coordinator_update_intervals(
-                hass, config_entry.data[CONF_API_KEY]
-            )
+            async_sync_geo_coordinator_update_intervals(hass, entry.data[CONF_API_KEY])
 
     return unload_ok
 
 
-async def async_reload_entry(hass, config_entry):
+async def async_reload_entry(hass, entry):
     """Handle an options update."""
-    await hass.config_entries.async_reload(config_entry.entry_id)
+    await hass.config_entries.async_reload(entry.entry_id)
 
 
 class AirVisualEntity(CoordinatorEntity):
