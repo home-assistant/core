910f7d769da99a7a53c81bb0eaef2a7fe779e141
Franck Nijhof <git@frenck.dev>
2021-07-01 15:42:26 +0200

    Use helpers in modukle init

--- homeassistant/components/youless/__init__.py
+++ homeassistant/components/youless/__init__.py
@@ -1,5 +1,4 @@
 """The youless integration."""
-import asyncio
 from urllib.error import URLError
 
 from youless_api import YoulessAPI
@@ -30,24 +29,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         raise ConfigEntryNotReady from exception
 
     hass.data[DOMAIN][entry.entry_id] = api
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
