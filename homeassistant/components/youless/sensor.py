36a58f7d8eaf0a99d95718a0e40bee6b63cb406b
Franck Nijhof <git@frenck.dev>
2021-07-01 15:49:31 +0200

    Move coordinator, cleanup sensor

--- homeassistant/components/youless/sensor.py
+++ homeassistant/components/youless/sensor.py
@@ -1,45 +1,27 @@
 """The sensor entity for the Youless integration."""
-from datetime import timedelta
-import logging
-from typing import Any, Dict, Optional
+from __future__ import annotations
 
 from youless_api.youless_sensor import YoulessSensor
 
-from homeassistant import core
 from homeassistant.components.youless import DOMAIN
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_DEVICE, DEVICE_CLASS_POWER
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
 )
 
-_LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_entry(
-    hass: core.HomeAssistant, entry: ConfigEntry, async_add_entities
-):
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
     """Initialize the integration."""
-    gateway = hass.data[DOMAIN][entry.entry_id]
+    coordinator = hass.data[DOMAIN][entry.entry_id]
     device = entry.data[CONF_DEVICE]
-
-    async def async_update_data():
-        """Fetch data from the API."""
-        await hass.async_add_executor_job(gateway.update)
-        return gateway
-
-    coordinator = DataUpdateCoordinator(
-        hass,
-        _LOGGER,
-        name="youless_gateway",
-        update_method=async_update_data,
-        update_interval=timedelta(seconds=2),
-    )
-
-    await coordinator.async_request_refresh()
 
     async_add_entities(
         [
@@ -64,21 +46,28 @@ class YoulessBaseSensor(CoordinatorEntity, Entity):
         device_group: str,
         friendly_name: str,
         sensor_id: str,
-    ):
+    ) -> None:
         """Create the sensor."""
         super().__init__(coordinator)
-        self._friendly_name = friendly_name
         self._device = device
         self._device_group = device_group
         self._sensor_id = sensor_id
 
+        self._attr_unique_id = f"{DOMAIN}_{device}_{sensor_id}"
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, f"{device}_{device_group}")},
+            "name": friendly_name,
+            "manufacturer": "YouLess",
+            "model": self.coordinator.data.model,
+        }
+
     @property
-    def get_sensor(self) -> Optional[YoulessSensor]:
+    def get_sensor(self) -> YoulessSensor | None:
         """Property to get the underlying sensor object."""
         return None
 
     @property
-    def unit_of_measurement(self) -> Optional[str]:
+    def unit_of_measurement(self) -> str | None:
         """Return the unit of measurement for the sensor."""
         if self.get_sensor is None:
             return None
@@ -94,45 +83,22 @@ class YoulessBaseSensor(CoordinatorEntity, Entity):
         return self.get_sensor.value
 
     @property
-    def unique_id(self) -> Optional[str]:
-        """Return the uniquely generated id."""
-        return f"{DOMAIN}_{self._device}_{self._sensor_id}"
-
-    @property
     def available(self) -> bool:
         """Return a flag to indicate the sensor not being available."""
-        return self.get_sensor is not None
-
-    @property
-    def device_info(self) -> Optional[Dict[str, Any]]:
-        """Return the device information."""
-        return {
-            "identifiers": {(DOMAIN, self._device, self._device_group)},
-            "name": self._friendly_name,
-            "manufacturer": "YouLess",
-            "model": self.coordinator.data.model,
-        }
+        return super().available and self.get_sensor is not None
 
 
 class GasSensor(YoulessBaseSensor):
     """The Youless gas sensor."""
 
-    def __init__(self, coordinator: DataUpdateCoordinator, device: str):
+    def __init__(self, coordinator: DataUpdateCoordinator, device: str) -> None:
         """Instantiate a gas sensor."""
         super().__init__(coordinator, device, "gas", "Gas meter", "gas")
+        self._attr_name = "Gas usage"
+        self._attr_icon = "mdi:fire"
 
     @property
-    def name(self) -> Optional[str]:
-        """Return the name of the meter."""
-        return "Gas usage"
-
-    @property
-    def icon(self) -> Optional[str]:
-        """Return the icon to use in the frontend, if any."""
-        return "mdi:fire"
-
-    @property
-    def get_sensor(self) -> Optional[YoulessSensor]:
+    def get_sensor(self) -> YoulessSensor | None:
         """Get the sensor for providing the value."""
         return self.coordinator.data.gas_meter
 
@@ -140,81 +106,64 @@ class GasSensor(YoulessBaseSensor):
 class CurrentPowerSensor(YoulessBaseSensor):
     """The current power usage sensor."""
 
-    def __init__(self, coordinator: DataUpdateCoordinator, device: str):
+    _attr_device_class = DEVICE_CLASS_POWER
+
+    def __init__(self, coordinator: DataUpdateCoordinator, device: str) -> None:
         """Instantiate the usage meter."""
         super().__init__(coordinator, device, "power", "Power usage", "usage")
         self._device = device
+        self._attr_name = "Power Usage"
 
     @property
-    def name(self) -> Optional[str]:
-        """Return the name of the meter."""
-        return "Power Usage"
-
-    @property
-    def get_sensor(self) -> Optional[YoulessSensor]:
+    def get_sensor(self) -> YoulessSensor | None:
         """Get the sensor for providing the value."""
         return self.coordinator.data.current_power_usage
-
-    @property
-    def device_class(self) -> Optional[str]:
-        """Return the class of this device, from component DEVICE_CLASSES."""
-        return DEVICE_CLASS_POWER
 
 
 class DeliveryMeterSensor(YoulessBaseSensor):
     """The Youless delivery meter value sensor."""
 
-    def __init__(self, coordinator: DataUpdateCoordinator, device: str, dev_type: str):
+    _attr_device_class = DEVICE_CLASS_POWER
+
+    def __init__(
+        self, coordinator: DataUpdateCoordinator, device: str, dev_type: str
+    ) -> None:
         """Instantiate a delivery meter sensor."""
         super().__init__(
             coordinator, device, "delivery", "Power delivery", f"delivery_{dev_type}"
         )
         self._type = dev_type
+        self._attr_name = f"Power delivery {dev_type}"
 
     @property
-    def name(self) -> Optional[str]:
-        """Return the name of the meter."""
-        return f"Power delivery {self._type}"
-
-    @property
-    def get_sensor(self) -> Optional[YoulessSensor]:
+    def get_sensor(self) -> YoulessSensor | None:
         """Get the sensor for providing the value."""
         if self.coordinator.data.delivery_meter is None:
             return None
 
         return getattr(self.coordinator.data.delivery_meter, f"_{self._type}", None)
 
-    @property
-    def device_class(self) -> Optional[str]:
-        """Return the class of this device, from component DEVICE_CLASSES."""
-        return DEVICE_CLASS_POWER
-
 
 class PowerMeterSensor(YoulessBaseSensor):
     """The Youless low meter value sensor."""
 
-    def __init__(self, coordinator: DataUpdateCoordinator, device: str, dev_type: str):
+    _attr_device_class = DEVICE_CLASS_POWER
+
+    def __init__(
+        self, coordinator: DataUpdateCoordinator, device: str, dev_type: str
+    ) -> None:
         """Instantiate a power meter sensor."""
         super().__init__(
             coordinator, device, "power", "Power usage", f"power_{dev_type}"
         )
         self._device = device
         self._type = dev_type
+        self._attr_name = f"Power {dev_type}"
 
     @property
-    def name(self) -> Optional[str]:
-        """Return the name of the meter."""
-        return f"Power {self._type}"
-
-    @property
-    def get_sensor(self) -> Optional[YoulessSensor]:
+    def get_sensor(self) -> YoulessSensor | None:
         """Get the sensor for providing the value."""
         if self.coordinator.data.power_meter is None:
             return None
 
         return getattr(self.coordinator.data.power_meter, f"_{self._type}", None)
-
-    @property
-    def device_class(self) -> Optional[str]:
-        """Return the class of this device, from component DEVICE_CLASSES."""
-        return DEVICE_CLASS_POWER
