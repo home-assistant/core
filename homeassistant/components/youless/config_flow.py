a6f8ec95e9a05b3a7b8aac0ec04312da57019b30
Franck Nijhof <git@frenck.dev>
2021-07-01 15:41:07 +0200

    Tweak and type config flow

--- homeassistant/components/youless/config_flow.py
+++ homeassistant/components/youless/config_flow.py
@@ -1,53 +1,50 @@
 """Config flow for youless integration."""
+from __future__ import annotations
+
 import logging
+from typing import Any
 from urllib.error import HTTPError, URLError
 
 import voluptuous as vol
 from youless_api import YoulessAPI
 
-from homeassistant import config_entries, exceptions
+from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import CONF_DEVICE, CONF_HOST, CONF_NAME
+from homeassistant.data_entry_flow import FlowResult
 
-from .const import DOMAIN  # pylint:disable=unused-import
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 DATA_SCHEMA = vol.Schema({vol.Required(CONF_NAME): str, vol.Required(CONF_HOST): str})
 
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class YoulessConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for youless."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
         """Handle the initial step."""
         errors = {}
         if user_input is not None:
             try:
                 api = YoulessAPI(user_input[CONF_HOST])
                 await self.hass.async_add_executor_job(api.initialize)
-
-                device_id = config_entries.uuid_util.random_uuid_hex()
-                if api.mac_address is not None:
-                    device_id = api.mac_address
-
+            except (HTTPError, URLError):
+                _LOGGER.exception("Cannot connect to host")
+                errors["base"] = "cannot_connect"
+            else:
                 return self.async_create_entry(
                     title=user_input[CONF_NAME],
                     data={
                         CONF_HOST: user_input[CONF_HOST],
-                        CONF_DEVICE: device_id,
+                        CONF_DEVICE: api.mac_address,
                     },
                 )
-            except (HTTPError, URLError):
-                _LOGGER.exception("Cannot connect to host")
-                errors["base"] = "cannot_connect"
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
-
-
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
