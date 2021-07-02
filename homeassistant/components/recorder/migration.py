c70c5c2371122e5a4a0bf95879f4814c7402c238
Erik <erik@montnemery.com>
2021-07-02 12:05:44 +0200

    Remove useless double drop of statistics table

--- homeassistant/components/recorder/migration.py
+++ homeassistant/components/recorder/migration.py
@@ -347,7 +347,7 @@ def _drop_foreign_key_constraints(connection, engine, table, columns):
             )
 
 
-def _apply_update(engine, session, new_version, old_version):  # noqa: C901
+def _apply_update(engine, session, new_version, old_version):
     """Perform operations to bring schema up to date."""
     connection = session.connection()
     if new_version == 1:
@@ -460,13 +460,11 @@ def _apply_update(engine, session, new_version, old_version):  # noqa: C901
             connection, engine, TABLE_STATES, ["old_state_id"]
         )
     elif new_version == 17:
-        if sqlalchemy.inspect(engine).has_table(Statistics.__tablename__):
-            # Recreate the statistics table
-            Statistics.__table__.drop(engine)
-            Statistics.__table__.create(engine)
+        # This dropped the statistics table, done again in version 18.
+        pass
     elif new_version == 18:
         if sqlalchemy.inspect(engine).has_table(Statistics.__tablename__):
-            # Recreate the statistics and StatisticsMeta tables
+            # Recreate the statistics and statisticsmeta tables
             Statistics.__table__.drop(engine)
             Statistics.__table__.create(engine)
             StatisticsMeta.__table__.drop(engine)
