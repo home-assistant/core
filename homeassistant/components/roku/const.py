3fbb9401a6de416f18c54e8de2a6d0a466e523f3
Chris Talkington <chris@talkingtontech.com>
2021-07-03 14:02:22 -0500

    Update const.py

--- homeassistant/components/roku/const.py
+++ homeassistant/components/roku/const.py
@@ -2,11 +2,6 @@
 DOMAIN = "roku"
 
 # Attributes
-ATTR_IDENTIFIERS = "identifiers"
-ATTR_KEYWORD = "keyword"
-ATTR_MANUFACTURER = "manufacturer"
-ATTR_MODEL = "model"
-ATTR_SOFTWARE_VERSION = "sw_version"
 ATTR_SUGGESTED_AREA = "suggested_area"
 
 # Default Values
