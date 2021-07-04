b732b988dd4049d8e54e5c36326bac8cceeb2f81
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-04 19:32:50 -0400

    Clean up channels

--- homeassistant/components/channels/media_player.py
+++ homeassistant/components/channels/media_player.py
@@ -30,7 +30,6 @@ from homeassistant.helpers import config_validation as cv, entity_platform
 
 from .const import SERVICE_SEEK_BACKWARD, SERVICE_SEEK_BY, SERVICE_SEEK_FORWARD
 
-DATA_CHANNELS = "channels"
 DEFAULT_NAME = "Channels"
 DEFAULT_PORT = 57000
 
@@ -81,29 +80,17 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 class ChannelsPlayer(MediaPlayerEntity):
     """Representation of a Channels instance."""
 
+    _attr_media_content_type = MEDIA_TYPE_CHANNEL
+    _attr_supported_features = FEATURE_SUPPORT
+
     def __init__(self, name, host, port):
         """Initialize the Channels app."""
 
-        self._name = name
+        self._attr_name = name
         self._host = host
         self._port = port
-
         self.client = Channels(self._host, self._port)
-
         self.status = None
-        self.muted = None
-
-        self.channel_number = None
-        self.channel_name = None
-        self.channel_image_url = None
-
-        self.now_playing_title = None
-        self.now_playing_episode_title = None
-        self.now_playing_season_number = None
-        self.now_playing_episode_number = None
-        self.now_playing_summary = None
-        self.now_playing_image_url = None
-
         self.favorite_channels = []
 
     def update_favorite_channels(self):
@@ -113,106 +100,41 @@ class ChannelsPlayer(MediaPlayerEntity):
     def update_state(self, state_hash):
         """Update all the state properties with the passed in dictionary."""
         self.status = state_hash.get("status", "stopped")
-        self.muted = state_hash.get("muted", False)
-
+        self._attr_is_volume_muted = state_hash.get("muted", False)
         channel_hash = state_hash.get("channel")
         np_hash = state_hash.get("now_playing")
-
+        channel_image_url = None
         if channel_hash:
-            self.channel_number = channel_hash.get("channel_number")
-            self.channel_name = channel_hash.get("channel_name")
-            self.channel_image_url = channel_hash.get("channel_image_url")
-        else:
-            self.channel_number = None
-            self.channel_name = None
-            self.channel_image_url = None
-
+            channel_image_url = channel_hash.get("channel_image_url")
+        now_playing_title = now_playing_image_url = None
         if np_hash:
-            self.now_playing_title = np_hash.get("title")
-            self.now_playing_episode_title = np_hash.get("episode_title")
-            self.now_playing_season_number = np_hash.get("season_number")
-            self.now_playing_episode_number = np_hash.get("episode_number")
-            self.now_playing_summary = np_hash.get("summary")
-            self.now_playing_image_url = np_hash.get("image_url")
-        else:
-            self.now_playing_title = None
-            self.now_playing_episode_title = None
-            self.now_playing_season_number = None
-            self.now_playing_episode_number = None
-            self.now_playing_summary = None
-            self.now_playing_image_url = None
-
-    @property
-    def name(self):
-        """Return the name of the player."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the player."""
+            now_playing_title = np_hash.get("title")
+            now_playing_image_url = np_hash.get("image_url")
+        self._attr_source_list = [channel["name"] for channel in self.favorite_channels]
+        self._attr_media_title = now_playing_title if self._attr_state else None
+        self._attr_media_image_url = (
+            now_playing_image_url
+            if now_playing_image_url
+            else channel_image_url
+            if channel_image_url
+            else "https://getchannels.com/assets/img/icon-1024.png"
+        )
+        self._attr_state = None
         if self.status == "stopped":
-            return STATE_IDLE
-
-        if self.status == "paused":
-            return STATE_PAUSED
-
-        if self.status == "playing":
-            return STATE_PLAYING
-
-        return None
+            self._attr_state = STATE_IDLE
+        elif self.status == "paused":
+            self._attr_state = STATE_PAUSED
+        elif self.status == "playing":
+            self._attr_state = STATE_PLAYING
 
     def update(self):
         """Retrieve latest state."""
         self.update_favorite_channels()
         self.update_state(self.client.status())
 
-    @property
-    def source_list(self):
-        """List of favorite channels."""
-        sources = [channel["name"] for channel in self.favorite_channels]
-        return sources
-
-    @property
-    def is_volume_muted(self):
-        """Boolean if volume is currently muted."""
-        return self.muted
-
-    @property
-    def media_content_id(self):
-        """Content ID of current playing channel."""
-        return self.channel_number
-
-    @property
-    def media_content_type(self):
-        """Content type of current playing media."""
-        return MEDIA_TYPE_CHANNEL
-
-    @property
-    def media_image_url(self):
-        """Image url of current playing media."""
-        if self.now_playing_image_url:
-            return self.now_playing_image_url
-        if self.channel_image_url:
-            return self.channel_image_url
-
-        return "https://getchannels.com/assets/img/icon-1024.png"
-
-    @property
-    def media_title(self):
-        """Title of current playing media."""
-        if self.state:
-            return self.now_playing_title
-
-        return None
-
-    @property
-    def supported_features(self):
-        """Flag of media commands that are supported."""
-        return FEATURE_SUPPORT
-
     def mute_volume(self, mute):
         """Mute (true) or unmute (false) player."""
-        if mute != self.muted:
+        if mute != self._attr_is_volume_muted:
             response = self.client.toggle_muted()
             self.update_state(response)
 
