6815af94b96d23265c037763c2de068086788c2a
Kuzj <kuzj99@gmail.com>
2021-07-04 23:05:29 +0300

    scan_interval with coordinator

--- homeassistant/components/bme280/sensor.py
+++ homeassistant/components/bme280/sensor.py
@@ -1,6 +1,5 @@
 """Support for BME280 temperature, humidity and pressure sensor."""
 from contextlib import suppress
-from datetime import timedelta
 from functools import partial
 import logging
 
@@ -8,7 +7,7 @@ from bme280spi import BME280 as BME280_spi  # pylint: disable=import-error
 from i2csense.bme280 import BME280 as BME280_i2c  # pylint: disable=import-error
 import smbus
 
-from homeassistant.components.sensor import DOMAIN, SensorEntity
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN, SensorEntity
 from homeassistant.const import (
     CONF_MONITORED_CONDITIONS,
     CONF_NAME,
@@ -17,6 +16,10 @@ from homeassistant.const import (
 )
 from homeassistant.util import Throttle
 from homeassistant.util.temperature import celsius_to_fahrenheit
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 
 from .const import (
     CONF_DELTA_TEMP,
@@ -30,7 +33,7 @@ from .const import (
     CONF_SPI_BUS,
     CONF_SPI_DEV,
     CONF_T_STANDBY,
-    DEFAULT_SCAN_INTERVAL,
+    DOMAIN,
     INTERFACE_I2C,
     INTERFACE_SPI,
     MIN_TIME_BETWEEN_UPDATES,
@@ -42,26 +45,20 @@ from .const import (
 
 _LOGGER = logging.getLogger(__name__)
 
-SCAN_INTERVAL = timedelta(seconds=DEFAULT_SCAN_INTERVAL)
-
-
-def _set_scan_interval(interval: timedelta) -> None:
-    global SCAN_INTERVAL  # pylint: disable=global-statement
-    SCAN_INTERVAL = interval
-
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the BME280 sensor."""
     if discovery_info is None:
         return
     SENSOR_TYPES[SENSOR_TEMP][1] = hass.config.units.temperature_unit
-    sensor_conf = discovery_info[DOMAIN]
-    _set_scan_interval(sensor_conf[CONF_SCAN_INTERVAL])
+    sensor_conf = discovery_info[SENSOR_DOMAIN]
     name = sensor_conf[CONF_NAME]
+    scan_interval = sensor_conf[CONF_SCAN_INTERVAL]
     if CONF_SPI_BUS in sensor_conf and CONF_SPI_DEV in sensor_conf:
         spi_dev = sensor_conf[CONF_SPI_DEV]
         spi_bus = sensor_conf[CONF_SPI_BUS]
         _LOGGER.debug("BME280 sensor initialize at %s.%s", spi_bus, spi_dev)
+        interface = INTERFACE_SPI
         sensor = await hass.async_add_executor_job(
             partial(
                 BME280_spi,
@@ -77,11 +74,11 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         if not sensor.sample_ok:
             _LOGGER.error("BME280 sensor not detected at %s.%s", spi_bus, spi_dev)
             return
-        sensor.update()
-        sensor_handler = await hass.async_add_executor_job(BME280Handler, sensor, INTERFACE_SPI)
+        sensor_update = sensor.update
     else:
         i2c_address = sensor_conf[CONF_I2C_ADDRESS]
         bus = smbus.SMBus(sensor_conf[CONF_I2C_BUS])
+        interface = INTERFACE_I2C
         sensor = await hass.async_add_executor_job(
             partial(
                 BME280_i2c,
@@ -99,13 +96,15 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         if not sensor.sample_ok:
             _LOGGER.error("BME280 sensor not detected at %s", i2c_address)
             return
-        sensor.update(True)
-        sensor_handler = await hass.async_add_executor_job(BME280Handler, sensor, INTERFACE_I2C)
-
+        sensor_update = partial(sensor.update, True)
+    await hass.async_add_executor_job(sensor_update)
+    sensor_handler = BME280Handler(sensor, interface)
     entities = []
     with suppress(KeyError):
         for condition in sensor_conf[CONF_MONITORED_CONDITIONS]:
-            entities.append(BME280Sensor(sensor_handler, condition, SENSOR_TYPES[condition][1], name))
+            entities.append(
+                BME280Sensor(hass, sensor_handler, condition, SENSOR_TYPES[condition][1], name, scan_interval)
+            )
     async_add_entities(entities, True)
 
 
@@ -132,10 +131,10 @@ class BME280Handler:
         self.sensor.update(first_reading)
 
 
-class BME280Sensor(SensorEntity):
+class BME280Sensor(CoordinatorEntity, SensorEntity):
     """Implementation of the BME280 sensor."""
 
-    def __init__(self, bme280_client, sensor_type, temp_unit, name):
+    def __init__(self, hass, bme280_client, sensor_type, temp_unit, name, scan_interval):
         """Initialize the sensor."""
         self.client_name = name
         self._name = SENSOR_TYPES[sensor_type][0]
@@ -144,6 +143,15 @@ class BME280Sensor(SensorEntity):
         self.type = sensor_type
         self._state = None
         self._unit_of_measurement = SENSOR_TYPES[sensor_type][1]
+        super().__init__(
+            DataUpdateCoordinator(
+                hass,
+                _LOGGER,
+                name=DOMAIN,
+                update_method=self.async_update,
+                update_interval=scan_interval,
+            )
+        )
 
     @property
     def name(self):
