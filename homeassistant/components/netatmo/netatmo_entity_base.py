f6d2fcced8e8064f8d91ece2c57229462eed8034
Tobias Sauerwein <cgtobi@users.noreply.github.com>
2021-07-05 13:05:18 +0200

    Use dataclasses in netatmo data handler (#52537)
    * Switch to using dataclasses
    
    * Clean up
    
    * Update homeassistant/components/netatmo/data_handler.py

--- homeassistant/components/netatmo/netatmo_entity_base.py
+++ homeassistant/components/netatmo/netatmo_entity_base.py
@@ -61,7 +61,7 @@ class NetatmoBase(Entity):
                     data_class["name"], signal_name, self.async_update_callback
                 )
 
-            for sub in self.data_handler.data_classes[signal_name].get("subscriptions"):
+            for sub in self.data_handler.data_classes[signal_name].subscriptions:
                 if sub is None:
                     await self.data_handler.unregister_data_class(signal_name, None)
 
