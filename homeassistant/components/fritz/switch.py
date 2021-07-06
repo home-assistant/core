bad2525a6d99168529b7d43a915f283552db92e7
Simone Chemelli <simone.chemelli@gmail.com>
2021-07-06 15:49:22 +0200

    Fix Fritz Wi-Fi 6 networks with same name as other Wi-Fi (#52588)

--- homeassistant/components/fritz/switch.py
+++ homeassistant/components/fritz/switch.py
@@ -245,7 +245,7 @@ def wifi_entities_list(
 ) -> list[FritzBoxWifiSwitch]:
     """Get list of wifi entities."""
     _LOGGER.debug("Setting up %s switches", SWITCH_TYPE_WIFINETWORK)
-    std_table = {"ac": "5Ghz", "n": "2.4Ghz"}
+    std_table = {"ax": "Wifi6", "ac": "5Ghz", "n": "2.4Ghz"}
     networks: dict = {}
     for i in range(4):
         if not ("WLANConfiguration" + str(i)) in fritzbox_tools.connection.services:
