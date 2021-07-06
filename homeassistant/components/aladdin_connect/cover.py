bc9092b8ec4c72b1764d9a4410b4778d4de76769
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-05 21:01:49 -0400

    fix pylint

--- homeassistant/components/aladdin_connect/cover.py
+++ homeassistant/components/aladdin_connect/cover.py
@@ -83,11 +83,9 @@ class AladdinDevice(CoverEntity):
 
     def update(self) -> None:
         """Update status of cover."""
-        self._status = STATES_MAP.get(
+        status = STATES_MAP.get(
             self._acc.get_door_status(self._device_id, self._number)
         )
-        self._attr_is_opening = self._status == STATE_OPENING
-        self._attr_is_closing = self._status == STATE_CLOSING
-        self._attr_is_closed = (
-            None if self._status is None else self._status == STATE_CLOSED
-        )
+        self._attr_is_opening = status == STATE_OPENING
+        self._attr_is_closing = status == STATE_CLOSING
+        self._attr_is_closed = None if status is None else status == STATE_CLOSED
