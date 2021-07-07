1ae6655dcf93cec8ae4b239840fd7455fe9c69eb
J. Nick Koston <nick@koston.org>
2021-07-07 08:34:44 -1000

    wip

--- homeassistant/components/sonos/config_flow.py
+++ homeassistant/components/sonos/config_flow.py
@@ -1,10 +1,13 @@
 """Config flow for SONOS."""
 import pysonos
 
+from homeassistant.const import CONF_HOST, CONF_HOSTNAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_entry_flow
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers.config_entry_flow import DiscoveryFlowHandler
+from homeassistant.helpers.typing import DiscoveryInfoType
 
-from .const import DOMAIN
+from .const import DATA_SONOS_DISCOVERY_MANAGER, DOMAIN
 
 
 async def _async_has_devices(hass: HomeAssistant) -> bool:
@@ -13,4 +16,23 @@ async def _async_has_devices(hass: HomeAssistant) -> bool:
     return bool(result)
 
 
-config_entry_flow.register_discovery_flow(DOMAIN, "Sonos", _async_has_devices)
+class SonosDiscoveryFlowHandler(DiscoveryFlowHandler):
+    """Sonos discovery flow that callsback zeroconf updates."""
+
+    def __init__(self) -> None:
+        """Init discovery flow."""
+        super().__init__(DOMAIN, "Sonos", _async_has_devices)
+
+    async def async_step_zeroconf(
+        self, discovery_info: DiscoveryInfoType
+    ) -> FlowResult:
+        """Handle a flow initialized by zeroconf."""
+        hostname = discovery_info[CONF_HOSTNAME]
+        if hostname is None or not hostname.startswith("Sonos-"):
+            return self.async_abort(reason="not_sonos_device")
+        await self.async_set_unique_id(self._domain, raise_on_progress=False)
+        host = discovery_info[CONF_HOST]
+        boot_seqnum = discovery_info["properties"].get("bootseq")
+        uid = hostname.split("-")[1].replace(".local.", "")
+        self.hass.data[DATA_SONOS_DISCOVERY_MANAGER].async_discovered_player(discovery_info["properties"], host, uid, boot_seqnum)
+        return await self.async_step_discovery(discovery_info)
