32e40ce1ce2ad7919a5841801d6ad7fa5b33841a
jjlawren <jjlawren@users.noreply.github.com>
2021-07-08 04:56:50 -0500

    Ignore unused keys from Sonos device properties callback (#52660)
    * Ignore known but unused keys from device callback
    
    * Fix bug, add test

--- homeassistant/components/sonos/speaker.py
+++ homeassistant/components/sonos/speaker.py
@@ -71,6 +71,7 @@ SUBSCRIPTION_SERVICES = [
     "zoneGroupTopology",
 ]
 UNAVAILABLE_VALUES = {"", "NOT_IMPLEMENTED", None}
+UNUSED_DEVICE_KEYS = ["SPID", "TargetRoomName"]
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -407,6 +408,10 @@ class SonosSpeaker:
         """Update device properties from an event."""
         if more_info := event.variables.get("more_info"):
             battery_dict = dict(x.split(":") for x in more_info.split(","))
+            for unused in UNUSED_DEVICE_KEYS:
+                battery_dict.pop(unused, None)
+            if not battery_dict:
+                return
             if "BattChg" not in battery_dict:
                 _LOGGER.debug(
                     "Unknown device properties update for %s (%s), please report an issue: '%s'",
