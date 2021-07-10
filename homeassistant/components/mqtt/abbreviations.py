cd2e9462c4fb668496119830c380cd731d91ac41
jbouwh <jan@jbsoft.nl>
2021-07-10 08:22:17 +0000

    Typo

--- homeassistant/components/mqtt/abbreviations.py
+++ homeassistant/components/mqtt/abbreviations.py
@@ -79,7 +79,7 @@ ABBREVIATIONS = {
     "hum_stat_t": "target_humidity_state_topic",
     "hum_stat_tpl": "target_humidity_state_template",
     "hum_rng_max": "humidity_range_max",
-    "hum_rng_min": "humidity_range_mmin",
+    "hum_rng_min": "humidity_range_min",
     "json_attr": "json_attributes",
     "json_attr_t": "json_attributes_topic",
     "json_attr_tpl": "json_attributes_template",
