8bb863d2c77eb2440d3d3e86a7186639a984eeb8
Paulus Schoutsen <balloob@gmail.com>
2021-07-19 20:23:39 -0700

    Fix pylint

--- homeassistant/components/co2signal/util.py
+++ homeassistant/components/co2signal/util.py
@@ -11,7 +11,8 @@ def get_extra_name(hass: HomeAssistant, config: dict) -> str | None:
     """Return the extra name describing the location if not home."""
     if CONF_COUNTRY_CODE in config:
         return config[CONF_COUNTRY_CODE]
-    elif CONF_LATITUDE in config:
+
+    if CONF_LATITUDE in config:
         return f"{round(config[CONF_LATITUDE], 2)}, {round(config[CONF_LONGITUDE], 2)}"
 
     return None
