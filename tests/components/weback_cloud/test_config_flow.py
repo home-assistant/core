f0f58f6c056ee147600aca712e368b29a513b512
Nicolas Wehmeyer <nicolas.wehmeyer@benicsolutions.com>
2021-07-20 10:39:05 +0000

    Fixed CI errors

--- tests/components/weback_cloud/test_config_flow.py
+++ tests/components/weback_cloud/test_config_flow.py
@@ -2,7 +2,6 @@
 from unittest.mock import patch
 
 from homeassistant import config_entries, setup
-from homeassistant.components.weback_cloud.config_flow import CannotConnect, InvalidAuth
 from homeassistant.components.weback_cloud.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import RESULT_TYPE_CREATE_ENTRY, RESULT_TYPE_FORM
@@ -52,7 +51,7 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
 
     with patch(
         "homeassistant.components.weback_cloud.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
+        side_effect=Exception,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -65,26 +64,3 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
 
     assert result2["type"] == RESULT_TYPE_FORM
     assert result2["errors"] == {"base": "invalid_auth"}
-
-
-async def test_form_cannot_connect(hass: HomeAssistant) -> None:
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.weback_cloud.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "region": "+49",
-                "phone_number": "015112345678",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == RESULT_TYPE_FORM
-    assert result2["errors"] == {"base": "cannot_connect"}
