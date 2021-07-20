686699f6069df9a2e7302ee3c170d84a2faf5069
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-07-20 06:30:00 +0200

    Co2signal configflow (#53193)
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- homeassistant/components/co2signal/__init__.py
+++ homeassistant/components/co2signal/__init__.py
@@ -1,1 +1,20 @@
-"""The co2signal component."""
+"""The CO2 Signal integration."""
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN  # noqa: F401
+
+PLATFORMS = ["sensor"]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up CO2 Signal from a config entry."""
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
