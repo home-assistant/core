0f4fe9c3bf2eb8abc06e230ea87f538d0d061f18
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-07-20 05:22:13 +0200

    Fix issues after pylint update (#53205)

--- homeassistant/components/xiaomi_miio/switch.py
+++ homeassistant/components/xiaomi_miio/switch.py
@@ -251,7 +251,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                 await asyncio.wait(update_tasks)
 
         for plug_service, method in SERVICE_TO_METHOD.items():
-            schema = method[plug_service].get("schema", SERVICE_SCHEMA)
+            schema = method.get("schema", SERVICE_SCHEMA)
             hass.services.async_register(
                 DOMAIN, plug_service, async_service_handler, schema=schema
             )
