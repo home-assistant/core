026bd9baf4a52df56b36b6f24506de69c885f473
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-07-21 07:41:08 +0200

    Convert Mill consumption attributes to sensors (#52311)

--- homeassistant/components/mill/__init__.py
+++ homeassistant/components/mill/__init__.py
@@ -1,10 +1,29 @@
 """The mill component."""
+from mill import Mill
 
-PLATFORMS = ["climate"]
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+from .const import DOMAIN
+
+PLATFORMS = ["climate", "sensor"]
 
 
 async def async_setup_entry(hass, entry):
     """Set up the Mill heater."""
+    mill_data_connection = Mill(
+        entry.data[CONF_USERNAME],
+        entry.data[CONF_PASSWORD],
+        websession=async_get_clientsession(hass),
+    )
+    if not await mill_data_connection.connect():
+        raise ConfigEntryNotReady
+
+    await mill_data_connection.find_all_heaters()
+
+    hass.data[DOMAIN] = mill_data_connection
+
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
 
