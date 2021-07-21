2ec3f6bbacd8ac1f633cc5a8850d42723462a00e
Aaron Bach <bachya1208@gmail.com>
2021-07-21 17:33:50 -0600

    Code review

--- homeassistant/components/guardian/__init__.py
+++ homeassistant/components/guardian/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import asyncio
 from collections.abc import Awaitable, MutableMapping
-from typing import Any, Callable, cast
+from typing import Any, cast
 
 from aioguardian import Client
 
@@ -143,19 +143,15 @@ class PairedSensorManager:
 
         self._paired_uids.add(uid)
 
-        def get_paired_sensor_status_coro() -> Callable[..., Awaitable]:
-            """Define a corotoune for the coordinator to use when getting status."""
-            return cast(
-                Callable[..., Awaitable], self._client.sensor.paired_sensor_status(uid)
-            )
-
         coordinator = self._hass.data[DOMAIN][DATA_COORDINATOR][self._entry.entry_id][
             API_SENSOR_PAIRED_SENSOR_STATUS
         ][uid] = GuardianDataUpdateCoordinator(
             self._hass,
             client=self._client,
             api_name=f"{API_SENSOR_PAIRED_SENSOR_STATUS}_{uid}",
-            api_coro=get_paired_sensor_status_coro(),
+            api_coro=lambda: cast(
+                Awaitable, self._client.sensor.paired_sensor_status(uid)
+            ),
             api_lock=self._api_lock,
             valve_controller_uid=self._entry.data[CONF_UID],
         )
