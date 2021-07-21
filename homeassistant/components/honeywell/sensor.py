fd969d1d4c264b750cd3366a33f7934bf6430344
Randy <randydfurman@gmail.com>
2021-07-21 13:30:07 +0000

    Code cleanup

--- homeassistant/components/honeywell/sensor.py
+++ homeassistant/components/honeywell/sensor.py
@@ -1,7 +1,6 @@
 """Support for Honeywell (US) Total Connect Comfort sensors."""
 from __future__ import annotations
 
-import homeassistant
 from homeassistant.components.honeywell.const import (
     DOMAIN,
     SENSOR_LOCATION_INDOOR,
@@ -15,11 +14,10 @@ from homeassistant.const import (
     TEMP_CELSIUS,
     TEMP_FAHRENHEIT,
 )
+import homeassistant.helpers.device_registry as dr
 
 
-async def async_setup_entry(
-    hass, config, async_add_entities, discovery_info=None
-) -> None:
+async def async_setup_entry(hass, config, async_add_entities, discovery_info=None):
     """Set up the Honeywell thermostat."""
     data = hass.data[DOMAIN][config.entry_id]
     sensors = []
@@ -43,8 +41,6 @@ async def async_setup_entry(
 
     async_add_entities(sensors)
 
-    return True
-
 
 class HoneywellUSSensor(SensorEntity):
     """Representation of a Honeywell US Sensor."""
@@ -52,8 +48,10 @@ class HoneywellUSSensor(SensorEntity):
     def __init__(self, data, location, sensor_type):
         """Initialize the sensor."""
         self._data = data
-        self._unique_id = f"{homeassistant.helpers.device_registry.format_mac(data.device.mac_address)}_{location}_{sensor_type}"
-        self._name = f"{data.device.name} {location} {sensor_type}"
+        self._attr_unique_id = (
+            f"{dr.format_mac(data.device.mac_address)}_{location}_{sensor_type}"
+        )
+        self._attr_name = f"{data.device.name} {location} {sensor_type}"
         self._location = location
         self._type = sensor_type
 
@@ -61,16 +59,6 @@ class HoneywellUSSensor(SensorEntity):
     def device(self):
         """Shortcut to access the device."""
         return self._data.device
-
-    @property
-    def unique_id(self) -> str | None:
-        """Return a unique ID."""
-        return self._unique_id
-
-    @property
-    def name(self):
-        """Return the name of the Honeywell US Sensor."""
-        return self._name
 
     @property
     def device_class(self):
