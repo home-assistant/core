b277ea419273e1875e8c724aee31d0102300e0ab
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2021-07-22 08:43:34 +0200

    Xiaomi humidifier migration migrate name to led brightness select (#52420)

--- homeassistant/components/xiaomi_miio/select.py
+++ homeassistant/components/xiaomi_miio/select.py
@@ -18,6 +18,7 @@ from .const import (
     FEATURE_SET_LED_BRIGHTNESS,
     KEY_COORDINATOR,
     KEY_DEVICE,
+    KEY_MIGRATE_ENTITY_NAME,
     MODEL_AIRHUMIDIFIER_CA1,
     MODEL_AIRHUMIDIFIER_CA4,
     MODEL_AIRHUMIDIFIER_CB1,
@@ -72,6 +73,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     model = config_entry.data[CONF_MODEL]
     device = hass.data[DOMAIN][config_entry.entry_id][KEY_DEVICE]
     coordinator = hass.data[DOMAIN][config_entry.entry_id][KEY_COORDINATOR]
+    if KEY_MIGRATE_ENTITY_NAME in hass.data[DOMAIN][config_entry.entry_id]:
+        name = hass.data[DOMAIN][config_entry.entry_id][KEY_MIGRATE_ENTITY_NAME]
+    else:
+        name = config_entry.title
 
     _LOGGER.debug("Initializing with host %s (token %s...)", host, token[:5])
 
@@ -87,7 +92,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     for selector in SELECTOR_TYPES.values():
         entities.append(
             entity_class(
-                f"{config_entry.title} {selector.name}",
+                f"{name} {selector.name}",
                 device,
                 config_entry,
                 f"{selector.short_name}_{config_entry.unique_id}",
