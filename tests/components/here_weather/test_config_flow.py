bf7f9adf72fba862ce9335013e48e3bef1b74b1e
Kevin Eifinger <k.eifinger@googlemail.com>
2021-07-22 16:07:45 +0200

    remove fire start event

--- tests/components/here_weather/test_config_flow.py
+++ tests/components/here_weather/test_config_flow.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 import herepy
 
+from homeassistant import config_entries, setup
 from homeassistant.components.here_weather.const import DOMAIN, HERE_API_KEYS
 from homeassistant.const import (
     CONF_API_KEY,
@@ -12,36 +13,49 @@ from homeassistant.const import (
     CONF_SCAN_INTERVAL,
     EVENT_HOMEASSISTANT_START,
 )
+from homeassistant.core import HomeAssistant
 
 from . import mock_weather_for_coordinates
 
 from tests.common import MockConfigEntry
 
 
-async def test_config_flow(hass):
-    """Test we can finish a config flow."""
+async def test_form(hass: HomeAssistant) -> None:
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == "form"
+    assert result["errors"] == {}
+
     with patch(
         "herepy.DestinationWeatherApi.weather_for_coordinates",
-        side_effect=mock_weather_for_coordinates,
-    ):
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": "user"}
+        return_value=None,
+    ), patch(
+        "homeassistant.components.here_weather.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_API_KEY: "test",
+                CONF_NAME: DOMAIN,
+                CONF_LATITUDE: "40.79962",
+                CONF_LONGITUDE: "-73.970314",
+            },
         )
-        assert result["type"] == "form"
-        config = {
-            CONF_API_KEY: "test",
-            CONF_NAME: DOMAIN,
-            CONF_LATITUDE: "40.79962",
-            CONF_LONGITUDE: "-73.970314",
-        }
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], config
-        )
-        assert result["type"] == "create_entry"
-
         await hass.async_block_till_done()
-        state = hass.states.get("weather.here_weather_forecast_7days_simple")
-        assert state
+
+    assert result2["type"] == "create_entry"
+    assert result2["title"] == DOMAIN
+    assert result2["data"] == {
+        CONF_API_KEY: "test",
+        CONF_NAME: DOMAIN,
+        CONF_LATITUDE: 40.79962,
+        CONF_LONGITUDE: -73.970314,
+    }
+    assert len(mock_setup_entry.mock_calls) == 1
 
 
 async def test_config_flow_known_api_key(hass):
@@ -135,7 +149,6 @@ async def test_options(hass):
         entry.add_to_hass(hass)
         await hass.config_entries.async_setup(entry.entry_id)
         await hass.async_block_till_done()
-        hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
         result = await hass.config_entries.options.async_init(entry.entry_id)
         result = await hass.config_entries.options.async_configure(
             result["flow_id"],
