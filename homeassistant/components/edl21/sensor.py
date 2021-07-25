ba473e2bb63d13c90de70d676a4a61b4916d02d2
jan Iversen <jancasacondor@gmail.com>
2021-07-25 13:47:49 +0200

    Late review comment.

--- homeassistant/components/edl21/sensor.py
+++ homeassistant/components/edl21/sensor.py
@@ -10,13 +10,14 @@ import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 from homeassistant.const import CONF_NAME
-from homeassistant.core import callback
+from homeassistant.core import HomeAssistant, callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
 )
 from homeassistant.helpers.entity_registry import async_get_registry
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.util.dt import utcnow
 
 _LOGGER = logging.getLogger(__name__)
@@ -35,7 +36,12 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities,
+    discovery_info: DiscoveryInfoType = None,
+):
     """Set up the EDL21 sensor."""
     hass.data[DOMAIN] = EDL21(hass, config, async_add_entities)
     await hass.data[DOMAIN].connect()
