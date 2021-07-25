fd947207b46f7b471d2f55503b3c085dc901f5f9
Peter Nijssen <peter@peternijssen.nl>
2021-07-25 10:19:10 +0200

    Corrected feedback on Spider HEM update

--- homeassistant/components/spider/sensor.py
+++ homeassistant/components/spider/sensor.py
@@ -1,5 +1,5 @@
 """Support for Spider Powerplugs (energy & power)."""
-from datetime import date, datetime
+from datetime import datetime
 
 from homeassistant.components.sensor import STATE_CLASS_MEASUREMENT, SensorEntity
 from homeassistant.const import (
@@ -9,6 +9,7 @@ from homeassistant.const import (
     POWER_WATT,
 )
 from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.util import dt as dt_util
 
 from .const import DOMAIN
 
@@ -16,18 +17,13 @@ from .const import DOMAIN
 async def async_setup_entry(hass, config, async_add_entities):
     """Initialize a Spider Power Plug."""
     api = hass.data[DOMAIN][config.entry_id]
-    async_add_entities(
-        [
-            SpiderPowerPlugEnergy(api, entity)
-            for entity in await hass.async_add_executor_job(api.get_power_plugs)
-        ]
-    )
-    async_add_entities(
-        [
-            SpiderPowerPlugPower(api, entity)
-            for entity in await hass.async_add_executor_job(api.get_power_plugs)
-        ]
-    )
+    entities = []
+
+    for entity in await hass.async_add_executor_job(api.get_power_plugs):
+        entities.append(SpiderPowerPlugEnergy(api, entity))
+        entities.append(SpiderPowerPlugPower(api, entity))
+
+    async_add_entities(entities)
 
 
 class SpiderPowerPlugEnergy(SensorEntity):
@@ -40,16 +36,14 @@ class SpiderPowerPlugEnergy(SensorEntity):
     def __init__(self, api, power_plug) -> None:
         """Initialize the Spider Power Plug."""
         self.api = api
-        self.attr_name = f"{power_plug.name} Total Energy"
-        self.id = f"{power_plug.id}_total_energy"
         self.power_plug = power_plug
 
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device_info of the device."""
         return {
-            "identifiers": {(DOMAIN, self.id)},
-            "name": self.attr_name,
+            "identifiers": {(DOMAIN, self.power_plug.id)},
+            "name": self.power_plug.name,
             "manufacturer": self.power_plug.manufacturer,
             "model": self.power_plug.model,
         }
@@ -57,12 +51,12 @@ class SpiderPowerPlugEnergy(SensorEntity):
     @property
     def unique_id(self) -> str:
         """Return the ID of this sensor."""
-        return self.id
+        return f"{self.power_plug.id}_total_energy_today"
 
     @property
     def name(self) -> str:
         """Return the name of the sensor if any."""
-        return self.attr_name
+        return f"{self.power_plug.name} Total Energy Today"
 
     @property
     def state(self) -> float:
@@ -71,8 +65,10 @@ class SpiderPowerPlugEnergy(SensorEntity):
 
     @property
     def last_reset(self) -> datetime:
-        """Return the time when the sensor was last reset, if any."""
-        return datetime.combine(date.today(), datetime.min.time())
+        """Return the time when last reset; Every midnight."""
+        return dt_util.as_utc(
+            dt_util.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        )
 
     def update(self) -> None:
         """Get the latest data."""
@@ -85,19 +81,18 @@ class SpiderPowerPlugPower(SensorEntity):
     _attr_device_class = DEVICE_CLASS_POWER
     _attr_state_class = STATE_CLASS_MEASUREMENT
     _attr_unit_of_measurement = POWER_WATT
+
     def __init__(self, api, power_plug) -> None:
         """Initialize the Spider Power Plug."""
         self.api = api
-        self.attr_name = f"{power_plug.name} Power Consumption"
-        self.id = f"{power_plug.id}_power_consumption"
         self.power_plug = power_plug
 
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device_info of the device."""
         return {
-            "identifiers": {(DOMAIN, self.id)},
-            "name": self.attr_name,
+            "identifiers": {(DOMAIN, self.power_plug.id)},
+            "name": self.power_plug.name,
             "manufacturer": self.power_plug.manufacturer,
             "model": self.power_plug.model,
         }
@@ -105,12 +100,12 @@ class SpiderPowerPlugPower(SensorEntity):
     @property
     def unique_id(self) -> str:
         """Return the ID of this sensor."""
-        return self.id
+        return f"{self.power_plug.id}_power_consumption"
 
     @property
     def name(self) -> str:
         """Return the name of the sensor if any."""
-        return self.attr_name
+        return f"{self.power_plug.name} Power Consumption"
 
     @property
     def state(self) -> float:
