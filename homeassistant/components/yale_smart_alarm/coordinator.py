abcc899558eba867e0d8ce8fe2f9e74fb351ec64
G Johansson <goran.johansson@shiftit.se>
2021-07-26 19:42:10 +0000

    Add jammed status for doorlock

--- homeassistant/components/yale_smart_alarm/coordinator.py
+++ homeassistant/components/yale_smart_alarm/coordinator.py
@@ -47,14 +47,28 @@ class YaleDataUpdateCoordinator(DataUpdateCoordinator):
             if device["type"] == "device_type.door_lock":
                 lock_status_str = device["minigw_lock_status"]
                 lock_status = int(str(lock_status_str or 0), 16)
+                jammed = (lock_status & 48) == 48
                 closed = (lock_status & 16) == 16
                 locked = (lock_status & 1) == 1
                 if not lock_status and "device_status.lock" in state:
                     device["_state"] = "locked"
+                    device["_state2"] = "unknown"
                     locks.append(device)
                     continue
                 if not lock_status and "device_status.unlock" in state:
                     device["_state"] = "unlocked"
+                    device["_state2"] = "unknown"
+                    locks.append(device)
+                    continue
+                if (
+                    lock_status
+                    and (
+                        "device_status.lock" in state or "device_status.unlock" in state
+                    )
+                    and jammed
+                ):
+                    device["_state"] = "jammed"
+                    device["_state2"] = "closed"
                     locks.append(device)
                     continue
                 if (
@@ -66,6 +80,7 @@ class YaleDataUpdateCoordinator(DataUpdateCoordinator):
                     and locked
                 ):
                     device["_state"] = "locked"
+                    device["_state2"] = "closed"
                     locks.append(device)
                     continue
                 if (
@@ -77,6 +92,7 @@ class YaleDataUpdateCoordinator(DataUpdateCoordinator):
                     and not locked
                 ):
                     device["_state"] = "unlocked"
+                    device["_state2"] = "closed"
                     locks.append(device)
                     continue
                 if (
@@ -87,6 +103,7 @@ class YaleDataUpdateCoordinator(DataUpdateCoordinator):
                     and not closed
                 ):
                     device["_state"] = "unlocked"
+                    device["_state2"] = "open"
                     locks.append(device)
                     continue
                 device["_state"] = "unavailable"
