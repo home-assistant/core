1681bbe5a55a8847eed8053fba4056a33ba1891a
Jc2k <john.carr@unrouted.co.uk>
2021-07-26 22:11:27 +0100

    Make sure that vocolinc flowerbud humidity sensor is exposed (via homekit_controller) (#53518)
    * Make sure that vocolinc flowerbud humidity sensor is exposed
    
    * Was a no-op to request these from humidifier, so remove them
    
    * Fix typo

--- homeassistant/components/homekit_controller/const.py
+++ homeassistant/components/homekit_controller/const.py
@@ -49,4 +49,7 @@ CHARACTERISTIC_PLATFORMS = {
     CharacteristicsTypes.Vendor.KOOGEEK_REALTIME_ENERGY_2: "sensor",
     CharacteristicsTypes.Vendor.VOCOLINC_HUMIDIFIER_SPRAY_LEVEL: "number",
     CharacteristicsTypes.get_uuid(CharacteristicsTypes.TEMPERATURE_CURRENT): "sensor",
+    CharacteristicsTypes.get_uuid(
+        CharacteristicsTypes.RELATIVE_HUMIDITY_CURRENT
+    ): "sensor",
 }
