18483d2844f03e9a42697a571ed3d774ea299413
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-07-28 10:21:02 +1200

    ESPHome - Auto reset on value going back to 0

--- homeassistant/components/esphome/sensor.py
+++ homeassistant/components/esphome/sensor.py
@@ -1,6 +1,9 @@
 """Support for esphome sensors."""
 from __future__ import annotations
 
+from datetime import datetime
+from decimal import Decimal, DecimalException
+import logging
 import math
 from typing import cast
 
@@ -11,8 +14,10 @@ from aioesphomeapi import (
     TextSensorInfo,
     TextSensorState,
 )
+from aioesphomeapi.model import LastResetType
 import voluptuous as vol
 
+from homeassistant.components.esphome.entry_data import RuntimeEntryData
 from homeassistant.components.sensor import (
     DEVICE_CLASS_TIMESTAMP,
     DEVICE_CLASSES,
@@ -20,9 +25,10 @@ from homeassistant.components.sensor import (
     SensorEntity,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import Event, HomeAssistant, callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.event import async_track_state_change_event
 from homeassistant.util import dt
 
 from . import (
@@ -33,6 +39,8 @@ from . import (
 )
 
 ICON_SCHEMA = vol.Schema(cv.icon)
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
@@ -73,6 +81,67 @@ _STATE_CLASSES: EsphomeEnumMapper[SensorStateClass, str | None] = EsphomeEnumMap
 
 class EsphomeSensor(EsphomeEntity[SensorInfo, SensorState], SensorEntity):
     """A sensor implementation for esphome."""
+
+    def __init__(
+        self, entry_data: RuntimeEntryData, component_key: str, key: int
+    ) -> None:
+        """Initialize."""
+        super().__init__(entry_data, component_key, key)
+
+    async def async_added_to_hass(self) -> None:
+        """Register callbacks."""
+        await super().async_added_to_hass()
+
+        if self._static_info.last_reset_type is LastResetType.NEVER:
+            self._attr_last_reset = datetime.min
+        elif self._static_info.last_reset_type is LastResetType.AUTO:
+
+            @callback
+            def check_last_state(event: Event) -> None:
+                """Check the states to see if last_reset should be set to now."""
+                old_state = event.data.get("old_state")
+                new_state = event.data.get("new_state")
+                _LOGGER.warning(f"Checking states {old_state} -> {new_state}")
+
+                reset = old_state is None
+                old = None
+                new = None
+                try:
+                    assert new_state is not None
+                    new = Decimal(new_state.state)
+                except DecimalException:
+                    return
+
+                _LOGGER.error(f"Reset? {reset}")
+
+                if not reset:
+                    assert old_state is not None
+                    try:
+                        old = Decimal(old_state.state)
+                    except DecimalException as err:
+                        _LOGGER.warning(
+                            "Invalid old state (%s > %s): %s",
+                            old_state.state,
+                            new,
+                            err,
+                        )
+                if old == 0 and new == 0:
+                    return
+
+                if not reset:
+                    _LOGGER.warning(
+                        f"new == 0? {new == 0}; old is None? {True if old is None else f'False; new < (old / 2)? {new < (old / 2)}'}"
+                    )
+                    reset = new == 0 or (old is not None and new < (old / 2))
+
+                if reset:
+                    _LOGGER.warning("Setting last_reset to now")
+                    self._attr_last_reset = datetime.now()
+                    self.async_write_ha_state()
+
+            async_track_state_change_event(
+                self.hass, [self.entity_id], check_last_state
+            )
 
     @property
     def icon(self) -> str | None:
