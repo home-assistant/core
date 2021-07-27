0b1573428a22a700eb039435d14b2602f6c79061
farmio <farmio@alphart.net>
2021-07-27 02:56:04 +0200

    use async_capture_events

--- tests/components/knx/test_events.py
+++ tests/components/knx/test_events.py
@@ -1,8 +1,11 @@
 """Test KNX events."""
+
 from homeassistant.components.knx import CONF_KNX_EVENT_FILTER
 from homeassistant.core import HomeAssistant
 
 from .conftest import KNXTestKit
+
+from tests.common import async_capture_events
 
 
 async def test_knx_event(hass: HomeAssistant, knx: KNXTestKit):
@@ -17,12 +20,10 @@ async def test_knx_event(hass: HomeAssistant, knx: KNXTestKit):
     test_address_c_1 = "2/6/4"
     test_address_c_2 = "2/6/5"
     test_address_d = "5/4/3"
-    events = []
+    events = async_capture_events(hass, "knx_event")
 
-    def listener(event):
-        events.append(event)
-
-    def test_event_data(address, payload):
+    async def test_event_data(address, payload):
+        await hass.async_block_till_done()
         assert len(events) == 1
         event = events.pop()
         assert event.data["data"] == payload
@@ -37,7 +38,6 @@ async def test_knx_event(hass: HomeAssistant, knx: KNXTestKit):
             )
         assert event.data["source"] == KNXTestKit.INDIVIDUAL_ADDRESS
 
-    hass.bus.async_listen("knx_event", listener)
     await knx.setup_integration(
         {
             CONF_KNX_EVENT_FILTER: [
@@ -50,33 +50,34 @@ async def test_knx_event(hass: HomeAssistant, knx: KNXTestKit):
     )
 
     # no event received
+    await hass.async_block_till_done()
     assert len(events) == 0
 
     # receive telegrams for group addresses matching the filter
     await knx.receive_write(test_address_a_1, True)
-    test_event_data(test_address_a_1, True)
+    await test_event_data(test_address_a_1, True)
 
     await knx.receive_response(test_address_a_2, False)
-    test_event_data(test_address_a_2, False)
+    await test_event_data(test_address_a_2, False)
 
     await knx.receive_write(test_address_b_1, (1,))
-    test_event_data(test_address_b_1, (1,))
+    await test_event_data(test_address_b_1, (1,))
 
     await knx.receive_response(test_address_b_2, (255,))
-    test_event_data(test_address_b_2, (255,))
+    await test_event_data(test_address_b_2, (255,))
 
     await knx.receive_write(test_address_c_1, (89, 43, 34, 11))
-    test_event_data(test_address_c_1, (89, 43, 34, 11))
+    await test_event_data(test_address_c_1, (89, 43, 34, 11))
 
     await knx.receive_response(test_address_c_2, (255, 255, 255, 255))
-    test_event_data(test_address_c_2, (255, 255, 255, 255))
+    await test_event_data(test_address_c_2, (255, 255, 255, 255))
 
     await knx.receive_read(test_address_d)
-    test_event_data(test_address_d, None)
+    await test_event_data(test_address_d, None)
 
     # receive telegrams for group addresses not matching the filter
-    events = []
     await knx.receive_write("0/5/0", True)
     await knx.receive_write("1/7/0", True)
     await knx.receive_write("2/6/6", True)
+    await hass.async_block_till_done()
     assert len(events) == 0
