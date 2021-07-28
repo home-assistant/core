0f6a6a0994cd4e848c88f809e17711a70b8e5ca6
Paulus Schoutsen <balloob@gmail.com>
2021-07-28 15:38:59 -0700

    Remove old sensor constants

--- homeassistant/components/enphase_envoy/const.py
+++ homeassistant/components/enphase_envoy/const.py
@@ -16,26 +16,6 @@ PLATFORMS = ["sensor"]
 COORDINATOR = "coordinator"
 NAME = "name"
 
-SENSORS = {
-    "production": ("Current Energy Production", POWER_WATT, STATE_CLASS_MEASUREMENT),
-    "daily_production": ("Today's Energy Production", ENERGY_WATT_HOUR, None),
-    "seven_days_production": (
-        "Last Seven Days Energy Production",
-        ENERGY_WATT_HOUR,
-        None,
-    ),
-    "lifetime_production": ("Lifetime Energy Production", ENERGY_WATT_HOUR, None),
-    "consumption": ("Current Energy Consumption", POWER_WATT, STATE_CLASS_MEASUREMENT),
-    "daily_consumption": ("Today's Energy Consumption", ENERGY_WATT_HOUR, None),
-    "seven_days_consumption": (
-        "Last Seven Days Energy Consumption",
-        ENERGY_WATT_HOUR,
-        None,
-    ),
-    "lifetime_consumption": ("Lifetime Energy Consumption", ENERGY_WATT_HOUR, None),
-    "inverters": ("Inverter", POWER_WATT, STATE_CLASS_MEASUREMENT),
-}
-
 SENSORS = (
     SensorEntityDescription(
         key="production",
