5ced7b2280a319c925e25d30f934c8f67b2ca2ed
Nicko van Someren <nicko@nicko.org>
2021-07-29 00:22:02 +0100

    Fix Lutron button events to have unambiguous names

--- homeassistant/components/lutron/__init__.py
+++ homeassistant/components/lutron/__init__.py
@@ -140,6 +140,8 @@ class LutronButton:
     def __init__(self, hass, area_name, keypad, button):
         """Register callback for activity on the button."""
         name = f"{keypad.name}: {button.name}"
+        if button.name == "Unknown Button":
+            name += f" {button.number}"
         self._hass = hass
         self._has_release_event = (
             button.button_type is not None and "RaiseLower" in button.button_type
@@ -150,7 +152,7 @@ class LutronButton:
         self._button_name = button.name
         self._button = button
         self._event = "lutron_event"
-        self._full_id = slugify(f"{area_name} {keypad.name}: {button.name}")
+        self._full_id = slugify(f"{area_name} {name}")
 
         button.subscribe(self.button_callback, None)
 
