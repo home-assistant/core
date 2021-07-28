8b902f4829201bd02cfc93802e113de1c8dcc248
Paulus Schoutsen <balloob@gmail.com>
2021-07-28 15:12:40 -0700

    Add last reset to enphase sensors

--- homeassistant/components/enphase_envoy/const.py
+++ homeassistant/components/enphase_envoy/const.py
@@ -1,8 +1,12 @@
 """The enphase_envoy component."""
 
 
-from homeassistant.components.sensor import STATE_CLASS_MEASUREMENT
+from homeassistant.components.sensor import (
+    STATE_CLASS_MEASUREMENT,
+    SensorEntityDescription,
+)
 from homeassistant.const import ENERGY_WATT_HOUR, POWER_WATT
+from homeassistant.util import dt
 
 DOMAIN = "enphase_envoy"
 
@@ -31,3 +35,62 @@ SENSORS = {
     "lifetime_consumption": ("Lifetime Energy Consumption", ENERGY_WATT_HOUR, None),
     "inverters": ("Inverter", POWER_WATT, STATE_CLASS_MEASUREMENT),
 }
+
+SENSORS = (
+    SensorEntityDescription(
+        key="production",
+        name="Current Energy Production",
+        unit_of_measurement=POWER_WATT,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    SensorEntityDescription(
+        key="daily_production",
+        name="Today's Energy Production",
+        unit_of_measurement=ENERGY_WATT_HOUR,
+        state_class=None,
+    ),
+    SensorEntityDescription(
+        key="seven_days_production",
+        name="Last Seven Days Energy Production",
+        unit_of_measurement=ENERGY_WATT_HOUR,
+        state_class=None,
+    ),
+    SensorEntityDescription(
+        key="lifetime_production",
+        name="Lifetime Energy Production",
+        unit_of_measurement=ENERGY_WATT_HOUR,
+        state_class=None,
+        last_reset=dt.utc_from_timestamp(0),
+    ),
+    SensorEntityDescription(
+        key="consumption",
+        name="Current Energy Consumption",
+        unit_of_measurement=POWER_WATT,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    SensorEntityDescription(
+        key="daily_consumption",
+        name="Today's Energy Consumption",
+        unit_of_measurement=ENERGY_WATT_HOUR,
+        state_class=None,
+    ),
+    SensorEntityDescription(
+        key="seven_days_consumption",
+        name="Last Seven Days Energy Consumption",
+        unit_of_measurement=ENERGY_WATT_HOUR,
+        state_class=None,
+    ),
+    SensorEntityDescription(
+        key="lifetime_consumption",
+        name="Lifetime Energy Consumption",
+        unit_of_measurement=ENERGY_WATT_HOUR,
+        state_class=None,
+        last_reset=dt.utc_from_timestamp(0),
+    ),
+    SensorEntityDescription(
+        key="inverters",
+        name="Inverter",
+        unit_of_measurement=POWER_WATT,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+)
