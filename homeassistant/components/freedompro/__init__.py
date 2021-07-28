3f7cc176a8a77130bec0073b623e5f31ba2ae34e
stefano055415 <stefano055415@gmail.com>
2021-07-28 19:27:31 +0200

    Add climate support to Freedompro (#52720)
    * Update Freedompro
    
    * fix code and add test
    
    * add check for unsupported mode
    
    * add code for unsupported hvac_mode
    
    * HVAC_INVERT_MAP and fix test
    
    * change params hass to session
    
    * set const and add ValueError
    
    * fix ValueError text

--- homeassistant/components/freedompro/__init__.py
+++ homeassistant/components/freedompro/__init__.py
@@ -14,7 +14,16 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = ["binary_sensor", "cover", "fan", "light", "lock", "sensor", "switch"]
+PLATFORMS = [
+    "binary_sensor",
+    "climate",
+    "cover",
+    "fan",
+    "light",
+    "lock",
+    "sensor",
+    "switch",
+]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
