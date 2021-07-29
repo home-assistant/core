ad8882321ae7ebe583fca7720fe4999da77e2e19
Robert Svensson <Kane610@users.noreply.github.com>
2021-07-30 01:16:08 +0200

    Add energy device class to deCONZ consumption sensors (#53731)

--- homeassistant/components/deconz/sensor.py
+++ homeassistant/components/deconz/sensor.py
@@ -23,6 +23,7 @@ from homeassistant.const import (
     ATTR_TEMPERATURE,
     ATTR_VOLTAGE,
     DEVICE_CLASS_BATTERY,
+    DEVICE_CLASS_ENERGY,
     DEVICE_CLASS_HUMIDITY,
     DEVICE_CLASS_ILLUMINANCE,
     DEVICE_CLASS_POWER,
@@ -40,6 +41,7 @@ from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
 )
+from homeassistant.util import dt as dt_util
 
 from .const import ATTR_DARK, ATTR_ON, NEW_SENSOR
 from .deconz_device import DeconzDevice
@@ -51,6 +53,7 @@ ATTR_DAYLIGHT = "daylight"
 ATTR_EVENT_ID = "event_id"
 
 DEVICE_CLASS = {
+    Consumption: DEVICE_CLASS_ENERGY,
     Humidity: DEVICE_CLASS_HUMIDITY,
     LightLevel: DEVICE_CLASS_ILLUMINANCE,
     Power: DEVICE_CLASS_POWER,
@@ -65,6 +68,7 @@ ICON = {
 }
 
 STATE_CLASS = {
+    Consumption: STATE_CLASS_MEASUREMENT,
     Humidity: STATE_CLASS_MEASUREMENT,
     Pressure: STATE_CLASS_MEASUREMENT,
     Temperature: STATE_CLASS_MEASUREMENT,
@@ -157,6 +161,9 @@ class DeconzSensor(DeconzDevice, SensorEntity):
         self._attr_icon = ICON.get(type(self._device))
         self._attr_state_class = STATE_CLASS.get(type(self._device))
         self._attr_unit_of_measurement = UNIT_OF_MEASUREMENT.get(type(self._device))
+
+        if device.type in Consumption.ZHATYPE:
+            self._attr_last_reset = dt_util.utc_from_timestamp(0)
 
     @callback
     def async_update_callback(self, force_update=False):
