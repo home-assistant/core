dbce879053ae6965cb6e93decb25d6a7965b7bd6
jan Iversen <jancasacondor@gmail.com>
2021-07-29 13:45:02 +0200

    Convert property to _attr_variable.

--- homeassistant/components/sabnzbd/sensor.py
+++ homeassistant/components/sabnzbd/sensor.py
@@ -23,13 +23,13 @@ class SabnzbdSensor(SensorEntity):
 
     def __init__(self, sensor_type, sabnzbd_api_data, client_name):
         """Initialize the sensor."""
-        self._client_name = client_name
         self._field_name = SENSOR_TYPES[sensor_type][2]
-        self._name = SENSOR_TYPES[sensor_type][0]
         self._sabnzbd_api = sabnzbd_api_data
-        self._state = None
+        self._attr_state = None
         self._type = sensor_type
-        self._unit_of_measurement = SENSOR_TYPES[sensor_type][1]
+        self._attr_unit_of_measurement = SENSOR_TYPES[sensor_type][1]
+        self._attr_name = f"{client_name} {SENSOR_TYPES[sensor_type][0]}"
+        self._attr_should_poll = False
 
     async def async_added_to_hass(self):
         """Call when entity about to be added to hass."""
@@ -39,33 +39,15 @@ class SabnzbdSensor(SensorEntity):
             )
         )
 
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return f"{self._client_name} {self._name}"
-
-    @property
-    def native_value(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def should_poll(self):
-        """Don't poll. Will be updated by dispatcher signal."""
-        return False
-
-    @property
-    def native_unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return self._unit_of_measurement
-
     def update_state(self, args):
         """Get the latest data and updates the states."""
-        self._state = self._sabnzbd_api.get_queue_field(self._field_name)
+        new_state = self._sabnzbd_api.get_queue_field(self._field_name)
 
         if self._type == "speed":
-            self._state = round(float(self._state) / 1024, 1)
+            self._attr_state = round(float(new_state) / 1024, 1)
         elif "size" in self._type:
-            self._state = round(float(self._state), 2)
+            self._attr_state = round(float(new_state), 2)
+        else:
+            self._attr_state = new_state
 
         self.schedule_update_ha_state()
