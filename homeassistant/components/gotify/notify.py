a4bc0417d60eeae8bda3b4dfdf408a1b72392f8b
Ben Marshall <ben@benmarshall.co.uk>
2021-07-30 21:15:09 +0000

    Use external library for service communication

--- homeassistant/components/gotify/notify.py
+++ homeassistant/components/gotify/notify.py
@@ -1,8 +1,7 @@
 """Gotify platform for notify component."""
-import json
 import logging
 
-import requests
+import gotify
 
 from homeassistant.components.notify import (
     ATTR_DATA,
@@ -36,39 +35,28 @@ class GotifyNotificationService(BaseNotificationService):
         """Initialize the service."""
         self.token = token
         self.url = url
+        gotify.config(
+            base_url=url,
+            app_token=token,
+        )
 
     def send_message(self, message, **kwargs):
         """Send a message."""
         data = kwargs.get(ATTR_DATA) or {}
-        token = data.get(ATTR_TOKEN, self.token)
-        title = kwargs.get(ATTR_TITLE) or {}
+        title = kwargs.get(ATTR_TITLE) or None
         priority = data.get(ATTR_PRIORITY) or 4
-        url = data.get(ATTR_URL, self.url)
         link = data.get(ATTR_LINK) or ""
 
-        headers = {"Content-Type": "application/json"}
-
-        request_url = url + "/message?token=" + token
-
-        request_dict = {
-            "message": message,
-            "priority": priority,
-            "extras": {
-                "client::display": {"contentType": "text/markdown"},
-                "client::notification": {
-                    "click": {"url": "homeassistant://navigate/" + link}
-                },
+        extras = {
+            "client::display": {"contentType": "text/markdown"},
+            "client::notification": {
+                "click": {"url": "homeassistant://navigate/" + link}
             },
         }
 
-        if title:
-            request_dict["title"] = title
-
-        request_json = json.dumps(request_dict)
-
-        resp = requests.request(
-            method="POST", url=request_url, headers=headers, data=request_json
-        )
-
-        if resp.status_code != 200:
-            _LOGGER.error("API POST Error: %s", resp.text)
+        try:
+            gotify.create_message(
+                message, title=title, priority=priority, extras=extras
+            )
+        except gotify.GotifyError as exception:
+            _LOGGER.error("Send message failed: %s", str(exception))
