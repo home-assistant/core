8c6ca78ffb1041a0f5057351e7e3d30a1e279b7e
Michael <35783820+mib1185@users.noreply.github.com>
2021-07-30 07:11:15 +0200

    Apply left suggestions #53596 for TP-Link (#53737)

--- homeassistant/components/tplink/sensor.py
+++ homeassistant/components/tplink/sensor.py
@@ -1,18 +1,32 @@
 """Support for TPLink HS100/HS110/HS200 smart switch energy sensors."""
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Final
 
 from pyHS100 import SmartPlug
 
 from homeassistant.components.sensor import (
     ATTR_LAST_RESET,
+    STATE_CLASS_MEASUREMENT,
     SensorEntity,
     SensorEntityDescription,
 )
 from homeassistant.components.tplink import SmartPlugDataUpdateCoordinator
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_ALIAS, CONF_DEVICE_ID, CONF_MAC
+from homeassistant.const import (
+    ATTR_VOLTAGE,
+    CONF_ALIAS,
+    CONF_DEVICE_ID,
+    CONF_MAC,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_VOLTAGE,
+    ELECTRIC_CURRENT_AMPERE,
+    ELECTRIC_POTENTIAL_VOLT,
+    ENERGY_KILO_WATT_HOUR,
+    POWER_WATT,
+)
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.device_registry as dr
 from homeassistant.helpers.entity import DeviceInfo
@@ -29,8 +43,50 @@ from .const import (
     CONF_SWITCH,
     COORDINATORS,
     DOMAIN as TPLINK_DOMAIN,
-    ENERGY_SENSORS,
 )
+
+ATTR_CURRENT_A = "current_a"
+ATTR_CURRENT_POWER_W = "current_power_w"
+ATTR_TODAY_ENERGY_KWH = "today_energy_kwh"
+ATTR_TOTAL_ENERGY_KWH = "total_energy_kwh"
+
+ENERGY_SENSORS: Final[list[SensorEntityDescription]] = [
+    SensorEntityDescription(
+        key=ATTR_CURRENT_POWER_W,
+        unit_of_measurement=POWER_WATT,
+        device_class=DEVICE_CLASS_POWER,
+        state_class=STATE_CLASS_MEASUREMENT,
+        name="Current Consumption",
+    ),
+    SensorEntityDescription(
+        key=ATTR_TOTAL_ENERGY_KWH,
+        unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        device_class=DEVICE_CLASS_ENERGY,
+        state_class=STATE_CLASS_MEASUREMENT,
+        name="Total Consumption",
+    ),
+    SensorEntityDescription(
+        key=ATTR_TODAY_ENERGY_KWH,
+        unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        device_class=DEVICE_CLASS_ENERGY,
+        state_class=STATE_CLASS_MEASUREMENT,
+        name="Today's Consumption",
+    ),
+    SensorEntityDescription(
+        key=ATTR_VOLTAGE,
+        unit_of_measurement=ELECTRIC_POTENTIAL_VOLT,
+        device_class=DEVICE_CLASS_VOLTAGE,
+        state_class=STATE_CLASS_MEASUREMENT,
+        name="Voltage",
+    ),
+    SensorEntityDescription(
+        key=ATTR_CURRENT_A,
+        unit_of_measurement=ELECTRIC_CURRENT_AMPERE,
+        device_class=DEVICE_CLASS_CURRENT,
+        state_class=STATE_CLASS_MEASUREMENT,
+        name="Current",
+    ),
+]
 
 
 async def async_setup_entry(
