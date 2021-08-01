0105c3ad5d0400c8e8e57ca32ed1f1d26e93eaa7
asauerwein <andreas@sauerwein.se>
2021-08-01 19:27:31 +0000

    remove obsolete comment

--- homeassistant/components/pcf8574/switch.py
+++ homeassistant/components/pcf8574/switch.py
@@ -13,7 +13,6 @@ async def async_setup_entry(
 ):
     """Set up switch from a config entry created in the integrations UI."""
     config = hass.data[DOMAIN][config_entry.entry_id]
-    # Update our config to include new repos and remove those that have been removed.
     if config_entry.options:
         config.update(config_entry.options)
 
