530f669fea39c0e12da863c3c2c64657a11fe5a5
Aidan Timson <contact@timmo.xyz>
2021-08-02 14:20:34 +0100

    Cleanup

--- homeassistant/components/system_bridge/coordinator.py
+++ homeassistant/components/system_bridge/coordinator.py
@@ -108,9 +108,7 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[Bridge]):
             if self.unsub:
                 self.unsub()
                 self.unsub = None
-            raise ConfigEntryAuthFailed(
-                f"Authentication failed for {self.title} ({self.host})"
-            ) from exception
+            raise ConfigEntryAuthFailed() from exception
         except (*BRIDGE_CONNECTION_ERRORS, ConnectionRefusedError) as exception:
             if self.unsub:
                 self.unsub()
