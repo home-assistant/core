7ca7fea69a08d9c503f24216fb12d1ad0b0fd932
RogerSelwyn <RogerSelwyn@users.noreply.github.com>
2021-08-03 16:11:48 +0100

    Add a lock state entity for cat and pet flaps

--- homeassistant/components/surepetcare/sensor.py
+++ homeassistant/components/surepetcare/sensor.py
@@ -41,6 +41,11 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
             EntityType.FELAQUA,
         ]:
             entities.append(SureBattery(surepy_entity.id, spc))
+        if surepy_entity.type in [
+            EntityType.CAT_FLAP,
+            EntityType.PET_FLAP,
+        ]:
+            entities.append(SureLock(surepy_entity.id, spc))
 
     async_add_entities(entities)
 
@@ -52,7 +57,6 @@ class SureBattery(SensorEntity):
 
     def __init__(self, _id: int, spc: SurePetcareAPI) -> None:
         """Initialize a Sure Petcare sensor."""
-
         self._id = _id
         self._spc: SurePetcareAPI = spc
 
@@ -89,6 +93,40 @@ class SureBattery(SensorEntity):
             }
         else:
             self._attr_extra_state_attributes = None
+        self.async_write_ha_state()
+        _LOGGER.debug("%s -> state: %s", self.name, state)
+
+    async def async_added_to_hass(self) -> None:
+        """Register callbacks."""
+        self.async_on_remove(
+            async_dispatcher_connect(self.hass, TOPIC_UPDATE, self._async_update)
+        )
+        self._async_update()
+
+
+class SureLock(SensorEntity):
+    """A sensor implementation for Sure Petcare Entities."""
+
+    _attr_should_poll = False
+
+    def __init__(self, _id: int, spc: SurePetcareAPI) -> None:
+        """Initialize a Sure Petcare sensor."""
+        self._id = _id
+        self._spc: SurePetcareAPI = spc
+
+        surepy_entity: SurepyEntity = self._spc.states[_id]
+
+        self._attr_device_class = None
+        self._attr_name = f"{surepy_entity.type.name.capitalize()} {surepy_entity.name.capitalize()} Lock"
+        self._attr_unique_id = f"{surepy_entity.household_id}-{surepy_entity.id}-lock"
+
+    @callback
+    def _async_update(self) -> None:
+        """Get the latest data and update the state."""
+        surepy_entity = self._spc.states[self._id]
+        state = surepy_entity.state
+        self._attr_state = state
+
         self.async_write_ha_state()
         _LOGGER.debug("%s -> state: %s", self.name, state)
 
