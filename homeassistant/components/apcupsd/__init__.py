fd66f514979699aa52f9c44ed1cc20db18b4c346
mib1185 <mail@mib85.de>
2021-08-05 16:45:46 +0000

    Add SCAN_INTERVAL option

--- homeassistant/components/apcupsd/__init__.py
+++ homeassistant/components/apcupsd/__init__.py
@@ -5,7 +5,7 @@ import logging
 from apcaccess import status
 import voluptuous as vol
 
-from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_SCAN_INTERVAL
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util import Throttle
 
@@ -13,11 +13,10 @@ _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_HOST = "localhost"
 DEFAULT_PORT = 3551
+DEFAULT_SCAN_INTERVAL = 60
 DOMAIN = "apcupsd"
 
 KEY_STATUS = "STATFLAG"
-
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
 
 VALUE_ONLINE = 8
 
@@ -27,6 +26,9 @@ CONFIG_SCHEMA = vol.Schema(
             {
                 vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
                 vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+                vol.Optional(
+                    CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL
+                ): cv.Number,
             }
         )
     },
@@ -39,8 +41,9 @@ def setup(hass, config):
     conf = config[DOMAIN]
     host = conf[CONF_HOST]
     port = conf[CONF_PORT]
+    scan_interval = conf[CONF_SCAN_INTERVAL]
 
-    apcups_data = APCUPSdData(host, port)
+    apcups_data = APCUPSdData(host, port, scan_interval)
     hass.data[DOMAIN] = apcups_data
 
     # It doesn't really matter why we're not able to get the status, just that
@@ -60,11 +63,14 @@ class APCUPSdData:
     updates from the server.
     """
 
-    def __init__(self, host, port):
+    _scan_interval = int()
+
+    def __init__(self, host, port, scan_interval):
         """Initialize the data object."""
 
         self._host = host
         self._port = port
+        self._scan_interval = scan_interval
         self._status = None
         self._get = status.get
         self._parse = status.parse
@@ -79,7 +85,7 @@ class APCUPSdData:
         """Get the status from APCUPSd and parse it into a dict."""
         return self._parse(self._get(host=self._host, port=self._port))
 
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    @Throttle(timedelta(seconds=_scan_interval))
     def update(self, **kwargs):
         """Fetch the latest status from APCUPSd."""
         self._status = self._get_status()
