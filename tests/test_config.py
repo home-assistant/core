a835155c47285d2c407c42ac8919872e0e54d09e
Paulus Schoutsen <balloob@gmail.com>
2021-08-05 13:11:01 -0700

    Packages to support config platforms (#54085)

--- tests/test_config.py
+++ tests/test_config.py
@@ -650,19 +650,30 @@ async def test_merge(merge_log_err, hass):
         "pack_list": {"light": {"platform": "test"}},
         "pack_list2": {"light": [{"platform": "test"}]},
         "pack_none": {"wake_on_lan": None},
+        "pack_special": {
+            "automation": [{"some": "yay"}],
+            "script": {"a_script": "yay"},
+            "template": [{"some": "yay"}],
+        },
     }
     config = {
         config_util.CONF_CORE: {config_util.CONF_PACKAGES: packages},
         "input_boolean": {"ib2": None},
         "light": {"platform": "test"},
+        "automation": [],
+        "script": {},
+        "template": [],
     }
     await config_util.merge_packages_config(hass, config, packages)
 
     assert merge_log_err.call_count == 0
-    assert len(config) == 5
+    assert len(config) == 8
     assert len(config["input_boolean"]) == 2
     assert len(config["input_select"]) == 1
     assert len(config["light"]) == 3
+    assert len(config["automation"]) == 1
+    assert len(config["script"]) == 1
+    assert len(config["template"]) == 1
     assert isinstance(config["wake_on_lan"], OrderedDict)
 
 
