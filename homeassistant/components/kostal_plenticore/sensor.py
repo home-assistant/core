cd5d4d5ef08609adaa4d2b3fdbf74d567c545de1
Gian Klug <51193103+gianklug@users.noreply.github.com>
2021-08-06 16:41:51 +0200

    Import datetime

--- homeassistant/components/kostal_plenticore/sensor.py
+++ homeassistant/components/kostal_plenticore/sensor.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from datetime import timedelta
 import logging
+import datetime
 from typing import Any, Callable
 
 from homeassistant.components.sensor import (
