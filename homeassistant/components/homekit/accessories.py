4c21422cf6ed35e414a2773668ab0caaf774adeb
Niccolo Zapponi <nzapponi@users.noreply.github.com>
2021-08-06 17:34:42 +0100

    Handle software version being None when setting up HomeKit accessories  (#54130)
    * Convert all HomeKit service info to string prior to checking for max length
    
    * Added check for None software version
    
    * Added test case for numeric version number
    
    * Update tests/components/homekit/test_accessories.py
    
    Co-authored-by: J. Nick Koston <nick@koston.org>
    
    * Fix style & none version test
    
    * Fix test
    
    * revert other change since it should be covered by the format_sw_version fix
    
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/homekit/accessories.py
+++ homeassistant/components/homekit/accessories.py
@@ -238,9 +238,10 @@ class HomeAccessory(Accessory):
             model = self.config[ATTR_MODEL]
         else:
             model = domain.title()
+        sw_version = None
         if self.config.get(ATTR_SW_VERSION) is not None:
             sw_version = format_sw_version(self.config[ATTR_SW_VERSION])
-        else:
+        if sw_version is None:
             sw_version = __version__
 
         self.set_info_service(
