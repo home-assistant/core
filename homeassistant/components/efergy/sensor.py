0c5599878894d919619cdf037591d1defcce16e9
Robert Hillis <tkdrob4390@yahoo.com>
2021-08-07 08:30:02 -0400

    Rework efergy for energy management

--- homeassistant/components/efergy/sensor.py
+++ homeassistant/components/efergy/sensor.py
@@ -1,14 +1,25 @@
 """Support for Efergy sensors."""
+from datetime import datetime, timedelta
 import logging
 
 import requests
 import voluptuous as vol
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+from homeassistant.components.sensor import (
+    ATTR_STATE_CLASS,
+    PLATFORM_SCHEMA,
+    STATE_CLASS_MEASUREMENT,
+    SensorEntity,
+)
 from homeassistant.const import (
+    ATTR_DEVICE_CLASS,
+    ATTR_NAME,
+    ATTR_UNIT_OF_MEASUREMENT,
     CONF_CURRENCY,
     CONF_MONITORED_VARIABLES,
     CONF_TYPE,
+    DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_POWER,
     ENERGY_KILO_WATT_HOUR,
     POWER_WATT,
 )
@@ -32,11 +43,26 @@ DEFAULT_PERIOD = "year"
 DEFAULT_UTC_OFFSET = "0"
 
 SENSOR_TYPES = {
-    CONF_INSTANT: ["Energy Usage", POWER_WATT],
-    CONF_AMOUNT: ["Energy Consumed", ENERGY_KILO_WATT_HOUR],
-    CONF_BUDGET: ["Energy Budget", None],
-    CONF_COST: ["Energy Cost", None],
-    CONF_CURRENT_VALUES: ["Per-Device Usage", POWER_WATT],
+    CONF_INSTANT: {
+        ATTR_NAME: "Energy Usage",
+        ATTR_UNIT_OF_MEASUREMENT: POWER_WATT,
+        ATTR_DEVICE_CLASS: DEVICE_CLASS_POWER,
+        ATTR_STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
+    CONF_AMOUNT: {
+        ATTR_NAME: "Energy Consumed",
+        ATTR_UNIT_OF_MEASUREMENT: ENERGY_KILO_WATT_HOUR,
+        ATTR_DEVICE_CLASS: DEVICE_CLASS_ENERGY,
+        ATTR_STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
+    CONF_BUDGET: {ATTR_NAME: "Energy Budget"},
+    CONF_COST: {ATTR_NAME: "Energy Cost"},
+    CONF_CURRENT_VALUES: {
+        ATTR_NAME: "Per-Device Usage",
+        ATTR_UNIT_OF_MEASUREMENT: POWER_WATT,
+        ATTR_DEVICE_CLASS: DEVICE_CLASS_POWER,
+        ATTR_STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
 }
 
 TYPES_SCHEMA = vol.In(SENSOR_TYPES)
@@ -99,35 +125,21 @@ class EfergySensor(SensorEntity):
     def __init__(self, sensor_type, app_token, utc_offset, period, currency, sid=None):
         """Initialize the sensor."""
         self.sid = sid
+        sensor = SENSOR_TYPES[sensor_type]
         if sid:
-            self._name = f"efergy_{sid}"
+            self._attr_name = f"efergy_{sid}"
         else:
-            self._name = SENSOR_TYPES[sensor_type][0]
+            self._attr_name = sensor.get(ATTR_NAME)
         self.type = sensor_type
         self.app_token = app_token
         self.utc_offset = utc_offset
-        self._state = None
         self.period = period
-        self.currency = currency
-        if self.type == "cost":
-            self._unit_of_measurement = f"{self.currency}/{self.period}"
+        if sensor_type == "cost":
+            self._attr_unit_of_measurement = f"{currency}/{period}"
         else:
-            self._unit_of_measurement = SENSOR_TYPES[sensor_type][1]
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return self._unit_of_measurement
+            self._attr_unit_of_measurement = sensor.get(ATTR_UNIT_OF_MEASUREMENT)
+        self._attr_device_class = sensor.get(ATTR_DEVICE_CLASS)
+        self._attr_state_class = sensor.get(ATTR_STATE_CLASS)
 
     def update(self):
         """Get the Efergy monitor data from the web service."""
@@ -135,19 +147,27 @@ class EfergySensor(SensorEntity):
             if self.type == "instant_readings":
                 url_string = f"{_RESOURCE}getInstant?token={self.app_token}"
                 response = requests.get(url_string, timeout=10)
-                self._state = response.json()["reading"]
+                self._attr_state = response.json()["reading"]
             elif self.type == "amount":
                 url_string = f"{_RESOURCE}getEnergy?token={self.app_token}&offset={self.utc_offset}&period={self.period}"
                 response = requests.get(url_string, timeout=10)
-                self._state = response.json()["sum"]
+                self._attr_state = response.json()["sum"]
+                last_reset = datetime.now() - timedelta(
+                    seconds=int(response.json()["duration"])
+                )
+                last_reset -= timedelta(
+                    seconds=last_reset.second, microseconds=last_reset.microsecond
+                )
+                self._attr_last_reset = last_reset
+
             elif self.type == "budget":
                 url_string = f"{_RESOURCE}getBudget?token={self.app_token}"
                 response = requests.get(url_string, timeout=10)
-                self._state = response.json()["status"]
+                self._attr_state = response.json()["status"]
             elif self.type == "cost":
                 url_string = f"{_RESOURCE}getCost?token={self.app_token}&offset={self.utc_offset}&period={self.period}"
                 response = requests.get(url_string, timeout=10)
-                self._state = response.json()["sum"]
+                self._attr_state = response.json()["sum"]
             elif self.type == "current_values":
                 url_string = (
                     f"{_RESOURCE}getCurrentValuesSummary?token={self.app_token}"
@@ -156,8 +176,8 @@ class EfergySensor(SensorEntity):
                 for sensor in response.json():
                     if self.sid == sensor["sid"]:
                         measurement = next(iter(sensor["data"][0].values()))
-                        self._state = measurement
+                        self._attr_state = measurement
             else:
-                self._state = None
+                self._attr_state = None
         except (requests.RequestException, ValueError, KeyError):
             _LOGGER.warning("Could not update status for %s", self.name)
