507ca111c0898be517d3b512c5504efe6717ea6d
abmantis <amfcalt@gmail.com>
2021-08-08 01:07:16 +0000

    Do real integration setup in tests

--- tests/components/whirlpool/test_climate.py
+++ tests/components/whirlpool/test_climate.py
@@ -1,194 +1,283 @@
 """Test the Whirlpool Sixth Sense climate domain."""
 from unittest import mock
-from unittest.mock import AsyncMock, MagicMock, Mock
+from unittest.mock import AsyncMock, MagicMock
 
-import pytest
+import aiohttp
 import whirlpool
 
 from homeassistant.components.climate.const import (
+    ATTR_CURRENT_HUMIDITY,
+    ATTR_CURRENT_TEMPERATURE,
+    ATTR_FAN_MODE,
+    ATTR_FAN_MODES,
+    ATTR_HVAC_MODE,
+    ATTR_HVAC_MODES,
+    ATTR_MAX_TEMP,
+    ATTR_MIN_TEMP,
+    ATTR_SWING_MODE,
+    ATTR_SWING_MODES,
+    ATTR_TARGET_TEMP_STEP,
+    DOMAIN as CLIMATE_DOMAIN,
     FAN_AUTO,
     FAN_HIGH,
     FAN_LOW,
     FAN_MEDIUM,
     FAN_MIDDLE,
     FAN_OFF,
+    HVAC_MODE_AUTO,
     HVAC_MODE_COOL,
     HVAC_MODE_DRY,
     HVAC_MODE_FAN_ONLY,
     HVAC_MODE_HEAT,
     HVAC_MODE_OFF,
+    SERVICE_SET_FAN_MODE,
+    SERVICE_SET_HVAC_MODE,
+    SERVICE_SET_SWING_MODE,
+    SERVICE_SET_TEMPERATURE,
     SUPPORT_FAN_MODE,
     SUPPORT_SWING_MODE,
     SUPPORT_TARGET_TEMPERATURE,
     SWING_HORIZONTAL,
     SWING_OFF,
 )
-from homeassistant.components.whirlpool.climate import AirConEntity, async_setup_entry
-from homeassistant.components.whirlpool.const import DOMAIN
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    ATTR_FRIENDLY_NAME,
+    ATTR_SUPPORTED_FEATURES,
+    ATTR_TEMPERATURE,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+    STATE_UNAVAILABLE,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from . import init_integration
 
 
-@pytest.fixture(name="aircon_api")
-def aircon_api_fixture():
-    """Set up air conditioner API fixture."""
-    with mock.patch(
-        "homeassistant.components.whirlpool.climate.Aircon"
-    ) as mock_aircon_api:
-        yield mock_aircon_api
+async def update_ac_state(hass: HomeAssistant, mock_aircon_api: MagicMock):
+    """Simulate an update trigger from the API."""
+    schedule_update_ha_state_cb = mock_aircon_api.call_args.args[2]
+    schedule_update_ha_state_cb()
+    await hass.async_block_till_done()
+    return hass.states.get("climate.said1")
 
 
-@pytest.fixture(name="aircon_entity")
-def aircon_entity_fixture(aircon_api):
-    """Set up whirlpool air conditioner entity."""
-    auth = Mock()
-    return AirConEntity("TEST-SAID", auth)
-
-
-async def test_async_setup_entry(hass):
-    """Test the setup of the climate entities."""
-    auth_mock = Mock()
-    auth_mock.get_said_list.return_value = ["SAID1"]
-    async_add_entities_mock = MagicMock()
-    config_mock = MagicMock(
-        data={
-            "entry_id": "0123456789",
-        }
-    )
-    hass.data[DOMAIN] = {config_mock.entry_id: {"auth": auth_mock}}
-
-    await async_setup_entry(hass, config_mock, async_add_entities_mock)
-    async_add_entities_mock.assert_called_once()
-    entities = async_add_entities_mock.call_args.args[0]
-    assert len(entities) == 1
-    assert entities[0].unique_id == "SAID1"
-
-
-async def test_async_setup_entry_no_appliances(hass):
+async def test_no_appliances(hass: HomeAssistant, mock_auth_api: MagicMock):
     """Test the setup of the climate entities when there are no appliances available."""
-    auth_mock = Mock()
-    auth_mock.get_said_list.return_value = []
-    async_add_entities_mock = MagicMock()
-    config_mock = MagicMock(
-        data={
-            "entry_id": "0123456789",
-        }
+    mock_auth_api.return_value.get_said_list.return_value = []
+    await init_integration(hass)
+    assert len(hass.states.async_all()) == 0
+
+
+async def test_name_fallback_on_exception(
+    hass: HomeAssistant, mock_aircon_api: MagicMock
+):
+    """Test name property."""
+    mock_aircon_api.return_value.fetch_name = AsyncMock(
+        side_effect=aiohttp.ClientError()
     )
-    hass.data[DOMAIN] = {config_mock.entry_id: {"auth": auth_mock}}
 
-    await async_setup_entry(hass, config_mock, async_add_entities_mock)
-    async_add_entities_mock.assert_not_called()
-
-
-async def test_name(aircon_entity, aircon_api):
-    """Test name property."""
-    aircon_api.return_value.connect = AsyncMock()
-    aircon_api.return_value.fetch_name = AsyncMock(return_value="TestZone")
-
-    assert aircon_entity.name == "TEST-SAID"
-    await aircon_entity.async_added_to_hass()
-    assert aircon_entity.name == "TestZone"
+    await init_integration(hass)
+    state = hass.states.get("climate.said1")
+    assert state.attributes.get(ATTR_FRIENDLY_NAME) == "said1"
 
 
-async def test_unique_id(aircon_entity, aircon_api):
-    """Test name property."""
-    aircon_api.return_value.said = "TEST-SAID-123"
-    assert aircon_entity.unique_id == "TEST-SAID-123"
+async def test_static_attributes(hass: HomeAssistant, mock_aircon_api: MagicMock):
+    """Test static climate attributes."""
+    await init_integration(hass)
 
+    entry = er.async_get(hass).async_get("climate.said1")
+    assert entry
+    assert entry.unique_id == "said1"
 
-async def test_static_attributes(aircon_entity: AirConEntity, aircon_api):
-    """Test getting attributes."""
+    state = hass.states.get("climate.said1")
+    assert state is not None
+    assert state.state != STATE_UNAVAILABLE
+    assert state.state == HVAC_MODE_COOL
+    assert state.attributes.get(ATTR_FRIENDLY_NAME) == "TestZone"
+
     assert (
-        aircon_entity.supported_features
+        state.attributes.get(ATTR_SUPPORTED_FEATURES)
         == SUPPORT_TARGET_TEMPERATURE | SUPPORT_FAN_MODE | SUPPORT_SWING_MODE
     )
-    assert aircon_entity.hvac_modes == [
+    assert state.attributes.get(ATTR_HVAC_MODES) == [
         HVAC_MODE_COOL,
         HVAC_MODE_HEAT,
         HVAC_MODE_FAN_ONLY,
         HVAC_MODE_OFF,
     ]
-    assert aircon_entity.fan_modes == [FAN_AUTO, FAN_HIGH, FAN_MEDIUM, FAN_LOW, FAN_OFF]
-    assert aircon_entity.swing_modes == [SWING_HORIZONTAL, SWING_OFF]
-    assert aircon_entity.temperature_unit == TEMP_CELSIUS
-    assert aircon_entity.target_temperature_step == 1
-    assert aircon_entity.target_humidity_step == 10
-    assert aircon_entity.min_temp == 16
-    assert aircon_entity.max_temp == 30
+    assert state.attributes.get(ATTR_FAN_MODES) == [
+        FAN_AUTO,
+        FAN_HIGH,
+        FAN_MEDIUM,
+        FAN_LOW,
+        FAN_OFF,
+    ]
+    print(state.attributes)
+    assert state.attributes.get(ATTR_SWING_MODES) == [SWING_HORIZONTAL, SWING_OFF]
+    assert state.attributes.get(ATTR_TARGET_TEMP_STEP) == 1
+    assert state.attributes.get(ATTR_MIN_TEMP) == 16
+    assert state.attributes.get(ATTR_MAX_TEMP) == 30
 
 
-async def test_get_attributes(aircon_entity: AirConEntity, aircon_api):
-    """Test getting attributes."""
-    aircon_api.return_value.get_online.return_value = False
-    aircon_api.return_value.get_power_on.return_value = False
-    assert not aircon_entity.available
-    assert aircon_entity.hvac_mode == HVAC_MODE_OFF
+async def test_dynamic_attributes(hass: HomeAssistant, mock_aircon_api: MagicMock):
+    """Test dynamic attributes."""
+    await init_integration(hass)
 
-    aircon_api.return_value.get_online.return_value = True
-    assert aircon_entity.available
-    assert aircon_entity.hvac_mode == HVAC_MODE_OFF
+    state = hass.states.get("climate.said1")
+    assert state is not None
+    assert state.state == HVAC_MODE_COOL
 
-    aircon_api.return_value.get_power_on.return_value = True
+    mock_aircon_api.return_value.get_power_on.return_value = False
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.state == HVAC_MODE_OFF
 
-    aircon_api.return_value.get_mode.return_value = whirlpool.aircon.Mode.Cool
-    assert aircon_entity.hvac_mode == HVAC_MODE_COOL
-    aircon_api.return_value.get_mode.return_value = whirlpool.aircon.Mode.Heat
-    assert aircon_entity.hvac_mode == HVAC_MODE_HEAT
-    aircon_api.return_value.get_mode.return_value = whirlpool.aircon.Mode.Fan
-    assert aircon_entity.hvac_mode == HVAC_MODE_FAN_ONLY
+    mock_aircon_api.return_value.get_online.return_value = False
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.state == STATE_UNAVAILABLE
 
-    aircon_api.return_value.get_fanspeed.return_value = whirlpool.aircon.FanSpeed.Auto
-    assert aircon_entity.fan_mode == FAN_AUTO
-    aircon_api.return_value.get_fanspeed.return_value = whirlpool.aircon.FanSpeed.Low
-    assert aircon_entity.fan_mode == FAN_LOW
-    aircon_api.return_value.get_fanspeed.return_value = whirlpool.aircon.FanSpeed.Medium
-    assert aircon_entity.fan_mode == FAN_MEDIUM
-    aircon_api.return_value.get_fanspeed.return_value = whirlpool.aircon.FanSpeed.High
-    assert aircon_entity.fan_mode == FAN_HIGH
-    aircon_api.return_value.get_fanspeed.return_value = whirlpool.aircon.FanSpeed.Off
-    assert aircon_entity.fan_mode == FAN_OFF
+    mock_aircon_api.return_value.get_power_on.return_value = True
+    mock_aircon_api.return_value.get_online.return_value = True
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.state == HVAC_MODE_COOL
 
-    aircon_api.return_value.get_current_temp.return_value = 15
-    aircon_api.return_value.get_temp.return_value = 20
-    aircon_api.return_value.get_current_humidity.return_value = 80
-    aircon_api.return_value.get_humidity.return_value = 50
-    aircon_api.return_value.get_h_louver_swing.return_value = True
-    assert aircon_entity.current_temperature == 15
-    assert aircon_entity.target_temperature == 20
-    assert aircon_entity.current_humidity == 80
-    assert aircon_entity.target_humidity == 50
-    assert aircon_entity.swing_mode == SWING_HORIZONTAL
+    mock_aircon_api.return_value.get_mode.return_value = whirlpool.aircon.Mode.Heat
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.state == HVAC_MODE_HEAT
 
-    aircon_api.return_value.get_current_temp.return_value = 16
-    aircon_api.return_value.get_temp.return_value = 21
-    aircon_api.return_value.get_current_humidity.return_value = 70
-    aircon_api.return_value.get_humidity.return_value = 55
-    aircon_api.return_value.get_h_louver_swing.return_value = False
-    assert aircon_entity.current_temperature == 16
-    assert aircon_entity.target_temperature == 21
-    assert aircon_entity.current_humidity == 70
-    assert aircon_entity.target_humidity == 55
-    assert aircon_entity.swing_mode == SWING_OFF
+    mock_aircon_api.return_value.get_mode.return_value = whirlpool.aircon.Mode.Fan
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.state == HVAC_MODE_FAN_ONLY
+
+    mock_aircon_api.return_value.get_fanspeed.return_value = (
+        whirlpool.aircon.FanSpeed.Auto
+    )
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.attributes.get(ATTR_FAN_MODE) == HVAC_MODE_AUTO
+
+    mock_aircon_api.return_value.get_fanspeed.return_value = (
+        whirlpool.aircon.FanSpeed.Low
+    )
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.attributes.get(ATTR_FAN_MODE) == FAN_LOW
+
+    mock_aircon_api.return_value.get_fanspeed.return_value = (
+        whirlpool.aircon.FanSpeed.Medium
+    )
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.attributes.get(ATTR_FAN_MODE) == FAN_MEDIUM
+
+    mock_aircon_api.return_value.get_fanspeed.return_value = (
+        whirlpool.aircon.FanSpeed.High
+    )
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.attributes.get(ATTR_FAN_MODE) == FAN_HIGH
+
+    mock_aircon_api.return_value.get_fanspeed.return_value = (
+        whirlpool.aircon.FanSpeed.Off
+    )
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.attributes.get(ATTR_FAN_MODE) == FAN_OFF
+
+    mock_aircon_api.return_value.get_current_temp.return_value = 15
+    mock_aircon_api.return_value.get_temp.return_value = 20
+    mock_aircon_api.return_value.get_current_humidity.return_value = 80
+    mock_aircon_api.return_value.get_h_louver_swing.return_value = True
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.attributes.get(ATTR_CURRENT_TEMPERATURE) == 15
+    assert state.attributes.get(ATTR_TEMPERATURE) == 20
+    assert state.attributes.get(ATTR_CURRENT_HUMIDITY) == 80
+    assert state.attributes.get(ATTR_SWING_MODE) == SWING_HORIZONTAL
+
+    mock_aircon_api.return_value.get_current_temp.return_value = 16
+    mock_aircon_api.return_value.get_temp.return_value = 21
+    mock_aircon_api.return_value.get_current_humidity.return_value = 70
+    mock_aircon_api.return_value.get_h_louver_swing.return_value = False
+    state = await update_ac_state(hass, mock_aircon_api)
+    assert state.attributes.get(ATTR_CURRENT_TEMPERATURE) == 16
+    assert state.attributes.get(ATTR_TEMPERATURE) == 21
+    assert state.attributes.get(ATTR_CURRENT_HUMIDITY) == 70
+    assert state.attributes.get(ATTR_SWING_MODE) == SWING_OFF
 
 
-async def test_set_attributes(aircon_entity: AirConEntity, aircon_api):
-    """Test setting attributes."""
-    aircon_api.return_value.set_temp = AsyncMock()
-    aircon_api.return_value.set_humidity = AsyncMock()
-    aircon_api.return_value.set_mode = AsyncMock()
-    aircon_api.return_value.set_fanspeed = AsyncMock()
-    aircon_api.return_value.set_h_louver_swing = AsyncMock()
+async def test_service_calls(hass: HomeAssistant, mock_aircon_api: MagicMock):
+    """Test controlling the entity through service calls."""
+    await init_integration(hass)
+    mock_aircon_api.return_value.set_power_on = AsyncMock()
+    mock_aircon_api.return_value.set_mode = AsyncMock()
+    mock_aircon_api.return_value.set_temp = AsyncMock()
+    mock_aircon_api.return_value.set_humidity = AsyncMock()
+    mock_aircon_api.return_value.set_mode = AsyncMock()
+    mock_aircon_api.return_value.set_fanspeed = AsyncMock()
+    mock_aircon_api.return_value.set_h_louver_swing = AsyncMock()
 
-    await aircon_entity.async_set_temperature(temperature=15)
-    aircon_api.return_value.set_temp.assert_called_once_with(15)
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_TURN_OFF,
+        {ATTR_ENTITY_ID: "climate.said1"},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: "climate.said1"},
+        blocking=True,
+    )
+    mock_aircon_api.return_value.set_power_on.assert_has_calls(
+        [mock.call(False), mock.call(True)]
+    )
 
-    await aircon_entity.async_set_humidity(80)
-    aircon_api.return_value.set_humidity.assert_called_once_with(80)
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_TURN_OFF,
+        {ATTR_ENTITY_ID: "climate.said1"},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_HVAC_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_HVAC_MODE: HVAC_MODE_COOL},
+        blocking=True,
+    )
+    mock_aircon_api.return_value.set_power_on.assert_has_calls(
+        [mock.call(False), mock.call(True)]
+    )
 
-    await aircon_entity.async_set_hvac_mode(HVAC_MODE_COOL)
-    await aircon_entity.async_set_hvac_mode(HVAC_MODE_HEAT)
-    await aircon_entity.async_set_hvac_mode(HVAC_MODE_DRY)  # this should be ignored
-    await aircon_entity.async_set_hvac_mode(HVAC_MODE_FAN_ONLY)
-    aircon_api.return_value.set_mode.assert_has_calls(
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_TEMPERATURE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_TEMPERATURE: 15},
+        blocking=True,
+    )
+    mock_aircon_api.return_value.set_temp.assert_called_once_with(15)
+
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_HVAC_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_HVAC_MODE: HVAC_MODE_COOL},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_HVAC_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_HVAC_MODE: HVAC_MODE_HEAT},
+        blocking=True,
+    )
+    # HVAC_MODE_DRY should be ignored
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_HVAC_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_HVAC_MODE: HVAC_MODE_DRY},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_HVAC_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_HVAC_MODE: HVAC_MODE_FAN_ONLY},
+        blocking=True,
+    )
+    mock_aircon_api.return_value.set_mode.assert_has_calls(
         [
             mock.call(whirlpool.aircon.Mode.Cool),
             mock.call(whirlpool.aircon.Mode.Heat),
@@ -196,12 +285,39 @@ async def test_set_attributes(aircon_entity: AirConEntity, aircon_api):
         ]
     )
 
-    await aircon_entity.async_set_fan_mode(FAN_AUTO)
-    await aircon_entity.async_set_fan_mode(FAN_LOW)
-    await aircon_entity.async_set_fan_mode(FAN_MEDIUM)
-    await aircon_entity.async_set_fan_mode(FAN_MIDDLE)  # this should be ignored
-    await aircon_entity.async_set_fan_mode(FAN_HIGH)
-    aircon_api.return_value.set_fanspeed.assert_has_calls(
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_FAN_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_FAN_MODE: FAN_AUTO},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_FAN_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_FAN_MODE: FAN_LOW},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_FAN_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_FAN_MODE: FAN_MEDIUM},
+        blocking=True,
+    )
+    # FAN_MIDDLE should be ignored
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_FAN_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_FAN_MODE: FAN_MIDDLE},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_FAN_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_FAN_MODE: FAN_HIGH},
+        blocking=True,
+    )
+
+    mock_aircon_api.return_value.set_fanspeed.assert_has_calls(
         [
             mock.call(whirlpool.aircon.FanSpeed.Auto),
             mock.call(whirlpool.aircon.FanSpeed.Low),
@@ -210,27 +326,18 @@ async def test_set_attributes(aircon_entity: AirConEntity, aircon_api):
         ]
     )
 
-    await aircon_entity.async_set_swing_mode(SWING_HORIZONTAL)
-    await aircon_entity.async_set_swing_mode(SWING_OFF)
-    aircon_api.return_value.set_h_louver_swing.assert_has_calls(
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_SWING_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_SWING_MODE: SWING_HORIZONTAL},
+        blocking=True,
+    )
+    await hass.services.async_call(
+        CLIMATE_DOMAIN,
+        SERVICE_SET_SWING_MODE,
+        {ATTR_ENTITY_ID: "climate.said1", ATTR_SWING_MODE: SWING_OFF},
+        blocking=True,
+    )
+    mock_aircon_api.return_value.set_h_louver_swing.assert_has_calls(
         [mock.call(True), mock.call(False)]
-    )
-
-
-async def test_on_off(aircon_entity: AirConEntity, aircon_api):
-    """Test turning on and off."""
-    aircon_api.return_value.set_power_on = AsyncMock()
-    aircon_api.return_value.set_mode = AsyncMock()
-
-    await aircon_entity.async_turn_off()
-    await aircon_entity.async_turn_on()
-    aircon_api.return_value.set_power_on.assert_has_calls(
-        [mock.call(False), mock.call(True)]
-    )
-
-    aircon_api.return_value.get_power_on.return_value = False
-    await aircon_entity.async_set_hvac_mode(HVAC_MODE_OFF)
-    await aircon_entity.async_set_hvac_mode(HVAC_MODE_COOL)
-    aircon_api.return_value.set_power_on.assert_has_calls(
-        [mock.call(False), mock.call(True)]
     )
--- tests/components/whirlpool/test_init.py
+++ tests/components/whirlpool/test_init.py
@@ -1,83 +1,49 @@
 """Test the Whirlpool Sixth Sense init."""
-from unittest import mock
-from unittest.mock import AsyncMock, Mock
-from uuid import uuid4
+from unittest.mock import AsyncMock, MagicMock
 
 import aiohttp
-import pytest
 
-from homeassistant.components.whirlpool import async_setup_entry, async_unload_entry
 from homeassistant.components.whirlpool.const import DOMAIN
-from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.config_entries import ConfigEntryState
+from homeassistant.core import HomeAssistant
 
-from tests.common import MockConfigEntry
+from tests.components.whirlpool import init_integration
 
 
-async def init_integration(hass, entry_id):
-    """Do integration initialization."""
-    config_entry_mock = MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            "username": "user@mail.com",
-            "password": "pass",
-        },
-        entry_id=entry_id,
+async def test_setup(hass: HomeAssistant):
+    """Test setup."""
+    entry = await init_integration(hass)
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+    assert entry.state is ConfigEntryState.LOADED
+
+
+async def test_setup_http_exception(hass: HomeAssistant, mock_auth_api: MagicMock):
+    """Test setup with an http exception."""
+    mock_auth_api.return_value.do_auth = AsyncMock(
+        side_effect=aiohttp.ClientConnectionError()
     )
-    return await async_setup_entry(hass, config_entry_mock)
+    entry = await init_integration(hass)
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+    assert entry.state is ConfigEntryState.SETUP_RETRY
 
 
-@pytest.fixture(name="auth_api")
-def auth_api_fixture():
-    """Set up auth API fixture."""
-    with mock.patch("homeassistant.components.whirlpool.Auth") as mock_auth_api:
-        yield mock_auth_api
+async def test_setup_auth_failed(hass: HomeAssistant, mock_auth_api: MagicMock):
+    """Test setup with failed auth."""
+    mock_auth_api.return_value.do_auth = AsyncMock()
+    mock_auth_api.return_value.is_access_token_valid.return_value = False
+    entry = await init_integration(hass)
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+    assert entry.state is ConfigEntryState.SETUP_RETRY
 
 
-async def test_setup_entry(hass, auth_api):
-    """Test setup entry."""
-    auth_api.return_value.do_auth = AsyncMock()
-    auth_api.return_value.is_access_token_valid.return_value = True
+async def test_unload_entry(hass: HomeAssistant):
+    """Test successful unload of entry."""
+    entry = await init_integration(hass)
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+    assert entry.state is ConfigEntryState.LOADED
 
-    entry_id = str(uuid4())
-    assert await init_integration(hass, entry_id)
-    auth_api.return_value.do_auth.assert_called_once_with(store=False)
-    assert hass.data[DOMAIN][entry_id]["auth"] is not None
+    assert await hass.config_entries.async_unload(entry.entry_id)
+    await hass.async_block_till_done()
 
-
-async def test_setup_entry_http_exception(hass, auth_api):
-    """Test setup entry."""
-    auth_api.return_value.do_auth = AsyncMock(side_effect=aiohttp.ClientConnectionError)
-
-    entry_id = str(uuid4())
-    with pytest.raises(ConfigEntryNotReady):
-        await init_integration(hass, entry_id)
-    assert entry_id not in hass.data[DOMAIN]
-
-
-async def test_setup_entry_auth_failed(hass, auth_api):
-    """Test setup entry."""
-    auth_api.return_value.do_auth = AsyncMock()
-    auth_api.return_value.is_access_token_valid.return_value = False
-
-    entry_id = str(uuid4())
-    with pytest.raises(ConfigEntryNotReady):
-        await init_integration(hass, entry_id)
-    assert entry_id not in hass.data[DOMAIN]
-
-
-async def test_unload_entry(hass):
-    """Test being able to unload an entry."""
-    entry_id = str(uuid4())
-    config_entry_mock = MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            "username": "user@mail.com",
-            "password": "pass",
-        },
-        entry_id=entry_id,
-    )
-    # Put random content at the location where the client should have been placed by setup
-    hass.data.setdefault(DOMAIN, {})[entry_id] = {"auth": Mock}
-
-    await async_unload_entry(hass, config_entry_mock)
-    assert entry_id not in hass.data[DOMAIN]
+    assert entry.state is ConfigEntryState.NOT_LOADED
+    assert not hass.data.get(DOMAIN)
