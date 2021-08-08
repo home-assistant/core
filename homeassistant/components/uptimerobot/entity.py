45b66145764cbe4e517ebda7826b1f548f32a85c
Ludeeus <ludeeus@ludeeus.dev>
2021-08-08 09:54:53 +0000

    Remove properties that are no longer needed

--- homeassistant/components/uptimerobot/entity.py
+++ homeassistant/components/uptimerobot/entity.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from pyuptimerobot import UptimeRobotMonitor
 
 from homeassistant.const import ATTR_ATTRIBUTION
-from homeassistant.helpers.entity import DeviceInfo, EntityDescription
+from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
@@ -25,30 +25,21 @@ class UptimeRobotEntity(CoordinatorEntity):
         """Initialize Uptime Robot entities."""
         super().__init__(coordinator)
         self.entity_description = description
-        self._target = target
-        self._attr_extra_state_attributes = {
-            ATTR_ATTRIBUTION: ATTRIBUTION,
-            ATTR_TARGET: self._target,
-        }
-
-    @property
-    def unique_id(self) -> str | None:
-        """Return the unique_id of the entity."""
-        return str(self.monitor.id)
-
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return device information about this AdGuard Home instance."""
-        return {
+        self._attr_device_info = {
             "identifiers": {(DOMAIN, str(self.monitor.id))},
             "name": "Uptime Robot",
             "manufacturer": "Uptime Robot Team",
             "entry_type": "service",
             "model": self.monitor.type.name,
         }
+        self._attr_extra_state_attributes = {
+            ATTR_ATTRIBUTION: ATTRIBUTION,
+            ATTR_TARGET: target,
+        }
+        self._attr_unique_id = str(self.monitor.id)
 
     @property
-    def monitors(self) -> list[UptimeRobotMonitor]:
+    def _monitors(self) -> list[UptimeRobotMonitor]:
         """Return all monitors."""
         return self.coordinator.data or []
 
@@ -57,7 +48,7 @@ class UptimeRobotEntity(CoordinatorEntity):
         """Return the monitor for this entity."""
         return next(
             monitor
-            for monitor in self.monitors
+            for monitor in self._monitors
             if str(monitor.id) == self.entity_description.key
         )
 
