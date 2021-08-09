1a0bb888ee764773473093c166b138ae35043deb
Guido Schmitz <Shutgun@users.noreply.github.com>
2021-08-09 23:45:15 +0200

    Remove useless attribute in devolo Home Control (#54284)

--- homeassistant/components/devolo_home_control/devolo_device.py
+++ homeassistant/components/devolo_home_control/devolo_device.py
@@ -45,7 +45,6 @@ class DevoloDeviceEntity(Entity):
         self.subscriber: Subscriber | None = None
         self.sync_callback = self._sync
         self._value: int
-        self._unit = ""
 
     async def async_added_to_hass(self) -> None:
         """Call when entity is added to hass."""
