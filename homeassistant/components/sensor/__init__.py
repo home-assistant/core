4ff9baa69d4d2f0ed06dbfda9aaaf8adf1c51599
Bram Kragten <mail@bramkragten.nl>
2021-08-09 22:43:43 +0200

    Update homeassistant/components/sensor/__init__.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/sensor/__init__.py
+++ homeassistant/components/sensor/__init__.py
@@ -63,7 +63,7 @@ DEVICE_CLASSES: Final[list[str]] = [
     DEVICE_CLASS_POWER,  # power (W/kW)
     DEVICE_CLASS_POWER_FACTOR,  # power factor (%)
     DEVICE_CLASS_VOLTAGE,  # voltage (V)
-    DEVICE_CLASS_GAS,  # gas (m3/feet3)
+    DEVICE_CLASS_GAS,  # gas (m³ or ft³)
 ]
 
 DEVICE_CLASSES_SCHEMA: Final = vol.All(vol.Lower, vol.In(DEVICE_CLASSES))
