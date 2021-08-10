0b64e96757f6f63adc66ec31f8cd39fa6f75c3d7
Milan Meulemans <milan.meulemans@live.be>
2021-08-10 10:46:33 +0200

    Activate mypy for Alexa (#54042)

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -16,7 +16,6 @@ from .model import Config, Integration
 IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.adguard.*",
     "homeassistant.components.aemet.*",
-    "homeassistant.components.alexa.*",
     "homeassistant.components.almond.*",
     "homeassistant.components.amcrest.*",
     "homeassistant.components.analytics.*",
