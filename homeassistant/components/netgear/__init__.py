23d3dca2a0457c25973e669f67a1138effddd8fd
starkillerOG <starkiller.og@gmail.com>
2021-08-11 14:24:32 +0200

    use new tracked list on update

--- homeassistant/components/netgear/__init__.py
+++ homeassistant/components/netgear/__init__.py
@@ -8,8 +8,8 @@ from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers import device_registry as dr
 
-from .const import DOMAIN, PLATFORMS
-from .router import NetgearRouter
+from .const import CONF_TRACKED_LIST, DOMAIN, PLATFORMS
+from .router import convert_tracked_list, NetgearRouter
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -43,25 +43,25 @@ async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry):
         entry, PLATFORMS
     )
 
-    router = hass.data[DOMAIN][entry.unique_id]
+    tracked_list = convert_tracked_list(entry.options.get(CONF_TRACKED_LIST, ""))
 
-    if router._tracked_list:
+    if tracked_list:
         # Remove entities that are no longer tracked
         entity_registry = er.async_get(hass)
         entries = er.async_entries_for_config_entry(entity_registry, entry.entry_id)
         for entity_entry in entries:
-            if entity_entry.unique_id not in router._tracked_list:
+            if entity_entry.unique_id not in tracked_list:
                 entity_registry.async_remove(entity_entry.entity_id)
 
         # Remove devices that are no longer tracked
         device_registry = dr.async_get(hass)
         devices = dr.async_entries_for_config_entry(device_registry, entry.entry_id)
         for device_entry in devices:
-            if dict(device_entry.connections)["mac"] not in router._tracked_list and len(device_entry.config_entries) == 1:
+            if dict(device_entry.connections)["mac"] not in tracked_list and len(device_entry.config_entries) == 1:
                 device_registry.async_remove_device(device_entry.id)
 
     if unload_ok:
-        await router.async_unload()
+        await hass.data[DOMAIN][entry.unique_id].async_unload()
         hass.data[DOMAIN].pop(entry.unique_id)
         if not hass.data[DOMAIN]:
             hass.data.pop(DOMAIN)
