0f4f9081c1aebdff5904805b1981c2fde314218c
starkillerOG <starkiller.og@gmail.com>
2021-08-11 13:53:34 +0200

    clean up registries

--- homeassistant/components/netgear/__init__.py
+++ homeassistant/components/netgear/__init__.py
@@ -2,8 +2,11 @@
 import asyncio
 import logging
 
+from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import HomeAssistantType
+from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers import device_registry as dr
 
 from .const import DOMAIN, PLATFORMS
 from .router import NetgearRouter
@@ -24,6 +27,8 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.unique_id] = router
 
+    entry.async_on_unload(entry.add_update_listener(update_listener))
+
     for platform in PLATFORMS:
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, platform)
@@ -38,8 +43,25 @@ async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry):
         entry, PLATFORMS
     )
 
+    router = hass.data[DOMAIN][entry.unique_id]
+
+    if router._tracked_list:
+        # Remove entities that are no longer tracked
+        entity_registry = er.async_get(hass)
+        entries = er.async_entries_for_config_entry(entity_registry, entry.entry_id)
+        for entity_entry in entries:
+            if entity_entry.unique_id not in router._tracked_list:
+                entity_registry.async_remove(entity_entry.entity_id)
+
+        # Remove devices that are no longer tracked
+        device_registry = dr.async_get(hass)
+        devices = dr.async_entries_for_config_entry(device_registry, entry.entry_id)
+        for device_entry in devices:
+            if dict(device_entry.connections)["mac"] not in router._tracked_list:
+                device_registry.async_remove_device(device_entry.id)
+
     if unload_ok:
-        await hass.data[DOMAIN][entry.unique_id].async_unload()
+        await router.async_unload()
         hass.data[DOMAIN].pop(entry.unique_id)
         if not hass.data[DOMAIN]:
             hass.data.pop(DOMAIN)
@@ -47,7 +69,7 @@ async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry):
     return unload_ok
 
 async def update_listener(
-    hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry
+    hass: HomeAssistant, config_entry: ConfigEntry
 ):
     """Handle options update."""
     await hass.config_entries.async_reload(config_entry.entry_id)
