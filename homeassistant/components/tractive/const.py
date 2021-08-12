e411a0858a6ad3af78e3dcb5683ce90c5fbb3e16
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-08-12 16:47:36 +0200

    Add domain as prefix to the signal

--- homeassistant/components/tractive/const.py
+++ homeassistant/components/tractive/const.py
@@ -11,6 +11,6 @@ ATTR_MINUTES_ACTIVE = "minutes_active"
 
 TRACKER_HARDWARE_STATUS_UPDATED = "tracker_hardware_status_updated"
 TRACKER_POSITION_UPDATED = "tracker_position_updated"
-TRACKER_ACTIVITY_STATUS_UPDATED = "tractive_tracker_activity_updated"
+TRACKER_ACTIVITY_STATUS_UPDATED = f"{DOMAIN}_tracker_activity_updated"
 
 SERVER_UNAVAILABLE = "tractive_server_unavailable"
