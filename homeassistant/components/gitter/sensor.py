6de6a5dc141c433a1f4761ccb052bbd37115f746
Erik Montnemery <erik@montnemery.com>
2021-08-12 14:23:56 +0200

    Move temperature conversions to sensor base class (3/8) (#54469)
    * Move temperature conversions to entity base class (3/8)
    
    * Fix FritzBox sensor
    
    * Fix tests

--- homeassistant/components/gitter/sensor.py
+++ homeassistant/components/gitter/sensor.py
@@ -65,12 +65,12 @@ class GitterSensor(SensorEntity):
         return self._name
 
     @property
-    def state(self):
+    def native_value(self):
         """Return the state of the sensor."""
         return self._state
 
     @property
-    def unit_of_measurement(self):
+    def native_unit_of_measurement(self):
         """Return the unit the value is expressed in."""
         return self._unit_of_measurement
 
