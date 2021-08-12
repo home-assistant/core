6e2995af78e71d209d98bc7d29c8cb0408bc353a
J. Nick Koston <nick@koston.org>
2021-08-12 14:16:49 -0500

    one indexed

--- homeassistant/components/homekit/type_triggers.py
+++ homeassistant/components/homekit/type_triggers.py
@@ -46,7 +46,9 @@ class DeviceTriggerAccessory(HomeAccessory):
                 )
             )
             serv_stateless_switch.configure_char(CHAR_NAME, value=trigger_name)
-            serv_stateless_switch.configure_char(CHAR_SERVICE_LABEL_INDEX, value=idx)
+            serv_stateless_switch.configure_char(
+                CHAR_SERVICE_LABEL_INDEX, value=idx + 1
+            )
             serv_service_label = self.add_preload_service(SERV_SERVICE_LABEL)
             serv_service_label.configure_char(CHAR_SERVICE_LABEL_NAMESPACE, value=1)
             serv_stateless_switch.add_linked_service(serv_service_label)
