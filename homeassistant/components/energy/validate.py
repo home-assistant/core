01392f24e4140c3fe35ba62c2945cf434ea52179
Paulus Schoutsen <balloob@gmail.com>
2021-08-12 15:14:38 -0700

    Add energy validation

---
+++ homeassistant/components/energy/validate.py
@@ -0,0 +1,257 @@
+"""Validate the energy preferences provide valid data."""
+from __future__ import annotations
+
+import dataclasses
+
+from homeassistant.components import recorder
+from homeassistant.const import (
+    ENERGY_KILO_WATT_HOUR,
+    ENERGY_WATT_HOUR,
+    STATE_UNAVAILABLE,
+    STATE_UNKNOWN,
+)
+from homeassistant.core import HomeAssistant, callback, valid_entity_id
+
+from . import data
+from .const import DOMAIN
+
+
+@dataclasses.dataclass
+class ValidationMessage:
+    """Error or warning message."""
+
+    message: str
+    link: str | None = None
+
+
+@dataclasses.dataclass
+class ValidationResult:
+    """Result of validation."""
+
+    errors: list[ValidationMessage] = dataclasses.field(default_factory=list)
+    warnings: list[ValidationMessage] = dataclasses.field(default_factory=list)
+
+
+@dataclasses.dataclass
+class EnergyPreferencesValidation(ValidationResult):
+    """Dictionary holding validation information."""
+
+    energy_sources: list[ValidationResult] = dataclasses.field(default_factory=list)
+    device_consumption: list[ValidationResult] = dataclasses.field(default_factory=list)
+
+    def as_dict(self) -> dict:
+        """Return dictionary version."""
+        return dataclasses.asdict(self)
+
+
+@callback
+def _async_validate_energy_stat(
+    hass: HomeAssistant, stat_value: str, result: ValidationResult
+) -> None:
+    """Validate a statistic."""
+    has_entity_source = valid_entity_id(stat_value)
+
+    if not has_entity_source:
+        return
+
+    if not recorder.is_entity_recorded(hass, stat_value):
+        result.errors.append(
+            ValidationMessage(
+                f"Entity {stat_value} needs to be tracked by the recorder",
+                "https://www.home-assistant.io/integrations/recorder#configure-filter",
+            )
+        )
+        return
+
+    state = hass.states.get(stat_value)
+
+    if state is None:
+        result.warnings.append(
+            ValidationMessage(f"Entity {stat_value} is not currently defined")
+        )
+        return
+
+    if state.state in (STATE_UNAVAILABLE, STATE_UNKNOWN):
+        result.warnings.append(
+            ValidationMessage(
+                f"Entity {stat_value} is currently unavailable ({state.state})"
+            )
+        )
+        return
+
+    try:
+        current_value: float | None = float(state.state)
+    except ValueError:
+        result.errors.append(
+            ValidationMessage(
+                f"Entity {stat_value} has non-numeric value {state.state}"
+            )
+        )
+        current_value = None
+
+    if current_value is not None and current_value < 0:
+        result.errors.append(
+            ValidationMessage(f"Entity {stat_value} has negative value {current_value}")
+        )
+
+    unit = state.attributes.get("unit_of_measurement")
+
+    if unit is None:
+        result.warnings.append(
+            ValidationMessage(f"Entity {stat_value} has no unit of measurement")
+        )
+
+    elif unit not in (ENERGY_KILO_WATT_HOUR, ENERGY_WATT_HOUR):
+        result.errors.append(
+            ValidationMessage(
+                f"Entity {stat_value} has an unexpected unit. Expected {ENERGY_KILO_WATT_HOUR} or {ENERGY_WATT_HOUR}"
+            )
+        )
+
+
+@callback
+def _async_validate_price_entity(
+    hass: HomeAssistant, entity_id: str, result: ValidationResult
+) -> None:
+    """Validate that the price entity is correct."""
+    state = hass.states.get(entity_id)
+
+    if state is None:
+        result.errors.append(ValidationMessage(f"Unable to find entity {entity_id}"))
+        return
+
+    try:
+        value: float | None = float(state.state)
+    except ValueError:
+        result.errors.append(
+            ValidationMessage(f"Entity {entity_id} has non-numeric value {state.state}")
+        )
+        value = None
+        return
+
+    if value is not None and value < 0:
+        result.errors.append(
+            ValidationMessage(f"Entity {entity_id} has negative value {value}")
+        )
+
+    unit = state.attributes.get("unit_of_measurement")
+
+    if unit is None:
+        result.warnings.append(
+            ValidationMessage(f"Entity {entity_id} has no unit of measurement")
+        )
+
+    elif not unit.endswith((f"/{ENERGY_KILO_WATT_HOUR}", f"/{ENERGY_WATT_HOUR}")):
+        result.errors.append(
+            ValidationMessage(
+                f"Entity {entity_id} has a unit that is not per kilowatt or watt hour"
+            )
+        )
+
+
+@callback
+def _async_validate_cost_stat(
+    hass: HomeAssistant, stat_id: str, result: ValidationResult
+) -> None:
+    """Validate that the cost stat is correct."""
+    has_entity = valid_entity_id(stat_id)
+
+    if not has_entity:
+        return
+
+    if not recorder.is_entity_recorded(hass, stat_id):
+        result.errors.append(
+            ValidationMessage(
+                f"Entity {stat_id} needs to be tracked by the recorder",
+                "https://www.home-assistant.io/integrations/recorder#configure-filter",
+            )
+        )
+
+
+@callback
+def _async_validate_cost_entity(
+    hass: HomeAssistant, entity_id: str, result: ValidationResult
+) -> None:
+    """Validate that the cost entity is correct."""
+    if not recorder.is_entity_recorded(hass, entity_id):
+        result.errors.append(
+            ValidationMessage(
+                f"Entity {entity_id} needs to be tracked by the recorder",
+                "https://www.home-assistant.io/integrations/recorder#configure-filter",
+            )
+        )
+
+
+async def async_validate(hass: HomeAssistant) -> EnergyPreferencesValidation:
+    """Validate the energy configuration."""
+    manager = await data.async_get_manager(hass)
+
+    result = EnergyPreferencesValidation()
+
+    if manager.data is None:
+        return result
+
+    if "recorder" not in hass.config.components:
+        result.errors.append(
+            ValidationMessage(
+                "Recorder integration is not configured",
+                "https://www.home-assistant.io/integrations/recorder",
+            )
+        )
+        return result
+
+    for source in manager.data["energy_sources"]:
+        source_result = ValidationResult()
+        result.energy_sources.append(source_result)
+
+        if source["type"] == "grid":
+            for flow in source["flow_from"]:
+                _async_validate_energy_stat(
+                    hass, flow["stat_energy_from"], source_result
+                )
+
+                if "stat_cost" in flow:
+                    _async_validate_cost_stat(hass, flow["stat_cost"], source_result)
+
+                elif "entity_energy_price" in flow:
+                    _async_validate_price_entity(
+                        hass, flow["entity_energy_price"], source_result
+                    )
+                    _async_validate_cost_entity(
+                        hass,
+                        hass.data[DOMAIN]["cost_sensors"][flow["stat_energy_from"]],
+                        source_result,
+                    )
+
+            for flow in source["flow_to"]:
+                _async_validate_energy_stat(hass, flow["stat_energy_to"], source_result)
+
+                if "stat_compensation" in flow:
+                    _async_validate_cost_stat(
+                        hass, flow["stat_compensation"], source_result
+                    )
+
+                elif "entity_energy_price" in flow:
+                    _async_validate_price_entity(
+                        hass, flow["entity_energy_price"], source_result
+                    )
+                    _async_validate_cost_entity(
+                        hass,
+                        hass.data[DOMAIN]["cost_sensors"][flow["stat_energy_to"]],
+                        source_result,
+                    )
+
+        elif source["type"] == "solar":
+            _async_validate_energy_stat(hass, source["stat_energy_from"], source_result)
+
+        elif source["type"] == "battery":
+            _async_validate_energy_stat(hass, source["stat_energy_from"], source_result)
+            _async_validate_energy_stat(hass, source["stat_energy_to"], source_result)
+
+    for device in manager.data["device_consumption"]:
+        result.device_consumption.append(ValidationResult())
+        _async_validate_energy_stat(
+            hass, device["stat_consumption"], result.device_consumption[-1]
+        )
+
+    return result
---
+++ tests/components/energy/test_validate.py
@@ -0,0 +1,491 @@
+"""Test that validation works."""
+from unittest.mock import patch
+
+import pytest
+
+from homeassistant.components.energy import async_get_manager, validate
+from homeassistant.setup import async_setup_component
+
+from tests.common import async_init_recorder_component
+
+
+@pytest.fixture
+def mock_is_entity_recorded():
+    """Mock recorder.is_entity_recorded."""
+    mocks = {}
+
+    with patch(
+        "homeassistant.components.recorder.is_entity_recorded",
+        side_effect=lambda hass, entity_id: mocks.get(entity_id, True),
+    ):
+        yield mocks
+
+
+@pytest.fixture(autouse=True)
+async def mock_energy_manager(hass):
+    """Set up energy."""
+    await async_init_recorder_component(hass)
+    assert await async_setup_component(hass, "energy", {"energy": {}})
+    manager = await async_get_manager(hass)
+    manager.data = manager.default_preferences()
+    return manager
+
+
+async def test_validation_empty_config(hass):
+    """Test validating an empty config."""
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [],
+        "device_consumption": [],
+    }
+
+
+async def test_validation_no_recorder(hass):
+    """Test validating an empty config."""
+    hass.config.components.remove("recorder")
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [
+            {
+                "message": "Recorder integration is not configured",
+                "link": "https://www.home-assistant.io/integrations/recorder",
+            }
+        ],
+        "warnings": [],
+        "energy_sources": [],
+        "device_consumption": [],
+    }
+
+
+async def test_validation_device_consumption_entity_missing(hass, mock_energy_manager):
+    """Test validating missing stat for device."""
+    await mock_energy_manager.async_update(
+        {"device_consumption": [{"stat_consumption": "sensor.not_exist"}]}
+    )
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [],
+        "device_consumption": [
+            {
+                "errors": [],
+                "warnings": [
+                    {
+                        "message": "Entity sensor.not_exist is not currently defined",
+                        "link": None,
+                    }
+                ],
+            }
+        ],
+    }
+
+
+async def test_validation_device_consumption_entity_unavailable(
+    hass, mock_energy_manager
+):
+    """Test validating missing stat for device."""
+    await mock_energy_manager.async_update(
+        {"device_consumption": [{"stat_consumption": "sensor.unavailable"}]}
+    )
+    hass.states.async_set("sensor.unavailable", "unavailable", {})
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [],
+        "device_consumption": [
+            {
+                "errors": [],
+                "warnings": [
+                    {
+                        "message": "Entity sensor.unavailable is currently unavailable (unavailable)",
+                        "link": None,
+                    }
+                ],
+            }
+        ],
+    }
+
+
+async def test_validation_device_consumption_entity_non_numeric(
+    hass, mock_energy_manager
+):
+    """Test validating missing stat for device."""
+    await mock_energy_manager.async_update(
+        {"device_consumption": [{"stat_consumption": "sensor.non_numeric"}]}
+    )
+    hass.states.async_set("sensor.non_numeric", "123,123.10", {})
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [],
+        "device_consumption": [
+            {
+                "errors": [
+                    {
+                        "message": "Entity sensor.non_numeric has non-numeric value 123,123.10",
+                        "link": None,
+                    }
+                ],
+                "warnings": [
+                    {
+                        "message": "Entity sensor.non_numeric has no unit of measurement",
+                        "link": None,
+                    }
+                ],
+            }
+        ],
+    }
+
+
+async def test_validation_device_consumption_entity_unexpected_unit(
+    hass, mock_energy_manager
+):
+    """Test validating missing stat for device."""
+    await mock_energy_manager.async_update(
+        {"device_consumption": [{"stat_consumption": "sensor.unexpected_unit"}]}
+    )
+    hass.states.async_set(
+        "sensor.unexpected_unit", "10.10", {"unit_of_measurement": "beers"}
+    )
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [],
+        "device_consumption": [
+            {
+                "errors": [
+                    {
+                        "message": "Entity sensor.unexpected_unit has an unexpected unit. Expected kWh or Wh",
+                        "link": None,
+                    }
+                ],
+                "warnings": [],
+            }
+        ],
+    }
+
+
+async def test_validation_device_consumption_recorder_not_tracked(
+    hass, mock_energy_manager, mock_is_entity_recorded
+):
+    """Test validating device based on untracked entity."""
+    mock_is_entity_recorded["sensor.not_recorded"] = False
+    await mock_energy_manager.async_update(
+        {"device_consumption": [{"stat_consumption": "sensor.not_recorded"}]}
+    )
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [],
+        "device_consumption": [
+            {
+                "errors": [
+                    {
+                        "message": "Entity sensor.not_recorded needs to be tracked by the recorder",
+                        "link": "https://www.home-assistant.io/integrations/recorder#configure-filter",
+                    }
+                ],
+                "warnings": [],
+            }
+        ],
+    }
+
+
+async def test_validation_solar(hass, mock_energy_manager):
+    """Test validating missing stat for device."""
+    await mock_energy_manager.async_update(
+        {
+            "energy_sources": [
+                {"type": "solar", "stat_energy_from": "sensor.solar_production"}
+            ]
+        }
+    )
+    hass.states.async_set(
+        "sensor.solar_production", "10.10", {"unit_of_measurement": "beers"}
+    )
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [
+            {
+                "errors": [
+                    {
+                        "message": "Entity sensor.solar_production has an unexpected unit. Expected kWh or Wh",
+                        "link": None,
+                    }
+                ],
+                "warnings": [],
+            }
+        ],
+        "device_consumption": [],
+    }
+
+
+async def test_validation_battery(hass, mock_energy_manager):
+    """Test validating missing stat for device."""
+    await mock_energy_manager.async_update(
+        {
+            "energy_sources": [
+                {
+                    "type": "battery",
+                    "stat_energy_from": "sensor.battery_import",
+                    "stat_energy_to": "sensor.battery_export",
+                }
+            ]
+        }
+    )
+    hass.states.async_set(
+        "sensor.battery_import", "10.10", {"unit_of_measurement": "beers"}
+    )
+    hass.states.async_set(
+        "sensor.battery_export", "10.10", {"unit_of_measurement": "beers"}
+    )
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [
+            {
+                "errors": [
+                    {
+                        "message": "Entity sensor.battery_import has an unexpected unit. Expected kWh or Wh",
+                        "link": None,
+                    },
+                    {
+                        "message": "Entity sensor.battery_export has an unexpected unit. Expected kWh or Wh",
+                        "link": None,
+                    },
+                ],
+                "warnings": [],
+            }
+        ],
+        "device_consumption": [],
+    }
+
+
+async def test_validation_grid(hass, mock_energy_manager, mock_is_entity_recorded):
+    """Test validating grid with sensors for energy and cost/compensation."""
+    mock_is_entity_recorded["sensor.grid_cost_1"] = False
+    mock_is_entity_recorded["sensor.grid_compensation_1"] = False
+    await mock_energy_manager.async_update(
+        {
+            "energy_sources": [
+                {
+                    "type": "grid",
+                    "flow_from": [
+                        {
+                            "stat_energy_from": "sensor.grid_consumption_1",
+                            "stat_cost": "sensor.grid_cost_1",
+                        }
+                    ],
+                    "flow_to": [
+                        {
+                            "stat_energy_to": "sensor.grid_production_1",
+                            "stat_compensation": "sensor.grid_compensation_1",
+                        }
+                    ],
+                }
+            ]
+        }
+    )
+    hass.states.async_set(
+        "sensor.grid_consumption_1", "10.10", {"unit_of_measurement": "beers"}
+    )
+    hass.states.async_set(
+        "sensor.grid_production_1", "10.10", {"unit_of_measurement": "beers"}
+    )
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [
+            {
+                "errors": [
+                    {
+                        "message": "Entity sensor.grid_consumption_1 has an unexpected unit. Expected kWh or Wh",
+                        "link": None,
+                    },
+                    {
+                        "message": "Entity sensor.grid_cost_1 needs to be tracked by the recorder",
+                        "link": "https://www.home-assistant.io/integrations/recorder#configure-filter",
+                    },
+                    {
+                        "message": "Entity sensor.grid_production_1 has an unexpected unit. Expected kWh or Wh",
+                        "link": None,
+                    },
+                    {
+                        "message": "Entity sensor.grid_compensation_1 needs to be tracked by the recorder",
+                        "link": "https://www.home-assistant.io/integrations/recorder#configure-filter",
+                    },
+                ],
+                "warnings": [],
+            }
+        ],
+        "device_consumption": [],
+    }
+
+
+async def test_validation_grid_price_not_exist(hass, mock_energy_manager):
+    """Test validating grid with price entity that does not exist."""
+    hass.states.async_set(
+        "sensor.grid_consumption_1", "10.10", {"unit_of_measurement": "kWh"}
+    )
+    hass.states.async_set(
+        "sensor.grid_production_1", "10.10", {"unit_of_measurement": "kWh"}
+    )
+    await mock_energy_manager.async_update(
+        {
+            "energy_sources": [
+                {
+                    "type": "grid",
+                    "flow_from": [
+                        {
+                            "stat_energy_from": "sensor.grid_consumption_1",
+                            "entity_energy_from": "sensor.grid_consumption_1",
+                            "entity_energy_price": "sensor.grid_price_1",
+                        }
+                    ],
+                    "flow_to": [
+                        {
+                            "stat_energy_to": "sensor.grid_production_1",
+                            "entity_energy_to": "sensor.grid_production_1",
+                            "number_energy_price": 0.10,
+                        }
+                    ],
+                }
+            ]
+        }
+    )
+    await hass.async_block_till_done()
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [
+            {
+                "errors": [
+                    {
+                        "message": "Unable to find entity sensor.grid_price_1",
+                        "link": None,
+                    },
+                ],
+                "warnings": [],
+            }
+        ],
+        "device_consumption": [],
+    }
+
+
+@pytest.mark.parametrize(
+    "state, unit, expected",
+    (
+        (
+            "123,123.12",
+            "$/kWh",
+            "Entity sensor.grid_price_1 has non-numeric value 123,123.12",
+        ),
+        ("-100", "$/kWh", "Entity sensor.grid_price_1 has negative value -100"),
+        (
+            "123",
+            "$/Ws",
+            "Entity sensor.grid_price_1 has a unit that is not per kilowatt or watt hour",
+        ),
+    ),
+)
+async def test_validation_grid_price_errors(
+    hass, mock_energy_manager, state, unit, expected
+):
+    """Test validating grid with price data that gives errors."""
+    hass.states.async_set(
+        "sensor.grid_consumption_1", "10.10", {"unit_of_measurement": "kWh"}
+    )
+    hass.states.async_set("sensor.grid_price_1", state, {"unit_of_measurement": unit})
+    await mock_energy_manager.async_update(
+        {
+            "energy_sources": [
+                {
+                    "type": "grid",
+                    "flow_from": [
+                        {
+                            "stat_energy_from": "sensor.grid_consumption_1",
+                            "entity_energy_from": "sensor.grid_consumption_1",
+                            "entity_energy_price": "sensor.grid_price_1",
+                        }
+                    ],
+                    "flow_to": [],
+                }
+            ]
+        }
+    )
+    await hass.async_block_till_done()
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [
+            {
+                "errors": [
+                    {
+                        "message": expected,
+                        "link": None,
+                    },
+                ],
+                "warnings": [],
+            }
+        ],
+        "device_consumption": [],
+    }
+
+
+@pytest.mark.parametrize(
+    "state, unit, expected",
+    (("123", None, "Entity sensor.grid_price_1 has no unit of measurement"),),
+)
+async def test_validation_grid_price_warnings(
+    hass, mock_energy_manager, state, unit, expected
+):
+    """Test validating grid with config that causes price warnings."""
+    hass.states.async_set(
+        "sensor.grid_consumption_1", "10.10", {"unit_of_measurement": "kWh"}
+    )
+    hass.states.async_set("sensor.grid_price_1", state, {"unit_of_measurement": unit})
+    await mock_energy_manager.async_update(
+        {
+            "energy_sources": [
+                {
+                    "type": "grid",
+                    "flow_from": [
+                        {
+                            "stat_energy_from": "sensor.grid_consumption_1",
+                            "entity_energy_from": "sensor.grid_consumption_1",
+                            "entity_energy_price": "sensor.grid_price_1",
+                        }
+                    ],
+                    "flow_to": [],
+                }
+            ]
+        }
+    )
+    await hass.async_block_till_done()
+
+    assert (await validate.async_validate(hass)).as_dict() == {
+        "errors": [],
+        "warnings": [],
+        "energy_sources": [
+            {
+                "errors": [],
+                "warnings": [
+                    {
+                        "message": expected,
+                        "link": None,
+                    },
+                ],
+            }
+        ],
+        "device_consumption": [],
+    }
