e6e2544e138256f7e91760d6e87194c7c9fac4fd
Ryan Adolf <ryanadolf123@gmail.com>
2021-08-13 19:32:19 -0700

    Add test

---
+++ tests/components/ted6000/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for the ted6000 component."""
---
+++ tests/components/ted6000/test_config_flow.py
@@ -0,0 +1,32 @@
+"""Tests for the Cast config flow."""
+from unittest.mock import patch
+
+from homeassistant import config_entries, data_entry_flow
+from homeassistant.components.ted6000 import DOMAIN
+from homeassistant.const import CONF_IP_ADDRESS
+
+CONFIG = {CONF_IP_ADDRESS: "127.0.0.1"}
+
+
+async def test_form(hass):
+    """Test we get the form."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.ted6000.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            CONFIG,
+        )
+
+        await hass.async_block_till_done()
+
+    assert result2["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result2["data"] == CONFIG
+    assert len(mock_setup_entry.mock_calls) == 1
