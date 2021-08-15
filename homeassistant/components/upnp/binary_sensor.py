4b2910b7b6ea3ebc84b43d1adc4e9f348fcb1452
Erik Hendrix <hendrix_erik@hotmail.com>
2021-08-15 17:11:22 -0600

    Updates on coordinator

--- homeassistant/components/upnp/binary_sensor.py
+++ homeassistant/components/upnp/binary_sensor.py
@@ -46,7 +46,7 @@ class UpnpStatusBinarySensor(UpnpEntity, BinarySensorEntity):
     @property
     def available(self) -> bool:
         """Return if entity is available."""
-        return super().available and self._device.coordinator.data.get(WANSTATUS)
+        return super().available and self._coordinator.data.get(WANSTATUS)
 
     @property
     def is_on(self) -> bool:
