e821b7206521333f599f9c768d15e7375129c655
jan Iversen <jancasacondor@gmail.com>
2021-08-16 13:49:13 +0200

    Add fixtures.

--- tests/components/template/conftest.py
+++ tests/components/template/conftest.py
@@ -1,2 +1,27 @@
 """template conftest."""
-from tests.components.light.conftest import mock_light_profiles  # noqa: F401
+import pytest
+
+from homeassistant.setup import async_setup_component
+
+from tests.common import assert_setup_component, async_mock_service
+
+
+@pytest.fixture
+def calls(hass):
+    """Track calls to a mock service."""
+    return async_mock_service(hass, "test", "automation")
+
+
+@pytest.fixture
+async def start_ha(hass, do_count, do_domain, do_config, caplog):
+    """Do setup of integration."""
+    with assert_setup_component(do_count, do_domain):
+        assert await async_setup_component(
+            hass,
+            do_domain,
+            do_config,
+        )
+
+    await hass.async_block_till_done()
+    await hass.async_start()
+    await hass.async_block_till_done()
