9d15866029638b5beef739b03bd83f382e008f71
Geoffrey Lagaisse <geoffreylagaisse@gmail.com>
2021-08-17 12:47:57 +0000

    Fix Pyupgrade error, rest I  fix after my holiday

--- homeassistant/components/qbittorrent/__init__.py
+++ homeassistant/components/qbittorrent/__init__.py
@@ -75,10 +75,10 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
 
     unload_ok = all(
         await asyncio.gather(
-            *[
+            *(
                 hass.config_entries.async_forward_entry_unload(config_entry, platform)
                 for platform in PLATFORMS
-            ]
+            )
         )
     )
 
