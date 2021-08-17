017babd9d9dedcc3c953e7b11d873b27beab8286
Nick Whyte <nick@nickwhyte.com>
2021-08-17 20:50:19 +1000

    Fix patching in tests

--- tests/components/aussie_broadband/test_config_flow.py
+++ tests/components/aussie_broadband/test_config_flow.py
@@ -9,8 +9,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import RESULT_TYPE_CREATE_ENTRY, RESULT_TYPE_FORM
 
 
-@patch("aussiebb.AussieBB")
-async def test_form(aussie_bb, hass: HomeAssistant) -> None:
+async def test_form(hass: HomeAssistant) -> None:
     """Test we get the form."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -18,12 +17,13 @@ async def test_form(aussie_bb, hass: HomeAssistant) -> None:
     assert result["type"] == RESULT_TYPE_FORM
     assert result["errors"] is None
 
-    aussie_bb_instance = aussie_bb.return_value
-    aussie_bb_instance.get_services.return_value = [
-        {"service_id": "12345678", "description": "Fake ABB Service"}
+    fake_services = [
+        {"service_id": "12345678", "description": "Fake ABB Service"},
     ]
 
-    with patch(
+    with patch("aussiebb.AussieBB.__init__", return_value=None), patch(
+        "aussiebb.AussieBB.get_services", return_value=fake_services
+    ), patch(
         "homeassistant.components.aussie_broadband.async_setup_entry",
         return_value=True,
     ) as mock_setup_entry:
@@ -46,8 +46,7 @@ async def test_form(aussie_bb, hass: HomeAssistant) -> None:
     assert len(mock_setup_entry.mock_calls) == 1
 
 
-@patch("aussiebb.AussieBB")
-async def test_form_multiple_services(aussie_bb, hass: HomeAssistant) -> None:
+async def test_form_multiple_services(hass: HomeAssistant) -> None:
     """Test we get the form."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -55,20 +54,22 @@ async def test_form_multiple_services(aussie_bb, hass: HomeAssistant) -> None:
     assert result["type"] == RESULT_TYPE_FORM
     assert result["errors"] is None
 
-    aussie_bb_instance = aussie_bb.return_value
-    aussie_bb_instance.get_services.return_value = [
+    fake_services = [
         {"service_id": "12345678", "description": "Fake ABB Service 1"},
         {"service_id": "87654321", "description": "Fake ABB Service 2"},
     ]
 
-    result2 = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {
-            "username": "test-username",
-            "password": "test-password",
-        },
-    )
-    await hass.async_block_till_done()
+    with patch("aussiebb.AussieBB.__init__", return_value=None), patch(
+        "aussiebb.AussieBB.get_services", return_value=fake_services
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
+        await hass.async_block_till_done()
 
     assert result2["type"] == RESULT_TYPE_FORM
     assert result2["step_id"] == "service"
@@ -94,20 +95,20 @@ async def test_form_multiple_services(aussie_bb, hass: HomeAssistant) -> None:
     assert len(mock_setup_entry.mock_calls) == 1
 
 
-@patch("aussiebb.AussieBB", side_effect=AuthenticationException())
-async def test_form_invalid_auth(aussie_bb, hass: HomeAssistant) -> None:
+async def test_form_invalid_auth(hass: HomeAssistant) -> None:
     """Test we handle invalid auth."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    result2 = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {
-            "username": "test-username",
-            "password": "test-password",
-        },
-    )
+    with patch("aussiebb.AussieBB.__init__", side_effect=AuthenticationException()):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
 
     assert result2["type"] == RESULT_TYPE_FORM
     assert result2["errors"] == {"base": "invalid_credentials"}
