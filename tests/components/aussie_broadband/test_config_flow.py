4608673718a72c3a77bc84f047ed8d14f22ec6dc
Nick Whyte <nick@nickwhyte.com>
2021-08-19 09:07:32 +1000

    improve coverage

--- tests/components/aussie_broadband/test_config_flow.py
+++ tests/components/aussie_broadband/test_config_flow.py
@@ -100,8 +100,86 @@ async def test_no_services(hass: HomeAssistant) -> None:
     assert len(mock_setup_entry.mock_calls) == 0
 
 
+async def test_form_duplicate_service(hass: HomeAssistant) -> None:
+    """Test form fails if adding a service twice"""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] is None
+
+    fake_services = [
+        {"service_id": "12345678", "description": "Fake ABB Service"},
+    ]
+
+    with patch("aussiebb.AussieBB.__init__", return_value=None), patch(
+        "aussiebb.AussieBB.get_services", return_value=fake_services
+    ), patch(
+        "homeassistant.components.aussie_broadband.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
+    assert len(mock_setup_entry.mock_calls) == 1
+
+    result3 = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result3["type"] == RESULT_TYPE_FORM
+    assert result3["errors"] is None
+    with patch("aussiebb.AussieBB.__init__", return_value=None), patch(
+        "aussiebb.AussieBB.get_services", return_value=fake_services
+    ), patch(
+        "homeassistant.components.aussie_broadband.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result4 = await hass.config_entries.flow.async_configure(
+            result3["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result4["type"] == RESULT_TYPE_ABORT
+    assert result4["reason"] == "already_configured"
+
+
+async def test_form_no_services(hass: HomeAssistant) -> None:
+    """Test the config flow with no services."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] is None
+
+    with patch("aussiebb.AussieBB.__init__", return_value=None), patch(
+        "aussiebb.AussieBB.get_services", return_value=[]
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] == RESULT_TYPE_ABORT
+    assert result2["reason"] == "no_devices_found"
+
+
 async def test_form_multiple_services(hass: HomeAssistant) -> None:
-    """Test we get the form."""
+    """Test the config flow with multiple services."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
@@ -147,6 +225,86 @@ async def test_form_multiple_services(hass: HomeAssistant) -> None:
         "password": "test-password",
     }
     assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_form_multiple_services_duplicate(hass: HomeAssistant) -> None:
+    """Test that the form fails if adding a service twice."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] is None
+
+    fake_services = [
+        {"service_id": "12345678", "description": "Fake ABB Service 1"},
+        {"service_id": "87654321", "description": "Fake ABB Service 2"},
+    ]
+
+    with patch("aussiebb.AussieBB.__init__", return_value=None), patch(
+        "aussiebb.AussieBB.get_services", return_value=fake_services
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] == RESULT_TYPE_FORM
+    assert result2["step_id"] == "service"
+    assert result2["errors"] is None
+
+    with patch(
+        "homeassistant.components.aussie_broadband.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result3 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {"service_id": "87654321"},
+        )
+        await hass.async_block_till_done()
+
+    assert result3["type"] == RESULT_TYPE_CREATE_ENTRY
+    assert result3["title"] == "Fake ABB Service 2"
+    assert result3["data"] == {
+        "service_id": "87654321",
+        "username": "test-username",
+        "password": "test-password",
+    }
+    assert len(mock_setup_entry.mock_calls) == 1
+
+    result4 = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result4["type"] == RESULT_TYPE_FORM
+    assert result4["errors"] is None
+
+    with patch("aussiebb.AussieBB.__init__", return_value=None), patch(
+        "aussiebb.AussieBB.get_services", return_value=fake_services
+    ):
+        result5 = await hass.config_entries.flow.async_configure(
+            result4["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result5["type"] == RESULT_TYPE_FORM
+    assert result5["step_id"] == "service"
+    assert result5["errors"] is None
+
+    result6 = await hass.config_entries.flow.async_configure(
+        result5["flow_id"],
+        {"service_id": "87654321"},
+    )
+    await hass.async_block_till_done()
+
+    assert result6["type"] == RESULT_TYPE_ABORT
+    assert result6["reason"] == "already_configured"
 
 
 async def test_form_invalid_auth(hass: HomeAssistant) -> None:
