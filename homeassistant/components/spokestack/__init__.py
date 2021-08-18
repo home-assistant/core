8c5759b7ba74ed8a0dcd8e3611e6947fb7fedad1
Will Rice <will@spokestack.io>
2021-08-18 15:47:38 -0400

    Update homeassistant/components/spokestack/__init__.py
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/spokestack/__init__.py
+++ homeassistant/components/spokestack/__init__.py
@@ -11,16 +11,6 @@ PLATFORMS = ["tts"]
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the Hello World component."""
-    # Ensure our name space for storing objects is a known type. A dict is
-    # common/preferred as it allows a separate instance of your class for each
-    # instance that has been created in the UI.
-    hass.data.setdefault(DOMAIN, {})
-
-    return True
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Spokestack from a config entry."""
     hass.data[DOMAIN][entry.entry_id] = entry.data["voice"]
