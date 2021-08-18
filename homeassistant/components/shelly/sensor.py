f7f2ea8226bd2e9fbc4e842acbf00b6382d3f11d
Erik <erik@montnemery.com>
2021-08-18 11:23:05 +0200

    Remove last_reset attribute from shelly energy sensors

--- homeassistant/components/shelly/sensor.py
+++ homeassistant/components/shelly/sensor.py
@@ -1,7 +1,6 @@
 """Sensor for Shelly."""
 from __future__ import annotations
 
-from datetime import timedelta
 import logging
 from typing import Final, cast
 
@@ -24,10 +23,9 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import StateType
-from homeassistant.util import dt
 
 from . import ShellyDeviceWrapper
-from .const import LAST_RESET_NEVER, LAST_RESET_UPTIME, SHAIR_MAX_WORK_HOURS
+from .const import SHAIR_MAX_WORK_HOURS
 from .entity import (
     BlockAttributeDescription,
     RestAttributeDescription,
@@ -119,49 +117,43 @@ SENSORS: Final = {
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 60 / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
-        state_class=sensor.STATE_CLASS_MEASUREMENT,
-        last_reset=LAST_RESET_UPTIME,
+        state_class=sensor.STATE_CLASS_TOTAL_INCREASING,
     ),
     ("emeter", "energy"): BlockAttributeDescription(
         name="Energy",
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
-        state_class=sensor.STATE_CLASS_MEASUREMENT,
-        last_reset=LAST_RESET_NEVER,
+        state_class=sensor.STATE_CLASS_TOTAL_INCREASING,
     ),
     ("emeter", "energyReturned"): BlockAttributeDescription(
         name="Energy Returned",
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
-        state_class=sensor.STATE_CLASS_MEASUREMENT,
-        last_reset=LAST_RESET_NEVER,
+        state_class=sensor.STATE_CLASS_TOTAL_INCREASING,
     ),
     ("light", "energy"): BlockAttributeDescription(
         name="Energy",
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 60 / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
-        state_class=sensor.STATE_CLASS_MEASUREMENT,
+        state_class=sensor.STATE_CLASS_TOTAL_INCREASING,
         default_enabled=False,
-        last_reset=LAST_RESET_UPTIME,
     ),
     ("relay", "energy"): BlockAttributeDescription(
         name="Energy",
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 60 / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
-        state_class=sensor.STATE_CLASS_MEASUREMENT,
-        last_reset=LAST_RESET_UPTIME,
+        state_class=sensor.STATE_CLASS_TOTAL_INCREASING,
     ),
     ("roller", "rollerEnergy"): BlockAttributeDescription(
         name="Energy",
         unit=ENERGY_KILO_WATT_HOUR,
         value=lambda value: round(value / 60 / 1000, 2),
         device_class=sensor.DEVICE_CLASS_ENERGY,
-        state_class=sensor.STATE_CLASS_MEASUREMENT,
-        last_reset=LAST_RESET_UPTIME,
+        state_class=sensor.STATE_CLASS_TOTAL_INCREASING,
     ),
     ("sensor", "concentration"): BlockAttributeDescription(
         name="Gas Concentration",
@@ -270,30 +262,10 @@ class ShellySensor(ShellyBlockAttributeEntity, SensorEntity):
     ) -> None:
         """Initialize sensor."""
         super().__init__(wrapper, block, attribute, description)
-        self._last_value: float | None = None
-
-        if description.last_reset == LAST_RESET_NEVER:
-            self._attr_last_reset = dt.utc_from_timestamp(0)
-        elif description.last_reset == LAST_RESET_UPTIME:
-            self._attr_last_reset = (
-                dt.utcnow() - timedelta(seconds=wrapper.device.status["uptime"])
-            ).replace(second=0, microsecond=0)
 
     @property
     def native_value(self) -> StateType:
         """Return value of sensor."""
-        if (
-            self.description.last_reset == LAST_RESET_UPTIME
-            and self.attribute_value is not None
-        ):
-            value = cast(float, self.attribute_value)
-
-            if self._last_value and self._last_value > value:
-                self._attr_last_reset = dt.utcnow().replace(second=0, microsecond=0)
-                _LOGGER.info("Energy reset detected for entity %s", self.name)
-
-            self._last_value = value
-
         return self.attribute_value
 
     @property
