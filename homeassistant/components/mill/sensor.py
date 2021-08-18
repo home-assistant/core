446e211e3882cca1ddf6207cb7ec1efe8e300965
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-08-18 21:30:37 +0200

    Mill data coordinator (#53603)
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- homeassistant/components/mill/sensor.py
+++ homeassistant/components/mill/sensor.py
@@ -6,6 +6,8 @@ from homeassistant.components.sensor import (
     SensorEntity,
 )
 from homeassistant.const import ENERGY_KILO_WATT_HOUR
+from homeassistant.core import callback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import CONSUMPTION_TODAY, CONSUMPTION_YEAR, DOMAIN, MANUFACTURER
 
@@ -13,27 +15,28 @@ from .const import CONSUMPTION_TODAY, CONSUMPTION_YEAR, DOMAIN, MANUFACTURER
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up the Mill sensor."""
 
-    mill_data_connection = hass.data[DOMAIN]
+    mill_data_coordinator = hass.data[DOMAIN]
 
     entities = [
-        MillHeaterEnergySensor(heater, mill_data_connection, sensor_type)
+        MillHeaterEnergySensor(mill_data_coordinator, sensor_type, heater)
         for sensor_type in (CONSUMPTION_TODAY, CONSUMPTION_YEAR)
-        for heater in mill_data_connection.heaters.values()
+        for heater in mill_data_coordinator.data.values()
     ]
     async_add_entities(entities)
 
 
-class MillHeaterEnergySensor(SensorEntity):
+class MillHeaterEnergySensor(CoordinatorEntity, SensorEntity):
     """Representation of a Mill Sensor device."""
 
     _attr_device_class = DEVICE_CLASS_ENERGY
     _attr_native_unit_of_measurement = ENERGY_KILO_WATT_HOUR
     _attr_state_class = STATE_CLASS_TOTAL_INCREASING
 
-    def __init__(self, heater, mill_data_connection, sensor_type):
+    def __init__(self, coordinator, sensor_type, heater):
         """Initialize the sensor."""
+        super().__init__(coordinator)
+
         self._id = heater.device_id
-        self._conn = mill_data_connection
         self._sensor_type = sensor_type
 
         self._attr_name = f"{heater.name} {sensor_type.replace('_', ' ')}"
@@ -44,20 +47,19 @@ class MillHeaterEnergySensor(SensorEntity):
             "manufacturer": MANUFACTURER,
             "model": f"generation {1 if heater.is_gen1 else 2}",
         }
+        self._update_attr(heater)
 
-    async def async_update(self):
-        """Retrieve latest state."""
-        heater = await self._conn.update_device(self._id)
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self._update_attr(self.coordinator.data[self._id])
+        self.async_write_ha_state()
+
+    @callback
+    def _update_attr(self, heater):
         self._attr_available = heater.available
 
         if self._sensor_type == CONSUMPTION_TODAY:
-            _state = heater.day_consumption
+            self._attr_native_value = heater.day_consumption
         elif self._sensor_type == CONSUMPTION_YEAR:
-            _state = heater.year_consumption
-        else:
-            _state = None
-        if _state is None:
-            self._attr_native_value = _state
-            return
-
-        self._attr_native_value = _state
+            self._attr_native_value = heater.year_consumption
