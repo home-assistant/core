b7d20b8fff808314cb50c430179e9f7c2d1ff062
J. Nick Koston <nick@koston.org>
2021-08-19 18:30:27 -0500

    adjust

--- homeassistant/components/usb/__init__.py
+++ homeassistant/components/usb/__init__.py
@@ -5,6 +5,9 @@ import datetime
 import logging
 import sys
 
+from serial.tools.list_ports import comports
+from serial.tools.list_ports_common import ListPortInfo
+
 from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.event import async_track_time_interval
@@ -18,53 +21,57 @@ _LOGGER = logging.getLogger(__name__)
 SCAN_INTERVAL = datetime.timedelta(minutes=5)
 
 
+def _usb_device_from_port(port: ListPortInfo) -> USBDevice:
+    return {
+        "device": port.device,
+        "vid": f"{hex(port.vid)[2:]:0>4}".upper(),
+        "pid": f"{hex(port.pid)[2:]:0>4}".upper(),
+        "serial_number": port.serial_number,
+    }
+
+
 @callback
 def _async_process_discovered_usb_device(
     hass: HomeAssistant, device: USBDevice
 ) -> None:
     domain_data = hass.data[DOMAIN]
     seen = domain_data[SEEN]
-
-    _LOGGER.warning("Discovered USB Device: %s", device)
-
+    _LOGGER.debug("Discovered USB Device: %s", device)
     device_tuple = _usb_device_tuple(device)
     if device_tuple in seen:
         return
     seen.add(device_tuple)
 
 
+@callback
+def _async_process_ports(hass: HomeAssistant, ports: list[ListPortInfo]) -> None:
+    for port in ports:
+        if port.vid is None and port.pid is None:
+            continue
+        _async_process_discovered_usb_device(hass, _usb_device_from_port(port))
+
+
+def scan_serial(hass: HomeAssistant) -> None:
+    """Scan serial ports."""
+    hass.loop.call_soon_threadsafe(_async_process_ports, hass, comports())
+
+
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the USB Discovery integration."""
     hass.data[DOMAIN] = {SEEN: set()}
 
-    if await _async_start_monitor(hass):
-        return True
-    await _async_start_scanner(hass)
+    if not await _async_start_monitor(hass):
+        await _async_start_scanner(hass)
+
+    await hass.async_add_executor_job(scan_serial, hass)
     return True
 
 
 async def _async_start_scanner(hass: HomeAssistant) -> None:
     """Perodic scan with pyserial."""
-    from serial.tools.list_ports import comports
-    from serial.tools.list_ports_common import ListPortInfo
 
-    def _usb_device_from_port(port: ListPortInfo) -> USBDevice:
-        return {
-            "device": port.device,
-            "vid": f"{hex(port.vid)[2:]:0>4}".upper(),
-            "pid": f"{hex(port.pid)[2:]:0>4}".upper(),
-            "serial_number": port.serial_number,
-        }
-
-    @callback
-    def _async_process_ports(ports):
-        for port in ports:
-            if port.vid is None and port.pid is None:
-                continue
-            _async_process_discovered_usb_device(hass, _usb_device_from_port(port))
-
-    def _scan_serial(*_):
-        hass.loop.call_soon_threadsafe(_async_process_ports, comports())
+    def _scan_serial():
+        scan_serial(hass)
 
     stop_track = async_track_time_interval(hass, _scan_serial, SCAN_INTERVAL)
 
@@ -73,7 +80,6 @@ async def _async_start_scanner(hass: HomeAssistant) -> None:
         stop_track()
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, _async_stop_scanner)
-    await hass.async_add_executor_job(_scan_serial)
 
 
 async def _async_start_monitor(hass: HomeAssistant) -> bool:
@@ -91,7 +97,8 @@ async def _async_start_monitor(hass: HomeAssistant) -> bool:
     monitor.filter_by(subsystem="tty")
 
     def _device_discovered(device):
-        _LOGGER.warning("Discovered Device: %s", device)
+        _LOGGER.debug("Discovered Device at path: %s, trigger scan", device.device_path)
+        scan_serial(hass)
 
     observer = MonitorObserver(
         monitor, callback=_device_discovered, name="usb-observer"
