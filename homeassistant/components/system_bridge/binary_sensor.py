5839c900dcf9936daf7ea069de57327987387e3d
Aidan Timson <contact@timmo.xyz>
2021-08-19 22:00:23 +0100

    Use binary sensor entity description

--- homeassistant/components/system_bridge/binary_sensor.py
+++ homeassistant/components/system_bridge/binary_sensor.py
@@ -7,12 +7,37 @@ from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_BATTERY_CHARGING,
     BinarySensorEntity,
 )
+from homeassistant.components.system_bridge.model import (
+    SystemBridgeBinarySensorEntityDescription,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from . import SystemBridgeDeviceEntity
 from .const import DOMAIN
 from .coordinator import SystemBridgeDataUpdateCoordinator
+
+BASE_BINARY_SENSOR_TYPES: tuple[SystemBridgeBinarySensorEntityDescription, ...] = (
+    SystemBridgeBinarySensorEntityDescription(
+        key="version_available",
+        name="New Version Available",
+        entity_registry_enabled_default=True,
+        device_class=None,
+        icon="mdi:counter",
+        value=lambda bridge: bridge.information.updates.available,
+    ),
+)
+
+BATTERY_BINARY_SENSOR_TYPES: tuple[SystemBridgeBinarySensorEntityDescription, ...] = (
+    SystemBridgeBinarySensorEntityDescription(
+        key="battery_is_charging",
+        name="Battery Is Charging",
+        entity_registry_enabled_default=True,
+        device_class=DEVICE_CLASS_BATTERY_CHARGING,
+        icon=None,
+        value=lambda bridge: bridge.information.updates.available,
+    ),
+)
 
 
 async def async_setup_entry(
@@ -22,49 +47,38 @@ async def async_setup_entry(
     coordinator: SystemBridgeDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
     bridge: Bridge = coordinator.data
 
+    entities = []
+    for description in BASE_BINARY_SENSOR_TYPES:
+        entities.append(SystemBridgeBinarySensor(coordinator, description))
+
     if bridge.battery and bridge.battery.hasBattery:
-        async_add_entities([SystemBridgeBatteryIsChargingBinarySensor(coordinator)])
+        for description in BATTERY_BINARY_SENSOR_TYPES:
+            entities.append(SystemBridgeBinarySensor(coordinator, description))
+
+    async_add_entities(entities)
 
 
 class SystemBridgeBinarySensor(SystemBridgeDeviceEntity, BinarySensorEntity):
-    """Defines a System Bridge binary sensor."""
+    """Define a System Bridge binary sensor."""
+
+    coordinator: SystemBridgeDataUpdateCoordinator
+    entity_description: SystemBridgeBinarySensorEntityDescription
 
     def __init__(
         self,
         coordinator: SystemBridgeDataUpdateCoordinator,
-        key: str,
-        name: str,
-        icon: str | None,
-        device_class: str | None,
-        enabled_by_default: bool,
+        description: SystemBridgeBinarySensorEntityDescription,
     ) -> None:
-        """Initialize System Bridge binary sensor."""
-        self._device_class = device_class
-
-        super().__init__(coordinator, key, name, icon, enabled_by_default)
-
-    @property
-    def device_class(self) -> str | None:
-        """Return the class of this binary sensor."""
-        return self._device_class
-
-
-class SystemBridgeBatteryIsChargingBinarySensor(SystemBridgeBinarySensor):
-    """Defines a Battery is charging binary sensor."""
-
-    def __init__(self, coordinator: SystemBridgeDataUpdateCoordinator) -> None:
-        """Initialize System Bridge binary sensor."""
+        """Initialize."""
         super().__init__(
             coordinator,
-            "battery_is_charging",
-            "Battery Is Charging",
-            None,
-            DEVICE_CLASS_BATTERY_CHARGING,
-            True,
+            description.key,
+            description.name,
         )
+        self.entity_description = description
 
     @property
-    def is_on(self) -> bool:
-        """Return if the state is on."""
+    def native_value(self) -> bool:
+        """Return the state."""
         bridge: Bridge = self.coordinator.data
-        return bridge.battery.isCharging
+        return self.entity_description.value(bridge)
--- homeassistant/components/system_bridge/model.py
+++ homeassistant/components/system_bridge/model.py
@@ -4,7 +4,15 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Callable
 
+from homeassistant.components.binary_sensor import BinarySensorEntityDescription
 from homeassistant.components.sensor import SensorEntityDescription
+
+
+@dataclass
+class SystemBridgeBinarySensorEntityDescription(BinarySensorEntityDescription):
+    """Class describing System Bridge binary sensor entities."""
+
+    value: Callable = round
 
 
 @dataclass
