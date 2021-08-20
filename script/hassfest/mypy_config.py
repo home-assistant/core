6b18ee6eea0e1fe6a828c95e3237ef6edfe78203
Erik Montnemery <erik@montnemery.com>
2021-08-20 19:10:19 +0200

    Enable basic type checking for aemet (#54925)

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -15,7 +15,6 @@ from .model import Config, Integration
 # Do your best to not add anything new here.
 IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.adguard.*",
-    "homeassistant.components.aemet.*",
     "homeassistant.components.almond.*",
     "homeassistant.components.analytics.*",
     "homeassistant.components.atag.*",
