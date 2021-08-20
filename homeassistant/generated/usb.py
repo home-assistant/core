4f8b59e5e8d5e28f1b7987a32771db6d168a8a66
J. Nick Koston <nick@koston.org>
2021-08-19 19:59:55 -0500

    adjust

--- homeassistant/generated/usb.py
+++ homeassistant/generated/usb.py
@@ -15,5 +15,15 @@ USB = [
         "domain": "zha",
         "vid": "1CF1",
         "pid": "0030"
+    },
+    {
+        "domain": "zha",
+        "vid": "1A86",
+        "pid": "7523"
+    },
+    {
+        "domain": "zha",
+        "vid": "10C4",
+        "pid": "8A2A"
     }
 ]
