cb3372ee76719758fda7845852ad4a6466ada16d
Joakim Plate <elupus@ecce.se>
2021-08-20 16:09:05 +0200

    Switch to entity description

--- homeassistant/components/fjaraskupan/binary_sensor.py
+++ homeassistant/components/fjaraskupan/binary_sensor.py
@@ -1,11 +1,15 @@
 """Support for sensors."""
 from __future__ import annotations
 
+from dataclasses import dataclass
+from typing import Callable
+
 from fjaraskupan import Device, State
 
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_PROBLEM,
     BinarySensorEntity,
+    BinarySensorEntityDescription,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -19,6 +23,29 @@ from homeassistant.helpers.update_coordinator import (
 from . import DeviceState, async_setup_entry_platform
 
 
+@dataclass
+class EntityDescription(BinarySensorEntityDescription):
+    """Entity description."""
+
+    is_on: Callable = lambda _: False
+
+
+SENSORS = (
+    EntityDescription(
+        key="grease-filter",
+        name="Grease Filter",
+        device_class=DEVICE_CLASS_PROBLEM,
+        is_on=lambda state: state.grease_filter_full,
+    ),
+    EntityDescription(
+        key="carbon-filter",
+        name="Carbon Filter",
+        device_class=DEVICE_CLASS_PROBLEM,
+        is_on=lambda state: state.carbon_filter_full,
+    ),
+)
+
+
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
@@ -26,62 +53,43 @@ async def async_setup_entry(
 ) -> None:
     """Set up sensors dynamically through discovery."""
 
-    def _constructor(devicestate: DeviceState):
+    def _constructor(device_state: DeviceState):
         return [
-            GreaseFilterSensor(
-                devicestate.coordinator, devicestate.device, devicestate.device_info
-            ),
-            CarbonFilterSensor(
-                devicestate.coordinator, devicestate.device, devicestate.device_info
-            ),
+            BinarySensor(
+                device_state.coordinator,
+                device_state.device,
+                device_state.device_info,
+                entity_description,
+            )
+            for entity_description in SENSORS
         ]
 
     async_setup_entry_platform(hass, config_entry, async_add_entities, _constructor)
 
 
-class GreaseFilterSensor(CoordinatorEntity[State], BinarySensorEntity):
+class BinarySensor(CoordinatorEntity[State], BinarySensorEntity):
     """Grease filter sensor."""
+
+    entity_description: EntityDescription
 
     def __init__(
         self,
         coordinator: DataUpdateCoordinator[State],
         device: Device,
         device_info: DeviceInfo,
+        entity_description: EntityDescription,
     ) -> None:
         """Init sensor."""
         super().__init__(coordinator)
-        self._attr_unique_id = f"{device.address}-grease-filter"
+        self.entity_description = entity_description
+
+        self._attr_unique_id = f"{device.address}-{entity_description.key}"
         self._attr_device_info = device_info
-        self._attr_name = f"{device_info['name']} Grease Filter"
-        self._attr_device_class = DEVICE_CLASS_PROBLEM
+        self._attr_name = f"{device_info['name']} {entity_description.name}"
 
     @property
     def is_on(self) -> bool | None:
         """Return true if the binary sensor is on."""
         if data := self.coordinator.data:
-            return data.grease_filter_full
-        return None
-
-
-class CarbonFilterSensor(CoordinatorEntity[State], BinarySensorEntity):
-    """Grease filter sensor."""
-
-    def __init__(
-        self,
-        coordinator: DataUpdateCoordinator[State],
-        device: Device,
-        device_info: DeviceInfo,
-    ) -> None:
-        """Init sensor."""
-        super().__init__(coordinator)
-        self._attr_unique_id = f"{device.address}-carbon-filter"
-        self._attr_device_info = device_info
-        self._attr_name = f"{device_info['name']} Carbon Filter"
-        self._attr_device_class = DEVICE_CLASS_PROBLEM
-
-    @property
-    def is_on(self) -> bool | None:
-        """Return true if the binary sensor is on."""
-        if data := self.coordinator.data:
-            return data.carbon_filter_full
+            return self.entity_description.is_on(data)
         return None
