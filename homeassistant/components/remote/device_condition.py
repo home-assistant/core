378c22e0fded173ab7e88c2cde6731526e3dd4a6
Ville Skyttä <ville.skytta@iki.fi>
2021-08-20 23:21:40 +0300

    Improve device condition type hinting (#54906)

--- homeassistant/components/remote/device_condition.py
+++ homeassistant/components/remote/device_condition.py
@@ -11,6 +11,8 @@ from homeassistant.helpers.typing import ConfigType
 
 from . import DOMAIN
 
+# mypy: disallow-any-generics
+
 CONDITION_SCHEMA = toggle_entity.CONDITION_SCHEMA.extend(
     {vol.Required(CONF_DOMAIN): DOMAIN}
 )
@@ -33,6 +35,8 @@ async def async_get_conditions(
     return await toggle_entity.async_get_conditions(hass, device_id, DOMAIN)
 
 
-async def async_get_condition_capabilities(hass: HomeAssistant, config: dict) -> dict:
+async def async_get_condition_capabilities(
+    hass: HomeAssistant, config: ConfigType
+) -> dict[str, vol.Schema]:
     """List condition capabilities."""
     return await toggle_entity.async_get_condition_capabilities(hass, config)
