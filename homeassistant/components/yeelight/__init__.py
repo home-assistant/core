e8865dff6b2969340320b9f8ad27247f30e67893
J. Nick Koston <nick@koston.org>
2021-08-20 19:09:22 -0500

    Make yeelight aware of the network integration (#54854)

--- homeassistant/components/yeelight/__init__.py
+++ homeassistant/components/yeelight/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import asyncio
 import contextlib
 from datetime import timedelta
+from ipaddress import IPv4Address, IPv6Address
 import logging
 from urllib.parse import urlparse
 
@@ -13,6 +14,7 @@ from yeelight import BulbException
 from yeelight.aio import KEY_CONNECTED, AsyncBulb
 
 from homeassistant import config_entries
+from homeassistant.components import network
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry, ConfigEntryNotReady
 from homeassistant.const import (
     CONF_DEVICES,
@@ -269,13 +271,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                 raise ConfigEntryNotReady from ex
             # Otherwise fall through to discovery
         else:
-            # manually added device
-            try:
-                await _async_initialize(
-                    hass, entry, entry.data[CONF_HOST], device=device
-                )
-            except BulbException as ex:
-                raise ConfigEntryNotReady from ex
+            # Since device is passed this cannot throw an exception anymore
+            await _async_initialize(hass, entry, entry.data[CONF_HOST], device=device)
             return True
 
     async def _async_from_discovery(capabilities: dict[str, str]) -> None:
@@ -367,34 +364,77 @@ class YeelightScanner:
         self._unique_id_capabilities = {}
         self._host_capabilities = {}
         self._track_interval = None
-        self._listener = None
-        self._connected_event = None
+        self._listeners = []
+        self._connected_events = []
 
     async def async_setup(self):
         """Set up the scanner."""
-        if self._connected_event:
-            await self._connected_event.wait()
+        if self._connected_events:
+            await asyncio.gather(*(event.wait() for event in self._connected_events))
             return
-        self._connected_event = asyncio.Event()
 
-        async def _async_connected():
-            self._listener.async_search()
-            self._connected_event.set()
+        for idx, source_ip in enumerate(await self._async_build_source_set()):
+            self._connected_events.append(asyncio.Event())
 
-        self._listener = SSDPListener(
-            async_callback=self._async_process_entry,
-            service_type=SSDP_ST,
-            target=SSDP_TARGET,
-            async_connect_callback=_async_connected,
+            def _wrap_async_connected_idx(idx):
+                """Create a function to capture the idx cell variable."""
+
+                async def _async_connected():
+                    self._connected_events[idx].set()
+
+                return _async_connected
+
+            self._listeners.append(
+                SSDPListener(
+                    async_callback=self._async_process_entry,
+                    service_type=SSDP_ST,
+                    target=SSDP_TARGET,
+                    source_ip=source_ip,
+                    async_connect_callback=_wrap_async_connected_idx(idx),
+                )
+            )
+
+        results = await asyncio.gather(
+            *(listener.async_start() for listener in self._listeners),
+            return_exceptions=True,
         )
-        await self._listener.async_start()
-        await self._connected_event.wait()
+        failed_listeners = []
+        for idx, result in enumerate(results):
+            if not isinstance(result, Exception):
+                continue
+            _LOGGER.warning(
+                "Failed to setup listener for %s: %s",
+                self._listeners[idx].source_ip,
+                result,
+            )
+            failed_listeners.append(self._listeners[idx])
+            self._connected_events[idx].set()
+
+        for listener in failed_listeners:
+            self._listeners.remove(listener)
+
+        await asyncio.gather(*(event.wait() for event in self._connected_events))
+        self.async_scan()
+
+    async def _async_build_source_set(self) -> set[IPv4Address]:
+        """Build the list of ssdp sources."""
+        adapters = await network.async_get_adapters(self._hass)
+        sources: set[IPv4Address] = set()
+        if network.async_only_default_interface_enabled(adapters):
+            sources.add(IPv4Address("0.0.0.0"))
+            return sources
+
+        return {
+            source_ip
+            for source_ip in await network.async_get_enabled_source_ips(self._hass)
+            if not source_ip.is_loopback and not isinstance(source_ip, IPv6Address)
+        }
 
     async def async_discover(self):
         """Discover bulbs."""
         await self.async_setup()
         for _ in range(DISCOVERY_ATTEMPTS):
-            self._listener.async_search()
+            self.async_scan()
             await asyncio.sleep(DISCOVERY_SEARCH_INTERVAL.total_seconds())
         return self._unique_id_capabilities.values()
 
@@ -402,7 +442,8 @@ class YeelightScanner:
     def async_scan(self, *_):
         """Send discovery packets."""
         _LOGGER.debug("Yeelight scanning")
-        self._listener.async_search()
+        for listener in self._listeners:
+            listener.async_search()
 
     async def async_get_capabilities(self, host):
         """Get capabilities via SSDP."""
@@ -413,7 +454,8 @@ class YeelightScanner:
         self._host_discovered_events.setdefault(host, []).append(host_event)
         await self.async_setup()
 
-        self._listener.async_search((host, SSDP_TARGET[1]))
+        for listener in self._listeners:
+            listener.async_search((host, SSDP_TARGET[1]))
 
         with contextlib.suppress(asyncio.TimeoutError):
             await asyncio.wait_for(host_event.wait(), timeout=DISCOVERY_TIMEOUT)
