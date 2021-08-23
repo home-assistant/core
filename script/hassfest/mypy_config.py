90788245569fc3cc03b7becc9093709769a99814
jan iversen <jancasacondor@gmail.com>
2021-08-23 20:22:55 +0200

    Activate mypy for timer (#55058)

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -136,7 +136,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.telegram_bot.*",
     "homeassistant.components.template.*",
     "homeassistant.components.tesla.*",
-    "homeassistant.components.timer.*",
     "homeassistant.components.todoist.*",
     "homeassistant.components.toon.*",
     "homeassistant.components.tplink.*",
