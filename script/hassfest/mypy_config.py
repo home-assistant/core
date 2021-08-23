65e6f22a16381674e91976aa238d7b7755658c9d
Erik <erik@montnemery.com>
2021-08-23 09:26:42 +0200

    Enable basic type checking for azure_event_hub

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -16,7 +16,6 @@ from .model import Config, Integration
 IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.almond.*",
     "homeassistant.components.awair.*",
-    "homeassistant.components.azure_event_hub.*",
     "homeassistant.components.blueprint.*",
     "homeassistant.components.bmw_connected_drive.*",
     "homeassistant.components.cert_expiry.*",
