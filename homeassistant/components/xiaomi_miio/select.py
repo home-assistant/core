81295fcaa99173aa470536b4a5d4591f7c31e26a
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-08-24 00:14:50 +0200

    Fix condition

--- homeassistant/components/xiaomi_miio/select.py
+++ homeassistant/components/xiaomi_miio/select.py
@@ -25,7 +25,9 @@ from .const import (
     MODEL_AIRFRESH_VA2,
     MODEL_AIRPURIFIER_M1,
     MODEL_AIRPURIFIER_M2,
-    MODELS_FAN_MIIO,
+    MODEL_FAN_ZA1,
+    MODEL_FAN_ZA3,
+    MODEL_FAN_ZA4,
     MODELS_HUMIDIFIER_MIIO,
     MODELS_HUMIDIFIER_MIOT,
     MODELS_PURIFIER_MIOT,
@@ -80,7 +82,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         entity_class = XiaomiAirPurifierMiotSelector
     elif model == MODEL_AIRFRESH_VA2:
         entity_class = XiaomiAirFreshSelector
-    elif model == MODELS_FAN_MIIO:
+    elif model in (MODEL_FAN_ZA1, MODEL_FAN_ZA3, MODEL_FAN_ZA4):
         entity_class = XiaomiFanSelector
     else:
         return
