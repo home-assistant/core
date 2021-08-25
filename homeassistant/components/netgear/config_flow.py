c88be23367ee2fd24bea957cf2f3fc7a8b68d327
starkillerOG <starkiller.og@gmail.com>
2021-08-25 19:45:38 +0200

    cleanup

--- homeassistant/components/netgear/config_flow.py
+++ homeassistant/components/netgear/config_flow.py
@@ -129,8 +129,9 @@ class NetgearFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_ssdp(self, discovery_info: dict):
         """Initialize flow from ssdp."""
-        await self.async_set_unique_id(discovery_info[ssdp.ATTR_UPNP_SERIAL])
         host = urlparse(discovery_info[ssdp.ATTR_SSDP_LOCATION]).hostname
+
+        await self.async_set_unique_id(discovery_info[ssdp.ATTR_UPNP_SERIAL])
         self._abort_if_unique_id_configured(updates={CONF_HOST: host})
 
         self.placeholders[CONF_NAME] = discovery_info[ssdp.ATTR_UPNP_MODEL_NUMBER]
