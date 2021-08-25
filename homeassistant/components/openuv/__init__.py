ec8276861d5208976792f59e77088899b9850492
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-08-25 11:44:17 +0200

    Update homeassistant/components/openuv/__init__.py
    Co-authored-by: Franck Nijhof <git@frenck.dev>

--- homeassistant/components/openuv/__init__.py
+++ homeassistant/components/openuv/__init__.py
@@ -179,7 +179,7 @@ class OpenUvEntity(Entity):
         """Initialize."""
         self._attr_extra_state_attributes = {ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION}
         self._attr_should_poll = False
-        self._attr_unique_id = f"{openuv.client.latitude}_{openuv.client.longitude}_{self.entity_description.key}"
+        self._attr_unique_id = f"{openuv.client.latitude}_{openuv.client.longitude}_{description.key}"
         self.entity_description = description
         self.openuv = openuv
 
