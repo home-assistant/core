06a30c882ff44dafe1bde58b7a41961043b9abfd
Franck Nijhof <git@frenck.dev>
2021-08-25 23:19:14 +0200

    Bump version to 2021.10.0dev0 (#55227)

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import Final
 
 MAJOR_VERSION: Final = 2021
-MINOR_VERSION: Final = 9
+MINOR_VERSION: Final = 10
 PATCH_VERSION: Final = "0.dev0"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"
