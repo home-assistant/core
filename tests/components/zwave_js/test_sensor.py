f5fe545c01147cdc76fc7ff3e9f815b61448892a
raman325 <7243222+raman325@users.noreply.github.com>
2021-08-27 16:23:51 -0400

    Test node status sensor is available even after client disconnects and reconnects

--- tests/components/zwave_js/test_sensor.py
+++ tests/components/zwave_js/test_sensor.py
@@ -23,6 +23,7 @@ from homeassistant.const import (
     ELECTRIC_POTENTIAL_VOLT,
     ENERGY_KILO_WATT_HOUR,
     POWER_WATT,
+    STATE_UNAVAILABLE,
     TEMP_CELSIUS,
 )
 from homeassistant.helpers import entity_registry as er
@@ -136,7 +137,7 @@ async def test_config_parameter_sensor(hass, lock_id_lock_as_id150, integration)
     assert entity_entry.disabled
 
 
-async def test_node_status_sensor(hass, lock_id_lock_as_id150, integration):
+async def test_node_status_sensor(hass, client, lock_id_lock_as_id150, integration):
     """Test node status sensor is created and gets updated on node state changes."""
     NODE_STATUS_ENTITY = "sensor.z_wave_module_for_id_lock_150_and_101_node_status"
     node = lock_id_lock_as_id150
@@ -178,6 +179,10 @@ async def test_node_status_sensor(hass, lock_id_lock_as_id150, integration):
     )
     node.receive_event(event)
     assert hass.states.get(NODE_STATUS_ENTITY).state == "alive"
+
+    # Disconnect the client and make sure the entity is still available
+    await client.disconnect()
+    assert hass.states.get(NODE_STATUS_ENTITY).state != STATE_UNAVAILABLE
 
 
 async def test_reset_meter(
