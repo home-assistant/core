161eb2e8341c01b568a837f062bdb0932db11ea1
Aaron Bach <bachya1208@gmail.com>
2021-08-28 16:14:37 -0600

    Code review

--- homeassistant/components/ambient_station/__init__.py
+++ homeassistant/components/ambient_station/__init__.py
@@ -216,6 +216,8 @@ class AmbientStation:
 class AmbientWeatherEntity(Entity):
     """Define a base Ambient PWS entity."""
 
+    _attr_should_poll = False
+
     def __init__(
         self,
         ambient: AmbientStation,
@@ -231,7 +233,6 @@ class AmbientWeatherEntity(Entity):
             "manufacturer": "Ambient Weather",
         }
         self._attr_name = f"{station_name}_{description.name}"
-        self._attr_should_poll = False
         self._attr_unique_id = f"{mac_address}_{description.key}"
         self._mac_address = mac_address
         self.entity_description = description
