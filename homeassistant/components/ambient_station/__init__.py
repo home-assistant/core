e7e0a3b40bd137765cccad67a5f09ca6a4179355
Aaron Bach <bachya1208@gmail.com>
2021-08-27 21:14:50 -0600

    Remove comment

--- homeassistant/components/ambient_station/__init__.py
+++ homeassistant/components/ambient_station/__init__.py
@@ -52,8 +52,6 @@ def async_wm2_to_lx(value: float) -> float:
 def async_hydrate_station_data(data: dict[str, Any]) -> dict[str, Any]:
     """Hydrate station data with addition or normalized data."""
     if TYPE_SOLARRADIATION in data:
-        # If the user is monitoring brightness (in W/m^2), make sure we also add a
-        # calculated sensor for the same data measured in lx:
         data[TYPE_SOLARRADIATION_LX] = async_wm2_to_lx(data[TYPE_SOLARRADIATION])
 
     return data
