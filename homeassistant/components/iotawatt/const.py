c7a8d722574796ac7f49525f36083445ca8b3225
Greg <greg.diehl.gtd@gmail.com>
2021-08-29 03:23:43 +0000

    Removing unused consts

--- homeassistant/components/iotawatt/const.py
+++ homeassistant/components/iotawatt/const.py
@@ -4,6 +4,4 @@ DEFAULT_ICON = "mdi:flash"
 DEFAULT_SCAN_INTERVAL = 30
 DOMAIN = "iotawatt"
 COORDINATOR = "coordinator"
-IOTAWATT_API = "iotawatt_api"
 SIGNAL_ADD_DEVICE = "iotawatt_add_device"
-SIGNAL_DELETE_DEVICE = "iotawatt_delete_device"
