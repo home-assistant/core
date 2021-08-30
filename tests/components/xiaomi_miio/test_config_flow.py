e51a56580afda3a545c68c574129d0aba9f9af64
starkillerOG <starkiller.og@gmail.com>
2021-08-30 23:04:46 +0200

    add test

--- tests/components/xiaomi_miio/test_config_flow.py
+++ tests/components/xiaomi_miio/test_config_flow.py
@@ -1,6 +1,7 @@
 """Test the Xiaomi Miio config flow."""
 from unittest.mock import Mock, patch
 
+from micloud.micloudexception import MiCloudAccessDenied
 from miio import DeviceException
 import pytest
 
@@ -287,6 +288,23 @@ async def test_config_flow_gateway_cloud_login_error(hass):
     with patch(
         "homeassistant.components.xiaomi_miio.config_flow.MiCloud.login",
         return_value=False,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                const.CONF_CLOUD_USERNAME: TEST_CLOUD_USER,
+                const.CONF_CLOUD_PASSWORD: TEST_CLOUD_PASS,
+                const.CONF_CLOUD_COUNTRY: TEST_CLOUD_COUNTRY,
+            },
+        )
+
+    assert result["type"] == "form"
+    assert result["step_id"] == "cloud"
+    assert result["errors"] == {"base": "cloud_login_error"}
+
+    with patch(
+        "homeassistant.components.xiaomi_miio.config_flow.MiCloud.login",
+        side_effect=MiCloudAccessDenied({}),
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
