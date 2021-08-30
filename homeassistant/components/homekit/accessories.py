84f9ad73448ec474b360c758d8e6ce410e34fa8d
ha0y <30557072+ha0y@users.noreply.github.com>
2021-08-30 13:28:26 -0700

    Add input_select and select domain support for HomeKit (#54760)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/homekit/accessories.py
+++ homeassistant/components/homekit/accessories.py
@@ -198,6 +198,9 @@ def get_accessory(hass, driver, state, aid, config):  # noqa: C901
     elif state.domain in ("automation", "input_boolean", "remote", "scene", "script"):
         a_type = "Switch"
 
+    elif state.domain in ("input_select", "select"):
+        a_type = "SelectSwitch"
+
     elif state.domain == "water_heater":
         a_type = "WaterHeater"
 
