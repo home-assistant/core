243333b995cf60795c07c42b592ab4552927f030
Stefan <37924749+stefanroelofs@users.noreply.github.com>
2021-09-01 10:59:34 +0200

    Fixes moon phases
    The moon entity state would never be any of the primary moon phases (New Moon, First Quarter, Full Moon, Third Quarter).
    This because `self._state` is a floating point, and is never exactly 0 or 7 or 14 or 21.
    Now the moon has the state `New Moon` for (about) 24h around the event.

--- homeassistant/components/moon/sensor.py
+++ homeassistant/components/moon/sensor.py
@@ -62,19 +62,19 @@ class MoonSensor(SensorEntity):
     @property
     def native_value(self):
         """Return the state of the device."""
-        if self._state == 0:
+        if self._state < 0.5 or self._state > 27.5:
             return STATE_NEW_MOON
-        if self._state < 7:
+        elif self._state < 6.5:
             return STATE_WAXING_CRESCENT
-        if self._state == 7:
+        elif self._state < 7.5:
             return STATE_FIRST_QUARTER
-        if self._state < 14:
+        elif self._state < 13.5:
             return STATE_WAXING_GIBBOUS
-        if self._state == 14:
+        elif self._state < 14.5:
             return STATE_FULL_MOON
-        if self._state < 21:
+        elif self._state < 20.5:
             return STATE_WANING_GIBBOUS
-        if self._state == 21:
+        elif self._state < 21.5:
             return STATE_LAST_QUARTER
         return STATE_WANING_CRESCENT
 
