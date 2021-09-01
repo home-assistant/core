80ced6b5cfb5e36c987f8b6c7f0f99ff62ec63ec
Duco Sebel <d.sebel@homewizard.nl>
2021-09-01 21:01:51 +0000

    Clean up config flow

--- homeassistant/components/homewizard_energy/config_flow.py
+++ homeassistant/components/homewizard_energy/config_flow.py
@@ -1,12 +1,10 @@
 """Config flow for Homewizard Energy."""
-import asyncio
 import logging
 from typing import Optional
 
 import aiohwenergy
 from aiohwenergy.hwenergy import SUPPORTED_DEVICES
 import async_timeout
-import voluptuous as vol
 from voluptuous import All, Length, Required, Schema
 from voluptuous.util import Lower
 
@@ -38,69 +36,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is None:
             return self.async_show_form(
                 step_id="user",
-                data_schema=vol.Schema(
+                data_schema=Schema(
                     {
-                        vol.Required(CONF_IP_ADDRESS): str,
+                        Required(CONF_IP_ADDRESS): str,
                     }
                 ),
                 errors=None,
             )
 
-        # Make connection with device
-        energy_api = aiohwenergy.HomeWizardEnergy(user_input[CONF_IP_ADDRESS])
-
-        initialized = False
-        try:
-            with async_timeout.timeout(10):
-                await energy_api.initialize()
-                if energy_api.device is not None:
-                    initialized = True
-        except (asyncio.TimeoutError, aiohwenergy.RequestError):
-            _LOGGER.error(
-                "Error connecting to the Energy device at %s",
-                energy_api._host,
-            )
-            return self.async_abort(reason="manual_config_request_error")
-
-        except aiohwenergy.DisabledError:
-            _LOGGER.error("API disabled, API must be enabled in the app")
-            return self.async_abort(reason="api_not_enabled")
-
-        except aiohwenergy.AiohwenergyException:
-            _LOGGER.error("Unknown Energy API error occurred")
-            return self.async_abort(reason="manual_config_unknown_error")
-
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.error(
-                "Unknown error connecting with Energy Device at %s",
-                energy_api._host["host"],
-            )
-            return self.async_abort(reason="manual_config_unknown_error")
-
-        finally:
-            _LOGGER.debug("Manual configuration finished")
-            await energy_api.close()
-
-        if not initialized:
-            _LOGGER.error("Config finished but not initialized")
-            return self.async_abort(reason="manual_config_unknown_error")
-
-        # Validate metadata
-        if energy_api.device.api_version != "v1":
-            return self.async_abort(reason="manual_config_unsupported_api_version")
-
-        # Configure device
-        entry_info = {
-            "host": user_input[CONF_IP_ADDRESS],
-            "port": 80,
-            "api_enabled": "1",
-            "path": "/api/v1",
-            "product_name": energy_api.device.product_name,
-            "product_type": energy_api.device.product_type,
-            "serial": energy_api.device.serial,
-        }
-
-        _LOGGER.debug(entry_info)
+        entry_info = {CONF_IP_ADDRESS: user_input[CONF_IP_ADDRESS]}
 
         return await self.async_step_check(entry_info)
 
@@ -109,24 +53,28 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         _LOGGER.debug("config_flow async_step_zeroconf")
 
+        # Validate doscovery entry
+        if discovery_info["host"] is None:
+            return self.async_abort(reason="invalid_discovery_parameters")
+
+        if (
+            "api_enabled" not in discovery_info["properties"]
+            or "path" not in discovery_info["properties"]
+            or "product_name" not in discovery_info["properties"]
+            or "product_type" not in discovery_info["properties"]
+            or "serial" not in discovery_info["properties"]
+        ):
+            return self.async_abort(reason="invalid_discovery_parameters")
+
+        if (discovery_info["properties"]["path"]) != "/api/v1":
+            return self.async_abort(reason="unsupported_api_version")
+
+        if (discovery_info["properties"]["api_enabled"]) != "1":
+            return self.async_abort(reason="api_not_enabled")
+
+        # Pass parameters
         entry_info = {
-            "host": discovery_info["host"],
-            "port": discovery_info["port"],
-            "api_enabled": discovery_info["properties"]["api_enabled"]
-            if "api_enabled" in discovery_info["properties"]
-            else None,
-            "path": discovery_info["properties"]["path"]
-            if "path" in discovery_info["properties"]
-            else None,
-            "product_name": discovery_info["properties"]["product_name"]
-            if "product_name" in discovery_info["properties"]
-            else None,
-            "product_type": discovery_info["properties"]["product_type"]
-            if "product_type" in discovery_info["properties"]
-            else None,
-            "serial": discovery_info["properties"]["serial"]
-            if "serial" in discovery_info["properties"]
-            else None,
+            CONF_IP_ADDRESS: discovery_info["host"],
         }
 
         return await self.async_step_check(entry_info)
@@ -136,46 +84,59 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         _LOGGER.debug("config_flow async_step_check")
 
-        if entry_info["product_type"] not in SUPPORTED_DEVICES:
-            _LOGGER.warning(
-                "Device (%s) not supported by integration", entry_info["product_type"]
-            )
-            # return self.async_abort(reason="device_not_supported")
+        # Make connection with device
+        energy_api = aiohwenergy.HomeWizardEnergy(entry_info[CONF_IP_ADDRESS])
 
-        if entry_info["api_enabled"] != "1":
-            _LOGGER.warning("API not enabled, please enable API in app")
+        initialized = False
+        try:
+            with async_timeout.timeout(10):
+                await energy_api.initialize()
+                if energy_api.device is not None:
+                    initialized = True
+
+        except aiohwenergy.DisabledError:
+            _LOGGER.error("API disabled, API must be enabled in the app")
             return self.async_abort(reason="api_not_enabled")
 
-        _LOGGER.debug("entry_info: %s", entry_info)
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.error(
+                "Error connecting with Energy Device at %s",
+                entry_info[CONF_IP_ADDRESS],
+            )
+            return self.async_abort(reason="unknown_error")
 
-        if (
-            ("host" not in entry_info)
-            or ("port" not in entry_info)
-            or ("serial" not in entry_info)
-            or ("product_name" not in entry_info)
-            or ("product_type" not in entry_info)
-            or ("path" not in entry_info)
-            or ("api_enabled" not in entry_info)
-        ):
-            _LOGGER.warning("Invalid discovery parameters")
-            return self.async_abort(reason="invalid_discovery_parameters")
+        finally:
+            await energy_api.close()
 
-        self.context["host"] = entry_info["host"]
-        self.context["unique_id"] = "{}_{}".format(
-            entry_info["product_type"],
-            entry_info["serial"],
-        )
-        self.context["serial"] = entry_info["serial"]
-        self.context["port"] = entry_info["port"]
-        self.context["path"] = entry_info["path"]
+        if not initialized:
+            _LOGGER.error("Initialization failed")
+            return self.async_abort(reason="unknown_error")
+
+        # Validate metadata
+        if energy_api.device.api_version != "v1":
+            return self.async_abort(reason="unsupported_api_version")
+
+        if energy_api.device.product_type not in SUPPORTED_DEVICES:
+            _LOGGER.error(
+                "Device (%s) not supported by integration", entry_info["product_type"]
+            )
+            return self.async_abort(reason="device_not_supported")
+
+        # Configure device
+        entry_info["product_name"] = energy_api.device.product_name
+        entry_info["product_type"] = energy_api.device.product_type
+        entry_info["serial"] = energy_api.device.product_name
+
+        self.context["host"] = entry_info[CONF_IP_ADDRESS]
         self.context["product_name"] = entry_info["product_name"]
         self.context["product_type"] = entry_info["product_type"]
-        self.context["api_enabled"] = entry_info["api_enabled"]
-
-        self.context["name"] = "{} ({})".format(
-            self.context["product_name"],
-            self.context["serial"][-6:],
-        )
+        self.context["serial"] = entry_info["serial"]
+        self.context[
+            "unique_id"
+        ] = f"{entry_info['product_type']}_{entry_info['serial']}"
+        self.context[
+            "name"
+        ] = f"{self.context['product_name']} ({self.context['serial'][-6:]})"
 
         await self.async_set_unique_id(self.context["unique_id"])
         self._abort_if_unique_id_configured(updates=entry_info)
@@ -192,32 +153,22 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         _LOGGER.debug("config_flow async_step_confirm")
 
-        errors = {}
-
-        schema = Schema(
-            {
-                Required("name", default=self.context["product_name"]): All(
-                    str, Length(min=1)
-                )
-            }
-        )
-
         if user_input is None:
             return self.async_show_form(
                 step_id="confirm",
                 description_placeholders={"name": self.context["product_name"]},
-                data_schema=schema,
-                errors=errors,
+                data_schema=Schema(
+                    {
+                        Required("name", default=self.context["product_name"]): All(
+                            str, Length(min=1)
+                        )
+                    }
+                ),
+                errors=None,
             )
 
-        if self.context["api_enabled"] != "1":
-            _LOGGER.warning("API not enabled")
-            return self.async_abort(reason="api_not_enabled")
-
-        _LOGGER.debug("async_step_confirm _create_entry")
-        self.context["custom_name"] = (
-            user_input["name"] if user_input["name"] != "" else self.context["name"]
-        )
+        # Format name
+        self.context["custom_name"] = user_input["name"]
         if Lower(self.context["product_name"]) != Lower(user_input["name"]):
             title = "{} ({})".format(
                 self.context["product_name"],
@@ -226,6 +177,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         else:
             title = self.context["custom_name"]
 
+        # Finish up
         return self.async_create_entry(
             title=title,
             data=self.context,
