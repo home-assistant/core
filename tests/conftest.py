292cbd0b2c6268ec97ed5f6b80fcd94ee5631e0f
Erik <erik@montnemery.com>
2021-09-02 09:13:10 +0200

    Remove things from the future

--- tests/conftest.py
+++ tests/conftest.py
@@ -332,7 +332,7 @@ def hass_client(hass, aiohttp_client, hass_access_token):
 
 
 @pytest.fixture
-def hass_client_no_auth(hass, aiohttp_client, socket_enabled):
+def hass_client_no_auth(hass, aiohttp_client):
     """Return an unauthenticated HTTP client."""
 
     async def client():
