e05951d2e9d58b251c71f8139e7e2c231edb74db
Aidan Timson <contact@timmo.xyz>
2021-09-03 22:21:36 +0100

    Move refresh above hass.data

--- homeassistant/components/azure_devops/__init__.py
+++ homeassistant/components/azure_devops/__init__.py
@@ -74,10 +74,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         update_interval=timedelta(seconds=300),
     )
 
+    await coordinator.async_config_entry_first_refresh()
+
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator, project
-
-    await coordinator.async_config_entry_first_refresh()
 
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
