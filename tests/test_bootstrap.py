0cebaa23ea69552ca39dd7473111e00893530fd8
Paulus Schoutsen <balloob@gmail.com>
2021-09-03 09:12:02 -0700

    Guard for doRollover failing

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -56,11 +56,14 @@ async def test_home_assistant_core_config_validation(hass):
     assert result is None
 
 
-async def test_async_enable_logging(hass):
+async def test_async_enable_logging(hass, caplog):
     """Test to ensure logging is migrated to the queue handlers."""
     with patch("logging.getLogger"), patch(
         "homeassistant.bootstrap.async_activate_log_queue_handler"
-    ) as mock_async_activate_log_queue_handler:
+    ) as mock_async_activate_log_queue_handler, patch(
+        "homeassistant.bootstrap.logging.handlers.RotatingFileHandler.doRollover",
+        side_effect=OSError,
+    ):
         bootstrap.async_enable_logging(hass)
         mock_async_activate_log_queue_handler.assert_called_once()
         mock_async_activate_log_queue_handler.reset_mock()
@@ -74,6 +77,8 @@ async def test_async_enable_logging(hass):
             os.remove(f)
         for f in glob.glob("testing_config/home-assistant.log*"):
             os.remove(f)
+
+    assert "Error rolling over log file" in caplog.text
 
 
 async def test_load_hassio(hass):
