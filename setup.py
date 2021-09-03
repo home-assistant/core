81f4fa740cb8e646daf1e6319b1b62220b8e081b
Paulus Schoutsen <balloob@gmail.com>
2021-09-03 10:05:19 -0700

    Drop unused ruamel

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ REQUIRES = [
     "python-slugify==4.0.1",
     "pyyaml==5.4.1",
     "requests==2.25.1",
-    "ruamel.yaml==0.15.100",
     "voluptuous==0.12.1",
     "voluptuous-serialize==2.4.0",
     "yarl==1.6.3",
