62d626d9bb89a537c3cd150494024249f0bfe680
Ofir Haviv <ofirhavi@microsoft.com>
2021-09-05 17:11:19 +0000

    Fixed bdraco comments

--- homeassistant/components/contec_controllers/light.py
+++ homeassistant/components/contec_controllers/light.py
@@ -20,7 +20,7 @@ async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
-):
+) -> None:
     """Set up the Contec Lights."""
     controllerManager: ControllerManager = hass.data[DOMAIN][config_entry.entry_id]
     allLights: List[ContecLight] = [
@@ -34,7 +34,7 @@ class ContecLight(LightEntity):
 
     _on_off_activation: ContecOnOffActivation
 
-    def __init__(self, on_off_activation: ContecOnOffActivation):
+    def __init__(self, on_off_activation: ContecOnOffActivation) -> None:
         """Initialize an ContecLight."""
         self._on_off_activation = on_off_activation
         self._attr_unique_id = f"{on_off_activation.ControllerUnit.UnitId}-{on_off_activation.StartActivationNumber}"
@@ -46,19 +46,19 @@ class ContecLight(LightEntity):
         """Return true if light is on."""
         return self._on_off_activation.IsOn
 
-    async def async_added_to_hass(self):
+    async def async_added_to_hass(self) -> None:
         """Subscribe to changes in on/off activation."""
 
         @callback
-        def StateUpdated(isOn: bool):
+        def _async_state_updated(isOn: bool) -> None:
             self.async_write_ha_state()
 
-        self._on_off_activation.SetStateChangedCallback(StateUpdated)
+        self._on_off_activation.SetStateChangedCallback(_async_state_updated)
 
     async def async_turn_on(self, **kwargs) -> None:
         """Instruct the light to turn on."""
         await self._on_off_activation.SetActivationState(True)
 
-    async def async_turn_off(self, **kwargs):
+    async def async_turn_off(self, **kwargs) -> None:
         """Instruct the light to turn off."""
         await self._on_off_activation.SetActivationState(False)
