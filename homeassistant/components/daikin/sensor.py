efa820ca84739380b2fe3c2f5022f2709ca9e988
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-09-08 00:28:23 +0200

    Move SENSOR_TYPES to the top

--- homeassistant/components/daikin/sensor.py
+++ homeassistant/components/daikin/sensor.py
@@ -32,71 +32,6 @@ from .const import (
 )
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Old way of setting up the Daikin sensors.
-
-    Can only be called when a user accidentally mentions the platform in their
-    config. But even in that case it would have been ignored.
-    """
-
-
-async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up Daikin climate based on config_entry."""
-    daikin_api = hass.data[DAIKIN_DOMAIN].get(entry.entry_id)
-    sensors = [ATTR_INSIDE_TEMPERATURE]
-    if daikin_api.device.support_outside_temperature:
-        sensors.append(ATTR_OUTSIDE_TEMPERATURE)
-    if daikin_api.device.support_energy_consumption:
-        sensors.append(ATTR_TOTAL_POWER)
-        sensors.append(ATTR_COOL_ENERGY)
-        sensors.append(ATTR_HEAT_ENERGY)
-    if daikin_api.device.support_humidity:
-        sensors.append(ATTR_HUMIDITY)
-        sensors.append(ATTR_TARGET_HUMIDITY)
-    if daikin_api.device.support_compressor_frequency:
-        sensors.append(ATTR_COMPRESSOR_FREQUENCY)
-
-    entities = [
-        DaikinSensor(daikin_api, description)
-        for description in SENSOR_TYPES
-        if description.key in sensors
-    ]
-    async_add_entities(entities)
-
-
-class DaikinSensor(SensorEntity):
-    """Representation of a Sensor."""
-
-    entity_description: DaikinSensorEntityDescription
-
-    def __init__(
-        self, api: DaikinApi, description: DaikinSensorEntityDescription
-    ) -> None:
-        """Initialize the sensor."""
-        self.entity_description = description
-        self._api = api
-        self._attr_name = f"{api.name} {description.name}"
-
-    @property
-    def unique_id(self):
-        """Return a unique ID."""
-        return f"{self._api.device.mac}-{self.entity_description.key}"
-
-    @property
-    def native_value(self) -> float | None:
-        """Return the state of the sensor."""
-        return self.entity_description.value_func(self._api.device)
-
-    async def async_update(self):
-        """Retrieve latest state."""
-        await self._api.async_update()
-
-    @property
-    def device_info(self):
-        """Return a device description for device registry."""
-        return self._api.device_info
-
-
 @dataclass
 class DaikinRequiredKeysMixin:
     """Mixin for required keys."""
@@ -169,3 +104,68 @@ SENSOR_TYPES: tuple[DaikinSensorEntityDescription, ...] = (
         value_func=lambda device: device.compressor_frequency,
     ),
 )
+
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Old way of setting up the Daikin sensors.
+
+    Can only be called when a user accidentally mentions the platform in their
+    config. But even in that case it would have been ignored.
+    """
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up Daikin climate based on config_entry."""
+    daikin_api = hass.data[DAIKIN_DOMAIN].get(entry.entry_id)
+    sensors = [ATTR_INSIDE_TEMPERATURE]
+    if daikin_api.device.support_outside_temperature:
+        sensors.append(ATTR_OUTSIDE_TEMPERATURE)
+    if daikin_api.device.support_energy_consumption:
+        sensors.append(ATTR_TOTAL_POWER)
+        sensors.append(ATTR_COOL_ENERGY)
+        sensors.append(ATTR_HEAT_ENERGY)
+    if daikin_api.device.support_humidity:
+        sensors.append(ATTR_HUMIDITY)
+        sensors.append(ATTR_TARGET_HUMIDITY)
+    if daikin_api.device.support_compressor_frequency:
+        sensors.append(ATTR_COMPRESSOR_FREQUENCY)
+
+    entities = [
+        DaikinSensor(daikin_api, description)
+        for description in SENSOR_TYPES
+        if description.key in sensors
+    ]
+    async_add_entities(entities)
+
+
+class DaikinSensor(SensorEntity):
+    """Representation of a Sensor."""
+
+    entity_description: DaikinSensorEntityDescription
+
+    def __init__(
+        self, api: DaikinApi, description: DaikinSensorEntityDescription
+    ) -> None:
+        """Initialize the sensor."""
+        self.entity_description = description
+        self._api = api
+        self._attr_name = f"{api.name} {description.name}"
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return f"{self._api.device.mac}-{self.entity_description.key}"
+
+    @property
+    def native_value(self) -> float | None:
+        """Return the state of the sensor."""
+        return self.entity_description.value_func(self._api.device)
+
+    async def async_update(self):
+        """Retrieve latest state."""
+        await self._api.async_update()
+
+    @property
+    def device_info(self):
+        """Return a device description for device registry."""
+        return self._api.device_info
