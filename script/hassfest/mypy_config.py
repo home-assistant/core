744f8531569ba43a5a1e35768f2bd709aa090c33
jan Iversen <jancasacondor@gmail.com>
2021-09-08 16:10:54 +0200

    Activate mypy.

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -29,7 +29,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.doorbird.*",
     "homeassistant.components.elkm1.*",
     "homeassistant.components.enphase_envoy.*",
-    "homeassistant.components.entur_public_transport.*",
     "homeassistant.components.evohome.*",
     "homeassistant.components.fireservicerota.*",
     "homeassistant.components.firmata.*",
