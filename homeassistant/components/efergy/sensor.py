e0ae11a813369b0022b6f6cdf136ed2ffdf18fe9
Robert Hillis <tkdrob4390@yahoo.com>
2021-09-09 07:08:37 -0400

    Use dict for SensorEntityDescription

--- homeassistant/components/efergy/sensor.py
+++ homeassistant/components/efergy/sensor.py
@@ -1,19 +1,20 @@
 """Support for Efergy sensors."""
+from __future__ import annotations
+
 import logging
+from typing import Any
 
 import requests
 import voluptuous as vol
 
 from homeassistant.components.sensor import (
-    ATTR_STATE_CLASS,
     PLATFORM_SCHEMA,
+    STATE_CLASS_MEASUREMENT,
     STATE_CLASS_TOTAL_INCREASING,
     SensorEntity,
+    SensorEntityDescription,
 )
 from homeassistant.const import (
-    ATTR_DEVICE_CLASS,
-    ATTR_NAME,
-    ATTR_UNIT_OF_MEASUREMENT,
     CONF_CURRENCY,
     CONF_MONITORED_VARIABLES,
     CONF_TYPE,
@@ -23,7 +24,10 @@ from homeassistant.const import (
     ENERGY_KILO_WATT_HOUR,
     POWER_WATT,
 )
+from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 _LOGGER = logging.getLogger(__name__)
 _RESOURCE = "https://engage.efergy.com/mobile_proxy/"
@@ -42,31 +46,38 @@ CONF_CURRENT_VALUES = "current_values"
 DEFAULT_PERIOD = "year"
 DEFAULT_UTC_OFFSET = "0"
 
-SENSOR_TYPES = {
-    CONF_INSTANT: {
-        ATTR_NAME: "Energy Usage",
-        ATTR_UNIT_OF_MEASUREMENT: POWER_WATT,
-        ATTR_DEVICE_CLASS: DEVICE_CLASS_POWER,
-        ATTR_STATE_CLASS: STATE_CLASS_TOTAL_INCREASING,
-    },
-    CONF_AMOUNT: {
-        ATTR_NAME: "Energy Consumed",
-        ATTR_UNIT_OF_MEASUREMENT: ENERGY_KILO_WATT_HOUR,
-        ATTR_DEVICE_CLASS: DEVICE_CLASS_ENERGY,
-        ATTR_STATE_CLASS: STATE_CLASS_TOTAL_INCREASING,
-    },
-    CONF_BUDGET: {ATTR_NAME: "Energy Budget"},
-    CONF_COST: {
-        ATTR_NAME: "Energy Cost",
-        ATTR_DEVICE_CLASS: DEVICE_CLASS_MONETARY,
-        ATTR_STATE_CLASS: STATE_CLASS_TOTAL_INCREASING,
-    },
-    CONF_CURRENT_VALUES: {
-        ATTR_NAME: "Per-Device Usage",
-        ATTR_UNIT_OF_MEASUREMENT: POWER_WATT,
-        ATTR_DEVICE_CLASS: DEVICE_CLASS_POWER,
-        ATTR_STATE_CLASS: STATE_CLASS_TOTAL_INCREASING,
-    },
+SENSOR_TYPES: dict[str, SensorEntityDescription] = {
+    CONF_INSTANT: SensorEntityDescription(
+        key=CONF_INSTANT,
+        name="Energy Usage",
+        device_class=DEVICE_CLASS_POWER,
+        native_unit_of_measurement=POWER_WATT,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    CONF_AMOUNT: SensorEntityDescription(
+        key=CONF_AMOUNT,
+        name="Energy Consumed",
+        device_class=DEVICE_CLASS_ENERGY,
+        native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        state_class=STATE_CLASS_TOTAL_INCREASING,
+    ),
+    CONF_BUDGET: SensorEntityDescription(
+        key=CONF_BUDGET,
+        name="Energy Budget",
+    ),
+    CONF_COST: SensorEntityDescription(
+        key=CONF_COST,
+        name="Energy Cost",
+        device_class=DEVICE_CLASS_MONETARY,
+        state_class=STATE_CLASS_TOTAL_INCREASING,
+    ),
+    CONF_CURRENT_VALUES: SensorEntityDescription(
+        key=CONF_CURRENT_VALUES,
+        name="Per-Device Usage",
+        device_class=DEVICE_CLASS_POWER,
+        native_unit_of_measurement=POWER_WATT,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
 }
 
 TYPES_SCHEMA = vol.In(SENSOR_TYPES)
@@ -88,13 +99,19 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType = None,
+) -> None:
     """Set up the Efergy sensor."""
     app_token = config.get(CONF_APPTOKEN)
     utc_offset = str(config.get(CONF_UTC_OFFSET))
 
     dev = []
     for variable in config[CONF_MONITORED_VARIABLES]:
+        _LOGGER.warning(variable)
         if variable[CONF_TYPE] == CONF_CURRENT_VALUES:
             url_string = f"{_RESOURCE}getCurrentValuesSummary?token={app_token}"
             response = requests.get(url_string, timeout=10)
@@ -102,21 +119,21 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
                 sid = sensor["sid"]
                 dev.append(
                     EfergySensor(
-                        variable[CONF_TYPE],
                         app_token,
                         utc_offset,
                         variable[CONF_PERIOD],
                         variable[CONF_CURRENCY],
-                        sid,
+                        SENSOR_TYPES[variable[CONF_TYPE]],
+                        sid=sid,
                     )
                 )
         dev.append(
             EfergySensor(
-                variable[CONF_TYPE],
                 app_token,
                 utc_offset,
                 variable[CONF_PERIOD],
                 variable[CONF_CURRENCY],
+                SENSOR_TYPES[variable[CONF_TYPE]],
             )
         )
 
@@ -126,45 +143,47 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class EfergySensor(SensorEntity):
     """Implementation of an Efergy sensor."""
 
-    def __init__(self, sensor_type, app_token, utc_offset, period, currency, sid=None):
+    def __init__(
+        self,
+        app_token: Any,
+        utc_offset: str,
+        period: str,
+        currency: str,
+        description: SensorEntityDescription,
+        sid: str = None,
+    ) -> None:
         """Initialize the sensor."""
+        self.entity_description = description
+        _LOGGER.warning(description)
         self.sid = sid
-        sensor = SENSOR_TYPES[sensor_type]
         if sid:
             self._attr_name = f"efergy_{sid}"
-        else:
-            self._attr_name = sensor.get(ATTR_NAME)
-        self.type = sensor_type
         self.app_token = app_token
         self.utc_offset = utc_offset
         self.period = period
-        if sensor_type == "cost":
+        if description.key == CONF_COST:
             self._attr_native_unit_of_measurement = f"{currency}/{period}"
-        else:
-            self._attr_native_unit_of_measurement = sensor.get(ATTR_UNIT_OF_MEASUREMENT)
-        self._attr_device_class = sensor.get(ATTR_DEVICE_CLASS)
-        self._attr_state_class = sensor.get(ATTR_STATE_CLASS)
 
-    def update(self):
+    def update(self) -> None:
         """Get the Efergy monitor data from the web service."""
         try:
-            if self.type == "instant_readings":
+            if self.entity_description.key == CONF_INSTANT:
                 url_string = f"{_RESOURCE}getInstant?token={self.app_token}"
                 response = requests.get(url_string, timeout=10)
                 self._attr_native_value = response.json()["reading"]
-            elif self.type == "amount":
+            elif self.entity_description.key == CONF_AMOUNT:
                 url_string = f"{_RESOURCE}getEnergy?token={self.app_token}&offset={self.utc_offset}&period={self.period}"
                 response = requests.get(url_string, timeout=10)
                 self._attr_native_value = response.json()["sum"]
-            elif self.type == "budget":
+            elif self.entity_description.key == CONF_BUDGET:
                 url_string = f"{_RESOURCE}getBudget?token={self.app_token}"
                 response = requests.get(url_string, timeout=10)
                 self._attr_native_value = response.json()["status"]
-            elif self.type == "cost":
+            elif self.entity_description.key == CONF_COST:
                 url_string = f"{_RESOURCE}getCost?token={self.app_token}&offset={self.utc_offset}&period={self.period}"
                 response = requests.get(url_string, timeout=10)
                 self._attr_native_value = response.json()["sum"]
-            elif self.type == "current_values":
+            elif self.entity_description.key == CONF_CURRENT_VALUES:
                 url_string = (
                     f"{_RESOURCE}getCurrentValuesSummary?token={self.app_token}"
                 )
