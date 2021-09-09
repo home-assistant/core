9d2861afe3c7a305593dec275af868d037c24ecb
jan iversen <jancasacondor@gmail.com>
2021-09-09 13:14:28 +0200

    Add mypy to elkm1. (#55964)

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -27,7 +27,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.dhcp.*",
     "homeassistant.components.directv.*",
     "homeassistant.components.doorbird.*",
-    "homeassistant.components.elkm1.*",
     "homeassistant.components.enphase_envoy.*",
     "homeassistant.components.entur_public_transport.*",
     "homeassistant.components.evohome.*",
