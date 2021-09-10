2b1b5fbbd7d58a6a5626f35ca8183cabf382fec5
Glen Eustace <88570674+geustace@users.noreply.github.com>
2021-09-10 19:05:19 +1200

    Update homeassistant/components/enasolar/sensor.py
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/enasolar/sensor.py
+++ homeassistant/components/enasolar/sensor.py
@@ -104,7 +104,6 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                 )
 
     async_add_entities([*hass_meter_sensors, *hass_data_sensors])
-    async_add_entities(hass_data_sensors)
 
     async def async_enasolar_meters():
         """Update the EnaSolar Meter sensors."""
