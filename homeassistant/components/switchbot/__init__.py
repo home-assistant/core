9beae857807f4be207f048cab4b2a8fa5261b3fc
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2021-09-10 06:42:46 +0200

    Update homeassistant/components/switchbot/__init__.py
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/switchbot/__init__.py
+++ homeassistant/components/switchbot/__init__.py
@@ -85,7 +85,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     if unload_ok:
-        hass.data[DOMAIN][entry.entry_id][DATA_UNDO_UPDATE_LISTENER]()
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
