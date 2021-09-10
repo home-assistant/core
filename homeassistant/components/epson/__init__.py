c8d195d5bbfd2cca0281a346f25fc73bbbfd5a0e
Paweł Szafer <pszafer@gmail.com>
2021-09-10 12:17:37 +0200

    add option timeout_scale to epson

--- homeassistant/components/epson/__init__.py
+++ homeassistant/components/epson/__init__.py
@@ -12,8 +12,9 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.dispatcher import async_dispatcher_send
 
-from .const import DOMAIN, HTTP
+from .const import DOMAIN, HTTP, SIGNAL_CONFIG_OPTIONS_UPDATE, TIMEOUT_SCALE
 from .exceptions import CannotConnect, PoweredOff
 
 PLATFORMS = [MEDIA_PLAYER_PLATFORM]
@@ -22,13 +23,18 @@ _LOGGER = logging.getLogger(__name__)
 
 
 async def validate_projector(
-    hass: HomeAssistant, host, check_power=True, check_powered_on=True
+    hass: HomeAssistant,
+    host,
+    timeout_scale=1.0,
+    check_power=True,
+    check_powered_on=True,
 ):
     """Validate the given projector host allows us to connect."""
     epson_proj = Projector(
         host=host,
         websession=async_get_clientsession(hass, verify_ssl=False),
         type=HTTP,
+        timeout_scale=timeout_scale,
     )
     if check_power:
         _power = await epson_proj.get_power()
@@ -46,13 +52,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     projector = await validate_projector(
         hass=hass,
         host=entry.data[CONF_HOST],
+        timeout_scale=entry.options.get(TIMEOUT_SCALE, 1.0),
         check_power=False,
         check_powered_on=False,
     )
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = projector
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
-
+    entry.async_on_unload(entry.add_update_listener(update_listener))
     return True
 
 
@@ -62,3 +69,10 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
+
+
+async def update_listener(hass, entry):
+    """Handle options update."""
+    async_dispatcher_send(
+        hass, SIGNAL_CONFIG_OPTIONS_UPDATE.format(entry.entry_id), entry.options
+    )
