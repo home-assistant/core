88cb09222ba7e45330b3e7d7408ec7219b0284de
Martin Hjelmare <marhje52@gmail.com>
2021-09-12 23:50:20 +0200

    Remove zwave ozw migration websocket import flow test

--- tests/components/zwave/test_websocket_api.py
+++ tests/components/zwave/test_websocket_api.py
@@ -44,51 +44,6 @@ async def test_zwave_ws_api(hass, mock_openzwave, hass_ws_client):
     assert result[CONF_POLLING_INTERVAL] == 6000
 
 
-async def test_zwave_ozw_migration_api(hass, mock_openzwave, hass_ws_client):
-    """Test Z-Wave to OpenZWave websocket migration API."""
-
-    await async_setup_component(
-        hass,
-        "zwave",
-        {
-            "zwave": {
-                CONF_AUTOHEAL: False,
-                CONF_USB_STICK_PATH: "/dev/zwave",
-                CONF_POLLING_INTERVAL: 6000,
-                CONF_NETWORK_KEY: NETWORK_KEY,
-            }
-        },
-    )
-
-    await hass.async_block_till_done()
-
-    client = await hass_ws_client(hass)
-
-    await client.send_json({ID: 6, TYPE: "zwave/get_migration_config"})
-    msg = await client.receive_json()
-    result = msg["result"]
-
-    assert result[CONF_USB_STICK_PATH] == "/dev/zwave"
-    assert result[CONF_NETWORK_KEY] == NETWORK_KEY
-
-    with patch(
-        "homeassistant.config_entries.ConfigEntriesFlowManager.async_init"
-    ) as async_init:
-
-        async_init.return_value = {"flow_id": "mock_flow_id"}
-        await client.send_json({ID: 7, TYPE: "zwave/start_ozw_config_flow"})
-        msg = await client.receive_json()
-
-    result = msg["result"]
-
-    assert result["flow_id"] == "mock_flow_id"
-    assert async_init.call_args == call(
-        "ozw",
-        context={"source": config_entries.SOURCE_IMPORT},
-        data={"usb_path": "/dev/zwave", "network_key": NETWORK_KEY},
-    )
-
-
 async def test_zwave_zwave_js_migration_api(hass, mock_openzwave, hass_ws_client):
     """Test Z-Wave to Z-Wave JS websocket migration API."""
 
