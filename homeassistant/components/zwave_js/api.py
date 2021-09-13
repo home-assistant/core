5eaba777e2d9d49ae1566264fcffc572da8f613a
Martin Hjelmare <marhje52@gmail.com>
2021-09-13 11:38:00 +0200

    Remove not needed error log

--- homeassistant/components/zwave_js/api.py
+++ homeassistant/components/zwave_js/api.py
@@ -1775,7 +1775,6 @@ async def websocket_migrate_zwave(
 ) -> None:
     """Migrate Z-Wave device and entity data to Z-Wave JS integration."""
     if "zwave" not in hass.config.components:
-        LOGGER.error("Can not migrate, zwave integration is not loaded")
         connection.send_message(
             websocket_api.error_message(
                 msg["id"], "zwave_not_loaded", "Integration zwave is not loaded"
