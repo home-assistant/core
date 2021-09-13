adcc4c9f8dc9f572f4301fef36b46e9e40f5d21b
Erik <erik@montnemery.com>
2021-09-13 15:55:15 +0200

    Add WS API for updating statistics metadata

--- homeassistant/components/recorder/websocket_api.py
+++ homeassistant/components/recorder/websocket_api.py
@@ -1,4 +1,6 @@
 """The Energy websocket API."""
+from __future__ import annotations
+
 import voluptuous as vol
 
 from homeassistant.components import websocket_api
@@ -13,6 +15,7 @@ def async_setup(hass: HomeAssistant) -> None:
     """Set up the recorder websocket API."""
     websocket_api.async_register_command(hass, ws_validate_statistics)
     websocket_api.async_register_command(hass, ws_clear_statistics)
+    websocket_api.async_register_command(hass, ws_update_statistics_metadata)
 
 
 @websocket_api.websocket_command(
@@ -49,4 +52,23 @@ def ws_clear_statistics(
     wait until the job is completed.
     """
     hass.data[DATA_INSTANCE].async_clear_statistics(msg["statistic_ids"])
+    connection.send_result(msg["id"])
+
+
+@websocket_api.require_admin
+@websocket_api.websocket_command(
+    {
+        vol.Required("type"): "recorder/update_statistics_metadata",
+        vol.Required("statistic_id"): str,
+        vol.Required("unit_of_measurement"): vol.Any(str, None),
+    }
+)
+@websocket_api.async_response
+async def ws_update_statistics_metadata(
+    hass: HomeAssistant, connection: websocket_api.ActiveConnection, msg: dict
+) -> None:
+    """Clear statistics for a list of statistic_ids."""
+    hass.data[DATA_INSTANCE].async_update_statistics_metadata(
+        msg["statistic_id"], msg["unit_of_measurement"]
+    )
     connection.send_result(msg["id"])
