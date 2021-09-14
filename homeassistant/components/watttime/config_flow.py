9ee86310f0b9b5e9095a1d755f68d11277990c8d
Aaron Bach <bachya1208@gmail.com>
2021-09-14 17:56:05 -0600

    Add ability to re-auth WattTime

--- homeassistant/components/watttime/config_flow.py
+++ homeassistant/components/watttime/config_flow.py
@@ -16,6 +16,7 @@ from homeassistant.const import (
 )
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import aiohttp_client, config_validation as cv
+from homeassistant.helpers.typing import ConfigType
 
 from .const import CONF_BALANCING_AUTHORITY_ABBREV, DOMAIN, LOGGER
 
@@ -39,6 +40,12 @@ STEP_LOCATION_DATA_SCHEMA = vol.Schema(
     }
 )
 
+STEP_REAUTH_CONFIRM_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_PASSWORD): str,
+    }
+)
+
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_USERNAME): str,
@@ -55,8 +62,47 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def __init__(self) -> None:
         """Initialize."""
         self._client: Client | None = None
-        self._password: str | None = None
-        self._username: str | None = None
+        self._data: dict[str, Any] = {}
+
+    async def _async_validate_credentials(
+        self, username: str, password: str, error_step_id: str, error_schema: vol.Schema
+    ):
+        """Validate input credentials and proceed accordingly."""
+        session = aiohttp_client.async_get_clientsession(self.hass)
+
+        try:
+            self._client = await Client.async_login(username, password, session=session)
+        except InvalidCredentialsError:
+            return self.async_show_form(
+                step_id=error_step_id,
+                data_schema=error_schema,
+                errors={"base": "invalid_auth"},
+                description_placeholders={CONF_USERNAME: username},
+            )
+        except Exception as err:  # pylint: disable=broad-except
+            LOGGER.exception("Unexpected exception while logging in: %s", err)
+            return self.async_show_form(
+                step_id=error_step_id,
+                data_schema=error_schema,
+                errors={"base": "unknown"},
+                description_placeholders={CONF_USERNAME: username},
+            )
+
+        # If an entry already exists, we're in a re-auth flow – store the new data and
+        # reload the config entry:
+        if existing_entry := await self.async_set_unique_id(
+            f"{self._data[CONF_LATITUDE]}, {self._data[CONF_LONGITUDE]}"
+        ):
+            self.hass.config_entries.async_update_entry(existing_entry, data=self._data)
+            self.hass.async_create_task(
+                self.hass.config_entries.async_reload(existing_entry.entry_id)
+            )
+            return self.async_abort(reason="reauth_successful")
+
+        # ...otherwise, we're setting up a new config entry:
+        self._data[CONF_USERNAME] = username
+        self._data[CONF_PASSWORD] = password
+        return await self.async_step_location()
 
     async def async_step_coordinates(
         self, user_input: dict[str, Any] | None = None
@@ -123,6 +169,31 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             }
         )
 
+    async def async_step_reauth(self, config: ConfigType) -> FlowResult:
+        """Handle configuration by re-auth."""
+        self._data = {**config}
+        return await self.async_step_reauth_confirm()
+
+    async def async_step_reauth_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle re-auth completion."""
+        if not user_input:
+            return self.async_show_form(
+                step_id="reauth_confirm",
+                data_schema=STEP_REAUTH_CONFIRM_DATA_SCHEMA,
+                description_placeholders={CONF_USERNAME: self._data[CONF_USERNAME]},
+            )
+
+        self._data[CONF_PASSWORD] = user_input[CONF_PASSWORD]
+
+        return await self._async_validate_credentials(
+            self._data[CONF_USERNAME],
+            self._data[CONF_PASSWORD],
+            "reauth_confirm",
+            STEP_REAUTH_CONFIRM_DATA_SCHEMA,
+        )
+
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
@@ -132,28 +203,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 step_id="user", data_schema=STEP_USER_DATA_SCHEMA
             )
 
-        session = aiohttp_client.async_get_clientsession(self.hass)
-
-        try:
-            self._client = await Client.async_login(
-                user_input[CONF_USERNAME],
-                user_input[CONF_PASSWORD],
-                session=session,
-            )
-        except InvalidCredentialsError:
-            return self.async_show_form(
-                step_id="user",
-                data_schema=STEP_USER_DATA_SCHEMA,
-                errors={CONF_USERNAME: "invalid_auth"},
-            )
-        except Exception as err:  # pylint: disable=broad-except
-            LOGGER.exception("Unexpected exception while logging in: %s", err)
-            return self.async_show_form(
-                step_id="user",
-                data_schema=STEP_USER_DATA_SCHEMA,
-                errors={"base": "unknown"},
-            )
-
-        self._username = user_input[CONF_USERNAME]
-        self._password = user_input[CONF_PASSWORD]
-        return await self.async_step_location()
+        return await self._async_validate_credentials(
+            user_input[CONF_USERNAME],
+            user_input[CONF_PASSWORD],
+            "user",
+            STEP_USER_DATA_SCHEMA,
+        )
