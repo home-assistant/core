1f2003a5abdd04a811f155d763f1ba7b5771444d
Miguel Camba <miguel.camba@gmail.com>
2021-09-14 20:47:25 +0000

    Fix last test

--- tests/components/poolstation/test_sensor.py
+++ tests/components/poolstation/test_sensor.py
@@ -27,41 +27,41 @@ async def test_pool_sensors(hass: HomeAssistant) -> None:
     registry = entity_registry.async_get(hass)
 
     # PH
-    state = hass.states.get("sensor.home_ph")
+    state = hass.states.get("sensor.my_pool_ph")
     assert state
     assert state.state == str(pool.current_ph)
     assert state.attributes.get(ATTR_ICON) == "mdi:ph"
-    entry = registry.async_get("sensor.home_ph")
+    entry = registry.async_get("sensor.my_pool_ph")
     assert entry
     assert entry.unique_id == f"{pool.alias}{PH_SUFFIX}"
 
     # Temperature
-    state = hass.states.get("sensor.home_temperature")
+    state = hass.states.get("sensor.my_pool_temperature")
     assert state
     assert state.state == str(pool.temperature)
     assert state.attributes.get(ATTR_ICON) == "mdi:coolant-temperature"
     assert state.attributes.get(ATTR_DEVICE_CLASS) == DEVICE_CLASS_TEMPERATURE
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == TEMP_CELSIUS
-    entry = registry.async_get("sensor.home_temperature")
+    entry = registry.async_get("sensor.my_pool_temperature")
     assert entry
     assert entry.unique_id == f"{pool.alias}{TEMPERATURE_SUFFIX}"
 
     # Salt concentration
-    state = hass.states.get("sensor.home_salt_concentration")
+    state = hass.states.get("sensor.my_pool_salt_concentration")
     assert state
     assert state.state == str(pool.salt_concentration)
     assert state.attributes.get(ATTR_ICON) == "mdi:shaker"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == "gr/l"
-    entry = registry.async_get("sensor.home_salt_concentration")
+    entry = registry.async_get("sensor.my_pool_salt_concentration")
     assert entry
     assert entry.unique_id == f"{pool.alias}{SALT_SUFFIX}"
 
     # Electrolysis
-    state = hass.states.get("sensor.home_electrolysis")
+    state = hass.states.get("sensor.my_pool_electrolysis")
     assert state
     assert state.state == str(pool.percentage_electrolysis)
     assert state.attributes.get(ATTR_ICON) == "mdi:water-percent"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == PERCENTAGE
-    entry = registry.async_get("sensor.home_electrolysis")
+    entry = registry.async_get("sensor.my_pool_electrolysis")
     assert entry
     assert entry.unique_id == f"{pool.alias}{ELECTROLYSIS_SUFFIX}"
