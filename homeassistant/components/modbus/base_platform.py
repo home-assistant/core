36a62c6ad2d70f5fa1e14b93b02f7c09e5ddc66d
jan Iversen <jancasacondor@gmail.com>
2021-09-15 09:46:54 +0200

    Review comments.

--- homeassistant/components/modbus/base_platform.py
+++ homeassistant/components/modbus/base_platform.py
@@ -28,6 +28,7 @@ from homeassistant.helpers.event import async_call_later, async_track_time_inter
 from homeassistant.helpers.restore_state import RestoreEntity
 
 from .const import (
+    ACTIVE_SCAN_INTERVAL,
     CALL_TYPE_COIL,
     CALL_TYPE_DISCRETE,
     CALL_TYPE_REGISTER_HOLDING,
@@ -96,7 +97,7 @@ class BasePlatform(Entity):
     def async_run(self) -> None:
         """Remote start entity."""
         self.async_hold(update=False)
-        if self._scan_interval == 0 | self._scan_interval > 2:
+        if self._scan_interval == 0 | self._scan_interval > ACTIVE_SCAN_INTERVAL:
             self._cancel_call = async_call_later(self.hass, 1, self.async_update)
         if self._scan_interval > 0:
             self._cancel_timer = async_track_time_interval(
