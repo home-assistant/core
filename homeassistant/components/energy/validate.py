b14d1f8dfb20ad82eecdf15554045d076c88caef
Erik <erik@montnemery.com>
2021-09-15 12:50:40 +0200

    Require last_reset attribute to be set for measurement state_class

--- homeassistant/components/energy/validate.py
+++ homeassistant/components/energy/validate.py
@@ -135,11 +135,17 @@ def _async_validate_usage_stat(
     if state_class not in allowed_state_classes:
         result.append(
             ValidationIssue(
-                "entity_unexpected_state_class_total_increasing",
+                "entity_unexpected_state_class",
                 stat_value,
                 state_class,
             )
         )
+
+    if (
+        state_class == sensor.STATE_CLASS_TOTAL_INCREASING
+        and sensor.ATTR_LAST_RESET not in state.attributes
+    ):
+        result.append(ValidationIssue("entity_measurement_no_last_reset", stat_value))
 
 
 @callback
@@ -213,9 +219,15 @@ def _async_validate_cost_stat(
     if state_class not in supported_state_classes:
         result.append(
             ValidationIssue(
-                "entity_unexpected_state_class_total_increasing", stat_id, state_class
+                "entity_unexpected_state_class", stat_id, state_class
             )
         )
+
+    if (
+        state_class == sensor.STATE_CLASS_TOTAL_INCREASING
+        and sensor.ATTR_LAST_RESET not in state.attributes
+    ):
+        result.append(ValidationIssue("entity_measurement_no_last_reset", stat_id))
 
 
 @callback
