0478a4e1ea92387921d9798fbe56a13029546719
some-guy-in-oz <85833124+some-guy-in-oz@users.noreply.github.com>
2021-09-17 12:00:56 +0930

    Add grid services active sensor

--- homeassistant/components/powerwall/const.py
+++ homeassistant/components/powerwall/const.py
@@ -9,6 +9,8 @@ POWERWALL_API_CHANGED = "api_changed"
 UPDATE_INTERVAL = 30
 
 ATTR_FREQUENCY = "frequency"
+ATTR_ENERGY_EXPORTED = "energy_exported_(in_kW)"
+ATTR_ENERGY_IMPORTED = "energy_imported_(in_kW)"
 ATTR_INSTANT_AVERAGE_VOLTAGE = "instant_average_voltage"
 ATTR_INSTANT_TOTAL_CURRENT = "instant_total_current"
 ATTR_IS_ACTIVE = "is_active"
@@ -19,6 +21,7 @@ POWERWALL_SITE_NAME = "site_name"
 
 POWERWALL_API_METERS = "meters"
 POWERWALL_API_CHARGE = "charge"
+POWERWALL_API_GRID_SERVICES_ACTIVE = "grid_services_active"
 POWERWALL_API_GRID_STATUS = "grid_status"
 POWERWALL_API_SITEMASTER = "sitemaster"
 POWERWALL_API_STATUS = "status"
