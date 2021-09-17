bce4c5eb1149721a4abc2b53f9d9d599b1c01c02
Erik Montnemery <erik@montnemery.com>
2021-09-17 11:19:32 +0200

    Prevent 3rd party lib from opening sockets in zeroconf tests (#56324)

--- tests/components/zeroconf/conftest.py
+++ tests/components/zeroconf/conftest.py
@@ -4,8 +4,14 @@ from unittest.mock import AsyncMock, patch
 import pytest
 
 
+@pytest.fixture(autouse=True)
+def zc_mock_get_source_ip(mock_get_source_ip):
+    """Enable the mock_get_source_ip fixture for all zeroconf tests."""
+    return mock_get_source_ip
+
+
 @pytest.fixture
-def mock_async_zeroconf():
+def mock_async_zeroconf(mock_zeroconf):
     """Mock AsyncZeroconf."""
     with patch("homeassistant.components.zeroconf.HaAsyncZeroconf") as mock_aiozc:
         zc = mock_aiozc.return_value
