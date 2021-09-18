e372b73589da16f2ffa54168c69dfdb87fd092cd
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-09-18 13:52:59 +0200

    Update pylint to 2.11.1 (#56364)

--- homeassistant/components/api/__init__.py
+++ homeassistant/components/api/__init__.py
@@ -93,6 +93,7 @@ class APIEventStream(HomeAssistantView):
 
     async def get(self, request):
         """Provide a streaming interface for the event bus."""
+        # pylint: disable=no-self-use
         if not request["hass_user"].is_admin:
             raise Unauthorized()
         hass = request.app["hass"]
@@ -414,6 +415,7 @@ class APIErrorLog(HomeAssistantView):
 
     async def get(self, request):
         """Retrieve API error log."""
+        # pylint: disable=no-self-use
         if not request["hass_user"].is_admin:
             raise Unauthorized()
         return web.FileResponse(request.app["hass"].data[DATA_LOGGING])
