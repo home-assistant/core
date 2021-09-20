5d5506248187a7397376594d16926c486ee6f987
Myles Eftos <myles@madpilot.com.au>
2021-09-20 19:33:13 +1000

    Removing files that shouldn't have been changed

--- tests/components/modbus/test_fan.py
+++ tests/components/modbus/test_fan.py
@@ -10,11 +10,13 @@ from homeassistant.components.modbus.const import (
     CALL_TYPE_REGISTER_INPUT,
     CONF_FANS,
     CONF_INPUT_TYPE,
+    CONF_LAZY_ERROR,
     CONF_STATE_OFF,
     CONF_STATE_ON,
     CONF_VERIFY,
     CONF_WRITE_TYPE,
     MODBUS_DOMAIN,
+    TCP,
 )
 from homeassistant.const import (
     CONF_ADDRESS,
@@ -33,10 +35,9 @@ from homeassistant.const import (
 from homeassistant.core import State
 from homeassistant.setup import async_setup_component
 
-from .conftest import ReadResult, base_test
+from .conftest import TEST_ENTITY_NAME, TEST_MODBUS_HOST, TEST_PORT_TCP, ReadResult
 
-FAN_NAME = "test_fan"
-ENTITY_ID = f"{FAN_DOMAIN}.{FAN_NAME}"
+ENTITY_ID = f"{FAN_DOMAIN}.{TEST_ENTITY_NAME}"
 
 
 @pytest.mark.parametrize(
@@ -45,7 +46,7 @@ ENTITY_ID = f"{FAN_DOMAIN}.{FAN_NAME}"
         {
             CONF_FANS: [
                 {
-                    CONF_NAME: FAN_NAME,
+                    CONF_NAME: TEST_ENTITY_NAME,
                     CONF_ADDRESS: 1234,
                 }
             ]
@@ -53,7 +54,7 @@ ENTITY_ID = f"{FAN_DOMAIN}.{FAN_NAME}"
         {
             CONF_FANS: [
                 {
-                    CONF_NAME: FAN_NAME,
+                    CONF_NAME: TEST_ENTITY_NAME,
                     CONF_ADDRESS: 1234,
                     CONF_WRITE_TYPE: CALL_TYPE_COIL,
                 }
@@ -62,11 +63,12 @@ ENTITY_ID = f"{FAN_DOMAIN}.{FAN_NAME}"
         {
             CONF_FANS: [
                 {
-                    CONF_NAME: FAN_NAME,
+                    CONF_NAME: TEST_ENTITY_NAME,
                     CONF_ADDRESS: 1234,
                     CONF_SLAVE: 1,
                     CONF_COMMAND_OFF: 0x00,
                     CONF_COMMAND_ON: 0x01,
+                    CONF_LAZY_ERROR: 10,
                     CONF_VERIFY: {
                         CONF_INPUT_TYPE: CALL_TYPE_REGISTER_HOLDING,
                         CONF_ADDRESS: 1235,
@@ -79,7 +81,7 @@ ENTITY_ID = f"{FAN_DOMAIN}.{FAN_NAME}"
         {
             CONF_FANS: [
                 {
-                    CONF_NAME: FAN_NAME,
+                    CONF_NAME: TEST_ENTITY_NAME,
                     CONF_ADDRESS: 1234,
                     CONF_SLAVE: 1,
                     CONF_COMMAND_OFF: 0x00,
@@ -96,7 +98,7 @@ ENTITY_ID = f"{FAN_DOMAIN}.{FAN_NAME}"
         {
             CONF_FANS: [
                 {
-                    CONF_NAME: FAN_NAME,
+                    CONF_NAME: TEST_ENTITY_NAME,
                     CONF_ADDRESS: 1234,
                     CONF_SLAVE: 1,
                     CONF_COMMAND_OFF: 0x00,
@@ -113,7 +115,7 @@ ENTITY_ID = f"{FAN_DOMAIN}.{FAN_NAME}"
         {
             CONF_FANS: [
                 {
-                    CONF_NAME: FAN_NAME,
+                    CONF_NAME: TEST_ENTITY_NAME,
                     CONF_ADDRESS: 1234,
                     CONF_SLAVE: 1,
                     CONF_COMMAND_OFF: 0x00,
@@ -129,58 +131,69 @@ async def test_config_fan(hass, mock_modbus):
     assert FAN_DOMAIN in hass.config.components
 
 
-@pytest.mark.parametrize("call_type", [CALL_TYPE_COIL, CALL_TYPE_REGISTER_HOLDING])
 @pytest.mark.parametrize(
-    "regs,verify,expected",
+    "do_config",
+    [
+        {
+            CONF_FANS: [
+                {
+                    CONF_NAME: TEST_ENTITY_NAME,
+                    CONF_ADDRESS: 1234,
+                    CONF_SLAVE: 1,
+                    CONF_WRITE_TYPE: CALL_TYPE_COIL,
+                },
+            ],
+        },
+        {
+            CONF_FANS: [
+                {
+                    CONF_NAME: TEST_ENTITY_NAME,
+                    CONF_ADDRESS: 1234,
+                    CONF_SLAVE: 1,
+                    CONF_WRITE_TYPE: CALL_TYPE_REGISTER_HOLDING,
+                },
+            ],
+        },
+    ],
+)
+@pytest.mark.parametrize(
+    "register_words,do_exception,config_addon,expected",
     [
         (
             [0x00],
+            False,
             {CONF_VERIFY: {}},
             STATE_OFF,
         ),
         (
             [0x01],
+            False,
             {CONF_VERIFY: {}},
             STATE_ON,
         ),
         (
             [0xFE],
+            False,
             {CONF_VERIFY: {}},
             STATE_OFF,
         ),
         (
-            None,
+            [0x00],
+            True,
             {CONF_VERIFY: {}},
             STATE_UNAVAILABLE,
         ),
         (
+            [0x00],
+            True,
             None,
-            {},
             STATE_OFF,
         ),
     ],
 )
-async def test_all_fan(hass, call_type, regs, verify, expected):
+async def test_all_fan(hass, mock_do_cycle, expected):
     """Run test for given config."""
-    state = await base_test(
-        hass,
-        {
-            CONF_NAME: FAN_NAME,
-            CONF_ADDRESS: 1234,
-            CONF_SLAVE: 1,
-            CONF_WRITE_TYPE: call_type,
-            **verify,
-        },
-        FAN_NAME,
-        FAN_DOMAIN,
-        CONF_FANS,
-        None,
-        regs,
-        expected,
-        method_discovery=True,
-        scan_interval=5,
-    )
-    assert state == expected
+    assert hass.states.get(ENTITY_ID).state == expected
 
 
 @pytest.mark.parametrize(
@@ -194,7 +207,7 @@ async def test_all_fan(hass, call_type, regs, verify, expected):
         {
             CONF_FANS: [
                 {
-                    CONF_NAME: FAN_NAME,
+                    CONF_NAME: TEST_ENTITY_NAME,
                     CONF_ADDRESS: 1234,
                     CONF_SCAN_INTERVAL: 0,
                 }
@@ -210,22 +223,22 @@ async def test_restore_state_fan(hass, mock_test_state, mock_modbus):
 async def test_fan_service_turn(hass, caplog, mock_pymodbus):
     """Run test for service turn_on/turn_off."""
 
-    ENTITY_ID2 = f"{FAN_DOMAIN}.{FAN_NAME}2"
+    ENTITY_ID2 = f"{FAN_DOMAIN}.{TEST_ENTITY_NAME}2"
     config = {
         MODBUS_DOMAIN: {
-            CONF_TYPE: "tcp",
-            CONF_HOST: "modbusTestHost",
-            CONF_PORT: 5501,
+            CONF_TYPE: TCP,
+            CONF_HOST: TEST_MODBUS_HOST,
+            CONF_PORT: TEST_PORT_TCP,
             CONF_FANS: [
                 {
-                    CONF_NAME: FAN_NAME,
+                    CONF_NAME: TEST_ENTITY_NAME,
                     CONF_ADDRESS: 17,
                     CONF_WRITE_TYPE: CALL_TYPE_REGISTER_HOLDING,
                     CONF_SCAN_INTERVAL: 0,
                 },
                 {
-                    CONF_NAME: f"{FAN_NAME}2",
-                    CONF_ADDRESS: 17,
+                    CONF_NAME: f"{TEST_ENTITY_NAME}2",
+                    CONF_ADDRESS: 18,
                     CONF_WRITE_TYPE: CALL_TYPE_REGISTER_HOLDING,
                     CONF_SCAN_INTERVAL: 0,
                     CONF_VERIFY: {},
@@ -283,7 +296,7 @@ async def test_fan_service_turn(hass, caplog, mock_pymodbus):
         {
             CONF_FANS: [
                 {
-                    CONF_NAME: FAN_NAME,
+                    CONF_NAME: TEST_ENTITY_NAME,
                     CONF_ADDRESS: 1234,
                     CONF_WRITE_TYPE: CALL_TYPE_COIL,
                     CONF_VERIFY: {},
