d7f2d7d8d9d1ff142e6ba1dafb73a1e1978b3ad7
Tom Scholten <git@scholten.nu>
2021-09-20 07:52:37 +0200

    Revert to defaulting username

--- homeassistant/components/plugwise/gateway.py
+++ homeassistant/components/plugwise/gateway.py
@@ -54,19 +54,10 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry_gw(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Plugwise Smiles from a config entry."""
     websession = async_get_clientsession(hass, verify_ssl=False)
-    entry_updates = {}
-
-    if CONF_USERNAME not in entry.data:
-        data = {**entry.data}
-        data.update({CONF_USERNAME: DEFAULT_USERNAME})
-        entry_updates["data"] = data
-
-    if entry_updates:
-        hass.config_entries.async_update_entry(entry, **entry_updates)
 
     api = Smile(
         host=entry.data[CONF_HOST],
-        username=entry.data[CONF_USERNAME],
+        username=entry.data.get(CONF_USERNAME, DEFAULT_USERNAME),
         password=entry.data[CONF_PASSWORD],
         port=entry.data.get(CONF_PORT, DEFAULT_PORT),
         timeout=30,
