a918200dbadcfae42d659d90039a2fdfee4a5d65
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2021-09-22 17:42:38 +0000

    Cleanup sensor, make use of new sensorentity functions.

--- homeassistant/components/switchbot/sensor.py
+++ homeassistant/components/switchbot/sensor.py
@@ -1,16 +1,15 @@
 """Support for SwitchBot sensors."""
 from __future__ import annotations
 
+from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_MAC, CONF_NAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr
-from homeassistant.helpers.entity import DeviceInfo, Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DATA_COORDINATOR, DOMAIN, MANUFACTURER, SensorType
+from .const import DATA_COORDINATOR, DOMAIN, SensorType
 from .coordinator import SwitchbotDataUpdateCoordinator
+from .entity import SwitchbotEntity
 
 PARALLEL_UPDATES = 1
 
@@ -43,7 +42,7 @@ async def async_setup_entry(
     async_add_entities(sensors)
 
 
-class SwitchBotSensor(CoordinatorEntity, Entity):
+class SwitchBotSensor(SwitchbotEntity, SensorEntity):
     """Representation of a Switchbot sensor."""
 
     coordinator: SwitchbotDataUpdateCoordinator
@@ -58,22 +57,13 @@ class SwitchBotSensor(CoordinatorEntity, Entity):
         switchbot_name: str,
     ) -> None:
         """Initialize the Switchbot sensor."""
-        super().__init__(coordinator)
-        self._idx = idx
+        super().__init__(coordinator, idx, mac, name=f"{switchbot_name}.{sensor}")
         self._sensor = sensor
-        self._mac = mac
-        self._attr_unique_id = f"{self._mac.replace(':', '')}-{sensor}"
-        self._attr_name = f"{switchbot_name}.{sensor}"
+        self._attr_unique_id = f"{idx}-{sensor}"
         self._attr_device_class = sensor_type[0]
-        self._attr_unit_of_measurement = sensor_type[1]
-        self._attr_device_info: DeviceInfo = {
-            "connections": {(dr.CONNECTION_NETWORK_MAC, self._mac)},
-            "name": switchbot_name,
-            "model": self.coordinator.data[self._idx]["modelName"],
-            "manufacturer": MANUFACTURER,
-        }
+        self._attr_native_unit_of_measurement = sensor_type[1]
 
     @property
     def native_value(self) -> str:
         """Return the state of the sensor."""
-        return self.coordinator.data[self._idx]["data"][self._sensor]
+        return self.data["data"][self._sensor]
