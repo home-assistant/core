f7352cdc6c16bddc0d26c47dde9e2be14b6159e6
Jan-Philipp Benecke <jan-philipp@bnck.me>
2021-09-22 14:01:57 +0200

    Bump pydiscovergy & minor changes

--- homeassistant/components/discovergy/sensor.py
+++ homeassistant/components/discovergy/sensor.py
@@ -26,7 +26,7 @@ from homeassistant.helpers.update_coordinator import (
 
 from .const import DOMAIN, ELECTRICITY_SENSORS, MANUFACTURER, MIN_TIME_BETWEEN_UPDATES
 
-PARALLEL_UPDATES = 0
+PARALLEL_UPDATES = 1
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -90,7 +90,7 @@ async def async_setup_entry(
 
                 for description in ELECTRICITY_SENSORS:
                     # check if this meter has this data, then add this sensor
-                    if coordinator.data.values[description.key]:
+                    if description.key in coordinator.data.values:
                         entities.append(
                             DiscovergyElectricitySensor(description, meter, coordinator)
                         )
@@ -117,11 +117,11 @@ class DiscovergyElectricitySensor(CoordinatorEntity, SensorEntity):
             f"{meter.location.street_number} - "
             f"{description.name}"
         )
-        self._attr_unique_id = f"{meter.serial_number}-{description.key}"
+        self._attr_unique_id = f"{meter.full_serial_number}-{description.key}"
         self._attr_device_info = {
             ATTR_IDENTIFIERS: {(DOMAIN, meter.get_meter_id())},
-            ATTR_NAME: f"{meter.type} {meter.measurement_type.capitalize()} {meter.location.street} {meter.location.street_number}",
-            ATTR_MODEL: f"{meter.type.capitalize()} {meter.measurement_type.capitalize()}",
+            ATTR_NAME: f"{meter.measurement_type.capitalize()} {meter.location.street} {meter.location.street_number}",
+            ATTR_MODEL: f"{meter.type} {meter.full_serial_number}",
             ATTR_MANUFACTURER: MANUFACTURER,
         }
 
