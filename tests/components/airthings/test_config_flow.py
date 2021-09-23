fe82f268cb2e1205cad34fe18188e59cb59a2156
Daniel Hjelseth Høyer <github@dahoiv.net>
2021-09-23 18:16:46 +0200

    Airthings
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- tests/components/airthings/test_config_flow.py
+++ tests/components/airthings/test_config_flow.py
@@ -79,6 +79,25 @@ async def test_form_cannot_connect(hass: HomeAssistant) -> None:
     assert result2["errors"] == {"base": "cannot_connect"}
 
 
+async def test_form_unknown_error(hass: HomeAssistant) -> None:
+    """Test we handle unknown error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "airthings.get_token",
+        side_effect=Exception,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            TEST_DATA,
+        )
+
+    assert result2["type"] == RESULT_TYPE_FORM
+    assert result2["errors"] == {"base": "unknown"}
+
+
 async def test_flow_entry_already_exists(hass: HomeAssistant) -> None:
     """Test user input for config_entry that already exists."""
 
