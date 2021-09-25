7e18b05a71ea2af513197abd92a7082cfd711fe7
J. Nick Koston <nick@koston.org>
2021-09-25 10:20:05 -0500

    type

--- homeassistant/components/tplink/const.py
+++ homeassistant/components/tplink/const.py
@@ -1,23 +1,24 @@
 """Const for TP-Link."""
 from __future__ import annotations
 
+from typing import Final
+
 DOMAIN = "tplink"
 
-ATTR_CURRENT_A = "current_a"
-ATTR_CURRENT_POWER_W = "current_power_w"
-ATTR_TODAY_ENERGY_KWH = "today_energy_kwh"
-ATTR_TOTAL_ENERGY_KWH = "total_energy_kwh"
+ATTR_CURRENT_A: Final = "current_a"
+ATTR_CURRENT_POWER_W: Final = "current_power_w"
+ATTR_TODAY_ENERGY_KWH: Final = "today_energy_kwh"
+ATTR_TOTAL_ENERGY_KWH: Final = "total_energy_kwh"
 
-MAC_ADDRESS_LEN = 17
-CONF_LEGACY_ENTRY_ID = "legacy_entry_id"
-DISCOVERED_DEVICES = "discovered_devices"
+CONF_LEGACY_ENTRY_ID: Final = "legacy_entry_id"
+DISCOVERED_DEVICES: Final = "discovered_devices"
 
-CONF_EMETER_PARAMS = "emeter_params"
-CONF_DIMMER = "dimmer"
-CONF_DISCOVERY = "discovery"
-CONF_LIGHT = "light"
-CONF_STRIP = "strip"
-CONF_SWITCH = "switch"
-CONF_SENSOR = "sensor"
+CONF_EMETER_PARAMS: Final = "emeter_params"
+CONF_DIMMER: Final = "dimmer"
+CONF_DISCOVERY: Final = "discovery"
+CONF_LIGHT: Final = "light"
+CONF_STRIP: Final = "strip"
+CONF_SWITCH: Final = "switch"
+CONF_SENSOR: Final = "sensor"
 
-PLATFORMS = [CONF_LIGHT, CONF_SENSOR, CONF_SWITCH]
+PLATFORMS: Final = [CONF_LIGHT, CONF_SENSOR, CONF_SWITCH]
