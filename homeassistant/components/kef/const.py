58480e000e005b960e89117b5ba6c21ab19fdc32
Bas Nijholt <bas@nijho.lt>
2021-09-26 16:49:00 +0200

    remove unused consts

--- homeassistant/components/kef/const.py
+++ homeassistant/components/kef/const.py
@@ -1,5 +1,5 @@
 """Constants for the KEF component."""
-DOMAIN = "kef_custom"
+DOMAIN = "kef"
 
 CONF_MAX_VOLUME = "maximum_volume"
 CONF_VOLUME_STEP = "volume_step"
@@ -13,12 +13,3 @@ DEFAULT_MAX_VOLUME = 0.5
 DEFAULT_VOLUME_STEP = 0.05
 DEFAULT_INVERSE_SPEAKER_MODE = False
 DEFAULT_SUPPORTS_ON = True
-
-SLIDERS = [
-    ("desk_db", "db"),
-    ("wall_db", "db"),
-    ("treble_db", "db"),
-    ("high_hz", "hz"),
-    ("low_hz", "hz"),
-    ("sub_db", "db"),
-]
