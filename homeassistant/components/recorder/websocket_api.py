8184b6bec23773259f44533d90878bd08088b4f2
Erik Montnemery <erik@montnemery.com>
2021-09-27 23:30:13 +0200

    Add WS API for removing statistics for a list of statistic_ids (#55078)
    * Add WS API for removing statistics for a list of statistic_ids
    
    * Refactor according to code review, enable foreign keys support for sqlite
    
    * Adjust tests
    
    * Move clear_statistics WS API to recorder
    
    * Adjust tests after rebase
    
    * Update docstring
    
    * Update homeassistant/components/recorder/websocket_api.py
    
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>
    
    * Adjust tests after rebase
    
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/recorder/websocket_api.py
+++ homeassistant/components/recorder/websocket_api.py
@@ -4,6 +4,7 @@ import voluptuous as vol
 from homeassistant.components import websocket_api
 from homeassistant.core import HomeAssistant, callback
 
+from .const import DATA_INSTANCE
 from .statistics import validate_statistics
 
 
@@ -11,6 +12,7 @@ from .statistics import validate_statistics
 def async_setup(hass: HomeAssistant) -> None:
     """Set up the recorder websocket API."""
     websocket_api.async_register_command(hass, ws_validate_statistics)
+    websocket_api.async_register_command(hass, ws_clear_statistics)
 
 
 @websocket_api.websocket_command(
@@ -28,3 +30,23 @@ async def ws_validate_statistics(
         hass,
     )
     connection.send_result(msg["id"], statistic_ids)
+
+
+@websocket_api.require_admin
+@websocket_api.websocket_command(
+    {
+        vol.Required("type"): "recorder/clear_statistics",
+        vol.Required("statistic_ids"): [str],
+    }
+)
+@callback
+def ws_clear_statistics(
+    hass: HomeAssistant, connection: websocket_api.ActiveConnection, msg: dict
+) -> None:
+    """Clear statistics for a list of statistic_ids.
+
+    Note: The WS call posts a job to the recorder's queue and then returns, it doesn't
+    wait until the job is completed.
+    """
+    hass.data[DATA_INSTANCE].async_clear_statistics(msg["statistic_ids"])
+    connection.send_result(msg["id"])
