29831e105703902fa72a681be3da46190ae3c0ee
Andre Richter <andre-richter@users.noreply.github.com>
2021-09-29 17:14:41 +0200

    Migrate Vallox to new fan entity model (#56663)
    * Migrate Vallox to new fan entity model
    
    * Review comments 1
    
    * Minor corrections
    
    * Review comments 2

--- homeassistant/components/vallox/sensor.py
+++ homeassistant/components/vallox/sensor.py
@@ -25,7 +25,13 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from . import ValloxStateProxy
-from .const import DOMAIN, METRIC_KEY_MODE, MODE_ON, SIGNAL_VALLOX_STATE_UPDATE
+from .const import (
+    DOMAIN,
+    METRIC_KEY_MODE,
+    MODE_ON,
+    SIGNAL_VALLOX_STATE_UPDATE,
+    VALLOX_PROFILE_TO_STR_REPORTABLE,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -89,13 +95,14 @@ class ValloxProfileSensor(ValloxSensor):
     async def async_update(self) -> None:
         """Fetch state from the ventilation unit."""
         try:
-            self._attr_native_value = self._state_proxy.get_profile()
+            vallox_profile = self._state_proxy.get_profile()
 
         except OSError as err:
             self._attr_available = False
             _LOGGER.error("Error updating sensor: %s", err)
             return
 
+        self._attr_native_value = VALLOX_PROFILE_TO_STR_REPORTABLE.get(vallox_profile)
         self._attr_available = True
 
 
