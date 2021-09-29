3d2fb1585c0e7597966419109096f8c32340ef6f
zlinoliver <zlinoliver@gmail.com>
2021-09-29 17:04:52 +0800

    update modifications.

--- homeassistant/components/tuya/cover.py
+++ homeassistant/components/tuya/cover.py
@@ -55,7 +55,9 @@ async def async_setup_entry(
         _LOGGER.debug("cover add-> %s", dev_ids)
         if not dev_ids:
             return
-        entities = _setup_entities(hass, entry, dev_ids)
+        entities = await hass.async_add_executor_job(
+            _setup_entities, hass, entry, dev_ids
+        )
         async_add_entities(entities)
 
     entry.async_on_unload(
