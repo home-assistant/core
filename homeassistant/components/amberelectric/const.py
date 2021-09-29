ef850bd355660c2bb6ea5f7b1ba84f769446ad3e
Myles Eftos <myles@madpilot.com.au>
2021-09-29 11:01:35 +1000

    Adding price spike binary sensor to the Amber electric integration (#56736)

--- homeassistant/components/amberelectric/const.py
+++ homeassistant/components/amberelectric/const.py
@@ -7,5 +7,7 @@ CONF_SITE_NAME = "site_name"
 CONF_SITE_ID = "site_id"
 CONF_SITE_NMI = "site_nmi"
 
+ATTRIBUTION = "Data provided by Amber Electric"
+
 LOGGER = logging.getLogger(__package__)
-PLATFORMS = ["sensor"]
+PLATFORMS = ["sensor", "binary_sensor"]
