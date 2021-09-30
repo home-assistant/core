97edc01d10fd27b4eda58f2b08ff4838941b1545
J. Nick Koston <nick@koston.org>
2021-09-30 10:08:00 -0500

    coverage for options flow

--- tests/components/flux_led/test_config_flow.py
+++ tests/components/flux_led/test_config_flow.py
@@ -11,7 +11,9 @@ from homeassistant.components.flux_led.const import (
     CONF_CUSTOM_EFFECT_SPEED_PCT,
     CONF_CUSTOM_EFFECT_TRANSITION,
     DOMAIN,
+    MODE_AUTO,
     MODE_RGB,
+    TRANSITION_JUMP,
     TRANSITION_STROBE,
 )
 from homeassistant.const import (
@@ -415,3 +417,43 @@ async def test_discovered_by_dhcp_or_discovery_adds_missing_unique_id(
     assert result["reason"] == "already_configured"
 
     assert config_entry.unique_id == MAC_ADDRESS
+
+
+async def test_options(hass: HomeAssistant):
+    """Test options flow."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_HOST: IP_ADDRESS, CONF_NAME: DEFAULT_ENTRY_TITLE},
+        options={
+            CONF_MODE: MODE_RGB,
+            CONF_CUSTOM_EFFECT_COLORS: "[255,0,0], [0,0,255]",
+            CONF_CUSTOM_EFFECT_SPEED_PCT: 30,
+            CONF_CUSTOM_EFFECT_TRANSITION: TRANSITION_STROBE,
+        },
+        unique_id=MAC_ADDRESS,
+    )
+    config_entry.add_to_hass(hass)
+
+    with _patch_discovery(), _patch_wifibulb():
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    result = await hass.config_entries.options.async_init(config_entry.entry_id)
+    assert result["type"] == "form"
+    assert result["step_id"] == "init"
+
+    user_input = {
+        CONF_MODE: MODE_AUTO,
+        CONF_CUSTOM_EFFECT_COLORS: "[0,0,255], [255,0,0]",
+        CONF_CUSTOM_EFFECT_SPEED_PCT: 50,
+        CONF_CUSTOM_EFFECT_TRANSITION: TRANSITION_JUMP,
+    }
+    with _patch_discovery(), _patch_wifibulb():
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"], user_input
+        )
+        await hass.async_block_till_done()
+    assert result2["type"] == "create_entry"
+    assert result2["data"] == user_input
+    assert result2["data"] == config_entry.options
+    assert hass.states.get("light.az120444_aabbccddeeff") is not None
