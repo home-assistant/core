42494fdc10e1d9c24afb3bb13afd606e109a2f9f
Erik <erik@montnemery.com>
2021-09-30 11:17:01 +0200

    Optimize statistics generation

--- tests/components/sensor/test_recorder.py
+++ tests/components/sensor/test_recorder.py
@@ -1618,12 +1618,17 @@ def test_compile_hourly_statistics_changing_statistics(
     assert statistic_ids == [
         {"statistic_id": "sensor.test1", "unit_of_measurement": None}
     ]
-    metadata = get_metadata(hass, "sensor.test1")
+    metadata = get_metadata(hass, ("sensor.test1",))
     assert metadata == {
-        "has_mean": True,
-        "has_sum": False,
-        "statistic_id": "sensor.test1",
-        "unit_of_measurement": None,
+        "sensor.test1": (
+            1,
+            {
+                "has_mean": True,
+                "has_sum": False,
+                "statistic_id": "sensor.test1",
+                "unit_of_measurement": None,
+            },
+        )
     }
 
     # Add more states, with changed state class
@@ -1638,12 +1643,17 @@ def test_compile_hourly_statistics_changing_statistics(
     assert statistic_ids == [
         {"statistic_id": "sensor.test1", "unit_of_measurement": None}
     ]
-    metadata = get_metadata(hass, "sensor.test1")
+    metadata = get_metadata(hass, ("sensor.test1",))
     assert metadata == {
-        "has_mean": False,
-        "has_sum": True,
-        "statistic_id": "sensor.test1",
-        "unit_of_measurement": None,
+        "sensor.test1": (
+            1,
+            {
+                "has_mean": False,
+                "has_sum": True,
+                "statistic_id": "sensor.test1",
+                "unit_of_measurement": None,
+            },
+        )
     }
     stats = statistics_during_period(hass, period0, period="5minute")
     assert stats == {
