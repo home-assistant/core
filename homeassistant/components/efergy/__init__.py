66a471a4ade082d4816ff05a69d99fa157cdac7d
Robert Hillis <tkdrob4390@yahoo.com>
2021-10-01 07:43:16 -0400

    Add config flow for efergy

--- homeassistant/components/efergy/__init__.py
+++ homeassistant/components/efergy/__init__.py
@@ -1,1 +1,84 @@
-"""The efergy component."""
+"""The Efergy integration."""
+from __future__ import annotations
+
+from pyefergy import Efergy, exceptions
+
+from homeassistant.components.sensor import DOMAIN as DOMAIN_SENSOR
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    ATTR_ATTRIBUTION,
+    ATTR_IDENTIFIERS,
+    ATTR_MANUFACTURER,
+    ATTR_MODEL,
+    ATTR_NAME,
+    ATTR_SW_VERSION,
+    CONF_API_KEY,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.entity import DeviceInfo, Entity
+
+from .const import ATTRIBUTION, DATA_KEY_API, DEFAULT_NAME, DOMAIN
+
+PLATFORMS = [DOMAIN_SENSOR]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Efergy from a config entry."""
+    api = Efergy(
+        entry.data[CONF_API_KEY],
+        async_get_clientsession(hass),
+        utc_offset=hass.config.time_zone,
+        currency=hass.config.currency,
+    )
+
+    try:
+        await api.async_status()
+    except (exceptions.ConnectError, exceptions.DataError) as ex:
+        raise ConfigEntryNotReady(f"Failed to connect to device: {ex}") from ex
+    except exceptions.InvalidAuth as ex:
+        raise ConfigEntryAuthFailed(
+            "API Key is no longer valid. Please reauthenticate"
+        ) from ex
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {DATA_KEY_API: api}
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
+
+
+class EfergyEntity(Entity):
+    """Representation of a Efergy entity."""
+
+    _attr_extra_state_attributes = {ATTR_ATTRIBUTION: ATTRIBUTION}
+
+    def __init__(
+        self,
+        api: Efergy,
+        server_unique_id: str,
+    ) -> None:
+        """Initialize an Efergy entity."""
+        self.api = api
+        self._server_unique_id = server_unique_id
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device information of the entity."""
+        return {
+            "connections": {(dr.CONNECTION_NETWORK_MAC, self.api.info["mac"])},
+            ATTR_IDENTIFIERS: {(DOMAIN, self._server_unique_id)},
+            ATTR_MANUFACTURER: DEFAULT_NAME,
+            ATTR_NAME: DEFAULT_NAME,
+            ATTR_MODEL: self.api.info["type"],
+            ATTR_SW_VERSION: self.api.info["version"],
+        }
--- homeassistant/components/efergy/sensor.py
+++ homeassistant/components/efergy/sensor.py
@@ -1,18 +1,19 @@
 """Support for Efergy sensors."""
 from __future__ import annotations
 
+from re import sub
+
 from pyefergy import Efergy
 import voluptuous as vol
 
+from homeassistant.components.efergy import EfergyEntity
 from homeassistant.components.sensor import (
     PLATFORM_SCHEMA,
     SensorEntity,
     SensorEntityDescription,
 )
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import (
-    CONF_CURRENCY,
-    CONF_MONITORED_VARIABLES,
-    CONF_TYPE,
     DEVICE_CLASS_ENERGY,
     DEVICE_CLASS_MONETARY,
     DEVICE_CLASS_POWER,
@@ -20,71 +21,86 @@ from homeassistant.const import (
     POWER_WATT,
 )
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers import entity_platform
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-CONF_APPTOKEN = "app_token"
-CONF_UTC_OFFSET = "utc_offset"
+from .const import CONF_APPTOKEN, CONF_CURRENT_VALUES, DATA_KEY_API, DOMAIN
 
-CONF_PERIOD = "period"
-
-CONF_INSTANT = "instant_readings"
-CONF_AMOUNT = "amount"
-CONF_BUDGET = "budget"
-CONF_COST = "cost"
-CONF_CURRENT_VALUES = "current_values"
-
-DEFAULT_PERIOD = "year"
-DEFAULT_UTC_OFFSET = "0"
-
-SENSOR_TYPES: dict[str, SensorEntityDescription] = {
-    CONF_INSTANT: SensorEntityDescription(
-        key=CONF_INSTANT,
-        name="Energy Usage",
+SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
+    SensorEntityDescription(
+        key="instant_readings",
+        name="Power Usage",
         device_class=DEVICE_CLASS_POWER,
         native_unit_of_measurement=POWER_WATT,
     ),
-    CONF_AMOUNT: SensorEntityDescription(
-        key=CONF_AMOUNT,
-        name="Energy Consumed",
+    SensorEntityDescription(
+        key="energy_day",
+        name="Daily Consumption",
+        device_class=DEVICE_CLASS_ENERGY,
+        native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="energy_week",
+        name="Weekly Consumption",
+        device_class=DEVICE_CLASS_ENERGY,
+        native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="energy_month",
+        name="Monthly Consumption",
         device_class=DEVICE_CLASS_ENERGY,
         native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
     ),
-    CONF_BUDGET: SensorEntityDescription(
-        key=CONF_BUDGET,
-        name="Energy Budget",
+    SensorEntityDescription(
+        key="energy_year",
+        name="Yearly Consumption",
+        device_class=DEVICE_CLASS_ENERGY,
+        native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        entity_registry_enabled_default=False,
     ),
-    CONF_COST: SensorEntityDescription(
-        key=CONF_COST,
-        name="Energy Cost",
+    SensorEntityDescription(
+        key="budget",
+        name="Energy Budget",
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="cost_day",
+        name="Daily Energy Cost",
+        device_class=DEVICE_CLASS_MONETARY,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="cost_week",
+        name="Weekly Energy Cost",
+        device_class=DEVICE_CLASS_MONETARY,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
+        key="cost_month",
+        name="Monthly Energy Cost",
         device_class=DEVICE_CLASS_MONETARY,
     ),
-    CONF_CURRENT_VALUES: SensorEntityDescription(
+    SensorEntityDescription(
+        key="cost_year",
+        name="Yearly Energy Cost",
+        device_class=DEVICE_CLASS_MONETARY,
+        entity_registry_enabled_default=False,
+    ),
+    SensorEntityDescription(
         key=CONF_CURRENT_VALUES,
-        name="Per-Device Usage",
+        name="Power Usage",
         device_class=DEVICE_CLASS_POWER,
         native_unit_of_measurement=POWER_WATT,
     ),
-}
-
-TYPES_SCHEMA = vol.In(SENSOR_TYPES)
-
-SENSORS_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_TYPE): TYPES_SCHEMA,
-        vol.Optional(CONF_CURRENCY, default=""): cv.string,
-        vol.Optional(CONF_PERIOD, default=DEFAULT_PERIOD): cv.string,
-    }
 )
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Required(CONF_APPTOKEN): cv.string,
-        vol.Optional(CONF_UTC_OFFSET, default=DEFAULT_UTC_OFFSET): cv.string,
-        vol.Required(CONF_MONITORED_VARIABLES): [SENSORS_SCHEMA],
-    }
+# Deprecated in Home Assistant 2021.11
+PLATFORM_SCHEMA = cv.deprecated(
+    vol.All(PLATFORM_SCHEMA.extend({vol.Required(CONF_APPTOKEN): cv.string}))
 )
 
 
@@ -95,58 +111,71 @@ async def async_setup_platform(
     discovery_info: DiscoveryInfoType = None,
 ) -> None:
     """Set up the Efergy sensor."""
-    api = Efergy(
-        config[CONF_APPTOKEN],
-        async_get_clientsession(hass),
-        utc_offset=config[CONF_UTC_OFFSET],
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=config
+        )
     )
 
-    dev = []
-    sensors = await api.get_sids()
-    for variable in config[CONF_MONITORED_VARIABLES]:
-        if variable[CONF_TYPE] == CONF_CURRENT_VALUES:
-            for sensor in sensors:
-                dev.append(
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: entity_platform.AddEntitiesCallback,
+) -> None:
+    """Set up the Modem Caller ID sensor."""
+    api: Efergy = hass.data[DOMAIN][entry.entry_id][DATA_KEY_API]
+    sids = await api.async_get_sids()
+    sensors = []
+    for description in SENSOR_TYPES:
+        if description.key != CONF_CURRENT_VALUES:
+            sensors.append(
+                EfergySensor(
+                    api,
+                    description,
+                    entry.entry_id,
+                    period=sub("^energy_|^cost_", "", description.key),
+                    currency=hass.config.currency,
+                )
+            )
+        else:
+            description.entity_registry_enabled_default = len(sids) != 1
+            for sid in sids:
+                sensors.append(
                     EfergySensor(
                         api,
-                        variable[CONF_PERIOD],
-                        variable[CONF_CURRENCY],
-                        SENSOR_TYPES[variable[CONF_TYPE]],
-                        sid=sensor["sid"],
+                        description,
+                        entry.entry_id,
+                        sid=sid,
                     )
                 )
-        dev.append(
-            EfergySensor(
-                api,
-                variable[CONF_PERIOD],
-                variable[CONF_CURRENCY],
-                SENSOR_TYPES[variable[CONF_TYPE]],
-            )
-        )
-
-    add_entities(dev, True)
+    async_add_entities(sensors, True)
 
 
-class EfergySensor(SensorEntity):
+class EfergySensor(EfergyEntity, SensorEntity):
     """Implementation of an Efergy sensor."""
 
     def __init__(
         self,
         api: Efergy,
-        period: str,
-        currency: str,
         description: SensorEntityDescription,
+        server_unique_id: str,
+        period: str = None,
+        currency: str = None,
         sid: str = None,
     ) -> None:
         """Initialize the sensor."""
+        super().__init__(api, server_unique_id)
         self.entity_description = description
+        if description.key == CONF_CURRENT_VALUES:
+            self._attr_name = f"{description.name}_{sid}"
+            self._attr_unique_id = f"{server_unique_id}/{description.key}/{sid}"
+        else:
+            self._attr_unique_id = f"{server_unique_id}/{description.key}"
+        if "cost" in description.key:
+            self._attr_native_unit_of_measurement = currency
         self.sid = sid
-        self.api = api
         self.period = period
-        if sid:
-            self._attr_name = f"efergy_{sid}"
-        if description.key == CONF_COST:
-            self._attr_native_unit_of_measurement = f"{currency}/{period}"
 
     async def async_update(self) -> None:
         """Get the Efergy monitor data from the web service."""
