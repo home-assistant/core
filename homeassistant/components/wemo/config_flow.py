771d9e5debc68ecf0f59d3c0d8e0567cdf62d369
Eric Severance <esev@esev.com>
2021-10-02 19:50:16 -0700

    Add wemo options enable_subscription & enable_long_press

--- homeassistant/components/wemo/config_flow.py
+++ homeassistant/components/wemo/config_flow.py
@@ -1,11 +1,18 @@
 """Config flow for Wemo."""
 
+from __future__ import annotations
+
+from typing import Any
+
 import pywemo
 
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_entry_flow
+from homeassistant.config_entries import ConfigEntry, OptionsFlow
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers.config_entry_flow import DiscoveryFlowHandler
 
 from .const import DOMAIN
+from .wemo_device import Options
 
 
 async def _async_has_devices(hass: HomeAssistant) -> bool:
@@ -13,4 +20,35 @@ async def _async_has_devices(hass: HomeAssistant) -> bool:
     return bool(await hass.async_add_executor_job(pywemo.discover_devices))
 
 
-config_entry_flow.register_discovery_flow(DOMAIN, "Wemo", _async_has_devices)
+class WemoFlow(DiscoveryFlowHandler, domain=DOMAIN):
+    """Discovery flow with options for Wemo."""
+
+    def __init__(self) -> None:
+        """Init discovery flow."""
+        super().__init__(DOMAIN, "Wemo", _async_has_devices)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlow:
+        """Get the options flow for this handler."""
+        return WemoOptionsFlow(config_entry)
+
+
+class WemoOptionsFlow(OptionsFlow):
+    """Options flow for the WeMo component."""
+
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Manage options for the WeMo component."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=Options(**self.config_entry.options).schema(),
+        )
