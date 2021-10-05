78a2812d106030e7247ea40b9ffbd2477ff84519
John Parchem <john@parchem.net>
2021-10-05 01:19:17 -0500

    Add support for Levoit Core400S air purifier to VeSync integration

--- homeassistant/components/vesync/fan.py
+++ homeassistant/components/vesync/fan.py
@@ -19,6 +19,7 @@ _LOGGER = logging.getLogger(__name__)
 DEV_TYPE_TO_HA = {
     "LV-PUR131S": "fan",
     "Core200S": "fan",
+    "Core400S": "fan",
 }
 
 FAN_MODE_AUTO = "auto"
@@ -27,6 +28,7 @@ FAN_MODE_SLEEP = "sleep"
 PRESET_MODES = {
     "LV-PUR131S": [FAN_MODE_AUTO, FAN_MODE_SLEEP],
     "Core200S": [FAN_MODE_SLEEP],
+    "Core400S": [FAN_MODE_AUTO, FAN_MODE_SLEEP],
 }
 SPEED_RANGE = (1, 3)  # off is not included
 
