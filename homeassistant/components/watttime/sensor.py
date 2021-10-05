5ba585d9e380eb13787c29d1ec5fcbc71d4005c2
Aaron Bach <bachya1208@gmail.com>
2021-10-05 11:45:44 -0600

    Add WattTime configuration option for showing the monitored location on the map

--- homeassistant/components/watttime/sensor.py
+++ homeassistant/components/watttime/sensor.py
@@ -1,7 +1,8 @@
 """Support for WattTime sensors."""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, cast
+from collections.abc import Mapping
+from typing import TYPE_CHECKING, Any, cast
 
 from homeassistant.components.sensor import (
     STATE_CLASS_MEASUREMENT,
@@ -27,6 +28,7 @@ from homeassistant.helpers.update_coordinator import (
 from .const import (
     CONF_BALANCING_AUTHORITY,
     CONF_BALANCING_AUTHORITY_ABBREV,
+    CONF_SHOW_ON_MAP,
     DATA_COORDINATOR,
     DOMAIN,
 )
@@ -85,17 +87,35 @@ class RealtimeEmissionsSensor(CoordinatorEntity, SensorEntity):
         if TYPE_CHECKING:
             assert coordinator.config_entry
 
-        self._attr_extra_state_attributes = {
-            ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION,
-            ATTR_BALANCING_AUTHORITY: coordinator.config_entry.data[
-                CONF_BALANCING_AUTHORITY
-            ],
-            ATTR_LATITUDE: coordinator.config_entry.data[ATTR_LATITUDE],
-            ATTR_LONGITUDE: coordinator.config_entry.data[ATTR_LONGITUDE],
-        }
         self._attr_name = f"{description.name} ({coordinator.config_entry.data[CONF_BALANCING_AUTHORITY_ABBREV]})"
         self._attr_unique_id = f"{coordinator.config_entry.entry_id}_{description.key}"
         self.entity_description = description
+
+    @property
+    def extra_state_attributes(self) -> Mapping[str, Any] | None:
+        """Return entity specific state attributes."""
+        if TYPE_CHECKING:
+            assert self.coordinator.config_entry
+
+        attrs = {
+            ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION,
+            ATTR_BALANCING_AUTHORITY: self.coordinator.config_entry.data[
+                CONF_BALANCING_AUTHORITY
+            ],
+        }
+
+        # Displaying the geography on the map relies upon putting the latitude/longitude
+        # in the entity attributes with "latitude" and "longitude" as the keys.
+        # Conversely, we can hide the location on the map by using other keys, like
+        # "lati" and "long".
+        if self.coordinator.config_entry.options.get(CONF_SHOW_ON_MAP) is not False:
+            attrs[ATTR_LATITUDE] = self.coordinator.config_entry.data[ATTR_LATITUDE]
+            attrs[ATTR_LONGITUDE] = self.coordinator.config_entry.data[ATTR_LONGITUDE]
+        else:
+            attrs["lati"] = self.coordinator.config_entry.data[ATTR_LATITUDE]
+            attrs["long"] = self.coordinator.config_entry.data[ATTR_LONGITUDE]
+
+        return attrs
 
     @property
     def native_value(self) -> StateType:
