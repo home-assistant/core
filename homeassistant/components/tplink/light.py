e3e64130f1aab3b4e72bb4e84374c11e7b61ecff
Teemu R <tpr@iki.fi>
2021-10-08 00:20:26 +0200

    Fix transition handling for tplink lights (#57272)
    * Fix transition handling for tplink light
    
    * Apply suggestions from code review
    
    * Test that all transitions are passed correctly
    
    * Fix linting
    
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/tplink/light.py
+++ homeassistant/components/tplink/light.py
@@ -63,7 +63,9 @@ class TPLinkSmartBulb(CoordinatedTPLinkEntity, LightEntity):
     @async_refresh_after
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the light on."""
-        transition = kwargs.get(ATTR_TRANSITION)
+        if (transition := kwargs.get(ATTR_TRANSITION)) is not None:
+            transition = int(transition * 1_000)
+
         if (brightness := kwargs.get(ATTR_BRIGHTNESS)) is not None:
             brightness = round((brightness * 100.0) / 255.0)
 
@@ -92,7 +94,9 @@ class TPLinkSmartBulb(CoordinatedTPLinkEntity, LightEntity):
     @async_refresh_after
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the light off."""
-        await self.device.turn_off(transition=kwargs.get(ATTR_TRANSITION))
+        if (transition := kwargs.get(ATTR_TRANSITION)) is not None:
+            transition = int(transition * 1_000)
+        await self.device.turn_off(transition=transition)
 
     @property
     def min_mireds(self) -> int:
