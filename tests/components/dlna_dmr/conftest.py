29f7dfc1504b61f10ce32b77d5ca5fa24efd84ef
Michael Chisholm <chishm-git@chishm.com>
2021-10-08 07:14:00 +1100

    Improve dlna_dmr code quality (#56886)
    * Listen for config updates from DlnaDmrEntity.async_added_to_hass
    
    Use `Entity.async_on_remove` for dealing with callback cancellation,
    instead of re-inventing the wheel with `_remove_ssdp_callbacks`.
    
    * Use async_write_ha_state within async methods
    
    * Import YAML config from async_setup_platform
    
    * Import flow prompts user when device is uncontactable during migration
    
    When config flow is able to contact a device, or when it has information
    from SSDP, it will create config entries without error. If the device is
    uncontactable at this point then it will appear as unavailable in HA
    until it is turned on again.
    
    When import flow cannot migrate an entry because it needs to contact the
    device and can't, it will notify the user with a config flow form.
    
    * Don't del unused parameters, HA pylint doesn't care
    
    * Remove unused imports from tests
    
    * Abort config flow at earliest opportunity
    
    * Return async_abort instead of raising AbortFlow
    
    * Consolidate config entry test cleanup into a single function
    
    * fixup! Consolidate config entry test cleanup into a single function
    
    Revert "Consolidate config entry test cleanup into a single function"
    
    This reverts commit 8220da7263e346a37c3e1a219dc374c4a43c7df5.
    
    * Check resource acquisition/release in specific tests
    
    * fixup! Check resource acquisition/release in specific tests
    
    * Remove unused network dependency from manifest
    
    * _on_event runs in async context
    
    * Call async_write_ha_state directly (not via shedule_update)
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/dlna_dmr/conftest.py
+++ tests/components/dlna_dmr/conftest.py
@@ -52,20 +52,12 @@ def domain_data_mock(hass: HomeAssistant) -> Iterable[Mock]:
     seal(upnp_device)
     domain_data.upnp_factory.async_create_device.return_value = upnp_device
 
-    domain_data.unmigrated_config = {}
-
     with patch.dict(hass.data, {DLNA_DOMAIN: domain_data}):
         yield domain_data
 
-    # Make sure the event notifiers are released
-    assert (
-        domain_data.async_get_event_notifier.await_count
-        == domain_data.async_release_event_notifier.await_count
-    )
-
 
 @pytest.fixture
-def config_entry_mock() -> Iterable[MockConfigEntry]:
+def config_entry_mock() -> MockConfigEntry:
     """Mock a config entry for this platform."""
     mock_entry = MockConfigEntry(
         unique_id=MOCK_DEVICE_UDN,
@@ -78,7 +70,7 @@ def config_entry_mock() -> Iterable[MockConfigEntry]:
         title=MOCK_DEVICE_NAME,
         options={},
     )
-    yield mock_entry
+    return mock_entry
 
 
 @pytest.fixture
@@ -100,14 +92,6 @@ def dmr_device_mock(domain_data_mock: Mock) -> Iterable[Mock]:
 
         yield device
 
-        # Make sure the device is disconnected
-        assert (
-            device.async_subscribe_services.await_count
-            == device.async_unsubscribe_services.await_count
-        )
-
-        assert device.on_event is None
-
 
 @pytest.fixture(name="skip_notifications", autouse=True)
 def skip_notifications_fixture() -> Iterable[None]:
@@ -125,9 +109,6 @@ def ssdp_scanner_mock() -> Iterable[Mock]:
         reg_callback = mock_scanner.return_value.async_register_callback
         reg_callback.return_value = Mock(return_value=None)
         yield mock_scanner.return_value
-        assert (
-            reg_callback.call_count == reg_callback.return_value.call_count
-        ), "Not all callbacks unregistered"
 
 
 @pytest.fixture(autouse=True)
