2433278840f62cb3f05912cc840e9dbf439906f1
Allen Porter <allen@thebends.org>
2021-10-08 00:11:51 -0700

    Remove unnecessary checks covered by websocket function

--- homeassistant/components/nest/camera_sdm.py
+++ homeassistant/components/nest/camera_sdm.py
@@ -269,10 +269,6 @@ class NestCamera(Camera):
 
     async def async_handle_web_rtc_offer(self, offer_sdp: str) -> str:
         """Return the source of the stream."""
-        if not self.supported_features & SUPPORT_STREAM:
-            raise ValueError("Camera does not support streaming")
-        if self.stream_type != STREAM_TYPE_WEB_RTC:
-            raise ValueError("Camera does not support Web RTC")
         trait: CameraLiveStreamTrait = self._device.traits[CameraLiveStreamTrait.NAME]
         stream = await trait.generate_web_rtc_stream(offer_sdp)
         return stream.answer_sdp
