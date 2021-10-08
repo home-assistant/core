668c19d0fe0eeff9a5d48626c0a99c2a121bc764
Aaron Bach <bachya1208@gmail.com>
2021-10-08 12:03:47 -0600

    Reorganize RainMachine services (#57145)
    * Reorganize RainMachine services
    
    * Code review
    
    * Ensure integration services aren't tied to a particular config entry
    
    * Cleanup
    
    * linting
    
    * Code review
    
    * Code review
    
    * Code review
    
    * Code review

--- homeassistant/components/rainmachine/__init__.py
+++ homeassistant/components/rainmachine/__init__.py
@@ -9,16 +9,18 @@ from typing import Any
 from regenmaschine import Client
 from regenmaschine.controller import Controller
 from regenmaschine.errors import RainMachineError
+import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     ATTR_ATTRIBUTION,
+    CONF_DEVICE_ID,
     CONF_IP_ADDRESS,
     CONF_PASSWORD,
     CONF_PORT,
     CONF_SSL,
 )
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant, ServiceCall, callback
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import aiohttp_client, config_validation as cv
 import homeassistant.helpers.device_registry as dr
@@ -44,6 +46,8 @@ from .const import (
     LOGGER,
 )
 
+CONF_SECONDS = "seconds"
+
 DEFAULT_ATTRIBUTION = "Data provided by Green Electronics LLC"
 DEFAULT_ICON = "mdi:water"
 DEFAULT_SSL = True
@@ -52,6 +56,39 @@ DEFAULT_UPDATE_INTERVAL = timedelta(seconds=15)
 CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 
 PLATFORMS = ["binary_sensor", "sensor", "switch"]
+
+SERVICE_NAME_PAUSE_WATERING = "pause_watering"
+SERVICE_NAME_STOP_ALL = "stop_all"
+SERVICE_NAME_UNPAUSE_WATERING = "unpause_watering"
+
+SERVICE_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_DEVICE_ID): cv.string,
+    }
+)
+
+SERVICE_PAUSE_WATERING_SCHEMA = SERVICE_SCHEMA.extend(
+    {
+        vol.Required(CONF_SECONDS): cv.positive_int,
+    }
+)
+
+
+@callback
+def async_get_controller_for_service_call(
+    hass: HomeAssistant, call: ServiceCall
+) -> Controller:
+    """Get the controller related to a service call (by device ID)."""
+    controllers: dict[str, Controller] = hass.data[DOMAIN][DATA_CONTROLLER]
+    device_id = call.data[CONF_DEVICE_ID]
+    device_registry = dr.async_get(hass)
+
+    if device_entry := device_registry.async_get(device_id):
+        for entry_id in device_entry.config_entries:
+            if entry_id in controllers:
+                return controllers[entry_id]
+
+    raise ValueError(f"No controller for device ID: {device_id}")
 
 
 async def async_update_programs_and_zones(
@@ -158,6 +195,37 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
 
+    async def async_pause_watering(call: ServiceCall) -> None:
+        """Pause watering for a set number of seconds."""
+        controller = async_get_controller_for_service_call(hass, call)
+        await controller.watering.pause_all(call.data[CONF_SECONDS])
+        await async_update_programs_and_zones(hass, entry)
+
+    async def async_stop_all(call: ServiceCall) -> None:
+        """Stop all watering."""
+        controller = async_get_controller_for_service_call(hass, call)
+        await controller.watering.stop_all()
+        await async_update_programs_and_zones(hass, entry)
+
+    async def async_unpause_watering(call: ServiceCall) -> None:
+        """Unpause watering."""
+        controller = async_get_controller_for_service_call(hass, call)
+        await controller.watering.unpause_all()
+        await async_update_programs_and_zones(hass, entry)
+
+    for service_name, schema, method in (
+        (
+            SERVICE_NAME_PAUSE_WATERING,
+            SERVICE_PAUSE_WATERING_SCHEMA,
+            async_pause_watering,
+        ),
+        (SERVICE_NAME_STOP_ALL, SERVICE_SCHEMA, async_stop_all),
+        (SERVICE_NAME_UNPAUSE_WATERING, SERVICE_SCHEMA, async_unpause_watering),
+    ):
+        if hass.services.has_service(DOMAIN, service_name):
+            continue
+        hass.services.async_register(DOMAIN, service_name, method, schema=schema)
+
     return True
 
 
@@ -166,6 +234,17 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN][DATA_COORDINATOR].pop(entry.entry_id)
+
+    if len(hass.config_entries.async_entries(DOMAIN)) == 1:
+        # If this is the last instance of RainMachine, deregister any services defined
+        # during integration setup:
+        for service_name in (
+            SERVICE_NAME_PAUSE_WATERING,
+            SERVICE_NAME_STOP_ALL,
+            SERVICE_NAME_UNPAUSE_WATERING,
+        ):
+            hass.services.async_remove(DOMAIN, service_name)
+
     return unload_ok
 
 
