d39db4f662ddf06475e1fdab7f29046ffb0aba5a
Abílio Costa <abmantis@users.noreply.github.com>
2021-10-09 23:49:47 +0100

    Add return typing
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/whirlpool/config_flow.py
+++ homeassistant/components/whirlpool/config_flow.py
@@ -18,7 +18,9 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 
-async def validate_input(hass: core.HomeAssistant, data: dict):
+async def validate_input(
+    hass: core.HomeAssistant, data: dict[str, str]
+) -> dict[str, str]:
     """Validate the user input allows us to connect.
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
