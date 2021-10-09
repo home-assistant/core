fa79a546116d7689acefc18841dad937ee0149b9
Randy <randydfurman@gmail.com>
2021-10-09 18:13:25 +0000

    Fix honeywell sensor

--- homeassistant/components/honeywell/sensor.py
+++ homeassistant/components/honeywell/sensor.py
@@ -1,12 +1,8 @@
 """Support for Honeywell (US) Total Connect Comfort sensors."""
 from __future__ import annotations
 
-from homeassistant.components.honeywell.const import (
-    DOMAIN,
-    SENSOR_LOCATION_INDOOR,
-    SENSOR_LOCATION_OUTDOOR,
-)
-from homeassistant.components.sensor import STATE_CLASS_MEASUREMENT, SensorEntity
+from homeassistant.components.honeywell.const import DOMAIN
+from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import (
     DEVICE_CLASS_HUMIDITY,
     DEVICE_CLASS_TEMPERATURE,
@@ -14,7 +10,6 @@ from homeassistant.const import (
     TEMP_CELSIUS,
     TEMP_FAHRENHEIT,
 )
-import homeassistant.helpers.device_registry as dr
 
 
 async def async_setup_entry(hass, config, async_add_entities, discovery_info=None):
@@ -22,88 +17,62 @@ async def async_setup_entry(hass, config, async_add_entities, discovery_info=Non
     data = hass.data[DOMAIN][config.entry_id]
     sensors = []
 
-    if data.device.current_temperature is not None:
-        sensors.append(
-            HoneywellUSSensor(data, SENSOR_LOCATION_INDOOR, DEVICE_CLASS_TEMPERATURE)
-        )
-    if data.device.current_humidity is not None:
-        sensors.append(
-            HoneywellUSSensor(data, SENSOR_LOCATION_INDOOR, DEVICE_CLASS_HUMIDITY)
-        )
-    if data.device.outdoor_temperature is not None:
-        sensors.append(
-            HoneywellUSSensor(data, SENSOR_LOCATION_OUTDOOR, DEVICE_CLASS_TEMPERATURE)
-        )
-    if data.device.outdoor_humidity is not None:
-        sensors.append(
-            HoneywellUSSensor(data, SENSOR_LOCATION_OUTDOOR, DEVICE_CLASS_HUMIDITY)
-        )
+    for device in data.devices:
+        if device.outdoor_temperature is not None:
+            sensors.append(HoneywellOutdoorTemperatureSensor(device))
+        if device.outdoor_humidity is not None:
+            sensors.append(HoneywellOutdoorHumiditySensor(device))
 
     async_add_entities(sensors)
 
 
-class HoneywellUSSensor(SensorEntity):
-    """Representation of a Honeywell US Sensor."""
+class HoneywellOutdoorTemperatureSensor(SensorEntity):
+    """Representation of a Honeywell US Outdoor Temperature Sensor."""
 
-    def __init__(self, data, location, sensor_type):
-        """Initialize the sensor."""
-        self._data = data
-        self._attr_unique_id = (
-            f"{dr.format_mac(data.device.mac_address)}_{location}_{sensor_type}"
-        )
-        self._attr_name = f"{data.device.name} {location} {sensor_type}"
-        self._location = location
-        self._type = sensor_type
-
-    @property
-    def device(self):
-        """Shortcut to access the device."""
-        return self._data.device
-
-    @property
-    def device_class(self):
-        """Return the device class of the sensor."""
-        return self._type
+    def __init__(self, device):
+        """Initialize the outdoor temperature sensor."""
+        self._device = device
+        self._state = None
+        self._attr_unique_id = f"{device.deviceid}_outdoor_{DEVICE_CLASS_TEMPERATURE}"
+        self._attr_name = f"{device.name} outdoor {DEVICE_CLASS_TEMPERATURE}"
+        self._attr_device_class = DEVICE_CLASS_TEMPERATURE
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        if self._type == DEVICE_CLASS_TEMPERATURE:
-            return self.temperature
-        if self._type == DEVICE_CLASS_HUMIDITY:
-            return self.humidity
-
-    @property
-    def state_class(self):
-        """Return the state class of the sensor."""
-        return STATE_CLASS_MEASUREMENT
+        return self._state
 
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement this sensor expresses itself in."""
-        if self._type == DEVICE_CLASS_TEMPERATURE:
-            return (
-                TEMP_CELSIUS if self.device.temperature_unit == "C" else TEMP_FAHRENHEIT
-            )
+        return TEMP_CELSIUS if self._device.temperature_unit == "C" else TEMP_FAHRENHEIT
 
+    def update(self):
+        """Get the latest state from the service."""
+        self._state = self._device.outdoor_temperature
+
+
+class HoneywellOutdoorHumiditySensor(SensorEntity):
+    """Representation of a Honeywell US Outdoor Humidity Sensor."""
+
+    def __init__(self, device):
+        """Initialize the outdoor humidity sensor."""
+        self._device = device
+        self._state = None
+        self._attr_unique_id = f"{device.deviceid}_outdoor_{DEVICE_CLASS_HUMIDITY}"
+        self._attr_name = f"{device.name} outdoor {DEVICE_CLASS_HUMIDITY}"
+        self._attr_device_class = DEVICE_CLASS_HUMIDITY
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement this sensor expresses itself in."""
         return PERCENTAGE
 
-    @property
-    def humidity(self) -> int | None:
-        """Return the current outdoor humidity."""
-        if self._location == SENSOR_LOCATION_INDOOR:
-            return self.device.current_humidity
-        if self._location == SENSOR_LOCATION_OUTDOOR:
-            return self.device.outdoor_humidity
-
-    @property
-    def temperature(self) -> float | None:
-        """Return the current outdoor temperature."""
-        if self._location == SENSOR_LOCATION_INDOOR:
-            return self.device.current_temperature
-        if self._location == SENSOR_LOCATION_OUTDOOR:
-            return self.device.outdoor_temperature
-
-    async def async_update(self):
-        """Get the latest state of the sensor."""
-        await self._data.update()
+    def update(self):
+        """Get the latest state from the service."""
+        self._state = self._device.outdoor_humidity
