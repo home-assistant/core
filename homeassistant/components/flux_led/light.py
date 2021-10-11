d7c43dc162dbaf7025da4b99f804c79ef9092afd
J. Nick Koston <nick@koston.org>
2021-10-10 14:19:40 -1000

    Revert "Use hs color to avoid brightness adjustment on RGB only lights that do not support RGBW/RGBWW"
    This reverts commit 199383721dc1449c02912a693432a96664b1639d.

--- homeassistant/components/flux_led/light.py
+++ homeassistant/components/flux_led/light.py
@@ -28,14 +28,14 @@ from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
     ATTR_COLOR_TEMP,
     ATTR_EFFECT,
-    ATTR_HS_COLOR,
+    ATTR_RGB_COLOR,
     ATTR_RGBW_COLOR,
     ATTR_RGBWW_COLOR,
     ATTR_WHITE,
     COLOR_MODE_BRIGHTNESS,
     COLOR_MODE_COLOR_TEMP,
-    COLOR_MODE_HS,
     COLOR_MODE_ONOFF,
+    COLOR_MODE_RGB,
     COLOR_MODE_RGBW,
     COLOR_MODE_RGBWW,
     COLOR_MODE_WHITE,
@@ -67,8 +67,6 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util.color import (
-    color_hs_to_RGB,
-    color_RGB_to_hs,
     color_temperature_kelvin_to_mired,
     color_temperature_mired_to_kelvin,
 )
@@ -104,10 +102,7 @@ SUPPORT_FLUX_LED: Final = SUPPORT_EFFECT | SUPPORT_TRANSITION
 
 
 FLUX_COLOR_MODE_TO_HASS: Final = {
-    # If the bulb is in RGB mode and does not support RGBW or RGBWW
-    # the UI will decrease the brightness every time the color is adjusted
-    # via https://github.com/home-assistant/frontend/blob/e797c017614797bb11671496d6bd65863de22063/src/dialogs/more-info/controls/more-info-light.ts#L263
-    FLUX_COLOR_MODE_RGB: COLOR_MODE_HS,
+    FLUX_COLOR_MODE_RGB: COLOR_MODE_RGB,
     FLUX_COLOR_MODE_RGBW: COLOR_MODE_RGBW,
     FLUX_COLOR_MODE_RGBWW: COLOR_MODE_RGBWW,
     FLUX_COLOR_MODE_CCT: COLOR_MODE_COLOR_TEMP,
@@ -347,9 +342,10 @@ class FluxLight(CoordinatorEntity, LightEntity):
         return color_temperature_kelvin_to_mired(self._device.color_temp)
 
     @property
-    def hs_color(self) -> tuple[float, float]:
-        """Return the hs color value."""
-        return color_RGB_to_hs(*self._device.rgb)
+    def rgb_color(self) -> tuple[int, int, int]:
+        """Return the rgb color value."""
+        rgb: tuple[int, int, int] = self._device.rgb
+        return rgb
 
     @property
     def rgbw_color(self) -> tuple[int, int, int, int]:
@@ -420,9 +416,9 @@ class FluxLight(CoordinatorEntity, LightEntity):
             await self._device.async_set_levels(r=0, b=0, g=0, w=warm, w2=cold)
             return
         # Handle switch to HS Color Mode
-        if ATTR_HS_COLOR in kwargs:
+        if ATTR_RGB_COLOR in kwargs:
             await self._device.async_set_levels(
-                *color_hs_to_RGB(*kwargs[ATTR_HS_COLOR]), brightness=brightness
+                *kwargs[ATTR_RGB_COLOR], brightness=brightness
             )
             return
         # Handle switch to RGBW Color Mode
@@ -475,10 +471,9 @@ class FluxLight(CoordinatorEntity, LightEntity):
         if self.color_mode == COLOR_MODE_COLOR_TEMP:
             await self._device.async_set_white_temp(self._device.color_temp, brightness)
             return
-        # Handle brightness adjustment in HS Color Mode
-        if self.color_mode == COLOR_MODE_HS:
-            rgb = color_hs_to_RGB(*self.hs_color)
-            await self._device.async_set_levels(*rgb, brightness=brightness)
+        # Handle brightness adjustment in RGB Color Mode
+        if self.color_mode == COLOR_MODE_RGB:
+            await self._device.async_set_levels(*self.rgb_color, brightness=brightness)
             return
         # Handle brightness adjustment in RGBW Color Mode
         if self.color_mode == COLOR_MODE_RGBW:
