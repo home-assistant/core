c6cb1a91c1589672a3c16c2cbdc4b724ca449f31
Glenn Waters <gwwaters@gmail.com>
2021-10-11 10:51:40 -0400

    Fix review comments.

--- tests/components/environment_canada/test_config_flow.py
+++ tests/components/environment_canada/test_config_flow.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as et
 import aiohttp
 import pytest
 
-from homeassistant import config_entries, data_entry_flow, setup
+from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.environment_canada.const import (
     CONF_LANGUAGE,
     CONF_STATION,
@@ -52,10 +52,26 @@ def mocked_ec(
     )
 
 
+async def test_create_entry(hass):
+    """Test creating an entry."""
+    with mocked_ec(), patch(
+        "homeassistant.components.environment_canada.async_setup_entry",
+        return_value=True,
+    ):
+        flow = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
+        result = await hass.config_entries.flow.async_configure(
+            flow["flow_id"], FAKE_CONFIG
+        )
+        await hass.async_block_till_done()
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result["data"] == FAKE_CONFIG
+        assert result["title"] == FAKE_TITLE
+
+
 async def test_create_same_entry_twice(hass):
     """Test duplicate entries."""
-    await setup.async_setup_component(hass, "persistent_notification", {})
-
     entry = MockConfigEntry(
         domain=DOMAIN,
         data=FAKE_CONFIG,
@@ -80,8 +96,6 @@ async def test_create_same_entry_twice(hass):
 
 async def test_too_many_attempts(hass):
     """Test hitting rate limit."""
-    await setup.async_setup_component(hass, "persistent_notification", {})
-
     with mocked_ec(metadata={}), patch(
         "homeassistant.components.environment_canada.async_setup_entry",
         return_value=True,
@@ -111,8 +125,6 @@ async def test_too_many_attempts(hass):
 async def test_exception_handling(hass, error):
     """Test exception handling."""
     exc, base_error = error
-    await setup.async_setup_component(hass, "persistent_notification", {})
-
     with patch(
         "homeassistant.components.environment_canada.config_flow.ECData",
         side_effect=exc,
