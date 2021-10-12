2a4071994e7a5822cfd27e0ecec0ffff1fd2a1f2
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-10-12 13:02:28 +0200

    Use list comprehension

--- homeassistant/components/bmw_connected_drive/binary_sensor.py
+++ homeassistant/components/bmw_connected_drive/binary_sensor.py
@@ -85,9 +85,10 @@ def _check_control_messages(
     check_control_messages = vehicle_state.check_control_messages
     has_check_control_messages = vehicle_state.has_check_control_messages
     if has_check_control_messages:
-        cbs_list = []
-        for message in check_control_messages:
-            cbs_list.append(message["ccmDescriptionShort"])  # type: ignore  # needs fix in 'bimmer_connected'
+        cbs_list = [
+            message["ccmDescriptionShort"]  # type: ignore  # needs fix in 'bimmer_connected'
+            for message in check_control_messages
+        ]
         extra_attributes["check_control_messages"] = cbs_list
     else:
         extra_attributes["check_control_messages"] = "OK"
