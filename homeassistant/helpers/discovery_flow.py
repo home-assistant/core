30790d3a44067b0af3542cc24037577a88bb02c2
J. Nick Koston <nick@koston.org>
2021-10-12 10:51:43 -1000

    fixes

--- homeassistant/helpers/discovery_flow.py
+++ homeassistant/helpers/discovery_flow.py
@@ -5,7 +5,7 @@ from collections.abc import Coroutine
 from typing import Any
 
 from homeassistant.const import EVENT_HOMEASSISTANT_STARTED
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import CoreState, Event, HomeAssistant, callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.loader import bind_hass
 from homeassistant.util.async_ import gather_with_concurrency
@@ -20,7 +20,7 @@ def async_create_flow(
     hass: HomeAssistant, domain: str, context: dict[str, Any], data: Any
 ) -> None:
     """Create a discovery flow."""
-    if DISCOVERY_FLOW_DISPATCHER not in hass.data[DISCOVERY_FLOW_DISPATCHER]:
+    if DISCOVERY_FLOW_DISPATCHER not in hass.data:
         dispatcher = hass.data[DISCOVERY_FLOW_DISPATCHER] = FlowDispatcher(hass)
         dispatcher.async_setup()
     else:
@@ -40,15 +40,19 @@ class FlowDispatcher:
     @callback
     def async_setup(self) -> None:
         """Set up the flow disptcher."""
+        if self.hass.state == CoreState.running:
+            self.started = True
+            return
         self.hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STARTED, self.async_start)
 
     @callback
-    def async_start(self, *_: Any) -> None:
+    def async_start(self, event: Event) -> None:
         """Start processing pending flows."""
         self.started = True
         self.hass.async_create_task(self._async_process_pending_flows())
 
     async def _async_process_pending_flows(self) -> None:
+        """Process any pending discovery flows."""
         init_coros = [self._init_flow(*flow) for flow in self.pending_flows]
         await gather_with_concurrency(
             FLOW_INIT_LIMIT,
@@ -59,8 +63,7 @@ class FlowDispatcher:
     def async_create(self, domain: str, context: dict[str, Any], data: Any) -> None:
         """Create and add or queue a flow."""
         if self.started:
-            init_coro = self._init_flow(domain, context, data)
-            if init_coro:
+            if init_coro := self._init_flow(domain, context, data):
                 self.hass.async_create_task(init_coro)
         else:
             self.pending_flows.append((domain, context, data))
