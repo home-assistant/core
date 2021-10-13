70a4bb38d9f48ce24118c71bceacf42b14468220
Daniel Hjelseth Høyer <github@dahoiv.net>
2021-10-13 21:46:55 +0200

    Add Mill sense sensor
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- homeassistant/components/mill/climate.py
+++ homeassistant/components/mill/climate.py
@@ -1,4 +1,5 @@
 """Support for mill wifi-enabled home heaters."""
+import mill
 import voluptuous as vol
 
 from homeassistant.components.climate import ClimateEntity
@@ -45,10 +46,12 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
     mill_data_coordinator = hass.data[DOMAIN]
 
-    dev = []
-    for heater in mill_data_coordinator.data.values():
-        dev.append(MillHeater(mill_data_coordinator, heater))
-    async_add_entities(dev)
+    entities = [
+        MillHeater(mill_data_coordinator, device)
+        for device in mill_data_coordinator.data.values()
+        if isinstance(device, mill.Heater)
+    ]
+    async_add_entities(entities)
 
     async def set_room_temp(service):
         """Set room temp."""
