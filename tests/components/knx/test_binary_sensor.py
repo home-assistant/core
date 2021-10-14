1c2f83194293d038c4f5d4b645d107f804ea7a1b
Marvin Wichmann <marvin.wichmann@unic.com>
2021-10-14 19:05:55 +0200

    Fix tests in a cleaner way

--- tests/components/knx/test_binary_sensor.py
+++ tests/components/knx/test_binary_sensor.py
@@ -37,7 +37,6 @@ async def test_binary_sensor(hass: HomeAssistant, knx: KNXTestKit):
     await knx.assert_read("2/2/2")
     await knx.receive_response("1/1/1", True)
     await knx.receive_response("2/2/2", False)
-    await hass.async_block_till_done()
     state_normal = hass.states.get("binary_sensor.test_normal")
     state_invert = hass.states.get("binary_sensor.test_invert")
     assert state_normal.state is STATE_ON
@@ -46,7 +45,6 @@ async def test_binary_sensor(hass: HomeAssistant, knx: KNXTestKit):
     # receive OFF telegram
     await knx.receive_write("1/1/1", False)
     await knx.receive_write("2/2/2", True)
-    await hass.async_block_till_done()
     state_normal = hass.states.get("binary_sensor.test_normal")
     state_invert = hass.states.get("binary_sensor.test_invert")
     assert state_normal.state is STATE_OFF
@@ -55,7 +53,6 @@ async def test_binary_sensor(hass: HomeAssistant, knx: KNXTestKit):
     # receive ON telegram
     await knx.receive_write("1/1/1", True)
     await knx.receive_write("2/2/2", False)
-    await hass.async_block_till_done()
     state_normal = hass.states.get("binary_sensor.test_normal")
     state_invert = hass.states.get("binary_sensor.test_invert")
     assert state_normal.state is STATE_ON
@@ -130,7 +127,7 @@ async def test_binary_sensor_counter(hass: HomeAssistant, knx: KNXTestKit):
                 {
                     CONF_NAME: "test",
                     CONF_STATE_ADDRESS: "2/2/2",
-                    BinarySensorSchema.CONF_CONTEXT_TIMEOUT: 0.1,
+                    BinarySensorSchema.CONF_CONTEXT_TIMEOUT: 0.001,
                     CONF_SYNC_STATE: False,
                 },
             ]
@@ -148,19 +145,22 @@ async def test_binary_sensor_counter(hass: HomeAssistant, knx: KNXTestKit):
     state = hass.states.get("binary_sensor.test")
     assert state.state is STATE_OFF
     assert state.attributes.get("counter") == 0
-    async_fire_time_changed(hass, dt.utcnow() + timedelta(seconds=0.1))
+    async_fire_time_changed(hass, dt.utcnow() + timedelta(seconds=0.001))
     await hass.async_block_till_done()
-    await asyncio.sleep(0.2)
+    await asyncio.sleep(0.002)
     # state changed twice after context timeout - once to ON with counter 1 and once to counter 0
     state = hass.states.get("binary_sensor.test")
     assert state.state is STATE_ON
     assert state.attributes.get("counter") == 0
     # additional async_block_till_done needed event capture
     await hass.async_block_till_done()
-    await hass.async_block_till_done()
     assert len(events) == 2
-    assert events.pop(0).data.get("new_state").attributes.get("counter") == 1
-    assert events.pop(0).data.get("new_state").attributes.get("counter") == 0
+    event = events.pop(0).data
+    assert event.get("new_state").attributes.get("counter") == 1
+    assert event.get("old_state").attributes.get("counter") == 0
+    event = events.pop(0).data
+    assert event.get("new_state").attributes.get("counter") == 0
+    assert event.get("old_state").attributes.get("counter") == 1
 
     # receive 2 telegrams in context
     await knx.receive_write("2/2/2", True)
