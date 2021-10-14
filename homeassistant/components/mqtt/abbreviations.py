1cdf917b82b7f038fe4b1ba855af77da5e2fe8d3
Franck Nijhof <git@frenck.dev>
2021-10-15 00:00:03 +0200

    cfg_url -> cu, to match other device info abbr.

--- homeassistant/components/mqtt/abbreviations.py
+++ homeassistant/components/mqtt/abbreviations.py
@@ -22,7 +22,7 @@ ABBREVIATIONS = {
     "clr_temp_cmd_tpl": "color_temp_command_template",
     "bat_lev_t": "battery_level_topic",
     "bat_lev_tpl": "battery_level_template",
-    "cfg_url": "configuration_url",
+    "cu": "configuration_url",
     "chrg_t": "charging_topic",
     "chrg_tpl": "charging_template",
     "clrm": "color_mode",
