3bd728a74751699a8726e726a5e058271b4ccf18
J. Nick Koston <nick@koston.org>
2021-10-13 20:53:40 -1000

    typing fixes

--- homeassistant/components/lookin/__init__.py
+++ homeassistant/components/lookin/__init__.py
@@ -60,3 +60,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
