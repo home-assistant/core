e17db16c2226d3bdd7741b427f92762d974298fb
jan iversen <jancasacondor@gmail.com>
2021-10-15 07:09:59 +0200

    COnvert DATA_TYPE to enum. (#57699)

--- homeassistant/components/modbus/climate.py
+++ homeassistant/components/modbus/climate.py
@@ -33,13 +33,8 @@ from .const import (
     CONF_MIN_TEMP,
     CONF_STEP,
     CONF_TARGET_TEMP,
-    DATA_TYPE_INT16,
-    DATA_TYPE_INT32,
-    DATA_TYPE_INT64,
-    DATA_TYPE_UINT16,
-    DATA_TYPE_UINT32,
-    DATA_TYPE_UINT64,
     AttrType,
+    DataType,
 )
 from .modbus import ModbusHub
 
@@ -115,12 +110,12 @@ class ModbusThermostat(BaseStructPlatform, RestoreEntity, ClimateEntity):
             float(kwargs[AttrType.TEMPERATURE]) - self._offset
         ) / self._scale
         if self._data_type in (
-            DATA_TYPE_INT16,
-            DATA_TYPE_INT32,
-            DATA_TYPE_INT64,
-            DATA_TYPE_UINT16,
-            DATA_TYPE_UINT32,
-            DATA_TYPE_UINT64,
+            DataType.INT16,
+            DataType.INT32,
+            DataType.INT64,
+            DataType.UINT16,
+            DataType.UINT32,
+            DataType.UINT64,
         ):
             target_temperature = int(target_temperature)
         as_bytes = struct.pack(self._structure, target_temperature)
