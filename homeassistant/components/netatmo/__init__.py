80c8b8e163b410fd65d21a1c4d29f81737f88fbc
cgtobi <cgtobi@gmail.com>
2021-10-15 22:48:49 +0000

    Add test

--- homeassistant/components/netatmo/__init__.py
+++ homeassistant/components/netatmo/__init__.py
@@ -246,7 +246,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
-    if unload_ok:
+    if unload_ok and entry.entry_id in hass.data[DOMAIN]:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
