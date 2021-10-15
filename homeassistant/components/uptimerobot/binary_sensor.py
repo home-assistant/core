736156852155abae89fc97ef22c77aae90d749a7
Joakim Sørensen <joasoe@gmail.com>
2021-10-15 12:33:10 +0200

    Remove YAML import from UptimeRobot (#57761)

--- homeassistant/components/uptimerobot/binary_sensor.py
+++ homeassistant/components/uptimerobot/binary_sensor.py
@@ -1,42 +1,18 @@
 """UptimeRobot binary_sensor platform."""
 from __future__ import annotations
 
-import voluptuous as vol
-
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_CONNECTIVITY,
-    PLATFORM_SCHEMA,
     BinarySensorEntity,
     BinarySensorEntityDescription,
 )
-from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
-from homeassistant.const import CONF_API_KEY
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from .const import DOMAIN
 from .entity import UptimeRobotEntity
-
-PLATFORM_SCHEMA = cv.deprecated(
-    vol.All(PLATFORM_SCHEMA.extend({vol.Required(CONF_API_KEY): cv.string}))
-)
-
-
-async def async_setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    async_add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
-) -> None:
-    """Set up the UptimeRobot binary_sensor platform."""
-    hass.async_create_task(
-        hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": SOURCE_IMPORT}, data=config
-        )
-    )
 
 
 async def async_setup_entry(
