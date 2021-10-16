94474b4eb6174590157ffbd64858284bdc74fead
Aaron Bach <bachya1208@gmail.com>
2021-10-15 19:40:37 -0600

    Organization

--- homeassistant/components/simplisafe/const.py
+++ homeassistant/components/simplisafe/const.py
@@ -5,10 +5,6 @@ LOGGER = logging.getLogger(__package__)
 
 DOMAIN = "simplisafe"
 
-CONF_USER_ID = "user_id"
-
-DATA_CLIENT = "client"
-
 ATTR_ALARM_DURATION = "alarm_duration"
 ATTR_ALARM_VOLUME = "alarm_volume"
 ATTR_CHIME_VOLUME = "chime_volume"
@@ -18,3 +14,7 @@ ATTR_EXIT_DELAY_AWAY = "exit_delay_away"
 ATTR_EXIT_DELAY_HOME = "exit_delay_home"
 ATTR_LIGHT = "light"
 ATTR_VOICE_PROMPT_VOLUME = "voice_prompt_volume"
+
+CONF_USER_ID = "user_id"
+
+DATA_CLIENT = "client"
