7390ba18843abfdf7186a58925b3008714402664
J. Nick Koston <nick@koston.org>
2021-10-16 10:37:28 -1000

    json

--- homeassistant/generated/dhcp.py
+++ homeassistant/generated/dhcp.py
@@ -428,11 +428,19 @@ DHCP = [
     },
     {
         "domain": "tuya",
+        "macaddress": "105A17*"
+    },
+    {
+        "domain": "tuya",
         "macaddress": "10D561*"
     },
     {
         "domain": "tuya",
         "macaddress": "1869D8*"
+    },
+    {
+        "domain": "tuya",
+        "macaddress": "381F8D*"
     },
     {
         "domain": "tuya",
@@ -444,11 +452,11 @@ DHCP = [
     },
     {
         "domain": "tuya",
-        "macaddress": "7CF666*"
+        "macaddress": "708976*"
     },
     {
         "domain": "tuya",
-        "macaddress": "708976*"
+        "macaddress": "7CF666*"
     },
     {
         "domain": "tuya",
@@ -457,6 +465,10 @@ DHCP = [
     {
         "domain": "tuya",
         "macaddress": "D4A651*"
+    },
+    {
+        "domain": "tuya",
+        "macaddress": "D81F12*"
     },
     {
         "domain": "verisure",
