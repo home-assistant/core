4aadb848e18e8e69f01d31a163f76de2e87025d7
Franck Nijhof <git@frenck.dev>
2021-10-17 19:24:49 +0200

    Add unit/device_class validation and normalization to Tuya (#57913)
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/tuya/const.py
+++ homeassistant/components/tuya/const.py
@@ -1,8 +1,67 @@
 """Constants for the Tuya integration."""
-from dataclasses import dataclass
+from __future__ import annotations
+
+from dataclasses import dataclass, field
 from enum import Enum
+from typing import Callable
 
 from tuya_iot import TuyaCloudOpenAPIEndpoint
+
+from homeassistant.const import (
+    CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    CONCENTRATION_MILLIGRAMS_PER_CUBIC_METER,
+    CONCENTRATION_PARTS_PER_BILLION,
+    CONCENTRATION_PARTS_PER_MILLION,
+    DEVICE_CLASS_AQI,
+    DEVICE_CLASS_BATTERY,
+    DEVICE_CLASS_CO,
+    DEVICE_CLASS_CO2,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_DATE,
+    DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_GAS,
+    DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_ILLUMINANCE,
+    DEVICE_CLASS_MONETARY,
+    DEVICE_CLASS_NITROGEN_DIOXIDE,
+    DEVICE_CLASS_NITROGEN_MONOXIDE,
+    DEVICE_CLASS_NITROUS_OXIDE,
+    DEVICE_CLASS_OZONE,
+    DEVICE_CLASS_PM1,
+    DEVICE_CLASS_PM10,
+    DEVICE_CLASS_PM25,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_POWER_FACTOR,
+    DEVICE_CLASS_PRESSURE,
+    DEVICE_CLASS_SIGNAL_STRENGTH,
+    DEVICE_CLASS_SULPHUR_DIOXIDE,
+    DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_TIMESTAMP,
+    DEVICE_CLASS_VOLATILE_ORGANIC_COMPOUNDS,
+    DEVICE_CLASS_VOLTAGE,
+    ELECTRIC_CURRENT_AMPERE,
+    ELECTRIC_CURRENT_MILLIAMPERE,
+    ELECTRIC_POTENTIAL_MILLIVOLT,
+    ELECTRIC_POTENTIAL_VOLT,
+    ENERGY_KILO_WATT_HOUR,
+    ENERGY_WATT_HOUR,
+    LIGHT_LUX,
+    PERCENTAGE,
+    POWER_KILO_WATT,
+    POWER_WATT,
+    PRESSURE_BAR,
+    PRESSURE_HPA,
+    PRESSURE_INHG,
+    PRESSURE_MBAR,
+    PRESSURE_PA,
+    PRESSURE_PSI,
+    SIGNAL_STRENGTH_DECIBELS,
+    SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+    TEMP_CELSIUS,
+    TEMP_FAHRENHEIT,
+    VOLUME_CUBIC_FEET,
+    VOLUME_CUBIC_METERS,
+)
 
 DOMAIN = "tuya"
 
@@ -153,6 +212,212 @@ class DPCode(str, Enum):
     WATER_SET = "water_set"  # Water level
     WET = "wet"  # Humidification
     WORK_MODE = "work_mode"  # Working mode
+
+
+@dataclass
+class UnitOfMeasurement:
+    """Describes a unit of measurement."""
+
+    unit: str
+    device_classes: set[str]
+
+    aliases: set[str] = field(default_factory=set)
+    conversion_unit: str | None = None
+    conversion_fn: Callable[[float], float] | None = None
+
+
+# A tuple of available units of measurements we can work with.
+# Tuya's devices aren't consistent in UOM use, thus this provides
+# a list of aliases for units and possible conversions we can do
+# to make them compatible with our model.
+UNITS = (
+    UnitOfMeasurement(
+        unit="",
+        aliases={" "},
+        device_classes={
+            DEVICE_CLASS_AQI,
+            DEVICE_CLASS_DATE,
+            DEVICE_CLASS_MONETARY,
+            DEVICE_CLASS_TIMESTAMP,
+        },
+    ),
+    UnitOfMeasurement(
+        unit=PERCENTAGE,
+        aliases={"pct", "percent"},
+        device_classes={
+            DEVICE_CLASS_BATTERY,
+            DEVICE_CLASS_HUMIDITY,
+            DEVICE_CLASS_POWER_FACTOR,
+        },
+    ),
+    UnitOfMeasurement(
+        unit=CONCENTRATION_PARTS_PER_MILLION,
+        device_classes={
+            DEVICE_CLASS_CO,
+            DEVICE_CLASS_CO2,
+        },
+    ),
+    UnitOfMeasurement(
+        unit=CONCENTRATION_PARTS_PER_BILLION,
+        device_classes={
+            DEVICE_CLASS_CO,
+            DEVICE_CLASS_CO2,
+        },
+        conversion_unit=CONCENTRATION_PARTS_PER_MILLION,
+        conversion_fn=lambda x: x / 1000,
+    ),
+    UnitOfMeasurement(
+        unit=ELECTRIC_CURRENT_AMPERE,
+        aliases={"a", "ampere"},
+        device_classes={DEVICE_CLASS_CURRENT},
+    ),
+    UnitOfMeasurement(
+        unit=ELECTRIC_CURRENT_MILLIAMPERE,
+        aliases={"ma", "milliampere"},
+        device_classes={DEVICE_CLASS_CURRENT},
+        conversion_unit=ELECTRIC_CURRENT_AMPERE,
+        conversion_fn=lambda x: x / 1000,
+    ),
+    UnitOfMeasurement(
+        unit=ENERGY_WATT_HOUR,
+        aliases={"wh", "watthour"},
+        device_classes={DEVICE_CLASS_ENERGY},
+    ),
+    UnitOfMeasurement(
+        unit=ENERGY_KILO_WATT_HOUR,
+        aliases={"kwh", "kilowatt-hour"},
+        device_classes={DEVICE_CLASS_ENERGY},
+    ),
+    UnitOfMeasurement(
+        unit=VOLUME_CUBIC_FEET,
+        aliases={"ft3"},
+        device_classes={DEVICE_CLASS_GAS},
+    ),
+    UnitOfMeasurement(
+        unit=VOLUME_CUBIC_METERS,
+        aliases={"m3"},
+        device_classes={DEVICE_CLASS_GAS},
+    ),
+    UnitOfMeasurement(
+        unit=LIGHT_LUX,
+        aliases={"lux"},
+        device_classes={DEVICE_CLASS_ILLUMINANCE},
+    ),
+    UnitOfMeasurement(
+        unit="lm",
+        aliases={"lum", "lumen"},
+        device_classes={DEVICE_CLASS_ILLUMINANCE},
+    ),
+    UnitOfMeasurement(
+        unit=CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+        aliases={"ug/m3", "µg/m3", "ug/m³"},
+        device_classes={
+            DEVICE_CLASS_NITROGEN_DIOXIDE,
+            DEVICE_CLASS_NITROGEN_MONOXIDE,
+            DEVICE_CLASS_NITROUS_OXIDE,
+            DEVICE_CLASS_OZONE,
+            DEVICE_CLASS_PM1,
+            DEVICE_CLASS_PM25,
+            DEVICE_CLASS_PM10,
+            DEVICE_CLASS_SULPHUR_DIOXIDE,
+            DEVICE_CLASS_VOLATILE_ORGANIC_COMPOUNDS,
+        },
+    ),
+    UnitOfMeasurement(
+        unit=CONCENTRATION_MILLIGRAMS_PER_CUBIC_METER,
+        aliases={"mg/m3"},
+        device_classes={
+            DEVICE_CLASS_NITROGEN_DIOXIDE,
+            DEVICE_CLASS_NITROGEN_MONOXIDE,
+            DEVICE_CLASS_NITROUS_OXIDE,
+            DEVICE_CLASS_OZONE,
+            DEVICE_CLASS_PM1,
+            DEVICE_CLASS_PM25,
+            DEVICE_CLASS_PM10,
+            DEVICE_CLASS_SULPHUR_DIOXIDE,
+            DEVICE_CLASS_VOLATILE_ORGANIC_COMPOUNDS,
+        },
+        conversion_unit=CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+        conversion_fn=lambda x: x * 1000,
+    ),
+    UnitOfMeasurement(
+        unit=POWER_WATT,
+        aliases={"watt"},
+        device_classes={DEVICE_CLASS_POWER},
+    ),
+    UnitOfMeasurement(
+        unit=POWER_KILO_WATT,
+        aliases={"kilowatt"},
+        device_classes={DEVICE_CLASS_POWER},
+    ),
+    UnitOfMeasurement(
+        unit=PRESSURE_BAR,
+        device_classes={DEVICE_CLASS_PRESSURE},
+    ),
+    UnitOfMeasurement(
+        unit=PRESSURE_MBAR,
+        aliases={"millibar"},
+        device_classes={DEVICE_CLASS_PRESSURE},
+    ),
+    UnitOfMeasurement(
+        unit=PRESSURE_HPA,
+        aliases={"hpa", "hectopascal"},
+        device_classes={DEVICE_CLASS_PRESSURE},
+    ),
+    UnitOfMeasurement(
+        unit=PRESSURE_INHG,
+        aliases={"inhg"},
+        device_classes={DEVICE_CLASS_PRESSURE},
+    ),
+    UnitOfMeasurement(
+        unit=PRESSURE_PSI,
+        device_classes={DEVICE_CLASS_PRESSURE},
+    ),
+    UnitOfMeasurement(
+        unit=PRESSURE_PA,
+        device_classes={DEVICE_CLASS_PRESSURE},
+    ),
+    UnitOfMeasurement(
+        unit=SIGNAL_STRENGTH_DECIBELS,
+        aliases={"db"},
+        device_classes={DEVICE_CLASS_SIGNAL_STRENGTH},
+    ),
+    UnitOfMeasurement(
+        unit=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+        aliases={"dbm"},
+        device_classes={DEVICE_CLASS_SIGNAL_STRENGTH},
+    ),
+    UnitOfMeasurement(
+        unit=TEMP_CELSIUS,
+        aliases={"°c", "c", "celsius"},
+        device_classes={DEVICE_CLASS_TEMPERATURE},
+    ),
+    UnitOfMeasurement(
+        unit=TEMP_FAHRENHEIT,
+        aliases={"°f", "f", "fahrenheit"},
+        device_classes={DEVICE_CLASS_TEMPERATURE},
+    ),
+    UnitOfMeasurement(
+        unit=ELECTRIC_POTENTIAL_VOLT,
+        aliases={"volt"},
+        device_classes={DEVICE_CLASS_VOLTAGE},
+    ),
+    UnitOfMeasurement(
+        unit=ELECTRIC_POTENTIAL_MILLIVOLT,
+        aliases={"mv", "millivolt"},
+        device_classes={DEVICE_CLASS_VOLTAGE},
+        conversion_unit=ELECTRIC_POTENTIAL_VOLT,
+        conversion_fn=lambda x: x / 1000,
+    ),
+)
+
+
+DEVICE_CLASS_UNITS: dict[str, dict[str, UnitOfMeasurement]] = {}
+for uom in UNITS:
+    for device_class in uom.device_classes:
+        DEVICE_CLASS_UNITS.setdefault(device_class, {})[uom.unit] = uom
+        for unit_alias in uom.aliases:
+            DEVICE_CLASS_UNITS[device_class][unit_alias] = uom
 
 
 @dataclass
