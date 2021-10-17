b22dbe16198950c81a9f7d3ea3aaab927430d9d3
Guido Schmitz <guido.schmitz@fedaix.de>
2021-10-17 14:16:06 +0000

    Apply changes to init

--- homeassistant/components/devolo_home_network/__init__.py
+++ homeassistant/components/devolo_home_network/__init__.py
@@ -1,6 +1,4 @@
 """The devolo Home Network integration."""
-import logging
-
 from devolo_plc_api.device import Device
 from devolo_plc_api.exceptions.device import DeviceNotFound
 
@@ -12,8 +10,6 @@ from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.httpx_client import get_async_client
 
 from .const import DOMAIN, PLATFORMS
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -28,17 +24,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         )
         await device.async_connect(session_instance=async_client)
     except DeviceNotFound as error:
-        _LOGGER.warning("Unable to connect to %s", entry.data[CONF_IP_ADDRESS])
-        raise ConfigEntryNotReady from error
+        raise ConfigEntryNotReady(
+            f"Unable to connect to {entry.data[CONF_IP_ADDRESS]}"
+        ) from error
 
     hass.data[DOMAIN][entry.entry_id] = device
 
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     async def disconnect(event: Event) -> None:
-        await hass.data[DOMAIN][entry.entry_id].async_disconnect()
+        await device.async_disconnect()
 
-    # Listen when EVENT_HOMEASSISTANT_STOP is fired
     entry.async_on_unload(
         hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, disconnect)
     )
