a909e3ad4652bd92d2101148d458f28fde009186
epenet <6771947+epenet@users.noreply.github.com>
2021-10-18 19:16:53 +0200

    Use pytest fixtures on Onewire tests (#57973)
    * Add pytest fixtures
    
    * Add sysbus fixtures
    
    * Adjust parameter name
    
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- tests/components/onewire/test_binary_sensor.py
+++ tests/components/onewire/test_binary_sensor.py
@@ -1,51 +1,52 @@
 """Tests for 1-Wire devices connected on OWServer."""
 import copy
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 
 from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAIN
 from homeassistant.components.onewire.binary_sensor import DEVICE_BINARY_SENSORS
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
-from . import setup_onewire_patched_owserver_integration, setup_owproxy_mock_devices
+from . import setup_owproxy_mock_devices
 from .const import MOCK_OWPROXY_DEVICES
 
 from tests.common import mock_registry
 
-MOCK_BINARY_SENSORS = {
-    key: value
-    for (key, value) in MOCK_OWPROXY_DEVICES.items()
-    if BINARY_SENSOR_DOMAIN in value
-}
+
+@pytest.fixture(autouse=True)
+def override_platforms():
+    """Override PLATFORMS."""
+    with patch("homeassistant.components.onewire.PLATFORMS", [BINARY_SENSOR_DOMAIN]):
+        yield
 
 
-@pytest.mark.parametrize("device_id", MOCK_BINARY_SENSORS.keys())
-@patch("homeassistant.components.onewire.onewirehub.protocol.proxy")
-async def test_owserver_binary_sensor(owproxy, hass, device_id):
+async def test_owserver_binary_sensor(
+    hass: HomeAssistant, config_entry: ConfigEntry, owproxy: MagicMock, device_id: str
+):
     """Test for 1-Wire binary sensor.
 
     This test forces all entities to be enabled.
     """
-
     entity_registry = mock_registry(hass)
 
     setup_owproxy_mock_devices(owproxy, BINARY_SENSOR_DOMAIN, [device_id])
 
-    mock_device = MOCK_BINARY_SENSORS[device_id]
-    expected_entities = mock_device[BINARY_SENSOR_DOMAIN]
+    mock_device = MOCK_OWPROXY_DEVICES[device_id]
+    expected_entities = mock_device.get(BINARY_SENSOR_DOMAIN, [])
 
     # Force enable binary sensors
     patch_device_binary_sensors = copy.deepcopy(DEVICE_BINARY_SENSORS)
-    for item in patch_device_binary_sensors[device_id[0:2]]:
-        item.entity_registry_enabled_default = True
+    if device_binary_sensor := patch_device_binary_sensors.get(device_id[0:2]):
+        for item in device_binary_sensor:
+            item.entity_registry_enabled_default = True
 
-    with patch(
-        "homeassistant.components.onewire.PLATFORMS", [BINARY_SENSOR_DOMAIN]
-    ), patch.dict(
+    with patch.dict(
         "homeassistant.components.onewire.binary_sensor.DEVICE_BINARY_SENSORS",
         patch_device_binary_sensors,
     ):
-        await setup_onewire_patched_owserver_integration(hass)
+        await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
 
     assert len(entity_registry.entities) == len(expected_entities)
