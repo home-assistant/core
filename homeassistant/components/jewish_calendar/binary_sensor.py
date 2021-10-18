eba2e4cdc38715ce5d0064aabccfd078e4f69ad6
Chaim Turkel <cyturel@gmail.com>
2021-10-18 23:11:48 +0300

    Update homeassistant/components/jewish_calendar/binary_sensor.py
    Co-authored-by: Yuval Aboulafia <yuval.abou@gmail.com>

--- homeassistant/components/jewish_calendar/binary_sensor.py
+++ homeassistant/components/jewish_calendar/binary_sensor.py
@@ -9,9 +9,6 @@ from homeassistant.components.binary_sensor import (
     BinarySensorEntity,
     BinarySensorEntityDescription,
 )
-from homeassistant.components.jewish_calendar.erev_shabbt_hag_binary_sensor import (
-    ErevShabbtHagBinarySensor,
-)
 from homeassistant.components.jewish_calendar.motzei_shabbt_hag_binary_sensor import (
     MotzeiShabbtHagBinarySensor,
 )
