1e279c1f5b28228ce78d89a105b3b0aaa5a3a467
Robert Svensson <Kane610@users.noreply.github.com>
2021-10-20 14:59:36 +0200

    Simplify signalling for updating available property of deCONZ entities (#58078)

--- homeassistant/components/deconz/deconz_event.py
+++ homeassistant/components/deconz/deconz_event.py
@@ -110,7 +110,7 @@ class DeconzEvent(DeconzBase):
         self._device.remove_callback(self.async_update_callback)
 
     @callback
-    def async_update_callback(self, force_update=False):
+    def async_update_callback(self):
         """Fire the event if reason is that state is updated."""
         if (
             self.gateway.ignore_state_updates
@@ -155,7 +155,7 @@ class DeconzAlarmEvent(DeconzEvent):
     """Alarm control panel companion event when user interacts with a keypad."""
 
     @callback
-    def async_update_callback(self, force_update=False):
+    def async_update_callback(self):
         """Fire the event if reason is new action is updated."""
         if (
             self.gateway.ignore_state_updates
