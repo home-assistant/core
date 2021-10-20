92abc2c89863e7638356839d11282642e09dc656
J. Nick Koston <nick@koston.org>
2021-10-19 16:08:01 -1000

    test coverage

--- tests/components/group/test_config_flow.py
+++ tests/components/group/test_config_flow.py
@@ -1,89 +1,100 @@
 """Test the Group config flow."""
 from unittest.mock import patch
 
+import pytest
+
 from homeassistant import config_entries
-from homeassistant.components.group.config_flow import CannotConnect, InvalidAuth
+from homeassistant.components.group.config_flow import SUPPORTED_DOMAINS
 from homeassistant.components.group.const import DOMAIN
+from homeassistant.const import CONF_DOMAIN, CONF_ENTITIES, STATE_ON
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import RESULT_TYPE_CREATE_ENTRY, RESULT_TYPE_FORM
+from homeassistant.loader import async_get_integration
+
+from tests.common import MockConfigEntry
 
 
-async def test_form(hass: HomeAssistant) -> None:
-    """Test we get the form."""
+@pytest.mark.parametrize("domain", SUPPORTED_DOMAINS)
+async def test_form(hass: HomeAssistant, domain: str) -> None:
+    """Test we can create a new group from the UI."""
+
+    hass.states.async_set(f"{domain}.dummy", STATE_ON)
+    hass.states.async_set(f"{domain}.dummy2", STATE_ON)
+
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     assert result["type"] == RESULT_TYPE_FORM
     assert result["errors"] is None
 
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_DOMAIN: domain,
+        },
+    )
+    await hass.async_block_till_done()
+
     with patch(
-        "homeassistant.components.group.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
-    ), patch(
         "homeassistant.components.group.async_setup_entry",
         return_value=True,
     ) as mock_setup_entry:
-        result2 = await hass.config_entries.flow.async_configure(
+        result3 = await hass.config_entries.flow.async_configure(
+            result2["flow_id"],
+            {
+                CONF_ENTITIES: [f"{domain}.dummy", f"{domain}.dummy2"],
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result3["type"] == RESULT_TYPE_CREATE_ENTRY
+    integration = await async_get_integration(hass, domain)
+    assert result3["title"] == f"{integration.name} Group"
+    assert result3["data"] == {CONF_DOMAIN: domain}
+    assert result3["options"] == {
+        CONF_ENTITIES: [f"{domain}.dummy", f"{domain}.dummy2"],
+    }
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+@pytest.mark.parametrize("domain", SUPPORTED_DOMAINS)
+async def test_options(hass: HomeAssistant, domain: str) -> None:
+    """Test we can edit an existing group from the UI."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_DOMAIN: domain},
+        options={CONF_ENTITIES: [f"{domain}.dummy", f"{domain}.dummy2"]},
+    )
+    entry.add_to_hass(hass)
+
+    hass.states.async_set(f"{domain}.dummy", STATE_ON)
+    hass.states.async_set(f"{domain}.dummy2", STATE_ON)
+    hass.states.async_set(f"{domain}.dummy3", STATE_ON)
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] is None
+
+    with patch(
+        "homeassistant.components.group.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.options.async_configure(
             result["flow_id"],
             {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
+                CONF_ENTITIES: [
+                    f"{domain}.dummy",
+                    f"{domain}.dummy2",
+                    f"{domain}.dummy3",
+                ],
             },
         )
         await hass.async_block_till_done()
 
     assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
-    assert result2["title"] == "Name of the device"
-    assert result2["data"] == {
-        "host": "1.1.1.1",
-        "username": "test-username",
-        "password": "test-password",
+    assert entry.options == {
+        CONF_ENTITIES: [f"{domain}.dummy", f"{domain}.dummy2", f"{domain}.dummy3"]
     }
     assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_invalid_auth(hass: HomeAssistant) -> None:
-    """Test we handle invalid auth."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.group.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == RESULT_TYPE_FORM
-    assert result2["errors"] == {"base": "invalid_auth"}
-
-
-async def test_form_cannot_connect(hass: HomeAssistant) -> None:
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.group.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == RESULT_TYPE_FORM
-    assert result2["errors"] == {"base": "cannot_connect"}
