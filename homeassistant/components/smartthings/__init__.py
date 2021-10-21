1bcf39517a0b9581b017526bbccaa066bacca914
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-10-21 08:27:42 +0200

    Use assignment expressions 13 (#57938)

--- homeassistant/components/smartthings/__init__.py
+++ homeassistant/components/smartthings/__init__.py
@@ -367,8 +367,7 @@ class DeviceBroker:
         for evt in req.events:
             if evt.event_type != EVENT_TYPE_DEVICE:
                 continue
-            device = self.devices.get(evt.device_id)
-            if not device:
+            if not (device := self.devices.get(evt.device_id)):
                 continue
             device.status.apply_attribute_update(
                 evt.component_id,
