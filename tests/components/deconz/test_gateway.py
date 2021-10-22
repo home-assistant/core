25f2d6a1d7fed10982e1a803b142f4844c9d9c68
Robert Svensson <Kane610@users.noreply.github.com>
2021-10-22 19:41:49 +0200

    Add support for device configuration URL to deCONZ gateway (#58184)

--- tests/components/deconz/test_gateway.py
+++ tests/components/deconz/test_gateway.py
@@ -34,7 +34,7 @@ from homeassistant.components.ssdp import (
     ATTR_UPNP_UDN,
 )
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
-from homeassistant.config_entries import SOURCE_SSDP, SOURCE_USER
+from homeassistant.config_entries import SOURCE_HASSIO, SOURCE_SSDP, SOURCE_USER
 from homeassistant.const import (
     CONF_API_KEY,
     CONF_HOST,
@@ -43,6 +43,7 @@ from homeassistant.const import (
     STATE_OFF,
     STATE_UNAVAILABLE,
 )
+from homeassistant.helpers import device_registry as dr
 
 from tests.common import MockConfigEntry
 
@@ -168,6 +169,33 @@ async def test_gateway_setup(hass, aioclient_mock):
         assert forward_entry_setup.mock_calls[9][1] == (config_entry, SENSOR_DOMAIN)
         assert forward_entry_setup.mock_calls[10][1] == (config_entry, SIREN_DOMAIN)
         assert forward_entry_setup.mock_calls[11][1] == (config_entry, SWITCH_DOMAIN)
+
+    device_registry = dr.async_get(hass)
+    gateway_entry = device_registry.async_get_device(
+        identifiers={(DECONZ_DOMAIN, gateway.bridgeid)}
+    )
+
+    assert gateway_entry.configuration_url == f"http://{HOST}:{PORT}"
+    assert gateway_entry.entry_type == "service"
+
+
+async def test_gateway_device_no_configuration_url_when_addon(hass, aioclient_mock):
+    """Successful setup."""
+    with patch(
+        "homeassistant.config_entries.ConfigEntries.async_forward_entry_setup",
+        return_value=True,
+    ):
+        config_entry = await setup_deconz_integration(
+            hass, aioclient_mock, source=SOURCE_HASSIO
+        )
+        gateway = get_gateway_from_config_entry(hass, config_entry)
+
+    device_registry = dr.async_get(hass)
+    gateway_entry = device_registry.async_get_device(
+        identifiers={(DECONZ_DOMAIN, gateway.bridgeid)}
+    )
+
+    assert not gateway_entry.configuration_url
 
 
 async def test_gateway_retry(hass):
