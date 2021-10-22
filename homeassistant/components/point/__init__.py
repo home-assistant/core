176ed4f7ba562ab25734b847d35039afda48a6a9
epenet <6771947+epenet@users.noreply.github.com>
2021-10-22 12:31:12 +0200

    Use DeviceInfo on components with via_device (I-T) (#58212)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/point/__init__.py
+++ homeassistant/components/point/__init__.py
@@ -21,7 +21,7 @@ from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
 )
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import DeviceInfo, Entity
 from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.util.dt import as_local, parse_datetime, utc_from_timestamp
 
@@ -308,20 +308,20 @@ class MinutPointEntity(Entity):
         return attrs
 
     @property
-    def device_info(self):
+    def device_info(self) -> DeviceInfo:
         """Return a device description for device registry."""
         device = self.device.device
-        return {
-            "connections": {
+        return DeviceInfo(
+            connections={
                 (device_registry.CONNECTION_NETWORK_MAC, device["device_mac"])
             },
-            "identifieres": device["device_id"],
-            "manufacturer": "Minut",
-            "model": f"Point v{device['hardware_version']}",
-            "name": device["description"],
-            "sw_version": device["firmware"]["installed"],
-            "via_device": (DOMAIN, device["home"]),
-        }
+            identifieres=device["device_id"],
+            manufacturer="Minut",
+            model=f"Point v{device['hardware_version']}",
+            name=device["description"],
+            sw_version=device["firmware"]["installed"],
+            via_device=(DOMAIN, device["home"]),
+        )
 
     @property
     def name(self):
