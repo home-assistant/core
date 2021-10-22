a298d6e5439b054e14a4751094bd0249d0726e5d
J. Nick Koston <nick@koston.org>
2021-10-21 15:35:19 -1000

    Alternate solution

--- homeassistant/components/lookin/entity.py
+++ homeassistant/components/lookin/entity.py
@@ -25,31 +25,71 @@ def _lookin_device_to_device_info(lookin_device: Device) -> DeviceInfo:
     )
 
 
-class LookinDeviceEntity(Entity):
+def _lookin_enttiy_device_to_device_info(
+    lookin_device: Device, uuid: str, device: Device
+) -> DeviceInfo:
+    return DeviceInfo(
+        identifiers={(DOMAIN, uuid)},
+        name=device.name,
+        model=device.device_type,
+        via_device=(DOMAIN, lookin_device.id),
+    )
+
+
+class LookinDeviceMixIn:
+    """A mix in to set lookin attributes for the lookin device."""
+
+    def _set_lookin_device_attrs(self, lookin_data: LookinData) -> None:
+        """Set attrs for the lookin device."""
+        self._lookin_device = lookin_data.lookin_device
+        self._lookin_protocol = lookin_data.lookin_protocol
+        self._lookin_udp_subs = lookin_data.lookin_udp_subs
+
+
+class LookinDeviceEntity(LookinDeviceMixIn, Entity):
     """A lookin device entity on the device itself."""
 
     _attr_should_poll = False
 
     def __init__(self, lookin_data: LookinData) -> None:
         """Init the lookin device entity."""
-        self._lookin_device = lookin_data.lookin_device
-        self._lookin_protocol = lookin_data.lookin_protocol
-        self._lookin_udp_subs = lookin_data.lookin_udp_subs
+        self._set_lookin_device_attrs(lookin_data)
         self._attr_device_info = _lookin_device_to_device_info(
             lookin_data.lookin_device
         )
 
 
-class LookinDeviceCoordinatorEntity(LookinDeviceEntity, CoordinatorEntity):
+class LookinDeviceCoordinatorEntity(LookinDeviceMixIn, CoordinatorEntity):
     """A lookin device entity on the device itself that uses the coordinator."""
+
+    _attr_should_poll = False
 
     def __init__(self, lookin_data: LookinData) -> None:
         """Init the lookin device entity."""
-        super().__init__(lookin_data)
-        CoordinatorEntity.__init__(self, lookin_data.meteo_coordinator)
+        super().__init__(lookin_data.meteo_coordinator)
+        self._set_lookin_device_attrs(lookin_data)
+        self._attr_device_info = _lookin_device_to_device_info(
+            lookin_data.lookin_device
+        )
 
 
-class LookinEntity(Entity):
+class LookinEntityMixIn:
+    """A mix in to set attributes for a lookin entity."""
+
+    def _set_lookin_entity_attrs(
+        self,
+        uuid: str,
+        device: Remote | Climate,
+        lookin_data: LookinData,
+    ) -> None:
+        """Set attrs for the device controlled via the lookin device."""
+        self._device = device
+        self._uuid = uuid
+        self._meteo_coordinator = lookin_data.meteo_coordinator
+        self._function_names = {function.name for function in self._device.functions}
+
+
+class LookinEntity(LookinDeviceMixIn, LookinEntityMixIn, Entity):
     """A base class for lookin entities."""
 
     _attr_should_poll = False
@@ -62,21 +102,13 @@ class LookinEntity(Entity):
         lookin_data: LookinData,
     ) -> None:
         """Init the base entity."""
-        self._device = device
-        self._uuid = uuid
-        self._lookin_device = lookin_data.lookin_device
-        self._lookin_protocol = lookin_data.lookin_protocol
-        self._lookin_udp_subs = lookin_data.lookin_udp_subs
-        self._meteo_coordinator = lookin_data.meteo_coordinator
-        self._function_names = {function.name for function in self._device.functions}
-        self._attr_unique_id = uuid
-        self._attr_name = self._device.name
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, self._uuid)},
-            name=self._device.name,
-            model=self._device.device_type,
-            via_device=(DOMAIN, self._lookin_device.id),
+        self._set_lookin_device_attrs(lookin_data)
+        self._set_lookin_entity_attrs(uuid, device, lookin_data)
+        self._attr_device_info = _lookin_enttiy_device_to_device_info(
+            self._lookin_device, uuid, device
         )
+        self._attr_unique_id = uuid
+        self._attr_name = device.name
 
     async def _async_send_command(self, command: str) -> None:
         """Send command from saved IR device."""
@@ -85,8 +117,11 @@ class LookinEntity(Entity):
         )
 
 
-class LookinCoordinatorEntity(LookinEntity, CoordinatorEntity):
+class LookinCoordinatorEntity(LookinDeviceMixIn, LookinEntityMixIn, CoordinatorEntity):
     """A lookin device entity for an external device that uses the coordinator."""
+
+    _attr_should_poll = False
+    _attr_assumed_state = True
 
     def __init__(
         self,
@@ -95,9 +130,15 @@ class LookinCoordinatorEntity(LookinEntity, CoordinatorEntity):
         device: Remote | Climate,
         lookin_data: LookinData,
     ) -> None:
-        """Init the lookin device entity."""
-        super().__init__(uuid, device, lookin_data)
-        CoordinatorEntity.__init__(self, coordinator)
+        """Init the base entity."""
+        super().__init__(coordinator)
+        self._set_lookin_device_attrs(lookin_data)
+        self._set_lookin_entity_attrs(uuid, device, lookin_data)
+        self._attr_device_info = _lookin_enttiy_device_to_device_info(
+            self._lookin_device, uuid, device
+        )
+        self._attr_unique_id = uuid
+        self._attr_name = device.name
 
 
 class LookinPowerEntity(LookinEntity):
