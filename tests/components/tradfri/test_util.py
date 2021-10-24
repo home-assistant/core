4d37b5621393cb2b6540bb04b9f1a6bb2fb9a795
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2021-10-24 08:36:38 +0000

    Add tests

---
+++ tests/components/tradfri/test_util.py
@@ -0,0 +1,13 @@
+"""Tradfri utility function tests."""
+
+from homeassistant.components.tradfri.fan import _from_fan_speed, _from_percentage
+
+
+def test_from_fan_speed():
+    """Test that we can convert fan speed to percentage value."""
+    assert _from_fan_speed(40) == 80
+
+
+def test_from_percentage():
+    """Test that we can convert percentage to fan speed."""
+    assert _from_percentage(80) == 40
