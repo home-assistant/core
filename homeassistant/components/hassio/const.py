971096dba16f54d4a4bebdfd6a257c98c9d8d873
Joakim Sørensen <joasoe@gmail.com>
2021-10-26 00:21:44 +0200

    Add binary sensor to add-ons to show if they are running (#58120)

--- homeassistant/components/hassio/const.py
+++ homeassistant/components/hassio/const.py
@@ -45,6 +45,7 @@ ATTR_UPDATE_AVAILABLE = "update_available"
 ATTR_CPU_PERCENT = "cpu_percent"
 ATTR_MEMORY_PERCENT = "memory_percent"
 ATTR_SLUG = "slug"
+ATTR_STATE = "state"
 ATTR_URL = "url"
 ATTR_REPOSITORY = "repository"
 
