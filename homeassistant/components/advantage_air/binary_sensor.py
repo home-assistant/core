e529a5643064d2bda4b172144c589a37e7ce3857
Brett Adams <Bre77@users.noreply.github.com>
2021-10-25 20:45:09 +1000

    Add entity category to Advantage Air (#58371)

--- homeassistant/components/advantage_air/binary_sensor.py
+++ homeassistant/components/advantage_air/binary_sensor.py
@@ -5,6 +5,7 @@ from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_PROBLEM,
     BinarySensorEntity,
 )
+from homeassistant.const import ENTITY_CATEGORY_CONFIG, ENTITY_CATEGORY_DIAGNOSTIC
 
 from .const import DOMAIN as ADVANTAGE_AIR_DOMAIN
 from .entity import AdvantageAirEntity
@@ -34,6 +35,7 @@ class AdvantageAirZoneFilter(AdvantageAirEntity, BinarySensorEntity):
     """Advantage Air Filter."""
 
     _attr_device_class = DEVICE_CLASS_PROBLEM
+    _attr_entity_category = ENTITY_CATEGORY_DIAGNOSTIC
 
     def __init__(self, instance, ac_key):
         """Initialize an Advantage Air Filter."""
@@ -72,6 +74,7 @@ class AdvantageAirZoneMyZone(AdvantageAirEntity, BinarySensorEntity):
     """Advantage Air Zone MyZone."""
 
     _attr_entity_registry_enabled_default = False
+    _attr_entity_category = ENTITY_CATEGORY_CONFIG
 
     def __init__(self, instance, ac_key, zone_key):
         """Initialize an Advantage Air Zone MyZone."""
