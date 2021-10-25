76486ca1fbd8f138121fc4b1d8dd45f216fccba7
Paulus Schoutsen <balloob@gmail.com>
2021-10-24 20:57:17 -0700

    Typo

--- homeassistant/components/myq/binary_sensor.py
+++ homeassistant/components/myq/binary_sensor.py
@@ -1,5 +1,5 @@
 """Support for MyQ gateways."""
-from homeassistant.const import ENTITY_CATEGORY_DIAGNOSTICS
+from homeassistant.const import ENTITY_CATEGORY_DIAGNOSTIC
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_CONNECTIVITY,
     BinarySensorEntity,
@@ -27,7 +27,7 @@ class MyQBinarySensorEntity(MyQEntity, BinarySensorEntity):
     """Representation of a MyQ gateway."""
 
     _attr_device_class = DEVICE_CLASS_CONNECTIVITY
-    _attr_entity_category = ENTITY_CATEGORY_DIAGNOSTICS
+    _attr_entity_category = ENTITY_CATEGORY_DIAGNOSTIC
 
     @property
     def name(self):
