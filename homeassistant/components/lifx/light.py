home-assistant/core

commit 767df98d8ced4a43ac0348f3a6a5e80dd57713d8

Use DeviceInfo Class L-M (#58312)

homeassistant/components/lifx/light.py
@@ -34,11 +34,18 @@ from homeassistant.components.light import (
     LightEntity,
     preprocess_turn_on_alternatives,
 )
-from homeassistant.const import ATTR_ENTITY_ID, ATTR_MODE, EVENT_HOMEASSISTANT_STOP
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    ATTR_MODE,
+    ATTR_MODEL,
+    ATTR_SW_VERSION,
+    EVENT_HOMEASSISTANT_STOP,
+)
 from homeassistant.core import callback
 from homeassistant.helpers import entity_platform
 import homeassistant.helpers.config_validation as cv
 import homeassistant.helpers.device_registry as dr
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.event import async_track_point_in_utc_time
 import homeassistant.util.color as color_util
 
@@ -449,24 +456,19 @@ class LIFXLight(LightEntity):
         self.lock = asyncio.Lock()
 
     @property
-    def device_info(self):
+    def device_info(self) -> DeviceInfo:
         """Return information about the device."""
-        info = {
-            "identifiers": {(LIFX_DOMAIN, self.unique_id)},
-            "name": self.name,
-            "connections": {(dr.CONNECTION_NETWORK_MAC, self.bulb.mac_addr)},
-            "manufacturer": "LIFX",
-        }
-
+        _map = aiolifx().products.product_map
+        info = DeviceInfo(
+            identifiers={(LIFX_DOMAIN, self.unique_id)},
+            connections={(dr.CONNECTION_NETWORK_MAC, self.bulb.mac_addr)},
+            manufacturer="LIFX",
+            name=self.name,
+        )
+        if model := (_map.get(self.bulb.product) or self.bulb.product) is not None:
+            info[ATTR_MODEL] = str(model)
         if (version := self.bulb.host_firmware_version) is not None:
-            info["sw_version"] = version
-
-        product_map = aiolifx().products.product_map
-
-        model = product_map.get(self.bulb.product) or self.bulb.product
-        if model is not None:
-            info["model"] = str(model)
-
+            info[ATTR_SW_VERSION] = version
         return info
 
     @property

homeassistant/components/litterrobot/entity.py
@@ -46,12 +46,12 @@ class LitterRobotEntity(CoordinatorEntity):
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device information for a Litter-Robot."""
-        return {
-            "identifiers": {(DOMAIN, self.robot.serial)},
-            "name": self.robot.name,
-            "manufacturer": "Litter-Robot",
-            "model": self.robot.model,
-        }
+        return DeviceInfo(
+            identifiers={(DOMAIN, self.robot.serial)},
+            manufacturer="Litter-Robot",
+            model=self.robot.model,
+            name=self.robot.name,
+        )
 
 
 class LitterRobotControlEntity(LitterRobotEntity):

homeassistant/components/logi_circle/camera.py
@@ -14,6 +14,7 @@ from homeassistant.const import (
     STATE_ON,
 )
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.entity import DeviceInfo
 
 from .const import (
     ATTRIBUTION,
@@ -116,15 +117,15 @@ class LogiCam(Camera):
         return SUPPORT_ON_OFF
 
     @property
-    def device_info(self):
+    def device_info(self) -> DeviceInfo:
         """Return information about the device."""
-        return {
-            "name": self._camera.name,
-            "identifiers": {(LOGI_CIRCLE_DOMAIN, self._camera.id)},
-            "model": self._camera.model_name,
-            "sw_version": self._camera.firmware,
-            "manufacturer": DEVICE_BRAND,
-        }
+        return DeviceInfo(
+            identifiers={(LOGI_CIRCLE_DOMAIN, self._camera.id)},
+            manufacturer=DEVICE_BRAND,
+            model=self._camera.model_name,
+            name=self._camera.name,
+            sw_version=self._camera.firmware,
+        )
 
     @property
     def extra_state_attributes(self):

homeassistant/components/logi_circle/sensor.py
@@ -13,6 +13,7 @@ from homeassistant.const import (
     STATE_OFF,
     STATE_ON,
 )
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.icon import icon_for_battery_level
 from homeassistant.util.dt import as_local
 
@@ -56,15 +57,15 @@ class LogiSensor(SensorEntity):
         self._tz = time_zone
 
     @property
-    def device_info(self):
+    def device_info(self) -> DeviceInfo:
         """Return information about the device."""
-        return {
-            "name": self._camera.name,
-            "identifiers": {(LOGI_CIRCLE_DOMAIN, self._camera.id)},
-            "model": self._camera.model_name,
-            "sw_version": self._camera.firmware,
-            "manufacturer": DEVICE_BRAND,
-        }
+        return DeviceInfo(
+            identifiers={(LOGI_CIRCLE_DOMAIN, self._camera.id)},
+            manufacturer=DEVICE_BRAND,
+            model=self._camera.model_name,
+            name=self._camera.name,
+            sw_version=self._camera.firmware,
+        )
 
     @property
     def extra_state_attributes(self):

homeassistant/components/lutron_caseta/__init__.py
@@ -333,10 +333,10 @@ class LutronCasetaDevice(Entity):
         """Return the device info."""
         return DeviceInfo(
             identifiers={(DOMAIN, self.serial)},
-            name=self.name,
-            suggested_area=self._device["name"].split("_")[0],
             manufacturer=MANUFACTURER,
             model=f"{self._device['model']} ({self._device['type']})",
+            name=self.name,
+            suggested_area=self._device["name"].split("_")[0],
             via_device=(DOMAIN, self._bridge_device["serial"]),
         )
 

homeassistant/components/lyric/__init__.py
@@ -170,9 +170,9 @@ class LyricDeviceEntity(LyricEntity):
     @property
     def device_info(self) -> DeviceInfo:
         """Return device information about this Honeywell Lyric instance."""
-        return {
-            "connections": {(dr.CONNECTION_NETWORK_MAC, self._mac_id)},
-            "manufacturer": "Honeywell",
-            "model": self.device.deviceModel,
-            "name": self.device.name,
-        }
+        return DeviceInfo(
+            connections={(dr.CONNECTION_NETWORK_MAC, self._mac_id)},
+            manufacturer="Honeywell",
+            model=self.device.deviceModel,
+            name=self.device.name,
+        )

homeassistant/components/mazda/__init__.py
@@ -23,6 +23,7 @@ from homeassistant.exceptions import (
 )
 from homeassistant.helpers import aiohttp_client, device_registry
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
@@ -229,14 +230,14 @@ class MazdaEntity(CoordinatorEntity):
         return self.coordinator.data[self.index]
 
     @property
-    def device_info(self):
+    def device_info(self) -> DeviceInfo:
         """Return device info for the Mazda entity."""
-        return {
-            "identifiers": {(DOMAIN, self.vin)},
-            "name": self.get_vehicle_name(),
-            "manufacturer": "Mazda",
-            "model": f"{self.data['modelYear']} {self.data['carlineName']}",
-        }
+        return DeviceInfo(
+            identifiers={(DOMAIN, self.vin)},
+            manufacturer="Mazda",
+            model=f"{self.data['modelYear']} {self.data['carlineName']}",
+            name=self.get_vehicle_name(),
+        )
 
     def get_vehicle_name(self):
         """Return the vehicle name, to be used as a prefix for names of other entities."""

homeassistant/components/melcloud/__init__.py
@@ -17,6 +17,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.util import Throttle
 
@@ -126,19 +127,19 @@ class MelCloudDevice:
         return self.device.building_id
 
     @property
-    def device_info(self):
+    def device_info(self) -> DeviceInfo:
         """Return a device description for device registry."""
-        _device_info = {
-            "connections": {(CONNECTION_NETWORK_MAC, self.device.mac)},
-            "identifiers": {(DOMAIN, f"{self.device.mac}-{self.device.serial}")},
-            "manufacturer": "Mitsubishi Electric",
-            "name": self.name,
-        }
-        if (unit_infos := self.device.units) is not None:
-            _device_info["model"] = ", ".join(
-                [x["model"] for x in unit_infos if x["model"]]
-            )
-        return _device_info
+        model = None
+        unit_infos = self.device.units
+        if unit_infos is not None:
+            model = ", ".join([x["model"] for x in unit_infos if x["model"]])
+        return DeviceInfo(
+            connections={(CONNECTION_NETWORK_MAC, self.device.mac)},
+            identifiers={(DOMAIN, f"{self.device.mac}-{self.device.serial}")},
+            manufacturer="Mitsubishi Electric",
+            model=model,
+            name=self.name,
+        )
 
 
 async def mel_devices_setup(hass, token) -> list[MelCloudDevice]:

commit 5cf41f82416f3b65925b035d932efe40db02671a

Fix LIFX component returning the wrong mac address for connection
Fixes 57457.

Also resolves a bug introduced in #58312 which made all LIFX
bulbs get the model "True"

Signed-off-by: Avi Miller <me@dje.li>

homeassistant/components/lifx/light.py
@@ -7,6 +7,7 @@ import math
 
 import aiolifx as aiolifx_module
 import aiolifx_effects as aiolifx_effects_module
+from getmac import get_mac_address
 import voluptuous as vol
 
 from homeassistant import util
@@ -397,6 +398,13 @@ class LIFXManager:
                     entity = LIFXWhite(bulb, self.effects_conductor)
 
                 _LOGGER.debug("%s register READY", entity.who)
+                if bulb.mac_addr != entity.lifx_mac_addr:
+                    _LOGGER.debug(
+                        "%s reports mac_addr: %s but uses %s",
+                        entity.who,
+                        bulb.mac_addr,
+                        entity.lifx_mac_addr,
+                    )
                 self.entities[bulb.mac_addr] = entity
                 self.async_add_entities([entity], True)
 
@@ -450,6 +458,7 @@ class LIFXLight(LightEntity):
     def __init__(self, bulb, effects_conductor):
         """Initialize the light."""
         self.bulb = bulb
+        self.lifx_mac_addr = dr.format_mac(get_mac_address(ip=self.bulb.ip_addr))
         self.effects_conductor = effects_conductor
         self.registered = True
         self.postponed_update = None
@@ -459,13 +468,14 @@ class LIFXLight(LightEntity):
     def device_info(self) -> DeviceInfo:
         """Return information about the device."""
         _map = aiolifx().products.product_map
+
         info = DeviceInfo(
             identifiers={(LIFX_DOMAIN, self.unique_id)},
-            connections={(dr.CONNECTION_NETWORK_MAC, self.bulb.mac_addr)},
+            connections={(dr.CONNECTION_NETWORK_MAC, self.lifx_mac_addr)},
             manufacturer="LIFX",
             name=self.name,
         )
-        if model := (_map.get(self.bulb.product) or self.bulb.product) is not None:
+        if (model := (_map.get(self.bulb.product) or self.bulb.product)) is not None:
             info[ATTR_MODEL] = str(model)
         if (version := self.bulb.host_firmware_version) is not None:
             info[ATTR_SW_VERSION] = version