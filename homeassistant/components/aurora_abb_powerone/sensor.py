e22aaea5b2bcd8766535cdda6c61ea98298ee722
Dave T <17680170+davet2001@users.noreply.github.com>
2021-10-26 02:04:42 +0100

    Aurora abb (solar) configflow (#36300)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/aurora_abb_powerone/sensor.py
+++ homeassistant/components/aurora_abb_powerone/sensor.py
@@ -10,54 +10,85 @@ from homeassistant.components.sensor import (
     STATE_CLASS_MEASUREMENT,
     SensorEntity,
 )
+from homeassistant.config_entries import SOURCE_IMPORT
 from homeassistant.const import (
     CONF_ADDRESS,
     CONF_DEVICE,
     CONF_NAME,
     DEVICE_CLASS_POWER,
+    DEVICE_CLASS_TEMPERATURE,
     POWER_WATT,
+    TEMP_CELSIUS,
 )
+from homeassistant.exceptions import InvalidStateError
 import homeassistant.helpers.config_validation as cv
+
+from .aurora_device import AuroraDevice
+from .const import DEFAULT_ADDRESS, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_ADDRESS = 2
-DEFAULT_NAME = "Solar PV"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_DEVICE): cv.string,
         vol.Optional(CONF_ADDRESS, default=DEFAULT_ADDRESS): cv.positive_int,
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_NAME, default="Solar PV"): cv.string,
     }
 )
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Aurora ABB PowerOne device."""
-    devices = []
-    comport = config[CONF_DEVICE]
-    address = config[CONF_ADDRESS]
-    name = config[CONF_NAME]
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up based on configuration.yaml (DEPRECATED)."""
+    _LOGGER.warning(
+        "Loading aurora_abb_powerone via platform config is deprecated; The configuration"
+        " has been migrated to a config entry and can be safely removed from configuration.yaml"
+    )
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=config
+        )
+    )
 
-    _LOGGER.debug("Intitialising com port=%s address=%s", comport, address)
-    client = AuroraSerialClient(address, comport, parity="N", timeout=1)
 
-    devices.append(AuroraABBSolarPVMonitorSensor(client, name, "Power"))
-    add_entities(devices, True)
+async def async_setup_entry(hass, config_entry, async_add_entities) -> None:
+    """Set up aurora_abb_powerone sensor based on a config entry."""
+    entities = []
+
+    sensortypes = [
+        {"parameter": "instantaneouspower", "name": "Power Output"},
+        {"parameter": "temperature", "name": "Temperature"},
+    ]
+    client = hass.data[DOMAIN][config_entry.unique_id]
+    data = config_entry.data
+
+    for sens in sensortypes:
+        entities.append(AuroraSensor(client, data, sens["name"], sens["parameter"]))
+
+    _LOGGER.debug("async_setup_entry adding %d entities", len(entities))
+    async_add_entities(entities, True)
 
 
-class AuroraABBSolarPVMonitorSensor(SensorEntity):
-    """Representation of a Sensor."""
+class AuroraSensor(AuroraDevice, SensorEntity):
+    """Representation of a Sensor on a Aurora ABB PowerOne Solar inverter."""
 
     _attr_state_class = STATE_CLASS_MEASUREMENT
-    _attr_native_unit_of_measurement = POWER_WATT
-    _attr_device_class = DEVICE_CLASS_POWER
 
-    def __init__(self, client, name, typename):
+    def __init__(self, client: AuroraSerialClient, data, name, typename):
         """Initialize the sensor."""
-        self._attr_name = f"{name} {typename}"
-        self.client = client
+        super().__init__(client, data)
+        if typename == "instantaneouspower":
+            self.type = typename
+            self._attr_unit_of_measurement = POWER_WATT
+            self._attr_device_class = DEVICE_CLASS_POWER
+        elif typename == "temperature":
+            self.type = typename
+            self._attr_native_unit_of_measurement = TEMP_CELSIUS
+            self._attr_device_class = DEVICE_CLASS_TEMPERATURE
+        else:
+            raise InvalidStateError(f"Unrecognised typename '{typename}'")
+        self._attr_name = f"{name}"
+        self.availableprev = True
 
     def update(self):
         """Fetch new state data for the sensor.
@@ -65,11 +96,21 @@ class AuroraABBSolarPVMonitorSensor(SensorEntity):
         This is the only method that should fetch new data for Home Assistant.
         """
         try:
+            self.availableprev = self._attr_available
             self.client.connect()
-            # read ADC channel 3 (grid power output)
-            power_watts = self.client.measure(3, True)
-            self._attr_native_value = round(power_watts, 1)
+            if self.type == "instantaneouspower":
+                # read ADC channel 3 (grid power output)
+                power_watts = self.client.measure(3, True)
+                self._attr_state = round(power_watts, 1)
+            elif self.type == "temperature":
+                temperature_c = self.client.measure(21)
+                self._attr_native_value = round(temperature_c, 1)
+            self._attr_available = True
+
         except AuroraError as error:
+            self._attr_state = None
+            self._attr_native_value = None
+            self._attr_available = False
             # aurorapy does not have different exceptions (yet) for dealing
             # with timeout vs other comms errors.
             # This means the (normal) situation of no response during darkness
@@ -82,7 +123,14 @@ class AuroraABBSolarPVMonitorSensor(SensorEntity):
                 _LOGGER.debug("No response from inverter (could be dark)")
             else:
                 raise error
-            self._attr_native_value = None
         finally:
+            if self._attr_available != self.availableprev:
+                if self._attr_available:
+                    _LOGGER.info("Communication with %s back online", self.name)
+                else:
+                    _LOGGER.warning(
+                        "Communication with %s lost",
+                        self.name,
+                    )
             if self.client.serline.isOpen():
                 self.client.close()
