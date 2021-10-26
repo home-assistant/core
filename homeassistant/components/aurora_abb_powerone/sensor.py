c05bfd2e7a76452d09d04c8a7a6cdb6e8399c4c9
J. Nick Koston <nick@koston.org>
2021-10-25 21:48:20 -0500

    Fix one more place

--- homeassistant/components/aurora_abb_powerone/sensor.py
+++ homeassistant/components/aurora_abb_powerone/sensor.py
@@ -101,7 +101,7 @@ class AuroraSensor(AuroraDevice, SensorEntity):
             if self.type == "instantaneouspower":
                 # read ADC channel 3 (grid power output)
                 power_watts = self.client.measure(3, True)
-                self._attr_state = round(power_watts, 1)
+                self._attr_native_value = round(power_watts, 1)
             elif self.type == "temperature":
                 temperature_c = self.client.measure(21)
                 self._attr_native_value = round(temperature_c, 1)
