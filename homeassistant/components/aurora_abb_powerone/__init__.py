0ebc74420a4a3f5cf9919993ef7fd30f339837a5
Dave T <17680170+davet2001@users.noreply.github.com>
2021-10-26 20:00:15 +0000

    Add type hints.

--- homeassistant/components/aurora_abb_powerone/__init__.py
+++ homeassistant/components/aurora_abb_powerone/__init__.py
@@ -25,7 +25,7 @@ PLATFORMS = ["sensor"]
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Aurora ABB PowerOne from a config entry."""
 
     comport = entry.data[CONF_PORT]
@@ -73,7 +73,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
 
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
