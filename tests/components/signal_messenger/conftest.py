d1beffe480c9448731f0d4f42be2c79b4e835b75
Antoni Rozanski <antoni.rozanski@allegro.pl>
2021-10-26 13:47:29 +0200

    Flake8 related issues fixed

--- tests/components/signal_messenger/conftest.py
+++ tests/components/signal_messenger/conftest.py
@@ -1,12 +1,13 @@
-import pytest
+"""Signal notification test helpers."""
 from pysignalclirestapi import SignalCliRestApi
+import pytest
 
 from homeassistant.components.signal_messenger.notify import SignalNotificationService
 
 
 @pytest.fixture
 def signal_notification_service():
-    """Setting up signal notification service"""
+    """Set up signal notification service."""
     recipients = ["+435565656565"]
     number = "+43443434343"
     client = SignalCliRestApi("http://127.0.0.1:8080", number)
@@ -21,6 +22,7 @@ NUMBERS_TO = ["+435565656565"]
 
 @pytest.fixture
 def signal_requests_mock(requests_mock):
+    """Prepare signal service mock."""
     requests_mock.register_uri(
         "POST",
         "http://127.0.0.1:8080" + SIGNAL_SEND_PATH_SUFIX,
