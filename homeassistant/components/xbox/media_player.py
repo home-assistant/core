46ae51441b99e4720c1b7a73d4f433cb41b83e4e
Robert Hillis <tkdrob4390@yahoo.com>
2021-10-26 05:35:12 -0400

    tweak

--- homeassistant/components/xbox/media_player.py
+++ homeassistant/components/xbox/media_player.py
@@ -221,7 +221,7 @@ class XboxMediaPlayer(CoordinatorEntity, MediaPlayerEntity):
             ".+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)",
             self._console.console_type,
         )
-        self._attr_device_info = DeviceInfo(
+        return DeviceInfo(
             identifiers={(DOMAIN, self._console.id)},
             manufacturer="Microsoft",
             model=" ".join([m.group(0) for m in matches]),
