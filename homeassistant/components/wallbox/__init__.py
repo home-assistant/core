6d6cb03848f45117e4a8e3b6112a51a94f0cfe3e
hesselonline <hesselonline@users.noreply.github.com>
2021-10-27 19:53:14 +0200

    Add Number platform to Wallbox (#52786)
    Co-authored-by: jan iversen <jancasacondor@gmail.com>

--- homeassistant/components/wallbox/__init__.py
+++ homeassistant/components/wallbox/__init__.py
@@ -6,37 +6,40 @@ import logging
 import requests
 from wallbox import Wallbox
 
-from homeassistant import exceptions
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from .const import CONF_CONNECTIONS, CONF_ROUND, CONF_SENSOR_TYPES, CONF_STATION, DOMAIN
+from .const import (
+    CONF_CONNECTIONS,
+    CONF_DATA_KEY,
+    CONF_MAX_CHARGING_CURRENT_KEY,
+    CONF_ROUND,
+    CONF_SENSOR_TYPES,
+    CONF_STATION,
+    DOMAIN,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = ["sensor"]
+PLATFORMS = ["sensor", "number"]
 UPDATE_INTERVAL = 30
 
 
-class WallboxHub:
-    """Wallbox Hub class."""
+class WallboxCoordinator(DataUpdateCoordinator):
+    """Wallbox Coordinator class."""
 
-    def __init__(self, station, username, password, hass):
+    def __init__(self, station, wallbox, hass):
         """Initialize."""
         self._station = station
-        self._username = username
-        self._password = password
-        self._wallbox = Wallbox(self._username, self._password)
-        self._hass = hass
-        self._coordinator = DataUpdateCoordinator(
+        self._wallbox = wallbox
+
+        super().__init__(
             hass,
             _LOGGER,
-            # Name of the data. For logging purposes.
-            name="wallbox",
-            update_method=self.async_get_data,
-            # Polling interval. Will only be polled if there are subscribers.
+            name=DOMAIN,
             update_interval=timedelta(seconds=UPDATE_INTERVAL),
         )
 
@@ -50,11 +53,24 @@ class WallboxHub:
                 raise InvalidAuth from wallbox_connection_error
             raise ConnectionError from wallbox_connection_error
 
+    def _validate(self):
+        """Authenticate using Wallbox API."""
+        try:
+            self._wallbox.authenticate()
+            return True
+        except requests.exceptions.HTTPError as wallbox_connection_error:
+            if wallbox_connection_error.response.status_code == 403:
+                raise InvalidAuth from wallbox_connection_error
+            raise ConnectionError from wallbox_connection_error
+
     def _get_data(self):
         """Get new sensor data for Wallbox component."""
         try:
             self._authenticate()
             data = self._wallbox.getChargerStatus(self._station)
+            data[CONF_MAX_CHARGING_CURRENT_KEY] = data[CONF_DATA_KEY][
+                CONF_MAX_CHARGING_CURRENT_KEY
+            ]
 
             filtered_data = {k: data[k] for k in CONF_SENSOR_TYPES if k in data}
 
@@ -69,42 +85,52 @@ class WallboxHub:
         except requests.exceptions.HTTPError as wallbox_connection_error:
             raise ConnectionError from wallbox_connection_error
 
-    async def async_coordinator_first_refresh(self):
-        """Refresh coordinator for the first time."""
-        await self._coordinator.async_config_entry_first_refresh()
+    def _set_charging_current(self, charging_current):
+        """Set maximum charging current for Wallbox."""
+        try:
+            self._authenticate()
+            self._wallbox.setMaxChargingCurrent(self._station, charging_current)
+        except requests.exceptions.HTTPError as wallbox_connection_error:
+            if wallbox_connection_error.response.status_code == 403:
+                raise InvalidAuth from wallbox_connection_error
+            raise ConnectionError from wallbox_connection_error
 
-    async def async_authenticate(self) -> bool:
-        """Authenticate using Wallbox API."""
-        return await self._hass.async_add_executor_job(self._authenticate)
+    async def async_set_charging_current(self, charging_current):
+        """Set maximum charging current for Wallbox."""
+        await self.hass.async_add_executor_job(
+            self._set_charging_current, charging_current
+        )
+        await self.async_request_refresh()
 
-    async def async_get_data(self) -> bool:
+    async def _async_update_data(self) -> bool:
         """Get new sensor data for Wallbox component."""
-        data = await self._hass.async_add_executor_job(self._get_data)
+        data = await self.hass.async_add_executor_job(self._get_data)
         return data
 
-    @property
-    def coordinator(self):
-        """Return the coordinator."""
-        return self._coordinator
+    async def async_validate_input(self) -> bool:
+        """Get new sensor data for Wallbox component."""
+        data = await self.hass.async_add_executor_job(self._validate)
+        return data
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Wallbox from a config entry."""
-    wallbox = WallboxHub(
+    wallbox = Wallbox(entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD])
+    wallbox_coordinator = WallboxCoordinator(
         entry.data[CONF_STATION],
-        entry.data[CONF_USERNAME],
-        entry.data[CONF_PASSWORD],
+        wallbox,
         hass,
     )
 
-    await wallbox.async_authenticate()
+    await wallbox_coordinator.async_validate_input()
 
-    await wallbox.async_coordinator_first_refresh()
+    await wallbox_coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {CONF_CONNECTIONS: {}})
-    hass.data[DOMAIN][CONF_CONNECTIONS][entry.entry_id] = wallbox
+    hass.data[DOMAIN][CONF_CONNECTIONS][entry.entry_id] = wallbox_coordinator
 
     for platform in PLATFORMS:
+
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, platform)
         )
@@ -116,10 +142,10 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
-        hass.data[DOMAIN]["connections"].pop(entry.entry_id)
+        hass.data[DOMAIN][CONF_CONNECTIONS].pop(entry.entry_id)
 
     return unload_ok
 
 
-class InvalidAuth(exceptions.HomeAssistantError):
+class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
