c28eff751623f2c7dea3a399bbed8f86ee13065f
Jonathan Keljo <jkeljo@users.noreply.github.com>
2021-10-27 21:35:48 +0000

    Trim unused constants

--- homeassistant/components/greeneye_monitor/const.py
+++ homeassistant/components/greeneye_monitor/const.py
@@ -1,9 +1,8 @@
-"""Shared constants for the greeneye_monitor integration."""
+"""Constants for the greeneye_monitor component."""
 
 CONF_CHANNELS = "channels"
 CONF_COUNTED_QUANTITY = "counted_quantity"
 CONF_COUNTED_QUANTITY_PER_PULSE = "counted_quantity_per_pulse"
-CONF_MONITOR_SERIAL_NUMBER = "monitor"
 CONF_MONITORS = "monitors"
 CONF_NET_METERING = "net_metering"
 CONF_NUMBER = "number"
@@ -14,11 +13,7 @@ CONF_TIME_UNIT = "time_unit"
 CONF_VOLTAGE_SENSORS = "voltage"
 
 DATA_GREENEYE_MONITOR = "greeneye_monitor"
-DOMAIN = "greeneye_monitor"
 
-SENSOR_TYPE_CURRENT = "current_sensor"
-SENSOR_TYPE_PULSE_COUNTER = "pulse_counter"
-SENSOR_TYPE_TEMPERATURE = "temperature_sensor"
-SENSOR_TYPE_VOLTAGE = "voltage_sensor"
+DOMAIN = "greeneye_monitor"
 
 TEMPERATURE_UNIT_CELSIUS = "C"
