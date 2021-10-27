59cc43c7ba920c83520e5995691c9bcb830c959d
Glenn Waters <gwwaters@gmail.com>
2021-10-26 21:00:37 -0400

    Update sensors to use SensorEntityDescription and SensorDeviceClass

--- homeassistant/components/environment_canada/sensor.py
+++ homeassistant/components/environment_canada/sensor.py
@@ -4,11 +4,16 @@ import re
 
 import voluptuous as vol
 
-from homeassistant.components.sensor import SensorDeviceClass, SensorEntity
-from homeassistant.const import ATTR_LOCATION, TEMP_CELSIUS
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import ATTR_LOCATION
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import ATTR_OBSERVATION_TIME, ATTR_STATION, DOMAIN
+from .const import (
+    ALERT_TYPES,
+    ATTR_STATION,
+    DOMAIN,
+    SENSOR_TYPES,
+)
 
 ATTR_TIME = "alert time"
 
@@ -27,94 +32,75 @@ def validate_station(station):
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Add a weather entity from a config_entry."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]["weather_coordinator"]
-    weather_data = coordinator.ec_data
-
-    sensors = list(weather_data.conditions)
-    labels = [weather_data.conditions[sensor]["label"] for sensor in sensors]
-    alerts_list = list(weather_data.alerts)
-    labels = labels + [weather_data.alerts[sensor]["label"] for sensor in alerts_list]
-    sensors = sensors + alerts_list
-
-    async_add_entities(
-        [
-            ECSensor(coordinator, sensor, label)
-            for sensor, label in zip(sensors, labels)
-        ],
-        True,
-    )
+    async_add_entities(ECSensor(coordinator, desc) for desc in SENSOR_TYPES)
+    async_add_entities(ECAlertSensor(coordinator, desc) for desc in ALERT_TYPES)
 
 
 class ECSensor(CoordinatorEntity, SensorEntity):
     """Implementation of an Environment Canada sensor."""
 
-    def __init__(self, coordinator, sensor, label):
+    def __init__(self, coordinator, description):
         """Initialize the sensor."""
         super().__init__(coordinator)
-        self.sensor_type = sensor
+        self.entity_description = description
         self.ec_data = coordinator.ec_data
 
         self._attr_attribution = self.ec_data.metadata["attribution"]
-        self._attr_name = f"{coordinator.config_entry.title} {label}"
-        self._attr_unique_id = f"{self.ec_data.metadata['location']}-{sensor}"
-        self._attr = None
-        self._unit = None
-        self._device_class = None
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes of the device."""
-        return self._attr
-
-    @property
-    def native_unit_of_measurement(self):
-        """Return the units of measurement."""
-        return self._unit
-
-    @property
-    def device_class(self):
-        """Return the class of this device, from component DEVICE_CLASSES."""
-        return self._device_class
+        self._attr_name = f"{coordinator.config_entry.title} {description.name}"
+        self._attr_unique_id = f"{self.ec_data.metadata['location']}-{description.key}"
+        self._attr_extra_state_attributes = {
+            ATTR_LOCATION: self.ec_data.metadata.get("location"),
+            ATTR_STATION: self.ec_data.metadata.get("station"),
+        }
 
     @property
     def native_value(self):
         """Update current conditions."""
-        metadata = self.ec_data.metadata
-        sensor_data = self.ec_data.conditions.get(self.sensor_type)
-        if not sensor_data:
-            sensor_data = self.ec_data.alerts.get(self.sensor_type)
+        sensor_type = self.entity_description.key
+        if sensor_type == "timestamp":
+            return self.ec_data.metadata.get("timestamp")
 
-        self._attr = {}
-        value = sensor_data.get("value")
-
-        if isinstance(value, list):
-            state = " | ".join([str(s.get("title")) for s in value])[:255]
-            self._attr.update(
-                {ATTR_TIME: " | ".join([str(s.get("date")) for s in value])}
-            )
-        elif self.sensor_type == "tendency":
-            state = str(value).capitalize()
+        value = self.ec_data.conditions.get(sensor_type, {}).get("value")
+        if sensor_type == "tendency":
+            value = str(value).capitalize()
         elif isinstance(value, str) and len(value) > 255:
-            state = value[:255]
+            value = value[:255]
             _LOGGER.info(
                 "Value for %s truncated to 255 characters", self._attr_unique_id
             )
-        else:
-            state = value
+        return value
 
-        if sensor_data.get("unit") == "C" or self.sensor_type in (
-            "wind_chill",
-            "humidex",
-        ):
-            self._unit = TEMP_CELSIUS
-            self._device_class = SensorDeviceClass.TEMPERATURE
-        else:
-            self._unit = sensor_data.get("unit")
 
-        self._attr.update(
-            {
-                ATTR_OBSERVATION_TIME: metadata.get("timestamp"),
-                ATTR_LOCATION: metadata.get("location"),
-                ATTR_STATION: metadata.get("station"),
-            }
-        )
-        return state
+class ECAlertSensor(CoordinatorEntity, SensorEntity):
+    """Implementation of an Environment Canada sensor."""
+
+    def __init__(self, coordinator, description):
+        """Initialize the alert sensor."""
+        super().__init__(coordinator)
+        self.entity_description = description
+        self.ec_data = coordinator.ec_data
+
+        self._attr_attribution = self.ec_data.metadata["attribution"]
+        self._attr_name = f"{coordinator.config_entry.title} {description.name}"
+        self._attr_unique_id = f"{self.ec_data.metadata['location']}-{description.key}"
+
+    @property
+    def native_value(self):
+        """Return the state."""
+        alert_name = self.entity_description.key
+        value = self.ec_data.alerts.get(alert_name, {}).get("value")
+
+        metadata = self.ec_data.metadata
+        self._attr_extra_state_attributes = {
+            ATTR_LOCATION: metadata.get("location"),
+            ATTR_STATION: metadata.get("station"),
+        }
+
+        if value is None:
+            return 0
+
+        for index, alert in enumerate(value, start=1):
+            self._attr_extra_state_attributes[f"alert_{index}"] = alert.get("title")
+            self._attr_extra_state_attributes[f"alert_time_{index}"] = alert.get("date")
+
+        return len(value)
