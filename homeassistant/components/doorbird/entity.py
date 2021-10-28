0b6100a4f0e5df8026a9d65aa871b19d83c72d9d
Tom Matheussen <tmatheussen@avia-gis.com>
2021-10-28 06:28:49 +0000

    Fix black

--- homeassistant/components/doorbird/entity.py
+++ homeassistant/components/doorbird/entity.py
@@ -33,5 +33,5 @@ class DoorBirdEntity(Entity):
             model=self._doorstation_info[DOORBIRD_INFO_KEY_DEVICE_TYPE],
             name=self._doorstation.name,
             sw_version=f"{firmware} {firmware_build}",
-            configuration_url="https://webadmin.doorbird.com/"
+            configuration_url="https://webadmin.doorbird.com/",
         )
