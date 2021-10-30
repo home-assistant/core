53ca8fa4e8639c9b031d10ed31397de48c5f37bb
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-10-30 16:29:07 +0200

    Use assignment expressions 31 (#58715)

--- homeassistant/components/integration/sensor.py
+++ homeassistant/components/integration/sensor.py
@@ -124,8 +124,7 @@ class IntegrationSensor(RestoreEntity, SensorEntity):
     async def async_added_to_hass(self):
         """Handle entity which will be added."""
         await super().async_added_to_hass()
-        state = await self.async_get_last_state()
-        if state:
+        if state := await self.async_get_last_state():
             try:
                 self._state = Decimal(state.state)
             except (DecimalException, ValueError) as err:
