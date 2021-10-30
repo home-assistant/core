4874a97909d29cb67d9e28b3e2bd84f36329896a
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2021-10-30 14:26:54 +0000

    Cleanup

--- homeassistant/components/switchbot/switch.py
+++ homeassistant/components/switchbot/switch.py
@@ -131,7 +131,7 @@ class SwitchBotBotEntity(SwitchbotEntity, SwitchEntity, RestoreEntity):
             self._last_run_success = bool(
                 await self.hass.async_add_executor_job(self._device.turn_on)
             )
-            if not self.data["data"]["switchMode"] and self._last_run_success:
+            if self._last_run_success:
                 self._attr_is_on = True
 
     async def async_turn_off(self, **kwargs: Any) -> None:
@@ -142,7 +142,7 @@ class SwitchBotBotEntity(SwitchbotEntity, SwitchEntity, RestoreEntity):
             self._last_run_success = bool(
                 await self.hass.async_add_executor_job(self._device.turn_off)
             )
-            if not self.data["data"]["switchMode"] and self._last_run_success:
+            if self._last_run_success:
                 self._attr_is_on = False
 
     @property
