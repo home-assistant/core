2b87076450a64b14b7b1c17e9b87247ff753dfd9
hesselonline <hesselonline@users.noreply.github.com>
2021-11-02 11:11:46 +0100

    Split wallbox sensor type and number type (#58807)

--- homeassistant/components/wallbox/number.py
+++ homeassistant/components/wallbox/number.py
@@ -1,5 +1,10 @@
 """Home Assistant component for accessing the Wallbox Portal API. The sensor component creates multiple sensors regarding wallbox performance."""
-from homeassistant.components.number import NumberEntity
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from homeassistant.components.number import NumberEntity, NumberEntityDescription
+from homeassistant.const import DEVICE_CLASS_CURRENT
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from . import InvalidAuth
@@ -8,35 +13,59 @@ from .const import (
     CONF_MAX_AVAILABLE_POWER_KEY,
     CONF_MAX_CHARGING_CURRENT_KEY,
     DOMAIN,
-    SENSOR_TYPES,
 )
+
+
+@dataclass
+class WallboxNumberEntityDescription(NumberEntityDescription):
+    """Describes Wallbox sensor entity."""
+
+    min_value: float = 0
+
+
+NUMBER_TYPES: dict[str, NumberEntityDescription] = {
+    CONF_MAX_CHARGING_CURRENT_KEY: WallboxNumberEntityDescription(
+        key=CONF_MAX_CHARGING_CURRENT_KEY,
+        name="Max. Charging Current",
+        device_class=DEVICE_CLASS_CURRENT,
+        min_value=6,
+    ),
+}
 
 
 async def async_setup_entry(hass, config, async_add_entities):
     """Create wallbox sensor entities in HASS."""
     coordinator = hass.data[DOMAIN][CONF_CONNECTIONS][config.entry_id]
+
     # Check if the user is authorized to change current, if so, add number component:
     try:
         await coordinator.async_set_charging_current(
             coordinator.data[CONF_MAX_CHARGING_CURRENT_KEY]
         )
     except InvalidAuth:
-        pass
-    else:
-        async_add_entities([WallboxNumber(coordinator, config)])
+        return
+
+    async_add_entities(
+        [
+            WallboxNumber(coordinator, config, description)
+            for ent in coordinator.data
+            if (description := NUMBER_TYPES.get(ent))
+        ]
+    )
 
 
 class WallboxNumber(CoordinatorEntity, NumberEntity):
     """Representation of the Wallbox portal."""
 
-    def __init__(self, coordinator, config):
+    def __init__(
+        self, coordinator, config, description: WallboxNumberEntityDescription
+    ):
         """Initialize a Wallbox sensor."""
         super().__init__(coordinator)
-        sensor_description = SENSOR_TYPES[CONF_MAX_CHARGING_CURRENT_KEY]
+        self.entity_description = description
         self._coordinator = coordinator
-        self._attr_name = f"{config.title} {sensor_description.name}"
-        self._attr_min_value = 6
-        self._attr_device_class = sensor_description.device_class
+        self._attr_name = f"{config.title} {description.name}"
+        self._attr_min_value = description.min_value
 
     @property
     def max_value(self):
