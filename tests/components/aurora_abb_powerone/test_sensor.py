dff98b024cfd43e8be1150f9d88fba4bb466541f
Dave T <17680170+davet2001@users.noreply.github.com>
2021-11-03 00:30:29 +0000

    Aurora abb defer unique_id assignment during yaml import (#58887)
    * Defer unique_id assignment during yaml import if dark
    
    * Back out variable name change to simplify.
    
    * Allow config flow yaml setup deferral.
    
    * Fix deferred yaml import
    
    * Code review: only wrap necessary lines in try blk
    
    * Code review: catch possible duplicate unique_id
    
    * Simplify assignment.
    
    * Code review: use timedelta to retry yaml import
    
    * Code review: if a different error occurs, raise it
    
    * Remove current config entry if duplicate unique_id
    
    * Code review: remove unnecessary line.
    
    * Code review: revert change, leave to other PR.
    
    * Code review: remove unnecessary patch & min->sec
    
    * Remove unnecessary else after raise.
    
    * Increase test coverage.
    
    * Check the number of config entries at each stage
    
    * Raise ConfigEntryNotReady when connection fails.
    
    * Log & return false for error on yaml import

--- tests/components/aurora_abb_powerone/test_sensor.py
+++ tests/components/aurora_abb_powerone/test_sensor.py
@@ -65,6 +65,18 @@ async def test_setup_platform_valid_config(hass):
         "aurorapy.client.AuroraSerialClient.measure",
         side_effect=_simulated_returns,
     ), patch(
+        "aurorapy.client.AuroraSerialClient.serial_number",
+        return_value="9876543",
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.version",
+        return_value="9.8.7.6",
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.pn",
+        return_value="A.B.C",
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.firmware",
+        return_value="1.234",
+    ), patch(
         "aurorapy.client.AuroraSerialClient.cumulated_energy",
         side_effect=_simulated_returns,
     ), assert_setup_component(
@@ -95,6 +107,18 @@ async def test_sensors(hass):
         "aurorapy.client.AuroraSerialClient.measure",
         side_effect=_simulated_returns,
     ), patch(
+        "aurorapy.client.AuroraSerialClient.serial_number",
+        return_value="9876543",
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.version",
+        return_value="9.8.7.6",
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.pn",
+        return_value="A.B.C",
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.firmware",
+        return_value="1.234",
+    ), patch(
         "aurorapy.client.AuroraSerialClient.cumulated_energy",
         side_effect=_simulated_returns,
     ):
@@ -123,6 +147,18 @@ async def test_sensor_dark(hass):
     # sun is up
     with patch("aurorapy.client.AuroraSerialClient.connect", return_value=None), patch(
         "aurorapy.client.AuroraSerialClient.measure", side_effect=_simulated_returns
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.serial_number",
+        return_value="9876543",
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.version",
+        return_value="9.8.7.6",
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.pn",
+        return_value="A.B.C",
+    ), patch(
+        "aurorapy.client.AuroraSerialClient.firmware",
+        return_value="1.234",
     ):
         mock_entry.add_to_hass(hass)
         await hass.config_entries.async_setup(mock_entry.entry_id)
