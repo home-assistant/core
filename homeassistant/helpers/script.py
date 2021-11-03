b1ef9196fb615913455e1709a6e48f35d82ebec7
J. Nick Koston <nick@koston.org>
2021-11-02 23:18:29 -0500

    fixes

--- homeassistant/helpers/script.py
+++ homeassistant/helpers/script.py
@@ -476,7 +476,7 @@ class _ScriptRun:
         def async_script_wait(entity_id, from_s, to_s):
             """Handle script after template condition is true."""
             wait_var = self._variables["wait"]
-            if to_context:
+            if to_context and to_context.deadline:
                 wait_var["remaining"] = to_context.deadline - self._hass.loop.time()
             else:
                 wait_var["remaining"] = timeout
@@ -780,7 +780,7 @@ class _ScriptRun:
 
         async def async_done(variables, context=None):
             wait_var = self._variables["wait"]
-            if to_context:
+            if to_context and to_context.deadline:
                 wait_var["remaining"] = to_context.deadline - self._hass.loop.time()
             else:
                 wait_var["remaining"] = timeout
