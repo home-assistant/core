8ff1d71bdc7c4b639a6c48429da4b34527f83d63
Damien Duboeuf <smeagolworms4@gmail.com>
2021-11-08 14:02:18 +0100

    Add MQTT object_id option (#58728)
    * Add MQTT object_id option
    
    * Add MQTT object_id option
    
    * Add MQTT object_id option
    
    * Add MQTT object_id option - Fix light and vacuum
    
    * Add MQTT object_id option - Fix light and vacuum
    
    * Add MQTT object_id option - Fix lock
    
    * Add MQTT object_id option - Fix device
    
    * Add MQTT object_id option - Fix device
    
    * Update tests/components/mqtt/test_discovery.py
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    
    * Change deprecated method
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>

--- homeassistant/components/mqtt/mixins.py
+++ homeassistant/components/mqtt/mixins.py
@@ -28,7 +28,12 @@ from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
 )
-from homeassistant.helpers.entity import ENTITY_CATEGORIES_SCHEMA, DeviceInfo, Entity
+from homeassistant.helpers.entity import (
+    ENTITY_CATEGORIES_SCHEMA,
+    DeviceInfo,
+    Entity,
+    async_generate_entity_id,
+)
 from homeassistant.helpers.typing import ConfigType
 
 from . import DATA_MQTT, debug_info, publish, subscription
@@ -82,6 +87,7 @@ CONF_VIA_DEVICE = "via_device"
 CONF_DEPRECATED_VIA_HUB = "via_hub"
 CONF_SUGGESTED_AREA = "suggested_area"
 CONF_CONFIGURATION_URL = "configuration_url"
+CONF_OBJECT_ID = "object_id"
 
 MQTT_ATTRIBUTES_BLOCKED = {
     "assumed_state",
@@ -183,6 +189,7 @@ MQTT_ENTITY_COMMON_SCHEMA = MQTT_AVAILABILITY_SCHEMA.extend(
         vol.Optional(CONF_ICON): cv.icon,
         vol.Optional(CONF_JSON_ATTRS_TOPIC): valid_subscribe_topic,
         vol.Optional(CONF_JSON_ATTRS_TEMPLATE): cv.template,
+        vol.Optional(CONF_OBJECT_ID): cv.string,
         vol.Optional(CONF_UNIQUE_ID): cv.string,
     }
 )
@@ -208,6 +215,14 @@ async def async_setup_entry_helper(hass, domain, async_setup, schema):
     async_dispatcher_connect(
         hass, MQTT_DISCOVERY_NEW.format(domain, "mqtt"), async_discover
     )
+
+
+def init_entity_id_from_config(hass, entity, config, entity_id_format):
+    """Set entity_id from object_id if defined in config."""
+    if CONF_OBJECT_ID in config:
+        entity.entity_id = async_generate_entity_id(
+            entity_id_format, config[CONF_OBJECT_ID], None, hass
+        )
 
 
 class MqttAttributes(Entity):
@@ -588,6 +603,8 @@ class MqttEntity(
 ):
     """Representation of an MQTT entity."""
 
+    _entity_id_format: str
+
     def __init__(self, hass, config, config_entry, discovery_data):
         """Init the MQTT Entity."""
         self.hass = hass
@@ -598,11 +615,20 @@ class MqttEntity(
         # Load config
         self._setup_from_config(self._config)
 
+        # Initialize entity_id from config
+        self._init_entity_id()
+
         # Initialize mixin classes
         MqttAttributes.__init__(self, config)
         MqttAvailability.__init__(self, config)
         MqttDiscoveryUpdate.__init__(self, discovery_data, self.discovery_update)
         MqttEntityDeviceInfo.__init__(self, config.get(CONF_DEVICE), config_entry)
+
+    def _init_entity_id(self):
+        """Set entity_id from object_id if defined in config."""
+        init_entity_id_from_config(
+            self.hass, self, self._config, self._entity_id_format
+        )
 
     async def async_added_to_hass(self):
         """Subscribe mqtt events."""
