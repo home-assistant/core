14347c5f59e99f934d5729518b8f0b4c206edb12
Matthias Alphart <farmio@alphart.net>
2021-11-11 07:29:16 +0100

    Make util.color._match_max_scale public (#59207)

--- tests/util/test_color.py
+++ tests/util/test_color.py
@@ -279,6 +279,20 @@ def test_color_rgb_to_hex():
     assert color_util.color_rgb_to_hex(255, 67.9204190, 0) == "ff4400"
 
 
+def test_match_max_scale():
+    """Test match_max_scale."""
+    match_max_scale = color_util.match_max_scale
+    assert match_max_scale((255, 255, 255), (255, 255, 255)) == (255, 255, 255)
+    assert match_max_scale((0, 0, 0), (0, 0, 0)) == (0, 0, 0)
+    assert match_max_scale((255, 255, 255), (128, 128, 128)) == (255, 255, 255)
+    assert match_max_scale((0, 255, 0), (64, 128, 128)) == (128, 255, 255)
+    assert match_max_scale((0, 100, 0), (128, 64, 64)) == (100, 50, 50)
+    assert match_max_scale((10, 20, 33), (100, 200, 333)) == (10, 20, 33)
+    assert match_max_scale((255,), (100, 200, 333)) == (77, 153, 255)
+    assert match_max_scale((128,), (10.5, 20.9, 30.4)) == (44, 88, 128)
+    assert match_max_scale((10, 20, 30, 128), (100, 200, 333)) == (38, 77, 128)
+
+
 def test_gamut():
     """Test gamut functions."""
     assert color_util.check_valid_gamut(GAMUT)
