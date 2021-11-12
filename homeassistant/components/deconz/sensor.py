40201f2de136e7c34ddebef4dc1dd8b555883f6d
Robert Svensson <kane610@me.com>
2021-11-12 20:40:21 +0100

    Add typing to deCONZ Number and Sensor platforms

--- homeassistant/components/deconz/sensor.py
+++ homeassistant/components/deconz/sensor.py
@@ -1,9 +1,14 @@
 """Support for deCONZ sensors."""
+from __future__ import annotations
+
+from collections.abc import ValuesView
+
 from pydeconz.sensor import (
     AirQuality,
     Battery,
     Consumption,
     Daylight,
+    DeconzSensor as PydeconzSensor,
     GenericStatus,
     Humidity,
     LightLevel,
@@ -22,6 +27,7 @@ from homeassistant.components.sensor import (
     SensorEntity,
     SensorEntityDescription,
 )
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     ATTR_TEMPERATURE,
     ATTR_VOLTAGE,
@@ -40,15 +46,16 @@ from homeassistant.const import (
     PRESSURE_HPA,
     TEMP_CELSIUS,
 )
-from homeassistant.core import callback
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
 )
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import ATTR_DARK, ATTR_ON
 from .deconz_device import DeconzDevice
-from .gateway import get_gateway_from_config_entry
+from .gateway import DeconzGateway, get_gateway_from_config_entry
 
 DECONZ_SENSORS = (
     AirQuality,
@@ -119,7 +126,11 @@ ENTITY_DESCRIPTIONS = {
 }
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
     """Set up the deCONZ sensors."""
     gateway = get_gateway_from_config_entry(hass, config_entry)
     gateway.entities[DOMAIN] = set()
@@ -127,13 +138,16 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     battery_handler = DeconzBatteryHandler(gateway)
 
     @callback
-    def async_add_sensor(sensors=gateway.api.sensors.values()):
+    def async_add_sensor(
+        sensors: list[PydeconzSensor]
+        | ValuesView[PydeconzSensor] = gateway.api.sensors.values(),
+    ) -> None:
         """Add sensors from deCONZ.
 
         Create DeconzBattery if sensor has a battery attribute.
         Create DeconzSensor if not a battery, switch or thermostat and not a binary sensor.
         """
-        entities = []
+        entities: list[DeconzBattery | DeconzSensor | DeconzTemperature] = []
 
         for sensor in sensors:
 
@@ -184,8 +198,9 @@ class DeconzSensor(DeconzDevice, SensorEntity):
     """Representation of a deCONZ sensor."""
 
     TYPE = DOMAIN
+    _device: PydeconzSensor
 
-    def __init__(self, device, gateway):
+    def __init__(self, device: PydeconzSensor, gateway: DeconzGateway) -> None:
         """Initialize deCONZ binary sensor."""
         super().__init__(device, gateway)
 
@@ -193,19 +208,20 @@ class DeconzSensor(DeconzDevice, SensorEntity):
             self.entity_description = entity_description
 
     @callback
-    def async_update_callback(self):
+    def async_update_callback(self) -> None:
         """Update the sensor's state."""
         keys = {"on", "reachable", "state"}
         if self._device.changed_keys.intersection(keys):
             super().async_update_callback()
 
     @property
-    def native_value(self):
+    def native_value(self) -> bool | float | int | str | None:
         """Return the state of the sensor."""
+        assert isinstance(self._device.state, (bool, float, int, str))
         return self._device.state
 
     @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict:
         """Return the state attributes of the sensor."""
         attr = {}
 
@@ -243,8 +259,9 @@ class DeconzTemperature(DeconzDevice, SensorEntity):
     """
 
     TYPE = DOMAIN
+    _device: PydeconzSensor
 
-    def __init__(self, device, gateway):
+    def __init__(self, device: PydeconzSensor, gateway: DeconzGateway) -> None:
         """Initialize deCONZ temperature sensor."""
         super().__init__(device, gateway)
 
@@ -252,20 +269,21 @@ class DeconzTemperature(DeconzDevice, SensorEntity):
         self._attr_name = f"{self._device.name} Temperature"
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         """Return a unique identifier for this device."""
         return f"{self.serial}-temperature"
 
     @callback
-    def async_update_callback(self):
+    def async_update_callback(self) -> None:
         """Update the sensor's state."""
         keys = {"temperature", "reachable"}
         if self._device.changed_keys.intersection(keys):
             super().async_update_callback()
 
     @property
-    def native_value(self):
+    def native_value(self) -> float:
         """Return the state of the sensor."""
+        assert isinstance(self._device.secondary_temperature, float)
         return self._device.secondary_temperature
 
 
@@ -273,8 +291,9 @@ class DeconzBattery(DeconzDevice, SensorEntity):
     """Battery class for when a device is only represented as an event."""
 
     TYPE = DOMAIN
+    _device: PydeconzSensor
 
-    def __init__(self, device, gateway):
+    def __init__(self, device: PydeconzSensor, gateway: DeconzGateway) -> None:
         """Initialize deCONZ battery level sensor."""
         super().__init__(device, gateway)
 
@@ -282,14 +301,14 @@ class DeconzBattery(DeconzDevice, SensorEntity):
         self._attr_name = f"{self._device.name} Battery Level"
 
     @callback
-    def async_update_callback(self):
+    def async_update_callback(self) -> None:
         """Update the battery's state, if needed."""
         keys = {"battery", "reachable"}
         if self._device.changed_keys.intersection(keys):
             super().async_update_callback()
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         """Return a unique identifier for this device.
 
         Normally there should only be one battery sensor per device from deCONZ.
@@ -305,12 +324,13 @@ class DeconzBattery(DeconzDevice, SensorEntity):
         return f"{self.serial}-battery"
 
     @property
-    def native_value(self):
+    def native_value(self) -> int:
         """Return the state of the battery."""
+        assert isinstance(self._device.battery, int)
         return self._device.battery
 
     @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict:
         """Return the state attributes of the battery."""
         attr = {}
 
@@ -325,21 +345,20 @@ class DeconzBattery(DeconzDevice, SensorEntity):
 class DeconzSensorStateTracker:
     """Track sensors without a battery state and signal when battery state exist."""
 
-    def __init__(self, sensor, gateway):
+    def __init__(self, sensor: PydeconzSensor, gateway: DeconzGateway) -> None:
         """Set up tracker."""
         self.sensor = sensor
         self.gateway = gateway
         sensor.register_callback(self.async_update_callback)
 
     @callback
-    def close(self):
+    def close(self) -> None:
         """Clean up tracker."""
         self.sensor.remove_callback(self.async_update_callback)
-        self.gateway = None
         self.sensor = None
 
     @callback
-    def async_update_callback(self):
+    def async_update_callback(self) -> None:
         """Sensor state updated."""
         if "battery" in self.sensor.changed_keys:
             async_dispatcher_send(
@@ -352,13 +371,13 @@ class DeconzSensorStateTracker:
 class DeconzBatteryHandler:
     """Creates and stores trackers for sensors without a battery state."""
 
-    def __init__(self, gateway):
+    def __init__(self, gateway: DeconzGateway) -> None:
         """Set up battery handler."""
         self.gateway = gateway
-        self._trackers = set()
+        self._trackers: set[DeconzSensorStateTracker] = set()
 
     @callback
-    def create_tracker(self, sensor):
+    def create_tracker(self, sensor: PydeconzSensor) -> None:
         """Create new tracker for battery state."""
         for tracker in self._trackers:
             if sensor == tracker.sensor:
@@ -366,7 +385,7 @@ class DeconzBatteryHandler:
         self._trackers.add(DeconzSensorStateTracker(sensor, self.gateway))
 
     @callback
-    def remove_tracker(self, sensor):
+    def remove_tracker(self, sensor: PydeconzSensor) -> None:
         """Remove tracker of battery state."""
         for tracker in self._trackers:
             if sensor == tracker.sensor:
