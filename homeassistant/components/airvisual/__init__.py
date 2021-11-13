f79b33c026e9283d287d51cb8d919b7f9cb127a7
jugla <59493499+jugla@users.noreply.github.com>
2021-11-13 09:46:42 +0100

    optimize async_get_cloud_coordinators_by_api_key

--- homeassistant/components/airvisual/__init__.py
+++ homeassistant/components/airvisual/__init__.py
@@ -104,11 +104,11 @@ def async_get_cloud_coordinators_by_api_key(
 ) -> list[DataUpdateCoordinator]:
     """Get all DataUpdateCoordinator objects related to a particular API key."""
     return [
-        attrs[DATA_COORDINATOR]
+        coordinator
         for entry_id, attrs in hass.data[DOMAIN].items()
         if (entry := hass.config_entries.async_get_entry(entry_id))
+        and (coordinator := attrs.get(DATA_COORDINATOR))
         and entry.data.get(CONF_API_KEY) == api_key
-        and attrs.get(DATA_COORDINATOR) is not None
     ]
 
 
