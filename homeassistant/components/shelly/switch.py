9ea6b15c4f2ff8aabed55e5d1065569f530d4305
mib1185 <mail@mib85.de>
2021-11-13 22:52:45 +0000

    remove virtual switch

--- homeassistant/components/shelly/switch.py
+++ homeassistant/components/shelly/switch.py
@@ -7,21 +7,15 @@ from aioshelly.block_device import Block
 
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ENTITY_CATEGORY_CONFIG
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
-from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.util import slugify
 
 from . import BlockDeviceWrapper, RpcDeviceWrapper
-from .const import BLOCK, CONF_OTA_BETA_CHANNEL, DATA_CONFIG_ENTRY, DOMAIN, RPC
+from .const import BLOCK, DATA_CONFIG_ENTRY, DOMAIN, RPC
 from .entity import ShellyBlockEntity, ShellyRpcEntity
 from .utils import (
     async_remove_shelly_entity,
-    get_block_device_name,
     get_device_entry_gen,
-    get_rpc_device_name,
     get_rpc_key_ids,
     is_block_channel_type_light,
     is_rpc_channel_type_light,
@@ -47,8 +41,6 @@ async def async_setup_block_entry(
 ) -> None:
     """Set up entities for block device."""
     wrapper = hass.data[DOMAIN][DATA_CONFIG_ENTRY][config_entry.entry_id][BLOCK]
-
-    async_add_entities([ShellyOtaUpdateBetaChannelSwitch(wrapper, config_entry)])
 
     # In roller mode the relay blocks exist but do not contain required info
     if (
@@ -82,8 +74,6 @@ async def async_setup_rpc_entry(
 ) -> None:
     """Set up entities for RPC device."""
     wrapper = hass.data[DOMAIN][DATA_CONFIG_ENTRY][config_entry.entry_id][RPC]
-
-    async_add_entities([ShellyOtaUpdateBetaChannelSwitch(wrapper, config_entry)])
 
     switch_key_ids = get_rpc_key_ids(wrapper.device.status, "switch")
 
@@ -155,46 +145,3 @@ class RpcRelaySwitch(ShellyRpcEntity, SwitchEntity):
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off relay."""
         await self.call_rpc("Switch.Set", {"id": self._id, "on": False})
-
-
-class ShellyOtaUpdateBetaChannelSwitch(SwitchEntity):
-    """Defines a Shelly OTA update beta channel switch."""
-
-    _attr_icon = "mdi:flask-outline"
-    _attr_entity_category = ENTITY_CATEGORY_CONFIG
-
-    def __init__(
-        self, wrapper: RpcDeviceWrapper | BlockDeviceWrapper, entry: ConfigEntry
-    ) -> None:
-        """Initialize Shelly OTA update beta channel switch."""
-        self._attr_device_info = DeviceInfo(
-            connections={(CONNECTION_NETWORK_MAC, wrapper.mac)}
-        )
-
-        if isinstance(wrapper, RpcDeviceWrapper):
-            device_name = get_rpc_device_name(wrapper.device)
-        else:
-            device_name = get_block_device_name(wrapper.device)
-
-        self._attr_name = f"{device_name} OTA Update Beta channel"
-        self._attr_unique_id = slugify(self._attr_name)
-
-        self.entry = entry
-        self.wrapper = wrapper
-
-    @property
-    def is_on(self) -> bool:
-        """Return true if device is on."""
-        return bool(self.entry.options.get(CONF_OTA_BETA_CHANNEL))
-
-    async def async_turn_on(self, **kwargs: Any) -> None:
-        """Turn the device on."""
-        self.hass.config_entries.async_update_entry(
-            self.entry, options={**self.entry.options, CONF_OTA_BETA_CHANNEL: True}
-        )
-
-    async def async_turn_off(self, **kwargs: Any) -> None:
-        """Turn the device off."""
-        self.hass.config_entries.async_update_entry(
-            self.entry, options={**self.entry.options, CONF_OTA_BETA_CHANNEL: False}
-        )
