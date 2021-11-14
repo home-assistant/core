d430eff2609ba7c17ce07fe80d659ef8e6074ba5
Daniel Hjelseth Høyer <github@dahoiv.net>
2021-11-14 21:16:41 +0100

    Fix review comments
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- tests/components/mill/test_init.py
+++ tests/components/mill/test_init.py
@@ -24,9 +24,6 @@ async def test_setup_with_cloud_config(hass):
     with patch("mill.Mill.fetch_heater_and_sensor_data", return_value={}) as mock_fetch:
         with patch("mill.Mill.connect", return_value=True) as mock_connect:
             assert await mill.async_setup_entry(hass, mock_entry)
-    assert isinstance(
-        hass.data[mill.DOMAIN][mill.CLOUD]["user"].mill_data_connection, mill.Mill
-    )
     assert len(mock_fetch.mock_calls) == 1
     assert len(mock_connect.mock_calls) == 1
 
@@ -42,9 +39,10 @@ async def test_setup_with_cloud_config_fails(hass):
         },
     )
     mock_entry.add_to_hass(hass)
-    with patch("mill.Mill.connect", return_value=False):
-        with pytest.raises(ConfigEntryNotReady):
-            assert not await mill.async_setup_entry(hass, mock_entry)
+    with patch("mill.Mill.connect", return_value=False), pytest.raises(
+        ConfigEntryNotReady
+    ):
+        assert not await mill.async_setup_entry(hass, mock_entry)
 
 
 async def test_setup_with_old_cloud_config(hass):
@@ -57,13 +55,12 @@ async def test_setup_with_old_cloud_config(hass):
         },
     )
     mock_entry.add_to_hass(hass)
-    with patch("mill.Mill.fetch_heater_and_sensor_data", return_value={}):
-        with patch("mill.Mill.connect", return_value=True):
-            assert await mill.async_setup_entry(hass, mock_entry)
+    with patch("mill.Mill.fetch_heater_and_sensor_data", return_value={}), patch(
+        "mill.Mill.connect", return_value=True
+    ) as mock_connect:
+        assert await mill.async_setup_entry(hass, mock_entry)
 
-    assert isinstance(
-        hass.data[mill.DOMAIN][mill.CLOUD]["user"].mill_data_connection, mill.Mill
-    )
+    assert len(mock_connect.mock_calls) == 1
 
 
 async def test_setup_with_local_config(hass):
@@ -92,12 +89,6 @@ async def test_setup_with_local_config(hass):
         assert await mill.async_setup_entry(hass, mock_entry)
         await hass.async_block_till_done()
 
-    assert isinstance(
-        hass.data[mill.DOMAIN][mill.LOCAL][
-            mock_entry.data[mill.CONF_IP_ADDRESS]
-        ].mill_data_connection,
-        mill.MillLocal,
-    )
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_fetch.mock_calls) == 1
     assert len(mock_connect.mock_calls) == 1
@@ -117,9 +108,8 @@ async def test_unload_entry(hass):
 
     with patch.object(
         hass.config_entries, "async_forward_entry_unload", return_value=mock_coro(True)
-    ) as unload_entry:
-        with patch("mill.Mill.connect", return_value=True):
-            assert await mill.async_setup_entry(hass, mock_entry)
+    ) as unload_entry, patch("mill.Mill.connect", return_value=True):
+        assert await mill.async_setup_entry(hass, mock_entry)
 
         assert await mill.async_unload_entry(hass, mock_entry)
         assert unload_entry.call_count == 2
