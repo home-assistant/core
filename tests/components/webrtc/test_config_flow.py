d0c21179bc2a96bdc660f4c7613eada6fcd002d7
Allen Porter <allen@thebends.org>
2021-11-13 16:34:37 -0800

    Readability improvements for webrtc

--- tests/components/webrtc/test_config_flow.py
+++ tests/components/webrtc/test_config_flow.py
@@ -16,16 +16,16 @@ async def test_web_full_flow(hass: HomeAssistant):
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
-    assert result["type"] == "form"
-    assert result["step_id"] == "user"
-    assert result["data_schema"].schema["rtsp_to_webrtc_url"] == str
-    assert not result["errors"]
-
+    assert result.get("type") == "form"
+    assert result.get("step_id") == "user"
+    assert result.get("data_schema").schema.get("rtsp_to_webrtc_url") == str
+    assert result.get("errors") is None
+    assert "flow_id" in result
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"], {"rtsp_to_webrtc_url": "https://example.com"}
     )
-
-    assert result["type"] == "create_entry"
+    assert result.get("type") == "create_entry"
+    assert "result" in result
     assert result["result"].data == {"rtsp_to_webrtc_url": "https://example.com"}
 
 
@@ -39,5 +39,5 @@ async def test_single_config_entry(hass):
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
-    assert result["type"] == "abort"
-    assert result["reason"] == "single_instance_allowed"
+    assert result.get("type") == "abort"
+    assert result.get("reason") == "single_instance_allowed"
