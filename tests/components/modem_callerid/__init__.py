9e9be3165c7e89e13512dac38629a2213af1fb2b
Robert Hillis <tkdrob4390@yahoo.com>
2021-11-14 18:05:01 -0500

    Refactor tests for modem_callerid

--- tests/components/modem_callerid/__init__.py
+++ tests/components/modem_callerid/__init__.py
@@ -3,24 +3,32 @@
 from unittest.mock import patch
 
 from phone_modem import DEFAULT_PORT
-
-from homeassistant.const import CONF_DEVICE
-
-CONF_DATA = {CONF_DEVICE: DEFAULT_PORT}
-
-IMPORT_DATA = {"sensor": {"platform": "modem_callerid"}}
+import serial.tools.list_ports_common
 
 
-def _patch_init_modem():
+def patch_init_modem():
+    """Mock modem."""
     return patch(
-        "homeassistant.components.modem_callerid.PhoneModem",
+        "homeassistant.components.modem_callerid.PhoneModem.initialize",
         autospec=True,
     )
 
 
-def _patch_config_flow_modem(mocked_modem):
+def patch_config_flow_modem(mocked_modem):
+    """Mock modem config flow."""
     return patch(
-        "homeassistant.components.modem_callerid.config_flow.PhoneModem",
+        "homeassistant.components.modem_callerid.config_flow.PhoneModem.test",
         autospec=True,
         return_value=mocked_modem,
     )
+
+
+def com_port():
+    """Mock of a serial port."""
+    port = serial.tools.list_ports_common.ListPortInfo(DEFAULT_PORT)
+    port.serial_number = "1234"
+    port.manufacturer = "Virtual serial port"
+    port.device = DEFAULT_PORT
+    port.description = "Some serial port"
+
+    return port
