17fb862cfdd99ea6c210c36e7d3e6e7c00c57b71
chemelli74 <simone.chemelli@gmail.com>
2021-11-15 00:38:03 +0000

    Update status logic, switch to property

--- homeassistant/components/shelly/climate.py
+++ homeassistant/components/shelly/climate.py
@@ -10,11 +10,10 @@ import async_timeout
 
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
-    CURRENT_HVAC_COOL,
     CURRENT_HVAC_HEAT,
     CURRENT_HVAC_OFF,
     HVAC_MODE_AUTO,
-    HVAC_MODE_HEAT_COOL,
+    HVAC_MODE_HEAT,
     HVAC_MODE_OFF,
     SUPPORT_PRESET_MODE,
     SUPPORT_TARGET_TEMPERATURE,
@@ -26,6 +25,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.restore_state import RestoreEntity
 
 from .const import (
     AIOSHELLY_DEVICE_TIMEOUT_SEC,
@@ -57,10 +57,10 @@ async def async_setup_entry(
     async_add_entities(ShellyClimate(wrapper, block) for block in blocks)
 
 
-class ShellyClimate(ShellyBlockEntity, ClimateEntity):
+class ShellyClimate(ShellyBlockEntity, RestoreEntity, ClimateEntity):
     """Representation of a KNX climate device."""
 
-    _attr_hvac_modes = [HVAC_MODE_OFF, HVAC_MODE_HEAT_COOL, HVAC_MODE_AUTO]
+    _attr_hvac_modes = [HVAC_MODE_OFF, HVAC_MODE_HEAT, HVAC_MODE_AUTO]
     _attr_icon = "hass:thermostat"
     _attr_max_temp = SHTRV_01_TEMPERATURE_SETTINGS["max"]
     _attr_min_temp = SHTRV_01_TEMPERATURE_SETTINGS["min"]
@@ -76,31 +76,58 @@ class ShellyClimate(ShellyBlockEntity, ClimateEntity):
         self.block = block
         self.control_result: dict[str, Any] | None = None
 
-        target_temp = cast(float, block.targetTemp)
-        current_temp = cast(float, block.temp)
-        modes: list[str] = wrapper.device.settings["schedule_profile_names"]
-        modes.append("None")
-
-        self._attr_target_temperature = target_temp
-        self._attr_available = cast(bool, block.valveError)
-        self._attr_current_temperature = current_temp
         self._attr_name = self.wrapper.name
         self._attr_unique_id = f"{self.wrapper.mac}-climate"
-        self._attr_preset_modes = modes
+        self._attr_preset_modes: list[str] = wrapper.device.settings[
+            "schedule_profile_names"
+        ]
+        self._attr_preset_modes.insert(0, "None")
 
-        if hasattr(block, "mode") and block.mode != "none":
-            self._attr_hvac_mode = HVAC_MODE_AUTO
-            self._attr_preset_mode = modes[cast(int, block.mode)]
-        else:
-            self._attr_hvac_mode = HVAC_MODE_HEAT_COOL
-            self._attr_preset_mode = "None"
+    @property
+    def target_temperature(self) -> float | None:
+        """Set target temperature."""
+        return cast(float, self.block.targetTemp)
 
-        if current_temp <= self._attr_min_temp:
-            self._attr_hvac_action = CURRENT_HVAC_OFF
-        elif current_temp < target_temp:
-            self._attr_hvac_action = CURRENT_HVAC_HEAT
-        else:
-            self._attr_hvac_action = CURRENT_HVAC_COOL
+    @property
+    def current_temperature(self) -> float | None:
+        """Set current temperature."""
+        return cast(float, self.block.temp)
+
+    # @property
+    # def is_available(self) -> bool:
+    #     """Device a vailability."""
+    #     return not cast(bool, self.block.valveError)
+
+    @property
+    def hvac_mode(self) -> str:
+        """HVAC current mode."""
+        if not hasattr(self.block, "mode") or self._async_check_is_off():
+            return HVAC_MODE_OFF
+
+        if cast(int, self.block.mode) != 0:
+            return HVAC_MODE_AUTO
+        return HVAC_MODE_HEAT
+
+    @property
+    def preset_mode(self) -> str | None:
+        """Preset current mode."""
+        if not hasattr(self.block, "mode"):
+            return None
+        return self._attr_preset_modes[cast(int, self.block.mode)]
+
+    @property
+    def hvac_action(self) -> str | None:
+        """HVAC current action."""
+        if self._async_check_is_off():
+            return CURRENT_HVAC_OFF
+        return CURRENT_HVAC_HEAT
+
+    def _async_check_is_off(self) -> bool:
+        """Return if valve is off or on."""
+        return bool(
+            not self.target_temperature
+            or (self.target_temperature <= self._attr_min_temp)
+        )
 
     async def async_added_to_hass(self) -> None:
         """When entity is added to HASS."""
@@ -137,6 +164,12 @@ class ShellyClimate(ShellyBlockEntity, ClimateEntity):
         if (current_temp := kwargs.get(ATTR_TEMPERATURE)) is None:
             return
         await self.set_state(targetTemp=f"{current_temp}")
+        if self._async_check_is_off():
+            self._hvac_action = CURRENT_HVAC_OFF
+            self.set_hvac_mode(HVAC_MODE_OFF)
+        else:
+            self._hvac_action = CURRENT_HVAC_HEAT
+            self.set_hvac_mode(HVAC_MODE_AUTO if self.preset_mode else HVAC_MODE_HEAT)
         self.async_write_ha_state()
 
     async def async_set_hvac_mode(self, hvac_mode: str) -> None:
@@ -147,6 +180,7 @@ class ShellyClimate(ShellyBlockEntity, ClimateEntity):
             await self.set_state(schedule=0)
         if hvac_mode == HVAC_MODE_OFF:
             await self.set_state(targetTemp=f"{self._attr_min_temp}")
+            self._hvac_action = CURRENT_HVAC_OFF
         self.async_write_ha_state()
 
     async def async_set_preset_mode(self, preset_mode: str) -> None:
@@ -155,9 +189,10 @@ class ShellyClimate(ShellyBlockEntity, ClimateEntity):
             return
         if preset_mode == "None":
             await self.set_state(schedule=0)
-            await self.async_set_hvac_mode(HVAC_MODE_HEAT_COOL)
+            await self.async_set_hvac_mode(HVAC_MODE_HEAT)
         else:
             await self.set_state(
                 schedule_profile=f"{self._attr_preset_modes.index(preset_mode)}"
             )
+            await self.async_set_hvac_mode(HVAC_MODE_AUTO)
         self.async_write_ha_state()
