a145adf80f2e8bc5594b914cde25e56ab5a720a3
Arto Jantunen <viiru@iki.fi>
2021-11-15 08:44:50 +0200

    Import the library get_uuid under a different name
    There are a few options here:
    
    1. Rename the get_uuid method with a synonym
    2. Import get_uuid under a different name
    3. Convert get_uuid into a property
    4. Rename get_uuid in the Vallox library
    
    None of these options is that appealing. I'll start with option two,
    anyways.

--- homeassistant/components/vallox/__init__.py
+++ homeassistant/components/vallox/__init__.py
@@ -9,7 +9,7 @@ from uuid import UUID
 
 from vallox_websocket_api import PROFILE as VALLOX_PROFILE, Vallox
 from vallox_websocket_api.exceptions import ValloxApiException
-from vallox_websocket_api.vallox import get_uuid
+from vallox_websocket_api.vallox import get_uuid as calculate_uuid
 import voluptuous as vol
 
 from homeassistant.const import CONF_HOST, CONF_NAME, EVENT_HOMEASSISTANT_STARTED
@@ -118,7 +118,7 @@ class ValloxState:
 
     def get_uuid(self) -> UUID | None:
         """Return cached UUID value."""
-        uuid = get_uuid(self.metric_cache)
+        uuid = calculate_uuid(self.metric_cache)
         if not isinstance(uuid, UUID):
             raise ValueError
         return uuid
