97782c92fe14eabb7e2b23e478648a219c060313
Daniel Hjelseth Høyer <github@dahoiv.net>
2021-11-19 07:51:53 +0100

    Correct supported_features and model for Mill
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- homeassistant/components/tibber/sensor.py
+++ homeassistant/components/tibber/sensor.py
@@ -98,6 +98,12 @@ RT_SENSORS: tuple[SensorEntityDescription, ...] = (
         state_class=STATE_CLASS_TOTAL_INCREASING,
     ),
     SensorEntityDescription(
+        key="estimatedHourConsumption",
+        name="Estimated consumption current hour",
+        device_class=DEVICE_CLASS_ENERGY,
+        native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+    ),
+    SensorEntityDescription(
         key="accumulatedProduction",
         name="accumulated production",
         device_class=DEVICE_CLASS_ENERGY,
