2aa8c2cf74e2d2c2a41c87ed1a37739e469d9366
epenet <6771947+epenet@users.noreply.github.com>
2021-11-19 12:19:37 +0100

    Use DhcpServiceInfo in gogogate2 (#59968)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/gogogate2/config_flow.py
+++ homeassistant/components/gogogate2/config_flow.py
@@ -7,8 +7,7 @@ from ismartgate.const import GogoGate2ApiErrorCode, ISmartGateApiErrorCode
 import voluptuous as vol
 
 from homeassistant import data_entry_flow
-from homeassistant.components import zeroconf
-from homeassistant.components.dhcp import IP_ADDRESS, MAC_ADDRESS
+from homeassistant.components import dhcp, zeroconf
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import (
     CONF_DEVICE,
@@ -43,10 +42,12 @@ class Gogogate2FlowHandler(ConfigFlow, domain=DOMAIN):
         await self.async_set_unique_id(discovery_info[zeroconf.ATTR_PROPERTIES]["id"])
         return await self._async_discovery_handler(discovery_info[zeroconf.ATTR_HOST])
 
-    async def async_step_dhcp(self, discovery_info):
+    async def async_step_dhcp(
+        self, discovery_info: dhcp.DhcpServiceInfo
+    ) -> data_entry_flow.FlowResult:
         """Handle dhcp discovery."""
-        await self.async_set_unique_id(discovery_info[MAC_ADDRESS])
-        return await self._async_discovery_handler(discovery_info[IP_ADDRESS])
+        await self.async_set_unique_id(discovery_info[dhcp.MAC_ADDRESS])
+        return await self._async_discovery_handler(discovery_info[dhcp.IP_ADDRESS])
 
     async def _async_discovery_handler(self, ip_address):
         """Start the user flow from any discovery."""
