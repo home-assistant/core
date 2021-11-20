01ae176b92d1932efb9208d0d19b58728edd363f
Hans Oischinger <hans.oischinger@gmail.com>
2021-11-20 21:18:22 +0100

    More tests for vicare yaml import

--- tests/components/vicare/test_config_flow.py
+++ tests/components/vicare/test_config_flow.py
@@ -5,7 +5,11 @@ from PyViCare.PyViCareUtils import PyViCareInvalidCredentialsError
 
 from homeassistant import config_entries, data_entry_flow, setup
 from homeassistant.components import dhcp
-from homeassistant.components.vicare.const import DOMAIN
+from homeassistant.components.vicare.const import (
+    CONF_CIRCUIT,
+    CONF_HEATING_TYPE,
+    DOMAIN,
+)
 from homeassistant.const import CONF_CLIENT_ID, CONF_PASSWORD, CONF_USERNAME
 
 from . import ENTRY_CONFIG, MOCK_MAC
@@ -61,6 +65,62 @@ async def test_import(hass):
         "homeassistant.components.vicare.async_setup_entry",
         return_value=True,
     ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_IMPORT},
+            data=ENTRY_CONFIG,
+        )
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result["title"] == "Configuration.yaml"
+        assert result["data"] == ENTRY_CONFIG
+
+        await hass.async_block_till_done()
+        assert len(mock_setup.mock_calls) == 1
+        assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_import_removes_circuit(hass):
+    """Test that the import works."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+
+    with patch(
+        "homeassistant.components.vicare.config_flow.vicare_login",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.vicare.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.vicare.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        ENTRY_CONFIG[CONF_CIRCUIT] = 1
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_IMPORT},
+            data=ENTRY_CONFIG,
+        )
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result["title"] == "Configuration.yaml"
+        assert result["data"] == ENTRY_CONFIG
+
+        await hass.async_block_till_done()
+        assert len(mock_setup.mock_calls) == 1
+        assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_import_adds_heating_type(hass):
+    """Test that the import works."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+
+    with patch(
+        "homeassistant.components.vicare.config_flow.vicare_login",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.vicare.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "homeassistant.components.vicare.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        del ENTRY_CONFIG[CONF_HEATING_TYPE]
         result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_IMPORT},
