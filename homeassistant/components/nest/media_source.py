d5355340ff1b0c18dc62b645b69e0413e2bc1373
Allen Porter <allen@thebends.org>
2021-11-20 23:35:35 -0800

    Address pylint errors and warnings

--- homeassistant/components/nest/media_source.py
+++ homeassistant/components/nest/media_source.py
@@ -16,10 +16,12 @@ For additional background on Nest Camera events see:
 https://developers.google.com/nest/device-access/api/camera#handle_camera_events
 """
 
-from collections import Mapping, OrderedDict
+from __future__ import annotations
+
+from collections import OrderedDict
+from collections.abc import Mapping
 from dataclasses import dataclass
 import logging
-from typing import Optional
 
 from google_nest_sdm.camera_traits import CameraClipPreviewTrait, CameraEventImageTrait
 from google_nest_sdm.device import Device
@@ -85,7 +87,7 @@ class MediaId:
     """
 
     device_id: str
-    event_id: Optional[str] = None
+    event_id: str | None = None
 
     @property
     def identifier(self) -> str:
@@ -95,7 +97,7 @@ class MediaId:
         return self.device_id
 
 
-def parse_media_id(identifier: Optional[str]) -> Optional[MediaId]:
+def parse_media_id(identifier: str | None = None) -> MediaId | None:
     """Parse the identifier path string into a MediaId."""
     if identifier is None or identifier == "":
         return None
@@ -125,7 +127,7 @@ class NestMediaSource(MediaSource):
 
     async def async_resolve_media(self, item: MediaSourceItem) -> PlayMedia:
         """Resolve media identifier to a url."""
-        media_id: Optional[MediaId] = parse_media_id(item.identifier)
+        media_id: MediaId | None = parse_media_id(item.identifier)
         if not media_id:
             raise Unresolvable("No identifier specified for MediaSourceItem")
         if not media_id.event_id:
@@ -154,18 +156,18 @@ class NestMediaSource(MediaSource):
         The top level is the root that contains devices. Inside each device are
         media for events for that device.
         """
-        media_id: Optional[MediaId] = parse_media_id(item.identifier)
+        media_id: MediaId | None = parse_media_id(item.identifier)
         _LOGGER.debug(
             "Browsing media for identifier=%s, media_id=%s", item.identifier, media_id
         )
         devices = await self.devices()
         if media_id is None:
             # Browse the root and return child devices
-            browse_root = self._browse_root()
+            browse_root = _browse_root()
             browse_root.children = []
             for device_id, child_device in devices.items():
                 browse_root.children.append(
-                    self._browse_device(MediaId(device_id), child_device)
+                    _browse_device(MediaId(device_id), child_device)
                 )
             return browse_root
 
@@ -176,13 +178,13 @@ class NestMediaSource(MediaSource):
             )
         if media_id.event_id is None:
             # Browse a specific device and return child events
-            browse_device = self._browse_device(media_id, device)
+            browse_device = _browse_device(media_id, device)
             browse_device.children = []
             events = self._get_events(device)
             for child_event in events.values():
                 event_id = MediaId(media_id.device_id, child_event.event_id)
                 browse_device.children.append(
-                    self._browse_event(event_id, device, child_event)
+                    _browse_event(event_id, device, child_event)
                 )
             return browse_device
 
@@ -192,57 +194,7 @@ class NestMediaSource(MediaSource):
             raise BrowseError(
                 "Unable to find event with identiifer: %s" % item.identifier
             )
-        return self._browse_event(media_id, device, event)
-
-    def _browse_root(self) -> BrowseMediaSource:
-        """Return devices in the root."""
-        return BrowseMediaSource(
-            domain=DOMAIN,
-            identifier="",
-            media_class=MEDIA_CLASS_DIRECTORY,
-            media_content_type=MEDIA_TYPE_VIDEO,
-            children_media_class=MEDIA_CLASS_VIDEO,
-            title=MEDIA_SOURCE_TITLE,
-            can_play=False,
-            can_expand=True,
-            thumbnail=None,
-            children=[],
-        )
-
-    def _browse_device(self, device_id: MediaId, device: Device) -> BrowseMediaSource:
-        """Return details for the specified device."""
-        device_info = NestDeviceInfo(device)
-        return BrowseMediaSource(
-            domain=DOMAIN,
-            identifier=device_id.identifier,
-            media_class=MEDIA_CLASS_DIRECTORY,
-            media_content_type=MEDIA_TYPE_VIDEO,
-            children_media_class=MEDIA_CLASS_VIDEO,
-            title=DEVICE_TITLE_FORMAT.format(device_name=device_info.device_name),
-            can_play=False,
-            can_expand=True,
-            thumbnail=None,
-            children=[],
-        )
-
-    def _browse_event(
-        self, event_id: MediaId, device: Device, event: ImageEventBase
-    ) -> BrowseMediaSource:
-        """Build a BrowseMediaSource for a specific event."""
-        return BrowseMediaSource(
-            domain=DOMAIN,
-            identifier=event_id.identifier,
-            media_class=MEDIA_CLASS_IMAGE,
-            media_content_type=MEDIA_TYPE_IMAGE,
-            title=CLIP_TITLE_FORMAT.format(
-                event_name=MEDIA_SOURCE_EVENT_TITLE_MAP.get(event.event_type, "Event"),
-                event_time=dt_util.as_local(event.timestamp),
-            ),
-            can_play=True,
-            can_expand=False,
-            thumbnail=None,
-            children=[],
-        )
+        return _browse_event(media_id, device, event)
 
     async def devices(self) -> Mapping[str, Device]:
         """Return all event media related devices."""
@@ -251,3 +203,56 @@ class NestMediaSource(MediaSource):
     def _get_events(self, device: Device) -> Mapping[str, ImageEventBase]:
         """Return relevant events for the specified device."""
         return OrderedDict({e.event_id: e for e in device.event_media_manager.events})
+
+
+def _browse_root() -> BrowseMediaSource:
+    """Return devices in the root."""
+    return BrowseMediaSource(
+        domain=DOMAIN,
+        identifier="",
+        media_class=MEDIA_CLASS_DIRECTORY,
+        media_content_type=MEDIA_TYPE_VIDEO,
+        children_media_class=MEDIA_CLASS_VIDEO,
+        title=MEDIA_SOURCE_TITLE,
+        can_play=False,
+        can_expand=True,
+        thumbnail=None,
+        children=[],
+    )
+
+
+def _browse_device(device_id: MediaId, device: Device) -> BrowseMediaSource:
+    """Return details for the specified device."""
+    device_info = NestDeviceInfo(device)
+    return BrowseMediaSource(
+        domain=DOMAIN,
+        identifier=device_id.identifier,
+        media_class=MEDIA_CLASS_DIRECTORY,
+        media_content_type=MEDIA_TYPE_VIDEO,
+        children_media_class=MEDIA_CLASS_VIDEO,
+        title=DEVICE_TITLE_FORMAT.format(device_name=device_info.device_name),
+        can_play=False,
+        can_expand=True,
+        thumbnail=None,
+        children=[],
+    )
+
+
+def _browse_event(
+    event_id: MediaId, device: Device, event: ImageEventBase
+) -> BrowseMediaSource:
+    """Build a BrowseMediaSource for a specific event."""
+    return BrowseMediaSource(
+        domain=DOMAIN,
+        identifier=event_id.identifier,
+        media_class=MEDIA_CLASS_IMAGE,
+        media_content_type=MEDIA_TYPE_IMAGE,
+        title=CLIP_TITLE_FORMAT.format(
+            event_name=MEDIA_SOURCE_EVENT_TITLE_MAP.get(event.event_type, "Event"),
+            event_time=dt_util.as_local(event.timestamp),
+        ),
+        can_play=True,
+        can_expand=False,
+        thumbnail=None,
+        children=[],
+    )
