efb35a51c41d68e473fe7a0b6fb1396f5410d228
epenet <epenet@users.noreply.github.com>
2021-11-22 14:45:16 +0000

    Adjust warning

--- homeassistant/components/usb/__init__.py
+++ homeassistant/components/usb/__init__.py
@@ -49,7 +49,10 @@ class UsbServiceInfo:
         Deprecated, and will be removed in version 2022.6.
         """
         _LOGGER.warning(
-            "__getitem__ will fail in version 2022.6. Please use <cls>.<name> instead."
+            "Accessing ['%s'] will fail in version 2022.6. Please use %s.%s instead.",
+            name,
+            self,
+            name,
         )
         return getattr(self, name)
 
