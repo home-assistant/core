77c4183aef7fd31f0ab9d9831daad1060f653278
jugla <59493499+jugla@users.noreply.github.com>
2021-11-22 17:54:46 +0100

    code format and add a function for readibility

--- homeassistant/components/atome/sensor.py
+++ homeassistant/components/atome/sensor.py
@@ -155,6 +155,7 @@ class AtomeLiveSensor(AtomeGenericSensor):
 
     @property
     def state(self):
+        """Return the state of this device."""
         _LOGGER.debug("Live Data : display")
         return self._live_power
 
@@ -233,7 +234,13 @@ class AtomePeriodSensor(RestoreEntity, AtomeGenericSensor):
 
     @property
     def state(self):
+        """Return the state of this device."""
         return self._period_usage
+
+    #This function can be instanciated
+    def update_period_usage(self):
+        """Return current daily power usage."""
+        self._retrieve_period_usage_with_retry()
 
     async def async_update(self):
         """Fetch new state data for this sensor."""
