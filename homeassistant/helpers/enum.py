425df3b30bf4d2bd0ea23d5f26116128dca7c5a2
Ville Skyttä <ville.skytta@iki.fi>
2021-11-22 17:16:25 +0200

    Implement _generate_next_value_ to make auto() explicitly unsupported for now

--- homeassistant/helpers/enum.py
+++ homeassistant/helpers/enum.py
@@ -10,8 +10,6 @@ class StrEnum(str, Enum):
 
     def __new__(cls, value: str, *args: Any, **kwargs: Any) -> StrEnum:
         """Create a new StrEnum instance."""
-        # enum.auto intentionally not backported yet, waiting until it's
-        # very clear that its implementation will no longer change.
         if not isinstance(value, str):
             raise TypeError(f"{value!r} is not a string")
         return super().__new__(cls, value, *args, **kwargs)  # type: ignore[call-overload,no-any-return]
@@ -19,3 +17,15 @@ class StrEnum(str, Enum):
     def __str__(self) -> str:
         """Return self.value."""
         return str(self.value)
+
+    @staticmethod
+    def _generate_next_value_(  # pylint: disable=arguments-differ # https://github.com/PyCQA/pylint/issues/5371
+        name: str, start: int, count: int, last_values: list[Any]
+    ) -> Any:
+        """
+        Make `auto()` explicitly unsupported.
+
+        We may revisit this when it's very clear that Python 3.11's
+        `StrEnum.auto()` behavior will no longer change.
+        """
+        raise TypeError("auto() is not supported by this implementation")
