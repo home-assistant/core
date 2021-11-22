d0c2b50d9d545814db9556956529cf6b51f81f3b
Franck Nijhof <git@frenck.dev>
2021-11-23 00:46:51 +0100

    Add button support to HomeKit (#60165)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- tests/components/homekit/test_type_switches.py
+++ tests/components/homekit/test_type_switches.py
@@ -449,3 +449,46 @@ async def test_input_select_switch(hass, hk_driver, events, domain):
     assert acc.select_chars["option1"].value is False
     assert acc.select_chars["option2"].value is False
     assert acc.select_chars["option3"].value is False
+
+
+async def test_button_switch(hass, hk_driver, events):
+    """Test switch accessory from a button entity."""
+    domain = "button"
+    entity_id = "button.test"
+
+    hass.states.async_set(entity_id, None)
+    await hass.async_block_till_done()
+    acc = Switch(hass, hk_driver, "Switch", entity_id, 2, None)
+    await acc.run()
+    await hass.async_block_till_done()
+
+    assert acc.activate_only is True
+    assert acc.char_on.value is False
+
+    call_press = async_mock_service(hass, domain, "press")
+
+    acc.char_on.client_update_value(True)
+    await hass.async_block_till_done()
+    assert acc.char_on.value is True
+    assert len(call_press) == 1
+    assert call_press[0].data[ATTR_ENTITY_ID] == entity_id
+    assert len(events) == 1
+    assert events[-1].data[ATTR_VALUE] is None
+
+    future = dt_util.utcnow() + timedelta(seconds=1)
+    async_fire_time_changed(hass, future)
+    await hass.async_block_till_done()
+    assert acc.char_on.value is True
+
+    future = dt_util.utcnow() + timedelta(seconds=10)
+    async_fire_time_changed(hass, future)
+    await hass.async_block_till_done()
+    assert acc.char_on.value is False
+
+    assert len(events) == 1
+    assert len(call_press) == 1
+
+    acc.char_on.client_update_value(False)
+    await hass.async_block_till_done()
+    assert acc.char_on.value is False
+    assert len(events) == 1
