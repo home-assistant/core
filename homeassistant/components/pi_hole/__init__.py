1e4e1e9641d460e48c357fdc1fcf647e05e9dfb4
Andreas Brett <andreasbrett@users.noreply.github.com>
2021-11-23 11:25:49 +0100

    Update homeassistant/components/pi_hole/__init__.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/pi_hole/__init__.py
+++ homeassistant/components/pi_hole/__init__.py
@@ -153,7 +153,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 @callback
 def _async_platforms(entry: ConfigEntry) -> list[str]:
     """Return platforms to be loaded / unloaded."""
-    platforms = ["sensor", "binary_sensor"]
+    platforms = ["binary_sensor", "sensor"]
     if not entry.data[CONF_STATISTICS_ONLY]:
         platforms.append("switch")
     return platforms
