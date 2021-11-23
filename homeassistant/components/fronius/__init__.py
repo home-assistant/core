2e4d50327bc720b6fc15139c69f4e612241006a3
Matthias Alphart <farmio@alphart.net>
2021-11-23 17:06:59 +0100

    Add devices to Fronius integration (#60104)

--- homeassistant/components/fronius/__init__.py
+++ homeassistant/components/fronius/__init__.py
@@ -9,16 +9,19 @@ from typing import Callable, TypeVar
 from pyfronius import Fronius, FroniusError
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST
+from homeassistant.const import ATTR_MODEL, ATTR_SW_VERSION, CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.entity import DeviceInfo
 
 from .const import (
     DEFAULT_UPDATE_INTERVAL,
     DEFAULT_UPDATE_INTERVAL_LOGGER,
     DEFAULT_UPDATE_INTERVAL_POWER_FLOW,
     DOMAIN,
+    SOLAR_NET_ID_SYSTEM,
     FroniusDeviceInfo,
 )
 from .coordinator import (
@@ -82,6 +85,7 @@ class FroniusSolarNet:
         # entry.unique_id is either logger uid or first inverter uid if no logger available
         # prepended by "solar_net_" to have individual device for whole system (power_flow)
         self.solar_net_device_id = f"solar_net_{entry.unique_id}"
+        self.system_device_info: DeviceInfo | None = None
 
         self.inverter_coordinators: list[FroniusInverterUpdateCoordinator] = []
         self.logger_coordinator: FroniusLoggerUpdateCoordinator | None = None
@@ -100,6 +104,9 @@ class FroniusSolarNet:
                 update_interval=timedelta(seconds=DEFAULT_UPDATE_INTERVAL_LOGGER),
             )
             await self.logger_coordinator.async_config_entry_first_refresh()
+
+        # _create_solar_net_device uses data from self.logger_coordinator when available
+        self.system_device_info = await self._create_solar_net_device()
 
         _inverter_infos = await self._get_inverter_infos()
         for inverter_info in _inverter_infos:
@@ -144,6 +151,28 @@ class FroniusSolarNet:
             )
         )
 
+    async def _create_solar_net_device(self) -> DeviceInfo:
+        """Create a device for the Fronius SolarNet system."""
+        solar_net_device: DeviceInfo = DeviceInfo(
+            configuration_url=self.host,
+            identifiers={(DOMAIN, self.solar_net_device_id)},
+            manufacturer="Fronius",
+            name="SolarNet",
+        )
+        if self.logger_coordinator:
+            _logger_info = self.logger_coordinator.data[SOLAR_NET_ID_SYSTEM]
+            solar_net_device[ATTR_MODEL] = _logger_info["product_type"]["value"]
+            solar_net_device[ATTR_SW_VERSION] = _logger_info["software_version"][
+                "value"
+            ]
+
+        device_registry = await dr.async_get_registry(self.hass)
+        device_registry.async_get_or_create(
+            config_entry_id=self.config_entry.entry_id,
+            **solar_net_device,
+        )
+        return solar_net_device
+
     async def _get_inverter_infos(self) -> list[FroniusDeviceInfo]:
         """Get information about the inverters in the SolarNet system."""
         try:
@@ -155,8 +184,18 @@ class FroniusSolarNet:
         for inverter in _inverter_info["inverters"]:
             solar_net_id = inverter["device_id"]["value"]
             unique_id = inverter["unique_id"]["value"]
+            device_info = DeviceInfo(
+                identifiers={(DOMAIN, unique_id)},
+                manufacturer=inverter["device_type"].get("manufacturer", "Fronius"),
+                model=inverter["device_type"].get(
+                    "model", inverter["device_type"]["value"]
+                ),
+                name=inverter.get("custom_name", {}).get("value"),
+                via_device=(DOMAIN, self.solar_net_device_id),
+            )
             inverter_infos.append(
                 FroniusDeviceInfo(
+                    device_info=device_info,
                     solar_net_id=solar_net_id,
                     unique_id=unique_id,
                 )
