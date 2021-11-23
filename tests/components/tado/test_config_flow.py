8917592241b67895fdf9f9a9a84204821d0cef18
epenet <epenet@users.noreply.github.com>
2021-11-23 15:23:47 +0000

    Update ZeroconfServiceInfo in tests (n-t)

--- tests/components/tado/test_config_flow.py
+++ tests/components/tado/test_config_flow.py
@@ -128,7 +128,12 @@ async def test_form_homekit(hass):
         DOMAIN,
         context={"source": config_entries.SOURCE_HOMEKIT},
         data=zeroconf.ZeroconfServiceInfo(
-            properties={zeroconf.ATTR_PROPERTIES_ID: "AA:BB:CC:DD:EE:FF"}
+            host="mock_host",
+            hostname="mock_hostname",
+            name="mock_name",
+            port=None,
+            properties={zeroconf.ATTR_PROPERTIES_ID: "AA:BB:CC:DD:EE:FF"},
+            type="mock_type",
         ),
     )
     assert result["type"] == "form"
@@ -149,7 +154,12 @@ async def test_form_homekit(hass):
         DOMAIN,
         context={"source": config_entries.SOURCE_HOMEKIT},
         data=zeroconf.ZeroconfServiceInfo(
-            properties={zeroconf.ATTR_PROPERTIES_ID: "AA:BB:CC:DD:EE:FF"}
+            host="mock_host",
+            hostname="mock_hostname",
+            name="mock_name",
+            port=None,
+            properties={zeroconf.ATTR_PROPERTIES_ID: "AA:BB:CC:DD:EE:FF"},
+            type="mock_type",
         ),
     )
     assert result["type"] == "abort"
