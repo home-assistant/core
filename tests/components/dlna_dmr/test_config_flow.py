9fa6daf47a5d60e1584bff50c3a1d42da1b6a6f7
Michael Chisholm <chishm@chishm.com>
2021-11-24 09:22:34 +1100

    dlna_dmr: Ignore philips tv (#60204)

--- tests/components/dlna_dmr/test_config_flow.py
+++ tests/components/dlna_dmr/test_config_flow.py
@@ -558,6 +558,21 @@ async def test_ssdp_flow_existing(
     assert config_entry_mock.data[CONF_URL] == NEW_DEVICE_LOCATION
 
 
+async def test_ssdp_flow_duplicate_location(
+    hass: HomeAssistant, config_entry_mock: MockConfigEntry
+) -> None:
+    """Test that discovery of device with URL matching existing entry gets aborted."""
+    config_entry_mock.add_to_hass(hass)
+    result = await hass.config_entries.flow.async_init(
+        DLNA_DOMAIN,
+        context={"source": config_entries.SOURCE_SSDP},
+        data=MOCK_DISCOVERY,
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "already_configured"
+    assert config_entry_mock.data[CONF_URL] == MOCK_DEVICE_LOCATION
+
+
 async def test_ssdp_flow_upnp_udn(
     hass: HomeAssistant, config_entry_mock: MockConfigEntry
 ) -> None:
@@ -635,6 +650,7 @@ async def test_ssdp_ignore_device(hass: HomeAssistant) -> None:
         ("XBMC Foundation", "Kodi"),
         ("Samsung", "Smart TV"),
         ("LG Electronics.", "LG TV"),
+        ("Royal Philips Electronics", "Philips TV DMR"),
     ]:
         discovery = dict(MOCK_DISCOVERY)
         discovery[ssdp.ATTR_UPNP_MANUFACTURER] = manufacturer
