fe8f3da90a5d407e9f5a9ec5559727f7ca2f3999
garbled1 <root@garbled.net>
2021-11-24 03:39:55 -0700

    Put this back to time.time, we are comparing against the api, and the api is not monotonic (yet). When I bump the API to add this, will come back to replace this call.

--- homeassistant/components/balboa/entity.py
+++ homeassistant/components/balboa/entity.py
@@ -49,7 +49,7 @@ class BalboaEntity(Entity):
     @property
     def assumed_state(self) -> bool:
         """Return whether the state is based on actual reading from device."""
-        if (self._client.lastupd + 5 * 60) < time.monotonic():
+        if (self._client.lastupd + 5 * 60) < time.time():
             return True
         return False
 
