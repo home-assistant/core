800a669a7217f9a7b92a3d8ae336df5c00cff27a
J. Nick Koston <nick@koston.org>
2021-11-24 16:46:22 -0600

    Use random effect from flux_led library
    - Various cleanups

--- homeassistant/components/flux_led/const.py
+++ homeassistant/components/flux_led/const.py
@@ -71,9 +71,6 @@ CONF_SPEED_PCT: Final = "speed_pct"
 CONF_TRANSITION: Final = "transition"
 
 
-EFFECT_SUPPORT_MODES = {COLOR_MODE_RGB, COLOR_MODE_RGBW, COLOR_MODE_RGBWW}
-
-
 CONF_CUSTOM_EFFECT_COLORS: Final = "custom_effect_colors"
 CONF_CUSTOM_EFFECT_SPEED_PCT: Final = "custom_effect_speed_pct"
 CONF_CUSTOM_EFFECT_TRANSITION: Final = "custom_effect_transition"
