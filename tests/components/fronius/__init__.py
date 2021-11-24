3dac6614807808efe2b8a5464232fe5350a11e99
Matthias Alphart <farmio@alphart.net>
2021-11-24 02:04:36 +0100

    Rewrite Fronius integration (#59686)
    * Add unique_id and use DataUpdateCoordinator in Fronius (#57879)
    
    * initial refactoring commit - meters
    
    - config_flow (no strings, no tests yet)
    - import yaml config
    - FroniusSolarNet class for holding Fronius object , coordinators and some common data
    - meter descriptions
    - update coordinator
    - entities (including devices)
    
    * storage controllers
    
    * error handling on init; inverter unique_id
    
    * inverters
    
    * power_flow
    
    * fix VA, var, varh not valid for device_class power/energy
    
    and add custom icons
    
    * add SolarNet device for system wide values
    
    * cleanup
    
    * config_flow strings
    
    * test config_flow
    
    * use pyfronius 0.7.0
    
    * enable strict typing
    
    * remove TODO comments
    
    * fix lint errors; move FroniusSensorEntity to sensor.py
    
    * power_flow as optional coordinator
    
    API V0 doesn't support power_flow endpoint
    
    * show error message in logs
    
    * prevent parallel requests to one host
    
    * logger_info coordinator
    
    * store FroniusSolarNet reference directly in coordinator
    
    * cleanup coordinators when unloading entry
    
    * round floats returned by Fronius API
    
    * default icons for grid im/export tariffs
    
    * small typing fix
    
    * Update homeassistant/components/fronius/sensor.py
    
    Co-authored-by: Brett Adams <Bre77@users.noreply.github.com>
    
    * DC icons
    
    * prepend names with "Fronius" and device type
    
    to get more reasonable default entity_ids (eg. have them next to each other when alphabetically sorted)
    
    * remove config_flow and devices
    
    * rename _FroniusUpdateCoordinator to FroniusCoordinatorBase
    
    and mark ABC
    
    * move SensorEntityDescriptions to sensor.py
    
    * Revert "move SensorEntityDescriptions to sensor.py"
    
    This reverts commit 2e5a726eb65854f236a0c72f3f67f04a6f8a2eff.
    
    * Don't raise ConfigEntryNotReady and use regular refresh method
    
    * move bridge initialization out of helper class
    
    * no coverage tests
    
    * power_flow update interval 10 seconds
    
    * move SensorEntityDescriptions to sensor.py
    
    without introducing a circular dependency
    
    * deprecation warning for CONF_MONITORED_CONDITIONS
    
    * remove extra_state_attributes form meter sensor entities
    
    * readd diagnostic entities
    
    * decouple default entity_id from default name
    
    * use key instead of name for entity_id
    
    and make deprecated config key optional
    
    * adjust tests
    
    * use old entity_ids
    
    these changes are now backwards compatible
    
    * check coverage
    
    * simplify entity description definitions
    
    * restore entity names of previous implementation
    
    Co-authored-by: Brett Adams <Bre77@users.noreply.github.com>
    
    * Add config_flow for Fronius integration (#59677)
    
    * Cleanup Fronius config_flow and tests (#60094)
    
    * Add devices to Fronius integration (#60104)
    
    * New entity names for Fronius entities (#60215)
    
    * Adaptive update interval for Fronius coordinators (#60192)
    
    Co-authored-by: Brett Adams <Bre77@users.noreply.github.com>

--- tests/components/fronius/__init__.py
+++ tests/components/fronius/__init__.py
@@ -1,23 +1,72 @@
 """Tests for the Fronius integration."""
+from homeassistant.components.fronius.const import DOMAIN
+from homeassistant.const import CONF_HOST
 
-from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
-from homeassistant.const import CONF_MONITORED_CONDITIONS, CONF_RESOURCE
-from homeassistant.setup import async_setup_component
+from tests.common import MockConfigEntry, load_fixture
+from tests.test_util.aiohttp import AiohttpClientMocker
 
-from .const import DOMAIN, MOCK_HOST
+MOCK_HOST = "http://fronius"
+MOCK_UID = "123.4567890"  # has to match mocked logger unique_id
 
 
-async def setup_fronius_integration(hass, devices):
+async def setup_fronius_integration(hass):
     """Create the Fronius integration."""
-    assert await async_setup_component(
-        hass,
-        SENSOR_DOMAIN,
-        {
-            SENSOR_DOMAIN: {
-                "platform": DOMAIN,
-                CONF_RESOURCE: MOCK_HOST,
-                CONF_MONITORED_CONDITIONS: devices,
-            }
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id=MOCK_UID,
+        data={
+            CONF_HOST: MOCK_HOST,
+            "is_logger": True,
         },
     )
+    entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
+
+
+def mock_responses(
+    aioclient_mock: AiohttpClientMocker,
+    host: str = MOCK_HOST,
+    night: bool = False,
+) -> None:
+    """Mock responses for Fronius Symo inverter with meter."""
+    aioclient_mock.clear_requests()
+    _day_or_night = "night" if night else "day"
+
+    aioclient_mock.get(
+        f"{host}/solar_api/GetAPIVersion.cgi",
+        text=load_fixture("symo/GetAPIVersion.json", "fronius"),
+    )
+    aioclient_mock.get(
+        f"{host}/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&"
+        "DeviceId=1&DataCollection=CommonInverterData",
+        text=load_fixture(
+            f"symo/GetInverterRealtimeDate_Device_1_{_day_or_night}.json", "fronius"
+        ),
+    )
+    aioclient_mock.get(
+        f"{host}/solar_api/v1/GetInverterInfo.cgi",
+        text=load_fixture("symo/GetInverterInfo.json", "fronius"),
+    )
+    aioclient_mock.get(
+        f"{host}/solar_api/v1/GetLoggerInfo.cgi",
+        text=load_fixture("symo/GetLoggerInfo.json", "fronius"),
+    )
+    aioclient_mock.get(
+        f"{host}/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0",
+        text=load_fixture("symo/GetMeterRealtimeData_Device_0.json", "fronius"),
+    )
+    aioclient_mock.get(
+        f"{host}/solar_api/v1/GetMeterRealtimeData.cgi?Scope=System",
+        text=load_fixture("symo/GetMeterRealtimeData_System.json", "fronius"),
+    )
+    aioclient_mock.get(
+        f"{host}/solar_api/v1/GetPowerFlowRealtimeData.fcgi",
+        text=load_fixture(
+            f"symo/GetPowerFlowRealtimeData_{_day_or_night}.json", "fronius"
+        ),
+    )
+    aioclient_mock.get(
+        f"{host}/solar_api/v1/GetStorageRealtimeData.cgi?Scope=System",
+        text=load_fixture("symo/GetStorageRealtimeData_System.json", "fronius"),
+    )
