3d5e47deac5a0efd666edf5f9511247f31ce4ddb
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-11-25 22:38:41 +0100

    Import Callable from collections.abc

--- homeassistant/components/xiaomi_miio/binary_sensor.py
+++ homeassistant/components/xiaomi_miio/binary_sensor.py
@@ -1,9 +1,9 @@
 """Support for Xiaomi Miio binary sensors."""
 from __future__ import annotations
 
+from collections.abc import Callable
 from dataclasses import dataclass
 import logging
-from typing import Callable
 
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_CONNECTIVITY,
