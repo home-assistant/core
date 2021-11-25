c9bcdb1205f3729d3c9104314227a221f492c515
Tuen Lee <leeyuentuen@gmail.com>
2021-11-25 16:37:41 +0000

    fix comments

--- homeassistant/components/tuya/alarm_control_panel.py
+++ homeassistant/components/tuya/alarm_control_panel.py
@@ -2,15 +2,23 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-import logging
 
 from tuya_iot import TuyaDevice, TuyaDeviceManager
 
 from homeassistant.components.alarm_control_panel import (
+    SUPPORT_ALARM_ARM_AWAY,
+    SUPPORT_ALARM_ARM_HOME,
+    SUPPORT_ALARM_TRIGGER,
     AlarmControlPanelEntity,
     AlarmControlPanelEntityDescription,
 )
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    STATE_ALARM_ARMED_AWAY,
+    STATE_ALARM_ARMED_HOME,
+    STATE_ALARM_DISARMED,
+    STATE_ALARM_TRIGGERED,
+)
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -19,37 +27,30 @@ from . import HomeAssistantTuyaData
 from .base import TuyaEntity
 from .const import DOMAIN, TUYA_DISCOVERY_NEW, DPCode
 
-_LOGGER = logging.getLogger(__name__)
-
 ICON = "mdi:security"
 
-SUPPORT_ALARM_ARM_HOME = 1
-SUPPORT_ALARM_ARM_AWAY = 2
-SUPPORT_ALARM_ARM_NIGHT = 4
-SUPPORT_ALARM_TRIGGER = 8
-SUPPORT_ALARM_ARM_CUSTOM_BYPASS = 16
-SUPPORT_ALARM_ARM_VACATION = 32
+MODE_ALARM_ARM = "arm"
+MODE_ALARM_DISARMED = "disarmed"
+MODE_ALARM_HOME = "home"
+MODE_ALARM_SOS = "sos"
 
 
 @dataclass
-class TuyaAlarmEntityDescription(AlarmControlPanelEntityDescription):
-    """Describe an Tuya cover entity."""
-
-    battery_percentage: DPCode | None = None
+class TuyaAlarmControlPanelEntityDescription(AlarmControlPanelEntityDescription):
+    """Describe an Tuya Alarm entity."""
 
 
 # All descriptions can be found here:
 # https://developer.tuya.com/en/docs/iot/standarddescription?id=K9i5ql6waswzq
-ALARM: dict[str, tuple[TuyaAlarmEntityDescription, ...]] = {
-    # Alarm
+ALARM: dict[str, tuple[TuyaAlarmControlPanelEntityDescription, ...]] = {
+    # Alarm Host
     # https://developer.tuya.com/en/docs/iot/categorymal?id=Kaiuz33clqxaf
     "mal": (
-        TuyaAlarmEntityDescription(
-            key="master_mode",
+        TuyaAlarmControlPanelEntityDescription(
+            key=DPCode.ALARM_MODE,
             name="Alarm",
-            battery_percentage=DPCode.BATTERY_PERCENTAGE,
         ),
-    ),
+    )
 }
 
 
@@ -58,7 +59,6 @@ async def async_setup_entry(
 ) -> None:
     """Set up Tuya alarm dynamically through Tuya discovery."""
     hass_data: HomeAssistantTuyaData = hass.data[DOMAIN][entry.entry_id]
-    _LOGGER.info(hass_data)
 
     @callback
     def async_discover_device(device_ids: list[str]) -> None:
@@ -89,11 +89,15 @@ async def async_setup_entry(
 class TuyaAlarmEntity(TuyaEntity, AlarmControlPanelEntity):
     """Tuya Alarm Entity."""
 
+    _attr_supported_features = (
+        SUPPORT_ALARM_ARM_HOME | SUPPORT_ALARM_ARM_AWAY | SUPPORT_ALARM_TRIGGER
+    )
+
     def __init__(
         self,
         device: TuyaDevice,
         device_manager: TuyaDeviceManager,
-        description: TuyaAlarmEntityDescription,
+        description: TuyaAlarmControlPanelEntityDescription,
     ) -> None:
         """Init Tuya Alarm."""
         super().__init__(device, device_manager)
@@ -102,48 +106,33 @@ class TuyaAlarmEntity(TuyaEntity, AlarmControlPanelEntity):
 
     @property
     def state(self):
-        """Return the current Alarm State."""
-        return self.device.status.get(DPCode.ALARM_STATE)
+        """Return the state of the device."""
+        status = self.device.status.get(DPCode.ALARM_MODE)
 
-    @property
-    def code_format(self) -> str | None:
-        """Regex for code format or None if no code is required."""
-        return self._attr_code_format
-
-    @property
-    def changed_by(self) -> str | None:
-        """Last change triggered by."""
-        return self._attr_changed_by
-
-    @property
-    def code_arm_required(self) -> bool:
-        """Whether the code is required for arm actions."""
-        return self._attr_code_arm_required
-
-    @property
-    def supported_features(self) -> int:
-        """Return the list of supported features."""
-        return (
-            SUPPORT_ALARM_ARM_HOME
-            | SUPPORT_ALARM_ARM_AWAY
-            | SUPPORT_ALARM_ARM_NIGHT
-            | SUPPORT_ALARM_TRIGGER
-        )
+        if status == MODE_ALARM_DISARMED:
+            state = STATE_ALARM_DISARMED
+        elif status == MODE_ALARM_ARM:
+            state = STATE_ALARM_ARMED_AWAY
+        elif status == MODE_ALARM_HOME:
+            state = STATE_ALARM_ARMED_HOME
+        elif status == MODE_ALARM_SOS:
+            state = STATE_ALARM_TRIGGERED
+        else:
+            state = None
+        return state
 
     def alarm_disarm(self, code: str | None = None) -> None:
         """Send Disarm command."""
-        self._send_command(
-            [{"code": DPCode.ALARM_STATE, "value": DPCode.ALARM_DISARMED}]
-        )
+        self._send_command([{"code": DPCode.ALARM_MODE, "value": MODE_ALARM_DISARMED}])
 
     def alarm_arm_home(self, code: str | None = None) -> None:
         """Send Home command."""
-        self._send_command([{"code": DPCode.ALARM_STATE, "value": DPCode.ALARM_HOME}])
+        self._send_command([{"code": DPCode.ALARM_MODE, "value": MODE_ALARM_HOME}])
 
     def alarm_arm_away(self, code: str | None = None) -> None:
         """Send Arm command."""
-        self._send_command([{"code": DPCode.ALARM_STATE, "value": DPCode.ALARM_ARM}])
+        self._send_command([{"code": DPCode.ALARM_MODE, "value": MODE_ALARM_ARM}])
 
     def alarm_trigger(self, code: str | None = None) -> None:
         """Send SOS command."""
-        self._send_command([{"code": DPCode.ALARM_STATE, "value": DPCode.SOS}])
+        self._send_command([{"code": DPCode.ALARM_MODE, "value": MODE_ALARM_SOS}])
