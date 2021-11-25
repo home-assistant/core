ae4cc31497be712159913fa8ab99db6d8399320a
Guido Schmitz <guido.schmitz@fedaix.de>
2021-11-25 19:34:05 +0000

    Switch to single DevoloBinarySensorEntity

--- homeassistant/components/devolo_home_network/binary_sensor.py
+++ homeassistant/components/devolo_home_network/binary_sensor.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Any
 
 from devolo_plc_api.device import Device
 
@@ -32,7 +31,7 @@ from .entity import DevoloEntity
 class DevoloBinarySensorRequiredKeysMixin:
     """Mixin for required keys."""
 
-    value_func: Callable[[dict[str, Any]], bool]
+    value_func: Callable[[DevoloBinarySensorEntity], bool]
 
 
 @dataclass
@@ -43,6 +42,19 @@ class DevoloBinarySensorEntityDescription(
 
 
 SENSOR_TYPES: dict[str, DevoloBinarySensorEntityDescription] = {
+    CONNECTED_TO_ROUTER: DevoloBinarySensorEntityDescription(
+        key=CONNECTED_TO_ROUTER,
+        device_class=DEVICE_CLASS_PLUG,
+        entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        entity_registry_enabled_default=False,
+        icon="mdi:router-network",
+        name="Connected to router",
+        value_func=lambda entity: all(
+            device["attached_to_router"]
+            for device in entity.coordinator.data["network"]["devices"]
+            if device["mac_address"] == entity._device.mac
+        ),
+    ),
     FIRMWARE_UPDATE_AVAILABLE: DevoloBinarySensorEntityDescription(
         key=FIRMWARE_UPDATE_AVAILABLE,
         device_class=DEVICE_CLASS_UPDATE,
@@ -50,7 +62,7 @@ SENSOR_TYPES: dict[str, DevoloBinarySensorEntityDescription] = {
         entity_registry_enabled_default=True,
         icon="mdi:update",
         name="Firmware update available",
-        value_func=lambda data: data["result"] == "UPDATE_AVAILABLE",  # type: ignore[no-any-return]
+        value_func=lambda entity: entity.coordinator.data["result"] == "UPDATE_AVAILABLE",  # type: ignore[no-any-return]
     ),
 }
 
@@ -76,8 +88,9 @@ async def async_setup_entry(
         )
     if device.plcnet:
         entities.append(
-            DevoloAttachedToRouterEntity(
+            DevoloBinarySensorEntity(
                 coordinators[CONNECTED_PLC_DEVICES],
+                SENSOR_TYPES[CONNECTED_TO_ROUTER],
                 device,
                 entry.title,
             )
@@ -102,34 +115,4 @@ class DevoloBinarySensorEntity(DevoloEntity, BinarySensorEntity):
     @property
     def is_on(self) -> bool:
         """State of the binary sensor."""
-        return self.entity_description.value_func(self.coordinator.data)
-
-
-class DevoloAttachedToRouterEntity(DevoloEntity, BinarySensorEntity):
-    """Representation of a special devolo binary sensor. It is special, because the current value cannot be extracted from coordinator data only."""
-
-    def __init__(
-        self,
-        coordinator: DataUpdateCoordinator,
-        device: Device,
-        device_name: str,
-    ) -> None:
-        """Initialize entity."""
-        self.entity_description = BinarySensorEntityDescription(
-            key=CONNECTED_TO_ROUTER,
-            device_class=DEVICE_CLASS_PLUG,
-            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-            entity_registry_enabled_default=False,
-            icon="mdi:router-network",
-            name="Connected to router",
-        )
-        super().__init__(coordinator, device, device_name)
-
-    @property
-    def is_on(self) -> bool:
-        """State of the binary sensor."""
-        return all(
-            d["attached_to_router"]
-            for d in self.coordinator.data["network"]["devices"]
-            if d["mac_address"] == self._device.mac
-        )
+        return self.entity_description.value_func(self)
