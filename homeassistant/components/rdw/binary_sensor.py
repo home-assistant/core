653e2281153eb544f89fc82706772757a192ed9a
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-11-25 22:23:48 +0100

    Import Callable from collections.abc (#60354)

--- homeassistant/components/rdw/binary_sensor.py
+++ homeassistant/components/rdw/binary_sensor.py
@@ -1,8 +1,8 @@
 """Support for RDW binary sensors."""
 from __future__ import annotations
 
+from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Callable
 
 from vehicle import Vehicle
 
