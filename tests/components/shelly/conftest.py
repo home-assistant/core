dcf3517f0a17574bc635df718d01a9db1c41bed8
mib1185 <mail@mib85.de>
2021-11-26 21:38:49 +0000

    improve tests

--- tests/components/shelly/conftest.py
+++ tests/components/shelly/conftest.py
@@ -138,6 +138,7 @@ async def coap_wrapper(hass):
         firmware_version="some fw string",
         update=AsyncMock(),
         trigger_ota_update=AsyncMock(),
+        trigger_reboot=AsyncMock(),
         initialized=True,
     )
 
@@ -173,6 +174,7 @@ async def rpc_wrapper(hass):
         firmware_version="some fw string",
         update=AsyncMock(),
         trigger_ota_update=AsyncMock(),
+        trigger_reboot=AsyncMock(),
         initialized=True,
         shutdown=AsyncMock(),
     )
