d88da63eb805eefc74874fc64e4e7d9cc4f93cd7
J. Nick Koston <nick@koston.org>
2021-11-27 08:24:38 -1000

    fix

--- homeassistant/components/flux_led/__init__.py
+++ homeassistant/components/flux_led/__init__.py
@@ -1,6 +1,7 @@
 """The Flux LED/MagicLight integration."""
 from __future__ import annotations
 
+import asyncio
 from datetime import timedelta
 import logging
 from typing import Any, Final, cast
@@ -26,6 +27,7 @@ from .const import (
     DISCOVER_SCAN_TIMEOUT,
     DOMAIN,
     FLUX_LED_DISCOVERY,
+    FLUX_LED_DISCOVERY_LOCK,
     FLUX_LED_EXCEPTIONS,
     SIGNAL_STATE_UPDATED,
     STARTUP_SCAN_TIMEOUT,
@@ -76,17 +78,18 @@ async def async_discover_devices(
     hass: HomeAssistant, timeout: int, address: str | None = None
 ) -> list[dict[str, str]]:
     """Discover flux led devices."""
-    scanner = AIOBulbScanner()
-    _LOGGER.warning("Calling async_scan for address=%s", address)
-    try:
-        discovered: list[dict[str, str]] = await scanner.async_scan(
-            timeout=timeout, address=address
-        )
-    except OSError as ex:
-        _LOGGER.debug("Scanning failed with error: %s", ex)
-        return []
-    else:
-        return discovered
+    async with hass.data[DOMAIN][FLUX_LED_DISCOVERY_LOCK]:
+        scanner = AIOBulbScanner()
+        _LOGGER.warning("Calling async_scan for address=%s", address)
+        try:
+            discovered: list[dict[str, str]] = await scanner.async_scan(
+                timeout=timeout, address=address
+            )
+        except OSError as ex:
+            _LOGGER.debug("Scanning failed with error: %s", ex)
+            return []
+        else:
+            return discovered
 
 
 async def async_discover_device(
@@ -120,6 +123,8 @@ def async_trigger_discovery(
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the flux_led component."""
     domain_data = hass.data[DOMAIN] = {}
+    if FLUX_LED_DISCOVERY_LOCK not in domain_data:
+        domain_data[FLUX_LED_DISCOVERY_LOCK] = asyncio.Lock()
     domain_data[FLUX_LED_DISCOVERY] = await async_discover_devices(
         hass, STARTUP_SCAN_TIMEOUT
     )
