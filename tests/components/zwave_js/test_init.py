dc7329f43b0b69d6e7af36765b1baa84b67255ac
Keith Pine <keith.pine@gmail.com>
2021-11-27 15:47:48 -0800

    improve test_replace_same_node

--- tests/components/zwave_js/test_init.py
+++ tests/components/zwave_js/test_init.py
@@ -844,57 +844,25 @@ async def test_node_removed(hass, multisensor_6_state, client, integration):
     assert not dev_reg.async_get(old_device.id)
 
 
-async def test_replace_same_node(hass, multisensor_6_state, client, integration):
+async def test_replace_same_node(
+    hass, multisensor_6, multisensor_6_state, client, integration
+):
     """Test when a node is replaced with itself that the device remains."""
     dev_reg = dr.async_get(hass)
-    node = Node(client, deepcopy(multisensor_6_state))
-    device_id = f"{client.driver.controller.home_id}-{node.node_id}"
-    event = {"node": node}
-
-    client.driver.controller.emit("node added", event)
-    await hass.async_block_till_done()
-    old_device = dev_reg.async_get_device(identifiers={(DOMAIN, device_id)})
-    assert old_device.id
-
-    event = {"node": node, "replaced": True}
-
-    client.driver.controller.emit("node removed", event)
-    await hass.async_block_till_done()
-    # Assert device has remained
-    assert dev_reg.async_get(old_device.id)
-
-    event = {"node": node}
-
-    client.driver.controller.emit("node added", event)
-    await hass.async_block_till_done()
-    # Assert device has remained
-    assert dev_reg.async_get(old_device.id)
-
-
-async def test_replace_different_node(
-    hass,
-    multisensor_6,
-    multisensor_6_state,
-    hank_binary_switch_state,
-    client,
-    integration,
-):
-    """Test when a node is replaced with a different node."""
     node_id = multisensor_6.node_id
-    hank_binary_switch_state = deepcopy(hank_binary_switch_state)
-    hank_binary_switch_state["nodeId"] = node_id
+    multisensor_6_state = deepcopy(multisensor_6_state)
 
     device_id = f"{client.driver.controller.home_id}-{node_id}"
     multisensor_6_device_id = f"{device_id}-{multisensor_6.manufacturer_id}:{multisensor_6.product_type}:{multisensor_6.product_id}"
-    hank_device_id = f"{device_id}-{hank_binary_switch_state['manufacturerId']}:{hank_binary_switch_state['productType']}:{hank_binary_switch_state['productId']}"
 
-    dev_reg = dr.async_get(hass)
-    multisensor_6_device = dev_reg.async_get_device(identifiers={(DOMAIN, device_id)})
-    assert multisensor_6_device == dev_reg.async_get_device(
+    device = dev_reg.async_get_device(identifiers={(DOMAIN, device_id)})
+    assert device
+    assert device == dev_reg.async_get_device(
         identifiers={(DOMAIN, multisensor_6_device_id)}
     )
-    assert multisensor_6_device.manufacturer == "AEON Labs"
-    assert multisensor_6_device.model == "ZW100"
+    assert device.manufacturer == "AEON Labs"
+    assert device.model == "ZW100"
+    dev_id = device.id
 
     assert hass.states.get(AIR_TEMPERATURE_SENSOR)
 
@@ -908,11 +876,118 @@ async def test_replace_different_node(
             "node": multisensor_6_state,
         },
     )
-
     client.driver.receive_event(event)
     await hass.async_block_till_done()
+
     # Device should still be there after the node was removed
-    device = dev_reg.async_get(multisensor_6_device.id)
+    device = dev_reg.async_get(dev_id)
+    assert device
+
+    # When the node is replaced, a non-ready node added event is emitted
+    event = Event(
+        type="node added",
+        data={
+            "source": "controller",
+            "event": "node added",
+            "node": {
+                "nodeId": node_id,
+                "index": 0,
+                "status": 4,
+                "ready": False,
+                "isSecure": "unknown",
+                "interviewAttempts": 1,
+                "endpoints": [{"nodeId": node_id, "index": 0, "deviceClass": None}],
+                "values": [],
+                "deviceClass": None,
+                "commandClasses": [],
+                "interviewStage": "None",
+                "statistics": {
+                    "commandsTX": 0,
+                    "commandsRX": 0,
+                    "commandsDroppedRX": 0,
+                    "commandsDroppedTX": 0,
+                    "timeoutResponse": 0,
+                },
+            },
+        },
+    )
+
+    # Device is still not removed
+    client.driver.receive_event(event)
+    await hass.async_block_till_done()
+
+    device = dev_reg.async_get(dev_id)
+    assert device
+
+    event = Event(
+        type="ready",
+        data={
+            "source": "node",
+            "event": "ready",
+            "nodeId": node_id,
+            "nodeState": multisensor_6_state,
+        },
+    )
+    client.driver.receive_event(event)
+    await hass.async_block_till_done()
+
+    # Device is the same
+    device = dev_reg.async_get(dev_id)
+    assert device
+    assert device == dev_reg.async_get_device(identifiers={(DOMAIN, device_id)})
+    assert device == dev_reg.async_get_device(
+        identifiers={(DOMAIN, multisensor_6_device_id)}
+    )
+    assert device.manufacturer == "AEON Labs"
+    assert device.model == "ZW100"
+
+    assert hass.states.get(AIR_TEMPERATURE_SENSOR)
+
+
+async def test_replace_different_node(
+    hass,
+    multisensor_6,
+    multisensor_6_state,
+    hank_binary_switch_state,
+    client,
+    integration,
+):
+    """Test when a node is replaced with a different node."""
+    dev_reg = dr.async_get(hass)
+    node_id = multisensor_6.node_id
+    hank_binary_switch_state = deepcopy(hank_binary_switch_state)
+    hank_binary_switch_state["nodeId"] = node_id
+
+    device_id = f"{client.driver.controller.home_id}-{node_id}"
+    multisensor_6_device_id = f"{device_id}-{multisensor_6.manufacturer_id}:{multisensor_6.product_type}:{multisensor_6.product_id}"
+    hank_device_id = f"{device_id}-{hank_binary_switch_state['manufacturerId']}:{hank_binary_switch_state['productType']}:{hank_binary_switch_state['productId']}"
+
+    device = dev_reg.async_get_device(identifiers={(DOMAIN, device_id)})
+    assert device
+    assert device == dev_reg.async_get_device(
+        identifiers={(DOMAIN, multisensor_6_device_id)}
+    )
+    assert device.manufacturer == "AEON Labs"
+    assert device.model == "ZW100"
+    dev_id = device.id
+
+    assert hass.states.get(AIR_TEMPERATURE_SENSOR)
+
+    # A replace node event has the extra field "replaced" set to True to distinguish it from an exclusion
+    event = Event(
+        type="node removed",
+        data={
+            "source": "controller",
+            "event": "node removed",
+            "replaced": True,
+            "node": multisensor_6_state,
+        },
+    )
+    client.driver.receive_event(event)
+    await hass.async_block_till_done()
+
+    # Device should still be there after the node was removed
+    device = dev_reg.async_get(dev_id)
     assert device
 
     # When the node is replaced, a non-ready node added event is emitted
@@ -950,7 +1025,7 @@ async def test_replace_different_node(
     client.driver.receive_event(event)
     await hass.async_block_till_done()
 
-    device = dev_reg.async_get(multisensor_6_device.id)
+    device = dev_reg.async_get(dev_id)
     assert device
 
     event = Event(
@@ -966,7 +1041,7 @@ async def test_replace_different_node(
     await hass.async_block_till_done()
 
     # Old device and entities were removed, but the ID is re-used
-    device = dev_reg.async_get(multisensor_6_device.id)
+    device = dev_reg.async_get(dev_id)
     assert device
     assert device == dev_reg.async_get_device(identifiers={(DOMAIN, device_id)})
     assert device == dev_reg.async_get_device(identifiers={(DOMAIN, hank_device_id)})
