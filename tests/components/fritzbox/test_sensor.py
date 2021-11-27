fe602c60e829fd3b760f0c4493be09832a0cdb8b
mib1185 <mail@mib85.de>
2021-11-27 00:24:48 +0000

    move lock and devicelock into sensors

--- tests/components/fritzbox/test_sensor.py
+++ tests/components/fritzbox/test_sensor.py
@@ -4,11 +4,7 @@ from unittest.mock import Mock
 
 from requests.exceptions import HTTPError
 
-from homeassistant.components.fritzbox.const import (
-    ATTR_STATE_DEVICE_LOCKED,
-    ATTR_STATE_LOCKED,
-    DOMAIN as FB_DOMAIN,
-)
+from homeassistant.components.fritzbox.const import DOMAIN as FB_DOMAIN
 from homeassistant.components.sensor import ATTR_STATE_CLASS, DOMAIN, SensorStateClass
 from homeassistant.const import (
     ATTR_FRIENDLY_NAME,
@@ -40,8 +36,6 @@ async def test_setup(hass: HomeAssistant, fritz: Mock):
     assert state
     assert state.state == "1.23"
     assert state.attributes[ATTR_FRIENDLY_NAME] == f"{CONF_FAKE_NAME} Temperature"
-    assert state.attributes[ATTR_STATE_DEVICE_LOCKED] == "fake_locked_device"
-    assert state.attributes[ATTR_STATE_LOCKED] == "fake_locked"
     assert state.attributes[ATTR_UNIT_OF_MEASUREMENT] == TEMP_CELSIUS
     assert state.attributes[ATTR_STATE_CLASS] == SensorStateClass.MEASUREMENT
 
