6bd24c5544bac3a2dfb003c9d9034d6803752fc5
Daniel Hjelseth Høyer <github@dahoiv.net>
2021-11-28 17:10:17 +0100

    Tibber, fix #60490 not strictly increasing
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- homeassistant/components/tibber/sensor.py
+++ homeassistant/components/tibber/sensor.py
@@ -88,7 +88,6 @@ RT_SENSORS: tuple[SensorEntityDescription, ...] = (
         name="accumulated consumption",
         device_class=DEVICE_CLASS_ENERGY,
         native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
-        state_class=STATE_CLASS_TOTAL_INCREASING,
     ),
     SensorEntityDescription(
         key="accumulatedConsumptionLastHour",
@@ -102,7 +101,6 @@ RT_SENSORS: tuple[SensorEntityDescription, ...] = (
         name="accumulated production",
         device_class=DEVICE_CLASS_ENERGY,
         native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
-        state_class=STATE_CLASS_TOTAL_INCREASING,
     ),
     SensorEntityDescription(
         key="accumulatedProductionLastHour",
