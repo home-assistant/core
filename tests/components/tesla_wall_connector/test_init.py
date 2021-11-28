776b8816d73981f1fcf77df61382c0cb1e7014a9
Einar Bragi Hauksson <einar.hauksson@gmail.com>
2021-11-28 15:38:01 +0000

    Remove internals check in tests

--- tests/components/tesla_wall_connector/test_init.py
+++ tests/components/tesla_wall_connector/test_init.py
@@ -2,7 +2,6 @@
 from tesla_wall_connector.exceptions import WallConnectorConnectionError
 
 from homeassistant import config_entries
-from homeassistant.components.tesla_wall_connector.const import DOMAIN
 from homeassistant.core import HomeAssistant
 
 from .conftest import create_wall_connector_entry
@@ -14,8 +13,6 @@ async def test_init_success(hass: HomeAssistant) -> None:
     entry = await create_wall_connector_entry(hass)
 
     assert entry.state == config_entries.ConfigEntryState.LOADED
-    assert hass.data[DOMAIN]
-    assert hass.data[DOMAIN][entry.entry_id]
 
 
 async def test_init_while_offline(hass: HomeAssistant) -> None:
@@ -25,7 +22,6 @@ async def test_init_while_offline(hass: HomeAssistant) -> None:
     )
 
     assert entry.state == config_entries.ConfigEntryState.SETUP_RETRY
-    assert entry.entry_id not in hass.data[DOMAIN]
 
 
 async def test_load_unload(hass):
