01756d10886089fb9c8592a64cbc9399b7929e99
evgeniavolkova <eivolkova3@gmail.com>
2021-11-29 23:59:55 +0300

    Remove unused constants

--- homeassistant/components/walkingpad/const.py
+++ homeassistant/components/walkingpad/const.py
@@ -1,9 +1,6 @@
 """Constants for the WalkingPad component."""
 from typing import Final
 
-ATTR_MODE: Final = "mode"
-ATTR_SPEED: Final = "speed"
-
 AWAIT_SLEEP_INTERVAL: Final = 0.7
 
 CONF_CONN_TYPE: Final = "conn_type"
@@ -21,9 +18,3 @@ DOMAIN: Final = "walkingpad"
 MAX_SPEED: Final = 6.0
 MIN_SPEED: Final = 0.0
 MODES_DICT: Final = {0: "auto", 1: "manual", 2: "standby"}
-
-SERVICE_SET_MODE: Final = "set_mode"
-SERVICE_SET_SPEED_USER: Final = "set_speed_user"
-SERVICE_SET_SPEED: Final = "set_speed"
-SERVICE_START_BELT: Final = "start_belt"
-SERVICE_STOP_BELT: Final = "stop_belt"
