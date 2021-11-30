ef447cd1794fee84d74eb7de01d9e18cdef20321
J. Nick Koston <nick@koston.org>
2021-11-30 06:41:04 -1000

    try one more thing

--- tests/components/lutron_caseta/test_device_trigger.py
+++ tests/components/lutron_caseta/test_device_trigger.py
@@ -314,6 +314,7 @@ async def test_validate_trigger_invalid_triggers(hass, device_reg):
     config_entry_id = await _async_setup_lutron_with_picos(hass, device_reg)
     dr_button_devices = hass.data[DOMAIN][config_entry_id][BUTTON_DEVICES]
     device_id = list(dr_button_devices)[0]
+    assert await async_setup_component(hass, "persistent_notification", {})
     assert await async_setup_component(
         hass,
         automation.DOMAIN,
