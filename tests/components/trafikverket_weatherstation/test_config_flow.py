00a7922cecb85dc97969793e4dcdc1edeb373488
G Johansson <goran.johansson@shiftit.se>
2021-11-30 21:04:46 +0000

    Fix ADR 0003 issues

--- tests/components/trafikverket_weatherstation/test_config_flow.py
+++ tests/components/trafikverket_weatherstation/test_config_flow.py
@@ -1,11 +1,29 @@
 """Test the Trafikverket weatherstation config flow."""
 from __future__ import annotations
 
+import json
 from unittest.mock import patch
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_API_KEY, CONF_MONITORED_CONDITIONS, CONF_NAME
+from homeassistant.components.trafikverket_weatherstation.sensor import SENSOR_TYPES
+from homeassistant.const import CONF_API_KEY, CONF_NAME
 from homeassistant.core import HomeAssistant
+
+SENSOR_LIST: list[str | None] = {description.key for (description) in SENSOR_TYPES}
+
+"""SENSOR_LIST: dict(str) = {
+    "air_temp",
+    "road_temp",
+    "precipitation",
+    "wind_direction",
+    "wind_direction_text",
+    "wind_speed",
+    "wind_speed_max",
+    "humidity",
+    "precipitation_amount",
+    "precipitation_amountname",
+}"""
+
 
 DOMAIN = "trafikverket_weatherstation"
 CONF_STATION = "station"
@@ -30,7 +48,6 @@ async def test_form(hass: HomeAssistant) -> None:
                 CONF_NAME: "Vallby Vasteras",
                 CONF_API_KEY: "1234567890",
                 CONF_STATION: "Vallby",
-                CONF_MONITORED_CONDITIONS: ["air_temp", "road_temp"],
             },
         )
         await hass.async_block_till_done()
@@ -41,7 +58,7 @@ async def test_form(hass: HomeAssistant) -> None:
         "name": "Vallby Vasteras",
         "api_key": "1234567890",
         "station": "Vallby",
-        "monitored_conditions": ["air_temp", "road_temp"],
+        "monitored_conditions": json.dumps(list(SENSOR_LIST)),
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -60,7 +77,6 @@ async def test_import_flow_success(hass: HomeAssistant) -> None:
                 CONF_NAME: "Vallby Vasteras",
                 CONF_API_KEY: "1234567890",
                 CONF_STATION: "Vallby",
-                CONF_MONITORED_CONDITIONS: ["air_temp", "road_temp"],
             },
         )
         await hass.async_block_till_done()
@@ -71,6 +87,6 @@ async def test_import_flow_success(hass: HomeAssistant) -> None:
         "name": "Vallby Vasteras",
         "api_key": "1234567890",
         "station": "Vallby",
-        "monitored_conditions": ["air_temp", "road_temp"],
+        "monitored_conditions": json.dumps(list(SENSOR_LIST)),
     }
     assert len(mock_setup_entry.mock_calls) == 1
