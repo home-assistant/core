523b0a21ff6e0c3d345806b1f31659f688050733
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-11-30 16:26:02 +0100

    Add return type annotation to StrEnum (#60624)

--- homeassistant/util/enum.py
+++ homeassistant/util/enum.py
@@ -2,13 +2,15 @@
 from __future__ import annotations
 
 from enum import Enum
-from typing import Any
+from typing import Any, TypeVar
+
+T = TypeVar("T", bound="StrEnum")
 
 
 class StrEnum(str, Enum):
     """Partial backport of Python 3.11's StrEnum for our basic use cases."""
 
-    def __new__(cls, value: str, *args: Any, **kwargs: Any):  # type: ignore[no-untyped-def]
+    def __new__(cls: type[T], value: str, *args: Any, **kwargs: Any) -> T:
         """Create a new StrEnum instance."""
         if not isinstance(value, str):
             raise TypeError(f"{value!r} is not a string")
