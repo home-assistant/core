3aadfde14096d89b35d74460708cfc03e6d3f8ac
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-11-30 20:33:14 +1300

    Esphome button device class (#60569)

--- homeassistant/components/esphome/button.py
+++ homeassistant/components/esphome/button.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from aioesphomeapi import ButtonInfo
-from aioesphomeapi.model import EntityState
+from aioesphomeapi import ButtonInfo, EntityState
 
 from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
@@ -31,6 +30,11 @@ async def async_setup_entry(
 
 class EsphomeButton(EsphomeEntity[ButtonInfo, EntityState], ButtonEntity):
     """A button implementation for ESPHome."""
+
+    @property
+    def device_class(self) -> str:
+        """Return the class of this device, from component DEVICE_CLASSES."""
+        return self._static_info.device_class
 
     @callback
     def _on_device_update(self) -> None:
