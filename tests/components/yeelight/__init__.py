73a4dba2ae6073cd3e37b37fa253050f30d896eb
epenet <6771947+epenet@users.noreply.github.com>
2021-12-01 09:28:19 +0100

    Use dataclass properties in yeelight discovery (#60640)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- tests/components/yeelight/__init__.py
+++ tests/components/yeelight/__init__.py
@@ -8,7 +8,7 @@ from async_upnp_client.search import SsdpSearchListener
 from yeelight import BulbException, BulbType
 from yeelight.main import _MODEL_SPECS
 
-from homeassistant.components import zeroconf
+from homeassistant.components import ssdp, zeroconf
 from homeassistant.components.yeelight import (
     CONF_MODE_MUSIC,
     CONF_NIGHTLIGHT_SWITCH_TYPE,
@@ -179,11 +179,15 @@ def _patch_discovery(no_device=False, capabilities=None):
     YeelightScanner._scanner = None  # Clear class scanner to reset hass
 
     def _generate_fake_ssdp_listener(*args, **kwargs):
-        return _patched_ssdp_listener(
-            None if no_device else capabilities or CAPABILITIES,
-            *args,
-            **kwargs,
-        )
+        info = None
+        if not no_device:
+            info = ssdp.SsdpServiceInfo(
+                ssdp_usn="mock_usn",
+                ssdp_st="mock_st",
+                upnp={},
+                ssdp_headers=capabilities or CAPABILITIES,
+            )
+        return _patched_ssdp_listener(info, *args, **kwargs)
 
     return patch(
         "homeassistant.components.yeelight.scanner.SsdpSearchListener",
