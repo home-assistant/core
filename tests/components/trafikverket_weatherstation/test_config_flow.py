445ff992335adceaf53da3010b2c319b0c7162b0
G Johansson <goran.johansson@shiftit.se>
2021-12-01 17:24:19 +0000

    Modify test according to fixes

--- tests/components/trafikverket_weatherstation/test_config_flow.py
+++ tests/components/trafikverket_weatherstation/test_config_flow.py
@@ -1,20 +1,16 @@
 """Test the Trafikverket weatherstation config flow."""
 from __future__ import annotations
 
-import json
 from unittest.mock import patch
 
 import pytest
 
 from homeassistant import config_entries
-from homeassistant.components.trafikverket_weatherstation.sensor import SENSOR_TYPES
-from homeassistant.const import CONF_API_KEY, CONF_MONITORED_CONDITIONS, CONF_NAME
+from homeassistant.const import CONF_API_KEY, CONF_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import RESULT_TYPE_FORM
 
 from tests.common import MockConfigEntry
-
-SENSOR_LIST: list[str | None] = {description.key for (description) in SENSOR_TYPES}
 
 DOMAIN = "trafikverket_weatherstation"
 CONF_STATION = "station"
@@ -39,7 +35,6 @@ async def test_form(hass: HomeAssistant) -> None:
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                CONF_NAME: "Vallby Vasteras",
                 CONF_API_KEY: "1234567890",
                 CONF_STATION: "Vallby",
             },
@@ -47,12 +42,10 @@ async def test_form(hass: HomeAssistant) -> None:
         await hass.async_block_till_done()
 
     assert result2["type"] == "create_entry"
-    assert result2["title"] == "Vallby Vasteras"
+    assert result2["title"] == "Vallby"
     assert result2["data"] == {
-        "name": "Vallby Vasteras",
         "api_key": "1234567890",
         "station": "Vallby",
-        "monitored_conditions": json.dumps(list(SENSOR_LIST)),
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -71,7 +64,7 @@ async def test_import_flow_success(hass: HomeAssistant) -> None:
             DOMAIN,
             context={"source": config_entries.SOURCE_IMPORT},
             data={
-                CONF_NAME: "Vallby Vasteras",
+                CONF_NAME: "Vallby",
                 CONF_API_KEY: "1234567890",
                 CONF_STATION: "Vallby",
             },
@@ -79,12 +72,10 @@ async def test_import_flow_success(hass: HomeAssistant) -> None:
         await hass.async_block_till_done()
 
     assert result2["type"] == "create_entry"
-    assert result2["title"] == "Vallby Vasteras"
+    assert result2["title"] == "Vallby"
     assert result2["data"] == {
-        "name": "Vallby Vasteras",
         "api_key": "1234567890",
         "station": "Vallby",
-        "monitored_conditions": json.dumps(list(SENSOR_LIST)),
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -95,12 +86,10 @@ async def test_import_flow_already_exist(hass: HomeAssistant) -> None:
     MockConfigEntry(
         domain=DOMAIN,
         data={
-            CONF_NAME: "Vallby Vasteras",
             CONF_API_KEY: "1234567890",
             CONF_STATION: "Vallby",
-            CONF_MONITORED_CONDITIONS: json.dumps(list(SENSOR_LIST)),
         },
-        unique_id="Vallby Vasteras",
+        unique_id="Vallby",
     ).add_to_hass(hass)
 
     with patch(
@@ -114,7 +103,7 @@ async def test_import_flow_already_exist(hass: HomeAssistant) -> None:
             DOMAIN,
             context={"source": config_entries.SOURCE_IMPORT},
             data={
-                CONF_NAME: "Vallby Vasteras",
+                CONF_NAME: "Vallby",
                 CONF_API_KEY: "1234567890",
                 CONF_STATION: "Vallby",
             },
@@ -126,7 +115,7 @@ async def test_import_flow_already_exist(hass: HomeAssistant) -> None:
 
 
 @pytest.mark.parametrize(
-    "errormessage,baseerror",
+    "errormessage,base_error",
     [
         (
             "Source: Security, message: Invalid authentication",
@@ -146,8 +135,8 @@ async def test_import_flow_already_exist(hass: HomeAssistant) -> None:
         ),
     ],
 )
-async def test_flow_fails(hass, errormessage: str, baseerror: str):
-    """Test that config flow fails on faulty credentials."""
+async def test_flow_fails(hass: HomeAssistant, errormessage: str, base_error: str):
+    """Test config flow errors."""
     result4 = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
@@ -156,16 +145,15 @@ async def test_flow_fails(hass, errormessage: str, baseerror: str):
     assert result4["step_id"] == config_entries.SOURCE_USER
 
     with patch(
-        "homeassistant.components.trafikverket_weatherstation.config_flow.TVWeatherConfigFlow.validate_input",
-        return_value=errormessage,
+        "homeassistant.components.trafikverket_weatherstation.config_flow.TrafikverketWeather.async_get_weather",
+        return_value=ValueError(errormessage),
     ):
         result4 = await hass.config_entries.flow.async_configure(
             result4["flow_id"],
             user_input={
-                CONF_NAME: "Vallby Vasteras",
                 CONF_API_KEY: "1234567890",
                 CONF_STATION: "Vallby",
             },
         )
 
-    assert result4["errors"] == {"base": baseerror}
+    assert result4["errors"] == {"base": base_error}
