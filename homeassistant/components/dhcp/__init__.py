1e199847b2ebeb041323eb16ac73d765a894e336
epenet <6771947+epenet@users.noreply.github.com>
2021-12-01 16:56:54 +0100

    Enable warnings for DhcpServiceInfo (#60750)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/dhcp/__init__.py
+++ homeassistant/components/dhcp/__init__.py
@@ -82,7 +82,6 @@ class DhcpServiceInfo(BaseServiceInfo):
                 f"accessed discovery_info['{name}'] instead of discovery_info.{name}; this will fail in version 2022.6",
                 exclude_integrations={DOMAIN},
                 error_if_core=False,
-                level=logging.DEBUG,
             )
             self._warning_logged = True
         return getattr(self, name)
@@ -98,7 +97,6 @@ class DhcpServiceInfo(BaseServiceInfo):
                 f"accessed discovery_info.get('{name}') instead of discovery_info.{name}; this will fail in version 2022.6",
                 exclude_integrations={DOMAIN},
                 error_if_core=False,
-                level=logging.DEBUG,
             )
             self._warning_logged = True
         if hasattr(self, name):
