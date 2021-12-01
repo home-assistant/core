0957bd9059a8f6950de9f8b17cbdbba8fe76f1fd
epenet <6771947+epenet@users.noreply.github.com>
2021-12-01 19:59:12 +0100

    Use dataclass properties in dlna_dmr discovery (#60693)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/dlna_dmr/media_player.py
+++ homeassistant/components/dlna_dmr/media_player.py
@@ -247,12 +247,12 @@ class DlnaDmrEntity(MediaPlayerEntity):
         _LOGGER.debug(
             "SSDP %s notification of device %s at %s",
             change,
-            info[ssdp.ATTR_SSDP_USN],
-            info.get(ssdp.ATTR_SSDP_LOCATION),
+            info.ssdp_usn,
+            info.ssdp_location,
         )
 
         try:
-            bootid_str = info[ssdp.ATTR_SSDP_BOOTID]
+            bootid_str = info.ssdp_headers[ssdp.ATTR_SSDP_BOOTID]
             bootid: int | None = int(bootid_str, 10)
         except (KeyError, ValueError):
             bootid = None
@@ -263,7 +263,7 @@ class DlnaDmrEntity(MediaPlayerEntity):
                 # Store the new value (because our old value matches) so that we
                 # can ignore subsequent ssdp:alive messages
                 with contextlib.suppress(KeyError, ValueError):
-                    next_bootid_str = info[ssdp.ATTR_SSDP_NEXTBOOTID]
+                    next_bootid_str = info.ssdp_headers[ssdp.ATTR_SSDP_NEXTBOOTID]
                     self._bootid = int(next_bootid_str, 10)
             # Nothing left to do until ssdp:alive comes through
             return
@@ -278,7 +278,7 @@ class DlnaDmrEntity(MediaPlayerEntity):
             await self._device_disconnect()
 
         if change == ssdp.SsdpChange.ALIVE and not self._device:
-            location = info[ssdp.ATTR_SSDP_LOCATION]
+            location = info.ssdp_location or ""
             try:
                 await self._device_connect(location)
             except UpnpError as err:
