4f0234d3d29063baaad092d5c8f967496ac42636
epenet <6771947+epenet@users.noreply.github.com>
2021-12-02 20:57:45 +0100

    Use HassioServiceInfo in zwave_js tests (#60863)

--- tests/components/zwave_js/test_config_flow.py
+++ tests/components/zwave_js/test_config_flow.py
@@ -8,17 +8,20 @@ from zwave_js_server.version import VersionInfo
 
 from homeassistant import config_entries
 from homeassistant.components import usb
+from homeassistant.components.hassio.discovery import HassioServiceInfo
 from homeassistant.components.hassio.handler import HassioAPIError
 from homeassistant.components.zwave_js.config_flow import SERVER_VERSION_TIMEOUT, TITLE
 from homeassistant.components.zwave_js.const import DOMAIN
 
 from tests.common import MockConfigEntry
 
-ADDON_DISCOVERY_INFO = {
-    "addon": "Z-Wave JS",
-    "host": "host1",
-    "port": 3001,
-}
+ADDON_DISCOVERY_INFO = HassioServiceInfo(
+    config={
+        "addon": "Z-Wave JS",
+        "host": "host1",
+        "port": 3001,
+    }
+)
 
 
 USB_DISCOVERY_INFO = usb.UsbServiceInfo(
