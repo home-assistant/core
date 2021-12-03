967bb418c0765f37fc37e4bca3ebe504646ae56e
Tuen Lee <leeyuentuen@gmail.com>
2021-12-03 12:43:57 +0000

    fix comments

--- homeassistant/components/tuya/alarm_control_panel.py
+++ homeassistant/components/tuya/alarm_control_panel.py
@@ -1,10 +1,9 @@
 """Support for Tuya Alarm."""
 from __future__ import annotations
 
-from enum import Enum
-
 from tuya_iot import TuyaDevice, TuyaDeviceManager
 
+from homeassistant.backports.enum import StrEnum
 from homeassistant.components.alarm_control_panel import (
     SUPPORT_ALARM_ARM_AWAY,
     SUPPORT_ALARM_ARM_HOME,
@@ -28,13 +27,21 @@ from .base import EnumTypeData, TuyaEntity
 from .const import DOMAIN, TUYA_DISCOVERY_NEW, DPCode
 
 
-class Mode(Enum, str):
+class Mode(StrEnum):
     """Alarm modes."""
 
-    ALARM_ARM = "arm"
-    ALARM_DISARMED = "disarmed"
-    ALARM_HOME = "home"
-    ALARM_SOS = "sos"
+    ARM = "arm"
+    DISARMED = "disarmed"
+    HOME = "home"
+    SOS = "sos"
+
+
+STATE_MAPPING: dict[str, str] = {
+    Mode.DISARMED: STATE_ALARM_DISARMED,
+    Mode.ARM: STATE_ALARM_ARMED_AWAY,
+    Mode.HOME: STATE_ALARM_ARMED_HOME,
+    Mode.SOS: STATE_ALARM_TRIGGERED,
+}
 
 
 # All descriptions can be found here:
@@ -86,16 +93,8 @@ async def async_setup_entry(
 class TuyaAlarmEntity(TuyaEntity, AlarmControlPanelEntity):
     """Tuya Alarm Entity."""
 
-    entity_description: AlarmControlPanelEntityDescription
     _attr_icon = "mdi:security"
     _attr_supported_features = 0
-
-    state_mapping = {
-        Mode.ALARM_DISARMED: STATE_ALARM_DISARMED,
-        Mode.ALARM_ARM: STATE_ALARM_ARMED_AWAY,
-        Mode.ALARM_HOME: STATE_ALARM_ARMED_HOME,
-        Mode.ALARM_SOS: STATE_ALARM_TRIGGERED,
-    }
 
     def __init__(
         self,
@@ -113,38 +112,32 @@ class TuyaAlarmEntity(TuyaEntity, AlarmControlPanelEntity):
             device.function[DPCode.MASTER_MODE].values
         ).range
 
-        if Mode.ALARM_HOME in supported_mode:
+        if Mode.HOME in supported_mode:
             self._attr_supported_features |= SUPPORT_ALARM_ARM_HOME
 
-        if Mode.ALARM_ARM in supported_mode:
+        if Mode.ARM in supported_mode:
             self._attr_supported_features |= SUPPORT_ALARM_ARM_AWAY
 
-        if Mode.ALARM_SOS in supported_mode:
+        if Mode.SOS in supported_mode:
             self._attr_supported_features |= SUPPORT_ALARM_TRIGGER
 
     @property
     def state(self):
         """Return the state of the device."""
-        return self.state_mapping.get(self.device.status.get(DPCode.MASTER_MODE))
+        return STATE_MAPPING.get(self.device.status.get(DPCode.MASTER_MODE))
 
     def alarm_disarm(self, code: str | None = None) -> None:
         """Send Disarm command."""
-        if DPCode.MASTER_MODE in self.device.function:
-            self._send_command(
-                [{"code": DPCode.MASTER_MODE, "value": Mode.ALARM_DISARMED}]
-            )
+        self._send_command([{"code": DPCode.MASTER_MODE, "value": Mode.DISARMED}])
 
     def alarm_arm_home(self, code: str | None = None) -> None:
         """Send Home command."""
-        if DPCode.MASTER_MODE in self.device.function:
-            self._send_command([{"code": DPCode.MASTER_MODE, "value": Mode.ALARM_HOME}])
+        self._send_command([{"code": DPCode.MASTER_MODE, "value": Mode.HOME}])
 
     def alarm_arm_away(self, code: str | None = None) -> None:
         """Send Arm command."""
-        if DPCode.MASTER_MODE in self.device.function:
-            self._send_command([{"code": DPCode.MASTER_MODE, "value": Mode.ALARM_ARM}])
+        self._send_command([{"code": DPCode.MASTER_MODE, "value": Mode.ARM}])
 
     def alarm_trigger(self, code: str | None = None) -> None:
         """Send SOS command."""
-        if DPCode.MASTER_MODE in self.device.function:
-            self._send_command([{"code": DPCode.MASTER_MODE, "value": Mode.ALARM_SOS}])
+        self._send_command([{"code": DPCode.MASTER_MODE, "value": Mode.SOS}])
