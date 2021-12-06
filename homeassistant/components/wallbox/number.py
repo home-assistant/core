f57369a34cde9ff2c5dfa91d7c021c5a3b5e08f3
epenet <epenet@users.noreply.github.com>
2021-12-06 14:06:53 +0000

    Revert "Adjust wallbox accordingly"
    This reverts commit adc4fdc5af20fc931bdfec004e317bdc8cbcf4f4.

--- homeassistant/components/wallbox/number.py
+++ homeassistant/components/wallbox/number.py
@@ -19,6 +19,8 @@ from .const import CONF_MAX_AVAILABLE_POWER_KEY, CONF_MAX_CHARGING_CURRENT_KEY, 
 class WallboxNumberEntityDescription(NumberEntityDescription):
     """Describes Wallbox sensor entity."""
 
+    min_value: float = 0
+
 
 NUMBER_TYPES: dict[str, WallboxNumberEntityDescription] = {
     CONF_MAX_CHARGING_CURRENT_KEY: WallboxNumberEntityDescription(
@@ -69,6 +71,7 @@ class WallboxNumber(CoordinatorEntity, NumberEntity):
         self.entity_description = description
         self._coordinator = coordinator
         self._attr_name = f"{entry.title} {description.name}"
+        self._attr_min_value = description.min_value
 
     @property
     def max_value(self) -> float:
