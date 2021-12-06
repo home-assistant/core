8e21ac63565c5f111fe75995e7adb42603fe7c98
Robert Hillis <tkdrob4390@yahoo.com>
2021-12-06 16:17:08 -0500

    ci

--- homeassistant/components/tautulli/__init__.py
+++ homeassistant/components/tautulli/__init__.py
@@ -5,7 +5,7 @@ from pytautulli import PyTautulli, PyTautulliHostConfiguration
 
 from homeassistant.components.sensor import SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_API_KEY, CONF_SSL, CONF_URL, CONF_VERIFY_SSL, Platform
+from homeassistant.const import CONF_API_KEY, CONF_URL, CONF_VERIFY_SSL, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.device_registry import DeviceEntryType
@@ -24,7 +24,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         api_token=entry.data[CONF_API_KEY],
         url=entry.data[CONF_URL],
         verify_ssl=entry.data[CONF_VERIFY_SSL],
-        ssl=entry.data[CONF_SSL],
     )
     api_client = PyTautulli(
         host_configuration=host_configuration,
