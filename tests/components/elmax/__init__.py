ff1276b44cebae12be6d059acda8219ce2f2cdf6
Alberto Geniola <albertogeniola@users.noreply.github.com>
2021-12-07 22:42:55 +0100

    Elmax integration (#59321)
    * Add elmax integration.
    
    * Run hassfest and generate requirements_all
    
    * Remove secondary platforms from elmax integration as per first component integration.
    
    * Move ElmaxCoordinator and ElmaxEntity into external file
    Linting review
    
    * Remove useless variables
    
    * Fix wrong indentation.
    
    * Remove unecessary platforms.
    
    * Remove unnecessary attributes from manifest.
    
    * Rely on property getters/setters rathern than private attribute from parent.
    Update internal entity state just after transitory state update.
    
    * Update homeassistant/components/elmax/const.py
    
    Reference Platform constant
    
    Co-authored-by: Marvin Wichmann <marvin@fam-wichmann.de>
    
    * Update username/password values
    
    Rely on already-present templating constants
    
    Co-authored-by: Marvin Wichmann <marvin@fam-wichmann.de>
    
    * Add missing constant import.
    
    * Remove unnecessary test_unhandled_error() callback implementation.
    
    * Add common.py to coverage ignore list.
    
    * Improve coverage of config_flow.
    
    * Rename the integration.
    
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>
    
    * Fix reauth bug and improve testing.
    
    * Refactor lambdas into generators.
    
    Co-authored-by: Marvin Wichmann <marvin@fam-wichmann.de>
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

---
+++ tests/components/elmax/__init__.py
@@ -0,0 +1,15 @@
+"""Tests for the Elmax component."""
+
+MOCK_USER_JWT = (
+    "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+    ".eyJfaWQiOiIxYjExYmIxMWJiYjExMTExYjFiMTFiMWIiLCJlbWFpbCI6InRoaXMuaXNAdGVzdC5jb20iLCJyb2xlIjoid"
+    "XNlciIsImlhdCI6MTYzNjE5OTk5OCwiZXhwIjoxNjM2MjM1OTk4fQ.1C7lXuKyX1HEGOfMxNwxJ2n-CjoW4rwvNRITQxLI"
+    "Cv0"
+)
+MOCK_USERNAME = "this.is@test.com"
+MOCK_USER_ROLE = "user"
+MOCK_USER_ID = "1b11bb11bbb11111b1b11b1b"
+MOCK_PANEL_ID = "2db3dae30b9102de4d078706f94d0708"
+MOCK_PANEL_NAME = "Test Panel Name"
+MOCK_PANEL_PIN = "000000"
+MOCK_PASSWORD = "password"
