bf7f1f44df6bb4a03c64f64b0b3a9c035505c1a2
Alberto Geniola <albertogeniola@gmail.com>
2021-12-07 01:01:03 +0100

    Fix reauth bug and improve testing.

--- homeassistant/components/elmax/config_flow.py
+++ homeassistant/components/elmax/config_flow.py
@@ -23,7 +23,6 @@ from homeassistant.exceptions import HomeAssistantError
 
 _LOGGER = logging.getLogger(__name__)
 
-
 LOGIN_FORM_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_ELMAX_USERNAME): str,
@@ -58,6 +57,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self._password: str = None
         self._panels_schema = None
         self._panel_names = None
+        self._reauth_username = None
+        self._reauth_panelid = None
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -130,30 +131,18 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user", data_schema=LOGIN_FORM_SCHEMA, errors=errors
         )
 
-    async def async_step_panels(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    async def async_step_panels(self, user_input: dict[str, Any]) -> FlowResult:
         """Handle Panel selection step."""
-        if user_input is None:
-            # In in any case this step is invoked without any parameter, just restart the user step.
-            return self.async_show_form(step_id="user", data_schema=LOGIN_FORM_SCHEMA)
-
         errors = {}
-        panel_name = user_input.get(CONF_ELMAX_PANEL_NAME)
-        panel_pin = user_input.get(CONF_ELMAX_PANEL_PIN)
+        panel_name = user_input[CONF_ELMAX_PANEL_NAME]
+        panel_pin = user_input[CONF_ELMAX_PANEL_PIN]
 
         # Lookup the panel id from the panel name.
         panel_id = self._panel_names[panel_name]
 
         # Make sure this is the only elmax integration for this specific panel id.
-        current_entries = self._async_current_entries()
-        for entry in current_entries:
-            if entry.data["panel_id"] == panel_id:
-                _LOGGER.error(
-                    "An Elmax integration has been already set up for panel %s",
-                    panel_id,
-                )
-                return self.async_abort(reason="single_instance_allowed")
+        await self.async_set_unique_id(panel_id)
+        self._abort_if_unique_id_configured()
 
         # Try to list all the devices using the given PIN.
         try:
@@ -181,12 +170,81 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_reauth(self, user_input=None):
         """Perform reauth upon an API authentication error."""
+        self._reauth_username = user_input.get(CONF_ELMAX_USERNAME)
+        self._reauth_panelid = user_input.get(CONF_ELMAX_PANEL_ID)
         return await self.async_step_reauth_confirm()
 
     async def async_step_reauth_confirm(self, user_input=None):
         """Handle reauthorization flow."""
-        if user_input is None:
-            return self.async_show_form(step_id="user", data_schema=LOGIN_FORM_SCHEMA)
+        errors = {}
+        if user_input is not None:
+            panel_pin = user_input.get(CONF_ELMAX_PANEL_PIN)
+            password = user_input.get(CONF_ELMAX_PASSWORD)
+            entry = await self.async_set_unique_id(self._reauth_panelid)
+
+            # Handle authentication, make sure the panel we are re-authenticating against is listed among results
+            # and verify its pin is correct.
+            try:
+                # Test login.
+                client = Elmax(username=self._reauth_username, password=password)
+                await client.login()
+
+                # Make sure the panel we are authenticating to is still available.
+                panels = await client.list_control_panels()
+                if (
+                    next(filter(lambda p: p.hash == self._reauth_panelid, panels), None)
+                    is None
+                ):
+                    raise NoOnlinePanelsError()
+
+                # Verify the pin is still valid.from
+                await client.get_panel_status(
+                    control_panel_id=self._reauth_panelid, pin=panel_pin
+                )
+
+                # If it is, proceed with configuration update.
+                self.hass.config_entries.async_update_entry(
+                    entry,
+                    data={
+                        CONF_ELMAX_PANEL_ID: self._reauth_panelid,
+                        CONF_ELMAX_PANEL_PIN: panel_pin,
+                        CONF_ELMAX_USERNAME: self._reauth_username,
+                        CONF_ELMAX_PASSWORD: password,
+                    },
+                )
+                await self.hass.config_entries.async_reload(entry.entry_id)
+                self._reauth_username = None
+                self._reauth_panelid = None
+                return self.async_abort(reason="reauth_successful")
+
+            except ElmaxBadLoginError:
+                _LOGGER.error(
+                    "Wrong credentials or failed login while re-authenticating"
+                )
+                errors["base"] = "bad_auth"
+            except NoOnlinePanelsError:
+                _LOGGER.warning(
+                    "Panel ID %s is no longer associated to this user",
+                    self._reauth_panelid,
+                )
+                errors["base"] = "reauth_panel_disappeared"
+            except ElmaxBadPinError:
+                errors["base"] = "invalid_pin"
+
+        # We want the user to re-authenticate only for the given panel id using the same login.
+        # We pin them to the UI, so the user realizes she must log in with the appropriate credentials
+        # for the that specific panel.
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_ELMAX_USERNAME): self._reauth_username,
+                vol.Required(CONF_ELMAX_PASSWORD): str,
+                vol.Required(CONF_ELMAX_PANEL_ID): self._reauth_panelid,
+                vol.Required(CONF_ELMAX_PANEL_PIN): str,
+            }
+        )
+        return self.async_show_form(
+            step_id="reauth_confirm", data_schema=schema, errors=errors
+        )
 
 
 class NoOnlinePanelsError(HomeAssistantError):
--- tests/components/elmax/test_config_flow.py
+++ tests/components/elmax/test_config_flow.py
@@ -3,8 +3,7 @@ from unittest.mock import patch
 
 from elmax_api.exceptions import ElmaxBadLoginError, ElmaxBadPinError, ElmaxNetworkError
 
-from homeassistant import data_entry_flow
-from homeassistant.components.elmax import config_flow
+from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.elmax.const import (
     CONF_ELMAX_PANEL_ID,
     CONF_ELMAX_PANEL_NAME,
@@ -14,207 +13,256 @@ from homeassistant.components.elmax.const import (
     DOMAIN,
 )
 from homeassistant.config_entries import SOURCE_REAUTH
+from homeassistant.data_entry_flow import FlowResult
 
-from tests.common import MockConfigEntry
 from tests.components.elmax import (
     MOCK_PANEL_ID,
     MOCK_PANEL_NAME,
     MOCK_PANEL_PIN,
+    MOCK_PASSWORD,
     MOCK_USERNAME,
 )
 
 CONF_POLLING = "polling"
 
 
+def _has_error(errors):
+    return errors is not None and len(errors.keys()) > 0
+
+
+async def _bootstrap(
+    hass,
+    source=config_entries.SOURCE_USER,
+    username=MOCK_USERNAME,
+    password=MOCK_PASSWORD,
+    panel_name=MOCK_PANEL_NAME,
+    panel_pin=MOCK_PANEL_PIN,
+) -> FlowResult:
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": source}
+    )
+    if result["type"] != data_entry_flow.RESULT_TYPE_FORM or _has_error(
+        result["errors"]
+    ):
+        return result
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_ELMAX_USERNAME: username,
+            CONF_ELMAX_PASSWORD: password,
+        },
+    )
+    if result2["type"] != data_entry_flow.RESULT_TYPE_FORM or _has_error(
+        result2["errors"]
+    ):
+        return result2
+    result3 = await hass.config_entries.flow.async_configure(
+        result2["flow_id"],
+        {
+            CONF_ELMAX_PANEL_NAME: panel_name,
+            CONF_ELMAX_PANEL_PIN: panel_pin,
+        },
+    )
+    return result3
+
+
+async def _reauth(hass):
+
+    # Trigger reauth
+    result2 = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_REAUTH},
+        data={
+            CONF_ELMAX_PANEL_ID: MOCK_PANEL_ID,
+            CONF_ELMAX_PANEL_PIN: MOCK_PANEL_PIN,
+            CONF_ELMAX_USERNAME: MOCK_USERNAME,
+            CONF_ELMAX_PASSWORD: MOCK_PASSWORD,
+        },
+    )
+    if result2["type"] != data_entry_flow.RESULT_TYPE_FORM or _has_error(
+        result2["errors"]
+    ):
+        return result2
+
+    # Perform reauth confirm step
+    result3 = await hass.config_entries.flow.async_configure(
+        result2["flow_id"],
+        {
+            CONF_ELMAX_PANEL_ID: MOCK_PANEL_ID,
+            CONF_ELMAX_PANEL_PIN: MOCK_PANEL_PIN,
+            CONF_ELMAX_USERNAME: MOCK_USERNAME,
+            CONF_ELMAX_PASSWORD: MOCK_PASSWORD,
+        },
+    )
+    return result3
+
+
 async def test_show_form(hass):
     """Test that the form is served with no input."""
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-
-    result = await flow.async_step_user(user_input=None)
-
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
 
 
 async def test_one_config_allowed(hass):
-    """Test that only one Elmax configuration is allowed per panel ID Panel."""
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
+    """Test that only one Elmax configuration is allowed for each panel."""
+    # Setup once.
+    attempt1 = await _bootstrap(hass)
+    assert attempt1["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
 
-    # Mock an existing config entry
-    MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            CONF_ELMAX_PANEL_ID: MOCK_PANEL_ID,
-            CONF_ELMAX_PANEL_PIN: MOCK_PANEL_PIN,
-            CONF_ELMAX_USERNAME: MOCK_USERNAME,
-            CONF_ELMAX_PASSWORD: "password",
-        },
-    ).add_to_hass(hass)
-
-    # Try to add another instance of the integration for the very same panel.
-    user_input = {CONF_ELMAX_USERNAME: MOCK_USERNAME, CONF_ELMAX_PASSWORD: "password"}
-    step_user_result = await flow.async_step_user(user_input=user_input)
-    assert step_user_result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert step_user_result["step_id"] == "panels"
-
-    user_input = {
-        CONF_ELMAX_PANEL_NAME: MOCK_PANEL_NAME,
-        CONF_ELMAX_PANEL_PIN: MOCK_PANEL_PIN,
-    }
-    step_panels_result = await flow.async_step_panels(user_input=user_input)
-    assert step_panels_result["type"] == data_entry_flow.RESULT_TYPE_ABORT
-    assert step_panels_result["reason"] == "single_instance_allowed"
+    # Attempt to add another instance of the integration for the very same panel, it must fail.
+    attempt2 = await _bootstrap(hass)
+    assert attempt2["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert attempt2["reason"] == "already_configured"
 
 
 async def test_invalid_credentials(hass):
     """Test that invalid credentials throws an error."""
-    conf = {
-        CONF_ELMAX_USERNAME: "wrong_user_name@email.com",
-        CONF_ELMAX_PASSWORD: "incorrect_password",
-    }
-
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-
     with patch(
         "elmax_api.http.Elmax.login",
         side_effect=ElmaxBadLoginError(),
     ):
-        result = await flow.async_step_user(user_input=conf)
+        result = await _bootstrap(
+            hass, username="wrong_user_name@email.com", password="incorrect_password"
+        )
+        assert result["step_id"] == "user"
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
         assert result["errors"] == {"base": "bad_auth"}
 
 
 async def test_connection_error(hass):
     """Test other than invalid credentials throws an error."""
-    conf = {CONF_ELMAX_USERNAME: MOCK_USERNAME, CONF_ELMAX_PASSWORD: "password"}
-
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-
     with patch(
         "elmax_api.http.Elmax.login",
         side_effect=ElmaxNetworkError(),
     ):
-        result = await flow.async_step_user(user_input=conf)
+        result = await _bootstrap(
+            hass, username="wrong_user_name@email.com", password="incorrect_password"
+        )
+        assert result["step_id"] == "user"
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
         assert result["errors"] == {"base": "network_error"}
 
 
 async def test_unhandled_error(hass):
     """Test unhandled exceptions."""
-    conf = {
-        CONF_ELMAX_PANEL_NAME: MOCK_PANEL_NAME,
-        CONF_ELMAX_PANEL_PIN: MOCK_PANEL_PIN,
-    }
-
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-
-    # Perform first step (user) to set up ConfigFlow internal state objects.
-    user_input = {CONF_ELMAX_USERNAME: MOCK_USERNAME, CONF_ELMAX_PASSWORD: "password"}
-    await flow.async_step_user(user_input=user_input)
-
     with patch(
         "elmax_api.http.Elmax.get_panel_status",
         side_effect=Exception(),
     ):
-        result = await flow.async_step_panels(user_input=conf)
+        result = await _bootstrap(hass)
+        assert result["step_id"] == "panels"
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
         assert result["errors"] == {"base": "unknown_error"}
 
 
 async def test_invalid_pin(hass):
     """Test error is thrown when a wrong pin is used to pair a panel."""
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-
-    # Perform first step (user) to setup ConfigFlow internal state objects.
-    user_input = {CONF_ELMAX_USERNAME: MOCK_USERNAME, CONF_ELMAX_PASSWORD: "password"}
-    await flow.async_step_user(user_input=user_input)
-
     # Simulate bad pin response.
     with patch(
         "elmax_api.http.Elmax.get_panel_status",
         side_effect=ElmaxBadPinError(),
     ):
-        user_input = {
-            CONF_ELMAX_PANEL_NAME: MOCK_PANEL_NAME,
-            CONF_ELMAX_PANEL_PIN: "111111",
-        }
-        step_panels_result = await flow.async_step_panels(user_input=user_input)
-        assert step_panels_result["errors"] == {"base": "invalid_pin"}
+        result = await _bootstrap(hass)
+        assert result["step_id"] == "panels"
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["errors"] == {"base": "invalid_pin"}
 
 
 async def test_no_online_panel(hass):
     """Test no-online panel is available."""
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-
     # Simulate low-level api returns no panels.
     with patch(
         "elmax_api.http.Elmax.list_control_panels",
         return_value=[],
     ):
-        # Perform first step (user) to setup ConfigFlow internal state objects.
-        user_input = {
-            CONF_ELMAX_USERNAME: MOCK_USERNAME,
-            CONF_ELMAX_PASSWORD: "password",
-        }
-        result = await flow.async_step_user(user_input=user_input)
+        result = await _bootstrap(hass)
+        assert result["step_id"] == "user"
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
         assert result["errors"] == {"base": "no_panel_online"}
 
 
 async def test_step_user(hass):
     """Test that the user step works."""
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
-
-    # Try to add another instance of the integration for the very same panel.
-    user_input = {CONF_ELMAX_USERNAME: MOCK_USERNAME, CONF_ELMAX_PASSWORD: "password"}
-    step_user_result = await flow.async_step_user(user_input=user_input)
-    assert step_user_result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert step_user_result["step_id"] == "panels"
-
-    user_input = {
-        CONF_ELMAX_PANEL_NAME: MOCK_PANEL_NAME,
-        CONF_ELMAX_PANEL_PIN: MOCK_PANEL_PIN,
-    }
-    step_panels_result = await flow.async_step_panels(user_input=user_input)
-    assert step_panels_result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert step_panels_result["data"] == {
+    result = await _bootstrap(hass)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["data"] == {
         CONF_ELMAX_PANEL_ID: MOCK_PANEL_ID,
         CONF_ELMAX_PANEL_PIN: MOCK_PANEL_PIN,
         CONF_ELMAX_USERNAME: MOCK_USERNAME,
-        CONF_ELMAX_PASSWORD: "password",
+        CONF_ELMAX_PASSWORD: MOCK_PASSWORD,
     }
 
 
-async def test_step_panels_missing_input(hass):
-    """Test that the user step works."""
-    flow = config_flow.ConfigFlow()
-    flow.hass = hass
+async def test_show_reauth(hass):
+    """Test that the reauth form shows."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_REAUTH},
+        data={
+            CONF_ELMAX_PANEL_ID: MOCK_PANEL_ID,
+            CONF_ELMAX_PANEL_PIN: MOCK_PANEL_PIN,
+            CONF_ELMAX_USERNAME: MOCK_USERNAME,
+            CONF_ELMAX_PASSWORD: MOCK_PASSWORD,
+        },
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "reauth_confirm"
 
-    step_panels_result = await flow.async_step_panels(user_input=None)
-    assert step_panels_result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert step_panels_result["step_id"] == "user"
+
+async def test_reauth_flow(hass):
+    """Test that the reauth flow works."""
+    # Simulate a first setup
+    await _bootstrap(hass)
+    # Trigger reauth
+    result = await _reauth(hass)
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "reauth_successful"
 
 
-async def test_step_reauth(hass):
-    """Test the reauth flow."""
-    conf = {
-        CONF_ELMAX_USERNAME: MOCK_USERNAME,
-        CONF_ELMAX_PASSWORD: "password",
-    }
-
-    MockConfigEntry(
-        domain=DOMAIN,
-        data=conf,
-    ).add_to_hass(hass)
-
-    with patch("homeassistant.components.elmax.config_flow.ConfigFlow"):
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": SOURCE_REAUTH}, data=conf
-        )
-
+async def test_reauth_panel_disappeared(hass):
+    """Test that the case where panel is no longer associated with the user."""
+    # Simulate a first setup
+    await _bootstrap(hass)
+    # Trigger reauth
+    with patch(
+        "elmax_api.http.Elmax.list_control_panels",
+        return_value=[],
+    ):
+        result = await _reauth(hass)
+        assert result["step_id"] == "reauth_confirm"
         assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-        assert result["step_id"] == "user"
+        assert result["errors"] == {"base": "reauth_panel_disappeared"}
+
+
+async def test_reauth_invalid_pin(hass):
+    """Test that the case where panel is no longer associated with the user."""
+    # Simulate a first setup
+    await _bootstrap(hass)
+    # Trigger reauth
+    with patch(
+        "elmax_api.http.Elmax.get_panel_status",
+        side_effect=ElmaxBadPinError(),
+    ):
+        result = await _reauth(hass)
+        assert result["step_id"] == "reauth_confirm"
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["errors"] == {"base": "invalid_pin"}
+
+
+async def test_reauth_bad_login(hass):
+    """Test bad login attempt at reauth time."""
+    # Simulate a first setup
+    await _bootstrap(hass)
+    # Trigger reauth
+    with patch(
+        "elmax_api.http.Elmax.login",
+        side_effect=ElmaxBadLoginError(),
+    ):
+        result = await _reauth(hass)
+        assert result["step_id"] == "reauth_confirm"
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["errors"] == {"base": "bad_auth"}
