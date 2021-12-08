c9cebc5121c2dbf66601e9ca929f2c94c0036485
Erik <erik@montnemery.com>
2021-12-08 14:25:47 +0100

    Set state class for additional Tasmota sensors

--- homeassistant/components/tasmota/sensor.py
+++ homeassistant/components/tasmota/sensor.py
@@ -64,27 +64,48 @@ ICON = "icon"
 
 # A Tasmota sensor type may be mapped to either a device class or an icon, not both
 SENSOR_DEVICE_CLASS_ICON_MAP = {
-    hc.SENSOR_AMBIENT: {DEVICE_CLASS: DEVICE_CLASS_ILLUMINANCE},
-    hc.SENSOR_APPARENT_POWERUSAGE: {DEVICE_CLASS: DEVICE_CLASS_POWER},
+    hc.SENSOR_AMBIENT: {
+        DEVICE_CLASS: DEVICE_CLASS_ILLUMINANCE,
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
+    hc.SENSOR_APPARENT_POWERUSAGE: {
+        DEVICE_CLASS: DEVICE_CLASS_POWER,
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
     hc.SENSOR_BATTERY: {
         DEVICE_CLASS: DEVICE_CLASS_BATTERY,
         STATE_CLASS: STATE_CLASS_MEASUREMENT,
     },
-    hc.SENSOR_CCT: {ICON: "mdi:temperature-kelvin"},
-    hc.SENSOR_CO2: {DEVICE_CLASS: DEVICE_CLASS_CO2},
+    hc.SENSOR_CCT: {
+        ICON: "mdi:temperature-kelvin",
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
+    hc.SENSOR_CO2: {
+        DEVICE_CLASS: DEVICE_CLASS_CO2,
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
     hc.SENSOR_COLOR_BLUE: {ICON: "mdi:palette"},
     hc.SENSOR_COLOR_GREEN: {ICON: "mdi:palette"},
     hc.SENSOR_COLOR_RED: {ICON: "mdi:palette"},
-    hc.SENSOR_CURRENT: {ICON: "mdi:alpha-a-circle-outline"},
-    hc.SENSOR_DEWPOINT: {ICON: "mdi:weather-rainy"},
-    hc.SENSOR_DISTANCE: {ICON: "mdi:leak"},
+    hc.SENSOR_CURRENT: {
+        ICON: "mdi:alpha-a-circle-outline",
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
+    hc.SENSOR_DEWPOINT: {
+        ICON: "mdi:weather-rainy",
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
+    hc.SENSOR_DISTANCE: {ICON: "mdi:leak", STATE_CLASS: STATE_CLASS_MEASUREMENT},
     hc.SENSOR_ECO2: {ICON: "mdi:molecule-co2"},
     hc.SENSOR_FREQUENCY: {ICON: "mdi:current-ac"},
     hc.SENSOR_HUMIDITY: {
         DEVICE_CLASS: DEVICE_CLASS_HUMIDITY,
         STATE_CLASS: STATE_CLASS_MEASUREMENT,
     },
-    hc.SENSOR_ILLUMINANCE: {DEVICE_CLASS: DEVICE_CLASS_ILLUMINANCE},
+    hc.SENSOR_ILLUMINANCE: {
+        DEVICE_CLASS: DEVICE_CLASS_ILLUMINANCE,
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
     hc.SENSOR_STATUS_IP: {ICON: "mdi:ip-network"},
     hc.SENSOR_STATUS_LINK_COUNT: {ICON: "mdi:counter"},
     hc.SENSOR_MOISTURE: {ICON: "mdi:cup-water"},
@@ -98,8 +119,14 @@ SENSOR_DEVICE_CLASS_ICON_MAP = {
     hc.SENSOR_PM10: {ICON: "mdi:air-filter"},
     hc.SENSOR_PM1: {ICON: "mdi:air-filter"},
     hc.SENSOR_PM2_5: {ICON: "mdi:air-filter"},
-    hc.SENSOR_POWERFACTOR: {ICON: "mdi:alpha-f-circle-outline"},
-    hc.SENSOR_POWERUSAGE: {DEVICE_CLASS: DEVICE_CLASS_POWER},
+    hc.SENSOR_POWERFACTOR: {
+        ICON: "mdi:alpha-f-circle-outline",
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
+    hc.SENSOR_POWERUSAGE: {
+        DEVICE_CLASS: DEVICE_CLASS_POWER,
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
     hc.SENSOR_PRESSURE: {
         DEVICE_CLASS: DEVICE_CLASS_PRESSURE,
         STATE_CLASS: STATE_CLASS_MEASUREMENT,
@@ -109,11 +136,20 @@ SENSOR_DEVICE_CLASS_ICON_MAP = {
         STATE_CLASS: STATE_CLASS_MEASUREMENT,
     },
     hc.SENSOR_PROXIMITY: {ICON: "mdi:ruler"},
-    hc.SENSOR_REACTIVE_POWERUSAGE: {DEVICE_CLASS: DEVICE_CLASS_POWER},
+    hc.SENSOR_REACTIVE_POWERUSAGE: {
+        DEVICE_CLASS: DEVICE_CLASS_POWER,
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
     hc.SENSOR_STATUS_LAST_RESTART_TIME: {DEVICE_CLASS: DEVICE_CLASS_TIMESTAMP},
     hc.SENSOR_STATUS_RESTART_REASON: {ICON: "mdi:information-outline"},
-    hc.SENSOR_STATUS_SIGNAL: {DEVICE_CLASS: DEVICE_CLASS_SIGNAL_STRENGTH},
-    hc.SENSOR_STATUS_RSSI: {ICON: "mdi:access-point"},
+    hc.SENSOR_STATUS_SIGNAL: {
+        DEVICE_CLASS: DEVICE_CLASS_SIGNAL_STRENGTH,
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
+    hc.SENSOR_STATUS_RSSI: {
+        ICON: "mdi:access-point",
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
     hc.SENSOR_STATUS_SSID: {ICON: "mdi:access-point-network"},
     hc.SENSOR_TEMPERATURE: {
         DEVICE_CLASS: DEVICE_CLASS_TEMPERATURE,
@@ -126,8 +162,11 @@ SENSOR_DEVICE_CLASS_ICON_MAP = {
     },
     hc.SENSOR_TOTAL_START_TIME: {ICON: "mdi:progress-clock"},
     hc.SENSOR_TVOC: {ICON: "mdi:air-filter"},
-    hc.SENSOR_VOLTAGE: {ICON: "mdi:alpha-v-circle-outline"},
-    hc.SENSOR_WEIGHT: {ICON: "mdi:scale"},
+    hc.SENSOR_VOLTAGE: {
+        ICON: "mdi:alpha-v-circle-outline",
+        STATE_CLASS: STATE_CLASS_MEASUREMENT,
+    },
+    hc.SENSOR_WEIGHT: {ICON: "mdi:scale", STATE_CLASS: STATE_CLASS_MEASUREMENT},
     hc.SENSOR_YESTERDAY: {DEVICE_CLASS: DEVICE_CLASS_ENERGY},
 }
 
