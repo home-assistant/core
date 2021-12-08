5de86b58e19a128214c742f444ba8593c2d60248
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2021-12-08 21:49:31 +0000

    Remove unused values

--- homeassistant/components/homewizard/const.py
+++ homeassistant/components/homewizard/const.py
@@ -5,7 +5,6 @@ from datetime import timedelta
 
 DOMAIN = "homewizard"
 COORDINATOR = "coordinator"
-MANUFACTURER_NAME = "HomeWizard"
 PLATFORMS = ["sensor"]
 
 # Platform config.
@@ -13,18 +12,6 @@ CONF_SERIAL = "serial"
 CONF_PRODUCT_NAME = "product_name"
 CONF_PRODUCT_TYPE = "product_type"
 CONF_DEVICE = "device"
-CONF_API = "api"
-CONF_MODEL = "model"
-CONF_SW_VERSION = "sw_ver"
 CONF_DATA = "data"
 
 UPDATE_INTERVAL = timedelta(seconds=5)
-
-# Default values.
-DEFAULT_STR_VALUE = "undefined"
-DEVICE_DEFAULT_NAME = "P1 Meter"
-
-# Device models
-MODEL_P1 = "HWE-P1"
-MODEL_KWH_1 = "SDM230-wifi"
-MODEL_KWH_3 = "SDM630-wifi"
