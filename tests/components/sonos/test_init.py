66b473702c81742b81e631a4c967eb853b6d80a1
jjlawren <jjlawren@users.noreply.github.com>
2021-12-08 12:28:27 -0600

    Improve Sonos tests, begin adding coverage (#61198)
    * Update entity registry handling
    
    * Add and use fixtures to test setup via config entry
    
    * Remove legacy redundant tests
    
    * Remove unnecessary mock_coro
    
    * Remove unnecessary namespace change
    
    * Move zeroconf payload to fixture
    
    * Begin adding Sonos to codecov
    
    * Mock proper return value
    
    * Revert return value for platform

--- tests/components/sonos/test_init.py
+++ tests/components/sonos/test_init.py
@@ -5,14 +5,11 @@ from homeassistant import config_entries, data_entry_flow
 from homeassistant.components import sonos
 from homeassistant.setup import async_setup_component
 
-from tests.common import mock_coro
-
 
 async def test_creating_entry_sets_up_media_player(hass):
     """Test setting up Sonos loads the media player."""
     with patch(
         "homeassistant.components.sonos.media_player.async_setup_entry",
-        return_value=mock_coro(True),
     ) as mock_setup, patch("soco.discover", return_value=True):
         result = await hass.config_entries.flow.async_init(
             sonos.DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -32,7 +29,8 @@ async def test_creating_entry_sets_up_media_player(hass):
 async def test_configuring_sonos_creates_entry(hass):
     """Test that specifying config will create an entry."""
     with patch(
-        "homeassistant.components.sonos.async_setup_entry", return_value=mock_coro(True)
+        "homeassistant.components.sonos.async_setup_entry",
+        return_value=True,
     ) as mock_setup, patch("soco.discover", return_value=True):
         await async_setup_component(
             hass,
@@ -47,7 +45,8 @@ async def test_configuring_sonos_creates_entry(hass):
 async def test_not_configuring_sonos_not_creates_entry(hass):
     """Test that no config will not create an entry."""
     with patch(
-        "homeassistant.components.sonos.async_setup_entry", return_value=mock_coro(True)
+        "homeassistant.components.sonos.async_setup_entry",
+        return_value=True,
     ) as mock_setup, patch("soco.discover", return_value=True):
         await async_setup_component(hass, sonos.DOMAIN, {})
         await hass.async_block_till_done()
