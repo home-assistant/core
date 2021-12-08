3e0b7fc02a8c593920dfdac897c649a109ad0cd7
Poltorak Serguei <ps@z-wave.me>
2021-12-08 15:46:04 +0300

    unused command removed

--- homeassistant/components/zwave_me/const.py
+++ homeassistant/components/zwave_me/const.py
@@ -5,7 +5,6 @@ DOMAIN = "zwave_me"
 DOMAIN_DATA = f"{DOMAIN}_data"
 VERSION = "0.0.1"
 ATTRIBUTION = "Z-Wave-Me"
-ISSUE_URL = "https://github.com/Z-Wave-Me/ha-core/pulls"
 
 ZWAVE_PLATFORMS = [
     "switchMultilevel",
