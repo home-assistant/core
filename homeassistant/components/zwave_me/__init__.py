202c72f7b3accc24ece4beb6a8f54cc46a09609b
Poltorak Serguei <poltorak@alsenet.com>
2021-12-08 14:14:53 +0300

    Apply suggestions from code review
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/zwave_me/__init__.py
+++ homeassistant/components/zwave_me/__init__.py
@@ -43,7 +43,7 @@ class ZWaveMeController:
             platforms=ZWAVEPLATFORMS,
         )
 
-    def add_device(self, device: ZWaveMeData):
+    def add_device(self, device: ZWaveMeData) -> None:
         """Send signal to create device."""
         if device.deviceType in ZWAVEPLATFORMS:
             if device.id in self.device_ids:
@@ -54,14 +54,14 @@ class ZWaveMeController:
                 )
                 self.device_ids.append(device.id)
 
-    def on_device_create(self, devices: list[ZWaveMeData]):
+    def on_device_create(self, devices: list[ZWaveMeData]) -> None:
         """Create multiple devices."""
         if devices is None:
             return
         for device in devices:
             self.add_device(device)
 
-    def on_device_update(self, new_info: ZWaveMeData):
+    def on_device_update(self, new_info: ZWaveMeData) -> None:
         """Send signal to update device."""
         dispatcher_send(self._hass, "ZWAVE_ME_INFO_" + new_info.id, new_info)
 
@@ -94,7 +94,7 @@ class ZWaveMeEntity(Entity):
         """Update info in the HAss."""
         self.device = new_data
         self._attr_available = not newdata.isFailed
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state
 
     def get_available(self):
         """Get availability of a generic device."""
