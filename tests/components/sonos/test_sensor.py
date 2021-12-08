9f3a4c361773f3c821503da4ffab276ac6a0d892
jjlawren <jjlawren@users.noreply.github.com>
2021-12-08 12:28:27 -0600

    Improve Sonos tests, begin adding coverage (#61198)
    * Update entity registry handling
    
    * Add and use fixtures to test setup via config entry
    
    * Remove legacy redundant tests
    
    * Remove unnecessary mock_coro
    
    * Remove unnecessary namespace change
    
    * Move zeroconf payload to fixture
    
    * Begin adding Sonos to codecov
    
    * Mock proper return value
    
    * Revert return value for platform

--- tests/components/sonos/test_sensor.py
+++ tests/components/sonos/test_sensor.py
@@ -1,48 +1,36 @@
 """Tests for the Sonos battery sensor platform."""
 from soco.exceptions import NotSupportedException
 
-from homeassistant.components.sonos import DOMAIN
 from homeassistant.components.sonos.binary_sensor import ATTR_BATTERY_POWER_SOURCE
 from homeassistant.const import STATE_OFF, STATE_ON
-from homeassistant.setup import async_setup_component
+from homeassistant.helpers import entity_registry as ent_reg
 
 
-async def setup_platform(hass, config_entry, config):
-    """Set up the media player platform for testing."""
-    config_entry.add_to_hass(hass)
-    assert await async_setup_component(hass, DOMAIN, config)
-    await hass.async_block_till_done()
-
-
-async def test_entity_registry_unsupported(hass, config_entry, config, soco):
+async def test_entity_registry_unsupported(hass, async_setup_sonos, soco):
     """Test sonos device without battery registered in the device registry."""
     soco.get_battery_info.side_effect = NotSupportedException
 
-    await setup_platform(hass, config_entry, config)
+    await async_setup_sonos()
 
-    entity_registry = await hass.helpers.entity_registry.async_get_registry()
+    entity_registry = ent_reg.async_get(hass)
 
     assert "media_player.zone_a" in entity_registry.entities
     assert "sensor.zone_a_battery" not in entity_registry.entities
     assert "binary_sensor.zone_a_power" not in entity_registry.entities
 
 
-async def test_entity_registry_supported(hass, config_entry, config, soco):
+async def test_entity_registry_supported(hass, async_autosetup_sonos, soco):
     """Test sonos device with battery registered in the device registry."""
-    await setup_platform(hass, config_entry, config)
-
-    entity_registry = await hass.helpers.entity_registry.async_get_registry()
+    entity_registry = ent_reg.async_get(hass)
 
     assert "media_player.zone_a" in entity_registry.entities
     assert "sensor.zone_a_battery" in entity_registry.entities
     assert "binary_sensor.zone_a_power" in entity_registry.entities
 
 
-async def test_battery_attributes(hass, config_entry, config, soco):
+async def test_battery_attributes(hass, async_autosetup_sonos, soco):
     """Test sonos device with battery state."""
-    await setup_platform(hass, config_entry, config)
-
-    entity_registry = await hass.helpers.entity_registry.async_get_registry()
+    entity_registry = ent_reg.async_get(hass)
 
     battery = entity_registry.entities["sensor.zone_a_battery"]
     battery_state = hass.states.get(battery.entity_id)
@@ -57,16 +45,16 @@ async def test_battery_attributes(hass, config_entry, config, soco):
     )
 
 
-async def test_battery_on_S1(hass, config_entry, config, soco, battery_event):
+async def test_battery_on_S1(hass, async_setup_sonos, soco, battery_event):
     """Test battery state updates on a Sonos S1 device."""
     soco.get_battery_info.return_value = {}
 
-    await setup_platform(hass, config_entry, config)
+    await async_setup_sonos()
 
     subscription = soco.deviceProperties.subscribe.return_value
     sub_callback = subscription.callback
 
-    entity_registry = await hass.helpers.entity_registry.async_get_registry()
+    entity_registry = ent_reg.async_get(hass)
 
     assert "sensor.zone_a_battery" not in entity_registry.entities
     assert "binary_sensor.zone_a_power" not in entity_registry.entities
@@ -86,12 +74,12 @@ async def test_battery_on_S1(hass, config_entry, config, soco, battery_event):
 
 
 async def test_device_payload_without_battery(
-    hass, config_entry, config, soco, battery_event, caplog
+    hass, async_setup_sonos, soco, battery_event, caplog
 ):
     """Test device properties event update without battery info."""
     soco.get_battery_info.return_value = None
 
-    await setup_platform(hass, config_entry, config)
+    await async_setup_sonos()
 
     subscription = soco.deviceProperties.subscribe.return_value
     sub_callback = subscription.callback
@@ -106,12 +94,12 @@ async def test_device_payload_without_battery(
 
 
 async def test_device_payload_without_battery_and_ignored_keys(
-    hass, config_entry, config, soco, battery_event, caplog
+    hass, async_setup_sonos, soco, battery_event, caplog
 ):
     """Test device properties event update without battery info and ignored keys."""
     soco.get_battery_info.return_value = None
 
-    await setup_platform(hass, config_entry, config)
+    await async_setup_sonos()
 
     subscription = soco.deviceProperties.subscribe.return_value
     sub_callback = subscription.callback
@@ -125,11 +113,9 @@ async def test_device_payload_without_battery_and_ignored_keys(
     assert ignored_payload not in caplog.text
 
 
-async def test_audio_input_sensor(hass, config_entry, config, soco):
+async def test_audio_input_sensor(hass, async_autosetup_sonos, soco):
     """Test sonos device with battery state."""
-    await setup_platform(hass, config_entry, config)
-
-    entity_registry = await hass.helpers.entity_registry.async_get_registry()
+    entity_registry = ent_reg.async_get(hass)
 
     audio_input_sensor = entity_registry.entities["sensor.zone_a_audio_input_format"]
     audio_input_state = hass.states.get(audio_input_sensor.entity_id)
