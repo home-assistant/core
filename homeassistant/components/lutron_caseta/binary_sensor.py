1c8c7dd8a4b9764fee409b76b867073ccbae7a61
Joey Marianer <joey@rescale.com>
2021-12-10 20:21:26 +0000

    Support Lutron Pico remotes
    Support was recently added in pylutron_caseta and this change carries over the support here. Each Pico remote will show as up to five binary sensors (one for each button).
    
    I'm not sure what a good way is to test this (except manually: I managed to set up automation to turn off all my lights when I hit the remote), and am seeking feedback on that or anything else.

--- homeassistant/components/lutron_caseta/binary_sensor.py
+++ homeassistant/components/lutron_caseta/binary_sensor.py
@@ -1,5 +1,5 @@
 """Support for Lutron Caseta Occupancy/Vacancy Sensors."""
-from pylutron_caseta import OCCUPANCY_GROUP_OCCUPIED
+from pylutron_caseta import BUTTON_STATUS_PRESSED, OCCUPANCY_GROUP_OCCUPIED
 
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_OCCUPANCY,
@@ -26,6 +26,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         entity = LutronOccupancySensor(occupancy_group, bridge, bridge_device)
         entities.append(entity)
 
+    for button in bridge.buttons.values():
+        entity = LutronPicoButton(button, bridge, bridge_device)
+        entities.append(entity)
+
     async_add_entities(entities, True)
 
 
@@ -39,7 +43,7 @@ class LutronOccupancySensor(LutronCasetaDevice, BinarySensorEntity):
 
     @property
     def is_on(self):
-        """Return the brightness of the light."""
+        """Return True iff the sensor is on."""
         return self._device["status"] == OCCUPANCY_GROUP_OCCUPIED
 
     async def async_added_to_hass(self):
@@ -57,6 +61,52 @@ class LutronOccupancySensor(LutronCasetaDevice, BinarySensorEntity):
     def unique_id(self):
         """Return a unique identifier."""
         return f"occupancygroup_{self.device_id}"
+
+    @property
+    def device_info(self):
+        """Return the device info.
+
+        Sensor entities are aggregated from one or more physical
+        sensors by each room. Therefore, there shouldn't be devices
+        related to any sensor entities.
+        """
+        return None
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        return {"device_id": self.device_id}
+
+
+class LutronPicoButton(LutronCasetaDevice, BinarySensorEntity):
+    """Representation of a Lutron occupancy group."""
+
+    @property
+    def is_on(self):
+        """Return the brightness of the light."""
+        return self._device["current_state"] == BUTTON_STATUS_PRESSED
+
+    async def async_added_to_hass(self):
+        """Register callbacks."""
+        self._smartbridge.add_button_subscriber(self.device_id, self._handle_event)
+
+    def _handle_event(self, event_type):
+        self.async_write_ha_state()
+
+    @property
+    def name(self):
+        """Return the name of the device."""
+        return self._device["name"] + str(self._device["button_number"])
+
+    @property
+    def device_id(self):
+        """Return the device ID used for calling pylutron_caseta."""
+        return self._device["device_id"]
+
+    @property
+    def unique_id(self):
+        """Return a unique identifier."""
+        return f"pico_{self.device_id}"
 
     @property
     def device_info(self):
