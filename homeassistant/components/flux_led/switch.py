314e9a165896f711aef2d5025d35304419abe9e9
J. Nick Koston <nick@koston.org>
2021-12-11 14:24:45 -1000

    fix

--- homeassistant/components/flux_led/switch.py
+++ homeassistant/components/flux_led/switch.py
@@ -14,7 +14,7 @@ from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from . import FluxLedUpdateCoordinator
+from . import FluxLedUpdateCoordinator, _async_clear_discovery_cache
 from .const import (
     CONF_REMOTE_ACCESS_ENABLED,
     CONF_REMOTE_ACCESS_HOST,
@@ -85,6 +85,7 @@ class FluxRemoteAccessSwitch(FluxBaseEntity, SwitchEntity):
 
     async def _async_update_entry(self, new_state: bool) -> None:
         """Update the entry with the new state on success."""
+        _async_clear_discovery_cache(self.hass, self._device.ipaddr)
         self.hass.config_entries.async_update_entry(
             self.entry,
             data={**self.entry.data, CONF_REMOTE_ACCESS_ENABLED: new_state},
