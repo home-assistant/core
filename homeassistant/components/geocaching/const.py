3cc2e46b34d1f594dc5424acdfabb1a594c82418
Rudolf Offereins <r.offereins@gmail.com>
2021-12-13 23:17:49 +0000

    Use SensorEntityDescription for sensors

--- homeassistant/components/geocaching/const.py
+++ homeassistant/components/geocaching/const.py
@@ -2,17 +2,6 @@
 from __future__ import annotations
 
 from datetime import timedelta
-from typing import TYPE_CHECKING, Final
-
-from homeassistant.const import (
-    ATTR_ICON,
-    ATTR_NAME,
-    ATTR_STATE,
-    ATTR_UNIT_OF_MEASUREMENT,
-)
-
-if TYPE_CHECKING:
-    from .models import GeocachingSensorSettings
 
 DOMAIN = "geocaching"
 
@@ -21,51 +10,3 @@ OAUTH2_TOKEN_URL = "https://oauth-staging.geocaching.com/token"
 API_ENDPOINT_URL = "https://staging.api.groundspeak.com"
 
 UPDATE_INTERVAL = timedelta(hours=1)
-
-ATTR_DEFAULT_ENABLED: Final = "default_enabled"
-ATTR_SECTION: Final = "section"
-
-SENSOR_DATA: Final[dict[str, GeocachingSensorSettings]] = {
-    "username": {
-        ATTR_NAME: "Username",
-        ATTR_SECTION: "user",
-        ATTR_STATE: "username",
-        ATTR_ICON: "mdi:account",
-        ATTR_DEFAULT_ENABLED: False,
-    },
-    "find_count": {
-        ATTR_NAME: "Total finds",
-        ATTR_SECTION: "user",
-        ATTR_STATE: "find_count",
-        ATTR_UNIT_OF_MEASUREMENT: "caches",
-        ATTR_ICON: "mdi:notebook-edit-outline",
-    },
-    "hide_count": {
-        ATTR_NAME: "Total hides",
-        ATTR_SECTION: "user",
-        ATTR_STATE: "hide_count",
-        ATTR_UNIT_OF_MEASUREMENT: "caches",
-        ATTR_ICON: "mdi:eye-off-outline",
-    },
-    "favorite_points": {
-        ATTR_NAME: "Favorite points",
-        ATTR_SECTION: "user",
-        ATTR_STATE: "favorite_points",
-        ATTR_UNIT_OF_MEASUREMENT: "points",
-        ATTR_ICON: "mdi:heart-outline",
-    },
-    "souvenir_count": {
-        ATTR_NAME: "Total souvenirs",
-        ATTR_SECTION: "user",
-        ATTR_STATE: "souvenir_count",
-        ATTR_UNIT_OF_MEASUREMENT: "souvenirs",
-        ATTR_ICON: "mdi:license",
-    },
-    "awarded_favorite_points": {
-        ATTR_NAME: "Awarded favorite points",
-        ATTR_SECTION: "user",
-        ATTR_STATE: "awarded_favorite_points",
-        ATTR_UNIT_OF_MEASUREMENT: "points",
-        ATTR_ICON: "mdi:heart",
-    },
-}
--- homeassistant/components/geocaching/sensor.py
+++ homeassistant/components/geocaching/sensor.py
@@ -1,23 +1,90 @@
 """Platform for sensor integration."""
 from __future__ import annotations
 
-from typing import Any
+from dataclasses import dataclass
+from typing import Any, Final
 
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (
-    ATTR_ICON,
-    ATTR_NAME,
-    ATTR_STATE,
-    ATTR_UNIT_OF_MEASUREMENT,
-)
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import ATTR_DEFAULT_ENABLED, ATTR_SECTION, DOMAIN, SENSOR_DATA
+from .const import DOMAIN
 from .coordinator import GeocachingDataUpdateCoordinator
-from .models import GeocachingSensorSettings
+
+
+@dataclass
+class GeocachingRequiredKeysMixin:
+    """Mixin for required keys."""
+
+    default_enabled: bool
+    section: str
+    measurement: str
+
+
+@dataclass
+class GeocachingSensorEntityDescription(
+    SensorEntityDescription, GeocachingRequiredKeysMixin
+):
+    """Define Sensor entity description class."""
+
+
+SENSOR_DATA: Final[dict[str, GeocachingSensorEntityDescription]] = {
+    "username": GeocachingSensorEntityDescription(
+        key="username",
+        name="username",
+        section="user",
+        measurement="username",
+        icon="mdi:account",
+        default_enabled=False,
+    ),
+    "find_count": GeocachingSensorEntityDescription(
+        key="find_count",
+        name="Total finds",
+        section="user",
+        measurement="find_count",
+        icon="mdi:notebook-edit-outline",
+        native_unit_of_measurement="caches",
+        default_enabled=True,
+    ),
+    "hide_count": GeocachingSensorEntityDescription(
+        key="hide_count",
+        name="Total hides",
+        section="user",
+        measurement="hide_count",
+        icon="mdi:eye-off-outline",
+        native_unit_of_measurement="caches",
+        default_enabled=True,
+    ),
+    "favorite_points": GeocachingSensorEntityDescription(
+        key="favorite_points",
+        name="Favorite points",
+        section="user",
+        measurement="favorite_points",
+        icon="mdi:heart-outline",
+        native_unit_of_measurement="points",
+        default_enabled=True,
+    ),
+    "souvenir_count": GeocachingSensorEntityDescription(
+        key="souvenir_count",
+        name="Total souvenirs",
+        section="user",
+        measurement="souvenir_count",
+        icon="mdi:license",
+        native_unit_of_measurement="souvenirs",
+        default_enabled=True,
+    ),
+    "awarded_favorite_points": GeocachingSensorEntityDescription(
+        key="awarded_favorite_points",
+        name="Awarded favorite points",
+        section="user",
+        measurement="awarded_favorite_points",
+        icon="mdi:heart",
+        native_unit_of_measurement="points",
+        default_enabled=True,
+    ),
+}
 
 
 async def async_setup_entry(
@@ -27,7 +94,7 @@ async def async_setup_entry(
     coordinator = hass.data[DOMAIN][entry.entry_id]
     async_add_entities(
         [
-            GeocachingSensor(coordinator, key=key, settings=SENSOR_DATA[key])
+            GeocachingSensor(coordinator, key=key, entity_description=SENSOR_DATA[key])
             for key in SENSOR_DATA
         ]
     )
@@ -36,7 +103,7 @@ async def async_setup_entry(
 class GeocachingSensor(CoordinatorEntity, SensorEntity):
     """Representation of a Sensor."""
 
-    settings: GeocachingSensorSettings
+    entity_description: GeocachingSensorEntityDescription
     key: str
 
     def __init__(
@@ -44,27 +111,25 @@ class GeocachingSensor(CoordinatorEntity, SensorEntity):
         coordinator: GeocachingDataUpdateCoordinator,
         *,
         key: str,
-        settings: GeocachingSensorSettings,
+        entity_description: GeocachingSensorEntityDescription,
     ) -> None:
         """Initialize the Geocaching sensor."""
         super().__init__(coordinator)
-        self.settings = settings
+        self.entity_description = entity_description
         self.key = key
 
-        self._attr_entity_registry_enabled_default = settings.get(
-            ATTR_DEFAULT_ENABLED, True
+        self._attr_entity_registry_enabled_default = (
+            entity_description.default_enabled or True
         )
-        self._attr_icon = settings.get(ATTR_ICON)
         self._attr_name = (
-            f"Geocaching {coordinator.data.user.username} {settings[ATTR_NAME]}"
+            f"Geocaching {coordinator.data.user.username} {entity_description.name}"
         )
         self._attr_unique_id = (
             f"geocaching_{coordinator.data.user.reference_code}_{key}"
         )
-        self._attr_unit_of_measurement = settings.get(ATTR_UNIT_OF_MEASUREMENT)
 
     @property
-    def state(self) -> Any:
+    def native_value(self) -> Any | None:
         """Return the state of the sensor."""
-        section = getattr(self.coordinator.data, self.settings[ATTR_SECTION])
-        return getattr(section, self.settings[ATTR_STATE])
+        section = getattr(self.coordinator.data, self.entity_description.section)
+        return getattr(section, self.entity_description.measurement)
