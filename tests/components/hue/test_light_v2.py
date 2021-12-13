77b097e3ff249be32b0e586105b7227396d15eae
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-12-13 21:31:45 +0100

    fix turn_off with transition for grouped hue lights

--- tests/components/hue/test_light_v2.py
+++ tests/components/hue/test_light_v2.py
@@ -295,7 +295,12 @@ async def test_grouped_lights(hass, mock_bridge_v2, v2_resources_test_data):
     await hass.services.async_call(
         "light",
         "turn_on",
-        {"entity_id": test_light_id, "brightness_pct": 100, "xy_color": (0.123, 0.123)},
+        {
+            "entity_id": test_light_id,
+            "brightness_pct": 100,
+            "xy_color": (0.123, 0.123),
+            "transition": 6,
+        },
         blocking=True,
     )
 
@@ -308,6 +313,9 @@ async def test_grouped_lights(hass, mock_bridge_v2, v2_resources_test_data):
         )
         assert mock_bridge_v2.mock_requests[index]["json"]["color"]["xy"]["x"] == 0.123
         assert mock_bridge_v2.mock_requests[index]["json"]["color"]["xy"]["y"] == 0.123
+        assert (
+            mock_bridge_v2.mock_requests[index]["json"]["dynamics"]["duration"] == 6000
+        )
 
     # Now generate update events by emitting the json we've sent as incoming events
     for index in range(0, 3):
