eb577b6ee1af9734920eef94d096b31a4d54bb72
Lester Lo <21245380+lesterlo@users.noreply.github.com>
2021-12-13 16:27:34 +0000

    Add the pm10 and pm25 sensor tests

--- tests/components/homekit/test_type_sensors.py
+++ tests/components/homekit/test_type_sensors.py
@@ -14,6 +14,8 @@ from homeassistant.components.homekit.type_sensors import (
     CarbonMonoxideSensor,
     HumiditySensor,
     LightSensor,
+    PM10Sensor,
+    PM25Sensor,
     TemperatureSensor,
 )
 from homeassistant.const import (
@@ -98,6 +100,70 @@ async def test_air_quality(hass, hk_driver):
     hass.states.async_set(entity_id, None)
     await hass.async_block_till_done()
     acc = AirQualitySensor(hass, hk_driver, "Air Quality", entity_id, 2, None)
+    await acc.run()
+    await hass.async_block_till_done()
+
+    assert acc.aid == 2
+    assert acc.category == 10  # Sensor
+
+    assert acc.char_density.value == 0
+    assert acc.char_quality.value == 0
+
+    hass.states.async_set(entity_id, STATE_UNKNOWN)
+    await hass.async_block_till_done()
+    assert acc.char_density.value == 0
+    assert acc.char_quality.value == 0
+
+    hass.states.async_set(entity_id, "34")
+    await hass.async_block_till_done()
+    assert acc.char_density.value == 34
+    assert acc.char_quality.value == 1
+
+    hass.states.async_set(entity_id, "200")
+    await hass.async_block_till_done()
+    assert acc.char_density.value == 200
+    assert acc.char_quality.value == 5
+
+
+async def test_pm10(hass, hk_driver):
+    """Test if accessory is updated after state change."""
+    entity_id = "sensor.air_quality_pm10"
+
+    hass.states.async_set(entity_id, None)
+    await hass.async_block_till_done()
+    acc = PM10Sensor(hass, hk_driver, "PM10 Sensor", entity_id, 2, None)
+    await acc.run()
+    await hass.async_block_till_done()
+
+    assert acc.aid == 2
+    assert acc.category == 10  # Sensor
+
+    assert acc.char_density.value == 0
+    assert acc.char_quality.value == 0
+
+    hass.states.async_set(entity_id, STATE_UNKNOWN)
+    await hass.async_block_till_done()
+    assert acc.char_density.value == 0
+    assert acc.char_quality.value == 0
+
+    hass.states.async_set(entity_id, "34")
+    await hass.async_block_till_done()
+    assert acc.char_density.value == 34
+    assert acc.char_quality.value == 1
+
+    hass.states.async_set(entity_id, "200")
+    await hass.async_block_till_done()
+    assert acc.char_density.value == 200
+    assert acc.char_quality.value == 5
+
+
+async def test_pm25(hass, hk_driver):
+    """Test if accessory is updated after state change."""
+    entity_id = "sensor.air_quality_pm25"
+
+    hass.states.async_set(entity_id, None)
+    await hass.async_block_till_done()
+    acc = PM25Sensor(hass, hk_driver, "PM25 Sensor", entity_id, 2, None)
     await acc.run()
     await hass.async_block_till_done()
 
