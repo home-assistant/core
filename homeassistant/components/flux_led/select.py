cbd529f340ea344c98be50d19b9688cf58f81383
J. Nick Koston <nick@koston.org>
2021-12-16 05:10:02 -0600

    tweaks

--- homeassistant/components/flux_led/select.py
+++ homeassistant/components/flux_led/select.py
@@ -26,10 +26,6 @@ async def async_setup_entry(
     async_add_entities([FluxPowerState(coordinator.device, entry)])
 
 
-def _human_readable_option(const_option: str) -> str:
-    return const_option.replace("_", " ").title()
-
-
 class FluxPowerState(FluxBaseEntity, SelectEntity):
     """Representation of a Flux power restore state option."""
 
@@ -47,7 +43,8 @@ class FluxPowerState(FluxBaseEntity, SelectEntity):
         if entry.unique_id:
             self._attr_unique_id = f"{entry.unique_id}_power_restored"
         self._name_to_state = {
-            _human_readable_option(option.name): option for option in PowerRestoreState
+            option.name.replace("_", " ").title(): option
+            for option in PowerRestoreState
         }
         self._attr_options = list(self._name_to_state)
         self._async_set_current_option_from_device()
