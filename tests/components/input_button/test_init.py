dbf82be259601bc07d9567d85992b3c136e7bba2
Franck Nijhof <git@frenck.dev>
2021-12-16 12:11:16 +0100

    Improve test coverage

--- tests/components/input_button/test_init.py
+++ tests/components/input_button/test_init.py
@@ -241,6 +241,38 @@ async def test_ws_list(hass, hass_ws_client, storage_setup):
     assert result[storage_ent][ATTR_NAME] == "from storage"
 
 
+async def test_ws_create_update(hass, hass_ws_client, storage_setup):
+    """Test creating and updating via WS."""
+    assert await storage_setup(config={DOMAIN: {}})
+
+    client = await hass_ws_client(hass)
+
+    await client.send_json({"id": 7, "type": f"{DOMAIN}/create", "name": "new"})
+    resp = await client.receive_json()
+    assert resp["success"]
+
+    state = hass.states.get(f"{DOMAIN}.new")
+    assert state is not None
+    assert state.state == STATE_UNKNOWN
+    assert state.attributes.get(ATTR_FRIENDLY_NAME) == "new"
+
+    ent_reg = er.async_get(hass)
+    assert ent_reg.async_get_entity_id(DOMAIN, DOMAIN, "new") is not None
+
+    await client.send_json(
+        {"id": 8, "type": f"{DOMAIN}/update", f"{DOMAIN}_id": "new", "name": "newer"}
+    )
+    resp = await client.receive_json()
+    assert resp["success"]
+
+    state = hass.states.get(f"{DOMAIN}.new")
+    assert state is not None
+    assert state.state == STATE_UNKNOWN
+    assert state.attributes.get(ATTR_FRIENDLY_NAME) == "newer"
+
+    assert ent_reg.async_get_entity_id(DOMAIN, DOMAIN, "new") is not None
+
+
 async def test_ws_delete(hass, hass_ws_client, storage_setup):
     """Test WS delete cleans up entity registry."""
     assert await storage_setup()
