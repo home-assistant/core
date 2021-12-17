0c7f1f82aaa1157f2438f20a7ab5461269260984
Robert Hillis <tkdrob4390@yahoo.com>
2021-12-17 08:30:28 -0500

    uno mas

--- tests/components/rest/test_sensor.py
+++ tests/components/rest/test_sensor.py
@@ -264,7 +264,7 @@ async def test_setup_timestamp(hass, caplog):
     )
     state = hass.states.get("sensor.rest_sensor")
     assert state.state == "unknown"
-    assert state.attributes[ATTR_DEVICE_CLASS] is SensorDeviceClass.TIMESTAMP
+    assert state.attributes[ATTR_DEVICE_CLASS] is SensorDeviceClass.TIMESTAMP.value
     assert "sensor.rest_sensor rendered invalid timestamp" in caplog.text
 
     # Bad response: No timezone
@@ -279,7 +279,7 @@ async def test_setup_timestamp(hass, caplog):
     )
     state = hass.states.get("sensor.rest_sensor")
     assert state.state == "unknown"
-    assert state.attributes[ATTR_DEVICE_CLASS] is SensorDeviceClass.TIMESTAMP
+    assert state.attributes[ATTR_DEVICE_CLASS] is SensorDeviceClass.TIMESTAMP.value
     assert "sensor.rest_sensor rendered timestamp without timezone" in caplog.text
 
 
