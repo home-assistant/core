d65b2dffb9aa02ca9e546c0a421823b48642a1e8
lawtancool <26829131+lawtancool@users.noreply.github.com>
2021-12-17 22:10:26 -0800

    Remove unused imports from director_utils.py

--- homeassistant/components/control4/director_utils.py
+++ homeassistant/components/control4/director_utils.py
@@ -1,40 +1,21 @@
 """Provides data updates from the Control4 controller for platforms."""
 import json
-import logging
-
-from pyControl4.account import C4Account
-from pyControl4.director import C4Director
-from pyControl4.error_handling import BadToken
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_TOKEN, CONF_USERNAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import aiohttp_client
-from homeassistant.helpers.event import async_track_point_in_utc_time
 
 from .const import (
-    CONF_ACCOUNT,
-    CONF_CONTROLLER_UNIQUE_ID,
     CONF_DIRECTOR,
-    CONF_DIRECTOR_TOKEN_EXPIRATION,
     DOMAIN,
 )
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def director_get_entry_variables(
     hass: HomeAssistant, entry: ConfigEntry, item_id: int
 ) -> dict:
     """Retrieve variable data for Control4 entity."""
-    try:
-        director = hass.data[DOMAIN][entry.entry_id][CONF_DIRECTOR]
-        data = await director.getItemVariables(item_id)
-    except BadToken:
-        _LOGGER.info("Updating Control4 director token")
-        await refresh_tokens(hass, entry)
-        director = hass.data[DOMAIN][entry.entry_id][CONF_DIRECTOR]
-        data = await director.getItemVariables(item_id)
+    director = hass.data[DOMAIN][entry.entry_id][CONF_DIRECTOR]
+    data = await director.getItemVariables(item_id)
 
     result = {}
     for item in json.loads(data):
