0390c7ef0d4d985c34b8ba76185fdf9a99e5b3ec
J. Nick Koston <nick@koston.org>
2021-12-19 01:18:24 -0600

    Add support for changing Magic Home socket power restore state

--- homeassistant/components/flux_led/__init__.py
+++ homeassistant/components/flux_led/__init__.py
@@ -41,7 +41,7 @@ _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS_BY_TYPE: Final = {
     DeviceType.Bulb: [Platform.LIGHT, Platform.NUMBER, Platform.SWITCH],
-    DeviceType.Switch: [Platform.SWITCH],
+    DeviceType.Switch: [Platform.SELECT, Platform.SWITCH],
 }
 DISCOVERY_INTERVAL: Final = timedelta(minutes=15)
 REQUEST_REFRESH_DELAY: Final = 1.5
