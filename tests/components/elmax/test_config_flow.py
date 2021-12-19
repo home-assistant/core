f6483708fb9d6e9bdbb06b90a7c2fe1b5a785fff
Alberto Geniola <albertogeniola@gmail.com>
2021-12-19 22:50:12 +0100

    Correct Adobe typo.

--- tests/components/elmax/test_config_flow.py
+++ tests/components/elmax/test_config_flow.py
@@ -1,4 +1,4 @@
-"""Tests for the Abode config flow."""
+"""Tests for the Elmax config flow."""
 from unittest.mock import patch
 
 from elmax_api.exceptions import ElmaxBadLoginError, ElmaxBadPinError, ElmaxNetworkError
