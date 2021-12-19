d0420cee595b8ebfd9ad1643c9dad185bbc8bb61
J. Nick Koston <nick@koston.org>
2021-12-19 10:57:33 -0600

    Add switch to enable/disable music mode in Magic Home/flux_led (#62320)

--- tests/components/flux_led/test_switch.py
+++ tests/components/flux_led/test_switch.py
@@ -1,4 +1,6 @@
 """Tests for switch platform."""
+from flux_led.const import MODE_MUSIC
+
 from homeassistant.components import flux_led
 from homeassistant.components.flux_led.const import CONF_REMOTE_ACCESS_ENABLED, DOMAIN
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
@@ -95,3 +97,46 @@ async def test_remote_access_on_off(hass: HomeAssistant) -> None:
 
     assert hass.states.get(entity_id).state == STATE_ON
     assert config_entry.data[CONF_REMOTE_ACCESS_ENABLED] is True
+
+
+async def test_music_mode_switch(hass: HomeAssistant) -> None:
+    """Test music mode switch."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_HOST: IP_ADDRESS, CONF_NAME: DEFAULT_ENTRY_TITLE},
+        unique_id=MAC_ADDRESS,
+    )
+    config_entry.add_to_hass(hass)
+    bulb = _mocked_bulb()
+    bulb.raw_state = bulb.raw_state._replace(model_num=0xA3)  # has music mode
+    bulb.microphone = True
+    with _patch_discovery(), _patch_wifibulb(device=bulb):
+        await async_setup_component(hass, flux_led.DOMAIN, {flux_led.DOMAIN: {}})
+        await hass.async_block_till_done()
+
+    entity_id = "switch.bulb_rgbcw_ddeeff_music"
+
+    assert hass.states.get(entity_id).state == STATE_OFF
+
+    bulb.effect = MODE_MUSIC
+    bulb.is_on = False
+    await hass.services.async_call(
+        SWITCH_DOMAIN, "turn_on", {ATTR_ENTITY_ID: entity_id}, blocking=True
+    )
+    bulb.async_set_music_mode.assert_called_once()
+    assert hass.states.get(entity_id).state == STATE_OFF
+
+    bulb.async_set_music_mode.reset_mock()
+    bulb.is_on = True
+    await hass.services.async_call(
+        SWITCH_DOMAIN, "turn_on", {ATTR_ENTITY_ID: entity_id}, blocking=True
+    )
+    bulb.async_set_music_mode.assert_called_once()
+    assert hass.states.get(entity_id).state == STATE_ON
+
+    bulb.effect = None
+    await hass.services.async_call(
+        SWITCH_DOMAIN, "turn_off", {ATTR_ENTITY_ID: entity_id}, blocking=True
+    )
+    bulb.async_set_levels.assert_called_once()
+    assert hass.states.get(entity_id).state == STATE_OFF
