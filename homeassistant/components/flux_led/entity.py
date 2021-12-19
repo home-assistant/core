e7df090833870e52b7cb5334feb0541b65b5daf0
J. Nick Koston <nick@koston.org>
2021-12-19 00:59:16 -0600

    Add support for enabling/disabling cloud access in flux_led (#61138)
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/flux_led/entity.py
+++ homeassistant/components/flux_led/entity.py
@@ -6,18 +6,56 @@ from typing import Any
 
 from flux_led.aiodevice import AIOWifiLedBulb
 
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
 from homeassistant.core import callback
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity import DeviceInfo, Entity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from . import FluxLedUpdateCoordinator
-from .const import SIGNAL_STATE_UPDATED
+from .const import CONF_MINOR_VERSION, CONF_MODEL, SIGNAL_STATE_UPDATED
+
+
+def _async_device_info(
+    unique_id: str, device: AIOWifiLedBulb, entry: config_entries.ConfigEntry
+) -> DeviceInfo:
+    version_num = device.version_num
+    if minor_version := entry.data.get(CONF_MINOR_VERSION):
+        sw_version = version_num + int(hex(minor_version)[2:]) / 100
+        sw_version_str = f"{sw_version:0.3f}"
+    else:
+        sw_version_str = str(device.version_num)
+    return DeviceInfo(
+        connections={(dr.CONNECTION_NETWORK_MAC, unique_id)},
+        manufacturer="Zengge",
+        model=device.model,
+        name=entry.data[CONF_NAME],
+        sw_version=sw_version_str,
+        hw_version=entry.data.get(CONF_MODEL),
+    )
+
+
+class FluxBaseEntity(Entity):
+    """Representation of a Flux entity without a coordinator."""
+
+    def __init__(
+        self,
+        device: AIOWifiLedBulb,
+        entry: config_entries.ConfigEntry,
+    ) -> None:
+        """Initialize the light."""
+        self._device: AIOWifiLedBulb = device
+        self.entry = entry
+        if entry.unique_id:
+            self._attr_device_info = _async_device_info(
+                entry.unique_id, self._device, entry
+            )
 
 
 class FluxEntity(CoordinatorEntity):
-    """Representation of a Flux entity."""
+    """Representation of a Flux entity with a coordinator."""
 
     coordinator: FluxLedUpdateCoordinator
 
@@ -33,13 +71,9 @@ class FluxEntity(CoordinatorEntity):
         self._responding = True
         self._attr_name = name
         self._attr_unique_id = unique_id
-        if self.unique_id:
-            self._attr_device_info = DeviceInfo(
-                connections={(dr.CONNECTION_NETWORK_MAC, self.unique_id)},
-                manufacturer="Magic Home (Zengge)",
-                model=self._device.model,
-                name=self.name,
-                sw_version=str(self._device.version_num),
+        if unique_id:
+            self._attr_device_info = _async_device_info(
+                unique_id, self._device, coordinator.entry
             )
 
     @property
--- homeassistant/components/flux_led/switch.py
+++ homeassistant/components/flux_led/switch.py
@@ -3,16 +3,26 @@ from __future__ import annotations
 
 from typing import Any
 
+from flux_led import DeviceType
+from flux_led.aio import AIOWifiLedBulb
+
 from homeassistant import config_entries
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from . import FluxLedUpdateCoordinator
-from .const import DOMAIN
-from .entity import FluxOnOffEntity
+from .const import (
+    CONF_REMOTE_ACCESS_ENABLED,
+    CONF_REMOTE_ACCESS_HOST,
+    CONF_REMOTE_ACCESS_PORT,
+    DOMAIN,
+)
+from .discovery import async_clear_discovery_cache
+from .entity import FluxBaseEntity, FluxOnOffEntity
 
 
 async def async_setup_entry(
@@ -22,15 +32,22 @@ async def async_setup_entry(
 ) -> None:
     """Set up the Flux lights."""
     coordinator: FluxLedUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities(
-        [
+    entities: list[FluxSwitch | FluxRemoteAccessSwitch] = []
+
+    if coordinator.device.device_type == DeviceType.Switch:
+        entities.append(
             FluxSwitch(
                 coordinator,
                 entry.unique_id,
                 entry.data[CONF_NAME],
             )
-        ]
-    )
+        )
+
+    if entry.data.get(CONF_REMOTE_ACCESS_HOST):
+        entities.append(FluxRemoteAccessSwitch(coordinator.device, entry))
+
+    if entities:
+        async_add_entities(entities)
 
 
 class FluxSwitch(FluxOnOffEntity, CoordinatorEntity, SwitchEntity):
@@ -40,3 +57,53 @@ class FluxSwitch(FluxOnOffEntity, CoordinatorEntity, SwitchEntity):
         """Turn the device on."""
         if not self.is_on:
             await self._device.async_turn_on()
+
+
+class FluxRemoteAccessSwitch(FluxBaseEntity, SwitchEntity):
+    """Representation of a Flux remote access switch."""
+
+    _attr_should_poll = False
+    _attr_entity_category = EntityCategory.CONFIG
+
+    def __init__(
+        self,
+        device: AIOWifiLedBulb,
+        entry: config_entries.ConfigEntry,
+    ) -> None:
+        """Initialize the light."""
+        super().__init__(device, entry)
+        self._attr_name = f"{entry.data[CONF_NAME]} Remote Access"
+        if entry.unique_id:
+            self._attr_unique_id = f"{entry.unique_id}_remote_access"
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the remote access on."""
+        await self._device.async_enable_remote_access(
+            self.entry.data[CONF_REMOTE_ACCESS_HOST],
+            self.entry.data[CONF_REMOTE_ACCESS_PORT],
+        )
+        await self._async_update_entry(True)
+
+    async def _async_update_entry(self, new_state: bool) -> None:
+        """Update the entry with the new state on success."""
+        async_clear_discovery_cache(self.hass, self._device.ipaddr)
+        self.hass.config_entries.async_update_entry(
+            self.entry,
+            data={**self.entry.data, CONF_REMOTE_ACCESS_ENABLED: new_state},
+        )
+        self.async_write_ha_state()
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the remote access off."""
+        await self._device.async_disable_remote_access()
+        await self._async_update_entry(False)
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if remote access is enabled."""
+        return bool(self.entry.data[CONF_REMOTE_ACCESS_ENABLED])
+
+    @property
+    def icon(self) -> str:
+        """Return icon based on state."""
+        return "mdi:cloud-outline" if self.is_on else "mdi:cloud-off-outline"
