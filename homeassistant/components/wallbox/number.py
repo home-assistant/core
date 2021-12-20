50490172718b1f446758fe62ffa4624801693b2f
epenet <epenet@users.noreply.github.com>
2021-12-20 20:46:01 +0000

    Use a string for wallbox number device class

--- homeassistant/components/wallbox/number.py
+++ homeassistant/components/wallbox/number.py
@@ -6,7 +6,6 @@ from typing import Optional, cast
 
 from homeassistant.components.number import NumberEntity, NumberEntityDescription
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import DEVICE_CLASS_CURRENT
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
@@ -24,7 +23,7 @@ NUMBER_TYPES: dict[str, WallboxNumberEntityDescription] = {
     CONF_MAX_CHARGING_CURRENT_KEY: WallboxNumberEntityDescription(
         key=CONF_MAX_CHARGING_CURRENT_KEY,
         name="Max. Charging Current",
-        device_class=DEVICE_CLASS_CURRENT,
+        device_class="current",
         min_value=6,
     ),
 }
