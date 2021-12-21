e8c972c55d18dd941e4839b3ef713ae0b78c4e6e
Franck Nijhof <git@frenck.dev>
2021-12-21 17:13:53 +0100

    Deprecate Sensirion SHT31 integration (ADR-0019) (#62496)

--- homeassistant/components/sht31/sensor.py
+++ homeassistant/components/sht31/sensor.py
@@ -84,6 +84,13 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
+    _LOGGER.warning(
+        "The Sensirion SHT31 integration is deprecated and will be removed "
+        "in Home Assistant Core 2022.4; this integration is removed under "
+        "Architectural Decision Record 0019, more information can be found here: "
+        "https://github.com/home-assistant/architecture/blob/master/adr/0019-GPIO.md"
+    )
+
     name = config[CONF_NAME]
     monitored_conditions = config[CONF_MONITORED_CONDITIONS]
     i2c_address = config[CONF_I2C_ADDRESS]
