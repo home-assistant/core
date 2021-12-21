119eb925d4c557531e46dd0af0f6aa36e2835662
Eugenio Panadero <eugenio.panadero@gmail.com>
2021-12-21 21:24:53 +0100

    :sparkles: Upgrade sensor platform to use DataUpdateCoordinator and modern descriptions
    * Remove `RestoreEntity` inheritance for `ElecPriceSensor`,
      as it has no meaning for this one.
    * Use `SensorEntityDescription` and `DeviceInfo` attributes for better
      definition of the price sensor, and get ready for future sensors for this service
    * Add an `ElecPricesDataUpdateCoordinator` with the API handler,
      use `UpdateFailed` when price becomes unavailable, and relay on DataUpdateCoordinator
      for the availability state.
    
    As the DUC can't manage the scheduled state changes at x:00h, AFAIK,
    the `async_track_time_change` for the `update_current_price` is maintained in
    the `async_added_to_hass`, but the call for download each half hour is now
    handled by the DUC (the API handler won't call the external API each time,
    it will only make 1-2 calls/day)

--- homeassistant/components/pvpc_hourly_pricing/sensor.py
+++ homeassistant/components/pvpc_hourly_pricing/sensor.py
@@ -1,73 +1,78 @@
 """Sensor to collect the reference daily prices of electricity ('PVPC') in Spain."""
 from __future__ import annotations
 
+from datetime import datetime
 import logging
-from random import randint
-from typing import Any
+from typing import Any, Mapping
 
-from aiopvpc import PVPCData
-
-from homeassistant.components.sensor import SensorEntity, SensorStateClass
+from homeassistant.components.sensor import (
+    STATE_CLASS_MEASUREMENT,
+    SensorEntity,
+    SensorEntityDescription,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_NAME, CURRENCY_EURO, ENERGY_KILO_WATT_HOUR
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.entity import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.event import async_call_later, async_track_time_change
-from homeassistant.helpers.restore_state import RestoreEntity
-import homeassistant.util.dt as dt_util
+from homeassistant.helpers.event import async_track_time_change
+from homeassistant.helpers.typing import StateType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import ATTR_POWER, ATTR_POWER_P3, ATTR_TARIFF
+from . import ElecPricesDataUpdateCoordinator
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
-
-ATTR_PRICE = "price"
-ICON = "mdi:currency-eur"
-UNIT = f"{CURRENCY_EURO}/{ENERGY_KILO_WATT_HOUR}"
-
-_DEFAULT_TIMEOUT = 10
+PARALLEL_UPDATES = 1
+SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
+    SensorEntityDescription(
+        key="PVPC",
+        icon="mdi:currency-eur",
+        native_unit_of_measurement=f"{CURRENCY_EURO}/{ENERGY_KILO_WATT_HOUR}",
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+)
 
 
 async def async_setup_entry(
-    hass: HomeAssistant,
-    config_entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback,
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up the electricity price sensor from config_entry."""
-    name = config_entry.data[CONF_NAME]
-    pvpc_data_handler = PVPCData(
-        session=async_get_clientsession(hass),
-        tariff=config_entry.data[ATTR_TARIFF],
-        power=config_entry.data[ATTR_POWER],
-        power_valley=config_entry.data[ATTR_POWER_P3],
-        local_timezone=hass.config.time_zone,
-        timeout=_DEFAULT_TIMEOUT,
-    )
+    coordinator: ElecPricesDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+    name = entry.data[CONF_NAME]
     async_add_entities(
-        [ElecPriceSensor(name, config_entry.unique_id, pvpc_data_handler)], False
+        [ElecPriceSensor(coordinator, SENSOR_TYPES[0], entry.unique_id, name)]
     )
 
 
-class ElecPriceSensor(RestoreEntity, SensorEntity):
+class ElecPriceSensor(CoordinatorEntity, SensorEntity):
     """Class to hold the prices of electricity as a sensor."""
 
-    _attr_icon = ICON
-    _attr_native_unit_of_measurement = UNIT
-    _attr_should_poll = False
-    _attr_state_class = SensorStateClass.MEASUREMENT
+    coordinator: ElecPricesDataUpdateCoordinator
 
-    def __init__(self, name, unique_id, pvpc_data_handler):
-        """Initialize the sensor object."""
-        self._name = name
-        self._unique_id = unique_id
-        self._pvpc_data = pvpc_data_handler
-        self._num_retries = 0
+    def __init__(
+        self,
+        coordinator: ElecPricesDataUpdateCoordinator,
+        description: SensorEntityDescription,
+        unique_id: str | None,
+        name: str,
+    ) -> None:
+        """Initialize ESIOS sensor."""
+        super().__init__(coordinator)
+        self.entity_description = description
+        self._attr_unique_id = unique_id
+        self._attr_name = name
+        self._attr_device_info = DeviceInfo(
+            configuration_url="https://www.ree.es/es/apidatos",
+            entry_type=DeviceEntryType.SERVICE,
+            identifiers={(DOMAIN, coordinator.entry_id)},
+            manufacturer="REE",
+            name="PVPC (REData API)",
+        )
 
     async def async_added_to_hass(self) -> None:
         """Handle entity which will be added."""
         await super().async_added_to_hass()
-        if state := await self.async_get_last_state():
-            self._pvpc_data.state = state.state
 
         # Update 'state' value in hour changes
         self.async_on_remove(
@@ -75,86 +80,25 @@ class ElecPriceSensor(RestoreEntity, SensorEntity):
                 self.hass, self.update_current_price, second=[0], minute=[0]
             )
         )
-        # Update prices at random time, 2 times/hour (don't want to upset API)
-        random_minute = randint(1, 29)
-        mins_update = [random_minute, random_minute + 30]
-        self.async_on_remove(
-            async_track_time_change(
-                self.hass, self.async_update_prices, second=[0], minute=mins_update
-            )
-        )
         _LOGGER.debug(
-            "Setup of price sensor %s (%s) with tariff '%s', "
-            "updating prices each hour at %s min",
+            "Setup of price sensor %s (%s) with tariff '%s'",
             self.name,
             self.entity_id,
-            self._pvpc_data.tariff,
-            mins_update,
+            self.coordinator.api.tariff,
         )
-        now = dt_util.utcnow()
-        await self.async_update_prices(now)
-        self.update_current_price(now)
-
-    @property
-    def unique_id(self) -> str | None:
-        """Return a unique ID."""
-        return self._unique_id
-
-    @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def native_value(self) -> float:
-        """Return the state of the sensor."""
-        return self._pvpc_data.state
-
-    @property
-    def available(self) -> bool:
-        """Return True if entity is available."""
-        return self._pvpc_data.state_available
-
-    @property
-    def extra_state_attributes(self) -> dict[str, Any]:
-        """Return the state attributes."""
-        return self._pvpc_data.attributes
 
     @callback
-    def update_current_price(self, now):
+    def update_current_price(self, now: datetime) -> None:
         """Update the sensor state, by selecting the current price for this hour."""
-        self._pvpc_data.process_state_and_attributes(now)
+        self.coordinator.api.process_state_and_attributes(now)
         self.async_write_ha_state()
 
-    async def async_update_prices(self, now):
-        """Update electricity prices from the ESIOS API."""
-        prices = await self._pvpc_data.async_update_prices(now)
-        if not prices and self._pvpc_data.source_available:
-            self._num_retries += 1
-            if self._num_retries > 2:
-                _LOGGER.warning(
-                    "%s: repeated bad data update, mark component as unavailable source",
-                    self.entity_id,
-                )
-                self._pvpc_data.source_available = False
-                return
+    @property
+    def native_value(self) -> StateType:
+        """Return the state of the sensor."""
+        return self.coordinator.api.state
 
-            retry_delay = 2 * self._num_retries * self._pvpc_data.timeout
-            _LOGGER.debug(
-                "%s: Bad update[retry:%d], will try again in %d s",
-                self.entity_id,
-                self._num_retries,
-                retry_delay,
-            )
-            async_call_later(self.hass, retry_delay, self.async_update_prices)
-            return
-
-        if not prices:
-            _LOGGER.debug("%s: data source is not yet available", self.entity_id)
-            return
-
-        self._num_retries = 0
-        if not self._pvpc_data.source_available:
-            self._pvpc_data.source_available = True
-            _LOGGER.warning("%s: component has recovered data access", self.entity_id)
-            self.update_current_price(now)
+    @property
+    def extra_state_attributes(self) -> Mapping[str, Any] | None:
+        """Return the state attributes."""
+        return self.coordinator.api.attributes
