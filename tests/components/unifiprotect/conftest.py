bb81f7d242ff21e728a5d5283f76d3c77b40b62c
Christopher Bailey <cbailey@mort.is>
2021-12-24 14:10:02 -0500

    Missing test files

---
+++ tests/components/unifiprotect/conftest.py
@@ -0,0 +1,58 @@
+"""Fixtures and test data for UniFi Protect methods."""
+
+from dataclasses import dataclass
+from typing import Any
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+from pyunifiprotect.data.types import Version
+
+from homeassistant.components.unifiprotect.const import MIN_REQUIRED_PROTECT_V
+
+MAC_ADDR = "aa:bb:cc:dd:ee:ff"
+
+
+@dataclass
+class MockNvrData:
+    """Mock for NVR."""
+
+    version: Version
+    mac: str
+    name: str
+    id: str
+
+
+@dataclass
+class MockBootstrap:
+    """Mock for Bootstrap."""
+
+    nvr: MockNvrData
+    cameras: dict[str, Any]
+    lights: dict[str, Any]
+    sensors: dict[str, Any]
+    viewers: dict[str, Any]
+
+
+MOCK_NVR_DATA = MockNvrData(
+    version=MIN_REQUIRED_PROTECT_V, mac=MAC_ADDR, name="UnifiProtect", id="test_id"
+)
+MOCK_OLD_NVR_DATA = MockNvrData(
+    version=Version("1.19.0"), mac=MAC_ADDR, name="UnifiProtect", id="test_id"
+)
+
+MOCK_BOOTSTRAP = MockBootstrap(
+    nvr=MOCK_NVR_DATA, cameras={}, lights={}, sensors={}, viewers={}
+)
+
+
+@pytest.fixture
+def mock_client():
+    """Mock ProtectApiClient for testing."""
+    client = Mock()
+    client.bootstrap = MOCK_BOOTSTRAP
+
+    client.get_nvr = AsyncMock(return_value=MOCK_NVR_DATA)
+    client.update = AsyncMock(return_value=MOCK_BOOTSTRAP)
+    client.async_disconnect_ws = AsyncMock()
+
+    return client
---
+++ tests/components/unifiprotect/test_init.py
@@ -0,0 +1,207 @@
+"""Test the UniFi Protect setup flow."""
+from unittest.mock import AsyncMock, Mock, patch
+
+import pytest
+from pyunifiprotect import NotAuthorized, NvrError
+
+from homeassistant.components.unifiprotect import async_setup_entry, async_unload_entry
+from homeassistant.components.unifiprotect.const import CONF_DISABLE_RTSP, DOMAIN
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
+from homeassistant.helpers import device_registry as dr
+
+from .conftest import MAC_ADDR, MOCK_OLD_NVR_DATA
+
+from tests.common import MockConfigEntry
+
+
+@patch("homeassistant.components.unifiprotect.ProtectApiClient")
+async def test_setup(mock_api, hass: HomeAssistant, mock_client):
+    """Test working setup of unifiprotect entry."""
+    mock_config = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "host": "1.1.1.1",
+            "username": "test-username",
+            "password": "test-password",
+            "id": "UnifiProtect",
+            "port": 443,
+            "verify_ssl": False,
+        },
+    )
+
+    mock_api.return_value = mock_client
+
+    assert await async_setup_entry(hass, mock_config) is True
+    assert mock_client.update.called
+    assert mock_config.unique_id == mock_client.bootstrap.nvr.mac
+    assert mock_config.entry_id in hass.data[DOMAIN]
+
+
+@patch("homeassistant.components.unifiprotect.ProtectApiClient")
+async def test_reload(mock_api, hass: HomeAssistant, mock_client):
+    """Test updating entry reload entry."""
+    mock_config = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "host": "1.1.1.1",
+            "username": "test-username",
+            "password": "test-password",
+            "id": "UnifiProtect",
+            "port": 443,
+            "verify_ssl": False,
+        },
+    )
+
+    mock_api.return_value = mock_client
+    await async_setup_entry(hass, mock_config)
+
+    options = dict(mock_config.options)
+    options[CONF_DISABLE_RTSP] = True
+
+    hass.config_entries.async_update_entry(mock_config, options=options)
+
+
+@patch("homeassistant.components.unifiprotect.ProtectApiClient")
+async def test_unload(mock_api, hass: HomeAssistant, mock_client):
+    """Test unloading of unifiprotect entry."""
+    mock_config = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "host": "1.1.1.1",
+            "username": "test-username",
+            "password": "test-password",
+            "id": "UnifiProtect",
+            "port": 443,
+            "verify_ssl": False,
+        },
+    )
+
+    mock_api.return_value = mock_client
+    await async_setup_entry(hass, mock_config)
+
+    await async_unload_entry(hass, mock_config)
+    assert mock_config.entry_id not in hass.data[DOMAIN]
+
+
+@patch("homeassistant.components.unifiprotect.ProtectApiClient")
+async def test_setup_too_old(mock_api, hass: HomeAssistant, mock_client):
+    """Test setup of unifiprotect entry with too old of version of UniFi Protect."""
+    mock_config = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "host": "1.1.1.1",
+            "username": "test-username",
+            "password": "test-password",
+            "id": "UnifiProtect",
+            "port": 443,
+            "verify_ssl": False,
+        },
+        unique_id=dr.format_mac(MAC_ADDR),
+    )
+
+    mock_client.get_nvr.return_value = MOCK_OLD_NVR_DATA
+    mock_api.return_value = mock_client
+
+    assert await async_setup_entry(hass, mock_config) is False
+    assert not mock_client.update.called
+
+
+@patch("homeassistant.components.unifiprotect.ProtectApiClient")
+async def test_setup_failed_update(mock_api, hass: HomeAssistant, mock_client):
+    """Test setup of unifiprotect entry with failed update."""
+    mock_config = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "host": "1.1.1.1",
+            "username": "test-username",
+            "password": "test-password",
+            "id": "UnifiProtect",
+            "port": 443,
+            "verify_ssl": False,
+        },
+        unique_id=dr.format_mac(MAC_ADDR),
+    )
+    mock_config.async_start_reauth = Mock()
+
+    mock_client.update = AsyncMock(side_effect=NvrError)
+    mock_api.return_value = mock_client
+
+    with pytest.raises(ConfigEntryNotReady):
+        await async_setup_entry(hass, mock_config)
+    assert not mock_config.async_start_reauth.called
+    assert mock_client.update.called
+
+
+@patch("homeassistant.components.unifiprotect.ProtectApiClient")
+async def test_setup_failed_update_reauth(mock_api, hass: HomeAssistant, mock_client):
+    """Test setup of unifiprotect entry with update that gives unauthroized error."""
+    mock_config = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "host": "1.1.1.1",
+            "username": "test-username",
+            "password": "test-password",
+            "id": "UnifiProtect",
+            "port": 443,
+            "verify_ssl": False,
+        },
+        unique_id=dr.format_mac(MAC_ADDR),
+    )
+    mock_config.async_start_reauth = Mock()
+
+    mock_client.update = AsyncMock(side_effect=NotAuthorized)
+    mock_api.return_value = mock_client
+
+    with pytest.raises(ConfigEntryNotReady):
+        await async_setup_entry(hass, mock_config)
+    assert mock_config.async_start_reauth.called
+    assert mock_client.update.called
+
+
+@patch("homeassistant.components.unifiprotect.ProtectApiClient")
+async def test_setup_failed_error(mock_api, hass: HomeAssistant, mock_client):
+    """Test setup of unifiprotect entry with generic error."""
+    mock_config = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "host": "1.1.1.1",
+            "username": "test-username",
+            "password": "test-password",
+            "id": "UnifiProtect",
+            "port": 443,
+            "verify_ssl": False,
+        },
+        unique_id=dr.format_mac(MAC_ADDR),
+    )
+
+    mock_client.get_nvr = AsyncMock(side_effect=NvrError)
+    mock_api.return_value = mock_client
+
+    with pytest.raises(ConfigEntryNotReady):
+        await async_setup_entry(hass, mock_config)
+    assert not mock_client.update.called
+
+
+@patch("homeassistant.components.unifiprotect.ProtectApiClient")
+async def test_setup_failed_auth(mock_api, hass: HomeAssistant, mock_client):
+    """Test setup of unifiprotect entry with unauthorized error."""
+    mock_config = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "host": "1.1.1.1",
+            "username": "test-username",
+            "password": "test-password",
+            "id": "UnifiProtect",
+            "port": 443,
+            "verify_ssl": False,
+        },
+        unique_id=dr.format_mac(MAC_ADDR),
+    )
+
+    mock_client.get_nvr = AsyncMock(side_effect=NotAuthorized)
+    mock_api.return_value = mock_client
+
+    with pytest.raises(ConfigEntryAuthFailed):
+        await async_setup_entry(hass, mock_config)
+    assert not mock_client.update.called
