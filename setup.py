e78e04fe3877481a1fabdadd402a5dfca279d70d
tomasz75 <tomek@sviete.pl>
2021-12-25 18:58:04 +0100

    20211212.0 LB

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ REQUIRES = [
     "requests==2.26.0",
     "voluptuous==0.12.2",
     "voluptuous-serialize==2.4.0",
-    "ais-dom-frontend==20211209.0",
+    "ais-dom-frontend==20211212.0",
     "yarl==1.6.3",
     # AIS
     "aisapi==0.1.1",
