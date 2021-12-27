417172eef237bc06c336af2f9c50d5ed2c4e8ab2
J. Nick Koston <nick@koston.org>
2021-12-26 22:53:33 -1000

    Cleanup HomeKit names to avoid unknown error when adding (#62831)

--- tests/components/homekit/test_type_remote.py
+++ tests/components/homekit/test_type_remote.py
@@ -167,3 +167,41 @@ async def test_activity_remote(hass, hk_driver, events, caplog):
     )
     await hass.async_block_till_done()
     assert call_reset_accessory[0].data[ATTR_ENTITY_ID] == entity_id
+
+
+async def test_activity_remote_bad_names(hass, hk_driver, events, caplog):
+    """Test if remote accessory with invalid names works as expected."""
+    entity_id = "remote.harmony"
+    hass.states.async_set(
+        entity_id,
+        None,
+        {
+            ATTR_SUPPORTED_FEATURES: SUPPORT_ACTIVITY,
+            ATTR_CURRENT_ACTIVITY: "Apple TV",
+            ATTR_ACTIVITY_LIST: ["TV", "Apple TV", "[[[--Special--]]]", "Super"],
+        },
+    )
+    await hass.async_block_till_done()
+    acc = ActivityRemote(hass, hk_driver, "ActivityRemote", entity_id, 2, None)
+    await acc.run()
+    await hass.async_block_till_done()
+
+    assert acc.aid == 2
+    assert acc.category == 31  # Television
+
+    assert acc.char_active.value == 0
+    assert acc.char_remote_key.value == 0
+    assert acc.char_input_source.value == 1
+
+    hass.states.async_set(
+        entity_id,
+        STATE_ON,
+        {
+            ATTR_SUPPORTED_FEATURES: SUPPORT_ACTIVITY,
+            ATTR_CURRENT_ACTIVITY: "[[[--Special--]]]",
+            ATTR_ACTIVITY_LIST: ["TV", "Apple TV", "[[[--Special--]]]", "Super"],
+        },
+    )
+    await hass.async_block_till_done()
+    assert acc.char_active.value == 1
+    assert acc.char_input_source.value == 2
