39fc1abdcfc4ec853e2a53ae62cbe0ae8f21069e
epenet <6771947+epenet@users.noreply.github.com>
2021-12-28 00:14:20 +0100

    Ensure service calls are typed [a-d] (#62891)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/alert/__init__.py
+++ homeassistant/components/alert/__init__.py
@@ -23,6 +23,7 @@ from homeassistant.const import (
     STATE_OFF,
     STATE_ON,
 )
+from homeassistant.core import ServiceCall
 from homeassistant.helpers import event, service
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import ToggleEntity
@@ -117,7 +118,7 @@ async def async_setup(hass, config):
     if not entities:
         return False
 
-    async def async_handle_alert_service(service_call):
+    async def async_handle_alert_service(service_call: ServiceCall) -> None:
         """Handle calls to alert services."""
         alert_ids = await service.async_extract_entity_ids(hass, service_call)
 
