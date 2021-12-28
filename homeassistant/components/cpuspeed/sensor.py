60c32fddb8927d835dd6d56a3329bd9cf0d8be49
Franck Nijhof <git@frenck.dev>
2021-12-28 11:38:42 +0100

    Use shorthand attributes in the CPU Speed integration (#62896)

--- homeassistant/components/cpuspeed/sensor.py
+++ homeassistant/components/cpuspeed/sensor.py
@@ -15,8 +15,6 @@ HZ_ADVERTISED = "hz_advertised"
 
 DEFAULT_NAME = "CPU speed"
 
-ICON = "mdi:pulse"
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string}
 )
@@ -31,26 +29,13 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class CpuSpeedSensor(SensorEntity):
     """Representation of a CPU sensor."""
 
+    _attr_native_unit_of_measurement = FREQUENCY_GIGAHERTZ
+    _attr_icon = "mdi:pulse"
+
     def __init__(self, name):
         """Initialize the CPU sensor."""
-        self._name = name
-        self._state = None
+        self._attr_name = name
         self.info = None
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def native_value(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def native_unit_of_measurement(self):
-        """Return the unit the value is expressed in."""
-        return FREQUENCY_GIGAHERTZ
 
     @property
     def extra_state_attributes(self):
@@ -64,15 +49,10 @@ class CpuSpeedSensor(SensorEntity):
                 attrs[ATTR_HZ] = round(self.info[HZ_ADVERTISED][0] / 10 ** 9, 2)
             return attrs
 
-    @property
-    def icon(self):
-        """Return the icon to use in the frontend, if any."""
-        return ICON
-
     def update(self):
         """Get the latest data and updates the state."""
         self.info = cpuinfo.get_cpu_info()
         if HZ_ACTUAL in self.info:
-            self._state = round(float(self.info[HZ_ACTUAL][0]) / 10 ** 9, 2)
+            self._attr_native_value = round(float(self.info[HZ_ACTUAL][0]) / 10 ** 9, 2)
         else:
-            self._state = None
+            self._attr_native_value = None
