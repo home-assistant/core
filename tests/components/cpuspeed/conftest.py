2185137bac1ad20ddea90c593c26d2dd2aa69e30
Franck Nijhof <git@frenck.dev>
2021-12-29 18:54:47 +0100

    Detect unsupported system in CPU Speed integration (#63012)

--- tests/components/cpuspeed/conftest.py
+++ tests/components/cpuspeed/conftest.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 from collections.abc import Generator
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 
@@ -20,6 +20,20 @@ def mock_config_entry() -> MockConfigEntry:
         data={},
         unique_id=DOMAIN,
     )
+
+
+@pytest.fixture
+def mock_cpuinfo_config_flow() -> Generator[MagicMock, None, None]:
+    """Return a mocked get_cpu_info.
+
+    It is only used to check thruthy or falsy values, so it is mocked
+    to return True.
+    """
+    with patch(
+        "homeassistant.components.cpuspeed.config_flow.cpuinfo.get_cpu_info",
+        return_value=True,
+    ) as cpuinfo_mock:
+        yield cpuinfo_mock
 
 
 @pytest.fixture
