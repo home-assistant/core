94461aaa2cad6b3003f8b54b86f114983d1817a2
Franck Nijhof <git@frenck.dev>
2021-12-29 22:18:56 +0100

    Add full test coverage to CPU Speed

--- tests/components/cpuspeed/conftest.py
+++ tests/components/cpuspeed/conftest.py
@@ -7,6 +7,7 @@ from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 
 from homeassistant.components.cpuspeed.const import DOMAIN
+from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
 
@@ -43,3 +44,33 @@ def mock_setup_entry() -> Generator[AsyncMock, None, None]:
         "homeassistant.components.cpuspeed.async_setup_entry", return_value=True
     ) as mock_setup:
         yield mock_setup
+
+
+@pytest.fixture
+def mock_cpuinfo() -> Generator[MagicMock, None, None]:
+    """Return a mocked get_cpu_info."""
+    info = {
+        "hz_actual": (3200000001, 0),
+        "arch_string_raw": "aargh",
+        "brand_raw": "Intel Ryzen 7",
+        "hz_advertised": (3600000001, 0),
+    }
+
+    with patch(
+        "homeassistant.components.cpuspeed.cpuinfo.get_cpu_info",
+        return_value=info,
+    ) as cpuinfo_mock:
+        yield cpuinfo_mock
+
+
+@pytest.fixture
+async def init_integration(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry, mock_cpuinfo: MagicMock
+) -> MockConfigEntry:
+    """Set up the CPU Speed integration for testing."""
+    mock_config_entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    return mock_config_entry
