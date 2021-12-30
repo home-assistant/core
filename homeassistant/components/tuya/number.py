6633eba5539051cbf55e7fe39650e8f5a2bc9d8f
Franck Nijhof <git@frenck.dev>
2021-12-30 13:45:19 +0100

    Only rely on device status when adding Tuya entities

--- homeassistant/components/tuya/number.py
+++ homeassistant/components/tuya/number.py
@@ -247,10 +247,7 @@ async def async_setup_entry(
             device = hass_data.device_manager.device_map[device_id]
             if descriptions := NUMBERS.get(device.category):
                 for description in descriptions:
-                    if (
-                        description.key in device.function
-                        or description.key in device.status
-                    ):
+                    if description.key in device.status:
                         entities.append(
                             TuyaNumberEntity(
                                 device, hass_data.device_manager, description
