2110c66171a750ce8a5eae9cb450e625e625ba13
J. Nick Koston <nick@koston.org>
2021-12-29 16:03:57 -1000

    fix updating

--- homeassistant/components/flux_led/sensor.py
+++ homeassistant/components/flux_led/sensor.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 
 from datetime import date, datetime
 
-from flux_led.aio import AIOWifiLedBulb
-
 from homeassistant import config_entries
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import CONF_NAME
@@ -15,7 +13,7 @@ from homeassistant.helpers.typing import StateType
 
 from .const import DOMAIN
 from .coordinator import FluxLedUpdateCoordinator
-from .entity import FluxBaseEntity
+from .entity import FluxEntity
 
 
 async def async_setup_entry(
@@ -27,23 +25,24 @@ async def async_setup_entry(
     coordinator: FluxLedUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
     device = coordinator.device
     if device.paired_remotes is not None:
-        async_add_entities([FluxPairedRemotes(coordinator.device, entry)])
+        async_add_entities([FluxPairedRemotes(coordinator, entry)])
 
 
-class FluxPairedRemotes(FluxBaseEntity, SensorEntity):
+class FluxPairedRemotes(FluxEntity, SensorEntity):
     """Representation of a Flux power restore state option."""
 
     _attr_should_poll = False
 
     def __init__(
         self,
-        device: AIOWifiLedBulb,
+        coordinator: FluxLedUpdateCoordinator,
         entry: config_entries.ConfigEntry,
     ) -> None:
         """Initialize the power state select."""
-        super().__init__(device, entry)
+        super().__init__(
+            coordinator, entry.unique_id, f"{entry.data[CONF_NAME]} Paired Remotes"
+        )
         self._attr_entity_category = EntityCategory.CONFIG
-        self._attr_name = f"{entry.data[CONF_NAME]} Paired Remotes"
         self._attr_icon = "mdi:remote"
         if entry.unique_id:
             self._attr_unique_id = f"{entry.unique_id}_paired_remotes"
