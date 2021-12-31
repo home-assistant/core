5e2d6441103570a76228917d0681072059191d24
Ronald Pijnacker <ronald.pijnacker@gmail.com>
2021-12-31 10:12:28 +0000

    Revert "Process constant renames"
    This reverts commit df823cd07c27fdfadbe8f47fddcb3ceb3413e584.

--- tests/components/dsmr/test_sensor.py
+++ tests/components/dsmr/test_sensor.py
@@ -309,9 +309,9 @@ async def test_luxembourg_meter(hass, dsmr_connection_fixture):
     (connection_factory, transport, protocol) = dsmr_connection_fixture
 
     from dsmr_parser.obis_references import (
-        ELECTRICITY_EXPORTED_TOTAL,
-        ELECTRICITY_IMPORTED_TOTAL,
         HOURLY_GAS_METER_READING,
+        LUXEMBOURG_ELECTRICITY_DELIVERED_TARIFF_GLOBAL,
+        LUXEMBOURG_ELECTRICITY_USED_TARIFF_GLOBAL,
     )
     from dsmr_parser.objects import CosemObject, MBusObject
 
@@ -334,10 +334,10 @@ async def test_luxembourg_meter(hass, dsmr_connection_fixture):
                 {"value": Decimal(745.695), "unit": "m3"},
             ]
         ),
-        ELECTRICITY_IMPORTED_TOTAL: CosemObject(
+        LUXEMBOURG_ELECTRICITY_USED_TARIFF_GLOBAL: CosemObject(
             [{"value": Decimal(123.456), "unit": ENERGY_KILO_WATT_HOUR}]
         ),
-        ELECTRICITY_EXPORTED_TOTAL: CosemObject(
+        LUXEMBOURG_ELECTRICITY_DELIVERED_TARIFF_GLOBAL: CosemObject(
             [{"value": Decimal(654.321), "unit": ENERGY_KILO_WATT_HOUR}]
         ),
     }
@@ -510,8 +510,8 @@ async def test_swedish_meter(hass, dsmr_connection_fixture):
     (connection_factory, transport, protocol) = dsmr_connection_fixture
 
     from dsmr_parser.obis_references import (
-        ELECTRICITY_EXPORTED_TOTAL,
-        ELECTRICITY_IMPORTED_TOTAL,
+        SWEDEN_ELECTRICITY_DELIVERED_TARIFF_GLOBAL,
+        SWEDEN_ELECTRICITY_USED_TARIFF_GLOBAL,
     )
     from dsmr_parser.objects import CosemObject
 
@@ -528,10 +528,10 @@ async def test_swedish_meter(hass, dsmr_connection_fixture):
     }
 
     telegram = {
-        ELECTRICITY_IMPORTED_TOTAL: CosemObject(
+        SWEDEN_ELECTRICITY_USED_TARIFF_GLOBAL: CosemObject(
             [{"value": Decimal(123.456), "unit": ENERGY_KILO_WATT_HOUR}]
         ),
-        ELECTRICITY_EXPORTED_TOTAL: CosemObject(
+        SWEDEN_ELECTRICITY_DELIVERED_TARIFF_GLOBAL: CosemObject(
             [{"value": Decimal(654.321), "unit": ENERGY_KILO_WATT_HOUR}]
         ),
     }
