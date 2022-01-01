236fb55efd5a42389aa0616360b8df30b1dcbebc
J. Nick Koston <nick@koston.org>
2021-12-31 22:41:44 -1000

    merge

--- homeassistant/components/oncue/sensor.py
+++ homeassistant/components/oncue/sensor.py
@@ -6,9 +6,11 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 
-from . import OnCueDataUpdateCoordinator
 from .const import DOMAIN
 
 STATIC_SENSORS = {"GensetSerialNumber", "GensetModelNumberSelect"}
@@ -20,7 +22,7 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up sensors."""
-    coordinator: OnCueDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
+    coordinator: DataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
     entities: list[OncueSensor] = []
     for device_id, device in coordinator.data.items():
         entities.extend(
@@ -39,10 +41,8 @@ async def async_setup_entry(
 class OncueSensor(CoordinatorEntity, SensorEntity):
     """Representation of an Oncue sensor."""
 
-    coordinator: OnCueDataUpdateCoordinator
-
     def __init__(
-        self, coordinator: OnCueDataUpdateCoordinator, device_id: str, name: str
+        self, coordinator: DataUpdateCoordinator, device_id: str, name: str
     ) -> None:
         """Initialize the switch."""
         super().__init__(coordinator)
