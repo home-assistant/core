f1b592057a50f7b2ffe986b60f7c51a7b8b08edc
epenet <6771947+epenet@users.noreply.github.com>
2022-01-01 22:38:11 +0100

    Add init type hints [f-g] (#63186)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/forked_daapd/__init__.py
+++ homeassistant/components/forked_daapd/__init__.py
@@ -1,18 +1,20 @@
 """The forked_daapd component."""
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, HASS_DATA_REMOVE_LISTENERS_KEY, HASS_DATA_UPDATER_KEY
 
 PLATFORMS = [Platform.MEDIA_PLAYER]
 
 
-async def async_setup_entry(hass, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up forked-daapd from a config entry by forwarding to platform."""
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
 
 
-async def async_unload_entry(hass, entry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Remove forked-daapd component."""
     status = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if status and hass.data.get(DOMAIN) and hass.data[DOMAIN].get(entry.entry_id):
