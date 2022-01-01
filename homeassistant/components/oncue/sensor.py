9f437b5c60b0321a4ddaebb8f79a3a6f0ee4bedf
J. Nick Koston <nick@koston.org>
2022-01-01 10:59:51 -1000

    adjust

--- homeassistant/components/oncue/sensor.py
+++ homeassistant/components/oncue/sensor.py
@@ -3,11 +3,16 @@ from __future__ import annotations
 
 from aiooncue import OncueDevice, OncueSensor
 
-from homeassistant.components.sensor import SensorDeviceClass, SensorEntity
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     ELECTRIC_POTENTIAL_VOLT,
     FREQUENCY_HERTZ,
+    PERCENTAGE,
     POWER_WATT,
     PRESSURE_PSI,
     TEMP_CELSIUS,
@@ -23,31 +28,81 @@ from homeassistant.helpers.update_coordinator import (
 
 from .const import DOMAIN
 
-STATIC_SENSORS = {
-    "GensetSerialNumber",
-    "GensetModelNumberSelect",
-    "FirmwareVersion",
-    "Product",
+SENSORS: dict[str, SensorEntityDescription] = {
+    "LatestFirmware": SensorEntityDescription(
+        key="LatestFirmware",
+    ),
+    "EngineSpeed": SensorEntityDescription(
+        key="EngineSpeed",
+    ),
+    "EngineOilPressure": SensorEntityDescription(
+        key="EngineOilPressure",
+        native_unit_of_measurement=PRESSURE_PSI,
+        device_class=SensorDeviceClass.PRESSURE,
+    ),
+    "EngineCoolantTemperature": SensorEntityDescription(
+        key="EngineCoolantTemperature", device_class=SensorDeviceClass.TEMPERATURE
+    ),
+    "BatteryVoltage": SensorEntityDescription(
+        key="BatteryVoltage",
+        native_unit_of_measurement=ELECTRIC_POTENTIAL_VOLT,
+        device_class=SensorDeviceClass.VOLTAGE,
+    ),
+    "LubeOilTemperature": SensorEntityDescription(
+        key="LubeOilTemperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+    ),
+    "GensetControllerTemperature": SensorEntityDescription(
+        key="GensetControllerTemperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+    ),
+    "EngineCompartmentTemperature": SensorEntityDescription(
+        key="EngineCompartmentTemperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+    ),
+    "GeneratorTrueTotalPower": SensorEntityDescription(
+        key="GeneratorTrueTotalPower",
+        native_unit_of_measurement=POWER_WATT,
+        device_class=SensorDeviceClass.POWER,
+    ),
+    "GeneratorTruePercentOfRatedPower": SensorEntityDescription(
+        key="GeneratorTruePercentOfRatedPower",
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+    "GeneratorVoltageAverageLineToLine": SensorEntityDescription(
+        key="GeneratorVoltageAverageLineToLine",
+        native_unit_of_measurement=ELECTRIC_POTENTIAL_VOLT,
+        device_class=SensorDeviceClass.VOLTAGE,
+    ),
+    "GeneratorFrequency": SensorEntityDescription(
+        key="GeneratorFrequency",
+        native_unit_of_measurement=FREQUENCY_HERTZ,
+        device_class=SensorDeviceClass.FREQUENCY,
+    ),
+    "GensetState": SensorEntityDescription(
+        key="GensetState",
+    ),
+    "GensetControllerTotalOperationTime": SensorEntityDescription(
+        key="GensetControllerTotalOperationTime",
+    ),
+    "EngineTotalRunTime": SensorEntityDescription(
+        key="EngineTotalRunTime",
+    ),
+    "AtsContactorPosition": SensorEntityDescription(
+        key="AtsContactorPosition",
+    ),
+    "IPAddress": SensorEntityDescription(
+        key="IPAddress",
+    ),
+    "ConnectedServerIPAddress": SensorEntityDescription(
+        key="ConnectedServerIPAddress",
+    ),
 }
 
-DEVICE_CLASSES = {
-    "EngineOilPressure": SensorDeviceClass.PRESSURE,
-    "EngineCoolantTemperature": SensorDeviceClass.TEMPERATURE,
-    "BatteryVoltage": SensorDeviceClass.VOLTAGE,
-    "LubeOilTemperature": SensorDeviceClass.TEMPERATURE,
-    "GensetControllerTemperature": SensorDeviceClass.TEMPERATURE,
-    "EngineCompartmentTemperature": SensorDeviceClass.TEMPERATURE,
-    "GeneratorTrueTotalPower": SensorDeviceClass.POWER,
-    "GeneratorVoltageAverageLineToLine": SensorDeviceClass.VOLTAGE,
-    "GeneratorFrequency": SensorDeviceClass.FREQUENCY,
-}
+
 UNIT_MAPPINGS = {
     "C": TEMP_CELSIUS,
     "F": TEMP_FAHRENHEIT,
-    "psi": PRESSURE_PSI,
-    "H": FREQUENCY_HERTZ,
-    "V": ELECTRIC_POTENTIAL_VOLT,
-    "W": POWER_WATT,
 }
 
 
@@ -62,14 +117,9 @@ async def async_setup_entry(
     devices: dict[str, OncueDevice] = coordinator.data
     for device_id, device in devices.items():
         entities.extend(
-            OncueSensorEntity(
-                coordinator,
-                device_id,
-                device,
-                sensor,
-            )
+            OncueSensorEntity(coordinator, device_id, device, sensor, SENSORS[name])
             for name, sensor in device.sensors.items()
-            if name not in STATIC_SENSORS
+            if name not in SENSORS
         )
 
     async_add_entities(entities)
@@ -84,19 +134,18 @@ class OncueSensorEntity(CoordinatorEntity, SensorEntity):
         device_id: str,
         device: OncueDevice,
         sensor: OncueSensor,
+        description: SensorEntityDescription,
     ) -> None:
         """Initialize the sensor."""
         super().__init__(coordinator)
+        self.entity_description = description
         sensors = device.sensors
         self._device_id = device_id
         self._sensor_name = sensor.name
         self._attr_unique_id = f"{device_id}_{sensor.name}"
         self._attr_name = f"{device.name} {sensor.display_name}"
-        self._attr_device_class = DEVICE_CLASSES.get(sensor.name)
-        if sensor.unit is not None:
-            self._attr_native_unit_of_measurement = UNIT_MAPPINGS.get(
-                sensor.unit, sensor.unit
-            )
+        if description.native_unit_of_measurement is None and sensor.unit is not None:
+            self._attr_native_unit_of_measurement = UNIT_MAPPINGS.get(sensor.unit)
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, device_id)},
             name=device.name,
