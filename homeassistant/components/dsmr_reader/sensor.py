025544d0099435f1000707569855c9058e7c7571
Nick Elbers <nickelbers@gmail.com>
2022-01-02 22:14:44 +0000

    Generate unique IDs for DSMR-reader

--- homeassistant/components/dsmr_reader/sensor.py
+++ homeassistant/components/dsmr_reader/sensor.py
@@ -1,17 +1,30 @@
 """Support for DSMR Reader through MQTT."""
+import logging
+
 from homeassistant.components import mqtt
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
 from homeassistant.core import callback
 from homeassistant.util import slugify
 
 from .definitions import SENSORS, DSMRReaderSensorEntityDescription
+
+_LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "dsmr_reader"
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up DSMR Reader sensors."""
-    async_add_entities(DSMRSensor(description) for description in SENSORS)
+    latitude = config.get(CONF_LATITUDE, hass.config.latitude)
+    longitude = config.get(CONF_LONGITUDE, hass.config.longitude)
+
+    if None in (latitude, longitude):
+        _LOGGER.error("Latitude or longitude not set in Home Assistant config")
+        return
+
+    coordinates = {CONF_LATITUDE: float(latitude), CONF_LONGITUDE: float(longitude)}
+    async_add_entities(DSMRSensor(coordinates, description) for description in SENSORS)
 
 
 class DSMRSensor(SensorEntity):
@@ -19,12 +32,17 @@ class DSMRSensor(SensorEntity):
 
     entity_description: DSMRReaderSensorEntityDescription
 
-    def __init__(self, description: DSMRReaderSensorEntityDescription) -> None:
+    def __init__(
+        self, coordinates, description: DSMRReaderSensorEntityDescription
+    ) -> None:
         """Initialize the sensor."""
         self.entity_description = description
 
         slug = slugify(description.key.replace("/", "_"))
         self.entity_id = f"sensor.{slug}"
+        self._attr_unique_id = "{:2.6f}{:2.6f}{}".format(
+            coordinates[CONF_LATITUDE], coordinates[CONF_LONGITUDE], description.key
+        )
 
     async def async_added_to_hass(self):
         """Subscribe to MQTT events."""
