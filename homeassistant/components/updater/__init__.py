18056f3a8a2c6238218b9b9acd9ac12bb3bc9ad1
epenet <6771947+epenet@users.noreply.github.com>
2022-01-02 07:06:11 +0100

    Add init type hints [t-u] (#63194)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/updater/__init__.py
+++ homeassistant/components/updater/__init__.py
@@ -8,9 +8,11 @@ from awesomeversion import AwesomeVersion
 import voluptuous as vol
 
 from homeassistant.const import __version__ as current_version
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import discovery, update_coordinator
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.typing import ConfigType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -53,7 +55,7 @@ class Updater:
         self.newest_version = newest_version
 
 
-async def async_setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the updater component."""
     conf = config.get(DOMAIN, {})
 
