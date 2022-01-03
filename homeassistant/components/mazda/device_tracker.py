19b17fcf3bcd27b690b27c71f29f79388521a76d
epenet <6771947+epenet@users.noreply.github.com>
2022-01-03 15:19:38 +0100

    Add device_tracker setup type hints (#63286)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/mazda/device_tracker.py
+++ homeassistant/components/mazda/device_tracker.py
@@ -1,12 +1,19 @@
 """Platform for Mazda device tracker integration."""
 from homeassistant.components.device_tracker import SOURCE_TYPE_GPS
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import MazdaEntity
 from .const import DATA_CLIENT, DATA_COORDINATOR, DOMAIN
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
     """Set up the device tracker platform."""
     client = hass.data[DOMAIN][config_entry.entry_id][DATA_CLIENT]
     coordinator = hass.data[DOMAIN][config_entry.entry_id][DATA_COORDINATOR]
