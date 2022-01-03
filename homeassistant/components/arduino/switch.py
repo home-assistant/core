d99f562408448e69608a0aa6addf019a5c77e01b
epenet <epenet@users.noreply.github.com>
2022-01-03 14:46:47 +0000

    Add switch setup type hints [a-f]

--- homeassistant/components/arduino/switch.py
+++ homeassistant/components/arduino/switch.py
@@ -1,9 +1,14 @@
 """Support for switching Arduino pins on and off."""
+from __future__ import annotations
+
 import voluptuous as vol
 
 from homeassistant.components.switch import PLATFORM_SCHEMA, SwitchEntity
 from homeassistant.const import CONF_NAME
+from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from . import DOMAIN
 
@@ -25,7 +30,12 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
     """Set up the Arduino platform."""
     board = hass.data[DOMAIN]
 
