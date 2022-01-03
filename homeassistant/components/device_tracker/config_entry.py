55a91c95a9741e7a7b2fbaa510e116b33a8e6d6b
Paulus Schoutsen <balloob@gmail.com>
2022-01-03 10:41:54 -0800

    Fix order of things

--- homeassistant/components/device_tracker/config_entry.py
+++ homeassistant/components/device_tracker/config_entry.py
@@ -192,14 +192,13 @@ class ScannerEntity(BaseTrackerEntity):
 
     async def async_internal_added_to_hass(self) -> None:
         """Handle added to Home Assistant."""
-        await super().async_internal_added_to_hass()
-
         # Entities without a unique ID don't have a device
         if (
             not self.registry_entry
             or not self.platform
             or not self.platform.config_entry
         ):
+            await super().async_internal_added_to_hass()
             return
 
         device_entry = self.find_device_entry()
@@ -214,21 +213,22 @@ class ScannerEntity(BaseTrackerEntity):
             dr.async_get(self.hass).async_remove_device(device_entry.id)
             device_entry = None
 
-        if device_entry is None:
-            return
+        if device_entry is not None:
+            # Attach entry to device
+            if self.registry_entry.device_id != device_entry.id:
+                self.registry_entry = er.async_get(self.hass).async_update_entity(
+                    self.entity_id, device_id=device_entry.id
+                )
 
-        # Attach entry to device
-        if self.registry_entry.device_id != device_entry.id:
-            er.async_get(self.hass).async_update_entity(
-                self.entity_id, device_id=device_entry.id
-            )
+            # Attach device to config entry
+            if self.platform.config_entry.entry_id not in device_entry.config_entries:
+                dr.async_get(self.hass).async_update_device(
+                    device_entry.id,
+                    add_config_entry_id=self.platform.config_entry.entry_id,
+                )
 
-        # Attach device to config entry
-        if self.platform.config_entry.entry_id not in device_entry.config_entries:
-            dr.async_get(self.hass).async_update_device(
-                device_entry.id,
-                add_config_entry_id=self.platform.config_entry.entry_id,
-            )
+        # Do this last or else the entity registry update listener has been installed
+        await super().async_internal_added_to_hass()
 
     @final
     @property
