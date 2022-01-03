2b292b08807b9b8606ee2d056aab5bccbb47958b
Erik Montnemery <erik@montnemery.com>
2022-01-03 10:41:30 +0100

    Add turned on or off device trigger to toggle entity (#61089)
    * Add turned on or off device trigger to toggle entity
    
    * Renamed changed_states trigger to toggled
    
    * Adjust tests
    
    * Fix homekit triggers test
    
    * Add tests
    
    * Adjust tests after rebase
    
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- tests/components/device_automation/test_toggle_entity.py
+++ tests/components/device_automation/test_toggle_entity.py
@@ -1,38 +1,15 @@
-"""The test for light device automation."""
+"""The test for device automation toggle entity helpers."""
 from datetime import timedelta
 
 import pytest
 
 import homeassistant.components.automation as automation
-from homeassistant.components.device_automation import DeviceAutomationType
-from homeassistant.components.light import DOMAIN
 from homeassistant.const import CONF_PLATFORM, STATE_OFF, STATE_ON
-from homeassistant.helpers import device_registry
 from homeassistant.setup import async_setup_component
 import homeassistant.util.dt as dt_util
 
-from tests.common import (
-    MockConfigEntry,
-    async_fire_time_changed,
-    async_get_device_automation_capabilities,
-    async_get_device_automations,
-    async_mock_service,
-    mock_device_registry,
-    mock_registry,
-)
+from tests.common import async_fire_time_changed, async_mock_service
 from tests.components.blueprint.conftest import stub_blueprint_populate  # noqa: F401
-
-
-@pytest.fixture
-def device_reg(hass):
-    """Return an empty, loaded, registry."""
-    return mock_device_registry(hass)
-
-
-@pytest.fixture
-def entity_reg(hass):
-    """Return an empty, loaded, registry."""
-    return mock_registry(hass)
 
 
 @pytest.fixture
@@ -41,76 +18,18 @@ def calls(hass):
     return async_mock_service(hass, "test", "automation")
 
 
-async def test_get_triggers(hass, device_reg, entity_reg):
-    """Test we get the expected triggers from a light."""
-    config_entry = MockConfigEntry(domain="test", data={})
-    config_entry.add_to_hass(hass)
-    device_entry = device_reg.async_get_or_create(
-        config_entry_id=config_entry.entry_id,
-        connections={(device_registry.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
-    )
-    entity_reg.async_get_or_create(DOMAIN, "test", "5678", device_id=device_entry.id)
-    expected_triggers = [
-        {
-            "platform": "device",
-            "domain": DOMAIN,
-            "type": "toggled",
-            "device_id": device_entry.id,
-            "entity_id": f"{DOMAIN}.test_5678",
-        },
-        {
-            "platform": "device",
-            "domain": DOMAIN,
-            "type": "turned_off",
-            "device_id": device_entry.id,
-            "entity_id": f"{DOMAIN}.test_5678",
-        },
-        {
-            "platform": "device",
-            "domain": DOMAIN,
-            "type": "turned_on",
-            "device_id": device_entry.id,
-            "entity_id": f"{DOMAIN}.test_5678",
-        },
-    ]
-    triggers = await async_get_device_automations(
-        hass, DeviceAutomationType.TRIGGER, device_entry.id
-    )
-    assert triggers == expected_triggers
-
-
-async def test_get_trigger_capabilities(hass, device_reg, entity_reg):
-    """Test we get the expected capabilities from a light trigger."""
-    config_entry = MockConfigEntry(domain="test", data={})
-    config_entry.add_to_hass(hass)
-    device_entry = device_reg.async_get_or_create(
-        config_entry_id=config_entry.entry_id,
-        connections={(device_registry.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
-    )
-    entity_reg.async_get_or_create(DOMAIN, "test", "5678", device_id=device_entry.id)
-    expected_capabilities = {
-        "extra_fields": [
-            {"name": "for", "optional": True, "type": "positive_time_period_dict"}
-        ]
-    }
-    triggers = await async_get_device_automations(
-        hass, DeviceAutomationType.TRIGGER, device_entry.id
-    )
-    for trigger in triggers:
-        capabilities = await async_get_device_automation_capabilities(
-            hass, DeviceAutomationType.TRIGGER, trigger
-        )
-        assert capabilities == expected_capabilities
-
-
 async def test_if_fires_on_state_change(hass, calls, enable_custom_integrations):
-    """Test for turn_on and turn_off triggers firing."""
-    platform = getattr(hass.components, f"test.{DOMAIN}")
+    """Test for turn_on and turn_off triggers firing.
+
+    This is a sanity test for the toggle entity device automation helper, this is
+    tested by each integration too.
+    """
+    platform = getattr(hass.components, "test.switch")
 
     platform.init()
-    await hass.async_block_till_done()
-
-    assert await async_setup_component(hass, DOMAIN, {DOMAIN: {CONF_PLATFORM: "test"}})
+    assert await async_setup_component(
+        hass, "switch", {"switch": {CONF_PLATFORM: "test"}}
+    )
     await hass.async_block_till_done()
 
     ent1, ent2, ent3 = platform.ENTITIES
@@ -123,7 +42,7 @@ async def test_if_fires_on_state_change(hass, calls, enable_custom_integrations)
                 {
                     "trigger": {
                         "platform": "device",
-                        "domain": DOMAIN,
+                        "domain": "switch",
                         "device_id": "",
                         "entity_id": ent1.entity_id,
                         "type": "turned_on",
@@ -147,7 +66,7 @@ async def test_if_fires_on_state_change(hass, calls, enable_custom_integrations)
                 {
                     "trigger": {
                         "platform": "device",
-                        "domain": DOMAIN,
+                        "domain": "switch",
                         "device_id": "",
                         "entity_id": ent1.entity_id,
                         "type": "turned_off",
@@ -171,7 +90,7 @@ async def test_if_fires_on_state_change(hass, calls, enable_custom_integrations)
                 {
                     "trigger": {
                         "platform": "device",
-                        "domain": DOMAIN,
+                        "domain": "switch",
                         "device_id": "",
                         "entity_id": ent1.entity_id,
                         "type": "toggled",
@@ -216,16 +135,17 @@ async def test_if_fires_on_state_change(hass, calls, enable_custom_integrations)
     }
 
 
+@pytest.mark.parametrize("trigger", ["turned_off", "toggled"])
 async def test_if_fires_on_state_change_with_for(
-    hass, calls, enable_custom_integrations
+    hass, calls, enable_custom_integrations, trigger
 ):
     """Test for triggers firing with delay."""
-    platform = getattr(hass.components, f"test.{DOMAIN}")
+    platform = getattr(hass.components, "test.switch")
 
     platform.init()
-    await hass.async_block_till_done()
-
-    assert await async_setup_component(hass, DOMAIN, {DOMAIN: {CONF_PLATFORM: "test"}})
+    assert await async_setup_component(
+        hass, "switch", {"switch": {CONF_PLATFORM: "test"}}
+    )
     await hass.async_block_till_done()
 
     ent1, ent2, ent3 = platform.ENTITIES
@@ -238,10 +158,10 @@ async def test_if_fires_on_state_change_with_for(
                 {
                     "trigger": {
                         "platform": "device",
-                        "domain": DOMAIN,
+                        "domain": "switch",
                         "device_id": "",
                         "entity_id": ent1.entity_id,
-                        "type": "turned_off",
+                        "type": trigger,
                         "for": {"seconds": 5},
                     },
                     "action": {
