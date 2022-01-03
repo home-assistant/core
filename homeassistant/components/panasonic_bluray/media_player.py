1519aa202d1c5ca3aec3e3cae9d32717cd5df01e
epenet <6771947+epenet@users.noreply.github.com>
2022-01-03 16:09:34 +0100

    Add media_player setup type hints [m-x] (#63298)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/panasonic_bluray/media_player.py
+++ homeassistant/components/panasonic_bluray/media_player.py
@@ -1,4 +1,6 @@
 """Support for Panasonic Blu-ray players."""
+from __future__ import annotations
+
 from datetime import timedelta
 
 from panacotta import PanasonicBD
@@ -19,7 +21,10 @@ from homeassistant.const import (
     STATE_OFF,
     STATE_PLAYING,
 )
+from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.util.dt import utcnow
 
 DEFAULT_NAME = "Panasonic Blu-Ray"
@@ -38,7 +43,12 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
     """Set up the Panasonic Blu-ray platform."""
     conf = discovery_info if discovery_info else config
 
