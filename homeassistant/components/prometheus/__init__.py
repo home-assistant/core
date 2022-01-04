6af99882c5de5ee38051d94fc21b781ba0b72ae2
Jevgeni Kiski <yozik04@gmail.com>
2022-01-04 19:52:43 +0200

    Prometheus to support input_number (#58964)
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/prometheus/__init__.py
+++ homeassistant/components/prometheus/__init__.py
@@ -1,4 +1,5 @@
 """Support for Prometheus metrics export."""
+from contextlib import suppress
 import logging
 import string
 
@@ -284,13 +285,24 @@ class PrometheusMetrics:
         metric.labels(**self._labels(state)).set(value)
 
     def _handle_input_number(self, state):
-        metric = self._metric(
-            "input_number_state",
-            self.prometheus_cli.Gauge,
-            "State of the input number",
-        )
-        value = self.state_as_number(state)
-        metric.labels(**self._labels(state)).set(value)
+        if unit := self._unit_string(state.attributes.get(ATTR_UNIT_OF_MEASUREMENT)):
+            metric = self._metric(
+                f"input_number_state_{unit}",
+                self.prometheus_cli.Gauge,
+                f"State of the input number measured in {unit}",
+            )
+        else:
+            metric = self._metric(
+                "input_number_state",
+                self.prometheus_cli.Gauge,
+                "State of the input number",
+            )
+
+        with suppress(ValueError):
+            value = self.state_as_number(state)
+            if state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == TEMP_FAHRENHEIT:
+                value = fahrenheit_to_celsius(value)
+            metric.labels(**self._labels(state)).set(value)
 
     def _handle_device_tracker(self, state):
         metric = self._metric(
