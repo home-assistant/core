9a1a75ad40dccdae4aaf1ad8642134d9d24810ad
Paulus Schoutsen <balloob@gmail.com>
2022-01-04 11:03:53 -0800

    Make device entry check more lean

--- homeassistant/components/device_tracker/config_entry.py
+++ homeassistant/components/device_tracker/config_entry.py
@@ -314,6 +314,7 @@ class ScannerEntity(BaseTrackerEntity):
             or not self.platform
             or not self.platform.config_entry
             or not self.mac_address
+            or (device_entry := self.find_device_entry()) is None
             # Entities should not have a device info. We opt them out
             # of this logic if they do.
             or self.device_info
@@ -323,22 +324,18 @@ class ScannerEntity(BaseTrackerEntity):
             await super().async_internal_added_to_hass()
             return
 
-        device_entry = self.find_device_entry()
-        assert device_entry is not None
+        # Attach entry to device
+        if self.registry_entry.device_id != device_entry.id:
+            self.registry_entry = er.async_get(self.hass).async_update_entity(
+                self.entity_id, device_id=device_entry.id
+            )
 
-        if device_entry is not None:
-            # Attach entry to device
-            if self.registry_entry.device_id != device_entry.id:
-                self.registry_entry = er.async_get(self.hass).async_update_entity(
-                    self.entity_id, device_id=device_entry.id
-                )
-
-            # Attach device to config entry
-            if self.platform.config_entry.entry_id not in device_entry.config_entries:
-                dr.async_get(self.hass).async_update_device(
-                    device_entry.id,
-                    add_config_entry_id=self.platform.config_entry.entry_id,
-                )
+        # Attach device to config entry
+        if self.platform.config_entry.entry_id not in device_entry.config_entries:
+            dr.async_get(self.hass).async_update_device(
+                device_entry.id,
+                add_config_entry_id=self.platform.config_entry.entry_id,
+            )
 
         # Do this last or else the entity registry update listener has been installed
         await super().async_internal_added_to_hass()
