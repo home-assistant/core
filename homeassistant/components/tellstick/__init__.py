2025c8f0e026a26827220da9de81c17ee8de2c3b
Michaël Arnauts <michael.arnauts@gmail.com>
2022-01-04 18:41:07 +0100

    Add unique_id to tellstick devices

--- homeassistant/components/tellstick/__init__.py
+++ homeassistant/components/tellstick/__init__.py
@@ -179,7 +179,10 @@ class TellstickDevice(Entity):
 
         # Look up our corresponding tellcore device
         self._tellcore_device = tellcore_device
-        self._name = tellcore_device.name
+        self._attr_assumed_state = True
+        self._attr_name = tellcore_device.name
+        self._attr_should_poll = False
+        self._attr_unique_id = tellcore_device.id
 
     async def async_added_to_hass(self):
         """Register callbacks."""
@@ -188,21 +191,6 @@ class TellstickDevice(Entity):
                 SIGNAL_TELLCORE_CALLBACK, self.update_from_callback
             )
         )
-
-    @property
-    def should_poll(self):
-        """Tell Home Assistant not to poll this device."""
-        return False
-
-    @property
-    def assumed_state(self):
-        """Tellstick devices are always assumed state."""
-        return True
-
-    @property
-    def name(self):
-        """Return the name of the device as reported by tellcore."""
-        return self._name
 
     @property
     def is_on(self):
