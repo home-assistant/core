953a4f07fa9f8829e993769d9231c9c62c986169
epenet <6771947+epenet@users.noreply.github.com>
2022-01-05 15:04:17 +0100

    Add setup type hints [b] (#63427)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/blinkt/light.py
+++ homeassistant/components/blinkt/light.py
@@ -1,4 +1,6 @@
 """Support for Blinkt! lights on Raspberry Pi."""
+from __future__ import annotations
+
 import importlib
 
 import voluptuous as vol
@@ -12,7 +14,10 @@ from homeassistant.components.light import (
     LightEntity,
 )
 from homeassistant.const import CONF_NAME
+from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 import homeassistant.util.color as color_util
 
 SUPPORT_BLINKT = SUPPORT_BRIGHTNESS | SUPPORT_COLOR
@@ -24,7 +29,12 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
     """Set up the Blinkt Light platform."""
     blinkt = importlib.import_module("blinkt")
 
