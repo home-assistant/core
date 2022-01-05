5e70812c732be3f699d9009462070f11bc0e2e22
Chris Talkington <chris@talkingtontech.com>
2022-01-04 23:57:37 -0600

    Update entity.py

--- homeassistant/components/roku/entity.py
+++ homeassistant/components/roku/entity.py
@@ -1,6 +1,7 @@
 """Base Entity for Roku."""
 from __future__ import annotations
 
+from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
 from homeassistant.helpers.entity import DeviceInfo, EntityDescription
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
