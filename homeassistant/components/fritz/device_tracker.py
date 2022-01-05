46a7b567fd6de56ed5e9d741ac3bad1c418861ba
chemelli74 <simone.chemelli@gmail.com>
2022-01-05 22:19:39 +0000

    Fritz cleanup - part 1

--- homeassistant/components/fritz/device_tracker.py
+++ homeassistant/components/fritz/device_tracker.py
@@ -1,4 +1,4 @@
-"""Support for FRITZ!Box routers."""
+"""Support for FRITZ!Box devices."""
 from __future__ import annotations
 
 import datetime
@@ -74,56 +74,56 @@ async def async_setup_entry(
 ) -> None:
     """Set up device tracker for FRITZ!Box component."""
     _LOGGER.debug("Starting FRITZ!Box device tracker")
-    router: FritzBoxTools = hass.data[DOMAIN][entry.entry_id]
+    avm_device: FritzBoxTools = hass.data[DOMAIN][entry.entry_id]
     data_fritz: FritzData = hass.data[DATA_FRITZ]
 
     @callback
-    def update_router() -> None:
-        """Update the values of the router."""
-        _async_add_entities(router, async_add_entities, data_fritz)
+    def update_avm_device() -> None:
+        """Update the values of AVM device."""
+        _async_add_entities(avm_device, async_add_entities, data_fritz)
 
     entry.async_on_unload(
-        async_dispatcher_connect(hass, router.signal_device_new, update_router)
+        async_dispatcher_connect(hass, avm_device.signal_device_new, update_avm_device)
     )
 
-    update_router()
+    update_avm_device()
 
 
 @callback
 def _async_add_entities(
-    router: FritzBoxTools,
+    avm_device: FritzBoxTools,
     async_add_entities: AddEntitiesCallback,
     data_fritz: FritzData,
 ) -> None:
-    """Add new tracker entities from the router."""
+    """Add new tracker entities from the AVM device."""
 
     new_tracked = []
-    if router.unique_id not in data_fritz.tracked:
-        data_fritz.tracked[router.unique_id] = set()
+    if avm_device.unique_id not in data_fritz.tracked:
+        data_fritz.tracked[avm_device.unique_id] = set()
 
-    for mac, device in router.devices.items():
+    for mac, device in avm_device.devices.items():
         if device_filter_out_from_trackers(mac, device, data_fritz.tracked.values()):
             continue
 
-        new_tracked.append(FritzBoxTracker(router, device))
-        data_fritz.tracked[router.unique_id].add(mac)
+        new_tracked.append(FritzBoxTracker(avm_device, device))
+        data_fritz.tracked[avm_device.unique_id].add(mac)
 
     if new_tracked:
         async_add_entities(new_tracked)
 
 
 class FritzBoxTracker(FritzDeviceBase, ScannerEntity):
-    """This class queries a FRITZ!Box router."""
+    """This class queries a FRITZ!Box device."""
 
-    def __init__(self, router: FritzBoxTools, device: FritzDevice) -> None:
+    def __init__(self, avm_device: FritzBoxTools, device: FritzDevice) -> None:
         """Initialize a FRITZ!Box device."""
-        super().__init__(router, device)
+        super().__init__(avm_device, device)
         self._last_activity: datetime.datetime | None = device.last_activity
 
     @property
     def is_connected(self) -> bool:
         """Return device status."""
-        return self._router.devices[self._mac].is_connected
+        return self._avm_device.devices[self._mac].is_connected
 
     @property
     def unique_id(self) -> str:
@@ -146,7 +146,7 @@ class FritzBoxTracker(FritzDeviceBase, ScannerEntity):
     def extra_state_attributes(self) -> dict[str, str]:
         """Return the attributes."""
         attrs: dict[str, str] = {}
-        device = self._router.devices[self._mac]
+        device = self._avm_device.devices[self._mac]
         self._last_activity = device.last_activity
         if self._last_activity is not None:
             attrs["last_time_reachable"] = self._last_activity.isoformat(
