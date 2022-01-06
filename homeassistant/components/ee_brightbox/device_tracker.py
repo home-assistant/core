b7c9e55707b7fd1a4e9a05d8387dc05cb336ff4e
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-01-06 14:41:04 +0100

    Revert ee_brightbox changes - Integrations is disabled

--- homeassistant/components/ee_brightbox/device_tracker.py
+++ homeassistant/components/ee_brightbox/device_tracker.py
@@ -6,10 +6,11 @@ from eebrightbox import EEBrightBox, EEBrightBoxException
 import voluptuous as vol
 
 from homeassistant.components.device_tracker import (
+    DOMAIN,
     PLATFORM_SCHEMA as PARENT_PLATFORM_SCHEMA,
     DeviceScanner,
 )
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME, Platform
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
@@ -32,7 +33,7 @@ PLATFORM_SCHEMA = PARENT_PLATFORM_SCHEMA.extend(
 
 def get_scanner(hass, config):
     """Return a router scanner instance."""
-    scanner = EEBrightBoxScanner(config[Platform.DEVICE_TRACKER])
+    scanner = EEBrightBoxScanner(config[DOMAIN])
 
     return scanner if scanner.check_config() else None
 
