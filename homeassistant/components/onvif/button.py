0729236692bcde1b0f6c2b3fb9044eaa8b8bc654
Eric Severance <esev@esev.com>
2022-01-05 17:17:29 -0800

    Raise exception to indicate failure

--- homeassistant/components/onvif/button.py
+++ homeassistant/components/onvif/button.py
@@ -1,7 +1,5 @@
 """ONVIF Buttons."""
 
-from httpx import RequestError
-
 from homeassistant.components.button import ButtonDeviceClass, ButtonEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ENTITY_CATEGORY_CONFIG
@@ -9,7 +7,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .base import ONVIFBaseEntity
-from .const import DOMAIN, LOGGER
+from .const import DOMAIN
 from .device import ONVIFDevice
 
 
@@ -40,7 +38,4 @@ class RebootButton(ONVIFBaseEntity, ButtonEntity):
     async def async_press(self) -> None:
         """Send out a SystemReboot command."""
         device_mgmt = self.device.device.create_devicemgmt_service()
-        try:
-            await device_mgmt.SystemReboot()
-        except RequestError as err:
-            LOGGER.warning("Couldn't reboot %s: %s", self.device.name, err)
+        await device_mgmt.SystemReboot()
--- tests/components/onvif/test_button.py
+++ tests/components/onvif/test_button.py
@@ -1,8 +1,6 @@
 """Test button of ONVIF integration."""
 from unittest.mock import AsyncMock
 
-from httpx import RequestError
-
 from homeassistant.components.button import DOMAIN as BUTTON_DOMAIN, ButtonDeviceClass
 from homeassistant.const import ATTR_DEVICE_CLASS, ATTR_ENTITY_ID, STATE_UNKNOWN
 from homeassistant.helpers import entity_registry as er
@@ -30,23 +28,6 @@ async def test_reboot_button_press(hass):
     _, camera, _ = await setup_onvif_integration(hass)
     devicemgmt = camera.create_devicemgmt_service()
     devicemgmt.SystemReboot = AsyncMock(return_value=True)
-
-    await hass.services.async_call(
-        BUTTON_DOMAIN,
-        "press",
-        {ATTR_ENTITY_ID: "button.testcamera_reboot"},
-        blocking=True,
-    )
-    await hass.async_block_till_done()
-
-    devicemgmt.SystemReboot.assert_called_once()
-
-
-async def test_reboot_button_press_failure(hass):
-    """Test Reboot button press failure."""
-    _, camera, _ = await setup_onvif_integration(hass)
-    devicemgmt = camera.create_devicemgmt_service()
-    devicemgmt.SystemReboot = AsyncMock(side_effect=RequestError("Test"))
 
     await hass.services.async_call(
         BUTTON_DOMAIN,
