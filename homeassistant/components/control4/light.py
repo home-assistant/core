f59f04ff48da6250635169a8d08f01bd4d473007
lawtancool <26829131+lawtancool@users.noreply.github.com>
2022-01-07 10:46:46 -0800

    Add back AddEntitiesCallback import

--- homeassistant/components/control4/light.py
+++ homeassistant/components/control4/light.py
@@ -14,6 +14,7 @@ from homeassistant.components.light import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import Control4Entity, get_items_of_category
 from .const import CONF_DIRECTOR, CONTROL4_ENTITY_TYPE, DOMAIN
