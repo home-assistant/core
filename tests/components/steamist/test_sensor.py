69fb6b4c73880bbbe9662fa95e51da12574bbcb0
J. Nick Koston <nick@koston.org>
2022-01-07 19:47:35 -1000

    Update tests/components/steamist/test_sensor.py

--- tests/components/steamist/test_sensor.py
+++ tests/components/steamist/test_sensor.py
@@ -18,7 +18,7 @@ async def test_steam_active(hass: HomeAssistant) -> None:
 
 
 async def test_steam_inactive(hass: HomeAssistant) -> None:
-    """Test that the binary sensors are setup with the expected values when steam is not active."""
+    """Test that the sensors are setup with the expected values when steam is not active."""
     await _async_setup_entry_with_status(hass, MOCK_ASYNC_GET_STATUS_INACTIVE)
     assert hass.states.get("sensor.steam_temperature").state == "21"
     assert hass.states.get("sensor.steam_minutes_remain").state == "0"
