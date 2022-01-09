e343954b8d7aa103418c65f75cd37490ca8bd38e
Naya Verdier <nayaverdier@users.noreply.github.com>
2022-01-08 23:38:39 -0800

    Refresh device list on halohome integration reload

--- homeassistant/components/halohome/__init__.py
+++ homeassistant/components/halohome/__init__.py
@@ -1,16 +1,36 @@
 """Integrate HALO Home into home assistant."""
+import logging
+
+import halohome
 from halohome import Connection
 
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 
 from .const import CONF_LOCATIONS, DOMAIN, PLATFORMS
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up HALO Home from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = Connection(entry.data[CONF_LOCATIONS])
+
+    try:
+        locations = await halohome.list_devices(
+            entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD]
+        )
+    except Exception as e:
+        _LOGGER.error(f"Caught exception refreshing HALO devices: {e}")
+        locations = entry.data[CONF_LOCATIONS]
+
+    if locations != entry.data[CONF_LOCATIONS]:
+        hass.config_entries.async_update_entry(
+            entry, data={**entry.data, CONF_LOCATIONS: locations}
+        )
+
+    hass.data[DOMAIN][entry.entry_id] = Connection(locations)
 
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
