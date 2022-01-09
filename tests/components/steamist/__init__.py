88fb2da5d4305c3669e089b2eb6ce8655d3e46a8
J. Nick Koston <nick@koston.org>
2022-01-09 09:17:52 -1000

    discovery test

--- tests/components/steamist/__init__.py
+++ tests/components/steamist/__init__.py
@@ -5,12 +5,14 @@ from contextlib import contextmanager
 from unittest.mock import AsyncMock, MagicMock, patch
 
 from aiosteamist import Steamist, SteamistStatus
+from discovery30303 import AIODiscovery30303, Device30303
 
 from homeassistant.components import steamist
 from homeassistant.components.steamist.const import DOMAIN
 from homeassistant.config_entries import ConfigEntry, ConfigEntryState
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 from homeassistant.setup import async_setup_component
 
 from tests.common import MockConfigEntry
@@ -20,6 +22,14 @@ MOCK_ASYNC_GET_STATUS_INACTIVE = SteamistStatus(
 )
 MOCK_ASYNC_GET_STATUS_ACTIVE = SteamistStatus(
     temp=102, temp_units="F", minutes_remain=14, active=True
+)
+DEVICE_MAC_ADDRESS = "AA:BB:CC:DD:EE:FF"
+FORMATTED_MAC_ADDRESS = dr.format_mac(DEVICE_MAC_ADDRESS)
+DEVICE_30303 = Device30303(
+    ipaddress="127.0.0.1",
+    name="Master Bath",
+    mac="AA:BB:CC:DD:EE:FF",
+    hostname="MY450-EEFF",
 )
 
 
@@ -56,6 +66,25 @@ def _patch_status(status: SteamistStatus, client: Steamist | None = None):
     @contextmanager
     def _patcher():
         with patch("homeassistant.components.steamist.Steamist", return_value=client):
+            yield
+
+    return _patcher()
+
+
+def _patch_discovery(device=None, no_device=False):
+    mock_aio_discovery = MagicMock(auto_spec=AIODiscovery30303)
+    if no_device:
+        mock_aio_discovery.async_scan = AsyncMock(side_effect=OSError)
+    else:
+        mock_aio_discovery.async_scan = AsyncMock()
+    mock_aio_discovery.found_devices = [] if no_device else [device or DEVICE_30303]
+
+    @contextmanager
+    def _patcher():
+        with patch(
+            "homeassistant.components.steamist.discovery.AIODiscovery30303",
+            return_value=mock_aio_discovery,
+        ):
             yield
 
     return _patcher()
--- tests/components/steamist/test_config_flow.py
+++ tests/components/steamist/test_config_flow.py
@@ -4,8 +4,18 @@ from unittest.mock import patch
 
 from homeassistant import config_entries
 from homeassistant.components.steamist.const import DOMAIN
+from homeassistant.const import CONF_DEVICE
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import RESULT_TYPE_CREATE_ENTRY, RESULT_TYPE_FORM
+
+from . import (
+    FORMATTED_MAC_ADDRESS,
+    MOCK_ASYNC_GET_STATUS_INACTIVE,
+    _patch_discovery,
+    _patch_status,
+)
+
+MODULE = "homeassistant.components.steamist"
 
 
 async def test_form(hass: HomeAssistant) -> None:
@@ -78,3 +88,67 @@ async def test_form_unknown_exception(hass: HomeAssistant) -> None:
 
     assert result2["type"] == RESULT_TYPE_FORM
     assert result2["errors"] == {"base": "unknown"}
+
+
+async def test_discovery(hass: HomeAssistant):
+    """Test setting up discovery."""
+    with _patch_discovery(), _patch_status(MOCK_ASYNC_GET_STATUS_INACTIVE):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
+        await hass.async_block_till_done()
+        assert result["type"] == "form"
+        assert result["step_id"] == "user"
+        assert not result["errors"]
+
+        result2 = await hass.config_entries.flow.async_configure(result["flow_id"], {})
+        await hass.async_block_till_done()
+        assert result2["type"] == "form"
+        assert result2["step_id"] == "pick_device"
+        assert not result2["errors"]
+
+        # test we can try again
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
+        assert result["type"] == "form"
+        assert result["step_id"] == "user"
+        assert not result["errors"]
+
+        result2 = await hass.config_entries.flow.async_configure(result["flow_id"], {})
+        await hass.async_block_till_done()
+        assert result2["type"] == "form"
+        assert result2["step_id"] == "pick_device"
+        assert not result2["errors"]
+
+    with _patch_discovery(), _patch_status(MOCK_ASYNC_GET_STATUS_INACTIVE), patch(
+        f"{MODULE}.async_setup", return_value=True
+    ) as mock_setup, patch(
+        f"{MODULE}.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        result3 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_DEVICE: FORMATTED_MAC_ADDRESS},
+        )
+        await hass.async_block_till_done()
+
+    assert result3["type"] == "create_entry"
+    assert result3["title"] == "Master Bath"
+    assert result3["data"] == {"host": "127.0.0.1", "name": "Master Bath"}
+    mock_setup.assert_called_once()
+    mock_setup_entry.assert_called_once()
+
+    # ignore configured devices
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == "form"
+    assert result["step_id"] == "user"
+    assert not result["errors"]
+
+    with _patch_discovery(), _patch_status(MOCK_ASYNC_GET_STATUS_INACTIVE):
+        result2 = await hass.config_entries.flow.async_configure(result["flow_id"], {})
+        await hass.async_block_till_done()
+
+    assert result2["type"] == "abort"
+    assert result2["reason"] == "no_devices_found"
