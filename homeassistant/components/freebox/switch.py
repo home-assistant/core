4fbcb6d905e55ec5886af6c2bd5192978eae9a34
Eric Chauvet <eric.chauvet@test-tree.com>
2022-01-09 01:58:32 +0000

    Added support for RFDomus and IOHomeControl adapters in Freebox integration

--- homeassistant/components/freebox/switch.py
+++ homeassistant/components/freebox/switch.py
@@ -2,12 +2,14 @@
 from __future__ import annotations
 
 import logging
+from typing import Any
 
 from freebox_api.exceptions import InsufficientPermissionsError
 
-from homeassistant.components.switch import SwitchEntity
+from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -22,7 +24,180 @@ async def async_setup_entry(
 ) -> None:
     """Set up the switch."""
     router = hass.data[DOMAIN][entry.unique_id]
-    async_add_entities([FreeboxWifiSwitch(router)], True)
+
+    entities = []
+
+    entities.append(FreeboxWifiSwitch(router))
+
+    _LOGGER.info(
+        "%s - %s - %s home node(s)", router.name, router.mac, len(router.home_nodes)
+    )
+
+    for home_node in router.home_nodes.values():
+        if home_node["category"] != "shutter":
+            for endpoint in home_node.get("show_endpoints"):
+                if endpoint["ep_type"] == "slot" and endpoint["value_type"] == "bool":
+                    entities.append(
+                        FreeboxHomeNodeSwitch(
+                            router,
+                            home_node,
+                            endpoint,
+                            endpoint["name"],
+                        )
+                    )
+
+    async_add_entities(entities, True)
+
+
+class FreeboxSwitch(SwitchEntity):
+    """Representation of a Freebox switch."""
+
+    _attr_should_poll = False
+
+    def __init__(
+        self,
+        router: FreeboxRouter,
+        endpoint_name: str,
+        unik: Any,
+    ) -> None:
+        """Initialize a Freebox binary_sensors."""
+        self.entity_description = SwitchEntityDescription(
+            key="switch", name=endpoint_name
+        )
+        self._router = router
+        self._unik = unik
+        self._attr_unique_id = f"{router.mac} {endpoint_name} {unik}"
+
+    @callback
+    def async_update_state(self) -> None:
+        """Update the Freebox binary_sensors."""
+        # state = self._router.sensors[self.entity_description.key]
+        # self._attr_is_on = state
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device information."""
+        return self._router.device_info
+
+    @callback
+    def async_on_demand_update(self):
+        """Update state."""
+        self.async_update_state()
+        self.async_write_ha_state()
+
+    async def async_added_to_hass(self):
+        """Register state update callback."""
+        self.async_update_state()
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass,
+                self._router.signal_sensor_update,
+                self.async_on_demand_update,
+            )
+        )
+
+
+class FreeboxHomeNodeSwitch(FreeboxSwitch):
+    """Representation of a Freebox Home node switch."""
+
+    def __init__(
+        self,
+        router: FreeboxRouter,
+        home_node: dict[str, Any],
+        endpoint: dict[str, Any],
+        description: SwitchEntityDescription,
+    ) -> None:
+        """Initialize a Freebox Home node switch."""
+        super().__init__(router, description, f"{home_node['id']} {endpoint['id']}")
+        self._home_node = home_node
+        self._endpoint = endpoint
+        self._attr_name = f"{home_node['label']} {endpoint['name']}"
+        self._unique_id = f"{self._router.mac} {endpoint['name']} {self._home_node['id']} {endpoint['id']}"
+        self._enabled = None
+        self._get_endpoint_id = None
+
+        # Discover for get endpoint
+        for endpoint_candidate in home_node.get("show_endpoints"):
+            if (
+                endpoint_candidate["name"] == endpoint["name"]
+                and endpoint_candidate["ep_type"] == "signal"
+            ):
+                self._get_endpoint_id = endpoint_candidate["id"]
+                break
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device information."""
+        fw_version = None
+        if "props" in self._home_node:
+            props = self._home_node["props"]
+            if "FwVersion" in props:
+                fw_version = props["FwVersion"]
+
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._home_node["id"])},
+            model=f'{self._home_node["category"]}',
+            name=f"{self._home_node['label']}",
+            sw_version=fw_version,
+            via_device=(
+                DOMAIN,
+                self._router.mac,
+            ),
+            vendor_name="Freebox SAS",
+            manufacturer="Freebox SAS",
+        )
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if device is on."""
+        return self._enabled
+
+    @callback
+    def async_update_state(self) -> None:
+        """Update the Freebox Home node switch."""
+        current_home_node = self._router.home_nodes.get(self._home_node.get("id"))
+        if current_home_node.get("show_endpoints"):
+            for end_point in current_home_node["show_endpoints"]:
+                if self._get_endpoint_id == end_point["id"]:
+                    self._enabled = end_point["value"]
+                    break
+        self._attr_is_on = self._enabled
+
+    async def _async_set_state(self, enabled: bool):
+        """Turn the switch on or off."""
+        value_enabled = {"value": enabled}
+        try:
+            await self._router.api.home.set_home_endpoint_value(
+                self._home_node["id"], self._endpoint["id"], value_enabled
+            )
+            self._enabled = enabled
+        except InsufficientPermissionsError:
+            _LOGGER.warning(
+                "Home Assistant does not have permissions to modify the Freebox settings. Please refer to documentation"
+            )
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the switch on."""
+        await self._async_set_state(True)
+        self.async_write_ha_state()
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the switch off."""
+        await self._async_set_state(False)
+        self.async_write_ha_state()
+
+    async def async_update(self):
+        """Get switch status and update it."""
+        try:
+            ret = await self._router.api.home.get_home_endpoint_value(
+                self._home_node["id"], self._get_endpoint_id
+            )
+            self._enabled = ret["value"]
+        except InsufficientPermissionsError:
+            _LOGGER.warning(
+                "Home Assistant does not have permissions to modify the Freebox settings. Please refer to documentation"
+            )
+        return self._enabled
 
 
 class FreeboxWifiSwitch(SwitchEntity):
