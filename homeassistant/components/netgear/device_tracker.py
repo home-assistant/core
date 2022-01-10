36fba71f7a0c7cf2cc90f6f41dc491b6d085c47e
epenet <6771947+epenet@users.noreply.github.com>
2022-01-10 22:54:35 +0100

    Remove deprecated async_get_scanner from netgear (#63862)

--- homeassistant/components/netgear/device_tracker.py
+++ homeassistant/components/netgear/device_tracker.py
@@ -1,15 +1,16 @@
 """Support for Netgear routers."""
+from __future__ import annotations
+
 import logging
 
 import voluptuous as vol
 
 from homeassistant.components.device_tracker import (
-    DOMAIN as DEVICE_TRACKER_DOMAIN,
     PLATFORM_SCHEMA as PARENT_PLATFORM_SCHEMA,
     SOURCE_TYPE_ROUTER,
 )
 from homeassistant.components.device_tracker.config_entry import ScannerEntity
-from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     CONF_DEVICES,
     CONF_EXCLUDE,
@@ -23,7 +24,7 @@ from homeassistant.core import HomeAssistant, callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DEVICE_ICONS, DOMAIN
+from .const import DEVICE_ICONS
 from .router import NetgearDeviceEntity, NetgearRouter, async_setup_netgear_entry
 
 _LOGGER = logging.getLogger(__name__)
@@ -42,25 +43,6 @@ PLATFORM_SCHEMA = PARENT_PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_APS, default=[]): vol.All(cv.ensure_list, [cv.string]),
     }
 )
-
-
-async def async_get_scanner(hass, config):
-    """Import Netgear configuration from YAML."""
-    hass.async_create_task(
-        hass.config_entries.flow.async_init(
-            DOMAIN,
-            context={"source": SOURCE_IMPORT},
-            data=config[DEVICE_TRACKER_DOMAIN],
-        )
-    )
-
-    _LOGGER.warning(
-        "Your Netgear configuration has been imported into the UI, "
-        "please remove it from configuration.yaml. "
-        "Loading Netgear via platform setup is now deprecated"
-    )
-
-    return None
 
 
 async def async_setup_entry(
