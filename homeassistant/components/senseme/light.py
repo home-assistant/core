543f72102ffd5ae53d898712fa0d826bb397b9fd
J. Nick Koston <nick@koston.org>
2022-01-09 18:17:07 -1000

    merge updates

--- homeassistant/components/senseme/light.py
+++ homeassistant/components/senseme/light.py
@@ -1,27 +1,36 @@
 """Support for Big Ass Fans SenseME light."""
-import logging
-from typing import Any, Optional
+from __future__ import annotations
+
+from typing import Any
 
 from aiosenseme import SensemeDevice
 
+from homeassistant import config_entries
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
     ATTR_COLOR_TEMP,
-    SUPPORT_BRIGHTNESS,
-    SUPPORT_COLOR_TEMP,
+    COLOR_MODE_BRIGHTNESS,
+    COLOR_MODE_COLOR_TEMP,
     LightEntity,
 )
-from homeassistant.const import CONF_DEVICE
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.util.color import (
+    color_temperature_kelvin_to_mired,
+    color_temperature_mired_to_kelvin,
+)
 
-from . import SensemeEntity
 from .const import DOMAIN
+from .entity import SensemeEntity
 
-_LOGGER = logging.getLogger(__name__)
 
-
-async def async_setup_entry(hass, entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: config_entries.ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
     """Set up SenseME lights."""
-    device = hass.data[DOMAIN][entry.entry_id][CONF_DEVICE]
+    device = hass.data[DOMAIN][entry.entry_id]
     if device.has_light:
         async_add_entities([HASensemeLight(device)])
 
@@ -33,69 +42,46 @@ class HASensemeLight(SensemeEntity, LightEntity):
         """Initialize the entity."""
         self._device = device
         if device.is_light:
-            name = device.name
+            name = device.name  # The device itself is a light
         else:
-            name = f"{device.name} Light"
+            name = f"{device.name} Light"  # A fan light
         super().__init__(device, name)
-        self._supported_features = SUPPORT_BRIGHTNESS
         if device.is_light:
-            self._supported_features |= SUPPORT_COLOR_TEMP
+            self._attr_supported_color_modes = {COLOR_MODE_COLOR_TEMP}
+            self._attr_color_mode = COLOR_MODE_COLOR_TEMP
+        else:
+            self._attr_supported_color_modes = {COLOR_MODE_BRIGHTNESS}
+            self._attr_color_mode = COLOR_MODE_BRIGHTNESS
+        self._attr_unique_id = f"{self._device.uuid}-LIGHT"  # for legacy compat
+        self._attr_min_mireds = color_temperature_kelvin_to_mired(
+            self._device.light_color_temp_max
+        )
+        self._attr_max_mireds = color_temperature_kelvin_to_mired(
+            self._device.light_color_temp_min
+        )
 
-    @property
-    def unique_id(self) -> str:
-        """Return a unique identifier for this light."""
-        return f"{self._device.uuid}-LIGHT"
-
-    @property
-    def is_on(self) -> bool:
-        """Return true if light is on."""
-        return self._device.light_on
-
-    @property
-    def brightness(self) -> int:
-        """Return the brightness of the light."""
-        light_brightness = self._device.light_brightness * 16
-        if light_brightness == 256:
-            light_brightness = 255
-        return int(light_brightness)
-
-    @property
-    def color_temp(self) -> Optional[int]:
-        """Return the color temp value in mireds."""
-        color_temp = int(round(1000000.0 / float(self._device.light_color_temp)))
-        return color_temp
-
-    @property
-    def min_mireds(self) -> int:
-        """Return the coldest color temp that this light supports."""
-        color_temp = int(round(1000000.0 / float(self._device.light_color_temp_max)))
-        return color_temp
-
-    @property
-    def max_mireds(self) -> int:
-        """Return the warmest color temp that this light supports."""
-        color_temp = int(round(1000000.0 / float(self._device.light_color_temp_min)))
-        return color_temp
-
-    @property
-    def supported_features(self) -> int:
-        """Flag supported features."""
-        return self._supported_features
+    @callback
+    def _async_update_attrs(self) -> None:
+        """Update attrs from device."""
+        self._attr_is_on = self._device.light_on
+        self._attr_brightness = int(min(255, self._device.light_brightness * 16))
+        self._attr_color_temp = color_temperature_kelvin_to_mired(
+            self._device.light_color_temp
+        )
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn on the light."""
-        brightness = kwargs.get(ATTR_BRIGHTNESS)
-        color_temp = kwargs.get(ATTR_COLOR_TEMP)
-        if color_temp is not None:
-            self._device.light_color_temp = int(round(1000000.0 / float(color_temp)))
-        if brightness is None:
-            # no brightness, just turn the light on
-            self._device.light_on = True
-        else:
+        if (color_temp := kwargs.get(ATTR_COLOR_TEMP)) is not None:
+            self._device.light_color_temp = color_temperature_mired_to_kelvin(
+                color_temp
+            )
+        if (brightness := kwargs.get(ATTR_BRIGHTNESS)) is not None:
             # set the brightness, which will also turn on/off light
             if brightness == 255:
                 brightness = 256  # this will end up as 16 which is max
             self._device.light_brightness = int(brightness / 16)
+        else:
+            self._device.light_on = True
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the light."""
