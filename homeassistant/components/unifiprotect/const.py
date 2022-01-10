b324ceb99d9879f39b232e69772d81f12254013a
Christopher Bailey <cbailey@mort.is>
2022-01-10 16:04:53 -0500

    Add UniFi Protect global services (#63768)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/unifiprotect/const.py
+++ homeassistant/components/unifiprotect/const.py
@@ -1,8 +1,10 @@
 """Constant definitions for UniFi Protect Integration."""
 
 from pyunifiprotect.data.types import ModelType, Version
+import voluptuous as vol
 
-from homeassistant.const import Platform
+from homeassistant.const import ATTR_DEVICE_ID, ATTR_ENTITY_ID, Platform
+from homeassistant.helpers import config_validation as cv
 
 DOMAIN = "unifiprotect"
 
@@ -15,6 +17,7 @@ ATTR_BITRATE = "bitrate"
 ATTR_CHANNEL_ID = "channel_id"
 ATTR_MESSAGE = "message"
 ATTR_DURATION = "duration"
+ATTR_ANONYMIZE = "anonymize"
 
 CONF_DISABLE_RTSP = "disable_rtsp"
 CONF_ALL_UPDATES = "all_updates"
@@ -46,6 +49,16 @@ OUTDATED_LOG_MESSAGE = "You are running v%s of UniFi Protect. Minimum required v
 
 TYPE_EMPTY_VALUE = ""
 
+SERVICE_ADD_DOORBELL_TEXT = "add_doorbell_text"
+SERVICE_REMOVE_DOORBELL_TEXT = "remove_doorbell_text"
+SERVICE_SET_DEFAULT_DOORBELL_TEXT = "set_default_doorbell_text"
+
+ALL_GLOBAL_SERIVCES = [
+    SERVICE_ADD_DOORBELL_TEXT,
+    SERVICE_REMOVE_DOORBELL_TEXT,
+    SERVICE_SET_DEFAULT_DOORBELL_TEXT,
+]
+
 PLATFORMS = [
     Platform.BINARY_SENSOR,
     Platform.BUTTON,
@@ -57,3 +70,43 @@ PLATFORMS = [
     Platform.SENSOR,
     Platform.SWITCH,
 ]
+
+
+DOORBELL_TEXT_SCHEMA = vol.All(
+    vol.Schema(
+        {
+            **cv.ENTITY_SERVICE_FIELDS,
+            vol.Required(ATTR_MESSAGE): cv.string,
+        },
+    ),
+    cv.has_at_least_one_key(ATTR_DEVICE_ID),
+)
+
+GENERATE_DATA_SCHEMA = vol.All(
+    vol.Schema(
+        {
+            **cv.ENTITY_SERVICE_FIELDS,
+            vol.Required(ATTR_DURATION): vol.Coerce(int),
+            vol.Required(ATTR_ANONYMIZE): vol.Coerce(bool),
+        },
+    ),
+    cv.has_at_least_one_key(ATTR_DEVICE_ID),
+)
+
+PROFILE_WS_SCHEMA = vol.All(
+    vol.Schema(
+        {
+            **cv.ENTITY_SERVICE_FIELDS,
+            vol.Required(ATTR_DURATION): vol.Coerce(int),
+        },
+    ),
+    cv.has_at_least_one_key(ATTR_DEVICE_ID),
+)
+
+SET_DOORBELL_LCD_MESSAGE_SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_ENTITY_ID): cv.entity_ids,
+        vol.Required(ATTR_MESSAGE): cv.string,
+        vol.Optional(ATTR_DURATION, default="None"): cv.string,
+    }
+)
