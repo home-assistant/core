8b8dd0b61e0ca425ae5479b20cbe5250ca8384e8
J. Nick Koston <nick@koston.org>
2022-01-09 19:28:59 -1000

    Add DeviceInfo to steamist (#63773)

--- homeassistant/components/steamist/entity.py
+++ homeassistant/components/steamist/entity.py
@@ -4,9 +4,12 @@ from __future__ import annotations
 from aiosteamist import SteamistStatus
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.entity import Entity, EntityDescription
+from homeassistant.const import CONF_HOST, CONF_NAME
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.entity import DeviceInfo, Entity, EntityDescription
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
+from .const import CONF_MODEL
 from .coordinator import SteamistDataUpdateCoordinator
 
 
@@ -27,6 +30,14 @@ class SteamistEntity(CoordinatorEntity, Entity):
         if coordinator.device_name:
             self._attr_name = f"{coordinator.device_name} {description.name}"
         self._attr_unique_id = f"{entry.entry_id}_{description.key}"
+        if entry.unique_id:  # Only present if UDP broadcast works
+            self._attr_device_info = DeviceInfo(
+                connections={(dr.CONNECTION_NETWORK_MAC, entry.unique_id)},
+                name=entry.data[CONF_NAME],
+                manufacturer="Steamist",
+                model=entry.data[CONF_MODEL],
+                configuration_url=f"http://{entry.data[CONF_HOST]}",
+            )
 
     @property
     def _status(self) -> SteamistStatus:
