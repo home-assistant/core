da6c93eb1dd040df2353ce869b9dbdd2032677c4
Christopher Bailey <cbailey@mort.is>
2022-01-10 16:04:53 -0500

    Add UniFi Protect global services (#63768)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- tests/components/unifiprotect/test_init.py
+++ tests/components/unifiprotect/test_init.py
@@ -1,16 +1,18 @@
 """Test the UniFi Protect setup flow."""
 from __future__ import annotations
 
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, patch
 
 from pyunifiprotect import NotAuthorized, NvrError
 from pyunifiprotect.data import NVR
 
-from homeassistant.components.unifiprotect.const import CONF_DISABLE_RTSP
+from homeassistant.components.unifiprotect.const import CONF_DISABLE_RTSP, DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
-from .conftest import MockEntityFixture
+from .conftest import MockBootstrap, MockEntityFixture
+
+from tests.common import MockConfigEntry
 
 
 async def test_setup(hass: HomeAssistant, mock_entry: MockEntityFixture):
@@ -22,6 +24,52 @@ async def test_setup(hass: HomeAssistant, mock_entry: MockEntityFixture):
     assert mock_entry.entry.state == ConfigEntryState.LOADED
     assert mock_entry.api.update.called
     assert mock_entry.entry.unique_id == mock_entry.api.bootstrap.nvr.mac
+
+
+async def test_setup_multiple(
+    hass: HomeAssistant,
+    mock_entry: MockEntityFixture,
+    mock_client,
+    mock_bootstrap: MockBootstrap,
+):
+    """Test working setup of unifiprotect entry."""
+
+    await hass.config_entries.async_setup(mock_entry.entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert mock_entry.entry.state == ConfigEntryState.LOADED
+    assert mock_entry.api.update.called
+    assert mock_entry.entry.unique_id == mock_entry.api.bootstrap.nvr.mac
+
+    nvr = mock_bootstrap.nvr
+    nvr._api = mock_client
+    nvr.mac = "A1E00C826983"
+    nvr.id
+    mock_client.get_nvr = AsyncMock(return_value=nvr)
+
+    with patch("homeassistant.components.unifiprotect.ProtectApiClient") as mock_api:
+        mock_config = MockConfigEntry(
+            domain=DOMAIN,
+            data={
+                "host": "1.1.1.1",
+                "username": "test-username",
+                "password": "test-password",
+                "id": "UnifiProtect",
+                "port": 443,
+                "verify_ssl": False,
+            },
+            version=2,
+        )
+        mock_config.add_to_hass(hass)
+
+        mock_api.return_value = mock_client
+
+        await hass.config_entries.async_setup(mock_config.entry_id)
+        await hass.async_block_till_done()
+
+        assert mock_config.state == ConfigEntryState.LOADED
+        assert mock_client.update.called
+        assert mock_config.unique_id == mock_client.bootstrap.nvr.mac
 
 
 async def test_reload(hass: HomeAssistant, mock_entry: MockEntityFixture):
