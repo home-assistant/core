63389f0ec8ad4c52d084aa5c5d4a0c20900f3517
Erik Montnemery <erik@montnemery.com>
2022-01-11 18:51:16 +0100

    Import persistent notification (part 3) (#63900)

--- homeassistant/components/lupusec/__init__.py
+++ homeassistant/components/lupusec/__init__.py
@@ -5,6 +5,7 @@ import lupupy
 from lupupy.exceptions import LupusecException
 import voluptuous as vol
 
+from homeassistant.components import persistent_notification
 from homeassistant.const import (
     CONF_IP_ADDRESS,
     CONF_NAME,
@@ -58,7 +59,8 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     except LupusecException as ex:
         _LOGGER.error(ex)
 
-        hass.components.persistent_notification.create(
+        persistent_notification.create(
+            hass,
             f"Error: {ex}<br />You will need to restart hass after fixing.",
             title=NOTIFICATION_TITLE,
             notification_id=NOTIFICATION_ID,
