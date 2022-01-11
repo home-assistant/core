0793445c40f5695a3523a499b1fc21b6149cce04
epenet <6771947+epenet@users.noreply.github.com>
2022-01-11 08:07:56 +0100

    Cleanup netgear platform schema (#63873)
    * Cleanup netgear platform schema
    
    * Cleanup config_flow
    
    * Cleanup tests
    
    * Cleanup CONF_APS constant
    
    * Cleanup tests (take 2)
    
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- homeassistant/components/netgear/device_tracker.py
+++ homeassistant/components/netgear/device_tracker.py
@@ -3,46 +3,16 @@ from __future__ import annotations
 
 import logging
 
-import voluptuous as vol
-
-from homeassistant.components.device_tracker import (
-    PLATFORM_SCHEMA as PARENT_PLATFORM_SCHEMA,
-    SOURCE_TYPE_ROUTER,
-)
+from homeassistant.components.device_tracker import SOURCE_TYPE_ROUTER
 from homeassistant.components.device_tracker.config_entry import ScannerEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (
-    CONF_DEVICES,
-    CONF_EXCLUDE,
-    CONF_HOST,
-    CONF_PASSWORD,
-    CONF_PORT,
-    CONF_SSL,
-    CONF_USERNAME,
-)
 from homeassistant.core import HomeAssistant, callback
-import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DEVICE_ICONS
 from .router import NetgearDeviceEntity, NetgearRouter, async_setup_netgear_entry
 
 _LOGGER = logging.getLogger(__name__)
-
-CONF_APS = "accesspoints"
-
-PLATFORM_SCHEMA = PARENT_PLATFORM_SCHEMA.extend(
-    {
-        vol.Optional(CONF_HOST): cv.string,
-        vol.Optional(CONF_SSL): cv.boolean,
-        vol.Optional(CONF_USERNAME): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string,
-        vol.Optional(CONF_PORT): cv.port,
-        vol.Optional(CONF_DEVICES, default=[]): vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_EXCLUDE, default=[]): vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_APS, default=[]): vol.All(cv.ensure_list, [cv.string]),
-    }
-)
 
 
 async def async_setup_entry(
