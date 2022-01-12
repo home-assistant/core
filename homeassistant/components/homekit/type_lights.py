2cb4e955ca658ebe228e08991b560c63a9ce0ad9
J. Nick Koston <nick@koston.org>
2022-01-11 14:22:46 -1000

    fixes

--- homeassistant/components/homekit/type_lights.py
+++ homeassistant/components/homekit/type_lights.py
@@ -190,7 +190,11 @@ class Light(HomeAccessory):
             else:
                 params[ATTR_HS_COLOR] = hue_sat
 
-        if ATTR_RGBWW_COLOR not in params and ATTR_RGBW_COLOR not in params:
+        if (
+            brightness_pct
+            and ATTR_RGBWW_COLOR not in params
+            and ATTR_RGBW_COLOR not in params
+        ):
             params[ATTR_BRIGHTNESS_PCT] = brightness_pct
 
         self.async_call_service(DOMAIN, service, params, ", ".join(events))
