d0578509710ac27a20d1e5576e7eee15d140057d
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-01-12 08:04:17 +0100

    Update typing - collections.abc (3) (#63947)

--- tests/components/rtsp_to_webrtc/test_init.py
+++ tests/components/rtsp_to_webrtc/test_init.py
@@ -3,7 +3,8 @@
 from __future__ import annotations
 
 import base64
-from typing import Any, AsyncGenerator, Awaitable, Callable
+from collections.abc import AsyncGenerator, Awaitable, Callable
+from typing import Any
 from unittest.mock import patch
 
 import aiohttp
