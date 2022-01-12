bf1170ea288361170e7a9c060ee999ec99368148
Leonardo Figueiro <leoagfig@gmail.com>
2022-01-12 18:07:00 +0000

    Updated support.py

--- homeassistant/components/wilight/support.py
+++ homeassistant/components/wilight/support.py
@@ -9,33 +9,44 @@ from typing import Any
 import voluptuous as vol
 
 
-def wilight_trigger(value: Any) -> str:
+def wilight_trigger(value: Any) -> str | None:
     """Check rules for WiLight Trigger."""
-    # used if nested blocks to reduce not covered by tests
+    step = 1
     err_desc = "Value is None"
+    result_128 = False
+    result_24 = False
+    result_60 = False
+    result_2 = False
+
     if value is not None:
-
+        step = 2
         err_desc = "Expected a string"
-        if isinstance(value, str):
 
-            err_desc = "String should only contain 8 decimals character"
-            regex = re.compile(r"^([0-9]{8})$")
-            if regex.search(value):
+    if (step == 2) & isinstance(value, str):
+        step = 3
+        err_desc = "String should only contain 8 decimals character"
+        if re.search(r"^([0-9]{8})$", value) is not None:
+            step = 4
+            err_desc = "First 3 character should be less than 128"
+            result_128 = int(value[0:3]) < 128
+            result_24 = int(value[3:5]) < 24
+            result_60 = int(value[5:7]) < 60
+            result_2 = int(value[7:8]) < 2
 
-                err_desc = "First 3 character should be less than 128"
-                if int(value[0:3]) < 128:
+    if (step == 4) & result_128:
+        step = 5
+        err_desc = "Hour part should be less than 24"
 
-                    # changed test mode due to 5 nested blocks limit in pylint 3.8
-                    if int(value[3:5]) >= 24:
-                        raise vol.Invalid("Hour part should be less than 24")
+    if (step == 5) & result_24:
+        step = 6
+        err_desc = "Minute part should be less than 60"
 
-                    if int(value[5:7]) >= 60:
-                        raise vol.Invalid("Minute part should be less than 60")
+    if (step == 6) & result_60:
+        step = 7
+        err_desc = "Active part shoul be less than 2"
 
-                    if int(value[7:8]) >= 2:
-                        raise vol.Invalid("Active part shoul be less than 2")
-
-                    return str(value)
+    if (step == 7) & result_2:
+        return value
 
     raise vol.Invalid(err_desc)
 
