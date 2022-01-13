87ec6ce942fbb5dc5fef90306e52f59d771a0fd0
Sean Vig <sean.v.775@gmail.com>
2022-01-13 09:07:56 -0500

    Create custom entity description with press action

--- homeassistant/components/doorbird/button.py
+++ homeassistant/components/doorbird/button.py
@@ -1,5 +1,10 @@
 """Support for powering relays in a DoorBird video doorbell."""
 
+from dataclasses import dataclass
+from typing import Callable
+
+from doorbirdpy import DoorBird
+
 from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -10,8 +15,31 @@ from .entity import DoorBirdEntity
 
 IR_RELAY = "__ir_light__"
 
-RELAY_ENTITY_DESCRIPTION = ButtonEntityDescription(icon="mdi:dip-switch")
-IR_ENTITY_DESCRIPTION = ButtonEntityDescription(icon="mdi:lightbulb")
+
+@dataclass
+class DoorbirdButtonEntityDescriptionMixin:
+    """Mixin to describe a Doorbird Button entity."""
+
+    press_action: Callable[[DoorBird, str], None]
+
+
+@dataclass
+class DoorbirdButtonEntityDescription(
+    ButtonEntityDescription, DoorbirdButtonEntityDescriptionMixin
+):
+    """Class to describe a Doorbird Button entity."""
+
+
+RELAY_ENTITY_DESCRIPTION = DoorbirdButtonEntityDescription(
+    key="relay",
+    press_action=lambda device, relay: device.energize_relay(relay),
+    icon="mdi:dip-switch",
+)
+IR_ENTITY_DESCRIPTION = DoorbirdButtonEntityDescription(
+    key="ir",
+    press_action=lambda device, _: device.turn_light_on(),
+    icon="mdi:lightbulb",
+)
 
 
 async def async_setup_entry(
@@ -27,11 +55,14 @@ async def async_setup_entry(
     doorstation_info = data[DOOR_STATION_INFO]
 
     relays = doorstation_info["RELAYS"]
-    relays.append(IR_RELAY)
 
     entities = [
-        DoorBirdButton(doorstation, doorstation_info, relay) for relay in relays
+        DoorBirdButton(doorstation, doorstation_info, relay, RELAY_ENTITY_DESCRIPTION)
+        for relay in relays
     ]
+    entities.append(
+        DoorBirdButton(doorstation, doorstation_info, IR_RELAY, IR_ENTITY_DESCRIPTION)
+    )
 
     async_add_entities(entities)
 
@@ -39,23 +70,25 @@ async def async_setup_entry(
 class DoorBirdButton(DoorBirdEntity, ButtonEntity):
     """A relay in a DoorBird device."""
 
-    def __init__(self, doorstation, doorstation_info, relay):
+    def __init__(
+        self,
+        doorstation: DoorBird,
+        doorstation_info,
+        relay: str,
+        entity_description: DoorbirdButtonEntityDescription,
+    ) -> None:
         """Initialize a relay in a DoorBird device."""
         super().__init__(doorstation, doorstation_info)
         self._doorstation = doorstation
         self._relay = relay
+        self.entity_description = entity_description
+
         if self._relay == IR_RELAY:
             self._attr_name = f"{self._doorstation.name} IR"
-            self.entity_description = IR_ENTITY_DESCRIPTION
         else:
             self._attr_name = f"{self._doorstation.name} Relay {self._relay}"
-            self.entity_description = RELAY_ENTITY_DESCRIPTION
-
         self._attr_unique_id = f"{self._mac_addr}_{self._relay}"
 
-    def press(self):
+    def press(self) -> None:
         """Power the relay."""
-        if self._relay == IR_RELAY:
-            self._doorstation.device.turn_light_on()
-        else:
-            self._doorstation.device.energize_relay(self._relay)
+        self.entity_description.press_action(self._doorstation.device, self._relay)
