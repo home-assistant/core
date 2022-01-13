daeed5ff42daee41c1e946f21778b15dd7426ebd
Rob Bierbooms <mail@robbierbooms.nl>
2022-01-13 18:44:27 +0100

    Add support for setting RGB and RGBW values for Twinkly lights (#62337)
    * Change library to ttls
    
    * Add rgbw support
    
    * Add client session to config flow
    
    * Fix config flow
    
    * Adjust tests 1
    
    * Fix more tests
    
    * Fix last tests
    
    * Add new tests
    
    * Update test for coverage
    
    * Update test for coverage 2
    
    * Update test for coverage 3
    
    * Change brightness to attribute
    
    * Set RGBW mode only when available
    
    * Add RGB support

--- tests/components/twinkly/test_init.py
+++ tests/components/twinkly/test_init.py
@@ -11,14 +11,21 @@ from homeassistant.components.twinkly.const import (
     CONF_ENTRY_NAME,
     DOMAIN as TWINKLY_DOMAIN,
 )
+from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
-from tests.components.twinkly import TEST_HOST, TEST_MODEL, TEST_NAME_ORIGINAL
+from tests.components.twinkly import (
+    TEST_HOST,
+    TEST_MODEL,
+    TEST_NAME_ORIGINAL,
+    ClientMock,
+)
 
 
 async def test_setup_entry(hass: HomeAssistant):
     """Validate that setup entry also configure the client."""
+    client = ClientMock()
 
     id = str(uuid4())
     config_entry = MockConfigEntry(
@@ -38,7 +45,7 @@ async def test_setup_entry(hass: HomeAssistant):
     with patch(
         "homeassistant.config_entries.ConfigEntries.async_forward_entry_setup",
         side_effect=setup_mock,
-    ):
+    ), patch("homeassistant.components.twinkly.Twinkly", return_value=client):
         await async_setup_entry(hass, config_entry)
 
     assert hass.data[TWINKLY_DOMAIN][id] is not None
@@ -65,3 +72,26 @@ async def test_unload_entry(hass: HomeAssistant):
     await async_unload_entry(hass, config_entry)
 
     assert hass.data[TWINKLY_DOMAIN].get(id) is None
+
+
+async def test_config_entry_not_ready(hass: HomeAssistant):
+    """Validate that config entry is retried."""
+    client = ClientMock()
+    client.is_offline = True
+
+    config_entry = MockConfigEntry(
+        domain=TWINKLY_DOMAIN,
+        data={
+            CONF_ENTRY_HOST: TEST_HOST,
+            CONF_ENTRY_ID: id,
+            CONF_ENTRY_NAME: TEST_NAME_ORIGINAL,
+            CONF_ENTRY_MODEL: TEST_MODEL,
+        },
+    )
+
+    config_entry.add_to_hass(hass)
+
+    with patch("homeassistant.components.twinkly.Twinkly", return_value=client):
+        await hass.config_entries.async_setup(config_entry.entry_id)
+
+    assert config_entry.state is ConfigEntryState.SETUP_RETRY
