d774b4cccb079c49aac2a0404f0f127eba5017a1
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2022-01-14 07:31:12 +0000

    Use const for platforms

--- homeassistant/components/homewizard/const.py
+++ homeassistant/components/homewizard/const.py
@@ -7,11 +7,12 @@ from typing import TypedDict
 # Set up.
 from aiohwenergy.device import Device
 
+from homeassistant.const import Platform
 from homeassistant.helpers.typing import StateType
 
 DOMAIN = "homewizard"
 COORDINATOR = "coordinator"
-PLATFORMS = ["sensor", "switch"]
+PLATFORMS = [Platform.SENSOR, Platform.SWITCH]
 
 # Platform config.
 CONF_SERIAL = "serial"
