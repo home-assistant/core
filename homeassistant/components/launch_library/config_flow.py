fd4a763fe4c1c84e33092220a29a5c7b55f87cf5
Simon Hansen <67142049+DurgNomis-drol@users.noreply.github.com>
2022-01-14 20:02:09 +0100

    Add missing type annotation in config flow

--- homeassistant/components/launch_library/config_flow.py
+++ homeassistant/components/launch_library/config_flow.py
@@ -11,7 +11,7 @@ class LaunchLibraryFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None) -> FlowResult:
+    async def async_step_user(self, user_input: dict[str, Any] | None) -> FlowResult:
         """Handle a flow initialized by the user."""
         # Check if already configured
         if self._async_current_entries():
