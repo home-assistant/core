4b56e84933c111a5b3849c4e3a37180322fbb8cd
Josh Soref <jsoref@users.noreply.github.com>
2022-01-14 02:07:24 -0500

    spelling: emotions
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- homeassistant/components/tts/__init__.py
+++ homeassistant/components/tts/__init__.py
@@ -562,7 +562,7 @@ class Provider:
 
     @property
     def supported_options(self):
-        """Return a list of supported options like voice, emotionen."""
+        """Return a list of supported options like voice, emotions."""
         return None
 
     @property
