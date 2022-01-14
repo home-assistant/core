a3febbc47f81e0e31e6533339ea60ef6a81d6827
Josh Soref <jsoref@users.noreply.github.com>
2022-01-14 02:10:31 -0500

    spelling: lannouncer
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- homeassistant/components/lannouncer/notify.py
+++ homeassistant/components/lannouncer/notify.py
@@ -74,11 +74,11 @@ class LannouncerNotificationService(BaseNotificationService):
             # Check response
             buffer = sock.recv(1024)
             if buffer != b"LANnouncer: OK":
-                _LOGGER.error("Error sending data to Lannnouncer: %s", buffer.decode())
+                _LOGGER.error("Error sending data to Lannouncer: %s", buffer.decode())
 
             # Close socket
             sock.close()
         except socket.gaierror:
             _LOGGER.error("Unable to connect to host %s", self._host)
         except OSError:
-            _LOGGER.exception("Failed to send data to Lannnouncer")
+            _LOGGER.exception("Failed to send data to Lannouncer")
