b3579e8827fb8ea138a8482e5ebd535262c031bf
epenet <epenet@users.noreply.github.com>
2022-01-14 15:36:27 +0000

    Revert "Adjust callback registration"
    This reverts commit 88c179f870536174fca969064789a19ff847f27e.

--- homeassistant/components/raspihats/switch.py
+++ homeassistant/components/raspihats/switch.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING
 
 import voluptuous as vol
 
@@ -108,11 +107,6 @@ class I2CHatSwitch(ToggleEntity):
             else:
                 state = initial_state
             self.I2C_HATS_MANAGER.write_dq(self._address, self._channel, state)
-
-    async def async_added_to_hass(self) -> None:
-        """Register callbacks."""
-        if TYPE_CHECKING:
-            assert self.I2C_HATS_MANAGER
 
         def online_callback():
             """Call fired when board is online."""
