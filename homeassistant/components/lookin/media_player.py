d0c0e7a4011f3733ac73aca93beb397d23e71e60
J. Nick Koston <nick@koston.org>
2022-01-14 09:29:37 -1000

    Fix logic reversal in lookin media_player

--- homeassistant/components/lookin/media_player.py
+++ homeassistant/components/lookin/media_player.py
@@ -142,5 +142,5 @@ class LookinMedia(LookinPowerPushRemoteEntity, MediaPlayerEntity):
         state = status[0]
         mute = status[2]
 
-        self._attr_state = STATE_STANDBY if state == "1" else STATE_ON
+        self._attr_state = STATE_ON if state == "1" else STATE_STANDBY
         self._attr_is_volume_muted = mute == "0"
