5d71fb8537ace8b55439b34c83c1e2f8d79f6d8e
Chris Talkington <chris@talkingtontech.com>
2022-01-16 17:08:46 -0600

    Update sensor.py

--- homeassistant/components/roku/sensor.py
+++ homeassistant/components/roku/sensor.py
@@ -8,10 +8,8 @@ from datetime import datetime
 from rokuecp.models import Device as RokuDevice 
 
 from homeassistant.components.sensor import (
-    SensorDeviceClass,
     SensorEntity,
     SensorEntityDescription,
-    SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
