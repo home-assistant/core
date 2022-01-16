b2ea4d356f0d9e31a9bcb226ce2b3d4644f019c2
Chris Talkington <chris@talkingtontech.com>
2022-01-16 17:30:20 -0600

    Update sensor.py

--- homeassistant/components/roku/sensor.py
+++ homeassistant/components/roku/sensor.py
@@ -41,6 +41,7 @@ SENSORS: tuple[RokuSensorEntityDescription, ...] = (
         key="active_app_id",
         name="Active App ID",
         entity_category=EntityCategory.DIAGNOSTIC,
+        icon="mdi:application-cog",
         value_fn=lambda device: device.app.app_id,
     ),
 )
