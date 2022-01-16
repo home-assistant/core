2697785d568bdf05505c038eaec79224dbc65af1
G Johansson <goran.johansson@shiftit.se>
2022-01-16 15:18:53 +0000

    Cleanup code config flow smhi

--- homeassistant/components/smhi/const.py
+++ homeassistant/components/smhi/const.py
@@ -1,7 +1,7 @@
 """Constants in smhi component."""
 from typing import Final
 
-from homeassistant.components.weather import DOMAIN as WEATHER_DOMAIN
+from homeassistant.const import Platform
 
 ATTR_SMHI_CLOUDINESS: Final = "cloudiness"
 ATTR_SMHI_WIND_GUST_SPEED: Final = "wind_gust_speed"
@@ -10,5 +10,6 @@ ATTR_SMHI_THUNDER_PROBABILITY: Final = "thunder_probability"
 DOMAIN = "smhi"
 
 HOME_LOCATION_NAME = "Home"
+DEFAULT_NAME = "Weather"
 
-ENTITY_ID_SENSOR_FORMAT = WEATHER_DOMAIN + ".smhi_{}"
+ENTITY_ID_SENSOR_FORMAT = Platform.WEATHER + ".smhi_{}"
