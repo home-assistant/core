0244e16efc786027b555c35667fa8332144cdbb2
Sebastien Fortier <sebfortier2288@gmail.com>
2022-01-16 21:03:19 +0000

    Remove unnecessary ()

--- homeassistant/components/soma/utils.py
+++ homeassistant/components/soma/utils.py
@@ -3,4 +3,4 @@
 
 def is_api_response_success(apiResponse: dict) -> bool:
     """Check if the response returned from the Connect API is a success or not."""
-    return ("result" in apiResponse) and (apiResponse["result"].lower() == "success")
+    return "result" in apiResponse and apiResponse["result"].lower() == "success"
