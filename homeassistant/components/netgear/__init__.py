c6c7bb025186f6ebae6895abfb900ed3f15767e7
starkillerOG <starkiller.og@gmail.com>
2022-01-17 12:14:37 +0100

    fix spelling

--- homeassistant/components/netgear/__init__.py
+++ homeassistant/components/netgear/__init__.py
@@ -30,7 +30,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         data[CONF_SSL] = router.ssl
         hass.config_entries.async_update_entry(entry, data=data)
         _LOGGER.warning(
-            "Netgear port-SSL combination updated from (%i, %r) to (%i, %r), this schould only occur after a firmware update.",
+            "Netgear port-SSL combination updated from (%i, %r) to (%i, %r), this should only occur after a firmware update.",
             port,
             ssl,
             router.port,
