87723db85bc424dfb34e8e68d890ea91a027627c
Mick Vleeshouwer <mick@imick.nl>
2022-01-17 19:43:49 +0000

    Fix model stringify None

--- homeassistant/components/overkiz/entity.py
+++ homeassistant/components/overkiz/entity.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from enum import unique
+from typing import cast
 
 from pyoverkiz.enums import OverkizAttribute, OverkizState
 from pyoverkiz.models import Device
@@ -59,23 +60,22 @@ class OverkizEntity(CoordinatorEntity):
         )
 
         model = (
-            str(
-                self.executor.select_state(
-                    OverkizState.CORE_MODEL,
-                    OverkizState.CORE_PRODUCT_MODEL_NAME,
-                    OverkizState.IO_MODEL,
-                ),
+            self.executor.select_state(
+                OverkizState.CORE_MODEL,
+                OverkizState.CORE_PRODUCT_MODEL_NAME,
+                OverkizState.IO_MODEL,
             )
-            or self.device.widget
+            or self.device.widget.value
         )
 
         return DeviceInfo(
             identifiers={(DOMAIN, self.executor.base_device_url)},
             name=self.device.label,
             manufacturer=str(manufacturer),
-            model=model,
-            sw_version=str(
-                self.executor.select_attribute(OverkizAttribute.CORE_FIRMWARE_REVISION)
+            model=str(model),
+            sw_version=cast(
+                str,
+                self.executor.select_attribute(OverkizAttribute.CORE_FIRMWARE_REVISION),
             ),
             hw_version=self.device.controllable_name,
             suggested_area=self.coordinator.areas[self.device.place_oid],
