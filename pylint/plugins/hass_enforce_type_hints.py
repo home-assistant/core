5e918e05939a8de8fa3987b44428f2ec6bfd518e
epenet <epenet@users.noreply.github.com>
2022-01-17 20:13:42 +0000

    Use a dataclasses for list of matches

--- pylint/plugins/hass_enforce_type_hints.py
+++ pylint/plugins/hass_enforce_type_hints.py
@@ -1,8 +1,66 @@
 """Plugin for constructor definitions."""
+from __future__ import annotations
+
+from ast import arguments
+from dataclasses import dataclass
+import re
+
 import astroid
 from pylint.checkers import BaseChecker
 from pylint.interfaces import IAstroidChecker
 from pylint.lint import PyLinter
+
+
+@dataclass
+class TypeHintMatch:
+    """Class for pattern matching."""
+
+    module_filter: re.Pattern
+    function_name: str
+    # TODO: add the correct message
+    arg_types: dict[int, str]
+    return_type: str | None
+    disabled: bool = False
+
+
+_METHOD_MATCH: list[TypeHintMatch] = [
+    TypeHintMatch(
+        module_filter=re.compile(r"^homeassistant.components.[a-zA-Z_]+$"),
+        function_name="setup",
+        arg_types={0: "HomeAssistant", 1: "ConfigType"},
+        return_type="bool",
+    ),
+    TypeHintMatch(
+        module_filter=re.compile(r"^homeassistant.components.[a-zA-Z_]+$"),
+        function_name="async_setup",
+        arg_types={0: "HomeAssistant", 1: "ConfigType"},
+        return_type="bool",
+    ),
+    TypeHintMatch(
+        module_filter=re.compile(r"^homeassistant.components.[a-zA-Z_]+$"),
+        function_name="async_setup_entry",
+        arg_types={0: "HomeAssistant", 1: "ConfigEntry"},
+        return_type="bool",
+    ),
+    TypeHintMatch(
+        module_filter=re.compile(r"^homeassistant.components.[a-zA-Z_]+$"),
+        function_name="async_remove_entry",
+        arg_types={0: "HomeAssistant", 1: "ConfigEntry"},
+        return_type=None,
+    ),
+    TypeHintMatch(
+        module_filter=re.compile(r"^homeassistant.components.[a-zA-Z_]+$"),
+        function_name="async_unload_entry",
+        arg_types={0: "HomeAssistant", 1: "ConfigEntry"},
+        return_type="bool",
+    ),
+    TypeHintMatch(
+        module_filter=re.compile(r"^homeassistant.components.[a-zA-Z_]+$"),
+        function_name="async_migrate_entry",
+        arg_types={0: "HomeAssistant", 1: "ConfigEntry"},
+        return_type="bool",
+    ),
+]
 
 
 class HassTypeHintChecker(BaseChecker):  # type: ignore[misc]
@@ -41,20 +99,36 @@ class HassTypeHintChecker(BaseChecker):  # type: ignore[misc]
     }
     options = ()
 
+    def __init__(self, linter: PyLinter | None = None) -> None:
+        super().__init__(linter)
+        self.current_package: str | None = None
+        self.module: str | None = None
+
+    def visit_module(self, node: astroid.Module) -> None:
+        """Called when a Module node is visited."""
+        self.module = node.name
+        if node.package:
+            self.current_package = node.name
+        else:
+            # Strip name of the current module
+            self.current_package = node.name[: node.name.rfind(".")]
+
     def visit_functiondef(self, node: astroid.FunctionDef) -> None:
         """Called when a FunctionDef node is visited."""
-        if node.is_method() or node.name not in [
-            "setup",
-            "async_setup",
-            "async_setup_entry",
-            "async_remove_entry",
-            "async_unload_entry",
-            "async_migrate_entry",
-        ]:
+        for match in _METHOD_MATCH:
+            self._visit_functiondef(node, match)
+
+    def _visit_functiondef(
+        self, node: astroid.FunctionDef, match: TypeHintMatch
+    ) -> None:
+        if node.name != match.function_name:
+            return
+        if node.is_method():
+            return
+        if not match.module_filter.match(self.module):
             return
 
-        # Check that all arguments are annotated.
-        # The first argument is "self".
+        # Check that at least one argument is annotated.
         args = node.args
         annotations = (
             args.posonlyargs_annotations
@@ -68,38 +142,31 @@ class HassTypeHintChecker(BaseChecker):  # type: ignore[misc]
         if not [annotation for annotation in annotations if annotation is not None]:
             return
 
-        # Check that the first argument is "HomeAssistant".
-        if (
-            not isinstance(annotations[0], astroid.Name)
-            or annotations[0].name != "HomeAssistant"
-        ):
-            self.add_message("hass-enforce-type-hints-hass", node=node)
-        # Check that the second argument is "ConfigType" or "ConfigEntry".
-        if node.name in ["setup", "async_setup"]:
+        # Check that all arguments are annotated.
+        for key, value in match.arg_types.items():
             if (
-                not isinstance(annotations[1], astroid.Name)
-                or annotations[1].name != "ConfigType"
+                not isinstance(annotations[key], astroid.Name)
+                or annotations[key].name != value
             ):
-                self.add_message("hass-enforce-type-hints-config-type", node=node)
-        else:
-            if (
-                not isinstance(annotations[1], astroid.Name)
-                or annotations[1].name != "ConfigEntry"
-            ):
-                self.add_message("hass-enforce-type-hints-config-entry", node=node)
-        # Check that return type is specified and it is "None".
-        if node.name == "async_remove_entry":
+                # TODO: use the correct message
+                # "hass-type-hint-config-entry"
+                # "hass-type-hint-config-type"
+                # "hass-type-hint-hass"
+                self.add_message("hass-type-hint-hass", node=node)
+
+        # Check the return type.
+        if match.return_type is None:
             if (
                 not isinstance(node.returns, astroid.Const)
                 or node.returns.value is not None
             ):
-                self.add_message("hass-enforce-type-hints-return-none", node=node)
+                self.add_message("hass-type-hint-return-none", node=node)
         else:
             if (
                 not isinstance(node.returns, astroid.Name)
                 or node.returns.name != "bool"
             ):
-                self.add_message("hass-enforce-type-hints-return-bool", node=node)
+                self.add_message("hass-type-hint-return-bool", node=node)
 
 
 def register(linter: PyLinter) -> None:
