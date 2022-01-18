318b35384c81344d9cc599fbcbaaafb94a9ecb89
Franck Nijhof <git@frenck.dev>
2022-01-18 21:19:59 +0100

    Tweak
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- homeassistant/__main__.py
+++ homeassistant/__main__.py
@@ -14,7 +14,7 @@ FAULT_LOG_FILENAME = "home-assistant.log.fault"
 
 def validate_os() -> None:
     """Validate that Home Assistant is running in a supported operating system."""
-    if not sys.platform.startswith("darwin") and not sys.platform.startswith("linux"):
+    if not sys.platform.startswith(("darwin", "linux")):
         print("Home Assistant only supports Linux, OSX and Windows using WSL")
         sys.exit(1)
 
