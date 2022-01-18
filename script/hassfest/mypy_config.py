7453638f7f82c6b3715bfd28eaa761b8b4fc46e2
epenet <epenet@users.noreply.github.com>
2022-01-18 07:11:22 +0000

    Remove glances from mypy ignored modules

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -34,7 +34,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.foscam.*",
     "homeassistant.components.freebox.*",
     "homeassistant.components.geniushub.*",
-    "homeassistant.components.glances.*",
     "homeassistant.components.google_assistant.*",
     "homeassistant.components.gree.*",
     "homeassistant.components.growatt_server.*",
