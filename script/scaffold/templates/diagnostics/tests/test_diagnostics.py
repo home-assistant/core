775ff1b3f2bad193dd34828905fb69f20c03a7df
Ludeeus <ludeeus@ludeeus.dev>
2022-01-18 08:24:29 +0000

    Use NEW_DOMAIN in case const does not exist

--- script/scaffold/templates/diagnostics/tests/test_diagnostics.py
+++ script/scaffold/templates/diagnostics/tests/test_diagnostics.py
@@ -1,5 +1,4 @@
 """Test the NEW_NAME diagnostics platform."""
-from homeassistant.components.NEW_DOMAIN.const import DOMAIN
 from homeassistant.components.NEW_DOMAIN.diagnostics import (
     async_get_config_entry_diagnostics,
 )
@@ -10,5 +9,5 @@ from tests.common import MockConfigEntry
 
 async def test_diagnostics(hass: HomeAssistant) -> None:
     """Test NEW_NAME diagnostics."""
-    config_entry = MockConfigEntry(domain=DOMAIN)
+    config_entry = MockConfigEntry(domain="NEW_DOMAIN")
     assert async_get_config_entry_diagnostics(hass, config_entry) == {}
