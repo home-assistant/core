e5d153bb9ea59a14d0adad5ae6b0b1089f58b858
Simon Hansen <67142049+DurgNomis-drol@users.noreply.github.com>
2022-01-18 20:58:36 +0100

    Add two new sensors to Launch Library (#64203)
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>

--- homeassistant/components/launch_library/const.py
+++ homeassistant/components/launch_library/const.py
@@ -2,11 +2,18 @@
 
 DOMAIN = "launch_library"
 
-ATTR_AGENCY = "agency"
-ATTR_AGENCY_COUNTRY_CODE = "agency_country_code"
-ATTR_LAUNCH_TIME = "launch_time"
-ATTR_STREAM = "stream"
+ATTR_LAUNCH_FACILITY = "facility"
+ATTR_STREAM_LIVE = "stream_live"
+ATTR_LAUNCH_PAD = "pad"
+ATTR_LAUNCH_PAD_COUNTRY_CODE = "provider_country_code"
+ATTR_LAUNCH_PROVIDER = "provider"
+ATTR_WINDOW_START = "window_start"
+ATTR_WINDOW_END = "window_end"
 
 ATTRIBUTION = "Data provided by Launch Library."
 
 DEFAULT_NAME = "Next launch"
+
+NEXT_LAUNCH = "next_launch"
+LAUNCH_TIME = "launch_time"
+LAUNCH_PROBABILITY = "launch_probability"
