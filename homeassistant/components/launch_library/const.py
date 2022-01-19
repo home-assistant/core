89214ce734d47ff72be58116998b85a9f26e5fb5
Joakim Sørensen <joasoe@gmail.com>
2022-01-19 09:52:22 +0100

    Clean up Launch Library consts (#64420)

--- homeassistant/components/launch_library/const.py
+++ homeassistant/components/launch_library/const.py
@@ -1,25 +1,3 @@
 """Constants for launch_library."""
 
 DOMAIN = "launch_library"
-
-ATTR_DESCRIPTION = "description"
-ATTR_LAUNCH_FACILITY = "facility"
-ATTR_LAUNCH_PAD = "pad"
-ATTR_LAUNCH_PAD_COUNTRY_CODE = "provider_country_code"
-ATTR_LAUNCH_PROVIDER = "provider"
-ATTR_ORBIT = "target_orbit"
-ATTR_REASON = "reason"
-ATTR_STREAM_LIVE = "stream_live"
-ATTR_TYPE = "mission_type"
-ATTR_WINDOW_END = "window_end"
-ATTR_WINDOW_START = "window_start"
-
-ATTRIBUTION = "Data provided by Launch Library."
-
-DEFAULT_NAME = "Next launch"
-
-NEXT_LAUNCH = "next_launch"
-LAUNCH_TIME = "launch_time"
-LAUNCH_PROBABILITY = "launch_probability"
-LAUNCH_STATUS = "launch_status"
-LAUNCH_MISSION = "launch_mission"
