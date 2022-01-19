ec43336859ffcb1448ad274bf14e947bfbdd2fc3
J. Nick Koston <nick@koston.org>
2022-01-19 08:55:26 -1000

    make ENTITY_CATEGORIES a set

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -752,11 +752,11 @@ CLOUD_NEVER_EXPOSED_ENTITIES: Final[list[str]] = ["group.all_locks"]
 ENTITY_CATEGORY_CONFIG: Final = "config"
 ENTITY_CATEGORY_DIAGNOSTIC: Final = "diagnostic"
 ENTITY_CATEGORY_SYSTEM: Final = "system"
-ENTITY_CATEGORIES: Final[list[str]] = [
+ENTITY_CATEGORIES: Final[set[str]] = {
     ENTITY_CATEGORY_CONFIG,
     ENTITY_CATEGORY_DIAGNOSTIC,
     ENTITY_CATEGORY_SYSTEM,
-]
+}
 
 # The ID of the Home Assistant Media Player Cast App
 CAST_APP_ID_HOMEASSISTANT_MEDIA: Final = "B45F4572"
