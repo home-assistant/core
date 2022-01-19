0758eed6dab19de5b3319dad61d365bd0f9837b9
Aaron Bach <bachya1208@gmail.com>
2022-01-19 14:32:52 -0700

    Perform some more AirVisual test cleanup (#64470)
    * Perform some more AirVisual test cleanup
    
    * Smarter startup patch
    
    * Simplify

--- tests/components/airvisual/conftest.py
+++ tests/components/airvisual/conftest.py
@@ -3,30 +3,50 @@ from unittest.mock import patch
 
 import pytest
 
-from homeassistant.components.airvisual.const import DOMAIN
-from homeassistant.const import CONF_SHOW_ON_MAP
+from homeassistant.components.airvisual.const import (
+    CONF_INTEGRATION_TYPE,
+    DOMAIN,
+    INTEGRATION_TYPE_GEOGRAPHY_COORDS,
+)
+from homeassistant.const import (
+    CONF_API_KEY,
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+    CONF_SHOW_ON_MAP,
+)
 from homeassistant.setup import async_setup_component
 
 from tests.common import MockConfigEntry
 
 
 @pytest.fixture(name="config_entry")
-def config_entry_fixture(hass, config, unique_id):
+def config_entry_fixture(hass, config, config_entry_version, unique_id):
     """Define a config entry fixture."""
     entry = MockConfigEntry(
         domain=DOMAIN,
         unique_id=unique_id,
-        data=config,
+        data={CONF_INTEGRATION_TYPE: INTEGRATION_TYPE_GEOGRAPHY_COORDS, **config},
         options={CONF_SHOW_ON_MAP: True},
+        version=config_entry_version,
     )
     entry.add_to_hass(hass)
     return entry
 
 
+@pytest.fixture(name="config_entry_version")
+def config_entry_version_fixture():
+    """Define a config entry version fixture."""
+    return 2
+
+
 @pytest.fixture(name="config")
 def config_fixture(hass):
     """Define a config entry data fixture."""
-    return {}
+    return {
+        CONF_API_KEY: "abcde12345",
+        CONF_LATITUDE: 51.528308,
+        CONF_LONGITUDE: -0.3817765,
+    }
 
 
 @pytest.fixture(name="setup_airvisual")
@@ -38,10 +58,10 @@ async def setup_airvisual_fixture(hass, config):
         "pyairvisual.node.NodeSamba.async_get_latest_measurements"
     ), patch(
         "pyairvisual.node.NodeSamba.async_disconnect"
-    ), patch.object(
-        hass.config_entries, "async_forward_entry_setup"
+    ), patch(
+        "homeassistant.components.airvisual.PLATFORMS", []
     ):
-        assert await async_setup_component(hass, DOMAIN, {DOMAIN: config})
+        assert await async_setup_component(hass, DOMAIN, config)
         await hass.async_block_till_done()
         yield
 
