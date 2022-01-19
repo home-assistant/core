f9c9ac605948b61f17038778d52812b137a16a63
Mick Vleeshouwer <mick@imick.nl>
2022-01-19 10:41:53 +0000

    Improve typing

--- homeassistant/components/overkiz/diagnostics.py
+++ homeassistant/components/overkiz/diagnostics.py
@@ -1,6 +1,8 @@
 """Provides diagnostics for Overkiz."""
 from __future__ import annotations
 
+from typing import Any
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
@@ -10,7 +12,7 @@ from .const import DOMAIN
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
-) -> list[dict]:
+) -> list[dict[str, Any]]:
     """Return diagnostics for a config entry."""
     data: HomeAssistantOverkizData = hass.data[DOMAIN][entry.entry_id]
     client = data.coordinator.client
