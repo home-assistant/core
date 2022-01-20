114cbc2c804c2cd8938432da9427ee224f6ff0d0
Franck Nijhof <git@frenck.dev>
2022-01-20 13:14:39 +0100

    Remove unused error

--- homeassistant/components/intellifire/config_flow.py
+++ homeassistant/components/intellifire/config_flow.py
@@ -11,7 +11,6 @@ from homeassistant import config_entries
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
-from homeassistant.exceptions import HomeAssistantError
 
 from .const import DOMAIN
 
@@ -62,7 +61,3 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
-
-
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
