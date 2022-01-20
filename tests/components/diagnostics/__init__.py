11d0dcf7ac4ddc2638f403ef0ee6b796ac5bbceb
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-01-20 05:49:24 -0500

    Add zwave_js device diagnostics (#64504)
    * Add zwave_js device diagnostics
    
    * Add diagnostics as a dependency in manifest
    
    * Add failure scenario test
    
    * fix device diagnostics helper and remove dependency
    
    * tweak

--- tests/components/diagnostics/__init__.py
+++ tests/components/diagnostics/__init__.py
@@ -18,6 +18,8 @@ async def get_diagnostics_for_config_entry(hass, hass_client, config_entry):
 
 async def get_diagnostics_for_device(hass, hass_client, config_entry, device):
     """Return the diagnostics for the specified device."""
+    assert await async_setup_component(hass, "diagnostics", {})
+
     client = await hass_client()
     response = await client.get(
         f"/api/diagnostics/config_entry/{config_entry.entry_id}/device/{device.id}"
