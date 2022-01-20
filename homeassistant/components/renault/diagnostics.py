8a812f998af2f8f80f2b8333337c930610584cef
epenet <epenet@users.noreply.github.com>
2022-01-20 09:28:09 +0000

    Add device diagnostics

--- homeassistant/components/renault/diagnostics.py
+++ homeassistant/components/renault/diagnostics.py
@@ -8,6 +8,7 @@ from homeassistant.components.diagnostics import REDACTED
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.device_registry import DeviceEntry
 
 from . import RenaultHub
 from .const import CONF_KAMEREON_ACCOUNT_ID, DOMAIN
@@ -49,8 +50,17 @@ async def async_get_config_entry_diagnostics(
             "title": entry.title,
             "data": _async_redact_data(entry.data),
         },
-        "vehicles": [
-            _async_redact_data(vehicle.details.raw_data)
-            for vehicle in renault_hub.vehicles.values()
-        ],
+        "vehicles": len(renault_hub.vehicles),
+    }
+
+
+async def async_get_device_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry, device: DeviceEntry
+) -> dict:
+    """Return diagnostics for a device."""
+    renault_hub: RenaultHub = hass.data[DOMAIN][entry.entry_id]
+    vin = next(iter(device.identifiers))[1]
+
+    return {
+        "details": _async_redact_data(renault_hub.vehicles[vin].details.raw_data),
     }
