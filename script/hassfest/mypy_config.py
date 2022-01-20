18ab9b8513c10226c177295c6e9eb449a222ce5f
epenet <epenet@users.noreply.github.com>
2022-01-20 06:37:12 +0000

    Adjust mypy_config

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -73,7 +73,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.screenlogic.*",
     "homeassistant.components.search.*",
     "homeassistant.components.sense.*",
-    "homeassistant.components.sharkiq.*",
     "homeassistant.components.sma.*",
     "homeassistant.components.smartthings.*",
     "homeassistant.components.solaredge.*",
