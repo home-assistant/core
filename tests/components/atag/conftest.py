f7c8e14fcd29107ae5895d548ac4d2ba29994a68
thecode <levyshay1@gmail.com>
2022-01-20 18:00:50 +0000

    Fix sleep fixture

--- tests/components/atag/conftest.py
+++ tests/components/atag/conftest.py
@@ -1,4 +1,5 @@
 """Provide common Atag fixtures."""
+import asyncio
 from unittest.mock import patch
 
 import pytest
@@ -7,5 +8,10 @@ import pytest
 @pytest.fixture(autouse=True)
 async def mock_pyatag_sleep():
     """Mock out pyatag sleeps."""
-    with patch("pyatag.gateway.asyncio.sleep"):
+    asyncio_sleep = asyncio.sleep
+
+    async def sleep(duration, loop=None):
+        await asyncio_sleep(0)
+
+    with patch("pyatag.gateway.asyncio.sleep", new=sleep):
         yield
