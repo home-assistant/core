7121377eb93133e1d20ce65aa5396f674a38052f
Erik <erik@montnemery.com>
2022-01-20 13:51:29 +0100

    Add test

--- tests/components/cloud/test_prefs.py
+++ tests/components/cloud/test_prefs.py
@@ -2,7 +2,10 @@
 from unittest.mock import patch
 
 from homeassistant.auth.const import GROUP_ID_ADMIN
+from homeassistant.components.cloud import const
 from homeassistant.components.cloud.prefs import STORAGE_KEY, CloudPreferences
+
+from tests.common import flush_store
 
 
 async def test_set_username(hass):
@@ -90,3 +93,66 @@ async def test_setup_remove_cloud_user(hass, hass_storage):
     assert cloud_user2
     assert cloud_user2.groups[0].id == GROUP_ID_ADMIN
     assert cloud_user2.id != cloud_user.id
+
+
+async def test_migration_1_1_to_1_2(hass, hass_storage):
+    """Test migration from version 1 to 1.2."""
+    hass_storage[STORAGE_KEY] = {
+        "version": 1,
+        "data": {
+            const.PREF_ENABLE_ALEXA: False,
+            const.PREF_ENABLE_GOOGLE: False,
+            const.PREF_ENABLE_REMOTE: True,
+            const.PREF_GOOGLE_LOCAL_WEBHOOK_ID: "abc",
+        },
+    }
+
+    prefs = CloudPreferences(hass)
+    await prefs.async_initialize()
+
+    # Test data was loaded and mmigrated
+    assert prefs._prefs == {
+        const.PREF_ALEXA_DEFAULT_EXPOSE: None,
+        const.PREF_ALEXA_ENTITY_CONFIGS: {},
+        const.PREF_ALEXA_REPORT_STATE: True,
+        const.PREF_CLOUD_USER: None,
+        const.PREF_CLOUDHOOKS: {},
+        const.PREF_ENABLE_ALEXA: False,
+        const.PREF_ENABLE_GOOGLE: False,
+        const.PREF_ENABLE_REMOTE: True,
+        const.PREF_GOOGLE_DEFAULT_EXPOSE: None,
+        const.PREF_GOOGLE_ENTITY_CONFIGS: {},
+        const.PREF_GOOGLE_LOCAL_WEBHOOK_ID: "abc",
+        const.PREF_GOOGLE_REPORT_STATE: True,
+        const.PREF_GOOGLE_SECURE_DEVICES_PIN: None,
+        const.PREF_TTS_DEFAULT_VOICE: None,
+        const.PREF_USERNAME: None,
+    }
+
+    # Update to trigger a store
+    await prefs.async_update(remote_enabled=True)
+
+    # Check we store migrated data
+    await flush_store(prefs._store)
+    assert hass_storage[STORAGE_KEY] == {
+        "version": 1,
+        "minor_version": 2,
+        "key": STORAGE_KEY,
+        "data": {
+            const.PREF_ALEXA_DEFAULT_EXPOSE: None,
+            const.PREF_ALEXA_ENTITY_CONFIGS: {},
+            const.PREF_ALEXA_REPORT_STATE: True,
+            const.PREF_CLOUD_USER: None,
+            const.PREF_CLOUDHOOKS: {},
+            const.PREF_ENABLE_ALEXA: False,
+            const.PREF_ENABLE_GOOGLE: False,
+            const.PREF_ENABLE_REMOTE: True,
+            const.PREF_GOOGLE_DEFAULT_EXPOSE: None,
+            const.PREF_GOOGLE_ENTITY_CONFIGS: {},
+            const.PREF_GOOGLE_LOCAL_WEBHOOK_ID: "abc",
+            const.PREF_GOOGLE_REPORT_STATE: True,
+            const.PREF_GOOGLE_SECURE_DEVICES_PIN: None,
+            const.PREF_TTS_DEFAULT_VOICE: None,
+            const.PREF_USERNAME: None,
+        },
+    }
