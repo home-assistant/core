6711bc4155de6b258345dae7ca6abcf7d3690825
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-01-20 01:32:12 -0500

    Add failure scenario test

--- tests/components/zwave_js/test_diagnostics.py
+++ tests/components/zwave_js/test_diagnostics.py
@@ -1,6 +1,9 @@
 """Test the Z-Wave JS diagnostics."""
 from unittest.mock import patch
 
+import pytest
+
+from homeassistant.components.zwave_js.diagnostics import async_get_device_diagnostics
 from homeassistant.components.zwave_js.helpers import get_device_id
 from homeassistant.helpers.device_registry import async_get
 
@@ -37,3 +40,13 @@ async def test_device_diagnostics(
         await get_diagnostics_for_device(hass, hass_client, integration, device)
         == aeon_smart_switch_6_state
     )
+
+
+async def test_device_diagnostics_error(hass, integration):
+    """Test the device diagnostics raises exception when an invalid device is used."""
+    dev_reg = async_get(hass)
+    device = dev_reg.async_get_or_create(
+        config_entry_id=integration.entry_id, identifiers={("test", "test")}
+    )
+    with pytest.raises(ValueError):
+        await async_get_device_diagnostics(hass, integration, device)
