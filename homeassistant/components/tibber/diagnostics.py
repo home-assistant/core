b1970c22f2d459e1cd124551061e4c38e16beb02
Daniel Hjelseth Høyer <mail@dahoiv.net>
2022-01-20 06:24:08 +0100

    Update homeassistant/components/tibber/diagnostics.py
    Co-authored-by: Franck Nijhof <git@frenck.dev>

--- homeassistant/components/tibber/diagnostics.py
+++ homeassistant/components/tibber/diagnostics.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import tibber
 
+from homeassistant.components.diagnostics import REDACTED
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ACCESS_TOKEN
 from homeassistant.core import HomeAssistant
