58b33c8c4d3c3bda1a815cdc447be0e5fa9bbea8
Brandon Rothweiler <brandonrothweiler@gmail.com>
2022-01-21 18:03:47 -0500

    Add device diagnostics

--- homeassistant/components/mazda/diagnostics.py
+++ homeassistant/components/mazda/diagnostics.py
@@ -5,6 +5,8 @@ from homeassistant.components.diagnostics.util import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers.device_registry import DeviceEntry
 
 from .const import DATA_COORDINATOR, DOMAIN
 
@@ -23,6 +25,31 @@ async def async_get_config_entry_diagnostics(
         "data": [
             async_redact_data(vehicle, TO_REDACT_DATA) for vehicle in coordinator.data
         ],
+    }
+
+    return diagnostics_data
+
+
+async def async_get_device_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry, device: DeviceEntry
+) -> dict:
+    """Return diagnostics for a device."""
+    coordinator = hass.data[DOMAIN][config_entry.entry_id][DATA_COORDINATOR]
+
+    vin = next(iter(device.identifiers))[1]
+
+    target_vehicle = None
+    for vehicle in coordinator.data:
+        if vehicle["vin"] == vin:
+            target_vehicle = vehicle
+            break
+
+    if target_vehicle is None:
+        raise HomeAssistantError("Vehicle not found")
+
+    diagnostics_data = {
+        "info": async_redact_data(config_entry.data, TO_REDACT_INFO),
+        "data": async_redact_data(target_vehicle, TO_REDACT_DATA),
     }
 
     return diagnostics_data
