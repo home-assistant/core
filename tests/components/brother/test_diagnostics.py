942d5989a54fc2ca1348ae015b1c58cf542e325c
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-01-21 16:26:37 +0100

    Fix tests

--- tests/components/brother/test_diagnostics.py
+++ tests/components/brother/test_diagnostics.py
@@ -25,7 +25,5 @@ async def test_entry_diagnostics(hass, hass_client):
 
     result = await get_diagnostics_for_config_entry(hass, hass_client, entry)
 
-    result["info"].pop("entry_id")
-
     assert result["info"] == {"host": "localhost", "type": "laser"}
     assert result["data"] == diagnostics_data
