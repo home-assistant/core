933e1b4040d0a997037720c52aed5b5fb458a8f4
Aaron Bach <bachya1208@gmail.com>
2022-01-20 19:48:05 -0700

    Tests

--- homeassistant/components/flunearyou/diagnostics.py
+++ homeassistant/components/flunearyou/diagnostics.py
@@ -1,48 +1,40 @@
 """Diagnostics support for Flu Near You."""
 from __future__ import annotations
 
-from types import MappingProxyType
 from typing import Any
 
-from homeassistant.components.diagnostics import REDACTED
+from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from .const import DOMAIN
+from .const import CATEGORY_CDC_REPORT, CATEGORY_USER_REPORT, DOMAIN
 
-CONF_COORDINATES = "coordinates"
-
-
-@callback
-def _async_redact_data(data: MappingProxyType | dict) -> dict[str, Any]:
-    """Redact sensitive data in a dict."""
-    redacted = {**data}
-
-    for key, value in redacted.items():
-        if key in (CONF_LATITUDE, CONF_LONGITUDE):
-            redacted[key] = REDACTED
-        elif isinstance(value, dict):
-            redacted[key] = _async_redact_data(value)
-
-    return redacted
+TO_REDACT = {
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+}
 
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
-    coordinators: DataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+    coordinators: dict[str, DataUpdateCoordinator] = hass.data[DOMAIN][entry.entry_id]
 
-    return {}
-    # return {
-    #     "entry": {
-    #         "title": entry.title,
-    #         "data": _async_redact_data(entry.data),
-    #     },
-    #     "data": {
-    #         api_category: _async_redact_data(coordinator.data)
-    #         for api_category, coordinator in coordinators.items()
-    #     },
-    # }
+    return {
+        "entry": {
+            "title": entry.title,
+            "data": async_redact_data(entry.data, TO_REDACT),
+        },
+        "data": {
+            CATEGORY_CDC_REPORT: async_redact_data(
+                coordinators[CATEGORY_CDC_REPORT].data, TO_REDACT
+            ),
+            CATEGORY_USER_REPORT: [
+                async_redact_data(report, TO_REDACT)
+                for report in coordinators[CATEGORY_USER_REPORT].data
+            ],
+        },
+    }
