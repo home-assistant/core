20a14b7eff28bb640341626a3e4e0387a2ca1396
Jeef <jeeftor@users.noreply.github.com>
2022-01-21 06:18:00 -0700

    Update homeassistant/components/intellifire/sensor.py
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/intellifire/sensor.py
+++ homeassistant/components/intellifire/sensor.py
@@ -30,9 +30,11 @@ from .const import DOMAIN
 class IntellifireSensorEntityDescriptionMixin:
     """Mixin for required keys."""
 
+    # Although sensors could have a variety of different return values,
+    # all the ones below are only returning ints
     value_fn: Callable[
         [IntellifirePollData], int
-    ]  # Although sensours could have a variety of different return values - all the ones below are only reutrning ints
+    ]
 
 
 @dataclass
