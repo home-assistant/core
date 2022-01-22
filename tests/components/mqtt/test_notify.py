9c83c043f3f373e36ca3aadddfd0739fc779d376
jbouwh <jan@jbsoft.nl>
2022-01-22 22:31:36 +0000

    complete tests

--- tests/components/mqtt/test_notify.py
+++ tests/components/mqtt/test_notify.py
@@ -12,9 +12,7 @@ from homeassistant.setup import async_setup_component
 
 from tests.common import async_fire_mqtt_message
 
-DEFAULT_CONFIG = {
-    notify.DOMAIN: {"platform": "mqtt", "target": "test", "command_topic": "test-topic"}
-}
+DEFAULT_CONFIG = {notify.DOMAIN: {"platform": "mqtt", "command_topic": "test-topic"}}
 
 
 async def test_sending_mqtt_commands(hass, mqtt_mock, caplog):
@@ -26,21 +24,17 @@ async def test_sending_mqtt_commands(hass, mqtt_mock, caplog):
             notify.DOMAIN: {
                 "command_topic": "command-topic",
                 "name": "test",
-                "target": "test",
                 "platform": "mqtt",
                 "qos": "2",
             }
         },
     )
     await hass.async_block_till_done()
-    assert (
-        "<Event service_registered[L]: domain=notify, service=mqtt_test>" in caplog.text
-    )
-    assert "<Event service_registered[L]: domain=notify, service=mqtt>" in caplog.text
+    assert "<Event service_registered[L]: domain=notify, service=test>" in caplog.text
 
     await hass.services.async_call(
         notify.DOMAIN,
-        f"{DOMAIN}_test",
+        "test",
         {notify.ATTR_TITLE: "Title", notify.ATTR_MESSAGE: "Message"},
         blocking=True,
     )
@@ -53,21 +47,19 @@ async def test_sending_mqtt_commands(hass, mqtt_mock, caplog):
 
 async def test_discovery(hass, mqtt_mock, caplog):
     """Test discovery, update and removal of notify service."""
-    data = '{ "target": "test", "command_topic": "test_topic" }'
-    data_update = (
-        '{ "target": "test", "command_topic": "test_topic_update", "name": "New name" }'
-    )
+    data = '{ "name": "Old name", "command_topic": "test_topic" }'
+    data_update = '{ "command_topic": "test_topic_update", "name": "New name" }'
 
     async_fire_mqtt_message(hass, f"homeassistant/{notify.DOMAIN}/bla/config", data)
     await hass.async_block_till_done()
 
     assert (
-        "<Event service_registered[L]: domain=notify, service=mqtt_test>" in caplog.text
+        "<Event service_registered[L]: domain=notify, service=old_name>" in caplog.text
     )
 
     await hass.services.async_call(
         notify.DOMAIN,
-        f"{DOMAIN}_test",
+        "old_name",
         {notify.ATTR_TITLE: "Title", notify.ATTR_MESSAGE: "Message"},
         blocking=True,
     )
@@ -80,20 +72,16 @@ async def test_discovery(hass, mqtt_mock, caplog):
     )
     await hass.async_block_till_done()
 
+    assert "<Event service_removed[L]: domain=notify, service=old_name>" in caplog.text
     assert (
-        "<Event service_registered[L]: domain=notify, service=mqtt_new_name>"
-        in caplog.text
+        "<Event service_registered[L]: domain=notify, service=new_name>" in caplog.text
     )
-    assert "<Event service_removed[L]: domain=notify, service=mqtt_test>" in caplog.text
 
-    assert (
-        "Notify service ('notify', 'bla') for target test has been updated"
-        in caplog.text
-    )
+    assert "Notify service ('notify', 'bla') has been updated" in caplog.text
 
     await hass.services.async_call(
         notify.DOMAIN,
-        f"{DOMAIN}_new_name",
+        "new_name",
         {notify.ATTR_TITLE: "Title", notify.ATTR_MESSAGE: "Message"},
         blocking=True,
     )
@@ -106,11 +94,12 @@ async def test_discovery(hass, mqtt_mock, caplog):
     async_fire_mqtt_message(hass, f"homeassistant/{notify.DOMAIN}/bla/config", "")
     await hass.async_block_till_done()
 
-    assert "<Event service_removed[L]: domain=notify, service=mqtt_test>" in caplog.text
+    assert "<Event service_removed[L]: domain=notify, service=new_name>" in caplog.text
 
 
 async def test_publishing_with_custom_encoding(hass, mqtt_mock, caplog):
-    """Test publishing MQTT payload with different encoding."""
+    """Test publishing MQTT payload with different encoding via discovery and configuration."""
+    # test with default encoding using configuration setup
     assert await async_setup_component(
         hass,
         notify.DOMAIN,
@@ -118,7 +107,6 @@ async def test_publishing_with_custom_encoding(hass, mqtt_mock, caplog):
             notify.DOMAIN: {
                 "command_topic": "command-topic",
                 "name": "test",
-                "target": "test",
                 "platform": "mqtt",
                 "qos": "2",
             }
@@ -126,39 +114,47 @@ async def test_publishing_with_custom_encoding(hass, mqtt_mock, caplog):
     )
     await hass.async_block_till_done()
 
-    data = '{"target": "test", "command_topic": "test_topic", "command_template": "{{ pack(int(message), \'b\') }}" }'
+    # test with raw encoding and discovery
+    data = '{"name": "test2", "command_topic": "test_topic2", "command_template": "{{ pack(int(message), \'b\') }}" }'
     async_fire_mqtt_message(hass, f"homeassistant/{notify.DOMAIN}/bla/config", data)
     await hass.async_block_till_done()
 
-    assert (
-        "<Event service_registered[L]: domain=notify, service=mqtt_test>" in caplog.text
-    )
+    assert "Notify service ('notify', 'bla') has been initialized" in caplog.text
+    assert "<Event service_registered[L]: domain=notify, service=test2>" in caplog.text
 
-    # test raw payload
     await hass.services.async_call(
         notify.DOMAIN,
-        f"{DOMAIN}_test",
+        "test2",
         {notify.ATTR_TITLE: "Title", notify.ATTR_MESSAGE: "4"},
         blocking=True,
     )
-    mqtt_mock.async_publish.assert_called_once_with("test_topic", b"\x04", 0, False)
+    mqtt_mock.async_publish.assert_called_once_with("test_topic2", b"\x04", 0, False)
     mqtt_mock.async_publish.reset_mock()
 
-    # test with utf-16
-    data = '{"encoding":"utf-16", "target": "test", "command_topic": "test_topic", "command_template": "{{ message }}" }'
+    # test with utf-16 and update discovery
+    data = '{"encoding":"utf-16", "name": "test3", "command_topic": "test_topic3", "command_template": "{{ message }}" }'
     async_fire_mqtt_message(hass, f"homeassistant/{notify.DOMAIN}/bla/config", data)
     await hass.async_block_till_done()
+    assert (
+        "Component has already been discovered: notify bla, sending update"
+        in caplog.text
+    )
 
     await hass.services.async_call(
         notify.DOMAIN,
-        f"{DOMAIN}_test",
+        "test3",
         {notify.ATTR_TITLE: "Title", notify.ATTR_MESSAGE: "Message"},
         blocking=True,
     )
     mqtt_mock.async_publish.assert_called_once_with(
-        "test_topic", "Message".encode("utf-16"), 0, False
+        "test_topic3", "Message".encode("utf-16"), 0, False
     )
     mqtt_mock.async_publish.reset_mock()
+
+    async_fire_mqtt_message(hass, f"homeassistant/{notify.DOMAIN}/bla/config", "")
+    await hass.async_block_till_done()
+
+    assert "Notify service ('notify', 'bla') has been removed" in caplog.text
 
 
 async def test_reloadable(hass, mqtt_mock, caplog, tmp_path):
@@ -168,35 +164,42 @@ async def test_reloadable(hass, mqtt_mock, caplog, tmp_path):
 
     # Create and test an old config of 2 entities based on the config supplied
     old_config_1 = copy.deepcopy(config)
-    old_config_1["name"] = "test_old_1"
-    old_config_1["target"] = "test_old_1"
+    old_config_1["name"] = "Test old 1"
     old_config_2 = copy.deepcopy(config)
-    old_config_2["name"] = "test_old_2"
-    old_config_2["target"] = "test_old_2"
+    old_config_2["name"] = "Test old 2"
 
-    assert await async_setup_component(hass, domain, {domain: old_config_1})
+    assert await async_setup_component(
+        hass, domain, {domain: [old_config_1, old_config_2]}
+    )
     await hass.async_block_till_done()
     assert (
-        "<Event service_registered[L]: domain=notify, service=mqtt_test_old_1>"
+        "<Event service_registered[L]: domain=notify, service=test_old_1>"
         in caplog.text
     )
-    assert await async_setup_component(hass, domain, {domain: old_config_2})
     assert (
-        "<Event service_registered[L]: domain=notify, service=mqtt_test_old_2>"
+        "<Event service_registered[L]: domain=notify, service=test_old_2>"
         in caplog.text
     )
     caplog.clear()
 
+    # Add an auto discovered notify target
+    data = '{"name": "Test old 3", "command_topic": "test_topic_discovery" }'
+    async_fire_mqtt_message(hass, f"homeassistant/{notify.DOMAIN}/bla/config", data)
+    await hass.async_block_till_done()
+
+    assert "Notify service ('notify', 'bla') has been initialized" in caplog.text
+    assert (
+        "<Event service_registered[L]: domain=notify, service=test_old_3>"
+        in caplog.text
+    )
+
     # Create temporary fixture for configuration.yaml based on the supplied config and test a reload with this new config
     new_config_1 = copy.deepcopy(config)
-    new_config_1["name"] = "test_new_1"
-    new_config_1["target"] = "test_new_1"
+    new_config_1["name"] = "Test new 1"
     new_config_2 = copy.deepcopy(config)
-    new_config_2["name"] = "test_new_2"
-    new_config_2["target"] = "test_new_2"
+    new_config_2["name"] = "test new 2"
     new_config_3 = copy.deepcopy(config)
-    new_config_3["name"] = "test_new_3"
-    new_config_3["target"] = "test_new_3"
+    new_config_3["name"] = "test new 3"
     new_yaml_config_file = tmp_path / "configuration.yaml"
     new_yaml_config = yaml.dump({domain: [new_config_1, new_config_2, new_config_3]})
     new_yaml_config_file.write_text(new_yaml_config)
@@ -204,27 +207,49 @@ async def test_reloadable(hass, mqtt_mock, caplog, tmp_path):
 
     with patch.object(hass_config, "YAML_CONFIG_FILE", new_yaml_config_file):
         await hass.services.async_call(
-            "mqtt",
+            DOMAIN,
             SERVICE_RELOAD,
             {},
             blocking=True,
         )
         await hass.async_block_till_done()
 
+    assert (
+        "<Event service_removed[L]: domain=notify, service=test_old_1>" in caplog.text
+    )
+    assert (
+        "<Event service_removed[L]: domain=notify, service=test_old_2>" in caplog.text
+    )
+
+    assert (
+        "<Event service_registered[L]: domain=notify, service=test_new_1>"
+        in caplog.text
+    )
+    assert (
+        "<Event service_registered[L]: domain=notify, service=test_new_2>"
+        in caplog.text
+    )
+    assert (
+        "<Event service_registered[L]: domain=notify, service=test_new_3>"
+        in caplog.text
+    )
     assert "<Event event_mqtt_reloaded[L]>" in caplog.text
-
-    assert len(hass.states.async_all(domain)) == 3
-
-    assert (
-        "<Event service_registered[L]: domain=notify, service=mqtt_test_new_1>"
-        in caplog.text
-    )
-    assert (
-        "<Event service_registered[L]: domain=notify, service=mqtt_test_new_2>"
-        in caplog.text
-    )
-    assert (
-        "<Event service_registered[L]: domain=notify, service=mqtt_test_new_3>"
-        in caplog.text
-    )
     caplog.clear()
+
+    # test if the auto discovered item survived the platform reload
+    await hass.services.async_call(
+        notify.DOMAIN,
+        "test_old_3",
+        {notify.ATTR_TITLE: "Title", notify.ATTR_MESSAGE: "Message"},
+        blocking=True,
+    )
+    mqtt_mock.async_publish.assert_called_once_with(
+        "test_topic_discovery", "Message", 0, False
+    )
+
+    mqtt_mock.async_publish.reset_mock()
+
+    async_fire_mqtt_message(hass, f"homeassistant/{notify.DOMAIN}/bla/config", "")
+    await hass.async_block_till_done()
+
+    assert "Notify service ('notify', 'bla') has been removed" in caplog.text
