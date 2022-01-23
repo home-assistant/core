6af69fdb4e3fd6b0157d3b213949bd66d1c97e4d
Michael <35783820+mib1185@users.noreply.github.com>
2022-01-23 22:47:12 +0100

    Apply suggestions from code review

--- tests/components/axis/test_diagnostics.py
+++ tests/components/axis/test_diagnostics.py
@@ -30,8 +30,8 @@ async def test_entry_diagnostics(hass, hass_client):
             "title": "Mock Title",
             "data": {
                 "host": "1.2.3.4",
-                "username": "**REDACTED**",
-                "password": "**REDACTED**",
+                "username": REDACTED,
+                "password": REDACTED,
                 "port": 80,
                 "model": "model",
                 "name": "name",
@@ -40,7 +40,7 @@ async def test_entry_diagnostics(hass, hass_client):
             "pref_disable_new_entities": False,
             "pref_disable_polling": False,
             "source": "user",
-            "unique_id": "**REDACTED**",
+            "unique_id": REDACTED,
             "disabled_by": None,
         },
         "api_discovery": [
