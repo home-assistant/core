7fcd76524b46c75ceeeebe373fd669df238e3b9b
alexanv1 <44785744+alexanv1@users.noreply.github.com>
2022-01-22 21:48:28 -0800

    Tuya: Add support for turning smart heater LEDs on and off

--- homeassistant/components/tuya/switch.py
+++ homeassistant/components/tuya/switch.py
@@ -299,6 +299,11 @@ SWITCHES: dict[str, tuple[SwitchEntityDescription, ...]] = {
             icon="mdi:account-lock",
             entity_category=EntityCategory.CONFIG,
         ),
+        SwitchEntityDescription(
+            key=DPCode.LIGHT,
+            name="Light",
+            entity_category=EntityCategory.CONFIG,
+        ),
     ),
     # Robot Vacuum
     # https://developer.tuya.com/en/docs/iot/fsd?id=K9gf487ck1tlo
