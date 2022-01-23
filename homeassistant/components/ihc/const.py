1d18e59509e9ca6d0ac4b277835367e95004508c
Jens Østergaard Nielsen <jensoen@gmail.com>
2022-01-23 05:33:10 +0100

    Make service functions async (#64705)

--- homeassistant/components/ihc/const.py
+++ homeassistant/components/ihc/const.py
@@ -1,5 +1,9 @@
 """IHC component constants."""
 
+ATTR_IHC_ID = "ihc_id"
+ATTR_VALUE = "value"
+ATTR_CONTROLLER_ID = "controller_id"
+
 CONF_AUTOSETUP = "auto_setup"
 CONF_BINARY_SENSOR = "binary_sensor"
 CONF_DIMMABLE = "dimmable"
@@ -15,9 +19,9 @@ CONF_SENSOR = "sensor"
 CONF_SWITCH = "switch"
 CONF_XPATH = "xpath"
 
-ATTR_IHC_ID = "ihc_id"
-ATTR_VALUE = "value"
-ATTR_CONTROLLER_ID = "controller_id"
+DOMAIN = "ihc"
+
+IHC_CONTROLLER = "controller"
 
 SERVICE_SET_RUNTIME_VALUE_BOOL = "set_runtime_value_bool"
 SERVICE_SET_RUNTIME_VALUE_FLOAT = "set_runtime_value_float"
