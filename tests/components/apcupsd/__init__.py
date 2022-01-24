8327b150735cef316064938be22447e967bd74fe
Yuxin Wang <yuxinwang.dev@gmail.com>
2022-01-23 19:18:47 -0500

    Add tests for config flows.

--- tests/components/apcupsd/__init__.py
+++ tests/components/apcupsd/__init__.py
@@ -1,1 +1,62 @@
 """Tests for the APCUPSd component."""
+from collections import OrderedDict
+from typing import Final
+
+MOCK_STATUS: Final = OrderedDict(
+    [
+        ("APC", "001,038,0985"),
+        ("DATE", "1970-01-01 00:00:00 0000"),
+        ("VERSION", "3.14.14 (31 May 2016) unknown"),
+        ("CABLE", "USB Cable"),
+        ("DRIVER", "USB UPS Driver"),
+        ("UPSMODE", "Stand Alone"),
+        ("MODEL", "Back-UPS ES 600"),
+        ("STATUS", "ONLINE"),
+        ("LINEV", "124.0 Volts"),
+        ("LOADPCT", "14.0 Percent"),
+        ("BCHARGE", "100.0 Percent"),
+        ("TIMELEFT", "51.0 Minutes"),
+        ("MBATTCHG", "5 Percent"),
+        ("MINTIMEL", "3 Minutes"),
+        ("MAXTIME", "0 Seconds"),
+        ("SENSE", "Medium"),
+        ("LOTRANS", "92.0 Volts"),
+        ("HITRANS", "139.0 Volts"),
+        ("ALARMDEL", "30 Seconds"),
+        ("BATTV", "13.7 Volts"),
+        ("LASTXFER", "Automatic or explicit self test"),
+        ("NUMXFERS", "1"),
+        ("XONBATT", "1970-01-01 00:00:00 0000"),
+        ("TONBATT", "0 Seconds"),
+        ("CUMONBATT", "8 Seconds"),
+        ("XOFFBATT", "1970-01-01 00:00:00 0000"),
+        ("LASTSTEST", "1970-01-01 00:00:00 0000"),
+        ("SELFTEST", "NO"),
+        ("STATFLAG", "0x05000008"),
+        ("SERIALNO", "XXXXXXXXXXXX"),
+        ("BATTDATE", "1970-01-01"),
+        ("NOMINV", "120 Volts"),
+        ("NOMBATTV", "12.0 Volts"),
+        ("NOMPOWER", "330 Watts"),
+        ("FIRMWARE", "928.a8 .D USB FW:a8"),
+        ("END APC", "1970-01-01 00:00:00 0000"),
+    ]
+)
+
+# Minimal status taken from http://www.apcupsd.org/manual/manual.html.
+MOCK_MINIMAL_STATUS: Final = OrderedDict(
+    [
+        ("APC", "001,012,0319"),
+        ("DATE", "1970-01-01 00:00:00 0000"),
+        ("RELEASE", "3.8.5"),
+        ("UPSNAME", "UPS_IDEN"),
+        ("CABLE", "APC Cable 940-0128A"),
+        ("MODEL", "BackUPS"),
+        ("UPSMODE", "Stand Alone"),
+        ("STARTTIME", "1970-01-01 00:00:00 0000"),
+        ("LINEFAIL", "OK"),
+        ("BATTSTAT", "OK"),
+        ("STATFLAG", "0x008"),
+        ("END APC", "1970-01-01 00:00:00 0000"),
+    ]
+)
--- tests/components/apcupsd/test_config_flow.py
+++ tests/components/apcupsd/test_config_flow.py
@@ -1,78 +1,154 @@
 """Test APCUPSd setup process."""
+from copy import copy
+from unittest.mock import patch
 
-from collections import OrderedDict
-from typing import Final
+import pytest
 
 from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.apcupsd import DOMAIN
-from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_RESOURCES
+from homeassistant.core import HomeAssistant
 
-MOCK_DATA: Final = {
-    CONF_HOST: "localhost",
-    CONF_PORT: 3551,
-}
+from . import MOCK_MINIMAL_STATUS, MOCK_STATUS
 
-MOCK_STATUS: Final = OrderedDict(
-    [
-        ("APC", "001,038,0985"),
-        ("DATE", "2022-01-22 23:07:19 -0500"),
-        ("VERSION", "3.14.14 (31 May 2016) unknown"),
-        ("CABLE", "USB Cable"),
-        ("DRIVER", "USB UPS Driver"),
-        ("UPSMODE", "Stand Alone"),
-        ("MODEL", "Back-UPS ES 600"),
-        ("STATUS", "ONLINE"),
-        ("LINEV", "124.0 Volts"),
-        ("LOADPCT", "14.0 Percent"),
-        ("BCHARGE", "100.0 Percent"),
-        ("TIMELEFT", "51.0 Minutes"),
-        ("MBATTCHG", "5 Percent"),
-        ("MINTIMEL", "3 Minutes"),
-        ("MAXTIME", "0 Seconds"),
-        ("SENSE", "Medium"),
-        ("LOTRANS", "92.0 Volts"),
-        ("HITRANS", "139.0 Volts"),
-        ("ALARMDEL", "30 Seconds"),
-        ("BATTV", "13.7 Volts"),
-        ("LASTXFER", "Automatic or explicit self test"),
-        ("NUMXFERS", "1"),
-        ("XONBATT", "2022-01-22 17:12:34 -0500"),
-        ("TONBATT", "0 Seconds"),
-        ("CUMONBATT", "8 Seconds"),
-        ("XOFFBATT", "2022-01-22 17:12:42 -0500"),
-        ("LASTSTEST", "2022-01-22 17:12:34 -0500"),
-        ("SELFTEST", "NO"),
-        ("STATFLAG", "0x05000008"),
-        ("SERIALNO", "XXXXXXXXXXXX"),
-        ("BATTDATE", "1970-01-01"),
-        ("NOMINV", "120 Volts"),
-        ("NOMBATTV", "12.0 Volts"),
-        ("NOMPOWER", "330 Watts"),
-        ("FIRMWARE", "928.a8 .D USB FW:a8"),
-        ("END APC", "2022-01-22 23:08:16 -050"),
-    ]
-)
+from tests.common import MockConfigEntry
 
 
-async def test_flow_works(hass):
-    """Test config flow."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+@pytest.fixture(name="config_entry")
+def config_entry_fixture():
+    """Create hass config_entry fixture."""
+    return MockConfigEntry(
+        version=1,
+        domain=DOMAIN,
+        title=MOCK_STATUS["MODEL"],
+        data={CONF_HOST: "test", CONF_PORT: 1234},
+        options={},
+        unique_id=MOCK_STATUS["SERIALNO"],
+        source=config_entries.SOURCE_USER,
+        entry_id="1",
     )
 
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "user"
-    assert result["data_schema"]({CONF_HOST: "", CONF_PORT: ""}) == {
-        CONF_HOST: "localhost",
-        CONF_PORT: 3551,
-    }
 
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        user_input={CONF_HOST: "localhost", CONF_PORT: 3551},
-    )
+async def test_flow_works(hass: HomeAssistant, config_entry: MockConfigEntry):
+    """Test we get the form."""
+    with patch("apcaccess.status.parse") as mock_parse, patch(
+        "apcaccess.status.get"
+    ) as mock_get:
+        mock_get.return_value = b""
 
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert "UPS" in result["title"]
-    assert result["description"] == "APCUPSd"
-    assert result["data"] == {CONF_HOST: "localhost", CONF_PORT: 3551}
+        for status in (MOCK_STATUS, MOCK_MINIMAL_STATUS):
+            mock_parse.return_value = status
+
+            result = await hass.config_entries.flow.async_init(
+                DOMAIN,
+                context={"source": config_entries.SOURCE_USER},
+                data=config_entry.data,
+            )
+            await hass.async_block_till_done()
+            assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+            assert result["title"] == status["MODEL"]
+            assert result["data"][CONF_HOST] == config_entry.data[CONF_HOST]
+            assert result["data"][CONF_PORT] == config_entry.data[CONF_PORT]
+            assert result["description"] == "APCUPSd"
+
+
+async def test_config_flow_cannot_connect(hass: HomeAssistant):
+    """Test config flow setup with connection error."""
+    with patch("apcaccess.status.get") as mock_get:
+        mock_get.side_effect = OSError()
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
+        result = await hass.config_entries.flow.async_configure(result["flow_id"], {})
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["errors"]["base"] == "cannot_connect"
+
+
+async def test_config_flow_no_status(hass: HomeAssistant):
+    """Test config flow setup with successful connection but no status is reported."""
+    with patch("apcaccess.status.parse") as mock_parse, patch(
+        "apcaccess.status.get"
+    ) as mock_get:
+        mock_get.return_value = b""
+        mock_parse.return_value = None
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
+        result = await hass.config_entries.flow.async_configure(result["flow_id"], {})
+        assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+        assert result["reason"] == "no_status"
+
+
+async def test_config_flow_duplicate(
+    hass: HomeAssistant, config_entry: MockConfigEntry
+):
+    """Test config flow setup with duplicate integration."""
+    # Add a config entry.
+    config_entry.add_to_hass(hass)
+
+    # Add a duplicate integration.
+    with patch("apcaccess.status.parse") as mock_parse, patch(
+        "apcaccess.status.get"
+    ) as mock_get:
+        mock_get.return_value = b""
+        mock_parse.return_value = MOCK_STATUS
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+            data=config_entry.data,
+        )
+        assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+        assert result["reason"] == "already_configured"
+
+        # Now we change the serial number and add it again.
+        another_device_status = copy(MOCK_STATUS)
+        another_device_status["SERIALNO"] = "ZZZZZZZZZ"
+        mock_parse.return_value = another_device_status
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+            data=config_entry.data,
+        )
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result["data"] == config_entry.data
+        assert result["title"] == MOCK_STATUS["MODEL"]
+        assert result["description"] == "APCUPSd"
+
+
+async def test_options_flow(hass: HomeAssistant, config_entry: MockConfigEntry):
+    """Test config flow options."""
+    with patch("apcaccess.status.parse") as mock_parse, patch(
+        "apcaccess.status.get"
+    ) as mock_get:
+        mock_get.return_value = b""
+        mock_parse.return_value = MOCK_STATUS
+        config_entry.add_to_hass(hass)
+        await config_entry.async_setup(hass)
+        await hass.async_block_till_done()
+
+        result = await hass.config_entries.options.async_init(config_entry.entry_id)
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert CONF_RESOURCES in result["data_schema"]({})
+        resources = result["data_schema"]({})[CONF_RESOURCES]
+
+        # The underlying library apcaccess would report undocumented extra fields that are not supported by HA
+        # for some model, so the available resources are actually a subset of the reported MOCK_STATUS.
+        assert all(resource in MOCK_STATUS for resource in resources)
+
+        # Now deselect one resource and submit.
+        resources.remove("LOADPCT")
+        result = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={CONF_RESOURCES: resources},
+        )
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert (
+            hass.config_entries.async_get_entry(config_entry.entry_id).options[
+                CONF_RESOURCES
+            ]
+            == resources
+        )
