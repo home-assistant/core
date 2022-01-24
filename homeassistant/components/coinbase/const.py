40c8c76d784044237341ab6c355791283a2cb1ac
Tom Brien <tom@byth.co.uk>
2022-01-24 21:28:20 +0000

    Add diagnostics to Coinbase

--- homeassistant/components/coinbase/const.py
+++ homeassistant/components/coinbase/const.py
@@ -18,6 +18,7 @@ API_ACCOUNT_NATIVE_BALANCE = "native_balance"
 API_ACCOUNT_NAME = "name"
 API_ACCOUNTS_DATA = "data"
 API_RATES = "rates"
+API_RESOURCE_PATH = "resource_path"
 API_RESOURCE_TYPE = "type"
 API_TYPE_VAULT = "vault"
 
---
+++ homeassistant/components/coinbase/diagnostics.py
@@ -0,0 +1,43 @@
+"""Diagnostics support for Coinbase."""
+from __future__ import annotations
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_API_KEY, CONF_API_TOKEN, CONF_ID
+from homeassistant.core import HomeAssistant
+
+from . import CoinbaseData
+from .const import (
+    API_ACCOUNT_AMOUNT,
+    API_ACCOUNT_NATIVE_BALANCE,
+    API_RESOURCE_PATH,
+    DOMAIN,
+)
+
+TO_REDACT = {
+    API_ACCOUNT_NATIVE_BALANCE,
+    API_ACCOUNT_AMOUNT,
+    API_RESOURCE_PATH,
+    CONF_API_KEY,
+    CONF_API_TOKEN,
+    CONF_ID,
+}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict:
+    """Return diagnostics for a config entry."""
+    instance: CoinbaseData = hass.data[DOMAIN][entry.entry_id]
+
+    diag_data = {
+        "entry": async_redact_data(entry.as_dict(), TO_REDACT),
+        "data": {},
+    }
+    if not isinstance(instance.accounts, list):
+        return diag_data
+
+    diag_data["accounts"] = [
+        async_redact_data(account, TO_REDACT) for account in instance.accounts
+    ]
+    return diag_data
