48fde9ce78221910f0817d118a8af29438e68fa8
Aaron Bach <bachya1208@gmail.com>
2022-01-23 21:31:32 -0700

    Add diagnostics to ReCollect Waste

---
+++ tests/components/recollect_waste/test_diagnostics.py
@@ -0,0 +1,27 @@
+"""Test ReCollect Waste diagnostics."""
+from tests.components.diagnostics import get_diagnostics_for_config_entry
+
+
+async def test_entry_diagnostics(
+    hass, config_entry, hass_client, setup_recollect_waste
+):
+    """Test config entry diagnostics."""
+    assert await get_diagnostics_for_config_entry(hass, hass_client, config_entry) == {
+        "entry": {
+            "title": "Mock Title",
+            "data": {"place_id": "12345", "service_id": "12345"},
+            "options": {},
+        },
+        "data": [
+            {
+                "date": {
+                    "__type": "<class 'datetime.date'>",
+                    "isoformat": "2022-01-23",
+                },
+                "pickup_types": [
+                    {"name": "garbage", "friendly_name": "Trash Collection"}
+                ],
+                "area_name": "The Sun",
+            }
+        ],
+    }
