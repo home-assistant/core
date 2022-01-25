9b8fe4f4db6a02d1807156b8ece679a3b32fcdb9
dougiteixeira <31328123+dougiteixeira@users.noreply.github.com>
2022-01-25 04:29:13 -0300

    Changing the Return to home in Tuya vacuum (#64810)
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/tuya/vacuum.py
+++ homeassistant/components/tuya/vacuum.py
@@ -33,6 +33,7 @@ from . import HomeAssistantTuyaData
 from .base import EnumTypeData, IntegerTypeData, TuyaEntity
 from .const import DOMAIN, TUYA_DISCOVERY_NEW, DPCode, DPType
 
+TUYA_MODE_RETURN_HOME = "chargego"
 TUYA_STATUS_TO_HA = {
     "charge_done": STATE_DOCKED,
     "chargecompleted": STATE_DOCKED,
@@ -95,6 +96,12 @@ class TuyaVacuumEntity(TuyaEntity, StateVacuumEntity):
             self._supported_features |= SUPPORT_PAUSE
 
         if self.find_dpcode(DPCode.SWITCH_CHARGE, prefer_function=True):
+            self._supported_features |= SUPPORT_RETURN_HOME
+        elif (
+            enum_type := self.find_dpcode(
+                DPCode.MODE, dptype=DPType.ENUM, prefer_function=True
+            )
+        ) and TUYA_MODE_RETURN_HOME in enum_type.range:
             self._supported_features |= SUPPORT_RETURN_HOME
 
         if self.find_dpcode(DPCode.SEEK, prefer_function=True):
@@ -178,7 +185,12 @@ class TuyaVacuumEntity(TuyaEntity, StateVacuumEntity):
 
     def return_to_base(self, **kwargs: Any) -> None:
         """Return device to dock."""
-        self._send_command([{"code": DPCode.MODE, "value": "chargego"}])
+        self._send_command(
+            [
+                {"code": DPCode.SWITCH_CHARGE, "value": True},
+                {"code": DPCode.MODE, "value": TUYA_MODE_RETURN_HOME},
+            ]
+        )
 
     def locate(self, **kwargs: Any) -> None:
         """Locate the device."""
