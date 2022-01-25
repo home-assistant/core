f272a08cd034172579903029090a937d96738b15
Tom Brien <tom@byth.co.uk>
2022-01-25 08:53:18 +0000

    Tidy up typing

--- homeassistant/components/coinbase/diagnostics.py
+++ homeassistant/components/coinbase/diagnostics.py
@@ -1,6 +1,8 @@
 """Diagnostics support for Coinbase."""
 from __future__ import annotations
 
+from typing import Any
+
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_API_KEY, CONF_API_TOKEN, CONF_ID
@@ -24,14 +26,14 @@ async def async_get_config_entry_diagnostics(
     """Return diagnostics for a config entry."""
     instance: CoinbaseData = hass.data[DOMAIN][entry.entry_id]
 
-    diag_data = {
+    diag_data: dict[str, Any] = {
         "entry": async_redact_data(entry.as_dict(), TO_REDACT),
-        "accounts": [{}],
+        "data": [],
     }
     if not isinstance(instance.accounts, list):
         return diag_data
 
-    diag_data["accounts"] = [
+    diag_data["data"] = [
         async_redact_data(account, TO_REDACT) for account in instance.accounts
     ]
     return diag_data
