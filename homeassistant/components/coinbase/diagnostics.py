8d79d15f94629475d80cbddc9084ba3d697b87e7
Tom Brien <tom@byth.co.uk>
2022-01-25 09:45:03 +0000

    Fix (auto) import from wrong place

--- homeassistant/components/coinbase/diagnostics.py
+++ homeassistant/components/coinbase/diagnostics.py
@@ -2,14 +2,13 @@
 
 from typing import Any
 
-from homeassistant.components.aftership.const import CONF_TITLE
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_API_KEY, CONF_API_TOKEN, CONF_ID
 from homeassistant.core import HomeAssistant
 
 from . import CoinbaseData
-from .const import API_ACCOUNT_AMOUNT, API_RESOURCE_PATH, DOMAIN
+from .const import API_ACCOUNT_AMOUNT, API_RESOURCE_PATH, CONF_TITLE, DOMAIN
 
 TO_REDACT = {
     API_ACCOUNT_AMOUNT,
