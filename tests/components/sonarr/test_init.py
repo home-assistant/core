c5f7bebb8b00b3dba551000929702562343612d2
Chris Talkington <chris@talkingtontech.com>
2022-01-25 01:33:26 -0600

    Update test_init.py

--- tests/components/sonarr/test_init.py
+++ tests/components/sonarr/test_init.py
@@ -69,11 +69,11 @@ async def test_unload_config_entry(
         await hass.async_block_till_done()
 
     assert hass.data[DOMAIN]
-    assert entry.state is ConfigEntryState.LOADED
-    assert entry.entry_id in hass.data[DOMAIN]
+    assert mock_config_entry.state is ConfigEntryState.LOADED
+    assert mock_config_entry.entry_id in hass.data[DOMAIN]
 
-    await hass.config_entries.async_unload(entry.entry_id)
+    await hass.config_entries.async_unload(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
-    assert entry.state is ConfigEntryState.NOT_LOADED
-    assert entry.entry_id not in hass.data[DOMAIN]
+    assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
+    assert mock_config_entry.entry_id not in hass.data[DOMAIN]
