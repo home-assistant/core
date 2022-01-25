6fcd9fb589cc01cb08cea7691f3934efbebf257e
chemelli74 <simone.chemelli@gmail.com>
2022-01-25 09:23:46 +0000

    Apply review comments

--- homeassistant/components/uptimerobot/sensor.py
+++ homeassistant/components/uptimerobot/sensor.py
@@ -1,29 +1,42 @@
 """UptimeRobot sensor platform."""
 from __future__ import annotations
 
+from typing import TypedDict
+
 from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
+from . import UptimeRobotDataUpdateCoordinator
 from .const import DOMAIN
 from .entity import UptimeRobotEntity
 
+
+class StatusValue(TypedDict):
+    """Sensor details."""
+
+    value: str
+    icon: str
+
+
 SENSORS_INFO = {
-    "0": {"state": "Pause", "icon": "mdi:television-pause"},
-    "1": {"state": "Not checked yet", "icon": "mdi:television"},
-    "2": {"state": "Up", "icon": "mdi:television-shimmer"},
-    "8": {"state": "Seems down", "icon": "mdi:television-off"},
-    "9": {"state": "Down", "icon": "mdi:television-off"},
+    0: StatusValue(value="Pause", icon="mdi:television-pause"),
+    1: StatusValue(value="Not checked yet", icon="mdi:television"),
+    2: StatusValue(value="Up", icon="mdi:television-shimmer"),
+    8: StatusValue(value="Seems down", icon="mdi:television-off"),
+    9: StatusValue(value="Down", icon="mdi:television-off"),
 }
 
 
 async def async_setup_entry(
-    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the UptimeRobot sensors."""
-    coordinator: DataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+    coordinator: UptimeRobotDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
     async_add_entities(
         [
             UptimeRobotSensor(
@@ -31,6 +44,7 @@ async def async_setup_entry(
                 SensorEntityDescription(
                     key=str(monitor.id),
                     name=monitor.friendly_name,
+                    entity_category=EntityCategory.DIAGNOSTIC,
                 ),
                 monitor=monitor,
             )
@@ -45,9 +59,9 @@ class UptimeRobotSensor(UptimeRobotEntity, SensorEntity):
     @property
     def native_value(self) -> str:
         """Return the status of the monitor."""
-        return SENSORS_INFO[str(self.monitor.status)]["state"]
+        return SENSORS_INFO[self.monitor.status]["value"]
 
     @property
     def icon(self) -> str:
         """Return the status of the monitor."""
-        return SENSORS_INFO[str(self.monitor.status)]["icon"]
+        return SENSORS_INFO[self.monitor.status]["icon"]
