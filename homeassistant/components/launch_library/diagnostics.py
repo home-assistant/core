65664fec90a6e57ffbe26fcceda73a079bc8c067
Simon Hansen <67142049+DurgNomis-drol@users.noreply.github.com>
2022-01-26 12:31:03 +0100

    Add next starship launch sensor to launch_library (#64929)
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>

--- homeassistant/components/launch_library/diagnostics.py
+++ homeassistant/components/launch_library/diagnostics.py
@@ -3,12 +3,11 @@ from __future__ import annotations
 
 from typing import Any
 
-from pylaunches.objects.launch import Launch
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
+from . import LaunchLibraryData
 from .const import DOMAIN
 
 
@@ -17,8 +16,12 @@ async def async_get_config_entry_diagnostics(
     entry: ConfigEntry,
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
-    coordinator: DataUpdateCoordinator[list[Launch]] = hass.data[DOMAIN]
-    next_launch = coordinator.data[0] if coordinator.data else None
+    coordinator: DataUpdateCoordinator[LaunchLibraryData] = hass.data[DOMAIN]
+    if coordinator.data is None:
+        return {}
+    next_launch = coordinator.data["upcoming_launches"][0]
+    starship_launch = coordinator.data["starship_events"].upcoming.launches[0]
     return {
-        "next_launch": next_launch.raw_data_contents if next_launch else None,
+        "next_launch": next_launch.raw_data_contents,
+        "starship": starship_launch.raw_data_contents,
     }
