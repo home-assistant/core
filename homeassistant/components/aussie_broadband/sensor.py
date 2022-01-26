8f7e7e5f1468f4875b4fed743663244b1595bd3d
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-01-26 02:32:28 +0100

    Fix new issues

--- homeassistant/components/aussie_broadband/sensor.py
+++ homeassistant/components/aussie_broadband/sensor.py
@@ -11,7 +11,8 @@ from homeassistant.components.sensor import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import DATA_KILOBYTES, DATA_MEGABYTES, TIME_DAYS
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceEntryType, DeviceInfo
+from homeassistant.helpers.device_registry import DeviceEntryType
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
