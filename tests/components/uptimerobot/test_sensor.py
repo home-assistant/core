3f12ce06afca498bd413a5c9c1a9f577dd0198d8
Simone Chemelli <simone.chemelli@gmail.com>
2022-01-26 16:48:15 +0100

    Add detailed status for UptimeRobot (#64879)
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>

--- tests/components/uptimerobot/test_sensor.py
+++ tests/components/uptimerobot/test_sensor.py
@@ -1,36 +1,34 @@
-"""Test UptimeRobot binary_sensor."""
+"""Test UptimeRobot sensor."""
 
 from unittest.mock import patch
 
 from pyuptimerobot import UptimeRobotAuthenticationException
 
-from homeassistant.components.binary_sensor import BinarySensorDeviceClass
-from homeassistant.components.uptimerobot.const import (
-    ATTRIBUTION,
-    COORDINATOR_UPDATE_INTERVAL,
-)
-from homeassistant.const import STATE_ON, STATE_UNAVAILABLE
+from homeassistant.components.uptimerobot.const import COORDINATOR_UPDATE_INTERVAL
+from homeassistant.const import STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
 from homeassistant.util import dt
 
 from .common import (
     MOCK_UPTIMEROBOT_MONITOR,
-    UPTIMEROBOT_BINARY_SENSOR_TEST_ENTITY,
+    STATE_UP,
+    UPTIMEROBOT_SENSOR_TEST_ENTITY,
     setup_uptimerobot_integration,
 )
 
 from tests.common import async_fire_time_changed
 
+SENSOR_ICON = "mdi:television-shimmer"
+
 
 async def test_presentation(hass: HomeAssistant) -> None:
-    """Test the presenstation of UptimeRobot binary_sensors."""
+    """Test the presenstation of UptimeRobot sensors."""
     await setup_uptimerobot_integration(hass)
 
-    entity = hass.states.get(UPTIMEROBOT_BINARY_SENSOR_TEST_ENTITY)
+    entity = hass.states.get(UPTIMEROBOT_SENSOR_TEST_ENTITY)
 
-    assert entity.state == STATE_ON
-    assert entity.attributes["device_class"] == BinarySensorDeviceClass.CONNECTIVITY
-    assert entity.attributes["attribution"] == ATTRIBUTION
+    assert entity.state == STATE_UP
+    assert entity.attributes["icon"] == SENSOR_ICON
     assert entity.attributes["target"] == MOCK_UPTIMEROBOT_MONITOR["url"]
 
 
@@ -38,8 +36,8 @@ async def test_unaviable_on_update_failure(hass: HomeAssistant) -> None:
     """Test entity unaviable on update failure."""
     await setup_uptimerobot_integration(hass)
 
-    entity = hass.states.get(UPTIMEROBOT_BINARY_SENSOR_TEST_ENTITY)
-    assert entity.state == STATE_ON
+    entity = hass.states.get(UPTIMEROBOT_SENSOR_TEST_ENTITY)
+    assert entity.state == STATE_UP
 
     with patch(
         "pyuptimerobot.UptimeRobot.async_get_monitors",
@@ -48,5 +46,5 @@ async def test_unaviable_on_update_failure(hass: HomeAssistant) -> None:
         async_fire_time_changed(hass, dt.utcnow() + COORDINATOR_UPDATE_INTERVAL)
         await hass.async_block_till_done()
 
-    entity = hass.states.get(UPTIMEROBOT_BINARY_SENSOR_TEST_ENTITY)
+    entity = hass.states.get(UPTIMEROBOT_SENSOR_TEST_ENTITY)
     assert entity.state == STATE_UNAVAILABLE
