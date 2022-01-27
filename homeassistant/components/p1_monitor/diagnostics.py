efe10e31880d6fe6fb338e93226160ea428f0cdd
Klaas Schoute <klaas_schoute@hotmail.com>
2022-01-27 16:12:07 +0100

    Add diagnostics to P1 Monitor

---
+++ homeassistant/components/p1_monitor/diagnostics.py
@@ -0,0 +1,30 @@
+"""Diagnostics support for P1 Monitor."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST
+from homeassistant.core import HomeAssistant
+
+from . import P1MonitorDataUpdateCoordinator
+from .const import DOMAIN, SERVICE_PHASES, SERVICE_SETTINGS, SERVICE_SMARTMETER
+
+TO_REDACT = {
+    CONF_HOST,
+}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    coordinator: P1MonitorDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+
+    return {
+        "config_entry": async_redact_data(entry.as_dict(), TO_REDACT),
+        "smartmeter": coordinator.data[SERVICE_SMARTMETER].__dict__,
+        "phases": coordinator.data[SERVICE_PHASES].__dict__,
+        "settings": coordinator.data[SERVICE_SETTINGS].__dict__,
+    }
