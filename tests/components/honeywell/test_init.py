6f83478fc40e631a18e9cd57e4495791cfe67eeb
Randy <randydfurman@gmail.com>
2022-01-27 20:36:06 +0000

    Add away temp migration to options config

--- tests/components/honeywell/test_init.py
+++ tests/components/honeywell/test_init.py
@@ -2,10 +2,17 @@
 
 from unittest.mock import patch
 
+from homeassistant.components.honeywell.const import (
+    CONF_COOL_AWAY_TEMPERATURE,
+    CONF_HEAT_AWAY_TEMPERATURE,
+)
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_registry import EntityRegistry
 
 from tests.common import MockConfigEntry
+
+MIGRATE_OPTIONS_KEYS = {CONF_COOL_AWAY_TEMPERATURE, CONF_HEAT_AWAY_TEMPERATURE}
 
 
 @patch("homeassistant.components.honeywell.UPDATE_LOOP_SLEEP_TIME", 0)
@@ -29,3 +36,23 @@ async def test_setup_multiple_thermostats(
     await hass.async_block_till_done()
     assert config_entry.state is ConfigEntryState.LOADED
     assert hass.states.async_entity_ids_count() == 2
+
+
+async def test_away_temps_migration(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    entity_reg: EntityRegistry,
+    client,
+    location,
+    device,
+) -> None:
+    """Test away temps migrate to config options."""
+    # Create legacy config data
+    config_entry.add_to_hass(hass)
+    # Run integration setup
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    # Assert away temps are in config options
+    # registry = entity_reg.async_get(hass)
+    # registry.
+    # assert MIGRATE_OPTIONS_KEYS.intersection(entry.options)
