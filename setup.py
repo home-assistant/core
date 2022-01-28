44572ff35455a27010c2eccd150417acaaa4aaad
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-01-28 17:11:46 +0100

    Move `project_urls` to `setup.cfg` (#65129)

--- setup.py
+++ setup.py
@@ -4,38 +4,17 @@ from datetime import datetime as dt
 
 from setuptools import find_packages, setup
 
-import homeassistant.const as hass_const
-
 PROJECT_NAME = "Home Assistant"
 PROJECT_PACKAGE_NAME = "homeassistant"
 PROJECT_LICENSE = "Apache License 2.0"
 PROJECT_AUTHOR = "The Home Assistant Authors"
 PROJECT_COPYRIGHT = f" 2013-{dt.now().year}, {PROJECT_AUTHOR}"
-PROJECT_URL = "https://www.home-assistant.io/"
 PROJECT_EMAIL = "hello@home-assistant.io"
-
-PROJECT_GITHUB_USERNAME = "home-assistant"
-PROJECT_GITHUB_REPOSITORY = "core"
-
-PYPI_URL = f"https://pypi.python.org/pypi/{PROJECT_PACKAGE_NAME}"
-GITHUB_PATH = f"{PROJECT_GITHUB_USERNAME}/{PROJECT_GITHUB_REPOSITORY}"
-GITHUB_URL = f"https://github.com/{GITHUB_PATH}"
-
-DOWNLOAD_URL = f"{GITHUB_URL}/archive/{hass_const.__version__}.zip"
-PROJECT_URLS = {
-    "Bug Reports": f"{GITHUB_URL}/issues",
-    "Dev Docs": "https://developers.home-assistant.io/",
-    "Discord": "https://discordapp.com/invite/c5DvZ4e",
-    "Forum": "https://community.home-assistant.io/",
-}
 
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
 
 setup(
     name=PROJECT_PACKAGE_NAME,
-    url=PROJECT_URL,
-    download_url=DOWNLOAD_URL,
-    project_urls=PROJECT_URLS,
     author=PROJECT_AUTHOR,
     author_email=PROJECT_EMAIL,
     packages=PACKAGES,
