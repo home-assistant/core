216d004b443ba948fd58da2d312fc2800d1cde13
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-01-28 20:05:20 +0100

    Move options

--- setup.py
+++ setup.py
@@ -2,19 +2,13 @@
 """Home Assistant setup script."""
 from datetime import datetime as dt
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 PROJECT_NAME = "Home Assistant"
 PROJECT_LICENSE = "Apache License 2.0"
 PROJECT_AUTHOR = "The Home Assistant Authors"
 PROJECT_COPYRIGHT = f" 2013-{dt.now().year}, {PROJECT_AUTHOR}"
 
-PACKAGES = find_packages(exclude=["tests", "tests.*"])
-
 setup(
-    packages=PACKAGES,
-    include_package_data=True,
-    zip_safe=False,
     test_suite="tests",
-    entry_points={"console_scripts": ["hass = homeassistant.__main__:main"]},
 )
