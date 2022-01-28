6243d58cea9037c8920dd0dcbc9d082c4cd25637
Dmitry Vlasov <kerbalspacema@gmail.com>
2022-01-28 12:11:08 +0300

    lock entity

--- homeassistant/components/zwave_me/const.py
+++ homeassistant/components/zwave_me/const.py
@@ -5,6 +5,6 @@ from homeassistant.const import Platform
 # Base component constants
 DOMAIN = "zwave_me"
 
-ZWAVE_PLATFORMS = ["switchMultilevel", "binarySwitch", "toggleButton"]
+ZWAVE_PLATFORMS = ["switchMultilevel", "binarySwitch", "toggleButton", "doorlock"]
 
-PLATFORMS = [Platform.BUTTON, Platform.NUMBER, Platform.SWITCH]
+PLATFORMS = [Platform.BUTTON, Platform.LOCK, Platform.NUMBER, Platform.SWITCH]
---
+++ homeassistant/components/zwave_me/lock.py
@@ -0,0 +1,49 @@
+"""Representation of a doorlock."""
+from typing import Any
+
+from homeassistant.components.lock import LockEntity
+from homeassistant.core import callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+
+from . import ZWaveMeEntity
+from .const import DOMAIN
+
+DEVICE_NAME = "doorlock"
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the lock platform."""
+
+    @callback
+    def add_new_device(new_device):
+        controller = hass.data[DOMAIN][config_entry.entry_id]
+        lock = ZWaveMeLock(controller, new_device)
+
+        async_add_entities(
+            [
+                lock,
+            ]
+        )
+
+    config_entry.async_on_unload(
+        async_dispatcher_connect(
+            hass, f"ZWAVE_ME_NEW_{DEVICE_NAME.upper()}", add_new_device
+        )
+    )
+
+
+class ZWaveMeLock(ZWaveMeEntity, LockEntity):
+    """Representation of a ZWaveMe lock."""
+
+    @property
+    def is_locked(self) -> bool:
+        """Return the state of the lock."""
+        return self.device.level == "close"
+
+    def unlock(self, **kwargs: Any):
+        """Send command to unlock the lock."""
+        self.hass.data[DOMAIN].zwave_api.send_command(self.device.id, "open")
+
+    def lock(self, **kwargs: Any):
+        """Send command to unlock the lock."""
+        self.hass.data[DOMAIN].zwave_api.send_command(self.device.id, "close")
