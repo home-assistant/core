0a2f57e4f8b87cfb7f8c1e839163a33e108a1517
Joakim Sørensen <joasoe@gmail.com>
2022-01-28 10:51:32 +0100

    Move netatmo dataclass registrations (#65052)

--- tests/components/netatmo/test_camera.py
+++ tests/components/netatmo/test_camera.py
@@ -364,7 +364,7 @@ async def test_camera_reconnect_webhook(hass, config_entry):
         await simulate_webhook(hass, webhook_id, response)
         await hass.async_block_till_done()
 
-        assert fake_post_hits == 5
+        assert fake_post_hits == 8
 
         calls = fake_post_hits
 
@@ -446,7 +446,7 @@ async def test_setup_component_no_devices(hass, config_entry):
         await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
 
-        assert fake_post_hits == 1
+        assert fake_post_hits == 4
 
 
 async def test_camera_image_raises_exception(hass, config_entry, requests_mock):
@@ -491,4 +491,4 @@ async def test_camera_image_raises_exception(hass, config_entry, requests_mock):
         await camera.async_get_image(hass, camera_entity_indoor)
 
     assert excinfo.value.args == ("Unable to get image",)
-    assert fake_post_hits == 6
+    assert fake_post_hits == 9
