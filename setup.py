471d44577219c392f4c564d79abffd0e47679825
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-01-28 19:53:59 +0100

    Move metadata keys

--- setup.py
+++ setup.py
@@ -5,18 +5,13 @@ from datetime import datetime as dt
 from setuptools import find_packages, setup
 
 PROJECT_NAME = "Home Assistant"
-PROJECT_PACKAGE_NAME = "homeassistant"
 PROJECT_LICENSE = "Apache License 2.0"
 PROJECT_AUTHOR = "The Home Assistant Authors"
 PROJECT_COPYRIGHT = f" 2013-{dt.now().year}, {PROJECT_AUTHOR}"
-PROJECT_EMAIL = "hello@home-assistant.io"
 
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
 
 setup(
-    name=PROJECT_PACKAGE_NAME,
-    author=PROJECT_AUTHOR,
-    author_email=PROJECT_EMAIL,
     packages=PACKAGES,
     include_package_data=True,
     zip_safe=False,
