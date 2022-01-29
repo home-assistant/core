9f958282482cbcdf11c2967400a11ffbf43db458
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-01-29 05:18:09 +0100

    Move remaining keys to `setup.cfg` (#65154)
    * Move metadata keys
    
    * Move options
    
    * Delete setup.py
    
    * Remove unused constants
    * Remove deprecated test_suite key
    
    * Improve metadata
    
    * Only include homeassistant*, not script*
    * Add long_desc_content_type
    * Remove license file (auto-included by setuptools + wheels)
    
    * Add setup.py
    
    Pip 21.2 doesn't support editable installs without it.

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
-#!/usr/bin/env python3
-"""Home Assistant setup script."""
-from datetime import datetime as dt
+"""
+Entry point for setuptools. Required for editable installs.
+TODO: Remove file after updating to pip 21.3
+"""
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-PROJECT_NAME = "Home Assistant"
-PROJECT_PACKAGE_NAME = "homeassistant"
-PROJECT_LICENSE = "Apache License 2.0"
-PROJECT_AUTHOR = "The Home Assistant Authors"
-PROJECT_COPYRIGHT = f" 2013-{dt.now().year}, {PROJECT_AUTHOR}"
-PROJECT_EMAIL = "hello@home-assistant.io"
-
-PACKAGES = find_packages(exclude=["tests", "tests.*"])
-
-setup(
-    name=PROJECT_PACKAGE_NAME,
-    author=PROJECT_AUTHOR,
-    author_email=PROJECT_EMAIL,
-    packages=PACKAGES,
-    include_package_data=True,
-    zip_safe=False,
-    test_suite="tests",
-    entry_points={"console_scripts": ["hass = homeassistant.__main__:main"]},
-)
+setup()
