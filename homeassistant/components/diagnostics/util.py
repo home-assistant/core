8f98f239ce34cf2a467f13fc8306f24b50240d6c
Aaron Bach <bachya1208@gmail.com>
2022-01-28 22:29:10 -0700

    Update homeassistant/components/diagnostics/util.py
    Co-authored-by: Paulus Schoutsen <paulus@home-assistant.io>

--- homeassistant/components/diagnostics/util.py
+++ homeassistant/components/diagnostics/util.py
@@ -29,7 +29,6 @@ def async_redact_data(data: T, to_redact: Iterable[Any]) -> T:
             elif isinstance(value, list):
                 redacted[key] = [async_redact_data(item, to_redact) for item in value]
     elif isinstance(redacted, list):
-        for idx, value in enumerate(redacted):
-            redacted[idx] = async_redact_data(value, to_redact)
+        return [async_redact_data(val, to_redact) for val in data]
 
     return redacted
