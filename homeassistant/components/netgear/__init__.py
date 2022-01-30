a586a83f598579cf0772426bda2119a75af4cbdd
starkillerOG <starkiller.og@gmail.com>
2022-01-30 18:18:02 +0100

    fix async_uload_entry

--- homeassistant/components/netgear/__init__.py
+++ homeassistant/components/netgear/__init__.py
@@ -81,7 +81,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["router"])
 
     if unload_ok:
         hass.data[DOMAIN].pop(entry.unique_id)
