910396938ee7538347d9c0f1a925907d55ce8500
G Johansson <goran.johansson@shiftit.se>
2022-01-30 10:20:53 +0000

    Coordinator trafikverket_weather

--- homeassistant/components/trafikverket_weatherstation/const.py
+++ homeassistant/components/trafikverket_weatherstation/const.py
@@ -6,7 +6,6 @@ CONF_STATION = "station"
 PLATFORMS = [Platform.SENSOR]
 ATTRIBUTION = "Data provided by Trafikverket"
 ATTR_MEASURE_TIME = "measure_time"
-ATTR_ACTIVE = "active"
 
 NONE_IS_ZERO_SENSORS = {
     "air_temp",
