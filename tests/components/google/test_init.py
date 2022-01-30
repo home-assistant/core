7fba872833935ad59efbc5c494c415f3e46bc3d7
Allen Porter <allen@thebends.org>
2022-01-29 21:29:04 -0800

    Remove commented out code.

--- tests/components/google/test_init.py
+++ tests/components/google/test_init.py
@@ -11,8 +11,6 @@ from oauth2client.client import (
     OAuth2DeviceCodeError,
 )
 from oauth2client.file import Storage
-
-# from google.oauth2.credentials import Credentials
 import pytest
 import yaml
 
@@ -168,70 +166,11 @@ async def google_service() -> YieldFixture[GoogleCalendarService]:
         yield mock
 
 
-@pytest.fixture(name="google_setup")
-def mock_google_setup(hass):
-    """Mock the google set up functions."""
-    p_auth = patch(
-        "homeassistant.components.google.do_authentication", side_effect=google.do_setup
-    )
-    p_service = patch("homeassistant.components.google.GoogleCalendarService.get")
-    p_discovery = patch("homeassistant.components.google.discovery.load_platform")
-    p_load = patch("homeassistant.components.google.load_config", return_value={})
-    p_save = patch("homeassistant.components.google.update_config")
-
-    with p_auth, p_load, p_service, p_discovery, p_save:
-        yield
-
-
 async def fire_alarm(hass, point_in_time):
     """Fire an alarm and wait for callbacks to run."""
     with patch("homeassistant.util.dt.utcnow", return_value=point_in_time):
         async_fire_time_changed(hass, point_in_time)
         await hass.async_block_till_done()
-
-
-# async def test_setup_component(hass, google_setup):
-#    """Test setup component."""
-#    config = {"google": {CONF_CLIENT_ID: "id", CONF_CLIENT_SECRET: "secret"}}
-#
-#    assert await async_setup_component(hass, "google", config)
-
-
-async def test_get_calendar_info(hass, test_calendar):
-    """Test getting the calendar info."""
-    calendar_info = await hass.async_add_executor_job(
-        google.get_calendar_info, hass, test_calendar
-    )
-    assert calendar_info == {
-        "cal_id": "qwertyuiopasdfghjklzxcvbnm@import.calendar.google.com",
-        "entities": [
-            {
-                "device_id": "we_are_we_are_a_test_calendar",
-                "name": "We are, we are, a... Test Calendar",
-                "track": True,
-                "ignore_availability": True,
-            }
-        ],
-    }
-
-
-# async def test_found_calendar(hass, google_setup, mock_next_event, test_calendar):
-#    """Test when a calendar is found."""
-#    config = {
-#        "google": {
-#            CONF_CLIENT_ID: "id",
-#            CONF_CLIENT_SECRET: "secret",
-#            "track_new_calendar": True,
-#        }
-#    }
-#    assert await async_setup_component(hass, "google", config)
-#    assert hass.data[google.DATA_INDEX] == {}
-#
-#    await hass.services.async_call(
-#        "google", google.SERVICE_FOUND_CALENDARS, test_calendar, blocking=True
-#    )
-#
-#    assert hass.data[google.DATA_INDEX].get(test_calendar["id"]) is not None
 
 
 @pytest.mark.parametrize("config", [{}])
@@ -293,9 +232,6 @@ async def test_code_error(
     assert "Error: Test Failure" in mock_notification.call_args[0][1]
     # No credentials persisted
     assert not os.path.exists(hass.config.path(TOKEN_FILE))
-
-
-# assert "In order to authorize Home-Assistant to view your" in mock_notification.call_args[0][1]
 
 
 @pytest.mark.parametrize("code_expiration_delta", [datetime.timedelta(minutes=-5)])
@@ -470,7 +406,7 @@ async def test_invalid_calendar_config_format(
     assert hass.data[google.DATA_INDEX] == {}
 
 
-async def test_found_calendar(
+async def test_found_calendar_from_api(
     hass: HomeAssistant,
     token_file: None,
     component_setup: ComponentSetup,
