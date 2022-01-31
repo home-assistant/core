b60205189e45d93f76b0bc68641b2e45f2f636ea
Dmitry Vlasov <kerbalspacema@gmail.com>
2022-01-31 11:14:48 +0300

    State Classes for sensor

--- homeassistant/components/zwave_me/sensor.py
+++ homeassistant/components/zwave_me/sensor.py
@@ -7,6 +7,7 @@ from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
     SensorEntityDescription,
+    SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
@@ -29,41 +30,49 @@ SENSORS_MAP: dict[str, SensorEntityDescription] = {
         key="meterElectric_watt",
         device_class=SensorDeviceClass.POWER,
         native_unit_of_measurement=POWER_WATT,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
     "meterElectric_kilowatt_hour": SensorEntityDescription(
         key="meterElectric_kilowatt_hour",
         device_class=SensorDeviceClass.ENERGY,
         native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        state_class=SensorStateClass.TOTAL_INCREASING,
     ),
     "meterElectric_voltage": SensorEntityDescription(
         key="meterElectric_voltage",
         device_class=SensorDeviceClass.VOLTAGE,
         native_unit_of_measurement=ELECTRIC_POTENTIAL_VOLT,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
     "light": SensorEntityDescription(
         key="light",
         device_class=SensorDeviceClass.ILLUMINANCE,
         native_unit_of_measurement=LIGHT_LUX,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
     "noise": SensorEntityDescription(
         key="noise",
         device_class=SensorDeviceClass.SIGNAL_STRENGTH,
         native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
     "currentTemperature": SensorEntityDescription(
         key="currentTemperature",
         device_class=SensorDeviceClass.TEMPERATURE,
         native_unit_of_measurement=TEMP_CELSIUS,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
     "temperature": SensorEntityDescription(
         key="temperature",
         device_class=SensorDeviceClass.TEMPERATURE,
         native_unit_of_measurement=TEMP_CELSIUS,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
     "generic": SensorEntityDescription(
         key="temperature",
         device_class=SensorDeviceClass.TEMPERATURE,
         native_unit_of_measurement=TEMP_CELSIUS,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
 }
 DEVICE_NAME = "sensorMultilevel"
@@ -74,7 +83,7 @@ async def async_setup_entry(
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Set up the number platform."""
+    """Set up the sensor platform."""
 
     @callback
     def add_new_device(new_device: ZWaveMeData) -> None:
