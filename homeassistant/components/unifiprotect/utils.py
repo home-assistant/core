52f8ffd0ea0641a1fe1aea5ad53a61c0ac846f43
J. Nick Koston <nick@koston.org>
2022-01-31 17:27:26 -0600

    Prevent unifiprotect from being rediscovered on UDM-PROs (#65335)

--- homeassistant/components/unifiprotect/utils.py
+++ homeassistant/components/unifiprotect/utils.py
@@ -1,10 +1,12 @@
 """UniFi Protect Integration utils."""
 from __future__ import annotations
 
+import contextlib
 from enum import Enum
+import socket
 from typing import Any
 
-from homeassistant.core import callback
+from homeassistant.core import HomeAssistant, callback
 
 
 def get_nested_attr(obj: Any, attr: str) -> Any:
@@ -33,3 +35,19 @@ def _async_unifi_mac_from_hass(mac: str) -> str:
 def _async_short_mac(mac: str) -> str:
     """Get the short mac address from the full mac."""
     return _async_unifi_mac_from_hass(mac)[-6:]
+
+
+async def _async_resolve(hass: HomeAssistant, host: str) -> str | None:
+    """Resolve a hostname to an ip."""
+    with contextlib.suppress(OSError):
+        return next(
+            iter(
+                raw[0]
+                for family, _, _, _, raw in await hass.loop.getaddrinfo(
+                    host, None, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
+                )
+                if family == socket.AF_INET
+            ),
+            None,
+        )
+    return None
