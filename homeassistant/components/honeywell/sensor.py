3d2780a9ad65fedc7316b4807ee208ba813dfcc2
Randy <randydfurman@gmail.com>
2022-01-31 15:32:00 +0000

    Update sensor class to work with device dictionary

--- homeassistant/components/honeywell/sensor.py
+++ homeassistant/components/honeywell/sensor.py
@@ -1,8 +1,7 @@
 """Support for Honeywell (US) Total Connect Comfort sensors."""
 from __future__ import annotations
 
-from homeassistant.components.honeywell.const import DOMAIN
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorEntity, SensorStateClass
 from homeassistant.const import (
     DEVICE_CLASS_HUMIDITY,
     DEVICE_CLASS_TEMPERATURE,
@@ -11,13 +10,15 @@ from homeassistant.const import (
     TEMP_FAHRENHEIT,
 )
 
+from .const import DOMAIN
+
 
 async def async_setup_entry(hass, config, async_add_entities, discovery_info=None):
     """Set up the Honeywell thermostat."""
     data = hass.data[DOMAIN][config.entry_id]
     sensors = []
 
-    for device in data.devices:
+    for device in data.devices.values():
         if device.outdoor_temperature is not None:
             sensors.append(HoneywellOutdoorTemperatureSensor(device))
         if device.outdoor_humidity is not None:
@@ -32,24 +33,17 @@ class HoneywellOutdoorTemperatureSensor(SensorEntity):
     def __init__(self, device):
         """Initialize the outdoor temperature sensor."""
         self._device = device
-        self._state = None
+        self._attr_state_class = SensorStateClass.MEASUREMENT
         self._attr_unique_id = f"{device.deviceid}_outdoor_{DEVICE_CLASS_TEMPERATURE}"
         self._attr_name = f"{device.name} outdoor {DEVICE_CLASS_TEMPERATURE}"
         self._attr_device_class = DEVICE_CLASS_TEMPERATURE
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement this sensor expresses itself in."""
-        return TEMP_CELSIUS if self._device.temperature_unit == "C" else TEMP_FAHRENHEIT
+        self._attr_native_unit_of_measurement = (
+            TEMP_CELSIUS if self._device.temperature_unit == "C" else TEMP_FAHRENHEIT
+        )
 
     def update(self):
         """Get the latest state from the service."""
-        self._state = self._device.outdoor_temperature
+        self._attr_native_value = self._device.outdoor_temperature
 
 
 class HoneywellOutdoorHumiditySensor(SensorEntity):
@@ -58,21 +52,12 @@ class HoneywellOutdoorHumiditySensor(SensorEntity):
     def __init__(self, device):
         """Initialize the outdoor humidity sensor."""
         self._device = device
-        self._state = None
+        self._attr_state_class = SensorStateClass.MEASUREMENT
         self._attr_unique_id = f"{device.deviceid}_outdoor_{DEVICE_CLASS_HUMIDITY}"
         self._attr_name = f"{device.name} outdoor {DEVICE_CLASS_HUMIDITY}"
         self._attr_device_class = DEVICE_CLASS_HUMIDITY
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement this sensor expresses itself in."""
-        return PERCENTAGE
+        self._attr_native_unit_of_measurement = PERCENTAGE
 
     def update(self):
         """Get the latest state from the service."""
-        self._state = self._device.outdoor_humidity
+        self._attr_native_value = self._device.outdoor_humidity
