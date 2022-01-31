4bfe3d8d97a29957c44e92609d5495dd0d7d7886
Poltorak Serguei <poltorak@alsenet.com>
2022-01-31 15:09:23 +0300

    Add sensor platform to Z-Wave.Me integration (#65132)
    * Sensor Entity
    
    * Sensor fixes
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * Inline descriotion according to review proposal
    
    * State Classes for sensor
    
    * Generic sensor
    
    * Generic sensor
    
    Co-authored-by: Dmitry Vlasov <kerbalspacema@gmail.com>
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/zwave_me/const.py
+++ homeassistant/components/zwave_me/const.py
@@ -5,6 +5,18 @@ from homeassistant.const import Platform
 # Base component constants
 DOMAIN = "zwave_me"
 
-ZWAVE_PLATFORMS = ["switchMultilevel", "binarySwitch", "toggleButton", "doorlock"]
+ZWAVE_PLATFORMS = [
+    "switchMultilevel",
+    "binarySwitch",
+    "toggleButton",
+    "doorlock",
+    "sensorMultilevel",
+]
 
-PLATFORMS = [Platform.BUTTON, Platform.LOCK, Platform.NUMBER, Platform.SWITCH]
+PLATFORMS = [
+    Platform.BUTTON,
+    Platform.LOCK,
+    Platform.NUMBER,
+    Platform.SENSOR,
+    Platform.SWITCH,
+]
