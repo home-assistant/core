d55f7535d662ae7c24897d04197ce846e07d9a52
cereal2nd <maikel.punie@gmail.com>
2022-02-02 18:26:26 +0100

    Add comments, update the diagnostics info a bit

--- homeassistant/components/velbus/diagnostics.py
+++ homeassistant/components/velbus/diagnostics.py
@@ -1,6 +1,8 @@
 """Diagnostics support for Velbus."""
 from __future__ import annotations
 
+from typing import Any
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
@@ -9,7 +11,20 @@ from .const import DOMAIN
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
-) -> dict:
+) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     cntrl = hass.data[DOMAIN][entry.entry_id]["cntrl"]
-    return {"entry": entry.as_dict(), "modules": cntrl.get_modules()}
+    diag: dict[str, Any] = {"entry": entry.as_dict(), "modules": []}
+    for addr, mod in cntrl.get_modules().items():
+        diag["modules"].append(
+            {
+                "type": mod.get_type_name(),
+                "address": addr,
+                "name": mod.get_name(),
+                "sw_version": mod.get_sw_version(),
+                "is_loaded": mod.is_loaded(),
+                "channels": mod.get_channels(),
+            }
+        )
+
+    return diag
