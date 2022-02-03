6702f85e1587217f7856f67177c88855d5875015
G Johansson <goran.johansson@shiftit.se>
2022-02-03 11:51:03 +0000

    Modify from review

--- homeassistant/components/sensibo/diagnostics.py
+++ homeassistant/components/sensibo/diagnostics.py
@@ -3,14 +3,11 @@ from __future__ import annotations
 
 from typing import Any
 
-from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 from .coordinator import SensiboDataUpdateCoordinator
-
-TO_REDACT: dict[str, Any] = {}
 
 
 async def async_get_config_entry_diagnostics(
@@ -18,4 +15,4 @@ async def async_get_config_entry_diagnostics(
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     coordinator: SensiboDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
-    return async_redact_data(coordinator.data, TO_REDACT)
+    return coordinator.data
