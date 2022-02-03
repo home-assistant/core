23eb34d117d67f228afb063f648016314fb9197f
Vincent Le Bourlot <vlebourl@gmail.com>
2022-02-03 18:42:47 +0100

    Remove obfuscation of gateway_id.

--- homeassistant/components/overkiz/config_flow.py
+++ homeassistant/components/overkiz/config_flow.py
@@ -147,7 +147,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         await self.async_set_unique_id(gateway_id)
         self._abort_if_unique_id_configured()
         self.context["title_placeholders"] = {
-            CONF_NAME: f"Gateway: {obfuscate_id(gateway_id)}"
+            CONF_NAME: f"Gateway: {gateway_id}"
         }
 
         return await self.async_step_user()
