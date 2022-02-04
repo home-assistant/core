ec6254d0d47a009999f4e785ab0aff1ed77e3314
ollo69 <60491700+ollo69@users.noreply.github.com>
2022-02-04 09:03:50 +0000

    Apply suggested chamges

--- homeassistant/components/asuswrt/diagnostics.py
+++ homeassistant/components/asuswrt/diagnostics.py
@@ -1,6 +1,8 @@
 """Diagnostics support for Asuswrt."""
 from __future__ import annotations
 
+from typing import Any
+
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
@@ -15,7 +17,7 @@ TO_REDACT = {CONF_PASSWORD, CONF_USERNAME}
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
-) -> dict:
+) -> dict[str, dict[str, Any]]:
     """Return diagnostics for a config entry."""
     data = {"entry": async_redact_data(entry.as_dict(), TO_REDACT)}
 
@@ -27,52 +29,54 @@ async def async_get_config_entry_diagnostics(
     hass_device = device_registry.async_get_device(
         identifiers=router.device_info["identifiers"]
     )
-    if hass_device:
-        data["device"] = {
-            "name": hass_device.name,
-            "name_by_user": hass_device.name_by_user,
-            "disabled": hass_device.disabled,
-            "disabled_by": hass_device.disabled_by,
-            "device_info": async_redact_data(dict(router.device_info), {"identifiers"}),
-            "entities": [],
-            "tracked_devices": [],
-        }
+    if not hass_device:
+        return data
 
-        hass_entities = er.async_entries_for_device(
-            entity_registry,
-            device_id=hass_device.id,
-            include_disabled_entities=True,
+    data["device"] = {
+        "name": hass_device.name,
+        "name_by_user": hass_device.name_by_user,
+        "disabled": hass_device.disabled,
+        "disabled_by": hass_device.disabled_by,
+        "device_info": async_redact_data(dict(router.device_info), {"identifiers"}),
+        "entities": [],
+        "tracked_devices": [],
+    }
+
+    hass_entities = er.async_entries_for_device(
+        entity_registry,
+        device_id=hass_device.id,
+        include_disabled_entities=True,
+    )
+
+    for entity_entry in hass_entities:
+        state = hass.states.get(entity_entry.entity_id)
+        state_dict = None
+        if state:
+            state_dict = state.as_dict()
+            # The context doesn't provide useful information in this case.
+            state_dict.pop("context", None)
+
+        data["device"]["entities"].append(
+            {
+                "disabled": entity_entry.disabled,
+                "disabled_by": entity_entry.disabled_by,
+                "entity_category": entity_entry.entity_category,
+                "device_class": entity_entry.device_class,
+                "original_device_class": entity_entry.original_device_class,
+                "icon": entity_entry.icon,
+                "original_icon": entity_entry.original_icon,
+                "unit_of_measurement": entity_entry.unit_of_measurement,
+                "state": state_dict,
+            }
         )
 
-        for entity_entry in hass_entities:
-            state = hass.states.get(entity_entry.entity_id)
-            state_dict = None
-            if state:
-                state_dict = state.as_dict()
-                # The context doesn't provide useful information in this case.
-                state_dict.pop("context", None)
-
-            data["device"]["entities"].append(
-                {
-                    "disabled": entity_entry.disabled,
-                    "disabled_by": entity_entry.disabled_by,
-                    "entity_category": entity_entry.entity_category,
-                    "device_class": entity_entry.device_class,
-                    "original_device_class": entity_entry.original_device_class,
-                    "icon": entity_entry.icon,
-                    "original_icon": entity_entry.original_icon,
-                    "unit_of_measurement": entity_entry.unit_of_measurement,
-                    "state": state_dict,
-                }
-            )
-
-        for device in router.devices.values():
-            data["device"]["tracked_devices"].append(
-                {
-                    "name": device.name or "Unknown device",
-                    "ip_address": device.ip_address,
-                    "last_activity": device.last_activity,
-                }
-            )
+    for device in router.devices.values():
+        data["device"]["tracked_devices"].append(
+            {
+                "name": device.name or "Unknown device",
+                "ip_address": device.ip_address,
+                "last_activity": device.last_activity,
+            }
+        )
 
     return data
