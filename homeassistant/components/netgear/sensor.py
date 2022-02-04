8e668100c944614cfe8fd65d314b20814d2c3e35
starkillerOG <starkiller.og@gmail.com>
2022-02-04 16:29:16 +0100

    Update homeassistant/components/netgear/sensor.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/netgear/sensor.py
+++ homeassistant/components/netgear/sensor.py
@@ -303,7 +303,7 @@ class NetgearRouterSensorEntity(NetgearRouterEntity, SensorEntity):
         super().__init__(coordinator, router)
         self.entity_description = entity_description
         self._name = f"{router.device_name} {entity_description.name}"
-        self._unique_id = f"{router.serial_number}-{self.entity_description.key}-{self.entity_description.index}"
+        self._unique_id = f"{router.serial_number}-{entity_description.key}-{entity_description.index}"
 
         self._value = None
         self.async_update_device()
