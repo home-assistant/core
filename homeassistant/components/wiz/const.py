2bcd4f8f9315aeb0918958a2e01956062c8a2c44
J. Nick Koston <nick@koston.org>
2022-02-05 10:36:44 -0600

    Add discovery support to WiZ Part 1 (#65752)

--- homeassistant/components/wiz/const.py
+++ homeassistant/components/wiz/const.py
@@ -1,8 +1,15 @@
 """Constants for the WiZ Platform integration."""
+from datetime import timedelta
+
 from pywizlight.exceptions import WizLightConnectionError, WizLightTimeOutError
 
 DOMAIN = "wiz"
 DEFAULT_NAME = "WiZ"
+
+DISCOVER_SCAN_TIMEOUT = 10
+DISCOVERY_INTERVAL = timedelta(minutes=15)
+
+SOCKET_DEVICE_STR = "_SOCKET_"
 
 WIZ_EXCEPTIONS = (
     OSError,
