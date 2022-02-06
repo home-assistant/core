51e8d6e2ccd8bdaaf09598f4c47b9a8f6570f268
Jens <jensoen@gmail.com>
2022-02-06 22:59:38 +0000

    Catch up with dev

--- homeassistant/components/ihc/__init__.py
+++ homeassistant/components/ihc/__init__.py
@@ -12,94 +12,16 @@ from homeassistant.helpers.typing import ConfigType
 from .auto_setup import autosetup_ihc_products
 from .const import (
     CONF_AUTOSETUP,
-    CONF_BINARY_SENSOR,
-    CONF_DIMMABLE,
     CONF_INFO,
-    CONF_INVERTING,
-    CONF_LIGHT,
-    CONF_NOTE,
-    CONF_OFF_ID,
-    CONF_ON_ID,
-    CONF_POSITION,
-    CONF_SENSOR,
-    CONF_SWITCH,
     DOMAIN,
     IHC_CONTROLLER,
     IHC_CONTROLLER_INDEX,
-    IHC_PLATFORMS,
 )
+from .manual_setup import IHC_SCHEMA, get_manual_configuration
 from .service_functions import setup_service_functions
 
 _LOGGER = logging.getLogger(__name__)
 
-
-def validate_name(config):
-    """Validate the device name."""
-    if CONF_NAME in config:
-        return config
-    ihcid = config[CONF_ID]
-    name = f"ihc_{ihcid}"
-    config[CONF_NAME] = name
-    return config
-
-
-DEVICE_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_ID): cv.positive_int,
-        vol.Optional(CONF_NAME): cv.string,
-        vol.Optional(CONF_NOTE): cv.string,
-        vol.Optional(CONF_POSITION): cv.string,
-    }
-)
-
-
-SWITCH_SCHEMA = DEVICE_SCHEMA.extend(
-    {
-        vol.Optional(CONF_OFF_ID, default=0): cv.positive_int,
-        vol.Optional(CONF_ON_ID, default=0): cv.positive_int,
-    }
-)
-
-BINARY_SENSOR_SCHEMA = DEVICE_SCHEMA.extend(
-    {
-        vol.Optional(CONF_INVERTING, default=False): cv.boolean,
-        vol.Optional(CONF_TYPE): DEVICE_CLASSES_SCHEMA,
-    }
-)
-
-LIGHT_SCHEMA = DEVICE_SCHEMA.extend(
-    {
-        vol.Optional(CONF_DIMMABLE, default=False): cv.boolean,
-        vol.Optional(CONF_OFF_ID, default=0): cv.positive_int,
-        vol.Optional(CONF_ON_ID, default=0): cv.positive_int,
-    }
-)
-
-SENSOR_SCHEMA = DEVICE_SCHEMA.extend(
-    {vol.Optional(CONF_UNIT_OF_MEASUREMENT, default=TEMP_CELSIUS): cv.string}
-)
-
-IHC_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_PASSWORD): cv.string,
-        vol.Required(CONF_URL): cv.string,
-        vol.Required(CONF_USERNAME): cv.string,
-        vol.Optional(CONF_AUTOSETUP, default=True): cv.boolean,
-        vol.Optional(CONF_BINARY_SENSOR, default=[]): vol.All(
-            cv.ensure_list, [vol.All(BINARY_SENSOR_SCHEMA, validate_name)]
-        ),
-        vol.Optional(CONF_INFO, default=True): cv.boolean,
-        vol.Optional(CONF_LIGHT, default=[]): vol.All(
-            cv.ensure_list, [vol.All(LIGHT_SCHEMA, validate_name)]
-        ),
-        vol.Optional(CONF_SENSOR, default=[]): vol.All(
-            cv.ensure_list, [vol.All(SENSOR_SCHEMA, validate_name)]
-        ),
-        vol.Optional(CONF_SWITCH, default=[]): vol.All(
-            cv.ensure_list, [vol.All(SWITCH_SCHEMA, validate_name)]
-        ),
-    }
-)
 
 CONFIG_SCHEMA = vol.Schema(
     {DOMAIN: vol.Schema(vol.All(cv.ensure_list, [IHC_SCHEMA]))}, extra=vol.ALLOW_EXTRA
@@ -112,7 +34,6 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     for index, controller_conf in enumerate(conf):
         if not ihc_setup(hass, config, controller_conf, index):
             return False
-
     return True
 
 
@@ -148,38 +69,3 @@ def ihc_setup(
     if controller_index == 0:
         setup_service_functions(hass)
     return True
-
-
-def get_manual_configuration(
-    hass: HomeAssistant,
-    config: ConfigType,
-    controller_conf: ConfigType,
-    controller_id: str,
-) -> None:
-    """Get manual configuration for IHC devices."""
-    for platform in IHC_PLATFORMS:
-        discovery_info = {}
-        if platform in controller_conf:
-            platform_setup = controller_conf.get(platform, {})
-            for sensor_cfg in platform_setup:
-                name = sensor_cfg[CONF_NAME]
-                device = {
-                    "ihc_id": sensor_cfg[CONF_ID],
-                    "ctrl_id": controller_id,
-                    "product": {
-                        "name": name,
-                        "note": sensor_cfg.get(CONF_NOTE) or "",
-                        "position": sensor_cfg.get(CONF_POSITION) or "",
-                    },
-                    "product_cfg": {
-                        "type": sensor_cfg.get(CONF_TYPE),
-                        "inverting": sensor_cfg.get(CONF_INVERTING),
-                        "off_id": sensor_cfg.get(CONF_OFF_ID),
-                        "on_id": sensor_cfg.get(CONF_ON_ID),
-                        "dimmable": sensor_cfg.get(CONF_DIMMABLE),
-                        "unit_of_measurement": sensor_cfg.get(CONF_UNIT_OF_MEASUREMENT),
-                    },
-                }
-                discovery_info[name] = device
-        if discovery_info:
-            discovery.load_platform(hass, platform, DOMAIN, discovery_info, config)
