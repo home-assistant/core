4c2f6be59803bef48d665c080d6b664761f55bd8
Nicolas Bourasseau <nicolas.bourasseau@outlook.com>
2022-02-07 15:15:06 +0100

    Update helpers.py
    Improved code formatting

--- homeassistant/components/waze_travel_time/helpers.py
+++ homeassistant/components/waze_travel_time/helpers.py
@@ -9,22 +9,20 @@ from .const import (
 
 import re
 
+
 def is_valid_config_entry(hass, origin, destination, region):
     """Return whether the config entry data is valid."""
     cmpl_re = re.compile(ENTITY_ID_PATTERN)
-
     if cmpl_re.fullmatch(origin):
         if find_coordinates(hass, origin) is not None:
             origin = find_coordinates(hass, origin)
         else:
             origin = hass.states.get(origin).state
-
     if cmpl_re.fullmatch(destination):
         if find_coordinates(hass, destination) is not None:
             destination = find_coordinates(hass, destination)
         else:
             destination = hass.states.get(destination).state
-
     try:
         WazeRouteCalculator(origin, destination, region).calc_all_routes_info()
     except WRCError:
