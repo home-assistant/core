160da3325bef8cca2329256524727a79da184675
Milan Meulemans <milan.meulemans@live.be>
2022-02-07 14:30:51 +0000

    Add senz integration

---
+++ homeassistant/components/senz/config_flow.py
@@ -0,0 +1,24 @@
+"""Config flow for nVent RAYCHEM SENZ."""
+import logging
+
+from homeassistant.helpers import config_entry_oauth2_flow
+
+from .const import DOMAIN
+
+
+class OAuth2FlowHandler(
+    config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN
+):
+    """Config flow to handle SENZ OAuth2 authentication."""
+
+    DOMAIN = DOMAIN
+
+    @property
+    def logger(self) -> logging.Logger:
+        """Return logger."""
+        return logging.getLogger(__name__)
+
+    @property
+    def extra_authorize_data(self) -> dict:
+        """Extra data that needs to be appended to the authorize url."""
+        return {"scope": "restapi offline_access"}
