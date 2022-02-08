7e22a1146e8d4cf4852bca8595c4e3651462fe8d
Sander Jochems <sanderjochems@hotmail.nl>
2022-02-08 14:17:10 +0000

    Remove update listener

--- homeassistant/components/fivem/__init__.py
+++ homeassistant/components/fivem/__init__.py
@@ -53,7 +53,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         raise ConfigEntryNotReady from err
 
     await coordinator.async_config_entry_first_refresh()
-    entry.async_on_unload(entry.add_update_listener(update_listener))
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
@@ -68,11 +67,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
-
-
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Update listener."""
-    await hass.config_entries.async_reload(entry.entry_id)
 
 
 class FiveMDataUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
