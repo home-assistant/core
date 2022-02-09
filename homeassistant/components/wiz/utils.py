b20faeca1de0a8950e4ee6c5fbe640d2cc56086a
J. Nick Koston <nick@koston.org>
2022-02-09 10:47:45 -0600

    wip

--- homeassistant/components/wiz/utils.py
+++ homeassistant/components/wiz/utils.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from pywizlight import BulbType
+from pywizlight.bulblibrary import BulbClass
 
 from .const import DEFAULT_NAME
 
@@ -13,8 +14,11 @@ def _short_mac(mac: str) -> str:
 
 def name_from_bulb_type_and_mac(bulb_type: BulbType, mac: str) -> str:
     """Generate a name from bulb_type and mac."""
-    if hasattr(bulb_type, "description") and bulb_type.description:
-        description = f"{bulb_type.description} {bulb_type.bulb_type.value}"
+    if bulb_type.bulb_type == BulbClass.RGB:
+        if bulb_type.white_channels == 2:
+            description = "RGBWW Tunable"
+        else:
+            description = "RGBW Tunable"
     else:
         description = bulb_type.bulb_type.value
     return f"{DEFAULT_NAME} {description} {_short_mac(mac)}"
