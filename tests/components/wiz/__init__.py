636ca215dbb7b6e9b80d4fc5156e13c553d7410b
J. Nick Koston <nick@koston.org>
2022-02-09 11:56:24 -0600

    wip

--- tests/components/wiz/__init__.py
+++ tests/components/wiz/__init__.py
@@ -1,12 +1,42 @@
 """Tests for the WiZ Platform integration."""
 
+from contextlib import contextmanager
+from copy import deepcopy
 import json
+from unittest.mock import patch
+
+from pywizlight.discovery import DiscoveredBulb
 
 from homeassistant.components.wiz.const import DOMAIN
-from homeassistant.const import CONF_IP_ADDRESS, CONF_NAME
+from homeassistant.const import CONF_HOST, CONF_NAME
 from homeassistant.helpers.typing import HomeAssistantType
 
 from tests.common import MockConfigEntry
+
+FAKE_IP = "1.1.1.1"
+FAKE_MAC = "ABCABCABCABC"
+FAKE_BULB_CONFIG = {
+    "method": "getSystemConfig",
+    "env": "pro",
+    "result": {
+        "mac": FAKE_MAC,
+        "homeId": 653906,
+        "roomId": 989983,
+        "moduleName": "ESP_0711_STR",
+        "fwVersion": "1.21.0",
+        "groupId": 0,
+        "drvConf": [20, 2],
+        "ewf": [255, 0, 255, 255, 0, 0, 0],
+        "ewfHex": "ff00ffff000000",
+        "ping": 0,
+    },
+}
+FAKE_SOCKET_CONFIG = deepcopy(FAKE_BULB_CONFIG)
+FAKE_SOCKET_CONFIG["result"]["moduleName"] = "ESP10_SOCKET_06"
+FAKE_EXTENDED_WHITE_RANGE = [2200, 2700, 6500, 6500]
+TEST_SYSTEM_INFO = {"id": FAKE_MAC, "name": "Test Bulb"}
+TEST_CONNECTION = {CONF_HOST: "1.1.1.1"}
+TEST_NO_IP = {CONF_HOST: "this is no IP input"}
 
 FAKE_BULB_CONFIG = json.loads(
     '{"method":"getSystemConfig","env":"pro","result":\
@@ -36,7 +66,7 @@ REAL_BULB_CONFIG = json.loads(
 
 TEST_SYSTEM_INFO = {"id": "ABCABCABCABC", "name": "Test Bulb"}
 
-TEST_CONNECTION = {CONF_IP_ADDRESS: "1.1.1.1", CONF_NAME: "Test Bulb"}
+TEST_CONNECTION = {CONF_HOST: "1.1.1.1", CONF_NAME: "Test Bulb"}
 
 
 async def setup_integration(
@@ -48,7 +78,7 @@ async def setup_integration(
         domain=DOMAIN,
         unique_id=TEST_SYSTEM_INFO["id"],
         data={
-            CONF_IP_ADDRESS: "127.0.0.1",
+            CONF_HOST: "127.0.0.1",
             CONF_NAME: TEST_SYSTEM_INFO["name"],
         },
     )
@@ -59,3 +89,33 @@ async def setup_integration(
     await hass.async_block_till_done()
 
     return entry
+
+
+def _patch_wizlight(device=None, extended_white_range=None):
+    @contextmanager
+    def _patcher():
+        with patch(
+            "homeassistant.components.wiz.wizlight.getBulbConfig",
+            return_value=device or FAKE_BULB_CONFIG,
+        ), patch(
+            "homeassistant.components.wiz.wizlight.getExtendedWhiteRange",
+            return_value=extended_white_range or FAKE_EXTENDED_WHITE_RANGE,
+        ), patch(
+            "homeassistant.components.wiz.wizlight.getMac",
+            return_value=FAKE_MAC,
+        ):
+            yield
+
+    return _patcher()
+
+
+def _patch_discovery():
+    @contextmanager
+    def _patcher():
+        with patch(
+            "homeassistant.components.wiz.discovery.find_wizlights",
+            return_value=[DiscoveredBulb(FAKE_IP, FAKE_MAC)],
+        ):
+            yield
+
+    return _patcher()
--- tests/components/wiz/test_config_flow.py
+++ tests/components/wiz/test_config_flow.py
@@ -1,10 +1,7 @@
 """Test the WiZ Platform config flow."""
-from contextlib import contextmanager
-from copy import deepcopy
 from unittest.mock import patch
 
 import pytest
-from pywizlight.discovery import DiscoveredBulb
 from pywizlight.exceptions import WizLightConnectionError, WizLightTimeOutError
 
 from homeassistant import config_entries
@@ -14,33 +11,19 @@ from homeassistant.components.wiz.const import DOMAIN
 from homeassistant.const import CONF_HOST
 from homeassistant.data_entry_flow import RESULT_TYPE_ABORT, RESULT_TYPE_FORM
 
+from . import (
+    FAKE_BULB_CONFIG,
+    FAKE_EXTENDED_WHITE_RANGE,
+    FAKE_IP,
+    FAKE_MAC,
+    FAKE_SOCKET_CONFIG,
+    TEST_CONNECTION,
+    TEST_SYSTEM_INFO,
+    _patch_discovery,
+    _patch_wizlight,
+)
+
 from tests.common import MockConfigEntry
-
-FAKE_IP = "1.1.1.1"
-FAKE_MAC = "ABCABCABCABC"
-FAKE_BULB_CONFIG = {
-    "method": "getSystemConfig",
-    "env": "pro",
-    "result": {
-        "mac": FAKE_MAC,
-        "homeId": 653906,
-        "roomId": 989983,
-        "moduleName": "ESP_0711_STR",
-        "fwVersion": "1.21.0",
-        "groupId": 0,
-        "drvConf": [20, 2],
-        "ewf": [255, 0, 255, 255, 0, 0, 0],
-        "ewfHex": "ff00ffff000000",
-        "ping": 0,
-    },
-}
-FAKE_SOCKET_CONFIG = deepcopy(FAKE_BULB_CONFIG)
-FAKE_SOCKET_CONFIG["result"]["moduleName"] = "ESP10_SOCKET_06"
-FAKE_EXTENDED_WHITE_RANGE = [2200, 2700, 6500, 6500]
-TEST_SYSTEM_INFO = {"id": FAKE_MAC, "name": "Test Bulb"}
-TEST_CONNECTION = {CONF_HOST: "1.1.1.1"}
-TEST_NO_IP = {CONF_HOST: "this is no IP input"}
-
 
 DHCP_DISCOVERY = dhcp.DhcpServiceInfo(
     hostname="wiz_abcabc",
@@ -53,36 +36,6 @@ INTEGRATION_DISCOVERY = {
     "ip_address": FAKE_IP,
     "mac_address": FAKE_MAC,
 }
-
-
-def _patch_wizlight(device=None, extended_white_range=None):
-    @contextmanager
-    def _patcher():
-        with patch(
-            "homeassistant.components.wiz.wizlight.getBulbConfig",
-            return_value=device or FAKE_BULB_CONFIG,
-        ), patch(
-            "homeassistant.components.wiz.wizlight.getExtendedWhiteRange",
-            return_value=extended_white_range or FAKE_EXTENDED_WHITE_RANGE,
-        ), patch(
-            "homeassistant.components.wiz.wizlight.getMac",
-            return_value=FAKE_MAC,
-        ):
-            yield
-
-    return _patcher()
-
-
-def _patch_discovery():
-    @contextmanager
-    def _patcher():
-        with patch(
-            "homeassistant.components.wiz.discovery.find_wizlights",
-            return_value=[DiscoveredBulb(FAKE_IP, FAKE_MAC)],
-        ):
-            yield
-
-    return _patcher()
 
 
 async def test_form(hass):
