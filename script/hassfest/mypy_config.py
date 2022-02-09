a80ada35cf047ce834f3554934352e4d0831bbcb
epenet <6771947+epenet@users.noreply.github.com>
2022-02-09 21:01:25 +0000

    Adjust weather

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -193,7 +193,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.template",
     "homeassistant.components.template.number",
     "homeassistant.components.template.sensor",
-    "homeassistant.components.template.weather",
     "homeassistant.components.toon",
     "homeassistant.components.toon.config_flow",
     "homeassistant.components.toon.models",
