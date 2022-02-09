2dbc811edb535553a6f0022a39c365de41b4214e
J. Nick Koston <nick@koston.org>
2022-02-09 10:39:54 -0600

    adjust

--- homeassistant/components/wiz/light.py
+++ homeassistant/components/wiz/light.py
@@ -1,7 +1,6 @@
 """WiZ integration light platform."""
 from __future__ import annotations
 
-import logging
 from typing import Any
 
 from pywizlight import PilotBuilder
@@ -18,7 +17,6 @@ from homeassistant.components.light import (
     COLOR_MODE_COLOR_TEMP,
     COLOR_MODE_RGBW,
     COLOR_MODE_RGBWW,
-    COLOR_MODES_COLOR,
     SUPPORT_EFFECT,
     LightEntity,
 )
@@ -34,7 +32,30 @@ from .const import DOMAIN
 from .entity import WizToggleEntity
 from .models import WizData
 
-_LOGGER = logging.getLogger(__name__)
+
+def _async_pilot_builder(**kwargs: Any) -> PilotBuilder:
+    """Create the PilotBuilder for turn on."""
+    brightness = kwargs.get(ATTR_BRIGHTNESS)
+
+    if ATTR_RGBWW_COLOR in kwargs:
+        return PilotBuilder(brightness=brightness, rgbww=kwargs[ATTR_RGBWW_COLOR])
+
+    if ATTR_RGBW_COLOR in kwargs:
+        return PilotBuilder(brightness=brightness, rgbw=kwargs[ATTR_RGBW_COLOR])
+
+    if ATTR_COLOR_TEMP in kwargs:
+        return PilotBuilder(
+            brightness=brightness,
+            colortemp=color_temperature_mired_to_kelvin(kwargs[ATTR_COLOR_TEMP]),
+        )
+
+    if ATTR_EFFECT in kwargs:
+        scene_id = get_id_from_scene_name(kwargs[ATTR_EFFECT])
+        if scene_id == 1000:  # rhythm
+            return PilotBuilder()
+        return PilotBuilder(brightness=brightness, scene=scene_id)
+
+    return PilotBuilder(brightness=brightness)
 
 
 async def async_setup_entry(
@@ -87,75 +108,26 @@ class WizBulbEntity(WizToggleEntity, LightEntity):
         assert color_modes is not None
         if (brightness := state.get_brightness()) is not None:
             self._attr_brightness = max(0, min(255, brightness))
-        if COLOR_MODE_COLOR_TEMP in color_modes and state.get_colortemp() is not None:
-            self._attr_color_mode = COLOR_MODE_COLOR_TEMP
-            if color_temp := state.get_colortemp():
-                self._attr_color_temp = color_temperature_kelvin_to_mired(color_temp)
-        elif (
-            COLOR_MODES_COLOR.intersection(color_modes)
-            and (rgb := state.get_rgb()) is not None
-            and rgb[0] is not None
+        if (
+            COLOR_MODE_COLOR_TEMP in color_modes
+            and (color_temp := state.get_colortemp()) is not None
         ):
-            warm = state.get_warm_white() or 0
-            if COLOR_MODE_RGBWW in color_modes:
-                cold = state.get_cold_white() or 0
-                self._attr_rgbww_color = (rgb[0], rgb[1], rgb[2], cold, warm)
-                self._attr_color_mode = COLOR_MODE_RGBWW
-            else:
-                self._attr_rgbw_color = (rgb[0], rgb[1], rgb[2], warm)
-                self._attr_color_mode = COLOR_MODE_RGBW
+            self._attr_color_mode = COLOR_MODE_COLOR_TEMP
+            self._attr_color_temp = color_temperature_kelvin_to_mired(color_temp)
+        elif (
+            COLOR_MODE_RGBWW in color_modes and (rgbww := state.get_rgbww()) is not None
+        ):
+            self._attr_rgbww_color = rgbww
+            self._attr_color_mode = COLOR_MODE_RGBWW
+        elif COLOR_MODE_RGBW in color_modes and (rgbw := state.get_rgbw()) is not None:
+            self._attr_rgbww_color = rgbw
+            self._attr_color_mode = COLOR_MODE_RGBW
         else:
             self._attr_color_mode = COLOR_MODE_BRIGHTNESS
         self._attr_effect = state.get_scene()
         super()._async_update_attrs()
 
-    @callback
-    def _async_pilot_builder(self, **kwargs: Any) -> PilotBuilder:
-        """Create the PilotBuilder for turn on."""
-        brightness = kwargs.get(ATTR_BRIGHTNESS)
-
-        if ATTR_RGBWW_COLOR in kwargs:
-            builder = PilotBuilder(brightness=brightness)
-            rgbww = kwargs[ATTR_RGBWW_COLOR]
-            builder.pilot_params.update(
-                {
-                    "r": rgbww[0],
-                    "g": rgbww[1],
-                    "b": rgbww[2],
-                    "c": rgbww[3],
-                    "w": rgbww[4],
-                }
-            )
-            return builder
-
-        if ATTR_RGBW_COLOR in kwargs:
-            builder = PilotBuilder(brightness=brightness)
-            rgbw = kwargs[ATTR_RGBW_COLOR]
-            builder.pilot_params.update(
-                {"r": rgbw[0], "g": rgbw[1], "b": rgbw[2], "w": rgbw[3]}
-            )
-            return builder
-
-        color_temp = None
-        if ATTR_COLOR_TEMP in kwargs:
-            color_temp = color_temperature_mired_to_kelvin(kwargs[ATTR_COLOR_TEMP])
-
-        scene_id = None
-        if ATTR_EFFECT in kwargs:
-            scene_id = get_id_from_scene_name(kwargs[ATTR_EFFECT])
-            if scene_id == 1000:  # rhythm
-                return PilotBuilder()
-
-        _LOGGER.debug(
-            "[wizlight %s] Pilot will be sent with brightness=%s, color_temp=%s, scene_id=%s",
-            self._device.ip,
-            brightness,
-            color_temp,
-            scene_id,
-        )
-        return PilotBuilder(brightness=brightness, colortemp=color_temp, scene=scene_id)
-
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Instruct the light to turn on."""
-        await self._device.turn_on(self._async_pilot_builder(**kwargs))
+        await self._device.turn_on(_async_pilot_builder(**kwargs))
         await self.coordinator.async_request_refresh()
