a62bae339f16be815fe6c124278fbe357495bf16
J. Nick Koston <nick@koston.org>
2022-02-12 09:19:58 -0600

    Add more coverage to wiz

--- tests/components/wiz/test_binary_sensor.py
+++ tests/components/wiz/test_binary_sensor.py
@@ -1,7 +1,5 @@
 """Tests for WiZ binary_sensor platform."""
 
-from pywizlight import PilotParser
-
 from homeassistant import config_entries
 from homeassistant.components import wiz
 from homeassistant.components.wiz.binary_sensor import OCCUPANCY_UNIQUE_ID
@@ -10,34 +8,24 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 from homeassistant.setup import async_setup_component
 
-from . import FAKE_IP, FAKE_MAC, _mocked_wizlight, _patch_discovery, _patch_wizlight
+from . import (
+    FAKE_IP,
+    FAKE_MAC,
+    _mocked_wizlight,
+    _patch_discovery,
+    _patch_wizlight,
+    async_push_update,
+    async_setup_integration,
+)
 
 from tests.common import MockConfigEntry
 
 
 async def test_binary_sensor_created_from_push_updates(hass: HomeAssistant) -> None:
     """Test a binary sensor created from push updates."""
-    entry = MockConfigEntry(
-        domain=wiz.DOMAIN,
-        unique_id=FAKE_MAC,
-        data={CONF_HOST: FAKE_IP},
-    )
-    entry.add_to_hass(hass)
-    bulb = _mocked_wizlight(None, None, None)
-    with _patch_discovery(), _patch_wizlight(device=bulb):
-        await async_setup_component(hass, wiz.DOMAIN, {wiz.DOMAIN: {}})
-        await hass.async_block_till_done()
+    bulb, _ = await async_setup_integration(hass)
 
-    bulb.status = True
-    bulb.state = PilotParser(
-        {
-            "mac": "a8bb50d46a1c",
-            "src": "pir",
-            "state": True,
-        }
-    )
-    bulb.push_callback(bulb.state)
-    await hass.async_block_till_done()
+    await async_push_update(hass, bulb, {"mac": FAKE_MAC, "src": "pir", "state": True})
 
     entity_id = "binary_sensor.mock_title_occupancy"
     entity_registry = er.async_get(hass)
@@ -45,16 +33,7 @@ async def test_binary_sensor_created_from_push_updates(hass: HomeAssistant) -> N
     state = hass.states.get(entity_id)
     assert state.state == STATE_ON
 
-    bulb.status = False
-    bulb.state = PilotParser(
-        {
-            "mac": "a8bb50d46a1c",
-            "src": "pir",
-            "state": False,
-        }
-    )
-    bulb.push_callback(bulb.state)
-    await hass.async_block_till_done()
+    await async_push_update(hass, bulb, {"mac": FAKE_MAC, "src": "pir", "state": False})
 
     state = hass.states.get(entity_id)
     assert state.state == STATE_OFF
@@ -83,16 +62,7 @@ async def test_binary_sensor_restored_from_registry(hass: HomeAssistant) -> None
     state = hass.states.get(entity_id)
     assert state.state == STATE_UNKNOWN
 
-    bulb.status = True
-    bulb.state = PilotParser(
-        {
-            "mac": "a8bb50d46a1c",
-            "src": "pir",
-            "state": True,
-        }
-    )
-    bulb.push_callback(bulb.state)
-    await hass.async_block_till_done()
+    await async_push_update(hass, bulb, {"mac": FAKE_MAC, "src": "pir", "state": True})
 
     assert entity_registry.async_get(entity_id).unique_id == f"{FAKE_MAC}_occupancy"
     state = hass.states.get(entity_id)
@@ -107,18 +77,7 @@ async def test_binary_sensor_never_created_no_error_on_unload(
     hass: HomeAssistant,
 ) -> None:
     """Test a binary sensor does not error on unload."""
-    entry = MockConfigEntry(
-        domain=wiz.DOMAIN,
-        unique_id=FAKE_MAC,
-        data={CONF_HOST: FAKE_IP},
-    )
-    entry.add_to_hass(hass)
-    bulb = _mocked_wizlight(None, None, None)
-
-    with _patch_discovery(), _patch_wizlight(device=bulb):
-        await async_setup_component(hass, wiz.DOMAIN, {wiz.DOMAIN: {}})
-        await hass.async_block_till_done()
-
+    _, entry = await async_setup_integration(hass)
     await hass.config_entries.async_unload(entry.entry_id)
     await hass.async_block_till_done()
     assert entry.state == config_entries.ConfigEntryState.NOT_LOADED
