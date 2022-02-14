3ab69f975fcf023e4b4067c9ed280cd6a6e4d549
J. Nick Koston <nick@koston.org>
2022-02-14 09:56:57 -0600

    Ensure WiZ cleans up on shutdown and failed setup
    Fixes
    ```
    Exception ignored in: <function wizlight.__del__ at 0x1007e0f70>
    Traceback (most recent call last):
      File "/Users/bdraco/home-assistant/venv/lib/python3.9/site-packages/pywizlight/bulb.py", line 693, in __del__
        self.loop.call_soon_threadsafe(self._async_close)
      File "/opt/homebrew/Cellar/python@3.9/3.9.6/Frameworks/Python.framework/Versions/3.9/lib/python3.9/asyncio/base_events.py", line 791, in call_soon_threadsafe
        self._check_closed()
      File "/opt/homebrew/Cellar/python@3.9/3.9.6/Frameworks/Python.framework/Versions/3.9/lib/python3.9/asyncio/base_events.py", line 510, in _check_closed
        raise RuntimeError('Event loop is closed')
    RuntimeError: Event loop is closed
    
    ```

--- tests/components/wiz/test_init.py
+++ tests/components/wiz/test_init.py
@@ -3,6 +3,7 @@ import datetime
 from unittest.mock import AsyncMock
 
 from homeassistant import config_entries
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import HomeAssistant
 from homeassistant.util.dt import utcnow
 
@@ -30,3 +31,22 @@ async def test_setup_retry(hass: HomeAssistant) -> None:
         async_fire_time_changed(hass, utcnow() + datetime.timedelta(minutes=15))
         await hass.async_block_till_done()
     assert entry.state == config_entries.ConfigEntryState.LOADED
+
+
+async def test_cleanup_on_shutdown(hass: HomeAssistant) -> None:
+    """Test the socket is cleaned up on shutdown."""
+    bulb = _mocked_wizlight(None, None, FAKE_SOCKET)
+    _, entry = await async_setup_integration(hass, wizlight=bulb)
+    assert entry.state == config_entries.ConfigEntryState.LOADED
+    hass.bus.async_fire(EVENT_HOMEASSISTANT_STOP)
+    await hass.async_block_till_done()
+    bulb.async_close.assert_called_once()
+
+
+async def test_cleanup_on_failed_first_update(hass: HomeAssistant) -> None:
+    """Test the socket is cleaned up on failed first update."""
+    bulb = _mocked_wizlight(None, None, FAKE_SOCKET)
+    bulb.updateState = AsyncMock(side_effect=OSError)
+    _, entry = await async_setup_integration(hass, wizlight=bulb)
+    assert entry.state == config_entries.ConfigEntryState.SETUP_RETRY
+    bulb.async_close.assert_called_once()
