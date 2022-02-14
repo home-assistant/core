24fc04a964139e5cfecbfa20f91e2d30ab145d77
Diogo Gomes <dgomes@ua.pt>
2022-02-14 21:50:29 +0000

    async_tariff_change tracks state change - state machine will not pass a None

--- homeassistant/components/utility_meter/sensor.py
+++ homeassistant/components/utility_meter/sensor.py
@@ -238,9 +238,7 @@ class UtilityMeterSensor(RestoreEntity, SensorEntity):
     @callback
     def async_tariff_change(self, event):
         """Handle tariff changes."""
-        if (new_state := event.data.get("new_state")) is None:
-            return
-
+        new_state = event.data.get("new_state")
         self._change_status(new_state.state)
 
     def _change_status(self, tariff):
