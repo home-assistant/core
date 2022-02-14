b699b3bf081b46027f2239ce27243f60aebfc61c
Mike Fugate <mfugate@carnegielearning.com>
2022-02-14 23:19:41 +0000

    add test coverage for config flow import

--- tests/components/sleepiq/test_config_flow.py
+++ tests/components/sleepiq/test_config_flow.py
@@ -1,17 +1,30 @@
 """Tests for the SleepIQ config flow."""
 from unittest.mock import patch
 
-from homeassistant import config_entries, data_entry_flow
+from homeassistant import config_entries, data_entry_flow, setup
 from homeassistant.components.sleepiq.const import DOMAIN
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
 
-USER_INPUT = {
+SLEEPIQ_CONFIG = {
     CONF_USERNAME: "username",
     CONF_PASSWORD: "password",
 }
 
 
-async def test_show_set_form(hass) -> None:
+async def test_import(hass: HomeAssistant) -> None:
+    """Test that we can import a config entry."""
+    with patch("sleepyq.Sleepyq.login"):
+        assert await setup.async_setup_component(hass, DOMAIN, {DOMAIN: SLEEPIQ_CONFIG})
+        await hass.async_block_till_done()
+
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+    entry = hass.config_entries.async_entries(DOMAIN)[0]
+    assert entry.data[CONF_USERNAME] == SLEEPIQ_CONFIG[CONF_USERNAME]
+    assert entry.data[CONF_PASSWORD] == SLEEPIQ_CONFIG[CONF_PASSWORD]
+
+
+async def test_show_set_form(hass: HomeAssistant) -> None:
     """Test that the setup form is served."""
     with patch("sleepyq.Sleepyq.login"):
         result = await hass.config_entries.flow.async_init(
@@ -22,11 +35,11 @@ async def test_show_set_form(hass) -> None:
         assert result["step_id"] == "user"
 
 
-async def test_login_error(hass) -> None:
+async def test_login_error(hass: HomeAssistant) -> None:
     """Test we show user form with appropriate error on login failure."""
     with patch("sleepyq.Sleepyq.login", side_effect=ValueError()):
         result = await hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": config_entries.SOURCE_USER}, data=USER_INPUT
+            DOMAIN, context={"source": config_entries.SOURCE_USER}, data=SLEEPIQ_CONFIG
         )
 
         assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
@@ -34,13 +47,13 @@ async def test_login_error(hass) -> None:
         assert result["errors"] == {"base": "cannot_connect"}
 
 
-async def test_success(hass) -> None:
+async def test_success(hass: HomeAssistant) -> None:
     """Test successful flow provides entry creation data."""
     with patch("sleepyq.Sleepyq.login"):
         result = await hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": config_entries.SOURCE_USER}, data=USER_INPUT
+            DOMAIN, context={"source": config_entries.SOURCE_USER}, data=SLEEPIQ_CONFIG
         )
 
         assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-        assert result["data"][CONF_USERNAME] == USER_INPUT[CONF_USERNAME]
-        assert result["data"][CONF_PASSWORD] == USER_INPUT[CONF_PASSWORD]
+        assert result["data"][CONF_USERNAME] == SLEEPIQ_CONFIG[CONF_USERNAME]
+        assert result["data"][CONF_PASSWORD] == SLEEPIQ_CONFIG[CONF_PASSWORD]
