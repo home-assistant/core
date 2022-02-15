37f0a19cd8745a9809ed60a9e6f613f982b2d63c
Mick Vleeshouwer <mick@imick.nl>
2022-02-15 20:01:43 +0000

    Always create new session in ConfigFlow

--- homeassistant/components/overkiz/config_flow.py
+++ homeassistant/components/overkiz/config_flow.py
@@ -19,7 +19,7 @@ from homeassistant.components import dhcp, zeroconf
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.data_entry_flow import FlowResult
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
 from .const import CONF_HUB, DEFAULT_HUB, DOMAIN, LOGGER
 
@@ -46,7 +46,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         username = user_input[CONF_USERNAME]
         password = user_input[CONF_PASSWORD]
         server = SUPPORTED_SERVERS[user_input[CONF_HUB]]
-        session = async_get_clientsession(self.hass)
+        session = async_create_clientsession(self.hass)
 
         client = OverkizClient(
             username=username, password=password, server=server, session=session
