68811756db0b11ea447873d273e89796b6a56869
Keilin <TrumpetGod@gmail.com>
2022-02-14 21:25:17 -0500

    Fix isort mismatch

--- homeassistant/components/sleepiq/__init__.py
+++ homeassistant/components/sleepiq/__init__.py
@@ -2,14 +2,14 @@
 from datetime import timedelta
 import logging
 
-import voluptuous as vol
-
 from asyncsleepiq import (
     AsyncSleepIQ,
     SleepIQAPIException,
     SleepIQLoginException,
     SleepIQTimeoutException,
 )
+import voluptuous as vol
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
 from homeassistant.core import HomeAssistant
