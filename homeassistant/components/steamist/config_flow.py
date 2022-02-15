b28754e5fef1450e3b0e20a6a160cd1257cceea3
J. Nick Koston <nick@koston.org>
2022-02-15 16:18:13 -0600

    Switch steamist to use integration discovery (#66578)

--- homeassistant/components/steamist/config_flow.py
+++ homeassistant/components/steamist/config_flow.py
@@ -48,10 +48,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
         return await self._async_handle_discovery()
 
-    async def async_step_discovery(
+    async def async_step_integration_discovery(
         self, discovery_info: DiscoveryInfoType
     ) -> FlowResult:
-        """Handle discovery."""
+        """Handle integration discovery."""
         self._discovered_device = Device30303(
             ipaddress=discovery_info["ipaddress"],
             name=discovery_info["name"],
