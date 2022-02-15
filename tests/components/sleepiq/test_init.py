086db0cebd5dc4cb0e1cabbb8209db552580d6af
Mike Fugate <mfugate@carnegielearning.com>
2022-02-15 23:23:54 +0000

    add test coverage for coordinator update

--- tests/components/sleepiq/test_init.py
+++ tests/components/sleepiq/test_init.py
@@ -2,8 +2,17 @@
 from unittest.mock import patch
 
 from homeassistant.components.sleepiq.const import DOMAIN
+from homeassistant.components.sleepiq.coordinator import UPDATE_INTERVAL
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
+from homeassistant.util.dt import utcnow
+
+from tests.common import async_fire_time_changed
+from tests.components.sleepiq.conftest import (
+    mock_bed_family_status,
+    mock_beds,
+    mock_sleepers,
+)
 
 
 async def test_unload_entry(hass: HomeAssistant, setup_entry) -> None:
@@ -21,3 +30,25 @@ async def test_entry_setup_login_error(hass: HomeAssistant, config_entry) -> Non
     with patch("sleepyq.Sleepyq.login", return_value=False):
         config_entry.add_to_hass(hass)
         assert not await hass.config_entries.async_setup(config_entry.entry_id)
+
+
+async def test_update_interval(hass: HomeAssistant, setup_entry) -> None:
+    """Test update interval."""
+    with patch("sleepyq.Sleepyq.beds", return_value=mock_beds("")) as beds, patch(
+        "sleepyq.Sleepyq.sleepers", return_value=mock_sleepers()
+    ) as sleepers, patch(
+        "sleepyq.Sleepyq.bed_family_status",
+        return_value=mock_bed_family_status(""),
+    ) as bed_family_status, patch(
+        "sleepyq.Sleepyq.login", return_value=True
+    ):
+        assert beds.call_count == 0
+        assert sleepers.call_count == 0
+        assert bed_family_status.call_count == 0
+
+        async_fire_time_changed(hass, utcnow() + UPDATE_INTERVAL)
+        await hass.async_block_till_done()
+
+        assert beds.call_count == 1
+        assert sleepers.call_count == 1
+        assert bed_family_status.call_count == 1
