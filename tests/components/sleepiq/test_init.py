ddc3eef2e3e16ef1c042ce1dbafced5fc2f6a8b6
Mike Fugate <mfugate@carnegielearning.com>
2022-02-15 01:51:22 +0000

    add test coverage for failed entry setup

--- tests/components/sleepiq/test_init.py
+++ tests/components/sleepiq/test_init.py
@@ -1,4 +1,6 @@
 """Tests for the SleepIQ integration."""
+from unittest.mock import patch
+
 from homeassistant.components.sleepiq.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
@@ -12,3 +14,10 @@ async def test_unload_entry(hass: HomeAssistant, setup_entry) -> None:
 
     assert entry.state is ConfigEntryState.NOT_LOADED
     assert not hass.data.get(DOMAIN)
+
+
+async def test_entry_setup_login_error(hass: HomeAssistant, config_entry) -> None:
+    """Test when sleepyq client is unable to login."""
+    with patch("sleepyq.Sleepyq.login", return_value=False):
+        config_entry.add_to_hass(hass)
+        assert not await hass.config_entries.async_setup(config_entry.entry_id)
