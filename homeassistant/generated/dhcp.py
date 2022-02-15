424864aa02a903c544db1fd8a0d2480732b3b2e9
Mike Fugate <mfugate@carnegielearning.com>
2022-02-15 21:37:52 +0000

    add dhcp discovery support for sleepiq

--- homeassistant/generated/dhcp.py
+++ homeassistant/generated/dhcp.py
@@ -322,6 +322,10 @@ DHCP = [
         "macaddress": "30AEA4*"
     },
     {
+        "domain": "sleepiq",
+        "macaddress": "64DBA0*"
+    },
+    {
         "domain": "smartthings",
         "hostname": "st*",
         "macaddress": "24FD5B*"
