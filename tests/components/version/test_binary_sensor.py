686f6d7bf9dd62de5731aca6212fb97cc1d39c76
Ludeeus <ludeeus@ludeeus.dev>
2022-02-15 06:53:43 +0000

    Add test to check we not create for local

--- tests/components/version/test_binary_sensor.py
+++ tests/components/version/test_binary_sensor.py
@@ -7,11 +7,17 @@ from homeassistant.core import HomeAssistant
 from .common import setup_version_integration
 
 
+async def test_version_binary_sensor_local_source(hass: HomeAssistant):
+    """Test the Version binary sensor with local source."""
+    await setup_version_integration(hass)
+
+    state = hass.states.get("binary_sensor.local_installation_update_available")
+    assert not state
+
+
 async def test_version_binary_sensor(hass: HomeAssistant):
     """Test the Version binary sensor."""
-    await setup_version_integration(
-        hass, {**DEFAULT_CONFIGURATION, "source": "pypi", "name": "PyPi"}
-    )
+    await setup_version_integration(hass, {**DEFAULT_CONFIGURATION, "source": "pypi"})
 
-    state = hass.states.get("binary_sensor.pypi_update_available")
-    assert state.state == "off"
+    state = hass.states.get("binary_sensor.local_installation_update_available")
+    assert state
