d3ed99ee96aec51aa6e5f3019b0fd4a990d94590
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2022-02-16 14:04:56 +0000

    Add tests for diagnostics

--- tests/components/homewizard/conftest.py
+++ tests/components/homewizard/conftest.py
@@ -1,22 +1,15 @@
 """Fixtures for HomeWizard integration tests."""
+import json
+from unittest.mock import AsyncMock, MagicMock, patch
+
+from homewizard_energy.models import Data, Device, State
 import pytest
 
 from homeassistant.components.homewizard.const import DOMAIN
 from homeassistant.const import CONF_IP_ADDRESS
+from homeassistant.core import HomeAssistant
 
-from tests.common import MockConfigEntry
-
-
-@pytest.fixture
-def mock_config_entry_data():
-    """Return the default mocked config entry data."""
-    return {
-        "product_name": "Product Name",
-        "product_type": "product_type",
-        "serial": "aabbccddeeff",
-        "name": "Product Name",
-        CONF_IP_ADDRESS: "1.2.3.4",
-    }
+from tests.common import MockConfigEntry, load_fixture
 
 
 @pytest.fixture
@@ -25,6 +18,44 @@ def mock_config_entry() -> MockConfigEntry:
     return MockConfigEntry(
         title="Product Name (aabbccddeeff)",
         domain=DOMAIN,
-        data={},
+        data={CONF_IP_ADDRESS: "example.com"},
         unique_id="aabbccddeeff",
     )
+
+
+@pytest.fixture
+def mock_homewizard_energy():
+    """Return a mocked P1 Monitor client."""
+    with patch("homeassistant.components.homewizard.HomeWizardEnergy") as mock:
+        api = mock.return_value
+        api.device = AsyncMock(
+            return_value=Device.from_dict(
+                json.loads(load_fixture("homewizard/device.json"))
+            )
+        )
+        api.data = AsyncMock(
+            return_value=Data.from_dict(
+                json.loads(load_fixture("homewizard/data.json"))
+            )
+        )
+        api.state = AsyncMock(
+            return_value=State.from_dict(
+                json.loads(load_fixture("homewizard/state.json"))
+            )
+        )
+        yield mock
+
+
+@pytest.fixture
+async def init_integration(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_homewizard_energy: MagicMock,
+) -> MockConfigEntry:
+    """Set up the HomeWizard integration for testing."""
+    mock_config_entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    return mock_config_entry
