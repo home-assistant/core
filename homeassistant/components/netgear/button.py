afa7253b90f4317c37bbffda1d30ca66f2888f95
Martin Hjelmare <marhje52@gmail.com>
2022-02-16 21:06:26 +0100

    Add extra blank line

--- homeassistant/components/netgear/button.py
+++ homeassistant/components/netgear/button.py
@@ -22,6 +22,7 @@ class NetgearButtonEntityDescriptionRequired:
 
     action: Callable
 
+
 @dataclass
 class NetgearButtonEntityDescription(
     ButtonEntityDescription, NetgearButtonEntityDescriptionRequired
