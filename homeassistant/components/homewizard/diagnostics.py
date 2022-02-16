afb058a37f579df161e5c2c287711d5ece23f2b5
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2022-02-16 13:02:57 +0000

    FIx diagnostics implementation

--- homeassistant/components/homewizard/diagnostics.py
+++ homeassistant/components/homewizard/diagnostics.py
@@ -1,6 +1,7 @@
 """Diagnostics support for P1 Monitor."""
 from __future__ import annotations
 
+from dataclasses import asdict
 from typing import Any
 
 from homeassistant.components.diagnostics import async_redact_data
@@ -8,7 +9,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_IP_ADDRESS
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
+from .const import DOMAIN, SERVICE_DATA, SERVICE_DEVICE, SERVICE_STATE
 from .coordinator import HWEnergyDeviceUpdateCoordinator
 
 TO_REDACT = {CONF_IP_ADDRESS, "serial", "wifi_ssid"}
@@ -21,10 +22,10 @@ async def async_get_config_entry_diagnostics(
     coordinator: HWEnergyDeviceUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
     meter_data = {
-        "device": coordinator.api.device.todict(),
-        "data": coordinator.api.data.todict(),
-        "state": coordinator.api.state.todict()
-        if coordinator.api.state is not None
+        "device": asdict(coordinator.data[SERVICE_DEVICE]),
+        "data": asdict(coordinator.data[SERVICE_DATA]),
+        "state": asdict(coordinator.data[SERVICE_STATE])
+        if coordinator.data[SERVICE_STATE] is not None
         else None,
     }
 
