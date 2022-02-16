08547586581b1b1df57eca6e1283c5efcd1625a7
Stuart Clark <stuart.clark@Jahingo.com>
2022-02-16 23:30:16 +0000

    Try to fix

--- homeassistant/components/ovo_energy/const.py
+++ homeassistant/components/ovo_energy/const.py
@@ -1,9 +1,7 @@
 """Constants for the OVO Energy integration."""
-from typing import Final
-
 DOMAIN = "ovo_energy"
 
 DATA_CLIENT = "ovo_client"
 DATA_COORDINATOR = "coordinator"
 
-CONF_ACCOUNT: Final = "account"
+CONF_ACCOUNT = "account"
