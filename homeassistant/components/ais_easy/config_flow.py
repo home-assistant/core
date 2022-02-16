68eef08abf53414b78817551e0dd36e3c5664578
tomasz75 <tomek@sviete.pl>
2022-02-16 16:28:25 +0100

    validate connection to PLC

--- homeassistant/components/ais_easy/config_flow.py
+++ homeassistant/components/ais_easy/config_flow.py
@@ -1,8 +1,12 @@
+from base64 import b64encode
+import json
 import logging
 
+import async_timeout
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.helpers import aiohttp_client
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,9 +34,39 @@ class AisNbpConfigFlow(config_entries.ConfigFlow, domain="ais_easy"):
         return self.async_show_form(step_id="confirm")
 
     async def async_step_settings(self, user_input=None):
-        """Krok wyboru walut do śledzenia"""
+        """Krok parametry połączenie z plc"""
         if user_input is not None:
-            # Zakończenie i zapis konfiguracji
-            return self.async_create_entry(title="Easy PLC", data=user_input)
+            # check the connection to PLC
+            try:
+                with async_timeout.timeout(15):
+                    web_session = aiohttp_client.async_get_clientsession(self.hass)
+                    encoded_credentials = b64encode(
+                        bytes(
+                            f"{user_input['user']}:{user_input['pass']}",
+                            encoding="ascii",
+                        )
+                    ).decode("ascii")
+
+                    header = {
+                        "Authorization": "Basic %s" % encoded_credentials,
+                        "Content-Type": "application/json",
+                    }
+                    url = "http://" + user_input["host"] + "/api/get/data?elm=STATE"
+
+                    ws_resp = await web_session.get(url, headers=header)
+                    info = await ws_resp.text()
+                    info_json = json.loads(info)
+                    # Zakończenie i zapis konfiguracji
+                    return self.async_create_entry(title="Easy PLC", data=user_input)
+
+            except Exception as e:
+                _LOGGER.error("Ask Easy error: " + str(e))
+                # Informacja o błędzie
+                errors = {"host": "connection_error"}
+                return self.async_show_form(
+                    step_id="settings",
+                    data_schema=vol.Schema({"host": str, "user": str, "pass": str}),
+                    errors=errors,
+                )
 
         return self.async_show_form(step_id="confirm")
