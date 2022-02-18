0a77b249ba752be1904390aa9d03cccc5fac0172
DurgNomis-drol <simongrud@gmail.com>
2022-02-18 15:06:30 +0000

    Code enhancements for ISS

--- homeassistant/components/iss/__init__.py
+++ homeassistant/components/iss/__init__.py
@@ -12,7 +12,6 @@ PLATFORMS = [Platform.BINARY_SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up this integration using UI."""
-
     hass.data.setdefault(DOMAIN, {})
 
     entry.async_on_unload(entry.add_update_listener(update_listener))
@@ -29,6 +28,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return unload_ok
 
 
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Handle options update."""
     await hass.config_entries.async_reload(entry.entry_id)
