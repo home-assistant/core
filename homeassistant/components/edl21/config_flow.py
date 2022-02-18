f2fcb135ccba1f99f919a418a810d629032cc41e
dh4rry <harald.gutbrod+github@gmail.com>
2022-02-18 20:52:25 +0100

    doc & types added

--- homeassistant/components/edl21/config_flow.py
+++ homeassistant/components/edl21/config_flow.py
@@ -1,19 +1,29 @@
 from homeassistant import config_entries
+from homeassistant.data_entry_flow import FlowResult
 import voluptuous as vol
 from .sensor import DOMAIN, CONF_SERIAL_PORT
 from homeassistant.const import CONF_NAME
 import homeassistant.helpers.config_validation as cv
 
+from typing import Any
 
 EDL_SCHEMA = vol.Schema(
-    {vol.Required(CONF_SERIAL_PORT): cv.string, vol.Optional(CONF_NAME): cv.string}
+    {
+        vol.Required(CONF_SERIAL_PORT): cv.string,
+        vol.Optional(CONF_NAME, default=""): cv.string,
+    }
 )
 
 
 class EDL21ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    async def async_step_user(self, info):
-        if info is not None:
-            self.data = info
+    """Handle a config flow for EDL21."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input: dict[str, Any]) -> FlowResult:
+        """Handle a flow initiated by the user."""
+        if user_input is not None:
+            self.data = user_input
             return self.async_create_entry(title="EDL21", data=self.data)
 
         return self.async_show_form(step_id="user", data_schema=EDL_SCHEMA)
