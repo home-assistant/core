e1649a19b829aa4829e1d37c7a378328132ed981
Matthias Lohr <mail@mlohr.com>
2022-02-18 16:27:14 +0100

    use EntityDescription

--- homeassistant/components/tolo/number.py
+++ homeassistant/components/tolo/number.py
@@ -1,10 +1,17 @@
 """TOLO Sauna number controls."""
-from homeassistant.components.number import NumberEntity
+from __future__ import annotations
+
+from collections.abc import Callable
+from dataclasses import dataclass
+
+from homeassistant.components.number import NumberEntity, NumberEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import ToloSaunaCoordinatorEntity, ToloSaunaUpdateCoordinator
+from ...const import TIME_MINUTES
+from ...helpers.entity import EntityCategory
 from .const import DOMAIN, FAN_TIMER_MAX, POWER_TIMER_MAX, SALT_BATH_TIMER_MAX
 
 
@@ -17,113 +24,84 @@ async def async_setup_entry(
     coordinator = hass.data[DOMAIN][entry.entry_id]
     async_add_entities(
         [
-            ToloPowerTimerDuration(coordinator, entry),
-            ToloSaltBathTimerDuration(coordinator, entry),
-            ToloFanTimerDuration(coordinator, entry),
+            ToloNumberEntity(coordinator, entry, description)
+            for description in (
+                ToloNumberEntityDescription(
+                    key="power_timer",
+                    icon="mdi:power-settings",
+                    name="Power Timer",
+                    unit_of_measurement=TIME_MINUTES,
+                    max_value=POWER_TIMER_MAX,
+                    getter=coordinator.data.settings.power_timer,
+                    setter=coordinator.client.set_power_timer,
+                ),
+                ToloNumberEntityDescription(
+                    key="salt_bath_timer",
+                    icon="mdi:shaker-outline",
+                    name="Salt Bath Timer",
+                    unit_of_measurement=TIME_MINUTES,
+                    max_value=SALT_BATH_TIMER_MAX,
+                    getter=coordinator.data.settings.salt_bath_timer,
+                    setter=coordinator.client.set_salt_bath_timer,
+                ),
+                ToloNumberEntityDescription(
+                    key="fan_timer",
+                    icon="mdi:fan-auto",
+                    name="Fan Timer",
+                    unit_of_measurement=TIME_MINUTES,
+                    max_value=FAN_TIMER_MAX,
+                    getter=coordinator.data.settings.fan_timer,
+                    setter=coordinator.client.set_fan_timer,
+                ),
+            )
         ]
     )
 
 
-class ToloPowerTimerDuration(ToloSaunaCoordinatorEntity, NumberEntity):
-    """
-    TOLO Sauna Power Timer Control.
+@dataclass
+class ToloNumberEntityDescriptionBase:
+    """Required values when describing TOLO Number entities."""
 
-    Automatic shuts down the device after configured timeout.
-    """
+    getter: Callable[[], int | None]
+    setter: Callable[[int | None], None]
 
-    _attr_name = "Power Timer"
-    _attr_min_value = 0
-    _attr_max_value = POWER_TIMER_MAX
-    _attr_step = 1
-    _attr_icon = "mdi:power-settings"
+
+@dataclass
+class ToloNumberEntityDescription(
+    NumberEntityDescription, ToloNumberEntityDescriptionBase
+):
+    """Class describing TOLO Number entities."""
+
+    entity_category = EntityCategory.CONFIG
+    min_value = 0
+    step = 1
+
+
+class ToloNumberEntity(ToloSaunaCoordinatorEntity, NumberEntity):
+    """TOLO Number entity."""
+
+    entity_description: ToloNumberEntityDescription
 
     def __init__(
-        self, coordinator: ToloSaunaUpdateCoordinator, entry: ConfigEntry
+        self,
+        coordinator: ToloSaunaUpdateCoordinator,
+        entry: ConfigEntry,
+        entity_description: ToloNumberEntityDescription,
     ) -> None:
-        """Initialize TOLO Power Timer Duration entity."""
+        """Initialize TOLO Number entity."""
         super().__init__(coordinator, entry)
-
-        self._attr_unique_id = f"{entry.entry_id}_power_timer_duration"
+        self.entity_description = entity_description
+        self._attr_unique_id = f"{entry.entry_id}_{entity_description.key}"
 
     @property
     def value(self) -> float:
-        """Return current power timer setting."""
-        return self.coordinator.data.settings.power_timer or 0
+        """Return the value of this TOLO Number entity."""
+        return self.entity_description.getter() or 0
 
     def set_value(self, value: float) -> None:
-        """Set power timeout. 0 = disabled."""
+        """Set the value of this TOLO Number entity."""
         int_value = int(value)
         if int_value == 0:
-            self.coordinator.client.set_power_timer(None)
+            self.entity_description.setter(None)
             return
-        self.coordinator.client.set_power_timer(int_value)
-
-
-class ToloSaltBathTimerDuration(ToloSaunaCoordinatorEntity, NumberEntity):
-    """
-    TOLO Sauna Salt Bath Timer Control.
-
-    Activates the salt sprayer in the configured interval.
-    """
-
-    _attr_name = "Salt Bath Timer"
-    _attr_min_value = 0
-    _attr_max_value = SALT_BATH_TIMER_MAX
-    _attr_step = 1
-    _attr_icon = "mdi:shaker-outline"
-
-    def __init__(
-        self, coordinator: ToloSaunaUpdateCoordinator, entry: ConfigEntry
-    ) -> None:
-        """Initialize TOLO Salt Bath Timer Duration entity."""
-        super().__init__(coordinator, entry)
-
-        self._attr_unique_id = f"{entry.entry_id}_salt_bath_timer_duration"
-
-    @property
-    def value(self) -> float:
-        """Return current salt bath timer setting."""
-        return self.coordinator.data.settings.salt_bath_timer or 0
-
-    def set_value(self, value: float) -> None:
-        """Set the salt bath spray interval. 0 = disabled."""
-        int_value = int(value)
-        if int_value == 0:
-            self.coordinator.client.set_salt_bath_timer(None)
-            return
-        self.coordinator.client.set_salt_bath_timer(int_value)
-
-
-class ToloFanTimerDuration(ToloSaunaCoordinatorEntity, NumberEntity):
-    """
-    TOLO Sauna Fan Timer Control.
-
-    Deactivate the fan after the configured timeout.
-    """
-
-    _attr_name = "Fan Timer"
-    _attr_min_value = 0
-    _attr_max_value = FAN_TIMER_MAX
-    _attr_step = 1
-    _attr_icon = "mdi:fan-auto"
-
-    def __init__(
-        self, coordinator: ToloSaunaUpdateCoordinator, entry: ConfigEntry
-    ) -> None:
-        """Initialize TOLO Salt Bath Timer Duration entity."""
-        super().__init__(coordinator, entry)
-
-        self._attr_unique_id = f"{entry.entry_id}_fan_timer_duration"
-
-    @property
-    def value(self) -> float:
-        """Return current fan timer setting."""
-        return self.coordinator.data.settings.fan_timer or 0
-
-    def set_value(self, value: float) -> None:
-        """Set Fan timer. 0 = no automatic shutdown."""
-        int_value = int(value)
-        if int_value == 0:
-            self.coordinator.client.set_fan_timer(None)
-            return
-        self.coordinator.client.set_fan_timer(int_value)
+        self.entity_description.setter(int_value)
