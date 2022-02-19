71b482c390e7ac32fc55cab457e4b37092d2e400
Keilin <TrumpetGod@gmail.com>
2022-02-19 16:11:21 +0000

    Created duplicate class to avoid init call

--- homeassistant/components/sleepiq/entity.py
+++ homeassistant/components/sleepiq/entity.py
@@ -28,7 +28,7 @@ class SleepIQEntity(Entity):
         )
 
 
-class SleepIQSensor(CoordinatorEntity, SleepIQEntity):
+class SleepIQSensor(CoordinatorEntity):
     """Implementation of a SleepIQ sensor."""
 
     _attr_icon = ICON_OCCUPIED
@@ -42,12 +42,18 @@ class SleepIQSensor(CoordinatorEntity, SleepIQEntity):
     ) -> None:
         """Initialize the SleepIQ sensor entity."""
         super().__init__(coordinator)
-        SleepIQEntity.__init__(self, bed)
         self.sleeper = sleeper
-        self._async_update_attrs()
+        self.bed = bed
+        self._attr_device_info = DeviceInfo(
+            connections={(device_registry.CONNECTION_NETWORK_MAC, bed.mac_addr)},
+            manufacturer="SleepNumber",
+            name=bed.name,
+            model=bed.model,
+        )
 
         self._attr_name = f"SleepNumber {bed.name} {sleeper.name} {SENSOR_TYPES[name]}"
         self._attr_unique_id = f"{bed.id}_{sleeper.name}_{name}"
+        self._async_update_attrs()
 
     @callback
     def _handle_coordinator_update(self) -> None:
