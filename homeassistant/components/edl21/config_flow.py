269eba81b4790a30793f2796becdbae549c8025a
dh4rry <harald.gutbrod+github@gmail.com>
2022-02-19 18:41:24 +0100

    enhanced config flow with port selection

--- homeassistant/components/edl21/config_flow.py
+++ homeassistant/components/edl21/config_flow.py
@@ -7,12 +7,9 @@ import homeassistant.helpers.config_validation as cv
 
 from typing import Any
 
-EDL_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_SERIAL_PORT): cv.string,
-        vol.Optional(CONF_NAME, default=""): cv.string,
-    }
-)
+import serial.tools.list_ports
+
+CONF_MANUAL_PATH = "Enter Manually"
 
 
 class EDL21ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -20,9 +17,46 @@ class EDL21ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
+    async def async_step_pick_manual(self, user_input: dict[str, Any]) -> FlowResult:
+        return self.async_create_entry(title="EDL21", data=user_input)
+
     async def async_step_user(self, user_input: dict[str, Any]) -> FlowResult:
         """Handle a flow initiated by the user."""
         if user_input is not None:
+            if user_input[CONF_SERIAL_PORT] == CONF_MANUAL_PATH:
+                return self.async_show_form(
+                    step_id="pick_manual",
+                    data_schema=vol.Schema(
+                        {
+                            vol.Optional(
+                                CONF_NAME, default=user_input[CONF_NAME]
+                            ): cv.string,
+                            vol.Required(CONF_SERIAL_PORT): cv.string,
+                        }
+                    ),
+                )
             return self.async_create_entry(title="EDL21", data=user_input)
 
-        return self.async_show_form(step_id="user", data_schema=EDL_SCHEMA)
+        ports = await self.hass.async_add_executor_job(serial.tools.list_ports.comports)
+        list_of_ports = [f"{p.device}" for p in ports]
+
+        if not list_of_ports:
+            return self.async_show_form(
+                step_id="pick_manual",
+                data_schema=vol.Schema(
+                    {
+                        vol.Optional(CONF_NAME, default=""): cv.string,
+                        vol.Required(CONF_SERIAL_PORT): cv.string,
+                    }
+                ),
+            )
+
+        list_of_ports.append(CONF_MANUAL_PATH)
+
+        schema = vol.Schema(
+            {
+                vol.Optional(CONF_NAME, default=""): cv.string,
+                vol.Required(CONF_SERIAL_PORT): vol.In(list_of_ports),
+            }
+        )
+        return self.async_show_form(step_id="user", data_schema=schema)
--- tests/components/edl21/test_config_flow.py
+++ tests/components/edl21/test_config_flow.py
@@ -1,29 +1,83 @@
 """Tests for the EDL21 config flow."""
 from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.edl21.sensor import DOMAIN, CONF_SERIAL_PORT
+from homeassistant.components.edl21.config_flow import CONF_MANUAL_PATH
 from homeassistant.components.hassio import HassioServiceInfo
 from homeassistant.config_entries import SOURCE_USER
 from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
+import homeassistant.components.edl21
+from homeassistant.const import CONF_SOURCE
+
+from unittest.mock import AsyncMock, MagicMock, patch, sentinel
 
 from tests.common import MockConfigEntry
 
+import pytest
+import serial.tools.list_ports
 
-async def test_show_config_form(hass: HomeAssistant) -> None:
+
+def com_port():
+    """Mock of a serial port."""
+    port = serial.tools.list_ports_common.ListPortInfo("/dev/ttyUSB1234")
+    port.serial_number = "1234"
+    port.manufacturer = "Virtual serial port"
+    port.device = "/dev/ttyUSB1"
+    port.description = "Some serial port"
+
+    return port
+
+
+@patch("serial.tools.list_ports.comports", MagicMock(return_value=[]))
+async def test_no_port_config_form(hass: HomeAssistant) -> None:
     """Test that the setup form is served."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
+        DOMAIN, context={CONF_SOURCE: SOURCE_USER}
     )
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "user"
+    assert result["step_id"] == "pick_manual"
 
 
 async def test_with_data(hass: HomeAssistant) -> None:
     """Test that entry creation."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
-        context={"source": SOURCE_USER},
+        context={CONF_SOURCE: SOURCE_USER},
+        data={CONF_SERIAL_PORT: "/dev/ttyTEST", CONF_NAME: "TEST"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["data"][CONF_NAME] == "TEST"
+    assert result["data"][CONF_SERIAL_PORT] == "/dev/ttyTEST"
+
+
+@patch("serial.tools.list_ports.comports", MagicMock(return_value=[com_port()]))
+async def test_user_flow_ports(hass: HomeAssistant):
+    """Test user flow -- radio detected."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={CONF_SOURCE: SOURCE_USER}
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+
+
+async def test_with_manual(hass: HomeAssistant) -> None:
+    """Test that entry creation."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={CONF_SOURCE: SOURCE_USER},
+        data={CONF_SERIAL_PORT: CONF_MANUAL_PATH, CONF_NAME: "TEST"},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "pick_manual"
+
+
+async def test_with_data_manual(hass: HomeAssistant) -> None:
+    """Test that entry creation."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={CONF_SOURCE: "pick_manual"},
         data={CONF_SERIAL_PORT: "/dev/ttyTEST", CONF_NAME: "TEST"},
     )
 
