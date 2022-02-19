f3d5f162b8781c0b3f1f4a357775d1e48c1c8332
Maxim Oei <maxim.oei@teampicnic.com>
2022-02-20 00:04:31 +0100

    Update const.py

--- homeassistant/components/songpal/const.py
+++ homeassistant/components/songpal/const.py
@@ -1,5 +1,8 @@
 """Constants for the Songpal component."""
 DOMAIN = "songpal"
 SET_SOUND_SETTING = "set_sound_setting"
-
+CREATE_GROUP = "create_group"
+ABORT_GROUP = "abort_group"
 CONF_ENDPOINT = "endpoint"
+CONF_URL = "url"
+CONF_UUID = "uuid"
