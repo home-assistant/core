0e1431e086c350d847bcc4af2b5d4c53ff092be2
Ludeeus <ludeeus@ludeeus.dev>
2022-02-20 10:18:58 +0000

    Use GraphQL for GitHub integration

--- homeassistant/components/github/const.py
+++ homeassistant/components/github/const.py
@@ -3,9 +3,6 @@ from __future__ import annotations
 
 from datetime import timedelta
 from logging import Logger, getLogger
-from typing import NamedTuple
-
-from aiogithubapi import GitHubIssueModel
 
 LOGGER: Logger = getLogger(__package__)
 
@@ -18,12 +15,3 @@ DEFAULT_UPDATE_INTERVAL = timedelta(seconds=300)
 
 CONF_ACCESS_TOKEN = "access_token"
 CONF_REPOSITORIES = "repositories"
-
-
-class IssuesPulls(NamedTuple):
-    """Issues and pull requests."""
-
-    issues_count: int
-    issue_last: GitHubIssueModel | None
-    pulls_count: int
-    pull_last: GitHubIssueModel | None
