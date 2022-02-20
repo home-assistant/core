128d9a31bff2fb27bf1786f5565e66c1b3944156
Matthias Lohr <mail@mlohr.com>
2022-02-20 13:54:25 +0100

    added sensor entities for running timers

--- homeassistant/components/tolo/sensor.py
+++ homeassistant/components/tolo/sensor.py
@@ -1,8 +1,10 @@
 """TOLO Sauna (non-binary, general) sensors."""
+from __future__ import annotations
+
 from collections.abc import Callable
 from dataclasses import dataclass
 
-from tololib.message_info import StatusInfo
+from tololib.message_info import SettingsInfo, StatusInfo
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -11,7 +13,7 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import PERCENTAGE, TEMP_CELSIUS
+from homeassistant.const import PERCENTAGE, TEMP_CELSIUS, TIME_MINUTES
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -24,7 +26,8 @@ from .const import DOMAIN
 class ToloSensorEntityDescriptionBase:
     """Required values when describing TOLO Sensor entities."""
 
-    getter: Callable[[StatusInfo], int]
+    getter: Callable[[StatusInfo], int | None]
+    availability_checker: Callable[[SettingsInfo, StatusInfo], bool] | None
 
 
 @dataclass
@@ -32,6 +35,8 @@ class ToloSensorEntityDescription(
     SensorEntityDescription, ToloSensorEntityDescriptionBase
 ):
     """Class describing TOLO Sensor entities."""
+
+    state_class = SensorStateClass.MEASUREMENT
 
 
 SENSORS = (
@@ -41,8 +46,8 @@ SENSORS = (
         icon="mdi:waves-arrow-up",
         name="Water Level",
         native_unit_of_measurement=PERCENTAGE,
-        state_class=SensorStateClass.MEASUREMENT,
-        getter=lambda s: s.water_level_percent,
+        getter=lambda status: status.water_level_percent,
+        availability_checker=None,
     ),
     ToloSensorEntityDescription(
         key="tank_temperature",
@@ -50,8 +55,38 @@ SENSORS = (
         entity_category=EntityCategory.DIAGNOSTIC,
         name="Tank Temperature",
         native_unit_of_measurement=TEMP_CELSIUS,
-        state_class=SensorStateClass.MEASUREMENT,
-        getter=lambda s: s.tank_temperature,
+        getter=lambda status: status.tank_temperature,
+        availability_checker=None,
+    ),
+    ToloSensorEntityDescription(
+        key="power_timer_remaining",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        icon="mdi:power-settings",
+        name="Power Timer",
+        native_unit_of_measurement=TIME_MINUTES,
+        getter=lambda status: status.power_timer,
+        availability_checker=lambda settings, status: status.power_on
+        and settings.power_timer is not None,
+    ),
+    ToloSensorEntityDescription(
+        key="salt_bath_timer_remaining",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        icon="mdi:shaker-outline",
+        name="Salt Bath Timer",
+        native_unit_of_measurement=TIME_MINUTES,
+        getter=lambda status: status.salt_bath_timer,
+        availability_checker=lambda settings, status: status.salt_bath_on
+        and settings.salt_bath_timer is not None,
+    ),
+    ToloSensorEntityDescription(
+        key="fan_timer_remaining",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        icon="mdi:fan-auto",
+        name="Fan Timer",
+        native_unit_of_measurement=TIME_MINUTES,
+        getter=lambda status: status.fan_timer,
+        availability_checker=lambda settings, status: status.fan_on
+        and settings.fan_timer is not None,
     ),
 )
 
@@ -85,6 +120,15 @@ class ToloSensorEntity(ToloSaunaCoordinatorEntity, SensorEntity):
         self._attr_unique_id = f"{entry.entry_id}_{entity_description.key}"
 
     @property
-    def native_value(self) -> int:
+    def available(self) -> bool:
+        """Return availability of the TOLO sensor."""
+        if self.entity_description.availability_checker is None:
+            return super().available
+        return self.entity_description.availability_checker(
+            self.coordinator.data.settings, self.coordinator.data.status
+        )
+
+    @property
+    def native_value(self) -> int | None:
         """Return native value of the TOLO sensor."""
         return self.entity_description.getter(self.coordinator.data.status)
