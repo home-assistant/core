53055674763b95f45c22d83af921c583a746cb74
rikroe <rikroe@users.noreply.github.com>
2022-02-20 21:11:29 +0100

    Clear const.py

--- homeassistant/components/bmw_connected_drive/const.py
+++ homeassistant/components/bmw_connected_drive/const.py
@@ -12,11 +12,9 @@ ATTR_DIRECTION = "direction"
 
 CONF_ALLOWED_REGIONS = ["china", "north_america", "rest_of_world"]
 CONF_READ_ONLY = "read_only"
-CONF_USE_LOCATION = "use_location"
 
 CONF_ACCOUNT = "account"
 
-DATA_HASS_CONFIG = "hass_config"
 DATA_ENTRIES = "entries"
 
 UNIT_MAP = {
