e6af7847fc6106f069d556e6d76a55e1a8f600b7
Keilin Bickar <TrumpetGod@gmail.com>
2022-02-21 17:05:12 -0500

    Add Multi factor authentication support for Sense (#66498)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- tests/components/sense/test_config_flow.py
+++ tests/components/sense/test_config_flow.py
@@ -1,13 +1,44 @@
 """Test the Sense config flow."""
-from unittest.mock import patch
+from unittest.mock import AsyncMock, patch
 
-from sense_energy import SenseAPITimeoutException, SenseAuthenticationException
+import pytest
+from sense_energy import (
+    SenseAPITimeoutException,
+    SenseAuthenticationException,
+    SenseMFARequiredException,
+)
 
 from homeassistant import config_entries
 from homeassistant.components.sense.const import DOMAIN
+from homeassistant.const import CONF_CODE
+
+from tests.common import MockConfigEntry
+
+MOCK_CONFIG = {
+    "timeout": 6,
+    "email": "test-email",
+    "password": "test-password",
+    "access_token": "ABC",
+    "user_id": "123",
+    "monitor_id": "456",
+}
 
 
-async def test_form(hass):
+@pytest.fixture(name="mock_sense")
+def mock_sense():
+    """Mock Sense object for authenticatation."""
+    with patch(
+        "homeassistant.components.sense.config_flow.ASyncSenseable"
+    ) as mock_sense:
+        mock_sense.return_value.authenticate = AsyncMock(return_value=True)
+        mock_sense.return_value.validate_mfa = AsyncMock(return_value=True)
+        mock_sense.return_value.sense_access_token = "ABC"
+        mock_sense.return_value.sense_user_id = "123"
+        mock_sense.return_value.sense_monitor_id = "456"
+        yield mock_sense
+
+
+async def test_form(hass, mock_sense):
     """Test we get the form."""
 
     result = await hass.config_entries.flow.async_init(
@@ -16,7 +47,7 @@ async def test_form(hass):
     assert result["type"] == "form"
     assert result["errors"] == {}
 
-    with patch("sense_energy.ASyncSenseable.authenticate", return_value=True,), patch(
+    with patch(
         "homeassistant.components.sense.async_setup_entry",
         return_value=True,
     ) as mock_setup_entry:
@@ -28,11 +59,7 @@ async def test_form(hass):
 
     assert result2["type"] == "create_entry"
     assert result2["title"] == "test-email"
-    assert result2["data"] == {
-        "timeout": 6,
-        "email": "test-email",
-        "password": "test-password",
-    }
+    assert result2["data"] == MOCK_CONFIG
     assert len(mock_setup_entry.mock_calls) == 1
 
 
@@ -53,6 +80,113 @@ async def test_form_invalid_auth(hass):
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "invalid_auth"}
+
+
+async def test_form_mfa_required(hass, mock_sense):
+    """Test we handle invalid auth."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    mock_sense.return_value.authenticate.side_effect = SenseMFARequiredException
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"timeout": "6", "email": "test-email", "password": "test-password"},
+    )
+
+    assert result2["type"] == "form"
+    assert result2["step_id"] == "validation"
+
+    mock_sense.return_value.validate_mfa.side_effect = None
+    result3 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_CODE: "012345"},
+    )
+
+    assert result3["type"] == "create_entry"
+    assert result3["title"] == "test-email"
+    assert result3["data"] == MOCK_CONFIG
+
+
+async def test_form_mfa_required_wrong(hass, mock_sense):
+    """Test we handle invalid auth."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    mock_sense.return_value.authenticate.side_effect = SenseMFARequiredException
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"timeout": "6", "email": "test-email", "password": "test-password"},
+    )
+
+    assert result2["type"] == "form"
+    assert result2["step_id"] == "validation"
+
+    mock_sense.return_value.validate_mfa.side_effect = SenseAuthenticationException
+    # Try with the WRONG verification code give us the form back again
+    result3 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_CODE: "000000"},
+    )
+
+    assert result3["type"] == "form"
+    assert result3["errors"] == {"base": "invalid_auth"}
+    assert result3["step_id"] == "validation"
+
+
+async def test_form_mfa_required_timeout(hass, mock_sense):
+    """Test we handle invalid auth."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    mock_sense.return_value.authenticate.side_effect = SenseMFARequiredException
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"timeout": "6", "email": "test-email", "password": "test-password"},
+    )
+
+    assert result2["type"] == "form"
+    assert result2["step_id"] == "validation"
+
+    mock_sense.return_value.validate_mfa.side_effect = SenseAPITimeoutException
+    result3 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_CODE: "000000"},
+    )
+
+    assert result3["type"] == "form"
+    assert result3["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_mfa_required_exception(hass, mock_sense):
+    """Test we handle invalid auth."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    mock_sense.return_value.authenticate.side_effect = SenseMFARequiredException
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"timeout": "6", "email": "test-email", "password": "test-password"},
+    )
+
+    assert result2["type"] == "form"
+    assert result2["step_id"] == "validation"
+
+    mock_sense.return_value.validate_mfa.side_effect = Exception
+    result3 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_CODE: "000000"},
+    )
+
+    assert result3["type"] == "form"
+    assert result3["errors"] == {"base": "unknown"}
 
 
 async def test_form_cannot_connect(hass):
@@ -91,3 +225,57 @@ async def test_form_unknown_exception(hass):
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "unknown"}
+
+
+async def test_reauth_no_form(hass, mock_sense):
+    """Test reauth where no form needed."""
+
+    # set up initially
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data=MOCK_CONFIG,
+        unique_id="test-email",
+    )
+    entry.add_to_hass(hass)
+    with patch(
+        "homeassistant.config_entries.ConfigEntries.async_reload",
+        return_value=True,
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_REAUTH}, data=MOCK_CONFIG
+        )
+    assert result["type"] == "abort"
+    assert result["reason"] == "reauth_successful"
+
+
+async def test_reauth_password(hass, mock_sense):
+    """Test reauth form."""
+
+    # set up initially
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data=MOCK_CONFIG,
+        unique_id="test-email",
+    )
+    entry.add_to_hass(hass)
+    mock_sense.return_value.authenticate.side_effect = SenseAuthenticationException
+
+    # Reauth success without user input
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_REAUTH}, data=entry.data
+    )
+    assert result["type"] == "form"
+
+    mock_sense.return_value.authenticate.side_effect = None
+    with patch(
+        "homeassistant.components.sense.async_setup_entry",
+        return_value=True,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {"password": "test-password"},
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] == "abort"
+    assert result2["reason"] == "reauth_successful"
