fc764e4e69eace34083a9ee41884bcb338e41ca9
G Johansson <goran.johansson@shiftit.se>
2022-02-21 22:50:50 +0100

    Implement number platform for Sensibo (#66898)

--- homeassistant/components/sensibo/const.py
+++ homeassistant/components/sensibo/const.py
@@ -1,6 +1,10 @@
 """Constants for Sensibo."""
 
+import asyncio
 import logging
+
+from aiohttp.client_exceptions import ClientConnectionError
+from pysensibo.exceptions import AuthenticationError, SensiboError
 
 from homeassistant.const import Platform
 
@@ -8,7 +12,14 @@ LOGGER = logging.getLogger(__package__)
 
 DEFAULT_SCAN_INTERVAL = 60
 DOMAIN = "sensibo"
-PLATFORMS = [Platform.CLIMATE]
+PLATFORMS = [Platform.CLIMATE, Platform.NUMBER]
 ALL = ["all"]
 DEFAULT_NAME = "Sensibo"
 TIMEOUT = 8
+
+SENSIBO_ERRORS = (
+    ClientConnectionError,
+    asyncio.TimeoutError,
+    AuthenticationError,
+    SensiboError,
+)
