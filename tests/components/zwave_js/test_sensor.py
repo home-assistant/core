0983a0fc98f343a85d45a0ae2d3fe7f4149d9f93
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-02-22 06:03:01 -0500

    Log error when using zwave_js 'refresh_value' on ping button/node status sensor (#66847)
    * Raise when using 'zwave_js.refresh_value' on ping button
    
    * Revert test change
    
    * block till done
    
    * Switch from raising an exception to logging an error for both the ping button and node status sensor
    
    * missed commit

--- tests/components/zwave_js/test_sensor.py
+++ tests/components/zwave_js/test_sensor.py
@@ -15,6 +15,7 @@ from homeassistant.components.zwave_js.const import (
     ATTR_METER_TYPE_NAME,
     ATTR_VALUE,
     DOMAIN,
+    SERVICE_REFRESH_VALUE,
     SERVICE_RESET_METER,
 )
 from homeassistant.const import (
@@ -207,6 +208,7 @@ async def test_node_status_sensor_not_ready(
     lock_id_lock_as_id150_not_ready,
     lock_id_lock_as_id150_state,
     integration,
+    caplog,
 ):
     """Test node status sensor is created and available if node is not ready."""
     NODE_STATUS_ENTITY = "sensor.z_wave_module_for_id_lock_150_and_101_node_status"
@@ -233,6 +235,17 @@ async def test_node_status_sensor_not_ready(
     assert node.ready
     assert hass.states.get(NODE_STATUS_ENTITY)
     assert hass.states.get(NODE_STATUS_ENTITY).state == "alive"
+
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_REFRESH_VALUE,
+        {
+            ATTR_ENTITY_ID: NODE_STATUS_ENTITY,
+        },
+        blocking=True,
+    )
+
+    assert "There is no value to refresh for this entity" in caplog.text
 
 
 async def test_reset_meter(
