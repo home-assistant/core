c2e62e4d9f888ee56a02fd78ee98bbd192ed1074
Paulus Schoutsen <balloob@gmail.com>
2022-02-22 13:15:16 -0800

    Re-org device automations (#67064)
    Co-authored-by: Franck Nijhof <git@frenck.dev>

--- tests/helpers/test_script.py
+++ tests/helpers/test_script.py
@@ -3721,7 +3721,7 @@ async def test_platform_async_validate_action_config(hass):
     config = {CONF_DEVICE_ID: "test", CONF_DOMAIN: "test"}
     platform = AsyncMock()
     with patch(
-        "homeassistant.helpers.script.device_automation.async_get_device_automation_platform",
+        "homeassistant.components.device_automation.action.async_get_device_automation_platform",
         return_value=platform,
     ):
         platform.async_validate_action_config.return_value = config
