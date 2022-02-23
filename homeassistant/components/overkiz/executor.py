d049e147b33e12ce630d8c556252621582a76517
Mick Vleeshouwer <mick@imick.nl>
2022-02-23 10:46:08 -0800

    Fix RTS device delays in Overkiz integration (#67124)

--- homeassistant/components/overkiz/executor.py
+++ homeassistant/components/overkiz/executor.py
@@ -4,11 +4,21 @@ from __future__ import annotations
 from typing import Any
 from urllib.parse import urlparse
 
-from pyoverkiz.enums.command import OverkizCommand
+from pyoverkiz.enums import OverkizCommand, Protocol
 from pyoverkiz.models import Command, Device
 from pyoverkiz.types import StateType as OverkizStateType
 
 from .coordinator import OverkizDataUpdateCoordinator
+
+# Commands that don't support setting
+# the delay to another value
+COMMANDS_WITHOUT_DELAY = [
+    OverkizCommand.IDENTIFY,
+    OverkizCommand.OFF,
+    OverkizCommand.ON,
+    OverkizCommand.ON_WITH_TIMER,
+    OverkizCommand.TEST,
+]
 
 
 class OverkizExecutor:
@@ -58,6 +68,14 @@ class OverkizExecutor:
 
     async def async_execute_command(self, command_name: str, *args: Any) -> None:
         """Execute device command in async context."""
+        # Set the execution duration to 0 seconds for RTS devices on supported commands
+        # Default execution duration is 30 seconds and will block consecutive commands
+        if (
+            self.device.protocol == Protocol.RTS
+            and command_name not in COMMANDS_WITHOUT_DELAY
+        ):
+            args = args + (0,)
+
         exec_id = await self.coordinator.client.execute_command(
             self.device.device_url,
             Command(command_name, list(args)),
