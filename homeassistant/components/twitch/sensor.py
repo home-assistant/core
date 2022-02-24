bdd020c5d0218e191c40344dd20d96e4c96781a3
Mark Dietzer <git@doridian.net>
2022-02-23 19:55:08 -0800

    fix(twitch): Fix Twitch component to use new API

--- homeassistant/components/twitch/sensor.py
+++ homeassistant/components/twitch/sensor.py
@@ -3,12 +3,17 @@ from __future__ import annotations
 
 import logging
 
-from requests.exceptions import HTTPError
-from twitch import TwitchClient
+from twitchAPI.twitch import (
+    Twitch,
+    InvalidTokenException,
+    MissingScopeException,
+    TwitchAuthorizationException,
+    AuthScope,
+)
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
-from homeassistant.const import CONF_CLIENT_ID, CONF_TOKEN
+from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_TOKEN
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -33,9 +38,12 @@ ICON = "mdi:twitch"
 STATE_OFFLINE = "offline"
 STATE_STREAMING = "streaming"
 
+OAUTH_SCOPES = [AuthScope.USER_READ_SUBSCRIPTIONS]
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_CLIENT_ID): cv.string,
+        vol.Required(CONF_CLIENT_SECRET): cv.string,
         vol.Required(CONF_CHANNELS): vol.All(cv.ensure_list, [cv.string]),
         vol.Optional(CONF_TOKEN): cv.string,
     }
@@ -51,28 +59,50 @@ def setup_platform(
     """Set up the Twitch platform."""
     channels = config[CONF_CHANNELS]
     client_id = config[CONF_CLIENT_ID]
+    client_secret = config[CONF_CLIENT_SECRET]
     oauth_token = config.get(CONF_TOKEN)
-    client = TwitchClient(client_id, oauth_token)
+    client = Twitch(app_id=client_id, app_secret=client_secret)
+    client.auto_refresh_auth = False
 
     try:
-        client.ingests.get_server_list()
-    except HTTPError:
-        _LOGGER.error("Client ID or OAuth token is not valid")
+        client.authenticate_app(scope=OAUTH_SCOPES)
+    except TwitchAuthorizationException:
+        _LOGGER.error("INvalid client ID or client secret")
         return
 
-    channel_ids = client.users.translate_usernames_to_ids(channels)
+    if oauth_token:
+        try:
+            client.set_user_authentication(
+                token=oauth_token, scope=OAUTH_SCOPES, validate=True
+            )
+        except MissingScopeException:
+            _LOGGER.error("OAuth token is missing required scope")
+            return
+        except InvalidTokenException:
+            _LOGGER.error("OAuth token is invalid")
+            return
 
-    add_entities([TwitchSensor(channel_id, client) for channel_id in channel_ids], True)
+    channels = client.get_users(logins=channels)
+
+    add_entities(
+        [
+            TwitchSensor(
+                channel=channel, client=client, enable_user_auth=bool(oauth_token)
+            )
+            for channel in channels["data"]
+        ],
+        True,
+    )
 
 
 class TwitchSensor(SensorEntity):
     """Representation of an Twitch channel."""
 
-    def __init__(self, channel, client):
+    def __init__(self, channel, client: Twitch, enable_user_auth: bool):
         """Initialize the sensor."""
         self._client = client
         self._channel = channel
-        self._oauth_enabled = client._oauth_token is not None
+        self._enable_user_auth = enable_user_auth
         self._state = None
         self._preview = None
         self._game = None
@@ -84,7 +114,7 @@ class TwitchSensor(SensorEntity):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._channel.display_name
+        return self._channel["display_name"]
 
     @property
     def native_value(self):
@@ -101,7 +131,7 @@ class TwitchSensor(SensorEntity):
         """Return the state attributes."""
         attr = dict(self._statistics)
 
-        if self._oauth_enabled:
+        if self._enable_user_auth:
             attr.update(self._subscription)
             attr.update(self._follow)
 
@@ -112,7 +142,7 @@ class TwitchSensor(SensorEntity):
     @property
     def unique_id(self):
         """Return unique ID for this sensor."""
-        return self._channel.id
+        return self._channel["id"]
 
     @property
     def icon(self):
@@ -122,41 +152,49 @@ class TwitchSensor(SensorEntity):
     def update(self):
         """Update device state."""
 
-        channel = self._client.channels.get_by_id(self._channel.id)
+        followers = self._client.get_users_follows(to_id=self._channel["id"])["total"]
+        channel = self._client.get_users(user_ids=[self._channel["id"]])["data"][0]
 
         self._statistics = {
-            ATTR_FOLLOWING: channel.followers,
-            ATTR_VIEWS: channel.views,
+            ATTR_FOLLOWING: followers,
+            ATTR_VIEWS: channel["view_count"],
         }
-        if self._oauth_enabled:
-            user = self._client.users.get()
+        if self._enable_user_auth:
+            user = self._client.get_users()["data"][0]
 
-            try:
-                sub = self._client.users.check_subscribed_to_channel(
-                    user.id, self._channel.id
-                )
+            sub = self._client.check_user_subscription(
+                user_id=user["id"], broadcaster_id=self._channel["id"]
+            )
+            if sub["status"] == 200:
                 self._subscription = {
                     ATTR_SUBSCRIPTION: True,
-                    ATTR_SUBSCRIPTION_SINCE: sub.created_at,
-                    ATTR_SUBSCRIPTION_GIFTED: sub.is_gift,
+                    ATTR_SUBSCRIPTION_GIFTED: sub["data"]["is_gift"],
                 }
-            except HTTPError:
+            elif sub["status"] == 404:
                 self._subscription = {ATTR_SUBSCRIPTION: False}
-
-            try:
-                follow = self._client.users.check_follows_channel(
-                    user.id, self._channel.id
+            else:
+                raise Exception(
+                    f"Error response on check_user_subscription: {sub['error']}"
                 )
-                self._follow = {ATTR_FOLLOW: True, ATTR_FOLLOW_SINCE: follow.created_at}
-            except HTTPError:
+
+            follows = self._client.get_users_follows(
+                from_id=user["id"], to_id=self._channel["id"]
+            )["data"]
+            if len(follows) > 0:
+                self._follow = {
+                    ATTR_FOLLOW: True,
+                    ATTR_FOLLOW_SINCE: follows[0]["followed_at"],
+                }
+            else:
                 self._follow = {ATTR_FOLLOW: False}
 
-        stream = self._client.streams.get_stream_by_user(self._channel.id)
-        if stream:
-            self._game = stream.channel.get("game")
-            self._title = stream.channel.get("status")
-            self._preview = stream.preview.get("medium")
+        streams = self._client.get_streams(user_id=[self._channel["id"]])["data"]
+        if len(streams) > 0:
+            stream = streams[0]
+            self._game = stream["game_name"]
+            self._title = stream["title"]
+            self._preview = stream["thumbnail_url"]
             self._state = STATE_STREAMING
         else:
-            self._preview = self._channel.logo
+            self._preview = channel["offline_image_url"]
             self._state = STATE_OFFLINE
