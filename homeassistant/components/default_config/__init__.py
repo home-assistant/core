8bef4829cd544e13e878504ded326ecbdfafd2f7
Ludeeus <ludeeus@ludeeus.dev>
2022-02-24 11:14:52 +0000

    Add to default_config

--- homeassistant/components/default_config/__init__.py
+++ homeassistant/components/default_config/__init__.py
@@ -5,6 +5,9 @@ try:
 except ImportError:
     av = None
 
+from homeassistant.components.backup import DOMAIN as BACKUP_DOMAIN
+from homeassistant.components.hassio import is_hassio
+from homeassistant.components.stream import DOMAIN as STREAM_DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.setup import async_setup_component
@@ -14,7 +17,10 @@ DOMAIN = "default_config"
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Initialize default configuration."""
+    if not is_hassio(hass):
+        await async_setup_component(hass, BACKUP_DOMAIN, config)
+
     if av is None:
         return True
 
-    return await async_setup_component(hass, "stream", config)
+    return await async_setup_component(hass, STREAM_DOMAIN, config)
