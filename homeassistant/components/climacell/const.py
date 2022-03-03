1773fd309e27b5bc9b8cbe4f16b1fb1139d040c3
Robert Bergelt <bergeltr1@gmail.com>
2022-03-03 00:50:45 +0000

    Weather card was corrected to display km/h instead of m/s

--- homeassistant/components/climacell/const.py
+++ homeassistant/components/climacell/const.py
@@ -45,7 +45,7 @@ from homeassistant.const import (
     PERCENTAGE,
     PRESSURE_HPA,
     PRESSURE_INHG,
-    SPEED_METERS_PER_SECOND,
+    SPEED_KILOMETERS_PER_HOUR,
     SPEED_MILES_PER_HOUR,
     TEMP_CELSIUS,
     TEMP_FAHRENHEIT,
@@ -237,7 +237,7 @@ CC_SENSOR_TYPES = (
         key=CC_ATTR_WIND_GUST,
         name="Wind Gust",
         unit_imperial=SPEED_MILES_PER_HOUR,
-        unit_metric=SPEED_METERS_PER_SECOND,
+        unit_metric=SPEED_KILOMETERS_PER_HOUR,
         metric_conversion=lambda val: distance_convert(val, LENGTH_MILES, LENGTH_METERS)
         / 3600,
         is_metric_check=True,
