af7670a5a53c0e3dca25d606cd6856e1fd255b8d
G Johansson <goran.johansson@shiftit.se>
2022-03-05 22:37:44 +0100

    Add base entity for Sensibo (#67696)

--- homeassistant/components/sensibo/number.py
+++ homeassistant/components/sensibo/number.py
@@ -3,19 +3,16 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-import async_timeout
-
 from homeassistant.components.number import NumberEntity, NumberEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
-from homeassistant.helpers.entity import DeviceInfo, EntityCategory
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DOMAIN, LOGGER, SENSIBO_ERRORS, TIMEOUT
+from .const import DOMAIN
 from .coordinator import SensiboDataUpdateCoordinator
+from .entity import SensiboBaseEntity
 
 
 @dataclass
@@ -73,10 +70,9 @@ async def async_setup_entry(
     )
 
 
-class SensiboNumber(CoordinatorEntity, NumberEntity):
+class SensiboNumber(SensiboBaseEntity, NumberEntity):
     """Representation of a Sensibo numbers."""
 
-    coordinator: SensiboDataUpdateCoordinator
     entity_description: SensiboNumberEntityDescription
 
     def __init__(
@@ -86,24 +82,11 @@ class SensiboNumber(CoordinatorEntity, NumberEntity):
         entity_description: SensiboNumberEntityDescription,
     ) -> None:
         """Initiate Sensibo Number."""
-        super().__init__(coordinator)
+        super().__init__(coordinator, device_id)
         self.entity_description = entity_description
-        self._device_id = device_id
-        self._client = coordinator.client
         self._attr_unique_id = f"{device_id}-{entity_description.key}"
         self._attr_name = (
             f"{coordinator.data[device_id]['name']} {entity_description.name}"
-        )
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, coordinator.data[device_id]["id"])},
-            name=coordinator.data[device_id]["name"],
-            connections={(CONNECTION_NETWORK_MAC, coordinator.data[device_id]["mac"])},
-            manufacturer="Sensibo",
-            configuration_url="https://home.sensibo.com/",
-            model=coordinator.data[device_id]["model"],
-            sw_version=coordinator.data[device_id]["fw_ver"],
-            hw_version=coordinator.data[device_id]["fw_type"],
-            suggested_area=coordinator.data[device_id]["name"],
         )
 
     @property
@@ -114,17 +97,7 @@ class SensiboNumber(CoordinatorEntity, NumberEntity):
     async def async_set_value(self, value: float) -> None:
         """Set value for calibration."""
         data = {self.entity_description.remote_key: value}
-        try:
-            async with async_timeout.timeout(TIMEOUT):
-                result = await self._client.async_set_calibration(
-                    self._device_id,
-                    data,
-                )
-        except SENSIBO_ERRORS as err:
-            raise HomeAssistantError(
-                f"Failed to set calibration for device {self.name} to Sensibo servers: {err}"
-            ) from err
-        LOGGER.debug("Result: %s", result)
+        result = await self.async_send_command("set_calibration", {"data": data})
         if result["status"] == "success":
             self.coordinator.data[self._device_id][self.entity_description.key] = value
             self.async_write_ha_state()
