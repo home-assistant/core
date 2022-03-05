a8baebee8d4b05c779377e7e8de64751fb9793f3
G Johansson <goran.johansson@shiftit.se>
2022-03-05 22:42:03 +0100

    Return None for fields not reported in Sensibo (#67693)

--- homeassistant/components/sensibo/climate.py
+++ homeassistant/components/sensibo/climate.py
@@ -146,7 +146,7 @@ class SensiboClimate(SensiboBaseEntity, ClimateEntity):
         return features
 
     @property
-    def current_humidity(self) -> int:
+    def current_humidity(self) -> int | None:
         """Return the current humidity."""
         return self.coordinator.data[self.unique_id]["humidity"]
 
@@ -168,7 +168,7 @@ class SensiboClimate(SensiboBaseEntity, ClimateEntity):
         ]
 
     @property
-    def current_temperature(self) -> float:
+    def current_temperature(self) -> float | None:
         """Return the current temperature."""
         return convert_temperature(
             self.coordinator.data[self.unique_id]["temp"],
