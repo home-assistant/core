1c412189b15023fcb207c91d53328861a1eff9b4
Emil Burzo <contact@emilburzo.com>
2022-03-05 21:42:44 +0200

    improve nmap_tracker default options

--- homeassistant/components/nmap_tracker/const.py
+++ homeassistant/components/nmap_tracker/const.py
@@ -12,6 +12,6 @@ NMAP_TRACKED_DEVICES: Final = "nmap_tracked_devices"
 # Interval in minutes to exclude devices from a scan while they are home
 CONF_HOME_INTERVAL: Final = "home_interval"
 CONF_OPTIONS: Final = "scan_options"
-DEFAULT_OPTIONS: Final = "-F -T4 --min-rate 10 --host-timeout 5s"
+DEFAULT_OPTIONS: Final = "-n -sn --disable-arp-ping --host-timeout 5s"
 
 TRACKER_SCAN_INTERVAL: Final = 120
