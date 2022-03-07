aba862e90d13268e4ce68a3f919a15c7908f8e67
Álvaro Fernández Rojas <noltari@gmail.com>
2022-03-07 21:05:08 +0100

    airzone: remove triple loop as requested by @tkdrob
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- homeassistant/components/airzone/sensor.py
+++ homeassistant/components/airzone/sensor.py
@@ -1,7 +1,7 @@
 """Support for the Airzone sensors."""
 from __future__ import annotations
 
-from aioairzone.const import AZD_NAME, AZD_SYSTEMS, AZD_ZONES
+from aioairzone.const import AZD_NAME, AZD_ZONES
 
 from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
@@ -21,28 +21,26 @@ async def async_setup_entry(
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
     sensors = []
-    for system_id, system_data in coordinator.data[AZD_SYSTEMS].items():
-        for zone_id, zone_data in system_data[AZD_ZONES].items():
-            zone_name = zone_data[AZD_NAME]
+    for zone_id, zone_data in coordinator.data[AZD_ZONES].items():
+        zone_name = zone_data[AZD_NAME]
 
-            device_info: DeviceInfo = {
-                "identifiers": {(DOMAIN, f"{system_id},{zone_id}")},
-                "manufacturer": MANUFACTURER,
-                "name": f"Airzone [{system_id}:{zone_id}] {zone_name}",
-            }
+        device_info: DeviceInfo = {
+            "identifiers": {(DOMAIN, zone_id)},
+            "manufacturer": MANUFACTURER,
+            "name": f"Airzone [{zone_id}] {zone_name}",
+        }
 
-            for description in SENSOR_TYPES:
-                if description.key in zone_data:
-                    sensors.append(
-                        AirzoneSensor(
-                            coordinator,
-                            description,
-                            device_info,
-                            system_id,
-                            zone_id,
-                            zone_name,
-                        )
+        for description in SENSOR_TYPES:
+            if description.key in zone_data:
+                sensors.append(
+                    AirzoneSensor(
+                        coordinator,
+                        description,
+                        device_info,
+                        zone_id,
+                        zone_name,
                     )
+                )
 
     async_add_entities(sensors, False)
 
@@ -55,7 +53,6 @@ class AirzoneSensor(CoordinatorEntity, SensorEntity):
         coordinator: AirzoneUpdateCoordinator,
         description: SensorEntityDescription,
         device_info: DeviceInfo,
-        system_id: int,
         zone_id: int,
         zone_name: str,
     ) -> None:
@@ -63,14 +60,16 @@ class AirzoneSensor(CoordinatorEntity, SensorEntity):
         super().__init__(coordinator)
         self._attr_device_info = device_info
         self._attr_name = f"{zone_name} {description.name}"
-        self._attr_unique_id = f"airzone_{system_id}_{zone_id}_{description.key}"
+        self._attr_unique_id = f"airzone_{zone_id}_{description.key}"
         self.entity_description = description
-        self.system_id = system_id
         self.zone_id = zone_id
 
     @property
     def native_value(self):
         """Return the state."""
-        system = self.coordinator.data[AZD_SYSTEMS][self.system_id]
-        zone = system[AZD_ZONES][self.zone_id]
-        return zone[self.entity_description.key]
+        value = None
+        if self.zone_id in self.coordinator.data[AZD_ZONES]:
+            zone = self.coordinator.data[AZD_ZONES][self.zone_id]
+            if self.entity_description.key in zone:
+                value = zone[self.entity_description.key]
+        return value
