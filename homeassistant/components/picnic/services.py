30fc4c39b1d61b1091e00dd8f42426a7caf6db7d
corneyl <cornieljoosse@gmail.com>
2022-03-07 22:15:02 +0100

    Fix pre-commit check issues

--- homeassistant/components/picnic/services.py
+++ homeassistant/components/picnic/services.py
@@ -1,20 +1,30 @@
+"""Services for the Picnic integration."""
+
 from python_picnic_api import PicnicAPI
 import voluptuous as vol
 
 from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.helpers import device_registry
 import homeassistant.helpers.config_validation as cv
-from .const import ATTR_AMOUNT, ATTR_DEVICE_ID, ATTR_PRODUCT_ID, ATTR_PRODUCT_NAME, CONF_API, DOMAIN, \
-    PRODUCT_DECORATOR_LABEL, \
-    PRODUCT_DECORATOR_MAPPING, \
-    PRODUCT_DECORATOR_PRICE, \
-    PRODUCT_DECORATOR_VALIDITY, \
-    SERVICE_ADD_PRODUCT_TO_CART, \
-    SERVICE_SEARCH_PRODUCT
+
+from .const import (
+    ATTR_AMOUNT,
+    ATTR_DEVICE_ID,
+    ATTR_PRODUCT_ID,
+    ATTR_PRODUCT_NAME,
+    CONF_API,
+    DOMAIN,
+    PRODUCT_DECORATOR_LABEL,
+    PRODUCT_DECORATOR_MAPPING,
+    PRODUCT_DECORATOR_PRICE,
+    PRODUCT_DECORATOR_VALIDITY,
+    SERVICE_ADD_PRODUCT_TO_CART,
+    SERVICE_SEARCH_PRODUCT,
+)
 
 
 class PicnicServiceException(Exception):
-    """Exception for Picnic services"""
+    """Exception for Picnic services."""
 
 
 async def async_register_services(hass: HomeAssistant) -> None:
@@ -22,32 +32,48 @@ async def async_register_services(hass: HomeAssistant) -> None:
 
     # Register the add_product service
     if not hass.services.has_service(DOMAIN, SERVICE_ADD_PRODUCT_TO_CART):
+
         async def async_add_product_service(call: ServiceCall):
             # Get the picnic API client and call the handler with api client and hass
-            api_client: PicnicAPI = await get_api_client(hass, call.data.get('device_id'))
+            api_client: PicnicAPI = await get_api_client(
+                hass, call.data.get("device_id")
+            )
             await handle_add_product(hass, api_client, call)
 
         hass.services.async_register(
-            DOMAIN, SERVICE_ADD_PRODUCT_TO_CART, async_add_product_service, schema=vol.Schema({
-                vol.Optional(ATTR_DEVICE_ID): cv.string,
-                vol.Optional(ATTR_PRODUCT_ID): cv.positive_int,
-                vol.Optional(ATTR_PRODUCT_NAME): cv.string,
-                vol.Optional(ATTR_AMOUNT): cv.positive_int,
-            }),
+            DOMAIN,
+            SERVICE_ADD_PRODUCT_TO_CART,
+            async_add_product_service,
+            schema=vol.Schema(
+                {
+                    vol.Optional(ATTR_DEVICE_ID): cv.string,
+                    vol.Optional(ATTR_PRODUCT_ID): cv.positive_int,
+                    vol.Optional(ATTR_PRODUCT_NAME): cv.string,
+                    vol.Optional(ATTR_AMOUNT): cv.positive_int,
+                }
+            ),
         )
 
     # Register the search service
     if not hass.services.has_service(DOMAIN, SERVICE_SEARCH_PRODUCT):
+
         async def async_search_product_service(call: ServiceCall):
             # Get the picnic API client and call the handler with api client and hass
-            api_client: PicnicAPI = await get_api_client(hass, call.data.get('device_id'))
+            api_client: PicnicAPI = await get_api_client(
+                hass, call.data.get("device_id")
+            )
             await handle_search(hass, api_client, call)
 
         hass.services.async_register(
-            DOMAIN, SERVICE_SEARCH_PRODUCT, async_search_product_service, schema=vol.Schema({
-                vol.Optional(ATTR_DEVICE_ID): cv.string,
-                vol.Required(ATTR_PRODUCT_NAME): cv.string,
-            }),
+            DOMAIN,
+            SERVICE_SEARCH_PRODUCT,
+            async_search_product_service,
+            schema=vol.Schema(
+                {
+                    vol.Optional(ATTR_DEVICE_ID): cv.string,
+                    vol.Required(ATTR_PRODUCT_NAME): cv.string,
+                }
+            ),
         )
 
 
@@ -61,23 +87,30 @@ async def get_api_client(hass, device_id: str = None):
     registry = await device_registry.async_get_registry(hass)
     device = registry.async_get(device_id)
 
+    if device is None:
+        raise PicnicServiceException(f"Device with id {device_id} not found!")
+
     # Get Picnic API client for the config entry id
     try:
         config_entry_id = next(iter(device.config_entries))
         return hass.data[DOMAIN][config_entry_id][CONF_API]
-    except (AttributeError, StopIteration, KeyError):
-        raise PicnicServiceException(f"Device with id {device_id} not found!")
+    except (AttributeError, StopIteration, KeyError) as error:
+        raise PicnicServiceException(
+            f"Device with id {device_id} not found!"
+        ) from error
 
 
-async def handle_add_product(hass: HomeAssistant, api_client: PicnicAPI, call: ServiceCall) -> None:
+async def handle_add_product(
+    hass: HomeAssistant, api_client: PicnicAPI, call: ServiceCall
+) -> None:
     """Handle the call for the add_product service."""
-    product_id = call.data.get('product_id')
+    product_id = call.data.get("product_id")
     if not product_id:
         search_results = await hass.async_add_executor_job(
-            _product_search, api_client, call.data.get('product_name')
+            _product_search, api_client, call.data.get("product_name")
         )
         try:
-            product_id = search_results[0]['id']
+            product_id = search_results[0]["id"]
         except (IndexError, KeyError, TypeError):
             product_id = None
 
@@ -85,17 +118,19 @@ async def handle_add_product(hass: HomeAssistant, api_client: PicnicAPI, call: S
         raise PicnicServiceException("No product found or no product ID given!")
 
     await hass.async_add_executor_job(
-        api_client.add_product, str(product_id), call.data.get('amount', 1)
+        api_client.add_product, str(product_id), call.data.get("amount", 1)
     )
 
 
-async def handle_search(hass: HomeAssistant, api_client: PicnicAPI, call: ServiceCall) -> None:
+async def handle_search(
+    hass: HomeAssistant, api_client: PicnicAPI, call: ServiceCall
+) -> None:
     """Handle the call for the search service."""
     products = await hass.async_add_executor_job(
-        _product_search, api_client, call.data.get('product_name')
+        _product_search, api_client, call.data.get("product_name")
     )
 
-    products_dict = {p['id']: p for p in products[:5]}
+    products_dict = {p["id"]: p for p in products[:5]}
     hass.bus.async_fire("picnic_search_result", products_dict)
 
 
@@ -105,30 +140,35 @@ def _product_search(api_client: PicnicAPI, product_name: str):
     search_result = api_client.search(product_name)
 
     # Return empty list if the result doesn't contain items
-    if not search_result or 'items' not in search_result[0]:
+    if not search_result or "items" not in search_result[0]:
         return []
 
     # Curate a list of Product objects
     products = []
-    for item in search_result[0]['items']:
-        if 'name' in item:
+    for item in search_result[0]["items"]:
+        if "name" in item:
             # Set the base values
             product = {
-                'id': item['id'],
-                'name': item['name'],
-                'price': item['display_price'] / 100,
-                'quantity': item['unit_quantity'],
+                "id": item["id"],
+                "name": item["name"],
+                "price": item["display_price"] / 100,
+                "quantity": item["unit_quantity"],
             }
             # Get the known decorators based on the mapping
             decorators = {
-                d['type']: d[PRODUCT_DECORATOR_MAPPING[d['type']]]
-                for d in item.get('decorators', []) if d['type'] in PRODUCT_DECORATOR_MAPPING
+                d["type"]: d[PRODUCT_DECORATOR_MAPPING[d["type"]]]
+                for d in item.get("decorators", [])
+                if d["type"] in PRODUCT_DECORATOR_MAPPING
             }
             # If a price decorator is present, then the item has a discount. Add this to the product
             if PRODUCT_DECORATOR_PRICE in decorators:
-                product['discount_price'] = decorators[PRODUCT_DECORATOR_PRICE] / 100
-                product['discount_label'] = decorators.get(PRODUCT_DECORATOR_LABEL, '').lower()
-                product['discount_validity'] = decorators.get(PRODUCT_DECORATOR_VALIDITY)
+                product["discount_price"] = decorators[PRODUCT_DECORATOR_PRICE] / 100
+                product["discount_label"] = decorators.get(
+                    PRODUCT_DECORATOR_LABEL, ""
+                ).lower()
+                product["discount_validity"] = decorators.get(
+                    PRODUCT_DECORATOR_VALIDITY
+                )
 
             products += [product]
 
