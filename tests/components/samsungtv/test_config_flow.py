2d4ccddd99bcc4bd314f813214be693788b1c0bf
epenet <6771947+epenet@users.noreply.github.com>
2022-03-08 07:53:59 +0100

    Fix SSDP unique id in SamsungTV config flow (#67811)
    Co-authored-by: epenet <epenet@users.noreply.github.com>
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- tests/components/samsungtv/test_config_flow.py
+++ tests/components/samsungtv/test_config_flow.py
@@ -339,7 +339,7 @@ async def test_ssdp(hass: HomeAssistant) -> None:
         assert result["data"][CONF_NAME] == "fake_name"
         assert result["data"][CONF_MANUFACTURER] == "Samsung fake_manufacturer"
         assert result["data"][CONF_MODEL] == "fake_model"
-        assert result["result"].unique_id == "0d1cef00-00dc-1000-9c80-4844f7b172de"
+        assert result["result"].unique_id == "123"
 
 
 @pytest.mark.usefixtures("remote")
@@ -373,7 +373,7 @@ async def test_ssdp_noprefix(hass: HomeAssistant) -> None:
             assert result["data"][CONF_NAME] == "fake2_name"
             assert result["data"][CONF_MANUFACTURER] == "Samsung fake2_manufacturer"
             assert result["data"][CONF_MODEL] == "fake2_model"
-            assert result["result"].unique_id == "0d1cef00-00dc-1000-9c80-4844f7b172df"
+            assert result["result"].unique_id == "345"
 
 
 @pytest.mark.usefixtures("remotews")
@@ -448,7 +448,7 @@ async def test_ssdp_websocket_success_populates_mac_address(
     assert result["data"][CONF_MAC] == "aa:bb:ww:ii:ff:ii"
     assert result["data"][CONF_MANUFACTURER] == "Samsung fake_manufacturer"
     assert result["data"][CONF_MODEL] == "82GXARRS"
-    assert result["result"].unique_id == "0d1cef00-00dc-1000-9c80-4844f7b172de"
+    assert result["result"].unique_id == "be9554b9-c9fb-41f4-8920-22da015376a4"
 
 
 async def test_ssdp_websocket_not_supported(
@@ -591,7 +591,7 @@ async def test_ssdp_already_configured(hass: HomeAssistant) -> None:
         assert result2["reason"] == RESULT_ALREADY_CONFIGURED
 
         # check updated device info
-        assert entry.unique_id == "0d1cef00-00dc-1000-9c80-4844f7b172de"
+        assert entry.unique_id == "123"
 
 
 @pytest.mark.usefixtures("remote")
@@ -1013,7 +1013,7 @@ async def test_update_old_entry(hass: HomeAssistant) -> None:
         # check updated device info
         assert entry2.data.get(CONF_ID) is not None
         assert entry2.data.get(CONF_IP_ADDRESS) is not None
-        assert entry2.unique_id == "0d1cef00-00dc-1000-9c80-4844f7b172de"
+        assert entry2.unique_id == "be9554b9-c9fb-41f4-8920-22da015376a4"
 
 
 @pytest.mark.usefixtures("remotews")
@@ -1099,7 +1099,7 @@ async def test_update_missing_mac_unique_id_added_from_ssdp(
     assert result["type"] == "abort"
     assert result["reason"] == "already_configured"
     assert entry.data[CONF_MAC] == "aa:bb:ww:ii:ff:ii"
-    assert entry.unique_id == "0d1cef00-00dc-1000-9c80-4844f7b172de"
+    assert entry.unique_id == "be9554b9-c9fb-41f4-8920-22da015376a4"
 
 
 @pytest.mark.usefixtures("remotews")
@@ -1308,3 +1308,69 @@ async def test_form_reauth_websocket_not_supported(hass: HomeAssistant) -> None:
 
     assert result2["type"] == "abort"
     assert result2["reason"] == "not_supported"
+
+
+@pytest.mark.usefixtures("remotews")
+async def test_update_incorrect_udn_matching_upnp_udn_unique_id_added_from_ssdp(
+    hass: HomeAssistant,
+) -> None:
+    """Test updating the wrong udn from ssdp via upnp udn match."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data=MOCK_OLD_ENTRY,
+        unique_id="0d1cef00-00dc-1000-9c80-4844f7b172de",
+    )
+    entry.add_to_hass(hass)
+    with patch(
+        "homeassistant.components.samsungtv.async_setup",
+        return_value=True,
+    ) as mock_setup, patch(
+        "homeassistant.components.samsungtv.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_SSDP},
+            data=MOCK_SSDP_DATA,
+        )
+        await hass.async_block_till_done()
+        assert len(mock_setup.mock_calls) == 1
+        assert len(mock_setup_entry.mock_calls) == 1
+
+    assert result["type"] == "abort"
+    assert result["reason"] == "already_configured"
+    assert entry.data[CONF_MAC] == "aa:bb:ww:ii:ff:ii"
+    assert entry.unique_id == "be9554b9-c9fb-41f4-8920-22da015376a4"
+
+
+@pytest.mark.usefixtures("remotews")
+async def test_update_incorrect_udn_matching_mac_unique_id_added_from_ssdp(
+    hass: HomeAssistant,
+) -> None:
+    """Test updating the wrong udn from ssdp via mac match."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={**MOCK_OLD_ENTRY, CONF_MAC: "aa:bb:ww:ii:ff:ii"},
+        unique_id=None,
+    )
+    entry.add_to_hass(hass)
+    with patch(
+        "homeassistant.components.samsungtv.async_setup",
+        return_value=True,
+    ) as mock_setup, patch(
+        "homeassistant.components.samsungtv.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_SSDP},
+            data=MOCK_SSDP_DATA,
+        )
+        await hass.async_block_till_done()
+        assert len(mock_setup.mock_calls) == 1
+        assert len(mock_setup_entry.mock_calls) == 1
+
+    assert result["type"] == "abort"
+    assert result["reason"] == "already_configured"
+    assert entry.data[CONF_MAC] == "aa:bb:ww:ii:ff:ii"
+    assert entry.unique_id == "be9554b9-c9fb-41f4-8920-22da015376a4"
