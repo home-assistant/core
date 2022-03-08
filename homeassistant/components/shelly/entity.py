a6e62c123e7427f0a629c333df070a095d940a75
Shay Levy <levyshay1@gmail.com>
2022-03-08 11:58:08 +0200

    Add Shelly gen2 cover support (#67705)
    * Add Shelly gen2 cover support
    
    * Make status property

--- homeassistant/components/shelly/entity.py
+++ homeassistant/components/shelly/entity.py
@@ -351,6 +351,11 @@ class ShellyRpcEntity(entity.Entity):
         """Available."""
         return self.wrapper.device.connected
 
+    @property
+    def status(self) -> dict:
+        """Device status by entity key."""
+        return cast(dict, self.wrapper.device.status[self.key])
+
     async def async_added_to_hass(self) -> None:
         """When entity is added to HASS."""
         self.async_on_remove(self.wrapper.async_add_listener(self._update_callback))
