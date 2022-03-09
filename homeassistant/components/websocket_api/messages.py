409ecbe43af574dd503ebad52cf94ab56f2cfff1
J. Nick Koston <nick@koston.org>
2022-03-09 08:18:47 +0100

    POC: Reduce websocket payloads by ~50%

--- homeassistant/components/websocket_api/messages.py
+++ homeassistant/components/websocket_api/messages.py
@@ -1,13 +1,14 @@
 """Message templates for websocket commands."""
 from __future__ import annotations
 
+from copy import copy
 from functools import lru_cache
 import logging
 from typing import Any, Final
 
 import voluptuous as vol
 
-from homeassistant.core import Event
+from homeassistant.core import Context, Event
 from homeassistant.helpers import config_validation as cv
 from homeassistant.util.json import (
     find_paths_unserializable_data,
@@ -30,6 +31,19 @@ BASE_COMMAND_MESSAGE_SCHEMA: Final = vol.Schema({vol.Required("id"): cv.positive
 
 IDEN_TEMPLATE: Final = "__IDEN__"
 IDEN_JSON_TEMPLATE: Final = '"__IDEN__"'
+
+NULL_DICT: dict[str, str] = {}
+
+
+class NullContext(Context):
+    """A null context."""
+
+    def as_dict(self) -> dict:
+        """Return an empty dict for a null context."""
+        return NULL_DICT
+
+
+NULL_CONTEXT = NullContext()
 
 
 def result_message(iden: int, result: Any = None) -> dict[str, Any]:
@@ -72,6 +86,46 @@ def _cached_event_message(event: Event) -> str:
     with the actual iden in cached_event_message
     """
     return message_to_json(event_message(IDEN_TEMPLATE, event))
+
+
+def cached_state_changed_event_message(iden: int, event: Event) -> str:
+    """Return an event message.
+
+    Serialize to json once per message.
+
+    Since we can have many clients connected that are
+    all getting many of the same events (mostly state changed)
+    we can avoid serializing the same data for each connection.
+    """
+    return _cached_state_changed_event_message(event).replace(
+        IDEN_JSON_TEMPLATE, str(iden), 1
+    )
+
+
+@lru_cache(maxsize=128)
+def _cached_state_changed_event_message(event: Event) -> str:
+    """Cache and serialize the event to json.
+
+    The IDEN_TEMPLATE is used which will be replaced
+    with the actual iden in cached_event_message
+    """
+    return message_to_json(
+        event_message(IDEN_TEMPLATE, _minimal_state_changed_event(event))
+    )
+
+
+def _minimal_state_changed_event(event: Event) -> Event:
+    """Convert a state_changed event to the minimal version."""
+    # We likely don't need event_copy.time_fired either since last_updated on the new_state is the same
+    # We only need event.context as its the same for the new_state
+    # The entity_id is also duplicated in the message twice but its actually used
+    new_state = copy(event.data["new_state"])
+    event_copy = copy(event)
+    new_state.context = NULL_CONTEXT
+    event_copy.data["old_state"] = None
+    event_copy.data["context"] = NULL_CONTEXT
+    event_copy.data["new_state"] = new_state
+    return event_copy
 
 
 def message_to_json(message: dict[str, Any]) -> str:
