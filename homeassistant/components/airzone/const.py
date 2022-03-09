542c3cbf90a8daca58ba5438b36d2ed3e59cb486
Álvaro Fernández Rojas <noltari@gmail.com>
2022-03-09 20:51:39 +0100

    Address late Airzone PR comments and fix Fahrenheit temperatures (#67904)
    * airzone: address late PR comments
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    
    * airzone: fix fahrenheit temperatures
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    
    * airzone: add changes suggested by @MartinHjelmare
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- homeassistant/components/airzone/const.py
+++ homeassistant/components/airzone/const.py
@@ -2,8 +2,17 @@
 
 from typing import Final
 
+from aioairzone.common import TemperatureUnit
+
+from homeassistant.const import TEMP_CELSIUS, TEMP_FAHRENHEIT
+
 DOMAIN: Final = "airzone"
 MANUFACTURER: Final = "Airzone"
 
 AIOAIRZONE_DEVICE_TIMEOUT_SEC: Final = 10
 DEFAULT_LOCAL_API_PORT: Final = 3000
+
+TEMP_UNIT_LIB_TO_HASS: Final[dict[TemperatureUnit, str]] = {
+    TemperatureUnit.CELSIUS: TEMP_CELSIUS,
+    TemperatureUnit.FAHRENHEIT: TEMP_FAHRENHEIT,
+}
--- tests/components/airzone/test_init.py
+++ tests/components/airzone/test_init.py
@@ -13,15 +13,15 @@ from tests.common import MockConfigEntry
 async def test_unload_entry(hass):
     """Test unload."""
 
+    config_entry = MockConfigEntry(
+        domain=DOMAIN, unique_id="airzone_unique_id", data=CONFIG
+    )
+    config_entry.add_to_hass(hass)
+
     with patch(
         "aioairzone.localapi_device.AirzoneLocalApi.get_hvac",
         return_value=HVAC_MOCK,
     ):
-        config_entry = MockConfigEntry(
-            domain=DOMAIN, unique_id="airzone_unique_id", data=CONFIG
-        )
-        config_entry.add_to_hass(hass)
-
         assert await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
         assert config_entry.state is ConfigEntryState.LOADED
