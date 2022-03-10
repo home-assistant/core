e43c8b513ef932519923c1832ea4cff14887af32
Erik Montnemery <erik@montnemery.com>
2022-03-10 21:26:35 +0100

    Listen to entity registry events for wrapped switch in switch_as_x (#67962)
    * Listen to entity registry events for wrapped switch in switch_as_x
    
    * Simplify test

--- homeassistant/components/switch_as_x/__init__.py
+++ homeassistant/components/switch_as_x/__init__.py
@@ -7,8 +7,9 @@ import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ENTITY_ID
-from homeassistant.core import HomeAssistant
+from homeassistant.core import Event, HomeAssistant
 from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers.event import async_track_entity_registry_updated_event
 
 from .light import LightSwitch
 
@@ -23,7 +24,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up a config entry."""
     registry = er.async_get(hass)
     try:
-        er.async_validate_entity_id(registry, entry.options[CONF_ENTITY_ID])
+        entity_id = er.async_validate_entity_id(registry, entry.options[CONF_ENTITY_ID])
     except vol.Invalid:
         # The entity is identified by an unknown entity registry ID
         _LOGGER.error(
@@ -31,6 +32,24 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             entry.options[CONF_ENTITY_ID],
         )
         return False
+
+    async def async_registry_updated(event: Event) -> None:
+        """Handle entity registry update."""
+        data = event.data
+        if data["action"] == "remove":
+            await hass.config_entries.async_remove(entry.entry_id)
+
+        if data["action"] != "update" or "entity_id" not in data["changes"]:
+            return
+
+        # Entity_id changed, reload the config entry
+        await hass.config_entries.async_reload(entry.entry_id)
+
+    entry.async_on_unload(
+        async_track_entity_registry_updated_event(
+            hass, entity_id, async_registry_updated
+        )
+    )
 
     hass.config_entries.async_setup_platforms(entry, (entry.options["target_domain"],))
     return True
