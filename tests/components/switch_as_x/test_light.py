605fe3de9e54776a73439e91f5e71f0815acd82e
Erik <erik@montnemery.com>
2022-03-10 13:52:25 +0100

    Correct test

--- tests/components/switch_as_x/test_light.py
+++ tests/components/switch_as_x/test_light.py
@@ -155,14 +155,15 @@ async def test_config_entry_uuid(hass: HomeAssistant, target_domain):
     assert hass.states.get(f"{target_domain}.abc")
 
 
-async def test_config_entry_unregistered_uuid(hass: HomeAssistant):
+@pytest.mark.parametrize("target_domain", ("light",))
+async def test_config_entry_unregistered_uuid(hass: HomeAssistant, target_domain):
     """Test light switch setup from config entry with unknown entity registry id."""
     fake_uuid = "a266a680b608c32770e6c45bfe6b8411"
 
     config_entry = MockConfigEntry(
         data={},
         domain=DOMAIN,
-        options={"entity_id": fake_uuid},
+        options={"entity_id": fake_uuid, "target_domain": target_domain},
         title="ABC",
     )
 
