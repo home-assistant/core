7559b4a837ea4c2b176f928f110bc9f996857b75
Erik Montnemery <erik@montnemery.com>
2022-03-10 16:18:26 +0100

    Add helper to set name of helper config entries (#67950)

--- tests/components/switch_as_x/test_light.py
+++ tests/components/switch_as_x/test_light.py
@@ -107,34 +107,6 @@ async def test_switch_service_calls(hass):
 
 
 @pytest.mark.parametrize("target_domain", ("light",))
-async def test_config_entry(hass: HomeAssistant, target_domain):
-    """Test light switch setup from config entry."""
-    config_entry = MockConfigEntry(
-        data={},
-        domain=DOMAIN,
-        options={"entity_id": "switch.abc", "target_domain": target_domain},
-        title="ABC",
-    )
-
-    config_entry.add_to_hass(hass)
-
-    assert await hass.config_entries.async_setup(config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    assert DOMAIN in hass.config.components
-
-    state = hass.states.get(f"{target_domain}.abc")
-    assert state.state == "unavailable"
-    # Name copied from config entry title
-    assert state.name == "ABC"
-
-    # Check the light is added to the entity registry
-    registry = er.async_get(hass)
-    entity_entry = registry.async_get(f"{target_domain}.abc")
-    assert entity_entry.unique_id == config_entry.entry_id
-
-
-@pytest.mark.parametrize("target_domain", ("light",))
 async def test_config_entry_uuid(hass: HomeAssistant, target_domain):
     """Test light switch setup from config entry with entity registry id."""
     registry = er.async_get(hass)
