318356bf796a4ea7da54740a42a9094d3725a48a
Erik <erik@montnemery.com>
2022-03-10 14:58:13 +0100

    Move test instead of duplicating it

--- tests/components/switch_as_x/test_init.py
+++ tests/components/switch_as_x/test_init.py
@@ -1,18 +1,21 @@
 """Tests for the Switch as X."""
+import pytest
+
 from homeassistant.components.switch_as_x import DOMAIN
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
 
 
-async def test_config_entry_unregistered_uuid(hass: HomeAssistant):
+@pytest.mark.parametrize("target_domain", ("light",))
+async def test_config_entry_unregistered_uuid(hass: HomeAssistant, target_domain):
     """Test light switch setup from config entry with unknown entity registry id."""
     fake_uuid = "a266a680b608c32770e6c45bfe6b8411"
 
     config_entry = MockConfigEntry(
         data={},
         domain=DOMAIN,
-        options={"entity_id": fake_uuid},
+        options={"entity_id": fake_uuid, "target_domain": target_domain},
         title="ABC",
     )
 
--- tests/components/switch_as_x/test_light.py
+++ tests/components/switch_as_x/test_light.py
@@ -153,23 +153,3 @@ async def test_config_entry_uuid(hass: HomeAssistant, target_domain):
     await hass.async_block_till_done()
 
     assert hass.states.get(f"{target_domain}.abc")
-
-
-@pytest.mark.parametrize("target_domain", ("light",))
-async def test_config_entry_unregistered_uuid(hass: HomeAssistant, target_domain):
-    """Test light switch setup from config entry with unknown entity registry id."""
-    fake_uuid = "a266a680b608c32770e6c45bfe6b8411"
-
-    config_entry = MockConfigEntry(
-        data={},
-        domain=DOMAIN,
-        options={"entity_id": fake_uuid, "target_domain": target_domain},
-        title="ABC",
-    )
-
-    config_entry.add_to_hass(hass)
-
-    assert not await hass.config_entries.async_setup(config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    assert len(hass.states.async_all()) == 0
