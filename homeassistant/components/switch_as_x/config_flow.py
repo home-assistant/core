cb7e492e815715d13422c239b0ef23f3ed4c6fef
Erik Montnemery <erik@montnemery.com>
2022-03-10 12:23:01 +0100

    Add switch_as_x integration (#67878)
    * Add switch_as_x integration
    
    * Address review comments
    
    * Add translation strings
    
    * Rename entity_type option to target_domain
    
    * Move LightSwitch class definition to switch_as_x/light.py
    
    * Update manifest
    
    * Move tests

--- homeassistant/components/switch_as_x/config_flow.py
+++ homeassistant/components/switch_as_x/config_flow.py
@@ -1,4 +1,4 @@
-"""Config flow for Switch integration."""
+"""Config flow for Switch as X integration."""
 from __future__ import annotations
 
 from collections.abc import Mapping
@@ -13,7 +13,7 @@ from homeassistant.helpers import (
     selector,
 )
 
-from .const import DOMAIN
+from . import DOMAIN
 
 CONFIG_FLOW = {
     "user": helper_config_entry_flow.HelperFlowStep(
@@ -22,16 +22,19 @@ CONFIG_FLOW = {
                 vol.Required("entity_id"): selector.selector(
                     {"entity": {"domain": "switch"}}
                 ),
+                vol.Required("target_domain"): selector.selector(
+                    {"select": {"options": ["light"]}}
+                ),
             }
         )
     )
 }
 
 
-class SwitchLightConfigFlowHandler(
+class SwitchAsXConfigFlowHandler(
     helper_config_entry_flow.HelperConfigFlowHandler, domain=DOMAIN
 ):
-    """Handle a config or options flow for Switch Light."""
+    """Handle a config flow for Switch as X."""
 
     config_flow = CONFIG_FLOW
 
