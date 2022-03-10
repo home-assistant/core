d1f790f515ca5e937bf119d68d280188afc1936d
epenet <epenet@users.noreply.github.com>
2022-03-10 10:37:44 +0000

    Split remote and authenticator examples

--- homeassistant/components/samsungtv/encrypted/authenticator_example.py
+++ homeassistant/components/samsungtv/encrypted/authenticator_example.py
@@ -6,13 +6,9 @@ import asyncio
 import aiohttp
 
 from .authenticator import SamsungTVEncryptedWSAsyncAuthenticator
-from .remote import SamsungTVEncryptedWSAsyncRemote, SendRemoteKey
 
 _HOST = "192.168.0.14"
 _PORT = 8000
-
-_TOKEN = ""
-_SESSION_ID = ""
 
 
 async def _get_token(
@@ -36,28 +32,10 @@ async def main() -> None:
     """Get token."""
     host = _HOST
     port = _PORT
-    token = _TOKEN
-    session_id = _SESSION_ID
 
     async with aiohttp.ClientSession() as web_session:
-        if not token or not session_id:
-            token, session_id = await _get_token(host, web_session, port)
-            print(f"Token: '{token}', session: '{session_id}'")
-
-        remote = SamsungTVEncryptedWSAsyncRemote(
-            host=host,
-            web_session=web_session,
-            token=token,
-            session_id=session_id,
-            port=port,
-        )
-        await remote.start_listening()
-
-        await remote.send_command(SendRemoteKey.click("KEY_POWER"))
-
-        await asyncio.sleep(15)
-
-        await remote.close()
+        token, session_id = await _get_token(host, web_session, port)
+        print(f"Token: '{token}', session: '{session_id}'")
 
 
 loop = asyncio.get_event_loop()
