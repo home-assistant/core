a333a35430f779e35acd48cac9264936be3d2acb
Jeef <jeffstein@gmail.com>
2022-03-10 12:08:49 -0700

    oops commented out some code

--- tests/components/intellifire/test_config_flow.py
+++ tests/components/intellifire/test_config_flow.py
@@ -7,58 +7,59 @@ from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import RESULT_TYPE_CREATE_ENTRY, RESULT_TYPE_FORM
 
-# async def test_form(
-#     hass: HomeAssistant,
-#     mock_setup_entry: AsyncMock,
-#     mock_intellifire_config_flow: MagicMock,
-#     mock_fireplace_finder_single: AsyncMock,
-# ) -> None:
-#     """Test we get the form."""
-#     result = await hass.config_entries.flow.async_init(
-#         DOMAIN, context={"source": config_entries.SOURCE_USER}
-#     )
-#     assert result["type"] == RESULT_TYPE_FORM
-#     assert result["errors"] == {}
 
-#     result2 = await hass.config_entries.flow.async_configure(
-#         result["flow_id"],
-#         {
-#             CONF_HOST: "1.1.1.1",
-#         },
-#     )
-#     await hass.async_block_till_done()
+async def test_form(
+    hass: HomeAssistant,
+    mock_setup_entry: AsyncMock,
+    mock_intellifire_config_flow: MagicMock,
+    mock_fireplace_finder_single: AsyncMock,
+) -> None:
+    """Test we get the form."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] == {}
 
-#     assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
-#     assert result2["title"] == "Fireplace"
-#     assert result2["data"] == {CONF_HOST: "1.1.1.1"}
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_HOST: "1.1.1.1",
+        },
+    )
+    await hass.async_block_till_done()
 
-#     assert len(mock_setup_entry.mock_calls) == 1
+    assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
+    assert result2["title"] == "Fireplace"
+    assert result2["data"] == {CONF_HOST: "1.1.1.1"}
+
+    assert len(mock_setup_entry.mock_calls) == 1
 
 
-# async def test_no_discovery(
-#     hass: HomeAssistant,
-#     mock_setup_entry: AsyncMock,
-#     mock_intellifire_config_flow: MagicMock,
-#     mock_fireplace_finder_none: AsyncMock,
-# ) -> None:
-#     """Test we get the form."""
-#     result = await hass.config_entries.flow.async_init(
-#         DOMAIN, context={"source": config_entries.SOURCE_USER}
-#     )
-#     assert result["type"] == RESULT_TYPE_FORM
-#     assert result["errors"] == {}
+async def test_no_discovery(
+    hass: HomeAssistant,
+    mock_setup_entry: AsyncMock,
+    mock_intellifire_config_flow: MagicMock,
+    mock_fireplace_finder_none: AsyncMock,
+) -> None:
+    """Test we get the form."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["errors"] == {}
 
-#     result2 = await hass.config_entries.flow.async_configure(
-#         result["flow_id"],
-#         {
-#             CONF_HOST: "1.1.1.1",
-#         },
-#     )
-#     await hass.async_block_till_done()
-#     assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
-#     assert result2["title"] == "Fireplace"
-#     assert result2["data"] == {CONF_HOST: "1.1.1.1"}
-#     assert len(mock_setup_entry.mock_calls) == 1
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_HOST: "1.1.1.1",
+        },
+    )
+    await hass.async_block_till_done()
+    assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
+    assert result2["title"] == "Fireplace"
+    assert result2["data"] == {CONF_HOST: "1.1.1.1"}
+    assert len(mock_setup_entry.mock_calls) == 1
 
 
 async def test_single_discovery(
