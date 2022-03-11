8948bada58ef185d084e96b3edd3615f39af6a14
Erik Montnemery <erik@montnemery.com>
2022-03-11 09:46:32 +0100

    Add switch_as_x entity to wrapped switch's device (#67961)

--- homeassistant/components/switch_as_x/__init__.py
+++ homeassistant/components/switch_as_x/__init__.py
@@ -7,8 +7,8 @@ import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ENTITY_ID
-from homeassistant.core import Event, HomeAssistant
-from homeassistant.helpers import entity_registry as er
+from homeassistant.core import Event, HomeAssistant, callback
+from homeassistant.helpers import device_registry as dr, entity_registry as er
 from homeassistant.helpers.event import async_track_entity_registry_updated_event
 
 from .light import LightSwitch
@@ -20,9 +20,31 @@ DOMAIN = "switch_as_x"
 _LOGGER = logging.getLogger(__name__)
 
 
+@callback
+def async_add_to_device(
+    hass: HomeAssistant, entry: ConfigEntry, entity_id: str
+) -> str | None:
+    """Add our config entry to the tracked entity's device."""
+    registry = er.async_get(hass)
+    device_registry = dr.async_get(hass)
+    device_id = None
+
+    if (
+        not (wrapped_switch := registry.async_get(entity_id))
+        or not (device_id := wrapped_switch.device_id)
+        or not (device_registry.async_get(device_id))
+    ):
+        return device_id
+
+    device_registry.async_update_device(device_id, add_config_entry_id=entry.entry_id)
+
+    return device_id
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up a config entry."""
     registry = er.async_get(hass)
+    device_registry = dr.async_get(hass)
     try:
         entity_id = er.async_validate_entity_id(registry, entry.options[CONF_ENTITY_ID])
     except vol.Invalid:
@@ -39,17 +61,35 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         if data["action"] == "remove":
             await hass.config_entries.async_remove(entry.entry_id)
 
-        if data["action"] != "update" or "entity_id" not in data["changes"]:
+        if data["action"] != "update":
             return
 
-        # Entity_id changed, reload the config entry
-        await hass.config_entries.async_reload(entry.entry_id)
+        if "entity_id" in data["changes"]:
+            # Entity_id changed, reload the config entry
+            await hass.config_entries.async_reload(entry.entry_id)
+
+        if device_id and "device_id" in data["changes"]:
+            # If the tracked switch is no longer in the device, remove our config entry
+            # from the device
+            if (
+                not (entity_entry := registry.async_get(data["entity_id"]))
+                or not device_registry.async_get(device_id)
+                or entity_entry.device_id == device_id
+            ):
+                # No need to do any cleanup
+                return
+
+            device_registry.async_update_device(
+                device_id, remove_config_entry_id=entry.entry_id
+            )
 
     entry.async_on_unload(
         async_track_entity_registry_updated_event(
             hass, entity_id, async_registry_updated
         )
     )
+
+    device_id = async_add_to_device(hass, entry, entity_id)
 
     hass.config_entries.async_setup_platforms(entry, (entry.options["target_domain"],))
     return True
--- tests/components/switch_as_x/test_init.py
+++ tests/components/switch_as_x/test_init.py
@@ -5,7 +5,7 @@ import pytest
 
 from homeassistant.components.switch_as_x import DOMAIN
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 from tests.common import MockConfigEntry
 
@@ -82,3 +82,102 @@ async def test_entity_registry_events(hass: HomeAssistant, target_domain):
     assert hass.states.get(f"{target_domain}.abc") is None
     assert registry.async_get(f"{target_domain}.abc") is None
     assert len(hass.config_entries.async_entries("switch_as_x")) == 0
+
+
+@pytest.mark.parametrize("target_domain", ("light",))
+async def test_device_registry_config_entry_1(hass: HomeAssistant, target_domain):
+    """Test we add our config entry to the tracked switch's device."""
+    device_registry = dr.async_get(hass)
+    entity_registry = er.async_get(hass)
+
+    switch_config_entry = MockConfigEntry()
+
+    device_entry = device_registry.async_get_or_create(
+        config_entry_id=switch_config_entry.entry_id,
+        connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
+    )
+    switch_entity_entry = entity_registry.async_get_or_create(
+        "switch",
+        "test",
+        "unique",
+        config_entry=switch_config_entry,
+        device_id=device_entry.id,
+    )
+    # Add another config entry to the same device
+    device_registry.async_update_device(
+        device_entry.id, add_config_entry_id=MockConfigEntry().entry_id
+    )
+
+    switch_as_x_config_entry = MockConfigEntry(
+        data={},
+        domain=DOMAIN,
+        options={"entity_id": switch_entity_entry.id, "target_domain": target_domain},
+        title="ABC",
+    )
+
+    switch_as_x_config_entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(switch_as_x_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    entity_entry = entity_registry.async_get(f"{target_domain}.abc")
+    assert entity_entry.device_id == switch_entity_entry.device_id
+
+    device_entry = device_registry.async_get(device_entry.id)
+    assert switch_as_x_config_entry.entry_id in device_entry.config_entries
+
+    # Remove the wrapped switch's config entry from the device
+    device_registry.async_update_device(
+        device_entry.id, remove_config_entry_id=switch_config_entry.entry_id
+    )
+    await hass.async_block_till_done()
+    await hass.async_block_till_done()
+    # Check that the switch_as_x config entry is removed from the device
+    device_entry = device_registry.async_get(device_entry.id)
+    assert switch_as_x_config_entry.entry_id not in device_entry.config_entries
+
+
+@pytest.mark.parametrize("target_domain", ("light",))
+async def test_device_registry_config_entry_2(hass: HomeAssistant, target_domain):
+    """Test we add our config entry to the tracked switch's device."""
+    device_registry = dr.async_get(hass)
+    entity_registry = er.async_get(hass)
+
+    switch_config_entry = MockConfigEntry()
+
+    device_entry = device_registry.async_get_or_create(
+        config_entry_id=switch_config_entry.entry_id,
+        connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
+    )
+    switch_entity_entry = entity_registry.async_get_or_create(
+        "switch",
+        "test",
+        "unique",
+        config_entry=switch_config_entry,
+        device_id=device_entry.id,
+    )
+
+    switch_as_x_config_entry = MockConfigEntry(
+        data={},
+        domain=DOMAIN,
+        options={"entity_id": switch_entity_entry.id, "target_domain": target_domain},
+        title="ABC",
+    )
+
+    switch_as_x_config_entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(switch_as_x_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    entity_entry = entity_registry.async_get(f"{target_domain}.abc")
+    assert entity_entry.device_id == switch_entity_entry.device_id
+
+    device_entry = device_registry.async_get(device_entry.id)
+    assert switch_as_x_config_entry.entry_id in device_entry.config_entries
+
+    # Remove the wrapped switch from the device
+    entity_registry.async_update_entity(switch_entity_entry.entity_id, device_id=None)
+    await hass.async_block_till_done()
+    # Check that the switch_as_x config entry is removed from the device
+    device_entry = device_registry.async_get(device_entry.id)
+    assert switch_as_x_config_entry.entry_id not in device_entry.config_entries
