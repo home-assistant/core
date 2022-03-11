2b52289a5d324262598704c883388af9aee6f599
Peeter Normak <peeter.normak@eesti.ee>
2022-03-11 09:55:30 +0200

    Refactor async_reload_entry

--- homeassistant/components/maxcube/__init__.py
+++ homeassistant/components/maxcube/__init__.py
@@ -189,5 +189,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+    await hass.config_entries.async_reload(entry.entry_id)
