8b1703c970f6896cf681e512277cf07802b16b3c
Jeef <jeffstein@gmail.com>
2022-03-10 21:33:54 -0700

    fix: tests are updated

--- tests/components/intellifire/test_config_flow.py
+++ tests/components/intellifire/test_config_flow.py
@@ -8,58 +8,33 @@ from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import RESULT_TYPE_CREATE_ENTRY, RESULT_TYPE_FORM
 
 
-async def test_form(
-    hass: HomeAssistant,
-    mock_setup_entry: AsyncMock,
-    mock_intellifire_config_flow: MagicMock,
-    mock_fireplace_finder_single: AsyncMock,
-) -> None:
-    """Test we get the form."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert result["type"] == RESULT_TYPE_FORM
-    assert result["errors"] == {}
-
-    result2 = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {
-            CONF_HOST: "1.1.1.1",
-        },
-    )
-    await hass.async_block_till_done()
-
-    assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
-    assert result2["title"] == "Fireplace"
-    assert result2["data"] == {CONF_HOST: "1.1.1.1"}
-
-    assert len(mock_setup_entry.mock_calls) == 1
-
-
 async def test_no_discovery(
     hass: HomeAssistant,
     mock_setup_entry: AsyncMock,
     mock_intellifire_config_flow: MagicMock,
-    mock_fireplace_finder_none: AsyncMock,
 ) -> None:
     """Test we get the form."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert result["type"] == RESULT_TYPE_FORM
-    assert result["errors"] == {}
+    with patch(
+        "intellifire4py.udp.AsyncUDPFireplaceFinder.search_fireplace",
+        return_value=[],
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}
+        )
+        assert result["type"] == RESULT_TYPE_FORM
+        assert result["errors"] == {}
 
-    result2 = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {
-            CONF_HOST: "1.1.1.1",
-        },
-    )
-    await hass.async_block_till_done()
-    assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
-    assert result2["title"] == "Fireplace"
-    assert result2["data"] == {CONF_HOST: "1.1.1.1"}
-    assert len(mock_setup_entry.mock_calls) == 1
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_HOST: "1.1.1.1",
+            },
+        )
+        await hass.async_block_till_done()
+        assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
+        assert result2["title"] == "Fireplace"
+        assert result2["data"] == {CONF_HOST: "1.1.1.1"}
+        assert len(mock_setup_entry.mock_calls) == 1
 
 
 async def test_single_discovery(
@@ -76,11 +51,16 @@ async def test_single_discovery(
         result = await hass.config_entries.flow.async_init(
             DOMAIN, context={"source": config_entries.SOURCE_USER}
         )
-        await hass.async_block_till_done()
 
-        assert result["type"] == RESULT_TYPE_CREATE_ENTRY
-        assert result["title"] == "Fireplace"
-        assert result["data"] == {CONF_HOST: "192.168.1.69"}
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {CONF_HOST: "192.168.1.69"}
+        )
+        await hass.async_block_till_done()
+        print("Result:", result)
+
+        assert result2["type"] == RESULT_TYPE_CREATE_ENTRY
+        assert result2["title"] == "Fireplace"
+        assert result2["data"] == {CONF_HOST: "192.168.1.69"}
         assert len(mock_setup_entry.mock_calls) == 1
 
 
