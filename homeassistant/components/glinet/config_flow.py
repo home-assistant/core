b4a241a27996b38107d124f533fda4442a491878
HarvsG <11440490+HarvsG@users.noreply.github.com>
2022-03-13 13:36:52 +0000

    Remove comments
    Co-authored-by: Duco Sebel <74970928+DCSBL@users.noreply.github.com>

--- homeassistant/components/glinet/config_flow.py
+++ homeassistant/components/glinet/config_flow.py
@@ -77,13 +77,6 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    # TODO validate the data can be used to set up a connection.
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
 
     hub = TestingHub(data[CONF_HOST])
 
