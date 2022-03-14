3577b6e79f977b8151b28f61e3a50d8d7f288191
J. Nick Koston <nick@koston.org>
2022-03-13 15:11:57 -1000

    pool tests

--- tests/components/recorder/test_pool.py
+++ tests/components/recorder/test_pool.py
@@ -4,10 +4,11 @@ import threading
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from homeassistant.components.recorder.const import DB_WORKER_PREFIX
 from homeassistant.components.recorder.pool import RecorderPool
 
 
-def test_recorder_pool():
+def test_recorder_pool(caplog):
     """Test RecorderPool gives the same connection in the creating thread."""
 
     engine = create_engine("sqlite://", poolclass=RecorderPool)
@@ -25,10 +26,26 @@ def test_recorder_pool():
         session.close()
 
     _get_connection_twice()
-    assert connections[0] == connections[1]
+    assert "Database access is slower in the default executor" in caplog.text
+    assert connections[0] != connections[1]
 
+    caplog.clear()
     new_thread = threading.Thread(target=_get_connection_twice)
     new_thread.start()
     new_thread.join()
-
+    assert "Database access is slower in the default executor" in caplog.text
     assert connections[2] != connections[3]
+
+    caplog.clear()
+    new_thread = threading.Thread(target=_get_connection_twice, name=DB_WORKER_PREFIX)
+    new_thread.start()
+    new_thread.join()
+    assert "Database access is slower in the default executor" not in caplog.text
+    assert connections[4] == connections[5]
+
+    caplog.clear()
+    new_thread = threading.Thread(target=_get_connection_twice, name="Recorder")
+    new_thread.start()
+    new_thread.join()
+    assert "Database access is slower in the default executor" not in caplog.text
+    assert connections[6] == connections[7]
