9cd6e7fb3eff356e12bd3d61a0458a77840cf92a
mib1185 <mail@mib85.de>
2022-03-14 21:26:27 +0000

    respect disable_new_entities for new entities

--- homeassistant/components/device_tracker/config_entry.py
+++ homeassistant/components/device_tracker/config_entry.py
@@ -149,9 +149,19 @@ def _async_register_mac(
             return
 
         # Make sure entity has a config entry and was disabled by the
-        # default disable logic in the integration.
+        # default disable logic in the integration and new entities
+        # are allowed to be added.
         if (
             entity_entry.config_entry_id is None
+            or (
+                (
+                    config_entry := hass.config_entries.async_get_entry(
+                        entity_entry.config_entry_id
+                    )
+                )
+                is not None
+                and config_entry.pref_disable_new_entities
+            )
             or entity_entry.disabled_by != er.RegistryEntryDisabler.INTEGRATION
         ):
             return
