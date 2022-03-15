92a47ce219bfdefb3217f6de5f24c82fe4bddb9a
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-03-15 21:09:36 +0100

    Make TypeVars private (2)

--- homeassistant/backports/enum.py
+++ homeassistant/backports/enum.py
@@ -4,13 +4,13 @@ from __future__ import annotations
 from enum import Enum
 from typing import Any, TypeVar
 
-T = TypeVar("T", bound="StrEnum")
+_T = TypeVar("_T", bound="StrEnum")
 
 
 class StrEnum(str, Enum):
     """Partial backport of Python 3.11's StrEnum for our basic use cases."""
 
-    def __new__(cls: type[T], value: str, *args: Any, **kwargs: Any) -> T:
+    def __new__(cls: type[_T], value: str, *args: Any, **kwargs: Any) -> _T:
         """Create a new StrEnum instance."""
         if not isinstance(value, str):
             raise TypeError(f"{value!r} is not a string")
