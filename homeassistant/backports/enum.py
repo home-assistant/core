21f32c4e7e2c1a1f206c507b6237de1fa1ae526d
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-03-15 21:45:16 +0100

    Rename StrEnum TypeVar

--- homeassistant/backports/enum.py
+++ homeassistant/backports/enum.py
@@ -4,13 +4,15 @@ from __future__ import annotations
 from enum import Enum
 from typing import Any, TypeVar
 
-_T = TypeVar("_T", bound="StrEnum")
+_StrEnumT = TypeVar("_StrEnumT", bound="StrEnum")
 
 
 class StrEnum(str, Enum):
     """Partial backport of Python 3.11's StrEnum for our basic use cases."""
 
-    def __new__(cls: type[_T], value: str, *args: Any, **kwargs: Any) -> _T:
+    def __new__(
+        cls: type[_StrEnumT], value: str, *args: Any, **kwargs: Any
+    ) -> _StrEnumT:
         """Create a new StrEnum instance."""
         if not isinstance(value, str):
             raise TypeError(f"{value!r} is not a string")
