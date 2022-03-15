ec2bfe702e24513a64f584d3ed6fc230683ee33e
J. Nick Koston <nick@koston.org>
2022-03-14 17:31:05 -1000

    bump

--- homeassistant/components/august/__init__.py
+++ homeassistant/components/august/__init__.py
@@ -1,10 +1,15 @@
 """Support for August devices."""
+from __future__ import annotations
+
 import asyncio
+from collections.abc import ValuesView
 from itertools import chain
 import logging
 
 from aiohttp import ClientError, ClientResponseError
+from yalexs.doorbell import Doorbell, DoorbellDetail
 from yalexs.exceptions import AugustApiAIOHTTPError
+from yalexs.lock import Lock, LockDetail
 from yalexs.pubnub_activity import activities_from_pubnub_message
 from yalexs.pubnub_async import AugustPubNub, async_create_pubnub
 
@@ -188,16 +193,16 @@ class AugustData(AugustSubscriberMixin):
         self.activity_stream.async_stop()
 
     @property
-    def doorbells(self):
+    def doorbells(self) -> ValuesView[Doorbell]:
         """Return a list of py-august Doorbell objects."""
         return self._doorbells_by_id.values()
 
     @property
-    def locks(self):
+    def locks(self) -> ValuesView[Lock]:
         """Return a list of py-august Lock objects."""
         return self._locks_by_id.values()
 
-    def get_device_detail(self, device_id):
+    def get_device_detail(self, device_id: str) -> DoorbellDetail | LockDetail:
         """Return the py-august LockDetail or DoorbellDetail object for a device."""
         return self._device_detail_by_id[device_id]
 
