aabf46b1b35eeb9132c34eb87b1e009c6e0b2ad8
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-03-15 15:56:08 +0100

    Fix point by adding authlib constraint (#68176)
    * Fix point by pinning authlib
    
    * Use constraint

--- script/gen_requirements_all.py
+++ script/gen_requirements_all.py
@@ -115,6 +115,10 @@ python-socketio>=4.6.0,<5.0
 # Constrain multidict to avoid typing issues
 # https://github.com/home-assistant/core/pull/67046
 multidict>=6.0.2
+
+# Required for compatibility with point integration - ensure_active_token
+# https://github.com/home-assistant/core/pull/68176
+authlib<1.0
 """
 
 IGNORE_PRE_COMMIT_HOOK_ID = (
