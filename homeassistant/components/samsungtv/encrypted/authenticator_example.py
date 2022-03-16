28a8ee0a570056e1d8afe1780001991d55b24729
epenet <epenet@users.noreply.github.com>
2022-03-16 08:52:32 +0000

    Reset encrypted folder to align with samsungtvws

---
+++ homeassistant/components/samsungtv/encrypted/authenticator_example.py
@@ -0,0 +1,45 @@
+"""SamsungTV Encrypted."""
+from __future__ import annotations
+
+import asyncio
+import logging
+
+import aiohttp
+
+from .authenticator import SamsungTVEncryptedWSAsyncAuthenticator
+
+logging.basicConfig(level=logging.DEBUG)
+
+_HOST = "192.168.0.14"
+_PORT = 8080  # Warning: this can be different from the remote port
+
+
+async def _get_token(
+    host: str, web_session: aiohttp.ClientSession, port: int
+) -> tuple[str, str]:
+    authenticator = SamsungTVEncryptedWSAsyncAuthenticator(
+        host, web_session=web_session, port=port
+    )
+    await authenticator.start_pairing()
+    token: str | None = None
+    while not token:
+        pin = input("Please enter pin from tv: ")
+        token = await authenticator.try_pin(pin)
+
+    session_id = await authenticator.get_session_id_and_close()
+
+    return (token, session_id)
+
+
+async def main() -> None:
+    """Get token."""
+    host = _HOST
+    port = _PORT
+
+    async with aiohttp.ClientSession() as web_session:
+        token, session_id = await _get_token(host, web_session, port)
+        print(f"Token: '{token}', session: '{session_id}'")
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
---
+++ homeassistant/components/samsungtv/encrypted/remote_example.py
@@ -0,0 +1,45 @@
+"""SamsungTV Encrypted."""
+from __future__ import annotations
+
+import asyncio
+import logging
+
+import aiohttp
+
+from .remote import SamsungTVEncryptedWSAsyncRemote, SendRemoteKey
+
+logging.basicConfig(level=logging.DEBUG)
+
+_HOST = "192.168.0.14"
+_PORT = 8000  # Warning: this can be different from the authenticator port
+
+_TOKEN = ""
+_SESSION_ID = ""
+
+
+async def main() -> None:
+    """Get token."""
+    host = _HOST
+    port = _PORT
+    token = _TOKEN
+    session_id = _SESSION_ID
+
+    async with aiohttp.ClientSession() as web_session:
+        remote = SamsungTVEncryptedWSAsyncRemote(
+            host=host,
+            web_session=web_session,
+            token=token,
+            session_id=session_id,
+            port=port,
+        )
+        await remote.start_listening()
+
+        await remote.send_command(SendRemoteKey.click("KEY_POWEROFF"))
+
+        await asyncio.sleep(15)
+
+        await remote.close()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
