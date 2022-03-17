f03ea545fa218f781e06e770eee86c2d8f7d66e4
J. Nick Koston <nick@koston.org>
2022-03-16 23:16:04 -1000

    fix mysql

--- homeassistant/components/recorder/models.py
+++ homeassistant/components/recorder/models.py
@@ -8,6 +8,7 @@ from typing import TypedDict, overload
 
 from fnvhash import fnv1a_32
 from sqlalchemy import (
+    BigInteger,
     Boolean,
     Column,
     DateTime,
@@ -238,7 +239,7 @@ class StateAttributes(Base):  # type: ignore[misc,valid-type]
     )
     __tablename__ = TABLE_STATE_ATTRIBUTES
     attributes_id = Column(Integer, Identity(), primary_key=True)
-    hash = Column(Integer, index=True)
+    hash = Column(BigInteger, index=True)
     # Note that this is not named attributes to avoid confusion with the states table
     shared_attrs = Column(Text().with_variant(mysql.LONGTEXT, "mysql"))
 
