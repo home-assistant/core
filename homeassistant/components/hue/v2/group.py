94564d2d81026444ef51f19b09e8dcb0dfcb600c
Marcel van der Veldt <m.vanderveldt@outlook.com>
2022-03-18 17:53:08 +0100

    update errors to ignore

--- homeassistant/components/hue/v2/group.py
+++ homeassistant/components/hue/v2/group.py
@@ -42,6 +42,10 @@ ALLOWED_ERRORS = [
     'device (groupedLight) is "soft off", command (on) may not have effect',
     "device (light) has communication issues, command (on) may not have effect",
     'device (light) is "soft off", command (on) may not have effect',
+    "device (grouped_light) has communication issues, command (.on) may not have effect",
+    'device (grouped_light) is "soft off", command (.on) may not have effect'
+    "device (light) has communication issues, command (.on) may not have effect",
+    'device (light) is "soft off", command (.on) may not have effect',
 ]
 
 
