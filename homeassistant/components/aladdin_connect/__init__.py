ad5a5e3fc10f5e6d662f4594079935a4c666626a
mkmer <m_kasper@sbcglobal.net>
2022-03-19 15:01:19 -0400

    Update homeassistant/components/aladdin_connect/__init__.py
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- homeassistant/components/aladdin_connect/__init__.py
+++ homeassistant/components/aladdin_connect/__init__.py
@@ -20,13 +20,6 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: config_entries.ConfigEntry
 ) -> bool:
     """Set up platform from a ConfigEntry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass_data = dict(entry.data)
-    # Registers update listener to update config entry when options are updated.
-    # unsub_options_update_listener = entry.add_update_listener(options_update_listener)
-    # Store a reference to the unsubscribe function to cleanup if an entry is unloaded.
-    # hass_data["unsub_options_update_listener"] = unsub_options_update_listener
-    hass.data[DOMAIN][entry.entry_id] = hass_data
 
     # Forward the setup to the sensor platform.
     hass.async_create_task(
