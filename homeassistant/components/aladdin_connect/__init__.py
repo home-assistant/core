bfed9d9a173d217cc12fbc07464a68c09fa38a18
Mike Kasper <m_kasper@sbcglobal.net>
2022-03-19 20:16:42 +0000

    recommended change broke cover.py - backed out

--- homeassistant/components/aladdin_connect/__init__.py
+++ homeassistant/components/aladdin_connect/__init__.py
@@ -7,7 +7,6 @@ from homeassistant import config_entries
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 
@@ -20,6 +19,9 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: config_entries.ConfigEntry
 ) -> bool:
     """Set up platform from a ConfigEntry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass_data = dict(entry.data)
+    hass.data[DOMAIN][entry.entry_id] = hass_data
 
     # Forward the setup to the sensor platform.
     hass.async_create_task(
@@ -39,5 +41,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Update listener."""
     await hass.config_entries.async_reload(entry.entry_id)
-
-
