5c83260993c6667499835a2ab6fea806ac690f6d
Erik Montnemery <erik@montnemery.com>
2022-03-20 10:25:15 +0100

    Simplify time zone setting in tests (#68330)
    * Simplify timezone setting in tests
    
    * Fix typo
    
    * Adjust caldav tests
    
    * Adjust input_datetime tests
    
    * Adjust time_date tests
    
    * Adjust tod tests
    
    * Adjust helper tests
    
    * Adjust recorder tests
    
    * Adjust risco tests
    
    * Adjust aemet tests
    
    * Adjust flux tests
    
    * Adjust forecast_solar tests
    
    * Revert unnecessary change in forecast_solar test
    
    * Adjust climacell tests
    
    * Adjust google tests
    
    * Adjust sensor tests
    
    * Adjust sonarr tests
    
    * Adjust template tests
    
    * Adjust zodiac tests
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/sonarr/test_sensor.py
+++ tests/components/sonarr/test_sensor.py
@@ -96,10 +96,10 @@ async def test_sensors(
     assert state
     assert state.attributes.get(ATTR_ICON) == "mdi:television"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == "Episodes"
-    assert state.attributes.get("Bob's Burgers S04E11") == "2014-01-27T01:30:00+00:00"
+    assert state.attributes.get("Bob's Burgers S04E11") == "2014-01-26T17:30:00-08:00"
     assert (
         state.attributes.get("The Andy Griffith Show S01E01")
-        == "1960-10-03T01:00:00+00:00"
+        == "1960-10-02T17:00:00-08:00"
     )
     assert state.state == "2"
 
