ff2de04d7ca5cf0f1dcdb31746d9a0492539a5f4
Hessel van Es <hessel@hesselonline.nl>
2022-03-21 19:18:12 +0000

    Added a test for authentication check in lock

--- tests/components/wallbox/test_lock.py
+++ tests/components/wallbox/test_lock.py
@@ -8,7 +8,11 @@ from homeassistant.components.lock import SERVICE_LOCK, SERVICE_UNLOCK
 from homeassistant.components.wallbox import CONF_LOCKED_UNLOCKED_KEY
 from homeassistant.const import ATTR_ENTITY_ID
 
-from tests.components.wallbox import entry, setup_integration
+from tests.components.wallbox import (
+    entry,
+    setup_integration,
+    setup_integration_read_only,
+)
 from tests.components.wallbox.const import (
     CONF_ERROR,
     CONF_JWT,
@@ -104,5 +108,28 @@ async def test_wallbox_lock_class_connection_error(hass):
                 },
                 blocking=True,
             )
+
+    await hass.config_entries.async_unload(entry.entry_id)
+
+
+async def test_wallbox_lock_class_authentication_error(hass):
+    """Test wallbox lock not loaded on authentication error."""
+
+    await setup_integration_read_only(hass)
+
+    with requests_mock.Mocker() as mock_request:
+        mock_request.get(
+            "https://api.wall-box.com/auth/token/user",
+            json=authorisation_response,
+            status_code=200,
+        )
+        mock_request.put(
+            "https://api.wall-box.com/v2/charger/12345",
+            json=json.loads(json.dumps({CONF_LOCKED_UNLOCKED_KEY: False})),
+            status_code=401,
+        )
+        state = hass.states.get(CONF_MOCK_LOCK_ENTITY_ID)
+
+        assert state is None
 
     await hass.config_entries.async_unload(entry.entry_id)
