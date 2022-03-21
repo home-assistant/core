310a4e0f15043bcefeffebcadf7f374647335115
Tom Scholten <git@scholten.nu>
2022-03-21 18:14:46 +0100

    Replace logging with exception

--- homeassistant/components/plugwise/select.py
+++ homeassistant/components/plugwise/select.py
@@ -5,6 +5,7 @@ from homeassistant.components.select import SelectEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import STATE_ON
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN, LOGGER, THERMOSTAT_CLASSES
@@ -52,13 +53,13 @@ class PlugwiseSelectEntity(PlugwiseEntity, SelectEntity):
 
     async def async_select_option(self, option: str) -> None:
         """Change the selected option."""
-        result = await self.coordinator.api.set_schedule_state(
-            self.device.get("location"),
-            option,
-            STATE_ON,
-        )
-        if result:
-            LOGGER.debug("Change schedule to %s was successful", option)
-            await self.coordinator.async_request_refresh()
-        else:
-            LOGGER.error("Failed changing schedule")
+        if not (
+            await self.coordinator.api.set_schedule_state(
+                self.device.get("location"),
+                option,
+                STATE_ON,
+            )
+        ):
+            raise HomeAssistantError(f"Failed to change to schedule {option}")
+        LOGGER.debug("Change schedule to %s was successful", option)
+        await self.coordinator.async_request_refresh()
