97d9de4b328ec97480c7b9e7faec5553126cacdf
Franck Nijhof <frenck@frenck.nl>
2022-03-22 12:54:36 +0100

    Apply suggestions from code review

--- homeassistant/components/tuya/light.py
+++ homeassistant/components/tuya/light.py
@@ -110,7 +110,6 @@ LIGHTS: dict[str, tuple[TuyaLightEntityDescription, ...]] = {
         ),
         # Not documented
         # Based on multiple reports: manufacturer customized Dimmer 2 switches
-        # Second Light is not SWITCH_LED_1, it is SWITCH_1
         TuyaLightEntityDescription(
             key=DPCode.SWITCH_1,
             name="Light",
