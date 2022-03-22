e9365346529c9b55a6c44164078098de652d8e9d
Tom Scholten <git@scholten.nu>
2022-03-22 20:11:20 +0100

    Failed attempt at testing migrate

--- tests/components/plugwise/test_init.py
+++ tests/components/plugwise/test_init.py
@@ -10,8 +10,10 @@ from plugwise.exceptions import (
 import pytest
 
 from homeassistant.components.plugwise.const import DOMAIN
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from tests.common import MockConfigEntry
 
@@ -60,3 +62,42 @@ async def test_config_entry_not_ready(
 
     assert len(mock_smile_anna.connect.mock_calls) == 1
     assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
+
+
+@pytest.mark.parametrize(
+    "entitydata,old_unique_id,new_unique_id",
+    [
+        (
+            {
+                "domain": SENSOR_DOMAIN,
+                "platform": DOMAIN,
+                "unique_id": "opentherm_outdoor_temperature",
+            },
+            "opentherm_outdoor_temperature",
+            "opentherm_outdoor_air_temperature",
+        ),
+    ],
+)
+async def test_migrate_unique_id(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_smile_anna: MagicMock,
+    entitydata: dict,
+    old_unique_id: str,
+    new_unique_id: str,
+):
+    """Test migration of unique_id."""
+    mock_config_entry.add_to_hass(hass)
+
+    entity_registry = er.async_get(hass)
+    entity: er.RegistryEntry = entity_registry.async_get_or_create(
+        **entitydata,
+        config_entry=mock_config_entry,
+    )
+    assert entity.unique_id == old_unique_id
+    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    entity_migrated = entity_registry.async_get(entity.entity_id)
+    assert entity_migrated
+    assert entity_migrated.unique_id == new_unique_id
