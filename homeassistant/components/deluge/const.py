81befc0802c48e30bb4019cf8ef0e1a4398b16b6
Robert Hillis <tkdrob4390@yahoo.com>
2022-03-23 00:01:24 -0400

    Add config flow to deluge (#58789)

---
+++ homeassistant/components/deluge/const.py
@@ -0,0 +1,12 @@
+"""Constants for the Deluge integration."""
+import logging
+
+CONF_WEB_PORT = "web_port"
+DEFAULT_NAME = "Deluge"
+DEFAULT_RPC_PORT = 58846
+DEFAULT_WEB_PORT = 8112
+DHT_UPLOAD = 1000
+DHT_DOWNLOAD = 1000
+DOMAIN = "deluge"
+
+LOGGER = logging.getLogger(__package__)
