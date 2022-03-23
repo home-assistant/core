7f9cfbd035427f5c04fd7cfe2f91496397920eaa
jbouwh <jan@jbsoft.nl>
2022-03-23 09:47:58 +0000

    add test

--- tests/components/notify/test_init.py
+++ tests/components/notify/test_init.py
@@ -1,7 +1,27 @@
 """The tests for notify services that change targets."""
+
+from unittest.mock import Mock, patch
+
+import yaml
+
+from homeassistant import config as hass_config
 from homeassistant.components import notify
+from homeassistant.const import SERVICE_RELOAD
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.reload import async_setup_reload_service
+from homeassistant.loader import DATA_INTEGRATIONS
 from homeassistant.setup import async_setup_component
+
+from tests.common import MockPlatform, mock_platform
+
+
+class MockNotifyPlatform(MockPlatform):
+    """Help to set up test notify service."""
+
+    def __init__(self, async_get_service):
+        """Return the notify service."""
+        super().__init__()
+        self.async_get_service = async_get_service
 
 
 async def test_same_targets(hass: HomeAssistant):
@@ -73,10 +93,16 @@ async def test_remove_targets(hass: HomeAssistant):
 class NotificationService(notify.BaseNotificationService):
     """A test class for notification services."""
 
-    def __init__(self, hass):
+    def __init__(self, hass, target_list={"a": 1, "b": 2}):
         """Initialize the service."""
+
+        async def _async_make_reloadable(hass):
+            """Initialize the reload service."""
+            await async_setup_reload_service(hass, "testnotify", [notify.DOMAIN])
+
         self.hass = hass
-        self.target_list = {"a": 1, "b": 2}
+        self.target_list = target_list
+        hass.async_create_task(_async_make_reloadable(hass))
 
     @property
     def targets(self):
@@ -97,3 +123,52 @@ async def test_warn_template(hass, caplog):
     # We should only log it once
     assert caplog.text.count("Passing templates to notify service is deprecated") == 1
     assert hass.states.get("persistent_notification.notification") is not None
+
+
+async def test_setup_platform_and_reload(hass, caplog, tmp_path):
+    """Test service setup and reload."""
+    get_service_called = Mock()
+
+    async def async_get_service(hass, config, discovery_info=None):
+        """Get notify service for mocked platform."""
+        get_service_called()
+        targetlist = {"a": 1, "b": 2}
+        return NotificationService(hass, targetlist)
+
+    # Mock notify services file
+    services_yaml_file = tmp_path / "services.yaml"
+    services_config = yaml.dump(
+        {"notify": [{"description": "My custom notify service", "fields": {}}]}
+    )
+    services_yaml_file.write_text(services_config)
+    loaded_platform = MockNotifyPlatform(async_get_service=async_get_service)
+    mock_platform(hass, "testnotify.notify", loaded_platform)
+    integration = hass.data[DATA_INTEGRATIONS]["testnotify"]
+    integration.file_path = tmp_path
+
+    # Setup the platform
+    await notify.async_setup(hass, {"notify": [{"platform": "testnotify"}]})
+    await hass.async_block_till_done()
+    assert hass.services.has_service("testnotify", SERVICE_RELOAD)
+    assert hass.services.has_service(notify.DOMAIN, "testnotify_a")
+    assert hass.services.has_service(notify.DOMAIN, "testnotify_b")
+    assert get_service_called.call_count == 1
+
+    # Perform a reload
+    new_yaml_config_file = tmp_path / "configuration.yaml"
+    new_yaml_config = yaml.dump({"notify": [{"platform": "testnotify"}]})
+    new_yaml_config_file.write_text(new_yaml_config)
+
+    with patch.object(hass_config, "YAML_CONFIG_FILE", new_yaml_config_file):
+        await hass.services.async_call(
+            "testnotify",
+            SERVICE_RELOAD,
+            {},
+            blocking=True,
+        )
+        await hass.async_block_till_done()
+
+    # Check if the notify services still exist
+    assert hass.services.has_service(notify.DOMAIN, "testnotify_a")
+    assert hass.services.has_service(notify.DOMAIN, "testnotify_b")
+    assert get_service_called.call_count == 2
