7f63b59f7dc911f3c78b550f90512d822a2bdfd2
J. Nick Koston <nick@koston.org>
2022-03-25 16:41:14 -1000

    more tweaks

--- homeassistant/components/samsungtv/config_flow.py
+++ homeassistant/components/samsungtv/config_flow.py
@@ -25,12 +25,7 @@ from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.device_registry import format_mac
 
-from .bridge import (
-    SamsungTVBridge,
-    SamsungTVEncryptedBridge,
-    async_get_device_info,
-    mac_from_device_info,
-)
+from .bridge import SamsungTVBridge, async_get_device_info, mac_from_device_info
 from .const import (
     CONF_MANUFACTURER,
     CONF_MODEL,
@@ -157,9 +152,7 @@ class SamsungTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def _try_connect(self) -> None:
         """Try to connect and check auth."""
-        _port, method, info = await async_get_device_info(
-            self.hass, self._bridge, self._host
-        )
+        method, info = await async_get_device_info(self.hass, self._bridge, self._host)
         if method is None:
             LOGGER.debug("No working config found")
             raise data_entry_flow.AbortFlow(RESULT_CANNOT_CONNECT)
@@ -182,11 +175,11 @@ class SamsungTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def _async_get_and_check_device_info(self) -> bool:
         """Try to get the device info."""
         if self._device_info is None:
-            _port, _method, info = await async_get_device_info(
+            method, info = await async_get_device_info(
                 self.hass, self._bridge, self._host
             )
             if not info:
-                if not _method:
+                if not method:
                     LOGGER.debug(
                         "Samsung host %s is not supported by either %s, %s or %s methods",
                         self._host,
@@ -263,8 +256,8 @@ class SamsungTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             if self._bridge.method != METHOD_LEGACY:
                 # Legacy bridge does not provide device info
                 await self._async_set_device_unique_id(raise_on_progress=False)
-            if isinstance(self._bridge, SamsungTVEncryptedBridge):
-                await self.async_step_encrypted_pairing()
+            if self._bridge.method == METHOD_ENCRYPTED_WEBSOCKET:
+                return await self.async_step_encrypted_pairing()
             return self._get_entry_from_bridge()
 
         return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
@@ -447,8 +440,8 @@ class SamsungTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             await self._try_connect()
             assert self._bridge
-            if isinstance(self._bridge, SamsungTVEncryptedBridge):
-                await self.async_step_encrypted_pairing()
+            if self._bridge.method == METHOD_ENCRYPTED_WEBSOCKET:
+                return await self.async_step_encrypted_pairing()
             return self._get_entry_from_bridge()
 
         return self.async_show_form(
