0ace998cf11f1191d4379a118c9be67839b69470
G Johansson <goran.johansson@shiftit.se>
2022-03-26 20:43:15 +0100

    Break out sensors for filesize (#68702)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- tests/components/filesize/test_sensor.py
+++ tests/components/filesize/test_sensor.py
@@ -1,9 +1,11 @@
 """The tests for the filesize sensor."""
 import os
 
-from homeassistant.const import CONF_FILE_PATH, STATE_UNKNOWN
+from homeassistant.components.filesize.const import DOMAIN
+from homeassistant.const import CONF_FILE_PATH, STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_component import async_update_entity
+from homeassistant.setup import async_setup_component
 
 from . import TEST_FILE, TEST_FILE_NAME, create_file
 
@@ -38,19 +40,18 @@ async def test_valid_path(
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
-    state = hass.states.get("sensor.file_txt")
+    state = hass.states.get("sensor.file_txt_size")
     assert state
     assert state.state == "0.0"
-    assert state.attributes.get("bytes") == 4
 
     await hass.async_add_executor_job(os.remove, testfile)
 
 
-async def test_state_unknown(
+async def test_state_unavailable(
     hass: HomeAssistant, tmpdir: str, mock_config_entry: MockConfigEntry
 ) -> None:
     """Verify we handle state unavailable."""
-    testfile = f"{tmpdir}/file"
+    testfile = f"{tmpdir}/file.txt"
     create_file(testfile)
     hass.config.allowlist_external_dirs = {tmpdir}
     mock_config_entry.add_to_hass(hass)
@@ -61,12 +62,32 @@ async def test_state_unknown(
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
-    state = hass.states.get("sensor.file")
+    state = hass.states.get("sensor.file_txt_size")
     assert state
     assert state.state == "0.0"
 
     await hass.async_add_executor_job(os.remove, testfile)
-    await async_update_entity(hass, "sensor.file")
+    await async_update_entity(hass, "sensor.file_txt_size")
 
-    state = hass.states.get("sensor.file")
-    assert state.state == STATE_UNKNOWN
+    state = hass.states.get("sensor.file_txt_size")
+    assert state.state == STATE_UNAVAILABLE
+
+
+async def test_import_query(hass: HomeAssistant, tmpdir: str) -> None:
+    """Test import from yaml."""
+    testfile = f"{tmpdir}/file.txt"
+    create_file(testfile)
+    hass.config.allowlist_external_dirs = {tmpdir}
+    config = {
+        "sensor": {
+            "platform": "filesize",
+            "file_paths": [testfile],
+        }
+    }
+
+    assert await async_setup_component(hass, "sensor", config)
+    await hass.async_block_till_done()
+
+    assert hass.config_entries.async_entries(DOMAIN)
+    data = hass.config_entries.async_entries(DOMAIN)[0].data
+    assert data[CONF_FILE_PATH] == testfile
