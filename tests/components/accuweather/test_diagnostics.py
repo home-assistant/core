71fced9c86790b5005fab0ef15e481498526cd97
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-03-27 21:24:33 +0200

    Fix load fixture

--- tests/components/accuweather/test_diagnostics.py
+++ tests/components/accuweather/test_diagnostics.py
@@ -10,7 +10,9 @@ async def test_entry_diagnostics(hass, hass_client):
     """Test config entry diagnostics."""
     entry = await init_integration(hass)
 
-    coordinator_data = json.loads(load_fixture("current_conditions_data.json"))
+    coordinator_data = json.loads(
+        load_fixture("current_conditions_data.json", "accuweather")
+    )
     coordinator_data["forecast"] = {}
 
     result = await get_diagnostics_for_config_entry(hass, hass_client, entry)
