a53f7f9474d6d2b5d54e406cebfdac7fec7f2068
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2022-03-27 16:08:24 +0200

    Update pylint to 2.13.2 (#68704)

--- homeassistant/components/recorder/util.py
+++ homeassistant/components/recorder/util.py
@@ -359,7 +359,9 @@ def setup_connection_for_dialect(
             version = _extract_version_from_server_response(version_string)
 
             if version and version < MIN_VERSION_SQLITE_ROWNUM:
-                instance._db_supports_row_number = False
+                instance._db_supports_row_number = (  # pylint: disable=[protected-access]
+                    False
+                )
             if not version or version < MIN_VERSION_SQLITE:
                 _warn_unsupported_version(
                     version or version_string, "SQLite", MIN_VERSION_SQLITE
@@ -381,14 +383,18 @@ def setup_connection_for_dialect(
 
             if is_maria_db:
                 if version and version < MIN_VERSION_MARIA_DB_ROWNUM:
-                    instance._db_supports_row_number = False
+                    instance._db_supports_row_number = (  # pylint: disable=[protected-access]
+                        False
+                    )
                 if not version or version < MIN_VERSION_MARIA_DB:
                     _warn_unsupported_version(
                         version or version_string, "MariaDB", MIN_VERSION_MARIA_DB
                     )
             else:
                 if version and version < MIN_VERSION_MYSQL_ROWNUM:
-                    instance._db_supports_row_number = False
+                    instance._db_supports_row_number = (  # pylint: disable=[protected-access]
+                        False
+                    )
                 if not version or version < MIN_VERSION_MYSQL:
                     _warn_unsupported_version(
                         version or version_string, "MySQL", MIN_VERSION_MYSQL
