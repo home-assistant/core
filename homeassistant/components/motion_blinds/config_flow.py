b287470910ad0a987869b78ea280f1d9e443f492
starkillerOG <starkiller.og@gmail.com>
2022-03-29 00:18:46 +0200

    include mac_address

--- homeassistant/components/motion_blinds/config_flow.py
+++ homeassistant/components/motion_blinds/config_flow.py
@@ -80,7 +80,9 @@ class MotionBlindsFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         await self.async_set_unique_id(mac_address)
         self._abort_if_unique_id_configured(updates={CONF_HOST: discovery_info.ip})
 
-        self.context.update({"title_placeholders": {"name": discovery_info.ip}})
+        self.context.update(
+            {"title_placeholders": {"name": f"{mac_address} ({discovery_info.ip})"}}
+        )
 
         self._host = discovery_info.ip
         return await self.async_step_connect()
