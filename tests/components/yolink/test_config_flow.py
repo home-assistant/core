bd13866b571f0fafdf87750c78f859b5a8b00f7a
justin <justin@yosmart.com>
2022-03-31 02:56:23 +0000

    Add test flow and update .coveragerc

---
+++ tests/components/yolink/test_config_flow.py
@@ -0,0 +1,138 @@
+"""Test yolink config flow."""
+import asyncio
+from http import HTTPStatus
+from unittest.mock import patch
+
+import pytest
+
+from homeassistant import config_entries, data_entry_flow, setup
+from homeassistant.components.http import CONF_BASE_URL, DOMAIN as DOMAIN_HTTP
+from homeassistant.components.yolink import config_flow
+from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET
+from homeassistant.helpers import config_entry_oauth2_flow
+
+CLIENT_ID = "12345"
+CLIENT_SECRET = "6789"
+YOLINK_HOST = "192.168.30.104"
+YOLINK_HTTP_HOST = f"http://{YOLINK_HOST}:1080"
+DOMAIN = "yolink"
+OAUTH2_AUTHORIZE = f"{YOLINK_HTTP_HOST}/oauth/v2/authorization.htm"
+OAUTH2_TOKEN = f"{YOLINK_HTTP_HOST}/opesn/yolink/token"
+
+
+@pytest.fixture()
+async def mock_impl(hass):
+    """Mock implementation."""
+    await setup.async_setup_component(hass, "http", {})
+
+    impl = config_entry_oauth2_flow.LocalOAuth2Implementation(
+        hass,
+        DOMAIN,
+        CLIENT_ID,
+        CLIENT_SECRET,
+        OAUTH2_AUTHORIZE,
+        OAUTH2_TOKEN,
+    )
+    config_flow.OAuth2FlowHandler.async_register_implementation(hass, impl)
+    return impl
+
+
+async def test_abort_if_no_configuration(hass):
+    """Check flow abort when no configuration."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "missing_configuration"
+
+
+async def test_full_flow(
+    hass, hass_client_no_auth, aioclient_mock, current_request_with_host
+):
+    """Check full flow."""
+    assert await setup.async_setup_component(
+        hass,
+        DOMAIN,
+        {
+            DOMAIN: {
+                CONF_CLIENT_ID: CLIENT_ID,
+                CONF_CLIENT_SECRET: CLIENT_SECRET,
+            },
+            DOMAIN_HTTP: {CONF_BASE_URL: "https://example.com"},
+        },
+    )
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    state = config_entry_oauth2_flow._encode_jwt(
+        hass,
+        {
+            "flow_id": result["flow_id"],
+            "redirect_uri": "https://example.com/auth/external/callback",
+        },
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_EXTERNAL_STEP
+    assert result["url"] == (
+        f"{OAUTH2_AUTHORIZE}?response_type=code&client_id={CLIENT_ID}"
+        "&redirect_uri=https://example.com/auth/external/callback"
+        f"&state={state}"
+    )
+
+    client = await hass_client_no_auth()
+    resp = await client.get(f"/auth/external/callback?code=abcd&state={state}")
+    assert resp.status == HTTPStatus.OK
+    assert resp.headers["content-type"] == "text/html; charset=utf-8"
+
+    aioclient_mock.post(
+        OAUTH2_TOKEN,
+        json={
+            "refresh_token": "mock-refresh-token",
+            "access_token": "mock-access-token",
+            "type": "Bearer",
+            "expires_in": 60,
+        },
+    )
+
+    with patch("homeassistant.components.yolink.api.AuthenticationManager"), patch(
+        "homeassistant.components.yolink.async_setup_entry", return_value=True
+    ) as mock_setup:
+        result = await hass.config_entries.flow.async_configure(result["flow_id"])
+
+    assert result["data"]["auth_implementation"] == DOMAIN
+
+    result["data"]["token"].pop("expires_at")
+    assert result["data"]["token"] == {
+        "refresh_token": "mock-refresh-token",
+        "access_token": "mock-access-token",
+        "type": "Bearer",
+        "expires_in": 60,
+    }
+
+    assert DOMAIN in hass.config.components
+    entry = hass.config_entries.async_entries(DOMAIN)[0]
+    assert entry.state is config_entries.ConfigEntryState.LOADED
+
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+    assert len(mock_setup.mock_calls) == 1
+
+
+async def test_abort_if_authorization_timeout(
+    hass, mock_impl, current_request_with_host
+):
+    """Check Somfy authorization timeout."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    flow = config_flow.OAuth2FlowHandler()
+    flow.hass = hass
+
+    with patch.object(
+        mock_impl, "async_generate_authorize_url", side_effect=asyncio.TimeoutError
+    ):
+        result = await flow.async_step_user()
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "authorize_url_timeout"
