7eecf486453cc9bd0cd3437d47305270592eaa11
Paulus Schoutsen <balloob@gmail.com>
2022-03-31 12:04:55 -0700

    DNS IP: Remove deprecated YAML import (#69007)
    Co-authored-by: Franck Nijhof <git@frenck.dev>

--- homeassistant/components/dnsip/__init__.py
+++ homeassistant/components/dnsip/__init__.py
@@ -1,14 +1,10 @@
 """The dnsip component."""
 from __future__ import annotations
 
-import logging
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import PLATFORMS
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
