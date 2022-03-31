70ec25acf7ea99e48731fd2d633777279a22a0ca
Mike Kasper <m_kasper@sbcglobal.net>
2022-03-31 14:27:22 +0000

    Added test coverage for model and init

---
+++ tests/components/aladdin_connect/test_init.py
@@ -0,0 +1,26 @@
+"""Test for Aladdin Connect init logic."""
+from homeassistant.components.aladdin_connect.const import DOMAIN
+from homeassistant.config_entries import ConfigEntryState
+from homeassistant.setup import async_setup_component
+
+from tests.common import MockConfigEntry
+
+
+async def test_unload_entry(hass):
+    """Test successful unload of entry."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={"username": "test-user", "password": "test-password"},
+    )
+    entry.add_to_hass(hass)
+
+    assert (await async_setup_component(hass, DOMAIN, entry)) is True
+
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+    assert entry.state is ConfigEntryState.LOADED
+
+    assert await hass.config_entries.async_unload(entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert entry.state is ConfigEntryState.NOT_LOADED
+    assert not hass.data.get(DOMAIN)
