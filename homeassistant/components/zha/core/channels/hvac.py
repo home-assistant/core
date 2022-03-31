a8ad3292c82c646dd627e66a546b0a3722c691a7
puddly <32534428+puddly@users.noreply.github.com>
2022-03-31 11:26:27 -0400

    Bump zigpy to 0.44.1 and zha-quirks to 0.0.69 (#68921)
    * Make unit tests pass
    
    * Flip response type check to not rely on it being a list
    https://github.com/zigpy/zigpy/pull/716#issuecomment-1025236190
    
    * Bump zigpy and quirks versions to ZCLR8 releases
    
    * Fix renamed zigpy cluster attributes
    
    * Handle the default response for ZLL `get_group_identifiers`
    
    * Add more error context to `stage failed` errors
    
    * Fix unit test returning lists as ZCL request responses
    
    * Always load quirks when testing ZHA
    
    * Bump zha-quirks to 0.0.69

--- homeassistant/components/zha/core/channels/hvac.py
+++ homeassistant/components/zha/core/channels/hvac.py
@@ -70,7 +70,7 @@ class FanChannel(ZigbeeChannel):
     @callback
     def attribute_updated(self, attrid: int, value: Any) -> None:
         """Handle attribute update from fan cluster."""
-        attr_name = self.cluster.attributes.get(attrid, [attrid])[0]
+        attr_name = self._get_attribute_name(attrid)
         self.debug(
             "Attribute report '%s'[%s] = %s", self.cluster.name, attr_name, value
         )
@@ -90,7 +90,7 @@ class ThermostatChannel(ZigbeeChannel):
     """Thermostat channel."""
 
     REPORT_CONFIG = (
-        {"attr": "local_temp", "config": REPORT_CONFIG_CLIMATE},
+        {"attr": "local_temperature", "config": REPORT_CONFIG_CLIMATE},
         {"attr": "occupied_cooling_setpoint", "config": REPORT_CONFIG_CLIMATE},
         {"attr": "occupied_heating_setpoint", "config": REPORT_CONFIG_CLIMATE},
         {"attr": "unoccupied_cooling_setpoint", "config": REPORT_CONFIG_CLIMATE},
@@ -107,7 +107,7 @@ class ThermostatChannel(ZigbeeChannel):
         "abs_max_heat_setpoint_limit": True,
         "abs_min_cool_setpoint_limit": True,
         "abs_max_cool_setpoint_limit": True,
-        "ctrl_seqe_of_oper": False,
+        "ctrl_sequence_of_oper": False,
         "max_cool_setpoint_limit": True,
         "max_heat_setpoint_limit": True,
         "min_cool_setpoint_limit": True,
@@ -135,9 +135,9 @@ class ThermostatChannel(ZigbeeChannel):
         return self.cluster.get("abs_min_heat_setpoint_limit", 700)
 
     @property
-    def ctrl_seqe_of_oper(self) -> int:
+    def ctrl_sequence_of_oper(self) -> int:
         """Control Sequence of operations attribute."""
-        return self.cluster.get("ctrl_seqe_of_oper", 0xFF)
+        return self.cluster.get("ctrl_sequence_of_oper", 0xFF)
 
     @property
     def max_cool_setpoint_limit(self) -> int:
@@ -172,9 +172,9 @@ class ThermostatChannel(ZigbeeChannel):
         return sp_limit
 
     @property
-    def local_temp(self) -> int | None:
+    def local_temperature(self) -> int | None:
         """Thermostat temperature."""
-        return self.cluster.get("local_temp")
+        return self.cluster.get("local_temperature")
 
     @property
     def occupancy(self) -> int | None:
@@ -229,7 +229,7 @@ class ThermostatChannel(ZigbeeChannel):
     @callback
     def attribute_updated(self, attrid, value):
         """Handle attribute update cluster."""
-        attr_name = self.cluster.attributes.get(attrid, [attrid])[0]
+        attr_name = self._get_attribute_name(attrid)
         self.debug(
             "Attribute report '%s'[%s] = %s", self.cluster.name, attr_name, value
         )
@@ -300,7 +300,7 @@ class ThermostatChannel(ZigbeeChannel):
     @staticmethod
     def check_result(res: list) -> bool:
         """Normalize the result."""
-        if not isinstance(res, list):
+        if isinstance(res, Exception):
             return False
 
         return all(record.status == Status.SUCCESS for record in res[0])
