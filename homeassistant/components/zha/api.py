94ae8dd3fa82cd2627aed39e65ed5e4220800db4
puddly <32534428+puddly@users.noreply.github.com>
2022-03-31 11:26:27 -0400

    Bump zigpy to 0.44.1 and zha-quirks to 0.0.69 (#68921)
    * Make unit tests pass
    
    * Flip response type check to not rely on it being a list
    https://github.com/zigpy/zigpy/pull/716#issuecomment-1025236190
    
    * Bump zigpy and quirks versions to ZCLR8 releases
    
    * Fix renamed zigpy cluster attributes
    
    * Handle the default response for ZLL `get_group_identifiers`
    
    * Add more error context to `stage failed` errors
    
    * Fix unit test returning lists as ZCL request responses
    
    * Always load quirks when testing ZHA
    
    * Bump zha-quirks to 0.0.69

--- homeassistant/components/zha/api.py
+++ homeassistant/components/zha/api.py
@@ -650,7 +650,7 @@ async def websocket_device_cluster_attributes(
         )
         if attributes is not None:
             for attr_id, attr in attributes.items():
-                cluster_attributes.append({ID: attr_id, ATTR_NAME: attr[0]})
+                cluster_attributes.append({ID: attr_id, ATTR_NAME: attr.name})
     _LOGGER.debug(
         "Requested attributes for: %s: %s, %s: '%s', %s: %s, %s: %s",
         ATTR_CLUSTER_ID,
@@ -700,7 +700,7 @@ async def websocket_device_cluster_commands(
                     {
                         TYPE: CLIENT,
                         ID: cmd_id,
-                        ATTR_NAME: cmd[0],
+                        ATTR_NAME: cmd.name,
                     }
                 )
             for cmd_id, cmd in commands[CLUSTER_COMMANDS_SERVER].items():
@@ -708,7 +708,7 @@ async def websocket_device_cluster_commands(
                     {
                         TYPE: CLUSTER_COMMAND_SERVER,
                         ID: cmd_id,
-                        ATTR_NAME: cmd[0],
+                        ATTR_NAME: cmd.name,
                     }
                 )
     _LOGGER.debug(
