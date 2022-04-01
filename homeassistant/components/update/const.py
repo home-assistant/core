eed580f0a8847a0fdbae7c5a4465ef1324997ac0
Franck Nijhof <git@frenck.dev>
2022-04-01 20:11:17 +0200

    Rename current_version to installed_version in Update platform (#69093)

--- homeassistant/components/update/const.py
+++ homeassistant/components/update/const.py
@@ -22,7 +22,7 @@ SERVICE_SKIP: Final = "skip"
 
 ATTR_AUTO_UPDATE: Final = "auto_update"
 ATTR_BACKUP: Final = "backup"
-ATTR_CURRENT_VERSION: Final = "current_version"
+ATTR_INSTALLED_VERSION: Final = "installed_version"
 ATTR_IN_PROGRESS: Final = "in_progress"
 ATTR_LATEST_VERSION: Final = "latest_version"
 ATTR_RELEASE_SUMMARY: Final = "release_summary"
