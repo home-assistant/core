62457be2501a3ebf571ac1eeb933161e523b04a6
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2022-04-02 10:27:24 +0000

    Refactor constants

--- homeassistant/components/tradfri/const.py
+++ homeassistant/components/tradfri/const.py
@@ -1,32 +1,35 @@
 """Consts used by Tradfri."""
+import logging
 from typing import Final
 
 from homeassistant.components.light import SUPPORT_TRANSITION
-from homeassistant.const import (  # noqa: F401 pylint: disable=unused-import
-    CONF_HOST,
-    Platform,
-)
+from homeassistant.const import Platform
+
+_LOGGER = logging.getLogger(__name__)
 
 ATTR_AUTO = "Auto"
 ATTR_DIMMER = "dimmer"
+ATTR_FILTER_LIFE_REMAINING: Final = "filter_life_remaining"
 ATTR_HUE = "hue"
 ATTR_SAT = "saturation"
+ATTR_MAX_FAN_STEPS = 49
+ATTR_MODEL = "model"
 ATTR_TRADFRI_GATEWAY = "Gateway"
 ATTR_TRADFRI_GATEWAY_MODEL = "E1526"
 ATTR_TRADFRI_MANUFACTURER = "IKEA of Sweden"
 ATTR_TRANSITION_TIME = "transition_time"
-ATTR_MODEL = "model"
 CONF_IDENTITY = "identity"
 CONF_IMPORT_GROUPS = "import_groups"
 CONF_GATEWAY_ID = "gateway_id"
 CONF_KEY = "key"
-
-DOMAIN = "tradfri"
-KEY_API = "tradfri_api"
+COORDINATOR = "coordinator"
+COORDINATOR_LIST = "coordinator_list"
 DEVICES = "tradfri_devices"
-SIGNAL_GW = "tradfri.gw_status"
+DOMAIN = "tradfri"
+FACTORY = "tradfri_factory"
+KEY_API = "tradfri_api"
 KEY_SECURITY_CODE = "security_code"
-SUPPORTED_LIGHT_FEATURES = SUPPORT_TRANSITION
+LISTENERS = "tradfri_listeners"
 PLATFORMS = [
     Platform.COVER,
     Platform.FAN,
@@ -34,12 +37,7 @@ PLATFORMS = [
     Platform.SENSOR,
     Platform.SWITCH,
 ]
-TIMEOUT_API = 30
-ATTR_MAX_FAN_STEPS = 49
-
 SCAN_INTERVAL = 60  # Interval for updating the coordinator
-
-COORDINATOR = "coordinator"
-COORDINATOR_LIST = "coordinator_list"
-
-ATTR_FILTER_LIFE_REMAINING: Final = "filter_life_remaining"
+SIGNAL_GW = "tradfri.gw_status"
+SUPPORTED_LIGHT_FEATURES = SUPPORT_TRANSITION
+TIMEOUT_API = 30
