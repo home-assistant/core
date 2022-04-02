9d9f7132a544004a7178ed6f0916a29edbb87968
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2022-04-02 11:11:11 +0000

    Run isort

--- homeassistant/components/tradfri/cover.py
+++ homeassistant/components/tradfri/cover.py
@@ -12,13 +12,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .base_class import TradfriBaseEntity
-from .const import (
-    CONF_GATEWAY_ID,
-    COORDINATOR,
-    COORDINATOR_LIST,
-    DOMAIN,
-    KEY_API,
-)
+from .const import CONF_GATEWAY_ID, COORDINATOR, COORDINATOR_LIST, DOMAIN, KEY_API
 from .coordinator import TradfriDeviceDataUpdateCoordinator
 
 ATTR_MODEL = "model"
