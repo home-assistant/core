2bdde2e731fa0f3ce242284464cea621545f7e97
J. Nick Koston <nick@koston.org>
2022-04-03 05:51:42 -0600

    Exclude supported features and attribution from being recorded in the database (#69165)

--- homeassistant/components/recorder/models.py
+++ homeassistant/components/recorder/models.py
@@ -38,7 +38,7 @@ from homeassistant.core import Context, Event, EventOrigin, State, split_entity_
 from homeassistant.helpers.typing import UNDEFINED, UndefinedType
 import homeassistant.util.dt as dt_util
 
-from .const import JSON_DUMP
+from .const import ALL_DOMAIN_EXCLUDE_ATTRS, JSON_DUMP
 
 # SQLAlchemy Schema
 # pylint: disable=invalid-name
@@ -269,11 +269,12 @@ class StateAttributes(Base):  # type: ignore[misc,valid-type]
         if state is None:
             return "{}"
         domain = split_entity_id(state.entity_id)[0]
-        if exclude_attrs := exclude_attrs_by_domain.get(domain):
-            return JSON_DUMP(
-                {k: v for k, v in state.attributes.items() if k not in exclude_attrs}
-            )
-        return JSON_DUMP(state.attributes)
+        exclude_attrs = (
+            exclude_attrs_by_domain.get(domain, set()) | ALL_DOMAIN_EXCLUDE_ATTRS
+        )
+        return JSON_DUMP(
+            {k: v for k, v in state.attributes.items() if k not in exclude_attrs}
+        )
 
     @staticmethod
     def hash_shared_attrs(shared_attrs: str) -> int:
