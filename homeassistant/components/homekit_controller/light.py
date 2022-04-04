5ee6331b3c7b80c072564ad2eef1c54cfb4d36bb
Erik <erik@montnemery.com>
2022-04-04 20:47:58 +0200

    Update according to discussion in PR

--- homeassistant/components/homekit_controller/light.py
+++ homeassistant/components/homekit_controller/light.py
@@ -82,41 +82,21 @@ class HomeKitLight(HomeKitEntity, LightEntity):
     @property
     def color_mode(self) -> str:
         """Return the color mode of the light."""
-        if self.service.has(CharacteristicsTypes.HUE):
+        # Homekit does not allow using CT for lights supporting HS
+        if self.service.has(CharacteristicsTypes.HUE) or self.service.has(
+            CharacteristicsTypes.SATURATION
+        ):
             return COLOR_MODE_HS
-
-        if self.service.has(CharacteristicsTypes.SATURATION):
-            return COLOR_MODE_HS
-
         if self.service.has(CharacteristicsTypes.COLOR_TEMPERATURE):
             return COLOR_MODE_COLOR_TEMP
-
         if self.service.has(CharacteristicsTypes.BRIGHTNESS):
             return COLOR_MODE_BRIGHTNESS
-
         return COLOR_MODE_ONOFF
 
     @property
     def supported_color_modes(self) -> set[str] | None:
         """Flag supported color modes."""
-        color_modes = set()
-
-        if self.service.has(CharacteristicsTypes.COLOR_TEMPERATURE):
-            color_modes.add(COLOR_MODE_COLOR_TEMP)
-
-        if self.service.has(CharacteristicsTypes.HUE):
-            color_modes.add(COLOR_MODE_HS)
-
-        if self.service.has(CharacteristicsTypes.SATURATION):
-            color_modes.add(COLOR_MODE_HS)
-
-        if not color_modes and self.service.has(CharacteristicsTypes.BRIGHTNESS):
-            color_modes.add(COLOR_MODE_BRIGHTNESS)
-
-        if not color_modes:
-            color_modes.add(COLOR_MODE_ONOFF)
-
-        return color_modes
+        return {self.color_mode}
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the specified light on."""
