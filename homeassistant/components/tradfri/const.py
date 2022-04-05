6e718d4e23cc41b61af80f26b2bcd445e0dce24e
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2022-04-05 14:00:45 +0200

    Cleanup constants in Tradfri integration (#69125)
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/tradfri/const.py
+++ homeassistant/components/tradfri/const.py
@@ -1,45 +1,13 @@
 """Consts used by Tradfri."""
-from typing import Final
+import logging
 
-from homeassistant.components.light import SUPPORT_TRANSITION
-from homeassistant.const import (  # noqa: F401 pylint: disable=unused-import
-    CONF_HOST,
-    Platform,
-)
+LOGGER = logging.getLogger(__package__)
 
-ATTR_AUTO = "Auto"
-ATTR_DIMMER = "dimmer"
-ATTR_HUE = "hue"
-ATTR_SAT = "saturation"
-ATTR_TRADFRI_GATEWAY = "Gateway"
-ATTR_TRADFRI_GATEWAY_MODEL = "E1526"
-ATTR_TRADFRI_MANUFACTURER = "IKEA of Sweden"
-ATTR_TRANSITION_TIME = "transition_time"
-ATTR_MODEL = "model"
-CONF_IDENTITY = "identity"
-CONF_IMPORT_GROUPS = "import_groups"
 CONF_GATEWAY_ID = "gateway_id"
+CONF_IDENTITY = "identity"
 CONF_KEY = "key"
-
-DOMAIN = "tradfri"
-KEY_API = "tradfri_api"
-DEVICES = "tradfri_devices"
-SIGNAL_GW = "tradfri.gw_status"
-KEY_SECURITY_CODE = "security_code"
-SUPPORTED_LIGHT_FEATURES = SUPPORT_TRANSITION
-PLATFORMS = [
-    Platform.COVER,
-    Platform.FAN,
-    Platform.LIGHT,
-    Platform.SENSOR,
-    Platform.SWITCH,
-]
-TIMEOUT_API = 30
-ATTR_MAX_FAN_STEPS = 49
-
-SCAN_INTERVAL = 60  # Interval for updating the coordinator
-
 COORDINATOR = "coordinator"
 COORDINATOR_LIST = "coordinator_list"
-
-ATTR_FILTER_LIFE_REMAINING: Final = "filter_life_remaining"
+DOMAIN = "tradfri"
+FACTORY = "tradfri_factory"
+KEY_API = "tradfri_api"
