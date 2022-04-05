6eb4ab61023d4e46eca602927089d82bf4e3a516
J. Nick Koston <nick@koston.org>
2022-04-05 10:41:14 -1000

    Reduce amount of I/O that happens in the event loop when importing components

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -296,7 +296,7 @@ class ConfigEntry:
         )
 
         try:
-            component = integration.get_component()
+            component = await integration.async_get_component()
         except ImportError as err:
             _LOGGER.error(
                 "Error importing integration %s to set up %s configuration entry: %s",
@@ -449,7 +449,7 @@ class ConfigEntry:
                 self.reason = None
                 return True
 
-        component = integration.get_component()
+        component = await integration.async_get_component()
 
         if integration.domain == self.domain:
             if not self.state.recoverable:
@@ -507,7 +507,7 @@ class ConfigEntry:
             # entry.
             return
 
-        component = integration.get_component()
+        component = await integration.async_get_component()
         if not hasattr(component, "async_remove_entry"):
             return
         try:
@@ -539,7 +539,7 @@ class ConfigEntry:
             return True
 
         integration = await loader.async_get_integration(hass, self.domain)
-        component = integration.get_component()
+        component = await integration.async_get_component()
         supports_migrate = hasattr(component, "async_migrate_entry")
         if not supports_migrate:
             _LOGGER.error(
@@ -1622,12 +1622,12 @@ def _handle_entry_updated_filter(event: Event) -> bool:
 async def support_entry_unload(hass: HomeAssistant, domain: str) -> bool:
     """Test if a domain supports entry unloading."""
     integration = await loader.async_get_integration(hass, domain)
-    component = integration.get_component()
+    component = await integration.async_get_component()
     return hasattr(component, "async_unload_entry")
 
 
 async def support_remove_from_device(hass: HomeAssistant, domain: str) -> bool:
     """Test if a domain supports being removed from a device."""
     integration = await loader.async_get_integration(hass, domain)
-    component = integration.get_component()
+    component = await integration.async_get_component()
     return hasattr(component, "async_remove_config_entry_device")
