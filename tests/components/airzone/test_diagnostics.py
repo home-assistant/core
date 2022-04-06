b5871543b4030f68c5d1cb22dfa8c22c798f4828
Álvaro Fernández Rojas <noltari@gmail.com>
2022-04-06 08:45:50 +0200

    tests: airzone: remove diagnostics fixture
    Only check some key values instead of the full diagnostics, since it would
    increase the changes whenever the aioairzone library evolved.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- tests/components/airzone/test_diagnostics.py
+++ tests/components/airzone/test_diagnostics.py
@@ -1,15 +1,21 @@
 """The diagnostics tests for the Airzone platform."""
 
-import json
-
+from aioairzone.const import (
+    AZD_ID,
+    AZD_MASTER,
+    AZD_SYSTEM,
+    AZD_SYSTEMS,
+    AZD_ZONES,
+    AZD_ZONES_NUM,
+)
 from aiohttp import ClientSession
 
 from homeassistant.components.airzone.const import DOMAIN
+from homeassistant.const import CONF_HOST, CONF_PORT
 from homeassistant.core import HomeAssistant
 
-from .util import async_init_integration
+from .util import CONFIG, async_init_integration
 
-from tests.common import load_fixture
 from tests.components.diagnostics import get_diagnostics_for_config_entry
 
 
@@ -22,11 +28,30 @@ async def test_config_entry_diagnostics(
 
     config_entry = hass.config_entries.async_entries(DOMAIN)[0]
 
-    diagnostics_fixture = json.loads(
-        load_fixture("airzone/diagnostics_config_entry.json")
-    )
+    diag = await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
 
-    assert (
-        await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
-        == diagnostics_fixture
-    )
+    assert diag["info"][CONF_HOST] == CONFIG[CONF_HOST]
+    assert diag["info"][CONF_PORT] == CONFIG[CONF_PORT]
+
+    assert diag["data"][AZD_SYSTEMS]["1"][AZD_ID] == 1
+    assert diag["data"][AZD_SYSTEMS]["1"][AZD_ZONES_NUM] == 5
+
+    assert diag["data"][AZD_ZONES]["1:1"][AZD_ID] == 1
+    assert diag["data"][AZD_ZONES]["1:1"][AZD_MASTER] == 1
+    assert diag["data"][AZD_ZONES]["1:1"][AZD_SYSTEM] == 1
+
+    assert diag["data"][AZD_ZONES]["1:2"][AZD_ID] == 2
+    assert diag["data"][AZD_ZONES]["1:2"][AZD_MASTER] == 0
+    assert diag["data"][AZD_ZONES]["1:2"][AZD_SYSTEM] == 1
+
+    assert diag["data"][AZD_ZONES]["1:3"][AZD_ID] == 3
+    assert diag["data"][AZD_ZONES]["1:3"][AZD_MASTER] == 0
+    assert diag["data"][AZD_ZONES]["1:3"][AZD_SYSTEM] == 1
+
+    assert diag["data"][AZD_ZONES]["1:4"][AZD_ID] == 4
+    assert diag["data"][AZD_ZONES]["1:4"][AZD_MASTER] == 0
+    assert diag["data"][AZD_ZONES]["1:4"][AZD_SYSTEM] == 1
+
+    assert diag["data"][AZD_ZONES]["1:5"][AZD_ID] == 5
+    assert diag["data"][AZD_ZONES]["1:5"][AZD_MASTER] == 0
+    assert diag["data"][AZD_ZONES]["1:5"][AZD_SYSTEM] == 1
