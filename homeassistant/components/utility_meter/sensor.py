891be26ec4523593d9e07891ff081749325cb1f3
Diogo Gomes <dgomes@ua.pt>
2022-04-09 00:44:22 +0100

    stale

--- homeassistant/components/utility_meter/sensor.py
+++ homeassistant/components/utility_meter/sensor.py
@@ -254,9 +254,6 @@ class UtilitySensorExtraStoredData(SensorExtraStoredData):
             last_period: Decimal = Decimal(restored["last_period"])
             last_reset: datetime | None = dt_util.parse_datetime(restored["last_reset"])
             status: str = restored["status"]
-
-        except InvalidOperation:
-            return None
         except TypeError:
             # restored is not a dict
             return None
