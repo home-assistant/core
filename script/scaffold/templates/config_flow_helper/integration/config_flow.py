cee3d5cc49cbb2c583025ad286613414ab1fe6e1
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-04-11 03:20:56 -0400

    Add SelectorType enum and TypedDicts for each selector's data (#68399)
    * rebase off current
    
    * rearrange
    
    * Overload selector function
    
    * Update/fix all selector references
    
    * better typing?
    
    * remove extra option
    
    * move things around
    
    * Switch to Sequence type to avoid ignoring mypy error
    
    * Get rid of ...'s
    
    * Improve typing to reduce number of ignores
    
    * Remove all typing ignores
    
    * Make config optional for selectors that don't need a config
    
    * add missing unit prefixes
    
    * Rename TypedDicts
    
    * Update homeassistant/helpers/selector.py
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>
    
    * review feedback
    
    * remove peta from integration integration
    
    * Fix min_max
    
    * Revert change to selector function
    
    * Fix logic
    
    * Add typing for selector classes
    
    * Update selector.py
    
    * Fix indent
    
    Co-authored-by: Erik Montnemery <erik@montnemery.com>

--- script/scaffold/templates/config_flow_helper/integration/config_flow.py
+++ script/scaffold/templates/config_flow_helper/integration/config_flow.py
@@ -18,15 +18,15 @@ from .const import DOMAIN
 
 OPTIONS_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_ENTITY_ID): selector.selector(
-            {"entity": {"domain": "sensor"}}
+        vol.Required(CONF_ENTITY_ID): selector.EntitySelector(
+            selector.EntitySelectorConfig(domain="sensor")
         ),
     }
 )
 
 CONFIG_SCHEMA = vol.Schema(
     {
-        vol.Required("name"): selector.selector({"text": {}}),
+        vol.Required("name"): selector.TextSelector(),
     }
 ).extend(OPTIONS_SCHEMA.schema)
 
