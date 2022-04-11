6973aaf21c9570baec3322a8cd1ddc936939cd04
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-04-11 18:12:58 +0200

    Add diagnostics platform

---
+++ tests/components/airly/test_diagnostics.py
@@ -0,0 +1,23 @@
+"""Test Airly diagnostics."""
+import json
+
+from tests.common import load_fixture
+from tests.components.airly import init_integration
+from tests.components.diagnostics import get_diagnostics_for_config_entry
+
+
+async def test_entry_diagnostics(hass, hass_client):
+    """Test config entry diagnostics."""
+    entry = await init_integration(hass)
+
+    coordinator_data = json.loads(load_fixture("diagnostics_data.json", "airly"))
+
+    result = await get_diagnostics_for_config_entry(hass, hass_client, entry)
+
+    assert result["config_entry_data"] == {
+        "api_key": "**REDACTED**",
+        "latitude": "**REDACTED**",
+        "longitude": "**REDACTED**",
+        "name": "Home",
+    }
+    assert result["coordinator_data"] == coordinator_data
