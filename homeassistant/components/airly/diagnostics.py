37706c42d6ffb4f8ab85b77683b02baa18bce9cc
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-04-12 15:12:59 +0200

    Use the entire config_entry for diagnostics

--- homeassistant/components/airly/diagnostics.py
+++ homeassistant/components/airly/diagnostics.py
@@ -3,13 +3,18 @@ from __future__ import annotations
 
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE
+from homeassistant.const import (
+    CONF_API_KEY,
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+    CONF_UNIQUE_ID,
+)
 from homeassistant.core import HomeAssistant
 
 from . import AirlyDataUpdateCoordinator
 from .const import DOMAIN
 
-TO_REDACT = {CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE}
+TO_REDACT = {CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE, CONF_UNIQUE_ID}
 
 
 async def async_get_config_entry_diagnostics(
@@ -19,7 +24,7 @@ async def async_get_config_entry_diagnostics(
     coordinator: AirlyDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     diagnostics_data = {
-        "config_entry_data": async_redact_data(dict(config_entry.as_dict(), TO_REDACT),
+        "config_entry": async_redact_data(config_entry.as_dict(), TO_REDACT),
         "coordinator_data": coordinator.data,
     }
 
--- tests/components/airly/test_diagnostics.py
+++ tests/components/airly/test_diagnostics.py
@@ -1,6 +1,8 @@
 """Test Airly diagnostics."""
 import json
 
+from homeassistant.components.diagnostics import REDACTED
+
 from tests.common import load_fixture
 from tests.components.airly import init_integration
 from tests.components.diagnostics import get_diagnostics_for_config_entry
@@ -14,10 +16,22 @@ async def test_entry_diagnostics(hass, aioclient_mock, hass_client):
 
     result = await get_diagnostics_for_config_entry(hass, hass_client, entry)
 
-    assert result["config_entry_data"] == {
-        "api_key": "**REDACTED**",
-        "latitude": "**REDACTED**",
-        "longitude": "**REDACTED**",
-        "name": "Home",
+    assert result["config_entry"] == {
+        "entry_id": entry.entry_id,
+        "version": 1,
+        "domain": "airly",
+        "title": "Dom",
+        "data": {
+            "latitude": REDACTED,
+            "longitude": REDACTED,
+            "name": "Home",
+            "api_key": REDACTED,
+        },
+        "options": {},
+        "pref_disable_new_entities": False,
+        "pref_disable_polling": False,
+        "source": "user",
+        "unique_id": REDACTED,
+        "disabled_by": None,
     }
     assert result["coordinator_data"] == coordinator_data
