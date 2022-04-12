633e4c2935de4c3f6be45da5f348b8ab99d6ed73
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-04-12 11:10:04 +0200

    Suggested change
    Co-authored-by: Robert Svensson <Kane610@users.noreply.github.com>

--- homeassistant/components/airly/diagnostics.py
+++ homeassistant/components/airly/diagnostics.py
@@ -19,7 +19,7 @@ async def async_get_config_entry_diagnostics(
     coordinator: AirlyDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     diagnostics_data = {
-        "config_entry_data": async_redact_data(dict(config_entry.data), TO_REDACT),
+        "config_entry_data": async_redact_data(dict(config_entry.as_dict(), TO_REDACT),
         "coordinator_data": coordinator.data,
     }
 
