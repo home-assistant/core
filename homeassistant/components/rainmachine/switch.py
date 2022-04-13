80019c033e431d49ae9461ded83ec584c6ba4be2
Aaron Bach <bachya1208@gmail.com>
2022-04-13 14:12:30 -0600

    Reduce precision of over-precise RainmMachine switch attributes

--- homeassistant/components/rainmachine/switch.py
+++ homeassistant/components/rainmachine/switch.py
@@ -5,7 +5,7 @@ import asyncio
 from collections.abc import Coroutine
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any
+from typing import Any, TypeVar, cast
 
 from regenmaschine.controller import Controller
 from regenmaschine.errors import RequestError
@@ -114,6 +114,17 @@ class RainMachineSwitchDescription(
     SwitchEntityDescription, RainMachineDescriptionMixinUid
 ):
     """Describe a RainMachine switch."""
+
+
+_T = TypeVar("_T")
+
+
+@callback
+def async_limit_precision(value: _T) -> _T:
+    """Reduce a value's decimal precision to 1 place (if appropriate)."""
+    if isinstance(value, float):
+        return cast(_T, round(value, 2))
+    return value
 
 
 async def async_setup_entry(
@@ -392,9 +403,11 @@ class RainMachineZone(RainMachineActivitySwitch):
 
         self._attr_extra_state_attributes.update(
             {
-                ATTR_AREA: data.get("waterSense").get("area"),
+                ATTR_AREA: async_limit_precision(data.get("waterSense").get("area")),
                 ATTR_CURRENT_CYCLE: data.get("cycle"),
-                ATTR_FIELD_CAPACITY: data.get("waterSense").get("fieldCapacity"),
+                ATTR_FIELD_CAPACITY: async_limit_precision(
+                    data.get("waterSense").get("fieldCapacity")
+                ),
                 ATTR_ID: data["uid"],
                 ATTR_NO_CYCLES: data.get("noOfCycles"),
                 ATTR_PRECIP_RATE: data.get("waterSense").get("precipitationRate"),
