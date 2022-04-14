a2abcfaf4a72d4a8a0b287a5163273c7205a0a69
Jason Lawrence <jjlawren@users.noreply.github.com>
2022-04-14 16:04:59 -0500

    Restore favorites to Sonos source_list

--- homeassistant/components/sonos/media_player.py
+++ homeassistant/components/sonos/media_player.py
@@ -463,14 +463,15 @@ class SonosMediaPlayerEntity(SonosEntity, MediaPlayerEntity):
     @property
     def source_list(self) -> list[str]:
         """List of available input sources."""
+        sources = [fav.title for fav in self.speaker.favorites]
         model = self.coordinator.model_name.split()[-1].upper()
         if model in MODELS_LINEIN_ONLY:
-            return [SOURCE_LINEIN]
+            sources += [SOURCE_LINEIN]
         if model in MODELS_TV_ONLY:
-            return [SOURCE_TV]
+            sources += [SOURCE_TV]
         if model in MODELS_LINEIN_AND_TV:
-            return [SOURCE_LINEIN, SOURCE_TV]
-        return []
+            sources += [SOURCE_LINEIN, SOURCE_TV]
+        return sources
 
     @soco_error(UPNP_ERRORS_TO_IGNORE)
     def media_play(self) -> None:
