eda27d252c359f52a7cab94f0ae89e23b667d86d
Milan Meulemans <milan.meulemans@live.be>
2022-04-14 10:33:34 +0000

    Apply suggestions from code review

--- homeassistant/components/senz/climate.py
+++ homeassistant/components/senz/climate.py
@@ -9,7 +9,7 @@ from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
     HVAC_MODE_AUTO,
     HVAC_MODE_HEAT,
-    SUPPORT_TARGET_TEMPERATURE,
+    ClimateEntityFeature,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_TEMPERATURE, PRECISION_TENTHS, TEMP_CELSIUS
@@ -40,7 +40,7 @@ class SENZClimate(CoordinatorEntity, ClimateEntity):
     _attr_temperature_unit = TEMP_CELSIUS
     _attr_precision = PRECISION_TENTHS
     _attr_hvac_modes = [HVAC_MODE_HEAT, HVAC_MODE_AUTO]
-    _attr_supported_features = SUPPORT_TARGET_TEMPERATURE
+    _attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
     _attr_max_temp = 35
     _attr_min_temp = 5
 
@@ -52,13 +52,13 @@ class SENZClimate(CoordinatorEntity, ClimateEntity):
         """Init SENZ climate."""
         super().__init__(coordinator)
         self._thermostat = thermostat
-        self._attr_name = self._thermostat.name
-        self._attr_unique_id = self._thermostat.serial_number
+        self._attr_name = thermostat.name
+        self._attr_unique_id = thermostat.serial_number
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, self._thermostat.serial_number)},
+            identifiers={(DOMAIN, thermostat.serial_number)},
             manufacturer="nVent Raychem",
             model="SENZ WIFI",
-            name=self._thermostat.name,
+            name=thermostat.name,
         )
 
     @callback
