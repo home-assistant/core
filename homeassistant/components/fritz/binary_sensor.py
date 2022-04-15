c63a47f0669c5a9648759c0617de51cd05ed29d7
Mark Kunze <mark.a.kunze@gmail.com>
2022-04-15 13:52:09 +0000

    Comment old binary sensor for updates to be deprecated

--- homeassistant/components/fritz/binary_sensor.py
+++ homeassistant/components/fritz/binary_sensor.py
@@ -42,6 +42,7 @@ SENSOR_TYPES: tuple[FritzBinarySensorEntityDescription, ...] = (
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     FritzBinarySensorEntityDescription(
+        # Deprecated, scheduled to be removed in 2022.7 (#70096)
         entity_registry_enabled_default=False,
         key="firmware_update",
         name="Firmware Update",
