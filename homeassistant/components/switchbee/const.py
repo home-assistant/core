6c3f222197129f37fe5e05f6cd0e7adf4f30b1b4
Jafar Atili <at.jafar@outlook.com>
2022-04-19 19:30:33 +0000

    CR fixes, added advanced setup form to let the user choose the following:     - scan interval in seconds: default 5     - whether to expose scenarios and group switches from the CU or not

--- homeassistant/components/switchbee/const.py
+++ homeassistant/components/switchbee/const.py
@@ -3,3 +3,6 @@
 
 DOMAIN = "switchbee"
 SCAN_INTERVAL_SEC = 3
+CONF_SCAN_INTERVAL = "scan_interval"
+CONF_EXPOSE_SCENARIOS = "expose_scenarios"
+CONF_EXPOSE_GROUP_SWITCHES = "expose_group_switches"
--- homeassistant/components/switchbee/switch.py
+++ homeassistant/components/switchbee/switch.py
@@ -25,7 +25,11 @@ async def async_setup_entry(
         Device(hass, coordinator.data[device], coordinator)
         for device in coordinator.data
         if coordinator.data[device]["type"]
-        in [switchbee.TYPE_SWITCH, switchbee.TYPE_OUTLET]
+        in [
+            switchbee.TYPE_SWITCH,
+            switchbee.TYPE_TIMED_POWER,
+            switchbee.TYPE_GROUP_SWITCH,
+        ]
     )
 
 
@@ -39,23 +43,26 @@ class Device(CoordinatorEntity, SwitchEntity):
         self._attr_name = device["name"]
         self._device_id = device[switchbee.ATTR_ID]
         self._attr_unique_id = device["uid"]
-        self._attr_device_info = DeviceInfo(
-            identifiers={
-                (DOMAIN, device["uid"]),
-            },
-            manufacturer="SwitchBee",
-            model=device["type"].title(),
-            name=self.name,
-        )
+        if device[switchbee.ATTR_HARDWARE] != switchbee.HW_VIRTUAL:
+            self._attr_device_info = DeviceInfo(
+                identifiers={
+                    (DOMAIN, device["uid"]),
+                },
+                manufacturer="SwitchBee",
+                model=(str(device["type"]).replace("_", " ")).title(),
+                suggested_area=device["area"],
+                name=self.name,
+            )
         self._attr_is_on = False
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-
+        # timed power switch state will represent a number of minutes until it goes off
+        # regulare switches state is ON/OFF
         self._attr_is_on = (
             self.coordinator.data[self._device_id][switchbee.ATTR_STATE]
-            == switchbee.STATE_ON
+            != switchbee.STATE_OFF
         )
 
         super()._handle_coordinator_update()
@@ -67,34 +74,18 @@ class Device(CoordinatorEntity, SwitchEntity):
 
     async def async_turn_on(self, **kwargs):
         """Async function to set on to switch."""
-        result = await self.coordinator.api.set_state(
-            self._device_id, switchbee.STATE_ON
-        )
-        if (
-            result[switchbee.ATTR_STATUS] != switchbee.STATUS_OK
-            or result[switchbee.ATTR_DATA] != switchbee.STATE_ON
-        ):
-            _LOGGER.error(
-                "Failed to set %s state ON, status=%s, state=%s",
-                self._attr_name,
-                result[switchbee.ATTR_STATUS],
-                result[switchbee.ATTR_DATA],
-            )
-        await self.coordinator.async_refresh()
+        return await self._async_set_state(switchbee.STATE_ON)
 
     async def async_turn_off(self, **kwargs):
         """Async function to set off to switch."""
-        result = await self.coordinator.api.set_state(
-            self._device_id, switchbee.STATE_OFF
-        )
-        if (
-            result[switchbee.ATTR_STATUS] != switchbee.STATUS_OK
-            or result[switchbee.ATTR_DATA] != switchbee.STATE_OFF
-        ):
+        return await self._async_set_state(switchbee.STATE_OFF)
+
+    async def _async_set_state(self, state):
+        try:
+            await self.coordinator.api.set_state(self._device_id, state)
+        except switchbee.SwitchBeeError as exp:
             _LOGGER.error(
-                "Failed to set %s state ON, status=%s, state=%s",
-                self._attr_name,
-                result[switchbee.ATTR_STATUS],
-                result[switchbee.ATTR_DATA],
+                "Failed to set %s state %s, error: %s", self._attr_name, state, exp
             )
-        await self.coordinator.async_refresh()
+        else:
+            await self.coordinator.async_refresh()
