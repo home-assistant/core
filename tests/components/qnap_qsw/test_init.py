3c5a60f74d9026f0fc0d7944ee3ae6a4478f680e
Álvaro Fernández Rojas <noltari@gmail.com>
2022-04-19 20:51:33 +0200

    tests: qnap_qsw: remove uneeded mocks from test_unload_entry
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- tests/components/qnap_qsw/test_init.py
+++ tests/components/qnap_qsw/test_init.py
@@ -6,15 +6,7 @@ from homeassistant.components.qnap_qsw.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
-from .util import (
-    CONFIG,
-    FIRMWARE_CONDITION_MOCK,
-    FIRMWARE_INFO_MOCK,
-    SYSTEM_BOARD_MOCK,
-    SYSTEM_SENSOR_MOCK,
-    SYSTEM_TIME_MOCK,
-    USERS_LOGIN_MOCK,
-)
+from .util import CONFIG
 
 from tests.common import MockConfigEntry
 
@@ -28,23 +20,11 @@ async def test_unload_entry(hass: HomeAssistant) -> None:
     config_entry.add_to_hass(hass)
 
     with patch(
-        "homeassistant.components.qnap_qsw.QnapQswApi.get_firmware_condition",
-        return_value=FIRMWARE_CONDITION_MOCK,
+        "homeassistant.components.qnap_qsw.QnapQswApi.validate",
+        return_value=None,
     ), patch(
-        "homeassistant.components.qnap_qsw.QnapQswApi.get_firmware_info",
-        return_value=FIRMWARE_INFO_MOCK,
-    ), patch(
-        "homeassistant.components.qnap_qsw.QnapQswApi.get_system_board",
-        return_value=SYSTEM_BOARD_MOCK,
-    ), patch(
-        "homeassistant.components.qnap_qsw.QnapQswApi.get_system_sensor",
-        return_value=SYSTEM_SENSOR_MOCK,
-    ), patch(
-        "homeassistant.components.qnap_qsw.QnapQswApi.get_system_time",
-        return_value=SYSTEM_TIME_MOCK,
-    ), patch(
-        "homeassistant.components.qnap_qsw.QnapQswApi.post_users_login",
-        return_value=USERS_LOGIN_MOCK,
+        "homeassistant.components.qnap_qsw.QnapQswApi.update",
+        return_value=None,
     ):
         assert await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
