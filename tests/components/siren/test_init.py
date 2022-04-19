a8abefc7ff76b246f04f0513b651b075ec9d7b2e
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-04-18 22:23:58 -0400

    Add test

--- tests/components/siren/test_init.py
+++ tests/components/siren/test_init.py
@@ -3,7 +3,11 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from homeassistant.components.siren import SirenEntity, process_turn_on_params
+from homeassistant.components.siren import (
+    SirenEntity,
+    SirenEntityDescription,
+    process_turn_on_params,
+)
 from homeassistant.components.siren.const import SirenEntityFeature
 
 
@@ -12,10 +16,20 @@ class MockSirenEntity(SirenEntity):
 
     _attr_is_on = True
 
-    def __init__(self, supported_features=0, available_tones=None):
+    def __init__(
+        self,
+        supported_features=0,
+        available_tones_as_attr=None,
+        available_tones_in_desc=None,
+    ):
         """Initialize mock siren entity."""
         self._attr_supported_features = supported_features
-        self._attr_available_tones = available_tones
+        if available_tones_as_attr:
+            self._attr_available_tones = available_tones_as_attr
+        else:
+            self.entity_description = SirenEntityDescription(
+                "mock", available_tones=available_tones_in_desc
+            )
 
 
 async def test_sync_turn_on(hass):
@@ -51,6 +65,15 @@ async def test_no_available_tones(hass):
 async def test_available_tones_list(hass):
     """Test that valid tones from tone list will get passed in."""
     siren = MockSirenEntity(SirenEntityFeature.TONES, ["a", "b"])
+    siren.hass = hass
+    assert process_turn_on_params(siren, {"tone": "a"}) == {"tone": "a"}
+
+
+async def test_available_tones_in_description(hass):
+    """Test that valid tones from tone list get passed in from entity description."""
+    siren = MockSirenEntity(
+        SirenEntityFeature.TONES, available_tones_in_desc=["a", "b"]
+    )
     siren.hass = hass
     assert process_turn_on_params(siren, {"tone": "a"}) == {"tone": "a"}
 
