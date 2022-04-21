70b86938fe3ad397f2ec1d8019bea87c1163d9c6
Malte Franken <exxamalte@users.noreply.github.com>
2022-04-22 00:40:08 +1000

    increase test coverage

--- tests/components/geo_json_events/test_geo_location.py
+++ tests/components/geo_json_events/test_geo_location.py
@@ -4,7 +4,7 @@ from unittest.mock import ANY, MagicMock, call, patch
 from aio_geojson_generic_client import GenericFeed
 from freezegun import freeze_time
 
-from homeassistant.components import geo_location
+from homeassistant.components import geo_json_events
 from homeassistant.components.geo_json_events.const import (
     ATTR_EXTERNAL_ID,
     DEFAULT_SCAN_INTERVAL,
@@ -12,6 +12,7 @@ from homeassistant.components.geo_json_events.const import (
 from homeassistant.components.geo_location import ATTR_SOURCE
 from homeassistant.const import (
     ATTR_FRIENDLY_NAME,
+    ATTR_ICON,
     ATTR_LATITUDE,
     ATTR_LONGITUDE,
     ATTR_UNIT_OF_MEASUREMENT,
@@ -22,29 +23,22 @@ from homeassistant.const import (
     EVENT_HOMEASSISTANT_START,
     LENGTH_KILOMETERS,
 )
-from homeassistant.helpers.dispatcher import DATA_DISPATCHER
+from homeassistant.helpers import entity_registry as er
 from homeassistant.setup import async_setup_component
 import homeassistant.util.dt as dt_util
 
-from tests.common import assert_setup_component, async_fire_time_changed
+from tests.common import async_fire_time_changed
 
 URL = "http://geo.json.local/geo_json_events.json"
-CONFIG = {
-    geo_location.DOMAIN: [
-        {"platform": "geo_json_events", CONF_URL: URL, CONF_RADIUS: 200}
-    ]
-}
+CONFIG = {geo_json_events.DOMAIN: {CONF_URL: URL, CONF_RADIUS: 200}}
 
 CONFIG_WITH_CUSTOM_LOCATION = {
-    geo_location.DOMAIN: [
-        {
-            "platform": "geo_json_events",
-            CONF_URL: URL,
-            CONF_RADIUS: 200,
-            CONF_LATITUDE: 15.1,
-            CONF_LONGITUDE: 25.2,
-        }
-    ]
+    geo_json_events.DOMAIN: {
+        CONF_URL: URL,
+        CONF_RADIUS: 200,
+        CONF_LATITUDE: 15.1,
+        CONF_LONGITUDE: 25.2,
+    }
 }
 
 
@@ -75,84 +69,107 @@ async def test_setup(hass):
             "OK",
             [mock_entry_1, mock_entry_2, mock_entry_3],
         )
-        with assert_setup_component(1, geo_location.DOMAIN):
-            assert await async_setup_component(hass, geo_location.DOMAIN, CONFIG)
-            await hass.async_block_till_done()
-            # Artificially trigger update.
-            hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
-            # Collect events.
-            await hass.async_block_till_done()
+        assert await async_setup_component(hass, geo_json_events.DOMAIN, CONFIG)
+        await hass.async_block_till_done()
+        # Artificially trigger update and collect events.
+        hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
+        await hass.async_block_till_done()
 
-            all_states = hass.states.async_all()
-            assert len(all_states) == 3
+        # 3 geolocation and 1 sensor entities
+        assert (
+            len(hass.states.async_entity_ids("geo_location"))
+            + len(hass.states.async_entity_ids("sensor"))
+            == 4
+        )
+        entity_registry = er.async_get(hass)
+        assert len(entity_registry.entities) == 4
 
-            state = hass.states.get("geo_location.title_1")
-            assert state is not None
-            assert state.name == "Title 1"
-            assert state.attributes == {
-                ATTR_EXTERNAL_ID: "1234",
-                ATTR_LATITUDE: -31.0,
-                ATTR_LONGITUDE: 150.0,
-                ATTR_FRIENDLY_NAME: "Title 1",
-                ATTR_UNIT_OF_MEASUREMENT: LENGTH_KILOMETERS,
-                ATTR_SOURCE: "NEW_geo_json_events",
-            }
-            assert round(abs(float(state.state) - 15.5), 7) == 0
+        state = hass.states.get(
+            "geo_location.geo_json_events_http_geo_json_local_geo_json_events_json_32_87336_117_22743_1234"
+        )
+        assert state is not None
+        assert state.name == "Title 1"
+        assert state.attributes == {
+            ATTR_EXTERNAL_ID: "1234",
+            ATTR_LATITUDE: -31.0,
+            ATTR_LONGITUDE: 150.0,
+            ATTR_FRIENDLY_NAME: "Title 1",
+            ATTR_UNIT_OF_MEASUREMENT: LENGTH_KILOMETERS,
+            ATTR_SOURCE: "geo_json_events",
+            ATTR_ICON: "mdi:pin",
+        }
+        assert round(abs(float(state.state) - 15.5), 7) == 0
 
-            state = hass.states.get("geo_location.title_2")
-            assert state is not None
-            assert state.name == "Title 2"
-            assert state.attributes == {
-                ATTR_EXTERNAL_ID: "2345",
-                ATTR_LATITUDE: -31.1,
-                ATTR_LONGITUDE: 150.1,
-                ATTR_FRIENDLY_NAME: "Title 2",
-                ATTR_UNIT_OF_MEASUREMENT: LENGTH_KILOMETERS,
-                ATTR_SOURCE: "NEW_geo_json_events",
-            }
-            assert round(abs(float(state.state) - 20.5), 7) == 0
+        state = hass.states.get(
+            "geo_location.geo_json_events_http_geo_json_local_geo_json_events_json_32_87336_117_22743_2345"
+        )
+        assert state is not None
+        assert state.name == "Title 2"
+        assert state.attributes == {
+            ATTR_EXTERNAL_ID: "2345",
+            ATTR_LATITUDE: -31.1,
+            ATTR_LONGITUDE: 150.1,
+            ATTR_FRIENDLY_NAME: "Title 2",
+            ATTR_UNIT_OF_MEASUREMENT: LENGTH_KILOMETERS,
+            ATTR_SOURCE: "geo_json_events",
+            ATTR_ICON: "mdi:pin",
+        }
+        assert round(abs(float(state.state) - 20.5), 7) == 0
 
-            state = hass.states.get("geo_location.title_3")
-            assert state is not None
-            assert state.name == "Title 3"
-            assert state.attributes == {
-                ATTR_EXTERNAL_ID: "3456",
-                ATTR_LATITUDE: -31.2,
-                ATTR_LONGITUDE: 150.2,
-                ATTR_FRIENDLY_NAME: "Title 3",
-                ATTR_UNIT_OF_MEASUREMENT: LENGTH_KILOMETERS,
-                ATTR_SOURCE: "NEW_geo_json_events",
-            }
-            assert round(abs(float(state.state) - 25.5), 7) == 0
+        state = hass.states.get(
+            "geo_location.geo_json_events_http_geo_json_local_geo_json_events_json_32_87336_117_22743_3456"
+        )
+        assert state is not None
+        assert state.name == "Title 3"
+        assert state.attributes == {
+            ATTR_EXTERNAL_ID: "3456",
+            ATTR_LATITUDE: -31.2,
+            ATTR_LONGITUDE: 150.2,
+            ATTR_FRIENDLY_NAME: "Title 3",
+            ATTR_UNIT_OF_MEASUREMENT: LENGTH_KILOMETERS,
+            ATTR_SOURCE: "geo_json_events",
+            ATTR_ICON: "mdi:pin",
+        }
+        assert round(abs(float(state.state) - 25.5), 7) == 0
 
-            # Simulate an update - one existing, one new entry,
-            # one outdated entry
-            mock_feed_update.return_value = (
-                "OK",
-                [mock_entry_1, mock_entry_4, mock_entry_3],
-            )
-            async_fire_time_changed(hass, utcnow + DEFAULT_SCAN_INTERVAL)
-            await hass.async_block_till_done()
+        # Simulate an update - two existing, one new entry,
+        # one outdated entry
+        mock_feed_update.return_value = (
+            "OK",
+            [mock_entry_1, mock_entry_4, mock_entry_3],
+        )
+        async_fire_time_changed(hass, utcnow + DEFAULT_SCAN_INTERVAL)
+        await hass.async_block_till_done()
 
-            all_states = hass.states.async_all()
-            assert len(all_states) == 3
+        assert (
+            len(hass.states.async_entity_ids("geo_location"))
+            + len(hass.states.async_entity_ids("sensor"))
+            == 4
+        )
 
-            # Simulate an update - empty data, but successful update,
-            # so no changes to entities.
-            mock_feed_update.return_value = "OK_NO_DATA", None
-            async_fire_time_changed(hass, utcnow + 2 * DEFAULT_SCAN_INTERVAL)
-            await hass.async_block_till_done()
+        # Simulate an update - empty data, but successful update,
+        # so no changes to entities.
+        mock_feed_update.return_value = "OK_NO_DATA", None
+        async_fire_time_changed(hass, utcnow + 2 * DEFAULT_SCAN_INTERVAL)
+        await hass.async_block_till_done()
 
-            all_states = hass.states.async_all()
-            assert len(all_states) == 3
+        assert (
+            len(hass.states.async_entity_ids("geo_location"))
+            + len(hass.states.async_entity_ids("sensor"))
+            == 4
+        )
 
-            # Simulate an update - empty data, removes all entities
-            mock_feed_update.return_value = "ERROR", None
-            async_fire_time_changed(hass, utcnow + 3 * DEFAULT_SCAN_INTERVAL)
-            await hass.async_block_till_done()
+        # Simulate an update - empty data, removes all entities
+        mock_feed_update.return_value = "ERROR", None
+        async_fire_time_changed(hass, utcnow + 3 * DEFAULT_SCAN_INTERVAL)
+        await hass.async_block_till_done()
 
-            all_states = hass.states.async_all()
-            assert len(all_states) == 0
+        assert (
+            len(hass.states.async_entity_ids("geo_location"))
+            + len(hass.states.async_entity_ids("sensor"))
+            == 1
+        )
+        assert len(entity_registry.entities) == 1
 
 
 async def test_setup_with_custom_location(hass):
@@ -168,99 +185,19 @@ async def test_setup_with_custom_location(hass):
     ) as mock_feed_update:
         mock_feed_update.return_value = "OK", [mock_entry_1]
 
-        with assert_setup_component(1, geo_location.DOMAIN):
-            assert await async_setup_component(
-                hass, geo_location.DOMAIN, CONFIG_WITH_CUSTOM_LOCATION
-            )
-            await hass.async_block_till_done()
-
-            # Artificially trigger update.
-            hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
-            # Collect events.
-            await hass.async_block_till_done()
-
-            all_states = hass.states.async_all()
-            assert len(all_states) == 1
-
-            assert mock_feed.call_args == call(
-                ANY, (15.1, 25.2), URL, filter_radius=200.0
-            )
-
-
-async def test_setup_race_condition(hass):
-    """Test a particular race condition experienced."""
-    # 1. Feed returns 1 entry -> Feed manager creates 1 entity.
-    # 2. Feed returns error -> Feed manager removes 1 entity.
-    #    However, this stayed on and kept listening for dispatcher signals.
-    # 3. Feed returns 1 entry -> Feed manager creates 1 entity.
-    # 4. Feed returns 1 entry -> Feed manager updates 1 entity.
-    #    Internally, the previous entity is updating itself, too.
-    # 5. Feed returns error -> Feed manager removes 1 entity.
-    #    There are now 2 entities trying to remove themselves from HA, but
-    #    the second attempt fails of course.
-
-    # Set up some mock feed entries for this test.
-    mock_entry_1 = _generate_mock_feed_entry("1234", "Title 1", 15.5, (-31.0, 150.0))
-    delete_signal = "geo_json_events_delete_1234"
-    update_signal = "geo_json_events_update_1234"
-
-    # Patching 'utcnow' to gain more control over the timed update.
-    utcnow = dt_util.utcnow()
-    with freeze_time(utcnow), patch(
-        "aio_geojson_client.feed.GeoJsonFeed.update"
-    ) as mock_feed_update, assert_setup_component(1, geo_location.DOMAIN):
-        assert await async_setup_component(hass, geo_location.DOMAIN, CONFIG)
+        assert await async_setup_component(
+            hass, geo_json_events.DOMAIN, CONFIG_WITH_CUSTOM_LOCATION
+        )
         await hass.async_block_till_done()
 
-        mock_feed_update.return_value = "OK", [mock_entry_1]
-
-        # Artificially trigger update.
+        # Artificially trigger update and collect events.
         hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
-        # Collect events.
         await hass.async_block_till_done()
 
-        all_states = hass.states.async_all()
-        assert len(all_states) == 1
-        assert len(hass.data[DATA_DISPATCHER][delete_signal]) == 1
-        assert len(hass.data[DATA_DISPATCHER][update_signal]) == 1
+        assert (
+            len(hass.states.async_entity_ids("geo_location"))
+            + len(hass.states.async_entity_ids("sensor"))
+            == 2
+        )
 
-        # Simulate an update - empty data, removes all entities
-        mock_feed_update.return_value = "ERROR", None
-        async_fire_time_changed(hass, utcnow + DEFAULT_SCAN_INTERVAL)
-        await hass.async_block_till_done()
-
-        all_states = hass.states.async_all()
-        assert len(all_states) == 0
-        assert len(hass.data[DATA_DISPATCHER][delete_signal]) == 0
-        assert len(hass.data[DATA_DISPATCHER][update_signal]) == 0
-
-        # Simulate an update - 1 entry
-        mock_feed_update.return_value = "OK", [mock_entry_1]
-        async_fire_time_changed(hass, utcnow + 2 * DEFAULT_SCAN_INTERVAL)
-        await hass.async_block_till_done()
-
-        all_states = hass.states.async_all()
-        assert len(all_states) == 1
-        assert len(hass.data[DATA_DISPATCHER][delete_signal]) == 1
-        assert len(hass.data[DATA_DISPATCHER][update_signal]) == 1
-
-        # Simulate an update - 1 entry
-        mock_feed_update.return_value = "OK", [mock_entry_1]
-        async_fire_time_changed(hass, utcnow + 3 * DEFAULT_SCAN_INTERVAL)
-        await hass.async_block_till_done()
-
-        all_states = hass.states.async_all()
-        assert len(all_states) == 1
-        assert len(hass.data[DATA_DISPATCHER][delete_signal]) == 1
-        assert len(hass.data[DATA_DISPATCHER][update_signal]) == 1
-
-        # Simulate an update - empty data, removes all entities
-        mock_feed_update.return_value = "ERROR", None
-        async_fire_time_changed(hass, utcnow + 4 * DEFAULT_SCAN_INTERVAL)
-        await hass.async_block_till_done()
-
-        all_states = hass.states.async_all()
-        assert len(all_states) == 0
-        # Ensure that delete and update signal targets are now empty.
-        assert len(hass.data[DATA_DISPATCHER][delete_signal]) == 0
-        assert len(hass.data[DATA_DISPATCHER][update_signal]) == 0
+        assert mock_feed.call_args == call(ANY, (15.1, 25.2), URL, filter_radius=200.0)
