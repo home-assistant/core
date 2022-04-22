b2abea94d7ce4429549fa82b2d17e41965945ae4
Maxim Oei <maximoei@gmail.com>
2022-04-22 22:07:11 +0200

    Update Telegram bot to support URLs in inlineKeyboard
    Update creation of InlineKeyboardButton to support url case, on top of callbacks

--- homeassistant/components/telegram_bot/__init__.py
+++ homeassistant/components/telegram_bot/__init__.py
@@ -499,17 +499,26 @@ class TelegramNotificationService:
                 (text_b2, data_callback_b2), ...]
               - a string like: `/cmd1, /cmd2, /cmd3`
               - or a string like: `text_b1:/cmd1, text_b2:/cmd2`
+              - also supports urls instead of callback commands
             """
             buttons = []
             if isinstance(row_keyboard, str):
                 for key in row_keyboard.split(","):
                     if ":/" in key:
-                        # commands like: 'Label:/cmd' become ('Label', '/cmd')
-                        label = key.split(":/")[0]
-                        command = key[len(label) + 1 :]
-                        buttons.append(
-                            InlineKeyboardButton(label, callback_data=command)
-                        )
+                        # check if command or URL
+                        if "https://" in key:
+                            label = key.split(",")[0]
+                            url = key[len(label) + 1 :]
+                            buttons.append(
+                                InlineKeyboardButton(label, url=url)
+                            )
+                        else:
+                            # commands like: 'Label:/cmd' become ('Label', '/cmd')
+                            label = key.split(":/")[0]
+                            command = key[len(label) + 1 :]
+                            buttons.append(
+                                InlineKeyboardButton(label, callback_data=command)
+                            )
                     else:
                         # commands like: '/cmd' become ('CMD', '/cmd')
                         label = key.strip()[1:].upper()
@@ -517,9 +526,14 @@ class TelegramNotificationService:
             elif isinstance(row_keyboard, list):
                 for entry in row_keyboard:
                     text_btn, data_btn = entry
-                    buttons.append(
-                        InlineKeyboardButton(text_btn, callback_data=data_btn)
-                    )
+                    if "https://" in data_btn:
+                         buttons.append(
+                            InlineKeyboardButton(text_btn, url=data_btn)
+                        )
+                    else: 
+                        buttons.append(
+                            InlineKeyboardButton(text_btn, callback_data=data_btn)
+                        )
             else:
                 raise ValueError(str(row_keyboard))
             return buttons
