7f0a906a7299ab22fa9978ab72842b141f56c6a4
Malte Franken <exxamalte@users.noreply.github.com>
2022-04-22 19:22:33 +1000

    improve test coverage

--- tests/components/geo_json_events/test_init.py
+++ tests/components/geo_json_events/test_init.py
@@ -2,6 +2,12 @@
 from unittest.mock import patch
 
 from homeassistant.components.geo_json_events.const import DOMAIN, FEED
+from homeassistant.helpers.entity_registry import (
+    async_entries_for_config_entry,
+    async_get_registry,
+)
+
+from tests.components.geo_json_events import _generate_mock_feed_entry
 
 
 async def test_component_unload_config_entry(hass, config_entry):
@@ -19,3 +25,47 @@ async def test_component_unload_config_entry(hass, config_entry):
         assert await hass.config_entries.async_unload(config_entry.entry_id)
         await hass.async_block_till_done()
         assert hass.data[DOMAIN][FEED].get(config_entry.entry_id) is None
+
+
+async def test_remove_orphaned_entities(hass, config_entry):
+    """Test removing orphaned geolocation entities."""
+    config_entry.add_to_hass(hass)
+
+    entity_registry = await async_get_registry(hass)
+    entity_registry.async_get_or_create(
+        "geo_location", "geo_json_events", "1", config_entry=config_entry
+    )
+    entity_registry.async_get_or_create(
+        "geo_location", "geo_json_events", "2", config_entry=config_entry
+    )
+    entity_registry.async_get_or_create(
+        "geo_location", "geo_json_events", "3", config_entry=config_entry
+    )
+
+    # There should now be 3 "orphaned" entries available which will be removed
+    # when the component is set up.
+    entries = async_entries_for_config_entry(entity_registry, config_entry.entry_id)
+    assert len(entries) == 3
+
+    # Set up a mock feed entry for this test.
+    mock_entry_1 = _generate_mock_feed_entry(
+        "1234",
+        "Title 1",
+        15.5,
+        (38.0, -3.0),
+    )
+
+    with patch("aio_geojson_client.feed.GeoJsonFeed.update") as mock_feed_update:
+        mock_feed_update.return_value = "OK", [mock_entry_1]
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+        # 1 geolocation and 1 sensor entity.
+        entries = async_entries_for_config_entry(entity_registry, config_entry.entry_id)
+        assert len(entries) == 2
+
+        assert (
+            len(hass.states.async_entity_ids("geo_location"))
+            + len(hass.states.async_entity_ids("sensor"))
+            == 2
+        )
