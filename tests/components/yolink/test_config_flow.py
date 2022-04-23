9a7ce9c228740815e08b2f3d9a58556e146639f3
justin <justin@yosmart.com>
2022-04-23 17:08:51 +0000

    resovle test flow

--- tests/components/yolink/test_config_flow.py
+++ tests/components/yolink/test_config_flow.py
@@ -11,13 +11,15 @@ from homeassistant.components.yolink import config_flow
 from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET
 from homeassistant.helpers import config_entry_oauth2_flow
 
+from tests.common import MockConfigEntry
+
 CLIENT_ID = "12345"
 CLIENT_SECRET = "6789"
-YOLINK_HOST = "192.168.30.104"
-YOLINK_HTTP_HOST = f"http://{YOLINK_HOST}:1080"
+YOLINK_HOST = "api.yosmart.com"
+YOLINK_HTTP_HOST = f"http://{YOLINK_HOST}"
 DOMAIN = "yolink"
 OAUTH2_AUTHORIZE = f"{YOLINK_HTTP_HOST}/oauth/v2/authorization.htm"
-OAUTH2_TOKEN = f"{YOLINK_HTTP_HOST}/opesn/yolink/token"
+OAUTH2_TOKEN = f"{YOLINK_HTTP_HOST}/open/yolink/token"
 
 
 @pytest.fixture()
@@ -136,3 +138,69 @@ async def test_abort_if_authorization_timeout(
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
     assert result["reason"] == "authorize_url_timeout"
+
+
+async def test_reauthentication_flow(
+    hass, hass_client_no_auth, aioclient_mock, current_request_with_host
+):
+    """Test reauthentication flow."""
+    await setup.async_setup_component(
+        hass,
+        DOMAIN,
+        {
+            DOMAIN: {
+                CONF_CLIENT_ID: CLIENT_ID,
+                CONF_CLIENT_SECRET: CLIENT_SECRET,
+            },
+            DOMAIN_HTTP: {CONF_BASE_URL: "https://example.com"},
+        },
+    )
+
+    old_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id=DOMAIN,
+        version=1,
+        data={"id": "timmo", "auth_implementation": DOMAIN},
+    )
+    old_entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_REAUTH}, data=old_entry.data
+    )
+
+    flows = hass.config_entries.flow.async_progress()
+    assert len(flows) == 1
+
+    result = await hass.config_entries.flow.async_configure(flows[0]["flow_id"], {})
+
+    # pylint: disable=protected-access
+    state = config_entry_oauth2_flow._encode_jwt(
+        hass,
+        {
+            "flow_id": result["flow_id"],
+            "redirect_uri": "https://example.com/auth/external/callback",
+        },
+    )
+    client = await hass_client_no_auth()
+    await client.get(f"/auth/external/callback?code=abcd&state={state}")
+
+    aioclient_mock.post(
+        OAUTH2_TOKEN,
+        json={
+            "refresh_token": "mock-refresh-token",
+            "access_token": "mock-access-token",
+            "type": "Bearer",
+            "expires_in": 60,
+        },
+    )
+
+    with patch("homeassistant.components.yolink.api.AuthenticationManager"):
+        with patch(
+            "homeassistant.components.yolink.async_setup_entry", return_value=True
+        ) as mock_setup:
+            result = await hass.config_entries.flow.async_configure(result["flow_id"])
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "reauth_successful"
+
+    assert len(mock_setup.mock_calls) == 1
