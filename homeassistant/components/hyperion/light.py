4d8907ca3874abbb843f4871ab827d1144ff5c23
epenet <6771947+epenet@users.noreply.github.com>
2022-04-23 18:54:36 +0000

    Use ColorMode enum in hyperion

--- homeassistant/components/hyperion/light.py
+++ homeassistant/components/hyperion/light.py
@@ -13,7 +13,7 @@ from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
     ATTR_EFFECT,
     ATTR_HS_COLOR,
-    COLOR_MODE_HS,
+    ColorMode,
     LightEntity,
     LightEntityFeature,
 )
@@ -124,8 +124,8 @@ async def async_setup_entry(
 class HyperionBaseLight(LightEntity):
     """A Hyperion light base class."""
 
-    _attr_color_mode = COLOR_MODE_HS
-    _attr_supported_color_modes = {COLOR_MODE_HS}
+    _attr_color_mode = ColorMode.HS
+    _attr_supported_color_modes = {ColorMode.HS}
     _attr_supported_features = LightEntityFeature.EFFECT
 
     def __init__(
