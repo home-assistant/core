5dbcd228e08b77b050391d5906139cfe33c21b35
Erik <erik@montnemery.com>
2022-04-26 15:49:18 +0200

    Correct filter_supported_color_modes + add test

--- homeassistant/components/light/__init__.py
+++ homeassistant/components/light/__init__.py
@@ -116,20 +116,20 @@ COLOR_MODES_COLOR = {
 }
 
 
-def filter_supported_color_modes(color_modes: Iterable[ColorMode | str]) -> set[ColorMode | str]:
+def filter_supported_color_modes(color_modes: Iterable[ColorMode]) -> set[ColorMode]:
     """Filter the given color modes."""
     color_modes = set(color_modes)
     if (
         not color_modes
-        or COLOR_MODE_UNKNOWN in color_modes
-        or (COLOR_MODE_WHITE in color_modes and not color_supported(color_modes))
+        or ColorMode.UNKNOWN in color_modes
+        or (ColorMode.WHITE in color_modes and not color_supported(color_modes))
     ):
         raise HomeAssistantError
 
-    if COLOR_MODE_BRIGHTNESS in color_modes and len(color_modes) > 1:
-        color_modes.remove(COLOR_MODE_BRIGHTNESS)
-    if COLOR_MODE_ONOFF in color_modes and len(color_modes) > 1:
-        color_modes.remove(COLOR_MODE_ONOFF)
+    if ColorMode.ONOFF in color_modes and len(color_modes) > 1:
+        color_modes.remove(ColorMode.ONOFF)
+    if ColorMode.BRIGHTNESS in color_modes and len(color_modes) > 1:
+        color_modes.remove(ColorMode.BRIGHTNESS)
     return color_modes
 
 
