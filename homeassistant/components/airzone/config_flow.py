8d71595f3104d05e5b671379dd11728bdf2d7466
Álvaro Fernández Rojas <noltari@gmail.com>
2022-04-26 06:25:11 +0200

    Improve and fix Airzone config flow (#70474)

--- homeassistant/components/airzone/config_flow.py
+++ homeassistant/components/airzone/config_flow.py
@@ -21,10 +21,8 @@ CONFIG_SCHEMA = vol.Schema(
         vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
     }
 )
-SYSTEM_ID_SCHEMA = vol.Schema(
+SYSTEM_ID_SCHEMA = CONFIG_SCHEMA.extend(
     {
-        vol.Required(CONF_HOST): str,
-        vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
         vol.Required(CONF_ID, default=1): int,
     }
 )
@@ -41,22 +39,14 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            system_id = user_input.get(CONF_ID, DEFAULT_SYSTEM_ID)
-
-            self._async_abort_entries_match(
-                {
-                    CONF_HOST: user_input[CONF_HOST],
-                    CONF_PORT: user_input[CONF_PORT],
-                    CONF_ID: system_id,
-                }
-            )
+            self._async_abort_entries_match(user_input)
 
             airzone = AirzoneLocalApi(
                 aiohttp_client.async_get_clientsession(self.hass),
                 ConnectionOptions(
                     user_input[CONF_HOST],
                     user_input[CONF_PORT],
-                    system_id,
+                    user_input.get(CONF_ID, DEFAULT_SYSTEM_ID),
                 ),
             )
 
@@ -64,7 +54,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 await airzone.validate()
             except InvalidSystem:
                 data_schema = SYSTEM_ID_SCHEMA
-                errors["base"] = "invalid_system_id"
+                errors[CONF_ID] = "invalid_system_id"
             except AirzoneError:
                 errors["base"] = "cannot_connect"
             else:
