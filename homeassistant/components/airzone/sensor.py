a7945f4e0c6288209d9834d1464cc60ac0cd172b
Álvaro Fernández Rojas <noltari@gmail.com>
2022-04-26 10:52:55 +0200

    Use Airzone WebServer MAC address as unique ID (#70287)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/airzone/sensor.py
+++ homeassistant/components/airzone/sensor.py
@@ -83,8 +83,11 @@ class AirzoneZoneSensor(AirzoneZoneEntity, AirzoneSensor):
     ) -> None:
         """Initialize."""
         super().__init__(coordinator, entry, system_zone_id, zone_data)
+
         self._attr_name = f"{zone_data[AZD_NAME]} {description.name}"
-        self._attr_unique_id = f"{entry.entry_id}_{system_zone_id}_{description.key}"
+        self._attr_unique_id = (
+            f"{self._attr_unique_id}_{system_zone_id}_{description.key}"
+        )
         self.entity_description = description
 
         if description.key == AZD_TEMP:
