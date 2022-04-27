7dc8f096e2dfd62366be483c91744007d868559b
Erik Montnemery <erik@montnemery.com>
2022-04-27 12:55:08 +0200

    Update sensor.py

--- homeassistant/components/tasmota/sensor.py
+++ homeassistant/components/tasmota/sensor.py
@@ -54,8 +54,14 @@ ICON = "icon"
 
 # A Tasmota sensor type may be mapped to either a device class or an icon, not both
 SENSOR_DEVICE_CLASS_ICON_MAP: dict[str, dict[str, Any]] = {
-    hc.SENSOR_ACTIVE_ENERGYEXPORT: {DEVICE_CLASS: SensorDeviceClass.ENERGY},
-    hc.SENSOR_ACTIVE_ENERGYIMPORT: {DEVICE_CLASS: SensorDeviceClass.ENERGY},
+    hc.SENSOR_ACTIVE_ENERGYEXPORT: {
+        DEVICE_CLASS: SensorDeviceClass.ENERGY,
+        STATE_CLASS: SensorStateClass.TOTAL,
+    },
+    hc.SENSOR_ACTIVE_ENERGYIMPORT: {
+        DEVICE_CLASS: SensorDeviceClass.ENERGY,
+        STATE_CLASS: SensorStateClass.TOTAL,
+    },
     hc.SENSOR_ACTIVE_POWERUSAGE: {
         DEVICE_CLASS: SensorDeviceClass.POWER,
         STATE_CLASS: SensorStateClass.MEASUREMENT,
@@ -89,6 +95,7 @@ SENSOR_DEVICE_CLASS_ICON_MAP: dict[str, dict[str, Any]] = {
     },
     hc.SENSOR_CURRENTNEUTRAL: {
         ICON: "mdi:alpha-a-circle-outline",
+        DEVICE_CLASS: SensorDeviceClass.CURRENT,
         STATE_CLASS: SensorStateClass.MEASUREMENT,
     },
     hc.SENSOR_DEWPOINT: {
@@ -152,8 +159,8 @@ SENSOR_DEVICE_CLASS_ICON_MAP: dict[str, dict[str, Any]] = {
         STATE_CLASS: SensorStateClass.MEASUREMENT,
     },
     hc.SENSOR_PROXIMITY: {ICON: "mdi:ruler"},
-    hc.SENSOR_REACTIVE_ENERGYEXPORT: {DEVICE_CLASS: SensorDeviceClass.ENERGY},
-    hc.SENSOR_REACTIVE_ENERGYIMPORT: {DEVICE_CLASS: SensorDeviceClass.ENERGY},
+    hc.SENSOR_REACTIVE_ENERGYEXPORT: {STATE_CLASS: SensorStateClass.TOTAL},
+    hc.SENSOR_REACTIVE_ENERGYIMPORT: {STATE_CLASS: SensorStateClass.TOTAL},
     hc.SENSOR_REACTIVE_POWERUSAGE: {
         ICON: "mdi:flash",
         STATE_CLASS: SensorStateClass.MEASUREMENT,
@@ -176,7 +183,7 @@ SENSOR_DEVICE_CLASS_ICON_MAP: dict[str, dict[str, Any]] = {
     hc.SENSOR_TODAY: {DEVICE_CLASS: SensorDeviceClass.ENERGY},
     hc.SENSOR_TOTAL: {
         DEVICE_CLASS: SensorDeviceClass.ENERGY,
-        #        STATE_CLASS: SensorStateClass.TOTAL_INCREASING,
+        STATE_CLASS: SensorStateClass.TOTAL,
     },
     hc.SENSOR_TOTAL_START_TIME: {ICON: "mdi:progress-clock"},
     hc.SENSOR_TVOC: {ICON: "mdi:air-filter"},
