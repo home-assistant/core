3241b36a75fbad3b9bda6338d7c48e5b8066bfa5
Aaron Bach <bachya1208@gmail.com>
2022-04-28 15:05:55 -0600

    Ensure that email-based 2FA in SimpliSafe shows the progress UI (#71021)

--- homeassistant/components/simplisafe/config_flow.py
+++ homeassistant/components/simplisafe/config_flow.py
@@ -49,13 +49,14 @@ class SimpliSafeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     def __init__(self) -> None:
         """Initialize the config flow."""
+        self._email_2fa_task: asyncio.Task | None = None
         self._password: str | None = None
         self._reauth: bool = False
         self._simplisafe: API | None = None
         self._username: str | None = None
 
     async def _async_authenticate(
-        self, error_step_id: str, error_schema: vol.Schema
+        self, originating_step_id: str, originating_step_schema: vol.Schema
     ) -> FlowResult:
         """Attempt to authenticate to the SimpliSafe API."""
         assert self._password
@@ -76,8 +77,8 @@ class SimpliSafeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         if errors:
             return self.async_show_form(
-                step_id=error_step_id,
-                data_schema=error_schema,
+                step_id=originating_step_id,
+                data_schema=originating_step_schema,
                 errors=errors,
                 description_placeholders={CONF_USERNAME: self._username},
             )
@@ -86,6 +87,31 @@ class SimpliSafeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         if self._simplisafe.auth_state == AuthStates.PENDING_2FA_SMS:
             return await self.async_step_sms_2fa()
+        return await self.async_step_email_2fa()
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> SimpliSafeOptionsFlowHandler:
+        """Define the config flow to handle options."""
+        return SimpliSafeOptionsFlowHandler(config_entry)
+
+    async def async_step_reauth(self, config: dict[str, Any]) -> FlowResult:
+        """Handle configuration by re-auth."""
+        self._reauth = True
+
+        if CONF_USERNAME not in config:
+            # Old versions of the config flow may not have the username by this point;
+            # in that case, we reauth them by making them go through the user flow:
+            return await self.async_step_user()
+
+        self._username = config[CONF_USERNAME]
+        return await self.async_step_reauth_confirm()
+
+    async def _async_get_email_2fa(self) -> None:
+        """Define a task to wait for email-based 2FA."""
+        assert self._simplisafe
 
         try:
             async with async_timeout.timeout(DEFAULT_EMAIL_2FA_TIMEOUT):
@@ -97,17 +123,39 @@ class SimpliSafeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                         await asyncio.sleep(DEFAULT_EMAIL_2FA_SLEEP)
                     else:
                         break
-        except asyncio.TimeoutError:
-            return self.async_show_form(
-                step_id="user",
-                data_schema=STEP_USER_SCHEMA,
-                errors={"base": "2fa_timed_out"},
+        finally:
+            self.hass.async_create_task(
+                self.hass.config_entries.flow.async_configure(flow_id=self.flow_id)
             )
 
-        return await self._async_finish_setup()
+    async def async_step_email_2fa(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle email-based two-factor authentication."""
+        if not self._email_2fa_task:
+            self._email_2fa_task = self.hass.async_create_task(
+                self._async_get_email_2fa()
+            )
+            return self.async_show_progress(
+                step_id="email_2fa", progress_action="email_2fa"
+            )
 
-    async def _async_finish_setup(self) -> FlowResult:
-        """Complete setup with an authenticated API object."""
+        try:
+            await self._email_2fa_task
+        except asyncio.TimeoutError:
+            return self.async_show_progress_done(next_step_id="email_2fa_error")
+        return self.async_show_progress_done(next_step_id="finish")
+
+    async def async_step_email_2fa_error(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle an error during email-based two-factor authentication."""
+        return self.async_abort(reason="email_2fa_timed_out")
+
+    async def async_step_finish(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the final step."""
         assert self._simplisafe
         assert self._username
 
@@ -141,26 +189,6 @@ class SimpliSafeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         await self.async_set_unique_id(user_id)
         self._abort_if_unique_id_configured()
         return self.async_create_entry(title=self._username, data=data)
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(
-        config_entry: ConfigEntry,
-    ) -> SimpliSafeOptionsFlowHandler:
-        """Define the config flow to handle options."""
-        return SimpliSafeOptionsFlowHandler(config_entry)
-
-    async def async_step_reauth(self, config: dict[str, Any]) -> FlowResult:
-        """Handle configuration by re-auth."""
-        self._reauth = True
-
-        if CONF_USERNAME not in config:
-            # Old versions of the config flow may not have the username by this point;
-            # in that case, we reauth them by making them go through the user flow:
-            return await self.async_step_user()
-
-        self._username = config[CONF_USERNAME]
-        return await self.async_step_reauth_confirm()
 
     async def async_step_reauth_confirm(
         self, user_input: dict[str, Any] | None = None
@@ -197,7 +225,7 @@ class SimpliSafeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 errors={CONF_CODE: "invalid_auth"},
             )
 
-        return await self._async_finish_setup()
+        return await self.async_step_finish()
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
