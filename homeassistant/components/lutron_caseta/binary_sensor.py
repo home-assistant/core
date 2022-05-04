69d19dc0088bd1b3483cfc481ed2f72e49599cf8
J. Nick Koston <nick@koston.org>
2022-05-04 16:07:17 -0400

    merge

--- homeassistant/components/lutron_caseta/binary_sensor.py
+++ homeassistant/components/lutron_caseta/binary_sensor.py
@@ -6,14 +6,12 @@ from homeassistant.components.binary_sensor import (
     BinarySensorEntity,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_SUGGESTED_AREA
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceEntryType
-from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from . import DOMAIN as CASETA_DOMAIN, LutronCasetaDevice, _area_and_name_from_name
-from .const import BRIDGE_DEVICE, BRIDGE_LEAP, CONFIG_URL, MANUFACTURER, UNASSIGNED_AREA
+from . import DOMAIN as CASETA_DOMAIN, LutronCasetaDevice
+from .const import BRIDGE_DEVICE, BRIDGE_LEAP
 
 
 async def async_setup_entry(
@@ -45,19 +43,7 @@ class LutronOccupancySensor(LutronCasetaDevice, BinarySensorEntity):
     def __init__(self, device, bridge, bridge_device):
         """Init an occupancy sensor."""
         super().__init__(device, bridge, bridge_device)
-        info = DeviceInfo(
-            identifiers={(CASETA_DOMAIN, self.unique_id)},
-            manufacturer=MANUFACTURER,
-            model=f"{device['model']} ({device['type']})",
-            name=self.name,
-            via_device=(CASETA_DOMAIN, self._bridge_device["serial"]),
-            configuration_url=CONFIG_URL,
-            entry_type=DeviceEntryType.SERVICE,
-        )
-        area, _ = _area_and_name_from_name(device["name"])
-        if area != UNASSIGNED_AREA:
-            info[ATTR_SUGGESTED_AREA] = area
-        self._attr_device_info = info
+        self._attr_device_info["entry_type"] = DeviceEntryType.SERVICE
 
     @property
     def device_class(self):
