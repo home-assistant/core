9621645fb223d2b93bd6e35d620af721e4f86ad4
Erik Montnemery <erik@montnemery.com>
2022-05-05 20:04:00 +0200

    Only lookup unknown Google Cast models once (#71348)
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- homeassistant/components/cast/helpers.py
+++ homeassistant/components/cast/helpers.py
@@ -15,7 +15,10 @@ from pychromecast import dial
 from pychromecast.const import CAST_TYPE_GROUP
 from pychromecast.models import CastInfo
 
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import aiohttp_client
+
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -47,18 +50,50 @@ class ChromecastInfo:
         """Return the UUID."""
         return self.cast_info.uuid
 
-    def fill_out_missing_chromecast_info(self) -> ChromecastInfo:
+    def fill_out_missing_chromecast_info(self, hass: HomeAssistant) -> ChromecastInfo:
         """Return a new ChromecastInfo object with missing attributes filled in.
 
         Uses blocking HTTP / HTTPS.
         """
         cast_info = self.cast_info
         if self.cast_info.cast_type is None or self.cast_info.manufacturer is None:
-            # Manufacturer and cast type is not available in mDNS data, get it over http
-            cast_info = dial.get_cast_type(
-                cast_info,
-                zconf=ChromeCastZeroconf.get_zeroconf(),
-            )
+            unknown_models = hass.data[DOMAIN]["unknown_models"]
+            if self.cast_info.model_name not in unknown_models:
+                # Manufacturer and cast type is not available in mDNS data, get it over http
+                cast_info = dial.get_cast_type(
+                    cast_info,
+                    zconf=ChromeCastZeroconf.get_zeroconf(),
+                )
+                unknown_models[self.cast_info.model_name] = (
+                    cast_info.cast_type,
+                    cast_info.manufacturer,
+                )
+
+                report_issue = (
+                    "create a bug report at "
+                    "https://github.com/home-assistant/core/issues?q=is%3Aopen+is%3Aissue"
+                    "+label%3A%22integration%3A+cast%22"
+                )
+
+                _LOGGER.info(
+                    "Fetched cast details for unknown model '%s' manufacturer: '%s', type: '%s'. Please %s",
+                    cast_info.model_name,
+                    cast_info.manufacturer,
+                    cast_info.cast_type,
+                    report_issue,
+                )
+            else:
+                cast_type, manufacturer = unknown_models[self.cast_info.model_name]
+                cast_info = CastInfo(
+                    cast_info.services,
+                    cast_info.uuid,
+                    cast_info.model_name,
+                    cast_info.friendly_name,
+                    cast_info.host,
+                    cast_info.port,
+                    cast_type,
+                    manufacturer,
+                )
 
         if not self.is_audio_group or self.is_dynamic_group is not None:
             # We have all information, no need to check HTTP API.
