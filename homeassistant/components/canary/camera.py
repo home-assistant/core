2369ebfa929a3f3e14bb09d74fa898f343731076
Brian May <brian@bmay.org>
2022-05-05 16:16:19 -0700

    refactor: black formatting changes
    tsia

--- homeassistant/components/canary/camera.py
+++ homeassistant/components/canary/camera.py
@@ -148,9 +148,7 @@ class CanaryCamera(CoordinatorEntity[CanaryDataUpdateCoordinator], Camera):
             getattr, self._live_stream_session, "live_stream_url"
         )
         stream = CameraMjpeg(self._ffmpeg.binary)
-        await stream.open_camera(
-            live_stream_url, extra_cmd=self._ffmpeg_arguments
-        )
+        await stream.open_camera(live_stream_url, extra_cmd=self._ffmpeg_arguments)
 
         try:
             stream_reader = await stream.get_reader()
