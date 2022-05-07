86a526128e86f6e93c01e86917853921aa35275f
Dave T <17680170+davet2001@users.noreply.github.com>
2022-05-07 17:16:54 +0000

    Add diagnostics to generic camera

---
+++ homeassistant/components/generic/diagnostics.py
@@ -0,0 +1,33 @@
+"""Diagnostics support for generic (IP camera)."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.components.sql.sensor import redact_credentials
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
+
+from .const import CONF_STILL_IMAGE_URL, CONF_STREAM_SOURCE
+
+TO_REDACT = {
+    CONF_PASSWORD,
+    CONF_USERNAME,
+}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    options = async_redact_data(entry.options, TO_REDACT)
+    for key in (CONF_STREAM_SOURCE, CONF_STILL_IMAGE_URL):
+        if (value := options.get(key)) is not None:
+            options[key] = redact_credentials(value)
+
+    return {
+        "title": entry.title,
+        "data": async_redact_data(entry.data, TO_REDACT),
+        "options": options,
+    }
