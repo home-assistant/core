28f86446a7737b130d351e79b5c836ec035f9afc
J. Nick Koston <nick@koston.org>
2022-05-07 10:14:21 -0500

    add manual setup

--- homeassistant/components/baf/config_flow.py
+++ homeassistant/components/baf/config_flow.py
@@ -1,17 +1,35 @@
 """Config flow for baf."""
 from __future__ import annotations
 
+import asyncio
 from typing import Any
+
+from aiobafi6 import Device, Service
+from aiobafi6.discovery import PORT
+import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.components import zeroconf
 from homeassistant.const import CONF_IP_ADDRESS
 from homeassistant.data_entry_flow import FlowResult
 
-from .const import DOMAIN
+from .const import DOMAIN, RUN_TIMEOUT
 from .models import BAFDiscovery
 
 API_SUFFIX = "._api._tcp.local."
+
+
+async def async_try_connect(ip_address: str) -> Device:
+    """Validate we can connect to a device."""
+    device = Device(Service(ip_addresses=[ip_address], port=PORT))
+    run_task = device.run()
+    try:
+        await asyncio.wait_for(device.async_wait_available(), timeout=RUN_TIMEOUT)
+    except asyncio.TimeoutError as ex:
+        raise CannotConnect from ex
+    finally:
+        run_task.cancel()
+    return device
 
 
 class BAFFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
@@ -27,13 +45,11 @@ class BAFFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         self, discovery_info: zeroconf.ZeroconfServiceInfo
     ) -> FlowResult:
         """Handle zeroconf discovery."""
-        name = discovery_info.name
-        if name.endswith(API_SUFFIX):
-            name = name[: -len(API_SUFFIX)]
         properties = discovery_info.properties
         ip_address = discovery_info.host
         uuid = properties["uuid"]
         model = properties["model"]
+        name = properties["name"]
         await self.async_set_unique_id(uuid, raise_on_progress=False)
         self._abort_if_unique_id_configured(updates={CONF_IP_ADDRESS: ip_address})
         self.discovery = BAFDiscovery(name, ip_address, uuid, model)
@@ -46,28 +62,50 @@ class BAFFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         assert self.discovery is not None
         discovery = self.discovery
         if user_input is not None:
-            return await self._async_entry_for_discovered_device(discovery)
+            return self.async_create_entry(
+                title=discovery.name,
+                data={CONF_IP_ADDRESS: discovery.ip_address},
+            )
         placeholders = {
             "name": discovery.name,
             "model": discovery.model,
             "ip_address": discovery.ip_address,
         }
         self.context["title_placeholders"] = placeholders
+        self._set_confirm_only()
         return self.async_show_form(
             step_id="discovery_confirm", description_placeholders=placeholders
-        )
-
-    async def _async_entry_for_discovered_device(
-        self, discovery: BAFDiscovery
-    ) -> FlowResult:
-        """Create a config entry for a device."""
-        return self.async_create_entry(
-            title=discovery.name,
-            data={CONF_IP_ADDRESS: discovery.ip_address},
         )
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
-        """Handle a flow initialized by the user."""
-        raise NotImplementedError
+        """Handle the initial step."""
+        errors = {}
+        ip_address = (user_input or {}).get(CONF_IP_ADDRESS, "")
+        if user_input is not None:
+            try:
+                device = await async_try_connect(ip_address)
+            except CannotConnect:
+                errors[CONF_IP_ADDRESS] = "cannot_connect"
+            else:
+                await self.async_set_unique_id(device.dns_sd_uuid)
+                self._abort_if_unique_id_configured(
+                    updates={CONF_IP_ADDRESS: ip_address}
+                )
+                return self.async_create_entry(
+                    title=device.name,
+                    data={CONF_IP_ADDRESS: ip_address},
+                )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {vol.Required(CONF_IP_ADDRESS, default=ip_address): str}
+            ),
+            errors=errors,
+        )
+
+
+class CannotConnect(Exception):
+    """Exception to raise when we cannot connect."""
