0ac5ef333aedcdf2053a9a428373b38d7d3bd0da
J. Nick Koston <nick@koston.org>
2022-05-07 16:18:40 -0500

    Ensure sql sensors keep working after using the options flow (#71453)
    * Ensure sql sensors keep working after using the options flow
    
    Fixes
    ```
    2022-05-06 16:17:57 ERROR (MainThread) [homeassistant.components.sensor] Error while setting up sql platform for sensor
    Traceback (most recent call last):
      File "/Users/bdraco/home-assistant/homeassistant/helpers/entity_platform.py", line 249, in _async_setup_platform
        await asyncio.shield(task)
      File "/Users/bdraco/home-assistant/homeassistant/components/sql/sensor.py", line 97, in async_setup_entry
        name: str = entry.options[CONF_NAME]
    KeyError: name
    ```
    
    * ensure saving the options flow fixes the broken config entry
    
    * ensure options changes take effect right away
    
    * Add cover to validate the reload

--- homeassistant/components/sql/config_flow.py
+++ homeassistant/components/sql/config_flow.py
@@ -165,7 +165,14 @@ class SQLOptionsFlowHandler(config_entries.OptionsFlow):
             except ValueError:
                 errors["query"] = "query_invalid"
             else:
-                return self.async_create_entry(title="", data=user_input)
+                return self.async_create_entry(
+                    title="",
+                    data={
+                        CONF_NAME: self.entry.title,
+                        **self.entry.options,
+                        **user_input,
+                    },
+                )
 
         return self.async_show_form(
             step_id="init",
