713a582d855cdaa2c09a962b288bc6be3d5fd8e0
Tom Puttemans <github@tomputtemans.com>
2022-05-09 23:59:04 +0200

    Add config flow and mqtt discovery to dsmr_reader

--- homeassistant/components/dsmr_reader/__init__.py
+++ homeassistant/components/dsmr_reader/__init__.py
@@ -1,1 +1,18 @@
 """The DSMR Reader component."""
+
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+
+PLATFORMS = [Platform.SENSOR]
+
+LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Setup the DSMR Reader integration"""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
