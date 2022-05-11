7ba045efd2ba016d22be4bdf19e6dd0b851efc92
IceBotYT <34712694+IceBotYT@users.noreply.github.com>
2022-05-11 22:12:09 +0000

    Add retry logic

--- homeassistant/components/lacrosse_view/__init__.py
+++ homeassistant/components/lacrosse_view/__init__.py
@@ -8,7 +8,7 @@ from lacrosse_view import HTTPError, LaCrosse, Location, LoginError, Sensor
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.exceptions import ConfigEntryNotReady, HomeAssistantError
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
@@ -19,6 +19,16 @@ PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up LaCrosse View from a config entry."""
+
+    async def get_data() -> list[Sensor]:
+        """Get the data from the LaCrosse View."""
+        return await api.get_sensors(
+            location=Location(id=entry.data["id"], name=entry.data["name"]),
+            tz=hass.config.time_zone,
+            start=int(datetime.timestamp(datetime.utcnow() - timedelta(minutes=30))),
+            end=int(datetime.timestamp(datetime.utcnow())),
+        )
+
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
         "api": LaCrosse(async_get_clientsession(hass))
     }
@@ -31,18 +41,34 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     async def async_update_data() -> list[Sensor]:
         """Fetch data from API."""
-        try:
-            data: list[Sensor] = await api.get_sensors(
-                location=Location(id=entry.data["id"], name=entry.data["name"]),
-                tz=hass.config.time_zone,
-                start=int(
-                    datetime.timestamp(datetime.utcnow() - timedelta(minutes=30))
-                ),
-                end=int(datetime.timestamp(datetime.utcnow())),
-            )
-            return data
-        except HTTPError as error:
-            raise ConfigEntryNotReady from error
+        start = 15
+        retry_count = 0
+        while True:
+            try:
+                data: list[Sensor] = await get_data()
+                # Test if the data is valid
+                for sensor in data:
+                    if not sensor.data:
+                        raise NoData(
+                            "No data for sensor {}. Can you make sure your display can connect to the app server?".format(
+                                sensor.name
+                            )
+                        )
+                break
+            except HTTPError as error:
+                raise error
+            except NoData as error:
+                retry_count += 1
+                if retry_count > 10:
+                    raise error
+                start += 15
+                LOGGER.warning(
+                    "No data found, getting data from %s minutes ago (retry attempt: %s)",
+                    start,
+                    retry_count,
+                )
+
+        return data
 
     coordinator = DataUpdateCoordinator(
         hass,
@@ -67,3 +93,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+class NoData(HomeAssistantError):
+    """Raised when the data is invalid."""
