bc6b4eb500973ef7827b7247c71ea446e19f1eba
Tom Puttemans <github@tomputtemans.com>
2022-05-14 01:50:47 +0200

    Cover missing config flow test coverage section

--- tests/components/dsmr_reader/test_config_flow.py
+++ tests/components/dsmr_reader/test_config_flow.py
@@ -14,7 +14,7 @@ async def test_initial_user_step(hass: HomeAssistant):
     )
 
     assert init_result["type"] == RESULT_TYPE_FORM
-    assert init_result["step_id"] == SOURCE_USER
+    assert init_result["step_id"] == "user"
     assert {"base": "mqtt_missing"} == init_result["errors"]
 
 
@@ -26,20 +26,20 @@ async def test_duplicate_user_step(hass: HomeAssistant):
     )
 
     assert init_result["type"] == RESULT_TYPE_FORM
-    assert init_result["step_id"] == SOURCE_USER
+    assert init_result["step_id"] == "user"
     assert {"base": "single_instance_allowed"} == init_result["errors"]
 
 
 async def test_user_step_with_mqtt(hass: HomeAssistant):
     """Test the user step call with mqtt available."""
     # configure bogus mqtt service to pass validation
-    hass.services.async_register("mqtt", "publish", lambda: {})
+    hass.services.async_register("mqtt", "publish", None)
     init_result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": SOURCE_USER}
     )
 
     assert init_result["type"] == RESULT_TYPE_FORM
-    assert init_result["step_id"] == SOURCE_USER
+    assert init_result["step_id"] == "user"
     assert init_result["errors"] is None
 
     config_result = await hass.config_entries.flow.async_configure(
@@ -51,6 +51,19 @@ async def test_user_step_with_mqtt(hass: HomeAssistant):
 
 
 async def test_mqtt_step(hass: HomeAssistant):
+    """Test the MQTT discovery step."""
+    # configure bogus mqtt service to pass validation
+    hass.services.async_register("mqtt", "publish", lambda: {})
+    result = await hass.config_entries.flow.async_init(
+        "dsmr_reader", context={"source": SOURCE_MQTT}
+    )
+
+    assert result["type"] == RESULT_TYPE_FORM
+    assert result["step_id"] == "user"
+    assert result["errors"] is None
+
+
+async def test_mqtt_duplicate_step(hass: HomeAssistant):
     """Test the MQTT discovery step preventing duplicate entries."""
     MockConfigEntry(domain="dsmr_reader").add_to_hass(hass)
 
