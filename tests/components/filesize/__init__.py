c0ae31d86c841107930cf471fd60d65b5c163f16
G Johansson <goran.johansson@shiftit.se>
2022-05-14 13:16:22 +0200

    Code quality Filesize (#71768)

--- tests/components/filesize/__init__.py
+++ tests/components/filesize/__init__.py
@@ -1,6 +1,8 @@
 """Tests for the filesize component."""
 import os
 
+from homeassistant.core import HomeAssistant
+
 TEST_DIR = os.path.join(os.path.dirname(__file__))
 TEST_FILE_NAME = "mock_file_test_filesize.txt"
 TEST_FILE_NAME2 = "mock_file_test_filesize2.txt"
@@ -8,7 +10,12 @@ TEST_FILE = os.path.join(TEST_DIR, TEST_FILE_NAME)
 TEST_FILE2 = os.path.join(TEST_DIR, TEST_FILE_NAME2)
 
 
-def create_file(path) -> None:
+async def async_create_file(hass: HomeAssistant, path: str) -> None:
     """Create a test file."""
+    await hass.async_add_executor_job(create_file, path)
+
+
+def create_file(path: str) -> None:
+    """Create the test file."""
     with open(path, "w", encoding="utf-8") as test_file:
         test_file.write("test")
