53997c517d65403c38cc923d36aec145a40487e6
Kevin Cathcart <kevincathcart@gmail.com>
2022-05-14 00:29:10 +0000

    Support notification_id in notify.persistant_notification

--- tests/components/notify/test_persistent_notification.py
+++ tests/components/notify/test_persistent_notification.py
@@ -23,3 +23,42 @@ async def test_async_send_message(hass: HomeAssistant):
     state = hass.states.get(entity_ids[0])
     assert state.attributes.get("message") == "Hello"
     assert state.attributes.get("title") == "Test notification"
+
+
+async def test_async_supports_notification_id(hass: HomeAssistant):
+    """Test that notify.persistent_notification supports notification_id."""
+    await async_setup_component(hass, pn.DOMAIN, {"core": {}})
+    await async_setup_component(hass, notify.DOMAIN, {})
+    await hass.async_block_till_done()
+
+    message = {
+        "message": "Hello",
+        "title": "Test notification",
+        "data": {"notification_id": "my_id"},
+    }
+    await hass.services.async_call(
+        notify.DOMAIN, notify.SERVICE_PERSISTENT_NOTIFICATION, message
+    )
+    await hass.async_block_till_done()
+
+    entity_ids = hass.states.async_entity_ids(pn.DOMAIN)
+    assert len(entity_ids) == 1
+
+    # Send second message with same ID
+
+    message = {
+        "message": "Goodbye",
+        "title": "Notification was updated",
+        "data": {"notification_id": "my_id"},
+    }
+    await hass.services.async_call(
+        notify.DOMAIN, notify.SERVICE_PERSISTENT_NOTIFICATION, message
+    )
+    await hass.async_block_till_done()
+
+    entity_ids = hass.states.async_entity_ids(pn.DOMAIN)
+    assert len(entity_ids) == 1
+
+    state = hass.states.get(entity_ids[0])
+    assert state.attributes.get("message") == "Goodbye"
+    assert state.attributes.get("title") == "Notification was updated"
