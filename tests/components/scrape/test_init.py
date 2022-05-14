911784ae1f41fe48aa9d763f83f69aa87c1d4834
G Johansson <goran.johansson@shiftit.se>
2022-05-14 13:06:53 +0000

    Move rest data and fix tests

--- tests/components/scrape/test_init.py
+++ tests/components/scrape/test_init.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from unittest.mock import patch
 
 from homeassistant.components.scrape.const import DOMAIN
+from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
 from . import MockRestData
@@ -31,7 +32,7 @@ async def test_setup_entry(hass: HomeAssistant) -> None:
     entry.add_to_hass(hass)
 
     with patch(
-        "homeassistant.components.scrape.sensor.RestData",
+        "homeassistant.components.scrape.RestData",
         return_value=MockRestData("test_scrape_sensor"),
     ):
         await hass.config_entries.async_setup(entry.entry_id)
@@ -39,6 +40,26 @@ async def test_setup_entry(hass: HomeAssistant) -> None:
 
     state = hass.states.get("sensor.release")
     assert state
+
+
+async def test_setup_entry_no_data_fails(hass: HomeAssistant) -> None:
+    """Test setup entry no data fails."""
+    entry = MockConfigEntry(
+        domain=DOMAIN, data={}, options=TEST_CONFIG, title="Release", entry_id="1"
+    )
+    entry.add_to_hass(hass)
+
+    with patch(
+        "homeassistant.components.scrape.RestData",
+        return_value=MockRestData("test_scrape_sensor_no_data"),
+    ):
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.ha_version")
+    assert state is None
+    entry = hass.config_entries.async_get_entry("1")
+    assert entry.state == ConfigEntryState.SETUP_RETRY
 
 
 async def test_remove_entry(hass: HomeAssistant) -> None:
@@ -52,7 +73,7 @@ async def test_remove_entry(hass: HomeAssistant) -> None:
     entry.add_to_hass(hass)
 
     with patch(
-        "homeassistant.components.scrape.sensor.RestData",
+        "homeassistant.components.scrape.RestData",
         return_value=MockRestData("test_scrape_sensor"),
     ):
         await hass.config_entries.async_setup(entry.entry_id)
