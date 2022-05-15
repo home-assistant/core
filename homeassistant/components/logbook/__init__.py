cfd35a4d6403cdb68f9e63f52855f4351bdf81f8
J. Nick Koston <nick@koston.org>
2022-05-15 07:51:06 -0500

    tweak

--- homeassistant/components/logbook/__init__.py
+++ homeassistant/components/logbook/__init__.py
@@ -742,8 +742,6 @@ class EntityNameCache:
             friendly_name := current_state.attributes.get(ATTR_FRIENDLY_NAME)
         ):
             self._names[entity_id] = friendly_name
-        elif extracted_name := row.friendly_name:
-            self._names[entity_id] = extracted_name
         else:
             return split_entity_id(entity_id)[1].replace("_", " ")
 
