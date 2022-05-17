4a48925de3078f446b43f98c28376ef1fc6b5790
Franck Nijhof <git@frenck.dev>
2022-05-17 16:24:10 +0200

    Clean up accessing service helpers via hass

--- tests/helpers/test_service.py
+++ tests/helpers/test_service.py
@@ -819,8 +819,9 @@ async def test_register_admin_service(hass, hass_read_only_user, hass_admin_user
     async def mock_service(call):
         calls.append(call)
 
-    hass.helpers.service.async_register_admin_service("test", "test", mock_service)
-    hass.helpers.service.async_register_admin_service(
+    service.async_register_admin_service(hass, "test", "test", mock_service)
+    service.async_register_admin_service(
+        hass,
         "test",
         "test2",
         mock_service,
@@ -887,7 +888,7 @@ async def test_domain_control_not_async(hass, mock_entities):
         calls.append(call)
 
     with pytest.raises(exceptions.HomeAssistantError):
-        hass.helpers.service.verify_domain_control("test_domain")(mock_service_log)
+        service.verify_domain_control(hass, "test_domain")(mock_service_log)
 
 
 async def test_domain_control_unknown(hass, mock_entities):
@@ -902,9 +903,9 @@ async def test_domain_control_unknown(hass, mock_entities):
         "homeassistant.helpers.entity_registry.async_get_registry",
         return_value=Mock(entities=mock_entities),
     ):
-        protected_mock_service = hass.helpers.service.verify_domain_control(
-            "test_domain"
-        )(mock_service_log)
+        protected_mock_service = service.verify_domain_control(hass, "test_domain")(
+            mock_service_log
+        )
 
         hass.services.async_register(
             "test_domain", "test_service", protected_mock_service, schema=None
@@ -940,7 +941,7 @@ async def test_domain_control_unauthorized(hass, hass_read_only_user):
         """Define a protected service."""
         calls.append(call)
 
-    protected_mock_service = hass.helpers.service.verify_domain_control("test_domain")(
+    protected_mock_service = service.verify_domain_control(hass, "test_domain")(
         mock_service_log
     )
 
@@ -979,7 +980,7 @@ async def test_domain_control_admin(hass, hass_admin_user):
         """Define a protected service."""
         calls.append(call)
 
-    protected_mock_service = hass.helpers.service.verify_domain_control("test_domain")(
+    protected_mock_service = service.verify_domain_control(hass, "test_domain")(
         mock_service_log
     )
 
@@ -1017,7 +1018,7 @@ async def test_domain_control_no_user(hass):
         """Define a protected service."""
         calls.append(call)
 
-    protected_mock_service = hass.helpers.service.verify_domain_control("test_domain")(
+    protected_mock_service = service.verify_domain_control(hass, "test_domain")(
         mock_service_log
     )
 
