d530ff89230ecdbaa5ea79803835ded8839fe8ba
J. Nick Koston <nick@koston.org>
2022-05-17 17:10:17 -0500

    add coverage to make sure we did not break the old api

--- tests/components/logbook/test_init.py
+++ tests/components/logbook/test_init.py
@@ -2573,3 +2573,136 @@ async def test_get_events_with_device_ids(hass, hass_ws_client, recorder_mock):
     assert results[3]["entity_id"] == "light.kitchen"
     assert results[3]["state"] == "off"
     assert isinstance(results[3]["when"], float)
+
+
+async def test_logbook_select_entities_context_id(hass, recorder_mock, hass_client):
+    """Test the logbook view with end_time and entity with automations and scripts."""
+    await asyncio.gather(
+        *[
+            async_setup_component(hass, comp, {})
+            for comp in ("homeassistant", "logbook", "automation", "script")
+        ]
+    )
+
+    await async_recorder_block_till_done(hass)
+
+    context = ha.Context(
+        id="ac5bd62de45711eaaeb351041eec8dd9",
+        user_id="b400facee45711eaa9308bfd3d19e474",
+    )
+
+    # An Automation
+    automation_entity_id_test = "automation.alarm"
+    hass.bus.async_fire(
+        EVENT_AUTOMATION_TRIGGERED,
+        {ATTR_NAME: "Mock automation", ATTR_ENTITY_ID: automation_entity_id_test},
+        context=context,
+    )
+    hass.bus.async_fire(
+        EVENT_SCRIPT_STARTED,
+        {ATTR_NAME: "Mock script", ATTR_ENTITY_ID: "script.mock_script"},
+        context=context,
+    )
+    hass.states.async_set(
+        automation_entity_id_test,
+        STATE_ON,
+        {ATTR_FRIENDLY_NAME: "Alarm Automation"},
+        context=context,
+    )
+
+    entity_id_test = "alarm_control_panel.area_001"
+    hass.states.async_set(entity_id_test, STATE_OFF, context=context)
+    await hass.async_block_till_done()
+    hass.states.async_set(entity_id_test, STATE_ON, context=context)
+    await hass.async_block_till_done()
+    entity_id_second = "alarm_control_panel.area_002"
+    hass.states.async_set(entity_id_second, STATE_OFF, context=context)
+    await hass.async_block_till_done()
+    hass.states.async_set(entity_id_second, STATE_ON, context=context)
+    await hass.async_block_till_done()
+    hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
+    await hass.async_block_till_done()
+    entity_id_third = "alarm_control_panel.area_003"
+
+    logbook.async_log_entry(
+        hass,
+        "mock_name",
+        "mock_message",
+        "alarm_control_panel",
+        entity_id_third,
+        context,
+    )
+    await hass.async_block_till_done()
+
+    logbook.async_log_entry(
+        hass,
+        "mock_name",
+        "mock_message",
+        "homeassistant",
+        None,
+        context,
+    )
+    await hass.async_block_till_done()
+
+    # A service call
+    light_turn_off_service_context = ha.Context(
+        id="9c5bd62de45711eaaeb351041eec8dd9",
+        user_id="9400facee45711eaa9308bfd3d19e474",
+    )
+    hass.states.async_set("light.switch", STATE_ON)
+    await hass.async_block_till_done()
+
+    hass.bus.async_fire(
+        EVENT_CALL_SERVICE,
+        {
+            ATTR_DOMAIN: "light",
+            ATTR_SERVICE: "turn_off",
+            ATTR_ENTITY_ID: "light.switch",
+        },
+        context=light_turn_off_service_context,
+    )
+    await hass.async_block_till_done()
+
+    hass.states.async_set(
+        "light.switch", STATE_OFF, context=light_turn_off_service_context
+    )
+    await async_wait_recording_done(hass)
+
+    client = await hass_client()
+
+    # Today time 00:00:00
+    start = dt_util.utcnow().date()
+    start_date = datetime(start.year, start.month, start.day)
+
+    # Test today entries with filter by end_time
+    end_time = start + timedelta(hours=24)
+    response = await client.get(
+        f"/api/logbook/{start_date.isoformat()}?end_time={end_time}&entity={entity_id_test},{entity_id_second},{entity_id_third},light.switch"
+    )
+    assert response.status == HTTPStatus.OK
+    json_dict = await response.json()
+
+    assert json_dict[0]["entity_id"] == entity_id_test
+    assert json_dict[0]["context_event_type"] == "automation_triggered"
+    assert json_dict[0]["context_entity_id"] == "automation.alarm"
+    assert json_dict[0]["context_entity_id_name"] == "Alarm Automation"
+    assert json_dict[0]["context_user_id"] == "b400facee45711eaa9308bfd3d19e474"
+
+    assert json_dict[1]["entity_id"] == entity_id_second
+    assert json_dict[1]["context_event_type"] == "automation_triggered"
+    assert json_dict[1]["context_entity_id"] == "automation.alarm"
+    assert json_dict[1]["context_entity_id_name"] == "Alarm Automation"
+    assert json_dict[1]["context_user_id"] == "b400facee45711eaa9308bfd3d19e474"
+
+    assert json_dict[2]["entity_id"] == "alarm_control_panel.area_003"
+    assert json_dict[2]["context_event_type"] == "automation_triggered"
+    assert json_dict[2]["context_entity_id"] == "automation.alarm"
+    assert json_dict[2]["domain"] == "alarm_control_panel"
+    assert json_dict[2]["context_entity_id_name"] == "Alarm Automation"
+    assert json_dict[2]["context_user_id"] == "b400facee45711eaa9308bfd3d19e474"
+
+    assert json_dict[3]["entity_id"] == "light.switch"
+    assert json_dict[3]["context_event_type"] == "call_service"
+    assert json_dict[3]["context_domain"] == "light"
+    assert json_dict[3]["context_service"] == "turn_off"
+    assert json_dict[3]["context_user_id"] == "9400facee45711eaa9308bfd3d19e474"
