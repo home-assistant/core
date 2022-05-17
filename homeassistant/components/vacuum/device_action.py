7d2deae5921989fc4a9add9aef910160ab0ac358
Franck Nijhof <git@frenck.dev>
2022-05-17 13:40:19 +0200

    Clean up use of deprecated async_get_registry methods (#72001)

--- homeassistant/components/vacuum/device_action.py
+++ homeassistant/components/vacuum/device_action.py
@@ -30,7 +30,7 @@ async def async_get_actions(
     hass: HomeAssistant, device_id: str
 ) -> list[dict[str, str]]:
     """List device actions for Vacuum devices."""
-    registry = await entity_registry.async_get_registry(hass)
+    registry = entity_registry.async_get(hass)
     actions = []
 
     # Get all the integrations entities for this device
