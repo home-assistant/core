e1f92ac93028ad078a2f44d0e6ee67ce2a033908
epenet <6771947+epenet@users.noreply.github.com>
2022-05-18 05:26:31 +0000

    keep prefix

--- homeassistant/components/samsungtv/config_flow.py
+++ homeassistant/components/samsungtv/config_flow.py
@@ -408,7 +408,8 @@ class SamsungTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             )
         return entry
 
-    def _start_discovery_with_mac_address(self) -> None:
+    @callback
+    def _async_start_discovery_with_mac_address(self) -> None:
         """Start discovery."""
         assert self._host is not None
         if (entry := self._async_update_existing_matching_entry()) and entry.unique_id:
@@ -485,7 +486,7 @@ class SamsungTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         LOGGER.debug("Samsung device found via DHCP: %s", discovery_info)
         self._mac = discovery_info.macaddress
         self._host = discovery_info.ip
-        self._start_discovery_with_mac_address()
+        self._async_start_discovery_with_mac_address()
         await self._async_set_device_unique_id()
         self.context["title_placeholders"] = {"device": self._title}
         return await self.async_step_confirm()
@@ -497,7 +498,7 @@ class SamsungTVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         LOGGER.debug("Samsung device found via ZEROCONF: %s", discovery_info)
         self._mac = format_mac(discovery_info.properties["deviceid"])
         self._host = discovery_info.host
-        self._start_discovery_with_mac_address()
+        self._async_start_discovery_with_mac_address()
         await self._async_set_device_unique_id()
         self.context["title_placeholders"] = {"device": self._title}
         return await self.async_step_confirm()
