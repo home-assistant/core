453c6af5956ee5c6e296cc69bc17613ee5f78804
epenet <6771947+epenet@users.noreply.github.com>
2022-05-19 09:16:46 +0200

    Adjust device_automation type hints in arcam_fmj (#72128)

--- homeassistant/components/arcam_fmj/device_trigger.py
+++ homeassistant/components/arcam_fmj/device_trigger.py
@@ -1,8 +1,6 @@
 """Provides device automations for Arcam FMJ Receiver control."""
 from __future__ import annotations
 
-from typing import Any
-
 import voluptuous as vol
 
 from homeassistant.components.automation import (
@@ -35,7 +33,7 @@ TRIGGER_SCHEMA = DEVICE_TRIGGER_BASE_SCHEMA.extend(
 
 async def async_get_triggers(
     hass: HomeAssistant, device_id: str
-) -> list[dict[str, Any]]:
+) -> list[dict[str, str]]:
     """List device triggers for Arcam FMJ Receiver control devices."""
     registry = entity_registry.async_get(hass)
     triggers = []
