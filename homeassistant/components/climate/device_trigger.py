9b0f198394de075ee653f23e056e1a44338c7e0a
epenet <6771947+epenet@users.noreply.github.com>
2022-05-19 06:27:10 +0000

    Adjust device_automation type hints in core

--- homeassistant/components/climate/device_trigger.py
+++ homeassistant/components/climate/device_trigger.py
@@ -1,8 +1,6 @@
 """Provides device automations for Climate."""
 from __future__ import annotations
 
-from typing import Any
-
 import voluptuous as vol
 
 from homeassistant.components.automation import (
@@ -65,7 +63,7 @@ TRIGGER_SCHEMA = vol.Any(HVAC_MODE_TRIGGER_SCHEMA, CURRENT_TRIGGER_SCHEMA)
 
 async def async_get_triggers(
     hass: HomeAssistant, device_id: str
-) -> list[dict[str, Any]]:
+) -> list[dict[str, str]]:
     """List device triggers for Climate devices."""
     registry = entity_registry.async_get(hass)
     triggers = []
