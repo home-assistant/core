6f5bab91c3b847c2b65fe6ec90fc9c6197ef65fe
epenet <6771947+epenet@users.noreply.github.com>
2022-05-19 20:12:48 +0000

    Adjust number

--- homeassistant/components/number/device_action.py
+++ homeassistant/components/number/device_action.py
@@ -3,6 +3,10 @@ from __future__ import annotations
 
 import voluptuous as vol
 
+from homeassistant.components.device_automation import (
+    GetAutomationCapabilitiesResult,
+    GetAutomationsResult,
+)
 from homeassistant.const import (
     ATTR_ENTITY_ID,
     CONF_DEVICE_ID,
@@ -13,7 +17,7 @@ from homeassistant.const import (
 from homeassistant.core import Context, HomeAssistant
 from homeassistant.helpers import entity_registry
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.typing import ConfigType, TemplateVarsType
 
 from .const import ATTR_VALUE, DOMAIN, SERVICE_SET_VALUE
 
@@ -30,10 +34,10 @@ ACTION_SCHEMA = cv.DEVICE_ACTION_BASE_SCHEMA.extend(
 
 async def async_get_actions(
     hass: HomeAssistant, device_id: str
-) -> list[dict[str, str]]:
+) -> GetAutomationsResult:
     """List device actions for Number."""
     registry = entity_registry.async_get(hass)
-    actions: list[dict[str, str]] = []
+    actions: GetAutomationsResult = []
 
     # Get all the integrations entities for this device
     for entry in entity_registry.async_entries_for_device(registry, device_id):
@@ -53,7 +57,10 @@ async def async_get_actions(
 
 
 async def async_call_action_from_config(
-    hass: HomeAssistant, config: dict, variables: dict, context: Context | None
+    hass: HomeAssistant,
+    config: ConfigType,
+    variables: TemplateVarsType,
+    context: Context | None,
 ) -> None:
     """Execute a device action."""
     await hass.services.async_call(
@@ -70,7 +77,7 @@ async def async_call_action_from_config(
 
 async def async_get_action_capabilities(
     hass: HomeAssistant, config: ConfigType
-) -> dict[str, vol.Schema]:
+) -> GetAutomationCapabilitiesResult:
     """List action capabilities."""
     fields = {vol.Required(ATTR_VALUE): vol.Coerce(float)}
 
