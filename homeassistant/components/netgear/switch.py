9d97727e6211ab5bcb7f619c33e2a07cd01816ac
starkillerOG <starkiller.og@gmail.com>
2022-05-19 15:48:15 +0200

    prevent extra logbook entries

--- homeassistant/components/netgear/switch.py
+++ homeassistant/components/netgear/switch.py
@@ -229,8 +229,8 @@ class NetgearRouterSwitchEntity(NetgearRouterEntity, SwitchEntity):
 
     async def async_added_to_hass(self):
         """Fetch state when entity is added."""
+        await async_update()
         await super().async_added_to_hass()
-        self.async_schedule_update_ha_state(force_refresh=True)
 
     async def async_update(self):
         """Poll the state of the switch."""
