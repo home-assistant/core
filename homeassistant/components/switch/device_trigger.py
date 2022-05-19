e1b6ef2db17077de5f1b2f35813afc6b4c0efd6f
epenet <6771947+epenet@users.noreply.github.com>
2022-05-19 20:14:12 +0000

    Adjust switch

--- homeassistant/components/switch/device_trigger.py
+++ homeassistant/components/switch/device_trigger.py
@@ -1,15 +1,17 @@
 """Provides device triggers for switches."""
 from __future__ import annotations
 
-from typing import Any
-
 import voluptuous as vol
 
 from homeassistant.components.automation import (
     AutomationActionType,
     AutomationTriggerInfo,
 )
-from homeassistant.components.device_automation import toggle_entity
+from homeassistant.components.device_automation import (
+    GetAutomationCapabilitiesResult,
+    GetAutomationsResult,
+    toggle_entity,
+)
 from homeassistant.const import CONF_DOMAIN
 from homeassistant.core import CALLBACK_TYPE, HomeAssistant
 from homeassistant.helpers.typing import ConfigType
@@ -36,13 +38,13 @@ async def async_attach_trigger(
 
 async def async_get_triggers(
     hass: HomeAssistant, device_id: str
-) -> list[dict[str, Any]]:
+) -> GetAutomationsResult:
     """List device triggers."""
     return await toggle_entity.async_get_triggers(hass, device_id, DOMAIN)
 
 
 async def async_get_trigger_capabilities(
     hass: HomeAssistant, config: ConfigType
-) -> dict[str, vol.Schema]:
+) -> GetAutomationCapabilitiesResult:
     """List trigger capabilities."""
     return await toggle_entity.async_get_trigger_capabilities(hass, config)
