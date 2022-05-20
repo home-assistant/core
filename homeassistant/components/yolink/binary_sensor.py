d07fd765d8d56c32731f4ea71c96a116d255a290
Matrix <justin@yosmart.com>
2022-05-21 01:06:47 +0800

    Add yolink sensors (#72186)
    * Add more sensor support
    
    * change codeowner to active account
    
    * fix suggest

--- homeassistant/components/yolink/binary_sensor.py
+++ homeassistant/components/yolink/binary_sensor.py
@@ -15,7 +15,13 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import ATTR_COORDINATOR, ATTR_DEVICE_DOOR_SENSOR, DOMAIN
+from .const import (
+    ATTR_COORDINATOR,
+    ATTR_DEVICE_DOOR_SENSOR,
+    ATTR_DEVICE_LEAK_SENSOR,
+    ATTR_DEVICE_MOTION_SENSOR,
+    DOMAIN,
+)
 from .coordinator import YoLinkCoordinator
 from .entity import YoLinkEntity
 
@@ -25,19 +31,36 @@ class YoLinkBinarySensorEntityDescription(BinarySensorEntityDescription):
     """YoLink BinarySensorEntityDescription."""
 
     exists_fn: Callable[[YoLinkDevice], bool] = lambda _: True
+    key: str = "state"
     value: Callable[[str], bool | None] = lambda _: None
 
 
-SENSOR_DEVICE_TYPE = [ATTR_DEVICE_DOOR_SENSOR]
+SENSOR_DEVICE_TYPE = [
+    ATTR_DEVICE_DOOR_SENSOR,
+    ATTR_DEVICE_MOTION_SENSOR,
+    ATTR_DEVICE_LEAK_SENSOR,
+]
 
 SENSOR_TYPES: tuple[YoLinkBinarySensorEntityDescription, ...] = (
     YoLinkBinarySensorEntityDescription(
-        key="state",
         icon="mdi:door",
         device_class=BinarySensorDeviceClass.DOOR,
-        name="state",
+        name="State",
         value=lambda value: value == "open",
         exists_fn=lambda device: device.device_type in [ATTR_DEVICE_DOOR_SENSOR],
+    ),
+    YoLinkBinarySensorEntityDescription(
+        device_class=BinarySensorDeviceClass.MOTION,
+        name="Motion",
+        value=lambda value: value == "alert",
+        exists_fn=lambda device: device.device_type in [ATTR_DEVICE_MOTION_SENSOR],
+    ),
+    YoLinkBinarySensorEntityDescription(
+        name="Leak",
+        icon="mdi:water",
+        device_class=BinarySensorDeviceClass.MOISTURE,
+        value=lambda value: value == "alert",
+        exists_fn=lambda device: device.device_type in [ATTR_DEVICE_LEAK_SENSOR],
     ),
 )
 
