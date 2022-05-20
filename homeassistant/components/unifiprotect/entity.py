96eded8accc965560f32286f72a6f0d466e86894
J. Nick Koston <nick@koston.org>
2022-05-19 21:04:51 -0500

    fix conflict resolution

--- homeassistant/components/unifiprotect/entity.py
+++ homeassistant/components/unifiprotect/entity.py
@@ -179,12 +179,11 @@ class ProtectDeviceEntity(Entity):
     async def async_added_to_hass(self) -> None:
         """When entity is added to hass."""
         await super().async_added_to_hass()
-        if self.unique_id is not None:
-            self.async_on_remove(
-                self.data.async_subscribe_device_id(
-                    self.device.id, self._async_updated_event
-                )
+        self.async_on_remove(
+            self.data.async_subscribe_device_id(
+                self.device.id, self._async_updated_event
             )
+        )
 
 
 class ProtectNVREntity(ProtectDeviceEntity):
