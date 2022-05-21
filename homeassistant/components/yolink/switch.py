c6c2eb2aaf49131ffcb1da7d73444779ed20801a
justin <justin@yosmart.com>
2022-05-21 22:08:26 +0000

    suggest fix

--- homeassistant/components/yolink/switch.py
+++ homeassistant/components/yolink/switch.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from typing import Any
 
 from yolink.device import YoLinkDevice
-from yolink.exception import YoLinkAuthFailError
+from yolink.exception import YoLinkAuthFailError, YoLinkClientError
 
 from homeassistant.components.switch import (
     SwitchDeviceClass,
@@ -15,7 +15,7 @@ from homeassistant.components.switch import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.exceptions import ConfigEntryAuthFailed
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import ATTR_COORDINATOR, ATTR_DEVICE_OUTLET, DOMAIN
@@ -58,7 +58,9 @@ async def async_setup_entry(
     for device in devices:
         for description in DEVICE_TYPES:
             if description.exists_fn(device):
-                entities.append(YoLinkSwitchEntity(coordinator, description, device))
+                entities.append(
+                    YoLinkSwitchEntity(config_entry, coordinator, description, device)
+                )
     async_add_entities(entities)
 
 
@@ -69,12 +71,14 @@ class YoLinkSwitchEntity(YoLinkEntity, SwitchEntity):
 
     def __init__(
         self,
+        config_entry: ConfigEntry,
         coordinator: YoLinkCoordinator,
         description: YoLinkSwitchEntityDescription,
         device: YoLinkDevice,
     ) -> None:
         """Init YoLink Outlet."""
         super().__init__(coordinator, device)
+        self.config_entry = config_entry
         self.entity_description = description
         self._attr_unique_id = f"{device.device_id} {self.entity_description.key}"
         self._attr_name = f"{device.device_name} ({self.entity_description.name})"
@@ -87,18 +91,24 @@ class YoLinkSwitchEntity(YoLinkEntity, SwitchEntity):
         )
         self.async_write_ha_state()
 
-    async def async_turn_on(self, **kwargs: Any) -> None:
-        """Turn the entity on."""
+    async def call_state_change(self, state: str) -> None:
+        """Call setState api to change outlet state."""
         try:
-            await self.device.call_device_http_api("setState", {"state": "open"})
+            # call_device_http_api will check result, fail by raise YoLinkClientError
+            await self.device.call_device_http_api("setState", {"state": state})
+            self._attr_is_on = self.entity_description.value(state)
             self.async_write_ha_state()
         except YoLinkAuthFailError as yl_auth_err:
-            raise ConfigEntryAuthFailed from yl_auth_err
+            self.config_entry.async_start_reauth(self.hass)
+            raise HomeAssistantError(yl_auth_err) from yl_auth_err
+        except YoLinkClientError as yl_client_err:
+            self.coordinator.last_update_success = False
+            raise HomeAssistantError(yl_client_err) from yl_client_err
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the entity on."""
+        await self.call_state_change("open")
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the entity off."""
-        try:
-            await self.device.call_device_http_api("setState", {"state": "close"})
-            self.async_write_ha_state()
-        except YoLinkAuthFailError as yl_auth_err:
-            raise ConfigEntryAuthFailed from yl_auth_err
+        await self.call_state_change("close")
