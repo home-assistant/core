a860e62cd51598f39124f8f632e0e6f9609fa5a2
Mick Vleeshouwer <mick@imick.nl>
2022-05-23 20:53:46 +0000

    Modernize integration

--- homeassistant/components/overkiz/climate_entities/atlantic_electrical_heater_with_adjustable_temperature_setpoint.py
+++ homeassistant/components/overkiz/climate_entities/atlantic_electrical_heater_with_adjustable_temperature_setpoint.py
@@ -1,23 +1,17 @@
 """Support for Atlantic Electrical Heater With Adjustable Temperature Setpoint."""
 from __future__ import annotations
 
-from typing import cast
+from typing import Any, cast
 
 from pyoverkiz.enums import OverkizCommand, OverkizCommandParam, OverkizState
 
-from homeassistant.components.climate import (
-    SUPPORT_PRESET_MODE,
-    SUPPORT_TARGET_TEMPERATURE,
-    ClimateEntity,
-)
+from homeassistant.components.climate import ClimateEntity, ClimateEntityFeature
 from homeassistant.components.climate.const import (
-    HVAC_MODE_AUTO,
-    HVAC_MODE_HEAT,
-    HVAC_MODE_OFF,
     PRESET_BOOST,
     PRESET_COMFORT,
     PRESET_ECO,
     PRESET_NONE,
+    HVACMode,
 )
 from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
 
@@ -29,7 +23,7 @@ PRESET_FROST_PROTECTION = "frost_protection"
 PRESET_PROG = "prog"
 
 # Map OVERKIZ presets to Home Assistant presets
-OVERKIZ_TO_PRESET_MODE = {
+OVERKIZ_TO_PRESET_MODE: dict[str, str] = {
     OverkizCommandParam.OFF: PRESET_NONE,
     OverkizCommandParam.FROSTPROTECTION: PRESET_FROST_PROTECTION,
     OverkizCommandParam.ECO: PRESET_ECO,
@@ -42,13 +36,13 @@ OVERKIZ_TO_PRESET_MODE = {
 PRESET_MODE_TO_OVERKIZ = {v: k for k, v in OVERKIZ_TO_PRESET_MODE.items()}
 
 # Map OVERKIZ HVAC modes to Home Assistant HVAC modes
-OVERKIZ_TO_HVAC_MODE = {
-    OverkizCommandParam.ON: HVAC_MODE_HEAT,
-    OverkizCommandParam.OFF: HVAC_MODE_OFF,
-    OverkizCommandParam.AUTO: HVAC_MODE_AUTO,
-    "basic": HVAC_MODE_HEAT,
-    OverkizCommandParam.STANDBY: HVAC_MODE_OFF,
-    OverkizCommandParam.INTERNAL: HVAC_MODE_AUTO,
+OVERKIZ_TO_HVAC_MODE: dict[str, str] = {
+    OverkizCommandParam.ON: HVACMode.HEAT,
+    OverkizCommandParam.OFF: HVACMode.OFF,
+    OverkizCommandParam.AUTO: HVACMode.AUTO,
+    OverkizCommandParam.BASIC: HVACMode.HEAT,
+    OverkizCommandParam.STANDBY: HVACMode.OFF,
+    OverkizCommandParam.INTERNAL: HVACMode.AUTO,
 }
 
 HVAC_MODE_TO_OVERKIZ = {v: k for k, v in OVERKIZ_TO_HVAC_MODE.items()}
@@ -62,7 +56,9 @@ class AtlanticElectricalHeaterWithAdjustableTemperatureSetpoint(
     _attr_hvac_modes = [*HVAC_MODE_TO_OVERKIZ]
     _attr_preset_modes = [*PRESET_MODE_TO_OVERKIZ]
     _attr_temperature_unit = TEMP_CELSIUS
-    _attr_supported_features = SUPPORT_PRESET_MODE | SUPPORT_TARGET_TEMPERATURE
+    _attr_supported_features = (
+        ClimateEntityFeature.PRESET_MODE | ClimateEntityFeature.TARGET_TEMPERATURE
+    )
 
     def __init__(
         self, device_url: str, coordinator: OverkizDataUpdateCoordinator
@@ -76,12 +72,14 @@ class AtlanticElectricalHeaterWithAdjustableTemperatureSetpoint(
         """Return hvac operation ie. heat, cool mode."""
         if OverkizState.CORE_OPERATING_MODE in self.device.states:
             return OVERKIZ_TO_HVAC_MODE[
-                self.executor.select_state(OverkizState.CORE_OPERATING_MODE)
+                cast(str, self.executor.select_state(OverkizState.CORE_OPERATING_MODE))
             ]
         if OverkizState.CORE_ON_OFF in self.device.states:
             return OVERKIZ_TO_HVAC_MODE[
-                self.executor.select_state(OverkizState.CORE_ON_OFF)
+                cast(str, self.executor.select_state(OverkizState.CORE_ON_OFF))
             ]
+
+        return HVACMode.OFF
 
     async def async_set_hvac_mode(self, hvac_mode: str) -> None:
         """Set new target hvac mode."""
@@ -90,7 +88,7 @@ class AtlanticElectricalHeaterWithAdjustableTemperatureSetpoint(
                 OverkizCommand.SET_OPERATING_MODE, HVAC_MODE_TO_OVERKIZ[hvac_mode]
             )
         else:
-            if hvac_mode == HVAC_MODE_OFF:
+            if hvac_mode == HVACMode.OFF:
                 await self.executor.async_execute_command(
                     OverkizCommand.OFF,
                 )
@@ -103,12 +101,12 @@ class AtlanticElectricalHeaterWithAdjustableTemperatureSetpoint(
     def preset_mode(self) -> str | None:
         """Return the current preset mode, e.g., home, away, temp."""
         return OVERKIZ_TO_PRESET_MODE[
-            self.executor.select_state(OverkizState.IO_TARGET_HEATING_LEVEL)
+            cast(str, self.executor.select_state(OverkizState.IO_TARGET_HEATING_LEVEL))
         ]
 
     async def async_set_preset_mode(self, preset_mode: str) -> None:
         """Set new preset mode."""
-        if [PRESET_AUTO, PRESET_PROG] in preset_mode:
+        if preset_mode in [PRESET_AUTO, PRESET_PROG]:
             await self.executor.async_execute_command(
                 OverkizCommand.SET_OPERATING_MODE, PRESET_MODE_TO_OVERKIZ[preset_mode]
             )
@@ -120,7 +118,9 @@ class AtlanticElectricalHeaterWithAdjustableTemperatureSetpoint(
     @property
     def target_temperature(self) -> float | None:
         """Return the temperature."""
-        return self.executor.select_state(OverkizState.CORE_TARGET_TEMPERATURE)
+        return cast(
+            float, self.executor.select_state(OverkizState.CORE_TARGET_TEMPERATURE)
+        )
 
     @property
     def current_temperature(self) -> float | None:
@@ -130,7 +130,7 @@ class AtlanticElectricalHeaterWithAdjustableTemperatureSetpoint(
 
         return None
 
-    async def async_set_temperature(self, **kwargs) -> None:
+    async def async_set_temperature(self, **kwargs: Any) -> None:
         """Set new temperature."""
         temperature = kwargs[ATTR_TEMPERATURE]
 
