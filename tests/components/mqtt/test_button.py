e996ee8b600a8786e4512e2589fc7b5c6c80fc80
jbouwh <jan@jbsoft.nl>
2022-05-24 11:58:24 +0000

    Add button

--- tests/components/mqtt/test_button.py
+++ tests/components/mqtt/test_button.py
@@ -42,7 +42,7 @@ DEFAULT_CONFIG = {
 
 
 @pytest.mark.freeze_time("2021-11-08 13:31:44+00:00")
-async def test_sending_mqtt_commands(hass, mqtt_mock):
+async def test_sending_mqtt_commands(hass, mqtt_mock_entry):
     """Test the sending MQTT commands."""
     assert await async_setup_component(
         hass,
@@ -59,6 +59,7 @@ async def test_sending_mqtt_commands(hass, mqtt_mock):
         },
     )
     await hass.async_block_till_done()
+    mqtt_mock = await mqtt_mock_entry()
 
     state = hass.states.get("button.test_button")
     assert state.state == STATE_UNKNOWN
@@ -79,7 +80,7 @@ async def test_sending_mqtt_commands(hass, mqtt_mock):
     assert state.state == "2021-11-08T13:31:44+00:00"
 
 
-async def test_command_template(hass, mqtt_mock):
+async def test_command_template(hass, mqtt_mock_entry):
     """Test the sending of MQTT commands through a command template."""
     assert await async_setup_component(
         hass,
@@ -95,6 +96,7 @@ async def test_command_template(hass, mqtt_mock):
         },
     )
     await hass.async_block_till_done()
+    mqtt_mock = await mqtt_mock_entry()
 
     state = hass.states.get("button.test")
     assert state.state == STATE_UNKNOWN
@@ -113,21 +115,21 @@ async def test_command_template(hass, mqtt_mock):
     mqtt_mock.async_publish.reset_mock()
 
 
-async def test_availability_when_connection_lost(hass, mqtt_mock):
+async def test_availability_when_connection_lost(hass, mqtt_mock_entry):
     """Test availability after MQTT disconnection."""
     await help_test_availability_when_connection_lost(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_availability_without_topic(hass, mqtt_mock):
+async def test_availability_without_topic(hass, mqtt_mock_entry):
     """Test availability without defined availability topic."""
     await help_test_availability_without_topic(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_default_availability_payload(hass, mqtt_mock):
+async def test_default_availability_payload(hass, mqtt_mock_entry):
     """Test availability by default payload with defined topic."""
     config = {
         button.DOMAIN: {
@@ -139,11 +141,11 @@ async def test_default_availability_payload(hass, mqtt_mock):
     }
 
     await help_test_default_availability_payload(
-        hass, mqtt_mock, button.DOMAIN, config, True, "state-topic", "1"
+        hass, mqtt_mock_entry, button.DOMAIN, config, True, "state-topic", "1"
     )
 
 
-async def test_custom_availability_payload(hass, mqtt_mock):
+async def test_custom_availability_payload(hass, mqtt_mock_entry):
     """Test availability by custom payload with defined topic."""
     config = {
         button.DOMAIN: {
@@ -155,53 +157,53 @@ async def test_custom_availability_payload(hass, mqtt_mock):
     }
 
     await help_test_custom_availability_payload(
-        hass, mqtt_mock, button.DOMAIN, config, True, "state-topic", "1"
+        hass, mqtt_mock_entry, button.DOMAIN, config, True, "state-topic", "1"
     )
 
 
-async def test_setting_attribute_via_mqtt_json_message(hass, mqtt_mock):
+async def test_setting_attribute_via_mqtt_json_message(hass, mqtt_mock_entry):
     """Test the setting of attribute via MQTT with JSON payload."""
     await help_test_setting_attribute_via_mqtt_json_message(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_setting_blocked_attribute_via_mqtt_json_message(hass, mqtt_mock):
+async def test_setting_blocked_attribute_via_mqtt_json_message(hass, mqtt_mock_entry):
     """Test the setting of attribute via MQTT with JSON payload."""
     await help_test_setting_blocked_attribute_via_mqtt_json_message(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG, None
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG, None
     )
 
 
-async def test_setting_attribute_with_template(hass, mqtt_mock):
+async def test_setting_attribute_with_template(hass, mqtt_mock_entry):
     """Test the setting of attribute via MQTT with JSON payload."""
     await help_test_setting_attribute_with_template(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_update_with_json_attrs_not_dict(hass, mqtt_mock, caplog):
+async def test_update_with_json_attrs_not_dict(hass, mqtt_mock_entry, caplog):
     """Test attributes get extracted from a JSON result."""
     await help_test_update_with_json_attrs_not_dict(
-        hass, mqtt_mock, caplog, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, caplog, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_update_with_json_attrs_bad_JSON(hass, mqtt_mock, caplog):
+async def test_update_with_json_attrs_bad_JSON(hass, mqtt_mock_entry, caplog):
     """Test attributes get extracted from a JSON result."""
     await help_test_update_with_json_attrs_bad_JSON(
-        hass, mqtt_mock, caplog, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, caplog, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_discovery_update_attr(hass, mqtt_mock, caplog):
+async def test_discovery_update_attr(hass, mqtt_mock_entry, caplog):
     """Test update of discovered MQTTAttributes."""
     await help_test_discovery_update_attr(
-        hass, mqtt_mock, caplog, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, caplog, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_unique_id(hass, mqtt_mock):
+async def test_unique_id(hass, mqtt_mock_entry):
     """Test unique id option only creates one button per unique_id."""
     config = {
         button.DOMAIN: [
@@ -219,16 +221,18 @@ async def test_unique_id(hass, mqtt_mock):
             },
         ]
     }
-    await help_test_unique_id(hass, mqtt_mock, button.DOMAIN, config)
+    await help_test_unique_id(hass, mqtt_mock_entry, button.DOMAIN, config)
 
 
-async def test_discovery_removal_button(hass, mqtt_mock, caplog):
+async def test_discovery_removal_button(hass, mqtt_mock_entry, caplog):
     """Test removal of discovered button."""
     data = '{ "name": "test", "command_topic": "test_topic" }'
-    await help_test_discovery_removal(hass, mqtt_mock, caplog, button.DOMAIN, data)
+    await help_test_discovery_removal(
+        hass, mqtt_mock_entry, caplog, button.DOMAIN, data
+    )
 
 
-async def test_discovery_update_button(hass, mqtt_mock, caplog):
+async def test_discovery_update_button(hass, mqtt_mock_entry, caplog):
     """Test update of discovered button."""
     config1 = copy.deepcopy(DEFAULT_CONFIG[button.DOMAIN])
     config2 = copy.deepcopy(DEFAULT_CONFIG[button.DOMAIN])
@@ -237,7 +241,7 @@ async def test_discovery_update_button(hass, mqtt_mock, caplog):
 
     await help_test_discovery_update(
         hass,
-        mqtt_mock,
+        mqtt_mock_entry,
         caplog,
         button.DOMAIN,
         config1,
@@ -245,7 +249,7 @@ async def test_discovery_update_button(hass, mqtt_mock, caplog):
     )
 
 
-async def test_discovery_update_unchanged_button(hass, mqtt_mock, caplog):
+async def test_discovery_update_unchanged_button(hass, mqtt_mock_entry, caplog):
     """Test update of discovered button."""
     data1 = (
         '{ "name": "Beer",'
@@ -256,60 +260,60 @@ async def test_discovery_update_unchanged_button(hass, mqtt_mock, caplog):
         "homeassistant.components.mqtt.button.MqttButton.discovery_update"
     ) as discovery_update:
         await help_test_discovery_update_unchanged(
-            hass, mqtt_mock, caplog, button.DOMAIN, data1, discovery_update
+            hass, mqtt_mock_entry, caplog, button.DOMAIN, data1, discovery_update
         )
 
 
 @pytest.mark.no_fail_on_log_exception
-async def test_discovery_broken(hass, mqtt_mock, caplog):
+async def test_discovery_broken(hass, mqtt_mock_entry, caplog):
     """Test handling of bad discovery message."""
     data1 = '{ "name": "Beer" }'
     data2 = '{ "name": "Milk", "command_topic": "test_topic" }'
     await help_test_discovery_broken(
-        hass, mqtt_mock, caplog, button.DOMAIN, data1, data2
+        hass, mqtt_mock_entry, caplog, button.DOMAIN, data1, data2
     )
 
 
-async def test_entity_device_info_with_connection(hass, mqtt_mock):
+async def test_entity_device_info_with_connection(hass, mqtt_mock_entry):
     """Test MQTT button device registry integration."""
     await help_test_entity_device_info_with_connection(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_device_info_with_identifier(hass, mqtt_mock):
+async def test_entity_device_info_with_identifier(hass, mqtt_mock_entry):
     """Test MQTT button device registry integration."""
     await help_test_entity_device_info_with_identifier(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_device_info_update(hass, mqtt_mock):
+async def test_entity_device_info_update(hass, mqtt_mock_entry):
     """Test device registry update."""
     await help_test_entity_device_info_update(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_device_info_remove(hass, mqtt_mock):
+async def test_entity_device_info_remove(hass, mqtt_mock_entry):
     """Test device registry remove."""
     await help_test_entity_device_info_remove(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_id_update_discovery_update(hass, mqtt_mock):
+async def test_entity_id_update_discovery_update(hass, mqtt_mock_entry):
     """Test MQTT discovery update when entity_id is updated."""
     await help_test_entity_id_update_discovery_update(
-        hass, mqtt_mock, button.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, button.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_debug_info_message(hass, mqtt_mock):
+async def test_entity_debug_info_message(hass, mqtt_mock_entry):
     """Test MQTT debug info."""
     await help_test_entity_debug_info_message(
         hass,
-        mqtt_mock,
+        mqtt_mock_entry,
         button.DOMAIN,
         DEFAULT_CONFIG,
         button.SERVICE_PRESS,
@@ -318,7 +322,7 @@ async def test_entity_debug_info_message(hass, mqtt_mock):
     )
 
 
-async def test_invalid_device_class(hass, mqtt_mock):
+async def test_invalid_device_class(hass, mqtt_mock_entry):
     """Test device_class option with invalid value."""
     assert await async_setup_component(
         hass,
@@ -333,12 +337,13 @@ async def test_invalid_device_class(hass, mqtt_mock):
         },
     )
     await hass.async_block_till_done()
+    await mqtt_mock_entry()
 
     state = hass.states.get("button.test")
     assert state is None
 
 
-async def test_valid_device_class(hass, mqtt_mock):
+async def test_valid_device_class(hass, mqtt_mock_entry):
     """Test device_class option with valid values."""
     assert await async_setup_component(
         hass,
@@ -366,6 +371,7 @@ async def test_valid_device_class(hass, mqtt_mock):
         },
     )
     await hass.async_block_till_done()
+    await mqtt_mock_entry()
 
     state = hass.states.get("button.test_1")
     assert state.attributes["device_class"] == button.ButtonDeviceClass.UPDATE
@@ -382,7 +388,7 @@ async def test_valid_device_class(hass, mqtt_mock):
     ],
 )
 async def test_publishing_with_custom_encoding(
-    hass, mqtt_mock, caplog, service, topic, parameters, payload, template
+    hass, mqtt_mock_entry, caplog, service, topic, parameters, payload, template
 ):
     """Test publishing MQTT payload with different encoding."""
     domain = button.DOMAIN
@@ -390,7 +396,7 @@ async def test_publishing_with_custom_encoding(
 
     await help_test_publishing_with_custom_encoding(
         hass,
-        mqtt_mock,
+        mqtt_mock_entry,
         caplog,
         domain,
         config,
@@ -402,11 +408,11 @@ async def test_publishing_with_custom_encoding(
     )
 
 
-async def test_reloadable(hass, mqtt_mock, caplog, tmp_path):
+async def test_reloadable(hass, mqtt_mock_entry, caplog, tmp_path):
     """Test reloading the MQTT platform."""
     domain = button.DOMAIN
     config = DEFAULT_CONFIG[domain]
-    await help_test_reloadable(hass, mqtt_mock, caplog, tmp_path, domain, config)
+    await help_test_reloadable(hass, mqtt_mock_entry, caplog, tmp_path, domain, config)
 
 
 async def test_reloadable_late(hass, mqtt_client_mock, caplog, tmp_path):
