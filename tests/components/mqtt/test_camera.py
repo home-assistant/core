6951563c6d8addc084b77941e1ba0181d580d281
jbouwh <jan@jbsoft.nl>
2022-05-24 12:07:52 +0000

    Add camera

--- tests/components/mqtt/test_camera.py
+++ tests/components/mqtt/test_camera.py
@@ -46,7 +46,7 @@ DEFAULT_CONFIG = {
 }
 
 
-async def test_run_camera_setup(hass, hass_client_no_auth, mqtt_mock):
+async def test_run_camera_setup(hass, hass_client_no_auth, mqtt_mock_entry):
     """Test that it fetches the given payload."""
     topic = "test/camera"
     await async_setup_component(
@@ -55,6 +55,7 @@ async def test_run_camera_setup(hass, hass_client_no_auth, mqtt_mock):
         {"camera": {"platform": "mqtt", "topic": topic, "name": "Test Camera"}},
     )
     await hass.async_block_till_done()
+    await mqtt_mock_entry()
 
     url = hass.states.get("camera.test_camera").attributes["entity_picture"]
 
@@ -67,7 +68,7 @@ async def test_run_camera_setup(hass, hass_client_no_auth, mqtt_mock):
     assert body == "beer"
 
 
-async def test_run_camera_b64_encoded(hass, hass_client_no_auth, mqtt_mock):
+async def test_run_camera_b64_encoded(hass, hass_client_no_auth, mqtt_mock_entry):
     """Test that it fetches the given encoded payload."""
     topic = "test/camera"
     await async_setup_component(
@@ -83,6 +84,7 @@ async def test_run_camera_b64_encoded(hass, hass_client_no_auth, mqtt_mock):
         },
     )
     await hass.async_block_till_done()
+    await mqtt_mock_entry()
 
     url = hass.states.get("camera.test_camera").attributes["entity_picture"]
 
@@ -95,77 +97,81 @@ async def test_run_camera_b64_encoded(hass, hass_client_no_auth, mqtt_mock):
     assert body == "grass"
 
 
-async def test_availability_when_connection_lost(hass, mqtt_mock):
+async def test_availability_when_connection_lost(hass, mqtt_mock_entry):
     """Test availability after MQTT disconnection."""
     await help_test_availability_when_connection_lost(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_availability_without_topic(hass, mqtt_mock):
+async def test_availability_without_topic(hass, mqtt_mock_entry):
     """Test availability without defined availability topic."""
     await help_test_availability_without_topic(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_default_availability_payload(hass, mqtt_mock):
+async def test_default_availability_payload(hass, mqtt_mock_entry):
     """Test availability by default payload with defined topic."""
     await help_test_default_availability_payload(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_custom_availability_payload(hass, mqtt_mock):
+async def test_custom_availability_payload(hass, mqtt_mock_entry):
     """Test availability by custom payload with defined topic."""
     await help_test_custom_availability_payload(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_setting_attribute_via_mqtt_json_message(hass, mqtt_mock):
+async def test_setting_attribute_via_mqtt_json_message(hass, mqtt_mock_entry):
     """Test the setting of attribute via MQTT with JSON payload."""
     await help_test_setting_attribute_via_mqtt_json_message(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_setting_blocked_attribute_via_mqtt_json_message(hass, mqtt_mock):
+async def test_setting_blocked_attribute_via_mqtt_json_message(hass, mqtt_mock_entry):
     """Test the setting of attribute via MQTT with JSON payload."""
     await help_test_setting_blocked_attribute_via_mqtt_json_message(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG, MQTT_CAMERA_ATTRIBUTES_BLOCKED
+        hass,
+        mqtt_mock_entry,
+        camera.DOMAIN,
+        DEFAULT_CONFIG,
+        MQTT_CAMERA_ATTRIBUTES_BLOCKED,
     )
 
 
-async def test_setting_attribute_with_template(hass, mqtt_mock):
+async def test_setting_attribute_with_template(hass, mqtt_mock_entry):
     """Test the setting of attribute via MQTT with JSON payload."""
     await help_test_setting_attribute_with_template(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_update_with_json_attrs_not_dict(hass, mqtt_mock, caplog):
+async def test_update_with_json_attrs_not_dict(hass, mqtt_mock_entry, caplog):
     """Test attributes get extracted from a JSON result."""
     await help_test_update_with_json_attrs_not_dict(
-        hass, mqtt_mock, caplog, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, caplog, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_update_with_json_attrs_bad_JSON(hass, mqtt_mock, caplog):
+async def test_update_with_json_attrs_bad_JSON(hass, mqtt_mock_entry, caplog):
     """Test attributes get extracted from a JSON result."""
     await help_test_update_with_json_attrs_bad_JSON(
-        hass, mqtt_mock, caplog, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, caplog, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_discovery_update_attr(hass, mqtt_mock, caplog):
+async def test_discovery_update_attr(hass, mqtt_mock_entry, caplog):
     """Test update of discovered MQTTAttributes."""
     await help_test_discovery_update_attr(
-        hass, mqtt_mock, caplog, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, caplog, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_unique_id(hass, mqtt_mock):
+async def test_unique_id(hass, mqtt_mock_entry):
     """Test unique id option only creates one camera per unique_id."""
     config = {
         camera.DOMAIN: [
@@ -183,94 +189,96 @@ async def test_unique_id(hass, mqtt_mock):
             },
         ]
     }
-    await help_test_unique_id(hass, mqtt_mock, camera.DOMAIN, config)
+    await help_test_unique_id(hass, mqtt_mock_entry, camera.DOMAIN, config)
 
 
-async def test_discovery_removal_camera(hass, mqtt_mock, caplog):
+async def test_discovery_removal_camera(hass, mqtt_mock_entry, caplog):
     """Test removal of discovered camera."""
     data = json.dumps(DEFAULT_CONFIG[camera.DOMAIN])
-    await help_test_discovery_removal(hass, mqtt_mock, caplog, camera.DOMAIN, data)
+    await help_test_discovery_removal(
+        hass, mqtt_mock_entry, caplog, camera.DOMAIN, data
+    )
 
 
-async def test_discovery_update_camera(hass, mqtt_mock, caplog):
+async def test_discovery_update_camera(hass, mqtt_mock_entry, caplog):
     """Test update of discovered camera."""
     config1 = {"name": "Beer", "topic": "test_topic"}
     config2 = {"name": "Milk", "topic": "test_topic"}
 
     await help_test_discovery_update(
-        hass, mqtt_mock, caplog, camera.DOMAIN, config1, config2
+        hass, mqtt_mock_entry, caplog, camera.DOMAIN, config1, config2
     )
 
 
-async def test_discovery_update_unchanged_camera(hass, mqtt_mock, caplog):
+async def test_discovery_update_unchanged_camera(hass, mqtt_mock_entry, caplog):
     """Test update of discovered camera."""
     data1 = '{ "name": "Beer", "topic": "test_topic"}'
     with patch(
         "homeassistant.components.mqtt.camera.MqttCamera.discovery_update"
     ) as discovery_update:
         await help_test_discovery_update_unchanged(
-            hass, mqtt_mock, caplog, camera.DOMAIN, data1, discovery_update
+            hass, mqtt_mock_entry, caplog, camera.DOMAIN, data1, discovery_update
         )
 
 
 @pytest.mark.no_fail_on_log_exception
-async def test_discovery_broken(hass, mqtt_mock, caplog):
+async def test_discovery_broken(hass, mqtt_mock_entry, caplog):
     """Test handling of bad discovery message."""
     data1 = '{ "name": "Beer" }'
     data2 = '{ "name": "Milk", "topic": "test_topic"}'
 
     await help_test_discovery_broken(
-        hass, mqtt_mock, caplog, camera.DOMAIN, data1, data2
+        hass, mqtt_mock_entry, caplog, camera.DOMAIN, data1, data2
     )
 
 
-async def test_entity_device_info_with_connection(hass, mqtt_mock):
+async def test_entity_device_info_with_connection(hass, mqtt_mock_entry):
     """Test MQTT camera device registry integration."""
     await help_test_entity_device_info_with_connection(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_device_info_with_identifier(hass, mqtt_mock):
+async def test_entity_device_info_with_identifier(hass, mqtt_mock_entry):
     """Test MQTT camera device registry integration."""
     await help_test_entity_device_info_with_identifier(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_device_info_update(hass, mqtt_mock):
+async def test_entity_device_info_update(hass, mqtt_mock_entry):
     """Test device registry update."""
     await help_test_entity_device_info_update(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_device_info_remove(hass, mqtt_mock):
+async def test_entity_device_info_remove(hass, mqtt_mock_entry):
     """Test device registry remove."""
     await help_test_entity_device_info_remove(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_id_update_subscriptions(hass, mqtt_mock):
+async def test_entity_id_update_subscriptions(hass, mqtt_mock_entry):
     """Test MQTT subscriptions are managed when entity_id is updated."""
     await help_test_entity_id_update_subscriptions(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG, ["test_topic"]
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG, ["test_topic"]
     )
 
 
-async def test_entity_id_update_discovery_update(hass, mqtt_mock):
+async def test_entity_id_update_discovery_update(hass, mqtt_mock_entry):
     """Test MQTT discovery update when entity_id is updated."""
     await help_test_entity_id_update_discovery_update(
-        hass, mqtt_mock, camera.DOMAIN, DEFAULT_CONFIG
+        hass, mqtt_mock_entry, camera.DOMAIN, DEFAULT_CONFIG
     )
 
 
-async def test_entity_debug_info_message(hass, mqtt_mock):
+async def test_entity_debug_info_message(hass, mqtt_mock_entry):
     """Test MQTT debug info."""
     await help_test_entity_debug_info_message(
         hass,
-        mqtt_mock,
+        mqtt_mock_entry,
         camera.DOMAIN,
         DEFAULT_CONFIG,
         None,
@@ -279,11 +287,11 @@ async def test_entity_debug_info_message(hass, mqtt_mock):
     )
 
 
-async def test_reloadable(hass, mqtt_mock, caplog, tmp_path):
+async def test_reloadable(hass, mqtt_mock_entry, caplog, tmp_path):
     """Test reloading the MQTT platform."""
     domain = camera.DOMAIN
     config = DEFAULT_CONFIG[domain]
-    await help_test_reloadable(hass, mqtt_mock, caplog, tmp_path, domain, config)
+    await help_test_reloadable(hass, mqtt_mock_entry, caplog, tmp_path, domain, config)
 
 
 async def test_reloadable_late(hass, mqtt_client_mock, caplog, tmp_path):
