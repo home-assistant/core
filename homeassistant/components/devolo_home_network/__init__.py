888b2288f9e0c2aa2d32023f11a0e235a4542de0
Guido Schmitz <guido.schmitz@fedaix.de>
2022-05-25 08:03:30 +0000

    Cover cases without existing password

--- homeassistant/components/devolo_home_network/__init__.py
+++ homeassistant/components/devolo_home_network/__init__.py
@@ -39,7 +39,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         device = Device(
             ip=entry.data[CONF_IP_ADDRESS], zeroconf_instance=zeroconf_instance
         )
-        device.password = entry.data[CONF_PASSWORD]
+        device.password = entry.data.get(CONF_PASSWORD, "")
         await device.async_connect(session_instance=async_client)
     except DeviceNotFound as err:
         raise ConfigEntryNotReady(
