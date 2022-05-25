5d9d2af5c7c1b833222a805a813b8afc861e8aa5
mrterdll <omert.erdal@gmail.com>
2022-05-25 06:20:34 +0000

    Updated

--- homeassistant/components/knovvu/tts.py
+++ homeassistant/components/knovvu/tts.py
@@ -1,12 +1,16 @@
 """Support for the Knovvu speech service."""
 import json
 import logging
+import os
 
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import jwt
 import requests
 import voluptuous as vol
 
 from homeassistant.components.tts import CONF_LANG, PLATFORM_SCHEMA, Provider
-from homeassistant.const import CONF_API_TOKEN, CONF_URL
+from homeassistant.const import CONF_URL
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
@@ -47,6 +51,9 @@ MAX_RATE = 3
 MIN_VOLUME = 0.0
 MAX_VOLUME = 2
 
+CONF_CLIENT_ID = "client_id"
+CONF_CLIENT_SECRET = "client_secret"
+CONF_URL_TOKEN = "url_token"
 CONF_CODEC = "codec"
 CONF_VOICE = "voice"
 CONF_VOLUME = "volume"
@@ -54,6 +61,7 @@ CONF_RATE = "rate"
 CONF_SAMPLE_RATE = "sample_rate"
 
 DEFAULT_URL = "https://ttsapi.knovvu.com/v1/speech/synthesis/tts"
+DEFAULT_URL_TOKEN = "https://identity.ldm.knovvu.com/connect/token"
 DEFAULT_CODEC = "wav"
 DEFAULT_LANG = "Sestek Gul 24k_HV_Premium"
 DEFAULT_VOICE = "Sestek Gul 24k_HV_Premium"
@@ -63,7 +71,9 @@ DEFAULT_SAMPLE_RATE = 24000
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Required(CONF_API_TOKEN): cv.string,
+        vol.Required(CONF_CLIENT_ID): cv.string,
+        vol.Required(CONF_CLIENT_SECRET): cv.string,
+        vol.Optional(CONF_URL_TOKEN, default=DEFAULT_URL_TOKEN): cv.string,
         vol.Optional(CONF_URL, default=DEFAULT_URL): cv.string,
         vol.Optional(CONF_LANG, default=DEFAULT_LANG): vol.In(SUPPORT_LANGUAGES),
         vol.Optional(CONF_CODEC, default=DEFAULT_CODEC): vol.In(SUPPORT_CODECS),
@@ -95,7 +105,9 @@ class KnovvuProvider(Provider):
         """Init knovvu TTS service."""
         self.hass = hass
         self._key = conf.get(CONF_URL)
-        self._token = conf.get(CONF_API_TOKEN)
+        self._url_token = conf.get(CONF_URL_TOKEN)
+        self._id = conf.get(CONF_CLIENT_ID)
+        self._secret = conf.get(CONF_CLIENT_SECRET)
         self._codec = conf.get(CONF_CODEC)
         self._language = conf.get(CONF_LANG)
         self._voice = conf.get(CONF_VOICE)
@@ -124,8 +136,61 @@ class KnovvuProvider(Provider):
         options = options or {}
         output_format = options.get(CONF_CODEC, self._codec)
 
+        if os.path.exists("encryptedldmtoken.bin"):
+
+            # Decryption
+            with open("encryptedldmtoken.bin", "rb") as file_in:
+                nonce, tag, ciphertext = (file_in.read(x) for x in (16, 16, -1))
+            file_in.close()
+
+            with open("key.bin", "rb") as ffile:
+                key = ffile.readline()
+            ffile.close()
+
+            cipher = AES.new(key, AES.MODE_EAX, nonce)
+            token = cipher.decrypt_and_verify(ciphertext, tag)
+            token = token.decode("utf-8")
+
+            token_data = token.split()
+            decoded = jwt.decode(token_data[1], options={"verify_signature": False})
+            expiry_date = decoded["exp"]
+
+            if expiry_date < 5000:
+                os.remove("ldmtoken.txt")
+                _LOGGER.warning(
+                    "Expiration date is less than 5000. Token is refreshing. : %s",
+                    expiry_date,
+                )
+        else:
+            url_token = self._url_token
+            header = {
+                "client_id": self._id,
+                "client_secret": self._secret,
+                "grant_type": "client_credentials",
+                "scop": "Ldm_Integration",
+                "Content-Type": "application/json",
+            }
+            ldm_token = requests.post(url=url_token, data=header, verify=False)
+
+            data = ldm_token.json()
+            token = data["token_type"] + " " + data["access_token"]
+
+            # Encryption
+            key = get_random_bytes(16)
+            cipher = AES.new(key, AES.MODE_EAX)
+            ciphertext, tag = cipher.encrypt_and_digest(token.encode("utf-8"))
+
+            with open("encryptedldmtoken.bin", "wb") as file_out:
+                for chiper_iter in (cipher.nonce, tag, ciphertext):
+                    file_out.write(chiper_iter)
+            file_out.close()
+
+            with open("key.bin", "wb") as ffile:
+                ffile.write(key)
+            ffile.close()
+
         url = self._key
-        header = {"Authorization": self._token, "Content-Type": "application/json"}
+        header = {"Authorization": token, "Content-Type": "application/json"}
         data_dict = {
             "Text": message,
             "Voice": {
