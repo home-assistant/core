b55452c7918cd04db564f85558c4defeb41a6ed7
Chris Talkington <chris@talkingtontech.com>
2022-05-25 21:38:46 -0500

    Update test_select.py

--- tests/components/roku/test_select.py
+++ tests/components/roku/test_select.py
@@ -10,6 +10,7 @@ from homeassistant.components.select import DOMAIN as SELECT_DOMAIN
 from homeassistant.components.select.const import ATTR_OPTION, ATTR_OPTIONS
 from homeassistant.const import ATTR_ENTITY_ID, ATTR_ICON, SERVICE_SELECT_OPTION
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import entity_registry as er
 import homeassistant.util.dt as dt_util
 
