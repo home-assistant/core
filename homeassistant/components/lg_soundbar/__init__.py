90ab2f8d87cfe5b70e35ad24d881d6d708881117
Mason Crawford <Mason.Crawford@empired.com>
2022-05-26 04:03:00 +0000

    Verify the device is reachable before setting up the platform

--- homeassistant/components/lg_soundbar/__init__.py
+++ homeassistant/components/lg_soundbar/__init__.py
@@ -2,8 +2,10 @@
 import logging
 
 from homeassistant import config_entries, core
-from homeassistant.const import Platform
+from homeassistant.const import CONF_HOST, CONF_PORT, Platform
+from homeassistant.exceptions import ConfigEntryNotReady
 
+from .config_flow import test_connect
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -16,6 +18,11 @@ async def async_setup_entry(
 ) -> bool:
     """Set up platform from a ConfigEntry."""
     hass.data.setdefault(DOMAIN, {})
+    # Verify the device is reachable before setting up the platform
+    try:
+        test_connect(entry.options[CONF_HOST], entry.options[CONF_PORT])
+    except ConnectionError as err:
+        raise ConfigEntryNotReady from err
 
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
