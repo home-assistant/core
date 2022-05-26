70d1fb0c6146626a4474868a9ad68f7922151b6e
Joakim Plate <elupus@ecce.se>
2022-05-27 00:45:03 +0200

    Don't set hardware filters for service id
    Turns out newer bluez will filter out and not report manufacturer data that is transmitted as a separate announcement.

--- homeassistant/components/fjaraskupan/config_flow.py
+++ homeassistant/components/fjaraskupan/config_flow.py
@@ -7,7 +7,7 @@ import async_timeout
 from bleak import BleakScanner
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
-from fjaraskupan import UUID_SERVICE, device_filter
+from fjaraskupan import device_filter
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.config_entry_flow import register_discovery_flow
@@ -26,9 +26,7 @@ async def _async_has_devices(hass: HomeAssistant) -> bool:
         if device_filter(device, advertisement_data):
             event.set()
 
-    async with BleakScanner(
-        detection_callback=detection, filters={"UUIDs": [str(UUID_SERVICE)]}
-    ):
+    async with BleakScanner(detection_callback=detection):
         try:
             async with async_timeout.timeout(CONST_WAIT_TIME):
                 await event.wait()
