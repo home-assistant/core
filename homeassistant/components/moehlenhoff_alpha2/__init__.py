0295bc07b8cbc251e44922de5a01bcb9f6d182a0
j-a-n <oss@janschneider.net>
2022-05-26 13:23:49 +0200

    Fix Moehlenhoff Alpha2 set_target_temperature and set_heat_area_mode (#72533)
    Fix set_target_temperature and set_heat_area_mode

--- homeassistant/components/moehlenhoff_alpha2/__init__.py
+++ homeassistant/components/moehlenhoff_alpha2/__init__.py
@@ -98,7 +98,7 @@ class Alpha2BaseCoordinator(DataUpdateCoordinator[dict[str, dict]]):
 
         update_data = {"T_TARGET": target_temperature}
         is_cooling = self.get_cooling()
-        heat_area_mode = self.data[heat_area_id]["HEATAREA_MODE"]
+        heat_area_mode = self.data["heat_areas"][heat_area_id]["HEATAREA_MODE"]
         if heat_area_mode == 1:
             if is_cooling:
                 update_data["T_COOL_DAY"] = target_temperature
@@ -116,7 +116,7 @@ class Alpha2BaseCoordinator(DataUpdateCoordinator[dict[str, dict]]):
             raise HomeAssistantError(
                 "Failed to set target temperature, communication error with alpha2 base"
             ) from http_err
-        self.data[heat_area_id].update(update_data)
+        self.data["heat_areas"][heat_area_id].update(update_data)
         for update_callback in self._listeners:
             update_callback()
 
@@ -141,25 +141,25 @@ class Alpha2BaseCoordinator(DataUpdateCoordinator[dict[str, dict]]):
                 "Failed to set heat area mode, communication error with alpha2 base"
             ) from http_err
 
-        self.data[heat_area_id]["HEATAREA_MODE"] = heat_area_mode
+        self.data["heat_areas"][heat_area_id]["HEATAREA_MODE"] = heat_area_mode
         is_cooling = self.get_cooling()
         if heat_area_mode == 1:
             if is_cooling:
-                self.data[heat_area_id]["T_TARGET"] = self.data[heat_area_id][
-                    "T_COOL_DAY"
-                ]
+                self.data["heat_areas"][heat_area_id]["T_TARGET"] = self.data[
+                    "heat_areas"
+                ][heat_area_id]["T_COOL_DAY"]
             else:
-                self.data[heat_area_id]["T_TARGET"] = self.data[heat_area_id][
-                    "T_HEAT_DAY"
-                ]
+                self.data["heat_areas"][heat_area_id]["T_TARGET"] = self.data[
+                    "heat_areas"
+                ][heat_area_id]["T_HEAT_DAY"]
         elif heat_area_mode == 2:
             if is_cooling:
-                self.data[heat_area_id]["T_TARGET"] = self.data[heat_area_id][
-                    "T_COOL_NIGHT"
-                ]
+                self.data["heat_areas"][heat_area_id]["T_TARGET"] = self.data[
+                    "heat_areas"
+                ][heat_area_id]["T_COOL_NIGHT"]
             else:
-                self.data[heat_area_id]["T_TARGET"] = self.data[heat_area_id][
-                    "T_HEAT_NIGHT"
-                ]
+                self.data["heat_areas"][heat_area_id]["T_TARGET"] = self.data[
+                    "heat_areas"
+                ][heat_area_id]["T_HEAT_NIGHT"]
         for update_callback in self._listeners:
             update_callback()
