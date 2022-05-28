3e6df99b2d9cca892b301dfaca9b255dc14b834d
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2022-05-28 08:24:56 +0200

    Remove copied name

--- tests/components/homewizard/conftest.py
+++ tests/components/homewizard/conftest.py
@@ -66,7 +66,7 @@ async def init_integration(
     mock_config_entry: MockConfigEntry,
     mock_homewizardenergy: AsyncMock,
 ) -> MockConfigEntry:
-    """Set up the PVOutput integration for testing."""
+    """Set up the HomeWizard integration for testing."""
     mock_config_entry.add_to_hass(hass)
 
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
