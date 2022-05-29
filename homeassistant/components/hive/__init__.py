1910c18f68f835b12b537004a20a107147eb1b2d
Khole <kjtech6@outlook.com>
2022-05-29 11:08:50 +0100

    Refactor hive entity (#72311)
    * Add hive category entity changes
    
    * Updates based on PR feedback
    
    * Revert libary bump
    
    * Update after PR feedback
    
    * Update Binary device class for smoke sensor
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * Remove entity category
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * Updates after PR review
    
    * Remove unused import
    
    * Update light based on PR feedback
    
    * Update light code from PR review
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/hive/__init__.py
+++ homeassistant/components/hive/__init__.py
@@ -22,7 +22,7 @@ from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
 )
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import DeviceInfo, Entity
 from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN, PLATFORM_LOOKUP, PLATFORMS
@@ -132,8 +132,17 @@ class HiveEntity(Entity):
         """Initialize the instance."""
         self.hive = hive
         self.device = hive_device
+        self._attr_name = self.device["haName"]
+        self._attr_unique_id = f'{self.device["hiveID"]}-{self.device["hiveType"]}'
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, self.device["device_id"])},
+            model=self.device["deviceData"]["model"],
+            manufacturer=self.device["deviceData"]["manufacturer"],
+            name=self.device["device_name"],
+            sw_version=self.device["deviceData"]["version"],
+            via_device=(DOMAIN, self.device["parentDevice"]),
+        )
         self.attributes = {}
-        self._unique_id = f'{self.device["hiveID"]}-{self.device["hiveType"]}'
 
     async def async_added_to_hass(self):
         """When entity is added to Home Assistant."""
