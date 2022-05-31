7337c49a5a47d40a9891a887258aea3aa8ab8046
epenet <6771947+epenet@users.noreply.github.com>
2022-05-31 07:32:07 +0000

    cleanup

--- tests/components/onewire/test_init.py
+++ tests/components/onewire/test_init.py
@@ -87,30 +87,28 @@ async def test_registry_cleanup(
 
     entry_id = config_entry.entry_id
     device_registry = dr.async_get(hass)
+    live_id = "10.111111111111"
+    dead_id = "28.111111111111"
 
     # Initialise with two components
-    setup_owproxy_mock_devices(
-        owproxy, Platform.SENSOR, ["10.111111111111", "28.111111111111"]
-    )
+    setup_owproxy_mock_devices(owproxy, Platform.SENSOR, [live_id, dead_id])
     await hass.config_entries.async_setup(entry_id)
     await hass.async_block_till_done()
 
     # Reload with a device no longer on bus
-    setup_owproxy_mock_devices(owproxy, Platform.SENSOR, ["10.111111111111"])
+    setup_owproxy_mock_devices(owproxy, Platform.SENSOR, [live_id])
     await hass.config_entries.async_reload(entry_id)
     await hass.async_block_till_done()
     assert len(dr.async_entries_for_config_entry(device_registry, entry_id)) == 2
 
-    # Try to remove "10.111111111111" - fails
-    identifiers = {(DOMAIN, "10.111111111111")}
-    device = device_registry.async_get_device(identifiers=identifiers)
+    # Try to remove "10.111111111111" - fails as it is live
+    device = device_registry.async_get_device(identifiers={(DOMAIN, live_id)})
     assert await remove_device(await hass_ws_client(hass), device.id, entry_id) is False
     assert len(dr.async_entries_for_config_entry(device_registry, entry_id)) == 2
-    assert device_registry.async_get_device(identifiers=identifiers) is not None
+    assert device_registry.async_get_device(identifiers={(DOMAIN, live_id)}) is not None
 
-    # Try to remove "28.111111111111" - succeeds
-    identifiers = {(DOMAIN, "28.111111111111")}
-    device = device_registry.async_get_device(identifiers=identifiers)
+    # Try to remove "28.111111111111" - succeeds as it is dead
+    device = device_registry.async_get_device(identifiers={(DOMAIN, dead_id)})
     assert await remove_device(await hass_ws_client(hass), device.id, entry_id) is True
     assert len(dr.async_entries_for_config_entry(device_registry, entry_id)) == 1
-    assert device_registry.async_get_device(identifiers=identifiers) is None
+    assert device_registry.async_get_device(identifiers={(DOMAIN, dead_id)}) is None
