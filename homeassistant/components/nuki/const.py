7be94183894910dba02ef1b796ca73f3e1a14c54
Iván Cea Fontenla <ivancea96@outlook.com>
2022-05-31 22:17:10 +0000

    feat(nuki): add ring action timestamp attribute

--- homeassistant/components/nuki/const.py
+++ homeassistant/components/nuki/const.py
@@ -6,6 +6,7 @@ ATTR_BATTERY_CRITICAL = "battery_critical"
 ATTR_NUKI_ID = "nuki_id"
 ATTR_ENABLE = "enable"
 ATTR_UNLATCH = "unlatch"
+ATTR_RING_ACTION_TIMESTAMP = "ring_action_timestamp"
 
 # Data
 DATA_BRIDGE = "nuki_bridge_data"
--- homeassistant/components/nuki/lock.py
+++ homeassistant/components/nuki/lock.py
@@ -9,12 +9,14 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv, entity_platform
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.util import dt as dt_util
 
 from . import NukiEntity
 from .const import (
     ATTR_BATTERY_CRITICAL,
     ATTR_ENABLE,
     ATTR_NUKI_ID,
+    ATTR_RING_ACTION_TIMESTAMP,
     ATTR_UNLATCH,
     DATA_COORDINATOR,
     DATA_LOCKS,
@@ -143,6 +145,17 @@ class NukiOpenerEntity(NukiDeviceEntity):
             self._nuki_device.is_rto_activated
             or self._nuki_device.mode == MODE_OPENER_CONTINUOUS
         )
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        attrs = super().extra_state_attributes
+
+        attrs[ATTR_RING_ACTION_TIMESTAMP] = dt_util.parse_datetime(
+            self._nuki_device.ring_action_timestamp
+        )
+
+        return attrs
 
     def lock(self, **kwargs):
         """Disable ring-to-open."""
