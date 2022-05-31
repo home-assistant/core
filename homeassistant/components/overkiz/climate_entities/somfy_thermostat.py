d353051330296a29fefb09beeac77339dfcac485
Thibaut <thibaut@etienne.pw>
2022-05-31 08:00:55 +0000

    Use HVACMode

--- homeassistant/components/overkiz/climate_entities/somfy_thermostat.py
+++ homeassistant/components/overkiz/climate_entities/somfy_thermostat.py
@@ -7,13 +7,12 @@ from pyoverkiz.enums import OverkizCommand, OverkizState
 
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
-    HVAC_MODE_AUTO,
-    HVAC_MODE_HEAT,
     PRESET_AWAY,
     PRESET_HOME,
     PRESET_NONE,
     ClimateEntityFeature,
     HVACAction,
+    HVACMode,
 )
 from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
 
@@ -33,10 +32,13 @@ STATE_PRESET_MANUAL = "manualMode"
 STATE_PRESET_SLEEPING_MODE = "sleepingMode"
 STATE_PRESET_SUDDEN_DROP_MODE = "suddenDropMode"
 
-OVERKIZ_TO_HVAC_MODES: dict[str, str] = {
-    STATE_DEROGATION_ACTIVE: HVAC_MODE_HEAT,
-    STATE_DEROGATION_INACTIVE: HVAC_MODE_AUTO,
+
+OVERKIZ_TO_HVAC_MODES: dict[str, HVACMode] = {
+    STATE_DEROGATION_ACTIVE: HVACMode.HEAT,
+    STATE_DEROGATION_INACTIVE: HVACMode.AUTO,
 }
+HVAC_MODES_TO_OVERKIZ = {v: k for k, v in OVERKIZ_TO_HVAC_MODES.items()}
+
 OVERKIZ_TO_PRESET_MODES: dict[str, str] = {
     STATE_PRESET_AT_HOME: PRESET_HOME,
     STATE_PRESET_AWAY: PRESET_AWAY,
@@ -61,7 +63,7 @@ class SomfyThermostat(OverkizEntity, ClimateEntity):
     _attr_supported_features = (
         ClimateEntityFeature.PRESET_MODE | ClimateEntityFeature.TARGET_TEMPERATURE
     )
-    _attr_hvac_modes = [HVAC_MODE_AUTO, HVAC_MODE_HEAT]
+    _attr_hvac_modes = [*HVAC_MODES_TO_OVERKIZ]
     _attr_preset_modes = [*PRESET_MODES_TO_OVERKIZ]
     # Both min and max temp values have been retrieved from the Somfy Application.
     _attr_min_temp = 15.0
@@ -95,7 +97,7 @@ class SomfyThermostat(OverkizEntity, ClimateEntity):
     @property
     def preset_mode(self) -> str:
         """Return the current preset mode, e.g., home, away, temp."""
-        if self.hvac_mode == HVAC_MODE_AUTO:
+        if self.hvac_mode == HVACMode.AUTO:
             state_key = OverkizState.SOMFY_THERMOSTAT_HEATING_MODE
         else:
             state_key = OverkizState.SOMFY_THERMOSTAT_DEROGATION_HEATING_MODE
@@ -117,7 +119,7 @@ class SomfyThermostat(OverkizEntity, ClimateEntity):
     @property
     def target_temperature(self) -> float | None:
         """Return the temperature we try to reach."""
-        if self.hvac_mode == HVAC_MODE_AUTO:
+        if self.hvac_mode == HVACMode.AUTO:
             if self.preset_mode == PRESET_NONE:
                 return None
             return cast(
@@ -141,9 +143,9 @@ class SomfyThermostat(OverkizEntity, ClimateEntity):
         )
         await self.executor.async_execute_command(OverkizCommand.REFRESH_STATE)
 
-    async def async_set_hvac_mode(self, hvac_mode: str) -> None:
+    async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:
         """Set new target hvac mode."""
-        if hvac_mode == HVAC_MODE_AUTO:
+        if hvac_mode == HVACMode.AUTO:
             await self.executor.async_execute_command(OverkizCommand.EXIT_DEROGATION)
             await self.executor.async_execute_command(OverkizCommand.REFRESH_STATE)
         else:
