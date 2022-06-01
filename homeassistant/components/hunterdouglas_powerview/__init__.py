701223a32e858fd23b8118449b83d89eccc406a9
kingy444 <toddlesking4@hotmail.com>
2022-06-01 14:38:06 +0000

    Update Diagnostics to work with PowerviewShadeDataModel

--- homeassistant/components/hunterdouglas_powerview/__init__.py
+++ homeassistant/components/hunterdouglas_powerview/__init__.py
@@ -87,9 +87,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
         async with async_timeout.timeout(10):
             shades = Shades(pv_request)
-            shade_data = async_map_data_by_id(
-                (await shades.get_resources())[SHADE_DATA]
-            )
+            shade_entries = await shades.get_resources()
+            shade_data = async_map_data_by_id(shade_entries[SHADE_DATA])
     except HUB_EXCEPTIONS as err:
         raise ConfigEntryNotReady(
             f"Connection error to PowerView hub: {hub_address}: {err}"
@@ -99,6 +98,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     coordinator = PowerviewShadeUpdateCoordinator(hass, shades, hub_address)
     coordinator.async_set_updated_data(PowerviewShadeData())
+    coordinator.data.store_group_data(shade_entries[SHADE_DATA])
+
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
         PV_API: pv_request,
         PV_ROOM_DATA: room_data,
