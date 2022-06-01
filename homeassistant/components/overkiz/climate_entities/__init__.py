0d0e95c4af4f5635e7de0da0b437f47d8e848e85
Thibaut <thibaut@etienne.pw>
2022-06-01 09:13:30 +0000

    Sort import

--- homeassistant/components/overkiz/climate_entities/__init__.py
+++ homeassistant/components/overkiz/climate_entities/__init__.py
@@ -1,6 +1,7 @@
 """Climate entities for the Overkiz (by Somfy) integration."""
 from pyoverkiz.enums.ui import UIWidget
 
+from .atlantic_electrical_heater import AtlanticElectricalHeater
 from .atlantic_electrical_heater_with_adjustable_temperature_setpoint import (
     AtlanticElectricalHeaterWithAdjustableTemperatureSetpoint,
 )
