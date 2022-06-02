5b4d4b322344312e64e0037d346fcddf28293db6
J. Nick Koston <nick@koston.org>
2022-06-01 15:06:29 -1000

    tweak

--- homeassistant/components/radiotherm/config_flow.py
+++ homeassistant/components/radiotherm/config_flow.py
@@ -90,6 +90,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_import(self, import_info: dict[str, Any]) -> FlowResult:
         """Import from yaml."""
         host = import_info[CONF_HOST]
+        _LOGGER.debug("Importing entry for host: %s", host)
         try:
             init_data = await validate_connection(self.hass, host)
         except CannotConnect as ex:
