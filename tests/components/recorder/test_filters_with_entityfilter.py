c9a701890292ed3d5abe5199cc87a17b931be714
J. Nick Koston <nick@koston.org>
2022-06-02 14:29:49 -1000

    add a test to verify the history test is wrong based on the entity filter

--- tests/components/recorder/test_filters_with_entityfilter.py
+++ tests/components/recorder/test_filters_with_entityfilter.py
@@ -328,3 +328,49 @@ async def test_included_and_excluded_complex_case(hass, recorder_mock):
 
     assert filtered_events_entity_ids == filter_accept
     assert not filtered_events_entity_ids.intersection(filter_reject)
+
+
+async def test_included_entities_and_excluded_domain(hass, recorder_mock):
+    """Test filters with included entities and excluded domain."""
+    filter_accept = {
+        "media_player.test",
+        "media_player.test3",
+        "thermostat.test",
+        "zone.home",
+        "script.can_cancel_this_one",
+    }
+    filter_reject = {
+        "thermostat.test2",
+    }
+    conf = {
+        CONF_INCLUDE: {
+            CONF_ENTITIES: ["media_player.test", "thermostat.test"],
+        },
+        CONF_EXCLUDE: {
+            CONF_DOMAINS: ["thermostat"],
+        },
+    }
+
+    extracted_filter = extract_include_exclude_filter_conf(conf)
+    entity_filter = convert_include_exclude_filter(extracted_filter)
+    sqlalchemy_filter = sqlalchemy_filter_from_include_exclude_conf(extracted_filter)
+    assert sqlalchemy_filter is not None
+
+    for entity_id in filter_accept:
+        assert entity_filter(entity_id) is True
+
+    for entity_id in filter_reject:
+        assert entity_filter(entity_id) is False
+
+    (
+        filtered_states_entity_ids,
+        filtered_events_entity_ids,
+    ) = await _async_get_states_and_events_with_filter(
+        hass, sqlalchemy_filter, filter_accept | filter_reject
+    )
+
+    assert filtered_states_entity_ids == filter_accept
+    assert not filtered_states_entity_ids.intersection(filter_reject)
+
+    assert filtered_events_entity_ids == filter_accept
+    assert not filtered_events_entity_ids.intersection(filter_reject)
