aaf1ad34e1327129be692da3c5aab69ee7ff6cc3
J. Nick Koston <nick@koston.org>
2022-06-05 11:50:46 -1000

    tweaks

--- homeassistant/components/logbook/queries/common.py
+++ homeassistant/components/logbook/queries/common.py
@@ -10,6 +10,7 @@ from sqlalchemy.sql.elements import ClauseList
 from sqlalchemy.sql.expression import literal
 from sqlalchemy.sql.selectable import Select
 
+from homeassistant.components.recorder.filters import like_domain_matchers
 from homeassistant.components.recorder.models import (
     EVENTS_CONTEXT_ID_INDEX,
     OLD_FORMAT_ATTRS_JSON,
@@ -24,16 +25,15 @@ from homeassistant.components.recorder.models import (
 
 from ..const import ALWAYS_CONTINUOUS_DOMAINS, CONDITIONALLY_CONTINUOUS_DOMAINS
 
-CONDITIONALLY_CONTINUOUS_ENTITY_ID_LIKE = [
-    f"{domain}.%" for domain in CONDITIONALLY_CONTINUOUS_DOMAINS
-]
-ALWAYS_CONTINUOUS_ENTITY_ID_LIKE = [
-    f"{domain}.%" for domain in ALWAYS_CONTINUOUS_DOMAINS
-]
+# Domains that are continuous if there is a UOM set on the entity
+CONDITIONALLY_CONTINUOUS_ENTITY_ID_LIKE = like_domain_matchers(
+    CONDITIONALLY_CONTINUOUS_DOMAINS
+)
+# Domains that are always continuous
+ALWAYS_CONTINUOUS_ENTITY_ID_LIKE = like_domain_matchers(ALWAYS_CONTINUOUS_DOMAINS)
 
 UNIT_OF_MEASUREMENT_JSON = '"unit_of_measurement":'
 UNIT_OF_MEASUREMENT_JSON_LIKE = f"%{UNIT_OF_MEASUREMENT_JSON}%"
-
 
 PSUEDO_EVENT_STATE_CHANGED = None
 # Since we don't store event_types and None
