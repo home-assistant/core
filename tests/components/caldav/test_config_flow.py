f7d44816613d14b6a6d774f5882a6bfb87999a2a
cyr-ius <cyr-ius@ipocus.net>
2022-06-05 18:23:05 +0200

    Add tests for config_flow

---
+++ tests/components/caldav/test_config_flow.py
@@ -0,0 +1,238 @@
+"""Test the Caldav config flow."""
+
+from unittest.mock import patch
+
+from caldav.lib.error import DAVError
+
+from homeassistant.components.caldav.const import (
+    CONF_ADD_CUSTO_CALENDAR,
+    CONF_CALENDAR,
+    CONF_CALENDARS,
+    CONF_CUSTOM_CALENDARS,
+    CONF_DAYS,
+    CONF_SEARCH,
+    DOMAIN,
+)
+from homeassistant.config_entries import SOURCE_IMPORT, SOURCE_USER
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_PASSWORD,
+    CONF_SOURCE,
+    CONF_URL,
+    CONF_USERNAME,
+    CONF_VERIFY_SSL,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import (
+    RESULT_TYPE_ABORT,
+    RESULT_TYPE_CREATE_ENTRY,
+    RESULT_TYPE_FORM,
+)
+
+from tests.common import MockConfigEntry
+
+USER_INPUT = {
+    CONF_USERNAME: "username",
+    CONF_PASSWORD: "password",
+    CONF_URL: "http://homeassistant.io",
+    CONF_VERIFY_SSL: True,
+    CONF_DAYS: 2,
+}
+
+USER_INPUT_CUSTO = {
+    CONF_USERNAME: "username",
+    CONF_PASSWORD: "password",
+    CONF_URL: "http://homeassistant.io",
+    CONF_VERIFY_SSL: True,
+    CONF_DAYS: 2,
+    CONF_ADD_CUSTO_CALENDAR: True,
+}
+
+CUSTOM_INPUT = {
+    CONF_CUSTOM_CALENDARS: [
+        {CONF_NAME: "name", CONF_CALENDAR: "calendar", CONF_SEARCH: "search"}
+    ]
+}
+
+IMPORT_INPUT = {
+    CONF_USERNAME: "username",
+    CONF_PASSWORD: "password",
+    CONF_URL: "http://homeassistant.io",
+    CONF_VERIFY_SSL: True,
+    CONF_DAYS: 2,
+    CONF_CALENDARS: ["home", "assistant"],
+    CONF_CUSTOM_CALENDARS: [
+        {CONF_NAME: "name", CONF_CALENDAR: "calendar", CONF_SEARCH: "search"},
+        {CONF_NAME: "name_1", CONF_CALENDAR: "calendar_1", CONF_SEARCH: "search_1"},
+    ],
+}
+
+OPTIONS_INPUT = {
+    CONF_CALENDARS: ["home", "assistant"],
+    CONF_CUSTOM_CALENDARS: [
+        {CONF_NAME: "name", CONF_CALENDAR: "calendar", CONF_SEARCH: "search"},
+        {CONF_NAME: "name_1", CONF_CALENDAR: "calendar_1", CONF_SEARCH: "search_1"},
+    ],
+}
+
+
+async def test_user_form(hass):
+    """Test we get the user initiated form."""
+
+    with patch(
+        "homeassistant.components.caldav.config_flow.async_caldav_connect",
+        return_value=[],
+    ):
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={CONF_SOURCE: SOURCE_USER}
+        )
+        assert result["type"] == RESULT_TYPE_FORM
+        assert result["step_id"] == "user"
+        assert result["errors"] == {}
+
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], USER_INPUT
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] == RESULT_TYPE_CREATE_ENTRY
+        assert result["title"] == "caldav"
+
+        assert result["data"]
+        assert result["data"][CONF_URL] == USER_INPUT[CONF_URL]
+        assert result["data"][CONF_USERNAME] == USER_INPUT[CONF_USERNAME]
+        assert result["data"][CONF_PASSWORD] == USER_INPUT[CONF_PASSWORD]
+        assert result["data"][CONF_DAYS] == USER_INPUT[CONF_DAYS]
+        assert result["data"][CONF_VERIFY_SSL] is USER_INPUT[CONF_VERIFY_SSL]
+
+        assert result["result"]
+        assert (
+            result["result"].unique_id
+            == f"{USER_INPUT[CONF_USERNAME]}:{USER_INPUT[CONF_URL]}"
+        )
+
+
+async def test_user_form_custom(hass):
+    """Test we get the user initiated form."""
+
+    with patch(
+        "homeassistant.components.caldav.config_flow.async_caldav_connect",
+        return_value=[],
+    ):
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={CONF_SOURCE: SOURCE_USER}
+        )
+        assert result["type"] == RESULT_TYPE_FORM
+        assert result["step_id"] == "user"
+        assert result["errors"] == {}
+
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], USER_INPUT_CUSTO
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] == RESULT_TYPE_FORM
+        assert result["step_id"] == "custom_calendars"
+        assert result["errors"] is None
+
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], CUSTOM_INPUT
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] == RESULT_TYPE_CREATE_ENTRY
+        assert result["data"]
+        assert result["data"][CONF_URL] == USER_INPUT[CONF_URL]
+        assert result["data"][CONF_USERNAME] == USER_INPUT[CONF_USERNAME]
+        assert result["data"][CONF_PASSWORD] == USER_INPUT[CONF_PASSWORD]
+        assert result["data"][CONF_DAYS] == USER_INPUT[CONF_DAYS]
+        assert result["data"][CONF_VERIFY_SSL] is USER_INPUT[CONF_VERIFY_SSL]
+        assert (
+            result["options"][CONF_CUSTOM_CALENDARS]
+            == CUSTOM_INPUT[CONF_CUSTOM_CALENDARS]
+        )
+
+        assert result["result"]
+        assert (
+            result["result"].unique_id
+            == f"{USER_INPUT[CONF_USERNAME]}:{USER_INPUT[CONF_URL]}"
+        )
+
+
+async def test_abort_on_connection_error(hass: HomeAssistant) -> None:
+    """Test we abort on connection error."""
+
+    with patch(
+        "homeassistant.components.caldav.config_flow.async_caldav_connect",
+        side_effect=DAVError(),
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_USER}
+        )
+
+        assert result["type"] == RESULT_TYPE_FORM
+        assert result["step_id"] == "user"
+        assert result["errors"] == {}
+
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], USER_INPUT
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] == RESULT_TYPE_FORM
+        assert result["errors"] == {"base": "cannot_connect"}
+
+
+async def test_abort_if_already_setup(hass: HomeAssistant):
+    """Test we abort if component is already setup."""
+    MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_URL: "url", CONF_USERNAME: "username"},
+        unique_id="username:url",
+    ).add_to_hass(hass)
+
+    # Should fail, same MOCK_HOST (import)
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_IMPORT},
+        data={CONF_URL: "url", CONF_USERNAME: "username"},
+    )
+    assert result["type"] == RESULT_TYPE_ABORT
+    assert result["reason"] == "already_configured"
+
+    # Should fail, same MOCK_HOST (flow)
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_USER},
+        data={CONF_URL: "url", CONF_USERNAME: "username"},
+    )
+    assert result["type"] == RESULT_TYPE_ABORT
+    assert result["reason"] == "already_configured"
+
+
+async def test_import(hass: HomeAssistant):
+    """Test import step."""
+    with patch(
+        "homeassistant.components.caldav.config_flow.async_caldav_connect",
+        return_value=[],
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": SOURCE_IMPORT},
+            data=IMPORT_INPUT,
+        )
+
+        assert result["type"] == RESULT_TYPE_CREATE_ENTRY
+        assert result["data"]
+        assert result["data"][CONF_URL] == USER_INPUT[CONF_URL]
+        assert result["data"][CONF_USERNAME] == USER_INPUT[CONF_USERNAME]
+        assert result["data"][CONF_PASSWORD] == USER_INPUT[CONF_PASSWORD]
+        assert result["data"][CONF_DAYS] == USER_INPUT[CONF_DAYS]
+        assert result["data"][CONF_VERIFY_SSL] is USER_INPUT[CONF_VERIFY_SSL]
+        assert result["options"] == OPTIONS_INPUT
+        assert (
+            result["result"].unique_id
+            == f"{USER_INPUT[CONF_USERNAME]}:{USER_INPUT[CONF_URL]}"
+        )
