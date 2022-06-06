ef1b713087e0f627d0d61ce7da15cb50982f248a
Glenn Waters <glenn@watrs.ca>
2022-06-05 22:21:37 -0400

    Only serialize Enums

--- homeassistant/components/elkm1/__init__.py
+++ homeassistant/components/elkm1/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import asyncio
+from enum import Enum
 import logging
 import re
 from types import MappingProxyType
@@ -481,8 +482,10 @@ class ElkEntity(Entity):
     @property
     def extra_state_attributes(self) -> dict[str, Any]:
         """Return the default attributes of the element."""
-        strdict = {k: str(v) for k, v in self._element.as_dict().items()}
-        return {**strdict, **self.initial_attrs()}
+        dict_as_str = {}
+        for key, val in self._element.as_dict().items():
+            dict_as_str[key] = val.value if isinstance(val, Enum) else val
+        return {**dict_as_str, **self.initial_attrs()}
 
     @property
     def available(self) -> bool:
