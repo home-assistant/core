90a2c1a9a3d9b08658601e2d9be1438172213391
epenet <6771947+epenet@users.noreply.github.com>
2022-06-07 06:32:52 +0000

    Adjust tests

--- tests/components/awair/test_config_flow.py
+++ tests/components/awair/test_config_flow.py
@@ -8,6 +8,7 @@ from homeassistant import data_entry_flow
 from homeassistant.components.awair.const import DOMAIN
 from homeassistant.config_entries import SOURCE_REAUTH, SOURCE_USER
 from homeassistant.const import CONF_ACCESS_TOKEN
+from homeassistant.core import HomeAssistant
 
 from .const import CONFIG, DEVICES_FIXTURE, NO_DEVICES_FIXTURE, UNIQUE_ID, USER_FIXTURE
 
@@ -82,46 +83,69 @@ async def test_no_devices_error(hass):
         assert result["reason"] == "no_devices_found"
 
 
-async def test_reauth(hass):
+async def test_reauth(hass: HomeAssistant) -> None:
     """Test reauth flow."""
-    with patch(
-        "python_awair.AwairClient.query", side_effect=[USER_FIXTURE, DEVICES_FIXTURE]
-    ), patch(
-        "homeassistant.components.awair.sensor.async_setup_entry",
-        return_value=True,
-    ):
-        mock_config = MockConfigEntry(domain=DOMAIN, unique_id=UNIQUE_ID, data=CONFIG)
-        mock_config.add_to_hass(hass)
-        hass.config_entries.async_update_entry(
-            mock_config, data={**CONFIG, CONF_ACCESS_TOKEN: "blah"}
-        )
+    mock_config = MockConfigEntry(domain=DOMAIN, unique_id=UNIQUE_ID, data=CONFIG)
+    mock_config.add_to_hass(hass)
+    hass.config_entries.async_update_entry(
+        mock_config, data={**CONFIG, CONF_ACCESS_TOKEN: "blah"}
+    )
 
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN,
-            context={"source": SOURCE_REAUTH, "unique_id": UNIQUE_ID},
-            data=CONFIG,
-        )
-
-        assert result["type"] == "abort"
-        assert result["reason"] == "reauth_successful"
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_REAUTH, "unique_id": UNIQUE_ID},
+        data=CONFIG,
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "reauth_confirm"
+    assert result["errors"] == {}
 
     with patch("python_awair.AwairClient.query", side_effect=AuthError()):
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN,
-            context={"source": SOURCE_REAUTH, "unique_id": UNIQUE_ID},
-            data=CONFIG,
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input=CONFIG,
         )
 
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["step_id"] == "reauth_confirm"
         assert result["errors"] == {CONF_ACCESS_TOKEN: "invalid_access_token"}
 
-    with patch("python_awair.AwairClient.query", side_effect=AwairError()):
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN,
-            context={"source": SOURCE_REAUTH, "unique_id": UNIQUE_ID},
-            data=CONFIG,
+    with patch(
+        "python_awair.AwairClient.query", side_effect=[USER_FIXTURE, DEVICES_FIXTURE]
+    ), patch("homeassistant.config_entries.ConfigEntries.async_reload"):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input=CONFIG,
         )
 
-        assert result["type"] == "abort"
+        assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+        assert result["reason"] == "reauth_successful"
+
+
+async def test_reauth_error(hass: HomeAssistant) -> None:
+    """Test reauth flow."""
+    mock_config = MockConfigEntry(domain=DOMAIN, unique_id=UNIQUE_ID, data=CONFIG)
+    mock_config.add_to_hass(hass)
+    hass.config_entries.async_update_entry(
+        mock_config, data={**CONFIG, CONF_ACCESS_TOKEN: "blah"}
+    )
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_REAUTH, "unique_id": UNIQUE_ID},
+        data=CONFIG,
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "reauth_confirm"
+    assert result["errors"] == {}
+
+    with patch("python_awair.AwairClient.query", side_effect=AwairError()):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input=CONFIG,
+        )
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
         assert result["reason"] == "unknown"
 
 
