0e99029da4abcce670b181b1f19c4f9ee0e7ab76
Daniel Schramm <daniel.schramm@gmx.de>
2022-06-07 21:49:33 +0200

    Communication successful

--- homeassistant/components/pylontech_us/sensor.py
+++ homeassistant/components/pylontech_us/sensor.py
@@ -14,8 +14,9 @@ from homeassistant.const import (  # CONF_PORT,; ELECTRIC_CURRENT_AMPERE,; ELECT
     PERCENTAGE,
     POWER_KILO_WATT,
 )
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 
@@ -119,42 +120,75 @@ async def async_setup_entry(
 
     entities: list[SensorEntity] = []
     entities.extend(
-        PylontechStackSensor(hass, desc, config_entry.entry_id)
+        PylontechStackSensor(
+            hass, hass.data[DOMAIN][config_entry.entry_id], desc, config_entry.entry_id
+        )
         for desc in PYLONTECH_STACK_SENSORS
     )
 
     async_add_entities(entities)
 
 
-class PylontechStackSensor(SensorEntity):
+class PylontechStackSensor(CoordinatorEntity, SensorEntity):
     """Representation of an Pylontech sensor."""
 
     def __init__(
-        self, hass: HomeAssistant, desc: PylontechSensorEntityDescription, entry_id: str
+        self,
+        hass: HomeAssistant,
+        coordinator,
+        desc: PylontechSensorEntityDescription,
+        entry_id: str,
     ) -> None:
         """Stack summery value."""
-        self._charge = 54.321
+        super().__init__(coordinator)
+        self._hass = hass
+        self._key = desc.key
+        self._entry_id = entry_id
+        result = self._hass.data[DOMAIN][self._entry_id].get_result()
+        # result = await hass.async_add_executor_job(hub.update)
+        self._attr_native_value = result["Calculated"][self._key]
+
         self._attr_name = desc.name
         self._attr_state_class = desc.state_class
         self._attr_native_unit_of_measurement = desc.native_unit_of_measurement
         self._attr_device_class = desc.device_class
         self._attr_icon = desc.icon
-        self._entry_id = entry_id
-        self._hass = hass
-        self._key = desc.key
+        self._attr_available = True
+        self._attr_should_poll = True
 
     @property
     def unique_id(self) -> str:
         """Device Uniqueid."""
         return "pylontech_stack_" + self._entry_id + "_" + str(self._attr_name)
 
-    @property
-    def native_value(self) -> float:
-        """Get the current value in percentage."""
-        return round(self._charge, 2)
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        # self._attr_is_on = self.coordinator.data[self.idx]["state"]
+        # self.async_write_ha_state()
+        result = self._hass.data[DOMAIN][self._entry_id].get_result()
+        # result = await hass.async_add_executor_job(hub.update)
+        self._attr_native_value = result["Calculated"][self._key]
+
+        # super()._handle_coordinator_update()
+        self._attr_available = True
+        self.async_write_ha_state()
 
     async def async_update(self) -> None:
         """Poll battery stack."""
-        result = self._hass.data[DOMAIN][self._entry_id].get_result()
-        # result = await hass.async_add_executor_job(hub.update)
-        self._charge = result["Calculated"][self._key]
+        await self.coordinator.async_request_refresh()
+
+    @property
+    def entity_registry_enabled_default(self) -> bool:
+        """Return if the entity should be enabled when first added to the entity registry."""
+        # return self._enabled_default
+
+    async def async_added_to_hass(self) -> None:
+        """Run when entity about to be added to hass."""
+        await super().async_added_to_hass()
+        # self._sensor.enabled = True
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Run when entity will be removed from hass."""
+        await super().async_will_remove_from_hass()
+        # self._sensor.enabled = False
