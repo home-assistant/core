abaaadd591ed54c7fe316ef577dd2c397b9c5c6c
Daniel Schramm <daniel.schramm@gmx.de>
2022-06-07 21:49:33 +0200

    Communication successful

--- homeassistant/components/pylontech_us/__init__.py
+++ homeassistant/components/pylontech_us/__init__.py
@@ -4,6 +4,9 @@ from __future__ import annotations
 from datetime import timedelta
 import logging
 
+from aioesphomeapi import Any
+import async_timeout
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -24,22 +27,37 @@ class PylontechCoordinator(DataUpdateCoordinator):
 
     def __init__(self, hass: HomeAssistant, port: str, baud: int) -> None:
         """Pylontech setup."""
-        self._stack = PylontechStack(device=port, baud=baud)
-        self._last_result = self._stack.update()
-        self._hass = hass
-
         super().__init__(
             hass,
             _LOGGER,
             name=DOMAIN,
             update_interval=SCAN_INTERVAL,
         )
+        self._stack = PylontechStack(device=port, baud=baud, manualBattcountLimit=7)
+        self._last_result = self._stack.update()
+        self._hass = hass
 
-    async def _async_update_data(self):
+    def update(self):
+        """Create callable for call from async."""
+        self._stack.update()
+
+    async def _async_update_data(self) -> (Any | None):
         """Fetch data from Battery."""
-        self._last_result = await self._hass.async_add_executor_job(
-            self._stack.update()
-        )
+        self.last_update_success = True
+        # try:
+        # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+        # handled by the data update coordinator.
+        async with async_timeout.timeout(15):
+            await self._hass.async_add_executor_job(self.update)
+            return self._stack.pylonData
+        # except TypeError as err:
+        #    print('TypeError')
+        # except:
+        #    e = sys.exc_info()[0]
+        #    print( "Error: %s" % e )
+        #    print('ignore exception')
+        # raise UpdateFailed(f"Error communicating with API.")
+
         # self._lastResult = await self._stack.update()
 
     def get_result(self):
@@ -47,7 +65,7 @@ class PylontechCoordinator(DataUpdateCoordinator):
         return self._last_result
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up pylontech_rs485 from a config entry."""
     # TOODOO Store an API object for your platforms to access
     # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
@@ -55,16 +73,18 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator = PylontechCoordinator(hass, port="/dev/ttyUSB0", baud=115200)
 
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
 
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
+    if unload_ok := await hass.config_entries.async_unload_platforms(
+        config_entry, PLATFORMS
+    ):
+        hass.data[DOMAIN].pop(config_entry.entry_id)
 
     return unload_ok
