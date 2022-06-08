25ed80b53d4651794f31c2f877fc2cdc9300cae3
Tom Puttemans <github@tomputtemans.com>
2022-06-09 00:09:36 +0200

    Implement async_unload_entry and simplify async_setup_entry

--- homeassistant/components/dsmr_reader/__init__.py
+++ homeassistant/components/dsmr_reader/__init__.py
@@ -9,7 +9,11 @@ PLATFORMS = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up the DSMR Reader integration."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload the DSMR Reader integration."""
+    # no data stored in hass.data
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
