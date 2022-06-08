8449de81b0e7f904ccc066a79179d8aa9bb558cf
Justin Wong <46082645+uvjustin@users.noreply.github.com>
2022-06-08 08:44:59 +0000

    Make convert_stream_options inner function

--- homeassistant/components/stream/__init__.py
+++ homeassistant/components/stream/__init__.py
@@ -106,10 +106,34 @@ def create_stream(
 
     The stream_label is a string used as an additional message in logging.
     """
+
+    def convert_stream_options(
+        hass: HomeAssistant, stream_options: Mapping[str, str | bool | float]
+    ) -> tuple[dict[str, str], StreamSettings]:
+        """Convert options from stream options into PyAV options and stream settings."""
+        stream_settings = copy.copy(hass.data[DOMAIN][ATTR_SETTINGS])
+        pyav_options: dict[str, str] = {}
+        try:
+            STREAM_OPTIONS_SCHEMA(stream_options)
+        except vol.Invalid as exc:
+            raise HomeAssistantError("Invalid stream options") from exc
+
+        if extra_wait_time := stream_options.get(CONF_EXTRA_PART_WAIT_TIME):
+            stream_settings.hls_part_timeout += extra_wait_time
+        if rtsp_transport := stream_options.get(CONF_RTSP_TRANSPORT):
+            assert isinstance(rtsp_transport, str)
+            # The PyAV options currently match the stream CONF constants, but this
+            # will not necessarily always be the case, so they are hard coded here
+            pyav_options["rtsp_transport"] = rtsp_transport
+        if stream_options.get(CONF_USE_WALLCLOCK_AS_TIMESTAMPS):
+            pyav_options["use_wallclock_as_timestamps"] = "1"
+
+        return pyav_options, stream_settings
+
     if DOMAIN not in hass.config.components:
         raise HomeAssistantError("Stream integration is not set up.")
 
-    # Convert extra stream options into PyAV options
+    # Convert extra stream options into PyAV options and stream settings
     pyav_options, stream_settings = convert_stream_options(hass, options)
     # For RTSP streams, prefer TCP
     if isinstance(stream_source, str) and stream_source[:7] == "rtsp://":
@@ -524,25 +548,3 @@ STREAM_OPTIONS_SCHEMA: Final = vol.Schema(
         vol.Optional(CONF_EXTRA_PART_WAIT_TIME): cv.positive_float,
     }
 )
-
-
-def convert_stream_options(
-    hass: HomeAssistant, stream_options: Mapping[str, str | bool | float]
-) -> tuple[dict[str, str], StreamSettings]:
-    """Convert options from stream options into PyAV options."""
-    stream_settings = copy.copy(hass.data[DOMAIN][ATTR_SETTINGS])
-    pyav_options: dict[str, str] = {}
-    try:
-        STREAM_OPTIONS_SCHEMA(stream_options)
-    except vol.Invalid as exc:
-        raise HomeAssistantError("Invalid stream options") from exc
-
-    if extra_wait_time := stream_options.get(CONF_EXTRA_PART_WAIT_TIME):
-        stream_settings.hls_part_timeout += extra_wait_time
-    if rtsp_transport := stream_options.get(CONF_RTSP_TRANSPORT):
-        assert isinstance(rtsp_transport, str)
-        pyav_options["rtsp_transport"] = rtsp_transport
-    if stream_options.get(CONF_USE_WALLCLOCK_AS_TIMESTAMPS):
-        pyav_options["use_wallclock_as_timestamps"] = "1"
-
-    return pyav_options, stream_settings
