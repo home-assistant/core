195de1ebc4ed2df85eb576573b60b1c02e04953d
elBoz73 <26686772+elBoz73@users.noreply.github.com>
2022-06-10 20:20:33 +0200

    add target management for the service call
    This small modification enable to :
    - use "target" attribute to overwrite the "recipient" (phone number) attribute defined in the config number. 
    - send sms to multiple persons (family or admin to receive all sms coming from other phones when HA send a message)
    - use add on such as node red to manage the phone number to which the sms will be send
    
    Rules :
    - if "target" is define (one or many values) it overwrite the one define in the "recipient"
    - if "target" is not define it continue to send sms to the defined "recipient". That mean it should retro compatible with all existing installations without modification
    
    
    Exemples : 
    ::Service call
    service: notify.sms_person1
    data:
      message: "test sms message from service call"
      target:
        - "+33XXXXXXXX"
        - "+33YYYYYYYY"
    
    ::Node red data of a service call
    {
       "message":"Hello I am your house and I received your message :' " &
       payload.event.text & "' le " & payload.event.date & " from number " &
       payload.event.phone,
       "target":["+33XXXXXXX","+33YYYYYYYY"]
    }
    
    I tested it (and it works) with : 
    - Core 2022.6.0
    - Supervisor 2022.05.3
    - OS 8.1
    - Tested using a SIM800C

--- homeassistant/components/sms/notify.py
+++ homeassistant/components/sms/notify.py
@@ -5,7 +5,7 @@ import gammu  # pylint: disable=import-error
 import voluptuous as vol
 
 from homeassistant.components.notify import PLATFORM_SCHEMA, BaseNotificationService
-from homeassistant.const import CONF_NAME, CONF_RECIPIENT
+from homeassistant.const import CONF_NAME, CONF_RECIPIENT, CONF_TARGET
 import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN, SMS_GATEWAY
@@ -44,6 +44,9 @@ class SMSNotificationService(BaseNotificationService):
 
     async def async_send_message(self, message="", **kwargs):
         """Send SMS message."""
+        
+        """ if a number or a list of numbers exist in the CONF_TARGET attribute overwrite the number define in the config."""
+        targets = kwargs.get(CONF_TARGET, [self.number])
         smsinfo = {
             "Class": -1,
             "Unicode": True,
@@ -60,9 +63,12 @@ class SMSNotificationService(BaseNotificationService):
         for encoded_message in encoded:
             # Fill in numbers
             encoded_message["SMSC"] = {"Location": 1}
-            encoded_message["Number"] = self.number
-            try:
-                # Actually send the message
-                await self.gateway.send_sms_async(encoded_message)
-            except gammu.GSMError as exc:
-                _LOGGER.error("Sending to %s failed: %s", self.number, exc)
+            
+            """ send sms to all numbers defined"""
+            for target in targets:
+                encoded_message["Number"] = target            
+                try:
+                    # Actually send the message
+                    await self.gateway.send_sms_async(encoded_message)
+                except gammu.GSMError as exc:
+                    _LOGGER.error("Sending to %s failed: %s", target, exc)
