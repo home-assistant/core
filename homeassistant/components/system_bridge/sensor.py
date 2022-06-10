dfc3434a393a0618c29e8c7f4995a33fdb259380
Aidan Timson <contact@timmo.xyz>
2022-06-10 22:27:54 +0000

    Add missing import

--- homeassistant/components/system_bridge/sensor.py
+++ homeassistant/components/system_bridge/sensor.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from collections.abc import Callable
 from dataclasses import dataclass
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Final, cast
 
 from homeassistant.components.sensor import (
