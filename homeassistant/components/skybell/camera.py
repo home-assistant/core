8eda00ab00339d6aeed2991e450943f94cb4b05f
Robert Hillis <tkdrob4390@yahoo.com>
2022-06-11 16:38:01 -0400

    Add support for playing latest activity video

--- homeassistant/components/skybell/camera.py
+++ homeassistant/components/skybell/camera.py
@@ -1,6 +1,8 @@
 """Camera support for the Skybell HD Doorbell."""
 from __future__ import annotations
 
+from aiohttp import web
+from haffmpeg.camera import CameraMjpeg
 import voluptuous as vol
 
 from homeassistant.components.camera import (
@@ -8,9 +10,11 @@ from homeassistant.components.camera import (
     Camera,
     CameraEntityDescription,
 )
+from homeassistant.components.ffmpeg import DATA_FFMPEG
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_MONITORED_CONDITIONS
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_aiohttp_proxy_stream
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -21,6 +25,7 @@ from .const import (
     DOMAIN,
     IMAGE_ACTIVITY,
     IMAGE_AVATAR,
+    LOGGER,
 )
 from .coordinator import SkybellDataUpdateCoordinator
 from .entity import SkybellEntity
@@ -46,11 +51,14 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up Skybell switch."""
-    async_add_entities(
-        SkybellCamera(coordinator, description)
-        for description in CAMERA_TYPES
-        for coordinator in hass.data[DOMAIN][entry.entry_id]
-    )
+    entities = []
+    for description in CAMERA_TYPES:
+        for coordinator in hass.data[DOMAIN][entry.entry_id]:
+            if description.key == "avatar":
+                entities.append(SkybellCamera(coordinator, description))
+            else:
+                entities.append(SkybellActivityCamera(coordinator, description))
+    async_add_entities(entities)
 
 
 class SkybellCamera(SkybellEntity, Camera):
@@ -70,3 +78,26 @@ class SkybellCamera(SkybellEntity, Camera):
     ) -> bytes | None:
         """Get the latest camera image."""
         return self._device.images[self.entity_description.key]
+
+
+class SkybellActivityCamera(SkybellCamera):
+    """A camera implementation for latest Skybell activity."""
+
+    async def handle_async_mjpeg_stream(
+        self, request: web.Request
+    ) -> web.StreamResponse | None:
+        """Generate an HTTP MJPEG stream from the latest recorded activity."""
+        stream = CameraMjpeg(self.hass.data[DATA_FFMPEG].binary)
+        url = await self.coordinator.device.async_get_activity_video_url()
+        await stream.open_camera(url, extra_cmd="-r 210")
+
+        try:
+            return await async_aiohttp_proxy_stream(
+                self.hass,
+                request,
+                await stream.get_reader(),
+                self.hass.data[DATA_FFMPEG].ffmpeg_stream_content_type,
+            )
+        except Exception as ex:  # pylint: disable=broad-except
+            LOGGER.error("Error occurred while playing stream: %s", ex)
+            return None
