f3ed8743013b70c6e5a03a130d426fd572ca8180
kingy444 <toddlesking4@hotmail.com>
2022-06-11 13:55:53 +0000

    Pause polling for hub maintenance

--- homeassistant/components/hunterdouglas_powerview/coordinator.py
+++ homeassistant/components/hunterdouglas_powerview/coordinator.py
@@ -16,6 +16,10 @@ from .shade_data import PowerviewShadeData
 _LOGGER = logging.getLogger(__name__)
 
 
+UPDATE_INTERVAL_DEFAULT = timedelta(seconds=60)
+UPDATE_INTERVAL_MAINTENANCE = timedelta(minutes=5)
+
+
 class PowerviewShadeUpdateCoordinator(DataUpdateCoordinator[PowerviewShadeData]):
     """DataUpdateCoordinator to gather data from a powerview hub."""
 
@@ -27,18 +31,39 @@ class PowerviewShadeUpdateCoordinator(DataUpdateCoordinator[PowerviewShadeData])
     ) -> None:
         """Initialize DataUpdateCoordinator to gather data for specific SmartPlug."""
         self.shades = shades
+        self.maintenance = False
         super().__init__(
             hass,
             _LOGGER,
             name=f"powerview hub {hub_address}",
-            update_interval=timedelta(seconds=60),
+            update_interval=UPDATE_INTERVAL_DEFAULT,
         )
 
     async def _async_update_data(self) -> PowerviewShadeData:
         """Fetch data from shade endpoint."""
+
+        if self.maintenance is True:
+            self.update_interval = UPDATE_INTERVAL_DEFAULT
+            self.maintenance = False
+            _LOGGER.debug("Polling returned to %s", UPDATE_INTERVAL_DEFAULT)
+
         async with async_timeout.timeout(10):
             shade_entries = await self.shades.get_resources()
-        if not shade_entries:
+
+        if isinstance(shade_entries, bool):
+            # hub returns boolean on a 204/423 empty response (maintenance)
+            # continual polling results in inevitable error
+            # restart of hub takes between 3-5 minutes and generally between 12am-3am
+            _LOGGER.debug(
+                "Hub maintenance underway. Pausing polling for %s",
+                UPDATE_INTERVAL_MAINTENANCE,
+            )
+            self.update_interval = UPDATE_INTERVAL_MAINTENANCE
+            self.maintenance = True
+        elif not shade_entries:
             raise UpdateFailed("Failed to fetch new shade data")
-        self.data.store_group_data(shade_entries[SHADE_DATA])
+        else:
+            # only update if shade_entries is valid
+            self.data.store_group_data(shade_entries[SHADE_DATA])
+
         return self.data
