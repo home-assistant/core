3e5c7e0588bf65b61b5bd273a187a58d4602915b
Roman Appenzeller <rappenze@yahoo.com>
2022-06-12 07:47:52 +0000

    Code cleanup fibaro sensor

--- homeassistant/components/fibaro/sensor.py
+++ homeassistant/components/fibaro/sensor.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from contextlib import suppress
+from typing import Any
 
 from homeassistant.components.sensor import (
     ENTITY_ID_FORMAT,
@@ -95,56 +96,34 @@ async def async_setup_entry(
 class FibaroSensor(FibaroDevice, SensorEntity):
     """Representation of a Fibaro Sensor."""
 
-    def __init__(self, fibaro_device):
+    def __init__(self, fibaro_device: Any) -> None:
         """Initialize the sensor."""
-        self.current_value = None
-        self.last_changed_time = None
         super().__init__(fibaro_device)
         self.entity_id = ENTITY_ID_FORMAT.format(self.ha_id)
+
+        self._attr_native_unit_of_measurement = None
         if fibaro_device.type in SENSOR_TYPES:
-            self._unit = SENSOR_TYPES[fibaro_device.type][1]
-            self._icon = SENSOR_TYPES[fibaro_device.type][2]
-            self._device_class = SENSOR_TYPES[fibaro_device.type][3]
+            self._attr_native_unit_of_measurement = SENSOR_TYPES[fibaro_device.type][1]
+            self._attr_icon = SENSOR_TYPES[fibaro_device.type][2]
+            self._attr_device_class = SENSOR_TYPES[fibaro_device.type][3]
             self._attr_state_class = SENSOR_TYPES[fibaro_device.type][4]
-        else:
-            self._unit = None
-            self._icon = None
-            self._device_class = None
+
         with suppress(KeyError, ValueError):
-            if not self._unit:
-                if self.fibaro_device.properties.unit == "lux":
-                    self._unit = LIGHT_LUX
-                elif self.fibaro_device.properties.unit == "C":
-                    self._unit = TEMP_CELSIUS
-                elif self.fibaro_device.properties.unit == "F":
-                    self._unit = TEMP_FAHRENHEIT
+            if not self._attr_native_unit_of_measurement:
+                unit = self.fibaro_device.properties.unit
+                if unit == "lux":
+                    self._attr_native_unit_of_measurement = LIGHT_LUX
+                elif unit == "C":
+                    self._attr_native_unit_of_measurement = TEMP_CELSIUS
+                elif unit == "F":
+                    self._attr_native_unit_of_measurement = TEMP_FAHRENHEIT
                 else:
-                    self._unit = self.fibaro_device.properties.unit
+                    self._attr_native_unit_of_measurement = unit
 
-    @property
-    def native_value(self):
-        """Return the state of the sensor."""
-        return self.current_value
-
-    @property
-    def native_unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return self._unit
-
-    @property
-    def icon(self):
-        """Icon to use in the frontend, if any."""
-        return self._icon
-
-    @property
-    def device_class(self):
-        """Return the device class of the sensor."""
-        return self._device_class
-
-    def update(self):
+    def update(self) -> None:
         """Update the state."""
         with suppress(KeyError, ValueError):
-            self.current_value = float(self.fibaro_device.properties.value)
+            self._attr_native_value = float(self.fibaro_device.properties.value)
 
 
 class FibaroEnergySensor(FibaroDevice, SensorEntity):
@@ -154,14 +133,14 @@ class FibaroEnergySensor(FibaroDevice, SensorEntity):
     _attr_state_class = SensorStateClass.TOTAL_INCREASING
     _attr_native_unit_of_measurement = ENERGY_KILO_WATT_HOUR
 
-    def __init__(self, fibaro_device):
+    def __init__(self, fibaro_device: Any) -> None:
         """Initialize the sensor."""
         super().__init__(fibaro_device)
         self.entity_id = ENTITY_ID_FORMAT.format(f"{self.ha_id}_energy")
         self._attr_name = f"{fibaro_device.friendly_name} Energy"
         self._attr_unique_id = f"{fibaro_device.unique_id_str}_energy"
 
-    def update(self):
+    def update(self) -> None:
         """Update the state."""
         with suppress(KeyError, ValueError):
             self._attr_native_value = convert(
@@ -176,14 +155,14 @@ class FibaroPowerSensor(FibaroDevice, SensorEntity):
     _attr_state_class = SensorStateClass.MEASUREMENT
     _attr_native_unit_of_measurement = POWER_WATT
 
-    def __init__(self, fibaro_device):
+    def __init__(self, fibaro_device: Any) -> None:
         """Initialize the sensor."""
         super().__init__(fibaro_device)
         self.entity_id = ENTITY_ID_FORMAT.format(f"{self.ha_id}_power")
         self._attr_name = f"{fibaro_device.friendly_name} Power"
         self._attr_unique_id = f"{fibaro_device.unique_id_str}_power"
 
-    def update(self):
+    def update(self) -> None:
         """Update the state."""
         with suppress(KeyError, ValueError):
             self._attr_native_value = convert(
