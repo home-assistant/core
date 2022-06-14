c6ba212747c677897cec66e0102aa0f30195243b
Tobias Sauerwein <cgtobi@gmail.com>
2022-06-14 12:35:20 +0000

    Address review comment

--- homeassistant/components/netatmo/netatmo_entity_base.py
+++ homeassistant/components/netatmo/netatmo_entity_base.py
@@ -8,6 +8,7 @@ from pyatmo.modules.device_types import (
 
 from homeassistant.const import ATTR_ATTRIBUTION
 from homeassistant.core import callback
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.entity import DeviceInfo, Entity
 
 from .const import DATA_DEVICE_IDS, DEFAULT_ATTRIBUTION, DOMAIN, SIGNAL_NAME
@@ -63,9 +64,9 @@ class NetatmoBase(Entity):
                 if sub is None:
                     await self.data_handler.unsubscribe(signal_name, None)
 
-        registry = await self.hass.helpers.device_registry.async_get_registry()
-        device = registry.async_get_device({(DOMAIN, self._id)})
-        self.hass.data[DOMAIN][DATA_DEVICE_IDS][self._id] = device.id
+        registry = dr.async_get(self.hass)
+        if device := registry.async_get_device({(DOMAIN, self._id)}):
+            self.hass.data[DOMAIN][DATA_DEVICE_IDS][self._id] = device.id
 
         self.async_update_callback()
 
