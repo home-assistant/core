7d5c0dfed9f86688f86e454a14eb57b643f2a2ce
Allen Porter <allen.porter@gmail.com>
2022-06-15 07:15:53 -0700

    Add application credentials platform for nest and deprecate yaml for SDM API (#73050)
    * Update the nest integration to be useable fully from the config flow
    
    * Support discovery in nest config flow
    
    * Remove configuration entries
    
    * Remove unused import
    
    * Remove dead code
    
    * Update homeassistant/components/nest/strings.json
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * Remove commented out code
    
    * Use config flow for app auth reauthentication path
    
    * Improves for re-auth for upgrading existing project and creds
    
    * More dead code removal
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * Remove outdated code
    
    * Update homeassistant/components/nest/config_flow.py
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/nest/conftest.py
+++ tests/components/nest/conftest.py
@@ -14,6 +14,9 @@ from google_nest_sdm.auth import AbstractAuth
 from google_nest_sdm.device_manager import DeviceManager
 import pytest
 
+from homeassistant.components.application_credentials import (
+    async_import_client_credential,
+)
 from homeassistant.components.nest import DOMAIN
 from homeassistant.components.nest.const import CONF_SUBSCRIBER_ID
 from homeassistant.core import HomeAssistant
@@ -22,9 +25,8 @@ from homeassistant.setup import async_setup_component
 from .common import (
     DEVICE_ID,
     SUBSCRIBER_ID,
-    TEST_CONFIG_HYBRID,
+    TEST_CONFIG_APP_CREDS,
     TEST_CONFIG_YAML_ONLY,
-    WEB_AUTH_DOMAIN,
     CreateDevice,
     FakeSubscriber,
     NestTestConfig,
@@ -183,14 +185,14 @@ def subscriber_id() -> str:
 
 
 @pytest.fixture
-def auth_implementation() -> str | None:
+def auth_implementation(nest_test_config: NestTestConfig) -> str | None:
     """Fixture to let tests override the auth implementation in the config entry."""
-    return WEB_AUTH_DOMAIN
+    return nest_test_config.auth_implementation
 
 
 @pytest.fixture(
-    params=[TEST_CONFIG_YAML_ONLY, TEST_CONFIG_HYBRID],
-    ids=["yaml-config-only", "hybrid-config"],
+    params=[TEST_CONFIG_YAML_ONLY, TEST_CONFIG_APP_CREDS],
+    ids=["yaml-config-only", "app-creds"],
 )
 def nest_test_config(request) -> NestTestConfig:
     """Fixture that sets up the configuration used for the test."""
@@ -230,6 +232,20 @@ def config_entry(
     return MockConfigEntry(domain=DOMAIN, data=data)
 
 
+@pytest.fixture(autouse=True)
+async def credential(hass: HomeAssistant, nest_test_config: NestTestConfig) -> None:
+    """Fixture that provides the ClientCredential for the test if any."""
+    if not nest_test_config.credential:
+        return
+    assert await async_setup_component(hass, "application_credentials", {})
+    await async_import_client_credential(
+        hass,
+        DOMAIN,
+        nest_test_config.credential,
+        nest_test_config.auth_implementation,
+    )
+
+
 @pytest.fixture
 async def setup_base_platform(
     hass: HomeAssistant,
@@ -240,9 +256,7 @@ async def setup_base_platform(
     """Fixture to setup the integration platform."""
     if config_entry:
         config_entry.add_to_hass(hass)
-    with patch(
-        "homeassistant.helpers.config_entry_oauth2_flow.async_get_config_entry_implementation"
-    ), patch("homeassistant.components.nest.PLATFORMS", platforms):
+    with patch("homeassistant.components.nest.PLATFORMS", platforms):
 
         async def _setup_func() -> bool:
             assert await async_setup_component(hass, DOMAIN, config)
