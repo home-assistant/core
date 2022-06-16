1d6815f5756c0044e5d07085a2d442abadaca31a
Corbeno <corbin@kemsfamily.us>
2022-06-16 05:01:52 +0000

    basic start, need to wait forother PR first

---
+++ homeassistant/components/proxmoxve/sensor.py
@@ -0,0 +1,24 @@
+"""Sensor to read Proxmox VE data."""
+
+# from homeassistant.components.proxmoxve import ProxmoxEntity
+# from homeassistant.components.sensor import SensorEntity
+# from homeassistant.core import HomeAssistant
+# from homeassistant.helpers.entity_platform import AddEntitiesCallback
+# from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+
+# async def async_setup_platform(
+#     hass: HomeAssistant,
+#     config: ConfigType,
+#     add_entities: AddEntitiesCallback,
+#     discovery_info: DiscoveryInfoType | None = None,
+# ) -> None:
+#     """Set up sensors."""
+
+
+# class ProxmoxSensor(ProxmoxEntity, SensorEntity):
+#     """A sensor for reading Proxmox VE data."""
+
+#     def __init__():
+#         """Create the sensor for vms or containers."""
+#         super().__init__()
