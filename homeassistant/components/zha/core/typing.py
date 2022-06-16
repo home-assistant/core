6be3aea07b9b20a27e7905cacbafa8a5e152a50b
epenet <6771947+epenet@users.noreply.github.com>
2022-06-16 15:16:08 +0000

    Remove from typing

--- homeassistant/components/zha/core/typing.py
+++ homeassistant/components/zha/core/typing.py
@@ -10,12 +10,10 @@ import zigpy.zdo
 
 # pylint: disable=invalid-name
 CALLABLE_T = TypeVar("CALLABLE_T", bound=Callable)
-ChannelType = "ZigbeeChannel"
 ChannelsType = "Channels"
 ChannelPoolType = "ChannelPool"
 ClientChannelType = "ClientChannel"
 ZDOChannelType = "ZDOChannel"
-ZhaDeviceType = "ZHADevice"
 ZhaEntityType = "ZHAEntity"
 ZhaGatewayType = "ZHAGateway"
 ZhaGroupType = "ZHAGroupType"
@@ -28,15 +26,13 @@ ZigpyZdoType = zigpy.zdo.ZDO
 if TYPE_CHECKING:
     import homeassistant.components.zha.entity
 
-    from . import channels, device, gateway, group
+    from . import channels, gateway, group
     from .channels import base as base_channels
 
-    ChannelType = base_channels.ZigbeeChannel
     ChannelsType = channels.Channels
     ChannelPoolType = channels.ChannelPool
     ClientChannelType = base_channels.ClientChannel
     ZDOChannelType = base_channels.ZDOChannel
-    ZhaDeviceType = device.ZHADevice
     ZhaEntityType = homeassistant.components.zha.entity.ZhaEntity
     ZhaGatewayType = gateway.ZHAGateway
     ZhaGroupType = group.ZHAGroup
