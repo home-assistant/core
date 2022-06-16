79d5a127dd3215b343dd311cd41e520e026dc95b
J. Nick Koston <nick@koston.org>
2022-06-16 13:02:58 -1000

    put back the constant

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -144,7 +144,8 @@ class ConfigEntryDisabler(StrEnum):
 # DISABLED_* is deprecated, to be removed in 2022.3
 DISABLED_USER = ConfigEntryDisabler.USER.value
 
-RELOAD_COOLDOWN = timedelta(seconds=30)
+RELOAD_AFTER_UPDATE_DELAY = 30
+RELOAD_COOLDOWN = timedelta(seconds=RELOAD_AFTER_UPDATE_DELAY)
 
 # Deprecated: Connection classes
 # These aren't used anymore since 2021.6.0
