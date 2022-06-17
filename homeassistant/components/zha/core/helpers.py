023eb129eae4e66b92a7104626f965e89b972b41
epenet <6771947+epenet@users.noreply.github.com>
2022-06-17 06:58:22 +0000

    Adjust helpers.py

--- homeassistant/components/zha/core/helpers.py
+++ homeassistant/components/zha/core/helpers.py
@@ -172,6 +172,7 @@ def async_get_zha_device(hass: HomeAssistant, device_id: str) -> ZHADevice:
     if not registry_device:
         raise ValueError(f"Device id `{device_id}` not found in registry.")
     zha_gateway: ZHAGateway = hass.data[DATA_ZHA][DATA_ZHA_GATEWAY]
+    assert registry_device
     ieee_address = list(list(registry_device.identifiers)[0])[1]
     ieee = zigpy.types.EUI64.convert(ieee_address)
     return zha_gateway.devices[ieee]
