ed0c1b40ec531f52bea8755203ec231201297156
Phil Bruckner <pnbruckner@gmail.com>
2022-06-17 14:33:27 -0500

    Update tests per review changes

--- tests/components/life360/test_config_flow.py
+++ tests/components/life360/test_config_flow.py
@@ -1,6 +1,6 @@
 """Test the Life360 config flow."""
 
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 import pytest
 import voluptuous as vol
@@ -10,13 +10,11 @@ from homeassistant.components.life360.const import (
     CONF_AUTHORIZATION,
     CONF_DRIVING_SPEED,
     CONF_MAX_GPS_ACCURACY,
-    CONF_PREFIX,
     CONF_SCAN_INTERVAL,
     DEFAULT_SCAN_INTERVAL_SEC,
     DOMAIN,
     SHOW_DRIVING,
 )
-from homeassistant.components.life360.helpers import init_integ_data
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 
 from tests.common import MockConfigEntry
@@ -28,7 +26,6 @@ TEST_PW_3 = "password_3"
 TEST_AUTHORIZATION = "authorization_string"
 TEST_AUTHORIZATION_2 = "authorization_string_2"
 TEST_AUTHORIZATION_3 = "authorization_string_3"
-TEST_PREFIX = "life360"
 TEST_MAX_GPS_ACCURACY = "300"
 TEST_DRIVING_SPEED = "18"
 TEST_SCAN_INTERVAL = "10"
@@ -51,11 +48,14 @@ TEST_CONFIG_DATA_3 = {
     CONF_PASSWORD: TEST_PW_3,
     CONF_AUTHORIZATION: TEST_AUTHORIZATION_3,
 }
-TEST_DEF_OPTIONS = {CONF_SCAN_INTERVAL: DEFAULT_SCAN_INTERVAL_SEC}
+TEST_DEF_OPTIONS = {
+    CONF_MAX_GPS_ACCURACY: None,
+    CONF_DRIVING_SPEED: None,
+    CONF_SCAN_INTERVAL: DEFAULT_SCAN_INTERVAL_SEC,
+    SHOW_DRIVING: False,
+}
 
 USER_OPTIONS = {
-    "use_prefix": True,
-    CONF_PREFIX: TEST_PREFIX,
     "limit_gps_acc": True,
     CONF_MAX_GPS_ACCURACY: TEST_MAX_GPS_ACCURACY,
     "set_drive_speed": True,
@@ -64,14 +64,11 @@ USER_OPTIONS = {
     SHOW_DRIVING: TEST_SHOW_DRIVING,
 }
 TEST_OPTIONS = {
-    CONF_PREFIX: TEST_PREFIX,
     CONF_MAX_GPS_ACCURACY: float(TEST_MAX_GPS_ACCURACY),
     CONF_DRIVING_SPEED: float(TEST_DRIVING_SPEED),
     CONF_SCAN_INTERVAL: float(TEST_SCAN_INTERVAL),
     SHOW_DRIVING: TEST_SHOW_DRIVING,
 }
-
-USER_INPUT_AND_OPTIONS = USER_INPUT | USER_OPTIONS
 
 
 @pytest.fixture(name="life360", autouse=True)
@@ -100,14 +97,13 @@ def _authorization_login_error(hass, api, username, password, errors):
     return None
 
 
-def _test_config_entry(hass, state=None, options=None):
-    options = options or TEST_DEF_OPTIONS
+def _test_config_entry(hass, state=None):
     config_entry = MockConfigEntry(
         domain=DOMAIN,
         data=TEST_CONFIG_DATA,
-        version=2,
+        version=1,
         state=state,
-        options=options,
+        options=TEST_DEF_OPTIONS,
         unique_id=TEST_USER.lower(),
     )
     config_entry.add_to_hass(hass)
@@ -119,15 +115,15 @@ async def test_show_form(hass):
     result = await _start_flow(hass)
 
     schema = result["data_schema"].schema
-    assert set(schema.keys()) == set(USER_INPUT_AND_OPTIONS.keys())
+    assert set(schema.keys()) == set(USER_INPUT.keys())
     # username and password fields should be empty.
     keys = list(schema.keys())
     for key in USER_INPUT:
         assert keys[keys.index(key)].default == vol.UNDEFINED
 
 
-async def test_config_flow_success_defaults(hass):
-    """Test a successful config flow with default options."""
+async def test_config_flow_success(hass):
+    """Test a successful config flow."""
     result = await _start_flow(hass)
     flow_id = result["flow_id"]
 
@@ -141,28 +137,6 @@ async def test_config_flow_success_defaults(hass):
     assert result["title"] == TEST_USER.lower()
     assert result["data"] == TEST_CONFIG_DATA
     assert result["options"] == TEST_DEF_OPTIONS
-
-    assert DOMAIN in hass.data
-    assert TEST_USER.lower() in hass.data[DOMAIN]["accounts"]
-
-
-async def test_config_flow_success_all_options(hass):
-    """Test a successful config flow with all options entered by user."""
-    result = await _start_flow(hass)
-    flow_id = result["flow_id"]
-
-    with patch(
-        "homeassistant.components.life360.config_flow.get_life360_authorization",
-        return_value=TEST_AUTHORIZATION,
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            flow_id, USER_INPUT_AND_OPTIONS
-        )
-
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == TEST_USER.lower()
-    assert result["data"] == TEST_CONFIG_DATA
-    assert result["options"] == TEST_OPTIONS
 
 
 async def test_config_flow_login_error(hass):
@@ -181,7 +155,7 @@ async def test_config_flow_login_error(hass):
     assert result["errors"]
 
     schema = result["data_schema"].schema
-    assert set(schema.keys()) == set(USER_INPUT_AND_OPTIONS.keys())
+    assert set(schema.keys()) == set(USER_INPUT.keys())
     # username and password fields should be prefilled with current values.
     keys = list(schema.keys())
     for key, val in USER_INPUT.items():
@@ -208,15 +182,7 @@ async def test_config_flow_already_configured(hass):
 
 
 def _start_reauth_flow(hass, state=None):
-    config_entry = _test_config_entry(hass, state=state)
-
-    init_integ_data(hass)
-    hass.data[DOMAIN]["accounts"][config_entry.unique_id] = {
-        "api": MagicMock(),
-        "coordinator": MagicMock(),
-    }
-
-    return config_entry
+    return _test_config_entry(hass, state=state)
 
 
 async def test_config_flow_reauth_success_unloaded(hass):
@@ -304,13 +270,11 @@ async def test_config_flow_reauth_login_error(hass):
     assert result["step_id"] == "reauth_confirm"
     assert result["errors"]
 
-    # First form opened should NOT show current password; i.e., password entry in schema
-    # should have vol.UNDEFINED as default value.
     schema = result["data_schema"].schema
     assert len(schema) == 1
     assert "password" in schema
     key = list(schema.keys())[0]
-    assert key.default == vol.UNDEFINED
+    assert key.default() == TEST_PW
 
     flow_id = result["flow_id"]
 
@@ -328,14 +292,10 @@ async def test_config_flow_reauth_login_error(hass):
     assert result["step_id"] == "reauth_confirm"
     assert result["errors"]
 
-    # This form opened SHOULD show current password; i.e., password entry in schema
-    # should have a default value other than vol.UNDEFINED. In fact, it should be a
-    # function that returns the current password.
     schema = result["data_schema"].schema
     assert len(schema) == 1
     assert "password" in schema
     key = list(schema.keys())[0]
-    assert key.default != vol.UNDEFINED
     assert key.default() == TEST_PW_2
 
     # Simulate getting a new, valid password.
@@ -355,12 +315,9 @@ async def test_config_flow_reauth_login_error(hass):
     assert config_entry.data == TEST_CONFIG_DATA_3
 
 
-async def test_options_flow_all(hass):
-    """Test an options flow changing all options."""
+async def test_options_flow(hass):
+    """Test an options flow."""
     config_entry = _test_config_entry(hass)
-
-    init_integ_data(hass)
-    hass.data[DOMAIN]["accounts"][config_entry.unique_id] = {}
 
     result = await hass.config_entries.options.async_init(config_entry.entry_id)
 
@@ -378,35 +335,4 @@ async def test_options_flow_all(hass):
     assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result["data"] == TEST_OPTIONS
 
-    assert hass.data[DOMAIN]["accounts"][config_entry.unique_id]["re_add_entry"]
     assert config_entry.options == TEST_OPTIONS
-
-
-async def test_options_flow_fixed_prefix(hass):
-    """Test an options flow where prefix does not change."""
-    config_entry = _test_config_entry(hass, options=TEST_OPTIONS)
-
-    init_integ_data(hass)
-    hass.data[DOMAIN]["accounts"][config_entry.unique_id] = {}
-
-    result = await hass.config_entries.options.async_init(config_entry.entry_id)
-
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
-    assert result["step_id"] == "init"
-    assert not result["errors"]
-
-    schema = result["data_schema"].schema
-    assert set(schema.keys()) == set(USER_OPTIONS.keys())
-
-    flow_id = result["flow_id"]
-
-    user_options = USER_OPTIONS | {SHOW_DRIVING: not TEST_SHOW_DRIVING}
-    test_options = TEST_OPTIONS | {SHOW_DRIVING: not TEST_SHOW_DRIVING}
-
-    result = await hass.config_entries.options.async_configure(flow_id, user_options)
-
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["data"] == test_options
-
-    assert not hass.data[DOMAIN]["accounts"][config_entry.unique_id]["re_add_entry"]
-    assert config_entry.options == test_options
