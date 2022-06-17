9281c4f75314bafc2cc36c585b4f2b3709a1b16a
epenet <6771947+epenet@users.noreply.github.com>
2022-06-17 06:16:20 +0000

    Finish typing cleanup

--- homeassistant/components/zha/core/typing.py
+++ homeassistant/components/zha/core/typing.py
@@ -1,6 +1,6 @@
 """Typing helpers for ZHA component."""
 from collections.abc import Callable
-from typing import TYPE_CHECKING, TypeVar
+from typing import TypeVar
 
 import zigpy.device
 import zigpy.endpoint
@@ -10,23 +10,8 @@ import zigpy.zdo
 
 # pylint: disable=invalid-name
 CALLABLE_T = TypeVar("CALLABLE_T", bound=Callable)
-ClientChannelType = "ClientChannel"
-ZDOChannelType = "ZDOChannel"
-ZhaEntityType = "ZHAEntity"
-ZhaGroupType = "ZHAGroupType"
 ZigpyClusterType = zigpy.zcl.Cluster
 ZigpyDeviceType = zigpy.device.Device
 ZigpyEndpointType = zigpy.endpoint.Endpoint
 ZigpyGroupType = zigpy.group.Group
 ZigpyZdoType = zigpy.zdo.ZDO
-
-if TYPE_CHECKING:
-    import homeassistant.components.zha.entity
-
-    from . import group
-    from .channels import base as base_channels
-
-    ClientChannelType = base_channels.ClientChannel
-    ZDOChannelType = base_channels.ZDOChannel
-    ZhaEntityType = homeassistant.components.zha.entity.ZhaEntity
-    ZhaGroupType = group.ZHAGroup
