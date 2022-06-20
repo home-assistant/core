08382204a348453cec7630fe144e947ecf8f1b26
Erik Montnemery <erik@montnemery.com>
2022-06-20 10:26:50 +0200

    Don't attempt to reload MQTT device tracker (#73577)

--- homeassistant/components/mqtt/__init__.py
+++ homeassistant/components/mqtt/__init__.py
@@ -76,6 +76,7 @@ from .const import (  # noqa: F401
     MQTT_DISCONNECTED,
     MQTT_RELOADED,
     PLATFORMS,
+    RELOADABLE_PLATFORMS,
 )
 from .models import (  # noqa: F401
     MqttCommandTemplate,
@@ -378,7 +379,7 @@ async def async_setup_entry(  # noqa: C901
     # Setup reload service. Once support for legacy config is removed in 2022.9, we
     # should no longer call async_setup_reload_service but instead implement a custom
     # service
-    await async_setup_reload_service(hass, DOMAIN, PLATFORMS)
+    await async_setup_reload_service(hass, DOMAIN, RELOADABLE_PLATFORMS)
 
     async def _async_reload_platforms(_: Event | None) -> None:
         """Discover entities for a platform."""
