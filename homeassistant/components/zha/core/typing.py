3d819f95a2a810d2e38213eb29b74fe8be3d472d
epenet <6771947+epenet@users.noreply.github.com>
2022-06-20 10:00:37 +0000

    Cleanup ZigpyEndpointType

--- homeassistant/components/zha/core/typing.py
+++ homeassistant/components/zha/core/typing.py
@@ -2,12 +2,10 @@
 from collections.abc import Callable
 from typing import TypeVar
 
-import zigpy.endpoint
 import zigpy.group
 import zigpy.zdo
 
 # pylint: disable=invalid-name
 CALLABLE_T = TypeVar("CALLABLE_T", bound=Callable)
-ZigpyEndpointType = zigpy.endpoint.Endpoint
 ZigpyGroupType = zigpy.group.Group
 ZigpyZdoType = zigpy.zdo.ZDO
