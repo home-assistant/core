d1d1aedb087d586f97d4e1b47ccc6a49e6c1bc99
J. Nick Koston <nick@koston.org>
2022-06-21 17:54:57 -0500

    few more

--- homeassistant/components/xiaomi_miio/__init__.py
+++ homeassistant/components/xiaomi_miio/__init__.py
@@ -436,10 +436,7 @@ async def async_setup_gateway_entry(hass: HomeAssistant, entry: ConfigEntry) -> 
         KEY_COORDINATOR: coordinator_dict,
     }
 
-    for platform in GATEWAY_PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, GATEWAY_PLATFORMS)
 
 
 async def async_setup_device_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
