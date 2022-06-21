df5a754b1ea9d5b2e1b27858829c2e2a5ba2e856
Joakim Plate <elupus@ecce.se>
2022-06-21 23:25:30 +0200

    Cleanup entity registry on load

--- homeassistant/components/broadlink/button.py
+++ homeassistant/components/broadlink/button.py
@@ -6,8 +6,12 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity_registry import (
+    async_entries_for_config_entry,
+    async_get as async_get_registry,
+)
 
-from .const import DOMAIN
+from .const import DOMAIN, Platform
 from .device import BroadlinkDevice
 from .entity import BroadlinkEntity
 
@@ -20,14 +24,27 @@ async def async_setup_entry(
     """Set up the Broadlink light."""
     device: BroadlinkDevice = hass.data[DOMAIN].devices[config_entry.entry_id]
     store = device.store
+    entity_registry = async_get_registry(hass)
 
-    entities = [
-        BroadlinkButton(device, subdevice, command)
-        for subdevice, commands in store.extract_devices_and_commands().items()
-        for command in commands
-    ]
+    entity_ids = {
+        entry.unique_id: entry.entity_id
+        for entry in async_entries_for_config_entry(
+            entity_registry, config_entry.entry_id
+        )
+        if entry.domain == Platform.BUTTON
+    }
+
+    entities: list[BroadlinkButton] = []
+    for subdevice, commands in store.extract_devices_and_commands().items():
+        for command in commands:
+            unique_id = f"{device.unique_id}-codes-{subdevice}-{command}"
+            entities.append(BroadlinkButton(device, subdevice, command, unique_id))
+            entity_ids.pop(unique_id, None)
 
     async_add_entities(entities)
+
+    for entity_id in entity_ids.values():
+        entity_registry.async_remove(entity_id)
 
 
 class BroadlinkButton(BroadlinkEntity, ButtonEntity):
@@ -35,11 +52,13 @@ class BroadlinkButton(BroadlinkEntity, ButtonEntity):
 
     _device: BroadlinkDevice
 
-    def __init__(self, device: BroadlinkDevice, subdevice: str, command: str) -> None:
+    def __init__(
+        self, device: BroadlinkDevice, subdevice: str, command: str, unique_id: str
+    ) -> None:
         """Initialize the light."""
         super().__init__(device)
-        self._attr_name = f"{device.name} {subdevice} {command}"
-        self._attr_unique_id = f"{device.unique_id}-codes-{subdevice}-{command}"
+        self._attr_name = f"{subdevice} {command}"
+        self._attr_unique_id = unique_id
         self._subdevice = subdevice
         self._command = command
 
