837957d89e80e4f34b88efa346f8030c8daff5ff
epenet <6771947+epenet@users.noreply.github.com>
2022-06-22 18:22:01 +0200

    Adjust set_percentage routine in fans (#73837)

--- homeassistant/components/smartthings/fan.py
+++ homeassistant/components/smartthings/fan.py
@@ -52,8 +52,11 @@ class SmartThingsFan(SmartThingsEntity, FanEntity):
 
     _attr_supported_features = FanEntityFeature.SET_SPEED
 
-    async def async_set_percentage(self, percentage: int | None) -> None:
+    async def async_set_percentage(self, percentage: int) -> None:
         """Set the speed percentage of the fan."""
+        await self._async_set_percentage(percentage)
+
+    async def _async_set_percentage(self, percentage: int | None) -> None:
         if percentage is None:
             await self._device.switch_on(set_status=True)
         elif percentage == 0:
@@ -72,7 +75,7 @@ class SmartThingsFan(SmartThingsEntity, FanEntity):
         **kwargs,
     ) -> None:
         """Turn the fan on."""
-        await self.async_set_percentage(percentage)
+        await self._async_set_percentage(percentage)
 
     async def async_turn_off(self, **kwargs) -> None:
         """Turn the fan off."""
