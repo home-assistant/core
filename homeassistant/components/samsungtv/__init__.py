0ffadc8691111c18408d49aa3e1d0bc60c501274
J. Nick Koston <nick@koston.org>
2022-06-22 10:47:54 -0500

    revert samsungtv

--- homeassistant/components/samsungtv/__init__.py
+++ homeassistant/components/samsungtv/__init__.py
@@ -215,7 +215,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     entry.async_on_unload(entry.add_update_listener(debounced_reloader.async_call))
 
     hass.data[DOMAIN][entry.entry_id] = bridge
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
