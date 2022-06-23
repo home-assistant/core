3512baf5b9599b929c8791291f6be5529135e391
Erik Montnemery <erik@montnemery.com>
2022-06-23 21:38:17 +0200

    Improve group tests (#73630)

--- tests/components/group/test_switch.py
+++ tests/components/group/test_switch.py
@@ -56,7 +56,13 @@ async def test_default_state(hass):
 
 
 async def test_state_reporting(hass):
-    """Test the state reporting."""
+    """Test the state reporting in 'any' mode.
+
+    The group state is unavailable if all group members are unavailable.
+    Otherwise, the group state is unknown if all group members are unknown.
+    Otherwise, the group state is on if at least one group member is on.
+    Otherwise, the group state is off.
+    """
     await async_setup_component(
         hass,
         SWITCH_DOMAIN,
@@ -72,29 +78,79 @@ async def test_state_reporting(hass):
     await hass.async_start()
     await hass.async_block_till_done()
 
-    hass.states.async_set("switch.test1", STATE_ON)
-    hass.states.async_set("switch.test2", STATE_UNAVAILABLE)
-    await hass.async_block_till_done()
-    assert hass.states.get("switch.switch_group").state == STATE_ON
+    # Initial state with no group member in the state machine -> unavailable
+    assert hass.states.get("switch.switch_group").state == STATE_UNAVAILABLE
 
-    hass.states.async_set("switch.test1", STATE_ON)
-    hass.states.async_set("switch.test2", STATE_OFF)
-    await hass.async_block_till_done()
-    assert hass.states.get("switch.switch_group").state == STATE_ON
-
-    hass.states.async_set("switch.test1", STATE_OFF)
-    hass.states.async_set("switch.test2", STATE_OFF)
-    await hass.async_block_till_done()
-    assert hass.states.get("switch.switch_group").state == STATE_OFF
-
+    # All group members unavailable -> unavailable
     hass.states.async_set("switch.test1", STATE_UNAVAILABLE)
     hass.states.async_set("switch.test2", STATE_UNAVAILABLE)
     await hass.async_block_till_done()
     assert hass.states.get("switch.switch_group").state == STATE_UNAVAILABLE
 
+    # All group members unknown -> unknown
+    hass.states.async_set("switch.test1", STATE_UNKNOWN)
+    hass.states.async_set("switch.test2", STATE_UNKNOWN)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_UNKNOWN
+
+    # Group members unknown or unavailable -> unknown
+    hass.states.async_set("switch.test1", STATE_UNKNOWN)
+    hass.states.async_set("switch.test2", STATE_UNAVAILABLE)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_UNKNOWN
+
+    # At least one member on -> group on
+    hass.states.async_set("switch.test1", STATE_ON)
+    hass.states.async_set("switch.test2", STATE_UNAVAILABLE)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_ON
+
+    hass.states.async_set("switch.test1", STATE_ON)
+    hass.states.async_set("switch.test2", STATE_OFF)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_ON
+
+    hass.states.async_set("switch.test1", STATE_ON)
+    hass.states.async_set("switch.test2", STATE_ON)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_ON
+
+    hass.states.async_set("switch.test1", STATE_ON)
+    hass.states.async_set("switch.test2", STATE_UNKNOWN)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_ON
+
+    # Otherwise -> off
+    hass.states.async_set("switch.test1", STATE_OFF)
+    hass.states.async_set("switch.test2", STATE_OFF)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_OFF
+
+    hass.states.async_set("switch.test1", STATE_UNKNOWN)
+    hass.states.async_set("switch.test2", STATE_OFF)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_OFF
+
+    hass.states.async_set("switch.test1", STATE_UNAVAILABLE)
+    hass.states.async_set("switch.test2", STATE_OFF)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_OFF
+
+    # All group members removed from the state machine -> unavailable
+    hass.states.async_remove("switch.test1")
+    hass.states.async_remove("switch.test2")
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_UNAVAILABLE
+
 
 async def test_state_reporting_all(hass):
-    """Test the state reporting."""
+    """Test the state reporting in 'all' mode.
+
+    The group state is unavailable if all group members are unavailable.
+    Otherwise, the group state is unknown if at least one group member is unknown or unavailable.
+    Otherwise, the group state is off if at least one group member is off.
+    Otherwise, the group state is on.
+    """
     await async_setup_component(
         hass,
         SWITCH_DOMAIN,
@@ -110,11 +166,47 @@ async def test_state_reporting_all(hass):
     await hass.async_start()
     await hass.async_block_till_done()
 
+    # Initial state with no group member in the state machine -> unavailable
+    assert hass.states.get("switch.switch_group").state == STATE_UNAVAILABLE
+
+    # All group members unavailable -> unavailable
+    hass.states.async_set("switch.test1", STATE_UNAVAILABLE)
+    hass.states.async_set("switch.test2", STATE_UNAVAILABLE)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_UNAVAILABLE
+
+    # At least one member unknown or unavailable -> group unknown
     hass.states.async_set("switch.test1", STATE_ON)
     hass.states.async_set("switch.test2", STATE_UNAVAILABLE)
     await hass.async_block_till_done()
     assert hass.states.get("switch.switch_group").state == STATE_UNKNOWN
 
+    hass.states.async_set("switch.test1", STATE_ON)
+    hass.states.async_set("switch.test2", STATE_UNKNOWN)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_UNKNOWN
+
+    hass.states.async_set("switch.test1", STATE_UNKNOWN)
+    hass.states.async_set("switch.test2", STATE_UNKNOWN)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_UNKNOWN
+
+    hass.states.async_set("switch.test1", STATE_OFF)
+    hass.states.async_set("switch.test2", STATE_UNAVAILABLE)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_UNKNOWN
+
+    hass.states.async_set("switch.test1", STATE_OFF)
+    hass.states.async_set("switch.test2", STATE_UNKNOWN)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_UNKNOWN
+
+    hass.states.async_set("switch.test1", STATE_UNKNOWN)
+    hass.states.async_set("switch.test2", STATE_UNAVAILABLE)
+    await hass.async_block_till_done()
+    assert hass.states.get("switch.switch_group").state == STATE_UNKNOWN
+
+    # At least one member off -> group off
     hass.states.async_set("switch.test1", STATE_ON)
     hass.states.async_set("switch.test2", STATE_OFF)
     await hass.async_block_till_done()
@@ -125,13 +217,15 @@ async def test_state_reporting_all(hass):
     await hass.async_block_till_done()
     assert hass.states.get("switch.switch_group").state == STATE_OFF
 
+    # Otherwise -> on
     hass.states.async_set("switch.test1", STATE_ON)
     hass.states.async_set("switch.test2", STATE_ON)
     await hass.async_block_till_done()
     assert hass.states.get("switch.switch_group").state == STATE_ON
 
-    hass.states.async_set("switch.test1", STATE_UNAVAILABLE)
-    hass.states.async_set("switch.test2", STATE_UNAVAILABLE)
+    # All group members removed from the state machine -> unavailable
+    hass.states.async_remove("switch.test1")
+    hass.states.async_remove("switch.test2")
     await hass.async_block_till_done()
     assert hass.states.get("switch.switch_group").state == STATE_UNAVAILABLE
 
