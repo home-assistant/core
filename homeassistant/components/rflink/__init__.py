c4e17bfd98fd35d52a5002015d626117e0d5fb02
epenet <6771947+epenet@users.noreply.github.com>
2022-06-24 07:50:49 +0000

    Adjust type hints in rflink cover

--- homeassistant/components/rflink/__init__.py
+++ homeassistant/components/rflink/__init__.py
@@ -315,7 +315,6 @@ class RflinkDevice(Entity):
     """
 
     platform = None
-    _state = None
     _available = True
 
     def __init__(
@@ -345,6 +344,7 @@ class RflinkDevice(Entity):
         self._nogroup_aliases = nogroup_aliases
         self._should_fire_event = fire_event
         self._signal_repetitions = signal_repetitions
+        self._state = None
 
     @callback
     def handle_event_callback(self, event):
