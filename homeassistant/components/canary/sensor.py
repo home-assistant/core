b8206b4f7ad697ef77f486b51279102c8867b588
Brian May <brian@bmay.org>
2022-06-25 12:06:41 -0700

    Add support for Canary View
    add support for Canary View cameras

--- homeassistant/components/canary/sensor.py
+++ homeassistant/components/canary/sensor.py
@@ -17,6 +17,7 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.icon import icon_for_battery_level
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import (
@@ -34,28 +35,65 @@ SENSOR_VALUE_PRECISION: Final = 2
 ATTR_AIR_QUALITY: Final = "air_quality"
 
 # Define variables to store the device names, as referred to by the Canary API.
-# Note: If Canary change the name of any of their devices (which they have done),
+# Note: If Canary changes a name of a device (which they have done),
 # then these variables will need updating, otherwise the sensors will stop working
 # and disappear in Home Assistant.
 CANARY_PRO: Final = "Canary Pro"
 CANARY_FLEX: Final = "Canary Flex"
+CANARY_VIEW: Final = "Canary View"
 
 # Sensor types are defined like so:
-# sensor type name, unit_of_measurement, icon, device class, products supported
+# sensor type name, unit_of_measurement, icon, products supported, source of data
 SENSOR_TYPES: Final[list[SensorTypeItem]] = [
-    ("temperature", TEMP_CELSIUS, None, SensorDeviceClass.TEMPERATURE, [CANARY_PRO]),
-    ("humidity", PERCENTAGE, None, SensorDeviceClass.HUMIDITY, [CANARY_PRO]),
-    ("air_quality", None, "mdi:weather-windy", None, [CANARY_PRO]),
     (
-        "wifi",
+        SensorType.TEMPERATURE,
+        TEMP_CELSIUS,
+        None,
+        [CANARY_PRO],
+        DATA_TYPE_READING,
+    ),
+    (
+        SensorType.HUMIDITY,
+        PERCENTAGE,
+        None,
+        [CANARY_PRO],
+        DATA_TYPE_READING,
+    ),
+    (
+        SensorType.AIR_QUALITY,
+        None,
+        "weather-windy",
+        [CANARY_PRO],
+        DATA_TYPE_READING,
+    ),
+    (
+        SensorType.WIFI,
         SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
         None,
-        SensorDeviceClass.SIGNAL_STRENGTH,
-        [CANARY_PRO, CANARY_FLEX],
+        [CANARY_PRO, CANARY_FLEX, CANARY_VIEW],
+        DATA_TYPE_READING,
     ),
-    ("battery", PERCENTAGE, None, SensorDeviceClass.BATTERY, [CANARY_FLEX]),
-    ("last_entry_date", None, "mdi:run-fast", None, [CANARY_PRO, CANARY_FLEX]),
-    ("entries_captured_today", None, "mdi:file-video", None, [CANARY_PRO, CANARY_FLEX]),
+    (
+        SensorType.BATTERY,
+        PERCENTAGE,
+        None,
+        [CANARY_FLEX],
+        DATA_TYPE_READING,
+    ),
+    (
+        SensorType.DATE_LAST_ENTRY,
+        None,
+        "run-fast",
+        [CANARY_PRO, CANARY_FLEX, CANARY_VIEW],
+        DATA_TYPE_ENTRY,
+    ),
+    (
+        SensorType.ENTRIES_CAPTURED_TODAY,
+        None,
+        "file-video",
+        [CANARY_PRO, CANARY_FLEX, CANARY_VIEW],
+        DATA_TYPE_ENTRY,
+    ),
 ]
 
 STATE_AIR_QUALITY_NORMAL: Final = "normal"
@@ -81,7 +119,7 @@ async def async_setup_entry(
             if device.is_online:
                 device_type = device.device_type
                 for sensor_type in SENSOR_TYPES:
-                    if device_type.get("name") in sensor_type[4]:
+                    if device_type.get("name") in sensor_type[3]:
                         sensors.append(
                             CanarySensor(coordinator, sensor_type, location, device)
                         )
@@ -105,30 +143,11 @@ class CanarySensor(CoordinatorEntity[CanaryDataUpdateCoordinator], SensorEntity)
         self._sensor_type = sensor_type
         self._device_id = device.device_id
 
-        sensor_type_name = sensor_type[0].replace("_", " ").title()
+        sensor_type_name = sensor_type[0].value.replace("_", " ").title()
         self._attr_name = f"{location.name} {device.name} {sensor_type_name}"
-
-        canary_sensor_type = None
-        self._canary_data_type = DATA_TYPE_READING
-        if self._sensor_type[0] == "air_quality":
-            canary_sensor_type = SensorType.AIR_QUALITY
-        elif self._sensor_type[0] == "temperature":
-            canary_sensor_type = SensorType.TEMPERATURE
-        elif self._sensor_type[0] == "humidity":
-            canary_sensor_type = SensorType.HUMIDITY
-        elif self._sensor_type[0] == "wifi":
-            canary_sensor_type = SensorType.WIFI
-        elif self._sensor_type[0] == "battery":
-            canary_sensor_type = SensorType.BATTERY
-        elif self._sensor_type[0] == "last_entry_date":
-            canary_sensor_type = SensorType.DATE_LAST_ENTRY
-            self._canary_data_type = DATA_TYPE_ENTRY
-        elif self._sensor_type[0] == "entries_captured_today":
-            canary_sensor_type = SensorType.ENTRIES_CAPTURED_TODAY
-            self._canary_data_type = DATA_TYPE_ENTRY
-
-        self._canary_type = canary_sensor_type
-        self._attr_unique_id = f"{device.device_id}_{sensor_type[0]}"
+        self._canary_type = self._sensor_type[0]
+        self._canary_data_type = self._sensor_type[4]
+        self._attr_unique_id = f"{device.device_id}_{sensor_type[0].value}"
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, str(device.device_id))},
             model=device.device_type["name"],
@@ -136,8 +155,8 @@ class CanarySensor(CoordinatorEntity[CanaryDataUpdateCoordinator], SensorEntity)
             name=device.name,
         )
         self._attr_native_unit_of_measurement = sensor_type[1]
+        self._icon = None if sensor_type[2] is None else f"mdi:{sensor_type[2]}"
         self._state: str | int | float | datetime | None = None
-        self._attr_icon = sensor_type[2]
         _LOGGER.info("CanarySensor: %s created", self.name)
 
     @property
@@ -159,6 +178,17 @@ class CanarySensor(CoordinatorEntity[CanaryDataUpdateCoordinator], SensorEntity)
     def name(self) -> str:
         """Name of sensor."""
         return str(self._attr_name)
+
+    @property
+    def icon(self) -> str | None:
+        """Icon to use in the frontend, if any."""
+        self._state = None if self._state is None else str(self._state)
+        if self._canary_type == SensorType.BATTERY and self._state is not None:
+            return icon_for_battery_level(
+                battery_level=int(float(self._state)), charging=False
+            )
+
+        return self._icon
 
     # @property
     def reading(self) -> None:
--- tests/components/canary/test_sensor.py
+++ tests/components/canary/test_sensor.py
@@ -225,12 +225,63 @@ async def test_sensors_flex(hass, canary) -> None:
     assert device.model == "Canary Flex"
 
 
+async def test_sensors_view(hass, canary) -> None:
+    """Test the creation and values of the sensors for Canary View."""
+
+    registry = mock_registry(hass)
+    device_registry = mock_device_registry(hass)
+
+    online_device_at_home = mock_device(20, "Dining Room", True, "Canary View")
+
+    instance = canary.return_value
+    instance.get_locations.return_value = [
+        mock_location(100, "Home", True, devices=[online_device_at_home]),
+    ]
+
+    instance.get_latest_readings.return_value = [
+        mock_reading("wifi", "-77"),
+    ]
+
+    config = {DOMAIN: {"username": "test-username", "password": "test-password"}}
+    with patch("homeassistant.components.canary.PLATFORMS", ["sensor"]):
+        assert await async_setup_component(hass, DOMAIN, config)
+        await hass.async_block_till_done()
+
+    sensors = {
+        "home_dining_room_wifi": (
+            "20_wifi",
+            "-77.0",
+            SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+            SensorDeviceClass.SIGNAL_STRENGTH,
+            None,
+        ),
+    }
+
+    for (sensor_id, data) in sensors.items():
+        entity_entry = registry.async_get(f"sensor.{sensor_id}")
+        assert entity_entry
+        assert entity_entry.original_device_class == data[3]
+        assert entity_entry.unique_id == data[0]
+        assert entity_entry.original_icon == data[4]
+
+        state = hass.states.get(f"sensor.{sensor_id}")
+        assert state
+        assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == data[2]
+        assert state.state == data[1]
+
+    device = device_registry.async_get_device({(DOMAIN, "20")})
+    assert device
+    assert device.manufacturer == MANUFACTURER
+    assert device.name == "Dining Room"
+    assert device.model == "Canary View"
+
+
 async def test_sensors_entries(hass, canary) -> None:
     """Test the creation and values of the sensors for Canary Entry data."""
 
     registry = mock_registry(hass)
 
-    online_device_at_home = mock_device(20, "Dining Room", True, "Canary Pro", "12345")
+    online_device_at_home = mock_device(20, "Dining Room", True, "Canary View", "12345")
 
     instance = canary.return_value
     instance.get_locations.return_value = [
@@ -289,7 +340,7 @@ async def test_sensors_entries_with_no_data(hass, canary) -> None:
 
     registry = mock_registry(hass)
 
-    online_device_at_home = mock_device(21, "Living Room", True, "Canary Flex", "22345")
+    online_device_at_home = mock_device(21, "Living Room", True, "Canary View", "22345")
 
     instance = canary.return_value
     instance.get_locations.return_value = [
