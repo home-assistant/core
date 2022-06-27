604b85e12c2194fbdf1c926de3c298967a1f741c
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-06-27 23:59:53 +0200

    More tests

--- tests/components/nextdns/test_config_flow.py
+++ tests/components/nextdns/test_config_flow.py
@@ -1,10 +1,20 @@
 """Define tests for the NextDNS config flow."""
+import asyncio
 from unittest.mock import patch
 
+from nextdns import ApiError, InvalidApiKeyError
+import pytest
+
 from homeassistant import data_entry_flow
-from homeassistant.components.nextdns.const import CONF_PROFILE_NAME, DOMAIN
+from homeassistant.components.nextdns.const import (
+    CONF_PROFILE_ID,
+    CONF_PROFILE_NAME,
+    DOMAIN,
+)
 from homeassistant.config_entries import SOURCE_USER
 from homeassistant.const import CONF_API_KEY
+
+from tests.common import MockConfigEntry
 
 PROFILES = [{"id": "xyz12", "fingerprint": "aabbccdd123", "name": "Fake Profile"}]
 
@@ -38,6 +48,59 @@ async def test_form_create_entry(hass):
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result["title"] == "Fake Profile"
-    assert result["data"]["api_key"] == "fake_api_key"
-    assert result["data"]["profile_id"] == "xyz12"
+    assert result["data"][CONF_API_KEY] == "fake_api_key"
+    assert result["data"][CONF_PROFILE_ID] == "xyz12"
     assert len(mock_setup_entry.mock_calls) == 1
+
+
+@pytest.mark.parametrize(
+    "error",
+    [
+        (ApiError("API Error"), "cannot_connect"),
+        (InvalidApiKeyError, "invalid_api_key"),
+        (asyncio.TimeoutError, "cannot_connect"),
+        (ValueError, "unknown"),
+    ],
+)
+async def test_form_errors(hass, error):
+    """Test we handle errors."""
+    exc, base_error = error
+    with patch(
+        "homeassistant.components.nextdns.NextDns.get_profiles", side_effect=exc
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": SOURCE_USER},
+            data={CONF_API_KEY: "fake_api_key"},
+        )
+
+    assert result["errors"] == {"base": base_error}
+
+
+async def test_form_already_configured(hass):
+    """Test that errors are shown when duplicates are added."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="xyz12",
+        data={CONF_API_KEY: "fake_api_key", CONF_PROFILE_ID: "xyz12"},
+    )
+    entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.nextdns.NextDns.get_profiles", return_value=PROFILES
+    ):
+        await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_API_KEY: "fake_api_key"},
+        )
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], {CONF_PROFILE_NAME: "Fake Profile"}
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "already_configured"
