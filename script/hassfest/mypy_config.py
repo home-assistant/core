fb108533580d5f4c326ca970d8e6fd4998cc5593
epenet <6771947+epenet@users.noreply.github.com>
2022-06-28 08:50:16 +0200

    Fix mypy issues in zha core modules (#74028)
    * Fix mypy issues in zha gateway, group and helpers
    
    * Cleanup device
    
    * Apply suggestion
    
    * Raise ValueError
    
    * Use hass.config.path

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -144,11 +144,7 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.xiaomi_miio.light",
     "homeassistant.components.xiaomi_miio.sensor",
     "homeassistant.components.xiaomi_miio.switch",
-    "homeassistant.components.zha.core.device",
     "homeassistant.components.zha.core.discovery",
-    "homeassistant.components.zha.core.gateway",
-    "homeassistant.components.zha.core.group",
-    "homeassistant.components.zha.core.helpers",
     "homeassistant.components.zha.core.registries",
     "homeassistant.components.zha.core.store",
 ]
