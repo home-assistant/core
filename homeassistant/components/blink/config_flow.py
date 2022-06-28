b61bfec3ccc5b76261eb15d83c3d71f3a1c2fcca
epenet <6771947+epenet@users.noreply.github.com>
2022-06-28 15:35:30 +0000

    Revert changes to blink as needs more adjustment

--- homeassistant/components/blink/config_flow.py
+++ homeassistant/components/blink/config_flow.py
@@ -1,9 +1,7 @@
 """Config flow to configure Blink."""
 from __future__ import annotations
 
-from collections.abc import Mapping
 import logging
-from typing import Any
 
 from blinkpy.auth import Auth, LoginError, TokenRefreshFailed
 from blinkpy.blinkpy import Blink, BlinkSetupError
@@ -17,7 +15,6 @@ from homeassistant.const import (
     CONF_USERNAME,
 )
 from homeassistant.core import callback
-from homeassistant.data_entry_flow import FlowResult
 
 from .const import DEFAULT_SCAN_INTERVAL, DEVICE_ID, DOMAIN
 
@@ -123,9 +120,9 @@ class BlinkConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
-    async def async_step_reauth(self, entry_data: Mapping[str, Any]) -> FlowResult:
+    async def async_step_reauth(self, entry_data):
         """Perform reauth upon migration of old entries."""
-        return await self.async_step_user(dict(entry_data))
+        return await self.async_step_user(entry_data)
 
     @callback
     def _async_finish_flow(self):
