def766bf9aaf50684fdd35edb892279b5bd0ceeb
Jeef <jeeftor@users.noreply.github.com>
2022-06-29 12:40:02 -0600

    Update homeassistant/components/intellifire/fan.py
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/intellifire/fan.py
+++ homeassistant/components/intellifire/fan.py
@@ -121,7 +121,7 @@ class IntellifireFan(IntellifireEntity, FanEntity):
     async def async_set_percentage(self, percentage: int) -> None:
         """Set the speed percentage of the fan."""
         # Calculate percentage steps
-        LOGGER.debug("--Setting Fan Speed %s", percentage)
+        LOGGER.debug("Setting Fan Speed %s", percentage)
         percent_step = 100.0 / len(self.entity_description.named_speeds)
         int_value = int(math.ceil(float(percentage) / percent_step))
         await self.entity_description.set_fn(self.coordinator.control_api, int_value)
