4f40a94aeaaca3419ccc316417cf48a865a60d70
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-06-29 12:29:47 +0200

    Remove deprecated YAML import for Tautulli (#74172)

--- homeassistant/components/tautulli/const.py
+++ homeassistant/components/tautulli/const.py
@@ -5,9 +5,5 @@ ATTR_TOP_USER = "top_user"
 
 CONF_MONITORED_USERS = "monitored_users"
 DEFAULT_NAME = "Tautulli"
-DEFAULT_PATH = ""
-DEFAULT_PORT = "8181"
-DEFAULT_SSL = False
-DEFAULT_VERIFY_SSL = True
 DOMAIN = "tautulli"
 LOGGER: Logger = getLogger(__package__)
