be74d8f07cb2f74c49e09175095757b9a07072ef
Robert Hillis <tkdrob4390@yahoo.com>
2022-06-29 08:14:59 -0400

    uno mas

--- homeassistant/components/slack/number.py
+++ homeassistant/components/slack/number.py
@@ -11,6 +11,14 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from . import SlackEntity
 from .const import DATA_CLIENT, DOMAIN
 
+NUMBER_TYPES = NumberEntityDescription(
+    key="do_not_disturb_period",
+    name="Do Not Disturb Period",
+    icon="mdi:clock",
+    unit_of_measurement=TIME_MINUTES,
+    step=1,
+)
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -22,13 +30,7 @@ async def async_setup_entry(
         [
             SlackNumberEntity(
                 hass.data[DOMAIN][entry.entry_id][DATA_CLIENT],
-                NumberEntityDescription(
-                    key="do_not_disturb_period",
-                    name="Do Not Disturb Period",
-                    icon="mdi:clock",
-                    unit_of_measurement=TIME_MINUTES,
-                    step=1,
-                ),
+                NUMBER_TYPES,
                 entry,
             )
         ]
