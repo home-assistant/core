20dc1b2cd44bc70ecfbeadbe48e9cd43b5611d24
Erik <erik@montnemery.com>
2022-06-30 09:51:44 +0200

    Fire event_mqtt_reloaded only after reload is completed

--- homeassistant/components/mqtt/cover.py
+++ homeassistant/components/mqtt/cover.py
@@ -46,8 +46,8 @@ from .debug_info import log_messages
 from .mixins import (
     MQTT_ENTITY_COMMON_SCHEMA,
     MqttEntity,
+    async_discover_yaml_entities,
     async_setup_entry_helper,
-    async_setup_platform_discovery,
     async_setup_platform_helper,
     warn_for_legacy_schema,
 )
@@ -242,9 +242,7 @@ async def async_setup_entry(
 ) -> None:
     """Set up MQTT cover through configuration.yaml and dynamically through MQTT discovery."""
     # load and initialize platform config from configuration.yaml
-    config_entry.async_on_unload(
-        await async_setup_platform_discovery(hass, cover.DOMAIN)
-    )
+    await async_discover_yaml_entities(hass, cover.DOMAIN)
     # setup for discovery
     setup = functools.partial(
         _async_setup_entity, hass, async_add_entities, config_entry=config_entry
