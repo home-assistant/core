home-assistant/core

commit 1a968e6aac8fc9f42a6185bbe92656fde0d0d741

Simplify esphome state updates (#73409)

homeassistant/components/esphome/__init__.py
@@ -568,6 +568,7 @@ async def platform_async_setup_entry(
     @callback
     def async_list_entities(infos: list[EntityInfo]) -> None:
         """Update entities of this platform when entities are listed."""
+        key_to_component = entry_data.key_to_component
         old_infos = entry_data.info[component_key]
         new_infos: dict[int, EntityInfo] = {}
         add_entities = []
@@ -586,10 +587,12 @@ async def platform_async_setup_entry(
                 entity = entity_type(entry_data, component_key, info.key)
                 add_entities.append(entity)
             new_infos[info.key] = info
+            key_to_component[info.key] = component_key
 
         # Remove old entities
         for info in old_infos.values():
             entry_data.async_remove_entity(hass, component_key, info.key)
+            key_to_component.pop(info.key, None)
 
         # First copy the now-old info into the backup object
         entry_data.old_info[component_key] = entry_data.info[component_key]
@@ -602,22 +605,6 @@ async def platform_async_setup_entry(
     signal = f"esphome_{entry.entry_id}_on_list"
     entry_data.cleanup_callbacks.append(
         async_dispatcher_connect(hass, signal, async_list_entities)
-    )
-
-    @callback
-    def async_entity_state(state: EntityState) -> None:
-        """Notify the appropriate entity of an updated state."""
-        if not isinstance(state, state_type):
-            return
-        # cast back to upper type, otherwise mypy gets confused
-        state = cast(EntityState, state)
-
-        entry_data.state[component_key][state.key] = state
-        entry_data.async_update_entity(hass, component_key, state.key)
-
-    signal = f"esphome_{entry.entry_id}_on_state"
-    entry_data.cleanup_callbacks.append(
-        async_dispatcher_connect(hass, signal, async_entity_state)
     )
 
 

homeassistant/components/esphome/entry_data.py
@@ -65,6 +65,7 @@ class RuntimeEntryData:
     store: Store
     state: dict[str, dict[int, EntityState]] = field(default_factory=dict)
     info: dict[str, dict[int, EntityInfo]] = field(default_factory=dict)
+    key_to_component: dict[int, str] = field(default_factory=dict)
 
     # A second list of EntityInfo objects
     # This is necessary for when an entity is being removed. HA requires
@@ -81,14 +82,6 @@ class RuntimeEntryData:
     loaded_platforms: set[str] = field(default_factory=set)
     platform_load_lock: asyncio.Lock = field(default_factory=asyncio.Lock)
     _storage_contents: dict[str, Any] | None = None
-
-    @callback
-    def async_update_entity(
-        self, hass: HomeAssistant, component_key: str, key: int
-    ) -> None:
-        """Schedule the update of an entity."""
-        signal = f"esphome_{self.entry_id}_update_{component_key}_{key}"
-        async_dispatcher_send(hass, signal)
 
     @callback
     def async_remove_entity(
@@ -131,9 +124,11 @@ class RuntimeEntryData:
 
     @callback
     def async_update_state(self, hass: HomeAssistant, state: EntityState) -> None:
-        """Distribute an update of state information to all platforms."""
-        signal = f"esphome_{self.entry_id}_on_state"
-        async_dispatcher_send(hass, signal, state)
+        """Distribute an update of state information to the target."""
+        component_key = self.key_to_component[state.key]
+        self.state[component_key][state.key] = state
+        signal = f"esphome_{self.entry_id}_update_{component_key}_{state.key}"
+        async_dispatcher_send(hass, signal)
 
     @callback
     def async_update_device_state(self, hass: HomeAssistant) -> None:

commit 8431499de6e1827d1dab5c709aee72ed2ac02ce8

Fix key collision between platforms in esphome state updates
Fixes #74231

Regressed in #73409

homeassistant/components/esphome/__init__.py
@@ -151,11 +151,6 @@ async def async_setup_entry(  # noqa: C901
     )
 
     @callback
-    def async_on_state(state: EntityState) -> None:
-        """Send dispatcher updates when a new state is received."""
-        entry_data.async_update_state(hass, state)
-
-    @callback
     def async_on_service_call(service: HomeassistantServiceCall) -> None:
         """Call service when user automation in ESPHome config is triggered."""
         domain, service_name = service.service.split(".", 1)
@@ -288,7 +283,7 @@ async def async_setup_entry(  # noqa: C901
             entity_infos, services = await cli.list_entities_services()
             await entry_data.async_update_static_infos(hass, entry, entity_infos)
             await _setup_services(hass, entry_data, services)
-            await cli.subscribe_states(async_on_state)
+            await cli.subscribe_states(entry_data.async_update_state)
             await cli.subscribe_service_calls(async_on_service_call)
             await cli.subscribe_home_assistant_states(async_on_state_subscription)
 
@@ -564,11 +559,11 @@ async def platform_async_setup_entry(
     entry_data.info[component_key] = {}
     entry_data.old_info[component_key] = {}
     entry_data.state[component_key] = {}
+    entry_data.register_state_type(state_type, component_key)
 
     @callback
     def async_list_entities(infos: list[EntityInfo]) -> None:
         """Update entities of this platform when entities are listed."""
-        key_to_component = entry_data.key_to_component
         old_infos = entry_data.info[component_key]
         new_infos: dict[int, EntityInfo] = {}
         add_entities = []
@@ -587,12 +582,10 @@ async def platform_async_setup_entry(
                 entity = entity_type(entry_data, component_key, info.key)
                 add_entities.append(entity)
             new_infos[info.key] = info
-            key_to_component[info.key] = component_key
 
         # Remove old entities
         for info in old_infos.values():
             entry_data.async_remove_entity(hass, component_key, info.key)
-            key_to_component.pop(info.key, None)
 
         # First copy the now-old info into the backup object
         entry_data.old_info[component_key] = entry_data.info[component_key]
@@ -714,13 +707,8 @@ class EsphomeEntity(Entity, Generic[_InfoT, _StateT]):
         )
 
         self.async_on_remove(
-            async_dispatcher_connect(
-                self.hass,
-                (
-                    f"esphome_{self._entry_id}"
-                    f"_update_{self._component_key}_{self._key}"
-                ),
-                self._on_state_update,
+            self._entry_data.async_subscribe_state_update(
+                self._component_key, self._key, self._on_state_update
             )
         )
 

homeassistant/components/esphome/entry_data.py
@@ -67,7 +67,12 @@ class RuntimeEntryData:
     store: Store
     state: dict[str, dict[int, EntityState]] = field(default_factory=dict)
     info: dict[str, dict[int, EntityInfo]] = field(default_factory=dict)
-    key_to_component: dict[int, str] = field(default_factory=dict)
+    entity_state_type_name_to_component_key: dict[str, str] = field(
+        default_factory=dict
+    )
+    entity_state_type_to_component_key: dict[type[EntityState], str] = field(
+        default_factory=dict
+    )
 
     # A second list of EntityInfo objects
     # This is necessary for when an entity is being removed. HA requires
@@ -81,6 +86,9 @@ class RuntimeEntryData:
     api_version: APIVersion = field(default_factory=APIVersion)
     cleanup_callbacks: list[Callable[[], None]] = field(default_factory=list)
     disconnect_callbacks: list[Callable[[], None]] = field(default_factory=list)
+    state_subscriptions: dict[tuple[str, int], Callable[[], None]] = field(
+        default_factory=dict
+    )
     loaded_platforms: set[str] = field(default_factory=set)
     platform_load_lock: asyncio.Lock = field(default_factory=asyncio.Lock)
     _storage_contents: dict[str, Any] | None = None
@@ -125,18 +133,65 @@ class RuntimeEntryData:
         async_dispatcher_send(hass, signal, infos)
 
     @callback
-    def async_update_state(self, hass: HomeAssistant, state: EntityState) -> None:
-        """Distribute an update of state information to the target."""
-        component_key = self.key_to_component[state.key]
-        self.state[component_key][state.key] = state
-        signal = f"esphome_{self.entry_id}_update_{component_key}_{state.key}"
-        _LOGGER.debug(
-            "Dispatching update for component %s with state key %s: %s",
+    def register_state_type(
+        self, state_type: type[EntityState], component_key: str
+    ) -> None:
+        """Register a state type."""
+        self.entity_state_type_to_component_key[state_type] = component_key
+        self.entity_state_type_name_to_component_key[
+            state_type.__name__
+        ] = component_key
+
+    @callback
+    def component_key_for_state(self, state: EntityState) -> str:
+        """Return the component key for a state."""
+        state_class_name = state.__class__.__name__
+
+        # We try to avoid doing a linear search for the
+        # the component key by looking up the class name
+        # in the mapping. If it's not there, we fall back
+        # to a linear search for the component key by doing
+        # an instance search over entity_state_type_to_component_key.
+        if state_class_name in self.entity_state_type_name_to_component_key:
+            return self.entity_state_type_name_to_component_key[state_class_name]
+
+        for (
+            state_type,
             component_key,
-            state.key,
+        ) in self.entity_state_type_to_component_key.items():
+            if isinstance(state, state_type):
+                return component_key
+
+        raise ValueError(f"{state} is not a registered state_type")
+
+    @callback
+    def async_subscribe_state_update(
+        self,
+        component_key: str,
+        state_key: int,
+        entity_callback: Callable[[], None],
+    ) -> Callable[[], None]:
+        """Subscribe to state updates."""
+
+        def _unsubscribe() -> None:
+            self.state_subscriptions.pop((component_key, state_key))
+
+        self.state_subscriptions[(component_key, state_key)] = entity_callback
+        return _unsubscribe
+
+    @callback
+    def async_update_state(self, state: EntityState) -> None:
+        """Distribute an update of state information to the target."""
+        component_key = self.component_key_for_state(state)
+        subscription_key = (component_key, state.key)
+        self.state[component_key][state.key] = state
+        _LOGGER.debug(
+            "Dispatching update with key %s: %s",
+            subscription_key,
             state,
         )
-        async_dispatcher_send(hass, signal)
+        if subscription_key in self.state_subscriptions:
+            self.state_subscriptions[subscription_key]()
 
     @callback
     def async_update_device_state(self, hass: HomeAssistant) -> None: