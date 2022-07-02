f35527cdecdbea677ebc3c185362bea22ea2552b
thecode <levyshay1@gmail.com>
2022-07-02 14:49:57 +0000

    debug fail in test_setup_without_yaml_file

--- tests/components/device_tracker/test_init.py
+++ tests/components/device_tracker/test_init.py
@@ -162,11 +162,12 @@ async def test_duplicate_mac_dev_id(mock_warning, hass):
     assert "Duplicate device IDs" in args[0], "Duplicate device IDs warning expected"
 
 
-@pytest.mark.skip(reason="verified that it fail CI")
+# @pytest.mark.skip(reason="verified that it fail CI")
 async def test_setup_without_yaml_file(hass, enable_custom_integrations):
     """Test with no YAML file."""
     with assert_setup_component(1, device_tracker.DOMAIN):
         assert await async_setup_component(hass, device_tracker.DOMAIN, TEST_PLATFORM)
+        await hass.async_block_till_done()
 
 
 async def test_gravatar(hass):
