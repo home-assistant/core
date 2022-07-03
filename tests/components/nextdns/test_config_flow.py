f5b3518a54819ca21d7fd3baac50d5e5abfbe3b7
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-07-03 18:51:50 +0200

    Add NextDNS integration (#74150)
    * Initial commit
    
    * Update manifest
    
    * Add first test
    
    * Simplify init
    
    * More tests
    
    * Update tests
    
    * More tests
    
    * More tests
    
    * Add tests for sensor platform
    
    * More tests for sensor platform
    
    * Add tests for system_health
    
    * Fix typo
    
    * Improve test coverage
    
    * Improve test coverage
    
    * Add tests for diagnostics
    
    * Add comment
    
    * Run hassfest
    
    * Fix typo
    
    * Run gen_requirements_all
    
    * Fix tests
    
    * Change key name in diagnostics
    
    * Remove diagnostics and system_health platforms
    
    * Bump library

---
+++ tests/components/nextdns/test_config_flow.py
@@ -0,0 +1,100 @@
+"""Define tests for the NextDNS config flow."""
+import asyncio
+from unittest.mock import patch
+
+from nextdns import ApiError, InvalidApiKeyError
+import pytest
+
+from homeassistant import data_entry_flow
+from homeassistant.components.nextdns.const import (
+    CONF_PROFILE_ID,
+    CONF_PROFILE_NAME,
+    DOMAIN,
+)
+from homeassistant.config_entries import SOURCE_USER
+from homeassistant.const import CONF_API_KEY
+
+from . import PROFILES, init_integration
+
+
+async def test_form_create_entry(hass):
+    """Test that the user step works."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == SOURCE_USER
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.nextdns.NextDns.get_profiles", return_value=PROFILES
+    ), patch(
+        "homeassistant.components.nextdns.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_API_KEY: "fake_api_key"},
+        )
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+        assert result["step_id"] == "profiles"
+
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {CONF_PROFILE_NAME: "Fake Profile"}
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == "Fake Profile"
+    assert result["data"][CONF_API_KEY] == "fake_api_key"
+    assert result["data"][CONF_PROFILE_ID] == "xyz12"
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+@pytest.mark.parametrize(
+    "error",
+    [
+        (ApiError("API Error"), "cannot_connect"),
+        (InvalidApiKeyError, "invalid_api_key"),
+        (asyncio.TimeoutError, "cannot_connect"),
+        (ValueError, "unknown"),
+    ],
+)
+async def test_form_errors(hass, error):
+    """Test we handle errors."""
+    exc, base_error = error
+    with patch(
+        "homeassistant.components.nextdns.NextDns.get_profiles", side_effect=exc
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": SOURCE_USER},
+            data={CONF_API_KEY: "fake_api_key"},
+        )
+
+    assert result["errors"] == {"base": base_error}
+
+
+async def test_form_already_configured(hass):
+    """Test that errors are shown when duplicates are added."""
+    entry = await init_integration(hass)
+    entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.nextdns.NextDns.get_profiles", return_value=PROFILES
+    ):
+        await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_API_KEY: "fake_api_key"},
+        )
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], {CONF_PROFILE_NAME: "Fake Profile"}
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    assert result["reason"] == "already_configured"
