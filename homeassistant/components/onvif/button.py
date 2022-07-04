75d83ded9dcc389b5fae7d229429663c9e63b146
Diogo Gomes <dgomes@ua.pt>
2022-07-04 16:40:07 +0100

    address review

--- homeassistant/components/onvif/button.py
+++ homeassistant/components/onvif/button.py
@@ -1,9 +1,9 @@
 """ONVIF Buttons."""
-from datetime import datetime
+from datetime import datetime, timezone
+import time
 
 from homeassistant.components.button import ButtonDeviceClass, ButtonEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ENTITY_CATEGORY_CONFIG
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -46,7 +46,7 @@ class RebootButton(ONVIFBaseEntity, ButtonEntity):
 class SetSystemDateAndTimeButton(ONVIFBaseEntity, ButtonEntity):
     """Defines a ONVIF SetSystemDateAndTime button."""
 
-    _attr_entity_category = ENTITY_CATEGORY_CONFIG
+    _attr_entity_category = EntityCategory.CONFIG
 
     def __init__(self, device: ONVIFDevice) -> None:
         """Initialize the button entity."""
@@ -57,18 +57,18 @@ class SetSystemDateAndTimeButton(ONVIFBaseEntity, ButtonEntity):
     async def async_press(self) -> None:
         """Send out a SetSystemDateAndTime command."""
         device_mgmt = self.device.device.create_devicemgmt_service()
-        now = datetime.now()
+        now_utc = datetime.now(timezone.utc)
         current = await device_mgmt.GetSystemDateAndTime()
 
         dt_param = device_mgmt.create_type("SetSystemDateAndTime")
         dt_param.DateTimeType = "Manual"
-        dt_param.DaylightSavings = current.DaylightSavings
-        dt_param.TimeZone = current.TimeZone
+        dt_param.DaylightSavings = bool(time.localtime().tm_isdst)
+        dt_param.TimeZone = str(now_utc.astimezone().tzinfo)
         dt_param.UTCDateTime = current.UTCDateTime
-        dt_param.UTCDateTime.Date.Year = now.year
-        dt_param.UTCDateTime.Date.Month = now.month
-        dt_param.UTCDateTime.Date.Day = now.day
-        dt_param.UTCDateTime.Time.Hour = now.hour
-        dt_param.UTCDateTime.Time.Minute = now.minute
-        dt_param.UTCDateTime.Time.Second = now.second
+        dt_param.UTCDateTime.Date.Year = now_utc.year
+        dt_param.UTCDateTime.Date.Month = now_utc.month
+        dt_param.UTCDateTime.Date.Day = now_utc.day
+        dt_param.UTCDateTime.Time.Hour = now_utc.hour
+        dt_param.UTCDateTime.Time.Minute = now_utc.minute
+        dt_param.UTCDateTime.Time.Second = now_utc.second
         await device_mgmt.SetSystemDateAndTime(dt_param)
