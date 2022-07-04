735cca08e4a42faa383ad33830000f2eb1591d4d
Diogo Gomes <dgomes@ua.pt>
2022-07-04 19:03:16 +0100

    follow recommendation to set date and time on start

--- homeassistant/components/onvif/button.py
+++ homeassistant/components/onvif/button.py
@@ -1,7 +1,4 @@
 """ONVIF Buttons."""
-from datetime import datetime, timezone
-import time
-
 from homeassistant.components.button import ButtonDeviceClass, ButtonEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -56,19 +53,4 @@ class SetSystemDateAndTimeButton(ONVIFBaseEntity, ButtonEntity):
 
     async def async_press(self) -> None:
         """Send out a SetSystemDateAndTime command."""
-        device_mgmt = self.device.device.create_devicemgmt_service()
-        now_utc = datetime.now(timezone.utc)
-        current = await device_mgmt.GetSystemDateAndTime()
-
-        dt_param = device_mgmt.create_type("SetSystemDateAndTime")
-        dt_param.DateTimeType = "Manual"
-        dt_param.DaylightSavings = bool(time.localtime().tm_isdst)
-        dt_param.TimeZone = str(now_utc.astimezone().tzinfo)
-        dt_param.UTCDateTime = current.UTCDateTime
-        dt_param.UTCDateTime.Date.Year = now_utc.year
-        dt_param.UTCDateTime.Date.Month = now_utc.month
-        dt_param.UTCDateTime.Date.Day = now_utc.day
-        dt_param.UTCDateTime.Time.Hour = now_utc.hour
-        dt_param.UTCDateTime.Time.Minute = now_utc.minute
-        dt_param.UTCDateTime.Time.Second = now_utc.second
-        await device_mgmt.SetSystemDateAndTime(dt_param)
+        await self.device.async_manually_set_date_and_time()
