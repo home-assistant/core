c0f79b39676ab3d302fa4455a93fddba946a874b
J. Nick Koston <nick@koston.org>
2022-07-05 13:27:14 -0500

    revert tomorrowio

--- homeassistant/components/tomorrowio/__init__.py
+++ homeassistant/components/tomorrowio/__init__.py
@@ -206,7 +206,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await coordinator.async_setup_entry(entry)
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
 
