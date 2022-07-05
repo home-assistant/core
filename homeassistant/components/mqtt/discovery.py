f8b96103687d66819c2f65c6416db4f19b59afde
J. Nick Koston <nick@koston.org>
2022-07-05 08:32:30 -0500

    adjust missed imports

--- homeassistant/components/mqtt/discovery.py
+++ homeassistant/components/mqtt/discovery.py
@@ -17,6 +17,7 @@ from homeassistant.helpers.dispatcher import (
     async_dispatcher_send,
 )
 from homeassistant.helpers.json import json_loads
+from homeassistant.helpers.service_info.mqtt import MqttServiceInfo
 from homeassistant.loader import async_get_mqtt
 
 from .. import mqtt
@@ -268,7 +269,7 @@ async def async_start(  # noqa: C901
                 if key not in hass.data[INTEGRATION_UNSUBSCRIBE]:
                     return
 
-                data = mqtt.MqttServiceInfo(
+                data = MqttServiceInfo(
                     topic=msg.topic,
                     payload=msg.payload,
                     qos=msg.qos,
