8b01c132c1c441c615ad8ddec11d5e15bab74720
Arne Mauer <arne.mauer@gmail.com>
2022-07-07 20:28:33 +0200

    Ikea Starkvind support all models (#74615)
    * Add Particulate Matter 2.5 of ZCL concentration clusters to ZHA component
    
    * Fixed black and flake8 test
    
    * New sensors and manufacturer cluster to support IKEA STARKVIND (with quirk)
    
    * Fix multi_match for FilterLifeTime, device_run_time, filter_run_time sensors for Ikea starkvind
    
    * Remove model match because sensors are matched with manufacturer channel
    
    * Update manufacturerspecific.py
    
    * Update number.py

--- homeassistant/components/zha/sensor.py
+++ homeassistant/components/zha/sensor.py
@@ -810,7 +810,7 @@ class TimeLeft(Sensor, id_suffix="time_left"):
     _unit = TIME_MINUTES
 
 
-@MULTI_MATCH(channel_names="ikea_airpurifier", models={"STARKVIND Air purifier"})
+@MULTI_MATCH(channel_names="ikea_airpurifier")
 class IkeaDeviceRunTime(Sensor, id_suffix="device_run_time"):
     """Sensor that displays device run time (in minutes)."""
 
@@ -820,7 +820,7 @@ class IkeaDeviceRunTime(Sensor, id_suffix="device_run_time"):
     _unit = TIME_MINUTES
 
 
-@MULTI_MATCH(channel_names="ikea_airpurifier", models={"STARKVIND Air purifier"})
+@MULTI_MATCH(channel_names="ikea_airpurifier")
 class IkeaFilterRunTime(Sensor, id_suffix="filter_run_time"):
     """Sensor that displays run time of the current filter (in minutes)."""
 
