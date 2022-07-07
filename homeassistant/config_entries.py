ad82270f6ca7b124256470ff780e37b4ec908028
epenet <6771947+epenet@users.noreply.github.com>
2022-07-07 13:25:34 +0000

    Adjust config_entries

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -842,7 +842,9 @@ class ConfigEntries:
         self._hass_config = hass_config
         self._entries: dict[str, ConfigEntry] = {}
         self._domain_index: dict[str, list[str]] = {}
-        self._store = storage.Store(hass, STORAGE_VERSION, STORAGE_KEY)
+        self._store: storage.Store[dict[str, list[dict[str, Any]]]] = storage.Store(
+            hass, STORAGE_VERSION, STORAGE_KEY
+        )
         EntityRegistryDisabledHandler(hass).async_setup()
 
     @callback
