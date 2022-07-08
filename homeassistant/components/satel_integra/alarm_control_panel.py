c5158570691b11b33876ab40611fed9cc5b23cd0
Tom Matheussen <tmatheussen@avia-gis.com>
2022-07-08 10:21:05 +0200

    Remove trailing whitespace

--- homeassistant/components/satel_integra/alarm_control_panel.py
+++ homeassistant/components/satel_integra/alarm_control_panel.py
@@ -74,7 +74,7 @@ class SatelIntegraAlarmPanel(alarm.AlarmControlPanelEntity):
         self._attr_name = name
         self._arm_home_mode = arm_home_mode
         self._partition_id = partition_id
-        self._satel = controller        
+        self._satel = controller
         self._attr_unique_id = f"{partition_id}"
 
     async def async_added_to_hass(self) -> None:
