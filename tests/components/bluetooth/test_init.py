f45a544cf54adbf34deb8a4433480d2cf42a1cc0
J. Nick Koston <nick@koston.org>
2022-07-08 13:43:29 -0500

    more tests

--- tests/components/bluetooth/test_init.py
+++ tests/components/bluetooth/test_init.py
@@ -1,34 +1,49 @@
 """Tests for the Bluetooth integration."""
 from unittest.mock import patch
 
+from bleak.backends.scanner import AdvertisementData, BLEDevice
 import pytest
 
 from homeassistant.components import bluetooth
-from homeassistant.components.bluetooth.models import HaBleakScanner
+from homeassistant.components.bluetooth import (
+    BluetoothChange,
+    BluetoothServiceInfo,
+    models,
+)
 from homeassistant.const import EVENT_HOMEASSISTANT_STARTED, EVENT_HOMEASSISTANT_STOP
 from homeassistant.generated import bluetooth as bt_gen
 from homeassistant.setup import async_setup_component
 
 
 @pytest.fixture
-def mock_bleak_scanner():
+def mock_bleak_scanner_start():
     """Fixture to mock starting the bleak scanner."""
-
-    class MockedBleakScanner(HaBleakScanner):
-        """Mocked BleakScanner."""
-
-        async def start(self):
-            """Start the scanner."""
-            pass
-
     with patch(
-        "homeassistant.components.bluetooth.HaBleakScanner",
-        side_effect=MockedBleakScanner,
-    ) as mock_bleak_scanner:
-        yield mock_bleak_scanner
+        "homeassistant.components.bluetooth.HaBleakScanner.start",
+    ) as mock_bleak_scanner_start:
+        yield mock_bleak_scanner_start
 
 
-async def test_setup(hass, mock_bleak_scanner):
+async def test_setup_and_stop(hass, mock_bleak_scanner_start):
+    """Test configured options for a device are loaded via config entry."""
+    mock_bt = [
+        {"domain": "switchbot", "service_uuid": "cba20d00-224d-11e6-9fb8-0002a5d5c51b"}
+    ]
+    with patch.object(bt_gen, "BLUETOOTH", mock_bt), patch.object(
+        hass.config_entries.flow, "async_init"
+    ):
+        assert await async_setup_component(
+            hass, bluetooth.DOMAIN, {bluetooth.DOMAIN: {}}
+        )
+        hass.bus.async_fire(EVENT_HOMEASSISTANT_STARTED)
+        await hass.async_block_till_done()
+
+    hass.bus.async_fire(EVENT_HOMEASSISTANT_STOP)
+    await hass.async_block_till_done()
+    assert len(mock_bleak_scanner_start.mock_calls) == 1
+
+
+async def test_discovery(hass, mock_bleak_scanner_start):
     """Test configured options for a device are loaded via config entry."""
     mock_bt = [
         {"domain": "switchbot", "service_uuid": "cba20d00-224d-11e6-9fb8-0002a5d5c51b"}
@@ -42,14 +57,63 @@ async def test_setup(hass, mock_bleak_scanner):
         hass.bus.async_fire(EVENT_HOMEASSISTANT_STARTED)
         await hass.async_block_till_done()
 
-    hass.bus.async_fire(EVENT_HOMEASSISTANT_STOP)
-    await hass.async_block_till_done()
-    assert len(mock_bleak_scanner.mock_calls) == 1
-    expected_flow_calls = 0
-    for matching_components in mock_bt:
-        domains = set()
-        for component in matching_components:
-            if len(component) == 1:
-                domains.add(component["domain"])
-        expected_flow_calls += len(domains)
-    assert len(mock_config_flow.mock_calls) == expected_flow_calls
+        assert len(mock_bleak_scanner_start.mock_calls) == 1
+
+        switchbot_device = BLEDevice("44:44:33:11:23:45", "wohand")
+        switchbot_adv = AdvertisementData(
+            local_name="wohand", service_uuids=["cba20d00-224d-11e6-9fb8-0002a5d5c51b"]
+        )
+
+        models.HA_BLEAK_SCANNER._callback(switchbot_device, switchbot_adv)
+        await hass.async_block_till_done()
+
+    assert len(mock_config_flow.mock_calls) == 1
+    assert mock_config_flow.mock_calls[0][1][0] == "switchbot"
+
+
+async def test_register_callbacks(hass, mock_bleak_scanner_start):
+    """Test configured options for a device are loaded via config entry."""
+    mock_bt = []
+    callbacks = []
+
+    def _fake_subscriber(
+        service_info: BluetoothServiceInfo, change: BluetoothChange
+    ) -> None:
+        """Fake subscriber for the BleakScanner."""
+        callbacks.append((service_info, change))
+
+    with patch.object(bt_gen, "BLUETOOTH", mock_bt), patch.object(
+        hass.config_entries.flow, "async_init"
+    ):
+        assert await async_setup_component(
+            hass, bluetooth.DOMAIN, {bluetooth.DOMAIN: {}}
+        )
+        hass.bus.async_fire(EVENT_HOMEASSISTANT_STARTED)
+        await hass.async_block_till_done()
+
+        bluetooth.async_register_callback(
+            hass,
+            _fake_subscriber,
+            {"service_uuids": {"cba20d00-224d-11e6-9fb8-0002a5d5c51b"}},
+        )
+
+        assert len(mock_bleak_scanner_start.mock_calls) == 1
+
+        switchbot_device = BLEDevice("44:44:33:11:23:45", "wohand")
+        switchbot_adv = AdvertisementData(
+            local_name="wohand",
+            service_uuids=["cba20d00-224d-11e6-9fb8-0002a5d5c51b"],
+            manufacturer_data={89: b"\xd8.\xad\xcd\r\x85"},
+            service_data={"00000d00-0000-1000-8000-00805f9b34fb": b"H\x10c"},
+        )
+
+        models.HA_BLEAK_SCANNER._callback(switchbot_device, switchbot_adv)
+        await hass.async_block_till_done()
+
+    assert len(callbacks) == 1
+
+    service_info: BluetoothServiceInfo = callbacks[0][0]
+
+    assert service_info.name == "wohand"
+    assert service_info.manufacturer == "Nordic Semiconductor ASA"
+    assert service_info.manufacturer_id == 89
