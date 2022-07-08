df5df9738f6a3489ad25476c4f193d78ff0586a2
J. Nick Koston <nick@koston.org>
2022-07-08 13:28:56 -0500

    cover

--- tests/components/bluetooth/test_usage.py
+++ tests/components/bluetooth/test_usage.py
@@ -1,69 +1,22 @@
 """Tests for the Bluetooth integration."""
-from unittest.mock import Mock, patch
 
-import zeroconf
+from unittest.mock import MagicMock
 
-from homeassistant.components.zeroconf import async_get_instance
-from homeassistant.components.zeroconf.usage import install_multiple_zeroconf_catcher
-from homeassistant.setup import async_setup_component
+import bleak
 
-DOMAIN = "zeroconf"
+from homeassistant.components.bluetooth import models
+from homeassistant.components.bluetooth.models import HaBleakScannerWrapper
+from homeassistant.components.bluetooth.usage import install_multiple_bleak_catcher
 
 
-async def test_multiple_zeroconf_instances(
-    hass, mock_async_zeroconf, mock_zeroconf, caplog
-):
+async def test_multiple_bleak_scanner_instances(hass):
     """Test creating multiple zeroconf throws without an integration."""
-    assert await async_setup_component(hass, DOMAIN, {DOMAIN: {}})
+    assert models.HA_BLEAK_SCANNER is None
+    mock_scanner = MagicMock()
 
-    zeroconf_instance = await async_get_instance(hass)
+    install_multiple_bleak_catcher(mock_scanner)
 
-    install_multiple_zeroconf_catcher(zeroconf_instance)
+    instance = bleak.BleakScanner()
 
-    new_zeroconf_instance = zeroconf.Zeroconf()
-    assert new_zeroconf_instance == zeroconf_instance
-
-    assert "Zeroconf" in caplog.text
-
-
-async def test_multiple_zeroconf_instances_gives_shared(
-    hass, mock_async_zeroconf, mock_zeroconf, caplog
-):
-    """Test creating multiple zeroconf gives the shared instance to an integration."""
-    assert await async_setup_component(hass, DOMAIN, {DOMAIN: {}})
-
-    zeroconf_instance = await async_get_instance(hass)
-
-    install_multiple_zeroconf_catcher(zeroconf_instance)
-
-    correct_frame = Mock(
-        filename="/config/custom_components/burncpu/light.py",
-        lineno="23",
-        line="self.light.is_on",
-    )
-    with patch(
-        "homeassistant.helpers.frame.extract_stack",
-        return_value=[
-            Mock(
-                filename="/home/dev/homeassistant/core.py",
-                lineno="23",
-                line="do_something()",
-            ),
-            correct_frame,
-            Mock(
-                filename="/home/dev/homeassistant/components/zeroconf/usage.py",
-                lineno="23",
-                line="self.light.is_on",
-            ),
-            Mock(
-                filename="/home/dev/mdns/lights.py",
-                lineno="2",
-                line="something()",
-            ),
-        ],
-    ):
-        assert zeroconf.Zeroconf() == zeroconf_instance
-
-    assert "custom_components/burncpu/light.py" in caplog.text
-    assert "23" in caplog.text
-    assert "self.light.is_on" in caplog.text
+    assert isinstance(instance, HaBleakScannerWrapper)
+    assert models.HA_BLEAK_SCANNER is mock_scanner
