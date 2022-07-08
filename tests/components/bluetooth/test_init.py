75244c7e65296474874b1991dfac65330c9be363
J. Nick Koston <nick@koston.org>
2022-07-08 14:10:57 -0500

    more tests

--- tests/components/bluetooth/test_init.py
+++ tests/components/bluetooth/test_init.py
@@ -1,6 +1,7 @@
 """Tests for the Bluetooth integration."""
 from unittest.mock import patch
 
+import bleak
 from bleak.backends.scanner import AdvertisementData, BLEDevice
 import pytest
 
@@ -14,13 +15,20 @@ from homeassistant.const import EVENT_HOMEASSISTANT_STARTED, EVENT_HOMEASSISTANT
 from homeassistant.setup import async_setup_component
 
 
-@pytest.fixture
+@pytest.fixture()
 def mock_bleak_scanner_start():
     """Fixture to mock starting the bleak scanner."""
+    scanner = bleak.BleakScanner
+    models.HA_BLEAK_SCANNER = None
+    models.HaBleakScanner._history.clear()
+
     with patch(
         "homeassistant.components.bluetooth.HaBleakScanner.start",
     ) as mock_bleak_scanner_start:
         yield mock_bleak_scanner_start
+
+    bleak.BleakScanner = scanner
+    models.HaBleakScanner._history.clear()
 
 
 async def test_setup_and_stop(hass, mock_bleak_scanner_start):
@@ -58,6 +66,14 @@ async def test_discovery_match_by_service_uuid(hass, mock_bleak_scanner_start):
 
         assert len(mock_bleak_scanner_start.mock_calls) == 1
 
+        wrong_device = BLEDevice("44:44:33:11:23:45", "wrong_name")
+        wrong_adv = AdvertisementData(local_name="wrong_name", service_uuids=[])
+
+        models.HA_BLEAK_SCANNER._callback(wrong_device, wrong_adv)
+        await hass.async_block_till_done()
+
+        assert len(mock_config_flow.mock_calls) == 0
+
         switchbot_device = BLEDevice("44:44:33:11:23:45", "wohand")
         switchbot_adv = AdvertisementData(
             local_name="wohand", service_uuids=["cba20d00-224d-11e6-9fb8-0002a5d5c51b"]
@@ -66,8 +82,8 @@ async def test_discovery_match_by_service_uuid(hass, mock_bleak_scanner_start):
         models.HA_BLEAK_SCANNER._callback(switchbot_device, switchbot_adv)
         await hass.async_block_till_done()
 
-    assert len(mock_config_flow.mock_calls) == 1
-    assert mock_config_flow.mock_calls[0][1][0] == "switchbot"
+        assert len(mock_config_flow.mock_calls) == 1
+        assert mock_config_flow.mock_calls[0][1][0] == "switchbot"
 
 
 async def test_discovery_match_by_local_name(hass, mock_bleak_scanner_start):
@@ -84,14 +100,22 @@ async def test_discovery_match_by_local_name(hass, mock_bleak_scanner_start):
 
         assert len(mock_bleak_scanner_start.mock_calls) == 1
 
+        wrong_device = BLEDevice("44:44:33:11:23:45", "wrong_name")
+        wrong_adv = AdvertisementData(local_name="wrong_name", service_uuids=[])
+
+        models.HA_BLEAK_SCANNER._callback(wrong_device, wrong_adv)
+        await hass.async_block_till_done()
+
+        assert len(mock_config_flow.mock_calls) == 0
+
         switchbot_device = BLEDevice("44:44:33:11:23:45", "wohand")
         switchbot_adv = AdvertisementData(local_name="wohand", service_uuids=[])
 
         models.HA_BLEAK_SCANNER._callback(switchbot_device, switchbot_adv)
         await hass.async_block_till_done()
 
-    assert len(mock_config_flow.mock_calls) == 1
-    assert mock_config_flow.mock_calls[0][1][0] == "switchbot"
+        assert len(mock_config_flow.mock_calls) == 1
+        assert mock_config_flow.mock_calls[0][1][0] == "switchbot"
 
 
 async def test_discovery_match_by_manufacturer_id_and_first_byte(
@@ -112,16 +136,36 @@ async def test_discovery_match_by_manufacturer_id_and_first_byte(
 
         assert len(mock_bleak_scanner_start.mock_calls) == 1
 
-        switchbot_device = BLEDevice("44:44:33:11:23:45", "lock")
-        switchbot_adv = AdvertisementData(
+        hkc_device = BLEDevice("44:44:33:11:23:45", "lock")
+        hkc_adv = AdvertisementData(
             local_name="lock", service_uuids=[], manufacturer_data={76: b"\x06"}
         )
 
-        models.HA_BLEAK_SCANNER._callback(switchbot_device, switchbot_adv)
+        models.HA_BLEAK_SCANNER._callback(hkc_device, hkc_adv)
         await hass.async_block_till_done()
 
-    assert len(mock_config_flow.mock_calls) == 1
-    assert mock_config_flow.mock_calls[0][1][0] == "homekit_controller"
+        assert len(mock_config_flow.mock_calls) == 1
+        assert mock_config_flow.mock_calls[0][1][0] == "homekit_controller"
+
+        mock_config_flow.reset_mock()
+        not_hkc_device = BLEDevice("44:44:33:11:23:45", "lock")
+        not_hkc_adv = AdvertisementData(
+            local_name="lock", service_uuids=[], manufacturer_data={76: b"\x02"}
+        )
+
+        models.HA_BLEAK_SCANNER._callback(not_hkc_device, not_hkc_adv)
+        await hass.async_block_till_done()
+
+        assert len(mock_config_flow.mock_calls) == 0
+        not_apple_device = BLEDevice("44:44:33:11:23:45", "lock")
+        not_apple_adv = AdvertisementData(
+            local_name="lock", service_uuids=[], manufacturer_data={21: b"\x02"}
+        )
+
+        models.HA_BLEAK_SCANNER._callback(not_apple_device, not_apple_adv)
+        await hass.async_block_till_done()
+
+        assert len(mock_config_flow.mock_calls) == 0
 
 
 async def test_register_callbacks(hass, mock_bleak_scanner_start):
