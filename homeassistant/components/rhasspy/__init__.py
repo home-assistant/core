7cfc0b2c4c4b452c926d1d0c385e21c35b01c59c
Paulus Schoutsen <balloob@gmail.com>
2022-07-11 08:24:14 -0700

    Remove __all__ and add config flow description

--- homeassistant/components/rhasspy/__init__.py
+++ homeassistant/components/rhasspy/__init__.py
@@ -4,10 +4,6 @@ from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
-
-__all__ = ["DOMAIN", "async_setup_entry", "async_unload_entry"]
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Rhasspy from a config entry."""
