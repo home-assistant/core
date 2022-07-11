84461a4f06678d80281ae87160a8241b3815d840
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-07-11 09:26:43 +0200

    Remove unnecessary code

--- homeassistant/components/nextdns/switch.py
+++ homeassistant/components/nextdns/switch.py
@@ -216,7 +216,6 @@ class NextDnsSwitch(CoordinatorEntity[NextDnsSettingsUpdateCoordinator], SwitchE
         super().__init__(coordinator)
         self._attr_device_info = coordinator.device_info
         self._attr_unique_id = f"{coordinator.profile_id}_{description.key}"
-        self._attr_name = description.name
         self._attr_is_on = description.state(coordinator.data)
         self.entity_description: NextDnsSwitchEntityDescription = description
 
