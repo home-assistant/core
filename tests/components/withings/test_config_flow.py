17354b190659a7700e71ebf0037841c26746ae4f
epenet <6771947+epenet@users.noreply.github.com>
2022-07-11 12:02:29 +0000

    Add full context

--- tests/components/withings/test_config_flow.py
+++ tests/components/withings/test_config_flow.py
@@ -62,7 +62,12 @@ async def test_config_reauth_profile(
 
     result = await hass.config_entries.flow.async_init(
         const.DOMAIN,
-        context={"source": config_entries.SOURCE_REAUTH},
+        context={
+            "source": config_entries.SOURCE_REAUTH,
+            "entry_id": config_entry.entry_id,
+            "title_placeholders": {"name": config_entry.title},
+            "unique_id": config_entry.unique_id,
+        },
         data={"profile": "person0"},
     )
     assert result
