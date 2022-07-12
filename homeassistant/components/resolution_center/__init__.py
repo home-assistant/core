c78404a3d60dc4a0f6fc7673d772387ff809cd07
Erik Montnemery <erik@montnemery.com>
2022-07-12 19:26:06 +0200

    Teach resolution center about fixing issues (#74694)

--- homeassistant/components/resolution_center/__init__.py
+++ homeassistant/components/resolution_center/__init__.py
@@ -4,16 +4,19 @@ from __future__ import annotations
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
-from . import websocket_api
+from . import issue_handler, websocket_api
 from .const import DOMAIN
-from .issue_handler import async_create_issue, async_delete_issue
+from .issue_handler import ResolutionCenterFlow, async_create_issue, async_delete_issue
 from .issue_registry import async_load as async_load_issue_registry
 
-__all__ = ["DOMAIN", "async_create_issue", "async_delete_issue"]
+__all__ = ["DOMAIN", "ResolutionCenterFlow", "async_create_issue", "async_delete_issue"]
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up Resolution Center."""
+    hass.data[DOMAIN] = {}
+
+    issue_handler.async_setup(hass)
     websocket_api.async_setup(hass)
     await async_load_issue_registry(hass)
 
