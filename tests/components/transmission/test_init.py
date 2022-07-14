78f0d4ea6c097b9d8cc6e8bfd9c02f72d583e893
avee87 <6134677+avee87@users.noreply.github.com>
2022-07-14 21:35:49 +0000

    tests

--- tests/components/transmission/test_init.py
+++ tests/components/transmission/test_init.py
@@ -5,9 +5,13 @@ from unittest.mock import MagicMock, patch
 import pytest
 from transmission_rpc.error import TransmissionError
 
+from homeassistant.components import transmission
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.components.transmission.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from . import MOCK_CONFIG_DATA
 
@@ -73,3 +77,51 @@ async def test_unload_entry(hass: HomeAssistant) -> None:
 
     assert entry.state is ConfigEntryState.NOT_LOADED
     assert not hass.data[DOMAIN]
+
+
+@pytest.mark.parametrize(
+    "domain,old_unique_id,key",
+    [
+        (SENSOR_DOMAIN, "0.0.0.0-Transmission Down Speed", "download"),
+        (SENSOR_DOMAIN, "0.0.0.0-Transmission Up Speed", "upload"),
+        (SENSOR_DOMAIN, "0.0.0.0-Transmission Status", "status"),
+        (SENSOR_DOMAIN, "0.0.0.0-Transmission Active Torrents", "active_torrents"),
+        (SENSOR_DOMAIN, "0.0.0.0-Transmission Paused Torrents", "paused_torrents"),
+        (SENSOR_DOMAIN, "0.0.0.0-Transmission Total Torrents", "total_torrents"),
+        (
+            SENSOR_DOMAIN,
+            "0.0.0.0-Transmission Completed Torrents",
+            "completed_torrents",
+        ),
+        (SENSOR_DOMAIN, "0.0.0.0-Transmission Started Torrents", "started_torrents"),
+        (SWITCH_DOMAIN, "0.0.0.0-Transmission Switch", "on_off"),
+        (SWITCH_DOMAIN, "0.0.0.0-Transmission Turtle Mode", "turtle_mode"),
+    ],
+)
+async def test_migrate_unique_id(hass, domain, old_unique_id: str, key: str):
+    """Test unique id migration."""
+    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG_DATA)
+    entry.add_to_hass(hass)
+
+    ent_reg = er.async_get(hass)
+
+    entity: er.RegistryEntry = ent_reg.async_get_or_create(
+        suggested_object_id=f"my_{domain}",
+        disabled_by=None,
+        domain=domain,
+        platform=transmission.DOMAIN,
+        unique_id=old_unique_id,
+        config_entry=entry,
+    )
+    assert entity.unique_id == old_unique_id
+
+    assert await transmission.async_setup_entry(hass, entry) is True
+
+    new_unique_id = f"{entry.entry_id}-{key}"
+    assert (
+        ent_reg.async_get_entity_id(domain, transmission.DOMAIN, old_unique_id) is None
+    )
+    assert (
+        ent_reg.async_get_entity_id(domain, transmission.DOMAIN, new_unique_id)
+        == f"{domain}.my_{domain}"
+    )
