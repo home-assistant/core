8dc235d46b6c81fab265818bd134e06e020ac265
Franck Nijhof <git@frenck.dev>
2022-07-14 23:19:11 +0200

    Fix filenames

--- homeassistant/components/verisure/config_flow.py
+++ homeassistant/components/verisure/config_flow.py
@@ -57,7 +57,7 @@ class VerisureConfigFlowHandler(ConfigFlow, domain=DOMAIN):
                 username=self.email,
                 password=self.password,
                 cookieFileName=self.hass.config.path(
-                    STORAGE_DIR, f"verisure-{user_input[CONF_EMAIL]}"
+                    STORAGE_DIR, f"verisure_{user_input[CONF_EMAIL]}"
                 ),
             )
 
