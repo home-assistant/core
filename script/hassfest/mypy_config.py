abe235b787b11713ba523bef9a6820abc99a0084
epenet <6771947+epenet@users.noreply.github.com>
2022-07-14 21:40:39 +0200

    Remove evohome from mypy ignore list (#75175)

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -18,7 +18,6 @@ from .model import Config, Integration
 IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.cloud.client",
     "homeassistant.components.cloud.http_api",
-    "homeassistant.components.evohome",
     "homeassistant.components.sonos",
     "homeassistant.components.sonos.alarms",
     "homeassistant.components.sonos.binary_sensor",
