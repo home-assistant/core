0ad3978ab83c546de4fcfd14aa4904a417b34ddd
epenet <6771947+epenet@users.noreply.github.com>
2022-07-14 13:46:37 +0200

    Remove evohome climate from mypy ignore list (#75169)
    * Remove evohome climate from mypy ignore list
    
    * Raise error

--- script/hassfest/mypy_config.py
+++ script/hassfest/mypy_config.py
@@ -19,7 +19,6 @@ IGNORED_MODULES: Final[list[str]] = [
     "homeassistant.components.cloud.client",
     "homeassistant.components.cloud.http_api",
     "homeassistant.components.evohome",
-    "homeassistant.components.evohome.climate",
     "homeassistant.components.minecraft_server",
     "homeassistant.components.minecraft_server.helpers",
     "homeassistant.components.minecraft_server.sensor",
