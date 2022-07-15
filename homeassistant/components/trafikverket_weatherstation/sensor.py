7b8158581bc2413a942e6a872071067b0c802a41
G Johansson <goran.johansson@shiftit.se>
2022-07-15 04:31:43 +0000

    Fix name

--- homeassistant/components/trafikverket_weatherstation/sensor.py
+++ homeassistant/components/trafikverket_weatherstation/sensor.py
@@ -177,7 +177,6 @@ class TrafikverketWeatherStation(
         """Initialize the sensor."""
         super().__init__(coordinator)
         self.entity_description = description
-        self._attr_name = description.name
         self._attr_unique_id = f"{entry_id}_{description.key}"
         self._attr_device_info = DeviceInfo(
             entry_type=DeviceEntryType.SERVICE,
