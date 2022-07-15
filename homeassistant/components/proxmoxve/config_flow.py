bcf8b4934ee24742df0106b1c46db1dced958f4d
dougiteixeira <douglas.teixeira2@gmail.com>
2022-07-15 07:53:39 +0000

    Retrieves configuration flow data in case of error

--- homeassistant/components/proxmoxve/config_flow.py
+++ homeassistant/components/proxmoxve/config_flow.py
@@ -106,6 +106,33 @@ class ProxmoxVEConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
                     return self.async_create_entry(title="ProxmoxVE", data=self._config)
 
+            if errors:
+                data_schema = vol.Schema(
+                    {
+                        vol.Required(CONF_HOST, default=info.get(CONF_HOST, "")): str,
+                        vol.Required(
+                            CONF_USERNAME, default=info.get(CONF_USERNAME, "")
+                        ): str,
+                        vol.Required(
+                            CONF_PASSWORD, default=info.get(CONF_PASSWORD, "")
+                        ): str,
+                        vol.Required(
+                            CONF_REALM, default=info.get(CONF_REALM, DEFAULT_REALM)
+                        ): str,
+                        vol.Required(
+                            CONF_PORT, default=info.get(CONF_PORT, DEFAULT_PORT)
+                        ): int,
+                        vol.Required(
+                            CONF_VERIFY_SSL,
+                            default=info.get(CONF_VERIFY_SSL, DEFAULT_VERIFY_SSL),
+                        ): bool,
+                    }
+                )
+
+                return self.async_show_form(
+                    step_id="user", data_schema=data_schema, errors=errors
+                )
+
         info = {}
 
         if errors and is_import:
