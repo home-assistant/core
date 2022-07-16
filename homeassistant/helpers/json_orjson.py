8b04c429b0808eaa26ded1392414f739b861c713
Arnie97 <arnie97@gmail.com>
2022-07-16 10:45:12 +0800

    Move orjson implementation

--- homeassistant/helpers/json_orjson.py
+++ homeassistant/helpers/json_orjson.py
@@ -1,31 +1,12 @@
 """Helpers to help with encoding Home Assistant objects in JSON."""
-import datetime
-import json
+
 from pathlib import Path
-from typing import Any, Final
+from typing import Any
 
 import orjson
 
 JSON_ENCODE_EXCEPTIONS = (TypeError, ValueError)
 JSON_DECODE_EXCEPTIONS = (orjson.JSONDecodeError,)
-
-
-class JSONEncoder(json.JSONEncoder):
-    """JSONEncoder that supports Home Assistant objects."""
-
-    def default(self, o: Any) -> Any:
-        """Convert Home Assistant objects.
-
-        Hand other objects to the original method.
-        """
-        if isinstance(o, datetime.datetime):
-            return o.isoformat()
-        if isinstance(o, set):
-            return list(o)
-        if hasattr(o, "as_dict"):
-            return o.as_dict()
-
-        return json.JSONEncoder.default(self, o)
 
 
 def json_encoder_default(obj: Any) -> Any:
@@ -42,26 +23,6 @@ def json_encoder_default(obj: Any) -> Any:
     if isinstance(obj, Path):
         return obj.as_posix()
     raise TypeError
-
-
-class ExtendedJSONEncoder(JSONEncoder):
-    """JSONEncoder that supports Home Assistant objects and falls back to repr(o)."""
-
-    def default(self, o: Any) -> Any:
-        """Convert certain objects.
-
-        Fall back to repr(o).
-        """
-        if isinstance(o, datetime.timedelta):
-            return {"__type": str(type(o)), "total_seconds": o.total_seconds()}
-        if isinstance(o, datetime.datetime):
-            return o.isoformat()
-        if isinstance(o, (datetime.date, datetime.time)):
-            return {"__type": str(type(o)), "isoformat": o.isoformat()}
-        try:
-            return super().default(o)
-        except TypeError:
-            return {"__type": str(type(o)), "repr": repr(o)}
 
 
 def json_bytes(data: Any) -> bytes:
@@ -99,6 +60,3 @@ def json_dumps_sorted(data: Any) -> str:
 
 
 json_loads = orjson.loads
-
-
-JSON_DUMP: Final = json_dumps
