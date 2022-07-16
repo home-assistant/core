de3629d7930e15400db6102dc06e03ed4742c95f
Arnie97 <arnie97@gmail.com>
2022-07-16 10:44:39 +0800

    Move stdlib json implementation

--- homeassistant/helpers/json_stdlib.py
+++ homeassistant/helpers/json_stdlib.py
@@ -1,13 +1,14 @@
 """Helpers to help with encoding Home Assistant objects in JSON."""
+
 import datetime
 import json
-from pathlib import Path
-from typing import Any, Final
-
-import orjson
+from typing import Any
 
 JSON_ENCODE_EXCEPTIONS = (TypeError, ValueError)
-JSON_DECODE_EXCEPTIONS = (orjson.JSONDecodeError,)
+JSON_DECODE_EXCEPTIONS = (json.JSONDecodeError,)
+
+json_dumps = json.dumps
+json_loads = json.loads
 
 
 class JSONEncoder(json.JSONEncoder):
@@ -26,22 +27,6 @@ class JSONEncoder(json.JSONEncoder):
             return o.as_dict()
 
         return json.JSONEncoder.default(self, o)
-
-
-def json_encoder_default(obj: Any) -> Any:
-    """Convert Home Assistant objects.
-
-    Hand other objects to the original method.
-    """
-    if isinstance(obj, (set, tuple)):
-        return list(obj)
-    if isinstance(obj, float):
-        return float(obj)
-    if hasattr(obj, "as_dict"):
-        return obj.as_dict()
-    if isinstance(obj, Path):
-        return obj.as_posix()
-    raise TypeError
 
 
 class ExtendedJSONEncoder(JSONEncoder):
@@ -66,39 +51,9 @@ class ExtendedJSONEncoder(JSONEncoder):
 
 def json_bytes(data: Any) -> bytes:
     """Dump json bytes."""
-    return orjson.dumps(
-        data, option=orjson.OPT_NON_STR_KEYS, default=json_encoder_default
-    )
-
-
-def json_dumps(data: Any) -> str:
-    """Dump json string.
-
-    orjson supports serializing dataclasses natively which
-    eliminates the need to implement as_dict in many places
-    when the data is already in a dataclass. This works
-    well as long as all the data in the dataclass can also
-    be serialized.
-
-    If it turns out to be a problem we can disable this
-    with option |= orjson.OPT_PASSTHROUGH_DATACLASS and it
-    will fallback to as_dict
-    """
-    return orjson.dumps(
-        data, option=orjson.OPT_NON_STR_KEYS, default=json_encoder_default
-    ).decode("utf-8")
+    return json.dumps(data, cls=JSONEncoder).encode("utf-8")
 
 
 def json_dumps_sorted(data: Any) -> str:
     """Dump json string with keys sorted."""
-    return orjson.dumps(
-        data,
-        option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SORT_KEYS,
-        default=json_encoder_default,
-    ).decode("utf-8")
-
-
-json_loads = orjson.loads
-
-
-JSON_DUMP: Final = json_dumps
+    return json.dumps(data, cls=JSONEncoder, sort_keys=True)
