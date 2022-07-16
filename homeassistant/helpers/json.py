90789768e8bb76b971185bc1ccbf2bc1402df094
Arnie97 <arnie97@gmail.com>
2022-07-16 14:50:46 +0800

    Fix pylint complaints

--- homeassistant/helpers/json.py
+++ homeassistant/helpers/json.py
@@ -1,13 +1,25 @@
 """Helpers to help with encoding Home Assistant objects in JSON."""
 
+__all__ = [
+    "ExtendedJSONEncoder",
+    "JSONEncoder",
+    "JSON_DUMP",
+    "JSON_ENCODE_EXCEPTIONS",
+    "JSON_DECODE_EXCEPTIONS",
+    "json_bytes",
+    "json_dumps",
+    "json_dumps_indent",
+    "json_dumps_indent_no_encoder",
+    "json_dumps_sorted",
+    "json_loads",
+]
+
 from typing import Final
 
 from .json_stdlib import JSONEncoder, ExtendedJSONEncoder
 
 try:
-    import orjson
-except ImportError:
-    from .json_stdlib import (
+    from .json_orjson import (
         JSON_ENCODE_EXCEPTIONS,
         JSON_DECODE_EXCEPTIONS,
         json_bytes,
@@ -17,8 +29,8 @@ except ImportError:
         json_dumps_sorted,
         json_loads,
     )
-else:
-    from .json_orjson import (
+except ImportError:
+    from .json_stdlib import (
         JSON_ENCODE_EXCEPTIONS,
         JSON_DECODE_EXCEPTIONS,
         json_bytes,
