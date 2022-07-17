d375619347c3be16c586330913be139e82634686
J. Nick Koston <nick@koston.org>
2022-07-17 09:35:16 -0500

    handle available change

--- homeassistant/components/homekit_controller/connection.py
+++ homeassistant/components/homekit_controller/connection.py
@@ -140,6 +140,7 @@ class HKDevice:
 
         self.pairing.dispatcher_connect(self.process_new_events)
         self.pairing.dispatcher_connect_config_changed(self.process_config_changed)
+        self.pairing.dispatcher_availability_changed(self.async_set_available_state)
 
     @property
     def entity_map(self) -> Accessories:
