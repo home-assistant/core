726baf7c4fc7d908dd4f5ba9383b6b3df12fb73a
Stienstra.Jan@gmail.com <Stienstra.Jan@gmail.com>
2022-07-20 17:54:51 +0000

    Remove config entry migration

--- homeassistant/components/jellyfin/__init__.py
+++ homeassistant/components/jellyfin/__init__.py
@@ -12,26 +12,15 @@ from .const import DATA_CLIENT, DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    """Migrate old entry."""
-    _LOGGER.debug("Migrating from version %s", config_entry.version)
-
-    if config_entry.version == 1:
-        unique_id = str(uuid.uuid4())
-        hass.config_entries.async_update_entry(config_entry, unique_id=unique_id)
-        config_entry.version = 2
-
-    _LOGGER.debug("Migration to version %s successful", config_entry.version)
-
-    return True
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Jellyfin from a config entry."""
     hass.data.setdefault(DOMAIN, {})
 
-    assert entry.unique_id is not None
+    if entry.unique_id is None:
+        unique_id = str(uuid.uuid4())
+        hass.config_entries.async_update_entry(entry, unique_id=unique_id)
 
+    assert entry.unique_id is not None
     client = create_client(entry.unique_id)
     try:
         await validate_input(hass, dict(entry.data), client)
