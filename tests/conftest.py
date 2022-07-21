7a08316cc8b9f81ad382699925010a4dd41c1471
J. Nick Koston <nick@koston.org>
2022-07-21 15:02:07 -0500

    adjust

--- tests/conftest.py
+++ tests/conftest.py
@@ -858,6 +858,15 @@ def mock_integration_frame():
         yield correct_frame
 
 
+@pytest.fixture(name="enable_bluetooh")
+async def mock_enable_bluetooth(hass, mock_bleak_scanner_start):
+    """Fixture to mock starting the bleak scanner."""
+    entry = MockConfigEntry(domain="bluetooth")
+    entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+
 @pytest.fixture(name="mock_bleak_scanner_start")
 def mock_bleak_scanner_start():
     """Fixture to mock starting the bleak scanner."""
