4c0e9d066c3b1f423cc13ce49f7f6177834f4775
J. Nick Koston <nick@koston.org>
2022-07-21 09:46:23 -0500

    drop setup

--- homeassistant/components/sensorpush/__init__.py
+++ homeassistant/components/sensorpush/__init__.py
@@ -36,7 +36,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         """Update data from SensorPush Bluetooth."""
         return sensor_update_to_bluetooth_data_update(data.update(service_info))
 
-    coordinator = hass.data.setdefault(DOMAIN, {})[
+    hass.data.setdefault(DOMAIN, {})[
         entry.entry_id
     ] = PassiveBluetoothDataUpdateCoordinator(
         hass,
@@ -45,7 +45,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         address=address,
     )
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(coordinator.async_setup())
     return True
 
 
