3a5c6e17520d473ca15872b65f03001de91244be
J. Nick Koston <nick@koston.org>
2022-07-22 16:47:10 -0500

    fix test

--- tests/components/xiaomi_ble/test_config_flow.py
+++ tests/components/xiaomi_ble/test_config_flow.py
@@ -149,12 +149,26 @@ async def test_async_step_user_takes_precedence_over_discovery(hass):
     assert result["step_id"] == "bluetooth_confirm"
 
     with patch(
-        "homeassistant.components.sensorpush.async_setup_entry", return_value=True
+        "homeassistant.components.xiaomi_ble.config_flow.async_discovered_service_info",
+        return_value=[MMC_T201_1_SERVICE_INFO],
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+        )
+        assert result["type"] == FlowResultType.FORM
+
+    with patch(
+        "homeassistant.components.xiaomi_ble.async_setup_entry", return_value=True
     ):
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input={}
+            result["flow_id"],
+            user_input={"address": "00:81:F9:DD:6F:C1"},
         )
     assert result2["type"] == FlowResultType.CREATE_ENTRY
     assert result2["title"] == "MMC_T201_1"
     assert result2["data"] == {}
     assert result2["result"].unique_id == "00:81:F9:DD:6F:C1"
+
+    # Verify the original one was aborted
+    assert not hass.config_entries.flow.async_progress(DOMAIN)
