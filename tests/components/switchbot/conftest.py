ba365daf4ce39335cba6da5d5ad4afffa77e0496
J. Nick Koston <nick@koston.org>
2022-07-23 02:04:31 -0500

    adjust

--- tests/components/switchbot/conftest.py
+++ tests/components/switchbot/conftest.py
@@ -1,8 +1,6 @@
 """Define fixtures available for all tests."""
-import sys
-from unittest.mock import MagicMock, patch
 
-from pytest import fixture
+import pytest
 
 
 class MocGetSwitchbotDevices:
@@ -116,24 +114,6 @@ class MocGetSwitchbotDevices:
         return None
 
 
-class MocNotConnectedError(Exception):
-    """Mock exception."""
-
-
-module = type(sys)("switchbot")
-module.GetSwitchbotDevices = MocGetSwitchbotDevices
-module.NotConnectedError = MocNotConnectedError
-sys.modules["switchbot"] = module
-
-
-@fixture
-def switchbot_config_flow(hass):
-    """Mock the bluepy api for easier config flow testing."""
-    with patch.object(MocGetSwitchbotDevices, "discover", return_value=True), patch(
-        "homeassistant.components.switchbot.config_flow.GetSwitchbotDevices"
-    ) as mock_switchbot:
-        instance = mock_switchbot.return_value
-
-        instance.discover = MagicMock(return_value=True)
-
-        yield mock_switchbot
+@pytest.fixture(autouse=True)
+def mock_bluetooth(enable_bluetooth):
+    """Auto mock bluetooth."""
