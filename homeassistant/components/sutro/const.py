026e47b0b8639d729c9554f44994e0661bdc0fd7
Yuri Dogandjiev <yuri.dogandjiev@live.com>
2022-07-24 16:24:41 -0700

    Update homeassistant/components/sutro/const.py
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/sutro/const.py
+++ homeassistant/components/sutro/const.py
@@ -22,12 +22,3 @@ PLATFORMS = [SENSOR]
 # Configuration and options
 CONF_TOKEN = "token"
 
-STARTUP_MESSAGE = f"""
--------------------------------------------------------------------
-{NAME}
-Version: {VERSION}
-This is a custom integration!
-If you have any issues with this you need to open an issue here:
-{ISSUE_URL}
--------------------------------------------------------------------
-"""
