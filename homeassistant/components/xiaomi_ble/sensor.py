da131beced42203630778aa1460c1767200f00ad
J. Nick Koston <nick@koston.org>
2022-07-23 19:33:47 -0500

    Split bluetooth coordinator into two classes (#75675)

--- homeassistant/components/xiaomi_ble/sensor.py
+++ homeassistant/components/xiaomi_ble/sensor.py
@@ -13,11 +13,11 @@ from xiaomi_ble import (
 )
 
 from homeassistant import config_entries
-from homeassistant.components.bluetooth.passive_update_coordinator import (
+from homeassistant.components.bluetooth.passive_update_processor import (
     PassiveBluetoothDataProcessor,
     PassiveBluetoothDataUpdate,
-    PassiveBluetoothDataUpdateCoordinator,
     PassiveBluetoothEntityKey,
+    PassiveBluetoothProcessorCoordinator,
     PassiveBluetoothProcessorEntity,
 )
 from homeassistant.components.sensor import (
@@ -155,7 +155,7 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the Xiaomi BLE sensors."""
-    coordinator: PassiveBluetoothDataUpdateCoordinator = hass.data[DOMAIN][
+    coordinator: PassiveBluetoothProcessorCoordinator = hass.data[DOMAIN][
         entry.entry_id
     ]
     data = XiaomiBluetoothDeviceData()
