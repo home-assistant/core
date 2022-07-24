721cac4afe68af393710f843779c613430fc5c01
J. Nick Koston <nick@koston.org>
2022-07-24 18:20:45 -0500

    fixes

--- homeassistant/components/bluetooth/util.py
+++ homeassistant/components/bluetooth/util.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import platform
 
-MAC_OS_BLUETOOTH_NAME = "corebluetooth"
+from .const import LINUX_DEFAULT_BLUETOOTH_ADAPTER, MACOS_DEFAULT_BLUETOOTH_ADAPTER
 
 
 async def async_get_bluetooth_adapters() -> list[str]:
@@ -11,9 +11,15 @@ async def async_get_bluetooth_adapters() -> list[str]:
     if platform.system() == "Windows":  # We don't have a good way to detect on windows
         return []
     if platform.system() == "Darwin":  # CoreBluetooth is built in on MacOS hardware
-        return [MAC_OS_BLUETOOTH_NAME]
+        return [MACOS_DEFAULT_BLUETOOTH_ADAPTER]
     from bluetooth_adapters import (  # pylint: disable=import-outside-toplevel
         get_bluetooth_adapters,
     )
 
-    return await get_bluetooth_adapters()
+    adapters = await get_bluetooth_adapters()
+    if LINUX_DEFAULT_BLUETOOTH_ADAPTER in adapters:
+        # The default adapter always needs to be the first in the list
+        # because that is how bleak works.
+        adapters.remove(LINUX_DEFAULT_BLUETOOTH_ADAPTER)
+        return [LINUX_DEFAULT_BLUETOOTH_ADAPTER, *adapters]
+    return adapters
