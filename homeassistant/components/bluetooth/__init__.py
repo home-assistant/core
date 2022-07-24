3cd8d644d0284160f168be24d7be94545f34e984
J. Nick Koston <nick@koston.org>
2022-07-24 12:30:09 -0500

    improve logging to avoid overly large messages

--- homeassistant/components/bluetooth/__init__.py
+++ homeassistant/components/bluetooth/__init__.py
@@ -323,9 +323,8 @@ class BluetoothManager:
             device, advertisement_data
         )
         _LOGGER.debug(
-            "Device detected: %s (%s) with advertisement_data: %s matched domains: %s",
+            "Device detected: %s with advertisement_data: %s matched domains: %s",
             device.address,
-            device.name,
             advertisement_data,
             matched_domains,
         )
