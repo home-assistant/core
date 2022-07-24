2aa6a2ec396c8f82199645c81cf3f567fa46beaf
J. Nick Koston <nick@koston.org>
2022-07-24 03:39:23 -0500

    Add Govee BLE integration (#75631)
    * Add Govee BLE integration
    
    * add missing files
    
    * remove test file not needed yet
    
    * fix
    
    * add bbq sensors
    
    * fixed lib
    
    * bump again to fix the names
    
    * fix discovery of the newer bbq devices
    
    * fix the test to test the right thing
    
    * verify no outstanding flows
    
    * only accept entities that match the platform
    
    * refactor
    
    * refactor
    
    * refactor
    
    * Refactor PassiveBluetoothDataUpdateCoordinator to support multiple platforms
    
    * cover
    
    * Update for new model
    
    * Update for new model
    
    * Update tests/components/govee_ble/test_sensor.py
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * purge dead code
    
    * backmerge from integration
    
    * Update docstring
    
    * Update docstring
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/generated/bluetooth.py
+++ homeassistant/generated/bluetooth.py
@@ -8,6 +8,33 @@ from __future__ import annotations
 
 BLUETOOTH: list[dict[str, str | int | list[int]]] = [
     {
+        "domain": "govee_ble",
+        "local_name": "Govee*"
+    },
+    {
+        "domain": "govee_ble",
+        "local_name": "GVH5*"
+    },
+    {
+        "domain": "govee_ble",
+        "local_name": "B5178*"
+    },
+    {
+        "domain": "govee_ble",
+        "manufacturer_id": 26589,
+        "service_uuid": "00008351-0000-1000-8000-00805f9b34fb"
+    },
+    {
+        "domain": "govee_ble",
+        "manufacturer_id": 18994,
+        "service_uuid": "00008551-0000-1000-8000-00805f9b34fb"
+    },
+    {
+        "domain": "govee_ble",
+        "manufacturer_id": 14474,
+        "service_uuid": "00008151-0000-1000-8000-00805f9b34fb"
+    },
+    {
         "domain": "homekit_controller",
         "manufacturer_id": 76,
         "manufacturer_data_start": [
