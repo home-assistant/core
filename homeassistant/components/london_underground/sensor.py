b071fdee82e69da37bbddffbd50927fe5fe3e364
Pedro Lamas <pedrolamas@gmail.com>
2022-07-25 09:15:32 +0100

    Removes unused callback import

--- homeassistant/components/london_underground/sensor.py
+++ homeassistant/components/london_underground/sensor.py
@@ -10,7 +10,7 @@ import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 from homeassistant.const import ATTR_ATTRIBUTION
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
