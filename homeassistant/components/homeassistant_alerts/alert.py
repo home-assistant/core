287949fe87ce4377e5a32fbd8b3ee3fa8bea9ac9
Paulus Schoutsen <balloob@gmail.com>
2022-07-25 22:36:24 -0700

    Extend DataUpdateCoordinator

--- homeassistant/components/homeassistant_alerts/alert.py
+++ homeassistant/components/homeassistant_alerts/alert.py
@@ -32,60 +32,14 @@ class IntegrationAlert:
 
 async def async_setup(hass: HomeAssistant) -> None:
     """Set up alerts."""
-    hass.data[DOMAIN]["alerts"] = set()
-    ha_version = AwesomeVersion(
-        __version__,
-        ensure_strategy=AwesomeVersionStrategy.CALVER,
-        find_first_match=False,
-    )
-
-    async def async_update_data() -> dict[str, IntegrationAlert]:
-        """Fetch data from API endpoint."""
-        response = await session.request(
-            "get",
-            "https://alerts.home-assistant.io/alerts.json",
-            timeout=aiohttp.ClientTimeout(total=10),
-        )
-        alerts = await response.json()
-
-        result = {}
-
-        for alert in alerts:
-            if "alert_url" not in alert or "integrations" not in alert:
-                continue
-
-            if "homeassistant" in alert:
-                if "affected_from_version" in alert["homeassistant"]:
-                    affected_from_version = AwesomeVersion(
-                        alert["homeassistant"]["affected_from_version"],
-                        find_first_match=False,
-                    )
-                    if ha_version < affected_from_version:
-                        continue
-                if "resolved_in_version" in alert["homeassistant"]:
-                    resolved_in_version = AwesomeVersion(
-                        alert["homeassistant"]["resolved_in_version"],
-                        find_first_match=False,
-                    )
-                    if ha_version >= resolved_in_version:
-                        continue
-
-            for integration in alert["integrations"]:
-                if "package" not in integration:
-                    continue
-                issue_id = f"{alert['filename']}_{integration['package']}"
-                result[issue_id] = IntegrationAlert(
-                    integration["package"], issue_id, alert["alert_url"]
-                )
-
-        return result
+    last_alerts = set()
 
     @callback
     def async_update_alerts() -> None:
         if not coordinator.last_update_success:
             return
 
-        old_alerts = hass.data[DOMAIN]["alerts"]
+        nonlocal last_alerts
 
         active_alerts = set()
 
@@ -104,19 +58,65 @@ async def async_setup(hass: HomeAssistant) -> None:
             )
             active_alerts.add(issue_id)
 
-        inactive_alerts = old_alerts - active_alerts
+        inactive_alerts = last_alerts - active_alerts
         for issue_id in inactive_alerts:
             async_delete_issue(hass, DOMAIN, issue_id)
 
-        hass.data[DOMAIN]["alerts"] = active_alerts
+        last_alerts = active_alerts
 
-    session = async_get_clientsession(hass)
-    coordinator = DataUpdateCoordinator[dict[str, IntegrationAlert]](
-        hass,
-        _LOGGER,
-        name=DOMAIN,
-        update_method=async_update_data,
-        update_interval=UPDATE_INTERVAL,
-    )
+    coordinator = AlertUpdateCoordinator(hass)
     coordinator.async_add_listener(async_update_alerts)
     await coordinator.async_refresh()
+
+
+class AlertUpdateCoordinator(DataUpdateCoordinator[dict[str, IntegrationAlert]]):
+    """Data fetcher for HA Alerts."""
+
+    def __init__(self, hass: HomeAssistant) -> None:
+        """Initialize the data updater."""
+        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=UPDATE_INTERVAL)
+        self.ha_version = AwesomeVersion(
+            __version__,
+            ensure_strategy=AwesomeVersionStrategy.CALVER,
+            find_first_match=False,
+        )
+
+    async def _async_update_data(self) -> dict[str, IntegrationAlert]:
+        response = await async_get_clientsession(self.hass).request(
+            "get",
+            "https://alerts.home-assistant.io/alerts.json",
+            timeout=aiohttp.ClientTimeout(total=10),
+        )
+        alerts = await response.json()
+
+        result = {}
+
+        for alert in alerts:
+            if "alert_url" not in alert or "integrations" not in alert:
+                continue
+
+            if "homeassistant" in alert:
+                if "affected_from_version" in alert["homeassistant"]:
+                    affected_from_version = AwesomeVersion(
+                        alert["homeassistant"]["affected_from_version"],
+                        find_first_match=False,
+                    )
+                    if self.ha_version < affected_from_version:
+                        continue
+                if "resolved_in_version" in alert["homeassistant"]:
+                    resolved_in_version = AwesomeVersion(
+                        alert["homeassistant"]["resolved_in_version"],
+                        find_first_match=False,
+                    )
+                    if self.ha_version >= resolved_in_version:
+                        continue
+
+            for integration in alert["integrations"]:
+                if "package" not in integration:
+                    continue
+                issue_id = f"{alert['filename']}_{integration['package']}"
+                result[issue_id] = IntegrationAlert(
+                    integration["package"], issue_id, alert["alert_url"]
+                )
+
+        return result
