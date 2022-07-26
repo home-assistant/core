b2e0e99d2bf6e628a4e078c99896c3fe8e8dc4cd
Rami Mosleh <engrbm87@gmail.com>
2022-07-26 12:03:38 +0000

    change to async_forward_entry_setups

--- homeassistant/components/glances/__init__.py
+++ homeassistant/components/glances/__init__.py
@@ -35,7 +35,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
 
     hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     return True
 
