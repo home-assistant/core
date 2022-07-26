0ff34f232c931367d5c221170bd2c31de65d07a6
Franck Nijhof <git@frenck.dev>
2022-07-26 22:42:19 +0200

    Add events to repairs issue registry changes (#75784)

--- tests/components/repairs/test_issue_registry.py
+++ tests/components/repairs/test_issue_registry.py
@@ -1,11 +1,14 @@
 """Test the repairs websocket API."""
 from homeassistant.components.repairs import async_create_issue, issue_registry
 from homeassistant.components.repairs.const import DOMAIN
-from homeassistant.components.repairs.issue_handler import async_ignore_issue
+from homeassistant.components.repairs.issue_handler import (
+    async_delete_issue,
+    async_ignore_issue,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
-from tests.common import flush_store
+from tests.common import async_capture_events, flush_store
 
 
 async def test_load_issues(hass: HomeAssistant) -> None:
@@ -33,7 +36,21 @@ async def test_load_issues(hass: HomeAssistant) -> None:
             "translation_key": "even_worse",
             "translation_placeholders": {"def": "456"},
         },
+        {
+            "breaks_in_ha_version": "2022.7",
+            "domain": "test",
+            "issue_id": "issue_3",
+            "is_fixable": True,
+            "learn_more_url": "https://checkboxrace.com",
+            "severity": "other",
+            "translation_key": "even_worse",
+            "translation_placeholders": {"def": "789"},
+        },
     ]
+
+    events = async_capture_events(
+        hass, issue_registry.EVENT_REPAIRS_ISSUE_REGISTRY_UPDATED
+    )
 
     for issue in issues:
         async_create_issue(
@@ -47,7 +64,45 @@ async def test_load_issues(hass: HomeAssistant) -> None:
             translation_key=issue["translation_key"],
             translation_placeholders=issue["translation_placeholders"],
         )
+
+    await hass.async_block_till_done()
+
+    assert len(events) == 3
+    assert events[0].data == {
+        "action": "create",
+        "domain": "test",
+        "issue_id": "issue_1",
+    }
+    assert events[1].data == {
+        "action": "create",
+        "domain": "test",
+        "issue_id": "issue_2",
+    }
+    assert events[2].data == {
+        "action": "create",
+        "domain": "test",
+        "issue_id": "issue_3",
+    }
+
     async_ignore_issue(hass, issues[0]["domain"], issues[0]["issue_id"], True)
+    await hass.async_block_till_done()
+
+    assert len(events) == 4
+    assert events[3].data == {
+        "action": "update",
+        "domain": "test",
+        "issue_id": "issue_1",
+    }
+
+    async_delete_issue(hass, issues[2]["domain"], issues[2]["issue_id"])
+    await hass.async_block_till_done()
+
+    assert len(events) == 5
+    assert events[4].data == {
+        "action": "remove",
+        "domain": "test",
+        "issue_id": "issue_3",
+    }
 
     registry: issue_registry.IssueRegistry = hass.data[issue_registry.DATA_REGISTRY]
     assert len(registry.issues) == 2
