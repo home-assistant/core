7f177d84f0aec86979d7a31d9b83fbe58c86f206
Hyralex <hyralex@gmail.com>
2022-07-28 21:16:36 +1200

    Add test for anthemav dispatcher signal

--- tests/components/anthemav/test_init.py
+++ tests/components/anthemav/test_init.py
@@ -1,8 +1,10 @@
 """Test the Anthem A/V Receivers config flow."""
-from unittest.mock import ANY, AsyncMock, patch
+from unittest.mock import ANY, AsyncMock, Mock, patch
 
 from homeassistant import config_entries
+from homeassistant.components.anthemav.const import ANTHEMAV_UDATE_SIGNAL
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
 from tests.common import MockConfigEntry
 
@@ -45,3 +47,32 @@ async def test_config_entry_not_ready(
         await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
         assert mock_config_entry.state is config_entries.ConfigEntryState.SETUP_RETRY
+
+
+async def test_anthemav_dispatcher_signal(
+    hass: HomeAssistant,
+    mock_connection_create: AsyncMock,
+    mock_anthemav: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test send update signal to dispatcher."""
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    # get the callback function that trigger the signal
+    avr_update_callback = mock_connection_create.call_args[1]["update_callback"]
+
+    mock_signal_received = Mock()
+
+    async_dispatcher_connect(
+        hass,
+        f"{ANTHEMAV_UDATE_SIGNAL}_{mock_config_entry.entry_id}",
+        mock_signal_received,
+    )
+
+    avr_update_callback("Fake Command")
+
+    await hass.async_block_till_done()
+
+    mock_signal_received.assert_called()
