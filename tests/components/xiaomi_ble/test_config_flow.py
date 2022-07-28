f03d1393dd20a408b5d2a8b0b7a63687d605787d
Jc2k <john.carr@unrouted.co.uk>
2022-07-28 19:10:37 +0100

    Fix Xiaomi BLE not detecting encryption for some devices (#75851)

--- tests/components/xiaomi_ble/test_config_flow.py
+++ tests/components/xiaomi_ble/test_config_flow.py
@@ -1,5 +1,6 @@
 """Test the Xiaomi config flow."""
 
+import asyncio
 from unittest.mock import patch
 
 from homeassistant import config_entries
@@ -9,9 +10,11 @@ from homeassistant.data_entry_flow import FlowResultType
 from . import (
     JTYJGD03MI_SERVICE_INFO,
     LYWSDCGQ_SERVICE_INFO,
+    MISSING_PAYLOAD_ENCRYPTED,
     MMC_T201_1_SERVICE_INFO,
     NOT_SENSOR_PUSH_SERVICE_INFO,
     YLKG07YL_SERVICE_INFO,
+    make_advertisement,
 )
 
 from tests.common import MockConfigEntry
@@ -36,6 +39,57 @@ async def test_async_step_bluetooth_valid_device(hass):
     assert result2["title"] == "MMC_T201_1"
     assert result2["data"] == {}
     assert result2["result"].unique_id == "00:81:F9:DD:6F:C1"
+
+
+async def test_async_step_bluetooth_valid_device_but_missing_payload(hass):
+    """Test discovery via bluetooth with a valid device but missing payload."""
+    with patch(
+        "homeassistant.components.xiaomi_ble.config_flow.async_process_advertisements",
+        side_effect=asyncio.TimeoutError(),
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_BLUETOOTH},
+            data=MISSING_PAYLOAD_ENCRYPTED,
+        )
+    assert result["type"] == FlowResultType.ABORT
+    assert result["reason"] == "not_supported"
+
+
+async def test_async_step_bluetooth_valid_device_but_missing_payload_then_full(hass):
+    """Test discovering a valid device. Payload is too short, but later we get full one."""
+
+    async def _async_process_advertisements(_hass, _callback, _matcher, _timeout):
+        service_info = make_advertisement(
+            "A4:C1:38:56:53:84",
+            b"XX\xe4\x16,\x84SV8\xc1\xa4+n\xf2\xe9\x12\x00\x00l\x88M\x9e",
+        )
+        assert _callback(service_info)
+        return service_info
+
+    with patch(
+        "homeassistant.components.xiaomi_ble.config_flow.async_process_advertisements",
+        _async_process_advertisements,
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_BLUETOOTH},
+            data=MISSING_PAYLOAD_ENCRYPTED,
+        )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "get_encryption_key_4_5"
+
+    with patch(
+        "homeassistant.components.xiaomi_ble.async_setup_entry", return_value=True
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input={"bindkey": "a115210eed7a88e50ad52662e732a9fb"},
+        )
+
+    assert result2["type"] == FlowResultType.CREATE_ENTRY
+    assert result2["data"] == {"bindkey": "a115210eed7a88e50ad52662e732a9fb"}
+    assert result2["result"].unique_id == "A4:C1:38:56:53:84"
 
 
 async def test_async_step_bluetooth_during_onboarding(hass):
@@ -285,6 +339,75 @@ async def test_async_step_user_with_found_devices(hass):
     assert result2["title"] == "LYWSDCGQ"
     assert result2["data"] == {}
     assert result2["result"].unique_id == "58:2D:34:35:93:21"
+
+
+async def test_async_step_user_short_payload(hass):
+    """Test setup from service info cache with devices found but short payloads."""
+    with patch(
+        "homeassistant.components.xiaomi_ble.config_flow.async_discovered_service_info",
+        return_value=[MISSING_PAYLOAD_ENCRYPTED],
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+        )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+    with patch(
+        "homeassistant.components.xiaomi_ble.config_flow.async_process_advertisements",
+        side_effect=asyncio.TimeoutError(),
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input={"address": "A4:C1:38:56:53:84"},
+        )
+    assert result2["type"] == FlowResultType.ABORT
+    assert result2["reason"] == "not_supported"
+
+
+async def test_async_step_user_short_payload_then_full(hass):
+    """Test setup from service info cache with devices found."""
+    with patch(
+        "homeassistant.components.xiaomi_ble.config_flow.async_discovered_service_info",
+        return_value=[MISSING_PAYLOAD_ENCRYPTED],
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+        )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+
+    async def _async_process_advertisements(_hass, _callback, _matcher, _timeout):
+        service_info = make_advertisement(
+            "A4:C1:38:56:53:84",
+            b"XX\xe4\x16,\x84SV8\xc1\xa4+n\xf2\xe9\x12\x00\x00l\x88M\x9e",
+        )
+        assert _callback(service_info)
+        return service_info
+
+    with patch(
+        "homeassistant.components.xiaomi_ble.config_flow.async_process_advertisements",
+        _async_process_advertisements,
+    ):
+        result1 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input={"address": "A4:C1:38:56:53:84"},
+        )
+    assert result1["type"] == FlowResultType.FORM
+    assert result1["step_id"] == "get_encryption_key_4_5"
+
+    with patch(
+        "homeassistant.components.xiaomi_ble.async_setup_entry", return_value=True
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input={"bindkey": "a115210eed7a88e50ad52662e732a9fb"},
+        )
+
+    assert result2["type"] == FlowResultType.CREATE_ENTRY
+    assert result2["title"] == "LYWSD02MMC"
+    assert result2["data"] == {"bindkey": "a115210eed7a88e50ad52662e732a9fb"}
 
 
 async def test_async_step_user_with_found_devices_v4_encryption(hass):
