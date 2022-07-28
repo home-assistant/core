fb6c5f36651a48629a0128dd34d8ded2ed536c58
Leonardo Figueiro <leoagfig@gmail.com>
2022-07-28 13:02:35 -0300

    Update support.py

--- homeassistant/components/wilight/support.py
+++ homeassistant/components/wilight/support.py
@@ -52,7 +52,10 @@ def wilight_trigger(value: Any) -> str | None:
 
 
 def wilight_to_hass_trigger(value: str | None) -> str | None:
-    """Convert wilight trigger to hass description."""
+    """Convert wilight trigger to hass description.
+    Ex: "12719001" -> "sun mon tue wed thu fri sat 19:00 On"
+        "00000000" -> "00:00 Off"
+    """
     if value is None:
         return value
 
@@ -63,21 +66,21 @@ def wilight_to_hass_trigger(value: str | None) -> str | None:
     if int(days[7:8]) == 1:
         desc += f"{week_days[6]} "
     if int(days[6:7]) == 1:
-        desc = desc + week_days[0] + " "
+        desc += f"{week_days[0]} "
     if int(days[5:6]) == 1:
-        desc = desc + week_days[1] + " "
+        desc += f"{week_days[1]} "
     if int(days[4:5]) == 1:
-        desc = desc + week_days[2] + " "
+        desc += f"{week_days[2]} "
     if int(days[3:4]) == 1:
-        desc = desc + week_days[3] + " "
+        desc += f"{week_days[3]} "
     if int(days[2:3]) == 1:
-        desc = desc + week_days[4] + " "
+        desc += f"{week_days[4]} "
     if int(days[1:2]) == 1:
-        desc = desc + week_days[5] + " "
-    desc = desc + value[3:5] + ":" + value[5:7] + " "
+        desc += f"{week_days[5]} "
+    desc += f"{value[3:5]}:{value[5:7]} "
     if int(value[7:8]) == 1:
-        desc = desc + "On"
+        desc += "On"
     else:
-        desc = desc + "Off"
+        desc += "Off"
 
     return desc
