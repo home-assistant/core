9338cb4c881b872fd953f1feaf918f91197436f0
epenet <6771947+epenet@users.noreply.github.com>
2022-08-01 10:55:16 +0000

    Adjust lock

--- homeassistant/components/deconz/lock.py
+++ homeassistant/components/deconz/lock.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Any, TypeVar, Union
+from typing import Any, Union
 
 from pydeconz.models.event import EventType
 from pydeconz.models.light.lock import Lock
@@ -15,8 +15,6 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .deconz_device import DeconzDevice
 from .gateway import get_gateway_from_config_entry
-
-_LockDeviceTypeT = TypeVar("_LockDeviceTypeT", bound=Union[DoorLock, Lock])
 
 
 async def async_setup_entry(
@@ -52,7 +50,7 @@ async def async_setup_entry(
     )
 
 
-class DeconzLock(DeconzDevice[_LockDeviceTypeT], LockEntity):
+class DeconzLock(DeconzDevice[Union[DoorLock, Lock]], LockEntity):
     """Representation of a deCONZ lock."""
 
     TYPE = DOMAIN
