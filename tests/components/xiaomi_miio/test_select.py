21c4717161cd946630f2305143a5062c51590e41
Kirmas <krosven@outlook.com>
2022-08-02 08:20:09 +0300

    use patching to avoid IO.

--- tests/components/xiaomi_miio/test_select.py
+++ tests/components/xiaomi_miio/test_select.py
@@ -2,11 +2,13 @@
 
 import enum
 import logging
+from unittest.mock import patch
 
 from arrow import utcnow
 import pytest
 
 from homeassistant.components.asuswrt.router import KEY_COORDINATOR
+from homeassistant.components.select import DOMAIN
 from homeassistant.components.select.const import (
     ATTR_OPTION,
     ATTR_OPTIONS,
@@ -20,16 +22,21 @@ from homeassistant.components.xiaomi_miio.const import (
     CONF_DEVICE,
     CONF_FLOW_TYPE,
     CONF_MAC,
-    DOMAIN,
+    DOMAIN as XIAOMI_DOMAIN,
     KEY_DEVICE,
 )
 from homeassistant.components.xiaomi_miio.select import (
-    MODEL_TO_ATTR_MAP,
-    SELECTOR_TYPES,
     AttributeEnumMapping,
     XiaomiMiioSelectDescription,
 )
-from homeassistant.const import ATTR_ENTITY_ID, CONF_MODEL
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    CONF_HOST,
+    CONF_MODEL,
+    CONF_TOKEN,
+    Platform,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
@@ -84,78 +91,85 @@ class DummySelectDevice:
         return True
 
 
-@pytest.fixture(autouse=True)
-async def setup_select(hass: HomeAssistant):
-    """Initialize setup xiaomi_miio airhumidifier select entity."""
-
-    dummy_device = DummySelectDevice("Dummy")
-
-    MODEL_TO_ATTR_MAP["Dummy"] = [
+TEST_MODEL_TO_ATTR_MAP: dict[str, list] = {
+    "Dummy": [
         AttributeEnumMapping("dummy", DummyEnum_0),
         AttributeEnumMapping("bad_name", DummyEnum_1),
     ]
-    SELECTOR_TYPES.append(
-        XiaomiMiioSelectDescription(
-            key="dummy",
-            attr_name="dummy",
-            name="Dummy",
-            set_method="set_dummy_value",
-            set_method_error_message="Set dummy value failed.",
-            options=("option_0", "option_1"),
-        )
-    )
-    SELECTOR_TYPES.append(
-        XiaomiMiioSelectDescription(
-            key="bad_name",
-            attr_name="bad_name",
-            name="Bad Name",
-            options_map={
-                "BadName_0": "New_0",
-                "BadName_1": "New_1",
-            },
-            set_method="set_bad_name_value",
-            set_method_error_message="Set bad name value failed.",
-            options=("new_0", "new_1"),
-        )
-    )
+}
 
-    hass.data.setdefault(DOMAIN, {})
-
-    config_entry = MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            CONF_FLOW_TYPE: CONF_DEVICE,
-            CONF_MODEL: "Dummy",
-            CONF_MAC: TEST_MAC,
+TEST_SELECTOR_TYPES = (
+    XiaomiMiioSelectDescription(
+        key="dummy",
+        attr_name="dummy",
+        name="Dummy",
+        set_method="set_dummy_value",
+        set_method_error_message="Set dummy value failed.",
+        options=("option_0", "option_1"),
+    ),
+    XiaomiMiioSelectDescription(
+        key="bad_name",
+        attr_name="bad_name",
+        name="Bad Name",
+        options_map={
+            "BadName_0": "New_0",
+            "BadName_1": "New_1",
         },
-        title="dummy",
-    )
-    config_entry.add_to_hass(hass)
+        set_method="set_bad_name_value",
+        set_method_error_message="Set bad name value failed.",
+        options=("new_0", "new_1"),
+    ),
+)
+
+
+async def mock_async_create_miio_device_and_coordinator(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> None:
+    """Set up a data coordinator and one miio device to service multiple entities."""
+    name = entry.title
+    update_method = _async_update_data_default
+
+    device = DummySelectDevice("Dummy")
 
     # Create update miio device and coordinator
     coordinator = DataUpdateCoordinator(
         hass,
         _LOGGER,
-        name=config_entry.title,
-        update_method=_async_update_data_default(hass, dummy_device),
+        name=name,
+        update_method=update_method(hass, device),
         # Polling interval. Will only be polled if there are subscribers.
         update_interval=UPDATE_INTERVAL,
     )
-    hass.data[DOMAIN][config_entry.entry_id] = {
-        KEY_DEVICE: dummy_device,
+    hass.data["device_for_test"] = device
+    hass.data[XIAOMI_DOMAIN][entry.entry_id] = {
+        KEY_DEVICE: device,
         KEY_COORDINATOR: coordinator,
     }
 
     # Trigger first data fetch
     await coordinator.async_config_entry_first_refresh()
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "select")
-    )
 
-    await hass.async_block_till_done()
+@pytest.fixture(autouse=True)
+async def setup_select(hass: HomeAssistant):
+    """Initialize setup xiaomi_miio airhumidifier select entity."""
 
-    return dummy_device
+    with patch(
+        "homeassistant.components.xiaomi_miio.select.MODEL_TO_ATTR_MAP",
+        TEST_MODEL_TO_ATTR_MAP,
+    ), patch(
+        "homeassistant.components.xiaomi_miio.select.SELECTOR_TYPES",
+        TEST_SELECTOR_TYPES,
+    ), patch(
+        "homeassistant.components.xiaomi_miio.get_platforms",
+        return_value=[
+            Platform.SELECT,
+        ],
+    ), patch(
+        "homeassistant.components.xiaomi_miio.async_create_miio_device_and_coordinator",
+        mock_async_create_miio_device_and_coordinator,
+    ):
+        await setup_component(hass, "dummy")
 
 
 def test_select_params(hass: HomeAssistant) -> None:
@@ -212,14 +226,14 @@ async def test_select_option(hass: HomeAssistant) -> None:
     assert state.state == "option_0"
 
 
-async def test_select_coordinator_update(hass: HomeAssistant, setup_select) -> None:
+async def test_select_coordinator_update(hass: HomeAssistant) -> None:
     """Test coordinator update of a option."""
     state = hass.states.get("select.dummy_dummy")
     assert state
     assert state.state == "option_1"
 
     # emulate someone change state from device maybe used app
-    setup_select.set_dummy_value(DummyEnum_0.Option_0)
+    hass.data["device_for_test"].set_dummy_value(DummyEnum_0.Option_0)
 
     async_fire_time_changed(hass, utcnow() + UPDATE_INTERVAL)
     await hass.async_block_till_done()
@@ -227,3 +241,27 @@ async def test_select_coordinator_update(hass: HomeAssistant, setup_select) -> N
     state = hass.states.get("select.dummy_dummy")
     assert state
     assert state.state == "option_0"
+
+
+async def setup_component(hass, entity_name):
+    """Set up vacuum component."""
+    entity_id = f"{DOMAIN}.{entity_name}"
+
+    config_entry = MockConfigEntry(
+        domain=XIAOMI_DOMAIN,
+        unique_id="123456",
+        title=entity_name,
+        data={
+            CONF_FLOW_TYPE: CONF_DEVICE,
+            CONF_HOST: "0.0.0.0",
+            CONF_TOKEN: "12345678901234567890123456789012",
+            CONF_MODEL: "Dummy",
+            CONF_MAC: TEST_MAC,
+        },
+    )
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    return entity_id
