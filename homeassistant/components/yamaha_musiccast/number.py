4d6fb88b64ab71b34db132fdba7bc961070507ff
epenet <6771947+epenet@users.noreply.github.com>
2022-08-11 00:16:38 +0200

    Improve type hints in yamaha_musiccast number (#76467)

--- homeassistant/components/yamaha_musiccast/number.py
+++ homeassistant/components/yamaha_musiccast/number.py
@@ -1,4 +1,5 @@
 """Number entities for musiccast."""
+from __future__ import annotations
 
 from aiomusiccast.capabilities import NumberSetter
 
@@ -50,10 +51,10 @@ class NumberCapability(MusicCastCapabilityEntity, NumberEntity):
         self._attr_native_step = capability.value_range.step
 
     @property
-    def native_value(self):
+    def native_value(self) -> float | None:
         """Return the current value."""
         return self.capability.current
 
-    async def async_set_native_value(self, value: float):
+    async def async_set_native_value(self, value: float) -> None:
         """Set a new value."""
         await self.capability.set(value)
