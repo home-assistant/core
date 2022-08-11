9c8bd66acd531570222c333f6108807ddd4c69dc
spycle <48740594+spycle@users.noreply.github.com>
2022-08-11 20:18:45 +0100

    Remove stale startup message

--- homeassistant/components/microbot_push/const.py
+++ homeassistant/components/microbot_push/const.py
@@ -31,13 +31,3 @@ DEFAULT_RETRY_COUNT = 5
 # Defaults
 DEFAULT_NAME = "Microbot"
 
-
-STARTUP_MESSAGE = f"""
--------------------------------------------------------------------
-{NAME}
-Version: {VERSION}
-This is a custom integration!
-If you have any issues with this you need to open an issue here:
-{ISSUE_URL}
--------------------------------------------------------------------
-"""
