8378f96e0bde3cd2c92e3abc26e3f9136b09a88d
spycle <48740594+spycle@users.noreply.github.com>
2022-08-11 22:38:26 +0100

    Update unload to use helper

--- homeassistant/components/microbot_push/__init__.py
+++ homeassistant/components/microbot_push/__init__.py
@@ -139,14 +139,8 @@ class MicroBotDataUpdateCoordinator(PassiveBluetoothDataUpdateCoordinator):
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Handle removal of an entry."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    unloaded = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-                if platform in coordinator.platforms
-            ]
-        )
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry, PLATFORMS
     )
     if unloaded:
         hass.data[DOMAIN].pop(entry.entry_id)
