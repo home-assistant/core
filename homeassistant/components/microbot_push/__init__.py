0048df1f062f2d53b7c485300dd430d6309fc59b
spycle <48740594+spycle@users.noreply.github.com>
2022-08-11 20:29:08 +0100

    Remove async_reload_entry
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/microbot_push/__init__.py
+++ homeassistant/components/microbot_push/__init__.py
@@ -163,7 +163,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return unloaded
 
 
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
