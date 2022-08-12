46369b274b0309518e2dd555bf075379e97cd633
Jc2k <john.carr@unrouted.co.uk>
2022-08-12 09:25:24 +0100

    Initial binary_sensor support for Xiaomi BLE (#76635)

--- homeassistant/components/xiaomi_ble/__init__.py
+++ homeassistant/components/xiaomi_ble/__init__.py
@@ -20,7 +20,7 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-PLATFORMS: list[Platform] = [Platform.SENSOR]
+PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.SENSOR]
 
 _LOGGER = logging.getLogger(__name__)
 
