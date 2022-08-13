9553cd8507f79e1b3d7eb545b0beff7af1673fab
spycle <48740594+spycle@users.noreply.github.com>
2022-08-13 14:43:00 +0100

    Update to single commands

--- homeassistant/components/keymitt_ble/switch.py
+++ homeassistant/components/keymitt_ble/switch.py
@@ -19,16 +19,12 @@ class MicroBotBinarySwitch(MicroBotEntity, SwitchEntity):
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn on the switch."""
-        await self.coordinator.api.connect()
         await self.coordinator.api.push_on()
-        await self.coordinator.api.disconnect()
         self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the switch."""
-        await self.coordinator.api.connect()
         await self.coordinator.api.push_off()
-        await self.coordinator.api.disconnect()
         self.async_write_ha_state()
 
     @property
