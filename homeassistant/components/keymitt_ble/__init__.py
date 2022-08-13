f04ccb0ca85d23282cabb48d96ee277285a69d47
spycle <48740594+spycle@users.noreply.github.com>
2022-08-14 00:52:19 +0100

    Fix typo in unload

--- homeassistant/components/keymitt_ble/__init__.py
+++ homeassistant/components/keymitt_ble/__init__.py
@@ -142,7 +142,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(
         entry, PLATFORMS
     )
-    if unloaded:
+    if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unloaded
