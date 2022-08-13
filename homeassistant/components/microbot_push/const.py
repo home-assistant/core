3bc2ef04c11e1cb2a866091b9d6bfee4dbae08ec
spycle <48740594+spycle@users.noreply.github.com>
2022-08-13 13:13:30 +0100

    Remove unused

--- homeassistant/components/microbot_push/const.py
+++ homeassistant/components/microbot_push/const.py
@@ -3,25 +3,13 @@
 NAME = "MicroBot Push"
 DOMAIN = "keymitt_ble"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "2022.08.0"
 MANUFACTURER = "Naran/Keymitt"
 
 # Icons
 ICON = "mdi:toggle-switch-variant"
 
-# Device classes
-BINARY_SENSOR_DEVICE_CLASS = "connectivity"
-
-# Platforms
-BINARY_SENSOR = "binary_sensor"
-SENSOR = "sensor"
-SWITCH = "switch"
-
-
 # Configuration and options
-CONF_ENABLED = "enabled"
 CONF_NAME = "name"
-CONF_PASSWORD = "password"
 CONF_BDADDR = "bdaddr"
 DEFAULT_RETRY_COUNT = 5
 
