28eaaf707ea147e294dd3d9ce41e41ac16866d6c
spycle <48740594+spycle@users.noreply.github.com>
2022-08-14 16:26:52 +0100

    Fix typo

--- homeassistant/components/keymitt_ble/__init__.py
+++ homeassistant/components/keymitt_ble/__init__.py
@@ -130,4 +130,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
-    return unload_pk
+    return unload_ok
