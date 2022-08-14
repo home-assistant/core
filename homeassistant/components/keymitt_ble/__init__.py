27c52db492cc5784dcdfaf76ed30f797b5d1c6f2
spycle <48740594+spycle@users.noreply.github.com>
2022-08-14 14:07:39 +0100

    Remove assertions
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/keymitt_ble/__init__.py
+++ homeassistant/components/keymitt_ble/__init__.py
@@ -39,8 +39,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     token: str = entry.data[CONF_ACCESS_TOKEN]
     bdaddr: str = entry.data[CONF_BDADDR]
-    assert bdaddr is not None
-    assert token is not None
     ble_device: BLEDevice | None = bluetooth.async_ble_device_from_address(
         hass, bdaddr.upper()
     )
