4d931f43ea17887aa64ad12aeeb5c5d294237c56
Charles Garwood <cgarwood@gmail.com>
2022-08-15 13:53:36 -0400

    binary_sensor updates per code review

--- homeassistant/components/fully_kiosk/binary_sensor.py
+++ homeassistant/components/fully_kiosk/binary_sensor.py
@@ -8,21 +8,26 @@ from homeassistant.components.binary_sensor import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
+from .coordinator import FullyKioskDataUpdateCoordinator
 
-SENSOR_TYPES: tuple[BinarySensorEntityDescription, ...] = (
-    BinarySensorEntityDescription(key="kioskMode", name="Kiosk Mode"),
+SENSORS: tuple[BinarySensorEntityDescription, ...] = (
+    BinarySensorEntityDescription(
+        key="kioskMode", name="Kiosk mode", entity_category=EntityCategory.DIAGNOSTIC
+    ),
     BinarySensorEntityDescription(
         key="plugged",
-        name="Plugged In",
+        name="Plugged in",
         device_class=BinarySensorDeviceClass.PLUG,
     ),
     BinarySensorEntityDescription(
         key="isDeviceAdmin",
-        name="Device Admin",
+        name="Device admin",
+        entity_category=EntityCategory.DIAGNOSTIC,
     ),
 )
 
@@ -33,35 +38,41 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the Fully Kiosk Browser sensor."""
-    coordinator = hass.data[DOMAIN][config_entry.entry_id]
-
-    sensors = [
-        FullyBinarySensor(coordinator, sensor)
-        for sensor in SENSOR_TYPES
-        if sensor.key in coordinator.data
+    coordinator: FullyKioskDataUpdateCoordinator = hass.data[DOMAIN][
+        config_entry.entry_id
     ]
 
-    async_add_entities(sensors, False)
+    async_add_entities(
+        FullyBinarySensor(coordinator, description)
+        for description in SENSORS
+        if description.key in coordinator.data
+    )
 
 
-class FullyBinarySensor(CoordinatorEntity, BinarySensorEntity):
+class FullyBinarySensor(
+    CoordinatorEntity[FullyKioskDataUpdateCoordinator], BinarySensorEntity
+):
     """Representation of a Fully Kiosk Browser binary sensor."""
 
-    def __init__(self, coordinator, sensor):
-        """Initialize the binary sensor."""
-        self.entity_description = sensor
-        self._sensor = sensor.key
-        self.coordinator = coordinator
+    _attr_has_entity_name: bool = True
 
-        self._attr_name = f"{coordinator.data['deviceName']} {sensor.name}"
-        self._attr_unique_id = f"{coordinator.data['deviceID']}-{sensor.key}"
+    def __init__(
+        self,
+        coordinator: FullyKioskDataUpdateCoordinator,
+        description: BinarySensorEntityDescription,
+    ) -> None:
+        """Initialize the binary sensor."""
+        self.entity_description = description
+        self._sensor = description.key
+
+        self._attr_unique_id = f"{coordinator.data['deviceID']}-{description.key}"
         self._attr_device_info = {
-            "identifiers": {(DOMAIN, self.coordinator.data["deviceID"])},
-            "name": self.coordinator.data["deviceName"],
-            "manufacturer": self.coordinator.data["deviceManufacturer"],
-            "model": self.coordinator.data["deviceModel"],
-            "sw_version": self.coordinator.data["appVersionName"],
-            "configuration_url": f"http://{self.coordinator.data['ip4']}:2323",
+            "identifiers": {(DOMAIN, coordinator.data["deviceID"])},
+            "name": coordinator.data["deviceName"],
+            "manufacturer": coordinator.data["deviceManufacturer"],
+            "model": coordinator.data["deviceModel"],
+            "sw_version": coordinator.data["appVersionName"],
+            "configuration_url": f"http://{coordinator.data['ip4']}:2323",
         }
         super().__init__(coordinator)
 
