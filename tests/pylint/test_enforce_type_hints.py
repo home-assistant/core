56e8ffde035f1498ba32733505504b7cdb9a85e8
epenet <6771947+epenet@users.noreply.github.com>
2022-08-15 09:48:03 +0200

    Add media-player checks to pylint plugin (#76675)
    * Add media-player checks to pylint plugin
    
    * Fix invalid hints
    
    * Add tests
    
    * Adjust tests
    
    * Add extra test
    
    * Adjust regex
    
    * Cleanup comment
    
    * Move media player tests up

--- tests/pylint/test_enforce_type_hints.py
+++ tests/pylint/test_enforce_type_hints.py
@@ -53,6 +53,7 @@ def test_regex_get_module_platform(
         ("Awaitable[None]", 1, ("Awaitable", "None")),
         ("list[dict[str, str]]", 1, ("list", "dict[str, str]")),
         ("list[dict[str, Any]]", 1, ("list", "dict[str, Any]")),
+        ("tuple[bytes | None, str | None]", 2, ("tuple", "bytes | None", "str | None")),
     ],
 )
 def test_regex_x_of_y_i(
@@ -899,6 +900,35 @@ def test_invalid_device_class(
             end_col_offset=20,
         ),
     ):
+        type_hint_checker.visit_classdef(class_node)
+
+
+def test_media_player_entity(
+    linter: UnittestLinter, type_hint_checker: BaseChecker
+) -> None:
+    """Ensure valid hints are accepted for media_player entity."""
+    # Set bypass option
+    type_hint_checker.config.ignore_missing_annotations = False
+
+    class_node = astroid.extract_node(
+        """
+    class Entity():
+        pass
+
+    class MediaPlayerEntity(Entity):
+        pass
+
+    class MyMediaPlayer( #@
+        MediaPlayerEntity
+    ):
+        async def async_get_media_image(self) -> tuple[bytes | None, str | None]:
+            pass
+    """,
+        "homeassistant.components.pylint_test.media_player",
+    )
+    type_hint_checker.visit_module(class_node.parent)
+
+    with assert_no_messages(linter):
         type_hint_checker.visit_classdef(class_node)
 
 
