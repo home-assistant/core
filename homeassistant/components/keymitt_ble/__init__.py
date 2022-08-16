d41893b01a9b7b04c4f2b411cee9f51809a04b02
spycle <48740594+spycle@users.noreply.github.com>
2022-08-16 20:35:26 +0100

    Fix typing

--- homeassistant/components/keymitt_ble/__init__.py
+++ homeassistant/components/keymitt_ble/__init__.py
@@ -35,7 +35,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     token: str = entry.data[CONF_ACCESS_TOKEN]
     bdaddr: str = entry.data[CONF_ADDRESS]
-    ble_device: BLEDevice | None = bluetooth.async_ble_device_from_address(
+    ble_device = bluetooth.async_ble_device_from_address(
         hass, bdaddr
     )
     if not ble_device:
