dff419e5fad8966b8b5e2ed75c3fe9ef03824c00
Erik <erik@montnemery.com>
2022-08-16 16:10:31 +0200

    Add periodic system stats to hardware integration

--- homeassistant/components/hardware/websocket_api.py
+++ homeassistant/components/hardware/websocket_api.py
@@ -2,23 +2,50 @@
 from __future__ import annotations
 
 import contextlib
-from dataclasses import asdict
+from dataclasses import asdict, dataclass
+from datetime import datetime, timedelta
+import importlib
+from typing import TYPE_CHECKING
 
 import voluptuous as vol
 
 from homeassistant.components import websocket_api
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import CALLBACK_TYPE, HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers.event import async_track_time_interval
 
 from .const import DOMAIN
 from .hardware import async_process_hardware_platforms
 from .models import HardwareProtocol
+
+if TYPE_CHECKING:
+    import psutil  # noqa: F401
+
+
+@dataclass
+class SystemStatus:
+    """System status."""
+
+    psutil: psutil
+    remove_periodic_timer: CALLBACK_TYPE | None
+    subscribers: set[tuple[websocket_api.ActiveConnection, int]]
 
 
 @callback
 def async_setup(hass: HomeAssistant) -> None:
     """Set up the hardware websocket API."""
     websocket_api.async_register_command(hass, ws_info)
+    websocket_api.async_register_command(hass, ws_subscribe_system_status)
+    # Make a local copy of the psutil module because it relies on global variables
+    psutil_spec = importlib.util.find_spec("psutil")
+    assert psutil_spec and psutil_spec.loader
+    psutil_module = importlib.util.module_from_spec(psutil_spec)
+    psutil_spec.loader.exec_module(psutil_module)
+    hass.data[DOMAIN]["system_status"] = SystemStatus(
+        psutil=psutil_module,
+        remove_periodic_timer=None,
+        subscribers=set(),
+    )
 
 
 @websocket_api.websocket_command(
@@ -45,3 +72,48 @@ async def ws_info(
                 hardware_info.append(asdict(platform.async_info(hass)))
 
     connection.send_result(msg["id"], {"hardware": hardware_info})
+
+
+@websocket_api.websocket_command(
+    {
+        vol.Required("type"): "hardware/subscribe_system_status",
+    }
+)
+@websocket_api.async_response
+async def ws_subscribe_system_status(hass, connection, msg):
+    """Subscribe to system status updates."""
+
+    system_status: SystemStatus = hass.data[DOMAIN]["system_status"]
+
+    @callback
+    def async_update_status(now: datetime) -> None:
+        cpu_percentage = round(system_status.psutil.cpu_percent(interval=None))
+        virtual_memory = system_status.psutil.virtual_memory()
+        json_msg = {
+            "cpu_percentage": cpu_percentage,
+            "memory_use_percent": virtual_memory.percent,
+            "memory_use": round(
+                (virtual_memory.total - virtual_memory.available) / 1024**2, 1
+            ),
+            "memory_free": round(virtual_memory.available / 1024**2, 1),
+        }
+        for connection, msg_id in system_status.subscribers:
+            connection.send_message(websocket_api.event_message(msg_id, json_msg))
+
+    if not system_status.subscribers:
+        system_status.remove_periodic_timer = async_track_time_interval(
+            hass, async_update_status, timedelta(seconds=5)
+        )
+
+    system_status.subscribers.add((connection, msg["id"]))
+
+    @callback
+    def cancel_subscription() -> None:
+        system_status.subscribers.remove((connection, msg["id"]))
+        if not system_status.subscribers and system_status.remove_periodic_timer:
+            system_status.remove_periodic_timer()
+            system_status.remove_periodic_timer = None
+
+    connection.subscriptions[msg["id"]] = cancel_subscription
+
+    connection.send_message(websocket_api.result_message(msg["id"]))
--- tests/components/hardware/test_websocket_api.py
+++ tests/components/hardware/test_websocket_api.py
@@ -1,4 +1,8 @@
 """Test the hardware websocket API."""
+from collections import namedtuple
+import datetime
+from unittest.mock import patch
+
 from homeassistant.components.hardware.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
@@ -16,3 +20,51 @@ async def test_board_info(hass: HomeAssistant, hass_ws_client) -> None:
     assert msg["id"] == 1
     assert msg["success"]
     assert msg["result"] == {"hardware": []}
+
+
+TEST_TIME_ADVANCE_INTERVAL = datetime.timedelta(seconds=5 + 1)
+
+
+async def test_mqtt_ws_subscription(hass: HomeAssistant, hass_ws_client, freezer):
+    """Test websocket system stateus subscription."""
+    assert await async_setup_component(hass, DOMAIN, {})
+
+    client = await hass_ws_client(hass)
+
+    await client.send_json({"id": 1, "type": "hardware/subscribe_system_status"})
+    response = await client.receive_json()
+    assert response["success"]
+
+    VirtualMem = namedtuple("VirtualMemory", ["available", "percent", "total"])
+    vmem = VirtualMem(10 * 1024**2, 50, 30 * 1024**2)
+
+    with patch.object(
+        hass.data[DOMAIN]["system_status"].psutil, "cpu_percent", return_value=123
+    ), patch.object(
+        hass.data[DOMAIN]["system_status"].psutil, "virtual_memory", return_value=vmem
+    ):
+        freezer.tick(TEST_TIME_ADVANCE_INTERVAL)
+        await hass.async_block_till_done()
+
+    response = await client.receive_json()
+    assert response["event"] == {
+        "cpu_percentage": 123,
+        "memory_free": 10.0,
+        "memory_use": 20.0,
+        "memory_use_percent": 50,
+    }
+
+    # Unsubscribe
+    await client.send_json({"id": 8, "type": "unsubscribe_events", "subscription": 1})
+    response = await client.receive_json()
+    assert response["success"]
+
+    with patch.object(
+        hass.data[DOMAIN]["system_status"].psutil, "cpu_percent"
+    ) as cpu_mock, patch.object(
+        hass.data[DOMAIN]["system_status"].psutil, "virtual_memory"
+    ) as vmem_mock:
+        freezer.tick(TEST_TIME_ADVANCE_INTERVAL)
+        await hass.async_block_till_done()
+        cpu_mock.assert_not_called()
+        vmem_mock.assert_not_called()
