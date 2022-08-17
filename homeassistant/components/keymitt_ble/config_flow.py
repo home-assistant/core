4c0549dbe6ca8ff6a51ca4d57e677cfebb4455e5
spycle <48740594+spycle@users.noreply.github.com>
2022-08-17 19:50:23 +0100

    Fix mypy

--- homeassistant/components/keymitt_ble/config_flow.py
+++ homeassistant/components/keymitt_ble/config_flow.py
@@ -148,7 +148,7 @@ class MicroBotConfigFlow(ConfigFlow, domain=DOMAIN):
         if errors:
             return self.async_show_form(step_id="link", errors=errors)
 
-        user_input[CONF_ADDRESS] = self._ble_device.address
+        user_input[CONF_ADDRESS] = self._bdaddr
         user_input[CONF_ACCESS_TOKEN] = token
 
         return self.async_create_entry(title=self._name, data=user_input)
