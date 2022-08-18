8be1d823adf36e7908fd54402881357f02a93a82
spycle <48740594+spycle@users.noreply.github.com>
2022-08-18 21:39:55 +0100

    Add type checking import

--- homeassistant/components/keymitt_ble/switch.py
+++ homeassistant/components/keymitt_ble/switch.py
@@ -1,7 +1,7 @@
 """Switch platform for MicroBot."""
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, TYPE_CHECKING
 
 from homeassistant.components.switch import SwitchEntity
 
