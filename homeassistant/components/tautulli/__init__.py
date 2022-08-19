04e432c8b93f367db48864ee15486009931beeae
Robert Hillis <tkdrob4390@yahoo.com>
2022-08-19 08:33:43 -0400

    uno mas

--- homeassistant/components/tautulli/__init__.py
+++ homeassistant/components/tautulli/__init__.py
@@ -30,7 +30,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
     coordinator = TautulliDataUpdateCoordinator(hass, host_configuration, api_client)
     await coordinator.async_config_entry_first_refresh()
-    hass.data[DOMAIN] = coordinator
+    hass.data[DOMAIN][entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
