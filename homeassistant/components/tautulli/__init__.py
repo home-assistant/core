df6c1ae44ab6d8f254e8b728ae0ab106b3f5404f
Robert Hillis <tkdrob4390@yahoo.com>
2022-08-19 09:30:56 -0400

    pop entry not domain

--- homeassistant/components/tautulli/__init__.py
+++ homeassistant/components/tautulli/__init__.py
@@ -39,7 +39,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data.pop(DOMAIN)
+        hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
 
 
