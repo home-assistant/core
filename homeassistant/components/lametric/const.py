9ed9f630a9078960fe57f0b7de9dbce6813f7530
Franck Nijhof <git@frenck.dev>
2022-08-20 19:06:35 +0200

    Add coordinator and number platform to LaMetric (#76766)

--- homeassistant/components/lametric/const.py
+++ homeassistant/components/lametric/const.py
@@ -1,11 +1,16 @@
 """Constants for the LaMetric integration."""
 
+from datetime import timedelta
 import logging
 from typing import Final
 
+from homeassistant.const import Platform
+
 DOMAIN: Final = "lametric"
+PLATFORMS = [Platform.NUMBER]
 
 LOGGER = logging.getLogger(__package__)
+SCAN_INTERVAL = timedelta(seconds=30)
 
 CONF_CYCLES: Final = "cycles"
 CONF_ICON_TYPE: Final = "icon_type"
