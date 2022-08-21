5867b072e73258bea7c0f0c0b09a29d0b4999a3c
spycle <matthewmorgan77@aol.com>
2022-08-21 15:44:43 +0000

    Add coordinator

--- homeassistant/components/keymitt_ble/__init__.py
+++ homeassistant/components/keymitt_ble/__init__.py
@@ -1,25 +1,19 @@
 """Integration to integrate MicroBot with Home Assistant."""
 from __future__ import annotations
 
-import asyncio
 import logging
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
-from microbot import (
-    MicroBotApiClient,
-    parse_advertisement_data,
-)
+from microbot import MicroBotApiClient  # pylint: disable=import-error
 
 from homeassistant.components import bluetooth
-from homeassistant.components.bluetooth.passive_update_coordinator import (
-    PassiveBluetoothDataUpdateCoordinator,
-)
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, ServiceCall, callback
-from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.const import CONF_ACCESS_TOKEN, CONF_ADDRESS, Platform
+from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN
+from .coordinator import MicroBotDataUpdateCoordinator
 
 if TYPE_CHECKING:
     from bleak.backends.device import BLEDevice
@@ -33,9 +27,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     token: str = entry.data[CONF_ACCESS_TOKEN]
     bdaddr: str = entry.data[CONF_ADDRESS]
-    ble_device = bluetooth.async_ble_device_from_address(
-        hass, bdaddr
-    )
+    ble_device: BLEDevice | None = bluetooth.async_ble_device_from_address(hass, bdaddr)
     if not ble_device:
         raise ConfigEntryNotReady(f"Could not find MicroBot with address {bdaddr}")
     client = MicroBotApiClient(
@@ -48,9 +40,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    await hass.config_entries.async_forward_entry_setups(
-        entry, PLATFORMS
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(coordinator.async_start())
 
     async def calibrate(call: ServiceCall) -> None:
@@ -64,46 +54,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-class MicroBotDataUpdateCoordinator(PassiveBluetoothDataUpdateCoordinator):
-    """Class to manage fetching data from the MicroBot."""
-
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        client: MicroBotApiClient,
-        ble_device: BLEDevice,
-    ) -> None:
-        """Initialize."""
-        self.api: MicroBotApiClient = client
-        self._ready_event = asyncio.Event()
-        self.data: dict[str, Any] = {}
-        self.ble_device = ble_device
-
-        super().__init__(
-            hass, _LOGGER, ble_device.address, bluetooth.BluetoothScanningMode.ACTIVE
-        )
-
-    @callback
-    def _async_handle_bluetooth_event(
-        self,
-        service_info: bluetooth.BluetoothServiceInfoBleak,
-        change: bluetooth.BluetoothChange,
-    ) -> None:
-        """Handle a Bluetooth event."""
-        super()._async_handle_bluetooth_event(service_info, change)
-        if adv := parse_advertisement_data(
-            service_info.device, service_info.advertisement
-        ):
-            self.data = adv.data
-            _LOGGER.debug("%s: MicroBot data: %s", self.ble_device.address, self.data)
-            self.api.update_from_advertisement(adv)
-
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Handle removal of an entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(
-        entry, PLATFORMS
-    ):
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
---
+++ homeassistant/components/keymitt_ble/coordinator.py
@@ -0,0 +1,62 @@
+"""Integration to integrate MicroBot with Home Assistant."""
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING, Any
+
+from microbot import (  # pylint: disable=import-error
+    MicroBotApiClient,
+    parse_advertisement_data,
+)
+
+from homeassistant.components import bluetooth
+from homeassistant.components.bluetooth.passive_update_coordinator import (
+    PassiveBluetoothDataUpdateCoordinator,
+)
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant, callback
+
+if TYPE_CHECKING:
+    from bleak.backends.device import BLEDevice
+
+_LOGGER: logging.Logger = logging.getLogger(__package__)
+PLATFORMS: list[str] = [Platform.SWITCH]
+
+
+class MicroBotDataUpdateCoordinator(PassiveBluetoothDataUpdateCoordinator):
+    """Class to manage fetching data from the MicroBot."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        client: MicroBotApiClient,
+        ble_device: BLEDevice,
+    ) -> None:
+        """Initialize."""
+        self.api: MicroBotApiClient = client
+        self.data: dict[str, Any] = {}
+        self.ble_device = ble_device
+        super().__init__(
+            hass,
+            _LOGGER,
+            ble_device.address,
+            bluetooth.BluetoothScanningMode.ACTIVE,
+        )
+
+    @callback
+    def _async_handle_bluetooth_event(
+        self,
+        service_info: bluetooth.BluetoothServiceInfoBleak,
+        change: bluetooth.BluetoothChange,
+    ) -> None:
+        """Handle a Bluetooth event."""
+        super()._async_handle_bluetooth_event(service_info, change)
+        if adv := parse_advertisement_data(
+            service_info.device, service_info.advertisement
+        ):
+            _LOGGER.debug("Service_info: %s", service_info)
+            _LOGGER.debug("Change: %s", change)
+            self.data = adv.data
+            _LOGGER.debug("self.data: %s", self.data)
+            _LOGGER.debug("%s: MicroBot data: %s", self.ble_device.address, self.data)
+            self.api.update_from_advertisement(adv)
