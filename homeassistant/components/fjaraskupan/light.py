2689eddbe84adfb40cba25cdce72cdc9ec39afba
Joakim Plate <elupus@ecce.se>
2022-08-21 09:47:04 +0200

    Make sure we always connect to last known bluetooth device in fjäråskupan (#77088)
    Make sure we always connect to last known device

--- homeassistant/components/fjaraskupan/light.py
+++ homeassistant/components/fjaraskupan/light.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from fjaraskupan import COMMAND_LIGHT_ON_OFF, Device
+from fjaraskupan import COMMAND_LIGHT_ON_OFF
 
 from homeassistant.components.light import ATTR_BRIGHTNESS, ColorMode, LightEntity
 from homeassistant.config_entries import ConfigEntry
@@ -23,7 +23,7 @@ async def async_setup_entry(
     """Set up tuya sensors dynamically through tuya discovery."""
 
     def _constructor(coordinator: Coordinator) -> list[Entity]:
-        return [Light(coordinator, coordinator.device, coordinator.device_info)]
+        return [Light(coordinator, coordinator.device_info)]
 
     async_setup_entry_platform(hass, config_entry, async_add_entities, _constructor)
 
@@ -36,31 +36,29 @@ class Light(CoordinatorEntity[Coordinator], LightEntity):
     def __init__(
         self,
         coordinator: Coordinator,
-        device: Device,
         device_info: DeviceInfo,
     ) -> None:
         """Init light entity."""
         super().__init__(coordinator)
-        self._device = device
         self._attr_color_mode = ColorMode.BRIGHTNESS
         self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
-        self._attr_unique_id = device.address
+        self._attr_unique_id = coordinator.device.address
         self._attr_device_info = device_info
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the light on."""
-        if ATTR_BRIGHTNESS in kwargs:
-            await self._device.send_dim(int(kwargs[ATTR_BRIGHTNESS] * (100.0 / 255.0)))
-        else:
-            if not self.is_on:
-                await self._device.send_command(COMMAND_LIGHT_ON_OFF)
-        self.coordinator.async_set_updated_data(self._device.state)
+        async with self.coordinator.async_connect_and_update() as device:
+            if ATTR_BRIGHTNESS in kwargs:
+                await device.send_dim(int(kwargs[ATTR_BRIGHTNESS] * (100.0 / 255.0)))
+            else:
+                if not self.is_on:
+                    await device.send_command(COMMAND_LIGHT_ON_OFF)
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the entity off."""
         if self.is_on:
-            await self._device.send_command(COMMAND_LIGHT_ON_OFF)
-        self.coordinator.async_set_updated_data(self._device.state)
+            async with self.coordinator.async_connect_and_update() as device:
+                await device.send_command(COMMAND_LIGHT_ON_OFF)
 
     @property
     def is_on(self) -> bool:
