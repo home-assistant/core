a817718177c6f0dac2f08ad96d3facc98da70c34
epenet <6771947+epenet@users.noreply.github.com>
2022-08-22 18:28:29 +0000

    Improve

--- homeassistant/components/fibaro/climate.py
+++ homeassistant/components/fibaro/climate.py
@@ -124,7 +124,7 @@ class FibaroThermostat(FibaroDevice, ClimateEntity):
         self._temp_sensor_device: FibaroDevice | None = None
         self._target_temp_device: FibaroDevice | None = None
         self._op_mode_device: FibaroDevice | None = None
-        self._fan_mode_device = None
+        self._fan_mode_device: FibaroDevice | None = None
         self._attr_supported_features = 0
         self.entity_id = ENTITY_ID_FORMAT.format(self.ha_id)
 
