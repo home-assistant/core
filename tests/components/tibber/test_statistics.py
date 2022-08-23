9e66b30af97063b500fb41a9c82a2565bf431082
Erik Flodin <erik@ejohansson.se>
2022-08-23 10:02:58 +0200

    Add new sensors for energy produced (via Tibbber) (#76165)
    The new sensors tibber:energy_(production|profit)_<home_id> are like the
    existing consumption/totalCost ones except that they report outgoing
    energy instead of incomeing.

--- tests/components/tibber/test_statistics.py
+++ tests/components/tibber/test_statistics.py
@@ -5,7 +5,7 @@ from homeassistant.components.recorder.statistics import statistics_during_perio
 from homeassistant.components.tibber.sensor import TibberDataCoordinator
 from homeassistant.util import dt as dt_util
 
-from .test_common import CONSUMPTION_DATA_1, mock_get_homes
+from .test_common import CONSUMPTION_DATA_1, PRODUCTION_DATA_1, mock_get_homes
 
 from tests.components.recorder.common import async_wait_recording_done
 
@@ -15,62 +15,39 @@ async def test_async_setup_entry(hass, recorder_mock):
     tibber_connection = AsyncMock()
     tibber_connection.name = "tibber"
     tibber_connection.fetch_consumption_data_active_homes.return_value = None
+    tibber_connection.fetch_production_data_active_homes.return_value = None
     tibber_connection.get_homes = mock_get_homes
 
     coordinator = TibberDataCoordinator(hass, tibber_connection)
     await coordinator._async_update_data()
     await async_wait_recording_done(hass)
 
-    # Validate consumption
-    statistic_id = "tibber:energy_consumption_home_id"
+    for (statistic_id, data, key) in (
+        ("tibber:energy_consumption_home_id", CONSUMPTION_DATA_1, "consumption"),
+        ("tibber:energy_totalcost_home_id", CONSUMPTION_DATA_1, "totalCost"),
+        ("tibber:energy_production_home_id", PRODUCTION_DATA_1, "production"),
+        ("tibber:energy_profit_home_id", PRODUCTION_DATA_1, "profit"),
+    ):
+        stats = await hass.async_add_executor_job(
+            statistics_during_period,
+            hass,
+            dt_util.parse_datetime(data[0]["from"]),
+            None,
+            [statistic_id],
+            "hour",
+            True,
+        )
 
-    stats = await hass.async_add_executor_job(
-        statistics_during_period,
-        hass,
-        dt_util.parse_datetime(CONSUMPTION_DATA_1[0]["from"]),
-        None,
-        [statistic_id],
-        "hour",
-        True,
-    )
+        assert len(stats) == 1
+        assert len(stats[statistic_id]) == 3
+        _sum = 0
+        for k, stat in enumerate(stats[statistic_id]):
+            assert stat["start"] == dt_util.parse_datetime(data[k]["from"])
+            assert stat["state"] == data[k][key]
+            assert stat["mean"] is None
+            assert stat["min"] is None
+            assert stat["max"] is None
+            assert stat["last_reset"] is None
 
-    assert len(stats) == 1
-    assert len(stats[statistic_id]) == 3
-    _sum = 0
-    for k, stat in enumerate(stats[statistic_id]):
-        assert stat["start"] == dt_util.parse_datetime(CONSUMPTION_DATA_1[k]["from"])
-        assert stat["state"] == CONSUMPTION_DATA_1[k]["consumption"]
-        assert stat["mean"] is None
-        assert stat["min"] is None
-        assert stat["max"] is None
-        assert stat["last_reset"] is None
-
-        _sum += CONSUMPTION_DATA_1[k]["consumption"]
-        assert stat["sum"] == _sum
-
-    # Validate cost
-    statistic_id = "tibber:energy_totalcost_home_id"
-
-    stats = await hass.async_add_executor_job(
-        statistics_during_period,
-        hass,
-        dt_util.parse_datetime(CONSUMPTION_DATA_1[0]["from"]),
-        None,
-        [statistic_id],
-        "hour",
-        True,
-    )
-
-    assert len(stats) == 1
-    assert len(stats[statistic_id]) == 3
-    _sum = 0
-    for k, stat in enumerate(stats[statistic_id]):
-        assert stat["start"] == dt_util.parse_datetime(CONSUMPTION_DATA_1[k]["from"])
-        assert stat["state"] == CONSUMPTION_DATA_1[k]["totalCost"]
-        assert stat["mean"] is None
-        assert stat["min"] is None
-        assert stat["max"] is None
-        assert stat["last_reset"] is None
-
-        _sum += CONSUMPTION_DATA_1[k]["totalCost"]
-        assert stat["sum"] == _sum
+            _sum += data[k][key]
+            assert stat["sum"] == _sum
