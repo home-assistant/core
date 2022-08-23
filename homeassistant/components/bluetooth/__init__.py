41043bd004056440893a9bd65b4d88bb65ccc334
J. Nick Koston <nick@koston.org>
2022-08-22 15:45:08 -1000

    Discover new bluetooth adapters when they are plugged in (#77006)

--- homeassistant/components/bluetooth/__init__.py
+++ homeassistant/components/bluetooth/__init__.py
@@ -3,16 +3,19 @@ from __future__ import annotations
 
 from asyncio import Future
 from collections.abc import Callable, Iterable
+import logging
 import platform
 from typing import TYPE_CHECKING, cast
 
 import async_timeout
 
 from homeassistant import config_entries
+from homeassistant.components import usb
 from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import CALLBACK_TYPE, HomeAssistant, callback as hass_callback
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import device_registry as dr, discovery_flow
+from homeassistant.helpers.debounce import Debouncer
 from homeassistant.loader import async_get_bluetooth
 
 from . import models
@@ -64,6 +67,8 @@ __all__ = [
     "BluetoothCallback",
     "SOURCE_LOCAL",
 ]
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def _get_manager(hass: HomeAssistant) -> BluetoothManager:
@@ -213,6 +218,31 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
     async_migrate_entries(hass, adapters)
     await async_discover_adapters(hass, adapters)
+
+    async def _async_rediscover_adapters() -> None:
+        """Rediscover adapters when a new one may be available."""
+        discovered_adapters = await manager.async_get_bluetooth_adapters(cached=False)
+        _LOGGER.debug("Rediscovered adapters: %s", discovered_adapters)
+        await async_discover_adapters(hass, discovered_adapters)
+
+    discovery_debouncer = Debouncer(
+        hass, _LOGGER, cooldown=5, immediate=False, function=_async_rediscover_adapters
+    )
+
+    def _async_trigger_discovery() -> None:
+        # There are so many bluetooth adapter models that
+        # we check the bus whenever a usb device is plugged in
+        # to see if it is a bluetooth adapter since we can't
+        # tell if the device is a bluetooth adapter or if its
+        # actually supported unless we ask DBus if its now
+        # present.
+        _LOGGER.debug("Triggering bluetooth usb discovery")
+        hass.async_create_task(discovery_debouncer.async_call())
+
+    cancel = usb.async_register_scan_request_callback(hass, _async_trigger_discovery)
+    hass.bus.async_listen_once(
+        EVENT_HOMEASSISTANT_STOP, hass_callback(lambda event: cancel())
+    )
 
     return True
 
