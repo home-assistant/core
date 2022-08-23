380b727ebe49332ebffbeba7f7560b15808d533e
Dan Quintas <dan@quint.as>
2022-08-23 17:46:34 +0000

    implemented feedback on tests

--- tests/components/volvooncall/test_config_flow.py
+++ tests/components/volvooncall/test_config_flow.py
@@ -1,12 +1,14 @@
 """Test the Volvo On Call config flow."""
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
-from aiohttp.client_exceptions import ClientResponseError
+from aiohttp import ClientResponseError
 
 from homeassistant import config_entries
 from homeassistant.components.volvooncall.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
+
+from tests.common import MockConfigEntry
 
 
 async def test_form(hass: HomeAssistant) -> None:
@@ -51,11 +53,11 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
+    exc = ClientResponseError(Mock(), (), status=401)
+
     with patch(
-        "aiohttp.client_exceptions.ClientResponseError.__init__", return_value=None
-    ), patch(
         "volvooncall.Connection.get",
-        side_effect=ClientResponseError,
+        side_effect=exc,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -70,6 +72,37 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
 
     assert result2["type"] == FlowResultType.FORM
     assert result2["errors"] == {"base": "invalid_auth"}
+
+
+async def test_flow_already_configured(hass: HomeAssistant) -> None:
+    """Test we handle a flow that has already been configured."""
+    first_entry = MockConfigEntry(domain=DOMAIN, unique_id="test-username")
+    first_entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert len(result["errors"]) == 0
+
+    with patch("volvooncall.Connection.get", return_value="",), patch(
+        "homeassistant.components.volvooncall.async_setup_entry",
+        return_value=True,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+                "region": "na",
+                "mutable": True,
+                "scandinavian_miles": False,
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] == FlowResultType.ABORT
+    assert result2["reason"] == "already_configured"
 
 
 async def test_form_other_exception(hass: HomeAssistant) -> None:
@@ -98,7 +131,7 @@ async def test_form_other_exception(hass: HomeAssistant) -> None:
 
 
 async def test_import(hass: HomeAssistant) -> None:
-    """Test we get the form."""
+    """Test a YAML import."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_IMPORT}
     )
@@ -107,6 +140,9 @@ async def test_import(hass: HomeAssistant) -> None:
 
     with patch("volvooncall.Connection.get", return_value="",), patch(
         "homeassistant.components.volvooncall.async_setup",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.volvooncall.async_setup_entry",
         return_value=True,
     ) as mock_setup_entry:
         result2 = await hass.config_entries.flow.async_configure(
