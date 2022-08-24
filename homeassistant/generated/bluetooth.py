5ba1641f8129a29308c0fc474986494b4fd69d09
J. Nick Koston <nick@koston.org>
2022-08-23 19:13:09 -0700

    Add thermopro integration

--- homeassistant/generated/bluetooth.py
+++ homeassistant/generated/bluetooth.py
@@ -134,6 +134,11 @@ BLUETOOTH: list[dict[str, bool | str | int | list[int]]] = [
         "connectable": False
     },
     {
+        "domain": "thermopro",
+        "local_name": "TH35*",
+        "connectable": False
+    },
+    {
         "domain": "xiaomi_ble",
         "connectable": False,
         "service_data_uuid": "0000fe95-0000-1000-8000-00805f9b34fb"
