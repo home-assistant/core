2497ff5a39292b7167733206fe2e1e63a6a3b86e
Erik Montnemery <erik@montnemery.com>
2022-08-24 11:09:23 +0200

    Add energy and gas sensors to demo integration (#77206)

--- tests/components/demo/test_init.py
+++ tests/components/demo/test_init.py
@@ -57,7 +57,7 @@ async def test_demo_statistics(hass, recorder_mock):
     assert {
         "has_mean": True,
         "has_sum": False,
-        "name": None,
+        "name": "Outdoor temperature",
         "source": "demo",
         "statistic_id": "demo:temperature_outdoor",
         "unit_of_measurement": "°C",
@@ -65,9 +65,9 @@ async def test_demo_statistics(hass, recorder_mock):
     assert {
         "has_mean": False,
         "has_sum": True,
-        "name": None,
+        "name": "Energy consumption 1",
         "source": "demo",
-        "statistic_id": "demo:energy_consumption",
+        "statistic_id": "demo:energy_consumption_kwh",
         "unit_of_measurement": "kWh",
     } in statistic_ids
 
