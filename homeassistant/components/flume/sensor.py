ee2a8cc8016192b7859235bc268c31244080183d
Jeef <jeeftor@users.noreply.github.com>
2022-08-25 13:45:35 -0600

    Refactor Flume to use base entity class (#77115)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/flume/sensor.py
+++ homeassistant/components/flume/sensor.py
@@ -5,14 +5,10 @@ from pyflume import FlumeData
 
 from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import (
-    DEFAULT_NAME,
     DEVICE_SCAN_INTERVAL,
     DOMAIN,
     FLUME_AUTH,
@@ -22,11 +18,11 @@ from .const import (
     FLUME_TYPE_SENSOR,
     KEY_DEVICE_ID,
     KEY_DEVICE_LOCATION,
-    KEY_DEVICE_LOCATION_NAME,
     KEY_DEVICE_LOCATION_TIMEZONE,
     KEY_DEVICE_TYPE,
 )
 from .coordinator import FlumeDeviceDataUpdateCoordinator
+from .entity import FlumeEntity
 
 
 async def async_setup_entry(
@@ -35,14 +31,12 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the Flume sensor."""
+
     flume_domain_data = hass.data[DOMAIN][config_entry.entry_id]
 
     flume_auth = flume_domain_data[FLUME_AUTH]
     http_session = flume_domain_data[FLUME_HTTP_SESSION]
     flume_devices = flume_domain_data[FLUME_DEVICES]
-
-    config = config_entry.data
-    name = config.get(CONF_NAME, DEFAULT_NAME)
 
     flume_entity_list = []
     for device in flume_devices.device_list:
@@ -50,9 +44,7 @@ async def async_setup_entry(
             continue
 
         device_id = device[KEY_DEVICE_ID]
-        device_name = device[KEY_DEVICE_LOCATION][KEY_DEVICE_LOCATION_NAME]
         device_timezone = device[KEY_DEVICE_LOCATION][KEY_DEVICE_LOCATION_TIMEZONE]
-        device_friendly_name = f"{name} {device_name}"
 
         flume_device = FlumeData(
             flume_auth,
@@ -72,7 +64,6 @@ async def async_setup_entry(
                 FlumeSensor(
                     coordinator=coordinator,
                     description=description,
-                    name=device_friendly_name,
                     device_id=device_id,
                 )
                 for description in FLUME_QUERIES_SENSOR
@@ -83,28 +74,19 @@ async def async_setup_entry(
         async_add_entities(flume_entity_list)
 
 
-class FlumeSensor(CoordinatorEntity, SensorEntity):
+class FlumeSensor(FlumeEntity, SensorEntity):
     """Representation of the Flume sensor."""
+
+    coordinator: FlumeDeviceDataUpdateCoordinator
 
     def __init__(
         self,
-        coordinator,
-        name,
-        device_id,
+        coordinator: FlumeDeviceDataUpdateCoordinator,
+        device_id: str,
         description: SensorEntityDescription,
-    ):
-        """Initialize the Flume sensor."""
-        super().__init__(coordinator)
-        self.entity_description = description
-
-        self._attr_name = f"{name} {description.name}"
-        self._attr_unique_id = f"{description.key}_{device_id}"
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, device_id)},
-            manufacturer="Flume, Inc.",
-            model="Flume Smart Water Monitor",
-            name=self.name,
-        )
+    ) -> None:
+        """Inlitializer function with type hints."""
+        super().__init__(coordinator, description, device_id)
 
     @property
     def native_value(self):
@@ -114,13 +96,6 @@ class FlumeSensor(CoordinatorEntity, SensorEntity):
             return None
 
         return _format_state_value(self.coordinator.flume_device.values[sensor_key])
-
-    async def async_added_to_hass(self) -> None:
-        """Request an update when added."""
-        await super().async_added_to_hass()
-        # We do not ask for an update with async_add_entities()
-        # because it will update disabled entities
-        await self.coordinator.async_request_refresh()
 
 
 def _format_state_value(value):
