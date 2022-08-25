0add85a8421cf276fa98dc17dc3774250e6b40b0
spycle <48740594+spycle@users.noreply.github.com>
2022-08-25 14:37:41 +0100

    Remove duplication
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/keymitt_ble/config_flow.py
+++ homeassistant/components/keymitt_ble/config_flow.py
@@ -94,8 +94,8 @@ class MicroBotConfigFlow(ConfigFlow, domain=DOMAIN):
                 parsed = parse_advertisement_data(
                     discovery_info.device, discovery_info.advertisement
                 )
-                self._discovered_adv = parsed
                 if parsed:
+                    self._discovered_adv = parsed
                     self._discovered_advs[address] = parsed
 
         if not self._discovered_advs:
