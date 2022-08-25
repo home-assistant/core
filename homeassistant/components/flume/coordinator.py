e30ddaabdfd4656d8163fbfc07ae936d2c566a26
Jeef <jeeftor@users.noreply.github.com>
2022-08-25 07:26:21 -0600

    Update homeassistant/components/flume/coordinator.py
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/flume/coordinator.py
+++ homeassistant/components/flume/coordinator.py
@@ -31,7 +31,7 @@ class FlumeDeviceDataUpdateCoordinator(DataUpdateCoordinator[FlumeData]):
         except Exception as ex:
             raise UpdateFailed(f"Error communicating with flume API: {ex}") from ex
         _LOGGER.debug(
-            "Flume update details: {'values': %s, 'query_payload': %s}",
+            "Flume update details: values=%s query_payload=%s",
             self.flume_device.values,
             self.flume_device.query_payload,
         )
