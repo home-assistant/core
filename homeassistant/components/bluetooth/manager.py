adcb644d26d0bca740b6f02119a419112aa572fa
J. Nick Koston <nick@koston.org>
2022-08-27 16:41:49 -0500

    Add diagnostics to bluetooth (#77393)

--- homeassistant/components/bluetooth/manager.py
+++ homeassistant/components/bluetooth/manager.py
@@ -1,11 +1,13 @@
 """The bluetooth integration."""
 from __future__ import annotations
 
+import asyncio
 from collections.abc import Callable, Iterable
+from dataclasses import asdict
 from datetime import datetime, timedelta
 import itertools
 import logging
-from typing import TYPE_CHECKING, Final
+from typing import TYPE_CHECKING, Any, Final
 
 from bleak.backends.scanner import AdvertisementDataCallback
 
@@ -144,6 +146,28 @@ class BluetoothManager:
         self._scanners: list[BaseHaScanner] = []
         self._connectable_scanners: list[BaseHaScanner] = []
         self._adapters: dict[str, AdapterDetails] = {}
+
+    async def async_diagnostics(self) -> dict[str, Any]:
+        """Diagnostics for the manager."""
+        scanner_diagnostics = await asyncio.gather(
+            *[
+                scanner.async_diagnostics()
+                for scanner in itertools.chain(
+                    self._scanners, self._connectable_scanners
+                )
+            ]
+        )
+        return {
+            "adapters": self._adapters,
+            "scanners": scanner_diagnostics,
+            "connectable_history": [
+                asdict(service_info)
+                for service_info in self._connectable_history.values()
+            ],
+            "history": [
+                asdict(service_info) for service_info in self._history.values()
+            ],
+        }
 
     def _find_adapter_by_address(self, address: str) -> str | None:
         for adapter, details in self._adapters.items():
