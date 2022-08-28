e60312235e7a10f313aab732c7760d1310073c1d
J. Nick Koston <nick@koston.org>
2022-08-27 19:06:41 -0500

    Add lutron alternate brands

--- homeassistant/generated/supported_brands.py
+++ homeassistant/generated/supported_brands.py
@@ -8,6 +8,7 @@ To update, run python3 -m script.hassfest
 HAS_SUPPORTED_BRANDS = (
     "denonavr",
     "hunterdouglas_powerview",
+    "lutron",
     "motion_blinds",
     "overkiz",
     "renault",
