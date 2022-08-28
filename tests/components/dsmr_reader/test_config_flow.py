d4d7b4b2fc14060957921fdc40a484c9e748945d
Tom Puttemans <github@tomputtemans.com>
2022-08-29 01:56:05 +0200

    Add test for MQTT presence validation and fix existing test

--- tests/components/dsmr_reader/test_config_flow.py
+++ tests/components/dsmr_reader/test_config_flow.py
@@ -1,5 +1,8 @@
 """Tests for the config flow."""
+from unittest.mock import MagicMock
+
 from homeassistant.components.dsmr_reader.const import DOMAIN
+from homeassistant.components.mqtt import DATA_MQTT
 from homeassistant.config_entries import SOURCE_IMPORT, SOURCE_USER
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
@@ -22,6 +25,24 @@ async def test_import_step(hass: HomeAssistant):
     assert second_result["reason"] == "single_instance_allowed"
 
 
+async def test_user_step_without_mqtt(hass: HomeAssistant):
+    """Test the user step call without mqtt."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "confirm"
+    assert result["errors"] is None
+
+    config_result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={}
+    )
+
+    assert config_result["type"] == FlowResultType.ABORT
+    assert config_result["reason"] == "no_devices_found"
+
+
 async def test_user_step_with_mqtt(hass: HomeAssistant):
     """Test the user step call with mqtt available."""
     result = await hass.config_entries.flow.async_init(
@@ -31,6 +52,10 @@ async def test_user_step_with_mqtt(hass: HomeAssistant):
     assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "confirm"
     assert result["errors"] is None
+
+    # configure bogus mqtt service to pass flow
+    hass.services.async_register("mqtt", "publish", None)
+    hass.data[DATA_MQTT] = {"async_subscribe": MagicMock()}
 
     config_result = await hass.config_entries.flow.async_configure(
         result["flow_id"], user_input={}
