cfc691cd7fda229f946e6ff25370a6806c2f4df0
Clifford Roche <clifford.roche@gmail.com>
2022-08-27 20:59:18 -0400

    Update homeassistant/components/moonraker/__init__.py
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/moonraker/__init__.py
+++ homeassistant/components/moonraker/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import logging
 
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
