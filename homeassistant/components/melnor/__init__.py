8d37f908afd6fc0c9f3428c4de26a0b979b7af08
Justin Vanderhooft <justinvdhooft@gmail.com>
2022-08-28 15:02:43 -0400

    fixup! tweak model attributes

--- homeassistant/components/melnor/__init__.py
+++ homeassistant/components/melnor/__init__.py
@@ -34,8 +34,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id]["coordinator"] = coordinator
 
+    coordinator = MelnorDataUpdateCoordinator(hass, device)
     await coordinator.async_config_entry_first_refresh()
 
+    hass.data[DOMAIN][entry.entry_id] = coordinator
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
@@ -44,7 +46,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
 
-    device: Device = hass.data[DOMAIN][entry.entry_id]["coordinator"].data
+    device: Device = hass.data[DOMAIN][entry.entry_id].data
 
     await device.disconnect()
 
