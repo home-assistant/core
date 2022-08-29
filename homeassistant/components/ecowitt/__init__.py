57f1e56da45459377140cb549637255ea3d39fad
Pascal Vizeli <pvizeli@syshack.ch>
2022-08-29 15:24:41 +0200

    Update homeassistant/components/ecowitt/__init__.py
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- homeassistant/components/ecowitt/__init__.py
+++ homeassistant/components/ecowitt/__init__.py
@@ -20,7 +20,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         port=entry.data[CONF_PORT], path=entry.data[CONF_PATH]
     )
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     await ecowitt.start()
 
