d4ac0c66683919364b08e229fd78da05b60f7bb9
J. Nick Koston <nick@koston.org>
2022-08-30 17:04:07 -0500

    fix tests

--- tests/components/sensorpro/test_sensor.py
+++ tests/components/sensorpro/test_sensor.py
@@ -39,7 +39,7 @@ async def test_sensors(hass):
     await hass.async_block_till_done()
     assert len(hass.states.async_all("sensor")) == 4
 
-    humid_sensor = hass.states.get("sensor.lanyard_mini_hygrometer_eeff_humidity")
+    humid_sensor = hass.states.get("sensor.t201_eeff_humidity")
     humid_sensor_attrs = humid_sensor.attributes
     assert humid_sensor.state == "43.38"
     assert (
