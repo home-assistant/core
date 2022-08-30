ba499dff25623a30e47e16c03fff0101a3fb3732
J. Nick Koston <nick@koston.org>
2022-08-30 13:29:13 -0500

    Add Nutrichef as a supported brand of inkbird (#77551)

--- homeassistant/generated/supported_brands.py
+++ homeassistant/generated/supported_brands.py
@@ -8,6 +8,7 @@ To update, run python3 -m script.hassfest
 HAS_SUPPORTED_BRANDS = (
     "denonavr",
     "hunterdouglas_powerview",
+    "inkbird",
     "motion_blinds",
     "overkiz",
     "renault",
