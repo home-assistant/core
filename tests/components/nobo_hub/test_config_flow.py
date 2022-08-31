972dbe12e51a9a39a66154f60fa271c2429bda25
Øyvind Matheson Wergeland <oyvind@wergeland.org>
2022-08-31 21:32:41 +0200

    Apply suggestions from code review
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/nobo_hub/test_config_flow.py
+++ tests/components/nobo_hub/test_config_flow.py
@@ -260,6 +260,7 @@ async def test_options_flow(hass: HomeAssistant) -> None:
         "homeassistant.components.nobo_hub.async_setup_entry", return_value=True
     ):
         assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
 
     result = await hass.config_entries.options.async_init(config_entry.entry_id)
 
