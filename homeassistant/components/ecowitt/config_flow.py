708e61482373dc9cb406cf39b203c12cafaef70a
Paulus Schoutsen <balloob@gmail.com>
2022-08-31 12:41:04 -0400

    Migrate Ecowitt to webhooks (#77610)

--- homeassistant/components/ecowitt/config_flow.py
+++ homeassistant/components/ecowitt/config_flow.py
@@ -1,77 +1,50 @@
 """Config flow for ecowitt."""
 from __future__ import annotations
 
-import logging
 import secrets
 from typing import Any
 
-from aioecowitt import EcoWittListener
-import voluptuous as vol
+from yarl import URL
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_PORT
-from homeassistant.core import HomeAssistant
+from homeassistant.components import webhook
+from homeassistant.const import CONF_WEBHOOK_ID
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
-import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.network import get_url
 
-from .const import CONF_PATH, DEFAULT_PORT, DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
-
-STEP_USER_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-        vol.Optional(CONF_PATH, default=f"/{secrets.token_urlsafe(16)}"): cv.string,
-    }
-)
-
-
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, str]:
-    """Validate user input."""
-    # Check if the port is in use
-    try:
-        listener = EcoWittListener(port=data[CONF_PORT])
-        await listener.start()
-        await listener.stop()
-    except OSError:
-        raise InvalidPort from None
-
-    return {"title": f"Ecowitt on port {data[CONF_PORT]}"}
+from .const import DOMAIN
 
 
 class EcowittConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for the Ecowitt."""
 
     VERSION = 1
+    _webhook_id: str
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
         if user_input is None:
+            self._webhook_id = secrets.token_hex(16)
             return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+                step_id="user",
             )
 
-        errors = {}
+        base_url = URL(get_url(self.hass))
+        assert base_url.host
 
-        # Check if the port is in use by another config entry
-        self._async_abort_entries_match({CONF_PORT: user_input[CONF_PORT]})
-
-        try:
-            info = await validate_input(self.hass, user_input)
-        except InvalidPort:
-            errors["base"] = "invalid_port"
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
-        else:
-            return self.async_create_entry(title=info["title"], data=user_input)
-
-        return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        return self.async_create_entry(
+            title="Ecowitt",
+            data={
+                CONF_WEBHOOK_ID: self._webhook_id,
+            },
+            description_placeholders={
+                "path": webhook.async_generate_path(self._webhook_id),
+                "server": base_url.host,
+                "port": str(base_url.port),
+            },
         )
 
 
