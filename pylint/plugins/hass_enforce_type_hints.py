d265ce5a971f720f3937c61cbac29324dccc1227
epenet <6771947+epenet@users.noreply.github.com>
2022-09-01 08:05:55 +0200

    Apply suggestion
    Co-authored-by: Marc Mueller <30130371+cdce8p@users.noreply.github.com>

--- pylint/plugins/hass_enforce_type_hints.py
+++ pylint/plugins/hass_enforce_type_hints.py
@@ -2535,7 +2535,7 @@ def _is_valid_return_type(match: TypeHintMatch, node: nodes.NodeNG) -> bool:
 
     if (
         match.check_return_type_inheritance
-        and (isinstance(match.return_type, str) or isinstance(match.return_type, list))
+        and isinstance(match.return_type, (str, list))
         and isinstance(node, nodes.Name)
     ):
         if isinstance(match.return_type, str):
