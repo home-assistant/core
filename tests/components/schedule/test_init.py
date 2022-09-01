252bc3b0b3fe651e5f349bba450586ec8f18a12e
Erik <erik@montnemery.com>
2022-09-01 23:47:10 +0200

    Improve tests

--- tests/components/schedule/test_init.py
+++ tests/components/schedule/test_init.py
@@ -225,21 +225,10 @@ async def test_events_one_day(
     assert state.attributes[ATTR_NEXT_EVENT].isoformat() == "2022-09-11T07:00:00-07:00"
 
 
-@pytest.mark.parametrize(
-    "sun_schedule, mon_schedule",
-    (
-        (
-            {CONF_FROM: "23:00:00", CONF_TO: "24:00:00"},
-            {CONF_FROM: "00:00:00", CONF_TO: "01:00:00"},
-        ),
-    ),
-)
-async def test_adjacent(
+async def test_adjacent_cross_midnight(
     hass: HomeAssistant,
     schedule_setup: Callable[..., Coroutine[Any, Any, bool]],
     caplog: pytest.LogCaptureFixture,
-    sun_schedule: dict[str, str],
-    mon_schedule: dict[str, str],
     freezer,
 ) -> None:
     """Test adjacent events don't toggle on->off->on."""
@@ -251,8 +240,8 @@ async def test_adjacent(
                 "from_yaml": {
                     CONF_NAME: "from yaml",
                     CONF_ICON: "mdi:party-popper",
-                    CONF_SUNDAY: sun_schedule,
-                    CONF_MONDAY: mon_schedule,
+                    CONF_SUNDAY: {CONF_FROM: "23:00:00", CONF_TO: "24:00:00"},
+                    CONF_MONDAY: {CONF_FROM: "00:00:00", CONF_TO: "01:00:00"},
                 }
             }
         },
@@ -289,6 +278,69 @@ async def test_adjacent(
     assert state
     assert state.state == STATE_OFF
     assert state.attributes[ATTR_NEXT_EVENT].isoformat() == "2022-09-11T23:00:00-07:00"
+
+    await hass.async_block_till_done()
+    assert len(state_changes) == 3
+    for event in state_changes[:-1]:
+        assert event.data["new_state"].state == STATE_ON
+    assert state_changes[2].data["new_state"].state == STATE_OFF
+
+
+async def test_adjacent_within_day(
+    hass: HomeAssistant,
+    schedule_setup: Callable[..., Coroutine[Any, Any, bool]],
+    caplog: pytest.LogCaptureFixture,
+    freezer,
+) -> None:
+    """Test adjacent events don't toggle on->off->on."""
+    freezer.move_to("2022-08-30 13:20:00-07:00")
+
+    assert await schedule_setup(
+        config={
+            DOMAIN: {
+                "from_yaml": {
+                    CONF_NAME: "from yaml",
+                    CONF_ICON: "mdi:party-popper",
+                    CONF_SUNDAY: [
+                        {CONF_FROM: "22:00:00", CONF_TO: "22:30:00"},
+                        {CONF_FROM: "22:30:00", CONF_TO: "23:00:00"},
+                    ],
+                }
+            }
+        },
+        items=[],
+    )
+
+    state = hass.states.get(f"{DOMAIN}.from_yaml")
+    assert state
+    assert state.state == STATE_OFF
+    assert state.attributes[ATTR_NEXT_EVENT].isoformat() == "2022-09-04T22:00:00-07:00"
+
+    state_changes = async_capture_events(hass, EVENT_STATE_CHANGED)
+
+    freezer.move_to(state.attributes[ATTR_NEXT_EVENT])
+    async_fire_time_changed(hass)
+
+    state = hass.states.get(f"{DOMAIN}.from_yaml")
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes[ATTR_NEXT_EVENT].isoformat() == "2022-09-04T22:30:00-07:00"
+
+    freezer.move_to(state.attributes[ATTR_NEXT_EVENT])
+    async_fire_time_changed(hass)
+
+    state = hass.states.get(f"{DOMAIN}.from_yaml")
+    assert state
+    assert state.state == STATE_ON
+    assert state.attributes[ATTR_NEXT_EVENT].isoformat() == "2022-09-04T23:00:00-07:00"
+
+    freezer.move_to(state.attributes[ATTR_NEXT_EVENT])
+    async_fire_time_changed(hass)
+
+    state = hass.states.get(f"{DOMAIN}.from_yaml")
+    assert state
+    assert state.state == STATE_OFF
+    assert state.attributes[ATTR_NEXT_EVENT].isoformat() == "2022-09-11T22:00:00-07:00"
 
     await hass.async_block_till_done()
     assert len(state_changes) == 3
