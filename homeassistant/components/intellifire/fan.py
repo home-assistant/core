59c8c690f72fd3bc168bdf5fefe7faada891ad22
jeff <jeffstein@gmail.com>
2022-09-01 14:53:29 -0600

    fix: Updating state correctly

--- homeassistant/components/intellifire/fan.py
+++ homeassistant/components/intellifire/fan.py
@@ -99,9 +99,11 @@ class IntellifireFan(IntellifireEntity, FanEntity):
         # Calculate percentage steps
         LOGGER.debug("Setting Fan Speed %s", percentage)
 
-        int_value = math.ceil(percentage_to_ranged_value(self.entity_description.speed_range, percentage))
+        int_value = math.ceil(
+            percentage_to_ranged_value(self.entity_description.speed_range, percentage)
+        )
         await self.entity_description.set_fn(self.coordinator.control_api, int_value)
-        await self.async_update_ha_state(force_refresh=True)
+        await self.coordinator.async_request_refresh()
 
     async def async_turn_on(
         self,
@@ -111,10 +113,10 @@ class IntellifireFan(IntellifireEntity, FanEntity):
     ) -> None:
         """Turn on the fan."""
         await self.entity_description.set_fn(self.coordinator.control_api, 1)
-        await self.async_update_ha_state(force_refresh=True)
+        await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the fan."""
         self.coordinator.control_api.fan_off()
         await self.entity_description.set_fn(self.coordinator.control_api, 0)
-        await self.async_update_ha_state(force_refresh=True)
+        await self.coordinator.async_request_refresh()
