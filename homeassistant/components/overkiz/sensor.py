36cfe479049d43f0a87099c7b6e212ebce586e43
Mick Vleeshouwer <mick@imick.nl>
2022-09-03 22:18:56 +0200

    Enhance operating time sensor in Overkiz integration (#76688)

--- homeassistant/components/overkiz/sensor.py
+++ homeassistant/components/overkiz/sensor.py
@@ -354,10 +354,16 @@ SENSOR_DESCRIPTIONS: list[OverkizSensorDescription] = [
     OverkizSensorDescription(
         key=OverkizState.IO_HEAT_PUMP_OPERATING_TIME,
         name="Heat Pump Operating Time",
+        icon="mdi:home-clock",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=TIME_SECONDS,
     ),
     OverkizSensorDescription(
         key=OverkizState.IO_ELECTRIC_BOOSTER_OPERATING_TIME,
         name="Electric Booster Operating Time",
+        icon="mdi:home-clock",
+        native_unit_of_measurement=TIME_SECONDS,
+        entity_category=EntityCategory.DIAGNOSTIC,
     ),
     # Cover
     OverkizSensorDescription(
