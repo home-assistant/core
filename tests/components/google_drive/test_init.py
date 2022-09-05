0f27c3d71339beed6b0328a9d37ce6e03ba6e0eb
Allen Porter <allen@thebends.org>
2022-09-05 14:58:16 -0700

    Add test coverage for service calls

--- tests/components/google_drive/test_init.py
+++ tests/components/google_drive/test_init.py
@@ -1,60 +1,79 @@
 """Tests for Google Drive."""
 
 from collections.abc import Awaitable, Callable, Generator
+import http
 import time
 from unittest.mock import patch
 
 import pytest
 
+from homeassistant.components.application_credentials import (
+    ClientCredential,
+    async_import_client_credential,
+)
 from homeassistant.components.google_drive import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
 from tests.common import MockConfigEntry
+from tests.test_util.aiohttp import AiohttpClientMocker
 
 TEST_SHEET_ID = "google-sheet-it"
 
 ComponentSetup = Callable[[], Awaitable[None]]
 
 
-@pytest.fixture
-async def scopes() -> list[str]:
+@pytest.fixture(name="scopes")
+def mock_scopes() -> list[str]:
     """Fixture to set the scopes present in the OAuth token."""
     return ["https://www.googleapis.com/auth/drive.file"]
 
 
-@pytest.fixture
-async def config_entry(scopes: list[str]) -> MockConfigEntry:
+@pytest.fixture(name="expires_at")
+def mock_expires_at() -> int:
+    """Fixture to set the oauth token expiration time."""
+    return time.time() + 3600
+
+
+@pytest.fixture(name="config_entry")
+def mock_config_entry(expires_at: int, scopes: list[str]) -> MockConfigEntry:
     """Fixture for MockConfigEntry."""
     return MockConfigEntry(
         domain=DOMAIN,
         unique_id=TEST_SHEET_ID,
         data={
+            "auth_implementation": DOMAIN,
             "token": {
                 "access_token": "mock-access-token",
-                "expires_at": time.time() + 3600,
+                "refresh_token": "mock-refresh-token",
+                "expires_at": expires_at,
                 "scope": " ".join(scopes),
             },
         },
     )
 
 
-@pytest.fixture
-async def setup_integration(
+@pytest.fixture(name="setup_integration")
+async def mock_setup_integration(
     hass: HomeAssistant, config_entry: MockConfigEntry
 ) -> Generator[ComponentSetup, None, None]:
     """Fixture for setting up the component."""
     config_entry.add_to_hass(hass)
 
+    assert await async_setup_component(hass, "application_credentials", {})
+    await async_import_client_credential(
+        hass,
+        DOMAIN,
+        ClientCredential("client-id", "client-secret"),
+        DOMAIN,
+    )
+
     async def func() -> None:
         assert await async_setup_component(hass, DOMAIN, {})
         await hass.async_block_till_done()
 
-    with patch(
-        "homeassistant.components.google_drive.async_get_config_entry_implementation"
-    ):
-        yield func
+    yield func
 
     # Verify clean unload
     entries = hass.config_entries.async_entries(DOMAIN)
@@ -91,7 +110,7 @@ async def test_setup_success(
 async def test_missing_required_scopes_requires_reauth(
     hass: HomeAssistant, setup_integration: ComponentSetup
 ) -> None:
-    """Test successful setup and unload."""
+    """Test that reauth is invoked when required scopes are not present."""
     await setup_integration()
 
     entries = hass.config_entries.async_entries(DOMAIN)
@@ -101,3 +120,94 @@ async def test_missing_required_scopes_requires_reauth(
     flows = hass.config_entries.flow.async_progress()
     assert len(flows) == 1
     assert flows[0]["step_id"] == "reauth_confirm"
+
+
+@pytest.mark.parametrize("expires_at", [time.time() - 3600])
+async def test_expired_token_refresh_success(
+    hass: HomeAssistant,
+    setup_integration: ComponentSetup,
+    scopes: list[str],
+    aioclient_mock: AiohttpClientMocker,
+) -> None:
+    """Test expired token is refreshed."""
+
+    aioclient_mock.post(
+        "https://oauth2.googleapis.com/token",
+        json={
+            "access_token": "updated-access-token",
+            "refresh_token": "updated-refresh-token",
+            "expires_at": time.time() + 3600,
+            "expires_in": 3600,
+        },
+    )
+
+    await setup_integration()
+
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
+    assert entries[0].state is ConfigEntryState.LOADED
+    assert entries[0].data["token"]["access_token"] == "updated-access-token"
+    assert entries[0].data["token"]["expires_in"] == 3600
+
+
+@pytest.mark.parametrize(
+    "expires_at,status,expected_state",
+    [
+        (
+            time.time() - 3600,
+            http.HTTPStatus.UNAUTHORIZED,
+            ConfigEntryState.SETUP_ERROR,
+        ),
+        (
+            time.time() - 3600,
+            http.HTTPStatus.INTERNAL_SERVER_ERROR,
+            ConfigEntryState.SETUP_RETRY,
+        ),
+    ],
+    ids=["failure_requires_reauth", "transient_failure"],
+)
+async def test_expired_token_refresh_failure(
+    hass: HomeAssistant,
+    setup_integration: ComponentSetup,
+    scopes: list[str],
+    aioclient_mock: AiohttpClientMocker,
+    status: http.HTTPStatus,
+    expected_state: ConfigEntryState,
+) -> None:
+    """Test failure while refreshing token with a transient error."""
+
+    aioclient_mock.post(
+        "https://oauth2.googleapis.com/token",
+        status=status,
+    )
+
+    await setup_integration()
+
+    # Verify a transient failure has occurred
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert entries[0].state is expected_state
+
+
+async def test_append_sheet(
+    hass: HomeAssistant,
+    setup_integration: ComponentSetup,
+    config_entry: MockConfigEntry,
+) -> None:
+    """Test successful setup and unload."""
+    await setup_integration()
+
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
+    assert entries[0].state is ConfigEntryState.LOADED
+
+    with patch("homeassistant.components.google_drive.Client"):
+        await hass.services.async_call(
+            DOMAIN,
+            "append_sheet",
+            {
+                "config_entry": config_entry.entry_id,
+                "worksheet": "Sheet1",
+                "data": ["example"],
+            },
+            blocking=True,
+        )
