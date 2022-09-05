50c0cd23649d7b08494a47d8c0d815eb9e42d276
dougiteixeira <douglas.teixeira2@gmail.com>
2022-09-05 05:52:42 +0000

    Update tests config flow

--- tests/components/proxmoxve/test_config_flow.py
+++ tests/components/proxmoxve/test_config_flow.py
@@ -85,22 +85,6 @@ USER_INPUT_IMPORT_OK = {
     ],
 }
 
-USER_INPUT_IMPORT_ERRORS = {
-    CONF_HOST: "192.168.10.101",
-    CONF_PORT: 8006,
-    CONF_USERNAME: "root",
-    CONF_PASSWORD: "secret",
-    CONF_REALM: "pam",
-    CONF_VERIFY_SSL: True,
-    CONF_NODES: [
-        {
-            CONF_NODE: "pve",
-            CONF_VMS: [100, 101],
-            CONF_CONTAINERS: [201, 202],
-        },
-    ],
-}
-
 USER_INPUT_NOT_EXIST = {
     CONF_HOST: "192.168.10.101",
     CONF_PORT: 8006,
@@ -115,12 +99,6 @@ USER_INPUT_NOT_EXIST = {
             CONF_CONTAINERS: [201, 202],
         },
     ],
-}
-
-USER_INPUT_REQONLY = {
-    CONF_HOST: "192.168.10.101",
-    CONF_USERNAME: "root",
-    CONF_PASSWORD: "secret",
 }
 
 USER_INPUT_PORT_TOO_BIG = {
@@ -296,7 +274,7 @@ async def test_flow_unknown_exception(hass: HomeAssistant):
 
 
 async def test_flow_already_configured(hass: HomeAssistant):
-    """Test flow import error entry already configured."""
+    """Test flow in case entry already configured."""
 
     entry = MockConfigEntry(
         domain=DOMAIN,
@@ -333,7 +311,7 @@ async def test_flow_already_configured(hass: HomeAssistant):
 
 
 async def test_flow_import_ok(hass: HomeAssistant):
-    """Test flow ok."""
+    """Test import flow ok."""
 
     with patch("proxmoxer.ProxmoxResource.get", return_value=MOCK_GET_RESPONSE), patch(
         "proxmoxer.backends.https.ProxmoxHTTPAuth._get_new_tokens",
@@ -387,7 +365,7 @@ async def test_flow_import_error_port_big(hass: HomeAssistant):
 
 
 async def test_flow_import_error_auth_error(hass: HomeAssistant):
-    """Test flow import error auth."""
+    """Test import errors in case username or password are incorrect."""
 
     with patch(
         "homeassistant.components.proxmoxve.ProxmoxClient.build_client",
@@ -397,7 +375,7 @@ async def test_flow_import_error_auth_error(hass: HomeAssistant):
 
         # imported config is identical to the one generated from config flow
         result = await hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": SOURCE_IMPORT}, data=USER_INPUT_IMPORT_ERRORS
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=USER_INPUT_IMPORT_OK
         )
 
         assert result["type"] == FlowResultType.ABORT
@@ -405,7 +383,7 @@ async def test_flow_import_error_auth_error(hass: HomeAssistant):
 
 
 async def test_flow_import_error_ssl_rejection(hass: HomeAssistant):
-    """Test flow import error SSL rejection."""
+    """Test import errors in case the SSL certificare is not present or is not valid or is expired."""
 
     with patch(
         "homeassistant.components.proxmoxve.ProxmoxClient.build_client",
@@ -423,7 +401,7 @@ async def test_flow_import_error_ssl_rejection(hass: HomeAssistant):
 
 
 async def test_flow_import_error_cant_connect(hass: HomeAssistant):
-    """Test flow import error can't connect."""
+    """Test import errors in case the connection fails."""
 
     with patch(
         "homeassistant.components.proxmoxve.ProxmoxClient.build_client",
@@ -433,7 +411,7 @@ async def test_flow_import_error_cant_connect(hass: HomeAssistant):
 
         # imported config is identical to the one generated from config flow
         result = await hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": SOURCE_IMPORT}, data=USER_INPUT_IMPORT_ERRORS
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=USER_INPUT_IMPORT_OK
         )
 
         assert result["type"] == FlowResultType.ABORT
@@ -441,7 +419,7 @@ async def test_flow_import_error_cant_connect(hass: HomeAssistant):
 
 
 async def test_flow_import_error_general_error(hass: HomeAssistant):
-    """Test flow import general error."""
+    """Test import errors in case of an unknown exception occurs."""
 
     with patch(
         "homeassistant.components.proxmoxve.ProxmoxClient.build_client",
@@ -451,7 +429,7 @@ async def test_flow_import_error_general_error(hass: HomeAssistant):
 
         # imported config is identical to the one generated from config flow
         result = await hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": SOURCE_IMPORT}, data=USER_INPUT_IMPORT_ERRORS
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=USER_INPUT_IMPORT_OK
         )
 
         assert result["type"] == FlowResultType.ABORT
@@ -459,7 +437,7 @@ async def test_flow_import_error_general_error(hass: HomeAssistant):
 
 
 async def test_flow_import_error_node_not_exist(hass: HomeAssistant):
-    """Test flow import error node not exist in Proxmox."""
+    """Test import error in case node not exist in Proxmox."""
 
     with patch("proxmoxer.ProxmoxResource.get", return_value=MOCK_GET_RESPONSE), patch(
         "homeassistant.components.proxmoxve.ProxmoxClient.build_client",
@@ -476,7 +454,7 @@ async def test_flow_import_error_node_not_exist(hass: HomeAssistant):
 
 
 async def test_flow_import_error_already_configured(hass: HomeAssistant):
-    """Test flow import error entry already configured."""
+    """Test import error in case entry already configured."""
 
     entry = MockConfigEntry(
         domain=DOMAIN,
@@ -571,11 +549,11 @@ async def test_step_reauth(hass: HomeAssistant) -> None:
         side_effect=SSLError,
         return_value=None,
     ):
-        result_auth_ssl_rejectio = await hass.config_entries.flow.async_configure(
+        result_auth_ssl_rejection = await hass.config_entries.flow.async_configure(
             result["flow_id"], user_input=USER_INPUT_AUTH
         )
-        assert result_auth_ssl_rejectio["type"] == FlowResultType.FORM
-        assert result_auth_ssl_rejectio["errors"][CONF_BASE] == "ssl_rejection"
+        assert result_auth_ssl_rejection["type"] == FlowResultType.FORM
+        assert result_auth_ssl_rejection["errors"][CONF_BASE] == "ssl_rejection"
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
@@ -616,3 +594,114 @@ async def test_step_reauth(hass: HomeAssistant) -> None:
         )
         assert result_auth_ssl_rejectio["type"] == FlowResultType.FORM
         assert result_auth_ssl_rejectio["errors"][CONF_BASE] == "general_error"
+
+
+async def test_options_flow_v1(hass: HomeAssistant):
+    """Test options config flow."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="Test",
+        data=USER_INPUT_OK,
+    )
+    entry.add_to_hass(hass)
+
+    with patch(
+        "homeassistant.components.proxmoxve.async_setup_entry", return_value=True
+    ):
+
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+        result = await hass.config_entries.options.async_init(entry.entry_id, data=None)
+
+        assert result["type"] == FlowResultType.FORM
+        assert result["step_id"] == "host"
+
+        result = await hass.config_entries.options.async_init(entry.entry_id)
+
+        with patch(
+            "homeassistant.components.proxmoxve.ProxmoxClient.build_client",
+            side_effect=proxmoxer.backends.https.AuthenticationError("mock msg"),
+            return_value=None,
+        ):
+
+            result_auth_error = await hass.config_entries.options.async_configure(
+                result["flow_id"],
+                user_input=USER_INPUT_OPTION_AUTH,
+            )
+            assert result_auth_error["type"] == FlowResultType.FORM
+            assert result_auth_error["errors"][CONF_USERNAME] == "auth_error"
+
+        with patch(
+            "homeassistant.components.proxmoxve.ProxmoxClient.build_client",
+            side_effect=SSLError,
+            return_value=None,
+        ):
+
+            result_auth_ssl_rejection = (
+                await hass.config_entries.options.async_configure(
+                    result["flow_id"],
+                    user_input=USER_INPUT_OPTION_AUTH,
+                )
+            )
+            assert result_auth_ssl_rejection["type"] == FlowResultType.FORM
+            assert (
+                result_auth_ssl_rejection["errors"][CONF_VERIFY_SSL] == "ssl_rejection"
+            )
+
+        with patch(
+            "homeassistant.components.proxmoxve.ProxmoxClient.build_client",
+            side_effect=ConnectTimeout,
+            return_value=None,
+        ):
+
+            result_auth_cant_connect = (
+                await hass.config_entries.options.async_configure(
+                    result["flow_id"],
+                    user_input=USER_INPUT_OPTION_AUTH,
+                )
+            )
+            assert result_auth_cant_connect["type"] == FlowResultType.FORM
+            assert result_auth_cant_connect["errors"][CONF_HOST] == "cant_connect"
+
+        with patch(
+            "homeassistant.components.proxmoxve.ProxmoxClient.build_client",
+            side_effect=Exception,
+            return_value=None,
+        ):
+
+            result_auth_general_error = (
+                await hass.config_entries.options.async_configure(
+                    result["flow_id"],
+                    user_input=USER_INPUT_OPTION_AUTH,
+                )
+            )
+            assert result_auth_general_error["type"] == FlowResultType.FORM
+            assert result_auth_general_error["errors"][CONF_BASE] == "general_error"
+
+        with patch(
+            "proxmoxer.ProxmoxResource.get", return_value=MOCK_GET_RESPONSE
+        ), patch(
+            "proxmoxer.backends.https.ProxmoxHTTPAuth._get_new_tokens",
+            return_value=None,
+        ):
+
+            result = await hass.config_entries.options.async_configure(
+                result["flow_id"],
+                user_input=USER_INPUT_OPTION_AUTH,
+            )
+
+            assert result["step_id"] == "selection_qemu_lxc"
+            assert result["type"] == FlowResultType.FORM
+            assert "flow_id" in result
+
+            result = await hass.config_entries.options.async_configure(
+                result["flow_id"],
+                user_input=USER_INPUT_QEMU_LXC,
+            )
+
+            assert result["type"] == FlowResultType.ABORT
+            assert result["reason"] == "changes_successful"
+
+            result = hass.config_entries.async_get_entry(entry.entry_id)
+            assert entry.data[CONF_USERNAME] == USER_INPUT_OPTION_AUTH[CONF_USERNAME]
