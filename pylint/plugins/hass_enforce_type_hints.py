0fbe81356c1aa28326453defa145f51c9fe589fe
epenet <6771947+epenet@users.noreply.github.com>
2022-09-06 12:18:29 +0200

    Add BaseNotificationService to pylint checks (#77663)
    * Add BaseNotificationService to pylint checks
    
    * Remove comment

--- pylint/plugins/hass_enforce_type_hints.py
+++ pylint/plugins/hass_enforce_type_hints.py
@@ -1873,6 +1873,20 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
             ],
         ),
     ],
+    "notify": [
+        ClassTypeHintMatch(
+            base_class="BaseNotificationService",
+            matches=[
+                TypeHintMatch(
+                    function_name="send_message",
+                    arg_types={1: "str"},
+                    kwargs_type="Any",
+                    return_type=None,
+                    has_async_counterpart=True,
+                ),
+            ],
+        ),
+    ],
     "remote": [
         ClassTypeHintMatch(
             base_class="Entity",
