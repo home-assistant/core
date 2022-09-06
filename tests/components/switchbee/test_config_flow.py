613efe1b5425d0f8930a17751e69cf6eff44701a
Jafar Atili <at.jafar@outlook.com>
2022-09-06 13:44:04 +0000

    test: increased config_flow test coverage

--- tests/components/switchbee/test_config_flow.py
+++ tests/components/switchbee/test_config_flow.py
@@ -2,9 +2,15 @@
 from unittest.mock import patch
 
 from homeassistant import config_entries
-from homeassistant.components.switchbee.config_flow import SwitchBeeError
+from homeassistant.components.switchbee.config_flow import DeviceType, SwitchBeeError
 from homeassistant.components.switchbee.const import CONF_SWITCHES_AS_LIGHTS, DOMAIN
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import (
+    CONF_DEVICES,
+    CONF_HOST,
+    CONF_PASSWORD,
+    CONF_SCAN_INTERVAL,
+    CONF_USERNAME,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import RESULT_TYPE_FORM, FlowResultType
 
@@ -171,3 +177,35 @@ async def test_form_entry_exists(hass):
 
     assert form_result["type"] == FlowResultType.ABORT
     assert form_result["reason"] == "already_configured"
+
+
+async def test_option_flow(hass):
+    """Test config flow options."""
+    entry = MockConfigEntry(
+        unique_id="a8:21:08:e7:67:b6",
+        domain=DOMAIN,
+        data={
+            CONF_HOST: "1.1.1.1",
+            CONF_USERNAME: "test-username",
+            CONF_PASSWORD: "test-password",
+            CONF_SWITCHES_AS_LIGHTS: False,
+        },
+        title="1.1.1.1",
+    )
+    entry.add_to_hass(hass)
+
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+    assert result["type"] == "form"
+    assert result["step_id"] == "init"
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_SCAN_INTERVAL: 10,
+            CONF_DEVICES: [DeviceType.Switch.display, DeviceType.GroupSwitch.display],
+        },
+    )
+    assert result["type"] == "create_entry"
+    assert result["data"] == {
+        CONF_SCAN_INTERVAL: 10,
+        CONF_DEVICES: [DeviceType.Switch.display, DeviceType.GroupSwitch.display],
+    }
