566fc405c9a194831d3824dffbd1a3b7d3f8a778
Steven Looman <steven.looman@gmail.com>
2022-09-06 17:33:16 +0200

    Use identifiers host and serial number to match device (#75657)

--- homeassistant/components/upnp/config_flow.py
+++ homeassistant/components/upnp/config_flow.py
@@ -13,6 +13,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import (
+    CONFIG_ENTRY_HOST,
     CONFIG_ENTRY_LOCATION,
     CONFIG_ENTRY_MAC_ADDRESS,
     CONFIG_ENTRY_ORIGINAL_UDN,
@@ -161,22 +162,25 @@ class UpnpFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         unique_id = discovery_info.ssdp_usn
         await self.async_set_unique_id(unique_id)
         mac_address = await _async_mac_address_from_discovery(self.hass, discovery_info)
+        host = discovery_info.ssdp_headers["_host"]
         self._abort_if_unique_id_configured(
             # Store mac address for older entries.
             # The location is stored in the config entry such that when the location changes, the entry is reloaded.
             updates={
                 CONFIG_ENTRY_MAC_ADDRESS: mac_address,
                 CONFIG_ENTRY_LOCATION: discovery_info.ssdp_location,
+                CONFIG_ENTRY_HOST: host,
             },
         )
 
         # Handle devices changing their UDN, only allow a single host.
         for entry in self._async_current_entries(include_ignore=True):
             entry_mac_address = entry.data.get(CONFIG_ENTRY_MAC_ADDRESS)
-            entry_st = entry.data.get(CONFIG_ENTRY_ST)
-            if entry_mac_address != mac_address:
+            entry_host = entry.data.get(CONFIG_ENTRY_HOST)
+            if entry_mac_address != mac_address and entry_host != host:
                 continue
 
+            entry_st = entry.data.get(CONFIG_ENTRY_ST)
             if discovery_info.ssdp_st != entry_st:
                 # Check ssdp_st to prevent swapping between IGDv1 and IGDv2.
                 continue
--- homeassistant/components/upnp/const.py
+++ homeassistant/components/upnp/const.py
@@ -6,7 +6,6 @@ from homeassistant.const import TIME_SECONDS
 
 LOGGER = logging.getLogger(__package__)
 
-CONF_LOCAL_IP = "local_ip"
 DOMAIN = "upnp"
 BYTES_RECEIVED = "bytes_received"
 BYTES_SENT = "bytes_sent"
@@ -24,7 +23,9 @@ CONFIG_ENTRY_UDN = "udn"
 CONFIG_ENTRY_ORIGINAL_UDN = "original_udn"
 CONFIG_ENTRY_MAC_ADDRESS = "mac_address"
 CONFIG_ENTRY_LOCATION = "location"
+CONFIG_ENTRY_HOST = "host"
+IDENTIFIER_HOST = "upnp_host"
+IDENTIFIER_SERIAL_NUMBER = "upnp_serial_number"
 DEFAULT_SCAN_INTERVAL = timedelta(seconds=30).total_seconds()
 ST_IGD_V1 = "urn:schemas-upnp-org:device:InternetGatewayDevice:1"
 ST_IGD_V2 = "urn:schemas-upnp-org:device:InternetGatewayDevice:2"
-SSDP_SEARCH_TIMEOUT = 4
