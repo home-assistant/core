db8fe650aa35f32d2c58d516ec42fc42195f7149
HarvsG <11440490+HarvsG@users.noreply.github.com>
2022-09-07 19:47:21 +0000

    Explain 2 step updating

--- tests/components/bayesian/test_binary_sensor.py
+++ tests/components/bayesian/test_binary_sensor.py
@@ -144,7 +144,7 @@ async def test_sensor_numeric_state(hass):
     ]
     assert abs(state.attributes.get("probability") - 0.111) < 0.01
     # As abve but since the value is equal to 4 then this is a negative observation (~B) where P(~B) == 1 - P(B) because B is binary
-    # We therefore want to calculate P(A|~B) so we use P(~B|A) (1-0.6) and P(~B|~A) (1-0.4)
+    # We therefore want to calculate P(A|~B) so we use P(~B|A) (1-0.7) and P(~B|~A) (1-0.4)
     # Calculated using bayes theorum where P(A) = 0.2, P(~B|A) = 1-0.7 (as negative observation), P(~B|notA) = 1-0.4 -> 0.11
 
     assert state.state == "off"
@@ -159,7 +159,8 @@ async def test_sensor_numeric_state(hass):
     assert state.attributes.get("observations")[1]["prob_given_true"] == 0.9
     assert state.attributes.get("observations")[1]["prob_given_false"] == 0.2
     assert abs(state.attributes.get("probability") - 0.663) < 0.01
-    # Calculated using bayes theorum where P(A) = 0.2, P(B|A) = 0.7, P(B|notA) = 0.4 -> 0.304
+    # Here we have two positive observations as both are in range. We do a 2-step bayes. The output of the first is used as the (updated) prior in the second.
+    # C1st step P(A) = 0.2, P(B|A) = 0.7, P(B|notA) = 0.4 -> 0.304
     # 2nd update: P(A) = 0.304, P(B|A) = 0.9, P(B|notA) = 0.2 -> 0.663
 
     assert state.state == "on"
