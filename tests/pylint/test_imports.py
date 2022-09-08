1f3c447f6ca30efd71283d8e181d55a77a8c2b97
epenet <6771947+epenet@users.noreply.github.com>
2022-09-08 08:06:36 +0000

    Add tests

--- tests/pylint/test_imports.py
+++ tests/pylint/test_imports.py
@@ -132,3 +132,40 @@ def test_bad_import(
         ),
     ):
         imports_checker.visit_importfrom(import_node)
+
+
+def test_bad_root_import(linter: UnittestLinter, imports_checker: BaseChecker) -> None:
+    """Ensure bad root imports are rejected."""
+
+    first_node, second_node = astroid.extract_node(
+        """
+    import homeassistant.components.climate.const as climate #@
+    from homeassistant.components.climate.const import ClimateEntityFeature #@
+    """,
+        "homeassistant.components.pylint_test.climate",
+    )
+    imports_checker.visit_module(first_node.parent)
+
+    with assert_adds_messages(
+        linter,
+        pylint.testutils.MessageTest(
+            msg_id="hass-component-root-import",
+            node=first_node,
+            args=None,
+            line=2,
+            col_offset=0,
+            end_line=2,
+            end_col_offset=56,
+        ),
+        pylint.testutils.MessageTest(
+            msg_id="hass-component-root-import",
+            node=second_node,
+            args=None,
+            line=3,
+            col_offset=0,
+            end_line=3,
+            end_col_offset=71,
+        ),
+    ):
+        imports_checker.visit_import(first_node)
+        imports_checker.visit_importfrom(second_node)
