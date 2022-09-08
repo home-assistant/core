e5b56d3fc219236171dcb30870d6e26f1577161b
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-09-08 17:04:37 +0200

    Improve test coverage

--- tests/components/brother/test_init.py
+++ tests/components/brother/test_init.py
@@ -1,6 +1,9 @@
 """Test init of Brother integration."""
 from unittest.mock import patch
 
+from brother import SnmpError
+import pytest
+
 from homeassistant.components.brother.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import CONF_HOST, CONF_TYPE, STATE_UNAVAILABLE
@@ -31,6 +34,22 @@ async def test_config_not_ready(hass):
     with patch("brother.Brother.initialize"), patch(
         "brother.Brother._get_data", side_effect=ConnectionError()
     ):
+        entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(entry.entry_id)
+        assert entry.state is ConfigEntryState.SETUP_RETRY
+
+
+@pytest.mark.parametrize("exc", [(SnmpError("SNMP Error")), (ConnectionError)])
+async def test_error_on_init(hass, exc) -> None:
+    """Test for error on init."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="HL-L2340DW 0123456789",
+        unique_id="0123456789",
+        data={CONF_HOST: "localhost", CONF_TYPE: "laser"},
+    )
+
+    with patch("brother.Brother.initialize", side_effect=exc):
         entry.add_to_hass(hass)
         await hass.config_entries.async_setup(entry.entry_id)
         assert entry.state is ConfigEntryState.SETUP_RETRY
