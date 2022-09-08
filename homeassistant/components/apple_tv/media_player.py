ca0022639ea9be1f9907390939476cb512e15e2e
epenet <6771947+epenet@users.noreply.github.com>
2022-09-08 07:54:51 +0000

    Adjust async_process_play_media_url

--- homeassistant/components/apple_tv/media_player.py
+++ homeassistant/components/apple_tv/media_player.py
@@ -26,8 +26,6 @@ from homeassistant.components.media_player import (
     MediaPlayerState,
     MediaType,
     RepeatMode,
-)
-from homeassistant.components.media_player.browse_media import (
     async_process_play_media_url,
 )
 from homeassistant.config_entries import ConfigEntry
