d5c19e4c5aecb68a5c2ea6fa62a378b4b57d1eab
Paulus Schoutsen <balloob@gmail.com>
2022-09-09 01:47:33 -0400

    Handle missing supported brands (#78090)

--- tests/components/websocket_api/test_commands.py
+++ tests/components/websocket_api/test_commands.py
@@ -1760,6 +1760,12 @@ async def test_validate_config_invalid(websocket_client, key, config, error):
 
 async def test_supported_brands(hass, websocket_client):
     """Test supported brands."""
+    # Custom components without supported brands that override a built-in component with
+    # supported brand will still be listed in HAS_SUPPORTED_BRANDS and should be ignored.
+    mock_integration(
+        hass,
+        MockModule("override_without_brands"),
+    )
     mock_integration(
         hass,
         MockModule("test", partial_manifest={"supported_brands": {"hello": "World"}}),
@@ -1773,7 +1779,7 @@ async def test_supported_brands(hass, websocket_client):
 
     with patch(
         "homeassistant.generated.supported_brands.HAS_SUPPORTED_BRANDS",
-        ("abcd", "test"),
+        ("abcd", "test", "override_without_brands"),
     ):
         await websocket_client.send_json({"id": 7, "type": "supported_brands"})
         msg = await websocket_client.receive_json()
