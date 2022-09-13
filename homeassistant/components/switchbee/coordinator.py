35e37e860ad0533d14e0b96d8891b4a0e7e8a627
Jafar Atili <at.jafar@outlook.com>
2022-09-13 21:00:27 +0000

    Address late review of SwitchBee

---
+++ homeassistant/components/switchbee/coordinator.py
@@ -0,0 +1,84 @@
+"""SwitchBee integration Coordinator."""
+
+from datetime import timedelta
+import logging
+
+from switchbee.api import CentralUnitAPI, SwitchBeeError
+from switchbee.device import DeviceType
+
+from homeassistant.helpers.device_registry import format_mac
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SwitchBeeCoordinator(DataUpdateCoordinator):
+    """Class to manage fetching Freedompro data API."""
+
+    def __init__(
+        self,
+        hass,
+        swb_api,
+        scan_interval,
+    ):
+        """Initialize."""
+        self._api: CentralUnitAPI = swb_api
+        self._reconnect_counts: int = 0
+        self._prev_devices_to_include_to_include: list[DeviceType] = []
+        self._mac_addr_fmt: str = format_mac(swb_api.mac)
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=DOMAIN,
+            update_interval=timedelta(seconds=scan_interval),
+        )
+
+    @property
+    def api(self) -> CentralUnitAPI:
+        """Return SwitchBee API object."""
+        return self._api
+
+    @property
+    def mac_formated(self) -> str:
+        """Return formatted MAC address."""
+        return self._mac_addr_fmt
+
+    async def _async_update_data(self):
+
+        if self._reconnect_counts != self._api.reconnect_count:
+            self._reconnect_counts = self._api.reconnect_count
+            _LOGGER.debug(
+                "Central Unit re-connected again due to invalid token, total %i",
+                self._reconnect_counts,
+            )
+
+        # The devices are loaded once during the config_entry
+        if not self._api.devices:
+            # Try to load the devices from the CU for the first time
+            try:
+                await self._api.fetch_configuration(
+                    [
+                        DeviceType.Switch,
+                        DeviceType.TimedSwitch,
+                        DeviceType.GroupSwitch,
+                        DeviceType.TimedPowerSwitch,
+                    ]
+                )
+            except SwitchBeeError as exp:
+                raise UpdateFailed(
+                    f"Error communicating with API: {exp}"
+                ) from SwitchBeeError
+            else:
+                _LOGGER.debug("Loaded devices")
+
+        # Get the state of the devices
+        try:
+            await self._api.fetch_states()
+        except SwitchBeeError as exp:
+            raise UpdateFailed(
+                f"Error communicating with API: {exp}"
+            ) from SwitchBeeError
+        else:
+            return self._api.devices
--- homeassistant/components/switchbee/switch.py
+++ homeassistant/components/switchbee/switch.py
@@ -9,13 +9,14 @@ from switchbee.device import ApiStateCommand, DeviceType, SwitchBeeBaseDevice
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import aiohttp_client
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from . import SwitchBeeCoordinator
 from .const import DOMAIN
+from .coordinator import SwitchBeeCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -25,29 +26,29 @@ async def async_setup_entry(
 ) -> None:
     """Set up Switchbee switch."""
     coordinator: SwitchBeeCoordinator = hass.data[DOMAIN][entry.entry_id]
-    device_types = (
-        [DeviceType.TimedPowerSwitch]
-        if coordinator.switch_as_light
-        else [
-            DeviceType.TimedPowerSwitch,
-            DeviceType.GroupSwitch,
-            DeviceType.Switch,
-            DeviceType.TimedSwitch,
-            DeviceType.TwoWay,
-        ]
-    )
 
     async_add_entities(
         Device(hass, device, coordinator)
         for device in coordinator.data.values()
-        if device.type in device_types
+        if device.type
+        in [
+            DeviceType.TimedPowerSwitch,
+            DeviceType.GroupSwitch,
+            DeviceType.Switch,
+            DeviceType.TimedSwitch,
+        ]
     )
 
 
 class Device(CoordinatorEntity, SwitchEntity):
     """Representation of an Switchbee switch."""
 
-    def __init__(self, hass, device: SwitchBeeBaseDevice, coordinator):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        device: SwitchBeeBaseDevice,
+        coordinator: SwitchBeeCoordinator,
+    ) -> None:
         """Initialize the Switchbee switch."""
         super().__init__(coordinator)
         self._session = aiohttp_client.async_get_clientsession(hass)
@@ -133,9 +134,8 @@ class Device(CoordinatorEntity, SwitchEntity):
         try:
             await self.coordinator.api.set_state(self._device_id, state)
         except (SwitchBeeError, SwitchBeeDeviceOfflineError) as exp:
-            _LOGGER.error(
-                "Failed to set %s state %s, error: %s", self._attr_name, state, exp
-            )
-            self._async_write_ha_state()
+            raise HomeAssistantError(
+                f"Failed to set {self._attr_name} state {state}"
+            ) from exp
         else:
             await self.coordinator.async_refresh()
