7e863edc996c2df2072b3502e4a3643aa8551a72
Tobias Sauerwein <cgtobi@users.noreply.github.com>
2022-09-15 15:05:02 +0200

    Add Netatmo light platform (#74340)
    * Update to use pyatmo 7
    
    * Clean up old tests
    
    * Revert accidental changes
    
    * Increase test coverage
    
    * Improve tests
    
    * Address review comment
    
    * Address review comments
    
    * Clean up comment
    
    * Update test data
    
    * Revert most breaking changes for sensors
    
    * Clean up
    
    * Update to use pyatmo 7
    
    * Bump pyatmo to 7.0.0
    
    * Clean up old tests
    
    * Revert accidental changes
    
    * Increase test coverage
    
    * Improve tests
    
    * Add light
    
    * Add light and switch platforms
    
    * Add sensor for power and switch devices
    
    * Address review comment
    
    * Address review comments
    
    * Clean up print
    
    * Revert accidental change
    
    * Test clean up
    
    * remove switch
    
    * Address review comments
    
    * Add brightness feature
    
    * Set brightness
    
    * fix test

--- homeassistant/components/netatmo/light.py
+++ homeassistant/components/netatmo/light.py
@@ -6,17 +6,19 @@ from typing import Any, cast
 
 from pyatmo import modules as NaModules
 
-from homeassistant.components.light import LightEntity
+from homeassistant.components.light import ATTR_BRIGHTNESS, ColorMode, LightEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import (
+    CONF_URL_CONTROL,
     CONF_URL_SECURITY,
     DOMAIN,
     EVENT_TYPE_LIGHT_MODE,
     NETATMO_CREATE_CAMERA_LIGHT,
+    NETATMO_CREATE_LIGHT,
     WEBHOOK_LIGHT_MODE,
     WEBHOOK_PUSH_TYPE,
 )
@@ -35,6 +37,7 @@ async def async_setup_entry(
     def _create_camera_light_entity(netatmo_device: NetatmoDevice) -> None:
         if not hasattr(netatmo_device.device, "floodlight"):
             return
+
         entity = NetatmoCameraLight(netatmo_device)
         async_add_entities([entity])
 
@@ -42,6 +45,19 @@ async def async_setup_entry(
         async_dispatcher_connect(
             hass, NETATMO_CREATE_CAMERA_LIGHT, _create_camera_light_entity
         )
+    )
+
+    @callback
+    def _create_entity(netatmo_device: NetatmoDevice) -> None:
+        if not hasattr(netatmo_device.device, "brightness"):
+            return
+
+        entity = NetatmoLight(netatmo_device)
+        _LOGGER.debug("Adding light %s", entity)
+        async_add_entities([entity])
+
+    entry.async_on_unload(
+        async_dispatcher_connect(hass, NETATMO_CREATE_LIGHT, _create_entity)
     )
 
 
@@ -132,3 +148,68 @@ class NetatmoCameraLight(NetatmoBase, LightEntity):
     def async_update_callback(self) -> None:
         """Update the entity's state."""
         self._is_on = bool(self._camera.floodlight == "on")
+
+
+class NetatmoLight(NetatmoBase, LightEntity):
+    """Representation of a dimmable light by Legrand/BTicino."""
+
+    def __init__(
+        self,
+        netatmo_device: NetatmoDevice,
+    ) -> None:
+        """Initialize a Netatmo light."""
+        super().__init__(netatmo_device.data_handler)
+
+        self._dimmer = cast(NaModules.NLFN, netatmo_device.device)
+        self._id = self._dimmer.entity_id
+        self._home_id = self._dimmer.home.entity_id
+        self._device_name = self._dimmer.name
+        self._attr_name = f"{self._device_name}"
+        self._model = self._dimmer.device_type
+        self._config_url = CONF_URL_CONTROL
+        self._attr_brightness = 0
+        self._attr_unique_id = f"{self._id}-light"
+
+        self._attr_supported_color_modes: set[str] = set()
+
+        if not self._attr_supported_color_modes and self._dimmer.brightness is not None:
+            self._attr_supported_color_modes.add(ColorMode.BRIGHTNESS)
+
+        self._signal_name = f"{HOME}-{self._home_id}"
+        self._publishers.extend(
+            [
+                {
+                    "name": HOME,
+                    "home_id": self._dimmer.home.entity_id,
+                    SIGNAL_NAME: self._signal_name,
+                },
+            ]
+        )
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if light is on."""
+        return self._dimmer.on is True
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn light on."""
+        _LOGGER.debug("Turn light '%s' on", self.name)
+        if ATTR_BRIGHTNESS in kwargs:
+            await self._dimmer.async_set_brightness(kwargs[ATTR_BRIGHTNESS])
+
+        else:
+            await self._dimmer.async_on()
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn light off."""
+        _LOGGER.debug("Turn light '%s' off", self.name)
+        await self._dimmer.async_off()
+
+    @callback
+    def async_update_callback(self) -> None:
+        """Update the entity's state."""
+        if self._dimmer.brightness is not None:
+            # Netatmo uses a range of [0, 100] to control brightness
+            self._attr_brightness = round((self._dimmer.brightness / 100) * 255)
+        else:
+            self._attr_brightness = None
