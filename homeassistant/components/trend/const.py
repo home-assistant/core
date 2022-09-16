b8de167b1575f50203884382794a400d4da5a3f2
Jack Boswell <boswelja@outlook.com>
2022-09-16 13:33:24 +1200

    Reorder attrs

--- homeassistant/components/trend/const.py
+++ homeassistant/components/trend/const.py
@@ -3,8 +3,8 @@ DOMAIN = "trend"
 
 ATTR_ATTRIBUTE = "attribute"
 ATTR_GRADIENT = "gradient"
-ATTR_MIN_GRADIENT = "min_gradient"
 ATTR_INVERT = "invert"
+ATTR_MIN_GRADIENT = "min_gradient"
 ATTR_SAMPLE_DURATION = "sample_duration"
 ATTR_SAMPLE_COUNT = "sample_count"
 
