0ad7cb55017393dd8ec1d217037451f540fcb52d
J. Nick Koston <nick@koston.org>
2022-09-16 10:15:58 +0200

    use inject_bluetooth_service_info helper

--- tests/components/qingping/test_binary_sensor.py
+++ tests/components/qingping/test_binary_sensor.py
@@ -1,14 +1,13 @@
 """Test the Qingping binary sensors."""
 
-from unittest.mock import patch
 
-from homeassistant.components.bluetooth import BluetoothChange
 from homeassistant.components.qingping.const import DOMAIN
 from homeassistant.const import ATTR_FRIENDLY_NAME
 
 from . import LIGHT_AND_SIGNAL_SERVICE_INFO
 
 from tests.common import MockConfigEntry
+from tests.components.bluetooth import inject_bluetooth_service_info
 
 
 async def test_binary_sensors(hass):
@@ -19,22 +18,11 @@ async def test_binary_sensors(hass):
     )
     entry.add_to_hass(hass)
 
-    saved_callback = None
-
-    def _async_register_callback(_hass, _callback, _matcher, _mode):
-        nonlocal saved_callback
-        saved_callback = _callback
-        return lambda: None
-
-    with patch(
-        "homeassistant.components.bluetooth.update_coordinator.async_register_callback",
-        _async_register_callback,
-    ):
-        assert await hass.config_entries.async_setup(entry.entry_id)
-        await hass.async_block_till_done()
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
 
     assert len(hass.states.async_all("binary_sensor")) == 0
-    saved_callback(LIGHT_AND_SIGNAL_SERVICE_INFO, BluetoothChange.ADVERTISEMENT)
+    inject_bluetooth_service_info(hass, LIGHT_AND_SIGNAL_SERVICE_INFO)
     await hass.async_block_till_done()
     assert len(hass.states.async_all("binary_sensor")) == 1
 
