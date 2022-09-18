5c5f1899543043a3780c7dc746a70a08e52fdd05
Robert Hillis <tkdrob4390@yahoo.com>
2022-09-18 14:17:53 -0400

    remove unneeded reload handling

--- homeassistant/components/radarr/__init__.py
+++ homeassistant/components/radarr/__init__.py
@@ -46,7 +46,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         if isinstance(coordinator, StatusDataUpdateCoordinator):
             _version = coordinator.system_status.version
         coordinator.system_version = _version
-    entry.async_on_unload(entry.add_update_listener(_async_update_listener))
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinators
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
@@ -58,11 +57,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
-
-
-async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
 
 
 class RadarrEntity(CoordinatorEntity[RadarrDataUpdateCoordinator]):
