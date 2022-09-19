43645cb73a1c338e392c76dde0b295ce2b07389c
epenet <6771947+epenet@users.noreply.github.com>
2022-09-19 12:30:20 +0000

    Adjust number

--- tests/components/sleepiq/test_number.py
+++ tests/components/sleepiq/test_number.py
@@ -1,10 +1,10 @@
 """The tests for SleepIQ number platform."""
-from homeassistant.components.number import DOMAIN
-from homeassistant.components.number.const import (
+from homeassistant.components.number import (
     ATTR_MAX,
     ATTR_MIN,
     ATTR_STEP,
     ATTR_VALUE,
+    DOMAIN,
     SERVICE_SET_VALUE,
 )
 from homeassistant.const import ATTR_ENTITY_ID, ATTR_FRIENDLY_NAME, ATTR_ICON
