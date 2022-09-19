3e54adc7b9add4043aa8c8c5904a6482fe2db0a6
epenet <6771947+epenet@users.noreply.github.com>
2022-09-19 07:20:02 +0000

    Revert recorder

--- tests/components/recorder/test_statistics_v23_migration.py
+++ tests/components/recorder/test_statistics_v23_migration.py
@@ -20,9 +20,8 @@ from homeassistant.helpers import recorder as recorder_helper
 from homeassistant.setup import setup_component
 import homeassistant.util.dt as dt_util
 
-from .common import wait_recording_done
-
 from tests.common import get_test_home_assistant
+from tests.components.recorder.common import wait_recording_done
 
 ORIG_TZ = dt_util.DEFAULT_TIME_ZONE
 
