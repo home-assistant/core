20fa88914e7524cb0b479e84909bc85a71f1f39f
Joakim Plate <elupus@ecce.se>
2022-09-22 10:21:36 +0200

    Use entity descriptions for sensors

--- homeassistant/components/nibe_heatpump/sensor.py
+++ homeassistant/components/nibe_heatpump/sensor.py
@@ -7,6 +7,7 @@ from homeassistant.components.sensor import (
     ENTITY_ID_FORMAT,
     SensorDeviceClass,
     SensorEntity,
+    SensorEntityDescription,
     SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
@@ -20,7 +21,6 @@ from homeassistant.const import (
     ENERGY_WATT_HOUR,
     TEMP_CELSIUS,
     TEMP_FAHRENHEIT,
-    TEMP_KELVIN,
     TIME_HOURS,
 )
 from homeassistant.core import HomeAssistant
@@ -28,6 +28,78 @@ from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import DOMAIN, CoilEntity, Coordinator
+
+UNIT_DESCRIPTIONS = {
+    TEMP_CELSIUS: SensorEntityDescription(
+        key=TEMP_CELSIUS,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=TEMP_CELSIUS,
+    ),
+    TEMP_FAHRENHEIT: SensorEntityDescription(
+        key=TEMP_FAHRENHEIT,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=TEMP_FAHRENHEIT,
+    ),
+    ELECTRIC_CURRENT_AMPERE: SensorEntityDescription(
+        key=ELECTRIC_CURRENT_AMPERE,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=ELECTRIC_CURRENT_AMPERE,
+    ),
+    ELECTRIC_CURRENT_MILLIAMPERE: SensorEntityDescription(
+        key=ELECTRIC_CURRENT_MILLIAMPERE,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=ELECTRIC_CURRENT_MILLIAMPERE,
+    ),
+    ELECTRIC_POTENTIAL_VOLT: SensorEntityDescription(
+        key=ELECTRIC_POTENTIAL_VOLT,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=ELECTRIC_POTENTIAL_VOLT,
+    ),
+    ELECTRIC_POTENTIAL_MILLIVOLT: SensorEntityDescription(
+        key=ELECTRIC_POTENTIAL_VOLT,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=ELECTRIC_POTENTIAL_MILLIVOLT,
+    ),
+    ENERGY_WATT_HOUR: SensorEntityDescription(
+        key=ENERGY_WATT_HOUR,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.POWER,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        native_unit_of_measurement=ENERGY_WATT_HOUR,
+    ),
+    ENERGY_KILO_WATT_HOUR: SensorEntityDescription(
+        key=ENERGY_WATT_HOUR,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.POWER,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+    ),
+    ENERGY_MEGA_WATT_HOUR: SensorEntityDescription(
+        key=ENERGY_MEGA_WATT_HOUR,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.POWER,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        native_unit_of_measurement=ENERGY_MEGA_WATT_HOUR,
+    ),
+    TIME_HOURS: SensorEntityDescription(
+        key=TIME_HOURS,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        device_class=SensorDeviceClass.DURATION,
+        native_unit_of_measurement=TIME_HOURS,
+    ),
+}
 
 
 async def async_setup_entry(
@@ -40,7 +112,7 @@ async def async_setup_entry(
     coordinator: Coordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     async_add_entities(
-        Sensor(coordinator, coil)
+        Sensor(coordinator, coil, UNIT_DESCRIPTIONS.get(coil.unit))
         for coil in coordinator.coils
         if not coil.is_writable and not coil.is_boolean
     )
@@ -49,29 +121,18 @@ async def async_setup_entry(
 class Sensor(SensorEntity, CoilEntity):
     """Sensor entity."""
 
-    _attr_entity_category = EntityCategory.DIAGNOSTIC
-
-    def __init__(self, coordinator: Coordinator, coil: Coil) -> None:
+    def __init__(
+        self,
+        coordinator: Coordinator,
+        coil: Coil,
+        entity_description: SensorEntityDescription | None,
+    ) -> None:
         """Initialize entity."""
         super().__init__(coordinator, coil, ENTITY_ID_FORMAT)
-        self._attr_native_unit_of_measurement = coil.unit
-
-        unit = self.native_unit_of_measurement
-        if unit in {TEMP_CELSIUS, TEMP_FAHRENHEIT, TEMP_KELVIN}:
-            self._attr_device_class = SensorDeviceClass.TEMPERATURE
-        elif unit in {ELECTRIC_CURRENT_AMPERE, ELECTRIC_CURRENT_MILLIAMPERE}:
-            self._attr_device_class = SensorDeviceClass.CURRENT
-        elif unit in {ELECTRIC_POTENTIAL_VOLT, ELECTRIC_POTENTIAL_MILLIVOLT}:
-            self._attr_device_class = SensorDeviceClass.VOLTAGE
-        elif unit in {ENERGY_WATT_HOUR, ENERGY_KILO_WATT_HOUR, ENERGY_MEGA_WATT_HOUR}:
-            self._attr_device_class = SensorDeviceClass.ENERGY
-        elif unit in {TIME_HOURS}:
-            self._attr_device_class = SensorDeviceClass.DURATION
+        if entity_description:
+            self.entity_description = entity_description
         else:
-            self._attr_device_class = None
-
-        if unit:
-            self._attr_state_class = SensorStateClass.MEASUREMENT
+            self._attr_native_unit_of_measurement = coil.unit
 
     def _async_read_coil(self, coil: Coil):
         self._attr_native_value = coil.value
