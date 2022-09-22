3e32668b4cab5c6b55a0e8d6a3fa8ff50703ca77
epenet <6771947+epenet@users.noreply.github.com>
2022-09-22 16:44:09 +0200

    Move pressure utility to unit_conversion (#78953)

--- homeassistant/util/pressure.py
+++ homeassistant/util/pressure.py
@@ -1,9 +1,7 @@
 """Pressure util functions."""
 from __future__ import annotations
 
-from numbers import Number
-
-from homeassistant.const import (
+from homeassistant.const import (  # pylint: disable=unused-import # noqa: F401
     PRESSURE,
     PRESSURE_BAR,
     PRESSURE_CBAR,
@@ -17,45 +15,13 @@ from homeassistant.const import (
     UNIT_NOT_RECOGNIZED_TEMPLATE,
 )
 
-VALID_UNITS: tuple[str, ...] = (
-    PRESSURE_PA,
-    PRESSURE_HPA,
-    PRESSURE_KPA,
-    PRESSURE_BAR,
-    PRESSURE_CBAR,
-    PRESSURE_MBAR,
-    PRESSURE_INHG,
-    PRESSURE_PSI,
-    PRESSURE_MMHG,
-)
+from .unit_conversion import PressureConverter
 
-UNIT_CONVERSION: dict[str, float] = {
-    PRESSURE_PA: 1,
-    PRESSURE_HPA: 1 / 100,
-    PRESSURE_KPA: 1 / 1000,
-    PRESSURE_BAR: 1 / 100000,
-    PRESSURE_CBAR: 1 / 1000,
-    PRESSURE_MBAR: 1 / 100,
-    PRESSURE_INHG: 1 / 3386.389,
-    PRESSURE_PSI: 1 / 6894.757,
-    PRESSURE_MMHG: 1 / 133.322,
-}
-
-NORMALIZED_UNIT = PRESSURE_PA
+UNIT_CONVERSION: dict[str, float] = PressureConverter.UNIT_CONVERSION
+VALID_UNITS: tuple[str, ...] = PressureConverter.VALID_UNITS
 
 
 def convert(value: float, from_unit: str, to_unit: str) -> float:
     """Convert one unit of measurement to another."""
-    if from_unit not in VALID_UNITS:
-        raise ValueError(UNIT_NOT_RECOGNIZED_TEMPLATE.format(from_unit, PRESSURE))
-    if to_unit not in VALID_UNITS:
-        raise ValueError(UNIT_NOT_RECOGNIZED_TEMPLATE.format(to_unit, PRESSURE))
-
-    if not isinstance(value, Number):
-        raise TypeError(f"{value} is not of numeric type")
-
-    if from_unit == to_unit:
-        return value
-
-    pascals = value / UNIT_CONVERSION[from_unit]
-    return pascals * UNIT_CONVERSION[to_unit]
+    # Need to add warning when core migration finished
+    return PressureConverter.convert(value, from_unit, to_unit)
