e01014888c0a48e20a9f020ff38e79548372f224
kevdliu <1766838+kevdliu@users.noreply.github.com>
2022-09-23 13:55:21 -0400

    set up notify platform via discovery

--- homeassistant/components/ecobee/const.py
+++ homeassistant/components/ecobee/const.py
@@ -20,8 +20,8 @@ _LOGGER = logging.getLogger(__package__)
 
 DOMAIN = "ecobee"
 DATA_ECOBEE_CONFIG = "ecobee_config"
+ATTR_CONFIG_ENTRY_ID = "entry_id"
 
-CONF_INDEX = "index"
 CONF_REFRESH_TOKEN = "refresh_token"
 
 ECOBEE_MODEL_TO_NAME = {
--- homeassistant/components/ecobee/notify.py
+++ homeassistant/components/ecobee/notify.py
@@ -1,31 +1,35 @@
 """Support for Ecobee Send Message service."""
-import voluptuous as vol
 
-from homeassistant.components.notify import PLATFORM_SCHEMA, BaseNotificationService
-import homeassistant.helpers.config_validation as cv
+from homeassistant.components.notify import ATTR_TARGET, BaseNotificationService
 
-from .const import CONF_INDEX, DOMAIN
+from homeassistant.exceptions import RequiredParameterMissing
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {vol.Optional(CONF_INDEX, default=0): cv.positive_int}
-)
+from .const import DOMAIN
 
 
 def get_service(hass, config, discovery_info=None):
     """Get the Ecobee notification service."""
-    index = config.get(CONF_INDEX)
-    return EcobeeNotificationService(hass, index)
+    if discovery_info is None:
+        return None
+
+    data = hass.data[DOMAIN]
+    return EcobeeNotificationService(data.ecobee)
 
 
 class EcobeeNotificationService(BaseNotificationService):
     """Implement the notification service for the Ecobee thermostat."""
 
-    def __init__(self, hass, thermostat_index):
+    def __init__(self, ecobee):
         """Initialize the service."""
-        self.hass = hass
-        self.thermostat_index = thermostat_index
+        self.ecobee = ecobee
 
     def send_message(self, message="", **kwargs):
         """Send a message."""
-        data = self.hass.data[DOMAIN]
-        data.ecobee.send_message(self.thermostat_index, message)
+        targets = kwargs.get(ATTR_TARGET, None)
+
+        if not targets:
+            raise RequiredParameterMissing(["target"])
+
+        for target in targets:
+            thermostat_index = int(target)
+            self.ecobee.send_message(thermostat_index, message)
