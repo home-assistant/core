62317546492d81d6d950bdab32dd99844b88893b
J. Nick Koston <nick@koston.org>
2022-09-22 18:19:06 -1000

    Avoid creating iBeacon trackers when the device has no name
    When the beacon is too far away from the reciever it will
    not be able to get the name of the device. As these devices
    are not useful to track since they will flip-flop state as
    rssi fluctuates instead of actually going home/away.

--- tests/components/ibeacon/test_coordinator.py
+++ tests/components/ibeacon/test_coordinator.py
@@ -73,3 +73,22 @@ async def test_ignore_not_ibeacons(hass):
     )
     await hass.async_block_till_done()
     assert len(hass.states.async_entity_ids()) == before_entity_count
+
+
+async def test_ignore_no_name(hass):
+    """Test we ignore devices with no name."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+    )
+    entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    before_entity_count = len(hass.states.async_entity_ids())
+    inject_bluetooth_service_info(
+        hass,
+        replace(BLUECHARM_BEACON_SERVICE_INFO, name=None),
+    )
+    await hass.async_block_till_done()
+    assert len(hass.states.async_entity_ids()) == before_entity_count
