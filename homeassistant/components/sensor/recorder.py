4cf57355e1d0ca0a710977fe9eea37a41811bcf2
epenet <6771947+epenet@users.noreply.github.com>
2022-09-23 07:56:57 +0000

    Add ability to bypass checks in unit conversion

--- homeassistant/components/sensor/recorder.py
+++ homeassistant/components/sensor/recorder.py
@@ -215,7 +215,10 @@ def _normalize_states(
         fstates.append(
             (
                 converter.convert(
-                    fstate, from_unit=state_unit, to_unit=converter.NORMALIZED_UNIT
+                    fstate,
+                    from_unit=state_unit,
+                    to_unit=converter.NORMALIZED_UNIT,
+                    bypass_checks=True,
                 ),
                 state,
             )
