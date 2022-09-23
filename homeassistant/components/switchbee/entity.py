9d79f76a18dcc7e6fd4a557bf676859886a1ddc1
epenet <6771947+epenet@users.noreply.github.com>
2022-09-23 08:30:13 +0000

    Add base entity to switchbee

---
+++ homeassistant/components/switchbee/entity.py
@@ -0,0 +1,42 @@
+"""Support for SwitchBee entity."""
+from switchbee import SWITCHBEE_BRAND
+from switchbee.device import SwitchBeeBaseDevice
+
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN
+from .coordinator import SwitchBeeCoordinator
+
+
+class SwitchBeeEntity(CoordinatorEntity[SwitchBeeCoordinator]):
+    """Representation of a Switchbee entity."""
+
+    _attr_has_entity_name = True
+
+    def __init__(
+        self,
+        device: SwitchBeeBaseDevice,
+        coordinator: SwitchBeeCoordinator,
+    ) -> None:
+        """Initialize the Switchbee switch."""
+        super().__init__(coordinator)
+        self._device = device
+        self._attr_name = device.name
+        self._attr_unique_id = f"{coordinator.mac_formated}-{device.id}"
+        self._attr_device_info = DeviceInfo(
+            name=f"SwitchBee {device.unit_id}",
+            identifiers={
+                (
+                    DOMAIN,
+                    f"{device.unit_id}-{coordinator.mac_formated}",
+                )
+            },
+            manufacturer=SWITCHBEE_BRAND,
+            model=coordinator.api.module_display(device.unit_id),
+            suggested_area=device.zone,
+            via_device=(
+                DOMAIN,
+                f"{coordinator.api.name} ({coordinator.api.mac})",
+            ),
+        )
