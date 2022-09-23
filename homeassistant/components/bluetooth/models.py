ee6822dd03c15f76db506bdb857751d6b322a340
J. Nick Koston <nick@koston.org>
2022-09-23 07:47:23 -1000

    fixes

--- homeassistant/components/bluetooth/models.py
+++ homeassistant/components/bluetooth/models.py
@@ -249,12 +249,14 @@ class HaBleakClientWrapper(BleakClient):
 
     async def connect(self, **kwargs: Any) -> bool:
         """Connect to the specified GATT server."""
-        wrapped_backend = self._async_get_backend()
-        self._backend = wrapped_backend.client(
-            await freshen_ble_device(wrapped_backend.device) or wrapped_backend.device,
-            disconnected_callback=self.__disconnected_callback,
-            timeout=self.__timeout,
-        )
+        if not self._backend:
+            wrapped_backend = self._async_get_backend()
+            self._backend = wrapped_backend.client(
+                await freshen_ble_device(wrapped_backend.device)
+                or wrapped_backend.device,
+                disconnected_callback=self.__disconnected_callback,
+                timeout=self.__timeout,
+            )
         return await super().connect(**kwargs)
 
     @hass_callback
