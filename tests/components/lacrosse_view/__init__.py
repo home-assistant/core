975b7131350c55ed20f41464ec8c2ae163c0b090
IceBotYT <34712694+IceBotYT@users.noreply.github.com>
2022-09-25 17:36:07 +0000

    Improve test coverage

--- tests/components/lacrosse_view/__init__.py
+++ tests/components/lacrosse_view/__init__.py
@@ -41,3 +41,25 @@ TEST_UNSUPPORTED_SENSOR = Sensor(
     permissions={"read": True},
     model="Test",
 )
+TEST_FLOAT_SENSOR = Sensor(
+    name="Test",
+    device_id="1",
+    type="Test",
+    sensor_id="2",
+    sensor_field_names=["Temperature"],
+    location=Location(id="1", name="Test"),
+    data={"Temperature": {"values": [{"s": "2.3"}], "unit": "degrees_celsius"}},
+    permissions={"read": True},
+    model="Test",
+)
+TEST_STRING_SENSOR = Sensor(
+    name="Test",
+    device_id="1",
+    type="Test",
+    sensor_id="2",
+    sensor_field_names=["WetDry"],
+    location=Location(id="1", name="Test"),
+    data={"WetDry": {"values": [{"s": "dry"}], "unit": "wet_dry"}},
+    permissions={"read": True},
+    model="Test",
+)
--- tests/components/lacrosse_view/test_sensor.py
+++ tests/components/lacrosse_view/test_sensor.py
@@ -7,8 +7,10 @@ from homeassistant.core import HomeAssistant
 
 from . import (
     MOCK_ENTRY_DATA,
+    TEST_FLOAT_SENSOR,
     TEST_NO_PERMISSION_SENSOR,
     TEST_SENSOR,
+    TEST_STRING_SENSOR,
     TEST_UNSUPPORTED_SENSOR,
 )
 
@@ -71,3 +73,24 @@ async def test_field_not_supported(hass: HomeAssistant, caplog) -> None:
     assert entries[0].state == ConfigEntryState.LOADED
     assert hass.states.get("sensor.test_some_unsupported_field") is None
     assert "Unsupported sensor field" in caplog.text
+
+
+async def test_field_types(hass: HomeAssistant) -> None:
+    """Test the different data types for fields."""
+    config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_ENTRY_DATA)
+    config_entry.add_to_hass(hass)
+
+    with patch("lacrosse_view.LaCrosse.login", return_value=True), patch(
+        "lacrosse_view.LaCrosse.get_sensors",
+        return_value=[TEST_FLOAT_SENSOR, TEST_STRING_SENSOR],
+    ):
+        assert await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    assert hass.data[DOMAIN]
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert entries
+    assert len(entries) == 1
+    assert entries[0].state == ConfigEntryState.LOADED
+    assert hass.states.get("sensor.test_temperature")
+    assert hass.states.get("sensor.test_wet_dry")
