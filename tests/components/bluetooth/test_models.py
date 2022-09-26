02e259bfa8a35de4d35a1723cd0e08a3288e2220
J. Nick Koston <nick@koston.org>
2022-09-25 19:41:00 -1000

    fix patching

--- tests/components/bluetooth/test_models.py
+++ tests/components/bluetooth/test_models.py
@@ -297,7 +297,10 @@ async def test_ble_device_with_proxy_client_out_of_connections_uses_best_availab
     ]
 
     client = HaBleakClientWrapper(switchbot_proxy_device_no_connection_slot)
-    await client.connect()
+    with patch(
+        "homeassistant.components.bluetooth.models.get_platform_client_backend_type"
+    ):
+        await client.connect()
     assert client.is_connected is True
     client.set_disconnected_callback(lambda client: None)
     await client.disconnect()
