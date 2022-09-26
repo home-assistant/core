ae7abe1f6680d81812a4e73460699033cb99c3b2
David F. Mulcahey <david.mulcahey@me.com>
2022-09-26 14:03:03 -0400

    fix attribute name collision

--- homeassistant/components/zha/select.py
+++ homeassistant/components/zha/select.py
@@ -69,7 +69,7 @@ class ZHAEnumSelectEntity(ZhaEntity, SelectEntity):
     """Representation of a ZHA select entity."""
 
     _attr_entity_category = EntityCategory.CONFIG
-    _attr_name: str
+    _attribute: str
     _enum: type[Enum]
 
     def __init__(
@@ -80,7 +80,7 @@ class ZHAEnumSelectEntity(ZhaEntity, SelectEntity):
         **kwargs: Any,
     ) -> None:
         """Init this select entity."""
-        self._attr_name = self._enum.__name__
+        self._attribute = self._enum.__name__
         self._attr_options = [entry.name.replace("_", " ") for entry in self._enum]
         self._channel: ZigbeeChannel = channels[0]
         super().__init__(unique_id, zha_device, channels, **kwargs)
@@ -88,21 +88,21 @@ class ZHAEnumSelectEntity(ZhaEntity, SelectEntity):
     @property
     def current_option(self) -> str | None:
         """Return the selected entity option to represent the entity state."""
-        option = self._channel.data_cache.get(self._attr_name)
+        option = self._channel.data_cache.get(self._attribute)
         if option is None:
             return None
         return option.name.replace("_", " ")
 
     async def async_select_option(self, option: str) -> None:
         """Change the selected option."""
-        self._channel.data_cache[self._attr_name] = self._enum[option.replace(" ", "_")]
+        self._channel.data_cache[self._attribute] = self._enum[option.replace(" ", "_")]
         self.async_write_ha_state()
 
     @callback
     def async_restore_last_state(self, last_state) -> None:
         """Restore previous state."""
         if last_state.state and last_state.state != STATE_UNKNOWN:
-            self._channel.data_cache[self._attr_name] = self._enum[
+            self._channel.data_cache[self._attribute] = self._enum[
                 last_state.state.replace(" ", "_")
             ]
 
