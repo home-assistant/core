461a86997539e0167ef9645d8e97a6d9a4967549
Paarth Shah <mail@shahpaarth.com>
2022-09-27 03:31:08 -0700

    Address code review, add HTML message unit tests.

--- tests/components/matrix/test_send_message.py
+++ tests/components/matrix/test_send_message.py
@@ -1,11 +1,12 @@
 """Test the send_message service."""
 
 from homeassistant.components.matrix import (
+    ATTR_FORMAT,
     ATTR_IMAGES,
     DOMAIN as MATRIX_DOMAIN,
     MatrixBot,
 )
-from homeassistant.components.matrix.const import SERVICE_SEND_MESSAGE
+from homeassistant.components.matrix.const import FORMAT_HTML, SERVICE_SEND_MESSAGE
 from homeassistant.components.notify import ATTR_DATA, ATTR_MESSAGE, ATTR_TARGET
 from homeassistant.core import HomeAssistant
 
@@ -21,6 +22,20 @@ async def test_send_message(
 
     # Send a message without an attached image.
     data = {ATTR_MESSAGE: "Test message", ATTR_TARGET: TEST_JOINABLE_ROOMS}
+    await hass.services.async_call(
+        MATRIX_DOMAIN, SERVICE_SEND_MESSAGE, data, blocking=True
+    )
+    await hass.async_block_till_done()
+
+    for room_id in TEST_JOINABLE_ROOMS:
+        assert f"Message delivered to room '{room_id}'" in caplog.messages
+
+    # Send an HTML message without an attached image.
+    data = {
+        ATTR_MESSAGE: "Test message",
+        ATTR_TARGET: TEST_JOINABLE_ROOMS,
+        ATTR_DATA: {ATTR_FORMAT: FORMAT_HTML},
+    }
     await hass.services.async_call(
         MATRIX_DOMAIN, SERVICE_SEND_MESSAGE, data, blocking=True
     )
