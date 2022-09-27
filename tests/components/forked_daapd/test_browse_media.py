622ed979f663f97092b363577dbdf00ee7de0531
uvjustin <46082645+uvjustin@users.noreply.github.com>
2022-09-27 11:32:05 -0700

    Add spotify support to forked-daapd (#79136)

--- tests/components/forked_daapd/test_browse_media.py
+++ tests/components/forked_daapd/test_browse_media.py
@@ -3,9 +3,12 @@
 from http import HTTPStatus
 from unittest.mock import patch
 
-from homeassistant.components import media_source
+from homeassistant.components import media_source, spotify
 from homeassistant.components.forked_daapd.browse_media import create_media_content_id
-from homeassistant.components.media_player import MediaType
+from homeassistant.components.media_player import BrowseMedia, MediaClass, MediaType
+from homeassistant.components.spotify.const import (
+    MEDIA_PLAYER_PREFIX as SPOTIFY_MEDIA_PLAYER_PREFIX,
+)
 from homeassistant.components.websocket_api.const import TYPE_RESULT
 from homeassistant.setup import async_setup_component
 
@@ -218,6 +221,56 @@ async def test_async_browse_media_not_found(hass, hass_ws_client, config_entry):
                 == f"Media not found for {media_type} / {media_content_id}"
             )
             msg_id += 1
+
+
+async def test_async_browse_spotify(hass, hass_ws_client, config_entry):
+    """Test browsing spotify."""
+
+    assert await async_setup_component(hass, spotify.DOMAIN, {})
+    await hass.async_block_till_done()
+    config_entry.add_to_hass(hass)
+    await config_entry.async_setup(hass)
+    await hass.async_block_till_done()
+    with patch(
+        "homeassistant.components.forked_daapd.media_player.spotify_async_browse_media"
+    ) as mock_spotify_browse:
+        children = [
+            BrowseMedia(
+                title="Spotify",
+                media_class=MediaClass.APP,
+                media_content_id=f"{SPOTIFY_MEDIA_PLAYER_PREFIX}some_id",
+                media_content_type=f"{SPOTIFY_MEDIA_PLAYER_PREFIX}track",
+                thumbnail="https://brands.home-assistant.io/_/spotify/logo.png",
+                can_play=False,
+                can_expand=True,
+            )
+        ]
+        mock_spotify_browse.return_value = BrowseMedia(
+            title="Spotify",
+            media_class=MediaClass.APP,
+            media_content_id=SPOTIFY_MEDIA_PLAYER_PREFIX,
+            media_content_type=f"{SPOTIFY_MEDIA_PLAYER_PREFIX}library",
+            thumbnail="https://brands.home-assistant.io/_/spotify/logo.png",
+            can_play=False,
+            can_expand=True,
+            children=children,
+        )
+
+        client = await hass_ws_client(hass)
+        await client.send_json(
+            {
+                "id": 1,
+                "type": "media_player/browse_media",
+                "entity_id": TEST_MASTER_ENTITY_NAME,
+                "media_content_type": f"{SPOTIFY_MEDIA_PLAYER_PREFIX}library",
+                "media_content_id": SPOTIFY_MEDIA_PLAYER_PREFIX,
+            }
+        )
+        msg = await client.receive_json()
+        # Assert WebSocket response
+        assert msg["id"] == 1
+        assert msg["type"] == TYPE_RESULT
+        assert msg["success"]
 
 
 async def test_async_browse_image(hass, hass_client, config_entry):
