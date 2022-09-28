bc7f5e0417daca0610978a06cd0aad0159ea4458
PeteRager <76050312+PeteRager@users.noreply.github.com>
2022-09-28 09:16:41 -0400

    Define self._device is mixin to make linter happier

--- homeassistant/components/nest/availability_mixin.py
+++ homeassistant/components/nest/availability_mixin.py
@@ -7,12 +7,15 @@ from .const import CONNECTIVITY_TRAIT_OFFLINE
 class AvailabilityMixin:
     """Mixin for entities to set availability based on the connectivity trait."""
 
+    def __init__(self):
+        """Initialize the mixin."""
+        self._device: Device = None
+
     @property
     def available(self) -> bool:
         """Return entity availability."""
-        device: Device = self._device  # To enable type hinting
-        if ConnectivityTrait.NAME in device.traits:
-            trait: ConnectivityTrait = device.traits[ConnectivityTrait.NAME]
+        if ConnectivityTrait.NAME in self._device.traits:
+            trait: ConnectivityTrait = self._device.traits[ConnectivityTrait.NAME]
             if trait.status == CONNECTIVITY_TRAIT_OFFLINE:
                 return False
         return True
