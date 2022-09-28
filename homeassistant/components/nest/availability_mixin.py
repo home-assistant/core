9811620ea205964e736e16bfd3710fae974ba5bc
PeteRager <76050312+PeteRager@users.noreply.github.com>
2022-09-28 09:40:12 -0400

    Update availability_mixin.py

--- homeassistant/components/nest/availability_mixin.py
+++ homeassistant/components/nest/availability_mixin.py
@@ -1,21 +1,23 @@
 """Mixin for SDM availability."""
+from __future__ import annotations
+
 from google_nest_sdm.device import Device
 from google_nest_sdm.device_traits import ConnectivityTrait
+
 from .const import CONNECTIVITY_TRAIT_OFFLINE
 
 
 class AvailabilityMixin:
     """Mixin for entities to set availability based on the connectivity trait."""
 
-    def __init__(self):
-        """Initialize the mixin."""
-        self._device: Device = None
-
     @property
     def available(self) -> bool:
         """Return entity availability."""
-        if ConnectivityTrait.NAME in self._device.traits:
-            trait: ConnectivityTrait = self._device.traits[ConnectivityTrait.NAME]
+        device: Device = getattr(
+            self, "_device"
+        )  # To prevent linter errors on access to member in instantiated class
+        if ConnectivityTrait.NAME in device.traits:
+            trait: ConnectivityTrait = device.traits[ConnectivityTrait.NAME]
             if trait.status == CONNECTIVITY_TRAIT_OFFLINE:
                 return False
         return True
