55b2de3c7814ee102a376157dbceb37b63940f94
Martin Hjelmare <marhje52@gmail.com>
2022-09-28 08:15:52 +0200

    Apply suggestions from code review

--- homeassistant/components/apcupsd/sensor.py
+++ homeassistant/components/apcupsd/sensor.py
@@ -465,7 +465,7 @@ async def async_setup_platform(
         hass,
         DOMAIN,
         "deprecated_yaml",
-        breaks_in_ha_version="2022.10.0",
+        breaks_in_ha_version="2022.12.0",
         is_fixable=False,
         severity=IssueSeverity.WARNING,
         translation_key="deprecated_yaml",
