27c548ee16e4092166ac4a5a01eca9bdb02cda66
epenet <6771947+epenet@users.noreply.github.com>
2022-09-28 14:16:13 +0000

    Simplify temperature deprecation

--- homeassistant/util/temperature.py
+++ homeassistant/util/temperature.py
@@ -15,46 +15,22 @@ VALID_UNITS = TemperatureConverter.VALID_UNITS
 
 def fahrenheit_to_celsius(fahrenheit: float, interval: bool = False) -> float:
     """Convert a temperature in Fahrenheit to Celsius."""
-    report(
-        "uses temperature utility. This is deprecated since 2022.10 and will "
-        "stop working in Home Assistant 2022.4, it should be updated to use "
-        "unit_conversion.TemperatureConverter instead",
-        error_if_core=False,
-    )
-    return TemperatureConverter.fahrenheit_to_celsius(fahrenheit, interval)
+    return convert(fahrenheit, TEMP_FAHRENHEIT, TEMP_CELSIUS, interval)
 
 
 def kelvin_to_celsius(kelvin: float, interval: bool = False) -> float:
     """Convert a temperature in Kelvin to Celsius."""
-    report(
-        "uses temperature utility. This is deprecated since 2022.10 and will "
-        "stop working in Home Assistant 2022.4, it should be updated to use "
-        "unit_conversion.TemperatureConverter instead",
-        error_if_core=False,
-    )
-    return TemperatureConverter.kelvin_to_celsius(kelvin, interval)
+    return convert(kelvin, TEMP_KELVIN, TEMP_CELSIUS, interval)
 
 
 def celsius_to_fahrenheit(celsius: float, interval: bool = False) -> float:
     """Convert a temperature in Celsius to Fahrenheit."""
-    report(
-        "uses temperature utility. This is deprecated since 2022.10 and will "
-        "stop working in Home Assistant 2022.4, it should be updated to use "
-        "unit_conversion.TemperatureConverter instead",
-        error_if_core=False,
-    )
-    return TemperatureConverter.celsius_to_fahrenheit(celsius, interval)
+    return convert(celsius, TEMP_CELSIUS, TEMP_FAHRENHEIT, interval)
 
 
 def celsius_to_kelvin(celsius: float, interval: bool = False) -> float:
     """Convert a temperature in Celsius to Fahrenheit."""
-    report(
-        "uses temperature utility. This is deprecated since 2022.10 and will "
-        "stop working in Home Assistant 2022.4, it should be updated to use "
-        "unit_conversion.TemperatureConverter instead",
-        error_if_core=False,
-    )
-    return TemperatureConverter.celsius_to_kelvin(celsius, interval)
+    return convert(celsius, TEMP_CELSIUS, TEMP_KELVIN, interval)
 
 
 def convert(
