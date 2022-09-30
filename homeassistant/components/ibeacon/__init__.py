054d635a9c582db8ca901e230f5e887e888d73d6
J. Nick Koston <nick@koston.org>
2022-09-29 14:46:17 -1000

    Enable delete device support for iBeacon

--- homeassistant/components/ibeacon/__init__.py
+++ homeassistant/components/ibeacon/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.device_registry import async_get
+from homeassistant.helpers.device_registry import DeviceEntry, async_get
 
 from .const import DOMAIN, PLATFORMS
 from .coordinator import IBeaconCoordinator
@@ -22,3 +22,12 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data.pop(DOMAIN)
     return unload_ok
+
+
+async def async_remove_config_entry_device(
+    hass: HomeAssistant, config_entry: ConfigEntry, device_entry: DeviceEntry
+) -> bool:
+    """Remove iBeacon config entry from a device."""
+    # There is little risk in allowing remove since they will
+    # come back if they are still there anyways.
+    return True
