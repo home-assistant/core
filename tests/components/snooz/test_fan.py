b7c7b451d20b65b23c03b0e9ac0716e9eb678670
Austin Brunkhorst <contact@austinbrunkhorst.com>
2022-10-01 23:39:10 +0000

    Remove service tests

--- tests/components/snooz/test_fan.py
+++ tests/components/snooz/test_fan.py
@@ -5,14 +5,7 @@ from pysnooz.api import SnoozDeviceState
 import pytest
 
 from homeassistant.components import fan
-from homeassistant.components.snooz import DOMAIN
-from homeassistant.components.snooz.fan import (
-    ATTR_LAST_COMMAND_SUCCESSFUL,
-    ATTR_TRANSITION,
-    ATTR_VOLUME,
-    SERVICE_DISCONNECT,
-    FanEntity,
-)
+from homeassistant.components.snooz.fan import ATTR_LAST_COMMAND_SUCCESSFUL, FanEntity
 from homeassistant.const import (
     ATTR_ASSUMED_STATE,
     ATTR_ENTITY_ID,
@@ -60,57 +53,6 @@ async def test_turn_on_with_percentage(
     assert ATTR_ASSUMED_STATE not in state.attributes
 
 
-async def test_turn_on_with_transition(hass: HomeAssistant, snooz_fan: FanEntity):
-    """Test turning on the device with a transition."""
-    await hass.services.async_call(
-        DOMAIN,
-        SERVICE_TURN_ON,
-        {ATTR_ENTITY_ID: [snooz_fan.entity_id], ATTR_TRANSITION: 1},
-        blocking=True,
-    )
-    await hass.async_block_till_done()
-
-    state = hass.states.get(snooz_fan.entity_id)
-    assert state.state == STATE_ON
-    assert state.attributes[ATTR_LAST_COMMAND_SUCCESSFUL] is True
-    assert ATTR_ASSUMED_STATE not in state.attributes
-
-
-async def test_turn_on_with_transition_and_volume(
-    hass: HomeAssistant, snooz_fan: FanEntity
-):
-    """Test turning on the device with a transition and target volume."""
-    # increases volume
-    await hass.services.async_call(
-        DOMAIN,
-        SERVICE_TURN_ON,
-        {ATTR_ENTITY_ID: [snooz_fan.entity_id], ATTR_TRANSITION: 1, ATTR_VOLUME: 33},
-        blocking=True,
-    )
-    await hass.async_block_till_done()
-
-    state = hass.states.get(snooz_fan.entity_id)
-    assert state.state == STATE_ON
-    assert state.attributes[ATTR_LAST_COMMAND_SUCCESSFUL] is True
-    assert state.attributes[fan.ATTR_PERCENTAGE] == 33
-    assert ATTR_ASSUMED_STATE not in state.attributes
-
-    # decreases volume
-    await hass.services.async_call(
-        DOMAIN,
-        SERVICE_TURN_ON,
-        {ATTR_ENTITY_ID: [snooz_fan.entity_id], ATTR_TRANSITION: 1, ATTR_VOLUME: 12},
-        blocking=True,
-    )
-    await hass.async_block_till_done()
-
-    state = hass.states.get(snooz_fan.entity_id)
-    assert state.state == STATE_ON
-    assert state.attributes[ATTR_LAST_COMMAND_SUCCESSFUL] is True
-    assert state.attributes[fan.ATTR_PERCENTAGE] == 12
-    assert ATTR_ASSUMED_STATE not in state.attributes
-
-
 async def test_turn_off(hass: HomeAssistant, snooz_fan: FanEntity):
     """Test turning off the device."""
     await hass.services.async_call(
@@ -124,45 +66,6 @@ async def test_turn_off(hass: HomeAssistant, snooz_fan: FanEntity):
     assert state.state == STATE_OFF
     assert state.attributes[ATTR_LAST_COMMAND_SUCCESSFUL] is True
     assert ATTR_ASSUMED_STATE not in state.attributes
-
-
-async def test_turn_off_with_transition(hass: HomeAssistant, snooz_fan: FanEntity):
-    """Test turning off the device with a transition."""
-    await hass.services.async_call(
-        DOMAIN,
-        SERVICE_TURN_OFF,
-        {ATTR_ENTITY_ID: [snooz_fan.entity_id], ATTR_TRANSITION: 1},
-        blocking=True,
-    )
-    await hass.async_block_till_done()
-
-    state = hass.states.get(snooz_fan.entity_id)
-    assert state.state == STATE_OFF
-    assert state.attributes[ATTR_LAST_COMMAND_SUCCESSFUL] is True
-    assert ATTR_ASSUMED_STATE not in state.attributes
-
-
-async def test_service_disconnect(hass: HomeAssistant, snooz_fan: FanEntity):
-    """Test service to disconnect device."""
-    await hass.services.async_call(
-        fan.DOMAIN,
-        SERVICE_TURN_ON,
-        {ATTR_ENTITY_ID: [snooz_fan.entity_id]},
-        blocking=True,
-    )
-
-    state = hass.states.get(snooz_fan.entity_id)
-    assert ATTR_ASSUMED_STATE not in state.attributes
-
-    await hass.services.async_call(
-        DOMAIN,
-        SERVICE_DISCONNECT,
-        {ATTR_ENTITY_ID: [snooz_fan.entity_id]},
-        blocking=True,
-    )
-
-    state = hass.states.get(snooz_fan.entity_id)
-    assert state.attributes[ATTR_ASSUMED_STATE] is True
 
 
 async def test_push_events(
