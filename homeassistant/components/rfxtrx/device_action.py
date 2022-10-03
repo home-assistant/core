b519bf533234aa8839568fe7f112210ed2e19c7a
Ben Randall <veleek@gmail.com>
2022-10-03 10:42:57 -0400

    Improve device_automation trigger validation (#75044)
    * improve device_automation trigger validation
    
    Validates the trigger configuration against the device_trigger schema before trying to access any of the properties in order to provide better error messages.
    Updates the error message to include an explicit indication that the error is coming from a trigger configuration.  The inner error message from the validator can be accessed by viewing the stack trace.
    Add test case for trigger missing domain.
    
    Make action and condition validation consistent with trigger.  This is not strictly necessary, but should be helpful for certain use cases that bypass some of the outer validation.
    
    Removed redundant schema elements from humidifier device_trigger.
    
    **Blueprint with missing `domain`**
    ```
    2022-07-12 06:02:18.351 ERROR (MainThread) [homeassistant.setup] Error during setup of component automation
    Traceback (most recent call last):
      File "/workspaces/core/homeassistant/setup.py", line 235, in _async_setup_component
        result = await task
      File "/workspaces/core/homeassistant/components/automation/__init__.py", line 241, in async_setup
        if not await _async_process_config(hass, config, component):
      File "/workspaces/core/homeassistant/components/automation/__init__.py", line 648, in _async_process_config
        await async_validate_config_item(hass, raw_config),
      File "/workspaces/core/homeassistant/components/automation/config.py", line 74, in async_validate_config_item
        config[CONF_TRIGGER] = await async_validate_trigger_config(
      File "/workspaces/core/homeassistant/helpers/trigger.py", line 59, in async_validate_trigger_config
        conf = await platform.async_validate_trigger_config(hass, conf)
      File "/workspaces/core/homeassistant/components/device_automation/trigger.py", line 67, in async_validate_trigger_config
        hass, config[CONF_DOMAIN], DeviceAutomationType.TRIGGER
    KeyError: 'domain'
    ```
    
    **Blueprint with missing `property` (specific to zwave_js event schema)**
    ```
    2022-07-12 06:09:54.206 ERROR (MainThread) [homeassistant.components.automation] Blueprint Missing Property generated invalid automation with inputs OrderedDict([('control_switch', '498be56d796836a67406e9ad373d23db')]): required key not provided @ data['property']. Got None
    ```
    
    **Blueprint with missing `domain`**
    ```
    2022-07-12 06:12:16.080 ERROR (MainThread) [homeassistant.components.automation] Blueprint Missing Domain generated invalid automation with inputs OrderedDict([('control_switch', '498be56d796836a67406e9ad373d23db')]): invalid trigger configuration: required key not provided @ data['domain']. Got <homeassistant.components.blueprint.models.BlueprintInputs object at 0x7f581e097820>
    ```
    
    **Blueprint with missing `property` (specific to zwave_js event schema)**
    ```
    2022-07-12 06:12:16.680 ERROR (MainThread) [homeassistant.components.automation] Blueprint Missing Property generated invalid automation with inputs OrderedDict([('control_switch', '498be56d796836a67406e9ad373d23db')]): invalid trigger configuration: required key not provided @ data['property']. Got <homeassistant.components.blueprint.models.BlueprintInputs object at 0x7f581c0dc9d0>
    ```
    
    * Revert humifidier TRIGGER_SCHEMA change.

--- homeassistant/components/rfxtrx/device_action.py
+++ homeassistant/components/rfxtrx/device_action.py
@@ -80,7 +80,6 @@ async def async_validate_action_config(
     hass: HomeAssistant, config: ConfigType
 ) -> ConfigType:
     """Validate config."""
-    config = ACTION_SCHEMA(config)
     commands, _ = _get_commands(hass, config[CONF_DEVICE_ID], config[CONF_TYPE])
     sub_type = config[CONF_SUBTYPE]
 
