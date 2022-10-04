f0812da173c509cd3c1cce4e824d91fd67303b8f
Glenn Waters <glenn@watrs.ca>
2022-10-03 22:06:49 -0400

    Simplify diagnostics returned.

--- homeassistant/components/environment_canada/diagnostics.py
+++ homeassistant/components/environment_canada/diagnostics.py
@@ -20,10 +20,7 @@ async def async_get_config_entry_diagnostics(
 
     diagnostics_data = {
         "config_entry_data": async_redact_data(dict(config_entry.data), TO_REDACT),
-        "weather_coordinator_data": weather_coord.data,
         "weather_data": dict(weather_coord.ec_data.conditions),
-        "radar_coordinator_data": coordinators["radar_coordinator"].data,
-        "aqhi_coordinator_data": coordinators["aqhi_coordinator"].data,
     }
 
     return diagnostics_data
