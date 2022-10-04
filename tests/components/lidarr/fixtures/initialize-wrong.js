7ece8c8191c21548581a94330e5b1d8249650705
Robert Hillis <tkdrob4390@yahoo.com>
2022-10-04 15:09:06 -0400

    fix js files

--- tests/components/lidarr/fixtures/initialize-wrong.js
+++ tests/components/lidarr/fixtures/initialize-wrong.js
@@ -1,11 +1,12 @@
 window.Radarr = {
-  apiRoot: "/api/v3",
-  apiKey: "1234567890abcdef1234567890abcdef",
-  release: "4.0.3.5849-develop",
-  version: "4.0.3.5849",
-  branch: "nightly",
-  analytics: true,
-  userHash: "abcd1234",
-  urlBase: "",
-  isProduction: true,
-};
+    apiRoot: '/api/v3',
+    apiKey: '1234567890abcdef1234567890abcdef',
+    release: '4.0.3.5849-develop',
+    version: '4.0.3.5849',
+    instanceName: 'Radarr',
+    branch: 'nightly',
+    analytics: true,
+    userHash: 'abcd1234',
+    urlBase: '',
+    isProduction: true
+  };
