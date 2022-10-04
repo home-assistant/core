85bec95120ddc2a1dcb7521dde6bd36436517aeb
riokuu <michalhuryn@gmail.com>
2022-10-04 13:50:00 +0200

    Applied suggestions by @epenet

--- homeassistant/components/blebox/binary_sensor.py
+++ homeassistant/components/blebox/binary_sensor.py
@@ -1,11 +1,31 @@
 """BleBox sensor entities."""
+from dataclasses import dataclass
 
-from homeassistant.components.binary_sensor import BinarySensorEntity
+from blebox_uniapi.binary_sensor import BinarySensor as BinarySensorFeature
+
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntity,
+    BinarySensorEntityDescription,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import BleBoxEntity, create_blebox_entities
+
+
+@dataclass
+class BleboxBinarySensorEntityDescription(BinarySensorEntityDescription):
+    """Class describing Blebox binary sensor entities."""
+
+
+BINARY_SENSOR_TYPES = (
+    BleboxBinarySensorEntityDescription(
+        key="moisture",
+        device_class=BinarySensorDeviceClass.MOISTURE,
+    ),
+)
 
 
 async def async_setup_entry(
@@ -25,14 +45,18 @@ async def async_setup_entry(
 
 
 class BleBoxBinarySensorEntity(BleBoxEntity, BinarySensorEntity):
-    """Representation of a BleBox sensor feature."""
+    """Representation of a BleBox binary sensor feature."""
+
+    def __init__(self, feature: BinarySensorFeature) -> None:
+        """Initialize a BleBox sensor feature."""
+        super().__init__(feature)
+
+        for description in BINARY_SENSOR_TYPES:
+            if description.key == feature.device_class:
+                self.entity_description = description
+                break
 
     @property
-    def is_on(self):
+    def is_on(self) -> bool:
         """Return the state."""
         return self._feature.state
-
-    @property
-    def device_class(self):
-        """Return the device class."""
-        return self._feature.device_class
