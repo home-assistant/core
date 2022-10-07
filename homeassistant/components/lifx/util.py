63822b2eaa13b485ccb608eb857bd737795ca309
Avi Miller <me@dje.li>
2022-10-07 12:34:27 +1100

    Check for kelvin changes after HS because it sets sat to 0
    Signed-off-by: Avi Miller <me@dje.li>

--- homeassistant/components/lifx/util.py
+++ homeassistant/components/lifx/util.py
@@ -87,15 +87,15 @@ def find_hsbk(hass: HomeAssistant, **kwargs: Any) -> list[float | int | None] | 
     elif ATTR_XY_COLOR in kwargs:
         hue, saturation = color_util.color_xy_to_hs(*kwargs[ATTR_XY_COLOR])
 
-    if ATTR_COLOR_TEMP_KELVIN in kwargs:
-        kelvin = kwargs.pop(ATTR_COLOR_TEMP_KELVIN)
-        saturation = 0
-
     if hue is not None:
         assert saturation is not None
         hue = int(hue / 360 * 65535)
         saturation = int(saturation / 100 * 65535)
         kelvin = 3500
+
+    if ATTR_COLOR_TEMP_KELVIN in kwargs:
+        kelvin = kwargs.pop(ATTR_COLOR_TEMP_KELVIN)
+        saturation = 0
 
     if ATTR_BRIGHTNESS in kwargs:
         brightness = convert_8_to_16(kwargs[ATTR_BRIGHTNESS])
