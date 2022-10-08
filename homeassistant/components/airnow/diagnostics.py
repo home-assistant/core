120a4e46031e0d2bcc392a415710426513934b86
Aaron Bach <bachya1208@gmail.com>
2022-10-08 13:55:02 -0600

    Add diagnostics to AirNow

---
+++ homeassistant/components/airnow/diagnostics.py
@@ -0,0 +1,42 @@
+"""Diagnostics support for AirNow."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE
+from homeassistant.core import HomeAssistant
+
+from . import AirNowDataUpdateCoordinator
+from .const import DOMAIN
+
+ATTR_LATITUDE_CAP = "Latitude"
+ATTR_LONGITUDE_CAP = "Longitude"
+ATTR_REPORTING_AREA = "ReportingArea"
+ATTR_STATE_CODE = "StateCode"
+
+TO_REDACT = {
+    ATTR_LATITUDE_CAP,
+    ATTR_LONGITUDE_CAP,
+    ATTR_REPORTING_AREA,
+    ATTR_STATE_CODE,
+    CONF_API_KEY,
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    coordinator: AirNowDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+
+    return {
+        "entry": {
+            "title": entry.title,
+            "data": async_redact_data(dict(entry.data), TO_REDACT),
+        },
+        "data": async_redact_data(coordinator.data, TO_REDACT),
+    }
---
+++ tests/components/airnow/test_diagnostics.py
@@ -0,0 +1,34 @@
+"""Test AirNow diagnostics."""
+from homeassistant.components.diagnostics import REDACTED
+
+from tests.components.diagnostics import get_diagnostics_for_config_entry
+
+
+async def test_entry_diagnostics(hass, config_entry, hass_client, setup_airnow):
+    """Test config entry diagnostics."""
+    assert await get_diagnostics_for_config_entry(hass, hass_client, config_entry) == {
+        "entry": {
+            "title": "Mock Title",
+            "data": {
+                "api_key": REDACTED,
+                "latitude": REDACTED,
+                "longitude": REDACTED,
+                "radius": 75,
+            },
+        },
+        "data": {
+            "O3": 0.048,
+            "PM2.5": 8.9,
+            "HourObserved": 15,
+            "DateObserved": "2020-12-20",
+            "StateCode": REDACTED,
+            "ReportingArea": REDACTED,
+            "Latitude": REDACTED,
+            "Longitude": REDACTED,
+            "PM10": 12,
+            "AQI": 44,
+            "Category.Number": 1,
+            "Category.Name": "Good",
+            "Pollutant": "O3",
+        },
+    }
