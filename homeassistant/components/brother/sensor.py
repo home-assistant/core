791f1bcf7ba6c3810980143e44f8d2903718b640
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-10-08 11:32:57 +0200

    Rework BrotherSensorEntityDescription

--- homeassistant/components/brother/sensor.py
+++ homeassistant/components/brother/sensor.py
@@ -1,10 +1,13 @@
 """Support for the Brother service."""
 from __future__ import annotations
 
+from collections.abc import Callable
 from dataclasses import dataclass
 from datetime import datetime
 import logging
-from typing import Any, cast
+from typing import Any
+
+from brother import BrotherSensors
 
 from homeassistant.components.sensor import (
     DOMAIN as PLATFORM,
@@ -25,43 +28,24 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from . import BrotherDataUpdateCoordinator
 from .const import DATA_CONFIG_ENTRY, DOMAIN
 
-ATTR_BELT_UNIT_REMAINING_LIFE = "belt_unit_remaining_life"
 ATTR_BLACK_DRUM_COUNTER = "black_drum_counter"
 ATTR_BLACK_DRUM_REMAINING_LIFE = "black_drum_remaining_life"
 ATTR_BLACK_DRUM_REMAINING_PAGES = "black_drum_remaining_pages"
-ATTR_BLACK_INK_REMAINING = "black_ink_remaining"
-ATTR_BLACK_TONER_REMAINING = "black_toner_remaining"
-ATTR_BW_COUNTER = "bw_counter"
-ATTR_COLOR_COUNTER = "color_counter"
 ATTR_COUNTER = "counter"
 ATTR_CYAN_DRUM_COUNTER = "cyan_drum_counter"
 ATTR_CYAN_DRUM_REMAINING_LIFE = "cyan_drum_remaining_life"
 ATTR_CYAN_DRUM_REMAINING_PAGES = "cyan_drum_remaining_pages"
-ATTR_CYAN_INK_REMAINING = "cyan_ink_remaining"
-ATTR_CYAN_TONER_REMAINING = "cyan_toner_remaining"
 ATTR_DRUM_COUNTER = "drum_counter"
 ATTR_DRUM_REMAINING_LIFE = "drum_remaining_life"
 ATTR_DRUM_REMAINING_PAGES = "drum_remaining_pages"
-ATTR_DUPLEX_COUNTER = "duplex_unit_pages_counter"
-ATTR_FUSER_REMAINING_LIFE = "fuser_remaining_life"
-ATTR_LASER_REMAINING_LIFE = "laser_remaining_life"
 ATTR_MAGENTA_DRUM_COUNTER = "magenta_drum_counter"
 ATTR_MAGENTA_DRUM_REMAINING_LIFE = "magenta_drum_remaining_life"
 ATTR_MAGENTA_DRUM_REMAINING_PAGES = "magenta_drum_remaining_pages"
-ATTR_MAGENTA_INK_REMAINING = "magenta_ink_remaining"
-ATTR_MAGENTA_TONER_REMAINING = "magenta_toner_remaining"
 ATTR_MANUFACTURER = "Brother"
-ATTR_PAGE_COUNTER = "page_counter"
-ATTR_PF_KIT_1_REMAINING_LIFE = "pf_kit_1_remaining_life"
-ATTR_PF_KIT_MP_REMAINING_LIFE = "pf_kit_mp_remaining_life"
 ATTR_REMAINING_PAGES = "remaining_pages"
-ATTR_STATUS = "status"
-ATTR_UPTIME = "uptime"
 ATTR_YELLOW_DRUM_COUNTER = "yellow_drum_counter"
 ATTR_YELLOW_DRUM_REMAINING_LIFE = "yellow_drum_remaining_life"
 ATTR_YELLOW_DRUM_REMAINING_PAGES = "yellow_drum_remaining_pages"
-ATTR_YELLOW_INK_REMAINING = "yellow_ink_remaining"
-ATTR_YELLOW_TONER_REMAINING = "yellow_toner_remaining"
 
 UNIT_PAGES = "p"
 
@@ -122,10 +106,8 @@ async def async_setup_entry(
     )
 
     for description in SENSOR_TYPES:
-        if getattr(coordinator.data, description.key) is not None:
-            sensors.append(
-                description.entity_class(coordinator, description, device_info)
-            )
+        if description.value(coordinator.data) is not None:
+            sensors.append(BrotherPrinterSensor(coordinator, description, device_info))
     async_add_entities(sensors, False)
 
 
@@ -144,15 +126,9 @@ class BrotherPrinterSensor(CoordinatorEntity, SensorEntity):
         super().__init__(coordinator)
         self._attrs: dict[str, Any] = {}
         self._attr_device_info = device_info
+        self._attr_native_value = description.value(coordinator.data)
         self._attr_unique_id = f"{coordinator.data.serial.lower()}_{description.key}"
         self.entity_description = description
-
-    @property
-    def native_value(self) -> StateType | datetime:
-        """Return the state."""
-        return cast(
-            StateType, getattr(self.coordinator.data, self.entity_description.key)
-        )
 
     @property
     def extra_state_attributes(self) -> dict[str, Any]:
@@ -168,213 +144,232 @@ class BrotherPrinterSensor(CoordinatorEntity, SensorEntity):
         return self._attrs
 
 
-class BrotherPrinterUptimeSensor(BrotherPrinterSensor):
-    """Define an Brother Printer Uptime sensor."""
+@dataclass
+class BrotherSensorRequiredKeysMixin:
+    """Class for Brother entity required keys."""
 
-    @property
-    def native_value(self) -> datetime:
-        """Return the state."""
-        return cast(
-            datetime, getattr(self.coordinator.data, self.entity_description.key)
-        )
+    value: Callable[[BrotherSensors], StateType | datetime]
 
 
 @dataclass
-class BrotherSensorEntityDescription(SensorEntityDescription):
+class BrotherSensorEntityDescription(
+    SensorEntityDescription, BrotherSensorRequiredKeysMixin
+):
     """A class that describes sensor entities."""
-
-    entity_class: type[BrotherPrinterSensor] = BrotherPrinterSensor
 
 
 SENSOR_TYPES: tuple[BrotherSensorEntityDescription, ...] = (
     BrotherSensorEntityDescription(
-        key=ATTR_STATUS,
+        key="status",
         icon="mdi:printer",
         name="Status",
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.status,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_PAGE_COUNTER,
+        key="page_counter",
         icon="mdi:file-document-outline",
         name="Page counter",
         native_unit_of_measurement=UNIT_PAGES,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.page_counter,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_BW_COUNTER,
+        key="bw_counter",
         icon="mdi:file-document-outline",
         name="B/W counter",
         native_unit_of_measurement=UNIT_PAGES,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.bw_counter,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_COLOR_COUNTER,
+        key="color_counter",
         icon="mdi:file-document-outline",
         name="Color counter",
         native_unit_of_measurement=UNIT_PAGES,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.color_counter,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_DUPLEX_COUNTER,
+        key="duplex_unit_pages_counter",
         icon="mdi:file-document-outline",
         name="Duplex unit pages counter",
         native_unit_of_measurement=UNIT_PAGES,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.duplex_unit_pages_counter,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_DRUM_REMAINING_LIFE,
+        key="drum_remaining_life",
         icon="mdi:chart-donut",
         name="Drum remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.drum_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_BLACK_DRUM_REMAINING_LIFE,
+        key="black_drum_remaining_life",
         icon="mdi:chart-donut",
         name="Black drum remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.black_drum_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_CYAN_DRUM_REMAINING_LIFE,
+        key="cyan_drum_remaining_life",
         icon="mdi:chart-donut",
         name="Cyan drum remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.cyan_drum_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_MAGENTA_DRUM_REMAINING_LIFE,
+        key="magenta_drum_remaining_life",
         icon="mdi:chart-donut",
         name="Magenta drum remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.magenta_drum_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_YELLOW_DRUM_REMAINING_LIFE,
+        key="yellow_drum_remaining_life",
         icon="mdi:chart-donut",
         name="Yellow drum remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.yellow_drum_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_BELT_UNIT_REMAINING_LIFE,
+        key="belt_unit_remaining_life",
         icon="mdi:current-ac",
         name="Belt unit remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.belt_unit_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_FUSER_REMAINING_LIFE,
+        key="fuser_remaining_life",
         icon="mdi:water-outline",
         name="Fuser remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.fuser_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_LASER_REMAINING_LIFE,
+        key="laser_remaining_life",
         icon="mdi:spotlight-beam",
         name="Laser remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.laser_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_PF_KIT_1_REMAINING_LIFE,
+        key="pf_kit_1_remaining_life",
         icon="mdi:printer-3d",
         name="PF Kit 1 remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.pf_kit_1_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_PF_KIT_MP_REMAINING_LIFE,
+        key="pf_kit_mp_remaining_life",
         icon="mdi:printer-3d",
         name="PF Kit MP remaining life",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.pf_kit_mp_remaining_life,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_BLACK_TONER_REMAINING,
+        key="black_toner_remaining",
         icon="mdi:printer-3d-nozzle",
         name="Black toner remaining",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.black_toner_remaining,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_CYAN_TONER_REMAINING,
+        key="cyan_toner_remaining",
         icon="mdi:printer-3d-nozzle",
         name="Cyan toner remaining",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.cyan_toner_remaining,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_MAGENTA_TONER_REMAINING,
+        key="magenta_toner_remaining",
         icon="mdi:printer-3d-nozzle",
         name="Magenta toner remaining",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.magenta_toner_remaining,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_YELLOW_TONER_REMAINING,
+        key="yellow_toner_remaining",
         icon="mdi:printer-3d-nozzle",
         name="Yellow toner remaining",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.yellow_toner_remaining,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_BLACK_INK_REMAINING,
+        key="black_ink_remaining",
         icon="mdi:printer-3d-nozzle",
         name="Black ink remaining",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.black_ink_remaining,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_CYAN_INK_REMAINING,
+        key="cyan_ink_remaining",
         icon="mdi:printer-3d-nozzle",
         name="Cyan ink remaining",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.cyan_ink_remaining,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_MAGENTA_INK_REMAINING,
+        key="magenta_ink_remaining",
         icon="mdi:printer-3d-nozzle",
         name="Magenta ink remaining",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.magenta_ink_remaining,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_YELLOW_INK_REMAINING,
+        key="yellow_ink_remaining",
         icon="mdi:printer-3d-nozzle",
         name="Yellow ink remaining",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value=lambda data: data.yellow_ink_remaining,
     ),
     BrotherSensorEntityDescription(
-        key=ATTR_UPTIME,
+        key="uptime",
         name="Uptime",
         entity_registry_enabled_default=False,
         device_class=SensorDeviceClass.TIMESTAMP,
         entity_category=EntityCategory.DIAGNOSTIC,
-        entity_class=BrotherPrinterUptimeSensor,
+        value=lambda data: data.uptime,
     ),
 )
