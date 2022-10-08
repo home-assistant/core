429614cc00a47824105e225f380f22253a3c9ab5
Charlie Bailly <charliebailly29@gmail.com>
2022-10-08 14:17:43 +0000

    Setup async and use notify service in ClickSend TTS tests

--- tests/components/clicksend_tts/test_notify.py
+++ tests/components/clicksend_tts/test_notify.py
@@ -1,11 +1,17 @@
 """The test for the Facebook notify module."""
 import base64
 from http import HTTPStatus
+import logging
+from unittest.mock import patch
 
 import pytest
 import requests_mock
 
+from homeassistant.components import notify
 import homeassistant.components.clicksend_tts.notify as cs_tts
+from homeassistant.setup import async_setup_component
+
+from tests.common import assert_setup_component
 
 # Infos from https://developers.clicksend.com/docs/rest/v3/#testing
 TEST_USERNAME = "nocredit"
@@ -17,37 +23,75 @@ TEST_LANGUAGE = "fr-fr"
 TEST_MESSAGE = "Just a test message!"
 
 
-@pytest.fixture
-def cs_tts_test_config():
-    """Fixture for a test config of ClickSend TTS."""
-    return {
+CONFIG = {
+    notify.DOMAIN: {
+        "platform": "clicksend_tts",
         cs_tts.CONF_USERNAME: TEST_USERNAME,
         cs_tts.CONF_API_KEY: TEST_API_KEY,
         cs_tts.CONF_RECIPIENT: TEST_VOICE_NUMBER,
         cs_tts.CONF_LANGUAGE: TEST_LANGUAGE,
         cs_tts.CONF_VOICE: TEST_VOICE,
     }
+}
 
 
 @pytest.fixture
-def clicksend_tts(cs_tts_test_config):
-    """Fixture for ClickSend TTS."""
-    return cs_tts.ClicksendNotificationService(cs_tts_test_config)
+def mock_clicksend_tts_notify():
+    """Mock Clicksend TTS notify service."""
+    with patch(
+        "homeassistant.components.clicksend_tts.notify.get_service", autospec=True
+    ) as ns:
+        yield ns
 
 
-async def test_send_simple_message(hass, clicksend_tts):
+async def setup_notify(hass):
+    """Test setup."""
+    with assert_setup_component(1, notify.DOMAIN) as config:
+        assert await async_setup_component(hass, notify.DOMAIN, CONFIG)
+        assert config[notify.DOMAIN]
+        await hass.async_block_till_done()
+
+
+async def test_no_notify_service(hass, mock_clicksend_tts_notify, caplog):
+    """Test missing platform notify service instance."""
+    caplog.set_level(logging.ERROR)
+    mock_clicksend_tts_notify.return_value = None
+    await setup_notify(hass)
+    await hass.async_block_till_done()
+    assert mock_clicksend_tts_notify.called
+    assert "Failed to initialize notification service clicksend_tts" in caplog.text
+
+
+async def test_send_simple_message(hass):
     """Test sending a simple message with success."""
+
     with requests_mock.Mocker() as mock:
-        url = f"{cs_tts.BASE_API_URL}/voice/send"
-        mock.register_uri(
-            requests_mock.POST,
-            url,
+        # Mocking authentication endpoint
+        mock.get(
+            f"{cs_tts.BASE_API_URL}/account",
             status_code=HTTPStatus.OK,
         )
 
-        clicksend_tts.send_message(message=TEST_MESSAGE)
+        # Mocking TTS endpoint
+        mock.post(
+            f"{cs_tts.BASE_API_URL}/voice/send",
+            status_code=HTTPStatus.OK,
+        )
+
+        # Setting up integration
+        await setup_notify(hass)
+
+        # Sending message
+        data = {
+            notify.ATTR_MESSAGE: TEST_MESSAGE,
+        }
+        await hass.services.async_call(
+            notify.DOMAIN, cs_tts.DEFAULT_NAME, data, blocking=True
+        )
+
+        # Checking if everything went well
         assert mock.called
-        assert mock.call_count == 1
+        assert mock.call_count == 2
 
         expected_body = {
             "messages": [
