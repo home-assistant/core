4e04207574ae5a2355bab14e51f9f1a9f9b54485
Taiye <taiyeoguns@yahoo.com>
2022-10-08 17:03:56 +0000

    Convert darksky tests to pytest

--- tests/components/darksky/test_sensor.py
+++ tests/components/darksky/test_sensor.py
@@ -1,17 +1,15 @@
 """The tests for the Dark Sky platform."""
 from datetime import timedelta
 import re
-import unittest
 from unittest.mock import MagicMock, patch
 
 import forecastio
 from requests.exceptions import HTTPError
-import requests_mock
 
 from homeassistant.components.darksky import sensor as darksky
-from homeassistant.setup import setup_component
+from homeassistant.setup import async_setup_component
 
-from tests.common import get_test_home_assistant, load_fixture
+from tests.common import load_fixture
 
 VALID_CONFIG_MINIMAL = {
     "sensor": {
@@ -81,128 +79,122 @@ VALID_CONFIG_ALERTS = {
 }
 
 
+ENTITIES = []
+KEY = "foo"
+LAT = 37.8267
+LON = -122.423
+
+
 def load_forecastMock(key, lat, lon, units, lang):  # pylint: disable=invalid-name
     """Mock darksky forecast loading."""
     return ""
 
 
-class TestDarkSkySetup(unittest.TestCase):
-    """Test the Dark Sky platform."""
-
-    def add_entities(self, new_entities, update_before_add=False):
-        """Mock add entities."""
-        if update_before_add:
-            for entity in new_entities:
-                entity.update()
-
+def add_entities(new_entities, update_before_add=False):
+    """Mock add entities."""
+    if update_before_add:
         for entity in new_entities:
-            self.entities.append(entity)
+            entity.update()
 
-    def setUp(self):
-        """Initialize values for this testcase class."""
-        self.hass = get_test_home_assistant()
-        self.key = "foo"
-        self.lat = self.hass.config.latitude = 37.8267
-        self.lon = self.hass.config.longitude = -122.423
-        self.entities = []
-        self.addCleanup(self.tear_down_cleanup)
+    for entity in new_entities:
+        ENTITIES.append(entity)
 
-    def tear_down_cleanup(self):
-        """Stop everything that was started."""
-        self.hass.stop()
 
-    @patch(
-        "homeassistant.components.darksky.sensor.forecastio.load_forecast",
-        new=load_forecastMock,
+@patch(
+    "homeassistant.components.darksky.sensor.forecastio.load_forecast",
+    new=load_forecastMock,
+)
+async def test_setup_with_config(hass):
+    """Test the platform setup with configuration."""
+    await async_setup_component(hass, "sensor", VALID_CONFIG_MINIMAL)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.dark_sky_summary")
+    assert state is not None
+
+
+async def test_setup_with_invalid_config(hass):
+    """Test the platform setup with invalid configuration."""
+    await async_setup_component(hass, "sensor", INVALID_CONFIG_MINIMAL)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.dark_sky_summary")
+    assert state is None
+
+
+@patch(
+    "homeassistant.components.darksky.sensor.forecastio.load_forecast",
+    new=load_forecastMock,
+)
+async def test_setup_with_language_config(hass):
+    """Test the platform setup with language configuration."""
+    await async_setup_component(hass, "sensor", VALID_CONFIG_LANG_DE)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.dark_sky_summary")
+    assert state is not None
+
+
+async def test_setup_with_invalid_language_config(hass):
+    """Test the platform setup with language configuration."""
+    await async_setup_component(hass, "sensor", INVALID_CONFIG_LANG)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.dark_sky_summary")
+    assert state is None
+
+
+@patch("forecastio.api.get_forecast")
+def test_setup_bad_api_key(mock_get_forecast, hass):
+    """Test for handling a bad API key."""
+    # The Dark Sky API wrapper that we use raises an HTTP error
+    # when you try to use a bad (or no) API key.
+    url = "https://api.darksky.net/forecast/{}/{},{}?units=auto".format(
+        KEY, str(LAT), str(LON)
     )
-    def test_setup_with_config(self):
-        """Test the platform setup with configuration."""
-        setup_component(self.hass, "sensor", VALID_CONFIG_MINIMAL)
-        self.hass.block_till_done()
+    msg = f"400 Client Error: Bad Request for url: {url}"
+    mock_get_forecast.side_effect = HTTPError(msg)
 
-        state = self.hass.states.get("sensor.dark_sky_summary")
-        assert state is not None
+    response = darksky.setup_platform(hass, VALID_CONFIG_MINIMAL["sensor"], MagicMock())
+    assert not response
 
-    def test_setup_with_invalid_config(self):
-        """Test the platform setup with invalid configuration."""
-        setup_component(self.hass, "sensor", INVALID_CONFIG_MINIMAL)
-        self.hass.block_till_done()
 
-        state = self.hass.states.get("sensor.dark_sky_summary")
-        assert state is None
+@patch(
+    "homeassistant.components.darksky.sensor.forecastio.load_forecast",
+    new=load_forecastMock,
+)
+async def test_setup_with_alerts_config(hass):
+    """Test the platform setup with alert configuration."""
+    await async_setup_component(hass, "sensor", VALID_CONFIG_ALERTS)
+    await hass.async_block_till_done()
 
-    @patch(
-        "homeassistant.components.darksky.sensor.forecastio.load_forecast",
-        new=load_forecastMock,
+    state = hass.states.get("sensor.dark_sky_alerts")
+    assert state.state == "0"
+
+
+@patch("forecastio.api.get_forecast", wraps=forecastio.api.get_forecast)
+async def test_setup(mock_get_forecast, requests_mock, hass):
+    """Test for successfully setting up the forecast.io platform."""
+    uri = (
+        r"https://api.(darksky.net|forecast.io)\/forecast\/(\w+)\/"
+        r"(-?\d+\.?\d*),(-?\d+\.?\d*)"
     )
-    def test_setup_with_language_config(self):
-        """Test the platform setup with language configuration."""
-        setup_component(self.hass, "sensor", VALID_CONFIG_LANG_DE)
-        self.hass.block_till_done()
+    requests_mock.get(re.compile(uri), text=load_fixture("darksky.json"))
 
-        state = self.hass.states.get("sensor.dark_sky_summary")
-        assert state is not None
+    await async_setup_component(hass, "sensor", VALID_CONFIG_MINIMAL)
+    await hass.async_block_till_done()
 
-    def test_setup_with_invalid_language_config(self):
-        """Test the platform setup with language configuration."""
-        setup_component(self.hass, "sensor", INVALID_CONFIG_LANG)
-        self.hass.block_till_done()
+    assert mock_get_forecast.called
+    assert mock_get_forecast.call_count == 1
+    assert hass.states.async_entity_ids_count() == 13
 
-        state = self.hass.states.get("sensor.dark_sky_summary")
-        assert state is None
+    state = hass.states.get("sensor.dark_sky_summary")
+    assert state is not None
+    assert state.state == "Clear"
+    assert state.attributes.get("friendly_name") == "Dark Sky Summary"
+    state = hass.states.get("sensor.dark_sky_alerts")
+    assert state.state == "2"
 
-    @patch("forecastio.api.get_forecast")
-    def test_setup_bad_api_key(self, mock_get_forecast):
-        """Test for handling a bad API key."""
-        # The Dark Sky API wrapper that we use raises an HTTP error
-        # when you try to use a bad (or no) API key.
-        url = "https://api.darksky.net/forecast/{}/{},{}?units=auto".format(
-            self.key, str(self.lat), str(self.lon)
-        )
-        msg = f"400 Client Error: Bad Request for url: {url}"
-        mock_get_forecast.side_effect = HTTPError(msg)
-
-        response = darksky.setup_platform(
-            self.hass, VALID_CONFIG_MINIMAL["sensor"], MagicMock()
-        )
-        assert not response
-
-    @patch(
-        "homeassistant.components.darksky.sensor.forecastio.load_forecast",
-        new=load_forecastMock,
-    )
-    def test_setup_with_alerts_config(self):
-        """Test the platform setup with alert configuration."""
-        setup_component(self.hass, "sensor", VALID_CONFIG_ALERTS)
-        self.hass.block_till_done()
-
-        state = self.hass.states.get("sensor.dark_sky_alerts")
-        assert state.state == "0"
-
-    @requests_mock.Mocker()
-    @patch("forecastio.api.get_forecast", wraps=forecastio.api.get_forecast)
-    def test_setup(self, mock_req, mock_get_forecast):
-        """Test for successfully setting up the forecast.io platform."""
-        uri = (
-            r"https://api.(darksky.net|forecast.io)\/forecast\/(\w+)\/"
-            r"(-?\d+\.?\d*),(-?\d+\.?\d*)"
-        )
-        mock_req.get(re.compile(uri), text=load_fixture("darksky.json"))
-
-        assert setup_component(self.hass, "sensor", VALID_CONFIG_MINIMAL)
-        self.hass.block_till_done()
-
-        assert mock_get_forecast.called
-        assert mock_get_forecast.call_count == 1
-        assert len(self.hass.states.entity_ids()) == 13
-
-        state = self.hass.states.get("sensor.dark_sky_summary")
-        assert state is not None
-        assert state.state == "Clear"
-        assert state.attributes.get("friendly_name") == "Dark Sky Summary"
-        state = self.hass.states.get("sensor.dark_sky_alerts")
-        assert state.state == "2"
-
-        state = self.hass.states.get("sensor.dark_sky_daytime_high_temperature_1d")
-        assert state is not None
-        assert state.attributes.get("device_class") == "temperature"
+    state = hass.states.get("sensor.dark_sky_daytime_high_temperature_1d")
+    assert state is not None
+    assert state.attributes.get("device_class") == "temperature"
--- tests/components/darksky/test_weather.py
+++ tests/components/darksky/test_weather.py
@@ -1,67 +1,51 @@
 """The tests for the Dark Sky weather component."""
 import re
-import unittest
 from unittest.mock import patch
 
 import forecastio
 from requests.exceptions import ConnectionError
-import requests_mock
 
 from homeassistant.components import weather
-from homeassistant.setup import setup_component
-from homeassistant.util.unit_system import METRIC_SYSTEM
+from homeassistant.setup import async_setup_component
 
-from tests.common import get_test_home_assistant, load_fixture
+from tests.common import load_fixture
 
 
-class TestDarkSky(unittest.TestCase):
-    """Test the Dark Sky weather component."""
+@patch("forecastio.api.get_forecast", wraps=forecastio.api.get_forecast)
+async def test_setup(mock_get_forecast, requests_mock, hass):
+    """Test for successfully setting up the forecast.io platform."""
+    uri = (
+        r"https://api.(darksky.net|forecast.io)\/forecast\/(\w+)\/"
+        r"(-?\d+\.?\d*),(-?\d+\.?\d*)"
+    )
+    requests_mock.get(re.compile(uri), text=load_fixture("darksky.json"))
 
-    def setUp(self):
-        """Set up things to be run when tests are started."""
-        self.hass = get_test_home_assistant()
-        self.hass.config.units = METRIC_SYSTEM
-        self.lat = self.hass.config.latitude = 37.8267
-        self.lon = self.hass.config.longitude = -122.423
-        self.addCleanup(self.tear_down_cleanup)
+    assert await async_setup_component(
+        hass,
+        weather.DOMAIN,
+        {"weather": {"name": "test", "platform": "darksky", "api_key": "foo"}},
+    )
+    await hass.async_block_till_done()
 
-    def tear_down_cleanup(self):
-        """Stop down everything that was started."""
-        self.hass.stop()
+    assert mock_get_forecast.called
+    assert mock_get_forecast.call_count == 1
 
-    @requests_mock.Mocker()
-    @patch("forecastio.api.get_forecast", wraps=forecastio.api.get_forecast)
-    def test_setup(self, mock_req, mock_get_forecast):
-        """Test for successfully setting up the forecast.io platform."""
-        uri = (
-            r"https://api.(darksky.net|forecast.io)\/forecast\/(\w+)\/"
-            r"(-?\d+\.?\d*),(-?\d+\.?\d*)"
-        )
-        mock_req.get(re.compile(uri), text=load_fixture("darksky.json"))
+    state = hass.states.get("weather.test")
+    assert state.state == "sunny"
 
-        assert setup_component(
-            self.hass,
-            weather.DOMAIN,
-            {"weather": {"name": "test", "platform": "darksky", "api_key": "foo"}},
-        )
-        self.hass.block_till_done()
 
-        assert mock_get_forecast.called
-        assert mock_get_forecast.call_count == 1
+@patch("forecastio.load_forecast", side_effect=ConnectionError())
+async def test_failed_setup(mock_load_forecast, hass):
+    """Test to ensure that a network error does not break component state."""
 
-        state = self.hass.states.get("weather.test")
-        assert state.state == "sunny"
+    assert await async_setup_component(
+        hass,
+        weather.DOMAIN,
+        {"weather": {"name": "test", "platform": "darksky", "api_key": "foo"}},
+    )
+    await hass.async_block_till_done()
 
-    @patch("forecastio.load_forecast", side_effect=ConnectionError())
-    def test_failed_setup(self, mock_load_forecast):
-        """Test to ensure that a network error does not break component state."""
+    assert mock_load_forecast.called
 
-        assert setup_component(
-            self.hass,
-            weather.DOMAIN,
-            {"weather": {"name": "test", "platform": "darksky", "api_key": "foo"}},
-        )
-        self.hass.block_till_done()
-
-        state = self.hass.states.get("weather.test")
-        assert state.state == "unavailable"
+    state = hass.states.get("weather.test")
+    assert state.state == "unavailable"
