933b5e569af4dd77095f3f2e5155ceb0d91a8324
jbouwh <jan@jbsoft.nl>
2022-10-10 08:56:01 +0000

    Process write req after processing callback jobs

--- homeassistant/components/mqtt/client.py
+++ homeassistant/components/mqtt/client.py
@@ -658,7 +658,7 @@ class MQTT:
                     timestamp,
                 ),
             )
-            self._mqtt_data.state_write_requests.process_write_state_requests(msg.topic)
+        self._mqtt_data.state_write_requests.process_write_state_requests(msg.topic)
 
     def _mqtt_on_callback(self, _mqttc, _userdata, mid, _granted_qos=None) -> None:
         """Publish / Subscribe / Unsubscribe callback."""
