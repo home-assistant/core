6c5650b523c2f9d718c4d7cc4a29106bbb1550b4
Avi Miller <me@dje.li>
2022-10-11 06:01:31 +1100

    Add support for the Flame and Morph effects for Tile and Candle (#80014)

--- homeassistant/components/lifx/manager.py
+++ homeassistant/components/lifx/manager.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 from typing import Any
 
 import aiolifx_effects
+from aiolifx_themes.themes import Theme, ThemeLibrary
 import voluptuous as vol
 
 from homeassistant.components.light import (
@@ -34,9 +35,11 @@ from .util import convert_8_to_16, find_hsbk
 
 SCAN_INTERVAL = timedelta(seconds=10)
 
-SERVICE_EFFECT_PULSE = "effect_pulse"
 SERVICE_EFFECT_COLORLOOP = "effect_colorloop"
+SERVICE_EFFECT_FLAME = "effect_flame"
+SERVICE_EFFECT_MORPH = "effect_morph"
 SERVICE_EFFECT_MOVE = "effect_move"
+SERVICE_EFFECT_PULSE = "effect_pulse"
 SERVICE_EFFECT_STOP = "effect_stop"
 
 ATTR_POWER_OFF = "power_off"
@@ -47,11 +50,20 @@ ATTR_SPREAD = "spread"
 ATTR_CHANGE = "change"
 ATTR_DIRECTION = "direction"
 ATTR_SPEED = "speed"
+ATTR_PALETTE = "palette"
+ATTR_THEME = "theme"
 
+EFFECT_FLAME = "FLAME"
+EFFECT_MORPH = "MORPH"
 EFFECT_MOVE = "MOVE"
 EFFECT_OFF = "OFF"
 
-EFFECT_MOVE_DEFAULT_SPEED = 3.0
+EFFECT_FLAME_DEFAULT_SPEED = 3
+
+EFFECT_MORPH_DEFAULT_SPEED = 3
+EFFECT_MORPH_DEFAULT_THEME = "exciting"
+
+EFFECT_MOVE_DEFAULT_SPEED = 3
 EFFECT_MOVE_DEFAULT_DIRECTION = "right"
 EFFECT_MOVE_DIRECTION_RIGHT = "right"
 EFFECT_MOVE_DIRECTION_LEFT = "left"
@@ -128,6 +140,37 @@ SERVICES = (
     SERVICE_EFFECT_COLORLOOP,
 )
 
+LIFX_EFFECT_FLAME_SCHEMA = cv.make_entity_service_schema(
+    {
+        **LIFX_EFFECT_SCHEMA,
+        ATTR_SPEED: vol.All(vol.Coerce(int), vol.Clamp(min=1, max=25)),
+    }
+)
+
+HSBK_SCHEMA = vol.All(
+    vol.Coerce(tuple),
+    vol.ExactSequence(
+        (
+            vol.All(vol.Coerce(float), vol.Range(min=0, max=360)),
+            vol.All(vol.Coerce(float), vol.Range(min=0, max=100)),
+            vol.All(vol.Coerce(float), vol.Clamp(min=0, max=100)),
+            vol.All(vol.Coerce(int), vol.Clamp(min=1500, max=9000)),
+        )
+    ),
+)
+
+LIFX_EFFECT_MORPH_SCHEMA = cv.make_entity_service_schema(
+    {
+        **LIFX_EFFECT_SCHEMA,
+        ATTR_SPEED: vol.All(vol.Coerce(int), vol.Clamp(min=1, max=25)),
+        vol.Exclusive(ATTR_THEME, COLOR_GROUP): vol.Optional(
+            vol.In(ThemeLibrary().themes)
+        ),
+        vol.Exclusive(ATTR_PALETTE, COLOR_GROUP): vol.All(
+            cv.ensure_list, [HSBK_SCHEMA]
+        ),
+    }
+)
 
 LIFX_EFFECT_MOVE_SCHEMA = cv.make_entity_service_schema(
     {
@@ -194,6 +237,20 @@ class LIFXManager:
 
         self.hass.services.async_register(
             DOMAIN,
+            SERVICE_EFFECT_FLAME,
+            service_handler,
+            schema=LIFX_EFFECT_FLAME_SCHEMA,
+        )
+
+        self.hass.services.async_register(
+            DOMAIN,
+            SERVICE_EFFECT_MORPH,
+            service_handler,
+            schema=LIFX_EFFECT_MORPH_SCHEMA,
+        )
+
+        self.hass.services.async_register(
+            DOMAIN,
             SERVICE_EFFECT_MOVE,
             service_handler,
             schema=LIFX_EFFECT_MOVE_SCHEMA,
@@ -222,7 +279,43 @@ class LIFXManager:
                 coordinators.append(coordinator)
                 bulbs.append(coordinator.device)
 
-        if service == SERVICE_EFFECT_MOVE:
+        if service == SERVICE_EFFECT_FLAME:
+            await asyncio.gather(
+                *(
+                    coordinator.async_set_matrix_effect(
+                        effect=EFFECT_FLAME,
+                        speed=kwargs.get(ATTR_SPEED, EFFECT_FLAME_DEFAULT_SPEED),
+                        power_on=kwargs.get(ATTR_POWER_ON, True),
+                    )
+                    for coordinator in coordinators
+                )
+            )
+
+        elif service == SERVICE_EFFECT_MORPH:
+
+            theme_name = kwargs.get(ATTR_THEME, "exciting")
+            palette = kwargs.get(ATTR_PALETTE, None)
+
+            if palette is not None:
+                theme = Theme()
+                for hsbk in palette:
+                    theme.add_hsbk(hsbk[0], hsbk[1], hsbk[2], hsbk[3])
+            else:
+                theme = ThemeLibrary().get_theme(theme_name)
+
+            await asyncio.gather(
+                *(
+                    coordinator.async_set_matrix_effect(
+                        effect=EFFECT_MORPH,
+                        speed=kwargs.get(ATTR_SPEED, EFFECT_MORPH_DEFAULT_SPEED),
+                        palette=theme.colors,
+                        power_on=kwargs.get(ATTR_POWER_ON, True),
+                    )
+                    for coordinator in coordinators
+                )
+            )
+
+        elif service == SERVICE_EFFECT_MOVE:
             await asyncio.gather(
                 *(
                     coordinator.async_set_multizone_effect(
@@ -269,9 +362,9 @@ class LIFXManager:
             await self.effects_conductor.stop(bulbs)
 
             for coordinator in coordinators:
+                await coordinator.async_set_matrix_effect(
+                    effect=EFFECT_OFF, power_on=False
+                )
                 await coordinator.async_set_multizone_effect(
-                    effect=EFFECT_OFF,
-                    speed=EFFECT_MOVE_DEFAULT_SPEED,
-                    direction=EFFECT_MOVE_DEFAULT_DIRECTION,
-                    power_on=False,
+                    effect=EFFECT_OFF, power_on=False
                 )
