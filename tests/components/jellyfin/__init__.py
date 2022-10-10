09dc1dc89fc9c31d5b90666bc8157e71d8d6a6e7
Chris Talkington <chris@talkingtontech.com>
2022-10-09 21:43:52 -0500

    tweaks to fixture loading

--- tests/components/jellyfin/__init__.py
+++ tests/components/jellyfin/__init__.py
@@ -8,20 +8,3 @@ from tests.common import load_fixture
 def load_json_fixture(filename: str) -> Any:
     """Load JSON fixture on-demand."""
     return json.loads(load_fixture(f"jellyfin/{filename}"))
-
-
-MOCK_AUTH_CONNECT_ADDRESS_SUCCESS = json.loads(
-    load_fixture("jellyfin/auth-connect-address.json")
-)
-
-MOCK_AUTH_CONNECT_ADDRESS_FAILURE = json.loads(
-    load_fixture("jellyfin/auth-connect-address-failure.json")
-)
-
-MOCK_AUTH_LOGIN_SUCCESS = json.loads(load_fixture("jellyfin/auth-login.json"))
-
-MOCK_AUTH_LOGIN_FAILURE = json.loads(load_fixture("jellyfin/auth-login-failure.json"))
-
-MOCK_GET_USER_SETTINGS_SUCCESS = json.loads(
-    load_fixture("jellyfin/get-user-settings.json")
-)
