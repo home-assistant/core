5852b82b2151a6e8d200af4db712732346e50af5
Chris Talkington <chris@talkingtontech.com>
2022-10-09 22:23:00 -0500

    tweaks to fixture loading (async)

--- tests/components/jellyfin/__init__.py
+++ tests/components/jellyfin/__init__.py
@@ -2,9 +2,18 @@
 import json
 from typing import Any
 
+from homeassistant.core import HomeAssistant
+from homeassistant.loader import bind_hass
+
 from tests.common import load_fixture
 
 
 def load_json_fixture(filename: str) -> Any:
     """Load JSON fixture on-demand."""
     return json.loads(load_fixture(f"jellyfin/{filename}"))
+
+
+@bind_hass
+async def async_load_json_fixture(hass: HomeAssistant, filename: str) -> Any:
+    """Load JSON fixture on-demand asynchronously."""
+    return await hass.async_add_executor_job(load_json_fixture, filename)
--- tests/components/jellyfin/test_config_flow.py
+++ tests/components/jellyfin/test_config_flow.py
@@ -6,7 +6,7 @@ from homeassistant.components.jellyfin.const import CONF_CLIENT_DEVICE_ID, DOMAI
 from homeassistant.const import CONF_PASSWORD, CONF_URL, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 
-from . import load_json_fixture
+from . import async_load_json_fixture
 from .const import TEST_PASSWORD, TEST_URL, TEST_USERNAME
 
 from tests.common import MockConfigEntry
@@ -76,8 +76,8 @@ async def test_form_cannot_connect(
     assert result["type"] == "form"
     assert result["errors"] == {}
 
-    mock_client.auth.connect_to_address.return_value = load_json_fixture(
-        "auth-connect-address-failure.json"
+    mock_client.auth.connect_to_address.return_value = await async_load_json_fixture(
+        hass, "auth-connect-address-failure.json"
     )
 
     result2 = await hass.config_entries.flow.async_configure(
@@ -109,7 +109,9 @@ async def test_form_invalid_auth(
     assert result["type"] == "form"
     assert result["errors"] == {}
 
-    mock_client.auth.login.return_value = load_json_fixture("auth-login-failure.json")
+    mock_client.auth.login.return_value = await async_load_json_fixture(
+        hass, "auth-login-failure.json"
+    )
 
     result2 = await hass.config_entries.flow.async_configure(
         result["flow_id"],
@@ -170,7 +172,9 @@ async def test_form_persists_device_id_on_error(
     assert result["errors"] == {}
 
     mock_client_device_id.return_value = "TEST-UUID-1"
-    mock_client.auth.login.return_value = load_json_fixture("auth-login-failure.json")
+    mock_client.auth.login.return_value = await async_load_json_fixture(
+        hass, "auth-login-failure.json"
+    )
 
     result2 = await hass.config_entries.flow.async_configure(
         result["flow_id"],
@@ -186,7 +190,9 @@ async def test_form_persists_device_id_on_error(
     assert result2["errors"] == {"base": "invalid_auth"}
 
     mock_client_device_id.return_value = "TEST-UUID-2"
-    mock_client.auth.login.return_value = load_json_fixture("auth-login.json")
+    mock_client.auth.login.return_value = await async_load_json_fixture(
+        hass, "auth-login.json"
+    )
 
     result3 = await hass.config_entries.flow.async_configure(
         result2["flow_id"],
