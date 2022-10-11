1ab5d1a5c9d1057bd5003d77b844ec81fc66e171
Aaron Bach <bachya1208@gmail.com>
2022-10-11 11:23:01 -0600

    Redact additional sensitive fields in ReCollect Waste diagnostics

--- homeassistant/components/recollect_waste/diagnostics.py
+++ homeassistant/components/recollect_waste/diagnostics.py
@@ -4,11 +4,24 @@ from __future__ import annotations
 import dataclasses
 from typing import Any
 
+from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_UNIQUE_ID
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from .const import DOMAIN
+from .const import CONF_PLACE_ID, DOMAIN
+
+CONF_AREA_NAME = "area_name"
+CONF_TITLE = "title"
+
+TO_REDACT = {
+    CONF_AREA_NAME,
+    CONF_PLACE_ID,
+    # Config entry title and unique ID may contain sensitive data:
+    CONF_TITLE,
+    CONF_UNIQUE_ID,
+}
 
 
 async def async_get_config_entry_diagnostics(
@@ -18,6 +31,8 @@ async def async_get_config_entry_diagnostics(
     coordinator: DataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
     return {
-        "entry": entry.as_dict(),
-        "data": [dataclasses.asdict(event) for event in coordinator.data],
+        "entry": async_redact_data(entry.as_dict(), TO_REDACT),
+        "data": async_redact_data(
+            [dataclasses.asdict(event) for event in coordinator.data], TO_REDACT
+        ),
     }
