c76c7acac2c6e46c4e25f76458cbfb7bf62a42b7
Erik <erik@montnemery.com>
2022-10-11 10:27:44 +0200

    Add test

--- tests/components/recorder/test_init.py
+++ tests/components/recorder/test_init.py
@@ -17,12 +17,15 @@ from homeassistant.components.recorder import (
     CONF_AUTO_PURGE,
     CONF_AUTO_REPACK,
     CONF_COMMIT_INTERVAL,
+    CONF_DB_MAX_RETRIES,
+    CONF_DB_RETRY_WAIT,
     CONF_DB_URL,
     CONFIG_SCHEMA,
     DOMAIN,
     SQLITE_URL_PREFIX,
     Recorder,
     get_instance,
+    pool,
 )
 from homeassistant.components.recorder.const import KEEPALIVE_TIME
 from homeassistant.components.recorder.db_schema import (
@@ -1682,3 +1685,73 @@ async def test_mysql_missing_utf8mb4(hass, config_url, connect_args):
         await async_setup_component(hass, DOMAIN, {DOMAIN: {CONF_DB_URL: config_url}})
         create_engine_mock.assert_called_once()
         assert create_engine_mock.mock_calls[0][2].get("connect_args") == connect_args
+
+
+@pytest.mark.parametrize(
+    "config_url",
+    (
+        "mysql://user:password@SERVER_IP/DB_NAME",
+        "mysql://user:password@SERVER_IP/DB_NAME?charset=utf8mb4",
+        "mysql://user:password@SERVER_IP/DB_NAME?blah=bleh&charset=other",
+    ),
+)
+async def test_connect_args_priority(hass, config_url):
+    """Test connect_args has priority over URL query."""
+    connect_params = []
+    recorder_helper.async_initialize_recorder(hass)
+
+    class MockDialect:
+        """Non functioning dialect, good enough that SQLAlchemy tries connecting."""
+
+        __bases__ = []
+        _has_events = False
+
+        def __init__(*args, **kwargs):
+            ...
+
+        def connect(self, *args, **params):
+            nonlocal connect_params
+            connect_params.append(params)
+            return True
+
+        def create_connect_args(self, url):
+            return ([], {"charset": "invalid"})
+
+        @classmethod
+        def dbapi(cls):
+            ...
+
+        def engine_created(*args):
+            ...
+
+        def get_dialect_pool_class(self, *args):
+            return pool.RecorderPool
+
+        def initialize(*args):
+            ...
+
+        def on_connect_url(self, url):
+            return False
+
+    class MockEntrypoint:
+        def engine_created(*_):
+            ...
+
+        def get_dialect_cls(*_):
+            return MockDialect
+
+    with patch("sqlalchemy.engine.url.URL._get_entrypoint", MockEntrypoint), patch(
+        "sqlalchemy.engine.create.util.get_cls_kwargs", return_value=["echo"]
+    ):
+        await async_setup_component(
+            hass,
+            DOMAIN,
+            {
+                DOMAIN: {
+                    CONF_DB_URL: config_url,
+                    CONF_DB_MAX_RETRIES: 1,
+                    CONF_DB_RETRY_WAIT: 0,
+                }
+            },
+        )
+    assert connect_params == [{"charset": "utf8mb4"}]
