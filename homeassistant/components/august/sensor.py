de3e536ebb038c005006d78628f77a3a8fdca39a
J. Nick Koston <nick@koston.org>
2022-10-11 13:15:18 -1000

    Revert "make sure we pull in the manual locks"
    This reverts commit 96dc0e98d45b610d1a1eb7a769469d9decacd53e.

--- homeassistant/components/august/sensor.py
+++ homeassistant/components/august/sensor.py
@@ -192,8 +192,7 @@ class AugustOperatorSensor(AugustEntityMixin, RestoreEntity, SensorEntity):
     def _update_from_data(self):
         """Get the latest state of the sensor and update activity."""
         lock_activity = self._data.activity_stream.get_latest_device_activity(
-            self._device_id,
-            {ActivityType.LOCK_OPERATION, ActivityType.LOCK_OPERATION_WITHOUT_OPERATOR},
+            self._device_id, {ActivityType.LOCK_OPERATION}
         )
 
         self._attr_available = True
