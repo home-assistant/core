a42ed93dda4b274216c43afbc6c78ea7c41289ff
Aaron Bach <bachya1208@gmail.com>
2022-10-11 11:34:37 -0600

    One call

--- homeassistant/components/airvisual_pro/diagnostics.py
+++ homeassistant/components/airvisual_pro/diagnostics.py
@@ -27,7 +27,10 @@ async def async_get_config_entry_diagnostics(
     """Return diagnostics for a config entry."""
     coordinator: DataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
-    return {
-        "entry": async_redact_data(entry.as_dict(), TO_REDACT),
-        "data": async_redact_data(coordinator.data, TO_REDACT),
-    }
+    return async_redact_data(
+        {
+            "entry": entry.as_dict(),
+            "data": coordinator.data,
+        },
+        TO_REDACT,
+    )
