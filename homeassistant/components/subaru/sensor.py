a0dca81a4ab7f8b68a0f005cfc616da02e29abd4
epenet <6771947+epenet@users.noreply.github.com>
2022-10-12 20:02:17 +0000

    Adjust

--- homeassistant/components/subaru/sensor.py
+++ homeassistant/components/subaru/sensor.py
@@ -30,7 +30,7 @@ from homeassistant.helpers.update_coordinator import (
     DataUpdateCoordinator,
 )
 from homeassistant.util.unit_conversion import DistanceConverter, VolumeConverter
-from homeassistant.util.unit_system import LENGTH_UNITS, PRESSURE_UNITS
+from homeassistant.util.unit_system import LENGTH_UNITS, METRIC_SYSTEM, PRESSURE_UNITS
 
 from . import get_device_info
 from .const import (
@@ -223,7 +223,7 @@ class SubaruSensor(
         if unit in LENGTH_UNITS:
             return round(unit_system.length(current_value, unit), 1)
 
-        if unit in PRESSURE_UNITS and not unit_system.is_metric:
+        if unit in PRESSURE_UNITS and unit_system != METRIC_SYSTEM:
             return round(
                 unit_system.pressure(current_value, unit),
                 1,
@@ -235,7 +235,7 @@ class SubaruSensor(
                 FUEL_CONSUMPTION_LITERS_PER_HUNDRED_KILOMETERS,
                 FUEL_CONSUMPTION_MILES_PER_GALLON,
             ]
-            and not unit_system.is_metric
+            and unit_system != METRIC_SYSTEM
         ):
             return round((100.0 * L_PER_GAL) / (KM_PER_MI * current_value), 1)
 
@@ -250,14 +250,14 @@ class SubaruSensor(
             return self.hass.config.units.length_unit
 
         if unit in PRESSURE_UNITS:
-            if not self.hass.config.units.is_metric:
+            if self.hass.config.units != METRIC_SYSTEM:
                 return self.hass.config.units.pressure_unit
 
         if unit in [
             FUEL_CONSUMPTION_LITERS_PER_HUNDRED_KILOMETERS,
             FUEL_CONSUMPTION_MILES_PER_GALLON,
         ]:
-            if not self.hass.config.units.is_metric:
+            if self.hass.config.units != METRIC_SYSTEM:
                 return FUEL_CONSUMPTION_MILES_PER_GALLON
 
         return unit
