b43008a32653dccc44e94db9ab0beb7966e144bd
cyr-ius <cyr-ius@ipocus.net>
2022-10-15 17:30:55 +0200

    Add flow for calendars filter

--- homeassistant/components/caldav/config_flow.py
+++ homeassistant/components/caldav/config_flow.py
@@ -1,11 +1,21 @@
 """Config flow to configure caldav."""
-import logging
+from __future__ import annotations
 
+import logging
+from typing import Any
+
+from caldav.lib.error import DAVError
 import voluptuous as vol
 from yarl import URL as yurl
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_PASSWORD, CONF_URL, CONF_USERNAME, CONF_VERIFY_SSL
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_PASSWORD,
+    CONF_URL,
+    CONF_USERNAME,
+    CONF_VERIFY_SSL,
+)
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import selector
@@ -13,11 +23,15 @@ import homeassistant.helpers.config_validation as cv
 
 from . import async_caldav_connect
 from .const import (
-    CALDAV_EXCEPTIONS,
-    CONF_ADD_CUSTOM_CALENDAR,
+    CONF_CALENDAR,
+    CONF_CALENDAR_DELETE,
+    CONF_CALENDAR_ID,
+    CONF_CALENDAR_NEW_ID,
+    CONF_CALENDAR_UP_ID,
     CONF_CALENDARS,
     CONF_CUSTOM_CALENDARS,
     CONF_DAYS,
+    CONF_SEARCH,
     DEFAULT_VERIFY_SSL,
     DOMAIN,
 )
@@ -36,31 +50,8 @@ DATA_SCHEMA = vol.Schema(
             CONF_VERIFY_SSL, default=DEFAULT_VERIFY_SSL
         ): selector.BooleanSelector(),
         vol.Optional(CONF_DAYS, default=1): cv.positive_int,
-        vol.Optional(CONF_CALENDARS, default=[]): selector.SelectSelector(
-            selector.SelectSelectorConfig(options=[], custom_value=True, multiple=True),
-        ),
-        vol.Required(CONF_ADD_CUSTOM_CALENDAR, default=False): bool,
     }
 )
-
-CUSTOM_CALENDARS_SCHEMA = vol.Schema(
-    {
-        vol.Optional(
-            CONF_CUSTOM_CALENDARS,
-            description={
-                "suggested_value": [
-                    {
-                        "name": "HomeOffice",
-                        "calendar": "Agenda",
-                        "search": "HomeOffice",
-                    },
-                    "# Please clean this example",
-                ]
-            },
-        ): selector.ObjectSelector(),
-    }
-)
-
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -91,13 +82,10 @@ class CaldavFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 }
             )
 
-            if user_input.get(CONF_ADD_CUSTOM_CALENDAR):
-                return await self.async_step_custom_calendars(user_input)
-
             try:
                 await async_caldav_connect(self.hass, user_input)
                 url = yurl(user_input[CONF_URL])
-            except CALDAV_EXCEPTIONS:
+            except DAVError:
                 errors["base"] = "cannot_connect"
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
@@ -118,19 +106,6 @@ class CaldavFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
 
-    async def async_step_custom_calendars(self, user_input=None) -> FlowResult:
-        """Add custom calendars."""
-        if user_input.get(CONF_ADD_CUSTOM_CALENDAR):
-            user_input[CONF_ADD_CUSTOM_CALENDAR] = False
-            self.user_input = user_input
-        elif user_input:
-            self.user_input.update(user_input)
-            return await self.async_step_user(self.user_input)
-
-        return self.async_show_form(
-            step_id="custom_calendars", data_schema=CUSTOM_CALENDARS_SCHEMA
-        )
-
     async def async_step_import(self, import_config: dict[str, str]) -> FlowResult:
         """Import a config entry from configuration.yaml."""
         return await self.async_step_user(import_config)
@@ -142,35 +117,118 @@ class CaldavOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Init object."""
         self.config_entry = config_entry
-        self.calendars = config_entry.options.get(CONF_CALENDARS, [])
-        self.custom_calendars = config_entry.options.get(CONF_CUSTOM_CALENDARS, [])
+        calendars = config_entry.options.get(CONF_CUSTOM_CALENDARS, [])
+        self._calendars: list = calendars.copy()
+        self._conf_calendar_id: int | None = None
 
     async def async_step_init(self, user_input=None) -> FlowResult:
         """Handle a flow initialized by the user."""
-        errors = {}
+        if user_input is not None:
+            if sel_calendars := user_input.get(CONF_CUSTOM_CALENDARS):
+                return await self.async_step_calendars(None, sel_calendars)
+            return self._save_config(user_input)
+
+        return self._async_init_form()
+
+    @callback
+    def _save_config(self, data: dict[str, Any]) -> FlowResult:
+        """Save the updated options."""
+        new_data = {k: v for k, v in data.items() if k not in [CONF_CUSTOM_CALENDARS]}
+        if self._calendars:
+            new_data[CONF_CUSTOM_CALENDARS] = self._calendars
+
+        return self.async_create_entry(title="", data=new_data)
+
+    @callback
+    def _async_init_form(self) -> FlowResult:
+        """Handle a flow initialized by the user."""
+        inc = 0
+        calendars_list: dict = {}
+        for _calendar in self._calendars:
+            inc += 1
+            calendars_list.update({inc: f"{_calendar.get(CONF_NAME)}"})
+        options = self.config_entry.options
+
         options_schema = vol.Schema(
             {
                 vol.Optional(
-                    CONF_CALENDARS, default=self.calendars
+                    CONF_CALENDARS,
+                    description={"suggested_value": options.get(CONF_CALENDARS)},
                 ): selector.SelectSelector(
                     selector.SelectSelectorConfig(
                         options=[], custom_value=True, multiple=True
-                    ),
+                    )
                 ),
-                vol.Optional(
-                    CONF_CUSTOM_CALENDARS,
-                    default=[],
-                    description={"suggested_value": self.custom_calendars},
-                ): selector.ObjectSelector(),
+                vol.Optional(CONF_CUSTOM_CALENDARS): vol.In(
+                    {CONF_CALENDAR_NEW_ID: "Add calendar", **calendars_list}
+                ),
             }
         )
-        if user_input:
-            if not (
-                user_input.get(CONF_CALENDARS) and user_input.get(CONF_CUSTOM_CALENDARS)
-            ):
-                return self.async_create_entry(title="", data=user_input)
-            errors["base"] = "choice_custom"
 
-        return self.async_show_form(
-            step_id="init", data_schema=options_schema, errors=errors
-        )
+        return self.async_show_form(step_id="init", data_schema=options_schema)
+
+    async def async_step_calendars(
+        self, user_input: dict[str, Any] | None = None, calendar_id: int | None = None
+    ) -> FlowResult:
+        """Handle options flow for apps list."""
+        if calendar_id is not None:
+            self._conf_calendar_id = (
+                calendar_id if str(calendar_id) != CONF_CALENDAR_NEW_ID else None
+            )
+            return self._async_calendars_form(calendar_id)
+
+        if user_input is not None:
+            calendar_id = user_input.get(CONF_CALENDAR_ID, self._conf_calendar_id)
+            if calendar_id:
+                if user_input.get(CONF_CALENDAR_DELETE, False):
+                    del self._calendars[calendar_id - 1]
+                else:
+                    if idx := user_input.get(CONF_CALENDAR_UP_ID):
+                        del self._calendars[idx - 1]
+                    self._calendars.append(
+                        {
+                            CONF_NAME: user_input.get(CONF_NAME),
+                            CONF_CALENDAR: user_input.get(CONF_CALENDAR),
+                            CONF_SEARCH: user_input.get(CONF_SEARCH),
+                        }
+                    )
+
+        return await self.async_step_init()
+
+    @callback
+    def _async_calendars_form(self, calendar_id: int) -> FlowResult:
+        """Return configuration form for calendars."""
+        if str(calendar_id) == CONF_CALENDAR_NEW_ID:
+            item = {}
+        else:
+            item = self._calendars[calendar_id - 1]
+
+        calendar_schema = {
+            vol.Optional(
+                CONF_NAME,
+                description={"suggested_value": item.get(CONF_NAME)},
+            ): str,
+            vol.Optional(
+                CONF_CALENDAR,
+                description={"suggested_value": item.get(CONF_CALENDAR)},
+            ): str,
+            vol.Optional(
+                CONF_SEARCH,
+                description={"suggested_value": item.get(CONF_SEARCH)},
+            ): str,
+        }
+        if str(calendar_id) == CONF_CALENDAR_NEW_ID:
+            cal_id = int(len(self._calendars) + 1)
+            data_schema = vol.Schema(
+                {vol.Required(CONF_CALENDAR_ID): cal_id, **calendar_schema}
+            )
+        else:
+            data_schema = vol.Schema(
+                {
+                    vol.Required(CONF_CALENDAR_UP_ID): calendar_id,
+                    **calendar_schema,
+                    vol.Optional(CONF_CALENDAR_DELETE, default=False): bool,
+                }
+            )
+
+        return self.async_show_form(step_id="calendars", data_schema=data_schema)
--- homeassistant/components/caldav/const.py
+++ homeassistant/components/caldav/const.py
@@ -1,14 +1,15 @@
 """Constants for caldav integration."""
-from caldav.lib.error import DAVError
-from requests.exceptions import RequestException
 
 CONF_ADD_CUSTOM_CALENDAR = "add_new_custom_calendars"
 CONF_CALENDAR = "calendar"
+CONF_CALENDAR_NEW_ID = "calendar_new_id"
+CONF_CALENDAR_ID = "calendar_id"
+CONF_CALENDAR_UP_ID = "calendar_update_id"
 CONF_CALENDARS = "calendars"
+CONF_CALENDAR_DELETE = "calendar_delete"
 CONF_CUSTOM_CALENDARS = "custom_calendars"
 CONF_DAYS = "days"
 CONF_SEARCH = "search"
 DEFAULT_VERIFY_SSL = True
 DOMAIN = "caldav"
 OFFSET = "!!"
-CALDAV_EXCEPTIONS = (RequestException, DAVError)
