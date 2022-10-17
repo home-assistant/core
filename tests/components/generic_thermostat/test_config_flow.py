f243fd5460254df3dbc866fdb023814eb1cfe8a5
cyr-ius <cyr-ius@ipocus.net>
2022-10-17 20:15:21 +0200

    Add config_flow test

---
+++ tests/components/generic_thermostat/test_config_flow.py
@@ -0,0 +1,78 @@
+"""Tests for the Gneric Thermostat config flow."""
+from homeassistant import config_entries, data_entry_flow
+from homeassistant.components.generic_thermostat.consts import DOMAIN
+from homeassistant.config_entries import SOURCE_IMPORT, SOURCE_USER
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+
+MOCK_DATA = {
+    "name": "Generic Thermostat",
+    "heater": "switch.doe",
+    "target_sensor": "sensor.doe",
+    "min_temp": 7,
+    "max_temp": 35,
+    "target_temp": 20.1,
+    "ac_mode": True,
+    "min_cycle_duration": "00:01:00",
+    "cold_tolerance": 0.1,
+    "hot_tolerance": 0.1,
+    "keep_alive": "00:05:00",
+    "initial_hvac_mode": "auto",
+    "precision": 0.5,
+    "target_temp_step": 17.5,
+}
+
+
+async def test_user(hass: HomeAssistant):
+    """Test user config."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] == data_entry_flow.FlowResultType.FORM
+    assert result["step_id"] == "user"
+
+    # test with all provided
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_USER},
+        data=MOCK_DATA,
+    )
+    assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
+    assert result["options"]["name"] == "Generic Thermostat"
+
+
+async def test_import(hass: HomeAssistant):
+    """Test import step."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_IMPORT},
+        data=MOCK_DATA,
+    )
+    assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
+    assert result["options"]["name"] == "Generic Thermostat"
+
+
+async def test_form_already_configured(hass):
+    """Test that an entry with unique id can only be added once."""
+    MockConfigEntry(
+        domain=DOMAIN,
+        unique_id=f"{DOMAIN}.generic_thermostat",
+        data=MOCK_DATA,
+    ).add_to_hass(hass)
+
+    result_init = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    result_configure = await hass.config_entries.flow.async_configure(
+        result_init["flow_id"],
+        {
+            "heater": "switch.doe",
+            "target_sensor": "sensor.doe",
+        },
+    )
+    await hass.async_block_till_done()
+
+    assert result_configure["type"] == data_entry_flow.FlowResultType.ABORT
+    assert result_configure["reason"] == "already_configured"
