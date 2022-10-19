3940acc2144e1cd7323a5cd0c75021e0468b311b
epenet <6771947+epenet@users.noreply.github.com>
2022-10-19 04:15:23 +0200

    Add websocket type hints in components (#80533)

--- homeassistant/components/webhook/__init__.py
+++ homeassistant/components/webhook/__init__.py
@@ -195,7 +195,11 @@ def websocket_list(hass, connection, msg):
     }
 )
 @websocket_api.async_response
-async def websocket_handle(hass, connection, msg):
+async def websocket_handle(
+    hass: HomeAssistant,
+    connection: websocket_api.ActiveConnection,
+    msg: dict[str, Any],
+) -> None:
     """Handle an incoming webhook via the WS API."""
     request = MockRequest(
         content=msg["body"].encode("utf-8"),
