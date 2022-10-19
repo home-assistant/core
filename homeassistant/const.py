698b454582105066100b624cb69f160fb0cf240f
epenet <6771947+epenet@users.noreply.github.com>
2022-10-19 11:53:17 +0000

    Adjust comment

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -605,12 +605,16 @@ IRRADIATION_WATTS_PER_SQUARE_METER: Final = "W/m²"
 IRRADIATION_BTUS_PER_HOUR_SQUARE_FOOT: Final = "BTU/(h×ft²)"
 
 # Precipitation intensity units
+# The derivation of these units is a volume of rain amassing in a container
+# with constant cross section in a given time
 PRECIPITATION_INTENSITY_INCHES_PER_DAY: Final = "in/d"
 PRECIPITATION_INTENSITY_INCHES_PER_HOUR: Final = "in/h"
 PRECIPITATION_INTENSITY_MILLIMETERS_PER_DAY: Final = "mm/d"
 PRECIPITATION_INTENSITY_MILLIMETERS_PER_HOUR: Final = "mm/h"
 
 # Precipitation units
+# The derivation of these units is a volume of rain amassing in a container
+# with constant cross section
 PRECIPITATION_INCHES: Final = "in"
 PRECIPITATION_MILLIMETERS: Final = "mm"
 
