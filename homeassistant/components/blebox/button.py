4bee7212f700f391bfa00b5bd9c97a8f2daa3e2a
epenet <6771947+epenet@users.noreply.github.com>
2022-10-19 15:39:50 +0000

    Apply suggestion

--- homeassistant/components/blebox/button.py
+++ homeassistant/components/blebox/button.py
@@ -35,7 +35,7 @@ class BleBoxButtonEntity(BleBoxEntity[blebox_uniapi.button.Button], ButtonEntity
         super().__init__(feature)
         self._attr_icon = self.get_icon()
 
-    def get_icon(self) -> str:
+    def get_icon(self) -> str | None:
         """Return icon for endpoint."""
         if "up" in self._feature.query_string:
             return "mdi:arrow-up-circle"
@@ -47,7 +47,7 @@ class BleBoxButtonEntity(BleBoxEntity[blebox_uniapi.button.Button], ButtonEntity
             return "mdi:arrow-up-circle"
         if "close" in self._feature.query_string:
             return "mdi:arrow-down-circle"
-        return ""
+        return None
 
     async def async_press(self) -> None:
         """Handle the button press."""
