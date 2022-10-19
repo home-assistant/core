0a7cb01e2b3e55c6b857adc35c24af5eff363bb1
epenet <6771947+epenet@users.noreply.github.com>
2022-10-19 16:04:11 +0200

    Add precipitation_intensity sensor device class (#79779)

--- homeassistant/components/sensor/__init__.py
+++ homeassistant/components/sensor/__init__.py
@@ -241,6 +241,14 @@ class SensorDeviceClass(StrEnum):
     Unit of measurement: `W`, `kW`
     """
 
+    PRECIPITATION_INTENSITY = "precipitation_intensity"
+    """Precipitation intensity.
+
+    Unit of measurement:
+    - `in/d`, `in/h`
+    - `mm/d`, `mm/h`
+    """
+
     PRESSURE = "pressure"
     """Pressure.
 
@@ -361,6 +369,7 @@ STATE_CLASSES: Final[list[str]] = [cls.value for cls in SensorStateClass]
 
 UNIT_CONVERTERS: dict[str, type[BaseUnitConverter]] = {
     SensorDeviceClass.DISTANCE: DistanceConverter,
+    SensorDeviceClass.PRECIPITATION_INTENSITY: SpeedConverter,
     SensorDeviceClass.PRESSURE: PressureConverter,
     SensorDeviceClass.SPEED: SpeedConverter,
     SensorDeviceClass.TEMPERATURE: TemperatureConverter,
