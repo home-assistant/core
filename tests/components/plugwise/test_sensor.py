be8289fc9de2a045e63e6555ecb6723ed5864ab9
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-10-20 06:26:56 +0000

    Revert "Clean up test"
    This reverts commit 569d82648a5a70f1e312074c73746a657c7d9683.

--- tests/components/plugwise/test_sensor.py
+++ tests/components/plugwise/test_sensor.py
@@ -101,3 +101,5 @@ async def test_stretch_sensor_entities(
     # Test disabled sensor
     entity_id = "sensor.droger_52559_electricity_consumed_interval"
     assert hass.states.get(entity_id) is None
+    assert state
+    assert float(state.state) == 0.0
