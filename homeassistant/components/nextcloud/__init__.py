6a274e43bb48c81a453a7be7fdd9a93bc76596c4
Dusan Cervenka <cervenka.dusan@gmail.com>
2022-10-21 06:53:13 +0200

    #80673  Correct fix of ssl for nextcloud

--- homeassistant/components/nextcloud/__init__.py
+++ homeassistant/components/nextcloud/__init__.py
@@ -10,6 +10,7 @@ from homeassistant.const import (
     CONF_SCAN_INTERVAL,
     CONF_URL,
     CONF_USERNAME,
+    CONF_VERIFY_SSL,
     Platform,
 )
 from homeassistant.core import HomeAssistant
@@ -32,6 +33,7 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Required(CONF_USERNAME): cv.string,
                 vol.Required(CONF_PASSWORD): cv.string,
                 vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period,
+                vol.Optional(CONF_VERIFY_SSL, default=True): cv.boolean,
             }
         )
     },
@@ -97,7 +99,12 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     conf = config[DOMAIN]
 
     try:
-        ncm = NextcloudMonitor(conf[CONF_URL], conf[CONF_USERNAME], conf[CONF_PASSWORD])
+        ncm = NextcloudMonitor(
+            conf[CONF_URL],
+            conf[CONF_USERNAME],
+            conf[CONF_PASSWORD],
+            conf[CONF_VERIFY_SSL],
+        )
     except NextcloudMonitorError:
         _LOGGER.error("Nextcloud setup failed - Check configuration")
 
