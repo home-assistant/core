88244c83100a363a1adf394ff8a1c05444678ec6
TheJulianJES <TheJulianJES@users.noreply.github.com>
2022-10-22 02:59:51 +0200

    Add Philips Hue motion sensor config entities to ZHA (#79923)
    * Add Hue trigger LED config switch
    
    * Add Hue sensitivity config select
    
    * Use existing consts for channel names
    
    * Add friendly names to config entities
    
    * Follow HA capitalization conventions
    
    * Move Hue motion sensor check to a helper method
    
    * Move helper method to a new helpers file in channels folder

--- homeassistant/components/zha/core/channels/general.py
+++ homeassistant/components/zha/core/channels/general.py
@@ -28,6 +28,7 @@ from ..const import (
     SIGNAL_UPDATE_DEVICE,
 )
 from .base import AttrReportConfig, ClientChannel, ZigbeeChannel, parse_and_log_command
+from .helpers import is_hue_motion_sensor
 
 if TYPE_CHECKING:
     from . import ChannelPool
@@ -151,6 +152,15 @@ class BasicChannel(ZigbeeChannel):
         5: "Emergency mains constantly powered",
         6: "Emergency mains and transfer switch",
     }
+
+    def __init__(self, cluster: zigpy.zcl.Cluster, ch_pool: ChannelPool) -> None:
+        """Initialize Basic channel."""
+        super().__init__(cluster, ch_pool)
+        if is_hue_motion_sensor(self):
+            self.ZCL_INIT_ATTRS = (  # pylint: disable=invalid-name
+                self.ZCL_INIT_ATTRS.copy()
+            )
+            self.ZCL_INIT_ATTRS["trigger_indicator"] = True
 
 
 @registries.ZIGBEE_CHANNEL_REGISTRY.register(general.BinaryInput.cluster_id)
