bd94c17179da6caaaf92087549f4a4b54e1bebdb
Quentin POLLET <polletquentin74@me.com>
2022-10-24 01:22:04 +0200

    Freebox switch: use EntityDescription

--- homeassistant/components/freebox/switch.py
+++ homeassistant/components/freebox/switch.py
@@ -1,15 +1,17 @@
 """Support for Freebox Delta, Revolution and Mini 4K."""
 from __future__ import annotations
 
+from collections.abc import Callable
+from dataclasses import dataclass
 import logging
 from typing import Any
 
 from freebox_api.exceptions import InsufficientPermissionsError
 
-from homeassistant.components.switch import SwitchEntity
+from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
@@ -18,49 +20,68 @@ from .router import FreeboxRouter
 _LOGGER = logging.getLogger(__name__)
 
 
+@dataclass
+class FreeboxSwitchRequiredKeysMixin:
+    """Mixin for required keys."""
+
+    state_fn: Callable[[FreeboxRouter], Any]
+    on_off_fn: Callable[[FreeboxRouter], Any]
+
+
+@dataclass
+class FreeboxSwitchEntityDescription(
+    SwitchEntityDescription, FreeboxSwitchRequiredKeysMixin
+):
+    """Describes Freebox switch entity."""
+
+
+SWITCH_DESCRIPTIONS = [
+    FreeboxSwitchEntityDescription(
+        key="wifi",
+        name="WiFi",
+        has_entity_name=True,
+        entity_category=EntityCategory.CONFIG,
+        state_fn=lambda router: router.wifi.get_global_config,
+        on_off_fn=lambda router: router.wifi.set_global_config,
+    )
+]
+
+
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up the switch."""
     router: FreeboxRouter = hass.data[DOMAIN][entry.unique_id]
-    async_add_entities([FreeboxWifiSwitch(router)], True)
+    async_add_entities(
+        [
+            FreeboxSwitch(
+                router,
+                entity_description,
+            )
+            for entity_description in SWITCH_DESCRIPTIONS
+        ],
+        True,
+    )
 
 
-class FreeboxWifiSwitch(SwitchEntity):
-    """Representation of a freebox wifi switch."""
+class FreeboxSwitch(SwitchEntity):
+    """Representation of a freebox switch."""
 
-    def __init__(self, router: FreeboxRouter) -> None:
-        """Initialize the Wifi switch."""
-        self._name = "Freebox WiFi"
-        self._state: bool | None = None
+    entity_description: FreeboxSwitchEntityDescription
+
+    def __init__(
+        self, router: FreeboxRouter, entity_description: FreeboxSwitchEntityDescription
+    ) -> None:
+        """Initialize the switch."""
+        self.entity_description = entity_description
         self._router = router
-        self._unique_id = f"{self._router.mac} {self._name}"
-
-    @property
-    def unique_id(self) -> str:
-        """Return a unique ID."""
-        return self._unique_id
-
-    @property
-    def name(self) -> str:
-        """Return the name of the switch."""
-        return self._name
-
-    @property
-    def is_on(self) -> bool | None:
-        """Return true if device is on."""
-        return self._state
-
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return the device information."""
-        return self._router.device_info
+        self._attr_device_info = self._router.device_info
+        self._attr_unique_id = f"{self._router.mac} {self.entity_description.key}"
 
     async def _async_set_state(self, enabled: bool):
         """Turn the switch on or off."""
-        wifi_config = {"enabled": enabled}
         try:
-            await self._router.wifi.set_global_config(wifi_config)
+            await self.entity_description.on_off_fn(self._router)({"enabled": enabled})
         except InsufficientPermissionsError:
             _LOGGER.warning(
                 "Home Assistant does not have permissions to modify the Freebox settings. Please refer to documentation"
@@ -76,6 +97,5 @@ class FreeboxWifiSwitch(SwitchEntity):
 
     async def async_update(self) -> None:
         """Get the state and update it."""
-        datas = await self._router.wifi.get_global_config()
-        active = datas["enabled"]
-        self._state = bool(active)
+        datas = await self.entity_description.state_fn(self._router)()
+        self._attr_is_on = bool(datas["enabled"])
