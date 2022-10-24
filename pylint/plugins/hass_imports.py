dd13e33c84db9aaa83ee10c02d468aa8f65eec14
epenet <6771947+epenet@users.noreply.github.com>
2022-10-24 08:31:27 +0000

    Use correct location

--- pylint/plugins/hass_imports.py
+++ pylint/plugins/hass_imports.py
@@ -271,10 +271,6 @@ _OBSOLETE_IMPORT: dict[str, list[ObsoleteImportMatch]] = {
             reason="replaced by local constants",
             constant=re.compile(r"^(CONF_UNIT_SYSTEM_(\w*))$"),
         ),
-        ObsoleteImportMatch(
-            reason="replaced by US_CUSTOMARY_SYSTEM",
-            constant=re.compile(r"^IMPERIAL_SYSTEM$"),
-        ),
     ],
     "homeassistant.core": [
         ObsoleteImportMatch(
@@ -298,6 +294,12 @@ _OBSOLETE_IMPORT: dict[str, list[ObsoleteImportMatch]] = {
         ObsoleteImportMatch(
             reason="replaced by unit_conversion.***Converter",
             constant=re.compile(r"^(distance|pressure|speed|temperature|volume)$"),
+        ),
+    ],
+    "homeassistant.util.unit_system": [
+        ObsoleteImportMatch(
+            reason="replaced by US_CUSTOMARY_SYSTEM",
+            constant=re.compile(r"^IMPERIAL_SYSTEM$"),
         ),
     ],
 }
