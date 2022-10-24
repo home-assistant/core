aeff62b5e86a7173e03b786140cddd26187ad4a0
J. Nick Koston <nick@koston.org>
2022-10-24 07:57:42 -0500

    fixes

--- homeassistant/components/esphome/bluetooth/client.py
+++ homeassistant/components/esphome/bluetooth/client.py
@@ -58,7 +58,7 @@ def verify_connected(func: _WrapFuncType) -> _WrapFuncType:
         )
         if disconnected_future.done():
             disconnected_future.result()
-        return next(iter(done))
+        return next(iter(done)).result()
 
     return cast(_WrapFuncType, _async_wrap_bluetooth_connected_operation)
 
