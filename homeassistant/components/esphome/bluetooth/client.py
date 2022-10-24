eb1b65e8715782fabb125a2b6224997d826419a3
J. Nick Koston <nick@koston.org>
2022-10-24 07:52:17 -0500

    Raise exception when esphome ble client disconnects during operation

--- homeassistant/components/esphome/bluetooth/client.py
+++ homeassistant/components/esphome/bluetooth/client.py
@@ -49,7 +49,17 @@ def verify_connected(func: _WrapFuncType) -> _WrapFuncType:
     ) -> Any:
         if not self._is_connected:  # pylint: disable=protected-access
             raise BleakError("Not connected")
-        return await func(self, *args, **kwargs)
+        disconnected_future = (
+            self._disconnected_future  # pylint: disable=protected-access
+        )
+        assert disconnected_future is not None
+        done, _ = await asyncio.wait(
+            (func(self, *args, **kwargs), disconnected_future),
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+        if disconnected_future.done():
+            disconnected_future.result()
+        return done[0]
 
     return cast(_WrapFuncType, _async_wrap_bluetooth_connected_operation)
 
@@ -91,6 +101,7 @@ class ESPHomeClient(BaseBleakClient):
         self._mtu: int | None = None
         self._cancel_connection_state: CALLBACK_TYPE | None = None
         self._notify_cancels: dict[int, Callable[[], Coroutine[Any, Any, None]]] = {}
+        self._disconnected_future: asyncio.Future[None] | None = None
 
     def __str__(self) -> str:
         """Return the string representation of the client."""
@@ -114,6 +125,12 @@ class ESPHomeClient(BaseBleakClient):
         _LOGGER.debug("%s: BLE device disconnected", self._source)
         self._is_connected = False
         self.services = BleakGATTServiceCollection()  # type: ignore[no-untyped-call]
+        if self._disconnected_future and not self._disconnected_future.done():
+            self._disconnected_future.set_exception(
+                BleakError(
+                    f"{self._ble_device.name} ({self._ble_device.address}): Disconnected during operation"
+                )
+            )
         self._async_call_bleak_disconnected_callback()
         self._unsubscribe_connection_state()
 
@@ -184,6 +201,7 @@ class ESPHomeClient(BaseBleakClient):
         )
         await connected_future
         await self.get_services(dangerous_use_bleak_cache=dangerous_use_bleak_cache)
+        self._disconnected_future = asyncio.Future()
         return True
 
     @api_error_as_bleak_error
