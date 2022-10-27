f6f0dbfa6d35d7bc5c0461b58fa6dc674d010253
G Johansson <goran.johansson@shiftit.se>
2022-10-27 19:19:11 +0000

    next version

--- homeassistant/components/scrape/sensor.py
+++ homeassistant/components/scrape/sensor.py
@@ -91,7 +91,7 @@ async def async_setup_platform(
             hass,
             DOMAIN,
             "moved_yaml",
-            breaks_in_ha_version="2022.11.0",
+            breaks_in_ha_version="2022.12.0",
             is_fixable=False,
             severity=IssueSeverity.WARNING,
             translation_key="moved_yaml",
