e31841e26481369606c091001cf9c9f7727717ea
epenet <6771947+epenet@users.noreply.github.com>
2022-10-27 12:45:30 +0000

    Add TagProtocol for type checking

--- homeassistant/components/tag/__init__.py
+++ homeassistant/components/tag/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import logging
+from typing import Protocol
 import uuid
 
 import voluptuous as vol
@@ -104,6 +105,15 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     ).async_setup(hass)
 
     return True
+
+
+class TagProtocol(Protocol):
+    """Protocol for type checking."""
+
+    async def async_scan_tag(
+        self, tag_id: str, device_id: str, context: Context | None = None
+    ) -> None:
+        """Handle when a tag is scanned."""
 
 
 @bind_hass
