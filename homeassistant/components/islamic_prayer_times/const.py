be1c68270294a942c883a76a2bd93890cdc0a2f1
Rami Mosleh <engrbm87@gmail.com>
2022-10-27 11:06:01 +0000

    fix sorting

--- homeassistant/components/islamic_prayer_times/const.py
+++ homeassistant/components/islamic_prayer_times/const.py
@@ -1,7 +1,7 @@
 """Constants for the Islamic Prayer component."""
-from prayer_times_calculator import PrayerTimesCalculator
-
 from typing import Final
+
+from prayer_times_calculator import PrayerTimesCalculator
 
 DOMAIN: Final = "islamic_prayer_times"
 NAME: Final = "Islamic Prayer Times"
@@ -9,4 +9,4 @@ NAME: Final = "Islamic Prayer Times"
 CONF_CALC_METHOD: Final = "calculation_method"
 
 CALC_METHODS: list[str] = list(PrayerTimesCalculator.CALCULATION_METHODS)
-DEFAULT_CALC_METHOD = "isna"
+DEFAULT_CALC_METHOD: Final = "isna"
