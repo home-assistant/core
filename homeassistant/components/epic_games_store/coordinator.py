e549bce06e53e9ba05eca1c7c258dd48344f4f83
Quentin POLLET <polletquentin74@me.com>
2022-10-28 11:55:41 +0200

    Working coordinator with good filters

--- homeassistant/components/epic_games_store/coordinator.py
+++ homeassistant/components/epic_games_store/coordinator.py
@@ -1,16 +1,15 @@
 """The EGS integration."""
 from __future__ import annotations
 
-from datetime import timedelta, datetime
+from datetime import timedelta
 import logging
 from typing import Any
 
 from epicstore_api import EpicGamesStoreAPI
-import async_timeout
 
 from homeassistant.core import HomeAssistant
-from homeassistant.util import dt
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.util import dt
 
 from .const import DOMAIN
 
@@ -37,81 +36,68 @@ class EGSUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
     async def _async_update_data(self) -> dict[str, Any]:
         """Update data via library."""
         try:
-            data = await self.hass.async_add_executor_job(
-                self._api.get_free_games
-            )
+            data = await self.hass.async_add_executor_job(self._api.get_free_games)
         except Exception as error:
             raise UpdateFailed(error) from error
 
-        promo_games = list(sorted(
-            filter(
-                lambda g: g.get('promotions') and 
-                    (
-                        (g['promotions']['promotionalOffers'] and g['price']['totalPrice']['discountPrice'] == 0)
-                        or
-                        (game['promotions']['upcomingPromotionalOffers'])
-                    ),
-                data["data"]["Catalog"]["searchStore"]["elements"]
-            ),
-            key=lambda g: g['title']
-        ))
+        _LOGGER.debug(data)
 
+        promo_games = sorted(
+            filter(
+                lambda g: g.get("promotions")
+                and (
+                    (
+                        g["promotions"]["promotionalOffers"]
+                        and g["price"]["totalPrice"]["discountPrice"] == 0
+                    )
+                    or (g["promotions"]["upcomingPromotionalOffers"])
+                ),
+                data["data"]["Catalog"]["searchStore"]["elements"],
+            ),
+            key=lambda g: g["title"],
+        )
 
         return_data = self.data or {}
         for game in promo_games:
-            prefix = ''
-            _LOGGER.error("-"*50)
-            _LOGGER.info(game)
-            game_title = game['title']
-            game_publisher = game['seller']['name']
-            game_portrait = None
-            game_landscape = None
-            game_url = game["catalogNs"]["mappings"][0]['pageSlug']
-            game_url = f"https://store.epicgames.com/fr/p/{game_url}"
-            game_promotion_data = {}
+            game_title = game["title"]
+            game_publisher = game["seller"]["name"]
+            game_url = f"https://store.epicgames.com/fr/p/{game['catalogNs']['mappings'][0]['pageSlug']}"
+            game_img_portrait = None
+            game_img_landscape = None
 
-            for image in game['keyImages']:
-                if image['type'] == 'OfferImageTall':
-                    game_portrait = image['url']
-                if image['type'] == 'OfferImageWide':
-                    game_landscape = image['url']
+            for image in game["keyImages"]:
+                if image["type"] == "OfferImageTall":
+                    game_img_portrait = image["url"]
+                if image["type"] == "OfferImageWide":
+                    game_img_landscape = image["url"]
 
-            # _LOGGER.warning(game_portrait)
-            # _LOGGER.warning(game_url)
-            _LOGGER.error("-"*50)
-            game_price = game['price']['totalPrice']['fmtPrice']['originalPrice']
-            game_promotions = game['promotions']['promotionalOffers']
-            upcoming_promotions = game['promotions']['upcomingPromotionalOffers']
-            if not game_promotions and upcoming_promotions:
-                # Promotion is not active yet, but will be active soon.
-                game_promotion_data = upcoming_promotions[0]['promotionalOffers'][0]
-                print('{} ({}) will be free from {} to {} UTC.'.format(
-                    game_title, game_price, game_promotion_data['startDate'], game_promotion_data['endDate']
-                ))
+            game_price = game["price"]["totalPrice"]["fmtPrice"]["originalPrice"]
+            game_promotions = game["promotions"]["promotionalOffers"]
+            upcoming_promotions = game["promotions"]["upcomingPromotionalOffers"]
 
+            prefix = ""
+            promotion_data = {}
+            if game_promotions and game["price"]["totalPrice"]["discountPrice"] == 0:
+                promotion_data = game_promotions[0]["promotionalOffers"][0]
+            elif not game_promotions and upcoming_promotions:
                 prefix = "next_"
-            elif len(game_promotions) > 0:
-                game_promotion_data = game_promotions[0]['promotionalOffers'][0]
-                print('{} ({}) is FREE now.'.format(
-                    game_title, game_price
-                ))
+                promotion_data = upcoming_promotions[0]["promotionalOffers"][0]
 
-            if game_promotion_data:
-                suffix="1"
+            if promotion_data:
+                suffix = "1"
                 if return_data.get(f"{prefix}free_game_{suffix}"):
                     suffix = "2"
-                
-                return_data[f"{prefix}free_game_{suffix}"]={
-                    'title': game_title,
-                    'publisher': game_publisher,
-                    'url': game_url,
-                    'img_portrait': game_portrait,
-                    'img_landscape': game_landscape,
-                    'original_price': game_price,
-                    'start_at': dt.parse_datetime(game_promotion_data['startDate']),
-                    'end_at': dt.parse_datetime(game_promotion_data['endDate']),
-                }
-        _LOGGER.warning("-"*200)
 
+                return_data[f"{prefix}free_game_{suffix}"] = {
+                    "title": game_title,
+                    "publisher": game_publisher,
+                    "url": game_url,
+                    "img_portrait": game_img_portrait,
+                    "img_landscape": game_img_landscape,
+                    "original_price": game_price,
+                    "start_at": dt.parse_datetime(promotion_data["startDate"]),
+                    "end_at": dt.parse_datetime(promotion_data["endDate"]),
+                }
+        _LOGGER.warning("-" * 200)
 
         return return_data
