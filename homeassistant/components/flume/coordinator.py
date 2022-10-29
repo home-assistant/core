8fa7a2d86e5a4ff9e3f53c423ad88624b60b60ef
Jeef <jeeftor@users.noreply.github.com>
2022-10-29 14:21:02 -0600

    Update coordinator.py

--- homeassistant/components/flume/coordinator.py
+++ homeassistant/components/flume/coordinator.py
@@ -112,7 +112,7 @@ class FlumeNotificationDataUpdateCoordinator(DataUpdateCoordinator[None]):
             device_id = notification["device_id"]
             rule = notification["extra"]["event_rule_name"]
             time = parse_datetime(notification["created_datetime"])
-            age = - time
+            age = -1 * time
             active_notifications_by_device_with_time.setdefault(device_id, {})[
                 rule
             ] = age
