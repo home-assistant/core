325a2ed2018340ec548adbd7b0f9465217d15d9b
Aarni Koskela <akx@iki.fi>
2022-10-31 21:58:50 +0200

    feat: add support for RuuviTag

--- homeassistant/components/ruuvitag_ble/sensor.py
+++ homeassistant/components/ruuvitag_ble/sensor.py
@@ -3,7 +3,16 @@ from __future__ import annotations
 
 from typing import Optional, Union
 
-from homeassistant import config_entries
+from sensor_state_data import (
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    Units,
+)
+
+from homeassistant import config_entries, const
 from homeassistant.components.bluetooth.passive_update_processor import (
     PassiveBluetoothDataProcessor,
     PassiveBluetoothDataUpdate,
@@ -13,18 +22,60 @@ from homeassistant.components.bluetooth.passive_update_processor import (
 )
 from homeassistant.components.sensor import (
     SensorEntity,
-)
-from homeassistant.const import (
-    ATTR_MANUFACTURER,
-    ATTR_MODEL,
-    ATTR_NAME,
+    SensorEntityDescription,
+    SensorStateClass,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
 from .const import DOMAIN
 
-SENSOR_DESCRIPTIONS = {}
+SENSOR_DESCRIPTIONS = {
+    (SensorDeviceClass.TEMPERATURE, Units.TEMP_CELSIUS): SensorEntityDescription(
+        key=f"{SensorDeviceClass.TEMPERATURE}_{Units.TEMP_CELSIUS}",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement=const.TEMP_CELSIUS,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    (SensorDeviceClass.HUMIDITY, Units.PERCENTAGE): SensorEntityDescription(
+        key=f"{SensorDeviceClass.HUMIDITY}_{Units.PERCENTAGE}",
+        device_class=SensorDeviceClass.HUMIDITY,
+        native_unit_of_measurement=const.PERCENTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    (SensorDeviceClass.PRESSURE, Units.PRESSURE_HPA): SensorEntityDescription(
+        key=f"{SensorDeviceClass.PRESSURE}_{Units.PRESSURE_HPA}",
+        device_class=SensorDeviceClass.PRESSURE,
+        native_unit_of_measurement=const.PRESSURE_HPA,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    (
+        SensorDeviceClass.VOLTAGE,
+        Units.ELECTRIC_POTENTIAL_MILLIVOLT,
+    ): SensorEntityDescription(
+        key=f"{SensorDeviceClass.VOLTAGE}_{Units.ELECTRIC_POTENTIAL_MILLIVOLT}",
+        device_class=SensorDeviceClass.VOLTAGE,
+        native_unit_of_measurement=const.ELECTRIC_POTENTIAL_MILLIVOLT,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    (
+        SensorDeviceClass.SIGNAL_STRENGTH,
+        Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+    ): SensorEntityDescription(
+        key=f"{SensorDeviceClass.SIGNAL_STRENGTH}_{Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT}",
+        device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+        native_unit_of_measurement=const.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+        state_class=SensorStateClass.MEASUREMENT,
+        entity_registry_enabled_default=False,
+    ),
+    (SensorDeviceClass.COUNT, None): SensorEntityDescription(
+        key="movement_counter",
+        device_class=SensorDeviceClass.COUNT,
+        state_class=SensorStateClass.MEASUREMENT,
+        entity_registry_enabled_default=False,
+    ),
+}
 
 
 def _device_key_to_bluetooth_entity_key(
@@ -38,14 +89,21 @@ def _sensor_device_info_to_hass(
     sensor_device_info: SensorDeviceInfo,
 ) -> DeviceInfo:
     """Convert a sensor device info to a sensor device info."""
-    hass_device_info = DeviceInfo({})
+    hass_device_info = DeviceInfo()
     if sensor_device_info.name is not None:
-        hass_device_info[ATTR_NAME] = sensor_device_info.name
+        hass_device_info[const.ATTR_NAME] = sensor_device_info.name
     if sensor_device_info.manufacturer is not None:
-        hass_device_info[ATTR_MANUFACTURER] = sensor_device_info.manufacturer
+        hass_device_info[const.ATTR_MANUFACTURER] = sensor_device_info.manufacturer
     if sensor_device_info.model is not None:
-        hass_device_info[ATTR_MODEL] = sensor_device_info.model
+        hass_device_info[const.ATTR_MODEL] = sensor_device_info.model
     return hass_device_info
+
+
+def _to_sensor_key(
+    description: SensorDescription,
+) -> tuple[SensorDeviceClass, Units | None]:
+    assert description.device_class is not None
+    return (description.device_class, description.native_unit_of_measurement)
 
 
 def sensor_update_to_bluetooth_data_update(
@@ -59,10 +117,10 @@ def sensor_update_to_bluetooth_data_update(
         },
         entity_descriptions={
             _device_key_to_bluetooth_entity_key(device_key): SENSOR_DESCRIPTIONS[
-                (description.device_class, description.native_unit_of_measurement)
+                _to_sensor_key(description)
             ]
             for device_key, description in sensor_update.entity_descriptions.items()
-            if description.device_class and description.native_unit_of_measurement
+            if _to_sensor_key(description) in SENSOR_DESCRIPTIONS
         },
         entity_data={
             _device_key_to_bluetooth_entity_key(device_key): sensor_values.native_value
---
+++ tests/components/ruuvitag_ble/fixtures.py
@@ -0,0 +1,16 @@
+"""Fixtures for testing RuuviTag BLE."""
+from homeassistant.helpers.service_info.bluetooth import BluetoothServiceInfo
+
+RUUVITAG_SERVICE_INFO = BluetoothServiceInfo(
+    name="RuuviTag 0911",
+    address="01:03:05:07:09:11",  # Ignored (the payload encodes the correct MAC)
+    rssi=-60,
+    manufacturer_data={
+        1177: b"\x05\x05\xa0`\xa0\xc8\x9a\xfd4\x02\x8c\xff\x00cvriv\xde\xad{?\xef\xaf"
+    },
+    service_data={},
+    service_uuids=[],
+    source="local",
+)
+CONFIGURED_NAME = "RuuviTag EFAF"
+CONFIGURED_PREFIX = "ruuvitag_efaf"
