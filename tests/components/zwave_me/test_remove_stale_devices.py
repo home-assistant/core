f2a67f72178881c705e66df1c13c42ae4afe170a
Dmitry Vlasov <kerbalspacema@gmail.com>
2022-10-31 14:11:41 +0300

    Fix tests

--- tests/components/zwave_me/test_remove_stale_devices.py
+++ tests/components/zwave_me/test_remove_stale_devices.py
@@ -8,7 +8,7 @@ from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry, mock_device_registry
 
-DEFAULT_DEVICE_INFO = {"device_id": "DummyDevice-1", "device_identifier": "16-23"}
+DEFAULT_DEVICE_INFO = {"device_id": "DummyDevice", "device_identifier": "16-23"}
 
 
 @pytest.fixture
@@ -17,48 +17,51 @@ def device_reg(hass):
     return mock_device_registry(hass)
 
 
+async def mock_connection(controller):
+    """Mock established connection and setting identifiers."""
+    controller.device_ids = {DEFAULT_DEVICE_INFO["device_id"]}
+    return True
+
+
 @pytest.mark.parametrize(
-    "identifier,use_device",
+    "identifier,should_exist",
     [
         (DEFAULT_DEVICE_INFO["device_id"], False),
         (DEFAULT_DEVICE_INFO["device_identifier"], True),
     ],
 )
 async def test_remove_stale_devices(
-    hass: HomeAssistant, device_reg, identifier, use_device
+    hass: HomeAssistant, device_reg, identifier, should_exist
 ):
     """Test removing devices with old-format ids."""
+
     config_entry = MockConfigEntry(
         domain="zwave_me", data={CONF_TOKEN: "test_token", CONF_URL: "http://test_test"}
     )
     config_entry.add_to_hass(hass)
+    device_reg.async_get_or_create(
+        config_entry_id=config_entry.entry_id,
+        connections={("mac", "12:34:56:AB:CD:EF")},
+        identifiers={("zwave_me", f"{config_entry.unique_id}-{identifier}")},
+    )
     with patch(
         "homeassistant.components.zwave_me.ZWaveMeController.async_establish_connection",
-        return_value=True,
+        mock_connection,
     ), patch(
         "homeassistant.components.zwave_me.async_setup_platforms",
     ):
         await hass.config_entries.async_setup(config_entry.entry_id)
-        if use_device:
-            result = device_reg.async_get_or_create(
-                config_entry_id=config_entry.entry_id,
-                connections={("mac", "12:34:56:AB:CD:EF")},
-                identifiers={("zwave_me", f"{config_entry.unique_id}-{identifier}")},
-            )
-        else:
-            result = None
-    controller = hass.data["zwave_me"][config_entry.entry_id]
-    controller.device_ids = {DEFAULT_DEVICE_INFO["device_id"]}
-    controller.remove_stale_devices(device_reg)
 
     assert (
-        device_reg.async_get_device(
-            {
-                (
-                    "zwave_me",
-                    f"{config_entry.unique_id}-{identifier}",
-                )
-            }
+        bool(
+            device_reg.async_get_device(
+                {
+                    (
+                        "zwave_me",
+                        f"{config_entry.unique_id}-{identifier}",
+                    )
+                }
+            )
         )
-        is result
+        == should_exist
     )
