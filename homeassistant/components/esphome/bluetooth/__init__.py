046a1a669d76371948d44e6760f65f22b8225506
J. Nick Koston <nick@koston.org>
2022-10-30 20:44:53 -0500

    fixes

--- homeassistant/components/esphome/bluetooth/__init__.py
+++ homeassistant/components/esphome/bluetooth/__init__.py
@@ -71,13 +71,11 @@ async def async_connect_scanner(
     unload_callbacks = [
         async_register_scanner(hass, scanner, connectable),
         scanner.async_setup(),
-        await cli.subscribe_bluetooth_le_advertisements(scanner.async_on_advertisement),
     ]
+    await cli.subscribe_bluetooth_le_advertisements(scanner.async_on_advertisement)
     if connectable:
-        unload_callbacks.append(
-            await cli.subscribe_bluetooth_connections_free(
-                entry_data.async_update_ble_connection_limits
-            )
+        await cli.subscribe_bluetooth_connections_free(
+            entry_data.async_update_ble_connection_limits
         )
 
     @hass_callback
