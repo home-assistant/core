c7806bb14a4b1d1ec959a24578a53747bd0f26ad
Martin Hjelmare <marhje52@gmail.com>
2022-11-02 16:00:21 +0100

    Update deprecation period

--- homeassistant/components/pushbullet/sensor.py
+++ homeassistant/components/pushbullet/sensor.py
@@ -96,7 +96,7 @@ async def async_setup_platform(
         hass,
         DOMAIN,
         "deprecated_yaml",
-        breaks_in_ha_version="2023.1.0",
+        breaks_in_ha_version="2023.2.0",
         is_fixable=False,
         severity=IssueSeverity.WARNING,
         translation_key="deprecated_yaml",
