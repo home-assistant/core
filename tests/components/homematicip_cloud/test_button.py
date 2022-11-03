604b22ce3b7e6fe6af0b34de735a73f4652e31c9
hahn-th <post@thomas-hahn.org>
2022-11-03 10:53:34 +0000

    removed test test_manually_configured_platform

--- tests/components/homematicip_cloud/test_button.py
+++ tests/components/homematicip_cloud/test_button.py
@@ -4,20 +4,10 @@ from unittest.mock import patch
 
 from homeassistant.components.button import DOMAIN as BUTTON_DOMAIN
 from homeassistant.components.button.const import SERVICE_PRESS
-from homeassistant.components.homematicip_cloud import DOMAIN as HMIPC_DOMAIN
 from homeassistant.const import ATTR_ENTITY_ID, STATE_UNKNOWN
-from homeassistant.setup import async_setup_component
 from homeassistant.util import dt as dt_util
 
 from .helper import get_and_check_entity_basics
-
-
-async def test_manually_configured_platform(hass):
-    """Test that we do not set up an access point."""
-    assert await async_setup_component(
-        hass, BUTTON_DOMAIN, {BUTTON_DOMAIN: {"platform": HMIPC_DOMAIN}}
-    )
-    assert not hass.data.get(HMIPC_DOMAIN)
 
 
 async def test_hmip_garage_door_controller_button(hass, default_mock_hap_factory):
@@ -29,9 +19,7 @@ async def test_hmip_garage_door_controller_button(hass, default_mock_hap_factory
         test_devices=[entity_name]
     )
 
-    ha_state, hmip_device = get_and_check_entity_basics(
-        hass, mock_hap, entity_id, entity_name, device_model
-    )
+    get_and_check_entity_basics(hass, mock_hap, entity_id, entity_name, device_model)
 
     state = hass.states.get(entity_id)
     assert state
