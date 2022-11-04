d0c655454bee553077368a5744b52eb8a58c3f6d
G Johansson <goran.johansson@shiftit.se>
2022-11-04 07:06:19 +0000

    Fix test init

--- tests/components/sun/test_init.py
+++ tests/components/sun/test_init.py
@@ -2,15 +2,12 @@
 from datetime import datetime, timedelta
 from unittest.mock import patch
 
-from astral import LocationInfo
-import astral.sun
 from freezegun import freeze_time
 import pytest
 
 from homeassistant.components import sun
 from homeassistant.const import EVENT_STATE_CHANGED
-import homeassistant.core as ha
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.setup import async_setup_component
 import homeassistant.util.dt as dt_util
 
@@ -198,7 +195,7 @@ async def test_state_change_count(hass: HomeAssistant) -> None:
 
     events = []
 
-    @ha.callback
+    @callback
     def state_change_listener(event):
         if event.data.get("entity_id") == "sun.sun":
             events.append(event)
