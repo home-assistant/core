6bf9553d583c4292c93b9bd6c12acd4d44ceec6a
nyro <cedric@nyro.dev>
2022-11-04 20:44:59 +0000

    Add new domestic hot water controllable_name

--- homeassistant/components/overkiz/water_heater_entities/domestic_hot_water_production.py
+++ homeassistant/components/overkiz/water_heater_entities/domestic_hot_water_production.py
@@ -17,10 +17,28 @@ from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
 
 from ..entity import OverkizEntity
 
-DHWP_TYPE_MURAL = "io:AtlanticDomesticHotWaterProductionV2_MURAL_IOComponent"
-DHWP_TYPE_CE_FLAT_C2 = "io:AtlanticDomesticHotWaterProductionV2_CE_FLAT_C2_IOComponent"
-DHWP_TYPE_CV4E_IO = "io:AtlanticDomesticHotWaterProductionV2_CV4E_IOComponent"
-DHWP_TYPE_MBL = "modbuslink:AtlanticDomesticHotWaterProductionMBLComponent"
+# Create standardized types to be used inside the device
+DHWP_TYPE_STANDARD = "STANDARD"
+DHWP_TYPE_MURAL = "MURAL"
+DHWP_TYPE_FLAT = "FLAT"
+DHWP_TYPE_MBL = "MBL"
+
+# Map controllable_name into standardized types
+DHWP_TYPES: dict[str, str] = {
+    "io:AtlanticDomesticHotWaterProductionIOComponent": DHWP_TYPE_STANDARD,
+    "io:AtlanticDomesticHotWaterProductionV2IOComponent": DHWP_TYPE_STANDARD,
+    "io:AtlanticDomesticHotWaterProductionV2_AEX_IOComponent": DHWP_TYPE_STANDARD,
+    "io:AtlanticDomesticHotWaterProductionV2_CE_FLAT_C2_IOComponent": DHWP_TYPE_FLAT,
+    "io:AtlanticDomesticHotWaterProductionV2_CE_S4_IOComponent": DHWP_TYPE_STANDARD,
+    "io:AtlanticDomesticHotWaterProductionV2_CETHI_V4_IOComponent": DHWP_TYPE_STANDARD,
+    "io:AtlanticDomesticHotWaterProductionV2_CV4E_IOComponent": DHWP_TYPE_STANDARD,
+    "io:AtlanticDomesticHotWaterProductionV2_MURAL_IOComponent": DHWP_TYPE_MURAL,
+    "io:AtlanticDomesticHotWaterProductionV2_SPLIT_IOComponent": DHWP_TYPE_STANDARD,
+    "io:AtlanticDomesticHotWaterProductionV3IOComponent": DHWP_TYPE_STANDARD,
+    "io:DHWCumulatedElectricalEnergyConsumptionIOSystemDeviceSensor": DHWP_TYPE_STANDARD,
+    "modbuslink:AtlanticDomesticHotWaterProductionMBLComponent": DHWP_TYPE_MBL,
+    "modbuslink:DHWCumulatedElectricalEnergyConsumptionMBLSystemDeviceSensor": DHWP_TYPE_MBL,
+}
 
 OVERKIZ_TO_OPERATION_MODE: dict[str, str] = {
     OverkizCommandParam.STANDARD: STATE_ECO,
@@ -47,9 +65,20 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
     _attr_operation_list = [*OPERATION_MODE_TO_OVERKIZ]
 
     @property
+    def _dhwp_type(self) -> str | None:
+        if self.device.controllable_name in DHWP_TYPES:
+            return DHWP_TYPES[self.device.controllable_name]
+        return None
+
+    @property
     def _is_boost_mode_on(self) -> bool:
         """Return true if boost mode is on."""
-        if self.device.controllable_name == DHWP_TYPE_MURAL:
+
+        dwhp_type = self._dhwp_type
+        if dwhp_type is None:
+            return False
+
+        if dwhp_type == DHWP_TYPE_MURAL:
             operating_state = self.executor.select_state(
                 OverkizState.CORE_OPERATING_MODE
             )
@@ -64,7 +93,7 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
                 )
 
             return False
-        if self.device.controllable_name == DHWP_TYPE_CV4E_IO:
+        if dwhp_type == DHWP_TYPE_STANDARD:
             return (
                 cast(
                     float,
@@ -72,7 +101,7 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
                 )
                 > 0
             )
-        if self.device.controllable_name == DHWP_TYPE_CE_FLAT_C2:
+        if dwhp_type == DHWP_TYPE_FLAT:
             return (
                 cast(str, self.executor.select_state(OverkizState.IO_DHW_BOOST_MODE))
                 == OverkizCommandParam.ON
@@ -82,7 +111,12 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
     @property
     def is_away_mode_on(self) -> bool | None:
         """Return true if away mode is on."""
-        if self.device.controllable_name == DHWP_TYPE_MURAL:
+
+        dwhp_type = self._dhwp_type
+        if dwhp_type is None:
+            return None
+
+        if dwhp_type == DHWP_TYPE_MURAL:
             operating_mode = self.executor.select_state(
                 OverkizState.CORE_OPERATING_MODE
             )
@@ -96,7 +130,7 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
                     == OverkizCommandParam.ON
                 )
             return False
-        if self.device.controllable_name == DHWP_TYPE_CV4E_IO:
+        if dwhp_type == DHWP_TYPE_STANDARD:
             operating_mode = self.executor.select_state(
                 OverkizState.CORE_OPERATING_MODE
             )
@@ -110,12 +144,12 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
                     == OverkizCommandParam.ON
                 )
             return False
-        if self.device.controllable_name == DHWP_TYPE_CE_FLAT_C2:
+        if dwhp_type == DHWP_TYPE_FLAT:
             return (
                 self.executor.select_state(OverkizState.IO_DHW_ABSENCE_MODE)
                 == OverkizCommandParam.ON
             )
-        if self.device.controllable_name == DHWP_TYPE_MBL:
+        if dwhp_type == DHWP_TYPE_MBL:
             return (
                 self.executor.select_state(OverkizState.MODBUSLINK_DHW_ABSENCE_MODE)
                 == OverkizCommandParam.ON
@@ -203,8 +237,13 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
 
     async def async_set_operation_mode(self, operation_mode: str) -> None:
         """Set new target operation mode."""
+
+        dwhp_type = self._dhwp_type
+        if dwhp_type is None:
+            return
+
         if operation_mode == STATE_PERFORMANCE:
-            if self.device.controllable_name in [DHWP_TYPE_MURAL, DHWP_TYPE_CV4E_IO]:
+            if dwhp_type in [DHWP_TYPE_MURAL, DHWP_TYPE_STANDARD]:
                 await self.executor.async_execute_command(
                     OverkizCommand.SET_CURRENT_OPERATING_MODE,
                     {
@@ -212,20 +251,20 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
                         OverkizCommandParam.ABSENCE: OverkizCommandParam.OFF,
                     },
                 )
-            if self.device.controllable_name == DHWP_TYPE_CV4E_IO:
+            if dwhp_type == DHWP_TYPE_STANDARD:
                 await self.executor.async_execute_command(
                     OverkizCommand.SET_BOOST_MODE_DURATION, 7
                 )
                 await self.executor.async_execute_command(
                     OverkizCommand.REFRESH_BOOST_MODE_DURATION
                 )
-            if self.device.controllable_name == DHWP_TYPE_CE_FLAT_C2:
+            if dwhp_type == DHWP_TYPE_FLAT:
                 await self.executor.async_execute_command(
                     OverkizCommand.SET_BOOST_MODE, OverkizCommand.ON
                 )
             return
         if self._is_boost_mode_on:
-            if self.device.controllable_name in [DHWP_TYPE_MURAL, DHWP_TYPE_CV4E_IO]:
+            if dwhp_type in [DHWP_TYPE_MURAL, DHWP_TYPE_STANDARD]:
                 await self.executor.async_execute_command(
                     OverkizCommand.SET_CURRENT_OPERATING_MODE,
                     {
@@ -233,14 +272,14 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
                         OverkizCommandParam.ABSENCE: OverkizCommandParam.OFF,
                     },
                 )
-            if self.device.controllable_name == DHWP_TYPE_CV4E_IO:
+            if dwhp_type == DHWP_TYPE_STANDARD:
                 await self.executor.async_execute_command(
                     OverkizCommand.SET_BOOST_MODE_DURATION, 0
                 )
                 await self.executor.async_execute_command(
                     OverkizCommand.REFRESH_BOOST_MODE_DURATION
                 )
-            if self.device.controllable_name == DHWP_TYPE_CE_FLAT_C2:
+            if dwhp_type == DHWP_TYPE_FLAT:
                 await self.executor.async_execute_command(
                     OverkizCommand.SET_BOOST_MODE, OverkizCommand.OFF
                 )
