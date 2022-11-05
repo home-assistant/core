a59653b6b7e156af3e9dd416ef196bacea7eb386
J. Nick Koston <nick@koston.org>
2022-11-05 12:18:32 -0500

    one more test to be sure

--- tests/components/homekit/test_iidmanager.py
+++ tests/components/homekit/test_iidmanager.py
@@ -126,6 +126,18 @@ async def test_iid_generation_and_restore_v2(hass, iid_storage, hass_storage):
 
     iid_storage = AccessoryIIDStorage(hass, entry.entry_id)
     await iid_storage.async_initialize()
+    not_accessory_info_service_iid = iid_storage.get_or_allocate_iid(
+        1, "000000AA-0000-1000-8000-0026BB765291", None, None, None
+    )
+    assert not_accessory_info_service_iid == 2
+    not_accessory_info_service_iid_2 = iid_storage.get_or_allocate_iid(
+        1, "000000BB-0000-1000-8000-0026BB765291", None, None, None
+    )
+    assert not_accessory_info_service_iid_2 == 3
+    accessory_info_service_iid = iid_storage.get_or_allocate_iid(
+        1, "0000003E-0000-1000-8000-0026BB765291", None, None, None
+    )
+    assert accessory_info_service_iid == 1
     accessory_info_service_iid = iid_storage.get_or_allocate_iid(
         1, "0000003E-0000-1000-8000-0026BB765291", None, None, None
     )
