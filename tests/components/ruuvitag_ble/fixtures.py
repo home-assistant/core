ebd86abf59733f8570c5c4bc1a3d7d23f88aa59e
Aarni Koskela <akx@iki.fi>
2022-11-06 18:38:41 +0000

    chore: copy tilt_ble config flow test to ruuvitag_ble

--- tests/components/ruuvitag_ble/fixtures.py
+++ tests/components/ruuvitag_ble/fixtures.py
@@ -1,6 +1,16 @@
 """Fixtures for testing RuuviTag BLE."""
 from homeassistant.helpers.service_info.bluetooth import BluetoothServiceInfo
 
+NOT_RUUVITAG_SERVICE_INFO = BluetoothServiceInfo(
+    name="Not it",
+    address="61DE521B-F0BF-9F44-64D4-75BBE1738105",
+    rssi=-63,
+    manufacturer_data={3234: b"\x00\x01"},
+    service_data={},
+    service_uuids=[],
+    source="local",
+)
+
 RUUVITAG_SERVICE_INFO = BluetoothServiceInfo(
     name="RuuviTag 0911",
     address="01:03:05:07:09:11",  # Ignored (the payload encodes the correct MAC)
