6bbee2745f57d4b970ea126b27848a7746b73127
Erik <erik@montnemery.com>
2022-11-07 19:46:29 +0100

    Use black for formatting

--- script/countries.py
+++ script/countries.py
@@ -3,13 +3,9 @@
 ISO does not publish a machine readable list free of charge, so the list is generated
 with help of the pycountry package.
 """
+import black
 import pycountry
 
 COUNTRIES = sorted({x.alpha_2 for x in pycountry.countries})
 
-print(
-    '    {\n        "',
-    '", \n        "'.join(COUNTRIES),
-    '",\n    }',
-    sep="",
-)
+print(black.format_str("{" + ",".join(COUNTRIES) + "}", mode=black.Mode()))
