33e46b0496affd84409111056b25165d81e3f0bc
Tim Savage <tim@savage.company>
2022-11-08 01:07:33 +1100

    Split sensors into different types

--- homeassistant/components/combined_energy/sensor.py
+++ homeassistant/components/combined_energy/sensor.py
@@ -1,14 +1,18 @@
-"""Support for getting collected information from Combined Energy API."""
+"""Support for getting collected information from Combined Energy."""
 from __future__ import annotations
 
-from collections.abc import Generator
+from collections.abc import Generator, Sequence
 from typing import Any
 
 from combined_energy import CombinedEnergy
 from combined_energy.models import Device, DeviceReadings, Installation
 
 from homeassistant.components.binary_sensor import BinarySensorEntity
-from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
@@ -19,8 +23,8 @@ from .const import (
     DATA_API_CLIENT,
     DATA_INSTALLATION,
     DOMAIN,
-    SENSOR_TYPE_CONNECTED,
-    SENSOR_TYPES,
+    SENSOR_DESCRIPTION_CONNECTED,
+    SENSOR_DESCRIPTIONS,
 )
 from .coordinator import (
     CombinedEnergyConnectivityDataService,
@@ -57,7 +61,7 @@ async def async_setup_entry(
 
 
 class CombinedEnergyConnectedSensor(CoordinatorEntity, BinarySensorEntity):
-    """Representation of a Combined Energy API connection status sensor."""
+    """Representation of a Combined Energy connection status sensor."""
 
     data_service: CombinedEnergyConnectivityDataService
 
@@ -68,7 +72,7 @@ class CombinedEnergyConnectedSensor(CoordinatorEntity, BinarySensorEntity):
         super().__init__(data_service.coordinator)
 
         self.data_service = data_service
-        self.entity_description = SENSOR_TYPE_CONNECTED
+        self.entity_description = SENSOR_DESCRIPTION_CONNECTED
 
         self._attr_name = f"{platform_name} {self.entity_description.name}"
         self._attr_unique_id = f"install_{self.data_service.api.installation_id}-{self.entity_description.key}"
@@ -85,6 +89,7 @@ class CombinedEnergyReadingsSensor(CoordinatorEntity, SensorEntity):
     """Representation of a Combined Energy API reading energy sensor."""
 
     data_service: CombinedEnergyReadingsDataService
+    entity_description: SensorEntityDescription
 
     def __init__(
         self,
@@ -116,6 +121,13 @@ class CombinedEnergyReadingsSensor(CoordinatorEntity, SensorEntity):
         return None
 
     @property
+    def _raw_value(self):
+        """Get raw reading value from device."""
+        if device_readings := self.device_readings:
+            return getattr(device_readings, self.entity_description.key)
+        return None
+
+    @property
     def extra_state_attributes(self) -> dict[str, Any] | None:
         """Return the state attributes."""
         return {}
@@ -123,12 +135,53 @@ class CombinedEnergyReadingsSensor(CoordinatorEntity, SensorEntity):
     @property
     def native_value(self) -> float | None:
         """Return the state of the sensor."""
+        value = self._raw_value
+        if isinstance(value, Sequence):
+            value = value[-1]
+        return round(value, 2) if value is not None else None
 
-        if device_readings := self.device_readings:
-            if readings := getattr(device_readings, self.entity_description.key):
-                return readings[-1]
 
+class EnergySensor(CombinedEnergyReadingsSensor):
+    """Sensor for energy readings."""
+
+    @property
+    def native_value(self) -> float | None:
+        """Return the state of the sensor for a power value."""
+        if (value := self._raw_value) is not None:
+            return round(sum(value), 2)
         return None
+
+
+class PowerSensor(CombinedEnergyReadingsSensor):
+    """Sensor for power readings."""
+
+    @property
+    def native_value(self) -> float | None:
+        """Return the state of the sensor for a power value."""
+        if (value := self._raw_value) is not None:
+            return round(value, 2)
+        return None
+
+
+class WaterVolumeSensor(CombinedEnergyReadingsSensor):
+    """Sensor for water volume readings."""
+
+    @property
+    def native_value(self) -> float | None:
+        """Return the state of the sensor for a power value."""
+        if (value := self._raw_value) is not None:
+            return int(round(value[-1], 0))
+        return None
+
+
+SENSOR_TYPE_MAP: dict[
+    SensorDeviceClass | str | None, type[CombinedEnergyReadingsSensor]
+] = {
+    SensorDeviceClass.ENERGY: EnergySensor,
+    SensorDeviceClass.POWER: PowerSensor,
+    SensorDeviceClass.WATER: WaterVolumeSensor,
+    None: CombinedEnergyReadingsSensor,
+}
 
 
 class CombinedEnergyReadingsSensorFactory:
@@ -153,7 +206,7 @@ class CombinedEnergyReadingsSensorFactory:
         """Generate entities."""
 
         for device in self.installation.devices:
-            if descriptions := SENSOR_TYPES.get(device.device_type):
+            if descriptions := SENSOR_DESCRIPTIONS.get(device.device_type):
                 device_info = DeviceInfo(
                     identifiers={
                         (
@@ -167,9 +220,12 @@ class CombinedEnergyReadingsSensorFactory:
                 )
 
                 for description in descriptions:
-                    yield CombinedEnergyReadingsSensor(
-                        device,
-                        device_info,
-                        description,
-                        self.readings,
-                    )
+                    if sensor_type := SENSOR_TYPE_MAP.get(
+                        description.device_class, CombinedEnergyReadingsSensor
+                    ):
+                        yield sensor_type(
+                            device,
+                            device_info,
+                            description,
+                            self.readings,
+                        )
