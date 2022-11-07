db6aaa73de199eff2c43ff47559a4cda58131a06
Mick Vleeshouwer <mick@imick.nl>
2022-11-07 19:30:44 +0000

    Sort by protocol

--- homeassistant/components/overkiz/climate_entities/hitachi_air_to_air_heat_pump.py
+++ homeassistant/components/overkiz/climate_entities/hitachi_air_to_air_heat_pump.py
@@ -25,8 +25,16 @@ from ..entity import OverkizEntity
 
 PRESET_HOLIDAY_MODE = "holiday_mode"
 
-FAN_SPEED_STATE = [OverkizState.OVP_FAN_SPEED, OverkizState.HLRRWIFI_FAN_SPEED]
-LEAVE_HOME_STATE = [OverkizState.OVP_LEAVE_HOME, OverkizState.HLRRWIFI_LEAVE_HOME]
+FAN_SPEED_STATE = {
+    Protocol.OVP: OverkizState.OVP_FAN_SPEED,
+    Protocol.HLRR_WIFI: OverkizState.HLRRWIFI_FAN_SPEED,
+}
+
+LEAVE_HOME_STATE = {
+    Protocol.OVP: OverkizState.OVP_LEAVE_HOME,
+    Protocol.HLRR_WIFI: OverkizState.HLRRWIFI_LEAVE_HOME,
+}
+
 MAIN_OPERATION_STATE = [
     OverkizState.OVP_MAIN_OPERATION,
     OverkizState.HLRRWIFI_MAIN_OPERATION,
@@ -60,25 +68,42 @@ OVERKIZ_TO_SWING_MODES: dict[str, str] = {
 
 SWING_MODES_TO_OVERKIZ = {v: k for k, v in OVERKIZ_TO_SWING_MODES.items()}
 
-
-HLRRWIFI_OVERKIZ_TO_FAN_MODES: dict[str, str] = {
-    OverkizCommandParam.AUTO: FAN_AUTO,
-    OverkizCommandParam.HIGH: FAN_HIGH,
-    OverkizCommandParam.LOW: FAN_LOW,
-    OverkizCommandParam.MEDIUM: FAN_MEDIUM,
-    OverkizCommandParam.SILENT: "silent",
+OVERKIZ_TO_FAN_MODES: dict[Protocol, dict[str, str]] = {
+    Protocol.OVP: {
+        OverkizCommandParam.AUTO: FAN_AUTO,
+        OverkizCommandParam.HI: FAN_HIGH,
+        OverkizCommandParam.LO: FAN_LOW,
+        OverkizCommandParam.MED: FAN_MEDIUM,
+        OverkizCommandParam.SILENT: "silent",
+    },
+    Protocol.HLRR_WIFI: {
+        OverkizCommandParam.AUTO: FAN_AUTO,
+        OverkizCommandParam.HIGH: FAN_HIGH,
+        OverkizCommandParam.LOW: FAN_LOW,
+        OverkizCommandParam.MEDIUM: FAN_MEDIUM,
+        OverkizCommandParam.SILENT: "silent",
+    },
 }
 
-OVP_OVERKIZ_TO_FAN_MODES: dict[str, str] = {
-    OverkizCommandParam.AUTO: FAN_AUTO,
-    OverkizCommandParam.HI: FAN_HIGH,
-    OverkizCommandParam.LO: FAN_LOW,
-    OverkizCommandParam.MED: FAN_MEDIUM,
-    OverkizCommandParam.SILENT: "silent",
-}
+# TODO figure out if we can reverse the dictionary above
+# FAN_MODES_TO_OVERKIZ = {v: k for k, v in OVERKIZ_TO_FAN_MODES.items()}
 
-FAN_MODES_TO_HLRRWIFI_OVERKIZ = {v: k for k, v in HLRRWIFI_OVERKIZ_TO_FAN_MODES.items()}
-FAN_MODES_TO_OVP_OVERKIZ = {v: k for k, v in OVP_OVERKIZ_TO_FAN_MODES.items()}
+FAN_MODES_TO_OVERKIZ: dict[Protocol, dict[str, str]] = {
+    Protocol.OVP: {
+        FAN_AUTO: OverkizCommandParam.AUTO,
+        FAN_HIGH: OverkizCommandParam.HI,
+        FAN_LOW: OverkizCommandParam.LO,
+        FAN_MEDIUM: OverkizCommandParam.MED,
+        "silent": OverkizCommandParam.SILENT,
+    },
+    Protocol.HLRR_WIFI: {
+        FAN_AUTO: OverkizCommandParam.AUTO,
+        FAN_HIGH: OverkizCommandParam.HIGH,
+        FAN_LOW: OverkizCommandParam.LOW,
+        FAN_MEDIUM: OverkizCommandParam.MEDIUM,
+        "silent": OverkizCommandParam.SILENT,
+    },
+}
 
 
 class HitachiAirToAirHeatPump(OverkizEntity, ClimateEntity):
@@ -129,25 +154,25 @@ class HitachiAirToAirHeatPump(OverkizEntity, ClimateEntity):
     @property
     def fan_mode(self) -> str | None:
         """Return the fan setting."""
-        if self.device.protocol == Protocol.OVP:
-            return OVP_OVERKIZ_TO_FAN_MODES[self._select_state(*FAN_SPEED_STATE)]
 
-        return HLRRWIFI_OVERKIZ_TO_FAN_MODES[self._select_state(*FAN_SPEED_STATE)]
+        states = self.device.states
+        if (
+            state := states[FAN_SPEED_STATE[self.device.protocol]]
+        ) and state.value_as_str:
+            return OVERKIZ_TO_FAN_MODES[self.device.protocol][state.value_as_str]
+
+        return None
 
     @property
     def fan_modes(self) -> list[str] | None:
         """Return the list of available fan modes."""
-        if self.device.protocol == Protocol.OVP:
-            return [*FAN_MODES_TO_OVP_OVERKIZ]
-
-        return [*FAN_MODES_TO_HLRRWIFI_OVERKIZ]
+        return [*FAN_MODES_TO_OVERKIZ[self.device.protocol]]
 
     async def async_set_fan_mode(self, fan_mode: str) -> None:
         """Set new target fan mode."""
-        if self.device.protocol == Protocol.OVP:
-            await self._global_control(fan_mode=FAN_MODES_TO_OVP_OVERKIZ[fan_mode])
-        else:
-            await self._global_control(fan_mode=FAN_MODES_TO_HLRRWIFI_OVERKIZ[fan_mode])
+        await self._global_control(
+            fan_mode=FAN_MODES_TO_OVERKIZ[self.device.protocol][fan_mode]
+        )
 
     @property
     def swing_mode(self) -> str | None:
