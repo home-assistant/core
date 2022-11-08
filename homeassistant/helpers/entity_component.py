d7173917b53e6b1c9e3cbb3fe47b7db30d4f7df6
Aarni Koskela <akx@iki.fi>
2022-11-08 21:13:32 +0200

    core: Use partition instead of split where possible
    partition/rpartition is faster and allocates less memory than split.
    
    Split out of #81493

--- homeassistant/helpers/entity_component.py
+++ homeassistant/helpers/entity_component.py
@@ -36,7 +36,7 @@ _EntityT = TypeVar("_EntityT", bound=entity.Entity)
 @bind_hass
 async def async_update_entity(hass: HomeAssistant, entity_id: str) -> None:
     """Trigger an update for an entity."""
-    domain = entity_id.split(".", 1)[0]
+    domain = entity_id.partition(".")[0]
     entity_comp: EntityComponent[entity.Entity] | None
     entity_comp = hass.data.get(DATA_INSTANCES, {}).get(domain)
 
