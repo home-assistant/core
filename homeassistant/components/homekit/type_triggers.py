ad05764acf6b3ccf02df9f4c1755a291594a8755
J. Nick Koston <nick@koston.org>
2022-11-08 11:41:18 -0600

    fixes

--- homeassistant/components/homekit/type_triggers.py
+++ homeassistant/components/homekit/type_triggers.py
@@ -46,13 +46,6 @@ class DeviceTriggerAccessory(HomeAccessory):
         for idx, trigger in enumerate(device_triggers):
             type_: str = trigger["type"]
             subtype: str | None = trigger.get("subtype")
-            #
-            # Triggers don't have a unique ID, so use the data in
-            # the dict to build one to ensure we assign the same iid between
-            # restarts.
-            #
-            # We are subject to the same caveats as AID allocation
-            #
             unique_id = f'{type_}-{subtype or ""}'
             if (entity_id := trigger.get("entity_id")) and (
                 entry := ent_reg.async_get(entity_id)
