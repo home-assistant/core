b3d2e45ef6f0332d64bd8c3c97ce08ddff9161db
Tyler Carberry <tyler.carberry@gmail.com>
2022-11-11 23:27:47 +0000

    Pass auth token to linksys_smart

--- homeassistant/components/linksys_smart/device_tracker.py
+++ homeassistant/components/linksys_smart/device_tracker.py
@@ -1,6 +1,7 @@
 """Support for Linksys Smart Wifi routers."""
 from __future__ import annotations
 
+import base64
 from http import HTTPStatus
 import logging
 
@@ -12,16 +13,23 @@ from homeassistant.components.device_tracker import (
     PLATFORM_SCHEMA as PARENT_PLATFORM_SCHEMA,
     DeviceScanner,
 )
-from homeassistant.const import CONF_HOST
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.typing import ConfigType
 
 DEFAULT_TIMEOUT = 10
+DEFAULT_USERNAME = "admin"
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORM_SCHEMA = PARENT_PLATFORM_SCHEMA.extend({vol.Required(CONF_HOST): cv.string})
+PLATFORM_SCHEMA = PARENT_PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_HOST): cv.string,
+        vol.Optional(CONF_USERNAME, default=DEFAULT_USERNAME): cv.string,
+        vol.Optional(CONF_PASSWORD): cv.string,
+    }
+)
 
 
 def get_scanner(
@@ -40,6 +48,8 @@ class LinksysSmartWifiDeviceScanner(DeviceScanner):
     def __init__(self, config):
         """Initialize the scanner."""
         self.host = config[CONF_HOST]
+        self.username = config[CONF_USERNAME]
+        self.password = config.get(CONF_PASSWORD)
         self.last_results = {}
 
         # Check if the access point is accessible
@@ -96,17 +106,34 @@ class LinksysSmartWifiDeviceScanner(DeviceScanner):
         return True
 
     def _make_request(self):
-        # Weirdly enough, this doesn't seem to require authentication
+        # Older firmware versions do not require authentication
         data = [
             {
                 "request": {"sinceRevision": 0},
                 "action": "http://linksys.com/jnap/devicelist/GetDevices",
             }
         ]
-        headers = {"X-JNAP-Action": "http://linksys.com/jnap/core/Transaction"}
+
+        auth_token = self._get_auth_token(self.username, self.password)
+        headers = {
+            "X-JNAP-Action": "http://linksys.com/jnap/core/Transaction",
+        }
+        if auth_token is not None:
+            headers["X-JNAP-Authorization"] = auth_token
+
         return requests.post(
             f"http://{self.host}/JNAP/",
             timeout=DEFAULT_TIMEOUT,
             headers=headers,
             json=data,
         )
+
+    def _get_auth_token(self, username, password):
+        # An X-JNAP-Authorization HTTP header with basic access authentication
+
+        if password is None:
+            return None
+
+        login = username + ":" + password
+        token = base64.b64encode(login.encode("ascii")).decode("ascii")
+        return "Basic " + token
