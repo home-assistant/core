b1b6022f010d416e37f6f1c416ece2ec3e09e8fd
G Johansson <goran.johansson@shiftit.se>
2022-11-13 19:50:21 +0000

    Code quality Local IP

--- homeassistant/components/local_ip/sensor.py
+++ homeassistant/components/local_ip/sensor.py
@@ -5,6 +5,7 @@ from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN, SENSOR
@@ -16,7 +17,7 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the platform from config_entry."""
-    name = entry.data.get(CONF_NAME) or DOMAIN
+    name = entry.data.get(CONF_NAME) or "Local IP"
     async_add_entities([IPSensor(name)], True)
 
 
@@ -25,10 +26,14 @@ class IPSensor(SensorEntity):
 
     _attr_unique_id = SENSOR
     _attr_icon = "mdi:ip"
+    _attr_has_entity_name = True
 
     def __init__(self, name: str) -> None:
         """Initialize the sensor."""
-        self._attr_name = name
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, DOMAIN)},
+            name=name,
+        )
 
     async def async_update(self) -> None:
         """Fetch new state data for the sensor."""
