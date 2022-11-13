59e4e5e8739e7b3d0d63afd075a12c8f4c6fe0c1
G Johansson <goran.johansson@shiftit.se>
2022-11-13 15:26:09 +0000

    Add test

--- tests/components/wake_on_lan/test_init.py
+++ tests/components/wake_on_lan/test_init.py
@@ -1,14 +1,26 @@
 """Tests for Wake On LAN component."""
+from __future__ import annotations
+
+import subprocess
 from unittest.mock import patch
 
 import pytest
 import voluptuous as vol
 
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.components.wake_on_lan import DOMAIN, SERVICE_SEND_MAGIC_PACKET
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+    STATE_OFF,
+    STATE_ON,
+)
+from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
 
-async def test_send_magic_packet(hass):
+async def test_send_magic_packet(hass: HomeAssistant) -> None:
     """Test of send magic packet service call."""
     with patch("homeassistant.components.wake_on_lan.wakeonlan") as mocked_wakeonlan:
         mac = "aa:bb:cc:dd:ee:ff"
@@ -65,3 +77,53 @@ async def test_send_magic_packet(hass):
         assert len(mocked_wakeonlan.mock_calls) == 4
         assert mocked_wakeonlan.mock_calls[-1][1][0] == mac
         assert not mocked_wakeonlan.mock_calls[-1][2]
+
+
+async def test_setup_from_integration_yaml(hass: HomeAssistant) -> None:
+    """Test setup from integration yaml."""
+    assert await async_setup_component(
+        hass,
+        DOMAIN,
+        {
+            DOMAIN: [
+                {
+                    "mac": "00-01-02-03-04-05",
+                    "host": "validhostname",
+                },
+                {
+                    "mac": "06-07-08-09-0A-0B",
+                    "host": "validhostname2",
+                    "name": "Friendly Name",
+                },
+            ],
+        },
+    )
+    await hass.async_block_till_done()
+
+    state = hass.states.get("switch.wake_on_lan")
+    state2 = hass.states.get("switch.friendly_name")
+
+    assert state.state == STATE_OFF
+    assert state2.state == STATE_OFF
+
+    with patch.object(subprocess, "call", return_value=0):
+
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            SERVICE_TURN_ON,
+            {ATTR_ENTITY_ID: "switch.wake_on_lan"},
+            blocking=True,
+        )
+
+        state = hass.states.get("switch.wake_on_lan")
+        assert state.state == STATE_ON
+
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            SERVICE_TURN_OFF,
+            {ATTR_ENTITY_ID: "switch.wake_on_lan"},
+            blocking=True,
+        )
+
+        state = hass.states.get("switch.wake_on_lan")
+        assert state.state == STATE_ON
