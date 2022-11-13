18b1d55ef7d1c42b3279e7ae5d61629fff914604
J. Nick Koston <nick@koston.org>
2022-11-13 13:30:50 -0600

    Add support for thermobeacon WS 03 aka device 0x18 (#82045)
    * Add support for thermobeacon WS 03 aka device 0x18
    
    changelog: https://github.com/Bluetooth-Devices/thermobeacon-ble/compare/v0.3.2...v0.4.0
    
    * empty

--- homeassistant/generated/bluetooth.py
+++ homeassistant/generated/bluetooth.py
@@ -352,6 +352,15 @@ BLUETOOTH: list[dict[str, bool | str | int | list[int]]] = [
     {
         "connectable": False,
         "domain": "thermobeacon",
+        "manufacturer_data_start": [
+            0,
+        ],
+        "manufacturer_id": 24,
+        "service_uuid": "0000fff0-0000-1000-8000-00805f9b34fb",
+    },
+    {
+        "connectable": False,
+        "domain": "thermobeacon",
         "local_name": "ThermoBeacon",
     },
     {
