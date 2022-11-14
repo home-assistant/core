8526155218f571bbe7d6a014ec4362ab97b6b0f0
Aarni Koskela <akx@iki.fi>
2022-11-14 16:43:15 +0200

    Update homeassistant/helpers/config_validation.py
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>

--- homeassistant/helpers/config_validation.py
+++ homeassistant/helpers/config_validation.py
@@ -914,7 +914,7 @@ def key_value_schemas(
             with contextlib.suppress(vol.Invalid):
                 return cast(dict[Hashable, Any], default_schema(value))
 
-        alternatives = ", ".join(str(alt) for alt in value_schemas)
+        alternatives = ", ".join(str(alternative) for alternative in value_schemas)
         if default_description:
             alternatives = f"{alternatives}, {default_description}"
         raise vol.Invalid(
