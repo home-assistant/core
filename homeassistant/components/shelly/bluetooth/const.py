fd3b3c944b3f7164372450c48d09ac8c0d38fff3
J. Nick Koston <nick@koston.org>
2022-11-14 08:11:31 -0600

    refactoring to prepare to move to lib

--- homeassistant/components/shelly/bluetooth/const.py
+++ homeassistant/components/shelly/bluetooth/const.py
@@ -6,13 +6,19 @@ BLE_SCRIPT_NAME = "homeassistant_ble_integration"
 
 BLE_SCAN_RESULT_VERSION = 1
 
-VAR_HA_VERSION = "%ha_version%"
 VAR_EVENT_TYPE = "%event_type%"
 VAR_ACTIVE = "%active%"
 VAR_VERSION = "%version%"
+VAR_INTERVAL_MS = "%interval_ms%"
+VAR_WINDOW_MS = "%window_ms%"
+VAR_DURATION_MS = "%duration_ms%"
+
+DEFAULT_INTERVAL_MS = 320
+DEFAULT_WINDOW_MS = 30
+DEFAULT_DURATION_MS = -1
 
 BLE_CODE = """
-// Home Assistant %ha_version% BLE script
+// aioshelly BLE script 1.0
 BLE.Scanner.Subscribe(function (ev, res) {
     if (ev === BLE.Scanner.SCAN_RESULT) {
         Shelly.emitEvent("%event_type%", [
@@ -25,9 +31,9 @@ BLE.Scanner.Subscribe(function (ev, res) {
     }
 });
 BLE.Scanner.Start({
-    duration_ms: -1,
+    duration_ms: %duration_ms%,
     active: %active%,
-    interval_ms: 320,
-    window_ms: 30,
+    interval_ms: %interval_ms%,
+    window_ms: %window_ms%,
 });
 """
