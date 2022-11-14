611098fd3931431886d2b05e06e6dc8c313dcfb9
epenet <6771947+epenet@users.noreply.github.com>
2022-11-14 08:47:57 +0000

    Adjust docstring

--- script/gen_requirements_all.py
+++ script/gen_requirements_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Generate an updated requirements_all.txt."""
+"""Generate updated constraint and requirements files."""
 from __future__ import annotations
 
 import difflib
