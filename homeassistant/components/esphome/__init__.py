a17f9f974f9f3b239f7b6a79a3c6d85be60d74b1
epenet <6771947+epenet@users.noreply.github.com>
2022-11-15 21:36:23 +0000

    Import tag from homeassistant.components

--- homeassistant/components/esphome/__init__.py
+++ homeassistant/components/esphome/__init__.py
@@ -5,7 +5,7 @@ from collections.abc import Callable
 import functools
 import logging
 import math
-from typing import TYPE_CHECKING, Any, Generic, NamedTuple, TypeVar, cast, overload
+from typing import Any, Generic, NamedTuple, TypeVar, cast, overload
 
 from aioesphomeapi import (
     APIClient,
@@ -27,7 +27,7 @@ from aioesphomeapi import (
 import voluptuous as vol
 
 from homeassistant import const
-from homeassistant.components import zeroconf
+from homeassistant.components import tag, zeroconf
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     ATTR_DEVICE_ID,
@@ -54,9 +54,6 @@ from .domain_data import DOMAIN, DomainData
 
 # Import config flow so that it's added to the registry
 from .entry_data import RuntimeEntryData
-
-if TYPE_CHECKING:
-    from homeassistant.components.tag import TagProtocol
 
 CONF_NOISE_PSK = "noise_psk"
 _LOGGER = logging.getLogger(__name__)
@@ -134,11 +131,8 @@ async def async_setup_entry(  # noqa: C901
 
             # Call native tag scan
             if service_name == "tag_scanned" and device_id is not None:
-                # Importing tag via hass.components in case it is overridden
-                # in a custom_components (custom_components.tag)
-                tag: TagProtocol = hass.components.tag
                 tag_id = service_data["tag_id"]
-                hass.async_create_task(tag.async_scan_tag(tag_id, device_id))
+                hass.async_create_task(tag.async_scan_tag(hass, tag_id, device_id))
                 return
 
             hass.bus.async_fire(
