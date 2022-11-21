2809d04d21163502fa50d4dfd872e663c817985f
J. Nick Koston <nick@koston.org>
2022-11-21 13:05:04 -0600

    test updates

--- tests/components/bluetooth/test_manager.py
+++ tests/components/bluetooth/test_manager.py
@@ -26,7 +26,9 @@ from . import (
 @pytest.fixture
 def register_hci0_scanner(hass: HomeAssistant) -> None:
     """Register an hci0 scanner."""
-    cancel = bluetooth.async_register_scanner(hass, BaseHaScanner(hass, "hci0"), True)
+    cancel = bluetooth.async_register_scanner(
+        hass, BaseHaScanner(hass, "00:00:00:00:00::01", "hci0"), True
+    )
     yield
     cancel()
 
@@ -34,7 +36,9 @@ def register_hci0_scanner(hass: HomeAssistant) -> None:
 @pytest.fixture
 def register_hci1_scanner(hass: HomeAssistant) -> None:
     """Register an hci1 scanner."""
-    cancel = bluetooth.async_register_scanner(hass, BaseHaScanner(hass, "hci1"), True)
+    cancel = bluetooth.async_register_scanner(
+        hass, BaseHaScanner(hass, "00:00:00:00:00::02", "hci1"), True
+    )
     yield
     cancel()
 
@@ -416,7 +420,7 @@ async def test_switching_adapters_when_one_goes_away(
 ):
     """Test switching adapters when one goes away."""
     cancel_hci2 = bluetooth.async_register_scanner(
-        hass, BaseHaScanner(hass, "hci2"), True
+        hass, BaseHaScanner(hass, "00:00:00:00:00:03", "hci2"), True
     )
 
     address = "44:44:33:11:23:45"
