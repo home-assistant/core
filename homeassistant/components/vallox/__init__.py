09b85395d7464f61b74e94c88cb9ed0c86a56a7f
Jevgeni Kiski <yozik04@gmail.com>
2022-11-22 10:27:05 +0200

    Removing fan related code

--- homeassistant/components/vallox/__init__.py
+++ homeassistant/components/vallox/__init__.py
@@ -20,10 +20,9 @@ import voluptuous as vol
 
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_NAME, Platform
-from homeassistant.core import HomeAssistant, ServiceCall, callback
+from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 from homeassistant.helpers.typing import ConfigType, StateType
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
@@ -207,7 +206,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await coordinator.async_config_entry_first_refresh()
 
-    service_handler = ValloxServiceHandler(client, coordinator, hass)
+    service_handler = ValloxServiceHandler(client, coordinator)
     for vallox_service, service_details in SERVICE_TO_METHOD.items():
         hass.services.async_register(
             DOMAIN,
@@ -245,15 +244,11 @@ class ValloxServiceHandler:
     """Services implementation."""
 
     def __init__(
-        self,
-        client: Vallox,
-        coordinator: DataUpdateCoordinator[ValloxState],
-        hass: HomeAssistant,
+        self, client: Vallox, coordinator: DataUpdateCoordinator[ValloxState]
     ) -> None:
         """Initialize the proxy."""
         self._client = client
         self._coordinator = coordinator
-        self._hass = hass
 
     async def async_set_profile_fan_speed_home(
         self, fan_speed: int = DEFAULT_FAN_SPEED_HOME
@@ -315,12 +310,6 @@ class ValloxServiceHandler:
             _LOGGER.error("Service not implemented: %s", service_details.method)
             return
 
-        _async_log_deprecated_service_call(
-            self._hass,
-            call,
-            "2023.01.0",
-        )
-
         result = await getattr(self, service_details.method)(**params)
 
         # This state change affects other entities like sensors. Force an immediate update that can
@@ -346,35 +335,3 @@ class ValloxEntity(CoordinatorEntity[ValloxDataUpdateCoordinator]):
             sw_version=self.coordinator.data.sw_version,
             configuration_url=f"http://{self.coordinator.config_entry.data[CONF_HOST]}",
         )
-
-
-@callback
-def _async_log_deprecated_service_call(
-    hass: HomeAssistant,
-    call: ServiceCall,
-    breaks_in_ha_version: str,
-) -> None:
-    """Log a warning about a deprecated service call."""
-    deprecated_service = f"{call.domain}.{call.service}"
-
-    async_create_issue(
-        hass,
-        DOMAIN,
-        f"deprecated_service_{deprecated_service}",
-        breaks_in_ha_version=breaks_in_ha_version,
-        is_fixable=True,
-        is_persistent=True,
-        severity=IssueSeverity.WARNING,
-        translation_key="deprecated_service",
-        translation_placeholders={
-            "deprecated_service": deprecated_service,
-        },
-    )
-
-    _LOGGER.warning(
-        (
-            'The "%s" service is deprecated and will be removed in %s; use corresponding entity instead'
-        ),
-        deprecated_service,
-        breaks_in_ha_version,
-    )
--- tests/components/vallox/test_number.py
+++ tests/components/vallox/test_number.py
@@ -27,6 +27,11 @@ from tests.common import MockConfigEntry
             "A_CYC_AWAY_AIR_TEMP_TARGET",
             18.0,
         ),
+        (
+            "number.vallox_supply_air_target_boost",
+            "A_CYC_BOOST_AIR_TEMP_TARGET",
+            17.0,
+        ),
     ],
 )
 async def test_temperature_number_entities(
@@ -49,54 +54,3 @@ async def test_temperature_number_entities(
     sensor = hass.states.get(entity_id)
     assert sensor.state == str(value)
     assert sensor.attributes["unit_of_measurement"] == "°C"
-
-
-@pytest.mark.parametrize(
-    "entity_id, metric_key, value",
-    [
-        ("number.vallox_fan_speed_home", "A_CYC_HOME_SPEED_SETTING", 60),
-        ("number.vallox_fan_speed_away", "A_CYC_AWAY_SPEED_SETTING", 30),
-        ("number.vallox_fan_speed_boost", "A_CYC_BOOST_SPEED_SETTING", 100),
-    ],
-)
-async def test_fan_speed_number_entities(
-    entity_id: str,
-    metric_key: str,
-    value: int,
-    mock_entry: MockConfigEntry,
-    hass: HomeAssistant,
-) -> None:
-    """Test fan speed entities."""
-    # Arrange
-    metrics = {metric_key: value}
-
-    # Act
-    with patch_metrics(metrics=metrics):
-        await hass.config_entries.async_setup(mock_entry.entry_id)
-        await hass.async_block_till_done()
-
-    # Assert
-    sensor = hass.states.get(entity_id)
-    assert sensor.state == str(float(value))
-    assert sensor.attributes["unit_of_measurement"] == "%"
-
-
-async def test_fan_speed_number_entitity_set(
-    mock_entry: MockConfigEntry,
-    hass: HomeAssistant,
-) -> None:
-    """Test fan speed set."""
-    # Act
-    with patch_metrics(metrics={}), patch_metrics_set() as metrics_set:
-        await hass.config_entries.async_setup(mock_entry.entry_id)
-        await hass.async_block_till_done()
-        await hass.services.async_call(
-            NUMBER_DOMAIN,
-            SERVICE_SET_VALUE,
-            service_data={
-                ATTR_ENTITY_ID: "number.vallox_fan_speed_home",
-                ATTR_VALUE: 10,
-            },
-        )
-        await hass.async_block_till_done()
-        metrics_set.assert_called_once_with({"A_CYC_HOME_SPEED_SETTING": 10.0})
