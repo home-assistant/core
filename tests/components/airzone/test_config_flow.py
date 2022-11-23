f3e80e85170ae3fa047774ffdfd73a0659dc67be
Álvaro Fernández Rojas <noltari@gmail.com>
2022-11-23 19:44:09 +0100

    tests: airzone: improve test_dhcp_connection_error
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- tests/components/airzone/test_config_flow.py
+++ tests/components/airzone/test_config_flow.py
@@ -2,7 +2,7 @@
 
 from unittest.mock import patch
 
-from aioairzone.const import API_SYSTEMS
+from aioairzone.const import API_MAC, API_SYSTEMS
 from aioairzone.exceptions import (
     AirzoneError,
     InvalidMethod,
@@ -244,11 +244,44 @@ async def test_dhcp_connection_error(hass: HomeAssistant):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                CONF_PORT: TEST_PORT,
+                CONF_PORT: 3001,
             },
         )
 
         assert result["errors"] == {"base": "cannot_connect"}
+
+    with patch(
+        "homeassistant.components.airzone.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry, patch(
+        "homeassistant.components.airzone.AirzoneLocalApi.get_hvac",
+        return_value=HVAC_MOCK,
+    ), patch(
+        "homeassistant.components.airzone.AirzoneLocalApi.get_hvac_systems",
+        side_effect=SystemOutOfRange,
+    ), patch(
+        "homeassistant.components.airzone.AirzoneLocalApi.get_webserver",
+        return_value=HVAC_WEBSERVER_MOCK,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_PORT: TEST_PORT,
+            },
+        )
+
+        await hass.async_block_till_done()
+
+        conf_entries = hass.config_entries.async_entries(DOMAIN)
+        entry = conf_entries[0]
+        assert entry.state is ConfigEntryState.LOADED
+
+        assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+        assert result["title"] == f"Airzone {short_mac(HVAC_WEBSERVER_MOCK[API_MAC])}"
+        assert result["data"][CONF_HOST] == TEST_IP
+        assert result["data"][CONF_PORT] == TEST_PORT
+
+        mock_setup_entry.assert_called_once()
 
 
 async def test_dhcp_invalid_system_id(hass: HomeAssistant) -> None:
