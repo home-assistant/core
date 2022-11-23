ae7b7640eb945cf6f7ea7b4460acfcf3be46ae5b
mkmer <mike.j.kasper@gmail.com>
2022-11-23 07:21:34 -0500

    Update homeassistant/components/whirlpool/config_flow.py
    Co-authored-by: Abílio Costa <abmantis@users.noreply.github.com>

--- homeassistant/components/whirlpool/config_flow.py
+++ homeassistant/components/whirlpool/config_flow.py
@@ -79,7 +79,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except (CannotConnect, asyncio.TimeoutError):
                 errors["base"] = "cannot_connect"
             else:
-
                 self.hass.config_entries.async_update_entry(
                     self.entry,
                     data={
