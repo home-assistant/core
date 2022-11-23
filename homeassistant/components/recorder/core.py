ba74594bd205bb9be5ab38d484a2c2b781330a50
Erik <erik@montnemery.com>
2022-11-23 14:24:27 +0100

    Fix do_adhoc_statistics

--- homeassistant/components/recorder/core.py
+++ homeassistant/components/recorder/core.py
@@ -375,17 +375,6 @@ class Recorder(threading.Thread):
         # Unknown what it is.
         return True
 
-    def do_adhoc_statistics(self, **kwargs: Any) -> None:
-        """Trigger an adhoc statistics run.
-
-        This is only used by tests, and will not fire events:
-        - EVENT_RECORDER_5MIN_STATISTICS_GENERATED
-        - EVENT_RECORDER_HOURLY_STATISTICS_GENERATED
-        """
-        if not (start := kwargs.get("start")):
-            start = statistics.get_start_time()
-        self.queue_task(StatisticsTask(start, False))
-
     def _empty_queue(self, event: Event) -> None:
         """Empty the queue if its still present at final write."""
 
