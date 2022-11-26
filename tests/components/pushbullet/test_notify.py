73ae4c5bbb52e45f34ddc424c84d267e4c53173d
G Johansson <goran.johansson@shiftit.se>
2022-11-26 14:32:51 +0000

    pushbullet break ci

--- tests/components/pushbullet/test_notify.py
+++ tests/components/pushbullet/test_notify.py
@@ -157,11 +157,14 @@ async def test_pushbullet_push_mixed(hass, requests_mock):
     )
     entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
     data = {
         "title": "Test Title",
         "message": "Test Message",
         "target": ["device/DESKTOP", "email/user@host.net"],
     }
+
     await hass.services.async_call(NOTIFY_DOMAIN, "pushbullet", data)
     await hass.async_block_till_done()
 
