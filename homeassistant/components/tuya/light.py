b278795fa33f2e2b21f90abfeab7a57e7b483f63
yeahme49 <58564160+yeahme49@users.noreply.github.com>
2022-11-27 07:57:01 -0600

    add DPCode for fsd
    Some ceiling fan lights use LIGHT for DPCode instead of SWITCH_LED

--- homeassistant/components/tuya/light.py
+++ homeassistant/components/tuya/light.py
@@ -123,6 +123,11 @@ LIGHTS: dict[str, tuple[TuyaLightEntityDescription, ...]] = {
             color_temp=DPCode.TEMP_VALUE,
             color_data=DPCode.COLOUR_DATA,
         ),
+        # Some ceiling fan lights use LIGHT for DPCode instead of SWITCH_LED
+        TuyaLightEntityDescription(
+            key=DPCode.LIGHT,
+            name="Light",
+        ),
     ),
     # Ambient Light
     # https://developer.tuya.com/en/docs/iot/ambient-light?id=Kaiuz06amhe6g
