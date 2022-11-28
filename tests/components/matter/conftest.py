44aa7887be79a07a6cf7d1fc1e160bbb8d408b8d
Martin Hjelmare <marhje52@gmail.com>
2022-11-28 14:55:57 +0100

    Add matter config entry add-on management

--- tests/components/matter/conftest.py
+++ tests/components/matter/conftest.py
@@ -53,6 +53,15 @@ async def integration_fixture(
     return entry
 
 
+@pytest.fixture(name="create_backup")
+def create_backup_fixture() -> Generator[AsyncMock, None, None]:
+    """Mock Supervisor create backup of add-on."""
+    with patch(
+        "homeassistant.components.hassio.addon_manager.async_create_backup"
+    ) as create_backup:
+        yield create_backup
+
+
 @pytest.fixture(name="addon_store_info")
 def addon_store_info_fixture() -> Generator[AsyncMock, None, None]:
     """Mock Supervisor add-on store info."""
@@ -124,11 +133,25 @@ def addon_running_fixture(
 
 
 @pytest.fixture(name="install_addon")
-def install_addon_fixture() -> Generator[AsyncMock, None, None]:
+def install_addon_fixture(
+    addon_store_info: AsyncMock, addon_info: AsyncMock
+) -> Generator[AsyncMock, None, None]:
     """Mock install add-on."""
+
+    async def install_addon_side_effect(hass: HomeAssistant, slug: str) -> None:
+        """Mock install add-on."""
+        addon_store_info.return_value = {
+            "installed": "1.0.0",
+            "state": "stopped",
+            "version": "1.0.0",
+        }
+        addon_info.return_value["state"] = "stopped"
+        addon_info.return_value["version"] = "1.0.0"
+
     with patch(
         "homeassistant.components.hassio.addon_manager.async_install_addon"
     ) as install_addon:
+        install_addon.side_effect = install_addon_side_effect
         yield install_addon
 
 
@@ -139,3 +162,30 @@ def start_addon_fixture() -> Generator[AsyncMock, None, None]:
         "homeassistant.components.hassio.addon_manager.async_start_addon"
     ) as start_addon:
         yield start_addon
+
+
+@pytest.fixture(name="stop_addon")
+def stop_addon_fixture() -> Generator[AsyncMock, None, None]:
+    """Mock stop add-on."""
+    with patch(
+        "homeassistant.components.hassio.addon_manager.async_stop_addon"
+    ) as stop_addon:
+        yield stop_addon
+
+
+@pytest.fixture(name="uninstall_addon")
+def uninstall_addon_fixture() -> Generator[AsyncMock, None, None]:
+    """Mock uninstall add-on."""
+    with patch(
+        "homeassistant.components.hassio.addon_manager.async_uninstall_addon"
+    ) as uninstall_addon:
+        yield uninstall_addon
+
+
+@pytest.fixture(name="update_addon")
+def update_addon_fixture() -> Generator[AsyncMock, None, None]:
+    """Mock update add-on."""
+    with patch(
+        "homeassistant.components.hassio.addon_manager.async_update_addon"
+    ) as update_addon:
+        yield update_addon
