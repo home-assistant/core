217f990251f90c8bc369fbcd3b901a1ccebddaf8
Joakim Plate <elupus@ecce.se>
2022-12-01 01:16:54 +0100

    Use parameters from library

--- homeassistant/components/nibe_heatpump/button.py
+++ homeassistant/components/nibe_heatpump/button.py
@@ -1,12 +1,10 @@
 """The Nibe Heat Pump sensors."""
 from __future__ import annotations
 
-from dataclasses import dataclass
-
+from nibe.coil_groups import UNIT_COILGROUPS, UnitCoilGroup
 from nibe.exceptions import CoilNotFoundException
-from nibe.heatpump import Series
 
-from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
+from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
@@ -14,43 +12,6 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from . import DOMAIN, LOGGER, Coordinator
-
-
-@dataclass
-class AlarmResetDescriptionMixin:
-    """Mixin for required fields."""
-
-    alarm_reset: int
-    alarm: int
-
-
-@dataclass
-class AlarmResetDescription(ButtonEntityDescription, AlarmResetDescriptionMixin):
-    """Base description."""
-
-
-RESET_BUTTONS_F = (
-    AlarmResetDescription(
-        key="reset-alarm",
-        name="Reset Alarm",
-        alarm_reset=45171,
-        alarm=45001,
-    ),
-)
-
-RESET_BUTTONS_S = (
-    AlarmResetDescription(
-        key="reset-alarm",
-        name="Reset Alarm",
-        alarm_reset=40023,
-        alarm=31976,
-    ),
-)
-
-RESET_BUTTONS = {
-    Series.F: RESET_BUTTONS_F,
-    Series.S: RESET_BUTTONS_S,
-}
 
 
 async def async_setup_entry(
@@ -63,7 +24,9 @@ async def async_setup_entry(
     coordinator: Coordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     def reset_buttons():
-        for entity_description in RESET_BUTTONS.get(coordinator.series, ()):
+        for entity_description in UNIT_COILGROUPS.get(coordinator.series, {}).get(
+            "main"
+        ):
             try:
                 yield NibeAlarmResetButton(coordinator, entity_description)
             except CoilNotFoundException as exception:
@@ -77,21 +40,13 @@ class NibeAlarmResetButton(CoordinatorEntity[Coordinator], ButtonEntity):
 
     _attr_has_entity_name = True
     _attr_entity_category = EntityCategory.DIAGNOSTIC
-    entity_description: AlarmResetDescription
 
-    def __init__(
-        self, coordinator: Coordinator, entity_description: AlarmResetDescription
-    ) -> None:
+    def __init__(self, coordinator: Coordinator, unit: UnitCoilGroup) -> None:
         """Initialize entity."""
-        self._reset_coil = coordinator.heatpump.get_coil_by_address(
-            entity_description.alarm_reset
-        )
-        self._alarm_coil = coordinator.heatpump.get_coil_by_address(
-            entity_description.alarm
-        )
+        self._reset_coil = coordinator.heatpump.get_coil_by_address(unit.alarm_reset)
+        self._alarm_coil = coordinator.heatpump.get_coil_by_address(unit.alarm)
         super().__init__(coordinator, {self._alarm_coil.address})
-        self.entity_description = entity_description
-        self._attr_unique_id = f"{coordinator.unique_id}-{entity_description.key}"
+        self._attr_unique_id = f"{coordinator.unique_id}-alarm_reset"
         self._attr_device_info = coordinator.device_info
 
     async def async_press(self) -> None:
