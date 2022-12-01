87955d60d5202bbe9fc5cf1f9a2348b2787cf6f5
Erik Montnemery <erik@montnemery.com>
2022-12-01 09:34:09 +0100

    Add `translation_key` property to entites (#82701)
    * Add translation_key attribute to entity state
    
    * Update accuweather test
    
    * Index entity translation keys by platform
    
    * Store translation key in entity registry

--- script/hassfest/translations.py
+++ script/hassfest/translations.py
@@ -255,6 +255,13 @@ def gen_strings_schema(config: Config, integration: Integration) -> vol.Schema:
                     ),
                 )
             },
+            vol.Optional("entity"): {
+                str: {
+                    str: vol.Schema(
+                        {vol.Optional("state"): {str: cv.string_with_no_html}}
+                    )
+                }
+            },
         }
     )
 
