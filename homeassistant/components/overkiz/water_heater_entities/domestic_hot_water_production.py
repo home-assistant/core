afe5e712d6fe17bb4614916288db36bf076d9749
Nyro <cedric@nyro.dev>
2022-12-02 11:22:35 +0100

    Fix small coding style in recent overkiz code (#82966)
    * Fix small coding style
    
    * Use kwargs[ATTR_TEMPERATURE] in overkiz water_heater
    
    * Fix grammar
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/overkiz/water_heater_entities/domestic_hot_water_production.py
+++ homeassistant/components/overkiz/water_heater_entities/domestic_hot_water_production.py
@@ -230,7 +230,7 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
 
     async def async_set_temperature(self, **kwargs: Any) -> None:
         """Set new target temperature."""
-        target_temperature = kwargs.get(ATTR_TEMPERATURE)
+        target_temperature = kwargs[ATTR_TEMPERATURE]
 
         if self.executor.has_command(OverkizCommand.SET_TARGET_TEMPERATURE):
             await self.executor.async_execute_command(
@@ -299,7 +299,8 @@ class DomesticHotWaterProduction(OverkizEntity, WaterHeaterEntity):
             return
 
         if self._is_boost_mode_on:
-            # We're setting a non Boost mode and the device is currently in Boost mode, the following code remove all boost operations
+            # We're setting a non Boost mode and the device is currently in Boost mode
+            # The following code removes all boost operations
             if self.executor.has_command(OverkizCommand.SET_BOOST_MODE):
                 await self.executor.async_execute_command(
                     OverkizCommand.SET_BOOST_MODE, OverkizCommand.OFF
