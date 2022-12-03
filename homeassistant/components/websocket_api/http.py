5c25d7c38b0419a08153b83b69a26f38bd9f91e7
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-12-03 16:19:16 +0100

    Add CLOSED as an expected type (#83180)

--- homeassistant/components/websocket_api/http.py
+++ homeassistant/components/websocket_api/http.py
@@ -214,7 +214,7 @@ class WebSocketHandler:
                 disconnect_warn = "Did not receive auth message within 10 seconds"
                 raise Disconnect from err
 
-            if msg.type in (WSMsgType.CLOSE, WSMsgType.CLOSING):
+            if msg.type in (WSMsgType.CLOSE, WSMsgType.CLOSED, WSMsgType.CLOSING):
                 raise Disconnect
 
             if msg.type != WSMsgType.TEXT:
@@ -238,7 +238,7 @@ class WebSocketHandler:
             while not wsock.closed:
                 msg = await wsock.receive()
 
-                if msg.type in (WSMsgType.CLOSE, WSMsgType.CLOSING):
+                if msg.type in (WSMsgType.CLOSE, WSMsgType.CLOSED, WSMsgType.CLOSING):
                     break
 
                 if msg.type != WSMsgType.TEXT:
