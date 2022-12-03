5c90a1f5e49b91e8f39603f1a5bd68cac2827d79
mib1185 <mail@mib85.de>
2022-12-03 17:09:14 +0000

    simplify cast to str

--- homeassistant/components/fritz/sensor.py
+++ homeassistant/components/fritz/sensor.py
@@ -5,7 +5,7 @@ from collections.abc import Callable
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 import logging
-from typing import Any, cast
+from typing import Any
 
 from fritzconnection.core.exceptions import FritzConnectionException
 from fritzconnection.lib.fritzstatus import FritzStatus
@@ -68,7 +68,7 @@ def _retrieve_external_ip_state(status: FritzStatus, last_value: str) -> str:
 
 def _retrieve_external_ipv6_state(status: FritzStatus, last_value: str) -> str:
     """Return external ipv6 from device."""
-    return cast(str, status.external_ipv6)
+    return str(status.external_ipv6)
 
 
 def _retrieve_kb_s_sent_state(status: FritzStatus, last_value: str) -> float:
