3857cf4b123a56c9617a36bc3824863217fe92da
epenet <6771947+epenet@users.noreply.github.com>
2022-12-05 20:03:35 +0100

    Update homeassistant/components/ambient_station/sensor.py

--- homeassistant/components/ambient_station/sensor.py
+++ homeassistant/components/ambient_station/sensor.py
@@ -114,7 +114,6 @@ SENSOR_DESCRIPTIONS = (
     SensorEntityDescription(
         key=TYPE_24HOURRAININ,
         name="24 hr rain",
-        icon="mdi:water",
         native_unit_of_measurement=UnitOfPrecipitationDepth.INCHES,
         device_class=SensorDeviceClass.PRECIPITATION,
         state_class=SensorStateClass.TOTAL_INCREASING,
