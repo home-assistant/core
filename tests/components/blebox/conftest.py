de629158a411ea49b5f2d24d9a0de685971bd0a1
Michał Huryn <michalhuryn@gmail.com>
2022-12-06 12:58:54 +0100

    Update tests/components/blebox/conftest.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/blebox/conftest.py
+++ tests/components/blebox/conftest.py
@@ -81,7 +81,7 @@ async def async_setup_entities(hass, entity_ids):
     config_entry = mock_config()
     config_entry.add_to_hass(hass)
 
-    assert await hass.config_entries.async_setup(config.entry_id)
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
 
     entity_registry = er.async_get(hass)
