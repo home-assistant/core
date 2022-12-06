07c60686b1b190ff9cca60b2b8e6622290089660
Martin Hjelmare <marhje52@gmail.com>
2022-12-06 21:57:24 +0100

    Add matter switch platform (#83149)

--- homeassistant/components/matter/device_platform.py
+++ homeassistant/components/matter/device_platform.py
@@ -8,6 +8,7 @@ from homeassistant.const import Platform
 from .binary_sensor import DEVICE_ENTITY as BINARY_SENSOR_DEVICE_ENTITY
 from .light import DEVICE_ENTITY as LIGHT_DEVICE_ENTITY
 from .sensor import DEVICE_ENTITY as SENSOR_DEVICE_ENTITY
+from .switch import DEVICE_ENTITY as SWITCH_DEVICE_ENTITY
 
 if TYPE_CHECKING:
     from matter_server.common.models.device_types import DeviceType
@@ -25,4 +26,5 @@ DEVICE_PLATFORM: dict[
     Platform.BINARY_SENSOR: BINARY_SENSOR_DEVICE_ENTITY,
     Platform.LIGHT: LIGHT_DEVICE_ENTITY,
     Platform.SENSOR: SENSOR_DEVICE_ENTITY,
+    Platform.SWITCH: SWITCH_DEVICE_ENTITY,
 }
