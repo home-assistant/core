af345d5f62b1a3e5e639b9f32036990546922040
Mandar Patil <mandarons@pm.me>
2022-12-06 11:30:28 -0800

    Update homeassistant/components/bouncie/__init__.py
    Co-authored-by: Quentame <polletquentin74@me.com>

--- homeassistant/components/bouncie/__init__.py
+++ homeassistant/components/bouncie/__init__.py
@@ -39,8 +39,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         update_interval=update_interval,
     )
     await coordinator.async_config_entry_first_refresh()
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
