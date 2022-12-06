66e24c474f18042150940ab2ca92c2fd19df9290
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-12-06 01:53:28 -0500

    Fix zwave_js_value_updated event

--- tests/components/zwave_js/test_events.py
+++ tests/components/zwave_js/test_events.py
@@ -235,7 +235,6 @@ async def test_value_updated(hass, vision_security_zl7432, integration, client):
     """Test value updated events."""
     node = vision_security_zl7432
     # Add states to the value we are updating to ensure the translation happens
-    # assert node.values.keys() == []
     node.values["7-37-1-currentValue"].metadata.data["states"] = {"1": "on", "0": "off"}
     events = async_capture_events(hass, "zwave_js_value_updated")
 
