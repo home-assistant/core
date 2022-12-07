cee86561e5440cddf44cf18084abfbd634a01c65
pilehave <pilehave@gmail.com>
2022-12-07 17:14:28 +0000

    Cleanup after rebase :-/

--- homeassistant/components/touchline/climate.py
+++ homeassistant/components/touchline/climate.py
@@ -74,6 +74,8 @@ class Touchline(ClimateEntity):
         """Initialize the Touchline device."""
         self.unit = touchline_thermostat
         self._name = None
+        self._device_id = None
+        self._controller_id = None
         self._current_temperature = None
         self._target_temperature = None
         self._min_temp = None
@@ -85,6 +87,8 @@ class Touchline(ClimateEntity):
         """Update thermostat attributes."""
         self.unit.update()
         self._name = self.unit.get_name()
+        self._device_id = self.unit.get_device_id()
+        self._controller_id = self.unit.get_controller_id()
         self._current_temperature = self.unit.get_current_temperature()
         self._target_temperature = self.unit.get_target_temperature()
         self._min_temp = self.unit.get_target_temperature_low()
