7e76fe74989c1f665f8b6b262dd9fa9086cab938
Ola Thoresen <ola@nytt.no>
2022-12-09 08:39:25 +0000

    Probably dont need the init in the exception

--- homeassistant/components/twinkly/exceptions.py
+++ homeassistant/components/twinkly/exceptions.py
@@ -3,8 +3,3 @@
 
 class TwinklyError(ValueError):
     """Error from the API."""
-
-    def __init__(self, message: str, response=None) -> None:
-        """Initiate the exception."""
-        self.message = message
-        self.response = response
