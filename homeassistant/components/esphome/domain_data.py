d194390b4788814b8bf1bea44cb2f93e287bb612
Paulus Schoutsen <balloob@gmail.com>
2022-12-10 22:26:42 -0500

    ESPHome: Use MAC as unique ID (#83741)
    * ESPHome: Use MAC as unique ID
    
    * Normalize incoming zeroconf/dhcp macs
    
    * Update comment
    
    * Test ESPHome without mac in zeroconf
    
    * Use format_mac
    
    * Remove unique ID index from DomainData

--- homeassistant/components/esphome/domain_data.py
+++ homeassistant/components/esphome/domain_data.py
@@ -23,11 +23,6 @@ class DomainData:
 
     _entry_datas: dict[str, RuntimeEntryData] = field(default_factory=dict)
     _stores: dict[str, Store] = field(default_factory=dict)
-    _entry_by_unique_id: dict[str, ConfigEntry] = field(default_factory=dict)
-
-    def get_by_unique_id(self, unique_id: str) -> ConfigEntry:
-        """Get the config entry by its unique ID."""
-        return self._entry_by_unique_id[unique_id]
 
     def get_entry_data(self, entry: ConfigEntry) -> RuntimeEntryData:
         """Return the runtime entry data associated with this config entry.
@@ -41,13 +36,9 @@ class DomainData:
         if entry.entry_id in self._entry_datas:
             raise ValueError("Entry data for this entry is already set")
         self._entry_datas[entry.entry_id] = entry_data
-        if entry.unique_id:
-            self._entry_by_unique_id[entry.unique_id] = entry
 
     def pop_entry_data(self, entry: ConfigEntry) -> RuntimeEntryData:
         """Pop the runtime entry data instance associated with this config entry."""
-        if entry.unique_id:
-            del self._entry_by_unique_id[entry.unique_id]
         return self._entry_datas.pop(entry.entry_id)
 
     def is_entry_loaded(self, entry: ConfigEntry) -> bool:
