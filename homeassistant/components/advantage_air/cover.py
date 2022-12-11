ed60d73df74cd2b189e029ab835fd8a0fbab3a37
Koraxe <koraxe@github.com>
2022-12-12 08:32:09 +1100

    Add garage door support

--- homeassistant/components/advantage_air/cover.py
+++ homeassistant/components/advantage_air/cover.py
@@ -10,13 +10,17 @@ from homeassistant.components.cover import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity import DeviceInfo
 
 from .const import (
     ADVANTAGE_AIR_STATE_CLOSE,
     ADVANTAGE_AIR_STATE_OPEN,
     DOMAIN as ADVANTAGE_AIR_DOMAIN,
 )
-from .entity import AdvantageAirZoneEntity
+from .entity import (
+    AdvantageAirZoneEntity,
+    AdvantageAirEntity,
+)
 
 PARALLEL_UPDATES = 0
 
@@ -37,6 +41,11 @@ async def async_setup_entry(
                 # Only add zone vent controls when zone in vent control mode.
                 if zone["type"] == 0:
                     entities.append(AdvantageAirZoneVent(instance, ac_key, zone_key))
+
+    if my_things := instance["coordinator"].data.get("myThings"):
+        for thing in my_things["things"].values():
+            if thing.get("buttonType") == "upDown":
+                entities.append(AdvantageAirGarageDoor(instance, thing))
     async_add_entities(entities)
 
 
@@ -113,3 +122,50 @@ class AdvantageAirZoneVent(AdvantageAirZoneEntity, CoverEntity):
                     }
                 }
             )
+
+
+class AdvantageAirGarageDoor(AdvantageAirEntity, CoverEntity):
+    """Advantage Air Garage Door."""
+
+    _attr_device_class = CoverDeviceClass.GARAGE
+    _attr_supported_features = (
+        CoverEntityFeature.OPEN
+        | CoverEntityFeature.CLOSE
+    )
+
+    def __init__(self, instance: dict[str, Any], thing: dict[str, Any]) -> None:
+        """Initialize an Advantage Air Garage Door."""
+        super().__init__(instance)
+        self.things = instance["things"]
+        self._id: str = thing["id"]
+        self._attr_unique_id += f"-{self._id}"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(ADVANTAGE_AIR_DOMAIN, self._attr_unique_id)},
+            via_device=(ADVANTAGE_AIR_DOMAIN, self.coordinator.data["system"]["rid"]),
+            manufacturer="Advantage Air",
+            model=thing.get("buttonType"),
+            name=thing["name"],
+        )
+
+    @property
+    def _thing(self) -> dict[str, Any]:
+        """Return the garage object."""
+        return self.coordinator.data["myThings"]["things"][self._id]
+
+    @property
+    def is_closed(self) -> bool:
+        """Return if garage is closed."""
+        return self._thing["value"] == 0
+
+    @property
+    def current_cover_position(self) -> int:
+        """Return garage current position as a percentage."""
+        return self._thing["value"]
+
+    async def async_open_cover(self, **kwargs: Any) -> None:
+        """Open garage door."""
+        await self.things({"id": self._id, "value": 100})
+
+    async def async_close_cover(self, **kwargs: Any) -> None:
+        """Open garage door."""
+        await self.things({"id": self._id, "value": 0})
