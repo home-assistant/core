03f92da7352a3fa01651707420833c71ad17379e
epenet <6771947+epenet@users.noreply.github.com>
2022-12-12 07:26:36 +0000

    Use POWER device class in enphase_envoy

--- homeassistant/components/enphase_envoy/const.py
+++ homeassistant/components/enphase_envoy/const.py
@@ -1,12 +1,5 @@
 """The enphase_envoy component."""
-
-
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntityDescription,
-    SensorStateClass,
-)
-from homeassistant.const import ENERGY_WATT_HOUR, POWER_WATT, Platform
+from homeassistant.const import Platform
 
 DOMAIN = "enphase_envoy"
 
@@ -15,60 +8,3 @@ PLATFORMS = [Platform.SENSOR]
 
 COORDINATOR = "coordinator"
 NAME = "name"
-
-SENSORS = (
-    SensorEntityDescription(
-        key="production",
-        name="Current Power Production",
-        native_unit_of_measurement=POWER_WATT,
-        state_class=SensorStateClass.MEASUREMENT,
-    ),
-    SensorEntityDescription(
-        key="daily_production",
-        name="Today's Energy Production",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
-        state_class=SensorStateClass.TOTAL_INCREASING,
-        device_class=SensorDeviceClass.ENERGY,
-    ),
-    SensorEntityDescription(
-        key="seven_days_production",
-        name="Last Seven Days Energy Production",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
-        state_class=SensorStateClass.TOTAL_INCREASING,
-        device_class=SensorDeviceClass.ENERGY,
-    ),
-    SensorEntityDescription(
-        key="lifetime_production",
-        name="Lifetime Energy Production",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
-        state_class=SensorStateClass.TOTAL_INCREASING,
-        device_class=SensorDeviceClass.ENERGY,
-    ),
-    SensorEntityDescription(
-        key="consumption",
-        name="Current Power Consumption",
-        native_unit_of_measurement=POWER_WATT,
-        state_class=SensorStateClass.MEASUREMENT,
-    ),
-    SensorEntityDescription(
-        key="daily_consumption",
-        name="Today's Energy Consumption",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
-        state_class=SensorStateClass.TOTAL_INCREASING,
-        device_class=SensorDeviceClass.ENERGY,
-    ),
-    SensorEntityDescription(
-        key="seven_days_consumption",
-        name="Last Seven Days Energy Consumption",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
-        state_class=SensorStateClass.TOTAL_INCREASING,
-        device_class=SensorDeviceClass.ENERGY,
-    ),
-    SensorEntityDescription(
-        key="lifetime_consumption",
-        name="Lifetime Energy Consumption",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
-        state_class=SensorStateClass.TOTAL_INCREASING,
-        device_class=SensorDeviceClass.ENERGY,
-    ),
-)
