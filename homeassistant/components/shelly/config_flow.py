d8424042f13df5e4e4867ffed5bc7a767628fde6
J. Nick Koston <nick@koston.org>
2022-12-12 11:48:37 -1000

    try harder to not connect

--- homeassistant/components/shelly/config_flow.py
+++ homeassistant/components/shelly/config_flow.py
@@ -41,6 +41,7 @@ from .utils import (
     get_rpc_device_name,
     get_rpc_device_sleep_period,
     get_ws_context,
+    mac_address_from_name,
 )
 
 HOST_SCHEMA: Final = vol.Schema({vol.Required(CONF_HOST): str})
@@ -210,16 +211,24 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="credentials", data_schema=vol.Schema(schema), errors=errors
         )
 
+    async def _async_discovered_mac(self, mac: str, host: str) -> None:
+        """Return True if mac is already discovered."""
+        if (
+            current_entry := await self.async_set_unique_id(mac)
+        ) and current_entry.data[CONF_HOST] == host:
+            await async_reconnect_soon(self.hass, current_entry)
+        self._abort_if_unique_id_configured({CONF_HOST: host})
+
     async def async_step_zeroconf(
         self, discovery_info: zeroconf.ZeroconfServiceInfo
     ) -> FlowResult:
         """Handle zeroconf discovery."""
         host = discovery_info.host
-        if (
-            current_entry := await self.async_set_unique_id(self.info["mac"])
-        ) and current_entry.data[CONF_HOST] == host:
-            await async_reconnect_soon(self.hass, current_entry)
-        self._abort_if_unique_id_configured({CONF_HOST: host})
+        # First try to get the mac address from the name
+        # so we can avoid making another connection to the
+        # device if we already have it configured
+        if mac := mac_address_from_name(discovery_info.name):
+            await self._async_discovered_mac(mac, host)
 
         try:
             self.info = await self._async_get_info(host)
@@ -227,6 +236,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="cannot_connect")
         except FirmwareUnsupported:
             return self.async_abort(reason="unsupported_firmware")
+
+        if not mac:
+            # We could not get the mac address from the name
+            # so need to check here since we just got the info
+            await self._async_discovered_mac(self.info["mac"], host)
 
         self.host = host
         self.context.update(
