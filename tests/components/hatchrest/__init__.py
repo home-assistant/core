1dd325fb2e2e5d2e14edcc0904680ab23735420b
ViViDboarder <ViViDboarder@gmail.com>
2022-12-15 11:27:14 -0800

    Fix tests

--- tests/components/hatchrest/__init__.py
+++ tests/components/hatchrest/__init__.py
@@ -1,6 +1,5 @@
 """Tests for the hatchrest integration."""
 from bleak.backends.device import BLEDevice
-from bleak.backends.scanner import AdvertisementData
 
 from homeassistant.components.bluetooth import BluetoothServiceInfoBleak
 from homeassistant.components.hatchrest.const import DOMAIN
@@ -8,19 +7,25 @@ from homeassistant.const import CONF_ADDRESS
 from homeassistant.helpers.device_registry import format_mac
 
 from tests.common import MockConfigEntry
+from tests.components.bluetooth import generate_advertisement_data
 
-VALID_HATCHREST = BluetoothServiceInfoBleak.from_advertisement(
-    BLEDevice("00:00:00:00:00:00", "Hatch Baby Rest"),
-    AdvertisementData(
+VALID_HATCHREST = BluetoothServiceInfoBleak(
+    name="Hatch Baby Rest",
+    address="00:00:00:00:00:00",
+    manufacturer_data={1076: b"\x00"},
+    service_data={},
+    service_uuids=[],
+    rssi=-60,
+    source="local",
+    advertisement=generate_advertisement_data(
         local_name="Hatch Baby Rest",
-        address="00:00:00:00:00:00",
-        rssi=-50,
         manufacturer_data={1076: b"\x00"},
         service_data={},
         service_uuids=[],
-        source="local",
     ),
-    source="local",
+    device=BLEDevice("00:00:00:00:00:00", "Hatch Baby Rest"),
+    time=0,
+    connectable=True,
 )
 
 VALID_HATCHREST_ENTRY = MockConfigEntry(
