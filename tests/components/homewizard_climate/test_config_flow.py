74f5f347149887e7cf8a9b9080c0eaf4433729e6
Mepla <soheil.nasirian@gmail.com>
2022-12-16 17:14:20 +0100

    Add config flow test

--- tests/components/homewizard_climate/test_config_flow.py
+++ tests/components/homewizard_climate/test_config_flow.py
@@ -1,11 +1,9 @@
 """Test the Homewizard Climate config flow."""
 from unittest.mock import patch
 
+from homewizard_climate_websocket.api.api import InvalidHomewizardAuth
+
 from homeassistant import config_entries
-from homeassistant.components.homewizard_climate.config_flow import (
-    CannotConnect,
-    InvalidAuth,
-)
 from homeassistant.components.homewizard_climate.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
@@ -20,8 +18,8 @@ async def test_form(hass: HomeAssistant) -> None:
     assert result["errors"] is None
 
     with patch(
-        "homeassistant.components.homewizard_climate.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
+        "homewizard_climate_websocket.api.api.HomeWizardClimateApi.login",
+        return_value="fake_token",
     ), patch(
         "homeassistant.components.homewizard_climate.async_setup_entry",
         return_value=True,
@@ -29,17 +27,14 @@ async def test_form(hass: HomeAssistant) -> None:
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "host": "1.1.1.1",
                 "username": "test-username",
                 "password": "test-password",
             },
         )
         await hass.async_block_till_done()
 
-    assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Name of the device"
+    assert result2["title"] == "test-username"
     assert result2["data"] == {
-        "host": "1.1.1.1",
         "username": "test-username",
         "password": "test-password",
     }
@@ -53,13 +48,12 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
     )
 
     with patch(
-        "homeassistant.components.homewizard_climate.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
+        "homewizard_climate_websocket.api.api.HomeWizardClimateApi.login",
+        side_effect=InvalidHomewizardAuth,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "host": "1.1.1.1",
                 "username": "test-username",
                 "password": "test-password",
             },
@@ -69,24 +63,23 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
     assert result2["errors"] == {"base": "invalid_auth"}
 
 
-async def test_form_cannot_connect(hass: HomeAssistant) -> None:
+async def test_form_unknown_error(hass: HomeAssistant) -> None:
     """Test we handle cannot connect error."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
     with patch(
-        "homeassistant.components.homewizard_climate.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
+        "homewizard_climate_websocket.api.api.HomeWizardClimateApi.login",
+        side_effect=RuntimeError,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "host": "1.1.1.1",
                 "username": "test-username",
                 "password": "test-password",
             },
         )
 
     assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"] == {"base": "cannot_connect"}
+    assert result2["errors"] == {"base": "unknown"}
