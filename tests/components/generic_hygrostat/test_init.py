c87d0366dc9ef63a6a1b31ae2c8aaafa96f7e044
Kevin Brown-Silva <kevin@kevin-brown.com>
2022-12-17 17:20:10 +0000

    Clean up test init file

--- tests/components/generic_hygrostat/test_init.py
+++ tests/components/generic_hygrostat/test_init.py
@@ -9,13 +9,22 @@ from tests.common import MockConfigEntry, assert_setup_component
 ENT_SENSOR = "sensor.test"
 ENT_SWITCH = "switch.test"
 
+MISSING_CONFIG = {
+    "name": "test",
+    "target_sensor": ENT_SENSOR,
+}
+
+VALID_CONFIG = {
+    **MISSING_CONFIG,
+    "target_sensor": ENT_SENSOR,
+}
+
 
 async def test_setup_missing_conf(hass):
     """Test set up humidity_control with missing config values."""
     config = {
         "platform": "generic_hygrostat",
-        "name": "test",
-        "target_sensor": ENT_SENSOR,
+        **MISSING_CONFIG,
     }
     with assert_setup_component(0):
         await async_setup_component(hass, "humidifier", {"humidifier": config})
@@ -27,14 +36,7 @@ async def test_valid_conf(hass):
     assert await async_setup_component(
         hass,
         "humidifier",
-        {
-            "humidifier": {
-                "platform": "generic_hygrostat",
-                "name": "test",
-                "humidifier": ENT_SWITCH,
-                "target_sensor": ENT_SENSOR,
-            }
-        },
+        {"humidifier": {"platform": "generic_hygrostat", **VALID_CONFIG}},
     )
     await hass.async_block_till_done()
 
@@ -44,11 +46,7 @@ async def test_valid_config_entry(hass):
 
     entry = MockConfigEntry(
         domain="generic_hygrostat",
-        data={
-            "name": "test",
-            "humidifier": ENT_SWITCH,
-            "target_sensor": ENT_SENSOR,
-        },
+        data=VALID_CONFIG,
     )
     entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(entry.entry_id)
@@ -56,14 +54,10 @@ async def test_valid_config_entry(hass):
 
 
 async def test_async_unload_entry(hass: HomeAssistant) -> None:
-    """Test pushbullet unload entry."""
+    """Test generic_hygrostat unload entry."""
     entry = MockConfigEntry(
         domain="generic_hygrostat",
-        data={
-            "name": "test",
-            "humidifier": ENT_SWITCH,
-            "target_sensor": ENT_SENSOR,
-        },
+        data=VALID_CONFIG,
     )
     entry.add_to_hass(hass)
 
