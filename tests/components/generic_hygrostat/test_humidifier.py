d7e688595a96760223b3d57f22ec4092d5ffaddc
Kevin Brown-Silva <kevin@kevin-brown.com>
2022-12-18 03:07:00 +0000

    Remove migrated tests

--- tests/components/generic_hygrostat/test_humidifier.py
+++ tests/components/generic_hygrostat/test_humidifier.py
@@ -28,12 +28,7 @@ from homeassistant.core import DOMAIN as HASS_DOMAIN, CoreState, State, callback
 from homeassistant.setup import async_setup_component
 import homeassistant.util.dt as dt_util
 
-from tests.common import (
-    MockConfigEntry,
-    assert_setup_component,
-    async_fire_time_changed,
-    mock_restore_cache,
-)
+from tests.common import async_fire_time_changed, mock_restore_cache
 
 ENTITY = "humidifier.test"
 ENT_SENSOR = "sensor.test"
@@ -42,51 +37,6 @@ ATTR_SAVED_HUMIDITY = "saved_humidity"
 MIN_HUMIDITY = 20
 MAX_HUMIDITY = 65
 TARGET_HUMIDITY = 42
-
-
-async def test_setup_missing_conf(hass):
-    """Test set up humidity_control with missing config values."""
-    config = {
-        "platform": "generic_hygrostat",
-        "name": "test",
-        "target_sensor": ENT_SENSOR,
-    }
-    with assert_setup_component(0):
-        await async_setup_component(hass, "humidifier", {"humidifier": config})
-        await hass.async_block_till_done()
-
-
-async def test_valid_conf(hass):
-    """Test set up generic_hygrostat with valid config values."""
-    assert await async_setup_component(
-        hass,
-        "humidifier",
-        {
-            "humidifier": {
-                "platform": "generic_hygrostat",
-                "name": "test",
-                "humidifier": ENT_SWITCH,
-                "target_sensor": ENT_SENSOR,
-            }
-        },
-    )
-    await hass.async_block_till_done()
-
-
-async def test_valid_config_entry(hass):
-    """Test setting up a generic hygrostat with a valid config entry."""
-
-    entry = MockConfigEntry(
-        domain="generic_hygrostat",
-        data={
-            "name": "test",
-            "humidifier": ENT_SWITCH,
-            "target_sensor": ENT_SENSOR,
-        },
-    )
-    entry.add_to_hass(hass)
-    assert await hass.config_entries.async_setup(entry.entry_id)
-    await hass.async_block_till_done()
 
 
 @pytest.fixture
