d423cbf8ebc3fec1303055b79e7006cc0701c94a
Aaron Bach <bachya1208@gmail.com>
2022-12-18 11:30:05 -0700

    Add ability to add/remove PurpleAir sensors in an existing config entry (#83440)

--- homeassistant/components/purpleair/const.py
+++ homeassistant/components/purpleair/const.py
@@ -5,5 +5,6 @@ DOMAIN = "purpleair"
 
 LOGGER = logging.getLogger(__package__)
 
+CONF_LAST_UPDATE_SENSOR_ADD = "last_update_sensor_add"
 CONF_READ_KEY = "read_key"
 CONF_SENSOR_INDICES = "sensor_indices"
