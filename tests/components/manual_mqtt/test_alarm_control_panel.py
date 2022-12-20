480158bfe192b62d4e647faa8050d087818c1535
Paolo Bonzini <pbonzini@redhat.com>
2022-12-20 10:21:17 +0100

    manual_mqtt: bring over a new test from manual

--- tests/components/manual_mqtt/test_alarm_control_panel.py
+++ tests/components/manual_mqtt/test_alarm_control_panel.py
@@ -1106,6 +1106,51 @@ async def test_trigger_with_specific_pending(hass, mqtt_mock_entry_with_yaml_con
     assert hass.states.get(entity_id).state == STATE_ALARM_DISARMED
 
 
+async def test_trigger_with_no_disarm_after_trigger(
+    hass, mqtt_mock_entry_with_yaml_config
+):
+    """Test disarm after trigger."""
+    assert await async_setup_component(
+        hass,
+        alarm_control_panel.DOMAIN,
+        {
+            "alarm_control_panel": {
+                "platform": "manual_mqtt",
+                "name": "test",
+                "trigger_time": 5,
+                "pending_time": 0,
+                "delay_time": 0,
+                "disarm_after_trigger": False,
+                "command_topic": "alarm/command",
+                "state_topic": "alarm/state",
+            }
+        },
+    )
+    await hass.async_block_till_done()
+
+    entity_id = "alarm_control_panel.test"
+
+    assert hass.states.get(entity_id).state == STATE_ALARM_DISARMED
+
+    await common.async_alarm_arm_away(hass, CODE, entity_id)
+
+    assert hass.states.get(entity_id).state == STATE_ALARM_ARMED_AWAY
+
+    await common.async_alarm_trigger(hass, entity_id=entity_id)
+
+    assert hass.states.get(entity_id).state == STATE_ALARM_TRIGGERED
+
+    future = dt_util.utcnow() + timedelta(seconds=5)
+    with patch(
+        ("homeassistant.components.manual_mqtt.alarm_control_panel.dt_util.utcnow"),
+        return_value=future,
+    ):
+        async_fire_time_changed(hass, future)
+        await hass.async_block_till_done()
+
+    assert hass.states.get(entity_id).state == STATE_ALARM_ARMED_AWAY
+
+
 async def test_arm_away_after_disabled_disarmed(hass, mqtt_mock_entry_with_yaml_config):
     """Test pending state with and without zero trigger time."""
     assert await async_setup_component(
