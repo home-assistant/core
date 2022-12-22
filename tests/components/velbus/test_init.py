386a1b971688db92323799f0f447354521ffc1e1
Martin Hjelmare <marhje52@gmail.com>
2022-12-22 10:15:48 +0100

    Fix and clean tests

--- tests/components/velbus/test_init.py
+++ tests/components/velbus/test_init.py
@@ -1,4 +1,7 @@
 """Tests for the Velbus component initialisation."""
+from collections.abc import Generator
+from unittest.mock import patch
+
 import pytest
 
 from homeassistant.components.velbus.const import DOMAIN
@@ -9,6 +12,15 @@ from homeassistant.core import HomeAssistant
 from tests.common import MockConfigEntry, mock_device_registry
 
 
+@pytest.fixture(name="config_flow_version")
+def config_flow_version_fixture() -> Generator[None, None, None]:
+    """Mock the config flow version."""
+    with patch(
+        "homeassistant.components.velbus.config_flow.VelbusConfigFlow.VERSION", 2
+    ):
+        yield
+
+
 @pytest.mark.usefixtures("controller")
 async def test_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     """Test being able to unload an entry."""
@@ -16,12 +28,12 @@ async def test_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     await hass.async_block_till_done()
 
     assert len(hass.config_entries.async_entries(DOMAIN)) == 1
-    assert config_entry.state is ConfigEntryState.LOADED
+    assert config_entry.state == ConfigEntryState.LOADED
 
     assert await hass.config_entries.async_unload(config_entry.entry_id)
     await hass.async_block_till_done()
 
-    assert config_entry.state is ConfigEntryState.NOT_LOADED
+    assert config_entry.state == ConfigEntryState.NOT_LOADED
     assert not hass.data.get(DOMAIN)
 
 
@@ -36,19 +48,23 @@ async def test_device_identifier_migration(
     device_registry = mock_device_registry(hass)
     device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
-        identifiers=original_identifiers,
+        identifiers=original_identifiers,  # type: ignore[arg-type]
         name="channel_name",
         manufacturer="Velleman",
         model="module_type_name",
         sw_version="module_sw_version",
     )
-    assert device_registry.async_get_device(original_identifiers)
+    assert device_registry.async_get_device(
+        original_identifiers  # type: ignore[arg-type]
+    )
     assert not device_registry.async_get_device(target_identifiers)
 
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
 
-    assert not device_registry.async_get_device(original_identifiers)
+    assert not device_registry.async_get_device(
+        original_identifiers  # type: ignore[arg-type]
+    )
     device_entry = device_registry.async_get_device(target_identifiers)
     assert device_entry
     assert device_entry.name == "channel_name"
@@ -57,16 +73,17 @@ async def test_device_identifier_migration(
     assert device_entry.sw_version == "module_sw_version"
 
 
-async def test_migrate_config_entry(hass: HomeAssistant):
+@pytest.mark.usefixtures("controller", "config_flow_version")
+async def test_migrate_config_entry(hass: HomeAssistant) -> None:
     """Test successful migration of entry data."""
     legacy_config = {CONF_NAME: "fake_name", CONF_PORT: "1.2.3.4:5678"}
     entry = MockConfigEntry(domain=DOMAIN, unique_id="my own id", data=legacy_config)
     entry.add_to_hass(hass)
 
-    assert entry.data == legacy_config
+    assert dict(entry.data) == legacy_config
     assert entry.version == 1
 
     await hass.config_entries.async_setup(entry.entry_id)
 
-    assert entry.data == legacy_config
-    assert entry.version == 1
+    assert dict(entry.data) == legacy_config
+    assert entry.version == 2
