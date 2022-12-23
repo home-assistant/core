84b14dad40bc4e0510c7de7e4c61d43403a9e602
Yuval Aboulafia <yuval.abou@gmail.com>
2022-12-23 15:54:22 +0200

    Various improvements

--- homeassistant/components/jewish_calendar/__init__.py
+++ homeassistant/components/jewish_calendar/__init__.py
@@ -7,6 +7,7 @@ import voluptuous as vol
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import (
     CONF_ELEVATION,
+    CONF_LANGUAGE,
     CONF_LATITUDE,
     CONF_LOCATION,
     CONF_LONGITUDE,
@@ -22,7 +23,6 @@ from .const import (
     CONF_CANDLE_LIGHT_MINUTES,
     CONF_DIASPORA,
     CONF_HAVDALAH_OFFSET_MINUTES,
-    CONF_LANGUAGE,
     DEFAULT_CANDLE_LIGHT,
     DEFAULT_DIASPORA,
     DEFAULT_HAVDALAH_OFFSET_MINUTES,
@@ -124,7 +124,6 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     location = Location(
         name=hass.config.location_name,
         diaspora=diaspora,
-        # If details of the location are not specified, use Hass's defaults.
         latitude=latitude,
         longitude=longitude,
         altitude=config_entry.data.get(CONF_ELEVATION, hass.config.elevation),
--- homeassistant/components/jewish_calendar/const.py
+++ homeassistant/components/jewish_calendar/const.py
@@ -1,14 +1,14 @@
 """Constants for the Jewish Calendar integration."""
+from typing import Final
 
-DOMAIN = "jewish_calendar"
+DOMAIN: Final = "jewish_calendar"
 
-CONF_DIASPORA = "diaspora"
-CONF_LANGUAGE = "language"
-CONF_CANDLE_LIGHT_MINUTES = "candle_lighting_minutes_before_sunset"
-CONF_HAVDALAH_OFFSET_MINUTES = "havdalah_minutes_after_sunset"
+CONF_DIASPORA: Final = "diaspora"
+CONF_CANDLE_LIGHT_MINUTES: Final = "candle_lighting_minutes_before_sunset"
+CONF_HAVDALAH_OFFSET_MINUTES: Final = "havdalah_minutes_after_sunset"
 
-DEFAULT_NAME = "Jewish Calendar"
-DEFAULT_CANDLE_LIGHT = 18
-DEFAULT_DIASPORA = False
-DEFAULT_HAVDALAH_OFFSET_MINUTES = 0
-DEFAULT_LANGUAGE = "english"
+DEFAULT_NAME: Final = "Jewish Calendar"
+DEFAULT_CANDLE_LIGHT: Final = 18
+DEFAULT_DIASPORA: Final = False
+DEFAULT_HAVDALAH_OFFSET_MINUTES: Final = 0
+DEFAULT_LANGUAGE: Final = "english"
