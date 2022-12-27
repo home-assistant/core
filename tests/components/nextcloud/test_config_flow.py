56aa20d1d441110cb5a1a2e09284824fef993e06
Peter Leipe <peter@mail-l.de>
2022-12-27 12:31:04 +0000

    Adding tests for config_flow

--- tests/components/nextcloud/test_config_flow.py
+++ tests/components/nextcloud/test_config_flow.py
@@ -1,112 +1,199 @@
 """Test the Nextcloud config flow."""
-from datetime import timedelta
 from unittest.mock import patch
 
-from homeassistant import config_entries
-from homeassistant.components.nextcloud.config_flow import (
-    NextcloudMonitorError,
-    UninitializedObject,
-)
+from homeassistant import config_entries, setup
+from homeassistant.components.nextcloud import NextcloudMonitorError
 from homeassistant.components.nextcloud.const import DOMAIN
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_PASSWORD,
+    CONF_URL,
+    CONF_USERNAME,
+    CONF_VERIFY_SSL,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
 
-async def test_form(hass: HomeAssistant) -> None:
-    """Test we get the form."""
-    result = await hass.config_entries.flow.async_init(
+def _patch_setup():
+    return patch("homeassistant.components.nextcloud.async_setup", return_value=True)
+
+
+def _patch_tryConnect():
+    return patch(
+        "homeassistant.components.nextcloud.config_flow.NextCloudFlowHandler._async_try_connect",
+        return_value=True,
+    )
+
+
+def _initiate_flow(hass: HomeAssistant):
+    setup.async_setup_component(hass, "persistent_notification", {})
+    result = hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
-    assert result["type"] == FlowResultType.FORM
-    assert result["errors"] is None
+    return result
 
-    with patch("homeassistant.components.nextcloud.config_flow.NextCloud.setup"), patch(
-        "homeassistant.components.nextcloud.config_flow.NextCloud.update"
+
+async def test_successful_connection(hass: HomeAssistant):
+    """This test patches the connection test to simulate a successful connection to the nextclodu API."""
+    result = await _initiate_flow(hass)
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.nextcloud.async_setup", return_value=True
     ), patch(
-        "homeassistant.components.nextcloud.async_setup_entry",
+        "homeassistant.components.nextcloud.config_flow.NextCloudFlowHandler._async_try_connect",
         return_value=True,
-    ) as mock_setup_entry:
+    ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "url": "localhost",
-                "username": "test-username",
-                "password": "test-password",
-                "scan_interval": timedelta(seconds=60),
-                "verify_ssl": True,
+                CONF_NAME: "Nextcloud",
+                CONF_URL: "test-url",
+                CONF_USERNAME: "test-user",
+                CONF_PASSWORD: "test-password",
+                CONF_VERIFY_SSL: True,
             },
         )
         await hass.async_block_till_done()
 
-    assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Nextcloud"
-    assert result2["data"] == {
-        "url": "localhost",
-        "username": "test-username",
-        "password": "test-password",
-        "scan_interval": timedelta(seconds=60),
-        "verify_ssl": True,
-    }
-    assert len(mock_setup_entry.mock_calls) == 1
+        assert result2["type"] == FlowResultType.CREATE_ENTRY
+        assert result2["data"] == {
+            CONF_NAME: "Nextcloud",
+            CONF_URL: "test-url",
+            CONF_USERNAME: "test-user",
+            CONF_PASSWORD: "test-password",
+            CONF_VERIFY_SSL: True,
+        }
 
 
-async def test_form_invalid_setup(hass: HomeAssistant) -> None:
-    """Test we handle invalid setup."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
+async def test_connection_method(hass: HomeAssistant):
+    """This test patches the NextcloudMonitorWrapper class to test the connection test method."""
+    result = await _initiate_flow(hass)
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.nextcloud.config_flow.NextCloud.setup",
+        "homeassistant.components.nextcloud.async_setup", return_value=True
+    ), patch(
+        "homeassistant.components.nextcloud.config_flow.NextCloudFlowHandler._async_endpoint_existed",
+        return_value=False,
+    ), patch(
+        "homeassistant.components.nextcloud.config_flow.NextcloudMonitorWrapper",
+        return_value=None,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_NAME: "Nextcloud",
+                CONF_URL: "test-url",
+                CONF_USERNAME: "test-user",
+                CONF_PASSWORD: "test-password",
+                CONF_VERIFY_SSL: True,
+            },
+        )
+        await hass.async_block_till_done()
+
+        assert result2["type"] == FlowResultType.CREATE_ENTRY
+        assert result2["data"] == {
+            CONF_NAME: "Nextcloud",
+            CONF_URL: "test-url",
+            CONF_USERNAME: "test-user",
+            CONF_PASSWORD: "test-password",
+            CONF_VERIFY_SSL: True,
+        }
+
+    result = await _initiate_flow(hass)
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.nextcloud.async_setup", return_value=True
+    ), patch(
+        "homeassistant.components.nextcloud.config_flow.NextCloudFlowHandler._async_endpoint_existed",
+        return_value=False,
+    ), patch(
+        "homeassistant.components.nextcloud.NextcloudMonitorWrapper",
+        return_value=None,
         side_effect=NextcloudMonitorError,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "url": "localhost",
-                "username": "test-username",
-                "password": "test-password",
-                "scan_interval": timedelta(seconds=60),
-                "verify_ssl": True,
-            },
-        )
-
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"] == {"base": "cannot_connect"}
-
-
-async def test_form_invalid_update(hass: HomeAssistant) -> None:
-    """Test we handle invalid update."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.nextcloud.config_flow.NextCloud.setup",
-    ), patch(
-        "homeassistant.components.nextcloud.config_flow.NextCloud.update",
-        side_effect=UninitializedObject,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "url": "localhost",
-                "username": "test-username",
-                "password": "test-password",
-                "scan_interval": timedelta(seconds=60),
-                "verify_ssl": True,
+                CONF_NAME: "Nextcloud",
+                CONF_URL: "test-url",
+                CONF_USERNAME: "test-user",
+                CONF_PASSWORD: "test-password",
+                CONF_VERIFY_SSL: True,
             },
         )
         await hass.async_block_till_done()
-    assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Nextcloud"
-    assert result2["data"] == {
-        "url": "localhost",
-        "username": "test-username",
-        "password": "test-password",
-        "scan_interval": timedelta(seconds=60),
-        "verify_ssl": True,
-    }
+        assert "data" not in result2.keys()
 
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"] == {"base": "cannot_connect"}
+
+async def test_duplicated_entry(hass: HomeAssistant):
+    """This test the NextcloudMonitorWrapper class to test the connection test method."""
+
+    # first entry must be successful
+    result = await _initiate_flow(hass)
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.nextcloud.async_setup", return_value=True
+    ), patch(
+        "homeassistant.components.nextcloud.config_flow.NextcloudMonitorWrapper",
+        return_value=None,
+    ):
+
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_NAME: "Nextcloud",
+                CONF_URL: "test-url",
+                CONF_USERNAME: "test-user",
+                CONF_PASSWORD: "test-password",
+                CONF_VERIFY_SSL: True,
+            },
+        )
+        await hass.async_block_till_done()
+
+        assert result2["type"] == FlowResultType.CREATE_ENTRY
+        assert result2["data"] == {
+            CONF_NAME: "Nextcloud",
+            CONF_URL: "test-url",
+            CONF_USERNAME: "test-user",
+            CONF_PASSWORD: "test-password",
+            CONF_VERIFY_SSL: True,
+        }
+
+    # second entry must fail
+    result = await _initiate_flow(hass)
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
+    result = await _initiate_flow(hass)
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.nextcloud.async_setup", return_value=True
+    ), patch(
+        "homeassistant.components.nextcloud.config_flow.NextcloudMonitorWrapper",
+        return_value=None,
+    ):
+
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_NAME: "Nextcloud",
+                CONF_URL: "test-url",
+                CONF_USERNAME: "test-user",
+                CONF_PASSWORD: "test-password",
+                CONF_VERIFY_SSL: True,
+            },
+        )
+        await hass.async_block_till_done()
+
+        assert result2["type"] == FlowResultType.FORM
+        assert "data" not in result2.keys()
