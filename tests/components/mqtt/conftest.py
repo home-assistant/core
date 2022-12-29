ee66ffc8deaa6d383becc60c0418f63a7cfa4dc9
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2022-12-29 18:29:28 +0100

    Do not depend MQTT CI tests on debug logs (#84783)
    * Do not depend MQTT CI tests on debug logs
    
    * Leave Clean up expire as debug message

--- tests/components/mqtt/conftest.py
+++ tests/components/mqtt/conftest.py
@@ -1,7 +1,3 @@
 """Test fixtures for mqtt component."""
-import logging
-
 from tests.components.blueprint.conftest import stub_blueprint_populate  # noqa: F401
 from tests.components.light.conftest import mock_light_profiles  # noqa: F401
-
-logging.basicConfig(level=logging.DEBUG)
