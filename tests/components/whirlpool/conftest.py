3d168a3e54c8d048c0f2e809afb48f07b667bb2c
mkmer <mike.j.kasper@gmail.com>
2022-12-30 13:19:32 +0000

    Default test

--- tests/components/whirlpool/conftest.py
+++ tests/components/whirlpool/conftest.py
@@ -114,7 +114,6 @@ def side_effect_function(*args, **kwargs):
         return "0"
     if args[0] == "WashCavity_OpStatusBulkDispense1Level":
         return "3"
-    return "3"
 
 
 def get_sensor_mock(said):
