1be98eae1f9f58ea23697fb94e0713132a021ee9
mkmer <mike.j.kasper@gmail.com>
2022-12-30 15:12:05 +0000

    restore brand selection

--- homeassistant/components/whirlpool/__init__.py
+++ homeassistant/components/whirlpool/__init__.py
@@ -6,7 +6,7 @@ import logging
 import aiohttp
 from whirlpool.appliancesmanager import AppliancesManager
 from whirlpool.auth import Auth
-from whirlpool.backendselector import BackendSelector, Brand
+from whirlpool.backendselector import BackendSelector
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_REGION, CONF_USERNAME, Platform
@@ -14,6 +14,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
 
 from .const import CONF_REGIONS_MAP, DOMAIN
+from .util import get_brand_for_region
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,11 +25,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Whirlpool Sixth Sense from a config entry."""
     hass.data.setdefault(DOMAIN, {})
 
-    region = entry.data.get(CONF_REGION, "EU")
-    brand = Brand.Whirlpool
-    if region == "US":
-        brand = Brand.Maytag
-    backend_selector = BackendSelector(brand, CONF_REGIONS_MAP[region])
+    region = CONF_REGIONS_MAP[entry.data.get(CONF_REGION, "EU")]
+    brand = get_brand_for_region(region)
+    backend_selector = BackendSelector(brand, region)
     auth = Auth(backend_selector, entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD])
     try:
         await auth.do_auth(store=False)
