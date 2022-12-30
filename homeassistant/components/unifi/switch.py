9718ff43a9e63dda1896a54aa1982a25e8db20f5
Robert Svensson <Kane610@users.noreply.github.com>
2022-12-31 00:23:00 +0100

    Use controller.register_platform_add_entities

--- homeassistant/components/unifi/switch.py
+++ homeassistant/components/unifi/switch.py
@@ -249,34 +249,9 @@ async def async_setup_entry(
             client = controller.api.clients_all[mac]
             controller.api.clients.process_raw([client.raw])
 
-    @callback
-    def async_load_entities(description: UnifiSwitchEntityDescription) -> None:
-        """Load and subscribe to UniFi devices."""
-        entities: list[SwitchEntity] = []
-        api_handler = description.api_handler_fn(controller.api)
-
-        @callback
-        def async_create_entity(event: ItemEvent, obj_id: str) -> None:
-            """Create UniFi entity."""
-            if not description.allowed_fn(
-                controller, obj_id
-            ) or not description.supported_fn(controller, obj_id):
-                return
-
-            entity = UnifiSwitchEntity(obj_id, controller, description)
-            if event == ItemEvent.ADDED:
-                async_add_entities([entity])
-                return
-            entities.append(entity)
-
-        for obj_id in api_handler:
-            async_create_entity(ItemEvent.CHANGED, obj_id)
-        async_add_entities(entities)
-
-        api_handler.subscribe(async_create_entity, ItemEvent.ADDED)
-
-    for description in ENTITY_DESCRIPTIONS:
-        async_load_entities(description)
+    controller.register_platform_add_entities(
+        UnifiSwitchEntity, ENTITY_DESCRIPTIONS, async_add_entities
+    )
 
 
 class UnifiSwitchEntity(UnifiEntity[HandlerT, DataT], SwitchEntity):
