9f553639e36fc28544867ec29104e35877754149
kvanzuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2022-12-31 01:41:35 +0100

    Fixed integrations.json and __init__.py

--- homeassistant/components/zeversolar/__init__.py
+++ homeassistant/components/zeversolar/__init__.py
@@ -5,12 +5,12 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, PLATFORMS
-from .coordinator import ZeverSolarCoordinator
+from .coordinator import ZeversolarCoordinator
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Zeversolar from a config entry."""
-    coordinator = ZeverSolarCoordinator(hass=hass, entry=entry)
+    coordinator = ZeversolarCoordinator(hass=hass, entry=entry)
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
