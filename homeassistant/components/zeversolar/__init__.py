11e652c0b3cdad65e67ce0cd8469059bc61295b1
kvanzuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2022-12-31 01:05:50 +0100

    Initial integration for zeversolar

---
+++ homeassistant/components/zeversolar/__init__.py
@@ -0,0 +1,25 @@
+"""The zeversolar integration."""
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN, PLATFORMS
+from .coordinator import ZeverSolarCoordinator
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up zeversolar from a config entry."""
+    coordinator = ZeverSolarCoordinator(hass=hass, entry=entry)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
---
+++ homeassistant/components/zeversolar/entity.py
@@ -0,0 +1,33 @@
+"""Base Entity for ZeverSolar sensors."""
+from __future__ import annotations
+
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN
+from .coordinator import ZeverSolarCoordinator
+
+
+class ZeverSolarEntity(
+    CoordinatorEntity[ZeverSolarCoordinator],
+):
+    """Defines a base ZeverSolar entity."""
+
+    def __init__(
+        self,
+        *,
+        device_id: str,
+        coordinator: ZeverSolarCoordinator,
+    ) -> None:
+        """Initialize the ZeverSolar entity."""
+        super().__init__(coordinator=coordinator)
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, device_id)},
+            name="ZeverSolar Sensor",
+            manufacturer="ZeverSolar",
+        )
+
+    @property
+    def available(self) -> bool:
+        """Return device availability."""
+        return super().available
