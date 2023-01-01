7d8697ef7fd0704671f20384470d7ff86dd9d000
kolaf <frankose@ifi.uio.no>
2023-01-01 14:27:21 +0000

    Most binary sensors are working

--- homeassistant/components/homely/__init__.py
+++ homeassistant/components/homely/__init__.py
@@ -1,27 +1,14 @@
 """The homely integration."""
 from __future__ import annotations
 
-from datetime import timedelta
 import logging
 
-from requests import ConnectTimeout, HTTPError
-
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
-)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-)
-from homelypy.devices import SingleLocation, WindowSensor
-from homelypy.homely import ConnectionFailedException, Homely
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .homely_device import HomelyHome
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -31,18 +18,10 @@ PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up homely from a config entry."""
 
-    username = entry.data["username"]
-    password = entry.data["password"]
-    location_id = entry.data["location_id"]
-
-    try:
-        homely = Homely(username, password)
-        await hass.async_add_executor_job(homely.get_location, location_id)
-    except (ConnectionFailedException, ConnectTimeout, HTTPError) as ex:
-        raise ConfigEntryNotReady(f"Unable to connect to Homely: {ex}") from ex
-
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = homely
+    homely_home = HomelyHome(hass, entry)
+    await homely_home.setup()
+    hass.data[DOMAIN][entry.entry_id] = homely_home
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
@@ -67,49 +46,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
-
-
-class PollingDataCoordinator(DataUpdateCoordinator):
-    """Homely polling data coordinator."""
-
-    def __init__(
-        self, hass: HomeAssistant, homely: Homely, location: SingleLocation
-    ) -> None:
-        """Initialise homely connection."""
-        super().__init__(
-            hass,
-            _LOGGER,
-            name=f"Homely {location.name}",
-            update_interval=timedelta(minutes=5),
-        )
-        self.homely = homely
-        self.location = location
-        self.added_sensors: set[str] = set()
-
-    async def _async_update_data(self) -> None:
-        self.location = await self.hass.async_add_executor_job(
-            self.homely.get_location, self.location.location_id
-        )
-
-
-class WindowSensorEntity(CoordinatorEntity, BinarySensorEntity):
-    """Homely window sensor."""
-
-    _attr_device_class = BinarySensorDeviceClass.DOOR
-
-    def __init__(self, coordinator, device_id):
-        """Pass coordinator to CoordinatorEntity."""
-        super().__init__(coordinator)
-        self.device_id = device_id
-
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        """Handle updated data from the coordinator."""
-        device: WindowSensor = next(
-            filter(
-                lambda device: (device.id == self.device_id),
-                self.coordinator.location.devices,
-            )
-        )
-        self._attr_is_on = device.alarm.alarm
-        self.async_write_ha_state()
--- homeassistant/components/homely/binary_sensor.py
+++ homeassistant/components/homely/binary_sensor.py
@@ -1,9 +1,5 @@
 """Sensors provided by Homely."""
-from datetime import timedelta
 import logging
-
-from httpcore import ConnectTimeout
-from httpx import HTTPError
 
 from homeassistant.components.binary_sensor import (
     BinarySensorDeviceClass,
@@ -11,17 +7,15 @@ from homeassistant.components.binary_sensor import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.exceptions import PlatformNotReady
-from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
 )
-from homelypy.devices import SingleLocation, WindowSensor
-from homelypy.homely import ConnectionFailedException, Homely
+from homelypy.devices import Device, MotionSensorMini, SmokeAlarm, State, WindowSensor
 
 from .const import DOMAIN
+from .homely_device import HomelyDevice, HomelyHome
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,82 +24,90 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up Plate Relays as switch based on a config entry."""
-    homely = hass.data[DOMAIN][entry.entry_id]
-    location_id = entry.data["location_id"]
+    homely_home: HomelyHome = hass.data[DOMAIN][entry.entry_id]
 
-    try:
-        location = await hass.async_add_executor_job(homely.get_location, location_id)
-    except (ConnectionFailedException, ConnectTimeout, HTTPError) as ex:
-        raise PlatformNotReady(f"Unable to connect to Homely: {ex}") from ex
-
-    coordinator = PollingDataCoordinator(hass, homely, location)
+    coordinator = homely_home.coordinator
     await coordinator.async_config_entry_first_refresh()
 
-    entities = []
-    for device in coordinator.location.devices:
-        if device.id not in coordinator.added_sensors:
-            coordinator.added_sensors.add(device.id)
-            print(f"Reviewing device {device}")
-            if isinstance(device, WindowSensor):
-                print("This is a Window sensor")
-                entities.append(WindowSensorEntity(coordinator, device))
+    entities: list[BinarySensorEntity] = []
+    for homely_device in homely_home.devices.values():
+        if isinstance(homely_device.homely_api_device, WindowSensor):
+            entities.append(WindowSensorEntity(coordinator, homely_device))
+        elif isinstance(homely_device.homely_api_device, SmokeAlarm):
+            entities.append(SmokeAlarmEntity(coordinator, homely_device))
+        elif isinstance(homely_device.homely_api_device, MotionSensorMini):
+            entities.append(MotionSensorEntity(coordinator, homely_device))
+        entities.append(BatteryLowEntity(coordinator, homely_device))
     async_add_entities(entities)
 
 
-class PollingDataCoordinator(DataUpdateCoordinator):
-    """Homely polling data coordinator."""
+class HomelyBinarySensorEntity(CoordinatorEntity, BinarySensorEntity):
+    """Abstract binary sensor class."""
+
+    _attr_name = "Basic"
 
     def __init__(
-        self, hass: HomeAssistant, homely: Homely, location: SingleLocation
+        self,
+        coordinator: DataUpdateCoordinator,
+        homely_device: HomelyDevice,
     ) -> None:
-        """Initialise homely connection."""
-        super().__init__(
-            hass,
-            _LOGGER,
-            name=f"Homely {location.name}",
-            update_interval=timedelta(minutes=5),
-        )
-        self.homely = homely
-        self.location = location
-        self.added_sensors: set[str] = set()
-
-    async def _async_update_data(self) -> None:
-        self.location = await self.hass.async_add_executor_job(
-            self.homely.get_location, self.location.location_id
-        )
-
-
-class WindowSensorEntity(CoordinatorEntity, BinarySensorEntity):
-    """Homely window sensor."""
-
-    _attr_device_class = BinarySensorDeviceClass.DOOR
-
-    def __init__(self, coordinator, device: WindowSensor):
         """Pass coordinator to CoordinatorEntity."""
         super().__init__(coordinator)
-        self.device = device
-        self._attr_name = device.name
-        self._attr_is_on = device.alarm.alarm
-        self._attr_unique_id = f"{DOMAIN}_{device.id}"
-
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return the device info."""
-        return DeviceInfo(
-            identifiers={(DOMAIN, self.unique_id)},  # type: ignore[arg-type]
-            name=f"{self.device.location} - {self.device.name}",
-            manufacturer="",
-            model=self.device.model_name,
+        self.homely_device = homely_device
+        self._attr_is_on = self.get_is_on_from_device(
+            self.homely_device.homely_api_device
         )
+        self._attr_device_info = homely_device.device_info
+        self._attr_unique_id = f"{homely_device.homely_api_device.id}_{self._attr_name}"
+
+    def get_is_on_from_device(self, device: Device) -> bool:
+        """Get the current state of the sensor."""
+        return device.alarm.alarm
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-        device: WindowSensor = next(
+        device: State = next(
             filter(
-                lambda device: (device.id == self.device.id),
+                lambda device: (device.id == self.homely_device.homely_api_device.id),
                 self.coordinator.location.devices,
             )
         )
-        self._attr_is_on = device.alarm.alarm
+        self._attr_is_on = self.get_is_on_from_device(device)
         self.async_write_ha_state()
+
+
+class WindowSensorEntity(HomelyBinarySensorEntity):
+    """Represent a window sensor."""
+
+    _attr_name = "Window"
+    _attr_device_class = BinarySensorDeviceClass.WINDOW
+
+
+class MotionSensorEntity(HomelyBinarySensorEntity):
+    """Represent a motion sensor."""
+
+    _attr_name = "Motion"
+    _attr_device_class = BinarySensorDeviceClass.MOTION
+
+
+class SmokeAlarmEntity(HomelyBinarySensorEntity):
+    """Represent a smoke alarm."""
+
+    _attr_name = "Smoke"
+    _attr_device_class = BinarySensorDeviceClass.SMOKE
+
+    def get_is_on_from_device(self, device: Device) -> bool:
+        """Get the current state of the sensor."""
+        return device.alarm.fire
+
+
+class BatteryLowEntity(HomelyBinarySensorEntity):
+    """Represent battery low state."""
+
+    _attr_name = "Battery low"
+    _attr_device_class = BinarySensorDeviceClass.BATTERY
+
+    def get_is_on_from_device(self, device: Device) -> bool:
+        """Get the current state of the sensor."""
+        return device.battery.low
