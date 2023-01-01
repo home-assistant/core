ae52c6f11777061c3a38ae706d62d825a9fc9465
starkillerOG <starkiller.og@gmail.com>
2023-01-01 23:32:17 +0100

    Process late feedback for Reolink (#84884)
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/reolink/const.py
+++ homeassistant/components/reolink/const.py
@@ -1,13 +1,9 @@
 """Constants for the Reolink Camera integration."""
 
 DOMAIN = "reolink"
-PLATFORMS = ["camera"]
 
 CONF_USE_HTTPS = "use_https"
 CONF_PROTOCOL = "protocol"
 
 DEFAULT_PROTOCOL = "rtsp"
 DEFAULT_TIMEOUT = 60
-
-HOST = "host"
-DEVICE_UPDATE_INTERVAL = 60
