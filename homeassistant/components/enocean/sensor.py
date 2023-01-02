8f4926a6bc2e89f8f31829dd52f8aa22c2abea64
kridgo <110250066+kridgo@users.noreply.github.com>
2023-01-02 19:34:14 +0100

    Add support for shutter contacts (EEP D5-00-01)

--- homeassistant/components/enocean/sensor.py
+++ homeassistant/components/enocean/sensor.py
@@ -43,6 +43,7 @@ SENSOR_TYPE_HUMIDITY = "humidity"
 SENSOR_TYPE_POWER = "powersensor"
 SENSOR_TYPE_TEMPERATURE = "temperature"
 SENSOR_TYPE_WINDOWHANDLE = "windowhandle"
+SENSOR_TYPE_SHUTTERCONTACT = "shuttercontact"
 
 
 @dataclass
@@ -96,6 +97,13 @@ SENSOR_DESC_WINDOWHANDLE = EnOceanSensorEntityDescription(
     unique_id=lambda dev_id: f"{combine_hex(dev_id)}-{SENSOR_TYPE_WINDOWHANDLE}",
 )
 
+SENSOR_DESC_SHUTTERCONTACT = EnOceanSensorEntityDescription(
+    key=SENSOR_TYPE_SHUTTERCONTACT,
+    name="ShutterContact",
+    icon="mdi:window-open-variant",
+    unique_id=lambda dev_id: f"{combine_hex(dev_id)}-{SENSOR_TYPE_SHUTTERCONTACT}",
+)
+
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
@@ -147,6 +155,9 @@ def setup_platform(
 
     elif sensor_type == SENSOR_TYPE_WINDOWHANDLE:
         entities = [EnOceanWindowHandle(dev_id, dev_name, SENSOR_DESC_WINDOWHANDLE)]
+
+    elif sensor_type == SENSOR_TYPE_SHUTTERCONTACT:
+        entities = [EnOceanShutterContact(dev_id, dev_name, SENSOR_DESC_SHUTTERCONTACT)]
 
     add_entities(entities)
 
@@ -279,5 +290,23 @@ class EnOceanWindowHandle(EnOceanSensor):
             self._attr_native_value = STATE_OPEN
         if action == 0x05:
             self._attr_native_value = "tilt"
+
+        self.schedule_update_ha_state()
+
+class EnOceanShutterContact(EnOceanSensor):
+    """Representation of an EnOcean shutter contact device.
+    
+    EEPs (EnOcean Equipment Profiles):
+    - D5-00-01 (Shutter contact)
+    """
+
+    def value_changed(self, packet):
+        """Update the internal state of the sensor."""
+        if packet.rorg != 0xD5:
+            return
+        if packet.data[1] == 0x09:
+            self._attr_native_value = STATE_CLOSED
+        if packet.data[1] == 0x08:
+            self._attr_native_value = STATE_OPEN
 
         self.schedule_update_ha_state()
