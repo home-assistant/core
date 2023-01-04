689e92e55a3c81060dd7933b8768faa0dc0348e6
Lewis England <lewis2004@hotmail.com>
2023-01-04 13:32:10 +0000

    test: Use the Calendar API to test Sonarr calendar entity

--- tests/components/sonarr/test_calendar.py
+++ tests/components/sonarr/test_calendar.py
@@ -1,7 +1,6 @@
 """Test the Sonarr calendar entity."""
-from unittest.mock import AsyncMock, MagicMock
+from http import HTTPStatus
 
-from homeassistant.components.sonarr.calendar import get_sonarr_episode_events
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
@@ -12,32 +11,30 @@ DOMAIN = "sonarr"
 
 async def test_calendar(
     hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_sonarr: MagicMock,
-    entity_registry_enabled_by_default: AsyncMock,
+    init_integration: MockConfigEntry,
+    hass_client,
 ) -> None:
-    """Test the creation and events in the calendar."""
-    mock_config_entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(mock_config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    state = hass.states.get(CALENDAR_ID)
-    assert state
-    assert state.name == "Sonarr Episodes"
-    assert state.state == "off"
+    """Test the API returns the Sonarr calendar."""
+    client = await hass_client()
+    response = await client.get("/api/calendars")
+    assert response.status == HTTPStatus.OK
+    data = await response.json()
+    assert data == [
+        {
+            "entity_id": "calendar.sonarr_episodes",
+            "name": "Sonarr Episodes",
+        }
+    ]
 
 
 async def test_get_episodes(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_sonarr: MagicMock,
-    entity_registry_enabled_by_default: AsyncMock,
+    hass: HomeAssistant, init_integration: MockConfigEntry, hass_client
 ) -> None:
     """Test data is extracted from the coordinator."""
-    mock_config_entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(mock_config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    coordinator = hass.data[DOMAIN][mock_config_entry.entry_id]["upcoming"]
-    episodes = await get_sonarr_episode_events(coordinator)
-    assert len(episodes) == 1
+    client = await hass_client()
+    response = await client.get(
+        "/api/calendars/calendar.sonarr_episodes?start=2014-01-26&end=2014-01-28"
+    )
+    assert response.status == HTTPStatus.OK
+    events = await response.json()
+    assert len(events) == 1
