82132bce853238811fbabc536a02b7d336e6c6fd
Guido Schmitz <guido.schmitz@fedaix.de>
2023-01-04 22:07:39 +0000

    Bump devolo_plc_api to 1.0.0

--- homeassistant/components/devolo_home_network/const.py
+++ homeassistant/components/devolo_home_network/const.py
@@ -2,12 +2,18 @@
 
 from datetime import timedelta
 
+from devolo_plc_api.device_api import (
+    WIFI_BAND_2G,
+    WIFI_BAND_5G,
+    WIFI_VAP_GUEST_AP,
+    WIFI_VAP_MAIN_AP,
+)
+
 from homeassistant.const import Platform
 
 DOMAIN = "devolo_home_network"
 PLATFORMS = [Platform.BINARY_SENSOR, Platform.DEVICE_TRACKER, Platform.SENSOR]
 
-MAC_ADDRESS = "mac_address"
 PRODUCT = "product"
 SERIAL_NUMBER = "serial_number"
 TITLE = "title"
@@ -16,16 +22,15 @@ LONG_UPDATE_INTERVAL = timedelta(minutes=5)
 SHORT_UPDATE_INTERVAL = timedelta(seconds=15)
 
 CONNECTED_PLC_DEVICES = "connected_plc_devices"
-CONNECTED_STATIONS = "connected_stations"
 CONNECTED_TO_ROUTER = "connected_to_router"
 CONNECTED_WIFI_CLIENTS = "connected_wifi_clients"
 NEIGHBORING_WIFI_NETWORKS = "neighboring_wifi_networks"
 
 WIFI_APTYPE = {
-    "WIFI_VAP_MAIN_AP": "Main",
-    "WIFI_VAP_GUEST_AP": "Guest",
+    WIFI_VAP_MAIN_AP: "Main",
+    WIFI_VAP_GUEST_AP: "Guest",
 }
 WIFI_BANDS = {
-    "WIFI_BAND_2G": 2.4,
-    "WIFI_BAND_5G": 5,
+    WIFI_BAND_2G: 2.4,
+    WIFI_BAND_5G: 5,
 }
