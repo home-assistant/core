c0a409efe1213fe185f19fbec2483c1ec2ff0e99
mkmer <mike.j.kasper@gmail.com>
2023-01-04 14:22:41 +0000

    Seperate washer tests Add restore_state test

--- tests/components/whirlpool/test_sensor.py
+++ tests/components/whirlpool/test_sensor.py
@@ -1,13 +1,16 @@
 """Test the Whirlpool Sensor domain."""
+from datetime import datetime, timezone
 from unittest.mock import MagicMock
 
 from attr import dataclass
 from whirlpool.washerdryer import MachineState
 
-from homeassistant.core import HomeAssistant
+from homeassistant.core import CoreState, HomeAssistant, State
 from homeassistant.helpers import entity_registry
 
 from . import init_integration
+
+from tests.common import mock_restore_cache_with_extra_data
 
 
 async def update_sensor_state(
@@ -37,12 +40,48 @@ def side_effect_function_open_door(*args, **kwargs):
         return "3"
 
 
-async def test_sensor_values(
+async def test_combined_sensor_values(
     hass: HomeAssistant,
     mock_sensor_api_instances: MagicMock,
     mock_sensor1_api: MagicMock,
     mock_sensor2_api: MagicMock,
-    # mock_sensor_api: MagicMock,
+):
+    """Test the sensor value callbacks."""
+    await init_integration(hass)
+
+    @dataclass
+    class SensorTestInstance:
+        """Helper class for multiple washer/dryer and mock instances."""
+
+        entity_id: str
+        mock_instance: MagicMock
+        mock_instance_idx: int
+
+    for sensor_test_instance in (
+        SensorTestInstance("sensor.washer_state", mock_sensor1_api, 0),
+        SensorTestInstance("sensor.dryer_state", mock_sensor2_api, 1),
+    ):
+        entity_id = sensor_test_instance.entity_id
+        mock_instance = sensor_test_instance.mock_instance
+        registry = entity_registry.async_get(hass)
+        entry = registry.async_get(entity_id)
+        assert entry
+        state = hass.states.get(entity_id)
+        assert state is not None
+        assert state.state == "Standby"
+
+        state = await update_sensor_state(hass, entity_id, mock_instance)
+        assert state is not None
+        state_id = f"{entity_id.split('_')[0]}_end_time"
+        state = hass.states.get(state_id)
+        assert state is not None
+
+
+async def test_washer_sensor_values(
+    hass: HomeAssistant,
+    mock_sensor_api_instances: MagicMock,
+    mock_sensor1_api: MagicMock,
+    mock_sensor2_api: MagicMock,
 ):
     """Test the sensor value callbacks."""
     await init_integration(hass)
@@ -57,11 +96,9 @@ async def test_sensor_values(
 
     for sensor_test_instance in (
         SensorTestInstance("sensor.washer_state", mock_sensor1_api, 0),
-        SensorTestInstance("sensor.dryer_state", mock_sensor2_api, 1),
     ):
         entity_id = sensor_test_instance.entity_id
         mock_instance = sensor_test_instance.mock_instance
-        mock_instance_idx = sensor_test_instance.mock_instance_idx
         registry = entity_registry.async_get(hass)
         entry = registry.async_get(entity_id)
         assert entry
@@ -80,101 +117,134 @@ async def test_sensor_values(
         assert state is not None
         assert state.state == "50%"
 
-        if mock_instance_idx == 0:
-            # Test the washer cycle states
-            mock_instance.get_machine_state.return_value = MachineState.RunningMainCycle
-            mock_instance.get_cycle_status_filling.return_value = True
-            mock_instance.attr_value_to_bool.side_effect = [
-                True,
-                False,
-                False,
-                False,
-                False,
-                False,
-            ]
+        # Test the washer cycle states
+        mock_instance.get_machine_state.return_value = MachineState.RunningMainCycle
+        mock_instance.get_cycle_status_filling.return_value = True
+        mock_instance.attr_value_to_bool.side_effect = [
+            True,
+            False,
+            False,
+            False,
+            False,
+            False,
+        ]
 
-            state = await update_sensor_state(hass, entity_id, mock_instance)
-            assert state is not None
-            assert state.state == "Cycle Filling"
+        state = await update_sensor_state(hass, entity_id, mock_instance)
+        assert state is not None
+        assert state.state == "Cycle Filling"
 
-            mock_instance.get_cycle_status_filling.return_value = False
-            mock_instance.get_cycle_status_rinsing.return_value = True
-            mock_instance.attr_value_to_bool.side_effect = [
-                False,
-                True,
-                False,
-                False,
-                False,
-                False,
-            ]
+        mock_instance.get_cycle_status_filling.return_value = False
+        mock_instance.get_cycle_status_rinsing.return_value = True
+        mock_instance.attr_value_to_bool.side_effect = [
+            False,
+            True,
+            False,
+            False,
+            False,
+            False,
+        ]
 
-            state = await update_sensor_state(hass, entity_id, mock_instance)
-            assert state is not None
-            assert state.state == "Cycle Rinsing"
+        state = await update_sensor_state(hass, entity_id, mock_instance)
+        assert state is not None
+        assert state.state == "Cycle Rinsing"
 
-            mock_instance.get_cycle_status_rinsing.return_value = False
-            mock_instance.get_cycle_status_sensing.return_value = True
-            mock_instance.attr_value_to_bool.side_effect = [
-                False,
-                False,
-                True,
-                False,
-                False,
-                False,
-            ]
+        mock_instance.get_cycle_status_rinsing.return_value = False
+        mock_instance.get_cycle_status_sensing.return_value = True
+        mock_instance.attr_value_to_bool.side_effect = [
+            False,
+            False,
+            True,
+            False,
+            False,
+            False,
+        ]
 
-            state = await update_sensor_state(hass, entity_id, mock_instance)
-            assert state is not None
-            assert state.state == "Cycle Sensing"
+        state = await update_sensor_state(hass, entity_id, mock_instance)
+        assert state is not None
+        assert state.state == "Cycle Sensing"
 
-            mock_instance.get_cycle_status_sensing.return_value = False
-            mock_instance.get_cycle_status_soaking.return_value = True
-            mock_instance.attr_value_to_bool.side_effect = [
-                False,
-                False,
-                False,
-                True,
-                False,
-                False,
-            ]
+        mock_instance.get_cycle_status_sensing.return_value = False
+        mock_instance.get_cycle_status_soaking.return_value = True
+        mock_instance.attr_value_to_bool.side_effect = [
+            False,
+            False,
+            False,
+            True,
+            False,
+            False,
+        ]
 
-            state = await update_sensor_state(hass, entity_id, mock_instance)
-            assert state is not None
-            assert state.state == "Cycle Soaking"
+        state = await update_sensor_state(hass, entity_id, mock_instance)
+        assert state is not None
+        assert state.state == "Cycle Soaking"
 
-            mock_instance.get_cycle_status_soaking.return_value = False
-            mock_instance.get_cycle_status_spinning.return_value = True
-            mock_instance.attr_value_to_bool.side_effect = [
-                False,
-                False,
-                False,
-                False,
-                True,
-                False,
-            ]
+        mock_instance.get_cycle_status_soaking.return_value = False
+        mock_instance.get_cycle_status_spinning.return_value = True
+        mock_instance.attr_value_to_bool.side_effect = [
+            False,
+            False,
+            False,
+            False,
+            True,
+            False,
+        ]
 
-            state = await update_sensor_state(hass, entity_id, mock_instance)
-            assert state is not None
-            assert state.state == "Cycle Spinning"
+        state = await update_sensor_state(hass, entity_id, mock_instance)
+        assert state is not None
+        assert state.state == "Cycle Spinning"
 
-            mock_instance.get_cycle_status_spinning.return_value = False
-            mock_instance.get_cycle_status_washing.return_value = True
-            mock_instance.attr_value_to_bool.side_effect = [
-                False,
-                False,
-                False,
-                False,
-                False,
-                True,
-            ]
+        mock_instance.get_cycle_status_spinning.return_value = False
+        mock_instance.get_cycle_status_washing.return_value = True
+        mock_instance.attr_value_to_bool.side_effect = [
+            False,
+            False,
+            False,
+            False,
+            False,
+            True,
+        ]
 
-            state = await update_sensor_state(hass, entity_id, mock_instance)
-            assert state is not None
-            assert state.state == "Cycle Washing"
+        state = await update_sensor_state(hass, entity_id, mock_instance)
+        assert state is not None
+        assert state.state == "Cycle Washing"
 
-            mock_instance.get_machine_state.return_value = MachineState.Complete
-            mock_instance.attr_value_to_bool.side_effect = None
-            mock_instance.get_attribute.side_effect = side_effect_function_open_door
-            state = await update_sensor_state(hass, entity_id, mock_instance)
-            assert state is not None
-            assert state.state == "Door open"
+        mock_instance.get_machine_state.return_value = MachineState.Complete
+        mock_instance.attr_value_to_bool.side_effect = None
+        mock_instance.get_attribute.side_effect = side_effect_function_open_door
+        state = await update_sensor_state(hass, entity_id, mock_instance)
+        assert state is not None
+        assert state.state == "Door open"
+
+
+async def test_restore_state(
+    hass: HomeAssistant,
+    mock_sensor_api_instances: MagicMock,
+):
+    """Test sensor restore state."""
+    # Home assistant is not running yet
+    hass.state = CoreState.not_running
+    thetimestamp: datetime = datetime(2022, 11, 29, 00, 00, 00, 00, timezone.utc)
+    mock_restore_cache_with_extra_data(
+        hass,
+        (
+            (
+                State(
+                    "sensor.washer_end_time",
+                    "1",
+                ),
+                {"native_value": thetimestamp, "native_unit_of_measurement": None},
+            ),
+            (
+                State("sensor.dryer_end_time", "1"),
+                {"native_value": thetimestamp, "native_unit_of_measurement": None},
+            ),
+        ),
+    )
+
+    # create and add entry
+    await init_integration(hass)
+    # restore from cache
+    state = hass.states.get("sensor.washer_end_time")
+    assert state.state == thetimestamp.isoformat()
+    state = hass.states.get("sensor.dryer_end_time")
+    assert state.state == thetimestamp.isoformat()
