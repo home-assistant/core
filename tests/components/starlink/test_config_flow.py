679facab57122b004e06b885eb142dc6436d66cb
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-01-05 22:12:52 +0000

    Create a test to make sure the flow is aborted when trying to create duplicate entries

--- tests/components/starlink/test_config_flow.py
+++ tests/components/starlink/test_config_flow.py
@@ -1,5 +1,5 @@
 """Test the Starlink config flow."""
-from homeassistant import data_entry_flow
+from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.starlink.const import DOMAIN
 from homeassistant.config_entries import SOURCE_USER
 from homeassistant.const import CONF_IP_ADDRESS
@@ -7,9 +7,11 @@ from homeassistant.core import HomeAssistant
 
 from .patchers import DEVICE_FOUND_PATCHER, NO_DEVICE_PATCHER, SETUP_ENTRY_PATCHER
 
+from tests.common import MockConfigEntry
+
 
 async def test_flow_user_fails_no_dishy(hass: HomeAssistant) -> None:
-    """Test user initialized flow when Starlink is available."""
+    """Test user initialized flow fails when Starlink is available."""
     user_input = {CONF_IP_ADDRESS: "192.168.100.1:9200"}
 
     with NO_DEVICE_PATCHER, SETUP_ENTRY_PATCHER:
@@ -31,7 +33,7 @@ async def test_flow_user_fails_no_dishy(hass: HomeAssistant) -> None:
 
 
 async def test_flow_user_success(hass: HomeAssistant) -> None:
-    """Test user initialized flow when Starlink is available."""
+    """Test user initialized flow succeeds when Starlink is available."""
     user_input = {CONF_IP_ADDRESS: "192.168.100.1:9200"}
 
     with DEVICE_FOUND_PATCHER, SETUP_ENTRY_PATCHER:
@@ -48,3 +50,31 @@ async def test_flow_user_success(hass: HomeAssistant) -> None:
 
     assert result["data"] == user_input
     assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
+
+
+async def test_flow_user_duplicate_abort(hass: HomeAssistant) -> None:
+    """Test user initialized flow aborts when Starlink is already configured."""
+    user_input = {CONF_IP_ADDRESS: "192.168.100.1:9200"}
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data=user_input,
+        unique_id="some-valid-id",
+        state=config_entries.ConfigEntryState.LOADED,
+    )
+    entry.add_to_hass(hass)
+
+    with DEVICE_FOUND_PATCHER, SETUP_ENTRY_PATCHER:
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": SOURCE_USER},
+        )
+        await hass.async_block_till_done()
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input=user_input,
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] == data_entry_flow.FlowResultType.ABORT
+    assert result["reason"] == "already_configured"
