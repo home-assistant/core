9fc3b5910a96d6d5acb571d6b49f0a2c57c601d7
930913 <3722064+930913@users.noreply.github.com>
2023-01-05 14:02:42 +0000

    Update homeassistant/components/roomba/sensor.py
    Co-authored-by: Xitee <59659167+Xitee1@users.noreply.github.com>

--- homeassistant/components/roomba/sensor.py
+++ homeassistant/components/roomba/sensor.py
@@ -108,7 +108,7 @@ class MissionSensor(IRobotEntity, SensorEntity):
     @property
     def unique_id(self):
         """Return the ID of this sensor."""
-        return f"total_missions_{self._blid}"
+        return f"{self._mission_type}_missions_{self._blid}"
 
     @property
     def icon(self):
