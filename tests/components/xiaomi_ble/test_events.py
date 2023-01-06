79dca2f7440ae6d6d9413209f3fc1864da2ac9ac
Ernst79 <e.klamer@gmail.com>
2023-01-06 20:16:46 +0000

    Add test for motion_detected event

--- tests/components/xiaomi_ble/test_events.py
+++ tests/components/xiaomi_ble/test_events.py
@@ -4,11 +4,11 @@ from homeassistant.components.xiaomi_ble.const import DOMAIN
 
 from . import make_advertisement
 
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, async_capture_events
 from tests.components.bluetooth import inject_bluetooth_service_info_bleak
 
 
-async def test_xiaomi_motion_detected(hass):
+async def test_event_motion_detected(hass):
     """Make sure that a motion detected event is fired."""
     entry = MockConfigEntry(
         domain=DOMAIN,
@@ -17,17 +17,22 @@ async def test_xiaomi_motion_detected(hass):
     entry.add_to_hass(hass)
 
     assert await hass.config_entries.async_setup(entry.entry_id)
-    await hass.async_block_till_done()
 
-    assert len(hass.states.async_all()) == 0
+    events = async_capture_events(hass, "xiaomi_ble_event")
 
+    # Emit motion detected event
     inject_bluetooth_service_info_bleak(
         hass,
         make_advertisement("DE:70:E8:B2:39:0C", b"@0\xdd\x03$\x03\x00\x01\x01"),
     )
 
+    # wait for the event
     await hass.async_block_till_done()
-    assert len(hass.states.async_all()) == 0
+    await hass.async_block_till_done()
+    assert len(events) == 1
+    assert events[0].data["address"] == "DE:70:E8:B2:39:0C"
+    assert events[0].data["event_type"] == "motion_detected"
+    assert events[0].data["event_properties"] is None
 
     assert await hass.config_entries.async_unload(entry.entry_id)
     await hass.async_block_till_done()
