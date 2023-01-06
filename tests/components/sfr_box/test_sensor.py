7821d0860c8710549e76cf3de6b4d1cc5e2aa87d
epenet <6771947+epenet@users.noreply.github.com>
2023-01-06 06:21:52 +0000

    renault -> sfr_box

--- tests/components/sfr_box/test_sensor.py
+++ tests/components/sfr_box/test_sensor.py
@@ -21,7 +21,7 @@ pytestmark = pytest.mark.usefixtures("system_get_info", "dsl_get_info")
 @pytest.fixture(autouse=True)
 def override_platforms() -> Generator[None, None, None]:
     """Override PLATFORMS."""
-    with patch("homeassistant.components.renault.PLATFORMS", [Platform.SENSOR]):
+    with patch("homeassistant.components.sfr_box.PLATFORMS", [Platform.SENSOR]):
         yield
 
 
