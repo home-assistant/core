328d0ee5b518a4ef76eab5550f2ecdf1c0ab9a9c
Tom Puttemans <github@tomputtemans.com>
2023-01-06 10:16:43 +0100

    The peak hour consumption doesn't express energy
    Instead of using kWh, the unit of measurement should remain kW

--- homeassistant/components/dsmr_reader/definitions.py
+++ homeassistant/components/dsmr_reader/definitions.py
@@ -561,7 +561,7 @@ SENSORS: tuple[DSMRReaderSensorEntityDescription, ...] = (
         key="dsmr/consumption/quarter-hour-peak-electricity/average_delivered",
         name="Previous quarter-hour peak usage",
         device_class=SensorDeviceClass.ENERGY,
-        native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
+        native_unit_of_measurement=UnitOfPower.KILO_WATT,
     ),
     DSMRReaderSensorEntityDescription(
         key="dsmr/consumption/quarter-hour-peak-electricity/read_at_start",
