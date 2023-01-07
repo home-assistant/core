2e3351058f8e508a0857a88d7e29e3c60f2f12c3
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-01-07 01:27:26 +0000

    Update config flow test to make sure the user can still complete the flow after invalid data

--- tests/components/trend/test_config_flow.py
+++ tests/components/trend/test_config_flow.py
@@ -9,8 +9,8 @@ from homeassistant.const import CONF_ENTITY_ID, CONF_NAME
 from homeassistant.core import HomeAssistant
 
 
-async def test_flow_fails_missing_data(hass: HomeAssistant) -> None:
-    """Test the config flow doesn't allow missing data."""
+async def test_flow_fails_can_still_succeed(hass: HomeAssistant) -> None:
+    """Test the config flow doesn't allow missing data, but still allows recovery."""
     # Start the config flow
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
@@ -24,9 +24,12 @@ async def test_flow_fails_missing_data(hass: HomeAssistant) -> None:
             user_input={},
         )
 
-    # Clean up
-    hass.config_entries.flow.async_abort(result["flow_id"])
-    await hass.async_block_till_done()
+    # Try configure with required fields
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={CONF_NAME: "Trend Sensor", CONF_ENTITY_ID: "sensor.test_sensor"},
+    )
+    assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
 
 
 async def test_flow_succeeds_with_minimum_data(hass: HomeAssistant) -> None:
