f85de15deb8d88daf0dd6867ed4cb639187b0cd4
Poltorak Serguei <ps@z-wave.me>
2023-01-08 22:16:17 +0300

    whitespace

--- homeassistant/components/zwave_me/cover.py
+++ homeassistant/components/zwave_me/cover.py
@@ -71,25 +71,25 @@ class ZWaveMeCover(ZWaveMeEntity, CoverEntity):
     @property
     def current_cover_position(self) -> int | None:
         """Return current position of cover.
-        
+
         None is unknown, 0 is closed, 100 is fully open.
-        
+
         Allow small calibration errors (some devices after a long time become not well calibrated)
         """
         if self.device.level > 95:
             return 100
-        
+
         return self.device.level
 
     @property
     def is_closed(self) -> bool | None:
         """Return true if cover is closed.
-        
+
         None is unknown.
-        
+
         Allow small calibration errors (some devices after a long time become not well calibrated)
         """
         if self.device.level is None:
             return None
-        
+
         return self.device.level < 5
