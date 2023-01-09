f31c3ff038a0af55680df3936aea5dc16651abde
Teemu Rytilahti <tpr@iki.fi>
2023-01-10 00:57:57 +0100

    Set the name for individual smartstrip sockets

--- homeassistant/components/tplink/switch.py
+++ homeassistant/components/tplink/switch.py
@@ -93,6 +93,9 @@ class SmartPlugSwitch(CoordinatedTPLinkEntity, SwitchEntity):
         super().__init__(device, coordinator)
         # For backwards compat with pyHS100
         self._attr_unique_id = legacy_device_id(device)
+        # Define names for single sockets
+        if device.is_strip_socket:
+            self._attr_name = device.alias
 
     @async_refresh_after
     async def async_turn_on(self, **kwargs: Any) -> None:
