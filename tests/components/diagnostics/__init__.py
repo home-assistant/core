aa82a878c172f1deb99ca6d4ea5a1ed0736e32c3
epenet <6771947+epenet@users.noreply.github.com>
2023-01-09 07:57:55 +0000

    Add type hints to diagnostics test helper

--- tests/components/diagnostics/__init__.py
+++ tests/components/diagnostics/__init__.py
@@ -1,14 +1,26 @@
 """Tests for the Diagnostics integration."""
 from http import HTTPStatus
+from typing import Any
 
+from aiohttp.test_utils import TestClient
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceEntry
 from homeassistant.setup import async_setup_component
 
+from tests.conftest import TestClientGenerator
 
-async def _get_diagnostics_for_config_entry(hass, hass_client, config_entry):
+
+async def _get_diagnostics_for_config_entry(
+    hass: HomeAssistant,
+    hass_client: TestClientGenerator,
+    config_entry: ConfigEntry,
+) -> dict[str, Any]:
     """Return the diagnostics config entry for the specified domain."""
     assert await async_setup_component(hass, "diagnostics", {})
 
-    client = await hass_client()
+    client: TestClient = await hass_client()
     response = await client.get(
         f"/api/diagnostics/config_entry/{config_entry.entry_id}"
     )
@@ -16,13 +28,22 @@ async def _get_diagnostics_for_config_entry(hass, hass_client, config_entry):
     return await response.json()
 
 
-async def get_diagnostics_for_config_entry(hass, hass_client, config_entry):
+async def get_diagnostics_for_config_entry(
+    hass: HomeAssistant,
+    hass_client: TestClientGenerator,
+    config_entry: ConfigEntry,
+) -> dict[str, Any]:
     """Return the diagnostics config entry for the specified domain."""
     data = await _get_diagnostics_for_config_entry(hass, hass_client, config_entry)
     return data["data"]
 
 
-async def _get_diagnostics_for_device(hass, hass_client, config_entry, device):
+async def _get_diagnostics_for_device(
+    hass: HomeAssistant,
+    hass_client: TestClientGenerator,
+    config_entry: ConfigEntry,
+    device: DeviceEntry,
+) -> dict[str, Any]:
     """Return the diagnostics for the specified device."""
     assert await async_setup_component(hass, "diagnostics", {})
 
@@ -34,7 +55,12 @@ async def _get_diagnostics_for_device(hass, hass_client, config_entry, device):
     return await response.json()
 
 
-async def get_diagnostics_for_device(hass, hass_client, config_entry, device):
+async def get_diagnostics_for_device(
+    hass: HomeAssistant,
+    hass_client: TestClientGenerator,
+    config_entry: ConfigEntry,
+    device: DeviceEntry,
+) -> dict[str, Any]:
     """Return the diagnostics for the specified device."""
     data = await _get_diagnostics_for_device(hass, hass_client, config_entry, device)
     return data["data"]
