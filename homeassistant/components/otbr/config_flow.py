923dff05769b66b258747b0a13db00a3c8b97b14
Erik <erik@montnemery.com>
2023-01-10 10:49:59 +0100

    Add files

---
+++ homeassistant/components/otbr/config_flow.py
@@ -0,0 +1,27 @@
+"""Config flow for the Open Thread Border Router integration."""
+from __future__ import annotations
+
+from homeassistant.components.hassio import HassioServiceInfo
+from homeassistant.config_entries import ConfigFlow
+from homeassistant.data_entry_flow import FlowResult
+
+from .const import DOMAIN
+
+
+class OTBRConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Home Assistant Sky Connect."""
+
+    VERSION = 1
+
+    async def async_step_hassio(self, discovery_info: HassioServiceInfo) -> FlowResult:
+        """Handle hassio discovery."""
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        return self.async_create_entry(
+            title="Thread",
+            data={
+                "host": discovery_info.config["host"],
+                "port": discovery_info.config["port"],
+            },
+        )
