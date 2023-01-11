3ef3a7d5d05735b785852db598d33744ab474a44
Erik <erik@montnemery.com>
2023-01-11 13:34:50 +0100

    Make the kitchen_sink integration set up a config entry

---
+++ homeassistant/components/kitchen_sink/config_flow.py
@@ -0,0 +1,19 @@
+"""Config flow to configure the Kitchen Sink component."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant import config_entries
+from homeassistant.data_entry_flow import FlowResult
+
+from . import DOMAIN
+
+
+class DemoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Demo configuration flow."""
+
+    VERSION = 1
+
+    async def async_step_import(self, import_info: dict[str, Any]) -> FlowResult:
+        """Set the config entry up from yaml."""
+        return self.async_create_entry(title="Kitchen Sink", data=import_info)
