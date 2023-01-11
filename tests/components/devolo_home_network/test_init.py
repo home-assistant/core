45d75ed38feaf09c3b137b83af98139aee84a0e5
Guido Schmitz <guido.schmitz@fedaix.de>
2023-01-11 21:46:36 +0000

    Adapt tests to newly added switch platform

--- tests/components/devolo_home_network/test_init.py
+++ tests/components/devolo_home_network/test_init.py
@@ -8,6 +8,7 @@ from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR
 from homeassistant.components.device_tracker import DOMAIN as DEVICE_TRACKER
 from homeassistant.components.devolo_home_network.const import DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR
+from homeassistant.components.switch import DOMAIN as SWITCH
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import CONF_IP_ADDRESS, EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import HomeAssistant
@@ -82,9 +83,9 @@ async def test_hass_stop(hass: HomeAssistant, mock_device: MockDevice):
 @pytest.mark.parametrize(
     "device, expected_platforms",
     [
-        ["mock_device", (BINARY_SENSOR, DEVICE_TRACKER, SENSOR)],
-        ["mock_repeater_device", (DEVICE_TRACKER, SENSOR)],
-        ["mock_nonwifi_device", (BINARY_SENSOR, SENSOR)],
+        ["mock_device", (BINARY_SENSOR, DEVICE_TRACKER, SENSOR, SWITCH)],
+        ["mock_repeater_device", (DEVICE_TRACKER, SENSOR, SWITCH)],
+        ["mock_nonwifi_device", (BINARY_SENSOR, SENSOR, SWITCH)],
     ],
 )
 async def test_platforms(
