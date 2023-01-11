480b9b0b6dc30a6c18c5f886f10575972fb70ecc
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2023-01-11 22:53:29 +0100

    address review comments

--- homeassistant/components/moehlenhoff_alpha2/button.py
+++ homeassistant/components/moehlenhoff_alpha2/button.py
@@ -1,13 +1,12 @@
 """Button entity to set the time of the Alpha2 base."""
 
-from datetime import datetime
-
 from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.util import dt
 
 from . import Alpha2BaseCoordinator
 from .const import DOMAIN
@@ -28,15 +27,15 @@ async def async_setup_entry(
 class Alpha2TimeSyncButton(CoordinatorEntity[Alpha2BaseCoordinator], ButtonEntity):
     """Alpha2 virtual time sync button."""
 
-    _attr_name = "Sync Time"
+    _attr_name = "Sync time"
     _attr_entity_category = EntityCategory.DIAGNOSTIC
 
     def __init__(self, coordinator: Alpha2BaseCoordinator, entry_id: str) -> None:
         """Initialize Alpha2TimeSyncButton."""
         super().__init__(coordinator)
 
-        self._attr_unique_id = f"{entry_id}:sync_time_button"
+        self._attr_unique_id = f"{entry_id}:sync_time"
 
     async def async_press(self) -> None:
         """Synchronize current local time from HA instance to base station."""
-        await self.coordinator.base.set_datetime(datetime.now())
+        await self.coordinator.base.set_datetime(dt.now())
