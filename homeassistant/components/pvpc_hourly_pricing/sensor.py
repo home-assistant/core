fc1fedd5835459078a759cd8c59a23bb9a7cf418
Eugenio Panadero Maciá <eugenio.panadero@gmail.com>
2023-01-11 12:45:14 +0100

    ⏪️ Revert changes unrelated to library bump

--- homeassistant/components/pvpc_hourly_pricing/sensor.py
+++ homeassistant/components/pvpc_hourly_pricing/sensor.py
@@ -6,11 +6,7 @@ from datetime import datetime
 import logging
 from typing import Any
 
-from aiopvpc.const import KEY_PVPC
-from aiopvpc.ha_helpers import make_sensor_unique_id
-
 from homeassistant.components.sensor import (
-    SensorDeviceClass,
     SensorEntity,
     SensorEntityDescription,
     SensorStateClass,
@@ -32,16 +28,13 @@ _LOGGER = logging.getLogger(__name__)
 PARALLEL_UPDATES = 1
 SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
     SensorEntityDescription(
-        key=KEY_PVPC,
+        key="PVPC",
         icon="mdi:currency-eur",
-        device_class=SensorDeviceClass.MONETARY,
         native_unit_of_measurement=f"{CURRENCY_EURO}/{UnitOfEnergy.KILO_WATT_HOUR}",
         state_class=SensorStateClass.MEASUREMENT,
     ),
 )
 _PRICE_SENSOR_ATTRIBUTES_MAP = {
-    "data_id": "data_id",
-    "name": "name",
     "tariff": "tariff",
     "period": "period",
     "available_power": "available_power",
@@ -146,7 +139,7 @@ class ElecPriceSensor(CoordinatorEntity[ElecPricesDataUpdateCoordinator], Sensor
         super().__init__(coordinator)
         self.entity_description = description
         self._attr_attribution = coordinator.api.attribution
-        self._attr_unique_id = make_sensor_unique_id(unique_id, description.key)
+        self._attr_unique_id = unique_id
         self._attr_name = name
         self._attr_device_info = DeviceInfo(
             configuration_url="https://api.esios.ree.es",
@@ -157,11 +150,6 @@ class ElecPriceSensor(CoordinatorEntity[ElecPricesDataUpdateCoordinator], Sensor
         )
         self._state: StateType = None
         self._attrs: Mapping[str, Any] = {}
-        self.async_on_remove(
-            lambda: coordinator.api.update_active_sensors(
-                self.entity_description.key, False
-            )
-        )
 
     async def async_added_to_hass(self) -> None:
         """Handle entity which will be added."""
@@ -174,16 +162,11 @@ class ElecPriceSensor(CoordinatorEntity[ElecPricesDataUpdateCoordinator], Sensor
             )
         )
         _LOGGER.debug(
-            "Setup of ESIOS sensor %s (%s, unique_id: %s)",
-            self.entity_description.key,
+            "Setup of price sensor %s (%s) with tariff '%s'",
+            self.name,
             self.entity_id,
-            self._attr_unique_id,
+            self.coordinator.api.tariff,
         )
-
-    @callback
-    def async_registry_entry_updated(self) -> None:
-        """Enable API downloads for this sensor."""
-        self.coordinator.api.update_active_sensors(self.entity_description.key, True)
 
     @callback
     def update_current_price(self, now: datetime) -> None:
@@ -198,14 +181,6 @@ class ElecPriceSensor(CoordinatorEntity[ElecPricesDataUpdateCoordinator], Sensor
         """Return the state of the sensor."""
         self._state = self.coordinator.api.states.get(self.entity_description.key)
         return self._state
-
-    @property
-    def available(self) -> bool:
-        """Return True if entity is available."""
-        self._attr_available = self.coordinator.data.availability.get(
-            self.entity_description.key, False
-        )
-        return self._attr_available
 
     @property
     def extra_state_attributes(self) -> Mapping[str, Any] | None:
