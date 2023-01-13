4ada7e4e62d73882f69318d4d7e0169cf517b0d6
G Johansson <goran.johansson@shiftit.se>
2023-01-13 20:25:07 +0000

    mod test from available change

--- tests/components/group/test_sensor.py
+++ tests/components/group/test_sensor.py
@@ -181,7 +181,7 @@ async def test_not_enough_sensor_value(hass: HomeAssistant) -> None:
     await hass.async_block_till_done()
 
     state = hass.states.get("sensor.test_max")
-    assert state.state == STATE_UNKNOWN
+    assert state.state == STATE_UNAVAILABLE
     assert state.attributes.get("min_entity_id") is None
     assert state.attributes.get("max_entity_id") is None
 
@@ -189,23 +189,23 @@ async def test_not_enough_sensor_value(hass: HomeAssistant) -> None:
     await hass.async_block_till_done()
 
     state = hass.states.get("sensor.test_max")
-    assert STATE_UNKNOWN != state.state
+    assert state.state not in [STATE_UNAVAILABLE, STATE_UNKNOWN]
     assert entity_ids[1] == state.attributes.get("max_entity_id")
 
     hass.states.async_set(entity_ids[2], STATE_UNKNOWN)
     await hass.async_block_till_done()
 
     state = hass.states.get("sensor.test_max")
-    assert STATE_UNKNOWN != state.state
+    assert state.state not in [STATE_UNAVAILABLE, STATE_UNKNOWN]
     assert entity_ids[1] == state.attributes.get("max_entity_id")
 
     hass.states.async_set(entity_ids[1], STATE_UNAVAILABLE)
     await hass.async_block_till_done()
 
     state = hass.states.get("sensor.test_max")
-    assert state.state == STATE_UNKNOWN
+    assert state.state == STATE_UNAVAILABLE
     assert state.attributes.get("min_entity_id") is None
-    assert state.attributes.get("max_entity_id") == entity_ids[1]
+    assert state.attributes.get("max_entity_id") is None
 
 
 async def test_reload(hass: HomeAssistant) -> None:
