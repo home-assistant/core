16c46e6803bd3cd78c18c00369fe6bd5b8315e83
Yuxin Wang <yuxinwang.dev@gmail.com>
2023-01-15 18:20:30 -0500

    Complete type annotations for test files.

--- tests/components/apcupsd/test_sensor.py
+++ tests/components/apcupsd/test_sensor.py
@@ -19,7 +19,7 @@ from homeassistant.helpers import entity_registry as er
 from . import MOCK_STATUS, init_integration
 
 
-async def test_sensor(hass: HomeAssistant):
+async def test_sensor(hass: HomeAssistant) -> None:
     """Test states of sensor."""
     await init_integration(hass, status=MOCK_STATUS)
     registry = er.async_get(hass)
@@ -78,7 +78,7 @@ async def test_sensor(hass: HomeAssistant):
     assert entry.unique_id == "XXXXXXXXXXXX_nompower"
 
 
-async def test_sensor_disabled(hass):
+async def test_sensor_disabled(hass: HomeAssistant) -> None:
     """Test sensor disabled by default."""
     await init_integration(hass)
     registry = er.async_get(hass)
