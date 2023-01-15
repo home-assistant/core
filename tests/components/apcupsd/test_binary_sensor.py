c61ad3461d9ce30063efb3b5e5bb72e5a566ed61
Yuxin Wang <yuxinwang.dev@gmail.com>
2023-01-15 15:46:49 -0500

    Add a test case for binary sensor when STATFLAG is not available.

--- tests/components/apcupsd/test_binary_sensor.py
+++ tests/components/apcupsd/test_binary_sensor.py
@@ -5,7 +5,7 @@ from homeassistant.helpers import entity_registry as er
 from . import MOCK_STATUS, init_integration
 
 
-async def test_binary_sensor(hass: HomeAssistant):
+async def test_binary_sensor(hass: HomeAssistant) -> None:
     """Test states of binary sensor."""
     await init_integration(hass, status=MOCK_STATUS)
     registry = er.async_get(hass)
@@ -16,3 +16,13 @@ async def test_binary_sensor(hass: HomeAssistant):
     entry = registry.async_get("binary_sensor.ups_online_status")
     assert entry
     assert entry.unique_id == "XXXXXXXXXXXX_statflag"
+
+
+async def test_no_binary_sensor(hass: HomeAssistant) -> None:
+    """Test binary sensor when STATFLAG is not available."""
+    status = MOCK_STATUS.copy()
+    status.pop("STATFLAG")
+    await init_integration(hass, status=status)
+
+    state = hass.states.get("binary_sensor.ups_online_status")
+    assert state is None
