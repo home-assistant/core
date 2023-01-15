92de87888837e2a438aea7b06a62d25fa2da31fa
mib1185 <mail@mib85.de>
2023-01-15 14:39:12 +0000

    replace unit conts by enums

--- homeassistant/components/withings/const.py
+++ homeassistant/components/withings/const.py
@@ -1,6 +1,6 @@
 """Constants used by the Withings component."""
-from homeassistant import const
 from homeassistant.backports.enum import StrEnum
+from homeassistant.const import UnitOfTime
 
 CONF_PROFILES = "profiles"
 CONF_USE_WEBHOOK = "use_webhook"
@@ -55,6 +55,6 @@ class Measurement(StrEnum):
 
 SCORE_POINTS = "points"
 UOM_BEATS_PER_MINUTE = "bpm"
-UOM_BREATHS_PER_MINUTE = f"br/{const.TIME_MINUTES}"
+UOM_BREATHS_PER_MINUTE = f"br/{UnitOfTime.MINUTES}"
 UOM_FREQUENCY = "times"
 UOM_MMHG = "mmhg"
