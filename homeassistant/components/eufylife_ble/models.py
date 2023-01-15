74e21d07d3c3938cb934d7a843173bb0228d007a
Brandon Rothweiler <brandonrothweiler@gmail.com>
2023-01-14 20:06:45 -0500

    Add EufyLife integration

---
+++ homeassistant/components/eufylife_ble/models.py
@@ -0,0 +1,15 @@
+"""The EufyLife integration models."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from eufylife_ble_client import EufyLifeBLEDevice
+
+
+@dataclass
+class EufyLifeData:
+    """Data for the EufyLife integration."""
+
+    address: str
+    model: str
+    client: EufyLifeBLEDevice
