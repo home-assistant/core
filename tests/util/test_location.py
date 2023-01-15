df1dab22b15f54d4472b19252165a95fb52aebf1
joao-henrique10 <71076129+joao-henrique10@users.noreply.github.com>
2023-01-15 12:02:00 -0300

    Adicinando 4 casos de teste

--- tests/util/test_location.py
+++ tests/util/test_location.py
@@ -14,6 +14,16 @@ COORDINATES_PARIS = (48.864716, 2.349014)
 # New York
 COORDINATES_NEW_YORK = (40.730610, -73.935242)
 
+# BARCELONA(Spain)
+COORDINATES_BARCELONA = (41.390205, 2.154007)
+
+# Berlim(Germany)
+COORDINATES_BERLIM = (52.520008, 13.404954)
+
+COORDINATES_CITY = ()
+
+COORDINATES_CITY2 = 48.864716
+
 # Results for the assertion (vincenty algorithm):
 #      Distance [km]   Distance [miles]
 # [0]  5846.39         3632.78
@@ -56,6 +66,18 @@ def test_get_distance():
         COORDINATES_PARIS[1],
         COORDINATES_NEW_YORK[0],
         COORDINATES_NEW_YORK[1],
+    )
+
+    assert meters / 1000 - DISTANCE_KM < 0.01
+
+
+def test_get_distance2():
+    """Test getting the distance."""
+    meters = location_util.distance(
+        COORDINATES_PARIS[1],
+        COORDINATES_PARIS[0],
+        COORDINATES_NEW_YORK[1],
+        COORDINATES_NEW_YORK[0],
     )
 
     assert meters / 1000 - DISTANCE_KM < 0.01
@@ -110,3 +132,44 @@ async def test_whoami_query_raises(raising_session):
     """Test whoami query when the request to API fails."""
     info = await location_util._get_whoami(raising_session)
     assert info is None
+
+
+def test_CT1():
+    meters = location_util.distance(
+        COORDINATES_CITY[1],
+        COORDINATES_CITY[0],
+        COORDINATES_BERLIM[1],
+        COORDINATES_BERLIM[0],
+    )
+    assert meters is None
+
+
+def test_CT2():
+    meters = location_util.distance(
+        COORDINATES_CITY2[1],
+        COORDINATES_CITY2[0],
+        COORDINATES_BERLIM[1],
+        COORDINATES_BERLIM[0],
+    )
+    assert meters is None
+
+
+def test_CT3():
+    meters = location_util.distance(
+        COORDINATES_BARCELONA[1],
+        COORDINATES_BARCELONA[0],
+        COORDINATES_BARCELONA[1],
+        COORDINATES_BARCELONA[0],
+    )
+    assert meters is None
+
+
+def test_CT4():
+    meters = location_util.distance(
+        COORDINATES_BARCELONA[1],
+        COORDINATES_BARCELONA[0],
+        COORDINATES_BERLIM[1],
+        COORDINATES_BERLIM[0],
+    )
+
+    assert meters / 1000 - DISTANCE_KM < 0.01
