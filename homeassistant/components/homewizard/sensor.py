a456181ca45fa2687a6276e59192494545678c8f
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2023-01-17 10:57:07 +0000

    Add Enum device class for HomeWizard active tariff

--- homeassistant/components/homewizard/sensor.py
+++ homeassistant/components/homewizard/sensor.py
@@ -74,7 +74,9 @@ SENSORS: Final[tuple[HomeWizardSensorEntityDescription, ...]] = (
         key="active_tariff",
         name="Active tariff",
         icon="mdi:calendar-clock",
-        value_fn=lambda data: data.active_tariff,
+        value_fn=lambda data: str(data.active_tariff),
+        device_class=SensorDeviceClass.ENUM,
+        options=["1", "2", "3", "4"],
     ),
     HomeWizardSensorEntityDescription(
         key="wifi_strength",
