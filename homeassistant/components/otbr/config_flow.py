0834874f0cb385aa8d69c71795a8c48d461e4bd6
Erik <erik@montnemery.com>
2023-01-17 19:07:11 +0100

    Adjust Thread config flow

--- homeassistant/components/otbr/config_flow.py
+++ homeassistant/components/otbr/config_flow.py
@@ -1,12 +1,14 @@
 """Config flow for the Open Thread Border Router integration."""
 from __future__ import annotations
 
+import python_otbr_api
 import voluptuous as vol
 
 from homeassistant.components.hassio import HassioServiceInfo
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import CONF_URL
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
 
@@ -23,14 +25,26 @@ class OTBRConfigFlow(ConfigFlow, domain=DOMAIN):
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
+        errors = {}
+
         if user_input is not None:
-            return self.async_create_entry(
-                title="Thread",
-                data={"url": user_input[CONF_URL]},
-            )
+            url = user_input[CONF_URL]
+            api = python_otbr_api.OTBR(url, async_get_clientsession(self.hass), 10)
+            try:
+                await api.get_active_dataset_tlvs()
+            except python_otbr_api.OTBRError:
+                errors["base"] = "cannot_connect"
+            else:
+                await self.async_set_unique_id(DOMAIN)
+                return self.async_create_entry(
+                    title="Thread",
+                    data={"url": url},
+                )
 
         data_schema = vol.Schema({CONF_URL: str})
-        return self.async_show_form(step_id="user", data_schema=data_schema)
+        return self.async_show_form(
+            step_id="user", data_schema=data_schema, errors=errors
+        )
 
     async def async_step_hassio(self, discovery_info: HassioServiceInfo) -> FlowResult:
         """Handle hassio discovery."""
@@ -38,7 +52,9 @@ class OTBRConfigFlow(ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="single_instance_allowed")
 
         config = discovery_info.config
+        url = f"http://{config['host']}:{config['port']}"
+        await self.async_set_unique_id(DOMAIN)
         return self.async_create_entry(
             title="Thread",
-            data={"url": f"http://{config['host']}:{config['port']}"},
+            data={"url": url},
         )
