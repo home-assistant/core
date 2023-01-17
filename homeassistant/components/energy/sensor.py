85580e834d6c4613e87f95978c63d476bc2284b6
SgtBatten <sgt@batten.net.au>
2023-01-17 12:43:56 +1100

    Add Mega joule

--- homeassistant/components/energy/sensor.py
+++ homeassistant/components/energy/sensor.py
@@ -42,6 +42,7 @@ SUPPORTED_STATE_CLASSES = {
 }
 VALID_ENERGY_UNITS: set[str] = {
     UnitOfEnergy.GIGA_JOULE,
+    UnitOfEnergy.MEGA_JOULE,
     UnitOfEnergy.KILO_WATT_HOUR,
     UnitOfEnergy.MEGA_WATT_HOUR,
     UnitOfEnergy.WATT_HOUR,
