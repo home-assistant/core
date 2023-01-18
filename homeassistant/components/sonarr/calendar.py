e2ae4d4de9e5576434c806f322fef9fe1d50d7c2
Lewis England <lewis2004@hotmail.com>
2023-01-18 11:24:13 +0000

    refactor: Rewrite to use coordinator updates
    * Base calendarEntity on SonarrEntity to use coordinator updates
    * get_events will only return events
    * coordinator_update will append new episodes and remove deleted ones
      not re-build the full list each time
    * generate the list of events when Sonarr component is added to HomeAssistant

--- homeassistant/components/sonarr/calendar.py
+++ homeassistant/components/sonarr/calendar.py
@@ -5,12 +5,14 @@ from datetime import datetime, timedelta
 
 from homeassistant.components.calendar import CalendarEntity, CalendarEvent
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.util.dt import as_utc
+from homeassistant.util.dt import as_utc, start_of_local_day
 
 from .const import DOMAIN
 from .coordinator import CalendarDataUpdateCoordinator
+from .entity import SonarrEntity
 
 
 async def async_setup_entry(
@@ -20,68 +22,72 @@ async def async_setup_entry(
 ) -> None:
     """Set up Sonarr calendar based on a config entry."""
     coordinator = hass.data[DOMAIN][entry.entry_id]["upcoming"]
-    async_add_entities([SonarrCalendarEntity(coordinator, "Sonarr Episodes")])
+    async_add_entities([SonarrCalendarEntity(coordinator, "Episodes")])
 
 
-async def get_sonarr_episode_events(
-    coordinator: CalendarDataUpdateCoordinator,
-) -> list[CalendarEvent]:
-    """Update the list of upcoming episodes."""
-    calendar_entries: list[CalendarEvent] = []
-    for episode in coordinator.data:
-        episode_endtime_utc = episode.airDateUtc + timedelta(
-            minutes=episode.series.runtime
-        )
-        calendar_entries.append(
-            CalendarEvent(
-                summary=episode.series.title,
-                description=episode.title,
-                location=episode.series.network,
-                start=episode.airDateUtc,
-                end=episode_endtime_utc,
-            )
-        )
-    return calendar_entries
-
-
-class SonarrCalendarEntity(CalendarEntity):
-    """Representation of a Sonarr Calendar element."""
-
-    coordinator: CalendarDataUpdateCoordinator
-    _events: list[CalendarEvent]
+class SonarrCalendarEntity(SonarrEntity, CalendarEntity):
+    """Defines a Sonarr Calendar."""
 
     def __init__(self, coordinator: CalendarDataUpdateCoordinator, name: str) -> None:
-        """Initialize Sonarr calendar."""
-        self.coordinator = coordinator
-        self._attr_name = name
-        self._events = []
+        """Initialize the Sonarr Calendar."""
+        super().__init__(
+            coordinator=coordinator,
+            description=EntityDescription(key="sonarr.episodes", name=name),
+        )
+
+        self._coordinator: CalendarDataUpdateCoordinator = coordinator
+        self._events: list[CalendarEvent] = []
 
     @property
     def event(self) -> CalendarEvent | None:
         """Return the next upcoming event."""
-        if self._events:
-            return self._events[0]
-        return None
+        return self._events[0] if self._events else None
+
+    def upcoming(self) -> list[str | None]:
+        """Return a list of all upcoming events."""
+        return [ep.uid for ep in self._events if ep.start >= start_of_local_day()]
+
+    def _calc_end_time(self, start_date: datetime, duration: int) -> datetime:
+        """Calculate the end datetime for an episode."""
+        return start_date + timedelta(minutes=duration)
 
     async def async_get_events(
-        self,
-        hass: HomeAssistant,
-        start_date: datetime,
-        end_date: datetime,
+        self, hass: HomeAssistant, start_date: datetime, end_date: datetime
     ) -> list[CalendarEvent]:
         """Return calendar events within a datetime range."""
-        episode_events = await get_sonarr_episode_events(self.coordinator)
         start_date = as_utc(start_date)
         end_date = as_utc(end_date)
-        self._events = [
+        return [
             episode
-            for episode in episode_events
+            for episode in self._events
             if start_date <= episode.start <= end_date
         ]
 
-        return self._events
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self._events.extend(
+            CalendarEvent(
+                uid=str(episode.id),
+                summary=episode.series.title,
+                description=episode.title,
+                location=episode.series.network,
+                start=episode.airDateUtc,
+                end=self._calc_end_time(episode.airDateUtc, episode.series.runtime),
+            )
+            for episode in self._coordinator.data
+            if str(episode.id) not in self.upcoming()
+        )
+        coord_upcoming = [str(episode.id) for episode in self._coordinator.data]
+        ids_to_remove = [
+            event.uid for event in self._events if event.uid not in coord_upcoming
+        ]
+        for event in self._events:
+            if event.uid in ids_to_remove:
+                self._events.remove(event)
+        self.async_write_ha_state()
 
-    async def async_update(self) -> None:
-        """Update the calendar for new entries."""
-        episodes = await get_sonarr_episode_events(self.coordinator)
-        self._events = episodes
+    async def async_added_to_hass(self) -> None:
+        """When entity is added to hass."""
+        await super().async_added_to_hass()
+        self._handle_coordinator_update()
