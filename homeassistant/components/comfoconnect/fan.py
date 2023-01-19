bdfe0f25a64ed98458d8b030b5ce208286a31ff5
N3rdix <tobias.abb+github@gmail.com>
2023-01-19 11:21:39 +0000

    Set default name in DeviceInfo

--- homeassistant/components/comfoconnect/fan.py
+++ homeassistant/components/comfoconnect/fan.py
@@ -84,7 +84,7 @@ class ComfoConnectFan(FanEntity):
         self._attr_unique_id = ccb.unique_id
         self._attr_preset_mode = None
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, ccb.unique_id)},
+            identifiers={(DOMAIN, ccb.unique_id)}, name=ccb.name
         )
 
     async def async_added_to_hass(self) -> None:
