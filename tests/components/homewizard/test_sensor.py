377491fe658d189b953e7d5dbcbc3e0f6ed78bb7
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2023-01-20 13:42:01 +0100

    Add sensors for smart and gas meter identifiers (serial numbers) in HomeWizard (#86282)

--- tests/components/homewizard/test_sensor.py
+++ tests/components/homewizard/test_sensor.py
@@ -110,6 +110,46 @@ async def test_sensor_entity_meter_model(
     assert state.attributes.get(ATTR_ICON) == "mdi:gauge"
 
 
+async def test_sensor_entity_unique_meter_id(
+    hass, mock_config_entry_data, mock_config_entry
+):
+    """Test entity loads unique meter id."""
+
+    api = get_mock_device()
+    api.data = AsyncMock(return_value=Data.from_dict({"unique_id": "4E47475955"}))
+
+    with patch(
+        "homeassistant.components.homewizard.coordinator.HomeWizardEnergy",
+        return_value=api,
+    ):
+        entry = mock_config_entry
+        entry.data = mock_config_entry_data
+        entry.add_to_hass(hass)
+
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    entity_registry = er.async_get(hass)
+
+    state = hass.states.get("sensor.product_name_aabbccddeeff_smart_meter_identifier")
+    entry = entity_registry.async_get(
+        "sensor.product_name_aabbccddeeff_smart_meter_identifier"
+    )
+    assert entry
+    assert state
+    assert entry.unique_id == "aabbccddeeff_unique_meter_id"
+    assert not entry.disabled
+    assert state.state == "NGGYU"
+    assert (
+        state.attributes.get(ATTR_FRIENDLY_NAME)
+        == "Product Name (aabbccddeeff) Smart meter identifier"
+    )
+    assert ATTR_STATE_CLASS not in state.attributes
+    assert ATTR_UNIT_OF_MEASUREMENT not in state.attributes
+    assert ATTR_DEVICE_CLASS not in state.attributes
+    assert state.attributes.get(ATTR_ICON) == "mdi:alphabetical-variant"
+
+
 async def test_sensor_entity_wifi_ssid(hass, mock_config_entry_data, mock_config_entry):
     """Test entity loads wifi ssid."""
 
@@ -572,6 +612,46 @@ async def test_sensor_entity_total_gas(hass, mock_config_entry_data, mock_config
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UnitOfVolume.CUBIC_METERS
     assert state.attributes.get(ATTR_DEVICE_CLASS) == SensorDeviceClass.GAS
     assert ATTR_ICON not in state.attributes
+
+
+async def test_sensor_entity_unique_gas_meter_id(
+    hass, mock_config_entry_data, mock_config_entry
+):
+    """Test entity loads unique gas meter id."""
+
+    api = get_mock_device()
+    api.data = AsyncMock(return_value=Data.from_dict({"gas_unique_id": "4E47475955"}))
+
+    with patch(
+        "homeassistant.components.homewizard.coordinator.HomeWizardEnergy",
+        return_value=api,
+    ):
+        entry = mock_config_entry
+        entry.data = mock_config_entry_data
+        entry.add_to_hass(hass)
+
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    entity_registry = er.async_get(hass)
+
+    state = hass.states.get("sensor.product_name_aabbccddeeff_gas_meter_identifier")
+    entry = entity_registry.async_get(
+        "sensor.product_name_aabbccddeeff_gas_meter_identifier"
+    )
+    assert entry
+    assert state
+    assert entry.unique_id == "aabbccddeeff_gas_unique_id"
+    assert not entry.disabled
+    assert state.state == "NGGYU"
+    assert (
+        state.attributes.get(ATTR_FRIENDLY_NAME)
+        == "Product Name (aabbccddeeff) Gas meter identifier"
+    )
+    assert ATTR_STATE_CLASS not in state.attributes
+    assert ATTR_UNIT_OF_MEASUREMENT not in state.attributes
+    assert ATTR_DEVICE_CLASS not in state.attributes
+    assert state.attributes.get(ATTR_ICON) == "mdi:alphabetical-variant"
 
 
 async def test_sensor_entity_active_voltage_l1(
