7019bc382c194e283056bdd515ef1fbfd590544c
conway220 <conway220@gmail.com>
2023-01-22 15:36:14 +0000

    Decreased timedelta to lowest functioning value.

--- tests/components/anova_sous_vide/__init__.py
+++ tests/components/anova_sous_vide/__init__.py
@@ -61,7 +61,7 @@ async def async_init_integration(
         if not skip_setup:
             await hass.config_entries.async_setup(entry.entry_id)
             await hass.async_block_till_done()
-            async_fire_time_changed(hass, dt.utcnow() + timedelta(seconds=70))
+            async_fire_time_changed(hass, dt.utcnow() + timedelta(seconds=31))
             await hass.async_block_till_done()
 
         return entry
