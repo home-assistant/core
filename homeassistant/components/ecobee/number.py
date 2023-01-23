cb5f21ea654c35597d4d48635bb92c30475da2a1
J. Nick Koston <nick@koston.org>
2023-01-23 12:32:15 -1000

    Update homeassistant/components/ecobee/number.py

--- homeassistant/components/ecobee/number.py
+++ homeassistant/components/ecobee/number.py
@@ -66,7 +66,6 @@ class EcobeeVentilatorMinTime(EcobeeBaseEntity, NumberEntity):
 
         self.data = data
         self.thermostat_index = thermostat_index
-        self.thermostat = data.ecobee.get_thermostat(self.thermostat_index)
         self.ecobee_setting_key = ecobee_setting_key
         self.set_func = set_func
         self._attr_name = f"Ventilator min time {mode}"
