450d4ea6637b5be11c112fd0b5780db9cbe20841
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-01-23 09:10:05 +0100

    Update Optional typing (2) [Py310] (#86419)

--- homeassistant/components/thermobeacon/sensor.py
+++ homeassistant/components/thermobeacon/sensor.py
@@ -1,8 +1,6 @@
 """Support for ThermoBeacon sensors."""
 from __future__ import annotations
 
-from typing import Optional, Union
-
 from thermobeacon_ble import (
     SensorDeviceClass as ThermoBeaconSensorDeviceClass,
     SensorUpdate,
@@ -128,9 +126,7 @@ async def async_setup_entry(
 
 
 class ThermoBeaconBluetoothSensorEntity(
-    PassiveBluetoothProcessorEntity[
-        PassiveBluetoothDataProcessor[Optional[Union[float, int]]]
-    ],
+    PassiveBluetoothProcessorEntity[PassiveBluetoothDataProcessor[float | int | None]],
     SensorEntity,
 ):
     """Representation of a ThermoBeacon sensor."""
