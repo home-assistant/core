94006c99552a8d1f25ed8643573ee8be7cd2e440
Dan Simpson <daniel_simpson1988@hotmail.com>
2023-01-24 00:11:48 +0000

    Test cleanup

--- tests/components/powerwall/test_switch.py
+++ tests/components/powerwall/test_switch.py
@@ -46,7 +46,7 @@ async def test_entity_registry(hass, mock_powerwall):
     assert ENTITY_ID in entity_registry.entities
 
 
-async def test_initial_gridstatus(hass, mock_powerwall):
+async def test_initial(hass, mock_powerwall):
     """Test initial grid status without off grid switch selected."""
 
     mock_powerwall.get_grid_status = Mock(return_value=GridStatus.CONNECTED)
@@ -55,7 +55,7 @@ async def test_initial_gridstatus(hass, mock_powerwall):
     assert state.state == STATE_OFF
 
 
-async def test_gridstatus_off(hass, mock_powerwall):
+async def test_on(hass, mock_powerwall):
     """Test state once offgrid switch has been turned on."""
 
     mock_powerwall.get_grid_status = Mock(return_value=GridStatus.ISLANDED)
@@ -71,7 +71,7 @@ async def test_gridstatus_off(hass, mock_powerwall):
     assert state.state == STATE_ON
 
 
-async def test_gridstatus_on(hass, mock_powerwall):
+async def test_off(hass, mock_powerwall):
     """Test state once offgrid switch has been turned off."""
 
     mock_powerwall.get_grid_status = Mock(return_value=GridStatus.CONNECTED)
@@ -81,32 +81,6 @@ async def test_gridstatus_on(hass, mock_powerwall):
         SERVICE_TURN_OFF,
         {ATTR_ENTITY_ID: ENTITY_ID},
         blocking=True,
-    )
-
-    state = hass.states.get(ENTITY_ID)
-    assert state.state == STATE_OFF
-
-
-async def test_turn_on_without_entity_id(hass, mock_powerwall):
-    """Test switch turn on all switches."""
-
-    mock_powerwall.get_grid_status = Mock(return_value=GridStatus.ISLANDED)
-
-    await hass.services.async_call(
-        SWITCH_DOMAIN, SERVICE_TURN_ON, {ATTR_ENTITY_ID: "all"}, blocking=True
-    )
-
-    state = hass.states.get(ENTITY_ID)
-    assert state.state == STATE_ON
-
-
-async def test_turn_off_without_entity_id(hass, mock_powerwall):
-    """Test switch turn off all switches."""
-
-    mock_powerwall.get_grid_status = Mock(return_value=GridStatus.CONNECTED)
-
-    await hass.services.async_call(
-        SWITCH_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: "all"}, blocking=True
     )
 
     state = hass.states.get(ENTITY_ID)
