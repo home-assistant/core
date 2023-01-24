14e8c9bdb17f2706ad7807b77e50617389f42d62
J. Nick Koston <nick@koston.org>
2023-01-23 18:20:03 -1000

    all sensors

--- homeassistant/components/mopeka/sensor.py
+++ homeassistant/components/mopeka/sensor.py
@@ -94,8 +94,7 @@ def sensor_update_to_bluetooth_data_update(
             device_key_to_bluetooth_entity_key(device_key): SENSOR_DESCRIPTIONS[
                 device_key.key
             ]
-            for device_key, description in sensor_update.entity_descriptions.items()
-            if description.device_class and description.native_unit_of_measurement
+            for device_key in sensor_update.entity_descriptions
         },
         entity_data={
             device_key_to_bluetooth_entity_key(device_key): sensor_values.native_value
