515dbb50e0d79ee68d266b1e538935c53a5a899f
J. Nick Koston <nick@koston.org>
2023-01-23 17:28:11 -1000

    cover

--- tests/components/mopeka/test_config_flow.py
+++ tests/components/mopeka/test_config_flow.py
@@ -20,16 +20,14 @@ async def test_async_step_bluetooth_valid_device(hass):
     )
     assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "bluetooth_confirm"
-    with patch(
-        "homeassistant.components.mopeka_ble.async_setup_entry", return_value=True
-    ):
+    with patch("homeassistant.components.mopeka.async_setup_entry", return_value=True):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"], user_input={}
         )
     assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "H5075 2762"
+    assert result2["title"] == "Pro+ EEFF"
     assert result2["data"] == {}
-    assert result2["result"].unique_id == "61DE521B-F0BF-9F44-64D4-75BBE1738105"
+    assert result2["result"].unique_id == "aa:bb:cc:dd:ee:ff"
 
 
 async def test_async_step_bluetooth_not_mopeka(hass):
@@ -56,7 +54,7 @@ async def test_async_step_user_no_devices_found(hass):
 async def test_async_step_user_with_found_devices(hass):
     """Test setup from service info cache with devices found."""
     with patch(
-        "homeassistant.components.mopeka_ble.config_flow.async_discovered_service_info",
+        "homeassistant.components.mopeka.config_flow.async_discovered_service_info",
         return_value=[PRO_SERVICE_INFO],
     ):
         result = await hass.config_entries.flow.async_init(
@@ -65,23 +63,21 @@ async def test_async_step_user_with_found_devices(hass):
         )
     assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "user"
-    with patch(
-        "homeassistant.components.mopeka_ble.async_setup_entry", return_value=True
-    ):
+    with patch("homeassistant.components.mopeka.async_setup_entry", return_value=True):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            user_input={"address": "4125DDBA-2774-4851-9889-6AADDD4CAC3D"},
+            user_input={"address": "aa:bb:cc:dd:ee:ff"},
         )
     assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "H5177 2EC8"
+    assert result2["title"] == "Pro+ EEFF"
     assert result2["data"] == {}
-    assert result2["result"].unique_id == "4125DDBA-2774-4851-9889-6AADDD4CAC3D"
+    assert result2["result"].unique_id == "aa:bb:cc:dd:ee:ff"
 
 
 async def test_async_step_user_device_added_between_steps(hass):
     """Test the device gets added via another flow between steps."""
     with patch(
-        "homeassistant.components.mopeka_ble.config_flow.async_discovered_service_info",
+        "homeassistant.components.mopeka.config_flow.async_discovered_service_info",
         return_value=[PRO_SERVICE_INFO],
     ):
         result = await hass.config_entries.flow.async_init(
@@ -93,16 +89,14 @@ async def test_async_step_user_device_added_between_steps(hass):
 
     entry = MockConfigEntry(
         domain=DOMAIN,
-        unique_id="4125DDBA-2774-4851-9889-6AADDD4CAC3D",
+        unique_id="aa:bb:cc:dd:ee:ff",
     )
     entry.add_to_hass(hass)
 
-    with patch(
-        "homeassistant.components.mopeka_ble.async_setup_entry", return_value=True
-    ):
+    with patch("homeassistant.components.mopeka.async_setup_entry", return_value=True):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            user_input={"address": "4125DDBA-2774-4851-9889-6AADDD4CAC3D"},
+            user_input={"address": "aa:bb:cc:dd:ee:ff"},
         )
     assert result2["type"] == FlowResultType.ABORT
     assert result2["reason"] == "already_configured"
@@ -112,12 +106,12 @@ async def test_async_step_user_with_found_devices_already_setup(hass):
     """Test setup from service info cache with devices found."""
     entry = MockConfigEntry(
         domain=DOMAIN,
-        unique_id="4125DDBA-2774-4851-9889-6AADDD4CAC3D",
+        unique_id="aa:bb:cc:dd:ee:ff",
     )
     entry.add_to_hass(hass)
 
     with patch(
-        "homeassistant.components.mopeka_ble.config_flow.async_discovered_service_info",
+        "homeassistant.components.mopeka.config_flow.async_discovered_service_info",
         return_value=[PRO_SERVICE_INFO],
     ):
         result = await hass.config_entries.flow.async_init(
@@ -132,7 +126,7 @@ async def test_async_step_bluetooth_devices_already_setup(hass):
     """Test we can't start a flow if there is already a config entry."""
     entry = MockConfigEntry(
         domain=DOMAIN,
-        unique_id="4125DDBA-2774-4851-9889-6AADDD4CAC3D",
+        unique_id="aa:bb:cc:dd:ee:ff",
     )
     entry.add_to_hass(hass)
 
@@ -175,7 +169,7 @@ async def test_async_step_user_takes_precedence_over_discovery(hass):
     assert result["step_id"] == "bluetooth_confirm"
 
     with patch(
-        "homeassistant.components.mopeka_ble.config_flow.async_discovered_service_info",
+        "homeassistant.components.mopeka.config_flow.async_discovered_service_info",
         return_value=[PRO_SERVICE_INFO],
     ):
         result = await hass.config_entries.flow.async_init(
@@ -184,17 +178,15 @@ async def test_async_step_user_takes_precedence_over_discovery(hass):
         )
         assert result["type"] == FlowResultType.FORM
 
-    with patch(
-        "homeassistant.components.mopeka_ble.async_setup_entry", return_value=True
-    ):
+    with patch("homeassistant.components.mopeka.async_setup_entry", return_value=True):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            user_input={"address": "4125DDBA-2774-4851-9889-6AADDD4CAC3D"},
+            user_input={"address": "aa:bb:cc:dd:ee:ff"},
         )
     assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "H5177 2EC8"
+    assert result2["title"] == "Pro+ EEFF"
     assert result2["data"] == {}
-    assert result2["result"].unique_id == "4125DDBA-2774-4851-9889-6AADDD4CAC3D"
+    assert result2["result"].unique_id == "aa:bb:cc:dd:ee:ff"
 
     # Verify the original one was aborted
     assert not hass.config_entries.flow.async_progress(DOMAIN)
