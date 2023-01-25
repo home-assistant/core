b105068b4c0a6cb079489c46ccd64950b36a3c9c
Jon Caruana <jon@joncaruana.com>
2023-01-25 14:52:15 +0000

    Revert "PR feedback"
    This reverts commit 13516fa3c7aa9fcd498448da1ae729d66890aab0.

--- homeassistant/components/litejet/switch.py
+++ homeassistant/components/litejet/switch.py
@@ -58,15 +58,15 @@ class LiteJetSwitch(SwitchEntity):
 
     def _on_switch_pressed(self):
         self._attr_is_on = True
-        self.async_write_ha_state()
+        self.schedule_update_ha_state()
 
     def _on_switch_released(self):
         self._attr_is_on = False
-        self.async_write_ha_state()
+        self.schedule_update_ha_state()
 
     def _on_connected_changed(self, connected: bool, reason: str) -> None:
         self._attr_available = connected
-        self.async_write_ha_state()
+        self.schedule_update_ha_state()
 
     @property
     def unique_id(self):
