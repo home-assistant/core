4ec351e61ae55fdf20970d86bd10cf9006703e99
Paulus Schoutsen <balloob@gmail.com>
2023-01-25 08:46:37 -0500

    Update homeassistant/components/openai_conversation/config_flow.py
    Co-authored-by: Franck Nijhof <git@frenck.dev>

--- homeassistant/components/openai_conversation/config_flow.py
+++ homeassistant/components/openai_conversation/config_flow.py
@@ -63,7 +63,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
         else:
-            return self.async_create_entry(title="", data=user_input)
+            return self.async_create_entry(title="OpenAI Conversation", data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
