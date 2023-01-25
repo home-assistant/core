15dbdbf2f50a86ea2fc30acc4f75cdf4904d09d5
jbouwh <jan@jbsoft.nl>
2023-01-25 10:54:11 +0000

    Add state report test

--- tests/components/alexa/test_state_report.py
+++ tests/components/alexa/test_state_report.py
@@ -333,6 +333,71 @@ async def test_report_state_humidifier(hass, aioclient_mock):
     assert call_json["event"]["endpoint"]["endpointId"] == "humidifier#test_humidifier"
 
 
+@pytest.mark.parametrize(
+    "domain,value",
+    [
+        ("number", 50),
+        ("input_number", 40),
+        ("number", 20.5),
+        ("input_number", 40.5),
+    ],
+)
+async def test_report_state_number(hass, aioclient_mock, domain, value):
+    """Test proactive state reports with number or input_number instance."""
+    aioclient_mock.post(TEST_URL, text="", status=202)
+
+    hass.states.async_set(
+        f"{domain}.test_{domain}",
+        None,
+        {
+            "friendly_name": f"Test {domain}",
+            "min": 10,
+            "max": 100,
+            "step": 0.1,
+        },
+    )
+
+    await state_report.async_enable_proactive_mode(hass, get_default_config(hass))
+
+    hass.states.async_set(
+        f"{domain}.test_{domain}",
+        value,
+        {
+            "friendly_name": f"Test {domain}",
+            "min": 10,
+            "max": 100,
+            "step": 0.1,
+        },
+    )
+
+    # To trigger event listener
+    await hass.async_block_till_done()
+
+    assert len(aioclient_mock.mock_calls) == 1
+    call = aioclient_mock.mock_calls
+
+    call_json = call[0][2]
+    assert call_json["event"]["header"]["namespace"] == "Alexa"
+    assert call_json["event"]["header"]["name"] == "ChangeReport"
+
+    change_reports = call_json["event"]["payload"]["change"]["properties"]
+
+    checks = 0
+    for report in change_reports:
+        if report["name"] == "connectivity":
+            assert report["value"] == {"value": "OK"}
+            assert report["namespace"] == "Alexa.EndpointHealth"
+            checks += 1
+        if report["name"] == "rangeValue":
+            assert report["value"] == value
+            assert report["instance"] == f"{domain}.value"
+            assert report["namespace"] == "Alexa.RangeController"
+            checks += 1
+    assert checks == 2
+
+    assert call_json["event"]["endpoint"]["endpointId"] == f"{domain}#test_{domain}"
+
+
 async def test_send_add_or_update_message(hass, aioclient_mock):
     """Test sending an AddOrUpdateReport message."""
     aioclient_mock.post(TEST_URL, text="")
