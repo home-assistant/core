d2dcc6ac887dddf059940a130644d50473a03c75
Petro31 <mhpetrosino@gmail.com>
2023-01-26 21:34:21 +0000

    fix zone_sleep select

--- homeassistant/components/yamaha_musiccast/select.py
+++ homeassistant/components/yamaha_musiccast/select.py
@@ -9,11 +9,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import DOMAIN, MusicCastCapabilityEntity, MusicCastDataUpdateCoordinator
-from .const import (
-    STATE_ZONE_SLEEP_MAPPING,
-    TRANSLATION_KEY_MAPPING,
-    ZONE_SLEEP_STATE_MAPPING,
-)
+from .const import TRANSLATION_KEY_MAPPING
 
 
 async def async_setup_entry(
@@ -47,11 +43,16 @@ class SelectableCapapility(MusicCastCapabilityEntity, SelectEntity):
 
     async def async_select_option(self, option: str) -> None:
         """Select the given option."""
-        value = {val: key for key, val in self.capability.options.items()}[option]
-        # If the translation key is "zone_sleep", we need to translate
-        # Home Assistant state back to the MusicCast value
-        if self.translation_key == "zone_sleep":
-            value = STATE_ZONE_SLEEP_MAPPING[value]
+        # the zone_sleep capability is a dict[int, str].  The dictionary value
+        # has spaces in it.  Translations require sluggified options, i.e. no spaces or integers
+        # To ensure that, return a dictionary that maps the stringified key to the key.
+        # All other capabilities do not have this issue.
+        values = (
+            {str(key): key for key in self.capability.options.keys()}
+            if self.translation_key == "zone_sleep"
+            else {val: key for key, val in self.capability.options.items()}
+        )
+        value = values[option]
         await self.capability.set(value)
 
     @property
@@ -62,20 +63,21 @@ class SelectableCapapility(MusicCastCapabilityEntity, SelectEntity):
     @property
     def options(self) -> list[str]:
         """Return the list possible options."""
-        # If the translation key is "zone_sleep", we need to translate
-        # the options to make them compatible with Home Assistant
-        if self.translation_key == "zone_sleep":
-            return list(STATE_ZONE_SLEEP_MAPPING)
-        return list(self.capability.options.values())
+        # the zone_sleep capability is a dict[int, str].  The dictionary value
+        # has spaces in it.  Translations require sluggified options, i.e. no spaces or integers
+        # To ensure that, return a list of strings.  This will allow us to map the
+        # stringified key to the capability option.
+        # All other capabilities do not have this issue.
+        options = (
+            self.capability.options.keys()
+            if self.translation_key == "zone_sleep"
+            else self.capability.options.values()
+        )
+        return [str(x) for x in options]
 
     @property
     def current_option(self) -> str | None:
         """Return the currently selected option."""
-        # If the translation key is "zone_sleep", we need to translate
-        # the value to make it compatible with Home Assistant
-        if (
-            value := self.capability.current
-        ) is not None and self.translation_key == "zone_sleep":
-            return ZONE_SLEEP_STATE_MAPPING[value]
-
-        return value
+        if (value := self.capability.current) is None:
+            return None
+        return str(value)
