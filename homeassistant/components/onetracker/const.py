96d4211cce0121a5b93f26217ceb469faee97991
Ethan Leisinger <packruler@gmail.com>
2023-01-29 23:03:17 +0000

    Add Final typing to const.py

--- homeassistant/components/onetracker/const.py
+++ homeassistant/components/onetracker/const.py
@@ -1,19 +1,20 @@
 """Constants for OneTracker."""
 
 from datetime import timedelta
+from typing import Final
 
-ATTR_CARRIER = "carrier"
-ATTR_CARRIER_NAME = "carrier_name"
-ATTR_TIME_UPDATED = "time_updated"
-ATTR_TRACKING_ID = "tracking_id"
-ATTR_TRACKING_LOCATION = "tracking_location"
-ATTR_TRACKING_STATUS_READABLE = "tracking_status_readable"
-ATTR_TRACKING_STATUS_DESCRIPTION = "tracking_status_description"
-ATTR_TRACKING_TIME_ESTIMATED = "tracking_time_estimated"
-ATTR_TRACKING_TIME_DELIVERED = "tracking_time_delivered"
+ATTR_CARRIER: Final = "carrier"
+ATTR_CARRIER_NAME: Final = "carrier_name"
+ATTR_TIME_UPDATED: Final = "time_updated"
+ATTR_TRACKING_ID: Final = "tracking_id"
+ATTR_TRACKING_LOCATION: Final = "tracking_location"
+ATTR_TRACKING_STATUS_READABLE: Final = "tracking_status_readable"
+ATTR_TRACKING_STATUS_DESCRIPTION: Final = "tracking_status_description"
+ATTR_TRACKING_TIME_ESTIMATED: Final = "tracking_time_estimated"
+ATTR_TRACKING_TIME_DELIVERED: Final = "tracking_time_delivered"
 
-DEFAULT_NAME = "OneTracker"
-DOMAIN = "onetracker"
+DEFAULT_NAME: Final = "OneTracker"
+DOMAIN: Final = "onetracker"
 
-PARALLEL_UPDATES = 1
-SCAN_INTERVAL = timedelta(minutes=15)
+PARALLEL_UPDATES: Final = 1
+SCAN_INTERVAL: Final = timedelta(minutes=15)
