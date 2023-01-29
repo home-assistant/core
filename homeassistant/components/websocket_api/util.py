0166410ab555fdd1c5ac590d8256aef31a3aec30
J. Nick Koston <nick@koston.org>
2023-01-28 17:14:06 -1000

    preserve text for backcompat

--- homeassistant/components/websocket_api/util.py
+++ homeassistant/components/websocket_api/util.py
@@ -7,7 +7,7 @@ from aiohttp import web
 
 def describe_request(request: web.Request) -> str:
     """Describe a request."""
-    description = f" @ {request.remote}"
+    description = f" from {request.remote}"
     if user_agent := request.headers.get("user-agent"):
         description += f" ({user_agent})"
     return description
