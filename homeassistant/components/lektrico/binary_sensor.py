f03da59a35f764e8416dbc65fd363a4685f12959
mtarjoianu <mihaela_tarjoianu@yahoo.com>
2023-01-30 14:42:36 +0000

    Come back to lektricowifi==0.0.20 and state_machine_e_activated (until new release appears for the charger; current release is 1.44).

--- homeassistant/components/lektrico/binary_sensor.py
+++ homeassistant/components/lektrico/binary_sensor.py
@@ -39,10 +39,12 @@ class HasActiveErrorsBinarySensorEntityDescription(
     @classmethod
     def set_extra_state_att(cls, lektrico_binary_sensor: LektricoBinarySensor) -> None:
         """Get the has_active_errors."""
-        if hasattr(lektrico_binary_sensor.coordinator.data, "state_e_activated"):
+        if hasattr(
+            lektrico_binary_sensor.coordinator.data, "state_machine_e_activated"
+        ):
             # error types exist => set their values in _attr_extra_state_attributes
             lektrico_binary_sensor.set_attr_extra_state_attributes_for_errors(
-                lektrico_binary_sensor.coordinator.data.state_e_activated,
+                lektrico_binary_sensor.coordinator.data.state_machine_e_activated,
                 lektrico_binary_sensor.coordinator.data.overtemp,
                 lektrico_binary_sensor.coordinator.data.critical_temp,
                 lektrico_binary_sensor.coordinator.data.overcurrent,
@@ -108,7 +110,7 @@ class LektricoBinarySensor(CoordinatorEntity, BinarySensorEntity):
         # add extra_state_attributes for HasActiveErrorsBinarySensorEntityDescription
         if isinstance(description, HasActiveErrorsBinarySensorEntityDescription):
             self._attr_extra_state_attributes = {
-                "state_e_activated": "",
+                "state_machine_e_activated": "",
                 "overtemp": "",
                 "critical_temp": "",
                 "overcurrent": "",
@@ -140,7 +142,7 @@ class LektricoBinarySensor(CoordinatorEntity, BinarySensorEntity):
         value7: bool,
     ) -> None:
         """Set _attr_extra_state_attributes for HasActiveErrors binary sensor."""
-        self._attr_extra_state_attributes["state_e_activated"] = value1
+        self._attr_extra_state_attributes["state_machine_e_activated"] = value1
         self._attr_extra_state_attributes["overtemp"] = value2
         self._attr_extra_state_attributes["critical_temp"] = value3
         self._attr_extra_state_attributes["overcurrent"] = value4
