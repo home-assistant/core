7091af97e3113a1740d6dd168fcc0400765369cc
Mike Kasper <m_kasper@sbcglobal.net>
2023-01-31 12:55:44 +0000

    Logic error around setpoint and auto mode

--- homeassistant/components/honeywell/climate.py
+++ homeassistant/components/honeywell/climate.py
@@ -309,7 +309,9 @@ class HoneywellUSThermostat(ClimateEntity):
 
     async def async_set_temperature(self, **kwargs: Any) -> None:
         """Set new target temperature."""
-        if {HVACMode.COOL, HVACMode.HEAT} & set(self._hvac_mode_map):
+        if {HVACMode.COOL, HVACMode.HEAT, HVACMode.HEAT_COOL} & set(
+            self._hvac_mode_map
+        ):
             await self._set_temperature(**kwargs)
             try:
                 if temperature := kwargs.get(ATTR_TARGET_TEMP_HIGH):
