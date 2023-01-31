4b2fbdb89f7e1dde1cf60009c3df32f15f4f0790
epenet <6771947+epenet@users.noreply.github.com>
2023-01-31 07:00:17 +0000

    Rename class

--- tests/typing.py
+++ tests/typing.py
@@ -1,16 +1,11 @@
 """Typing helpers for Home Assistant tests."""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, TypeAlias
+from collections.abc import Callable, Coroutine
+from typing import Any
 
-if TYPE_CHECKING:
-    from collections.abc import Callable, Coroutine
-    from typing import Any
+from aiohttp import ClientWebSocketResponse
+from aiohttp.test_utils import TestClient
 
-    from aiohttp import ClientWebSocketResponse
-    from aiohttp.test_utils import TestClient
-
-TestClientGenerator: TypeAlias = "Callable[..., Coroutine[Any, Any, TestClient]]"
-TestWebSocketGenerator: TypeAlias = (
-    "Callable[..., Coroutine[Any, Any, ClientWebSocketResponse]]"
-)
+ClientSessionGenerator = Callable[..., Coroutine[Any, Any, TestClient]]
+WebSocketGenerator = Callable[..., Coroutine[Any, Any, ClientWebSocketResponse]]
