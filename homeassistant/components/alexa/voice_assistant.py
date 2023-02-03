c37fb3966f4e7d731bda7053a91d18cbf48a0012
Pedro Menezes Rodiguero <56134679+pedro-rodiguero@users.noreply.github.com>
2023-02-03 18:09:10 -0300

    refactor (trigger_action) use dict for action handling

--- homeassistant/components/alexa/voice_assistant.py
+++ homeassistant/components/alexa/voice_assistant.py
@@ -1,22 +1,34 @@
 import homeassistant.components.light
-import homeassistant.components.media_
+import homeassistant.components.media_player
 
 def trigger_action(hass, intent):
     """Triggers the appropriate action based on the identified intent."""
-    if intent == "turn_on_lights":
-        entity_id = "light.bedroom"
-        if hass.states.get(entity_id) is None:
-            raise ValueError(f"Invalid entity id: {entity_id}")
-        hass.services.call("light", "turn_on", {"entity_id": entity_id})
-    elif intent == "play_music":
-        entity_id = "media_player.living_room"
-        if hass.states.get(entity_id) is None:
-            raise ValueError(f"Invalid entity id: {entity_id}")
-        hass.services.call("media_player", "play_media", {"entity_id": entity_id, "media_content_id": "https://www.example.com/music.mp3", "media_content_type": "audio"})
-    elif intent == "stop_music":
-        entity_id = "media_player.living_room"
-        if hass.states.get(entity_id) is None:
-            raise ValueError(f"Invalid entity id: {entity_id}")
-        hass.services.call("media_player", "stop", {"entity_id": entity_id})
-    else:
+    actions = {
+        "turn_on_lights": {
+            "domain": "light",
+            "service": "turn_on",
+            "entity_id": "light.bedroom"
+        },
+        "play_music": {
+            "domain": "media_player",
+            "service": "play_media",
+            "entity_id": "media_player.living_room",
+            "media_content_id": "https://www.example.com/music.mp3",
+            "media_content_type": "audio"
+        },
+        "stop_music": {
+            "domain": "media_player",
+            "service": "stop",
+            "entity_id": "media_player.living_room"
+        }
+    }
+
+    action = actions.get(intent)
+    if not action:
         raise ValueError(f"Unsupported intent: {intent}")
+
+    entity_id = action["entity_id"]
+    if hass.states.get(entity_id) is None:
+        raise ValueError(f"Invalid entity id: {entity_id}")
+
+    hass.services.call(action["domain"], action["service"], action)
