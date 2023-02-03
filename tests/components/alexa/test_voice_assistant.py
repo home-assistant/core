e5435320f3cb8bea23698b6f874553a0d7a5e810
Pedro Menezes Rodiguero <56134679+pedro-rodiguero@users.noreply.github.com>
2023-02-03 17:43:34 -0300

    test (coverage ) improved for voice processing

--- tests/components/alexa/test_voice_assistant.py
+++ tests/components/alexa/test_voice_assistant.py
@@ -1,16 +1,22 @@
-import pytest
-from homeassistant.components.voice_assistant import process_voice_input
-from homeassistant.core import HomeAssistant
-from typing import ParamSpec
+import unittest
+from homeassistant.components.voice_assistant import identify_intent
 
 class TestVoiceProcessing(unittest.TestCase):
     def test_identify_intent(self):
-    # Test input "turn on the lights" returns "turn_on_lights"
-    self.assertEqual(identify_intent("turn on the lights"), "turn_on_lights")
+        # Test input "turn on the lights" returns "turn_on_lights"
+        self.assertEqual(identify_intent("turn on the lights"), "turn_on_lights")
 
-    # Test input "play music" returns "play_music"
-    self.assertEqual(identify_intent("play music"), "play_music")
+        # Test input "play music" returns "play_music"
+        self.assertEqual(identify_intent("play music"), "play_music")
 
-    # Add more tests as necessary to cover additional functionality
-    # Test input "stop music" returns "stop_music"
-    self.assertEqual(identify_intent("stop music"), "stop_music")
+        # Test input "stop music" returns "stop_music"
+        self.assertEqual(identify_intent("stop music"), "stop_music")
+
+        # Test input "change the temperature to 25 degrees" returns "change_temperature"
+        self.assertEqual(identify_intent("change the temperature to 25 degrees"), "change_temperature")
+
+        # Test input "lock the front door" returns "lock_door"
+        self.assertEqual(identify_intent("lock the front door"), "lock_door")
+
+        # Test input "what is the weather like today?" returns "get_weather"
+        self.assertEqual(identify_intent("what is the weather like today?"), "get_weather")
