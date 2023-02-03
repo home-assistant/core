009c1b77cab1e1c765a8e8e46d8ae8e87fd0e41e
J. Nick Koston <nick@koston.org>
2023-02-03 14:27:56 -0600

    bump it

--- homeassistant/components/recorder/db_schema.py
+++ homeassistant/components/recorder/db_schema.py
@@ -55,7 +55,7 @@ from .models import StatisticData, StatisticMetaData, process_timestamp
 # pylint: disable=invalid-name
 Base = declarative_base()
 
-SCHEMA_VERSION = 33
+SCHEMA_VERSION = 34
 
 _StatisticsBaseSelfT = TypeVar("_StatisticsBaseSelfT", bound="StatisticsBase")
 
