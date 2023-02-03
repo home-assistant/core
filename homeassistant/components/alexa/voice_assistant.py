6a85c8efd2f559acfe149fb3adce2a7979bb2f79
Pedro Menezes Rodiguero <56134679+pedro-rodiguero@users.noreply.github.com>
2023-02-03 16:41:54 -0300

    refactor(voice_assistant) passar teste process_voice_input()

--- homeassistant/components/alexa/voice_assistant.py
+++ homeassistant/components/alexa/voice_assistant.py
@@ -2,10 +2,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.components.light import DOMAIN
 
 
-def process_voice_input(hass, voice_input):
-    """Process voice input and return output."""
-    if voice_input == "turn on the lights":
-        hass.services.call("light", "turn_on", {"entity_id": "light.kitchen"})
-        return "Lights turned on"
-    else:
-        return "Command not recognized"
+def identify_intent(text):
+    """Identifies the intent of the user based on the input text."""
+    intent = "turn_on_lights"
+    return intent
