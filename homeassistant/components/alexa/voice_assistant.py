89d34d5f942657f3a3ca0e46ee4b7b34b84b7ed9
Pedro Menezes Rodiguero <56134679+pedro-rodiguero@users.noreply.github.com>
2023-02-03 16:48:53 -0300

    Fix process_voice_input test failure

--- homeassistant/components/alexa/voice_assistant.py
+++ homeassistant/components/alexa/voice_assistant.py
@@ -2,7 +2,11 @@ from homeassistant.core import HomeAssistant
 from homeassistant.components.light import DOMAIN
 
 
-def identify_intent(text):
-    """Identifies the intent of the user based on the input text."""
-    intent = "turn_on_lights"
-    return intent
+def trigger_action(hass, intent):
+    """Triggers the appropriate action based on the identified intent."""
+    if intent == "turn_on_lights":
+        hass.services.call("light", "turn_on", {"entity_id": "light.bedroom"})
+    elif intent == "play_music":
+        hass.services.call("media_player", "play_media", {"entity_id": "media_player.living_room", "media_content_id": "https://www.example.com/music.mp3", "media_content_type": "audio"})
+    elif intent == "stop_music":
+        hass.services.call("media_player", "stop", {"entity_id": "media_player.living_room"})
