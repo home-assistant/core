c0cd88c9a7bcd8185a5099643c1e82d2d741177a
Pedro Menezes Rodiguero <56134679+pedro-rodiguero@users.noreply.github.com>
2023-02-03 18:00:30 -0300

    refactor (voice_processing) efficiency and readability

--- tests/components/alexa/test_voice_assistant.py
+++ tests/components/alexa/test_voice_assistant.py
@@ -3,20 +3,33 @@ from homeassistant.components.voice_assistant import identify_intent
 
 class TestVoiceProcessing(unittest.TestCase):
     def test_identify_intent(self):
-        # Test input "turn on the lights" returns "turn_on_lights"
         self.assertEqual(identify_intent("turn on the lights"), "turn_on_lights")
-
-        # Test input "play music" returns "play_music"
         self.assertEqual(identify_intent("play music"), "play_music")
-
-        # Test input "stop music" returns "stop_music"
         self.assertEqual(identify_intent("stop music"), "stop_music")
+        self.assertEqual(identify_intent("set the temperature to 72 degrees"), "set_temperature")
 
-        # Test input "change the temperature to 25 degrees" returns "change_temperature"
-        self.assertEqual(identify_intent("change the temperature to 25 degrees"), "change_temperature")
+    def test_process_voice_input(self):
+        process_voice_input("turn on the lights")
+        self.assertTrue(lights_are_on())
+        
+        process_voice_input("play music")
+        self.assertTrue(music_is_playing())
+        
+        process_voice_input("stop music")
+        self.assertFalse(music_is_playing())
+        
+        process_voice_input("set the temperature to 72 degrees")
+        self.assertEqual(get_temperature(), 72)
 
-        # Test input "lock the front door" returns "lock_door"
-        self.assertEqual(identify_intent("lock the front door"), "lock_door")
-
-        # Test input "what is the weather like today?" returns "get_weather"
-        self.assertEqual(identify_intent("what is the weather like today?"), "get_weather")
+def identify_intent(voice_input):
+    intents = {
+        "turn_on_lights": ["turn", "on", "the", "lights"],
+        "play_music": ["play", "music"],
+        "stop_music": ["stop", "music"],
+    }
+    
+    words = voice_input.split()
+    for intent, keywords in intents.items():
+        if all(word in words for word in keywords):
+            return intent
+    return None
