bf23da1845adc99f1cefa562fd512a0b4a465090
Pedro Menezes Rodiguero <56134679+pedro-rodiguero@users.noreply.github.com>
2023-02-03 16:45:08 -0300

    test(voice_assistant) add func process_voice_input()

--- tests/components/alexa/test_voice_assistant.py
+++ tests/components/alexa/test_voice_assistant.py
@@ -1,7 +1,16 @@
-import unittest
+import pytest
+from homeassistant.components.voice_assistant import process_voice_input
+from homeassistant.core import HomeAssistant
+from typing import ParamSpec
 
 class TestVoiceProcessing(unittest.TestCase):
-    def test_process_voice_input_fails(self):
-        audio = "invalid_file.wav"
-        result = process_voice_input(audio)
-        self.assertIsNone(result)
+    def test_identify_intent(self):
+    # Test input "turn on the lights" returns "turn_on_lights"
+    self.assertEqual(identify_intent("turn on the lights"), "turn_on_lights")
+
+    # Test input "play music" returns "play_music"
+    self.assertEqual(identify_intent("play music"), "play_music")
+
+    # Add more tests as necessary to cover additional functionality
+    # Test input "stop music" returns "stop_music"
+    self.assertEqual(identify_intent("stop music"), "stop_music")
