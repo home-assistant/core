3e50dd2e7a53375fe2f233b773b77fa1a16b0871
Pedro Menezes Rodiguero <56134679+pedro-rodiguero@users.noreply.github.com>
2023-02-03 17:39:29 -0300

    refactor(trigger_action) handle intents, code quality

--- homeassistant/components/alexa/voice_assistant.py
+++ homeassistant/components/alexa/voice_assistant.py
@@ -1,12 +1,19 @@
-from homeassistant.core import HomeAssistant
-from homeassistant.components.light import DOMAIN
-
-
 def trigger_action(hass, intent):
     """Triggers the appropriate action based on the identified intent."""
     if intent == "turn_on_lights":
-        hass.services.call("light", "turn_on", {"entity_id": "light.bedroom"})
+        entity_id = "light.bedroom"
+        if hass.states.get(entity_id) is None:
+            raise ValueError(f"Invalid entity id: {entity_id}")
+        hass.services.call("light", "turn_on", {"entity_id": entity_id})
     elif intent == "play_music":
-        hass.services.call("media_player", "play_media", {"entity_id": "media_player.living_room", "media_content_id": "https://www.example.com/music.mp3", "media_content_type": "audio"})
+        entity_id = "media_player.living_room"
+        if hass.states.get(entity_id) is None:
+            raise ValueError(f"Invalid entity id: {entity_id}")
+        hass.services.call("media_player", "play_media", {"entity_id": entity_id, "media_content_id": "https://www.example.com/music.mp3", "media_content_type": "audio"})
     elif intent == "stop_music":
-        hass.services.call("media_player", "stop", {"entity_id": "media_player.living_room"})
+        entity_id = "media_player.living_room"
+        if hass.states.get(entity_id) is None:
+            raise ValueError(f"Invalid entity id: {entity_id}")
+        hass.services.call("media_player", "stop", {"entity_id": entity_id})
+    else:
+        raise ValueError(f"Unsupported intent: {intent}")
