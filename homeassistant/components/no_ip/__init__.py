b2a6707048bda8e51020d043e5dc0e2d33e6ebed
Cameron Gray <camsaway@gmail.com>
2023-02-05 10:54:54 +0200

    amend as per black suggestion

--- homeassistant/components/no_ip/__init__.py
+++ homeassistant/components/no_ip/__init__.py
@@ -105,9 +105,7 @@ async def _update_no_ip(
             body = await resp.text()
 
             if body.startswith("good") or body.startswith("nochg"):
-                _LOGGER.debug(
-                    "Updating NO-IP success: %s", domain
-                )
+                _LOGGER.debug("Updating NO-IP success: %s", domain)
                 return True
 
             _LOGGER.warning(
