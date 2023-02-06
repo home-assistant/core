f019b290ff3bd303c61471e397505a369d2244fc
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2023-02-06 11:16:38 +0000

    Increase coverage of files not previously covered.

---
+++ tests/components/hassmpris/test_cert_data.py
@@ -0,0 +1,20 @@
+"""Test the MPRIS media playback remote control config flow."""
+import pytest
+
+from homeassistant.components.hassmpris.cert_data import CertStore
+from homeassistant.core import HomeAssistant
+
+
+async def test_no_cert_data_raises_keyerror(hass: HomeAssistant) -> None:
+    """Test loading absent certificate data raises KeyError."""
+
+    c = CertStore(hass, "nonexistent")
+    with pytest.raises(KeyError):
+        await c.load_cert_data()
+
+
+async def test_removals_raise_no_exception(hass: HomeAssistant) -> None:
+    """Test removing certificate data raises no exception."""
+
+    c = CertStore(hass, "nonexistent")
+    await c.async_remove()
