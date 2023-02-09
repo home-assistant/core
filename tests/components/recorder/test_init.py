4041bfe242cbec94e3a6eba3c7e15b8863b89d7f
Erik Montnemery <erik@montnemery.com>
2023-02-09 18:43:14 +0100

    Really ensure recorder test fixture is setup before hass fixture (#87796)

--- tests/components/recorder/test_init.py
+++ tests/components/recorder/test_init.py
@@ -324,7 +324,7 @@ async def test_saving_state_with_intermixed_time_changes(
         assert db_states[0].event_id is None
 
 
-def test_saving_state_with_exception(hass, hass_recorder, caplog):
+def test_saving_state_with_exception(hass_recorder, hass, caplog):
     """Test saving and restoring a state."""
     hass = hass_recorder()
 
@@ -362,7 +362,7 @@ def test_saving_state_with_exception(hass, hass_recorder, caplog):
     assert "Error saving events" not in caplog.text
 
 
-def test_saving_state_with_sqlalchemy_exception(hass, hass_recorder, caplog):
+def test_saving_state_with_sqlalchemy_exception(hass_recorder, hass, caplog):
     """Test saving state when there is an SQLAlchemyError."""
     hass = hass_recorder()
 
@@ -430,7 +430,7 @@ async def test_force_shutdown_with_queue_of_writes_that_generate_exceptions(
     assert "Error saving events" not in caplog.text
 
 
-def test_saving_event(hass, hass_recorder):
+def test_saving_event(hass_recorder):
     """Test saving and restoring an event."""
     hass = hass_recorder()
 
@@ -690,7 +690,7 @@ def test_saving_state_include_domain_glob_exclude_entity(hass_recorder):
     assert _state_with_context(hass, "test.ok").state == "state2"
 
 
-def test_saving_state_and_removing_entity(hass, hass_recorder):
+def test_saving_state_and_removing_entity(hass_recorder):
     """Test saving the state of a removed entity."""
     hass = hass_recorder()
     entity_id = "lock.mine"
@@ -1255,7 +1255,7 @@ def test_has_services(hass_recorder):
     assert hass.services.has_service(DOMAIN, SERVICE_PURGE_ENTITIES)
 
 
-def test_service_disable_events_not_recording(hass, hass_recorder):
+def test_service_disable_events_not_recording(hass_recorder):
     """Test that events are not recorded when recorder is disabled using service."""
     hass = hass_recorder()
 
@@ -1330,7 +1330,7 @@ def test_service_disable_events_not_recording(hass, hass_recorder):
     )
 
 
-def test_service_disable_states_not_recording(hass, hass_recorder):
+def test_service_disable_states_not_recording(hass_recorder):
     """Test that state changes are not recorded when recorder is disabled using service."""
     hass = hass_recorder()
 
