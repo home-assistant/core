cd6a6906403dc12a833318eb37cb9f05eba5972c
Luke <conway220@gmail.com>
2023-02-09 20:05:22 +0000

    Pulled auth into seperate function to not repeat

--- homeassistant/components/southern_company/config_flow.py
+++ homeassistant/components/southern_company/config_flow.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from collections.abc import Mapping
 import logging
+import typing
 from typing import Any
 
 from southern_company_api.parser import (
@@ -33,29 +34,34 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
+    async def async_authenticate(
+        self, user_input: Mapping[str, Any], errors: dict[str, str]
+    ) -> typing.Optional[FlowResult]:
+        """Handle authentication for all flows to reduce repetition of code."""
+        sca = SouthernCompanyAPI(user_input["username"], user_input["password"])
+        try:
+            await sca.authenticate()
+        except CantReachSouthernCompany:
+            errors["base"] = "cannot_connect"
+        except InvalidLogin:
+            errors["base"] = "invalid_auth"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            return self.async_create_entry(title="Southern Company", data=user_input)
+        return None
+
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
-        errors = {}
+        errors: dict[str, str] = {}
         if user_input is not None:
             self._async_abort_entries_match({CONF_USERNAME: user_input[CONF_USERNAME]})
-
-            sca = SouthernCompanyAPI(user_input["username"], user_input["password"])
-            try:
-                await sca.authenticate()
-            except CantReachSouthernCompany:
-                errors["base"] = "cannot_connect"
-            except InvalidLogin:
-                errors["base"] = "invalid_auth"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-            else:
-                return self.async_create_entry(
-                    title="Southern Company", data=user_input
-                )
-
+            auth = await self.async_authenticate(user_input, errors)
+            if auth is not None:
+                return auth
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
@@ -68,28 +74,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: Mapping[str, Any] | None = None
     ) -> FlowResult:
         """Handle a flow initiated by reauthentication."""
-        errors = {}
+        errors: dict[str, str] = {}
         if user_input is not None:
             data_schema = {
                 vol.Required(CONF_USERNAME, default=user_input[CONF_USERNAME]): str,
                 vol.Required(CONF_PASSWORD): str,
             }
-
-            sca = SouthernCompanyAPI(user_input["username"], user_input["password"])
-            try:
-                await sca.authenticate()
-            except CantReachSouthernCompany:
-                errors["base"] = "cannot_connect"
-            except InvalidLogin:
-                errors["base"] = "invalid_auth"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-            else:
-                return self.async_create_entry(
-                    title="Southern Company", data=user_input
-                )
-
+            auth = await self.async_authenticate(user_input, errors)
+            if auth is not None:
+                return auth
         return self.async_show_form(
             step_id="reauth_confirm", data_schema=data_schema, errors=errors
         )
