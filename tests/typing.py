3cc1f35f6b268988b7319525f567a492f10664d0
epenet <6771947+epenet@users.noreply.github.com>
2023-02-10 16:23:26 +0100

    Add MockHAClientWebSocket test helper (#87832)
    * Add MockHAClientWebSocket test helper
    
    * Add sample use
    
    * Add missing type hint

--- tests/typing.py
+++ tests/typing.py
@@ -13,6 +13,14 @@ if TYPE_CHECKING:
     # testcase which does not use the recorder.
     from homeassistant.components.recorder import Recorder
 
+
+class MockHAClientWebSocket(ClientWebSocketResponse):
+    """Protocol for a wrapped ClientWebSocketResponse."""
+
+    client: TestClient
+    send_json_auto_id: Callable[[dict[str, Any]], Coroutine[Any, Any, None]]
+
+
 ClientSessionGenerator = Callable[..., Coroutine[Any, Any, TestClient]]
 MqttMockPahoClient = MagicMock
 """MagicMock for `paho.mqtt.client.Client`"""
@@ -22,4 +30,4 @@ MqttMockHAClientGenerator = Callable[..., Coroutine[Any, Any, MqttMockHAClient]]
 """MagicMock generator for `homeassistant.components.mqtt.MQTT`."""
 RecorderInstanceGenerator: TypeAlias = Callable[..., Coroutine[Any, Any, "Recorder"]]
 """Instance generator for `homeassistant.components.recorder.Recorder`."""
-WebSocketGenerator = Callable[..., Coroutine[Any, Any, ClientWebSocketResponse]]
+WebSocketGenerator = Callable[..., Coroutine[Any, Any, MockHAClientWebSocket]]
