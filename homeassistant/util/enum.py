bd836f13c9d7b59811a476ade0cfde541f7e15d8
J. Nick Koston <nick@koston.org>
2023-02-09 20:19:38 -0600

    enum cache

--- homeassistant/util/enum.py
+++ homeassistant/util/enum.py
@@ -1,11 +1,13 @@
 """Helpers for working with enums."""
 import contextlib
 from enum import Enum
+from functools import lru_cache
 from typing import Any, TypeVar
 
 _EnumT = TypeVar("_EnumT", bound=Enum)
 
 
+@lru_cache(maxsize=256)
 def try_parse_enum(cls: type[_EnumT], value: Any) -> _EnumT | None:
     """Try to parse the value into an Enum.
 
