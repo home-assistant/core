56787b877cba99b49bbcdff1c24e175658a87edb
Arem Draft <artemon_93@mail.ru>
2023-02-10 17:41:15 +0300

    Pass assumed_state property in universal media player

--- homeassistant/components/universal/media_player.py
+++ homeassistant/components/universal/media_player.py
@@ -46,6 +46,7 @@ from homeassistant.components.media_player import (
     MediaPlayerState,
 )
 from homeassistant.const import (
+    ATTR_ASSUMED_STATE,
     ATTR_ENTITY_ID,
     ATTR_ENTITY_PICTURE,
     ATTR_SUPPORTED_FEATURES,
@@ -301,6 +302,11 @@ class UniversalMediaPlayer(MediaPlayerEntity):
     def name(self):
         """Return the name of universal player."""
         return self._name
+
+    @property
+    def assumed_state(self) -> bool:
+        """Return True if unable to access real state of the entity."""
+        return self._child_attr(ATTR_ASSUMED_STATE)
 
     @property
     def state(self):
