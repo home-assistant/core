627cee3767a4b455e6311fb738ae75c8c8a58de6
J. Nick Koston <nick@koston.org>
2023-02-11 17:15:22 -0600

    tweak

--- homeassistant/util/enum.py
+++ homeassistant/util/enum.py
@@ -2,23 +2,22 @@
 from collections.abc import Callable
 import contextlib
 from enum import Enum
-from typing import TYPE_CHECKING, Any, ParamSpec, TypeVar
+from typing import TYPE_CHECKING, Any, TypeVar
+
+_EnumT = TypeVar("_EnumT", bound=Enum)
 
 # https://github.com/python/mypy/issues/5107
 if TYPE_CHECKING:
-    _P = ParamSpec("_P")
-    _T = TypeVar("_T")
+    _LruCacheT = TypeVar("_LruCacheT", bound=Callable)
 
-    def lru_cache(maxsize: int | None) -> Callable[..., Callable[_P, _T]]:
+    def lru_cache(func: _LruCacheT) -> _LruCacheT:
         """Stub for lru_cache."""
 
 else:
     from functools import lru_cache
 
-_EnumT = TypeVar("_EnumT", bound=Enum)
 
-
-@lru_cache(1024)
+@lru_cache
 def try_parse_enum(cls: type[_EnumT], value: Any) -> _EnumT | None:
     """Try to parse the value into an Enum.
 
