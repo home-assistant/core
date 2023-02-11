ec788ab790c6f14aff379434bb068ed1bfa10572
J. Nick Koston <nick@koston.org>
2023-02-11 17:00:44 -0600

    mypy

--- homeassistant/util/enum.py
+++ homeassistant/util/enum.py
@@ -1,13 +1,24 @@
 """Helpers for working with enums."""
+from collections.abc import Callable
 import contextlib
 from enum import Enum
-from functools import lru_cache
-from typing import Any, TypeVar
+from typing import TYPE_CHECKING, Any, ParamSpec, TypeVar
+
+# https://github.com/python/mypy/issues/5107
+if TYPE_CHECKING:
+    _P = ParamSpec("_P")
+    _T = TypeVar("_T")
+
+    def lru_cache(maxsize: int | None) -> Callable[..., Callable[_P, _T]]:
+        """Stub for lru_cache."""
+
+else:
+    from functools import lru_cache
 
 _EnumT = TypeVar("_EnumT", bound=Enum)
 
 
-@lru_cache(maxsize=1024)
+@lru_cache(1024)
 def try_parse_enum(cls: type[_EnumT], value: Any) -> _EnumT | None:
     """Try to parse the value into an Enum.
 
