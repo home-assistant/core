cb4fb53d431e8b9602685e5bdabbbe4f56d9c3db
Erik <erik@montnemery.com>
2023-02-12 12:02:53 +0100

    Revert "Simplify user flow"
    This reverts commit 0ef9d1c6bb452b3a06856bc6bf5e81303a33c6b9.

--- tests/components/dormakaba_dkey/__init__.py
+++ tests/components/dormakaba_dkey/__init__.py
@@ -20,3 +20,21 @@ DKEY_DISCOVERY_INFO = BluetoothServiceInfoBleak(
     time=0,
     connectable=True,
 )
+
+
+NOT_DKEY_DISCOVERY_INFO = BluetoothServiceInfoBleak(
+    name="Not",
+    address="AA:BB:CC:DD:EE:F2",
+    rssi=-60,
+    manufacturer_data={
+        33: b"\x00\x00\xd1\xf0b;\xd8\x1dE\xd6\xba\xeeL\xdd]\xf5\xb2\xe9",
+        21: b"\x061\x00Z\x8f\x93\xb2\xec\x85\x06\x00i\x00\x02\x02Q\xed\x1d\xf0",
+    },
+    service_uuids=[],
+    service_data={},
+    source="local",
+    device=BLEDevice(address="AA:BB:CC:DD:EE:F2", name="Aug"),
+    advertisement=generate_advertisement_data(),
+    time=0,
+    connectable=True,
+)
