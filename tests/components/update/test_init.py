ec6d8e63d67ec27bf7c2f6975387d5f9afe428bc
G Johansson <goran.johansson@shiftit.se>
2023-02-12 22:18:09 +0100

    Change name to entity_id update platform error messages (#87960)

--- tests/components/update/test_init.py
+++ tests/components/update/test_init.py
@@ -297,7 +297,7 @@ async def test_entity_with_auto_update(
     # Should not be able to skip the update
     with pytest.raises(
         HomeAssistantError,
-        match="Skipping update is not supported for Update with auto update",
+        match="Skipping update is not supported for update.update_with_auto_update",
     ):
         await hass.services.async_call(
             DOMAIN,
@@ -309,7 +309,7 @@ async def test_entity_with_auto_update(
     # Should not be able to clear a skipped the update
     with pytest.raises(
         HomeAssistantError,
-        match="Clearing skipped update is not supported for Update with auto update",
+        match="Clearing skipped update is not supported for update.update_with_auto_update",
     ):
         await hass.services.async_call(
             DOMAIN,
