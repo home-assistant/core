e0ec07e6baf157ff6ba91fefb6a274faafc41c1c
starkillerOG <starkiller.og@gmail.com>
2023-02-12 17:20:11 +0100

    fix typo

--- homeassistant/components/reolink/__init__.py
+++ homeassistant/components/reolink/__init__.py
@@ -23,7 +23,7 @@ from .host import ReolinkHost
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = [Platform.BINARY_SENSOR, Platform.CAMERA, Platform.NUMBER, Platfrom.SELECT]
+PLATFORMS = [Platform.BINARY_SENSOR, Platform.CAMERA, Platform.NUMBER, Platform.SELECT]
 DEVICE_UPDATE_INTERVAL = 60
 
 
