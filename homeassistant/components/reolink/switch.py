46c23d975de96611fe501be47a8911d3bada02e4
starkillerOG <starkiller.og@gmail.com>
2023-02-12 15:33:54 +0100

    fix isort

--- homeassistant/components/reolink/switch.py
+++ homeassistant/components/reolink/switch.py
@@ -7,10 +7,7 @@ from typing import Any
 
 from reolink_aio.api import Host
 
-from homeassistant.components.switch import (
-    SwitchEntity,
-    SwitchEntityDescription,
-)
+from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
