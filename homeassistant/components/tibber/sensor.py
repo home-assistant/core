08bc7b27d11b0479f874426448400b8473ff0102
Daniel Hjelseth Høyer <github@dahoiv.net>
2023-02-13 19:00:38 +0100

    Fix Tibber stats
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- homeassistant/components/tibber/sensor.py
+++ homeassistant/components/tibber/sensor.py
@@ -636,7 +636,11 @@ class TibberDataCoordinator(DataUpdateCoordinator[None]):
 
                 statistics = []
 
-                last_stats_time_dt = dt_util.utc_from_timestamp(last_stats_time)
+                last_stats_time_dt = (
+                    dt_util.utc_from_timestamp(last_stats_time)
+                    if last_stats_time
+                    else None
+                )
 
                 for data in hourly_data:
                     if data.get(sensor_type) is None:
@@ -644,7 +648,8 @@ class TibberDataCoordinator(DataUpdateCoordinator[None]):
 
                     from_time = dt_util.parse_datetime(data["from"])
                     if from_time is None or (
-                        last_stats_time is not None and from_time <= last_stats_time_dt
+                        last_stats_time_dt is not None
+                        and from_time <= last_stats_time_dt
                     ):
                         continue
 
