f30ed00f4de6be04dd4424102c8763cc8e02203c
Guido Schmitz <guido.schmitz@fedaix.de>
2023-02-13 10:19:05 +0000

    Inherit from DevoloEntity

--- homeassistant/components/devolo_home_network/entity.py
+++ homeassistant/components/devolo_home_network/entity.py
@@ -32,35 +32,6 @@ _DataT = TypeVar(
 )
 
 
-class DevoloCoordinatorEntity(CoordinatorEntity[DataUpdateCoordinator[_DataT]]):
-    """Representation of a coordinated devolo home network device."""
-
-    _attr_has_entity_name = True
-
-    def __init__(
-        self,
-        entry: ConfigEntry,
-        coordinator: DataUpdateCoordinator[_DataT],
-        device: Device,
-    ) -> None:
-        """Initialize a devolo home network device."""
-        super().__init__(coordinator)
-
-        self.device = device
-        self.entry = entry
-
-        self._attr_device_info = DeviceInfo(
-            configuration_url=f"http://{device.ip}",
-            identifiers={(DOMAIN, str(device.serial_number))},
-            manufacturer="devolo",
-            model=device.product,
-            name=entry.title,
-            sw_version=device.firmware_version,
-        )
-        self._attr_translation_key = self.entity_description.key
-        self._attr_unique_id = f"{device.serial_number}_{self.entity_description.key}"
-
-
 class DevoloEntity(Entity):
     """Representation of a devolo home network device."""
 
@@ -84,3 +55,19 @@ class DevoloEntity(Entity):
             sw_version=device.firmware_version,
         )
         self._attr_unique_id = f"{device.serial_number}_{self.entity_description.key}"
+
+
+class DevoloCoordinatorEntity(
+    CoordinatorEntity[DataUpdateCoordinator[_DataT]], DevoloEntity
+):
+    """Representation of a coordinated devolo home network device."""
+
+    def __init__(
+        self,
+        entry: ConfigEntry,
+        coordinator: DataUpdateCoordinator[_DataT],
+        device: Device,
+    ) -> None:
+        """Initialize a devolo home network device."""
+        super().__init__(coordinator)
+        DevoloEntity.__init__(self, entry, device)
