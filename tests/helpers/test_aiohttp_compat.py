6d01e1983392699c0605fe4198a63d8cef9d3582
J. Nick Koston <nick@koston.org>
2023-02-13 21:02:26 -0600

    copy test

--- tests/helpers/test_aiohttp_compat.py
+++ tests/helpers/test_aiohttp_compat.py
@@ -4,7 +4,12 @@ import asyncio
 from contextlib import suppress
 
 from aiohttp import client, web
+import aiohttp.client_exceptions
 import pytest
+
+from homeassistant.helpers.aiohttp_compat import (
+    restore_original_aiohttp_cancel_behavior,
+)
 
 
 @pytest.mark.allow_hosts(["127.0.0.1"])
@@ -13,20 +18,24 @@ async def test_handler_cancellation(socket_enabled, aiohttp_unused_port) -> None
 
     From aiohttp tests/test_web_server.py
     """
-    timeout_event = asyncio.Event()
-    done_event = asyncio.Event()
+    restore_original_aiohttp_cancel_behavior()
+    event = asyncio.Event()
     port = aiohttp_unused_port()
 
     async def on_request(_: web.Request) -> web.Response:
-        nonlocal done_event, timeout_event
-        await asyncio.wait_for(timeout_event.wait(), timeout=5)
-        done_event.set()
-        return web.Response()
+        nonlocal event
+        try:
+            await asyncio.sleep(10)
+        except asyncio.CancelledError:
+            event.set()
+            raise
+        else:
+            raise web.HTTPInternalServerError()
 
     app = web.Application()
     app.router.add_route("GET", "/", on_request)
 
-    runner = web.AppRunner(app)
+    runner = web.AppRunner(app, handler_cancellation=True)
     await runner.setup()
 
     site = web.TCPSite(runner, host="127.0.0.1", port=port)
@@ -37,13 +46,13 @@ async def test_handler_cancellation(socket_enabled, aiohttp_unused_port) -> None
         async with client.ClientSession(
             timeout=client.ClientTimeout(total=0.1)
         ) as sess:
-            with pytest.raises(asyncio.TimeoutError):
+            with pytest.raises(
+                (asyncio.TimeoutError, aiohttp.client_exceptions.ClientOSError)
+            ):
                 await sess.get(f"http://127.0.0.1:{port}/")
-        await asyncio.sleep(0.1)
-        timeout_event.set()
 
         with suppress(asyncio.TimeoutError):
-            await asyncio.wait_for(done_event.wait(), timeout=1)
-        assert done_event.is_set()
+            await asyncio.wait_for(event.wait(), timeout=1)
+        assert event.is_set(), "Request handler hasn't been cancelled"
     finally:
         await asyncio.gather(runner.shutdown(), site.stop())
