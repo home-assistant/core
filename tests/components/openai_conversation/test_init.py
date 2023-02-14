ee1f4e9351999c757abbf4eeea2ced25f6fdcfcf
Paulus Schoutsen <balloob@gmail.com>
2023-02-14 05:45:27 -0500

    Handle device reg fields not being valid data in openai conversion (#88047)
    Handle device reg fields not being valid data

--- tests/components/openai_conversation/test_init.py
+++ tests/components/openai_conversation/test_init.py
@@ -69,14 +69,21 @@ async def test_default_prompt(hass, mock_init_component):
     device_reg.async_update_device(
         device.id, disabled_by=device_registry.DeviceEntryDisabler.USER
     )
-    device = device_reg.async_get_or_create(
+    device_reg.async_get_or_create(
         config_entry_id="1234",
         connections={("test", "9876-no-name")},
         manufacturer="Test Manufacturer NoName",
         model="Test Model NoName",
         suggested_area="Test Area 2",
     )
-
+    device_reg.async_get_or_create(
+        config_entry_id="1234",
+        connections={("test", "9876-integer-values")},
+        name=1,
+        manufacturer=2,
+        model=3,
+        suggested_area="Test Area 2",
+    )
     with patch("openai.Completion.acreate") as mock_create:
         result = await conversation.async_converse(hass, "hello", None, Context())
 
@@ -94,6 +101,7 @@ Test Area 2:
 - Test Device 2
 - Test Device 3 (Test Model 3A)
 - Test Device 4
+- 1 (3)
 
 Answer the users questions about the world truthfully.
 
