601ac3d9a985965f255d7fb27dcea2dacdf1f521
Erik Montnemery <erik@montnemery.com>
2023-02-16 13:54:32 +0100

    Drop unnecessary copy from entitydescription.name to _attr_name (#88251)

--- homeassistant/components/airq/sensor.py
+++ homeassistant/components/airq/sensor.py
@@ -350,7 +350,6 @@ class AirQSensor(CoordinatorEntity, SensorEntity):
         self.entity_description: AirQEntityDescription = description
 
         self._attr_device_info = coordinator.device_info
-        self._attr_name = description.name
         self._attr_unique_id = f"{coordinator.device_id}_{description.key}"
         self._attr_native_value = description.value(coordinator.data)
 
