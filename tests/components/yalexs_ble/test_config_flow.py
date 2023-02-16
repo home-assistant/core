c4a9b87d38bc42510351d42802dcd305bca4d91e
J. Nick Koston <nick@koston.org>
2023-02-16 14:20:53 -0600

    Switch yalexs_ble to use no_devices_found

--- tests/components/yalexs_ble/test_config_flow.py
+++ tests/components/yalexs_ble/test_config_flow.py
@@ -79,7 +79,7 @@ async def test_user_step_no_devices_found(hass: HomeAssistant) -> None:
             DOMAIN, context={"source": config_entries.SOURCE_USER}
         )
     assert result["type"] == FlowResultType.ABORT
-    assert result["reason"] == "no_unconfigured_devices"
+    assert result["reason"] == "no_devices_found"
 
 
 async def test_user_step_no_new_devices_found(hass: HomeAssistant) -> None:
@@ -103,7 +103,7 @@ async def test_user_step_no_new_devices_found(hass: HomeAssistant) -> None:
             DOMAIN, context={"source": config_entries.SOURCE_USER}
         )
     assert result["type"] == FlowResultType.ABORT
-    assert result["reason"] == "no_unconfigured_devices"
+    assert result["reason"] == "no_devices_found"
 
 
 async def test_user_step_invalid_keys(hass: HomeAssistant) -> None:
