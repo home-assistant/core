ee994b85eb0d9bca3338dc86f98a5b85cbb0fd0a
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2023-02-17 03:11:15 +0000

    test(sunweg): improved code coverage

--- tests/components/sunweg/common.py
+++ tests/components/sunweg/common.py
@@ -1,6 +1,7 @@
 """Common functions needed to setup tests for Sun WEG."""
 from datetime import datetime
 
+from sunweg.device import MPPT, Inverter, Phase, String
 from sunweg.plant import Plant
 
 from homeassistant.components.sunweg.const import CONF_PLANT_ID, DOMAIN
@@ -28,6 +29,16 @@ SUNWEG_PLANT_LIST_RESPONSE = [
     )
 ]
 
+SUNWEG_INVERTER_RESPONSE = Inverter(
+    21255, "INVERSOR01", "J63T233018RE074", 23.2, 0.0, 0.0, 0, 0.0, 1, 0
+)
+
+SUNWEG_PHASE_RESPONSE = Phase("PhaseA", 120.0, 3.2, 0, 0)
+
+SUNWEG_MPPT_RESPONSE = MPPT("MPPT1")
+
+SUNWEG_STRING_RESPONSE = String("STR1", 450.3, 23.4, 0)
+
 SUNWEG_LOGIN_RESPONSE = True
 
 SUNWEG_MOCK_ENTRY = MockConfigEntry(
@@ -35,7 +46,7 @@ SUNWEG_MOCK_ENTRY = MockConfigEntry(
     data={
         CONF_USERNAME: "user@email.com",
         CONF_PASSWORD: "password",
-        CONF_PLANT_ID: 12345,
+        CONF_PLANT_ID: 0,
         CONF_NAME: "Name",
     },
 )
--- tests/components/sunweg/test_init.py
+++ tests/components/sunweg/test_init.py
@@ -1,16 +1,54 @@
 """Tests for the Sun WEG init."""
 
+from unittest.mock import patch
+
+from sunweg.plant import Plant
+
 from homeassistant.components.sunweg.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
-from .common import SUNWEG_MOCK_ENTRY
+from .common import (
+    SUNWEG_INVERTER_RESPONSE,
+    SUNWEG_LOGIN_RESPONSE,
+    SUNWEG_MOCK_ENTRY,
+    SUNWEG_MPPT_RESPONSE,
+    SUNWEG_PHASE_RESPONSE,
+    SUNWEG_PLANT_LIST_RESPONSE,
+    SUNWEG_STRING_RESPONSE,
+)
 
 
 async def test_methods(hass: HomeAssistant) -> None:
     """Test methods."""
     mock_entry = SUNWEG_MOCK_ENTRY
     mock_entry.add_to_hass(hass)
-    assert await async_setup_component(hass, DOMAIN, mock_entry)
-    await hass.async_block_till_done()
+    mppt = SUNWEG_MPPT_RESPONSE
+    mppt.strings = [SUNWEG_STRING_RESPONSE]
+    inverter = SUNWEG_INVERTER_RESPONSE
+    inverter.phases = [SUNWEG_PHASE_RESPONSE]
+    inverter.mppts = [mppt]
+    plant: Plant = SUNWEG_PLANT_LIST_RESPONSE[0]
+    plant.inverters = [inverter]
+
+    with patch(
+        "sunweg.api.APIHelper.authenticate", return_value=SUNWEG_LOGIN_RESPONSE
+    ), patch("sunweg.api.APIHelper.listPlants", return_value=[plant]), patch(
+        "sunweg.api.APIHelper.plant", return_value=plant
+    ), patch(
+        "sunweg.api.APIHelper.listPlants", return_value=[plant]
+    ), patch(
+        "sunweg.api.APIHelper.inverter", return_value=inverter
+    ):
+        assert await async_setup_component(hass, DOMAIN, mock_entry)
+        await hass.async_block_till_done()
     assert await hass.config_entries.async_unload(mock_entry.entry_id)
+
+
+async def test_setup_wrongpass(hass: HomeAssistant) -> None:
+    """Test setup with wrong pass."""
+    mock_entry = SUNWEG_MOCK_ENTRY
+    mock_entry.add_to_hass(hass)
+    with patch("sunweg.api.APIHelper.authenticate", return_value=False):
+        assert await async_setup_component(hass, DOMAIN, mock_entry)
+        await hass.async_block_till_done()
