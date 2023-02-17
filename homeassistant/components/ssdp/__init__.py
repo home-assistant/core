5fdaef9cc01fff65172aba8a179ead0917d9d355
J. Nick Koston <nick@koston.org>
2023-02-17 14:51:19 -0600

    Dismiss discoveries when the underlying device disappears (#88340)
    * Implement discovery removals
    
    Bluetooth, HomeKit, SSDP, and Zeroconf now implement
    dismissing discoveries when the underlying discovered
    device disappears
    
    * cover
    
    * add zeroconf test
    
    * cover
    
    * cover bluetooth
    
    * fix rediscover

--- homeassistant/components/ssdp/__init__.py
+++ homeassistant/components/ssdp/__init__.py
@@ -518,7 +518,11 @@ class Scanner:
                 CaseInsensitiveDict(combined_headers.as_dict(), **info_desc)
             )
 
-        if not callbacks and not matching_domains:
+        if (
+            not callbacks
+            and not matching_domains
+            and source != SsdpSource.ADVERTISEMENT_BYEBYE
+        ):
             return
 
         discovery_info = discovery_info_from_headers_and_description(
@@ -534,6 +538,7 @@ class Scanner:
 
         # Config flows should only be created for alive/update messages from alive devices
         if source == SsdpSource.ADVERTISEMENT_BYEBYE:
+            self._async_dismiss_discoveries(discovery_info)
             return
 
         _LOGGER.debug("Discovery info: %s", discovery_info)
@@ -547,6 +552,19 @@ class Scanner:
                 {"source": config_entries.SOURCE_SSDP},
                 discovery_info,
             )
+
+    def _async_dismiss_discoveries(
+        self, byebye_discovery_info: SsdpServiceInfo
+    ) -> None:
+        """Dismiss all discoveries for the given address."""
+        for flow in self.hass.config_entries.flow.async_progress_by_init_data_type(
+            SsdpServiceInfo,
+            lambda service_info: bool(
+                service_info.ssdp_st == byebye_discovery_info.ssdp_st
+                and service_info.ssdp_location == byebye_discovery_info.ssdp_location
+            ),
+        ):
+            self.hass.config_entries.flow.async_abort(flow["flow_id"])
 
     async def _async_get_description_dict(
         self, location: str | None
