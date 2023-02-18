90b8a596e20a95c49288ca6754ca3d02e5cbb799
Florian Völker <github@floyer.de>
2023-02-18 00:24:51 +0000

    Added new functions for recording ongoing and new

--- homeassistant/components/philips_js/binary_sensor.py
+++ homeassistant/components/philips_js/binary_sensor.py
@@ -24,6 +24,7 @@ async def async_setup_entry(
 
     if coordinator.api.json_feature_supported("recordings", "List"):
         async_add_entities([PhilipsTVRecordingOngoing(coordinator)])
+        async_add_entities([PhilipsTVRecordingNew(coordinator)])
 
 
 class PhilipsTVRecordingOngoing(
@@ -53,11 +54,50 @@ class PhilipsTVRecordingOngoing(
         """Return true if entity is available."""
         if not super().available:
             return False
-        if not self.coordinator.api.on:
-            return False
-        return self.coordinator.api.powerstate == "On"
+        return True
 
     @property
     def is_on(self) -> bool:
-        """Return True if entity is on."""
+        """Return True if at least one recording is ongoing."""
+        for rec in self.coordinator.api.recordings_list["recordings"]:
+            if rec["RecordingType"] == "RECORDING_ONGOING":
+                return True
+        return False
+
+
+class PhilipsTVRecordingNew(
+    CoordinatorEntity[PhilipsTVDataUpdateCoordinator], BinarySensorEntity
+):
+    """A Philips TV binary sensor, which shows if a new recording is available."""
+
+    def __init__(
+        self,
+        coordinator: PhilipsTVDataUpdateCoordinator,
+    ) -> None:
+        """Initialize entity."""
+
+        super().__init__(coordinator)
+
+        self._attr_name = f"{coordinator.system['name']} New recording available"
+        self._attr_icon = "mdi:new-box"
+        self._attr_unique_id = f"{coordinator.unique_id}_recording_new"
+        self._attr_device_info = DeviceInfo(
+            identifiers={
+                (DOMAIN, coordinator.unique_id),
+            }
+        )
+
+    @property
+    def available(self) -> bool:
+        """Return true if entity is available."""
+        if not super().available:
+            return False
         return True
+
+    @property
+    def is_on(self) -> bool:
+        """Return True if at least one recording is new."""
+        for rec in self.coordinator.api.recordings_list["recordings"]:
+            if rec["RecordingType"] == "RECORDING_NEW":
+                return True
+        return False
