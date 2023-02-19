8b1a6d0882fde37663d777f0e3cc5e1198fdbd4d
J. Nick Koston <nick@koston.org>
2023-02-19 09:59:44 -0600

    fix bug

--- homeassistant/components/recorder/core.py
+++ homeassistant/components/recorder/core.py
@@ -1030,10 +1030,11 @@ class Recorder(threading.Thread):
     def _close_event_session(self) -> None:
         """Close the event session."""
         self._old_states = {}
-        self._state_attributes_ids = {}
-        self._event_data_ids = {}
+        self._state_attributes_ids = LRU(STATE_ATTRIBUTES_ID_CACHE_SIZE)
+        self._event_data_ids = LRU(EVENT_DATA_ID_CACHE_SIZE)
         self._pending_state_attributes = {}
         self._pending_event_data = {}
+        self._adjust_lru_size()
 
         if not self.event_session:
             return
