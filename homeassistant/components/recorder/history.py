74381279babb532f9eddd9dbde43e7efd6660d88
J. Nick Koston <nick@koston.org>
2023-02-21 00:16:16 -0600

    Only build the field_map once in recorder history
    Since the field_map will always be the same for each row, we
    should only build it once per call to _sorted_states_to_dict

--- homeassistant/components/recorder/history.py
+++ homeassistant/components/recorder/history.py
@@ -856,7 +856,7 @@ def _sorted_states_to_dict(
     """
     schema_version = _schema_version(hass)
     _process_timestamp: Callable[[datetime], float | str]
-    field_map: dict[str, int]
+    field_map: dict[str, int] = {}
     state_class: Callable[
         [Row, dict[str, dict[str, Any]], datetime | None], State | dict[str, Any]
     ]
@@ -919,7 +919,7 @@ def _sorted_states_to_dict(
         if row := initial_states.pop(ent_id, None):
             prev_state = row.state
             ent_results.append(state_class(row, attr_cache, start_time))
-            field_map = {key: idx for idx, key in enumerate(row._fields)}
+            field_map |= {key: idx for idx, key in enumerate(row._fields)}
 
         if not minimal_response or split_entity_id(ent_id)[0] in NEED_ATTRIBUTE_DOMAINS:
             ent_results.extend(
@@ -936,7 +936,7 @@ def _sorted_states_to_dict(
                 continue
             prev_state = first_state.state
             ent_results.append(state_class(first_state, attr_cache, None))
-            field_map = {key: idx for idx, key in enumerate(first_state._fields)}
+            field_map |= {key: idx for idx, key in enumerate(first_state._fields)}
 
         state_idx = field_map["state"]
 
