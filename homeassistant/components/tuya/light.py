9177c61147cfebf4bccd67b1b2ee9bac8fb61b3e
BNolet <BNolet@users.noreply.github.com>
2023-02-20 21:52:25 -0500

    Fix brightness-only turning colour mode to white
    This will take into account the case where brightness is the only parameter for both the case of colour mode and white mode.
    
    Tests passed after this change:
    
    * Brightness only (colour mode) ✅
    * Brightness only (white mode) ✅
    * Colour only (colour mode) ✅
    * Colour only (white mode) ✅
    * Colour temp only (colour mode) ✅
    * Colour temp only (white mode) ✅
    * Colour + brightness (colour mode) ✅
    * Colour + brightness (white mode) ✅
    * Colour temp + brightness (colour mode) ✅
    * Colour temp + brightness (white mode) ✅

--- homeassistant/components/tuya/light.py
+++ homeassistant/components/tuya/light.py
@@ -497,9 +497,12 @@ class TuyaLightEntity(TuyaEntity, LightEntity):
                     ),
                 },
             ]
-        elif self._color_data_type and (
+        
+        if self._color_data_type and (
             ATTR_HS_COLOR in kwargs
-            or (ATTR_BRIGHTNESS in kwargs and self.color_mode == ColorMode.HS)
+            or (ATTR_BRIGHTNESS in kwargs 
+            and self.color_mode == ColorMode.HS 
+            and ATTR_COLOR_TEMP not in kwargs)
         ):
             if self._color_mode_dpcode:
                 commands += [
@@ -540,9 +543,8 @@ class TuyaLightEntity(TuyaEntity, LightEntity):
                 },
             ]
 
-        if (
+        elif (
             ATTR_BRIGHTNESS in kwargs
-            and ATTR_HS_COLOR not in kwargs
             and self._brightness
         ):
             brightness = kwargs[ATTR_BRIGHTNESS]
