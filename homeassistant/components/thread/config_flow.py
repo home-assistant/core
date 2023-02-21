8803385c0eea60fed020d002c71f1183a5833f6b
John Carr <john.carr@unrouted.co.uk>
2023-02-21 15:23:28 +0000

    Activate integration if thread routers are present

--- homeassistant/components/thread/config_flow.py
+++ homeassistant/components/thread/config_flow.py
@@ -1,6 +1,7 @@
 """Config flow for the Thread integration."""
 from __future__ import annotations
 
+from homeassistant.components import zeroconf
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.data_entry_flow import FlowResult
 
@@ -12,8 +13,18 @@ class ThreadConfigFlow(ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
+    async def async_step_zeroconf(
+        self, discovery_info: zeroconf.ZeroconfServiceInfo
+    ) -> FlowResult:
+        """Set up because the user has border routers."""
+        if self._async_current_entries():
+            return self.async_abort(reason="already_configured")
+        return self.async_create_entry(title="Thread", data={})
+
     async def async_step_import(
         self, import_data: dict[str, str] | None = None
     ) -> FlowResult:
         """Set up by import from async_setup."""
+        if self._async_current_entries():
+            return self.async_abort(reason="already_configured")
         return self.async_create_entry(title="Thread", data={})
--- tests/components/thread/test_config_flow.py
+++ tests/components/thread/test_config_flow.py
@@ -27,3 +27,76 @@ async def test_import(hass: HomeAssistant) -> None:
     assert config_entry.options == {}
     assert config_entry.title == "Thread"
     assert config_entry.unique_id is None
+
+
+async def test_import_then_zeroconf(hass: HomeAssistant) -> None:
+    """Test the import flow."""
+    with patch(
+        "homeassistant.components.thread.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            thread.DOMAIN, context={"source": "import"}
+        )
+
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+
+    with patch(
+        "homeassistant.components.thread.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            thread.DOMAIN, context={"source": "zeroconf"}
+        )
+
+    assert result["type"] == FlowResultType.ABORT
+    assert result["reason"] == "already_configured"
+    assert len(mock_setup_entry.mock_calls) == 0
+
+
+async def test_zeroconf(hass: HomeAssistant) -> None:
+    """Test the zeroconf flow."""
+    with patch(
+        "homeassistant.components.thread.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            thread.DOMAIN, context={"source": "zeroconf"}
+        )
+
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    assert result["title"] == "Thread"
+    assert result["data"] == {}
+    assert result["options"] == {}
+    assert len(mock_setup_entry.mock_calls) == 1
+
+    config_entry = hass.config_entries.async_entries(thread.DOMAIN)[0]
+    assert config_entry.data == {}
+    assert config_entry.options == {}
+    assert config_entry.title == "Thread"
+    assert config_entry.unique_id is None
+
+
+async def test_zeroconf_then_import(hass: HomeAssistant) -> None:
+    """Test the import flow."""
+    with patch(
+        "homeassistant.components.thread.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            thread.DOMAIN, context={"source": "zeroconf"}
+        )
+
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+
+    with patch(
+        "homeassistant.components.thread.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            thread.DOMAIN, context={"source": "import"}
+        )
+
+    assert result["type"] == FlowResultType.ABORT
+    assert result["reason"] == "already_configured"
+    assert len(mock_setup_entry.mock_calls) == 0
