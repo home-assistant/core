ec19081842cad9699614f2820180bd64607b4b9d
Penney, Emory J <treadstoneit@gmail.com>
2023-02-22 23:35:40 +0000

    Add config_flow tests

--- homeassistant/components/obihai/sensor.py
+++ homeassistant/components/obihai/sensor.py
@@ -5,11 +5,7 @@ from datetime import timedelta
 
 import voluptuous as vol
 
-from homeassistant.components.sensor import (
-    PLATFORM_SCHEMA,
-    SensorDeviceClass,
-    SensorEntity,
-)
+from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
@@ -18,7 +14,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-from .const import DEFAULT_PASSWORD, DEFAULT_USERNAME, DOMAIN, OBIHAI
+from .const import DEFAULT_PASSWORD, DEFAULT_USERNAME, DOMAIN
 from .obihai_api import ObihaiConnection
 
 SCAN_INTERVAL = timedelta(seconds=5)
@@ -67,105 +63,12 @@ async def async_setup_entry(
 ) -> None:
     """Set up the Obihai sensor entries."""
 
-    username = entry.options[CONF_USERNAME]
-    password = entry.options[CONF_PASSWORD]
-    host = entry.options[CONF_HOST]
+    username = entry.data[CONF_USERNAME]
+    password = entry.data[CONF_PASSWORD]
+    host = entry.data[CONF_HOST]
     requester = ObihaiConnection(host, username, password)
 
     await hass.async_add_executor_job(requester.update)
     sensors = requester.sensors
 
     async_add_entities(sensors, update_before_add=True)
-
-
-class ObihaiServiceSensors(SensorEntity):
-    """Get the status of each Obihai Lines."""
-
-    def __init__(self, pyobihai, serial, service_name):
-        """Initialize monitor sensor."""
-        self._service_name = service_name
-        self._state = None
-        self._name = f"{OBIHAI} {self._service_name}"
-        self._pyobihai = pyobihai
-        self._unique_id = f"{serial}-{self._service_name}"
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def native_value(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def available(self):
-        """Return if sensor is available."""
-        if self._state is not None:
-            return True
-        return False
-
-    @property
-    def unique_id(self):
-        """Return the unique ID."""
-        return self._unique_id
-
-    @property
-    def device_class(self):
-        """Return the device class for uptime sensor."""
-        if self._service_name == "Last Reboot":
-            return SensorDeviceClass.TIMESTAMP
-        return None
-
-    @property
-    def icon(self):
-        """Return an icon."""
-        if self._service_name == "Call Direction":
-            if self._state == "No Active Calls":
-                return "mdi:phone-off"
-            if self._state == "Inbound Call":
-                return "mdi:phone-incoming"
-            return "mdi:phone-outgoing"
-        if "Caller Info" in self._service_name:
-            return "mdi:phone-log"
-        if "Port" in self._service_name:
-            if self._state == "Ringing":
-                return "mdi:phone-ring"
-            if self._state == "Off Hook":
-                return "mdi:phone-in-talk"
-            return "mdi:phone-hangup"
-        if "Service Status" in self._service_name:
-            if "OBiTALK Service Status" in self._service_name:
-                return "mdi:phone-check"
-            if self._state == "0":
-                return "mdi:phone-hangup"
-            return "mdi:phone-in-talk"
-        if "Reboot Required" in self._service_name:
-            if self._state == "false":
-                return "mdi:restart-off"
-            return "mdi:restart-alert"
-        return "mdi:phone"
-
-    def update(self) -> bool:
-        """Update the sensor."""
-        if self._pyobihai.check_account():
-            services = self._pyobihai.get_state()
-
-            if self._service_name in services:
-                self._state = services.get(self._service_name)
-
-            services = self._pyobihai.get_line_state()
-
-            if services is not None and self._service_name in services:
-                self._state = services.get(self._service_name)
-
-            call_direction = self._pyobihai.get_call_direction()
-
-            if self._service_name in call_direction:
-                self._state = call_direction.get(self._service_name)
-
-            return True
-
-        self._state = None
-        return False
---
+++ tests/components/obihai/test_config_flow.py
@@ -0,0 +1,68 @@
+"""Test the Obihai config flow."""
+from unittest.mock import patch
+
+from homeassistant import config_entries, data_entry_flow
+from homeassistant.components.obihai.const import DOMAIN
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResultType
+
+from . import USER_INPUT
+
+
+async def test_user_form(hass: HomeAssistant) -> None:
+    """Test we get the user initiated form."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] is None
+
+    with patch("pyobihai.PyObihai.check_account"), patch(
+        "homeassistant.components.obihai.async_setup_entry"
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            USER_INPUT,
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    assert result["title"] == "10.10.10.30"
+    assert result["data"] == {**USER_INPUT}
+
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_auth_failure(hass: HomeAssistant) -> None:
+    """Test we get the authentication error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.obihai.config_flow.validate_auth", return_value=False
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            USER_INPUT,
+        )
+        await hass.async_block_till_done()
+
+    assert result["errors"]["base"] == "cannot_connect"
+
+
+async def test_yaml_import(hass: HomeAssistant) -> None:
+    """Test we get the YAML imported."""
+    with patch(
+        "homeassistant.components.obihai.config_flow.validate_auth", return_value=True
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_IMPORT},
+            data=USER_INPUT,
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
+    assert "errors" not in result
