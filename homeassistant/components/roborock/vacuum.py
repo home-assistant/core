05130fe5df0f3b1178882fb997ec4a20975bfdce
Luke <conway220@gmail.com>
2023-02-22 21:10:44 +0000

    removed maps in code

--- homeassistant/components/roborock/vacuum.py
+++ homeassistant/components/roborock/vacuum.py
@@ -3,6 +3,7 @@ from abc import ABC
 import logging
 from typing import Any
 
+from roborock.code_mappings import FAN_SPEED_CODES, MOP_INTENSITY_CODES, MOP_MODE_CODES
 from roborock.typing import RoborockCommand, RoborockDeviceInfo, Status
 import voluptuous as vol
 
@@ -32,62 +33,6 @@ from .device import RoborockCoordinatedEntity
 
 _LOGGER = logging.getLogger(__name__)
 
-STATE_CODES_TO_STATUS = {
-    1: "starting",
-    2: "charger_disconnected",
-    3: "idle",
-    4: "remote_control_active",
-    5: "cleaning",
-    6: "returning_home",
-    7: "manual_mode",
-    8: "charging",
-    9: "charging_problem",
-    10: "paused",
-    11: "spot_cleaning",
-    12: "error",
-    13: "shutting_down",
-    14: "updating",
-    15: "docking",
-    16: "going_to_target",
-    17: "zoned_cleaning",
-    18: "segment_cleaning",
-    22: "emptying_the_bin",  # on s7+, see #1189
-    23: "washing_the_mop",  # on a46, #1435
-    26: "going_to_wash_the_mop",  # on a46, #1435
-    100: "charging_complete",
-    101: "device_offline",
-}
-
-ERROR_CODE_TO_TEXT = {
-    0: "None",
-    1: "LiDAR turret or laser blocked. Check for obstruction and retry.",
-    2: "Bumper stuck. Clean it and lightly tap to release it.",
-    3: "Wheels suspended. Move robot and restart.",
-    4: "Cliff sensor error. Clean cliff sensors, move robot away from drops and restart.",
-    5: "Main brush jammed. Clean main brush and bearings.",
-    6: "Side brush jammed. Remove and clean side brush.",
-    7: "Wheels jammed. Move the robot and restart.",
-    8: "Robot trapped. Clear obstacles surrounding robot.",
-    9: "No dustbin. Install dustbin and filter.",
-    12: "Low battery. Recharge and retry.",
-    13: "Charging error. Clean charging contacts and retry.",
-    14: "Battery error.",
-    15: "Wall sensor dirty. Clean wall sensor.",
-    16: "Robot tilted. Move to level ground and restart.",
-    17: "Side brush error. Reset robot.",
-    18: "Fan error. Reset robot.",
-    21: "Vertical bumper pressed. Move robot and retry.",
-    22: "Dock locator error. Clean and retry.",
-    23: "Could not return to dock. Clean dock location beacon and retry.",
-    24: "No-go zone or invisible wall detected. Move the robot.",
-    27: "VibraRise system jammed. Check for obstructions.",
-    28: "Robot on carpet. Move robot to floor and retry.",
-    29: "Filter blocked or wet. Clean, dry, and retry.",
-    30: "No-go zone or Invisible Wall detected. Move robot from this area.",
-    31: "Cannot cross carpet. Move robot across carpet and restart.",
-    32: "Internal error. Reset the robot.",
-}
-
 STATE_CODE_TO_STATE = {
     1: STATE_IDLE,  # "Starting"
     2: STATE_IDLE,  # "Charger disconnected"
@@ -114,30 +59,6 @@ STATE_CODE_TO_STATE = {
     101: STATE_ERROR,  # "Device offline"
 }
 
-FAN_SPEED_CODES = {
-    105: "off",
-    101: "silent",
-    102: "balanced",
-    103: "turbo",
-    104: "max",
-    108: "max_plus",
-    106: "custom",
-}
-
-MOP_MODE_CODES = {
-    300: "standard",
-    301: "deep",
-    303: "deep_plus",
-    302: "custom",
-}
-
-MOP_INTENSITY_CODES = {
-    200: "off",
-    201: "mild",
-    202: "moderate",
-    203: "intense",
-    204: "custom",
-}
 
 ATTR_STATUS = "vacuum_status"
 ATTR_MOP_MODE = "mop_mode"
@@ -276,16 +197,14 @@ class RoborockVacuum(RoborockCoordinatedEntity, StateVacuumEntity, ABC):
         """Return the status of the vacuum cleaner."""
         if not self._device_status:
             return None
-        state = self._device_status.state
-        return STATE_CODE_TO_STATE.get(state)
+        return STATE_CODE_TO_STATE.get(self._device_status.state)
 
     @property
     def status(self) -> Status | None:
         """Return the status of the vacuum cleaner."""
         if not self._device_status:
             return None
-        status = self._device_status.state
-        return STATE_CODES_TO_STATUS.get(status)
+        return self._device_status.status
 
     @property
     def state_attributes(self) -> dict[str, Any]:
@@ -322,8 +241,7 @@ class RoborockVacuum(RoborockCoordinatedEntity, StateVacuumEntity, ABC):
         """Return the fan speed of the vacuum cleaner."""
         if not self._device_status:
             return None
-        fan_speed = self._device_status.fan_power
-        return FAN_SPEED_CODES.get(fan_speed)
+        return self._device_status.fan_power
 
     @property
     def fan_speed_list(self) -> list[str]:
@@ -335,8 +253,7 @@ class RoborockVacuum(RoborockCoordinatedEntity, StateVacuumEntity, ABC):
         """Return the mop mode of the vacuum cleaner."""
         if not self._device_status:
             return None
-        mop_mode = self._device_status.mop_mode
-        return MOP_MODE_CODES.get(mop_mode)
+        return self._device_status.mop_mode
 
     @property
     def mop_mode_list(self) -> list[str]:
@@ -348,8 +265,7 @@ class RoborockVacuum(RoborockCoordinatedEntity, StateVacuumEntity, ABC):
         """Return the mop intensity of the vacuum cleaner."""
         if not self._device_status:
             return None
-        mop_intensity = self._device_status.water_box_mode
-        return MOP_INTENSITY_CODES.get(mop_intensity)
+        return self._device_status.mop_intensity
 
     @property
     def mop_intensity_list(self) -> list[str]:
@@ -361,7 +277,7 @@ class RoborockVacuum(RoborockCoordinatedEntity, StateVacuumEntity, ABC):
         """Get the error str if an error code exists."""
         if not self._device_status:
             return None
-        return ERROR_CODE_TO_TEXT.get(self._device_status.error_code, "Unknown Error")
+        return self._device_status.error
 
     @property
     def capability_attributes(self) -> dict[str, list[str]]:
