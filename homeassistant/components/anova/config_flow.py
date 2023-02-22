0d7d42df7ba8d338f8a7cfcdd0810eb98133387f
Luke <conway220@gmail.com>
2023-02-22 02:48:27 +0000

    added error for incorrect device id

--- homeassistant/components/anova/config_flow.py
+++ homeassistant/components/anova/config_flow.py
@@ -1,10 +1,12 @@
 """Config flow for Anova."""
 from __future__ import annotations
 
+from anova_wifi import AnovaOffline, AnovaPrecisionCooker
 import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers import aiohttp_client
 
 from .const import DOMAIN
 
@@ -18,15 +20,25 @@ class AnovaConfligFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, str] | None = None
     ) -> FlowResult:
         """Handle a flow initiated by the user."""
+        errors: dict[str, str] = {}
         if user_input is not None:
-            await self.async_set_unique_id(user_input["device_unique_id"])
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(
-                title="Anova",
-                data={"device_unique_id": user_input["device_unique_id"]},
+            apc = AnovaPrecisionCooker(
+                aiohttp_client.async_get_clientsession(self.hass)
             )
+            try:
+                await apc.update(user_input["device_unique_id"])
+            except AnovaOffline:
+                errors["base"] = "invalid_id"
+            else:
+                await self.async_set_unique_id(user_input["device_unique_id"])
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(
+                    title="Anova",
+                    data={"device_unique_id": user_input["device_unique_id"]},
+                )
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({vol.Required("device_unique_id"): str}),
+            errors=errors,
         )
--- tests/components/anova/test_config_flow.py
+++ tests/components/anova/test_config_flow.py
@@ -1,36 +1,66 @@
 """Test Anova config flow."""
 
+from unittest.mock import patch
+
+from anova_wifi import AnovaOffline
+
 from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.anova.const import DOMAIN
 from homeassistant.core import HomeAssistant
 
-from . import CONF_INPUT, create_entry
+from . import CONF_INPUT, ONLINE_UPDATE, create_entry
 
 
 async def test_flow_user(hass: HomeAssistant) -> None:
     """Test user initialized flow."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={"source": config_entries.SOURCE_USER},
-    )
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        user_input=CONF_INPUT,
-    )
-    assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
-    assert result["data"] == CONF_INPUT
+    with patch(
+        "homeassistant.components.anova.AnovaPrecisionCooker.update"
+    ) as update_patch:
+        update_patch.return_value = ONLINE_UPDATE
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+        )
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input=CONF_INPUT,
+        )
+        assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
+        assert result["data"] == CONF_INPUT
 
 
 async def test_flow_user_already_configured(hass: HomeAssistant) -> None:
     """Test user initialized flow with duplicate device."""
-    create_entry(hass)
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={"source": config_entries.SOURCE_USER},
-    )
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        user_input=CONF_INPUT,
-    )
-    assert result["type"] == data_entry_flow.FlowResultType.ABORT
-    assert result["reason"] == "already_configured"
+    with patch(
+        "homeassistant.components.anova.AnovaPrecisionCooker.update"
+    ) as update_patch:
+        update_patch.return_value = ONLINE_UPDATE
+        create_entry(hass)
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+        )
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input=CONF_INPUT,
+        )
+        assert result["type"] == data_entry_flow.FlowResultType.ABORT
+        assert result["reason"] == "already_configured"
+
+
+async def test_flow_incorrect_id(hass: HomeAssistant) -> None:
+    """Test incorrect device id throwing error."""
+    with patch(
+        "homeassistant.components.anova.AnovaPrecisionCooker.update",
+        side_effect=AnovaOffline(),
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+        )
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input=CONF_INPUT,
+        )
+        assert result["type"] == data_entry_flow.FlowResultType.FORM
+        assert result["errors"] == {"base": "invalid_id"}
