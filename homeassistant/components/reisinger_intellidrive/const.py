7cae29f1a17993363a9d5514ccc192e65421aa33
sreisi <sell@reisinger.de>
2023-02-22 13:43:24 +0000

    Increase update interval

--- homeassistant/components/reisinger_intellidrive/const.py
+++ homeassistant/components/reisinger_intellidrive/const.py
@@ -1,7 +1,7 @@
 """Constants for the Intellidrive integration."""
 
 DOMAIN = "reisinger_intellidrive"
-UPDATE_INTERVAL = 3
+UPDATE_INTERVAL = 5
 STATUSDICT_OPENSTATE = "open"
 STATUSDICT_SERIALNO = "serial"
 STATUSDICT_VERSION = "versionString"
