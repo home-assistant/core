3214e66e40d103bb02b7b3b3829cdcfdff10892d
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-02-23 16:46:31 +0100

    optimize a tiny bit

--- homeassistant/components/matter/discovery.py
+++ homeassistant/components/matter/discovery.py
@@ -15,17 +15,25 @@ from homeassistant.core import callback
 
 from .binary_sensor import DISCOVERY_SCHEMAS as BINARY_SENSOR_SCHEMAS
 from .light import DISCOVERY_SCHEMAS as LIGHT_SCHEMAS
-from .models import MatterEntityInfo
+from .models import MatterDiscoverySchema, MatterEntityInfo
 from .sensor import DISCOVERY_SCHEMAS as SENSOR_SCHEMAS
 from .switch import DISCOVERY_SCHEMAS as SWITCH_SCHEMAS
 
-DISCOVERY_SCHEMAS = {
+DISCOVERY_SCHEMAS: dict[Platform, list[MatterDiscoverySchema]] = {
     Platform.BINARY_SENSOR: BINARY_SENSOR_SCHEMAS,
     Platform.LIGHT: LIGHT_SCHEMAS,
     Platform.SENSOR: SENSOR_SCHEMAS,
     Platform.SWITCH: SWITCH_SCHEMAS,
 }
 SUPPORTED_PLATFORMS = tuple(DISCOVERY_SCHEMAS.keys())
+
+
+@callback
+def iter_schemas() -> Generator[MatterDiscoverySchema, None, None]:
+    """Iterate over all available discovery schemas."""
+    for platform_schemas in DISCOVERY_SCHEMAS.values():
+        for schema in platform_schemas:
+            yield schema
 
 
 @callback
@@ -54,98 +62,96 @@ def async_discover_single(
 ) -> Generator[MatterEntityInfo, None, None]:
     """Run discovery on a single Matter Attribute and return matching schema info."""
     device_info = endpoint.device_info
-    # pylint: disable=too-many-nested-blocks
-    for discovery_schemas in DISCOVERY_SCHEMAS.values():
-        for schema in discovery_schemas:
-            # check vendor_id
-            if (
-                schema.vendor_id is not None
-                and device_info.vendorID not in schema.vendor_id
-            ):
-                continue
+    for schema in iter_schemas():
+        # check vendor_id
+        if (
+            schema.vendor_id is not None
+            and device_info.vendorID not in schema.vendor_id
+        ):
+            continue
 
-            # check product_name
-            if (
-                schema.product_name is not None
-                and device_info.productName not in schema.product_name
-            ):
-                continue
+        # check product_name
+        if (
+            schema.product_name is not None
+            and device_info.productName not in schema.product_name
+        ):
+            continue
 
-            # check required device_type
-            if schema.device_type is not None and not any(
-                type(x) in schema.device_type for x in endpoint.device_types
-            ):
-                continue
+        # check required device_type
+        if schema.device_type is not None and not any(
+            type(x) in schema.device_type for x in endpoint.device_types
+        ):
+            continue
 
-            # check absent device_type
-            if schema.not_device_type is not None and any(
-                type(x) in schema.not_device_type for x in endpoint.device_types
-            ):
-                continue
+        # # check absent device_type
+        if schema.not_device_type is not None and any(
+            type(x) in schema.not_device_type for x in endpoint.device_types
+        ):
+            continue
 
-            # check endpoint_id
-            if (
-                schema.endpoint_id is not None
-                and endpoint.endpoint_id not in schema.endpoint_id
-            ):
-                continue
+        # check endpoint_id
+        if (
+            schema.endpoint_id is not None
+            and endpoint.endpoint_id not in schema.endpoint_id
+        ):
+            continue
 
-            # check required attributes
-            if schema.required_attributes is not None and not all(
-                any(
-                    check_attribute(val, val_schema, endpoint.endpoint_id)
-                    for val in endpoint.node.node_data.attributes
-                )
-                for val_schema in schema.required_attributes
-            ):
-                continue
-
-            # check for values that may not be present
-            if schema.absent_attributes is not None and any(
-                any(
-                    check_attribute(val, val_schema, endpoint.endpoint_id)
-                    for val in endpoint.node.node_data.attributes
-                )
-                for val_schema in schema.absent_attributes
-            ):
-                continue
-
-            # all checks passed, this value belongs to an entity
-
-            attributes_to_watch = list(schema.required_attributes)
-            if schema.optional_attributes:
-                # check optional attributes
-                for optional_attribute in schema.optional_attributes:
-                    if any(
-                        check_attribute(val, optional_attribute, endpoint.endpoint_id)
-                        for val in endpoint.node.node_data.attributes
-                    ):
-                        if optional_attribute in attributes_to_watch:
-                            continue
-                        attributes_to_watch.append(optional_attribute)
-
-            # prevent re-discovery of the same attributes
-            for attribute_to_watch in attributes_to_watch:
-                path = create_attribute_path(
-                    endpoint.endpoint_id,
-                    attribute_to_watch.cluster_id,
-                    attribute_to_watch.attribute_id,
-                )
-                discovered_attributes.add(path)
-
-            yield MatterEntityInfo(
-                endpoint=endpoint,
-                platform=schema.platform,
-                attributes_to_watch=attributes_to_watch,
-                entity_description=schema.entity_description,
-                entity_class=schema.entity_class,
-                measurement_to_ha=schema.measurement_to_ha,
+        # check required attributes
+        if schema.required_attributes is not None and not all(
+            any(
+                check_attribute(val, val_schema, endpoint.endpoint_id)
+                for val in endpoint.node.node_data.attributes
             )
+            for val_schema in schema.required_attributes
+        ):
+            continue
 
-            if not schema.allow_multi:
-                # return early since this value may not be discovered
-                # by other schemas/platforms
-                return
+        # check for values that may not be present
+        if schema.absent_attributes is not None and any(
+            any(
+                check_attribute(val, val_schema, endpoint.endpoint_id)
+                for val in endpoint.node.node_data.attributes
+            )
+            for val_schema in schema.absent_attributes
+        ):
+            continue
+
+        # all checks passed, this value belongs to an entity
+
+        attributes_to_watch = list(schema.required_attributes)
+        if schema.optional_attributes:
+            # check optional attributes
+            for optional_attribute in schema.optional_attributes:
+                if any(
+                    check_attribute(val, optional_attribute, endpoint.endpoint_id)
+                    for val in endpoint.node.node_data.attributes
+                ):
+                    if optional_attribute in attributes_to_watch:
+                        continue
+                    attributes_to_watch.append(optional_attribute)
+
+        # prevent re-discovery of the same attributes
+        for attribute_to_watch in attributes_to_watch:
+            path = create_attribute_path(
+                endpoint.endpoint_id,
+                attribute_to_watch.cluster_id,
+                attribute_to_watch.attribute_id,
+            )
+            discovered_attributes.add(path)
+
+        yield MatterEntityInfo(
+            endpoint=endpoint,
+            platform=schema.platform,
+            attributes_to_watch=attributes_to_watch,
+            entity_description=schema.entity_description,
+            entity_class=schema.entity_class,
+            measurement_to_ha=schema.measurement_to_ha,
+        )
+
+        if not schema.allow_multi:
+            # return early since this value may not be discovered
+            # by other schemas/platforms
+            return
 
 
 @callback
