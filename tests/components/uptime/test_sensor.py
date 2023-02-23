3dd7a251f324d356a1f7782570cfc66d04127623
Franck Nijhof <git@frenck.dev>
2023-02-23 12:17:15 +0100

    Fix tests

--- tests/components/uptime/test_sensor.py
+++ tests/components/uptime/test_sensor.py
@@ -1,9 +1,13 @@
 """The tests for the uptime sensor platform."""
 import pytest
 from syrupy.assertion import SnapshotAssertion
+from syrupy.filters import props
 
+from homeassistant.components.uptime.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr, entity_registry as er
+
+from tests.common import MockConfigEntry
 
 
 @pytest.mark.usefixtures("init_integration")
@@ -12,6 +16,7 @@ async def test_uptime_sensor(
     hass: HomeAssistant,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
+    init_integration: MockConfigEntry,
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test Uptime sensor."""
@@ -21,8 +26,10 @@ async def test_uptime_sensor(
     assert state == snapshot
 
     assert (entity_entry := entity_registry.async_get(state.entity_id))
-    assert entity_entry == snapshot
+    assert entity_entry == snapshot(exclude=props("unique_id"))
+    assert entity_entry.unique_id == init_integration.entry_id
 
     assert entity_entry.device_id
     assert (device_entry := device_registry.async_get(entity_entry.device_id))
-    assert device_entry == snapshot
+    assert device_entry == snapshot(exclude=props("identifiers"))
+    assert device_entry.identifiers == {(DOMAIN, init_integration.entry_id)}
