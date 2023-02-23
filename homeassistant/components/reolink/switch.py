803103bdc0702efc7edc0aca2d36514debaa0d58
starkillerOG <starkiller.og@gmail.com>
2023-02-23 23:05:34 +0100

    Add NVR switches

--- homeassistant/components/reolink/switch.py
+++ homeassistant/components/reolink/switch.py
@@ -14,7 +14,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import ReolinkData
 from .const import DOMAIN
-from .entity import ReolinkCoordinatorEntity
+from .entity import ReolinkBaseCoordinatorEntity, ReolinkCoordinatorEntity
 
 
 @dataclass
@@ -32,6 +32,23 @@ class ReolinkSwitchEntityDescription(
     """A class that describes switch entities."""
 
     supported: Callable[[Host, int | None], bool] = lambda api, ch: True
+
+
+@dataclass
+class ReolinkNVRSwitchEntityDescriptionMixin:
+    """Mixin values for Reolink NVR switch entities."""
+
+    value: Callable[[Host], bool]
+    method: Callable[[Host, bool], Any]
+
+
+@dataclass
+class ReolinkNVRSwitchEntityDescription(
+    SwitchEntityDescription, ReolinkNVRSwitchEntityDescriptionMixin
+):
+    """A class that describes NVR switch entities."""
+
+    supported: Callable[[Host], bool] = lambda api: True
 
 
 SWITCH_ENTITIES = (
@@ -77,6 +94,49 @@ SWITCH_ENTITIES = (
     ),
 )
 
+NVR_SWITCH_ENTITIES = (
+    ReolinkNVRSwitchEntityDescription(
+        key="email",
+        name="Email on event",
+        icon="mdi:email",
+        supported=lambda api: api.supported(None, "email"),
+        value=lambda api: api.email_enabled(),
+        method=lambda api, value: api.set_email(None, value),
+    ),
+    ReolinkNVRSwitchEntityDescription(
+        key="ftp_upload",
+        name="FTP upload",
+        icon="mdi:swap-horizontal",
+        supported=lambda api: api.supported(None, "ftp"),
+        value=lambda api: api.ftp_enabled(),
+        method=lambda api, value: api.set_ftp(None, value),
+    ),
+    ReolinkNVRSwitchEntityDescription(
+        key="push_notifications",
+        name="Push notifications",
+        icon="mdi:message-badge",
+        supported=lambda api: api.supported(None, "push"),
+        value=lambda api: api.push_enabled(),
+        method=lambda api, value: api.set_push(None, value),
+    ),
+    ReolinkNVRSwitchEntityDescription(
+        key="record",
+        name="Record",
+        icon="mdi:record-rec",
+        supported=lambda api: api.supported(None, "recording"),
+        value=lambda api: api.recording_enabled(),
+        method=lambda api, value: api.set_recording(None, value),
+    ),
+    ReolinkNVRSwitchEntityDescription(
+        key="buzzer",
+        name="Buzzer on event",
+        icon="mdi:room-service",
+        supported=lambda api: api.supported(None, "buzzer"),
+        value=lambda api: api.buzzer_enabled(),
+        method=lambda api, value: api.set_buzzer(None, value),
+    ),
+)
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -86,12 +146,20 @@ async def async_setup_entry(
     """Set up a Reolink switch entities."""
     reolink_data: ReolinkData = hass.data[DOMAIN][config_entry.entry_id]
 
-    async_add_entities(
+    entities: list[ReolinkSwitchEntity | ReolinkNVRSwitchEntity] = [
         ReolinkSwitchEntity(reolink_data, channel, entity_description)
         for entity_description in SWITCH_ENTITIES
         for channel in reolink_data.host.api.channels
         if entity_description.supported(reolink_data.host.api, channel)
+    ]
+    entities.extend(
+        [
+            ReolinkNVRSwitchEntity(reolink_data, entity_description)
+            for entity_description in NVR_SWITCH_ENTITIES
+            if entity_description.supported(reolink_data.host.api)
+        ]
     )
+    async_add_entities(entities)
 
 
 class ReolinkSwitchEntity(ReolinkCoordinatorEntity, SwitchEntity):
@@ -126,4 +194,36 @@ class ReolinkSwitchEntity(ReolinkCoordinatorEntity, SwitchEntity):
     async def async_turn_off(self, **kwargs):
         """Turn the entity off."""
         await self.entity_description.method(self._host.api, self._channel, False)
+        self.async_write_ha_state()
+
+
+class ReolinkNVRSwitchEntity(ReolinkBaseCoordinatorEntity, SwitchEntity):
+    """Switch entity class for Reolink NVR features."""
+
+    entity_description: ReolinkNVRSwitchEntityDescription
+
+    def __init__(
+        self,
+        reolink_data: ReolinkData,
+        entity_description: ReolinkNVRSwitchEntityDescription,
+    ) -> None:
+        """Initialize Reolink switch entity."""
+        super().__init__(reolink_data)
+        self.entity_description = entity_description
+
+        self._attr_unique_id = f"{self._host.unique_id}_{entity_description.key}"
+
+    @property
+    def is_on(self):
+        """Return true if switch is on."""
+        return self.entity_description.value(self._host.api)
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the entity on."""
+        await self.entity_description.method(self._host.api, True)
+        self.async_write_ha_state()
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the entity off."""
+        await self.entity_description.method(self._host.api, False)
         self.async_write_ha_state()
