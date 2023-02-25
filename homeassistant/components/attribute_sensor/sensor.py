4535bdafa7a919c6f3cdb2d6c520e62d9ccc576b
Albin Medoc <stamline203@gmail.com>
2023-02-25 07:14:44 +0000

    Fix initial loading

--- homeassistant/components/attribute_sensor/sensor.py
+++ homeassistant/components/attribute_sensor/sensor.py
@@ -24,8 +24,12 @@ from homeassistant.core import Event, HomeAssistant, State, callback
 from homeassistant.helpers import config_validation as cv, entity_registry as er
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.event import async_track_state_change_event
-from homeassistant.helpers.restore_state import RestoreEntity
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType, StateType
+from homeassistant.helpers.typing import (
+    ConfigType,
+    DiscoveryInfoType,
+    EventType,
+    StateType,
+)
 
 from .const import (
     CONF_ATTRIBUTE_NAME,
@@ -111,7 +115,7 @@ async def async_setup_platform(
     )
 
 
-class AttributeSensor(RestoreEntity, SensorEntity):
+class AttributeSensor(SensorEntity):
     """Representation of a attribute sensor."""
 
     _attr_icon = ICON
@@ -137,41 +141,62 @@ class AttributeSensor(RestoreEntity, SensorEntity):
         self._attr_device_class = device_class
         self._attr_native_unit_of_measurement = unit_of_measurement
 
+        self._attribute_value = None
+
         if name:
             self._attr_name = name
         else:
             self._attr_name = f"{attribute_name} sensor".capitalize()
 
     async def async_added_to_hass(self) -> None:
-        """Handle entity which will be added."""
-        await super().async_added_to_hass()
-        if state := await self.async_get_last_state():
-            self._state = state.state
-
-        @callback
-        def calc_attribute_sensor(event: Event) -> None:
-            """Handle the source entity state changes."""
-            new_state: State | None = event.data.get("new_state")
-
-            if new_state is None or new_state.state in (
-                STATE_UNKNOWN,
-                STATE_UNAVAILABLE,
-            ):
-                return
-
-            attribute_value = new_state.attributes.get(self._attribute_name)
-            if attribute_value is not None:
-                self._state = attribute_value
-                self.async_write_ha_state()
-
+        """Handle added to Hass."""
         self.async_on_remove(
             async_track_state_change_event(
-                self.hass, [self._source], calc_attribute_sensor
+                self.hass, [self._source], self._async_attribute_sensor_state_listener
             )
         )
+
+        state = self.hass.states.get(self._source)
+        state_event = Event("", {"entity_id": self._source, "new_state": state})
+        self._async_attribute_sensor_state_listener(state_event)
 
     @property
     def native_value(self) -> StateType | datetime:
         """Return the state of the sensor."""
         value: StateType | datetime = self._state
         return value
+
+    @callback
+    def _async_attribute_sensor_state_listener(
+        self, event: EventType, update_state: bool = True
+    ) -> None:
+        """Handle the source state change."""
+        new_state: State | None = event.data.get("new_state")
+        entity_id: str = event.data["entity_id"]
+
+        if (
+            new_state is None
+            or new_state.state is None
+            or new_state.state
+            in [
+                STATE_UNKNOWN,
+                STATE_UNAVAILABLE,
+            ]
+        ):
+            self._state = STATE_UNKNOWN
+            if update_state:
+                self.async_write_ha_state()
+            return
+
+        _LOGGER.debug("State changed for %s, updating attribute sensor", entity_id)
+
+        if update_state:
+            if self._attribute_name not in new_state.attributes:
+                _LOGGER.warning(
+                    'Entity "%s" does not have attribute "%s"',
+                    entity_id,
+                    self._attribute_name,
+                )
+                return
+            self._state = new_state.attributes.get(self._attribute_name)
+            self.async_write_ha_state()
