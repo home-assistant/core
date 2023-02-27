003dc4291645a8a0d78fc53866891aa776f9e26d
epenet <6771947+epenet@users.noreply.github.com>
2023-02-27 10:22:42 +0000

    Minimize more

--- tests/components/abode/test_config_flow.py
+++ tests/components/abode/test_config_flow.py
@@ -10,6 +10,7 @@ import pytest
 from requests.exceptions import ConnectTimeout
 
 from homeassistant import data_entry_flow
+from homeassistant.components.abode import config_flow
 from homeassistant.components.abode.const import CONF_POLLING, DOMAIN
 from homeassistant.config_entries import SOURCE_REAUTH, SOURCE_USER
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
@@ -22,9 +23,10 @@ pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 
 async def test_show_form(hass: HomeAssistant) -> None:
     """Test that the form is served with no input."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
+    flow = config_flow.AbodeFlowHandler()
+    flow.hass = hass
+
+    result = await flow.async_step_user(user_input=None)
 
     assert result["type"] == data_entry_flow.FlowResultType.FORM
     assert result["step_id"] == "user"
@@ -32,14 +34,15 @@ async def test_show_form(hass: HomeAssistant) -> None:
 
 async def test_one_config_allowed(hass: HomeAssistant) -> None:
     """Test that only one Abode configuration is allowed."""
+    flow = config_flow.AbodeFlowHandler()
+    flow.hass = hass
+
     MockConfigEntry(
         domain=DOMAIN,
         data={CONF_USERNAME: "user@email.com", CONF_PASSWORD: "password"},
     ).add_to_hass(hass)
 
-    step_user_result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
+    step_user_result = await flow.async_step_user()
 
     assert step_user_result["type"] == data_entry_flow.FlowResultType.ABORT
     assert step_user_result["reason"] == "single_instance_allowed"
@@ -49,9 +52,8 @@ async def test_invalid_credentials(hass: HomeAssistant) -> None:
     """Test that invalid credentials throws an error."""
     conf = {CONF_USERNAME: "user@email.com", CONF_PASSWORD: "password"}
 
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
+    flow = config_flow.AbodeFlowHandler()
+    flow.hass = hass
 
     with patch(
         "homeassistant.components.abode.config_flow.Abode",
@@ -59,11 +61,7 @@ async def test_invalid_credentials(hass: HomeAssistant) -> None:
             (HTTPStatus.BAD_REQUEST, "auth error")
         ),
     ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input=conf
-        )
-        assert result["type"] == data_entry_flow.FlowResultType.FORM
-        assert result["step_id"] == "user"
+        result = await flow.async_step_user(user_input=conf)
         assert result["errors"] == {"base": "invalid_auth"}
 
 
@@ -71,9 +69,8 @@ async def test_connection_auth_error(hass: HomeAssistant) -> None:
     """Test other than invalid credentials throws an error."""
     conf = {CONF_USERNAME: "user@email.com", CONF_PASSWORD: "password"}
 
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
+    flow = config_flow.AbodeFlowHandler()
+    flow.hass = hass
 
     with patch(
         "homeassistant.components.abode.config_flow.Abode",
@@ -81,11 +78,7 @@ async def test_connection_auth_error(hass: HomeAssistant) -> None:
             (HTTPStatus.INTERNAL_SERVER_ERROR, "connection error")
         ),
     ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input=conf
-        )
-        assert result["type"] == data_entry_flow.FlowResultType.FORM
-        assert result["step_id"] == "user"
+        result = await flow.async_step_user(user_input=conf)
         assert result["errors"] == {"base": "cannot_connect"}
 
 
@@ -93,19 +86,14 @@ async def test_connection_error(hass: HomeAssistant) -> None:
     """Test login throws an error if connection times out."""
     conf = {CONF_USERNAME: "user@email.com", CONF_PASSWORD: "password"}
 
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
+    flow = config_flow.AbodeFlowHandler()
+    flow.hass = hass
 
     with patch(
         "homeassistant.components.abode.config_flow.Abode",
         side_effect=ConnectTimeout,
     ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input=conf
-        )
-        assert result["type"] == data_entry_flow.FlowResultType.FORM
-        assert result["step_id"] == "user"
+        result = await flow.async_step_user(user_input=conf)
         assert result["errors"] == {"base": "cannot_connect"}
 
 
@@ -113,13 +101,9 @@ async def test_step_user(hass: HomeAssistant) -> None:
     """Test that the user step works."""
     conf = {CONF_USERNAME: "user@email.com", CONF_PASSWORD: "password"}
 
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
-
     with patch("homeassistant.components.abode.config_flow.Abode"):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input=conf
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_USER}, data=conf
         )
 
         assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
@@ -135,16 +119,12 @@ async def test_step_mfa(hass: HomeAssistant) -> None:
     """Test that the MFA step works."""
     conf = {CONF_USERNAME: "user@email.com", CONF_PASSWORD: "password"}
 
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
-
     with patch(
         "homeassistant.components.abode.config_flow.Abode",
         side_effect=AbodeAuthenticationException(MFA_CODE_REQUIRED),
     ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input=conf
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_USER}, data=conf
         )
 
         assert result["type"] == data_entry_flow.FlowResultType.FORM
@@ -159,8 +139,7 @@ async def test_step_mfa(hass: HomeAssistant) -> None:
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"], user_input={"mfa_code": "123456"}
         )
-        assert result["type"] == data_entry_flow.FlowResultType.FORM
-        assert result["step_id"] == "mfa"
+
         assert result["errors"] == {"base": "invalid_mfa_code"}
 
     with patch("homeassistant.components.abode.config_flow.Abode"):
@@ -197,9 +176,7 @@ async def test_step_reauth(hass: HomeAssistant) -> None:
         assert result["type"] == data_entry_flow.FlowResultType.FORM
         assert result["step_id"] == "reauth_confirm"
 
-        with patch(
-            "homeassistant.config_entries.ConfigEntries.async_reload"
-        ) as mock_reload:
+        with patch("homeassistant.config_entries.ConfigEntries.async_reload"):
             result = await hass.config_entries.flow.async_configure(
                 result["flow_id"],
                 user_input=conf,
@@ -209,4 +186,3 @@ async def test_step_reauth(hass: HomeAssistant) -> None:
             assert result["reason"] == "reauth_successful"
 
         assert len(hass.config_entries.async_entries()) == 1
-        mock_reload.assert_called_once()
