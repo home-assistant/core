db1dd16ab04a4718af01040d947239f2707f9670
Erik Montnemery <erik@montnemery.com>
2023-02-27 15:30:04 +0100

    Add thread user flow (#88842)

--- tests/components/thread/test_config_flow.py
+++ tests/components/thread/test_config_flow.py
@@ -78,6 +78,29 @@ async def test_import_then_zeroconf(hass: HomeAssistant) -> None:
     assert len(mock_setup_entry.mock_calls) == 0
 
 
+async def test_user(hass: HomeAssistant) -> None:
+    """Test the user flow."""
+    with patch(
+        "homeassistant.components.thread.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            thread.DOMAIN, context={"source": "user"}
+        )
+
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    assert result["title"] == "Thread"
+    assert result["data"] == {}
+    assert result["options"] == {}
+    assert len(mock_setup_entry.mock_calls) == 1
+
+    config_entry = hass.config_entries.async_entries(thread.DOMAIN)[0]
+    assert config_entry.data == {}
+    assert config_entry.options == {}
+    assert config_entry.title == "Thread"
+    assert config_entry.unique_id is None
+
+
 async def test_zeroconf(hass: HomeAssistant) -> None:
     """Test the zeroconf flow."""
     with patch(
