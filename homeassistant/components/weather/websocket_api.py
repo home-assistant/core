59b9bc8853e82e678946b80f1d88ce43b54ff7ee
Erik Montnemery <erik@montnemery.com>
2023-02-28 11:35:47 +0100

    Add WS command weather/convertible_units (#85681)

---
+++ homeassistant/components/weather/websocket_api.py
@@ -0,0 +1,30 @@
+"""The weather websocket API."""
+from __future__ import annotations
+
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant.components import websocket_api
+from homeassistant.core import HomeAssistant, callback
+
+from .const import VALID_UNITS
+
+
+@callback
+def async_setup(hass: HomeAssistant) -> None:
+    """Set up the weather websocket API."""
+    websocket_api.async_register_command(hass, ws_convertible_units)
+
+
+@callback
+@websocket_api.websocket_command(
+    {
+        vol.Required("type"): "weather/convertible_units",
+    }
+)
+def ws_convertible_units(
+    hass: HomeAssistant, connection: websocket_api.ActiveConnection, msg: dict[str, Any]
+) -> None:
+    """Return supported units for a device class."""
+    connection.send_result(msg["id"], {"units": VALID_UNITS})
