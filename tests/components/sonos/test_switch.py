b6e20c9773254954abfdef8085ef4ad012016b5c
epenet <6771947+epenet@users.noreply.github.com>
2023-03-01 13:13:02 +0000

    Adjust entity registry access in tests (3)

--- tests/components/sonos/test_switch.py
+++ tests/components/sonos/test_switch.py
@@ -15,7 +15,7 @@ from homeassistant.components.sonos.switch import (
 from homeassistant.config_entries import RELOAD_AFTER_UPDATE_DELAY
 from homeassistant.const import ATTR_TIME, STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as ent_reg
+from homeassistant.helpers import entity_registry as er
 from homeassistant.util import dt
 
 from .conftest import SonosMockEvent
@@ -23,10 +23,10 @@ from .conftest import SonosMockEvent
 from tests.common import async_fire_time_changed
 
 
-async def test_entity_registry(hass: HomeAssistant, async_autosetup_sonos) -> None:
+async def test_entity_registry(
+    hass: HomeAssistant, async_autosetup_sonos, entity_registry: er.EntityRegistry
+) -> None:
     """Test sonos device with alarm registered in the device registry."""
-    entity_registry = ent_reg.async_get(hass)
-
     assert "media_player.zone_a" in entity_registry.entities
     assert "switch.sonos_alarm_14" in entity_registry.entities
     assert "switch.zone_a_status_light" in entity_registry.entities
@@ -39,11 +39,13 @@ async def test_entity_registry(hass: HomeAssistant, async_autosetup_sonos) -> No
 
 
 async def test_switch_attributes(
-    hass: HomeAssistant, async_autosetup_sonos, soco, fire_zgs_event
+    hass: HomeAssistant,
+    async_autosetup_sonos,
+    soco,
+    fire_zgs_event,
+    entity_registry: er.EntityRegistry,
 ) -> None:
     """Test for correct Sonos switch states."""
-    entity_registry = ent_reg.async_get(hass)
-
     alarm = entity_registry.entities["switch.sonos_alarm_14"]
     alarm_state = hass.states.get(alarm.entity_id)
     assert alarm_state.state == STATE_ON
@@ -135,10 +137,9 @@ async def test_alarm_create_delete(
     alarm_clock,
     alarm_clock_extended,
     alarm_event,
+    entity_registry: er.EntityRegistry,
 ) -> None:
     """Test for correct creation and deletion of alarms during runtime."""
-    entity_registry = ent_reg.async_get(hass)
-
     one_alarm = copy(alarm_clock.ListAlarms.return_value)
     two_alarms = copy(alarm_clock_extended.ListAlarms.return_value)
 
