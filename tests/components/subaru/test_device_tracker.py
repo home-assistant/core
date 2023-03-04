c928114ed47bb411f61e89038f726e39dbf786a1
G-Two <7310260+G-Two@users.noreply.github.com>
2023-03-04 19:30:15 +0000

    Add test for device tracker

---
+++ tests/components/subaru/test_device_tracker.py
@@ -0,0 +1,22 @@
+"""Test Subaru device tracker."""
+from subarulink.const import LATITUDE, LONGITUDE
+
+from homeassistant.const import ATTR_LATITUDE, ATTR_LONGITUDE
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from .api_responses import EXPECTED_STATE_EV_IMPERIAL
+
+DEVICE_ID = "device_tracker.test_vehicle_2_location"
+
+
+async def test_location(hass: HomeAssistant, ev_entry) -> None:
+    """Test subaru location entity exists and has correct info."""
+    entity_registry = er.async_get(hass)
+    entry = entity_registry.async_get(DEVICE_ID)
+    assert entry
+    actual = hass.states.get(DEVICE_ID)
+    assert (
+        actual.attributes.get(ATTR_LONGITUDE) == EXPECTED_STATE_EV_IMPERIAL[LONGITUDE]
+    )
+    assert actual.attributes.get(ATTR_LATITUDE) == EXPECTED_STATE_EV_IMPERIAL[LATITUDE]
