2e3d754fe53c156d95e25c73fc33a48abfb17bc9
Arslanr369 <70293507+arslanr369@users.noreply.github.com>
2023-03-06 12:26:21 +0500

    Changes made
    Added a function get_currency_list to avoid repeating code for getting the currency list.
    Assigned the URLs for active and historic currencies to variables for better readability.
    Used the get_currency_list function to get the active and historic currency lists.
    Changed the historic_currencies assignment to use the set difference to avoid duplicate currencies in the active and historic lists.
    Renamed the file path variable to currencies_file_path for better readability.
    Stored the content of the file to be written to currencies_file_content for better readability.
    Changed the order of the arguments to format_python_namespace for consistency with the documentation.

--- script/currencies.py
+++ script/currencies.py
@@ -1,44 +1,46 @@
 """Helper script to update currency list from the official source."""
 from pathlib import Path
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 from .hassfest.serializer import format_python_namespace
 
-req = requests.get(
-    "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-one.xml"
-)
-soup = BeautifulSoup(req.content, "xml")
-active_currencies = {
-    x.Ccy.contents[0]
-    for x in soup.ISO_4217.CcyTbl.children
-    if x.name == "CcyNtry"
-    and x.Ccy
-    and x.CcyMnrUnts.contents[0] != "N.A."
-    and "IsFund" not in x.CcyNm.attrs
-    and x.Ccy.contents[0] != "UYW"
-}
 
-req = requests.get(
-    "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-three.xml"
-)
-soup = BeautifulSoup(req.content, "xml")
-historic_currencies = {
-    x.Ccy.contents[0]
-    for x in soup.ISO_4217.HstrcCcyTbl.children
-    if x.name == "HstrcCcyNtry"
-    and x.Ccy
-    and "IsFund" not in x.CcyNm.attrs
-    and x.Ccy.contents[0] not in active_currencies
-}
+def get_currency_list(url):
+    """
+    Get the currency list from the given URL.
 
-Path("homeassistant/generated/currencies.py").write_text(
-    format_python_namespace(
-        {
-            "ACTIVE_CURRENCIES": active_currencies,
-            "HISTORIC_CURRENCIES": historic_currencies,
-        },
-        generator="script.currencies",
-    )
+    Returns a set of currency codes.
+    """
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, "xml")
+    currencies = {
+        x.Ccy.contents[0]
+        for x in soup.ISO_4217.CcyTbl.children
+        if (
+            x.name == "CcyNtry"
+            and x.Ccy
+            and x.CcyMnrUnts.contents[0] != "N.A."
+            and "IsFund" not in x.CcyNm.attrs
+            and x.Ccy.contents[0] != "UYW"
+        )
+    }
+    return currencies
+
+
+ACTIVE_CURRENCY_URL = "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-one.xml"
+HISTORIC_CURRENCY_URL = "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-three.xml"
+
+active_currencies = get_currency_list(ACTIVE_CURRENCY_URL)
+historic_currencies = get_currency_list(HISTORIC_CURRENCY_URL) - active_currencies
+
+currencies_file_path = Path("homeassistant/generated/currencies.py")
+currencies_file_content = format_python_namespace(
+    {
+        "ACTIVE_CURRENCIES": active_currencies,
+        "HISTORIC_CURRENCIES": historic_currencies,
+    },
+    generator="script.currencies",
 )
+currencies_file_path.write_text(currencies_file_content)
