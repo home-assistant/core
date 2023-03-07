8d3e2d114ad4d07e6df0760804569a986431ccee
starkillerOG <starkiller.og@gmail.com>
2023-03-07 10:26:43 +0100

    review comments

--- tests/components/reolink/conftest.py
+++ tests/components/reolink/conftest.py
@@ -60,3 +60,25 @@ def reolink_init_fixture(mock_get_source_ip):
     """Mock reolink entry setup."""
     with patch("homeassistant.components.reolink.PLATFORMS", return_value=[]):
         yield
+
+
+@pytest.fixture(name="config_entry")
+def reolink_config_entry_fixture(hass: HomeAssistant) -> MockConfigEntry:
+    """Add the reolink mock config entry to hass."""
+    config_entry = MockConfigEntry(
+        domain=const.DOMAIN,
+        unique_id=format_mac(TEST_MAC),
+        data={
+            CONF_HOST: TEST_HOST,
+            CONF_USERNAME: TEST_USERNAME,
+            CONF_PASSWORD: TEST_PASSWORD,
+            CONF_PORT: TEST_PORT,
+            const.CONF_USE_HTTPS: TEST_USE_HTTPS,
+        },
+        options={
+            const.CONF_PROTOCOL: DEFAULT_PROTOCOL,
+        },
+        title=TEST_NVR_NAME,
+    )
+    config_entry.add_to_hass(hass)
+    return config_entry
--- tests/components/reolink/test_init.py
+++ tests/components/reolink/test_init.py
@@ -29,28 +29,6 @@ from tests.common import MockConfigEntry
 pytestmark = pytest.mark.usefixtures("reolink_connect", "reolink_init")
 
 
-@pytest.fixture(name="config_entry")
-def reolink_config_entry_fixture(hass: HomeAssistant) -> MockConfigEntry:
-    """Add the reolink mock config entry to hass."""
-    config_entry = MockConfigEntry(
-        domain=const.DOMAIN,
-        unique_id=format_mac(TEST_MAC),
-        data={
-            CONF_HOST: TEST_HOST,
-            CONF_USERNAME: TEST_USERNAME,
-            CONF_PASSWORD: TEST_PASSWORD,
-            CONF_PORT: TEST_PORT,
-            const.CONF_USE_HTTPS: TEST_USE_HTTPS,
-        },
-        options={
-            const.CONF_PROTOCOL: DEFAULT_PROTOCOL,
-        },
-        title=TEST_NVR_NAME,
-    )
-    config_entry.add_to_hass(hass)
-    return config_entry
-
-
 @pytest.mark.parametrize(
     ("attr", "value", "expected"),
     [
@@ -102,8 +80,6 @@ async def test_failures_parametrized(
     await hass.async_block_till_done()
 
     assert config_entry.state == expected
-    if expected != ConfigEntryState.LOADED:
-        assert not hass.data.get(const.DOMAIN)
 
 
 async def test_update_listener(
