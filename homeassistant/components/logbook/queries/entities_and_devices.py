86ad8261d8a0172dac92b6d76628752db271a744
J. Nick Koston <nick@koston.org>
2023-03-09 02:03:08 -1000

    Update logbook queries for SADeprecationWarning (#87108)

--- homeassistant/components/logbook/queries/entities_and_devices.py
+++ homeassistant/components/logbook/queries/entities_and_devices.py
@@ -46,7 +46,7 @@ def _select_entities_device_id_context_ids_sub_query(
             (States.last_updated_ts > start_day) & (States.last_updated_ts < end_day)
         )
         .where(States.entity_id.in_(entity_ids)),
-    )
+    ).subquery()
     return select(union.c.context_id_bin).group_by(union.c.context_id_bin)
 
 
