7518d18437689c55006a104b04eb5d9ac3c55162
Vincent Knoop Pathuis <git@vpathuis.dds.nl>
2023-03-09 11:03:08 +0000

    Move coordinator to own file and add test cases

--- tests/components/landisgyr_heat_meter/test_sensor.py
+++ tests/components/landisgyr_heat_meter/test_sensor.py
@@ -3,11 +3,13 @@ from dataclasses import dataclass
 import datetime
 from unittest.mock import patch
 
+import serial
+
 from homeassistant.components.homeassistant import (
     DOMAIN as HA_DOMAIN,
     SERVICE_UPDATE_ENTITY,
 )
-from homeassistant.components.landisgyr_heat_meter.const import DOMAIN
+from homeassistant.components.landisgyr_heat_meter.const import DOMAIN, POLLING_INTERVAL
 from homeassistant.components.sensor import (
     ATTR_LAST_RESET,
     ATTR_STATE_CLASS,
@@ -19,6 +21,7 @@ from homeassistant.const import (
     ATTR_ENTITY_ID,
     ATTR_ICON,
     ATTR_UNIT_OF_MEASUREMENT,
+    STATE_UNAVAILABLE,
     EntityCategory,
     UnitOfEnergy,
     UnitOfVolume,
@@ -28,7 +31,15 @@ from homeassistant.helpers import entity_registry as er
 from homeassistant.setup import async_setup_component
 from homeassistant.util import dt as dt_util
 
-from tests.common import MockConfigEntry, mock_restore_cache_with_extra_data
+from tests.common import (
+    MockConfigEntry,
+    async_fire_time_changed,
+    mock_restore_cache_with_extra_data,
+)
+
+API_HEAT_METER_SERVICE = (
+    "homeassistant.components.landisgyr_heat_meter.ultraheat_api.HeatMeterService"
+)
 
 
 @dataclass
@@ -42,7 +53,7 @@ class MockHeatMeterResponse:
     meter_date_time: datetime.datetime
 
 
-@patch("homeassistant.components.landisgyr_heat_meter.ultraheat_api.HeatMeterService")
+@patch(API_HEAT_METER_SERVICE)
 async def test_create_sensors(
     mock_heat_meter, hass: HomeAssistant, entity_registry: er.EntityRegistry
 ) -> None:
@@ -57,9 +68,9 @@ async def test_create_sensors(
     mock_entry.add_to_hass(hass)
 
     mock_heat_meter_response = MockHeatMeterResponse(
-        heat_usage_gj=123,
-        volume_usage_m3=456,
-        heat_previous_year_gj=111,
+        heat_usage_gj=123.0,
+        volume_usage_m3=456.0,
+        heat_previous_year_gj=111.0,
         device_number="devicenr_789",
         meter_date_time=dt_util.as_utc(datetime.datetime(2022, 5, 19, 19, 41, 17)),
     )
@@ -89,7 +100,7 @@ async def test_create_sensors(
 
     state = hass.states.get("sensor.heat_meter_volume_usage")
     assert state
-    assert state.state == "456"
+    assert state.state == "456.0"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UnitOfVolume.CUBIC_METERS
     assert state.attributes.get(ATTR_STATE_CLASS) == SensorStateClass.TOTAL
 
@@ -110,7 +121,7 @@ async def test_create_sensors(
     assert entity_registry_entry.entity_category == EntityCategory.DIAGNOSTIC
 
 
-@patch("homeassistant.components.landisgyr_heat_meter.ultraheat_api.HeatMeterService")
+@patch(API_HEAT_METER_SERVICE)
 async def test_restore_state(mock_heat_meter, hass: HomeAssistant) -> None:
     """Test sensor restore state."""
     # Home assistant is not running yet
@@ -199,3 +210,89 @@ async def test_restore_state(mock_heat_meter, hass: HomeAssistant) -> None:
     assert state
     assert state.state == "devicenr_789"
     assert state.attributes.get(ATTR_STATE_CLASS) is None
+
+
+@patch(API_HEAT_METER_SERVICE)
+async def test_exception_during_setup(mock_heat_meter, hass: HomeAssistant) -> None:
+    """Test sensor."""
+    entry_data = {
+        "device": "/dev/USB0",
+        "model": "LUGCUH50",
+        "device_number": "123456789",
+    }
+    mock_entry = MockConfigEntry(domain=DOMAIN, unique_id=DOMAIN, data=entry_data)
+    mock_heat_meter().read.side_effect = serial.serialutil.SerialException
+    mock_heat_meter.reset_mock()
+    mock_entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(mock_entry.entry_id)
+    await async_setup_component(hass, HA_DOMAIN, {})
+    await hass.async_block_till_done()
+
+    mock_heat_meter.assert_called_once()
+
+
+@patch(API_HEAT_METER_SERVICE)
+async def test_exception_on_polling(mock_heat_meter, hass: HomeAssistant) -> None:
+    """Test sensor."""
+    entry_data = {
+        "device": "/dev/USB0",
+        "model": "LUGCUH50",
+        "device_number": "123456789",
+    }
+    mock_entry = MockConfigEntry(domain=DOMAIN, unique_id=DOMAIN, data=entry_data)
+    mock_entry.add_to_hass(hass)
+
+    # First setup normally
+    mock_heat_meter_response = MockHeatMeterResponse(
+        heat_usage_gj=123,
+        volume_usage_m3=456,
+        heat_previous_year_gj=111,
+        device_number="devicenr_789",
+        meter_date_time=dt_util.as_utc(datetime.datetime(2022, 5, 19, 19, 41, 17)),
+    )
+
+    mock_heat_meter().read.return_value = mock_heat_meter_response
+
+    await hass.config_entries.async_setup(mock_entry.entry_id)
+    await async_setup_component(hass, HA_DOMAIN, {})
+    await hass.async_block_till_done()
+    await hass.services.async_call(
+        HA_DOMAIN,
+        SERVICE_UPDATE_ENTITY,
+        {ATTR_ENTITY_ID: "sensor.heat_meter_heat_usage"},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    # check if initial setup succeeded
+    assert len(hass.states.async_all()) == 27
+    state = hass.states.get("sensor.heat_meter_heat_usage")
+    assert state
+    assert state.state == "34.16669"
+
+    # Now 'disable' the connection and wait for polling
+    mock_heat_meter.reset_mock()
+    mock_heat_meter().read.side_effect = serial.serialutil.SerialException
+    async_fire_time_changed(hass, dt_util.utcnow() + POLLING_INTERVAL)
+    await hass.async_block_till_done()
+    mock_heat_meter.assert_called_once()
+    state = hass.states.get("sensor.heat_meter_heat_usage")
+    assert state.state == STATE_UNAVAILABLE
+
+    # Now 'enable' and see if next poll succeeds
+    mock_heat_meter().read.side_effect = None
+    mock_heat_meter.reset_mock()
+    mock_heat_meter_response = MockHeatMeterResponse(
+        heat_usage_gj=123.0,
+        volume_usage_m3=456.0,
+        heat_previous_year_gj=111.0,
+        device_number="devicenr_789",
+        meter_date_time=dt_util.as_utc(datetime.datetime(2022, 5, 19, 19, 41, 17)),
+    )
+    mock_heat_meter().read.return_value = mock_heat_meter_response
+    async_fire_time_changed(hass, dt_util.utcnow() + POLLING_INTERVAL)
+    await hass.async_block_till_done()
+    mock_heat_meter.assert_called_once()
+    state = hass.states.get("sensor.heat_meter_heat_usage")
+    assert state.state == "34.16669"
