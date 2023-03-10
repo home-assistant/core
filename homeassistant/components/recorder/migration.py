1d2c34ad36db5f2267c939e464f0b13772dfbf86
J. Nick Koston <nick@koston.org>
2023-03-10 13:37:27 -1000

    fix typo

--- homeassistant/components/recorder/migration.py
+++ homeassistant/components/recorder/migration.py
@@ -983,7 +983,7 @@ def _apply_update(  # noqa: C901
     elif new_version == 37:
         _add_columns(session_maker, "events", [f"event_type_id {big_int}"])
         _create_index(session_maker, "events", "ix_events_event_type_id")
-        _create_index(session_maker, "events", "ix_events_event_type_id_timed_fired_ts")
+        _create_index(session_maker, "events", "ix_events_event_type_id_time_fired_ts")
     else:
         raise ValueError(f"No schema migration defined for version {new_version}")
 
@@ -1341,7 +1341,7 @@ def migrate_event_type_ids(instance: Recorder) -> bool:
     if is_done:
         _drop_index(session_maker, "events", "ix_events_event_type", quiet=True)
         _drop_index(
-            session_maker, "events", "ix_events_event_type_timed_fired_ts", quiet=True
+            session_maker, "events", "ix_events_event_type_time_fired_ts", quiet=True
         )
         instance.event_type_manager.active = True
 
