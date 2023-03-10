526a9dde874242b8872589eb0ec9e69cc20e8246
J. Nick Koston <nick@koston.org>
2023-03-09 14:24:53 -1000

    fix tests

--- tests/components/recorder/test_models.py
+++ tests/components/recorder/test_models.py
@@ -35,6 +35,7 @@ def test_from_event_to_db_event() -> None:
     db_event = Events.from_event(event)
     dialect = SupportedDialect.MYSQL
     db_event.event_data = EventData.shared_data_bytes_from_event(event, dialect)
+    db_event.event_type = event.event_type
     assert event.as_dict() == db_event.to_native().as_dict()
 
 
@@ -308,11 +309,13 @@ async def test_event_to_db_model() -> None:
     db_event = Events.from_event(event)
     dialect = SupportedDialect.MYSQL
     db_event.event_data = EventData.shared_data_bytes_from_event(event, dialect)
+    db_event.event_type = event.event_type
     native = db_event.to_native()
     assert native.as_dict() == event.as_dict()
 
     native = Events.from_event(event).to_native()
     event.data = {}
+    native.event_type = event.event_type
     assert native.as_dict() == event.as_dict()
 
 
