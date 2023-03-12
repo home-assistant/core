4f9ee37beeb04aa5d50fd4dc8bf8454da49848ee
J. Nick Koston <nick@koston.org>
2023-03-12 13:18:16 -1000

    add retry dec

--- homeassistant/components/recorder/migration.py
+++ homeassistant/components/recorder/migration.py
@@ -64,7 +64,7 @@ from .tasks import (
     PostSchemaMigrationTask,
     StatisticsTimestampMigrationCleanupTask,
 )
-from .util import database_job_retry_wrapper, session_scope
+from .util import database_job_retry_wrapper, retryable_database_job, session_scope
 
 if TYPE_CHECKING:
     from . import Recorder
@@ -1254,6 +1254,7 @@ def _context_id_to_bytes(context_id: str | None) -> bytes | None:
     return None
 
 
+@retryable_database_job("migrate states context_ids to binary format")
 def migrate_states_context_ids(instance: Recorder) -> bool:
     """Migrate states context_ids to use binary format."""
     _to_bytes = _context_id_to_bytes
@@ -1287,6 +1288,7 @@ def migrate_states_context_ids(instance: Recorder) -> bool:
     return is_done
 
 
+@retryable_database_job("migrate events context_ids to binary format")
 def migrate_events_context_ids(instance: Recorder) -> bool:
     """Migrate events context_ids to use binary format."""
     _to_bytes = _context_id_to_bytes
@@ -1320,6 +1322,7 @@ def migrate_events_context_ids(instance: Recorder) -> bool:
     return is_done
 
 
+@retryable_database_job("migrate events event_types to event_type_ids")
 def migrate_event_type_ids(instance: Recorder) -> bool:
     """Migrate event_type to event_type_ids."""
     session_maker = instance.get_session
@@ -1376,6 +1379,7 @@ def migrate_event_type_ids(instance: Recorder) -> bool:
     return is_done
 
 
+@retryable_database_job("migrate states entity_ids to states_meta")
 def migrate_entity_ids(instance: Recorder) -> bool:
     """Migrate entity_ids to states_meta.
 
@@ -1437,6 +1441,7 @@ def migrate_entity_ids(instance: Recorder) -> bool:
     return is_done
 
 
+@retryable_database_job("post migrate states entity_ids to states_meta")
 def post_migrate_entity_ids(instance: Recorder) -> bool:
     """Remove old entity_id strings from states.
 
