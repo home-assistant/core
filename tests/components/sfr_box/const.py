2bedbc145d61bb559eaedaa04c205be20708a3d5
epenet <6771947+epenet@users.noreply.github.com>
2023-03-13 13:04:53 +0000

    Use SnapshotAssertion in SFR button tests

--- tests/components/sfr_box/const.py
+++ tests/components/sfr_box/const.py
@@ -1,18 +1,13 @@
 """Constants for SFR Box tests."""
-from homeassistant.components.button import ButtonDeviceClass
 from homeassistant.components.sensor import ATTR_OPTIONS, ATTR_STATE_CLASS
 from homeassistant.components.sfr_box.const import DOMAIN
 from homeassistant.const import (
     ATTR_DEVICE_CLASS,
-    ATTR_ENTITY_ID,
     ATTR_IDENTIFIERS,
     ATTR_MODEL,
     ATTR_NAME,
-    ATTR_STATE,
     ATTR_SW_VERSION,
     ATTR_UNIT_OF_MEASUREMENT,
-    STATE_UNKNOWN,
-    Platform,
 )
 
 ATTR_DEFAULT_DISABLED = "default_disabled"
@@ -31,12 +26,4 @@ EXPECTED_ENTITIES = {
         ATTR_NAME: "SFR Box",
         ATTR_SW_VERSION: "NB6VAC-MAIN-R4.0.44k",
     },
-    Platform.BUTTON: [
-        {
-            ATTR_DEVICE_CLASS: ButtonDeviceClass.RESTART,
-            ATTR_ENTITY_ID: "button.sfr_box_reboot",
-            ATTR_STATE: STATE_UNKNOWN,
-            ATTR_UNIQUE_ID: "e4:5d:51:00:11:22_system_reboot",
-        },
-    ],
 }
