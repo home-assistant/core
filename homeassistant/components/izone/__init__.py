47f7e736c4d9ba848eb0941d7e0749b48ca8a86a
Penny Wood <github@ninjateaparty.com>
2023-03-15 01:21:00 +0000

    Manage discovery service in async_setup

--- homeassistant/components/izone/__init__.py
+++ homeassistant/components/izone/__init__.py
@@ -1,7 +1,6 @@
 """Platform for the iZone AC."""
 import voluptuous as vol
 
-from homeassistant import config_entries
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_EXCLUDE, EVENT_HOMEASSISTANT_STOP, Platform
 from homeassistant.core import HomeAssistant
@@ -29,37 +28,28 @@ CONFIG_SCHEMA = vol.Schema(
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Register the iZone component config."""
-    if not (conf := config.get(IZONE)):
-        return True
 
-    hass.data[DATA_CONFIG] = conf
+    # Check for manually added config, this may exclude some devices
+    if conf := config.get(IZONE):
+        hass.data[DATA_CONFIG] = conf
 
-    # Explicitly added in the config file, create a config entry.
-    hass.async_create_task(
-        hass.config_entries.flow.async_init(
-            IZONE, context={"source": config_entries.SOURCE_IMPORT}
-        )
-    )
+    # Start the discovery service
+    await async_start_discovery_service(hass)
+
+    async def shutdown_event(event):
+        await async_stop_discovery_service(hass)
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, shutdown_event)
 
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up from a config entry."""
-    await async_start_discovery_service(hass)
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
-    async def shutdown_event(event):
-        await async_stop_discovery_service(hass)
-
-    entry.async_on_unload(
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, shutdown_event)
-    )
-
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload the config entry and stop discovery process."""
-    await async_stop_discovery_service(hass)
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
