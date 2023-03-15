c8e251ac7966ef3c2f8ace4138cdbfbd4055bc52
nachonam <nachonam@hotmail.com>
2023-03-15 08:07:25 +0100

    Update homeassistant/components/freebox/camera.py
    Use const not literal
    
    Co-authored-by: Quentame <polletquentin74@me.com>

--- homeassistant/components/freebox/camera.py
+++ homeassistant/components/freebox/camera.py
@@ -69,7 +69,7 @@ def add_entities(hass: HomeAssistant, router, async_add_entities, tracked):
     new_tracked = []
 
     for nodeid, node in router.home_devices.items():
-        if (node["category"] != "camera") or (nodeid in tracked):
+        if (node["category"] != Platform.CAMERA) or (nodeid in tracked):
             continue
         new_tracked.append(FreeboxCamera(hass, router, node))
         tracked.add(nodeid)
