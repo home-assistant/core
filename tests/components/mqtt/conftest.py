ff7473d12d4b76d5dc7311697050c01d044e4d64
jbouwh <jan@jbsoft.nl>
2023-03-16 15:03:20 +0000

    Fix typo in docstr

--- tests/components/mqtt/conftest.py
+++ tests/components/mqtt/conftest.py
@@ -8,4 +8,4 @@ from tests.components.light.conftest import mock_light_profiles  # noqa: F401
 
 @pytest.fixture(autouse=True)
 def patch_hass_config(mock_hass_config: None) -> None:
-    """Patch configuration].yaml."""
+    """Patch configuration.yaml."""
