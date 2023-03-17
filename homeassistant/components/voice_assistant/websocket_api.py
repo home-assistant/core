6f9cea32dbec05ed4e4ebd1bc8a7a39455e8da79
Michael Hansen <mike@rhasspy.org>
2023-03-17 13:52:31 -0500

    Refactor pipeline request to split text/audio

--- homeassistant/components/voice_assistant/websocket_api.py
+++ homeassistant/components/voice_assistant/websocket_api.py
@@ -7,7 +7,7 @@ from homeassistant.components import websocket_api
 from homeassistant.core import HomeAssistant, callback
 
 from .const import DOMAIN
-from .pipeline import DEFAULT_TIMEOUT, PipelineRequest, PipelineStage
+from .pipeline import DEFAULT_TIMEOUT, TextPipelineRequest
 
 
 @callback
@@ -23,8 +23,6 @@ def async_register_websocket_api(hass: HomeAssistant) -> None:
         vol.Required("intent_input"): str,
         vol.Optional("conversation_id"): vol.Any(str, None),
         vol.Optional("timeout"): vol.Any(float, int),
-        vol.Optional("start_stage"): str,
-        vol.Optional("stop_stage"): str,
     }
 )
 @websocket_api.async_response
@@ -42,25 +40,20 @@ async def websocket_run(
         )
         return
 
-    start_stage = PipelineStage(msg["start_stage"]) if "start_stage" in msg else None
-    stop_stage = PipelineStage(msg["stop_stage"]) if "stop_stage" in msg else None
-
     # Run pipeline with a timeout.
     # Events are sent over the websocket connection.
     timeout = msg.get("timeout", DEFAULT_TIMEOUT)
     run_task = hass.async_create_task(
-        pipeline.run(
+        TextPipelineRequest(
+            intent_input=msg["intent_input"],
+            conversation_id=msg.get("conversation_id"),
+        ).execute(
             hass,
             connection.context(msg),
-            request=PipelineRequest(
-                intent_input=msg["intent_input"],
-                conversation_id=msg.get("conversation_id"),
-            ),
+            pipeline,
             event_callback=lambda event: connection.send_event(
                 msg["id"], event.as_dict()
             ),
-            start_stage=start_stage,
-            stop_stage=stop_stage,
             timeout=timeout,
         )
     )
