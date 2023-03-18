e8aa2b3bc313ebfc03c8de5ca988585cc5c6e049
J. Nick Koston <nick@koston.org>
2023-03-18 09:49:23 -1000

    Add generate function for ble device since rssi and details are now a required field

--- tests/components/bluetooth/__init__.py
+++ tests/components/bluetooth/__init__.py
@@ -62,9 +62,23 @@ def generate_advertisement_data(**kwargs: Any) -> AdvertisementData:
     return AdvertisementData(**new)
 
 
-def generate_ble_device(**kwargs: Any) -> BLEDevice:
+def generate_ble_device(
+    address: str | None = None,
+    name: str | None = None,
+    details: Any | None = None,
+    rssi: int | None = None,
+    **kwargs: Any,
+) -> BLEDevice:
     """Generate a BLEDevice with defaults."""
     new = kwargs.copy()
+    if address is not None:
+        new["address"] = address
+    if name is not None:
+        new["name"] = name
+    if details is not None:
+        new["details"] = details
+    if rssi is not None:
+        new["rssi"] = rssi
     for key, value in BLE_DEVICE_DEFAULTS.items():
         new.setdefault(key, value)
     return BLEDevice(**new)
