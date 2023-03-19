8878a505bbd3ee5524fed64aec07ee2a332c170f
TheJulianJES <TheJulianJES@users.noreply.github.com>
2023-03-20 00:10:17 +0100

    Re-enable occupancy binary sensor test
    This test seems to work fine and I don't see any reason why it was commented out for a while

--- tests/components/zha/test_binary_sensor.py
+++ tests/components/zha/test_binary_sensor.py
@@ -85,7 +85,7 @@ async def async_test_iaszone_on_off(hass, cluster, entity_id):
     ("device", "on_off_test", "cluster_name", "reporting"),
     [
         (DEVICE_IAS, async_test_iaszone_on_off, "ias_zone", (0,)),
-        # (DEVICE_OCCUPANCY, async_test_binary_sensor_on_off, "occupancy", (1,)),
+        (DEVICE_OCCUPANCY, async_test_binary_sensor_on_off, "occupancy", (1,)),
     ],
 )
 async def test_binary_sensor(
