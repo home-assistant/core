7a2b6bd33a5827383954e4137e053b18cd0d11a9
Penney, Emory J <treadstoneit@gmail.com>
2023-03-20 20:38:15 +0000

    Use PyObihai to update Unique ID

--- homeassistant/components/obihai/__init__.py
+++ homeassistant/components/obihai/__init__.py
@@ -1,30 +1,24 @@
 """The Obihai integration."""
 
-from socket import gaierror, gethostbyname
-
-from getmac import get_mac_address
-
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
 
+from .connectivity import ObihaiConnection
 from .const import PLATFORMS
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up from a config entry."""
-    try:
-        ip = gethostbyname(entry.data[CONF_HOST])
-    except gaierror as ex:
-        raise ConfigEntryNotReady(
-            f"Unable to connect to {entry.data[CONF_HOST]}"
-        ) from ex
+    requester = ObihaiConnection(
+        entry.data[CONF_HOST],
+        username=entry.data[CONF_USERNAME],
+        password=entry.data[CONF_PASSWORD],
+    )
+    await hass.async_add_executor_job(requester.update)
 
-    new_unique_id = get_mac_address(ip=ip)
-    old_unique_id = entry.unique_id
-
-    if old_unique_id != new_unique_id:
+    new_unique_id = await hass.async_add_executor_job(requester.pyobihai.get_device_mac)
+    if entry.unique_id != new_unique_id:
         hass.config_entries.async_update_entry(entry, unique_id=new_unique_id)
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
