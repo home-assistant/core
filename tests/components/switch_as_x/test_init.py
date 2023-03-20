1d6869ccbf5d4f28b7cee9986343f5e8f76ddadf
Erik <erik@montnemery.com>
2023-03-20 15:34:43 +0100

    Fix switch_as_x entity naming

--- tests/components/switch_as_x/test_init.py
+++ tests/components/switch_as_x/test_init.py
@@ -474,3 +474,57 @@ async def test_entity_options(
     assert entity_entry.options == {
         DOMAIN: {"entity_id": switch_entity_entry.entity_id}
     }
+
+
+@pytest.mark.parametrize("target_domain", PLATFORMS_TO_TEST)
+async def test_entity_name(
+    hass: HomeAssistant,
+    target_domain: Platform,
+) -> None:
+    """Test the source entity has entity_name set to True."""
+    registry = er.async_get(hass)
+    device_registry = dr.async_get(hass)
+
+    switch_config_entry = MockConfigEntry()
+
+    device_entry = device_registry.async_get_or_create(
+        config_entry_id=switch_config_entry.entry_id,
+        connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
+        name="Device name",
+    )
+
+    switch_entity_entry = registry.async_get_or_create(
+        "switch",
+        "test",
+        "unique",
+        device_id=device_entry.id,
+        has_entity_name=True,
+    )
+    switch_entity_entry = registry.async_update_entity(
+        switch_entity_entry.entity_id,
+        config_entry_id=switch_config_entry.entry_id,
+    )
+
+    # Add the config entry
+    switch_as_x_config_entry = MockConfigEntry(
+        data={},
+        domain=DOMAIN,
+        options={
+            CONF_ENTITY_ID: switch_entity_entry.id,
+            CONF_TARGET_DOMAIN: target_domain,
+        },
+        title="ABC",
+    )
+    switch_as_x_config_entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(switch_as_x_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    entity_entry = registry.async_get(f"{target_domain}.device_name")
+    assert entity_entry
+    assert entity_entry.device_id == switch_entity_entry.device_id
+    assert entity_entry.has_entity_name is True
+    assert entity_entry.original_name is None
+    assert entity_entry.options == {
+        DOMAIN: {"entity_id": switch_entity_entry.entity_id}
+    }
