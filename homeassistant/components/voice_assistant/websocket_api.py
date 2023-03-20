66cd184a65d89180d03b0beea9df7440e1e921e4
Michael Hansen <mike@rhasspy.org>
2023-03-20 16:19:28 -0500

    Generate pipeline from language

--- homeassistant/components/voice_assistant/websocket_api.py
+++ homeassistant/components/voice_assistant/websocket_api.py
@@ -7,7 +7,7 @@ from homeassistant.components import websocket_api
 from homeassistant.core import HomeAssistant, callback
 
 from .const import DOMAIN
-from .pipeline import DEFAULT_TIMEOUT, PipelineRun, TextPipelineRequest
+from .pipeline import DEFAULT_TIMEOUT, Pipeline, PipelineRun, TextPipelineRequest
 
 
 @callback
@@ -19,7 +19,8 @@ def async_register_websocket_api(hass: HomeAssistant) -> None:
 @websocket_api.websocket_command(
     {
         vol.Required("type"): "voice_assistant/run",
-        vol.Optional("pipeline", default="default"): str,
+        vol.Optional("language"): str,
+        vol.Optional("pipeline"): str,
         vol.Required("intent_input"): str,
         vol.Optional("conversation_id"): vol.Any(str, None),
         vol.Optional("timeout"): vol.Any(float, int),
@@ -32,13 +33,26 @@ async def websocket_run(
     msg: dict[str, Any],
 ) -> None:
     """Run a pipeline."""
-    pipeline_id = msg["pipeline"]
-    pipeline = hass.data[DOMAIN].get(pipeline_id)
-    if pipeline is None:
-        connection.send_error(
-            msg["id"], "pipeline_not_found", f"Pipeline not found: {pipeline_id}"
+    pipeline_id = msg.get("pipeline")
+    if pipeline_id is not None:
+        pipeline = hass.data[DOMAIN].get(pipeline_id)
+        if pipeline is None:
+            connection.send_error(
+                msg["id"],
+                "pipeline_not_found",
+                f"Pipeline not found: {pipeline_id}",
+            )
+            return
+
+    else:
+        # Construct a pipeline for the required/configured language
+        language = msg.get("language", hass.config.language)
+        pipeline = Pipeline(
+            name=language,
+            language=language,
+            conversation_engine=None,
+            tts_engine=None,
         )
-        return
 
     # Run pipeline with a timeout.
     # Events are sent over the websocket connection.
