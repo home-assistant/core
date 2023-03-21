7f51bb2ddfe780e3dc8f9b0c3d61fbbaaa200042
epenet <6771947+epenet@users.noreply.github.com>
2023-03-21 15:53:51 +0100

    Improve type hints in rachio switch (#90050)

--- homeassistant/components/rachio/switch.py
+++ homeassistant/components/rachio/switch.py
@@ -506,7 +506,7 @@ class RachioSchedule(RachioSwitch):
         return "mdi:water" if self.schedule_is_enabled else "mdi:water-off"
 
     @property
-    def extra_state_attributes(self) -> dict:
+    def extra_state_attributes(self) -> dict[str, Any]:
         """Return the optional state attributes."""
         return {
             ATTR_SCHEDULE_SUMMARY: self._summary,
