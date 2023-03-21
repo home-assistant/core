41cc95bfda11135ef7ce96a3a81cc1dd9deebed6
guy shefer <g.shefer29@gmail.com>
2023-03-21 15:36:38 +0000

    Improve tests

--- tests/components/tami4/test_config_flow.py
+++ tests/components/tami4/test_config_flow.py
@@ -1,52 +1,55 @@
 """Tests for the Tami4 config flow."""
-from unittest.mock import patch
+from collections.abc import Generator
+from unittest.mock import AsyncMock, patch
 
 from Tami4EdgeAPI.device import Device
+import pytest
 
-from homeassistant import config_entries, data_entry_flow
+from homeassistant import config_entries
 from homeassistant.components.tami4.const import CONF_PHONE, DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
 
-async def test_form(hass: HomeAssistant) -> None:
-    """Test we get the form."""
+@pytest.fixture
+def mock_setup_entry() -> Generator[AsyncMock, None, None]:
+    """Mock setting up a config entry."""
+    with patch(
+        "homeassistant.components.tami4.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        yield mock_setup_entry
 
-    user_form = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-    assert user_form["type"] == FlowResultType.FORM
-    assert user_form["step_id"] == "user"
-    assert user_form["handler"] == DOMAIN
-    assert user_form["errors"] == {}
 
+@pytest.fixture
+def mock_request_otp():
+    """Mock request_otp."""
     with patch(
         "homeassistant.components.tami4.config_flow.Tami4EdgeAPI.request_otp",
         return_value=None,
-    ), patch(
-        "homeassistant.components.tami4.async_setup_entry",
-        return_value=True,
-    ):
-        otp_form = await hass.config_entries.flow.async_configure(
-            user_form["flow_id"],
-            {CONF_PHONE: "+972511111111"},
-        )
-        await hass.async_block_till_done()
+    ) as mock_request_otp:
+        yield mock_request_otp
 
-    assert otp_form["type"] == FlowResultType.FORM
-    assert otp_form["step_id"] == "otp"
-    assert otp_form["handler"] == DOMAIN
-    assert otp_form["errors"] == {}
 
+@pytest.fixture
+def mock_submit_otp():
+    """Mock submit_otp."""
     with patch(
         "homeassistant.components.tami4.config_flow.Tami4EdgeAPI.submit_otp",
-        return_value="",
-    ), patch(
+        return_value="refresh_token",
+    ) as mock_submit_otp:
+        yield mock_submit_otp
+
+
+@pytest.fixture
+def mock__get_devices():
+    """Mock _get_devices."""
+
+    with patch(
         "homeassistant.components.tami4.config_flow.Tami4EdgeAPI._get_devices",
         return_value=[
             Device(
                 id=1,
-                name="device name",
+                name="Drink Water!",
                 connected=True,
                 last_heart_beat=1,
                 psn="psn",
@@ -54,41 +57,61 @@ async def test_form(hass: HomeAssistant) -> None:
                 device_firmware="firmware",
             )
         ],
-    ), patch(
-        "homeassistant.components.tami4.async_setup_entry",
-        return_value=True,
-    ) as mock_setup_entry:
-        result = await hass.config_entries.flow.async_configure(
-            otp_form["flow_id"],
-            {"otp": "1111"},
-        )
-        await hass.async_block_till_done()
-
-    assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["handler"] == DOMAIN
-    # assert result["data"] == {CONF_REFRESH_TOKEN: ""}
-    assert len(mock_setup_entry.mock_calls) == 1
+    ) as mock_get_devices:
+        yield mock_get_devices
 
 
-async def test_form_invalid_phone(hass: HomeAssistant) -> None:
-    """Test we handle invalid phone number."""
-
-    user_form = await hass.config_entries.flow.async_init(
+async def test_step_user_valid_number(
+    hass: HomeAssistant,
+    mock_setup_entry,
+    mock_request_otp,
+    mock_submit_otp,
+    mock__get_devices,
+):
+    """Test user step with valid phone number."""
+    result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+    assert result["errors"] == {}
 
-    with patch(
-        "homeassistant.components.tami4.config_flow.Tami4EdgeAPI.request_otp",
-        return_value=None,
-    ), patch(
-        "homeassistant.components.tami4.async_setup_entry",
-        return_value=True,
-    ):
-        otp_form = await hass.config_entries.flow.async_configure(
-            user_form["flow_id"],
-            {CONF_PHONE: "9999"},
-        )
-        await hass.async_block_till_done()
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={CONF_PHONE: "+972555555555"},
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "otp"
+    assert result["errors"] == {}
 
-    assert otp_form["type"] == data_entry_flow.FlowResultType.FORM
-    assert otp_form["errors"] == {"base": "invalid_phone"}
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={"otp": "123456"},
+    )
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    assert result["title"] == "Drink Water!"
+    assert "refresh_token" in result["data"]
+
+
+async def test_step_user_invalid_number(
+    hass: HomeAssistant,
+    mock_setup_entry,
+    mock_request_otp,
+    mock_submit_otp,
+    mock__get_devices,
+):
+    """Test user step with invalid phone number."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={CONF_PHONE: "+275123"},
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+    assert result["errors"] == {"base": "invalid_phone"}
