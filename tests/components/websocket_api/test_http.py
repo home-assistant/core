8761e5b11e7100e7798931be7a69455272f509a9
Paulus Schoutsen <balloob@gmail.com>
2023-03-21 21:21:00 -0400

    Allow signaling end of stream using empty payloads

--- tests/components/websocket_api/test_http.py
+++ tests/components/websocket_api/test_http.py
@@ -169,8 +169,8 @@ async def test_binary_message(
 ) -> None:
     """Test binary messages."""
     binary_payloads = {
-        104: asyncio.Future(),
-        105: asyncio.Future(),
+        104: ([], asyncio.Future()),
+        105: ([], asyncio.Future()),
     }
 
     # Register a handler
@@ -190,8 +190,13 @@ async def test_binary_message(
             hass: HomeAssistant, connection: ActiveConnection, payload: bytes
         ):
             nonlocal unsub
-            binary_payloads[msg["id"]].set_result(payload)
-            unsub()
+            if payload:
+                binary_payloads[msg["id"]][0].append(payload)
+            else:
+                binary_payloads[msg["id"]][1].set_result(
+                    b"".join(binary_payloads[msg["id"]][0])
+                )
+                unsub()
 
         prefix, unsub = connection.async_register_binary_handler(binary_message_handler)
 
@@ -212,10 +217,13 @@ async def test_binary_message(
 
     # Send message to binary
     await websocket_client.send_bytes((4).to_bytes(1, "big") + b"test4")
+    await websocket_client.send_bytes((4).to_bytes(1, "big") + b"")
     await websocket_client.send_bytes((5).to_bytes(1, "big") + b"test5")
-    await websocket_client.send_bytes((10).to_bytes(1, "big") + b"test5")
+    await websocket_client.send_bytes((5).to_bytes(1, "big") + b"test5-2")
+    await websocket_client.send_bytes((5).to_bytes(1, "big") + b"")
+    await websocket_client.send_bytes((10).to_bytes(1, "big") + b"test10")
 
     # Verify received
-    assert await binary_payloads[104] == b"test4"
-    assert await binary_payloads[105] == b"test5"
+    assert await binary_payloads[104][1] == b"test4"
+    assert await binary_payloads[105][1] == b"test5test5-2"
     assert "Received binary message for non-existing handler 10" in caplog.text
