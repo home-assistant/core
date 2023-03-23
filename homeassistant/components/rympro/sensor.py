4dd3664432f758e8693a72eb85198b992f57dec3
On Freund <onfreund@gmail.com>
2023-03-23 18:39:07 +0200

    Refactor with SensorEntityDescription

--- homeassistant/components/rympro/sensor.py
+++ homeassistant/components/rympro/sensor.py
@@ -1,9 +1,12 @@
 """Sensor for RymPro meters."""
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
+    SensorEntityDescription,
     SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
@@ -17,6 +20,37 @@ from .const import DOMAIN, MeterSensor
 from .coordinator import RymProDataUpdateCoordinator
 
 
+@dataclass
+class RymProSensorDescription(SensorEntityDescription):
+    """Class describing rympro sensor entities."""
+
+    sensor: MeterSensor | None = None
+
+
+SENSOR_DESCRIPTIONS: tuple[RymProSensorDescription, ...] = (
+    RymProSensorDescription(
+        key="total_consumption",
+        sensor=MeterSensor.TOTAL_CONSUMPTION,
+        name="Total consumption",
+    ),
+    RymProSensorDescription(
+        key="monthly_forecast",
+        sensor=MeterSensor.FORECAST,
+        name="Monthly forecast",
+    ),
+    RymProSensorDescription(
+        key="consumption_today",
+        sensor=MeterSensor.DAILY_CONSUMPTION,
+        name="Consumption today",
+    ),
+    RymProSensorDescription(
+        key="consumption_this_month",
+        sensor=MeterSensor.MONTHLY_CONSUMPTION,
+        name="Consumption this month",
+    ),
+)
+
+
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
@@ -25,42 +59,9 @@ async def async_setup_entry(
     """Set up sensors for device."""
     coordinator: RymProDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
     async_add_entities(
-        entity
+        RymProSensor(coordinator, meter_id, description, config_entry.entry_id)
         for meter_id, meter in coordinator.data.items()
-        for entity in (
-            RymProSensor(
-                coordinator,
-                meter_id,
-                sensor=MeterSensor.TOTAL_CONSUMPTION,
-                name="Total consumption",
-                uid_suffix="total_consumption",
-                entry_id=config_entry.entry_id,
-            ),
-            RymProSensor(
-                coordinator,
-                meter_id,
-                sensor=MeterSensor.FORECAST,
-                name="Monthly forecast",
-                uid_suffix="monthly_forecast",
-                entry_id=config_entry.entry_id,
-            ),
-            RymProSensor(
-                coordinator,
-                meter_id,
-                sensor=MeterSensor.DAILY_CONSUMPTION,
-                name="Consumption today",
-                uid_suffix="consumption_today",
-                entry_id=config_entry.entry_id,
-            ),
-            RymProSensor(
-                coordinator,
-                meter_id,
-                sensor=MeterSensor.MONTHLY_CONSUMPTION,
-                name="Consumption this month",
-                uid_suffix="consumption_this_month",
-                entry_id=config_entry.entry_id,
-            ),
-        )
+        for description in SENSOR_DESCRIPTIONS
     )
 
 
@@ -70,25 +71,24 @@ class RymProSensor(CoordinatorEntity[RymProDataUpdateCoordinator], SensorEntity)
     _attr_has_entity_name = True
     _attr_device_class = SensorDeviceClass.WATER
     _attr_native_unit_of_measurement = UnitOfVolume.CUBIC_METERS
-    _attr_native_precision = 3
+    _attr_suggested_display_precision = 3
     _attr_state_class = SensorStateClass.TOTAL_INCREASING
 
     def __init__(
         self,
         coordinator: RymProDataUpdateCoordinator,
         meter_id: int,
-        sensor: MeterSensor,
-        name: str,
-        uid_suffix: str,
+        description: RymProSensorDescription,
         entry_id: str,
     ) -> None:
         """Initialize sensor."""
         super().__init__(coordinator)
+        self.entity_description = description
         self._meter_id = meter_id
-        self._sensor = sensor
-        self._attr_name = name
+        assert description.sensor
+        self._sensor = description.sensor
         unique_id = f"{entry_id}_{meter_id}"
-        self._attr_unique_id = f"{unique_id}_{uid_suffix}"
+        self._attr_unique_id = f"{unique_id}_{description.key}"
         self._attr_extra_state_attributes = {"meter_id": str(meter_id)}
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, unique_id)},
