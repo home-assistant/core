3aae4cb7e327e3e9a56df6f33026f235cb493f8e
Paarth Shah <mail@shahpaarth.com>
2023-03-23 21:42:23 -0700

    Fix 3.11 unit test

--- homeassistant/components/matrix/__init__.py
+++ homeassistant/components/matrix/__init__.py
@@ -276,7 +276,7 @@ class MatrixBot:
 
     async def _join_rooms(self) -> None:
         """Join the Matrix rooms that we listen for commands in."""
-        rooms = {self._join_room(room_id) for room_id in self._listening_rooms}
+        rooms = {asyncio.create_task(self._join_room(room_id)) for room_id in self._listening_rooms}
         await asyncio.wait(rooms)
 
     async def _get_auth_tokens(self) -> JsonObjectType:
