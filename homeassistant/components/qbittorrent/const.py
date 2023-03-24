3bf1ac481ea1c99f2625fda1a81f05c45c5b9732
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2023-03-24 13:17:42 +0000

    move SENSOR_TYPE_* consts back to sensors.py

--- homeassistant/components/qbittorrent/const.py
+++ homeassistant/components/qbittorrent/const.py
@@ -1,6 +1,3 @@
 """Constants for qBittorrent."""
 
 DEFAULT_NAME = "qBittorrent"
-SENSOR_TYPE_CURRENT_STATUS = "current_status"
-SENSOR_TYPE_DOWNLOAD_SPEED = "download_speed"
-SENSOR_TYPE_UPLOAD_SPEED = "upload_speed"
