686cc01b52f4c695eacc27389955006c0a946921
Robert Svensson <Kane610@users.noreply.github.com>
2023-03-24 23:29:18 +0100

    Replace local TypeVar with library TypeVar

--- homeassistant/components/unifi/entity.py
+++ homeassistant/components/unifi/entity.py
@@ -13,7 +13,7 @@ from aiounifi.interfaces.api_handlers import (
     ItemEvent,
     UnsubscribeType,
 )
-from aiounifi.models.api import ApiItem
+from aiounifi.models.api import ApiItemT
 from aiounifi.models.event import Event, EventKey
 
 from homeassistant.core import callback
@@ -27,7 +27,6 @@ from .const import ATTR_MANUFACTURER
 if TYPE_CHECKING:
     from .controller import UniFiController
 
-DataT = TypeVar("DataT", bound=ApiItem)
 HandlerT = TypeVar("HandlerT", bound=APIHandler)
 SubscriptionT = Callable[[CallbackType, ItemEvent], UnsubscribeType]
 
@@ -60,7 +59,7 @@ def async_device_device_info_fn(api: aiounifi.Controller, obj_id: str) -> Device
 
 
 @dataclass
-class UnifiDescription(Generic[HandlerT, DataT]):
+class UnifiDescription(Generic[HandlerT, ApiItemT]):
     """Validate and load entities from different UniFi handlers."""
 
     allowed_fn: Callable[[UniFiController, str], bool]
@@ -69,21 +68,21 @@ class UnifiDescription(Generic[HandlerT, DataT]):
     device_info_fn: Callable[[aiounifi.Controller, str], DeviceInfo | None]
     event_is_on: tuple[EventKey, ...] | None
     event_to_subscribe: tuple[EventKey, ...] | None
-    name_fn: Callable[[DataT], str | None]
-    object_fn: Callable[[aiounifi.Controller, str], DataT]
+    name_fn: Callable[[ApiItemT], str | None]
+    object_fn: Callable[[aiounifi.Controller, str], ApiItemT]
     supported_fn: Callable[[UniFiController, str], bool | None]
     unique_id_fn: Callable[[UniFiController, str], str]
 
 
 @dataclass
-class UnifiEntityDescription(EntityDescription, UnifiDescription[HandlerT, DataT]):
+class UnifiEntityDescription(EntityDescription, UnifiDescription[HandlerT, ApiItemT]):
     """UniFi Entity Description."""
 
 
-class UnifiEntity(Entity, Generic[HandlerT, DataT]):
+class UnifiEntity(Entity, Generic[HandlerT, ApiItemT]):
     """Representation of a UniFi entity."""
 
-    entity_description: UnifiEntityDescription[HandlerT, DataT]
+    entity_description: UnifiEntityDescription[HandlerT, ApiItemT]
     _attr_should_poll = False
 
     _attr_unique_id: str
@@ -92,7 +91,7 @@ class UnifiEntity(Entity, Generic[HandlerT, DataT]):
         self,
         obj_id: str,
         controller: UniFiController,
-        description: UnifiEntityDescription[HandlerT, DataT],
+        description: UnifiEntityDescription[HandlerT, ApiItemT],
     ) -> None:
         """Set up UniFi switch entity."""
         self._obj_id = obj_id
