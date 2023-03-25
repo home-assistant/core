d0d12771c59f19ddeca2c138dd89cce55b7fce16
epenet <6771947+epenet@users.noreply.github.com>
2023-03-25 05:49:12 +0100

    Improve browse_media type hints in media player (#90060)
    * Improve browse_media type hints in media player
    
    * Adjust components
    
    * Adjust base entity

--- homeassistant/components/media_player/__init__.py
+++ homeassistant/components/media_player/__init__.py
@@ -1037,7 +1037,7 @@ class MediaPlayerEntity(Entity):
 
     async def async_browse_media(
         self,
-        media_content_type: str | None = None,
+        media_content_type: MediaType | str | None = None,
         media_content_id: str | None = None,
     ) -> BrowseMedia:
         """Return a BrowseMedia instance.
