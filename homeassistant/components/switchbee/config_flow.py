3f28be2f2d7934e5c0e3a3a06025501ca842933e
Jafar Atili <jafar.atili@axonius.com>
2023-03-26 10:51:03 +0000

    added migration

--- homeassistant/components/switchbee/config_flow.py
+++ homeassistant/components/switchbee/config_flow.py
@@ -56,7 +56,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> str:
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for SwitchBee Smart Home."""
 
-    VERSION = 1
+    VERSION = 2
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
