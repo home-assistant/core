9546e2a9a9ba8b419ce2c4db2a0c9d30b37069d9
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2023-03-26 20:06:15 +0000

    improve user flow tests

--- tests/components/qbittorrent/test_config_flow.py
+++ tests/components/qbittorrent/test_config_flow.py
@@ -54,7 +54,7 @@ async def test_flow_user(hass: HomeAssistant, mock_api) -> None:
         DOMAIN, context={CONF_SOURCE: SOURCE_USER}
     )
     assert result["type"] == FlowResultType.FORM
-    assert result["step_id"] == SOURCE_USER
+    assert result["step_id"] == "user"
 
     # Test flow with connection failure, fail with cannot_connect
     with requests_mock.Mocker() as mock:
@@ -62,14 +62,13 @@ async def test_flow_user(hass: HomeAssistant, mock_api) -> None:
             f"{CONFIG_CANNOT_CONNECT[CONF_URL]}/api/v2/app/preferences",
             exc=RequestException,
         )
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN,
-            context={CONF_SOURCE: SOURCE_USER},
-            data=CONFIG_CANNOT_CONNECT,
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], CONFIG_CANNOT_CONNECT
         )
-        assert result["type"] == FlowResultType.FORM
-        assert result["step_id"] == SOURCE_USER
-        assert result["errors"] == {"base": "cannot_connect"}
+        await hass.async_block_till_done()
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+    assert result["errors"] == {"base": "cannot_connect"}
 
     # Test flow with wrong creds, fail with invalid_auth
     with requests_mock.Mocker() as mock:
@@ -81,21 +80,19 @@ async def test_flow_user(hass: HomeAssistant, mock_api) -> None:
             f"{CONFIG_INVALID_AUTH[CONF_URL]}/api/v2/auth/login",
             text="Wrong username/password",
         )
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN,
-            context={CONF_SOURCE: SOURCE_USER},
-            data=CONFIG_INVALID_AUTH,
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], CONFIG_INVALID_AUTH
         )
-        assert result["type"] == FlowResultType.FORM
-        assert result["step_id"] == SOURCE_USER
-        assert result["errors"] == {"base": "invalid_auth"}
+        await hass.async_block_till_done()
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+    assert result["errors"] == {"base": "invalid_auth"}
 
     # Test flow with proper input, succeed
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={CONF_SOURCE: SOURCE_USER},
-        data=CONFIG_VALID,
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], CONFIG_VALID
     )
+    await hass.async_block_till_done()
     assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["data"] == CONFIG_VALID
 
@@ -105,8 +102,16 @@ async def test_flow_user_already_configured(hass: HomeAssistant) -> None:
     entry = MockConfigEntry(domain=DOMAIN, data=CONFIG_VALID)
     entry.add_to_hass(hass)
 
+    # Open flow as USER with no input
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={CONF_SOURCE: SOURCE_USER}, data=CONFIG_VALID
+        DOMAIN, context={CONF_SOURCE: SOURCE_USER}
+    )
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+
+    # Test flow with duplicate config
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], CONFIG_VALID
     )
     assert result["type"] == FlowResultType.ABORT
     assert result["reason"] == "already_configured"
@@ -119,7 +124,6 @@ async def test_flow_import(hass: HomeAssistant) -> None:
         context={CONF_SOURCE: SOURCE_IMPORT},
         data=CONFIG_IMPORT_VALID,
     )
-
     assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["data"] == CONFIG_VALID
 
