1066ffc571a647d868a0d0aaf07b7a8c1785a005
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2023-03-27 13:50:15 +0000

    explicit result assertion & minor tweaks in tests
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- tests/components/qbittorrent/test_config_flow.py
+++ tests/components/qbittorrent/test_config_flow.py
@@ -19,28 +19,14 @@ from tests.common import MockConfigEntry
 
 pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 
-CONFIG_VALID = {
+USER_INPUT = {
     CONF_URL: "http://localhost:8080",
     CONF_USERNAME: "user",
     CONF_PASSWORD: "pass",
     CONF_VERIFY_SSL: True,
 }
 
-CONFIG_INVALID_AUTH = {
-    CONF_URL: "http://localhost:8080",
-    CONF_USERNAME: "null",
-    CONF_PASSWORD: "none",
-    CONF_VERIFY_SSL: True,
-}
-
-CONFIG_CANNOT_CONNECT = {
-    CONF_URL: "http://nowhere:23456",
-    CONF_USERNAME: "user",
-    CONF_PASSWORD: "pass",
-    CONF_VERIFY_SSL: True,
-}
-
-CONFIG_IMPORT_VALID = {
+YAML_IMPORT = {
     CONF_URL: "http://localhost:8080",
     CONF_USERNAME: "user",
     CONF_PASSWORD: "pass",
@@ -59,11 +45,11 @@ async def test_flow_user(hass: HomeAssistant, mock_api) -> None:
     # Test flow with connection failure, fail with cannot_connect
     with requests_mock.Mocker() as mock:
         mock.get(
-            f"{CONFIG_CANNOT_CONNECT[CONF_URL]}/api/v2/app/preferences",
+            f"{USER_INPUT[CONF_URL]}/api/v2/app/preferences",
             exc=RequestException,
         )
         result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], CONFIG_CANNOT_CONNECT
+            result["flow_id"], USER_INPUT
         )
         await hass.async_block_till_done()
     assert result["type"] == FlowResultType.FORM
@@ -72,16 +58,14 @@ async def test_flow_user(hass: HomeAssistant, mock_api) -> None:
 
     # Test flow with wrong creds, fail with invalid_auth
     with requests_mock.Mocker() as mock:
-        mock.get(f"{CONFIG_INVALID_AUTH[CONF_URL]}/api/v2/transfer/speedLimitsMode")
-        mock.get(
-            f"{CONFIG_INVALID_AUTH[CONF_URL]}/api/v2/app/preferences", status_code=403
-        )
+        mock.get(f"{USER_INPUT[CONF_URL]}/api/v2/transfer/speedLimitsMode")
+        mock.get(f"{USER_INPUT[CONF_URL]}/api/v2/app/preferences", status_code=403)
         mock.post(
-            f"{CONFIG_INVALID_AUTH[CONF_URL]}/api/v2/auth/login",
+            f"{USER_INPUT[CONF_URL]}/api/v2/auth/login",
             text="Wrong username/password",
         )
         result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], CONFIG_INVALID_AUTH
+            result["flow_id"], USER_INPUT
         )
         await hass.async_block_till_done()
     assert result["type"] == FlowResultType.FORM
@@ -90,16 +74,21 @@ async def test_flow_user(hass: HomeAssistant, mock_api) -> None:
 
     # Test flow with proper input, succeed
     result = await hass.config_entries.flow.async_configure(
-        result["flow_id"], CONFIG_VALID
+        result["flow_id"], USER_INPUT
     )
     await hass.async_block_till_done()
     assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["data"] == CONFIG_VALID
+    assert result["data"] == {
+        CONF_URL: "http://localhost:8080",
+        CONF_USERNAME: "user",
+        CONF_PASSWORD: "pass",
+        CONF_VERIFY_SSL: True,
+    }
 
 
 async def test_flow_user_already_configured(hass: HomeAssistant) -> None:
     """Test user initialized flow with duplicate server."""
-    entry = MockConfigEntry(domain=DOMAIN, data=CONFIG_VALID)
+    entry = MockConfigEntry(domain=DOMAIN, data=USER_INPUT)
     entry.add_to_hass(hass)
 
     # Open flow as USER with no input
@@ -111,7 +100,7 @@ async def test_flow_user_already_configured(hass: HomeAssistant) -> None:
 
     # Test flow with duplicate config
     result = await hass.config_entries.flow.async_configure(
-        result["flow_id"], CONFIG_VALID
+        result["flow_id"], USER_INPUT
     )
     assert result["type"] == FlowResultType.ABORT
     assert result["reason"] == "already_configured"
@@ -122,21 +111,26 @@ async def test_flow_import(hass: HomeAssistant) -> None:
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={CONF_SOURCE: SOURCE_IMPORT},
-        data=CONFIG_IMPORT_VALID,
+        data=YAML_IMPORT,
     )
     assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["data"] == CONFIG_VALID
+    assert result["data"] == {
+        CONF_URL: "http://localhost:8080",
+        CONF_USERNAME: "user",
+        CONF_PASSWORD: "pass",
+        CONF_VERIFY_SSL: True,
+    }
 
 
 async def test_flow_import_already_configured(hass: HomeAssistant) -> None:
     """Test import step already configured."""
-    entry = MockConfigEntry(domain=DOMAIN, data=CONFIG_VALID)
+    entry = MockConfigEntry(domain=DOMAIN, data=USER_INPUT)
     entry.add_to_hass(hass)
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={CONF_SOURCE: SOURCE_IMPORT},
-        data=CONFIG_IMPORT_VALID,
+        data=YAML_IMPORT,
     )
     assert result["type"] == FlowResultType.ABORT
     assert result["reason"] == "already_configured"
