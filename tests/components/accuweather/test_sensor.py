da21c4b54736a320f15493e168c419e3cb2c3c71
Maciej Bieniek <bieniu@users.noreply.github.com>
2023-03-28 16:29:49 +0200

    Use metric units internally

--- tests/components/accuweather/test_sensor.py
+++ tests/components/accuweather/test_sensor.py
@@ -741,10 +741,28 @@ async def test_sensor_imperial_units(hass: HomeAssistant) -> None:
 
     state = hass.states.get("sensor.home_cloud_ceiling")
     assert state
-    assert state.state == "10500.0"
+    assert state.state == "10498.687664042"
     assert state.attributes.get(ATTR_ATTRIBUTION) == ATTRIBUTION
     assert state.attributes.get(ATTR_ICON) == "mdi:weather-fog"
     assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UnitOfLength.FEET
+
+    state = hass.states.get("sensor.home_wind")
+    assert state
+    assert state.state == "14.5"
+    assert state.attributes.get(ATTR_ATTRIBUTION) == ATTRIBUTION
+    assert state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UnitOfSpeed.MILES_PER_HOUR
+    assert state.attributes.get(ATTR_STATE_CLASS) == SensorStateClass.MEASUREMENT
+    assert state.attributes.get(ATTR_DEVICE_CLASS) == SensorDeviceClass.WIND_SPEED
+
+    state = hass.states.get("sensor.home_realfeel_temperature")
+    assert state
+    assert state.state == "77.2"
+    assert state.attributes.get(ATTR_ATTRIBUTION) == ATTRIBUTION
+    assert (
+        state.attributes.get(ATTR_UNIT_OF_MEASUREMENT) == UnitOfTemperature.FAHRENHEIT
+    )
+    assert state.attributes.get(ATTR_DEVICE_CLASS) == SensorDeviceClass.TEMPERATURE
+    assert state.attributes.get(ATTR_STATE_CLASS) == SensorStateClass.MEASUREMENT
 
 
 async def test_state_update(hass: HomeAssistant) -> None:
