ee2101ef38870149ce1b61e46596259c847fdd35
MattWestb <49618193+MattWestb@users.noreply.github.com>
2023-03-29 01:33:06 +0200

    Add binding of IKEA Matter Switch cluster in ZHA (#89623)
    * Adding binding of IKEA Matter Switch cluster
    
    IKEA Symfonisk Gen 2 is using Matter ZCL Switch command but on manufacture cluster then its not supported in ZVL R8 that need being bond for sending the commands to the coordinator.
    
    * Update manufacturerspecific.py
    
    * Update manufacturerspecific.py
    
    Delete not needed function `@registries.BINDABLE_CLUSTERS.register(0xFC80)`

--- homeassistant/components/zha/core/channels/manufacturerspecific.py
+++ homeassistant/components/zha/core/channels/manufacturerspecific.py
@@ -324,3 +324,11 @@ class IkeaAirPurifierChannel(ZigbeeChannel):
             self.async_send_signal(
                 f"{self.unique_id}_{SIGNAL_ATTR_UPDATED}", attrid, attr_name, value
             )
+
+
+@registries.CHANNEL_ONLY_CLUSTERS.register(0xFC80)
+@registries.ZIGBEE_CHANNEL_REGISTRY.register(0xFC80)
+class IkeaRemote(ZigbeeChannel):
+    """Ikea Matter remote channel."""
+
+    REPORT_CONFIG = ()
