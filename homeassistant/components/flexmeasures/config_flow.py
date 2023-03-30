39101d9662baa67c50783525a68df77dedc55e71
F.N. Claessen <felix@seita.nl>
2023-03-30 13:56:46 +0200

    Support a single FlexMeasures configuration only
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- homeassistant/components/flexmeasures/config_flow.py
+++ homeassistant/components/flexmeasures/config_flow.py
@@ -15,7 +15,6 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-# TODO adjust the data schema to the data that you need
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required("host"): str,
@@ -64,11 +63,11 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
     # InvalidAuth
 
     # Return info that you want to store in the config entry.
-    return {"title": "Name of the device"}
+    return {"title": "FlexMeasures"}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for FlexMeasures."""
+    """Config flow for FlexMeasures integration."""
 
     VERSION = 1
 
@@ -76,6 +75,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
+
+        # Support a single FlexMeasures configuration only
+        await self.async_set_unique_id(DOMAIN)
+        self._abort_if_unique_id_configured()
+
         if user_input is None:
             return self.async_show_form(
                 step_id="user", data_schema=STEP_USER_DATA_SCHEMA
@@ -95,6 +99,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         else:
             return self.async_create_entry(title=info["title"], data=user_input)
 
+        # Show form again, showing captured errors
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
