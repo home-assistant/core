da9b4edd398ac1d435c820ff38cf20c6532d27f6
Paulus Schoutsen <balloob@gmail.com>
2023-03-31 15:13:13 -0400

    Allow multiple instances for OpenAI

--- homeassistant/components/openai_conversation/config_flow.py
+++ homeassistant/components/openai_conversation/config_flow.py
@@ -72,9 +72,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
-
         if user_input is None:
             return self.async_show_form(
                 step_id="user", data_schema=STEP_USER_DATA_SCHEMA
