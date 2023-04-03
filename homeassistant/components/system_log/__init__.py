68eb6e8f043bac93540811c5f0b957938d0d03d3
J. Nick Koston <nick@koston.org>
2023-04-02 15:05:08 -1000

    log the original exception (in this case they are the same)

--- homeassistant/components/system_log/__init__.py
+++ homeassistant/components/system_log/__init__.py
@@ -106,10 +106,10 @@ def _safe_get_message(record: logging.LogRecord) -> str:
     """
     try:
         return record.getMessage()
-    except Exception:  # pylint: disable=broad-except
+    except Exception as ex:  # pylint: disable=broad-except
         try:
             return f"Bad logger message: {record.msg} ({record.args})"
-        except Exception as ex:  # pylint: disable=broad-except
+        except Exception:  # pylint: disable=broad-except
             return f"Bad logger message: {ex}"
 
 
