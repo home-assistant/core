dede3c4abeb6a12d1501e172304cd5adb4f55f02
Kevin McCormack <harlemsquirrel@mccormack.tech>
2023-04-03 18:31:26 +0000

    Remove pjlink zeroconf

--- homeassistant/components/pjlink/config_flow.py
+++ homeassistant/components/pjlink/config_flow.py
@@ -7,7 +7,6 @@ from typing import Any
 import voluptuous as vol
 
 from homeassistant import config_entries, data_entry_flow
-from homeassistant.components import zeroconf
 from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PASSWORD, CONF_PORT
 from homeassistant.data_entry_flow import FlowResult
 import homeassistant.helpers.config_validation as cv
@@ -56,55 +55,4 @@ class PJLinkConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_create_entry(
             title=TITLE,
             data=user_input,
-        )
-
-    # avahi/zeroconf
-    # looks like there are these services that should be discoverable according
-    # to https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml?search=pjlink
-    #   _pjlink._tcp
-    #   _pjlink._udp
-    async def async_step_zeroconf(
-        self, discovery_info: zeroconf.ZeroconfServiceInfo
-    ) -> FlowResult:
-        """Handle zeroconf discovery."""
-        mac_addr = format_mac(discovery_info.properties["macaddress"])
-        self.host = discovery_info.host
-        self.port = discovery_info.port
-        await self.async_set_unique_id(
-            mac_addr
-        )  # How can we deal with this outside zeroconf?
-        self._abort_if_unique_id_configured()
-        return await self.async_step_zeroconf_confirm()
-
-    async def async_step_zeroconf_confirm(
-        self, user_input: dict | None = None
-    ) -> FlowResult:
-        """Confirm the setup."""
-
-        if user_input is None:
-            assert self.host
-            assert self.port
-            return self.async_show_form(
-                step_id="zeroconf_confirm",
-                description_placeholders={
-                    CONF_NAME: self.unique_id,
-                    CONF_HOST: self.host,
-                    CONF_PORT: str(self.port),
-                    CONF_ENCODING: DEFAULT_ENCODING,
-                    CONF_PASSWORD: None,
-                },
-            )
-
-        host = user_input[CONF_HOST]
-        port = user_input[CONF_PORT]
-        name = user_input[CONF_NAME]
-        password = user_input[CONF_PASSWORD]
-        return self.async_create_entry(
-            title=TITLE,
-            data={
-                CONF_HOST: host,
-                CONF_PORT: port,
-                CONF_NAME: name,
-                CONF_PASSWORD: password,
-            },
         )
