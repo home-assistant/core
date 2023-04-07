17bd3e5fe54b829a5734293276ccb23c93634bff
epenet <6771947+epenet@users.noreply.github.com>
2023-04-07 14:27:37 +0000

    Revert "asyncio.gather"
    This reverts commit 6077902ccd5b79f9e4419f4312ace4cbb3db3730.

--- tests/conftest.py
+++ tests/conftest.py
@@ -523,12 +523,8 @@ def hass(
 
     yield hass
 
-    loop.run_until_complete(
-        asyncio.gather(
-            config_entry.async_unload(hass)
-            for config_entry in hass.config_entries.async_entries()
-        )
-    )
+    for config_entry in hass.config_entries.async_entries():
+        loop.run_until_complete(config_entry.async_unload(hass))
 
     loop.run_until_complete(hass.async_stop(force=True))
 
