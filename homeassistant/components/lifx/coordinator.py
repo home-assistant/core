f3e38424781b8037a2b4927e646c6b02607daa3d
J. Nick Koston <nick@koston.org>
2023-04-10 00:38:52 -1000

    Update homeassistant/components/lifx/coordinator.py

--- homeassistant/components/lifx/coordinator.py
+++ homeassistant/components/lifx/coordinator.py
@@ -258,8 +258,7 @@ class LIFXUpdateCoordinator(DataUpdateCoordinator[None]):
             self.active_effect = FirmwareEffect[self.device.effect.get("effect", "OFF")]
         elif is_legacy_multizone and num_zones != len(device.color_zones):
             # The number of zones has changed so we need
-            # to update the zones again. This should only
-            # happen once after a device is added.
+            # to update the zones again. This happen rarely.
             await self.async_get_color_zones()
 
     async def async_get_color_zones(self) -> None:
