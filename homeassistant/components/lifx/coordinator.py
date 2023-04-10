ac5d39bc1cc8b58658f9d21050ba64caf9b5c95c
J. Nick Koston <nick@koston.org>
2023-04-10 00:36:47 -1000

    Update homeassistant/components/lifx/coordinator.py

--- homeassistant/components/lifx/coordinator.py
+++ homeassistant/components/lifx/coordinator.py
@@ -238,7 +238,7 @@ class LIFXUpdateCoordinator(DataUpdateCoordinator[None]):
         elif is_legacy_multizone:
             methods.append(self.device.get_extended_color_zones)
             methods.extend(self._async_build_color_zones_update_requests())
-            methods.extend(self.device.get_extended_color_zones)
+            methods.extend(self.device.async_get_multizone_effect)
         if features["hev"]:
             methods.append(self.device.get_hev_cycle)
         if features["infrared"]:
