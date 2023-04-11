aa68d1d61752927e284efd545880b9b2d19f56f5
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-04-11 17:41:38 +0200

    Cleanup mqtt CONFIG_SCHEMA_ENTRY (#90791)

--- homeassistant/components/mqtt/config_integration.py
+++ homeassistant/components/mqtt/config_integration.py
@@ -45,23 +45,8 @@ from .const import (
     CONF_DISCOVERY_PREFIX,
     CONF_KEEPALIVE,
     CONF_TLS_INSECURE,
-    CONF_TRANSPORT,
     CONF_WILL_MESSAGE,
-    CONF_WS_HEADERS,
-    CONF_WS_PATH,
-    DEFAULT_BIRTH,
-    DEFAULT_DISCOVERY,
-    DEFAULT_KEEPALIVE,
-    DEFAULT_PORT,
-    DEFAULT_PREFIX,
-    DEFAULT_PROTOCOL,
-    DEFAULT_TRANSPORT,
-    DEFAULT_WILL,
-    SUPPORTED_PROTOCOLS,
-    TRANSPORT_TCP,
-    TRANSPORT_WEBSOCKETS,
 )
-from .util import valid_birth_will, valid_publish_topic
 
 DEFAULT_TLS_PROTOCOL = "auto"
 
@@ -153,41 +138,6 @@ PLATFORM_CONFIG_SCHEMA_BASE = vol.Schema(
 
 CLIENT_KEY_AUTH_MSG = (
     "client_key and client_cert must both be present in the MQTT broker configuration"
-)
-
-CONFIG_SCHEMA_ENTRY = vol.Schema(
-    {
-        vol.Optional(CONF_CLIENT_ID): cv.string,
-        vol.Optional(CONF_KEEPALIVE, default=DEFAULT_KEEPALIVE): vol.All(
-            vol.Coerce(int), vol.Range(min=15)
-        ),
-        vol.Required(CONF_BROKER): cv.string,
-        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-        vol.Optional(CONF_USERNAME): cv.string,
-        vol.Optional(CONF_PASSWORD): cv.string,
-        vol.Optional(CONF_CERTIFICATE): str,
-        vol.Inclusive(CONF_CLIENT_KEY, "client_key_auth", msg=CLIENT_KEY_AUTH_MSG): str,
-        vol.Inclusive(
-            CONF_CLIENT_CERT, "client_key_auth", msg=CLIENT_KEY_AUTH_MSG
-        ): str,
-        vol.Optional(CONF_TLS_INSECURE): cv.boolean,
-        vol.Optional(CONF_PROTOCOL, default=DEFAULT_PROTOCOL): vol.All(
-            cv.string, vol.In(SUPPORTED_PROTOCOLS)
-        ),
-        vol.Optional(CONF_WILL_MESSAGE, default=DEFAULT_WILL): valid_birth_will,
-        vol.Optional(CONF_BIRTH_MESSAGE, default=DEFAULT_BIRTH): valid_birth_will,
-        vol.Optional(CONF_DISCOVERY, default=DEFAULT_DISCOVERY): cv.boolean,
-        # discovery_prefix must be a valid publish topic because if no
-        # state topic is specified, it will be created with the given prefix.
-        vol.Optional(
-            CONF_DISCOVERY_PREFIX, default=DEFAULT_PREFIX
-        ): valid_publish_topic,
-        vol.Optional(CONF_TRANSPORT, default=DEFAULT_TRANSPORT): vol.All(
-            cv.string, vol.In([TRANSPORT_TCP, TRANSPORT_WEBSOCKETS])
-        ),
-        vol.Optional(CONF_WS_PATH, default="/"): cv.string,
-        vol.Optional(CONF_WS_HEADERS, default={}): {cv.string: cv.string},
-    }
 )
 
 DEPRECATED_CONFIG_KEYS = [
