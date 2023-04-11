30839cdd5fee4a7fea85ba34bb1eaebb312e8f8e
Martin Hjelmare <marhje52@gmail.com>
2023-04-11 08:42:31 +0200

    Add stt entity

--- homeassistant/components/stt/__init__.py
+++ homeassistant/components/stt/__init__.py
@@ -1,9 +1,12 @@
 """Provide functionality to STT."""
 from __future__ import annotations
 
+from abc import abstractmethod
 import asyncio
+from collections.abc import AsyncIterable
 from dataclasses import asdict
-from typing import Any
+import logging
+from typing import Any, final
 
 from aiohttp import web
 from aiohttp.hdrs import istr
@@ -14,10 +17,20 @@ from aiohttp.web_exceptions import (
 )
 
 from homeassistant.components.http import HomeAssistantView
-from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    EVENT_HOMEASSISTANT_STOP,
+    STATE_UNAVAILABLE,
+    STATE_UNKNOWN,
+)
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_component import EntityComponent
+from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.util import dt as dt_util
 
 from .const import (
+    DATA_PROVIDERS,
     DOMAIN,
     AudioBitRates,
     AudioChannels,
@@ -36,6 +49,7 @@ from .legacy import (
 
 __all__ = [
     "async_get_provider",
+    "async_get_provider_entity",
     "AudioBitRates",
     "AudioChannels",
     "AudioCodecs",
@@ -43,26 +57,163 @@ __all__ = [
     "AudioSampleRates",
     "DOMAIN",
     "Provider",
+    "ProviderEntity",
     "SpeechMetadata",
     "SpeechResult",
     "SpeechResultState",
 ]
 
+_LOGGER = logging.getLogger(__name__)
+
+
+@callback
+def async_get_provider_entity(
+    hass: HomeAssistant, entity_id: str
+) -> ProviderEntity | None:
+    """Return provider entity."""
+    component: EntityComponent[ProviderEntity] = hass.data[DOMAIN]
+
+    return component.get_entity(entity_id)
+
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up STT."""
+    component = hass.data[DOMAIN] = EntityComponent[ProviderEntity](
+        _LOGGER, DOMAIN, hass
+    )
+
+    # Cancel the retry setup timer on shutdown (temporary workaround)
+    hass.bus.async_listen_once(
+        EVENT_HOMEASSISTANT_STOP,
+        component._async_shutdown,  # pylint: disable=protected-access
+    )
+
     platform_setups = async_setup_legacy(hass, config)
 
     if platform_setups:
         await asyncio.wait([asyncio.create_task(setup) for setup in platform_setups])
 
-    hass.http.register_view(SpeechToTextView(hass.data[DOMAIN]))
+    hass.http.register_view(SpeechToTextView(hass.data[DATA_PROVIDERS]))
     return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up a config entry."""
+    component: EntityComponent[ProviderEntity] = hass.data[DOMAIN]
+    return await component.async_setup_entry(entry)
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    component: EntityComponent[ProviderEntity] = hass.data[DOMAIN]
+    return await component.async_unload_entry(entry)
+
+
+class ProviderEntity(RestoreEntity):
+    """Represent a single STT provider."""
+
+    _attr_should_poll = False
+    __last_processed: str | None = None
+
+    @property
+    @final
+    def name(self) -> str:
+        """Return the name of the provider entity."""
+        # Only one entity is allowed per platform for now.
+        if self.platform is None:
+            raise RuntimeError("Entity is not added to hass yet.")
+
+        return self.platform.platform_name
+
+    @property
+    @final
+    def state(self) -> str | None:
+        """Return the state of the provider entity."""
+        if self.__last_processed is None:
+            return None
+        return self.__last_processed
+
+    @property
+    @abstractmethod
+    def supported_languages(self) -> list[str]:
+        """Return a list of supported languages."""
+
+    @property
+    @abstractmethod
+    def supported_formats(self) -> list[AudioFormats]:
+        """Return a list of supported formats."""
+
+    @property
+    @abstractmethod
+    def supported_codecs(self) -> list[AudioCodecs]:
+        """Return a list of supported codecs."""
+
+    @property
+    @abstractmethod
+    def supported_bit_rates(self) -> list[AudioBitRates]:
+        """Return a list of supported bit rates."""
+
+    @property
+    @abstractmethod
+    def supported_sample_rates(self) -> list[AudioSampleRates]:
+        """Return a list of supported sample rates."""
+
+    @property
+    @abstractmethod
+    def supported_channels(self) -> list[AudioChannels]:
+        """Return a list of supported channels."""
+
+    async def async_internal_added_to_hass(self) -> None:
+        """Call when the provider entity is added to hass."""
+        await super().async_internal_added_to_hass()
+        state = await self.async_get_last_state()
+        if (
+            state is not None
+            and state.state is not None
+            and state.state not in (STATE_UNAVAILABLE, STATE_UNKNOWN)
+        ):
+            self.__last_processed = state.state
+
+    @final
+    async def internal_async_process_audio_stream(
+        self, metadata: SpeechMetadata, stream: AsyncIterable[bytes]
+    ) -> SpeechResult:
+        """Process an audio stream to STT service.
+
+        Only streaming content is allowed!
+        """
+        self.__last_processed = dt_util.utcnow().isoformat()
+        self.async_write_ha_state()
+        return await self.async_process_audio_stream(metadata=metadata, stream=stream)
+
+    @abstractmethod
+    async def async_process_audio_stream(
+        self, metadata: SpeechMetadata, stream: AsyncIterable[bytes]
+    ) -> SpeechResult:
+        """Process an audio stream to STT service.
+
+        Only streaming content is allowed!
+        """
+
+    @callback
+    def check_metadata(self, metadata: SpeechMetadata) -> bool:
+        """Check if given metadata supported by this provider."""
+        if (
+            metadata.language not in self.supported_languages
+            or metadata.format not in self.supported_formats
+            or metadata.codec not in self.supported_codecs
+            or metadata.bit_rate not in self.supported_bit_rates
+            or metadata.sample_rate not in self.supported_sample_rates
+            or metadata.channel not in self.supported_channels
+        ):
+            return False
+        return True
 
 
 class SpeechToTextView(HomeAssistantView):
     """STT view to generate a text from audio stream."""
 
+    _legacy_provider_reported = False
     requires_auth = True
     url = "/api/stt/{provider}"
     name = "api:stt:provider"
@@ -73,9 +224,13 @@ class SpeechToTextView(HomeAssistantView):
 
     async def post(self, request: web.Request, provider: str) -> web.Response:
         """Convert Speech (audio) to text."""
-        if provider not in self.providers:
+        hass: HomeAssistant = request.app["hass"]
+        provider_entity: ProviderEntity | None = None
+        if (
+            not (provider_entity := async_get_provider_entity(hass, f"stt.{provider}"))
+            and provider not in self.providers
+        ):
             raise HTTPNotFound()
-        stt_provider: Provider = self.providers[provider]
 
         # Get metadata
         try:
@@ -83,17 +238,56 @@ class SpeechToTextView(HomeAssistantView):
         except ValueError as err:
             raise HTTPBadRequest(text=str(err)) from err
 
-        # Check format
-        if not stt_provider.check_metadata(metadata):
-            raise HTTPUnsupportedMediaType()
+        if not provider_entity:
+            stt_provider: Provider | ProviderEntity = self.providers[provider]
 
-        # Process audio stream
-        result = await stt_provider.async_process_audio_stream(
-            metadata, request.content
-        )
+            if not self._legacy_provider_reported:
+                self._legacy_provider_reported = True
+                report_issue = self._suggest_report_issue(provider, stt_provider)
+                # This should raise in Home Assistant Core 2022.5
+                _LOGGER.warning(
+                    "Provider %s (%s) is using a legacy implementation, "
+                    "and should be updated to use the ProviderEntity. Please "
+                    "%s",
+                    provider,
+                    type(stt_provider),
+                    report_issue,
+                )
+
+            # Check format
+            if not stt_provider.check_metadata(metadata):
+                raise HTTPUnsupportedMediaType()
+
+            # Process audio stream
+            result = await stt_provider.async_process_audio_stream(
+                metadata, request.content
+            )
+        else:
+            # Check format
+            if not provider_entity.check_metadata(metadata):
+                raise HTTPUnsupportedMediaType()
+
+            # Process audio stream
+            result = await provider_entity.internal_async_process_audio_stream(
+                metadata, request.content
+            )
 
         # Return result
         return self.json(asdict(result))
+
+    def _suggest_report_issue(self, provider: str, provider_instance: object) -> str:
+        """Suggest to report an issue."""
+        report_issue = ""
+        if "custom_components" in type(provider_instance).__module__:
+            report_issue = "report it to the custom integration author."
+        else:
+            report_issue = (
+                "create a bug report at "
+                "https://github.com/home-assistant/core/issues?q=is%3Aopen+is%3Aissue"
+            )
+            report_issue += f"+label%3A%22integration%3A+{provider}%22"
+
+        return report_issue
 
     async def get(self, request: web.Request, provider: str) -> web.Response:
         """Return provider specific audio information."""
