a21a48a13dac5849bdb5eaad9ccdf215f927fef6
dougiteixeira <31328123+dougiteixeira@users.noreply.github.com>
2023-04-11 20:30:35 -0300

    Update homeassistant/components/sql/config_flow.py
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- homeassistant/components/sql/config_flow.py
+++ homeassistant/components/sql/config_flow.py
@@ -189,8 +189,6 @@ class SQLOptionsFlowHandler(config_entries.OptionsFlow):
                     },
                 )
 
-            current_data = user_input
-
         return self.async_show_form(
             step_id="init",
             data_schema=self.add_suggested_values_to_schema(
