b18f6f1d5c3ba2f78858e2ff87cdfc2f17edd7e6
epenet <6771947+epenet@users.noreply.github.com>
2023-04-11 10:00:17 +0200

    Remove incorrect constant usage in test (#91198)

--- tests/components/apcupsd/test_config_flow.py
+++ tests/components/apcupsd/test_config_flow.py
@@ -117,7 +117,7 @@ async def test_flow_works(hass: HomeAssistant) -> None:
             context={CONF_SOURCE: SOURCE_USER},
         )
         assert result["type"] == FlowResultType.FORM
-        assert result["step_id"] == SOURCE_USER
+        assert result["step_id"] == "user"
 
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"], user_input=CONF_DATA
