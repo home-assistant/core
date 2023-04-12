b601c1db42339f5a7d1f808246a94ab337106c52
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2023-04-12 14:50:37 +0200

    Remove redundant code

--- homeassistant/components/broadlink/climate.py
+++ homeassistant/components/broadlink/climate.py
@@ -1,7 +1,5 @@
 """Support for Broadlink climate devices."""
 from homeassistant.components.climate import (
-    ATTR_CURRENT_TEMPERATURE,
-    ATTR_HVAC_ACTION,
     ClimateEntity,
     ClimateEntityFeature,
     HVACAction,
@@ -36,8 +34,6 @@ class BroadlinkThermostat(ClimateEntity, BroadlinkEntity, RestoreEntity):
     def __init__(self, device):
         """Initialize the climate entity."""
         super().__init__(device)
-        self._device = device
-        self._coordinator = device.update_manager.coordinator
         self._attr_hvac_action = None
         self._attr_hvac_mode = None
         self._attr_current_temperature = None
@@ -47,58 +43,42 @@ class BroadlinkThermostat(ClimateEntity, BroadlinkEntity, RestoreEntity):
     async def async_set_temperature(self, **kwargs):
         """Set new target temperature."""
         temperature = kwargs[ATTR_TEMPERATURE]
-        device = self._device
+        await self._device.async_request(self._device.api.set_temp, temperature)
         self._attr_target_temperature = temperature
         self.async_write_ha_state()
-        await device.async_request(device.api.set_temp, temperature)
 
     @callback
-    def update_data(self):
+    def _update_state(self, data):
         """Update data."""
-        if self._coordinator.last_update_success:
-            state = self._coordinator.data
-            if state["power"]:
-                if state["auto_mode"]:
-                    self._attr_hvac_mode = HVACMode.AUTO
-                else:
-                    self._attr_hvac_mode = HVACMode.HEAT
-
-                if state["active"]:
-                    self._attr_hvac_action = HVACAction.HEATING
-                else:
-                    self._attr_hvac_action = HVACAction.IDLE
+        if data["power"]:
+            if data["auto_mode"]:
+                self._attr_hvac_mode = HVACMode.AUTO
             else:
-                self._attr_hvac_mode = HVACMode.OFF
-                self._attr_hvac_action = HVACAction.OFF
+                self._attr_hvac_mode = HVACMode.HEAT
 
-            self._attr_current_temperature = state["room_temp"]
-            self._attr_target_temperature = state["thermostat_temp"]
+            if data["active"]:
+                self._attr_hvac_action = HVACAction.HEATING
+            else:
+                self._attr_hvac_action = HVACAction.IDLE
+        else:
+            self._attr_hvac_mode = HVACMode.OFF
+            self._attr_hvac_action = HVACAction.OFF
 
-        self.async_write_ha_state()
-
-    async def async_added_to_hass(self):
-        """Call when the climate device is added to hass."""
-        state = await self.async_get_last_state()
-        if state is not None:
-            self._attr_hvac_mode = state.state
-            self._attr_hvac_action = state.attributes[ATTR_HVAC_ACTION]
-            self._attr_current_temperature = state.attributes[ATTR_CURRENT_TEMPERATURE]
-            self._attr_target_temperature = state.attributes[ATTR_TEMPERATURE]
-        self.async_write_ha_state()
-        self.async_on_remove(self._coordinator.async_add_listener(self.update_data))
+        self._attr_current_temperature = data["room_temp"]
+        self._attr_target_temperature = data["thermostat_temp"]
 
     async def async_set_hvac_mode(self, hvac_mode):
         """Set new target hvac mode."""
-        device = self._device
-        self._attr_hvac_mode = hvac_mode
-        self.async_write_ha_state()
         if hvac_mode == HVACMode.OFF:
-            await device.async_request(device.api.set_power, 0)
+            await self._device.async_request(self._device.api.set_power, 0)
 
         elif hvac_mode == HVACMode.AUTO:
-            await device.async_request(device.api.set_power, 1)
-            await device.async_request(device.api.set_mode, 1, 0)
+            await self._device.async_request(self._device.api.set_power, 1)
+            await self._device.async_request(self._device.api.set_mode, 1, 0)
 
         elif hvac_mode == HVACMode.HEAT:
-            await device.async_request(device.api.set_power, 1)
-            await device.async_request(device.api.set_mode, 0, 0)
+            await self._device.async_request(self._device.api.set_power, 1)
+            await self._device.async_request(self._device.api.set_mode, 0, 0)
+
+        self._attr_hvac_mode = hvac_mode
+        self.async_write_ha_state()
