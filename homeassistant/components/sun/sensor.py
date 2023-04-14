26f5c1775674405a209e65c8172e57cb54515a0a
Robert Lippmann <rlippmann@hotmail.com>
2023-04-14 02:44:06 -0400

    remove slots from sun mixing

--- homeassistant/components/sun/sensor.py
+++ homeassistant/components/sun/sensor.py
@@ -26,7 +26,7 @@ from .const import DOMAIN
 ENTITY_ID_SENSOR_FORMAT = SENSOR_DOMAIN + ".sun_{}"
 
 
-@dataclass(slots=True)
+@dataclass
 class SunEntityDescriptionMixin:
     """Mixin for required Sun base description keys."""
 
