85acb04df0ea5781e646564ff16603f439b3d098
Timur Kh <tkhadimullin@users.noreply.github.com>
2023-04-14 10:44:08 +0000

    fixed unit tests

--- tests/components/contact_energy_nz/test_config_flow.py
+++ tests/components/contact_energy_nz/test_config_flow.py
@@ -1,14 +1,15 @@
 """Test the Contact Energy NZ config flow."""
 from unittest.mock import AsyncMock, patch
 
+from contact_energy_nz import ContactEnergyApi
 import pytest
 
 from homeassistant import config_entries
-from homeassistant.components.contact_energy_nz.config_flow import (
+from homeassistant.components.contact_energy_nz.const import DOMAIN
+from homeassistant.components.contact_energy_nz.exceptions import (
     CannotConnect,
     InvalidAuth,
 )
-from homeassistant.components.contact_energy_nz.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
@@ -21,26 +22,31 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     assert result["type"] == FlowResultType.FORM
-    assert result["errors"] is None
+    assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.contact_energy_nz.config_flow.PlaceholderHub.authenticate",
+        "homeassistant.components.contact_energy_nz.config_flow.ContactEnergyApi.from_credentials",
+        return_value=ContactEnergyApi(),
+    ), patch(
+        "homeassistant.components.contact_energy_nz.async_setup_entry",
         return_value=True,
-    ):
+    ) as mock_setup_entry:
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
                 "username": "test-username",
                 "password": "test-password",
+                "api_key": "test-api-key",
             },
         )
         await hass.async_block_till_done()
 
     assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Name of the device"
+    assert result2["title"] == "Contact Energy: test-username"
     assert result2["data"] == {
         "username": "test-username",
         "password": "test-password",
+        "api_key": "test-api-key",
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -52,7 +58,7 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
     )
 
     with patch(
-        "homeassistant.components.contact_energy_nz.config_flow.PlaceholderHub.authenticate",
+        "homeassistant.components.contact_energy_nz.config_flow.ContactEnergyApi.from_credentials",
         side_effect=InvalidAuth,
     ):
         result2 = await hass.config_entries.flow.async_configure(
@@ -74,7 +80,7 @@ async def test_form_cannot_connect(hass: HomeAssistant) -> None:
     )
 
     with patch(
-        "homeassistant.components.contact_energy_nz.config_flow.PlaceholderHub.authenticate",
+        "homeassistant.components.contact_energy_nz.config_flow.ContactEnergyApi.from_credentials",
         side_effect=CannotConnect,
     ):
         result2 = await hass.config_entries.flow.async_configure(
