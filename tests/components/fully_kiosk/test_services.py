1663d7241cd11d9e7e6feacdb423170fd03db3e5
Mike Heath <elcapo@gmail.com>
2023-04-17 22:31:06 +0000

    Refactor service invocation
    Raises `HomeAssistantError` when the user provides an device id that is
    not in the device registry or a device that is not a Fully Kiosk
    device. If the device's config entry is not loaded, a warning is
    logged.

--- tests/components/fully_kiosk/test_services.py
+++ tests/components/fully_kiosk/test_services.py
@@ -1,6 +1,8 @@
 """Test Fully Kiosk Browser services."""
 from unittest.mock import MagicMock
 
+import pytest
+
 from homeassistant.components.fully_kiosk.const import (
     ATTR_APPLICATION,
     ATTR_URL,
@@ -10,6 +12,7 @@ from homeassistant.components.fully_kiosk.const import (
 )
 from homeassistant.const import ATTR_DEVICE_ID
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr
 
 from tests.common import MockConfigEntry
@@ -28,14 +31,50 @@ async def test_services(
 
     assert device_entry
 
+    url = "https://example.com"
     await hass.services.async_call(
         DOMAIN,
         SERVICE_LOAD_URL,
-        {ATTR_DEVICE_ID: [device_entry.id], ATTR_URL: "https://example.com"},
+        {ATTR_DEVICE_ID: [device_entry.id], ATTR_URL: url},
         blocking=True,
     )
 
-    assert len(mock_fully_kiosk.loadUrl.mock_calls) == 1
+    mock_fully_kiosk.loadUrl.assert_called_once_with(url)
+
+    app = "de.ozerov.fully"
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_START_APPLICATION,
+        {ATTR_DEVICE_ID: [device_entry.id], ATTR_APPLICATION: app},
+        blocking=True,
+    )
+
+    mock_fully_kiosk.startApplication.assert_called_once_with(app)
+
+
+async def test_service_unloaded_entry(
+    hass: HomeAssistant,
+    mock_fully_kiosk: MagicMock,
+    init_integration: MockConfigEntry,
+) -> None:
+    """Test service not called when config entry unloaded."""
+    await init_integration.async_unload(hass)
+
+    device_registry = dr.async_get(hass)
+    device_entry = device_registry.async_get_device(
+        identifiers={(DOMAIN, "abcdef-123456")}
+    )
+
+    assert device_entry
+
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_LOAD_URL,
+        {ATTR_DEVICE_ID: [device_entry.id], ATTR_URL: "https://nabucasa.com"},
+        blocking=True,
+    )
+
+    mock_fully_kiosk.loadUrl.assert_not_called()
 
     await hass.services.async_call(
         DOMAIN,
@@ -43,5 +82,21 @@ async def test_services(
         {ATTR_DEVICE_ID: [device_entry.id], ATTR_APPLICATION: "de.ozerov.fully"},
         blocking=True,
     )
+    mock_fully_kiosk.startApplication.assert_not_called()
 
-    assert len(mock_fully_kiosk.startApplication.mock_calls) == 1
+
+async def test_service_bad_device_id(
+    hass: HomeAssistant,
+    mock_fully_kiosk: MagicMock,
+    init_integration: MockConfigEntry,
+) -> None:
+    """Test Fully Kiosk Browser service invocation with bad device id."""
+    with pytest.raises(HomeAssistantError) as excinfo:
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_LOAD_URL,
+            {ATTR_DEVICE_ID: ["bad-device_id"], ATTR_URL: "https://example.com"},
+            blocking=True,
+        )
+
+    assert "Device 'bad-device_id' not found in device registry" in str(excinfo)
