b627e2586d4dde44c1de64bbeab586759b80ad05
G Johansson <goran.johansson@shiftit.se>
2023-04-17 17:30:00 +0000

    Use super()

--- homeassistant/components/template/trigger_entity.py
+++ homeassistant/components/template/trigger_entity.py
@@ -181,12 +181,12 @@ class TriggerEntity(TriggerBaseEntity, CoordinatorEntity[TriggerUpdateCoordinato
         config: dict,
     ) -> None:
         """Initialize the entity."""
-        CoordinatorEntity.__init__(self, coordinator)
-        TriggerBaseEntity.__init__(self, hass, config)
+        super(CoordinatorEntity, self).__init__(coordinator)
+        super().__init__(hass, config)
 
     async def async_added_to_hass(self) -> None:
         """Handle being added to Home Assistant."""
-        await TriggerBaseEntity.async_added_to_hass(self)
+        await super().async_added_to_hass()
         await super(CoordinatorEntity, self).async_added_to_hass()
         if self.coordinator.data is not None:
             self._process_data()
