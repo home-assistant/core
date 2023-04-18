e62453d74b9c26936d64506a0fc2559e09452c24
epenet <6771947+epenet@users.noreply.github.com>
2023-04-18 21:04:26 +0000

    Adjust

--- homeassistant/components/asuswrt/router.py
+++ homeassistant/components/asuswrt/router.py
@@ -381,7 +381,7 @@ class AsusWrtRouter:
                 KEY_COORDINATOR: coordinator,
                 KEY_SENSORS: sensor_names,
             }
-            self.async_on_close(coordinator.async_shutdown)
+            self._entry.async_on_unload(coordinator.async_shutdown)
 
     async def _update_unpolled_sensors(self) -> None:
         """Request refresh for AsusWrt unpolled sensors."""
