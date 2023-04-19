9aeacd9b128b8a578666708b8ff9fa7a46f0c16d
J. Nick Koston <nick@koston.org>
2023-04-18 17:10:37 -1000

    Fix bluetooth devices not being discovered after they go unavailable and reavailable

--- homeassistant/components/bluetooth/manager.py
+++ homeassistant/components/bluetooth/manager.py
@@ -318,6 +318,7 @@ class BluetoothManager:
                     tracker.async_remove_address(address)
                     self._integration_matcher.async_clear_address(address)
                     self._async_dismiss_discoveries(address)
+                    self._integration_matcher.async_clear_address(address)
 
                 service_info = history.pop(address)
 
