b970f7977f874f7a6c74e7dda80d583f49897acb
joostlek <joostlek@outlook.com>
2023-04-19 13:48:42 +0200

    Fix tests

--- tests/components/lastfm/test_sensor.py
+++ tests/components/lastfm/test_sensor.py
@@ -1,28 +1,71 @@
 """Tests for the lastfm sensor."""
+from unittest.mock import patch
+
+from pylast import Track
+import pytest
 
 from homeassistant.components import sensor
 from homeassistant.components.lastfm.sensor import STATE_NOT_SCROBBLING
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
-from . import patch_interface
 
-from tests.components.lastfm import MOCK_TRACK
+class MockNetwork:
+    """Mock _Network object for pylast."""
 
-CONFIG = {
-    "sensor": {
-        "platform": "lastfm",
-        "api_key": "secret-key",
-        "users": ["test"],
-    }
-}
+    def __init__(self, username: str):
+        """Initialize the mock."""
+        self.username = username
 
 
-async def test_update_not_playing(hass: HomeAssistant) -> None:
+class MockUser:
+    """Mock User object for pylast."""
+
+    def __init__(self, now_playing_result):
+        """Initialize the mock."""
+        self._now_playing_result = now_playing_result
+        self.name = "test"
+
+    def get_playcount(self):
+        """Get mock play count."""
+        return 1
+
+    def get_image(self, size):
+        """Get mock image."""
+
+    def get_recent_tracks(self, limit):
+        """Get mock recent tracks."""
+        return []
+
+    def get_top_tracks(self, limit):
+        """Get mock top tracks."""
+        return []
+
+    def get_now_playing(self):
+        """Get mock now playing."""
+        return self._now_playing_result
+
+
+@pytest.fixture(name="lastfm_network")
+def lastfm_network_fixture():
+    """Create fixture for LastFMNetwork."""
+    with patch(
+        "homeassistant.components.lastfm.sensor.LastFMNetwork"
+    ) as lastfm_network:
+        yield lastfm_network
+
+
+async def test_update_not_playing(hass: HomeAssistant, lastfm_network) -> None:
     """Test update when no playing song."""
-    with patch_interface("test"):
-        assert await async_setup_component(hass, sensor.DOMAIN, CONFIG) is True
-        await hass.async_block_till_done()
+
+    lastfm_network.return_value.get_user.return_value = MockUser(None)
+
+    assert await async_setup_component(
+        hass,
+        sensor.DOMAIN,
+        {"sensor": {"platform": "lastfm", "api_key": "secret-key", "users": ["test"]}},
+    )
+    await hass.async_block_till_done()
 
     entity_id = "sensor.test"
 
@@ -31,40 +74,22 @@ async def test_update_not_playing(hass: HomeAssistant) -> None:
     assert state.state == STATE_NOT_SCROBBLING
 
 
-async def test_update_playing(hass: HomeAssistant) -> None:
+async def test_update_playing(hass: HomeAssistant, lastfm_network) -> None:
     """Test update when song playing."""
-    with patch_interface("test", MOCK_TRACK):
-        assert await async_setup_component(hass, sensor.DOMAIN, CONFIG) is True
-        await hass.async_block_till_done()
+
+    lastfm_network.return_value.get_user.return_value = MockUser(
+        Track("artist", "title", MockNetwork("test"))
+    )
+
+    assert await async_setup_component(
+        hass,
+        sensor.DOMAIN,
+        {"sensor": {"platform": "lastfm", "api_key": "secret-key", "users": ["test"]}},
+    )
+    await hass.async_block_till_done()
 
     entity_id = "sensor.test"
 
     state = hass.states.get(entity_id)
 
-    assert state.state == "Goldband - Noodgeval"
-
-
-async def test_failed_update(hass: HomeAssistant) -> None:
-    """Test that the integration does not break when an unknown user is added."""
-    with patch_interface("not_existing"):
-        assert (
-            await async_setup_component(
-                hass,
-                sensor.DOMAIN,
-                {
-                    "sensor": {
-                        "platform": "lastfm",
-                        "api_key": "secret-key",
-                        "users": ["not_existing"],
-                    }
-                },
-            )
-            is True
-        )
-        await hass.async_block_till_done()
-
-    entity_id = "sensor.not_existing"
-
-    state = hass.states.get(entity_id)
-
-    assert state is None
+    assert state.state == "artist - title"
