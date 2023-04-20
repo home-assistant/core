9a90e90472c23a08464d2013e474ba101b33f708
Franck Nijhof <git@frenck.dev>
2023-04-20 20:38:39 +0200

    Add Supervisor add-on discovery to Wyoming integration (#91761)

--- homeassistant/components/wyoming/config_flow.py
+++ homeassistant/components/wyoming/config_flow.py
@@ -2,10 +2,12 @@
 from __future__ import annotations
 
 from typing import Any
+from urllib.parse import urlparse
 
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.components.hassio import HassioServiceInfo
 from homeassistant.const import CONF_HOST, CONF_PORT
 from homeassistant.data_entry_flow import FlowResult
 
@@ -24,6 +26,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Wyoming integration."""
 
     VERSION = 1
+
+    _hassio_discovery: HassioServiceInfo
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -54,3 +58,36 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         name = asr_installed[0].name
 
         return self.async_create_entry(title=name, data=user_input)
+
+    async def async_step_hassio(self, discovery_info: HassioServiceInfo) -> FlowResult:
+        """Handle Supervisor add-on discovery."""
+        await self.async_set_unique_id(discovery_info.slug)
+        self._abort_if_unique_id_configured()
+
+        self._hassio_discovery = discovery_info
+        return await self.async_step_hassio_confirm()
+
+    async def async_step_hassio_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Confirm Supervisor discovery."""
+        errors: dict[str, str] = {}
+
+        if user_input is not None:
+            uri = urlparse(self._hassio_discovery.config["uri"])
+            if service := await WyomingService.create(uri.hostname, uri.port):
+                if not any(asr for asr in service.info.asr if asr.installed):
+                    return self.async_abort(reason="no_services")
+
+                return self.async_create_entry(
+                    title=self._hassio_discovery.name,
+                    data={CONF_HOST: uri.hostname, CONF_PORT: uri.port},
+                )
+
+            errors = {"base": "cannot_connect"}
+
+        return self.async_show_form(
+            step_id="hassio_confirm",
+            description_placeholders={"addon": self._hassio_discovery.name},
+            errors=errors,
+        )
