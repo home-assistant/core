b6f2b29a99d7314822dc8be1003514fa3890afbf
Michael Hansen <mike@rhasspy.org>
2023-04-23 13:06:56 -0500

    Wyoming tts (#91712)
    * Add tts entity
    
    * Add tts entity and tests
    
    * Re-add name to TextToSpeechEntity
    
    * Fix linting
    
    * Fix ruff linting
    
    * Support voice attr (unused)
    
    * Remove async_get_text_to_speech_entity
    
    * Move name property to Wyoming TTS entity
    
    * Fix id
    
    ---------
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- tests/components/wyoming/__init__.py
+++ tests/components/wyoming/__init__.py
@@ -1,5 +1,5 @@
 """Tests for the Wyoming integration."""
-from wyoming.info import AsrModel, AsrProgram, Attribution, Info
+from wyoming.info import AsrModel, AsrProgram, Attribution, Info, TtsProgram, TtsVoice
 
 TEST_ATTR = Attribution(name="Test", url="http://www.test.com")
 STT_INFO = Info(
@@ -19,4 +19,53 @@ STT_INFO = Info(
         )
     ]
 )
+TTS_INFO = Info(
+    tts=[
+        TtsProgram(
+            name="Test TTS",
+            installed=True,
+            attribution=TEST_ATTR,
+            voices=[
+                TtsVoice(
+                    name="Test Voice",
+                    installed=True,
+                    attribution=TEST_ATTR,
+                    languages=["en-US"],
+                )
+            ],
+        )
+    ]
+)
 EMPTY_INFO = Info()
+
+
+class MockAsyncTcpClient:
+    """Mock AsyncTcpClient."""
+
+    def __init__(self, responses) -> None:
+        """Initialize."""
+        self.host = None
+        self.port = None
+        self.written = []
+        self.responses = responses
+
+    async def write_event(self, event):
+        """Send."""
+        self.written.append(event)
+
+    async def read_event(self):
+        """Receive."""
+        return self.responses.pop(0)
+
+    async def __aenter__(self):
+        """Enter."""
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        """Exit."""
+
+    def __call__(self, host, port):
+        """Call."""
+        self.host = host
+        self.port = port
+        return self
