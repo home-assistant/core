fccd097bd945fc6c17a827d71f1eeaa576041b19
Dara Adib <daradib@ocf.berkeley.edu>
2023-04-25 04:30:26 +0000

    Update Microsoft TTS supported languages
    `script.microsoft_tts` scrapes Microsoft Azure documentation for
    the list of supported languages and saves them to
    `homeassistant.generated.microsoft_tts` for use in the component.
    
    This adds support for more TTS languages, like fa-ir (Persian).

---
+++ script/microsoft_tts.py
@@ -0,0 +1,29 @@
+"""Helper script to update supported languages for Microsoft Text-to-Speech (TTS)."""
+from pathlib import Path
+
+from lxml import html
+import requests
+
+from .hassfest.serializer import format_python_namespace
+
+URL = "https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support"
+XPATH_QUERY = "//*[@data-tab='tts']//tr/td[1]/code/text()"
+
+req = requests.get(URL)
+req.raise_for_status()
+tree = html.fromstring(req.content)
+supported_languages_raw = tree.xpath(XPATH_QUERY)
+supported_languages = {s.lower() for s in supported_languages_raw}
+
+assert "en-us" in supported_languages
+assert "fa-ir" in supported_languages
+assert len(supported_languages) > 10
+
+Path("homeassistant/generated/microsoft_tts.py").write_text(
+    format_python_namespace(
+        {
+            "SUPPORTED_LANGUAGES": supported_languages,
+        },
+        generator="script.microsoft_tts",
+    )
+)
