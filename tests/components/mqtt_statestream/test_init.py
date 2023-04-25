773c40b503d1979285896f1d8a9f34b8ee9dae06
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-04-25 14:53:49 +0200

    Update tests/components/mqtt_statestream/test_init.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- tests/components/mqtt_statestream/test_init.py
+++ tests/components/mqtt_statestream/test_init.py
@@ -96,7 +96,7 @@ async def test_setup_and_stop_waits_for_ha(
     mqtt_mock.async_publish.assert_not_called()
 
 
-# We use xfail with this test because there is an unhandled exception in this test.
+# We use xfail with this test because there is an unhandled exception in a background task in this test.
 # The exception is raised by mqtt.async_publish.
 @pytest.mark.xfail()
 async def test_startup_no_mqtt(
