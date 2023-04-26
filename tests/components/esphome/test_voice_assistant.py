29ca43acf648372c19bb0ce3cd8bc81a4595a8e1
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-04-27 10:29:08 +1200

    Set pipeline_id from pipeline select (#92085)

--- tests/components/esphome/test_voice_assistant.py
+++ tests/components/esphome/test_voice_assistant.py
@@ -8,6 +8,8 @@ import async_timeout
 import pytest
 
 from homeassistant.components import assist_pipeline, esphome
+from homeassistant.components.esphome import DomainData
+from homeassistant.components.esphome.voice_assistant import VoiceAssistantUDPServer
 from homeassistant.core import HomeAssistant
 
 _TEST_INPUT_TEXT = "This is an input test"
@@ -15,7 +17,19 @@ _TEST_OUTPUT_TEXT = "This is an output test"
 _TEST_OUTPUT_URL = "output.mp3"
 
 
-async def test_pipeline_events(hass: HomeAssistant) -> None:
+@pytest.fixture
+def voice_assistant_udp_server_v1(
+    hass: HomeAssistant,
+    mock_voice_assistant_v1_entry,
+) -> VoiceAssistantUDPServer:
+    """Return the UDP server."""
+    entry_data = DomainData.get(hass).get_entry_data(mock_voice_assistant_v1_entry)
+    return VoiceAssistantUDPServer(hass, entry_data)
+
+
+async def test_pipeline_events(
+    hass: HomeAssistant, voice_assistant_udp_server_v1: VoiceAssistantUDPServer
+) -> None:
     """Test that the pipeline function is called."""
 
     async def async_pipeline_from_audio_stream(*args, **kwargs):
@@ -67,75 +81,74 @@ async def test_pipeline_events(hass: HomeAssistant) -> None:
         "homeassistant.components.esphome.voice_assistant.async_pipeline_from_audio_stream",
         new=async_pipeline_from_audio_stream,
     ):
-        server = esphome.voice_assistant.VoiceAssistantUDPServer(hass)
-        server.transport = Mock()
+        voice_assistant_udp_server_v1.transport = Mock()
 
-        await server.run_pipeline(handle_event)
+        await voice_assistant_udp_server_v1.run_pipeline(handle_event)
 
 
 async def test_udp_server(
     hass: HomeAssistant,
     socket_enabled,
     unused_udp_port_factory,
+    voice_assistant_udp_server_v1: VoiceAssistantUDPServer,
 ) -> None:
     """Test the UDP server runs and queues incoming data."""
     port_to_use = unused_udp_port_factory()
 
-    server = esphome.voice_assistant.VoiceAssistantUDPServer(hass)
     with patch(
         "homeassistant.components.esphome.voice_assistant.UDP_PORT", new=port_to_use
     ):
-        port = await server.start_server()
+        port = await voice_assistant_udp_server_v1.start_server()
         assert port == port_to_use
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        assert server.queue.qsize() == 0
+        assert voice_assistant_udp_server_v1.queue.qsize() == 0
         sock.sendto(b"test", ("127.0.0.1", port))
 
         # Give the socket some time to send/receive the data
         async with async_timeout.timeout(1):
-            while server.queue.qsize() == 0:
+            while voice_assistant_udp_server_v1.queue.qsize() == 0:
                 await asyncio.sleep(0.1)
 
-        assert server.queue.qsize() == 1
+        assert voice_assistant_udp_server_v1.queue.qsize() == 1
 
-        server.stop()
+        voice_assistant_udp_server_v1.stop()
 
-        assert server.transport.is_closing()
+        assert voice_assistant_udp_server_v1.transport.is_closing()
 
 
 async def test_udp_server_multiple(
     hass: HomeAssistant,
     socket_enabled,
     unused_udp_port_factory,
+    voice_assistant_udp_server_v1: VoiceAssistantUDPServer,
 ) -> None:
     """Test that the UDP server raises an error if started twice."""
-    server = esphome.voice_assistant.VoiceAssistantUDPServer(hass)
     with patch(
         "homeassistant.components.esphome.voice_assistant.UDP_PORT",
         new=unused_udp_port_factory(),
     ):
-        await server.start_server()
+        await voice_assistant_udp_server_v1.start_server()
 
     with patch(
         "homeassistant.components.esphome.voice_assistant.UDP_PORT",
         new=unused_udp_port_factory(),
     ), pytest.raises(RuntimeError):
         pass
-        await server.start_server()
+        await voice_assistant_udp_server_v1.start_server()
 
 
 async def test_udp_server_after_stopped(
     hass: HomeAssistant,
     socket_enabled,
     unused_udp_port_factory,
+    voice_assistant_udp_server_v1: VoiceAssistantUDPServer,
 ) -> None:
     """Test that the UDP server raises an error if started after stopped."""
-    server = esphome.voice_assistant.VoiceAssistantUDPServer(hass)
-    server.stop()
+    voice_assistant_udp_server_v1.stop()
     with patch(
         "homeassistant.components.esphome.voice_assistant.UDP_PORT",
         new=unused_udp_port_factory(),
     ), pytest.raises(RuntimeError):
-        await server.start_server()
+        await voice_assistant_udp_server_v1.start_server()
