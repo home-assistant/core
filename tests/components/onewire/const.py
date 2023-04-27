db2cf0ab138e4e9dbcd660cf67022f38b0ca5b95
epenet <6771947+epenet@users.noreply.github.com>
2023-04-27 09:53:14 +0000

    Cleanup dict

--- tests/components/onewire/const.py
+++ tests/components/onewire/const.py
@@ -18,9 +18,7 @@ MOCK_OWPROXY_DEVICES = {
             b"DS2405",  # read device type
         ],
         Platform.SWITCH: [
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
+            {ATTR_INJECT_READS: b"    1"},
         ],
     },
     "10.111111111111": {
@@ -28,9 +26,7 @@ MOCK_OWPROXY_DEVICES = {
             b"DS18S20",  # read device type
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    25.123",
-            },
+            {ATTR_INJECT_READS: b"    25.123"},
         ],
     },
     "12.111111111111": {
@@ -38,34 +34,18 @@ MOCK_OWPROXY_DEVICES = {
             b"DS2406",  # read device type
         ],
         Platform.BINARY_SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    25.123",
-            },
-            {
-                ATTR_INJECT_READS: b"  1025.123",
-            },
+            {ATTR_INJECT_READS: b"    25.123"},
+            {ATTR_INJECT_READS: b"  1025.123"},
         ],
         Platform.SWITCH: [
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
         ],
     },
     "1D.111111111111": {
@@ -73,12 +53,8 @@ MOCK_OWPROXY_DEVICES = {
             b"DS2423",  # read device type
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    251123",
-            },
-            {
-                ATTR_INJECT_READS: b"    248125",
-            },
+            {ATTR_INJECT_READS: b"    251123"},
+            {ATTR_INJECT_READS: b"    248125"},
         ],
     },
     "1F.111111111111": {
@@ -121,44 +97,22 @@ MOCK_OWPROXY_DEVICES = {
             b"DS2438",  # read device type
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    25.123",
-            },
-            {
-                ATTR_INJECT_READS: b"    72.7563",
-            },
-            {
-                ATTR_INJECT_READS: b"    73.7563",
-            },
-            {
-                ATTR_INJECT_READS: b"    74.7563",
-            },
-            {
-                ATTR_INJECT_READS: b"    75.7563",
-            },
+            {ATTR_INJECT_READS: b"    25.123"},
+            {ATTR_INJECT_READS: b"    72.7563"},
+            {ATTR_INJECT_READS: b"    73.7563"},
+            {ATTR_INJECT_READS: b"    74.7563"},
+            {ATTR_INJECT_READS: b"    75.7563"},
             {
                 ATTR_INJECT_READS: ProtocolError,
             },
-            {
-                ATTR_INJECT_READS: b"    969.265",
-            },
-            {
-                ATTR_INJECT_READS: b"    65.8839",
-            },
-            {
-                ATTR_INJECT_READS: b"     2.97",
-            },
-            {
-                ATTR_INJECT_READS: b"    4.74",
-            },
-            {
-                ATTR_INJECT_READS: b"    0.12",
-            },
+            {ATTR_INJECT_READS: b"    969.265"},
+            {ATTR_INJECT_READS: b"    65.8839"},
+            {ATTR_INJECT_READS: b"     2.97"},
+            {ATTR_INJECT_READS: b"    4.74"},
+            {ATTR_INJECT_READS: b"    0.12"},
         ],
         Platform.SWITCH: [
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
+            {ATTR_INJECT_READS: b"    1"},
         ],
     },
     "28.111111111111": {
@@ -166,9 +120,7 @@ MOCK_OWPROXY_DEVICES = {
             b"DS18B20",  # read device type
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    26.984",
-            },
+            {ATTR_INJECT_READS: b"    26.984"},
         ],
     },
     "28.222222222222": {
@@ -200,80 +152,32 @@ MOCK_OWPROXY_DEVICES = {
             b"DS2408",  # read device type
         ],
         Platform.BINARY_SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    0"},
         ],
         Platform.SWITCH: [
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
         ],
     },
     "30.111111111111": {
@@ -281,19 +185,13 @@ MOCK_OWPROXY_DEVICES = {
             b"DS2760",  # read device type
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    26.984",
-            },
+            {ATTR_INJECT_READS: b"    26.984"},
             {
                 ATTR_DEVICE_FILE: "/30.111111111111/typeK/temperature",
                 ATTR_INJECT_READS: b"    173.7563",
             },
-            {
-                ATTR_INJECT_READS: b"     2.97",
-            },
-            {
-                ATTR_INJECT_READS: b"    0.12",
-            },
+            {ATTR_INJECT_READS: b"     2.97"},
+            {ATTR_INJECT_READS: b"     0.12"},
         ],
     },
     "3A.111111111111": {
@@ -301,20 +199,12 @@ MOCK_OWPROXY_DEVICES = {
             b"DS2413",  # read device type
         ],
         Platform.BINARY_SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
         ],
         Platform.SWITCH: [
-            {
-                ATTR_INJECT_READS: b"    1",
-            },
-            {
-                ATTR_INJECT_READS: b"    0",
-            },
+            {ATTR_INJECT_READS: b"    1"},
+            {ATTR_INJECT_READS: b"    0"},
         ],
     },
     "3B.111111111111": {
@@ -322,9 +212,7 @@ MOCK_OWPROXY_DEVICES = {
             b"DS1825",  # read device type
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    28.243",
-            },
+            {ATTR_INJECT_READS: b"    28.243"},
         ],
     },
     "42.111111111111": {
@@ -332,9 +220,7 @@ MOCK_OWPROXY_DEVICES = {
             b"DS28EA00",  # read device type
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    29.123",
-            },
+            {ATTR_INJECT_READS: b"    29.123"},
         ],
     },
     "EF.111111111111": {
@@ -342,15 +228,9 @@ MOCK_OWPROXY_DEVICES = {
             b"HobbyBoards_EF",  # read type
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    67.745",
-            },
-            {
-                ATTR_INJECT_READS: b"    65.541",
-            },
-            {
-                ATTR_INJECT_READS: b"    25.123",
-            },
+            {ATTR_INJECT_READS: b"    67.745"},
+            {ATTR_INJECT_READS: b"    65.541"},
+            {ATTR_INJECT_READS: b"    25.123"},
         ],
     },
     "EF.111111111112": {
@@ -362,44 +242,20 @@ MOCK_OWPROXY_DEVICES = {
             b"         0",  # read is_leaf_3
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    41.745",
-            },
-            {
-                ATTR_INJECT_READS: b"    42.541",
-            },
-            {
-                ATTR_INJECT_READS: b"    43.123",
-            },
-            {
-                ATTR_INJECT_READS: b"    44.123",
-            },
+            {ATTR_INJECT_READS: b"    41.745"},
+            {ATTR_INJECT_READS: b"    42.541"},
+            {ATTR_INJECT_READS: b"    43.123"},
+            {ATTR_INJECT_READS: b"    44.123"},
         ],
         Platform.SWITCH: [
-            {
-                ATTR_INJECT_READS: b"1",
-            },
-            {
-                ATTR_INJECT_READS: b"1",
-            },
-            {
-                ATTR_INJECT_READS: b"0",
-            },
-            {
-                ATTR_INJECT_READS: b"0",
-            },
-            {
-                ATTR_INJECT_READS: b"1",
-            },
-            {
-                ATTR_INJECT_READS: b"1",
-            },
-            {
-                ATTR_INJECT_READS: b"0",
-            },
-            {
-                ATTR_INJECT_READS: b"0",
-            },
+            {ATTR_INJECT_READS: b"1"},
+            {ATTR_INJECT_READS: b"1"},
+            {ATTR_INJECT_READS: b"0"},
+            {ATTR_INJECT_READS: b"0"},
+            {ATTR_INJECT_READS: b"1"},
+            {ATTR_INJECT_READS: b"1"},
+            {ATTR_INJECT_READS: b"0"},
+            {ATTR_INJECT_READS: b"0"},
         ],
     },
     "EF.111111111113": {
@@ -407,32 +263,16 @@ MOCK_OWPROXY_DEVICES = {
             b"HB_HUB",  # read type
         ],
         Platform.BINARY_SENSOR: [
-            {
-                ATTR_INJECT_READS: b"1",
-            },
-            {
-                ATTR_INJECT_READS: b"0",
-            },
-            {
-                ATTR_INJECT_READS: b"1",
-            },
-            {
-                ATTR_INJECT_READS: b"0",
-            },
+            {ATTR_INJECT_READS: b"1"},
+            {ATTR_INJECT_READS: b"0"},
+            {ATTR_INJECT_READS: b"1"},
+            {ATTR_INJECT_READS: b"0"},
         ],
         Platform.SWITCH: [
-            {
-                ATTR_INJECT_READS: b"1",
-            },
-            {
-                ATTR_INJECT_READS: b"0",
-            },
-            {
-                ATTR_INJECT_READS: b"1",
-            },
-            {
-                ATTR_INJECT_READS: b"0",
-            },
+            {ATTR_INJECT_READS: b"1"},
+            {ATTR_INJECT_READS: b"0"},
+            {ATTR_INJECT_READS: b"1"},
+            {ATTR_INJECT_READS: b"0"},
         ],
     },
     "7E.111111111111": {
@@ -441,18 +281,10 @@ MOCK_OWPROXY_DEVICES = {
             b"EDS0068",  # read device_type - note EDS specific
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    13.9375",
-            },
-            {
-                ATTR_INJECT_READS: b"  1012.21",
-            },
-            {
-                ATTR_INJECT_READS: b"  65.8839",
-            },
-            {
-                ATTR_INJECT_READS: b"    41.375",
-            },
+            {ATTR_INJECT_READS: b"  13.9375"},
+            {ATTR_INJECT_READS: b"  1012.21"},
+            {ATTR_INJECT_READS: b"  65.8839"},
+            {ATTR_INJECT_READS: b"   41.375"},
         ],
     },
     "7E.222222222222": {
@@ -461,12 +293,8 @@ MOCK_OWPROXY_DEVICES = {
             b"EDS0066",  # read device_type - note EDS specific
         ],
         Platform.SENSOR: [
-            {
-                ATTR_INJECT_READS: b"    13.9375",
-            },
-            {
-                ATTR_INJECT_READS: b"  1012.21",
-            },
+            {ATTR_INJECT_READS: b"    13.9375"},
+            {ATTR_INJECT_READS: b"  1012.21"},
         ],
     },
 }
