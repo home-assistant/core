479a45a631350728aad9e5317d1a80d66e33e810
IceBotYT <34712694+IceBotYT@users.noreply.github.com>
2023-04-28 19:57:36 +0000

    Use SnapshotAssertion

--- tests/components/lacrosse_view/test_diagnostics.py
+++ tests/components/lacrosse_view/test_diagnostics.py
@@ -1,6 +1,8 @@
 """Test diagnostics of LaCrosse View."""
 from unittest.mock import patch
 
+from syrupy.assertion import SnapshotAssertion
+
 from homeassistant.components.lacrosse_view import DOMAIN
 from homeassistant.core import HomeAssistant
 
@@ -12,10 +14,14 @@ from tests.typing import ClientSessionGenerator
 
 
 async def test_entry_diagnostics(
-    hass: HomeAssistant, hass_client: ClientSessionGenerator
+    hass: HomeAssistant,
+    hass_client: ClientSessionGenerator,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test config entry diagnostics."""
-    config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_ENTRY_DATA)
+    config_entry = MockConfigEntry(
+        domain=DOMAIN, data=MOCK_ENTRY_DATA, entry_id="lacrosse_view_test_entry_id"
+    )
     config_entry.add_to_hass(hass)
 
     with patch("lacrosse_view.LaCrosse.login", return_value=True), patch(
@@ -23,17 +29,8 @@ async def test_entry_diagnostics(
     ):
         assert await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
-    result = await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
 
-    assert result["entry"]["data"] == {
-        "username": "**REDACTED**",
-        "password": "**REDACTED**",
-        "id": "1",
-        "name": "Test",
-    }
-    assert result["coordinator_data"] == [
-        {
-            "__type": "<class 'lacrosse_view.Sensor'>",
-            "repr": "Sensor(name='Test', device_id='1', type='Test', sensor_id='2', sensor_field_names=['Temperature'], location=Location(id='1', name='Test'), permissions={'read': True}, model='Test', data={'Temperature': {'values': [{'s': '2'}], 'unit': 'degrees_celsius'}})",
-        }
-    ]
+    assert (
+        await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
+        == snapshot
+    )
