7af3f56f617277c0387075656088e9b7bbae8fe8
David Knowles <dknowles2@gmail.com>
2023-04-30 19:43:39 +0000

    Add a DataUpdateCoordinator to Hydrawise

--- homeassistant/components/hydrawise/__init__.py
+++ homeassistant/components/hydrawise/__init__.py
@@ -1,17 +1,21 @@
 """Support for Hydrawise cloud."""
 
+from typing import Any
+
 from hydrawiser.core import Hydrawiser
 from requests.exceptions import ConnectTimeout, HTTPError
 import voluptuous as vol
 
 from homeassistant.components import persistent_notification
 from homeassistant.const import CONF_ACCESS_TOKEN, CONF_SCAN_INTERVAL
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.dispatcher import async_dispatcher_connect, dispatcher_send
-from homeassistant.helpers.entity import Entity, EntityDescription
-from homeassistant.helpers.event import track_time_interval
+from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 
 from .const import (
     DATA_HYDRAWISE,
@@ -20,8 +24,8 @@ from .const import (
     NOTIFICATION_ID,
     NOTIFICATION_TITLE,
     SCAN_INTERVAL,
-    SIGNAL_UPDATE_HYDRAWISE,
 )
+from .coordinator import HydrawiseDataUpdateCoordinator
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -36,15 +40,17 @@ CONFIG_SCHEMA = vol.Schema(
 )
 
 
-def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Hunter Hydrawise component."""
     conf = config[DOMAIN]
     access_token = conf[CONF_ACCESS_TOKEN]
     scan_interval = conf.get(CONF_SCAN_INTERVAL)
 
     try:
-        hydrawise = Hydrawiser(user_token=access_token)
-        hass.data[DATA_HYDRAWISE] = HydrawiseHub(hydrawise)
+        hydrawise = await hass.async_add_executor_job(Hydrawiser, access_token)
+        hass.data[DATA_HYDRAWISE] = HydrawiseDataUpdateCoordinator(
+            hass, hydrawise, scan_interval
+        )
     except (ConnectTimeout, HTTPError) as ex:
         LOGGER.error("Unable to connect to Hydrawise cloud service: %s", str(ex))
         persistent_notification.create(
@@ -55,49 +61,30 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
         )
         return False
 
-    def hub_refresh(event_time):
-        """Call Hydrawise hub to refresh information."""
-        LOGGER.debug("Updating Hydrawise Hub component")
-        hass.data[DATA_HYDRAWISE].data.update_controller_info()
-        dispatcher_send(hass, SIGNAL_UPDATE_HYDRAWISE)
-
-    # Call the Hydrawise API to refresh updates
-    track_time_interval(hass, hub_refresh, scan_interval)
+    # NOTE: We don't need to call async_config_entry_first_refresh() because
+    # data is fetched when the Hydrawiser object is instantiated.
 
     return True
 
 
-class HydrawiseHub:
-    """Representation of a base Hydrawise device."""
-
-    def __init__(self, data):
-        """Initialize the entity."""
-        self.data = data
-
-
-class HydrawiseEntity(Entity):
+class HydrawiseEntity(CoordinatorEntity):
     """Entity class for Hydrawise devices."""
 
     _attr_attribution = "Data provided by hydrawise.com"
 
-    def __init__(self, data, description: EntityDescription) -> None:
+    def __init__(
+        self,
+        *,
+        data: dict[str, Any],
+        coordinator: DataUpdateCoordinator,
+        description: EntityDescription,
+    ) -> None:
         """Initialize the Hydrawise entity."""
-        self.entity_description = description
+        super().__init__(coordinator=coordinator)
         self.data = data
+        self.api = self.coordinator.api
+        self.entity_description = description
         self._attr_name = f"{self.data['name']} {description.name}"
-
-    async def async_added_to_hass(self):
-        """Register callbacks."""
-        self.async_on_remove(
-            async_dispatcher_connect(
-                self.hass, SIGNAL_UPDATE_HYDRAWISE, self._update_callback
-            )
-        )
-
-    @callback
-    def _update_callback(self):
-        """Call update method."""
-        self.async_schedule_update_ha_state(True)
 
     @property
     def extra_state_attributes(self):
