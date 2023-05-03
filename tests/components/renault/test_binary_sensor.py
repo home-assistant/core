94e791d3fe49916088801046d697768ad5541679
epenet <6771947+epenet@users.noreply.github.com>
2023-05-03 08:43:53 +0000

    Use SnapshotAssertion in Renault tests (part 2)

--- tests/components/renault/test_binary_sensor.py
+++ tests/components/renault/test_binary_sensor.py
@@ -6,11 +6,11 @@ import pytest
 from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import STATE_UNKNOWN, Platform
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 
-from . import check_device_registry, check_entities_no_data, check_entities_unavailable
+from . import check_device_registry, check_entities_unavailable
 from .const import MOCK_VEHICLES
 
 pytestmark = pytest.mark.usefixtures("patch_renault_account", "patch_get_vehicles")
@@ -56,20 +56,29 @@ async def test_binary_sensors(
 async def test_binary_sensor_empty(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
-    vehicle_type: str,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test for Renault binary sensors with empty data from Renault."""
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
 
-    mock_vehicle = MOCK_VEHICLES[vehicle_type]
-    check_device_registry(device_registry, mock_vehicle["expected_device"])
+    # Ensure devices are correctly registered
+    device_entries = dr.async_entries_for_config_entry(
+        device_registry, config_entry.entry_id
+    )
+    assert device_entries == snapshot
 
-    expected_entities = mock_vehicle[Platform.BINARY_SENSOR]
-    assert len(entity_registry.entities) == len(expected_entities)
-    check_entities_no_data(hass, entity_registry, expected_entities, STATE_UNKNOWN)
+    # Ensure entities are correctly registered
+    entity_entries = er.async_entries_for_config_entry(
+        entity_registry, config_entry.entry_id
+    )
+    assert entity_entries == snapshot
+
+    # Ensure entity states are correct
+    states = [hass.states.get(ent.entity_id) for ent in entity_entries]
+    assert states == snapshot
 
 
 @pytest.mark.usefixtures("fixtures_with_invalid_upstream_exception")
