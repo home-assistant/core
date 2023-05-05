e41a75f617a3495f6c9cf7c18155b89ad0078fce
G Johansson <goran.johansson@shiftit.se>
2023-05-05 16:53:40 +0200

    Remove yaml import rainbird (#92599)

--- homeassistant/components/rainbird/__init__.py
+++ homeassistant/components/rainbird/__init__.py
@@ -1,83 +1,20 @@
 """Support for Rain Bird Irrigation system LNK WiFi Module."""
 from __future__ import annotations
 
-import logging
-
 from pyrainbird.async_client import AsyncRainbirdClient, AsyncRainbirdController
-import voluptuous as vol
 
-from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
-from homeassistant.const import (
-    CONF_FRIENDLY_NAME,
-    CONF_HOST,
-    CONF_PASSWORD,
-    CONF_TRIGGER_TIME,
-    Platform,
-)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
-from homeassistant.helpers.typing import ConfigType
 
-from .const import CONF_SERIAL_NUMBER, CONF_ZONES
+from .const import CONF_SERIAL_NUMBER
 from .coordinator import RainbirdUpdateCoordinator
 
 PLATFORMS = [Platform.SWITCH, Platform.SENSOR, Platform.BINARY_SENSOR, Platform.NUMBER]
 
-_LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "rainbird"
-
-TRIGGER_TIME_SCHEMA = vol.All(
-    cv.time_period, cv.positive_timedelta, lambda td: (td.total_seconds() // 60)
-)
-
-ZONE_SCHEMA = vol.Schema(
-    {
-        vol.Optional(CONF_FRIENDLY_NAME): cv.string,
-        vol.Optional(CONF_TRIGGER_TIME): TRIGGER_TIME_SCHEMA,
-    }
-)
-CONTROLLER_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_HOST): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string,
-        vol.Required(CONF_TRIGGER_TIME): TRIGGER_TIME_SCHEMA,
-        vol.Optional(CONF_ZONES): vol.Schema({cv.positive_int: ZONE_SCHEMA}),
-    }
-)
-CONFIG_SCHEMA = vol.Schema(
-    {DOMAIN: vol.Schema(vol.All(cv.ensure_list, [CONTROLLER_SCHEMA]))},
-    extra=vol.ALLOW_EXTRA,
-)
-
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Rain Bird component."""
-    if DOMAIN not in config:
-        return True
-
-    for controller_config in config[DOMAIN]:
-        hass.async_create_task(
-            hass.config_entries.flow.async_init(
-                DOMAIN,
-                context={"source": SOURCE_IMPORT},
-                data=controller_config,
-            )
-        )
-
-    async_create_issue(
-        hass,
-        DOMAIN,
-        "deprecated_yaml",
-        breaks_in_ha_version="2023.4.0",
-        is_fixable=False,
-        severity=IssueSeverity.WARNING,
-        translation_key="deprecated_yaml",
-    )
-
-    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
--- tests/components/rainbird/test_init.py
+++ tests/components/rainbird/test_init.py
@@ -8,14 +8,7 @@ from homeassistant.components.rainbird import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
-from .conftest import (
-    CONFIG,
-    CONFIG_ENTRY_DATA,
-    SERIAL_RESPONSE,
-    UNAVAILABLE_RESPONSE,
-    ComponentSetup,
-    mock_response,
-)
+from .conftest import CONFIG_ENTRY_DATA, UNAVAILABLE_RESPONSE, ComponentSetup
 
 from tests.test_util.aiohttp import AiohttpClientMockResponse
 
@@ -24,18 +17,8 @@ from tests.test_util.aiohttp import AiohttpClientMockResponse
     ("yaml_config", "config_entry_data", "initial_response"),
     [
         ({}, CONFIG_ENTRY_DATA, None),
-        (
-            CONFIG,
-            None,
-            mock_response(SERIAL_RESPONSE),  # Extra import request
-        ),
-        (
-            CONFIG,
-            CONFIG_ENTRY_DATA,
-            None,
-        ),
     ],
-    ids=["config_entry", "yaml", "already_exists"],
+    ids=["config_entry"],
 )
 async def test_init_success(
     hass: HomeAssistant,
@@ -62,25 +45,8 @@ async def test_init_success(
     ("yaml_config", "config_entry_data", "responses", "config_entry_states"),
     [
         ({}, CONFIG_ENTRY_DATA, [UNAVAILABLE_RESPONSE], [ConfigEntryState.SETUP_RETRY]),
-        (
-            CONFIG,
-            None,
-            [
-                UNAVAILABLE_RESPONSE,  # Failure when importing yaml
-            ],
-            [],
-        ),
-        (
-            CONFIG,
-            None,
-            [
-                mock_response(SERIAL_RESPONSE),  # Import succeeds
-                UNAVAILABLE_RESPONSE,  # Failure on integration setup
-            ],
-            [ConfigEntryState.SETUP_RETRY],
-        ),
     ],
-    ids=["config_entry_failure", "yaml_import_failure", "yaml_init_failure"],
+    ids=["config_entry_failure"],
 )
 async def test_communication_failure(
     hass: HomeAssistant,
