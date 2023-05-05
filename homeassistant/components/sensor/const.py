7f1511ffbc29b5c66a04728d05855ac0c172b476
metalblue <steven@darkazur.co.uk>
2023-05-05 18:10:00 +0100

    Update permitted SensorStateClass for Monetary sensors

--- homeassistant/components/sensor/const.py
+++ homeassistant/components/sensor/const.py
@@ -561,7 +561,7 @@ DEVICE_CLASS_STATE_CLASSES: dict[SensorDeviceClass, set[SensorStateClass]] = {
     SensorDeviceClass.ILLUMINANCE: {SensorStateClass.MEASUREMENT},
     SensorDeviceClass.IRRADIANCE: {SensorStateClass.MEASUREMENT},
     SensorDeviceClass.MOISTURE: {SensorStateClass.MEASUREMENT},
-    SensorDeviceClass.MONETARY: {SensorStateClass.TOTAL},
+    SensorDeviceClass.MONETARY: {SensorStateClass.TOTAL, SensorStateClass.TOTAL_INCREASING, SensorStateClass.MEASUREMENT},
     SensorDeviceClass.NITROGEN_DIOXIDE: {SensorStateClass.MEASUREMENT},
     SensorDeviceClass.NITROGEN_MONOXIDE: {SensorStateClass.MEASUREMENT},
     SensorDeviceClass.NITROUS_OXIDE: {SensorStateClass.MEASUREMENT},
