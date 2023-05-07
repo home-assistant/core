318db57ed966854c864092b7405502d5c5f430e7
Luke <conway220@gmail.com>
2023-05-07 21:35:32 +0000

    add anova switch platform

---
+++ homeassistant/components/anova/switch.py
@@ -0,0 +1,90 @@
+"""Support for Anova Switches."""
+from collections.abc import Callable, Coroutine
+from dataclasses import dataclass
+from typing import Any
+
+from anova_wifi import AnovaException, APCUpdate
+
+from homeassistant import config_entries
+from homeassistant.components.switch import (
+    SwitchDeviceClass,
+    SwitchEntity,
+    SwitchEntityDescription,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .coordinator import AnovaCoordinator
+from .entity import AnovaDescriptionEntity
+from .models import AnovaData
+
+
+@dataclass
+class AnovaSwitchEntityDescriptionMixin:
+    """Extra attributes for an Anova switch."""
+
+    is_on_lambda: Callable[[APCUpdate], bool]
+    turn_on_lambda: Callable[[AnovaCoordinator], Coroutine[Any, Any, None]]
+    turn_off_lambda: Callable[[AnovaCoordinator], Coroutine[Any, Any, None]]
+
+
+@dataclass
+class AnovaSwitchDescription(
+    SwitchEntityDescription, AnovaSwitchEntityDescriptionMixin
+):
+    """Describes a Anova switch."""
+
+
+SWITCH_DESCRIPTIONS = [
+    AnovaSwitchDescription(
+        device_class=SwitchDeviceClass.SWITCH,
+        key="precision_cooker_swtich",
+        translation_key="precision_cooker_switch",
+        is_on_lambda=lambda data: data.binary_sensor.maintaining
+        or data.binary_sensor.cooking
+        or data.binary_sensor.preheating,
+        turn_on_lambda=lambda data: data.anova_device.set_mode("COOK"),
+        turn_off_lambda=lambda data: data.anova_device.set_mode("IDLE"),
+    )
+]
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: config_entries.ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Anova device."""
+    anova_data: AnovaData = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities(
+        AnovaSwitch(coordinator, description)
+        for coordinator in anova_data.coordinators
+        for description in SWITCH_DESCRIPTIONS
+    )
+
+
+class AnovaSwitch(AnovaDescriptionEntity, SwitchEntity):
+    """Creates an Anova device that can be toggled."""
+
+    entity_description: AnovaSwitchDescription
+
+    @property
+    def is_on(self) -> bool:
+        """Determines if the Anova device is on."""
+        return self.entity_description.is_on_lambda(self.coordinator.data)
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the Anova device on."""
+        try:
+            return await self.entity_description.turn_on_lambda(self.coordinator)
+        except AnovaException as err:
+            raise HomeAssistantError("Failed to turn the switch on.") from err
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the Anova device off."""
+        try:
+            return await self.entity_description.turn_off_lambda(self.coordinator)
+        except AnovaException as err:
+            raise HomeAssistantError("Failed to turn the switch off.") from err
---
+++ tests/components/anova/test_switch.py
@@ -0,0 +1,66 @@
+"""Test the Anova Switches."""
+from unittest.mock import AsyncMock
+
+from anova_wifi import AnovaApi
+import pytest
+
+from homeassistant.components.switch import (
+    DOMAIN as SWITCH_DOMAIN,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+)
+from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
+
+from . import async_init_integration
+
+DEVICE_ID = "switch.anova_precision_cooker"
+
+
+async def test_turn_on(
+    hass: HomeAssistant, anova_api: AnovaApi, anova_precision_cooker: AsyncMock
+) -> None:
+    """Test turning on an Anova switch."""
+    await async_init_integration(hass)
+    await hass.services.async_call(
+        SWITCH_DOMAIN, SERVICE_TURN_ON, {ATTR_ENTITY_ID: DEVICE_ID}, blocking=True
+    )
+    assert anova_precision_cooker.set_mode.call_args_list[0][0][0] == "COOK"
+
+
+async def test_turn_on_failure(
+    hass: HomeAssistant,
+    anova_api: AnovaApi,
+    anova_precision_cooker_setter_failure: AsyncMock,
+) -> None:
+    """Test an error when you turn on an Anova switch."""
+    with pytest.raises(HomeAssistantError):
+        await async_init_integration(hass)
+        await hass.services.async_call(
+            SWITCH_DOMAIN, SERVICE_TURN_ON, {ATTR_ENTITY_ID: DEVICE_ID}, blocking=True
+        )
+
+
+async def test_turn_off(
+    hass: HomeAssistant, anova_api: AnovaApi, anova_precision_cooker: AsyncMock
+) -> None:
+    """Test turning on an Anova switch."""
+    await async_init_integration(hass)
+    await hass.services.async_call(
+        SWITCH_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: DEVICE_ID}, blocking=True
+    )
+    assert anova_precision_cooker.set_mode.call_args_list[0][0][0] == "IDLE"
+
+
+async def test_turn_off_failure(
+    hass: HomeAssistant,
+    anova_api: AnovaApi,
+    anova_precision_cooker_setter_failure: AsyncMock,
+) -> None:
+    """Test an error when you turn on an Anova switch."""
+    with pytest.raises(HomeAssistantError):
+        await async_init_integration(hass)
+        await hass.services.async_call(
+            SWITCH_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: DEVICE_ID}, blocking=True
+        )
