d582ffb9248dc020aa79873af1d749eea551bca0
G Johansson <goran.johansson@shiftit.se>
2023-05-08 19:58:09 +0000

    Inital init commit

--- homeassistant/components/command_line/__init__.py
+++ homeassistant/components/command_line/__init__.py
@@ -1,1 +1,158 @@
 """The command_line component."""
+from __future__ import annotations
+
+import asyncio
+from collections.abc import Coroutine
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant.components.binary_sensor import (
+    DEVICE_CLASSES_SCHEMA as BINARY_SENSOR_DEVICE_CLASSES_SCHEMA,
+)
+from homeassistant.components.sensor import (
+    CONF_STATE_CLASS,
+    DEVICE_CLASSES_SCHEMA as SENSOR_DEVICE_CLASSES_SCHEMA,
+    STATE_CLASSES_SCHEMA as SENSOR_STATE_CLASSES_SCHEMA,
+)
+from homeassistant.const import (
+    CONF_BINARY_SENSORS,
+    CONF_COMMAND,
+    CONF_COMMAND_CLOSE,
+    CONF_COMMAND_OFF,
+    CONF_COMMAND_ON,
+    CONF_COMMAND_OPEN,
+    CONF_COMMAND_STATE,
+    CONF_COMMAND_STOP,
+    CONF_COVERS,
+    CONF_DEVICE_CLASS,
+    CONF_FRIENDLY_NAME,
+    CONF_ICON_TEMPLATE,
+    CONF_NAME,
+    CONF_PAYLOAD_OFF,
+    CONF_PAYLOAD_ON,
+    CONF_SENSORS,
+    CONF_SWITCHES,
+    CONF_UNIQUE_ID,
+    CONF_UNIT_OF_MEASUREMENT,
+    CONF_VALUE_TEMPLATE,
+    Platform,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import discovery
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.typing import ConfigType
+
+from .const import CONF_COMMAND_TIMEOUT, DEFAULT_TIMEOUT, DOMAIN
+
+BINARY_SENSOR_DEFAULT_NAME = "Binary Command Sensor"
+DEFAULT_PAYLOAD_ON = "ON"
+DEFAULT_PAYLOAD_OFF = "OFF"
+CONF_JSON_ATTRIBUTES = "json_attributes"
+SENSOR_DEFAULT_NAME = "Command Sensor"
+CONF_NOTIFIERS = "notifiers"
+
+PLATFORM_MAPPING = {
+    CONF_BINARY_SENSORS: Platform.BINARY_SENSOR,
+    CONF_COVERS: Platform.COVER,
+    CONF_NOTIFIERS: Platform.NOTIFY,
+    CONF_SENSORS: Platform.SENSOR,
+    CONF_SWITCHES: Platform.SWITCH,
+}
+
+BINARY_SENSOR_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_COMMAND): cv.string,
+        vol.Optional(CONF_NAME, default=BINARY_SENSOR_DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_PAYLOAD_OFF, default=DEFAULT_PAYLOAD_OFF): cv.string,
+        vol.Optional(CONF_PAYLOAD_ON, default=DEFAULT_PAYLOAD_ON): cv.string,
+        vol.Optional(CONF_DEVICE_CLASS): BINARY_SENSOR_DEVICE_CLASSES_SCHEMA,
+        vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+        vol.Optional(CONF_COMMAND_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+        vol.Optional(CONF_UNIQUE_ID): cv.string,
+    }
+)
+COVER_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_COMMAND_CLOSE, default="true"): cv.string,
+        vol.Optional(CONF_COMMAND_OPEN, default="true"): cv.string,
+        vol.Optional(CONF_COMMAND_STATE): cv.string,
+        vol.Optional(CONF_COMMAND_STOP, default="true"): cv.string,
+        vol.Optional(CONF_FRIENDLY_NAME): cv.string,
+        vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+        vol.Optional(CONF_COMMAND_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+        vol.Optional(CONF_UNIQUE_ID): cv.string,
+    }
+)
+NOTIFY_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_COMMAND): cv.string,
+        vol.Optional(CONF_NAME): cv.string,
+        vol.Optional(CONF_COMMAND_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+    }
+)
+SENSOR_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_COMMAND): cv.string,
+        vol.Optional(CONF_COMMAND_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+        vol.Optional(CONF_JSON_ATTRIBUTES): cv.ensure_list_csv,
+        vol.Optional(CONF_NAME, default=SENSOR_DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_UNIT_OF_MEASUREMENT): cv.string,
+        vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+        vol.Optional(CONF_UNIQUE_ID): cv.string,
+        vol.Optional(CONF_DEVICE_CLASS): SENSOR_DEVICE_CLASSES_SCHEMA,
+        vol.Optional(CONF_STATE_CLASS): SENSOR_STATE_CLASSES_SCHEMA,
+    }
+)
+SWITCH_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_COMMAND_OFF, default="true"): cv.string,
+        vol.Optional(CONF_COMMAND_ON, default="true"): cv.string,
+        vol.Optional(CONF_COMMAND_STATE): cv.string,
+        vol.Optional(CONF_FRIENDLY_NAME): cv.string,
+        vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+        vol.Optional(CONF_ICON_TEMPLATE): cv.template,
+        vol.Optional(CONF_COMMAND_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+        vol.Optional(CONF_UNIQUE_ID): cv.string,
+    }
+)
+COMBINED_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_BINARY_SENSORS): cv.schema_with_slug_keys(
+            CONF_BINARY_SENSORS
+        ),
+        vol.Required(CONF_COVERS): cv.schema_with_slug_keys(CONF_COVERS),
+        vol.Required(CONF_NOTIFIERS): cv.schema_with_slug_keys(CONF_NOTIFIERS),
+        vol.Required(CONF_SENSORS): cv.schema_with_slug_keys(CONF_SENSORS),
+        vol.Required(CONF_SWITCHES): cv.schema_with_slug_keys(SWITCH_SCHEMA),
+    }
+)
+CONFIG_SCHEMA = vol.Schema(
+    {vol.Optional(DOMAIN): COMBINED_SCHEMA},
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up Command Line from yaml config."""
+    command_line_config: dict[str, dict[str, Any]] = config.get(DOMAIN, {})
+    if not command_line_config:
+        return True
+
+    load_coroutines: list[Coroutine[Any, Any, None]] = []
+    for platform, configs in command_line_config.items():
+        for object_id, object_config in configs.items():
+            load_coroutines.append(
+                discovery.async_load_platform(
+                    hass,
+                    PLATFORM_MAPPING[platform],
+                    DOMAIN,
+                    {"object_id": object_id, "config": object_config},
+                    config,
+                )
+            )
+
+    if load_coroutines:
+        await asyncio.gather(*load_coroutines)
+
+    return True
