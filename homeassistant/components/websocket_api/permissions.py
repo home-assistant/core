d0cf693208993eb8699eea5964036298740c38d4
J. Nick Koston <nick@koston.org>
2023-05-08 18:28:22 -0500

    fixes

--- homeassistant/components/websocket_api/permissions.py
+++ homeassistant/components/websocket_api/permissions.py
@@ -8,9 +8,6 @@ from typing import Final
 
 from homeassistant.components.frontend import EVENT_PANELS_UPDATED
 from homeassistant.components.lovelace import EVENT_LOVELACE_UPDATED
-from homeassistant.components.persistent_notification import (
-    EVENT_PERSISTENT_NOTIFICATIONS_UPDATED,
-)
 from homeassistant.components.recorder import (
     EVENT_RECORDER_5MIN_STATISTICS_GENERATED,
     EVENT_RECORDER_HOURLY_STATISTICS_GENERATED,
@@ -38,7 +35,6 @@ SUBSCRIBE_ALLOWLIST: Final[set[str]] = {
     EVENT_ENTITY_REGISTRY_UPDATED,
     EVENT_LOVELACE_UPDATED,
     EVENT_PANELS_UPDATED,
-    EVENT_PERSISTENT_NOTIFICATIONS_UPDATED,
     EVENT_RECORDER_5MIN_STATISTICS_GENERATED,
     EVENT_RECORDER_HOURLY_STATISTICS_GENERATED,
     EVENT_SERVICE_REGISTERED,
