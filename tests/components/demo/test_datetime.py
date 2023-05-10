d38e56bb2e219aa0ef86353af6b7ebea60abe40e
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-05-09 22:50:39 -0400

    Fix test

--- tests/components/demo/test_datetime.py
+++ tests/components/demo/test_datetime.py
@@ -1,12 +1,7 @@
 """The tests for the demo datetime component."""
 import pytest
 
-from homeassistant.components.datetime import (
-    ATTR_DATETIME,
-    ATTR_TIME_ZONE,
-    DOMAIN,
-    SERVICE_SET_VALUE,
-)
+from homeassistant.components.datetime import ATTR_DATETIME, DOMAIN, SERVICE_SET_VALUE
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
@@ -29,14 +24,11 @@ def test_setup_params(hass: HomeAssistant) -> None:
 
 async def test_set_datetime(hass: HomeAssistant) -> None:
     """Test set datetime service."""
+    hass.config.set_time_zone("UTC")
     await hass.services.async_call(
         DOMAIN,
         SERVICE_SET_VALUE,
-        {
-            ATTR_ENTITY_ID: ENTITY_DATETIME,
-            ATTR_DATETIME: "2021-02-03 01:02:03",
-            ATTR_TIME_ZONE: "UTC",
-        },
+        {ATTR_ENTITY_ID: ENTITY_DATETIME, ATTR_DATETIME: "2021-02-03 01:02:03"},
         blocking=True,
     )
     state = hass.states.get(ENTITY_DATETIME)
