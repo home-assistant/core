c21a1e0798cf8b043bf9a6277dc0b79e38a57a71
Maciej Bieniek <bieniu@users.noreply.github.com>
2023-05-20 11:00:45 +0200

    Bump accuweather to version 1.0.0

--- homeassistant/components/accuweather/__init__.py
+++ homeassistant/components/accuweather/__init__.py
@@ -125,7 +125,9 @@ class AccuWeatherDataUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
             async with timeout(10):
                 current = await self.accuweather.async_get_current_conditions()
                 forecast = (
-                    await self.accuweather.async_get_forecast() if self.forecast else {}
+                    await self.accuweather.async_get_daily_forecast()
+                    if self.forecast
+                    else {}
                 )
         except (
             ApiError,
