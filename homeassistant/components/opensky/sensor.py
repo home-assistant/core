6234ca6a2fcef39ae88faf72e44e789605e7cc35
joostlek <joostlek@outlook.com>
2023-05-23 22:21:11 +0200

    Move function to library

--- homeassistant/components/opensky/sensor.py
+++ homeassistant/components/opensky/sensor.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 from datetime import timedelta
-import math
 
 from python_opensky import BoundingBox, OpenSky
 import voluptuous as vol
@@ -21,7 +20,6 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-from homeassistant.util.location import AXIS_A, FLATTENING
 
 CONF_ALTITUDE = "altitude"
 
@@ -70,124 +68,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-def calculate_point(
-    latitude: float, longitude: float, distance: float, degrees: float
-) -> tuple[float, float]:
-    """Calculate a point from an origin point, direction in degrees and distance."""
-    # pylint: disable=invalid-name
-    piD4 = math.atan(1.0)
-    two_pi = piD4 * 8.0
-    latitude = latitude * piD4 / 45.0
-    longitude = longitude * piD4 / 45.0
-    degrees = degrees * piD4 / 45.0
-    if degrees < 0.0:
-        degrees = degrees + two_pi
-    if degrees > two_pi:
-        degrees = degrees - two_pi
-    AXIS_B = AXIS_A * (1.0 - FLATTENING)
-    TanU1 = (1 - FLATTENING) * math.tan(latitude)
-    U1 = math.atan(TanU1)
-    sigma1 = math.atan2(TanU1, math.cos(degrees))
-    Sinalpha = math.cos(U1) * math.sin(degrees)
-    cosalpha_sq = 1.0 - Sinalpha * Sinalpha
-    u2 = cosalpha_sq * (AXIS_A * AXIS_A - AXIS_B * AXIS_B) / (AXIS_B * AXIS_B)
-    A = 1.0 + (u2 / 16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
-    B = (u2 / 1024) * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
-    # Starting with the approx
-    sigma = distance / (AXIS_B * A)
-    last_sigma = 2.0 * sigma + 2.0  # something impossible
-
-    # Iterate the following 3 eqs until no sig change in sigma
-    # two_sigma_m , delta_sigma
-    while abs((last_sigma - sigma) / sigma) > 1.0e-9:
-        two_sigma_m = 2 * sigma1 + sigma
-        delta_sigma = (
-            B
-            * math.sin(sigma)
-            * (
-                math.cos(two_sigma_m)
-                + (B / 4)
-                * (
-                    math.cos(sigma)
-                    * (
-                        -1
-                        + 2 * math.pow(math.cos(two_sigma_m), 2)
-                        - (B / 6)
-                        * math.cos(two_sigma_m)
-                        * (-3 + 4 * math.pow(math.sin(sigma), 2))
-                        * (-3 + 4 * math.pow(math.cos(two_sigma_m), 2))
-                    )
-                )
-            )
-        )
-        last_sigma = sigma
-        sigma = (distance / (AXIS_B * A)) + delta_sigma
-    phi2 = math.atan2(
-        (
-            math.sin(U1) * math.cos(sigma)
-            + math.cos(U1) * math.sin(sigma) * math.cos(degrees)
-        ),
-        (
-            (1 - FLATTENING)
-            * math.sqrt(
-                math.pow(Sinalpha, 2)
-                + pow(
-                    math.sin(U1) * math.sin(sigma)
-                    - math.cos(U1) * math.cos(sigma) * math.cos(degrees),
-                    2,
-                )
-            )
-        ),
-    )
-    lembda = math.atan2(
-        (math.sin(sigma) * math.sin(degrees)),
-        (
-            math.cos(U1) * math.cos(sigma)
-            - math.sin(U1) * math.sin(sigma) * math.cos(degrees)
-        ),
-    )
-    C = (FLATTENING / 16) * cosalpha_sq * (4 + FLATTENING * (4 - 3 * cosalpha_sq))
-    omega = lembda - (1 - C) * FLATTENING * Sinalpha * (
-        sigma
-        + C
-        * math.sin(sigma)
-        * (
-            math.cos(two_sigma_m)
-            + C * math.cos(sigma) * (-1 + 2 * math.pow(math.cos(two_sigma_m), 2))
-        )
-    )
-    lembda2 = longitude + omega
-    alpha21 = math.atan2(
-        Sinalpha,
-        (
-            -math.sin(U1) * math.sin(sigma)
-            + math.cos(U1) * math.cos(sigma) * math.cos(degrees)
-        ),
-    )
-    alpha21 = alpha21 + two_pi / 2.0
-    if alpha21 < 0.0:
-        alpha21 = alpha21 + two_pi
-    if alpha21 > two_pi:
-        alpha21 = alpha21 - two_pi
-    phi2 = phi2 * 45.0 / piD4
-    lembda2 = lembda2 * 45.0 / piD4
-    return phi2, lembda2
-
-
-def get_bounding_box(latitude: float, longitude: float, radius: float) -> BoundingBox:
-    """Get bounding box from radius and a point."""
-    north = calculate_point(latitude, longitude, radius, 0)
-    east = calculate_point(latitude, longitude, radius, 90)
-    south = calculate_point(latitude, longitude, radius, 180)
-    west = calculate_point(latitude, longitude, radius, 270)
-    return BoundingBox(
-        min_latitude=min(north[0], south[0]) + latitude,
-        max_latitude=max(north[0], south[0]) + latitude,
-        min_longitude=min(east[1], west[1]) + longitude,
-        max_longitude=max(east[1], west[1]) + longitude,
-    )
-
-
 def setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
@@ -198,7 +78,7 @@ def setup_platform(
     latitude = config.get(CONF_LATITUDE, hass.config.latitude)
     longitude = config.get(CONF_LONGITUDE, hass.config.longitude)
     radius = config.get(CONF_RADIUS, 0)
-    bounding_box = get_bounding_box(latitude, longitude, radius)
+    bounding_box = OpenSky.get_bounding_box(latitude, longitude, radius)
     session = async_get_clientsession(hass)
     opensky = OpenSky(session=session)
     add_entities(
