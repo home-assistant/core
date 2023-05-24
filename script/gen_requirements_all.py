12536713bf54c038dfe68d63528822f7d764a216
J. Nick Koston <nick@koston.org>
2023-05-24 16:27:16 -0500

    Add constraint for pyminiaudio to 1.57
    https://github.com/irmen/pyminiaudio/issues/67

--- script/gen_requirements_all.py
+++ script/gen_requirements_all.py
@@ -182,7 +182,7 @@ pysnmp==1000000000.0.0
 
 # pyminiaudio 1.58 is missing files in the package
 # https://github.com/irmen/pyminiaudio/issues/67
-pyminiaudio==1.57
+miniaudio==1.57
 """
 
 IGNORE_PRE_COMMIT_HOOK_ID = (
