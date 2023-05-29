1455fa967d7f6f7fa59134be04cf10d93eaedfad
jbouwh <jan@jbsoft.nl>
2023-05-29 11:30:16 +0000

    Improve test

--- tests/components/mqtt/test_init.py
+++ tests/components/mqtt/test_init.py
@@ -23,6 +23,8 @@ from homeassistant.const import (
     EVENT_HOMEASSISTANT_STARTED,
     EVENT_HOMEASSISTANT_STOP,
     SERVICE_RELOAD,
+    STATE_UNAVAILABLE,
+    STATE_UNKNOWN,
     Platform,
     UnitOfTemperature,
 )
@@ -3739,45 +3741,62 @@ async def test_link_config_entry(
     assert _check_entities() == 2
 
 
+@pytest.mark.parametrize(
+    "hass_config",
+    [
+        {
+            "mqtt": {
+                "sensor": [
+                    {
+                        "name": "test_manual1",
+                        "unique_id": "test_manual_unique_id123",
+                        "state_topic": "test-topic_manual1",
+                    },
+                    {
+                        "name": "test_manual3",
+                        "unique_id": "test_manual_unique_id789",
+                        "state_topic": "test-topic_manual3",
+                    },
+                ]
+            }
+        }
+    ],
+)
 async def test_reload_config_entry(
     hass: HomeAssistant,
+    mqtt_mock_entry: MqttMockHAClientGenerator,
     mqtt_client_mock: MqttMockPahoClient,
     record_calls: MessageCallbackType,
     calls: list[ReceiveMessage],
 ) -> None:
     """Test manual entities reloaded and set up correctly."""
+    await mqtt_mock_entry()
 
-    # set up initial manual items
-    hass_config_initial = {
-        "mqtt": {
-            "sensor": [
-                {
-                    "name": "test_manual1",
-                    "unique_id": "test_manual_unique_id123",
-                    "state_topic": "test-topic_manual1",
-                },
-                {
-                    "name": "test_manual3",
-                    "unique_id": "test_manual_unique_id789",
-                    "state_topic": "test-topic_manual3",
-                },
-            ]
-        }
-    }
-    entry = MockConfigEntry(domain=mqtt.DOMAIN, data={mqtt.CONF_BROKER: "test-broker"})
-    entry.add_to_hass(hass)
-    mqtt_client_mock.connect.return_value = 0
-    with patch(
-        "homeassistant.config.load_yaml_config_file", return_value=hass_config_initial
-    ):
-        await entry.async_setup(hass)
+    entry = hass.config_entries.async_entries(mqtt.DOMAIN)[0]
+
+    def _check_entities() -> int:
+        entities: list[Entity] = []
+        mqtt_platforms = async_get_platforms(hass, mqtt.DOMAIN)
+        for mqtt_platform in mqtt_platforms:
+            assert mqtt_platform.config_entry is entry
+            entities += (entity for entity in mqtt_platform.entities.values())
+
+        return len(entities)
+
+    # assert on initial set up manual items
+
+    async_fire_mqtt_message(hass, "test-topic_manual1", "manual1_intial")
+    async_fire_mqtt_message(hass, "test-topic_manual3", "manual3_intial")
 
     assert (state := hass.states.get("sensor.test_manual1")) is not None
     assert state.attributes["friendly_name"] == "test_manual1"
-    assert hass.states.get("sensor.test_manual2") is None
+    assert state.state == "manual1_intial"
     assert (state := hass.states.get("sensor.test_manual3")) is not None
     assert state.attributes["friendly_name"] == "test_manual3"
+    assert state.state == "manual3_intial"
+    assert _check_entities() == 2
 
+    # Reload the entry with a new configuration.yaml
     # Mock configuration.yaml was updated
     # The first item was updated, a new item was added, an item was removed
     hass_config_new = {
@@ -3796,19 +3815,6 @@ async def test_reload_config_entry(
             ]
         }
     }
-
-    def _check_entities() -> int:
-        entities: list[Entity] = []
-        mqtt_platforms = async_get_platforms(hass, mqtt.DOMAIN)
-        for mqtt_platform in mqtt_platforms:
-            assert mqtt_platform.config_entry is entry
-            entities += (entity for entity in mqtt_platform.entities.values())
-
-        return len(entities)
-
-    assert _check_entities() == 2
-
-    # Reload the entry
     with patch(
         "homeassistant.config.load_yaml_config_file", return_value=hass_config_new
     ):
@@ -3818,24 +3824,58 @@ async def test_reload_config_entry(
 
     assert (state := hass.states.get("sensor.test_manual1")) is not None
     assert state.attributes["friendly_name"] == "test_manual1_updated"
+    assert state.state == STATE_UNKNOWN
     assert (state := hass.states.get("sensor.test_manual2_new")) is not None
     assert state.attributes["friendly_name"] == "test_manual2_new"
-    # assert (state := hass.states.get("sensor.test_manual3")) is None
+    assert state.state is STATE_UNKNOWN
+    # State of test_manual3 is still loaded but is unavailable
+    assert (state := hass.states.get("sensor.test_manual3")) is not None
+    assert state.state is STATE_UNAVAILABLE
+    # The entity is not loaded anymore
     assert _check_entities() == 2
 
+    async_fire_mqtt_message(hass, "test-topic_manual1_updated", "manual1_update")
+    async_fire_mqtt_message(hass, "test-topic_manual2", "manual2_update")
+    async_fire_mqtt_message(hass, "test-topic_manual3", "manual3_update")
+
+    assert (state := hass.states.get("sensor.test_manual1")) is not None
+    assert state.state == "manual1_update"
+    assert (state := hass.states.get("sensor.test_manual2_new")) is not None
+    assert state.state == "manual2_update"
+    assert (state := hass.states.get("sensor.test_manual3")) is not None
+    assert state.state is STATE_UNAVAILABLE
+
     # Reload manual configured items and assert again
-    await hass.services.async_call(
-        "mqtt",
-        SERVICE_RELOAD,
-        {},
-        blocking=True,
-    )
-    await hass.async_block_till_done()
+    with patch(
+        "homeassistant.config.load_yaml_config_file", return_value=hass_config_new
+    ):
+        await hass.services.async_call(
+            "mqtt",
+            SERVICE_RELOAD,
+            {},
+            blocking=True,
+        )
+        await hass.async_block_till_done()
 
     assert (state := hass.states.get("sensor.test_manual1")) is not None
     assert state.attributes["friendly_name"] == "test_manual1_updated"
+    assert state.state == STATE_UNKNOWN
     assert (state := hass.states.get("sensor.test_manual2_new")) is not None
     assert state.attributes["friendly_name"] == "test_manual2_new"
-    # assert (state := hass.states.get("sensor.test_manual3")) is None
-
+    assert state.state == STATE_UNKNOWN
+    assert (state := hass.states.get("sensor.test_manual3")) is not None
+    assert state.state == STATE_UNAVAILABLE
     assert _check_entities() == 2
+
+    async_fire_mqtt_message(
+        hass, "test-topic_manual1_updated", "manual1_update_after_reload"
+    )
+    async_fire_mqtt_message(hass, "test-topic_manual2", "manual2_update_after_reload")
+    async_fire_mqtt_message(hass, "test-topic_manual3", "manual3_update_after_reload")
+
+    assert (state := hass.states.get("sensor.test_manual1")) is not None
+    assert state.state == "manual1_update_after_reload"
+    assert (state := hass.states.get("sensor.test_manual2_new")) is not None
+    assert state.state == "manual2_update_after_reload"
+    assert (state := hass.states.get("sensor.test_manual3")) is not None
+    assert state.state is STATE_UNAVAILABLE
