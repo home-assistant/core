35d2873b20a5f4fdedd2e10beda209765215dd82
David Knowles <dknowles2@gmail.com>
2023-05-30 22:57:50 +0000

    Raise ConfigEntryNotReady on init auth failures.

--- homeassistant/components/schlage/__init__.py
+++ homeassistant/components/schlage/__init__.py
@@ -1,11 +1,13 @@
 """The Schlage integration."""
 from __future__ import annotations
 
+from pycognito.exceptions import WarrantException
 import pyschlage
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN
 
@@ -16,9 +18,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Schlage from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
-    auth = await hass.async_add_executor_job(
-        pyschlage.Auth, entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD]
-    )
+    try:
+        auth = await hass.async_add_executor_job(
+            pyschlage.Auth, entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD]
+        )
+    except WarrantException as ex:
+        raise ConfigEntryNotReady from ex
+
     hass.data[DOMAIN][entry.entry_id] = pyschlage.Schlage(auth)
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
