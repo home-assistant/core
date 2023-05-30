21ab6c25a270ac3c09773b964455f15f6b1bc249
antonverburg <info@tec-V.nl>
2023-05-30 11:32:58 +0200

    Add check for dict type.

--- homeassistant/helpers/reload.py
+++ homeassistant/helpers/reload.py
@@ -82,10 +82,11 @@ async def _resetup_platform(
         # Check if it's a multi-platform config
         added = False
         for item in conf[integration_name]:
-            if item.get(integration_platform):  # Config for current platform
-                added = True
-                root_config[integration_platform].extend(item[integration_platform])
-        # If still  no valid item was found, it's a simple single platform config
+            if isinstance(item, dict):
+                if item.get(integration_platform):  # Config for current platform
+                    added = True
+                    root_config[integration_platform].extend(item[integration_platform])
+        # If no valid item was found, it's a simple single platform config
         if not added:
             root_config[integration_platform].extend(conf[integration_name])
 
