cda95d8b2292c8281431da443e148d2fbf400387
jbouwh <jan@jbsoft.nl>
2023-05-30 09:17:14 +0000

    Remove mqtt sensor support for `last_reset_topic`

--- tests/components/mqtt/test_sensor.py
+++ tests/components/mqtt/test_sensor.py
@@ -480,101 +480,8 @@ async def test_setting_sensor_value_via_mqtt_json_message_and_default_current_st
                     "state_class": "total",
                     "state_topic": "test-topic",
                     "unit_of_measurement": "fav unit",
-                    "last_reset_topic": "last-reset-topic",
-                }
-            }
-        }
-    ],
-)
-async def test_setting_sensor_last_reset_via_mqtt_message(
-    hass: HomeAssistant,
-    mqtt_mock_entry: MqttMockHAClientGenerator,
-    caplog: pytest.LogCaptureFixture,
-) -> None:
-    """Test the setting of the last_reset property via MQTT."""
-    await mqtt_mock_entry()
-
-    async_fire_mqtt_message(hass, "last-reset-topic", "2020-01-02 08:11:00")
-    state = hass.states.get("sensor.test")
-    assert state.attributes.get("last_reset") == "2020-01-02T08:11:00"
-    assert "'last_reset_topic' must be same as 'state_topic'" in caplog.text
-    assert (
-        "'last_reset_value_template' must be set if 'last_reset_topic' is set"
-        in caplog.text
-    )
-
-
-@pytest.mark.parametrize(
-    "hass_config",
-    [
-        {
-            mqtt.DOMAIN: {
-                sensor.DOMAIN: {
-                    "name": "test",
-                    "state_class": "total",
-                    "state_topic": "test-topic",
-                    "unit_of_measurement": "fav unit",
-                    "last_reset_topic": "last-reset-topic",
-                }
-            }
-        }
-    ],
-)
-@pytest.mark.parametrize("datestring", ["2020-21-02 08:11:00", "Hello there!"])
-async def test_setting_sensor_bad_last_reset_via_mqtt_message(
-    hass: HomeAssistant,
-    caplog: pytest.LogCaptureFixture,
-    datestring,
-    mqtt_mock_entry: MqttMockHAClientGenerator,
-) -> None:
-    """Test the setting of the last_reset property via MQTT."""
-    await mqtt_mock_entry()
-
-    async_fire_mqtt_message(hass, "last-reset-topic", datestring)
-    state = hass.states.get("sensor.test")
-    assert state.attributes.get("last_reset") is None
-    assert "Invalid last_reset message" in caplog.text
-
-
-@pytest.mark.parametrize(
-    "hass_config",
-    [
-        {
-            mqtt.DOMAIN: {
-                sensor.DOMAIN: {
-                    "name": "test",
-                    "state_class": "total",
-                    "state_topic": "test-topic",
-                    "unit_of_measurement": "fav unit",
-                    "last_reset_topic": "last-reset-topic",
-                }
-            }
-        }
-    ],
-)
-async def test_setting_sensor_empty_last_reset_via_mqtt_message(
-    hass: HomeAssistant, mqtt_mock_entry: MqttMockHAClientGenerator
-) -> None:
-    """Test the setting of the last_reset property via MQTT."""
-    await mqtt_mock_entry()
-
-    async_fire_mqtt_message(hass, "last-reset-topic", "")
-    state = hass.states.get("sensor.test")
-    assert state.attributes.get("last_reset") is None
-
-
-@pytest.mark.parametrize(
-    "hass_config",
-    [
-        {
-            mqtt.DOMAIN: {
-                sensor.DOMAIN: {
-                    "name": "test",
-                    "state_class": "total",
-                    "state_topic": "test-topic",
-                    "unit_of_measurement": "fav unit",
-                    "last_reset_topic": "last-reset-topic",
                     "last_reset_value_template": "{{ value_json.last_reset }}",
+                    "value_template": "{{ value_json.state }}",
                 }
             }
         }
@@ -587,10 +494,18 @@ async def test_setting_sensor_last_reset_via_mqtt_json_message(
     await mqtt_mock_entry()
 
     async_fire_mqtt_message(
-        hass, "last-reset-topic", '{ "last_reset": "2020-01-02 08:11:00" }'
+        hass, "test-topic", '{ "last_reset": "2020-01-02 08:11:00" }'
     )
     state = hass.states.get("sensor.test")
     assert state.attributes.get("last_reset") == "2020-01-02T08:11:00"
+    assert state.state == STATE_UNKNOWN
+
+    async_fire_mqtt_message(
+        hass, "test-topic", '{ "last_reset": "2020-01-02 08:11:03", "state": 10.0 }'
+    )
+    state = hass.states.get("sensor.test")
+    assert state.attributes.get("last_reset") == "2020-01-02T08:11:03"
+    assert state.state == "10.0"
 
 
 @pytest.mark.parametrize(
@@ -605,19 +520,6 @@ async def test_setting_sensor_last_reset_via_mqtt_json_message(
                     "unit_of_measurement": "kWh",
                     "value_template": "{{ value_json.value | float / 60000 }}",
                     "last_reset_value_template": "{{ utcnow().fromtimestamp(value_json.time / 1000, tz=utcnow().tzinfo) }}",
-                },
-            }
-        },
-        {
-            mqtt.DOMAIN: {
-                sensor.DOMAIN: {
-                    "name": "test",
-                    "state_class": "total",
-                    "state_topic": "test-topic",
-                    "unit_of_measurement": "kWh",
-                    "value_template": "{{ value_json.value | float / 60000 }}",
-                    "last_reset_value_template": "{{ utcnow().fromtimestamp(value_json.time / 1000, tz=utcnow().tzinfo) }}",
-                    "last_reset_topic": "test-topic",
                 },
             }
         },
@@ -640,11 +542,6 @@ async def test_setting_sensor_last_reset_via_mqtt_json_message_2(
     state = hass.states.get("sensor.test")
     assert float(state.state) == pytest.approx(0.015796176944444445)
     assert state.attributes.get("last_reset") == "2021-08-19T15:05:00+00:00"
-    assert "'last_reset_topic' must be same as 'state_topic'" not in caplog.text
-    assert (
-        "'last_reset_value_template' must be set if 'last_reset_topic' is set"
-        not in caplog.text
-    )
 
 
 @pytest.mark.parametrize(
