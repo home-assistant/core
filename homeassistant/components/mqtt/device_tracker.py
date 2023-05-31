4c5545d160ce937d149e2ce0b1758352ed89ac61
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-05-31 09:02:48 +0200

    Remove MQTT platform schema warnings (#93785)

--- homeassistant/components/mqtt/device_tracker.py
+++ homeassistant/components/mqtt/device_tracker.py
@@ -36,7 +36,6 @@ from .mixins import (
     MQTT_ENTITY_COMMON_SCHEMA,
     MqttEntity,
     async_setup_entry_helper,
-    warn_for_legacy_schema,
 )
 from .models import MqttValueTemplate, ReceiveMessage, ReceivePayloadType
 from .util import get_mqtt_data, valid_subscribe_topic
@@ -77,11 +76,6 @@ PLATFORM_SCHEMA_MODERN = vol.All(PLATFORM_SCHEMA_MODERN_BASE, valid_config)
 DISCOVERY_SCHEMA = vol.All(
     PLATFORM_SCHEMA_MODERN_BASE.extend({}, extra=vol.REMOVE_EXTRA), valid_config
 )
-
-# Configuring MQTT Device Trackers under the device_tracker platform key was deprecated
-# in HA Core 2022.6
-# Setup for the legacy YAML format was removed in HA Core 2022.12
-PLATFORM_SCHEMA = vol.All(warn_for_legacy_schema(device_tracker.DOMAIN))
 
 
 async def async_setup_entry(
