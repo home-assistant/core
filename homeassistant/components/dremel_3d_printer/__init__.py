d3a68634c758903b8d9a801711f49968e6606da9
Robert Hillis <tkdrob4390@yahoo.com>
2023-05-31 18:26:26 -0400

    Add camera platform to Dremel

--- homeassistant/components/dremel_3d_printer/__init__.py
+++ homeassistant/components/dremel_3d_printer/__init__.py
@@ -1,6 +1,8 @@
 """The Dremel 3D Printer (3D20, 3D40, 3D45) integration."""
 from __future__ import annotations
 
+from copy import deepcopy
+
 from dremel3dpy import Dremel3DPrinter
 from requests.exceptions import ConnectTimeout, HTTPError
 
@@ -12,7 +14,7 @@ from homeassistant.exceptions import ConfigEntryNotReady
 from .const import DOMAIN
 from .coordinator import Dremel3DPrinterDataUpdateCoordinator
 
-PLATFORMS = [Platform.SENSOR]
+PLATFORMS = [Platform.CAMERA, Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
@@ -30,7 +32,10 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     coordinator = Dremel3DPrinterDataUpdateCoordinator(hass, api)
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
-    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+    platforms = deepcopy(PLATFORMS)
+    if api.get_model() != "3D45":
+        platforms.remove(Platform.CAMERA)
+    await hass.config_entries.async_forward_entry_setups(config_entry, platforms)
     return True
 
 
