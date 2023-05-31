27c5db293039f87268612dc9c98ace625e464131
Paul Frank <pail@gmx.net>
2023-05-31 20:58:51 +0000

    fix missing import

--- homeassistant/components/mystrom/models.py
+++ homeassistant/components/mystrom/models.py
@@ -1,5 +1,6 @@
 """Models for the mystrom integration."""
 from dataclasses import dataclass
+from typing import Any
 
 from pymystrom.bulb import MyStromBulb
 from pymystrom.switch import MyStromSwitch
