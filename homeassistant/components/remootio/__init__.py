4788e55b43e8c75eaa71a8120c4054227b1a1612
Ilyes-Veisz, G. Gabor <ivgg@ivgg.me>
2023-05-31 14:19:14 +0200

    Add Remootio integration.

--- homeassistant/components/remootio/__init__.py
+++ homeassistant/components/remootio/__init__.py
@@ -6,16 +6,19 @@ import logging
 from aioremootio import ConnectionOptions, RemootioClient
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.const import ATTR_ENTITY_ID, ATTR_NAME, CONF_HOST, Platform
+from homeassistant.core import HomeAssistant, callback
 
 from .const import (
+    ATTR_SERIAL_NUMBER,
     CONF_API_AUTH_KEY,
     CONF_API_SECRET_KEY,
     CONF_SERIAL_NUMBER,
     DOMAIN,
+    EVENT_HANDLER_CALLBACK,
     REMOOTIO_CLIENT,
 )
+from .cover import RemootioCoverEvent
 from .utils import create_client
 
 _LOGGER = logging.getLogger(__name__)
@@ -27,6 +30,24 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Remootio from a config entry."""
 
     _LOGGER.debug("Doing async_setup_entry. entry [%s]", entry.as_dict())
+
+    @callback
+    def handle_event(event: RemootioCoverEvent) -> None:
+        _LOGGER.debug(
+            "Firing event. EvenType [%s] RemootioCoverEntityId [%s] RemootioDeviceSerialNumber [%s]",
+            event.type,
+            event.entity_id,
+            event.device_serial_number,
+        )
+
+        hass.bus.async_fire(
+            event.type,
+            {
+                ATTR_ENTITY_ID: event.entity_id,
+                ATTR_SERIAL_NUMBER: event.device_serial_number,
+                ATTR_NAME: event.entity_name,
+            },
+        )
 
     connection_options: ConnectionOptions = ConnectionOptions(
         entry.data[CONF_HOST],
@@ -41,6 +62,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass_data = hass.data.setdefault(DOMAIN, {}).setdefault(entry.entry_id, {})
     hass_data[REMOOTIO_CLIENT] = remootio_client
+    hass_data[EVENT_HANDLER_CALLBACK] = handle_event
 
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
