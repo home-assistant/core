17986eac99472229290eb78c8c3eec69eab91442
Manuel Dipolt <manuel@linux-home.at>
2023-05-31 23:21:10 +0200

    code review changes

--- homeassistant/components/romy/__init__.py
+++ homeassistant/components/romy/__init__.py
@@ -6,12 +6,12 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PASSWORD
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, LOGGER, PLATFORMS
+from .const import DOMAIN, PLATFORMS
 from .coordinator import RomyVacuumCoordinator
 
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    """TODO."""
+    """Initialize the ROMY platform via config entry."""
 
     password = ""
     if CONF_PASSWORD in config_entry.data:
@@ -22,13 +22,10 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     name = config_entry.data[CONF_NAME]
     if name != new_romy.name:
         await new_romy.set_name(name)
-        LOGGER.info("Settings ROMY's name to: %s", new_romy.name)
 
     coordinator = RomyVacuumCoordinator(hass, new_romy)
     await coordinator.async_config_entry_first_refresh()
 
-    # hass.data.setdefault(DOMAIN, {})
-    # hass.data[DOMAIN][config_entry.entry_id] = coordinator
     hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
