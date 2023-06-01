08993792a92535a08bf89ec10aab761d40479e79
Paul Frank <pail@gmx.net>
2023-06-01 08:45:10 +0200

    Update homeassistant/components/mystrom/config_flow.py
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>

--- homeassistant/components/mystrom/config_flow.py
+++ homeassistant/components/mystrom/config_flow.py
@@ -61,5 +61,3 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
 
 
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
