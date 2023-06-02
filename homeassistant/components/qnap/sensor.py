2d09295fe2205be566ddd51d50e6a4fd27279cb0
disforw <github@benzona.net>
2023-06-02 01:41:50 -0400

    Update sensor.py

--- homeassistant/components/qnap/sensor.py
+++ homeassistant/components/qnap/sensor.py
@@ -1,6 +1,7 @@
 """Support for QNAP NAS Sensors."""
 import logging
 
+from homeassistant import config_entries
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
