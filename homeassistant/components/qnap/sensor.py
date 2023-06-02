80e46e391418112015705202b090943fc4fe0205
disforw <github@benzona.net>
2023-06-02 01:30:40 -0400

    Update sensor.py

--- homeassistant/components/qnap/sensor.py
+++ homeassistant/components/qnap/sensor.py
@@ -44,7 +44,7 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    config_entry: ConfigType,
+    config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up entry."""
