84274cb4cef2313d36e382f6eee6ba8725d8bd9f
disforw <github@benzona.net>
2023-06-01 23:05:40 -0400

    Update __init__.py

--- homeassistant/components/qnap/__init__.py
+++ homeassistant/components/qnap/__init__.py
@@ -22,13 +22,11 @@ UPDATE_INTERVAL = timedelta(minutes=1)
 
 _LOGGER = logging.getLogger(__name__)
 
-
 async def async_setup(hass, config):
     """Set up the qnap environment."""
     hass.data.setdefault(DOMAIN, {})
 
     return True
-
 
 async def async_setup_entry(hass, config_entry):
     """Set the config entry up."""
