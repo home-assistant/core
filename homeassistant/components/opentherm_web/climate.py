28853d2a7686b054e84bd396f94a87e0ab7a67d8
Erik Dresto <erik.dresto@gmail.com>
2023-06-03 19:38:23 +0000

    Bug fixes to make entities visible/functional

--- homeassistant/components/opentherm_web/climate.py
+++ homeassistant/components/opentherm_web/climate.py
@@ -1,9 +1,17 @@
 """Platform for climate integration."""
 from __future__ import annotations
 
-from homeassistant.components.climate import ClimateEntity
+from typing import Any
+
+from homeassistant.components.climate import (
+    ClimateEntity,
+    ClimateEntityFeature,
+    HVACMode,
+)
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import ATTR_TEMPERATURE, UnitOfTemperature
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
@@ -28,6 +36,12 @@ async def async_setup_entry(
 class OpenThermClimate(ClimateEntity):
     """Class that represents Climate entity."""
 
+    _attr_hvac_modes = [HVACMode.AUTO, HVACMode.OFF]
+    _attr_hvac_mode = HVACMode.AUTO
+    _attr_target_temperature_step = 0.1
+    _attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
+    _attr_temperature_unit = UnitOfTemperature.CELSIUS
+
     controller: OpenThermController
 
     def __init__(
@@ -36,3 +50,40 @@ class OpenThermClimate(ClimateEntity):
     ) -> None:
         """Initialize."""
         self.controller = controller
+        self._attr_unique_id = f"climate_{controller.device_id}"
+        self._attr_name = "Thermostat"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, controller.device_id)},
+            name="OpenThermWeb",
+            manufacturer="Pohorelice",
+        )
+
+    async def async_update(self) -> None:
+        """Get the latest data."""
+        self._attr_current_temperature = self.controller.room_temperature
+        self._attr_target_temperature = self.controller.room_setpoint
+
+        if self.controller.enabled:
+            self._attr_hvac_mode = HVACMode.AUTO
+            if self.controller.chw_active:
+                self._attr_icon = "mdi:radiator"
+            else:
+                self._attr_icon = "mdi:radiator-off"
+        else:
+            self._attr_hvac_mode = HVACMode.OFF
+            self._attr_icon = "mdi:radiator-disabled"
+
+        return
+
+    def set_temperature(self, **kwargs: Any) -> None:
+        """Set new target temperature."""
+        if (temperature := kwargs.get(ATTR_TEMPERATURE)) is None:
+            return
+        self.controller.set_room_temperature(temperature)
+
+    def set_hvac_mode(self, hvac_mode: HVACMode) -> None:
+        """Set HVAC mode."""
+        if hvac_mode not in (HVACMode.AUTO, HVACMode.OFF):
+            return
+
+        self.controller.set_hvac_mode(hvac_mode == HVACMode.AUTO)
--- homeassistant/components/opentherm_web/opentherm_webapi.py
+++ homeassistant/components/opentherm_web/opentherm_webapi.py
@@ -60,14 +60,65 @@ class OpenThermWebApi:
         api_url = urllib.parse.urljoin(self.host, "/opentherm/controller")
         response = requests.get(api_url, headers=headers, timeout=TIMEOUT)
 
-        return OpenThermController(response)
+        return OpenThermController(self, response)
+
+    def push_change(self, controller: OpenThermController) -> None:
+        """Push controller."""
+        token = self.get_token()
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}",
+        }
+
+        api_url = urllib.parse.urljoin(self.host, "/opentherm/controller")
+        data = {
+            "deviceId": controller.device_id,
+            "enabled": controller.enabled,
+            "roomSetpoint": controller.room_setpoint,
+            "dhwSetpoint": controller.dhw_setpoint,
+            "away": controller.away,
+        }
+        requests.put(api_url, headers=headers, json=data, timeout=TIMEOUT)
 
 
 class OpenThermController:
     """Class that represents the data object that holds the data."""
 
-    def __init__(self, response: Response) -> None:
+    web_api: OpenThermWebApi
+
+    def __init__(self, web_api: OpenThermWebApi, response: Response) -> None:
         """Initiatlize."""
+        self.web_api = web_api
         json = response.json()
         self.device_id = json.get("deviceId")
-        self.poller = json.get("poller")
+        self.dhw_setpoint = json.get("dhwSetpoint")
+        self.chw_setpoint = json.get("chwSetpoint")
+        self.room_setpoint = json.get("roomSetpoint")
+        self.away = json.get("away")
+        self.enabled = json.get("enabled")
+        self.chw_temperature = json.get("chwTemperature")
+        self.dhw_temperature = json.get("dhwTemperature")
+        self.room_temperature = json.get("roomTemperature")
+        self.outside_temperature = json.get("outsideTemperature")
+        self.chw_active = json.get("chwActive")
+        self.dhw_active = json.get("dhwActive")
+
+    def set_room_temperature(self, temperature: float) -> None:
+        """Set room temperature."""
+        self.room_setpoint = temperature
+        self.web_api.push_change(self)
+
+    def set_dhw_temperature(self, temperature: float) -> None:
+        """Set domestic hot water temperature."""
+        self.dhw_setpoint = temperature
+        self.web_api.push_change(self)
+
+    def set_away_mode(self, away_mode: bool) -> None:
+        """Set away mode."""
+        self.away = away_mode
+        self.web_api.push_change(self)
+
+    def set_hvac_mode(self, enabled: bool) -> None:
+        """Set HVAC mode."""
+        self.enabled = enabled
+        self.web_api.push_change(self)
