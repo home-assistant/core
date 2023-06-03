ef816cf73e63102bc307e7f4cdf42c620ac57740
G Johansson <goran.johansson@shiftit.se>
2023-06-03 12:49:47 +0000

    platform

--- homeassistant/components/command_line/__init__.py
+++ homeassistant/components/command_line/__init__.py
@@ -202,7 +202,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up a config entry."""
     await hass.config_entries.async_forward_entry_setups(
-        entry, (entry.options["group_type"],)
+        entry, (entry.options["platform"],)
     )
     entry.async_on_unload(entry.add_update_listener(config_entry_update_listener))
     return True
@@ -216,5 +216,5 @@ async def config_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(
-        entry, (entry.options["group_type"],)
+        entry, (entry.options["platform"],)
     )
