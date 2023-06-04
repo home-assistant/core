1efe03d960144aab36465f5ecfc02e9256ca13de
Robert Hillis <tkdrob4390@yahoo.com>
2023-06-04 16:26:29 -0400

    unload and tests

--- homeassistant/components/dremel_3d_printer/__init__.py
+++ homeassistant/components/dremel_3d_printer/__init__.py
@@ -1,8 +1,6 @@
 """The Dremel 3D Printer (3D20, 3D40, 3D45) integration."""
 from __future__ import annotations
 
-from copy import deepcopy
-
 from dremel3dpy import Dremel3DPrinter
 from requests.exceptions import ConnectTimeout, HTTPError
 
@@ -11,7 +9,7 @@ from homeassistant.const import CONF_HOST, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
-from .const import DOMAIN
+from .const import CAMERA_MODEL, DOMAIN
 from .coordinator import Dremel3DPrinterDataUpdateCoordinator
 
 PLATFORMS = [Platform.CAMERA, Platform.SENSOR]
@@ -32,8 +30,8 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     coordinator = Dremel3DPrinterDataUpdateCoordinator(hass, api)
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
-    platforms = deepcopy(PLATFORMS)
-    if api.get_model() != "3D45":
+    platforms = list(PLATFORMS)
+    if api.get_model() != CAMERA_MODEL:
         platforms.remove(Platform.CAMERA)
     await hass.config_entries.async_forward_entry_setups(config_entry, platforms)
     return True
@@ -41,6 +39,10 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Dremel config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data.pop(DOMAIN)
+    platforms = list(PLATFORMS)
+    api: Dremel3DPrinter = hass.data[DOMAIN][entry.entry_id].api
+    if api.get_model() != CAMERA_MODEL:
+        platforms.remove(Platform.CAMERA)
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, platforms):
+        hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
