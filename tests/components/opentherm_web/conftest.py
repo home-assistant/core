9a367e91688ab958da89b84e54643bcf7b72070a
Erik Dresto <erik.dresto@gmail.com>
2023-06-04 19:21:41 +0000

    Fix tests

--- tests/components/opentherm_web/conftest.py
+++ tests/components/opentherm_web/conftest.py
@@ -1,7 +1,8 @@
 """Common fixtures for the OpenTherm Web tests."""
 from collections.abc import Generator
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, Mock, patch
 
+from opentherm_web_api import OpenThermController
 import pytest
 
 from homeassistant.components.opentherm_web.const import DOMAIN
@@ -13,9 +14,9 @@ from tests.common import MockConfigEntry
 def mock_config_entry() -> MockConfigEntry:
     """Return the default mocked config entry."""
     return MockConfigEntry(
-        title="OpenTherm",
+        title="Pohorelice",
         domain=DOMAIN,
-        data={"host": "example", "secret": "secret"},
+        data={"host": "example", "password": "secret"},
         unique_id="unique_thingy",
     )
 
@@ -27,3 +28,19 @@ def mock_setup_entry() -> Generator[AsyncMock, None, None]:
         "homeassistant.components.opentherm_web.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         yield mock_setup_entry
+
+
+@pytest.fixture
+def mock_opentherm():
+    """Return a mocked OpenthermWebApi."""
+    with patch(
+        "homeassistant.components.opentherm_web.OpenThermWebApi"
+    ) as mock_opentherm:
+        web_api = mock_opentherm.return_value
+        web_api.authenticate = Mock(return_value=True)
+
+        controller = OpenThermController
+        controller.device_id = 123
+        web_api.get_controller = Mock(return_value=controller)
+
+        yield web_api
--- tests/components/opentherm_web/test_config_flow.py
+++ tests/components/opentherm_web/test_config_flow.py
@@ -3,12 +3,9 @@ from unittest.mock import AsyncMock, patch
 
 import pytest
 
-from homeassistant import config_entries
-from homeassistant.components.opentherm_web.config_flow import (
-    CannotConnect,
-    InvalidAuth,
-)
+from homeassistant.components.opentherm_web.config_flow import CannotConnect
 from homeassistant.components.opentherm_web.const import DOMAIN
+from homeassistant.config_entries import SOURCE_USER
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
@@ -18,10 +15,10 @@ pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     """Test we get the form."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": SOURCE_USER}
     )
     assert result["type"] == FlowResultType.FORM
-    assert result["errors"] is None
+    assert result.get("step_id") == "user"
 
     with patch(
         "opentherm_web_api.opentherm_web_api.OpenThermWebApi.authenticate",
@@ -48,18 +45,18 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
 async def test_form_invalid_auth(hass: HomeAssistant) -> None:
     """Test we handle invalid auth."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": SOURCE_USER}
     )
 
     with patch(
         "opentherm_web_api.opentherm_web_api.OpenThermWebApi.authenticate",
-        side_effect=InvalidAuth,
+        return_value=False,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
                 "host": "1.1.1.1",
-                "secret": "test-secret",
+                "password": "test-secret",
             },
         )
 
@@ -70,7 +67,7 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
 async def test_form_cannot_connect(hass: HomeAssistant) -> None:
     """Test we handle cannot connect error."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": SOURCE_USER}
     )
 
     with patch(
@@ -81,9 +78,31 @@ async def test_form_cannot_connect(hass: HomeAssistant) -> None:
             result["flow_id"],
             {
                 "host": "1.1.1.1",
-                "secret": "test-secret",
+                "password": "test-secret",
             },
         )
 
     assert result2["type"] == FlowResultType.FORM
     assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_exception(hass: HomeAssistant) -> None:
+    """Test we handle unknown exception."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    with patch(
+        "opentherm_web_api.opentherm_web_api.OpenThermWebApi.authenticate",
+        side_effect=Exception,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "host": "1.1.1.1",
+                "password": "test-secret",
+            },
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {"base": "unknown"}
