bdb656742caae0696a0738072bac80a495b074a5
Jan-Philipp Benecke <jan-philipp@bnck.me>
2023-06-07 16:15:36 +0200

    Remove option flow, refactor and improve the code quality after review in PR #54280

--- tests/components/discovergy/test_config_flow.py
+++ tests/components/discovergy/test_config_flow.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 
 from pydiscovergy.error import HTTPError, InvalidLogin
 
-from homeassistant import data_entry_flow, setup
+from homeassistant import data_entry_flow
 from homeassistant.components.discovergy.const import DOMAIN
 from homeassistant.config_entries import SOURCE_REAUTH, SOURCE_USER
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
@@ -14,7 +14,6 @@ from tests.components.discovergy import init_integration
 
 async def test_form(hass: HomeAssistant, mock_meters) -> None:
     """Test we get the form."""
-    await setup.async_setup_component(hass, "persistent_notification", {})
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": SOURCE_USER}
     )
@@ -56,16 +55,21 @@ async def test_reauth(hass: HomeAssistant, mock_meters) -> None:
     assert init_result["type"] == data_entry_flow.FlowResultType.FORM
     assert init_result["step_id"] == "reauth"
 
-    configure_result = await hass.config_entries.flow.async_configure(
-        init_result["flow_id"],
-        {
-            CONF_EMAIL: "test@example.com",
-            CONF_PASSWORD: "test-password",
-        },
-    )
+    with patch(
+        "homeassistant.components.discovergy.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        configure_result = await hass.config_entries.flow.async_configure(
+            init_result["flow_id"],
+            {
+                CONF_EMAIL: "test@example.com",
+                CONF_PASSWORD: "test-password",
+            },
+        )
 
-    assert configure_result["type"] == data_entry_flow.FlowResultType.ABORT
-    assert configure_result["reason"] == "reauth_successful"
+        assert configure_result["type"] == data_entry_flow.FlowResultType.ABORT
+        assert configure_result["reason"] == "reauth_successful"
+        assert len(mock_setup_entry.mock_calls) == 1
 
 
 async def test_form_invalid_auth(hass: HomeAssistant) -> None:
@@ -126,20 +130,3 @@ async def test_form_unknown_exception(hass: HomeAssistant) -> None:
 
     assert result2["type"] == data_entry_flow.FlowResultType.FORM
     assert result2["errors"] == {"base": "unknown"}
-
-
-async def test_options_flow_init(hass: HomeAssistant) -> None:
-    """Test the options flow."""
-    entry = await init_integration(hass)
-
-    result = await hass.config_entries.options.async_init(entry.entry_id)
-
-    assert result["type"] == data_entry_flow.FlowResultType.FORM
-    assert result["step_id"] == "init"
-
-    create_result = await hass.config_entries.options.async_configure(
-        result["flow_id"], {"time_between_update": 2}
-    )
-
-    assert create_result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
-    assert create_result["data"] == {"time_between_update": 2}
