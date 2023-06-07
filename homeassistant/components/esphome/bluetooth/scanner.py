197248100ff79082dd2ed2478046670117cb36b0
J. Nick Koston <nick@koston.org>
2023-06-06 22:42:11 -0500

    fix

--- homeassistant/components/esphome/bluetooth/scanner.py
+++ homeassistant/components/esphome/bluetooth/scanner.py
@@ -31,9 +31,8 @@ class ESPHomeScanner(BaseHaRemoteScanner):
         self, advertisements: list[BluetoothLERawAdvertisement]
     ) -> None:
         """Call the registered callback."""
-        # [BluetoothLERawAdvertisement(address=92997118468307, rssi=-64, address_type=1, data=b'\x03\x03\x9f\xfe\x17\x16\x9f\xfe\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'), BluetoothLERawAdvertisement(address=277856570910367, rssi=-60, address_type=1, data=b'\x02\x01\x06\t\xffY\x00\xfc\xb5\x865&\x9f')]
         for adv in advertisements:
-            parsed = parse_advertisement_data(adv.data)
+            parsed = parse_advertisement_data((adv.data,))
             self._async_on_advertisement(
                 int_to_bluetooth_address(adv.address),
                 adv.rssi,
