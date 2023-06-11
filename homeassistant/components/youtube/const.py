a8dd2d520af40d0058b49edef82a42bd70caf9f0
Joost Lekkerkerker <joostlek@outlook.com>
2023-06-11 12:30:38 +0200

    Abort youtube configuration if user has no channel (#94402)
    * Abort configuration if user has no channel
    
    * Clean up
    
    ---------
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/youtube/const.py
+++ homeassistant/components/youtube/const.py
@@ -4,6 +4,7 @@ import logging
 DEFAULT_ACCESS = ["https://www.googleapis.com/auth/youtube.readonly"]
 DOMAIN = "youtube"
 MANUFACTURER = "Google, Inc."
+CHANNEL_CREATION_HELP_URL = "https://support.google.com/youtube/answer/1646861"
 
 CONF_CHANNELS = "channels"
 CONF_ID = "id"
