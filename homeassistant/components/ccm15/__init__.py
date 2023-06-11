fcabb53c7f969b689dc57c0653af01c49b9ebbb1
Oscar Calvo <oscar@calvonet.com>
2023-06-11 21:15:06 +0000

    Initial commit

---
+++ homeassistant/components/ccm15/__init__.py
@@ -0,0 +1,30 @@
+"""The Midea ccm15 AC Controller integration."""
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST, CONF_PORT, Platform
+from homeassistant.core import HomeAssistant
+
+from .const import DEFAULT_INTERVAL, DOMAIN
+from .coordinator import CCM15Coordinator
+
+PLATFORMS: list[Platform] = [Platform.CLIMATE]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Midea ccm15 AC Controller from a config entry."""
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = CCM15Coordinator(
+        entry.data[CONF_HOST], entry.data[CONF_PORT], DEFAULT_INTERVAL
+    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
---
+++ homeassistant/components/ccm15/const.py
@@ -0,0 +1,8 @@
+"""Constants for the Midea ccm15 AC Controller integration."""
+
+DOMAIN = "ccm15"
+BASE_URL = "http://{}:{}/{}"
+CONF_URL_STATUS = "status.xml"
+CONF_URL_CTRL = "control.xml"
+DEFAULT_TIMEOUT = 10
+DEFAULT_INTERVAL = 30
