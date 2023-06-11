8f49c998e2b4237b07bb030b557917508c4a9fc0
Oscar Calvo <oscar@calvonet.com>
2023-06-11 22:32:07 +0000

    Integration can be initialized

--- homeassistant/components/ccm15/config_flow.py
+++ homeassistant/components/ccm15/config_flow.py
@@ -63,7 +63,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
             else:
-                return self.async_create_entry(title=info["title"], data=user_input)
+                return self.async_create_entry(title=info[CONF_HOST], data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
