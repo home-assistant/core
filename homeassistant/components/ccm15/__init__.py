055370b5ab761148e6fb9284214557693310471d
Oscar Calvo <oscar@calvonet.com>
2023-06-13 02:26:39 +0000

    Call update API

--- homeassistant/components/ccm15/__init__.py
+++ homeassistant/components/ccm15/__init__.py
@@ -16,7 +16,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})
     coordinator = CCM15Coordinator(
-        entry.data[CONF_HOST], entry.data[CONF_PORT], entry.data[CONF_SCAN_INTERVAL]
+        entry.data[CONF_HOST],
+        entry.data[CONF_PORT],
+        entry.data[CONF_SCAN_INTERVAL],
+        hass,
     )
     await coordinator.async_config_entry_first_refresh()
 
