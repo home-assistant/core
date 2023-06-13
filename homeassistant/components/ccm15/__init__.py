4f4fa59ea7339b784f98ec82678617c045f96acf
Oscar Calvo <oscar@calvonet.com>
2023-06-13 00:53:11 +0000

    Use DataCoordinator

--- homeassistant/components/ccm15/__init__.py
+++ homeassistant/components/ccm15/__init__.py
@@ -18,6 +18,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator = CCM15Coordinator(
         entry.data[CONF_HOST], entry.data[CONF_PORT], entry.data[CONF_SCAN_INTERVAL]
     )
+    await coordinator.async_config_entry_first_refresh()
+
     hass.data[DOMAIN][entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
