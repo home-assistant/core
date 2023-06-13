1a73f46ea8be6affd128acde51480784a306c721
J. Nick Koston <nick@koston.org>
2023-06-12 16:22:09 -1000

    Bump httpcore to 0.17.2
    changelog: https://github.com/encode/httpcore/compare/0.17.0...0.17.2
    
    https://github.com/encode/httpcore/pull/688 is likely the cause of some onvif issues

--- script/gen_requirements_all.py
+++ script/gen_requirements_all.py
@@ -106,7 +106,7 @@ regex==2021.8.28
 # requirements so we can directly link HA versions to these library versions.
 anyio==3.6.2
 h11==0.14.0
-httpcore==0.17.0
+httpcore==0.17.2
 
 # Ensure we have a hyperframe version that works in Python 3.10
 # 5.2.0 fixed a collections abc deprecation
