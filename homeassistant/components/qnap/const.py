322af6d7e66451d01bfb1ad1a49664753139f265
disforw <github@benzona.net>
2023-06-13 23:58:41 -0400

    Update const.py remove attrs

--- homeassistant/components/qnap/const.py
+++ homeassistant/components/qnap/const.py
@@ -1,22 +1,6 @@
 """The Qnap constants."""
 from __future__ import annotations
 
-ATTR_DRIVE = "Drive"
-ATTR_ENABLED = "Sensor Enabled"
-ATTR_IP = "IP Address"
-ATTR_MAC = "MAC Address"
-ATTR_MASK = "Mask"
-ATTR_MAX_SPEED = "Max Speed"
-ATTR_MEMORY_SIZE = "Memory Size"
-ATTR_MODEL = "Model"
-ATTR_PACKETS_TX = "Packets (TX)"
-ATTR_PACKETS_RX = "Packets (RX)"
-ATTR_PACKETS_ERR = "Packets (Err)"
-ATTR_SERIAL = "Serial #"
-ATTR_TYPE = "Type"
-ATTR_UPTIME = "Uptime"
-ATTR_VOLUME_SIZE = "Volume Size"
-
 DEFAULT_NAME = "QNAP"
 DEFAULT_PORT = 8080
 DEFAULT_TIMEOUT = 5
