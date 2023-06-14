a65588f14ea461208b1418e1d79afb16c37e7941
milothomas <20719501+milothomas@users.noreply.github.com>
2023-06-14 05:58:27 +0000

    Added tests

--- tests/components/miraie_ac/test_config_flow.py
+++ tests/components/miraie_ac/test_config_flow.py
@@ -4,8 +4,15 @@ from unittest.mock import AsyncMock, patch
 import pytest
 
 from homeassistant import config_entries
-from homeassistant.components.miraie_ac.config_flow import CannotConnect, InvalidAuth
-from homeassistant.components.miraie_ac.const import DOMAIN
+from homeassistant.components.miraie_ac.config_flow import (
+    AuthException,
+    CannotConnect,
+)
+from homeassistant.components.miraie_ac.const import (
+    CONFIG_KEY_PASSWORD,
+    CONFIG_KEY_USER_ID,
+    DOMAIN,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
@@ -15,31 +22,28 @@ pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     """Test we get the form."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN,
+        context={"source": config_entries.SOURCE_USER},
     )
+
     assert result["type"] == FlowResultType.FORM
-    assert result["errors"] is None
+    assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.miraie_ac.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
+        "homeassistant.components.miraie_ac.config_flow.MirAIeAPI.initialize",
+        return_value=None,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
+            {CONFIG_KEY_USER_ID: "+919876543219", CONFIG_KEY_PASSWORD: "P@ssw0rD"},
         )
         await hass.async_block_till_done()
 
     assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Name of the device"
+    assert result2["title"] == "MirAIe"
     assert result2["data"] == {
-        "host": "1.1.1.1",
-        "username": "test-username",
-        "password": "test-password",
+        CONFIG_KEY_USER_ID: "+919876543219",
+        CONFIG_KEY_PASSWORD: "P@ssw0rD",
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -51,16 +55,12 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
     )
 
     with patch(
-        "homeassistant.components.miraie_ac.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
+        "homeassistant.components.miraie_ac.config_flow.MirAIeAPI.initialize",
+        side_effect=AuthException,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
+            {CONFIG_KEY_USER_ID: "+919876543219", CONFIG_KEY_PASSWORD: "P@ssw0rD"},
         )
 
     assert result2["type"] == FlowResultType.FORM
@@ -74,17 +74,32 @@ async def test_form_cannot_connect(hass: HomeAssistant) -> None:
     )
 
     with patch(
-        "homeassistant.components.miraie_ac.config_flow.PlaceholderHub.authenticate",
+        "homeassistant.components.miraie_ac.config_flow.MirAIeAPI.initialize",
         side_effect=CannotConnect,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
+            {CONFIG_KEY_USER_ID: "+919876543219", CONFIG_KEY_PASSWORD: "P@ssw0rD"},
         )
 
     assert result2["type"] == FlowResultType.FORM
     assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_unhandled_exception(hass: HomeAssistant) -> None:
+    """Test we handle cannot connect error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.miraie_ac.config_flow.MirAIeAPI.initialize",
+        side_effect=Exception,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONFIG_KEY_USER_ID: "+919876543219", CONFIG_KEY_PASSWORD: "P@ssw0rD"},
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {"base": "unknown"}
