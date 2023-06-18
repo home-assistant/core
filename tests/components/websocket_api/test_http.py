010e45fa3498b6164af7fec6f061ef1b982a4bcc
J. Nick Koston <nick@koston.org>
2023-06-18 14:23:56 -0500

    add a handler that raises on unsubscribe

--- tests/components/websocket_api/test_http.py
+++ tests/components/websocket_api/test_http.py
@@ -55,7 +55,7 @@ async def test_cleanup_on_cancellation(
 
     subscriptions = None
 
-    # Register a handler
+    # Register a handler that registers a subscription
     @callback
     @websocket_command(
         {
@@ -73,7 +73,30 @@ async def test_cleanup_on_cancellation(
 
     async_register_command(hass, fake_subscription)
 
-    # Register a handler
+    # Register a handler that raises on cancel
+    @callback
+    @websocket_command(
+        {
+            "type": "subscription_that_raises_on_cancel",
+        }
+    )
+    def subscription_that_raises_on_cancel(
+        hass: HomeAssistant, connection: ActiveConnection, msg: dict[str, Any]
+    ) -> None:
+        nonlocal subscriptions
+        msg_id: int = msg["id"]
+
+        @callback
+        def _raise():
+            raise ValueError()
+
+        connection.subscriptions[msg_id] = _raise
+        connection.send_result(msg_id)
+        subscriptions = connection.subscriptions
+
+    async_register_command(hass, subscription_that_raises_on_cancel)
+
+    # Register a handler that cancels in handler
     @callback
     @websocket_command(
         {
@@ -98,7 +121,15 @@ async def test_cleanup_on_cancellation(
     assert msg["type"] == const.TYPE_RESULT
     assert msg["success"]
     assert len(subscriptions) == 2
-    await websocket_client.send_json({"id": 3, "type": "cancel_in_handler"})
+    await websocket_client.send_json(
+        {"id": 3, "type": "subscription_that_raises_on_cancel"}
+    )
+    msg = await websocket_client.receive_json()
+    assert msg["id"] == 3
+    assert msg["type"] == const.TYPE_RESULT
+    assert msg["success"]
+    assert len(subscriptions) == 3
+    await websocket_client.send_json({"id": 4, "type": "cancel_in_handler"})
     await hass.async_block_till_done()
     msg = await websocket_client.receive()
     assert msg.type == WSMsgType.close
