dbca9c4581aa3f46f420761b5ee3c471fe92a38e
Nikola Nedevski <Nedevski@users.noreply.github.com>
2023-06-18 11:10:51 +0300

    PR Comments Fix 2

--- homeassistant/components/kat_bulgaria/binary_sensor.py
+++ homeassistant/components/kat_bulgaria/binary_sensor.py
@@ -24,9 +24,9 @@ async def async_setup_entry(
 ) -> None:
     """Set up the platform from config_entry."""
 
-    person_name: str = str(entry.data[CONF_PERSON_NAME])
-    person_egn: str = str(entry.data[CONF_PERSON_EGN])
-    license_number: str = str(entry.data[CONF_DRIVING_LICENSE])
+    person_name: str = entry.data[CONF_PERSON_NAME]
+    person_egn: str = entry.data[CONF_PERSON_EGN]
+    license_number: str = entry.data[CONF_DRIVING_LICENSE]
 
     api: KatApi = hass.data[DOMAIN][entry.entry_id]
 
@@ -47,9 +47,8 @@ class KatObligationsSensor(BinarySensorEntity):
 
         self.user_egn = egn
         self.user_license_number = license_number
-        self.user_name = name
 
-        self._attr_name = generate_entity_name(self.user_name)
+        self._attr_name = generate_entity_name(name)
 
     async def async_update(self) -> None:
         """Fetch new state data for the sensor."""
