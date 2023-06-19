5b47ccc2b5e23957eab835a10338b463bca6cfb6
disforw <github@benzona.net>
2023-06-18 21:14:17 -0400

    Update switch.py

--- homeassistant/components/switch_as_x/switch.py
+++ homeassistant/components/switch_as_x/switch.py
@@ -1,6 +1,8 @@
 """Inverse support for switch entities."""
 from __future__ import annotations
 
+from typing import Any
+
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN, SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ENTITY_ID
@@ -44,9 +46,9 @@ class InvertSwitch(BaseToggleEntity, SwitchEntity):
         return not self._attr_is_on
 
     async def async_turn_on(self, **kwargs: Any) -> None:
-        """Turn off original switch"""
+        """Turn off original switch."""
         await super().async_turn_off()
 
     async def async_turn_off(self) -> None:
-        """Turn on original switch"""
+        """Turn on original switch."""
         await super().async_turn_on()
