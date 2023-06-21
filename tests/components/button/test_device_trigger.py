49ec806046f1a8c548f6b561ffb9d10e40d376ae
Erik Montnemery <erik@montnemery.com>
2023-06-21 14:50:11 +0200

    Teach button device trigger about entity registry ids (#94965)
    * Teach button device trigger about entity registry ids
    
    * Update homekit_controller tests

--- tests/components/button/test_device_trigger.py
+++ tests/components/button/test_device_trigger.py
@@ -37,7 +37,7 @@ async def test_get_triggers(
         config_entry_id=config_entry.entry_id,
         connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
     )
-    entity_registry.async_get_or_create(
+    entity_entry = entity_registry.async_get_or_create(
         DOMAIN, "test", "5678", device_id=device_entry.id
     )
     expected_triggers = [
@@ -46,7 +46,7 @@ async def test_get_triggers(
             "domain": DOMAIN,
             "type": "pressed",
             "device_id": device_entry.id,
-            "entity_id": f"{DOMAIN}.test_5678",
+            "entity_id": entity_entry.id,
             "metadata": {"secondary": False},
         }
     ]
@@ -79,7 +79,7 @@ async def test_get_triggers_hidden_auxiliary(
         config_entry_id=config_entry.entry_id,
         connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
     )
-    entity_registry.async_get_or_create(
+    entity_entry = entity_registry.async_get_or_create(
         DOMAIN,
         "test",
         "5678",
@@ -93,7 +93,7 @@ async def test_get_triggers_hidden_auxiliary(
             "domain": DOMAIN,
             "type": trigger,
             "device_id": device_entry.id,
-            "entity_id": f"{DOMAIN}.test_5678",
+            "entity_id": entity_entry.id,
             "metadata": {"secondary": True},
         }
         for trigger in ["pressed"]
@@ -104,9 +104,13 @@ async def test_get_triggers_hidden_auxiliary(
     assert triggers == unordered(expected_triggers)
 
 
-async def test_if_fires_on_state_change(hass: HomeAssistant, calls) -> None:
+async def test_if_fires_on_state_change(
+    hass: HomeAssistant, entity_registry: er.EntityRegistry, calls
+) -> None:
     """Test for turn_on and turn_off triggers firing."""
-    hass.states.async_set("button.entity", "unknown")
+    entry = entity_registry.async_get_or_create(DOMAIN, "test", "5678")
+
+    hass.states.async_set(entry.entity_id, "unknown")
 
     assert await async_setup_component(
         hass,
@@ -118,7 +122,7 @@ async def test_if_fires_on_state_change(hass: HomeAssistant, calls) -> None:
                         "platform": "device",
                         "domain": DOMAIN,
                         "device_id": "",
-                        "entity_id": "button.entity",
+                        "entity_id": entry.id,
                         "type": "pressed",
                     },
                     "action": {
@@ -140,11 +144,59 @@ async def test_if_fires_on_state_change(hass: HomeAssistant, calls) -> None:
     )
 
     # Test triggering device trigger with a to state
-    hass.states.async_set("button.entity", "2021-01-01T23:59:59+00:00")
+    hass.states.async_set(entry.entity_id, "2021-01-01T23:59:59+00:00")
     await hass.async_block_till_done()
     assert len(calls) == 1
-    assert calls[0].data[
-        "some"
-    ] == "to - device - {} - unknown - 2021-01-01T23:59:59+00:00 - None - 0".format(
-        "button.entity"
+    assert (
+        calls[0].data["some"]
+        == f"to - device - {entry.entity_id} - unknown - 2021-01-01T23:59:59+00:00 - None - 0"
+    )
+
+
+async def test_if_fires_on_state_change_legacy(
+    hass: HomeAssistant, entity_registry: er.EntityRegistry, calls
+) -> None:
+    """Test for turn_on and turn_off triggers firing."""
+    entry = entity_registry.async_get_or_create(DOMAIN, "test", "5678")
+
+    hass.states.async_set(entry.entity_id, "unknown")
+
+    assert await async_setup_component(
+        hass,
+        automation.DOMAIN,
+        {
+            automation.DOMAIN: [
+                {
+                    "trigger": {
+                        "platform": "device",
+                        "domain": DOMAIN,
+                        "device_id": "",
+                        "entity_id": entry.entity_id,
+                        "type": "pressed",
+                    },
+                    "action": {
+                        "service": "test.automation",
+                        "data": {
+                            "some": (
+                                "to - {{ trigger.platform }} "
+                                "- {{ trigger.entity_id }} "
+                                "- {{ trigger.from_state.state }} "
+                                "- {{ trigger.to_state.state }} "
+                                "- {{ trigger.for }} "
+                                "- {{ trigger.id }}"
+                            )
+                        },
+                    },
+                }
+            ]
+        },
+    )
+
+    # Test triggering device trigger with a to state
+    hass.states.async_set(entry.entity_id, "2021-01-01T23:59:59+00:00")
+    await hass.async_block_till_done()
+    assert len(calls) == 1
+    assert (
+        calls[0].data["some"]
+        == f"to - device - {entry.entity_id} - unknown - 2021-01-01T23:59:59+00:00 - None - 0"
     )
