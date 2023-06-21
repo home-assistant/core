05b1f80f79566a2be6975ec2cbfb1ea67cdf825a
Jan Pecinovsky <jan.pecinovsky@energieid.be>
2023-06-21 16:48:30 +0000

    energyid integration

---
+++ homeassistant/components/energyid/const.py
@@ -0,0 +1,6 @@
+"""Constants for the EnergyID integration."""
+
+DOMAIN = "energyid"
+
+ENERGYID_INTERVALS = ["P1M", "P1D", "PT1H", "PT15M", "PT5M"]
+ENERGYID_METRIC_KINDS = ["cumulative", "total", "delta", "gauge"]
