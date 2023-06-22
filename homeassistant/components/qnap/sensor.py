961a36ce8f46282d433dfc8c1b15706a00e3bc46
disforw <github@benzona.net>
2023-06-22 07:13:07 -0400

    Update homeassistant/components/qnap/sensor.py
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>

--- homeassistant/components/qnap/sensor.py
+++ homeassistant/components/qnap/sensor.py
@@ -336,7 +336,6 @@ class QNAPSensor(CoordinatorEntity[QnapCoordinator], SensorEntity):
         self.uid = uniqueid
         self.device_name = self.coordinator.data["system_stats"]["system"]["name"]
         self.monitor_device = monitor_device
-        self.monitor_subdevice = monitor_subdevice
         self.coordinator_context = None
         self._attr_unique_id = f"{self.uid}_{self.device_name}_{self.name}"
         self._attr_device_info = DeviceInfo(
