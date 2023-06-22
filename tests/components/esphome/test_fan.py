30bacfe29cae85e71e65beb2bac0d2d8a1383f4d
J. Nick Koston <nick@koston.org>
2023-06-22 02:15:07 +0200

    Update tests/components/esphome/test_fan.py

--- tests/components/esphome/test_fan.py
+++ tests/components/esphome/test_fan.py
@@ -133,7 +133,7 @@ async def test_fan_entity_with_all_features_old_api(
 async def test_fan_entity_with_all_features_new_api(
     hass: HomeAssistant, mock_client: APIClient, mock_generic_device_entry
 ) -> None:
-    """Test a generic fan entity that has all features."""
+    """Test a generic fan entity that uses the new api and has all features."""
     mock_client.api_version = APIVersion(1, 4)
     entity_info = [
         FanInfo(
