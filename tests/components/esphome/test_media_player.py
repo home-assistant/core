03ea57ef30f3ea910df77b5f4482225d04f63eb1
J. Nick Koston <nick@koston.org>
2023-06-22 15:18:08 +0200

    coverage

--- tests/components/esphome/test_media_player.py
+++ tests/components/esphome/test_media_player.py
@@ -1,6 +1,5 @@
 """Test ESPHome media_players."""
 
-
 from unittest.mock import call
 
 from aioesphomeapi import (
@@ -10,20 +9,31 @@ from aioesphomeapi import (
     MediaPlayerInfo,
     MediaPlayerState,
 )
+import pytest
 
+from homeassistant.components import media_source
 from homeassistant.components.media_player import (
+    ATTR_MEDIA_CONTENT_ID,
+    ATTR_MEDIA_CONTENT_TYPE,
+    ATTR_MEDIA_VOLUME_LEVEL,
     ATTR_MEDIA_VOLUME_MUTED,
     DOMAIN as MEDIA_PLAYER_DOMAIN,
+    SERVICE_MEDIA_PAUSE,
+    SERVICE_MEDIA_PLAY,
+    SERVICE_MEDIA_STOP,
+    SERVICE_PLAY_MEDIA,
     SERVICE_VOLUME_MUTE,
+    SERVICE_VOLUME_SET,
+    MediaType,
 )
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
 
 
-async def test_media_player_entity_no_open(
+async def test_media_player_entity(
     hass: HomeAssistant, mock_client: APIClient, mock_generic_device_entry
 ) -> None:
-    """Test a generic media_player entity that does not support open."""
+    """Test a generic media_player entity."""
     entity_info = [
         MediaPlayerInfo(
             object_id="mymedia_player",
@@ -61,4 +71,83 @@ async def test_media_player_entity_no_open(
     mock_client.media_player_command.assert_has_calls(
         [call(1, command=MediaPlayerCommand.MUTE)]
     )
+    mock_client.media_player_command.reset_mock()
+
+    await hass.services.async_call(
+        MEDIA_PLAYER_DOMAIN,
+        SERVICE_VOLUME_MUTE,
+        {
+            ATTR_ENTITY_ID: "media_player.test_my_media_player",
+            ATTR_MEDIA_VOLUME_MUTED: True,
+        },
+        blocking=True,
+    )
+    mock_client.media_player_command.assert_has_calls(
+        [call(1, command=MediaPlayerCommand.MUTE)]
+    )
+    mock_client.media_player_command.reset_mock()
+
+    await hass.services.async_call(
+        MEDIA_PLAYER_DOMAIN,
+        SERVICE_VOLUME_SET,
+        {
+            ATTR_ENTITY_ID: "media_player.test_my_media_player",
+            ATTR_MEDIA_VOLUME_LEVEL: 0.5,
+        },
+        blocking=True,
+    )
+    mock_client.media_player_command.assert_has_calls([call(1, volume=0.5)])
+    mock_client.media_player_command.reset_mock()
+
+    await hass.services.async_call(
+        MEDIA_PLAYER_DOMAIN,
+        SERVICE_MEDIA_PAUSE,
+        {
+            ATTR_ENTITY_ID: "media_player.test_my_media_player",
+        },
+        blocking=True,
+    )
+    mock_client.media_player_command.assert_has_calls(
+        [call(1, command=MediaPlayerCommand.PAUSE)]
+    )
+    mock_client.media_player_command.reset_mock()
+
+    await hass.services.async_call(
+        MEDIA_PLAYER_DOMAIN,
+        SERVICE_MEDIA_PLAY,
+        {
+            ATTR_ENTITY_ID: "media_player.test_my_media_player",
+        },
+        blocking=True,
+    )
+    mock_client.media_player_command.assert_has_calls(
+        [call(1, command=MediaPlayerCommand.PLAY)]
+    )
+    mock_client.media_player_command.reset_mock()
+
+    await hass.services.async_call(
+        MEDIA_PLAYER_DOMAIN,
+        SERVICE_MEDIA_STOP,
+        {
+            ATTR_ENTITY_ID: "media_player.test_my_media_player",
+        },
+        blocking=True,
+    )
+    mock_client.media_player_command.assert_has_calls(
+        [call(1, command=MediaPlayerCommand.STOP)]
+    )
+    mock_client.media_player_command.reset_mock()
+
+    with pytest.raises(media_source.error.Unresolvable):
+        await hass.services.async_call(
+            MEDIA_PLAYER_DOMAIN,
+            SERVICE_PLAY_MEDIA,
+            {
+                ATTR_ENTITY_ID: "media_player.test_my_media_player",
+                ATTR_MEDIA_CONTENT_TYPE: MediaType.MUSIC,
+                ATTR_MEDIA_CONTENT_ID: "media-source://local/xz",
+            },
+            blocking=True,
+        )
+
     mock_client.media_player_command.reset_mock()
