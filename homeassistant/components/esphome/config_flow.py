b9d6059b26b0eb42585954f9fce499cc4ad316b7
J. Nick Koston <nick@koston.org>
2023-06-23 14:36:16 -0500

    coverage

--- homeassistant/components/esphome/config_flow.py
+++ homeassistant/components/esphome/config_flow.py
@@ -414,7 +414,9 @@ class OptionsFlowHandler(OptionsFlow):
         """Initialize options flow."""
         self.config_entry = config_entry
 
-    async def async_step_init(self, user_input=None):
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
         """Handle options flow."""
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
