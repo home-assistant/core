7348919763f47b76e3475b9259b6ee79a2b3e052
Jan Pecinovsky <jan.pecinovsky@energieid.be>
2023-06-23 08:42:01 +0000

    move config names to const

--- homeassistant/components/energyid/const.py
+++ homeassistant/components/energyid/const.py
@@ -1,6 +1,18 @@
 """Constants for the EnergyID integration."""
 
-DOMAIN = "energyid"
+from typing import Final
+
+DOMAIN: Final[str] = "energyid"
+
+CONF_WEBHOOK_URL: Final["str"] = "webhook_url"
+CONF_ENTITY_ID: Final["str"] = "entity_id"
+CONF_METRIC: Final["str"] = "metric"
+CONF_METRIC_KIND: Final["str"] = "metric_kind"
+CONF_UNIT: Final["str"] = "unit"
+CONF_DATA_INTERVAL: Final["str"] = "data_interval"
+DEFAULT_DATA_INTERVAL: Final["str"] = "P1D"
+CONF_UPLOAD_INTERVAL: Final["str"] = "upload_interval"
+DEFAULT_UPLOAD_INTERVAL: Final[int] = 300
 
 ENERGYID_INTERVALS = ["P1M", "P1D", "PT1H", "PT15M", "PT5M"]
 ENERGYID_METRIC_KINDS = ["cumulative", "total", "delta", "gauge"]
