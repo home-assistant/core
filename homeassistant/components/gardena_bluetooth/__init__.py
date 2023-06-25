a654bcdbc216fd4043115f46154e45a8ad6d46f8
Joakim Plate <elupus@ecce.se>
2023-06-25 19:32:00 +0200

    Can't call first refresh after forward entry setups
    Sub platforms will not be unloaded on exceptions.

--- homeassistant/components/gardena_bluetooth/__init__.py
+++ homeassistant/components/gardena_bluetooth/__init__.py
@@ -75,7 +75,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    await coordinator.async_config_entry_first_refresh()
+    await coordinator.async_refresh()
 
     return True
 
