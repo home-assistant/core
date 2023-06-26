574d8a82e88c0a3e94323767dc1a34cb52c234f2
Robert Hillis <tkdrob4390@yahoo.com>
2023-06-26 17:45:23 -0400

    Add entity translations to Goalzero

--- homeassistant/components/goalzero/binary_sensor.py
+++ homeassistant/components/goalzero/binary_sensor.py
@@ -21,23 +21,23 @@ PARALLEL_UPDATES = 0
 BINARY_SENSOR_TYPES: tuple[BinarySensorEntityDescription, ...] = (
     BinarySensorEntityDescription(
         key="backlight",
-        name="Backlight",
+        translation_key="backlight",
         icon="mdi:clock-digital",
     ),
     BinarySensorEntityDescription(
         key="app_online",
-        name="App online",
+        translation_key="app_online",
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     BinarySensorEntityDescription(
         key="isCharging",
-        name="Charging",
+        translation_key="is_charging",
         device_class=BinarySensorDeviceClass.BATTERY_CHARGING,
     ),
     BinarySensorEntityDescription(
         key="inputDetected",
-        name="Input detected",
+        translation_key="input_detected",
         device_class=BinarySensorDeviceClass.POWER,
     ),
 )
