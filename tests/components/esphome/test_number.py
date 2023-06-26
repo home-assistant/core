1ca771a20d2ce7073f687a1a3210d5980594e5eb
J. Nick Koston <nick@koston.org>
2023-06-25 22:57:53 -0500

    tweak

--- tests/components/esphome/test_number.py
+++ tests/components/esphome/test_number.py
@@ -1,10 +1,12 @@
 """Test ESPHome numbers."""
 
+import math
 from unittest.mock import call
 
 from aioesphomeapi import (
     APIClient,
     NumberInfo,
+    NumberMode as ESPHomeNumberMode,
     NumberState,
 )
 
@@ -13,11 +15,11 @@ from homeassistant.components.number import (
     DOMAIN as NUMBER_DOMAIN,
     SERVICE_SET_VALUE,
 )
-from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.const import ATTR_ENTITY_ID, STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
 
 
-async def test_generic_numeric_entity(
+async def test_generic_number_entity(
     hass: HomeAssistant,
     mock_client: APIClient,
     mock_generic_device_entry,
@@ -55,3 +57,35 @@ async def test_generic_numeric_entity(
     )
     mock_client.number_command.assert_has_calls([call(1, 50)])
     mock_client.number_command.reset_mock()
+
+
+async def test_generic_number_nan(
+    hass: HomeAssistant,
+    mock_client: APIClient,
+    mock_generic_device_entry,
+) -> None:
+    """Test a generic number entity with nan state."""
+    entity_info = [
+        NumberInfo(
+            object_id="mynumber",
+            key=1,
+            name="my number",
+            unique_id="my_number",
+            max_value=100,
+            min_value=0,
+            step=1,
+            unit_of_measurement="%",
+            mode=ESPHomeNumberMode.SLIDER,
+        )
+    ]
+    states = [NumberState(key=1, state=math.nan)]
+    user_service = []
+    await mock_generic_device_entry(
+        mock_client=mock_client,
+        entity_info=entity_info,
+        user_service=user_service,
+        states=states,
+    )
+    state = hass.states.get("number.test_my_number")
+    assert state is not None
+    assert state.state == STATE_UNKNOWN
