2b18fd19bd6d85a0210fd57610eddc6fb77edec4
jbouwh <jan@jbsoft.nl>
2023-06-26 09:14:58 +0000

    Remove url_topic / template feature from PR

--- homeassistant/components/mqtt/image.py
+++ homeassistant/components/mqtt/image.py
@@ -6,7 +6,6 @@ import binascii
 from collections.abc import Callable
 import functools
 import logging
-import ssl
 from typing import Any
 
 import httpx
@@ -29,10 +28,10 @@ from homeassistant.util import dt as dt_util
 
 from . import subscription
 from .config import MQTT_BASE_SCHEMA
-from .const import CONF_ENCODING, CONF_QOS
+from .const import CONF_QOS
 from .debug_info import log_messages
 from .mixins import MQTT_ENTITY_COMMON_SCHEMA, MqttEntity, async_setup_entry_helper
-from .models import MessageCallbackType, MqttValueTemplate, ReceiveMessage
+from .models import ReceiveMessage
 from .util import get_mqtt_data, valid_subscribe_topic
 
 _LOGGER = logging.getLogger(__name__)
@@ -40,39 +39,21 @@ _LOGGER = logging.getLogger(__name__)
 CONF_CONTENT_TYPE = "content_type"
 CONF_IMAGE_ENCODING = "image_encoding"
 CONF_IMAGE_TOPIC = "image_topic"
-CONF_URL_TEMPLATE = "url_template"
-CONF_URL_TOPIC = "url_topic"
 
 DEFAULT_NAME = "MQTT Image"
 
 
-def validate_topic_required(config: ConfigType) -> ConfigType:
-    """Ensure at least one subscribe topic is configured."""
-    if CONF_IMAGE_TOPIC not in config and CONF_URL_TOPIC not in config:
-        raise vol.Invalid("Expected one of [`image_topic`, `url_topic`], got none")
-    if CONF_CONTENT_TYPE in config and CONF_URL_TOPIC in config:
-        raise vol.Invalid(
-            "Option `content_type` can not be used together with `url_topic`"
-        )
-    return config
-
-
-PLATFORM_SCHEMA_BASE = MQTT_BASE_SCHEMA.extend(
+PLATFORM_SCHEMA_MODERN = MQTT_BASE_SCHEMA.extend(
     {
         vol.Optional(CONF_CONTENT_TYPE): cv.string,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Exclusive(CONF_URL_TOPIC, "image_topic"): valid_subscribe_topic,
-        vol.Exclusive(CONF_IMAGE_TOPIC, "image_topic"): valid_subscribe_topic,
+        vol.Required(CONF_IMAGE_TOPIC): valid_subscribe_topic,
         vol.Optional(CONF_IMAGE_ENCODING): "b64",
-        vol.Optional(CONF_URL_TEMPLATE): cv.template,
     }
 ).extend(MQTT_ENTITY_COMMON_SCHEMA.schema)
 
-PLATFORM_SCHEMA_MODERN = vol.All(PLATFORM_SCHEMA_BASE.schema, validate_topic_required)
 
-DISCOVERY_SCHEMA = vol.All(
-    PLATFORM_SCHEMA_BASE.extend({}, extra=vol.REMOVE_EXTRA), validate_topic_required
-)
+DISCOVERY_SCHEMA = vol.All(PLATFORM_SCHEMA_MODERN.extend({}, extra=vol.REMOVE_EXTRA))
 
 
 async def async_setup_entry(
@@ -126,57 +107,16 @@ class MqttImage(MqttEntity, ImageEntity):
 
     def _setup_from_config(self, config: ConfigType) -> None:
         """(Re)Setup the entity."""
-        self._topic = {
-            key: config.get(key)
-            for key in (
-                CONF_IMAGE_TOPIC,
-                CONF_URL_TOPIC,
-            )
-        }
+        self._topic = {key: config.get(key) for key in (CONF_IMAGE_TOPIC,)}
         if CONF_IMAGE_TOPIC in config:
             self._attr_content_type = config.get(
                 CONF_CONTENT_TYPE, DEFAULT_CONTENT_TYPE
             )
-        self._url_template = MqttValueTemplate(
-            config.get(CONF_URL_TEMPLATE), entity=self
-        ).async_render_with_possible_json_value
-
-    async def _async_load_image(self, url: str) -> None:
-        try:
-            response = await self._client.request("GET", url)
-        except (httpx.TimeoutException, httpx.RequestError, ssl.SSLError) as ex:
-            _LOGGER.warning("Connection failed to url %s files: %s", url, ex)
-            self._last_image = None
-            self._attr_image_last_updated = dt_util.utcnow()
-            self.async_write_ha_state()
-            return
-
-        self._attr_content_type = response.headers["content-type"]
-        self._last_image = response.content
-        self._attr_image_last_updated = dt_util.utcnow()
-        self.async_write_ha_state()
 
     def _prepare_subscribe_topics(self) -> None:
         """(Re)Subscribe to topics."""
 
         topics: dict[str, Any] = {}
-
-        def add_subscribe_topic(topic: str, msg_callback: MessageCallbackType) -> bool:
-            """Add a topic to subscribe to."""
-            encoding: str | None
-            encoding = (
-                None
-                if CONF_IMAGE_TOPIC in self._config
-                else self._config[CONF_ENCODING] or None
-            )
-            if has_topic := self._topic[topic] is not None:
-                topics[topic] = {
-                    "topic": self._topic[topic],
-                    "msg_callback": msg_callback,
-                    "qos": self._config[CONF_QOS],
-                    "encoding": encoding,
-                }
-            return has_topic
 
         @callback
         @log_messages(self.hass, self.entity_id)
@@ -198,25 +138,12 @@ class MqttImage(MqttEntity, ImageEntity):
             self._attr_image_last_updated = dt_util.utcnow()
             get_mqtt_data(self.hass).state_write_requests.write_state_request(self)
 
-        add_subscribe_topic(CONF_IMAGE_TOPIC, image_data_received)
-
-        @callback
-        @log_messages(self.hass, self.entity_id)
-        def image_from_url_request_received(msg: ReceiveMessage) -> None:
-            """Handle new MQTT messages."""
-
-            try:
-                url = cv.url(self._url_template(msg.payload))
-            except vol.Invalid:
-                _LOGGER.error(
-                    "Invalid image URL '%s' received at topic %s",
-                    msg.payload,
-                    msg.topic,
-                )
-                return
-            self.hass.async_create_task(self._async_load_image(url))
-
-        add_subscribe_topic(CONF_URL_TOPIC, image_from_url_request_received)
+        topics[self._config[CONF_IMAGE_TOPIC]] = {
+            "topic": self._config[CONF_IMAGE_TOPIC],
+            "msg_callback": image_data_received,
+            "qos": self._config[CONF_QOS],
+            "encoding": None,
+        }
 
         self._sub_state = subscription.async_prepare_subscribe_topics(
             self.hass, self._sub_state, topics
