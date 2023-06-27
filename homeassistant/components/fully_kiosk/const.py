dbc4e4785f0b279a15598cd8ca3ed2b34380bf3b
Charles Garwood <cgarwood@gmail.com>
2023-06-26 20:40:58 -0400

    Add fully_kiosk set_config service

--- homeassistant/components/fully_kiosk/const.py
+++ homeassistant/components/fully_kiosk/const.py
@@ -25,6 +25,10 @@ MEDIA_SUPPORT_FULLYKIOSK = (
 
 SERVICE_LOAD_URL = "load_url"
 SERVICE_START_APPLICATION = "start_application"
+SERVICE_SET_CONFIG = "set_config"
 
 ATTR_URL = "url"
 ATTR_APPLICATION = "application"
+ATTR_CONFIG_TYPE = "config_type"
+ATTR_KEY = "key"
+ATTR_VALUE = "value"
