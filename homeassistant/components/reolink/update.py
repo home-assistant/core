9b0518027c7c23ed3ecfe996b22a680105d6c3d4
starkillerOG <starkiller.og@gmail.com>
2023-06-28 09:21:56 +0200

    Update homeassistant/components/reolink/update.py
    Co-authored-by: c0ffeeca7 <38767475+c0ffeeca7@users.noreply.github.com>

--- homeassistant/components/reolink/update.py
+++ homeassistant/components/reolink/update.py
@@ -84,7 +84,7 @@ class ReolinkUpdateEntity(
         return (
             "If the install button fails, download this"
             f" [firmware zip file]({self.coordinator.data.download_url})."
-            ", including installation guide.\n\n"
+            " Then, follow the installation guide (PDF in the zip file).\n\n"
             f"## Release notes\n\n{self.coordinator.data.release_notes}"
         )
 
