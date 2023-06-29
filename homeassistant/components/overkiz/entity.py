7fe6f33f62a16bc557cfeda87dd63e255d3f7e6e
Mick Vleeshouwer <mick@imick.nl>
2023-06-29 21:08:07 +0000

    Fix labels

--- homeassistant/components/overkiz/entity.py
+++ homeassistant/components/overkiz/entity.py
@@ -119,3 +119,5 @@ class OverkizDescriptiveEntity(OverkizEntity):
             # In case of sub device, use the provided label
             # and append the name of the type of entity
             self._attr_name = f"{self.device.label} {description.name}"
+        else:
+            self._attr_name = f"{description.name}"
