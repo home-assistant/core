18dd3cae8602a0e827bed44c0c1cb679d29ae308
Brett Adams <brett.whynot@gmail.com>
2023-06-30 02:16:56 +0000

    Add tests

--- tests/components/advantage_air/test_switch.py
+++ tests/components/advantage_air/test_switch.py
@@ -5,6 +5,7 @@ from homeassistant.components.advantage_air.const import (
     ADVANTAGE_AIR_STATE_OFF,
     ADVANTAGE_AIR_STATE_ON,
 )
+from homeassistant.components.advantage_air.switch import ADVANTAGE_AIR_AUTOFAN
 from homeassistant.components.switch import (
     DOMAIN as SWITCH_DOMAIN,
     SERVICE_TURN_OFF,
@@ -44,7 +45,7 @@ async def test_cover_async_setup_entry(
 
     registry = er.async_get(hass)
 
-    # Test Switch Entity
+    # Test Fresh Air Switch Entity
     entity_id = "switch.myzone_fresh_air"
     state = hass.states.get(entity_id)
     assert state
@@ -77,6 +78,42 @@ async def test_cover_async_setup_entry(
     assert aioclient_mock.mock_calls[-2][1].path == "/setAircon"
     data = loads(aioclient_mock.mock_calls[-2][1].query["json"])
     assert data["ac1"]["info"]["freshAirStatus"] == ADVANTAGE_AIR_STATE_OFF
+    assert aioclient_mock.mock_calls[-1][0] == "GET"
+    assert aioclient_mock.mock_calls[-1][1].path == "/getSystemData"
+
+    # Test MyFan Switch Entity
+    entity_id = "switch.myzone_myfan"
+    state = hass.states.get(entity_id)
+    assert state
+    assert state.state == STATE_OFF
+
+    entry = registry.async_get(entity_id)
+    assert entry
+    assert entry.unique_id == "uniqueid-ac1-myfan"
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: [entity_id]},
+        blocking=True,
+    )
+    assert aioclient_mock.mock_calls[-2][0] == "GET"
+    assert aioclient_mock.mock_calls[-2][1].path == "/setAircon"
+    data = loads(aioclient_mock.mock_calls[-2][1].query["json"])
+    assert data["ac1"]["info"][ADVANTAGE_AIR_AUTOFAN] is True
+    assert aioclient_mock.mock_calls[-1][0] == "GET"
+    assert aioclient_mock.mock_calls[-1][1].path == "/getSystemData"
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        SERVICE_TURN_OFF,
+        {ATTR_ENTITY_ID: [entity_id]},
+        blocking=True,
+    )
+    assert aioclient_mock.mock_calls[-2][0] == "GET"
+    assert aioclient_mock.mock_calls[-2][1].path == "/setAircon"
+    data = loads(aioclient_mock.mock_calls[-2][1].query["json"])
+    assert data["ac1"]["info"][ADVANTAGE_AIR_AUTOFAN] is False
     assert aioclient_mock.mock_calls[-1][0] == "GET"
     assert aioclient_mock.mock_calls[-1][1].path == "/getSystemData"
 
