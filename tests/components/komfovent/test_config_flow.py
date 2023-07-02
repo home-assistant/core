1acebab3ee48085484f3a3b141e0e67ff8f819f5
Aleksandr Tsernoh <aleksandr@salv.com>
2023-07-02 17:38:40 +0000

    tests for form handling

--- tests/components/komfovent/test_config_flow.py
+++ tests/components/komfovent/test_config_flow.py
@@ -1,6 +1,7 @@
 """Test the Komfovent config flow."""
 from unittest.mock import AsyncMock, patch
 
+import komfovent_api
 import pytest
 
 from homeassistant import config_entries
@@ -20,8 +21,17 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     assert result["errors"] is None
 
     with patch(
-        "homeassistant.components.komfovent.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
+        "homeassistant.components.komfovent.config_flow.komfovent_api.get_credentials",
+        return_value=(
+            komfovent_api.KomfoventConnectionResult.SUCCESS,
+            komfovent_api.KomfoventCredentials("1.1.1.1", "user", "pass"),
+        ),
+    ), patch(
+        "homeassistant.components.komfovent.config_flow.komfovent_api.get_settings",
+        return_value=(
+            komfovent_api.KomfoventConnectionResult.SUCCESS,
+            komfovent_api.KomfoventSettings("test-name", None, None, None),
+        ),
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -34,7 +44,7 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
         await hass.async_block_till_done()
 
     assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Name of the device"
+    assert result2["title"] == "test-name"
     assert result2["data"] == {
         "host": "1.1.1.1",
         "username": "test-username",
@@ -43,47 +53,34 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     assert len(mock_setup_entry.mock_calls) == 1
 
 
-async def test_form_invalid_auth(hass: HomeAssistant) -> None:
+async def test_form_error_handling(hass: HomeAssistant) -> None:
     """Test we handle invalid auth."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    with patch(
-        "homeassistant.components.komfovent.config_flow.PlaceholderHub.authenticate",
-        side_effect=Exception,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
+    error_code_to_expected_message = {
+        komfovent_api.KomfoventConnectionResult.NOT_FOUND: "cannot_connect",
+        komfovent_api.KomfoventConnectionResult.UNAUTHORISED: "invalid_auth",
+        komfovent_api.KomfoventConnectionResult.INVALID_INPUT: "invalid_input",
+    }
 
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"] == {"base": "invalid_auth"}
+    for error, expected_response in error_code_to_expected_message.items():
+        with patch(
+            "homeassistant.components.komfovent.config_flow.komfovent_api.get_credentials",
+            return_value=(
+                error,
+                None,
+            ),
+        ):
+            result2 = await hass.config_entries.flow.async_configure(
+                result["flow_id"],
+                {
+                    "host": "1.1.1.1",
+                    "username": "test-username",
+                    "password": "test-password",
+                },
+            )
 
-
-async def test_form_cannot_connect(hass: HomeAssistant) -> None:
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.komfovent.config_flow.PlaceholderHub.authenticate",
-        side_effect=Exception,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"] == {"base": "cannot_connect"}
+        assert result2["type"] == FlowResultType.FORM
+        assert result2["errors"] == {"base": expected_response}
