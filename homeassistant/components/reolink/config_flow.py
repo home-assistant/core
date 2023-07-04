bfaf7da59455e948e6e36fa1f905d8de7cbe5b46
starkillerOG <starkiller.og@gmail.com>
2023-07-04 09:46:55 +0200

    fix black

--- homeassistant/components/reolink/config_flow.py
+++ homeassistant/components/reolink/config_flow.py
@@ -80,7 +80,9 @@ class ReolinkFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         self._password = entry_data[CONF_PASSWORD]
         self._reauth = True
         self.context["title_placeholders"]["ip_address"] = entry_data[CONF_HOST]
-        self.context["title_placeholders"]["hostname"] = self.context["title_placeholders"]["name"]
+        self.context["title_placeholders"]["hostname"] = self.context[
+            "title_placeholders"
+        ]["name"]
         return await self.async_step_reauth_confirm()
 
     async def async_step_reauth_confirm(
