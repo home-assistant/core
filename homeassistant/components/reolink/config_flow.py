2c510198c485ed924fb6f864d83f5d2bfaa45726
starkillerOG <starkiller.og@gmail.com>
2023-07-04 09:44:49 +0200

    fix hostname

--- homeassistant/components/reolink/config_flow.py
+++ homeassistant/components/reolink/config_flow.py
@@ -79,11 +79,8 @@ class ReolinkFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         self._username = entry_data[CONF_USERNAME]
         self._password = entry_data[CONF_PASSWORD]
         self._reauth = True
-        _LOGGER.error(self.context)
-        self.context["title_placeholders"] = {
-            "ip_address": entry_data[CONF_HOST],
-            #"hostname": discovery_info.hostname,
-        }
+        self.context["title_placeholders"]["ip_address"] = entry_data[CONF_HOST]
+        self.context["title_placeholders"]["hostname"] = self.context["title_placeholders"]["name"]
         return await self.async_step_reauth_confirm()
 
     async def async_step_reauth_confirm(
