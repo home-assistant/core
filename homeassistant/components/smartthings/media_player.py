5434994e0d97bbcc92d3386cccd3d5ec8aad33db
Guillaume Duveau <guillaume.duveau@gmail.com>
2023-07-05 20:32:59 +0000

    Cleanup SmartThings media_player code

--- homeassistant/components/smartthings/media_player.py
+++ homeassistant/components/smartthings/media_player.py
@@ -2,17 +2,18 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
+from typing import Any
 
 from pysmartthings import Capability, DeviceEntity
 
 from homeassistant.components.media_player import (
+    DOMAIN as MEDIA_PLAYER_DOMAIN,
     MediaPlayerEntity,
     MediaPlayerEntityFeature,
     MediaPlayerState,
     RepeatMode,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import STATE_PAUSED, STATE_PLAYING
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -22,11 +23,10 @@ from .const import DATA_BROKERS, DOMAIN
 CONTROLLABLE_SOURCES = ["bluetooth", "wifi"]
 
 VALUE_TO_STATE = {
-    "playing": MediaPlayerState.PLAYING,
+    "buffering": MediaPlayerState.BUFFERING,
     "paused": MediaPlayerState.PAUSED,
-    "on": MediaPlayerState.ON,
-    "off": MediaPlayerState.OFF,
-    "unknown": None,
+    "playing": MediaPlayerState.PLAYING,
+    "stopped": MediaPlayerState.IDLE,
 }
 
 
@@ -41,29 +41,36 @@ async def async_setup_entry(
         [
             SmartThingsMediaPlayer(device)
             for device in broker.devices.values()
-            if broker.any_assigned(device.device_id, "media_player")
+            if broker.any_assigned(device.device_id, MEDIA_PLAYER_DOMAIN)
         ]
     )
 
 
 def get_capabilities(capabilities: Sequence[str]) -> Sequence[str] | None:
     """Return all capabilities supported if minimum required are present."""
-    min_required = [
-        Capability.media_playback,
-        Capability.switch,
-    ]
-    all_supported = [
-        Capability.switch,
-        Capability.audio_volume,
-        Capability.media_playback_shuffle,
-        Capability.media_input_source,
+    supported = [
         Capability.audio_mute,
+        Capability.audio_volume,
+        Capability.media_input_source,
         Capability.media_playback,
         Capability.media_playback_repeat,
+        Capability.media_playback_shuffle,
+        Capability.switch,
     ]
-    # Must have one of the min_required
-    if any(capability in capabilities for capability in min_required):
-        return all_supported
+    # Must be able to be turned on/off.
+    if Capability.switch not in capabilities:
+        return None
+    # Must have one of these.
+    media_player_capabilities = [
+        Capability.audio_mute,
+        Capability.audio_volume,
+        Capability.media_input_source,
+        Capability.media_playback,
+        Capability.media_playback_repeat,
+        Capability.media_playback_shuffle,
+    ]
+    if any(capability in capabilities for capability in media_player_capabilities):
+        return supported
     return None
 
 
@@ -98,20 +105,15 @@ class SmartThingsMediaPlayer(SmartThingsEntity, MediaPlayerEntity):
         if Capability.media_playback_repeat in device.capabilities:
             self._supported_features |= MediaPlayerEntityFeature.REPEAT_SET
 
-    async def async_turn_off(self, **kwargs) -> None:
-        """Turn the switch off."""
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the media player off."""
         await self._device.switch_off(set_status=True)
-        # State is set optimistically in the command above, therefore update
-        # the entity state ahead of receiving the confirming push updates
-        self.async_schedule_update_ha_state()
         self.async_write_ha_state()
 
-    async def async_turn_on(self, **kwargs) -> None:
-        """Turn the switch on."""
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the media player on."""
         await self._device.switch_on(set_status=True)
-        # State is set optimistically in the command above, therefore update
-        # the entity state ahead of receiving the confirming push updates
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_mute_volume(self, mute: bool) -> None:
         """Mute volume."""
@@ -119,52 +121,52 @@ class SmartThingsMediaPlayer(SmartThingsEntity, MediaPlayerEntity):
             await self._device.mute(set_status=True)
         else:
             await self._device.unmute(set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_set_volume_level(self, volume: float) -> None:
         """Set volume level."""
         await self._device.set_volume(int(volume * 100), set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_volume_up(self) -> None:
         """Increase volume."""
         await self._device.volume_up(set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_volume_down(self) -> None:
         """Decrease volume."""
         await self._device.volume_down(set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_media_play(self) -> None:
         """Play media."""
         await self._device.play(set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_media_pause(self) -> None:
         """Pause media."""
         await self._device.pause(set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_media_stop(self) -> None:
         """Stop media."""
         await self._device.stop(set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_select_source(self, source: str) -> None:
         """Select source."""
         await self._device.set_input_source(source, set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_set_shuffle(self, shuffle: bool) -> None:
         """Set shuffle mode."""
         await self._device.set_playback_shuffle(shuffle, set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_set_repeat(self, repeat: RepeatMode) -> None:
         """Set repeat mode."""
         await self._device.set_repeat(repeat, set_status=True)
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     @property
     def supported_features(self) -> MediaPlayerEntityFeature:
@@ -174,23 +176,15 @@ class SmartThingsMediaPlayer(SmartThingsEntity, MediaPlayerEntity):
     @property
     def media_title(self) -> str | None:
         """Title of the current media."""
-        if self.state in [STATE_PLAYING, STATE_PAUSED]:
-            return self._device.status.attributes["trackDescription"].value
-        return None
+        return self._device.status.media_title
 
     @property
     def state(self) -> MediaPlayerState | None:
         """State of the media player."""
         if not self._device.status.switch:
             return MediaPlayerState.OFF
-            
-        if (
-            self.source is not None 
-            and self.source in CONTROLLABLE_SOURCES
-            and self._device.status.playback_status in VALUE_TO_STATE
-        ):
+        if self._device.status.playback_status in VALUE_TO_STATE:
             return VALUE_TO_STATE[self._device.status.playback_status]
-                    
         return MediaPlayerState.ON
 
     @property
@@ -234,11 +228,6 @@ class SmartThingsMediaPlayer(SmartThingsEntity, MediaPlayerEntity):
         if self.supported_features & MediaPlayerEntityFeature.REPEAT_SET:
             return self._device.status.playback_repeat_mode
         return None
-
-    @property
-    def should_poll(self) -> bool:
-        """We should poll cloud API."""
-        return True
 
     async def async_update(self) -> None:
         """Update the entity."""
