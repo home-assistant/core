ba22c3c658e34253322be37fc368a8b1011e2ec3
Adrián Moreno <adrian@morenomartinez.com>
2023-07-05 20:33:21 +0200

    Keep track of HA release when deprecation was introduced

--- homeassistant/components/zwave_js/climate.py
+++ homeassistant/components/zwave_js/climate.py
@@ -523,7 +523,8 @@ class ZWaveClimate(ZWaveBaseEntity, ClimateEntity):
         preset_mode_value = self._hvac_presets.get(preset_mode)
         if preset_mode_value is None:
             raise ValueError(f"Received an invalid preset mode: {preset_mode}")
-        # show a deprecation warning if preset_mode is dry or fan
+        # Dry and Fan preset modes are deprecated as of 2023.8
+        # Use Dry and Fan HVAC modes instead
         if preset_mode_value in (ThermostatMode.DRY, ThermostatMode.FAN):
             LOGGER.warning(
                 "Dry and Fan preset modes are deprecated and will be removed in a future release. "
