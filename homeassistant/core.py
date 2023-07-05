2e3d6555735d50922b4930f3231b8b1ab9f6d305
J. Nick Koston <nick@koston.org>
2023-07-05 07:00:37 -0500

    Add slots to the StateMachine class (#95849)

--- homeassistant/core.py
+++ homeassistant/core.py
@@ -1410,6 +1410,8 @@ class State:
 class StateMachine:
     """Helper class that tracks the state of different entities."""
 
+    __slots__ = ("_states", "_reservations", "_bus", "_loop")
+
     def __init__(self, bus: EventBus, loop: asyncio.events.AbstractEventLoop) -> None:
         """Initialize state machine."""
         self._states: dict[str, State] = {}
