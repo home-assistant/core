1aecbb9bd5e1cad82e457a690c471cb46bb8aa83
Jan Stienstra <65826735+j-stienstra@users.noreply.github.com>
2023-07-07 14:42:02 +0200

    Add full test coverage to Jellyfin (#86974)
    * Add full test coverage
    
    * Remove unreachable exception
    
    * Remove comment line. Conflicting with codecov
    
    * Use auto fixture and syrupy

--- tests/components/jellyfin/test_init.py
+++ tests/components/jellyfin/test_init.py
@@ -29,6 +29,26 @@ async def test_config_entry_not_ready(
     assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
 
 
+async def test_invalid_auth(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_jellyfin: MagicMock,
+    mock_client: MagicMock,
+) -> None:
+    """Test the Jellyfin integration handling invalid credentials."""
+    mock_client.auth.connect_to_address.return_value = await async_load_json_fixture(
+        hass,
+        "auth-connect-address.json",
+    )
+    mock_client.auth.login.return_value = await async_load_json_fixture(
+        hass,
+        "auth-login-failure.json",
+    )
+
+    mock_config_entry.add_to_hass(hass)
+    assert not await hass.config_entries.async_setup(mock_config_entry.entry_id)
+
+
 async def test_load_unload_config_entry(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
