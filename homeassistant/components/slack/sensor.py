cbd4bcd493feb62d0bf45f11d9b2e936fd9a8410
joostlek <joostlek@outlook.com>
2023-07-08 16:14:25 +0200

    Add entity translations to Slack

--- homeassistant/components/slack/sensor.py
+++ homeassistant/components/slack/sensor.py
@@ -29,7 +29,7 @@ async def async_setup_entry(
                 hass.data[DOMAIN][entry.entry_id][SLACK_DATA],
                 SensorEntityDescription(
                     key="do_not_disturb_until",
-                    name="Do not disturb until",
+                    translation_key="do_not_disturb_until",
                     icon="mdi:clock",
                     device_class=SensorDeviceClass.TIMESTAMP,
                 ),
