30a1a487733012470c1e2966d5ff1f092f02c3c1
starkillerOG <starkiller.og@gmail.com>
2023-07-08 23:04:04 +0200

    fix import

--- homeassistant/components/reolink/config_flow.py
+++ homeassistant/components/reolink/config_flow.py
@@ -17,7 +17,7 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.device_registry import format_mac
 
 from .const import CONF_PROTOCOL, CONF_USE_HTTPS, DOMAIN
-from .exceptions import ReolinkException, UserNotAdmin
+from .exceptions import ReolinkException, ReolinkWebhookException, UserNotAdmin
 from .host import ReolinkHost
 
 _LOGGER = logging.getLogger(__name__)
@@ -131,7 +131,9 @@ class ReolinkFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 errors[CONF_HOST] = "api_error"
             except ReolinkWebhookException as err:
                 placeholders["error"] = str(err)
-                placeholders["more_info"] = "https://www.home-assistant.io/more-info/no-url-available/#configuring-the-instance-url"
+                placeholders[
+                    "more_info"
+                ] = "https://www.home-assistant.io/more-info/no-url-available/#configuring-the-instance-url"
                 errors["base"] = "webhook_exception"
             except (ReolinkError, ReolinkException) as err:
                 placeholders["error"] = str(err)
