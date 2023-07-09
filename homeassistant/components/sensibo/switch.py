0c04dcad58e168c44c32fd1be3dc66c360c14ae1
G Johansson <goran.johansson@shiftit.se>
2023-07-09 11:29:50 +0000

    Fix switch

--- homeassistant/components/sensibo/switch.py
+++ homeassistant/components/sensibo/switch.py
@@ -103,7 +103,6 @@ class SensiboDeviceSwitch(SensiboDeviceBaseEntity, SwitchEntity):
     """Representation of a Sensibo Device Switch."""
 
     entity_description: SensiboDeviceSwitchEntityDescription
-    _attr_translation_key = "sensibo_switch"
 
     def __init__(
         self,
