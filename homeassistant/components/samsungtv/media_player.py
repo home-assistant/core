2fd08d7662a5d73cad55ce3b4d38561ee31a1249
joostlek <joostlek@outlook.com>
2023-07-10 19:14:14 +0200

    Migrate Samsung TV to has entity name

--- homeassistant/components/samsungtv/media_player.py
+++ homeassistant/components/samsungtv/media_player.py
@@ -73,6 +73,7 @@ class SamsungTVDevice(SamsungTVEntity, MediaPlayerEntity):
     """Representation of a Samsung TV."""
 
     _attr_source_list: list[str]
+    _attr_name = None
 
     def __init__(
         self,
