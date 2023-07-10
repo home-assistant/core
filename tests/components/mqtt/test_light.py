03628241dc075383515609fa37f66238a33f8fb5
jbouwh <jan@jbsoft.nl>
2023-07-10 20:34:34 +0000

    Fix handling MQTT light brightness from zero rgb

--- tests/components/mqtt/test_light.py
+++ tests/components/mqtt/test_light.py
@@ -666,6 +666,12 @@ async def test_brightness_from_rgb_controlling_scale(
     assert state.attributes.get("brightness") == 128
     assert state.attributes.get("rgb_color") == (255, 128, 64)
 
+    # Test zero rgb is ignored
+    async_fire_mqtt_message(hass, "test_scale_rgb/rgb/status", "0,0,0")
+    state = hass.states.get("light.test")
+    assert state.attributes.get("brightness") == 128
+    assert state.attributes.get("rgb_color") == (255, 128, 64)
+
     mqtt_mock.async_publish.reset_mock()
     await common.async_turn_on(hass, "light.test", brightness=191)
     await hass.async_block_till_done()
