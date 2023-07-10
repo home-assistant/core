1129d60c2af3e0c2c19b4c324b6e539b8d36757c
joostlek <joostlek@outlook.com>
2023-07-10 14:14:33 +0200

    Add entity translations to Tuya

--- homeassistant/helpers/check_config.py
+++ homeassistant/helpers/check_config.py
@@ -181,6 +181,7 @@ async def async_check_ha_config_file(  # noqa: C901
         if config_schema is not None:
             try:
                 config = config_schema(config)
+                result[domain] = config[domain]
             except vol.Invalid as ex:
                 _comp_error(ex, domain, config)
                 continue
