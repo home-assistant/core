82884a48904a54e5cbfc51a4f56e7d2ef93fcf82
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-07-11 15:27:31 +0200

    Avoid CI fail in command_line tests (#96324)
    * Avoid CI fail in command_line tests
    
    * Speedup tests manual update

--- tests/components/command_line/test_sensor.py
+++ tests/components/command_line/test_sensor.py
@@ -575,16 +575,18 @@ async def test_updating_to_often(
         )
         await hass.async_block_till_done()
 
-    assert len(called) == 1
+    assert called
     assert (
         "Updating Command Line Sensor Test took longer than the scheduled update interval"
         not in caplog.text
     )
+    called.clear()
+    caplog.clear()
 
     async_fire_time_changed(hass, dt_util.now() + timedelta(seconds=1))
     await hass.async_block_till_done()
 
-    assert len(called) == 2
+    assert called
     assert (
         "Updating Command Line Sensor Test took longer than the scheduled update interval"
         in caplog.text
@@ -601,13 +603,11 @@ async def test_updating_manually(
     called = []
 
     class MockCommandSensor(CommandSensor):
-        """Mock entity that updates slow."""
+        """Mock entity that updates."""
 
         async def _async_update(self) -> None:
             """Update slow."""
             called.append(1)
-            # Add waiting time
-            await asyncio.sleep(1)
 
     with patch(
         "homeassistant.components.command_line.sensor.CommandSensor",
@@ -630,7 +630,8 @@ async def test_updating_manually(
         )
         await hass.async_block_till_done()
 
-    assert len(called) == 1
+    assert called
+    called.clear()
 
     await hass.services.async_call(
         HA_DOMAIN,
@@ -639,6 +640,4 @@ async def test_updating_manually(
         blocking=True,
     )
     await hass.async_block_till_done()
-    assert len(called) == 2
-
-    await asyncio.sleep(0.2)
+    assert called
