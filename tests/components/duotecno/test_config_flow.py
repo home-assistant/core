0891fb1b6b9a90bbb01ad54945dcec22f9239b76
Maikel Punie <maikel.punie@gmail.com>
2023-07-12 09:37:30 +0200

    Finish the config flow test

--- tests/components/duotecno/test_config_flow.py
+++ tests/components/duotecno/test_config_flow.py
@@ -1,10 +1,10 @@
 """Test the duotecno config flow."""
 from unittest.mock import AsyncMock, patch
 
+from duotecno.exceptions import InvallidPassword
 import pytest
 
 from homeassistant import config_entries
-from homeassistant.components.duotecno.config_flow import CannotConnect, InvalidAuth
 from homeassistant.components.duotecno.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
@@ -18,27 +18,27 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     assert result["type"] == FlowResultType.FORM
-    assert result["errors"] is None
+    assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.duotecno.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
+        "homeassistant.components.duotecno.config_flow.validate_input",
+        return_value={"title": "1.1.1.1:1234"},
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
                 "host": "1.1.1.1",
-                "username": "test-username",
+                "port": 1234,
                 "password": "test-password",
             },
         )
         await hass.async_block_till_done()
 
     assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Name of the device"
+    assert result2["title"] == "1.1.1.1:1234"
     assert result2["data"] == {
         "host": "1.1.1.1",
-        "username": "test-username",
+        "port": 1234,
         "password": "test-password",
     }
     assert len(mock_setup_entry.mock_calls) == 1
@@ -51,14 +51,14 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
     )
 
     with patch(
-        "homeassistant.components.duotecno.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
+        "homeassistant.components.duotecno.config_flow.validate_input",
+        side_effect=InvallidPassword,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
                 "host": "1.1.1.1",
-                "username": "test-username",
+                "port": 1234,
                 "password": "test-password",
             },
         )
@@ -74,14 +74,14 @@ async def test_form_cannot_connect(hass: HomeAssistant) -> None:
     )
 
     with patch(
-        "homeassistant.components.duotecno.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
+        "homeassistant.components.duotecno.config_flow.validate_input",
+        side_effect=ConnectionError,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
                 "host": "1.1.1.1",
-                "username": "test-username",
+                "port": 1234,
                 "password": "test-password",
             },
         )
