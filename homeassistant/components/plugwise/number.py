71831ea4cd438e288610f03bd680d2bc8645b421
Tom Scholten <git@scholten.nu>
2023-07-12 15:18:46 +0200

    Remove (now redundant) ignore

--- homeassistant/components/plugwise/number.py
+++ homeassistant/components/plugwise/number.py
@@ -70,9 +70,7 @@ async def async_setup_entry(
     entities: list[PlugwiseNumberEntity] = []
     for device_id, device in coordinator.data.devices.items():
         for description in NUMBER_TYPES:
-            if (
-                actuator := device.get(description.key)
-            ) and "setpoint" in actuator:  # type: ignore[operator]
+            if (actuator := device.get(description.key)) and "setpoint" in actuator:
                 entities.append(
                     PlugwiseNumberEntity(coordinator, device_id, description)
                 )
