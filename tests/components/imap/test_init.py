0d915342e94bcc68b47d1e82b17f55746715c95d
jbouwh <jan@jbsoft.nl>
2023-07-13 07:27:07 +0000

    Add test

--- tests/components/imap/test_init.py
+++ tests/components/imap/test_init.py
@@ -2,7 +2,7 @@
 import asyncio
 from datetime import datetime, timedelta, timezone
 from typing import Any
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import AsyncMock, MagicMock, call, patch
 
 from aioimaplib import AUTH, NONAUTH, SELECTED, AioImapException, Response
 import pytest
@@ -625,3 +625,58 @@ async def test_custom_template(
     assert data["text"]
     assert data["custom"] == result
     assert error in caplog.text if error is not None else True
+
+
+@pytest.mark.parametrize(
+    ("imap_search", "imap_fetch"),
+    [(TEST_SEARCH_RESPONSE, TEST_FETCH_RESPONSE_TEXT_PLAIN)],
+)
+@pytest.mark.parametrize(
+    ("imap_has_capability", "enforce_polling", "should_poll"),
+    [
+        (True, True, True),
+        (False, True, True),
+        (True, False, False),
+        (False, False, True),
+    ],
+    ids=["enforce_poll", "poll", "auto_push", "auto_poll"],
+)
+async def test_enforce_polling(
+    hass: HomeAssistant,
+    mock_imap_protocol: MagicMock,
+    enforce_polling: bool,
+    should_poll: True,
+) -> None:
+    """Test enforce polling."""
+    event_called = async_capture_events(hass, "imap_content")
+    config = MOCK_CONFIG.copy()
+    config["enforce_polling"] = enforce_polling
+
+    config_entry = MockConfigEntry(domain=DOMAIN, data=config)
+    config_entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    # Make sure we have had one update (when polling)
+    async_fire_time_changed(hass, utcnow() + timedelta(seconds=5))
+    await hass.async_block_till_done()
+    state = hass.states.get("sensor.imap_email_email_com")
+    # we should have received one message
+    assert state is not None
+    assert state.state == "1"
+    assert state.attributes["state_class"] == SensorStateClass.MEASUREMENT
+
+    # we should have received one event
+    assert len(event_called) == 1
+    data: dict[str, Any] = event_called[0].data
+    assert data["server"] == "imap.server.com"
+    assert data["username"] == "email@email.com"
+    assert data["search"] == "UnSeen UnDeleted"
+    assert data["folder"] == "INBOX"
+    assert data["sender"] == "john.doe@example.com"
+    assert data["subject"] == "Test subject"
+    assert data["text"]
+
+    if should_poll:
+        mock_imap_protocol.wait_server_push.assert_not_called()
+    else:
+        mock_imap_protocol.assert_has_calls([call.wait_server_push])
