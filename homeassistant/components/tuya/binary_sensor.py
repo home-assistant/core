4e41331298e1f81b7f851aab3e325016626c18e2
Simon Smith <simonsmith5521@gmail.com>
2023-07-13 14:35:00 +0000

    dont need extra entitys only a set array

--- homeassistant/components/tuya/binary_sensor.py
+++ homeassistant/components/tuya/binary_sensor.py
@@ -318,17 +318,7 @@ BINARY_SENSORS: dict[str, tuple[TuyaBinarySensorEntityDescription, ...]] = {
         TuyaBinarySensorEntityDescription(
             key=DPCode.SMOKE_SENSOR_STATE,
             device_class=BinarySensorDeviceClass.SMOKE,
-            on_value="1",
-        ),
-        TuyaBinarySensorEntityDescription(
-            key=DPCode.SMOKE_SENSOR_STATUS,
-            device_class=BinarySensorDeviceClass.SMOKE,
-            on_value="1",
-        ),
-        TuyaBinarySensorEntityDescription(
-            key=DPCode.SMOKE_SENSOR_STATE,
-            device_class=BinarySensorDeviceClass.SMOKE,
-            on_value="alarm",
+            on_value={"1", "alarm"},
         ),
         TAMPER_BINARY_SENSOR,
     ),
