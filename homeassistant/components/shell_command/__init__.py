ef03696d8f165a3d34c1f64b588c460273c34977
RoboMagus <68224306+RoboMagus@users.noreply.github.com>
2023-07-16 15:50:52 +0200

    Fix mypy

--- homeassistant/components/shell_command/__init__.py
+++ homeassistant/components/shell_command/__init__.py
@@ -5,6 +5,7 @@ import asyncio
 from contextlib import suppress
 import logging
 import shlex
+from typing import cast
 
 import async_timeout
 import voluptuous as vol
@@ -130,7 +131,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             _LOGGER.exception(
                 "Error running command: `%s`, return code: %s", cmd, process.returncode
             )
-        return service_response
+        return cast(ServiceResponse, service_response)
 
     for name in conf:
         hass.services.async_register(
