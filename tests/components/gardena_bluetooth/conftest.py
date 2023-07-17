d02bf837a6cdb5e44e029bca39035212e2958d24
Joakim Plate <elupus@ecce.se>
2023-07-17 21:13:13 +0200

    Add some basic tests for gardena (#96777)

--- tests/components/gardena_bluetooth/conftest.py
+++ tests/components/gardena_bluetooth/conftest.py
@@ -1,9 +1,29 @@
 """Common fixtures for the Gardena Bluetooth tests."""
 from collections.abc import Generator
+from typing import Any
 from unittest.mock import AsyncMock, Mock, patch
 
+from freezegun import freeze_time
 from gardena_bluetooth.client import Client
+from gardena_bluetooth.const import DeviceInformation
+from gardena_bluetooth.exceptions import CharacteristicNotFound
+from gardena_bluetooth.parse import Characteristic
 import pytest
+
+from homeassistant.components.gardena_bluetooth.const import DOMAIN
+from homeassistant.const import CONF_ADDRESS
+
+from . import WATER_TIMER_SERVICE_INFO
+
+from tests.common import MockConfigEntry
+
+
+@pytest.fixture
+def mock_entry():
+    """Create hass config fixture."""
+    return MockConfigEntry(
+        domain=DOMAIN, data={CONF_ADDRESS: WATER_TIMER_SERVICE_INFO.address}
+    )
 
 
 @pytest.fixture
@@ -16,15 +36,52 @@ def mock_setup_entry() -> Generator[AsyncMock, None, None]:
         yield mock_setup_entry
 
 
+@pytest.fixture
+def mock_read_char_raw():
+    """Mock data on device."""
+    return {
+        DeviceInformation.firmware_version.uuid: b"1.2.3",
+        DeviceInformation.model_number.uuid: b"Mock Model",
+    }
+
+
 @pytest.fixture(autouse=True)
-def mock_client(enable_bluetooth):
+def mock_client(enable_bluetooth: None, mock_read_char_raw: dict[str, Any]) -> None:
     """Auto mock bluetooth."""
 
     client = Mock(spec_set=Client)
-    client.get_all_characteristics_uuid.return_value = set()
+
+    SENTINEL = object()
+
+    def _read_char(char: Characteristic, default: Any = SENTINEL):
+        try:
+            return char.decode(mock_read_char_raw[char.uuid])
+        except KeyError:
+            if default is SENTINEL:
+                raise CharacteristicNotFound from KeyError
+            return default
+
+    def _read_char_raw(uuid: str, default: Any = SENTINEL):
+        try:
+            return mock_read_char_raw[uuid]
+        except KeyError:
+            if default is SENTINEL:
+                raise CharacteristicNotFound from KeyError
+            return default
+
+    def _all_char():
+        return set(mock_read_char_raw.keys())
+
+    client.read_char.side_effect = _read_char
+    client.read_char_raw.side_effect = _read_char_raw
+    client.get_all_characteristics_uuid.side_effect = _all_char
 
     with patch(
         "homeassistant.components.gardena_bluetooth.config_flow.Client",
         return_value=client,
+    ), patch(
+        "homeassistant.components.gardena_bluetooth.Client", return_value=client
+    ), freeze_time(
+        "2023-01-01", tz_offset=1
     ):
         yield client
