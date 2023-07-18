7f5fdb102440b4f9b6344c6af02c0a4b6d4b6850
Joost Lekkerkerker <joostlek@outlook.com>
2023-07-18 17:41:33 +0200

    Clean up entity descriptions in Tuya (#96847)

--- homeassistant/components/tuya/number.py
+++ homeassistant/components/tuya/number.py
@@ -292,14 +292,14 @@ NUMBERS: dict[str, tuple[NumberEntityDescription, ...]] = {
     "szjqr": (
         NumberEntityDescription(
             key=DPCode.ARM_DOWN_PERCENT,
-            translation_key="move_down",
+            name="Move down",
             icon="mdi:arrow-down-bold",
             native_unit_of_measurement=PERCENTAGE,
             entity_category=EntityCategory.CONFIG,
         ),
         NumberEntityDescription(
             key=DPCode.ARM_UP_PERCENT,
-            translation_key="move_up",
+            name="Move up",
             icon="mdi:arrow-up-bold",
             native_unit_of_measurement=PERCENTAGE,
             entity_category=EntityCategory.CONFIG,
