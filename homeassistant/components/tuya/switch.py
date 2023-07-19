deade1032391f1deccf4ff0d9d5a1cae5070f99a
Dmitry Vasilyev <slydiman@gmail.com>
2023-07-19 22:10:12 +0400

    Update #2

--- homeassistant/components/tuya/switch.py
+++ homeassistant/components/tuya/switch.py
@@ -577,7 +577,7 @@ SWITCHES: dict[str, tuple[SwitchEntityDescription, ...]] = {
     "wnykq": (
         SwitchEntityDescription(
             key=DPCode.SWITCH,
-            name="Switch",
+            translation_key="switch",
         ),
     ),
     # SIREN: Siren (switch) with Temperature and humidity sensor
