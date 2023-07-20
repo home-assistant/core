9b54035e2926a0dc85edd3ce9d1a129abe00e4b5
Joakim Plate <elupus@ecce.se>
2023-07-20 08:47:26 +0200

    Avoid accessing coordinator in gardena_bluetooth tests (#96921)
    Avoid accessing coordinator in tests

--- tests/components/gardena_bluetooth/test_number.py
+++ tests/components/gardena_bluetooth/test_number.py
@@ -1,6 +1,7 @@
 """Test Gardena Bluetooth sensor."""
 
 
+from collections.abc import Awaitable, Callable
 from typing import Any
 from unittest.mock import Mock, call
 
@@ -62,6 +63,7 @@ async def test_setup(
     snapshot: SnapshotAssertion,
     mock_entry: MockConfigEntry,
     mock_read_char_raw: dict[str, bytes],
+    scan_step: Callable[[], Awaitable[None]],
     uuid: str,
     raw: list[bytes],
     entity_id: str,
@@ -69,12 +71,12 @@ async def test_setup(
     """Test setup creates expected entities."""
 
     mock_read_char_raw[uuid] = raw[0]
-    coordinator = await setup_entry(hass, mock_entry, [Platform.NUMBER])
+    await setup_entry(hass, mock_entry, [Platform.NUMBER])
     assert hass.states.get(entity_id) == snapshot
 
     for char_raw in raw[1:]:
         mock_read_char_raw[uuid] = char_raw
-        await coordinator.async_refresh()
+        await scan_step()
         assert hass.states.get(entity_id) == snapshot
 
 
@@ -128,6 +130,7 @@ async def test_bluetooth_error_unavailable(
     snapshot: SnapshotAssertion,
     mock_entry: MockConfigEntry,
     mock_read_char_raw: dict[str, bytes],
+    scan_step: Callable[[], Awaitable[None]],
 ) -> None:
     """Verify that a connectivity error makes all entities unavailable."""
 
@@ -138,7 +141,7 @@ async def test_bluetooth_error_unavailable(
         Valve.remaining_open_time.uuid
     ] = Valve.remaining_open_time.encode(0)
 
-    coordinator = await setup_entry(hass, mock_entry, [Platform.NUMBER])
+    await setup_entry(hass, mock_entry, [Platform.NUMBER])
     assert hass.states.get("number.mock_title_remaining_open_time") == snapshot
     assert hass.states.get("number.mock_title_manual_watering_time") == snapshot
 
@@ -146,6 +149,6 @@ async def test_bluetooth_error_unavailable(
         "Test for errors on bluetooth"
     )
 
-    await coordinator.async_refresh()
+    await scan_step()
     assert hass.states.get("number.mock_title_remaining_open_time") == snapshot
     assert hass.states.get("number.mock_title_manual_watering_time") == snapshot
