0f03909a2a88a7e0615c7e1504fec2e397a7d750
mkmer <m_kasper@sbcglobal.net>
2023-07-21 19:24:13 +0000

    missing session and AsyncMock

--- homeassistant/components/blink/__init__.py
+++ homeassistant/components/blink/__init__.py
@@ -82,7 +82,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     session = async_get_clientsession(hass)
     blink = Blink(session=session)
     auth_data = deepcopy(dict(entry.data))
-    blink.auth = Auth(auth_data, no_prompt=True, session=session)
+    blink.auth = Auth(auth_data, no_prompt=True, session=async_get_clientsession(hass))
     blink.refresh_rate = entry.options.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
 
     try:
