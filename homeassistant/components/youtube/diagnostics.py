5d810e6d87eb2bbd02351ab852ec0a8a1bf75203
joostlek <joostlek@outlook.com>
2023-07-21 11:26:38 +0200

    Fix feedback

--- homeassistant/components/youtube/diagnostics.py
+++ homeassistant/components/youtube/diagnostics.py
@@ -3,11 +3,10 @@ from __future__ import annotations
 
 from typing import Any
 
-from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import COORDINATOR, DOMAIN
+from .const import ATTR_DESCRIPTION, ATTR_LATEST_VIDEO, COORDINATOR, DOMAIN
 from .coordinator import YouTubeDataUpdateCoordinator
 
 
@@ -18,4 +17,8 @@ async def async_get_config_entry_diagnostics(
     coordinator: YouTubeDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
         COORDINATOR
     ]
-    return async_redact_data(coordinator.data, {})
+    sensor_data = {}
+    for channel_id, channel_data in coordinator.data.items():
+        channel_data.get(ATTR_LATEST_VIDEO, {}).pop(ATTR_DESCRIPTION)
+        sensor_data[channel_id] = channel_data
+    return sensor_data
--- tests/components/youtube/test_diagnostics.py
+++ tests/components/youtube/test_diagnostics.py
@@ -1,5 +1,5 @@
-"""Tests for the diagnostics data provided by the KNX integration."""
-import json
+"""Tests for the diagnostics data provided by the YouTube integration."""
+from syrupy import SnapshotAssertion
 
 from homeassistant.components.youtube.const import DOMAIN
 from homeassistant.core import HomeAssistant
@@ -14,13 +14,10 @@ async def test_diagnostics(
     hass: HomeAssistant,
     hass_client: ClientSessionGenerator,
     setup_integration: ComponentSetup,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test diagnostics."""
     await setup_integration()
     entry = hass.config_entries.async_entries(DOMAIN)[0]
 
-    from tests.common import load_fixture
-
-    assert await get_diagnostics_for_config_entry(
-        hass, hass_client, entry
-    ) == json.loads(load_fixture("youtube/diagnostics.json"))
+    assert await get_diagnostics_for_config_entry(hass, hass_client, entry) == snapshot
