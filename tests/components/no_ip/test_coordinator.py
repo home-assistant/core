a795fc6b521f67e00714fb8c4f1569862fe55cf7
Ludy87 <Ludy87@users.noreply.github.com>
2023-07-22 18:38:44 +0000

    add test_coordinator.py

---
+++ tests/components/no_ip/test_coordinator.py
@@ -0,0 +1,49 @@
+"""Test the No-IP.com Coordinator."""
+from typing import Any
+from unittest.mock import patch
+
+from homeassistant.components.no_ip import DOMAIN, NoIPDataUpdateCoordinator
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_DOMAIN, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
+
+
+# Mock-Funktion für _async_update_data
+async def mock_async_update_data(self) -> dict[str, Any]:
+    """Mock for the _async_update_data method."""
+    return {
+        CONF_DOMAIN: "test",
+        CONF_USERNAME: None,
+        CONF_PASSWORD: None,
+    }
+
+
+@patch(
+    "homeassistant.components.no_ip.coordinator.NoIPDataUpdateCoordinator._async_update_data",
+    mock_async_update_data,
+)
+async def test_coordinator_update(hass: HomeAssistant) -> None:
+    """Test coordinator update."""
+    # Create a new ConfigEntry with your configuration data
+    config_entry = ConfigEntry(
+        version=1,
+        domain=DOMAIN,
+        title="test",
+        data={CONF_DOMAIN: "test", CONF_USERNAME: None, CONF_PASSWORD: None},
+        source="test",
+        options={},
+    )
+
+    # Create a coordinator instance using the ConfigEntry
+    coordinator = NoIPDataUpdateCoordinator(hass, config_entry)
+
+    # Fetch the updated data using the coordinator
+    data = await coordinator._async_update_data()
+
+    # Add more assertions based on the actual data you expect
+    assert data == {
+        CONF_DOMAIN: "test",
+        CONF_USERNAME: None,
+        CONF_PASSWORD: None,
+        # Add more keys based on the expected response from the API
+    }
