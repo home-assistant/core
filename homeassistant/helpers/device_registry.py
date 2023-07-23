047f3bebe00fc39681c5c85dff15552240cd560a
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-07-23 22:27:53 +0200

    Use EventType for more helper methods

--- homeassistant/helpers/device_registry.py
+++ homeassistant/helpers/device_registry.py
@@ -5,10 +5,11 @@ from collections import UserDict
 from collections.abc import Coroutine, ValuesView
 import logging
 import time
-from typing import TYPE_CHECKING, Any, TypeVar, cast
+from typing import TYPE_CHECKING, Any, Literal, TypedDict, TypeVar, cast
 from urllib.parse import urlparse
 
 import attr
+from typing_extensions import NotRequired
 
 from homeassistant.backports.enum import StrEnum
 from homeassistant.const import EVENT_HOMEASSISTANT_STARTED, EVENT_HOMEASSISTANT_STOP
@@ -94,6 +95,14 @@ DEVICE_INFO_TYPES = {
 }
 
 DEVICE_INFO_KEYS = set.union(*(itm for itm in DEVICE_INFO_TYPES.values()))
+
+
+class EventDeviceRegistryUpdatedData(TypedDict):
+    """EventDeviceRegistryUpdated data."""
+
+    action: Literal["create", "remove", "update"]
+    device_id: str
+    changes: NotRequired[dict[str, Any]]
 
 
 class DeviceEntryType(StrEnum):
--- homeassistant/helpers/entity_registry.py
+++ homeassistant/helpers/entity_registry.py
@@ -14,9 +14,10 @@ from collections.abc import Callable, Iterable, Mapping, ValuesView
 from datetime import datetime, timedelta
 import logging
 import time
-from typing import TYPE_CHECKING, Any, TypeVar, cast
+from typing import TYPE_CHECKING, Any, Literal, TypedDict, TypeVar, cast
 
 import attr
+from typing_extensions import NotRequired
 import voluptuous as vol
 
 from homeassistant.backports.enum import StrEnum
@@ -105,6 +106,15 @@ class RegistryEntryHider(StrEnum):
 
     INTEGRATION = "integration"
     USER = "user"
+
+
+class EventEntityRegistryUpdatedData(TypedDict):
+    """EventEntityRegistryUpdated data."""
+
+    action: Literal["create", "remove", "update"]
+    entity_id: str
+    changes: NotRequired[dict[str, Any]]
+    old_entity_id: NotRequired[str]
 
 
 EntityOptionsType = Mapping[str, Mapping[str, Any]]
