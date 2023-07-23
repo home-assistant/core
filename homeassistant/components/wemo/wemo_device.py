86708b5590ebf79ba00895c2ae4f245b43ceee2c
Franck Nijhof <git@frenck.dev>
2023-07-23 22:00:26 +0200

    Update ruff to v0.0.280 (#97102)

--- homeassistant/components/wemo/wemo_device.py
+++ homeassistant/components/wemo/wemo_device.py
@@ -35,7 +35,7 @@ from .models import async_wemo_data
 _LOGGER = logging.getLogger(__name__)
 
 # Literal values must match options.error keys from strings.json.
-ErrorStringKey = Literal["long_press_requires_subscription"]
+ErrorStringKey = Literal["long_press_requires_subscription"]  # noqa: F821
 # Literal values must match options.step.init.data keys from strings.json.
 OptionsFieldKey = Literal["enable_subscription", "enable_long_press"]
 
