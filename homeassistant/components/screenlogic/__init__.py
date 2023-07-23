61616893b30f14b6888962a91a6186afee205b55
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2023-07-23 07:31:02 +0000

    Outdent entity migration block

--- homeassistant/components/screenlogic/__init__.py
+++ homeassistant/components/screenlogic/__init__.py
@@ -119,65 +119,65 @@ async def _async_migrate_entries(
             source_key,
         )
 
-        if source_key in ENTITY_MIGRATIONS:
-            _LOGGER.debug(
-                "Evaluating migration of '%s' from migration key '%s'",
-                entry.entity_id,
-                source_key,
-            )
-            migrations = ENTITY_MIGRATIONS[source_key]
-            updates: dict[str, Any] = {}
-            new_key = migrations["new_key"]
-            if new_key in SHARED_VALUES:
-                if (device := migrations.get("device")) is None:
-                    _LOGGER.debug(
-                        "Shared key '%s' is missing required migration data 'device'",
-                        new_key,
-                    )
-                    continue
-                assert device != "pump" or (device == "pump" and source_index)
-                new_unique_id = (
-                    f"{source_mac}_{generate_unique_id(device, source_index, new_key)}"
-                )
-            else:
-                new_unique_id = entry.unique_id.replace(source_key, new_key)
+        if source_key not in ENTITY_MIGRATIONS:
+            continue
 
-            if new_unique_id and new_unique_id != entry.unique_id:
-                if existing_entity_id := entity_registry.async_get_entity_id(
-                    entry.domain, entry.platform, new_unique_id
-                ):
-                    _LOGGER.debug(
-                        "Cannot migrate '%s' to unique_id '%s', already exists for entity '%s'. Aborting",
-                        entry.unique_id,
-                        new_unique_id,
-                        existing_entity_id,
-                    )
-                    continue
-                updates["new_unique_id"] = new_unique_id
-
-            if (old_name := migrations.get("old_name")) is not None:
-                assert old_name
-                new_name = migrations["new_name"]
-                if (s_old_name := slugify(old_name)) in entry.entity_id:
-                    new_entity_id = entry.entity_id.replace(
-                        s_old_name, slugify(new_name)
-                    )
-                    if new_entity_id and new_entity_id != entry.entity_id:
-                        updates["new_entity_id"] = new_entity_id
-
-                if entry.original_name and old_name in entry.original_name:
-                    new_original_name = entry.original_name.replace(old_name, new_name)
-                    if new_original_name and new_original_name != entry.original_name:
-                        updates["original_name"] = new_original_name
-
-            if updates:
+        _LOGGER.debug(
+            "Evaluating migration of '%s' from migration key '%s'",
+            entry.entity_id,
+            source_key,
+        )
+        migrations = ENTITY_MIGRATIONS[source_key]
+        updates: dict[str, Any] = {}
+        new_key = migrations["new_key"]
+        if new_key in SHARED_VALUES:
+            if (device := migrations.get("device")) is None:
                 _LOGGER.debug(
-                    "Migrating entity '%s' unique_id from '%s' to '%s'",
-                    entry.entity_id,
+                    "Shared key '%s' is missing required migration data 'device'",
+                    new_key,
+                )
+                continue
+            assert device != "pump" or (device == "pump" and source_index)
+            new_unique_id = (
+                f"{source_mac}_{generate_unique_id(device, source_index, new_key)}"
+            )
+        else:
+            new_unique_id = entry.unique_id.replace(source_key, new_key)
+
+        if new_unique_id and new_unique_id != entry.unique_id:
+            if existing_entity_id := entity_registry.async_get_entity_id(
+                entry.domain, entry.platform, new_unique_id
+            ):
+                _LOGGER.debug(
+                    "Cannot migrate '%s' to unique_id '%s', already exists for entity '%s'. Aborting",
                     entry.unique_id,
                     new_unique_id,
+                    existing_entity_id,
                 )
-                entity_registry.async_update_entity(entry.entity_id, **updates)
+                continue
+            updates["new_unique_id"] = new_unique_id
+
+        if (old_name := migrations.get("old_name")) is not None:
+            assert old_name
+            new_name = migrations["new_name"]
+            if (s_old_name := slugify(old_name)) in entry.entity_id:
+                new_entity_id = entry.entity_id.replace(s_old_name, slugify(new_name))
+                if new_entity_id and new_entity_id != entry.entity_id:
+                    updates["new_entity_id"] = new_entity_id
+
+            if entry.original_name and old_name in entry.original_name:
+                new_original_name = entry.original_name.replace(old_name, new_name)
+                if new_original_name and new_original_name != entry.original_name:
+                    updates["original_name"] = new_original_name
+
+        if updates:
+            _LOGGER.debug(
+                "Migrating entity '%s' unique_id from '%s' to '%s'",
+                entry.entity_id,
+                entry.unique_id,
+                new_unique_id,
+            )
+            entity_registry.async_update_entity(entry.entity_id, **updates)
 
 
 async def async_get_connect_info(
