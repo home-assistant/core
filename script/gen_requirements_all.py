c01dd2d67ebdbb85d3abb3f5e283b511336611c6
J. Nick Koston <nick@koston.org>
2023-07-24 12:54:54 -0500

    Bump anyio to 3.7.1
    changelog: https://anyio.readthedocs.io/en/stable/versionhistory.html

--- script/gen_requirements_all.py
+++ script/gen_requirements_all.py
@@ -102,7 +102,7 @@ regex==2021.8.28
 # these requirements are quite loose. As the entire stack has some outstanding issues, and
 # even newer versions seem to introduce new issues, it's useful for us to pin all these
 # requirements so we can directly link HA versions to these library versions.
-anyio==3.7.0
+anyio==3.7.1
 h11==0.14.0
 httpcore==0.17.3
 
