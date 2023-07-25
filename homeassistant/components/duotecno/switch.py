69d8748b1ba56ee2df226ea1f71acc4958c74560
Maikel Punie <maikel.punie@gmail.com>
2023-07-25 10:22:47 +0200

    Use oserror instead of broad exception

--- homeassistant/components/duotecno/switch.py
+++ homeassistant/components/duotecno/switch.py
@@ -39,12 +39,12 @@ class DuotecnoSwitch(DuotecnoEntity, SwitchEntity):
         """Instruct the switch to turn on."""
         try:
             await self._unit.turn_on()
-        except Exception as err:  # pylint: disable=broad-except
+        except OSError as err:
             raise HomeAssistantError("Transmit for the turn_on packet failed") from err
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Instruct the switch to turn off."""
         try:
             await self._unit.turn_off()
-        except Exception as err:  # pylint: disable=broad-except
+        except OSError as err:
             raise HomeAssistantError("Transmit for the turn_off packet failed") from err
