bf851f26a96bf0cf5da802ca50f0d3956e960798
Jan Pecinovsky <jan.pecinovsky@energieid.be>
2023-07-28 09:36:31 +0000

    remove options flow

--- homeassistant/components/energyid/const.py
+++ homeassistant/components/energyid/const.py
@@ -9,10 +9,7 @@ CONF_ENTITY_ID: Final["str"] = "entity_id"
 CONF_METRIC: Final["str"] = "metric"
 CONF_METRIC_KIND: Final["str"] = "metric_kind"
 CONF_UNIT: Final["str"] = "unit"
-CONF_DATA_INTERVAL: Final["str"] = "data_interval"
 DEFAULT_DATA_INTERVAL: Final["str"] = "P1D"
-CONF_UPLOAD_INTERVAL: Final["str"] = "upload_interval"
 DEFAULT_UPLOAD_INTERVAL: Final[int] = 300
 
-ENERGYID_INTERVALS = ["P1M", "P1D", "PT1H", "PT15M", "PT5M"]
 ENERGYID_METRIC_KINDS = ["cumulative", "total", "delta", "gauge"]
