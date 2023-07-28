f03a27a111e33781e37dbb626b3c20980b97c0a0
Jan Pecinovsky <jan.pecinovsky@energieid.be>
2023-07-28 09:36:31 +0000

    remove options flow

--- homeassistant/components/energyid/config_flow.py
+++ homeassistant/components/energyid/config_flow.py
@@ -6,38 +6,24 @@ from typing import Any
 
 import aiohttp
 from energyid_webhooks import WebhookClientAsync
-from energyid_webhooks.webhookpolicy import WebhookPolicy
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
-from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import (
-    CONF_DATA_INTERVAL,
     CONF_ENTITY_ID,
     CONF_METRIC,
     CONF_METRIC_KIND,
     CONF_UNIT,
-    CONF_UPLOAD_INTERVAL,
     CONF_WEBHOOK_URL,
-    DEFAULT_DATA_INTERVAL,
-    DEFAULT_UPLOAD_INTERVAL,
     DOMAIN,
-    ENERGYID_INTERVALS,
     ENERGYID_METRIC_KINDS,
 )
 
 _LOGGER = logging.getLogger(__name__)
-
-
-async def validate_interval(interval: str, webhook_policy: WebhookPolicy) -> bool:
-    """Validate if the interval is valid for the webhook policy."""
-    if interval not in webhook_policy.allowed_intervals:
-        raise InvalidInterval
-    return True
 
 
 def hass_entity_ids(hass: HomeAssistant) -> list[str]:
@@ -96,68 +82,3 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=data_schema, errors=errors
         )
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(
-        config_entry: config_entries.ConfigEntry,
-    ) -> config_entries.OptionsFlow:
-        """Get the options flow."""
-        return OptionsFlowHandler(config_entry)
-
-
-class OptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options flow changes."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Manage the options."""
-        errors: dict[str, str] = {}
-        if user_input is not None:
-            http_session = async_get_clientsession(self.hass)
-            client = WebhookClientAsync(
-                webhook_url=self.config_entry.data.get(CONF_WEBHOOK_URL),
-                session=http_session,
-            )
-            try:
-                webhook_policy = await client.policy
-                await validate_interval(
-                    interval=user_input[CONF_DATA_INTERVAL],
-                    webhook_policy=webhook_policy,
-                )
-            except InvalidInterval:
-                errors[CONF_DATA_INTERVAL] = "invalid_interval"
-            else:
-                return self.async_create_entry(
-                    title=self.config_entry.title, data=user_input
-                )
-
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_DATA_INTERVAL,
-                        default=self.config_entry.options.get(
-                            CONF_DATA_INTERVAL, DEFAULT_DATA_INTERVAL
-                        ),
-                    ): vol.In(ENERGYID_INTERVALS),
-                    vol.Required(
-                        CONF_UPLOAD_INTERVAL,
-                        default=self.config_entry.options.get(
-                            CONF_UPLOAD_INTERVAL, DEFAULT_UPLOAD_INTERVAL
-                        ),
-                    ): int,
-                }
-            ),
-            errors=errors,
-        )
-
-
-class InvalidInterval(HomeAssistantError):
-    """Error to indicate there is invalid interval."""
