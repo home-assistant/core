bd42bac41370b87208a3f363f1996bb109bf5d68
David Knowles <dknowles2@gmail.com>
2023-07-28 12:28:56 +0000

    Update sensor name in test

--- tests/components/schlage/test_sensor.py
+++ tests/components/schlage/test_sensor.py
@@ -23,7 +23,7 @@ async def test_battery_sensor(
     hass: HomeAssistant, mock_added_config_entry: ConfigEntry
 ) -> None:
     """Test the battery sensor."""
-    battery_sensor = hass.states.get("sensor.vault_door")
+    battery_sensor = hass.states.get("sensor.vault_door_battery")
     assert battery_sensor is not None
     assert battery_sensor.state == "20"
     assert battery_sensor.attributes["unit_of_measurement"] == PERCENTAGE
