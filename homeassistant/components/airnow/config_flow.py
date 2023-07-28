71e3aa9a6e61ecb0062637a91fa592abefe3e3e1
J.P. Krauss <jkrauss@asymworks.com>
2023-07-28 15:41:39 -0700

    Apply suggestions from code review
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- homeassistant/components/airnow/config_flow.py
+++ homeassistant/components/airnow/config_flow.py
@@ -103,21 +103,18 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def async_get_options_flow(
         config_entry: config_entries.ConfigEntry,
     ) -> config_entries.OptionsFlow:
-        """Create the options flow."""
+        """Return the options flow."""
         return OptionsFlowHandler(config_entry)
 
 
-class OptionsFlowHandler(config_entries.OptionsFlow):
+class OptionsFlowHandler(config_entries.OptionsFlowWithConfigEntry):
     """Handle an options flow for AirNow."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(self, user_input):
         """Manage the options."""
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            return self.async_create_entry(data=user_input)
 
         return self.async_show_form(
             step_id="init",
