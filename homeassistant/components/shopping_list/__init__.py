28e4f0cac04155c5c4539b16b557e737794096d1
dougiteixeira <31328123+dougiteixeira@users.noreply.github.com>
2023-07-28 03:41:49 +0000

    Remove title config entry for Shopping List

--- homeassistant/components/shopping_list/__init__.py
+++ homeassistant/components/shopping_list/__init__.py
@@ -46,6 +46,21 @@ SERVICE_SORT_SCHEMA = vol.Schema(
 )
 
 
+async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Migrate old entry."""
+    _LOGGER.debug("Migrating from version %s", config_entry.version)
+
+    if config_entry.version == 1:
+        # Remove title from configuration entry
+        config_entry.title = ""
+        config_entry.version = 2
+        hass.config_entries.async_update_entry(config_entry)
+
+    _LOGGER.info("Migration to version %s successful", config_entry.version)
+
+    return True
+
+
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Initialize the shopping list."""
 
