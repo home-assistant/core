c0bc36b431a0e4e8de96282af5df7ff8745223f8
J. Nick Koston <nick@koston.org>
2023-07-29 23:18:32 -0700

    fix

--- homeassistant/components/bluetooth/__init__.py
+++ homeassistant/components/bluetooth/__init__.py
@@ -38,7 +38,7 @@ from homeassistant.helpers.event import async_call_later
 from homeassistant.helpers.issue_registry import async_delete_issue
 from homeassistant.loader import async_get_bluetooth
 
-from . import models, passive_update_processor
+from . import models
 from .api import (
     _get_manager,
     async_address_present,
@@ -125,6 +125,9 @@ async def _async_get_adapter_from_address(
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the bluetooth integration."""
+    from . import passive_update_processor  # pylint: disable=import-outside-toplevel
+
+    await passive_update_processor.async_setup(hass)
     integration_matcher = IntegrationMatcher(await async_get_bluetooth(hass))
     integration_matcher.async_setup()
     bluetooth_adapters = get_adapters()
@@ -132,7 +135,6 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     await bluetooth_storage.async_setup()
     slot_manager = BleakSlotManager()
     await slot_manager.async_setup()
-    await passive_update_processor.async_setup(hass)
     manager = BluetoothManager(
         hass, integration_matcher, bluetooth_adapters, bluetooth_storage, slot_manager
     )
