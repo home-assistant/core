91ac9efd788ec100181ccedd6a71851187132631
Michael <35783820+mib1185@users.noreply.github.com>
2023-08-02 08:29:00 +0200

    Do not set hass data before first coordinator refresh (#97343)

--- homeassistant/components/devolo_home_network/__init__.py
+++ homeassistant/components/devolo_home_network/__init__.py
@@ -64,6 +64,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             f"Unable to connect to {entry.data[CONF_IP_ADDRESS]}"
         ) from err
 
+    hass.data[DOMAIN][entry.entry_id] = {"device": device}
+
     async def async_update_connected_plc_devices() -> LogicalNetwork:
         """Fetch data from API endpoint."""
         assert device.plcnet
@@ -155,10 +157,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             update_interval=SHORT_UPDATE_INTERVAL,
         )
 
-    hass.data[DOMAIN][entry.entry_id] = {"device": device, "coordinators": coordinators}
-
     for coordinator in coordinators.values():
         await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id]["coordinators"] = coordinators
 
     await hass.config_entries.async_forward_entry_setups(entry, platforms(device))
 
