51fa0c6f9ebd7626516cd4b3e482c22986d1b611
G Johansson <goran.johansson@shiftit.se>
2023-08-03 08:54:08 +0000

    Fix Melcloud import issue

--- homeassistant/components/melcloud/config_flow.py
+++ homeassistant/components/melcloud/config_flow.py
@@ -11,7 +11,10 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_TOKEN, CONF_USERNAME
+from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN
+from homeassistant.data_entry_flow import AbortFlow, FlowResultType
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 
 from .const import DOMAIN
 
@@ -24,7 +27,25 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     async def _create_entry(self, username: str, token: str):
         """Register new entry."""
         await self.async_set_unique_id(username)
-        self._abort_if_unique_id_configured({CONF_TOKEN: token})
+        try:
+            self._abort_if_unique_id_configured({CONF_TOKEN: token})
+        except AbortFlow:
+            if self.context["source"] == config_entries.SOURCE_IMPORT:
+                async_create_issue(
+                    self.hass,
+                    HOMEASSISTANT_DOMAIN,
+                    f"deprecated_yaml_{DOMAIN}",
+                    breaks_in_ha_version="2024.2.0",
+                    is_fixable=False,
+                    issue_domain=DOMAIN,
+                    severity=IssueSeverity.WARNING,
+                    translation_key="deprecated_yaml",
+                    translation_placeholders={
+                        "domain": DOMAIN,
+                        "integration_title": "Melcloud",
+                    },
+                )
+            raise
         return self.async_create_entry(
             title=username, data={CONF_USERNAME: username, CONF_TOKEN: token}
         )
@@ -37,11 +58,6 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         token: str | None = None,
     ):
         """Create client."""
-        if password is None and token is None:
-            raise ValueError(
-                "Invalid internal state. Called without either password or token"
-            )
-
         try:
             async with timeout(10):
                 if (acquired_token := token) is None:
@@ -56,9 +72,39 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 )
         except ClientResponseError as err:
             if err.status in (HTTPStatus.UNAUTHORIZED, HTTPStatus.FORBIDDEN):
+                if self.context["source"] == config_entries.SOURCE_IMPORT:
+                    async_create_issue(
+                        self.hass,
+                        DOMAIN,
+                        "deprecated_yaml_import_issue_invalid_auth",
+                        breaks_in_ha_version="2024.2.0",
+                        is_fixable=False,
+                        severity=IssueSeverity.ERROR,
+                        translation_key="deprecated_yaml_import_issue_invalid_auth",
+                    )
                 return self.async_abort(reason="invalid_auth")
+            if self.context["source"] == config_entries.SOURCE_IMPORT:
+                async_create_issue(
+                    self.hass,
+                    DOMAIN,
+                    "deprecated_yaml_import_issue_cannot_connect",
+                    breaks_in_ha_version="2024.2.0",
+                    is_fixable=False,
+                    severity=IssueSeverity.ERROR,
+                    translation_key="deprecated_yaml_import_issue_cannot_connect",
+                )
             return self.async_abort(reason="cannot_connect")
         except (asyncio.TimeoutError, ClientError):
+            if self.context["source"] == config_entries.SOURCE_IMPORT:
+                async_create_issue(
+                    self.hass,
+                    DOMAIN,
+                    "deprecated_yaml_import_issue_cannot_connect",
+                    breaks_in_ha_version="2024.2.0",
+                    is_fixable=False,
+                    severity=IssueSeverity.ERROR,
+                    translation_key="deprecated_yaml_import_issue_cannot_connect",
+                )
             return self.async_abort(reason="cannot_connect")
 
         return await self._create_entry(username, acquired_token)
@@ -77,6 +123,22 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_import(self, user_input):
         """Import a config entry."""
-        return await self._create_client(
+        result = await self._create_client(
             user_input[CONF_USERNAME], token=user_input[CONF_TOKEN]
         )
+        if result["type"] == FlowResultType.CREATE_ENTRY:
+            async_create_issue(
+                self.hass,
+                HOMEASSISTANT_DOMAIN,
+                f"deprecated_yaml_{DOMAIN}",
+                breaks_in_ha_version="2024.2.0",
+                is_fixable=False,
+                issue_domain=DOMAIN,
+                severity=IssueSeverity.WARNING,
+                translation_key="deprecated_yaml",
+                translation_placeholders={
+                    "domain": DOMAIN,
+                    "integration_title": "Melcloud",
+                },
+            )
+        return result
