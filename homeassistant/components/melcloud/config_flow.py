1c0a3f4a5d2c095f1b1c106acecac6a4c1060b57
G Johansson <goran.johansson@shiftit.se>
2023-08-03 10:15:34 +0000

    Simplify

--- homeassistant/components/melcloud/config_flow.py
+++ homeassistant/components/melcloud/config_flow.py
@@ -11,12 +11,45 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_TOKEN, CONF_USERNAME
-from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN
+from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
 from homeassistant.data_entry_flow import AbortFlow, FlowResultType
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 
 from .const import DOMAIN
+
+
+async def async_create_import_issue(
+    hass: HomeAssistant, source: str, issue: str, success: bool = False
+) -> None:
+    """Create issue from import."""
+    if source != config_entries.SOURCE_IMPORT:
+        return
+    if not success:
+        async_create_issue(
+            hass,
+            DOMAIN,
+            f"deprecated_yaml_import_issue_{issue}",
+            breaks_in_ha_version="2024.2.0",
+            is_fixable=False,
+            severity=IssueSeverity.ERROR,
+            translation_key=f"deprecated_yaml_import_issue_{issue}",
+        )
+        return
+    async_create_issue(
+        hass,
+        HOMEASSISTANT_DOMAIN,
+        f"deprecated_yaml_{DOMAIN}",
+        breaks_in_ha_version="2024.2.0",
+        is_fixable=False,
+        issue_domain=DOMAIN,
+        severity=IssueSeverity.WARNING,
+        translation_key="deprecated_yaml",
+        translation_placeholders={
+            "domain": DOMAIN,
+            "integration_title": "Melcloud",
+        },
+    )
 
 
 class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
@@ -30,21 +63,7 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         try:
             self._abort_if_unique_id_configured({CONF_TOKEN: token})
         except AbortFlow:
-            if self.context["source"] == config_entries.SOURCE_IMPORT:
-                async_create_issue(
-                    self.hass,
-                    HOMEASSISTANT_DOMAIN,
-                    f"deprecated_yaml_{DOMAIN}",
-                    breaks_in_ha_version="2024.2.0",
-                    is_fixable=False,
-                    issue_domain=DOMAIN,
-                    severity=IssueSeverity.WARNING,
-                    translation_key="deprecated_yaml",
-                    translation_placeholders={
-                        "domain": DOMAIN,
-                        "integration_title": "Melcloud",
-                    },
-                )
+            await async_create_import_issue(self.hass, self.context["source"], "", True)
             raise
         return self.async_create_entry(
             title=username, data={CONF_USERNAME: username, CONF_TOKEN: token}
@@ -72,39 +91,18 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 )
         except ClientResponseError as err:
             if err.status in (HTTPStatus.UNAUTHORIZED, HTTPStatus.FORBIDDEN):
-                if self.context["source"] == config_entries.SOURCE_IMPORT:
-                    async_create_issue(
-                        self.hass,
-                        DOMAIN,
-                        "deprecated_yaml_import_issue_invalid_auth",
-                        breaks_in_ha_version="2024.2.0",
-                        is_fixable=False,
-                        severity=IssueSeverity.ERROR,
-                        translation_key="deprecated_yaml_import_issue_invalid_auth",
-                    )
-                return self.async_abort(reason="invalid_auth")
-            if self.context["source"] == config_entries.SOURCE_IMPORT:
-                async_create_issue(
-                    self.hass,
-                    DOMAIN,
-                    "deprecated_yaml_import_issue_cannot_connect",
-                    breaks_in_ha_version="2024.2.0",
-                    is_fixable=False,
-                    severity=IssueSeverity.ERROR,
-                    translation_key="deprecated_yaml_import_issue_cannot_connect",
+                await async_create_import_issue(
+                    self.hass, self.context["source"], "invalid_auth"
                 )
+                return self.async_abort(reason="invalid_auth")
+            await async_create_import_issue(
+                self.hass, self.context["source"], "cannot_connect"
+            )
             return self.async_abort(reason="cannot_connect")
         except (asyncio.TimeoutError, ClientError):
-            if self.context["source"] == config_entries.SOURCE_IMPORT:
-                async_create_issue(
-                    self.hass,
-                    DOMAIN,
-                    "deprecated_yaml_import_issue_cannot_connect",
-                    breaks_in_ha_version="2024.2.0",
-                    is_fixable=False,
-                    severity=IssueSeverity.ERROR,
-                    translation_key="deprecated_yaml_import_issue_cannot_connect",
-                )
+            await async_create_import_issue(
+                self.hass, self.context["source"], "cannot_connect"
+            )
             return self.async_abort(reason="cannot_connect")
 
         return await self._create_entry(username, acquired_token)
@@ -127,18 +125,5 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             user_input[CONF_USERNAME], token=user_input[CONF_TOKEN]
         )
         if result["type"] == FlowResultType.CREATE_ENTRY:
-            async_create_issue(
-                self.hass,
-                HOMEASSISTANT_DOMAIN,
-                f"deprecated_yaml_{DOMAIN}",
-                breaks_in_ha_version="2024.2.0",
-                is_fixable=False,
-                issue_domain=DOMAIN,
-                severity=IssueSeverity.WARNING,
-                translation_key="deprecated_yaml",
-                translation_placeholders={
-                    "domain": DOMAIN,
-                    "integration_title": "Melcloud",
-                },
-            )
+            await async_create_import_issue(self.hass, self.context["source"], "", True)
         return result
