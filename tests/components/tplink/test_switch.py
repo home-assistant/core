home-assistant/core

commit 2dc86364f33411e18e077172fb8a860d559c5a99

Migrate TPLink to has entity name (#96246)

homeassistant/components/tplink/switch.py
@@ -35,7 +35,7 @@ async def async_setup_entry(
         # Historically we only add the children if the device is a strip
         _LOGGER.debug("Initializing strip with %s sockets", len(device.children))
         for child in device.children:
-            entities.append(SmartPlugSwitch(child, coordinator))
+            entities.append(SmartPlugSwitchChild(device, coordinator, child))
     elif device.is_plug:
         entities.append(SmartPlugSwitch(device, coordinator))
 
@@ -49,6 +49,7 @@ class SmartPlugLedSwitch(CoordinatedTPLinkEntity, SwitchEntity):
 
     device: SmartPlug
 
+    _attr_translation_key = "led"
     _attr_entity_category = EntityCategory.CONFIG
 
     def __init__(
@@ -57,7 +58,6 @@ class SmartPlugLedSwitch(CoordinatedTPLinkEntity, SwitchEntity):
         """Initialize the LED switch."""
         super().__init__(device, coordinator)
 
-        self._attr_name = f"{device.alias} LED"
         self._attr_unique_id = f"{self.device.mac}_led"
 
     @property
@@ -103,3 +103,29 @@ class SmartPlugSwitch(CoordinatedTPLinkEntity, SwitchEntity):
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the switch off."""
         await self.device.turn_off()
+
+
+class SmartPlugSwitchChild(SmartPlugSwitch):
+    """Representation of an individual plug of a TPLink Smart Plug strip."""
+
+    def __init__(
+        self,
+        device: SmartDevice,
+        coordinator: TPLinkDataUpdateCoordinator,
+        plug: SmartDevice,
+    ) -> None:
+        """Initialize the switch."""
+        super().__init__(device, coordinator)
+        self._plug = plug
+        self._attr_unique_id = legacy_device_id(plug)
+        self._attr_name = plug.alias
+
+    @async_refresh_after
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the switch on."""
+        await self._plug.turn_on()
+
+    @async_refresh_after
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the switch off."""
+        await self._plug.turn_off()

tests/components/tplink/test_switch.py
@@ -147,7 +147,7 @@ async def test_strip(hass: HomeAssistant) -> None:
     assert hass.states.get("switch.my_strip") is None
 
     for plug_id in range(2):
-        entity_id = f"switch.plug{plug_id}"
+        entity_id = f"switch.my_strip_plug{plug_id}"
         state = hass.states.get(entity_id)
         assert state.state == STATE_ON
 
@@ -176,7 +176,7 @@ async def test_strip_unique_ids(hass: HomeAssistant) -> None:
         await hass.async_block_till_done()
 
     for plug_id in range(2):
-        entity_id = f"switch.plug{plug_id}"
+        entity_id = f"switch.my_strip_plug{plug_id}"
         entity_registry = er.async_get(hass)
         assert (
             entity_registry.async_get(entity_id).unique_id == f"PLUG{plug_id}DEVICEID"

homeassistant/components/tplink/entity.py
@@ -32,13 +32,14 @@ def async_refresh_after(
 class CoordinatedTPLinkEntity(CoordinatorEntity[TPLinkDataUpdateCoordinator]):
     """Common base class for all coordinated tplink entities."""
 
+    _attr_has_entity_name = True
+
     def __init__(
         self, device: SmartDevice, coordinator: TPLinkDataUpdateCoordinator
     ) -> None:
         """Initialize the switch."""
         super().__init__(coordinator)
         self.device: SmartDevice = device
-        self._attr_name = self.device.alias
         self._attr_unique_id = self.device.device_id
 
     @property

homeassistant/components/tplink/light.py
@@ -162,6 +162,7 @@ class TPLinkSmartBulb(CoordinatedTPLinkEntity, LightEntity):
     """Representation of a TPLink Smart Bulb."""
 
     _attr_supported_features = LightEntityFeature.TRANSITION
+    _attr_name = None
 
     device: SmartBulb
 

homeassistant/components/tplink/sensor.py
@@ -46,6 +46,7 @@ class TPLinkSensorEntityDescription(SensorEntityDescription):
 ENERGY_SENSORS: tuple[TPLinkSensorEntityDescription, ...] = (
     TPLinkSensorEntityDescription(
         key=ATTR_CURRENT_POWER_W,
+        translation_key="current_consumption",
         native_unit_of_measurement=UnitOfPower.WATT,
         device_class=SensorDeviceClass.POWER,
         state_class=SensorStateClass.MEASUREMENT,
@@ -55,6 +56,7 @@ ENERGY_SENSORS: tuple[TPLinkSensorEntityDescription, ...] = (
     ),
     TPLinkSensorEntityDescription(
         key=ATTR_TOTAL_ENERGY_KWH,
+        translation_key="total_consumption",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
         device_class=SensorDeviceClass.ENERGY,
         state_class=SensorStateClass.TOTAL_INCREASING,
@@ -64,6 +66,7 @@ ENERGY_SENSORS: tuple[TPLinkSensorEntityDescription, ...] = (
     ),
     TPLinkSensorEntityDescription(
         key=ATTR_TODAY_ENERGY_KWH,
+        translation_key="today_consumption",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
         device_class=SensorDeviceClass.ENERGY,
         state_class=SensorStateClass.TOTAL_INCREASING,
@@ -75,7 +78,6 @@ ENERGY_SENSORS: tuple[TPLinkSensorEntityDescription, ...] = (
         native_unit_of_measurement=UnitOfElectricPotential.VOLT,
         device_class=SensorDeviceClass.VOLTAGE,
         state_class=SensorStateClass.MEASUREMENT,
-        name="Voltage",
         emeter_attr="voltage",
         precision=1,
     ),
@@ -84,7 +86,6 @@ ENERGY_SENSORS: tuple[TPLinkSensorEntityDescription, ...] = (
         native_unit_of_measurement=UnitOfElectricCurrent.AMPERE,
         device_class=SensorDeviceClass.CURRENT,
         state_class=SensorStateClass.MEASUREMENT,
-        name="Current",
         emeter_attr="current",
         precision=2,
     ),
@@ -154,14 +155,6 @@ class SmartPlugSensor(CoordinatedTPLinkEntity, SensorEntity):
         self._attr_unique_id = (
             f"{legacy_device_id(self.device)}_{self.entity_description.key}"
         )
-
-    @property
-    def name(self) -> str:
-        """Return the name of the Smart Plug.
-
-        Overridden to include the description.
-        """
-        return f"{self.device.alias} {self.entity_description.name}"
 
     @property
     def native_value(self) -> float | None:

commit 8924a70906db13d9368a933596a9d749736f6ab7

Fix tplink child plug state reporting
regressed in https://github.com/home-assistant/core/pull/96246

fixes #97487

homeassistant/components/tplink/switch.py
@@ -116,7 +116,7 @@ class SmartPlugSwitchChild(SmartPlugSwitch):
         coordinator: TPLinkDataUpdateCoordinator,
         plug: SmartDevice,
     ) -> None:
-        """Initialize the switch."""
+        """Initialize the child switch."""
         super().__init__(device, coordinator)
         self._plug = plug
         self._attr_unique_id = legacy_device_id(plug)
@@ -124,10 +124,15 @@ class SmartPlugSwitchChild(SmartPlugSwitch):
 
     @async_refresh_after
     async def async_turn_on(self, **kwargs: Any) -> None:
-        """Turn the switch on."""
+        """Turn the child switch on."""
         await self._plug.turn_on()
 
     @async_refresh_after
     async def async_turn_off(self, **kwargs: Any) -> None:
-        """Turn the switch off."""
+        """Turn the child switch off."""
         await self._plug.turn_off()
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if child switch is on."""
+        return bool(self._plug.is_on)

tests/components/tplink/__init__.py
@@ -180,12 +180,14 @@ def _mocked_strip() -> SmartStrip:
     plug0.alias = "Plug0"
     plug0.device_id = "bb:bb:cc:dd:ee:ff_PLUG0DEVICEID"
     plug0.mac = "bb:bb:cc:dd:ee:ff"
+    plug0.is_on = True
     plug0.protocol = _mock_protocol()
     plug1 = _mocked_plug()
     plug1.device_id = "cc:bb:cc:dd:ee:ff_PLUG1DEVICEID"
     plug1.mac = "cc:bb:cc:dd:ee:ff"
     plug1.alias = "Plug1"
     plug1.protocol = _mock_protocol()
+    plug1.is_on = False
     strip.children = [plug0, plug1]
     return strip
 

tests/components/tplink/test_switch.py
@@ -9,7 +9,7 @@ import pytest
 from homeassistant.components import tplink
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.components.tplink.const import DOMAIN
-from homeassistant.const import ATTR_ENTITY_ID, STATE_ON, STATE_UNAVAILABLE
+from homeassistant.const import ATTR_ENTITY_ID, STATE_OFF, STATE_ON, STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 from homeassistant.setup import async_setup_component
@@ -146,22 +146,37 @@ async def test_strip(hass: HomeAssistant) -> None:
     # since this is what the previous version did
     assert hass.states.get("switch.my_strip") is None
 
-    for plug_id in range(2):
-        entity_id = f"switch.my_strip_plug{plug_id}"
-        state = hass.states.get(entity_id)
-        assert state.state == STATE_ON
+    entity_id = "switch.my_strip_plug0"
+    state = hass.states.get(entity_id)
+    assert state.state == STATE_ON
 
-        await hass.services.async_call(
-            SWITCH_DOMAIN, "turn_off", {ATTR_ENTITY_ID: entity_id}, blocking=True
-        )
-        strip.children[plug_id].turn_off.assert_called_once()
-        strip.children[plug_id].turn_off.reset_mock()
+    await hass.services.async_call(
+        SWITCH_DOMAIN, "turn_off", {ATTR_ENTITY_ID: entity_id}, blocking=True
+    )
+    strip.children[0].turn_off.assert_called_once()
+    strip.children[0].turn_off.reset_mock()
 
-        await hass.services.async_call(
-            SWITCH_DOMAIN, "turn_on", {ATTR_ENTITY_ID: entity_id}, blocking=True
-        )
-        strip.children[plug_id].turn_on.assert_called_once()
-        strip.children[plug_id].turn_on.reset_mock()
+    await hass.services.async_call(
+        SWITCH_DOMAIN, "turn_on", {ATTR_ENTITY_ID: entity_id}, blocking=True
+    )
+    strip.children[0].turn_on.assert_called_once()
+    strip.children[0].turn_on.reset_mock()
+
+    entity_id = "switch.my_strip_plug1"
+    state = hass.states.get(entity_id)
+    assert state.state == STATE_OFF
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN, "turn_off", {ATTR_ENTITY_ID: entity_id}, blocking=True
+    )
+    strip.children[1].turn_off.assert_called_once()
+    strip.children[1].turn_off.reset_mock()
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN, "turn_on", {ATTR_ENTITY_ID: entity_id}, blocking=True
+    )
+    strip.children[1].turn_on.assert_called_once()
+    strip.children[1].turn_on.reset_mock()
 
 
 async def test_strip_unique_ids(hass: HomeAssistant) -> None: