c71b38328e8fafdd16db81deb5e7c7a30c0a6498
disforw <github@benzona.net>
2023-08-03 21:07:07 -0400

    Create __init__.py

---
+++ homeassistant/components/eyeonwater/__init__.py
@@ -0,0 +1,113 @@
+"""EyeOnWater integration."""
+import asyncio
+import logging
+
+from .eow import Account, Client, EyeOnWaterAPIError, EyeOnWaterAuthError
+from .config_flow import create_account_from_config
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import aiohttp_client
+from homeassistant.helpers.update_coordinator import (
+    DataUpdateCoordinator,
+    Debouncer,
+    UpdateFailed,
+)
+
+from .const import (
+    DATA_COORDINATOR,
+    DATA_SMART_METER,
+    DEBOUNCE_COOLDOWN,
+    DOMAIN,
+    SCAN_INTERVAL,
+)
+
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.addHandler(logging.StreamHandler())
+
+PLATFORMS = [Platform.SENSOR, Platform.BINARY_SENSOR]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Eye On Water from a config entry."""
+    account = create_account_from_config(data=entry.data)
+    eye_on_water_data = EyeOnWaterData(hass, entry, account)
+    try:
+        await eye_on_water_data.client.authenticate()
+    except EyeOnWaterAuthError:
+        _LOGGER.error("Username or password was not accepted")
+        return False
+    except asyncio.TimeoutError as error:
+        raise ConfigEntryNotReady from error
+
+    await eye_on_water_data.setup()
+
+    async def async_update_data():
+        _LOGGER.debug("Fetching latest data")
+        await eye_on_water_data.read_meters()
+        return eye_on_water_data
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="Eye On Water",
+        update_method=async_update_data,
+        update_interval=SCAN_INTERVAL,
+        request_refresh_debouncer=Debouncer(
+            hass, _LOGGER, cooldown=DEBOUNCE_COOLDOWN, immediate=True
+        ),
+    )
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {
+        DATA_COORDINATOR: coordinator,
+        DATA_SMART_METER: eye_on_water_data,
+    }
+
+    asyncio.create_task(coordinator.async_refresh())
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+
+class EyeOnWaterData:
+    """Manages coordinatation of API data updates."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        entry: ConfigEntry,
+        account: Account,
+    ) -> None:
+        """Initialize the data coordintator."""
+        self._entry = entry
+        self.account = account
+        websession = aiohttp_client.async_get_clientsession(hass)
+        self.client = Client(websession, account)
+        self.meters: list = []
+
+    async def setup(self):
+        """Fetch all of the user's meters."""
+        self.meters = await self.account.fetch_meters(self.client)
+        _LOGGER.debug("Discovered %s meter(s)", len(self.meters))
+
+    async def read_meters(self):
+        """Read each meter."""
+        for meter in self.meters:
+            try:
+                await meter.read_meter(self.client)
+            except (EyeOnWaterAPIError, EyeOnWaterAuthError) as error:
+                raise UpdateFailed(error) from error
+        return self.meters
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
