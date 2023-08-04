69253078490ef1ca58dd51480ec76bfc15bc61a0
disforw <github@benzona.net>
2023-08-03 21:54:13 -0400

    Fix errors

--- homeassistant/components/eyeonwater/__init__.py
+++ homeassistant/components/eyeonwater/__init__.py
@@ -2,9 +2,6 @@
 import asyncio
 import logging
 
-from .eow import Account, Client, EyeOnWaterAPIError, EyeOnWaterAuthError
-from .config_flow import create_account_from_config
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -16,6 +13,7 @@ from homeassistant.helpers.update_coordinator import (
     UpdateFailed,
 )
 
+from .config_flow import create_account_from_config
 from .const import (
     DATA_COORDINATOR,
     DATA_SMART_METER,
@@ -23,6 +21,7 @@ from .const import (
     DOMAIN,
     SCAN_INTERVAL,
 )
+from .eow import Account, Client, EyeOnWaterAPIError, EyeOnWaterAuthError
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.addHandler(logging.StreamHandler())
@@ -52,7 +51,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator = DataUpdateCoordinator(
         hass,
         _LOGGER,
-        name="Eye On Water",
+        name="EyeOnWater",
         update_method=async_update_data,
         update_interval=SCAN_INTERVAL,
         request_refresh_debouncer=Debouncer(
