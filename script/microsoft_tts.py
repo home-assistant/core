53114422bb0bd5f1747bb660211f4b1bd2a8ad1c
Hugo <42470993+hugobloem@users.noreply.github.com>
2023-08-05 10:23:25 +0100

    Scrape supported languages and default types

--- script/microsoft_tts.py
+++ script/microsoft_tts.py
@@ -7,18 +7,24 @@ import requests
 from .hassfest.serializer import format_python_namespace
 
 URL = "https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support"
-XPATH_QUERY = "//section[@data-tab='tts']/table[1]/tbody/tr/td[1]/code/text()"
+XPATH_QUERY = "//section[@data-tab='tts']/table[1]/tbody/tr/td[3]/code/text()"
 
 req = requests.get(URL)
 req.raise_for_status()
 tree = html.fromstring(req.content)
 supported_languages_raw = tree.xpath(XPATH_QUERY)
-supported_languages = {s.lower() for s in supported_languages_raw}
+default_languages_types = {}
+for s in supported_languages_raw:
+    key = "-".join(s.lower().split("-")[:-1])
+    if key in default_languages_types:
+        continue
+    default_languages_types[key] = s.lower().split("-")[-1]
+
 
 Path("homeassistant/generated/microsoft_tts.py").write_text(
     format_python_namespace(
         {
-            "SUPPORTED_LANGUAGES": supported_languages,
+            "DEFAULT_LANGUAGES_TYPES": default_languages_types,
         },
         generator="script.microsoft_tts",
     )
