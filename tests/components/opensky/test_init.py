ab40411ffb040a0142bdfcec8b6e2fdb7c9f41e3
joostlek <joostlek@outlook.com>
2023-08-06 20:38:34 +0200

    Catch up with reality

--- tests/components/opensky/test_init.py
+++ tests/components/opensky/test_init.py
@@ -1,10 +1,15 @@
 """Test OpenSky component setup process."""
 from __future__ import annotations
 
-from homeassistant.components.opensky.const import DOMAIN
-from homeassistant.core import HomeAssistant
+from unittest.mock import patch
 
-from . import MockOpenSky
+from python_opensky import OpenSkyError
+
+from homeassistant.components.opensky.const import DOMAIN
+from homeassistant.config_entries import ConfigEntryState
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
+
 from .conftest import ComponentSetup
 
 from tests.common import MockConfigEntry
@@ -16,7 +21,7 @@ async def test_load_unload_entry(
     config_entry: MockConfigEntry,
 ) -> None:
     """Test load and unload entry."""
-    await setup_integration(config_entry, MockOpenSky())
+    await setup_integration(config_entry)
     entry = hass.config_entries.async_entries(DOMAIN)[0]
 
     state = hass.states.get("sensor.opensky")
@@ -27,3 +32,19 @@ async def test_load_unload_entry(
 
     state = hass.states.get("sensor.opensky")
     assert not state
+
+
+async def test_load_entry_failure(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+) -> None:
+    """Test failure while loading."""
+    config_entry.add_to_hass(hass)
+    with patch(
+        "python_opensky.OpenSky.get_states",
+        side_effect=OpenSkyError(),
+    ):
+        assert await async_setup_component(hass, DOMAIN, {})
+        await hass.async_block_till_done()
+    entry = hass.config_entries.async_entries(DOMAIN)[0]
+    assert entry.state == ConfigEntryState.SETUP_RETRY
