home-assistant/core

commit a09090bf99263399858c07a6177cd2cf5aaacea5

Do not fire homekit_controller events from IP polling (#97869)
* Fix homekit_controller triggers when value is None

* fixes

* cover

homeassistant/components/homekit_controller/device_trigger.py
@@ -80,11 +80,11 @@ class TriggerSource:
             self._iid_trigger_keys.setdefault(iid, set()).add(trigger_key)
             await connection.add_watchable_characteristics([(aid, iid)])
 
-    def fire(self, iid: int, value: dict[str, Any]) -> None:
+    def fire(self, iid: int, ev: dict[str, Any]) -> None:
         """Process events that have been received from a HomeKit accessory."""
         for trigger_key in self._iid_trigger_keys.get(iid, set()):
             for event_handler in self._callbacks.get(trigger_key, []):
-                event_handler(value)
+                event_handler(ev)
 
     def async_get_triggers(self) -> Generator[tuple[str, str], None, None]:
         """List device triggers for HomeKit devices."""
@@ -99,20 +99,23 @@ class TriggerSource:
     ) -> CALLBACK_TYPE:
         """Attach a trigger."""
         trigger_data = trigger_info["trigger_data"]
-        trigger_key = (config[CONF_TYPE], config[CONF_SUBTYPE])
+        type_: str = config[CONF_TYPE]
+        sub_type: str = config[CONF_SUBTYPE]
+        trigger_key = (type_, sub_type)
         job = HassJob(action)
+        trigger_callbacks = self._callbacks.setdefault(trigger_key, [])
+        hass = self._hass
 
         @callback
-        def event_handler(char: dict[str, Any]) -> None:
-            if config[CONF_SUBTYPE] != HK_TO_HA_INPUT_EVENT_VALUES[char["value"]]:
+        def event_handler(ev: dict[str, Any]) -> None:
+            if sub_type != HK_TO_HA_INPUT_EVENT_VALUES[ev["value"]]:
                 return
-            self._hass.async_run_hass_job(job, {"trigger": {**trigger_data, **config}})
+            hass.async_run_hass_job(job, {"trigger": {**trigger_data, **config}})
 
-        self._callbacks.setdefault(trigger_key, []).append(event_handler)
+        trigger_callbacks.append(event_handler)
 
         def async_remove_handler():
-            if trigger_key in self._callbacks:
-                self._callbacks[trigger_key].remove(event_handler)
+            trigger_callbacks.remove(event_handler)
 
         return async_remove_handler
 
@@ -259,6 +262,10 @@ def async_fire_triggers(conn: HKDevice, events: dict[tuple[int, int], dict[str, 
     if not trigger_sources:
         return
     for (aid, iid), ev in events.items():
+        # If the value is None, we received the event via polling
+        # and we don't want to trigger on that
+        if ev["value"] is None:
+            continue
         if aid in conn.devices:
             device_id = conn.devices[aid]
             if source := trigger_sources.get(device_id):

tests/components/homekit_controller/test_device_trigger.py
@@ -425,6 +425,14 @@ async def test_handle_events_late_setup(hass: HomeAssistant, utcnow, calls) -> N
     assert len(calls) == 1
     assert calls[0].data["some"] == "device - button1 - single_press - 0"
 
+    # Make sure automation doesn't trigger for a polled None
+    helper.pairing.testing.update_named_service(
+        "Button 1", {CharacteristicsTypes.INPUT_EVENT: None}
+    )
+
+    await hass.async_block_till_done()
+    assert len(calls) == 1
+
     # Make sure automation doesn't trigger for long press
     helper.pairing.testing.update_named_service(
         "Button 1", {CharacteristicsTypes.INPUT_EVENT: 1}

commit 2d47d5133cc2f1cbbd1ff29c8d147ca8a91908fa

Fix handling HomeKit events when the device is in error state
Fixes the below which was introduced in #97869
```
2023-08-05 18:37:50.146 DEBUG (MainThread) [homeassistant.components.homekit_controller.device_trigger] Event (11158856,67) {'status': -70402, 'description': 'Unable to communicate with requested service, e.g. the power to the accessory was turned off.'}
2023-08-05 18:37:50.146 ERROR (MainThread) [homeassistant.components.homekit_controller.connection] Unexpected exception from <bound method HKDevice.async_update of <homeassistant.components.homekit_controller.connection.HKDevice object at 0x7fb3787fa690>>
Traceback (most recent call last):
  File "/usr/src/homeassistant/homeassistant/helpers/debounce.py", line 116, in _handle_timer_finish
    await task
  File "/usr/src/homeassistant/homeassistant/components/homekit_controller/connection.py", line 786, in async_update
    self.process_new_events(new_values_dict)
  File "/usr/src/homeassistant/homeassistant/components/homekit_controller/connection.py", line 797, in process_new_events
    async_fire_triggers(self, new_values_dict)
  File "/usr/src/homeassistant/homeassistant/components/homekit_controller/device_trigger.py", line 271, in async_fire_triggers
    if ev["value"] is None:
       ~~^^^^^^^^^
KeyError: 'value'
```

homeassistant/components/homekit_controller/device_trigger.py
@@ -262,14 +262,13 @@ def async_fire_triggers(conn: HKDevice, events: dict[tuple[int, int], dict[str, 
     if not trigger_sources:
         return
     for (aid, iid), ev in events.items():
-        # If the value is None, we received the event via polling
-        # and we don't want to trigger on that
-        if ev["value"] is None:
-            continue
         if aid in conn.devices:
             device_id = conn.devices[aid]
             if source := trigger_sources.get(device_id):
-                source.fire(iid, ev)
+                # If the value is None, we received the event via polling
+                # and we don't want to trigger on that
+                if ev.get("value") is not None:
+                    source.fire(iid, ev)
 
 
 async def async_get_triggers(