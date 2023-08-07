20f9693dd9ac046a6fc1f2cbaf7e1511a3f0593b
Erik <erik@montnemery.com>
2023-08-07 17:44:02 +0200

    Improve test coverage

--- tests/components/aemet/test_weather.py
+++ tests/components/aemet/test_weather.py
@@ -1,9 +1,16 @@
 """The sensor tests for the AEMET OpenData platform."""
+import datetime
 from unittest.mock import patch
 
+from freezegun.api import FrozenDateTimeFactory
+import pytest
+import requests_mock
 from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.aemet.const import ATTRIBUTION
+from homeassistant.components.aemet.weather_update_coordinator import (
+    WEATHER_UPDATE_INTERVAL,
+)
 from homeassistant.components.weather import (
     ATTR_CONDITION_PARTLYCLOUDY,
     ATTR_CONDITION_SNOWY,
@@ -28,7 +35,9 @@ from homeassistant.const import ATTR_ATTRIBUTION
 from homeassistant.core import HomeAssistant
 import homeassistant.util.dt as dt_util
 
-from .util import async_init_integration
+from .util import aemet_requests_mock, async_init_integration
+
+from tests.typing import WebSocketGenerator
 
 
 async def test_aemet_weather(hass: HomeAssistant) -> None:
@@ -67,7 +76,7 @@ async def test_aemet_weather(hass: HomeAssistant) -> None:
     assert state is None
 
 
-async def test_forecast_services(
+async def test_forecast_service(
     hass: HomeAssistant,
     snapshot: SnapshotAssertion,
 ) -> None:
@@ -102,3 +111,50 @@ async def test_forecast_services(
         return_response=True,
     )
     assert response == snapshot
+
+
+@pytest.mark.parametrize("forecast_type", ["daily", "hourly"])
+async def test_forecast_subscription(
+    hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
+    freezer: FrozenDateTimeFactory,
+    snapshot: SnapshotAssertion,
+    forecast_type: str,
+) -> None:
+    """Test multiple forecast."""
+    client = await hass_ws_client(hass)
+
+    hass.config.set_time_zone("UTC")
+    freezer.move_to("2021-01-09 12:00:00+00:00")
+    await async_init_integration(hass)
+
+    await client.send_json_auto_id(
+        {
+            "type": "weather/subscribe_forecast",
+            "forecast_type": forecast_type,
+            "entity_id": "weather.aemet_daily",
+        }
+    )
+    msg = await client.receive_json()
+    assert msg["success"]
+    assert msg["result"] is None
+    subscription_id = msg["id"]
+
+    msg = await client.receive_json()
+    assert msg["id"] == subscription_id
+    assert msg["type"] == "event"
+    forecast1 = msg["event"]["forecast"]
+
+    assert forecast1 == snapshot
+
+    with requests_mock.mock() as _m:
+        aemet_requests_mock(_m)
+        freezer.tick(WEATHER_UPDATE_INTERVAL + datetime.timedelta(seconds=1))
+        await hass.async_block_till_done()
+        msg = await client.receive_json()
+
+    assert msg["id"] == subscription_id
+    assert msg["type"] == "event"
+    forecast2 = msg["event"]["forecast"]
+
+    assert forecast2 == snapshot
