628399bd07331fefc06e08d35462a953e1047e17
David Knowles <dknowles2@gmail.com>
2023-08-07 12:07:48 -0400

    Schlage: Set the changed by attribute on locks based on log messages (#97469)

--- tests/components/schlage/conftest.py
+++ tests/components/schlage/conftest.py
@@ -36,6 +36,7 @@ async def mock_added_config_entry(
 ) -> MockConfigEntry:
     """Mock ConfigEntry that's been added to HA."""
     mock_schlage.locks.return_value = [mock_lock]
+    mock_schlage.users.return_value = []
     mock_config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
@@ -80,4 +81,6 @@ def mock_lock():
         battery_level=20,
         firmware_version="1.0",
     )
+    mock_lock.logs.return_value = []
+    mock_lock.last_changed_by.return_value = "thumbturn"
     return mock_lock
--- tests/components/schlage/test_lock.py
+++ tests/components/schlage/test_lock.py
@@ -1,11 +1,18 @@
 """Test schlage lock."""
+
+from datetime import timedelta
 from unittest.mock import Mock
+
+from pyschlage.exceptions import UnknownError
 
 from homeassistant.components.lock import DOMAIN as LOCK_DOMAIN
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_ENTITY_ID, SERVICE_LOCK, SERVICE_UNLOCK
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr
+from homeassistant.util.dt import utcnow
+
+from tests.common import async_fire_time_changed
 
 
 async def test_lock_device_registry(
@@ -43,3 +50,38 @@ async def test_lock_services(
     mock_lock.unlock.assert_called_once_with()
 
     await hass.config_entries.async_unload(mock_added_config_entry.entry_id)
+
+
+async def test_changed_by(
+    hass: HomeAssistant, mock_lock: Mock, mock_added_config_entry: ConfigEntry
+) -> None:
+    """Test population of the changed_by attribute."""
+    mock_lock.last_changed_by.reset_mock()
+    mock_lock.last_changed_by.return_value = "access code - foo"
+
+    # Make the coordinator refresh data.
+    async_fire_time_changed(hass, utcnow() + timedelta(seconds=31))
+    await hass.async_block_till_done()
+    mock_lock.last_changed_by.assert_called_once_with([])
+
+    lock_device = hass.states.get("lock.vault_door")
+    assert lock_device is not None
+    assert lock_device.attributes.get("changed_by") == "access code - foo"
+
+
+async def test_changed_by_uses_previous_logs_on_failure(
+    hass: HomeAssistant, mock_lock: Mock, mock_added_config_entry: ConfigEntry
+) -> None:
+    """Test that a failure to load logs is not terminal."""
+    mock_lock.last_changed_by.reset_mock()
+    mock_lock.last_changed_by.return_value = "thumbturn"
+    mock_lock.logs.side_effect = UnknownError("Cannot load logs")
+
+    # Make the coordinator refresh data.
+    async_fire_time_changed(hass, utcnow() + timedelta(seconds=31))
+    await hass.async_block_till_done()
+    mock_lock.last_changed_by.assert_called_once_with([])
+
+    lock_device = hass.states.get("lock.vault_door")
+    assert lock_device is not None
+    assert lock_device.attributes.get("changed_by") == "thumbturn"
