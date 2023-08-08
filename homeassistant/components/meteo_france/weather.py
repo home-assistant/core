bf7d6a7a7b7f01450521574d5fc8b943ee036f24
Erik <erik@montnemery.com>
2023-08-08 10:32:59 +0200

    Modernize meteofrance weather

--- homeassistant/components/meteo_france/weather.py
+++ homeassistant/components/meteo_france/weather.py
@@ -2,7 +2,7 @@
 import logging
 import time
 
-from meteofrance_api.model.forecast import Forecast
+from meteofrance_api.model.forecast import Forecast as MeteoFranceForecast
 
 from homeassistant.components.weather import (
     ATTR_FORECAST_CONDITION,
@@ -13,7 +13,9 @@ from homeassistant.components.weather import (
     ATTR_FORECAST_NATIVE_WIND_SPEED,
     ATTR_FORECAST_TIME,
     ATTR_FORECAST_WIND_BEARING,
+    Forecast,
     WeatherEntity,
+    WeatherEntityFeature,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
@@ -23,7 +25,7 @@ from homeassistant.const import (
     UnitOfSpeed,
     UnitOfTemperature,
 )
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.device_registry import DeviceEntryType
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -59,9 +61,9 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up the Meteo-France weather platform."""
-    coordinator: DataUpdateCoordinator[Forecast] = hass.data[DOMAIN][entry.entry_id][
-        COORDINATOR_FORECAST
-    ]
+    coordinator: DataUpdateCoordinator[MeteoFranceForecast] = hass.data[DOMAIN][
+        entry.entry_id
+    ][COORDINATOR_FORECAST]
 
     async_add_entities(
         [
@@ -80,7 +82,7 @@ async def async_setup_entry(
 
 
 class MeteoFranceWeather(
-    CoordinatorEntity[DataUpdateCoordinator[Forecast]], WeatherEntity
+    CoordinatorEntity[DataUpdateCoordinator[MeteoFranceForecast]], WeatherEntity
 ):
     """Representation of a weather condition."""
 
@@ -89,13 +91,27 @@ class MeteoFranceWeather(
     _attr_native_precipitation_unit = UnitOfPrecipitationDepth.MILLIMETERS
     _attr_native_pressure_unit = UnitOfPressure.HPA
     _attr_native_wind_speed_unit = UnitOfSpeed.METERS_PER_SECOND
+    _attr_supported_features = (
+        WeatherEntityFeature.FORECAST_DAILY | WeatherEntityFeature.FORECAST_HOURLY
+    )
 
-    def __init__(self, coordinator: DataUpdateCoordinator[Forecast], mode: str) -> None:
+    def __init__(
+        self, coordinator: DataUpdateCoordinator[MeteoFranceForecast], mode: str
+    ) -> None:
         """Initialise the platform with a data instance and station name."""
         super().__init__(coordinator)
         self._city_name = self.coordinator.data.position["name"]
         self._mode = mode
         self._unique_id = f"{self.coordinator.data.position['lat']},{self.coordinator.data.position['lon']}"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        super()._handle_coordinator_update()
+        assert self.platform.config_entry
+        self.platform.config_entry.async_create_task(
+            self.hass, self.async_update_listeners(("daily", "hourly"))
+        )
 
     @property
     def unique_id(self):
@@ -153,12 +169,11 @@ class MeteoFranceWeather(
         if wind_bearing != -1:
             return wind_bearing
 
-    @property
-    def forecast(self):
+    def _forecast(self, mode: str) -> list[Forecast]:
         """Return the forecast."""
-        forecast_data = []
+        forecast_data: list[Forecast] = []
 
-        if self._mode == FORECAST_MODE_HOURLY:
+        if mode == FORECAST_MODE_HOURLY:
             today = time.time()
             for forecast in self.coordinator.data.forecast:
                 # Can have data in the past
@@ -190,7 +205,7 @@ class MeteoFranceWeather(
                     {
                         ATTR_FORECAST_TIME: self.coordinator.data.timestamp_to_locale_time(
                             forecast["dt"]
-                        ),
+                        ).isoformat(),
                         ATTR_FORECAST_CONDITION: format_condition(
                             forecast["weather12H"]["desc"]
                         ),
@@ -203,3 +218,16 @@ class MeteoFranceWeather(
                     }
                 )
         return forecast_data
+
+    @property
+    def forecast(self) -> list[Forecast]:
+        """Return the forecast array."""
+        return self._forecast(self._mode)
+
+    async def async_forecast_daily(self) -> list[Forecast]:
+        """Return the daily forecast in native units."""
+        return self._forecast(FORECAST_MODE_DAILY)
+
+    async def async_forecast_hourly(self) -> list[Forecast]:
+        """Return the hourly forecast in native units."""
+        return self._forecast(FORECAST_MODE_HOURLY)
