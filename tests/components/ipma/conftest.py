2ce633b684b5bd67e6448dcda2430c4d42d93f37
Diogo Gomes <diogogomes@gmail.com>
2023-08-10 14:18:12 +0100

    update tests

---
+++ tests/components/ipma/conftest.py
@@ -0,0 +1,37 @@
+"""Define test fixtures for IPMA."""
+
+import pytest
+
+from homeassistant.components.ipma import DOMAIN
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
+
+from tests.common import MockConfigEntry
+
+
+@pytest.fixture(name="config_entry")
+def config_entry_fixture(hass, config):
+    """Define a config entry fixture."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id=f"{config[CONF_LATITUDE]}, {config[CONF_LONGITUDE]}",
+        data=config,
+    )
+    entry.add_to_hass(hass)
+    return entry
+
+
+@pytest.fixture(name="config")
+def config_fixture():
+    """Define a config entry data fixture."""
+    return {
+        CONF_NAME: "home",
+        CONF_LATITUDE: 40.0,
+        CONF_LONGITUDE: 30.0,
+    }
+
+
+@pytest.fixture(name="setup_config_entry")
+async def setup_config_entry_fixture(hass, config_entry):
+    """Define a fixture to set up ipma."""
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
