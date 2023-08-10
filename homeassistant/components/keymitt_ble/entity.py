f2d94fc779c12d2b38c5c048c3bb0290565d9f02
joostlek <joostlek@outlook.com>
2023-08-11 01:21:14 +0200

    Add entity translations to Keymitt ble

--- homeassistant/components/keymitt_ble/entity.py
+++ homeassistant/components/keymitt_ble/entity.py
@@ -16,11 +16,12 @@ from .coordinator import MicroBotDataUpdateCoordinator
 class MicroBotEntity(PassiveBluetoothCoordinatorEntity[MicroBotDataUpdateCoordinator]):
     """Generic entity for all MicroBots."""
 
+    _attr_has_entity_name = True
+
     def __init__(self, coordinator, config_entry):
         """Initialise the entity."""
         super().__init__(coordinator)
         self._address = self.coordinator.ble_device.address
-        self._attr_name = "Push"
         self._attr_unique_id = self._address
         self._attr_device_info = DeviceInfo(
             connections={(dr.CONNECTION_BLUETOOTH, self._address)},
