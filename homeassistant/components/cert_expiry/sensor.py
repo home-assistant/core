c5d05588823c7fa5f06d2233dc8865728ce7ea75
joostlek <joostlek@outlook.com>
2023-08-10 13:09:12 +0200

    Migrate Cert Expiry to has entity name

--- homeassistant/components/cert_expiry/sensor.py
+++ homeassistant/components/cert_expiry/sensor.py
@@ -78,6 +78,7 @@ class CertExpiryEntity(CoordinatorEntity[CertExpiryDataUpdateCoordinator]):
     """Defines a base Cert Expiry entity."""
 
     _attr_icon = "mdi:certificate"
+    _attr_has_entity_name = True
 
     @property
     def extra_state_attributes(self):
@@ -92,6 +93,7 @@ class SSLCertificateTimestamp(CertExpiryEntity, SensorEntity):
     """Implementation of the Cert Expiry timestamp sensor."""
 
     _attr_device_class = SensorDeviceClass.TIMESTAMP
+    _attr_translation_key = "certificate_expiry_timestamp"
 
     def __init__(
         self,
@@ -99,7 +101,6 @@ class SSLCertificateTimestamp(CertExpiryEntity, SensorEntity):
     ) -> None:
         """Initialize a Cert Expiry timestamp sensor."""
         super().__init__(coordinator)
-        self._attr_name = f"Cert Expiry Timestamp ({coordinator.name})"
         self._attr_unique_id = f"{coordinator.host}:{coordinator.port}-timestamp"
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, f"{coordinator.host}:{coordinator.port}")},
