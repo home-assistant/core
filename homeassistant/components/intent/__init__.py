283af6eab4b5ea6bd5f04206e984ba33edb86980
Tudor Sandu <tm.sandu@gmail.com>
2023-08-10 04:02:49 -0700

    Linting changes

--- homeassistant/components/intent/__init__.py
+++ homeassistant/components/intent/__init__.py
@@ -90,19 +90,25 @@ class OnOffIntentHandler(intent.ServiceIntentHandler):
             )
             return
           
-        elif state.domain == LOCK_DOMAIN:
+        if state.domain == LOCK_DOMAIN:
             # on = lock
             # off = unlock
-            await hass.services.async_call(
-                LOCK_DOMAIN,
-                SERVICE_LOCK if self.service == SERVICE_TURN_ON else SERVICE_UNLOCK,
-                {ATTR_ENTITY_ID: state.entity_id},
-                context=intent_obj.context,
-                blocking=True,
-                limit=self.service_timeout,
+            await self._run_then_background(
+                hass.async_create_task(
+                    hass.services.async_call(
+                        LOCK_DOMAIN,
+                        SERVICE_LOCK
+                        if self.service == SERVICE_TURN_ON
+                        else SERVICE_UNLOCK,
+                        {ATTR_ENTITY_ID: state.entity_id},
+                        context=intent_obj.context,
+                        blocking=True,
+                    )
+                )
             )
+            return
 
-        elif not hass.services.has_service(state.domain, self.service):
+        if not hass.services.has_service(state.domain, self.service):
             raise intent.IntentHandleError(
                 f"Service {self.service} does not support entity {state.entity_id}"
             )
