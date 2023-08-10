7ca9b3f7f08b75002d02991e27344b754aae95a3
joostlek <joostlek@outlook.com>
2023-08-10 13:05:32 +0200

    Migrate DirecTV to has entity name

--- homeassistant/components/directv/entity.py
+++ homeassistant/components/directv/entity.py
@@ -1,8 +1,6 @@
 """Base DirecTV Entity."""
 from __future__ import annotations
 
-from typing import cast
-
 from directv import DIRECTV
 
 from homeassistant.helpers.entity import DeviceInfo, Entity
@@ -13,23 +11,18 @@ from .const import DOMAIN
 class DIRECTVEntity(Entity):
     """Defines a base DirecTV entity."""
 
-    def __init__(self, *, dtv: DIRECTV, address: str = "0") -> None:
+    _attr_has_entity_name = True
+
+    def __init__(self, *, dtv: DIRECTV, name: str, address: str = "0") -> None:
         """Initialize the DirecTV entity."""
         self._address = address
         self._device_id = address if address != "0" else dtv.device.info.receiver_id
         self._is_client = address != "0"
         self.dtv = dtv
-
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return device information about this DirecTV receiver."""
-        return DeviceInfo(
+        self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, self._device_id)},
             manufacturer=self.dtv.device.info.brand,
-            # Instead of setting the device name to the entity name, directv
-            # should be updated to set has_entity_name = True, and set the entity
-            # name to None
-            name=cast(str | None, self.name),
+            name=name,
             sw_version=self.dtv.device.info.version,
             via_device=(DOMAIN, self.dtv.device.info.receiver_id),
         )
