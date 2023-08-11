dd26b73f71a2dcad49712528973a9005af9b2eaf
Erwin Douna <e.douna@gmail.com>
2023-08-12 00:00:57 +0200

    Update homeassistant/components/tado/config_flow.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/tado/config_flow.py
+++ homeassistant/components/tado/config_flow.py
@@ -66,7 +66,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None) -> FlowResult:
+    async def async_step_user(self, user_input: dict[str, Any] | None=None) -> FlowResult:
         """Handle the initial step."""
         errors = {}
         if user_input is not None:
