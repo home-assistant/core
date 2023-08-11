e6630980041d250c90cb865facb681b9814d767e
Hessel van Es <hessel@hesselonline.nl>
2023-08-11 14:26:54 +0000

    black formatting

--- homeassistant/components/wallbox/switch.py
+++ homeassistant/components/wallbox/switch.py
@@ -54,7 +54,9 @@ class WallboxSwitch(WallboxEntity, SwitchEntity):
     @property
     def available(self) -> bool:
         """Return the availability of the switch."""
-        return super().available and self.coordinator.data[CHARGER_STATUS_DESCRIPTION_KEY] not in {
+        return super().available and self.coordinator.data[
+            CHARGER_STATUS_DESCRIPTION_KEY
+        ] not in {
             ChargerStatus.UNKNOWN,
             ChargerStatus.UPDATING,
             ChargerStatus.ERROR,
