c58386d216478bcba40da5dc1d28a09c24e4ffeb
John Hollowell <jhollowe@johnhollowell.com>
2023-08-13 03:18:24 +0000

    Add fan binary_sensor for Venstar thermostats

--- homeassistant/components/venstar/binary_sensor.py
+++ homeassistant/components/venstar/binary_sensor.py
@@ -16,23 +16,29 @@ async def async_setup_entry(
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Set up Vensar device binary_sensors based on a config entry."""
+    """Set up Venstar device binary_sensors based on a config entry."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     if coordinator.client.alerts is None:
         return
-    async_add_entities(
-        VenstarBinarySensor(coordinator, config_entry, alert["name"])
-        for alert in coordinator.client.alerts
-    )
+
+    entities: list[VenstarEntity] = [
+        *(
+            VenstarAlertBinarySensor(coordinator, config_entry, alert["name"])
+            for alert in coordinator.client.alerts
+        ),
+        VenstarFanBinarySensor(coordinator, config_entry),
+    ]
+
+    async_add_entities(entities)
 
 
-class VenstarBinarySensor(VenstarEntity, BinarySensorEntity):
+class VenstarAlertBinarySensor(VenstarEntity, BinarySensorEntity):
     """Represent a Venstar alert."""
 
     _attr_device_class = BinarySensorDeviceClass.PROBLEM
 
-    def __init__(self, coordinator, config, alert):
+    def __init__(self, coordinator, config, alert) -> None:
         """Initialize the alert."""
         super().__init__(coordinator, config)
         self.alert = alert
@@ -40,10 +46,27 @@ class VenstarBinarySensor(VenstarEntity, BinarySensorEntity):
         self._attr_name = f"{self._client.name} {alert}"
 
     @property
-    def is_on(self):
+    def is_on(self) -> bool | None:
         """Return true if the binary sensor is on."""
         for alert in self._client.alerts:
             if alert["name"] == self.alert:
                 return alert["active"]
 
         return None
+
+
+class VenstarFanBinarySensor(VenstarEntity, BinarySensorEntity):
+    """Represent the on/off state of a Venstar fan."""
+
+    _attr_device_class = BinarySensorDeviceClass.RUNNING
+
+    def __init__(self, coordinator, config) -> None:
+        """Initialize the alert."""
+        super().__init__(coordinator, config)
+        self._attr_unique_id = f"{config.entry_id}_fan"
+        self._attr_name = f"{self._client.name} Fan"
+
+    @property
+    def is_on(self) -> bool | None:
+        """Return true if the fan is on."""
+        return self._client.fanstate
