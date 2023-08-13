d0cc2eb035b3e87bb532bceadbd01a6cb771d826
David Knowles <dknowles2@gmail.com>
2023-08-13 18:18:51 +0000

    Update comment in setup_platform

--- homeassistant/components/hydrawise/switch.py
+++ homeassistant/components/hydrawise/switch.py
@@ -64,7 +64,7 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up a sensor for a Hydrawise device."""
-    # Setup is a noop because we automatically trigger YAML import to config flow.
+    # We don't need to trigger import flow from here as it's triggered from `__init__.py`
     return
 
 
