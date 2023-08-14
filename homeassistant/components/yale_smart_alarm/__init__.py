a4dcf1a8b0855bc989e15828acbbb0afb6cd90a7
G Johansson <goran.johansson@shiftit.se>
2023-08-14 19:37:42 +0000

    add back 2

--- homeassistant/components/yale_smart_alarm/__init__.py
+++ homeassistant/components/yale_smart_alarm/__init__.py
@@ -25,6 +25,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     }
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(update_listener))
 
     return True
 
