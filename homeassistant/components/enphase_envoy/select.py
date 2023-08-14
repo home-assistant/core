bfeb36f752ae6932676222590622589f22946814
Charles Garwood <cgarwood@gmail.com>
2023-08-13 23:22:56 -0400

    Add select platform to Enphase integration

---
+++ homeassistant/components/enphase_envoy/select.py
@@ -0,0 +1,179 @@
+"""Select platform for Enphase Envoy solar energy monitor."""
+from __future__ import annotations
+
+from collections.abc import Callable
+from dataclasses import dataclass
+import logging
+from typing import Any
+
+from pyenphase import EnvoyDryContactSettings
+from pyenphase.models.dry_contacts import DryContactAction, DryContactMode
+
+from homeassistant.components.select import SelectEntity, SelectEntityDescription
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .coordinator import EnphaseUpdateCoordinator
+from .entity import EnvoyBaseEntity
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class EnvoyRelayRequiredKeysMixin:
+    """Mixin for required keys."""
+
+    value_fn: Callable[[EnvoyDryContactSettings], str]
+    update_fn: Callable[[Any, Any, Any], Any]
+
+
+@dataclass
+class EnvoyRelaySelectEntityDescription(
+    SelectEntityDescription, EnvoyRelayRequiredKeysMixin
+):
+    """Describes an Envoy Dry Contact Relay select entity."""
+
+
+RELAY_MODE_MAP = {
+    DryContactMode.MANUAL: "standard",
+    DryContactMode.STATE_OF_CHARGE: "battery",
+}
+RELAY_ACTION_MAP = {
+    DryContactAction.APPLY: "powered",
+    DryContactAction.SHED: "not_powered",
+    DryContactAction.SCHEDULE: "schedule",
+    DryContactAction.NONE: "none",
+}
+
+RELAY_ENTITIES = (
+    EnvoyRelaySelectEntityDescription(
+        key="mode",
+        name="Mode",
+        translation_key="relay_mode",
+        options=list(RELAY_MODE_MAP.values()),
+        value_fn=lambda relay: RELAY_MODE_MAP[relay.mode],
+        update_fn=lambda envoy, relay, value: envoy.update_dry_contact(
+            {
+                "id": relay.id,
+                "mode": next(
+                    key for key, val in RELAY_MODE_MAP.items() if val == value
+                ),
+            }
+        ),
+    ),
+    EnvoyRelaySelectEntityDescription(
+        key="grid_action",
+        name="Grid action",
+        translation_key="relay_grid_action",
+        options=list(RELAY_ACTION_MAP.values()),
+        value_fn=lambda relay: RELAY_ACTION_MAP[relay.grid_action],
+        update_fn=lambda envoy, relay, value: envoy.update_dry_contact(
+            {
+                "id": relay.id,
+                "grid_action": next(
+                    key for key, val in RELAY_ACTION_MAP.items() if val == value
+                ),
+            }
+        ),
+    ),
+    EnvoyRelaySelectEntityDescription(
+        key="microgrid_action",
+        name="Microgrid action",
+        translation_key="relay_microgrid_action",
+        options=list(RELAY_ACTION_MAP.values()),
+        value_fn=lambda relay: RELAY_ACTION_MAP[relay.micro_grid_action],
+        update_fn=lambda envoy, relay, value: envoy.update_dry_contact(
+            {
+                "id": relay.id,
+                "micro_grid_action": next(
+                    key for key, val in RELAY_ACTION_MAP.items() if val == value
+                ),
+            }
+        ),
+    ),
+    EnvoyRelaySelectEntityDescription(
+        key="generator_action",
+        name="Generator action",
+        translation_key="relay_generator_action",
+        options=list(RELAY_ACTION_MAP.values()),
+        value_fn=lambda relay: RELAY_ACTION_MAP[relay.generator_action],
+        update_fn=lambda envoy, relay, value: envoy.update_dry_contact(
+            {
+                "id": relay.id,
+                "generator_action": next(
+                    key for key, val in RELAY_ACTION_MAP.items() if val == value
+                ),
+            }
+        ),
+    ),
+)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Enphase Envoy select platform."""
+    coordinator: EnphaseUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
+    envoy_data = coordinator.envoy.data
+    assert envoy_data is not None
+    envoy_serial_num = config_entry.unique_id
+    assert envoy_serial_num is not None
+    entities: list[SelectEntity] = []
+    if envoy_data.dry_contact_settings:
+        entities.extend(
+            [
+                EnvoyRelaySelectEntity(coordinator, entity, relay)
+                for entity in RELAY_ENTITIES
+                for relay in envoy_data.dry_contact_settings
+            ]
+        )
+    async_add_entities(entities)
+
+
+class EnvoyRelaySelectEntity(EnvoyBaseEntity, SelectEntity):
+    """Representation of an Enphase Enpower switch entity."""
+
+    entity_description: EnvoyRelaySelectEntityDescription
+    _attr_has_entity_name = True
+
+    def __init__(
+        self,
+        coordinator: EnphaseUpdateCoordinator,
+        description: EnvoyRelaySelectEntityDescription,
+        relay: str,
+    ) -> None:
+        """Initialize the Enphase relay select entity."""
+        super().__init__(coordinator, description)
+        self.envoy = coordinator.envoy
+        assert self.envoy is not None
+        assert self.data is not None
+        self.enpower = self.data.enpower
+        assert self.enpower is not None
+        self._serial_number = self.enpower.serial_number
+        self.relay = self.data.dry_contact_settings[relay]
+        self._attr_unique_id = (
+            f"{self._serial_number}_relay_{relay}_{self.entity_description.key}"
+        )
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, relay)},
+            manufacturer="Enphase",
+            model="Dry Contact Relay",
+            name=self.relay.load_name,
+            sw_version=str(self.enpower.firmware_version),
+            via_device=(DOMAIN, self._serial_number),
+        )
+
+    @property
+    def current_option(self) -> str:
+        """Return the state of the Enpower switch."""
+        return self.entity_description.value_fn(self.relay)
+
+    async def async_select_option(self, option: str) -> None:
+        """Update the relay."""
+        await self.entity_description.update_fn(self.envoy, self.relay, option)
+        await self.coordinator.async_request_refresh()
