950bfddd75ee0d6ef8e991cb9ae0cefb7df3759b
starkillerOG <starkiller.og@gmail.com>
2023-08-15 00:01:48 +0200

    Add PTZ stop when zoom_basic supported

--- homeassistant/components/reolink/button.py
+++ homeassistant/components/reolink/button.py
@@ -36,6 +36,7 @@ class ReolinkButtonEntityDescription(
     """A class that describes button entities for a camera channel."""
 
     supported: Callable[[Host, int], bool] = lambda api, ch: True
+    enabled_default: Callable[[Host, int], bool] | None = None
 
 
 @dataclass
@@ -59,7 +60,8 @@ BUTTON_ENTITIES = (
         key="ptz_stop",
         name="PTZ stop",
         icon="mdi:pan",
-        supported=lambda api, ch: api.supported(ch, "pan_tilt"),
+        enabled_default=lambda api, ch: api.supported(ch, "pan_tilt"),
+        supported=lambda api, ch: api.supported(ch, "pan_tilt") or api.supported(ch, "zoom_basic"),
         method=lambda api, ch: api.set_ptz_command(ch, command=PtzEnum.stop.value),
     ),
     ReolinkButtonEntityDescription(
@@ -185,6 +187,8 @@ class ReolinkButtonEntity(ReolinkChannelCoordinatorEntity, ButtonEntity):
         self._attr_unique_id = (
             f"{self._host.unique_id}_{channel}_{entity_description.key}"
         )
+        if entity_description.enabled_default is not None:
+            self._attr_entity_registry_enabled_default = entity_description.enabled_default(self._host.api, self._channel)
 
     async def async_press(self) -> None:
         """Execute the button action."""
