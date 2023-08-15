2ec2568ea78e5f7e8af9c7aabbe3b1c3d4bc31a5
Michael Arthur <mikey0000@users.noreply.github.com>
2023-08-15 20:45:50 +1200

    Update homeassistant/components/lawn_mower/__init__.py
    Co-authored-by: Franck Nijhof <frenck@frenck.nl>

--- homeassistant/components/lawn_mower/__init__.py
+++ homeassistant/components/lawn_mower/__init__.py
@@ -83,7 +83,7 @@ class LawnMowerEntity(Entity):
         """Return the current state."""
         if (activity := self.activity) is None:
             return None
-        return str(activity.value)
+        return str(activity)
 
     @property
     def activity(self) -> LawnMowerActivity | None:
