119187e5d532e781a200c34c009f39e47233b458
Maciej Bieniek <bieniu@users.noreply.github.com>
2023-08-16 13:51:26 +0200

    Add removal condition

--- homeassistant/components/shelly/sensor.py
+++ homeassistant/components/shelly/sensor.py
@@ -542,6 +542,7 @@ RPC_SENSORS: Final = {
         device_class=SensorDeviceClass.CURRENT,
         state_class=SensorStateClass.MEASUREMENT,
         available=lambda status: (status and status["n_current"]) is not None,
+        removal_condition=lambda _config, status, _key: "n_current" not in status,
         entity_registry_enabled_default=False,
     ),
     "total_current": RpcSensorDescription(
