49c65e2987054d92529ea03c264ff75c66ac119f
Mateusz Nowakowski <matiutd888@gmail.com>
2023-08-17 20:11:31 +0000

    Add example sensor

---
+++ config/custom_components/example_sensor/README.md
@@ -0,0 +1,15 @@
+# Example Sensor
+
+This is a minimum implementation of an integration providing a sensor measurement.
+
+### Installation
+
+Copy this folder to `<config_dir>/custom_components/example_sensor/`.
+
+Add the following to your `configuration.yaml` file:
+
+```yaml
+# Example configuration.yaml entry
+sensor:
+  - platform: example_sensor
+```
---
+++ config/custom_components/example_sensor/sensor.py
@@ -0,0 +1,38 @@
+"""Platform for sensor integration."""
+from __future__ import annotations
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
+    """Set up the sensor platform."""
+    add_entities([ExampleSensor()])
+
+
+class ExampleSensor(SensorEntity):
+    """Representation of a Sensor."""
+
+    _attr_name = "Example Temperature"
+    _attr_native_unit_of_measurement = TEMP_CELSIUS
+    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._attr_native_value = 23
