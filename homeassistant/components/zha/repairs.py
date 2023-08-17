82bac38ea704ac7eb3dc062006d6d51c345c01e5
puddly <32534428+puddly@users.noreply.github.com>
2023-08-17 15:47:52 -0400

    Create a separate repair when using non-Nabu Casa hardware

--- homeassistant/components/zha/repairs.py
+++ homeassistant/components/zha/repairs.py
@@ -1,25 +1,69 @@
 """ZHA repairs for common environmental and device problems."""
 from __future__ import annotations
 
+import enum
+
 from universal_silabs_flasher.const import ApplicationType
 from universal_silabs_flasher.flasher import Flasher
 
-from homeassistant.components.homeassistant_yellow import hardware as yellow_hardware
+from homeassistant.components.homeassistant_sky_connect import (
+    hardware as skyconnect_hardware,
+)
+from homeassistant.components.homeassistant_yellow import (
+    RADIO_DEVICE as YELLOW_RADIO_DEVICE,
+    hardware as yellow_hardware,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady, HomeAssistantError
 from homeassistant.helpers import issue_registry as ir
 
 from .core.const import DOMAIN
 
-DISABLE_MULTIPAN_URL_YELLOW = (
-    "https://yellow.home-assistant.io/guides/disable-multiprotocol/"
-)
-DISABLE_MULTIPAN_URL_SKYCONNECT = (
-    "https://skyconnect.home-assistant.io/procedures/disable-multiprotocol/"
-)
 
+class HardwareType(enum.StrEnum):
+    """Detected Zigbee hardware type."""
+
+    SKYCONNECT = "skyconnect"
+    YELLOW = "yellow"
+    OTHER = "other"
+
+
+DISABLE_MULTIPAN_URL = {
+    HardwareType.YELLOW: (
+        "https://yellow.home-assistant.io/guides/disable-multiprotocol/"
+    ),
+    HardwareType.SKYCONNECT: (
+        "https://skyconnect.home-assistant.io/procedures/disable-multiprotocol/"
+    ),
+    HardwareType.OTHER: None,
+}
 
 ISSUE_WRONG_SILABS_FIRMWARE_INSTALLED = "wrong_silabs_firmware_installed"
+
+
+def detect_radio_hardware(hass: HomeAssistant, device: str) -> HardwareType:
+    """Identify the radio hardware with the given serial port."""
+    try:
+        info = yellow_hardware.async_info(hass)
+    except HomeAssistantError:
+        pass
+    else:
+        if device == YELLOW_RADIO_DEVICE:
+            return HardwareType.YELLOW
+
+    try:
+        info = skyconnect_hardware.async_info(hass)
+    except HomeAssistantError:
+        pass
+    else:
+        for hardware_info in info:
+            for entry_id in hardware_info.config_entries or []:
+                entry = hass.config_entries.async_get_entry(entry_id)
+
+                if entry is not None and entry.data["device"] == device:
+                    return HardwareType.SKYCONNECT
+
+    return HardwareType.OTHER
 
 
 async def probe_silabs_firmware_type(device: str) -> ApplicationType | None:
@@ -43,21 +87,19 @@ async def warn_on_wrong_silabs_firmware(hass: HomeAssistant, device: str) -> Non
         # reconnect, it should work
         raise ConfigEntryNotReady()
 
-    try:
-        yellow_hardware.async_info(hass)
-        learn_more_url = DISABLE_MULTIPAN_URL_YELLOW
-    except HomeAssistantError:
-        learn_more_url = DISABLE_MULTIPAN_URL_SKYCONNECT
-
+    hardware_type = detect_radio_hardware(hass, device)
     ir.async_create_issue(
         hass,
-        DOMAIN,
-        ISSUE_WRONG_SILABS_FIRMWARE_INSTALLED,
+        domain=DOMAIN,
+        issue_id=ISSUE_WRONG_SILABS_FIRMWARE_INSTALLED,
         is_fixable=False,
         is_persistent=False,
-        learn_more_url=learn_more_url,
+        learn_more_url=DISABLE_MULTIPAN_URL[hardware_type],
         severity=ir.IssueSeverity.CRITICAL,
-        translation_key=ISSUE_WRONG_SILABS_FIRMWARE_INSTALLED,
+        translation_key=(
+            ISSUE_WRONG_SILABS_FIRMWARE_INSTALLED
+            + ("_nabucasa" if hardware_type != HardwareType.OTHER else "_other")
+        ),
         translation_placeholders={"firmware_type": app_type.name},
     )
 
