16e534a950420b22b19f8d73562fd4b837451669
Erwin Douna <e.douna@gmail.com>
2023-08-18 10:50:32 +0000

    Updating deprecation period

--- homeassistant/components/fastdotcom/sensor.py
+++ homeassistant/components/fastdotcom/sensor.py
@@ -34,7 +34,7 @@ async def async_setup_platform(
         hass,
         HOMEASSISTANT_DOMAIN,
         f"deprecated_yaml_{FASTDOTCOM_DOMAIN}",
-        breaks_in_ha_version="2023.12.0",
+        breaks_in_ha_version="2024.2.0",
         is_fixable=False,
         issue_domain=FASTDOTCOM_DOMAIN,
         severity=IssueSeverity.WARNING,
