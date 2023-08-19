f2e53bacc70c246287784d6fea4bd71d95956dd0
username081581 <post@thomas-hahn.org>
2023-08-19 21:16:40 +0200

    Add tests for state

--- tests/components/homematicip_cloud/test_climate.py
+++ tests/components/homematicip_cloud/test_climate.py
@@ -224,6 +224,7 @@ async def test_hmip_heating_group_heat(
     # Only fire event from last async_manipulate_test_data available.
     assert hmip_device.mock_calls[-1][0] == "fire_update_event"
 
+    assert ha_state.state == HVACMode.AUTO
     await hass.services.async_call(
         "climate",
         "set_preset_mode",
@@ -236,6 +237,7 @@ async def test_hmip_heating_group_heat(
     await async_manipulate_test_data(hass, hmip_device, "controlMode", "ECO")
     ha_state = hass.states.get(entity_id)
     assert ha_state.attributes[ATTR_PRESET_MODE] == PRESET_ECO
+    assert ha_state.state == HVACMode.AUTO
 
     await async_manipulate_test_data(hass, hmip_device, "floorHeatingMode", "RADIATOR")
     await async_manipulate_test_data(hass, hmip_device, "valvePosition", 0.1)
