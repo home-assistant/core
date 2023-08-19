b0070277f47316a16e2c19127ff55836377ac2b1
starkillerOG <starkiller.og@gmail.com>
2023-08-19 12:02:30 +0200

    fix typing

--- homeassistant/components/reolink/sensor.py
+++ homeassistant/components/reolink/sensor.py
@@ -28,7 +28,7 @@ from .entity import ReolinkChannelCoordinatorEntity, ReolinkHostCoordinatorEntit
 class ReolinkSensorEntityDescriptionMixin:
     """Mixin values for Reolink  sensor entities for a camera channel."""
 
-    value: Callable[[Host, int], float]
+    value: Callable[[Host, int], int]
 
 
 @dataclass
@@ -44,7 +44,7 @@ class ReolinkSensorEntityDescription(
 class ReolinkHostSensorEntityDescriptionMixin:
     """Mixin values for Reolink host sensor entities."""
 
-    value: Callable[[Host], float]
+    value: Callable[[Host], int]
 
 
 @dataclass
