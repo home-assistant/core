9e8cb34110e0c2f3649b0158aab416e1de2efc96
Jake Colman <jake.colman@gmail.com>
2023-08-20 02:54:40 +0000

    Add test for specific entities

--- tests/components/honeywell/test_climate.py
+++ tests/components/honeywell/test_climate.py
@@ -54,7 +54,10 @@ async def test_no_thermostat_options(
     """Test the setup of the climate entities when there are no additional options available."""
     device._data = {}
     await init_integration(hass, config_entry)
-    assert len(hass.states.async_all()) == 2
+    assert len(hass.states.async_all()) == 3  # 1 climate sensor; 2 indoor sensors
+    assert hass.states.get("climate.device1")
+    assert hass.states.get("sensor.device1_temperature")
+    assert hass.states.get("sensor.device1_humidity")
 
 
 async def test_static_attributes(
