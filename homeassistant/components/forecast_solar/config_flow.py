2856267e22593189395a8b493cafd939130dc12e
joostlek <joostlek@outlook.com>
2023-08-21 00:05:14 +0200

    Add morning and evening damping to Forecast solar

--- homeassistant/components/forecast_solar/config_flow.py
+++ homeassistant/components/forecast_solar/config_flow.py
@@ -15,6 +15,8 @@ from homeassistant.helpers import config_validation as cv
 from .const import (
     CONF_AZIMUTH,
     CONF_DAMPING,
+    CONF_DAMPING_EVENING,
+    CONF_DAMPING_MORNING,
     CONF_DECLINATION,
     CONF_INVERTER_SIZE,
     CONF_MODULES_POWER,
@@ -27,7 +29,7 @@ RE_API_KEY = re.compile(r"^[a-zA-Z0-9]{16}$")
 class ForecastSolarFlowHandler(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Forecast.Solar."""
 
-    VERSION = 1
+    VERSION = 2
 
     @staticmethod
     @callback
@@ -127,7 +129,11 @@ class ForecastSolarOptionFlowHandler(OptionsFlow):
                         default=self.config_entry.options[CONF_MODULES_POWER],
                     ): vol.Coerce(int),
                     vol.Optional(
-                        CONF_DAMPING,
+                        CONF_DAMPING_EVENING,
+                        default=self.config_entry.options.get(CONF_DAMPING, 0.0),
+                    ): vol.Coerce(float),
+                    vol.Optional(
+                        CONF_DAMPING_MORNING,
                         default=self.config_entry.options.get(CONF_DAMPING, 0.0),
                     ): vol.Coerce(float),
                     vol.Optional(
