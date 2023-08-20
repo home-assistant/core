de806c36e76fbb7a9ab9e51c04a3cb3e6e649879
joostlek <joostlek@outlook.com>
2023-08-21 00:24:27 +0200

    Use snapshot assertion for Airly diagnostics

--- tests/components/airly/test_diagnostics.py
+++ tests/components/airly/test_diagnostics.py
@@ -1,7 +1,8 @@
 """Test Airly diagnostics."""
 import json
 
-from homeassistant.components.diagnostics import REDACTED
+from syrupy import SnapshotAssertion
+
 from homeassistant.core import HomeAssistant
 
 from . import init_integration
@@ -16,30 +17,13 @@ async def test_entry_diagnostics(
     hass: HomeAssistant,
     aioclient_mock: AiohttpClientMocker,
     hass_client: ClientSessionGenerator,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test config entry diagnostics."""
     entry = await init_integration(hass, aioclient_mock)
 
-    coordinator_data = json.loads(load_fixture("diagnostics_data.json", "airly"))
+    json.loads(load_fixture("diagnostics_data.json", "airly"))
 
     result = await get_diagnostics_for_config_entry(hass, hass_client, entry)
 
-    assert result["config_entry"] == {
-        "entry_id": entry.entry_id,
-        "version": 1,
-        "domain": "airly",
-        "title": "Home",
-        "data": {
-            "latitude": REDACTED,
-            "longitude": REDACTED,
-            "name": "Home",
-            "api_key": REDACTED,
-        },
-        "options": {},
-        "pref_disable_new_entities": False,
-        "pref_disable_polling": False,
-        "source": "user",
-        "unique_id": REDACTED,
-        "disabled_by": None,
-    }
-    assert result["coordinator_data"] == coordinator_data
+    assert result == snapshot
