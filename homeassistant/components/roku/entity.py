80edb39603a0535ac023ae65eb23f3d318881ada
Chris Talkington <chris@talkingtontech.com>
2023-08-21 00:53:50 -0500

    require device id for Roku entities

--- homeassistant/components/roku/entity.py
+++ homeassistant/components/roku/entity.py
@@ -15,7 +15,7 @@ class RokuEntity(CoordinatorEntity[RokuDataUpdateCoordinator]):
     def __init__(
         self,
         *,
-        device_id: str | None,
+        device_id: str,
         coordinator: RokuDataUpdateCoordinator,
         description: EntityDescription | None = None,
     ) -> None:
@@ -26,31 +26,27 @@ class RokuEntity(CoordinatorEntity[RokuDataUpdateCoordinator]):
         if description is not None:
             self.entity_description = description
 
-            if device_id is None:
-                self._attr_name = f"{coordinator.data.info.name} {description.name}"
+        self._attr_has_entity_name = True
 
-        if device_id is not None:
-            self._attr_has_entity_name = True
+        if description is not None:
+            self._attr_unique_id = f"{device_id}_{description.key}"
+        else:
+            self._attr_unique_id = device_id
 
-            if description is not None:
-                self._attr_unique_id = f"{device_id}_{description.key}"
-            else:
-                self._attr_unique_id = device_id
-
-            self._attr_device_info = DeviceInfo(
-                identifiers={(DOMAIN, device_id)},
-                connections={
-                    (CONNECTION_NETWORK_MAC, mac_address)
-                    for mac_address in (
-                        self.coordinator.data.info.wifi_mac,
-                        self.coordinator.data.info.ethernet_mac,
-                    )
-                    if mac_address is not None
-                },
-                name=self.coordinator.data.info.name,
-                manufacturer=self.coordinator.data.info.brand,
-                model=self.coordinator.data.info.model_name,
-                hw_version=self.coordinator.data.info.model_number,
-                sw_version=self.coordinator.data.info.version,
-                suggested_area=self.coordinator.data.info.device_location,
-            )
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, device_id)},
+            connections={
+                (CONNECTION_NETWORK_MAC, mac_address)
+                for mac_address in (
+                    self.coordinator.data.info.wifi_mac,
+                    self.coordinator.data.info.ethernet_mac,
+                )
+                if mac_address is not None
+            },
+            name=self.coordinator.data.info.name,
+            manufacturer=self.coordinator.data.info.brand,
+            model=self.coordinator.data.info.model_name,
+            hw_version=self.coordinator.data.info.model_number,
+            sw_version=self.coordinator.data.info.version,
+            suggested_area=self.coordinator.data.info.device_location,
+        )
