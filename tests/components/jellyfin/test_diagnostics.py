2348a7f21baf2c0bbea6c6b5b47cc709b9a4650a
Chris Talkington <chris@talkingtontech.com>
2023-08-20 23:06:37 -0400

    add snapshot.

--- tests/components/jellyfin/test_diagnostics.py
+++ tests/components/jellyfin/test_diagnostics.py
@@ -15,7 +15,9 @@ async def test_diagnostics(
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test generating diagnostics for a config entry."""
-    assert (
-        await get_diagnostics_for_config_entry(hass, hass_client, init_integration)
-        == snapshot
-    )
+    data = await get_diagnostics_for_config_entry(hass, hass_client, init_integration)
+
+    assert data["entry"]["data"]["client_device_id"] == init_integration.entry_id
+    data["entry"]["data"]["client_device_id"] = "entry-id"
+
+    assert data == snapshot
