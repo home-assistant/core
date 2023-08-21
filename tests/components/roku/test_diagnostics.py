8d26cc9997d2a79e38c193e3a1d848f2bf8b9b16
Chris Talkington <chris@talkingtontech.com>
2023-08-20 22:36:51 -0400

    fix state time variation. add snapshot.

--- tests/components/roku/test_diagnostics.py
+++ tests/components/roku/test_diagnostics.py
@@ -1,9 +1,11 @@
 """Tests for the diagnostics data provided by the Roku integration."""
+from rokuecp import Device as RokuDevice
 from syrupy import SnapshotAssertion
 
 from homeassistant.core import HomeAssistant
+from homeassistant.util import dt as dt_util
 
-from tests.common import MockConfigEntry, load_fixture
+from tests.common import MockConfigEntry
 from tests.components.diagnostics import get_diagnostics_for_config_entry
 from tests.typing import ClientSessionGenerator
 
@@ -11,10 +13,13 @@ from tests.typing import ClientSessionGenerator
 async def test_diagnostics(
     hass: HomeAssistant,
     hass_client: ClientSessionGenerator,
+    mock_device: RokuDevice,
     init_integration: MockConfigEntry,
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test diagnostics for config entry."""
+    mock_device.state.at = dt_util.parse_datetime("2023-08-15 17:00:00-00:00")
+
     assert (
         await get_diagnostics_for_config_entry(hass, hass_client, init_integration)
         == snapshot
