9a4b2e0359cee77ce9112a435e7ad0419c2cc24d
Erwin Douna <e.douna@gmail.com>
2023-08-23 07:27:54 +0000

    Removing hass.data.setdefault(DOMAIN, {})

--- homeassistant/components/fastdotcom/__init__.py
+++ homeassistant/components/fastdotcom/__init__.py
@@ -54,7 +54,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.services.async_register(DOMAIN, "speedtest", update)
 
-    hass.data.setdefault(DOMAIN, {})
     await hass.config_entries.async_forward_entry_setups(
         entry,
         PLATFORMS,
