8d9b131ea72daf5bee1edab0a950002d63f3ad91
Charles Garwood <cgarwood@gmail.com>
2023-08-22 20:42:02 -0400

    Mutate data in lib instead of HA

--- homeassistant/components/enphase_envoy/switch.py
+++ homeassistant/components/enphase_envoy/switch.py
@@ -182,11 +182,9 @@ class EnvoyDryContactSwitchEntity(EnvoyBaseEntity, SwitchEntity):
     async def async_turn_on(self):
         """Turn on (close) the dry contact."""
         if await self.entity_description.turn_on_fn(self.envoy, self.relay_id):
-            self.data.dry_contact_status[self.relay_id].status = DryContactStatus.CLOSED
             self.async_write_ha_state()
 
     async def async_turn_off(self):
         """Turn off (open) the dry contact."""
         if await self.entity_description.turn_off_fn(self.envoy, self.relay_id):
-            self.data.dry_contact_status[self.relay_id].status = DryContactStatus.OPEN
             self.async_write_ha_state()
