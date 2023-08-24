f568cbe4ad7369081aa83473789ebe8d4352c92a
joostlek <joostlek@outlook.com>
2023-08-24 12:56:54 +0200

    Add test coverage

--- tests/components/srp_energy/test_sensor.py
+++ tests/components/srp_energy/test_sensor.py
@@ -1,4 +1,9 @@
 """Tests for the srp_energy sensor platform."""
+from unittest.mock import patch
+
+import pytest
+from requests.models import HTTPError
+
 from homeassistant.components.sensor import SensorDeviceClass, SensorStateClass
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import (
@@ -9,6 +14,8 @@ from homeassistant.const import (
     UnitOfEnergy,
 )
 from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
 
 
 async def test_loading_sensors(hass: HomeAssistant, init_integration) -> None:
@@ -37,3 +44,26 @@ async def test_srp_entity(hass: HomeAssistant, init_integration) -> None:
 
     assert usage_state.attributes.get(ATTR_DEVICE_CLASS) == SensorDeviceClass.ENERGY
     assert usage_state.attributes.get(ATTR_ICON) == "mdi:flash"
+
+
+@pytest.mark.parametrize("error", [TimeoutError, HTTPError])
+async def test_srp_entity_update_failed(
+    hass: HomeAssistant,
+    error: Exception,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test the SrpEntity."""
+
+    with patch(
+        "homeassistant.components.srp_energy.SrpEnergyClient", autospec=True
+    ) as srp_energy_mock:
+        client = srp_energy_mock.return_value
+        client.validate.return_value = True
+        client.usage.side_effect = error
+        mock_config_entry.add_to_hass(hass)
+
+        await hass.config_entries.async_setup(mock_config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    usage_state = hass.states.get("sensor.home_energy_usage")
+    assert usage_state is None
