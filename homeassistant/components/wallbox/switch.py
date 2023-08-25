e5f835d4df88232363cc6f3139dae089aaf3c318
joostlek <joostlek@outlook.com>
2023-08-25 12:54:21 +0200

    Use pause resume

--- homeassistant/components/wallbox/switch.py
+++ homeassistant/components/wallbox/switch.py
@@ -21,7 +21,7 @@ from .const import (
 SWITCH_TYPES: dict[str, SwitchEntityDescription] = {
     CHARGER_PAUSE_RESUME_KEY: SwitchEntityDescription(
         key=CHARGER_PAUSE_RESUME_KEY,
-        name="pause_charging",
+        name="pause_resume",
     ),
 }
 
