e3beeacb006c53332b80197f754b09c46327639e
Michael Hansen <mike@rhasspy.org>
2023-08-25 10:52:42 -0500

    Refactor VAD to use bytearray

--- homeassistant/components/assist_pipeline/vad.py
+++ homeassistant/components/assist_pipeline/vad.py
@@ -1,12 +1,15 @@
 """Voice activity detection."""
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass, field
 from enum import StrEnum
+from typing import Any, Final
 
 import webrtcvad
 
-_SAMPLE_RATE = 16000
+_SAMPLE_RATE: Final = 16000  # Hz
+_SAMPLE_WIDTH: Final = 2  # bytes
 
 
 class VadSensitivity(StrEnum):
@@ -29,6 +32,47 @@ class VadSensitivity(StrEnum):
         return 1.0
 
 
+class AudioBuffer:
+    """Fixed-sized audio buffer with variable internal length."""
+
+    def __init__(self, maxlen: int) -> None:
+        """Initialize buffer."""
+        self._buffer = bytearray(maxlen)
+        self._length = 0
+
+    @property
+    def length(self) -> int:
+        """Get number of bytes currently in the buffer."""
+        return self._length
+
+    @length.setter
+    def length(self, value: int) -> None:
+        """Set the number of bytes in the buffer."""
+        if value > len(self._buffer):
+            raise ValueError("Length cannot be greater than buffer size")
+
+        if value < 0:
+            raise ValueError("Length cannot be negative")
+
+        self._length = value
+
+    def __len__(self) -> int:
+        """Get the number of bytes currently in the buffer."""
+        return self._length
+
+    def __getitem__(self, item: Any) -> Any:
+        """Get a slice of the buffer."""
+        return self._buffer[item]
+
+    def __setitem__(self, item: Any, value: Any) -> Any:
+        """Set a slice of the buffer."""
+        self._buffer[item] = value
+
+    def __bytes__(self) -> bytes:
+        """Convert written portion of buffer to bytes."""
+        return bytes(self._buffer[: self._length])
+
+
 @dataclass
 class VoiceCommandSegmenter:
     """Segments an audio stream into voice commands using webrtcvad."""
@@ -36,7 +80,7 @@ class VoiceCommandSegmenter:
     vad_mode: int = 3
     """Aggressiveness in filtering out non-speech. 3 is the most aggressive."""
 
-    vad_frames: int = 480  # 30 ms
+    vad_samples_per_chunk: int = 480  # 30 ms
     """Must be 10, 20, or 30 ms at 16Khz."""
 
     speech_seconds: float = 0.3
@@ -67,20 +111,21 @@ class VoiceCommandSegmenter:
     """Seconds left before resetting start/stop time counters."""
 
     _vad: webrtcvad.Vad = None
-    _audio_buffer: bytes = field(default_factory=bytes)
-    _bytes_per_chunk: int = 480 * 2  # 16-bit samples
-    _seconds_per_chunk: float = 0.03  # 30 ms
+    _sample_buffer: AudioBuffer = field(init=False)
+    _bytes_per_chunk: int = field(init=False)
+    _seconds_per_chunk: float = field(init=False)
 
     def __post_init__(self) -> None:
         """Initialize VAD."""
         self._vad = webrtcvad.Vad(self.vad_mode)
-        self._bytes_per_chunk = self.vad_frames * 2
-        self._seconds_per_chunk = self.vad_frames / _SAMPLE_RATE
+        self._bytes_per_chunk = self.vad_samples_per_chunk * _SAMPLE_WIDTH
+        self._seconds_per_chunk = self.vad_samples_per_chunk / _SAMPLE_RATE
+        self._sample_buffer = AudioBuffer(self.vad_samples_per_chunk * _SAMPLE_WIDTH)
         self.reset()
 
     def reset(self) -> None:
         """Reset all counters and state."""
-        self._audio_buffer = b""
+        self._sample_buffer.length = 0
         self._speech_seconds_left = self.speech_seconds
         self._silence_seconds_left = self.silence_seconds
         self._timeout_seconds_left = self.timeout_seconds
@@ -92,26 +137,17 @@ class VoiceCommandSegmenter:
 
         Returns False when command is done.
         """
-        self._audio_buffer += samples
-
-        # Process in 10, 20, or 30 ms chunks.
-        num_chunks = len(self._audio_buffer) // self._bytes_per_chunk
-        for chunk_idx in range(num_chunks):
-            chunk_offset = chunk_idx * self._bytes_per_chunk
-            chunk = self._audio_buffer[
-                chunk_offset : chunk_offset + self._bytes_per_chunk
-            ]
+        for chunk in chunk_samples(samples, self._bytes_per_chunk, self._sample_buffer):
             if not self._process_chunk(chunk):
                 self.reset()
                 return False
 
-        if num_chunks > 0:
-            # Remove from buffer
-            self._audio_buffer = self._audio_buffer[
-                num_chunks * self._bytes_per_chunk :
-            ]
-
         return True
+
+    @property
+    def audio_buffer(self) -> bytes:
+        """Get partial chunk in the audio buffer."""
+        return bytes(self._sample_buffer)
 
     def _process_chunk(self, chunk: bytes) -> bool:
         """Process a single chunk of 16-bit 16Khz mono audio.
@@ -163,7 +199,7 @@ class VoiceActivityTimeout:
     vad_mode: int = 3
     """Aggressiveness in filtering out non-speech. 3 is the most aggressive."""
 
-    vad_frames: int = 480  # 30 ms
+    vad_samples_per_chunk: int = 480  # 30 ms
     """Must be 10, 20, or 30 ms at 16Khz."""
 
     _silence_seconds_left: float = 0.0
@@ -173,20 +209,21 @@ class VoiceActivityTimeout:
     """Seconds left before resetting start/stop time counters."""
 
     _vad: webrtcvad.Vad = None
-    _audio_buffer: bytes = field(default_factory=bytes)
-    _bytes_per_chunk: int = 480 * 2  # 16-bit samples
-    _seconds_per_chunk: float = 0.03  # 30 ms
+    _sample_buffer: AudioBuffer = field(init=False)
+    _bytes_per_chunk: int = field(init=False)
+    _seconds_per_chunk: float = field(init=False)
 
     def __post_init__(self) -> None:
         """Initialize VAD."""
         self._vad = webrtcvad.Vad(self.vad_mode)
-        self._bytes_per_chunk = self.vad_frames * 2
-        self._seconds_per_chunk = self.vad_frames / _SAMPLE_RATE
+        self._bytes_per_chunk = self.vad_samples_per_chunk * _SAMPLE_WIDTH
+        self._seconds_per_chunk = self.vad_samples_per_chunk / _SAMPLE_RATE
+        self._sample_buffer = AudioBuffer(self.vad_samples_per_chunk * _SAMPLE_WIDTH)
         self.reset()
 
     def reset(self) -> None:
         """Reset all counters and state."""
-        self._audio_buffer = b""
+        self._sample_buffer.length = 0
         self._silence_seconds_left = self.silence_seconds
         self._reset_seconds_left = self.reset_seconds
 
@@ -195,23 +232,9 @@ class VoiceActivityTimeout:
 
         Returns False when timeout is reached.
         """
-        self._audio_buffer += samples
-
-        # Process in 10, 20, or 30 ms chunks.
-        num_chunks = len(self._audio_buffer) // self._bytes_per_chunk
-        for chunk_idx in range(num_chunks):
-            chunk_offset = chunk_idx * self._bytes_per_chunk
-            chunk = self._audio_buffer[
-                chunk_offset : chunk_offset + self._bytes_per_chunk
-            ]
+        for chunk in chunk_samples(samples, self._bytes_per_chunk, self._sample_buffer):
             if not self._process_chunk(chunk):
                 return False
-
-        if num_chunks > 0:
-            # Remove from buffer
-            self._audio_buffer = self._audio_buffer[
-                num_chunks * self._bytes_per_chunk :
-            ]
 
         return True
 
@@ -239,3 +262,40 @@ class VoiceActivityTimeout:
             )
 
         return True
+
+
+def chunk_samples(
+    samples: bytes,
+    bytes_per_chunk: int,
+    sample_buffer: AudioBuffer,
+) -> Iterable[bytes]:
+    """Yield fixed-sized chunks from samples, keeping leftover bytes in a buffer."""
+    samples_offset = 0
+    num_chunks_in_samples = len(samples) // bytes_per_chunk
+    num_bytes_left = len(samples) % bytes_per_chunk
+
+    if len(sample_buffer) > 0:
+        # Add to partial chunk in buffer
+        bytes_to_copy = min(num_bytes_left, bytes_per_chunk - len(sample_buffer))
+        sample_buffer[len(sample_buffer) :] = samples[:bytes_to_copy]
+        sample_buffer.length += bytes_to_copy
+        samples_offset = bytes_to_copy
+        num_bytes_left -= bytes_to_copy
+
+    if len(sample_buffer) == bytes_per_chunk:
+        # Process full chunk in buffer
+        yield bytes(sample_buffer)
+        sample_buffer.length = 0
+
+    if num_bytes_left > 0:
+        # Keep bytes at the end of samples for next chunk
+        sample_buffer[:num_bytes_left] = samples[len(samples) - num_bytes_left :]
+        sample_buffer.length = num_bytes_left
+
+    # Process samples in chunks.
+    for chunk_idx in range(num_chunks_in_samples):
+        chunk_offset = samples_offset + (chunk_idx * bytes_per_chunk)
+        chunk = samples[chunk_offset : chunk_offset + bytes_per_chunk]
+        yield chunk
+
+    return True
--- tests/components/assist_pipeline/test_vad.py
+++ tests/components/assist_pipeline/test_vad.py
@@ -1,4 +1,5 @@
 """Tests for webrtcvad voice command segmenter."""
+import itertools as it
 from unittest.mock import patch
 
 from homeassistant.components.assist_pipeline.vad import VoiceCommandSegmenter
@@ -36,3 +37,59 @@ def test_speech() -> None:
         # silence
         # False return value indicates voice command is finished
         assert not segmenter.process(bytes(_ONE_SECOND))
+
+
+def test_audio_buffer() -> None:
+    """Test audio buffer wrapping."""
+
+    def is_speech(self, chunk, sample_rate):
+        """Disable VAD."""
+        return False
+
+    with patch(
+        "webrtcvad.Vad.is_speech",
+        new=is_speech,
+    ):
+        segmenter = VoiceCommandSegmenter()
+        bytes_per_chunk = segmenter.vad_samples_per_chunk * 2
+
+        with patch.object(
+            segmenter, "_process_chunk", return_value=True
+        ) as mock_process:
+            # Partially fill audio buffer
+            half_chunk = bytes(it.islice(it.cycle(range(256)), bytes_per_chunk // 2))
+            segmenter.process(half_chunk)
+
+            assert not mock_process.called
+            assert segmenter.audio_buffer == half_chunk
+
+            # Fill and wrap with 1/4 chunk left over
+            three_quarters_chunk = bytes(
+                it.islice(it.cycle(range(256)), int(0.75 * bytes_per_chunk))
+            )
+            segmenter.process(three_quarters_chunk)
+
+            assert mock_process.call_count == 1
+            assert (
+                segmenter.audio_buffer
+                == three_quarters_chunk[
+                    len(three_quarters_chunk) - (bytes_per_chunk // 4) :
+                ]
+            )
+            assert (
+                mock_process.call_args[0][0]
+                == half_chunk + three_quarters_chunk[: bytes_per_chunk // 2]
+            )
+
+            # Run 2 chunks through
+            segmenter.reset()
+            assert len(segmenter.audio_buffer) == 0
+
+            mock_process.reset_mock()
+            two_chunks = bytes(it.islice(it.cycle(range(256)), bytes_per_chunk * 2))
+            segmenter.process(two_chunks)
+
+            assert mock_process.call_count == 2
+            assert len(segmenter.audio_buffer) == 0
+            assert mock_process.call_args_list[0][0][0] == two_chunks[:bytes_per_chunk]
+            assert mock_process.call_args_list[1][0][0] == two_chunks[bytes_per_chunk:]
