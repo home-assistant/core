13260565cd9f3168e3037ecdae1d8d709c6f3b25
Erik <erik@montnemery.com>
2023-08-26 10:17:14 +0200

    ..

--- homeassistant/components/trafikverket_camera/__init__.py
+++ homeassistant/components/trafikverket_camera/__init__.py
@@ -3,9 +3,21 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.integration_platform import (
+    async_process_integration_platform_for_component,
+)
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN, PLATFORMS
 from .coordinator import TVDataUpdateCoordinator
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up trafikverket_camera."""
+    # Process integration platforms right away since
+    # we will create entities before firing EVENT_COMPONENT_LOADED
+    await async_process_integration_platform_for_component(hass, DOMAIN)
+    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
