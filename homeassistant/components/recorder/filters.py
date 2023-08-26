b0ffaf9981f9a20d401a5a6e9ef3f46f589d65bb
Ville Skyttä <ville.skytta@iki.fi>
2023-08-26 17:46:03 +0300

    Remove pylint configs flagged by useless-suppression (#99081)

--- homeassistant/components/recorder/filters.py
+++ homeassistant/components/recorder/filters.py
@@ -187,8 +187,6 @@ class Filters:
         if self._included_domains or self._included_entity_globs:
             return or_(
                 i_entities,
-                # https://github.com/sqlalchemy/sqlalchemy/issues/9190
-                # pylint: disable-next=invalid-unary-operand-type
                 (~e_entities & (i_entity_globs | (~e_entity_globs & i_domains))),
             ).self_group()
 
