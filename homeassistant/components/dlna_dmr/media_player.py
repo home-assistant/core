99627f71223a33c90f16e8cbb9d4a7d03c19ede7
joostlek <joostlek@outlook.com>
2023-08-28 21:58:49 +0200

    Use shorthand attributes for DLNA dmr

--- homeassistant/components/dlna_dmr/media_player.py
+++ homeassistant/components/dlna_dmr/media_player.py
@@ -129,6 +129,9 @@ class DlnaDmrEntity(MediaPlayerEntity):
     # determine whether further device polling is required.
     _attr_should_poll = True
 
+    # Name of the current sound mode, not supported by DLNA
+    _attr_sound_mode = None
+
     def __init__(
         self,
         udn: str,
@@ -143,6 +146,7 @@ class DlnaDmrEntity(MediaPlayerEntity):
     ) -> None:
         """Initialize DLNA DMR entity."""
         self.udn = udn
+        self._attr_unique_id = udn
         self.device_type = device_type
         self._attr_name = name
         self._event_addr = EventListenAddr(None, event_port, event_callback_url)
@@ -744,11 +748,6 @@ class DlnaDmrEntity(MediaPlayerEntity):
         _LOGGER.debug(
             "Couldn't find a suitable mode for shuffle=%s, repeat=%s", shuffle, repeat
         )
-
-    @property
-    def sound_mode(self) -> str | None:
-        """Name of the current sound mode, not supported by DLNA."""
-        return None
 
     @property
     def sound_mode_list(self) -> list[str] | None:
