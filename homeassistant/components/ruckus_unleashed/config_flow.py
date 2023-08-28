b33c373ed15c4c90e5ed707f31787c0d2068aad2
ms264556 <29752086+ms264556@users.noreply.github.com>
2023-08-28 10:35:01 +0000

    Align with old pyruckus registry and mock entries

--- homeassistant/components/ruckus_unleashed/config_flow.py
+++ homeassistant/components/ruckus_unleashed/config_flow.py
@@ -14,8 +14,6 @@ from .const import (
     API_MESH_NAME,
     API_SYS_SYSINFO,
     API_SYS_SYSINFO_SERIAL,
-    API_SYS_UNLEASHEDNETWORK,
-    API_SYS_UNLEASHEDNETWORK_TOKEN,
     DOMAIN,
     KEY_SYS_SERIAL,
     KEY_SYS_TITLE,
@@ -42,14 +40,9 @@ async def validate_input(hass: core.HomeAssistant, data):
         ) as ruckus:
             system_info = await ruckus.api.get_system_info(
                 SystemStat.SYSINFO,
-                SystemStat.UNLEASHED_NETWORK,
             )
             mesh_name = (await ruckus.api.get_mesh_info())[API_MESH_NAME]
-            zd_serial = (
-                system_info[API_SYS_UNLEASHEDNETWORK][API_SYS_UNLEASHEDNETWORK_TOKEN]
-                if API_SYS_UNLEASHEDNETWORK in system_info
-                else system_info[API_SYS_SYSINFO][API_SYS_SYSINFO_SERIAL]
-            )
+            zd_serial = system_info[API_SYS_SYSINFO][API_SYS_SYSINFO_SERIAL]
             return {
                 KEY_SYS_TITLE: mesh_name,
                 KEY_SYS_SERIAL: zd_serial,
