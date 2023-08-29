f4caca7c47120a1a0c9c18b81a1c20f17098d0aa
James Hodgkinson <james@terminaloutcomes.com>
2023-08-29 06:57:26 +0000

    fixing tests, again

--- tests/components/aussie_broadband/test_init.py
+++ tests/components/aussie_broadband/test_init.py
@@ -23,11 +23,11 @@ async def test_unload(hass: HomeAssistant) -> None:
 
 async def test_validate_service_type() -> None:
     """Testing the validation function."""
-    test_service = {"type": "Hardware"}
+    test_service = {"type": "Hardware", "name": "test service"}
     validate_service_type(None, test_service)
 
     with pytest.raises(UnrecognisedServiceType):
-        test_service = {"type": "FunkyBob"}
+        test_service = {"type": "FunkyBob", "name": "test service"}
         validate_service_type(None, test_service)
 
 
