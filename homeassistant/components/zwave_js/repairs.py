4f05a2611a7deed95173d4307933f0a0340dc0a9
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-08-30 02:43:10 -0400

    Add zwave_js device config file change fix/repair

---
+++ homeassistant/components/zwave_js/repairs.py
@@ -0,0 +1,51 @@
+"""Repairs for Z-Wave JS."""
+from __future__ import annotations
+
+from typing import cast
+
+import voluptuous as vol
+from zwave_js_server.model.node import Node
+
+from homeassistant import data_entry_flow
+from homeassistant.components.repairs import ConfirmRepairFlow, RepairsFlow
+from homeassistant.core import HomeAssistant
+
+from .helpers import async_get_node_from_device_id
+
+
+class DeviceConfigFileChangedFlow(RepairsFlow):
+    """Handler for an issue fixing flow."""
+
+    def __init__(self, node: Node) -> None:
+        """Initialize."""
+        self.node = node
+        super().__init__()
+
+    async def async_step_init(
+        self, user_input: dict[str, str] | None = None
+    ) -> data_entry_flow.FlowResult:
+        """Handle the first step of a fix flow."""
+        return await self.async_step_confirm()
+
+    async def async_step_confirm(
+        self, user_input: dict[str, str] | None = None
+    ) -> data_entry_flow.FlowResult:
+        """Handle the confirm step of a fix flow."""
+        if user_input is not None:
+            self.hass.async_create_task(self.node.async_refresh_info())
+            return self.async_create_entry(title="", data={})
+
+        return self.async_show_form(step_id="confirm", data_schema=vol.Schema({}))
+
+
+async def async_create_fix_flow(
+    hass: HomeAssistant,
+    issue_id: str,
+    data: dict[str, str | int | float | None] | None,
+) -> RepairsFlow:
+    """Create flow."""
+    if issue_id.split(".")[0] == "device_config_file_changed":
+        return DeviceConfigFileChangedFlow(
+            async_get_node_from_device_id(hass, cast(dict, data)["device_id"])
+        )
+    return ConfirmRepairFlow()
---
+++ tests/components/zwave_js/test_repairs.py
@@ -0,0 +1,91 @@
+"""Test the Z-Wave JS repairs module."""
+from copy import deepcopy
+from http import HTTPStatus
+from unittest.mock import patch
+
+from zwave_js_server.event import Event
+from zwave_js_server.model.node import Node
+
+from homeassistant.components.repairs.issue_handler import (
+    async_process_repairs_platforms,
+)
+from homeassistant.components.repairs.websocket_api import (
+    RepairsFlowIndexView,
+    RepairsFlowResourceView,
+)
+from homeassistant.components.zwave_js import DOMAIN
+from homeassistant.components.zwave_js.helpers import get_device_id
+from homeassistant.core import HomeAssistant
+import homeassistant.helpers.device_registry as dr
+
+from tests.typing import ClientSessionGenerator, WebSocketGenerator
+
+
+async def test_device_config_file_changed(
+    hass: HomeAssistant,
+    hass_client: ClientSessionGenerator,
+    hass_ws_client: WebSocketGenerator,
+    client,
+    multisensor_6_state,
+    integration,
+) -> None:
+    """Test the device_config_file_changed issue."""
+    dev_reg = dr.async_get(hass)
+    # Create a node
+    node_state = deepcopy(multisensor_6_state)
+    node = Node(client, node_state)
+    event = Event(
+        "node added",
+        {
+            "source": "controller",
+            "event": "node added",
+            "node": node_state,
+            "result": "",
+        },
+    )
+    with patch(
+        "zwave_js_server.model.node.Node.async_has_device_config_changed",
+        return_value=True,
+    ):
+        client.driver.controller.receive_event(event)
+        await hass.async_block_till_done()
+
+    device = dev_reg.async_get_device(identifiers={get_device_id(client.driver, node)})
+    assert device
+    issue_id = f"device_config_file_changed.{device.id}"
+
+    await async_process_repairs_platforms(hass)
+    ws_client = await hass_ws_client(hass)
+    http_client = await hass_client()
+
+    # Assert the issue is present
+    await ws_client.send_json({"id": 1, "type": "repairs/list_issues"})
+    msg = await ws_client.receive_json()
+    assert msg["success"]
+    assert len(msg["result"]["issues"]) == 1
+    issue = msg["result"]["issues"][0]
+    assert issue["issue_id"] == issue_id
+    assert issue["translation_placeholders"] == {"device_name": device.name}
+
+    url = RepairsFlowIndexView.url
+    resp = await http_client.post(url, json={"handler": DOMAIN, "issue_id": issue_id})
+    assert resp.status == HTTPStatus.OK
+    data = await resp.json()
+
+    flow_id = data["flow_id"]
+    assert data["step_id"] == "confirm"
+
+    # Apply fix
+    url = RepairsFlowResourceView.url.format(flow_id=flow_id)
+    resp = await http_client.post(url)
+
+    assert resp.status == HTTPStatus.OK
+    data = await resp.json()
+
+    assert data["type"] == "create_entry"
+
+    # Assert the issue is resolved
+    await ws_client.send_json({"id": 2, "type": "repairs/list_issues"})
+    msg = await ws_client.receive_json()
+    assert msg["success"]
+    assert len(msg["result"]["issues"]) == 0
