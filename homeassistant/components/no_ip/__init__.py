940ac2698557de332b6882654b1c10ed9c91049c
Ludy87 <Ludy87@users.noreply.github.com>
2023-08-31 14:30:47 +0200

    update __init__.py

--- homeassistant/components/no_ip/__init__.py
+++ homeassistant/components/no_ip/__init__.py
@@ -57,7 +57,4 @@ async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload No-IP.com config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
