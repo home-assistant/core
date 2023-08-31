89f7c176598ea595246e295c753525eaceecbf4f
Bram Kragten <mail@bramkragten.nl>
2023-08-31 17:19:10 +0200

    Bump version to 2023.9.0b1

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -7,7 +7,7 @@ from typing import Final
 APPLICATION_NAME: Final = "HomeAssistant"
 MAJOR_VERSION: Final = 2023
 MINOR_VERSION: Final = 9
-PATCH_VERSION: Final = "0b0"
+PATCH_VERSION: Final = "0b1"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"
 REQUIRED_PYTHON_VER: Final[tuple[int, int, int]] = (3, 11, 0)
