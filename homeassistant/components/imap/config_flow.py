f2e31ab1fb392c46eec636044a0f5519dc2af7eb
jbouwh <jan@jbsoft.nl>
2023-09-01 21:05:39 +0000

    Remove imap_email_content integration

--- homeassistant/components/imap/config_flow.py
+++ homeassistant/components/imap/config_flow.py
@@ -10,13 +10,7 @@ from aioimaplib import AioImapException
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import (
-    CONF_NAME,
-    CONF_PASSWORD,
-    CONF_PORT,
-    CONF_USERNAME,
-    CONF_VERIFY_SSL,
-)
+from homeassistant.const import CONF_PASSWORD, CONF_PORT, CONF_USERNAME, CONF_VERIFY_SSL
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.data_entry_flow import AbortFlow, FlowResult
 from homeassistant.helpers import config_validation as cv
@@ -131,28 +125,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
     _reauth_entry: config_entries.ConfigEntry | None
-
-    async def async_step_import(self, user_input: dict[str, Any]) -> FlowResult:
-        """Handle the import from imap_email_content integration."""
-        data = CONFIG_SCHEMA(
-            {
-                CONF_SERVER: user_input[CONF_SERVER],
-                CONF_PORT: user_input[CONF_PORT],
-                CONF_USERNAME: user_input[CONF_USERNAME],
-                CONF_PASSWORD: user_input[CONF_PASSWORD],
-                CONF_FOLDER: user_input[CONF_FOLDER],
-            }
-        )
-        self._async_abort_entries_match(
-            {
-                key: data[key]
-                for key in (CONF_USERNAME, CONF_SERVER, CONF_FOLDER, CONF_SEARCH)
-            }
-        )
-        title = user_input[CONF_NAME]
-        if await validate_input(self.hass, data):
-            raise AbortFlow("cannot_connect")
-        return self.async_create_entry(title=title, data=data)
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
