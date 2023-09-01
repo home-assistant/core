bb432c745791032740d76b1a5f861af74317296e
John Carr <john.carr@unrouted.co.uk>
2023-09-01 23:05:13 +0100

    Apply PLATFORMS feedback

--- homeassistant/components/private_ble_device/__init__.py
+++ homeassistant/components/private_ble_device/__init__.py
@@ -5,17 +5,15 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
+PLATFORMS = [Platform.DEVICE_TRACKER, Platform.SENSOR]
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up tracking of a private bluetooth device from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(
-        entry, [Platform.DEVICE_TRACKER, Platform.SENSOR]
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload entities for a config entry."""
-    return await hass.config_entries.async_unload_platforms(
-        entry, [Platform.DEVICE_TRACKER, Platform.SENSOR]
-    )
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
