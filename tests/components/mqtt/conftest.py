7cc2e294ea67946d1aafaed782878a3cd7de7a76
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-09-02 19:26:11 +0200

    Rework on mqtt certificate tests (#99503)
    * Shared fixture on TEMP_DIR_NAME mock in MQTT tests
    
    * Improve mqtt certificate file tests
    
    * Update tests/components/mqtt/test_util.py
    
    Co-authored-by: J. Nick Koston <nick@koston.org>
    
    * Update tests/components/mqtt/conftest.py
    
    Co-authored-by: J. Nick Koston <nick@koston.org>
    
    * Avoid blocking code
    
    * typo in sub function
    
    ---------
    
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- tests/components/mqtt/conftest.py
+++ tests/components/mqtt/conftest.py
@@ -1,5 +1,9 @@
 """Test fixtures for mqtt component."""
 
+from collections.abc import Generator
+from random import getrandbits
+from unittest.mock import patch
+
 import pytest
 
 from tests.components.light.conftest import mock_light_profiles  # noqa: F401
@@ -8,3 +12,20 @@ from tests.components.light.conftest import mock_light_profiles  # noqa: F401
 @pytest.fixture(autouse=True)
 def patch_hass_config(mock_hass_config: None) -> None:
     """Patch configuration.yaml."""
+
+
+@pytest.fixture
+def temp_dir_prefix() -> str:
+    """Set an alternate temp dir prefix."""
+    return "test"
+
+
+@pytest.fixture
+def mock_temp_dir(temp_dir_prefix: str) -> Generator[None, None, str]:
+    """Mock the certificate temp directory."""
+    with patch(
+        # Patch temp dir name to avoid tests fail running in parallel
+        "homeassistant.components.mqtt.util.TEMP_DIR_NAME",
+        f"home-assistant-mqtt-{temp_dir_prefix}-{getrandbits(10):03x}",
+    ) as mocked_temp_dir:
+        yield mocked_temp_dir
