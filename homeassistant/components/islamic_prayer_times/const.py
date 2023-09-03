177a14fd938f33226d7b869e99c622067a507663
Rami Mosleh <engrbm87@gmail.com>
2023-09-03 17:12:37 +0300

    Change calculation methods to a fixed list (#99535)

--- homeassistant/components/islamic_prayer_times/const.py
+++ homeassistant/components/islamic_prayer_times/const.py
@@ -1,12 +1,27 @@
 """Constants for the Islamic Prayer component."""
 from typing import Final
 
-from prayer_times_calculator import PrayerTimesCalculator
-
 DOMAIN: Final = "islamic_prayer_times"
 NAME: Final = "Islamic Prayer Times"
 
 CONF_CALC_METHOD: Final = "calculation_method"
 
-CALC_METHODS: list[str] = list(PrayerTimesCalculator.CALCULATION_METHODS)
+CALC_METHODS: Final = [
+    "jafari",
+    "karachi",
+    "isna",
+    "mwl",
+    "makkah",
+    "egypt",
+    "tehran",
+    "gulf",
+    "kuwait",
+    "qatar",
+    "singapore",
+    "france",
+    "turkey",
+    "russia",
+    "moonsighting",
+    "custom",
+]
 DEFAULT_CALC_METHOD: Final = "isna"
