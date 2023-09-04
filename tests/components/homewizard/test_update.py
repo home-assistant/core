1aea26da6ef95e28cecd7588260a2a882b142d52
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2023-09-04 11:58:42 +0000

    Add tests to improve coverage

--- tests/components/homewizard/test_update.py
+++ tests/components/homewizard/test_update.py
@@ -10,13 +10,14 @@ from homeassistant.components.update import (
 )
 from homeassistant.const import (
     STATE_ON,
+    STATE_UNKNOWN,
 )
 from homeassistant.core import HomeAssistant
 
 from .generator import get_mock_device
 
 
-async def test_setup_params(
+async def test_update_init(
     hass: HomeAssistant, mock_config_entry_data, mock_config_entry
 ) -> None:
     """Test the initial parameters."""
@@ -41,4 +42,55 @@ async def test_setup_params(
     assert state.attributes[ATTR_INSTALLED_VERSION] == "1.00"
     assert state.attributes[ATTR_LATEST_VERSION] == "4.18"
     assert state.attributes[ATTR_RELEASE_SUMMARY] is None
+    assert state.attributes[ATTR_RELEASE_SUMMARY] is None
     assert state.attributes[ATTR_RELEASE_URL] is None
+
+
+async def test_update_handles_unexpected_product_type(
+    hass: HomeAssistant, mock_config_entry_data, mock_config_entry
+) -> None:
+    """Test unexpected (new or unsupported) device type."""
+
+    api = get_mock_device(product_type="HWE-DOES-NOT-EXISTS")
+    with patch(
+        "homeassistant.components.homewizard.coordinator.HomeWizardEnergy",
+        return_value=api,
+    ):
+        entry = mock_config_entry
+        entry.data = mock_config_entry_data
+        entry.add_to_hass(hass)
+
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    state = hass.states.get("update.product_name_aabbccddeeff_firmware")
+    assert state
+
+    assert state.state == STATE_UNKNOWN
+    assert state.attributes[ATTR_INSTALLED_VERSION] == "1.00"
+    assert state.attributes[ATTR_LATEST_VERSION] is None
+
+
+async def test_update_handles_nonexisting_product_type(
+    hass: HomeAssistant, mock_config_entry_data, mock_config_entry
+) -> None:
+    """Test device type not given by API."""
+
+    api = get_mock_device(product_type=None)
+    with patch(
+        "homeassistant.components.homewizard.coordinator.HomeWizardEnergy",
+        return_value=api,
+    ):
+        entry = mock_config_entry
+        entry.data = mock_config_entry_data
+        entry.add_to_hass(hass)
+
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    state = hass.states.get("update.product_name_aabbccddeeff_firmware")
+    assert state
+
+    assert state.state == STATE_UNKNOWN
+    assert state.attributes[ATTR_INSTALLED_VERSION] == "1.00"
+    assert state.attributes[ATTR_LATEST_VERSION] is None
