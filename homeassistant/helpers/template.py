d0969fdd662a0d5ea895f8a9748f10095d64b309
danieldotnl <danieldotnl@outlook.com>
2023-09-05 14:44:32 +0000

    Optionally parse templates rendered with possible json

--- homeassistant/helpers/template.py
+++ homeassistant/helpers/template.py
@@ -723,6 +723,7 @@ class Template:
         value: Any,
         error_value: Any = _SENTINEL,
         variables: dict[str, Any] | None = None,
+        parse_result: bool = False,
     ) -> Any:
         """Render template with value exposed.
 
@@ -744,7 +745,9 @@ class Template:
             variables["value_json"] = json_loads(value)
 
         try:
-            return _render_with_context(self.template, compiled, **variables).strip()
+            render_result = _render_with_context(
+                self.template, compiled, **variables
+            ).strip()
         except jinja2.TemplateError as ex:
             if error_value is _SENTINEL:
                 _LOGGER.error(
@@ -754,6 +757,13 @@ class Template:
                     self.template,
                 )
             return value if error_value is _SENTINEL else error_value
+
+        render_result = render_result.strip()
+
+        if not parse_result or self.hass and self.hass.config.legacy_templates:
+            return render_result
+
+        return self._parse_result(render_result)
 
     def _ensure_compiled(
         self,
