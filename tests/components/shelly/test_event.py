6139c1d546314b5d39bcda2c2af9974b4b494c6c
Maciej Bieniek <bieniu@users.noreply.github.com>
2023-09-05 21:36:19 +0200

    Improve tests coverage

--- tests/components/shelly/test_event.py
+++ tests/components/shelly/test_event.py
@@ -6,13 +6,14 @@ from pytest_unordered import unordered
 from homeassistant.components.event import (
     ATTR_EVENT_TYPE,
     ATTR_EVENT_TYPES,
+    DOMAIN as EVENT_DOMAIN,
     EventDeviceClass,
 )
 from homeassistant.const import ATTR_DEVICE_CLASS, STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_registry import async_get
 
-from . import init_integration, inject_rpc_device_event
+from . import init_integration, inject_rpc_device_event, register_entity
 
 
 async def test_rpc_button(hass: HomeAssistant, mock_rpc_device, monkeypatch) -> None:
@@ -52,3 +53,18 @@ async def test_rpc_button(hass: HomeAssistant, mock_rpc_device, monkeypatch) -> 
 
     state = hass.states.get(entity_id)
     assert state.attributes.get(ATTR_EVENT_TYPE) == "single_push"
+
+
+async def test_rpc_event_removal(
+    hass: HomeAssistant, mock_rpc_device, monkeypatch
+) -> None:
+    """Test RPC event entity is removed due to removal_condition."""
+    registry = async_get(hass)
+    entity_id = register_entity(hass, EVENT_DOMAIN, "test_name_input_0", "input:0")
+
+    assert registry.async_get(entity_id) is not None
+
+    monkeypatch.setitem(mock_rpc_device.config, "input:0", {"id": 0, "type": "switch"})
+    await init_integration(hass, 2)
+
+    assert registry.async_get(entity_id) is None
