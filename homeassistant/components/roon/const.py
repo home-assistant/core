af9c68aefdb0c1ecbfe0dd0b681f487abaf1b0a9
pavoni <mail@gregdowling.com>
2023-09-05 15:58:35 +0100

    Tidy.

--- homeassistant/components/roon/const.py
+++ homeassistant/components/roon/const.py
@@ -8,9 +8,6 @@ CONF_ROON_ID = "roon_server_id"
 CONF_ROON_NAME = "roon_server_name"
 CONF_ENABLE_VOLUME_HOOKS = "roon_volume_hooks"
 
-CONF_VOLUME_HOOK_ON = "volume_hook_on"
-CONF_VOLUME_HOOK_OFF = "volume_hook_off"
-
 DATA_CONFIGS = "roon_configs"
 
 DEFAULT_NAME = "Roon Labs Music Player"
