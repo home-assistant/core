09106c0244c1c8648b06665e0a7168ca596dca87
disforw <github@benzona.net>
2023-09-06 22:12:37 -0400

    Create coordinator.py

---
+++ homeassistant/components/eyeonwater/coordinator.py
@@ -0,0 +1,43 @@
+"""EyeOnWater coordinator."""
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import aiohttp_client
+from homeassistant.helpers.update_coordinator import UpdateFailed
+from pyonwater import Account, Client, EyeOnWaterAPIError, EyeOnWaterAuthError, Meter
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class EyeOnWaterData:
+    """Manages coordinatation of API data updates."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        entry: ConfigEntry,
+        account: Account,
+    ) -> None:
+        """Initialize the data coordintator."""
+        self._entry = entry
+        self.account = account
+        websession = aiohttp_client.async_get_clientsession(hass)
+        self.client = Client(websession, account)
+        self.meters: list[Meter] = []
+        self.hass = hass
+
+    async def setup(self):
+        """Fetch all of the user's meters."""
+        self.meters = await self.account.fetch_meters(self.client)
+        _LOGGER.debug("Discovered %s meter(s)", len(self.meters))
+
+    async def read_meters(self):
+        """Read each meter."""
+        for meter in self.meters:
+            try:
+                await meter.read_meter(client=self.client)
+            except (EyeOnWaterAPIError, EyeOnWaterAuthError) as error:
+                raise UpdateFailed(error) from error
+        return self.meters
