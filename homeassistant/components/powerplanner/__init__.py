30957c2a41eea14318a83b0c90859815985a0c8a
Gustaf B <gustaf@brahme.se>
2023-09-07 22:54:11 +0000

    Removed button platform for PR

--- homeassistant/components/powerplanner/__init__.py
+++ homeassistant/components/powerplanner/__init__.py
@@ -9,7 +9,7 @@ from homeassistant.core import HomeAssistant
 
 from .const import API_KEY, DOMAIN
 
-PLATFORMS = [Platform.BINARY_SENSOR, Platform.BUTTON]
+PLATFORMS = [Platform.BINARY_SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
