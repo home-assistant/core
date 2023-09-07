d45b33e55a4c48dd938e91df00ee37650c255ea0
mkmer <m_kasper@sbcglobal.net>
2023-09-07 17:10:37 +0000

    Remove config data bump aioaladdinconnect, use new doors property for diag

--- homeassistant/components/aladdin_connect/diagnostics.py
+++ homeassistant/components/aladdin_connect/diagnostics.py
@@ -7,25 +7,24 @@ from AIOAladdinConnect import AladdinConnectClient
 
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-TO_REDACT = {CONF_PASSWORD, CONF_USERNAME, "serial", "device_id"}
+TO_REDACT = {"serial", "device_id"}
 
 
 async def async_get_config_entry_diagnostics(
-    hass: HomeAssistant, config_entry: ConfigEntry
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
 
     acc: AladdinConnectClient = hass.data[DOMAIN][config_entry.entry_id]
 
     diagnostics_data = {
-        "config_entry_data": async_redact_data(dict(config_entry.data), TO_REDACT),
         "data": {
-            "doors": async_redact_data(await acc.get_doors(), TO_REDACT),
+            "doors": async_redact_data(acc.doors, TO_REDACT),
         },
     }
 
