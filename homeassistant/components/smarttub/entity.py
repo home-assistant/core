aa9d98be41156d139cd118de50ea392d825da8a6
joostlek <joostlek@outlook.com>
2023-09-07 16:27:11 +0200

    Use shorthand attributes in Smarttub

--- homeassistant/components/smarttub/entity.py
+++ homeassistant/components/smarttub/entity.py
@@ -25,27 +25,14 @@ class SmartTubEntity(CoordinatorEntity):
 
         super().__init__(coordinator)
         self.spa = spa
-        self._entity_name = entity_name
-
-    @property
-    def unique_id(self) -> str:
-        """Return a unique id for the entity."""
-        return f"{self.spa.id}-{self._entity_name}"
-
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return device info."""
-        return DeviceInfo(
-            identifiers={(DOMAIN, self.spa.id)},
-            manufacturer=self.spa.brand,
-            model=self.spa.model,
+        self._attr_unique_id = f"{spa.id}-{entity_name}"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, spa.id)},
+            manufacturer=spa.brand,
+            model=spa.model,
         )
-
-    @property
-    def name(self) -> str:
-        """Return the name of the entity."""
         spa_name = get_spa_name(self.spa)
-        return f"{spa_name} {self._entity_name}"
+        self._attr_name = f"{spa_name} {entity_name}"
 
     @property
     def spa_status(self) -> smarttub.SpaState:
