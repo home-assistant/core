f2717dc8ee88773c0895e1014cc0e030ca3ea058
TJ Horner <me@tjhorner.com>
2023-09-09 01:03:29 +0000

    Add missing import

--- homeassistant/components/weatherkit/__init__.py
+++ homeassistant/components/weatherkit/__init__.py
@@ -10,6 +10,7 @@ from apple_weatherkit.client import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import (
