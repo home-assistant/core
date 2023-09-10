10785cb0d508a5fa1b10ae3696a6c4d41aba9242
Aidan Timson <contact@timmo.xyz>
2023-09-10 15:14:36 +0000

    Remove duplicate import

--- homeassistant/components/system_bridge/__init__.py
+++ homeassistant/components/system_bridge/__init__.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 import asyncio
 import logging
 
-import voluptuous as vol
 from systembridgeconnector.exceptions import (
     AuthenticationException,
     ConnectionClosedException,
