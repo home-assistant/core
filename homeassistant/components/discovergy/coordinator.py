82b670aefacf9c6f1b738e376a095f9414bed96e
Jan-Philipp Benecke <jan-philipp@bnck.me>
2023-09-11 10:41:57 +0200

    Change AccessTokenExpired to InvalidLogin

--- homeassistant/components/discovergy/coordinator.py
+++ homeassistant/components/discovergy/coordinator.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 import logging
 
 from pydiscovergy import Discovergy
-from pydiscovergy.error import AccessTokenExpired, DiscovergyClientError, HTTPError
+from pydiscovergy.error import DiscovergyClientError, HTTPError, InvalidLogin
 from pydiscovergy.models import Meter, Reading
 
 from homeassistant.core import HomeAssistant
@@ -44,7 +44,7 @@ class DiscovergyUpdateCoordinator(DataUpdateCoordinator[Reading]):
         """Get last reading for meter."""
         try:
             return await self.discovergy_client.meter_last_reading(self.meter.meter_id)
-        except AccessTokenExpired as err:
+        except InvalidLogin as err:
             raise ConfigEntryAuthFailed(
                 f"Auth expired while fetching last reading for meter {self.meter.meter_id}"
             ) from err
