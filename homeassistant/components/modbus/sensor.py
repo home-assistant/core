ddfdbd3e2b904d448d03fef203a4718de47afcd7
jan iversen <jancasacondor@gmail.com>
2023-09-12 08:47:52 +0200

    Review comments.

--- homeassistant/components/modbus/sensor.py
+++ homeassistant/components/modbus/sensor.py
@@ -107,13 +107,16 @@ class ModbusRegisterSensor(BaseStructPlatform, RestoreSensor, SensorEntity):
         """Update the state of the sensor."""
         # remark "now" is a dummy parameter to avoid problems with
         # async_track_time_interval
+        self._cancel_call = None
         raw_result = await self._hub.async_pb_call(
             self._slave, self._address, self._count, self._input_type
         )
         if raw_result is None:
             if self._lazy_errors:
                 self._lazy_errors -= 1
-                async_call_later(self.hass, timedelta(seconds=1), self.async_update)
+                self._cancel_call = async_call_later(
+                    self.hass, timedelta(seconds=1), self.async_update
+                )
                 return
             self._lazy_errors = self._lazy_error_count
             self._attr_available = False
