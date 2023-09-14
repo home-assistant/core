868687591b18879c4735362efc02ae1bbe2cf2a9
Mateusz Nowakowski <matiutd888@gmail.com>
2023-09-15 00:25:50 +0200

    remove custom components

--- config/custom_components/detailed_hello_world_push/const.py
+++
@@ -1,5 +0,0 @@
-"""Constants for the Detailed Hello World Push integration."""
-
-# This is the internal name of the integration, it should also match the directory
-# name for the integration.
-DOMAIN = "detailed_hello_world_push"
--- config/custom_components/example_sensor/__init__.py
+++
@@ -1,1 +0,0 @@
-"""The example sensor integration."""
