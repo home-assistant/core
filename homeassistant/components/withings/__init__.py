d922da3d5da8a30efd63808a1b65e0259c0c0019
joostlek <joostlek@outlook.com>
2023-09-14 18:22:46 +0200

    Reload on Withings options flow update

--- homeassistant/components/withings/__init__.py
+++ homeassistant/components/withings/__init__.py
@@ -150,6 +150,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         entry.async_on_unload(async_call_later(hass, 1, async_call_later_callback))
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(update_listener))
 
     return True
 
@@ -169,6 +170,11 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     async_remove_data_manager(hass, entry)
 
     return True
+
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_webhook_handler(
