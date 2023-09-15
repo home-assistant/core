827d64fa0b38eb273889af4e5d13b2f1ea32d57f
joostlek <joostlek@outlook.com>
2023-09-15 17:26:38 +0200

    Call async added to hass super in Shelly

--- homeassistant/components/shelly/entity.py
+++ homeassistant/components/shelly/entity.py
@@ -334,6 +334,7 @@ class ShellyBlockEntity(CoordinatorEntity[ShellyBlockCoordinator]):
 
     async def async_added_to_hass(self) -> None:
         """When entity is added to HASS."""
+        await super().async_added_to_hass()
         self.async_on_remove(self.coordinator.async_add_listener(self._update_callback))
 
     @callback
@@ -377,6 +378,7 @@ class ShellyRpcEntity(CoordinatorEntity[ShellyRpcCoordinator]):
 
     async def async_added_to_hass(self) -> None:
         """When entity is added to HASS."""
+        await super().async_added_to_hass()
         self.async_on_remove(self.coordinator.async_add_listener(self._update_callback))
 
     @callback
