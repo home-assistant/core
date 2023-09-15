430cdd878e00b975af1365c9e235c7047a040022
joostlek <joostlek@outlook.com>
2023-09-15 17:24:58 +0200

    Call async added to hass super in HVV departures

--- homeassistant/components/hvv_departures/binary_sensor.py
+++ homeassistant/components/hvv_departures/binary_sensor.py
@@ -194,6 +194,7 @@ class HvvDepartureBinarySensor(CoordinatorEntity, BinarySensorEntity):
 
     async def async_added_to_hass(self) -> None:
         """When entity is added to hass."""
+        await super().async_added_to_hass()
         self.async_on_remove(
             self.coordinator.async_add_listener(self.async_write_ha_state)
         )
