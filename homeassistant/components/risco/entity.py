7e5039610c9c9398829628a796f76079de11949a
joostlek <joostlek@outlook.com>
2023-09-15 17:11:44 +0200

    Call async added to hass super in Risco

--- homeassistant/components/risco/entity.py
+++ homeassistant/components/risco/entity.py
@@ -37,6 +37,7 @@ class RiscoCloudEntity(CoordinatorEntity[RiscoDataUpdateCoordinator]):
 
     async def async_added_to_hass(self) -> None:
         """When entity is added to hass."""
+        await super().async_added_to_hass()
         self.async_on_remove(
             self.coordinator.async_add_listener(self._refresh_from_coordinator)
         )
--- homeassistant/components/risco/sensor.py
+++ homeassistant/components/risco/sensor.py
@@ -88,6 +88,7 @@ class RiscoSensor(CoordinatorEntity[RiscoEventsDataUpdateCoordinator], SensorEnt
 
     async def async_added_to_hass(self) -> None:
         """When entity is added to hass."""
+        await super().async_added_to_hass()
         self._entity_registry = er.async_get(self.hass)
         self.async_on_remove(
             self.coordinator.async_add_listener(self._refresh_from_coordinator)
