47c94a772972e20477cc76ab65fa256d7420bcdb
iamrgroot <github@rgroot.nl>
2023-09-16 21:38:23 +0000

    updated tests

--- tests/components/energyzero/test_services.py
+++ tests/components/energyzero/test_services.py
@@ -3,34 +3,27 @@
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.components.energyzero.const import DOMAIN, LOGGER, SERVICE_NAME
+from homeassistant.components.energyzero.const import DOMAIN, SERVICE_NAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
-from homeassistant.setup import async_setup_component
-
-from tests.common import MockConfigEntry
 
 pytestmark = [pytest.mark.freeze_time("2022-12-07 15:00:00")]
 
 
 @pytest.mark.parametrize("type", ["gas", "energy", "all"])
 @pytest.mark.parametrize("incl_btw", [True, False, None])
-@pytest.mark.usefixtures("start", ["2023-01-01 00:00:00", None])
-@pytest.mark.usefixtures("end", ["2023-01-01 00:00:00", None])
+@pytest.mark.parametrize("start", ["2023-01-01 00:00:00", None])
+@pytest.mark.parametrize("end", ["2023-01-01 00:00:00", None])
+@pytest.mark.usefixtures("mock_energyzero")
+@pytest.mark.usefixtures("init_integration")
 async def test_service(
     hass: HomeAssistant,
-    init_integration: MockConfigEntry,
-    device_registry: dr.DeviceRegistry,
-    entity_registry: er.EntityRegistry,
     snapshot: SnapshotAssertion,
     type: str,
     incl_btw: bool,
     start: str,
     end: str,
 ) -> None:
-    """Test the EnergyZero - No gas sensors available."""
-    await async_setup_component(hass, DOMAIN, {})
-
+    """Test the EnergyZero - Service."""
     data = {
         "type": type,
     }
@@ -47,7 +40,7 @@ async def test_service(
         SERVICE_NAME,
         data,
         blocking=True,
+        return_response=True,
     )
-    await hass.async_block_till_done()
 
-    LOGGER.debug("response: %s", response)
+    assert response == snapshot
