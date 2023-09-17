bb60ab1d6a75dc2fea1547adb76aece608eb2804
J. Nick Koston <nick@koston.org>
2023-09-17 18:39:32 -0500

    ensure ids are cleared on startup

--- tests/components/recorder/test_statistics.py
+++ tests/components/recorder/test_statistics.py
@@ -1,14 +1,24 @@
 """The tests for sensor recorder platform."""
+import asyncio
 from collections.abc import Callable
 from datetime import timedelta
+from pathlib import Path
 from unittest.mock import patch
 
 import pytest
 from sqlalchemy import select
 
 from homeassistant.components import recorder
-from homeassistant.components.recorder import Recorder, history, statistics
-from homeassistant.components.recorder.db_schema import StatisticsShortTerm
+from homeassistant.components.recorder import (
+    SQLITE_URL_PREFIX,
+    Recorder,
+    history,
+    statistics,
+)
+from homeassistant.components.recorder.db_schema import (
+    LatestStatisticsShortTermIDs,
+    StatisticsShortTerm,
+)
 from homeassistant.components.recorder.models import (
     datetime_to_timestamp_or_none,
     process_timestamp,
@@ -34,7 +44,8 @@ from homeassistant.components.recorder.util import session_scope
 from homeassistant.components.sensor import UNIT_CONVERTERS
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.setup import setup_component
+from homeassistant.helpers import recorder as recorder_helper
+from homeassistant.setup import async_setup_component, setup_component
 import homeassistant.util.dt as dt_util
 
 from .common import (
@@ -46,7 +57,7 @@ from .common import (
     wait_recording_done,
 )
 
-from tests.common import mock_registry
+from tests.common import async_test_home_assistant, mock_registry
 from tests.typing import WebSocketGenerator
 
 ORIG_TZ = dt_util.DEFAULT_TIME_ZONE
@@ -2359,3 +2370,65 @@ def test_change_with_none(
     assert stats == {}
 
     dt_util.set_default_time_zone(dt_util.get_time_zone("UTC"))
+
+
+async def test_upgrade_downgrade_upgrade_clear_latest_short_term_statistics_ids(
+    tmp_path: Path,
+) -> None:
+    """Test we clear out LatestStatisticsShortTermIDs on startup for upgrade/downgrade/upgrades."""
+    test_dir = tmp_path.joinpath("sqlite")
+    test_dir.mkdir()
+    test_db_file = test_dir.joinpath("test_run_info.db")
+    dburl = f"{SQLITE_URL_PREFIX}//{test_db_file}"
+
+    hass = await async_test_home_assistant(asyncio.get_running_loop())
+    recorder_helper.async_initialize_recorder(hass)
+    assert await async_setup_component(
+        hass, "recorder", {"recorder": {"db_url": dburl}}
+    )
+    await hass.async_block_till_done()
+    await async_wait_recording_done(hass)
+    await async_wait_recording_done(hass)
+
+    def _add_data():
+        with session_scope(hass=hass) as session:
+            session.add(LatestStatisticsShortTermIDs(metadata_id=1, id=1))
+
+    def _get_latest_statistics_short_term_ids():
+        with session_scope(hass=hass) as session:
+            return list(session.query(LatestStatisticsShortTermIDs).all())
+
+    await recorder.get_instance(hass).async_add_executor_job(_add_data)
+    await hass.async_block_till_done()
+    await recorder.get_instance(hass).async_block_till_done()
+
+    ids = await recorder.get_instance(hass).async_add_executor_job(
+        _get_latest_statistics_short_term_ids
+    )
+
+    assert len(ids) == 1
+
+    await hass.async_stop()
+    await hass.async_block_till_done()
+
+    hass = await async_test_home_assistant(asyncio.get_running_loop())
+    recorder_helper.async_initialize_recorder(hass)
+    assert await async_setup_component(
+        hass, "recorder", {"recorder": {"db_url": dburl}}
+    )
+    await hass.async_block_till_done()
+
+    # We need to wait for all the migration tasks to complete
+    # before we can check the database.
+    for _ in range(10):
+        await recorder.get_instance(hass).async_block_till_done()
+        await async_wait_recording_done(hass)
+
+    ids = await recorder.get_instance(hass).async_add_executor_job(
+        _get_latest_statistics_short_term_ids
+    )
+
+    assert len(ids) == 0
+
+    await hass.async_stop()
+    await hass.async_block_till_done()
