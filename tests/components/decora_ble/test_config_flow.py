da7ee1a5f258de61c979c9c9af32fdb692c31375
Colin Campbell <142777+ColinCampbell@users.noreply.github.com>
2023-09-18 09:34:47 -0700

    More descriptive names for the results in the config flow tests

--- tests/components/decora_ble/test_config_flow.py
+++ tests/components/decora_ble/test_config_flow.py
@@ -28,21 +28,21 @@ async def test_async_step_bluetooth_initial_form_when_decora_device(
     hass: HomeAssistant,
 ) -> None:
     """Test structure of initial form."""
-    result = await hass.config_entries.flow.async_init(
+    init_result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_BLUETOOTH},
         data=DECORA_BLE_SERVICE_INFO,
     )
-    assert result["type"] == FlowResultType.FORM
-    assert result["step_id"] == "bluetooth_confirm"
-    assert result["data_schema"].schema[CONF_NAME]
+    assert init_result["type"] == FlowResultType.FORM
+    assert init_result["step_id"] == "bluetooth_confirm"
+    assert init_result["data_schema"].schema[CONF_NAME]
 
 
 async def test_async_step_bluetooth_creates_entity_when_api_key_found(
     hass: HomeAssistant,
 ) -> None:
     """Test entity creation when things are right."""
-    result = await hass.config_entries.flow.async_init(
+    init_result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_BLUETOOTH},
         data=DECORA_BLE_SERVICE_INFO,
@@ -51,18 +51,18 @@ async def test_async_step_bluetooth_creates_entity_when_api_key_found(
     with patch_async_ble_device_from_address(
         DECORA_BLE_SERVICE_INFO
     ), patch_decora_ble_get_api_key("A1B2C3D4"):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input={CONF_NAME: "Garage Lights"}
+        bluetooth_confirm_result = await hass.config_entries.flow.async_configure(
+            init_result["flow_id"], user_input={CONF_NAME: "Garage Lights"}
         )
 
-    assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Garage Lights"
-    assert result2["data"] == {
+    assert bluetooth_confirm_result["type"] == FlowResultType.CREATE_ENTRY
+    assert bluetooth_confirm_result["title"] == "Garage Lights"
+    assert bluetooth_confirm_result["data"] == {
         CONF_ADDRESS: "11:22:33:44:55:66",
         CONF_API_KEY: "A1B2C3D4",
         CONF_NAME: "Garage Lights",
     }
-    assert result2["result"].unique_id == "11:22:33:44:55:66"
+    assert bluetooth_confirm_result["result"].unique_id == "11:22:33:44:55:66"
 
 
 async def test_async_step_bluetooth_aborts_when_device_already_configured(
@@ -75,38 +75,38 @@ async def test_async_step_bluetooth_aborts_when_device_already_configured(
     )
     entry.add_to_hass(hass)
 
-    result = await hass.config_entries.flow.async_init(
+    init_result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_BLUETOOTH},
         data=DECORA_BLE_SERVICE_INFO,
     )
 
-    assert result["type"] == FlowResultType.ABORT
-    assert result["reason"] == "already_configured"
+    assert init_result["type"] == FlowResultType.ABORT
+    assert init_result["reason"] == "already_configured"
 
 
 async def test_async_step_bluetooth_errors_on_no_device(hass: HomeAssistant) -> None:
     """Test showing an error if the device cannot be connected to over bluetooth."""
-    result = await hass.config_entries.flow.async_init(
+    init_result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_BLUETOOTH},
         data=DECORA_BLE_SERVICE_INFO,
     )
 
     with patch_async_ble_device_from_address(None):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input={CONF_NAME: "Garage Lights"}
+        bluetooth_confirm_result = await hass.config_entries.flow.async_configure(
+            init_result["flow_id"], user_input={CONF_NAME: "Garage Lights"}
         )
 
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"]["base"] == "cannot_connect"
+    assert bluetooth_confirm_result["type"] == FlowResultType.FORM
+    assert bluetooth_confirm_result["errors"]["base"] == "cannot_connect"
 
 
 async def test_async_step_bluetooth_errors_on_device_not_in_pairing_mode(
     hass: HomeAssistant,
 ) -> None:
     """Test showing an error if the device is not in pairing mode."""
-    result = await hass.config_entries.flow.async_init(
+    init_result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_BLUETOOTH},
         data=DECORA_BLE_SERVICE_INFO,
@@ -115,19 +115,19 @@ async def test_async_step_bluetooth_errors_on_device_not_in_pairing_mode(
     with patch_async_ble_device_from_address(
         DECORA_BLE_SERVICE_INFO
     ), patch_decora_ble_get_api_key_fail_with_exception(DeviceNotInPairingModeError):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input={CONF_NAME: "Garage Lights"}
+        bluetooth_confirm_result = await hass.config_entries.flow.async_configure(
+            init_result["flow_id"], user_input={CONF_NAME: "Garage Lights"}
         )
 
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"]["base"] == "not_in_pairing_mode"
+    assert bluetooth_confirm_result["type"] == FlowResultType.FORM
+    assert bluetooth_confirm_result["errors"]["base"] == "not_in_pairing_mode"
 
 
 async def test_async_step_bluetooth_errors_on_device_connection_problems(
     hass: HomeAssistant,
 ) -> None:
     """Test showing an error if the device is not in pairing mode."""
-    result = await hass.config_entries.flow.async_init(
+    init_result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_BLUETOOTH},
         data=DECORA_BLE_SERVICE_INFO,
@@ -136,24 +136,24 @@ async def test_async_step_bluetooth_errors_on_device_connection_problems(
     with patch_async_ble_device_from_address(
         DECORA_BLE_SERVICE_INFO
     ), patch_decora_ble_get_api_key_fail_with_exception(DeviceConnectionError):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input={CONF_NAME: "Garage Lights"}
+        bluetooth_confirm_result = await hass.config_entries.flow.async_configure(
+            init_result["flow_id"], user_input={CONF_NAME: "Garage Lights"}
         )
 
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"]["base"] == "cannot_connect"
+    assert bluetooth_confirm_result["type"] == FlowResultType.FORM
+    assert bluetooth_confirm_result["errors"]["base"] == "cannot_connect"
 
 
 async def test_async_step_user_no_devices_found_leads_to_abort(
     hass: HomeAssistant,
 ) -> None:
     """Test no bluetooth devices found."""
-    result = await hass.config_entries.flow.async_init(
+    init_result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_USER},
     )
-    assert result["type"] == FlowResultType.ABORT
-    assert result["reason"] == "no_devices_found"
+    assert init_result["type"] == FlowResultType.ABORT
+    assert init_result["reason"] == "no_devices_found"
 
 
 async def test_async_step_user_initial_form_when_decora_device(
@@ -164,15 +164,15 @@ async def test_async_step_user_initial_form_when_decora_device(
         "homeassistant.components.decora_ble.config_flow.async_discovered_service_info",
         return_value=[DECORA_BLE_SERVICE_INFO],
     ):
-        result = await hass.config_entries.flow.async_init(
+        init_result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_USER},
         )
 
-    assert result["type"] == FlowResultType.FORM
-    assert result["step_id"] == "user"
-    assert result["data_schema"].schema[CONF_ADDRESS]
-    assert result["data_schema"].schema[CONF_NAME]
+    assert init_result["type"] == FlowResultType.FORM
+    assert init_result["step_id"] == "user"
+    assert init_result["data_schema"].schema[CONF_ADDRESS]
+    assert init_result["data_schema"].schema[CONF_NAME]
 
 
 async def test_async_step_user_aborts_when_only_device_is_not_decora(
@@ -183,13 +183,13 @@ async def test_async_step_user_aborts_when_only_device_is_not_decora(
         "homeassistant.components.decora_ble.config_flow.async_discovered_service_info",
         return_value=[NOT_DECORA_BLE_SERVICE_INFO],
     ):
-        result = await hass.config_entries.flow.async_init(
+        init_result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_USER},
         )
 
-    assert result["type"] == FlowResultType.ABORT
-    assert result["reason"] == "no_devices_found"
+    assert init_result["type"] == FlowResultType.ABORT
+    assert init_result["reason"] == "no_devices_found"
 
 
 async def test_async_step_user_aborts_when_configured_decora_device_is_only_device(
@@ -206,13 +206,13 @@ async def test_async_step_user_aborts_when_configured_decora_device_is_only_devi
         "homeassistant.components.decora_ble.config_flow.async_discovered_service_info",
         return_value=[DECORA_BLE_SERVICE_INFO],
     ):
-        result = await hass.config_entries.flow.async_init(
+        init_result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_USER},
         )
 
-    assert result["type"] == FlowResultType.ABORT
-    assert result["reason"] == "no_devices_found"
+    assert init_result["type"] == FlowResultType.ABORT
+    assert init_result["reason"] == "no_devices_found"
 
 
 async def test_async_step_user_creates_entity_when_api_key_found(
@@ -223,7 +223,7 @@ async def test_async_step_user_creates_entity_when_api_key_found(
         "homeassistant.components.decora_ble.config_flow.async_discovered_service_info",
         return_value=[DECORA_BLE_SERVICE_INFO],
     ):
-        result = await hass.config_entries.flow.async_init(
+        init_result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_USER},
         )
@@ -231,19 +231,19 @@ async def test_async_step_user_creates_entity_when_api_key_found(
     with patch_async_ble_device_from_address(
         DECORA_BLE_SERVICE_INFO
     ), patch_decora_ble_get_api_key("A1B2C3D4"):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
+        user_result = await hass.config_entries.flow.async_configure(
+            init_result["flow_id"],
             user_input={CONF_ADDRESS: "11:22:33:44:55:66", CONF_NAME: "Garage Lights"},
         )
 
-    assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Garage Lights"
-    assert result2["data"] == {
+    assert user_result["type"] == FlowResultType.CREATE_ENTRY
+    assert user_result["title"] == "Garage Lights"
+    assert user_result["data"] == {
         CONF_ADDRESS: "11:22:33:44:55:66",
         CONF_API_KEY: "A1B2C3D4",
         CONF_NAME: "Garage Lights",
     }
-    assert result2["result"].unique_id == "11:22:33:44:55:66"
+    assert user_result["result"].unique_id == "11:22:33:44:55:66"
 
 
 async def test_async_step_user_errors_on_no_device(hass: HomeAssistant) -> None:
@@ -252,19 +252,19 @@ async def test_async_step_user_errors_on_no_device(hass: HomeAssistant) -> None:
         "homeassistant.components.decora_ble.config_flow.async_discovered_service_info",
         return_value=[DECORA_BLE_SERVICE_INFO],
     ):
-        result = await hass.config_entries.flow.async_init(
+        init_result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_USER},
         )
 
     with patch_async_ble_device_from_address(None):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
+        user_result = await hass.config_entries.flow.async_configure(
+            init_result["flow_id"],
             user_input={CONF_ADDRESS: "11:22:33:44:55:66", CONF_NAME: "Garage Lights"},
         )
 
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"]["base"] == "cannot_connect"
+    assert user_result["type"] == FlowResultType.FORM
+    assert user_result["errors"]["base"] == "cannot_connect"
 
 
 async def test_async_step_user_errors_on_device_not_in_pairing_mode(
@@ -275,7 +275,7 @@ async def test_async_step_user_errors_on_device_not_in_pairing_mode(
         "homeassistant.components.decora_ble.config_flow.async_discovered_service_info",
         return_value=[DECORA_BLE_SERVICE_INFO],
     ):
-        result = await hass.config_entries.flow.async_init(
+        init_result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_USER},
         )
@@ -283,13 +283,13 @@ async def test_async_step_user_errors_on_device_not_in_pairing_mode(
     with patch_async_ble_device_from_address(
         DECORA_BLE_SERVICE_INFO
     ), patch_decora_ble_get_api_key_fail_with_exception(DeviceNotInPairingModeError):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
+        user_result = await hass.config_entries.flow.async_configure(
+            init_result["flow_id"],
             user_input={CONF_ADDRESS: "11:22:33:44:55:66", CONF_NAME: "Garage Lights"},
         )
 
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"]["base"] == "not_in_pairing_mode"
+    assert user_result["type"] == FlowResultType.FORM
+    assert user_result["errors"]["base"] == "not_in_pairing_mode"
 
 
 async def test_async_step_user_errors_on_device_connection_problems(
@@ -300,7 +300,7 @@ async def test_async_step_user_errors_on_device_connection_problems(
         "homeassistant.components.decora_ble.config_flow.async_discovered_service_info",
         return_value=[DECORA_BLE_SERVICE_INFO],
     ):
-        result = await hass.config_entries.flow.async_init(
+        init_result = await hass.config_entries.flow.async_init(
             DOMAIN,
             context={"source": config_entries.SOURCE_USER},
         )
@@ -308,10 +308,10 @@ async def test_async_step_user_errors_on_device_connection_problems(
     with patch_async_ble_device_from_address(
         DECORA_BLE_SERVICE_INFO
     ), patch_decora_ble_get_api_key_fail_with_exception(DeviceConnectionError):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
+        user_result = await hass.config_entries.flow.async_configure(
+            init_result["flow_id"],
             user_input={CONF_ADDRESS: "11:22:33:44:55:66", CONF_NAME: "Garage Lights"},
         )
 
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"]["base"] == "cannot_connect"
+    assert user_result["type"] == FlowResultType.FORM
+    assert user_result["errors"]["base"] == "cannot_connect"
