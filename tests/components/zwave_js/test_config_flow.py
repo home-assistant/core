fcc7c7bef8999f8994d0630f5d8b80f69d4d706b
J. Nick Koston <nick@koston.org>
2023-09-18 18:03:12 -0500

    zwave

--- tests/components/zwave_js/test_config_flow.py
+++ tests/components/zwave_js/test_config_flow.py
@@ -2,6 +2,7 @@
 import asyncio
 from collections.abc import Generator
 from copy import copy
+from ipaddress import ip_address
 from unittest.mock import DEFAULT, MagicMock, call, patch
 
 import aiohttp
@@ -2672,8 +2673,8 @@ async def test_zeroconf(hass: HomeAssistant) -> None:
         DOMAIN,
         context={"source": config_entries.SOURCE_ZEROCONF},
         data=ZeroconfServiceInfo(
-            host="localhost",
-            addresses=["127.0.0.1"],
+            ip_address=ip_address("127.0.0.1"),
+            ip_addresses=[ip_address("127.0.0.1")],
             hostname="mock_hostname",
             name="mock_name",
             port=3000,
