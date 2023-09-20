734600ce86dbc4aa78faea1ec6c3183f053a1ed8
iamrgroot <github@rgroot.nl>
2023-09-20 10:20:56 +0000

    Fixed tests

--- tests/components/energyzero/test_services.py
+++ tests/components/energyzero/test_services.py
@@ -9,23 +9,31 @@ from homeassistant.core import HomeAssistant
 pytestmark = [pytest.mark.freeze_time("2022-12-07 15:00:00")]
 
 
-@pytest.mark.parametrize("type", ["gas", "energy", "all"])
+@pytest.mark.usefixtures("init_integration")
+async def test_has_service(
+    hass: HomeAssistant,
+) -> None:
+    """Test the existence of the EnergyZero Service."""
+    assert hass.services.has_service(DOMAIN, SERVICE_NAME)
+
+
+@pytest.mark.usefixtures("init_integration")
+@pytest.mark.parametrize("price_type", ["gas", "energy", "all"])
 @pytest.mark.parametrize("incl_btw", [True, False, None])
 @pytest.mark.parametrize("start", ["2023-01-01 00:00:00", None])
 @pytest.mark.parametrize("end", ["2023-01-01 00:00:00", None])
-@pytest.mark.usefixtures("mock_energyzero")
-@pytest.mark.usefixtures("init_integration")
 async def test_service(
     hass: HomeAssistant,
     snapshot: SnapshotAssertion,
-    type: str,
+    price_type: str,
     incl_btw: bool,
     start: str,
     end: str,
 ) -> None:
-    """Test the EnergyZero - Service."""
+    """Test the EnergyZero Service."""
+
     data = {
-        "type": type,
+        "type": price_type,
     }
 
     if incl_btw is not None:
