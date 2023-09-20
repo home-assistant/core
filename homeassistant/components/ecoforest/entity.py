868e3fc28d7f14145a68b8775d4cd9ed4f31f478
Pedro Januário <prnjanuario@gmail.com>
2023-09-20 20:56:23 +0100

    use serial number in entity unique id
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/ecoforest/entity.py
+++ homeassistant/components/ecoforest/entity.py
@@ -23,7 +23,7 @@ class EcoforestEntity(CoordinatorEntity[EcoforestCoordinator]):
     ) -> None:
         """Initialize device information."""
         self.entity_description = description
-        self._attr_unique_id = f"{DOMAIN}_{description.key}"
+        self._attr_unique_id = f"{coordinator.data.serial_number}_{description.key}"
 
         super().__init__(coordinator)
 
