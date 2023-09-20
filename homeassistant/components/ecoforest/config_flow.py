f6575aaac95a7752289a9c706789311d3db6cbf7
Pedro Januário <prnjanuario@gmail.com>
2023-09-20 20:48:51 +0100

    Remove unused errors
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/ecoforest/config_flow.py
+++ homeassistant/components/ecoforest/config_flow.py
@@ -65,9 +65,3 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
 
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(HomeAssistantError):
-    """Error to indicate there is invalid auth."""
