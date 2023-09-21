e9ac172a8b54cf58aa920f36cda33bf78b1df2da
J. Nick Koston <nick@koston.org>
2023-09-21 19:30:12 +0200

    Avoid polling in sun sensor entities
    The sun sensor entities were polling which meant they did not
    update right away, and also needlessly wrote state when nothing
    had changed. Use the dispatcher to update them as soon as
    there is a change instead of polling

--- homeassistant/components/sun/const.py
+++ homeassistant/components/sun/const.py
@@ -4,3 +4,6 @@ from typing import Final
 DOMAIN: Final = "sun"
 
 DEFAULT_NAME: Final = "Sun"
+
+SIGNAL_POSITION_CHANGED = f"{DOMAIN}_position_changed"
+SIGNAL_EVENTS_CHANGED = f"{DOMAIN}_events_changed"
