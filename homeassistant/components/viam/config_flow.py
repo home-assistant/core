60c7160e17abe3956604e5135cdd5776a545cef4
HipsterBrown <headhipster@hipsterbrown.com>
2023-09-22 17:41:58 -0400

    feat: register services, allow API key auth flow

--- homeassistant/components/viam/config_flow.py
+++ homeassistant/components/viam/config_flow.py
@@ -9,39 +9,58 @@ from viam.rpc.dial import Credentials, DialOptions
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers.selector import selector
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER_DATA_SCHEMA = vol.Schema(
+ORG_CRED = "Org API Key"
+ROBOT_CRED = "Robot Location Secret"
+STEP_AUTH_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required("credential_type"): selector(
+            {"select": {"options": [ORG_CRED, ROBOT_CRED]}}
+        )
+    }
+)
+STEP_AUTH_ROBOT_DATA_SCHEMA = vol.Schema(
     {
         vol.Required("address"): str,
         vol.Required("secret"): str,
     }
 )
+STEP_AUTH_ORG_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required("api_id"): str,
+        vol.Required("api_key"): str,
+    }
+)
 
 
 class ViamHub:
-    """Placeholder class to make tests pass.
+    """Collect user input during config flow and wrap ViamClient authentication logic."""
 
-    TODO Remove this placeholder class and replace with things from your PyPI package.
-    """
-
-    def __init__(self, address: str) -> None:
+    def __init__(self, auth_entity: str, credential_type: str) -> None:
         """Initialize."""
-        self.address = address
-        self.client = None
+        self.auth_entity = auth_entity
+        self.credential_type = credential_type
+        self.client: ViamClient | None = None
 
     async def authenticate(self, secret: str) -> bool:
         """Test if we can authenticate with the host."""
-        creds = Credentials(type="robot-location-secret", payload=secret)
-        opts = DialOptions(auth_entity=self.address, credentials=creds)
+        creds = Credentials(type=self.credential_type, payload=secret)
+        opts = DialOptions(auth_entity=self.auth_entity, credentials=creds)
         self.client = await ViamClient.create_from_dial_options(opts)
         return bool(self.client)
+
+    def close(self) -> None:
+        """Close out the open client if it exists."""
+        if self.client:
+            self.client.close()
 
 
 async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
@@ -49,10 +68,19 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
+    credential_type = data["credential_type"]
+    hub = ViamHub(
+        data["address"]
+        if credential_type == "robot-location-secret"
+        else data["api_id"],
+        data["credential_type"],
+    )
 
-    hub = ViamHub(data["address"])
-
-    if not await hub.authenticate(data["secret"]):
+    if not await hub.authenticate(
+        data["secret"]
+        if credential_type == "robot-location-secret"
+        else data["api_key"]
+    ):
         raise InvalidAuth
 
     # If you cannot connect:
@@ -60,13 +88,40 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
     # If the authentication is wrong:
     # InvalidAuth
     if hub.client:
-        location = await hub.client.app_client.get_location()
-        hub.client.close()
+        locations = await hub.client.app_client.list_locations()
+        location = await hub.client.app_client.get_location(locations.pop().id)
 
         # Return info that you want to store in the config entry.
-        return {"title": location.name}
+        return {"title": location.name, "hub": hub}
 
     raise CannotConnect
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Viam Options flow handler."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        """Initialize options flow."""
+        super().__init__()
+        self.config_entry: config_entries.ConfigEntry = config_entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Initialize form."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional(
+                        "test",
+                        default=self.config_entry.options.get("test"),
+                    ): bool
+                }
+            ),
+        )
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -74,14 +129,41 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: config_entries.ConfigEntry,
+    ) -> config_entries.OptionsFlow:
+        """Create the options flow."""
+        return OptionsFlowHandler(config_entry)
+
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
         errors: dict[str, str] = {}
         if user_input is not None:
+            self.credential_type = (
+                "api-key"
+                if user_input["credential_type"] == ORG_CRED
+                else "robot-location-secret"
+            )
+            return await self.async_step_auth()
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_AUTH_USER_DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_auth(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
             try:
-                info = await validate_input(self.hass, user_input)
+                self.info = await validate_input(
+                    self.hass, {"credential_type": self.credential_type, **user_input}
+                )
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except InvalidAuth:
@@ -90,10 +172,45 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
             else:
-                return self.async_create_entry(title=info["title"], data=user_input)
+                self.data = user_input
+                return await self.async_step_robot()
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="auth",
+            data_schema=STEP_AUTH_ORG_DATA_SCHEMA
+            if self.credential_type == "api-key"
+            else STEP_AUTH_ROBOT_DATA_SCHEMA,
+            errors=errors,
+        )
+
+    async def async_step_robot(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Select robot from location."""
+
+        locations = await self.info["hub"].client.app_client.list_locations()
+        robots = await self.info["hub"].client.app_client.list_robots(
+            locations.pop().id
+        )
+        if user_input is not None:
+            robot_id = next(
+                robot.id for robot in robots if robot.name == user_input["robot"]
+            )
+            self.data.update(
+                {"robot_id": robot_id, "credential_type": self.credential_type}
+            )
+            self.info["hub"].close()
+            return self.async_create_entry(title=self.info["title"], data=self.data)
+
+        return self.async_show_form(
+            step_id="robot",
+            data_schema=vol.Schema(
+                {
+                    vol.Required("robot"): selector(
+                        {"select": {"options": [robot.name for robot in robots]}}
+                    )
+                }
+            ),
         )
 
 
