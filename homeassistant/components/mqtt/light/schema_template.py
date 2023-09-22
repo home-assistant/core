5b422daf36c06636e5d5b00f5237b65e3b66fcfd
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-09-22 13:32:30 +0200

    Avoid redundant calls to `async_write_ha_state` in MQTT light (#100690)
    * Limit state writes for mqtt light
    
    * Additional tests and review follow up

--- homeassistant/components/mqtt/light/schema_template.py
+++ homeassistant/components/mqtt/light/schema_template.py
@@ -46,7 +46,7 @@ from ..const import (
     PAYLOAD_NONE,
 )
 from ..debug_info import log_messages
-from ..mixins import MQTT_ENTITY_COMMON_SCHEMA, MqttEntity
+from ..mixins import MQTT_ENTITY_COMMON_SCHEMA, MqttEntity, write_state_on_attr_change
 from ..models import (
     MqttCommandTemplate,
     MqttValueTemplate,
@@ -54,7 +54,6 @@ from ..models import (
     ReceiveMessage,
     ReceivePayloadType,
 )
-from ..util import get_mqtt_data
 from .schema import MQTT_LIGHT_SCHEMA_SCHEMA
 from .schema_basic import MQTT_LIGHT_ATTRIBUTES_BLOCKED
 
@@ -215,6 +214,17 @@ class MqttLightTemplate(MqttEntity, LightEntity, RestoreEntity):
 
         @callback
         @log_messages(self.hass, self.entity_id)
+        @write_state_on_attr_change(
+            self,
+            {
+                "_attr_brightness",
+                "_attr_color_mode",
+                "_attr_color_temp",
+                "_attr_effect",
+                "_attr_hs_color",
+                "_attr_is_on",
+            },
+        )
         def state_received(msg: ReceiveMessage) -> None:
             """Handle new MQTT messages."""
             state = self._value_templates[CONF_STATE_TEMPLATE](msg.payload)
@@ -282,8 +292,6 @@ class MqttLightTemplate(MqttEntity, LightEntity, RestoreEntity):
                     self._attr_effect = effect
                 else:
                     _LOGGER.warning("Unsupported effect value received")
-
-            get_mqtt_data(self.hass).state_write_requests.write_state_request(self)
 
         if self._topics[CONF_STATE_TOPIC] is not None:
             self._sub_state = subscription.async_prepare_subscribe_topics(
