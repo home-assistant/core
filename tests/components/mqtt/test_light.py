556e40add58cb3fd009e4b976870ee804204f895
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-09-22 13:31:29 +0200

    Fix mqtt light rgbww update without state topic (#100707)
    * Fix mqtt light rgbww update without state topic
    
    * Add @callback decprator and correct mired conv

--- tests/components/mqtt/test_light.py
+++ tests/components/mqtt/test_light.py
@@ -198,6 +198,7 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant, State
 
 from .test_common import (
+    help_custom_config,
     help_test_availability_when_connection_lost,
     help_test_availability_without_topic,
     help_test_custom_availability_payload,
@@ -439,6 +440,176 @@ async def test_controlling_state_via_topic(
     assert light_state.attributes.get("xy_color") == (0.675, 0.322)
     assert light_state.attributes.get(light.ATTR_COLOR_MODE) == "xy"
     assert light_state.attributes.get(light.ATTR_SUPPORTED_COLOR_MODES) == color_modes
+
+
+@pytest.mark.parametrize(
+    "hass_config",
+    [
+        help_custom_config(
+            light.DOMAIN,
+            DEFAULT_CONFIG,
+            (
+                {
+                    "state_topic": "test-topic",
+                    "optimistic": True,
+                    "brightness_command_topic": "test_light_rgb/brightness/set",
+                    "color_mode_state_topic": "color-mode-state-topic",
+                    "rgb_command_topic": "test_light_rgb/rgb/set",
+                    "rgb_state_topic": "rgb-state-topic",
+                    "rgbw_command_topic": "test_light_rgb/rgbw/set",
+                    "rgbw_state_topic": "rgbw-state-topic",
+                    "rgbww_command_topic": "test_light_rgb/rgbww/set",
+                    "rgbww_state_topic": "rgbww-state-topic",
+                },
+            ),
+        )
+    ],
+)
+async def test_received_rgbx_values_set_state_optimistic(
+    hass: HomeAssistant, mqtt_mock_entry: MqttMockHAClientGenerator
+) -> None:
+    """Test the state is set correctly when an rgbx update is received."""
+    await mqtt_mock_entry()
+    state = hass.states.get("light.test")
+    assert state and state.state is not None
+    async_fire_mqtt_message(hass, "test-topic", "ON")
+    ## Test rgb processing
+    async_fire_mqtt_message(hass, "rgb-state-topic", "255,255,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgb"
+    assert state.attributes["rgb_color"] == (255, 255, 255)
+
+    # Only update color mode
+    async_fire_mqtt_message(hass, "color-mode-state-topic", "rgbww")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgbww"
+
+    # Resending same rgb value should restore color mode
+    async_fire_mqtt_message(hass, "rgb-state-topic", "255,255,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgb"
+    assert state.attributes["rgb_color"] == (255, 255, 255)
+
+    # Only update brightness
+    await common.async_turn_on(hass, "light.test", brightness=128)
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 128
+    assert state.attributes["color_mode"] == "rgb"
+    assert state.attributes["rgb_color"] == (255, 255, 255)
+
+    # Resending same rgb value should restore brightness
+    async_fire_mqtt_message(hass, "rgb-state-topic", "255,255,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgb"
+    assert state.attributes["rgb_color"] == (255, 255, 255)
+
+    # Only change rgb value
+    async_fire_mqtt_message(hass, "rgb-state-topic", "255,255,0")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgb"
+    assert state.attributes["rgb_color"] == (255, 255, 0)
+
+    ## Test rgbw processing
+    async_fire_mqtt_message(hass, "rgbw-state-topic", "255,255,255,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgbw"
+    assert state.attributes["rgbw_color"] == (255, 255, 255, 255)
+
+    # Only update color mode
+    async_fire_mqtt_message(hass, "color-mode-state-topic", "rgb")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgb"
+
+    # Resending same rgbw value should restore color mode
+    async_fire_mqtt_message(hass, "rgbw-state-topic", "255,255,255,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgbw"
+    assert state.attributes["rgbw_color"] == (255, 255, 255, 255)
+
+    # Only update brightness
+    await common.async_turn_on(hass, "light.test", brightness=128)
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 128
+    assert state.attributes["color_mode"] == "rgbw"
+    assert state.attributes["rgbw_color"] == (255, 255, 255, 255)
+
+    # Resending same rgbw value should restore brightness
+    async_fire_mqtt_message(hass, "rgbw-state-topic", "255,255,255,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgbw"
+    assert state.attributes["rgbw_color"] == (255, 255, 255, 255)
+
+    # Only change rgbw value
+    async_fire_mqtt_message(hass, "rgbw-state-topic", "255,255,128,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgbw"
+    assert state.attributes["rgbw_color"] == (255, 255, 128, 255)
+
+    ## Test rgbww processing
+    async_fire_mqtt_message(hass, "rgbww-state-topic", "255,255,255,32,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgbww"
+    assert state.attributes["rgbww_color"] == (255, 255, 255, 32, 255)
+
+    # Only update color mode
+    async_fire_mqtt_message(hass, "color-mode-state-topic", "rgb")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgb"
+
+    # Resending same rgbw value should restore color mode
+    async_fire_mqtt_message(hass, "rgbww-state-topic", "255,255,255,32,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgbww"
+    assert state.attributes["rgbww_color"] == (255, 255, 255, 32, 255)
+
+    # Only update brightness
+    await common.async_turn_on(hass, "light.test", brightness=128)
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 128
+    assert state.attributes["color_mode"] == "rgbww"
+    assert state.attributes["rgbww_color"] == (255, 255, 255, 32, 255)
+
+    # Resending same rgbww value should restore brightness
+    async_fire_mqtt_message(hass, "rgbww-state-topic", "255,255,255,32,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgbww"
+    assert state.attributes["rgbww_color"] == (255, 255, 255, 32, 255)
+
+    # Only change rgbww value
+    async_fire_mqtt_message(hass, "rgbww-state-topic", "255,255,128,32,255")
+    await hass.async_block_till_done()
+    state = hass.states.get("light.test")
+    assert state.attributes["brightness"] == 255
+    assert state.attributes["color_mode"] == "rgbww"
+    assert state.attributes["rgbww_color"] == (255, 255, 128, 32, 255)
 
 
 @pytest.mark.parametrize(
