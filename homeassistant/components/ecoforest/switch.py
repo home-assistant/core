77f232399e74adacaf63af21f096dfda1d86a63a
Pedro Januário <prnjanuario@gmail.com>
2023-09-22 09:04:34 +0000

    add switch platform to ecoforest integration

---
+++ homeassistant/components/ecoforest/switch.py
@@ -0,0 +1,75 @@
+"""Switch platform for Ecoforest."""
+from __future__ import annotations
+
+from collections.abc import Callable
+from dataclasses import dataclass
+
+from pyecoforest.models.device import Device
+
+from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .coordinator import EcoforestCoordinator
+from .entity import EcoforestEntity
+
+
+@dataclass
+class EcoforestRequiredKeysMixin:
+    """Mixin for required keys."""
+
+    value_fn: Callable[[Device], bool]
+
+
+@dataclass
+class EcoforestSwitchEntityDescription(
+    SwitchEntityDescription, EcoforestRequiredKeysMixin
+):
+    """Describes an Ecoforest switch entity."""
+
+
+SWITCH_TYPES: tuple[EcoforestSwitchEntityDescription, ...] = (
+    EcoforestSwitchEntityDescription(
+        key="status", translation_key="status", value_fn=lambda data: data.on
+    ),
+)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Ecoforest switch platform."""
+    coordinator: EcoforestCoordinator = hass.data[DOMAIN][config_entry.entry_id]
+
+    entities = [
+        EcoforestSwitchEntity(coordinator, description) for description in SWITCH_TYPES
+    ]
+
+    async_add_entities(entities)
+
+
+class EcoforestSwitchEntity(EcoforestEntity, SwitchEntity):
+    """Representation of an Ecoforest switch entity."""
+
+    entity_description: EcoforestSwitchEntityDescription
+
+    _attr_name = None
+
+    @property
+    def is_on(self) -> bool:
+        """Return the state of the ecoforest device."""
+        return self.entity_description.value_fn(self.data)
+
+    async def async_turn_on(self):
+        """Turn on the ecoforest device."""
+        await self.coordinator.api.turn(True)
+        await self.coordinator.async_request_refresh()
+
+    async def async_turn_off(self):
+        """Turn off the ecoforest device."""
+        await self.coordinator.api.turn(False)
+        await self.coordinator.async_request_refresh()
