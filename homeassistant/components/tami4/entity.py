e8021e8afe9544deced59cdfd1cd0cea65049226
guy shefer <g.shefer29@gmail.com>
2023-09-23 16:30:33 +0000

    Fix DeviceInfo import

--- homeassistant/components/tami4/entity.py
+++ homeassistant/components/tami4/entity.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 
 from Tami4EdgeAPI import Tami4EdgeAPI
 
-from homeassistant.helpers.entity import DeviceInfo, Entity, EntityDescription
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity import Entity, EntityDescription
 
 from .const import DOMAIN
 
