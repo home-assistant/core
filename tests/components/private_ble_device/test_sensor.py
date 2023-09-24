7d71a7a7d5e907c029d3d21019f20be5d6f23b68
John Carr <john.carr@unrouted.co.uk>
2023-09-24 12:58:20 +0100

    Add test for sensor

--- tests/components/private_ble_device/test_sensor.py
+++ tests/components/private_ble_device/test_sensor.py
@@ -1,6 +1,10 @@
 """Tests for sensors."""
 
 
+from homeassistant.components.bluetooth import async_set_fallback_availability_interval
+from homeassistant.components.bluetooth.advertisement_tracker import (
+    ADVERTISING_TIMES_NEEDED,
+)
 from homeassistant.core import HomeAssistant
 
 from . import MAC_RPA_VALID_1, async_inject_broadcast, async_mock_config_entry
@@ -45,3 +49,39 @@ async def test_sensors_come_home(
     state = hass.states.get("sensor.private_ble_device_000000_signal_strength")
     assert state
     assert state.state == "-63"
+
+
+async def test_estimated_broadcast_interval(
+    hass: HomeAssistant,
+    enable_bluetooth: None,
+    entity_registry_enabled_by_default: None,
+) -> None:
+    """Test sensors get value when we receive a broadcast."""
+    await async_mock_config_entry(hass)
+    await async_inject_broadcast(hass, MAC_RPA_VALID_1)
+
+    state = hass.states.get(
+        "sensor.private_ble_device_000000_estimated_broadcast_interval"
+    )
+    assert state
+    assert state.state == "900"
+
+    async_set_fallback_availability_interval(hass, MAC_RPA_VALID_1, 90)
+    await async_inject_broadcast(hass, MAC_RPA_VALID_1.upper())
+
+    state = hass.states.get(
+        "sensor.private_ble_device_000000_estimated_broadcast_interval"
+    )
+    assert state
+    assert state.state == "90"
+
+    for i in range(ADVERTISING_TIMES_NEEDED):
+        await async_inject_broadcast(
+            hass, MAC_RPA_VALID_1, mfr_data=bytes(i), broadcast_time=i * 10
+        )
+
+    state = hass.states.get(
+        "sensor.private_ble_device_000000_estimated_broadcast_interval"
+    )
+    assert state
+    assert state.state == "10"
