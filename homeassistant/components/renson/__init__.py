787028ba7e352266759f54a1370a6dc851c16ab2
jimmyd-be <34766203+jimmyd-be@users.noreply.github.com>
2023-09-25 17:47:04 +0200

    Update homeassistant/components/renson/__init__.py
    Co-authored-by: Robert Resch <robert@resch.dev>

--- homeassistant/components/renson/__init__.py
+++ homeassistant/components/renson/__init__.py
@@ -79,9 +79,9 @@ def setup_hass_services(hass: HomeAssistant, renson_api: RensonVentilation) -> N
 
     async def set_breeze(call: ServiceCall) -> None:
         """Configure breeze feature."""
-        level = call.data.get("breeze_level", "")
-        temperature = call.data.get("temperature", 0)
-        activated = call.data.get("activate", False)
+        level = call.data.get["breeze_level"]
+        temperature = call.data.get["temperature"]
+        activated = call.data.get["activate"]
 
         await hass.async_add_executor_job(
             renson_api.set_breeze, level, temperature, activated
