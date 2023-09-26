3edc4a301814c6f504179db0269b675b454cfebd
Joost Lekkerkerker <joostlek@outlook.com>
2023-09-26 21:52:18 +0200

    Support cloudhooks in Withings (#100916)
    * Support cloudhooks in Withings
    
    * Support cloudhooks in Withings
    
    * Support cloudhooks in Withings
    
    * Remove strings

--- tests/components/withings/test_config_flow.py
+++ tests/components/withings/test_config_flow.py
@@ -1,7 +1,7 @@
 """Tests for config flow."""
 from unittest.mock import AsyncMock, patch
 
-from homeassistant.components.withings.const import CONF_USE_WEBHOOK, DOMAIN
+from homeassistant.components.withings.const import DOMAIN
 from homeassistant.config_entries import SOURCE_REAUTH, SOURCE_USER
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
@@ -84,7 +84,6 @@ async def test_config_non_unique_profile(
     current_request_with_host: None,
     withings: AsyncMock,
     polling_config_entry: MockConfigEntry,
-    disable_webhook_delay,
     aioclient_mock: AiohttpClientMocker,
 ) -> None:
     """Test setup a non-unique profile."""
@@ -138,7 +137,6 @@ async def test_config_reauth_profile(
     aioclient_mock: AiohttpClientMocker,
     polling_config_entry: MockConfigEntry,
     withings: AsyncMock,
-    disable_webhook_delay,
     current_request_with_host,
 ) -> None:
     """Test reauth an existing profile reauthenticates the config entry."""
@@ -201,7 +199,6 @@ async def test_config_reauth_wrong_account(
     aioclient_mock: AiohttpClientMocker,
     polling_config_entry: MockConfigEntry,
     withings: AsyncMock,
-    disable_webhook_delay,
     current_request_with_host,
 ) -> None:
     """Test reauth with wrong account."""
@@ -256,31 +253,3 @@ async def test_config_reauth_wrong_account(
     assert result
     assert result["type"] == FlowResultType.ABORT
     assert result["reason"] == "wrong_account"
-
-
-async def test_options_flow(
-    hass: HomeAssistant,
-    hass_client_no_auth: ClientSessionGenerator,
-    aioclient_mock: AiohttpClientMocker,
-    polling_config_entry: MockConfigEntry,
-    withings: AsyncMock,
-    disable_webhook_delay,
-    current_request_with_host,
-) -> None:
-    """Test options flow."""
-    await setup_integration(hass, polling_config_entry)
-
-    result = await hass.config_entries.options.async_init(polling_config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    assert result["type"] == FlowResultType.FORM
-    assert result["step_id"] == "init"
-
-    result = await hass.config_entries.options.async_configure(
-        result["flow_id"],
-        user_input={CONF_USE_WEBHOOK: True},
-    )
-    await hass.async_block_till_done()
-
-    assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["data"] == {CONF_USE_WEBHOOK: True}
