0f95de997f4b27ffcbf3899ffb2eef0be9d29e35
Joost Lekkerkerker <joostlek@outlook.com>
2023-09-26 21:52:18 +0200

    Support cloudhooks in Withings (#100916)
    * Support cloudhooks in Withings
    
    * Support cloudhooks in Withings
    
    * Support cloudhooks in Withings
    
    * Remove strings

--- tests/components/withings/__init__.py
+++ tests/components/withings/__init__.py
@@ -6,10 +6,8 @@ from urllib.parse import urlparse
 from aiohttp.test_utils import TestClient
 
 from homeassistant.components.webhook import async_generate_url
-from homeassistant.components.withings.const import CONF_USE_WEBHOOK, DOMAIN
 from homeassistant.config import async_process_ha_core_config
 from homeassistant.core import HomeAssistant
-from homeassistant.setup import async_setup_component
 
 from tests.common import MockConfigEntry
 
@@ -42,26 +40,16 @@ async def call_webhook(
     return WebhookResponse(message=data["message"], message_code=data["code"])
 
 
-async def setup_integration(hass: HomeAssistant, config_entry: MockConfigEntry) -> None:
+async def setup_integration(
+    hass: HomeAssistant, config_entry: MockConfigEntry, enable_webhooks: bool = True
+) -> None:
     """Fixture for setting up the component."""
     config_entry.add_to_hass(hass)
 
-    await async_process_ha_core_config(
-        hass,
-        {"external_url": "http://example.local:8123"},
-    )
+    if enable_webhooks:
+        await async_process_ha_core_config(
+            hass,
+            {"external_url": "https://example.local:8123"},
+        )
 
     await hass.config_entries.async_setup(config_entry.entry_id)
-
-
-async def enable_webhooks(hass: HomeAssistant) -> None:
-    """Enable webhooks."""
-    assert await async_setup_component(
-        hass,
-        DOMAIN,
-        {
-            DOMAIN: {
-                CONF_USE_WEBHOOK: True,
-            }
-        },
-    )
