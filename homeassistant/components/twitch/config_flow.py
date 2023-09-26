5a1b3fa5f488844d527e968716b292750e301dc1
joostlek <joostlek@outlook.com>
2023-09-26 20:32:05 +0200

    Fix feedback

--- homeassistant/components/twitch/config_flow.py
+++ homeassistant/components/twitch/config_flow.py
@@ -6,41 +6,14 @@ from typing import Any
 
 from twitchAPI.helper import first
 from twitchAPI.twitch import Twitch
-import voluptuous as vol
+from twitchAPI.types import AuthScope, InvalidTokenException
 
-from homeassistant.const import (
-    CONF_ACCESS_TOKEN,
-    CONF_CLIENT_ID,
-    CONF_CLIENT_SECRET,
-    CONF_TOKEN,
-)
+from homeassistant.const import CONF_ACCESS_TOKEN, CONF_CLIENT_ID, CONF_TOKEN
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import config_entry_oauth2_flow
-from homeassistant.helpers.selector import (
-    SelectOptionDict,
-    SelectSelector,
-    SelectSelectorConfig,
-)
+from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 
 from .const import CONF_CHANNELS, CONF_REFRESH_TOKEN, DOMAIN, LOGGER, OAUTH_SCOPES
-
-
-async def _get_twitch_client(
-    client_id: str, client_secret: str, access_token: str, refresh_token: str
-) -> Twitch:
-    client = await Twitch(
-        app_id=client_id,
-        app_secret=client_secret,
-        target_app_auth_scope=OAUTH_SCOPES,
-    )
-    client.auto_refresh_auth = False
-    await client.set_user_authentication(
-        token=access_token,
-        refresh_token=refresh_token,
-        scope=OAUTH_SCOPES,
-        validate=True,
-    )
-    return client
 
 
 class OAuth2FlowHandler(
@@ -49,10 +22,11 @@ class OAuth2FlowHandler(
     """Config flow to handle Twitch OAuth2 authentication."""
 
     DOMAIN = DOMAIN
-    _data: dict[str, Any] = {}
 
-    _user_display_name: str = ""
-    _user_id: str = ""
+    def __init__(self) -> None:
+        """Initialize flow."""
+        super().__init__()
+        self.data: dict[str, Any] = {}
 
     @property
     def logger(self) -> logging.Logger:
@@ -69,65 +43,76 @@ class OAuth2FlowHandler(
         data: dict[str, Any],
     ) -> FlowResult:
         """Handle the initial step."""
-        client_id = self.flow_impl.__dict__[CONF_CLIENT_ID]
-        client_secret = self.flow_impl.__dict__[CONF_CLIENT_SECRET]
-        access_token = data[CONF_TOKEN][CONF_ACCESS_TOKEN]
-        refresh_token = data[CONF_TOKEN][CONF_REFRESH_TOKEN]
 
-        client = await _get_twitch_client(
-            client_id, client_secret, access_token, refresh_token
+        client = await Twitch(
+            app_id=self.flow_impl.__dict__[CONF_CLIENT_ID],
+            target_app_auth_scope=OAUTH_SCOPES,
         )
-        if not (user := await first(client.get_users())):
-            return self.async_abort(reason="user_not_found")
+        client.auto_refresh_auth = False
+        await client.set_user_authentication(data[CONF_TOKEN][CONF_ACCESS_TOKEN])
+        user = await first(client.get_users())
+        assert user
 
-        self._user_display_name = user.display_name
-        self._user_id = user.id
-        self._data = data
-
-        await self.async_set_unique_id(user.id)
+        user_id = user.id
+        await self.async_set_unique_id(user_id)
         self._abort_if_unique_id_configured()
 
-        return await self.async_step_channels()
-
-    async def async_step_channels(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Select which channels to follow. The channels the user follow are prefilled."""
-        client_id = self.flow_impl.__dict__[CONF_CLIENT_ID]
-        client_secret = self.flow_impl.__dict__[CONF_CLIENT_SECRET]
-        access_token = self._data[CONF_TOKEN][CONF_ACCESS_TOKEN]
-        refresh_token = self._data[CONF_TOKEN][CONF_REFRESH_TOKEN]
-
-        client = await _get_twitch_client(
-            client_id, client_secret, access_token, refresh_token
-        )
-
-        if user_input is not None:
-            client.get_users(logins=user_input[CONF_CHANNELS])
-            #     Need to check if it checks all channels if they are valid
-            return self.async_create_entry(
-                title=self._user_display_name, data=self._data, options=user_input
-            )
-        followed_channels = await client.get_followed_channels(user_id=self._user_id)
-
-        followed_channel_list = [
-            SelectOptionDict(
-                value=channel.broadcaster_login, label=channel.broadcaster_name
-            )
-            for channel in followed_channels.data
+        channels = [
+            channel.broadcaster_login
+            async for channel in client.get_followed_channels(user_id)
         ]
 
-        return self.async_show_form(
-            step_id="channels",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_CHANNELS): SelectSelector(
-                        SelectSelectorConfig(
-                            options=followed_channel_list,
-                            custom_value=True,
-                            multiple=True,
-                        )
-                    )
-                }
-            ),
+        return self.async_create_entry(
+            title=user.display_name, data=data, options={CONF_CHANNELS: channels}
+        )
+
+    async def async_step_import(self, config: dict[str, Any]) -> FlowResult:
+        """Import from yaml."""
+        if CONF_TOKEN in config:
+            client = await Twitch(
+                app_id=config[CONF_CLIENT_ID],
+                target_app_auth_scope=[AuthScope.USER_READ_SUBSCRIPTIONS],
+            )
+            client.auto_refresh_auth = False
+            token = config[CONF_TOKEN]
+            try:
+                client.set_user_authentication(token, validate=True)
+            except InvalidTokenException:
+                async_create_issue(
+                    self.hass,
+                    DOMAIN,
+                    "deprecated_yaml_invalid_token",
+                    breaks_in_ha_version="2024.4.0",
+                    is_fixable=False,
+                    severity=IssueSeverity.WARNING,
+                    translation_key="deprecated_yaml_invalid_token",
+                )
+                return self.async_abort(reason="invalid_token")
+            user = await first(client.get_users())
+            assert user
+            await self.async_set_unique_id(user.id)
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(
+                title=user.display_name,
+                data={
+                    "auth_implementation": DOMAIN,
+                    CONF_TOKEN: {
+                        CONF_ACCESS_TOKEN: token,
+                        CONF_REFRESH_TOKEN: "",
+                        "expires_at": 0,
+                    },
+                },
+                options={CONF_CHANNELS: config[CONF_CHANNELS]},
+            )
+        return self.async_create_entry(
+            title="Twitch",
+            data={
+                "auth_implementation": DOMAIN,
+                CONF_TOKEN: {
+                    CONF_ACCESS_TOKEN: "",
+                    CONF_REFRESH_TOKEN: "",
+                    "expires_at": 0,
+                },
+            },
+            options={CONF_CHANNELS: config[CONF_CHANNELS]},
         )
--- homeassistant/components/twitch/sensor.py
+++ homeassistant/components/twitch/sensor.py
@@ -11,8 +11,12 @@ from twitchAPI.twitch import (
 )
 import voluptuous as vol
 
+from homeassistant.components.application_credentials import (
+    ClientCredential,
+    async_import_client_credential,
+)
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_TOKEN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_entry_oauth2_flow
@@ -63,12 +67,22 @@ async def async_setup_platform(
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the Twitch platform."""
+    await async_import_client_credential(
+        hass,
+        DOMAIN,
+        ClientCredential(config[CONF_CLIENT_ID], config[CONF_CLIENT_SECRET]),
+    )
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=config
+        )
+    )
 
     async_create_issue(
         hass,
         DOMAIN,
         "deprecated_yaml",
-        breaks_in_ha_version="2024.2.0",
+        breaks_in_ha_version="2024.4.0",
         is_fixable=False,
         severity=IssueSeverity.WARNING,
         translation_key="deprecated_yaml",
@@ -132,7 +146,7 @@ class TwitchSensor(SensorEntity):
 
     async def async_update(self) -> None:
         """Update device state."""
-        followers = (await self._client.get_users_follows(to_id=self._channel.id)).total
+        followers = (await self._client.get_channel_followers(self._channel.id)).total
         self._attr_extra_state_attributes = {
             ATTR_FOLLOWING: followers,
             ATTR_VIEWS: self._channel.view_count,
@@ -172,13 +186,11 @@ class TwitchSensor(SensorEntity):
         except TwitchAPIException as exc:
             LOGGER.error("Error response on check_user_subscription: %s", exc)
 
-        follows = (
-            await self._client.get_users_follows(
-                from_id=user.id, to_id=self._channel.id
-            )
-        ).data
-        self._attr_extra_state_attributes[ATTR_FOLLOW] = len(follows) > 0
-        if len(follows):
-            self._attr_extra_state_attributes[ATTR_FOLLOW_SINCE] = follows[
+        follows = await self._client.get_followed_channels(
+            user.id, broadcaster_id=self._channel.id
+        )
+        self._attr_extra_state_attributes[ATTR_FOLLOW] = follows.total > 0
+        if follows.total:
+            self._attr_extra_state_attributes[ATTR_FOLLOW_SINCE] = follows.data[
                 0
             ].followed_at
