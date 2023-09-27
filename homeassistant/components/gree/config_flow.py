99d22516f223d454a754a407121605e0b53544f7
Kyle Spearrin <kyle.spearrin@gmail.com>
2023-09-27 15:37:58 +0000

    entrys are networks, not devices

--- homeassistant/components/gree/config_flow.py
+++ homeassistant/components/gree/config_flow.py
@@ -23,7 +23,7 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]):
     """Validate the user input allows us to connect.
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
@@ -40,9 +40,6 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
     if len(devices) < 1:
         raise CannotConnect
 
-    # Return info that you want to store in the config entry.
-    return {"mac": devices[0].mac, "title": devices[0].name}
-
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Gree."""
@@ -56,16 +53,19 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
         if user_input is not None:
             try:
-                info = await validate_input(self.hass, user_input)
+                await validate_input(self.hass, user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
             else:
-                await self.async_set_unique_id(info["mac"])
-                self._abort_if_unique_id_configured()
-                return self.async_create_entry(title=info["title"], data=user_input)
+                if "ip" in user_input and user_input["ip"]:
+                    title = user_input["ip"]
+                else:
+                    title = "HomeAssistant"
+                await self.async_set_unique_id(title, raise_on_progress=False)
+                return self.async_create_entry(title=title, data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
