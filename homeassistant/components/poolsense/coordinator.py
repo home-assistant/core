fbb6e3b550225f0caf76098a1ccbca4e8cc602b0
Jan-Philipp Benecke <jan-philipp@bnck.me>
2023-09-27 08:03:20 +0200

    Add typing to poolsense

--- homeassistant/components/poolsense/coordinator.py
+++ homeassistant/components/poolsense/coordinator.py
@@ -2,11 +2,14 @@
 import asyncio
 from datetime import timedelta
 import logging
+from typing import Any
 
 from poolsense import PoolSense
 from poolsense.exceptions import PoolSenseError
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import aiohttp_client
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
@@ -15,10 +18,10 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 
-class PoolSenseDataUpdateCoordinator(DataUpdateCoordinator):
+class PoolSenseDataUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
     """Define an object to hold PoolSense data."""
 
-    def __init__(self, hass, entry):
+    def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
         """Initialize."""
         self.poolsense = PoolSense(
             aiohttp_client.async_get_clientsession(hass),
@@ -26,11 +29,10 @@ class PoolSenseDataUpdateCoordinator(DataUpdateCoordinator):
             entry.data[CONF_PASSWORD],
         )
         self.hass = hass
-        self.entry = entry
 
         super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=timedelta(hours=1))
 
-    async def _async_update_data(self):
+    async def _async_update_data(self) -> dict[str, Any]:
         """Update data via library."""
         data = {}
         async with asyncio.timeout(10):
--- homeassistant/components/poolsense/entity.py
+++ homeassistant/components/poolsense/entity.py
@@ -3,6 +3,7 @@ from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import ATTRIBUTION
+from .coordinator import PoolSenseDataUpdateCoordinator
 
 
 class PoolSenseEntity(CoordinatorEntity):
@@ -10,7 +11,12 @@ class PoolSenseEntity(CoordinatorEntity):
 
     _attr_attribution = ATTRIBUTION
 
-    def __init__(self, coordinator, email, description: EntityDescription) -> None:
+    def __init__(
+        self,
+        coordinator: PoolSenseDataUpdateCoordinator,
+        email: str,
+        description: EntityDescription,
+    ) -> None:
         """Initialize poolsense sensor."""
         super().__init__(coordinator)
         self.entity_description = description
