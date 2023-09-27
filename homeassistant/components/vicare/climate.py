9a43d295be9b6ad6166559cd14e41968f3bbb666
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2023-09-27 18:25:52 +0200

    change format

--- homeassistant/components/vicare/climate.py
+++ homeassistant/components/vicare/climate.py
@@ -36,7 +36,13 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import CONF_HEATING_TYPE, DOMAIN, HEATING_TYPE_TO_CREATOR_METHOD, VICARE_DEVICE_LIST, HeatingType
+from .const import (
+    CONF_HEATING_TYPE,
+    DOMAIN,
+    HEATING_TYPE_TO_CREATOR_METHOD,
+    VICARE_DEVICE_LIST,
+    HeatingType,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
