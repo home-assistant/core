7fdd4ffbb015f05a7965ed81481f7d0a36d5ada2
Zehuan Li <22104836+zehuanli@users.noreply.github.com>
2023-09-28 13:30:39 -0400

    Revert back to 401

--- homeassistant/components/telegram_bot/webhooks.py
+++ homeassistant/components/telegram_bot/webhooks.py
@@ -148,7 +148,7 @@ class PushBotView(HomeAssistantView):
         secret_token_header = request.headers.get("X-Telegram-Bot-Api-Secret-Token")
         if secret_token_header is None or self.secret_token != secret_token_header:
             _LOGGER.warning("Invalid secret token from %s", real_ip)
-            return self.json_message("Access denied", HTTPStatus.FORBIDDEN)
+            return self.json_message("Access denied", HTTPStatus.UNAUTHORIZED)
 
         try:
             update_data = await request.json()
