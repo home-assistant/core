fef778ec26fb17151533a9eff07c27dce6916916
Allen Porter <allen@thebends.org>
2023-09-30 16:23:16 +0000

    Config flow test improvements

--- tests/components/fitbit/test_config_flow.py
+++ tests/components/fitbit/test_config_flow.py
@@ -133,6 +133,55 @@ async def test_api_failure(
     assert result.get("reason") == "cannot_connect"
 
 
+async def test_config_entry_already_exists(
+    hass: HomeAssistant,
+    hass_client_no_auth: ClientSessionGenerator,
+    aioclient_mock: AiohttpClientMocker,
+    current_request_with_host: None,
+    requests_mock: Mocker,
+    setup_credentials: None,
+    integration_setup: Callable[[], Awaitable[bool]],
+    config_entry: MockConfigEntry,
+) -> None:
+    """Test that an account may only be configured once."""
+
+    # Verify existing config entry
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    state = config_entry_oauth2_flow._encode_jwt(
+        hass,
+        {
+            "flow_id": result["flow_id"],
+            "redirect_uri": REDIRECT_URL,
+        },
+    )
+    assert result["type"] == FlowResultType.EXTERNAL_STEP
+    assert result["url"] == (
+        f"{OAUTH2_AUTHORIZE}?response_type=code&client_id={CLIENT_ID}"
+        f"&redirect_uri={REDIRECT_URL}"
+        f"&state={state}"
+        "&scope=activity+heartrate+nutrition+profile+settings+sleep+weight&prompt=consent"
+    )
+
+    client = await hass_client_no_auth()
+    resp = await client.get(f"/auth/external/callback?code=abcd&state={state}")
+    assert resp.status == 200
+    assert resp.headers["content-type"] == "text/html; charset=utf-8"
+
+    aioclient_mock.post(
+        OAUTH2_TOKEN,
+        json=SERVER_ACCESS_TOKEN,
+    )
+
+    result = await hass.config_entries.flow.async_configure(result["flow_id"])
+    assert result.get("type") == FlowResultType.ABORT
+    assert result.get("reason") == "already_configured"
+
+
 async def test_import_fitbit_config(
     hass: HomeAssistant,
     fitbit_config_setup: None,
@@ -183,7 +232,7 @@ async def test_import_fitbit_config_failure_cannot_connect(
     issue_registry: ir.IssueRegistry,
     requests_mock: Mocker,
 ) -> None:
-    """Test that platform configuration is imported successfully."""
+    """Test platform configuration fails to import successfully."""
 
     requests_mock.register_uri(
         "GET", PROFILE_API_URL, status_code=HTTPStatus.INTERNAL_SERVER_ERROR
