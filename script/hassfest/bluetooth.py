0ed1c62de0c78fa1b013bceeb10a749e6d6dda02
Nathaniel Handan <87228776+Tinny-Robot@users.noreply.github.com>
2023-10-01 21:21:29 +0000

    Improve Bluetooth code formatting and documentation

--- script/hassfest/bluetooth.py
+++ script/hassfest/bluetooth.py
@@ -6,45 +6,67 @@ from .serializer import format_python_namespace
 
 
 def generate_and_validate(integrations: dict[str, Integration]) -> str:
-    """Validate and generate bluetooth data."""
-    match_list = []
+    """Validate and generate Bluetooth data.
+
+    Args:
+        integrations (dict[str, Integration]): A dictionary of integrations.
+
+    Returns:
+        str: The formatted Bluetooth data.
+    """
+    bluetooth_data = []
 
     for domain in sorted(integrations):
-        match_types = integrations[domain].manifest.get("bluetooth", [])
+        bluetooth_manifest = integrations[domain].manifest.get("bluetooth", [])
 
-        if not match_types:
+        if not bluetooth_manifest:
             continue
 
-        for entry in match_types:
-            match_list.append({"domain": domain, **entry})
+        for entry in bluetooth_manifest:
+            bluetooth_data.append({"domain": domain, **entry})
 
     return format_python_namespace(
-        {"BLUETOOTH": match_list},
-        annotations={"BLUETOOTH": "list[dict[str, bool | str | int | list[int]]]"},
+        {"BLUETOOTH": bluetooth_data},
+        annotations={"BLUETOOTH": list[dict[str, bool | str | int | list[int]]]},
     )
 
 
 def validate(integrations: dict[str, Integration], config: Config) -> None:
-    """Validate bluetooth file."""
+    """Validate the Bluetooth file.
+
+    Args:
+        integrations (dict[str, Integration]): A dictionary of integrations.
+        config (Config): The configuration.
+
+    Returns:
+        None
+    """
     bluetooth_path = config.root / "homeassistant/generated/bluetooth.py"
-    config.cache["bluetooth"] = content = generate_and_validate(integrations)
+    config.cache["bluetooth"] = generated_content = generate_and_validate(integrations)
 
     if config.specific_integrations:
         return
 
-    with open(str(bluetooth_path)) as fp:
-        current = fp.read()
-        if current != content:
+    with open(str(bluetooth_path)) as file:
+        current_content = file.read()
+        if current_content != generated_content:
             config.add_error(
                 "bluetooth",
                 "File bluetooth.py is not up to date. Run python3 -m script.hassfest",
                 fixable=True,
             )
-        return
 
 
 def generate(integrations: dict[str, Integration], config: Config) -> None:
-    """Generate bluetooth file."""
+    """Generate the Bluetooth file.
+
+    Args:
+        integrations (dict[str, Integration]): A dictionary of integrations.
+        config (Config): The configuration.
+
+    Returns:
+        None
+    """
     bluetooth_path = config.root / "homeassistant/generated/bluetooth.py"
-    with open(str(bluetooth_path), "w") as fp:
-        fp.write(f"{config.cache['bluetooth']}")
+    with open(str(bluetooth_path), "w") as file:
+        file.write(config.cache["bluetooth"])
