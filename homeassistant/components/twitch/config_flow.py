d3f95335363d687fcdb96efd634d0e984140697c
Aidan Timson <aidan@timmo.dev>
2023-10-01 09:05:59 +0000

    Add channels flow step

--- homeassistant/components/twitch/config_flow.py
+++ homeassistant/components/twitch/config_flow.py
@@ -6,17 +6,44 @@ import logging
 from typing import Any
 
 from twitchAPI.helper import first
-from twitchAPI.twitch import Twitch
+from twitchAPI.twitch import (
+    FollowedChannel,
+    Twitch,
+    TwitchAPIException,
+    TwitchBackendException,
+    TwitchUser,
+)
 from twitchAPI.type import AuthScope, InvalidTokenException
+import voluptuous as vol
 
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.config_entries import ConfigEntry, OptionsFlow
 from homeassistant.const import CONF_ACCESS_TOKEN, CONF_CLIENT_ID, CONF_TOKEN
-from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN
+from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, callback
 from homeassistant.data_entry_flow import AbortFlow, FlowResult
 from homeassistant.helpers import config_entry_oauth2_flow
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 
 from .const import CONF_CHANNELS, CONF_REFRESH_TOKEN, DOMAIN, LOGGER, OAUTH_SCOPES
+
+
+async def get_followed_channels(
+    client: Twitch,
+    user: TwitchUser,
+) -> list[FollowedChannel]:
+    """Return a list of channels the user is following."""
+    channels: list[FollowedChannel] = [
+        channel
+        async for channel in await client.get_followed_channels(
+            user_id=user.id,
+        )
+    ]
+
+    return sorted(
+        channels,
+        key=lambda channel: channel.broadcaster_name.lower(),
+        reverse=False,
+    )
 
 
 class OAuth2FlowHandler(
@@ -31,6 +58,9 @@ class OAuth2FlowHandler(
         """Initialize flow."""
         super().__init__()
         self.data: dict[str, Any] = {}
+        self._client: Twitch | None = None
+        self._oauth_data: dict[str, Any] = {}
+        self._user: TwitchUser | None = None
 
     @property
     def logger(self) -> logging.Logger:
@@ -56,23 +86,20 @@ class OAuth2FlowHandler(
         await client.set_user_authentication(
             data[CONF_TOKEN][CONF_ACCESS_TOKEN], scope=OAUTH_SCOPES
         )
-        user = await first(client.get_users())
-        assert user
 
-        user_id = user.id
+        self._client = client
+        self._oauth_data = data
+
+        self._user = await first(client.get_users())
+        assert self._user
+
+        user_id = self._user.id
 
         if not self.reauth_entry:
             await self.async_set_unique_id(user_id)
             self._abort_if_unique_id_configured()
 
-            channels = [
-                channel.broadcaster_login
-                async for channel in await client.get_followed_channels(user_id)
-            ]
-
-            return self.async_create_entry(
-                title=user.display_name, data=data, options={CONF_CHANNELS: channels}
-            )
+            return await self.async_step_channels()
 
         if self.reauth_entry.unique_id == user_id:
             new_channels = self.reauth_entry.options[CONF_CHANNELS]
@@ -187,3 +214,140 @@ class OAuth2FlowHandler(
             },
             options={CONF_CHANNELS: config[CONF_CHANNELS]},
         )
+
+    async def async_step_channels(
+        self,
+        user_input: dict[str, Any] | None = None,
+    ) -> FlowResult:
+        """Handle channels step."""
+        self.logger.debug("Step channels: %s", user_input)
+
+        self.logger.debug("User: %s", self._user)
+        assert self._client
+        assert self._user
+
+        if not user_input:
+            try:
+                channels = await get_followed_channels(
+                    self._client,
+                    self._user,
+                )
+            except (TwitchAPIException, TwitchBackendException) as err:
+                self.logger.error("Twitch API error: %s", err)
+                return self.async_abort(reason="cannot_connect")
+
+            self.logger.debug("Channels: %s", channels)
+
+            return self.async_show_form(
+                step_id="channels",
+                data_schema=vol.Schema(
+                    {
+                        vol.Required(CONF_CHANNELS): cv.multi_select(
+                            {
+                                channel.broadcaster_id: channel.broadcaster_name
+                                for channel in channels
+                            }
+                        ),
+                    }
+                ),
+            )
+
+        return self.async_create_entry(
+            title=self._user.display_name,
+            data=self._oauth_data,
+            options={CONF_CHANNELS: user_input[CONF_CHANNELS]},
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> OptionsFlowHandler:
+        """Get the options flow for this handler."""
+        return OptionsFlowHandler(config_entry)
+
+
+class OptionsFlowHandler(OptionsFlow):
+    """Handle a option flow for GitHub."""
+
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    @property
+    def logger(self) -> logging.Logger:
+        """Return logger."""
+        return logging.getLogger(__name__)
+
+    async def async_step_init(
+        self,
+        user_input: dict[str, Any] | None = None,
+    ) -> FlowResult:
+        """Handle init options flow."""
+        return await self.async_step_channels(user_input)
+
+    async def async_step_channels(
+        self,
+        user_input: dict[str, Any] | None = None,
+    ) -> FlowResult:
+        """Handle channels options flow."""
+        if not user_input:
+            implementation = (
+                await config_entry_oauth2_flow.async_get_config_entry_implementation(
+                    self.hass,
+                    self.config_entry,
+                )
+            )
+
+            configured_channels: list[str] = self.config_entry.options[CONF_CHANNELS]
+
+            client_id = implementation.__dict__[CONF_CLIENT_ID]
+            access_token = self.config_entry.data[CONF_TOKEN][CONF_ACCESS_TOKEN]
+            refresh_token = self.config_entry.data[CONF_TOKEN][CONF_REFRESH_TOKEN]
+
+            client = Twitch(
+                app_id=client_id,
+                authenticate_app=False,
+            )
+            client.auto_refresh_auth = False
+
+            await client.set_user_authentication(
+                access_token,
+                OAUTH_SCOPES,
+                refresh_token=refresh_token,
+                validate=True,
+            )
+
+            user = await first(client.get_users())
+            assert user
+
+            channels = await get_followed_channels(
+                client,
+                user,
+            )
+
+            channel_ids = [channel.broadcaster_id for channel in channels]
+            channels_dict = {
+                channel.broadcaster_id: channel.broadcaster_name for channel in channels
+            }
+
+            self.logger.debug("Channels: %s", channels_dict)
+
+            # In case the user has removed a channel that is already tracked
+            for channel in configured_channels:
+                if channel not in channel_ids:
+                    channels_dict[channel] = channel
+
+            return self.async_show_form(
+                step_id="channels",
+                data_schema=vol.Schema(
+                    {
+                        vol.Required(
+                            CONF_CHANNELS,
+                            default=configured_channels,
+                        ): cv.multi_select(channels_dict),
+                    }
+                ),
+            )
+
+        return self.async_create_entry(title="", data=user_input)
