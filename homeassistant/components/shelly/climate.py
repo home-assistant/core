0a371a5e8e629a2a910f7f58a02af91f9e35e7c8
Maciej Bieniek <bieniu@users.noreply.github.com>
2023-10-02 09:27:42 +0200

    Use _attr_name class attribute in the climate platform

--- homeassistant/components/shelly/climate.py
+++ homeassistant/components/shelly/climate.py
@@ -148,6 +148,7 @@ class BlockSleepingClimate(
         self.last_state_attributes: Mapping[str, Any]
         self._preset_modes: list[str] = []
         self._last_target_temp = SHTRV_01_TEMPERATURE_SETTINGS["default"]
+        self._attr_name = coordinator.name
 
         if self.block is not None and self.device_block is not None:
             self._unique_id = f"{self.coordinator.mac}-{self.block.description}"
@@ -175,11 +176,6 @@ class BlockSleepingClimate(
     def unique_id(self) -> str:
         """Set unique id of entity."""
         return self._unique_id
-
-    @property
-    def name(self) -> str:
-        """Name of entity."""
-        return self.coordinator.name
 
     @property
     def target_temperature(self) -> float | None:
@@ -350,7 +346,7 @@ class BlockSleepingClimate(
                 severity=ir.IssueSeverity.ERROR,
                 translation_key="device_not_calibrated",
                 translation_placeholders={
-                    "device_name": self.name,
+                    "device_name": self.coordinator.name,
                     "ip_address": self.coordinator.device.ip_address,
                 },
             )
