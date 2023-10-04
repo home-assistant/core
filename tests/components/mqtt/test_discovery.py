5603a6726097cc7b34fd25bf1f4df18230e343ca
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-10-04 19:36:34 +0200

    Raise vol.Invalid for invalid mqtt device_tracker config (#101399)
    Raise vol.Invalid for invalid mqtt device_tracker

--- tests/components/mqtt/test_discovery.py
+++ tests/components/mqtt/test_discovery.py
@@ -122,6 +122,28 @@ async def test_invalid_json(
         assert not mock_dispatcher_send.called
 
 
+@pytest.mark.no_fail_on_log_exception
+@patch("homeassistant.components.mqtt.PLATFORMS", [Platform.BINARY_SENSOR])
+async def test_discovery_schema_error(
+    hass: HomeAssistant,
+    mqtt_mock_entry: MqttMockHAClientGenerator,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    """Test unexpected error JSON config."""
+    with patch(
+        "homeassistant.components.mqtt.binary_sensor.DISCOVERY_SCHEMA",
+        side_effect=AttributeError("Attribute abc not found"),
+    ):
+        await mqtt_mock_entry()
+        async_fire_mqtt_message(
+            hass,
+            "homeassistant/binary_sensor/bla/config",
+            '{"name": "Beer", "state_topic": "ok"}',
+        )
+        await hass.async_block_till_done()
+        assert "AttributeError: Attribute abc not found" in caplog.text
+
+
 async def test_only_valid_components(
     hass: HomeAssistant,
     mqtt_mock_entry: MqttMockHAClientGenerator,
