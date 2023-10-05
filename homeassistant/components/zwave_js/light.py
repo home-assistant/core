1f45d813e94b64ccf1e3bf9f7f770d4fd9eaddc0
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-10-05 10:04:50 -0400

    Use subclass

--- homeassistant/components/zwave_js/light.py
+++ homeassistant/components/zwave_js/light.py
@@ -78,7 +78,7 @@ async def async_setup_entry(
         if info.platform_hint == "black_is_off":
             async_add_entities([ZwaveBlackIsOffLight(config_entry, driver, info)])
         elif info.platform_hint == "Basic":
-            async_add_entities([ZwaveBasicLight(config_entry, driver, info)])
+            async_add_entities([ZwaveBasicCCLight(config_entry, driver, info)])
         else:
             async_add_entities([ZwaveLight(config_entry, driver, info)])
 
@@ -358,7 +358,8 @@ class ZwaveLight(ZWaveBaseEntity, LightEntity):
         # typically delayed and causes a confusing UX.
         if (
             zwave_brightness == SET_TO_PREVIOUS_VALUE
-            and self.info.primary_value.command_class == CommandClass.SWITCH_MULTILEVEL
+            and self.info.primary_value.command_class
+            in (CommandClass.BASIC, CommandClass.SWITCH_MULTILEVEL)
         ):
             self._set_optimistic_state = True
             self.async_write_ha_state()
@@ -453,7 +454,7 @@ class ZwaveLight(ZWaveBaseEntity, LightEntity):
             self._color_mode = ColorMode.RGBW
 
 
-class ZwaveBasicLight(ZWaveBaseEntity, LightEntity):
+class ZwaveBasicCCLight(ZwaveLight, LightEntity):
     """Representation of a Z-Wave light for the Basic CC."""
 
     def __init__(
@@ -479,51 +480,6 @@ class ZwaveBasicLight(ZWaveBaseEntity, LightEntity):
         self._attr_name = self.generate_name(
             include_value_name=True, alternate_value_name=info.platform_hint
         )
-
-    @property
-    def brightness(self) -> int | None:
-        """Return the brightness of this light between 0..255.
-
-        Z-Wave multilevel switches use a range of [0, 99] to control brightness.
-        """
-        if self.info.primary_value.value is None:
-            return None
-        return round((cast(int, self.info.primary_value.value) / 99) * 255)
-
-    @property
-    def is_on(self) -> bool | None:
-        """Return true if device is on (brightness above 0)."""
-        if self._set_optimistic_state:
-            self._set_optimistic_state = False
-            return True
-        brightness = self.brightness
-        return brightness > 0 if brightness is not None else None
-
-    async def async_turn_on(self, **kwargs: Any) -> None:
-        """Turn the device on."""
-        # set brightness
-        await self._async_set_brightness(kwargs.get(ATTR_BRIGHTNESS))
-
-    async def async_turn_off(self, **kwargs: Any) -> None:
-        """Turn the light off."""
-        await self._async_set_brightness(0)
-
-    async def _async_set_brightness(self, brightness: int | None) -> None:
-        """Set new brightness to light."""
-        if brightness is None:
-            zwave_brightness = SET_TO_PREVIOUS_VALUE
-        else:
-            # Zwave multilevel switches use a range of [0, 99] to control brightness.
-            zwave_brightness = byte_to_zwave_brightness(brightness)
-
-        # setting a value requires setting targetValue
-        await self._async_set_value(self._target_brightness, zwave_brightness)
-        # We do an optimistic state update when setting to a previous value
-        # to avoid waiting for the value to be updated from the device which is
-        # typically delayed and causes a confusing UX.
-        if zwave_brightness == SET_TO_PREVIOUS_VALUE:
-            self._set_optimistic_state = True
-            self.async_write_ha_state()
 
 
 class ZwaveBlackIsOffLight(ZwaveLight):
