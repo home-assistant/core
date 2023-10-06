1cdead2ad97780cf97abdd3c44a8c07e96d7fe5f
Joost Lekkerkerker <joostlek@outlook.com>
2023-10-06 18:21:06 +0200

    Add Withings webhooks after a slight delay (#101542)

--- tests/components/withings/test_binary_sensor.py
+++ tests/components/withings/test_binary_sensor.py
@@ -2,13 +2,14 @@
 from unittest.mock import AsyncMock
 
 from aiohttp.client_exceptions import ClientResponseError
+from freezegun.api import FrozenDateTimeFactory
 import pytest
 from withings_api.common import NotifyAppli
 
 from homeassistant.const import STATE_OFF, STATE_ON, STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
 
-from . import call_webhook, setup_integration
+from . import call_webhook, prepare_webhook_setup, setup_integration
 from .conftest import USER_ID, WEBHOOK_ID
 
 from tests.common import MockConfigEntry
@@ -20,9 +21,11 @@ async def test_binary_sensor(
     withings: AsyncMock,
     webhook_config_entry: MockConfigEntry,
     hass_client_no_auth: ClientSessionGenerator,
+    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test binary sensor."""
     await setup_integration(hass, webhook_config_entry)
+    await prepare_webhook_setup(hass, freezer)
 
     client = await hass_client_no_auth()
 
