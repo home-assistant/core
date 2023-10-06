6fedd23e1222fb484d3e7653f2819c4036200439
joostlek <joostlek@outlook.com>
2023-10-06 16:45:38 +0200

    Automatically remove YouTube devices if they aren't fed with data

--- homeassistant/components/youtube/__init__.py
+++ homeassistant/components/youtube/__init__.py
@@ -7,6 +7,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
+from homeassistant.helpers import device_registry as dr, entity_registry as er
 from homeassistant.helpers.config_entry_oauth2_flow import (
     OAuth2Session,
     async_get_config_entry_implementation,
@@ -41,7 +42,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         COORDINATOR: coordinator,
         AUTH: auth,
     }
-    await hass.config_entries.async_forward_entry_setups(entry, list(PLATFORMS))
+    await async_cleanup(hass, entry)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
@@ -52,3 +54,22 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
+
+
+async def async_cleanup(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Clean up devices and entities."""
+
+    dev_reg = dr.async_get(hass)
+    devices = dr.async_entries_for_config_entry(dev_reg, entry.entry_id)
+    ent_reg = er.async_get(hass)
+    entities = er.async_entries_for_config_entry(ent_reg, entry.entry_id)
+    coordinator: YouTubeDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
+        COORDINATOR
+    ]
+    for device in devices:
+        channel_id = list(device.identifiers)[0][1].split("_", 1)[1]
+        if channel_id not in coordinator.data:
+            dev_reg.async_remove_device(device.id)
+            for entity in entities:
+                if entity.unique_id.startswith(f"{entry.entry_id}_{channel_id}"):
+                    ent_reg.async_remove(entity.entity_id)
