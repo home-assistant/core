aa1711b2fae9cecdb90eddf2fa2e5281d2995ef7
J. Nick Koston <nick@koston.org>
2023-10-07 10:46:23 -1000

    Always fetch orm rows for latest short term stats
    maybe fixes #101613

--- homeassistant/components/recorder/statistics.py
+++ homeassistant/components/recorder/statistics.py
@@ -1871,7 +1871,7 @@ def get_latest_short_term_statistics_by_ids(
     return list(
         cast(
             Sequence[Row],
-            execute_stmt_lambda_element(session, stmt, orm_rows=False),
+            execute_stmt_lambda_element(session, stmt),
         )
     )
 
@@ -1924,13 +1924,9 @@ def get_latest_short_term_statistics(
                 for metadata_id in missing_metadata_ids
                 if (
                     latest_id := cache_latest_short_term_statistic_id_for_metadata_id(
-                        # orm_rows=False is used here because we are in
-                        # a read-only session, and there will never be
-                        # any pending inserts in the session.
                         run_cache,
                         session,
                         metadata_id,
-                        orm_rows=False,
                     )
                 )
                 is not None
@@ -2316,14 +2312,8 @@ def _import_statistics_with_session(
     # We just inserted new short term statistics, so we need to update the
     # ShortTermStatisticsRunCache with the latest id for the metadata_id
     run_cache = get_short_term_statistics_run_cache(instance.hass)
-    #
-    # Because we are in the same session and we want to read rows
-    # that have not been flushed yet, we need to pass orm_rows=True
-    # to cache_latest_short_term_statistic_id_for_metadata_id
-    # to ensure that it gets the rows that were just inserted
-    #
     cache_latest_short_term_statistic_id_for_metadata_id(
-        run_cache, session, metadata_id, orm_rows=True
+        run_cache, session, metadata_id
     )
 
     return True
@@ -2341,7 +2331,6 @@ def cache_latest_short_term_statistic_id_for_metadata_id(
     run_cache: ShortTermStatisticsRunCache,
     session: Session,
     metadata_id: int,
-    orm_rows: bool,
 ) -> int | None:
     """Cache the latest short term statistic for a given metadata_id.
 
@@ -2352,13 +2341,7 @@ def cache_latest_short_term_statistic_id_for_metadata_id(
     if latest := cast(
         Sequence[Row],
         execute_stmt_lambda_element(
-            session,
-            _find_latest_short_term_statistic_for_metadata_id_stmt(metadata_id),
-            orm_rows=orm_rows
-            # _import_statistics_with_session needs to be able
-            # to read back the rows it just inserted without
-            # a flush so we have to pass orm_rows so we get
-            # back the latest data.
+            session, _find_latest_short_term_statistic_for_metadata_id_stmt(metadata_id)
         ),
     ):
         id_: int = latest[0].id
