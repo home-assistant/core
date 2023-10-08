1350117877306d843332cb8b3eec82fcc045950b
Oliv4945 <6248122+Oliv4945@users.noreply.github.com>
2023-10-08 21:38:44 +0000

    Fix platform unload

--- homeassistant/components/lorawan/__init__.py
+++ homeassistant/components/lorawan/__init__.py
@@ -21,7 +21,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     return unload_ok
