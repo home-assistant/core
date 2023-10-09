6b70847ac94ed750e6773e9cfc3207968fdbfd9c
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-10-10 01:35:03 +0200

    Modify Python Scripts

--- tests/components/python_script/conftest.py
+++ tests/components/python_script/conftest.py
@@ -1,3 +1,5 @@
-"""Skip test collection."""
+"""Skip test collection for Python 3.12."""
+import sys
 
-collect_ignore_glob = ["test_*.py"]
+if sys.version_info >= (3, 12):
+    collect_ignore_glob = ["test_*.py"]
