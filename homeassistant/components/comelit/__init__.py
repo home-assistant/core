e5d18a651cc3efeb2fe90519455ccfdd37dd49a8
Simone Chemelli <simone.chemelli@gmail.com>
2023-10-09 12:41:48 +0000

    add missing check during init

--- homeassistant/components/comelit/__init__.py
+++ homeassistant/components/comelit/__init__.py
@@ -5,7 +5,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_PIN, CONF_PORT, Platform
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
+from .const import DEFAULT_PORT, DOMAIN
 from .coordinator import ComelitSerialBridge
 
 PLATFORMS = [Platform.COVER, Platform.LIGHT]
@@ -14,7 +14,10 @@ PLATFORMS = [Platform.COVER, Platform.LIGHT]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Comelit platform."""
     coordinator = ComelitSerialBridge(
-        hass, entry.data[CONF_HOST], entry.data[CONF_PORT], entry.data[CONF_PIN]
+        hass,
+        entry.data[CONF_HOST],
+        entry.data.get(CONF_PORT, DEFAULT_PORT),
+        entry.data[CONF_PIN],
     )
 
     await coordinator.async_config_entry_first_refresh()
