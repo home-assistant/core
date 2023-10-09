5ba3bfd194144699226f7a59711e009fbf9d7134
J. Nick Koston <nick@koston.org>
2023-10-09 09:01:19 -1000

    tweak

--- homeassistant/components/homekit/type_thermostats.py
+++ homeassistant/components/homekit/type_thermostats.py
@@ -354,7 +354,7 @@ class Thermostat(HomeAccessory):
                 )
                 self.char_target_fan_state.display_name = "Fan Auto"
 
-        self._async_update_state(state)
+        self.async_update_state(state)
 
         serv_thermostat.setter_callback = self._set_chars
 
@@ -586,29 +586,6 @@ class Thermostat(HomeAccessory):
 
     @callback
     def async_update_state(self, new_state):
-        """Update thermostat state after state changed."""
-        # We always recheck valid hvac modes as the entity
-        # may not have been fully setup when we saw it last
-        original_hc_hass_to_homekit = self.hc_hass_to_homekit
-        self._configure_hvac_modes(new_state)
-
-        if self.hc_hass_to_homekit != original_hc_hass_to_homekit:
-            if self.char_target_heat_cool.value not in self.hc_homekit_to_hass:
-                # We must make sure the char value is
-                # in the new valid values before
-                # setting the new valid values or
-                # changing them with throw
-                self.char_target_heat_cool.set_value(
-                    list(self.hc_homekit_to_hass)[0], should_notify=False
-                )
-            self.char_target_heat_cool.override_properties(
-                valid_values=self.hc_hass_to_homekit
-            )
-
-        self._async_update_state(new_state)
-
-    @callback
-    def _async_update_state(self, new_state):
         """Update state without rechecking the device features."""
         attributes = new_state.attributes
         features = attributes.get(ATTR_SUPPORTED_FEATURES, 0)
