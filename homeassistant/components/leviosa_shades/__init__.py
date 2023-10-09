b4e3897bd4917dcd775d31428f75cd1d2be605f7
Greg1990 <greg.ha.1990@gmail.com>
2023-10-09 18:40:16 +0800

    Code optimization

--- homeassistant/components/leviosa_shades/__init__.py
+++ homeassistant/components/leviosa_shades/__init__.py
@@ -2,18 +2,16 @@
 import asyncio
 
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-PLATFORMS = ["cover"]
+PLATFORMS = [Platform.COVER]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Leviosa shades Zone from a config entry."""
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
