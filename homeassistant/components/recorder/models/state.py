a01f8eb5650e57098b810e58055782a7a934f6bb
linwahlg <70467935+linwahlg@users.noreply.github.com>
2023-10-10 12:30:37 +0000

    Change unused function parameter to _

--- homeassistant/components/recorder/models/state.py
+++ homeassistant/components/recorder/models/state.py
@@ -141,7 +141,7 @@ def row_to_compressed_state(
     row: Row,
     attr_cache: dict[str, dict[str, Any]],
     start_time_ts: float | None,
-    entity_id: str,
+    _: Any,
     state: str,
     last_updated_ts: float | None,
     no_attributes: bool,
