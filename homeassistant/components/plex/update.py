f4ca3c4ed166d47abdfff384f30d3519f10339ba
Jason Lawrence <jjlawren@users.noreply.github.com>
2023-10-09 21:31:04 -0500

    Simplify

--- homeassistant/components/plex/update.py
+++ homeassistant/components/plex/update.py
@@ -1,5 +1,4 @@
 """Representation of Plex updates."""
-import logging
 from typing import Any
 
 import plexapi.server
@@ -14,8 +13,6 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .const import CONF_SERVER_IDENTIFIER
 from .helpers import get_plex_server
 
-_LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -26,30 +23,7 @@ async def async_setup_entry(
     server_id = config_entry.data[CONF_SERVER_IDENTIFIER]
     server = get_plex_server(hass, server_id)
     plex_server = server.plex_server
-
-    def _fetch_data() -> tuple[bool, str, plexapi.server.Release | None]:
-        """Fetch all data from the Plex server in a single method."""
-        return (
-            plex_server.canInstallUpdate(),
-            plex_server.version,
-            plex_server.checkForUpdate(),
-        )
-
-    can_update, current_version, available_release = await hass.async_add_executor_job(
-        _fetch_data
-    )
-    _LOGGER.debug("Creating Plex updater")
-
-    available_version: str | None = None
-    release_notes: str | None = None
-    if available_release:
-        available_version = available_release.version
-        release_notes = available_release.fixed
-
-    plex_update = PlexUpdate(
-        plex_server, can_update, current_version, available_version, release_notes
-    )
-    async_add_entities([plex_update])
+    async_add_entities([PlexUpdate(plex_server)], update_before_add=True)
 
 
 class PlexUpdate(UpdateEntity):
@@ -60,39 +34,34 @@ class PlexUpdate(UpdateEntity):
         UpdateEntityFeature.INSTALL | UpdateEntityFeature.RELEASE_NOTES
     )
 
-    def __init__(
-        self,
-        plex_server: plexapi.server.PlexServer,
-        can_update: bool,
-        current_version: str,
-        available_version: str | None,
-        release_notes: str | None,
-    ) -> None:
+    def __init__(self, plex_server: plexapi.server.PlexServer) -> None:
         """Initialize the Update entity."""
         self.plex_server = plex_server
-        self.can_update = can_update
+        self.can_update: bool = False
         self._attr_name = f"Plex Media Server ({plex_server.friendlyName})"
-        self._attr_latest_version = available_version
-        self._attr_installed_version = current_version
         self._attr_unique_id = plex_server.machineIdentifier
-        self._release_notes = release_notes
+        self._release_notes: str | None = None
 
     def update(self) -> None:
         """Update sync attributes."""
         self._attr_installed_version = self.plex_server.version
         if release := self.plex_server.checkForUpdate():
+            self.can_update = self.plex_server.canInstallUpdate()
             self._attr_latest_version = release.version
-            self._release_notes = release.fixed
+            if release.fixed:
+                self._release_notes = "\n".join(
+                    f"* {line}" for line in release.fixed.split("\n")
+                )
+            else:
+                self._release_notes = None
 
     def release_notes(self) -> str | None:
         """Return release notes for the available upgrade."""
-        if not self._release_notes:
-            return None
-        return "\n".join(f"* {line}" for line in self._release_notes.split("\n"))
+        return self._release_notes
 
     def install(self, version: str | None, backup: bool, **kwargs: Any) -> None:
         """Install an update."""
-        if not self.plex_server.canInstallUpdate():
+        if not self.can_update:
             raise HomeAssistantError(
                 "Automatic updates cannot be performed on this Plex installation"
             )
