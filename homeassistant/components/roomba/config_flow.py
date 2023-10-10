88be1c6b3b1999d7ffd3365dc6cccd76e45da6ee
OscarSanner <o.sanner@hotmail.com>
2023-10-10 08:27:28 +0000

    format

--- homeassistant/components/roomba/config_flow.py
+++ homeassistant/components/roomba/config_flow.py
@@ -328,42 +328,6 @@ def _async_blid_from_hostname(hostname):
     return hostname.split("-")[1].split(".")[0].upper()
 
 
-async def _async_discover_roombas_1(hass, host):
-    discovered_hosts = set()
-    devices = []
-    discover_lock = hass.data.setdefault(ROOMBA_DISCOVERY_LOCK, asyncio.Lock())
-    discover_attempts = HOST_ATTEMPTS if host else ALL_ATTEMPTS
-
-    for attempt in range(discover_attempts + 1):
-        async with discover_lock:
-            discovery = _async_get_roomba_discovery()
-            try:
-                if host:
-                    device = await hass.async_add_executor_job(discovery.get, host)
-                    discovered = [device] if device else []
-                else:
-                    discovered = await hass.async_add_executor_job(discovery.get_all)
-            except OSError:
-                # Socket temporarily unavailable
-                await asyncio.sleep(ROOMBA_WAKE_TIME * attempt)
-                continue
-            else:
-                for device in discovered:
-                    if device.ip in discovered_hosts:
-                        continue
-                    discovered_hosts.add(device.ip)
-                    devices.append(device)
-            finally:
-                discovery.server_socket.close()
-
-        if host and host in discovered_hosts:
-            return devices
-
-        await asyncio.sleep(ROOMBA_WAKE_TIME)
-
-    return devices
-
-
 async def _async_discover_roombas(hass, host):
     discovered_hosts = set()
     devices = []
