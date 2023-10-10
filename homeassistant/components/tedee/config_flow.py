b04c748792743af123f92529f5fcbd6e7b730662
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-10-10 12:45:32 +0200

    add config flow tests

--- homeassistant/components/tedee/config_flow.py
+++ homeassistant/components/tedee/config_flow.py
@@ -3,79 +3,118 @@ from collections.abc import Mapping
 from typing import Any
 
 import voluptuous as vol
-from homeassistant import config_entries
+from homeassistant import config_entries, exceptions
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ACCESS_TOKEN, CONF_HOST
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 
-from .const import (DOMAIN, CONF_HOME_ASSISTANT_ACCESS_TOKEN, CONF_LOCAL_ACCESS_TOKEN,
-                    NAME, CONF_UNLOCK_PULLS_LATCH, CONF_USE_CLOUD)
+from pytedee_async import TedeeClient, TedeeAuthException, TedeeLocalAuthException
+
+from .const import (
+    DOMAIN,
+    CONF_HOME_ASSISTANT_ACCESS_TOKEN,
+    CONF_LOCAL_ACCESS_TOKEN,
+    NAME,
+    CONF_UNLOCK_PULLS_LATCH,
+    CONF_USE_CLOUD,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
+
+async def validate_input(user_input: dict[str, Any] = None) -> bool:
+    pak = user_input.get(CONF_ACCESS_TOKEN, "")
+    host = user_input.get(CONF_HOST, "")
+    local_access_token = user_input.get(CONF_LOCAL_ACCESS_TOKEN, "")
+    tedee_client = TedeeClient(pak, local_access_token, host)
+    try:
+        await tedee_client.get_locks()
+    except (TedeeAuthException, TedeeLocalAuthException) as ex:
+        raise InvalidAuth from ex
+    except Exception as ex:
+        raise CannotConnect from ex
+    return True
+
+
 class TedeeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
-    
+
     def __init__(self):
         self._errors: dict = {}
         self._reload: dict = False
         self._previous_step_data: dict = {}
         self._config: dict = {}
 
-    async def async_step_user(self, user_input: dict[str, Any]=None) -> FlowResult:
+    async def async_step_user(self, user_input: dict[str, Any] = None) -> FlowResult:
         errors: dict = {}
-        
-        if user_input is not None:
-            if (user_input.get(CONF_HOST) is None and user_input.get(CONF_LOCAL_ACCESS_TOKEN) is not None) \
-                or (user_input.get(CONF_HOST) is not None and user_input.get(CONF_LOCAL_ACCESS_TOKEN) is None):
-                errors["base"] = "invalid_local_config"
-            elif not user_input.get(CONF_USE_CLOUD, False) and user_input.get(CONF_LOCAL_ACCESS_TOKEN) is None and user_input.get(CONF_ACCESS_TOKEN) is None:
-                errors["base"] = "invalid_config"
-            if not errors:
 
+        if user_input is not None:
+            if (
+                user_input.get(CONF_HOST) is None
+                and user_input.get(CONF_LOCAL_ACCESS_TOKEN) is not None
+            ):
+                errors[CONF_HOST] = "invalid_host"
+            elif (
+                user_input.get(CONF_HOST) is not None
+                and user_input.get(CONF_LOCAL_ACCESS_TOKEN) is None
+            ):
+                errors[CONF_LOCAL_ACCESS_TOKEN] = "invalid_api_key"
+            elif (
+                user_input.get(CONF_HOST) is not None
+                and user_input.get(CONF_LOCAL_ACCESS_TOKEN) is not None
+            ):
+                try:
+                    await validate_input(user_input)
+                except InvalidAuth:
+                    errors[CONF_LOCAL_ACCESS_TOKEN] = "invalid_api_key"
+                except CannotConnect:
+                    errors[CONF_HOST] = "invalid_host"
+
+            if not errors:
                 if user_input.get(CONF_USE_CLOUD, False):
                     self._previous_step_data = user_input
                     return await self.async_step_configure_cloud()
-                
-                return self.async_create_entry(
-                        title=NAME, 
-                        data=user_input
-                    )
-        
+
+                return self.async_create_entry(title=NAME, data=user_input)
+
         return self.async_show_form(
-            step_id="user", 
+            step_id="user",
             data_schema=vol.Schema(
                 {
                     vol.Optional(CONF_HOST): str,
                     vol.Optional(CONF_LOCAL_ACCESS_TOKEN): str,
                     vol.Optional(CONF_HOME_ASSISTANT_ACCESS_TOKEN): str,
-                    vol.Optional(CONF_USE_CLOUD, False): bool
+                    vol.Optional(CONF_USE_CLOUD, False): bool,
                 }
             ),
-            errors=errors
+            errors=errors,
         )
-    
-    async def async_step_configure_cloud(self, user_input: dict[str, Any]=None) -> FlowResult:
+
+    async def async_step_configure_cloud(
+        self, user_input: dict[str, Any] = None
+    ) -> FlowResult:
         errors = {}
         if user_input is not None:
-            return self.async_create_entry(
-                title=NAME, 
-                data=user_input | self._previous_step_data
-            )
+            try:
+                await validate_input(user_input)
+            except InvalidAuth:
+                errors[CONF_ACCESS_TOKEN] = "invalid_api_key"
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+
+            if not errors:
+                return self.async_create_entry(
+                    title=NAME, data=user_input | self._previous_step_data
+                )
         else:
             return self.async_show_form(
-                step_id="configure_cloud", 
-                data_schema=vol.Schema(
-                    {
-                        vol.Required(CONF_ACCESS_TOKEN): str
-                    }
-                ),
-                errors=errors
+                step_id="configure_cloud",
+                data_schema=vol.Schema({vol.Required(CONF_ACCESS_TOKEN): str}),
+                errors=errors,
             )
-    
+
     @staticmethod
     @callback
     def async_get_options_flow(
@@ -83,42 +122,91 @@ class TedeeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> config_entries.OptionsFlow:
         """Create the options flow."""
         return OptionsFlowHandler(config_entry)
-    
-    
+
     async def async_step_reauth(self, entry_data: Mapping[str, Any]) -> FlowResult:
         """Perform reauth upon an API authentication error."""
         self._config = dict(entry_data)
         return await self.async_step_reauth_confirm()
 
-    async def async_step_reauth_confirm(self, user_input: dict[str, Any]=None) -> FlowResult:
+    async def async_step_reauth_confirm(
+        self, user_input: dict[str, Any] = None
+    ) -> FlowResult:
         """Dialog that informs the user that reauth is required."""
-        if user_input is None:
+        errors = {}
+
+        if user_input is not None:
+            try:
+                await validate_input(self._config | user_input)
+            except InvalidAuth:
+                if self._config.get(CONF_ACCESS_TOKEN):
+                    errors[CONF_ACCESS_TOKEN] = "invalid_api_key"
+                if self._config.get(CONF_LOCAL_ACCESS_TOKEN):
+                    errors[CONF_LOCAL_ACCESS_TOKEN] = "invalid_api_key"
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+
+            if not errors:
+                entry = self.hass.config_entries.async_get_entry(
+                    self.context["entry_id"]
+                )
+                self.hass.config_entries.async_update_entry(
+                    entry, data=self._config | user_input
+                )
+                await self.hass.config_entries.async_reload(self.context["entry_id"])
+                return self.async_abort(reason="reauth_successful")
+
+        if self._config.get(CONF_ACCESS_TOKEN) and self._config.get(
+            CONF_LOCAL_ACCESS_TOKEN
+        ):
             return self.async_show_form(
                 step_id="reauth_confirm",
                 data_schema=vol.Schema(
                     {
-                        vol.Optional(CONF_ACCESS_TOKEN, default=self._config.get(CONF_ACCESS_TOKEN, "")): str,
-                        vol.Optional(CONF_LOCAL_ACCESS_TOKEN, default=self._config.get(CONF_LOCAL_ACCESS_TOKEN, "")): str,
+                        vol.Required(
+                            CONF_ACCESS_TOKEN,
+                            default=self._config.get(CONF_ACCESS_TOKEN),
+                        ): str,
+                        vol.Required(
+                            CONF_LOCAL_ACCESS_TOKEN,
+                            default=self._config.get(CONF_LOCAL_ACCESS_TOKEN),
+                        ): str,
                     }
                 ),
+                errors=errors,
             )
-        entry = self.hass.config_entries.async_get_entry(
-            self.context["entry_id"]
-        )
-        self.hass.config_entries.async_update_entry(
-            entry, 
-            data=self._config | user_input
-        )   
-        await self.hass.config_entries.async_reload(self.context["entry_id"])
-        return self.async_abort(reason="reauth_successful")
-    
-    
+        elif self._config.get(CONF_ACCESS_TOKEN):
+            return self.async_show_form(
+                step_id="reauth_confirm",
+                data_schema=vol.Schema(
+                    {
+                        vol.Required(
+                            CONF_ACCESS_TOKEN,
+                            default=self._config.get(CONF_ACCESS_TOKEN),
+                        ): str
+                    }
+                ),
+                errors=errors,
+            )
+        elif self._config.get(CONF_LOCAL_ACCESS_TOKEN):
+            return self.async_show_form(
+                step_id="reauth_confirm",
+                data_schema=vol.Schema(
+                    {
+                        vol.Required(
+                            CONF_LOCAL_ACCESS_TOKEN,
+                            default=self._config.get(CONF_LOCAL_ACCESS_TOKEN),
+                        ): str
+                    }
+                ),
+                errors=errors,
+            )
+
+
 class OptionsFlowHandler(config_entries.OptionsFlow):
     """Handles options flow for the component."""
 
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         self.config_entry = config_entry
-
 
     async def async_step_init(
         self, user_input: dict[str, Any] = None
@@ -127,26 +215,84 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
         errors: dict[str, str] = {}
 
         if user_input is not None:
+            if user_input.get(CONF_HOST) and not user_input.get(
+                CONF_LOCAL_ACCESS_TOKEN
+            ):
+                errors[CONF_LOCAL_ACCESS_TOKEN] = "invalid_api_key"
+            elif not user_input.get(CONF_HOST) and user_input.get(
+                CONF_LOCAL_ACCESS_TOKEN
+            ):
+                errors[CONF_HOST] = "invalid_host"
+            elif user_input.get(CONF_HOST) and user_input.get(CONF_LOCAL_ACCESS_TOKEN):
+                try:
+                    await validate_input(
+                        {
+                            CONF_HOST: user_input.get(CONF_HOST),
+                            CONF_LOCAL_ACCESS_TOKEN: user_input.get(
+                                CONF_LOCAL_ACCESS_TOKEN
+                            ),
+                        }
+                    )
+                except InvalidAuth:
+                    errors[CONF_LOCAL_ACCESS_TOKEN] = "invalid_api_key"
+                except CannotConnect:
+                    errors[CONF_HOST] = "invalid_host"
+
+            if user_input.get(CONF_ACCESS_TOKEN):
+                try:
+                    await validate_input(
+                        {CONF_ACCESS_TOKEN: user_input.get(CONF_ACCESS_TOKEN)}
+                    )
+                except InvalidAuth:
+                    errors[CONF_ACCESS_TOKEN] = "invalid_api_key"
+                except CannotConnect:
+                    errors["base"] = "cannot_connect"
+
             if not errors:
                 # write entry to config and not options dict, pass empty options out
                 self.hass.config_entries.async_update_entry(
-                    self.config_entry, data=user_input, options=self.config_entry.options
-                )   
-                return self.async_create_entry(
-                    title="",
-                    data=user_input
+                    self.config_entry,
+                    data=user_input,
+                    options=self.config_entry.options,
                 )
+                return self.async_create_entry(title="", data=user_input)
 
         options_schema = vol.Schema(
             {
-                vol.Optional(CONF_ACCESS_TOKEN, default=self.config_entry.data.get(CONF_ACCESS_TOKEN, "")): str,
-                vol.Optional(CONF_UNLOCK_PULLS_LATCH, default=self.config_entry.options.get(CONF_UNLOCK_PULLS_LATCH, False)): bool,
-                vol.Optional(CONF_HOST, default=self.config_entry.data.get(CONF_HOST, "")): str,
-                vol.Optional(CONF_LOCAL_ACCESS_TOKEN, default=self.config_entry.data.get(CONF_LOCAL_ACCESS_TOKEN, "")): str,
-                vol.Optional(CONF_HOME_ASSISTANT_ACCESS_TOKEN, default=self.config_entry.data.get(CONF_HOME_ASSISTANT_ACCESS_TOKEN, "")): str,
+                vol.Optional(
+                    CONF_ACCESS_TOKEN,
+                    default=self.config_entry.data.get(CONF_ACCESS_TOKEN, ""),
+                ): str,
+                vol.Optional(
+                    CONF_UNLOCK_PULLS_LATCH,
+                    default=self.config_entry.options.get(
+                        CONF_UNLOCK_PULLS_LATCH, False
+                    ),
+                ): bool,
+                vol.Optional(
+                    CONF_HOST, default=self.config_entry.data.get(CONF_HOST, "")
+                ): str,
+                vol.Optional(
+                    CONF_LOCAL_ACCESS_TOKEN,
+                    default=self.config_entry.data.get(CONF_LOCAL_ACCESS_TOKEN, ""),
+                ): str,
+                vol.Optional(
+                    CONF_HOME_ASSISTANT_ACCESS_TOKEN,
+                    default=self.config_entry.data.get(
+                        CONF_HOME_ASSISTANT_ACCESS_TOKEN, ""
+                    ),
+                ): str,
             }
         )
 
         return self.async_show_form(
             step_id="init", data_schema=options_schema, errors=errors
         )
+
+
+class CannotConnect(exceptions.HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(exceptions.HomeAssistantError):
+    """Error to indicate there is invalid auth."""
--- tests/components/tedee/test_config_flow.py
+++ tests/components/tedee/test_config_flow.py
@@ -1,12 +1,11 @@
 from unittest.mock import patch
 
 from homeassistant.components.tedee.const import (
-    DOMAIN,
     CONF_HOME_ASSISTANT_ACCESS_TOKEN,
     CONF_LOCAL_ACCESS_TOKEN,
-    NAME,
     CONF_UNLOCK_PULLS_LATCH,
     CONF_USE_CLOUD,
+    DOMAIN,
 )
 
 from homeassistant import config_entries
@@ -16,6 +15,8 @@ from homeassistant.const import CONF_ACCESS_TOKEN, CONF_HOST
 from homeassistant.data_entry_flow import FlowResultType
 
 from tests.common import MockConfigEntry
+
+from pytedee_async import TedeeAuthException, TedeeLocalAuthException
 
 FLOW_UNIQUE_ID = "112233445566778899"
 ACCESS_TOKEN = "api_token"
@@ -32,21 +33,6 @@ async def test_show_config_form(hass: HomeAssistant) -> None:
     assert result["step_id"] == "user"
 
 
-async def test_flow(hass: HomeAssistant) -> None:
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    assert result["type"] == FlowResultType.FORM
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"], user_input={CONF_USE_CLOUD: True}
-    )
-
-    assert result["type"] == FlowResultType.FORM
-    assert result["step_id"] == "configure_cloud"
-
-
 async def local_api_configure_error(hass: HomeAssistant) -> None:
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -59,14 +45,79 @@ async def local_api_configure_error(hass: HomeAssistant) -> None:
     )
 
     assert result["type"] == FlowResultType.FORM
-    assert result["errors"] == {"base": "invalid_local_config"}
+    assert result["errors"] == {CONF_HOST: "invalid_host"}
 
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"], user_input={CONF_HOST: "192.168.1.42"}
     )
 
     assert result["type"] == FlowResultType.FORM
-    assert result["errors"] == {"base": "invalid_local_config"}
+    assert result["errors"] == {CONF_LOCAL_ACCESS_TOKEN: "invalid_api_key"}
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=TedeeLocalAuthException("Invalid token"),
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_HOST: "192.168.1.42",
+                CONF_LOCAL_ACCESS_TOKEN: "wrong_token",
+            },
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {CONF_LOCAL_ACCESS_TOKEN: "invalid_api_key"}
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=Exception(),
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_HOST: "192.168.1.x",
+                CONF_LOCAL_ACCESS_TOKEN: "token",
+            },
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {CONF_HOST: "invalid_host"}
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={CONF_USE_CLOUD: True}
+    )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["step_id"] == "configure_cloud"
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=TedeeAuthException("invalid_api_key"),
+    ):
+        result3 = await hass.config_entries.flow.async_configure(
+            result2["flow_id"],
+            {
+                CONF_ACCESS_TOKEN: "invalid_token",
+            },
+        )
+
+    assert result3["type"] == FlowResultType.FORM
+    assert result3["errors"] == {CONF_ACCESS_TOKEN: "invalid_api_key"}
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=Exception(),
+    ):
+        result3 = await hass.config_entries.flow.async_configure(
+            result2["flow_id"],
+            {
+                CONF_ACCESS_TOKEN: "token",
+            },
+        )
+
+    assert result3["type"] == FlowResultType.FORM
+    assert result3["errors"] == {"base": "cannot_connect"}
 
 
 async def test_show_reauth(hass: HomeAssistant) -> None:
@@ -126,6 +177,10 @@ async def test_reauth_flow(hass: HomeAssistant) -> None:
                 CONF_LOCAL_ACCESS_TOKEN: LOCAL_ACCESS_TOKEN,
             },
         )
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        return_value=True,
+    ):
         result = await hass.config_entries.flow.async_configure(
             reauth_result["flow_id"],
             {
@@ -136,3 +191,174 @@ async def test_reauth_flow(hass: HomeAssistant) -> None:
         assert result["type"] == FlowResultType.ABORT
         await hass.async_block_till_done()
         assert result["reason"] == "reauth_successful"
+
+
+async def test_reauth_flow_errors(hass: HomeAssistant) -> None:
+    """Test that the reauth flow works."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            CONF_ACCESS_TOKEN: ACCESS_TOKEN,
+            CONF_LOCAL_ACCESS_TOKEN: LOCAL_ACCESS_TOKEN,
+        },
+        unique_id=FLOW_UNIQUE_ID,
+    )
+    entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={
+            "source": SOURCE_REAUTH,
+            "unique_id": entry.unique_id,
+            "entry_id": entry.entry_id,
+        },
+        data={
+            CONF_ACCESS_TOKEN: ACCESS_TOKEN,
+            CONF_LOCAL_ACCESS_TOKEN: LOCAL_ACCESS_TOKEN,
+        },
+    )
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=TedeeLocalAuthException("Invalid Auth"),
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_ACCESS_TOKEN: "wrong_token",
+                CONF_LOCAL_ACCESS_TOKEN: "wrong_token",
+            },
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {
+        CONF_ACCESS_TOKEN: "invalid_api_key",
+        CONF_LOCAL_ACCESS_TOKEN: "invalid_api_key",
+    }
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=Exception(),
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_ACCESS_TOKEN: "token",
+                CONF_LOCAL_ACCESS_TOKEN: "token",
+            },
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_options_flow(hass: HomeAssistant) -> None:
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={},
+        unique_id=FLOW_UNIQUE_ID,
+    )
+
+    entry.add_to_hass(hass)
+
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "init"
+
+    result2 = await hass.config_entries.options.async_configure(
+        result["flow_id"],
+        user_input={CONF_LOCAL_ACCESS_TOKEN: "token"},
+    )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {CONF_HOST: "invalid_host"}
+
+    result2 = await hass.config_entries.options.async_configure(
+        result["flow_id"],
+        user_input={CONF_HOST: "192.168.1.42"},
+    )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {CONF_LOCAL_ACCESS_TOKEN: "invalid_api_key"}
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=TedeeLocalAuthException("Invalid Auth"),
+    ):
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_HOST: "192.168.1.42",
+                CONF_LOCAL_ACCESS_TOKEN: "wrong_token",
+            },
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {CONF_LOCAL_ACCESS_TOKEN: "invalid_api_key"}
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=Exception(),
+    ):
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_HOST: "192.168.1.x",
+                CONF_LOCAL_ACCESS_TOKEN: "wrong_token",
+            },
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {CONF_HOST: "invalid_host"}
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=TedeeAuthException("invalid auth"),
+    ):
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={CONF_ACCESS_TOKEN: "wrong_token"},
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {CONF_ACCESS_TOKEN: "invalid_api_key"}
+
+    with patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        side_effect=Exception(),
+    ):
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={CONF_ACCESS_TOKEN: "token"},
+        )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {"base": "cannot_connect"}
+
+    with patch(
+        "homeassistant.components.tedee.async_setup_entry",
+        return_value=True,
+    ), patch(
+        "homeassistant.components.tedee.config_flow.TedeeClient.get_locks",
+        return_value=True,
+    ):
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_HOST: "192.168.1.42",
+                CONF_LOCAL_ACCESS_TOKEN: "token1",
+                CONF_ACCESS_TOKEN: "token2",
+                CONF_HOME_ASSISTANT_ACCESS_TOKEN: "token3",
+                CONF_UNLOCK_PULLS_LATCH: False,
+            },
+        )
+
+    assert result2["type"] == FlowResultType.CREATE_ENTRY
+    assert result2["data"] == {
+        CONF_HOST: "192.168.1.42",
+        CONF_LOCAL_ACCESS_TOKEN: "token1",
+        CONF_ACCESS_TOKEN: "token2",
+        CONF_HOME_ASSISTANT_ACCESS_TOKEN: "token3",
+        CONF_UNLOCK_PULLS_LATCH: False,
+    }
