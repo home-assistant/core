4aaba39529264fbd6c907102a99449c4b1c8773b
Zuzana <zuzana.roh@gmail.com>
2023-10-11 17:15:40 +0000

    Extracted code into function

--- homeassistant/components/spotify/browse_media.py
+++ homeassistant/components/spotify/browse_media.py
@@ -250,6 +250,63 @@ def build_item_response(  # noqa: C901
     if media_content_type is None or media_content_id is None:
         return None
 
+    title, image, media, items = _build_item_browse_media(
+        media_content_type, spotify, user, media_content_id
+    )
+
+    if media is None:
+        return None
+
+    try:
+        media_class = CONTENT_TYPE_MEDIA_CLASS[media_content_type]
+    except KeyError:
+        _LOGGER.debug("Unknown media type received: %s", media_content_type)
+        return None
+
+    if media_content_type == BrowsableMedia.CATEGORIES:
+        media_item = BrowseMedia(
+            can_expand=True,
+            can_play=False,
+            children_media_class=media_class["children"],
+            media_class=media_class["parent"],
+            media_content_id=media_content_id,
+            media_content_type=f"{MEDIA_PLAYER_PREFIX}{media_content_type}",
+            title=LIBRARY_MAP.get(media_content_id, "Unknown"),
+        )
+
+        media_item.children = _make_media_children(items)
+        return media_item
+
+    title = _check_title(title, media, media_content_id)
+
+    can_play = _can_play_browse_media(media_content_type, can_play_artist)
+
+    browse_media = BrowseMedia(
+        can_expand=True,
+        can_play=can_play,
+        children_media_class=media_class["children"],
+        media_class=media_class["parent"],
+        media_content_id=media_content_id,
+        media_content_type=f"{MEDIA_PLAYER_PREFIX}{media_content_type}",
+        thumbnail=image,
+        title=title,
+    )
+
+    browse_media.children = _make_browse_media_children(items, can_play_artist)
+
+    if "images" in media:
+        browse_media.thumbnail = fetch_image_url(media)
+
+    return browse_media
+
+
+def _build_item_browse_media(  # noqa: C901
+    media_content_type: str,
+    spotify: Spotify,
+    user: dict[str, Any],
+    media_content_id: str,
+):
+    """Create media."""
     title = None
     image = None
     media: dict[str, Any] | None = None
@@ -321,81 +378,58 @@ def build_item_response(  # noqa: C901
             title = show.get("name")
             image = fetch_image_url(show)
             items = media.get("items", [])
+    return title, image, media, items
 
-    if media is None:
-        return None
 
-    try:
-        media_class = CONTENT_TYPE_MEDIA_CLASS[media_content_type]
-    except KeyError:
-        _LOGGER.debug("Unknown media type received: %s", media_content_type)
-        return None
-
-    if media_content_type == BrowsableMedia.CATEGORIES:
-        media_item = BrowseMedia(
-            can_expand=True,
-            can_play=False,
-            children_media_class=media_class["children"],
-            media_class=media_class["parent"],
-            media_content_id=media_content_id,
-            media_content_type=f"{MEDIA_PLAYER_PREFIX}{media_content_type}",
-            title=LIBRARY_MAP.get(media_content_id, "Unknown"),
-        )
-
-        media_item.children = []
-        for item in items:
-            try:
-                item_id = item["id"]
-            except KeyError:
-                _LOGGER.debug("Missing ID for media item: %s", item)
-                continue
-            media_item.children.append(
-                BrowseMedia(
-                    can_expand=True,
-                    can_play=False,
-                    children_media_class=MediaClass.TRACK,
-                    media_class=MediaClass.PLAYLIST,
-                    media_content_id=item_id,
-                    media_content_type=f"{MEDIA_PLAYER_PREFIX}category_playlists",
-                    thumbnail=fetch_image_url(item, key="icons"),
-                    title=item.get("name"),
-                )
+def _make_media_children(items):
+    """Create children for media item."""
+    children = []
+    for item in items:
+        try:
+            item_id = item["id"]
+        except KeyError:
+            _LOGGER.debug("Missing ID for media item: %s", item)
+            continue
+        children.append(
+            BrowseMedia(
+                can_expand=True,
+                can_play=False,
+                children_media_class=MediaClass.TRACK,
+                media_class=MediaClass.PLAYLIST,
+                media_content_id=item_id,
+                media_content_type=f"{MEDIA_PLAYER_PREFIX}category_playlists",
+                thumbnail=fetch_image_url(item, key="icons"),
+                title=item.get("name"),
             )
-        return media_item
+        )
+    return children
 
+
+def _check_title(title: str, media, media_content_id: str):
+    """Create title."""
     if title is None:
         title = LIBRARY_MAP.get(media_content_id, "Unknown")
         if "name" in media:
             title = media["name"]
+    return title
 
-    can_play = media_content_type in PLAYABLE_MEDIA_TYPES and (
+
+def _can_play_browse_media(media_content_type: str, can_play_artist: bool):
+    """Set whether the item can be played."""
+    return media_content_type in PLAYABLE_MEDIA_TYPES and (
         media_content_type != MediaType.ARTIST or can_play_artist
     )
 
-    browse_media = BrowseMedia(
-        can_expand=True,
-        can_play=can_play,
-        children_media_class=media_class["children"],
-        media_class=media_class["parent"],
-        media_content_id=media_content_id,
-        media_content_type=f"{MEDIA_PLAYER_PREFIX}{media_content_type}",
-        thumbnail=image,
-        title=title,
-    )
 
-    browse_media.children = []
+def _make_browse_media_children(items, can_play_artist: bool):
+    """Create browse media children."""
+    children = []
     for item in items:
         try:
-            browse_media.children.append(
-                item_payload(item, can_play_artist=can_play_artist)
-            )
+            children.append(item_payload(item, can_play_artist=can_play_artist))
         except (MissingMediaInformation, UnknownMediaType):
             continue
-
-    if "images" in media:
-        browse_media.thumbnail = fetch_image_url(media)
-
-    return browse_media
+    return children
 
 
 def item_payload(item: dict[str, Any], *, can_play_artist: bool) -> BrowseMedia:
