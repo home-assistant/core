0df28fc3aac5cc875db3536620131be881057f1d
Federico D'Amico <48856240+FedDam@users.noreply.github.com>
2023-10-12 13:30:51 +0200

    Update servicesMicrobees.py

--- homeassistant/components/microbees/servicesMicrobees.py
+++ homeassistant/components/microbees/servicesMicrobees.py
@@ -1,35 +1,29 @@
-
 from homeassistant.helpers import aiohttp_client
 import logging
 import json
-from .const import SENDCOMMAND_URL,GETMYBEES_URL
+from homeassistant.core import HomeAssistant
+from .const import SENDCOMMAND_URL, GETMYBEES_URL
+
 _LOGGER = logging.getLogger(__name__)
 
 
 async def sendCommand(self, data):
-    _LOGGER.info("sendCommand: %s" % data)
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer %s" % self.token,
     }
     async with aiohttp_client.async_create_clientsession(self.hass) as session:
-        async with session.post(
-            SENDCOMMAND_URL,
-            json=data,
-            headers=headers
-        ) as resp:
+        async with session.post(SENDCOMMAND_URL, json=data, headers=headers) as resp:
             await resp.text()
 
 
-async def getBees(hass,token):
+async def getBees(hass: HomeAssistant, token):
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer %s" % token,
     }
     async with aiohttp_client.async_create_clientsession(hass) as session:
-        async with session.post(
-            GETMYBEES_URL, headers=headers
-        ) as resp:
+        async with session.post(GETMYBEES_URL, headers=headers) as resp:
             data = await resp.text()
             js = json.loads(data)
             return js.get("data")
