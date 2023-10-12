d1bac1c99b58df41b3c9ab2c5cc1228980a52f05
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2023-10-12 12:55:45 +0200

    fix type issue

--- homeassistant/components/vicare/water_heater.py
+++ homeassistant/components/vicare/water_heater.py
@@ -121,7 +121,7 @@ class ViCareWater(ViCareEntity, WaterHeaterEntity):
         self._attr_name = name
         self._api = api
         self._circuit = circuit
-        self._attributes = {}
+        self._attributes: dict[str, Any] = {}
         self._current_mode = None
         self._attr_unique_id = f"{device_config.getConfig().serial}-{circuit.id}"
         ViCareEntity.__init__(self, device_config, has_multiple_devices)
