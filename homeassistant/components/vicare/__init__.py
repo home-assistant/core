205c6669bdacd1875d394069ce025f6af2376fc4
Christopher Fenner <Christopher.Fenner@me.com>
2023-10-12 13:27:43 +0200

    add device info to base class

--- homeassistant/components/vicare/__init__.py
+++ homeassistant/components/vicare/__init__.py
@@ -13,6 +13,8 @@ from PyViCare.PyViCareDevice import Device
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_CLIENT_ID, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.storage import STORAGE_DIR
 
 from .const import (
@@ -107,7 +109,13 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 class ViCareEntity(Entity):
     """Base class for ViCare entities."""
 
-    _attr_has_entity_name = True
-
-    def __init__(self) -> None:
+    def __init__(self, device_config) -> None:
         """Initialize the entity."""
+
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, device_config.getConfig().serial)},
+            name=device_config.getModel(),
+            manufacturer="Viessmann",
+            model=device_config.getModel(),
+            configuration_url="https://developer.viessmann.com/",
+        )
