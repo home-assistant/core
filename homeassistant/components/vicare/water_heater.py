5c52a15df7b057e00630dc04ce170410099cfdca
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2023-10-12 15:01:23 +0200

    Fix type issue in vicare integration (#101872)
    * fix type issue
    
    * fix type issue
    
    * fix type issue
    
    * extract from constructor
    
    * Revert "extract from constructor"
    
    This reverts commit a33de7ca8e75845b81c548c73745b15a98d2bd63.

--- homeassistant/components/vicare/water_heater.py
+++ homeassistant/components/vicare/water_heater.py
@@ -106,7 +106,7 @@ class ViCareWater(ViCareEntity, WaterHeaterEntity):
         self._attr_name = name
         self._api = api
         self._circuit = circuit
-        self._attributes = {}
+        self._attributes: dict[str, Any] = {}
         self._current_mode = None
         self._attr_unique_id = f"{device_config.getConfig().serial}-{circuit.id}"
 
