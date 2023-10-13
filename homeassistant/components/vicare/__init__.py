213be2d29a5f986c627b92b461ba3b9e11286824
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2023-10-13 11:58:54 +0200

    Update __init__.py

--- homeassistant/components/vicare/__init__.py
+++ homeassistant/components/vicare/__init__.py
@@ -1,14 +1,13 @@
 """The ViCare integration."""
 from __future__ import annotations
 
-from collections.abc import Callable
+from collections.abc import Callable, Mapping
 from contextlib import suppress
 from dataclasses import dataclass
 import logging
 import os
 from typing import Any
 
-from construct import Mapping
 from PyViCare.PyViCare import PyViCare
 from PyViCare.PyViCareDevice import Device
 
