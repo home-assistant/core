c217f0ea3067302f12f37dd26c94f32b429cc723
J. Nick Koston <nick@koston.org>
2023-10-15 11:14:19 -1000

    Move event permissions out of the websocket api into auth (#101975)

--- homeassistant/auth/permissions/events.py
+++ homeassistant/auth/permissions/events.py
@@ -1,26 +1,18 @@
-"""Permission constants for the websocket API.
-
-Separate file to avoid circular imports.
-"""
+"""Permission for events."""
 from __future__ import annotations
 
 from typing import Final
 
-from homeassistant.components.frontend import EVENT_PANELS_UPDATED
-from homeassistant.components.lovelace import EVENT_LOVELACE_UPDATED
-from homeassistant.components.persistent_notification import (
-    EVENT_PERSISTENT_NOTIFICATIONS_UPDATED,
-)
-from homeassistant.components.recorder import (
-    EVENT_RECORDER_5MIN_STATISTICS_GENERATED,
-    EVENT_RECORDER_HOURLY_STATISTICS_GENERATED,
-)
-from homeassistant.components.shopping_list import EVENT_SHOPPING_LIST_UPDATED
 from homeassistant.const import (
     EVENT_COMPONENT_LOADED,
     EVENT_CORE_CONFIG_UPDATE,
+    EVENT_LOVELACE_UPDATED,
+    EVENT_PANELS_UPDATED,
+    EVENT_RECORDER_5MIN_STATISTICS_GENERATED,
+    EVENT_RECORDER_HOURLY_STATISTICS_GENERATED,
     EVENT_SERVICE_REGISTERED,
     EVENT_SERVICE_REMOVED,
+    EVENT_SHOPPING_LIST_UPDATED,
     EVENT_STATE_CHANGED,
     EVENT_THEMES_UPDATED,
 )
@@ -38,7 +30,6 @@ SUBSCRIBE_ALLOWLIST: Final[set[str]] = {
     EVENT_ENTITY_REGISTRY_UPDATED,
     EVENT_LOVELACE_UPDATED,
     EVENT_PANELS_UPDATED,
-    EVENT_PERSISTENT_NOTIFICATIONS_UPDATED,
     EVENT_RECORDER_5MIN_STATISTICS_GENERATED,
     EVENT_RECORDER_HOURLY_STATISTICS_GENERATED,
     EVENT_SERVICE_REGISTERED,
