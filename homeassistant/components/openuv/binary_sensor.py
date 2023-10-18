d73ad2546cd86b2e56a311615c74f98d0d4d0245
Aaron Bach <bachya1208@gmail.com>
2023-10-18 11:37:26 -0600

    Don't leak state change schedules

--- homeassistant/components/openuv/binary_sensor.py
+++ homeassistant/components/openuv/binary_sensor.py
@@ -1,7 +1,8 @@
 """Support for OpenUV binary sensors."""
-from collections.abc import Mapping
+from collections.abc import Callable, Mapping
 from dataclasses import dataclass
 from datetime import datetime
+from enum import StrEnum
 from typing import Any
 
 from homeassistant.components.binary_sensor import (
@@ -9,7 +10,7 @@ from homeassistant.components.binary_sensor import (
     BinarySensorEntityDescription,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import CALLBACK_TYPE, HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.event import async_track_point_in_utc_time
 from homeassistant.util.dt import as_local, parse_datetime, utcnow
@@ -43,7 +44,7 @@ async def async_setup_entry(
     )
 
 
-@dataclass
+@dataclass(frozen=True)
 class ProtectionWindow:
     """Define a protection window."""
 
@@ -51,11 +52,65 @@ class ProtectionWindow:
     to_dt_utc: datetime
 
 
+class ProtectionWindowBoundary(StrEnum):
+    """Define a protection window boundary."""
+
+    START = "start"
+    END = "end"
+
+
+@dataclass
+class ScheduledProtectionWindowStateChange:
+    """Define a representation of a scheduled protection window state change."""
+
+    hass: HomeAssistant
+    boundary: ProtectionWindowBoundary
+    target: datetime
+    update_state_method: Callable[[datetime], None]
+    _unsub_state_change_callback: CALLBACK_TYPE | None = None
+
+    @callback
+    def async_deregister(self) -> None:
+        """Deregister the scheduled state change."""
+        if self._unsub_state_change_callback:
+            self._unsub_state_change_callback()
+            self._unsub_state_change_callback = None
+
+    @callback
+    def async_schedule(self, target: datetime) -> None:
+        """Schedule the state change."""
+        if self._unsub_state_change_callback and target == self.target:
+            # If a state change has already been scheduled for the same datetime, just
+            # return:
+            return
+
+        LOGGER.debug(
+            "Scheduling a protection window state change: %s (%s)",
+            self.boundary,
+            self.target,
+        )
+        if self._unsub_state_change_callback:
+            self._unsub_state_change_callback()
+        self._unsub_state_change_callback = async_track_point_in_utc_time(
+            self.hass, self.update_state_method, self.target
+        )
+
+
 class ProtectionWindowBinarySensor(OpenUvEntity, BinarySensorEntity):
     """Define a binary sensor for OpenUV."""
 
     _attr_icon = "mdi:sunglasses"
     _attr_translation_key = "protection_window"
+
+    def __init__(
+        self, coordinator: OpenUvCoordinator, description: BinarySensorEntityDescription
+    ) -> None:
+        """Initialize."""
+        super().__init__(coordinator, description)
+
+        self._scheduled_state_changes: dict[
+            ProtectionWindowBoundary, ScheduledProtectionWindowStateChange
+        ] = {}
 
     @callback
     def _get_current_window(self) -> ProtectionWindow | None:
@@ -110,12 +165,21 @@ class ProtectionWindowBinarySensor(OpenUvEntity, BinarySensorEntity):
             self.async_write_ha_state()
 
         @callback
-        def async_schedule_state_change(target: datetime) -> None:
+        def async_schedule_state_change(
+            boundary: ProtectionWindowBoundary, target: datetime
+        ) -> None:
             """Schedule an entity state change based upon a datetime."""
-            LOGGER.debug("Scheduling a protection window state change at %s", target)
-            self.async_on_remove(
-                async_track_point_in_utc_time(self.hass, async_update_state, target)
-            )
+            if (state_change := self._scheduled_state_changes.get(boundary)) is None:
+                state_change = self._scheduled_state_changes[
+                    boundary
+                ] = ScheduledProtectionWindowStateChange(
+                    hass=self.hass,
+                    boundary=boundary,
+                    target=target,
+                    update_state_method=async_update_state,
+                )
+
+            state_change.async_schedule(target)
 
         @callback
         def async_schedule_state_changes() -> None:
@@ -125,13 +189,22 @@ class ProtectionWindowBinarySensor(OpenUvEntity, BinarySensorEntity):
 
             now = utcnow()
             if now < window.from_dt_utc:
-                async_schedule_state_change(window.from_dt_utc)
-                async_schedule_state_change(window.to_dt_utc)
-            elif now < window.to_dt_utc:
-                async_schedule_state_change(window.to_dt_utc)
+                async_schedule_state_change(
+                    ProtectionWindowBoundary.START, window.from_dt_utc
+                )
+            if now < window.to_dt_utc:
+                async_schedule_state_change(
+                    ProtectionWindowBoundary.END, window.to_dt_utc
+                )
 
         self.async_on_remove(
             self.coordinator.async_add_listener(async_schedule_state_changes)
         )
 
         async_schedule_state_changes()
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Run when entity will be removed from hass."""
+        for state_change in self._scheduled_state_changes.values():
+            state_change.async_deregister()
+        self._scheduled_state_changes = {}
