eb7b04c4ea369cfb86db5fb336596245ed1df7c4
fconstantin <francois.constantin@kleegroup.com>
2023-10-18 18:00:08 +0200

    Add support for Heating Zone without zone temperature sensor
    Zone temperature sensor is optional. When temperature_device is not found, the change enable zone control.

--- homeassistant/components/overkiz/climate_entities/atlantic_pass_apc_heating_zone.py
+++ homeassistant/components/overkiz/climate_entities/atlantic_pass_apc_heating_zone.py
@@ -88,14 +88,17 @@ class AtlanticPassAPCHeatingZone(OverkizEntity, ClimateEntity):
         super().__init__(device_url, coordinator)
 
         # Temperature sensor use the same base_device_url and use the n+1 index
-        self.temperature_device = self.executor.linked_device(
-            int(self.index_device_url) + 1
-        )
+        try:
+            self.temperature_device = self.executor.linked_device(
+                int(self.index_device_url) + 1
+            )
+        except KeyError:
+            LOGGER.error("AtlanticPassAPCHeatingZone dont have temperature sensor")
 
     @property
     def current_temperature(self) -> float | None:
         """Return the current temperature."""
-        if temperature := self.temperature_device.states[OverkizState.CORE_TEMPERATURE]:
+        if self.temperature_device is not None and temperature := self.temperature_device.states[OverkizState.CORE_TEMPERATURE]:
             return cast(float, temperature.value)
 
         return None
