72c8e6e1ba94d626494604e78935b85dd5887b41
abmantis <amfcalt@gmail.com>
2023-10-18 22:38:12 +0100

    Restore dict operator

--- homeassistant/components/idasen_desk/cover.py
+++ homeassistant/components/idasen_desk/cover.py
@@ -53,7 +53,7 @@ class IdasenDeskCover(CoordinatorEntity, CoverEntity):
         """Initialize an Idasen Desk cover."""
         super().__init__(coordinator)
         self._desk = coordinator.desk
-        self._attr_name = device_info.get(ATTR_NAME)
+        self._attr_name = device_info[ATTR_NAME]
         self._attr_unique_id = address
         self._attr_device_info = device_info
 
