2eba71f9e2c567dbfdb2ab24d416b446b9d92786
Simone Chemelli <simone.chemelli@gmail.com>
2023-10-19 16:28:52 +0000

    Bump aiocomelit to 0.3.0

--- homeassistant/components/comelit/light.py
+++ homeassistant/components/comelit/light.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import Any
 
 from aiocomelit import ComelitSerialBridgeObject
-from aiocomelit.const import LIGHT
+from aiocomelit.const import LIGHT, STATE_OFF, STATE_ON
 
 from homeassistant.components.light import LightEntity
 from homeassistant.config_entries import ConfigEntry
@@ -12,7 +12,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DOMAIN, STATE_OFF, STATE_ON
+from .const import DOMAIN
 from .coordinator import ComelitSerialBridge
 
 
