080b30b9039613c4ed221597c64ecfa3cbd665b6
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-10-20 22:02:02 +0200

    more tests for init

--- tests/components/lamarzocco/test_init.py
+++ tests/components/lamarzocco/test_init.py
@@ -1,7 +1,10 @@
 """Test initialization of lamarzocco."""
 from unittest.mock import MagicMock
 
-from homeassistant.config_entries import ConfigEntryState
+from lmcloud.exceptions import AuthFail, RequestNotSuccessful
+
+from homeassistant.components.lamarzocco.const import DOMAIN
+from homeassistant.config_entries import SOURCE_REAUTH, ConfigEntryState
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
@@ -23,3 +26,45 @@ async def test_load_unload_config_entry(
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
+
+
+async def test_config_entry_not_ready(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_lamarzocco: MagicMock,
+) -> None:
+    """Test the LaMetric configuration entry not ready."""
+    mock_lamarzocco.connect.side_effect = RequestNotSuccessful("")
+
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert len(mock_lamarzocco.connect.mock_calls) == 1
+    assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
+
+
+async def test_invalid_auth(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_lamarzocco: MagicMock,
+) -> None:
+    """Test auth error during setup."""
+    mock_lamarzocco.connect.side_effect = AuthFail("")
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert mock_config_entry.state is ConfigEntryState.SETUP_ERROR
+    assert len(mock_lamarzocco.connect.mock_calls) == 1
+
+    flows = hass.config_entries.flow.async_progress()
+    assert len(flows) == 1
+
+    flow = flows[0]
+    assert flow.get("step_id") == "reauth_confirm"
+    assert flow.get("handler") == DOMAIN
+
+    assert "context" in flow
+    assert flow["context"].get("source") == SOURCE_REAUTH
+    assert flow["context"].get("entry_id") == mock_config_entry.entry_id
