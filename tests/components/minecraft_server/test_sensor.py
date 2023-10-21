b4f82f339bd1953973c727bd073d21760434b9e3
elmurato <1382097+elmurato@users.noreply.github.com>
2023-10-21 06:31:17 +0000

    Use freezegun for setting the time forward

--- tests/components/minecraft_server/test_sensor.py
+++ tests/components/minecraft_server/test_sensor.py
@@ -2,6 +2,7 @@
 from datetime import timedelta
 from unittest.mock import patch
 
+from freezegun.api import FrozenDateTimeFactory
 from mcstatus import BedrockServer, JavaServer
 from mcstatus.status_response import BedrockStatusResponse, JavaStatusResponse
 import pytest
@@ -10,7 +11,6 @@ from syrupy import SnapshotAssertion
 from homeassistant.const import STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
-from homeassistant.util import dt as dt_util
 
 from .const import (
     TEST_BEDROCK_STATUS_RESPONSE,
@@ -245,11 +245,11 @@ async def test_sensor_update(
     entity_id: er.EntityRegistry,
     request: pytest.FixtureRequest,
     snapshot: SnapshotAssertion,
+    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test sensor update."""
     mock_config_entry = request.getfixturevalue(mock_config_entry)
     mock_config_entry.add_to_hass(hass)
-    future = dt_util.utcnow() + timedelta(minutes=1)
 
     with patch(
         f"homeassistant.components.minecraft_server.api.{server.__name__}.lookup",
@@ -260,7 +260,8 @@ async def test_sensor_update(
     ):
         assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
-        async_fire_time_changed(hass, future)
+        freezer.tick(timedelta(minutes=1))
+        async_fire_time_changed(hass)
         await hass.async_block_till_done()
         assert hass.states.get(entity_id) == snapshot
 
@@ -337,7 +338,7 @@ async def test_sensor_update_failure(
     status_response: JavaStatusResponse | BedrockStatusResponse,
     entity_id: er.EntityRegistry,
     request: pytest.FixtureRequest,
-    snapshot: SnapshotAssertion,
+    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test failed sensor update."""
     mock_config_entry = request.getfixturevalue(mock_config_entry)
@@ -353,11 +354,11 @@ async def test_sensor_update_failure(
         assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
 
-    future = dt_util.utcnow() + timedelta(minutes=2)
     with patch(
         f"homeassistant.components.minecraft_server.api.{server.__name__}.async_status",
         side_effect=OSError,
     ):
-        async_fire_time_changed(hass, future)
+        freezer.tick(timedelta(minutes=1))
+        async_fire_time_changed(hass)
         await hass.async_block_till_done()
         assert hass.states.get(entity_id).state == STATE_UNAVAILABLE
