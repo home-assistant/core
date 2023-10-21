fd1fab62bc2b8fc870009167c1486c6a618f7256
elmurato <1382097+elmurato@users.noreply.github.com>
2023-10-21 06:44:02 +0000

    Use freezegun for forwarding the time

--- tests/components/minecraft_server/test_binary_sensor.py
+++ tests/components/minecraft_server/test_binary_sensor.py
@@ -2,13 +2,14 @@
 from datetime import timedelta
 from unittest.mock import patch
 
+from freezegun.api import FrozenDateTimeFactory
 from mcstatus import BedrockServer, JavaServer
 from mcstatus.status_response import BedrockStatusResponse, JavaStatusResponse
 import pytest
 from syrupy import SnapshotAssertion
 
+from homeassistant.const import STATE_OFF
 from homeassistant.core import HomeAssistant
-from homeassistant.util import dt as dt_util
 
 from .const import (
     TEST_BEDROCK_STATUS_RESPONSE,
@@ -65,22 +66,23 @@ async def test_binary_sensor_update(
     status_response: JavaStatusResponse | BedrockStatusResponse,
     request: pytest.FixtureRequest,
     snapshot: SnapshotAssertion,
+    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test binary sensor update."""
     mock_config_entry = request.getfixturevalue(mock_config_entry)
     mock_config_entry.add_to_hass(hass)
-    future = dt_util.utcnow() + timedelta(minutes=1)
 
     with patch(
         f"homeassistant.components.minecraft_server.api.{server.__name__}.lookup",
         return_value=server(host=TEST_HOST, port=TEST_PORT),
     ), patch(
         f"homeassistant.components.minecraft_server.api.{server.__name__}.async_status",
-        return_value=status_response,  # TODO: Use second test status_response?
+        return_value=status_response,
     ):
         assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
-        async_fire_time_changed(hass, future)
+        freezer.tick(timedelta(minutes=1))
+        async_fire_time_changed(hass)
         await hass.async_block_till_done()
         assert hass.states.get("binary_sensor.minecraft_server_status") == snapshot
 
@@ -98,7 +100,7 @@ async def test_sensor_update_failure(
     server: JavaServer | BedrockServer,
     status_response: JavaStatusResponse | BedrockStatusResponse,
     request: pytest.FixtureRequest,
-    snapshot: SnapshotAssertion,
+    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test failed sensor update."""
     mock_config_entry = request.getfixturevalue(mock_config_entry)
@@ -114,11 +116,13 @@ async def test_sensor_update_failure(
         assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
 
-    future = dt_util.utcnow() + timedelta(minutes=2)
     with patch(
         f"homeassistant.components.minecraft_server.api.{server.__name__}.async_status",
         side_effect=OSError,
     ):
-        async_fire_time_changed(hass, future)
+        freezer.tick(timedelta(minutes=1))
+        async_fire_time_changed(hass)
         await hass.async_block_till_done()
-        assert hass.states.get("binary_sensor.minecraft_server_status") == snapshot
+        assert (
+            hass.states.get("binary_sensor.minecraft_server_status").state == STATE_OFF
+        )
