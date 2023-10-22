ffbd9b9fe0238cbc05ad63253ce1f82961a8f38b
J. Nick Koston <nick@koston.org>
2023-10-22 11:55:13 -1000

    Avoid more device_class lookups for number entities when writing state (#102381)

--- tests/components/number/test_init.py
+++ tests/components/number/test_init.py
@@ -8,6 +8,7 @@ import pytest
 from homeassistant.components.number import (
     ATTR_MAX,
     ATTR_MIN,
+    ATTR_MODE,
     ATTR_STEP,
     ATTR_VALUE,
     DOMAIN,
@@ -227,6 +228,12 @@ async def test_attributes(hass: HomeAssistant) -> None:
     assert number.step == 1.0
     assert number.unit_of_measurement is None
     assert number.value == 0.5
+    assert number.capability_attributes == {
+        ATTR_MAX: 100.0,
+        ATTR_MIN: 0.0,
+        ATTR_MODE: NumberMode.AUTO,
+        ATTR_STEP: 1.0,
+    }
 
     number_2 = MockNumberEntity()
     number_2.hass = hass
@@ -235,6 +242,12 @@ async def test_attributes(hass: HomeAssistant) -> None:
     assert number_2.step == 0.1
     assert number_2.unit_of_measurement == "native_cats"
     assert number_2.value == 0.5
+    assert number_2.capability_attributes == {
+        ATTR_MAX: 0.5,
+        ATTR_MIN: -0.5,
+        ATTR_MODE: NumberMode.AUTO,
+        ATTR_STEP: 0.1,
+    }
 
     number_3 = MockNumberEntityAttr()
     number_3.hass = hass
@@ -243,6 +256,12 @@ async def test_attributes(hass: HomeAssistant) -> None:
     assert number_3.step == 100.0
     assert number_3.unit_of_measurement == "native_dogs"
     assert number_3.value == 500.0
+    assert number_3.capability_attributes == {
+        ATTR_MAX: 1000.0,
+        ATTR_MIN: -1000.0,
+        ATTR_MODE: NumberMode.AUTO,
+        ATTR_STEP: 100.0,
+    }
 
     number_4 = MockNumberEntityDescr()
     number_4.hass = hass
@@ -251,6 +270,12 @@ async def test_attributes(hass: HomeAssistant) -> None:
     assert number_4.step == 2.0
     assert number_4.unit_of_measurement == "native_rabbits"
     assert number_4.value is None
+    assert number_4.capability_attributes == {
+        ATTR_MAX: 10.0,
+        ATTR_MIN: -10.0,
+        ATTR_MODE: NumberMode.AUTO,
+        ATTR_STEP: 2.0,
+    }
 
 
 async def test_sync_set_value(hass: HomeAssistant) -> None:
