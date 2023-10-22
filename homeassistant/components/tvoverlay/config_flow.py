07914d1b801008ba473cb528b444354cce7e222f
Hareesh M U <hareesh.mu@gmail.com>
2023-10-22 09:32:26 +0000

    Add unique id and update tests

--- homeassistant/components/tvoverlay/config_flow.py
+++ homeassistant/components/tvoverlay/config_flow.py
@@ -25,17 +25,32 @@ class TVOverlayFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> FlowResult:
         """Handle a flow initiated by the user."""
         errors = {}
-
         if user_input is not None:
             self._async_abort_entries_match(
                 {CONF_HOST: user_input[CONF_HOST], CONF_NAME: user_input[CONF_NAME]}
             )
-            if not (error := await self._async_try_connect(user_input[CONF_HOST])):
+            error, info = await self._async_try_connect(user_input[CONF_HOST])
+            if error is not None:
+                errors["base"] = error
+            elif info is not None:
+                device_name = (
+                    info.get("result", {})
+                    .get("settings", {})
+                    .get("deviceName", DEFAULT_NAME)
+                )
+                unique_id = (
+                    device_name.replace(" ", "_")
+                    + "_"
+                    + str.replace(user_input[CONF_HOST], ".", "_")
+                )
+                await self.async_set_unique_id(unique_id)
+                if user_input[CONF_NAME] == DEFAULT_NAME:
+                    user_input[CONF_NAME] = device_name
+                self._abort_if_unique_id_configured()
                 return self.async_create_entry(
                     title=user_input[CONF_NAME],
                     data=user_input,
                 )
-            errors["base"] = error
 
         return self.async_show_form(
             step_id="user",
@@ -48,14 +63,18 @@ class TVOverlayFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
-    async def _async_try_connect(self, host: str) -> str | None:
+    async def _async_try_connect(
+        self, host: str
+    ) -> tuple[str | None, dict[str, Any] | None]:
         """Try connecting to TvOverlay."""
+        notifier = Notifications(host)
         try:
-            await self.hass.async_add_executor_job(Notifications, host)
+            info = await notifier.async_connect()
+            _LOGGER.debug("TvOverlay device info for host %s: %s", host, info)
         except ConnectError:
             _LOGGER.error("Error connecting to device at %s", host)
-            return "cannot_connect"
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception")
-            return "unknown"
-        return None
+            return "cannot_connect", None
+        except Exception as ex:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception: %s", ex)
+            return "unknown", None
+        return None, info
