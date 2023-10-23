2859c6915bfe4aa8ddab258935cd9fb44afb0cdb
SEIKH NABAB UDDIN <93948993+nababuddin@users.noreply.github.com>
2023-10-24 01:00:33 +0530

    Update countries.py

--- script/countries.py
+++ script/countries.py
@@ -1,27 +1,26 @@
-"""Helper script to update country list.
-
-ISO does not publish a machine readable list free of charge, so the list is generated
-with help of the pycountry package.
-"""
+# Import only what is necessary
 from pathlib import Path
-
-import pycountry
-
+from pycountry import countries
 from .hassfest.serializer import format_python_namespace
 
-countries = {x.alpha_2 for x in pycountry.countries}
+# Create a set of alpha-2 country codes
+country_codes = {country.alpha_2 for country in countries}
 
+# Build the generator string
 generator_string = """script.countries
 
 The values are directly corresponding to the ISO 3166 standard. If you need changes
 to the political situation in the world, please contact the ISO 3166 working group.
 """
 
-Path("homeassistant/generated/countries.py").write_text(
-    format_python_namespace(
-        {
-            "COUNTRIES": countries,
-        },
-        generator=generator_string,
-    )
-)
+# Build the Python namespace dictionary
+namespace = {
+    "COUNTRIES": country_codes,
+}
+
+# Combine content in memory
+content = format_python_namespace(namespace, generator=generator_string)
+
+# Write content to the file in one go
+file_path = "homeassistant/generated/countries.py"
+Path(file_path).write_text(content)
