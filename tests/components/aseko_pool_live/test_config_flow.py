b843d155910b086244b9ecf7d920f4b112115826
hopkins-tk <hopkins.tk@gmail.com>
2023-10-23 20:45:21 +0000

    fix: add tests for config flow

--- tests/components/aseko_pool_live/test_config_flow.py
+++ tests/components/aseko_pool_live/test_config_flow.py
@@ -4,21 +4,29 @@ from unittest.mock import patch
 from aioaseko import AccountInfo, APIUnavailable, InvalidAuthCredentials
 import pytest
 
-from homeassistant import config_entries, setup
+from homeassistant import config_entries
 from homeassistant.components.aseko_pool_live.const import DOMAIN
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
+from tests.common import MockConfigEntry
 
-async def test_form(hass: HomeAssistant) -> None:
+
+async def test_async_step_user_form(hass: HomeAssistant) -> None:
     """Test we get the form."""
-    await setup.async_setup_component(hass, "persistent_notification", {})
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     assert result["type"] == FlowResultType.FORM
     assert result["errors"] == {}
+
+
+async def test_async_step_user_success(hass: HomeAssistant) -> None:
+    """Test we get the form."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
 
     with patch(
         "homeassistant.components.aseko_pool_live.config_flow.WebAccount.login",
@@ -53,6 +61,39 @@ async def test_form(hass: HomeAssistant) -> None:
         (Exception, "unknown"),
     ],
 )
+async def test_async_step_user_exception(
+    hass: HomeAssistant, error_web: Exception, reason: str
+) -> None:
+    """Test we get the form."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "homeassistant.components.aseko_pool_live.config_flow.WebAccount.login",
+        return_value=AccountInfo("aseko@example.com", "a_user_id", "any_language"),
+        side_effect=error_web,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_EMAIL: "aseko@example.com",
+                CONF_PASSWORD: "passw0rd",
+            },
+        )
+
+        assert result2["type"] == FlowResultType.FORM
+        assert result2["errors"] == {"base": reason}
+
+
+@pytest.mark.parametrize(
+    ("error_web", "reason"),
+    [
+        (APIUnavailable, "cannot_connect"),
+        (InvalidAuthCredentials, "invalid_auth"),
+        (Exception, "unknown"),
+    ],
+)
 async def test_get_account_info_exceptions(
     hass: HomeAssistant, error_web: Exception, reason: str
 ) -> None:
@@ -76,3 +117,83 @@ async def test_get_account_info_exceptions(
 
     assert result2["type"] == FlowResultType.FORM
     assert result2["errors"] == {"base": reason}
+
+
+async def test_async_step_reauth_form(hass: HomeAssistant) -> None:
+    """Test we get the form."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_REAUTH, "entry_id": "test-id"}
+    )
+
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "reauth_confirm"
+    assert result["errors"] == {}
+
+
+async def test_async_step_reauth_success(hass: HomeAssistant) -> None:
+    """Test successful reauthentication."""
+
+    MockConfigEntry(
+        domain=DOMAIN,
+        entry_id="0",
+        unique_id="0",
+        data={CONF_EMAIL: "aseko@example.com"},
+    ).add_to_hass(hass)
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={
+            "source": config_entries.SOURCE_REAUTH,
+            "entry_id": "0",
+            "unique_id": "0",
+        },
+    )
+
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "reauth_confirm"
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.aseko_pool_live.config_flow.WebAccount.login",
+        return_value=AccountInfo("aseko@example.com", "a_user_id", "any_language"),
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_EMAIL: "aseko@example.com", CONF_PASSWORD: "passw0rd"},
+        )
+
+    assert result["type"] == FlowResultType.ABORT
+    assert result["reason"] == "reauth_successful"
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+@pytest.mark.parametrize(
+    ("error_web", "reason"),
+    [
+        (APIUnavailable, "cannot_connect"),
+        (InvalidAuthCredentials, "invalid_auth"),
+        (Exception, "unknown"),
+    ],
+)
+async def test_async_step_reauth_exception(
+    hass: HomeAssistant, error_web: Exception, reason: str
+) -> None:
+    """Test we get the form."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_REAUTH, "entry_id": "test-id"}
+    )
+
+    with patch(
+        "homeassistant.components.aseko_pool_live.config_flow.WebAccount.login",
+        return_value=AccountInfo("aseko@example.com", "a_user_id", "any_language"),
+        side_effect=error_web,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_EMAIL: "aseko@example.com",
+                CONF_PASSWORD: "passw0rd",
+            },
+        )
+
+        assert result2["type"] == FlowResultType.FORM
+        assert result2["errors"] == {"base": reason}
