8dfe83f696208c388d51addefce6c2d4e56f08c8
Joost Lekkerkerker <joostlek@outlook.com>
2023-10-24 10:15:05 +0200

    Update homeassistant/components/random/config_flow.py
    Co-authored-by: Robert Resch <robert@resch.dev>

--- homeassistant/components/random/config_flow.py
+++ homeassistant/components/random/config_flow.py
@@ -33,13 +33,20 @@ from .const import DOMAIN
 from .sensor import DEFAULT_MAX, DEFAULT_MIN
 
 
-def generate_schema(domain: str, flow_type: str) -> dict[vol.Marker, Any]:
+class _FlowType(StrEnum):
+    CONFIG = "config"
+    OPTION = "option"
+
+
+def _generate_schema(domain: str, flow_type: _FlowType) -> vol.Schema:
     """Generate schema."""
     schema: dict[vol.Marker, Any] = {}
 
-    if domain == Platform.BINARY_SENSOR and flow_type == "config":
-        schema = {
-            vol.Optional(CONF_DEVICE_CLASS): SelectSelector(
+    if flow_type == _FlowType.CONFIG:
+        schema[vol.Required(CONF_NAME)] = TextSelector()
+
+        if domain == Platform.BINARY_SENSOR:
+            schema[vol.Optional(CONF_DEVICE_CLASS)] = SelectSelector(
                 SelectSelectorConfig(
                     options=[cls.value for cls in BinarySensorDeviceClass],
                     sort=True,
@@ -47,58 +54,42 @@ def generate_schema(domain: str, flow_type: str) -> dict[vol.Marker, Any]:
                     translation_key="binary_sensor_device_class",
                 ),
             )
-        }
 
     if domain == Platform.SENSOR:
-        schema = {
-            vol.Optional(CONF_MINIMUM, default=DEFAULT_MIN): cv.positive_int,
-            vol.Optional(CONF_MAXIMUM, default=DEFAULT_MAX): cv.positive_int,
-            vol.Optional(CONF_DEVICE_CLASS): SelectSelector(
-                SelectSelectorConfig(
-                    options=[
-                        cls.value
-                        for cls in SensorDeviceClass
-                        if cls != SensorDeviceClass.ENUM
-                    ],
-                    sort=True,
-                    mode=SelectSelectorMode.DROPDOWN,
-                    translation_key="sensor_device_class",
+        schema.update(
+            {
+                vol.Optional(CONF_MINIMUM, default=DEFAULT_MIN): cv.positive_int,
+                vol.Optional(CONF_MAXIMUM, default=DEFAULT_MAX): cv.positive_int,
+                vol.Optional(CONF_DEVICE_CLASS): SelectSelector(
+                    SelectSelectorConfig(
+                        options=[
+                            cls.value
+                            for cls in SensorDeviceClass
+                            if cls != SensorDeviceClass.ENUM
+                        ],
+                        sort=True,
+                        mode=SelectSelectorMode.DROPDOWN,
+                        translation_key="sensor_device_class",
+                    ),
                 ),
-            ),
-            vol.Optional(CONF_UNIT_OF_MEASUREMENT): SelectSelector(
-                SelectSelectorConfig(
-                    options=[
-                        str(unit)
-                        for units in DEVICE_CLASS_UNITS.values()
-                        for unit in units
-                        if unit is not None
-                    ],
-                    sort=True,
-                    mode=SelectSelectorMode.DROPDOWN,
-                    translation_key="sensor_unit_of_measurement",
-                    custom_value=True,
+                vol.Optional(CONF_UNIT_OF_MEASUREMENT): SelectSelector(
+                    SelectSelectorConfig(
+                        options=[
+                            str(unit)
+                            for units in DEVICE_CLASS_UNITS.values()
+                            for unit in units
+                            if unit is not None
+                        ],
+                        sort=True,
+                        mode=SelectSelectorMode.DROPDOWN,
+                        translation_key="sensor_unit_of_measurement",
+                        custom_value=True,
+                    ),
                 ),
-            ),
-        }
+            }
+        )
 
-    return schema
-
-
-def options_schema(domain: str) -> vol.Schema:
-    """Generate options schema."""
-    return vol.Schema(
-        generate_schema(domain, "option"),
-    )
-
-
-def config_schema(domain: str) -> vol.Schema:
-    """Generate config schema."""
-    return vol.Schema(
-        {
-            vol.Required(CONF_NAME): TextSelector(),
-        }
-        | generate_schema(domain, "config"),
-    )
+    return vol.Schema(schema)
 
 
 async def choose_options_step(options: dict[str, Any]) -> str:
