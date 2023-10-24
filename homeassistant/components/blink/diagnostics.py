f5db76f695455100aac3d16ea808f2bddccb9cb4
mkmer <m_kasper@sbcglobal.net>
2023-10-24 11:51:22 +0000

    Add diagnostic platform and tests to blink

---
+++ homeassistant/components/blink/diagnostics.py
@@ -0,0 +1,35 @@
+"""Diagnostics support for Aladdin Connect."""
+from __future__ import annotations
+
+from typing import Any
+
+from blinkpy.blinkpy import Blink
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+TO_REDACT = {"serial", "macaddress", "username", "password", "token"}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+
+    api: Blink = hass.data[DOMAIN][config_entry.entry_id].api
+
+    cameras = {api.cameras[camera] for camera in api.cameras}
+    data = {}
+    for camera in cameras:
+        data[camera.name] = dict(camera.attributes.items())
+
+    diagnostics_data = {
+        "config_entry": async_redact_data(config_entry.as_dict(), TO_REDACT),
+        "cameras": async_redact_data(data, TO_REDACT),
+    }
+
+    return diagnostics_data
