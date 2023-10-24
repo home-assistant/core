d2ec819913701655e984fe688cfff32a5144cf90
Raine Pretorius <raine.pretorius1@gmail.com>
2023-10-24 07:16:42 +0000

    Fixed config flow error

--- homeassistant/components/olarm_sensors/config_flow.py
+++ homeassistant/components/olarm_sensors/config_flow.py
@@ -39,7 +39,7 @@ class OlarmSensorsConfigFlow(ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None) -> FlowResult:
         """Handle the initial step."""
-        errors: dict = {str: Any}
+        errors: dict[str, Any] = {}
 
         if user_input is None:
             return await self._show_setup_form()
@@ -113,7 +113,6 @@ class OlarmSensorsConfigFlow(ConfigFlow, domain=DOMAIN):
                     CONF_DEVICE_FIRMWARE: firmware,
                     CONF_ALARM_CODE: alarm_code,
                     CONF_OLARM_DEVICES: setup_devices,
-                    OLARM_DEVICES: json,
                     OLARM_DEVICE_AMOUNT: len(json),
                     OLARM_DEVICE_NAMES: setup_devices,
                 },
@@ -160,12 +159,10 @@ class OlarmSensorsConfigFlow(ConfigFlow, domain=DOMAIN):
                 vol.Required(
                     CONF_API_KEY,
                 ): cv.string,
-                vol.Required(
-                    CONF_SCAN_INTERVAL,
-                ): vol.All(vol.Coerce(int), vol.Range(min=5)),
-                vol.Optional(
-                    CONF_ALARM_CODE,
-                ): cv.string,
+                vol.Required(CONF_SCAN_INTERVAL): vol.All(
+                    vol.Coerce(int), vol.Range(min=5)
+                ),
+                vol.Optional(CONF_ALARM_CODE, default="1234567890"): cv.string,
             }
         )
 
@@ -187,6 +184,11 @@ class OlarmOptionsFlow(OptionsFlow):
 
     def _get_schema(self):
         """Return the data schema for the user form."""
+        if self.config_entry.data[CONF_ALARM_CODE] is None:
+            alarm_code = "1234567890"
+
+        else:
+            alarm_code = self.config_entry.data[CONF_ALARM_CODE]
 
         return vol.Schema(
             {
@@ -196,9 +198,7 @@ class OlarmOptionsFlow(OptionsFlow):
                 vol.Required(
                     CONF_SCAN_INTERVAL,
                 ): vol.All(vol.Coerce(int), vol.Range(min=5)),
-                vol.Optional(
-                    CONF_ALARM_CODE,
-                ): cv.string,
+                vol.Optional(CONF_ALARM_CODE, default=alarm_code): cv.string,
                 vol.Optional(
                     CONF_OLARM_DEVICES,
                 ): cv.multi_select(self.config_entry.data[OLARM_DEVICE_NAMES]),
