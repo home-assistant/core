5232a5512828a7d42d15311426d667db309fdb96
Martin Hjelmare <marhje52@gmail.com>
2023-10-25 11:34:41 +0200

    Fix cloud stt entity_id

--- tests/components/cloud/test_stt.py
+++ tests/components/cloud/test_stt.py
@@ -104,14 +104,14 @@ async def test_cloud_speech(
     on_start_callback = cloud.register_on_start.call_args[0][0]
     await on_start_callback()
 
-    state = hass.states.get("stt.cloud")
+    state = hass.states.get("stt.home_assistant_cloud")
     assert state
     assert state.state == STATE_UNKNOWN
 
     client = await hass_client()
 
     response = await client.post(
-        "/api/stt/stt.cloud",
+        "/api/stt/stt.home_assistant_cloud",
         headers={
             "X-Speech-Content": (
                 "format=wav; codec=pcm; sample_rate=16000; bit_rate=16; channel=1;"
@@ -131,7 +131,7 @@ async def test_cloud_speech(
     assert response.status == HTTPStatus.OK
     assert response_data == expected_response_data
 
-    state = hass.states.get("stt.cloud")
+    state = hass.states.get("stt.home_assistant_cloud")
     assert state
     assert state.state not in (STATE_UNAVAILABLE, STATE_UNKNOWN)
 
@@ -161,12 +161,15 @@ async def test_migrating_pipelines(
     await on_start_callback()
     await hass.async_block_till_done()
 
-    state = hass.states.get("stt.cloud")
+    state = hass.states.get("stt.home_assistant_cloud")
     assert state
     assert state.state == STATE_UNKNOWN
 
     # The stt engine should be updated to the new cloud stt engine id.
-    assert hass_storage[STORAGE_KEY]["data"]["items"][0]["stt_engine"] == "stt.cloud"
+    assert (
+        hass_storage[STORAGE_KEY]["data"]["items"][0]["stt_engine"]
+        == "stt.home_assistant_cloud"
+    )
 
     # The other items should stay the same.
     assert (
