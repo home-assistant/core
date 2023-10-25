dc77d5ac53a1b8eb5832d3fa47022cd2b79eaa5a
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-10-24 21:55:10 -0400

    Copy mazda deprecation

---
+++ homeassistant/components/eight_sleep/__init__.py
@@ -0,0 +1,37 @@
+"""The Eight Sleep integration."""
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry, ConfigEntryState
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import issue_registry as ir
+
+DOMAIN = "eight_sleep"
+
+
+async def async_setup_entry(hass: HomeAssistant, _: ConfigEntry) -> bool:
+    """Set up Eight Sleep from a config entry."""
+    ir.async_create_issue(
+        hass,
+        DOMAIN,
+        DOMAIN,
+        is_fixable=False,
+        severity=ir.IssueSeverity.ERROR,
+        translation_key="integration_removed",
+        translation_placeholders={
+            "entries": "/config/integrations/integration/eight_sleep"
+        },
+    )
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if all(
+        config_entry.state is ConfigEntryState.NOT_LOADED
+        for config_entry in hass.config_entries.async_entries(DOMAIN)
+        if config_entry.entry_id != entry.entry_id
+    ):
+        ir.async_delete_issue(hass, DOMAIN, DOMAIN)
+
+    return True
