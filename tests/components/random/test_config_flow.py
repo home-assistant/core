73162d784d39f2ff7accce42c736f6210637b2df
Joost Lekkerkerker <joostlek@outlook.com>
2023-10-25 12:40:14 +0200

    Update tests/components/random/test_config_flow.py
    Co-authored-by: Robert Resch <robert@resch.dev>

--- tests/components/random/test_config_flow.py
+++ tests/components/random/test_config_flow.py
@@ -117,7 +117,7 @@ async def test_wrong_uom(
     assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "sensor"
 
-    with pytest.raises(Invalid):
+    with pytest.raises(Invalid, match="todo"):
         await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
