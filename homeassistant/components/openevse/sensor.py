6809b4037dafddaa704be77f3c323b8353db40e5
Stephen Eisenhauer <bhs2007@gmail.com>
2023-10-25 04:43:27 +0000

    Add some missing sensors for openevse

--- homeassistant/components/openevse/sensor.py
+++ homeassistant/components/openevse/sensor.py
@@ -17,7 +17,10 @@ from homeassistant.components.sensor import (
 from homeassistant.const import (
     CONF_HOST,
     CONF_MONITORED_VARIABLES,
+    UnitOfElectricCurrent,
+    UnitOfElectricPotential,
     UnitOfEnergy,
+    UnitOfPower,
     UnitOfTemperature,
     UnitOfTime,
 )
@@ -30,8 +33,18 @@ _LOGGER = logging.getLogger(__name__)
 
 SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
     SensorEntityDescription(
-        key="status",
-        name="Charging Status",
+        key="ambient_temp",
+        name="Ambient Temperature",
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    SensorEntityDescription(
+        key="min_amps",
+        name="Min Amps",
+        native_unit_of_measurement=UnitOfElectricCurrent.AMPERE,
+        device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
         key="charge_time",
@@ -41,10 +54,31 @@ SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
-        key="ambient_temp",
-        name="Ambient Temperature",
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
-        device_class=SensorDeviceClass.TEMPERATURE,
+        key="charging_current",
+        name="Charging Current",
+        native_unit_of_measurement=UnitOfElectricCurrent.AMPERE,
+        device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    SensorEntityDescription(
+        key="charging_power",
+        name="Charging Power",
+        native_unit_of_measurement=UnitOfPower.WATT,
+        device_class=SensorDeviceClass.POWER,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    SensorEntityDescription(
+        key="charging_voltage",
+        name="Charging Voltage",
+        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
+        device_class=SensorDeviceClass.VOLTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    SensorEntityDescription(
+        key="current_capacity",
+        name="Current Capacity",
+        native_unit_of_measurement=UnitOfElectricCurrent.AMPERE,
+        device_class=SensorDeviceClass.CURRENT,
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
@@ -55,11 +89,22 @@ SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
+        key="max_amps",
+        name="Max Amps",
+        native_unit_of_measurement=UnitOfElectricCurrent.AMPERE,
+        device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    SensorEntityDescription(
         key="rtc_temp",
         name="RTC Temperature",
         native_unit_of_measurement=UnitOfTemperature.CELSIUS,
         device_class=SensorDeviceClass.TEMPERATURE,
         state_class=SensorStateClass.MEASUREMENT,
+    ),
+    SensorEntityDescription(
+        key="status",
+        name="Charging Status",
     ),
     SensorEntityDescription(
         key="usage_session",
@@ -122,21 +167,42 @@ class OpenEVSESensor(SensorEntity):
         """Get the monitored data from the charger."""
         try:
             sensor_type = self.entity_description.key
-            if sensor_type == "status":
-                self._attr_native_value = self.charger.getStatus()
-            elif sensor_type == "charge_time":
-                self._attr_native_value = self.charger.getChargeTimeElapsed() / 60
-            elif sensor_type == "ambient_temp":
-                self._attr_native_value = self.charger.getAmbientTemperature()
-            elif sensor_type == "ir_temp":
-                self._attr_native_value = self.charger.getIRTemperature()
-            elif sensor_type == "rtc_temp":
-                self._attr_native_value = self.charger.getRTCTemperature()
-            elif sensor_type == "usage_session":
-                self._attr_native_value = float(self.charger.getUsageSession()) / 1000
-            elif sensor_type == "usage_total":
-                self._attr_native_value = float(self.charger.getUsageTotal()) / 1000
-            else:
-                self._attr_native_value = "Unknown"
+            match sensor_type:
+                case "ambient_temp":
+                    self._attr_native_value = self.charger.ambient_temperature
+                case "charge_time":
+                    self._attr_native_value = (
+                        self.charger.charge_time_elapsed / 60
+                    )  # Convert seconds to minutes
+                case "charging_current":
+                    self._attr_native_value = self.charger.charging_current
+                case "charging_power":
+                    self._attr_native_value = (
+                        self.charger.charging_current * self.charger.charging_voltage
+                    )
+                case "charging_voltage":
+                    self._attr_native_value = self.charger.charging_voltage
+                case "current_capacity":
+                    self._attr_native_value = self.charger.current_capacity
+                case "ir_temp":
+                    self._attr_native_value = self.charger.ir_temperature
+                case "max_amps":
+                    self._attr_native_value = self.charger.max_amps
+                case "min_amps":
+                    self._attr_native_value = self.charger.min_amps
+                case "rtc_temp":
+                    self._attr_native_value = self.charger.rtc_temperature
+                case "status":
+                    self._attr_native_value = self.charger.status
+                case "usage_session":
+                    self._attr_native_value = (
+                        float(self.charger.usage_session) / 1000
+                    )  # Convert Wh to kWh
+                case "usage_total":
+                    self._attr_native_value = (
+                        float(self.charger.usage_total) / 1000
+                    )  # Convert Wh to kWh
+                case _:
+                    self._attr_native_value = "Unknown"
         except (RequestException, ValueError, KeyError):
             _LOGGER.warning("Could not update status for %s", self.name)
