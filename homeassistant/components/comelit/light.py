416c8ac1d70a375a0663443c9d8492248e6b93aa
Simone Chemelli <simone.chemelli@gmail.com>
2023-10-25 14:48:33 +0200

    Fix Comelit comments as per late review (#102783)

--- homeassistant/components/comelit/light.py
+++ homeassistant/components/comelit/light.py
@@ -47,7 +47,8 @@ class ComelitLightEntity(CoordinatorEntity[ComelitSerialBridge], LightEntity):
         self._api = coordinator.api
         self._device = device
         super().__init__(coordinator)
-        # Use config_entry.entry_id as base for unique_id because no serial number or mac is available
+        # Use config_entry.entry_id as base for unique_id
+        # because no serial number or mac is available
         self._attr_unique_id = f"{config_entry_entry_id}-{device.index}"
         self._attr_device_info = coordinator.platform_device_info(device)
 
