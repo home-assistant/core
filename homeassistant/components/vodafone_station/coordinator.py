c15f223d71c1153f211a4123033333eaea29d7f9
Simone Chemelli <simone.chemelli@gmail.com>
2023-10-25 07:41:55 +0000

    improve exception handling for Vodafone Station

--- homeassistant/components/vodafone_station/coordinator.py
+++ homeassistant/components/vodafone_station/coordinator.py
@@ -94,16 +94,20 @@ class VodafoneStationRouter(DataUpdateCoordinator[UpdateCoordinatorDataType]):
     async def _async_update_data(self) -> UpdateCoordinatorDataType:
         """Update router data."""
         _LOGGER.debug("Polling Vodafone Station host: %s", self._host)
+        logged = False
         try:
             logged = await self.api.login()
         except exceptions.CannotConnect as err:
             _LOGGER.warning("Connection error for %s", self._host)
+            await self.api.close()
             raise UpdateFailed(f"Error fetching data: {repr(err)}") from err
-        except exceptions.CannotAuthenticate as err:
-            raise ConfigEntryAuthFailed from err
-
-        if not logged:
-            raise ConfigEntryAuthFailed
+        except exceptions.AlreadyLogged as err:
+            _LOGGER.warning("User %s already logged. Retrying", self.api.username)
+            await self.api.close()
+            raise UpdateFailed(f"Error fetching data: {repr(err)}") from err
+        finally:
+            if not logged:
+                raise ConfigEntryAuthFailed
 
         utc_point_in_time = dt_util.utcnow()
         data_devices = {
