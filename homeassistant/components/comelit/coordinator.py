8b1b6b8d0b915e6496acc0557881bb087f46015e
Simone Chemelli <simone.chemelli@gmail.com>
2023-10-25 09:08:44 +0000

    Address late review comments for Comelit login

--- homeassistant/components/comelit/coordinator.py
+++ homeassistant/components/comelit/coordinator.py
@@ -68,15 +68,13 @@ class ComelitSerialBridge(DataUpdateCoordinator):
     async def _async_update_data(self) -> dict[str, Any]:
         """Update device data."""
         _LOGGER.debug("Polling Comelit Serial Bridge host: %s", self._host)
-        logged = False
         try:
-            logged = await self.api.login()
+            await self.api.login()
         except exceptions.CannotConnect as err:
             _LOGGER.warning("Connection error for %s", self._host)
             await self.api.close()
             raise UpdateFailed(f"Error fetching data: {repr(err)}") from err
-        finally:
-            if not logged:
-                raise ConfigEntryAuthFailed
+        except exceptions.CannotAuthenticate:
+            raise ConfigEntryAuthFailed
 
         return await self.api.get_all_devices()
