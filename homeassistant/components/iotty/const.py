2b2022ac7e2e342195b822ffd9739a28a7192d0e
Paolo Burgio <paolo.burgio@gmail.com>
2023-10-26 14:59:22 +0000

    Fixes to URL, and removed commits

--- homeassistant/components/iotty/const.py
+++ homeassistant/components/iotty/const.py
@@ -2,7 +2,7 @@
 
 DOMAIN = "iotty"
 
-OAUTH2_AUTHORIZE = "https:/auth.iotty.com/.auth/oauth2/login"
+OAUTH2_AUTHORIZE = "https://auth.iotty.com/.auth/oauth2/login"
 OAUTH2_TOKEN = "https://auth.iotty.com/.auth/oauth2/token"
 OAUTH2_CLIENT_ID = "hass-iotty"
 
