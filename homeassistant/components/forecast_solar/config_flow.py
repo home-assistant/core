cea23c5a9f98e82654196310435101208b839970
Ben Corrado <bencorrado@gmail.com>
2023-10-26 12:06:54 -0700

    Multiple-step config flow & az/dec sensor support

--- homeassistant/components/forecast_solar/config_flow.py
+++ homeassistant/components/forecast_solar/config_flow.py
@@ -13,12 +13,21 @@ from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import config_validation as cv
 
 from .const import (
+    CONF_ADJ,
     CONF_AZIMUTH,
+    CONF_AZIMUTH_CHOICE,
+    CONF_AZIMUTH_SENSOR,
     CONF_DAMPING_EVENING,
     CONF_DAMPING_MORNING,
     CONF_DECLINATION,
-    CONF_DYNAMIC_LOCATION,
+    CONF_DECLINATION_CHOICE,
+    CONF_DECLINATION_SENSOR,
+    CONF_FIXED,
+    CONF_HOME_LOCATION,
     CONF_INVERTER_SIZE,
+    CONF_LOCATION_CHOICE,
+    CONF_LOCATION_ZONE,
+    CONF_MANUAL_LOCATION,
     CONF_MODULES_POWER,
     DOMAIN,
 )
@@ -39,45 +48,215 @@ class ForecastSolarFlowHandler(ConfigFlow, domain=DOMAIN):
         """Get the options flow for this handler."""
         return ForecastSolarOptionFlowHandler(config_entry)
 
+    def __init__(self):
+        self.flow_data = {}
+
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle a flow initiated by the user."""
+
         if user_input is not None:
-            return self.async_create_entry(
-                title=user_input[CONF_NAME],
-                data={
-                    CONF_LATITUDE: user_input[CONF_LATITUDE],
-                    CONF_LONGITUDE: user_input[CONF_LONGITUDE],
-                    CONF_DYNAMIC_LOCATION: user_input[CONF_DYNAMIC_LOCATION],
-                },
-                options={
-                    CONF_AZIMUTH: user_input[CONF_AZIMUTH],
-                    CONF_DECLINATION: user_input[CONF_DECLINATION],
-                    CONF_MODULES_POWER: user_input[CONF_MODULES_POWER],
-                },
-            )
+            self.flow_data.update(user_input)
+            if user_input.get(CONF_LOCATION_CHOICE) == CONF_HOME_LOCATION:
+                return await self.async_step_declination()
+            elif user_input.get(CONF_LOCATION_CHOICE) == CONF_MANUAL_LOCATION:
+                return await self.async_step_manual_location()
+            elif user_input.get(CONF_LOCATION_CHOICE) == CONF_LOCATION_ZONE:
+                return await self.async_step_zone_location()
+
+        # Check if valid angle sensors exist.
+        angle_sensors = [
+            sensor
+            for sensor in self.hass.states.async_entity_ids("sensor")
+            if self.hass.states.get(sensor).attributes.get("unit_of_measurement")
+            in ["°", "degrees", "deg", "degree"]
+        ]
+        has_angle_sensors = bool(angle_sensors)
+
+        # Construct the choices for azimuth and declination based on the existence of valid sensors.
+        azimuth_choices = {CONF_FIXED: "Fixed Azimuth"}
+        declination_choices = {CONF_FIXED: "Fixed Declination"}
+
+        if has_angle_sensors:
+            azimuth_choices[CONF_ADJ] = "Adjustable Azimuth with a Sensor"
+            declination_choices[CONF_ADJ] = "Adjustable Declination with a Sensor"
+        location_choice_schema = vol.Schema(
+            {
+                vol.Required(CONF_NAME, default=self.hass.config.location_name): str,
+                vol.Required(CONF_LOCATION_CHOICE): vol.In(
+                    {
+                        CONF_HOME_LOCATION: "Use My Home Location",
+                        CONF_LOCATION_ZONE: "Select a Zone",
+                        CONF_MANUAL_LOCATION: "Input Latitude & Longitude Manually",
+                    }
+                ),
+                vol.Required(CONF_AZIMUTH_CHOICE): vol.In(azimuth_choices),
+                vol.Required(CONF_DECLINATION_CHOICE): vol.In(declination_choices),
+            }
+        )
+
+        location_choice_schema = vol.Schema(
+            {
+                vol.Required(CONF_NAME, default=self.hass.config.location_name): str,
+                vol.Required(CONF_LOCATION_CHOICE): vol.In(
+                    {
+                        CONF_HOME_LOCATION: "Use My Home Location",
+                        CONF_LOCATION_ZONE: "Select a Zone",
+                        CONF_MANUAL_LOCATION: "Input Latitude & Longitude Manually",
+                    }
+                ),
+                vol.Required(CONF_AZIMUTH_CHOICE): vol.In(
+                    {
+                        CONF_FIXED: "Fixed Azimuth",
+                        CONF_ADJ: "Adjustable Azimuth with a Sensor",
+                    }
+                ),
+                vol.Required(CONF_DECLINATION_CHOICE): vol.In(
+                    {
+                        CONF_FIXED: "Fixed Declination",
+                        CONF_ADJ: "Adjustable Declination with a Sensor",
+                    }
+                ),
+            }
+        )
+
+        return self.async_show_form(step_id="user", data_schema=location_choice_schema)
+
+    async def async_step_zone_location(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the zone location step."""
+        zones = await self.hass.async_add_executor_job(
+            self.hass.states.entity_ids, "zone"
+        )
+
+        if user_input is not None:
+            previous_input = self.flow_data
+            self.flow_data = {**previous_input, **user_input}
+            # Proceed to the next step with the selected zone.
+            return await self.async_step_declination()
+        return self.async_show_form(
+            step_id="zone_location",
+            data_schema=vol.Schema({vol.Required(CONF_LOCATION_ZONE): vol.In(zones)}),
+        )
+
+    async def async_step_manual_location(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle manual location input."""
+
+        if user_input is not None:
+            previous_input = self.flow_data
+            self.flow_data = {**previous_input, **user_input}
+            # Proceed to the next step with the manually inputted data.
+            return await self.async_step_declination()
+        manual_location_schema = vol.Schema(
+            {
+                vol.Required(CONF_LATITUDE): cv.latitude,
+                vol.Required(CONF_LONGITUDE): cv.longitude,
+            }
+        )
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
+            step_id="manual_location", data_schema=manual_location_schema
+        )
+
+    async def async_step_declination(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the declination step."""
+        errors = {}
+        previous_input = self.flow_data
+        angle_sensors = [
+            sensor
+            for sensor in self.hass.states.async_entity_ids("sensor")
+            if self.hass.states.get(sensor).attributes.get("unit_of_measurement")
+            in ["°", "degrees", "deg", "degree"]
+        ]
+
+        if previous_input.get(CONF_DECLINATION_CHOICE) == CONF_ADJ:
+            declination_schema = vol.Schema(
+                {vol.Required(CONF_DECLINATION_SENSOR): vol.In(angle_sensors)}
+            )
+        else:
+            declination_schema = vol.Schema(
                 {
-                    vol.Required(
-                        CONF_NAME, default=self.hass.config.location_name
-                    ): str,
-                    vol.Required(
-                        CONF_LATITUDE, default=self.hass.config.latitude
-                    ): cv.latitude,
-                    vol.Required(
-                        CONF_LONGITUDE, default=self.hass.config.longitude
-                    ): cv.longitude,
-                    vol.Required(CONF_DYNAMIC_LOCATION, default=False): bool,
                     vol.Required(CONF_DECLINATION, default=25): vol.All(
                         vol.Coerce(int), vol.Range(min=0, max=90)
-                    ),
+                    )
+                }
+            )
+        if user_input is not None:
+            self.flow_data = {**previous_input, **user_input}
+            # Proceed to the next step
+            return await self.async_step_azimuth()
+        return self.async_show_form(
+            step_id="declination", data_schema=declination_schema, errors=errors
+        )
+
+    async def async_step_azimuth(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the azimuth step."""
+        errors = {}
+        previous_input = self.flow_data
+        angle_sensors = [
+            sensor
+            for sensor in self.hass.states.async_entity_ids("sensor")
+            if self.hass.states.get(sensor).attributes.get("unit_of_measurement")
+            in ["°", "degrees", "deg", "degree"]
+        ]
+
+        if previous_input.get(CONF_AZIMUTH_CHOICE) == CONF_ADJ:
+            azimuth_schema = vol.Schema(
+                {vol.Required(CONF_AZIMUTH_SENSOR): vol.In(angle_sensors)}
+            )
+        else:
+            azimuth_schema = vol.Schema(
+                {
                     vol.Required(CONF_AZIMUTH, default=180): vol.All(
                         vol.Coerce(int), vol.Range(min=0, max=360)
                     ),
+                }
+            )
+        if user_input is not None:
+            self.flow_data = {**previous_input, **user_input}
+            # Proceed to the next step
+            return await self.async_step_wattage()
+        return self.async_show_form(
+            step_id="azimuth", data_schema=azimuth_schema, errors=errors
+        )
+
+    async def async_step_wattage(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the wattage step."""
+
+        if user_input is not None:
+            previous_input = self.flow_data
+            return self.async_create_entry(
+                title=previous_input[CONF_NAME],
+                data={
+                    CONF_HOME_LOCATION: previous_input.get(CONF_HOME_LOCATION),
+                    CONF_LOCATION_ZONE: previous_input.get(CONF_LOCATION_ZONE),
+                    CONF_LATITUDE: previous_input.get(CONF_LATITUDE),
+                    CONF_LONGITUDE: previous_input.get(CONF_LONGITUDE),
+                },
+                options={
+                    CONF_DECLINATION: previous_input.get(CONF_DECLINATION),
+                    CONF_DECLINATION_SENSOR: previous_input.get(
+                        CONF_DECLINATION_SENSOR
+                    ),
+                    CONF_AZIMUTH: previous_input.get(CONF_AZIMUTH),
+                    CONF_AZIMUTH_SENSOR: previous_input.get(CONF_AZIMUTH_SENSOR),
+                    CONF_MODULES_POWER: user_input.get(CONF_MODULES_POWER),
+                },
+            )
+        return self.async_show_form(
+            step_id="wattage",
+            data_schema=vol.Schema(
+                {
                     vol.Required(CONF_MODULES_POWER): vol.Coerce(int),
                 }
             ),
@@ -105,52 +284,74 @@ class ForecastSolarOptionFlowHandler(OptionsFlow):
                 return self.async_create_entry(
                     title="", data=user_input | {CONF_API_KEY: api_key or None}
                 )
+        # Check if the declination and azimuth sensors are set.
+        declination_sensor = self.config_entry.options.get(CONF_DECLINATION_SENSOR)
+        azimuth_sensor = self.config_entry.options.get(CONF_AZIMUTH_SENSOR)
 
+        # Generate the schema dynamically based on the sensor values.
+        options_schema = {
+            vol.Optional(
+                CONF_API_KEY,
+                description={
+                    "suggested_value": self.config_entry.options.get(CONF_API_KEY, "")
+                },
+            ): str,
+            vol.Required(
+                CONF_MODULES_POWER,
+                default=self.config_entry.options[CONF_MODULES_POWER],
+            ): vol.Coerce(int),
+            vol.Optional(
+                CONF_DAMPING_MORNING,
+                default=self.config_entry.options.get(CONF_DAMPING_MORNING, 0.0),
+            ): vol.Coerce(float),
+            vol.Optional(
+                CONF_DAMPING_EVENING,
+                default=self.config_entry.options.get(CONF_DAMPING_EVENING, 0.0),
+            ): vol.Coerce(float),
+            vol.Optional(
+                CONF_INVERTER_SIZE,
+                description={
+                    "suggested_value": self.config_entry.options.get(CONF_INVERTER_SIZE)
+                },
+            ): vol.Coerce(int),
+        }
+        angle_sensors = [
+            sensor
+            for sensor in self.hass.states.async_entity_ids("sensor")
+            if self.hass.states.get(sensor).attributes.get("unit_of_measurement")
+            in ["°", "degrees", "deg", "degree"]
+        ]
+        declination_sensors = angle_sensors.copy()
+
+        if declination_sensor and declination_sensor in angle_sensors:
+            declination_sensors.remove(declination_sensor)
+            declination_sensors.insert(0, declination_sensor)
+
+        if azimuth_sensor and azimuth_sensor in angle_sensors:
+            angle_sensors.remove(azimuth_sensor)
+            angle_sensors.insert(0, azimuth_sensor)
+
+        if declination_sensor:
+            options_schema[vol.Required(CONF_DECLINATION_SENSOR)] = vol.In(
+                declination_sensors
+            )
+        else:
+            options_schema[
+                vol.Required(
+                    CONF_DECLINATION,
+                    default=self.config_entry.options[CONF_DECLINATION],
+                )
+            ] = vol.All(vol.Coerce(int), vol.Range(min=0, max=90))
+        if azimuth_sensor:
+            options_schema[vol.Required(CONF_AZIMUTH_SENSOR)] = vol.In(angle_sensors)
+        else:
+            options_schema[
+                vol.Required(
+                    CONF_AZIMUTH, default=self.config_entry.options.get(CONF_AZIMUTH)
+                )
+            ] = vol.All(vol.Coerce(int), vol.Range(min=0, max=360))
         return self.async_show_form(
             step_id="init",
-            data_schema=vol.Schema(
-                {
-                    vol.Optional(
-                        CONF_API_KEY,
-                        description={
-                            "suggested_value": self.config_entry.options.get(
-                                CONF_API_KEY, ""
-                            )
-                        },
-                    ): str,
-                    vol.Required(
-                        CONF_DECLINATION,
-                        default=self.config_entry.options[CONF_DECLINATION],
-                    ): vol.All(vol.Coerce(int), vol.Range(min=0, max=90)),
-                    vol.Required(
-                        CONF_AZIMUTH,
-                        default=self.config_entry.options.get(CONF_AZIMUTH),
-                    ): vol.All(vol.Coerce(int), vol.Range(min=-0, max=360)),
-                    vol.Required(
-                        CONF_MODULES_POWER,
-                        default=self.config_entry.options[CONF_MODULES_POWER],
-                    ): vol.Coerce(int),
-                    vol.Optional(
-                        CONF_DAMPING_MORNING,
-                        default=self.config_entry.options.get(
-                            CONF_DAMPING_MORNING, 0.0
-                        ),
-                    ): vol.Coerce(float),
-                    vol.Optional(
-                        CONF_DAMPING_EVENING,
-                        default=self.config_entry.options.get(
-                            CONF_DAMPING_EVENING, 0.0
-                        ),
-                    ): vol.Coerce(float),
-                    vol.Optional(
-                        CONF_INVERTER_SIZE,
-                        description={
-                            "suggested_value": self.config_entry.options.get(
-                                CONF_INVERTER_SIZE
-                            )
-                        },
-                    ): vol.Coerce(int),
-                }
-            ),
+            data_schema=vol.Schema(options_schema),
             errors=errors,
         )
--- homeassistant/components/forecast_solar/coordinator.py
+++ homeassistant/components/forecast_solar/coordinator.py
@@ -9,15 +9,19 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers.event import async_call_later
 
 from .const import (
     CONF_AZIMUTH,
+    CONF_AZIMUTH_SENSOR,
     CONF_DAMPING_EVENING,
     CONF_DAMPING_MORNING,
     CONF_DECLINATION,
-    CONF_DYNAMIC_LOCATION,
+    CONF_DECLINATION_SENSOR,
+    CONF_HOME_LOCATION,
     CONF_INVERTER_SIZE,
+    CONF_LOCATION_ZONE,
     CONF_MODULES_POWER,
     DOMAIN,
     LOGGER,
@@ -26,6 +30,10 @@ from .const import (
 
 class ForecastSolarDataUpdateCoordinator(DataUpdateCoordinator[Estimate]):
     """The Forecast.Solar Data Update Coordinator."""
+
+    async def retry_setup(self, now=None):
+        """Retry setup if zone is not yet available."""
+        await self.async_config_entry_first_refresh()
 
     config_entry: ConfigEntry
 
@@ -41,21 +49,82 @@ class ForecastSolarDataUpdateCoordinator(DataUpdateCoordinator[Estimate]):
             inverter_size := entry.options.get(CONF_INVERTER_SIZE)
         ) is not None and inverter_size > 0:
             inverter_size = inverter_size / 1000
-
-        if entry.data.get(CONF_DYNAMIC_LOCATION):
+        if entry.data.get(CONF_HOME_LOCATION):
             latitude = hass.config.latitude
             longitude = hass.config.longitude
+        elif entry.data.get(CONF_LOCATION_ZONE):
+            zone = hass.states.get(entry.data.get(CONF_LOCATION_ZONE))
+            if (
+                zone is None
+                or "latitude" not in zone.attributes
+                or "longitude" not in zone.attributes
+            ):
+                LOGGER.debug(
+                    "Zone entity not yet available or missing latitude/longitude attributes."
+                )
+                async_call_later(hass, 10, self.retry_setup)
+                return False
+            latitude = zone.attributes["latitude"]
+            longitude = zone.attributes["longitude"]
         else:
             latitude = entry.data[CONF_LATITUDE]
             longitude = entry.data[CONF_LONGITUDE]
-
+        self.sensors_unavailable = False
+        if entry.options.get(CONF_DECLINATION_SENSOR):
+            declination_sensor = hass.states.get(
+                entry.options.get(CONF_DECLINATION_SENSOR)
+            )
+            if (
+                declination_sensor is None
+                or declination_sensor.state in ["unavailable", "unknown", None]
+                or not declination_sensor.state.replace(".", "", 1).isdigit()
+            ):
+                LOGGER.debug(
+                    "Sensor %s not available or in 'unavailable' state",
+                    entry.options.get(CONF_DECLINATION_SENSOR),
+                )
+                declination = 0
+                self.sensors_unavailable = True
+            else:
+                declination = float(declination_sensor.state)
+                if declination < 0 or declination > 90:
+                    LOGGER.debug(
+                        "Invalid declination value: %.3f.. Expected value between 0 and 90.",
+                        declination,
+                    )
+                    self.sensors_unavailable = True
+        else:
+            declination = entry.options[CONF_DECLINATION]
+        if entry.options.get(CONF_AZIMUTH_SENSOR):
+            azimuth_sensor = hass.states.get(entry.options.get(CONF_AZIMUTH_SENSOR))
+            if (
+                azimuth_sensor is None
+                or azimuth_sensor.state in ["unavailable", "unknown", None]
+                or not azimuth_sensor.state.replace(".", "", 1).isdigit()
+            ):
+                LOGGER.debug(
+                    "Sensor %s not available or in 'unavailable' state.",
+                    entry.options.get(CONF_AZIMUTH_SENSOR),
+                )
+                azimuth = 0
+                self.sensors_unavailable = True
+            else:
+                azimuth = float(azimuth_sensor.state) - 180
+                if azimuth < -180 or azimuth > 180:
+                    LOGGER.debug(
+                        "Invalid azimuth value: %.3f. Expected value between 0 and 360.",
+                        azimuth + 180,
+                    )
+                    self.sensors_unavailable = True
+        else:
+            azimuth = entry.options[CONF_AZIMUTH] - 180
         self.forecast = ForecastSolar(
             api_key=api_key,
             session=async_get_clientsession(hass),
             latitude=latitude,
             longitude=longitude,
-            declination=entry.options[CONF_DECLINATION],
-            azimuth=(entry.options[CONF_AZIMUTH] - 180),
+            declination=declination,
+            azimuth=azimuth,
             kwp=(entry.options[CONF_MODULES_POWER] / 1000),
             damping_morning=entry.options.get(CONF_DAMPING_MORNING, 0.0),
             damping_evening=entry.options.get(CONF_DAMPING_EVENING, 0.0),
@@ -72,4 +141,9 @@ class ForecastSolarDataUpdateCoordinator(DataUpdateCoordinator[Estimate]):
 
     async def _async_update_data(self) -> Estimate:
         """Fetch Forecast.Solar estimates."""
+        # Check if the required sensors are unavailable or have invalid values.
+        if self.sensors_unavailable:
+            raise UpdateFailed(
+                "Required sensors are unavailable or have invalid values."
+            )
         return await self.forecast.estimate()
