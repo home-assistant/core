11e1d499f24782a5d4165fb7146472d4cfb4c4bc
Joshua Harley <gibwar@gibixonline.com>
2023-10-26 22:41:03 -0600

    Fix utility meter reset without tarrifs
    When using a utility_meter helper set up with a "Meter reset cycle" of
    "No cycle" it is impossible to properly reset the meter to 0 and utilize
    the "last reset" and "last period" attributes on the helper.
    
    With these changes the service call can now directly reset a meter in
    such a configuration and the UI selector has been updated to include
    utility meters in the selection list.
    
    Fixes #96920

--- homeassistant/components/utility_meter/sensor.py
+++ homeassistant/components/utility_meter/sensor.py
@@ -570,7 +570,7 @@ class UtilityMeterSensor(RestoreSensor):
 
     async def async_reset_meter(self, entity_id):
         """Reset meter."""
-        if self._tariff_entity != entity_id:
+        if entity_id not in {self._tariff_entity, self.entity_id}:
             return
         _LOGGER.debug("Reset utility meter <%s>", self.entity_id)
         self._last_reset = dt_util.utcnow()
