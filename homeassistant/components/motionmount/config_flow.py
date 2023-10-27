f81e6d99efe3ac1df6e359d4784ec7e922dc245b
Remco Poelstra <r.poelstra@vogels.com>
2023-10-27 13:12:38 +0200

    Use a mac address as a unique id

--- homeassistant/components/motionmount/config_flow.py
+++ homeassistant/components/motionmount/config_flow.py
@@ -7,14 +7,22 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.components import zeroconf
-from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT, CONF_UUID
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers.device_registry import format_mac
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+EMPTY_MAC = "00:00:00:00:00:00"
 
 
+# A MotionMount can be in four states:
+# 1. Old CE and old Pro FW -> It doesn't supply any kind of mac
+# 2. Old CE but new Pro FW -> It supplies its mac using DNS-SD, but a read of the mac fails
+# 3. New CE but old Pro FW -> It doesn't supply the mac using DNS-SD but we can read it
+# 4. New CE and new Pro FW -> Both DNS-SD and a read gives us the mac
+# If we can't get the mac, we use DEFAULT_DISCOVERY_UNIQUE_ID as an ID, so we can always configure a single MotionMount. Most households will only have a single MotionMount
 class MotionMountFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a Vogel's MotionMount config flow."""
 
@@ -31,8 +39,9 @@ class MotionMountFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is None:
             return self._show_setup_form()
 
+        info = {}
         try:
-            await self._validate_input(user_input)
+            info = await self._validate_input(user_input)
         except ConnectionError:
             return self.async_abort(reason="cannot_connect")
         except TimeoutError:
@@ -40,27 +49,44 @@ class MotionMountFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         except motionmount.NotConnectedError:
             return self.async_abort(reason="not_connected")
         except motionmount.MotionMountResponseError:
-            return self.async_abort(reason="invalid_response")
+            # This is most likely due to missing support for the mac address property
+            # Abort if the handler has config entries already
+            if self._async_current_entries():
+                return self.async_abort(reason="already_configured")
 
-        await self._async_set_unique_id_and_abort_if_already_configured(
-            user_input[CONF_HOST]
+            # Otherwise we try to continue with the generic uid
+            info[CONF_UUID] = config_entries.DEFAULT_DISCOVERY_UNIQUE_ID
+
+        # If the device mac is valid we use it, otherwise we use the default id
+        if info.get(CONF_UUID, EMPTY_MAC) != EMPTY_MAC:
+            unique_id = info[CONF_UUID]
+        else:
+            unique_id = config_entries.DEFAULT_DISCOVERY_UNIQUE_ID
+
+        name = info.get(CONF_NAME, user_input[CONF_HOST])
+
+        await self.async_set_unique_id(unique_id)
+        self._abort_if_unique_id_configured(
+            updates={
+                CONF_HOST: user_input[CONF_HOST],
+                CONF_PORT: user_input[CONF_PORT],
+                CONF_NAME: name,
+            }
         )
 
-        return self.async_create_entry(title=user_input[CONF_HOST], data=user_input)
+        return self.async_create_entry(title=name, data=user_input)
 
     async def async_step_zeroconf(
         self, discovery_info: zeroconf.ZeroconfServiceInfo
     ) -> FlowResult:
         """Handle zeroconf discovery."""
-        host = discovery_info.hostname
-
-        # Avoid probing devices that already have an entry
-        self._async_abort_entries_match({CONF_HOST: host})
 
         # Extract information from discovery
+        host = discovery_info.hostname
         port = discovery_info.port
         zctype = discovery_info.type
         name = discovery_info.name.replace(f".{zctype}", "")
+        unique_id = discovery_info.properties.get("mac")
 
         self.discovery_info.update(
             {
@@ -70,12 +96,19 @@ class MotionMountFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             }
         )
 
-        await self._async_set_unique_id_and_abort_if_already_configured(host)
+        if unique_id:
+            # If we already have the unique id, try to set it now
+            # so we can avoid probing the device if its already
+            # configured or ignored
+            await self.async_set_unique_id(unique_id)
+            self._abort_if_unique_id_configured(
+                updates={CONF_HOST: host, CONF_PORT: port, CONF_NAME: name}
+            )
 
         self.context.update({"title_placeholders": {"name": name}})
 
         try:
-            await self._validate_input(self.discovery_info)
+            info = await self._validate_input(self.discovery_info)
         except ConnectionError:
             return self.async_abort(reason="cannot_connect")
         except TimeoutError:
@@ -83,8 +116,20 @@ class MotionMountFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         except motionmount.NotConnectedError:
             return self.async_abort(reason="not_connected")
         except motionmount.MotionMountResponseError:
-            return self.async_abort(reason="invalid_response")
+            info = {}
+            # We continue as we want to be able to connect with older FW that does not support MAC address
 
+        # If the device supplied as with a valid MAC we use that
+        if (CONF_UUID in info) and info[CONF_UUID] != "00:00:00:00:00:00":
+            unique_id = info[CONF_UUID]
+
+        if unique_id:
+            await self.async_set_unique_id(unique_id)
+            self._abort_if_unique_id_configured(
+                updates={CONF_HOST: host, CONF_PORT: port, CONF_NAME: name}
+            )
+
+        await self._async_handle_discovery_without_unique_id()
         return await self.async_step_zeroconf_confirm()
 
     async def async_step_zeroconf_confirm(
@@ -103,19 +148,18 @@ class MotionMountFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             data=self.discovery_info,
         )
 
-    async def _async_set_unique_id_and_abort_if_already_configured(
-        self, unique_id: str
-    ) -> None:
-        """Set the unique ID and abort if already configured."""
-        await self.async_set_unique_id(unique_id)
-        self._abort_if_unique_id_configured()
-
-    async def _validate_input(self, data: dict):
+    async def _validate_input(self, data: dict) -> dict[str, Any]:
         """Validate the user input allows us to connect."""
 
         mm = motionmount.MotionMount(data[CONF_HOST], data[CONF_PORT])
-        await mm.connect()
-        await mm.disconnect()
+        try:
+            await mm.connect()
+            mac = await mm.get_mac()
+            name = await mm.get_name()
+        finally:
+            await mm.disconnect()
+
+        return {CONF_UUID: format_mac(mac.hex()), CONF_NAME: name}
 
     def _show_setup_form(self, errors: dict | None = None) -> FlowResult:
         """Show the setup form to the user."""
