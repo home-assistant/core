979da64bb02bd17b5223a9aab3dd29a7a36d4ee7
Brett Adams <brett.whynot@gmail.com>
2023-10-27 23:33:49 +0000

    Add current_temperature

--- homeassistant/components/advantage_air/climate.py
+++ homeassistant/components/advantage_air/climate.py
@@ -123,6 +123,14 @@ class AdvantageAirAC(AdvantageAirAcEntity, ClimateEntity):
             self._attr_fan_modes += [FAN_AUTO]
 
     @property
+    def current_temperature(self) -> float | None:
+        """Return the selected zones current temperature."""
+        # This works when using MyZone, MyTemp, or MyAuto
+        if self._myzone:
+            return self._myzone["measuredTemp"]
+        return None
+
+    @property
     def target_temperature(self) -> float | None:
         """Return the current target temperature."""
         # If the system is in MyZone mode, and a zone is set, return that temperature instead.
