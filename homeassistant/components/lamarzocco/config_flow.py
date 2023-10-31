4f4f9c177f391096c3c43f1911fdbe0d4bfd417a
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-10-31 09:50:55 +0000

    send out full user input

--- homeassistant/components/lamarzocco/config_flow.py
+++ homeassistant/components/lamarzocco/config_flow.py
@@ -258,7 +258,7 @@ class LmOptionsFlowHandler(config_entries.OptionsFlowWithConfigEntry):
             if not errors:
                 self.hass.config_entries.async_update_entry(
                     self.config_entry,
-                    data=user_input,
+                    data=self.config_entry.data | user_input,
                     options=self.config_entry.options,
                 )
                 return self.async_create_entry(title="", data=user_input)
