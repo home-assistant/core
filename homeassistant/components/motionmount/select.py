ae0af8bfd83a91ca3850d81124a662b70e20e88f
Remco Poelstra <r.poelstra@vogels.com>
2023-11-02 16:05:00 +0100

    Preset selector now updates when a preset is chosen

--- homeassistant/components/motionmount/select.py
+++ homeassistant/components/motionmount/select.py
@@ -1,11 +1,12 @@
 """Support for MotionMount numeric control."""
+import motionmount  # type: ignore[import]
+
 from homeassistant.components.select import SelectEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
-from .entity import MotionMountEntity
 
 
 async def async_setup_entry(
@@ -18,37 +19,47 @@ async def async_setup_entry(
 
     async_add_entities(
         [
-            MotionMountPresets(coordinator, entry.entry_id, presets),
+            MotionMountPresets(coordinator.mm, entry.entry_id, presets),
         ]
     )
 
 
-class MotionMountPresets(MotionMountEntity, SelectEntity):
+class MotionMountPresets(SelectEntity):
     """The presets of a MotionMount."""
 
+    _attr_should_poll = False
+    _attr_has_entity_name = True
     _attr_name = "Preset"
 
-    def __init__(self, coordinator, unique_id, presets):
+    def __init__(
+        self,
+        mm: motionmount.MotionMount,
+        unique_id: str,
+        presets: dict[int, str],
+    ) -> None:
         """Initialize Extension number."""
-        super().__init__(coordinator)
         self._attr_unique_id = f"{unique_id}-preset"
+        self._mm = mm
 
+        self._update_options(presets)
+        self._attr_current_option = self._attr_options[0]
+
+    def _update_options(self, presets: dict[int, str]) -> None:
+        """Convert preset to select options."""
         options = ["0: Wall"]
         for index, name in presets.items():
             options.append(f"{index}: {name}")
 
         self._attr_options = options
-        self._attr_current_option = options[0]
 
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        #        self._attr_current_option
-        #        self.async_write_ha_state()
-        return
+    async def async_update(self) -> None:
+        """Get latest state from MotionMount."""
+        presets = await self._mm.get_presets()
+        self._update_options(presets)
 
     async def async_select_option(self, option: str) -> None:
         """Set the new option."""
         index = int(option[:1])
-        await self.coordinator.mm.go_to_preset(index)
+        await self._mm.go_to_preset(index)
         self._attr_current_option = option
-        self.async_write_ha_state()
+        self.async_schedule_update_ha_state(True)
