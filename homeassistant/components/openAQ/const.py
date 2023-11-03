09c224ee3f56fff6dd3f55ffd1eca606b2616fd0
OscarSanner <o.sanner@hotmail.com>
2023-11-03 12:45:29 +0000

    sad

---
+++ homeassistant/components/openAQ/const.py
@@ -0,0 +1,14 @@
+"""Define AirVisual constants."""
+import logging
+
+DOMAIN = "airvisual"
+LOGGER = logging.getLogger(__package__)
+
+INTEGRATION_TYPE_GEOGRAPHY_COORDS = "Geographical Location by Latitude/Longitude"
+INTEGRATION_TYPE_GEOGRAPHY_NAME = "Geographical Location by Name"
+INTEGRATION_TYPE_NODE_PRO = "AirVisual Node/Pro"
+
+CONF_CITY = "city"
+CONF_COUNTRY = "country"
+CONF_GEOGRAPHIES = "geographies"
+CONF_INTEGRATION_TYPE = "integration_type"
