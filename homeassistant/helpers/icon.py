f77219286a45d58d6ab07df6d1440639dbf9667f
Franck Nijhof <git@frenck.dev>
2023-11-03 11:38:36 +0100

    Add icon translations helper

--- homeassistant/helpers/icon.py
+++ homeassistant/helpers/icon.py
@@ -1,7 +1,188 @@
 """Icon helper methods."""
 from __future__ import annotations
 
+import asyncio
+from collections.abc import Iterable
 from functools import lru_cache
+import logging
+from typing import Any
+
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.loader import Integration, async_get_integrations
+from homeassistant.util.json import load_json
+
+from .translation import build_resources, recursive_flatten
+
+ICON_LOAD_LOCK = "icon_load_lock"
+ICON_FLATTEN_CACHE = "icon_flatten_cache"
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@callback
+def component_icons_path(component: str, integration: Integration) -> str | None:
+    """Return the icons json file location for a component.
+
+    Ex: components/hue/icons.json
+    If component is just a single file, will return None.
+    """
+    domain = component.rpartition(".")[-1]
+
+    # If it's a component that is just one file, we don't support icons
+    # Example custom_components/my_component.py
+    if integration.file_path.name != domain:
+        return None
+
+    return str(integration.file_path / "icons.json")
+
+
+def load_icons_files(icons_files: dict[str, str]) -> dict[str, dict[str, Any]]:
+    """Load and parse icons.json files."""
+    loaded = {}
+    for component, icons_file in icons_files.items():
+        loaded_json = load_json(icons_file)
+
+        if not isinstance(loaded_json, dict):
+            _LOGGER.warning(
+                "Icons file is unexpected type %s. Expected dict for %s",
+                type(loaded_json),
+                icons_file,
+            )
+            continue
+
+        loaded[component] = loaded_json
+
+    return loaded
+
+
+async def _async_get_component_icons(
+    hass: HomeAssistant,
+    components: set[str],
+    integrations: dict[str, Integration],
+) -> dict[str, Any]:
+    """Load icons."""
+    icons: dict[str, Any] = {}
+
+    # Determine files to load
+    files_to_load = {}
+    for loaded in components:
+        domain = loaded.rpartition(".")[-1]
+        if (path := component_icons_path(loaded, integrations[domain])) is None:
+            icons[loaded] = {}
+        else:
+            files_to_load[loaded] = path
+
+    if not files_to_load:
+        return icons
+
+    # Load files
+    if load_icons_job := hass.async_add_executor_job(load_icons_files, files_to_load):
+        icons |= await load_icons_job
+
+    return icons
+
+
+class _IconsCache:
+    """Cache for flattened icons."""
+
+    __slots__ = ("hass", "loaded", "cache")
+
+    def __init__(self, hass: HomeAssistant) -> None:
+        """Initialize the cache."""
+        self.hass = hass
+        self.loaded: set[str] = set()
+        self.cache: dict[str, dict[str, Any]] = {}
+
+    async def async_fetch(
+        self,
+        category: str,
+        components: set[str],
+    ) -> list[dict[str, dict[str, Any]]]:
+        """Load resources into the cache."""
+        if components_to_load := components - self.loaded:
+            await self._async_load(components_to_load)
+
+        return [
+            self.cache.get(component, {}).get(category, {}) for component in components
+        ]
+
+    async def _async_load(self, components: set[str]) -> None:
+        """Populate the cache for a given set of components."""
+        _LOGGER.debug(
+            "Cache miss for: %s",
+            ", ".join(components),
+        )
+
+        integrations: dict[str, Integration] = {}
+        domains = list({loaded.rpartition(".")[-1] for loaded in components})
+        ints_or_excs = await async_get_integrations(self.hass, domains)
+        for domain, int_or_exc in ints_or_excs.items():
+            if isinstance(int_or_exc, Exception):
+                raise int_or_exc
+            integrations[domain] = int_or_exc
+
+        icons = await _async_get_component_icons(self.hass, components, integrations)
+
+        self._build_category_cache(components, icons)
+
+        self.loaded.update(components)
+
+    @callback
+    def _build_category_cache(
+        self,
+        components: set[str],
+        icons: dict[str, dict[str, Any]],
+    ) -> None:
+        """Extract resources into the cache."""
+        resource: dict[str, Any] | str
+        categories: set[str] = set()
+        for resource in icons.values():
+            categories.update(resource)
+
+        for category in categories:
+            new_resources = build_resources(icons, components, category)
+            for component, resource in new_resources.items():
+                category_cache: dict[str, Any] = self.cache.setdefault(
+                    component, {}
+                ).setdefault(category, {})
+
+                if isinstance(resource, dict):
+                    category_cache |= recursive_flatten(
+                        f"component.{component}.{category}.",
+                        resource,
+                    )
+                else:
+                    category_cache[f"component.{component}.{category}"] = resource
+
+
+async def async_get_icons(
+    hass: HomeAssistant,
+    category: str,
+    integrations: Iterable[str] | None = None,
+) -> dict[str, Any]:
+    """Return all icons of integrations.
+
+    If integration specified, load it for that one; otherwise default to loaded
+    intgrations.
+    """
+    lock = hass.data.setdefault(ICON_LOAD_LOCK, asyncio.Lock())
+
+    if integrations is not None:
+        components = set(integrations)
+    else:
+        components = set(hass.config.components)
+
+    async with lock:
+        if ICON_FLATTEN_CACHE in hass.data:
+            cache = hass.data[ICON_FLATTEN_CACHE]
+        else:
+            cache = hass.data[ICON_FLATTEN_CACHE] = _IconsCache(hass)
+        cached = await cache.async_fetch(category, components)
+
+    result: dict[str, Any] = {}
+    for entry in cached:
+        result |= entry
+    return result
 
 
 @lru_cache
--- tests/helpers/test_icon.py
+++ tests/helpers/test_icon.py
@@ -1,18 +1,26 @@
 """Test Home Assistant icon util methods."""
 
+import asyncio
+import pathlib
+from unittest.mock import Mock, patch
+
+import pytest
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import icon
+from homeassistant.setup import async_setup_component
+
 
 def test_battery_icon() -> None:
     """Test icon generator for battery sensor."""
-    from homeassistant.helpers.icon import icon_for_battery_level
+    assert icon.icon_for_battery_level(None, True) == "mdi:battery-unknown"
+    assert icon.icon_for_battery_level(None, False) == "mdi:battery-unknown"
 
-    assert icon_for_battery_level(None, True) == "mdi:battery-unknown"
-    assert icon_for_battery_level(None, False) == "mdi:battery-unknown"
+    assert icon.icon_for_battery_level(5, True) == "mdi:battery-outline"
+    assert icon.icon_for_battery_level(5, False) == "mdi:battery-alert"
 
-    assert icon_for_battery_level(5, True) == "mdi:battery-outline"
-    assert icon_for_battery_level(5, False) == "mdi:battery-alert"
-
-    assert icon_for_battery_level(100, True) == "mdi:battery-charging-100"
-    assert icon_for_battery_level(100, False) == "mdi:battery"
+    assert icon.icon_for_battery_level(100, True) == "mdi:battery-charging-100"
+    assert icon.icon_for_battery_level(100, False) == "mdi:battery"
 
     iconbase = "mdi:battery"
     for level in range(0, 100, 5):
@@ -20,8 +28,8 @@ def test_battery_icon() -> None:
             "Level: %d. icon: %s, charging: %s"
             % (
                 level,
-                icon_for_battery_level(level, False),
-                icon_for_battery_level(level, True),
+                icon.icon_for_battery_level(level, False),
+                icon.icon_for_battery_level(level, True),
             )
         )
         if level <= 10:
@@ -42,17 +50,147 @@ def test_battery_icon() -> None:
             postfix = "-alert"
         else:
             postfix = ""
-        assert iconbase + postfix == icon_for_battery_level(level, False)
-        assert iconbase + postfix_charging == icon_for_battery_level(level, True)
+        assert iconbase + postfix == icon.icon_for_battery_level(level, False)
+        assert iconbase + postfix_charging == icon.icon_for_battery_level(level, True)
 
 
 def test_signal_icon() -> None:
     """Test icon generator for signal sensor."""
-    from homeassistant.helpers.icon import icon_for_signal_level
+    assert icon.icon_for_signal_level(None) == "mdi:signal-cellular-outline"
+    assert icon.icon_for_signal_level(0) == "mdi:signal-cellular-outline"
+    assert icon.icon_for_signal_level(5) == "mdi:signal-cellular-1"
+    assert icon.icon_for_signal_level(40) == "mdi:signal-cellular-2"
+    assert icon.icon_for_signal_level(80) == "mdi:signal-cellular-3"
+    assert icon.icon_for_signal_level(100) == "mdi:signal-cellular-3"
 
-    assert icon_for_signal_level(None) == "mdi:signal-cellular-outline"
-    assert icon_for_signal_level(0) == "mdi:signal-cellular-outline"
-    assert icon_for_signal_level(5) == "mdi:signal-cellular-1"
-    assert icon_for_signal_level(40) == "mdi:signal-cellular-2"
-    assert icon_for_signal_level(80) == "mdi:signal-cellular-3"
-    assert icon_for_signal_level(100) == "mdi:signal-cellular-3"
+
+def test_load_icons_files(hass: HomeAssistant) -> None:
+    """Test the load icons files function."""
+    file1 = hass.config.path("custom_components", "test", "icons.json")
+    file2 = hass.config.path("custom_components", "test", "invalid.json")
+    assert icon.load_icons_files({"test": file1, "invalid": file2}) == {
+        "test": {
+            "entity": {
+                "something": {"state": {"away": "mdi:home-outline", "home": "mdi:home"}}
+            },
+        },
+        "invalid": {},
+    }
+
+
+@pytest.mark.usefixtures("enable_custom_integrations")
+async def test_get_icons(hass: HomeAssistant) -> None:
+    """Test the get icon helper."""
+    icons = await icon.async_get_icons(hass, "entity")
+    assert icons == {}
+
+    assert await async_setup_component(hass, "switch", {"switch": {"platform": "test"}})
+    await hass.async_block_till_done()
+
+    icons = await icon.async_get_icons(hass, "entity")
+
+    assert icons["component.switch.test.entity.something.state.home"] == "mdi:home"
+    assert (
+        icons["component.switch.test.entity.something.state.away"] == "mdi:home-outline"
+    )
+
+
+async def test_get_icons_while_loading_components(hass: HomeAssistant) -> None:
+    """Test the get icons helper loads icons."""
+    integration = Mock(file_path=pathlib.Path(__file__))
+    integration.name = "Component 1"
+    hass.config.components.add("component1")
+    load_count = 0
+
+    def mock_load_icons_files(files):
+        """Mock load icon files."""
+        nonlocal load_count
+        load_count += 1
+        # Mimic race condition by loading a component during setup
+
+        return {"component1": {"entity": {"test": {"icon": "mdi:home"}}}}
+
+    with patch(
+        "homeassistant.helpers.icon.component_icons_path",
+        return_value="choochoo.json",
+    ), patch(
+        "homeassistant.helpers.icon.load_icons_files",
+        mock_load_icons_files,
+    ), patch(
+        "homeassistant.helpers.icon.async_get_integrations",
+        return_value={"component1": integration},
+    ):
+        tasks = [icon.async_get_icons(hass, "entity") for _ in range(5)]
+        all_icons = await asyncio.gather(*tasks)
+
+    assert all_icons[0] == {
+        "component.component1.entity.test.icon": "mdi:home",
+    }
+    assert load_count == 1
+
+
+async def test_caching(hass: HomeAssistant) -> None:
+    """Test we cache data."""
+    hass.config.components.add("switch")
+    hass.config.components.add("binary_sensor")
+
+    # Patch with same method so we can count invocations
+    with patch(
+        "homeassistant.helpers.icon.build_resources",
+        side_effect=icon.build_resources,
+    ) as mock_build:
+        load1 = await icon.async_get_icons(hass, "entity_component")
+        assert len(mock_build.mock_calls) == 1
+
+        load2 = await icon.async_get_icons(hass, "entity_component")
+        assert len(mock_build.mock_calls) == 1
+
+        assert load1 == load2
+
+        for key in load1:
+            assert key.startswith(
+                (
+                    "component.switch.entity_component.",
+                    "component.binary_sensor.entity_component.",
+                )
+            )
+
+    load_sensor_only = await icon.async_get_icons(
+        hass, "entity_component", integrations={"switch"}
+    )
+    assert load_sensor_only
+    for key in load_sensor_only:
+        assert key.startswith("component.switch.entity_component.")
+
+    load_light_only = await icon.async_get_icons(
+        hass, "entity_component", integrations={"binary_sensor"}
+    )
+    assert load_light_only
+    for key in load_light_only:
+        assert key.startswith("component.binary_sensor.entity_component.")
+
+    # Check if new loaded component, trigger load
+    hass.config.components.add("media_player")
+    with patch(
+        "homeassistant.helpers.icon.load_icons_files",
+        side_effect=icon.load_icons_files,
+    ) as mock_load:
+        load_sensor_only = await icon.async_get_icons(
+            hass, "entity_component", integrations={"switch"}
+        )
+        assert load_sensor_only
+        assert len(mock_load.mock_calls) == 0
+
+        await icon.async_get_icons(
+            hass, "entity_component", integrations={"media_player"}
+        )
+        assert len(mock_load.mock_calls) == 1
+
+
+async def test_custom_component_icons(
+    hass: HomeAssistant, enable_custom_integrations: None
+) -> None:
+    """Test getting icons from custom components."""
+    hass.config.components.add("test_embedded")
+    hass.config.components.add("test_package")
+    assert await icon.async_get_icons(hass, "entity") == {}
