b55dd075785feacee4c4118f6638ef10af324f7f
David Knowles <dknowles2@gmail.com>
2023-11-04 14:23:37 +0000

    Remove second list comprehension

--- homeassistant/components/schlage/diagnostics.py
+++ homeassistant/components/schlage/diagnostics.py
@@ -17,6 +17,7 @@ async def async_get_config_entry_diagnostics(
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     coordinator: SchlageDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
-    locks = [lock_data.lock for lock_data in coordinator.data.locks.values()]
-    # NOTE: Schalge diagnostics are already redacted.
-    return {"locks": [lock.get_diagnostics() for lock in locks]}
+    # NOTE: Schlage diagnostics are already redacted.
+    return {
+        "locks": [ld.lock.get_diagnostics() for ld in coordinator.data.locks.values()]
+    }
