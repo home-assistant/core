2a891051f5509b733c2d6c488a35f338cd103757
guy shefer <g.shefer29@gmail.com>
2023-11-04 19:23:11 +0000

    Get API directly

--- homeassistant/components/tami4/button.py
+++ homeassistant/components/tami4/button.py
@@ -24,8 +24,7 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Perform the setup for Tami4Edge."""
-    data = hass.data[DOMAIN][entry.entry_id]
-    api: Tami4EdgeAPI = data[API]
+    api: Tami4EdgeAPI = hass.data[DOMAIN][entry.entry_id][API]
 
     entity = Tami4EdgeBoilButton(api)
 
