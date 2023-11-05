31451fc66507f7221eab1aa185529b69f77d90dc
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2023-11-05 09:16:35 +0000

    Remove unused fixture

--- tests/components/frank_energie/conftest.py
+++ tests/components/frank_energie/conftest.py
@@ -1,9 +1,8 @@
 """Fixtures for Frank Energie integration tests."""
 from collections.abc import Generator
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import AsyncMock, patch
 
 import pytest
-from python_frank_energie.models import Authentication
 
 
 @pytest.fixture
@@ -13,15 +12,3 @@ def mock_setup_entry() -> Generator[AsyncMock, None, None]:
         "homeassistant.components.frank_energie.async_setup_entry", return_value=True
     ) as mock_setup:
         yield mock_setup
-
-
-@pytest.fixture
-def mock_frank_energie_api() -> MagicMock:
-    """Return a mock bridge."""
-    with patch(
-        "homeassistant.components.frank_energie.config_flow.FrankEnergie",
-    ) as mock:
-        client = mock.return_value
-        client.login.return_value = Authentication("auth_token", "refresh_token")
-
-        yield mock
