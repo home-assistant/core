2f19fdb5d616940ed54f4c84c2a880a459022bec
Joakim Plate <elupus@ecce.se>
2023-11-06 11:18:51 +0100

    Extend climate tests for nibe heatpump

--- tests/components/nibe_heatpump/test_climate.py
+++ tests/components/nibe_heatpump/test_climate.py
@@ -1,13 +1,24 @@
 """Test the Nibe Heat Pump config flow."""
 from typing import Any
-from unittest.mock import patch
+from unittest.mock import call, patch
 
+from nibe.coil import CoilData
 from nibe.coil_groups import CLIMATE_COILGROUPS, UNIT_COILGROUPS
 from nibe.heatpump import Model
 import pytest
 from syrupy import SnapshotAssertion
 
-from homeassistant.const import Platform
+from homeassistant.components.climate import (
+    ATTR_HVAC_MODE,
+    ATTR_TARGET_TEMP_HIGH,
+    ATTR_TARGET_TEMP_LOW,
+    ATTR_TEMPERATURE,
+    DOMAIN as PLATFORM_DOMAIN,
+    SERVICE_SET_HVAC_MODE,
+    SERVICE_SET_TEMPERATURE,
+    HVACMode,
+)
+from homeassistant.const import ATTR_ENTITY_ID, Platform
 from homeassistant.core import HomeAssistant
 
 from . import MockConnection, async_add_model
@@ -24,6 +35,7 @@ async def fixture_single_platform():
     ("model", "climate_id", "entity_id"),
     [
         (Model.S320, "s1", "climate.climate_system_s1"),
+        (Model.F1155, "s2", "climate.climate_system_s2"),
     ],
 )
 async def test_basic(
@@ -44,15 +56,249 @@ async def test_basic(
     coils[climate.current] = 20.5
     coils[climate.setpoint_heat] = 21.0
     coils[climate.setpoint_cool] = 30.0
-    coils[climate.mixing_valve_state] = "ON"
+    coils[climate.mixing_valve_state] = 20
     coils[climate.use_room_sensor] = "ON"
-    coils[unit.prio] = "HEAT"
+    coils[unit.prio] = "OFF"
     coils[unit.cooling_with_room_sensor] = "ON"
 
     await async_add_model(hass, model)
 
-    assert hass.states.get(entity_id) == snapshot(name="1. initial")
+    assert hass.states.get(entity_id) == snapshot(name="initial")
 
-    mock_connection.mock_coil_update(unit.prio, "OFF")
+    mock_connection.mock_coil_update(unit.prio, "COOLING")
+    assert hass.states.get(entity_id) == snapshot(name="cooling")
 
-    assert hass.states.get(entity_id) == snapshot(name="2. idle")
+    mock_connection.mock_coil_update(unit.prio, "HEAT")
+    assert hass.states.get(entity_id) == snapshot(name="heating")
+
+    mock_connection.mock_coil_update(climate.mixing_valve_state, 30)
+    assert hass.states.get(entity_id) == snapshot(name="idle (mixing valve)")
+
+    mock_connection.mock_coil_update(climate.mixing_valve_state, 20)
+    mock_connection.mock_coil_update(unit.cooling_with_room_sensor, "OFF")
+    assert hass.states.get(entity_id) == snapshot(name="heating (only)")
+
+    mock_connection.mock_coil_update(climate.use_room_sensor, "OFF")
+    assert hass.states.get(entity_id) == snapshot(name="heating (auto)")
+
+    mock_connection.mock_coil_update(unit.prio, None)
+    assert hass.states.get(entity_id) == snapshot(name="off (auto)")
+
+    if climate.active_accessory:
+        mock_connection.mock_coil_update(climate.active_accessory, "OFF")
+        assert hass.states.get(entity_id) == snapshot(
+            name="unavailable (not supported)"
+        )
+
+    coils.clear()
+    assert hass.states.get(entity_id) == snapshot(name="off (auto)")
+
+
+@pytest.mark.parametrize(
+    ("model", "climate_id", "entity_id"),
+    [
+        (Model.S320, "s1", "climate.climate_system_s1"),
+        (Model.F1155, "s2", "climate.climate_system_s2"),
+    ],
+)
+async def test_set_temperature(
+    hass: HomeAssistant,
+    mock_connection: MockConnection,
+    model: Model,
+    climate_id: str,
+    entity_id: str,
+    coils: dict[int, Any],
+    entity_registry_enabled_by_default: None,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test setting temperature."""
+    climate = CLIMATE_COILGROUPS[model.series][climate_id]
+    unit = UNIT_COILGROUPS[model.series]["main"]
+    if climate.active_accessory is not None:
+        coils[climate.active_accessory] = "ON"
+    coils[climate.current] = 20.5
+    coils[climate.setpoint_heat] = 21.0
+    coils[climate.setpoint_cool] = 30.0
+    coils[climate.mixing_valve_state] = 20
+    coils[climate.use_room_sensor] = "ON"
+    coils[unit.prio] = "OFF"
+    coils[unit.cooling_with_room_sensor] = "ON"
+
+    await async_add_model(hass, model)
+
+    coil_setpoint_heat = mock_connection.heatpump.get_coil_by_address(
+        climate.setpoint_heat
+    )
+    coil_setpoint_cool = mock_connection.heatpump.get_coil_by_address(
+        climate.setpoint_cool
+    )
+
+    await hass.services.async_call(
+        PLATFORM_DOMAIN,
+        SERVICE_SET_TEMPERATURE,
+        {
+            ATTR_ENTITY_ID: entity_id,
+            ATTR_TEMPERATURE: 22,
+            ATTR_HVAC_MODE: HVACMode.HEAT,
+        },
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    assert mock_connection.write_coil.mock_calls == [
+        call(CoilData(coil_setpoint_heat, 22))
+    ]
+    mock_connection.write_coil.reset_mock()
+
+    await hass.services.async_call(
+        PLATFORM_DOMAIN,
+        SERVICE_SET_TEMPERATURE,
+        {
+            ATTR_ENTITY_ID: entity_id,
+            ATTR_TEMPERATURE: 22,
+            ATTR_HVAC_MODE: HVACMode.COOL,
+        },
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    assert mock_connection.write_coil.mock_calls == [
+        call(CoilData(coil_setpoint_cool, 22))
+    ]
+    mock_connection.write_coil.reset_mock()
+
+    with pytest.raises(ValueError):
+        await hass.services.async_call(
+            PLATFORM_DOMAIN,
+            SERVICE_SET_TEMPERATURE,
+            {
+                ATTR_ENTITY_ID: entity_id,
+                ATTR_TEMPERATURE: 22,
+            },
+            blocking=True,
+        )
+
+    await hass.services.async_call(
+        PLATFORM_DOMAIN,
+        SERVICE_SET_TEMPERATURE,
+        {
+            ATTR_ENTITY_ID: entity_id,
+            ATTR_TARGET_TEMP_HIGH: 30,
+            ATTR_TARGET_TEMP_LOW: 22,
+        },
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    assert mock_connection.write_coil.mock_calls == [
+        call(CoilData(coil_setpoint_heat, 22)),
+        call(CoilData(coil_setpoint_cool, 30)),
+    ]
+
+    mock_connection.write_coil.reset_mock()
+
+
+@pytest.mark.parametrize(
+    ("model", "climate_id", "entity_id"),
+    [
+        (Model.S320, "s1", "climate.climate_system_s1"),
+        (Model.F1155, "s2", "climate.climate_system_s2"),
+    ],
+)
+async def test_set_hvac_mode(
+    hass: HomeAssistant,
+    mock_connection: MockConnection,
+    model: Model,
+    climate_id: str,
+    entity_id: str,
+    coils: dict[int, Any],
+    entity_registry_enabled_by_default: None,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test setting the hvac mode."""
+    climate = CLIMATE_COILGROUPS[model.series][climate_id]
+    unit = UNIT_COILGROUPS[model.series]["main"]
+    if climate.active_accessory is not None:
+        coils[climate.active_accessory] = "ON"
+    coils[climate.current] = 20.5
+    coils[climate.setpoint_heat] = 21.0
+    coils[climate.setpoint_cool] = 30.0
+    coils[climate.mixing_valve_state] = 20
+    coils[climate.use_room_sensor] = "ON"
+    coils[unit.prio] = "OFF"
+    coils[unit.cooling_with_room_sensor] = "ON"
+
+    await async_add_model(hass, model)
+
+    coil_use_room_sensor = mock_connection.heatpump.get_coil_by_address(
+        climate.use_room_sensor
+    )
+    coil_cooling_with_room_sensor = mock_connection.heatpump.get_coil_by_address(
+        unit.cooling_with_room_sensor
+    )
+
+    await hass.services.async_call(
+        PLATFORM_DOMAIN,
+        SERVICE_SET_HVAC_MODE,
+        {
+            ATTR_ENTITY_ID: entity_id,
+            ATTR_HVAC_MODE: HVACMode.HEAT_COOL,
+        },
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    assert mock_connection.write_coil.mock_calls == [
+        call(CoilData(coil_cooling_with_room_sensor, "ON")),
+        call(CoilData(coil_use_room_sensor, "ON")),
+    ]
+    mock_connection.write_coil.reset_mock()
+
+    await hass.services.async_call(
+        PLATFORM_DOMAIN,
+        SERVICE_SET_HVAC_MODE,
+        {
+            ATTR_ENTITY_ID: entity_id,
+            ATTR_HVAC_MODE: HVACMode.HEAT,
+        },
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    assert mock_connection.write_coil.mock_calls == [
+        call(CoilData(coil_cooling_with_room_sensor, "OFF")),
+        call(CoilData(coil_use_room_sensor, "ON")),
+    ]
+    mock_connection.write_coil.reset_mock()
+
+    await hass.services.async_call(
+        PLATFORM_DOMAIN,
+        SERVICE_SET_HVAC_MODE,
+        {
+            ATTR_ENTITY_ID: entity_id,
+            ATTR_HVAC_MODE: HVACMode.AUTO,
+        },
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    assert mock_connection.write_coil.mock_calls == [
+        call(CoilData(coil_cooling_with_room_sensor, "OFF")),
+        call(CoilData(coil_use_room_sensor, "OFF")),
+    ]
+    mock_connection.write_coil.reset_mock()
+
+    with pytest.raises(ValueError):
+        await hass.services.async_call(
+            PLATFORM_DOMAIN,
+            SERVICE_SET_HVAC_MODE,
+            {
+                ATTR_ENTITY_ID: entity_id,
+                ATTR_HVAC_MODE: HVACMode.COOL,
+            },
+            blocking=True,
+        )
+        await hass.async_block_till_done()
+
+    assert mock_connection.write_coil.mock_calls == []
+    mock_connection.write_coil.reset_mock()
