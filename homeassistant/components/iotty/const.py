85c85a7584279a4b30bde4e6216aed4a59cb2800
Paolo Burgio <paolo.burgio@gmail.com>
2023-11-06 11:35:21 +0000

    Modified REST EP for iotty CloudApi

--- homeassistant/components/iotty/const.py
+++ homeassistant/components/iotty/const.py
@@ -6,4 +6,4 @@ OAUTH2_AUTHORIZE = "https://auth.iotty.com/.auth/oauth2/login"
 OAUTH2_TOKEN = "https://auth.iotty.com/.auth/oauth2/token"
 OAUTH2_CLIENT_ID = "hass-iotty"
 
-IOTTYAPI_BASE = "https://cloudapi.iotty.com/"
+IOTTYAPI_BASE = "https://homeassistant.iotty.com/"
