3c8080090d8173893d829b2d06152c36aff7aa38
mletenay <mle@whitestein.com>
2023-11-07 21:36:51 +0000

    Revert change not related to test

--- tests/components/goodwe/test_diagnostics.py
+++ tests/components/goodwe/test_diagnostics.py
@@ -27,9 +27,7 @@ async def test_entry_diagnostics(
         entry_id="3bd2acb0e4f0476d40865546d0d91921",
     )
     config_entry.add_to_hass(hass)
-    with patch(
-        "homeassistant.components.goodwe._connect_inverter", return_value=MockInverter()
-    ):
+    with patch("homeassistant.components.goodwe.connect", return_value=MockInverter()):
         assert await async_setup_component(hass, DOMAIN, {})
 
     result = await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
