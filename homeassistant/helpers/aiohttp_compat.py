0955b94108043d28d8094acd9bbb2a7cafddd3f5
J. Nick Koston <nick@koston.org>
2023-11-07 07:43:23 -0600

    Ensure large payloads are compressed in the executor with aiohttp 3.9.0

--- homeassistant/helpers/aiohttp_compat.py
+++ homeassistant/helpers/aiohttp_compat.py
@@ -1,7 +1,7 @@
 """Helper to restore old aiohttp behavior."""
 from __future__ import annotations
 
-from aiohttp import web_protocol, web_server
+from aiohttp import web, web_protocol, web_server
 
 
 class CancelOnDisconnectRequestHandler(web_protocol.RequestHandler):
@@ -23,3 +23,16 @@ def restore_original_aiohttp_cancel_behavior() -> None:
     """
     web_protocol.RequestHandler = CancelOnDisconnectRequestHandler  # type: ignore[misc]
     web_server.RequestHandler = CancelOnDisconnectRequestHandler  # type: ignore[misc]
+
+
+def enable_compression(response: web.Response) -> None:
+    """Enable compression on the response."""
+    #
+    # Set _zlib_executor_size in the constructor once support for
+    # aiohttp < 3.9.0 is dropped
+    #
+    # We want large zlib payloads to be compressed in the executor
+    # to avoid blocking the event loop, 5192 bytes was chosen because
+    # its also the value used by python-zlib-ng to release the gil
+    response._zlib_executor_size = 5192  # pylint: disable=protected-access
+    response.enable_compression()
