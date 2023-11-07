2455524c6e280458c429dd787cc110ab939e3328
Allen Porter <allen@thebends.org>
2023-11-07 15:34:38 +0000

    Remove unnecessary guard to improve code coverage

--- homeassistant/components/google/config_flow.py
+++ homeassistant/components/google/config_flow.py
@@ -62,8 +62,6 @@ class OAuth2FlowHandler(
     @property
     def extra_authorize_data(self) -> dict[str, Any]:
         """Extra data that needs to be appended to the authorize url."""
-        if not self._web_auth:
-            raise ValueError("Unexpected state; should only be called during web auth")
         return {
             "scope": DEFAULT_FEATURE_ACCESS.scope,
             # Add params to ensure we get back a refresh token
