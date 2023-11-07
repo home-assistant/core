9a4567174a2e76fd34d6d973a0a004b0af0c580c
baldarn <lorenzo.farnararo@gmail.com>
2023-11-07 08:45:19 +0000

    add consts

--- homeassistant/components/melcloud/const.py
+++ homeassistant/components/melcloud/const.py
@@ -3,6 +3,8 @@
 DOMAIN = "melcloud"
 
 CONF_POSITION = "position"
+CONF_ZONE_1_TARGET_HEAT_TEMPERATURE = "temperature"
+CONF_ZONE_1_TARGET_HEAT_FLOW_TEMPERATURE = "temperature"
 
 ATTR_STATUS = "status"
 ATTR_VANE_HORIZONTAL = "vane_horizontal"
@@ -12,3 +14,7 @@ ATTR_VANE_VERTICAL_POSITIONS = "vane_vertical_positions"
 
 SERVICE_SET_VANE_HORIZONTAL = "set_vane_horizontal"
 SERVICE_SET_VANE_VERTICAL = "set_vane_vertical"
+SERVICE_SET_ZONE_1_TARGET_HEAT_TEMPERATURE = "set_zone_1_target_heat_temperature"
+SERVICE_SET_ZONE_1_TARGET_HEAT_FLOW_TEMPERATURE = (
+    "set_zone_1_target_heat_flow_temperature"
+)
