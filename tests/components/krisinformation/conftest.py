8d924e954b9c67f61136ec80c4253dca05eba60a
albin landgren <albinlandgreen@gmail.com>
2023-11-07 09:01:11 +0000

    Krisinformation init

---
+++ tests/components/krisinformation/conftest.py
@@ -0,0 +1,14 @@
+"""Common fixtures for the krisinformation tests."""
+from collections.abc import Generator
+from unittest.mock import AsyncMock, patch
+
+import pytest
+
+
+@pytest.fixture
+def mock_setup_entry() -> Generator[AsyncMock, None, None]:
+    """Override async_setup_entry."""
+    with patch(
+        "homeassistant.components.krisinformation.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        yield mock_setup_entry
