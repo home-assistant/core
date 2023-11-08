22caa678d113d5bdc52e706b67bc4b674f78f43d
J. Nick Koston <nick@koston.org>
2023-11-08 14:04:38 -0600

    Fix tplink overloading power strips
    - Split the child power sensors into their own coordinators to
      reduce the update interval of the power data to prevent overloading
      the device
    
    - Wait 10 seconds for the discovery since when the power strips
      are loaded they cannot respond in time

--- homeassistant/components/tplink/models.py
+++ homeassistant/components/tplink/models.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from .coordinator import TPLinkDataUpdateCoordinator
 
 
-@dataclass
+@dataclass(slots=True)
 class TPLinkData:
     """Data for the tplink integration."""
 
