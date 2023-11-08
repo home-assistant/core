e0f24d08b15cf538e2b1a01334af60ba0ed4e97c
J. Nick Koston <nick@koston.org>
2023-11-08 14:42:41 -0600

    revert 10s change for a new pr

--- tests/components/tplink/__init__.py
+++ tests/components/tplink/__init__.py
@@ -202,7 +202,7 @@ def _patch_discovery(device=None, no_device=False):
 
 
 def _patch_single_discovery(device=None, no_device=False):
-    async def _discover_single(*args, **kwargs):
+    async def _discover_single(*_):
         if no_device:
             raise SmartDeviceException
         return device if device else _mocked_bulb()
