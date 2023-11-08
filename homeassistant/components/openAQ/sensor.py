049e522f984fff6efa96233206db329f837ef850
Ronserro <clind0429@gmail.com>
2023-11-08 16:21:54 +0000

    update sensors

--- homeassistant/components/openAQ/sensor.py
+++ homeassistant/components/openAQ/sensor.py
@@ -1,9 +1,29 @@
+from enum import Enum
+
+class OpenAQDeviceSensors(str, Enum):
+    """Sensors to report in home assistant."""
+
+    Last_Update = "TIMESTAMP"
+    Air_Quality_index = "AQI"
+    Particle_Matter_25 = "PM25"
+    Particle_Matter_10 = "PM10"
+    Particle_Matter_1 = "PM1"
+    Concentration_of_Ozone = "OZONE"
+    Atmospheric_Pressure = "ATMOSPHERIC_PRESSURE"
+    TEMPERATURE = "TEMPERATURE"
+    Relative_humidity = "HUMIDITY"
+    Concentration_of_Nitrogen_Dioxide = "NITROGEN_DIOXIDE"
+    Concentration_of_NITROGEN_MONOXIDE = "NITROGEN_MONOXIDE"
+    Concentration_of_Carbon_Monoxide = "CO"
+    Concentration_of_Carbon_Dioxide = "CO2"
+    Concentration_of_Sulphure_Dioxide = "SULPHUR_DIOXIDE"
+
 async def async_setup_entry(hass, entry, async_add_devices):
     """Configure the sensor platform."""
     pass
 
 
-class ChargePointMetric(SensorEntity):
+class OpenAQSensor(SensorEntity):
     def __init__(
         self,
         hass: HomeAssistant,
