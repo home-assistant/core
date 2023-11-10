3e2b583ac5aa02517d7086665f74e08459b4068a
Dan Simpson <daniel_simpson1988@hotmail.com>
2023-11-10 11:15:00 +0000

    Lookup my DPCode.MODE if DPCode.STATUS value is not available

--- homeassistant/components/tuya/vacuum.py
+++ homeassistant/components/tuya/vacuum.py
@@ -151,9 +151,13 @@ class TuyaVacuumEntity(TuyaEntity, StateVacuumEntity):
             return STATE_PAUSED
         if self.device.status.get(DPCode.FAULT):
             return STATE_ERROR
-        if not (status := self.device.status.get(DPCode.STATUS)):
-            return None
-        return TUYA_STATUS_TO_HA.get(status)
+        if tuya_status := self.device.status.get(DPCode.STATUS):
+            if ha_status := TUYA_STATUS_TO_HA.get(tuya_status):
+                return ha_status
+        if tuya_mode := self.device.status.get(DPCode.MODE):
+            if ha_status := TUYA_STATUS_TO_HA.get(tuya_mode):
+                return ha_status
+        return None
 
     def turn_on(self, **kwargs: Any) -> None:
         """Turn the device on."""
