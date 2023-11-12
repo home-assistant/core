79bb643f9009c3b0d2feda7f81e4a3d7e103d70b
Miguel Camba <miguel.camba@gmail.com>
2023-11-12 20:49:26 +0000

    Code coverate inproved to 91%

--- tests/components/valve/test_init.py
+++ tests/components/valve/test_init.py
@@ -1,5 +1,15 @@
 """The tests for Valve."""
-import homeassistant.components.valve as valve
+from collections.abc import Generator
+
+import pytest
+
+from homeassistant.components.valve import (
+    DOMAIN,
+    ValveEntity,
+    ValveEntityFeature,
+    is_closed as valve_is_closed,
+)
+from homeassistant.config_entries import ConfigEntry, ConfigEntryState, ConfigFlow
 from homeassistant.const import (
     ATTR_ENTITY_ID,
     CONF_PLATFORM,
@@ -7,9 +17,117 @@ from homeassistant.const import (
     STATE_CLOSING,
     STATE_OPEN,
     STATE_OPENING,
+    STATE_UNAVAILABLE,
+    Platform,
 )
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.setup import async_setup_component
+
+from tests.common import (
+    MockConfigEntry,
+    MockModule,
+    MockPlatform,
+    mock_config_flow,
+    mock_integration,
+    mock_platform,
+)
+
+TEST_DOMAIN = "test"
+
+
+class MockFlow(ConfigFlow):
+    """Test flow."""
+
+
+class MockValveEntity(ValveEntity):
+    """Mock valve device to use in tests."""
+
+    def __init__(
+        self,
+        unique_id: str = "mock_valve",
+        name: str = "Valve",
+        features: ValveEntityFeature = ValveEntityFeature(0),
+    ) -> None:
+        """Initialize the valve."""
+        self._attr_name = name
+        self._attr_unique_id = unique_id
+        self._attr_supported_features = features
+        self._attr_is_closed = None
+
+
+@pytest.fixture(autouse=True)
+def config_flow_fixture(hass: HomeAssistant) -> Generator[None, None, None]:
+    """Mock config flow."""
+    mock_platform(hass, f"{TEST_DOMAIN}.config_flow")
+
+    with mock_config_flow(TEST_DOMAIN, MockFlow):
+        yield
+
+
+async def test_valve_setup(hass: HomeAssistant) -> None:
+    """Test setup and tear down of valve platform and entity."""
+
+    async def async_setup_entry_init(
+        hass: HomeAssistant, config_entry: ConfigEntry
+    ) -> bool:
+        """Set up test config entry."""
+        await hass.config_entries.async_forward_entry_setup(
+            config_entry, Platform.VALVE
+        )
+        return True
+
+    async def async_unload_entry_init(
+        hass: HomeAssistant, config_entry: ConfigEntry
+    ) -> bool:
+        """Unload up test config entry."""
+        await hass.config_entries.async_unload_platforms(config_entry, [Platform.VALVE])
+        return True
+
+    mock_platform(hass, f"{TEST_DOMAIN}.config_flow")
+    mock_integration(
+        hass,
+        MockModule(
+            TEST_DOMAIN,
+            async_setup_entry=async_setup_entry_init,
+            async_unload_entry=async_unload_entry_init,
+        ),
+    )
+
+    entity1 = MockValveEntity()
+    entity1.entity_id = "valve.mock_valve"
+
+    async def async_setup_entry_platform(
+        hass: HomeAssistant,
+        config_entry: ConfigEntry,
+        async_add_entities: AddEntitiesCallback,
+    ) -> None:
+        """Set up test platform via config entry."""
+        async_add_entities([entity1])
+
+    mock_platform(
+        hass,
+        f"{TEST_DOMAIN}.{DOMAIN}",
+        MockPlatform(async_setup_entry=async_setup_entry_platform),
+    )
+
+    config_entry = MockConfigEntry(domain=TEST_DOMAIN)
+    config_entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state == ConfigEntryState.LOADED
+    assert hass.states.get(entity1.entity_id)
+
+    assert await hass.config_entries.async_unload(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state == ConfigEntryState.NOT_LOADED
+    entity_state = hass.states.get(entity1.entity_id)
+
+    assert entity_state
+    assert entity_state.state == STATE_UNAVAILABLE
 
 
 async def test_services(hass: HomeAssistant, enable_custom_integrations: None) -> None:
@@ -17,9 +135,7 @@ async def test_services(hass: HomeAssistant, enable_custom_integrations: None) -
     platform = getattr(hass.components, "test.valve")
 
     platform.init()
-    assert await async_setup_component(
-        hass, valve.DOMAIN, {valve.DOMAIN: {CONF_PLATFORM: "test"}}
-    )
+    assert await async_setup_component(hass, DOMAIN, {DOMAIN: {CONF_PLATFORM: "test"}})
     await hass.async_block_till_done()
 
     # ent1 = valve without position
@@ -59,7 +175,7 @@ async def test_services(hass: HomeAssistant, enable_custom_integrations: None) -
 def call_service(hass, service, ent):
     """Call any service on entity."""
     return hass.services.async_call(
-        valve.DOMAIN, service, {ATTR_ENTITY_ID: ent.entity_id}, blocking=True
+        DOMAIN, service, {ATTR_ENTITY_ID: ent.entity_id}, blocking=True
     )
 
 
@@ -80,7 +196,7 @@ def is_opening(hass, ent):
 
 def is_closed(hass, ent):
     """Return if the valve is closed based on the statemachine."""
-    return valve.is_closed(hass, ent.entity_id)
+    return valve_is_closed(hass, ent.entity_id)
 
 
 def is_closing(hass, ent):
