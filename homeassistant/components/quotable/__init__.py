e1e7635d26ef684eb47ce9105e2c834a0137e0cf
Siam Zamir <siamzam@gmail.com>
2023-11-12 02:22:07 +0100

    Added service to save configuration changes made from the UI

--- homeassistant/components/quotable/__init__.py
+++ homeassistant/components/quotable/__init__.py
@@ -1,6 +1,7 @@
 """The Quotable integration."""
 
 import logging
+from typing import Any
 
 import voluptuous as vol
 
@@ -8,42 +9,72 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.typing import ConfigType
 
-from .services import register_services
 from .const import (
-    CONF_SELECTED_AUTHORS,
-    CONF_SELECTED_TAGS,
-    CONF_UPDATE_FREQUENCY,
+    ATTR_SELECTED_AUTHORS,
+    ATTR_SELECTED_TAGS,
+    ATTR_UPDATE_FREQUENCY,
     DEFAULT_UPDATE_FREQUENCY,
     DOMAIN,
 )
-
-_LOGGER = logging.getLogger(__name__)
+from .services import register_services
 
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
             {
-                vol.Optional(CONF_SELECTED_TAGS, default=[]): vol.All(
+                vol.Optional(ATTR_SELECTED_TAGS, default=[]): vol.All(
                     cv.ensure_list, [cv.string]
                 ),
-                vol.Optional(CONF_SELECTED_AUTHORS, default=[]): vol.All(
+                vol.Optional(ATTR_SELECTED_AUTHORS, default=[]): vol.All(
                     cv.ensure_list, [cv.string]
                 ),
                 vol.Optional(
-                    CONF_UPDATE_FREQUENCY, default=DEFAULT_UPDATE_FREQUENCY
-                ): cv.positive_int,
+                    ATTR_UPDATE_FREQUENCY, default=DEFAULT_UPDATE_FREQUENCY
+                ): vol.All(vol.Coerce(int), vol.Range(min=1)),
             }
         )
     },
     extra=vol.ALLOW_EXTRA,
 )
 
+_LOGGER = logging.getLogger(__name__)
+
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Quotable integration."""
 
+    quotable = hass.data.get(DOMAIN)
+
+    if not quotable:
+        static_config = config[DOMAIN]
+        quotable = Quotable(
+            static_config[ATTR_SELECTED_TAGS],
+            static_config[ATTR_SELECTED_AUTHORS],
+            static_config[ATTR_UPDATE_FREQUENCY],
+        )
+
+        hass.data[DOMAIN] = quotable
+
     register_services(hass)
 
-    hass.states.set(f"{DOMAIN}.testing", "It Works!")
-
     return True
+
+
+class Quotable:
+    """Quotable class."""
+
+    selected_tags = list[str]
+    selected_authors = list[str]
+    update_frequency = int
+    quotes = list[dict[str, Any]]
+
+    def __init__(self, selected_tags, selected_authors, update_frequency):
+        """Initialize Quotable."""
+        self.quotes = []
+        self.update_configuration(selected_tags, selected_authors, update_frequency)
+
+    def update_configuration(self, selected_tags, selected_authors, update_frequency):
+        """Update configuration."""
+        self.selected_tags = selected_tags
+        self.selected_authors = selected_authors
+        self.update_frequency = update_frequency
--- homeassistant/components/quotable/services.py
+++ homeassistant/components/quotable/services.py
@@ -15,12 +15,16 @@ from homeassistant.core import (
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import (
+    ATTR_SELECTED_AUTHORS,
+    ATTR_SELECTED_TAGS,
+    ATTR_UPDATE_FREQUENCY,
     DOMAIN,
     GET_TAGS_URL,
     HTTP_CLIENT_TIMEOUT,
     SERVICE_FETCH_A_QUOTE,
-    SERVICE_GET_TAGS,
+    SERVICE_FETCH_ALL_TAGS,
     SERVICE_SEARCH_AUTHORS,
+    SERVICE_UPDATE_CONFIGURATION,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -33,20 +37,32 @@ def register_services(hass: HomeAssistant) -> None:
 
     hass.services.async_register(
         domain=DOMAIN,
-        service=SERVICE_GET_TAGS,
-        service_func=partial(_get_tags_service, session),
+        service=SERVICE_FETCH_ALL_TAGS,
+        service_func=partial(_fetch_all_tags_service, session),
         supports_response=SupportsResponse.ONLY,
     )
 
     hass.services.async_register(
-        DOMAIN, SERVICE_SEARCH_AUTHORS, _search_authors_service
+        domain=DOMAIN,
+        service=SERVICE_SEARCH_AUTHORS,
+        service_func=partial(_search_authors_service, session),
     )
 
-    hass.services.async_register(DOMAIN, SERVICE_FETCH_A_QUOTE, _fetch_a_quote_service)
+    hass.services.async_register(
+        domain=DOMAIN,
+        service=SERVICE_FETCH_A_QUOTE,
+        service_func=partial(_fetch_a_quote_service, session, hass),
+    )
+
+    hass.services.register(
+        domain=DOMAIN,
+        service=SERVICE_UPDATE_CONFIGURATION,
+        service_func=partial(_update_configuration_service, hass),
+    )
 
 
-async def _get_tags_service(
-    session: aiohttp.ClientSession, _: ServiceCall
+async def _fetch_all_tags_service(
+    session: aiohttp.ClientSession, service: ServiceCall
 ) -> ServiceResponse:
     response = await session.get(GET_TAGS_URL, timeout=HTTP_CLIENT_TIMEOUT)
     if response.status == HTTPStatus.OK:
@@ -58,9 +74,22 @@ async def _get_tags_service(
     return None
 
 
-async def _search_authors_service(service: ServiceCall) -> None:
+async def _search_authors_service(
+    session: aiohttp.ClientSession, service: ServiceCall
+) -> None:
     pass
 
 
-async def _fetch_a_quote_service(service: ServiceCall) -> None:
+async def _fetch_a_quote_service(
+    session: aiohttp.ClientSession, hass: HomeAssistant, service: ServiceCall
+) -> None:
     pass
+
+
+def _update_configuration_service(hass: HomeAssistant, service: ServiceCall) -> None:
+    if quotable := hass.data.get(DOMAIN):
+        quotable.update_configuration(
+            service.data[ATTR_SELECTED_TAGS],
+            service.data[ATTR_SELECTED_AUTHORS],
+            service.data[ATTR_UPDATE_FREQUENCY],
+        )
