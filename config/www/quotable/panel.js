d5681bd68bdf5b2abe7c3f823f159519e99be680
Anastasia Koni <anastkoni@gmail.com>
2023-11-13 15:41:08 +0100

    Add basic structure for GUI panel

---
+++ config/www/quotable/panel.js
@@ -0,0 +1,49 @@
+import "https://unpkg.com/wired-card@2.1.0/lib/wired-card.js?module";
+import {
+  LitElement,
+  html,
+  css,
+} from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
+
+class ExamplePanel extends LitElement {
+  static get properties() {
+    return {
+      hass: { type: Object },
+      narrow: { type: Boolean },
+      route: { type: Object },
+      panel: { type: Object },
+    };
+  }
+
+  render() {
+    return html`
+      <wired-card elevation="2">
+        <p>There are ${Object.keys(this.hass.states).length} entities.</p>
+        <p>The screen is${this.narrow ? "" : " not"} narrow.</p>
+        Configured panel config
+        <pre>${JSON.stringify(this.panel.config, undefined, 2)}</pre>
+        Current route
+        <pre>${JSON.stringify(this.route, undefined, 2)}</pre>
+      </wired-card>
+    `;
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        background-color: #fafafa;
+        padding: 16px;
+        display: block;
+      }
+      wired-card {
+        background-color: white;
+        padding: 16px;
+        display: block;
+        font-size: 18px;
+        max-width: 600px;
+        margin: 0 auto;
+      }
+    `;
+  }
+}
+customElements.define("example-panel", ExamplePanel);
