b86436d977f03252deb5ca0956b07354d3eedd25
1onemike <moaduamah@gmail.com>
2023-11-13 19:25:12 +0000

    Updated card styling and gitignore

--- config/www/quotable/card.js
+++ config/www/quotable/card.js
@@ -8,27 +8,22 @@ class QuotableCard extends HTMLElement {
   set hass(hass) {
     if (!this.content) {
       this.innerHTML = `
-                <style>
-                    ha-card {
-                        position: relative;
-                        overflow: hidden;
-                        width: 400px; /* Set your preferred width */
-                        height: 200px; /* Set your preferred height */
-                    }
-
-                    .background-image {
-                        width: 100%;
-                        height: 100%;
-                        object-fit: cover;
-                    }
-                </style>
-                <ha-card>
-                    <img class="background-image" src="${
-                      this.imageUrls[this.currentImageIndex]
-                    }">
-                    <div class="overlay"></div>
-                </ha-card>
-            `;
+                  <style>
+                      .background-image {
+                          width: 100%;
+                          height: 100%;
+                          background-size:     cover;
+                          background-repeat:   no-repeat;
+                          background-position: center center;
+                      }
+                  </style>
+                  <ha-card>
+                      <img class="background-image" src="${
+                        this.imageUrls[this.currentImageIndex]
+                      }">
+                      <div class="overlay"></div>
+                  </ha-card>
+              `;
       this.content = this.querySelector(".background-image");
     }
   }
