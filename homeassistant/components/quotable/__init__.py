1f75897be74e7ac3769f4d909d1f7c6477c513cf
Siam Zamir <siamzam@gmail.com>
2023-11-14 00:30:53 +0100

    Add functionality to call fetch_a_quote service based on configured update frequency

--- homeassistant/components/quotable/__init__.py
+++ homeassistant/components/quotable/__init__.py
@@ -1,13 +1,15 @@
 """The Quotable integration."""
 
+from datetime import datetime, timedelta
 import logging
-import time
 from typing import Any
 
 import voluptuous as vol
 
-from homeassistant.core import HomeAssistant
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
+from homeassistant.core import Event, HomeAssistant, callback
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.helpers.typing import ConfigType
 
 from .const import (
@@ -18,6 +20,7 @@ from .const import (
     DEFAULT_UPDATE_FREQUENCY,
     DOMAIN,
     ENTITY_ID,
+    SERVICE_FETCH_A_QUOTE,
 )
 from .services import register_services
 
@@ -46,13 +49,13 @@ _LOGGER = logging.getLogger(__name__)
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Quotable integration."""
 
+    register_services(hass)
+
     quotable = hass.data.get(DOMAIN)
 
     if not quotable:
         quotable = Quotable(hass, config[DOMAIN])
         hass.data[DOMAIN] = quotable
-
-    register_services(hass)
 
     return True
 
@@ -62,22 +65,50 @@ class Quotable:
 
     def __init__(self, hass, config):
         """Initialize Quotable."""
-        self.hass = hass
+        self._hass = hass
         self.quotes = []
         self.config = config
+        self._unsubscribe = None
+        self._add_event_listener()
         self._update_state()
-
-    def update_configuration(self, selected_tags, selected_authors, update_frequency):
-        """Update configuration."""
-        self.config[ATTR_SELECTED_TAGS] = selected_tags
-        self.config[ATTR_SELECTED_AUTHORS] = selected_authors
-        self.config[ATTR_UPDATE_FREQUENCY] = update_frequency
-        self._update_state()
+        self._hass.bus.async_listen_once(
+            EVENT_HOMEASSISTANT_STOP, self._cancel_event_listener
+        )
 
     @property
     def attrs(self) -> dict[str, Any]:
         """Attributes that are saved in state."""
         return {**self.config, **{ATTR_QUOTES: self.quotes}}
 
+    def update_configuration(self, selected_tags, selected_authors, update_frequency):
+        """Update configuration."""
+        self.config[ATTR_SELECTED_TAGS] = selected_tags
+        self.config[ATTR_SELECTED_AUTHORS] = selected_authors
+        self.config[ATTR_UPDATE_FREQUENCY] = update_frequency
+        self._add_event_listener()
+        self._update_state()
+
     def _update_state(self):
-        self.hass.states.set(ENTITY_ID, time.time(), self.attrs)
+        self._hass.states.set(ENTITY_ID, datetime.now(), self.attrs)
+
+    def _add_event_listener(self):
+        self._cancel_event_listener(self)
+
+        self._unsubscribe = async_track_time_interval(
+            self._hass,
+            self._schedule_fetch_a_quote_task,
+            timedelta(seconds=self.config[ATTR_UPDATE_FREQUENCY]),
+            cancel_on_shutdown=True,
+        )
+
+    @callback
+    def _cancel_event_listener(self, _: Event):
+        if self._unsubscribe:
+            self._unsubscribe()
+            self._unsubscribe = None
+
+    @callback
+    def _schedule_fetch_a_quote_task(self, _: datetime) -> None:
+        self._hass.async_create_task(
+            self._hass.services.async_call(DOMAIN, SERVICE_FETCH_A_QUOTE)
+        )
