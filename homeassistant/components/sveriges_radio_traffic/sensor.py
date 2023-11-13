db6c98b223f3bee53e4cc4b7f01fac4f50aff0d6
OskarJakobsson <oskjak@student.chalmers.se>
2023-11-13 08:21:37 +0000

    US2.2, Showing trafficareas
    Co-authored-by: linwahlg <linwahlg@users.noreply.github.com>
    Co-authored-by: Markus-Moen <Markus-Moen@users.noreply.github.com>

--- homeassistant/components/sveriges_radio_traffic/sensor.py
+++ homeassistant/components/sveriges_radio_traffic/sensor.py
@@ -1,6 +1,9 @@
 """Sensor for traffic information."""
 from __future__ import annotations
 
+import defusedxml.ElementTree as defET
+import requests
+
 # from homeassistant.helpers.entity_platform import AddEntitiesCallback
 # from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.components.sensor import SensorEntity
@@ -31,6 +34,7 @@ class TrafficSensor(SensorEntity):
         self._attr_native_value = "Allt är kanon!"
         self._attr_auto_update = True
         self._attr_should_poll = True
+        self.area = ""
 
     def update(self) -> None:
         """Fetch new state data for the sensor.
@@ -38,35 +42,14 @@ class TrafficSensor(SensorEntity):
         This is the only method that should fetch new data for Home Assistant.
         """
         self._attr_native_value = self._api_caller()
-        # self._attr_native_value = "Stockholm-Västerås. Risk för förseningar. Reducerad spårkapacitet vid Bålsta."
 
     def _api_caller(self) -> str:
-        return "It totally worked"
-        # root = ET.fromstring(
-        #    requests.get("http://api.sr.se/api/v2/traffic/areas", timeout=10).text
-        # )
-        # areas = [child.attrib["name"] for child in root[2]]
-        # return ", ".join(areas)
-
-
-# def setup_platform(
-#   hass: HomeAssistant,
-#   config: ConfigType,
-#   add_entities: AddEntitiesCallback,
-#   discovery_info: DiscoveryInfoType | None = None
-# ) -> None:
-#   """Set up the sensor platform."""
-#   add_entities([ExampleSensor()])
-#
-# class ExampleSensor(SensorEntity):
-#   """Representation of a Sensor."""
-#   _attr_name = "Example Temperature"
-#   _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
-#   _attr_device_class = SensorDeviceClass.TEMPERATURE
-#   _attr_state_class = SensorStateClass.MEASUREMENT
-#   def update(self) -> None:
-#       """Fetch new state data for the sensor.
-#       This is the only method that should fetch new data for Home Assistant.
-#       """
-#       self._attr_native_value = 23
-#
+        """Fetch stuff from SR-API."""
+        response = requests.get("http://api.sr.se/api/v2/traffic/areas", timeout=10)
+        tree = defET.fromstring(response.content)
+        area_names = []
+        for areas in tree.findall(".//areas"):
+            for area in areas.findall(".//area"):
+                area_names.append(area.attrib["name"])
+        allAreasString = ", ".join(area_names)
+        return allAreasString
