fb5b33a97d7bf6995dce8bf022a1175a2e5d90fb
1onemike <moaduamah@gmail.com>
2023-11-14 10:31:40 +0000

    textview and bg image update

--- config/www/quotable/card.js
+++ config/www/quotable/card.js
@@ -2,26 +2,62 @@ class QuotableCard extends HTMLElement {
   constructor() {
     super();
     this.currentImageIndex = 0;
-    this.imageUrls = ["https://via.placeholder.com/400x200.png"];
+    this.imageUrls = [
+      "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+    ];
   }
 
   set hass(hass) {
     if (!this.content) {
       this.innerHTML = `
                   <style>
+                      ha-card {
+                        position: relative;
+                        overflow: hidden;
+                        height: 200px;
+                      }
                       .background-image {
-                          width: 100%;
-                          height: 100%;
-                          background-size:     cover;
-                          background-repeat:   no-repeat;
-                          background-position: center center;
+                        width: 100%;
+                        height: 100%;
+                        background-size:     cover;
+                        background-repeat:   no-repeat;
+                        background-position: center center;
+                      }
+
+                      .overlay {
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        width: 100%;
+                        height: 100%;
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: center;
+                        color: white;
+                        text-align: center;
+                      }
+
+                      .quote {
+                        font-size: 24px;
+                        font-weight: bold;
+                        margin-bottom: 16px;
+                      }
+
+                      .author {
+                        font-size: 18px;
+                        font-weight: bold;
+
                       }
                   </style>
                   <ha-card>
                       <img class="background-image" src="${
                         this.imageUrls[this.currentImageIndex]
                       }">
-                      <div class="overlay"></div>
+                      <div class="overlay">
+                      <div class="quote">"Quote goes here"</div>
+                      <div class="author">- Author Name</div>
+                      </div>
                   </ha-card>
               `;
       this.content = this.querySelector(".background-image");
