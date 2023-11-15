be32b923b0bf25c9a9226803065fbac04b8d11c1
Uzair <uzairahmed80@gmail.com>
2023-11-15 18:25:58 +0000

    updated  fetch service

--- homeassistant/components/quotable/services.py
+++ homeassistant/components/quotable/services.py
@@ -85,12 +85,26 @@ async def _search_authors_service(
 async def _fetch_a_quote_service(
     session: aiohttp.ClientSession, hass: HomeAssistant, service: ServiceCall
 ) -> ServiceResponse:
-    response = await session.get(FETCH_A_QUOTE_URL, timeout=HTTP_CLIENT_TIMEOUT)
+    """Get the Quotable instance from hass.data."""
+    quotable = hass.data.get(DOMAIN)
+    if quotable is None:
+        return None
+
+    selected_tags = quotable.config.get(ATTR_SELECTED_TAGS, [])
+    selected_authors = quotable.config.get(ATTR_SELECTED_AUTHORS, [])
+
+    tags_param = "".join(selected_tags).replace(",", "|")
+    authors_param = "".join(selected_authors).replace(",", "|")
+
+    url = f"{FETCH_A_QUOTE_URL}?tags={tags_param}&author={authors_param}&limit=1"
+
+    response = await session.get(url, timeout=HTTP_CLIENT_TIMEOUT)
+
     if response.status == HTTPStatus.OK:
         data = await response.json()
         if data:
-            quote = {data["content"]: data["author"]}
-            return quote
+            quotes = {data["content"]: data["author"]}
+            return quotes
 
     return None
 
