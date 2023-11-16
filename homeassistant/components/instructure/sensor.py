4eee89cfbeb927948ce46b279e78db13006d470c
Caodongying <jsjhcdy@163.com>
2023-11-16 15:27:47 +0100

    Co-authored-by: Elias Kjellberg Carlson <eliaskjellbergcarlson@gmail.com> Co-authored-by: erikwessman <erikwessman@users.noreply.github.com> Co-authored-by: theowiik <theowiik@users.noreply.github.com>

--- homeassistant/components/instructure/sensor.py
+++ homeassistant/components/instructure/sensor.py
@@ -21,12 +21,18 @@ from .const import DOMAIN
 
 from .canvas_api import CanvasAPI
 
+ANNOUNCMENTS_KEY: str = "announcements"
+ASSIGNMENTS_KEY: str = "assignments"
+CONVERSATIONS_KEY: str = "conversations"
+
 
 @dataclass
 class BaseEntityDescriptionMixin:
     """Mixin for required Canvas base description keys."""
     value_fn: Callable[[dict[str, Any]], StateType]
     name_fn: Callable[[dict[str, Any]], StateType]
+    device_name: str
+
 
 @dataclass
 class BaseEntityDescription(SensorEntityDescription):
@@ -43,57 +49,42 @@ class CanvasSensorEntityDescription(BaseEntityDescription, BaseEntityDescription
     fetch_data: Callable = None
 
 
-SENSOR_DESCRIPTIONS: tuple[CanvasSensorEntityDescription, ...] = (
-    CanvasSensorEntityDescription(
-        key="upcoming_assignments",
-        translation_key="upcoming_assignments",
+SENSOR_DESCRIPTIONS: {str: CanvasSensorEntityDescription} = {
+    ASSIGNMENTS_KEY: CanvasSensorEntityDescription(
+        device_name="Upcoming Assignments",
+        key="temp1",
+        translation_key="temp1",
         icon="mdi:note-outline",
-        avabl_fn=lambda data: len(data) > 0,
-        name_fn=lambda _: "Assignments",
-        value_fn= lambda data: datetime_process(data[0]["due_at"]),
-        attr_fn=lambda data: {
-            'assignments': [{
-                'title': assignment['name'],
-                'description': assignment['description'],
-                'due_at': assignment['due_at'],
-            } for assignment in data]
-        },
+        avabl_fn=lambda data: data is not None,
+        name_fn=lambda data: data["name"],
+        value_fn= lambda data: datetime_process(data["due_at"]),
+        attr_fn=lambda data: {},
         fetch_data=lambda api, course_id: api.async_get_assignments(course_id),
     ),
-    CanvasSensorEntityDescription(
-        key="announcements",
-        translation_key="announcements",
+    ANNOUNCMENTS_KEY: CanvasSensorEntityDescription(
+        device_name="Announcements",
+        key="temp2",
+        translation_key="temp2",
         icon="mdi:message-alert",
-        avabl_fn=lambda data: len(data) > 0,
-        name_fn=lambda _: "Announcements",
-        value_fn= lambda data: data[0]["title"][:20],
-        attr_fn=lambda data: {
-            'messages': [{
-                'title': announcement['title'],
-                'date': announcement['posted_at'],
-                'author': announcement['user_name'],
-            } for announcement in data]
-        },
+        avabl_fn=lambda data: data is not None,
+        name_fn=lambda data: data["title"],
+        value_fn= lambda data: data["message"][:20],
+        attr_fn=lambda data: {},
         fetch_data=lambda api, course_id: api.async_get_announcements(course_id),
     ),
-)
+    CONVERSATIONS_KEY: CanvasSensorEntityDescription(
+        device_name="Inbox",
+        key="temp2",
+        translation_key="temp2",
+        icon="mdi:email",
+        avabl_fn=lambda data: data is not None,
+        name_fn=lambda data: data["audience"],
+        value_fn= lambda data: data["subject"][:20],
+        attr_fn=lambda data: {},
+        fetch_data=lambda api, _: api.async_get_conversations(),
+    )
+}
 
-inbox_sensor_entity_description = CanvasSensorEntityDescription(
-    key="inbox",
-    translation_key="inbox",
-    icon="mdi:email",
-    avabl_fn=lambda data: len(data) > 0,
-    name_fn=lambda _: "Inbox",
-    value_fn= lambda data: data[0]["subject"][:20],
-    attr_fn=lambda data: {
-        'messages': [{
-            'title': message['subject'],
-            'date': message['last_message_at'],
-            'author': message['participants'][0]['full_name'],
-        } for message in data]
-    },
-    fetch_data=lambda api, _: api.async_get_conversations(),
-)
 
 def datetime_process(date_time):
     standard_timestamp = datetime.fromisoformat(date_time.replace("Z", "+00:00"))
@@ -109,28 +100,22 @@ class CanvasSensorEntity(SensorEntity):
 
     def __init__(
         self,
-        api: CanvasAPI,
         entity_description: CanvasSensorEntityDescription,
-        course_name: str,
-        course_id: int,
+        data: dict[str, Any],
+        api: CanvasAPI,
     ) -> None:
         """Initialize a Canvas sensor."""
-        self.api = api
         self.entity_description = entity_description
-        self.course_name = course_name
-        self.course_id = course_id
-        self._attr_unique_id = f"{self.course_id}-{self.entity_description.key}"
+        self.data = data
+        self.api = api
+        self._attr_unique_id = f"{self.entity_description.name_fn(self.data)}"
 
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, self.entity_description.key)},
-            name=self.course_name,
+            identifiers={(DOMAIN, self.entity_description.device_name)},
+            name=self.entity_description.device_name,
             manufacturer="Canvas",
             entry_type=DeviceEntryType.SERVICE,
         )
-
-    async def async_update(self):
-        # TODO: Add try-catch
-        self.data = await self.entity_description.fetch_data(self.api, self.course_id)
 
     @property
     def name(self):
@@ -138,7 +123,7 @@ class CanvasSensorEntity(SensorEntity):
         if not self.available:
             return None
 
-        return f"{self.course_name} - {self.entity_description.name_fn(self.data)}"
+        return f"{self.entity_description.name_fn(self.data)}"
 
     @property
     def available(self) -> bool:
@@ -162,7 +147,6 @@ class CanvasSensorEntity(SensorEntity):
         """Return the extra state attributes."""
         return self.entity_description.attr_fn(self.data)
 
-
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
@@ -170,17 +154,106 @@ async def async_setup_entry(
 ) -> None:
     """Set up Canvas sensor based on a config entry"""
 
+    hass.data[DOMAIN][ASSIGNMENTS_KEY] = [
+        {
+            "id": 76160,
+            "due_at": "2023-08-30T21:59:59Z",
+            "course_id": 25271,
+            "name": "First Assignment",
+            "html_url": "https://chalmers.instructure.com/courses/25271/assignments/76160"
+        },
+        {
+            "id": 76161,
+            "due_at": "2023-09-30T21:59:59Z",
+            "course_id": 25271,
+            "name": "Second Assignment",
+            "html_url": "https://chalmers.instructure.com/courses/25271/assignments/76160"
+        },
+        {
+            "id": 76162,
+            "due_at": "2023-10-30T21:59:59Z",
+            "course_id": 25271,
+            "name": "Third Assignment",
+            "html_url": "https://chalmers.instructure.com/courses/25271/assignments/76160"
+        }
+    ]
+
+    hass.data[DOMAIN][ANNOUNCMENTS_KEY] = [
+        {
+            "title": "Hello!",
+            "posted_at": "2023-10-30T21:59:59Z",
+            "user_name": "Farnaz Fotrousi",
+            "message": "Oh noooooooo"
+        },
+        {
+            "title": "Hello again, my name is Farnaz! 😜",
+            "posted_at": "2023-10-30T21:59:59Z",
+            "user_name": "Farnaz Fotrousi",
+            "message": "I am FARNAZZZZZZ"
+        }
+    ]
+
+    hass.data[DOMAIN][CONVERSATIONS_KEY] = [
+    {
+        "id": 2424059,
+        "subject": "",
+        "workflow_state": "unread",
+        "last_message": "Reminder that lecture tomorrow Tuesday 14/11 is cancelled due to illness. See you on Friday inste...",
+        "last_message_at": "2023-11-13T21:40:39Z",
+        "last_authored_message": None,
+        "last_authored_message_at": None,
+        "message_count": 1,
+        "subscribed": True,
+        "private": False,
+        "starred": False,
+        "properties": [],
+        "audience": [
+            1439
+        ],
+        "audience_contexts": {
+            "courses": {
+                "26488": [
+                    "TeacherEnrollment"
+                ]
+            },
+            "groups": {}
+        },
+        "avatar_url": "https://chalmers.instructure.com/images/messages/avatar-50.png",
+        "participants": [
+            {
+                "id": 1439,
+                "name": "Ulf Assarsson",
+                "full_name": "Ulf Assarsson",
+                "pronouns": None
+            },
+            {
+                "id": 26768,
+                "name": "Theo Wiik",
+                "full_name": "Theo Wiik",
+                "pronouns": None
+            }
+        ],
+        "visible": True,
+        "context_code": "course_26488",
+        "context_name": "TDA362 / DIT224 Computer graphics"
+    },
+    ]
+
     api = hass.data[DOMAIN][entry.entry_id]
-    courses = entry.options["courses"] # k: course name, v: course id
 
-    entities = [CanvasSensorEntity(api, description, course_name, courses[course_name])
-            for description in SENSOR_DESCRIPTIONS
-            for course_name in courses]
+    assignment_entities = [create_entity(ASSIGNMENTS_KEY, assignment, api) for assignment in hass.data[DOMAIN][ASSIGNMENTS_KEY]]
+    announcement_entities = [create_entity(ANNOUNCMENTS_KEY, announcement, api) for announcement in hass.data[DOMAIN][ANNOUNCMENTS_KEY]]
+    inbox_entities = [create_entity(CONVERSATIONS_KEY, conversation, api) for conversation in hass.data[DOMAIN][CONVERSATIONS_KEY]]
 
-    entities.append(CanvasSensorEntity(api, inbox_sensor_entity_description, "", ""))
+    entities = assignment_entities + announcement_entities + inbox_entities
 
     async_add_entities(
-        tuple(entities),
-        update_before_add = True
+        tuple(entities)
     )
 
+
+def create_entity(data_type: str, data: dict[str, Any], api: CanvasAPI) -> CanvasSensorEntity:
+    entity_description = SENSOR_DESCRIPTIONS[data_type]
+    entity = CanvasSensorEntity(entity_description, data, api)
+
+    return entity
