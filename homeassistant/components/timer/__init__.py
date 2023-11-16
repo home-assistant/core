2249023ffaf7c44e9b6243cdcf9a40c2a8f556c5
Michael Hansen <mike@rhasspy.org>
2023-11-16 16:58:00 -0600

    Starting on timer platform

--- homeassistant/components/timer/__init__.py
+++ homeassistant/components/timer/__init__.py
@@ -8,6 +8,7 @@ from typing import Self
 
 import voluptuous as vol
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     ATTR_EDITABLE,
     ATTR_ENTITY_ID,
@@ -168,6 +169,18 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     )
 
     return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up a config entry."""
+    component: EntityComponent[Timer] = hass.data[DOMAIN]
+    return await component.async_setup_entry(entry)
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    component: EntityComponent[Timer] = hass.data[DOMAIN]
+    return await component.async_unload_entry(entry)
 
 
 class TimerStorageCollection(collection.DictStorageCollection):
