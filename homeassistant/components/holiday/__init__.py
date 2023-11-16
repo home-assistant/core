32a414d88cd6650504bd96eacdf9f8bc946fd873
Jan Rieger <jan@mainzelmaennchen.net>
2023-11-16 15:01:40 +0100

    cleanup

--- homeassistant/components/holiday/__init__.py
+++ homeassistant/components/holiday/__init__.py
@@ -5,14 +5,11 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
-
 PLATFORMS: list[Platform] = [Platform.CALENDAR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Holiday from a config entry."""
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
