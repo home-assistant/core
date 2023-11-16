3610b1c38d59951bd0224ed66648b7af53bfc2be
Erwin Douna <e.douna@gmail.com>
2023-11-16 21:07:58 +0100

    Update homeassistant/components/fastdotcom/config_flow.py
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- homeassistant/components/fastdotcom/config_flow.py
+++ homeassistant/components/fastdotcom/config_flow.py
@@ -36,7 +36,7 @@ class FastdotcomConfigFlow(ConfigFlow, domain=DOMAIN):
             self.hass,
             HOMEASSISTANT_DOMAIN,
             f"deprecated_yaml_{DOMAIN}",
-            breaks_in_ha_version="2024.5.0",
+            breaks_in_ha_version="2024.6.0",
             is_fixable=False,
             issue_domain=DOMAIN,
             severity=IssueSeverity.WARNING,
