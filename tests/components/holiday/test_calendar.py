76df3161d1f829ec383c46bac226ba5d15311ec6
Jan Rieger <jan@mainzelmaennchen.net>
2023-11-16 20:58:15 +0100

    Set up the integration and test the state

--- tests/components/holiday/test_calendar.py
+++ tests/components/holiday/test_calendar.py
@@ -1,39 +1,77 @@
 """Tests for calendar platform of Holiday integration."""
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from freezegun.api import FrozenDateTimeFactory
 
 from homeassistant.components.holiday import calendar
+from homeassistant.components.holiday.const import CONF_PROVINCE, DOMAIN
+from homeassistant.const import CONF_COUNTRY
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 from homeassistant.util import dt as dt_util
 
 from tests.common import MockConfigEntry
 
 
-async def test_async_get_events(
+async def assert_setup_calendar(hass: HomeAssistant) -> MockConfigEntry:
+    """Set up the calendar."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_COUNTRY: "US", CONF_PROVINCE: "AK"},
+        title="United States, AK",
+    )
+    config_entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    return config_entry
+
+
+async def test_holiday_calendar_entity(
     hass: HomeAssistant, freezer: FrozenDateTimeFactory
 ) -> None:
-    """Test events in a specific time frame."""
+    """Test HolidayCalendarEntity functionality."""
     freezer.move_to(datetime(2023, 1, 1, 12, tzinfo=dt_util.UTC))
 
-    config_entry = MockConfigEntry(
-        data={
-            "country": "US",
-            "province": "AK",
-        }
-    )
+    config_entry = await assert_setup_calendar(hass)
     entity = calendar.HolidayCalendarEntity(
         "United States, AK", "US", "AK", config_entry.entry_id
     )
 
     start_date = datetime(2023, 1, 1)
-    end_date = datetime(2023, 1, 2)
+    end_date = datetime(2023, 1, 1)
 
     events = await entity.async_get_events(hass, start_date, end_date)
 
-    # assert len(events) == 1
+    assert len(events) == 1
+
     assert events[0].summary == "New Year's Day"
-    assert events[0].start == datetime(2023, 1, 1).date()
-    assert events[0].end == datetime(2023, 1, 2).date()
+    assert events[0].start == start_date.date()
+    assert events[0].end == end_date.date() + timedelta(days=1)
     assert events[0].location == "United States, AK"
+
+
+async def test_async_get_events(
+    hass: HomeAssistant,
+    entity_registry: er.EntityRegistry,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test events in a specific time frame."""
+    freezer.move_to(datetime(2023, 1, 1, 12, tzinfo=dt_util.UTC))
+
+    await assert_setup_calendar(hass)
+
+    entity_id = "calendar.united_states_ak"
+    assert entity_id in entity_registry.entities
+
+    state = hass.states.get(entity_id)
+    assert state is not None
+
+    assert state.state == "on"
+
+    assert state.attributes.get("message") == "New Year's Day"
+    assert state.attributes.get("start_time") == "2023-01-01 00:00:00"
+    assert state.attributes.get("end_time") == "2023-01-02 00:00:00"
+    assert state.attributes.get("location") == "United States, AK"
