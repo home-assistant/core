76854b7f8e1aab41554419b2ff5ce0eeeeca89cd
1onemike <moaduamah@gmail.com>
2023-11-17 17:40:31 +0000

    show quotes from state attributes

--- config/www/quotable/card.js
+++ config/www/quotable/card.js
@@ -3,71 +3,116 @@ class QuotableCard extends HTMLElement {
     super();
     this.currentImageIndex = 0;
     this.imageUrls = [
-      "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+      "https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_1280.jpg",
     ];
+    this.updateInProgress = false;
+    this.DEFAULT_AUTHOR = "Quotable";
+    this.DEFAULT_QUOTE =
+      "Today is a gift, that's why they call it the present. Make the most of it!";
+  }
+
+  setConfig(config) {
+    this._config = config;
   }
 
   set hass(hass) {
+    this._hass = hass;
+    this.updateQuoteAndAuthor();
+  }
+
+  async updateQuoteAndAuthor() {
+    if (this.updateInProgress) {
+      return;
+    }
+
+    if (this._hass) {
+      this.updateInProgress = true;
+
+      try {
+        const entityState = this._hass.states[this._config.entity];
+
+        // Check if the entity state and its attributes exist
+        if (entityState && entityState.attributes) {
+          const attributes = entityState.attributes;
+
+          const quotes = JSON.parse(attributes.quotes) || [];
+
+          // Use the first quote and author from the state attributes
+          const quote =
+            quotes.length > 0 ? quotes[0].content : this.DEFAULT_QUOTE;
+          const author =
+            quotes.length > 0 ? quotes[0].author : this.DEFAULT_AUTHOR;
+
+          this.render(quote, author);
+        }
+      } catch (error) {
+        this.render(this.DEFAULT_QUOTE, this.DEFAULT_AUTHOR);
+      } finally {
+        this.updateInProgress = false;
+      }
+    }
+  }
+
+  render(quote = this.DEFAULT_QUOTE, author = this.DEFAULT_AUTHOR) {
     if (!this.content) {
       this.innerHTML = `
-                  <style>
-                      ha-card {
-                        position: relative;
-                        overflow: hidden;
-                        height: 200px;
-                      }
-                      .background-image {
-                        width: 100%;
-                        height: 100%;
-                        background-size:     cover;
-                        background-repeat:   no-repeat;
-                        background-position: center center;
-                      }
+        <style>
+          ha-card {
+            position: relative;
+            overflow: hidden;
+            height: 200px;
+          }
+          .background-image {
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center center;
 
-                      .overlay {
-                        position: absolute;
-                        top: 0;
-                        left: 0;
-                        width: 100%;
-                        height: 100%;
-                        display: flex;
-                        flex-direction: column;
-                        justify-content: center;
-                        align-items: center;
-                        color: white;
-                        text-align: center;
-                      }
-
-                      .quote {
-                        font-size: 24px;
-                        font-weight: bold;
-                        margin-bottom: 16px;
-                      }
-
-                      .author {
-                        font-size: 18px;
-                        font-weight: bold;
-
-                      }
-                  </style>
-                  <ha-card>
-                      <img class="background-image" src="${
-                        this.imageUrls[this.currentImageIndex]
-                      }">
-                      <div class="overlay">
-                      <div class="quote">"Quote goes here"</div>
-                      <div class="author">- Author Name</div>
-                      </div>
-                  </ha-card>
-              `;
+          }
+          .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            color: white;
+            text-align: center;
+          }
+          .quote {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 16px;
+          }
+          .author {
+            font-size: 18px;
+            font-weight: bold;
+          }
+        </style>
+        <ha-card>
+          <img class="background-image" src="${
+            this.imageUrls[this.currentImageIndex]
+          }">
+          <div class="overlay">
+            <div class="quote">${quote}</div>
+            <div class="author">- ${author}</div>
+          </div>
+        </ha-card>
+      `;
       this.content = this.querySelector(".background-image");
     }
   }
 
-  setConfig(config) {}
-
   getCardSize() {
     return 3;
+  }
+
+  disconnectedCallback() {
+    clearInterval(this._updateInterval);
   }
 
   static getStubConfig() {
@@ -76,7 +121,7 @@ class QuotableCard extends HTMLElement {
 }
 
 customElements.define("quotable-card", QuotableCard);
-//Add card to card picker in UI
+
 window.customCards = window.customCards || [];
 window.customCards.push({
   type: "quotable-card",
