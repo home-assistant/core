3bd7ea61eb67d0b352f38b1e29fad74a5054efa8
Jan Rieger <jan@mainzelmaennchen.net>
2023-11-17 11:04:38 +0100

    Test that configuring more than one instance is rejected

--- tests/components/holiday/test_config_flow.py
+++ tests/components/holiday/test_config_flow.py
@@ -9,6 +9,8 @@ from homeassistant.const import CONF_COUNTRY
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
+from tests.common import MockConfigEntry
+
 pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 
 
@@ -66,3 +68,42 @@ async def test_form_no_subdivision(hass: HomeAssistant) -> None:
     assert result2["data"] == {
         "country": "SE",
     }
+
+
+async def test_single_instance(hass: HomeAssistant) -> None:
+    """Test that configuring more than one instance is rejected."""
+    data_de = {
+        CONF_COUNTRY: "DE",
+        CONF_PROVINCE: "BW",
+    }
+    data_se = {
+        CONF_COUNTRY: "SE",
+    }
+    MockConfigEntry(domain=DOMAIN, data=data_de).add_to_hass(hass)
+    MockConfigEntry(domain=DOMAIN, data=data_se).add_to_hass(hass)
+
+    # Test for country without subdivisions
+    result_se = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": config_entries.SOURCE_USER},
+        data=data_se,
+    )
+    assert result_se["type"] == FlowResultType.ABORT
+    assert result_se["reason"] == "already_configured"
+
+    # Test for country with subdivisions
+    result_de_step1 = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": config_entries.SOURCE_USER},
+        data=data_de,
+    )
+    assert result_de_step1["type"] == FlowResultType.FORM
+
+    result_de_step2 = await hass.config_entries.flow.async_configure(
+        result_de_step1["flow_id"],
+        {
+            CONF_PROVINCE: data_de[CONF_PROVINCE],
+        },
+    )
+    assert result_de_step2["type"] == FlowResultType.ABORT
+    assert result_de_step2["reason"] == "already_configured"
