e8171de2833fe2e1d86796e2933c3ee486805dee
Jan Rieger <jrieger@users.noreply.github.com>
2023-11-18 09:43:50 +0100

    Update tests/components/holiday/test_init.py
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- tests/components/holiday/test_init.py
+++ tests/components/holiday/test_init.py
@@ -30,6 +30,8 @@ async def test_unload_entry(hass: HomeAssistant) -> None:
 
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
+    
+    assert entry.state == ConfigEntryState.LOADED
 
     assert await hass.config_entries.async_unload(entry.entry_id)
     await hass.async_block_till_done()
