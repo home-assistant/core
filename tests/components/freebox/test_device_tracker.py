4148c32d5f7feba54375b4ca6111ec2c4aac077d
jean-francois.lefebvre <jean-francois.lefebvre@soprasteria.com>
2023-11-18 22:16:36 +0000

    Format

--- tests/components/freebox/test_device_tracker.py
+++ tests/components/freebox/test_device_tracker.py
@@ -3,8 +3,8 @@ from unittest.mock import Mock
 
 from freezegun.api import FrozenDateTimeFactory
 
+from homeassistant.components.device_tracker import DOMAIN as DEVICE_TRACKER_DOMAIN
 from homeassistant.components.freebox import SCAN_INTERVAL
-from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.core import HomeAssistant
 
 from .common import setup_platform
@@ -18,7 +18,7 @@ async def test_router_mode(
     router: Mock,
 ) -> None:
     """Test get_hosts_list invoqued multiple times if freebox into router mode."""
-    await setup_platform(hass, SENSOR_DOMAIN)
+    await setup_platform(hass, DEVICE_TRACKER_DOMAIN)
 
     assert router().lan.get_hosts_list.call_count == 1
 
@@ -36,7 +36,7 @@ async def test_bridge_mode(
     router_bridge_mode: Mock,
 ) -> None:
     """Test get_hosts_list invoqued once if freebox into bridge mode."""
-    await setup_platform(hass, SENSOR_DOMAIN)
+    await setup_platform(hass, DEVICE_TRACKER_DOMAIN)
 
     # Simulate an update
     freezer.tick(SCAN_INTERVAL)
