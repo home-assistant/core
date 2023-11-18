269aab07ca0d1eecd4087359ed791d91af44fc2f
Caius-Bonus <123886836+Caius-Bonus@users.noreply.github.com>
2023-11-18 15:46:10 +0100

    use quirk id from zha_quirks

--- homeassistant/components/zha/core/cluster_handlers/manufacturerspecific.py
+++ homeassistant/components/zha/core/cluster_handlers/manufacturerspecific.py
@@ -5,7 +5,7 @@ import logging
 from typing import TYPE_CHECKING, Any
 
 from zhaquirks.inovelli.types import AllLEDEffectType, SingleLEDEffectType
-from zhaquirks.quirk_ids import TUYA_PLUG_MANUFACTURER
+from zhaquirks.quirk_ids import TUYA_PLUG_MANUFACTURER, XIAOMI_AQARA_VIBRATION_AQ1
 import zigpy.zcl
 from zigpy.zcl.clusters.closures import DoorLock
 
@@ -408,7 +408,7 @@ class IkeaRemote(ClusterHandler):
 
 
 @registries.ZIGBEE_CLUSTER_HANDLER_REGISTRY.register(
-    DoorLock.cluster_id, "xiaomi.aqara_vibration_aq1"
+    DoorLock.cluster_id, XIAOMI_AQARA_VIBRATION_AQ1
 )
 class XiaomiVibrationAQ1ClusterHandler(MultistateInput):
     """Xiaomi DoorLock Cluster is in fact a MultiStateInput Cluster."""
