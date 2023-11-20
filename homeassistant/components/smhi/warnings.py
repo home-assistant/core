ee5aa23f47768a2348bd3d4336334f9c35d675cf
Faezeh <faezehmhmdi78@gmail.com>
2023-11-20 13:54:33 +0000

    refactor: reduced parse_warnings function complexity.

--- homeassistant/components/smhi/warnings.py
+++ homeassistant/components/smhi/warnings.py
@@ -24,53 +24,55 @@ class SmhiWarnings:
         """Parse warning data from smhi api."""
         geo_location_entities = []
 
-        warnings = []
-
-        for warning in data:
-            parsed_warning = {
-                "id": warning.get("id"),
-                "normalProbability": warning.get("normalProbability"),
-                "event": warning.get("event", {}),
-                "descriptions": warning.get("descriptions", []),
-            }
-            parsed_warning["warningAreas"] = []
-            for area in warning.get("warningAreas", []):
-                parsed_area = {
-                    "id": area.get("id"),
-                    "approximateStart": area.get("approximateStart"),
-                    "approximateEnd": area.get("approximateEnd"),
-                    "published": area.get("published"),
-                    "normalProbability": area.get("normalProbability"),
-                    "areaName": area.get("areaName", {}),
-                    "warningLevel": area.get("warningLevel", {}),
-                    "eventDescription": area.get("eventDescription", {}),
-                    "affectedAreas": area.get("affectedAreas", []),
-                    "descriptions": area.get("descriptions", []),
-                    "geometry": area.get("area", {})
-                    .get("features", [{}])[0]
-                    .get("geometry", {}),
-                }
-                parsed_warning["warningAreas"].append(parsed_area)
-
-            warnings.append(parsed_warning)
+        warnings = [self.parse_warning(warning) for warning in data]
 
         for warning in warnings:
-            name = str(warning["warningAreas"][0]["descriptions"][0]["text"]["en"])
-
-            if "coordinates" in warning["warningAreas"][0]["geometry"]:
-                if len(warning["warningAreas"][0]["geometry"]["coordinates"]) == 1:
-                    for location in warning["warningAreas"][0]["geometry"][
-                        "coordinates"
-                    ][0]:
-                        geo_location_entities.append(
-                            SmhiGeolocationEvent(name, location[1], location[0])
-                        )
-                else:
-                    for location in warning["warningAreas"][0]["geometry"][
-                        "coordinates"
-                    ]:
-                        geo_location_entities.append(
-                            SmhiGeolocationEvent(name, location[1], location[0])
-                        )
+            geo_location_entities.extend(self.create_geo_location_entities(warning))
 
         return geo_location_entities
+
+    def parse_warning(self, warning: dict[str, Any]) -> dict:
+        """Parse individual warning."""
+        parsed_warning = {
+            "id": warning.get("id"),
+            "normalProbability": warning.get("normalProbability"),
+            "event": warning.get("event", {}),
+            "descriptions": warning.get("descriptions", []),
+            "warningAreas": [
+                self.parse_warning_area(area)
+                for area in warning.get("warningAreas", [])
+            ],
+        }
+        return parsed_warning
+
+    def parse_warning_area(self, area: dict[str, Any]) -> dict:
+        """Parse individual warning area."""
+        return {
+            "id": area.get("id"),
+            "approximateStart": area.get("approximateStart"),
+            "approximateEnd": area.get("approximateEnd"),
+            "published": area.get("published"),
+            "normalProbability": area.get("normalProbability"),
+            "areaName": area.get("areaName", {}),
+            "warningLevel": area.get("warningLevel", {}),
+            "eventDescription": area.get("eventDescription", {}),
+            "affectedAreas": area.get("affectedAreas", []),
+            "descriptions": area.get("descriptions", []),
+            "geometry": area.get("area", {})
+            .get("features", [{}])[0]
+            .get("geometry", {}),
+        }
+
+    def create_geo_location_entities(self, warning: dict) -> list[SmhiGeolocationEvent]:
+        """Create geo location entities from warning."""
+        entities = []
+        name = str(warning["warningAreas"][0]["descriptions"][0]["text"]["en"])
+
+        coordinates = warning["warningAreas"][0]["geometry"].get("coordinates", [])
+        if len(coordinates) == 1:
+            coordinates = coordinates[0]
+
+        for location in coordinates:
+            entities.append(SmhiGeolocationEvent(name, location[1], location[0]))
+
+        return entities
