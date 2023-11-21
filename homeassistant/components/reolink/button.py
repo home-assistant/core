45c1366ae74ca8592ac34d11e9b5513454bb2455
starkillerOG <starkiller.og@gmail.com>
2023-11-21 23:40:58 +0100

    Add custom ptz speed service

--- homeassistant/components/reolink/button.py
+++ homeassistant/components/reolink/button.py
@@ -17,11 +17,14 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback, async_get_current_platform
 
 from . import ReolinkData
 from .const import DOMAIN
 from .entity import ReolinkChannelCoordinatorEntity, ReolinkHostCoordinatorEntity
+
+ATTR_SPEED = "speed"
+SUPPORT_PTZ_SPEED = 1024
 
 
 @dataclass(kw_only=True)
@@ -33,6 +36,7 @@ class ReolinkButtonEntityDescription(
     enabled_default: Callable[[Host, int], bool] | None = None
     method: Callable[[Host, int], Any]
     supported: Callable[[Host, int], bool] = lambda api, ch: True
+    ptz_cmd: str | None = None
 
 
 @dataclass(kw_only=True)
@@ -59,6 +63,7 @@ BUTTON_ENTITIES = (
         icon="mdi:pan",
         supported=lambda api, ch: api.supported(ch, "pan"),
         method=lambda api, ch: api.set_ptz_command(ch, command=PtzEnum.left.value),
+        ptz_cmd=PtzEnum.left.value,
     ),
     ReolinkButtonEntityDescription(
         key="ptz_right",
@@ -66,6 +71,7 @@ BUTTON_ENTITIES = (
         icon="mdi:pan",
         supported=lambda api, ch: api.supported(ch, "pan"),
         method=lambda api, ch: api.set_ptz_command(ch, command=PtzEnum.right.value),
+        ptz_cmd=PtzEnum.right.value,
     ),
     ReolinkButtonEntityDescription(
         key="ptz_up",
@@ -73,6 +79,7 @@ BUTTON_ENTITIES = (
         icon="mdi:pan",
         supported=lambda api, ch: api.supported(ch, "tilt"),
         method=lambda api, ch: api.set_ptz_command(ch, command=PtzEnum.up.value),
+        ptz_cmd=PtzEnum.up.value,
     ),
     ReolinkButtonEntityDescription(
         key="ptz_down",
@@ -80,6 +87,7 @@ BUTTON_ENTITIES = (
         icon="mdi:pan",
         supported=lambda api, ch: api.supported(ch, "tilt"),
         method=lambda api, ch: api.set_ptz_command(ch, command=PtzEnum.down.value),
+        ptz_cmd=PtzEnum.down.value,
     ),
     ReolinkButtonEntityDescription(
         key="ptz_zoom_in",
@@ -88,6 +96,7 @@ BUTTON_ENTITIES = (
         entity_registry_enabled_default=False,
         supported=lambda api, ch: api.supported(ch, "zoom_basic"),
         method=lambda api, ch: api.set_ptz_command(ch, command=PtzEnum.zoomin.value),
+        ptz_cmd=PtzEnum.zoomin.value,
     ),
     ReolinkButtonEntityDescription(
         key="ptz_zoom_out",
@@ -96,6 +105,7 @@ BUTTON_ENTITIES = (
         entity_registry_enabled_default=False,
         supported=lambda api, ch: api.supported(ch, "zoom_basic"),
         method=lambda api, ch: api.set_ptz_command(ch, command=PtzEnum.zoomout.value),
+        ptz_cmd=PtzEnum.zoomout.value,
     ),
     ReolinkButtonEntityDescription(
         key="ptz_calibrate",
@@ -157,6 +167,14 @@ async def async_setup_entry(
     )
     async_add_entities(entities)
 
+    platform = async_get_current_platform()
+    platform.async_register_entity_service(
+        "set_ptz_speed",
+        {vol.Required(ATTR_SPEED): cv.positive_int},
+        "async_ptz_speed",
+        [SUPPORT_PTZ_SPEED],
+    )
+
 
 class ReolinkButtonEntity(ReolinkChannelCoordinatorEntity, ButtonEntity):
     """Base button entity class for Reolink IP cameras."""
@@ -181,10 +199,21 @@ class ReolinkButtonEntity(ReolinkChannelCoordinatorEntity, ButtonEntity):
                 entity_description.enabled_default(self._host.api, self._channel)
             )
 
+        if self._host.api.supported(channel, "ptz_speed") and entity_description.ptz_cmd is not None:
+            self._attr_supported_features = SUPPORT_PTZ_SPEED
+
     async def async_press(self) -> None:
         """Execute the button action."""
         try:
             await self.entity_description.method(self._host.api, self._channel)
+        except ReolinkError as err:
+            raise HomeAssistantError(err) from err
+
+    async def async_ptz_speed(self, **kwargs) -> None:
+        """PTZ move with speed."""
+        speed = kwargs[ATTR_SPEED]
+        try:
+            await self._host.api.set_ptz_command(self._channel, command=ptz_cmd, speed=speed)
         except ReolinkError as err:
             raise HomeAssistantError(err) from err
 
