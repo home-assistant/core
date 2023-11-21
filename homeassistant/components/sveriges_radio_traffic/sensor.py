0b5c969ecbb932936f99708350fe991b8a8ec397
linwahlg <70467935+linwahlg@users.noreply.github.com>
2023-11-21 09:38:09 +0000

    Revert SR traffic back to working MVP
    Co-authored-by: Markus-Moen <Markus-Moen@users.noreply.github.com>

--- homeassistant/components/sveriges_radio_traffic/sensor.py
+++ homeassistant/components/sveriges_radio_traffic/sensor.py
@@ -8,14 +8,13 @@ import defusedxml.ElementTree as defET
 import requests
 
 from homeassistant.components.select import SelectEntity
+from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import CONF_AREA_NAME, DATE, DOMAIN, INFO
-from .coordinator import SverigesRadioTrafficCoordinator
 
 # dothis: Check trafikverket Train and try to do something similar. May be difficult/impossible to "dynamically" update location with sensor integration.
 # Instead set it during initial set up.
@@ -29,14 +28,11 @@ async def async_setup_entry(
     """Set up the sr traffic platform."""
     # Add entry to trafficsensor constructor
     area = entry.data.get("area")
-    entry.data.get("name")
+    titlename = entry.data.get("name")
     # print("Name i sensor:", name)
     names = INFO  # ["message", "time", "area"]
-
-    coordinator: SverigesRadioTrafficCoordinator = hass.data[DOMAIN][entry.entry_id]
-
     async_add_entities(
-        [TrafficSensor(hass, area, name, entry, coordinator) for name in names]
+        [TrafficSensor(hass, area, name, entry, titlename) for name in names]
     )
 
 
@@ -60,24 +56,21 @@ class MySelect(SelectEntity):
         """Change the selected option."""
 
 
-class TrafficSensor(CoordinatorEntity[SverigesRadioTrafficCoordinator]):
+class TrafficSensor(SensorEntity):
     """A class for the Sveriges Radio traffic sensor."""
 
     _attr_has_entity_name = True
+    # _attr_name = "Message"
     _attr_device_class = None
 
     def __init__(
-        self,
-        hass: HomeAssistant | None,
-        area,
-        name,
-        entry,
-        coordinator: SverigesRadioTrafficCoordinator,
+        self, hass: HomeAssistant | None, area, name, entry, titlename
     ) -> None:
         """Initialize the sensor."""
-        super().__init__(coordinator)
+        # do stuff
         self.entry = entry
         api_name, description_name, icon = name
+        self.thehass = hass
         self._attr_name = description_name
         self.api_name = api_name
         self._attr_native_value = (
@@ -94,15 +87,28 @@ class TrafficSensor(CoordinatorEntity[SverigesRadioTrafficCoordinator]):
             identifiers={(DOMAIN, entry.entry_id)},
             configuration_url="https://api.trafikinfo.trafikverket.se/",
         )
-        self._update_attr()
 
-    @callback
-    def _update_attr(self) -> None:
-        """Update _attr."""
-        # print(self.coordinator.data)
-        tree = self.coordinator.data
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+
+        # The stuff we send to the config entry is not saved in the config entry (After first setup)
+        # domainsaver = self.hass.config_entries.async_entries(domain=DOMAIN)
+        self.traffic_area = self.entry.data.get("area")
+        self._attr_native_value = self._get_traffic_info()
+
+    def _get_traffic_info(self) -> str | None:
+        """Fetch traffic information from specific area."""
+        # Obs! Below is dangerous if traffic_area isn't set, add security
+        response = requests.get(
+            "http://api.sr.se/api/v2/traffic/messages?trafficareaname="
+            + self.traffic_area,
+            timeout=10,
+        )
+        tree = defET.fromstring(response.content)
         return_string = ""
-
         if self._attr_name == CONF_AREA_NAME:
             return self.traffic_area
 
@@ -113,48 +119,7 @@ class TrafficSensor(CoordinatorEntity[SverigesRadioTrafficCoordinator]):
             (date, _, time) = return_string.partition("T")
             return_string = time[0:5] + ", " + date
 
-        self._attr_native_value = return_string
-        # self._attr_native_value = self.entity_description.value_fn(
-        #     self.coordinator.data
-        # )
-
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        self._update_attr()
-        return super()._handle_coordinator_update()
-
-    # def update(self) -> None:
-    #     """Fetch new state data for the sensor.
-
-    #     This is the only method that should fetch new data for Home Assistant.
-    #     """
-
-    #     # The stuff we send to the config entry is not saved in the config entry (After first setup)
-    #     # domainsaver = self.hass.config_entries.async_entries(domain=DOMAIN)
-    #     self.traffic_area = self.entry.data.get("area")
-    #     self._attr_native_value = self._get_traffic_info()
-
-    # def _get_traffic_info(self) -> str | None:
-    #     """Fetch traffic information from specific area."""
-    #     # Obs! Below is dangerous if traffic_area isn't set, add security
-    #     response = requests.get(
-    #         "http://api.sr.se/api/v2/traffic/messages?trafficareaname="
-    #         + self.traffic_area,
-    #         timeout=10,
-    #     )
-    #     tree = defET.fromstring(response.content)
-    #     return_string = ""
-    #     if self._attr_name == CONF_AREA_NAME:
-    #         return self.traffic_area
-
-    #     for message in tree.findall(".//message"):
-    #         return_string = message.find(self.api_name).text
-
-    #     if self.api_name == DATE:
-    #         (date, _, time) = return_string.partition("T")
-    #         return_string = time[0:5] + ", " + date
-
-    #     return return_string
+        return return_string
 
     # If we are going with Trafikverket plan then this method is probably not needed
     def _get_traffic_areas(self) -> str:
