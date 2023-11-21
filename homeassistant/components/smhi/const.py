956d57c5580b6d56b3e1cdff3ed4f29aedd91c0d
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2023-11-21 00:12:47 +0000

    Added icon for weather conditions

--- homeassistant/components/smhi/const.py
+++ homeassistant/components/smhi/const.py
@@ -18,3 +18,15 @@ smhi_warning_icons = {
     "ORANGE": "https://opendata.smhi.se/apidocs/IBWwarnings/res/orange-weather-warning-56x56.png",
     "RED": "https://opendata.smhi.se/apidocs/IBWwarnings/res/red-weather-warning-56x56.png",
 }
+
+# SOURCES
+# SUN: https://www.flaticon.com/free-icon/sun_6974833
+# CLOUD: https://www.flaticon.com/free-icon/cloud-computing_3208676
+# RAIN: https://uxwing.com/water-droplet-icon/
+# SNOWFLAKE: https://uxwing.com/snowflake-color-icon/
+weather_icons = {
+    "SUN": "https://cdn-icons-png.flaticon.com/512/6974/6974833.png",
+    "CLOUD": "https://cdn-icons-png.flaticon.com/512/3208/3208676.png",
+    "RAIN": "https://uxwing.com/wp-content/themes/uxwing/download/weather/water-droplet-icon.png",
+    "SNOWFLAKE": "https://uxwing.com/wp-content/themes/uxwing/download/weather/snowflake-color-icon.png",
+}
