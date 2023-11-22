4f24f7fe2514b0edcdf2728dde8addbe89597a6d
RasOtte <Rasmus.otterlind@gmail.com>
2023-11-22 09:31:18 +0000

    Map image and radar image are now combined into one image, It also displays the latest radar image

--- homeassistant/components/smhi/image.py
+++ homeassistant/components/smhi/image.py
@@ -1,8 +1,10 @@
 """Helps expose radar imageEntity."""
 import datetime
+from io import BytesIO
 import logging
 
 import aiohttp
+from PIL import Image, UnidentifiedImageError
 
 from homeassistant.components.image import ImageEntity
 from homeassistant.config_entries import ConfigEntry
@@ -36,12 +38,13 @@ class RadarImage(ImageEntity):
     """Representation of a image entity to display radar images."""
 
     async def async_image(self) -> bytes | None:
-        """Fetch and return bytes of SMHI radar map."""
+        """Fetch and return bytes of SMHI radar image and corresponding map combined."""
         try:
+            # Get the current date in Y/M/D format
             current_date = datetime.datetime.now().strftime("%Y/%m/%d")
 
             async with get_async_client(hass=self.hass) as client:
-                # Replace this URL with the actual SMHI API endpoint for radar map
+                # Get response from smhi API with radar png links
                 response = await client.get(
                     f"https://opendata-download-radar.smhi.se/api/version/latest/area/sweden/product/comp/{current_date}?timeZone=Europe/Stockholm"
                 )
@@ -50,6 +53,7 @@ class RadarImage(ImageEntity):
                 # Parse the JSON response
                 data = response.json()
 
+                # Find the png links
                 png_links = [
                     format_link["link"]
                     for file in data["files"]
@@ -58,21 +62,48 @@ class RadarImage(ImageEntity):
                     if "png" in format_link.get("key", "").lower()
                 ]
 
-                # Assuming you want to display the first PNG image, you can modify this logic
+                # Use the last link in the list as that is the latest radar, image
                 if png_links:
-                    self._attr_image_url = png_links[0]
+                    self._attr_image_url = png_links[-1]
 
-                    # Fetch the PNG image
+                    # Fetch Radar PNG image
                     png_response = await client.get(str(self._attr_image_url))
                     png_response.raise_for_status()
 
-                    self._attr_content_type = "image/png"
-                    self.async_write_ha_state()
+                    # Fetch the map
+                    actual_map_response = await client.get(
+                        "https://sid-proxy.smhi.se/radar/assets/basemap.53dbcde3.png"
+                    )
+                    actual_map_response.raise_for_status()
 
-                    return png_response.content
+                    # Open the radar and map image to combine them
+                    radar_image_content = png_response.read()
+                    radar_image_io = BytesIO(radar_image_content)
+                    radar_image = Image.open(radar_image_io)
+
+                    actual_map_content = actual_map_response.read()
+                    actual_map_io = BytesIO(actual_map_content)
+                    actual_map_image = Image.open(actual_map_io)
+
+                    # Resize radar image to match actual map dimensions (adjust as needed)
+                    radar_image = radar_image.resize(actual_map_image.size)
+
+                    # Combine the actual map and radar images using alpha compositing
+                    combined_image = Image.alpha_composite(
+                        actual_map_image.convert("RGBA"), radar_image.convert("RGBA")
+                    )
+
+                    # Convert the combined image to bytes and set entity attributes
+                    output_bytes = BytesIO()
+                    combined_image.save(output_bytes, format="PNG")
+                    self._attr_content_type = "image/png"
+
+                    return output_bytes.getvalue()
         except aiohttp.ClientError:
-            # Handle request errors (e.g., network issues)
-            _LOGGER.error("Failed to connect to SMHI API, retry in 5 minutes")
+            _LOGGER.error("Failed to connect to SMHI API")
+            return None
+        except UnidentifiedImageError as img_err:
+            _LOGGER.error("Error opening image retrieved from SMHI API: %s", img_err)
             return None
         return None
 
