1b665c1251f0a8027d35885dd12b594f6ffe8424
Luke <conway220@gmail.com>
2023-11-22 09:45:06 -0500

    remove duplicated test

--- tests/components/roborock/test_init.py
+++ tests/components/roborock/test_init.py
@@ -120,15 +120,3 @@ async def test_reauth_started(
     flows = hass.config_entries.flow.async_progress()
     assert len(flows) == 1
     assert flows[0]["step_id"] == "reauth_confirm"
-
-
-async def test_config_entry_not_ready_home_data(
-    hass: HomeAssistant, mock_roborock_entry: MockConfigEntry
-) -> None:
-    """Test that when we fail to get home data, entry retries."""
-    with patch(
-        "homeassistant.components.roborock.RoborockApiClient.get_home_data",
-        side_effect=RoborockException(),
-    ):
-        await async_setup_component(hass, DOMAIN, {})
-        assert mock_roborock_entry.state is ConfigEntryState.SETUP_RETRY
