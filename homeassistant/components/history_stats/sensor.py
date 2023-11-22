3d1aa991de907800abbc3c69eb06869a5f7564c4
Thierry Duvernoy <tduvernoy@free.fr>
2023-11-23 00:19:57 +0100

    Fix history stats time increment accuracy (switching accuracy from about 36s to 1s)

--- homeassistant/components/history_stats/sensor.py
+++ homeassistant/components/history_stats/sensor.py
@@ -176,7 +176,7 @@ class HistoryStatsSensor(HistoryStatsSensorBase):
         if self._type == CONF_TYPE_TIME:
             value = state.seconds_matched / 3600
             if self._attr_unique_id is None:
-                value = round(value, 2)
+                value = round(value, 10)
             self._attr_native_value = value
         elif self._type == CONF_TYPE_RATIO:
             self._attr_native_value = pretty_ratio(state.seconds_matched, state.period)
