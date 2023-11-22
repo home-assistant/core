a17c6a12b51ac17014771bf0054390863541f27a
Jan Rieger <jan@mainzelmaennchen.net>
2023-11-22 15:34:59 +0100

    Refactor tests

--- tests/components/holiday/test_calendar.py
+++ tests/components/holiday/test_calendar.py
@@ -1,32 +1,21 @@
 """Tests for calendar platform of Holiday integration."""
-
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from freezegun.api import FrozenDateTimeFactory
 
+from homeassistant.components.calendar import (
+    DOMAIN as CALENDAR_DOMAIN,
+    SERVICE_GET_EVENTS,
+)
 from homeassistant.components.holiday import calendar
 from homeassistant.components.holiday.const import CONF_PROVINCE, DOMAIN
 from homeassistant.const import CONF_COUNTRY
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
+from homeassistant.setup import async_setup_component
 from homeassistant.util import dt as dt_util
 
 from tests.common import MockConfigEntry
-
-
-async def assert_setup_calendar(hass: HomeAssistant) -> MockConfigEntry:
-    """Set up the calendar."""
-    config_entry = MockConfigEntry(
-        domain=DOMAIN,
-        data={CONF_COUNTRY: "CH", CONF_PROVINCE: "GE"},
-        title="Switzerland, GE",
-    )
-    config_entry.add_to_hass(hass)
-
-    await hass.config_entries.async_setup(config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    return config_entry
 
 
 async def test_holiday_calendar_entity(
@@ -37,52 +26,87 @@ async def test_holiday_calendar_entity(
     """Test HolidayCalendarEntity functionality."""
     freezer.move_to(datetime(2023, 1, 1, 12, tzinfo=dt_util.UTC))
 
-    config_entry = await assert_setup_calendar(hass)
-    entity = calendar.HolidayCalendarEntity(
-        hass, "Switzerland, GE", "CH", "GE", config_entry.entry_id
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_COUNTRY: "US", CONF_PROVINCE: "AK"},
+        title="United States, AK",
     )
-    entity.hass = hass
+    config_entry.add_to_hass(hass)
 
-    start_date = datetime(2023, 1, 1)
-    end_date = datetime(2023, 1, 1)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
 
-    events = await entity.async_get_events(hass, start_date, end_date)
+    await async_setup_component(hass, "calendar", {"calendar": {"platform": "demo"}})
+    await hass.async_block_till_done()
 
-    assert len(events) == 1
-
-    assert events[0].summary == "Neujahrestag"
-    assert events[0].start == start_date.date()
-    assert events[0].end == end_date.date() + timedelta(days=1)
-    assert events[0].location == "Switzerland, GE"
-
-    hass.config.language = "fr"
-    entity = calendar.HolidayCalendarEntity(
-        hass, "Switzerland, GE", "CH", "GE", config_entry.entry_id
+    response = await hass.services.async_call(
+        CALENDAR_DOMAIN,
+        SERVICE_GET_EVENTS,
+        {
+            "entity_id": "calendar.united_states_ak",
+            "end_date_time": dt_util.now(),
+        },
+        blocking=True,
+        return_response=True,
     )
+    assert response == {
+        "calendar.united_states_ak": {
+            "events": [
+                {
+                    "start": "2023-01-01",
+                    "end": "2023-01-02",
+                    "summary": "New Year's Day",
+                    "location": "United States, AK",
+                }
+            ]
+        }
+    }
 
-    events = await entity.async_get_events(hass, start_date, end_date)
-    assert events[0].summary == "Nouvel An"
+    state = hass.states.get("calendar.united_states_ak")
+    assert state is not None and state.state == "on"
 
 
-async def test_async_get_events(
+async def test_default_language(
     hass: HomeAssistant,
     entity_registry: er.EntityRegistry,
     freezer: FrozenDateTimeFactory,
 ) -> None:
-    """Test events in a specific time frame."""
+    """Test default language."""
     freezer.move_to(datetime(2023, 1, 1, 12, tzinfo=dt_util.UTC))
 
-    await assert_setup_calendar(hass)
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_COUNTRY: "FR", CONF_PROVINCE: "BL"},
+        title="France, BL",
+    )
+    config_entry.add_to_hass(hass)
 
-    entity_id = "calendar.switzerland_ge"
-    assert entity_id in entity_registry.entities
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
 
-    state = hass.states.get(entity_id)
-    assert state is not None
+    entity = calendar.HolidayCalendarEntity(
+        hass, "United States, AK", "US", "AK", config_entry.entry_id
+    )
+    assert entity._default_language == "en_US"
 
-    assert state.state == "on"
+    # holidays has only "en" translations for Canada
+    hass.config.language = "en"
+    entity = calendar.HolidayCalendarEntity(
+        hass, "Canada, AB", "CA", "AB", config_entry.entry_id
+    )
+    assert entity._default_language == "en"
 
-    assert state.attributes.get("message") == "Neujahrestag"
-    assert state.attributes.get("start_time") == "2023-01-01 00:00:00"
-    assert state.attributes.get("end_time") == "2023-01-02 00:00:00"
-    assert state.attributes.get("location") == "Switzerland, GE"
+    # Test French calendar with English language
+    entity = calendar.HolidayCalendarEntity(
+        hass, "France, BL", "FR", "BL", config_entry.entry_id
+    )
+    events = await entity.async_get_events(hass, dt_util.now(), dt_util.now())
+    assert events[0].summary == "New Year's Day"
+
+    # Test French calendar with French language
+    hass.config.language = "fr"
+    entity = calendar.HolidayCalendarEntity(
+        hass, "France, BL", "FR", "BL", config_entry.entry_id
+    )
+    events = await entity.async_get_events(hass, dt_util.now(), dt_util.now())
+    assert events[0].summary == "Jour de l'an"
