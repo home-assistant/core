0d67c1c87f087a82abef5908142d66bea37e90bf
Paolo Burgio <paolo.burgio@gmail.com>
2023-11-23 14:31:15 +0000

    Added test_init

--- tests/components/iotty/conftest.py
+++ tests/components/iotty/conftest.py
@@ -4,10 +4,11 @@ from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 
+from homeassistant import setup
 from homeassistant.components.iotty import DOMAIN
 from homeassistant.const import CONF_HOST, CONF_MAC, CONF_PORT
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.config_entry_oauth2_flow import LocalOAuth2Implementation
+from homeassistant.helpers import config_entry_oauth2_flow
 
 from tests.common import MockConfigEntry
 
@@ -17,20 +18,27 @@ REDIRECT_URI = "https://example.com/auth/external/callback"
 
 
 @pytest.fixture
+async def local_impl(hass: HomeAssistant):
+    """Local implementation."""
+    assert await setup.async_setup_component(hass, "auth", {})
+    return config_entry_oauth2_flow.LocalOAuth2Implementation(
+        hass, DOMAIN, "client_id", "client_secret", "authorize_url", "token_url"
+    )
+
+
+@pytest.fixture
 def mock_config_entry(hass: HomeAssistant) -> MockConfigEntry:
     """Return the default mocked config entry."""
     return MockConfigEntry(
-        title="CN11A1A00001",
+        title="IOTTY00001",
         domain=DOMAIN,
         data={
-            "auth_implementation": LocalOAuth2Implementation(
-                hass, DOMAIN, "client_id", "client_secret", "authorize_url", "token_url"
-            ),
+            "auth_implementation": DOMAIN,
             CONF_HOST: "127.0.0.1",
             CONF_MAC: "AA:BB:CC:DD:EE:FF",
             CONF_PORT: 9123,
         },
-        unique_id="CN11A1A00001",
+        unique_id="IOTTY00001",
     )
 
 
