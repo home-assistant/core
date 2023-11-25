50ba4dd7ed0fb49739201bdc4566bf8c721c67c8
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-11-25 18:49:41 +0000

    Update related test-asserts

--- tests/components/plugwise/test_climate.py
+++ tests/components/plugwise/test_climate.py
@@ -303,10 +303,11 @@ async def test_anna_climate_entity_attributes(
 
     assert state.attributes["current_temperature"] == 19.3
     assert state.attributes["preset_mode"] == "home"
-    assert state.attributes["supported_features"] == 17
-    assert state.attributes["temperature"] == 20.5
-    assert state.attributes["min_temp"] == 4.0
-    assert state.attributes["max_temp"] == 30.0
+    assert state.attributes["supported_features"] == 18
+    assert state.attributes["target_temp_high"] == 30
+    assert state.attributes["target_temp_low"] == 20.5
+    assert state.attributes["min_temp"] == 4
+    assert state.attributes["max_temp"] == 30
     assert state.attributes["target_temp_step"] == 0.1
 
 
@@ -325,7 +326,7 @@ async def test_anna_2_climate_entity_attributes(
         HVACMode.HEAT_COOL,
     ]
     assert state.attributes["supported_features"] == 18
-    assert state.attributes["target_temp_high"] == 24.0
+    assert state.attributes["target_temp_high"] == 30
     assert state.attributes["target_temp_low"] == 20.5
 
 
@@ -354,13 +355,13 @@ async def test_anna_climate_entity_climate_changes(
     await hass.services.async_call(
         "climate",
         "set_temperature",
-        {"entity_id": "climate.anna", "target_temp_high": 25, "target_temp_low": 20},
+        {"entity_id": "climate.anna", "target_temp_high": 30, "target_temp_low": 20},
         blocking=True,
     )
     assert mock_smile_anna.set_temperature.call_count == 1
     mock_smile_anna.set_temperature.assert_called_with(
         "c784ee9fdab44e1395b8dee7d7a497d5",
-        {"setpoint_high": 25.0, "setpoint_low": 20.0},
+        {"setpoint_high": 30.0, "setpoint_low": 20.0},
     )
 
     await hass.services.async_call(
@@ -400,4 +401,4 @@ async def test_anna_climate_entity_climate_changes(
         await hass.async_block_till_done()
         state = hass.states.get("climate.anna")
         assert state.state == HVACMode.HEAT
-        assert state.attributes["hvac_modes"] == [HVACMode.HEAT]
+        assert state.attributes["hvac_modes"] == [HVACMode.HEAT_COOL]
