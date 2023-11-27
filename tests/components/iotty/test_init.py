a137732b6632457846cf9fc3b28bdcccf6f56d52
Paolo Burgio <paolo.burgio@gmail.com>
2023-11-27 15:23:10 +0000

    Added TEST_CONFIG_FLOW

--- tests/components/iotty/test_init.py
+++ tests/components/iotty/test_init.py
@@ -1,10 +1,11 @@
-"""Tests for the Elgato Key Light integration."""
+"""Tests for the iotty integration."""
 from unittest.mock import MagicMock
 
 from homeassistant.components.iotty.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_entry_oauth2_flow
+from homeassistant.setup import async_setup_component
 
 from tests.common import MockConfigEntry
 
@@ -26,6 +27,8 @@ async def test_load_unload_config_entry(
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.LOADED
+    ## Calls both ctor and 'init' method
+    assert len(mock_iotty.mock_calls) == 2
 
     await hass.config_entries.async_unload(mock_config_entry.entry_id)
     await hass.async_block_till_done()
@@ -33,5 +36,14 @@ async def test_load_unload_config_entry(
     assert not hass.data.get(DOMAIN)
     assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
 
-    ## Calls both ctor and 'init' method
-    assert len(mock_iotty.mock_calls) == 2
+
+async def test_setup_component(hass: HomeAssistant) -> None:
+    """Testing init CloudApi proxy."""
+
+    entry = MockConfigEntry(domain="iotty", entry_id="00:00:00:00:01")
+    entry.add_to_hass(hass)
+
+    success = await async_setup_component(hass, "iotty", {})
+    await hass.async_block_till_done()
+
+    assert success is True
