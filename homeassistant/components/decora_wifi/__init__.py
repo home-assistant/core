c114a84800d4c91a63b99db65d546e590f300201
Gui Talarico <gtalarico@users.noreply.github.com>
2023-11-27 02:23:40 -0800

    rm create_task
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/decora_wifi/__init__.py
+++ homeassistant/components/decora_wifi/__init__.py
@@ -61,9 +61,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = session
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     def logout(event: Event) -> None:
         try:
