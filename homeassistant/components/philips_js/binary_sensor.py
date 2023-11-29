ffa9ab0d68580ab95f762b2de7a2d7aa63c28ec5
Joakim Plate <elupus@ecce.se>
2023-11-29 21:28:38 +0100

    Simplify check

--- homeassistant/components/philips_js/binary_sensor.py
+++ homeassistant/components/philips_js/binary_sensor.py
@@ -64,13 +64,10 @@ async def async_setup_entry(
         )
 
 
-def _recordings_list_available(api: PhilipsTV) -> bool:
-    """Return False if recordings_list is None Type."""
-    return not isinstance(api.recordings_list, type(None))
-
-
 def _check_for_recording_entry(api: PhilipsTV, entry: str, value: str) -> bool:
     """Return True if at least one specified value is available within entry of list."""
+    if api.recordings_list is None:
+        return False
     for rec in api.recordings_list["recordings"]:
         if rec.get(entry) == value:
             return True
@@ -91,22 +88,20 @@ class PhilipsTVBinarySensorEntityRecordingType(PhilipsJsEntity, BinarySensorEnti
         self.entity_description = description
         self._attr_unique_id = f"{coordinator.unique_id}_{description.key}"
         self._attr_device_info = coordinator.device_info
-        if _recordings_list_available(coordinator.api):
-            self._attr_is_on = _check_for_recording_entry(
-                coordinator.api,
-                "RecordingType",
-                description.recording_value,
-            )
+        self._attr_is_on = _check_for_recording_entry(
+            coordinator.api,
+            "RecordingType",
+            description.recording_value,
+        )
 
         super().__init__(coordinator)
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator and set is_on true if one specified value is available within given entry of list."""
-        if _recordings_list_available(self.coordinator.api):
-            self._attr_is_on = _check_for_recording_entry(
-                self.coordinator.api,
-                "RecordingType",
-                self.entity_description.recording_value,
-            )
+        self._attr_is_on = _check_for_recording_entry(
+            self.coordinator.api,
+            "RecordingType",
+            self.entity_description.recording_value,
+        )
         super()._handle_coordinator_update()
