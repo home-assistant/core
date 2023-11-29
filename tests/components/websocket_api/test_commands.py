afb7e0efc130803a047fca223dbd74c87de6ddbd
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-11-29 13:07:52 +0100

    Assign specific error code for HomeAssistantError on websocket_api connection exceptions (#104700)
    Assign specific error code for HomeAssistantError

--- tests/components/websocket_api/test_commands.py
+++ tests/components/websocket_api/test_commands.py
@@ -2327,7 +2327,7 @@ async def test_execute_script(
                 translation_key="test_error",
                 translation_placeholders={"option": "bla"},
             ),
-            "unknown_error",
+            "home_assistant_error",
         ),
         (
             ServiceValidationError(
