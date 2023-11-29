3090910159b88723fd2ce38e7d16dce112ffa035
grace43 <gracewanjiku71@gmail.com>
2023-11-29 15:56:38 +0000

    mock api using client

--- tests/components/openAQ/conftest.py
+++ tests/components/openAQ/conftest.py
@@ -1,1 +1,14 @@
 """Define test fixtures for openAQ."""
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+
+@pytest.fixture
+def mock_aq_client():
+    """Return a mocked AQClient."""
+    with patch("homeassistant.components.openAQ.aq_client.AQClient") as mock_client:
+        mock_instance = MagicMock()
+        mock_instance.get_device.return_value = MagicMock(sensors=[], locality="")
+        mock_client.return_value = mock_instance
+        yield mock_client
