d0d4274e650356c5019a2b570910ab7097a2db5a
Jonas Fors Lellky <lellky@gmail.com>
2023-11-29 15:49:26 +0100

    Refactors function to be inline code instead

--- homeassistant/components/flexit_bacnet/config_flow.py
+++ homeassistant/components/flexit_bacnet/config_flow.py
@@ -28,22 +28,6 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 
-async def validate_input(data: dict[str, Any]) -> str:
-    """Validate the user input allows us to connect.
-
-    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
-    """
-    device = FlexitBACnet(data[CONF_IP_ADDRESS], data[CONF_DEVICE_ID])
-
-    try:
-        await device.update()
-    except (asyncio.exceptions.TimeoutError, ConnectionError, DecodingError) as exc:
-        raise CannotConnect from exc
-
-    # Return info that you want to store in the config entry.
-    return device.serial_number
-
-
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Flexit Nordic (BACnet)."""
 
@@ -56,18 +40,23 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
 
         if user_input is not None:
+            device = FlexitBACnet(
+                user_input[CONF_IP_ADDRESS], user_input[CONF_DEVICE_ID]
+            )
             try:
-                info = await validate_input(user_input)
-            except CannotConnect:
+                await device.update()
+            except (asyncio.exceptions.TimeoutError, ConnectionError, DecodingError):
                 errors["base"] = "cannot_connect"
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
             else:
-                await self.async_set_unique_id(info)
+                await self.async_set_unique_id(device.serial_number)
                 self._abort_if_unique_id_configured()
 
-                return self.async_create_entry(title=info, data=user_input)
+                return self.async_create_entry(
+                    title=device.device_name, data=user_input
+                )
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
