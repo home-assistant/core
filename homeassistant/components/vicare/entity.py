270621f1358971101eb403842a226f39270856ce
Christopher Fenner <Christopher.Fenner@me.com>
2023-11-29 12:11:48 +0100

    create device only for heating circuits

--- homeassistant/components/vicare/entity.py
+++ homeassistant/components/vicare/entity.py
@@ -1,19 +1,15 @@
 """Entities for the ViCare integration."""
 from PyViCare.PyViCareDevice import Device as PyViCareDevice
 from PyViCare.PyViCareDeviceConfig import PyViCareDeviceConfig
-from PyViCare.PyViCareFuelCell import FuelCellBurner as PyViCareFuelCellBurner
-from PyViCare.PyViCareGazBoiler import GazBurner as PyViCareGazBurner
 from PyViCare.PyViCareHeatingDevice import (
     HeatingCircuit as PyViCareHeatingCircuit,
     HeatingDeviceWithComponent as PyViCareHeatingDevicWithComponent,
 )
-from PyViCare.PyViCareHeatPump import Compressor as PyViCareCompressor
-from PyViCare.PyViCareOilBoiler import OilBurner as PyViCareOilBurner
 
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 
-from .const import DOMAIN
+from .const import DEVELOPER_PORTAL, DOMAIN, MANUFACTURER
 
 
 class ViCareEntity(Entity):
@@ -36,20 +32,20 @@ class ViCareEntity(Entity):
         if hasattr(device, "id"):
             self._attr_unique_id += f"-{device.id}"
 
-        if isinstance(device, PyViCareHeatingDevicWithComponent):
-            self._attr_device_info = self._get_info_for_component(
+        if isinstance(device, PyViCareHeatingCircuit):
+            self._attr_device_info = self._get_info_for_heating_circuit(
                 device_config, device, custom_device_name
             )
         else:
             self._attr_device_info = self._get_info_for_device(device_config)
 
-    def _get_info_for_component(
+    def _get_info_for_heating_circuit(
         self,
         device_config: PyViCareDeviceConfig,
         device: PyViCareDevice,
         custom_device_name: str | None,
     ) -> DeviceInfo:
-        component_type = self._get_device_type(device)
+        component_type = "Heating Circuit"
         component_name = (
             component_type if custom_device_name is None else custom_device_name
         )
@@ -63,8 +59,8 @@ class ViCareEntity(Entity):
             },
             name=component_name,
             model=component_type,
-            manufacturer="Viessmann",
-            configuration_url="https://developer.viessmann.com/",
+            manufacturer=MANUFACTURER,
+            configuration_url=DEVELOPER_PORTAL,
         )
 
     def _get_info_for_device(self, device_config: PyViCareDeviceConfig) -> DeviceInfo:
@@ -72,21 +68,7 @@ class ViCareEntity(Entity):
             identifiers={(DOMAIN, device_config.getConfig().serial)},
             serial_number=device_config.getConfig().serial,
             name=device_config.getModel(),
-            manufacturer="Viessmann",
             model=device_config.getModel(),
-            configuration_url="https://developer.viessmann.com/",
+            manufacturer=MANUFACTURER,
+            configuration_url=DEVELOPER_PORTAL,
         )
-
-    def _get_device_type(
-        self,
-        device: PyViCareDevice | PyViCareHeatingDevicWithComponent,
-    ) -> str:
-        if isinstance(device, PyViCareHeatingCircuit):
-            return "Heating Circuit"
-        if isinstance(
-            device, PyViCareFuelCellBurner | PyViCareGazBurner | PyViCareOilBurner
-        ):
-            return "Burner"
-        if isinstance(device, PyViCareCompressor):
-            return "Compressor"
-        return ""
