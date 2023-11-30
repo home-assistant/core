7aa5e47602da65be380b7725cbba738abee48d01
mib1185 <mail@mib85.de>
2023-11-30 22:04:32 +0000

    remove check for insignificant attributes

--- homeassistant/components/weather/significant_change.py
+++ homeassistant/components/weather/significant_change.py
@@ -13,27 +13,17 @@ from .const import (
     ATTR_WEATHER_DEW_POINT,
     ATTR_WEATHER_HUMIDITY,
     ATTR_WEATHER_OZONE,
-    ATTR_WEATHER_PRECIPITATION_UNIT,
     ATTR_WEATHER_PRESSURE,
     ATTR_WEATHER_PRESSURE_UNIT,
     ATTR_WEATHER_TEMPERATURE,
     ATTR_WEATHER_TEMPERATURE_UNIT,
     ATTR_WEATHER_UV_INDEX,
     ATTR_WEATHER_VISIBILITY,
-    ATTR_WEATHER_VISIBILITY_UNIT,
     ATTR_WEATHER_WIND_BEARING,
     ATTR_WEATHER_WIND_GUST_SPEED,
     ATTR_WEATHER_WIND_SPEED,
     ATTR_WEATHER_WIND_SPEED_UNIT,
 )
-
-INSIGNIFICANT_ATTRIBUTES: set[str] = {
-    ATTR_WEATHER_PRECIPITATION_UNIT,
-    ATTR_WEATHER_PRESSURE_UNIT,
-    ATTR_WEATHER_TEMPERATURE_UNIT,
-    ATTR_WEATHER_VISIBILITY_UNIT,
-    ATTR_WEATHER_WIND_SPEED_UNIT,
-}
 
 SIGNIFICANT_ATTRIBUTES: set[str] = {
     ATTR_WEATHER_APPARENT_TEMPERATURE,
@@ -108,12 +98,7 @@ def async_check_significant_change(
     new_attrs_s = set(new_attrs.items())
     changed_attrs: set[str] = {item[0] for item in old_attrs_s ^ new_attrs_s}
 
-    # ignore if only insignificant attributes change
-    if all(a in INSIGNIFICANT_ATTRIBUTES for a in changed_attrs):
-        return False
-
     for attr_name in changed_attrs:
-        # ignore any unofficial attributes
         if attr_name not in SIGNIFICANT_ATTRIBUTES:
             continue
 
