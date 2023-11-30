a48826ef7181ebf11f151db7a99f0dd57df09f6b
Jonas Fors Lellky <lellky@gmail.com>
2023-11-30 15:34:20 +0100

    Use parametrize for similar tests i.e. test_flow_fails

--- tests/components/flexit_bacnet/test_config_flow.py
+++ tests/components/flexit_bacnet/test_config_flow.py
@@ -2,6 +2,7 @@
 import asyncio.exceptions
 
 from flexit_bacnet import DecodingError
+import pytest
 
 from homeassistant.components.flexit_bacnet.const import DOMAIN
 from homeassistant.const import CONF_DEVICE_ID, CONF_IP_ADDRESS
@@ -34,14 +35,26 @@ async def test_form(hass: HomeAssistant, flow_id: str, mock_setup_entry) -> None
     assert len(mock_setup_entry.mock_calls) == 1
 
 
-async def test_form_invalid_device(
-    hass: HomeAssistant, flow_id: str, mock_setup_entry
+@pytest.mark.parametrize(
+    ("error", "message"),
+    [
+        (
+            asyncio.exceptions.TimeoutError,
+            "cannot_connect",
+        ),
+        (ConnectionError, "cannot_connect"),
+        (DecodingError, "cannot_connect"),
+        (Exception(), "unknown"),
+    ],
+)
+async def test_flow_fails(
+    hass: HomeAssistant, flow_id: str, error: Exception, message: str, mock_setup_entry
 ) -> None:
     """Test that we return 'cannot_connect' error when attempting to connect to an incorrect IP address.
 
     The flexit_bacnet library raises asyncio.exceptions.TimeoutError in that scenario.
     """
-    with _patch_update(side_effect=asyncio.exceptions.TimeoutError):
+    with _patch_update(side_effect=error):
         result = await hass.config_entries.flow.async_configure(
             flow_id,
             {
@@ -51,7 +64,7 @@ async def test_form_invalid_device(
         )
 
     assert result["type"] == FlowResultType.FORM
-    assert result["errors"] == {"base": "cannot_connect"}
+    assert result["errors"] == {"base": message}
     assert len(mock_setup_entry.mock_calls) == 0
 
     # ensure that user can recover from this error
@@ -96,96 +109,3 @@ async def test_form_device_already_exist(hass: HomeAssistant, flow_id: str) -> N
         await hass.async_block_till_done()
     assert result["type"] == FlowResultType.ABORT
     assert result["reason"] == "already_configured"
-
-
-async def test_form_connection_error(
-    hass: HomeAssistant, flow_id: str, mock_setup_entry
-) -> None:
-    """Test that we return 'cannot_connect' error when ConnectionError happens.
-
-    The flexit_bacnet library raises ConnectionError in case of connectivity issues.
-    """
-    with _patch_update(side_effect=ConnectionError):
-        result = await hass.config_entries.flow.async_configure(
-            flow_id,
-            {
-                CONF_IP_ADDRESS: "1.1.1.1",
-                CONF_DEVICE_ID: 2,
-            },
-        )
-
-    assert result["type"] == FlowResultType.FORM
-    assert result["errors"] == {"base": "cannot_connect"}
-
-    # ensure that user can recover from this error
-    with _patch_update():
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                CONF_IP_ADDRESS: "1.1.1.1",
-                CONF_DEVICE_ID: 2,
-            },
-        )
-
-    assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Device Name"
-    assert result2["context"]["unique_id"] == "0000-0001"
-    assert result2["data"] == {
-        CONF_IP_ADDRESS: "1.1.1.1",
-        CONF_DEVICE_ID: 2,
-    }
-    assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_decoding_error(
-    hass: HomeAssistant, flow_id: str, mock_setup_entry
-) -> None:
-    """Test that we return 'cannot_connect' error when DecodingError happens.
-
-    The flexit_bacnet library raises DecodingError when it receives invalid response from a BACnet peer.
-    """
-    with _patch_update(side_effect=DecodingError):
-        result = await hass.config_entries.flow.async_configure(
-            flow_id,
-            {
-                CONF_IP_ADDRESS: "1.1.1.1",
-                CONF_DEVICE_ID: 2,
-            },
-        )
-
-    assert result["type"] == FlowResultType.FORM
-    assert result["errors"] == {"base": "cannot_connect"}
-
-    # ensure that user can recover from this error
-    with _patch_update():
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                CONF_IP_ADDRESS: "1.1.1.1",
-                CONF_DEVICE_ID: 2,
-            },
-        )
-
-    assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Device Name"
-    assert result2["context"]["unique_id"] == "0000-0001"
-    assert result2["data"] == {
-        CONF_IP_ADDRESS: "1.1.1.1",
-        CONF_DEVICE_ID: 2,
-    }
-    assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_unexpected_error(hass: HomeAssistant, flow_id: str) -> None:
-    """Test we handle unexpected errors."""
-    with _patch_update(side_effect=Exception):
-        result2 = await hass.config_entries.flow.async_configure(
-            flow_id,
-            {
-                CONF_IP_ADDRESS: "1.1.1.1",
-                CONF_DEVICE_ID: 2,
-            },
-        )
-
-    assert result2["type"] == FlowResultType.FORM
-    assert result2["errors"] == {"base": "unknown"}
