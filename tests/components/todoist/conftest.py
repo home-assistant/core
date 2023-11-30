db6b80429856bb066193a808457852380c49df18
Allen Porter <allen@thebends.org>
2023-11-29 22:01:57 -0800

    Add due date and description fields to Todoist To-do entity (#104655)
    * Add Todoist Due date and description fields
    
    * Update entity features with new names
    
    * Make items into walrus
    
    * Update due_datetime field
    
    * Add additional tests for adding new fields to items
    
    * Fix call args in todoist test

--- tests/components/todoist/conftest.py
+++ tests/components/todoist/conftest.py
@@ -45,6 +45,7 @@ def make_api_task(
     is_completed: bool = False,
     due: Due | None = None,
     project_id: str | None = None,
+    description: str | None = None,
 ) -> Task:
     """Mock a todoist Task instance."""
     return Task(
@@ -55,8 +56,8 @@ def make_api_task(
         content=content or SUMMARY,
         created_at="2021-10-01T00:00:00",
         creator_id="1",
-        description="A task",
-        due=due or Due(is_recurring=False, date=TODAY, string="today"),
+        description=description,
+        due=due,
         id=id or "1",
         labels=["Label1"],
         order=1,
