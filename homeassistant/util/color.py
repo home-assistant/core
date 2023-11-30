6253582460ee79492e9918175c4a4c9d261c952d
Niccolò Maggioni <nicco.maggioni@gmail.com>
2023-11-30 18:34:08 +0100

    Add citation for McCamy's approximation formula
    If still existing, also see page 3 of https://www.st.com/resource/en/application_note/an5638-how-correlated-color-temperature-is-calculated-by-vd6283-stmicroelectronics.pdf

--- homeassistant/util/color.py
+++ homeassistant/util/color.py
@@ -579,7 +579,7 @@ def _white_levels_to_color_temperature(
 def color_xy_to_temperature(x: float, y: float) -> int:
     """Convert an xy color to a color temperature in Kelvin.
 
-    Uses McCamy's approximation, close enough for uses between 2000 K and 10000 K.
+    Uses McCamy's approximation (https://doi.org/10.1002/col.5080170211), close enough for uses between 2000 K and 10000 K.
     """
     n = (x - 0.3320) / (0.1858 - y)
     CCT = 437 * (n**3) + 3601 * (n**2) + 6861 * n + 5517
