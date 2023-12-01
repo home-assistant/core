454640d5d50cab07d39ce4e807686c0d6d2e4f9e
Michael Hansen <mike@rhasspy.org>
2023-12-01 15:22:55 -0600

    More tests

--- tests/components/wyoming/test_select.py
+++ tests/components/wyoming/test_select.py
@@ -1,8 +1,13 @@
 """Test Wyoming select."""
+from unittest.mock import Mock, patch
+
+from homeassistant.components import assist_pipeline
+from homeassistant.components.assist_pipeline.pipeline import PipelineData
 from homeassistant.components.assist_pipeline.select import OPTION_PREFERRED
 from homeassistant.components.wyoming.devices import SatelliteDevice
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
 
 
 async def test_pipeline_select(
@@ -15,9 +20,66 @@ async def test_pipeline_select(
     Functionality is tested in assist_pipeline/test_select.py.
     This test is only to ensure it is set up.
     """
+    assert await async_setup_component(hass, assist_pipeline.DOMAIN, {})
+    pipeline_data: PipelineData = hass.data[assist_pipeline.DOMAIN]
+
+    # Create second pipeline
+    await pipeline_data.pipeline_store.async_create_item(
+        {
+            "name": "Test 1",
+            "language": "en-US",
+            "conversation_engine": None,
+            "conversation_language": "en-US",
+            "tts_engine": None,
+            "tts_language": None,
+            "tts_voice": None,
+            "stt_engine": None,
+            "stt_language": None,
+            "wake_word_entity": None,
+            "wake_word_id": None,
+        }
+    )
+
+    # Preferred pipeline is the default
     pipeline_entity_id = satellite_device.get_pipeline_entity_id(hass)
     assert pipeline_entity_id
 
     state = hass.states.get(pipeline_entity_id)
     assert state is not None
     assert state.state == OPTION_PREFERRED
+
+    # Change to second pipeline
+    with patch.object(
+        satellite_device, "async_pipeline_changed"
+    ) as mock_pipeline_changed:
+        await hass.services.async_call(
+            "select",
+            "select_option",
+            {"entity_id": pipeline_entity_id, "option": "Test 1"},
+            blocking=True,
+        )
+
+        state = hass.states.get(pipeline_entity_id)
+        assert state is not None
+        assert state.state == "Test 1"
+
+        # async_pipeline_changed should have been called
+        mock_pipeline_changed.assert_called()
+
+    # Change back and check update listener
+    update_listener = Mock()
+    satellite_device.async_listen_update(update_listener)
+
+    await hass.services.async_call(
+        "select",
+        "select_option",
+        {"entity_id": pipeline_entity_id, "option": OPTION_PREFERRED},
+        blocking=True,
+    )
+
+    state = hass.states.get(pipeline_entity_id)
+    assert state is not None
+    assert state.state == OPTION_PREFERRED
+
+    # update listener should have been called
+    update_listener.assert_called()
