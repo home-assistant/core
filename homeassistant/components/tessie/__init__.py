708d9f40bf34edbf21ed76aef197788a0f8006cb
Brett Adams <brett.whynot@gmail.com>
2023-12-01 10:49:01 +0000

    Move session from init to coordinator

--- homeassistant/components/tessie/__init__.py
+++ homeassistant/components/tessie/__init__.py
@@ -2,7 +2,6 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ACCESS_TOKEN, Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
 from .coordinator import TessieDataUpdateCoordinator
@@ -12,19 +11,13 @@ PLATFORMS = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Tessie config."""
-    api_key = entry.data[CONF_ACCESS_TOKEN]
-    session = async_get_clientsession(hass)
-
     coordinator = TessieDataUpdateCoordinator(
         hass,
-        api_key=api_key,
-        session=session,
+        api_key=entry.data[CONF_ACCESS_TOKEN],
     )
 
     await coordinator.async_config_entry_first_refresh()
-
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
