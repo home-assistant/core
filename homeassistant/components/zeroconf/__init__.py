81355607bf017b45c20be5a15ff3dbbdab29cc5d
J. Nick Koston <nick@koston.org>
2023-12-02 11:19:16 -1000

    Fix missing TYPE_CHECKING guard in zeroconf hot path
    I missed this one when I previously refactored

--- homeassistant/components/zeroconf/__init__.py
+++ homeassistant/components/zeroconf/__init__.py
@@ -589,7 +589,10 @@ def info_from_service(service: AsyncServiceInfo) -> ZeroconfServiceInfo | None:
     # Service properties are always bytes if they are set from the network.
     # For legacy backwards compatibility zeroconf allows properties to be set
     # as strings but we never do that so we can safely cast here.
-    service_properties = cast(dict[bytes, bytes | None], service.properties)
+    if TYPE_CHECKING:
+        service_properties = cast(dict[bytes, bytes | None], service.properties)
+    else:
+        service_properties = service.properties
 
     properties: dict[str, Any] = {
         k.decode("ascii", "replace"): None
