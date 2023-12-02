c847ccc67d0229ff8be590459f7316ad83bf2892
Erwin Douna <e.douna@gmail.com>
2023-12-02 23:07:06 +0100

    Add MELCloud token refresh upon firmware upgrade (#104391)
    * Adding initial setup
    
    * Update homeassistant/components/melcloud/__init__.py
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * Adding ConfigEntryNotReady exception
    
    * Update homeassistant/components/melcloud/__init__.py
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * Update homeassistant/components/melcloud/config_flow.py
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * Update homeassistant/components/melcloud/__init__.py
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * Placing exception handling in setup_entry
    
    * Expanding test cases
    
    ---------
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- homeassistant/components/melcloud/config_flow.py
+++ homeassistant/components/melcloud/config_flow.py
@@ -2,7 +2,10 @@
 from __future__ import annotations
 
 import asyncio
+from collections.abc import Mapping
 from http import HTTPStatus
+import logging
+from typing import Any
 
 from aiohttp import ClientError, ClientResponseError
 import pymelcloud
@@ -11,11 +14,13 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_TOKEN, CONF_USERNAME
 from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
-from homeassistant.data_entry_flow import AbortFlow, FlowResultType
+from homeassistant.data_entry_flow import AbortFlow, FlowResult, FlowResultType
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_create_import_issue(
@@ -55,6 +60,8 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow."""
 
     VERSION = 1
+
+    entry: config_entries.ConfigEntry | None = None
 
     async def _create_entry(self, username: str, token: str):
         """Register new entry."""
@@ -126,3 +133,67 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         if result["type"] == FlowResultType.CREATE_ENTRY:
             await async_create_import_issue(self.hass, self.context["source"], "", True)
         return result
+
+    async def async_step_reauth(self, entry_data: Mapping[str, Any]) -> FlowResult:
+        """Handle initiation of re-authentication with MELCloud."""
+        self.entry = self.hass.config_entries.async_get_entry(self.context["entry_id"])
+        return await self.async_step_reauth_confirm()
+
+    async def async_step_reauth_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle re-authentication with MELCloud."""
+        errors: dict[str, str] = {}
+
+        if user_input is not None and self.entry:
+            aquired_token, errors = await self.async_reauthenticate_client(user_input)
+
+            if not errors:
+                self.hass.config_entries.async_update_entry(
+                    self.entry,
+                    data={CONF_TOKEN: aquired_token},
+                )
+                self.hass.async_create_task(
+                    self.hass.config_entries.async_reload(self.entry.entry_id)
+                )
+                return self.async_abort(reason="reauth_successful")
+
+        return self.async_show_form(
+            step_id="reauth_confirm",
+            data_schema=vol.Schema(
+                {vol.Required(CONF_USERNAME): str, vol.Required(CONF_PASSWORD): str}
+            ),
+            errors=errors,
+        )
+
+    async def async_reauthenticate_client(
+        self, user_input: dict[str, Any]
+    ) -> tuple[str | None, dict[str, str]]:
+        """Reauthenticate with MELCloud."""
+        errors: dict[str, str] = {}
+        acquired_token = None
+
+        try:
+            async with asyncio.timeout(10):
+                acquired_token = await pymelcloud.login(
+                    user_input[CONF_USERNAME],
+                    user_input[CONF_PASSWORD],
+                    async_get_clientsession(self.hass),
+                )
+        except (ClientResponseError, AttributeError) as err:
+            if isinstance(err, ClientResponseError) and err.status in (
+                HTTPStatus.UNAUTHORIZED,
+                HTTPStatus.FORBIDDEN,
+            ):
+                errors["base"] = "invalid_auth"
+            elif isinstance(err, AttributeError) and err.name == "get":
+                errors["base"] = "invalid_auth"
+            else:
+                errors["base"] = "cannot_connect"
+        except (
+            asyncio.TimeoutError,
+            ClientError,
+        ):
+            errors["base"] = "cannot_connect"
+
+        return acquired_token, errors
