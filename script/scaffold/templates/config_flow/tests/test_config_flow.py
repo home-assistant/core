c4b3a54eef212276812db7bcb7a73cdb6892b5d3
joostlek <joostlek@outlook.com>
2023-12-03 22:51:16 +0100

    Fix bug in config flow scaffold

--- script/scaffold/templates/config_flow/tests/test_config_flow.py
+++ script/scaffold/templates/config_flow/tests/test_config_flow.py
@@ -18,7 +18,7 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
     assert result["type"] == FlowResultType.FORM
-    assert result["errors"] is None
+    assert result["errors"] == {}
 
     with patch(
         "homeassistant.components.NEW_DOMAIN.config_flow.PlaceholderHub.authenticate",
