5e92ba04addca72344ad405beca1819fe63ede44
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-12-04 15:00:11 +0100

    trigger (fake) event callback on all subscribers

--- tests/components/matter/common.py
+++ tests/components/matter/common.py
@@ -71,6 +71,12 @@ async def trigger_subscription_callback(
     data: Any = None,
 ) -> None:
     """Trigger a subscription callback."""
-    callback = client.subscribe_events.call_args.kwargs["callback"]
-    callback(event, data)
+    # trigger callback on all subscribers
+    for sub in client.subscribe_events.call_args_list:
+        callback = sub.args[0] if len(sub.args) > 0 else sub.kwargs.get("callback")
+        event_filter = (
+            sub.args[1] if len(sub.args) > 1 else sub.kwargs.get("event_filter")
+        )
+        if event_filter in (None, event):
+            callback(event, data)
     await hass.async_block_till_done()
