bb327f2f5a1e66f06aaae56920a7e3aabd4fd204
goos <goos@deako.com>
2023-12-04 14:45:54 -0800

    update init.py logger

--- homeassistant/components/deako/__init__.py
+++ homeassistant/components/deako/__init__.py
@@ -14,7 +14,7 @@ from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN
 
-_LOGGER: logging.Logger = logging.getLogger(__package__)
+_LOGGER: logging.Logger = logging.getLogger(__name__)
 
 PLATFORMS: list[Platform] = [Platform.LIGHT]
 
