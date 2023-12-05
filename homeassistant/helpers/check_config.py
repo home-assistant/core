3da019891e439c6a2ca9877d79bd73fdac4e4568
Erik Montnemery <erik@montnemery.com>
2023-12-05 15:06:13 +0100

    Use modern platform path when reporting platform config errors (#104238)
    * Use modern platform path when reporting platform config errors
    
    * Update tests
    
    * Address review comment
    
    * Explicitly pass platform domain to log helpers
    
    * Revert overly complicated changes
    
    * Try a simpler solution

--- homeassistant/helpers/check_config.py
+++ homeassistant/helpers/check_config.py
@@ -276,13 +276,17 @@ async def async_check_ha_config_file(  # noqa: C901
                 # show errors for a missing integration in recovery mode or safe mode to
                 # not confuse the user.
                 if not hass.config.recovery_mode and not hass.config.safe_mode:
-                    result.add_warning(f"Platform error {domain}.{p_name} - {ex}")
+                    result.add_warning(
+                        f"Platform error '{domain}' from integration '{p_name}' - {ex}"
+                    )
                 continue
             except (
                 RequirementsNotFound,
                 ImportError,
             ) as ex:
-                result.add_warning(f"Platform error {domain}.{p_name} - {ex}")
+                result.add_warning(
+                    f"Platform error '{domain}' from integration '{p_name}' - {ex}"
+                )
                 continue
 
             # Validate platform specific schema
