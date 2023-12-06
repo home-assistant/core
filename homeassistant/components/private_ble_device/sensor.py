2edcd1f77f89e3227d45ca7d0bc4e546a3ce5348
J. Nick Koston <nick@koston.org>
2023-12-05 22:24:11 -1000

    Revert "value can be a float now"
    This reverts commit b7e7127143bd2947345c7590fc2727aa47e28d88.

--- homeassistant/components/private_ble_device/sensor.py
+++ homeassistant/components/private_ble_device/sensor.py
@@ -84,7 +84,7 @@ SENSOR_DESCRIPTIONS = (
         entity_registry_enabled_default=False,
         entity_category=EntityCategory.DIAGNOSTIC,
         value_fn=(
-            lambda hass, service_info: round(
+            lambda hass, service_info: (
                 bluetooth.async_get_learned_advertising_interval(
                     hass, service_info.address
                 )
