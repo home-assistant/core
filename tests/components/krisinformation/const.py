109c830f608fddeb554cb4e19cd30cc5568cd10e
albin landgren <albinlandgreen@gmail.com>
2023-12-07 09:31:41 +0000

    Mock config

--- tests/components/krisinformation/const.py
+++ tests/components/krisinformation/const.py
@@ -1,1 +1,8 @@
-"""Tests for const."""
+"""Constants for krisinformation tests."""
+from homeassistant.components.krisinformation.const import CONF_COUNTY, COUNTY_CODES
+from homeassistant.const import CONF_NAME
+
+MOCK_CONFIG = {
+    CONF_NAME: "Krisinformation test",
+    CONF_COUNTY: COUNTY_CODES["17"],
+}
