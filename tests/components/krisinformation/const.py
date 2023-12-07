bb32e2d1b422e16cef6f455eb51a9e265b0b4d29
albin landgren <albinlandgreen@gmail.com>
2023-12-07 09:32:36 +0000

    Added fake language to mock config

--- tests/components/krisinformation/const.py
+++ tests/components/krisinformation/const.py
@@ -1,8 +1,14 @@
 """Constants for krisinformation tests."""
-from homeassistant.components.krisinformation.const import CONF_COUNTY, COUNTY_CODES
+from homeassistant.components.krisinformation.const import (
+    CONF_COUNTY,
+    COUNTY_CODES,
+    LANGUAGES,
+)
 from homeassistant.const import CONF_NAME
 
 MOCK_CONFIG = {
     CONF_NAME: "Krisinformation test",
     CONF_COUNTY: COUNTY_CODES["17"],
 }
+
+FAKE_LANGUAGE = LANGUAGES[0]
