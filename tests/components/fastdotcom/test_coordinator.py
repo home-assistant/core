cceb3aa9ec04099261d586269f386a1207f3f28e
Erwin Douna <e.douna@gmail.com>
2023-12-07 19:45:58 +0100

    Update tests/components/fastdotcom/test_coordinator.py
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- tests/components/fastdotcom/test_coordinator.py
+++ tests/components/fastdotcom/test_coordinator.py
@@ -36,7 +36,6 @@ async def test_fastdotcom_data_update_coordinator(
     ):
         freezer.tick(timedelta(minutes=5, seconds=1))
         async_fire_time_changed(hass)
-        await coordinator.async_refresh()
 
     state = hass.states.get("sensor.fast_com_download")
     assert state.state == "10.0"
