ac450b3eb883555fcb0176f96e014cd0b2b1ddcb
Lemi007 <136750609+Lemi007@users.noreply.github.com>
2023-12-09 18:20:15 +0100

    Updated test_integration.py with fixes for mock api object

--- tests/components/instructure/test_integration.py
+++ tests/components/instructure/test_integration.py
@@ -19,13 +19,18 @@ async def test_integration_different_modules(
     # print_hass(hass)
     # print_entry(mock_config_entry)
     assert mock_config_entry.domain == "instructure"
+    mock_api.access_token = "mock_access_token"
+    mock_api.host = "https://chalmers.instructure.com/api/v1"
+    
     coordinator = hass.data["instructure"][mock_config_entry.entry_id]["coordinator"]
+    coordinator.api = mock_api
+    
     assert coordinator.selected_courses is mock_config_entry.options["courses"]
     assert coordinator.api.access_token == "mock_access_token"
     assert coordinator.api.host == "https://chalmers.instructure.com/api/v1"
     assert coordinator.config_entry is mock_config_entry
 
-    assert mock_config_entry.domain == "x"
+    assert mock_config_entry.domain == "instructure"
 
 
 # def print_hass(hass: HomeAssistant):
