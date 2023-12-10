85ba36a1f98e82d9fdf322193186b2fc9dec4ea3
Florian Baumann <derflob@derflob.de>
2023-12-10 17:01:52 +0100

    refactor: reorder import statement
    To make ruff happy.

--- tests/components/caldav/test_todo.py
+++ tests/components/caldav/test_todo.py
@@ -1,5 +1,5 @@
 """The tests for the webdav todo component."""
-from datetime import date, datetime, UTC
+from datetime import UTC, date, datetime
 from typing import Any
 from unittest.mock import MagicMock, Mock
 
