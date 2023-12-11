d80262c55f44cdfb38827235e19cc14527da574f
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-12-11 19:04:01 +0100

    Update tests/components/lamarzocco/test_config_flow.py
    Co-authored-by: Robert Resch <robert@resch.dev>

--- tests/components/lamarzocco/test_config_flow.py
+++ tests/components/lamarzocco/test_config_flow.py
@@ -41,6 +41,7 @@ async def test_form(hass: HomeAssistant, mock_lamarzocco: MagicMock) -> None:
     )
     assert result["type"] == FlowResultType.FORM
     assert result["errors"] == {}
+    assert result["step_id"] == "user"
 
     result2 = await hass.config_entries.flow.async_configure(
         result["flow_id"],
