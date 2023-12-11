9396e2e159818e330db931bc2409e8545d63b26f
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-11 08:48:55 +0100

    Remove Aftership import issue when entry already exists

--- tests/components/aftership/test_config_flow.py
+++ tests/components/aftership/test_config_flow.py
@@ -10,8 +10,6 @@ from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 from homeassistant.helpers import issue_registry as ir
 
-from tests.common import MockConfigEntry
-
 
 async def test_full_user_flow(hass: HomeAssistant, mock_setup_entry) -> None:
     """Test the full user configuration flow."""
@@ -97,14 +95,3 @@ async def test_import_flow(hass: HomeAssistant, mock_setup_entry) -> None:
         }
     issue_registry = ir.async_get(hass)
     assert len(issue_registry.issues) == 1
-
-
-async def test_import_flow_already_exists(hass: HomeAssistant) -> None:
-    """Test importing yaml config where entry already exists."""
-    entry = MockConfigEntry(domain=DOMAIN, data={CONF_API_KEY: "yaml-api-key"})
-    entry.add_to_hass(hass)
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_IMPORT}, data={CONF_API_KEY: "yaml-api-key"}
-    )
-    assert result["type"] == FlowResultType.ABORT
-    assert result["reason"] == "already_configured"
