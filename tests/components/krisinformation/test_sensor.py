f6be9e71061f9e10aba66523635807f9818aca5e
Julia Jönmark <jonmark@chalmers.se>
2023-12-12 10:35:59 +0000

    imports and mock data

--- tests/components/krisinformation/test_sensor.py
+++ tests/components/krisinformation/test_sensor.py
@@ -1,1 +1,11 @@
 """Tests for sensor."""
+from homeassistant.components.krisinformation.geo_location import (
+    KrisInformationGeolocationEvent,
+)
+from homeassistant.const import UnitOfLength
+
+
+def _generate_mock_feed_entry(headline: str, latitude: float, longitude: float):
+    return KrisInformationGeolocationEvent(
+        headline, latitude, longitude, UnitOfLength.KILOMETERS
+    )
