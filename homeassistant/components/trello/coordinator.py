c5dfbc96954ae1b075f90f000c0337e481cd89bf
Scott Giminiani <scottg489@gmail.com>
2023-12-11 22:12:54 -0500

    Use new trello client lib with native support for batch requests
    ha-py-trello has native support for batch requests and py-trello
    doesn't.
    
    Refactor coordinator to use the new client. This removes implementation
    details from HA as they are now in the lib.

--- homeassistant/components/trello/coordinator.py
+++ homeassistant/components/trello/coordinator.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 
 from datetime import timedelta
 
-from trello import TrelloClient
+from trello import BatchResponse, Board as TrelloBoard, List as TrelloList, TrelloClient
+from trello.batch.board import Board as BatchBoard
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -31,40 +32,41 @@ class TrelloDataUpdateCoordinator(DataUpdateCoordinator[dict[str, Board]]):
         self.board_ids = board_ids
 
     def _update(self) -> dict[str, Board]:
-        """Fetch data for all sensors using batch API endpoint."""
-        board_lists_query_params = "fields=name"
-        list_cards_query_params = "fields=name&cards=open&card_fields=idCard"
-        urls = ",".join(
-            f"/boards/{board_id}?{board_lists_query_params},/boards/{board_id}/lists?{list_cards_query_params}"
-            for board_id in self.board_ids
-        )
+        """Fetch data for all sensors as a batch."""
+        batch_requests = []
+        for board_id in self.board_ids:
+            batch_requests.append(BatchBoard.GetBoard(board_id, ["name"]))
+            batch_requests.append(
+                BatchBoard.GetLists(board_id, ["name"], "open", ["idCard"])
+            )
+        LOGGER.debug("Fetching boards lists")
+        batch_responses = self.client.fetch_batch(batch_requests)
 
-        LOGGER.debug("Fetching boards lists: %s", urls)
-        batch_response = self.client.fetch_json("batch", query_params={"urls": urls})
-
-        return _get_boards(batch_response, self.board_ids)
+        return _get_boards(batch_responses, self.board_ids)
 
     async def _async_update_data(self) -> dict[str, Board]:
         """Send request to the executor."""
         return await self.hass.async_add_executor_job(self._update)
 
 
-def _get_boards(batch_response: list[dict], board_ids: list[str]) -> dict[str, Board]:
+def _get_boards(
+    batch_response: list[BatchResponse], board_ids: list[str]
+) -> dict[str, Board]:
     board_id_boards: dict[str, Board] = {}
     for i, batch_response_pair in enumerate(
         zip(batch_response[::2], batch_response[1::2])
     ):
         board_response = batch_response_pair[0]
         list_response = batch_response_pair[1]
-        if _is_success(board_response) and _is_success(list_response):
-            board = board_response["200"]
-            lists = list_response["200"]
-            board_id_boards[board["id"]] = _get_board(board, lists)
+        if board_response.success and list_response.success:
+            board = board_response.payload
+            lists = list_response.payload
+            board_id_boards[board.id] = _get_board(board, lists)
         else:
             LOGGER.error(
                 "Unable to fetch lists for board with ID '%s'. Response was: %s)",
                 board_ids[i],
-                board_response,
+                board_response.payload.message,
             )
             board_id_boards[board_ids[i]] = Board(board_ids[i], "", {})
             continue
@@ -72,16 +74,9 @@ def _get_boards(batch_response: list[dict], board_ids: list[str]) -> dict[str, B
     return board_id_boards
 
 
-def _get_board(board: dict, lists: dict) -> Board:
+def _get_board(board: TrelloBoard, lists: list[TrelloList]) -> Board:
     return Board(
-        board["id"],
-        board["name"],
-        {
-            list_["id"]: List(list_["id"], list_["name"], len(list_["cards"]))
-            for list_ in lists
-        },
+        board.id,
+        board.name,
+        {list_.id: List(list_.id, list_.name, len(list_.cards)) for list_ in lists},
     )
-
-
-def _is_success(response: dict) -> bool:
-    return "200" in response
