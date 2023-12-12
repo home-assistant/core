0744f862d74ed74f3be1259c0c2730d46f019880
Brett Adams <brett.whynot@gmail.com>
2023-12-12 05:10:42 +0000

    Add climate platform

--- homeassistant/components/tessie/entity.py
+++ homeassistant/components/tessie/entity.py
@@ -1,8 +1,13 @@
 """Tessie parent entity class."""
 
-
+from collections.abc import Callable
+from http import HTTPStatus
 from typing import Any
 
+from aiohttp import ClientResponseError
+
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers import issue_registry as ir
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
@@ -25,7 +30,7 @@ class TessieEntity(CoordinatorEntity[TessieDataUpdateCoordinator]):
         self.vin = coordinator.vin
         self.key = key
 
-        car_type = coordinator.data["vehicle_config-car_type"]
+        car_type = coordinator.data["vehicle_config_car_type"]
 
         self._attr_translation_key = key
         self._attr_unique_id = f"{self.vin}-{key}"
@@ -35,11 +40,41 @@ class TessieEntity(CoordinatorEntity[TessieDataUpdateCoordinator]):
             configuration_url="https://my.tessie.com/",
             name=coordinator.data["display_name"],
             model=MODELS.get(car_type, car_type),
-            sw_version=coordinator.data["vehicle_state-car_version"],
-            hw_version=coordinator.data["vehicle_config-driver_assist"],
+            sw_version=coordinator.data["vehicle_state_car_version"],
+            hw_version=coordinator.data["vehicle_config_driver_assist"],
         )
 
-    @property
-    def value(self) -> Any:
+    def get(self, key: str | None = None, default: Any | None = None) -> Any:
         """Return value from coordinator data."""
-        return self.coordinator.data[self.key]
+        return self.coordinator.data.get(key or self.key, default)
+
+    async def run(self, func: Callable, **kargs: Any):
+        """Run a tessie_api function and handle exceptions."""
+        try:
+            response = await func(
+                session=self.coordinator.session,
+                vin=self.vin,
+                api_key=self.coordinator.api_key,
+                **kargs,
+            )
+        except ClientResponseError as e:
+            if e.status == HTTPStatus.INTERNAL_SERVER_ERROR:
+                # Create issue for Virtual Key setup
+                ir.async_create_issue(
+                    self.hass,
+                    DOMAIN,
+                    "virtual_key",
+                    is_fixable=True,
+                    is_persistent=False,
+                    learn_more_url="https://help.tessie.com/article/117-virtual-key",
+                    severity=ir.IssueSeverity.ERROR,
+                    translation_key="virtual_key",
+                )
+            raise HomeAssistantError from e
+        return response.get("result") is True
+
+    def set(self, *args):
+        """Set a value in coordinator data."""
+        for key, value in args:
+            self.coordinator.data[key] = value
+        self.async_write_ha_state()
