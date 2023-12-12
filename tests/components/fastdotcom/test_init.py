764b7c08aaab1f37e702923bdff55ada8931eb3c
G Johansson <goran.johansson@shiftit.se>
2023-12-12 20:23:55 +0000

    Add service deprecated test

--- tests/components/fastdotcom/test_init.py
+++ tests/components/fastdotcom/test_init.py
@@ -9,6 +9,7 @@ from homeassistant import config_entries
 from homeassistant.components.fastdotcom.const import DEFAULT_NAME, DOMAIN
 from homeassistant.const import EVENT_HOMEASSISTANT_STARTED
 from homeassistant.core import CoreState, HomeAssistant
+from homeassistant.helpers import issue_registry as ir
 from homeassistant.setup import async_setup_component
 
 from tests.common import MockConfigEntry
@@ -72,3 +73,32 @@ async def test_not_start_until_hass_started(
     await hass.async_block_till_done()
 
     assert config_entry.state == config_entries.ConfigEntryState.LOADED
+
+
+async def test_service_deprecated(
+    hass: HomeAssistant, issue_registry: ir.IssueRegistry
+) -> None:
+    """Test deprecated service."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="UNIQUE_TEST_ID",
+        title=DEFAULT_NAME,
+    )
+    config_entry.add_to_hass(hass)
+    with patch(
+        "homeassistant.components.fastdotcom.coordinator.fast_com", return_value=5.0
+    ):
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+    await hass.services.async_call(
+        DOMAIN,
+        "speedtest",
+        {},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    issue = issue_registry.async_get_issue(DOMAIN, "service_deprecation")
+    assert issue
+    assert issue.is_fixable is True
+    assert issue.translation_key == "service_deprecation"
