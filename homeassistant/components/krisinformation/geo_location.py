0df7c6bd459a5f49e07e177751dbbc30e40de69b
albin landgren <albinlandgreen@gmail.com>
2023-12-13 08:13:49 +0000

    Linting fixes

--- homeassistant/components/krisinformation/geo_location.py
+++ homeassistant/components/krisinformation/geo_location.py
@@ -13,7 +13,6 @@ from homeassistant.helpers.event import track_time_interval
 from homeassistant.helpers.typing import DiscoveryInfoType
 
 from .const import CONF_COUNTY
-from .sensor import _LOGGER
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=120)
 
@@ -29,32 +28,31 @@ class KrisInformationGeolocationEvent(GeolocationEvent):
 
     def __init__(
         self,
-        id: str,
+        external_id: str,
         name: str,
         latitude: float,
         longitude: float,
         unit_of_measurement: str,
         web: str,
         published: str,
-        area: str
-
+        area: str,
     ) -> None:
         """Initialize entity with data provided."""
-        self._external_id = id
+        self._external_id = external_id
         self._attr_name = name
         self._latitude = latitude
         self._longitude = longitude
         self._unit_of_measurement = unit_of_measurement
         self._web = web
-        self._published = published #: str | None = None
-        self._area = area #: str | None = None
+        self._published = published  #: str | None = None
+        self._area = area  #: str | None = None
 
         self._update()
 
-
     def _update(self) -> None:
         """Schedule regular updates based on configured time interval."""
-       # _LOGGER.info(self._web, self._published, self._area)
+
+    # _LOGGER.info(self._web, self._published, self._area)
 
     @property
     def extra_state_attributes(self) -> dict[str, Any]:
@@ -116,14 +114,26 @@ class KrisInformationGeolocationManager:
         self._crisis_alerter = crisis_alerter
 
     def _generate_event(
-        self, id: str, headline: str, latitude: float, longitude: float, web: str, published: str, area: str
+        self,
+        external_id: str,
+        headline: str,
+        latitude: float,
+        longitude: float,
+        web: str,
+        published: str,
+        area: str,
     ) -> KrisInformationGeolocationEvent:
-        """Generate a krisinformation geolocation event"""
-        event = KrisInformationGeolocationEvent(
-            id, headline, latitude, longitude, UnitOfLength.KILOMETERS, web, published, area
+        """Generate a krisinformation geolocation event."""
+        return KrisInformationGeolocationEvent(
+            external_id,
+            headline,
+            latitude,
+            longitude,
+            UnitOfLength.KILOMETERS,
+            web,
+            published,
+            area,
         )
-
-        return event
 
     def init_regular_updates(self) -> None:
         """Schedule regular updates based on configured time interval."""
@@ -138,12 +148,12 @@ class KrisInformationGeolocationManager:
     def _update(self) -> None:
         """Clear events and add new events."""
         new_events = []
-        for event in self._events:
-            self._events.remove(event)
-            self._hass.add_job(event.async_remove())
+        for existing_event in self._events:
+            self._events.remove(existing_event)
+            self._hass.add_job(existing_event.async_remove())
         events = self._crisis_alerter.vmas(is_test=True)
         for event in events:
-           #_LOGGER.info(event)
+            # _LOGGER.info(event)
             new_event = self._generate_event(
                 event["Identifier"],
                 event["Headline"],
@@ -155,7 +165,7 @@ class KrisInformationGeolocationManager:
                 ][0],
                 event["Web"],
                 event["Published"],
-                event["Area"][0]["Description"]
+                event["Area"][0]["Description"],
             )
             new_events.append(new_event)
             self._events.append(new_event)
