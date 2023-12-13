95171b291a00d3278f4afd9033e892638c940a2e
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-13 14:21:44 +0100

    Deduplicate constants A-D (#105638)

--- tests/components/xiaomi_miio/test_select.py
+++ tests/components/xiaomi_miio/test_select.py
@@ -17,7 +17,6 @@ from homeassistant.components.select import (
 )
 from homeassistant.components.xiaomi_miio import UPDATE_INTERVAL
 from homeassistant.components.xiaomi_miio.const import (
-    CONF_DEVICE,
     CONF_FLOW_TYPE,
     CONF_MAC,
     DOMAIN as XIAOMI_DOMAIN,
@@ -25,6 +24,7 @@ from homeassistant.components.xiaomi_miio.const import (
 )
 from homeassistant.const import (
     ATTR_ENTITY_ID,
+    CONF_DEVICE,
     CONF_HOST,
     CONF_MODEL,
     CONF_TOKEN,
