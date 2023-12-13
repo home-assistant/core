6ab42ee905d8819554f1699c326febcac27f26f8
Diogo Gomes <dgomes@ua.pt>
2023-12-13 21:52:17 +0000

    add diagnostic

---
+++ homeassistant/components/ipma/diagnostics.py
@@ -0,0 +1,34 @@
+"""Diagnostics support for IPMA."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
+from homeassistant.core import HomeAssistant
+
+from .const import DATA_API, DATA_LOCATION, DOMAIN
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+
+    location = hass.data[DOMAIN][entry.entry_id][DATA_LOCATION]
+    api = hass.data[DOMAIN][entry.entry_id][DATA_API]
+
+    return {
+        "location_information": {
+            "latitude": round(entry.data[CONF_LATITUDE], 3),
+            "longitude": round(entry.data[CONF_LONGITUDE], 3),
+            "global_id_local": location.global_id_local,
+            "id_station": location.id_station,
+            "latitude_station": location.latitude_station,
+            "longitude_station": location.longitude_station,
+            "name": location.name,
+            "station": location.station,
+        },
+        "current_weather": await location.observation(api),
+        "weather_forecast": await location.forecast(api, 1),
+    }
