ac53b78a0c208feefc4626462ed132e6ef15897c
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-13 14:21:44 +0100

    Deduplicate constants A-D (#105638)

--- homeassistant/components/prosegur/config_flow.py
+++ homeassistant/components/prosegur/config_flow.py
@@ -9,11 +9,11 @@ import voluptuous as vol
 
 from homeassistant import config_entries, core, exceptions
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_COUNTRY, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import aiohttp_client, selector
 
-from .const import CONF_CONTRACT, CONF_COUNTRY, DOMAIN
+from .const import CONF_CONTRACT, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
