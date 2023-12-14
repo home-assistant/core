244fd57fb7e39f01d3937f76a46e92984c72d4a5
Manuel Dipolt <manuel@linux-home.at>
2023-12-14 15:24:10 +0100

    reworked config flow, added steps step_unlock_http_interface, finish_config

--- homeassistant/components/romy/config_flow.py
+++ homeassistant/components/romy/config_flow.py
@@ -15,20 +15,18 @@ import homeassistant.helpers.config_validation as cv
 from .const import DOMAIN, LOGGER
 
 
-def _schema_with_defaults(
-    default_values: dict[str, Any] | None = None,
-    requires_password: bool = False,
-) -> vol.Schema:
-    if default_values is None:
-        default_values = {}
-    schema = {
-        vol.Required(CONF_HOST, default=default_values.get(CONF_HOST, "")): cv.string,
-    }
+def _schema_with_host() -> vol.Schema:
+    return vol.Schema(
+        {
+            vol.Required(CONF_HOST): cv.string,
+        },
+    )
 
-    if requires_password:
-        schema[vol.Required(CONF_PASSWORD)] = vol.All(str, vol.Length(8))
 
-    return vol.Schema(schema)
+def _schema_with_password() -> vol.Schema:
+    return vol.Schema(
+        {vol.Required(CONF_PASSWORD): vol.All(cv.string, vol.Length(8))},
+    )
 
 
 class RomyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -39,35 +37,59 @@ class RomyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     def __init__(self) -> None:
         """Handle a config flow for ROMY."""
-        self.discovery_schema = None
-        self.discovery_info: dict[str, Any] = {}
+        self.romy_info: dict[str, Any] = {}
 
     async def async_step_user(
         self, user_input: dict[str, str] | None = None
     ) -> FlowResult:
         """Handle the user step."""
         errors: dict[str, str] = {}
-        data = self.discovery_schema or _schema_with_defaults()
+        data = _schema_with_host()
 
         if user_input:
-            # Save the user input and finish the setup
-            new_romy = await romy.create_romy(
-                user_input[CONF_HOST], user_input.get(CONF_PASSWORD, "")
-            )
+            self.romy_info[CONF_HOST] = user_input[CONF_HOST]
 
-            await self.async_set_unique_id(new_romy.unique_id)
+            new_romy = await romy.create_romy(self.romy_info[CONF_HOST], "")
 
             if not new_romy.is_initialized:
                 errors[CONF_HOST] = "cannot_connect"
 
+            # check if already setuped
+            await self.async_set_unique_id(new_romy.unique_id)
+            self._abort_if_unique_id_configured()
+
+            self.romy_info[CONF_NAME] = new_romy.name
+
             if not new_romy.is_unlocked:
-                data = _schema_with_defaults(user_input, requires_password=True)
+                return await self.async_step_unlock_http_interface()
+
+            if not errors:
+                return await self._async_step_finish_config()
+
+        return self.async_show_form(step_id="user", data_schema=data, errors=errors)
+
+    async def async_step_unlock_http_interface(
+        self, user_input: dict[str, str] | None = None
+    ) -> FlowResult:
+        """Unlock the http interface with password if robot is locked."""
+        errors: dict[str, str] = {}
+        data = _schema_with_password()
+
+        if user_input:
+            self.romy_info[CONF_PASSWORD] = user_input[CONF_PASSWORD]
+            new_romy = await romy.create_romy(
+                self.romy_info[CONF_HOST], self.romy_info[CONF_PASSWORD]
+            )
+
+            if not new_romy.is_initialized or not new_romy.is_unlocked:
                 errors[CONF_PASSWORD] = "invalid_auth"
 
             if not errors:
-                return self.async_create_entry(title=new_romy.name, data=user_input)
+                return await self._async_step_finish_config()
 
-        return self.async_show_form(step_id="user", data_schema=data, errors=errors)
+        return self.async_show_form(
+            step_id="unlock_http_interface", data_schema=data, errors=errors
+        )
 
     async def async_step_zeroconf(
         self, discovery_info: zeroconf.ZeroconfServiceInfo
@@ -76,28 +98,19 @@ class RomyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         LOGGER.debug("Zeroconf discovery_info: %s", discovery_info)
 
-        # get ROMY's name and check if local http interface is locked
+        # connect and gather information from your ROMY
         host = discovery_info.host
         LOGGER.debug("ZeroConf Host: %s", host)
 
         new_discovered_romy = await romy.create_romy(host, "")
 
+        name = new_discovered_romy.name
+        LOGGER.debug("ZeroConf Name: %s", name)
+
         # get unique id and stop discovery if robot is already added
         unique_id = new_discovered_romy.unique_id
         LOGGER.debug("ZeroConf Unique_id: %s", unique_id)
         await self.async_set_unique_id(unique_id)
-
-        name = new_discovered_romy.name
-        LOGGER.debug("ZeroConf Name: %s", name)
-
-        self.discovery_info.update(
-            {
-                CONF_HOST: host,
-                CONF_NAME: name,
-                CONF_UUID: unique_id,
-            }
-        )
-
         self._abort_if_unique_id_configured()
 
         self.context.update(
@@ -107,31 +120,32 @@ class RomyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             }
         )
 
-        self.discovery_schema = _schema_with_defaults(
-            {CONF_HOST: discovery_info.host},
-            requires_password=not new_discovered_romy.is_unlocked,
-        )
+        self.romy_info[CONF_HOST] = host
+        self.romy_info[CONF_NAME] = name
+        self.romy_info[CONF_UUID] = unique_id
 
         # if robot is already unlocked add it directly
         if new_discovered_romy.is_initialized and new_discovered_romy.is_unlocked:
             return await self.async_step_zeroconf_confirm()
 
-        return await self.async_step_user()
+        return await self.async_step_unlock_http_interface()
 
     async def async_step_zeroconf_confirm(
-        self, user_input: dict[str, Any] | None = None
+        self, user_input: dict[str, str] | None = None
     ) -> FlowResult:
         """Handle a confirmation flow initiated by zeroconf."""
         if user_input is None:
             return self.async_show_form(
                 step_id="zeroconf_confirm",
                 description_placeholders={
-                    "name": self.discovery_info[CONF_NAME],
-                    "host": self.discovery_info[CONF_HOST],
+                    "name": self.romy_info[CONF_NAME],
+                    "host": self.romy_info[CONF_HOST],
                 },
             )
+        return await self._async_step_finish_config()
 
+    async def _async_step_finish_config(self) -> FlowResult:
+        """Finish the configuration setup."""
         return self.async_create_entry(
-            title=self.discovery_info[CONF_NAME],
-            data=self.discovery_info,
+            title=self.romy_info[CONF_NAME], data=self.romy_info
         )
