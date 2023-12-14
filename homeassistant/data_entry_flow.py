375b0691ba2223fd5018cfc3cc4a7d4bf35eeb23
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-14 11:58:35 +0100

    Add generic type to FlowHandler

--- homeassistant/data_entry_flow.py
+++ homeassistant/data_entry_flow.py
@@ -185,7 +185,7 @@ class FlowManager(abc.ABC):
     @abc.abstractmethod
     async def async_finish_flow(
         self, flow: FlowHandler, result: FlowResult
-    ) -> FlowResult:
+    ) -> _FlowResultT:
         """Finish a data entry flow."""
 
     async def async_post_init(self, flow: FlowHandler, result: FlowResult) -> None:
@@ -570,7 +570,7 @@ class FlowHandler(Generic[_FlowResultT]):
         data: Mapping[str, Any],
         description: str | None = None,
         description_placeholders: Mapping[str, str] | None = None,
-    ) -> FlowResult:
+    ) -> _FlowResultT:
         """Finish flow."""
         flow_result = FlowResult(
             type=FlowResultType.CREATE_ENTRY,
