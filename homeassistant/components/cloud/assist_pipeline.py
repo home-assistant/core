4da04a358ab00ea471f86198aa6689b6bb74f3b2
Martin Hjelmare <marhje52@gmail.com>
2023-12-14 23:56:08 +0100

    Refactor cloud assist pipeline (#105723)
    * Refactor cloud assist pipeline
    
    * Return None early

---
+++ homeassistant/components/cloud/assist_pipeline.py
@@ -0,0 +1,44 @@
+"""Handle Cloud assist pipelines."""
+from homeassistant.components.assist_pipeline import (
+    async_create_default_pipeline,
+    async_get_pipelines,
+    async_setup_pipeline_store,
+)
+from homeassistant.components.conversation import HOME_ASSISTANT_AGENT
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+
+async def async_create_cloud_pipeline(hass: HomeAssistant) -> str | None:
+    """Create a cloud assist pipeline."""
+    # Make sure the pipeline store is loaded, needed because assist_pipeline
+    # is an after dependency of cloud
+    await async_setup_pipeline_store(hass)
+
+    def cloud_assist_pipeline(hass: HomeAssistant) -> str | None:
+        """Return the ID of a cloud-enabled assist pipeline or None.
+
+        Check if a cloud pipeline already exists with
+        legacy cloud engine id.
+        """
+        for pipeline in async_get_pipelines(hass):
+            if (
+                pipeline.conversation_engine == HOME_ASSISTANT_AGENT
+                and pipeline.stt_engine == DOMAIN
+                and pipeline.tts_engine == DOMAIN
+            ):
+                return pipeline.id
+        return None
+
+    if (cloud_assist_pipeline(hass)) is not None or (
+        cloud_pipeline := await async_create_default_pipeline(
+            hass,
+            stt_engine_id=DOMAIN,
+            tts_engine_id=DOMAIN,
+            pipeline_name="Home Assistant Cloud",
+        )
+    ) is None:
+        return None
+
+    return cloud_pipeline.id
