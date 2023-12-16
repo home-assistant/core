612518d4fe7dc2631201c38420d4a6c472ef7d2c
Michael <35783820+mib1185@users.noreply.github.com>
2023-12-16 19:38:58 +0100

    Address late review comments on AVM FRITZ!SmartHome (#105860)
    set copies dict keys by default

--- homeassistant/components/fritzbox/sensor.py
+++ homeassistant/components/fritzbox/sensor.py
@@ -230,7 +230,7 @@ async def async_setup_entry(
 
     entry.async_on_unload(coordinator.async_add_listener(_add_entities))
 
-    _add_entities(set(coordinator.data.devices.keys()))
+    _add_entities(set(coordinator.data.devices))
 
 
 class FritzBoxSensor(FritzBoxDeviceEntity, SensorEntity):
