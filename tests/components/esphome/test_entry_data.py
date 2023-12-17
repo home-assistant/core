6f86ade53f4fead37f6e1e73ddce2f5a2487d78b
J. Nick Koston <nick@koston.org>
2023-12-16 17:20:07 -1000

    add more cover

--- tests/components/esphome/test_entry_data.py
+++ tests/components/esphome/test_entry_data.py
@@ -122,3 +122,13 @@ async def test_bluetooth_connect(
     assert scanner is not None
     assert scanner.connectable is True
     assert scanner.scanning is True
+    assert scanner.connector.can_connect() is False  # no connection slots
+    await mock_bluetooth_entry_with_raw_adv.mock_disconnect(True)
+    await hass.async_block_till_done()
+
+    scanner = bluetooth.async_scanner_by_source(hass, "11:22:33:44:55:aa")
+    assert scanner is None
+    await mock_bluetooth_entry_with_raw_adv.mock_connect()
+    await hass.async_block_till_done()
+    scanner = bluetooth.async_scanner_by_source(hass, "11:22:33:44:55:aa")
+    assert scanner.scanning is True
