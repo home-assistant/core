6f527b614f8d98d4641c70981cffe26ac645d66d
J. Nick Koston <nick@koston.org>
2023-12-16 17:27:21 -1000

    test for legacy

--- tests/components/esphome/test_entry_data.py
+++ tests/components/esphome/test_entry_data.py
@@ -7,11 +7,8 @@ from aioesphomeapi import (
     SensorState,
 )
 
-from homeassistant.components import bluetooth
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
-
-from .conftest import MockESPHomeDevice
 
 
 async def test_migrate_entity_unique_id(
@@ -112,23 +109,3 @@ async def test_migrate_entity_unique_id_downgrade_upgrade(
     # Note that ESPHome includes the EntityInfo type in the unique id
     # as this is not a 1:1 mapping to the entity platform (ie. text_sensor)
     assert entry.unique_id == "11:22:33:44:55:aa-sensor-mysensor"
-
-
-async def test_bluetooth_connect(
-    hass: HomeAssistant, mock_bluetooth_entry_with_raw_adv: MockESPHomeDevice
-) -> None:
-    """Test bluetooth connect."""
-    scanner = bluetooth.async_scanner_by_source(hass, "11:22:33:44:55:aa")
-    assert scanner is not None
-    assert scanner.connectable is True
-    assert scanner.scanning is True
-    assert scanner.connector.can_connect() is False  # no connection slots
-    await mock_bluetooth_entry_with_raw_adv.mock_disconnect(True)
-    await hass.async_block_till_done()
-
-    scanner = bluetooth.async_scanner_by_source(hass, "11:22:33:44:55:aa")
-    assert scanner is None
-    await mock_bluetooth_entry_with_raw_adv.mock_connect()
-    await hass.async_block_till_done()
-    scanner = bluetooth.async_scanner_by_source(hass, "11:22:33:44:55:aa")
-    assert scanner.scanning is True
