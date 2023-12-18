5d2a4b555977625efb2b53dd503db626546ab2a4
Franck Nijhof <git@frenck.dev>
2023-12-18 14:11:43 +0100

    Add button platform to Tailwind integration (#105961)

--- homeassistant/components/tailwind/__init__.py
+++ homeassistant/components/tailwind/__init__.py
@@ -8,7 +8,7 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 from .coordinator import TailwindDataUpdateCoordinator
 
-PLATFORMS = [Platform.NUMBER]
+PLATFORMS = [Platform.BUTTON, Platform.NUMBER]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
