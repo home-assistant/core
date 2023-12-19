151b71b32631b7c26cf9fe839463318fee3917fd
Franck Nijhof <git@frenck.dev>
2023-12-19 10:38:56 +0100

    Add binary sensor platform to Tailwind integration (#106033)

--- homeassistant/components/tailwind/__init__.py
+++ homeassistant/components/tailwind/__init__.py
@@ -8,7 +8,7 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 from .coordinator import TailwindDataUpdateCoordinator
 
-PLATFORMS = [Platform.BUTTON, Platform.NUMBER]
+PLATFORMS = [Platform.BINARY_SENSOR, Platform.BUTTON, Platform.NUMBER]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
