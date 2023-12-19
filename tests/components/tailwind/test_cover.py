986e9e4ee07d5ceae93691530b10f9ff44cf569c
Franck Nijhof <git@frenck.dev>
2023-12-19 12:02:12 +0100

    Improve/fix snapshot tests

--- tests/components/tailwind/test_cover.py
+++ tests/components/tailwind/test_cover.py
@@ -17,35 +17,43 @@ from homeassistant.helpers import device_registry as dr, entity_registry as er
 pytestmark = pytest.mark.usefixtures("init_integration")
 
 
+@pytest.mark.parametrize(
+    "entity_id",
+    [
+        "cover.door_1",
+        "cover.door_2",
+    ],
+)
 async def test_cover_entities(
     hass: HomeAssistant,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
-    mock_tailwind: MagicMock,
     snapshot: SnapshotAssertion,
+    entity_id: str,
 ) -> None:
     """Test cover entities provided by the Tailwind integration."""
-    for entity_id in (
-        "cover.door_1",
-        "cover.door_2",
-    ):
-        assert (state := hass.states.get(entity_id))
-        assert state == snapshot(name=entity_id)
+    assert (state := hass.states.get(entity_id))
+    assert state == snapshot
 
-        assert (entity_entry := entity_registry.async_get(state.entity_id))
-        assert entity_entry == snapshot(name=entity_id)
+    assert (entity_entry := entity_registry.async_get(state.entity_id))
+    assert entity_entry == snapshot
 
-        assert entity_entry.device_id
-        assert (device_entry := device_registry.async_get(entity_entry.device_id))
-        assert device_entry == snapshot(name=entity_id)
+    assert entity_entry.device_id
+    assert (device_entry := device_registry.async_get(entity_entry.device_id))
+    assert device_entry == snapshot
 
-    # Test operating the doors
+
+async def test_cover_operations(
+    hass: HomeAssistant,
+    mock_tailwind: MagicMock,
+) -> None:
+    """Test operating the doors."""
     assert len(mock_tailwind.operate.mock_calls) == 0
     await hass.services.async_call(
         COVER_DOMAIN,
         SERVICE_OPEN_COVER,
         {
-            ATTR_ENTITY_ID: state.entity_id,
+            ATTR_ENTITY_ID: "cover.door_1",
         },
         blocking=True,
     )
@@ -58,7 +66,7 @@ async def test_cover_entities(
         COVER_DOMAIN,
         SERVICE_CLOSE_COVER,
         {
-            ATTR_ENTITY_ID: state.entity_id,
+            ATTR_ENTITY_ID: "cover.door_1",
         },
         blocking=True,
     )
