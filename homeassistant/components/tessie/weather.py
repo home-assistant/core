1a5d2a76f6575aaa14ce6809408bf509fee345a3
Brett Adams <brett.whynot@gmail.com>
2023-12-19 01:48:50 +0000

    Rework

--- homeassistant/components/tessie/weather.py
+++ homeassistant/components/tessie/weather.py
@@ -21,15 +21,15 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
 )
 
-from .const import DOMAIN
+from .const import DOMAIN, MODELS
 from .coordinator import TessieDataUpdateCoordinator
-from .entity import TessieEntity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -72,14 +72,13 @@ class TessieWeatherDataCoordinator(DataUpdateCoordinator):
             hass,
             _LOGGER,
             name="Tessie Weather",
-            update_method=self.async_update_data,
             update_interval=timedelta(seconds=TESSIE_WEATHER_SYNC_INTERVAL),
         )
         self.api_key = api_key
         self.vin = vin
         self.session = async_get_clientsession(hass)
 
-    async def async_update_data(self) -> dict[str, Any]:
+    async def _async_update_data(self) -> dict[str, Any]:
         """Update weather data using Tessie API."""
 
         try:
@@ -95,8 +94,8 @@ class TessieWeatherDataCoordinator(DataUpdateCoordinator):
             raise e
 
 
-class TessieWeatherEntity(TessieEntity, WeatherEntity):
-    """Base class for Tessie metric Weathers."""
+class TessieWeatherEntity(CoordinatorEntity, WeatherEntity):
+    """Base class for Tessie weathers entities."""
 
     _attr_native_temperature_unit = UnitOfTemperature.CELSIUS
     _attr_native_precipitation_unit = UnitOfPressure.HPA
@@ -108,54 +107,64 @@ class TessieWeatherEntity(TessieEntity, WeatherEntity):
         vehiclecoordinator: TessieDataUpdateCoordinator,
         weathercoordinator: TessieWeatherDataCoordinator,
     ) -> None:
-        """Initialize the sensor."""
-        # Setup base data
-        super().__init__(vehiclecoordinator, "weather")
-        # Setup CoordinatorEntity with weathercoordinator instead.
-        CoordinatorEntity.__init__(self, weathercoordinator)
-        self.weathercoordinator = weathercoordinator
+        """Initialize the data coordinator."""
+        super().__init__(weathercoordinator)
+
+        car_type = vehiclecoordinator.data["vehicle_config_car_type"]
+
+        self._attr_translation_key = "weather"
+        self._attr_unique_id = f"{vehiclecoordinator.vin}-weather"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, vehiclecoordinator.vin)},
+            manufacturer="Tesla",
+            configuration_url="https://my.tessie.com/",
+            name=vehiclecoordinator.data["display_name"],
+            model=MODELS.get(car_type, car_type),
+            sw_version=vehiclecoordinator.data["vehicle_state_car_version"],
+            hw_version=vehiclecoordinator.data["vehicle_config_driver_assist"],
+        )
 
     @property
     def native_apparent_temperature(self) -> float | None:
         """Return the apparent temperature in native units."""
-        return self.weathercoordinator.data.get("feels_like")
+        return self.coordinator.data.get("feels_like")
 
     @property
     def native_temperature(self) -> float | None:
         """Return the temperature in native units."""
-        return self.weathercoordinator.data.get("temperature")
+        return self.coordinator.data.get("temperature")
 
     @property
     def native_pressure(self) -> float | None:
         """Return the pressure in native units."""
-        return self.weathercoordinator.data.get("pressure")
+        return self.coordinator.data.get("pressure")
 
     @property
     def humidity(self) -> float | None:
         """Return the humidity in native units."""
-        return self.weathercoordinator.data.get("humidity")
+        return self.coordinator.data.get("humidity")
 
     @property
     def native_wind_speed(self) -> float | None:
         """Return the wind speed in native units."""
-        return self.weathercoordinator.data.get("wind_speed")
+        return self.coordinator.data.get("wind_speed")
 
     @property
     def wind_bearing(self) -> float | str | None:
         """Return the wind bearing."""
-        return self.weathercoordinator.data.get("wind_direction")
+        return self.coordinator.data.get("wind_direction")
 
     @property
     def cloud_coverage(self) -> float | None:
         """Return the Cloud coverage in %."""
-        return self.weathercoordinator.data.get("cloudiness")
+        return self.coordinator.data.get("cloudiness")
 
     @property
     def native_visibility(self) -> float | None:
         """Return the visibility in native units."""
-        return self.weathercoordinator.data.get("visibility")
+        return self.coordinator.data.get("visibility")
 
     @property
     def condition(self) -> str | None:
         """Return the current condition."""
-        return self.weathercoordinator.data.get("condition")
+        return self.coordinator.data.get("condition")
