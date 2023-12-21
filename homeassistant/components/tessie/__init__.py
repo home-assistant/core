8514dd6e5947416e8bcfffb430cbd0928b126633
Brett Adams <Bre77@users.noreply.github.com>
2023-12-21 15:34:52 +1000

    Add switch platform to Tessie (#106153)
    * Add switch platform
    
    * Make functions mandatory
    
    * Underscores
    
    * Improvements

--- homeassistant/components/tessie/__init__.py
+++ homeassistant/components/tessie/__init__.py
@@ -14,7 +14,7 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import DOMAIN
 from .coordinator import TessieDataUpdateCoordinator
 
-PLATFORMS = [Platform.BINARY_SENSOR, Platform.CLIMATE, Platform.SENSOR]
+PLATFORMS = [Platform.BINARY_SENSOR, Platform.CLIMATE, Platform.SENSOR, Platform.SWITCH]
 
 _LOGGER = logging.getLogger(__name__)
 
