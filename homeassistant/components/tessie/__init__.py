7c5824b4f389ae11a97361caf1f084677eca6aec
Brett Adams <Bre77@users.noreply.github.com>
2023-12-21 15:18:18 +1000

    Add climate platform to Tessie (#105420)
    * Add climate platform
    
    * Other fixes
    
    * Use super native value
    
    * change to _value
    
    * Sentence case strings
    
    * Add some more type definition
    
    * Add return types
    
    * Add some more assertions
    
    * Remove VirtualKey error
    
    * Add type to args
    
    * rename climate to primary
    
    * fix min max
    
    * Use String Enum
    
    * Add PRECISION_HALVES
    
    * Fix string enum
    
    * fix str enum
    
    * Simplify run logic
    
    * Rename enum to TessieClimateKeeper

--- homeassistant/components/tessie/__init__.py
+++ homeassistant/components/tessie/__init__.py
@@ -14,7 +14,7 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import DOMAIN
 from .coordinator import TessieDataUpdateCoordinator
 
-PLATFORMS = [Platform.BINARY_SENSOR, Platform.SENSOR]
+PLATFORMS = [Platform.BINARY_SENSOR, Platform.CLIMATE, Platform.SENSOR]
 
 _LOGGER = logging.getLogger(__name__)
 
