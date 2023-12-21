587c0b049ca883a359ce1cfae461ee5e7aa1ae06
Patrick Frazer <pfrazer@chandlersystemsinc.com>
2023-12-21 11:19:26 -0500

    Tidy up initializer

--- homeassistant/components/drop_connect/entity.py
+++ homeassistant/components/drop_connect/entity.py
@@ -31,22 +31,24 @@ class DROPEntity(CoordinatorEntity[DROPDeviceDataUpdateCoordinator]):
         if TYPE_CHECKING:
             assert coordinator.config_entry is not None
             assert coordinator.config_entry.unique_id is not None
-        self._attr_unique_id = f"{coordinator.config_entry.unique_id}_{entity_type}"
-        model: str = coordinator.config_entry.data[CONF_DEVICE_DESC]
-        if coordinator.config_entry.data[CONF_DEVICE_TYPE] == DEV_HUB:
-            model = f"Hub {coordinator.config_entry.data[CONF_HUB_ID]}"
+        unique_id = coordinator.config_entry.unique_id
+        self._attr_unique_id = f"{unique_id}_{entity_type}"
+        entry_data = coordinator.config_entry.data
+        model: str = entry_data[CONF_DEVICE_DESC]
+        if entry_data[CONF_DEVICE_TYPE] == DEV_HUB:
+            model = f"Hub {entry_data[CONF_HUB_ID]}"
         self._attr_device_info = DeviceInfo(
             manufacturer="Chandler Systems, Inc.",
             model=model,
-            name=coordinator.config_entry.data[CONF_DEVICE_NAME],
-            identifiers={(DOMAIN, coordinator.config_entry.unique_id)},
+            name=entry_data[CONF_DEVICE_NAME],
+            identifiers={(DOMAIN, unique_id)},
         )
-        if coordinator.config_entry.data[CONF_DEVICE_TYPE] != DEV_HUB:
+        if entry_data[CONF_DEVICE_TYPE] != DEV_HUB:
             self._attr_device_info.update(
                 {
                     "via_device": (
                         DOMAIN,
-                        coordinator.config_entry.data[CONF_DEVICE_OWNER_ID],
+                        entry_data[CONF_DEVICE_OWNER_ID],
                     )
                 }
             )
