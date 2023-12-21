688fae8f6df9d875fefb4645c1a2bb3c606ffd54
Brett Adams <brett.whynot@gmail.com>
2023-12-21 00:21:04 +0000

    Remove virtual key issue

--- tests/components/tessie/test_cover.py
+++ tests/components/tessie/test_cover.py
@@ -10,13 +10,11 @@ from homeassistant.components.cover import (
     STATE_CLOSED,
     STATE_OPEN,
 )
-from homeassistant.components.tessie.const import DOMAIN
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers import issue_registry as ir
 
-from .common import ERROR_UNKNOWN, ERROR_VIRTUAL_KEY, TEST_RESPONSE, setup_platform
+from .common import ERROR_UNKNOWN, TEST_RESPONSE, setup_platform
 
 
 async def test_window(hass: HomeAssistant) -> None:
@@ -94,25 +92,10 @@ async def test_charge_port(hass: HomeAssistant) -> None:
 
 
 async def test_errors(hass: HomeAssistant) -> None:
-    """Tests virtual key error is handled."""
+    """Tests unknown error is handled."""
 
     await setup_platform(hass)
     entity_id = "cover.test_charge_port_door"
-
-    # Test setting cover open with virtual key error
-    with patch(
-        "homeassistant.components.tessie.cover.open_unlock_charge_port",
-        side_effect=ERROR_VIRTUAL_KEY,
-    ) as mock_set, pytest.raises(HomeAssistantError) as error:
-        await hass.services.async_call(
-            COVER_DOMAIN,
-            SERVICE_OPEN_COVER,
-            {ATTR_ENTITY_ID: [entity_id]},
-            blocking=True,
-        )
-
-    issue_reg = ir.async_get(hass)
-    assert issue_reg.async_get_issue(DOMAIN, "virtual_key")
 
     # Test setting cover open with unknown error
     with patch(
