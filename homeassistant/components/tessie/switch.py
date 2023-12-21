9b3b5d3b08ff9d0b226f82a171ccf419202d910b
Brett Adams <brett.whynot@gmail.com>
2023-12-21 04:35:01 +0000

    Make functions mandatory

--- homeassistant/components/tessie/switch.py
+++ homeassistant/components/tessie/switch.py
@@ -36,8 +36,8 @@ from .entity import TessieEntity
 class TessieSwitchEntityDescription(SwitchEntityDescription):
     """Describes Tessie Switch entity."""
 
-    on_func: Callable | None = None
-    off_func: Callable | None = None
+    on_func: Callable
+    off_func: Callable
     device_class: SwitchDeviceClass = SwitchDeviceClass.SWITCH
 
 
@@ -108,14 +108,12 @@ class TessieSwitchEntity(TessieEntity, SwitchEntity):
     @property
     def is_on(self) -> bool:
         """Return the state of the Switch."""
-        return self.get()
+        return self._value
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn on the Switch."""
-        assert self.entity_description.on_func is not None
         await self.run(self.entity_description.on_func)
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the Switch."""
-        assert self.entity_description.off_func is not None
         await self.run(self.entity_description.off_func)
