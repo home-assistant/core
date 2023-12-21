599e58c2197c2d54bbd190000dfea160ec57e87e
Patrick Frazer <pfrazer@chandlersystemsinc.com>
2023-12-21 11:41:39 -0500

    Make drop_api a class property

--- homeassistant/components/drop_connect/coordinator.py
+++ homeassistant/components/drop_connect/coordinator.py
@@ -17,19 +17,9 @@ _LOGGER = logging.getLogger(__name__)
 class DROPDeviceDataUpdateCoordinator(DataUpdateCoordinator):
     """DROP device object."""
 
-    _drop_api: DropAPI | None = None
-
     def __init__(self, hass: HomeAssistant, unique_id: str) -> None:
         """Initialize the device."""
         super().__init__(hass, _LOGGER, name=f"{DOMAIN}-{unique_id}")
         if TYPE_CHECKING:
             assert self.config_entry is not None
-        self._drop_api = DropAPI()
-
-    # Device properties
-    @property
-    def drop_api(self) -> DropAPI:
-        """Return the API instance."""
-        if TYPE_CHECKING:
-            assert self._drop_api is not None
-        return self._drop_api
+        self.drop_api = DropAPI()
