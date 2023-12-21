a075b65b884c294680c7aa28768d732d796f87d7
Paul Daumlechner <paul.daumlechner@live.de>
2023-12-21 20:12:12 +0000

    Reduce complexity

--- homeassistant/components/velux/const.py
+++ homeassistant/components/velux/const.py
@@ -4,13 +4,9 @@ from homeassistant.const import Platform
 ATTR_VELOCITY = "velocity"
 DOMAIN = "velux"
 PLATFORMS = [
-    Platform.BUTTON,
     Platform.COVER,
     Platform.LIGHT,
-    Platform.NUMBER,
     Platform.SCENE,
-    Platform.SENSOR,
-    Platform.SWITCH,
 ]
 UPPER_COVER = "upper"
 LOWER_COVER = "lower"
