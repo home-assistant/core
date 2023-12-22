3a4c52f72220d3350a947e3d2867c4f5132d061a
Brett Adams <brett.whynot@gmail.com>
2023-12-22 01:40:31 +0000

    Add more props

--- homeassistant/components/tessie/media.py
+++ homeassistant/components/tessie/media.py
@@ -14,6 +14,12 @@ from .const import DOMAIN
 from .coordinator import TessieDataUpdateCoordinator
 from .entity import TessieEntity
 
+STATES = {
+    "Playing": MediaPlayerState.PLAYING,
+    "Paused": MediaPlayerState.PAUSED,
+    "Stopped": MediaPlayerState.IDLE,
+}
+
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
@@ -35,14 +41,58 @@ class TessieMediaEntity(TessieEntity, MediaPlayerEntity):
         coordinator: TessieDataUpdateCoordinator,
     ) -> None:
         """Initialize the media player entity."""
-        super().__init__(coordinator, "vehicle_state-media_info_media_playback_status")
+        super().__init__(coordinator, "vehicle_state_media_info_media_playback_status")
 
     @property
     def state(self) -> MediaPlayerState | None:
         """State of the player."""
-        state = self.get()
-        if state == "Playing":
-            return MediaPlayerState.PLAYING
-        if state == "Paused":
-            return MediaPlayerState.PAUSED
-        return MediaPlayerState.OFF
+        return STATES.get(self._value, None)
+
+    @property
+    def volume_level(self) -> float | None:
+        """Volume level of the media player (0..1)."""
+        return self.get("vehicle_state_media_info_audio_volume") / self.get(
+            "vehicle_state_media_info_audio_volume_max"
+        )
+
+    @property
+    def volume_step(self) -> float:
+        """Return the step to be used by the volume_up and volume_down services."""
+        return self.get("vehicle_state_media_info_audio_increment") / self.get(
+            "vehicle_state_media_info_audio_volume_max"
+        )
+
+    @property
+    def media_duration(self) -> int | None:
+        """Duration of current playing media in seconds."""
+        return self.get("vehicle_state_media_info_now_playing_duration", 0) / 1000
+
+    @property
+    def media_position(self) -> int | None:
+        """Position of current playing media in seconds."""
+        return self.get("vehicle_state_media_info_now_playing_elapsed", 0) / 1000
+
+    @property
+    def media_title(self) -> str | None:
+        """Title of current playing media."""
+        return self.get("vehicle_state_media_info_now_playing_title")
+
+    @property
+    def media_artist(self) -> str | None:
+        """Artist of current playing media, music track only."""
+        return self.get("vehicle_state_media_info_now_playing_artist")
+
+    @property
+    def media_album_name(self) -> str | None:
+        """Album name of current playing media, music track only."""
+        return self.get("vehicle_state_media_info_now_playing_album")
+
+    @property
+    def media_playlist(self) -> str | None:
+        """Title of Playlist currently playing."""
+        return self.get("vehicle_state_media_info_now_playing_station")
+
+    @property
+    def source(self) -> str | None:
+        """Name of the current input source."""
+        return self.get("vehicle_state_media_info_now_playing_source")
