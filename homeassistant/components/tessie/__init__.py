5ac2110db25c02fe68f2c8e656af2f47de4cd160
Brett Adams <Bre77@users.noreply.github.com>
2023-12-22 10:28:58 +1000

    Add device tracker to Tessie (#105428)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/tessie/__init__.py
+++ homeassistant/components/tessie/__init__.py
@@ -17,6 +17,7 @@ from .coordinator import TessieDataUpdateCoordinator
 PLATFORMS = [
     Platform.BINARY_SENSOR,
     Platform.CLIMATE,
+    Platform.DEVICE_TRACKER,
     Platform.SELECT,
     Platform.SENSOR,
     Platform.SWITCH,
