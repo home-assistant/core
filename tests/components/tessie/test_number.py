bbb221b9dcbba15d4e6704242e049d567c4b8ff3
Brett Adams <brett.whynot@gmail.com>
2023-12-22 05:39:28 +0000

    Add patch_description to tests

--- tests/components/tessie/test_number.py
+++ tests/components/tessie/test_number.py
@@ -1,12 +1,12 @@
 """Test the Tessie number platform."""
 
-from unittest.mock import patch
 
 from homeassistant.components.number import DOMAIN as NUMBER_DOMAIN, SERVICE_SET_VALUE
+from homeassistant.components.tessie.number import DESCRIPTIONS
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
 
-from .common import TEST_VEHICLE_STATE_ONLINE, setup_platform
+from .common import TEST_VEHICLE_STATE_ONLINE, patch_description, setup_platform
 
 
 async def test_numbers(hass: HomeAssistant) -> None:
@@ -16,7 +16,7 @@ async def test_numbers(hass: HomeAssistant) -> None:
 
     await setup_platform(hass)
 
-    assert len(hass.states.async_all("number")) == 3
+    assert len(hass.states.async_all("number")) == len(DESCRIPTIONS)
 
     assert hass.states.get("number.test_charge_current").state == str(
         TEST_VEHICLE_STATE_ONLINE["charge_state"]["charge_current_request"]
@@ -33,10 +33,9 @@ async def test_numbers(hass: HomeAssistant) -> None:
     )
 
     # Test number set value functions
-    with patch(
-        "homeassistant.components.tessie.number.set_charging_amps",
-        return_value=True,
-    ) as mock_run:
+    with patch_description(
+        "charge_state_charge_current_request", "func", DESCRIPTIONS
+    ) as mock_set_charging_amps:
         await hass.services.async_call(
             NUMBER_DOMAIN,
             SERVICE_SET_VALUE,
@@ -44,12 +43,11 @@ async def test_numbers(hass: HomeAssistant) -> None:
             blocking=True,
         )
         assert hass.states.get("number.test_charge_current").state == "16.0"
-        mock_run.assert_called_once()
+        mock_set_charging_amps.assert_called_once()
 
-    with patch(
-        "homeassistant.components.tessie.number.set_charge_limit",
-        return_value=True,
-    ) as mock_run:
+    with patch_description(
+        "charge_state_charge_limit_soc", "func", DESCRIPTIONS
+    ) as mock_set_charge_limit:
         await hass.services.async_call(
             NUMBER_DOMAIN,
             SERVICE_SET_VALUE,
@@ -57,12 +55,11 @@ async def test_numbers(hass: HomeAssistant) -> None:
             blocking=True,
         )
         assert hass.states.get("number.test_charge_limit").state == "80.0"
-        mock_run.assert_called_once()
+        mock_set_charge_limit.assert_called_once()
 
-    with patch(
-        "homeassistant.components.tessie.number.set_speed_limit",
-        return_value=True,
-    ) as mock_run:
+    with patch_description(
+        "vehicle_state_speed_limit_mode_current_limit_mph", "func", DESCRIPTIONS
+    ) as mock_set_speed_limit:
         await hass.services.async_call(
             NUMBER_DOMAIN,
             SERVICE_SET_VALUE,
@@ -70,4 +67,4 @@ async def test_numbers(hass: HomeAssistant) -> None:
             blocking=True,
         )
         assert hass.states.get("number.test_speed_limit").state == "60.0"
-        mock_run.assert_called_once()
+        mock_set_speed_limit.assert_called_once()
