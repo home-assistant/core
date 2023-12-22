02bf4e12bf669dd5319f224a973b8da721d631d1
Brett Adams <brett.whynot@gmail.com>
2023-12-22 03:55:44 +0000

    Update tests

--- tests/components/tessie/test_lock.py
+++ tests/components/tessie/test_lock.py
@@ -7,11 +7,10 @@ from homeassistant.components.lock import (
     SERVICE_LOCK,
     SERVICE_UNLOCK,
 )
-from homeassistant.components.tessie.const import TessieCategory
 from homeassistant.const import ATTR_ENTITY_ID, STATE_LOCKED, STATE_UNLOCKED
 from homeassistant.core import HomeAssistant
 
-from .common import TEST_RESPONSE, TEST_VEHICLE_STATE_ONLINE, setup_platform
+from .common import TEST_VEHICLE_STATE_ONLINE, setup_platform
 
 
 async def test_locks(hass: HomeAssistant) -> None:
@@ -23,33 +22,29 @@ async def test_locks(hass: HomeAssistant) -> None:
 
     assert len(hass.states.async_all("lock")) == 1
 
+    entity_id = "lock.test"
+
     assert (
-        hass.states.get("lock.test_lock").state == STATE_LOCKED
-    ) == TEST_VEHICLE_STATE_ONLINE[TessieCategory.VEHICLE_STATE]["locked"]
+        hass.states.get(entity_id).state == STATE_LOCKED
+    ) == TEST_VEHICLE_STATE_ONLINE["vehicle_state"]["locked"]
 
     # Test lock set value functions
-    with patch(
-        "homeassistant.components.tessie.lock.lock",
-        return_value=TEST_RESPONSE,
-    ) as mock_run:
+    with patch("homeassistant.components.tessie.lock.lock") as mock_run:
         await hass.services.async_call(
             LOCK_DOMAIN,
             SERVICE_LOCK,
-            {ATTR_ENTITY_ID: ["lock.test_lock"]},
+            {ATTR_ENTITY_ID: [entity_id]},
             blocking=True,
         )
-        assert hass.states.get("lock.test_lock").state == STATE_LOCKED
+        assert hass.states.get(entity_id).state == STATE_LOCKED
         mock_run.assert_called_once()
 
-    with patch(
-        "homeassistant.components.tessie.lock.unlock",
-        return_value=TEST_RESPONSE,
-    ) as mock_run:
+    with patch("homeassistant.components.tessie.lock.unlock") as mock_run:
         await hass.services.async_call(
             LOCK_DOMAIN,
             SERVICE_UNLOCK,
-            {ATTR_ENTITY_ID: ["lock.test_lock"]},
+            {ATTR_ENTITY_ID: [entity_id]},
             blocking=True,
         )
-        assert hass.states.get("lock.test_lock").state == STATE_UNLOCKED
+        assert hass.states.get(entity_id).state == STATE_UNLOCKED
         mock_run.assert_called_once()
