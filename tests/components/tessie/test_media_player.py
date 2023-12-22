90901a250642b24ba21308a3457285f96fcab778
Brett Adams <brett.whynot@gmail.com>
2023-12-22 03:35:22 +0000

    add more assertions

--- tests/components/tessie/test_media_player.py
+++ tests/components/tessie/test_media_player.py
@@ -1,8 +1,21 @@
 """Test the Tessie media player platform."""
-from homeassistant.components.media_player import MediaPlayerState
+from homeassistant.components.media_player import (
+    ATTR_INPUT_SOURCE,
+    ATTR_MEDIA_ALBUM_NAME,
+    ATTR_MEDIA_ARTIST,
+    ATTR_MEDIA_DURATION,
+    ATTR_MEDIA_PLAYLIST,
+    ATTR_MEDIA_POSITION,
+    ATTR_MEDIA_TITLE,
+    MediaPlayerState,
+)
 from homeassistant.core import HomeAssistant
 
-from .common import setup_platform
+from .common import TEST_STATE_OF_ALL_VEHICLES, setup_platform
+
+MEDIA_INFO = TEST_STATE_OF_ALL_VEHICLES["results"][0]["last_state"]["vehicle_state"][
+    "media_info"
+]
 
 
 async def test_sensors(hass: HomeAssistant) -> None:
@@ -14,4 +27,19 @@ async def test_sensors(hass: HomeAssistant) -> None:
 
     assert len(hass.states.async_all("media_player")) == 1
 
-    assert hass.states.get("media_player.test").state == MediaPlayerState.PLAYING
+    state = hass.states.get("media_player.test")
+    assert state.state == MediaPlayerState.PLAYING
+    assert state.attributes["volume_level"] == 2.3333 / 10.333333
+    assert state.attributes[ATTR_MEDIA_TITLE] == MEDIA_INFO["now_playing_title"]
+    assert state.attributes[ATTR_MEDIA_ARTIST] == MEDIA_INFO["now_playing_artist"]
+    assert state.attributes[ATTR_MEDIA_ALBUM_NAME] == MEDIA_INFO["now_playing_album"]
+    assert state.attributes[ATTR_INPUT_SOURCE] == MEDIA_INFO["now_playing_source"]
+    assert state.attributes[ATTR_MEDIA_PLAYLIST] == MEDIA_INFO["now_playing_station"]
+    assert (
+        state.attributes[ATTR_MEDIA_DURATION]
+        == MEDIA_INFO["now_playing_duration"] / 1000
+    )
+    assert (
+        state.attributes[ATTR_MEDIA_POSITION]
+        == MEDIA_INFO["now_playing_elapsed"] / 1000
+    )
