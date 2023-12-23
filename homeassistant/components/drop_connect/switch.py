da25145c1a4eaf6651794f6f4b892e2db128898c
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2023-12-23 19:38:05 +0100

    Update homeassistant/components/drop_connect/switch.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/drop_connect/switch.py
+++ homeassistant/components/drop_connect/switch.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from collections.abc import Callable, Coroutine
+from collections.abc import Awaitable, Callable
 from dataclasses import dataclass
 import logging
 from typing import Any
