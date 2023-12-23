9e83d62345efc745f0004da4682ada7f6f126161
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-23 17:18:42 +0100

    Update test_config_flow.py

--- tests/components/lutron/test_config_flow.py
+++ tests/components/lutron/test_config_flow.py
@@ -1,17 +1,16 @@
 """Test the lutron config flow."""
 from unittest.mock import AsyncMock, patch
+from urllib.error import HTTPError
 
 import pytest
 
-from homeassistant import config_entries
-from homeassistant.components.lutron import config_flow
+from homeassistant.components.lutron.const import DOMAIN
+from homeassistant.config_entries import SOURCE_IMPORT, SOURCE_USER
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
 from tests.common import MockConfigEntry
-
-pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 
 MOCK_DATA_STEP = {
     CONF_HOST: "127.0.0.1",
@@ -20,105 +19,120 @@ MOCK_DATA_STEP = {
 }
 
 
-async def test_flow_user_init_data_success(hass: HomeAssistant) -> None:
+async def test_full_flow(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     """Test success response."""
     result = await hass.config_entries.flow.async_init(
-        config_flow.DOMAIN, context={"source": "user"}
+        DOMAIN, context={"source": SOURCE_USER}
     )
 
-    assert result["type"] == "form"
+    assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "user"
-    assert result["handler"] == "lutron"
-    assert result["data_schema"] == config_flow.DATA_SCHEMA
 
-    with patch(
-        "homeassistant.components.lutron.config_flow.async_step_user",
-        autospec=True,
-        return_value=True,
+    with patch("homeassistant.components.lutron.config_flow.Lutron.load_xml_db"), patch(
+        "homeassistant.components.lutron.config_flow.Lutron.guid", "12345678901"
     ):
-        result = await hass.config_entries.flow.async_init(
-            config_flow.DOMAIN, context={"source": "user"}
-        )
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             user_input=MOCK_DATA_STEP,
         )
 
-        assert result["type"] == "create_entry"
-        assert result["result"].title == "test_start test_destination"
+        assert result["type"] == FlowResultType.CREATE_ENTRY
+        assert result["result"].title == "Lutron"
 
         assert result["data"] == MOCK_DATA_STEP
 
 
-"""
 @pytest.mark.parametrize(
     ("raise_error", "text_error"),
     [
-        (OpendataTransportConnectionError(), "cannot_connect"),
-        (OpendataTransportError(), "bad_config"),
-        (IndexError(), "unknown"),
+        (HTTPError("", 404, "", None, {}), "cannot_connect"),
+        (Exception, "unknown"),
     ],
 )
-"""
-
-
-async def test_flow_user_init_data_unknown_error_and_recover(
-    hass: HomeAssistant, raise_error, text_error
+async def test_flow_failure(
+    hass: HomeAssistant,
+    mock_setup_entry: AsyncMock,
+    raise_error: Exception,
+    text_error: str,
 ) -> None:
     """Test unknown errors."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+
     with patch(
-        "homeassistant.components.lutron.config_flow.async_step_user",
-        autospec=True,
+        "homeassistant.components.lutron.config_flow.Lutron.load_xml_db",
         side_effect=raise_error,
-    ) as mock_OpendataTransport:
-        result = await hass.config_entries.flow.async_init(
-            config_flow.DOMAIN, context={"source": "user"}
-        )
+    ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             user_input=MOCK_DATA_STEP,
         )
 
-        assert result["type"] == "form"
-        assert result["errors"]["base"] == text_error
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {"base": text_error}
 
-        # Recover
-        mock_OpendataTransport.side_effect = None
-        mock_OpendataTransport.return_value = True
-        result = await hass.config_entries.flow.async_init(
-            config_flow.DOMAIN, context={"source": "user"}
-        )
+    with patch("homeassistant.components.lutron.config_flow.Lutron.load_xml_db"), patch(
+        "homeassistant.components.lutron.config_flow.Lutron.guid", "12345678901"
+    ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             user_input=MOCK_DATA_STEP,
         )
 
-        assert result["type"] == "create_entry"
-        assert result["result"].title == "test_start test_destination"
+        assert result["type"] == FlowResultType.CREATE_ENTRY
+        assert result["result"].title == "Lutron"
 
         assert result["data"] == MOCK_DATA_STEP
 
 
-async def test_flow_user_init_data_already_configured(hass: HomeAssistant) -> None:
+async def test_flow_incorrect_guid(
+    hass: HomeAssistant, mock_setup_entry: AsyncMock
+) -> None:
+    """Test configuring flow with incorrect guid."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "user"
+
+    with patch("homeassistant.components.lutron.config_flow.Lutron.load_xml_db"), patch(
+        "homeassistant.components.lutron.config_flow.Lutron.guid", "12345"
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input=MOCK_DATA_STEP,
+        )
+
+        assert result["type"] == FlowResultType.FORM
+        assert result["errors"] == {"base": "missing_guid"}
+
+    with patch("homeassistant.components.lutron.config_flow.Lutron.load_xml_db"), patch(
+        "homeassistant.components.lutron.config_flow.Lutron.guid", "12345678901"
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input=MOCK_DATA_STEP,
+        )
+
+        assert result["type"] == FlowResultType.CREATE_ENTRY
+
+
+async def test_flow_single_instance_allowed(hass: HomeAssistant) -> None:
     """Test we abort user data set when entry is already configured."""
 
-    entry = MockConfigEntry(
-        domain=config_flow.DOMAIN,
-        data=MOCK_DATA_STEP,
-    )
+    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_DATA_STEP, unique_id="12345678901")
     entry.add_to_hass(hass)
 
     result = await hass.config_entries.flow.async_init(
-        config_flow.DOMAIN, context={"source": "user"}
+        DOMAIN, context={"source": SOURCE_USER}
     )
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        user_input=MOCK_DATA_STEP,
-    )
-
     assert result["type"] == FlowResultType.ABORT
-    assert result["reason"] == "already_configured"
+    assert result["reason"] == "single_instance_allowed"
 
 
 MOCK_DATA_IMPORT = {
@@ -133,69 +147,71 @@ async def test_import(
     mock_setup_entry: AsyncMock,
 ) -> None:
     """Test import flow."""
-    with patch(
-        "homeassistant.components.lutron.config_flow.async_step_import",
-        autospec=True,
-        return_value=True,
+    with patch("homeassistant.components.lutron.config_flow.Lutron.load_xml_db"), patch(
+        "homeassistant.components.lutron.config_flow.Lutron.guid", "12345678901"
     ):
         result = await hass.config_entries.flow.async_init(
-            config_flow.DOMAIN,
-            context={"source": config_entries.SOURCE_IMPORT},
-            data=MOCK_DATA_IMPORT,
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=MOCK_DATA_IMPORT
         )
         await hass.async_block_till_done()
 
-        assert result["type"] == FlowResultType.CREATE_ENTRY
-        assert result["data"] == MOCK_DATA_IMPORT
-        assert len(mock_setup_entry.mock_calls) == 1
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    assert result["data"] == MOCK_DATA_IMPORT
+    assert len(mock_setup_entry.mock_calls) == 1
 
 
-"""
 @pytest.mark.parametrize(
-    ("raise_error", "text_error"),
+    ("raise_error", "reason"),
     [
-        (OpendataTransportConnectionError(), "cannot_connect"),
-        (OpendataTransportError(), "bad_config"),
-        (IndexError(), "unknown"),
+        (HTTPError("", 404, "", None, {}), "cannot_connect"),
+        (Exception, "unknown"),
     ],
 )
-"""
-
-
-async def test_import_cannot_connect_error(
-    hass: HomeAssistant, raise_error, text_error
+async def test_import_flow_failure(
+    hass: HomeAssistant, raise_error: Exception, reason: str
 ) -> None:
-    """Test import flow cannot_connect error."""
+    """Test handling errors while importing."""
+
     with patch(
-        "homeassistant.components.lutron.config_flow.async_step_import",
-        autospec=True,
+        "homeassistant.components.lutron.config_flow.Lutron.load_xml_db",
         side_effect=raise_error,
     ):
         result = await hass.config_entries.flow.async_init(
-            config_flow.DOMAIN,
-            context={"source": config_entries.SOURCE_IMPORT},
-            data=MOCK_DATA_IMPORT,
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=MOCK_DATA_IMPORT
         )
-        await hass.async_block_till_done()
 
-        assert result["type"] == FlowResultType.ABORT
-        assert result["reason"] == text_error
+    assert result["type"] == FlowResultType.ABORT
+    assert result["reason"] == reason
+
+
+async def test_import_flow_guid_failure(hass: HomeAssistant) -> None:
+    """Test handling errors while importing."""
+
+    with patch("homeassistant.components.lutron.config_flow.Lutron.load_xml_db"), patch(
+        "homeassistant.components.lutron.config_flow.Lutron.guid", "123"
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=MOCK_DATA_IMPORT
+        )
+
+    assert result["type"] == FlowResultType.ABORT
+    assert result["reason"] == "missing_guid"
 
 
 async def test_import_already_configured(hass: HomeAssistant) -> None:
     """Test we abort import when entry is already configured."""
 
     entry = MockConfigEntry(
-        domain=config_flow.DOMAIN,
-        data=MOCK_DATA_IMPORT,
+        domain=DOMAIN, data=MOCK_DATA_IMPORT, unique_id="12345678901"
     )
     entry.add_to_hass(hass)
 
-    result = await hass.config_entries.flow.async_init(
-        config_flow.DOMAIN,
-        context={"source": config_entries.SOURCE_IMPORT},
-        data=MOCK_DATA_IMPORT,
-    )
+    with patch("homeassistant.components.lutron.config_flow.Lutron.load_xml_db"), patch(
+        "homeassistant.components.lutron.config_flow.Lutron.guid", "12345678901"
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=MOCK_DATA_IMPORT
+        )
 
     assert result["type"] == FlowResultType.ABORT
     assert result["reason"] == "already_configured"
