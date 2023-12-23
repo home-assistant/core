c0dbc2fa16acc4d0ae4733d86f81be4ffa97196e
Brett Adams <Bre77@users.noreply.github.com>
2023-12-23 22:45:06 +1000

    Rework entity description functions in Tessie (#106287)
    * use lamdba to return the library function
    
    * Rename mocks
    
    * lambda number
    
    * Lambda button
    
    * Add missing
    
    * Remove context manager

--- tests/components/tessie/test_button.py
+++ tests/components/tessie/test_button.py
@@ -1,11 +1,11 @@
 """Test the Tessie button platform."""
+from unittest.mock import patch
 
 from homeassistant.components.button import DOMAIN as BUTTON_DOMAIN, SERVICE_PRESS
-from homeassistant.components.tessie.button import DESCRIPTIONS
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
 
-from .common import patch_description, setup_platform
+from .common import setup_platform
 
 
 async def test_buttons(hass: HomeAssistant) -> None:
@@ -14,7 +14,9 @@ async def test_buttons(hass: HomeAssistant) -> None:
     await setup_platform(hass)
 
     # Test wake button
-    with patch_description("wake", "func", DESCRIPTIONS) as mock_wake:
+    with patch(
+        "homeassistant.components.tessie.button.wake",
+    ) as mock_wake:
         await hass.services.async_call(
             BUTTON_DOMAIN,
             SERVICE_PRESS,
