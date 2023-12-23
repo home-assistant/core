541717cbe70b6de9b9889536486da14a45d27094
Oscar Calvo <oscar@calvonet.com>
2023-12-23 19:12:31 +0000

    Remove not needed test

--- tests/components/ccm15/test_climate.py
+++ tests/components/ccm15/test_climate.py
@@ -2,13 +2,11 @@
 from datetime import timedelta
 from unittest.mock import AsyncMock, patch
 
-from ccm15 import CCM15DeviceState, CCM15SlaveDevice
+from ccm15 import CCM15DeviceState
 from freezegun.api import FrozenDateTimeFactory
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.components.ccm15.climate import CCM15Climate
 from homeassistant.components.ccm15.const import DOMAIN
-from homeassistant.components.ccm15.coordinator import CCM15Coordinator
 from homeassistant.components.climate import (
     ATTR_FAN_MODE,
     ATTR_HVAC_MODE,
@@ -20,7 +18,6 @@ from homeassistant.components.climate import (
     SERVICE_SET_TEMPERATURE,
     SERVICE_TURN_ON,
     HVACMode,
-    UnitOfTemperature,
 )
 from homeassistant.const import ATTR_ENTITY_ID, CONF_HOST, CONF_PORT, SERVICE_TURN_OFF
 from homeassistant.core import HomeAssistant
@@ -131,56 +128,3 @@ async def test_climate_state(
 
     assert hass.states.get("climate.midea_0") == snapshot
     assert hass.states.get("climate.midea_1") == snapshot
-
-
-async def test_cmm15_data_isread_correctly(
-    hass: HomeAssistant, mock_setup_entry: AsyncMock
-) -> None:
-    """Test the coordinator."""
-    # Create a dictionary of CCM15SlaveDevice objects
-    ccm15_devices = {
-        0: CCM15SlaveDevice(bytes.fromhex("000000b0b8001b")),
-        1: CCM15SlaveDevice(bytes.fromhex("00000041c0001a")),
-    }
-    # Create an instance of the CCM15DeviceState class
-    device_state = CCM15DeviceState(devices=ccm15_devices)
-    with patch(
-        "ccm15.CCM15Device.CCM15Device.get_status_async",
-        return_value=device_state,
-    ):
-        coordinator = CCM15Coordinator(hass, "1.1.1.1", "80")
-        await coordinator.async_refresh()
-
-    data = coordinator.data
-    devices = []
-    for ac_index in data.devices:
-        devices.append(CCM15Climate(coordinator.get_host(), ac_index, coordinator))
-    assert len(data.devices) == 2
-    assert len(devices) == 2
-    first_climate = list(devices)[0]
-    assert first_climate is not None
-    assert first_climate.available is True
-    assert first_climate.temperature_unit == UnitOfTemperature.CELSIUS
-    assert first_climate.current_temperature == 27
-    assert first_climate.target_temperature == 23
-    assert len(devices) == 2
-    climate = next(iter(devices))
-    assert climate is not None
-    assert climate.coordinator == coordinator
-    assert climate._ac_index == 0
-    assert coordinator.data == data
-    assert climate.unique_id == "1.1.1.1.0"
-
-    # Now test empty return from Network
-    device_state = CCM15DeviceState(devices={})
-    with patch(
-        "ccm15.CCM15Device.CCM15Device.get_status_async",
-        return_value=device_state,
-    ):
-        await coordinator.async_refresh()
-
-    assert first_climate is not None
-    assert first_climate.available is False
-    assert first_climate.hvac_mode is None
-    assert first_climate.current_temperature is None
-    assert first_climate.target_temperature is None
