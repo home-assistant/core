fe1aa82e64f5f02a7c40dfe2538ae3cf83dd04c6
Brett Adams <brett.whynot@gmail.com>
2023-12-23 02:50:22 +0000

    Lambda button

--- homeassistant/components/tessie/button.py
+++ homeassistant/components/tessie/button.py
@@ -35,19 +35,25 @@ class TessieButtonEntityDescription(ButtonEntityDescription):
 DESCRIPTIONS: tuple[TessieButtonEntityDescription, ...] = (
     TessieButtonEntityDescription(key="wake", func=wake, icon="mdi:sleep-off"),
     TessieButtonEntityDescription(
-        key="flash_lights", func=flash_lights, icon="mdi:flashlight"
+        key="flash_lights", func=lambda: flash_lights, icon="mdi:flashlight"
     ),
     TessieButtonEntityDescription(key="honk", func=honk, icon="mdi:bullhorn"),
     TessieButtonEntityDescription(
-        key="trigger_homelink", func=trigger_homelink, icon="mdi:garage"
+        key="trigger_homelink", func=lambda: trigger_homelink, icon="mdi:garage"
     ),
     TessieButtonEntityDescription(
-        key="enable_keyless_driving", func=enable_keyless_driving, icon="mdi:car-key"
+        key="enable_keyless_driving",
+        func=lambda: enable_keyless_driving,
+        icon="mdi:car-key",
     ),
-    TessieButtonEntityDescription(key="boombox", func=boombox, icon="mdi:volume-high"),
-    TessieButtonEntityDescription(key="frunk", func=open_front_trunk, icon="mdi:car"),
     TessieButtonEntityDescription(
-        key="trunk", func=open_close_rear_trunk, icon="mdi:car-back"
+        key="boombox", func=lambda: boombox, icon="mdi:volume-high"
+    ),
+    TessieButtonEntityDescription(
+        key="frunk", func=lambda: open_front_trunk, icon="mdi:car"
+    ),
+    TessieButtonEntityDescription(
+        key="trunk", func=lambda: open_close_rear_trunk, icon="mdi:car-back"
     ),
 )
 
@@ -81,4 +87,4 @@ class TessieButtonEntity(TessieEntity, ButtonEntity):
 
     async def async_press(self) -> None:
         """Press the button."""
-        await self.run(self.entity_description.func)
+        await self.run(self.entity_description.func())
