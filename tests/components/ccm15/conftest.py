893c1a55bef4356d596c45a9081da58b762cc7a5
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-23 15:55:26 +0100

    Update conftest.py

--- tests/components/ccm15/conftest.py
+++ tests/components/ccm15/conftest.py
@@ -2,6 +2,7 @@
 from collections.abc import Generator
 from unittest.mock import AsyncMock, patch
 
+from ccm15 import CCM15DeviceState, CCM15SlaveDevice
 import pytest
 
 
@@ -12,3 +13,18 @@ def mock_setup_entry() -> Generator[AsyncMock, None, None]:
         "homeassistant.components.ccm15.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         yield mock_setup_entry
+
+
+@pytest.fixture
+def ccm15_device() -> Generator[AsyncMock, None, None]:
+    """Mock ccm15 device."""
+    ccm15_devices = {
+        0: CCM15SlaveDevice(bytes.fromhex("000000b0b8001b")),
+        1: CCM15SlaveDevice(bytes.fromhex("00000041c0001a")),
+    }
+    device_state = CCM15DeviceState(devices=ccm15_devices)
+    with patch(
+        "homeassistant.components.ccm15.coordinator.CCM15Device.get_status_async",
+        return_value=device_state,
+    ):
+        yield
