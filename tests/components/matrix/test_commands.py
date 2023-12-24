18272ac503b5080f60f16839e2a0be01cd079560
Paarth Shah <mail@shahpaarth.com>
2023-12-24 01:03:35 -0800

    Test that a command sent by the bot user does not trigger anything

--- tests/components/matrix/test_commands.py
+++ tests/components/matrix/test_commands.py
@@ -13,6 +13,7 @@ from homeassistant.core import Event, HomeAssistant
 from tests.components.matrix.conftest import (
     MOCK_EXPRESSION_COMMANDS,
     MOCK_WORD_COMMANDS,
+    TEST_MXID,
     TEST_ROOM_A_ID,
     TEST_ROOM_B_ID,
     TEST_ROOM_C_ID,
@@ -64,7 +65,7 @@ word_command_global = partial(
         "args": ["arg1", "arg2"],
     },
 )
-expression_command_global = partial(
+expr_command_global = partial(
     CommandTestParameters,
     room_message=room_message_base(body="My name is FakeName"),
     expected_event_data_extra={
@@ -88,26 +89,41 @@ expr_command_subset = partial(
         "args": {"name": "FakeName"},
     },
 )
+# Messages without commands should trigger nothing
 fake_command_global = partial(
     CommandTestParameters,
     room_message=room_message_base(body="This is not a real command!"),
+    expected_event_data_extra=None,
+)
+# Valid commands sent by the bot user should trigger nothing
+self_command_global = partial(
+    CommandTestParameters,
+    room_message=room_message_base(
+        body="!WordTrigger arg1 arg2",
+        source={
+            "event_id": "fake_event_id",
+            "sender": TEST_MXID,
+            "origin_server_ts": 123456789,
+        },
+    ),
     expected_event_data_extra=None,
 )
 
 
 @pytest.mark.parametrize("room_id", [TEST_ROOM_A_ID, TEST_ROOM_B_ID, TEST_ROOM_C_ID])
 @pytest.mark.parametrize(
-    "command_partial",
+    "partial_param",
     [
         word_command_global,
-        expression_command_global,
+        expr_command_global,
         word_command_subset,
         expr_command_subset,
         fake_command_global,
+        self_command_global,
     ],
 )
 async def test_commands(
-    hass: HomeAssistant, matrix_bot: MatrixBot, command_events, command_partial, room_id
+    hass: HomeAssistant, matrix_bot: MatrixBot, command_events, partial_param, room_id
 ):
     """Test that the configured commands were parsed and used correctly."""
     await hass.async_start()
@@ -116,7 +132,7 @@ async def test_commands(
     assert matrix_bot._word_commands == MOCK_WORD_COMMANDS
     assert matrix_bot._expression_commands == MOCK_EXPRESSION_COMMANDS
 
-    command_param: CommandTestParameters = command_partial(room_id=room_id)
+    command_param: CommandTestParameters = partial_param(room_id=room_id)
 
     room = MatrixRoom(room_id=command_param.room_id, own_user_id=matrix_bot._mx_id)
     await matrix_bot._handle_room_message(room, command_param.room_message)
