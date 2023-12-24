4d76d9580efd2e40a5b5bdeba1ce9e63f51db8a9
Hareesh M U <hareesh.mu@gmail.com>
2023-12-24 06:12:37 +0000

    Move constants from const.py to notify.py

--- homeassistant/components/tvoverlay/notify.py
+++ homeassistant/components/tvoverlay/notify.py
@@ -28,31 +28,35 @@ from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-from .const import (
-    ATTR_APP_ICON,
-    ATTR_APP_TITLE,
-    ATTR_BADGE_COLOR,
-    ATTR_BADGE_ICON,
-    ATTR_BG_COLOR,
-    ATTR_BORDER_COLOR,
-    ATTR_DURATION,
-    ATTR_ID,
-    ATTR_IMAGE,
-    ATTR_IMAGE_AUTH,
-    ATTR_IMAGE_ICON,
-    ATTR_IMAGE_PASSWORD,
-    ATTR_IMAGE_PATH,
-    ATTR_IMAGE_URL,
-    ATTR_IMAGE_USERNAME,
-    ATTR_MESSAGE_COLOR,
-    ATTR_PERSISTENT,
-    ATTR_POSITION,
-    ATTR_SHAPE,
-    ATTR_SOURCE_NAME,
-    ATTR_VIDEO,
-    ATTR_VISIBLE,
-    DEFAULT_DURATION,
-)
+DEFAULT_DURATION = "5"
+
+# Parameters for Notifications
+ATTR_ID = "id"
+ATTR_APP_TITLE = "app_title"
+ATTR_SOURCE_NAME = "source_name"
+ATTR_APP_ICON = "app_icon"
+ATTR_BADGE_ICON = "badge_icon"
+ATTR_BADGE_COLOR = "badge_color"
+ATTR_POSITION = "position"
+ATTR_DURATION = "duration"
+ATTR_IMAGE = "image"
+ATTR_VIDEO = "video"
+
+# Attributes for image and app icon
+ATTR_IMAGE_URL = "url"
+ATTR_IMAGE_PATH = "path"
+ATTR_IMAGE_ICON = "mdi_icon"
+ATTR_IMAGE_USERNAME = "username"
+ATTR_IMAGE_PASSWORD = "password"
+ATTR_IMAGE_AUTH = "auth"
+
+# Additional Parameters for Persistent Notifications
+ATTR_PERSISTENT = "is_persistent"
+ATTR_MESSAGE_COLOR = "message_color"
+ATTR_BORDER_COLOR = "border_color"
+ATTR_BG_COLOR = "bg_color"
+ATTR_SHAPE = "shape"
+ATTR_VISIBLE = "visible"
 
 _LOGGER = logging.getLogger(__name__)
 
