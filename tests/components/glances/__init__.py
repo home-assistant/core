52b174b792b3ba905cd4b279a884a1a752b225e6
Rami Mousleh <engrbm87@gmail.com>
2023-12-26 10:24:49 +0000

    Fix glances test_sensor.py

--- tests/components/glances/__init__.py
+++ tests/components/glances/__init__.py
@@ -181,8 +181,8 @@ HA_SENSOR_DATA: dict[str, Any] = {
     },
     "sensors": {
         "cpu_thermal 1": {"temperature_core": 59},
-        "err_temp": {"temperature_hdd": "Unavailable"},
-        "na_temp": {"temperature_hdd": "Unavailable"},
+        "err_temp": {"temperature_hdd": "unavailable"},
+        "na_temp": {"temperature_hdd": "unavailable"},
     },
     "mem": {
         "memory_use_percent": 27.6,
