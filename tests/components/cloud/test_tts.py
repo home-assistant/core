4ab91a7cd8ffe4e8f2b3b75c170c9bd55c5390b8
Martin Hjelmare <marhje52@gmail.com>
2023-12-26 15:53:27 +0100

    Update test_prefs_default_voice

--- tests/components/cloud/test_tts.py
+++ tests/components/cloud/test_tts.py
@@ -1,12 +1,17 @@
 """Tests for cloud tts."""
-from unittest.mock import Mock
+from collections.abc import Callable, Coroutine
+from typing import Any
+from unittest.mock import MagicMock, Mock
 
 from hass_nabucasa import voice
 import pytest
 import voluptuous as vol
 
-from homeassistant.components.cloud import const, tts
+from homeassistant.components.cloud import DOMAIN, const, tts
+from homeassistant.components.tts import DOMAIN as TTS_DOMAIN
+from homeassistant.components.tts.helper import get_engine_instance
 from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
 
 
 @pytest.fixture
@@ -42,34 +47,54 @@ def test_schema() -> None:
     tts.PLATFORM_SCHEMA({"platform": "cloud"})
 
 
+@pytest.mark.parametrize(
+    ("engine_id", "platform_config"),
+    [
+        (
+            DOMAIN,
+            None,
+        ),
+        (
+            DOMAIN,
+            {
+                "platform": DOMAIN,
+                "service_name": "yaml",
+                "language": "fr-FR",
+                "gender": "female",
+            },
+        ),
+    ],
+)
 async def test_prefs_default_voice(
-    hass: HomeAssistant, cloud_with_prefs, cloud_prefs
+    hass: HomeAssistant,
+    cloud: MagicMock,
+    set_cloud_prefs: Callable[[dict[str, Any]], Coroutine[Any, Any, None]],
+    engine_id: str,
+    platform_config: dict[str, Any] | None,
 ) -> None:
     """Test cloud provider uses the preferences."""
-    assert cloud_prefs.tts_default_voice == ("en-US", "female")
-
-    tts_info = {"platform_loaded": Mock()}
-    provider_pref = await tts.async_get_engine(
-        Mock(data={const.DOMAIN: cloud_with_prefs}), None, tts_info
-    )
-    provider_conf = await tts.async_get_engine(
-        Mock(data={const.DOMAIN: cloud_with_prefs}),
-        {"language": "fr-FR", "gender": "female"},
-        None,
-    )
-
-    assert provider_pref.default_language == "en-US"
-    assert provider_pref.default_options == {"gender": "female", "audio_output": "mp3"}
-    assert provider_conf.default_language == "fr-FR"
-    assert provider_conf.default_options == {"gender": "female", "audio_output": "mp3"}
-
-    await cloud_prefs.async_update(tts_default_voice=("nl-NL", "male"))
+    assert await async_setup_component(hass, "homeassistant", {})
+    assert await async_setup_component(hass, TTS_DOMAIN, {TTS_DOMAIN: platform_config})
+    assert await async_setup_component(hass, DOMAIN, {DOMAIN: {}})
     await hass.async_block_till_done()
 
-    assert provider_pref.default_language == "nl-NL"
-    assert provider_pref.default_options == {"gender": "male", "audio_output": "mp3"}
-    assert provider_conf.default_language == "fr-FR"
-    assert provider_conf.default_options == {"gender": "female", "audio_output": "mp3"}
+    assert cloud.client.prefs.tts_default_voice == ("en-US", "female")
+
+    on_start_callback = cloud.register_on_start.call_args[0][0]
+    await on_start_callback()
+
+    engine = get_engine_instance(hass, engine_id)
+
+    assert engine is not None
+    # The platform config provider will be overridden by the discovery info provider.
+    assert engine.default_language == "en-US"
+    assert engine.default_options == {"gender": "female", "audio_output": "mp3"}
+
+    await set_cloud_prefs({"tts_default_voice": ("nl-NL", "male")})
+    await hass.async_block_till_done()
+
+    assert engine.default_language == "nl-NL"
+    assert engine.default_options == {"gender": "male", "audio_output": "mp3"}
 
 
 async def test_provider_properties(cloud_with_prefs) -> None:
