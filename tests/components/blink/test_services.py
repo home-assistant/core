937d0ebd37bd7de01733f504efb97261f9754963
Mike Kasper <mike.j.kasper@gmail.com>
2023-12-27 17:12:40 +0000

    Finish rollback on update service

--- tests/components/blink/test_services.py
+++ tests/components/blink/test_services.py
@@ -10,9 +10,10 @@ from homeassistant.components.blink.const import (
     SERVICE_SEND_PIN,
 )
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import CONF_PIN
+from homeassistant.const import ATTR_DEVICE_ID, CONF_PIN
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers import device_registry as dr
 
 from tests.common import MockConfigEntry
 
@@ -23,6 +24,7 @@ PIN = "1234"
 
 async def test_refresh_service_calls(
     hass: HomeAssistant,
+    device_registry: dr.DeviceRegistry,
     mock_blink_api: MagicMock,
     mock_blink_auth_api: MagicMock,
     mock_config_entry: MockConfigEntry,
@@ -33,13 +35,16 @@ async def test_refresh_service_calls(
     assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
+    device_entry = device_registry.async_get_device(identifiers={(DOMAIN, "12345")})
+    assert device_entry
+
     assert mock_config_entry.state is ConfigEntryState.LOADED
     assert mock_blink_api.refresh.call_count == 1
 
     await hass.services.async_call(
         DOMAIN,
         SERVICE_REFRESH,
-        {ATTR_CONFIG_ENTRY_ID: [mock_config_entry.entry_id]},
+        {ATTR_DEVICE_ID: [device_entry.id]},
         blocking=True,
     )
 
@@ -49,7 +54,7 @@ async def test_refresh_service_calls(
         await hass.services.async_call(
             DOMAIN,
             SERVICE_REFRESH,
-            {ATTR_CONFIG_ENTRY_ID: ["bad-config_id"]},
+            {ATTR_DEVICE_ID: ["bad-device_id"]},
             blocking=True,
         )
 
@@ -86,20 +91,11 @@ async def test_pin_service_calls(
         )
 
 
-@pytest.mark.parametrize(
-    ("service", "params"),
-    [
-        (SERVICE_SEND_PIN, {CONF_PIN: PIN}),
-        (SERVICE_REFRESH, {}),
-    ],
-)
-async def test_service_called_with_non_blink_device(
+async def test_service_pin_called_with_non_blink_device(
     hass: HomeAssistant,
     mock_blink_api: MagicMock,
     mock_blink_auth_api: MagicMock,
     mock_config_entry: MockConfigEntry,
-    service,
-    params,
 ) -> None:
     """Test service calls with non blink device."""
 
@@ -117,32 +113,66 @@ async def test_service_called_with_non_blink_device(
     hass.config.is_allowed_path = Mock(return_value=True)
     mock_blink_api.cameras = {CAMERA_NAME: AsyncMock()}
 
-    parameters = {ATTR_CONFIG_ENTRY_ID: [other_mock_config_entry.entry_id]}
-    parameters.update(params)
+    parameters = {
+        ATTR_CONFIG_ENTRY_ID: [other_mock_config_entry.entry_id],
+        CONF_PIN: PIN,
+    }
 
     with pytest.raises(HomeAssistantError):
         await hass.services.async_call(
             DOMAIN,
-            service,
+            SERVICE_SEND_PIN,
             parameters,
             blocking=True,
         )
 
 
-@pytest.mark.parametrize(
-    ("service", "params"),
-    [
-        (SERVICE_SEND_PIN, {CONF_PIN: PIN}),
-        (SERVICE_REFRESH, {}),
-    ],
-)
-async def test_service_called_with_unloaded_entry(
+async def test_service_update_called_with_non_blink_device(
+    hass: HomeAssistant,
+    mock_blink_api: MagicMock,
+    device_registry: dr.DeviceRegistry,
+    mock_blink_auth_api: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test service calls with non blink device."""
+
+    mock_config_entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    other_domain = "NotBlink"
+    other_config_id = "555"
+    other_mock_config_entry = MockConfigEntry(
+        title="Not Blink", domain=other_domain, entry_id=other_config_id
+    )
+    await hass.config_entries.async_add(other_mock_config_entry)
+
+    device_entry = device_registry.async_get_or_create(
+        config_entry_id=other_config_id,
+        identifiers={
+            (other_domain, 1),
+        },
+    )
+
+    hass.config.is_allowed_path = Mock(return_value=True)
+    mock_blink_api.cameras = {CAMERA_NAME: AsyncMock()}
+
+    parameters = {ATTR_DEVICE_ID: [device_entry.id]}
+
+    with pytest.raises(HomeAssistantError):
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_REFRESH,
+            parameters,
+            blocking=True,
+        )
+
+
+async def test_service_pin_called_with_unloaded_entry(
     hass: HomeAssistant,
     mock_blink_api: MagicMock,
     mock_blink_auth_api: MagicMock,
     mock_config_entry: MockConfigEntry,
-    service,
-    params,
 ) -> None:
     """Test service calls with not ready config entry."""
 
@@ -153,13 +183,43 @@ async def test_service_called_with_unloaded_entry(
     hass.config.is_allowed_path = Mock(return_value=True)
     mock_blink_api.cameras = {CAMERA_NAME: AsyncMock()}
 
-    parameters = {ATTR_CONFIG_ENTRY_ID: [mock_config_entry.entry_id]}
-    parameters.update(params)
+    parameters = {ATTR_CONFIG_ENTRY_ID: [mock_config_entry.entry_id], CONF_PIN: PIN}
 
     with pytest.raises(HomeAssistantError):
         await hass.services.async_call(
             DOMAIN,
-            service,
+            SERVICE_SEND_PIN,
+            parameters,
+            blocking=True,
+        )
+
+
+async def test_service_update_called_with_unloaded_entry(
+    hass: HomeAssistant,
+    device_registry: dr.DeviceRegistry,
+    mock_blink_api: MagicMock,
+    mock_blink_auth_api: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test service calls with not ready config entry."""
+
+    mock_config_entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    mock_config_entry.state = ConfigEntryState.SETUP_ERROR
+    hass.config.is_allowed_path = Mock(return_value=True)
+    mock_blink_api.cameras = {CAMERA_NAME: AsyncMock()}
+
+    device_entry = device_registry.async_get_device(identifiers={(DOMAIN, "12345")})
+    assert device_entry
+
+    parameters = {ATTR_DEVICE_ID: [device_entry.id]}
+
+    with pytest.raises(HomeAssistantError):
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_REFRESH,
             parameters,
             blocking=True,
         )
