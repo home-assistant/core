68ac4717dcf668aede91566dafc6ef60aedc6a91
Franck Nijhof <git@frenck.dev>
2023-12-27 10:22:37 +0100

    Revert "Raise HomeAssistantError if event is triggered with invalid event_type" (#106458)

--- homeassistant/components/mqtt/event.py
+++ homeassistant/components/mqtt/event.py
@@ -16,7 +16,6 @@ from homeassistant.components.event import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_DEVICE_CLASS, CONF_NAME, CONF_VALUE_TEMPLATE
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.exceptions import HomeAssistantError
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType
@@ -175,7 +174,7 @@ class MqttEvent(MqttEntity, EventEntity):
                 return
             try:
                 self._trigger_event(event_type, event_attributes)
-            except HomeAssistantError:
+            except ValueError:
                 _LOGGER.warning(
                     "Invalid event type %s for %s received on topic %s, payload %s",
                     event_type,
