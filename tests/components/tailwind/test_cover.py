b29cc1d1367fae8f291a3e5f61b6766eb9f6ba10
Franck Nijhof <git@frenck.dev>
2023-12-27 10:19:31 +0100

    Add error handling to Tailwind service methods

--- tests/components/tailwind/test_cover.py
+++ tests/components/tailwind/test_cover.py
@@ -1,7 +1,12 @@
 """Tests for cover entities provided by the Tailwind integration."""
 from unittest.mock import ANY, MagicMock
 
-from gotailwind import TailwindDoorOperationCommand
+from gotailwind import (
+    TailwindDoorDisabledError,
+    TailwindDoorLockedOutError,
+    TailwindDoorOperationCommand,
+    TailwindError,
+)
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
@@ -10,8 +15,10 @@ from homeassistant.components.cover import (
     SERVICE_CLOSE_COVER,
     SERVICE_OPEN_COVER,
 )
+from homeassistant.components.tailwind.const import DOMAIN
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 pytestmark = pytest.mark.usefixtures("init_integration")
@@ -74,3 +81,90 @@ async def test_cover_operations(
     mock_tailwind.operate.assert_called_with(
         door=ANY, operation=TailwindDoorOperationCommand.CLOSE
     )
+
+    # Test door disabled error handling
+    mock_tailwind.operate.side_effect = TailwindDoorDisabledError("Door disabled")
+
+    with pytest.raises(HomeAssistantError, match="Door disabled") as excinfo:
+        await hass.services.async_call(
+            COVER_DOMAIN,
+            SERVICE_OPEN_COVER,
+            {
+                ATTR_ENTITY_ID: "cover.door_1",
+            },
+            blocking=True,
+        )
+
+    assert excinfo.value.translation_domain == DOMAIN
+    assert excinfo.value.translation_key == "door_disabled"
+
+    with pytest.raises(HomeAssistantError, match="Door disabled") as excinfo:
+        await hass.services.async_call(
+            COVER_DOMAIN,
+            SERVICE_CLOSE_COVER,
+            {
+                ATTR_ENTITY_ID: "cover.door_1",
+            },
+            blocking=True,
+        )
+
+    assert excinfo.value.translation_domain == DOMAIN
+    assert excinfo.value.translation_key == "door_disabled"
+
+    # Test door locked out error handling
+    mock_tailwind.operate.side_effect = TailwindDoorLockedOutError("Door locked out")
+
+    with pytest.raises(HomeAssistantError, match="Door locked out") as excinfo:
+        await hass.services.async_call(
+            COVER_DOMAIN,
+            SERVICE_OPEN_COVER,
+            {
+                ATTR_ENTITY_ID: "cover.door_1",
+            },
+            blocking=True,
+        )
+
+    assert excinfo.value.translation_domain == DOMAIN
+    assert excinfo.value.translation_key == "door_locked_out"
+
+    with pytest.raises(HomeAssistantError, match="Door locked out") as excinfo:
+        await hass.services.async_call(
+            COVER_DOMAIN,
+            SERVICE_CLOSE_COVER,
+            {
+                ATTR_ENTITY_ID: "cover.door_1",
+            },
+            blocking=True,
+        )
+
+    assert excinfo.value.translation_domain == DOMAIN
+    assert excinfo.value.translation_key == "door_locked_out"
+
+    # Test door error handling
+    mock_tailwind.operate.side_effect = TailwindError("Some error")
+
+    with pytest.raises(HomeAssistantError, match="Some error") as excinfo:
+        await hass.services.async_call(
+            COVER_DOMAIN,
+            SERVICE_OPEN_COVER,
+            {
+                ATTR_ENTITY_ID: "cover.door_1",
+            },
+            blocking=True,
+        )
+
+    assert excinfo.value.translation_domain == DOMAIN
+    assert excinfo.value.translation_key == "communication_error"
+
+    with pytest.raises(HomeAssistantError, match="Some error") as excinfo:
+        await hass.services.async_call(
+            COVER_DOMAIN,
+            SERVICE_CLOSE_COVER,
+            {
+                ATTR_ENTITY_ID: "cover.door_1",
+            },
+            blocking=True,
+        )
+
+    assert excinfo.value.translation_domain == DOMAIN
+    assert excinfo.value.translation_key == "communication_error"
