d33ad57dd3d4ff32af6982eebd38d1e6936c3960
Joe Neuman <finder39@gmail.com>
2023-12-27 00:48:52 -0800

    Add qBittorrent torrent sensors (#105781)
    * Upgrade QBittorrent integration to show torrents
    
    This brings the QBittorrent integration to be more in line with the Transmission integration. It updates how the integration is written, along with adding sensors for Active Torrents, Inactive Torrents, Paused Torrents, Total Torrents, Seeding Torrents, Started Torrents.
    
    * Remove unused stuff
    
    * Fix codeowners
    
    * Correct name in comments
    
    * Update __init__.py
    
    * Make get torrents a service with a response
    
    * Update sensor.py
    
    * Update sensor.py
    
    * Update sensor.py
    
    * Add new sensors
    
    * remove service
    
    * more removes
    
    * more
    
    * Address comments
    
    * cleanup
    
    * Update coordinator.py
    
    * Fix most lint issues
    
    * Update sensor.py
    
    * Update sensor.py
    
    * Update manifest.json
    
    * Update sensor class
    
    * Update sensor.py
    
    * Fix lint issue with sensor class
    
    * Adding codeowners
    
    * Update homeassistant/components/qbittorrent/__init__.py
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/qbittorrent/const.py
+++ homeassistant/components/qbittorrent/const.py
@@ -5,3 +5,7 @@ DOMAIN: Final = "qbittorrent"
 
 DEFAULT_NAME = "qBittorrent"
 DEFAULT_URL = "http://127.0.0.1:8080"
+
+STATE_UP_DOWN = "up_down"
+STATE_SEEDING = "seeding"
+STATE_DOWNLOADING = "downloading"
