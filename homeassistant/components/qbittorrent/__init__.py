cb4dfe1b6b9c05b551966667e4e40e65da6ea63c
Joost Lekkerkerker <joostlek@outlook.com>
2023-12-27 09:43:59 +0100

    Update homeassistant/components/qbittorrent/__init__.py

--- homeassistant/components/qbittorrent/__init__.py
+++ homeassistant/components/qbittorrent/__init__.py
@@ -44,7 +44,6 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
 
-    hass.data[DOMAIN][config_entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     return True
