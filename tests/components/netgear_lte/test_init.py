fcbf3264fddd809145894123b7b6a443bf183f88
Robert Hillis <tkdrob4390@yahoo.com>
2023-12-28 11:08:57 -0500

    Add device info to Netgear LTE

--- tests/components/netgear_lte/test_init.py
+++ tests/components/netgear_lte/test_init.py
@@ -2,6 +2,7 @@
 from homeassistant.components.netgear_lte.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 
 from .conftest import CONF_DATA
 
@@ -26,3 +27,19 @@ async def test_async_setup_entry_not_ready(
     entry = hass.config_entries.async_entries(DOMAIN)[0]
     assert len(hass.config_entries.async_entries(DOMAIN)) == 1
     assert entry.state == ConfigEntryState.SETUP_RETRY
+
+
+async def test_device(hass: HomeAssistant, setup_integration: None) -> None:
+    """Test device info."""
+    entry = hass.config_entries.async_entries(DOMAIN)[0]
+    device_registry = dr.async_get(hass)
+    await hass.async_block_till_done()
+    device = device_registry.async_get_device(identifiers={(DOMAIN, entry.unique_id)})
+
+    assert device.configuration_url == "http://192.168.5.1"
+    assert device.manufacturer == "Netgear"
+    assert device.model == "LM1200"
+    assert device.name == "Netgear LM1200"
+    assert device.serial_number == "FFFFFFFFFFFFF"
+    assert device.sw_version == "EC25AFFDR07A09M4G"
+    assert device.hw_version == "1.0"
