76d51b86b8dd5616c30b45f1f8ecc9edd6ba480a
Rami Mousleh <engrbm87@gmail.com>
2023-12-28 08:55:18 +0000

    Auto detect api version

--- homeassistant/components/glances/__init__.py
+++ homeassistant/components/glances/__init__.py
@@ -1,11 +1,23 @@
 """The Glances component."""
+import logging
 from typing import Any
 
 from glances_api import Glances
+from glances_api.exceptions import (
+    GlancesApiAuthorizationError,
+    GlancesApiError,
+    GlancesApiNoDataAvailable,
+)
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_NAME, CONF_VERIFY_SSL, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import (
+    ConfigEntryAuthFailed,
+    ConfigEntryError,
+    ConfigEntryNotReady,
+    HomeAssistantError,
+)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.httpx_client import get_async_client
 from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
@@ -17,26 +29,25 @@ PLATFORMS = [Platform.SENSOR]
 
 CONFIG_SCHEMA = cv.removed(DOMAIN, raise_if_present=False)
 
+_LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up Glances from config entry."""
-    api = get_api(hass, dict(config_entry.data))
+    try:
+        api = await get_api(hass, dict(config_entry.data))
+    except GlancesApiAuthorizationError as err:
+        raise ConfigEntryAuthFailed from err
+    except GlancesApiError as err:
+        raise ConfigEntryNotReady from err
+    except UnknownError as err:
+        raise ConfigEntryError(err) from err
     coordinator = GlancesDataUpdateCoordinator(hass, config_entry, api)
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
-
-    if config_entry.data[CONF_VERSION] == 2:
-        async_create_issue(
-            hass,
-            DOMAIN,
-            "deprecated_version",
-            is_fixable=False,
-            severity=IssueSeverity.WARNING,
-            translation_key="deprecated_version",
-        )
 
     return True
 
@@ -50,8 +61,31 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return unload_ok
 
 
-def get_api(hass: HomeAssistant, entry_data: dict[str, Any]) -> Glances:
+async def get_api(hass: HomeAssistant, entry_data: dict[str, Any]) -> Glances:
     """Return the api from glances_api."""
     entry_data.pop(CONF_NAME, None)
+    entry_data.pop(CONF_VERSION, None)
+
     httpx_client = get_async_client(hass, verify_ssl=entry_data[CONF_VERIFY_SSL])
-    return Glances(httpx_client=httpx_client, **entry_data)
+    for version in (3, 2):
+        api = Glances(httpx_client=httpx_client, version=version, **entry_data)
+        try:
+            await api.get_ha_sensor_data()
+            _LOGGER.debug("Connected to Glances API v%s", version)
+            if version == 2:
+                async_create_issue(
+                    hass,
+                    DOMAIN,
+                    "deprecated_version",
+                    is_fixable=False,
+                    severity=IssueSeverity.WARNING,
+                    translation_key="deprecated_version",
+                )
+            return api
+        except GlancesApiNoDataAvailable as err:
+            _LOGGER.debug("Failed to connect to Glances API v%s: %s", version, err)
+    raise UnknownError("Could not connect to Glances API")
+
+
+class UnknownError(HomeAssistantError):
+    """Raise exception if we fail to connect to Glances API."""
--- tests/components/glances/test_init.py
+++ tests/components/glances/test_init.py
@@ -1,27 +1,25 @@
 """Tests for Glances integration."""
-from unittest.mock import MagicMock
+from unittest.mock import AsyncMock, MagicMock
 
 from glances_api.exceptions import (
     GlancesApiAuthorizationError,
     GlancesApiConnectionError,
+    GlancesApiNoDataAvailable,
 )
 import pytest
 
 from homeassistant.components.glances.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.issue_registry import (
-    IssueSeverity,
-    async_get as async_get_issue_registry,
-)
+from homeassistant.helpers import issue_registry as ir
 
-from . import MOCK_USER_INPUT
+from . import HA_SENSOR_DATA, MOCK_USER_INPUT
 
 from tests.common import MockConfigEntry
 
 
 async def test_successful_config_entry(hass: HomeAssistant) -> None:
-    """Test that Glances is configu red successfully."""
+    """Test that Glances is configured successfully."""
 
     entry = MockConfigEntry(domain=DOMAIN, data=MOCK_USER_INPUT)
     entry.add_to_hass(hass)
@@ -31,25 +29,26 @@ async def test_successful_config_entry(hass: HomeAssistant) -> None:
     assert entry.state == ConfigEntryState.LOADED
 
 
-async def test_entry_deprecated_version(hass: HomeAssistant) -> None:
+async def test_entry_deprecated_version(
+    hass: HomeAssistant, issue_registry: ir.IssueRegistry, mock_api: AsyncMock
+) -> None:
     """Test creating an issue if glances server is version 2."""
-    registry = async_get_issue_registry(hass)
-    issue = registry.async_get_issue(DOMAIN, "deprecated_version")
-    assert issue is None
-
-    v2_config_entry = MOCK_USER_INPUT.copy()
-    v2_config_entry["version"] = 2
-
-    entry = MockConfigEntry(domain=DOMAIN, data=v2_config_entry)
+    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_USER_INPUT)
     entry.add_to_hass(hass)
+
+    mock_api.return_value.get_ha_sensor_data.side_effect = [
+        GlancesApiNoDataAvailable("endpoint: 'all' is not valid"),
+        HA_SENSOR_DATA,
+        HA_SENSOR_DATA,
+    ]
 
     await hass.config_entries.async_setup(entry.entry_id)
 
     assert entry.state == ConfigEntryState.LOADED
 
-    issue = registry.async_get_issue(DOMAIN, "deprecated_version")
+    issue = issue_registry.async_get_issue(DOMAIN, "deprecated_version")
     assert issue is not None
-    assert issue.severity == IssueSeverity.WARNING
+    assert issue.severity == ir.IssueSeverity.WARNING
 
 
 @pytest.mark.parametrize(
@@ -57,6 +56,7 @@ async def test_entry_deprecated_version(hass: HomeAssistant) -> None:
     [
         (GlancesApiAuthorizationError, ConfigEntryState.SETUP_ERROR),
         (GlancesApiConnectionError, ConfigEntryState.SETUP_RETRY),
+        (GlancesApiNoDataAvailable, ConfigEntryState.SETUP_ERROR),
     ],
 )
 async def test_setup_error(
