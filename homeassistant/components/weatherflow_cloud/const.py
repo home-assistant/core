a640dc9daa0e301663ae8047ed31f30576dd288b
Jeef <jeeftor@users.noreply.github.com>
2023-12-29 08:56:00 -0700

    Update homeassistant/components/weatherflow_cloud/const.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/weatherflow_cloud/const.py
+++ homeassistant/components/weatherflow_cloud/const.py
@@ -2,7 +2,7 @@
 import logging
 
 DOMAIN = "weatherflow_cloud"
-_LOGGER = logging.getLogger(__package__)
+LOGGER = logging.getLogger(__package__)
 
 ATTR_ATTRIBUTION = "Weather data delivered by WeatherFlow"
 MANUFACTURER = "WeatherFlow"
