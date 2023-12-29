573141b66c1f224b36e484d667d1b7212446765e
Paul Daumlechner <paul.daumlechner@live.de>
2023-12-29 13:55:34 +0000

    Remove device_info

--- homeassistant/components/velux/__init__.py
+++ homeassistant/components/velux/__init__.py
@@ -9,7 +9,6 @@ from homeassistant.const import CONF_HOST, CONF_PASSWORD, Platform
 from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.exceptions import ConfigEntryNotReady
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 from homeassistant.helpers.typing import ConfigType
@@ -124,6 +123,10 @@ class VeluxEntity(Entity):
     def __init__(self, node: Node) -> None:
         """Initialize the Velux device."""
         self.node: Node = node
+        self._attr_unique_id = (
+            node.serial_number if node.serial_number else str(node.node_id)
+        )
+        self._attr_name = node.name if node.name else f"#{node.node_id}"
 
     async def after_update_callback(self, node: Node) -> None:
         """Call after device was updated."""
@@ -132,34 +135,3 @@ class VeluxEntity(Entity):
     async def async_added_to_hass(self) -> None:
         """Store register state change callback."""
         self.node.register_device_updated_cb(self.after_update_callback)
-
-    @property
-    def unique_id(self) -> str:
-        """Return the unique ID of this entity."""
-        # Some devices from other vendors does not provide a serial_number
-        # Node_id is used instead, which is unique within velux component
-        if self.node.serial_number is None:
-            unique_id = str(self.node.node_id)
-        else:
-            unique_id = self.node.serial_number
-        return unique_id
-
-    @property
-    def name(self) -> str:
-        """Return the name of the entity."""
-        if not self.node.name:
-            return "#" + str(self.node.node_id)
-        return self.node.name
-
-    @property
-    def should_poll(self) -> bool:
-        """No polling needed within Velux."""
-        return False
-
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return specific device attributes."""
-        return {
-            "identifiers": {(DOMAIN, str(self.node.node_id))},
-            "name": self.name,
-        }
