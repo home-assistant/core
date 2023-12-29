368176d783c92d38254d2ec0045c41338ae73f93
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-12-29 13:05:56 +0100

    add reauth

--- homeassistant/components/tedee/config_flow.py
+++ homeassistant/components/tedee/config_flow.py
@@ -8,11 +8,13 @@ from pytedee_async import (
     TedeeClientException,
     TedeeLocalAuthException,
 )
+from pytedee_async.bridge import TedeeBridge
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigEntry, ConfigFlow
+from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import CONF_HOST
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.exceptions import HomeAssistantError
 
 from .const import CONF_LOCAL_ACCESS_TOKEN, DOMAIN, NAME
 
@@ -20,7 +22,19 @@ from .const import CONF_LOCAL_ACCESS_TOKEN, DOMAIN, NAME
 class TedeeConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Tedee."""
 
-    reauth_entry: ConfigEntry | None = None
+    _config: dict[str, Any] = {}
+
+    async def get_tedee_bridge(self, user_input: dict[str, Any]) -> TedeeBridge:
+        """Validate the user input allows us to connect."""
+        host = user_input[CONF_HOST]
+        local_access_token = user_input[CONF_LOCAL_ACCESS_TOKEN]
+        tedee_client = TedeeClient(local_token=local_access_token, local_ip=host)
+        try:
+            return await tedee_client.get_local_bridge()
+        except (TedeeAuthException, TedeeLocalAuthException):
+            raise InvalidAuth
+        except TedeeClientException:
+            raise CannotConnect
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -29,16 +43,12 @@ class TedeeConfigFlow(ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
 
         if user_input is not None:
-            host = user_input[CONF_HOST]
-            local_access_token = user_input[CONF_LOCAL_ACCESS_TOKEN]
-            tedee_client = TedeeClient(local_token=local_access_token, local_ip=host)
             try:
-                local_bridge = await tedee_client.get_local_bridge()
-            except (TedeeAuthException, TedeeLocalAuthException):
-                errors[CONF_LOCAL_ACCESS_TOKEN] = "invalid_api_key"
-            except TedeeClientException:
+                local_bridge = await self.get_tedee_bridge(user_input)
+            except CannotConnect:
                 errors[CONF_HOST] = "invalid_host"
-
+            except InvalidAuth:
+                errors[CONF_LOCAL_ACCESS_TOKEN] = "invalid_api_key"
             else:
                 await self.async_set_unique_id(local_bridge.serial)
                 self._abort_if_unique_id_configured()
@@ -57,15 +67,48 @@ class TedeeConfigFlow(ConfigFlow, domain=DOMAIN):
 
     async def async_step_reauth(self, entry_data: Mapping[str, Any]) -> FlowResult:
         """Perform reauth upon an API authentication error."""
-        self.reauth_entry = self.hass.config_entries.async_get_entry(
-            self.context["entry_id"]
-        )
+        self._config = dict(entry_data)
         return await self.async_step_reauth_confirm()
 
     async def async_step_reauth_confirm(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
-        """Confirm reauth dialog."""
-        if user_input is None:
-            return self.async_show_form(step_id="reauth_confirm")
-        return await self.async_step_user()
+        """Dialog that informs the user that reauth is required."""
+        errors = {}
+
+        if user_input is not None:
+            try:
+                await self.get_tedee_bridge(self._config | user_input)
+            except InvalidAuth:
+                errors[CONF_LOCAL_ACCESS_TOKEN] = "invalid_api_key"
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            if not errors:
+                entry = self.hass.config_entries.async_get_entry(
+                    self.context["entry_id"]
+                )
+                self._config |= user_input
+                self.hass.config_entries.async_update_entry(entry, data=self._config)  # type: ignore[arg-type]
+                await self.hass.config_entries.async_reload(self.context["entry_id"])
+                return self.async_abort(reason="reauth_successful")
+
+        return self.async_show_form(
+            step_id="reauth_confirm",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_LOCAL_ACCESS_TOKEN,
+                        default=self._config.get(CONF_LOCAL_ACCESS_TOKEN),
+                    ): str
+                }
+            ),
+            errors=errors,
+        )
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(HomeAssistantError):
+    """Error to indicate there is invalid auth."""
--- tests/components/tedee/test_config_flow.py
+++ tests/components/tedee/test_config_flow.py
@@ -4,8 +4,8 @@ from unittest.mock import MagicMock
 from pytedee_async import TedeeClientException, TedeeLocalAuthException
 import pytest
 
-from homeassistant import config_entries
 from homeassistant.components.tedee.const import CONF_LOCAL_ACCESS_TOKEN, DOMAIN
+from homeassistant.config_entries import SOURCE_REAUTH, SOURCE_USER
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
@@ -19,7 +19,7 @@ LOCAL_ACCESS_TOKEN = "api_token"
 async def test_flow(hass: HomeAssistant, mock_tedee: MagicMock) -> None:
     """Test config flow with one bridge."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": SOURCE_USER}
     )
     await hass.async_block_till_done()
     assert result["type"] == FlowResultType.FORM
@@ -48,7 +48,7 @@ async def test_flow_already_configured(
     mock_config_entry.add_to_hass(hass)
 
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": SOURCE_USER}
     )
     await hass.async_block_till_done()
     assert result["type"] == FlowResultType.FORM
@@ -82,7 +82,7 @@ async def test_config_flow_errors(
 ) -> None:
     """Test the config flow errors."""
     result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
+        DOMAIN, context={"source": SOURCE_USER}
     )
 
     assert result["type"] == FlowResultType.FORM
@@ -100,3 +100,83 @@ async def test_config_flow_errors(
     assert result2["type"] == FlowResultType.FORM
     assert result2["errors"] == error
     assert len(mock_tedee.get_local_bridge.mock_calls) == 1
+
+
+async def test_reauth_flow(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry, mock_tedee: MagicMock
+) -> None:
+    """Test that the reauth flow works."""
+
+    mock_config_entry.add_to_hass(hass)
+
+    reauth_result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={
+            "source": SOURCE_REAUTH,
+            "unique_id": mock_config_entry.unique_id,
+            "entry_id": mock_config_entry.entry_id,
+        },
+        data={
+            CONF_LOCAL_ACCESS_TOKEN: LOCAL_ACCESS_TOKEN,
+            CONF_HOST: "192.168.1.42",
+        },
+    )
+
+    result = await hass.config_entries.flow.async_configure(
+        reauth_result["flow_id"],
+        {
+            CONF_LOCAL_ACCESS_TOKEN: LOCAL_ACCESS_TOKEN,
+        },
+    )
+    assert result["type"] == FlowResultType.ABORT
+    await hass.async_block_till_done()
+    assert result["reason"] == "reauth_successful"
+
+
+async def test_reauth_flow_errors(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry, mock_tedee: MagicMock
+) -> None:
+    """Test that the reauth flow errors."""
+
+    mock_config_entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={
+            "source": SOURCE_REAUTH,
+            "unique_id": mock_config_entry.unique_id,
+            "entry_id": mock_config_entry.entry_id,
+        },
+        data={
+            CONF_LOCAL_ACCESS_TOKEN: LOCAL_ACCESS_TOKEN,
+            CONF_HOST: "192.168.1.42",
+        },
+    )
+
+    mock_tedee.get_local_bridge.side_effect = TedeeLocalAuthException("Invalid token")
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_LOCAL_ACCESS_TOKEN: "wrong_token",
+        },
+    )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {
+        CONF_LOCAL_ACCESS_TOKEN: "invalid_api_key",
+    }
+    assert len(mock_tedee.get_local_bridge.mock_calls) == 1
+
+    mock_tedee.get_local_bridge.side_effect = TedeeClientException("")
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_LOCAL_ACCESS_TOKEN: "token",
+        },
+    )
+
+    assert result2["type"] == FlowResultType.FORM
+    assert result2["errors"] == {"base": "cannot_connect"}
+    assert len(mock_tedee.get_local_bridge.mock_calls) == 2
