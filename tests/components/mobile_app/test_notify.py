6df29f31e70b14c0f238c43be97796be9dfa7d88
escoand <escoand@users.noreply.github.com>
2023-12-29 22:59:07 +0100

    add tests

--- tests/components/mobile_app/test_notify.py
+++ tests/components/mobile_app/test_notify.py
@@ -159,6 +159,69 @@ async def test_notify_works(
     assert call_json["registration_info"]["webhook_id"] == "mock-webhook_id"
 
 
+async def test_notify_target_device_id(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker, setup_push_receiver
+) -> None:
+    """Test notify target device_id."""
+    assert hass.services.has_service("notify", "mobile_app_test") is True
+    await hass.services.async_call(
+        "notify",
+        "mobile_app_test",
+        {"message": "Hello world", "target": "4d5e6f"},
+        blocking=True,
+    )
+
+    assert len(aioclient_mock.mock_calls) == 1
+    call = aioclient_mock.mock_calls
+
+    call_json = call[0][2]
+
+    assert call_json["push_token"] == "PUSH_TOKEN"
+    assert call_json["message"] == "Hello world"
+    assert call_json["registration_info"]["app_id"] == "io.homeassistant.mobile_app"
+    assert call_json["registration_info"]["app_version"] == "1.0"
+    assert call_json["registration_info"]["webhook_id"] == "mock-webhook_id"
+
+
+async def test_notify_target_webhook_id(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker, setup_push_receiver
+) -> None:
+    """Test notify target webhook_id."""
+    assert hass.services.has_service("notify", "mobile_app_test") is True
+    await hass.services.async_call(
+        "notify",
+        "mobile_app_test",
+        {"message": "Hello world", "target": "mock-webhook_id"},
+        blocking=True,
+    )
+
+    assert len(aioclient_mock.mock_calls) == 1
+    call = aioclient_mock.mock_calls
+
+    call_json = call[0][2]
+
+    assert call_json["push_token"] == "PUSH_TOKEN"
+    assert call_json["message"] == "Hello world"
+    assert call_json["registration_info"]["app_id"] == "io.homeassistant.mobile_app"
+    assert call_json["registration_info"]["app_version"] == "1.0"
+    assert call_json["registration_info"]["webhook_id"] == "mock-webhook_id"
+
+
+async def test_notify_target_not_existing(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker, setup_push_receiver
+) -> None:
+    """Test notify target not existing."""
+    assert hass.services.has_service("notify", "mobile_app_test") is True
+    await hass.services.async_call(
+        "notify",
+        "mobile_app_test",
+        {"message": "Hello world", "target": "not_existing"},
+        blocking=True,
+    )
+
+    assert len(aioclient_mock.mock_calls) == 0
+
+
 async def test_notify_ws_works(
     hass: HomeAssistant,
     aioclient_mock: AiohttpClientMocker,
