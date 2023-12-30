ddd534895547c250f33f800704b5e4ce46d14c33
DeerMaximum <git983456@parabelmail.de>
2023-12-30 22:08:46 +0100

    Change deprecation date

--- homeassistant/components/velux/__init__.py
+++ homeassistant/components/velux/__init__.py
@@ -34,7 +34,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         hass,
         DOMAIN,
         "deprecated_yaml",
-        breaks_in_ha_version="2023.7.0",
+        breaks_in_ha_version="2024.5.0",
         is_fixable=False,
         severity=IssueSeverity.WARNING,
         translation_key="deprecated_yaml",
