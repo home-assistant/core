1237f63a4591d74f83bb89a50524930196729199
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-12-30 18:20:30 +0100

    loop instead of parametrize

--- tests/components/tedee/test_sensor.py
+++ tests/components/tedee/test_sensor.py
@@ -7,32 +7,30 @@ import pytest
 from syrupy import SnapshotAssertion
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import entity_registry as er
 
 pytestmark = pytest.mark.usefixtures("init_integration")
 
 
-@pytest.mark.parametrize(
-    "key",
-    [
-        "battery",
-        "pullspring_duration",
-    ],
+SENSORS = (
+    "battery",
+    "pullspring_duration",
 )
+
+
 async def test_sensors(
     hass: HomeAssistant,
     mock_tedee: MagicMock,
-    device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
     snapshot: SnapshotAssertion,
-    key: str,
 ) -> None:
     """Test tedee sensors."""
-    state = hass.states.get(f"sensor.lock_1a2b_{key}")
-    assert state
-    assert state == snapshot
+    for key in SENSORS:
+        state = hass.states.get(f"sensor.lock_1a2b_{key}")
+        assert state
+        assert state == snapshot
 
-    entry = entity_registry.async_get(state.entity_id)
-    assert entry
-    assert entry.device_id
-    assert entry == snapshot
+        entry = entity_registry.async_get(state.entity_id)
+        assert entry
+        assert entry.device_id
+        assert entry == snapshot
