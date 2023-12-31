ca9ef5f4d8e1e7627ea5551de0c73b2a4b0c364e
autinerd <27780930+autinerd@users.noreply.github.com>
2023-12-31 11:51:23 +0100

    Move urllib3 constraint to pyproject.toml

--- script/gen_requirements_all.py
+++ script/gen_requirements_all.py
@@ -59,11 +59,6 @@ CONSTRAINT_BASE = """
 # see https://github.com/home-assistant/core/pull/16238
 pycryptodome>=3.6.6
 
-# Constrain urllib3 to ensure we deal with CVE-2020-26137 and CVE-2021-33503
-# Temporary setting an upper bound, to prevent compat issues with urllib3>=2
-# https://github.com/home-assistant/core/issues/97248
-urllib3>=1.26.5,<2
-
 # Constrain httplib2 to protect against GHSA-93xj-8mrv-444m
 # https://github.com/advisories/GHSA-93xj-8mrv-444m
 httplib2>=0.19.0
