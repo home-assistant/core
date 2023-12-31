24e72388abae3d8f2923a562f75cc2b8b3d12d81
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-12-31 13:15:58 +0100

    Update homeassistant/components/lamarzocco/config_flow.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/lamarzocco/config_flow.py
+++ homeassistant/components/lamarzocco/config_flow.py
@@ -46,7 +46,7 @@ class LmConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         errors = {}
 
-        if user_input:
+        if user_input is not None:
             data = {
                 **user_input,
                 **self._discovered,
