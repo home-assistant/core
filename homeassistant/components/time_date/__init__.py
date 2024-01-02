48d961b5b4016c198630de4d06aa39fb36db7d13
Erik <erik@montnemery.com>
2024-01-02 21:57:31 +0100

    Fix cleanup of unused entities

--- homeassistant/components/time_date/__init__.py
+++ homeassistant/components/time_date/__init__.py
@@ -10,7 +10,7 @@ from .const import PLATFORMS
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Time & Date from a config entry."""
-    await remove_not_used_entities(hass, entry)
+    remove_unused_entities(hass, entry)
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(async_update_listener))
     return True
@@ -26,12 +26,10 @@ async def async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None
     await hass.config_entries.async_reload(entry.entry_id)
 
 
-async def remove_not_used_entities(hass: HomeAssistant, entry: ConfigEntry) -> None:
+def remove_unused_entities(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Cleanup entities not selected."""
     entity_reg = er.async_get(hass)
     entities = er.async_entries_for_config_entry(entity_reg, entry.entry_id)
     for entity in entities:
-        splitter = entity.entity_id.split(".")
-        check = splitter[1]
-        if check not in entry.options["display_options"]:
+        if entity.unique_id not in entry.options["display_options"]:
             entity_reg.async_remove(entity.entity_id)
