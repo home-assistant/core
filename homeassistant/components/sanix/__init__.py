8ff4e6912a88e78b72df7e4dcaf33f019735af2d
Tomasz Słuszniak <tomasz.sluszniak@gmail.com>
2024-01-02 10:37:34 +0100

    Add Sanix integration

--- homeassistant/components/sanix/__init__.py
+++ homeassistant/components/sanix/__init__.py
@@ -8,24 +8,20 @@ from .const import CONF_SERIAL_NO, CONF_TOKEN, DOMAIN
 from .coordinator import SanixCoordinator
 from .sanix import Sanix
 
-PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.BINARY_SENSOR]
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Sanix from a config entry."""
 
-    hass.data.setdefault(DOMAIN, {})
+    serial_no = entry.data[CONF_SERIAL_NO]
+    token = entry.data[CONF_TOKEN]
 
-    _serial_no = entry.data[CONF_SERIAL_NO]
-    _token = entry.data[CONF_TOKEN]
-
-    sanix_api = Sanix(_serial_no, _token, async_get_clientsession(hass))
+    sanix_api = Sanix(serial_no, token, async_get_clientsession(hass))
     coordinator = SanixCoordinator(hass, sanix_api)
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-
-    hass.config_entries.async_update_entry(entry, unique_id=_serial_no)
 
     await coordinator.async_config_entry_first_refresh()
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
