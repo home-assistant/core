122a5cd307b7fc9916b23cf0f905f2c8fbe6713d
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2024-01-02 08:51:41 +0000

    Super chlorination entities

--- homeassistant/components/screenlogic/number.py
+++ homeassistant/components/screenlogic/number.py
@@ -2,7 +2,11 @@
 from dataclasses import dataclass
 import logging
 
-from screenlogicpy.const.common import ScreenLogicCommunicationError, ScreenLogicError
+from screenlogicpy.const.common import (
+    UNIT,
+    ScreenLogicCommunicationError,
+    ScreenLogicError,
+)
 from screenlogicpy.const.data import ATTR, DEVICE, GROUP, VALUE
 from screenlogicpy.device_const.system import EQUIPMENT_FLAG
 
@@ -11,6 +15,7 @@ from homeassistant.components.number import (
     NumberEntity,
     NumberEntityDescription,
 )
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN, SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
@@ -48,6 +53,14 @@ SUPPORTED_SCG_NUMBERS = [
     ),
 ]
 
+SUPPORTED_SUPER_CHLOR_NUMBERS = [
+    ScreenLogicNumberDescription(
+        data_root=(DEVICE.SCG, GROUP.CONFIGURATION),
+        key="super_chlor_runtime",
+        entity_category=EntityCategory.CONFIG,
+    ),
+]
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -72,6 +85,20 @@ async def async_setup_entry(
         if gateway.get_data(*scg_number_data_path):
             entities.append(ScreenLogicSCGNumber(coordinator, scg_number_description))
 
+    for scg_super_chlor_number_description in SUPPORTED_SUPER_CHLOR_NUMBERS:
+        sup_chlor_number_data_path = (
+            *scg_super_chlor_number_description.data_root,
+            scg_super_chlor_number_description.key,
+        )
+        if EQUIPMENT_FLAG.CHLORINATOR not in gateway.equipment_flags:
+            cleanup_excluded_entity(coordinator, DOMAIN, sup_chlor_number_data_path)
+            continue
+        entities.append(
+            ScreenLogicSuperChlorinationNumber(
+                coordinator, scg_super_chlor_number_description
+            )
+        )
+
     async_add_entities(entities)
 
 
@@ -87,6 +114,10 @@ class ScreenLogicNumber(ScreenLogicEntity, NumberEntity):
     ) -> None:
         """Initialize a ScreenLogic number entity."""
         super().__init__(coordinator, entity_description)
+        if entity_description.enabled_lambda:
+            self._attr_entity_registry_enabled_default = (
+                entity_description.enabled_lambda(coordinator.gateway.equipment_flags)
+            )
 
         self._attr_native_unit_of_measurement = get_ha_unit(
             self.entity_data.get(ATTR.UNIT)
@@ -130,4 +161,56 @@ class ScreenLogicSCGNumber(ScreenLogicNumber):
                 f"Failed to set '{self._data_key}' to {value}: {sle.msg}"
             ) from sle
         _LOGGER.debug("Set '%s' to %s", self._data_key, value)
+        await self._async_refresh()
+
+
+class ScreenLogicSuperChlorinationNumber(ScreenLogicNumber):
+    """Class to represent a ScreenLogic Super Chlorination number."""
+
+    _attr_native_value = 0
+    _attr_native_max_value = 72
+    _attr_native_min_value = 0
+    _attr_native_step = 1
+    _attr_native_unit_of_measurement = get_ha_unit(UNIT.HOUR)
+
+    def __init__(
+        self,
+        coordinator: ScreenlogicDataUpdateCoordinator,
+        entity_description: ScreenLogicNumberDescription,
+    ) -> None:
+        """Initialize a ScreenLogic Super Chlorinate Runtime number entity."""
+        super().__init__(coordinator, entity_description)
+        coordinator.super_chlor_entities[DOMAIN] = self
+
+    @property
+    def entity_data(self) -> dict:
+        """The data for this entity."""
+        return {
+            ATTR.NAME: "Super Chlorination Runtime",
+            ATTR.VALUE: self._attr_native_value,
+        }
+
+    async def async_set_native_value(self, value: float) -> None:
+        """Update the current value."""
+
+        # Current API requires int values for this number.
+        self._attr_native_value = runtime = int(value)
+
+        sc_switch: SwitchEntity | None = self.coordinator.super_chlor_entities.get(
+            SWITCH_DOMAIN
+        )
+        if sc_switch is None or not sc_switch.is_on:
+            _LOGGER.debug(
+                "Not setting %s when 'super_chlorinate' is not on",
+                self._data_key,
+            )
+            return
+
+        try:
+            await self.gateway.async_set_scg_config(super_chlor_timer=runtime)
+        except (ScreenLogicCommunicationError, ScreenLogicError) as sle:
+            raise HomeAssistantError(
+                f"Failed to set '{self._data_key}' to {runtime}: {sle.msg}"
+            ) from sle
+        _LOGGER.debug("Set '%s' to %s", self._data_key, runtime)
         await self._async_refresh()
--- homeassistant/components/screenlogic/switch.py
+++ homeassistant/components/screenlogic/switch.py
@@ -2,31 +2,62 @@
 from dataclasses import dataclass
 import logging
 
-from screenlogicpy.const.data import ATTR, DEVICE
+from screenlogicpy.const.common import (
+    ON_OFF,
+    ScreenLogicCommunicationError,
+    ScreenLogicError,
+)
+from screenlogicpy.const.data import ATTR, DEVICE, VALUE
 from screenlogicpy.const.msg import CODE
 from screenlogicpy.device_const.circuit import GENERIC_CIRCUIT_NAMES, INTERFACE
+from screenlogicpy.device_const.system import EQUIPMENT_FLAG
 
-from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
+from homeassistant.components.number import DOMAIN as NUMBER_DOMAIN, NumberEntity
+from homeassistant.components.switch import (
+    DOMAIN,
+    SwitchEntity,
+    SwitchEntityDescription,
+)
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN as SL_DOMAIN, LIGHT_CIRCUIT_FUNCTIONS
 from .coordinator import ScreenlogicDataUpdateCoordinator
 from .entity import (
     ScreenLogicCircuitEntity,
+    ScreenLogicEntityDescription,
     ScreenLogicPushEntityDescription,
     ScreenLogicSwitchingEntity,
 )
+from .util import cleanup_excluded_entity
 
 _LOGGER = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class ScreenLogicSwitchDescription(
+    SwitchEntityDescription, ScreenLogicEntityDescription
+):
+    """Describes a ScreenLogic switch entity."""
 
 
 @dataclass(frozen=True)
 class ScreenLogicCircuitSwitchDescription(
     SwitchEntityDescription, ScreenLogicPushEntityDescription
 ):
-    """Describes a ScreenLogic switch entity."""
+    """Describes a ScreenLogic circuit switch entity."""
+
+
+SUPPORTED_SUPER_CHLOR_SWITCHES = [
+    ScreenLogicSwitchDescription(
+        data_root=(DEVICE.SCG,),
+        key=VALUE.SUPER_CHLORINATE,
+        entity_category=EntityCategory.CONFIG,
+    )
+]
 
 
 async def async_setup_entry(
@@ -63,6 +94,20 @@ async def async_setup_entry(
                 ),
             )
         )
+    for scg_super_chlor_switch_description in SUPPORTED_SUPER_CHLOR_SWITCHES:
+        sup_chlor_switch_data_path = (
+            *scg_super_chlor_switch_description.data_root,
+            scg_super_chlor_switch_description.key,
+        )
+        if EQUIPMENT_FLAG.CHLORINATOR not in gateway.equipment_flags:
+            cleanup_excluded_entity(coordinator, DOMAIN, sup_chlor_switch_data_path)
+            continue
+        if gateway.get_data(*sup_chlor_switch_data_path):
+            entities.append(
+                ScreenLogicSuperChlorinateSwitch(
+                    coordinator, scg_super_chlor_switch_description
+                )
+            )
 
     async_add_entities(entities)
 
@@ -71,3 +116,41 @@ class ScreenLogicCircuitSwitch(ScreenLogicCircuitEntity, SwitchEntity):
     """Class to represent a ScreenLogic Switch."""
 
     entity_description: ScreenLogicCircuitSwitchDescription
+
+
+class ScreenLogicSuperChlorinateSwitch(ScreenLogicSwitchingEntity, SwitchEntity):
+    """Class to represent a ScreenLogic Super Chlorination switch."""
+
+    entity_description: ScreenLogicSwitchDescription
+
+    def __init__(
+        self,
+        coordinator: ScreenlogicDataUpdateCoordinator,
+        entity_description: ScreenLogicSwitchDescription,
+    ) -> None:
+        """Initialize a ScreenLogic Super Chlorinate switch entity."""
+        super().__init__(coordinator, entity_description)
+        if entity_description.enabled_lambda:
+            self._attr_entity_registry_enabled_default = (
+                entity_description.enabled_lambda(coordinator.gateway.equipment_flags)
+            )
+        coordinator.super_chlor_entities[DOMAIN] = self
+
+    async def _async_set_state(self, state: ON_OFF) -> None:
+        runtime = None
+        sc_number: NumberEntity | None = self.coordinator.super_chlor_entities.get(
+            NUMBER_DOMAIN
+        )
+        if sc_number is not None and sc_number.native_value is not None:
+            runtime = int(sc_number.native_value)
+
+        try:
+            await self.gateway.async_set_scg_config(
+                super_chlorinate=state.value, super_chlor_timer=runtime
+            )
+        except (ScreenLogicCommunicationError, ScreenLogicError) as sle:
+            raise HomeAssistantError(
+                f"Failed to set '{self._data_key}' to {state.value}: {sle.msg}"
+            ) from sle
+        _LOGGER.debug("Set '%s' to %s", self._data_key, state.value)
+        await self._async_refresh()
