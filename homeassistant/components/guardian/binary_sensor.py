3b5551238b8af46847bd870f2743a311895823ad
Aaron Bach <bachya1208@gmail.com>
2024-01-04 11:01:45 -0700

    Formatting

--- homeassistant/components/guardian/binary_sensor.py
+++ homeassistant/components/guardian/binary_sensor.py
@@ -94,7 +94,12 @@ async def async_setup_entry(
     async_finish_entity_domain_replacements(
         hass,
         entry,
-        (EntityDomainReplacementStrategy(BINARY_SENSOR_DOMAIN, f"{uid}_ap_enabled"),),
+        (
+            EntityDomainReplacementStrategy(
+                BINARY_SENSOR_DOMAIN,
+                f"{uid}_ap_enabled",
+            ),
+        ),
     )
 
     @callback
