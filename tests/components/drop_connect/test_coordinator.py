6a02cadc1348d849f220ee7ecf7c00a3536a8176
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2024-01-04 16:17:48 +0100

    Rework drop_connect switch, select and coordinator tests and cleanup fixtures (#107119)
    * Refactor drop_connect switch and select tests
    
    * Update coordinator tests, cleanup fixtures

--- tests/components/drop_connect/test_coordinator.py
+++ tests/components/drop_connect/test_coordinator.py
@@ -1,74 +1,65 @@
 """Test DROP coordinator."""
-from homeassistant.components.drop_connect.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import STATE_UNAVAILABLE, STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
-from homeassistant.setup import async_setup_component
 
-from .common import TEST_DATA_HUB, TEST_DATA_HUB_RESET, TEST_DATA_HUB_TOPIC
+from .common import (
+    TEST_DATA_HUB,
+    TEST_DATA_HUB_RESET,
+    TEST_DATA_HUB_TOPIC,
+    config_entry_hub,
+)
 
 from tests.common import async_fire_mqtt_message
 from tests.typing import MqttMockHAClient
 
 
-async def test_bad_json(
-    hass: HomeAssistant, config_entry_hub, mqtt_mock: MqttMockHAClient
-) -> None:
+async def test_bad_json(hass: HomeAssistant, mqtt_mock: MqttMockHAClient) -> None:
     """Test bad JSON."""
-    config_entry_hub.add_to_hass(hass)
-    assert await async_setup_component(hass, DOMAIN, {})
-    await hass.async_block_till_done()
+    entry = config_entry_hub()
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id)
 
     current_flow_sensor_name = "sensor.hub_drop_1_c0ffee_water_flow_rate"
-    hass.states.async_set(current_flow_sensor_name, STATE_UNKNOWN)
+    assert hass.states.get(current_flow_sensor_name).state == STATE_UNKNOWN
 
     async_fire_mqtt_message(hass, TEST_DATA_HUB_TOPIC, "{BAD JSON}")
     await hass.async_block_till_done()
-
-    current_flow_sensor = hass.states.get(current_flow_sensor_name)
-    assert current_flow_sensor
-    assert current_flow_sensor.state == STATE_UNKNOWN
+    assert hass.states.get(current_flow_sensor_name).state == STATE_UNKNOWN
 
 
-async def test_unload(
-    hass: HomeAssistant, config_entry_hub, mqtt_mock: MqttMockHAClient
-) -> None:
+async def test_unload(hass: HomeAssistant, mqtt_mock: MqttMockHAClient) -> None:
     """Test entity unload."""
     # Load the hub device
-    config_entry_hub.add_to_hass(hass)
-    assert await async_setup_component(hass, DOMAIN, {})
-    await hass.async_block_till_done()
+    entry = config_entry_hub()
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id)
 
     current_flow_sensor_name = "sensor.hub_drop_1_c0ffee_water_flow_rate"
-    hass.states.async_set(current_flow_sensor_name, STATE_UNKNOWN)
+    assert hass.states.get(current_flow_sensor_name).state == STATE_UNKNOWN
 
     async_fire_mqtt_message(hass, TEST_DATA_HUB_TOPIC, TEST_DATA_HUB_RESET)
     await hass.async_block_till_done()
+    assert hass.states.get(current_flow_sensor_name).state == "0.0"
+
     async_fire_mqtt_message(hass, TEST_DATA_HUB_TOPIC, TEST_DATA_HUB)
     await hass.async_block_till_done()
 
-    current_flow_sensor = hass.states.get(current_flow_sensor_name)
-    assert current_flow_sensor
-    assert round(float(current_flow_sensor.state), 1) == 5.8
+    assert hass.states.get(current_flow_sensor_name).state == "5.77"
 
     # Unload the device
-    await hass.config_entries.async_unload(config_entry_hub.entry_id)
-    await hass.async_block_till_done()
-
-    assert config_entry_hub.state is ConfigEntryState.NOT_LOADED
+    await hass.config_entries.async_unload(entry.entry_id)
+    assert entry.state is ConfigEntryState.NOT_LOADED
 
     # Verify sensor is unavailable
-    current_flow_sensor = hass.states.get(current_flow_sensor_name)
-    assert current_flow_sensor
-    assert current_flow_sensor.state == STATE_UNAVAILABLE
+    assert hass.states.get(current_flow_sensor_name).state == STATE_UNAVAILABLE
 
 
-async def test_no_mqtt(hass: HomeAssistant, config_entry_hub) -> None:
+async def test_no_mqtt(hass: HomeAssistant) -> None:
     """Test no MQTT."""
-    config_entry_hub.add_to_hass(hass)
-    assert await async_setup_component(hass, DOMAIN, {})
-    await hass.async_block_till_done()
+    entry = config_entry_hub()
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id) is False
 
     protect_mode_select_name = "select.hub_drop_1_c0ffee_protect_mode"
-    protect_mode_select = hass.states.get(protect_mode_select_name)
-    assert protect_mode_select is None
+    assert hass.states.get(protect_mode_select_name) is None
