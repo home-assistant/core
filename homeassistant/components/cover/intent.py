9b411d96295aa10f91ab45e39aea3b06dae5ef40
Matthieu Bourgain <matthieu.bourgain@fr.clara.net>
2024-01-04 22:32:32 +0000

    add set position and stop intent for cover

--- homeassistant/components/cover/intent.py
+++ homeassistant/components/cover/intent.py
@@ -1,5 +1,10 @@
 """Intents for the cover integration."""
-from homeassistant.const import SERVICE_CLOSE_COVER, SERVICE_OPEN_COVER
+from homeassistant.const import (
+    SERVICE_CLOSE_COVER,
+    SERVICE_OPEN_COVER,
+    SERVICE_SET_COVER_POSITION,
+    SERVICE_STOP_COVER,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import intent
 
@@ -7,6 +12,8 @@ from . import DOMAIN
 
 INTENT_OPEN_COVER = "HassOpenCover"
 INTENT_CLOSE_COVER = "HassCloseCover"
+INTENT_STOP_COVER = "HassStopCover"
+INTENT_SET_COVER_POSITION = "HassSetCoverPosition"
 
 
 async def async_setup_intents(hass: HomeAssistant) -> None:
@@ -21,5 +28,20 @@ async def async_setup_intents(hass: HomeAssistant) -> None:
         hass,
         intent.ServiceIntentHandler(
             INTENT_CLOSE_COVER, DOMAIN, SERVICE_CLOSE_COVER, "Closed {}"
+        ),
+    )
+    intent.async_register(
+        hass,
+        intent.ServiceIntentHandler(
+            INTENT_STOP_COVER, DOMAIN, SERVICE_STOP_COVER, "Stopped {}"
+        ),
+    )
+    intent.async_register(
+        hass,
+        intent.ServiceIntentHandler(
+            INTENT_SET_COVER_POSITION,
+            DOMAIN,
+            SERVICE_SET_COVER_POSITION,
+            "Positioned {}",
         ),
     )
--- tests/components/cover/test_intent.py
+++ tests/components/cover/test_intent.py
@@ -1,7 +1,11 @@
 """The tests for the cover platform."""
+import asyncio
+
 from homeassistant.components.cover import (
     SERVICE_CLOSE_COVER,
     SERVICE_OPEN_COVER,
+    SERVICE_SET_COVER_POSITION,
+    SERVICE_STOP_COVER,
     intent as cover_intent,
 )
 from homeassistant.core import HomeAssistant
@@ -48,3 +52,60 @@ async def test_close_cover_intent(hass: HomeAssistant) -> None:
     assert call.domain == "cover"
     assert call.service == "close_cover"
     assert call.data == {"entity_id": "cover.garage_door"}
+
+
+async def test_stop_cover_intent(hass: HomeAssistant) -> None:
+    """Test HassStopCover intent."""
+    await cover_intent.async_setup_intents(hass)
+
+    hass.states.async_set("cover.bedroom_shutter", "open")
+    hass.states.async_set(
+        "cover.bedroom_shutter",
+        attributes={"current_position": 100, "supported_features": 15},
+    )
+    calls = async_mock_service(hass, "cover", SERVICE_STOP_COVER)
+
+    response = await intent.async_handle(
+        hass, "test", "HassCloseCover", {"name": {"value": "bedroom shutter"}}
+    )
+    await asyncio.sleep(2)
+    response = await intent.async_handle(
+        hass, "test", "HassStopCover", {"name": {"value": "bedroom shutter"}}
+    )
+    await hass.async_block_till_done()
+
+    assert response.speech["plain"]["speech"] == "Stopped bedroom shutter"
+    assert len(calls) == 1
+    call = calls[0]
+    assert call.domain == "cover"
+    assert call.service == "stop_cover"
+    assert call.data == {"entity_id": "cover.bedroom_shutter"}
+    assert hass.states.get("cover.bedroom_shutter").attributes["current_position"] < 100
+
+
+async def test_set_cover_position_intent(hass: HomeAssistant) -> None:
+    """Test HassSetCoverPosition intent."""
+    await cover_intent.async_setup_intents(hass)
+
+    hass.states.async_set("cover.bedroom_shutter", "open")
+    hass.states.async_set(
+        "cover.bedroom_shutter",
+        attributes={"current_position": 100, "supported_features": 15},
+    )
+    calls = async_mock_service(hass, "cover", SERVICE_SET_COVER_POSITION)
+
+    response = await intent.async_handle(
+        hass,
+        "test",
+        "HassSetCoverPosition",
+        {"name": {"value": "bedroom shutter"}, "position": 50},
+    )
+    await hass.async_block_till_done()
+
+    assert response.speech["plain"]["speech"] == "Positioned bedroom shutter"
+    assert len(calls) == 1
+    call = calls[0]
+    assert call.domain == "cover"
+    assert call.service == "set_cover_position"
+    assert call.data == {"entity_id": "cover.bedroom_shutter"}
+    assert hass.states.get("cover.bedroom_shutter").attributes["current_position"] < 50
