575e1ba2d667cf63755088028988452f00e0988b
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2024-01-04 00:15:03 +0000

    Add missing coverage for technoVE config flow

--- tests/components/technove/test_config_flow.py
+++ tests/components/technove/test_config_flow.py
@@ -1,12 +1,17 @@
 """Tests for the TechnoVE config flow."""
 
+from unittest.mock import MagicMock
+
 import pytest
+from technove import TechnoVEConnectionError
 
 from homeassistant.components.technove.const import DOMAIN
 from homeassistant.config_entries import SOURCE_USER
 from homeassistant.const import CONF_IP_ADDRESS
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
+
+from tests.common import MockConfigEntry
 
 
 @pytest.mark.usefixtures("mock_setup_entry", "mock_technove")
@@ -30,3 +35,35 @@ async def test_full_user_flow_implementation(hass: HomeAssistant) -> None:
     assert result["data"][CONF_IP_ADDRESS] == "192.168.1.123"
     assert "result" in result
     assert result["result"].unique_id == "AA:AA:AA:AA:AA:BB"
+
+
+@pytest.mark.usefixtures("mock_technove")
+async def test_user_device_exists_abort(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_technove: MagicMock,
+) -> None:
+    """Test we abort the config flow if TechnoVE station is already configured."""
+    mock_config_entry.add_to_hass(hass)
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_USER},
+        data={CONF_IP_ADDRESS: "192.168.1.123"},
+    )
+
+    assert result.get("type") == FlowResultType.ABORT
+    assert result.get("reason") == "already_configured"
+
+
+async def test_connection_error(hass: HomeAssistant, mock_technove: MagicMock) -> None:
+    """Test we show user form on TechnoVE connection error."""
+    mock_technove.update.side_effect = TechnoVEConnectionError
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_USER},
+        data={CONF_IP_ADDRESS: "example.com"},
+    )
+
+    assert result.get("type") == FlowResultType.FORM
+    assert result.get("step_id") == "user"
+    assert result.get("errors") == {"base": "cannot_connect"}
