e9388bc2498dcea18d1d6b920ba7801518c47a2d
Reagan Sanders <vexofp@gmail.com>
2024-01-03 21:31:35 -0500

    Add support for HLS and WebRTC Octoprint cameras

--- tests/components/octoprint/test_sensor.py
+++ tests/components/octoprint/test_sensor.py
@@ -19,7 +19,7 @@ async def test_sensors(hass: HomeAssistant, freezer: FrozenDateTimeFactory) -> N
         "temperature": {"tool1": {"actual": 18.83136, "target": 37.83136}},
     }
     job = {
-        "job": {},
+        "job": {"file": "test.stl"},
         "progress": {"completion": 50, "printTime": 600, "printTimeLeft": 6000},
         "state": "Printing",
     }
@@ -121,7 +121,7 @@ async def test_sensors_paused(
         "temperature": {"tool1": {"actual": 18.83136, "target": None}},
     }
     job = {
-        "job": {},
+        "job": {"file": "test.stl"},
         "progress": {"completion": 50, "printTime": 600, "printTimeLeft": 6000},
         "state": "Paused",
     }
@@ -150,7 +150,7 @@ async def test_sensors_printer_disconnected(
 ) -> None:
     """Test the underlying sensors."""
     job = {
-        "job": {},
+        "job": {"file": "test.stl"},
         "progress": {"completion": 50, "printTime": 600, "printTimeLeft": 6000},
         "state": "Paused",
     }
