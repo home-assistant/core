8a6dcb6c34effd619f84a00b72c6e801cc439f73
Greg Szabo <greg@philosobear.com>
2024-01-05 00:59:57 -0500

    Fixed ArcticSpa tests

--- tests/components/arcticspa/__init__.py
+++ tests/components/arcticspa/__init__.py
@@ -4,16 +4,18 @@ from unittest.mock import AsyncMock, MagicMock, Mock
 
 from pyarcticspas import Spa
 
-from homeassistant.components.arcticspa import CONF_API_KEY, Device
+from homeassistant.components.arcticspa import CONF_API_KEY
 from homeassistant.components.arcticspa.const import DOMAIN
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
 
 CONF_API_KEY_VALUE = "mockapikey"
+CONF_ID = "3b82ca882f2ebd283afc13c96209d6efc2e2b598ab6a9e02acb1090a55b7e643"  # id of "mockapikey"
+CONF_NAME = "API-3b82ca88"
 
 
-def _mock_device() -> Spa:
+def _mock_spa() -> Spa:
     spa = MagicMock(auto_spec=Spa, name="Mocked Arctic Spa Device")
     spa.async_status = AsyncMock()
     spa.status = Mock()
@@ -23,14 +25,13 @@ def _mock_device() -> Spa:
 
 
 async def initialize_config_entry_for_device(
-    hass: HomeAssistant, dev: Device
+    hass: HomeAssistant, dev: Spa
 ) -> MockConfigEntry:
     """Create a mocked configuration entry for the given device."""
 
     config_entry = MockConfigEntry(
         title="Arctic Spa",
         domain=DOMAIN,
-        unique_id=dev.id,
         data={CONF_API_KEY: CONF_API_KEY_VALUE},
     )
     config_entry.add_to_hass(hass)
--- tests/components/arcticspa/test_config_flow.py
+++ tests/components/arcticspa/test_config_flow.py
@@ -9,7 +9,12 @@ from homeassistant.components.arcticspa.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
-from tests.components.arcticspa import CONF_API_KEY_VALUE
+from tests.components.arcticspa import (
+    CONF_API_KEY,
+    CONF_API_KEY_VALUE,
+    CONF_ID,
+    CONF_NAME,
+)
 
 pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 
@@ -23,23 +28,24 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.arcticspa.config_flow.Device.authenticate",
-        return_value=200,
+        "homeassistant.components.arcticspa.config_flow.validate_input",
+        return_value={
+            "name": CONF_NAME,
+            "id": CONF_ID,
+            CONF_API_KEY: CONF_API_KEY_VALUE,
+        },
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "name": "Arctic Spa Hot Tub API Key",
-                "api_key": CONF_API_KEY_VALUE,
+                CONF_API_KEY: CONF_API_KEY_VALUE,
             },
         )
         await hass.async_block_till_done()
 
     assert result2["type"] == FlowResultType.CREATE_ENTRY
-    assert result2["title"] == "Arctic Spa Hot Tub API Key"
     assert result2["data"] == {
-        "name": "Arctic Spa Hot Tub API Key",
-        "api_key": CONF_API_KEY_VALUE,
+        CONF_API_KEY: CONF_API_KEY_VALUE,
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -51,14 +57,13 @@ async def test_form_invalid_auth(hass: HomeAssistant) -> None:
     )
 
     with patch(
-        "homeassistant.components.arcticspa.config_flow.Device.authenticate",
+        "homeassistant.components.arcticspa.config_flow.validate_input",
         side_effect=InvalidAuth,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "name": "Arctic Spa Hot Tub API Key",
-                "api_key": CONF_API_KEY_VALUE,
+                CONF_API_KEY: CONF_API_KEY_VALUE,
             },
         )
 
@@ -73,14 +78,13 @@ async def test_form_cannot_connect(hass: HomeAssistant) -> None:
     )
 
     with patch(
-        "homeassistant.components.arcticspa.config_flow.Device.authenticate",
+        "homeassistant.components.arcticspa.config_flow.validate_input",
         side_effect=CannotConnect,
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
-                "name": "Arctic Spa Hot Tub API Key",
-                "api_key": "fake_api_key",
+                CONF_API_KEY: "fake_api_key",
             },
         )
 
