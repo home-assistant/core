c74c3f22b5dfa9c040f488a2f867f9db53fc5bda
Brett Adams <brett.whynot@gmail.com>
2024-01-05 03:09:36 +0000

    Add cable lock

--- homeassistant/components/tessie/lock.py
+++ homeassistant/components/tessie/lock.py
@@ -3,16 +3,19 @@ from __future__ import annotations
 
 from typing import Any
 
-from tessie_api import lock, unlock
+from tessie_api import lock, open_unlock_charge_port, unlock
 
 from homeassistant.components.lock import LockEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
 from .coordinator import TessieStateUpdateCoordinator
 from .entity import TessieEntity
+
+ENGAGED = "Engaged"
 
 
 async def async_setup_entry(
@@ -21,11 +24,15 @@ async def async_setup_entry(
     """Set up the Tessie sensor platform from a config entry."""
     data = hass.data[DOMAIN][entry.entry_id]
 
-    async_add_entities(TessieLockEntity(vehicle.state_coordinator) for vehicle in data)
+    async_add_entities(
+        klass(vehicle.state_coordinator)
+        for klass in (TessieLockEntity, TessieCableLockEntity)
+        for vehicle in data
+    )
 
 
 class TessieLockEntity(TessieEntity, LockEntity):
-    """Lock entity for current charge."""
+    """Lock entity for Tessie."""
 
     def __init__(
         self,
@@ -48,3 +55,28 @@ class TessieLockEntity(TessieEntity, LockEntity):
         """Set new value."""
         await self.run(unlock)
         self.set((self.key, False))
+
+
+class TessieCableLockEntity(TessieEntity, LockEntity):
+    """Cable Lock entity for Tessie."""
+
+    def __init__(
+        self,
+        coordinator: TessieStateUpdateCoordinator,
+    ) -> None:
+        """Initialize the sensor."""
+        super().__init__(coordinator, "charge_state_charge_port_latch")
+
+    @property
+    def is_locked(self) -> bool | None:
+        """Return the state of the Lock."""
+        return self._value == ENGAGED
+
+    async def async_lock(self, **kwargs: Any) -> None:
+        """Charge cable Lock cannot be manually locked."""
+        raise HomeAssistantError("Insert cable to lock")
+
+    async def async_unlock(self, **kwargs: Any) -> None:
+        """Unlock charge cable lock."""
+        await self.run(open_unlock_charge_port)
+        self.set((self.key, ENGAGED))
