6ecd63cafb064964f0d70c50186c2ea5a2fdba39
G Johansson <goran.johansson@shiftit.se>
2024-01-07 12:10:42 +0000

    debug

--- homeassistant/components/systemmonitor/util.py
+++ homeassistant/components/systemmonitor/util.py
@@ -26,7 +26,7 @@ def get_all_disk_mounts() -> set[str]:
             )
             continue
         except OSError as err:
-            _LOGGER.warning(
+            _LOGGER.debug(
                 "Mountpoint %s was excluded because of: %s", part.mountpoint, err
             )
             continue
