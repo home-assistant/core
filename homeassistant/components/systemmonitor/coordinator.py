86df5a8b1561838111718d0dc833d36547b9e3a2
G Johansson <goran.johansson@shiftit.se>
2024-01-08 19:22:05 +0000

    Clean up coordinators

--- homeassistant/components/systemmonitor/coordinator.py
+++ homeassistant/components/systemmonitor/coordinator.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from abc import abstractmethod
+from collections.abc import Iterator
 from datetime import datetime
 import logging
 import os
@@ -9,12 +10,12 @@ import socket
 from typing import TypeVar
 
 import psutil
-from psutil._common import sdiskusage, shwtemp, sswap
+from psutil._common import sdiskusage, shwtemp, snetio, snicaddr, sswap
 from psutil._pslinux import svmem
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_component import DEFAULT_SCAN_INTERVAL
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.util import dt as dt_util
 
 from .const import DOMAIN
@@ -67,6 +68,10 @@ _dataT = TypeVar(
     | tuple[float, float, float]
     | datetime
     | bool
+    | dict[str, snetio]
+    | dict[str, list[snicaddr]]
+    | Iterator[psutil.Process]
+    | dict[str, list[shwtemp]]
     | None,
 )
 
@@ -74,17 +79,15 @@ _dataT = TypeVar(
 class MonitorCoordinator(DataUpdateCoordinator[_dataT]):
     """A System monitor Base Data Update Coordinator."""
 
-    def __init__(self, hass: HomeAssistant, type_: str, argument: str) -> None:
+    def __init__(self, hass: HomeAssistant) -> None:
         """Initialize the coordinator."""
         super().__init__(
             hass,
             _LOGGER,
-            name=f"{DOMAIN} {type_}_{argument}",
+            name=f"{DOMAIN} {self.__repr__}",
             update_interval=DEFAULT_SCAN_INTERVAL,
             always_update=False,
         )
-        self._type = type_
-        self._argument = argument
 
     async def _async_update_data(self) -> _dataT:
         """Fetch data."""
@@ -97,6 +100,11 @@ class MonitorCoordinator(DataUpdateCoordinator[_dataT]):
 
 class SystemMonitorDiskCoordinator(MonitorCoordinator[sdiskusage]):
     """A System monitor Disk Data Update Coordinator."""
+
+    def __init__(self, hass: HomeAssistant, argument: str) -> None:
+        """Initialize the disk coordinator."""
+        super().__init__(hass)
+        self._argument = argument
 
     def update_data(self) -> sdiskusage:
         """Fetch data."""
@@ -119,56 +127,20 @@ class SystemMonitorMemoryCoordinator(MonitorCoordinator[svmem]):
         return psutil.virtual_memory()
 
 
-class SystemMonitorNetIOCoordinator(MonitorCoordinator[int]):
+class SystemMonitorNetIOCoordinator(MonitorCoordinator[dict[str, snetio]]):
     """A System monitor Network IO Data Update Coordinator."""
 
-    def update_data(self) -> int:
+    def update_data(self) -> dict[str, snetio]:
         """Fetch data."""
-        counters = psutil.net_io_counters(pernic=True)
-        if self._argument in counters:
-            return counters[self._argument][IO_COUNTER[self._type]]
-        raise UpdateFailed(f"NIC {self._argument} could not be found")
+        return psutil.net_io_counters(pernic=True)
 
 
-class SystemMonitorNetThroughputCoordinator(MonitorCoordinator[float | None]):
-    """A System monitor Network Throughput Data Update Coordinator."""
-
-    def __init__(self, hass: HomeAssistant, type_: str, argument: str) -> None:
-        """Initialize the coordinator."""
-        super().__init__(hass, type_, argument)
-        self._value: float | None = None
-        self._update_time: datetime | None = None
-
-    def update_data(self) -> float | None:
-        """Fetch data."""
-        counters = psutil.net_io_counters(pernic=True)
-        if self._argument in counters:
-            counter = counters[self._argument][IO_COUNTER[self._type]]
-            now = dt_util.utcnow()
-            if self._value and self._value < counter:
-                return round(
-                    (counter - self._value)
-                    / 1000**2
-                    / (now - (self._update_time or now)).total_seconds(),
-                    3,
-                )
-            self._update_time = now
-            self._value = counter
-            return None
-        raise UpdateFailed(f"NIC {self._argument} could not be found")
-
-
-class SystemMonitorNetAddrCoordinator(MonitorCoordinator[str]):
+class SystemMonitorNetAddrCoordinator(MonitorCoordinator[dict[str, list[snicaddr]]]):
     """A System monitor Network Address Data Update Coordinator."""
 
-    def update_data(self) -> str:
+    def update_data(self) -> dict[str, list[snicaddr]]:
         """Fetch data."""
-        addresses = psutil.net_if_addrs()
-        if self._argument in addresses:
-            for addr in addresses[self._argument]:
-                if addr.family == IF_ADDRS_FAMILY[self._type]:
-                    return addr.address
-        raise UpdateFailed(f"NIC {self._argument} could not be found for {self._type}")
+        return psutil.net_if_addrs()
 
 
 class SystemMonitorLoadCoordinator(MonitorCoordinator[tuple[float, float, float]]):
@@ -195,39 +167,17 @@ class SystemMonitorBootTimeCoordinator(MonitorCoordinator[datetime]):
         return dt_util.utc_from_timestamp(psutil.boot_time())
 
 
-class SystemMonitorProcessCoordinator(MonitorCoordinator[bool]):
+class SystemMonitorProcessCoordinator(MonitorCoordinator[Iterator[psutil.Process]]):
     """A System monitor Process Data Update Coordinator."""
 
-    def update_data(self) -> bool:
+    def update_data(self) -> Iterator[psutil.Process]:
         """Fetch data."""
-        for proc in psutil.process_iter():
-            try:
-                if self._argument.lower() == proc.name().lower():
-                    return True
-            except psutil.NoSuchProcess as err:
-                _LOGGER.warning(
-                    "Failed to load process with ID: %s, old name: %s",
-                    err.pid,
-                    err.name,
-                )
-        return False
+        return psutil.process_iter()
 
 
-class SystemMonitorCPUtempCoordinator(MonitorCoordinator[float]):
+class SystemMonitorCPUtempCoordinator(MonitorCoordinator[dict[str, list[shwtemp]]]):
     """A System monitor CPU Temperature Data Update Coordinator."""
 
-    def update_data(self) -> float:
+    def update_data(self) -> dict[str, list[shwtemp]]:
         """Fetch data."""
-        entry: shwtemp
-        temps = psutil.sensors_temperatures()
-
-        for name, entries in temps.items():
-            for i, entry in enumerate(entries, start=1):
-                # In case the label is empty (e.g. on Raspberry PI 4),
-                # construct it ourself here based on the sensor key name.
-                _label = f"{name} {i}" if not entry.label else entry.label
-                # check both name and label because some systems embed cpu# in the
-                # name, which makes label not match because label adds cpu# at end.
-                if _label in CPU_SENSOR_PREFIXES or name in CPU_SENSOR_PREFIXES:
-                    return round(entry.current, 1)
-        raise UpdateFailed("No temp sensors available")
+        return psutil.sensors_temperatures()
