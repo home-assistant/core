93e6f1d9e50006f092b414876d8dccdd100962a0
Brett Adams <brett.whynot@gmail.com>
2024-01-08 07:26:40 +0000

    Add test that doesnt work

--- tests/components/tessie/test_coordinator.py
+++ tests/components/tessie/test_coordinator.py
@@ -1,14 +1,18 @@
 """Test the Tessie sensor platform."""
 from datetime import timedelta
 
+import pytest
+
 from homeassistant.components.tessie.coordinator import TESSIE_SYNC_INTERVAL
 from homeassistant.const import STATE_OFF, STATE_ON, STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import UpdateFailed
 from homeassistant.util.dt import utcnow
 
 from .common import (
     ERROR_AUTH,
     ERROR_CONNECTION,
+    ERROR_TIMEOUT,
     ERROR_UNKNOWN,
     TEST_VEHICLE_STATE_ASLEEP,
     TEST_VEHICLE_STATE_ONLINE,
@@ -42,6 +46,19 @@ async def test_coordinator_asleep(hass: HomeAssistant, mock_get_state) -> None:
     await hass.async_block_till_done()
     mock_get_state.assert_called_once()
     assert hass.states.get("binary_sensor.test_status").state == STATE_OFF
+
+
+async def test_coordinator_timeout(hass: HomeAssistant, mock_get_state) -> None:
+    """Tests that the coordinator handles asleep vehicles."""
+
+    mock_get_state.side_effect = ERROR_TIMEOUT
+    await setup_platform(hass)
+
+    with pytest.raises(UpdateFailed):
+        async_fire_time_changed(hass, utcnow() + WAIT)
+        await hass.async_block_till_done()
+        mock_get_state.assert_called_once()
+        assert hass.states.get("binary_sensor.test_status").state == STATE_UNAVAILABLE
 
 
 async def test_coordinator_clienterror(hass: HomeAssistant, mock_get_state) -> None:
