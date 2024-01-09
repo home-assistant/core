9b31f611a39e62a9917f46d2cbd3da991ca03529
J. Nick Koston <nick@koston.org>
2024-01-08 17:30:13 -1000

    verify caching

--- tests/auth/test_models.py
+++ tests/auth/test_models.py
@@ -33,13 +33,30 @@ def test_cache_cleared_on_group_change() -> None:
     group = models.Group(
         name="Test Group", policy={"entities": {"domains": {"switch": True}}}
     )
-    user = models.User(name="Test User", perm_lookup=None, groups=[group])
+    admin_group = models.Group(
+        name="Admin group", id=models.GROUP_ID_ADMIN, policy={"entities": {}}
+    )
+    user = models.User(
+        name="Test User", perm_lookup=None, groups=[group], is_active=True
+    )
     # Make sure we cache instance
     assert user.permissions is user.permissions
 
     # Make sure we cache is_admin
     assert user.is_admin is user.is_admin
+    assert user.is_active is True
 
     user.groups = []
-
     assert user.groups == []
+    assert user.is_admin is False
+
+    user.is_owner = True
+    assert user.is_admin is True
+    user.is_owner = False
+
+    assert user.is_admin is False
+    user.groups = [admin_group]
+    assert user.is_admin is True
+
+    user.is_active = False
+    assert user.is_admin is False
