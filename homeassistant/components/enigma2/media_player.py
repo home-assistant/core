a70cf62af4eed22bfc788510c140f67c126975ad
autinerd <27780930+autinerd@users.noreply.github.com>
2024-01-09 18:46:29 +0100

    handle deep standby and poweroff of enigma2 devices gracefully

--- homeassistant/components/enigma2/media_player.py
+++ homeassistant/components/enigma2/media_player.py
@@ -1,7 +1,9 @@
 """Support for Enigma2 media players."""
 from __future__ import annotations
 
-from aiohttp.client_exceptions import ClientConnectorError
+from logging import getLogger
+
+from aiohttp.client_exceptions import ClientConnectorError, ServerDisconnectedError
 from openwebif.api import OpenWebIfDevice
 from openwebif.enums import RemoteControlCodes, SetVolumeOption
 import voluptuous as vol
@@ -47,6 +49,8 @@ ATTR_MEDIA_CURRENTLY_RECORDING = "media_currently_recording"
 ATTR_MEDIA_DESCRIPTION = "media_description"
 ATTR_MEDIA_END_TIME = "media_end_time"
 ATTR_MEDIA_START_TIME = "media_start_time"
+
+_LOGGER = getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
@@ -136,7 +140,15 @@ class Enigma2Device(MediaPlayerEntity):
 
     async def async_turn_off(self) -> None:
         """Turn off media player."""
-        await self._device.turn_off()
+        try:
+            await self._device.turn_off()
+        except ServerDisconnectedError as err:
+            _LOGGER.warning(
+                "%s went into deep standby. Error: %s",
+                self._device.base.host,
+                str(err),
+            )
+            self._attr_available = False
 
     async def async_turn_on(self) -> None:
         """Turn the media player on."""
@@ -184,8 +196,18 @@ class Enigma2Device(MediaPlayerEntity):
 
     async def async_update(self) -> None:
         """Update state of the media_player."""
-        await self._device.update()
-        self._attr_available = not self._device.is_offline
+        try:
+            await self._device.update()
+            if not self._attr_available:
+                _LOGGER.debug("%s is available", self._device.base.host)
+                self._attr_available = True
+        except ClientConnectorError as err:
+            if self._attr_available:
+                _LOGGER.warning(
+                    "%s is unavailable. Error: %s", self._device.base.host, str(err)
+                )
+                self._attr_available = False
+            return
 
         if not self._device.status.in_standby:
             self._attr_extra_state_attributes = {
