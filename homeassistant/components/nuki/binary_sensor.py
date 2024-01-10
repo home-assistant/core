8ae573fb01225c5bf7faacf5bd0170978c19c483
steffenrapp <88974099+steffenrapp@users.noreply.github.com>
2024-01-10 19:14:23 +0000

    Expose ring_action_state and ring_action_timestamp of Nuki Opener

--- homeassistant/components/nuki/binary_sensor.py
+++ homeassistant/components/nuki/binary_sensor.py
@@ -13,22 +13,28 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import NukiEntity, NukiEntryData
-from .const import ATTR_NUKI_ID, DOMAIN as NUKI_DOMAIN
+from .const import ATTR_NUKI_ID, ATTR_RING_TIMESTAMP, DOMAIN as NUKI_DOMAIN
 
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """Set up the Nuki lock binary sensor."""
+    """Set up the Nuki binary sensors."""
     entry_data: NukiEntryData = hass.data[NUKI_DOMAIN][entry.entry_id]
 
-    entities = []
+    lock_entities = []
+    opener_entities = []
 
     for lock in entry_data.locks:
         if lock.is_door_sensor_activated:
-            entities.extend([NukiDoorsensorEntity(entry_data.coordinator, lock)])
+            lock_entities.extend([NukiDoorsensorEntity(entry_data.coordinator, lock)])
 
-    async_add_entities(entities)
+    async_add_entities(lock_entities)
+
+    for opener in entry_data.openers:
+        opener_entities.extend([NukiRingactionEntity(entry_data.coordinator, opener)])
+
+    async_add_entities(opener_entities)
 
 
 class NukiDoorsensorEntity(NukiEntity[NukiDevice], BinarySensorEntity):
@@ -70,3 +76,35 @@ class NukiDoorsensorEntity(NukiEntity[NukiDevice], BinarySensorEntity):
     def is_on(self):
         """Return true if the door is open."""
         return self.door_sensor_state == STATE_DOORSENSOR_OPENED
+
+
+class NukiRingactionEntity(NukiEntity[NukiDevice], BinarySensorEntity):
+    """Representation of a Nuki Opener Ringaction."""
+
+    _attr_has_entity_name = True
+    _attr_name = None
+    _attr_icon = "mdi:bell-ring"
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique ID."""
+        return f"{self._nuki_device.nuki_id}_ringaction"
+
+    @property
+    def extra_state_attributes(self):
+        """Return the device specific state attributes."""
+        data = {
+            ATTR_NUKI_ID: self._nuki_device.nuki_id,
+            ATTR_RING_TIMESTAMP: self._nuki_device.ring_action_timestamp,
+        }
+        return data
+
+    @property
+    def ring_action_state(self):
+        """Return the state of the ring action."""
+        return self._nuki_device.ring_action_state
+
+    @property
+    def is_on(self) -> bool:
+        """Return the value of the ring action state."""
+        return self.ring_action_state
--- homeassistant/components/nuki/const.py
+++ homeassistant/components/nuki/const.py
@@ -6,6 +6,7 @@ ATTR_BATTERY_CRITICAL = "battery_critical"
 ATTR_NUKI_ID = "nuki_id"
 ATTR_ENABLE = "enable"
 ATTR_UNLATCH = "unlatch"
+ATTR_RING_TIMESTAMP = "ring_action_timestamp"
 
 # Defaults
 DEFAULT_PORT = 8080
