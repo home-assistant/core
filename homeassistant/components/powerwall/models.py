e8958d04eb60eaf63f72b25a88856ee82124b601
bubonicbob <12600312+bubonicbob@users.noreply.github.com>
2024-01-10 13:21:53 -0800

    Update powerwall for tesla_powerwall 0.5.0 which is async (#107164)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/powerwall/models.py
+++ homeassistant/components/powerwall/models.py
@@ -4,15 +4,14 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TypedDict
 
-from requests import Session
 from tesla_powerwall import (
     DeviceType,
     GridStatus,
-    MetersAggregates,
+    MetersAggregatesResponse,
     Powerwall,
-    PowerwallStatus,
-    SiteInfo,
-    SiteMaster,
+    PowerwallStatusResponse,
+    SiteInfoResponse,
+    SiteMasterResponse,
 )
 
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
@@ -23,8 +22,8 @@ class PowerwallBaseInfo:
     """Base information for the powerwall integration."""
 
     gateway_din: None | str
-    site_info: SiteInfo
-    status: PowerwallStatus
+    site_info: SiteInfoResponse
+    status: PowerwallStatusResponse
     device_type: DeviceType
     serial_numbers: list[str]
     url: str
@@ -35,8 +34,8 @@ class PowerwallData:
     """Point in time data for the powerwall integration."""
 
     charge: float
-    site_master: SiteMaster
-    meters: MetersAggregates
+    site_master: SiteMasterResponse
+    meters: MetersAggregatesResponse
     grid_services_active: bool
     grid_status: GridStatus
     backup_reserve: float | None
@@ -49,4 +48,3 @@ class PowerwallRuntimeData(TypedDict):
     api_instance: Powerwall
     base_info: PowerwallBaseInfo
     api_changed: bool
-    http_session: Session
