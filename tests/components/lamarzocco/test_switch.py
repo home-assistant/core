cb62e1b68156dbad6b5ca7edc72d07d21b2f5b00
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-01-10 12:24:43 +0100

    switch to snapshot testing

--- tests/components/lamarzocco/test_switch.py
+++ tests/components/lamarzocco/test_switch.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock
 
 from lmcloud.const import LaMarzoccoModel
 import pytest
+from syrupy import SnapshotAssertion
 
 from homeassistant.components.lamarzocco.const import DOMAIN
 from homeassistant.components.switch import (
@@ -10,14 +11,7 @@ from homeassistant.components.switch import (
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
 )
-from homeassistant.const import (
-    ATTR_DEVICE_CLASS,
-    ATTR_ENTITY_ID,
-    ATTR_FRIENDLY_NAME,
-    ATTR_ICON,
-    STATE_OFF,
-    STATE_ON,
-)
+from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 
@@ -31,32 +25,23 @@ async def test_main(
     mock_lamarzocco: MagicMock,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test the La Marzocco Main switch."""
     serial_number = mock_lamarzocco.serial_number
 
     state = hass.states.get(f"switch.{serial_number}_main")
     assert state
-    assert state.attributes.get(ATTR_DEVICE_CLASS) is None
-    assert state.attributes.get(ATTR_FRIENDLY_NAME) == f"{serial_number} Main"
-    assert state.attributes.get(ATTR_ICON) == "mdi:power"
-    assert state.state == STATE_ON
+    assert state == snapshot
 
     entry = entity_registry.async_get(state.entity_id)
     assert entry
     assert entry.device_id
-    assert entry.unique_id == f"{serial_number}_main"
+    assert entry == snapshot
 
     device = device_registry.async_get(entry.device_id)
     assert device
-    assert device.configuration_url is None
-    assert device.entry_type is None
-    assert device.hw_version is None
-    assert device.identifiers == {(DOMAIN, serial_number)}
-    assert device.manufacturer == "La Marzocco"
-    assert device.name == serial_number
-    assert device.serial_number == serial_number
-    assert device.sw_version == "1.1"
+    assert device == snapshot
 
     await hass.services.async_call(
         SWITCH_DOMAIN,
@@ -88,32 +73,23 @@ async def test_auto_on_off(
     mock_lamarzocco: MagicMock,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test the La Marzocco Auto On/Off switch."""
     serial_number = mock_lamarzocco.serial_number
 
     state = hass.states.get(f"switch.{serial_number}_auto_on_off")
     assert state
-    assert state.attributes.get(ATTR_DEVICE_CLASS) is None
-    assert state.attributes.get(ATTR_FRIENDLY_NAME) == f"{serial_number} Auto on/off"
-    assert state.attributes.get(ATTR_ICON) == "mdi:alarm"
-    assert state.state == STATE_ON
+    assert state == snapshot
 
     entry = entity_registry.async_get(state.entity_id)
     assert entry
     assert entry.device_id
-    assert entry.unique_id == f"{serial_number}_auto_on_off"
+    assert entry == snapshot
 
     device = device_registry.async_get(entry.device_id)
     assert device
-    assert device.configuration_url is None
-    assert device.entry_type is None
-    assert device.hw_version is None
-    assert device.identifiers == {(DOMAIN, serial_number)}
-    assert device.manufacturer == "La Marzocco"
-    assert device.name == serial_number
-    assert device.serial_number == serial_number
-    assert device.sw_version == "1.1"
+    assert device == snapshot
 
     await hass.services.async_call(
         SWITCH_DOMAIN,
@@ -145,6 +121,7 @@ async def test_prebrew(
     mock_lamarzocco: MagicMock,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test the La Marzocco Prebrew switch."""
     serial_number = mock_lamarzocco.serial_number
@@ -155,26 +132,16 @@ async def test_prebrew(
         assert state is None
         return
     assert state
-    assert state.attributes.get(ATTR_DEVICE_CLASS) is None
-    assert state.attributes.get(ATTR_FRIENDLY_NAME) == f"{serial_number} Prebrew"
-    assert state.attributes.get(ATTR_ICON) == "mdi:water"
-    assert state.state == STATE_ON
+    assert state == snapshot
 
     entry = entity_registry.async_get(state.entity_id)
     assert entry
     assert entry.device_id
-    assert entry.unique_id == f"{serial_number}_prebrew"
+    assert entry == snapshot
 
     device = device_registry.async_get(entry.device_id)
     assert device
-    assert device.configuration_url is None
-    assert device.entry_type is None
-    assert device.hw_version is None
-    assert device.identifiers == {(DOMAIN, serial_number)}
-    assert device.manufacturer == "La Marzocco"
-    assert device.name == serial_number
-    assert device.serial_number == serial_number
-    assert device.sw_version == "1.1"
+    assert device == snapshot
 
     await hass.services.async_call(
         SWITCH_DOMAIN,
@@ -206,6 +173,7 @@ async def test_preinfusion(
     mock_lamarzocco: MagicMock,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test the La Marzocco Preinfusion switch."""
     serial_number = mock_lamarzocco.serial_number
@@ -217,26 +185,16 @@ async def test_preinfusion(
         return
 
     assert state
-    assert state.attributes.get(ATTR_DEVICE_CLASS) is None
-    assert state.attributes.get(ATTR_FRIENDLY_NAME) == f"{serial_number} Preinfusion"
-    assert state.attributes.get(ATTR_ICON) == "mdi:water"
-    assert state.state == STATE_OFF
+    assert state == snapshot
 
     entry = entity_registry.async_get(state.entity_id)
     assert entry
     assert entry.device_id
-    assert entry.unique_id == f"{serial_number}_preinfusion"
+    assert entry == snapshot
 
     device = device_registry.async_get(entry.device_id)
     assert device
-    assert device.configuration_url is None
-    assert device.entry_type is None
-    assert device.hw_version is None
-    assert device.identifiers == {(DOMAIN, serial_number)}
-    assert device.manufacturer == "La Marzocco"
-    assert device.name == serial_number
-    assert device.serial_number == serial_number
-    assert device.sw_version == "1.1"
+    assert device == snapshot
 
     await hass.services.async_call(
         SWITCH_DOMAIN,
@@ -268,32 +226,23 @@ async def test_steam_boiler_enable(
     mock_lamarzocco: MagicMock,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test the La Marzocco Steam Boiler switch."""
     serial_number = mock_lamarzocco.serial_number
 
     state = hass.states.get(f"switch.{serial_number}_steam_boiler")
     assert state
-    assert state.attributes.get(ATTR_DEVICE_CLASS) is None
-    assert state.attributes.get(ATTR_FRIENDLY_NAME) == f"{serial_number} Steam boiler"
-    assert state.attributes.get(ATTR_ICON) == "mdi:water-boiler"
-    assert state.state == STATE_ON
+    assert state == snapshot
 
     entry = entity_registry.async_get(state.entity_id)
     assert entry
     assert entry.device_id
-    assert entry.unique_id == f"{serial_number}_steam_boiler_enable"
+    assert entry == snapshot
 
     device = device_registry.async_get(entry.device_id)
     assert device
-    assert device.configuration_url is None
-    assert device.entry_type is None
-    assert device.hw_version is None
-    assert device.identifiers == {(DOMAIN, serial_number)}
-    assert device.manufacturer == "La Marzocco"
-    assert device.name == serial_number
-    assert device.serial_number == serial_number
-    assert device.sw_version == "1.1"
+    assert device == snapshot
 
     await hass.services.async_call(
         SWITCH_DOMAIN,
