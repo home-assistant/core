ee98c2f2d11c764c59474b35f142e38167752f2b
Erik <erik@montnemery.com>
2024-01-11 13:21:41 +0100

    Hide FlowResultType.SHOW_PROGRESS_DONE from frontend

--- tests/test_data_entry_flow.py
+++ tests/test_data_entry_flow.py
@@ -498,6 +498,54 @@ async def test_show_progress_error(hass: HomeAssistant, manager) -> None:
     assert result["reason"] == "error"
 
 
+async def test_show_progress_hidden_from_frontend(hass: HomeAssistant, manager) -> None:
+    """Test show progress done is not sent to frontend."""
+    manager.hass = hass
+    async_show_progress_done_called = False
+
+    @manager.mock_reg_handler("test")
+    class TestFlow(data_entry_flow.FlowHandler):
+        VERSION = 5
+        data = None
+        progress_task: asyncio.Task[None] | None = None
+
+        async def async_step_init(self, user_input=None):
+            async def long_running_job() -> None:
+                return
+
+            if not self.progress_task:
+                self.progress_task = hass.async_create_task(long_running_job())
+            if self.progress_task.done():
+                nonlocal async_show_progress_done_called
+                async_show_progress_done_called = True
+                return self.async_show_progress_done(next_step_id="finish")
+            return self.async_show_progress(
+                step_id="init",
+                progress_action="task",
+                # Set to None to simulate flow manager has not yet called when
+                # frontend loads
+                progress_task=None,
+            )
+
+        async def async_step_finish(self, user_input=None):
+            return self.async_create_entry(title=None, data=self.data)
+
+    result = await manager.async_init("test")
+    assert result["type"] == data_entry_flow.FlowResultType.SHOW_PROGRESS
+    assert result["progress_action"] == "task"
+    assert len(manager.async_progress()) == 1
+    assert len(manager.async_progress_by_handler("test")) == 1
+    assert manager.async_get(result["flow_id"])["handler"] == "test"
+
+    await hass.async_block_till_done()
+    assert not async_show_progress_done_called
+
+    # Frontend refreshes the flow
+    result = await manager.async_configure(result["flow_id"])
+    assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
+    assert async_show_progress_done_called
+
+
 async def test_show_progress_legacy(hass: HomeAssistant, manager, caplog) -> None:
     """Test show progress logic.
 
