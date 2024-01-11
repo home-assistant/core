0dfe0954ab1ecf4809b26d8fad7bcf8c7dcfdb18
J. Nick Koston <nick@koston.org>
2024-01-10 23:20:06 -1000

    commit the tests

--- tests/components/govee_ble/test_sensor.py
+++ tests/components/govee_ble/test_sensor.py
@@ -160,7 +160,7 @@ async def test_gvh5106(hass: HomeAssistant) -> None:
     """Test setting up creates the sensors for a device with PM25."""
     entry = MockConfigEntry(
         domain=DOMAIN,
-        unique_id="AA:BB:CC:DD:EE:FF",
+        unique_id="CC:32:37:35:4E:05",
     )
     entry.add_to_hass(hass)
 
@@ -170,14 +170,14 @@ async def test_gvh5106(hass: HomeAssistant) -> None:
     assert len(hass.states.async_all()) == 0
     inject_bluetooth_service_info(hass, GVH5106_SERVICE_INFO)
     await hass.async_block_till_done()
-    assert len(hass.states.async_all()) == 4
+    assert len(hass.states.async_all()) == 3
 
-    temp_sensor = hass.states.get("sensor.h5075_2762_temperature")
-    temp_sensor_attribtes = temp_sensor.attributes
-    assert temp_sensor.state == "21.3"
-    assert temp_sensor_attribtes[ATTR_FRIENDLY_NAME] == "H5075 2762 Temperature"
-    assert temp_sensor_attribtes[ATTR_UNIT_OF_MEASUREMENT] == "°C"
-    assert temp_sensor_attribtes[ATTR_STATE_CLASS] == "measurement"
+    pm25_sensor = hass.states.get("sensor.h5106_4e05_pm25")
+    pm25_sensor_attributes = pm25_sensor.attributes
+    assert pm25_sensor.state == "0"
+    assert pm25_sensor_attributes[ATTR_FRIENDLY_NAME] == "H5106 4E05 Pm25"
+    assert pm25_sensor_attributes[ATTR_UNIT_OF_MEASUREMENT] == "µg/m³"
+    assert pm25_sensor_attributes[ATTR_STATE_CLASS] == "measurement"
 
     assert await hass.config_entries.async_unload(entry.entry_id)
     await hass.async_block_till_done()
