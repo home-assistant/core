32002fbfc6f272cedd009dd454fe149f18cf0de8
Christopher Fenner <Christopher.Fenner@me.com>
2024-01-12 15:30:20 +0100

    add get_serial function

--- homeassistant/components/vicare/utils.py
+++ homeassistant/components/vicare/utils.py
@@ -91,3 +91,13 @@ def get_compressors(device: PyViCareDevice) -> list[PyViCareHeatingDeviceCompone
     except PyViCareNotSupportedFeatureError:
         _LOGGER.debug("No compressors found")
     return []
+
+
+def get_serial(device_config: PyViCareDeviceConfig) -> str:
+    """Return the serial number for a device config."""
+    # we cannot always use device_config.getConfig().serial as it returns the gateway serial for all connected devices
+    if device_config.getModel() == "Heatbox1":
+        return device_config.getConfig().serial
+
+    # we cannot always use device.getSerial() either as there is a different API endpoint used for gateways that does not provide the serial (yet)
+    return device_config.asAutoDetectDevice().getSerial()
