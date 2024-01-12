6cbb1f6e414514b1ac337cbf10f61d838cc00961
Joostlek <joostlek@outlook.com>
2024-01-12 11:49:37 +0100

    Add more Netatmo base entities

--- homeassistant/components/netatmo/entity.py
+++ homeassistant/components/netatmo/entity.py
@@ -1,9 +1,11 @@
 """Base class for Netatmo entities."""
 from __future__ import annotations
 
+from abc import abstractmethod
 from typing import Any
 
-from pyatmo import DeviceType
+from pyatmo import DeviceType, Home, Room
+from pyatmo.modules.base_class import NetatmoBase
 from pyatmo.modules.device_types import (
     DEVICE_DESCRIPTION_MAP,
     DeviceType as NetatmoDeviceType,
@@ -14,26 +16,27 @@ from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 
-from .const import DATA_DEVICE_IDS, DEFAULT_ATTRIBUTION, DOMAIN, SIGNAL_NAME
-from .data_handler import PUBLIC, NetatmoDataHandler
+from .const import (
+    CONF_URL_ENERGY,
+    DATA_DEVICE_IDS,
+    DEFAULT_ATTRIBUTION,
+    DOMAIN,
+    SIGNAL_NAME,
+)
+from .data_handler import PUBLIC, NetatmoDataHandler, NetatmoRoom
 
 
 class NetatmoBaseEntity(Entity):
     """Netatmo entity base class."""
 
     _attr_attribution = DEFAULT_ATTRIBUTION
+    _attr_has_entity_name = True
 
-    def __init__(self, data_handler: NetatmoDataHandler) -> None:
+    def __init__(self, data_handler: NetatmoDataHandler, device: NetatmoBase) -> None:
         """Set up Netatmo entity base."""
         self.data_handler = data_handler
+        self.device = device
         self._publishers: list[dict[str, Any]] = []
-
-        self._device_name: str = ""
-        self._id: str = ""
-        self._model: DeviceType
-        self._config_url: str | None = None
-        self._attr_name = None
-        self._attr_unique_id = None
         self._attr_extra_state_attributes = {}
 
     async def async_added_to_hass(self) -> None:
@@ -71,10 +74,6 @@ class NetatmoBaseEntity(Entity):
             ):
                 await self.data_handler.unsubscribe(signal_name, None)
 
-        registry = dr.async_get(self.hass)
-        if device := registry.async_get_device(identifiers={(DOMAIN, self._id)}):
-            self.hass.data[DOMAIN][DATA_DEVICE_IDS][self._id] = device.id
-
         self.async_update_callback()
 
     async def async_will_remove_from_hass(self) -> None:
@@ -92,17 +91,53 @@ class NetatmoBaseEntity(Entity):
         raise NotImplementedError
 
     @property
-    def device_info(self) -> DeviceInfo:
-        """Return the device info for the sensor."""
-        if "." in self._model:
-            netatmo_device = NetatmoDeviceType(self._model.partition(".")[2])
+    @abstractmethod
+    def device_type(self) -> DeviceType:
+        """Return the device type."""
+
+    @property
+    def device_description(self) -> tuple[str, str]:
+        """Return the model of this device."""
+        if "." in self.device_type:
+            netatmo_device = NetatmoDeviceType(self.device_type.partition(".")[2])
         else:
-            netatmo_device = getattr(NetatmoDeviceType, self._model)
-        manufacturer, model = DEVICE_DESCRIPTION_MAP[netatmo_device]
-        return DeviceInfo(
-            configuration_url=self._config_url,
-            identifiers={(DOMAIN, self._id)},
-            name=self._device_name,
-            manufacturer=manufacturer,
-            model=model,
+            netatmo_device = getattr(NetatmoDeviceType, self.device_type)
+        return DEVICE_DESCRIPTION_MAP[netatmo_device]
+
+
+class NetatmoRoomEntity(NetatmoBaseEntity):
+    """Netatmo room entity base class."""
+
+    device: Room
+
+    def __init__(self, room: NetatmoRoom) -> None:
+        """Set up a Netatmo room entity."""
+        super().__init__(room.data_handler, room.room)
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, room.room.entity_id)},
+            name=room.room.name,
+            manufacturer=self.device_description[0],
+            model=self.device_description[1],
+            configuration_url=CONF_URL_ENERGY,
+            suggested_area=room.room.name,
         )
+
+    async def async_added_to_hass(self) -> None:
+        """Entity created."""
+        await super().async_added_to_hass()
+        registry = dr.async_get(self.hass)
+        if device := registry.async_get_device(
+            identifiers={(DOMAIN, self.device.entity_id)}
+        ):
+            self.hass.data[DOMAIN][DATA_DEVICE_IDS][self.device.entity_id] = device.id
+
+    @property
+    def device_type(self) -> DeviceType:
+        """Return the device type."""
+        assert self.device.climate_type
+        return self.device.climate_type
+
+    @property
+    def home(self) -> Home:
+        """Return the home this room belongs to."""
+        return self.device.home
