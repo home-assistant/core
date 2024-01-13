8129d170b74b2f8d6e39afef947bdf829ef7ddd5
J. Nick Koston <nick@koston.org>
2024-01-13 08:39:27 -1000

    Remove useless _update function in ESPHome (#107927)
    This function is never overwritten so we can remove it

--- homeassistant/components/esphome/entity.py
+++ homeassistant/components/esphome/entity.py
@@ -301,13 +301,11 @@ class EsphomeAssistEntity(Entity):
             connections={(dr.CONNECTION_NETWORK_MAC, device_info.mac_address)}
         )
 
-    @callback
-    def _update(self) -> None:
-        self.async_write_ha_state()
-
     async def async_added_to_hass(self) -> None:
         """Register update callback."""
         await super().async_added_to_hass()
         self.async_on_remove(
-            self._entry_data.async_subscribe_assist_pipeline_update(self._update)
+            self._entry_data.async_subscribe_assist_pipeline_update(
+                self.async_write_ha_state
+            )
         )
