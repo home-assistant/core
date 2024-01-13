719e58c68d1a3ae2551d281929704086775e4fa4
Michael Arthur <mikey0000@users.noreply.github.com>
2024-01-14 06:12:40 +1300

    Add account sensors to electric kiwi integration (#97681)
    * add account sensors
    
    * tidy up same issues as other sensors
    
    * add unit tests for sensors
    
    edit and remove comments
    
    assert state and remove HOP sensor types since they aren't being used
    
    * try and fix tests
    
    * add frozen true
    
    * Update tests/components/electric_kiwi/test_sensor.py
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * return proper native types
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * tidy up attr unique id
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * add entities once and use native values properly
    
    * Improve conftest
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * tidy tests/components/electric_kiwi/test_sensor.py
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * add assert to component_setup
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * add extra parameters to test
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * Update tests/components/electric_kiwi/test_sensor.py
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * Update tests/components/electric_kiwi/test_sensor.py
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * change coordinator name
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>
    
    * tidy up sensor translation names
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- homeassistant/components/electric_kiwi/coordinator.py
+++ homeassistant/components/electric_kiwi/coordinator.py
@@ -6,7 +6,7 @@ import logging
 
 from electrickiwi_api import ElectricKiwiApi
 from electrickiwi_api.exceptions import ApiException, AuthException
-from electrickiwi_api.model import Hop, HopIntervals
+from electrickiwi_api.model import AccountBalance, Hop, HopIntervals
 
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
@@ -14,11 +14,38 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, Upda
 
 _LOGGER = logging.getLogger(__name__)
 
+ACCOUNT_SCAN_INTERVAL = timedelta(hours=6)
 HOP_SCAN_INTERVAL = timedelta(minutes=20)
 
 
+class ElectricKiwiAccountDataCoordinator(DataUpdateCoordinator):
+    """ElectricKiwi Account Data object."""
+
+    def __init__(self, hass: HomeAssistant, ek_api: ElectricKiwiApi) -> None:
+        """Initialize ElectricKiwiAccountDataCoordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="Electric Kiwi Account Data",
+            update_interval=ACCOUNT_SCAN_INTERVAL,
+        )
+        self._ek_api = ek_api
+
+    async def _async_update_data(self) -> AccountBalance:
+        """Fetch data from Account balance API endpoint."""
+        try:
+            async with asyncio.timeout(60):
+                return await self._ek_api.get_account_balance()
+        except AuthException as auth_err:
+            raise ConfigEntryAuthFailed from auth_err
+        except ApiException as api_err:
+            raise UpdateFailed(
+                f"Error communicating with EK API: {api_err}"
+            ) from api_err
+
+
 class ElectricKiwiHOPDataCoordinator(DataUpdateCoordinator[Hop]):
-    """ElectricKiwi Data object."""
+    """ElectricKiwi HOP Data object."""
 
     def __init__(self, hass: HomeAssistant, ek_api: ElectricKiwiApi) -> None:
         """Initialize ElectricKiwiAccountDataCoordinator."""
