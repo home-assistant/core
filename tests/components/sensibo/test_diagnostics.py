0004db27ba5a4e9d94d82b9d44aa451ae4b2af63
G Johansson <goran.johansson@shiftit.se>
2024-01-14 14:30:08 +0000

    use constant

--- tests/components/sensibo/test_diagnostics.py
+++ tests/components/sensibo/test_diagnostics.py
@@ -9,6 +9,8 @@ from homeassistant.core import HomeAssistant
 from tests.components.diagnostics import get_diagnostics_for_config_entry
 from tests.typing import ClientSessionGenerator
 
+EXCLUDE_ATTRIBUTES = {"full_features"}
+
 
 async def test_diagnostics(
     hass: HomeAssistant,
@@ -30,9 +32,7 @@ async def test_diagnostics(
     assert diag["ABC999111"]["pure_conf"] == snapshot
 
     def limit_attrs(prop, path):
-        exclude_attrs = {
-            "full_features",
-        }
+        exclude_attrs = EXCLUDE_ATTRIBUTES
         return prop in exclude_attrs
 
     assert diag == snapshot(name="full_snapshot", exclude=limit_attrs)
