b64561cdbbeb5e2956cdc91655e9ea551f0056d8
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-01-15 08:03:23 +0100

    Remove await

--- tests/test_config_entries.py
+++ tests/test_config_entries.py
@@ -4170,7 +4170,7 @@ async def test_update_entry_and_reload(
 
         async def async_step_reauth(self, data):
             """Mock Reauth."""
-            return await self.async_update_and_reload_entry(
+            return self.async_update_and_reload_entry(
                 entry=entry,
                 title="Updated Title",
                 data={"vendor": "data2"},
