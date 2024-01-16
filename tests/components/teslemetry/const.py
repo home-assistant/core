0d6019d1313694492874a16a7b83195422f70c72
Brett Adams <brett.whynot@gmail.com>
2024-01-16 09:28:48 +0000

    Delete other tests

--- tests/components/teslemetry/const.py
+++ tests/components/teslemetry/const.py
@@ -1,10 +1,5 @@
 """Constants for the teslemetry tests."""
 
-
 from homeassistant.const import CONF_ACCESS_TOKEN
-
-COMMAND_SUCCESS = {"response": {"reason": "", "result": True}}
-WAKE_AWAKE = {"response": {"state": "online"}}
-WAKE_ASLEEP = {"response": {"state": "asleep"}}
 
 CONFIG = {CONF_ACCESS_TOKEN: "1234567890"}
