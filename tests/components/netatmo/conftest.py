465550a00ebda1ed6c10d0f4075b08ee0144a3ea
G Johansson <goran.johansson@shiftit.se>
2024-01-16 22:17:18 +0100

    Remove config import from netatmo (#107972)
    * Remove config import from netatmo
    
    * Fix tests

--- tests/components/netatmo/conftest.py
+++ tests/components/netatmo/conftest.py
@@ -5,11 +5,31 @@ from unittest.mock import AsyncMock, patch
 from pyatmo.const import ALL_SCOPES
 import pytest
 
+from homeassistant.components.application_credentials import (
+    ClientCredential,
+    async_import_client_credential,
+)
+from homeassistant.components.netatmo.const import DOMAIN
 from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
 
 from .common import fake_get_image, fake_post_request
 
 from tests.common import MockConfigEntry
+
+CLIENT_ID = "1234"
+CLIENT_SECRET = "5678"
+
+
+@pytest.fixture(autouse=True)
+async def setup_credentials(hass: HomeAssistant) -> None:
+    """Fixture to setup credentials."""
+    assert await async_setup_component(hass, "application_credentials", {})
+    await async_import_client_credential(
+        hass,
+        DOMAIN,
+        ClientCredential(CLIENT_ID, CLIENT_SECRET),
+    )
 
 
 @pytest.fixture(name="config_entry")
