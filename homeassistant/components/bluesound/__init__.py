e55f11ee97e81fc26a27f19f4e330a040099cad2
Austin Hendrix <namniart@gmail.com>
2024-01-15 19:32:43 -0800

    Some porgress

--- homeassistant/components/bluesound/__init__.py
+++ homeassistant/components/bluesound/__init__.py
@@ -1,13 +1,43 @@
 """The bluesound component."""
 import logging
 
-from homeassistant.const import CONF_HOST, CONF_PORT, CONF_NAME
+from homeassistant import config_entries
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import CALLBACK_TYPE, Event, HomeAssistant, callback
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 
 
 _LOGGER = logging.getLogger(__name__)
 
+from .const import DOMAIN, PLATFORMS
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Bluesound component."""
+    conf = config.get(DOMAIN)
+
+    hass.data[DOMAIN] = conf or {}
+
+    if conf is not None:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN, context={"source": config_entries.SOURCE_IMPORT}
+            )
+        )
+
+    return True
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Bluesound from a config entry."""
-    _LOGGER.debug("Bluesound async_setup_entry with %r", entry)
+    _LOGGER.debug("Bluesound async_setup_entry: %r", entry.data)
+
+
+    # This creates each HA object for each platform your device requires.
+    # It's done by calling the `async_setup_entry` function in each platform module.
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a Bluesound config entry."""
+    _LOGGER.debug("Bluesound async_unload_entry with %r", entry)
+
+    return False
