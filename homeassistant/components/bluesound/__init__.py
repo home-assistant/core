6670b035edc87e6170cca038cb177172b89d0c0d
Austin Hendrix <namniart@gmail.com>
2024-01-15 19:05:07 -0800

    Try to add config entry point

--- homeassistant/components/bluesound/__init__.py
+++ homeassistant/components/bluesound/__init__.py
@@ -1,1 +1,13 @@
 """The bluesound component."""
+import logging
+
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_NAME
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import CALLBACK_TYPE, Event, HomeAssistant, callback
+
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Bluesound from a config entry."""
+    _LOGGER.debug("Bluesound async_setup_entry with %r", entry)
