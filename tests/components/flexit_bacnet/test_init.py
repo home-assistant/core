27a037ef1717ef61eb93d4239058ad5d29c47c1f
Joost Lekkerkerker <joostlek@outlook.com>
2024-01-18 14:55:44 +0100

    Add test for failed initialization in Flexit BACnet (#108294)

--- tests/components/flexit_bacnet/test_init.py
+++ tests/components/flexit_bacnet/test_init.py
@@ -1,5 +1,7 @@
 """Tests for the Flexit Nordic (BACnet) __init__."""
-from homeassistant.components.flexit_bacnet.const import DOMAIN as FLEXIT_BACNET_DOMAIN
+from flexit_bacnet import DecodingError
+
+from homeassistant.components.flexit_bacnet.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -14,11 +16,20 @@ async def test_loading_and_unloading_config_entry(
     """Test loading and unloading a config entry."""
     await setup_with_selected_platforms(hass, mock_config_entry, [Platform.CLIMATE])
 
-    assert len(hass.config_entries.async_entries(FLEXIT_BACNET_DOMAIN)) == 1
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
     assert mock_config_entry.state == ConfigEntryState.LOADED
 
     assert await hass.config_entries.async_unload(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
-    assert not hass.data.get(FLEXIT_BACNET_DOMAIN)
+
+
+async def test_failed_initialization(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry, mock_flexit_bacnet
+) -> None:
+    """Test failed initialization."""
+    mock_flexit_bacnet.update.side_effect = DecodingError
+    mock_config_entry.add_to_hass(hass)
+    assert not await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    assert mock_config_entry.state == ConfigEntryState.SETUP_RETRY
