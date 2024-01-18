4f9e3124006bcf7ec10dfdbd72f6d75566b8fd98
Jonas Fors Lellky <lellky@gmail.com>
2024-01-18 14:06:58 +0100

    Adds snapshot testing

--- tests/components/flexit_bacnet/test_climate.py
+++ tests/components/flexit_bacnet/test_climate.py
@@ -1,20 +1,11 @@
 """Tests for the Flexit Nordic (BACnet) climate entity."""
 from unittest.mock import AsyncMock
 
-from homeassistant.components.climate import (
-    ATTR_CURRENT_TEMPERATURE,
-    ATTR_HVAC_MODES,
-    ATTR_MAX_TEMP,
-    ATTR_MIN_TEMP,
-    ATTR_PRESET_MODES,
-    ATTR_TARGET_TEMP_STEP,
-    PRESET_AWAY,
-    PRESET_BOOST,
-    PRESET_HOME,
-    HVACMode,
-)
-from homeassistant.const import PRECISION_HALVES, Platform
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+import homeassistant.helpers.entity_registry as er
 
 from tests.common import MockConfigEntry
 from tests.components.flexit_bacnet import setup_with_selected_platforms
@@ -26,24 +17,11 @@ async def test_climate_entity(
     hass: HomeAssistant,
     mock_flexit_bacnet: AsyncMock,
     mock_config_entry: MockConfigEntry,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
 ) -> None:
     """Test the initial parameters."""
     await setup_with_selected_platforms(hass, mock_config_entry, [Platform.CLIMATE])
-    state = hass.states.get(ENTITY_CLIMATE)
 
-    assert state.attributes.get("name") is None
-    assert state.state == HVACMode.FAN_ONLY
-    assert state.attributes.get(ATTR_PRESET_MODES) == [
-        PRESET_AWAY,
-        PRESET_HOME,
-        PRESET_BOOST,
-    ]
-    assert state.attributes.get(ATTR_HVAC_MODES) == [
-        HVACMode.OFF,
-        HVACMode.FAN_ONLY,
-    ]
-    assert state.attributes.get(ATTR_MIN_TEMP) == 10
-    assert state.attributes.get(ATTR_MAX_TEMP) == 30
-
-    assert state.attributes.get(ATTR_TARGET_TEMP_STEP) == PRECISION_HALVES
-    assert state.attributes.get(ATTR_CURRENT_TEMPERATURE) == 19.00
+    assert hass.states.get(ENTITY_CLIMATE) == snapshot
+    assert entity_registry.async_get(ENTITY_CLIMATE) == snapshot
