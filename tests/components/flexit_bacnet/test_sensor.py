3d37a21faaa9200893bf05a2dabef006ea191db2
Jonas Fors Lellky <lellky@gmail.com>
2024-01-19 15:46:46 +0100

    Refactors the sensor test

--- tests/components/flexit_bacnet/test_sensor.py
+++ tests/components/flexit_bacnet/test_sensor.py
@@ -1,56 +1,33 @@
 """Tests for the Flexit Nordic (BACnet) sensor entities."""
 from unittest.mock import AsyncMock
 
-import pytest
 from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import entity_registry as er
 
 from tests.common import MockConfigEntry
 from tests.components.flexit_bacnet import setup_with_selected_platforms
 
 
-@pytest.mark.parametrize(
-    "entity_id",
-    [
-        "sensor.device_name_air_filter_operating_time",
-        "sensor.device_name_outside_air_temperature",
-        "sensor.device_name_supply_air_temperature",
-        "sensor.device_name_exhaust_air_temperature",
-        "sensor.device_name_extract_air_temperature",
-        "sensor.device_name_room_temperature",
-        "sensor.device_name_fireplace_ventilation_remaining_duration",
-        "sensor.device_name_rapid_ventilation_remaining_duration",
-        "sensor.device_name_supply_air_fan_control_signal",
-        "sensor.device_name_supply_air_fan_rpm",
-        "sensor.device_name_exhaust_air_fan_control_signal",
-        "sensor.device_name_exhaust_air_fan_rpm",
-        "sensor.device_name_electric_heater_power",
-        "sensor.device_name_air_filter_operating_time",
-        "sensor.device_name_heat_exchanger_efficiency",
-        "sensor.device_name_heat_exchanger_speed",
-    ],
-)
 async def test_sensors(
     hass: HomeAssistant,
-    device_registry: dr.DeviceRegistry,
-    entity_registry: er.EntityRegistry,
-    mock_config_entry: MockConfigEntry,
-    mock_flexit_bacnet: AsyncMock,
     snapshot: SnapshotAssertion,
-    entity_id: str,
+    entity_registry: er.EntityRegistry,
+    mock_flexit_bacnet: AsyncMock,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
-    """Test a Flexit (BACnet) sensor."""
+    """Test sensor states are correctly collected from library."""
+
     await setup_with_selected_platforms(hass, mock_config_entry, [Platform.SENSOR])
+    entity_entries = er.async_entries_for_config_entry(
+        entity_registry, mock_config_entry.entry_id
+    )
 
-    assert (state := hass.states.get(entity_id))
-    assert state == snapshot
-
-    assert (entry := entity_registry.async_get(entity_id))
-    assert entry == snapshot
-
-    assert entry.device_id
-    assert (device_entry := device_registry.async_get(entry.device_id))
-    assert device_entry == snapshot
+    assert entity_entries
+    for entity_entry in entity_entries:
+        assert entity_entry == snapshot(name=f"{entity_entry.entity_id}-entry")
+        assert hass.states.get(entity_entry.entity_id) == snapshot(
+            name=f"{entity_entry.entity_id}-state"
+        )
