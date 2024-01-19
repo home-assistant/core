ff017e4148e637663252ca2320fb542562b3c0ad
Brett <brett.whynot@gmail.com>
2024-01-19 22:09:25 +0000

    Redo device tracker

--- tests/components/tessie/test_device_tracker.py
+++ tests/components/tessie/test_device_tracker.py
@@ -2,17 +2,25 @@
 
 from syrupy import SnapshotAssertion
 
-from homeassistant.components.device_tracker import DOMAIN as DEVICE_TRACKER_DOMAIN
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from .common import setup_platform
 
 
-async def test_device_tracker(hass: HomeAssistant, snapshot: SnapshotAssertion) -> None:
+async def test_device_tracker(
+    hass: HomeAssistant, snapshot: SnapshotAssertion, entity_registry: er.EntityRegistry
+) -> None:
     """Tests that the device tracker entities are correct."""
 
-    assert len(hass.states.async_all(DEVICE_TRACKER_DOMAIN)) == 0
+    entry = await setup_platform(hass, [Platform.BINARY_SENSOR])
 
-    await setup_platform(hass)
+    entity_entries = er.async_entries_for_config_entry(entity_registry, entry.entry_id)
 
-    assert hass.states.async_all(DEVICE_TRACKER_DOMAIN) == snapshot(name="all")
+    assert entity_entries
+    for entity_entry in entity_entries:
+        assert entity_entry == snapshot(name=f"{entity_entry.entity_id}-entry")
+        assert hass.states.get(entity_entry.entity_id) == snapshot(
+            name=f"{entity_entry.entity_id}-state"
+        )
