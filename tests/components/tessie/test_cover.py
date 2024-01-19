8cd520412a4e4dc924413d474a26cd95e2832993
Brett <brett.whynot@gmail.com>
2024-01-19 22:08:18 +0000

    Redo Cover

--- tests/components/tessie/test_cover.py
+++ tests/components/tessie/test_cover.py
@@ -11,72 +11,73 @@ from homeassistant.components.cover import (
     STATE_CLOSED,
     STATE_OPEN,
 )
-from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.const import ATTR_ENTITY_ID, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers import entity_registry as er
 
 from .common import ERROR_UNKNOWN, TEST_RESPONSE, TEST_RESPONSE_ERROR, setup_platform
 
 
-@pytest.mark.parametrize(
-    ("entity_id", "openfunc", "closefunc"),
-    [
+async def test_covers(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Tests that the window cover entity is correct."""
+
+    entry = await setup_platform(hass, [Platform.COVER])
+
+    entity_entries = er.async_entries_for_config_entry(entity_registry, entry.entry_id)
+
+    assert entity_entries
+    for entity_entry in entity_entries:
+        assert entity_entry == snapshot(name=f"{entity_entry.entity_id}-entry")
+        assert hass.states.get(entity_entry.entity_id) == snapshot(
+            name=f"{entity_entry.entity_id}-state"
+        )
+
+    for entity_id, openfunc, closefunc in [
         ("cover.test_vent_windows", "vent_windows", "close_windows"),
         ("cover.test_charge_port_door", "open_unlock_charge_port", "close_charge_port"),
         ("cover.test_frunk", "open_front_trunk", False),
         ("cover.test_trunk", "open_close_rear_trunk", "open_close_rear_trunk"),
-    ],
-)
-async def test_covers(
-    hass: HomeAssistant,
-    entity_id: str,
-    openfunc: str,
-    closefunc: str,
-    snapshot: SnapshotAssertion,
-) -> None:
-    """Tests that the window cover entity is correct."""
+    ]:
+        # Test open windows
+        if openfunc:
+            with patch(
+                f"homeassistant.components.tessie.cover.{openfunc}",
+                return_value=TEST_RESPONSE,
+            ) as mock_open:
+                await hass.services.async_call(
+                    COVER_DOMAIN,
+                    SERVICE_OPEN_COVER,
+                    {ATTR_ENTITY_ID: [entity_id]},
+                    blocking=True,
+                )
+                mock_open.assert_called_once()
+            assert hass.states.get(entity_id).state == STATE_OPEN
 
-    assert len(hass.states.async_all(COVER_DOMAIN)) == 0
-
-    await setup_platform(hass)
-
-    assert hass.states.get(entity_id) == snapshot
-
-    # Test open windows
-    if openfunc:
-        with patch(
-            f"homeassistant.components.tessie.cover.{openfunc}",
-            return_value=TEST_RESPONSE,
-        ) as mock_open:
-            await hass.services.async_call(
-                COVER_DOMAIN,
-                SERVICE_OPEN_COVER,
-                {ATTR_ENTITY_ID: [entity_id]},
-                blocking=True,
-            )
-            mock_open.assert_called_once()
-        assert hass.states.get(entity_id).state == STATE_OPEN
-
-    # Test close windows
-    if closefunc:
-        with patch(
-            f"homeassistant.components.tessie.cover.{closefunc}",
-            return_value=TEST_RESPONSE,
-        ) as mock_close:
-            await hass.services.async_call(
-                COVER_DOMAIN,
-                SERVICE_CLOSE_COVER,
-                {ATTR_ENTITY_ID: [entity_id]},
-                blocking=True,
-            )
-            mock_close.assert_called_once()
-        assert hass.states.get(entity_id).state == STATE_CLOSED
+        # Test close windows
+        if closefunc:
+            with patch(
+                f"homeassistant.components.tessie.cover.{closefunc}",
+                return_value=TEST_RESPONSE,
+            ) as mock_close:
+                await hass.services.async_call(
+                    COVER_DOMAIN,
+                    SERVICE_CLOSE_COVER,
+                    {ATTR_ENTITY_ID: [entity_id]},
+                    blocking=True,
+                )
+                mock_close.assert_called_once()
+            assert hass.states.get(entity_id).state == STATE_CLOSED
 
 
 async def test_errors(hass: HomeAssistant) -> None:
     """Tests errors are handled."""
 
-    await setup_platform(hass)
+    await setup_platform(hass, [Platform.COVER])
     entity_id = "cover.test_charge_port_door"
 
     # Test setting cover open with unknown error
@@ -92,13 +93,6 @@ async def test_errors(hass: HomeAssistant) -> None:
         )
         mock_set.assert_called_once()
         assert error.from_exception == ERROR_UNKNOWN
-
-
-async def test_response_error(hass: HomeAssistant) -> None:
-    """Tests response errors are handled."""
-
-    await setup_platform(hass)
-    entity_id = "cover.test_charge_port_door"
 
     # Test setting cover open with unknown error
     with patch(
