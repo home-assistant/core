d83b5083a443b0622310e1b88aec862d776622f5
Brett <brett.whynot@gmail.com>
2024-01-19 22:22:21 +0000

    Redo Sensor

--- tests/components/tessie/test_sensor.py
+++ tests/components/tessie/test_sensor.py
@@ -1,16 +1,25 @@
 """Test the Tessie sensor platform."""
 from syrupy import SnapshotAssertion
 
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from .common import setup_platform
 
 
-async def test_sensors(hass: HomeAssistant, snapshot: SnapshotAssertion) -> None:
+async def test_sensors(
+    hass: HomeAssistant, snapshot: SnapshotAssertion, entity_registry: er.EntityRegistry
+) -> None:
     """Tests that the sensor entities are correct."""
 
-    assert len(hass.states.async_all("sensor")) == 0
+    entry = await setup_platform(hass, [Platform.SENSOR])
 
-    await setup_platform(hass)
+    entity_entries = er.async_entries_for_config_entry(entity_registry, entry.entry_id)
 
-    assert hass.states.async_all("sensor") == snapshot(name="all")
+    assert entity_entries
+    for entity_entry in entity_entries:
+        assert entity_entry == snapshot(name=f"{entity_entry.entity_id}-entry")
+        assert hass.states.get(entity_entry.entity_id) == snapshot(
+            name=f"{entity_entry.entity_id}-state"
+        )
