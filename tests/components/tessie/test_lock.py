88bebd66b01505548adafa3e6ec2b8f73554af56
Brett <brett.whynot@gmail.com>
2024-01-19 22:12:03 +0000

    Redo lock

--- tests/components/tessie/test_lock.py
+++ tests/components/tessie/test_lock.py
@@ -10,21 +10,29 @@ from homeassistant.components.lock import (
     SERVICE_LOCK,
     SERVICE_UNLOCK,
 )
-from homeassistant.const import ATTR_ENTITY_ID, STATE_LOCKED, STATE_UNLOCKED
+from homeassistant.const import ATTR_ENTITY_ID, STATE_LOCKED, STATE_UNLOCKED, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ServiceValidationError
+from homeassistant.helpers import entity_registry as er
 
 from .common import setup_platform
 
 
-async def test_locks(hass: HomeAssistant, snapshot: SnapshotAssertion) -> None:
+async def test_locks(
+    hass: HomeAssistant, snapshot: SnapshotAssertion, entity_registry: er.EntityRegistry
+) -> None:
     """Tests that the lock entity is correct."""
 
-    assert len(hass.states.async_all("lock")) == 0
+    entry = await setup_platform(hass, [Platform.LOCK])
 
-    await setup_platform(hass)
+    entity_entries = er.async_entries_for_config_entry(entity_registry, entry.entry_id)
 
-    assert hass.states.async_all("lock") == snapshot(name="all")
+    assert entity_entries
+    for entity_entry in entity_entries:
+        assert entity_entry == snapshot(name=f"{entity_entry.entity_id}-entry")
+        assert hass.states.get(entity_entry.entity_id) == snapshot(
+            name=f"{entity_entry.entity_id}-state"
+        )
 
     entity_id = "lock.test_lock"
 
