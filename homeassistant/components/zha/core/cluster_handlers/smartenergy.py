46acce07d8eb374cdc169f839c19ad78c5221c15
David Mulcahey <david.mulcahey@me.com>
2024-01-20 13:25:30 -0500

    shorten with rename due to clash

--- homeassistant/components/zha/core/cluster_handlers/smartenergy.py
+++ homeassistant/components/zha/core/cluster_handlers/smartenergy.py
@@ -67,59 +67,62 @@ class Messaging(ClusterHandler):
     """Messaging cluster handler."""
 
 
+SEAttrs = smartenergy.Metering.AttributeDefs
+
+
 @registries.ZIGBEE_CLUSTER_HANDLER_REGISTRY.register(smartenergy.Metering.cluster_id)
 class Metering(ClusterHandler):
     """Metering cluster handler."""
 
     REPORT_CONFIG = (
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.instantaneous_demand.name,
+            attr=SEAttrs.instantaneous_demand.name,
             config=REPORT_CONFIG_OP,
         ),
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.current_summ_delivered.name,
+            attr=SEAttrs.current_summ_delivered.name,
             config=REPORT_CONFIG_DEFAULT,
         ),
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.current_tier1_summ_delivered.name,
+            attr=SEAttrs.current_tier1_summ_delivered.name,
             config=REPORT_CONFIG_DEFAULT,
         ),
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.current_tier2_summ_delivered.name,
+            attr=SEAttrs.current_tier2_summ_delivered.name,
             config=REPORT_CONFIG_DEFAULT,
         ),
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.current_tier3_summ_delivered.name,
+            attr=SEAttrs.current_tier3_summ_delivered.name,
             config=REPORT_CONFIG_DEFAULT,
         ),
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.current_tier4_summ_delivered.name,
+            attr=SEAttrs.current_tier4_summ_delivered.name,
             config=REPORT_CONFIG_DEFAULT,
         ),
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.current_tier5_summ_delivered.name,
+            attr=SEAttrs.current_tier5_summ_delivered.name,
             config=REPORT_CONFIG_DEFAULT,
         ),
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.current_tier6_summ_delivered.name,
+            attr=SEAttrs.current_tier6_summ_delivered.name,
             config=REPORT_CONFIG_DEFAULT,
         ),
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.current_summ_received.name,
+            attr=SEAttrs.current_summ_received.name,
             config=REPORT_CONFIG_DEFAULT,
         ),
         AttrReportConfig(
-            attr=smartenergy.Metering.AttributeDefs.status.name,
+            attr=SEAttrs.status.name,
             config=REPORT_CONFIG_ASAP,
         ),
     )
     ZCL_INIT_ATTRS = {
-        smartenergy.Metering.AttributeDefs.demand_formatting.name: True,
-        smartenergy.Metering.AttributeDefs.divisor.name: True,
-        smartenergy.Metering.AttributeDefs.metering_device_type.name: True,
-        smartenergy.Metering.AttributeDefs.multiplier.name: True,
-        smartenergy.Metering.AttributeDefs.summation_formatting.name: True,
-        smartenergy.Metering.AttributeDefs.unit_of_measure.name: True,
+        SEAttrs.demand_formatting.name: True,
+        SEAttrs.divisor.name: True,
+        SEAttrs.metering_device_type.name: True,
+        SEAttrs.multiplier.name: True,
+        SEAttrs.summation_formatting.name: True,
+        SEAttrs.unit_of_measure.name: True,
     }
 
     metering_device_type = {
@@ -171,14 +174,12 @@ class Metering(ClusterHandler):
     @property
     def divisor(self) -> int:
         """Return divisor for the value."""
-        return self.cluster.get(smartenergy.Metering.AttributeDefs.divisor.name) or 1
+        return self.cluster.get(SEAttrs.divisor.name) or 1
 
     @property
     def device_type(self) -> str | int | None:
         """Return metering device type."""
-        dev_type = self.cluster.get(
-            smartenergy.Metering.AttributeDefs.metering_device_type.name
-        )
+        dev_type = self.cluster.get(SEAttrs.metering_device_type.name)
         if dev_type is None:
             return None
         return self.metering_device_type.get(dev_type, dev_type)
@@ -186,21 +187,14 @@ class Metering(ClusterHandler):
     @property
     def multiplier(self) -> int:
         """Return multiplier for the value."""
-        return self.cluster.get(smartenergy.Metering.AttributeDefs.multiplier.name) or 1
+        return self.cluster.get(SEAttrs.multiplier.name) or 1
 
     @property
     def status(self) -> int | None:
         """Return metering device status."""
-        if (
-            status := self.cluster.get(smartenergy.Metering.AttributeDefs.status.name)
-        ) is None:
+        if (status := self.cluster.get(SEAttrs.status.name)) is None:
             return None
-        if (
-            self.cluster.get(
-                smartenergy.Metering.AttributeDefs.metering_device_type.name
-            )
-            == 0
-        ):
+        if self.cluster.get(SEAttrs.metering_device_type.name) == 0:
             # Electric metering device type
             return self.DeviceStatusElectric(status)
         return self.DeviceStatusDefault(status)
@@ -208,18 +202,18 @@ class Metering(ClusterHandler):
     @property
     def unit_of_measurement(self) -> int:
         """Return unit of measurement."""
-        return self.cluster.get(smartenergy.Metering.AttributeDefs.unit_of_measure.name)
+        return self.cluster.get(SEAttrs.unit_of_measure.name)
 
     async def async_initialize_cluster_handler_specific(self, from_cache: bool) -> None:
         """Fetch config from device and updates format specifier."""
 
         fmting = self.cluster.get(
-            smartenergy.Metering.AttributeDefs.demand_formatting.name, 0xF9
+            SEAttrs.demand_formatting.name, 0xF9
         )  # 1 digit to the right, 15 digits to the left
         self._format_spec = self.get_formatting(fmting)
 
         fmting = self.cluster.get(
-            smartenergy.Metering.AttributeDefs.summation_formatting.name, 0xF9
+            SEAttrs.summation_formatting.name, 0xF9
         )  # 1 digit to the right, 15 digits to the left
         self._summa_format = self.get_formatting(fmting)
 
