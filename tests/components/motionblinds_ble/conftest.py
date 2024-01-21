ded91d2763cf4bd330d6f58d5d8b20d43372f636
starkillerOG <starkiller.og@gmail.com>
2024-01-21 22:14:03 +0100

    complete config flow tests

--- tests/components/motionblinds_ble/conftest.py
+++ tests/components/motionblinds_ble/conftest.py
@@ -1,21 +1,11 @@
 """Setup the MotionBlinds BLE tests."""
 
-import socket
-from unittest.mock import Mock, patch, AsyncMock
+from unittest.mock import AsyncMock, Mock, patch
 
 import pytest
 
-from homeassistant import config_entries, data_entry_flow
-from homeassistant.components import dhcp
-from homeassistant.components.motionblinds_ble import const
-from homeassistant.const import CONF_HOST
-from homeassistant.core import HomeAssistant
-
-from tests.common import MockConfigEntry
-
 TEST_MAC = "abcd"
 TEST_NAME = f"MOTION_{TEST_MAC.upper()}"
-TEST_BLIND_TYPE = const.MotionBlindType.ROLLER
 TEST_ADDRESS = "test_adress"
 
 
@@ -25,10 +15,10 @@ def motion_blinds_connect_fixture(enable_bluetooth):
     device = Mock()
     device.name = TEST_NAME
     device.address = TEST_ADDRESS
-    
+
     bleak_scanner = AsyncMock()
     bleak_scanner.discover.return_value = [device]
-    
+
     with patch(
         "homeassistant.components.motionblinds_ble.config_flow.bluetooth.async_scanner_count",
         return_value=1,
@@ -38,4 +28,4 @@ def motion_blinds_connect_fixture(enable_bluetooth):
     ), patch(
         "homeassistant.components.motionblinds_ble.async_setup_entry", return_value=True
     ):
-        yield
+        yield bleak_scanner, device
