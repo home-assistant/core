83142b469595489aa0caa2ba7af4124e3d98c8b7
G Johansson <goran.johansson@shiftit.se>
2024-01-21 16:42:32 +0000

    add platform

--- homeassistant/components/systemmonitor/__init__.py
+++ homeassistant/components/systemmonitor/__init__.py
@@ -4,7 +4,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-PLATFORMS = [Platform.SENSOR]
+PLATFORMS = [Platform.BINARY_SENSOR, Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
