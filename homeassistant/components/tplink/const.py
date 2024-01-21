49ed8829e6d61f34c51332feb2e54895106ef0cf
Teemu Rytilahti <tpr@iki.fi>
2024-01-21 12:33:39 +0100

    Remove more unused consts

--- homeassistant/components/tplink/const.py
+++ homeassistant/components/tplink/const.py
@@ -3,26 +3,9 @@ from __future__ import annotations
 
 from typing import Final
 
-import voluptuous as vol
-
-from homeassistant.const import (
-    CONF_AUTHENTICATION,
-    CONF_PASSWORD,
-    CONF_USERNAME,
-    Platform,
-)
+from homeassistant.const import Platform
 
 DOMAIN = "tplink"
-STORAGE_KEY: Final = DOMAIN
-STORAGE_VERSION: Final = 1
-STORAGE_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_AUTHENTICATION): {
-            vol.Required(CONF_USERNAME): str,
-            vol.Required(CONF_PASSWORD): str,
-        }
-    }
-)
 
 DISCOVERY_TIMEOUT = 5  # Home Assistant will complain if startup takes > 10s
 CONNECT_TIMEOUT = 5
@@ -33,10 +16,5 @@ ATTR_TODAY_ENERGY_KWH: Final = "today_energy_kwh"
 ATTR_TOTAL_ENERGY_KWH: Final = "total_energy_kwh"
 
 CONF_DEVICE_CONFIG: Final = "device_config"
-CONF_DIMMER: Final = "dimmer"
-CONF_LIGHT: Final = "light"
-CONF_STRIP: Final = "strip"
-CONF_SWITCH: Final = "switch"
-CONF_SENSOR: Final = "sensor"
 
 PLATFORMS: Final = [Platform.LIGHT, Platform.SENSOR, Platform.SWITCH]
