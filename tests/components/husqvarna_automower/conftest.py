bacbc9bcf8fca28b84227164cc3b9e0898c4afa2
Thomas55555 <thomas.protzner@gmail.com>
2024-01-22 20:43:23 +0000

    tests

--- tests/components/husqvarna_automower/conftest.py
+++ tests/components/husqvarna_automower/conftest.py
@@ -1,4 +1,5 @@
 """Test helpers for Husqvarna Automower."""
+import time
 from unittest.mock import patch
 
 from aioautomower.model import MowerAttributes, MowerList
@@ -34,7 +35,7 @@ def mock_config_entry(load_jwt_fixture) -> MockConfigEntry:
         domain=DOMAIN,
         title="Husqvarna Automower of Erika Mustermann",
         data={
-            "auth_implementation": "husqvarna_automower_433e5fdf_5129_452c_ba7f_fadce3213042",
+            "auth_implementation": DOMAIN,
             "token": {
                 "access_token": load_jwt_fixture,
                 "scope": "iam:read amc:api",
@@ -43,7 +44,7 @@ def mock_config_entry(load_jwt_fixture) -> MockConfigEntry:
                 "provider": "husqvarna",
                 "user_id": USER_ID,
                 "token_type": "Bearer",
-                "expires_at": 1697753347,
+                "expires_at": time.time() + 60 * 60 * 24,
             },
         },
         unique_id=USER_ID,
@@ -58,7 +59,7 @@ def load_mower_fixture():
 
 
 @pytest.fixture(scope="session")
-def mower_list_fixture(load_mower_fixture):
+def mower_list_fixture(load_mower_fixture) -> MowerList:
     """Generate a mower fixture object."""
     mower = load_mower_fixture
     mowers_list = MowerList(**mower)
@@ -69,13 +70,13 @@ def mower_list_fixture(load_mower_fixture):
 
 
 @pytest.fixture
-def activity():
+def activity() -> str:
     """Defaults value for activity."""
     return "PARKED_IN_CS"
 
 
 @pytest.fixture
-def state():
+def state() -> str:
     """Defaults value for state."""
     return "RESTRICTED"
 
@@ -83,9 +84,8 @@ def state():
 @pytest.fixture
 async def setup_entity(
     hass: HomeAssistant,
-    mower_list_fixture,
-    mock_config_entry,
-    load_token_decoded_fixture,
+    mower_list_fixture: MowerList,
+    mock_config_entry: MockConfigEntry,
     activity,
     state,
 ):
@@ -104,37 +104,17 @@ async def setup_entity(
         ),
     )
 
-    mower_data: MowerList = mower_list_fixture
+    mower_data = mower_list_fixture
     test_mower: MowerAttributes = mower_data[TEST_MOWER_ID]
     test_mower.mower.activity = activity
     test_mower.mower.state = state
 
-    mock_entry = MockConfigEntry(
-        version=1,
-        domain=DOMAIN,
-        title="Husqvarna Automower of Erika Mustermann",
-        data={
-            "auth_implementation": "husqvarna_automower",
-            "token": {
-                "access_token": load_jwt_fixture,
-                "scope": "iam:read amc:api",
-                "expires_in": 86399,
-                "refresh_token": "3012bc9f-7a65-4240-b817-9154ffdcc30f",
-                "provider": "husqvarna",
-                "user_id": USER_ID,
-                "token_type": "Bearer",
-                "expires_at": 10000000000000000,
-            },
-        },
-        unique_id=USER_ID,
-        entry_id="automower_test",
-    )
-    mock_entry.add_to_hass(hass)
+    mock_config_entry.add_to_hass(hass)
     with patch(
         "homeassistant.components.husqvarna_automower.coordinator.AutomowerDataUpdateCoordinator._async_update_data",
         return_value=mower_data,
     ):
-        await hass.config_entries.async_setup(mock_entry.entry_id)
+        await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
 
-    return mock_entry
+    return mock_config_entry
