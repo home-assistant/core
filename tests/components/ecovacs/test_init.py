8e7a17a9fafb807437f90c47ceb9d12257bc5ebe
Robert Resch <robert@resch.dev>
2024-01-22 13:36:26 +0100

    Add binary_sensor to Ecovacs (#108544)

--- tests/components/ecovacs/test_init.py
+++ tests/components/ecovacs/test_init.py
@@ -1,13 +1,16 @@
 """Test init of ecovacs."""
 from typing import Any
-from unittest.mock import AsyncMock, Mock
+from unittest.mock import AsyncMock, Mock, patch
 
 from deebot_client.exceptions import DeebotError, InvalidAuthenticationError
 import pytest
+from syrupy import SnapshotAssertion
 
 from homeassistant.components.ecovacs.const import DOMAIN
+from homeassistant.components.ecovacs.controller import EcovacsController
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 from homeassistant.setup import async_setup_component
 
 from .const import IMPORT_DATA
@@ -15,22 +18,31 @@ from .const import IMPORT_DATA
 from tests.common import MockConfigEntry
 
 
-@pytest.mark.usefixtures("mock_api_client")
+@pytest.mark.usefixtures("init_integration")
 async def test_load_unload_config_entry(
     hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
+    init_integration: MockConfigEntry,
 ) -> None:
     """Test loading and unloading the integration."""
-    mock_config_entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(mock_config_entry.entry_id)
-    await hass.async_block_till_done()
-
+    mock_config_entry = init_integration
     assert mock_config_entry.state is ConfigEntryState.LOADED
+    assert DOMAIN in hass.data
 
     await hass.config_entries.async_unload(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
+    assert DOMAIN not in hass.data
+
+
+@pytest.fixture
+def mock_api_client(mock_authenticator: Mock) -> Mock:
+    """Mock the API client."""
+    with patch(
+        "homeassistant.components.ecovacs.controller.ApiClient",
+        autospec=True,
+    ) as mock_api_client:
+        yield mock_api_client.return_value
 
 
 async def test_config_entry_not_ready(
@@ -83,3 +95,18 @@ async def test_async_setup_import(
     assert len(hass.config_entries.async_entries(DOMAIN)) == config_entries_expected
     assert mock_setup_entry.call_count == config_entries_expected
     assert mock_authenticator_authenticate.call_count == config_entries_expected
+
+
+async def test_devices_in_dr(
+    device_registry: dr.DeviceRegistry,
+    controller: EcovacsController,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test all devices are in the device registry."""
+    for device in controller.devices:
+        assert (
+            device_entry := device_registry.async_get_device(
+                identifiers={(DOMAIN, device.device_info.did)}
+            )
+        )
+        assert device_entry == snapshot(name=device.device_info.did)
