93328b2ceef9f270fadd56dd50a3f6a1c584d574
Jonas Fors Lellky <lellky@gmail.com>
2024-01-23 11:04:47 +0100

    Test switch on/off error recovery

--- tests/components/flexit_bacnet/test_switch.py
+++ tests/components/flexit_bacnet/test_switch.py
@@ -1,6 +1,8 @@
 """Tests for the Flexit Nordic (BACnet) switch entities."""
 from unittest.mock import AsyncMock
 
+from flexit_bacnet import DecodingError
+import pytest
 from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.switch import (
@@ -8,12 +10,15 @@ from homeassistant.components.switch import (
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
 )
-from homeassistant.const import ATTR_ENTITY_ID, Platform
+from homeassistant.const import ATTR_ENTITY_ID, STATE_OFF, STATE_ON, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import entity_registry as er
 
 from tests.common import MockConfigEntry
 from tests.components.flexit_bacnet import setup_with_selected_platforms
+
+ENTITY_ID = "switch.device_name_electric_heater"
 
 
 async def test_switches(
@@ -38,9 +43,6 @@ async def test_switches(
         )
 
 
-ENTITY_ID = "switch.device_name_electric_heater"
-
-
 async def test_switches_implementation(
     hass: HomeAssistant,
     snapshot: SnapshotAssertion,
@@ -51,6 +53,10 @@ async def test_switches_implementation(
     """Test that the switch can be turned on and off."""
 
     await setup_with_selected_platforms(hass, mock_config_entry, [Platform.SWITCH])
+    assert hass.states.get(ENTITY_ID) == snapshot(name=f"{ENTITY_ID}-state")
+
+    # Set to off
+    mock_flexit_bacnet.electric_heater = False
 
     await hass.services.async_call(
         SWITCH_DOMAIN,
@@ -61,7 +67,10 @@ async def test_switches_implementation(
 
     mocked_method = getattr(mock_flexit_bacnet, "disable_electric_heater")
     assert len(mocked_method.mock_calls) == 1
-    assert hass.states.get(ENTITY_ID) == snapshot(name=f"{ENTITY_ID}-state")
+    assert hass.states.get(ENTITY_ID).state == STATE_OFF
+
+    # Set to on
+    mock_flexit_bacnet.electric_heater = True
 
     await hass.services.async_call(
         SWITCH_DOMAIN,
@@ -72,4 +81,56 @@ async def test_switches_implementation(
 
     mocked_method = getattr(mock_flexit_bacnet, "enable_electric_heater")
     assert len(mocked_method.mock_calls) == 1
-    assert hass.states.get(ENTITY_ID) == snapshot(name=f"{ENTITY_ID}-state")
+    assert hass.states.get(ENTITY_ID).state == STATE_ON
+
+    # Error recovery, when turning off
+    mock_flexit_bacnet.disable_electric_heater.side_effect = DecodingError
+
+    with pytest.raises(HomeAssistantError):
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            SERVICE_TURN_OFF,
+            {ATTR_ENTITY_ID: ENTITY_ID},
+            blocking=True,
+        )
+
+    mocked_method = getattr(mock_flexit_bacnet, "disable_electric_heater")
+    assert len(mocked_method.mock_calls) == 2
+
+    mock_flexit_bacnet.disable_electric_heater.side_effect = None
+    mock_flexit_bacnet.electric_heater = False
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        SERVICE_TURN_OFF,
+        {ATTR_ENTITY_ID: ENTITY_ID},
+        blocking=True,
+    )
+
+    assert hass.states.get(ENTITY_ID).state == STATE_OFF
+
+    # Error recovery, when turning on
+    mock_flexit_bacnet.enable_electric_heater.side_effect = DecodingError
+
+    with pytest.raises(HomeAssistantError):
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            SERVICE_TURN_ON,
+            {ATTR_ENTITY_ID: ENTITY_ID},
+            blocking=True,
+        )
+
+    mocked_method = getattr(mock_flexit_bacnet, "enable_electric_heater")
+    assert len(mocked_method.mock_calls) == 2
+
+    mock_flexit_bacnet.enable_electric_heater.side_effect = None
+    mock_flexit_bacnet.electric_heater = True
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: ENTITY_ID},
+        blocking=True,
+    )
+
+    assert hass.states.get(ENTITY_ID).state == STATE_ON
