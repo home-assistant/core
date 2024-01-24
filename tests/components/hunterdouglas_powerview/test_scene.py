c5f81304739d8a2166fbeb7c19a287de612bd631
J. Nick Koston <nick@koston.org>
2024-01-24 13:43:51 -1000

    Add basic tests for powerview scenes

--- tests/components/hunterdouglas_powerview/test_scene.py
+++ tests/components/hunterdouglas_powerview/test_scene.py
@@ -6,12 +6,14 @@ from homeassistant.components.scene import DOMAIN as SCENE_DOMAIN, SERVICE_TURN_
 from homeassistant.const import STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
 
+from . import MOCK_MAC
+
 from tests.common import MockConfigEntry
 
 
 async def test_scenes(hass: HomeAssistant, mock_powerview_v2_hub: None) -> None:
     """Test the scenes."""
-    entry = MockConfigEntry(domain=DOMAIN, data={"host": "1.2.3.4"}, unique_id="1234")
+    entry = MockConfigEntry(domain=DOMAIN, data={"host": "1.2.3.4"}, unique_id=MOCK_MAC)
     entry.add_to_hass(hass)
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
