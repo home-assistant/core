21b5cde41a5b05adaffbfc9d3da8eda85945687a
Christopher Fenner <Christopher.Fenner@me.com>
2024-01-26 00:49:38 +0100

    increase config version entry

--- homeassistant/components/vicare/config_flow.py
+++ homeassistant/components/vicare/config_flow.py
@@ -43,7 +43,7 @@ USER_SCHEMA = REAUTH_SCHEMA.extend(
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for ViCare."""
 
-    VERSION = 1
+    VERSION = 2
     entry: config_entries.ConfigEntry | None
 
     async def async_step_user(
