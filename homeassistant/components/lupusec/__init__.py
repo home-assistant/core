900ec25ed3422f9213988a8a99dbe6f2093fd0b7
suaveolent <suaveolent@users.noreply.github.com>
2024-01-25 10:36:57 +0100

    added integration warning

--- homeassistant/components/lupusec/__init__.py
+++ homeassistant/components/lupusec/__init__.py
@@ -14,10 +14,14 @@ from homeassistant.const import (
     CONF_USERNAME,
     Platform,
 )
-from homeassistant.core import HomeAssistant
+from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
+from homeassistant.data_entry_flow import FlowResultType
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 from homeassistant.helpers.typing import ConfigType
+
+from .const import INTEGRATION_TITLE, ISSUE_PLACEHOLDER
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -47,6 +51,43 @@ PLATFORMS: list[Platform] = [
 ]
 
 
+async def handle_async_init_result(hass: HomeAssistant, domain: str, conf: dict):
+    """Handle the result of the async_init to issue deprecated warnings."""
+    flow = hass.config_entries.flow
+    result = await flow.async_init(domain, context={"source": SOURCE_IMPORT}, data=conf)
+
+    if (
+        result["type"] == FlowResultType.CREATE_ENTRY
+        or result["reason"] == "already_configured"
+    ):
+        async_create_issue(
+            hass,
+            HOMEASSISTANT_DOMAIN,
+            f"deprecated_yaml_{DOMAIN}",
+            breaks_in_ha_version="2024.7.0",
+            is_fixable=False,
+            issue_domain=DOMAIN,
+            severity=IssueSeverity.WARNING,
+            translation_key="deprecated_yaml",
+            translation_placeholders={
+                "domain": DOMAIN,
+                "integration_title": INTEGRATION_TITLE,
+            },
+        )
+    else:
+        async_create_issue(
+            hass,
+            DOMAIN,
+            f"deprecated_yaml_import_issue_${result['reason']}",
+            breaks_in_ha_version="2024.7.0",
+            is_fixable=False,
+            issue_domain=DOMAIN,
+            severity=IssueSeverity.WARNING,
+            translation_key=f"deprecated_yaml_import_issue_${result['reason']}",
+            translation_placeholders=ISSUE_PLACEHOLDER,
+        )
+
+
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the lupusec integration."""
 
@@ -56,11 +97,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     conf = config[DOMAIN]
     conf[CONF_HOST] = conf[CONF_IP_ADDRESS]
 
-    hass.async_create_task(
-        hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": SOURCE_IMPORT}, data=conf
-        )
-    )
+    hass.async_create_task(handle_async_init_result(hass, DOMAIN, conf))
 
     return True
 
--- homeassistant/components/lupusec/const.py
+++ homeassistant/components/lupusec/const.py
@@ -3,3 +3,4 @@
 DOMAIN = "lupusec"
 
 INTEGRATION_TITLE = "Lupus Electronics LUPUSEC"
+ISSUE_PLACEHOLDER = {"url": "/config/integrations/dashboard/add?domain=lupusec"}
