60ac2aecf7cd38f944fa42d664f54ea858f9660d
Jonas Fors Lellky <lellky@gmail.com>
2024-01-25 22:52:29 +0100

    Add service for shutting down the device

--- homeassistant/components/flexit_bacnet/__init__.py
+++ homeassistant/components/flexit_bacnet/__init__.py
@@ -1,9 +1,14 @@
 """The Flexit Nordic (BACnet) integration."""
 from __future__ import annotations
 
+import asyncio.exceptions
+
+from flexit_bacnet import VENTILATION_MODE_STOP, DecodingError
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_DEVICE_ID, Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.exceptions import HomeAssistantError
 
 from .const import DOMAIN
 from .coordinator import FlexitCoordinator
@@ -14,6 +19,8 @@ PLATFORMS: list[Platform] = [
     Platform.SENSOR,
     Platform.SWITCH,
 ]
+
+SET_VENTILATION_MODE_OFF_SERVICE_NAME = "set_ventilation_mode_off"
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -26,6 +33,22 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    # Register custom services
+    async def handle_set_ventilation_mode_off(service: ServiceCall) -> None:
+        """Handle the service call set_ventilation_mode_off."""
+        try:
+            await coordinator.device.set_ventilation_mode(VENTILATION_MODE_STOP)
+        except (asyncio.exceptions.TimeoutError, ConnectionError, DecodingError) as exc:
+            raise HomeAssistantError from exc
+        finally:
+            await coordinator.async_refresh()
+
+    hass.services.async_register(
+        DOMAIN,
+        SET_VENTILATION_MODE_OFF_SERVICE_NAME,
+        handle_set_ventilation_mode_off,
+    )
 
     return True
 
