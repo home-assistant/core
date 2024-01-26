267cbe0d26af2a3d16a1558cb69281613c4543ad
Brett <brett.whynot@gmail.com>
2024-01-26 00:59:41 +0000

    Revert test removal

--- tests/components/tessie/test_binary_sensors.py
+++ tests/components/tessie/test_binary_sensors.py
@@ -1,5 +1,7 @@
 """Test the Tessie binary sensor platform."""
+import pytest
 
+from homeassistant.components.tessie.binary_sensor import DESCRIPTIONS
 from homeassistant.const import STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
 
@@ -8,6 +10,7 @@ from .common import TEST_VEHICLE_STATE_ONLINE, setup_platform
 OFFON = [STATE_OFF, STATE_ON]
 
 
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
 async def test_binary_sensors(hass: HomeAssistant) -> None:
     """Tests that the binary sensor entities are correct."""
 
@@ -15,11 +18,17 @@ async def test_binary_sensors(hass: HomeAssistant) -> None:
 
     await setup_platform(hass)
 
-    assert len(hass.states.async_all("binary_sensor")) == 20
+    assert len(hass.states.async_all("binary_sensor")) == len(DESCRIPTIONS)
 
     state = hass.states.get("binary_sensor.test_battery_heater").state
     is_on = state == STATE_ON
     assert is_on == TEST_VEHICLE_STATE_ONLINE["charge_state"]["battery_heater_on"]
+
+    state = hass.states.get("binary_sensor.test_charging").state
+    is_on = state == STATE_ON
+    assert is_on == (
+        TEST_VEHICLE_STATE_ONLINE["charge_state"]["charging_state"] == "Charging"
+    )
 
     state = hass.states.get("binary_sensor.test_auto_seat_climate_left").state
     is_on = state == STATE_ON
