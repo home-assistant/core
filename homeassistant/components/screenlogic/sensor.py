7e1c0ac72ed1011e7ddfe0d19e9423bb2f9012dd
Kevin Worrel <37058192+dieselrabbit@users.noreply.github.com>
2024-01-27 20:33:56 +0000

    IntelliChem lsi values as numbers

--- homeassistant/components/screenlogic/sensor.py
+++ homeassistant/components/screenlogic/sensor.py
@@ -135,32 +135,12 @@ SUPPORTED_INTELLICHEM_SENSORS = [
     ScreenLogicPushSensorDescription(
         subscription_code=CODE.CHEMISTRY_CHANGED,
         data_root=(DEVICE.INTELLICHEM, GROUP.CONFIGURATION),
-        key=VALUE.CALCIUM_HARDNESS,
-    ),
-    ScreenLogicPushSensorDescription(
-        subscription_code=CODE.CHEMISTRY_CHANGED,
-        data_root=(DEVICE.INTELLICHEM, GROUP.CONFIGURATION),
-        key=VALUE.CYA,
-    ),
-    ScreenLogicPushSensorDescription(
-        subscription_code=CODE.CHEMISTRY_CHANGED,
-        data_root=(DEVICE.INTELLICHEM, GROUP.CONFIGURATION),
         key=VALUE.ORP_SETPOINT,
     ),
     ScreenLogicPushSensorDescription(
         subscription_code=CODE.CHEMISTRY_CHANGED,
         data_root=(DEVICE.INTELLICHEM, GROUP.CONFIGURATION),
         key=VALUE.PH_SETPOINT,
-    ),
-    ScreenLogicPushSensorDescription(
-        subscription_code=CODE.CHEMISTRY_CHANGED,
-        data_root=(DEVICE.INTELLICHEM, GROUP.CONFIGURATION),
-        key=VALUE.TOTAL_ALKALINITY,
-    ),
-    ScreenLogicPushSensorDescription(
-        subscription_code=CODE.CHEMISTRY_CHANGED,
-        data_root=(DEVICE.INTELLICHEM, GROUP.CONFIGURATION),
-        key=VALUE.SALT_TDS_PPM,
     ),
     ScreenLogicPushSensorDescription(
         subscription_code=CODE.CHEMISTRY_CHANGED,
@@ -218,6 +198,13 @@ SUPPORTED_SCG_SENSORS = [
         data_root=(DEVICE.SCG, GROUP.CONFIGURATION),
         key=VALUE.SUPER_CHLOR_TIMER,
     ),
+]
+
+REMOVED_SENSORS = [
+    (DEVICE.INTELLICHEM, GROUP.CONFIGURATION, VALUE.CALCIUM_HARDNESS),
+    (DEVICE.INTELLICHEM, GROUP.CONFIGURATION, VALUE.CYA),
+    (DEVICE.INTELLICHEM, GROUP.CONFIGURATION, VALUE.TOTAL_ALKALINITY),
+    (DEVICE.INTELLICHEM, GROUP.CONFIGURATION, VALUE.SALT_TDS_PPM),
 ]
 
 
@@ -288,6 +275,9 @@ async def async_setup_entry(
                 scg_sensor_description, entity_category=EntityCategory.DIAGNOSTIC
             )
             entities.append(ScreenLogicSensor(coordinator, scg_sensor_description))
+
+    for sensor_data_path in REMOVED_SENSORS:
+        cleanup_excluded_entity(coordinator, DOMAIN, sensor_data_path)
 
     async_add_entities(entities)
 
