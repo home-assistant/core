4f8702d734dc0a25d58ab4305c287314e7638f9d
Joost Lekkerkerker <joostlek@outlook.com>
2024-01-27 22:24:35 +0100

    Add options flow to Analytics Insights (#108716)
    * Add options flow to Analytics Insights
    
    * Fix options flow function

--- homeassistant/components/analytics_insights/__init__.py
+++ homeassistant/components/analytics_insights/__init__.py
@@ -46,6 +46,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(update_listener))
 
     return True
 
@@ -56,3 +57,8 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
