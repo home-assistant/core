8679e0ee33fe705dc2386387e79087912afb86e5
Oleg Fiksel <github-oleg-fiksel@spam.fiksel.info>
2024-01-27 19:31:06 +0000

    Refactor send_message test using parametrize

--- tests/components/matrix/test_send_message.py
+++ tests/components/matrix/test_send_message.py
@@ -7,6 +7,7 @@ from homeassistant.components.matrix import (
     MatrixBot,
 )
 from homeassistant.components.matrix.const import (
+    FORMAT_TEXT,
     FORMAT_HTML,
     FORMAT_NOTICE,
     SERVICE_SEND_MESSAGE,
@@ -15,10 +16,75 @@ from homeassistant.components.notify import ATTR_DATA, ATTR_MESSAGE, ATTR_TARGET
 from homeassistant.core import HomeAssistant
 
 from tests.components.matrix.conftest import TEST_BAD_ROOM, TEST_JOINABLE_ROOMS
+import pytest
 
 
+@pytest.mark.parametrize(
+    "test_name, test_params",
+    [
+        (
+            "Text message",
+            {
+                "data": {
+                    ATTR_MESSAGE: "Test text message",
+                    ATTR_DATA: {ATTR_FORMAT: FORMAT_TEXT},
+                    ATTR_TARGET: list(TEST_JOINABLE_ROOMS.keys()),
+                },
+                "expected_result": {
+                    "target_rooms": list(TEST_JOINABLE_ROOMS.keys()),
+                    "message_type": "m.room.message",
+                    "content": {"msgtype": "m.text", "body": "Test text message"},
+                },
+            },
+        ),
+        (
+            "HTML message",
+            {
+                "data": {
+                    ATTR_MESSAGE: "Test <b>html</b> message",
+                    ATTR_DATA: {ATTR_FORMAT: FORMAT_HTML},
+                    ATTR_TARGET: list(TEST_JOINABLE_ROOMS.keys()),
+                },
+                "expected_result": {
+                    "target_rooms": list(TEST_JOINABLE_ROOMS.keys()),
+                    "message_type": "m.room.message",
+                    "content": {
+                        "msgtype": "m.text",
+                        "body": "Test <b>html</b> message",
+                        "format": "org.matrix.custom.html",
+                        "formatted_body": "Test <b>html</b> message",
+                    },
+                },
+            },
+        ),
+        (
+            "Bot (notice) message",
+            {
+                "data": {
+                    ATTR_MESSAGE: "Test bot (notice) message",
+                    ATTR_DATA: {ATTR_FORMAT: FORMAT_NOTICE},
+                    ATTR_TARGET: list(TEST_JOINABLE_ROOMS.keys()),
+                },
+                "expected_result": {
+                    "target_rooms": list(TEST_JOINABLE_ROOMS.keys()),
+                    "message_type": "m.room.message",
+                    "content": {
+                        "msgtype": "m.notice",
+                        "body": "Test bot (notice) message",
+                    },
+                },
+            },
+        ),
+    ],
+)
 async def test_send_message(
-    hass: HomeAssistant, matrix_bot: MatrixBot, image_path, matrix_events, caplog
+    hass: HomeAssistant,
+    matrix_bot: MatrixBot,
+    image_path,
+    matrix_events,
+    caplog,
+    test_name,
+    test_params,
 ):
     """Test the send_message service."""
 
@@ -26,76 +92,36 @@ async def test_send_message(
     assert len(matrix_events) == 0
     await matrix_bot._login()
 
-    # Send a message without an attached image.
-    matrix_bot._handle_multi_room_send.reset_mock()
-    data = {ATTR_MESSAGE: "Test message", ATTR_TARGET: list(TEST_JOINABLE_ROOMS)}
+    print(test_params)
     await hass.services.async_call(
-        MATRIX_DOMAIN, SERVICE_SEND_MESSAGE, data, blocking=True
+        MATRIX_DOMAIN, SERVICE_SEND_MESSAGE, test_params["data"], blocking=True
     )
 
-    expected_data = {
-        "target_rooms": list(TEST_JOINABLE_ROOMS.keys()),
-        "message_type": "m.room.message",
-        "content": {"msgtype": "m.text", "body": data[ATTR_MESSAGE]},
-    }
-    matrix_bot._handle_multi_room_send.assert_called_once_with(**expected_data)
+    matrix_bot._handle_multi_room_send.assert_called_once_with(
+        **test_params["expected_result"]
+    )
 
     for room_alias_or_id in TEST_JOINABLE_ROOMS:
         assert f"Message delivered to room '{room_alias_or_id}'" in caplog.messages
 
-    # Send an HTML message without an attached image.
-    matrix_bot._handle_multi_room_send.reset_mock()
+
+async def test_send_image(
+    hass: HomeAssistant, matrix_bot: MatrixBot, image_path, matrix_events, caplog
+):
+    """Test send a message with an attached image"""
+
+    await hass.async_start()
+    assert len(matrix_events) == 0
+    await matrix_bot._login()
+
     data = {
         ATTR_MESSAGE: "Test <b>html</b> message",
         ATTR_TARGET: list(TEST_JOINABLE_ROOMS),
-        ATTR_DATA: {ATTR_FORMAT: FORMAT_HTML},
+        ATTR_DATA: {ATTR_IMAGES: [image_path.name]},
     }
     await hass.services.async_call(
         MATRIX_DOMAIN, SERVICE_SEND_MESSAGE, data, blocking=True
     )
-
-    expected_data = {
-        "target_rooms": list(TEST_JOINABLE_ROOMS.keys()),
-        "message_type": "m.room.message",
-        "content": {
-            "msgtype": "m.text",
-            "body": data[ATTR_MESSAGE],
-            "format": "org.matrix.custom.html",
-            "formatted_body": data[ATTR_MESSAGE],
-        },
-    }
-    matrix_bot._handle_multi_room_send.assert_called_once_with(**expected_data)
-
-    for room_alias_or_id in TEST_JOINABLE_ROOMS:
-        assert f"Message delivered to room '{room_alias_or_id}'" in caplog.messages
-
-    # Send a message with an attached image.
-    data[ATTR_DATA] = {ATTR_IMAGES: [image_path.name]}
-    await hass.services.async_call(
-        MATRIX_DOMAIN, SERVICE_SEND_MESSAGE, data, blocking=True
-    )
-
-    for room_alias_or_id in TEST_JOINABLE_ROOMS:
-        assert f"Message delivered to room '{room_alias_or_id}'" in caplog.messages
-
-    # Send bot notice (m.notice) message.
-    matrix_bot._handle_multi_room_send.reset_mock()
-    data = {
-        ATTR_MESSAGE: "Test bot (notice) message",
-        ATTR_TARGET: list(TEST_JOINABLE_ROOMS),
-        ATTR_DATA: {ATTR_FORMAT: FORMAT_NOTICE},
-    }
-
-    await hass.services.async_call(
-        MATRIX_DOMAIN, SERVICE_SEND_MESSAGE, data, blocking=True
-    )
-
-    expected_data = {
-        "target_rooms": list(TEST_JOINABLE_ROOMS.keys()),
-        "message_type": "m.room.message",
-        "content": {"msgtype": "m.notice", "body": data[ATTR_MESSAGE]},
-    }
-    matrix_bot._handle_multi_room_send.assert_called_once_with(**expected_data)
 
     for room_alias_or_id in TEST_JOINABLE_ROOMS:
         assert f"Message delivered to room '{room_alias_or_id}'" in caplog.messages
