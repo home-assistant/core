85a510596c599719c9cf55f1d233f4f29ff33e8a
Thomas Kistler <thomas@uyei-kistler.com>
2024-01-27 21:50:31 +0000

    Make coordinator more robust

--- homeassistant/components/ambient_network/__init__.py
+++ homeassistant/components/ambient_network/__init__.py
@@ -18,7 +18,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     api = OpenAPI()
     coordinator = AmbientNetworkDataUpdateCoordinator(hass, api)
-    await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
