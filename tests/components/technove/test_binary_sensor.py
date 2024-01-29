91e7e5e01a4a6b4d24c9ef7150870fc5ae021de2
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2024-01-29 05:56:57 -0500

    Add binary sensors to TechnoVE integration (#108938)
    * Add binary sensors to TechnoVE integration
    
    * Add unit tests for TechnoVE binary sensors
    
    * Implement PR feedback for TechnoVE
    
    * Limit to appropriate sensors in TechnoVE tests
    
    * Removed leftover code
    
    * Implement feedback in TechnoVE PR #108938

--- tests/components/technove/test_binary_sensor.py
+++ tests/components/technove/test_binary_sensor.py
@@ -1,13 +1,13 @@
-"""Tests for the TechnoVE sensor platform."""
+"""Tests for the TechnoVE binary sensor platform."""
 from datetime import timedelta
 from unittest.mock import MagicMock
 
 from freezegun.api import FrozenDateTimeFactory
 import pytest
 from syrupy import SnapshotAssertion
-from technove import Status, TechnoVEError
+from technove import TechnoVEError
 
-from homeassistant.const import STATE_UNAVAILABLE, STATE_UNKNOWN, Platform
+from homeassistant.const import STATE_ON, STATE_UNAVAILABLE, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
@@ -23,8 +23,11 @@ async def test_sensors(
     mock_config_entry: MockConfigEntry,
     entity_registry: er.EntityRegistry,
 ) -> None:
-    """Test the creation and values of the TechnoVE sensors."""
-    await setup_with_selected_platforms(hass, mock_config_entry, [Platform.SENSOR])
+    """Test the creation and values of the TechnoVE binary sensors."""
+    await setup_with_selected_platforms(
+        hass, mock_config_entry, [Platform.BINARY_SENSOR]
+    )
+
     entity_entries = er.async_entries_for_config_entry(
         entity_registry, mock_config_entry.entry_id
     )
@@ -39,16 +42,13 @@ async def test_sensors(
 
 @pytest.mark.parametrize(
     "entity_id",
-    (
-        "sensor.technove_station_signal_strength",
-        "sensor.technove_station_wi_fi_network_name",
-    ),
+    ("binary_sensor.technove_station_static_ip",),
 )
 @pytest.mark.usefixtures("init_integration")
-async def test_disabled_by_default_sensors(
+async def test_disabled_by_default_binary_sensors(
     hass: HomeAssistant, entity_registry: er.EntityRegistry, entity_id: str
 ) -> None:
-    """Test the disabled by default TechnoVE sensors."""
+    """Test the disabled by default TechnoVE binary sensors."""
     assert hass.states.get(entity_id) is None
 
     assert (entry := entity_registry.async_get(entity_id))
@@ -56,36 +56,16 @@ async def test_disabled_by_default_sensors(
     assert entry.disabled_by is er.RegistryEntryDisabler.INTEGRATION
 
 
-@pytest.mark.usefixtures("entity_registry_enabled_by_default")
-async def test_no_wifi_support(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_technove: MagicMock,
-) -> None:
-    """Test missing Wi-Fi information from TechnoVE device."""
-    # Remove Wi-Fi info
-    device = mock_technove.update.return_value
-    device.info.network_ssid = None
-
-    # Setup
-    mock_config_entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(mock_config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    assert (state := hass.states.get("sensor.technove_station_wi_fi_network_name"))
-    assert state.state == STATE_UNKNOWN
-
-
 @pytest.mark.usefixtures("init_integration")
-async def test_sensor_update_failure(
+async def test_binary_sensor_update_failure(
     hass: HomeAssistant,
     mock_technove: MagicMock,
     freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test coordinator update failure."""
-    entity_id = "sensor.technove_station_status"
+    entity_id = "binary_sensor.technove_station_charging"
 
-    assert hass.states.get(entity_id).state == Status.PLUGGED_CHARGING.value
+    assert hass.states.get(entity_id).state == STATE_ON
 
     mock_technove.update.side_effect = TechnoVEError("Test error")
     freezer.tick(timedelta(minutes=5, seconds=1))
