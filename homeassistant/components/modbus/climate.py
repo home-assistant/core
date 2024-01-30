14464a73673c217d25d6e1d51c4de57578296a06
G Johansson <goran.johansson@shiftit.se>
2024-01-30 14:58:39 +0000

    Add TURN_ON/OFF ClimateEntityFeature for Modbus

--- homeassistant/components/modbus/climate.py
+++ homeassistant/components/modbus/climate.py
@@ -97,7 +97,11 @@ async def async_setup_platform(
 class ModbusThermostat(BaseStructPlatform, RestoreEntity, ClimateEntity):
     """Representation of a Modbus Thermostat."""
 
-    _attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
+    _attr_supported_features = (
+        ClimateEntityFeature.TARGET_TEMPERATURE
+        | ClimateEntityFeature.TURN_OFF
+        | ClimateEntityFeature.TURN_ON
+    )
 
     def __init__(
         self,
