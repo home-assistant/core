4ba99581fac8d3f74c876d45111e1d9899081b07
mib1185 <mail@mib85.de>
2024-01-30 16:31:29 +0000

    check entry source if imported

--- homeassistant/components/proximity/__init__.py
+++ homeassistant/components/proximity/__init__.py
@@ -146,7 +146,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    if entry.data.get(CONF_NAME):
+    if entry.source == SOURCE_IMPORT:
         await _async_setup_legacy(hass, entry, coordinator)
 
     await hass.config_entries.async_forward_entry_setups(entry, [Platform.SENSOR])
