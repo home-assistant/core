5fb560456638ad2bfc298d599b9013e2bb144507
tr4nt0r <manni@zapto.de>
2024-01-31 08:05:35 +0000

    Add notification service for Bring component

--- homeassistant/components/bring/const.py
+++ homeassistant/components/bring/const.py
@@ -1,3 +1,12 @@
 """Constants for the Bring! integration."""
 
+from typing import Final
+
 DOMAIN = "bring"
+
+ATTR_SENDER: Final = "sender"
+ATTR_ITEM_NAME: Final = "item_name"
+ATTR_NOTIFICATION_TYPE: Final = "notification_type"
+ATTR_LIST: Final = "bring_list"
+
+SERVICE_PUSH_NOTIFICATION = "push_notification"
