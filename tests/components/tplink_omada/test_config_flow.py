565885869dc1f50f3739e728112043357b5119d9
Joostlek <joostlek@outlook.com>
2024-01-31 22:17:34 +0100

    Fix tests

--- tests/components/tplink_omada/test_config_flow.py
+++ tests/components/tplink_omada/test_config_flow.py
@@ -401,7 +401,7 @@ async def test_create_omada_client_with_ip_creates_clientsession(
             hass,
             {
                 "host": "10.10.10.10",
-                "verify_ssl": True,  # Verify is meaningless for IP
+                "verify_ssl": True,
                 "username": "test-username",
                 "password": "test-password",
             },
