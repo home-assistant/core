c2b2bdca7045b9e0fc88f76544488446ffc15a0c
sdb9696 <steven.beth@gmail.com>
2024-01-31 09:26:59 +0000

    Call first_refresh on notifications coordinator

--- homeassistant/components/ring/__init__.py
+++ homeassistant/components/ring/__init__.py
@@ -42,14 +42,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
     ring = ring_doorbell.Ring(auth)
 
-    coordinator = RingDataCoordinator(hass, ring)
-    await coordinator.async_config_entry_first_refresh()
+    devices_coordinator = RingDataCoordinator(hass, ring)
+    notifications_coordinator = RingNotificationsCoordinator(hass, ring)
+    await devices_coordinator.async_config_entry_first_refresh()
+    await notifications_coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
         RING_API: ring,
         RING_DEVICES: ring.devices(),
-        RING_DEVICES_COORDINATOR: coordinator,
-        RING_NOTIFICATIONS_COORDINATOR: RingNotificationsCoordinator(hass, ring),
+        RING_DEVICES_COORDINATOR: devices_coordinator,
+        RING_NOTIFICATIONS_COORDINATOR: notifications_coordinator,
     }
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
