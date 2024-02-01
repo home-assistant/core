825497b26a2d5e7109b7cc98e0525cbced8e6a95
Caius-Bonus <123886836+Caius-Bonus@users.noreply.github.com>
2024-02-01 17:56:57 +0100

    add note and fix enumsensor

--- homeassistant/components/zha/sensor.py
+++ homeassistant/components/zha/sensor.py
@@ -254,7 +254,8 @@ class EnumSensor(Sensor):
     def formatter(self, value: int) -> str | None:
         """Use name of enum."""
         assert self._enum is not None
-        return self._enum(value).name
+        # TODO: Change all enums to uncapitalized snake_case
+        return self._enum(value).name.lower()
 
 
 @MULTI_MATCH(
