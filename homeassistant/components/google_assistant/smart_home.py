b5df7debfb1d85323a067510e12bb511610e780f
Erik <erik@montnemery.com>
2024-02-02 12:19:36 +0100

    Tweak

--- homeassistant/components/google_assistant/smart_home.py
+++ homeassistant/components/google_assistant/smart_home.py
@@ -18,7 +18,7 @@ from .const import (
     EVENT_QUERY_RECEIVED,
     EVENT_SYNC_RECEIVED,
 )
-from .data_redaction import async_redact_sync
+from .data_redaction import async_redact_sync_msg
 from .error import SmartHomeError
 from .helpers import GoogleEntity, RequestData, async_get_entities
 
@@ -119,7 +119,7 @@ async def async_devices_sync(
     devices = await async_devices_sync_response(hass, data.config, agent_user_id)
     response = create_sync_response(agent_user_id, devices)
 
-    _LOGGER.debug("Syncing entities response: %s", async_redact_sync(response))
+    _LOGGER.debug("Syncing entities response: %s", async_redact_sync_msg(response))
 
     return response
 
