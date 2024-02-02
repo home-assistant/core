84a6185dc3e0032680d6c43ecdf31bf37cf649cc
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2024-02-02 12:07:23 +0000

    Fix test_deprecated_constants

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -1222,6 +1222,7 @@ class UnitOfSpeed(StrEnum):
     MILES_PER_HOUR = "mph"
 
 
+_DEPRECATED_SPEED_BEAUFORT: Final = DeprecatedConstantEnum(UnitOfSpeed.BEAUFORT, "0.0")
 _DEPRECATED_SPEED_FEET_PER_SECOND: Final = DeprecatedConstantEnum(
     UnitOfSpeed.FEET_PER_SECOND, "2025.1"
 )
