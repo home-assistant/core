5a71c171cc0da6aa027c1c9872554f7289acd272
Niels Perfors <niels.perfors1987@gmail.com>
2024-02-03 14:14:18 +0000

    update tests

--- tests/components/wireguard/test_coordinator.py
+++ tests/components/wireguard/test_coordinator.py
@@ -1,6 +1,8 @@
 """Test the WireGuardUpdateCoordinator."""
 from unittest.mock import patch
 
+import requests
+
 from homeassistant.components.wireguard.const import DEFAULT_HOST, DOMAIN
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
@@ -10,22 +12,39 @@ from .conftest import mocked_requests
 from tests.common import MockConfigEntry
 
 
-async def test_wireguard_data_update_coordinator(hass: HomeAssistant) -> None:
+async def test_coordinator_update_success(hass: HomeAssistant) -> None:
     """Test WireGuardUpdateCoordinator."""
-    with patch("requests.get", side_effect=mocked_requests):
-        config_entry = MockConfigEntry(
-            domain=DOMAIN,
-            unique_id="UNIQUE_TEST_ID",
-            data={CONF_HOST: DEFAULT_HOST},
-        )
-        config_entry.add_to_hass(hass)
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="UNIQUE_TEST_ID",
+        data={CONF_HOST: DEFAULT_HOST},
+    )
+    config_entry.add_to_hass(hass)
 
+    with patch("requests.get", side_effect=mocked_requests):
         await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
         coordinator = hass.data[DOMAIN][config_entry.entry_id]
         await coordinator.async_refresh()
         await hass.async_block_till_done()
 
-    state = hass.states.get("binary_sensor.dummy_connectivity")
-    assert state is not None
-    assert state.state == "off"
+    assert coordinator.last_update_success
+
+
+async def test_coordinator_update_failed(hass: HomeAssistant) -> None:
+    """Test WireGuardUpdateCoordinator."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="UNIQUE_TEST_ID",
+        data={CONF_HOST: DEFAULT_HOST},
+    )
+    config_entry.add_to_hass(hass)
+
+    with patch("requests.get", side_effect=requests.RequestException):
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+        coordinator = hass.data[DOMAIN][config_entry.entry_id]
+        await coordinator.async_refresh()
+        await hass.async_block_till_done()
+
+    assert not coordinator.last_update_success
