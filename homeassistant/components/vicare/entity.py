83e3398fa2d88c63fd394c93e4499e5edfedefe4
Christopher Fenner <Christopher.Fenner@me.com>
2024-02-04 13:22:20 +0100

    use correct serial

--- homeassistant/components/vicare/entity.py
+++ homeassistant/components/vicare/entity.py
@@ -1,6 +1,9 @@
 """Entities for the ViCare integration."""
 from PyViCare.PyViCareDevice import Device as PyViCareDevice
 from PyViCare.PyViCareDeviceConfig import PyViCareDeviceConfig
+from PyViCare.PyViCareHeatingDevice import (
+    HeatingDeviceWithComponent as PyViCareHeatingDeviceComponent,
+)
 
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
@@ -17,19 +20,21 @@ class ViCareEntity(Entity):
         self,
         device_config: PyViCareDeviceConfig,
         device: PyViCareDevice,
+        component: PyViCareHeatingDeviceComponent,
         unique_id_suffix: str,
     ) -> None:
         """Initialize the entity."""
-        self._api = device
-
-        self._attr_unique_id = f"{device_config.getConfig().serial}-{unique_id_suffix}"
+        self._api: PyViCareDevice | PyViCareHeatingDeviceComponent = (
+            component if component else device
+        )
+        self._attr_unique_id = f"{device.getSerial()}-{unique_id_suffix}"
         # valid for compressors, circuits, burners (HeatingDeviceWithComponent)
-        if hasattr(device, "id"):
-            self._attr_unique_id += f"-{device.id}"
+        if component:
+            self._attr_unique_id += f"-{component.id}"
 
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, device_config.getConfig().serial)},
-            serial_number=device_config.getConfig().serial,
+            identifiers={(DOMAIN, device.getSerial())},
+            serial_number=device.getSerial(),
             name=device_config.getModel(),
             manufacturer="Viessmann",
             model=device_config.getModel(),
