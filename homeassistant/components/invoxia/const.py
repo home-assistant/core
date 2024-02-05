d6724c83159c3f883fc0c6f9742f37187fb887b5
Ezlo Picori <ezlo@protonmail.com>
2024-02-05 20:34:10 +0000

    invoxia: convert DATE_UPDATE_INTERVAL definition to minutes

--- homeassistant/components/invoxia/const.py
+++ homeassistant/components/invoxia/const.py
@@ -11,7 +11,7 @@ ATTRIBUTION = "Data provided by an unofficial client for Invoxia API."
 
 CLIENT = "client"
 
-DATA_UPDATE_INTERVAL = timedelta(seconds=240)
+DATA_UPDATE_INTERVAL = timedelta(minutes=4)
 
 DOMAIN = "invoxia"
 
