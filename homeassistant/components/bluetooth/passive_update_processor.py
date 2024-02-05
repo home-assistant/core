a4859685a2a9904ece42f7d8f061f0548ad9c8e0
Joost Lekkerkerker <joostlek@outlook.com>
2024-02-05 23:37:51 +0100

    Update homeassistant/components/bluetooth/passive_update_processor.py
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/bluetooth/passive_update_processor.py
+++ homeassistant/components/bluetooth/passive_update_processor.py
@@ -649,8 +649,8 @@ class PassiveBluetoothProcessorEntity(Entity, Generic[_PassiveBluetoothDataProce
             self._attr_device_info[ATTR_NAME] = self.processor.coordinator.name
         if device_id is None:
             self._attr_device_info[ATTR_CONNECTIONS] = {(CONNECTION_BLUETOOTH, address)}
-        if processor.entity_names.get(entity_key) is not None:
-            self._attr_name = processor.entity_names[entity_key]
+        if (name := processor.entity_names.get(entity_key)) is not None:
+            self._attr_name = name
 
     @property
     def available(self) -> bool:
