5c0c266d5c84cf8399847968867b4f2621d658fe
Niels Perfors <niels.perfors1987@gmail.com>
2024-02-07 22:35:50 +0000

    remove binary_sensor

--- homeassistant/components/wireguard/__init__.py
+++ homeassistant/components/wireguard/__init__.py
@@ -8,7 +8,7 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 from .coordinator import WireGuardUpdateCoordinator
 
-PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.SENSOR]
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
