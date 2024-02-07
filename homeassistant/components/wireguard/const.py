939cd8f30e3094c9ac464ffad00ac104b0776361
Niels Perfors <niels.perfors1987@gmail.com>
2024-02-07 22:42:20 +0000

    rm unused const

--- homeassistant/components/wireguard/const.py
+++ homeassistant/components/wireguard/const.py
@@ -7,5 +7,3 @@ LOGGER = logging.getLogger(__package__)
 DEFAULT_SCAN_INTERVAL = timedelta(seconds=30)
 DEFAULT_HOST = "http://a0d7b954-wireguard"
 DEFAULT_NAME = "WireGuard"
-
-ATTR_LATEST_HANDSHAKE = "latest_handshake"
