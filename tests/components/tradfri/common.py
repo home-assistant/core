12388f13fed39b74a4222860e3ababe25510f295
Martin Hjelmare <marhje52@gmail.com>
2024-02-07 13:13:29 +0100

    Refactor tradfri tests

--- tests/components/tradfri/common.py
+++ tests/components/tradfri/common.py
@@ -1,9 +1,12 @@
 """Common tools used for the Tradfri test suite."""
 from copy import deepcopy
+from dataclasses import dataclass
 from typing import Any
-from unittest.mock import Mock
 
+from pytradfri.command import Command
+from pytradfri.const import ATTR_ID
 from pytradfri.device import Device
+from pytradfri.gateway import Gateway
 
 from homeassistant.components import tradfri
 from homeassistant.core import HomeAssistant
@@ -13,7 +16,15 @@ from . import GATEWAY_ID
 from tests.common import MockConfigEntry
 
 
-async def setup_integration(hass):
+@dataclass
+class CommandStore:
+    """Store commands and command responses for the API."""
+
+    sent_commands: list[Command]
+    mock_responses: dict[str, Any]
+
+
+async def setup_integration(hass: HomeAssistant) -> MockConfigEntry:
     """Load the Tradfri integration with a mock gateway."""
     entry = MockConfigEntry(
         domain=tradfri.DOMAIN,
@@ -32,6 +43,66 @@ async def setup_integration(hass):
     return entry
 
 
+def register_device(
+    command_store: CommandStore, gateway: Gateway, device_response: dict[str, Any]
+) -> None:
+    """Register device response."""
+    get_devices_command = gateway.get_devices()
+    register_response(command_store, get_devices_command, [device_response[ATTR_ID]])
+    get_device_command = gateway.get_device(device_response[ATTR_ID])
+    register_response(command_store, get_device_command, device_response)
+
+
+def register_response(
+    command_store: CommandStore, command: Command, response: Any
+) -> None:
+    """Register command response."""
+    command_store.mock_responses[command.path_str] = response
+
+
+def process_command(command_store: CommandStore, command: Command) -> Any | None:
+    """Process command."""
+    response = command_store.mock_responses.get(command.path_str)
+    if response is None or command.process_result is None:
+        return None
+    return command.process_result(response)
+
+
+async def trigger_observe_callback(
+    hass: HomeAssistant,
+    command_store: CommandStore,
+    device: Device,
+    new_device_state: dict[str, Any] | None = None,
+) -> None:
+    """Trigger the observe callback."""
+    observe_command = next(
+        (
+            command
+            for command in command_store.sent_commands
+            if command.path == device.path and command.observe
+        ),
+        None,
+    )
+    assert observe_command
+
+    device_path = "/".join(str(v) for v in device.path)
+    device_state = deepcopy(device.raw)
+
+    # Create a default observed state based on the sent commands.
+    for command in command_store.sent_commands:
+        if (data := command.data) is None or command.path_str != device_path:
+            continue
+        device_state = modify_state(device_state, data)
+
+    # Allow the test to override the default observed state.
+    if new_device_state is not None:
+        device_state = modify_state(device_state, new_device_state)
+
+    observe_command.process_result(device_state)
+
+    await hass.async_block_till_done()
+
+
 def modify_state(
     state: dict[str, Any], partial_state: dict[str, Any]
 ) -> dict[str, Any]:
@@ -46,31 +117,3 @@ def modify_state(
             state[key] = value
 
     return state
-
-
-async def trigger_observe_callback(
-    hass: HomeAssistant,
-    mock_gateway: Mock,
-    device: Device,
-    new_device_state: dict[str, Any] | None = None,
-) -> None:
-    """Trigger the observe callback."""
-    observe_command = next(
-        (
-            command
-            for command in mock_gateway.mock_commands
-            if command.path == device.path and command.observe
-        ),
-        None,
-    )
-    assert observe_command
-
-    if new_device_state is not None:
-        mock_gateway.mock_responses.append(new_device_state)
-
-    device_state = deepcopy(device.raw)
-    new_state = mock_gateway.mock_responses[-1]
-    device_state = modify_state(device_state, new_state)
-    observe_command.process_result(device_state)
-
-    await hass.async_block_till_done()
--- tests/components/tradfri/test_switch.py
+++ tests/components/tradfri/test_switch.py
@@ -1,21 +1,18 @@
 """Tradfri switch (recognised as sockets in the IKEA ecosystem) platform tests."""
 from __future__ import annotations
 
-import json
 from typing import Any
-from unittest.mock import MagicMock, Mock
 
 import pytest
 from pytradfri.const import ATTR_REACHABLE_STATE
 from pytradfri.device import Device
-from pytradfri.device.socket import Socket
 
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.components.tradfri.const import DOMAIN
 from homeassistant.const import STATE_OFF, STATE_ON, STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
 
-from .common import setup_integration, trigger_observe_callback
+from .common import CommandStore, setup_integration, trigger_observe_callback
 
 from tests.common import load_fixture
 
@@ -23,28 +20,17 @@ from tests.common import load_fixture
 @pytest.fixture(scope="module")
 def outlet() -> dict[str, Any]:
     """Return an outlet response."""
-    return json.loads(load_fixture("outlet.json", DOMAIN))
+    return load_fixture("outlet.json", DOMAIN)
 
 
-@pytest.fixture
-def socket(outlet: dict[str, Any]) -> Socket:
-    """Return socket."""
-    device = Device(outlet)
-    socket_control = device.socket_control
-    assert socket_control
-    return socket_control.sockets[0]
-
-
+@pytest.mark.parametrize("device", ["outlet"], indirect=True)
 async def test_switch_available(
     hass: HomeAssistant,
-    mock_gateway: Mock,
-    mock_api_factory: MagicMock,
-    socket: Socket,
+    command_store: CommandStore,
+    device: Device,
 ) -> None:
     """Test switch available property."""
     entity_id = "switch.test"
-    device = socket.device
-    mock_gateway.mock_devices.append(device)
     await setup_integration(hass)
 
     state = hass.states.get(entity_id)
@@ -52,7 +38,7 @@ async def test_switch_available(
     assert state.state == STATE_OFF
 
     await trigger_observe_callback(
-        hass, mock_gateway, device, {ATTR_REACHABLE_STATE: 0}
+        hass, command_store, device, {ATTR_REACHABLE_STATE: 0}
     )
 
     state = hass.states.get(entity_id)
@@ -60,6 +46,7 @@ async def test_switch_available(
     assert state.state == STATE_UNAVAILABLE
 
 
+@pytest.mark.parametrize("device", ["outlet"], indirect=True)
 @pytest.mark.parametrize(
     ("service", "expected_state"),
     [
@@ -69,16 +56,13 @@ async def test_switch_available(
 )
 async def test_turn_on_off(
     hass: HomeAssistant,
-    mock_gateway: Mock,
-    mock_api_factory: MagicMock,
-    socket: Socket,
+    command_store: CommandStore,
+    device: Device,
     service: str,
     expected_state: str,
 ) -> None:
     """Test turning switch on/off."""
     entity_id = "switch.test"
-    device = socket.device
-    mock_gateway.mock_devices.append(device)
     await setup_integration(hass)
 
     state = hass.states.get(entity_id)
@@ -95,7 +79,7 @@ async def test_turn_on_off(
     )
     await hass.async_block_till_done()
 
-    await trigger_observe_callback(hass, mock_gateway, device)
+    await trigger_observe_callback(hass, command_store, device)
 
     state = hass.states.get(entity_id)
     assert state
