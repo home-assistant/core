97a71427b2aaa3de5e214c3033b9c6a0b35ec3c7
Niels Perfors <niels.perfors1987@gmail.com>
2024-02-07 22:36:30 +0000

    increase interval

--- homeassistant/components/wireguard/const.py
+++ homeassistant/components/wireguard/const.py
@@ -4,7 +4,7 @@ import logging
 
 DOMAIN = "wireguard"
 LOGGER = logging.getLogger(__package__)
-DEFAULT_SCAN_INTERVAL = timedelta(minutes=1)
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=30)
 DEFAULT_HOST = "http://a0d7b954-wireguard"
 DEFAULT_NAME = "WireGuard"
 
