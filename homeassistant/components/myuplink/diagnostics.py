8a4e49596f6e7d7dfe743c42858aa035a2588097
Åke Strandberg <ake@strandberg.eu>
2024-02-07 22:27:40 +0000

    Cleanup and adjust

--- homeassistant/components/myuplink/diagnostics.py
+++ homeassistant/components/myuplink/diagnostics.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from myuplink.api import MyUplinkAPI
+from myuplink import MyUplinkAPI
 
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
@@ -17,7 +17,10 @@ TO_REDACT = {"access_token", "refresh_token"}
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, config_entry: ConfigEntry
 ) -> dict[str, Any]:
-    """Return diagnostics for a config entry."""
+    """Return diagnostics for a config entry.
+
+    Pick up fresh data from API and dump it.
+    """
     api: MyUplinkAPI = hass.data[DOMAIN][config_entry.entry_id].api
     myuplink_data = {}
     myuplink_data["my_systems"] = await api.async_get_systems_json()
@@ -27,7 +30,12 @@ async def async_get_config_entry_diagnostics(
             device_data = await api.async_get_device_json(device["id"])
             device_points = await api.async_get_device_points_json(device["id"])
             myuplink_data["my_systems"]["devices"].append(
-                {system["systemId"]: device_data, "points": device_points}
+                {
+                    system["systemId"]: {
+                        "device_data": device_data,
+                        "points": device_points,
+                    }
+                }
             )
 
     diagnostics_data = {
