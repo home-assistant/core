3be73a74e64b0ff023c2377c7895c7de75ae8ed4
Sarabveer Singh <me@sarabveer.me>
2024-02-07 11:26:27 -0500

    Update tests for HomeKit density_to_air_quality function

--- tests/components/homekit/test_util.py
+++ tests/components/homekit/test_util.py
@@ -230,8 +230,9 @@ def test_temperature_to_states() -> None:
 def test_density_to_air_quality() -> None:
     """Test map PM2.5 density to HomeKit AirQuality level."""
     assert density_to_air_quality(0) == 1
-    assert density_to_air_quality(12) == 1
-    assert density_to_air_quality(12.1) == 2
+    assert density_to_air_quality(9) == 1
+    assert density_to_air_quality(9.1) == 2
+    assert density_to_air_quality(12) == 2
     assert density_to_air_quality(35.4) == 2
     assert density_to_air_quality(35.5) == 3
     assert density_to_air_quality(55.4) == 3
