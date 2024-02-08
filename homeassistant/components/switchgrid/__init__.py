33ab06e38f7df46b8c2c90b204e21cf2ad27b75a
Thomas Carli <thomascarli@gmail.com>
2024-02-08 13:00:47 +0000

    refresh coordinator on startup

--- homeassistant/components/switchgrid/__init__.py
+++ homeassistant/components/switchgrid/__init__.py
@@ -29,6 +29,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await coordinator.async_refresh()
 
     return True
 
