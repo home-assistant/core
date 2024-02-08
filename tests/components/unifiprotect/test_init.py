75b21b9cc548616200025246cd8a5f1a579442b8
Christopher Bailey <cbailey@mort.is>
2024-02-08 10:23:58 -0500

    Improve UniFi Protect re-auth

--- tests/components/unifiprotect/test_init.py
+++ tests/components/unifiprotect/test_init.py
@@ -9,6 +9,7 @@ from pyunifiprotect import NotAuthorized, NvrError, ProtectApiClient
 from pyunifiprotect.data import NVR, Bootstrap, Light
 
 from homeassistant.components.unifiprotect.const import (
+    AUTH_RETRIES,
     CONF_DISABLE_RTSP,
     DEFAULT_SCAN_INTERVAL,
     DOMAIN,
@@ -155,6 +156,8 @@ async def test_setup_failed_update_reauth(
 ) -> None:
     """Test setup of unifiprotect entry with update that gives unauthroized error."""
 
+    reauth_key = f"{ufp.entry.entry_id}_auth"
+    hass.data.setdefault(DOMAIN, {})[reauth_key] = AUTH_RETRIES
     await hass.config_entries.async_setup(ufp.entry.entry_id)
     await hass.async_block_till_done()
     assert ufp.entry.state == ConfigEntryState.LOADED
@@ -162,15 +165,15 @@ async def test_setup_failed_update_reauth(
     # reauth should not be triggered until there are 10 auth failures in a row
     # to verify it is not transient
     ufp.api.update = AsyncMock(side_effect=NotAuthorized)
-    for _ in range(10):
+    for _ in range(AUTH_RETRIES):
         await time_changed(hass, DEFAULT_SCAN_INTERVAL)
         assert len(hass.config_entries.flow._progress) == 0
 
-    assert ufp.api.update.call_count == 10
+    assert ufp.api.update.call_count == AUTH_RETRIES
     assert ufp.entry.state == ConfigEntryState.LOADED
 
     await time_changed(hass, DEFAULT_SCAN_INTERVAL)
-    assert ufp.api.update.call_count == 11
+    assert ufp.api.update.call_count == AUTH_RETRIES + 1
     assert len(hass.config_entries.flow._progress) == 1
 
 
@@ -188,6 +191,8 @@ async def test_setup_failed_error(hass: HomeAssistant, ufp: MockUFPFixture) -> N
 async def test_setup_failed_auth(hass: HomeAssistant, ufp: MockUFPFixture) -> None:
     """Test setup of unifiprotect entry with unauthorized error."""
 
+    reauth_key = f"{ufp.entry.entry_id}_auth"
+    hass.data.setdefault(DOMAIN, {})[reauth_key] = AUTH_RETRIES
     ufp.api.get_nvr = AsyncMock(side_effect=NotAuthorized)
 
     await hass.config_entries.async_setup(ufp.entry.entry_id)
