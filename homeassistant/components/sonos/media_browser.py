53432016719d654215dc0bdd3748326d0e4afe98
IngoK1 <45150614+IngoK1@users.noreply.github.com>
2024-02-10 10:23:25 +0100

    Fix type casting problem

--- homeassistant/components/sonos/media_browser.py
+++ homeassistant/components/sonos/media_browser.py
@@ -62,11 +62,11 @@ def get_thumbnail_url_full(
         )
         return urllib.parse.unquote(getattr(item, "album_art_uri", None))
 
-    return urllib.parse.unquote(get_browse_image_url(
+    return urllib.parse.unquote(str(get_browse_image_url(
         media_content_type,
         media_content_id,
         media_image_id,
-    ))
+    )))
 
 
 def media_source_filter(item: BrowseMedia) -> bool:
