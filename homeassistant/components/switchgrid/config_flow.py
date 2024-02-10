273fbbd96571136c31dd131e5792190438f907e5
Thomas Carli <thomascarli@gmail.com>
2024-02-10 13:11:50 +0000

    remove async_step_import

--- homeassistant/components/switchgrid/config_flow.py
+++ homeassistant/components/switchgrid/config_flow.py
@@ -29,7 +29,3 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title="Switchgrid", data=user_input)
 
         return self.async_show_form(step_id="user")
-
-    async def async_step_import(self, user_input: dict[str, Any]) -> FlowResult:
-        """Handle import from configuration.yaml."""
-        return await self.async_step_user(user_input)
