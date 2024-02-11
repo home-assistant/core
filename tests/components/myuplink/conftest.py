d05c785047d275aaddd8d828438b3d527772b272
Åke Strandberg <ake@strandberg.eu>
2024-02-11 23:11:41 +0000

    Improve mocks

--- tests/components/myuplink/conftest.py
+++ tests/components/myuplink/conftest.py
@@ -1,8 +1,11 @@
 """Test helpers for myuplink."""
 from collections.abc import Generator
+import json
 import time
+from typing import Any
 from unittest.mock import AsyncMock, MagicMock, patch
 
+from myuplink import Device, System
 import pytest
 
 from homeassistant.components.application_credentials import (
@@ -15,7 +18,7 @@ from homeassistant.setup import async_setup_component
 
 from .const import CLIENT_ID, CLIENT_SECRET
 
-from tests.common import MockConfigEntry, load_json_value_fixture
+from tests.common import MockConfigEntry, load_fixture, load_json_value_fixture
 
 
 @pytest.fixture(name="expires_at")
@@ -62,21 +65,32 @@ async def setup_credentials(hass: HomeAssistant) -> None:
 
 
 @pytest.fixture
-def mock_myuplink_client() -> Generator[MagicMock, AsyncMock, None]:
+def mock_myuplink_client() -> Generator[AsyncMock, AsyncMock, AsyncMock]:
     """Mock a myuplink client."""
+
+    def process_json_system(data: dict[str, Any]) -> System:
+        array = json.loads(data)
+        return [System(system_data) for system_data in array["systems"]]
+
     with patch(
         "homeassistant.components.myuplink.MyUplinkAPI",
         autospec=True,
     ) as mock_client:
         client = mock_client.return_value
-        client.async_get_systems_json.return_value = load_json_value_fixture(
-            "systems.json", DOMAIN
+        client.async_get_device_points_json.return_value = load_json_value_fixture(
+            "device_points_nibe_f730.json", DOMAIN
+        )
+        client.async_get_systems.return_value = process_json_system(
+            load_fixture("systems.json", DOMAIN)
+        )
+        client.async_get_device.return_value = Device(
+            load_json_value_fixture("device.json", DOMAIN)
         )
         client.async_get_device_json.return_value = load_json_value_fixture(
             "device.json", DOMAIN
         )
-        client.async_get_device_points_json.return_value = load_json_value_fixture(
-            "device_points_nibe_f730.json", DOMAIN
+        client.async_get_systems_json.return_value = load_json_value_fixture(
+            "systems.json", DOMAIN
         )
         yield client
 
