202e83b047e3e2003395ca9dc8129dff3604681a
Åke Strandberg <ake@strandberg.eu>
2024-02-11 13:17:19 +0000

    Address review comments

--- tests/components/myuplink/conftest.py
+++ tests/components/myuplink/conftest.py
@@ -1,59 +1,102 @@
-"""Common fixtures and mocks for myplink tests."""
-
+"""Test helpers for myuplink."""
 from collections.abc import Generator
-from unittest.mock import MagicMock, patch
+import time
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 
+from homeassistant.components.application_credentials import (
+    ClientCredential,
+    async_import_client_credential,
+)
 from homeassistant.components.myuplink.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
-from tests.common import MockConfigEntry, load_json_value_fixture
+from .const import CLIENT_ID, CLIENT_SECRET
+
+from tests.common import MockConfigEntry, load_fixture, load_json_value_fixture
+
+
+@pytest.fixture(name="jwt")
+def load_jwt_fixture():
+    """Load Fixture data."""
+    return load_fixture("jwt", DOMAIN)
+
+
+@pytest.fixture(name="expires_at")
+def mock_expires_at() -> float:
+    """Fixture to set the oauth token expiration time."""
+    return time.time() + 3600
 
 
 @pytest.fixture
-def mock_config_entry() -> MockConfigEntry:
+def mock_config_entry(jwt, expires_at: int) -> MockConfigEntry:
     """Return the default mocked config entry."""
     return MockConfigEntry(
-        title="MyUplink Setup",
+        version=1,
         domain=DOMAIN,
-        data={},
+        title="myUplink test",
+        data={
+            "auth_implementation": DOMAIN,
+            "token": {
+                "access_token": jwt,
+                "scope": "READSYSTEM offline",
+                "expires_in": 86399,
+                "refresh_token": "3012bc9f-7a65-4240-b817-9154ffdcc30f",
+                "token_type": "Bearer",
+                "expires_at": expires_at,
+            },
+        },
+        entry_id="myuplink_test",
+    )
+
+
+@pytest.fixture(autouse=True)
+async def setup_credentials(hass: HomeAssistant) -> None:
+    """Fixture to setup credentials."""
+    assert await async_setup_component(hass, "application_credentials", {})
+    await async_import_client_credential(
+        hass,
+        DOMAIN,
+        ClientCredential(
+            CLIENT_ID,
+            CLIENT_SECRET,
+        ),
+        DOMAIN,
     )
 
 
 @pytest.fixture
-def mock_myuplink(request: pytest.FixtureRequest) -> Generator[None, MagicMock, None]:
-    """Return a mocked myuplink client."""
-
+def mock_myuplink_client() -> Generator[MagicMock, AsyncMock, None]:
+    """Mock a myuplink client."""
     with patch(
-        "homeassistant.components.myuplink.MyUplinkAPI", autospec=True
-    ) as myuplink_mock:
-        myuplink = myuplink_mock.return_value
-        myuplink.async_get_systems_json.return_value = load_json_value_fixture(
+        "homeassistant.components.myuplink.MyUplinkAPI",
+        autospec=True,
+    ) as mock_client:
+        client = mock_client.return_value
+        client.async_get_systems_json.return_value = load_json_value_fixture(
             "systems.json", DOMAIN
         )
-        myuplink.async_get_device_json.return_value = load_json_value_fixture(
+        client.async_get_device_json.return_value = load_json_value_fixture(
             "device.json", DOMAIN
         )
-        myuplink.async_get_device_points_json.return_value = load_json_value_fixture(
+        client.async_get_device_points_json.return_value = load_json_value_fixture(
             "device_points_nibe_f730.json", DOMAIN
         )
-
-        yield myuplink
+        yield client
 
 
 @pytest.fixture
 async def init_integration(
-    hass: HomeAssistant, mock_config_entry: MockConfigEntry, mock_myuplink: MagicMock
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_myuplink_client: MagicMock,
 ) -> MockConfigEntry:
     """Set up the myuplink integration for testing."""
     mock_config_entry.add_to_hass(hass)
 
-    with patch(
-        "homeassistant.helpers.config_entry_oauth2_flow.async_get_config_entry_implementation"
-    ):
-        assert await async_setup_component(hass, "myuplink", {})
-        await hass.async_block_till_done()
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
 
     return mock_config_entry
