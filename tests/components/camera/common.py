7c98663cd7613f7f02241366408cbbc5a2870d3b
sdb9696 <steven.beth@gmail.com>
2024-02-13 17:42:51 +0000

    Add WebRTC configuration and allow two way audio

--- tests/components/camera/common.py
+++ tests/components/camera/common.py
@@ -6,8 +6,16 @@ components. Instead call the service directly.
 
 from unittest.mock import Mock
 
+from homeassistant.components.camera import RtcConfiguration, WebRtcConfiguration
+
 EMPTY_8_6_JPEG = b"empty_8_6"
 WEBRTC_ANSWER = "a=sendonly"
+WEBRTC_CONFIG = WebRtcConfiguration(
+    rtc_configuration=RtcConfiguration(
+        ice_servers=[RtcConfiguration.IceServer(urls="stun:stun.foobar.com")]
+    ),
+    audio_direction=WebRtcConfiguration.TransportDirection.SENDRECV,
+)
 
 
 def mock_turbo_jpeg(
