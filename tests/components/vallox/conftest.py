ad13348f20053a1846c1357276068b0f2f9dc5fc
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-02-14 18:06:36 +0000

    Tests work in progress

--- tests/components/vallox/conftest.py
+++ tests/components/vallox/conftest.py
@@ -3,11 +3,16 @@
 import random
 import string
 from typing import Any
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 from uuid import UUID
 
 import pytest
-from vallox_websocket_api.vallox import PROFILE, MetricData
+from vallox_websocket_api.vallox import (
+    MODEL_METRIC,
+    PROFILE,
+    SW_VERSION_METRICS,
+    MetricData,
+)
 
 from homeassistant.components.vallox.const import DOMAIN
 from homeassistant.const import CONF_HOST, CONF_NAME
@@ -31,11 +36,28 @@ def mock_entry(hass: HomeAssistant) -> MockConfigEntry:
     return vallox_mock_entry
 
 
+def default_metrics():
+    """Return default metrics."""
+    return {
+        MODEL_METRIC: "Vallox Testmodel",
+        SW_VERSION_METRICS[0]: "0",
+        SW_VERSION_METRICS[1]: ".",
+        SW_VERSION_METRICS[2]: "1",
+        SW_VERSION_METRICS[3]: ".",
+        SW_VERSION_METRICS[4]: "2",
+    }
+
+
 def patch_metrics(metrics: dict[str, Any]):
     """Patch the Vallox metrics response."""
+    metric_data = MagicMock(MetricData)
+    metric_data.uuid = MagicMock(return_value=_random_uuid())
+    metric_data.model = MagicMock(return_value="Vallox Testmodel")
+    metric_data.sw_version = MagicMock(return_value="0.1.2")
+
     return patch(
         "homeassistant.components.vallox.Vallox.fetch_metric_data",
-        return_value=MetricData(data=metrics),
+        return_value=metric_data,
     )
 
 
@@ -60,51 +82,56 @@ def patch_metrics_set():
 @pytest.fixture(autouse=True)
 def patch_empty_metrics():
     """Patch the Vallox profile response."""
+    metric_data = MagicMock(MetricData)
+    metric_data.uuid = MagicMock(return_value=_random_uuid())
+    metric_data.model = MagicMock(return_value="Vallox Testmodel")
+    metric_data.sw_version = MagicMock(return_value="0.1.2")
+
     with patch(
         "homeassistant.components.vallox.Vallox.fetch_metric_data",
-        return_value={},
+        return_value=metric_data,
     ):
         yield
 
 
-@pytest.fixture(autouse=True)
-def patch_default_profile():
-    """Patch the Vallox profile response."""
-    with patch(
-        "homeassistant.components.vallox.MetricData.profile",
-        return_value=PROFILE.HOME,
-    ):
-        yield
+# @pytest.fixture(autouse=True)
+# def patch_default_profile():
+#    """Patch the Vallox profile response."""
+#    with patch(
+#        "homeassistant.components.vallox.MetricData.profile",
+#        return_value=PROFILE.HOME,
+#    ):
+#        yield
 
 
-@pytest.fixture(autouse=True)
-def patch_model():
-    """Patch the Vallox model response."""
-    with patch(
-        "homeassistant.components.vallox.MetricData.model",
-        return_value="Vallox Testmodel",
-    ):
-        yield
+# @pytest.fixture(autouse=True)
+# def patch_model():
+#    """Patch the Vallox model response."""
+#    with patch(
+#        "homeassistant.components.vallox.MetricData.model",
+#        return_value="Vallox Testmodel",
+#    ):
+#        yield
 
 
-@pytest.fixture(autouse=True)
-def patch_sw_version():
-    """Patch the Vallox SW version response."""
-    with patch(
-        "homeassistant.components.vallox.MetricData.sw_version",
-        return_value="0.1.2",
-    ):
-        yield
+# @pytest.fixture(autouse=True)
+# def patch_sw_version():
+#    """Patch the Vallox SW version response."""
+#    with patch(
+#        "homeassistant.components.vallox.MetricData.sw_version",
+#        return_value="0.1.2",
+#    ):
+#        yield
 
 
-@pytest.fixture(autouse=True)
-def patch_uuid():
-    """Patch the Vallox UUID response."""
-    with patch(
-        "homeassistant.components.vallox.MetricData.uuid",
-        return_value=_random_uuid(),
-    ):
-        yield
+# @pytest.fixture(autouse=True)
+# def patch_uuid():
+#    """Patch the Vallox UUID response."""
+#    with patch(
+#        "homeassistant.components.vallox.MetricData.uuid",
+#        return_value=_random_uuid(),
+#    ):
+#        yield
 
 
 def _random_uuid() -> UUID:
