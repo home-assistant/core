855d796c0c6ddd9e217f5dd849311923eead3b9e
Åke S - Piper <ake@strandberg.eu>
2024-02-14 06:06:00 +0000

    Add some myuplink tests

--- tests/components/myuplink/conftest.py
+++ tests/components/myuplink/conftest.py
@@ -2,10 +2,9 @@
 from collections.abc import Generator
 import json
 import time
-from typing import Any
 from unittest.mock import MagicMock, patch
 
-from myuplink import Device, System
+from myuplink import Device, DevicePoint, System
 import pytest
 
 from homeassistant.components.application_credentials import (
@@ -64,34 +63,70 @@ async def setup_credentials(hass: HomeAssistant) -> None:
     )
 
 
-@pytest.fixture
-def mock_myuplink_client() -> Generator[MagicMock, None, None]:
-    """Mock a myuplink client."""
+@pytest.fixture(scope="session")
+def device_fixture():
+    """Fixture for device."""
+    return Device(load_json_value_fixture("device.json", DOMAIN))
 
-    def process_json_system(data: dict[str, Any]) -> System:
-        array = json.loads(data)
-        return [System(system_data) for system_data in array["systems"]]
+
+@pytest.fixture(scope="session")
+def device_json_fixture():
+    """Fixture for device in json format."""
+    return load_json_value_fixture("device.json", DOMAIN)
+
+
+@pytest.fixture(scope="session")
+def system_fixture():
+    """Fixture for system."""
+    array = json.loads(load_fixture("systems.json", DOMAIN))
+    return [System(system_data) for system_data in array["systems"]]
+
+
+@pytest.fixture(scope="session")
+def system_json_fixture():
+    """Fixture for system in json format."""
+    return load_json_value_fixture("systems.json", DOMAIN)
+
+
+@pytest.fixture(scope="session")
+def device_points_fixture():
+    """Fixture for devce_points."""
+    array = json.loads(load_fixture("device_points_nibe_f730.json", DOMAIN))
+    return [DevicePoint(point_data) for point_data in array]
+
+
+@pytest.fixture(scope="session")
+def device_points_json_fixture():
+    """Fixture for device_points in json format."""
+    return load_json_value_fixture("device_points_nibe_f730.json", DOMAIN)
+
+
+@pytest.fixture
+def mock_myuplink_client(
+    device_json_fixture,
+    device_fixture,
+    device_points_json_fixture,
+    device_points_fixture,
+    system_fixture,
+    system_json_fixture,
+) -> Generator[MagicMock, None, None]:
+    """Mock a myuplink client."""
 
     with patch(
         "homeassistant.components.myuplink.MyUplinkAPI",
         autospec=True,
     ) as mock_client:
         client = mock_client.return_value
-        client.async_get_device_points_json.return_value = load_json_value_fixture(
-            "device_points_nibe_f730.json", DOMAIN
-        )
-        client.async_get_systems.return_value = process_json_system(
-            load_fixture("systems.json", DOMAIN)
-        )
-        client.async_get_device.return_value = Device(
-            load_json_value_fixture("device.json", DOMAIN)
-        )
-        client.async_get_device_json.return_value = load_json_value_fixture(
-            "device.json", DOMAIN
-        )
-        client.async_get_systems_json.return_value = load_json_value_fixture(
-            "systems.json", DOMAIN
-        )
+
+        client.async_get_systems.return_value = system_fixture
+        client.async_get_systems_json.return_value = system_json_fixture
+
+        client.async_get_device.return_value = device_fixture
+        client.async_get_device_json.return_value = device_json_fixture
+
+        client.async_get_device_points.return_value = device_points_fixture
+        client.async_get_device_points_json.return_value = device_points_json_fixture
+
         yield client
 
 
