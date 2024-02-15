359d1eaed523c96899990e751276c54b5eefe65a
Lenn Perik <lennperik@hotmail.nl>
2024-02-15 14:55:55 +0000

    Remove unused platform attributes and icons

--- homeassistant/components/motionblinds_ble/const.py
+++ homeassistant/components/motionblinds_ble/const.py
@@ -1,13 +1,6 @@
 """Constants for the Motionblinds BLE integration."""
 
-ATTR_CALIBRATION = "calibration"
 ATTR_CONNECTION = "connection"
-ATTR_SPEED = "speed"
-ATTR_BATTERY = "battery"
-ATTR_CONNECT = "connect"
-ATTR_DISCONNECT = "disconnect"
-ATTR_FAVORITE = "favorite"
-ATTR_SIGNAL_STRENGTH = "signal_strength"
 
 CONF_LOCAL_NAME = "local_name"
 CONF_MAC_CODE = "mac_code"
@@ -20,12 +13,6 @@ ERROR_INVALID_MAC_CODE = "invalid_mac_code"
 ERROR_NO_BLUETOOTH_ADAPTER = "no_bluetooth_adapter"
 ERROR_NO_DEVICES_FOUND = "no_devices_found"
 
-ICON_CALIBRATION = "mdi:tune"
-ICON_SPEED = "mdi:run-fast"
-ICON_CONNECT = "mdi:bluetooth"
-ICON_DISCONNECT = "mdi:bluetooth-off"
-ICON_FAVORITE = "mdi:star"
-ICON_CONNECTION_TYPE = "mdi:bluetooth-connect"
 ICON_VERTICAL_BLIND = "mdi:blinds-vertical-closed"
 
 MANUFACTURER = "Motionblinds"
