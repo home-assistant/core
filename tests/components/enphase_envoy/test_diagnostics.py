477e7e204af8e2b6436659ff36fdad1bfcee4575
Arie Catsman <catsmanac@outlook.com>
2024-02-15 13:11:22 +0000

    Include platforms in diagnostics test

--- tests/components/enphase_envoy/test_diagnostics.py
+++ tests/components/enphase_envoy/test_diagnostics.py
@@ -1,21 +1,31 @@
 """Test Enphase Envoy diagnostics."""
-from syrupy import SnapshotAssertion
+from syrupy.assertion import SnapshotAssertion
 
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from tests.components.diagnostics import get_diagnostics_for_config_entry
 from tests.typing import ClientSessionGenerator
 
+# Fields to patch so they are the same between repeated runs
+TO_REDACT = {
+    "id",
+    "device_id",
+    "via_device_id",
+    "last_updated",
+    "last_changed",
+}
+
 
 async def test_entry_diagnostics(
     hass: HomeAssistant,
-    config_entry,
+    config_entry: ConfigEntry,
     hass_client: ClientSessionGenerator,
     setup_enphase_envoy,
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test config entry diagnostics."""
-    assert (
-        await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
-        == snapshot
-    )
+    result = await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
+    # patch fields that change each run to fixed value and test
+    assert async_redact_data(result, TO_REDACT) == snapshot
