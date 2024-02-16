a1ca01ce27ad4f9cf3ef0cd82a767aeec5be969e
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-02-16 19:25:40 +0100

    Update related test-code

--- tests/components/plugwise/test_sensor.py
+++ tests/components/plugwise/test_sensor.py
@@ -111,38 +111,28 @@ async def test_anna_as_smt_climate_sensor_entities(
     assert float(state.state) == 86.0
 
 
-async def test_anna_climate_sensor_entities(
-    hass: HomeAssistant, mock_smile_anna: MagicMock, init_integration: MockConfigEntry
-) -> None:
-    """Test creation of climate related sensor entities."""
-    state = hass.states.get("sensor.opentherm_outdoor_air_temperature")
-    assert state
-    assert float(state.state) == 3.0
-
-
 async def test_p1_dsmr_sensor_entities(
     hass: HomeAssistant, mock_smile_p1: MagicMock, init_integration: MockConfigEntry
 ) -> None:
     """Test creation of power related sensor entities."""
     state = hass.states.get("sensor.p1_net_electricity_point")
     assert state
-    assert float(state.state) == -2816.0
+    assert int(state.state) == 486
 
     state = hass.states.get("sensor.p1_electricity_consumed_off_peak_cumulative")
     assert state
-    assert float(state.state) == 551.09
+    assert float(state.state) == 17643.423
 
     state = hass.states.get("sensor.p1_electricity_produced_peak_point")
     assert state
-    assert float(state.state) == 2816.0
+    assert int(state.state) == 0
 
     state = hass.states.get("sensor.p1_electricity_consumed_peak_cumulative")
     assert state
-    assert float(state.state) == 442.932
+    assert float(state.state) == 13966.608
 
     state = hass.states.get("sensor.p1_gas_consumed_cumulative")
-    assert state
-    assert float(state.state) == 584.85
+    assert not state
 
 
 async def test_p1_3ph_dsmr_sensor_entities(
@@ -151,15 +141,15 @@ async def test_p1_3ph_dsmr_sensor_entities(
     """Test creation of power related sensor entities."""
     state = hass.states.get("sensor.p1_electricity_phase_one_consumed")
     assert state
-    assert float(state.state) == 1763.0
+    assert int(state.state) == 1763
 
     state = hass.states.get("sensor.p1_electricity_phase_two_consumed")
     assert state
-    assert float(state.state) == 1703.0
+    assert int(state.state) == 1703
 
     state = hass.states.get("sensor.p1_electricity_phase_three_consumed")
     assert state
-    assert float(state.state) == 2080.0
+    assert int(state.state) == 2080
 
     entity_id = "sensor.p1_voltage_phase_one"
     state = hass.states.get(entity_id)
