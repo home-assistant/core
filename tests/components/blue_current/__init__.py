29607ba2744fb1856a213d3953fed023bcca2de0
Floris272 <florispuijk@outlook.com>
2024-02-16 06:59:03 +0000

    Add get_charge_cards mock

--- tests/components/blue_current/__init__.py
+++ tests/components/blue_current/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from asyncio import Event, Future
 from dataclasses import dataclass
+from typing import Any
 from unittest.mock import MagicMock, patch
 
 from bluecurrent_api import Client
@@ -16,6 +17,12 @@ DEFAULT_CHARGE_POINT = {
     "evse_id": "101",
     "model_type": "",
     "name": "",
+}
+
+DEFAULT_CHARGE_CARD = {
+    "uid": "123",
+    "id": "NL-TEST",
+    "name": "test",
 }
 
 
@@ -31,6 +38,7 @@ def create_client_mock(
     future_container: FutureContainer,
     started_loop: Event,
     charge_point: dict,
+    charge_cards: list[dict[str, Any]],
     status: dict,
     grid: dict,
 ) -> MagicMock:
@@ -46,6 +54,7 @@ def create_client_mock(
         """Set the receiver and await future."""
         client_mock.receiver = receiver
         await client_mock.get_charge_points()
+        await client_mock.get_charge_cards()
 
         started_loop.set()
         started_loop.clear()
@@ -64,6 +73,10 @@ def create_client_mock(
         )
         received_charge_points.set()
 
+    async def get_charge_cards() -> None:
+        """Send a list of charge cards to the callback."""
+        await client_mock.receiver({"object": "CHARGE_CARDS", "cards": charge_cards})
+
     async def get_status(evse_id: str) -> None:
         """Send the status of a charge point to the callback."""
         await client_mock.receiver(
@@ -80,6 +93,7 @@ def create_client_mock(
     client_mock.connect.side_effect = connect
     client_mock.wait_for_charge_points.side_effect = wait_for_charge_points
     client_mock.get_charge_points.side_effect = get_charge_points
+    client_mock.get_charge_cards.side_effect = get_charge_cards
     client_mock.get_status.side_effect = get_status
     client_mock.get_grid_status.side_effect = get_grid_status
 
@@ -93,11 +107,15 @@ async def init_integration(
     charge_point: dict | None = None,
     status: dict | None = None,
     grid: dict | None = None,
+    charge_cards: list[dict[str, Any]] | None = None,
 ) -> tuple[MagicMock, Event, FutureContainer]:
     """Set up the Blue Current integration in Home Assistant."""
 
     if charge_point is None:
         charge_point = DEFAULT_CHARGE_POINT
+
+    if charge_cards is None:
+        charge_cards = [DEFAULT_CHARGE_CARD]
 
     if status is None:
         status = {}
@@ -109,7 +127,7 @@ async def init_integration(
     started_loop = Event()
 
     client_mock = create_client_mock(
-        hass, future_container, started_loop, charge_point, status, grid
+        hass, future_container, started_loop, charge_point, charge_cards, status, grid
     )
 
     with patch("homeassistant.components.blue_current.PLATFORMS", [platform]), patch(
