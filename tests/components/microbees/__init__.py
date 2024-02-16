c91ce62ae3a6e76cb8573d6c130534960b07a5ae
FedDam <noceracity@gmail.com>
2024-02-16 16:22:14 +0000

    reverting .strict-typing and added microbees to .coveragerc

--- tests/components/microbees/__init__.py
+++ tests/components/microbees/__init__.py
@@ -4,9 +4,7 @@ from homeassistant.core import HomeAssistant
 from tests.common import MockConfigEntry
 
 
-async def setup_integration(
-    hass: HomeAssistant, config_entry: MockConfigEntry, enable_webhooks: bool = True
-) -> None:
+async def setup_integration(hass: HomeAssistant, config_entry: MockConfigEntry) -> None:
     """Fixture for setting up the component."""
     config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(config_entry.entry_id)
