d3b6e3d3691056cd0b8e2bc17e8cf6762efa1b32
dontinelli <73341522+dontinelli@users.noreply.github.com>
2024-02-17 21:39:41 +0100

    Update __init__.py
    Rewind wrongful deletion of comma

--- homeassistant/components/fyta/__init__.py
+++ homeassistant/components/fyta/__init__.py
@@ -22,7 +22,7 @@ from .coordinator import FytaCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = (Platform.SENSOR)
+PLATFORMS = (Platform.SENSOR,)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
