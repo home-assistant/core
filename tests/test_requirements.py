02273924eddbd6b5ac7c6d2770a513b70e6861af
J. Nick Koston <nick@koston.org>
2024-02-17 10:39:59 -0600

    reset_mock

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -207,40 +207,41 @@ async def test_get_integration_with_requirements_cache(hass: HomeAssistant) -> N
         assert integration
         assert integration.domain == "test_component"
 
-    assert len(mock_is_installed.mock_calls) == 3
-    assert sorted(mock_call[1][0] for mock_call in mock_is_installed.mock_calls) == [
-        "test-comp-after-dep==1.0.0",
-        "test-comp-dep==1.0.0",
-        "test-comp==1.0.0",
-    ]
+        assert len(mock_is_installed.mock_calls) == 3
+        assert sorted(
+            mock_call[1][0] for mock_call in mock_is_installed.mock_calls
+        ) == [
+            "test-comp-after-dep==1.0.0",
+            "test-comp-dep==1.0.0",
+            "test-comp==1.0.0",
+        ]
 
-    assert len(mock_inst.mock_calls) == 3
-    assert sorted(mock_call[1][0] for mock_call in mock_inst.mock_calls) == [
-        "test-comp-after-dep==1.0.0",
-        "test-comp-dep==1.0.0",
-        "test-comp==1.0.0",
-    ]
+        assert len(mock_inst.mock_calls) == 3
+        assert sorted(mock_call[1][0] for mock_call in mock_inst.mock_calls) == [
+            "test-comp-after-dep==1.0.0",
+            "test-comp-dep==1.0.0",
+            "test-comp==1.0.0",
+        ]
 
-    # The dependent integrations should be fetched since
-    assert len(mock_async_get_integration.mock_calls) == 3
-    assert sorted(
-        mock_call[1][1] for mock_call in mock_async_get_integration.mock_calls
-    ) == ["test_component", "test_component_after_dep", "test_component_dep"]
+        # The dependent integrations should be fetched since
+        assert len(mock_async_get_integration.mock_calls) == 3
+        assert sorted(
+            mock_call[1][1] for mock_call in mock_async_get_integration.mock_calls
+        ) == ["test_component", "test_component_after_dep", "test_component_dep"]
 
-    # test_component2 has the same deps as test_component and we should
-    # not check the requirements for the deps again
-    with patch(
-        "homeassistant.util.package.is_installed", return_value=False
-    ) as mock_is_installed, patch(
-        "homeassistant.util.package.install_package", return_value=True
-    ) as mock_inst, patch(
-        "homeassistant.requirements.async_get_integration", wraps=async_get_integration
-    ) as mock_async_get_integration:
+        # test_component2 has the same deps as test_component and we should
+        # not check the requirements for the deps again
+
+        mock_is_installed.reset_mock()
+        mock_inst.reset_mock()
+        mock_async_get_integration.reset_mock()
+
         integration = await async_get_integration_with_requirements(
             hass, "test_component2"
         )
-        assert integration
-        assert integration.domain == "test_component2"
+
+    assert integration
+    assert integration.domain == "test_component2"
 
     assert len(mock_is_installed.mock_calls) == 1
     assert sorted(mock_call[1][0] for mock_call in mock_is_installed.mock_calls) == [
