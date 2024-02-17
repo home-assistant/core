bbd8251dab0e5e4d5ce316868f9937e7605ae468
J. Nick Koston <nick@koston.org>
2024-02-17 09:01:55 -0600

    reduce

--- homeassistant/helpers/integration_platform.py
+++ homeassistant/helpers/integration_platform.py
@@ -119,20 +119,16 @@ async def async_process_integration_platforms(
             partial(_async_process_integration_platform_for_component, hass),
         )
 
-    if not (
-        top_level_components := [
-            comp for comp in hass.config.components if "." not in comp
-        ]
-    ):
+    top_level_components = {comp for comp in hass.config.components if "." not in comp}
+    if not top_level_components:
         return
 
     integrations = await async_get_integrations(hass, top_level_components)
-    seen_components: set[str] = set(top_level_components)
     integration_platforms: list[IntegrationPlatform] = hass.data[
         DATA_INTEGRATION_PLATFORMS
     ]
     integration_platform = IntegrationPlatform(
-        platform_name, HassJob(process_platform), seen_components
+        platform_name, HassJob(process_platform), top_level_components
     )
     integration_platforms.append(integration_platform)
     if futures := [
