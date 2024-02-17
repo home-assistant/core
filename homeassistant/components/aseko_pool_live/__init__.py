74efb943fa7fa03cb30a17aed994051f46f50921
hopkins-tk <hopkins-tk@users.noreply.github.com>
2024-02-17 14:48:50 +0100

    Update homeassistant/components/aseko_pool_live/__init__.py
    Co-authored-by: Erik Montnemery <erik@montnemery.com>

--- homeassistant/components/aseko_pool_live/__init__.py
+++ homeassistant/components/aseko_pool_live/__init__.py
@@ -64,8 +64,7 @@ async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) ->
             CONF_PASSWORD: "",
         }
 
-        config_entry.version = 2
-        hass.config_entries.async_update_entry(config_entry, data=new)
+        hass.config_entries.async_update_entry(config_entry, data=new, version=2)
 
     _LOGGER.debug("Migration to version %s successful", config_entry.version)
 
