718fe116d765690fe0315c12070f4ee35803eaf9
Thomas Kistler <thomas@uyei-kistler.com>
2024-02-18 22:11:48 +0000

    Disable some sensors by default

--- tests/components/ambient_network/test_sensor.py
+++ tests/components/ambient_network/test_sensor.py
@@ -92,9 +92,9 @@ async def test_sensors_disappearing(
     with freeze_time(initial_datetime) as frozen_datetime:
         # Normal state, sensor is available.
         await setup_platform(True, hass, config_entry)
-        sensor = hass.states.get("sensor.station_a_absolute_pressure")
+        sensor = hass.states.get("sensor.station_a_relative_pressure")
         assert sensor is not None
-        assert float(sensor.state) == pytest.approx(977.61653)
+        assert float(sensor.state) == pytest.approx(1001.89694313129)
 
         # Sensor becomes unavailable if the network is unavailable. Log message
         # should only show up once.
@@ -106,7 +106,7 @@ async def test_sensors_disappearing(
                 async_fire_time_changed(hass)
                 await hass.async_block_till_done()
 
-            sensor = hass.states.get("sensor.station_a_absolute_pressure")
+            sensor = hass.states.get("sensor.station_a_relative_pressure")
             assert sensor is not None
             assert sensor.state == "unavailable"
             assert caplog.text.count("Cannot connect to Ambient Network") == 1
@@ -117,7 +117,7 @@ async def test_sensors_disappearing(
             frozen_datetime.tick(timedelta(minutes=10))
             async_fire_time_changed(hass)
             await hass.async_block_till_done()
-            sensor = hass.states.get("sensor.station_a_absolute_pressure")
+            sensor = hass.states.get("sensor.station_a_relative_pressure")
             assert sensor is not None
-            assert float(sensor.state) == pytest.approx(977.61653)
+            assert float(sensor.state) == pytest.approx(1001.89694313129)
             assert caplog.text.count("Fetching ambient_network data recovered") == 1
