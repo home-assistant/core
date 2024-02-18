9eee9bb681d5e7509a0e7c057339e5c36719259e
dontinelli <73341522+dontinelli@users.noreply.github.com>
2024-02-18 19:52:32 +0100

    Update test_config_flow.py

--- tests/components/fyta/test_config_flow.py
+++ tests/components/fyta/test_config_flow.py
@@ -1,16 +1,50 @@
 """Test the fyta config flow."""
-from unittest.mock import patch
+from collections.abc import Generator
+from unittest.mock import AsyncMock, Mock, patch
 
 import pytest
 
 from homeassistant import config_entries, data_entry_flow
 from homeassistant.components.fyta import config_flow
 from homeassistant.components.fyta.const import DOMAIN
+from homeassistant.config_entries import SOURCE_REAUTH
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 
+from tests.common import MockConfigEntry
+
+pytestmark = pytest.mark.usefixtures("mock_setup_entry")
+
 USERNAME = "fyta_user"
 PASSWORD = "fyta_pass"
+
+
+@pytest.fixture
+def mock_fyta_connector() -> Mock:
+    """Mock of FytaConnector."""
+    fyta = Mock(
+        update=Mock(return_value=True),
+    )
+    return fyta
+
+
+@pytest.fixture
+def mock_setup_entry() -> Generator[AsyncMock, None, None]:
+    """Override async_setup_entry."""
+    with patch(
+        "homeassistant.components.fyta.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        yield mock_setup_entry
+
+
+@pytest.fixture(name="validate_input")
+def mock_controller():
+    """Mock a successful _host_in_configuration_exists."""
+    with patch(
+        "homeassistant.components.solarlog.config_flow.validate_input",
+        return_value={"username": "fyta_user", "password": "fyta_password"},
+    ):
+        yield
 
 
 async def test_form(hass: HomeAssistant) -> None:
@@ -38,16 +72,6 @@ async def test_form(hass: HomeAssistant) -> None:
     assert result2["title"] == USERNAME
     assert result2["data"] == {CONF_USERNAME: USERNAME, CONF_PASSWORD: PASSWORD}
     assert len(mock_setup_entry.mock_calls) == 1
-
-
-@pytest.fixture(name="validate_input")
-def mock_controller():
-    """Mock a successful _host_in_configuration_exists."""
-    with patch(
-        "homeassistant.components.solarlog.config_flow.validate_input",
-        return_value={CONF_USERNAME: USERNAME, CONF_PASSWORD: PASSWORD},
-    ):
-        yield
 
 
 def init_config_flow(hass: HomeAssistant):
@@ -79,18 +103,44 @@ async def test_user(hass: HomeAssistant) -> None:
     assert result["data"][CONF_PASSWORD] == PASSWORD
 
 
-async def test_reauth(hass: HomeAssistant) -> None:
+async def test_reauth(hass: HomeAssistant, mock_fyta_connector: Mock) -> None:
     """Test reauthentification."""
-    flow = init_config_flow(hass)
+    init_config_flow(hass)
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        title=USERNAME,
+        unique_id=USERNAME,
+        data={
+            CONF_USERNAME: USERNAME,
+            CONF_PASSWORD: PASSWORD,
+        },
+    )
+    entry.add_to_hass(hass)
+
+    # start reauth flow
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_REAUTH, "entry_id": entry.entry_id},
+        data=entry.data,
+    )
+    assert result["type"] == data_entry_flow.FlowResultType.FORM
+    assert result["step_id"] == "reauth_confirm"
 
     with patch(
         "homeassistant.components.fyta.config_flow.validate_input",
         return_value={"title": USERNAME},
     ):
-        result = await flow.async_step_reauth_confirm(
-            {CONF_USERNAME: USERNAME, CONF_PASSWORD: PASSWORD}
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_USERNAME: "other_user",
+                CONF_PASSWORD: "other_password",
+            },
         )
-    assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
-    assert result["title"] == USERNAME
-    assert result["data"][CONF_USERNAME] == USERNAME
-    assert result["data"][CONF_PASSWORD] == PASSWORD
+        await hass.async_block_till_done()
+
+    assert result["type"] == data_entry_flow.FlowResultType.ABORT
+    assert result["reason"] == "reauth_successful"
+    assert entry.data[CONF_USERNAME] == "other_user"
+    assert entry.data[CONF_PASSWORD] == "other_password"
