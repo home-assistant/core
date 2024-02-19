0be7135f8b5b982eeceaa3457ff6857d991fc043
Avishay Orpaz <avishorp@gmail.com>
2024-02-19 13:30:07 +0200

    Format

--- homeassistant/components/group/light.py
+++ homeassistant/components/group/light.py
@@ -1,4 +1,5 @@
 """Platform allowing several lights to be grouped into one light."""
+
 from __future__ import annotations
 
 from collections import Counter
@@ -50,7 +51,10 @@ from homeassistant.core import HomeAssistant, callback, State
 from homeassistant.helpers import config_validation as cv, entity_registry as er
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType, EventType
-from homeassistant.helpers.event import async_track_state_change_event, EventStateChangedData
+from homeassistant.helpers.event import (
+    async_track_state_change_event,
+    EventStateChangedData,
+)
 
 from . import GroupEntity
 from .util import find_state_attributes, mean_tuple, reduce_attribute
@@ -68,7 +72,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_UNIQUE_ID): cv.string,
         vol.Required(CONF_ENTITIES): cv.entities_domain(light.DOMAIN),
         vol.Optional(CONF_ALL): cv.boolean,
-        vol.Optional(CONF_SYNC): cv.boolean
+        vol.Optional(CONF_SYNC): cv.boolean,
     }
 )
 
@@ -94,7 +98,7 @@ async def async_setup_platform(
                 config[CONF_NAME],
                 config[CONF_ENTITIES],
                 config.get(CONF_ALL),
-                config.get(CONF_SYNC)
+                config.get(CONF_SYNC),
             )
         ]
     )
@@ -114,7 +118,11 @@ async def async_setup_entry(
     sync = config_entry.options.get(CONF_SYNC, False)
 
     async_add_entities(
-        [LightGroup(hass, config_entry.entry_id, config_entry.title, entities, mode, sync)]
+        [
+            LightGroup(
+                hass, config_entry.entry_id, config_entry.title, entities, mode, sync
+            )
+        ]
     )
 
 
@@ -160,7 +168,13 @@ class LightGroup(GroupEntity, LightEntity):
     _attr_should_poll = False
 
     def __init__(
-        self, hass: HomeAssistant, unique_id: str | None, name: str, entity_ids: list[str], mode: bool | None, sync: bool | None
+        self,
+        hass: HomeAssistant,
+        unique_id: str | None,
+        name: str,
+        entity_ids: list[str],
+        mode: bool | None,
+        sync: bool | None,
     ) -> None:
         """Initialize a light group."""
         self._entity_ids = entity_ids
@@ -180,9 +194,9 @@ class LightGroup(GroupEntity, LightEntity):
         if self.sync:
             unsub = async_track_state_change_event(
                 hass, entity_ids, self.watched_entity_change
-            )           
+            )
             self.async_on_remove(unsub)
-        
+
         self._target_sync_state: str | None = None
 
     async def async_turn_on(self, **kwargs: Any) -> None:
@@ -333,10 +347,12 @@ class LightGroup(GroupEntity, LightEntity):
             state = nst.state
             if state in (STATE_ON, STATE_OFF) and state != self._target_sync_state:
                 self._target_sync_state = state
-                
+
                 # Call the appropriate service for all the other lights in the group
                 data = {
-                    ATTR_ENTITY_ID: list(set(self._entity_ids).difference(set([nst.entity_id])))
+                    ATTR_ENTITY_ID: list(
+                        set(self._entity_ids).difference(set([nst.entity_id]))
+                    )
                 }
 
                 await self.hass.services.async_call(
