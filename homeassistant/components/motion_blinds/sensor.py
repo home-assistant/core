11921345eef21f094227e3653f71f661c442d662
some-guy-23 <some-guy-23@users.noreply.github.com>
2024-02-20 09:40:27 -0500

    Added State Class to Motion Blinds Battery Sensor
    Currently the battery sensor for Motion Blinds is not saved to Long Term Statistics, I believe this is due to not having an assigned "State Class" as mentioned here -
    
    https://developers.home-assistant.io/docs/core/entity/sensor/#long-term-statistics

--- homeassistant/components/motion_blinds/sensor.py
+++ homeassistant/components/motion_blinds/sensor.py
@@ -50,6 +50,7 @@ class MotionBatterySensor(MotionCoordinatorEntity, SensorEntity):
     _attr_device_class = SensorDeviceClass.BATTERY
     _attr_native_unit_of_measurement = PERCENTAGE
     _attr_entity_category = EntityCategory.DIAGNOSTIC
+    _attr_state_class = SensorStateClass.MEASUREMENT
 
     def __init__(self, coordinator, blind):
         """Initialize the Motion Battery Sensor."""
