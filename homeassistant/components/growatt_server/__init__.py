66028a661a50eb4e94d924c02898f0cc1ddf7d0f
Nick Harrison <njharrison@gmail.com>
2024-02-21 16:52:21 +0000

    Added set_battery_first code and fixed const import

--- homeassistant/components/growatt_server/__init__.py
+++ homeassistant/components/growatt_server/__init__.py
@@ -2,12 +2,11 @@
 from datetime import time
 import logging
 
-import const
-
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, ServiceCall
 
+from . import const
 from .api import get_configured_api
 from .plant import get_inverter_list
 
@@ -61,6 +60,51 @@ async def async_setup_entry(
 
         response = api.update_mix_inverter_setting(
             device_sn, "mix_ac_discharge_time_period", schedule_settings
+        )
+
+        process_response(response)
+
+    def set_battery_first(call: ServiceCall) -> None:
+        """Set inverter to charge battery (from grid)."""
+
+        config = {**config_entry.data}
+
+        api = get_configured_api(hass, config_entry)
+
+        [inverter_list, _] = get_inverter_list(api, config)
+
+        device_sn = inverter_list[0]["deviceSn"]
+
+        sTime1 = call.data.get(const.TIME_SLOT_1_START, time())
+        sTime2 = call.data.get(const.TIME_SLOT_2_START, time())
+        sTime3 = call.data.get(const.TIME_SLOT_3_START, time())
+        eTime1 = call.data.get(const.TIME_SLOT_1_END, time())
+        eTime2 = call.data.get(const.TIME_SLOT_2_END, time())
+        eTime3 = call.data.get(const.TIME_SLOT_3_END, time())
+
+        schedule_settings = [
+            int(call.data[const.CHARGE_POWER_RATE]),
+            str(call.data[const.CHARGE_STOPPED_SOC]),
+            str(int(call.data[const.AC_CHARGE])),
+            f"{sTime1.hour:02d}",
+            f"{sTime1.minute:02d}",
+            f"{eTime1.hour:02d}",
+            f"{eTime1.minute:02d}",
+            str(int(call.data[const.TIME_SLOT_1_ENABLED])),
+            f"{sTime2.hour:02d}",
+            f"{sTime2.minute:02d}",
+            f"{eTime2.hour:02d}",
+            f"{eTime2.minute:02d}",
+            str(int(call.data[const.TIME_SLOT_2_ENABLED])),
+            f"{sTime3.hour:02d}",
+            f"{sTime3.minute:02d}",
+            f"{eTime3.hour:02d}",
+            f"{eTime3.minute:02d}",
+            str(int(call.data[const.TIME_SLOT_3_ENABLED])),
+        ]
+
+        response = api.update_mix_inverter_setting(
+            device_sn, "mix_ac_charge_time_period", schedule_settings
         )
 
         process_response(response)
--- homeassistant/components/growatt_server/const.py
+++ homeassistant/components/growatt_server/const.py
@@ -27,3 +27,23 @@ DOMAIN = "growatt_server"
 PLATFORMS = [Platform.SENSOR]
 
 LOGIN_INVALID_AUTH_CODE = "502"
+
+SET_GRID_FIRST = "set_grid_first"
+SET_BATTERY_FIRST = "set_battery_first"
+
+DISCHARGE_POWER_RATE = "discharge_power_rate"
+DISCHARGE_STOPPED_SOC = "discharge_stopped_soc"
+
+CHARGE_POWER_RATE = "charge_power_rate"
+CHARGE_STOPPED_SOC = "charge_stopped_soc"
+AC_CHARGE = "ac_charge"
+
+TIME_SLOT_1_START = "time_slot_1_start"
+TIME_SLOT_1_END = "time_slot_1_end"
+TIME_SLOT_1_ENABLED = "time_slot_1_enabled"
+TIME_SLOT_2_START = "time_slot_2_start"
+TIME_SLOT_2_END = "time_slot_2_end"
+TIME_SLOT_2_ENABLED = "time_slot_2_enabled"
+TIME_SLOT_3_START = "time_slot_3_start"
+TIME_SLOT_3_END = "time_slot_3_end"
+TIME_SLOT_3_ENABLED = "time_slot_3_enabled"
