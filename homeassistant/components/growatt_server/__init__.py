7ac6ff363c16d8d7c2413088f1f8b153f0ecc4d8
Nick Harrison <njharrison@gmail.com>
2024-02-21 16:21:04 +0000

    Renaming "entry" to "config_entry" for consistency with other usage

--- homeassistant/components/growatt_server/__init__.py
+++ homeassistant/components/growatt_server/__init__.py
@@ -7,14 +7,14 @@ from .const import PLATFORMS
 
 
 async def async_setup_entry(
-    hass: HomeAssistant, entry: config_entries.ConfigEntry
+    hass: HomeAssistant, config_entry: config_entries.ConfigEntry
 ) -> bool:
     """Load the saved entities."""
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
