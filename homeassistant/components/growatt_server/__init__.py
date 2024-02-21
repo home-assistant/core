43a3fb9a5327ab9d201ed4d60347dbf02d17f1a2
Nick Harrison <njharrison@gmail.com>
2024-02-21 16:26:30 +0000

    Created a Service Call to set the inverter to Grid First mode

--- homeassistant/components/growatt_server/__init__.py
+++ homeassistant/components/growatt_server/__init__.py
@@ -1,9 +1,17 @@
 """The Growatt server PV inverter sensor integration."""
+from datetime import time
+import logging
+
+import const
+
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, ServiceCall
 
-from .const import PLATFORMS
+from .api import get_configured_api
+from .plant import get_inverter_list
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
@@ -11,10 +19,67 @@ async def async_setup_entry(
 ) -> bool:
     """Load the saved entities."""
 
-    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(config_entry, const.PLATFORMS)
+
+    def set_grid_first(call: ServiceCall) -> None:
+        """Set inverter to export to grid."""
+
+        config = {**config_entry.data}
+
+        api = get_configured_api(hass, config_entry)
+
+        [inverter_list, _] = get_inverter_list(api, config)
+
+        device_sn = inverter_list[0]["deviceSn"]
+
+        sTime1 = call.data.get(const.TIME_SLOT_1_START, time())
+        sTime2 = call.data.get(const.TIME_SLOT_2_START, time())
+        sTime3 = call.data.get(const.TIME_SLOT_3_START, time())
+        eTime1 = call.data.get(const.TIME_SLOT_1_END, time())
+        eTime2 = call.data.get(const.TIME_SLOT_2_END, time())
+        eTime3 = call.data.get(const.TIME_SLOT_3_END, time())
+
+        schedule_settings = [
+            int(call.data[const.DISCHARGE_POWER_RATE]),
+            str(call.data[const.DISCHARGE_STOPPED_SOC]),
+            f"{sTime1.hour:02d}",
+            f"{sTime1.minute:02d}",
+            f"{eTime1.hour:02d}",
+            f"{eTime1.minute:02d}",
+            str(int(call.data[const.TIME_SLOT_1_ENABLED])),
+            f"{sTime2.hour:02d}",
+            f"{sTime2.minute:02d}",
+            f"{eTime2.hour:02d}",
+            f"{eTime2.minute:02d}",
+            str(int(call.data[const.TIME_SLOT_2_ENABLED])),
+            f"{sTime3.hour:02d}",
+            f"{sTime3.minute:02d}",
+            f"{eTime3.hour:02d}",
+            f"{eTime3.minute:02d}",
+            str(int(call.data[const.TIME_SLOT_3_ENABLED])),
+        ]
+
+        response = api.update_mix_inverter_setting(
+            device_sn, "mix_ac_discharge_time_period", schedule_settings
+        )
+
+        process_response(response)
+
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
+    return await hass.config_entries.async_unload_platforms(
+        config_entry, const.PLATFORMS
+    )
+
+
+def process_response(response) -> bool:
+    """Process an HTTP response."""
+
+    if not response["success"]:
+        _LOGGER.error("Error setting inverter modes")
+        return False
+
+    return True
