66613673a8cbf3e74a00d4485e6673b96b05d167
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-02-22 12:34:24 +0100

    Add switch platform for husqvarna_automower (#110139)
    * Add switch platform for husqvarna_automower
    
    * Use RestrictedReasons const
    
    * Typing
    
    * Add snapshot testing
    
    * Invert switch
    
    * Test sucessfull servie calls
    
    * Assert client mock calls
    
    * Use getattr
    
    * Update snapshot
    
    * Add available property
    
    * Add a new base class for control entities
    
    * Make switch unavailabe if mower in error state
    
    * Sort platforms
    
    ---------
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/husqvarna_automower/__init__.py
+++ homeassistant/components/husqvarna_automower/__init__.py
@@ -18,7 +18,7 @@ from .coordinator import AutomowerDataUpdateCoordinator
 _LOGGER = logging.getLogger(__name__)
 
 
-PLATFORMS: list[Platform] = [Platform.LAWN_MOWER, Platform.SENSOR]
+PLATFORMS: list[Platform] = [Platform.LAWN_MOWER, Platform.SENSOR, Platform.SWITCH]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
