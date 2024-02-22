38bd599d20949dfe4aa123e53995d0d0ae018581
Joakim Sørensen <ludeeus@ludeeus.dev>
2024-02-22 16:08:30 +0100

    Revert "Move backup/* WS commands to the backup integration" (#111136)
    Revert "Move backup/* WS commands to the backup integration (#110651)"
    
    This reverts commit d2a7c51c979a84ea5b5247770257d0ef4e6c54bf.

--- homeassistant/bootstrap.py
+++ homeassistant/bootstrap.py
@@ -115,7 +115,6 @@ DEFAULT_INTEGRATIONS = {
     #
     # Integrations providing core functionality:
     "application_credentials",
-    "backup",
     "frontend",
     "hardware",
     "logger",
@@ -148,6 +147,10 @@ DEFAULT_INTEGRATIONS_RECOVERY_MODE = {
 DEFAULT_INTEGRATIONS_SUPERVISOR = {
     # These integrations are set up if using the Supervisor
     "hassio",
+}
+DEFAULT_INTEGRATIONS_NON_SUPERVISOR = {
+    # These integrations are set up if not using the Supervisor
+    "backup",
 }
 CRITICAL_INTEGRATIONS = {
     # Recovery mode is activated if these integrations fail to set up
@@ -538,6 +541,8 @@ def _get_domains(hass: core.HomeAssistant, config: dict[str, Any]) -> set[str]:
     # Add domains depending on if the Supervisor is used or not
     if "SUPERVISOR" in os.environ:
         domains.update(DEFAULT_INTEGRATIONS_SUPERVISOR)
+    else:
+        domains.update(DEFAULT_INTEGRATIONS_NON_SUPERVISOR)
 
     return domains
 
