ec4e6c3a7435400cf1a9afdf2c84165be957f294
Joakim Sørensen <ludeeus@ludeeus.dev>
2024-02-22 10:25:38 +0100

    Move backup/* WS commands to the backup integration (#110651)
    * Move backup/* WS commands to the backup integration
    
    * Call correct command
    
    * Use debug for logging
    
    * Remove assertion of hass.data for setup test
    
    * parametrize token fixture

--- homeassistant/bootstrap.py
+++ homeassistant/bootstrap.py
@@ -115,6 +115,7 @@ DEFAULT_INTEGRATIONS = {
     #
     # Integrations providing core functionality:
     "application_credentials",
+    "backup",
     "frontend",
     "hardware",
     "logger",
@@ -147,10 +148,6 @@ DEFAULT_INTEGRATIONS_RECOVERY_MODE = {
 DEFAULT_INTEGRATIONS_SUPERVISOR = {
     # These integrations are set up if using the Supervisor
     "hassio",
-}
-DEFAULT_INTEGRATIONS_NON_SUPERVISOR = {
-    # These integrations are set up if not using the Supervisor
-    "backup",
 }
 CRITICAL_INTEGRATIONS = {
     # Recovery mode is activated if these integrations fail to set up
@@ -541,8 +538,6 @@ def _get_domains(hass: core.HomeAssistant, config: dict[str, Any]) -> set[str]:
     # Add domains depending on if the Supervisor is used or not
     if "SUPERVISOR" in os.environ:
         domains.update(DEFAULT_INTEGRATIONS_SUPERVISOR)
-    else:
-        domains.update(DEFAULT_INTEGRATIONS_NON_SUPERVISOR)
 
     return domains
 
