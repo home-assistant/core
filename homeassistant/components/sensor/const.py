f246645eff25752b39428ece7fe9b7c911bd1c74
Carlos Gustavo Sarmiento <carlos5678@gmail.com>
2024-02-24 16:32:22 +0100

    Allow Monetary device_class to accept `Measurement` state_class

--- homeassistant/components/sensor/const.py
+++ homeassistant/components/sensor/const.py
@@ -606,7 +606,7 @@ DEVICE_CLASS_STATE_CLASSES: dict[SensorDeviceClass, set[SensorStateClass]] = {
     SensorDeviceClass.ILLUMINANCE: {SensorStateClass.MEASUREMENT},
     SensorDeviceClass.IRRADIANCE: {SensorStateClass.MEASUREMENT},
     SensorDeviceClass.MOISTURE: {SensorStateClass.MEASUREMENT},
-    SensorDeviceClass.MONETARY: {SensorStateClass.TOTAL},
+    SensorDeviceClass.MONETARY: {SensorStateClass.MEASUREMENT, SensorStateClass.TOTAL},
     SensorDeviceClass.NITROGEN_DIOXIDE: {SensorStateClass.MEASUREMENT},
     SensorDeviceClass.NITROGEN_MONOXIDE: {SensorStateClass.MEASUREMENT},
     SensorDeviceClass.NITROUS_OXIDE: {SensorStateClass.MEASUREMENT},
