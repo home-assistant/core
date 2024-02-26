515c1bb4e0f61ba9ab2fdcbe0c7e3314742afa96
dontinelli <73341522+dontinelli@users.noreply.github.com>
2024-02-26 11:48:24 +0100

    Update coordinator.py
    corrected typo

--- homeassistant/components/fyta/coordinator.py
+++ homeassistant/components/fyta/coordinator.py
@@ -19,7 +19,9 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 _LOGGER = logging.getLogger(__name__)
 
 
-class FytaCoordinator(DataUpdateCoordinator[dict[int | str, int | str | bool, dict[str, Any]]]):
+class FytaCoordinator(
+    DataUpdateCoordinator[dict[int | str, int | str | bool | dict[str, Any]]]
+):
     """Fyta custom coordinator."""
 
     config_entry: ConfigEntry
@@ -34,7 +36,9 @@ class FytaCoordinator(DataUpdateCoordinator[dict[int | str, int | str | bool, di
         )
         self.fyta = fyta
 
-    async def _async_update_data(self) -> dict[int | str, int | str | bool, dict[str, Any]]:
+    async def _async_update_data(
+        self,
+    ) -> dict[int | str, int | str | bool | dict[str, Any]]:
         """Fetch data from API endpoint."""
 
         if self.fyta.expiration is None or self.fyta.expiration < datetime.now():
