821392e71ecd5c2371905e84bbfb251bc7a15a64
Erwin Douna <e.douna@gmail.com>
2024-02-26 20:18:31 +0000

    Add diagnostics

---
+++ homeassistant/components/fastdotcom/diagnostics.py
@@ -0,0 +1,27 @@
+"""Diagnostics support for Fast.com."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+from .coordinator import FastdotcomDataUpdateCoordinator
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for the config entry."""
+    coordinator: FastdotcomDataUpdateCoordinator = hass.data[DOMAIN][
+        config_entry.entry_id
+    ]
+
+    diagnostics_data = {
+        "config_entry_data": dict(config_entry.data),
+        "coordinator_data": coordinator.data,
+    }
+
+    return diagnostics_data
---
+++ tests/components/fastdotcom/test_diagnostics.py
@@ -0,0 +1,43 @@
+"""Test the Fast.com component diagnostics."""
+
+from unittest.mock import patch
+
+from syrupy import SnapshotAssertion
+
+from homeassistant.components.fastdotcom.const import DEFAULT_NAME, DOMAIN
+from homeassistant.config_entries import SOURCE_USER
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+from tests.components.diagnostics import get_diagnostics_for_config_entry
+from tests.typing import ClientSessionGenerator
+
+
+async def test_get_config_entry_diagnostics(
+    hass: HomeAssistant,
+    hass_client: ClientSessionGenerator,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test if get_config_entry_diagnostics returns the correct data."""
+    config_entry = MockConfigEntry(
+        version=1,
+        domain=DOMAIN,
+        title=DEFAULT_NAME,
+        source=SOURCE_USER,
+        options={},
+        entry_id="TEST_ENTRY_ID",
+        unique_id="UNIQUE_TEST_ID",
+        minor_version=1,
+    )
+    config_entry.add_to_hass(hass)
+
+    with patch(
+        "homeassistant.components.fastdotcom.coordinator.fast_com", return_value=50.3
+    ):
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    assert (
+        await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
+        == snapshot
+    )
