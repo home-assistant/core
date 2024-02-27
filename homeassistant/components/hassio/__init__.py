22420d22d2e0bc592266db9827df05b981ef6b9c
Jan Čermák <sairon@users.noreply.github.com>
2024-02-27 21:51:55 +0100

    Add ODROID-M1S as a supported OS hardware board (#111022)

--- homeassistant/components/hassio/__init__.py
+++ homeassistant/components/hassio/__init__.py
@@ -264,6 +264,7 @@ HARDWARE_INTEGRATIONS = {
     "odroid-c2": "hardkernel",
     "odroid-c4": "hardkernel",
     "odroid-m1": "hardkernel",
+    "odroid-m1s": "hardkernel",
     "odroid-n2": "hardkernel",
     "odroid-xu4": "hardkernel",
     "rpi2": "raspberry_pi",
