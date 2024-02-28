92bbce727df5d5988a9e386c1fb1aa581cae02db
Bram Kragten <mail@bramkragten.nl>
2024-02-29 00:46:32 +0100

    Bump version to 2024.4.0.dev0 (#111755)

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -15,7 +15,7 @@ from .helpers.deprecation import (
 
 APPLICATION_NAME: Final = "HomeAssistant"
 MAJOR_VERSION: Final = 2024
-MINOR_VERSION: Final = 3
+MINOR_VERSION: Final = 4
 PATCH_VERSION: Final = "0.dev0"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"
