6d019d1fd13ceb232f9380eaffbe59cd00552372
paul-enno <paul.enno@gmail.com>
2024-02-28 11:01:52 +0000

    Added conversation agend

--- homeassistant/components/bedrock_agent/__init__.py
+++ homeassistant/components/bedrock_agent/__init__.py
@@ -1,28 +1,55 @@
-"""The Amazon Bedrock Agent integration."""
+"""The Bedrock Agent integration."""
 from __future__ import annotations
 
+import logging
+from typing import Literal
+
+from homeassistant.components import conversation
+from homeassistant.components.conversation import agent
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
+from homeassistant.const import MATCH_ALL
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import intent
 
 from .const import DOMAIN
 
-PLATFORMS: list[Platform] = [Platform.LIGHT]
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Amazon Bedrock Agent from a config entry."""
+    """Set up Bedrock Agent from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    conversation.async_set_agent(hass, entry, BedrockAgent(hass, entry))
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
+    conversation.async_unset_agent(hass, entry)
+    return True
 
-    return unload_ok
+
+class BedrockAgent(conversation.AbstractConversationAgent):
+    """Bedrock conversation agent."""
+
+    def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
+        """Initialize the agent."""
+        self.hass = hass
+        self.entry = entry
+        self.history: dict[str, list[dict]] = {}
+
+    @property
+    def supported_languages(self) -> list[str] | Literal["*"]:
+        """Return a list of supported languages."""
+        return MATCH_ALL
+
+    async def async_process(
+        self, user_input: agent.ConversationInput
+    ) -> agent.ConversationResult:
+        """Process a sentence."""
+        _LOGGER.info("User input")
+        response = intent.IntentResponse(language=user_input.language)
+        response.async_set_speech("Test response")
+        return agent.ConversationResult(conversation_id=None, response=response)
--- homeassistant/components/bedrock_agent/config_flow.py
+++ homeassistant/components/bedrock_agent/config_flow.py
@@ -63,7 +63,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
     # InvalidAuth
 
     # Return info that you want to store in the config entry.
-    return {"title": "Name of the device"}
+    return {"title": "Bedrock"}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
