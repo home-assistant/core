d21a24ca76de501e037f7f55e277f0e13f0c3e2b
J. Nick Koston <nick@koston.org>
2024-02-29 11:16:46 -1000

    remove unused logger

--- homeassistant/components/http/view.py
+++ homeassistant/components/http/view.py
@@ -1,11 +1,7 @@
 """Support for views."""
 from __future__ import annotations
 
-import logging
-
 from homeassistant.helpers.http import (  # noqa: F401
     HomeAssistantView,
     request_handler_factory,
 )
-
-_LOGGER = logging.getLogger(__name__)
