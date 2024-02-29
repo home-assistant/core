29adf93bf6b171b4154f046ae44ef88307a4cfd5
J. Nick Koston <nick@koston.org>
2024-02-29 06:53:12 -1000

    coverage

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1074,6 +1074,46 @@ async def test_async_get_component_deadlock_fallback(
     assert module is module_mock
 
 
+async def test_async_get_component_raises_after_import_failure(
+    hass: HomeAssistant, caplog: pytest.LogCaptureFixture
+) -> None:
+    """Verify async_get_component raises if we fail to import in both the executor and loop."""
+    executor_import_integration = _get_test_integration(
+        hass, "executor_import", True, import_executor=True
+    )
+    assert executor_import_integration.import_executor is True
+    module_mock = MagicMock()
+    import_attempts = 0
+
+    def mock_import(module: str, *args: Any, **kwargs: Any) -> Any:
+        nonlocal import_attempts
+        if module == "homeassistant.components.executor_import":
+            import_attempts += 1
+
+        if import_attempts == 1:
+            # _DeadlockError inherits from RuntimeError
+            raise RuntimeError(
+                "Detected deadlock trying to import homeassistant.components.executor_import"
+            )
+
+        if import_attempts == 2:
+            raise ImportError("Failed import homeassistant.components.executor_import")
+        return module_mock
+
+    assert "homeassistant.components.executor_import" not in sys.modules
+    assert "custom_components.executor_import" not in sys.modules
+    with patch(
+        "homeassistant.loader.importlib.import_module", mock_import
+    ), pytest.raises(ImportError):
+        await executor_import_integration.async_get_component()
+
+    assert (
+        "Detected deadlock trying to import homeassistant.components.executor_import"
+        in caplog.text
+    )
+    assert "loaded_executor=False" not in caplog.text
+
+
 async def test_async_get_platform_deadlock_fallback(
     hass: HomeAssistant, caplog: pytest.LogCaptureFixture
 ) -> None:
@@ -1109,3 +1149,47 @@ async def test_async_get_platform_deadlock_fallback(
     )
     assert "loaded_executor=False" in caplog.text
     assert module is module_mock
+
+
+async def test_async_get_platform_raises_after_import_failure(
+    hass: HomeAssistant, caplog: pytest.LogCaptureFixture
+) -> None:
+    """Verify async_get_platform raises if we fail to import in both the executor and loop."""
+    executor_import_integration = _get_test_integration(
+        hass, "executor_import", True, import_executor=True
+    )
+    assert executor_import_integration.import_executor is True
+    module_mock = MagicMock()
+    import_attempts = 0
+
+    def mock_import(module: str, *args: Any, **kwargs: Any) -> Any:
+        nonlocal import_attempts
+        if module == "homeassistant.components.executor_import.config_flow":
+            import_attempts += 1
+
+        if import_attempts == 1:
+            # _DeadlockError inherits from RuntimeError
+            raise RuntimeError(
+                "Detected deadlock trying to import homeassistant.components.executor_import"
+            )
+
+        if import_attempts == 2:
+            # _DeadlockError inherits from RuntimeError
+            raise ImportError(
+                "Error trying to import homeassistant.components.executor_import"
+            )
+
+        return module_mock
+
+    assert "homeassistant.components.executor_import" not in sys.modules
+    assert "custom_components.executor_import" not in sys.modules
+    with patch(
+        "homeassistant.loader.importlib.import_module", mock_import
+    ), pytest.raises(ImportError):
+        await executor_import_integration.async_get_platform("config_flow")
+
+    assert (
+        "Detected deadlock trying to import homeassistant.components.executor_import"
+        in caplog.text
+    )
+    assert "loaded_executor=False" not in caplog.text
