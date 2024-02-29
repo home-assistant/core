fa18065e3bd8310281bfbd7b80dac690ffac34ea
David F. Mulcahey <david.mulcahey@me.com>
2024-02-29 10:38:21 -0500

    Add support for ZHA entities exposed by Zigpy quirks (#111176)
    * Add counter entities to the ZHA coordinator device
    
    * rework to prepare for non coordinator device counters
    
    * Initial scaffolding to support quirks v2 entities
    
    * update for zigpy changes
    
    * add assertion error message
    
    * clean up test
    
    * update group entity discovery kwargs
    
    * constants and clearer names
    
    * apply custom device configuration
    
    * quirks switches
    
    * quirks select entities
    
    * quirks sensor entities
    
    * update discovery
    
    * move call to super
    
    * add complex quirks v2 discovery test
    
    * remove duplicate replaces
    
    * add quirks v2 button entity support
    
    * add quirks v2 binary sensor entity support
    
    * fix exception in counter entitiy discovery
    
    * oops
    
    * update formatting
    
    * support custom on and off values
    
    * logging
    
    * don't filter out entities quirks says should be created
    
    * fix type alias warnings
    
    * sync up with zigpy changes and additions
    
    * add a binary sensor test
    
    * button coverage
    
    * switch coverage
    
    * initial select coverage
    
    * number coverage
    
    * sensor coverage
    
    * update discovery after rebase
    
    * coverage
    
    * single line
    
    * line lengths
    
    * fix double underscore
    
    * review comments
    
    * set category from quirks in base entity
    
    * line lengths
    
    * move comment
    
    * imports
    
    * simplify
    
    * simplify

--- homeassistant/components/zha/core/const.py
+++ homeassistant/components/zha/core/const.py
@@ -64,6 +64,8 @@ ATTR_WARNING_DEVICE_STROBE_INTENSITY = "intensity"
 BAUD_RATES = [2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200, 128000, 256000]
 BINDINGS = "bindings"
 
+CLUSTER_DETAILS = "cluster_details"
+
 CLUSTER_HANDLER_ACCELEROMETER = "accelerometer"
 CLUSTER_HANDLER_BINARY_INPUT = "binary_input"
 CLUSTER_HANDLER_ANALOG_INPUT = "analog_input"
@@ -229,6 +231,10 @@ POWER_BATTERY_OR_UNKNOWN = "Battery or Unknown"
 PRESET_SCHEDULE = "Schedule"
 PRESET_COMPLEX = "Complex"
 PRESET_TEMP_MANUAL = "Temporary manual"
+
+QUIRK_METADATA = "quirk_metadata"
+
+ZCL_INIT_ATTRS = "ZCL_INIT_ATTRS"
 
 ZHA_ALARM_OPTIONS = "zha_alarm_options"
 ZHA_OPTIONS = "zha_options"
