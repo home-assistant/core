4f0a1fd265a7b4f39e362a8c019a38ba9d1c82fa
J. Nick Koston <nick@koston.org>
2024-02-29 08:29:59 -1000

    Fix circular imports in core integrations

---
+++ tests/test_circular_imports.py
@@ -0,0 +1,17 @@
+"""Test to check for circular imports in core components."""
+import asyncio
+import sys
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "component", ("auth", "config", "cloud", "http", "frontend", "websocket_api")
+)
+async def test_circular_imports(component: str) -> None:
+    """Test if we can detect circular dependencies of components."""
+    process = await asyncio.create_subprocess_exec(
+        sys.executable, "-c", f"import homeassistant.components.{component}"
+    )
+    await process.communicate()
+    assert process.returncode == 0
