8f355c11dae3f1c716a59423e8a75f4fd2235dc5
J. Nick Koston <nick@koston.org>
2024-02-29 16:02:13 -1000

    Limit executor imports to a single thread (#111898)
    * Limit executor imports to a single thread
    
    * test for import executor
    
    * test for import executor
    
    * test for import executor
    
    * fixes
    
    * better fix

--- homeassistant/loader.py
+++ homeassistant/loader.py
@@ -853,7 +853,7 @@ class Integration:
         # So we do it before validating config to catch these errors.
         if load_executor:
             try:
-                comp = await self.hass.async_add_executor_job(self.get_component)
+                comp = await self.hass.async_add_import_executor_job(self.get_component)
             except ImportError as ex:
                 load_executor = False
                 _LOGGER.debug("Failed to import %s in executor", domain, exc_info=ex)
@@ -924,7 +924,7 @@ class Integration:
         try:
             if load_executor:
                 try:
-                    platform = await self.hass.async_add_executor_job(
+                    platform = await self.hass.async_add_import_executor_job(
                         self._load_platform, platform_name
                     )
                 except ImportError as ex:
