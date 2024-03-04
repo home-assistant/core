03ad12dd2253fbb65feeaf2a75c6483e26f5fc58
Aidan Timson <contact@timmo.xyz>
2024-03-04 12:15:11 +0000

    Refactor system_bridge migration tests

--- tests/components/system_bridge/test_init.py
+++ tests/components/system_bridge/test_init.py
@@ -10,8 +10,8 @@ from . import FIXTURE_USER_INPUT, FIXTURE_UUID
 from tests.common import MockConfigEntry
 
 
-async def test_migration(hass: HomeAssistant) -> None:
-    """Test migration from system_bridge to system_bridge."""
+async def test_migration_minor_1_to_2(hass: HomeAssistant) -> None:
+    """Test migration."""
     config_entry = MockConfigEntry(
         domain=DOMAIN,
         unique_id=FIXTURE_UUID,
@@ -37,3 +37,29 @@ async def test_migration(hass: HomeAssistant) -> None:
         CONF_PORT: FIXTURE_USER_INPUT[CONF_PORT],
         CONF_TOKEN: FIXTURE_USER_INPUT[CONF_TOKEN],
     }
+
+
+async def test_migration_minor_2_to_1(hass: HomeAssistant) -> None:
+    """Test migration."""
+    config_entry_data = {
+        CONF_API_KEY: FIXTURE_USER_INPUT[CONF_TOKEN],
+        CONF_HOST: FIXTURE_USER_INPUT[CONF_HOST],
+        CONF_PORT: FIXTURE_USER_INPUT[CONF_PORT],
+        CONF_TOKEN: FIXTURE_USER_INPUT[CONF_TOKEN],
+    }
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id=FIXTURE_UUID,
+        data=config_entry_data,
+        version=1,
+        minor_version=2,
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    # Check that the version has been updated
+    assert config_entry.version == SystemBridgeConfigFlow.VERSION
+    assert config_entry.minor_version == SystemBridgeConfigFlow.MINOR_VERSION
+    assert config_entry.data[CONF_API_KEY] == FIXTURE_USER_INPUT[CONF_TOKEN]
+    assert config_entry.data == config_entry_data
