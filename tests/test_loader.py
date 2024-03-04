b8a8954536d7734ba532eb2a29167c6b09b260d4
J. Nick Koston <nick@koston.org>
2024-03-03 20:18:08 -1000

    concurrent platforms load test

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,6 +2,7 @@
 import asyncio
 import os
 import sys
+import threading
 from typing import Any
 from unittest.mock import MagicMock, Mock, patch
 
@@ -1456,3 +1457,60 @@ async def test_async_get_platforms_loads_loop_if_already_in_sys_modules(
         "switch": switch_module_mock,
         "light": light_module_mock,
     }
+
+
+async def test_async_get_platforms_concurrent_loads(
+    hass: HomeAssistant,
+    caplog: pytest.LogCaptureFixture,
+    enable_custom_integrations: None,
+) -> None:
+    """Verify async_get_platforms waits if the first load if called again when still in progress."""
+    integration = await loader.async_get_integration(
+        hass, "test_package_loaded_executor"
+    )
+    assert integration.pkg_path == "custom_components.test_package_loaded_executor"
+    assert integration.import_executor is True
+    assert integration.config_flow is True
+
+    assert "test_package_loaded_executor" not in hass.config.components
+    assert "test_package_loaded_executor.config_flow" not in hass.config.components
+    await integration.async_get_component()
+
+    button_module_name = f"{integration.pkg_path}.button"
+    button_module_mock = MagicMock()
+
+    imports = []
+    start_event = threading.Event()
+    import_event = asyncio.Event()
+
+    def import_module(name: str) -> Any:
+        hass.loop.call_soon_threadsafe(import_event.set)
+        imports.append(name)
+        start_event.wait()
+        if name == button_module_name:
+            return button_module_mock
+        raise ImportError
+
+    modules_without_button = {
+        k: v
+        for k, v in sys.modules.items()
+        if k != button_module_name and k != integration.pkg_path
+    }
+    with patch.dict(
+        "sys.modules",
+        modules_without_button,
+        clear=True,
+    ), patch("homeassistant.loader.importlib.import_module", import_module):
+        load_task1 = asyncio.create_task(integration.async_get_platforms(["button"]))
+        load_task2 = asyncio.create_task(integration.async_get_platforms(["button"]))
+        await import_event.wait()  # make sure the import is started
+        assert not integration._import_futures["button"].done()
+        start_event.set()
+        load_result1 = await load_task1
+        load_result2 = await load_task2
+        assert integration._import_futures is not None
+
+    assert load_result1 == {"button": button_module_mock}
+    assert load_result2 == {"button": button_module_mock}
+
+    assert imports == [button_module_name]
