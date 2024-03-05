446314bb27d5f6578d0d5c84d2fb43eca11774fb
Joost Lekkerkerker <joostlek@outlook.com>
2024-03-05 14:20:50 +0100

    Add icon translations to Syncthing (#112297)

--- homeassistant/components/syncthing/sensor.py
+++ homeassistant/components/syncthing/sensor.py
@@ -13,9 +13,6 @@ from homeassistant.helpers.event import async_track_time_interval
 from .const import (
     DOMAIN,
     FOLDER_PAUSED_RECEIVED,
-    FOLDER_SENSOR_ALERT_ICON,
-    FOLDER_SENSOR_DEFAULT_ICON,
-    FOLDER_SENSOR_ICONS,
     FOLDER_SUMMARY_RECEIVED,
     SCAN_INTERVAL,
     SERVER_AVAILABLE,
@@ -57,6 +54,7 @@ class FolderSensor(SensorEntity):
     """A Syncthing folder sensor."""
 
     _attr_should_poll = False
+    _attr_translation_key = "syncthing"
 
     STATE_ATTRIBUTES = {
         "errors": "errors",
@@ -115,15 +113,6 @@ class FolderSensor(SensorEntity):
     def available(self):
         """Could the device be accessed during the last update call."""
         return self._state is not None
-
-    @property
-    def icon(self):
-        """Return the icon for this sensor."""
-        if self._state is None:
-            return FOLDER_SENSOR_DEFAULT_ICON
-        if self.state in FOLDER_SENSOR_ICONS:
-            return FOLDER_SENSOR_ICONS[self.state]
-        return FOLDER_SENSOR_ALERT_ICON
 
     @property
     def extra_state_attributes(self):
