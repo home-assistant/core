ac2400d71c853051c997e768196f20155b0e82e5
Joostlek <joostlek@outlook.com>
2024-03-05 13:22:12 +0100

    Add icon translations to Whois

--- homeassistant/components/whois/sensor.py
+++ homeassistant/components/whois/sensor.py
@@ -61,7 +61,6 @@ SENSORS: tuple[WhoisSensorEntityDescription, ...] = (
     WhoisSensorEntityDescription(
         key="admin",
         translation_key="admin",
-        icon="mdi:account-star",
         entity_category=EntityCategory.DIAGNOSTIC,
         entity_registry_enabled_default=False,
         value_fn=lambda domain: getattr(domain, "admin", None),
@@ -76,7 +75,6 @@ SENSORS: tuple[WhoisSensorEntityDescription, ...] = (
     WhoisSensorEntityDescription(
         key="days_until_expiration",
         translation_key="days_until_expiration",
-        icon="mdi:calendar-clock",
         native_unit_of_measurement=UnitOfTime.DAYS,
         value_fn=_days_until_expiration,
     ),
@@ -97,7 +95,6 @@ SENSORS: tuple[WhoisSensorEntityDescription, ...] = (
     WhoisSensorEntityDescription(
         key="owner",
         translation_key="owner",
-        icon="mdi:account",
         entity_category=EntityCategory.DIAGNOSTIC,
         entity_registry_enabled_default=False,
         value_fn=lambda domain: getattr(domain, "owner", None),
@@ -105,7 +102,6 @@ SENSORS: tuple[WhoisSensorEntityDescription, ...] = (
     WhoisSensorEntityDescription(
         key="registrant",
         translation_key="registrant",
-        icon="mdi:account-edit",
         entity_category=EntityCategory.DIAGNOSTIC,
         entity_registry_enabled_default=False,
         value_fn=lambda domain: getattr(domain, "registrant", None),
@@ -113,7 +109,6 @@ SENSORS: tuple[WhoisSensorEntityDescription, ...] = (
     WhoisSensorEntityDescription(
         key="registrar",
         translation_key="registrar",
-        icon="mdi:store",
         entity_category=EntityCategory.DIAGNOSTIC,
         entity_registry_enabled_default=False,
         value_fn=lambda domain: domain.registrar if domain.registrar else None,
@@ -121,7 +116,6 @@ SENSORS: tuple[WhoisSensorEntityDescription, ...] = (
     WhoisSensorEntityDescription(
         key="reseller",
         translation_key="reseller",
-        icon="mdi:store",
         entity_category=EntityCategory.DIAGNOSTIC,
         entity_registry_enabled_default=False,
         value_fn=lambda domain: getattr(domain, "reseller", None),
