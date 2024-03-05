0c6df2aca70dd68d7788ae615738385774b87f54
J. Nick Koston <nick@koston.org>
2024-03-04 18:35:07 -1000

    failure during EVENT_COMPONENT_LOADED

--- tests/helpers/test_integration_platform.py
+++ tests/helpers/test_integration_platform.py
@@ -95,6 +95,42 @@ async def test_process_integration_platforms_import_fails(
     assert len(processed) == 1
 
 
+async def test_process_integration_platforms_import_fails_after_registered(
+    hass: HomeAssistant, caplog: pytest.LogCaptureFixture
+) -> None:
+    """Test processing integrations when one fails to import."""
+    loaded_platform = Mock()
+    mock_platform(hass, "loaded.platform_to_check", loaded_platform)
+    hass.config.components.add("loaded")
+
+    event_platform = Mock()
+    mock_platform(hass, "event.platform_to_check", event_platform)
+
+    processed = []
+
+    async def _process_platform(hass, domain, platform):
+        """Process platform."""
+        processed.append((domain, platform))
+
+    await async_process_integration_platforms(
+        hass, "platform_to_check", _process_platform
+    )
+
+    assert len(processed) == 1
+    assert processed[0][0] == "loaded"
+    assert processed[0][1] == loaded_platform
+
+    event_integration = await loader.async_get_integration(hass, "event")
+    with patch.object(
+        event_integration, "async_get_platforms", side_effect=ImportError
+    ), patch.object(event_integration, "get_platform_cached", return_value=None):
+        hass.bus.async_fire(EVENT_COMPONENT_LOADED, {ATTR_COMPONENT: "event"})
+        await hass.async_block_till_done()
+
+    assert len(processed) == 1
+    assert "Unexpected error importing integration platforms for event" in caplog.text
+
+
 @callback
 def _process_platform_callback(
     hass: HomeAssistant, domain: str, platform: ModuleType
