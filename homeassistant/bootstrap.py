3e45fdb3fc785cbc2a2e7857ba01bf71b4770c05
J. Nick Koston <nick@koston.org>
2024-03-05 10:47:33 -1000

    Pre import webhook before asyncio starts
    This one ends in stage 1 and other components have to wait
    for it to be imported. Its cheap to import but it ends up
    at the end of the line which means other end up waiting for
    it which is time we could be doing startup work
    
    `2024-03-04 23:13:04.347 INFO (MainThread) [homeassistant.bootstrap] Setting up stage 1: {usb, websocket_api, webhook, zeroconf, bluetooth, ssdp, dhcp, cloud, network, api, http, hassio}`
    
    It currently always has a wait time for the import executor
    
    `2024-03-04 23:13:04.496 DEBUG (MainThread) [homeassistant.loader] Component webhook import took 0.146 seconds (loaded_executor=True)`

--- homeassistant/bootstrap.py
+++ homeassistant/bootstrap.py
@@ -43,6 +43,7 @@ from .components import (
     search as search_pre_import,  # noqa: F401
     sensor as sensor_pre_import,  # noqa: F401
     system_log as system_log_pre_import,  # noqa: F401
+    webhook as webhook_pre_import,  # noqa: F401
     websocket_api as websocket_api_pre_import,  # noqa: F401
 )
 from .const import (
