2ce1b5dd2a46726f695bd999a9ded82757aa79f2
J. Nick Koston <nick@koston.org>
2024-03-04 14:46:18 -1000

    entity has group

--- homeassistant/loader.py
+++ homeassistant/loader.py
@@ -80,7 +80,7 @@ BASE_PRELOAD_PLATFORMS = [
     "trigger",
 ]
 
-SKIP_PRELOAD_INTEGRATION_TYPES = {"entity", "helper", "virtual"}
+SKIP_PRELOAD_INTEGRATION_TYPES = {"helper", "virtual"}
 
 DATA_COMPONENTS = "components"
 DATA_INTEGRATIONS = "integrations"
