3ae33dc41c1fc0338ac4195008aa06ec6d51e703
Aidan Timson <contact@timmo.xyz>
2024-03-05 09:11:52 +0000

    Refactor system bridge migration tests

--- tests/components/system_bridge/test_init.py
+++ tests/components/system_bridge/test_init.py
@@ -1,5 +1,7 @@
 """Test the System Bridge integration."""
 
+from unittest.mock import patch
+
 from homeassistant.components.system_bridge.config_flow import SystemBridgeConfigFlow
 from homeassistant.components.system_bridge.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
@@ -24,9 +26,16 @@ async def test_migration_minor_1_to_2(hass: HomeAssistant) -> None:
         version=SystemBridgeConfigFlow.VERSION,
         minor_version=1,
     )
-    config_entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(config_entry.entry_id)
-    await hass.async_block_till_done()
+
+    with patch(
+        "homeassistant.components.system_bridge.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        config_entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+        assert len(mock_setup_entry.mock_calls) == 1
 
     # Check that the version has been updated and the api_key has been moved to token
     assert config_entry.version == SystemBridgeConfigFlow.VERSION
@@ -38,10 +47,10 @@ async def test_migration_minor_1_to_2(hass: HomeAssistant) -> None:
         CONF_PORT: FIXTURE_USER_INPUT[CONF_PORT],
         CONF_TOKEN: FIXTURE_USER_INPUT[CONF_TOKEN],
     }
-    assert config_entry.state == ConfigEntryState.SETUP_RETRY
+    assert config_entry.state == ConfigEntryState.LOADED
 
 
-async def test_migration_minor_future_to_2(hass: HomeAssistant) -> None:
+async def test_migration_minor_future_version(hass: HomeAssistant) -> None:
     """Test migration."""
     config_entry_data = {
         CONF_API_KEY: FIXTURE_USER_INPUT[CONF_TOKEN],
@@ -56,13 +65,20 @@ async def test_migration_minor_future_to_2(hass: HomeAssistant) -> None:
         version=SystemBridgeConfigFlow.VERSION,
         minor_version=SystemBridgeConfigFlow.MINOR_VERSION + 1,
     )
-    config_entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(config_entry.entry_id)
-    await hass.async_block_till_done()
+
+    with patch(
+        "homeassistant.components.system_bridge.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        config_entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+        assert len(mock_setup_entry.mock_calls) == 1
 
     # Check that the version has been updated
     assert config_entry.version == SystemBridgeConfigFlow.VERSION
-    assert config_entry.minor_version == SystemBridgeConfigFlow.MINOR_VERSION
+    assert config_entry.minor_version == SystemBridgeConfigFlow.MINOR_VERSION + 1
     assert config_entry.data[CONF_API_KEY] == FIXTURE_USER_INPUT[CONF_TOKEN]
     assert config_entry.data == config_entry_data
-    assert config_entry.state == ConfigEntryState.SETUP_RETRY
+    assert config_entry.state == ConfigEntryState.LOADED
