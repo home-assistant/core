HomeBot-Automation/ha-core

Make FlowResult a generic type

homeassistant/auth/__init__.py
@@ -19,13 +19,13 @@ from homeassistant.core import (
     HomeAssistant,
     callback,
 )
-from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.event import async_track_point_in_utc_time
 from homeassistant.util import dt as dt_util
 
 from . import auth_store, jwt_wrapper, models
 from .const import ACCESS_TOKEN_EXPIRATION, GROUP_ID_ADMIN, REFRESH_TOKEN_EXPIRATION
 from .mfa_modules import MultiFactorAuthModule, auth_mfa_module_from_config
+from .models import AuthFlowResult
 from .providers import AuthProvider, LoginFlow, auth_provider_from_config
 
 EVENT_USER_ADDED = "user_added"
@@ -88,10 +88,12 @@ async def auth_manager_from_config(
     return manager
 
 
-class AuthManagerFlowManager(data_entry_flow.FlowManager):
+class AuthManagerFlowManager(
+    data_entry_flow.BaseFlowManager[AuthFlowResult, tuple[str, str]]
+):
     """Manage authentication flows."""
 
-    _flow_result = FlowResult
+    _flow_result = AuthFlowResult
 
     def __init__(self, hass: HomeAssistant, auth_manager: AuthManager) -> None:
         """Init auth manager flows."""
@@ -100,11 +102,11 @@ class AuthManagerFlowManager(data_entry_flow.FlowManager):
 
     async def async_create_flow(
         self,
-        handler_key: str,
+        handler_key: tuple[str, str],
         *,
         context: dict[str, Any] | None = None,
         data: dict[str, Any] | None = None,
-    ) -> data_entry_flow.FlowHandler:
+    ) -> data_entry_flow.BaseFlowHandler[AuthFlowResult, tuple[str, str]]:
         """Create a login flow."""
         auth_provider = self.auth_manager.get_auth_provider(*handler_key)
         if not auth_provider:
@@ -112,8 +114,8 @@ class AuthManagerFlowManager(data_entry_flow.FlowManager):
         return await auth_provider.async_login_flow(context)
 
     async def async_finish_flow(
-        self, flow: data_entry_flow.BaseFlowHandler, result: FlowResult
-    ) -> FlowResult:
+        self, flow: data_entry_flow.BaseFlowHandler, result: AuthFlowResult
+    ) -> AuthFlowResult:
         """Return a user as result of login flow."""
         flow = cast(LoginFlow, flow)
 

homeassistant/auth/models.py
@@ -11,6 +11,7 @@ from attr import Attribute
 from attr.setters import validate
 
 from homeassistant.const import __version__
+from homeassistant.data_entry_flow import BaseFlowResult
 from homeassistant.util import dt as dt_util
 
 from . import permissions as perm_mdl
@@ -25,6 +26,10 @@ else:
 TOKEN_TYPE_NORMAL = "normal"
 TOKEN_TYPE_SYSTEM = "system"
 TOKEN_TYPE_LONG_LIVED_ACCESS_TOKEN = "long_lived_access_token"
+
+
+class AuthFlowResult(BaseFlowResult[tuple[str, str]], total=False):
+    """Typed result dict for auth flow."""
 
 
 @attr.s(slots=True)

homeassistant/auth/providers/__init__.py
@@ -13,14 +13,13 @@ from voluptuous.humanize import humanize_error
 from homeassistant import data_entry_flow, requirements
 from homeassistant.const import CONF_ID, CONF_NAME, CONF_TYPE
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.util import dt as dt_util
 from homeassistant.util.decorator import Registry
 
 from ..auth_store import AuthStore
 from ..const import MFA_SESSION_EXPIRATION
-from ..models import Credentials, RefreshToken, User, UserMeta
+from ..models import AuthFlowResult, Credentials, RefreshToken, User, UserMeta
 
 _LOGGER = logging.getLogger(__name__)
 DATA_REQS = "auth_prov_reqs_processed"
@@ -181,10 +180,10 @@ async def load_auth_provider_module(
     return module
 
 
-class LoginFlow(data_entry_flow.FlowHandler):
+class LoginFlow(data_entry_flow.BaseFlowHandler[AuthFlowResult, tuple[str, str]]):
     """Handler for the login flow."""
 
-    _flow_result = FlowResult
+    _flow_result = AuthFlowResult
 
     def __init__(self, auth_provider: AuthProvider) -> None:
         """Initialize the login flow."""
@@ -199,7 +198,7 @@ class LoginFlow(data_entry_flow.FlowHandler):
 
     async def async_step_init(
         self, user_input: dict[str, str] | None = None
-    ) -> FlowResult:
+    ) -> AuthFlowResult:
         """Handle the first step of login flow.
 
         Return self.async_show_form(step_id='init') if user_input is None.
@@ -209,7 +208,7 @@ class LoginFlow(data_entry_flow.FlowHandler):
 
     async def async_step_select_mfa_module(
         self, user_input: dict[str, str] | None = None
-    ) -> FlowResult:
+    ) -> AuthFlowResult:
         """Handle the step of select mfa module."""
         errors = {}
 
@@ -234,7 +233,7 @@ class LoginFlow(data_entry_flow.FlowHandler):
 
     async def async_step_mfa(
         self, user_input: dict[str, str] | None = None
-    ) -> FlowResult:
+    ) -> AuthFlowResult:
         """Handle the step of mfa validation."""
         assert self.credential
         assert self.user
@@ -284,6 +283,6 @@ class LoginFlow(data_entry_flow.FlowHandler):
             errors=errors,
         )
 
-    async def async_finish(self, flow_result: Any) -> FlowResult:
+    async def async_finish(self, flow_result: Any) -> AuthFlowResult:
         """Handle the pass of login flow."""
         return self.async_create_entry(data=flow_result)

homeassistant/config_entries.py
@@ -1044,7 +1044,7 @@ class FlowCancelledError(Exception):
     """Error to indicate that a flow has been cancelled."""
 
 
-class ConfigEntriesFlowManager(data_entry_flow.BaseFlowManager[ConfigFlowResult]):
+class ConfigEntriesFlowManager(data_entry_flow.BaseFlowManager[ConfigFlowResult, str]):
     """Manage all the config entry flows that are in progress."""
 
     _flow_result = ConfigFlowResult
@@ -1928,7 +1928,7 @@ def _async_abort_entries_match(
             raise data_entry_flow.AbortFlow("already_configured")
 
 
-class ConfigEntryBaseFlow(data_entry_flow.BaseFlowHandler[ConfigFlowResult]):
+class ConfigEntryBaseFlow(data_entry_flow.BaseFlowHandler[ConfigFlowResult, str]):
     """Base class for config and option flows."""
 
     _flow_result = ConfigFlowResult
@@ -2280,7 +2280,7 @@ class ConfigFlow(ConfigEntryBaseFlow):
         return self.async_abort(reason=reason)
 
 
-class OptionsFlowManager(data_entry_flow.BaseFlowManager[ConfigFlowResult]):
+class OptionsFlowManager(data_entry_flow.BaseFlowManager[ConfigFlowResult, str]):
     """Flow to set options for a configuration entry."""
 
     _flow_result = ConfigFlowResult

homeassistant/data_entry_flow.py
@@ -84,7 +84,8 @@ STEP_ID_OPTIONAL_STEPS = {
 }
 
 
-_FlowResultT = TypeVar("_FlowResultT", bound="FlowResult")
+_FlowResultT = TypeVar("_FlowResultT", bound="BaseFlowResult")
+_HandlerT = TypeVar("_HandlerT")
 
 
 @dataclass(slots=True)
@@ -137,7 +138,7 @@ class AbortFlow(FlowError):
         self.description_placeholders = description_placeholders
 
 
-class FlowResult(TypedDict, total=False):
+class BaseFlowResult(Generic[_HandlerT], TypedDict, total=False):
     """Typed result dict."""
 
     context: dict[str, Any]
@@ -148,7 +149,7 @@ class FlowResult(TypedDict, total=False):
     errors: dict[str, str] | None
     extra: str
     flow_id: Required[str]
-    handler: Required[str]
+    handler: Required[_HandlerT]
     last_step: bool | None
     menu_options: list[str] | dict[str, str]
     options: Mapping[str, Any]
@@ -163,6 +164,10 @@ class FlowResult(TypedDict, total=False):
     translation_domain: str
     type: FlowResultType
     url: str
+
+
+class FlowResult(BaseFlowResult[str], total=False):
+    """Typed result dict."""
 
 
 def _map_error_to_schema_errors(
@@ -188,7 +193,7 @@ def _map_error_to_schema_errors(
     schema_errors[path_part_str] = error.error_message
 
 
-class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
+class BaseFlowManager(abc.ABC, Generic[_FlowResultT, _HandlerT]):
     """Manage all the flows that are in progress."""
 
     _flow_result: Callable[..., _FlowResultT]
@@ -201,17 +206,17 @@ class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
         self.hass = hass
         self._preview: set[str] = set()
         self._progress: dict[str, BaseFlowHandler] = {}
-        self._handler_progress_index: dict[str, set[BaseFlowHandler]] = {}
+        self._handler_progress_index: dict[_HandlerT, set[BaseFlowHandler]] = {}
         self._init_data_process_index: dict[type, set[BaseFlowHandler]] = {}
 
     @abc.abstractmethod
     async def async_create_flow(
         self,
-        handler_key: str,
+        handler_key: _HandlerT,
         *,
         context: dict[str, Any] | None = None,
         data: dict[str, Any] | None = None,
-    ) -> BaseFlowHandler[_FlowResultT]:
+    ) -> BaseFlowHandler[_FlowResultT, _HandlerT]:
         """Create a flow for specified handler.
 
         Handler key is the domain of the component that we want to set up.
@@ -230,7 +235,7 @@ class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
 
     @callback
     def async_has_matching_flow(
-        self, handler: str, match_context: dict[str, Any], data: Any
+        self, handler: _HandlerT, match_context: dict[str, Any], data: Any
     ) -> bool:
         """Check if an existing matching flow is in progress.
 
@@ -264,7 +269,7 @@ class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
     @callback
     def async_progress_by_handler(
         self,
-        handler: str,
+        handler: _HandlerT,
         include_uninitialized: bool = False,
         match_context: dict[str, Any] | None = None,
     ) -> list[_FlowResultT]:
@@ -297,8 +302,8 @@ class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
 
     @callback
     def _async_progress_by_handler(
-        self, handler: str, match_context: dict[str, Any] | None
-    ) -> list[BaseFlowHandler[_FlowResultT]]:
+        self, handler: _HandlerT, match_context: dict[str, Any] | None
+    ) -> list[BaseFlowHandler[_FlowResultT, _HandlerT]]:
         """Return the flows in progress by handler.
 
         If match_context is specified, only return flows with a context that
@@ -314,7 +319,11 @@ class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
         ]
 
     async def async_init(
-        self, handler: str, *, context: dict[str, Any] | None = None, data: Any = None
+        self,
+        handler: _HandlerT,
+        *,
+        context: dict[str, Any] | None = None,
+        data: Any = None,
     ) -> _FlowResultT:
         """Start a data entry flow."""
         if context is None:
@@ -444,7 +453,9 @@ class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
         self._async_remove_flow_progress(flow_id)
 
     @callback
-    def _async_add_flow_progress(self, flow: BaseFlowHandler[_FlowResultT]) -> None:
+    def _async_add_flow_progress(
+        self, flow: BaseFlowHandler[_FlowResultT, _HandlerT]
+    ) -> None:
         """Add a flow to in progress."""
         if flow.init_data is not None:
             init_data_type = type(flow.init_data)
@@ -454,7 +465,7 @@ class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
 
     @callback
     def _async_remove_flow_from_index(
-        self, flow: BaseFlowHandler[_FlowResultT]
+        self, flow: BaseFlowHandler[_FlowResultT, _HandlerT]
     ) -> None:
         """Remove a flow from in progress."""
         if flow.init_data is not None:
@@ -481,7 +492,7 @@ class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
 
     async def _async_handle_step(
         self,
-        flow: BaseFlowHandler[_FlowResultT],
+        flow: BaseFlowHandler[_FlowResultT, _HandlerT],
         step_id: str,
         user_input: dict | BaseServiceInfo | None,
     ) -> _FlowResultT:
@@ -595,13 +606,13 @@ class BaseFlowManager(abc.ABC, Generic[_FlowResultT]):
         return results
 
 
-class FlowManager(BaseFlowManager[FlowResult]):
+class FlowManager(BaseFlowManager[FlowResult, str]):
     """Manage all the flows that are in progress."""
 
     _flow_result = FlowResult
 
 
-class BaseFlowHandler(Generic[_FlowResultT]):
+class BaseFlowHandler(Generic[_FlowResultT, _HandlerT]):
     """Handle a data entry flow."""
 
     _flow_result: Callable[..., _FlowResultT]
@@ -613,7 +624,7 @@ class BaseFlowHandler(Generic[_FlowResultT]):
     # and removes the need for constant None checks or asserts.
     flow_id: str = None  # type: ignore[assignment]
     hass: HomeAssistant = None  # type: ignore[assignment]
-    handler: str = None  # type: ignore[assignment]
+    handler: _HandlerT = None  # type: ignore[assignment]
     # Ensure the attribute has a subscriptable, but immutable, default value.
     context: dict[str, Any] = MappingProxyType({})  # type: ignore[assignment]
 
@@ -881,7 +892,7 @@ class BaseFlowHandler(Generic[_FlowResultT]):
         self.__progress_task = progress_task
 
 
-class FlowHandler(BaseFlowHandler[FlowResult]):
+class FlowHandler(BaseFlowHandler[FlowResult, str]):
     """Handle a data entry flow."""
 
     _flow_result = FlowResult

home-assistant/core

homeassistant/auth/__init__.py
@@ -19,13 +19,13 @@ from homeassistant.core import (
     HomeAssistant,
     callback,
 )
-from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.event import async_track_point_in_utc_time
 from homeassistant.util import dt as dt_util
 
 from . import auth_store, jwt_wrapper, models
 from .const import ACCESS_TOKEN_EXPIRATION, GROUP_ID_ADMIN, REFRESH_TOKEN_EXPIRATION
 from .mfa_modules import MultiFactorAuthModule, auth_mfa_module_from_config
+from .models import AuthFlowResult
 from .providers import AuthProvider, LoginFlow, auth_provider_from_config
 
 EVENT_USER_ADDED = "user_added"
@@ -88,8 +88,12 @@ async def auth_manager_from_config(
     return manager
 
 
-class AuthManagerFlowManager(data_entry_flow.FlowManager):
+class AuthManagerFlowManager(
+    data_entry_flow.FlowManager[AuthFlowResult, tuple[str, str]]
+):
     """Manage authentication flows."""
+
+    _flow_result = AuthFlowResult
 
     def __init__(self, hass: HomeAssistant, auth_manager: AuthManager) -> None:
         """Init auth manager flows."""
@@ -98,11 +102,11 @@ class AuthManagerFlowManager(data_entry_flow.FlowManager):
 
     async def async_create_flow(
         self,
-        handler_key: str,
+        handler_key: tuple[str, str],
         *,
         context: dict[str, Any] | None = None,
         data: dict[str, Any] | None = None,
-    ) -> data_entry_flow.FlowHandler:
+    ) -> data_entry_flow.FlowHandler[AuthFlowResult, tuple[str, str]]:
         """Create a login flow."""
         auth_provider = self.auth_manager.get_auth_provider(*handler_key)
         if not auth_provider:
@@ -110,8 +114,10 @@ class AuthManagerFlowManager(data_entry_flow.FlowManager):
         return await auth_provider.async_login_flow(context)
 
     async def async_finish_flow(
-        self, flow: data_entry_flow.FlowHandler, result: FlowResult
-    ) -> FlowResult:
+        self,
+        flow: data_entry_flow.FlowHandler[AuthFlowResult, tuple[str, str]],
+        result: AuthFlowResult,
+    ) -> AuthFlowResult:
         """Return a user as result of login flow."""
         flow = cast(LoginFlow, flow)
 

homeassistant/auth/models.py
@@ -11,6 +11,7 @@ from attr import Attribute
 from attr.setters import validate
 
 from homeassistant.const import __version__
+from homeassistant.data_entry_flow import FlowResult
 from homeassistant.util import dt as dt_util
 
 from . import permissions as perm_mdl
@@ -25,6 +26,10 @@ else:
 TOKEN_TYPE_NORMAL = "normal"
 TOKEN_TYPE_SYSTEM = "system"
 TOKEN_TYPE_LONG_LIVED_ACCESS_TOKEN = "long_lived_access_token"
+
+
+class AuthFlowResult(FlowResult[tuple[str, str]], total=False):
+    """Typed result dict for auth flow."""
 
 
 @attr.s(slots=True)

homeassistant/auth/providers/__init__.py
@@ -13,14 +13,13 @@ from voluptuous.humanize import humanize_error
 from homeassistant import data_entry_flow, requirements
 from homeassistant.const import CONF_ID, CONF_NAME, CONF_TYPE
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.util import dt as dt_util
 from homeassistant.util.decorator import Registry
 
 from ..auth_store import AuthStore
 from ..const import MFA_SESSION_EXPIRATION
-from ..models import Credentials, RefreshToken, User, UserMeta
+from ..models import AuthFlowResult, Credentials, RefreshToken, User, UserMeta
 
 _LOGGER = logging.getLogger(__name__)
 DATA_REQS = "auth_prov_reqs_processed"
@@ -181,8 +180,10 @@ async def load_auth_provider_module(
     return module
 
 
-class LoginFlow(data_entry_flow.FlowHandler):
+class LoginFlow(data_entry_flow.FlowHandler[AuthFlowResult, tuple[str, str]]):
     """Handler for the login flow."""
+
+    _flow_result = AuthFlowResult
 
     def __init__(self, auth_provider: AuthProvider) -> None:
         """Initialize the login flow."""
@@ -197,7 +198,7 @@ class LoginFlow(data_entry_flow.FlowHandler):
 
     async def async_step_init(
         self, user_input: dict[str, str] | None = None
-    ) -> FlowResult:
+    ) -> AuthFlowResult:
         """Handle the first step of login flow.
 
         Return self.async_show_form(step_id='init') if user_input is None.
@@ -207,7 +208,7 @@ class LoginFlow(data_entry_flow.FlowHandler):
 
     async def async_step_select_mfa_module(
         self, user_input: dict[str, str] | None = None
-    ) -> FlowResult:
+    ) -> AuthFlowResult:
         """Handle the step of select mfa module."""
         errors = {}
 
@@ -232,7 +233,7 @@ class LoginFlow(data_entry_flow.FlowHandler):
 
     async def async_step_mfa(
         self, user_input: dict[str, str] | None = None
-    ) -> FlowResult:
+    ) -> AuthFlowResult:
         """Handle the step of mfa validation."""
         assert self.credential
         assert self.user
@@ -282,6 +283,6 @@ class LoginFlow(data_entry_flow.FlowHandler):
             errors=errors,
         )
 
-    async def async_finish(self, flow_result: Any) -> FlowResult:
+    async def async_finish(self, flow_result: Any) -> AuthFlowResult:
         """Handle the pass of login flow."""
         return self.async_create_entry(data=flow_result)

homeassistant/config_entries.py
@@ -1045,7 +1045,7 @@ class FlowCancelledError(Exception):
     """Error to indicate that a flow has been cancelled."""
 
 
-class ConfigEntriesFlowManager(data_entry_flow.FlowManager[ConfigFlowResult]):
+class ConfigEntriesFlowManager(data_entry_flow.FlowManager[ConfigFlowResult, str]):
     """Manage all the config entry flows that are in progress."""
 
     _flow_result = ConfigFlowResult
@@ -1171,7 +1171,7 @@ class ConfigEntriesFlowManager(data_entry_flow.FlowManager[ConfigFlowResult]):
 
     async def async_finish_flow(
         self,
-        flow: data_entry_flow.FlowHandler[ConfigFlowResult],
+        flow: data_entry_flow.FlowHandler[ConfigFlowResult, str],
         result: ConfigFlowResult,
     ) -> ConfigFlowResult:
         """Finish a config flow and add an entry."""
@@ -1293,7 +1293,7 @@ class ConfigEntriesFlowManager(data_entry_flow.FlowManager[ConfigFlowResult]):
 
     async def async_post_init(
         self,
-        flow: data_entry_flow.FlowHandler[ConfigFlowResult],
+        flow: data_entry_flow.FlowHandler[ConfigFlowResult, str],
         result: ConfigFlowResult,
     ) -> None:
         """After a flow is initialised trigger new flow notifications."""
@@ -1940,7 +1940,7 @@ def _async_abort_entries_match(
             raise data_entry_flow.AbortFlow("already_configured")
 
 
-class ConfigEntryBaseFlow(data_entry_flow.FlowHandler[ConfigFlowResult]):
+class ConfigEntryBaseFlow(data_entry_flow.FlowHandler[ConfigFlowResult, str]):
     """Base class for config and option flows."""
 
     _flow_result = ConfigFlowResult
@@ -2292,7 +2292,7 @@ class ConfigFlow(ConfigEntryBaseFlow):
         return self.async_abort(reason=reason)
 
 
-class OptionsFlowManager(data_entry_flow.FlowManager[ConfigFlowResult]):
+class OptionsFlowManager(data_entry_flow.FlowManager[ConfigFlowResult, str]):
     """Flow to set options for a configuration entry."""
 
     _flow_result = ConfigFlowResult
@@ -2322,7 +2322,7 @@ class OptionsFlowManager(data_entry_flow.FlowManager[ConfigFlowResult]):
 
     async def async_finish_flow(
         self,
-        flow: data_entry_flow.FlowHandler[ConfigFlowResult],
+        flow: data_entry_flow.FlowHandler[ConfigFlowResult, str],
         result: ConfigFlowResult,
     ) -> ConfigFlowResult:
         """Finish an options flow and update options for configuration entry.
@@ -2344,7 +2344,7 @@ class OptionsFlowManager(data_entry_flow.FlowManager[ConfigFlowResult]):
         return result
 
     async def _async_setup_preview(
-        self, flow: data_entry_flow.FlowHandler[ConfigFlowResult]
+        self, flow: data_entry_flow.FlowHandler[ConfigFlowResult, str]
     ) -> None:
         """Set up preview for an option flow handler."""
         entry = self._async_get_config_entry(flow.handler)

homeassistant/data_entry_flow.py
@@ -85,7 +85,8 @@ STEP_ID_OPTIONAL_STEPS = {
 }
 
 
-_FlowResultT = TypeVar("_FlowResultT", bound="FlowResult", default="FlowResult")
+_FlowResultT = TypeVar("_FlowResultT", bound="FlowResult[Any]", default="FlowResult")
+_HandlerT = TypeVar("_HandlerT", default=str)
 
 
 @dataclass(slots=True)
@@ -138,7 +139,7 @@ class AbortFlow(FlowError):
         self.description_placeholders = description_placeholders
 
 
-class FlowResult(TypedDict, total=False):
+class FlowResult(Generic[_HandlerT], TypedDict, total=False):
     """Typed result dict."""
 
     context: dict[str, Any]
@@ -149,7 +150,7 @@ class FlowResult(TypedDict, total=False):
     errors: dict[str, str] | None
     extra: str
     flow_id: Required[str]
-    handler: Required[str]
+    handler: Required[_HandlerT]
     last_step: bool | None
     menu_options: list[str] | dict[str, str]
     options: Mapping[str, Any]
@@ -189,7 +190,7 @@ def _map_error_to_schema_errors(
     schema_errors[path_part_str] = error.error_message
 
 
-class FlowManager(abc.ABC, Generic[_FlowResultT]):
+class FlowManager(abc.ABC, Generic[_FlowResultT, _HandlerT]):
     """Manage all the flows that are in progress."""
 
     _flow_result: Callable[..., _FlowResultT] = FlowResult  # type: ignore[assignment]
@@ -200,19 +201,23 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
     ) -> None:
         """Initialize the flow manager."""
         self.hass = hass
-        self._preview: set[str] = set()
-        self._progress: dict[str, FlowHandler[_FlowResultT]] = {}
-        self._handler_progress_index: dict[str, set[FlowHandler[_FlowResultT]]] = {}
-        self._init_data_process_index: dict[type, set[FlowHandler[_FlowResultT]]] = {}
+        self._preview: set[_HandlerT] = set()
+        self._progress: dict[str, FlowHandler[_FlowResultT, _HandlerT]] = {}
+        self._handler_progress_index: dict[
+            _HandlerT, set[FlowHandler[_FlowResultT, _HandlerT]]
+        ] = {}
+        self._init_data_process_index: dict[
+            type, set[FlowHandler[_FlowResultT, _HandlerT]]
+        ] = {}
 
     @abc.abstractmethod
     async def async_create_flow(
         self,
-        handler_key: str,
+        handler_key: _HandlerT,
         *,
         context: dict[str, Any] | None = None,
         data: dict[str, Any] | None = None,
-    ) -> FlowHandler[_FlowResultT]:
+    ) -> FlowHandler[_FlowResultT, _HandlerT]:
         """Create a flow for specified handler.
 
         Handler key is the domain of the component that we want to set up.
@@ -220,18 +225,18 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
 
     @abc.abstractmethod
     async def async_finish_flow(
-        self, flow: FlowHandler[_FlowResultT], result: _FlowResultT
+        self, flow: FlowHandler[_FlowResultT, _HandlerT], result: _FlowResultT
     ) -> _FlowResultT:
         """Finish a data entry flow."""
 
     async def async_post_init(
-        self, flow: FlowHandler[_FlowResultT], result: _FlowResultT
+        self, flow: FlowHandler[_FlowResultT, _HandlerT], result: _FlowResultT
     ) -> None:
         """Entry has finished executing its first step asynchronously."""
 
     @callback
     def async_has_matching_flow(
-        self, handler: str, match_context: dict[str, Any], data: Any
+        self, handler: _HandlerT, match_context: dict[str, Any], data: Any
     ) -> bool:
         """Check if an existing matching flow is in progress.
 
@@ -265,7 +270,7 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
     @callback
     def async_progress_by_handler(
         self,
-        handler: str,
+        handler: _HandlerT,
         include_uninitialized: bool = False,
         match_context: dict[str, Any] | None = None,
     ) -> list[_FlowResultT]:
@@ -298,8 +303,8 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
 
     @callback
     def _async_progress_by_handler(
-        self, handler: str, match_context: dict[str, Any] | None
-    ) -> list[FlowHandler[_FlowResultT]]:
+        self, handler: _HandlerT, match_context: dict[str, Any] | None
+    ) -> list[FlowHandler[_FlowResultT, _HandlerT]]:
         """Return the flows in progress by handler.
 
         If match_context is specified, only return flows with a context that
@@ -315,7 +320,11 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
         ]
 
     async def async_init(
-        self, handler: str, *, context: dict[str, Any] | None = None, data: Any = None
+        self,
+        handler: _HandlerT,
+        *,
+        context: dict[str, Any] | None = None,
+        data: Any = None,
     ) -> _FlowResultT:
         """Start a data entry flow."""
         if context is None:
@@ -445,7 +454,9 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
         self._async_remove_flow_progress(flow_id)
 
     @callback
-    def _async_add_flow_progress(self, flow: FlowHandler[_FlowResultT]) -> None:
+    def _async_add_flow_progress(
+        self, flow: FlowHandler[_FlowResultT, _HandlerT]
+    ) -> None:
         """Add a flow to in progress."""
         if flow.init_data is not None:
             init_data_type = type(flow.init_data)
@@ -454,7 +465,9 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
         self._handler_progress_index.setdefault(flow.handler, set()).add(flow)
 
     @callback
-    def _async_remove_flow_from_index(self, flow: FlowHandler[_FlowResultT]) -> None:
+    def _async_remove_flow_from_index(
+        self, flow: FlowHandler[_FlowResultT, _HandlerT]
+    ) -> None:
         """Remove a flow from in progress."""
         if flow.init_data is not None:
             init_data_type = type(flow.init_data)
@@ -480,7 +493,7 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
 
     async def _async_handle_step(
         self,
-        flow: FlowHandler[_FlowResultT],
+        flow: FlowHandler[_FlowResultT, _HandlerT],
         step_id: str,
         user_input: dict | BaseServiceInfo | None,
     ) -> _FlowResultT:
@@ -557,7 +570,7 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
         return result
 
     def _raise_if_step_does_not_exist(
-        self, flow: FlowHandler[_FlowResultT], step_id: str
+        self, flow: FlowHandler[_FlowResultT, _HandlerT], step_id: str
     ) -> None:
         """Raise if the step does not exist."""
         method = f"async_step_{step_id}"
@@ -568,7 +581,9 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
                 f"Handler {self.__class__.__name__} doesn't support step {step_id}"
             )
 
-    async def _async_setup_preview(self, flow: FlowHandler[_FlowResultT]) -> None:
+    async def _async_setup_preview(
+        self, flow: FlowHandler[_FlowResultT, _HandlerT]
+    ) -> None:
         """Set up preview for a flow handler."""
         if flow.handler not in self._preview:
             self._preview.add(flow.handler)
@@ -576,7 +591,9 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
 
     @callback
     def _async_flow_handler_to_flow_result(
-        self, flows: Iterable[FlowHandler[_FlowResultT]], include_uninitialized: bool
+        self,
+        flows: Iterable[FlowHandler[_FlowResultT, _HandlerT]],
+        include_uninitialized: bool,
     ) -> list[_FlowResultT]:
         """Convert a list of FlowHandler to a partial FlowResult that can be serialized."""
         results = []
@@ -594,7 +611,7 @@ class FlowManager(abc.ABC, Generic[_FlowResultT]):
         return results
 
 
-class FlowHandler(Generic[_FlowResultT]):
+class FlowHandler(Generic[_FlowResultT, _HandlerT]):
     """Handle a data entry flow."""
 
     _flow_result: Callable[..., _FlowResultT] = FlowResult  # type: ignore[assignment]
@@ -606,7 +623,7 @@ class FlowHandler(Generic[_FlowResultT]):
     # and removes the need for constant None checks or asserts.
     flow_id: str = None  # type: ignore[assignment]
     hass: HomeAssistant = None  # type: ignore[assignment]
-    handler: str = None  # type: ignore[assignment]
+    handler: _HandlerT = None  # type: ignore[assignment]
     # Ensure the attribute has a subscriptable, but immutable, default value.
     context: dict[str, Any] = MappingProxyType({})  # type: ignore[assignment]
 