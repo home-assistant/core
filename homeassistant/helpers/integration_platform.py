0ba8f2f2c303ec44b728e10c2f6d2e3919fb0d04
J. Nick Koston <nick@koston.org>
2024-03-05 14:46:19 -1000

    Avoid waiting for integration platforms in the parent integration
    If an integration platform is loaded after the parent integration
    we did not wait for it. If an integration platform for an integration
    that is already loaded needs to be loaded when the parent integration
    calls async_process_integration_platforms we waited for the integration
    platform to finish setting up. This made the parent ingration delay startup
    because it had to wait for all the integration platforms for child integrations

--- homeassistant/helpers/integration_platform.py
+++ homeassistant/helpers/integration_platform.py
@@ -194,6 +194,16 @@ async def async_process_integration_platforms(
     if not top_level_components:
         return
 
+    # We create a task here for two reasons:
+    #
+    # 1. We want the integration that provides the integration platform to
+    #    not be delayed by waiting on each individual platform to be processed
+    #    since the integration platforms may have to schedule I/O or executor
+    #    jobs.
+    #
+    # 2. We want the behavior to be the same as if the integration that has
+    #    the integration platform is loaded after the platform is processed.
+    #
     hass.async_create_task(
         _async_process_integration_platforms(
             hass, platform_name, top_level_components.copy(), process_job
