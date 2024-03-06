115e82d2dce9da6a20e4b87f081b7e79219d5542
J. Nick Koston <nick@koston.org>
2024-03-05 23:03:48 -1000

    preen

--- homeassistant/components/wemo/wemo_device.py
+++ homeassistant/components/wemo/wemo_device.py
@@ -106,6 +106,7 @@ class DeviceCoordinator(DataUpdateCoordinator[None]):  # pylint: disable=hass-en
         self.supports_long_press = isinstance(wemo, LongPressMixin)
         self.update_lock = asyncio.Lock()
 
+    @callback
     def async_setup(self, device_id: str) -> None:
         """Set up the device coordinator."""
         self.device_id = device_id
