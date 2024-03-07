fc412fbaa83540a28c1c63cb5ae4b804a3076bea
SebClem <seb6596@gmail.com>
2024-03-07 16:43:34 +0100

    Use coordinator for switch

--- homeassistant/components/qbittorrent/switch.py
+++ homeassistant/components/qbittorrent/switch.py
@@ -10,6 +10,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 from .coordinator import QBittorrentDataCoordinator
@@ -19,26 +20,10 @@ from .coordinator import QBittorrentDataCoordinator
 class QBittorrentSwitchEntityDescription(SwitchEntityDescription):
     """Describes qBittorren binary sensor entity."""
 
-    update_fn: Callable[["QBittorrentSwitch"], bool]
-    turn_on_fn: Callable[["QBittorrentSwitch"], None]
-    turn_off_fn: Callable[["QBittorrentSwitch"], None]
-
-
-def alternative_speed_turn_on(entity: "QBittorrentSwitch"):
-    """Turn on alternative speed.
-
-    qBittorrent only exposes the toggle for alternative speed, so we test if alternative_speed is false before using toggle.
-    """
-
-    if not entity.is_on:
-        entity.client.toggle_alternative_speed()
-
-
-def alternative_speed_turn_off(entity: "QBittorrentSwitch"):
-    """Turn off alternative speed."""
-
-    if entity.is_on:
-        entity.client.toggle_alternative_speed()
+    is_on_func: Callable[[QBittorrentDataCoordinator], bool]
+    turn_on_fn: Callable[["QBittorrentDataCoordinator"], None]
+    turn_off_fn: Callable[["QBittorrentDataCoordinator"], None]
+    toggle_func: Callable[[QBittorrentDataCoordinator], None]
 
 
 SWITCH_TYPES: tuple[QBittorrentSwitchEntityDescription, ...] = (
@@ -46,9 +31,10 @@ SWITCH_TYPES: tuple[QBittorrentSwitchEntityDescription, ...] = (
         key="alternative_speed",
         translation_key="alternative_speed",
         icon="mdi:speedometer-slow",
-        update_fn=lambda entity: bool(entity.client.get_alternative_speed_status()),
-        turn_on_fn=alternative_speed_turn_on,
-        turn_off_fn=alternative_speed_turn_off,
+        is_on_func=lambda coordinator: coordinator.get_alt_speed_enabled(),
+        turn_on_fn=lambda coordinator: coordinator.set_alt_speed_enabled(True),
+        turn_off_fn=lambda coordinator: coordinator.set_alt_speed_enabled(False),
+        toggle_func=lambda coordinator: coordinator.toggle_alt_speed_enabled(),
     ),
 )
 
@@ -63,12 +49,12 @@ async def async_setup_entry(
     coordinator: QBittorrentDataCoordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     async_add_entities(
-        QBittorrentSwitch(hass, coordinator.client, config_entry, description)
+        QBittorrentSwitch(coordinator, config_entry, description)
         for description in SWITCH_TYPES
     )
 
 
-class QBittorrentSwitch(SwitchEntity):
+class QBittorrentSwitch(CoordinatorEntity[QBittorrentDataCoordinator], SwitchEntity):
     """Representation of a qBittorrent switch."""
 
     _attr_has_entity_name = True
@@ -77,13 +63,12 @@ class QBittorrentSwitch(SwitchEntity):
 
     def __init__(
         self,
-        hass: HomeAssistant,
-        client: Client,
+        coordinator: QBittorrentDataCoordinator,
         config_entry: ConfigEntry,
         entity_description: QBittorrentSwitchEntityDescription,
     ) -> None:
         """Initialize qBittorrent switch."""
-        super().__init__()
+        super().__init__(coordinator)
         self.entity_description = entity_description
         self._attr_unique_id = f"{config_entry.entry_id}-{entity_description.key}"
         self._attr_device_info = DeviceInfo(
@@ -91,21 +76,29 @@ class QBittorrentSwitch(SwitchEntity):
             identifiers={(DOMAIN, config_entry.entry_id)},
             manufacturer="QBittorrent",
         )
-        self.client = client
 
-    async def async_update(self) -> None:
-        """Update entity value."""
-
-        self._attr_is_on = await self.hass.async_add_executor_job(
-            self.entity_description.update_fn, self
-        )
+    @property
+    def is_on(self) -> bool:
+        """Return true if device is on."""
+        return self.entity_description.is_on_func(self.coordinator)
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn on this switch."""
-        await self.hass.async_add_executor_job(self.entity_description.turn_on_fn, self)
+        await self.hass.async_add_executor_job(
+            self.entity_description.turn_on_fn, self.coordinator
+        )
+        await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off this switch."""
         await self.hass.async_add_executor_job(
-            self.entity_description.turn_off_fn, self
+            self.entity_description.turn_off_fn, self.coordinator
         )
+        await self.coordinator.async_request_refresh()
+
+    async def async_toggle(self, **kwargs: Any) -> None:
+        """Toggle the device."""
+        await self.hass.async_add_executor_job(
+            self.entity_description.toggle_func, self.coordinator
+        )
+        await self.coordinator.async_request_refresh()
