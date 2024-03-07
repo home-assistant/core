e21b651184b1f359d1bbe24a4d4110d42605fff9
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-03-07 20:45:03 +1300

    Add missing state test

--- tests/components/esphome/test_date.py
+++ tests/components/esphome/test_date.py
@@ -9,7 +9,7 @@ from homeassistant.components.date import (
     DOMAIN as DATE_DOMAIN,
     SERVICE_SET_VALUE,
 )
-from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.const import ATTR_ENTITY_ID, STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
 
 
@@ -47,3 +47,30 @@ async def test_generic_date_entity(
     )
     mock_client.date_command.assert_has_calls([call(1, 1999, 1, 1)])
     mock_client.date_command.reset_mock()
+
+
+async def test_generic_date_missing_state(
+    hass: HomeAssistant,
+    mock_client: APIClient,
+    mock_generic_device_entry,
+) -> None:
+    """Test a generic date entity with missing state."""
+    entity_info = [
+        DateInfo(
+            object_id="mydate",
+            key=1,
+            name="my date",
+            unique_id="my_date",
+        )
+    ]
+    states = [DateState(key=1, missing_state=True)]
+    user_service = []
+    await mock_generic_device_entry(
+        mock_client=mock_client,
+        entity_info=entity_info,
+        user_service=user_service,
+        states=states,
+    )
+    state = hass.states.get("date.test_mydate")
+    assert state is not None
+    assert state.state == STATE_UNKNOWN
