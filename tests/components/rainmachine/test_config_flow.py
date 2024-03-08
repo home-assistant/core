c1e0f2703db7d85fb24e404f02b3d11094409421
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-03-08 14:05:26 +0100

    Add empty line after module docstring [tests q-s]

--- tests/components/rainmachine/test_config_flow.py
+++ tests/components/rainmachine/test_config_flow.py
@@ -1,4 +1,5 @@
 """Define tests for the OpenUV config flow."""
+
 from ipaddress import ip_address
 from unittest.mock import patch
 
--- tests/components/sun/test_config_flow.py
+++ tests/components/sun/test_config_flow.py
@@ -1,4 +1,5 @@
 """Tests for the Sun config flow."""
+
 from unittest.mock import patch
 
 import pytest
