4f6fb7618d456da2425e005b29dc9f0f31a3128f
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-03-08 13:53:09 +0100

    Add empty line after module docstring [w-z]

--- homeassistant/components/water_heater/__init__.py
+++ homeassistant/components/water_heater/__init__.py
@@ -1,4 +1,5 @@
 """Support for water heater devices."""
+
 from __future__ import annotations
 
 from collections.abc import Mapping
--- homeassistant/components/zeversolar/sensor.py
+++ homeassistant/components/zeversolar/sensor.py
@@ -1,4 +1,5 @@
 """Support for the Zeversolar platform."""
+
 from __future__ import annotations
 
 from collections.abc import Callable
