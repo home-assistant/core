db0e62949a05758dcb355a90e49b11522476cd44
J. Nick Koston <nick@koston.org>
2024-03-08 15:20:32 -1000

    missed one

--- homeassistant/helpers/debounce.py
+++ homeassistant/helpers/debounce.py
@@ -130,7 +130,7 @@ class Debouncer(Generic[_R_co]):
                 return
 
             try:
-                if task := self.hass.async_run_hass_job(self._job):
+                if task := self.hass.async_run_hass_job(self._job, eager_start=True):
                     await task
             except Exception:  # pylint: disable=broad-except
                 self.logger.exception("Unexpected exception from %s", self.function)
