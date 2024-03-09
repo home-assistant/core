eeb0e2b6b5b0eda1af0af28b912d4c11f43ebce0
jan iversen <jancasacondor@gmail.com>
2024-03-09 13:58:45 +0100

    Ruff.

--- homeassistant/components/modbus/__init__.py
+++ homeassistant/components/modbus/__init__.py
@@ -132,7 +132,6 @@ from .const import (  # noqa: F401
 )
 from .modbus import ModbusHub, async_modbus_setup
 from .validators import (
-    check_config,
     check_hvac_target_temp_registers,
     duplicate_fan_mode_validator,
     hvac_fixedsize_reglist_validator,
