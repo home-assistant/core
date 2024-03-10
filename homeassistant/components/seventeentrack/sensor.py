f536ee6b4610b90b59fce7cf8c238485c1031adc
Shai Ungar <shaiungar@gmail.com>
2024-03-11 00:03:23 +0200

    Update homeassistant/components/seventeentrack/sensor.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/seventeentrack/sensor.py
+++ homeassistant/components/seventeentrack/sensor.py
@@ -96,7 +96,7 @@ async def async_setup_platform(
             HOMEASSISTANT_DOMAIN,
             f"deprecated_yaml_{DOMAIN}",
             is_fixable=False,
-            breaks_in_ha_version="2024.9.0",
+            breaks_in_ha_version="2024.10.0",
             severity=IssueSeverity.WARNING,
             translation_key="deprecated_yaml",
             translation_placeholders={
