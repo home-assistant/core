41ae7b4703d7a484401bd8ef7b3838bbfac5a8b0
alexyao2015 <alexyao2015@users.noreply.github.com>
2024-03-10 04:09:00 +0000

    test user config

--- tests/components/html5/test_config_flow.py
+++ tests/components/html5/test_config_flow.py
@@ -42,6 +42,43 @@ async def test_step_user_success(hass: HomeAssistant) -> None:
         assert mock_setup_entry.call_count == 1
 
 
+async def test_step_user_success_generate(hass: HomeAssistant) -> None:
+    """Test a successful user config flow, generating a key pair."""
+
+    with patch(
+        "homeassistant.components.html5.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        conf = {ATTR_VAPID_EMAIL: MOCK_CONF[ATTR_VAPID_EMAIL]}
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}, data=conf
+        )
+
+        await hass.async_block_till_done()
+
+        assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
+        assert result["data"][ATTR_VAPID_EMAIL] == MOCK_CONF[ATTR_VAPID_EMAIL]
+
+        assert mock_setup_entry.call_count == 1
+
+
+async def test_step_user_new_form(hass: HomeAssistant, key: str, value: str) -> None:
+    """Test new user input."""
+
+    with patch(
+        "homeassistant.components.html5.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": config_entries.SOURCE_USER}, data=None
+        )
+
+        await hass.async_block_till_done()
+
+        assert result["type"] == data_entry_flow.FlowResultType.FORM
+        assert mock_setup_entry.call_count == 0
+
+
 @pytest.mark.parametrize(
     ("key", "value"),
     [
@@ -49,7 +86,9 @@ async def test_step_user_success(hass: HomeAssistant) -> None:
         (ATTR_VAPID_PRV_KEY, "invalid"),
     ],
 )
-async def test_step_user_form(hass: HomeAssistant, key: str, value: str) -> None:
+async def test_step_user_form_invalid_key(
+    hass: HomeAssistant, key: str, value: str
+) -> None:
     """Test invalid user input."""
 
     with patch(
