862d095cad96c6f8835f20a58d229aed3f9a8aef
Shai Ungar <shai.ungar@riskified.com>
2024-03-10 19:57:28 +0200

    Improve 17track tests (#112734)
    * 17Track tests
    
    * add 17Track sensor to coverage
    
    * extract repeated code
    
    * 1. _goto_future - call tick only once
    2. change test name to reflect test
    3. remove ifs from test
    
    * remove undersocre from _goto_future

--- tests/components/seventeentrack/__init__.py
+++ tests/components/seventeentrack/__init__.py
@@ -1,1 +1,25 @@
 """Tests for the seventeentrack component."""
+
+from datetime import timedelta
+
+from freezegun.api import FrozenDateTimeFactory
+
+from homeassistant.components.seventeentrack.sensor import DEFAULT_SCAN_INTERVAL
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.setup import async_setup_component
+
+from tests.common import async_fire_time_changed
+
+
+async def init_integration(hass: HomeAssistant, config: ConfigType):
+    """Set up the seventeentrack integration in Home Assistant."""
+    assert await async_setup_component(hass, "sensor", config)
+    await hass.async_block_till_done()
+
+
+async def goto_future(hass: HomeAssistant, freezer: FrozenDateTimeFactory):
+    """Move to future."""
+    freezer.tick(DEFAULT_SCAN_INTERVAL + timedelta(minutes=1))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
