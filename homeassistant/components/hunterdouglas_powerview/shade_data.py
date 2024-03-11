7ab24aae4de5d44ea7357e8a0d16b1caccd1d355
J. Nick Koston <nick@koston.org>
2024-03-10 14:33:44 -1000

    tweak

--- homeassistant/components/hunterdouglas_powerview/shade_data.py
+++ homeassistant/components/hunterdouglas_powerview/shade_data.py
@@ -35,7 +35,7 @@ class PowerviewShadeData:
     def __init__(self) -> None:
         """Init the shade data."""
         self._raw_data_by_id: dict[int, dict[str | int, Any]] = {}
-        self._shade_data_by_id: dict[int, BaseShade] = {}
+        self._shade_group_data_by_id: dict[int, BaseShade] = {}
         self.positions: dict[int, ShadePosition] = {}
 
     def get_raw_data(self, shade_id: int) -> dict[str | int, Any]:
@@ -48,22 +48,22 @@ class PowerviewShadeData:
 
     def get_shade(self, shade_id: int) -> BaseShade:
         """Get specific shade from the coordinator."""
-        return self._shade_data_by_id[shade_id]
+        return self._shade_group_data_by_id[shade_id]
 
     def get_shade_position(self, shade_id: int) -> ShadePosition:
         """Get positions for a shade."""
         if shade_id not in self.positions:
             shade_position = ShadePosition()
             # If we have the group data, use it to populate the initial position
-            if shade := self._shade_data_by_id.get(shade_id):
+            if shade := self._shade_group_data_by_id.get(shade_id):
                 copy_position_data(shade.current_position, shade_position)
             self.positions[shade_id] = shade_position
         return self.positions[shade_id]
 
     def update_from_group_data(self, shade_id: int) -> None:
         """Process an update from the group data."""
-        data = self._shade_data_by_id[shade_id]
-        self.update_shade_position(data.id, data.current_position)
+        data = self._shade_group_data_by_id[shade_id]
+        copy_position_data(data.current_position, self.get_shade_position(data.id))
 
     def store_group_data(self, shade_data: PowerviewData) -> None:
         """Store data from the all shades endpoint.
@@ -73,10 +73,9 @@ class PowerviewShadeData:
         with a shade_id will update a specific shade
         from the group data.
         """
-        self._shade_data_by_id = shade_data.processed
+        self._shade_group_data_by_id = shade_data.processed
         self._raw_data_by_id = async_map_data_by_id(shade_data.raw)
 
     def update_shade_position(self, shade_id: int, new_position: ShadePosition) -> None:
         """Update a single shades position."""
-        # ShadePosition will return None if the value is not set
         copy_position_data(new_position, self.get_shade_position(shade_id))
