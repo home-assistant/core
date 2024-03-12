f800c564304601102ad78715fec2a81f7a7999ee
Marcus Priesch <marcus@priesch.priv.at>
2024-03-12 10:57:18 +0100

    update

--- homeassistant/components/awattar/const.py
+++ homeassistant/components/awattar/const.py
@@ -1,10 +1,8 @@
 """Constants for the aWATTar integration."""
 from __future__ import annotations
 
-from datetime import timedelta
 import logging
 from typing import Final
 
 DOMAIN: Final = "awattar"
 LOGGER = logging.getLogger(__package__)
-SCAN_INTERVAL = timedelta(hours=1)
