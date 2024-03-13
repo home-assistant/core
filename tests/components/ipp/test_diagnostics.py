e2d06b11599e18ceb217209908d5296ef65e1c61
Chris Talkington <chris@talkingtontech.com>
2024-03-13 01:04:46 -0500

    cleanup

--- tests/components/ipp/test_diagnostics.py
+++ tests/components/ipp/test_diagnostics.py
@@ -1,10 +1,8 @@
 """Tests for the diagnostics data provided by the Internet Printing Protocol (IPP) integration."""
 
-from pyipp import Printer
 from syrupy import SnapshotAssertion
 
 from homeassistant.core import HomeAssistant
-from homeassistant.util import dt as dt_util
 
 from tests.common import MockConfigEntry
 from tests.components.diagnostics import get_diagnostics_for_config_entry
@@ -14,13 +12,10 @@ from tests.typing import ClientSessionGenerator
 async def test_diagnostics(
     hass: HomeAssistant,
     hass_client: ClientSessionGenerator,
-    mock_printer: Printer,
     init_integration: MockConfigEntry,
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test diagnostics for config entry."""
-    mock_printer.state.at = dt_util.parse_datetime("2023-08-15 17:00:00-00:00")
-
     assert (
         await get_diagnostics_for_config_entry(hass, hass_client, init_integration)
         == snapshot
