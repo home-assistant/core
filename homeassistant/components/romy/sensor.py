3e53c1cba99b66e0a9b012607395c789a9f13915
Manuel Dipolt <manuel@linux-home.at>
2024-03-13 20:35:34 +0100

    code review changes base enitity.py see branch romy_binary_sensor

--- homeassistant/components/romy/sensor.py
+++ homeassistant/components/romy/sensor.py
@@ -1,8 +1,5 @@
 """Sensor checking adc and status values from your ROMY."""
 
-from dataclasses import dataclass
-
-from romy import RomyRobot
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -19,59 +16,53 @@ from homeassistant.const import (
     UnitOfLength,
     UnitOfTime,
 )
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 from .coordinator import RomyVacuumCoordinator
+from .entity import RomyEntity
 
-
-@dataclass(frozen=True)
-class RomySensorEntityDescription(SensorEntityDescription):
-    """Immutable class for describing Romy data."""
-
-
-SENSORS: list[RomySensorEntityDescription] = [
-    RomySensorEntityDescription(
+SENSORS: list[SensorEntityDescription] = [
+    SensorEntityDescription(
         key="battery_level",
         native_unit_of_measurement=PERCENTAGE,
         device_class=SensorDeviceClass.BATTERY,
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
-    RomySensorEntityDescription(
+    SensorEntityDescription(
         key="rssi",
         native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
         device_class=SensorDeviceClass.SIGNAL_STRENGTH,
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
-    RomySensorEntityDescription(
+    SensorEntityDescription(
         key="dustbin_sensor",
         translation_key="dustbin_sensor",
         state_class=SensorStateClass.MEASUREMENT,
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
-    RomySensorEntityDescription(
+    SensorEntityDescription(
         key="total_cleaning_time",
         translation_key="total_cleaning_time",
         native_unit_of_measurement=UnitOfTime.HOURS,
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
-    RomySensorEntityDescription(
+    SensorEntityDescription(
         key="total_number_of_cleaning_runs",
         translation_key="total_number_of_cleaning_runs",
         state_class=SensorStateClass.MEASUREMENT,
         native_unit_of_measurement="Cleaning Runs",
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
-    RomySensorEntityDescription(
+    SensorEntityDescription(
         key="total_area_cleaned",
         translation_key="total_area_cleaned",
         native_unit_of_measurement=AREA_SQUARE_METERS,
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
-    RomySensorEntityDescription(
+    SensorEntityDescription(
         key="total_distance_driven",
         translation_key="total_distance_driven",
         state_class=SensorStateClass.MEASUREMENT,
@@ -91,48 +82,29 @@ async def async_setup_entry(
     coordinator: RomyVacuumCoordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     async_add_entities(
-        RomySensor(coordinator, coordinator.romy, entity_description)
+        RomySensor(coordinator, entity_description)
         for entity_description in SENSORS
         if entity_description.key in coordinator.romy.sensors
     )
 
 
-class RomySensor(CoordinatorEntity[RomyVacuumCoordinator], SensorEntity):
+class RomySensor(RomyEntity, CoordinatorEntity[RomyVacuumCoordinator], SensorEntity):
     """RomySensor Class."""
 
-    entity_description: RomySensorEntityDescription
+    entity_description: SensorEntityDescription
 
     def __init__(
         self,
         coordinator: RomyVacuumCoordinator,
-        romy: RomyRobot,
-        entity_description: RomySensorEntityDescription,
+        entity_description: SensorEntityDescription,
     ) -> None:
         """Initialize ROMYs StatusSensor."""
-        self._sensor_value: int | None = None
-        super().__init__(coordinator)
-        self.romy = romy
-        self._attr_unique_id = self.romy.unique_id
-        self._device_info = DeviceInfo(
-            identifiers={(DOMAIN, romy.unique_id)},
-            manufacturer="ROMY",
-            name=romy.name,
-            model=romy.model,
-        )
+        RomyEntity.__init__(self, coordinator.romy)
+        CoordinatorEntity.__init__(self, coordinator)
+        self._attr_unique_id = f"{entity_description.key}_{self.romy.unique_id}"
         self.entity_description = entity_description
 
     @property
-    def unique_id(self) -> str:
-        """Return the ID of this sensor."""
-        return f"{self.entity_description.key}_{self._attr_unique_id}"
-
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        """Handle updated data from the coordinator."""
-        self._sensor_value = self.romy.sensors[self.entity_description.key]
-        self.async_write_ha_state()
-
-    @property
-    def native_value(self) -> int | None:
+    def native_value(self) -> int:
         """Return the value of the sensor."""
-        return self._sensor_value
+        return self.romy.sensors[self.entity_description.key]
