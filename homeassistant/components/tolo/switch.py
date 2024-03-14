bb1ac89a7864c8b9d1c3fa30f1bbca5c139b9d11
Matthias Lohr <mail@mlohr.com>
2024-03-14 18:44:44 +0100

    aroma therapy and salt bath switch

--- homeassistant/components/tolo/switch.py
+++ homeassistant/components/tolo/switch.py
@@ -1,17 +1,45 @@
-"""TOLO Sauna switches."""
+"""TOLO Sauna switch controls."""
 
 from __future__ import annotations
 
+from collections.abc import Callable
+from dataclasses import dataclass
 from typing import Any
 
-from homeassistant.components.switch import SwitchEntity
+from tololib import ToloClient, ToloStatus
+
+from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import ToloSaunaCoordinatorEntity, ToloSaunaUpdateCoordinator
 from .const import DOMAIN
+
+
+@dataclass(frozen=True, kw_only=True)
+class ToloSwitchEntityDescription(SwitchEntityDescription):
+    """Class describing TOLO switch entities."""
+
+    key: str
+    getter: Callable[[ToloStatus], bool]
+    setter: Callable[[ToloClient, bool], bool]
+
+
+SWITCHES = (
+    ToloSwitchEntityDescription(
+        key="aroma_therapy_on",
+        translation_key="aroma_therapy_on",
+        getter=lambda status: status.aroma_therapy_on,
+        setter=lambda client, value: client.set_aroma_therapy_on(value),
+    ),
+    ToloSwitchEntityDescription(
+        key="salt_bath_on",
+        translation_key="salt_bath_on",
+        getter=lambda status: status.salt_bath_on,
+        setter=lambda client, value: client.set_salt_bath_on(value),
+    ),
+)
 
 
 async def async_setup_entry(
@@ -21,32 +49,36 @@ async def async_setup_entry(
 ) -> None:
     """Set up switch controls for TOLO Sauna."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([AromaTherapyOnSwitch(coordinator, entry)])
+    async_add_entities(
+        ToloSwitchEntity(coordinator, entry, description) for description in SWITCHES
+    )
 
 
-class AromaTherapyOnSwitch(ToloSaunaCoordinatorEntity, SwitchEntity):
-    """Enable/disable Aroma Therapy for TOLO Sauna."""
+class ToloSwitchEntity(ToloSaunaCoordinatorEntity, SwitchEntity):
+    """TOLO switch entity."""
 
-    _attr_entity_category = EntityCategory.CONFIG
-    _attr_translation_key = "aroma_therapy_on"
+    entity_description: ToloSwitchEntityDescription
 
     def __init__(
-        self, coordinator: ToloSaunaUpdateCoordinator, entry: ConfigEntry
+        self,
+        coordinator: ToloSaunaUpdateCoordinator,
+        entry: ConfigEntry,
+        entity_description: ToloSwitchEntityDescription,
     ) -> None:
-        """Initialize TOLO Aroma Therapy Switch."""
+        """Initialize TOLO switch entity."""
         super().__init__(coordinator, entry)
-
-        self._attr_unique_id = f"{entry.entry_id}_aroma_therapy_on"
+        self.entity_description = entity_description
+        self._attr_unique_id = f"{entry.entry_id}_{entity_description.key}"
 
     @property
-    def is_on(self) -> bool | None:
-        """Return if Aroma Therapy is currently on."""
-        return self.coordinator.data.status.aroma_therapy_on
+    def is_on(self) -> bool:
+        """Return if the switch is currently on."""
+        return self.entity_description.getter(self.coordinator.data.status)
 
     def turn_on(self, **kwargs: Any) -> None:
-        """Enable Aroma Therapy."""
-        self.coordinator.client.set_aroma_therapy_on(True)
+        """Turn the switch on."""
+        self.entity_description.setter(self.coordinator.client, True)
 
     def turn_off(self, **kwargs: Any) -> None:
-        """Disable Aroma Therapy."""
-        self.coordinator.client.set_aroma_therapy_on(False)
+        """Turn the switch off."""
+        self.entity_description.setter(self.coordinator.client, False)
