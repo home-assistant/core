822cb467d8473db4fd0834779022bdca5d61e193
dontinelli <73341522+dontinelli@users.noreply.github.com>
2024-03-15 16:17:09 +0000

    move test-helpers into conftest.py, adjust import of coordinator.py

--- tests/components/fyta/test_config_flow.py
+++ tests/components/fyta/test_config_flow.py
@@ -1,7 +1,6 @@
 """Test the fyta config flow."""
-from collections.abc import Generator
 from datetime import datetime
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock
 
 from fyta_cli.fyta_exceptions import (
     FytaAuthentificationError,
@@ -21,31 +20,6 @@ USERNAME = "fyta_user"
 PASSWORD = "fyta_pass"
 ACCESS_TOKEN = "123xyz"
 EXPIRATION = datetime.now()
-
-
-@pytest.fixture
-def mock_fyta():
-    """Build a fixture for the Fyta API that connects successfully and returns one device."""
-
-    mock_fyta_api = AsyncMock()
-    with patch(
-        "homeassistant.components.fyta.config_flow.FytaConnector",
-        return_value=mock_fyta_api,
-    ) as mock_fyta_api:
-        mock_fyta_api.return_value.login.return_value = {
-            "access_token": ACCESS_TOKEN,
-            "expiration": EXPIRATION,
-        }
-        yield mock_fyta_api
-
-
-@pytest.fixture
-def mock_setup_entry() -> Generator[AsyncMock, None, None]:
-    """Override async_setup_entry."""
-    with patch(
-        "homeassistant.components.fyta.async_setup_entry", return_value=True
-    ) as mock_setup_entry:
-        yield mock_setup_entry
 
 
 async def test_user_flow(
