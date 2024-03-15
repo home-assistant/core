59bf05bca07c8e6109d043eae37710789ee93243
J. Nick Koston <nick@koston.org>
2024-03-14 23:36:12 -1000

    Speed up ffmpeg setup
    Only check for old versions of ffmpeg if is_official_image
    is False

--- homeassistant/components/ffmpeg/__init__.py
+++ homeassistant/components/ffmpeg/__init__.py
@@ -158,8 +158,8 @@ class FFmpegManager:
         if (
             not is_official_image()
             and (version := await FFVersion(self._bin).get_version())
-            and (major_ver := re.search(r"(\d+)\.", version))
-            and (int(major_ver.group(1))) <= 3
+            and (major_version := re.search(r"(\d+)\.", version))
+            and (int(major_version.group(1))) <= 3
         ):
             self._content_type = CONTENT_TYPE_MULTIPART.format("ffserver")
 
