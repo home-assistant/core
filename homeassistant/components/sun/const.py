6408e3035f3230587940118dd3b221b4f4d43855
J. Nick Koston <nick@koston.org>
2024-03-15 09:10:03 -1000

    Speed up loading sun

--- homeassistant/components/sun/const.py
+++ homeassistant/components/sun/const.py
@@ -8,3 +8,7 @@ DEFAULT_NAME: Final = "Sun"
 
 SIGNAL_POSITION_CHANGED = f"{DOMAIN}_position_changed"
 SIGNAL_EVENTS_CHANGED = f"{DOMAIN}_events_changed"
+
+
+STATE_ABOVE_HORIZON = "above_horizon"
+STATE_BELOW_HORIZON = "below_horizon"
