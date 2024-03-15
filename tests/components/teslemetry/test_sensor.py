65d7042bd1dbc6526a4558b2011db28fa98bcef0
Brett <brett.whynot@gmail.com>
2024-03-15 22:36:16 +0000

    Remove useless test

--- tests/components/teslemetry/test_sensor.py
+++ tests/components/teslemetry/test_sensor.py
@@ -1,19 +1,15 @@
 """Test the Teslemetry sensor platform."""
-from datetime import timedelta
 
 from freezegun.api import FrozenDateTimeFactory
 import pytest
 from syrupy import SnapshotAssertion
 
-from homeassistant.components.teslemetry.coordinator import SYNC_INTERVAL
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
 from . import assert_entities, setup_platform
 from .const import VEHICLE_DATA_ALT
-
-from tests.common import async_fire_time_changed
 
 
 @pytest.mark.usefixtures("entity_registry_enabled_by_default")
@@ -30,11 +26,6 @@ async def test_sensors(
     entry = await setup_platform(hass, [Platform.SENSOR])
 
     assert_entities(hass, entry.entry_id, entity_registry, snapshot)
-
-    # Coordinator refresh
-    freezer.tick(timedelta(seconds=SYNC_INTERVAL))
-    async_fire_time_changed(hass)
-    await hass.async_block_till_done()
 
 
 @pytest.mark.usefixtures("entity_registry_enabled_by_default")
