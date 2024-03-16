1f0b4ab182195c060668d8c345051e2b3eea2c79
J. Nick Koston <nick@koston.org>
2024-03-15 15:04:47 -1000

    cleanup

--- homeassistant/bootstrap.py
+++ homeassistant/bootstrap.py
@@ -709,11 +709,11 @@ async def _async_resolve_domains_to_setup(
     platform_integrations = conf_util.extract_platform_integrations(
         config, BASE_PLATFORMS
     )
-    # Ensure we setup of base platforms that have platform integrations
-    # are added to domains_to_setup so they can be setup first instead
-    # of discovering them when later when a config entry setup process
-    # notices it needs to be setup and there is already a long line
-    # to use the import executor.
+    # Ensure base platforms that have platform integrations are added to
+    # to `domains_to_setup so they can be setup first instead of
+    # discovering them when later when a config entry setup task
+    # notices its needed and there is already a long line to use
+    # the import executor.
     #
     # For example if we have
     # sensor:
