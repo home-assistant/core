7c52537086c93842f270a66b4c2c023807e20830
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-03-16 22:24:35 +0100

    Fix import

--- homeassistant/components/threshold/binary_sensor.py
+++ homeassistant/components/threshold/binary_sensor.py
@@ -23,7 +23,7 @@ from homeassistant.const import (
     STATE_UNAVAILABLE,
     STATE_UNKNOWN,
 )
-from homeassistant.core import Event, EventStateChangedData, HomeAssistant, callback
+from homeassistant.core import CALLBACK_TYPE, Event, EventStateChangedData, HomeAssistant, callback
 from homeassistant.helpers import (
     config_validation as cv,
     device_registry as dr,
