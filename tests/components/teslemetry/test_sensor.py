2891f99a51f07dbf3e45cdf9b97bc27b39ebdede
Brett <brett.whynot@gmail.com>
2024-03-16 10:23:49 +0000

    Add assertion to post coordinator refresh

--- tests/components/teslemetry/test_sensor.py
+++ tests/components/teslemetry/test_sensor.py
@@ -10,7 +10,7 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
-from . import assert_entities, setup_platform
+from . import assert_entities, assert_entities_alt, setup_platform
 from .const import VEHICLE_DATA_ALT
 
 from tests.common import async_fire_time_changed
@@ -22,6 +22,7 @@ async def test_sensors(
     snapshot: SnapshotAssertion,
     entity_registry: er.EntityRegistry,
     freezer: FrozenDateTimeFactory,
+    mock_vehicle_data,
 ) -> None:
     """Tests that the sensor entities are correct."""
 
@@ -32,28 +33,9 @@ async def test_sensors(
     assert_entities(hass, entry.entry_id, entity_registry, snapshot)
 
     # Coordinator refresh
+    mock_vehicle_data.return_value = VEHICLE_DATA_ALT
     freezer.tick(timedelta(seconds=SYNC_INTERVAL))
     async_fire_time_changed(hass)
     await hass.async_block_till_done()
 
-    entity_id = "sensor.test_battery_level"
-    state = hass.states.get(entity_id)
-    assert state.state == snapshot(name=entity_id)
-
-
-@pytest.mark.usefixtures("entity_registry_enabled_by_default")
-async def test_sensors_alt(
-    hass: HomeAssistant,
-    snapshot: SnapshotAssertion,
-    entity_registry: er.EntityRegistry,
-    freezer: FrozenDateTimeFactory,
-    mock_vehicle_data,
-) -> None:
-    """Tests that the sensor entities are correct."""
-    mock_vehicle_data.return_value = VEHICLE_DATA_ALT
-
-    freezer.move_to("2024-01-01 00:00:00+00:00")
-
-    entry = await setup_platform(hass, [Platform.SENSOR])
-
-    assert_entities(hass, entry.entry_id, entity_registry, snapshot)
+    assert_entities_alt(hass, entry.entry_id, entity_registry, snapshot)
