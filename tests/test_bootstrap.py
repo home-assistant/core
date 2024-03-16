78b529cd38f6f7235460b7bd212350dcc04bc017
J. Nick Koston <nick@koston.org>
2024-03-15 21:40:17 -1000

    coverage

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -223,6 +223,62 @@ async def test_setup_after_deps_in_stage_1_ignored(hass: HomeAssistant) -> None:
 
 
 @pytest.mark.parametrize("load_registries", [False])
+async def test_setup_after_deps_manifests_are_loaded_even_if_not_setup(
+    hass: HomeAssistant,
+) -> None:
+    """Ensure we preload manifests for after deps even if they are not setup.
+
+    Its important that we preload the after dep manifests even if they are not setup
+    since we will always have to check their requirements since any integration
+    that lists an after dep may import it and we have to ensure requirements are
+    up to date before the after dep can be imported.
+    """
+    # This test relies on this
+    assert "cloud" in bootstrap.STAGE_1_INTEGRATIONS
+    order = []
+
+    def gen_domain_setup(domain):
+        async def async_setup(hass, config):
+            order.append(domain)
+            return True
+
+        return async_setup
+
+    mock_integration(
+        hass,
+        MockModule(
+            domain="normal_integration",
+            async_setup=gen_domain_setup("normal_integration"),
+            partial_manifest={"after_dependencies": ["an_after_dep"]},
+        ),
+    )
+    mock_integration(
+        hass,
+        MockModule(
+            domain="an_after_dep",
+            async_setup=gen_domain_setup("an_after_dep"),
+        ),
+    )
+    mock_integration(
+        hass,
+        MockModule(
+            domain="cloud",
+            async_setup=gen_domain_setup("cloud"),
+            partial_manifest={"after_dependencies": ["normal_integration"]},
+        ),
+    )
+
+    await bootstrap._async_set_up_integrations(
+        hass, {"cloud": {}, "normal_integration": {}}
+    )
+
+    assert "normal_integration" in hass.config.components
+    assert "cloud" in hass.config.components
+    assert order == ["cloud", "normal_integration"]
+    assert loader.async_get_loaded_integration(hass, "an_after_dep") is not None
+
+
+@pytest.mark.parametrize("load_registries", [False])
 async def test_setup_frontend_before_recorder(hass: HomeAssistant) -> None:
     """Test frontend is setup before recorder."""
     order = []
