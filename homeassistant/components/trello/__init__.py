1663a01ccca383be93d9d07bcf2d55542b322830
Scott Giminiani <scottg489@gmail.com>
2024-03-17 19:55:47 -0400

    Remove unused async_update_entry method definition
    Since there's no options flow this isn't needed. There used to be an
    options flow but it was removed and this was never cleaned up.

--- homeassistant/components/trello/__init__.py
+++ homeassistant/components/trello/__init__.py
@@ -26,14 +26,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = trello_coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(async_update_entry))
 
     return True
-
-
-async def async_update_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Update a given config entry."""
-    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
