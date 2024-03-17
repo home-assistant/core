191cb31e42dd5673aa0a876a56673c5e07855482
Henning Claßen <code@clssn.de>
2024-03-17 15:13:58 +0000

    Handle error raised for devices not supporting notifications
    Before numato-gpio v0.13.0 no error was raised but the setup of the
    callback failed silently.
    
    The user is now informed via an info message that with their device
    push mode isn't possible.

--- tests/components/numato/test_binary_sensor.py
+++ tests/components/numato/test_binary_sensor.py
@@ -1,11 +1,17 @@
 """Tests for the numato binary_sensor platform."""
 
+import logging
+from unittest.mock import patch
+
+import pytest
+
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import discovery
 from homeassistant.setup import async_setup_component
 
 from .common import NUMATO_CFG, mockup_raise
+from .numato_mock import NumatoGpioError, NumatoModuleMock
 
 MOCKUP_ENTITY_IDS = {
     "binary_sensor.numato_binary_sensor_mock_port2",
@@ -73,3 +79,32 @@ async def test_binary_sensor_setup_without_discovery_info(
     await hass.async_block_till_done()  # wait for numato platform to be loaded
     for entity_id in MOCKUP_ENTITY_IDS:
         assert entity_id in hass.states.async_entity_ids()
+
+
+async def test_binary_sensor_setup_no_notify(
+    hass: HomeAssistant,
+    numato_fixture,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    """Setup of a device without notification capability shall print an info message."""
+    caplog.set_level(logging.INFO)
+
+    def raise_notification_error(self, port, callback, direction):
+        raise NumatoGpioError(
+            f"{repr(self)} Mockup device doesn't support notifications."
+        )
+
+    with patch.object(
+        NumatoModuleMock.NumatoDeviceMock,
+        "add_event_detect",
+        raise_notification_error,
+    ):
+        numato_fixture.discover()
+        assert await async_setup_component(hass, "numato", NUMATO_CFG)
+        await hass.async_block_till_done()  # wait until services are registered
+
+    assert all(
+        f"updates on binary sensor numato_binary_sensor_mock_port{port} only in polling mode"
+        in caplog.text
+        for port in NUMATO_CFG["numato"]["devices"][0]["binary_sensors"]["ports"]
+    )
