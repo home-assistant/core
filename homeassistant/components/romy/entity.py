55091745b47869987353577287febed4728bd0e6
Manuel Dipolt <manuel@linux-home.at>
2024-03-18 17:49:13 +0100

    cherry picked base entity changes from the branches romy_binary_sensor & romy_sensor

---
+++ homeassistant/components/romy/entity.py
@@ -0,0 +1,24 @@
+"""Base entity for ROMY."""
+
+from romy import RomyRobot
+
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity import Entity
+
+from .const import DOMAIN
+
+
+class RomyEntity(Entity):
+    """Base ROMY entity."""
+
+    _attr_has_entity_name = True
+
+    def __init__(self, romy: RomyRobot) -> None:
+        """Initialize ROMY entity."""
+        self.romy = romy
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, romy.unique_id)},
+            manufacturer="ROMY",
+            name=romy.name,
+            model=romy.model,
+        )
