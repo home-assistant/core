ed6c5f252b4fae137c8f80293bd98605efe8e44c
J. Nick Koston <nick@koston.org>
2024-03-17 19:25:54 -1000

    fix test

--- tests/components/cloud/test_tts.py
+++ tests/components/cloud/test_tts.py
@@ -103,12 +103,12 @@ async def test_prefs_default_voice(
     assert await async_setup_component(hass, TTS_DOMAIN, {TTS_DOMAIN: platform_config})
     assert await async_setup_component(hass, DOMAIN, {DOMAIN: {}})
     await hass.async_block_till_done()
-    await hass.async_block_till_done()
 
     assert cloud.client.prefs.tts_default_voice == ("en-US", "female")
 
     on_start_callback = cloud.register_on_start.call_args[0][0]
     await on_start_callback()
+    await hass.async_block_till_done()
 
     engine = get_engine_instance(hass, engine_id)
 
