5436e363898f9441f52f20eee76a17dcf3061e48
J. Nick Koston <nick@koston.org>
2024-03-17 17:28:42 -1000

    Move legacy device_tracker setup to a tracked task
    Legacy platforms are now loaded in a tracked task which
    allows anything waiting on device_tracker (such as
    a config entry that uses the device_tracker
    platform) to proceed.
    
    This also allows us to remove the workaround
    of adding device_tracker to
    hass.config.components in its setup

--- homeassistant/components/device_tracker/legacy.py
+++ homeassistant/components/device_tracker/legacy.py
@@ -203,6 +203,31 @@ async def async_setup_integration(hass: HomeAssistant, config: ConfigType) -> No
     """Set up the legacy integration."""
     tracker = await get_tracker(hass, config)
 
+    async def async_see_service(call: ServiceCall) -> None:
+        """Service to see a device."""
+        # Temp workaround for iOS, introduced in 0.65
+        data = dict(call.data)
+        data.pop("hostname", None)
+        data.pop("battery_status", None)
+        await tracker.async_see(**data)
+
+    hass.services.async_register(
+        DOMAIN, SERVICE_SEE, async_see_service, SERVICE_SEE_PAYLOAD_SCHEMA
+    )
+
+    # The platforms load in a tracked task to ensure
+    # device tracker setup can continue and config
+    # entry integrations are not waiting for
+    # legacy device tracker platforms to be set up.
+    hass.async_create_task(
+        _async_setup_legacy_integration(hass, config, tracker), eager_start=True
+    )
+
+
+async def _async_setup_legacy_integration(
+    hass: HomeAssistant, config: ConfigType, tracker: DeviceTracker
+) -> None:
+    """Set up the legacy integration."""
     legacy_platforms = await async_extract_config(hass, config)
 
     setup_tasks = [
@@ -229,18 +254,6 @@ async def async_setup_integration(hass: HomeAssistant, config: ConfigType) -> No
     # Clean up stale devices
     cancel_update_stale = async_track_utc_time_change(
         hass, tracker.async_update_stale, second=range(0, 60, 5)
-    )
-
-    async def async_see_service(call: ServiceCall) -> None:
-        """Service to see a device."""
-        # Temp workaround for iOS, introduced in 0.65
-        data = dict(call.data)
-        data.pop("hostname", None)
-        data.pop("battery_status", None)
-        await tracker.async_see(**data)
-
-    hass.services.async_register(
-        DOMAIN, SERVICE_SEE, async_see_service, SERVICE_SEE_PAYLOAD_SCHEMA
     )
 
     # restore
