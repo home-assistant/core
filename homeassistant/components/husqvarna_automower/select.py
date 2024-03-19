90c9f24b5562399e9801389af61f5f353e4737fa
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-03-19 16:22:21 +0000

    Add select platform to Husqvarna Automower

---
+++ homeassistant/components/husqvarna_automower/select.py
@@ -0,0 +1,63 @@
+"""Creates a the select entity for the mower."""
+
+import logging
+
+from aioautomower.exceptions import ApiException
+from aioautomower.model import HeadlightModes
+
+from homeassistant.components.select import SelectEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EntityCategory
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .coordinator import AutomowerDataUpdateCoordinator
+from .entity import AutomowerControlEntity
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Set up select platform."""
+    coordinator: AutomowerDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities(
+        AutomowerSelectEntity(mower_id, coordinator)
+        for mower_id in coordinator.data
+        if coordinator.data[mower_id].capabilities.headlights
+    )
+
+
+class AutomowerSelectEntity(AutomowerControlEntity, SelectEntity):
+    """Defining the headlight mode entity."""
+
+    _attr_options = options = [option.lower() for option in list(HeadlightModes)]
+    _attr_icon = "mdi:car-light-high"
+    _attr_entity_category = EntityCategory.CONFIG
+    _attr_translation_key = "headlight_mode"
+
+    def __init__(
+        self,
+        mower_id: str,
+        coordinator: AutomowerDataUpdateCoordinator,
+    ) -> None:
+        """Set up select platform."""
+        super().__init__(mower_id, coordinator)
+        self._attr_unique_id = f"{mower_id}_{self._attr_translation_key}"
+
+    @property
+    def current_option(self) -> str:
+        """Return a the current option for the entity."""
+        return self.mower_attributes.headlight.mode.lower()
+
+    async def async_select_option(self, option: str) -> None:
+        """Change the selected option."""
+        try:
+            await self.coordinator.api.set_headlight_mode(self.mower_id, option.upper())
+        except ApiException as exception:
+            raise HomeAssistantError(
+                f"Command couldn't be sent to the command queue: {exception}"
+            ) from exception
