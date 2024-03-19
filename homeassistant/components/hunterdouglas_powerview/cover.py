0e0d22fab39504b0afacf2cec6961cec947f933f
Kingy444 <toddlesking4@hotmail.com>
2024-03-19 07:24:35 +0000

    add current positioning

--- homeassistant/components/hunterdouglas_powerview/cover.py
+++ homeassistant/components/hunterdouglas_powerview/cover.py
@@ -521,6 +521,17 @@ class PowerViewShadeTiltOnly(PowerViewShadeWithTiltBase):
         self._max_tilt = self._shade.shade_limits.tilt_max
 
     @property
+    def current_cover_position(self) -> int:
+        """Return the current position of cover."""
+        # allows using parent class with no other alterations
+        return CLOSED_POSITION
+
+    @property
+    def transition_steps(self) -> int:
+        """Return the steps to make a move."""
+        return self.positions.tilt
+
+    @property
     def is_closed(self) -> bool:
         """Return if the cover is closed."""
         return self.positions.tilt <= CLOSED_POSITION
