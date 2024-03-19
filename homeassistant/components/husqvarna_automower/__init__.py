7f3fe5b25a9b45c6197d9d12ed8de9ec0ba8f4e2
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-03-19 15:30:30 +0100

    Add binary sensor platform for Husqvarna Automower (#113248)
    * Add binary sensor platform for Husqvarna Automower
    
    * revert changes in sensor.py
    
    * use == instead of is
    
    * remove the parantheses
    
    * Apply suggestions from code review
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>
    
    * Docstring
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/husqvarna_automower/__init__.py
+++ homeassistant/components/husqvarna_automower/__init__.py
@@ -17,7 +17,12 @@ from .coordinator import AutomowerDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS: list[Platform] = [Platform.LAWN_MOWER, Platform.SENSOR, Platform.SWITCH]
+PLATFORMS: list[Platform] = [
+    Platform.BINARY_SENSOR,
+    Platform.LAWN_MOWER,
+    Platform.SENSOR,
+    Platform.SWITCH,
+]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
