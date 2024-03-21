5ace68a0c6ee6dfdb29a03f4ace4ddbc58fa2634
Dave T <17680170+davet2001@users.noreply.github.com>
2024-03-21 23:12:55 +0000

    Updates following review

--- script/check_coveragerc.py
+++ script/check_coveragerc.py
@@ -15,7 +15,7 @@ COVERAGERC = ".coveragerc"
 TEMP_JSON = "build/_temp_coverage.json"
 
 
-def replace_all(file, before, after):
+def replace_all(file: str, before: str, after: str):
     """Replace all instances of string in file."""
     for line in fileinput.input(file, inplace=1):
         print(line.replace(before, after), end="")
@@ -23,45 +23,46 @@ def replace_all(file, before, after):
 
 def main():
     """Run the script."""
+
     if os.path.exists(TEMP_JSON):
         os.remove(TEMP_JSON)
 
-    # Turn off all .coveragerc ignoring
-    replace_all(COVERAGERC, "omit =", "omit_disabled =")
+    try:
+        # Turn off all .coveragerc ignoring
+        replace_all(COVERAGERC, "omit =", "omit_disabled =")
 
-    # Run tests in parallel for speed
-    pytest.main(
-        [
-            "-n",
-            "auto",
-            "tests/components/",
-            "--cov=homeassistant.components",
-            "--cov-report",
-            f"json:{TEMP_JSON}",
-        ]
-    )
+        # Run tests in parallel for speed
+        pytest.main(
+            [
+                "-n",
+                "auto",
+                "tests/components/",
+                "--cov=homeassistant.components",
+                "--cov-report",
+                f"json:{TEMP_JSON}",
+            ]
+        )
 
-    # parse the json results file
-    with open(TEMP_JSON, encoding="UTF-8") as infile:
-        data = json.load(infile)
-        infile.close()
+        # parse the json results file
+        with open(TEMP_JSON, encoding="UTF-8") as infile:
+            data = json.load(infile)
 
-    # make a list of 100% passing files
-    out_list = []
-    json.dumps(data, indent=2)
-    for filename, file_data in data["files"].items():
-        percent = file_data["summary"]["percent_covered"]
-        if percent == 100.0:
-            out_list.append(filename)
+        # make a list of 100% passing files
+        out_list = []
+        for filename, file_data in data["files"].items():
+            percent = file_data["summary"]["percent_covered"]
+            if percent == 100.0:
+                out_list.append(filename)
 
-    # remove those files from .coveragerc
-    print("100pc covered files:")
-    for filename in out_list:
-        print(filename)
-        replace_all(COVERAGERC, f"    {filename}\n", "")
+        # remove those files from .coveragerc
+        print("100pc covered files:")
+        for filename in out_list:
+            print(filename)
+            replace_all(COVERAGERC, f"    {filename}\n", "")
 
-    # Turn .coveragerc ignoring back on
-    replace_all(COVERAGERC, "omit_disabled =", "omit =")
+    finally:
+        # Turn .coveragerc ignoring back on
+        replace_all(COVERAGERC, "omit_disabled =", "omit =")
 
 
 if __name__ == "__main__":
