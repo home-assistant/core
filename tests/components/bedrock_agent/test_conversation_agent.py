ff2a5b61b14f791d1b77d06c264bf2af6430be6a
paul-enno <paul.enno@gmail.com>
2024-03-21 07:57:45 +0000

    First test suite for Antropic.

--- tests/components/bedrock_agent/test_conversation_agent.py
+++ tests/components/bedrock_agent/test_conversation_agent.py
@@ -1,37 +1,79 @@
 """Tests for the Amazon Bedrock integration."""
+from io import BytesIO
+import json
 import logging
+from unittest import mock
 
+import boto3
+from botocore.response import StreamingBody
+from botocore.stub import Stubber
 import pytest
 
+from homeassistant.components import bedrock_agent, conversation
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
 
 logging.basicConfig(level=logging.DEBUG)
-LOGGER = logging.getLogger(__name__)
-
-
-async def test_default_prompt(
-    hass: HomeAssistant,
-) -> None:
-    """Test that the default prompt works."""
-    result = "Awesome"
-    # conversationInput = conversation.ConversationInput("hello", None, None, "123", "DE")
-    # agent = bedrock_agent.BedrockAgent(hass, mock_config_entry)
-    # conersationResult = await agent.async_process(conversationInput)
-    # answer = conersationResult.response.speech["plain"]["speech"]
-    assert result == "Awesome"
-    assert 1 == 1
+_LOGGER = logging.getLogger(__name__)
 
 
 @pytest.fixture
-def mock_config_entry(hass):
+def mock_config_entry(hass: HomeAssistant):
     """Mock a config entry."""
     entry = MockConfigEntry(
         domain="bedrock_agent",
         data={
-            "api_key": "bla",
+            "region": "us-west-2",
+            "key_id": "abc",
+            "key_secret": "123",
+            "model_id": "anthropic.claude-v2:1",
+            "prompt_context": "Prompt Context",
         },
     )
     entry.add_to_hass(hass)
     return entry
+
+
+@pytest.fixture
+def mock_bedrock_client():
+    """Mock bedrock client."""
+    client = boto3.client(
+        service_name="bedrock-runtime",
+        region_name="Region",
+        aws_access_key_id="Key ID",
+        aws_secret_access_key="Key Secret",
+    )
+    stubber = Stubber(client)
+
+    body_encoded = json.dumps(
+        {
+            "completion": "General Kenobi.",
+            "stop_reason": "stop_sequence",
+            "stop": "\n\nHuman:",
+        }
+    ).encode()
+
+    body = StreamingBody(BytesIO(body_encoded), len(body_encoded))
+
+    expected_response = {
+        "body": body,
+        "contentType": "application/json",
+    }
+
+    stubber.add_response("invoke_model", expected_response)
+    stubber.activate()
+    return client
+
+
+async def test_default_prompt(
+    hass: HomeAssistant, mock_config_entry, mock_bedrock_client
+) -> None:
+    """Test that the default prompt works."""
+    conversationInput = conversation.ConversationInput("hello", None, None, "123", "DE")
+    entry = mock_config_entry
+    with mock.patch("boto3.client", mock.MagicMock(return_value=mock_bedrock_client)):
+        agent = bedrock_agent.BedrockAgent(hass, entry)
+        conversationResult = await agent.async_process(conversationInput)
+        answer = conversationResult.response.speech["plain"]["speech"]
+    assert answer == "General Kenobi."
