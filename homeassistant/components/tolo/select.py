f8925eb86977fd7780f7bb1ce0b4ab1f0cc8616b
Matthias Lohr <mail@mlohr.com>
2024-03-22 11:55:36 +0100

    use enums defined in HA to define choices

--- homeassistant/components/tolo/select.py
+++ homeassistant/components/tolo/select.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from collections.abc import Callable
 from dataclasses import dataclass
 
-from tololib import AromaTherapySlot, LampMode, ToloClient, ToloSettings
+from tololib import ToloClient, ToloSettings
 
 from homeassistant.components.select import SelectEntity, SelectEntityDescription
 from homeassistant.config_entries import ConfigEntry
@@ -15,6 +15,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import ToloSaunaCoordinatorEntity, ToloSaunaUpdateCoordinator
 from .const import DOMAIN
+from .enums import AromaTherapySlot, LampMode
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -32,7 +33,9 @@ SELECTS = (
         translation_key="lamp_mode",
         options=[lamp_mode.name.lower() for lamp_mode in LampMode],
         getter=lambda settings: settings.lamp_mode.name.lower(),
-        setter=lambda client, option: client.set_lamp_mode(LampMode[option.upper()]),
+        setter=lambda client, option: client.set_lamp_mode(
+            LampMode[option.upper()].value
+        ),
     ),
     ToloSelectEntityDescription(
         key="aroma_therapy_slot",
@@ -42,7 +45,7 @@ SELECTS = (
         ],
         getter=lambda settings: settings.aroma_therapy_slot.name.lower(),
         setter=lambda client, option: client.set_aroma_therapy_slot(
-            AromaTherapySlot[option.upper()]
+            AromaTherapySlot[option.upper()].value
         ),
     ),
 )
