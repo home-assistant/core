c61d7e8cd84551f24950b1ec8e4273519105b600
Dave T <17680170+davet2001@users.noreply.github.com>
2024-03-23 21:52:22 +0000

    Add more sensors to aurora_abb_powerone (#114074)

--- homeassistant/components/aurora_abb_powerone/coordinator.py
+++ homeassistant/components/aurora_abb_powerone/coordinator.py
@@ -36,8 +36,11 @@ class AuroraAbbDataUpdateCoordinator(DataUpdateCoordinator[dict[str, float]]):  
             try:
                 self.client.connect()
 
-                # read ADC channel 3 (grid power output)
+                # See command 59 in the protocol manual linked in __init__.py
+                grid_voltage = self.client.measure(1, True)
+                grid_current = self.client.measure(2, True)
                 power_watts = self.client.measure(3, True)
+                frequency = self.client.measure(4)
                 temperature_c = self.client.measure(21)
                 energy_wh = self.client.cumulated_energy(5)
                 [alarm, *_] = self.client.alarms()
@@ -57,7 +60,10 @@ class AuroraAbbDataUpdateCoordinator(DataUpdateCoordinator[dict[str, float]]):  
                 )
                 sleep(1)
             else:
+                data["grid_voltage"] = round(grid_voltage, 1)
+                data["grid_current"] = round(grid_current, 1)
                 data["instantaneouspower"] = round(power_watts, 1)
+                data["grid_frequency"] = round(frequency, 1)
                 data["temp"] = round(temperature_c, 1)
                 data["totalenergy"] = round(energy_wh / 1000, 2)
                 data["alarm"] = alarm
