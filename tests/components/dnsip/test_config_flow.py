5e70faca5f738150b7aec45acc43eafe841fe701
G Johansson <goran.johansson@shiftit.se>
2024-03-25 17:38:02 +0100

    Refactor options flow in dnsip (#114058)
    * Refactor options flow in dnsip
    
    * Mods

--- tests/components/dnsip/test_config_flow.py
+++ tests/components/dnsip/test_config_flow.py
@@ -221,6 +221,61 @@ async def test_options_flow(hass: HomeAssistant) -> None:
     assert entry.state == config_entries.ConfigEntryState.LOADED
 
 
+async def test_options_flow_empty_return(hass: HomeAssistant) -> None:
+    """Test options config flow with empty return from user."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="12345",
+        data={
+            CONF_HOSTNAME: "home-assistant.io",
+            CONF_NAME: "home-assistant.io",
+            CONF_IPV4: True,
+            CONF_IPV6: False,
+        },
+        options={
+            CONF_RESOLVER: "8.8.8.8",
+            CONF_RESOLVER_IPV6: "2620:119:53::1",
+        },
+    )
+    entry.add_to_hass(hass)
+
+    with patch(
+        "homeassistant.components.dnsip.config_flow.aiodns.DNSResolver",
+        return_value=RetrieveDNS(),
+    ):
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+
+    assert result["type"] == FlowResultType.FORM
+    assert result["step_id"] == "init"
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"],
+        user_input={},
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    assert result["data"] == {
+        "resolver": "208.67.222.222",
+        "resolver_ipv6": "2620:119:53::53",
+    }
+
+    entry = hass.config_entries.async_get_entry(entry.entry_id)
+    assert entry.data == {
+        "hostname": "home-assistant.io",
+        "ipv4": True,
+        "ipv6": False,
+        "name": "home-assistant.io",
+    }
+    assert entry.options == {
+        "resolver": "208.67.222.222",
+        "resolver_ipv6": "2620:119:53::53",
+    }
+
+
 @pytest.mark.parametrize(
     "p_input",
     [
