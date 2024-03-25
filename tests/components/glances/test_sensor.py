ef11b4ecc4bbf6b7c066c8ccc4c8cd28fdf52294
wittypluck <l@perlat.net>
2024-03-25 23:36:52 +0100

    Use freezer.tick for uptime tests

--- tests/components/glances/test_sensor.py
+++ tests/components/glances/test_sensor.py
@@ -2,13 +2,13 @@
 
 from datetime import timedelta
 
-from freezegun.api import freeze_time
+from freezegun.api import FrozenDateTimeFactory
 from syrupy import SnapshotAssertion
 
-from homeassistant.components.glances import GlancesDataUpdateCoordinator
 from homeassistant.components.glances.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
+from homeassistant.util.dt import parse_datetime
 
 from . import MOCK_REFERENCE_DATE, MOCK_USER_INPUT
 
@@ -16,16 +16,20 @@ from tests.common import MockConfigEntry
 
 
 async def test_sensor_states(
-    hass: HomeAssistant, snapshot: SnapshotAssertion, entity_registry: er.EntityRegistry
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test sensor states are correctly collected from library."""
+
+    freezer.move_to(MOCK_REFERENCE_DATE)
 
     entry = MockConfigEntry(domain=DOMAIN, data=MOCK_USER_INPUT, entry_id="test")
     entry.add_to_hass(hass)
 
-    with freeze_time(MOCK_REFERENCE_DATE):
-        assert await hass.config_entries.async_setup(entry.entry_id)
-        await hass.async_block_till_done()
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
 
     entity_entries = er.async_entries_for_config_entry(entity_registry, entry.entry_id)
 
@@ -38,25 +42,29 @@ async def test_sensor_states(
 
 
 async def test_uptime_variation(
-    hass: HomeAssistant, entity_registry: er.EntityRegistry
+    hass: HomeAssistant, freezer: FrozenDateTimeFactory
 ) -> None:
     """Test uptime small variation update."""
 
+    # Init with reference time
+    freezer.move_to(MOCK_REFERENCE_DATE)
     entry = MockConfigEntry(domain=DOMAIN, data=MOCK_USER_INPUT, entry_id="test")
     entry.add_to_hass(hass)
-
-    # Setup with expected timestamp for reference
-    with freeze_time(MOCK_REFERENCE_DATE):
-        assert await hass.config_entries.async_setup(entry.entry_id)
-        await hass.async_block_till_done()
-
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
     uptime_state = hass.states.get("sensor.0_0_0_0_uptime").state
 
-    coordinator: GlancesDataUpdateCoordinator = hass.data[DOMAIN]["test"]
+    # Small time change should not change uptime
+    freezer.tick(delta=timedelta(seconds=60))
+    await hass.async_block_till_done()
+    uptime_state2 = hass.states.get("sensor.0_0_0_0_uptime").state
+    assert uptime_state2 == uptime_state
 
-    # Expected timestamp + 60s => uptime should not change
-    with freeze_time(MOCK_REFERENCE_DATE + timedelta(seconds=60)):
-        await coordinator._async_update_data()
-        await hass.async_block_till_done()
-        uptime_state2 = hass.states.get("sensor.0_0_0_0_uptime").state
-        assert uptime_state == uptime_state2
+    # Large time change should change uptime
+    freezer.tick(delta=timedelta(minutes=60))
+    await hass.async_block_till_done()
+    uptime_state3 = hass.states.get("sensor.0_0_0_0_uptime").state
+    assert uptime_state3 != uptime_state
+    assert parse_datetime(uptime_state3) == parse_datetime(uptime_state) + timedelta(
+        minutes=60, seconds=60
+    )
