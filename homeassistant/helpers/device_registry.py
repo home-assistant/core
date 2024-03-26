43279920948429bafb6f2e00e4a5e6c122bd9c28
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-03-26 13:28:37 +0100

    Update existing code

--- homeassistant/helpers/device_registry.py
+++ homeassistant/helpers/device_registry.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from collections.abc import Mapping
 from enum import StrEnum
-from functools import lru_cache, partial
+from functools import cached_property, lru_cache, partial
 import logging
 import time
 from typing import TYPE_CHECKING, Any, Literal, TypedDict, TypeVar, cast
@@ -12,7 +12,6 @@ from typing import TYPE_CHECKING, Any, Literal, TypedDict, TypeVar, cast
 import attr
 from yarl import URL
 
-from homeassistant.backports.functools import cached_property
 from homeassistant.const import EVENT_HOMEASSISTANT_STARTED, EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import Event, HomeAssistant, callback, get_release_channel
 from homeassistant.exceptions import HomeAssistantError
