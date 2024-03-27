f1418dcc4f12fcd1aa44cc1e268779c3ee312978
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-03-27 14:44:32 +0100

    Use `setup_test_component_platform` helper for switch entity component tests instead of `hass.components`

---
+++ tests/components/generic_hygrostat/conftest.py
@@ -0,0 +1,3 @@
+"""Fixtures for generic_hygrostat tests."""
+
+from tests.components.switch.conftest import mock_switch_entities  # noqa: F401
--- tests/components/switch/conftest.py
+++ tests/components/switch/conftest.py
@@ -1,3 +1,18 @@
 """switch conftest."""
 
+import pytest
+
+from homeassistant.const import STATE_OFF, STATE_ON
+
+from tests.common import MockToggleEntity
 from tests.components.light.conftest import mock_light_profiles  # noqa: F401
+
+
+@pytest.fixture
+def mock_switch_entities() -> list[MockToggleEntity]:
+    """Return a list of mock switch entities."""
+    return [
+        MockToggleEntity("AC", STATE_ON),
+        MockToggleEntity("AC", STATE_OFF),
+        MockToggleEntity(None, STATE_OFF),
+    ]
