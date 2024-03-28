fc4d960d17ff27dbfc58ef00f317c5aa53cce2ff
G Johansson <goran.johansson@shiftit.se>
2024-03-28 10:52:21 +0100

    Add translation support to Config Entry errors (#106305)
    * Config Entry error translation
    
    * split key and placeholders
    
    * Fix config entries tests
    
    * translation optional
    
    * Mods

--- tests/test_config_entries.py
+++ tests/test_config_entries.py
@@ -821,6 +821,8 @@ async def test_as_dict(snapshot: SnapshotAssertion) -> None:
         "_setup_lock",
         "update_listeners",
         "reason",
+        "error_reason_translation_key",
+        "error_reason_translation_placeholders",
         "_async_cancel_retry_setup",
         "_on_unload",
         "reload_lock",
