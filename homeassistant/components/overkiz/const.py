f309bb652435406ac0b5bb84c361a5810aa4ad69
Sebastien Malissard <sebastien.malissard@dbmail.com>
2024-03-31 00:31:01 +0100

    Overkiz revert ignore of Pod class device
    The Pod device can contains useful information as
    the Wifi RSSI.

--- homeassistant/components/overkiz/const.py
+++ homeassistant/components/overkiz/const.py
@@ -65,7 +65,6 @@ PLATFORMS: list[Platform] = [
 
 IGNORED_OVERKIZ_DEVICES: list[UIClass | UIWidget] = [
     UIClass.PROTOCOL_GATEWAY,
-    UIClass.POD,
 ]
 
 # Used to map the Somfy widget and ui_class to the Home Assistant platform
