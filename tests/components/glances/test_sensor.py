054322a46bfc96dbea31d577ec2ce0943510d5ae
wittypluck <l@perlat.net>
2024-03-30 15:13:06 +0100

    Add unit test for uptime change

--- tests/components/glances/test_sensor.py
+++ tests/components/glances/test_sensor.py
@@ -1,6 +1,7 @@
 """Tests for glances sensors."""
 
 from datetime import timedelta
+from unittest.mock import AsyncMock
 
 from freezegun.api import FrozenDateTimeFactory
 from syrupy import SnapshotAssertion
@@ -9,7 +10,7 @@ from homeassistant.components.glances.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
-from . import MOCK_REFERENCE_DATE, MOCK_USER_INPUT
+from . import HA_SENSOR_DATA, MOCK_REFERENCE_DATE, MOCK_USER_INPUT
 
 from tests.common import MockConfigEntry, async_fire_time_changed
 
@@ -41,7 +42,7 @@ async def test_sensor_states(
 
 
 async def test_uptime_variation(
-    hass: HomeAssistant, freezer: FrozenDateTimeFactory
+    hass: HomeAssistant, freezer: FrozenDateTimeFactory, mock_api: AsyncMock
 ) -> None:
     """Test uptime small variation update."""
 
@@ -59,3 +60,14 @@ async def test_uptime_variation(
     await hass.async_block_till_done()
     uptime_state2 = hass.states.get("sensor.0_0_0_0_uptime").state
     assert uptime_state2 == uptime_state
+
+    mock_data = HA_SENSOR_DATA.copy()
+    mock_data["uptime"] = "1:25:20"
+    mock_api.return_value.get_ha_sensor_data = AsyncMock(return_value=mock_data)
+
+    # Server has been restarted so therefore we should have a new state
+    freezer.move_to(MOCK_REFERENCE_DATE + timedelta(days=2))
+    freezer.tick(delta=timedelta(seconds=120))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+    assert hass.states.get("sensor.0_0_0_0_uptime").state == "2024-02-15T12:49:52+00:00"
