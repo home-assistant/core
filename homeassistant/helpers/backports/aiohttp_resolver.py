601e682d0864c23f04af9aa62acfd4c2e0b2f861
J. Nick Koston <nick@koston.org>
2024-03-31 07:17:02 -1000

    Switch to using the AsyncResolver with aiohttp
    This avoids creating executor jobs to do DNS resolution
    
    AsyncResolver was not usable before https://github.com/aio-libs/aiohttp/pull/8270
    because it did not fallback to fallback to A records and only returned AAAA records
    in most cases when IPv6 was available
    
    This is a backport of https://github.com/aio-libs/aiohttp/pull/8270

---
+++ homeassistant/helpers/backports/aiohttp_resolver.py
@@ -0,0 +1,105 @@
+"""Backport of aiohttp's AsyncResolver for Home Assistant."""
+
+from __future__ import annotations
+
+import asyncio
+import socket
+import sys
+from typing import Any, TypedDict
+
+import aiodns
+from aiohttp.abc import AbstractResolver
+
+# This is a backport of https://github.com/aio-libs/aiohttp/pull/8270
+
+_NUMERIC_SOCKET_FLAGS = socket.AI_NUMERICHOST | socket.AI_NUMERICSERV
+_SUPPORTS_SCOPE_ID = sys.version_info >= (3, 9, 0)
+
+
+class ResolveResult(TypedDict):
+    """Resolve result.
+
+    This is the result returned from an AbstractResolver's
+    resolve method.
+
+    :param hostname: The hostname that was provided.
+    :param host: The IP address that was resolved.
+    :param port: The port that was resolved.
+    :param family: The address family that was resolved.
+    :param proto: The protocol that was resolved.
+    :param flags: The flags that were resolved.
+    """
+
+    hostname: str
+    host: str
+    port: int
+    family: int
+    proto: int
+    flags: int
+
+
+class AsyncResolver(AbstractResolver):
+    """Use the `aiodns` package to make asynchronous DNS lookups."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize the resolver."""
+        if aiodns is None:
+            raise RuntimeError("Resolver requires aiodns library")
+
+        self._loop = asyncio.get_running_loop()
+        self._resolver = aiodns.DNSResolver(*args, loop=self._loop, **kwargs)
+
+    async def resolve(  # type: ignore[override]
+        self, host: str, port: int = 0, family: int = socket.AF_INET
+    ) -> list[ResolveResult]:
+        """Resolve a host name to an IP address."""
+        try:
+            resp = await self._resolver.getaddrinfo(
+                host,
+                port=port,
+                type=socket.SOCK_STREAM,
+                family=family,
+                flags=socket.AI_ADDRCONFIG,
+            )
+        except aiodns.error.DNSError as exc:
+            msg = exc.args[1] if len(exc.args) >= 1 else "DNS lookup failed"
+            raise OSError(msg) from exc
+        hosts: list[ResolveResult] = []
+        for node in resp.nodes:
+            address: tuple[bytes, int] | tuple[bytes, int, int, int] = node.addr
+            family = node.family
+            if family == socket.AF_INET6:
+                if len(address) > 3 and address[3] and _SUPPORTS_SCOPE_ID:
+                    # This is essential for link-local IPv6 addresses.
+                    # LL IPv6 is a VERY rare case. Strictly speaking, we should use
+                    # getnameinfo() unconditionally, but performance makes sense.
+                    result = await self._resolver.getnameinfo(
+                        address[0].decode("ascii"), *address[1:], _NUMERIC_SOCKET_FLAGS
+                    )
+                    resolved_host = result.node
+                else:
+                    resolved_host = address[0].decode("ascii")
+                    port = address[1]
+            else:  # IPv4
+                assert family == socket.AF_INET
+                resolved_host = address[0].decode("ascii")
+                port = address[1]
+            hosts.append(
+                ResolveResult(
+                    hostname=host,
+                    host=resolved_host,
+                    port=port,
+                    family=family,
+                    proto=0,
+                    flags=_NUMERIC_SOCKET_FLAGS,
+                )
+            )
+
+        if not hosts:
+            raise OSError("DNS lookup failed")
+
+        return hosts
+
+    async def close(self) -> None:
+        """Close the resolver."""
+        self._resolver.cancel()
