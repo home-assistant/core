c3c1d81ebae6dbb8d68ab474581372a2b698f3dd
J. Nick Koston <nick@koston.org>
2024-03-30 14:45:36 -1000

    more fixes

--- tests/util/test_loop.py
+++ tests/util/test_loop.py
@@ -197,17 +197,23 @@ def test_protect_loop_sync() -> None:
 async def test_protect_loop_debugger_sleep(caplog: pytest.LogCaptureFixture) -> None:
     """Test time.sleep injected by the debugger is not reported."""
     block_async_io.enable()
-
-    with patch(
-        "homeassistant.helpers.frame.get_current_frame",
-        return_value=extract_stack_to_frame(
-            [
-                Mock(
-                    filename="/home/paulus/homeassistant/.venv/blah/pydevd.py",
-                    lineno="23",
-                    line="do_something()",
-                ),
-            ]
+    frames = extract_stack_to_frame(
+        [
+            Mock(
+                filename="/home/paulus/homeassistant/.venv/blah/pydevd.py",
+                lineno="23",
+                line="do_something()",
+            ),
+        ]
+    )
+    with (
+        patch(
+            "homeassistant.block_async_io.get_current_frame",
+            return_value=frames,
+        ),
+        patch(
+            "homeassistant.helpers.frame.get_current_frame",
+            return_value=frames,
         ),
     ):
         time.sleep(0)
