93dc1f7268a1e3f43d3427755fb74e196301d4ee
Aidan Timson <contact@timmo.xyz>
2024-04-01 14:12:17 +0000

    Fix

--- tests/components/azure_devops/test_sensor.py
+++ tests/components/azure_devops/test_sensor.py
@@ -1,19 +1,11 @@
 """Tests for init of Azure DevOps."""
 
-from typing import Final
-
 import pytest
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
-from . import DEVOPS_BUILD_DEFINITION, DEVOPS_PROJECT
-
 from tests.common import MockConfigEntry
-
-BASE_SENSOR_NAME: Final[str] = (
-    f"sensor.{DEVOPS_PROJECT.project_id}_{DEVOPS_BUILD_DEFINITION.build_id}"
-)
 
 
 @pytest.mark.usefixtures(
@@ -26,11 +18,11 @@ async def test_sensors(
     entity_registry: er.EntityRegistry,
 ) -> None:
     """Test the sensor entities."""
-    assert (state := hass.states.get(f"{BASE_SENSOR_NAME}_latest_build"))
+    assert (state := hass.states.get("sensor.testproject_test_build_latest_build"))
     assert state.state == "1"
-    assert state.attributes["definition_id"] == 1
+    assert state.attributes["definition_id"] == 9876
     assert state.attributes["definition_name"] == "Test Build"
-    assert state.attributes["id"] == 1
+    assert state.attributes["id"] == 5678
     assert state.attributes["reason"] == "manual"
     assert state.attributes["result"] == "succeeded"
     assert state.attributes["source_branch"] == "main"
