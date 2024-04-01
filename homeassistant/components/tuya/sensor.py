64de21a1b3341775bc5788249a73fc507c7e870c
focabr <focabr@gmail.com>
2024-04-01 22:10:33 +0000

    Changed the name as recommended, it really looks better within Home Assistant.

--- homeassistant/components/tuya/sensor.py
+++ homeassistant/components/tuya/sensor.py
@@ -780,7 +780,7 @@ SENSORS: dict[str, tuple[TuyaSensorEntityDescription, ...]] = {
         ),
         TuyaSensorEntityDescription(
             key=DPCode.CUR_NEUTRAL,
-            translation_key="total_reverse_energy",
+            translation_key="total_return_energy",
             device_class=SensorDeviceClass.ENERGY,
             state_class=SensorStateClass.TOTAL_INCREASING,
         ),
