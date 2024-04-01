de561ae3db1339b6e0ad6899c49c2029d8e19ac1
Aidan Timson <contact@timmo.xyz>
2024-04-01 18:02:45 +0000

    Add new services and test cases for system bridge component

--- tests/components/system_bridge/test_init.py
+++ tests/components/system_bridge/test_init.py
@@ -3,19 +3,44 @@
 import asyncio
 from unittest.mock import AsyncMock, MagicMock
 
+import pytest
 from systembridgeconnector.exceptions import (
     AuthenticationException,
     ConnectionClosedException,
     ConnectionErrorException,
 )
+from systembridgemodels.fixtures.modules.processes import FIXTURE_PROCESSES
+import voluptuous as vol
 
-from homeassistant.components.system_bridge import SERVICE_OPEN_URL
+from homeassistant.components.system_bridge import (
+    CONF_BRIDGE,
+    CONF_KEY,
+    CONF_TEXT,
+    SERVICE_GET_PROCESS_BY_ID,
+    SERVICE_GET_PROCESSES_BY_NAME,
+    SERVICE_OPEN_PATH,
+    SERVICE_OPEN_URL,
+    SERVICE_POWER_COMMAND,
+    SERVICE_SEND_KEYPRESS,
+    SERVICE_SEND_TEXT,
+)
 from homeassistant.components.system_bridge.config_flow import SystemBridgeConfigFlow
 from homeassistant.components.system_bridge.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import CONF_API_KEY, CONF_HOST, CONF_PORT, CONF_TOKEN
+from homeassistant.const import (
+    CONF_API_KEY,
+    CONF_COMMAND,
+    CONF_HOST,
+    CONF_ID,
+    CONF_NAME,
+    CONF_PATH,
+    CONF_PORT,
+    CONF_TOKEN,
+    CONF_URL,
+)
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import issue_registry as ir
+from homeassistant.exceptions import ServiceValidationError
+from homeassistant.helpers import device_registry as dr, issue_registry as ir
 
 from . import FIXTURE_USER_INPUT, FIXTURE_UUID, setup_integration
 
@@ -204,7 +229,13 @@ async def test_already_has_services(
 
     assert entry.state == ConfigEntryState.LOADED
 
+    assert hass.services.has_service(DOMAIN, SERVICE_GET_PROCESS_BY_ID)
+    assert hass.services.has_service(DOMAIN, SERVICE_GET_PROCESSES_BY_NAME)
+    assert hass.services.has_service(DOMAIN, SERVICE_OPEN_PATH)
+    assert hass.services.has_service(DOMAIN, SERVICE_POWER_COMMAND)
     assert hass.services.has_service(DOMAIN, SERVICE_OPEN_URL)
+    assert hass.services.has_service(DOMAIN, SERVICE_SEND_KEYPRESS)
+    assert hass.services.has_service(DOMAIN, SERVICE_SEND_TEXT)
 
     new_config_entry = MockConfigEntry(
         title="Title 2",
@@ -223,7 +254,149 @@ async def test_already_has_services(
 
     assert entry_2.state == ConfigEntryState.LOADED
 
+    assert hass.services.has_service(DOMAIN, SERVICE_GET_PROCESS_BY_ID)
+    assert hass.services.has_service(DOMAIN, SERVICE_GET_PROCESSES_BY_NAME)
+    assert hass.services.has_service(DOMAIN, SERVICE_OPEN_PATH)
+    assert hass.services.has_service(DOMAIN, SERVICE_POWER_COMMAND)
     assert hass.services.has_service(DOMAIN, SERVICE_OPEN_URL)
+    assert hass.services.has_service(DOMAIN, SERVICE_SEND_KEYPRESS)
+    assert hass.services.has_service(DOMAIN, SERVICE_SEND_TEXT)
+
+
+async def test_services(
+    hass: HomeAssistant,
+    mock_version: MagicMock,
+    mock_websocket_client: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test if services are registered."""
+    await setup_integration(hass, mock_config_entry)
+    entry = hass.config_entries.async_entries(DOMAIN)[0]
+
+    assert entry.state == ConfigEntryState.LOADED
+
+    assert hass.services.has_service(DOMAIN, SERVICE_GET_PROCESS_BY_ID)
+    assert hass.services.has_service(DOMAIN, SERVICE_GET_PROCESSES_BY_NAME)
+    assert hass.services.has_service(DOMAIN, SERVICE_OPEN_PATH)
+    assert hass.services.has_service(DOMAIN, SERVICE_POWER_COMMAND)
+    assert hass.services.has_service(DOMAIN, SERVICE_OPEN_URL)
+    assert hass.services.has_service(DOMAIN, SERVICE_SEND_KEYPRESS)
+    assert hass.services.has_service(DOMAIN, SERVICE_SEND_TEXT)
+
+    # Test invalid device
+    with pytest.raises(vol.Invalid):
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_GET_PROCESS_BY_ID,
+            {
+                CONF_BRIDGE: "invalidid",
+                CONF_ID: FIXTURE_PROCESSES[0].id,
+            },
+            blocking=True,
+            return_response=True,
+        )
+
+    # Get device from device registry
+    device_registry = dr.async_get(hass)
+    device = device_registry.async_get_device(identifiers={(DOMAIN, FIXTURE_UUID)})
+    assert device is not None
+
+    # Test process by id with valid id
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_GET_PROCESS_BY_ID,
+        {
+            CONF_BRIDGE: device.id,
+            CONF_ID: FIXTURE_PROCESSES[0].id,
+        },
+        blocking=True,
+        return_response=True,
+    )
+
+    # Test process by id with invalid id
+    with pytest.raises(ServiceValidationError):
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_GET_PROCESS_BY_ID,
+            {
+                CONF_BRIDGE: device.id,
+                CONF_ID: 999999999,
+            },
+            blocking=True,
+            return_response=True,
+        )
+
+    # Test processes by name
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_GET_PROCESSES_BY_NAME,
+        {
+            CONF_BRIDGE: device.id,
+            CONF_NAME: FIXTURE_PROCESSES[0].name,
+        },
+        blocking=True,
+        return_response=True,
+    )
+
+    # Test open path
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_OPEN_PATH,
+        {
+            CONF_BRIDGE: device.id,
+            CONF_PATH: "/test/path",
+        },
+        blocking=True,
+        return_response=True,
+    )
+
+    # Test power command
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_POWER_COMMAND,
+        {
+            CONF_BRIDGE: device.id,
+            CONF_COMMAND: "shutdown",
+        },
+        blocking=True,
+        return_response=True,
+    )
+
+    # Test open url
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_OPEN_URL,
+        {
+            CONF_BRIDGE: device.id,
+            CONF_URL: "https://www.example.com",
+        },
+        blocking=True,
+        return_response=True,
+    )
+
+    # Test send keypress
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_SEND_KEYPRESS,
+        {
+            CONF_BRIDGE: device.id,
+            CONF_KEY: "key",
+        },
+        blocking=True,
+        return_response=True,
+    )
+
+    # Test send text
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_SEND_TEXT,
+        {
+            CONF_BRIDGE: device.id,
+            CONF_TEXT: "text",
+        },
+        blocking=True,
+        return_response=True,
+    )
 
 
 async def test_migration_minor_1_to_2(
