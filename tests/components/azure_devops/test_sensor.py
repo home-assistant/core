6bf7cca5354d4c9feb6d85cce2cc1b27f6a84389
Aidan Timson <contact@timmo.xyz>
2024-04-02 14:27:00 +0000

    Use snapshot

--- tests/components/azure_devops/test_sensor.py
+++ tests/components/azure_devops/test_sensor.py
@@ -3,8 +3,8 @@
 from unittest.mock import AsyncMock
 
 import pytest
+from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.components.azure_devops.const import DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
@@ -16,25 +16,18 @@ from tests.common import MockConfigEntry
 @pytest.mark.usefixtures("entity_registry_enabled_by_default")
 async def test_sensors(
     hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
     entity_registry: er.EntityRegistry,
     mock_config_entry: MockConfigEntry,
     mock_devops_client: AsyncMock,
 ) -> None:
     """Test the sensor entities."""
     assert await setup_integration(hass, mock_config_entry)
-    entry = hass.config_entries.async_entries(DOMAIN)[0]
 
-    assert (state := hass.states.get("sensor.testproject_test_build_latest_build"))
-    assert state.state == "1"
-    assert state.attributes["definition_id"] == 9876
-    assert state.attributes["definition_name"] == "Test Build"
-    assert state.attributes["id"] == 5678
-    assert state.attributes["reason"] == "manual"
-    assert state.attributes["result"] == "succeeded"
-    assert state.attributes["source_branch"] == "main"
-    assert state.attributes["source_version"] == "123"
-    assert state.attributes["status"] == "completed"
+    assert (
+        entry := entity_registry.async_get("sensor.testproject_test_build_latest_build")
+    )
 
-    entry = entity_registry.async_get("sensor.testproject_test_build_latest_build")
-    assert entry
-    assert entry.unique_id == "testorg_1234_9876_latest_build"
+    assert entry == snapshot(name=f"{entry.entity_id}-entry")
+
+    assert hass.states.get(entry.entity_id) == snapshot(name=f"{entry.entity_id}-state")
