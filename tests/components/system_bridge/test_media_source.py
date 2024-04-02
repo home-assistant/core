02c00f989921086c26823b04758b09e1f511d1c1
Aidan Timson <contact@timmo.xyz>
2024-04-02 14:55:15 +0000

    Snapshot

--- tests/components/system_bridge/test_media_source.py
+++ tests/components/system_bridge/test_media_source.py
@@ -2,6 +2,8 @@
 
 from unittest.mock import MagicMock
 
+from syrupy.assertion import SnapshotAssertion
+
 from homeassistant.components.media_source import URI_SCHEME, async_browse_media
 from homeassistant.components.system_bridge.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
@@ -15,6 +17,8 @@ from tests.common import MockConfigEntry
 
 async def test_async_browse_media(
     hass: HomeAssistant,
+    device_registry: dr.DeviceRegistry,
+    snapshot: SnapshotAssertion,
     mock_config_entry: MockConfigEntry,
     mock_setup_entry: MagicMock,
 ) -> None:
@@ -25,44 +29,22 @@ async def test_async_browse_media(
     assert entry.state == ConfigEntryState.LOADED
 
     # Get device from device registry
-    device_registry = dr.async_get(hass)
     device = device_registry.async_get_or_create(
         config_entry_id=entry.entry_id,
         identifiers={(DOMAIN, FIXTURE_UUID)},
     )
     assert device is not None
 
-    # # Get media source
-    # media_source = await async_get_media_source(hass)
-
-    # # Test browse media
-    # browse_media = await media_source.async_browse_media(None)
-
-    media = await async_browse_media(
+    browse_media_root = await async_browse_media(
         hass,
         f"{URI_SCHEME}{DOMAIN}",
     )
 
-    assert media.as_dict() == {
-        "title": "System Bridge",
-        "media_class": "directory",
-        "media_content_type": "",
-        "media_content_id": "media-source://system_bridge",
-        "children_media_class": "directory",
-        "can_play": False,
-        "can_expand": True,
-        "thumbnail": None,
-        "not_shown": 0,
-        "children": [
-            {
-                "title": "TestSystem",
-                "media_class": "directory",
-                "media_content_type": "",
-                "media_content_id": f"media-source://system_bridge/{device.id}",
-                "children_media_class": "directory",
-                "can_play": False,
-                "can_expand": True,
-                "thumbnail": None,
-            }
-        ],
-    }
+    assert browse_media_root.as_dict() == snapshot(name=f"{DOMAIN}_browse_media_root")
+
+    # browse_media_entry = await async_browse_media(
+    #     hass,
+    #     f"{URI_SCHEME}{DOMAIN}~~{entry.entry_id}",
+    # )
+
+    # assert browse_media_entry.as_dict() == snapshot(name=f"{DOMAIN}_browse_media_entry")
