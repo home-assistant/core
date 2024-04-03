8a4e96f0ddb5dda50ff310d77d4b3f1a248fa0ea
Brent Harris <colinbrentharris@gmail.com>
2024-04-03 21:22:16 +0000

    Add json_attributes_path config option to command_line sensor

--- homeassistant/components/command_line/const.py
+++ homeassistant/components/command_line/const.py
@@ -7,6 +7,8 @@ from homeassistant.const import Platform
 LOGGER = logging.getLogger(__package__)
 
 CONF_COMMAND_TIMEOUT = "command_timeout"
+CONF_JSON_ATTRIBUTES = "json_attributes"
+CONF_JSON_ATTRIBUTES_PATH = "json_attributes_path"
 DEFAULT_TIMEOUT = 15
 DOMAIN = "command_line"
 PLATFORMS = [
