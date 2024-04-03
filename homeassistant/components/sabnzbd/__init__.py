d9d0f0d8be5c20dbdfee84f201a9ef84368eec8d
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-04-03 14:54:51 +0200

    Setup coordinator after migration

--- homeassistant/components/sabnzbd/__init__.py
+++ homeassistant/components/sabnzbd/__init__.py
@@ -172,12 +172,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if not sab_api:
         raise ConfigEntryNotReady
 
+    await migrate_unique_id(hass, entry)
+    update_device_identifiers(hass, entry)
+
     coordinator = SabnzbdUpdateCoordinator(hass, sab_api)
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
-    await migrate_unique_id(hass, entry)
-    update_device_identifiers(hass, entry)
 
     @callback
     def extract_api(
