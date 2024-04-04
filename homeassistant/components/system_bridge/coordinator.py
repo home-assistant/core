d0ec62bcf979137d32a9ee18288169a12c939091
Aidan Timson <aidan@timmo.dev>
2024-04-04 09:33:46 +0000

    Refactor

--- homeassistant/components/system_bridge/coordinator.py
+++ homeassistant/components/system_bridge/coordinator.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import asyncio
 from collections.abc import Callable
 from datetime import timedelta
 import logging
@@ -18,8 +17,12 @@ from systembridgemodels.media_directories import MediaDirectory
 from systembridgemodels.media_files import MediaFile, MediaFiles
 from systembridgemodels.media_get_file import MediaGetFile
 from systembridgemodels.media_get_files import MediaGetFiles
-from systembridgemodels.modules import GetData, RegisterDataListener
-from systembridgemodels.response import Response
+from systembridgemodels.modules import (
+    GetData,
+    Module,
+    ModulesData,
+    RegisterDataListener,
+)
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
@@ -51,7 +54,6 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
         self.title = entry.title
         self.unsub: Callable | None = None
 
-        self.systembridge_data = SystemBridgeData()
         self.websocket_client = WebSocketClient(
             entry.data[CONF_HOST],
             entry.data[CONF_PORT],
@@ -68,6 +70,8 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
             update_interval=timedelta(seconds=30),
         )
 
+        self.data = SystemBridgeData()
+
     @property
     def is_ready(self) -> bool:
         """Return if the data is ready."""
@@ -81,13 +85,21 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
 
     async def async_get_data(
         self,
-        modules: list[str],
-    ) -> Response:
+        modules: list[Module],
+    ) -> ModulesData:
         """Get data from WebSocket."""
         if not self.websocket_client.connected:
-            await self._setup_websocket()
+            await self.websocket_client.connect()
 
-        return await self.websocket_client.get_data(GetData(modules=modules))
+        modules_data = await self.websocket_client.get_data(GetData(modules=modules))
+
+        # Merge new data with existing data
+        for module in MODULES:
+            if hasattr(modules_data, module):
+                self.logger.debug("[async_get_data] Set new data for: %s", module)
+                setattr(self.data, module, getattr(modules_data, module))
+
+        return modules_data
 
     async def async_get_media_directories(self) -> list[MediaDirectory]:
         """Get media directories."""
@@ -125,9 +137,9 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
         module: Any,
     ) -> None:
         """Handle data from the WebSocket client."""
-        self.logger.debug("Set new data for: %s", module_name)
-        setattr(self.systembridge_data, module_name, module)
-        self.async_set_updated_data(self.systembridge_data)
+        self.logger.debug("[async_handle_module] Set new data for: %s", module_name)
+        setattr(self.data, module_name, module)
+        self.async_set_updated_data(self.data)
 
     async def _listen_for_data(self) -> None:
         """Listen for events from the WebSocket."""
@@ -147,7 +159,7 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
             self.async_update_listeners()
         except (ConnectionClosedException, ConnectionResetError) as exception:
             self.logger.debug(
-                "Websocket connection closed for %s. Will retry: %s",
+                "[_listen_for_data] Websocket connection closed for %s. Will retry: %s",
                 self.title,
                 exception,
             )
@@ -158,7 +170,7 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
             self.async_update_listeners()
         except ConnectionErrorException as exception:
             self.logger.debug(
-                "Connection error occurred for %s. Will retry: %s",
+                "[_listen_for_data] Connection error occurred for %s. Will retry: %s",
                 self.title,
                 exception,
             )
@@ -171,9 +183,6 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
     async def _setup_websocket(self) -> None:
         """Use WebSocket for updates."""
         try:
-            async with asyncio.timeout(20):
-                await self.websocket_client.connect()
-
             self.hass.async_create_background_task(
                 self._listen_for_data(),
                 name="System Bridge WebSocket Listener",
@@ -182,6 +191,7 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
             await self.websocket_client.register_data_listener(
                 RegisterDataListener(modules=MODULES)
             )
+
             self.last_update_success = True
             self.async_update_listeners()
         except AuthenticationException as exception:
@@ -220,7 +230,7 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
 
         async def close_websocket(_) -> None:
             """Close WebSocket connection."""
-            await self.websocket_client.close()
+            await self.websocket_client.close(True)
 
         # Clean disconnect WebSocket on Home Assistant shutdown
         self.unsub = self.hass.bus.async_listen_once(
@@ -230,12 +240,12 @@ class SystemBridgeDataUpdateCoordinator(DataUpdateCoordinator[SystemBridgeData])
     async def _async_update_data(self) -> SystemBridgeData:
         """Update System Bridge data from WebSocket."""
         self.logger.debug(
-            "_async_update_data - WebSocket Connected: %s",
+            "[_async_update_data] WebSocket Connected: %s",
             self.websocket_client.connected,
         )
         if not self.websocket_client.connected:
             await self._setup_websocket()
 
-        self.logger.debug("_async_update_data done")
+        self.logger.debug("[_async_update_data] Done")
 
-        return self.systembridge_data
+        return self.data
