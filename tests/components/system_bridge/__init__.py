2ef415a2da92f466d171a00e7888c9a7734d679f
Aidan Timson <aidan@timmo.dev>
2024-04-04 23:04:44 +0000

    Add missing tests for media source

--- tests/components/system_bridge/__init__.py
+++ tests/components/system_bridge/__init__.py
@@ -4,34 +4,49 @@ from collections.abc import Awaitable, Callable
 from ipaddress import ip_address
 from typing import Any
 
-from systembridgeconnector.const import EventType
+from systembridgemodels.fixtures.modules.battery import FIXTURE_BATTERY
+from systembridgemodels.fixtures.modules.cpu import FIXTURE_CPU
+from systembridgemodels.fixtures.modules.disks import FIXTURE_DISKS
+from systembridgemodels.fixtures.modules.displays import FIXTURE_DISPLAYS
+from systembridgemodels.fixtures.modules.gpus import FIXTURE_GPUS
+from systembridgemodels.fixtures.modules.media import FIXTURE_MEDIA
+from systembridgemodels.fixtures.modules.memory import FIXTURE_MEMORY
+from systembridgemodels.fixtures.modules.processes import FIXTURE_PROCESSES
 from systembridgemodels.fixtures.modules.system import FIXTURE_SYSTEM
 from systembridgemodels.modules import Module, ModulesData
-from systembridgemodels.response import Response
 
 from homeassistant.components import zeroconf
 from homeassistant.const import CONF_HOST, CONF_PORT, CONF_TOKEN
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+
+FIXTURE_TITLE = "TestSystem"
+
+FIXTURE_REQUEST_ID = "test"
 
 FIXTURE_MAC_ADDRESS = FIXTURE_SYSTEM.mac_address
 FIXTURE_UUID = FIXTURE_SYSTEM.uuid
 
-FIXTURE_AUTH_INPUT = {CONF_TOKEN: "abc-123-def-456-ghi"}
+FIXTURE_AUTH_INPUT = {
+    CONF_TOKEN: "abc-123-def-456-ghi",
+}
 
 FIXTURE_USER_INPUT = {
     CONF_TOKEN: "abc-123-def-456-ghi",
-    CONF_HOST: "test-bridge",
+    CONF_HOST: "127.0.0.1",
     CONF_PORT: "9170",
 }
 
 FIXTURE_ZEROCONF_INPUT = {
     CONF_TOKEN: "abc-123-def-456-ghi",
-    CONF_HOST: "1.1.1.1",
+    CONF_HOST: FIXTURE_USER_INPUT[CONF_HOST],
     CONF_PORT: "9170",
 }
 
 FIXTURE_ZEROCONF = zeroconf.ZeroconfServiceInfo(
-    ip_address=ip_address("1.1.1.1"),
-    ip_addresses=[ip_address("1.1.1.1")],
+    ip_address=ip_address(FIXTURE_USER_INPUT[CONF_HOST]),
+    ip_addresses=[ip_address(FIXTURE_USER_INPUT[CONF_HOST])],
     port=9170,
     hostname="test-bridge.local.",
     type="_system-bridge._tcp.local.",
@@ -40,7 +55,7 @@ FIXTURE_ZEROCONF = zeroconf.ZeroconfServiceInfo(
         "address": "http://test-bridge:9170",
         "fqdn": "test-bridge",
         "host": "test-bridge",
-        "ip": "1.1.1.1",
+        "ip": FIXTURE_USER_INPUT[CONF_HOST],
         "mac": FIXTURE_MAC_ADDRESS,
         "port": "9170",
         "uuid": FIXTURE_UUID,
@@ -48,8 +63,8 @@ FIXTURE_ZEROCONF = zeroconf.ZeroconfServiceInfo(
 )
 
 FIXTURE_ZEROCONF_BAD = zeroconf.ZeroconfServiceInfo(
-    ip_address=ip_address("1.1.1.1"),
-    ip_addresses=[ip_address("1.1.1.1")],
+    ip_address=ip_address(FIXTURE_USER_INPUT[CONF_HOST]),
+    ip_addresses=[ip_address(FIXTURE_USER_INPUT[CONF_HOST])],
     port=9170,
     hostname="test-bridge.local.",
     type="_system-bridge._tcp.local.",
@@ -63,31 +78,33 @@ FIXTURE_DATA_RESPONSE = ModulesData(
     system=FIXTURE_SYSTEM,
 )
 
-FIXTURE_DATA_RESPONSE_BAD = Response(
-    id="1234",
-    type=EventType.DATA_UPDATE,
-    subtype=None,
-    message="Data received",
-    module=Module.SYSTEM,
-    data={},
-)
 
-FIXTURE_DATA_RESPONSE_BAD = Response(
-    id="1234",
-    type=EventType.DATA_UPDATE,
-    subtype=None,
-    message="Data received",
-    module=Module.SYSTEM,
-    data={},
-)
+async def setup_integration(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+) -> bool:
+    """Fixture for setting up the component."""
+    config_entry.add_to_hass(hass)
+
+    setup_result = await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    return setup_result
 
 
 async def mock_data_listener(
-    self,
     callback: Callable[[str, Any], Awaitable[None]] | None = None,
     _: bool = False,
 ):
     """Mock websocket data listener."""
     if callback is not None:
         # Simulate data received from the websocket
+        await callback(Module.BATTERY, FIXTURE_BATTERY)
+        await callback(Module.CPU, FIXTURE_CPU)
+        await callback(Module.DISKS, FIXTURE_DISKS)
+        await callback(Module.DISPLAYS, FIXTURE_DISPLAYS)
+        await callback(Module.GPUS, FIXTURE_GPUS)
+        await callback(Module.MEDIA, FIXTURE_MEDIA)
+        await callback(Module.MEMORY, FIXTURE_MEMORY)
+        await callback(Module.PROCESSES, FIXTURE_PROCESSES)
         await callback(Module.SYSTEM, FIXTURE_SYSTEM)
