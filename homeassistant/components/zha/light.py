6dba7fe7936f0092569079841e069713bdea72a5
David Mulcahey <david.mulcahey@icloud.com>
2024-04-04 09:03:19 -0400

    don't send HA bad color modes

--- homeassistant/components/zha/light.py
+++ homeassistant/components/zha/light.py
@@ -51,7 +51,11 @@ class Light(light.LightEntity, ZHAEntity):
         super().__init__(entity_data)
         self._attr_supported_color_modes: set[ColorMode] = set()
         for color_mode in self.entity_data.entity.supported_color_modes:
-            self._attr_supported_color_modes.add(ColorMode(color_mode))
+            if (
+                color_mode is not ColorMode.BRIGHTNESS
+                and color_mode is not ColorMode.ONOFF
+            ):
+                self._attr_supported_color_modes.add(ColorMode(color_mode))
         self._attr_supported_features = LightEntityFeature(
             self.entity_data.entity.supported_features
         )
