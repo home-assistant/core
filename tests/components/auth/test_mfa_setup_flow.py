664b07b8f34db0b0f36406c6c32ffb126a49369e
epenet <6771947+epenet@users.noreply.github.com>
2024-04-05 06:47:09 +0000

    Adjust auth

--- tests/components/auth/test_mfa_setup_flow.py
+++ tests/components/auth/test_mfa_setup_flow.py
@@ -75,7 +75,7 @@ async def test_ws_setup_depose_mfa(
     assert result["success"]
 
     flow = result["result"]
-    assert flow["type"] is FlowResultType.FORM
+    assert flow["type"] == FlowResultType.FORM
     assert flow["handler"] == "example_module"
     assert flow["step_id"] == "init"
     assert flow["data_schema"][0] == {"type": "string", "name": "pin", "required": True}
@@ -94,7 +94,7 @@ async def test_ws_setup_depose_mfa(
     assert result["success"]
 
     flow = result["result"]
-    assert flow["type"] is FlowResultType.CREATE_ENTRY
+    assert flow["type"] == FlowResultType.CREATE_ENTRY
     assert flow["handler"] == "example_module"
     assert flow["data"]["result"] is None
 
