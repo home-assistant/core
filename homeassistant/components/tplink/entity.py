40a1e85f003b2b3310cd48b8f65c6807e25426bb
sdb9696 <steven.beth@gmail.com>
2024-04-05 08:04:58 +0100

    Wrap tplink exceptions caused by user actions inside HomeAssistantError

--- homeassistant/components/tplink/entity.py
+++ homeassistant/components/tplink/entity.py
@@ -5,8 +5,14 @@ from __future__ import annotations
 from collections.abc import Awaitable, Callable, Coroutine
 from typing import Any, Concatenate, ParamSpec, TypeVar
 
-from kasa import SmartDevice
+from kasa import (
+    AuthenticationException,
+    SmartDevice,
+    SmartDeviceException,
+    TimeoutException,
+)
 
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
@@ -18,13 +24,26 @@ _T = TypeVar("_T", bound="CoordinatedTPLinkEntity")
 _P = ParamSpec("_P")
 
 
-def async_refresh_after(
+def async_exception_wrap_refresh_after(
     func: Callable[Concatenate[_T, _P], Awaitable[None]],
 ) -> Callable[Concatenate[_T, _P], Coroutine[Any, Any, None]]:
-    """Define a wrapper to refresh after."""
+    """Define a wrapper to raise HA errors and refresh after."""
 
     async def _async_wrap(self: _T, *args: _P.args, **kwargs: _P.kwargs) -> None:
-        await func(self, *args, **kwargs)
+        try:
+            await func(self, *args, **kwargs)
+        except AuthenticationException as ex:
+            if self.coordinator.config_entry:
+                self.hass.loop.call_soon(
+                    self.coordinator.config_entry.async_start_reauth, self.hass
+                )
+            raise HomeAssistantError(ex) from ex
+        except TimeoutException as ex:
+            msg = f"Timeout communicating with the device {func}: {ex}"
+            raise HomeAssistantError(msg) from ex
+        except SmartDeviceException as ex:
+            msg = f"Unable to communicate with the device {func}: {ex}"
+            raise HomeAssistantError(msg) from ex
         await self.coordinator.async_request_refresh()
 
     return _async_wrap
