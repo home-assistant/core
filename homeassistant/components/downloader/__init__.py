86f0ff1d23c5b57035d687d5519cba160afd7fc3
Joostlek <joostlek@outlook.com>
2024-04-05 10:35:11 +0200

    Create right import issues in Downloader

--- homeassistant/components/downloader/__init__.py
+++ homeassistant/components/downloader/__init__.py
@@ -63,7 +63,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             hass,
             DOMAIN,
             f"deprecated_yaml_{DOMAIN}",
-            breaks_in_ha_version="2024.9.0",
+            breaks_in_ha_version="2024.10.0",
             is_fixable=False,
             issue_domain=DOMAIN,
             severity=IssueSeverity.WARNING,
