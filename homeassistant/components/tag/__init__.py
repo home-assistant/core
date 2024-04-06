ad037342dbd4b2665e85f002d5691d599ef6e997
G Johansson <goran.johansson@shiftit.se>
2024-04-06 19:34:12 +0000

    eager_start

--- homeassistant/components/tag/__init__.py
+++ homeassistant/components/tag/__init__.py
@@ -115,7 +115,8 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             DOMAIN,
             {},
             config,
-        )
+        ),
+        eager_start=True,
     )
 
     return True
