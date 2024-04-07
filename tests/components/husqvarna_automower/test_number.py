35437063c787546c10d1526d99ad440f8461d285
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-04-07 15:28:15 +0000

    use fixture to enable by default

--- tests/components/husqvarna_automower/test_number.py
+++ tests/components/husqvarna_automower/test_number.py
@@ -1,57 +1,42 @@
 """Tests for number platform."""
 
-from datetime import timedelta
 from unittest.mock import AsyncMock
 
 from aioautomower.exceptions import ApiException
-from freezegun.api import FrozenDateTimeFactory
 import pytest
 
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers import entity_registry as er
 
 from . import setup_integration
 
-from tests.common import MockConfigEntry, async_fire_time_changed
+from tests.common import MockConfigEntry
 
 
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
 async def test_number_states(
     hass: HomeAssistant,
     mock_automower_client: AsyncMock,
     mock_config_entry: MockConfigEntry,
-    freezer: FrozenDateTimeFactory,
-    entity_registry: er.EntityRegistry,
 ) -> None:
     """Test number state."""
     entity_id = "number.test_mower_1_cutting_height"
     await setup_integration(hass, mock_config_entry)
     state = hass.states.get(entity_id)
-    assert state is None  ## disabled by default
-    entity_registry.async_update_entity(entity_id=entity_id, disabled_by=None)
-    await hass.async_block_till_done()
-    freezer.tick(timedelta(minutes=5))
-    async_fire_time_changed(hass)
     await hass.async_block_till_done()
     state = hass.states.get(entity_id)
     assert state.state == "4"
 
 
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
 async def test_number_commands(
     hass: HomeAssistant,
     mock_automower_client: AsyncMock,
     mock_config_entry: MockConfigEntry,
-    freezer: FrozenDateTimeFactory,
-    entity_registry: er.EntityRegistry,
 ) -> None:
     """Test number commands."""
     entity_id = "number.test_mower_1_cutting_height"
     await setup_integration(hass, mock_config_entry)
-    entity_registry.async_update_entity(entity_id=entity_id, disabled_by=None)
-    await hass.async_block_till_done()
-    freezer.tick(timedelta(minutes=5))
-    async_fire_time_changed(hass)
-    await hass.async_block_till_done()
     await hass.services.async_call(
         domain="number",
         service="set_value",
