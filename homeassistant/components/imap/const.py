a90199b42b9b35c863972d7ef7cef834b31c3c79
Luca Angemi <luca.angemi@gmail.com>
2024-04-07 12:38:19 +0200

    Update const.py

--- homeassistant/components/imap/const.py
+++ homeassistant/components/imap/const.py
@@ -1,13 +1,14 @@
 """Constants for the imap integration."""
 
 from typing import Final
-
+import logging
 DOMAIN: Final = "imap"
 
 CONF_SERVER: Final = "server"
 CONF_FOLDER: Final = "folder"
 CONF_SEARCH: Final = "search"
 CONF_CHARSET: Final = "charset"
+CONF_MESSAGE_DATA = "message_data"
 CONF_MAX_MESSAGE_SIZE = "max_message_size"
 CONF_CUSTOM_EVENT_DATA_TEMPLATE: Final = "custom_event_data_template"
 CONF_SSL_CIPHER_LIST: Final = "ssl_cipher_list"
@@ -17,4 +18,8 @@ DEFAULT_PORT: Final = 993
 
 DEFAULT_MAX_MESSAGE_SIZE = 2048
 
+DEFAULT_MESSAGE_DATA = ["date", "text", "sender", "subject", "headers"]
+
 MAX_MESSAGE_SIZE_LIMIT = 30000
+
+LOGGER = logging.getLogger(__package__)
