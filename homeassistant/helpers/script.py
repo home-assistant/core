4ff14848436fb7a069ff481a189d6859c7725cc1
J. Nick Koston <nick@koston.org>
2024-04-07 03:01:08 -1000

    tweak

--- homeassistant/helpers/script.py
+++ homeassistant/helpers/script.py
@@ -914,14 +914,16 @@ class _ScriptRun:
                     break
 
                 if iteration > 1:
-                    # If the user creates an script with a tight loop, yield to the event loop
-                    # so the system stays responsive while all the cpu time is consumed.
+                    # If the user creates an script with a tight loop,
+                    # yield to the event loop so the system stays
+                    # responsive while all the cpu time is consumed.
                     await asyncio.sleep(0)
                     if iteration >= REPEAT_WARN_ITERATIONS:
                         if not warned_too_many_loops:
                             warned_too_many_loops = True
                             _LOGGER.warning(
-                                "While condition %s in script `%s` is looping more than %s times",
+                                "While condition %s in script `%s`"
+                                " is looping more than %s times",
                                 repeat[CONF_WHILE],
                                 self._script.name,
                                 REPEAT_WARN_ITERATIONS,
@@ -929,7 +931,9 @@ class _ScriptRun:
 
                         if iteration >= REPEAT_TERMINATE_ITERATIONS:
                             _LOGGER.critical(
-                                "While condition %s in script `%s` terminated because it looping more than %s times",
+                                "While condition %s in script `%s` "
+                                "terminated because it looping more "
+                                "than %s times",
                                 repeat[CONF_WHILE],
                                 self._script.name,
                                 REPEAT_TERMINATE_ITERATIONS,
@@ -958,7 +962,8 @@ class _ScriptRun:
                     if not warned_too_many_loops:
                         warned_too_many_loops = True
                         _LOGGER.warning(
-                            "Until condition %s in script `%s` is looping more than %s times",
+                            "Until condition %s in script `%s` "
+                            "is looping more than %s times",
                             repeat[CONF_UNTIL],
                             self._script.name,
                             REPEAT_WARN_ITERATIONS,
@@ -966,15 +971,18 @@ class _ScriptRun:
 
                     if iteration >= REPEAT_TERMINATE_ITERATIONS:
                         _LOGGER.critical(
-                            "Until condition %s in script `%s` terminated because it looping more than %s times",
+                            "Until condition %s in script `%s` "
+                            "terminated because it looping more "
+                            "than %s times",
                             repeat[CONF_UNTIL],
                             self._script.name,
                             REPEAT_TERMINATE_ITERATIONS,
                         )
                         break
 
-                # If the user creates an script with a tight loop, yield to the event loop
-                # so the system stays responsive while all the cpu time is consumed.
+                # If the user creates an script with a tight loop,
+                # yield to the event loop so the system stays responsive
+                # while all the cpu time is consumed.
                 await asyncio.sleep(0)
 
         if saved_repeat_vars:
