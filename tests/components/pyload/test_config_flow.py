f79f26f19dd8268c3dfb18c6fbfd25e6b8ad86e0
tr4nt0r <manni@zapto.de>
2024-04-10 06:05:04 +0000

    Add tests for import flow and add add untested to coveragerc

--- tests/components/pyload/test_config_flow.py
+++ tests/components/pyload/test_config_flow.py
@@ -7,9 +7,18 @@ import pytest
 
 from homeassistant import config_entries
 from homeassistant.components.pyload.const import DOMAIN
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_PORT, CONF_URL
-from homeassistant.core import HomeAssistant
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_SSL,
+    CONF_URL,
+    CONF_USERNAME,
+    CONF_VERIFY_SSL,
+)
+from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
+import homeassistant.helpers.issue_registry as ir
 
 from .conftest import TEST_USER_DATA
 
@@ -138,6 +147,75 @@ async def test_flow_reauth(
     assert result["reason"] == "reauth_successful"
 
     assert len(hass.config_entries.async_entries()) == 1
+
+
+async def test_flow_import(hass: HomeAssistant, mock_pyloadapi: AsyncMock) -> None:
+    """Test that we can import a YAML config."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": config_entries.SOURCE_IMPORT},
+        data={
+            CONF_HOST: TEST_USER_DATA[CONF_HOST],
+            CONF_PASSWORD: TEST_USER_DATA[CONF_PASSWORD],
+            CONF_PORT: TEST_USER_DATA[CONF_PORT],
+            CONF_SSL: TEST_USER_DATA[CONF_SSL],
+            CONF_USERNAME: TEST_USER_DATA[CONF_USERNAME],
+        },
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == f"{TEST_USER_DATA[CONF_HOST]}:{TEST_USER_DATA[CONF_PORT]}"
+    assert result["data"] == {
+        **TEST_USER_DATA,
+        CONF_VERIFY_SSL: False,
+        CONF_URL: f"https://{TEST_USER_DATA[CONF_HOST]}:{TEST_USER_DATA[CONF_PORT]}/",
+    }
+
+    issue_registry = ir.async_get(hass)
+    issue = issue_registry.async_get_issue(
+        HOMEASSISTANT_DOMAIN, "deprecated_yaml_pyload"
+    )
+    assert issue.translation_key == "deprecated_yaml"
+
+
+@pytest.mark.parametrize(
+    ("raise_error", "text_error"),
+    [
+        (CannotConnect(), "cannot_connect"),
+        (InvalidAuth(), "invalid_auth"),
+        (ParserError(), "unknown"),
+        (IndexError(), "unknown"),
+    ],
+)
+async def test_flow_import_error(
+    hass: HomeAssistant, mock_pyloadapi: AsyncMock, raise_error, text_error
+) -> None:
+    """Test that we can import a YAML config."""
+
+    mock_pyloadapi.login.side_effect = raise_error
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": config_entries.SOURCE_IMPORT},
+        data={
+            CONF_HOST: TEST_USER_DATA[CONF_HOST],
+            CONF_PASSWORD: TEST_USER_DATA[CONF_PASSWORD],
+            CONF_PORT: TEST_USER_DATA[CONF_PORT],
+            CONF_SSL: TEST_USER_DATA[CONF_SSL],
+            CONF_USERNAME: TEST_USER_DATA[CONF_USERNAME],
+        },
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {"base": text_error}
+
+    issue_registry = ir.async_get(hass)
+    issue = issue_registry.async_get_issue(
+        DOMAIN, f"deprecated_yaml_import_issue_{text_error}"
+    )
+    assert issue.translation_key == f"deprecated_yaml_import_issue_{text_error}"
 
 
 @pytest.mark.parametrize(
