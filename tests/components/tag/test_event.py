b2835fdbbc4196d6cc3ad6b324efbbdcc7db065e
G Johansson <goran.johansson@shiftit.se>
2024-04-12 18:54:56 +0000

    Fix tests

--- tests/components/tag/test_event.py
+++ tests/components/tag/test_event.py
@@ -1,24 +1,18 @@
 """Tests for the tag component."""
 
-import logging
-
 from freezegun.api import FrozenDateTimeFactory
 import pytest
 
-from homeassistant.components.event import DOMAIN as EVENT_DOMAIN
 from homeassistant.components.tag import DOMAIN, EVENT_TAG_SCANNED, async_scan_tag
-from homeassistant.const import CONF_NAME, STATE_UNKNOWN
+from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
 from homeassistant.setup import async_setup_component
 from homeassistant.util import dt as dt_util
 
+from . import TEST_DEVICE_ID, TEST_TAG_ID, TEST_TAG_NAME
+
 from tests.common import async_capture_events
 from tests.typing import WebSocketGenerator
-
-TEST_TAG_ID = "test tag id"
-TEST_TAG_NAME = "test tag name"
-TEST_DEVICE_ID = "device id"
 
 
 @pytest.fixture
@@ -122,91 +116,3 @@ async def test_unnamed_tag_scanned_event(
     assert event_data["name"] is None
     assert event_data["device_id"] == TEST_DEVICE_ID
     assert event_data["tag_id"] == TEST_TAG_ID
-
-
-async def test_entity(
-    hass: HomeAssistant,
-    hass_ws_client: WebSocketGenerator,
-    freezer: FrozenDateTimeFactory,
-    storage_setup_named_tag,
-) -> None:
-    """Test tag entity."""
-    assert await storage_setup_named_tag()
-
-    await hass_ws_client(hass)
-
-    entity = hass.states.get("event.test_tag_name")
-    assert entity
-    assert entity.state == STATE_UNKNOWN
-
-    now = dt_util.utcnow()
-    freezer.move_to(now)
-    await async_scan_tag(hass, TEST_TAG_ID, TEST_DEVICE_ID)
-
-    entity = hass.states.get("event.test_tag_name")
-    assert entity
-    assert entity.state == now.isoformat(timespec="milliseconds")
-    assert entity.attributes == {
-        "event_types": ["tag_scanned"],
-        "event_type": "tag_scanned",
-        "tag_id": "test tag id",
-        "device_id": "device id",
-        "friendly_name": "test tag name",
-    }
-
-
-async def test_entity_created_and_removed(
-    caplog: pytest.LogCaptureFixture,
-    hass: HomeAssistant,
-    hass_ws_client: WebSocketGenerator,
-    freezer: FrozenDateTimeFactory,
-    storage_setup_named_tag,
-    entity_registry: er.EntityRegistry,
-) -> None:
-    """Test tag entity created and removed."""
-    caplog.at_level(logging.DEBUG)
-    assert await storage_setup_named_tag()
-
-    client = await hass_ws_client(hass)
-
-    await client.send_json(
-        {
-            "id": 1,
-            "type": f"{DOMAIN}/create",
-            "tag_id": "1234567890",
-            "name": "Kitchen tag",
-        }
-    )
-    resp = await client.receive_json()
-    assert resp["success"]
-    item = resp["result"]
-
-    assert item["id"] == "1234567890"
-    assert item["name"] == "Kitchen tag"
-
-    entity = hass.states.get("event.kitchen_tag")
-    assert entity
-    assert entity.state == STATE_UNKNOWN
-    assert entity_registry.async_get_entity_id(EVENT_DOMAIN, DOMAIN, "1234567890")
-
-    now = dt_util.utcnow()
-    freezer.move_to(now)
-    await async_scan_tag(hass, "1234567890", TEST_DEVICE_ID)
-
-    entity = hass.states.get("event.kitchen_tag")
-    assert entity
-    assert entity.state == now.isoformat(timespec="milliseconds")
-
-    await client.send_json(
-        {
-            "id": 2,
-            "type": f"{DOMAIN}/delete",
-            "tag_id": "1234567890",
-        }
-    )
-    resp = await client.receive_json()
-    assert resp["success"]
-
-    entity = hass.states.get("event.kitchen_tag")
-    assert not entity
-    assert not entity_registry.async_get_entity_id(EVENT_DOMAIN, DOMAIN, "1234567890")
--- tests/components/tag/test_init.py
+++ tests/components/tag/test_init.py
@@ -1,13 +1,18 @@
 """Tests for the tag component."""
 
+import logging
+
 from freezegun.api import FrozenDateTimeFactory
 import pytest
 
 from homeassistant.components.tag import DOMAIN, async_scan_tag
+from homeassistant.const import CONF_NAME, STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import collection
 from homeassistant.setup import async_setup_component
 from homeassistant.util import dt as dt_util
+
+from . import TEST_DEVICE_ID, TEST_TAG_ID, TEST_TAG_NAME
 
 from tests.typing import WebSocketGenerator
 
@@ -21,7 +26,15 @@ def storage_setup(hass: HomeAssistant, hass_storage):
             hass_storage[DOMAIN] = {
                 "key": DOMAIN,
                 "version": 1,
-                "data": {"items": [{"id": "test tag", "tag_id": "test tag"}]},
+                "data": {
+                    "items": [
+                        {
+                            "id": TEST_TAG_ID,
+                            "tag_id": TEST_TAG_ID,
+                            CONF_NAME: TEST_TAG_NAME,
+                        }
+                    ]
+                },
             }
         else:
             hass_storage[DOMAIN] = items
@@ -46,7 +59,7 @@ async def test_ws_list(
     result = {item["id"]: item for item in resp["result"]}
 
     assert len(result) == 1
-    assert "test tag" in result
+    assert TEST_TAG_ID in result
 
 
 async def test_ws_update(
@@ -62,7 +75,7 @@ async def test_ws_update(
         {
             "id": 6,
             "type": f"{DOMAIN}/update",
-            f"{DOMAIN}_id": "test tag",
+            f"{DOMAIN}_id": TEST_TAG_ID,
             "name": "New name",
         }
     )
@@ -71,7 +84,7 @@ async def test_ws_update(
 
     item = resp["result"]
 
-    assert item["id"] == "test tag"
+    assert item["id"] == TEST_TAG_ID
     assert item["name"] == "New name"
 
 
@@ -93,7 +106,7 @@ async def test_tag_scanned(
     result = {item["id"]: item for item in resp["result"]}
 
     assert len(result) == 1
-    assert "test tag" in result
+    assert TEST_TAG_ID in result
 
     now = dt_util.utcnow()
     freezer.move_to(now)
@@ -106,7 +119,7 @@ async def test_tag_scanned(
     result = {item["id"]: item for item in resp["result"]}
 
     assert len(result) == 2
-    assert "test tag" in result
+    assert TEST_TAG_ID in result
     assert "new tag" in result
     assert result["new tag"]["last_scanned"] == now.isoformat()
 
@@ -131,8 +144,91 @@ async def test_tag_id_exists(
     changes = track_changes(hass.data[DOMAIN])
     client = await hass_ws_client(hass)
 
-    await client.send_json({"id": 2, "type": f"{DOMAIN}/create", "tag_id": "test tag"})
+    await client.send_json({"id": 2, "type": f"{DOMAIN}/create", "tag_id": TEST_TAG_ID})
     response = await client.receive_json()
     assert not response["success"]
     assert response["error"]["code"] == "home_assistant_error"
     assert len(changes) == 0
+
+
+async def test_entity(
+    hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
+    freezer: FrozenDateTimeFactory,
+    storage_setup,
+) -> None:
+    """Test tag entity."""
+    assert await storage_setup()
+
+    await hass_ws_client(hass)
+
+    entity = hass.states.get("tag.test_tag_name")
+    assert entity
+    assert entity.state == STATE_UNKNOWN
+
+    now = dt_util.utcnow()
+    freezer.move_to(now)
+    await async_scan_tag(hass, TEST_TAG_ID, TEST_DEVICE_ID)
+
+    entity = hass.states.get("tag.test_tag_name")
+    assert entity
+    assert entity.state == now.isoformat(timespec="milliseconds")
+    assert entity.attributes == {
+        "tag_id": "test tag id",
+        "device_id": "device id",
+        "friendly_name": "test tag name",
+    }
+
+
+async def test_entity_created_and_removed(
+    caplog: pytest.LogCaptureFixture,
+    hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
+    freezer: FrozenDateTimeFactory,
+    storage_setup,
+) -> None:
+    """Test tag entity created and removed."""
+    caplog.at_level(logging.DEBUG)
+    assert await storage_setup()
+
+    client = await hass_ws_client(hass)
+
+    await client.send_json(
+        {
+            "id": 1,
+            "type": f"{DOMAIN}/create",
+            "tag_id": "1234567890",
+            "name": "Kitchen tag",
+        }
+    )
+    resp = await client.receive_json()
+    assert resp["success"]
+    item = resp["result"]
+
+    assert item["id"] == "1234567890"
+    assert item["name"] == "Kitchen tag"
+
+    entity = hass.states.get("tag.kitchen_tag")
+    assert entity
+    assert entity.state == STATE_UNKNOWN
+
+    now = dt_util.utcnow()
+    freezer.move_to(now)
+    await async_scan_tag(hass, "1234567890", TEST_DEVICE_ID)
+
+    entity = hass.states.get("tag.kitchen_tag")
+    assert entity
+    assert entity.state == now.isoformat(timespec="milliseconds")
+
+    await client.send_json(
+        {
+            "id": 2,
+            "type": f"{DOMAIN}/delete",
+            "tag_id": "1234567890",
+        }
+    )
+    resp = await client.receive_json()
+    assert resp["success"]
+
+    entity = hass.states.get("tag.kitchen_tag")
+    assert not entity
