21ea346b79c9b265bb49c7b76ae4407bdac5a3e7
autinerd <27780930+autinerd@users.noreply.github.com>
2024-04-12 05:45:52 +0200

    fix tado tests

--- homeassistant/components/tado/__init__.py
+++ homeassistant/components/tado/__init__.py
@@ -221,7 +221,7 @@ class TadoConnector:
 
         # Errors are planned to be converted to exceptions
         # in PyTado library, so this can be removed
-        if mobile_devices.get("errors"):
+        if isinstance(mobile_devices, dict) and mobile_devices.get("errors"):
             _LOGGER.error(
                 "Error for home ID %s while updating mobile devices: %s",
                 self.home_id,
@@ -256,7 +256,7 @@ class TadoConnector:
 
         # Errors are planned to be converted to exceptions
         # in PyTado library, so this can be removed
-        if devices.get("errors"):
+        if isinstance(devices, dict) and devices.get("errors"):
             _LOGGER.error(
                 "Error for home ID %s while updating devices: %s",
                 self.home_id,
