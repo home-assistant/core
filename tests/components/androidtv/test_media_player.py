62d6004cf3a0f00fb5a6398f37ce6982b873a7b9
ollo69 <oliveroandrea@libero.it>
2024-04-13 09:03:33 +0000

    Check error log on generic exception test

--- tests/components/androidtv/test_media_player.py
+++ tests/components/androidtv/test_media_player.py
@@ -1182,7 +1182,7 @@ async def test_connection_closed_on_ha_stop(hass: HomeAssistant) -> None:
             assert adb_close.called
 
 
-async def test_exception(hass: HomeAssistant) -> None:
+async def test_exception(hass: HomeAssistant, caplog: pytest.LogCaptureFixture) -> None:
     """Test that the ADB connection gets closed when there is an unforeseen exception.
 
     HA will attempt to reconnect on the next update.
@@ -1202,6 +1202,9 @@ async def test_exception(hass: HomeAssistant) -> None:
         assert state is not None
         assert state.state == STATE_OFF
 
+        caplog.clear()
+        caplog.set_level(logging.ERROR)
+
         # When an unforeseen exception occurs, we close the ADB connection and raise the exception
         with patchers.PATCH_ANDROIDTV_UPDATE_EXCEPTION:
             await async_update_entity(hass, entity_id)
@@ -1209,6 +1212,11 @@ async def test_exception(hass: HomeAssistant) -> None:
         state = hass.states.get(entity_id)
         assert state is not None
         assert state.state == STATE_UNAVAILABLE
+        assert len(caplog.record_tuples) == 1
+        assert caplog.record_tuples[0][1] == logging.ERROR
+        assert caplog.record_tuples[0][2].startswith(
+            "Unexpected exception executing an ADB command"
+        )
 
         # On the next update, HA will reconnect to the device
         await async_update_entity(hass, entity_id)
