498c78ff85cd572dcca9d61f8a4406117bf59680
alexyao2015 <alexyao2015@users.noreply.github.com>
2024-04-13 02:19:32 +0000

    Adjust issues log

--- homeassistant/components/html5/issues.py
+++ homeassistant/components/html5/issues.py
@@ -9,13 +9,13 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 SUCCESSFUL_IMPORT_LOG = """Loading HTML5 push notification via configuration.yaml is deprecated,
-it has been automatically imported. Once you have confirmed correct
-operation, please remove HTML5 notification configuration from
-configuration.yaml"""
+ it has been automatically imported. Once you have confirmed correct
+ operation, please remove HTML5 notification configuration from
+ configuration.yaml""".replace("\n", "")
 
 FAILED_IMPORT_LOG = """Loading HTML5 push notification via configuration.yaml is deprecated and
-the automatic import has failed. Please remove the HTML5 notification configuration from configuration.yaml
-and setup HTML5 from scratch via the UI."""
+ the automatic import has failed. Please remove the HTML5 notification configuration from configuration.yaml
+ and setup HTML5 from scratch via the UI.""".replace("\n", "")
 
 
 def create_issue(hass: HomeAssistant, import_success: bool):
