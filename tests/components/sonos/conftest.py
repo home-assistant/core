e47c4e6c150a7652bac1e30ea995c67d9bf2fc51
PeteRager <pete.sage@icloud.com>
2024-04-13 13:31:14 -0400

    Refactor mock media

--- tests/components/sonos/conftest.py
+++ tests/components/sonos/conftest.py
@@ -18,6 +18,8 @@ from homeassistant.components.sonos import DOMAIN
 from homeassistant.const import CONF_HOSTS
 from homeassistant.core import HomeAssistant
 
+from .mock_media import mock_sonos_favorites
+
 from tests.common import MockConfigEntry, load_fixture
 
 
@@ -304,79 +306,17 @@ def config_fixture():
     return {DOMAIN: {MP_DOMAIN: {CONF_HOSTS: ["192.168.42.2"]}}}
 
 
-class MockMusicServiceItem:
-    """Mocks a Soco MusicServiceItem."""
-
-    def __init__(
-        self, title: str, item_id: str, parent_id: str, item_class: str, uri: str = None
-    ) -> None:
-        """Initialize the mock item."""
-        self.title = title
-        self.item_id = item_id
-        self.item_class = item_class
-        self.parent_id = parent_id
-
-    def get_uri(self) -> str:
-        """Return URI."""
-        return self.item_id.replace("S://", "x-file-cifs://")
-
-
-class MockDidlFavorite(MockMusicServiceItem):
-    """Mocks a Soco DidlFavorite."""
-
-    def __init__(
-        self,
-        title: str,
-        item_id: str,
-        parent_id: str,
-        item_class: str,
-        uri: str = None,
-        reference_item_id=None,
-    ) -> None:
-        """Initialize the mock item."""
-        MockMusicServiceItem.__init__(self, title, item_id, parent_id, item_class)
-        self.reference = MagicMock(name=item_id)
-        self.reference.resources.return_value = True
-        self.reference.item_id = reference_item_id
-        self.reference.get_uri.return_value = uri
-
-
-_mock_favorites = [
-    MockDidlFavorite(
-        # Sirius XM
-        title="66 - Watercolors",
-        item_id="FV:2/4",
-        parent_id="FV:2",
-        item_class="object.itemobject.item.sonos-favorite",
-        uri="x-sonosapi-hls:Api%3atune%3aliveAudio%3ajazzcafe%3aetc",
-    ),
-    MockDidlFavorite(
-        # Pandora
-        title="James Taylor Radio",
-        item_id="FV:2/13",
-        parent_id="FV:2",
-        item_class="object.itemobject.item.sonos-favorite",
-        uri="x-sonosapi-radio:ST%3aetc",
-    ),
-    MockDidlFavorite(
-        # Sonos Playlists for music library
-        title="1984",
-        item_id="FV:2/8",
-        parent_id="FV:2",
-        item_class="object.itemobject.item.sonos-favorite",
-        uri="x-rincon-playlist:RINCON_test#A:ALBUMARTIST/Aerosmith/1984",
-        reference_item_id="A:ALBUMARTIST/Aerosmith/1984",
-    ),
-]
+@pytest.fixture(name="sonos_favorites")
+def sonos_favorites_fixture():
+    """Create sonos favorites fixture."""
+    return SearchResult(mock_sonos_favorites, "favorites", 3, 3, 1)
 
 
 @pytest.fixture(name="music_library")
-def music_library_fixture():
+def music_library_fixture(sonos_favorites):
     """Create music_library fixture."""
     music_library = MagicMock()
-    music_library.get_sonos_favorites.return_value = SearchResult(
-        _mock_favorites, "favorites", 3, 3, 1
-    )
+    music_library.get_sonos_favorites.return_value = sonos_favorites
     return music_library
 
 
