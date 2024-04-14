2749f22392293392467d86e757bc808dbd0564d4
J. Nick Koston <nick@koston.org>
2024-04-13 19:19:40 -0500

    Deprecate async_track_state_change in favor of async_track_state_change_event
    needs
    - [ ] https://github.com/home-assistant/core/pull/115556
    - [ ] https://github.com/home-assistant/core/pull/115555

--- homeassistant/helpers/event.py
+++ homeassistant/helpers/event.py
@@ -40,6 +40,7 @@ from homeassistant.util import dt as dt_util
 from homeassistant.util.async_ import run_callback_threadsafe
 from homeassistant.util.event_type import EventType
 
+from . import frame
 from .device_registry import (
     EVENT_DEVICE_REGISTRY_UPDATED,
     EventDeviceRegistryUpdatedData,
@@ -207,6 +208,12 @@ def async_track_state_change(
 
     Must be run within the event loop.
     """
+    frame.report(
+        "calls `async_track_state_change` instead of `async_track_state_change_event`"
+        " which is deprecated and will be removed in Assistant 2025.5",
+        error_if_core=False,
+    )
+
     if from_state is not None:
         match_from_state = process_state_match(from_state)
     if to_state is not None:
