65b5c31e35f7d18343be60ef8701d519d4595111
autinerd <27780930+autinerd@users.noreply.github.com>
2024-04-17 18:30:51 +0200

    review comments

--- homeassistant/components/enigma2/config_flow.py
+++ homeassistant/components/enigma2/config_flow.py
@@ -68,17 +68,12 @@ class Enigma2ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     )
     OPTIONS_KEYS = (CONF_DEEP_STANDBY, CONF_SOURCE_BOUQUET, CONF_USE_CHANNEL_ICON)
 
-    def __init__(self) -> None:
-        """Initialize the config flow."""
-        super().__init__()
-        self.errors: dict[str, str] = {}
-        self._data: dict[str, Any] = {}
-        self._options: dict[str, Any] = {}
-
-    async def validate_user_input(self, user_input: dict[str, Any]) -> None:
+    async def validate_user_input(
+        self, user_input: dict[str, Any]
+    ) -> dict[str, str] | None:
         """Validate user input."""
 
-        self.errors = {}
+        errors = None
 
         self._async_abort_entries_match({CONF_HOST: user_input[CONF_HOST]})
 
@@ -97,16 +92,16 @@ class Enigma2ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
         try:
             about = await OpenWebIfDevice(session).get_about()
         except InvalidAuthError:
-            self.errors["base"] = "invalid_auth"
+            errors = {"base": "invalid_auth"}
         except ClientError:
-            self.errors["base"] = "cannot_connect"
+            errors = {"base": "cannot_connect"}
         except Exception:  # pylint: disable=broad-except
-            self.errors["base"] = "unknown"
+            errors = {"base": "unknown"}
         else:
             await self.async_set_unique_id(about["info"]["ifaces"][0]["mac"])
             self._abort_if_unique_id_configured()
 
-        self._data = user_input
+        return errors
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -115,14 +110,11 @@ class Enigma2ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
         if user_input is None:
             return self.async_show_form(step_id=SOURCE_USER, data_schema=CONFIG_SCHEMA)
 
-        await self.validate_user_input(user_input)
-        if "base" in self.errors:
+        if errors := await self.validate_user_input(user_input):
             return self.async_show_form(
-                step_id=SOURCE_USER, data_schema=CONFIG_SCHEMA, errors=self.errors
+                step_id=SOURCE_USER, data_schema=CONFIG_SCHEMA, errors=errors
             )
-        return self.async_create_entry(
-            data=self._data, title=self._data[CONF_HOST], options=self._options
-        )
+        return self.async_create_entry(data=user_input, title=user_input[CONF_HOST])
 
     async def async_step_import(self, user_input: dict[str, Any]) -> ConfigFlowResult:
         """Handle the import step."""
@@ -132,27 +124,26 @@ class Enigma2ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
             user_input[CONF_SSL] = DEFAULT_SSL
         user_input[CONF_VERIFY_SSL] = DEFAULT_VERIFY_SSL
 
-        self._data = {
-            key: user_input[key] for key in user_input if key in self.DATA_KEYS
-        }
-        self._options = {
+        data = {key: user_input[key] for key in user_input if key in self.DATA_KEYS}
+        options = {
             key: user_input[key] for key in user_input if key in self.OPTIONS_KEYS
         }
 
-        await self.validate_user_input(self._data)
-
-        if "base" in self.errors:
+        if errors := await self.validate_user_input(user_input):
             async_create_issue(
                 self.hass,
                 DOMAIN,
-                f"deprecated_yaml_{DOMAIN}_import_issue",
+                f"deprecated_yaml_{DOMAIN}_import_issue_{errors["base"]}",
                 breaks_in_ha_version="2024.11.0",
                 is_fixable=False,
                 issue_domain=DOMAIN,
                 severity=IssueSeverity.WARNING,
-                translation_key="deprecated_yaml_import_issue",
+                translation_key=f"deprecated_yaml_import_issue_{errors["base"]}",
+                translation_placeholders={
+                    "url": "/config/integrations/dashboard/add?domain=enigma2"
+                },
             )
-            return self.async_abort(reason=self.errors["base"])
+            return self.async_abort(reason=errors["base"])
 
         async_create_issue(
             self.hass,
@@ -170,5 +161,5 @@ class Enigma2ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
             },
         )
         return self.async_create_entry(
-            data=self._data, title=self._data[CONF_HOST], options=self._options
+            data=data, title=data[CONF_HOST], options=options
         )
