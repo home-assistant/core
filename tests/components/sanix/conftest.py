0f71ea348ffc9eb28d777464512d1066eb2acb35
Joost Lekkerkerker <joostlek@outlook.com>
2024-04-17 21:20:12 +0200

    Add sanix sensor tests (#115763)
    Add sanix tests

--- tests/components/sanix/conftest.py
+++ tests/components/sanix/conftest.py
@@ -1,9 +1,21 @@
 """Sanix tests configuration."""
 
 from collections.abc import Generator
-from unittest.mock import AsyncMock, MagicMock, patch
+from datetime import datetime
+from unittest.mock import AsyncMock, patch
+from zoneinfo import ZoneInfo
 
 import pytest
+from sanix import (
+    ATTR_API_BATTERY,
+    ATTR_API_DEVICE_NO,
+    ATTR_API_DISTANCE,
+    ATTR_API_FILL_PERC,
+    ATTR_API_SERVICE_DATE,
+    ATTR_API_SSID,
+    ATTR_API_STATUS,
+    ATTR_API_TIME,
+)
 from sanix.models import Measurement
 
 from homeassistant.components.sanix.const import CONF_SERIAL_NUMBER, DOMAIN
@@ -15,19 +27,31 @@ from tests.common import MockConfigEntry, load_json_object_fixture
 @pytest.fixture
 def mock_sanix():
     """Build a fixture for the Sanix API that connects successfully and returns measurements."""
-    fixture = load_json_object_fixture("sanix/get_measurements.json")
-    mock_sanix_api = MagicMock()
+    fixture = load_json_object_fixture("get_measurements.json", DOMAIN)
     with (
         patch(
             "homeassistant.components.sanix.config_flow.Sanix",
-            return_value=mock_sanix_api,
+            autospec=True,
         ) as mock_sanix_api,
         patch(
             "homeassistant.components.sanix.Sanix",
-            return_value=mock_sanix_api,
+            new=mock_sanix_api,
         ),
     ):
-        mock_sanix_api.return_value.fetch_data.return_value = Measurement(**fixture)
+        mock_sanix_api.return_value.fetch_data.return_value = Measurement(
+            battery=fixture[ATTR_API_BATTERY],
+            device_no=fixture[ATTR_API_DEVICE_NO],
+            distance=fixture[ATTR_API_DISTANCE],
+            fill_perc=fixture[ATTR_API_FILL_PERC],
+            service_date=datetime.strptime(
+                fixture[ATTR_API_SERVICE_DATE], "%d.%m.%Y"
+            ).date(),
+            ssid=fixture[ATTR_API_SSID],
+            status=fixture[ATTR_API_STATUS],
+            time=datetime.strptime(fixture[ATTR_API_TIME], "%d.%m.%Y %H:%M:%S").replace(
+                tzinfo=ZoneInfo("Europe/Warsaw")
+            ),
+        )
         yield mock_sanix_api
 
 
