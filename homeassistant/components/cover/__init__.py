8ba1340c2e287fca28861a41a14c6f45ad198a9a
vexofp <vexofp@gmail.com>
2024-04-18 08:58:16 -0400

    Clarify cover toggle logic; prevent opening when already open (#107920)
    Co-authored-by: Erik Montnemery <erik@montnemery.com>

--- homeassistant/components/cover/__init__.py
+++ homeassistant/components/cover/__init__.py
@@ -480,15 +480,30 @@ class CoverEntity(Entity, cached_properties=CACHED_PROPERTIES_WITH_ATTR_):
     def _get_toggle_function(
         self, fns: dict[str, Callable[_P, _R]]
     ) -> Callable[_P, _R]:
+        # If we are opening or closing and we support stopping, then we should stop
         if self.supported_features & CoverEntityFeature.STOP and (
             self.is_closing or self.is_opening
         ):
             return fns["stop"]
-        if self.is_closed:
+
+        # If we are fully closed or in the process of closing, then we should open
+        if self.is_closed or self.is_closing:
             return fns["open"]
-        if self._cover_is_last_toggle_direction_open:
+
+        # If we are fully open or in the process of opening, then we should close
+        if self.current_cover_position == 100 or self.is_opening:
             return fns["close"]
-        return fns["open"]
+
+        # We are any of:
+        # * fully open but do not report `current_cover_position`
+        # * stopped partially open
+        # * either opening or closing, but do not report them
+        # If we previously reported opening/closing, we should move in the opposite direction.
+        # Otherwise, we must assume we are (partially) open and should always close.
+        # Note: _cover_is_last_toggle_direction_open will always remain True if we never report opening/closing.
+        return (
+            fns["close"] if self._cover_is_last_toggle_direction_open else fns["open"]
+        )
 
 
 # These can be removed if no deprecated constant are in this module anymore
