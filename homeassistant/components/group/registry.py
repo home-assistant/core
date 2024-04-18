114dfe8469b4d54eb6e621faf9e0e8732dab2927
jbouwh <jan@jbsoft.nl>
2024-04-18 06:32:21 +0000

    Fix mutable group registry class

--- homeassistant/components/group/registry.py
+++ homeassistant/components/group/registry.py
@@ -47,16 +47,20 @@ def _process_group_platform(
 class GroupIntegrationRegistry:
     """Class to hold a registry of integrations."""
 
-    on_off_mapping: dict[str, str] = {STATE_ON: STATE_OFF}
-    off_on_mapping: dict[str, str] = {STATE_OFF: STATE_ON}
-    on_states_by_domain: dict[str, set] = {}
-    exclude_domains: set = set()
+    def __init__(self) -> None:
+        """Imitialize registry."""
+        self.on_off_mapping: dict[str, str] = {STATE_ON: STATE_OFF}
+        self.off_on_mapping: dict[str, str] = {STATE_OFF: STATE_ON}
+        self.on_states_by_domain: dict[str, set[str]] = {}
+        self.exclude_domains: set[str] = set()
 
     def exclude_domain(self) -> None:
         """Exclude the current domain."""
         self.exclude_domains.add(current_domain.get())
 
-    def on_off_states(self, on_states: set, off_state: str) -> None:
+    def on_off_states(
+        self, on_states: set, off_state: str, default_on_state: str | None = None
+    ) -> None:
         """Register on and off states for the current domain."""
         for on_state in on_states:
             if on_state not in self.on_off_mapping:
