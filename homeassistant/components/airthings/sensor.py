4502ffbfd58ee7cac98edc3b2055e2740cfd754a
Ståle Storø Hauknes <stale.semb@airthings.com>
2024-04-19 23:49:04 +0200

    Address PR comment

--- homeassistant/components/airthings/sensor.py
+++ homeassistant/components/airthings/sensor.py
@@ -156,4 +156,12 @@ class AirthingsHeaterEnergySensor(
     @property
     def native_value(self) -> StateType:
         """Return the value reported by the sensor."""
-        return self.coordinator.data[self._id].sensors.get(self.entity_description.key)  # type: ignore[no-any-return]
+        return self.coordinator.data[self._id].sensors[self.entity_description.key]  # type: ignore[no-any-return]
+
+    @property
+    def available(self) -> bool:
+        """Check if device and sensor is available in data."""
+        return (
+            super().available
+            and self.entity_description.key in self.coordinator.data[self._id].sensors
+        )
