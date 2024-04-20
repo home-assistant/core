6f0d5ef9b8315455a9746a26a60da943a42bdc75
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-04-20 19:02:07 +0000

    remove comment

--- homeassistant/components/husqvarna_automower/select.py
+++ homeassistant/components/husqvarna_automower/select.py
@@ -63,7 +63,6 @@ class AutomowerSelectEntity(AutomowerControlEntity, SelectEntity):
 
     async def async_select_option(self, option: str) -> None:
         """Change the selected option."""
-        # mode: Literal[HeadlightModes.ALWAYS_OFF,HeadlightModes.ALWAYS_ON,HeadlightModes.EVENING_AND_NIGHT, HeadlightModes.EVENING_ONLY] = cast(HeadlightModes, option.upper())
         try:
             await self.coordinator.api.set_headlight_mode(
                 self.mower_id, cast(HeadlightModes, option.upper())
