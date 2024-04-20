56fdeaf092d9e209f45d4e5a639ba5f08708d815
Erwin Douna <e.douna@gmail.com>
2024-04-20 17:05:00 +0000

    Go for 100% test coverage

--- tests/components/dsmr_reader/test_sensor.py
+++ tests/components/dsmr_reader/test_sensor.py
@@ -1,10 +1,5 @@
 """Tests for DSMR Reader sensor."""
 
-from collections.abc import Callable
-from unittest.mock import MagicMock, patch
-
-import pytest
-
 from homeassistant.components.dsmr_reader.const import DOMAIN
 from homeassistant.components.dsmr_reader.definitions import (
     DSMRReaderSensorEntityDescription,
@@ -17,55 +12,9 @@ from tests.common import MockConfigEntry, async_fire_mqtt_message
 from tests.typing import MqttMockHAClient
 
 
-@pytest.mark.parametrize(
-    ("payload", "state_function", "expected_native_value"),
-    [
-        # Test when message payload is empty
-        ("", None, None),
-        # Test when entity_description.state is not None
-        ("test_payload", lambda x: x, "test_payload"),
-        # Test when entity_description.state is None
-        ("test_payload", None, "test_payload"),
-    ],
-)
-@patch("homeassistant.components.dsmr_reader.sensor.mqtt.async_subscribe")
-async def test_dsmr_sensor_async_added_to_hass(
-    mock_mqtt_subscribe,
-    hass: HomeAssistant,
-    payload: str,
-    state_function: Callable,
-    expected_native_value: str,
-) -> None:
-    """Test the sensor via a typical unit test."""
-    config_entry = MockConfigEntry(
-        domain=DOMAIN,
-        title=DOMAIN,
-        options={},
-        entry_id="TEST_ENTRY_ID",
-        unique_id="UNIQUE_TEST_ID",
-    )
-    config_entry.add_to_hass(hass)
-
-    await hass.config_entries.async_setup(config_entry.entry_id)
-    await hass.async_block_till_done()
-
-    mock_mqtt_subscribe.side_effect = lambda hass, key, callback, qos: callback(message)
-    message = MagicMock()
-    message.payload = payload
-
-    description = DSMRReaderSensorEntityDescription(
-        key="DSMR_TEST_KEY",
-        name="DSMR_TEST_NAME",
-        state=state_function,
-    )
-    sensor = DSMRSensor(description, config_entry)
-    sensor.hass = hass
-    await sensor.async_added_to_hass()
-    assert sensor.native_value == expected_native_value
-
-
 async def test_dsmr_sensor_mqtt(
-    hass: HomeAssistant, mqtt_mock: MqttMockHAClient
+    hass: HomeAssistant,
+    mqtt_mock: MqttMockHAClient,
 ) -> None:
     """Test the DSMRSensor class, via an emluated MQTT message."""
     config_entry = MockConfigEntry(
@@ -85,6 +34,16 @@ async def test_dsmr_sensor_mqtt(
     electricity_delivered_2 = "sensor.dsmr_reading_electricity_delivered_2"
     assert hass.states.get(electricity_delivered_2).state == STATE_UNKNOWN
 
+    # Test if the payload is empty
+    async_fire_mqtt_message(hass, "dsmr/reading/electricity_delivered_1", "")
+    await hass.async_block_till_done()
+    async_fire_mqtt_message(hass, "dsmr/reading/electricity_delivered_2", "")
+    await hass.async_block_till_done()
+
+    assert hass.states.get(electricity_delivered_1).state == STATE_UNKNOWN
+    assert hass.states.get(electricity_delivered_2).state == STATE_UNKNOWN
+
+    # Test if the payload is not empty
     async_fire_mqtt_message(hass, "dsmr/reading/electricity_delivered_1", "1050.39")
     await hass.async_block_till_done()
     async_fire_mqtt_message(hass, "dsmr/reading/electricity_delivered_2", "2001.12")
@@ -92,3 +51,16 @@ async def test_dsmr_sensor_mqtt(
 
     assert hass.states.get(electricity_delivered_1).state == "1050.39"
     assert hass.states.get(electricity_delivered_2).state == "2001.12"
+
+    # Create a test entity to ensure the entity_description.state is not None
+    description = DSMRReaderSensorEntityDescription(
+        key="DSMR_TEST_KEY",
+        name="DSMR_TEST_NAME",
+        state=lambda x: x,
+    )
+    sensor = DSMRSensor(description, config_entry)
+    sensor.hass = hass
+    await sensor.async_added_to_hass()
+    async_fire_mqtt_message(hass, "DSMR_TEST_KEY", "192.8")
+    await hass.async_block_till_done()
+    assert sensor.native_value == "192.8"
