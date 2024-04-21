1300290e1a487703abc43339f51c0db2217c4088
J. Nick Koston <nick@koston.org>
2024-04-21 19:13:45 +0200

    always use fileno

--- homeassistant/components/mqtt/client.py
+++ homeassistant/components/mqtt/client.py
@@ -474,7 +474,8 @@ class MQTT:
         self._mqttc.on_socket_open = self._on_socket_open
         self._mqttc.on_socket_close = self._on_socket_close
         self._mqttc.on_socket_register_write = self._on_socket_register_write
-        self._mqttc.on_socket_unregister_write = self._on_socket_unregister_write
+        # on_socket_unregister_write is only ever called in the event loop
+        self._mqttc.on_socket_unregister_write = self._async_on_socket_unregister_write
 
         # These will be called in the event loop
         self._mqttc.on_connect = self._async_mqtt_on_connect
@@ -581,14 +582,6 @@ class MQTT:
         _LOGGER.debug("%s: register write %s", self.config_entry.title, fileno)
         if fileno > -1:
             self.loop.add_writer(fileno, partial(self._async_writer_callback, client))
-
-    def _on_socket_unregister_write(
-        self, client: mqtt.Client, userdata: Any, sock: SocketType
-    ) -> None:
-        """Unregister the socket for writing."""
-        self.loop.call_soon_threadsafe(
-            self._async_on_socket_unregister_write, client, None, sock
-        )
 
     @callback
     def _async_on_socket_unregister_write(
