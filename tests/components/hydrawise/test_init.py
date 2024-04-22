c86a2d443a7616f2800338caf54b448cde6aaf52
David Knowles <dknowles2@gmail.com>
2024-04-22 10:54:04 -0400

    Remove YAML support from Hydrawise (#115966)

--- tests/components/hydrawise/test_init.py
+++ tests/components/hydrawise/test_init.py
@@ -5,27 +5,9 @@ from unittest.mock import AsyncMock
 from aiohttp import ClientError
 
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import CONF_ACCESS_TOKEN
-from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
-import homeassistant.helpers.issue_registry as ir
-from homeassistant.setup import async_setup_component
+from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
-
-
-async def test_setup_import_success(
-    hass: HomeAssistant, mock_pydrawise: AsyncMock
-) -> None:
-    """Test that setup with a YAML config triggers an import and warning."""
-    config = {"hydrawise": {CONF_ACCESS_TOKEN: "_access-token_"}}
-    assert await async_setup_component(hass, "hydrawise", config)
-    await hass.async_block_till_done()
-
-    issue_registry = ir.async_get(hass)
-    issue = issue_registry.async_get_issue(
-        HOMEASSISTANT_DOMAIN, "deprecated_yaml_hydrawise"
-    )
-    assert issue.translation_key == "deprecated_yaml"
 
 
 async def test_connect_retry(
