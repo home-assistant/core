e9a2e807aae84e65019200f0902b711cf51faeac
Thomas Kistler <thomas@uyei-kistler.com>
2024-04-23 04:25:28 +0000

    Update Hydrawise from the legacy API to the new GraphQL API.

--- homeassistant/components/hydrawise/__init__.py
+++ homeassistant/components/hydrawise/__init__.py
@@ -1,10 +1,11 @@
 """Support for Hydrawise cloud."""
 
-from pydrawise import legacy
+from pydrawise import auth, client
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_API_KEY, Platform
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryAuthFailed
 
 from .const import DOMAIN, SCAN_INTERVAL
 from .coordinator import HydrawiseDataUpdateCoordinator
@@ -14,8 +15,15 @@ PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.SENSOR, Platform.S
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up Hydrawise from a config entry."""
-    access_token = config_entry.data[CONF_API_KEY]
-    hydrawise = legacy.LegacyHydrawiseAsync(access_token)
+    if CONF_USERNAME not in config_entry.data or CONF_PASSWORD not in config_entry.data:
+        # The GraphQL API requires username and password to authenticate. If either is
+        # missing, reauth is required.
+        raise ConfigEntryAuthFailed
+
+    hydrawise = client.Hydrawise(
+        auth.Auth(config_entry.data[CONF_USERNAME], config_entry.data[CONF_PASSWORD])
+    )
+
     coordinator = HydrawiseDataUpdateCoordinator(hass, hydrawise, SCAN_INTERVAL)
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
