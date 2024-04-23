fac57d06b7ed99a7fcd0f825b5670a0116f9de31
Joost Lekkerkerker <joostlek@outlook.com>
2024-04-23 10:59:42 +0200

    Update homeassistant/components/lutron/event.py

--- homeassistant/components/lutron/event.py
+++ homeassistant/components/lutron/event.py
@@ -91,7 +91,7 @@ class LutronEventEntity(LutronKeypad, EventEntity):
         elif event == Button.Event.PRESSED:
             action = LutronEventType.SINGLE_PRESS
 
-        # Event bus events are deprecated in 2024.4 and will be removed in 2024.10
+        # Event bus events are deprecated in 2024.5 and will be removed in 2024.11
         if action:
             data = {
                 ATTR_ID: self._id,
