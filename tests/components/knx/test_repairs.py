7e9c36f21258cbe15d9523f60766048a56d65cda
farmio <farmio@alphart.net>
2024-04-23 09:12:27 +0200

    add "repairs" to dependencies, clean up tests

--- tests/components/knx/test_repairs.py
+++ tests/components/knx/test_repairs.py
@@ -5,31 +5,26 @@ from http import HTTPStatus
 from homeassistant.components.knx.const import DOMAIN, KNX_ADDRESS
 from homeassistant.components.knx.schema import NotifySchema
 from homeassistant.components.notify import DOMAIN as NOTIFY_DOMAIN
-from homeassistant.components.repairs import DOMAIN as REPAIRS_DOMAIN
-from homeassistant.components.repairs.issue_handler import (
-    async_process_repairs_platforms,
-)
 from homeassistant.components.repairs.websocket_api import (
     RepairsFlowIndexView,
     RepairsFlowResourceView,
 )
 from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
-from homeassistant.setup import async_setup_component
+import homeassistant.helpers.issue_registry as ir
 
 from .conftest import KNXTestKit
 
-from tests.typing import ClientSessionGenerator, WebSocketGenerator
+from tests.typing import ClientSessionGenerator
 
 
 async def test_knx_notify_service_issue(
     hass: HomeAssistant,
     knx: KNXTestKit,
     hass_client: ClientSessionGenerator,
-    hass_ws_client: WebSocketGenerator,
+    issue_registry: ir.IssueRegistry,
 ) -> None:
     """Test the legacy notify service still works before migration and repair flow is triggered."""
-    assert await async_setup_component(hass, REPAIRS_DOMAIN, {REPAIRS_DOMAIN: {}})
     await knx.setup_integration(
         {
             NotifySchema.PLATFORM: {
@@ -38,17 +33,10 @@ async def test_knx_notify_service_issue(
             }
         }
     )
-
-    await async_process_repairs_platforms(hass)
-
-    ws_client = await hass_ws_client(hass)
     http_client = await hass_client()
 
     # Assert no issue is present
-    await ws_client.send_json_auto_id({"type": "repairs/list_issues"})
-    msg = await ws_client.receive_json()
-    assert msg["success"]
-    assert len(msg["result"]["issues"]) == 0
+    assert len(issue_registry.issues) == 0
 
     # Simulate legacy service being used
     assert hass.services.has_service(NOTIFY_DOMAIN, NOTIFY_DOMAIN)
@@ -64,16 +52,16 @@ async def test_knx_notify_service_issue(
     )
 
     # Assert the issue is present
-    await ws_client.send_json_auto_id({"type": "repairs/list_issues"})
-    msg = await ws_client.receive_json()
-    assert msg["success"]
-    assert len(msg["result"]["issues"]) == 1
-    issue = msg["result"]["issues"][0]
-    assert issue["issue_id"] == "migrate_notify"
+    assert len(issue_registry.issues) == 1
+    assert issue_registry.async_get_issue(
+        domain=DOMAIN,
+        issue_id="migrate_notify",
+    )
 
-    url = RepairsFlowIndexView.url
+    # Test confirm step in repair flow
     resp = await http_client.post(
-        url, json={"handler": DOMAIN, "issue_id": "migrate_notify"}
+        RepairsFlowIndexView.url,
+        json={"handler": DOMAIN, "issue_id": "migrate_notify"},
     )
     assert resp.status == HTTPStatus.OK
     data = await resp.json()
@@ -81,16 +69,16 @@ async def test_knx_notify_service_issue(
     flow_id = data["flow_id"]
     assert data["step_id"] == "confirm"
 
-    url = RepairsFlowResourceView.url.format(flow_id=flow_id)
-    resp = await http_client.post(url)
+    resp = await http_client.post(
+        RepairsFlowResourceView.url.format(flow_id=flow_id),
+    )
     assert resp.status == HTTPStatus.OK
     data = await resp.json()
     assert data["type"] == "create_entry"
-    # Test confirm step in repair flow
-    await hass.async_block_till_done()
 
     # Assert the issue is no longer present
-    await ws_client.send_json_auto_id({"type": "repairs/list_issues"})
-    msg = await ws_client.receive_json()
-    assert msg["success"]
-    assert len(msg["result"]["issues"]) == 0
+    assert not issue_registry.async_get_issue(
+        domain=DOMAIN,
+        issue_id="migrate_notify",
+    )
+    assert len(issue_registry.issues) == 0
