6192b5cf5097a902644140af1cdfb078307619e4
On Freund <onfreund@gmail.com>
2024-04-25 08:54:31 +0300

    Avoid blocking the event loop when unloading Monoprice

--- homeassistant/components/monoprice/__init__.py
+++ homeassistant/components/monoprice/__init__.py
@@ -57,11 +57,18 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN][entry.entry_id][UNDO_UPDATE_LISTENER]()
+    if not unload_ok:
+        return False
+
+    hass.data[DOMAIN][entry.entry_id][UNDO_UPDATE_LISTENER]()
+
+    def _cleanup():
+        del hass.data[DOMAIN][entry.entry_id][MONOPRICE_OBJECT]
         hass.data[DOMAIN].pop(entry.entry_id)
 
-    return unload_ok
+    await hass.async_add_executor_job(_cleanup)
+
+    return True
 
 
 async def _update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
