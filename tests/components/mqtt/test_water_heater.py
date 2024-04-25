7161f0b230edf210ba502de988bdf105dc851177
jbouwh <jan@jbsoft.nl>
2024-04-25 22:16:41 +0000

    Remove unneeded freezer

--- tests/components/mqtt/test_water_heater.py
+++ tests/components/mqtt/test_water_heater.py
@@ -1044,7 +1044,7 @@ async def test_entity_id_update_discovery_update(
 
 
 async def test_entity_debug_info_message(
-    hass: HomeAssistant, mqtt_mock_entry: MqttMockHAClientGenerator, freezer
+    hass: HomeAssistant, mqtt_mock_entry: MqttMockHAClientGenerator
 ) -> None:
     """Test MQTT debug info."""
     config = {
