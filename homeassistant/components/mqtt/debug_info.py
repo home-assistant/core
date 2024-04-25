c1922754c689b410c43c9891031504746297fb26
jbouwh <jan@jbsoft.nl>
2024-04-25 21:41:57 +0000

    Use time.monotonic() for mqtt message timestamps

--- homeassistant/components/mqtt/debug_info.py
+++ homeassistant/components/mqtt/debug_info.py
@@ -7,12 +7,12 @@ from collections.abc import Callable
 from dataclasses import dataclass
 import datetime as dt
 from functools import wraps
+import time
 from typing import TYPE_CHECKING, Any
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.typing import DiscoveryInfoType
-from homeassistant.util import dt as dt_util
 
 from .const import ATTR_DISCOVERY_PAYLOAD, ATTR_DISCOVERY_TOPIC
 from .models import MessageCallbackType, PublishPayloadType
@@ -57,7 +57,7 @@ class TimestampedPublishMessage:
     payload: PublishPayloadType
     qos: int
     retain: bool
-    timestamp: dt.datetime
+    timestamp: float
 
 
 def log_message(
@@ -77,7 +77,7 @@ def log_message(
             "messages": deque([], STORED_MESSAGES),
         }
     msg = TimestampedPublishMessage(
-        topic, payload, qos, retain, timestamp=dt_util.utcnow()
+        topic, payload, qos, retain, timestamp=time.monotonic()
     )
     entity_info["transmitted"][topic]["messages"].append(msg)
 
@@ -175,6 +175,7 @@ def remove_trigger_discovery_data(
 
 def _info_for_entity(hass: HomeAssistant, entity_id: str) -> dict[str, Any]:
     entity_info = get_mqtt_data(hass).debug_info_entities[entity_id]
+    monotonic_time_diff = time.time() - time.monotonic()
     subscriptions = [
         {
             "topic": topic,
@@ -183,7 +184,9 @@ def _info_for_entity(hass: HomeAssistant, entity_id: str) -> dict[str, Any]:
                     "payload": str(msg.payload),
                     "qos": msg.qos,
                     "retain": msg.retain,
-                    "time": msg.timestamp,
+                    "time": dt.datetime.fromtimestamp(
+                        msg.timestamp + monotonic_time_diff
+                    ),
                     "topic": msg.topic,
                 }
                 for msg in subscription["messages"]
@@ -199,7 +202,9 @@ def _info_for_entity(hass: HomeAssistant, entity_id: str) -> dict[str, Any]:
                     "payload": str(msg.payload),
                     "qos": msg.qos,
                     "retain": msg.retain,
-                    "time": msg.timestamp,
+                    "time": dt.datetime.fromtimestamp(
+                        msg.timestamp + monotonic_time_diff
+                    ),
                     "topic": msg.topic,
                 }
                 for msg in subscription["messages"]
