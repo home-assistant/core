d0966610d4239c6884fb7ed0e3e508f1cdae1d4d
J. Nick Koston <nick@koston.org>
2024-04-27 15:25:50 -0500

    Remove unneeded TYPE_CHECKING guard in core async_set

--- homeassistant/core.py
+++ homeassistant/core.py
@@ -2266,8 +2266,6 @@ class StateMachine:
             return
 
         if context is None:
-            if TYPE_CHECKING:
-                assert timestamp is not None
             context = Context(id=ulid_at_time(timestamp))
 
         if same_attr:
