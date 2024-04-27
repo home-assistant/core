98a5fbf81b7e49a2f135663a4b82b1800d530231
IceBotYT <34712694+IceBotYT@users.noreply.github.com>
2024-04-27 22:23:12 +0000

    Fix to use base entity

--- homeassistant/components/linear_garage_door/light.py
+++ homeassistant/components/linear_garage_door/light.py
@@ -9,13 +9,11 @@ from linear_garage_door import Linear
 from homeassistant.components.light import ATTR_BRIGHTNESS, ColorMode, LightEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 from .coordinator import LinearUpdateCoordinator
+from .entity import LinearEntity
 
 SCAN_INTERVAL = timedelta(seconds=60)
 PARALLEL_UPDATES = 0
@@ -34,100 +32,52 @@ async def async_setup_entry(
     async_add_entities(
         LinearLightEntity(
             device_id=device_id,
-            device_name=data[device_id]["name"],
-            subdevice=subdev,
-            config_entry=config_entry,
+            device_name=data[device_id].name,
+            sub_device_id=subdev,
             coordinator=coordinator,
         )
         for device_id in data
-        for subdev in data[device_id]["subdevices"]
+        for subdev in data[device_id].subdevices
         if subdev in SUPPORTED_SUBDEVICES
     )
 
 
-class LinearLightEntity(CoordinatorEntity[LinearUpdateCoordinator], LightEntity):
+class LinearLightEntity(LinearEntity, LightEntity):
     """Light for Linear devices."""
 
     _attr_color_mode = ColorMode.BRIGHTNESS
     _attr_supported_color_modes = {ColorMode.BRIGHTNESS}
 
-    def __init__(
-        self,
-        device_id: str,
-        device_name: str,
-        subdevice: str,
-        config_entry: ConfigEntry,
-        coordinator: LinearUpdateCoordinator,
-    ) -> None:
-        """Initialize the light."""
-        super().__init__(coordinator)
-
-        self._attr_has_entity_name = True
-        self._attr_name = subdevice
-        self._attr_unique_id = f"{device_id}-{subdevice}"
-        self._config_entry = config_entry
-        self._device_id = device_id
-        self._subdevice = subdevice
-        self.device_info = DeviceInfo(
-            identifiers={(DOMAIN, self._device_id)},
-            name=device_name,
-            manufacturer="Linear",
-            model="Garage Door Opener",
-        )
-
     @property
     def is_on(self) -> bool:
         """Return if the light is on or not."""
-        return bool(
-            self.coordinator.data[self._device_id]["subdevices"][self._subdevice][
-                "On_B"
-            ]
-            == "true"
-        )
+        return bool(self.sub_device["On_B"] == "true")
 
     @property
     def brightness(self) -> int | None:
         """Return the brightness of the light."""
-        return int(
-            int(
-                self.coordinator.data[self._device_id]["subdevices"][self._subdevice][
-                    "On_P"
-                ]
-            )
-            / 100
-            * 255
-        )
+        return int(int(self.sub_device["On_P"]) / 100 * 255)
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn on the light."""
 
-        linear = Linear()
-        await linear.login(
-            email=self._config_entry.data["email"],
-            password=self._config_entry.data["password"],
-            device_id=self._config_entry.data["device_id"],
-            client_session=async_get_clientsession(self.hass),
-        )
+        async def _turn_on(linear: Linear) -> None:
+            """Turn on the light."""
+            if not kwargs:
+                await linear.operate_device(self._device_id, self._sub_device_id, "On")
+            elif ATTR_BRIGHTNESS in kwargs:
+                brightness = str(math.floor((kwargs[ATTR_BRIGHTNESS] / 255) * 100))
+                await linear.operate_device(
+                    self._device_id, self._sub_device_id, f"DimPercent:{brightness}"
+                )
 
-        if not kwargs:
-            await linear.operate_device(self._device_id, self._subdevice, "On")
-        elif ATTR_BRIGHTNESS in kwargs:
-            brightness = str(math.floor((kwargs[ATTR_BRIGHTNESS] / 255) * 100))
-            await linear.operate_device(
-                self._device_id, self._subdevice, f"DimPercent:{brightness}"
-            )
-
-        await linear.close()
+        await self.coordinator.execute(_turn_on)
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the light."""
 
-        linear = Linear()
-        await linear.login(
-            email=self._config_entry.data["email"],
-            password=self._config_entry.data["password"],
-            device_id=self._config_entry.data["device_id"],
-            client_session=async_get_clientsession(self.hass),
+        await self.coordinator.execute(
+            lambda linear: linear.operate_device(
+                self._device_id, self._sub_device_id, "Off"
+            )
         )
-        await linear.operate_device(self._device_id, self._subdevice, "Off")
-        await linear.close()
