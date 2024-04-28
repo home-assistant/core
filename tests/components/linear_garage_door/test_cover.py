60ee8e7393e2c9c73602091e4b85f1f4e7227cad
Joostlek <joostlek@outlook.com>
2024-04-28 15:38:25 +0200

    Refactor Linear tests

--- tests/components/linear_garage_door/test_cover.py
+++ tests/components/linear_garage_door/test_cover.py
@@ -1,7 +1,9 @@
 """Test Linear Garage Door cover."""
 
+from datetime import timedelta
 from unittest.mock import AsyncMock
 
+from freezegun.api import FrozenDateTimeFactory
 from syrupy import SnapshotAssertion
 
 from homeassistant.components.cover import (
@@ -9,13 +11,26 @@ from homeassistant.components.cover import (
     SERVICE_CLOSE_COVER,
     SERVICE_OPEN_COVER,
 )
-from homeassistant.const import ATTR_ENTITY_ID, Platform
+from homeassistant.components.linear_garage_door import DOMAIN
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    STATE_CLOSED,
+    STATE_CLOSING,
+    STATE_OPEN,
+    STATE_OPENING,
+    Platform,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
 from . import setup_integration
 
-from tests.common import MockConfigEntry, snapshot_platform
+from tests.common import (
+    MockConfigEntry,
+    async_fire_time_changed,
+    load_json_object_fixture,
+    snapshot_platform,
+)
 
 
 async def test_covers(
@@ -82,3 +97,28 @@ async def test_close_cover(
     )
 
     assert mock_linear.operate_device.call_count == 1
+
+
+async def test_update_cover_state(
+    hass: HomeAssistant,
+    mock_linear: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test that closing the cover works as intended."""
+
+    await setup_integration(hass, mock_config_entry, [Platform.COVER])
+
+    assert hass.states.get("cover.test_garage_1").state == STATE_OPEN
+    assert hass.states.get("cover.test_garage_2").state == STATE_CLOSED
+
+    device_states = load_json_object_fixture("get_device_state_1.json", DOMAIN)
+    mock_linear.get_device_state.side_effect = lambda device_id: device_states[
+        device_id
+    ]
+
+    freezer.tick(timedelta(seconds=60))
+    async_fire_time_changed(hass)
+
+    assert hass.states.get("cover.test_garage_1").state == STATE_CLOSING
+    assert hass.states.get("cover.test_garage_2").state == STATE_OPENING
