51617d2768e1a45ebc3deda3389a179dbcb12cf4
Allen Porter <allen.porter@gmail.com>
2024-04-29 00:56:35 +0000

    Add Google Gen AI Conversation Agent Entity

---
+++ homeassistant/components/google_generative_ai_conversation/conversation.py
@@ -0,0 +1,167 @@
+"""Conversation support for the Google Generative AI Conversation integration."""
+
+from __future__ import annotations
+
+import logging
+from typing import Literal
+
+from google.api_core.exceptions import ClientError
+import google.generativeai as genai
+import google.generativeai.types as genai_types
+
+from homeassistant.components import assist_pipeline, conversation
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import MATCH_ALL
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import TemplateError
+from homeassistant.helpers import intent, template
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.util import ulid
+
+from .const import (
+    CONF_CHAT_MODEL,
+    CONF_MAX_TOKENS,
+    CONF_PROMPT,
+    CONF_TEMPERATURE,
+    CONF_TOP_K,
+    CONF_TOP_P,
+    DEFAULT_CHAT_MODEL,
+    DEFAULT_MAX_TOKENS,
+    DEFAULT_PROMPT,
+    DEFAULT_TEMPERATURE,
+    DEFAULT_TOP_K,
+    DEFAULT_TOP_P,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up conversation entities."""
+    agent = GoogleGenerativeAIAgent(hass, config_entry)
+    async_add_entities([agent])
+
+
+class GoogleGenerativeAIAgent(
+    conversation.ConversationEntity, conversation.AbstractConversationAgent
+):
+    """Google Generative AI conversation agent."""
+
+    _attr_has_entity_name = True
+
+    def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
+        """Initialize the agent."""
+        self.hass = hass
+        self.entry = entry
+        self.history: dict[str, list[genai_types.ContentType]] = {}
+        self._attr_name = entry.title
+        self._attr_unique_id = entry.entry_id
+
+    async def async_added_to_hass(self) -> None:
+        """When entity is added to Home Assistant."""
+        await super().async_added_to_hass()
+        assist_pipeline.async_migrate_engine(
+            self.hass, "conversation", self.entry.entry_id, self.entity_id
+        )
+        conversation.async_set_agent(self.hass, self.entry, self)
+
+    async def async_will_remove_from_hass(self) -> None:
+        """When entity will be removed from Home Assistant."""
+        conversation.async_unset_agent(self.hass, self.entry)
+        await super().async_will_remove_from_hass()
+
+    @property
+    def supported_languages(self) -> list[str] | Literal["*"]:
+        """Return a list of supported languages."""
+        return MATCH_ALL
+
+    async def async_process(
+        self, user_input: conversation.ConversationInput
+    ) -> conversation.ConversationResult:
+        """Process a sentence."""
+        raw_prompt = self.entry.options.get(CONF_PROMPT, DEFAULT_PROMPT)
+        model = genai.GenerativeModel(
+            model_name=self.entry.options.get(CONF_CHAT_MODEL, DEFAULT_CHAT_MODEL),
+            generation_config={
+                "temperature": self.entry.options.get(
+                    CONF_TEMPERATURE, DEFAULT_TEMPERATURE
+                ),
+                "top_p": self.entry.options.get(CONF_TOP_P, DEFAULT_TOP_P),
+                "top_k": self.entry.options.get(CONF_TOP_K, DEFAULT_TOP_K),
+                "max_output_tokens": self.entry.options.get(
+                    CONF_MAX_TOKENS, DEFAULT_MAX_TOKENS
+                ),
+            },
+        )
+        _LOGGER.debug("Model: %s", model)
+
+        if user_input.conversation_id in self.history:
+            conversation_id = user_input.conversation_id
+            messages = self.history[conversation_id]
+        else:
+            conversation_id = ulid.ulid_now()
+            messages = [{}, {}]
+
+        intent_response = intent.IntentResponse(language=user_input.language)
+        try:
+            prompt = self._async_generate_prompt(raw_prompt)
+        except TemplateError as err:
+            _LOGGER.error("Error rendering prompt: %s", err)
+            intent_response.async_set_error(
+                intent.IntentResponseErrorCode.UNKNOWN,
+                f"Sorry, I had a problem with my template: {err}",
+            )
+            return conversation.ConversationResult(
+                response=intent_response, conversation_id=conversation_id
+            )
+
+        messages[0] = {"role": "user", "parts": prompt}
+        messages[1] = {"role": "model", "parts": "Ok"}
+
+        _LOGGER.debug("Input: '%s' with history: %s", user_input.text, messages)
+
+        chat = model.start_chat(history=messages)
+        try:
+            chat_response = await chat.send_message_async(user_input.text)
+        except (
+            ClientError,
+            ValueError,
+            genai_types.BlockedPromptException,
+            genai_types.StopCandidateException,
+        ) as err:
+            _LOGGER.error("Error sending message: %s", err)
+            intent_response.async_set_error(
+                intent.IntentResponseErrorCode.UNKNOWN,
+                f"Sorry, I had a problem talking to Google Generative AI: {err}",
+            )
+            return conversation.ConversationResult(
+                response=intent_response, conversation_id=conversation_id
+            )
+
+        _LOGGER.debug("Response: %s", chat_response.parts)
+        if not chat_response.parts:
+            intent_response.async_set_error(
+                intent.IntentResponseErrorCode.UNKNOWN,
+                "Sorry, I had a problem talking to Google Generative AI. Likely blocked",
+            )
+            return conversation.ConversationResult(
+                response=intent_response, conversation_id=conversation_id
+            )
+        self.history[conversation_id] = chat.history
+        intent_response.async_set_speech(chat_response.text)
+        return conversation.ConversationResult(
+            response=intent_response, conversation_id=conversation_id
+        )
+
+    def _async_generate_prompt(self, raw_prompt: str) -> str:
+        """Generate a prompt for the user."""
+        return template.Template(raw_prompt, self.hass).async_render(
+            {
+                "ha_name": self.hass.config.location_name,
+            },
+            parse_result=False,
+        )
