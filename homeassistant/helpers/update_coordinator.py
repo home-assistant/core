2a5f607efa026fc8af8e212add2fce03ef9531c7
G Johansson <goran.johansson@shiftit.se>
2024-04-29 16:40:18 +0000

    Dont use coordinator for Button and Event

--- homeassistant/helpers/update_coordinator.py
+++ homeassistant/helpers/update_coordinator.py
@@ -17,6 +17,8 @@ import requests
 from typing_extensions import TypeVar
 
 from homeassistant import config_entries
+from homeassistant.components.button import ButtonEntity
+from homeassistant.components.event import EventEntity
 from homeassistant.const import EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import CALLBACK_TYPE, Event, HomeAssistant, callback
 from homeassistant.exceptions import (
@@ -465,12 +467,22 @@ class TimestampDataUpdateCoordinator(DataUpdateCoordinator[_DataT]):
 class BaseCoordinatorEntity(entity.Entity, Generic[_BaseDataUpdateCoordinatorT]):
     """Base class for all Coordinator entities."""
 
+    _no_use_coordinator = False
+
     def __init__(
         self, coordinator: _BaseDataUpdateCoordinatorT, context: Any = None
     ) -> None:
         """Create the entity with a DataUpdateCoordinator."""
         self.coordinator = coordinator
         self.coordinator_context = context
+
+    def __init_subclass__(cls, **kwargs: Any) -> None:
+        """Post initialisation processing."""
+        super().__init_subclass__(**kwargs)
+        # ButtonEntity and EventEntity provide their own states
+        # and should not use the coordinator for state updates.
+        if ButtonEntity in cls.__mro__ or EventEntity in cls.__mro__:
+            cls._no_use_coordinator = True
 
     @property
     def should_poll(self) -> bool:
@@ -480,11 +492,12 @@ class BaseCoordinatorEntity(entity.Entity, Generic[_BaseDataUpdateCoordinatorT])
     async def async_added_to_hass(self) -> None:
         """When entity is added to hass."""
         await super().async_added_to_hass()
-        self.async_on_remove(
-            self.coordinator.async_add_listener(
-                self._handle_coordinator_update, self.coordinator_context
+        if self._no_use_coordinator:
+            self.async_on_remove(
+                self.coordinator.async_add_listener(
+                    self._handle_coordinator_update, self.coordinator_context
+                )
             )
-        )
 
     @callback
     def _handle_coordinator_update(self) -> None:
