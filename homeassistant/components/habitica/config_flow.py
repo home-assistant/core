e33ae7c1fd92a513b98ded5994788c21cefc20d8
tr4nt0r <manni@zapto.de>
2024-04-30 16:44:01 +0000

    add login/password authentication

--- homeassistant/components/habitica/config_flow.py
+++ homeassistant/components/habitica/config_flow.py
@@ -2,14 +2,22 @@
 
 from __future__ import annotations
 
+from http import HTTPStatus
 import logging
+from typing import Any
 
 from aiohttp import ClientResponseError
 from habitipy.aio import HabitipyAsync
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow
-from homeassistant.const import CONF_API_KEY, CONF_NAME, CONF_URL
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.const import (
+    CONF_API_KEY,
+    CONF_NAME,
+    CONF_PASSWORD,
+    CONF_URL,
+    CONF_USERNAME,
+)
 from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
@@ -17,12 +25,19 @@ from homeassistant.helpers.issue_registry import IssueSeverity, async_create_iss
 
 from .const import CONF_API_USER, DEFAULT_URL, DOMAIN
 
-DATA_SCHEMA = vol.Schema(
+STEP_API_CREDENTIALS_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_API_USER): str,
         vol.Required(CONF_API_KEY): str,
         vol.Optional(CONF_NAME): str,
         vol.Optional(CONF_URL, default=DEFAULT_URL): str,
+    }
+)
+
+STEP_LOGIN_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_USERNAME): str,
+        vol.Required(CONF_PASSWORD): str,
     }
 )
 
@@ -47,7 +62,9 @@ async def validate_input(hass: HomeAssistant, data: dict[str, str]) -> dict[str,
             CONF_API_USER: data[CONF_API_USER],
         }
     except ClientResponseError as ex:
-        raise InvalidAuth from ex
+        if ex.status == HTTPStatus.UNAUTHORIZED:
+            raise InvalidAuth from ex
+        raise CannotConnect from ex
 
 
 class HabiticaConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -62,6 +79,8 @@ class HabiticaConfigFlow(ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             try:
                 info = await validate_input(self.hass, user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
             except InvalidAuth:
                 errors = {"base": "invalid_credentials"}
             except Exception:  # pylint: disable=broad-except
@@ -71,11 +90,66 @@ class HabiticaConfigFlow(ConfigFlow, domain=DOMAIN):
                 await self.async_set_unique_id(info[CONF_API_USER])
                 self._abort_if_unique_id_configured()
                 return self.async_create_entry(title=info["title"], data=user_input)
-        return self.async_show_form(
+        return self.async_show_menu(
             step_id="user",
-            data_schema=DATA_SCHEMA,
+            menu_options=["login", "api_credentials"],
+        )
+        # return self.async_show_form(
+        #     step_id="user",
+        #     data_schema=DATA_SCHEMA,
+        #     errors=errors,
+        #     description_placeholders={},
+        # )
+
+    async def async_step_login(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Fetch API credentials by authenticating with login and password."""
+        errors: dict[str, str] = {}
+
+        if user_input is not None:
+            try:
+                session = async_get_clientsession(self.hass)
+                api = HabitipyAsync(
+                    conf={
+                        "login": "",
+                        "password": "",
+                        "url": DEFAULT_URL,
+                    }
+                )
+                login_response = await api.user.auth.local.login.post(
+                    session=session,
+                    username=user_input[CONF_USERNAME],
+                    password=user_input[CONF_PASSWORD],
+                )
+
+            except ClientResponseError as ex:
+                if ex.status == HTTPStatus.UNAUTHORIZED:
+                    errors = {"base": "invalid_credentials"}
+                else:
+                    errors["base"] = "cannot_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors = {"base": "unknown"}
+            else:
+                await self.async_set_unique_id(login_response["id"])
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(
+                    title=login_response["username"],
+                    data={
+                        CONF_API_USER: login_response["id"],
+                        CONF_API_KEY: login_response["apiToken"],
+                        CONF_USERNAME: login_response["username"],
+                        CONF_URL: DEFAULT_URL,
+                    },
+                )
+
+        return self.async_show_form(
+            step_id="login",
+            data_schema=self.add_suggested_values_to_schema(
+                data_schema=STEP_LOGIN_DATA_SCHEMA, suggested_values=user_input
+            ),
             errors=errors,
-            description_placeholders={},
         )
 
     async def async_step_import(self, import_data):
@@ -99,3 +173,7 @@ class HabiticaConfigFlow(ConfigFlow, domain=DOMAIN):
 
 class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
