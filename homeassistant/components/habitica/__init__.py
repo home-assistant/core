c319bf0b741c8c04e09f8b2e8bc6babbdd71adcc
tr4nt0r <manni@zapto.de>
2024-05-01 19:56:49 +0000

    remove username from entry

--- homeassistant/components/habitica/__init__.py
+++ homeassistant/components/habitica/__init__.py
@@ -22,10 +22,12 @@ from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.selector import ConfigEntrySelector
 from homeassistant.helpers.typing import ConfigType
 
 from .const import (
     ATTR_ARGS,
+    ATTR_CONFIG_ENTRY,
     ATTR_DATA,
     ATTR_PATH,
     CONF_API_USER,
@@ -87,7 +89,7 @@ PLATFORMS = [Platform.SENSOR]
 
 SERVICE_API_CALL_SCHEMA = vol.Schema(
     {
-        vol.Required(ATTR_NAME): str,
+        vol.Required(ATTR_CONFIG_ENTRY): ConfigEntrySelector(),
         vol.Required(ATTR_PATH): vol.All(cv.ensure_list, [str]),
         vol.Optional(ATTR_ARGS): dict,
     }
@@ -124,6 +126,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: HabiticaConfigEntry) -> 
         name = call.data[ATTR_NAME]
         path = call.data[ATTR_PATH]
         entries = hass.config_entries.async_entries(DOMAIN)
+
         api = None
         for entry in entries:
             if entry.data[CONF_NAME] == name:
--- homeassistant/components/habitica/config_flow.py
+++ homeassistant/components/habitica/config_flow.py
@@ -99,7 +99,6 @@ class HabiticaConfigFlow(ConfigFlow, domain=DOMAIN):
                     data={
                         CONF_API_USER: login_response["id"],
                         CONF_API_KEY: login_response["apiToken"],
-                        CONF_USERNAME: login_response["username"],
                         CONF_URL: DEFAULT_URL,
                         CONF_VERIFY_SSL: True,
                     },
@@ -149,10 +148,8 @@ class HabiticaConfigFlow(ConfigFlow, domain=DOMAIN):
             else:
                 await self.async_set_unique_id(user_input[CONF_API_USER])
                 self._abort_if_unique_id_configured()
-                user_input[CONF_USERNAME] = api_response["auth"]["local"]["username"]
-                return self.async_create_entry(
-                    title=user_input[CONF_USERNAME], data=user_input
-                )
+                title = api_response["auth"]["local"]["username"]
+                return self.async_create_entry(title=title, data=user_input)
 
         return self.async_show_form(
             step_id="advanced",
