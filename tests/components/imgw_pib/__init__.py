36ac238a1b0a8b7a713278b35158eada9b0d36b8
Maciej Bieniek <478555+bieniu@users.noreply.github.com>
2024-05-01 13:42:07 +0200

    Add missing async_block_till_done

--- tests/components/imgw_pib/__init__.py
+++ tests/components/imgw_pib/__init__.py
@@ -8,3 +8,4 @@ async def init_integration(hass, config_entry: MockConfigEntry) -> MockConfigEnt
     config_entry.add_to_hass(hass)
 
     await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
