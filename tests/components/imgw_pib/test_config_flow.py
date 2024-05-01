dd1cf92f61cedcfbe32571c6390c2c553b74db45
Maciej Bieniek <478555+bieniu@users.noreply.github.com>
2024-05-01 13:39:41 +0200

    Test creating entry after error

--- tests/components/imgw_pib/test_config_flow.py
+++ tests/components/imgw_pib/test_config_flow.py
@@ -60,6 +60,7 @@ async def test_form_with_exceptions(
     hass: HomeAssistant,
     exc: Exception,
     base_error: str,
+    mock_setup_entry: AsyncMock,
     mock_imgw_pib_client: AsyncMock,
 ) -> None:
     """Test we get the form."""
@@ -78,3 +79,16 @@ async def test_form_with_exceptions(
 
     assert result["type"] is FlowResultType.FORM
     assert result["errors"] == {"base": base_error}
+
+    mock_imgw_pib_client.get_hydrological_data.side_effect = None
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_STATION_ID: "123"},
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == "River Name (Station Name)"
+    assert result["data"] == {CONF_STATION_ID: "123"}
+    assert len(mock_setup_entry.mock_calls) == 1
