a4324e4c5a9628e8bdb439115d111881a1d8c3f9
Thomas Kistler <thomas@uyei-kistler.com>
2024-05-01 05:14:35 +0000

    Add snapshot tests

--- tests/components/hydrawise/test_sensor.py
+++ tests/components/hydrawise/test_sensor.py
@@ -1,34 +1,33 @@
 """Test Hydrawise sensor."""
 
 from collections.abc import Awaitable, Callable
+from unittest.mock import patch
 
-from freezegun.api import FrozenDateTimeFactory
 from pydrawise.schema import Controller, Zone
 import pytest
+from syrupy.assertion import SnapshotAssertion
 
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, snapshot_platform
 
 
 @pytest.mark.freeze_time("2023-10-01 00:00:00+00:00")
-async def test_states(
+async def test_all_sensors(
     hass: HomeAssistant,
-    mock_added_config_entry: MockConfigEntry,
-    freezer: FrozenDateTimeFactory,
+    mock_add_config_entry: Callable[[], Awaitable[MockConfigEntry]],
+    entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
 ) -> None:
-    """Test sensor states."""
-    watering_time1 = hass.states.get("sensor.zone_one_watering_time")
-    assert watering_time1 is not None
-    assert watering_time1.state == "0"
-
-    watering_time2 = hass.states.get("sensor.zone_two_watering_time")
-    assert watering_time2 is not None
-    assert watering_time2.state == "29"
-
-    next_cycle = hass.states.get("sensor.zone_one_next_cycle")
-    assert next_cycle is not None
-    assert next_cycle.state == "2023-10-04T19:49:57+00:00"
+    """Test that all sensors are working."""
+    with patch(
+        "homeassistant.components.hydrawise.PLATFORMS",
+        [Platform.SENSOR],
+    ):
+        config_entry = await mock_add_config_entry()
+        await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
 
 
 @pytest.mark.freeze_time("2023-10-01 00:00:00+00:00")
@@ -44,39 +43,6 @@ async def test_suspended_state(
     next_cycle = hass.states.get("sensor.zone_one_next_cycle")
     assert next_cycle is not None
     assert next_cycle.state == "9999-12-31T23:59:59+00:00"
-
-
-async def test_sensor_and_water_state(
-    hass: HomeAssistant,
-    controller: Controller,
-    mock_add_config_entry: Callable[[], Awaitable[MockConfigEntry]],
-) -> None:
-    """Test rain sensor, flow sensor, and water use in the presence of flow and rain sensors."""
-    await mock_add_config_entry()
-
-    sensor = hass.states.get("sensor.zone_one_daily_active_water_use")
-    assert sensor is not None
-    assert sensor.state == "454.6279552584"
-
-    sensor = hass.states.get("sensor.zone_two_daily_active_water_use")
-    assert sensor is not None
-    assert sensor.state == "0.0"
-
-    sensor = hass.states.get("sensor.home_controller_daily_active_water_use")
-    assert sensor is not None
-    assert sensor.state == "1259.0279593584"
-
-    sensor = hass.states.get("sensor.home_controller_daily_inactive_water_use")
-    assert sensor is not None
-    assert sensor.state == "49.210353192"
-
-    sensor = hass.states.get("binary_sensor.home_controller_rain_sensor")
-    assert sensor is not None
-    assert sensor.state == "off"
-
-    sensor = hass.states.get("binary_sensor.home_controller_connectivity")
-    assert sensor is not None
-    assert sensor.state == "on"
 
 
 async def test_no_sensor_and_water_state2(
