bdf8d4c8332c44aac74651d0fbafa28845a11404
Denis Shulyaka <Shulyaka@gmail.com>
2024-05-03 01:37:44 +0300

    Allow a Python function to be registered as am LLM tool

--- homeassistant/helpers/llm.py
+++ homeassistant/helpers/llm.py
@@ -6,9 +6,10 @@ from abc import abstractmethod
 from collections.abc import Callable, Iterable
 from dataclasses import dataclass
 from functools import cached_property
+import inspect
 import logging
-from types import FunctionType
-from typing import Any
+from types import FunctionType, NoneType, UnionType
+from typing import Any, get_args, get_type_hints
 
 import voluptuous as vol
 from voluptuous_openapi import UNSUPPORTED, convert
@@ -67,14 +68,14 @@ class Tool:
     """LLM Tool base class."""
 
     name: str
-    description: str
+    description: str | None
     parameters: vol.Schema
     custom_serializer: Callable[[Any], Any]
 
     def __init__(
         self,
         name: str,
-        description: str,
+        description: str | None,
         parameters: vol.Schema = vol.Schema({}),
         custom_serializer: Callable[[Any], Any] = default_custom_serializer,
     ) -> None:
@@ -87,13 +88,13 @@ class Tool:
     @cached_property
     def specification(self) -> dict[str, Any]:
         """Get the tool specification in OpenAPI-compatible format."""
-        return {
-            "name": self.name,
-            "description": self.description,
-            "parameters": convert(
-                self.parameters, custom_serializer=self.custom_serializer
-            ),
-        }
+        result = {"name": self.name}
+        if self.description:
+            result["description"] = self.description
+        result["parameters"] = convert(
+            self.parameters, custom_serializer=self.custom_serializer
+        )
+        return result
 
     @abstractmethod
     async def async_call(self, hass: HomeAssistant, tool_input: ToolInput) -> Any:
@@ -107,52 +108,6 @@ class Tool:
     def __repr__(self) -> str:
         """Represent a string of a Tool."""
         return f"<{self.__class__.__name__} - {self.name}>"
-
-
-@callback
-def async_register_tool(hass: HomeAssistant, tool: Tool) -> None:
-    """Register an LLM tool with Home Assistant."""
-    if (tools := hass.data.get(DATA_KEY)) is None:
-        tools = hass.data[DATA_KEY] = {}
-
-    if tool.name in tools:
-        raise HomeAssistantError(f"Tool {tool.name} is already registered")
-
-    tools[tool.name] = tool
-
-
-@callback
-def async_remove_tool(hass: HomeAssistant, tool_name: str) -> None:
-    """Remove an LLM tool from Home Assistant."""
-    if (tools := hass.data.get(DATA_KEY)) is None:
-        return
-
-    tools.pop(tool_name, None)
-
-
-@callback
-def async_get_tools(hass: HomeAssistant) -> Iterable[Tool]:
-    """Return a list of registered LLM tools."""
-    if (tools := hass.data.get(DATA_KEY)) is None:
-        tools = {}
-
-    return tools.values()
-
-
-@callback
-async def async_call_tool(hass: HomeAssistant, tool_input: ToolInput) -> Any:
-    """Call a LLM tool, validate args and return the response."""
-    if (tools := hass.data.get(DATA_KEY)) is None:
-        tools = {}
-
-    tool = tools[tool_input.tool_name]
-
-    if tool_input.context is None:
-        tool_input.context = Context()
-
-    tool_input.tool_args = tool.parameters(tool_input.tool_args)
-
-    return await tool.async_call(hass, tool_input)
 
 
 class IntentTool(Tool):
@@ -238,3 +193,146 @@ class IntentTool(Tool):
             tool_input.assistant,
         )
         return intent_response.as_dict()
+
+
+class FunctionTool(Tool):
+    """LLM Tool representing an Python function.
+
+    The function is recommended to have annotations for all parameters.
+    If a parameter name is "hass", "tool_input", or any of the ToolInput attributes,
+    then the value for that parameter will be provided by the conversation agent.
+    All other arguments will be provided by the LLM.
+    """
+
+    function: Callable
+
+    def __init__(
+        self,
+        function: Callable,
+    ) -> None:
+        """Init the class."""
+
+        self.function = function
+
+        name = function.__name__
+        if name.startswith("async_"):
+            name = name[len("async_") :]
+
+        description = inspect.getdoc(function)
+
+        schema = {}
+        annotations = get_type_hints(function)
+        for param in inspect.signature(function).parameters.values():
+            if param.name == "hass":
+                continue
+            if param.name == "tool_input":
+                continue
+            if hasattr(ToolInput, param.name):
+                continue
+
+            hint = annotations.get(param.name, str)
+            if isinstance(hint, UnionType):
+                hints = get_args(hint)
+                if len(hints) == 2 and hints[0] is NoneType:
+                    hint = vol.Maybe(hints[1])
+                elif len(hints) == 2 and hints[1] is NoneType:
+                    hint = vol.Maybe(hints[0])
+                else:
+                    hint = vol.Any(
+                        *tuple(x if x is not NoneType else None for x in hints)
+                    )
+
+            schema[
+                vol.Required(param.name)
+                if param.default is inspect.Parameter.empty
+                else vol.Optional(param.name, default=param.default)
+            ] = hint
+
+        super().__init__(
+            name,
+            description,
+            vol.Schema(schema),
+        )
+
+    async def async_call(self, hass: HomeAssistant, tool_input: ToolInput) -> Any:
+        """Call the function."""
+        kwargs = tool_input.tool_args
+        for parameter in inspect.signature(self.function).parameters.values():
+            if parameter.name == "hass":
+                kwargs["hass"] = hass
+            elif parameter.name == "tool_input":
+                kwargs["tool_input"] = tool_input
+            elif hasattr(ToolInput, parameter.name):
+                kwargs[parameter.name] = getattr(tool_input, parameter.name)
+
+        if inspect.iscoroutinefunction(self.function):
+            return await self.function(**kwargs)
+        return self.function(**kwargs)
+
+
+@callback
+def async_register_tool(hass: HomeAssistant, tool: Tool | Callable) -> None:
+    """Register an LLM tool with Home Assistant."""
+    if (tools := hass.data.get(DATA_KEY)) is None:
+        tools = hass.data[DATA_KEY] = {}
+
+    if not isinstance(tool, Tool):
+        tool = FunctionTool(tool)
+
+    if tool.name in tools:
+        raise HomeAssistantError(f"Tool {tool.name} is already registered")
+
+    tools[tool.name] = tool
+
+
+@callback
+def async_remove_tool(hass: HomeAssistant, tool: Tool | Callable | str) -> None:
+    """Remove an LLM tool from Home Assistant."""
+    if (tools := hass.data.get(DATA_KEY)) is None:
+        return
+
+    if isinstance(tool, str):
+        tool_name = tool
+    elif isinstance(tool, Tool):
+        tool_name = tool.name
+    else:
+        tool_name = tool.__name__
+        if tool_name.startswith("async_"):
+            tool_name = tool_name[len("async_") :]
+
+    tools.pop(tool_name, None)
+
+
+@callback
+def async_get_tools(hass: HomeAssistant) -> Iterable[Tool]:
+    """Return a list of registered LLM tools."""
+    if (tools := hass.data.get(DATA_KEY)) is None:
+        tools = {}
+
+    return tools.values()
+
+
+@callback
+async def async_call_tool(hass: HomeAssistant, tool_input: ToolInput) -> Any:
+    """Call a LLM tool, validate args and return the response."""
+    if (tools := hass.data.get(DATA_KEY)) is None:
+        tools = {}
+
+    tool = tools[tool_input.tool_name]
+
+    if tool_input.context is None:
+        tool_input.context = Context()
+
+    tool_input.tool_args = tool.parameters(tool_input.tool_args)
+
+    return await tool.async_call(hass, tool_input)
+
+
+def llm_tool(hass: HomeAssistant) -> Callable:
+    """Register a function as an LLM Tool with decorator."""
+
+    def _llm_tool(func: Callable) -> Callable:
+        async_register_tool(hass, func)
+        return func
+
+    return _llm_tool
--- tests/helpers/test_llm.py
+++ tests/helpers/test_llm.py
@@ -46,7 +46,7 @@ def test_async_remove(hass: HomeAssistant) -> None:
     tool = llm.Tool("test_tool", "test_description")
 
     llm.async_register_tool(hass, tool)
-    llm.async_remove_tool(hass, "test_tool")
+    llm.async_remove_tool(hass, tool)
 
     assert "test_tool" not in hass.data[llm.DATA_KEY]
     assert list(llm.async_get_tools(hass)) == []
@@ -76,10 +76,8 @@ def test_async_remove_no_existing(hass: HomeAssistant) -> None:
 
 async def test_call_tool(hass: HomeAssistant) -> None:
     """Test calling an llm tool."""
-    tool = AsyncMock()
-    tool.name = "test_tool"
-    tool.parameters = vol.Schema({"test_arg": str})
-    tool.async_call.return_value = {"result": "test_response"}
+    tool = llm.Tool("test_tool", "test_description", vol.Schema({"test_arg": str}))
+    tool.async_call = AsyncMock(return_value={"result": "test_response"})
 
     llm.async_register_tool(hass, tool)
     test_context = Context()
@@ -121,6 +119,158 @@ async def test_call_tool_no_existing(hass: HomeAssistant) -> None:
                 None,
             ),
         )
+
+
+async def test_function_tool(hass: HomeAssistant) -> None:
+    """Test function tools."""
+    call = {}
+
+    @llm.llm_tool(hass)
+    def test_async_function_tool(
+        hass: HomeAssistant,
+        tool_input: llm.ToolInput,
+        platform: str,
+        context: Context,
+        required_arg: int,
+        optional_arg: float | str = 9.6,
+    ):
+        """Test tool description."""
+        call["hass"] = hass
+        call["tool_input"] = tool_input
+        call["platform"] = platform
+        call["context"] = context
+        call["required_arg"] = required_arg
+        call["optional_arg"] = optional_arg
+        return {"result": "test_response"}
+
+    tool = list(llm.async_get_tools(hass))[0]
+    assert tool.specification == {
+        "name": "test_async_function_tool",
+        "description": "Test tool description.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "optional_arg": {
+                    "anyOf": [
+                        {
+                            "type": "number",
+                        },
+                        {
+                            "type": "string",
+                        },
+                    ],
+                    "default": 9.6,
+                },
+                "required_arg": {
+                    "type": "integer",
+                },
+            },
+            "required": ["required_arg"],
+        },
+    }
+
+    test_context = Context()
+    tool_input = llm.ToolInput(
+        tool_name="test_async_function_tool",
+        tool_args={"required_arg": 4},
+        platform="test_platform",
+        context=test_context,
+        user_prompt="test_text",
+        language="en",
+        agent_id="test_agent",
+        conversation_id="test_conversation_id",
+        device_id="test_device_id",
+        assistant="test_assistant",
+    )
+
+    response = await llm.async_call_tool(hass, tool_input)
+
+    assert response == {"result": "test_response"}
+    assert call == {
+        "context": test_context,
+        "hass": hass,
+        "optional_arg": 9.6,
+        "platform": "test_platform",
+        "required_arg": 4,
+        "tool_input": tool_input,
+    }
+
+    llm.async_remove_tool(hass, "test_async_function_tool")
+
+    assert list(llm.async_get_tools(hass)) == []
+
+
+async def test_async_function_tool(hass: HomeAssistant) -> None:
+    """Test function tools with async function."""
+    call = {}
+
+    @llm.llm_tool(hass)
+    async def async_test_async_function_tool(
+        hass: HomeAssistant,
+        tool_input: llm.ToolInput,
+        platform: str,
+        context: Context,
+        required_arg: int,
+        optional_arg: float | None = None,
+    ):
+        """Test tool description."""
+        call["hass"] = hass
+        call["tool_input"] = tool_input
+        call["platform"] = platform
+        call["context"] = context
+        call["required_arg"] = required_arg
+        call["optional_arg"] = optional_arg
+        return {"result": "test_response"}
+
+    tool = list(llm.async_get_tools(hass))[0]
+    assert tool.specification == {
+        "name": "test_async_function_tool",
+        "description": "Test tool description.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "optional_arg": {
+                    "type": "number",
+                    "nullable": True,
+                    "default": None,
+                },
+                "required_arg": {
+                    "type": "integer",
+                },
+            },
+            "required": ["required_arg"],
+        },
+    }
+
+    test_context = Context()
+    tool_input = llm.ToolInput(
+        tool_name="test_async_function_tool",
+        tool_args={"required_arg": 4},
+        platform="test_platform",
+        context=test_context,
+        user_prompt="test_text",
+        language="en",
+        agent_id="test_agent",
+        conversation_id="test_conversation_id",
+        device_id="test_device_id",
+        assistant="test_assistant",
+    )
+
+    response = await llm.async_call_tool(hass, tool_input)
+
+    assert response == {"result": "test_response"}
+    assert call == {
+        "context": test_context,
+        "hass": hass,
+        "optional_arg": None,
+        "platform": "test_platform",
+        "required_arg": 4,
+        "tool_input": tool_input,
+    }
+
+    llm.async_remove_tool(hass, async_test_async_function_tool)
+
+    assert list(llm.async_get_tools(hass)) == []
 
 
 def test_custom_serializer() -> None:
