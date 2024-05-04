5658b201fdd86b437d9888b48eb58c1b9632b322
Martijn Tielen <martijn.tielen@gmail.com>
2024-05-04 08:14:32 +0000

    Revert "Update polling interval"
    This reverts commit 43f6815f1adea868ab2c8a3fc19a82b3f1161b05.

--- homeassistant/components/wolflink/__init__.py
+++ homeassistant/components/wolflink/__init__.py
@@ -100,7 +100,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         _LOGGER,
         name=DOMAIN,
         update_method=async_update_data,
-        update_interval=timedelta(seconds=60),
+        update_interval=timedelta(seconds=90),
     )
 
     await coordinator.async_refresh()
