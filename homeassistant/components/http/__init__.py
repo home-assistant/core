3096c5e69e086c67e6282ccf9142f6287b520e28
J. Nick Koston <nick@koston.org>
2024-05-04 16:16:21 -0500

    Switch out aiohttp-isal for aiohttp-fast-zlib to make isal optional
    aiohttp-isal does not work on core installs where the system has 32bit userland and a 64bit kernel because we have no way to detect this configuration or handle it.
    
    fixes #116681

--- homeassistant/components/http/__init__.py
+++ homeassistant/components/http/__init__.py
@@ -21,7 +21,7 @@ from aiohttp.typedefs import JSONDecoder, StrOrURL
 from aiohttp.web_exceptions import HTTPMovedPermanently, HTTPRedirection
 from aiohttp.web_protocol import RequestHandler
 from aiohttp_fast_url_dispatcher import FastUrlDispatcher, attach_fast_url_dispatcher
-from aiohttp_isal import enable_isal
+import aiohttp_fast_zlib
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -201,7 +201,7 @@ class ApiConfig:
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the HTTP API and debug interface."""
-    enable_isal()
+    aiohttp_fast_zlib.enable()
 
     conf: ConfData | None = config.get(DOMAIN)
 
