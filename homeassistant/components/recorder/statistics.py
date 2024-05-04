393d528a1dcc14a1067ad3ca24e5352a26fa367b
J. Nick Koston <nick@koston.org>
2024-05-04 12:14:35 -0500

    fix typing

--- homeassistant/components/recorder/statistics.py
+++ homeassistant/components/recorder/statistics.py
@@ -488,7 +488,7 @@ def compile_statistics(instance: Recorder, start: datetime, fire_events: bool) -
     # Define modified_statistic_ids outside of the "with" statement as context managers
     # with exception suppressing prevent defining the value of the local variable, yet
     # the flow continues
-    modified_statistic_ids = None
+    modified_statistic_ids: set[str] | None = None
 
     # Return if we already have 5-minute statistics for the requested period
     with session_scope(
