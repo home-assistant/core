9d29fdb4077236b7550987624389efed786f3d56
alexyao2015 <alexyao2015@users.noreply.github.com>
2024-05-05 05:01:40 +0000

    fix

--- homeassistant/components/html5/issues.py
+++ homeassistant/components/html5/issues.py
@@ -26,7 +26,7 @@ def async_create_html5_issue(hass: HomeAssistant, import_success: bool) -> None:
     """Create issues for HTML5."""
     _LOGGER.warning(SUCCESSFUL_IMPORT_LOG if import_success else FAILED_IMPORT_LOG)
 
-    translation_key = SUCCESSFUL_IMPORT_LOG
+    translation_key = SUCCESSFUL_IMPORT_TRANSLATION_KEY
     if not import_success:
         translation_key = FAILED_IMPORT_TRANSLATION_KEY
 
