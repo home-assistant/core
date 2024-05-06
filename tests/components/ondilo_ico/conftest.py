a040892a4dea711086af1b86092137f93f29f895
Dartdoka <dartdoka@mainguet.fr>
2024-05-06 10:22:05 +0000

    Updated test cases to test through state machine

--- tests/components/ondilo_ico/conftest.py
+++ tests/components/ondilo_ico/conftest.py
@@ -1,18 +1,75 @@
 """Provide basic Ondilo fixture."""
 
+from collections.abc import Generator
+from unittest.mock import MagicMock, patch
+
 import pytest
 
-from homeassistant.components.ondilo_ico.api import OndiloClient
 from homeassistant.components.ondilo_ico.const import DOMAIN
-from homeassistant.core import HomeAssistant
+from homeassistant.util.json import JsonArrayType, JsonObjectType
 
-from tests.common import MockConfigEntry
+from tests.common import (
+    MockConfigEntry,
+    load_json_array_fixture,
+    load_json_object_fixture,
+)
 
 
-@pytest.fixture(name="ondilo_client")
-def ondilo_client(hass: HomeAssistant) -> OndiloClient:
-    """Define Mock Ondilo client."""
-    config_entry = MockConfigEntry(domain=DOMAIN, data={"token": "mocktoken"})
-    config_entry.add_to_hass(hass)
+@pytest.fixture(name="config_entry")
+def mock_config_entry() -> MockConfigEntry:
+    """Mock a config entry."""
+    return MockConfigEntry(
+        domain=DOMAIN,
+        title="Ondilo ICO",
+        data={"auth_implementation": DOMAIN, "token": {"access_token": "fake_token"}},
+    )
 
-    return OndiloClient(hass, config_entry, None)
+
+@pytest.fixture
+def mock_ondilo_client() -> Generator[MagicMock, None, None]:
+    """Mock a Homeassistant Ondilo client."""
+    with (
+        patch(
+            "homeassistant.components.ondilo_ico.api.OndiloClient",
+            autospec=True,
+        ) as mock_ondilo,
+    ):
+        yield mock_ondilo.return_value
+
+
+@pytest.fixture
+def pool1() -> JsonArrayType:
+    """First pool description."""
+    return [load_json_object_fixture("pool1.json", "ondilo_ico")]
+
+
+@pytest.fixture
+def pool2() -> JsonArrayType:
+    """Second pool description."""
+    return [load_json_object_fixture("pool2.json", "ondilo_ico")]
+
+
+@pytest.fixture
+def ico_details1() -> JsonObjectType:
+    """ICO details of first pool."""
+    return load_json_object_fixture("ico_details1.json", "ondilo_ico")
+
+
+@pytest.fixture
+def ico_details2() -> JsonObjectType:
+    """ICO details of second pool."""
+    return load_json_object_fixture("ico_details2.json", "ondilo_ico")
+
+
+@pytest.fixture
+def last_measures() -> JsonArrayType:
+    """Pool measurements."""
+    return load_json_array_fixture("last_measures.json", "ondilo_ico")
+
+
+@pytest.fixture
+def two_pools() -> JsonArrayType:
+    """Two pools description."""
+    pool1 = load_json_object_fixture("pool1.json", "ondilo_ico")
+    pool2 = load_json_object_fixture("pool2.json", "ondilo_ico")
+    return [pool1, pool2]
---
+++ tests/components/ondilo_ico/test_sensors.py
@@ -0,0 +1,112 @@
+"""Test Ondilo ICO integration sensors."""
+
+from unittest.mock import MagicMock
+
+from ondilo import OndiloError
+
+from homeassistant.core import HomeAssistant
+from homeassistant.util.json import JsonArrayType, JsonObjectType
+
+from . import setup_integration
+
+from tests.common import MockConfigEntry
+
+
+async def test_can_get_pools_when_no_error(
+    hass: HomeAssistant,
+    mock_ondilo_client: MagicMock,
+    config_entry: MockConfigEntry,
+    two_pools: JsonArrayType,
+    ico_details1: JsonObjectType,
+    ico_details2: JsonObjectType,
+    last_measures: JsonArrayType,
+) -> None:
+    """Test that I can get all pools data when no error."""
+    mock_ondilo_client.get_pools.return_value = two_pools
+    mock_ondilo_client.get_ICO_details.side_effect = [ico_details1, ico_details2]
+    mock_ondilo_client.get_last_pool_measures.return_value = last_measures
+    await setup_integration(hass, config_entry, mock_ondilo_client)
+
+    # All sensors were created
+    assert len(hass.states.async_all()) == 14
+
+    # Check 2 of the sensors.
+    assert hass.states.get("sensor.pool_1_temperature").state == next(
+        str(item["value"])
+        for item in last_measures
+        if item["data_type"] == "temperature"
+    )
+    assert hass.states.get("sensor.pool_2_rssi").state == next(
+        str(item["value"]) for item in last_measures if item["data_type"] == "rssi"
+    )
+
+
+async def test_no_ico_attached(
+    hass: HomeAssistant,
+    mock_ondilo_client: MagicMock,
+    config_entry: MockConfigEntry,
+    two_pools: JsonArrayType,
+    pool1: JsonObjectType,
+    ico_details1: JsonObjectType,
+    ico_details2: JsonObjectType,
+    last_measures: JsonArrayType,
+) -> None:
+    """Test if an ICO is not attached to a pool, then no sensor is created."""
+    # Only one pool, but no ICO attached
+    mock_ondilo_client.get_pools.return_value = pool1
+    mock_ondilo_client.get_ICO_details.return_value = None
+    mock_ondilo_client.get_last_pool_measures.return_value = last_measures
+    await setup_integration(hass, config_entry, mock_ondilo_client)
+
+    # No sensor should be created
+    assert len(hass.states.async_all()) == 0
+    # We should not have tried to retrieve pool measures
+    mock_ondilo_client.get_last_pool_measures.assert_not_called()
+
+    # Some test with 2 pools, one of them has no ICO
+    mock_ondilo_client.get_pools.return_value = two_pools
+    mock_ondilo_client.get_ICO_details.side_effect = [None, ico_details2]
+    mock_ondilo_client.get_last_pool_measures.return_value = last_measures
+
+    await hass.config_entries.async_reload(config_entry.entry_id)
+    await hass.async_block_till_done()
+    # Only the second pool is created
+    assert len(hass.states.async_all()) == 7
+    assert hass.states.get("sensor.pool_1_temperature") is None
+    assert hass.states.get("sensor.pool_2_rssi").state == next(
+        str(item["value"]) for item in last_measures if item["data_type"] == "rssi"
+    )
+
+
+async def test_error_retrieving_ico(
+    hass: HomeAssistant,
+    mock_ondilo_client: MagicMock,
+    config_entry: MockConfigEntry,
+    pool1: JsonObjectType,
+) -> None:
+    """Test if there's an error retrieving ICO data, then no sensor is created."""
+    mock_ondilo_client.get_pools.return_value = pool1
+    mock_ondilo_client.get_ICO_details.side_effect = OndiloError(400, "error")
+
+    await setup_integration(hass, config_entry, mock_ondilo_client)
+
+    # No sensor should be created
+    assert len(hass.states.async_all()) == 0
+
+
+async def test_error_retrieving_measures(
+    hass: HomeAssistant,
+    mock_ondilo_client: MagicMock,
+    config_entry: MockConfigEntry,
+    pool1: JsonObjectType,
+    ico_details1: JsonObjectType,
+) -> None:
+    """Test if there's an error retrieving measures of ICO, then no sensor is created."""
+    mock_ondilo_client.get_pools.return_value = pool1
+    mock_ondilo_client.get_ICO_details.return_value = ico_details1
+    mock_ondilo_client.get_last_pool_measures.side_effect = OndiloError(400, "error")
+
+    await setup_integration(hass, config_entry, mock_ondilo_client)
+
+    # No sensor should be created
+    assert len(hass.states.async_all()) == 0
