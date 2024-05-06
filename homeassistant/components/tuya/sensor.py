97d8ea25bc57304a105e1e7fdc3e4330c63440d1
focabr <focabr@gmail.com>
2024-05-06 10:16:12 +0000

    fix: Adjusting the variable name to match the other components

--- homeassistant/components/tuya/sensor.py
+++ homeassistant/components/tuya/sensor.py
@@ -780,7 +780,7 @@ SENSORS: dict[str, tuple[TuyaSensorEntityDescription, ...]] = {
         ),
         TuyaSensorEntityDescription(
             key=DPCode.CUR_NEUTRAL,
-            translation_key="total_return_energy",
+            translation_key="total_production",
             device_class=SensorDeviceClass.ENERGY,
             state_class=SensorStateClass.TOTAL_INCREASING,
         ),
