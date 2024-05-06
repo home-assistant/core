3261f29ce94ff89c728a6f6db51d8308c5ce5f2f
G Johansson <goran.johansson@shiftit.se>
2024-05-06 15:13:19 +0000

    Fix ruff

--- tests/components/lock/test_device_trigger.py
+++ tests/components/lock/test_device_trigger.py
@@ -502,15 +502,12 @@ async def test_if_fires_on_state_change_with_for(
                     "action": {
                         "service": "test.automation",
                         "data_template": {
-                            "some": "turn_on {{ trigger.%s }}"
-                            % "}} - {{ trigger.".join(  # noqa: FLY002
-                                (
-                                    "platform",
-                                    "entity_id",
-                                    "from_state.state",
-                                    "to_state.state",
-                                    "for",
-                                )
+                            "some": (
+                                "turn_on {{ trigger.platform }}"
+                                " - {{ trigger.entity_id }}"
+                                " - {{ trigger.from_state.state }}"
+                                " - {{ trigger.to_state.state }}"
+                                " - {{ trigger.for }}"
                             )
                         },
                     },
