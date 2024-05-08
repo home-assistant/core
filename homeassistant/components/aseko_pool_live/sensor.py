2325da1fdcb4c5ca9e10b397a2af905e68b8f8f2
hopkins-tk <hopkins.tk@gmail.com>
2024-05-08 06:32:13 +0000

    Feat add entity descriptions for known sensors

--- homeassistant/components/aseko_pool_live/sensor.py
+++ homeassistant/components/aseko_pool_live/sensor.py
@@ -2,20 +2,85 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 from aioaseko import Unit, Variable
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
+    SensorEntityDescription,
     SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import UnitOfElectricPotential, UnitOfLength, UnitOfTemperature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
 from .coordinator import AsekoDataUpdateCoordinator
 from .entity import AsekoEntity
+
+
+@dataclass(frozen=True, kw_only=True)
+class AsekoSensorEntityDescription(SensorEntityDescription):
+    """Describes an Aseko binary sensor entity."""
+
+
+CONCENTRATION_KILOGRAMS_PER_CUBIC_METER = "kg/m³"
+CONCENTRATION_MILLIGRAMS_PER_LITER = "mg/l"
+GRAMS_PER_HOUR = "g/h"
+
+UNIT_SENSORS = {
+    "airTemp": AsekoSensorEntityDescription(
+        key="air_temperature",
+        translation_key="air_temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+    ),
+    "waterTemp": AsekoSensorEntityDescription(
+        key="water_temperature",
+        translation_key="water_temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+    ),
+    "ph": AsekoSensorEntityDescription(
+        key="ph",
+        translation_key="ph",
+        device_class=SensorDeviceClass.PH,
+    ),
+    "rx": AsekoSensorEntityDescription(
+        key="redox",
+        translation_key="redox",
+        device_class=SensorDeviceClass.VOLTAGE,
+        native_unit_of_measurement=UnitOfElectricPotential.MILLIVOLT,
+        icon="mdi:test-tube",
+    ),
+    "electrodePower": AsekoSensorEntityDescription(
+        key="electrolyzer",
+        translation_key="electrolyzer",
+        native_unit_of_measurement=GRAMS_PER_HOUR,
+        icon="mdi:lightning-bolt",
+    ),
+    "clf": AsekoSensorEntityDescription(
+        key="free_chlorine",
+        translation_key="free_chlorine",
+        native_unit_of_measurement=CONCENTRATION_MILLIGRAMS_PER_LITER,
+        icon="mdi:test-tube",
+    ),
+    "salinity": AsekoSensorEntityDescription(
+        key="salinity",
+        translation_key="salinity",
+        native_unit_of_measurement=CONCENTRATION_KILOGRAMS_PER_CUBIC_METER,
+    ),
+    "waterLevel": AsekoSensorEntityDescription(
+        key="water_level",
+        translation_key="water_level",
+        device_class=SensorDeviceClass.DISTANCE,
+        native_unit_of_measurement=UnitOfLength.CENTIMETERS,
+        icon="mdi:waves",
+    ),
+}
 
 
 async def async_setup_entry(
@@ -47,29 +112,14 @@ class VariableSensorEntity(AsekoEntity, SensorEntity):
         super().__init__(unit, coordinator)
         self._variable = variable
 
-        translation_key = {
-            "Air temp.": "air_temperature",
-            "Cl free": "free_chlorine",
-            "Water temp.": "water_temperature",
-        }.get(self._variable.name)
-        if translation_key is not None:
-            self._attr_translation_key = translation_key
+        entity_description = UNIT_SENSORS.get(self._variable.type)
+        if entity_description is not None:
+            self.entity_description = entity_description
         else:
             self._attr_name = self._variable.name
+            self._attr_native_unit_of_measurement = self._variable.unit
 
         self._attr_unique_id = f"{self._unit.serial_number}{self._variable.type}"
-        self._attr_native_unit_of_measurement = self._variable.unit
-
-        self._attr_icon = {
-            "rx": "mdi:test-tube",
-            "waterLevel": "mdi:waves",
-        }.get(self._variable.type)
-
-        self._attr_device_class = {
-            "airTemp": SensorDeviceClass.TEMPERATURE,
-            "waterTemp": SensorDeviceClass.TEMPERATURE,
-            "ph": SensorDeviceClass.PH,
-        }.get(self._variable.type)
 
     @property
     def native_value(self) -> int | None:
