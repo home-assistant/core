4ef3979f549fe6be525806e542edd2d30d450265
farmio <farmio@alphart.net>
2024-05-09 23:03:05 +0200

    Test deleting entities

--- tests/components/knx/test_config_store.py
+++ tests/components/knx/test_config_store.py
@@ -2,6 +2,9 @@
 
 from typing import Any
 
+from homeassistant.components.knx.storage.config_store import (
+    STORAGE_KEY as KNX_CONFIG_STORAGE_KEY,
+)
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
@@ -78,3 +81,86 @@ async def test_create_switch(
         },
         "schema_options": None,
     }
+
+
+async def test_delete_entity(
+    hass: HomeAssistant,
+    knx: KNXTestKit,
+    entity_registry: er.EntityRegistry,
+    hass_ws_client: WebSocketGenerator,
+    hass_storage: dict[str, Any],
+) -> None:
+    """Test entity deletion."""
+    await knx.setup_integration({})
+    client = await hass_ws_client(hass)
+
+    test_name = "Test no device"
+    test_entity_id = "switch.test_no_device"
+    assert not entity_registry.async_get(test_entity_id)
+
+    # create entity
+    await client.send_json_auto_id(
+        {
+            "type": "knx/create_entity",
+            "platform": Platform.SWITCH,
+            "data": {
+                "entity": {"name": test_name},
+                "knx": {"ga_switch": {"write": "1/2/3"}},
+            },
+        }
+    )
+    res = await client.receive_json()
+    assert res["success"], res
+    assert res["result"]["entity_id"] == test_entity_id
+    assert entity_registry.async_get(test_entity_id)
+
+    # delete entity
+    await client.send_json_auto_id(
+        {
+            "type": "knx/delete_entity",
+            "entity_id": test_entity_id,
+        }
+    )
+    res = await client.receive_json()
+    assert res["success"], res
+
+    assert not entity_registry.async_get(test_entity_id)
+    assert not hass_storage[KNX_CONFIG_STORAGE_KEY]["data"]["entities"].get("switch")
+
+
+async def test_delete_entity_error(
+    hass: HomeAssistant,
+    knx: KNXTestKit,
+    entity_registry: er.EntityRegistry,
+    hass_ws_client: WebSocketGenerator,
+    hass_storage: dict[str, Any],
+) -> None:
+    """Test unsuccessful entity deletion."""
+    await knx.setup_integration({})
+    client = await hass_ws_client(hass)
+
+    # delete unknown entity
+    await client.send_json_auto_id(
+        {
+            "type": "knx/delete_entity",
+            "entity_id": "switch.non_existing_entity",
+        }
+    )
+    res = await client.receive_json()
+    assert not res["success"], res
+    assert res["error"]["code"] == "home_assistant_error"
+    assert res["error"]["message"].startswith("Entity not found")
+
+    # delete entity not in config store
+    test_entity_id = "sensor.knx_interface_individual_address"
+    assert entity_registry.async_get(test_entity_id)
+    await client.send_json_auto_id(
+        {
+            "type": "knx/delete_entity",
+            "entity_id": test_entity_id,
+        }
+    )
+    res = await client.receive_json()
+    assert not res["success"], res
+    assert res["error"]["code"] == "home_assistant_error"
+    assert res["error"]["message"].startswith("Entity not found")
