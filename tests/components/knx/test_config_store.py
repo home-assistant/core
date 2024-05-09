5aa6434cbf928c59f02e412882698f65951afea6
farmio <farmio@alphart.net>
2024-05-09 23:15:10 +0200

    Test unsuccessful entity creation

--- tests/components/knx/test_config_store.py
+++ tests/components/knx/test_config_store.py
@@ -83,6 +83,52 @@ async def test_create_switch(
     }
 
 
+async def test_create_entity_error(
+    hass: HomeAssistant,
+    knx: KNXTestKit,
+    entity_registry: er.EntityRegistry,
+    hass_ws_client: WebSocketGenerator,
+    hass_storage: dict[str, Any],
+) -> None:
+    """Test unsuccessful entity creation."""
+    await knx.setup_integration({})
+    client = await hass_ws_client(hass)
+
+    # create entity with invalid platform
+    await client.send_json_auto_id(
+        {
+            "type": "knx/create_entity",
+            "platform": "invalid_platform",
+            "data": {
+                "entity": {"name": "Test invalid platform"},
+                "knx": {"ga_switch": {"write": "1/2/3"}},
+            },
+        }
+    )
+    res = await client.receive_json()
+    assert res["success"], res
+    assert not res["result"]["success"]
+    assert res["result"]["errors"][0]["path"] == ["platform"]
+    assert res["result"]["error_base"].startswith("expected Platform or one of")
+
+    # create entity with unsupported platform
+    await client.send_json_auto_id(
+        {
+            "type": "knx/create_entity",
+            "platform": "tts",  # "tts" is not a supported platform (and is unlikely te ever be)
+            "data": {
+                "entity": {"name": "Test invalid platform"},
+                "knx": {"ga_switch": {"write": "1/2/3"}},
+            },
+        }
+    )
+    res = await client.receive_json()
+    assert res["success"], res
+    assert not res["result"]["success"]
+    assert res["result"]["errors"][0]["path"] == ["platform"]
+    assert res["result"]["error_base"].startswith("value must be one of")
+
+
 async def test_delete_entity(
     hass: HomeAssistant,
     knx: KNXTestKit,
