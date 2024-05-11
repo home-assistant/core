8e8d3a795a6710de9ff0dc806fc29a5f87bc29dc
Brett <brett.whynot@gmail.com>
2024-05-11 04:16:32 +0000

    Add tests

---
+++ tests/components/teslemetry/test_binary_sensors.py
@@ -0,0 +1,37 @@
+"""Test the Teslemetry binary sensor platform."""
+
+import pytest
+from syrupy import SnapshotAssertion
+from tesla_fleet_api.exceptions import VehicleOffline
+
+from homeassistant.const import STATE_UNKNOWN, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from . import assert_entities, setup_platform
+
+
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_binary_sensor(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Tests that the climate entity is correct."""
+
+    entry = await setup_platform(hass, [Platform.BINARY_SENSOR])
+    assert_entities(hass, entry.entry_id, entity_registry, snapshot)
+
+
+async def test_binary_sensor_offline(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+    mock_vehicle_data,
+) -> None:
+    """Tests that the climate entity is correct."""
+
+    mock_vehicle_data.side_effect = VehicleOffline
+    await setup_platform(hass, [Platform.BINARY_SENSOR])
+    state = hass.states.get("binary_sensor.test_status")
+    assert state.state == STATE_UNKNOWN
