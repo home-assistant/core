efc654e5379a5ab6c0bb306a6c8fe3bb9c2eadb6
Denis Shulyaka <Shulyaka@gmail.com>
2024-05-11 23:23:15 +0300

    Add bind_hass back for now

--- homeassistant/helpers/intent.py
+++ homeassistant/helpers/intent.py
@@ -23,6 +23,7 @@ from homeassistant.const import (
 )
 from homeassistant.core import Context, HomeAssistant, State, callback
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.loader import bind_hass
 from homeassistant.util.hass_dict import HassKey
 
 from . import (
@@ -52,6 +53,7 @@ SPEECH_TYPE_SSML = "ssml"
 
 
 @callback
+@bind_hass
 def async_register(hass: HomeAssistant, handler: IntentHandler) -> None:
     """Register an intent with Home Assistant."""
     if (intents := hass.data.get(DATA_KEY)) is None:
@@ -68,6 +70,7 @@ def async_register(hass: HomeAssistant, handler: IntentHandler) -> None:
 
 
 @callback
+@bind_hass
 def async_remove(hass: HomeAssistant, intent_type: str) -> None:
     """Remove an intent from Home Assistant."""
     if (intents := hass.data.get(DATA_KEY)) is None:
@@ -76,6 +79,7 @@ def async_remove(hass: HomeAssistant, intent_type: str) -> None:
     intents.pop(intent_type, None)
 
 
+@bind_hass
 async def async_handle(
     hass: HomeAssistant,
     platform: str,
@@ -663,6 +667,7 @@ def async_match_targets(  # noqa: C901
 
 
 @callback
+@bind_hass
 def async_match_states(
     hass: HomeAssistant,
     name: str | None = None,
