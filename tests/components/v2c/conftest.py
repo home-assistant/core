425100799ea11a600b023c0d1750ecb23e342bd4
Joostlek <joostlek@outlook.com>
2024-05-11 19:20:20 +0200

    Refactor V2C tests

--- tests/components/v2c/conftest.py
+++ tests/components/v2c/conftest.py
@@ -13,3 +13,21 @@ def mock_setup_entry() -> Generator[AsyncMock, None, None]:
         "homeassistant.components.v2c.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         yield mock_setup_entry
+
+
+@pytest.fixture
+def mock_v2c_client() -> Generator[AsyncMock, None, None]:
+    """Mock a V2C client."""
+    with (
+        patch(
+            "homeassistant.components.v2c.Trydan",
+            autospec=True,
+        ) as mock_client,
+        patch(
+            "homeassistant.components.v2c.config_flow.Trydan",
+            new=mock_client,
+        ),
+    ):
+        client = mock_client.return_value
+        client.get_data = {}
+        yield client
