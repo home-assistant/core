21170e2386185c9e936674ff3ac45719c5583f57
Denis Shulyaka <Shulyaka@gmail.com>
2024-05-11 14:13:28 +0300

    Move IntentTool to the llm library

--- homeassistant/helpers/llm.py
+++ homeassistant/helpers/llm.py
@@ -14,6 +14,8 @@ from homeassistant.core import Context, HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.util.hass_dict import HassKey
 
+from . import area_registry, floor_registry, intent
+
 _LOGGER = logging.getLogger(__name__)
 
 DATA_KEY: HassKey[dict[str, Tool]] = HassKey("llm_tool")
@@ -100,3 +102,79 @@ async def async_call_tool(hass: HomeAssistant, tool_input: ToolInput) -> Any:
     tool_input.tool_args = tool.parameters(tool_input.tool_args)
 
     return await tool.async_call(hass, tool_input)
+
+
+class IntentTool(Tool):
+    """LLM Tool representing an Intent."""
+
+    def __init__(
+        self,
+        intent_type: str,
+        slot_schema: vol.Schema = vol.Schema({}),
+    ) -> None:
+        """Init the class."""
+        self.name = intent_type
+        self.description = f"Execute Home Assistant {intent_type} intent"
+        self.parameters = slot_schema
+
+    async def async_call(self, hass: HomeAssistant, tool_input: ToolInput) -> Any:
+        """Handle the intent."""
+        slots = {key: {"value": val} for key, val in tool_input.tool_args.items()}
+
+        if "area" in slots:
+            areas = area_registry.async_get(hass)
+            id_or_name = slots["area"]["value"]
+
+            area = areas.async_get_area(id_or_name) or areas.async_get_area_by_name(
+                id_or_name
+            )
+            if not area:
+                # Check area aliases
+                for maybe_area in areas.areas.values():
+                    if not maybe_area.aliases:
+                        continue
+
+                    for area_alias in maybe_area.aliases:
+                        if id_or_name == area_alias.casefold():
+                            area = maybe_area
+                            break
+            if area:
+                slots["area"]["value"] = area.id
+                slots["area"]["text"] = area.name
+            else:
+                slots["area"]["text"] = id_or_name
+
+        if "floor" in slots:
+            floors = floor_registry.async_get(hass)
+            id_or_name = slots["floor"]["value"]
+
+            floor = floors.async_get_floor(
+                id_or_name
+            ) or floors.async_get_floor_by_name(id_or_name)
+            if not floor:
+                # Check floor aliases
+                for maybe_floor in floors.floors.values():
+                    if not maybe_floor.aliases:
+                        continue
+
+                    for floor_alias in maybe_floor.aliases:
+                        if id_or_name == floor_alias.casefold():
+                            floor = maybe_floor
+                            break
+            if floor:
+                slots["floor"]["value"] = floor.floor_id
+                slots["floor"]["text"] = floor.name
+            else:
+                slots["floor"]["text"] = id_or_name
+
+        intent_response = await intent.async_handle(
+            hass,
+            tool_input.platform,
+            self.name,
+            slots,
+            tool_input.user_prompt,
+            tool_input.context,
+            tool_input.language,
+            tool_input.assistant,
+        )
+        return intent_response.as_dict()
