038d238566d651f980ca9750d1d93bf4dd223d0a
Joostlek <joostlek@outlook.com>
2024-05-12 21:51:24 +0200

    Fix

--- homeassistant/components/aurora/coordinator.py
+++ homeassistant/components/aurora/coordinator.py
@@ -4,11 +4,11 @@ from __future__ import annotations
 
 from datetime import timedelta
 import logging
+from typing import TYPE_CHECKING
 
 from aiohttp import ClientError
 from auroranoaa import AuroraForecast
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
@@ -16,13 +16,16 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, Upda
 
 from .const import CONF_THRESHOLD, DEFAULT_THRESHOLD
 
+if TYPE_CHECKING:
+    from . import AuroraConfigEntry
+
 _LOGGER = logging.getLogger(__name__)
 
 
 class AuroraDataUpdateCoordinator(DataUpdateCoordinator[int]):
     """Class to manage fetching data from the NOAA Aurora API."""
 
-    config_entry: ConfigEntry
+    config_entry: AuroraConfigEntry
 
     def __init__(self, hass: HomeAssistant) -> None:
         """Initialize the data updater."""
