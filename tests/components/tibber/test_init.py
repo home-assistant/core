7e279f7d9bdc29846ecc479dde0641a9e73f4e17
jbouwh <jan@jbsoft.nl>
2024-05-12 17:30:15 +0000

    Add entry state assertions to unload test

--- tests/components/tibber/test_init.py
+++ tests/components/tibber/test_init.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock
 
 from homeassistant.components.recorder import Recorder
 from homeassistant.components.tibber import DOMAIN
+from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
 
@@ -12,6 +13,9 @@ async def test_entry_unload(
 ) -> None:
     """Test unloading the entry."""
     entry = hass.config_entries.async_entry_for_domain_unique_id(DOMAIN, "tibber")
+    assert entry.state == ConfigEntryState.LOADED
+
     await hass.config_entries.async_unload(entry.entry_id)
     mock_tibber_setup.rt_disconnect.assert_called_once()
     await hass.async_block_till_done(wait_background_tasks=True)
+    assert entry.state == ConfigEntryState.NOT_LOADED
