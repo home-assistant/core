8a47c99c4c1d371f530964ea868fd0a08fee8d74
Jakob Schlyter <jakob@kirei.se>
2024-05-13 08:36:16 +0200

    also parse AdditionalLanguageCodes

--- homeassistant/components/amazon_polly/voices.py
+++ homeassistant/components/amazon_polly/voices.py
@@ -5,12 +5,15 @@ from pydantic import BaseModel, Field
 
 
 class AmazonPollyVoice(BaseModel):
-    id: str = Field(None, alias="Id")
-    name: str = Field(None, alias="Name")
-    gender: str = Field(None, alias="Gender")
-    language_name: str = Field(None, alias="LanguageName")
-    language_code: str = Field(None, alias="LanguageCode")
-    supported_engines: set[str] = Field(None, alias="SupportedEngines")
+    id: str = Field(alias="Id")
+    name: str = Field(alias="Name")
+    gender: str = Field(alias="Gender")
+    language_name: str = Field(alias="LanguageName")
+    language_code: str = Field(alias="LanguageCode")
+    supported_engines: set[str] = Field(alias="SupportedEngines")
+    additional_language_codes: set[str] = Field(
+        default=set(), alias="AdditionalLanguageCodes"
+    )
 
 
 def get_all_voices(client: boto3.client) -> list[AmazonPollyVoice]:
@@ -21,6 +24,7 @@ def get_all_voices(client: boto3.client) -> list[AmazonPollyVoice]:
 def main() -> None:
     client = boto3.client("polly")
     voices = get_all_voices(client)
+    print(voices)
     for voice in sorted(voices, key=lambda x: x.id):
         print(f'"{voice.id}", # {voice.language_name} ({voice.language_code})')
 
