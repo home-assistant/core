f98594c8355f1e3086fd07662132e3d68e920eba
Anrijs <anrijs@anrijs.lv>
2024-05-13 12:27:31 +0300

    mikrotik_bt5 import Callable

--- homeassistant/components/mikrotik_bt5/sensor.py
+++ homeassistant/components/mikrotik_bt5/sensor.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from typing import Any
+from typing import Callable
 
 from mikrotik_bt5 import MikrotikBeacon
 
@@ -45,7 +46,7 @@ class MikroTikSensorEntityDescription(SensorEntityDescription):
     # Restrict the type to satisfy the type checker and catch attempts
     # to use UNDEFINED in the entity descriptions.
     name: str | None = None
-    value: Callable[[Any], Any] = lambda a : None
+    value: Callable[[Any], Any] = lambda a: None
 
 
 SENSOR_DESCRIPTIONS = {
