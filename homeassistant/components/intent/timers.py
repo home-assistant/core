181d8cecbe64aaad9ff56325eb058883ee3fe784
Michael Hansen <mike@rhasspy.org>
2024-05-15 11:06:49 -0500

    Add rounded time to status

--- homeassistant/components/intent/timers.py
+++ homeassistant/components/intent/timers.py
@@ -640,6 +640,55 @@ def _get_total_seconds(slots: dict[str, Any]) -> int:
     return total_seconds
 
 
+def _round_time(hours: int, minutes: int, seconds: int) -> tuple[int, int, int]:
+    """Round time to a lower precision for feedback."""
+    if hours > 0:
+        # No seconds, round up above 45 minutes and down below 15
+        rounded_hours = hours
+        rounded_seconds = 0
+        if minutes > 45:
+            # 01:50:30 -> 02:00:00
+            rounded_hours += 1
+            rounded_minutes = 0
+        elif minutes < 15:
+            # 01:10:30 -> 01:00:00
+            rounded_minutes = 0
+        else:
+            # 01:25:30 -> 01:30:00
+            rounded_minutes = 30
+    elif minutes > 0:
+        # Round up above 45 seconds, down below 15
+        rounded_hours = 0
+        rounded_minutes = minutes
+        if seconds > 45:
+            # 00:01:50 -> 00:02:00
+            rounded_minutes += 1
+            rounded_seconds = 0
+        elif seconds < 15:
+            # 00:01:10 -> 00:01:00
+            rounded_seconds = 0
+        else:
+            # 00:01:25 -> 00:01:30
+            rounded_seconds = 30
+    else:
+        # Round up above 50 seconds, exact below 10, and down to nearest 10
+        # otherwise.
+        rounded_hours = 0
+        rounded_minutes = 0
+        if seconds > 50:
+            # 00:00:55 -> 00:01:00
+            rounded_minutes = 1
+            rounded_seconds = 0
+        elif seconds < 10:
+            # 00:00:09 -> 00:00:09
+            rounded_seconds = seconds
+        else:
+            # 00:01:25 -> 00:01:20
+            rounded_seconds = seconds - (seconds % 10)
+
+    return rounded_hours, rounded_minutes, rounded_seconds
+
+
 class StartTimerIntentHandler(intent.IntentHandler):
     """Intent handler for starting a new timer."""
 
@@ -819,6 +868,11 @@ class TimerStatusIntentHandler(intent.IntentHandler):
             minutes, seconds = divmod(total_seconds, 60)
             hours, minutes = divmod(minutes, 60)
 
+            # Get lower-precision time for feedback
+            rounded_hours, rounded_minutes, rounded_seconds = _round_time(
+                hours, minutes, seconds
+            )
+
             statuses.append(
                 {
                     ATTR_ID: timer.id,
@@ -832,7 +886,11 @@ class TimerStatusIntentHandler(intent.IntentHandler):
                     "hours_left": hours,
                     "minutes_left": minutes,
                     "seconds_left": seconds,
+                    "rounded_hours_left": rounded_hours,
+                    "rounded_minutes_left": rounded_minutes,
+                    "rounded_seconds_left": rounded_seconds,
                     "total_seconds_left": total_seconds,
+                    "created_at": timer.created_at,
                 }
             )
 
--- tests/components/intent/test_timers.py
+++ tests/components/intent/test_timers.py
@@ -10,6 +10,7 @@ from homeassistant.components.intent.timers import (
     TimerInfo,
     TimerManager,
     TimerNotFoundError,
+    _round_time,
     async_register_timer_handler,
 )
 from homeassistant.const import ATTR_DEVICE_ID, ATTR_NAME
@@ -1109,3 +1110,24 @@ async def test_area_filter(
     timers = result.speech_slots.get("timers", [])
     assert len(timers) == 1
     assert timers[0].get(ATTR_NAME) == "media"
+
+
+def test_round_time() -> None:
+    """Test lower-precision time rounded."""
+
+    # hours
+    assert _round_time(1, 10, 30) == (1, 0, 0)
+    assert _round_time(1, 48, 30) == (2, 0, 0)
+    assert _round_time(2, 25, 30) == (2, 30, 0)
+
+    # minutes
+    assert _round_time(0, 1, 10) == (0, 1, 0)
+    assert _round_time(0, 1, 48) == (0, 2, 0)
+    assert _round_time(0, 2, 25) == (0, 2, 30)
+
+    # seconds
+    assert _round_time(0, 0, 6) == (0, 0, 6)
+    assert _round_time(0, 0, 15) == (0, 0, 10)
+    assert _round_time(0, 0, 48) == (0, 1, 0)
+    assert _round_time(0, 0, 25) == (0, 0, 20)
+    assert _round_time(0, 0, 35) == (0, 0, 30)
