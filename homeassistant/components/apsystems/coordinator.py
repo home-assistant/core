066c18867c90aa3e70a11c3738cca923a58cb913
Marlon <git@mawoka.eu>
2024-05-15 19:56:12 +0200

    Post review comments on APsystems (#117504)
    * Cleanup for apsystems and fix for strings
    
    * Migrate to typed ConfigEntry Data for apsystems
    
    * Improve strings for apsystems
    
    * Improve config flow tests for apsystems by cleaning up fixtures
    
    * Do not use Dataclass for Config Entry Typing
    
    * Improve translations for apsystems by using sentence case and removing an apostrophe
    
    * Rename test fixture and remove unnecessary comment in tests from apsystems
    
    * Remove default override with default in coordinator from apsystems

--- homeassistant/components/apsystems/coordinator.py
+++ homeassistant/components/apsystems/coordinator.py
@@ -3,35 +3,27 @@
 from __future__ import annotations
 
 from datetime import timedelta
-import logging
 
 from APsystemsEZ1 import APsystemsEZ1M, ReturnOutputData
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-_LOGGER = logging.getLogger(__name__)
+from .const import LOGGER
 
 
-class InverterNotAvailable(Exception):
-    """Error used when Device is offline."""
-
-
-class ApSystemsDataCoordinator(DataUpdateCoordinator):
+class ApSystemsDataCoordinator(DataUpdateCoordinator[ReturnOutputData]):
     """Coordinator used for all sensors."""
 
     def __init__(self, hass: HomeAssistant, api: APsystemsEZ1M) -> None:
         """Initialize my coordinator."""
         super().__init__(
             hass,
-            _LOGGER,
-            # Name of the data. For logging purposes.
+            LOGGER,
             name="APSystems Data",
-            # Polling interval. Will only be polled if there are subscribers.
             update_interval=timedelta(seconds=12),
         )
         self.api = api
-        self.always_update = True
 
     async def _async_update_data(self) -> ReturnOutputData:
         return await self.api.get_output_data()
