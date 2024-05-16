a4466a2fb06935348a6453a9dc44689b965addfb
Joostlek <joostlek@outlook.com>
2024-05-16 18:56:32 +0200

    Use generator expression in poolsense

--- homeassistant/components/poolsense/coordinator.py
+++ homeassistant/components/poolsense/coordinator.py
@@ -3,15 +3,20 @@
 import asyncio
 from datetime import timedelta
 import logging
+from typing import TYPE_CHECKING
 
 from poolsense import PoolSense
 from poolsense.exceptions import PoolSenseError
 
+from homeassistant.const import CONF_EMAIL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import DOMAIN
+
+if TYPE_CHECKING:
+    from . import PoolSenseConfigEntry
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -19,10 +24,13 @@ _LOGGER = logging.getLogger(__name__)
 class PoolSenseDataUpdateCoordinator(DataUpdateCoordinator[dict[str, StateType]]):
     """Define an object to hold PoolSense data."""
 
+    config_entry: PoolSenseConfigEntry
+
     def __init__(self, hass: HomeAssistant, poolsense: PoolSense) -> None:
         """Initialize."""
         super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=timedelta(hours=1))
         self.poolsense = poolsense
+        self.email = self.config_entry.data[CONF_EMAIL]
 
     async def _async_update_data(self) -> dict[str, StateType]:
         """Update data via library."""
