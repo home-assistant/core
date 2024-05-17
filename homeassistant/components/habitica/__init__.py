311bbc144f421fd2f1e661377ac42b244ff2d5ad
tr4nt0r <manni@zapto.de>
2024-05-17 15:57:05 +0000

    Fix Habitica doing blocking I/O in the event loop

--- homeassistant/components/habitica/__init__.py
+++ homeassistant/components/habitica/__init__.py
@@ -151,12 +151,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: HabiticaConfigEntry) -> 
     username = entry.data[CONF_API_USER]
     password = entry.data[CONF_API_KEY]
 
-    api = HAHabitipyAsync(
+    api = await hass.async_add_executor_job(
+        HAHabitipyAsync,
         {
             "url": url,
             "login": username,
             "password": password,
-        }
+        },
     )
     try:
         user = await api.user.get(userFields="profile")
