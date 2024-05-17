938115cc1a4abb40e6da3bd0867d432e4a90d48b
Filipe Pina <hzlu1ot0@duck.com>
2024-05-17 01:19:15 +0100

    tcp: use sensor available flag to reflect connection errors

--- homeassistant/components/tcp/entity.py
+++ homeassistant/components/tcp/entity.py
@@ -66,6 +66,10 @@ class TcpEntity(Entity):
 
     def update(self) -> None:
         """Get the latest value for this sensor."""
+        if self._state is not None:
+            # Do not set it as unavailable if still unknown.
+            self.available = False
+
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.settimeout(self._config[CONF_TIMEOUT])
             try:
@@ -113,7 +117,9 @@ class TcpEntity(Entity):
             value = sock.recv(self._config[CONF_BUFFER_SIZE]).decode()
 
         value_template = self._config[CONF_VALUE_TEMPLATE]
-        if value_template is not None:
+        if value_template is None:
+            self._state = value
+        else:
             try:
                 self._state = value_template.render(parse_result=False, value=value)
             except TemplateError:
@@ -123,6 +129,5 @@ class TcpEntity(Entity):
                     value,
                 )
                 return
-            return
 
-        self._state = value
+        self.available = True
