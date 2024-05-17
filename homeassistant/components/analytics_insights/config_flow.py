30b4494c3d4116c6950664574b5b1b4d9ec1f4ea
Franck Nijhof <git@frenck.dev>
2024-05-17 14:13:10 +0200

    Fix rc pylint warning for Home Assistant Analytics (#117635)

--- homeassistant/components/analytics_insights/config_flow.py
+++ homeassistant/components/analytics_insights/config_flow.py
@@ -82,7 +82,7 @@ class HomeassistantAnalyticsConfigFlow(ConfigFlow, domain=DOMAIN):
         except HomeassistantAnalyticsConnectionError:
             LOGGER.exception("Error connecting to Home Assistant analytics")
             return self.async_abort(reason="cannot_connect")
-        except Exception:  # noqa: BLE001
+        except Exception:  # pylint: disable=broad-except
             LOGGER.exception("Unexpected error")
             return self.async_abort(reason="unknown")
 
