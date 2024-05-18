4974aae7fac52fc72dbd546b629e6dff50df72cf
Alberto Geniola <albertogeniola@gmail.com>
2024-05-18 15:39:58 +0200

    Add test to cover _handle_update callback in ElmaxEntity.

--- tests/components/elmax/test_alarm_control_panel.py
+++ tests/components/elmax/test_alarm_control_panel.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 
 from syrupy import SnapshotAssertion
 
+from homeassistant.components.elmax import ElmaxCoordinator
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
@@ -25,3 +26,27 @@ async def test_alarm_control_panels(
         entry = await init_integration(hass)
 
     await snapshot_platform(hass, entity_registry, snapshot, entry.entry_id)
+
+
+async def test_elmax_entity_update_callback(
+    hass: HomeAssistant,
+    entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test ElmaxEntity _handle_update."""
+    with patch(
+        "homeassistant.components.elmax.ELMAX_PLATFORMS", [Platform.ALARM_CONTROL_PANEL]
+    ):
+        entry = await init_integration(hass)
+    coordinator: ElmaxCoordinator = hass.data[entry.domain][entry.entry_id]
+
+    pre_update_state = hass.states.get(
+        "alarm_control_panel.direct_panel_https_1_1_1_1_443_api_v2_area_1"
+    )
+    assert pre_update_state.state == "unknown"
+    await coordinator.async_refresh()
+    await hass.async_block_till_done()
+    new_state = hass.states.get(
+        "alarm_control_panel.direct_panel_https_1_1_1_1_443_api_v2_area_1"
+    )
+    assert new_state.state == "disarmed"
