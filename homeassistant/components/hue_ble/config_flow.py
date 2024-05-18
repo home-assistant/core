d4649316b1281c4a2ae95610edc733916657a8e8
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2024-05-18 17:29:10 +0000

    Add tests for hue_ble

--- homeassistant/components/hue_ble/config_flow.py
+++ homeassistant/components/hue_ble/config_flow.py
@@ -75,7 +75,7 @@ class HueBleConfigFlow(ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
         if user_input is None:
             return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
             )
 
         try:
