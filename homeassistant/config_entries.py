f1325cc29e0b7a961fdc0ad3b5fa136650e910c9
J. Nick Koston <nick@koston.org>
2024-05-20 10:02:35 -1000

    Update homeassistant/config_entries.py
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -718,7 +718,7 @@ class ConfigEntry(Generic[_DataT]):
     ) -> None:
         """Raise exception if the wrong exception is raised in a forwarded platform."""
         report(
-            f"raises exception {type(exc)} {exc} in forwarded platform"
+            f"raises exception {type(exc)} {exc} in forwarded platform. Raise before forwarding instead."
             f" {integration.domain}",
             error_if_core=True,
             error_if_integration=True,
