d8b21927f07b4135179f222562544cf49bf6e0e8
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-05-20 11:08:34 +0200

    Use PEP 695 for function annotations with scoping

--- homeassistant/core.py
+++ homeassistant/core.py
@@ -129,7 +129,6 @@ FINAL_WRITE_STAGE_SHUTDOWN_TIMEOUT = 60
 CLOSE_STAGE_SHUTDOWN_TIMEOUT = 30
 
 
-_R = TypeVar("_R")
 # Internal; not helpers.typing.UNDEFINED due to circular dependency
 _UNDEF: dict[Any, Any] = {}
 _SENTINEL = object()
@@ -693,7 +692,7 @@ class HomeAssistant:
 
     @overload
     @callback
-    def _async_add_hass_job(
+    def _async_add_hass_job[_R](
         self,
         hassjob: HassJob[..., Coroutine[Any, Any, _R]],
         *args: Any,
@@ -702,7 +701,7 @@ class HomeAssistant:
 
     @overload
     @callback
-    def _async_add_hass_job(
+    def _async_add_hass_job[_R](
         self,
         hassjob: HassJob[..., Coroutine[Any, Any, _R] | _R],
         *args: Any,
@@ -710,7 +709,7 @@ class HomeAssistant:
     ) -> asyncio.Future[_R] | None: ...
 
     @callback
-    def _async_add_hass_job(
+    def _async_add_hass_job[_R](
         self,
         hassjob: HassJob[..., Coroutine[Any, Any, _R] | _R],
         *args: Any,
@@ -882,7 +881,7 @@ class HomeAssistant:
 
     @overload
     @callback
-    def async_run_hass_job(
+    def async_run_hass_job[_R](
         self,
         hassjob: HassJob[..., Coroutine[Any, Any, _R]],
         *args: Any,
@@ -891,7 +890,7 @@ class HomeAssistant:
 
     @overload
     @callback
-    def async_run_hass_job(
+    def async_run_hass_job[_R](
         self,
         hassjob: HassJob[..., Coroutine[Any, Any, _R] | _R],
         *args: Any,
@@ -899,7 +898,7 @@ class HomeAssistant:
     ) -> asyncio.Future[_R] | None: ...
 
     @callback
-    def async_run_hass_job(
+    def async_run_hass_job[_R](
         self,
         hassjob: HassJob[..., Coroutine[Any, Any, _R] | _R],
         *args: Any,
