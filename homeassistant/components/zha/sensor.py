9d7fba824de1798ad13c8f17951fa298f29b16e4
puddly <32534428+puddly@users.noreply.github.com>
2024-05-20 16:29:47 -0400

    Implement entity description "support" for SE sensors

--- homeassistant/components/zha/sensor.py
+++ homeassistant/components/zha/sensor.py
@@ -56,24 +56,47 @@ class Sensor(ZHAEntity, SensorEntity):
         """Initialize the ZHA select entity."""
         super().__init__(entity_data, **kwargs)
         entity = self.entity_data.entity
+
         if (
             hasattr(entity, "_attr_device_class")
             and entity._attr_device_class is not None
         ):
             self._attr_device_class = SensorDeviceClass(entity._attr_device_class.value)
+
         if (
             hasattr(entity, "_attr_state_class")
             and entity._attr_state_class is not None
         ):
             self._attr_state_class = SensorStateClass(entity._attr_state_class.value)
 
-        if hasattr(entity.info_object, "unit"):
+        if hasattr(entity.info_object, "unit") and entity.info_object.unit is not None:
             self._attr_native_unit_of_measurement = entity.info_object.unit
-        # TODO
-        """
-        if hasattr(entity, "entity_description"):
-            self.entity_description = entity.entity_description
-        """
+
+        # TODO: In the future, do we migrate all zigpy/zha entities to use entity
+        # descriptions? Or do we keep them simple and always use `_attr_`*?
+        if (
+            hasattr(entity, "entity_description")
+            and entity.entity_description is not None
+        ):
+            entity_description = entity.entity_description
+
+            if entity_description.state_class is not None:
+                self._attr_state_class = SensorStateClass(
+                    entity_description.state_class.value
+                )
+
+            if entity_description.scale is not None:
+                self._attr_scale = entity_description.scale
+
+            if entity_description.native_unit_of_measurement is not None:
+                self._attr_native_unit_of_measurement = (
+                    entity_description.native_unit_of_measurement
+                )
+
+            if entity_description.device_class is not None:
+                self._attr_device_class = SensorDeviceClass(
+                    entity_description.device_class.value
+                )
 
     @property
     def native_value(self) -> StateType:
