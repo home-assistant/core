32e28214fd3c10767df5a12296de57da9d411ace
J. Nick Koston <nick@koston.org>
2024-05-19 15:10:46 -1000

    Detect incorrect exception in forwarded platforms
    If an integration raises ConfigEntryError/ConfigEntryAuthFailed/ConfigEntryAuthFailed
    in a forwarded platform it would affect the state of the config entry and cause it to
    process unloads and setup retries in while the other platforms continued to setup

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -599,6 +599,8 @@ class ConfigEntry(Generic[_DataT]):
                 )
                 result = False
         except ConfigEntryError as exc:
+            if not domain_is_integration:
+                self._raise_for_exception_in_forwarded_platform(exc, integration)
             error_reason = str(exc) or "Unknown fatal config entry error"
             error_reason_translation_key = exc.translation_key
             error_reason_translation_placeholders = exc.translation_placeholders
@@ -611,6 +613,8 @@ class ConfigEntry(Generic[_DataT]):
             await self._async_process_on_unload(hass)
             result = False
         except ConfigEntryAuthFailed as exc:
+            if not domain_is_integration:
+                self._raise_for_exception_in_forwarded_platform(exc, integration)
             message = str(exc)
             auth_base_message = "could not authenticate"
             error_reason = message or auth_base_message
@@ -629,6 +633,8 @@ class ConfigEntry(Generic[_DataT]):
             self.async_start_reauth(hass)
             result = False
         except ConfigEntryNotReady as exc:
+            if not domain_is_integration:
+                self._raise_for_exception_in_forwarded_platform(exc, integration)
             message = str(exc)
             error_reason_translation_key = exc.translation_key
             error_reason_translation_placeholders = exc.translation_placeholders
@@ -705,6 +711,18 @@ class ConfigEntry(Generic[_DataT]):
                 error_reason_translation_key,
                 error_reason_translation_placeholders,
             )
+
+    @callback
+    def _raise_for_exception_in_forwarded_platform(
+        self, exc: Exception, integration: loader.Integration
+    ) -> None:
+        """Raise exception if it was raised in a forwarded platform."""
+        report(
+            f"raises exception {type(exc)} {exc} in forwarded platform"
+            f" {integration.domain}",
+            error_if_core=True,
+            error_if_integration=True,
+        )
 
     @callback
     def _async_setup_again(self, hass: HomeAssistant, *_: Any) -> None:
