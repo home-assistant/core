2a24c02349a70e7b409bab3ef2c4cc220206c7e4
Matthias Alphart <farmio@alphart.net>
2024-05-22 00:03:54 +0200

    Add data point type option to `knx.telegram` trigger (#117463)
    * Add data point type (dpt) option to `knx.telegram` trigger
    
    * Rename from `dpt` to `type` to match services
    
    * Add test for GroupValueRead telegrams
    
    * Fix device trigger schema inheritance
    
    * Typesafe dispatcher signal
    
    * readability
    
    * Avoid re-decoding with same transcoder

--- homeassistant/components/knx/trigger.py
+++ homeassistant/components/knx/trigger.py
@@ -3,18 +3,22 @@
 from typing import Final
 
 import voluptuous as vol
+from xknx.dpt import DPTBase
+from xknx.telegram import Telegram, TelegramDirection
 from xknx.telegram.address import DeviceGroupAddress, parse_device_group_address
+from xknx.telegram.apci import GroupValueRead, GroupValueResponse, GroupValueWrite
 
-from homeassistant.const import CONF_PLATFORM
+from homeassistant.const import CONF_PLATFORM, CONF_TYPE
 from homeassistant.core import CALLBACK_TYPE, HassJob, HomeAssistant, callback
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.trigger import TriggerActionType, TriggerInfo
 from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN, SIGNAL_KNX_TELEGRAM_DICT
+from .const import DOMAIN
 from .schema import ga_validator
-from .telegrams import TelegramDict
+from .telegrams import SIGNAL_KNX_TELEGRAM, TelegramDict, decode_telegram_payload
+from .validation import sensor_type_validator
 
 TRIGGER_TELEGRAM: Final = "telegram"
 
@@ -41,10 +45,11 @@ TELEGRAM_TRIGGER_SCHEMA: Final = {
     ),
     **TELEGRAM_TRIGGER_OPTIONS,
 }
-
+# TRIGGER_SCHEMA is exclusive to triggers, the above are used in device triggers too
 TRIGGER_SCHEMA = cv.TRIGGER_BASE_SCHEMA.extend(
     {
         vol.Required(CONF_PLATFORM): PLATFORM_TYPE_TRIGGER_TELEGRAM,
+        vol.Optional(CONF_TYPE, default=None): vol.Any(sensor_type_validator, None),
         **TELEGRAM_TRIGGER_SCHEMA,
     }
 )
@@ -61,41 +66,55 @@ async def async_attach_trigger(
     dst_addresses: list[DeviceGroupAddress] = [
         parse_device_group_address(address) for address in _addresses
     ]
+    _transcoder = config.get(CONF_TYPE)
+    trigger_transcoder = DPTBase.parse_transcoder(_transcoder) if _transcoder else None
+
     job = HassJob(action, f"KNX trigger {trigger_info}")
     trigger_data = trigger_info["trigger_data"]
 
     @callback
-    def async_call_trigger_action(telegram: TelegramDict) -> None:
+    def async_call_trigger_action(
+        telegram: Telegram, telegram_dict: TelegramDict
+    ) -> None:
         """Filter Telegram and call trigger action."""
-        if telegram["telegramtype"] == "GroupValueWrite":
+        payload_apci = type(telegram.payload)
+        if payload_apci is GroupValueWrite:
             if config[CONF_KNX_GROUP_VALUE_WRITE] is False:
                 return
-        elif telegram["telegramtype"] == "GroupValueResponse":
+        elif payload_apci is GroupValueResponse:
             if config[CONF_KNX_GROUP_VALUE_RESPONSE] is False:
                 return
-        elif telegram["telegramtype"] == "GroupValueRead":
+        elif payload_apci is GroupValueRead:
             if config[CONF_KNX_GROUP_VALUE_READ] is False:
                 return
 
-        if telegram["direction"] == "Incoming":
+        if telegram.direction is TelegramDirection.INCOMING:
             if config[CONF_KNX_INCOMING] is False:
                 return
         elif config[CONF_KNX_OUTGOING] is False:
             return
 
-        if (
-            dst_addresses
-            and parse_device_group_address(telegram["destination"]) not in dst_addresses
-        ):
+        if dst_addresses and telegram.destination_address not in dst_addresses:
             return
 
-        hass.async_run_hass_job(
-            job,
-            {"trigger": {**trigger_data, **telegram}},
-        )
+        if (
+            trigger_transcoder is not None
+            and payload_apci in (GroupValueWrite, GroupValueResponse)
+            and trigger_transcoder.value_type != telegram_dict["dpt_name"]
+        ):
+            decoded_payload = decode_telegram_payload(
+                payload=telegram.payload.value,  # type: ignore[union-attr]  # checked via payload_apci
+                transcoder=trigger_transcoder,  # type: ignore[type-abstract]  # parse_transcoder don't return abstract classes
+            )
+            # overwrite decoded payload values in telegram_dict
+            telegram_trigger_data = {**trigger_data, **telegram_dict, **decoded_payload}
+        else:
+            telegram_trigger_data = {**trigger_data, **telegram_dict}
+
+        hass.async_run_hass_job(job, {"trigger": telegram_trigger_data})
 
     return async_dispatcher_connect(
         hass,
-        signal=SIGNAL_KNX_TELEGRAM_DICT,
+        signal=SIGNAL_KNX_TELEGRAM,
         target=async_call_trigger_action,
     )
