fe45122b93fca71984892c7154f31d329af8570b
Allen Porter <allen.porter@gmail.com>
2024-05-22 04:07:13 +0000

    Add google conversation agent tracing

--- homeassistant/components/google_generative_ai_conversation/conversation.py
+++ homeassistant/components/google_generative_ai_conversation/conversation.py
@@ -16,7 +16,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_LLM_HASS_API, MATCH_ALL
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError, TemplateError
-from homeassistant.helpers import intent, llm, template
+from homeassistant.helpers import intent, llm, template, trace
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util import ulid
 
@@ -140,133 +140,150 @@ class GoogleGenerativeAIConversationEntity(
         llm_api: llm.API | None = None
         tools: list[dict[str, Any]] | None = None
 
-        if self.entry.options.get(CONF_LLM_HASS_API):
+        with trace.trace_path("google-generative-ai-conversation"):
+            if self.entry.options.get(CONF_LLM_HASS_API):
+                try:
+                    llm_api = llm.async_get_api(
+                        self.hass, self.entry.options[CONF_LLM_HASS_API]
+                    )
+                except HomeAssistantError as err:
+                    LOGGER.error("Error getting LLM API: %s", err)
+                    intent_response.async_set_error(
+                        intent.IntentResponseErrorCode.UNKNOWN,
+                        f"Error preparing LLM API: {err}",
+                    )
+                    return conversation.ConversationResult(
+                        response=intent_response,
+                        conversation_id=user_input.conversation_id,
+                    )
+                tools = [_format_tool(tool) for tool in llm_api.async_get_tools()]
+
+            raw_prompt = self.entry.options.get(CONF_PROMPT, DEFAULT_PROMPT)
+            model = genai.GenerativeModel(
+                model_name=self.entry.options.get(CONF_CHAT_MODEL, DEFAULT_CHAT_MODEL),
+                generation_config={
+                    "temperature": self.entry.options.get(
+                        CONF_TEMPERATURE, DEFAULT_TEMPERATURE
+                    ),
+                    "top_p": self.entry.options.get(CONF_TOP_P, DEFAULT_TOP_P),
+                    "top_k": self.entry.options.get(CONF_TOP_K, DEFAULT_TOP_K),
+                    "max_output_tokens": self.entry.options.get(
+                        CONF_MAX_TOKENS, DEFAULT_MAX_TOKENS
+                    ),
+                },
+                tools=tools or None,
+            )
+
+            if user_input.conversation_id in self.history:
+                conversation_id = user_input.conversation_id
+                messages = self.history[conversation_id]
+            else:
+                conversation_id = ulid.ulid_now()
+                messages = [{}, {}]
+
             try:
-                llm_api = llm.async_get_api(
-                    self.hass, self.entry.options[CONF_LLM_HASS_API]
-                )
-            except HomeAssistantError as err:
-                LOGGER.error("Error getting LLM API: %s", err)
+                prompt = self._async_generate_prompt(raw_prompt, llm_api)
+            except TemplateError as err:
+                LOGGER.error("Error rendering prompt: %s", err)
                 intent_response.async_set_error(
                     intent.IntentResponseErrorCode.UNKNOWN,
-                    f"Error preparing LLM API: {err}",
+                    f"Sorry, I had a problem with my template: {err}",
                 )
                 return conversation.ConversationResult(
-                    response=intent_response, conversation_id=user_input.conversation_id
+                    response=intent_response, conversation_id=conversation_id
                 )
-            tools = [_format_tool(tool) for tool in llm_api.async_get_tools()]
+            trace_element = trace.TraceElement({"prompt": prompt}, "google/prompt")
+            trace.trace_append_element(trace_element)
 
-        raw_prompt = self.entry.options.get(CONF_PROMPT, DEFAULT_PROMPT)
-        model = genai.GenerativeModel(
-            model_name=self.entry.options.get(CONF_CHAT_MODEL, DEFAULT_CHAT_MODEL),
-            generation_config={
-                "temperature": self.entry.options.get(
-                    CONF_TEMPERATURE, DEFAULT_TEMPERATURE
-                ),
-                "top_p": self.entry.options.get(CONF_TOP_P, DEFAULT_TOP_P),
-                "top_k": self.entry.options.get(CONF_TOP_K, DEFAULT_TOP_K),
-                "max_output_tokens": self.entry.options.get(
-                    CONF_MAX_TOKENS, DEFAULT_MAX_TOKENS
-                ),
-            },
-            tools=tools or None,
-        )
+            messages[0] = {"role": "user", "parts": prompt}
+            messages[1] = {"role": "model", "parts": "Ok"}
 
-        if user_input.conversation_id in self.history:
-            conversation_id = user_input.conversation_id
-            messages = self.history[conversation_id]
-        else:
-            conversation_id = ulid.ulid_now()
-            messages = [{}, {}]
+            LOGGER.debug("Input: '%s' with history: %s", user_input.text, messages)
 
-        try:
-            prompt = self._async_generate_prompt(raw_prompt, llm_api)
-        except TemplateError as err:
-            LOGGER.error("Error rendering prompt: %s", err)
-            intent_response.async_set_error(
-                intent.IntentResponseErrorCode.UNKNOWN,
-                f"Sorry, I had a problem with my template: {err}",
+            chat = model.start_chat(history=messages)
+            chat_request = user_input.text
+            # To prevent infinite loops, we limit the number of iterations
+            for _iteration in range(MAX_TOOL_ITERATIONS):
+                try:
+                    chat_response = await chat.send_message_async(chat_request)
+                except (
+                    ClientError,
+                    ValueError,
+                    genai_types.BlockedPromptException,
+                    genai_types.StopCandidateException,
+                ) as err:
+                    LOGGER.error("Error sending message: %s", err)
+                    intent_response.async_set_error(
+                        intent.IntentResponseErrorCode.UNKNOWN,
+                        f"Sorry, I had a problem talking to Google Generative AI: {err}",
+                    )
+                    return conversation.ConversationResult(
+                        response=intent_response, conversation_id=conversation_id
+                    )
+
+                LOGGER.debug("Response: %s", chat_response.parts)
+                if not chat_response.parts:
+                    intent_response.async_set_error(
+                        intent.IntentResponseErrorCode.UNKNOWN,
+                        "Sorry, I had a problem talking to Google Generative AI. Likely blocked",
+                    )
+                    return conversation.ConversationResult(
+                        response=intent_response, conversation_id=conversation_id
+                    )
+                self.history[conversation_id] = chat.history
+                tool_call = chat_response.parts[0].function_call
+
+                if not tool_call or not llm_api:
+                    break
+                trace_element = trace.TraceElement(
+                    {
+                        "tool_name": tool_call.name,
+                        "tool_args": dict(tool_call.args),
+                    },
+                    "google/tool_call",
+                )
+                trace.trace_append_element(trace_element)
+
+                tool_input = llm.ToolInput(
+                    tool_name=tool_call.name,
+                    tool_args=dict(tool_call.args),
+                    platform=DOMAIN,
+                    context=user_input.context,
+                    user_prompt=user_input.text,
+                    language=user_input.language,
+                    assistant=conversation.DOMAIN,
+                    device_id=user_input.device_id,
+                )
+                LOGGER.debug(
+                    "Tool call: %s(%s)", tool_input.tool_name, tool_input.tool_args
+                )
+                try:
+                    function_response = await llm_api.async_call_tool(tool_input)
+                except (HomeAssistantError, vol.Invalid) as e:
+                    function_response = {"error": type(e).__name__}
+                    if str(e):
+                        function_response["error_text"] = str(e)
+
+                LOGGER.debug("Tool response: %s", function_response)
+                chat_request = glm.Content(
+                    parts=[
+                        glm.Part(
+                            function_response=glm.FunctionResponse(
+                                name=tool_call.name, response=function_response
+                            )
+                        )
+                    ]
+                )
+
+            trace_element = trace.TraceElement(
+                {"response": chat_response.text}, "google/response"
             )
+            trace.trace_append_element(trace_element)
+
+            intent_response.async_set_speech(chat_response.text)
             return conversation.ConversationResult(
                 response=intent_response, conversation_id=conversation_id
             )
-
-        messages[0] = {"role": "user", "parts": prompt}
-        messages[1] = {"role": "model", "parts": "Ok"}
-
-        LOGGER.debug("Input: '%s' with history: %s", user_input.text, messages)
-
-        chat = model.start_chat(history=messages)
-        chat_request = user_input.text
-        # To prevent infinite loops, we limit the number of iterations
-        for _iteration in range(MAX_TOOL_ITERATIONS):
-            try:
-                chat_response = await chat.send_message_async(chat_request)
-            except (
-                ClientError,
-                ValueError,
-                genai_types.BlockedPromptException,
-                genai_types.StopCandidateException,
-            ) as err:
-                LOGGER.error("Error sending message: %s", err)
-                intent_response.async_set_error(
-                    intent.IntentResponseErrorCode.UNKNOWN,
-                    f"Sorry, I had a problem talking to Google Generative AI: {err}",
-                )
-                return conversation.ConversationResult(
-                    response=intent_response, conversation_id=conversation_id
-                )
-
-            LOGGER.debug("Response: %s", chat_response.parts)
-            if not chat_response.parts:
-                intent_response.async_set_error(
-                    intent.IntentResponseErrorCode.UNKNOWN,
-                    "Sorry, I had a problem talking to Google Generative AI. Likely blocked",
-                )
-                return conversation.ConversationResult(
-                    response=intent_response, conversation_id=conversation_id
-                )
-            self.history[conversation_id] = chat.history
-            tool_call = chat_response.parts[0].function_call
-
-            if not tool_call or not llm_api:
-                break
-
-            tool_input = llm.ToolInput(
-                tool_name=tool_call.name,
-                tool_args=dict(tool_call.args),
-                platform=DOMAIN,
-                context=user_input.context,
-                user_prompt=user_input.text,
-                language=user_input.language,
-                assistant=conversation.DOMAIN,
-                device_id=user_input.device_id,
-            )
-            LOGGER.debug(
-                "Tool call: %s(%s)", tool_input.tool_name, tool_input.tool_args
-            )
-            try:
-                function_response = await llm_api.async_call_tool(tool_input)
-            except (HomeAssistantError, vol.Invalid) as e:
-                function_response = {"error": type(e).__name__}
-                if str(e):
-                    function_response["error_text"] = str(e)
-
-            LOGGER.debug("Tool response: %s", function_response)
-            chat_request = glm.Content(
-                parts=[
-                    glm.Part(
-                        function_response=glm.FunctionResponse(
-                            name=tool_call.name, response=function_response
-                        )
-                    )
-                ]
-            )
-
-        intent_response.async_set_speech(chat_response.text)
-        return conversation.ConversationResult(
-            response=intent_response, conversation_id=conversation_id
-        )
 
     def _async_generate_prompt(self, raw_prompt: str, llm_api: llm.API | None) -> str:
         """Generate a prompt for the user."""
