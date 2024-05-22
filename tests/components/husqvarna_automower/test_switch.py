c515d20d6cc9aeae2f0708562d11fa4e6f251a31
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-05-22 18:51:21 +0200

    Add switch for stay out zones in Husqvarna Automower (#117809)
    Co-authored-by: Robert Resch <robert@resch.dev>

--- tests/components/husqvarna_automower/test_switch.py
+++ tests/components/husqvarna_automower/test_switch.py
@@ -26,6 +26,8 @@ from tests.common import (
     snapshot_platform,
 )
 
+TEST_ZONE_ID = "AAAAAAAA-BBBB-CCCC-DDDD-123456789101"
+
 
 async def test_switch_states(
     hass: HomeAssistant,
@@ -92,6 +94,82 @@ async def test_switch_commands(
         == "Command couldn't be sent to the command queue: Test error"
     )
     assert len(mocked_method.mock_calls) == 2
+
+
+@pytest.mark.parametrize(
+    ("service", "boolean", "excepted_state"),
+    [
+        ("turn_off", False, "off"),
+        ("turn_on", True, "on"),
+        ("toggle", True, "on"),
+    ],
+)
+async def test_stay_out_zone_switch_commands(
+    hass: HomeAssistant,
+    service: str,
+    boolean: bool,
+    excepted_state: str,
+    mock_automower_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test switch commands."""
+    entity_id = "switch.test_mower_1_avoid_danger_zone"
+    await setup_integration(hass, mock_config_entry)
+    values = mower_list_to_dictionary_dataclass(
+        load_json_value_fixture("mower.json", DOMAIN)
+    )
+    values[TEST_MOWER_ID].stay_out_zones.zones[TEST_ZONE_ID].enabled = boolean
+    mock_automower_client.get_status.return_value = values
+    mocked_method = AsyncMock()
+    setattr(mock_automower_client.commands, "switch_stay_out_zone", mocked_method)
+    await hass.services.async_call(
+        domain="switch",
+        service=service,
+        service_data={"entity_id": entity_id},
+        blocking=True,
+    )
+    mocked_method.assert_called_once_with(TEST_MOWER_ID, TEST_ZONE_ID, boolean)
+    state = hass.states.get(entity_id)
+    assert state is not None
+    assert state.state == excepted_state
+
+    mocked_method.side_effect = ApiException("Test error")
+    with pytest.raises(
+        HomeAssistantError,
+        match="Command couldn't be sent to the command queue: Test error",
+    ):
+        await hass.services.async_call(
+            domain="switch",
+            service=service,
+            service_data={"entity_id": entity_id},
+            blocking=True,
+        )
+    assert len(mocked_method.mock_calls) == 2
+
+
+async def test_zones_deleted(
+    hass: HomeAssistant,
+    mock_automower_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test if stay-out-zone is deleted after removed."""
+
+    values = mower_list_to_dictionary_dataclass(
+        load_json_value_fixture("mower.json", DOMAIN)
+    )
+    await setup_integration(hass, mock_config_entry)
+    current_entries = len(
+        er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id)
+    )
+
+    del values[TEST_MOWER_ID].stay_out_zones.zones[TEST_ZONE_ID]
+    mock_automower_client.get_status.return_value = values
+    await hass.config_entries.async_reload(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+    assert len(
+        er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id)
+    ) == (current_entries - 1)
 
 
 async def test_switch(
