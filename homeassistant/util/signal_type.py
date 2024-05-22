a098174d8aa7b331fc7ca55f54929c7405086a30
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-05-22 12:41:33 +0200

    Remove runtime implementation of format

--- homeassistant/util/signal_type.py
+++ homeassistant/util/signal_type.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-from typing import Any
-
 
 class _SignalTypeBase[*_Ts](str):
     """Generic base class for SignalType."""
@@ -21,7 +19,3 @@ class SignalTypeFormat[*_Ts](_SignalTypeBase[*_Ts]):
     """Generic string class for signal. Requires call to 'format' before use."""
 
     __slots__ = ()
-
-    def format(self, *args: Any, **kwargs: Any) -> SignalType[*_Ts]:
-        """Format name and return new SignalType instance."""
-        return SignalType(super().format(*args, **kwargs))
