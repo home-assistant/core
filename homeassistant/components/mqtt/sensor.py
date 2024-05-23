2d20bd8b64259c7553da7b28c0a75d2de8f58995
jbouwh <jan@jbsoft.nl>
2024-05-23 14:36:24 +0000

    Add sensor and binary sensor

--- homeassistant/components/mqtt/sensor.py
+++ homeassistant/components/mqtt/sensor.py
@@ -40,13 +40,7 @@ from homeassistant.util import dt as dt_util
 from . import subscription
 from .config import MQTT_RO_SCHEMA
 from .const import CONF_ENCODING, CONF_QOS, CONF_STATE_TOPIC, PAYLOAD_NONE
-from .debug_info import log_messages
-from .mixins import (
-    MqttAvailability,
-    MqttEntity,
-    async_setup_entity_entry_helper,
-    write_state_on_attr_change,
-)
+from .mixins import MqttAvailability, MqttEntity, async_setup_entity_entry_helper
 from .models import (
     MqttValueTemplate,
     PayloadSentinel,
@@ -215,9 +209,9 @@ class MqttSensor(MqttEntity, RestoreSensor):
             self._config.get(CONF_LAST_RESET_VALUE_TEMPLATE), entity=self
         ).async_render_with_possible_json_value
 
-    def _prepare_subscribe_topics(self) -> None:
-        """(Re)Subscribe to topics."""
-        topics: dict[str, dict[str, Any]] = {}
+    @callback
+    def _state_message_received(self, msg: ReceiveMessage) -> None:
+        """Handle new MQTT state messages."""
 
         def _update_state(msg: ReceiveMessage) -> None:
             # auto-expire enabled?
@@ -280,20 +274,21 @@ class MqttSensor(MqttEntity, RestoreSensor):
                     "Invalid last_reset message '%s' from '%s'", msg.payload, msg.topic
                 )
 
-        @callback
-        @write_state_on_attr_change(
-            self, {"_attr_native_value", "_attr_last_reset", "_expired"}
-        )
-        @log_messages(self.hass, self.entity_id)
-        def message_received(msg: ReceiveMessage) -> None:
-            """Handle new MQTT messages."""
-            _update_state(msg)
-            if CONF_LAST_RESET_VALUE_TEMPLATE in self._config:
-                _update_last_reset(msg)
+        _update_state(msg)
+        if CONF_LAST_RESET_VALUE_TEMPLATE in self._config:
+            _update_last_reset(msg)
+
+    def _prepare_subscribe_topics(self) -> None:
+        """(Re)Subscribe to topics."""
+        topics: dict[str, dict[str, Any]] = {}
 
         topics["state_topic"] = {
             "topic": self._config[CONF_STATE_TOPIC],
-            "msg_callback": message_received,
+            "msg_callback": self._make_callback_message_received(
+                self._state_message_received,
+                {"_attr_native_value", "_attr_last_reset", "_expired"},
+            ),
+            "entity_id": self.entity_id,
             "qos": self._config[CONF_QOS],
             "encoding": self._config[CONF_ENCODING] or None,
         }
