119084d026c81906585bbdb01484fcb3874cc2b5
Alberto Geniola <albertogeniola@gmail.com>
2024-05-23 19:19:26 +0200

    Align tests with last refactor.

--- tests/components/elmax/test_alarm_control_panel.py
+++ tests/components/elmax/test_alarm_control_panel.py
@@ -9,11 +9,10 @@ from homeassistant.components.elmax import POLLING_SECONDS
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
-from homeassistant.util.dt import utcnow
 
 from . import init_integration
 
-from tests.common import async_fire_time_changed, snapshot_platform
+from tests.common import snapshot_platform
 
 WAIT = timedelta(seconds=POLLING_SECONDS)
 
@@ -30,24 +29,3 @@ async def test_alarm_control_panels(
         entry = await init_integration(hass)
 
     await snapshot_platform(hass, entity_registry, snapshot, entry.entry_id)
-
-
-async def test_elmax_entity_update_callback(
-    hass: HomeAssistant,
-    entity_registry: er.EntityRegistry,
-    snapshot: SnapshotAssertion,
-) -> None:
-    """Test ElmaxEntity _handle_update."""
-    with patch(
-        "homeassistant.components.elmax.ELMAX_PLATFORMS", [Platform.ALARM_CONTROL_PANEL]
-    ):
-        await init_integration(hass)
-    pre_update_state = hass.states.get(
-        "alarm_control_panel.direct_panel_https_1_1_1_1_443_api_v2_area_1"
-    )
-    assert pre_update_state.state == "unknown"
-    async_fire_time_changed(hass, utcnow() + WAIT)
-    new_state = hass.states.get(
-        "alarm_control_panel.direct_panel_https_1_1_1_1_443_api_v2_area_1"
-    )
-    assert new_state.state == "disarmed"
