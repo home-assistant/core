1cfe9514c3f32218897aa1b03923c04aae892a4b
J. Nick Koston <nick@koston.org>
2024-05-22 14:13:48 -1000

    fix ruff

--- homeassistant/components/mqtt/alarm_control_panel.py
+++ homeassistant/components/mqtt/alarm_control_panel.py
@@ -40,10 +40,7 @@ from .const import (
     CONF_STATE_TOPIC,
     CONF_SUPPORTED_FEATURES,
 )
-from .mixins import (
-    MqttEntity,
-    async_setup_entity_entry_helper,
-)
+from .mixins import MqttEntity, async_setup_entity_entry_helper
 from .models import MqttCommandTemplate, MqttValueTemplate, ReceiveMessage
 from .schemas import MQTT_ENTITY_COMMON_SCHEMA
 from .util import valid_publish_topic, valid_subscribe_topic
