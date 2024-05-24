82c96d40344fb5b0823007f5d4073109ca1fcca7
Christian Neumeier <christian.neumeier@br-automation.com>
2024-05-24 12:27:03 +0000

    Removal of not needed tests and files.

--- tests/components/zeversolar/test_sensor.py
+++ tests/components/zeversolar/test_sensor.py
@@ -4,36 +4,13 @@ from unittest.mock import patch
 
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntityDescription,
-    SensorStateClass,
-)
-from homeassistant.components.zeversolar.sensor import ZeversolarEntityDescription
-from homeassistant.const import EntityCategory, Platform, UnitOfPower
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
 from . import init_integration
 
 from tests.common import snapshot_platform
-
-
-async def ZeversolarEntityDescription_constructor(hass: HomeAssistant) -> None:
-    """Perform simple tests for construction and initialization."""
-
-    description = ZeversolarEntityDescription(
-        key="pac",
-        translation_key="pac",
-        native_unit_of_measurement=UnitOfPower.WATT,
-        state_class=SensorStateClass.MEASUREMENT,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        device_class=SensorDeviceClass.POWER,
-        value_fn=lambda data: data.pac,
-    )
-
-    assert type(description) is ZeversolarEntityDescription
-    assert issubclass(type(description), SensorEntityDescription)
 
 
 async def test_sensors(
