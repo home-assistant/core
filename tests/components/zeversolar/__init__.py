91eb197e4345ba00c34dad1622d69a43646067b8
Christian Neumeier <christian.neumeier@br-automation.com>
2024-05-24 11:42:07 +0000

    Use snapshot tests instead of mocked tests.

--- tests/components/zeversolar/__init__.py
+++ tests/components/zeversolar/__init__.py
@@ -1,1 +1,51 @@
 """Tests for the Zeversolar integration."""
+
+from unittest.mock import patch
+
+from zeversolar import StatusEnum, ZeverSolarData
+
+from homeassistant.components.zeversolar.const import DOMAIN
+from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+
+MOCK_HOST_ZEVERSOLAR = "zeversolar-fake-host"
+MOCK_PORT_ZEVERSOLAR = 10200
+
+
+async def init_integration(hass: HomeAssistant) -> MockConfigEntry:
+    """Mock integration setup."""
+
+    zeverData = ZeverSolarData(
+        wifi_enabled=False,
+        serial_or_registry_id="1223",
+        registry_key="A-2",
+        hardware_version="M10",
+        software_version="123-23",
+        reported_datetime="19900101 23:00",
+        communication_status=StatusEnum.OK,
+        num_inverters=1,
+        serial_number="123456778",
+        pac=1234,
+        energy_today=123,
+        status=StatusEnum.OK,
+        meter_status=StatusEnum.OK,
+    )
+
+    with (
+        patch("zeversolar.ZeverSolarClient.get_data", return_value=zeverData),
+    ):
+        entry = MockConfigEntry(
+            domain=DOMAIN,
+            data={
+                CONF_HOST: MOCK_HOST_ZEVERSOLAR,
+                CONF_PORT: MOCK_PORT_ZEVERSOLAR,
+            },
+            entry_id="my_id",
+        )
+
+        entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+        return entry
--- tests/components/zeversolar/test_sensor.py
+++ tests/components/zeversolar/test_sensor.py
@@ -2,30 +2,25 @@
 
 from unittest.mock import patch
 
-from zeversolar import StatusEnum, ZeverSolarClient, ZeverSolarData
+from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
-    SensorEntity,
     SensorEntityDescription,
     SensorStateClass,
 )
-from homeassistant.components.zeversolar.const import DOMAIN
-from homeassistant.components.zeversolar.coordinator import ZeversolarCoordinator
-from homeassistant.components.zeversolar.entity import ZeversolarEntity
-from homeassistant.components.zeversolar.sensor import (
-    ZeversolarEntityDescription,
-    ZeversolarSensor,
-    async_setup_entry,
-)
-from homeassistant.const import EntityCategory, UnitOfPower
+from homeassistant.components.zeversolar.sensor import ZeversolarEntityDescription
+from homeassistant.const import EntityCategory, Platform, UnitOfPower
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
-from tests.common import MockConfigEntry, MockModule, mock_integration
+from . import init_integration
+
+from tests.common import snapshot_platform
 
 
-async def test_ZeversolarEntityDescription_constructor(hass: HomeAssistant) -> None:
-    """Simple test for construction and initialization."""
+async def ZeversolarEntityDescription_constructor(hass: HomeAssistant) -> None:
+    """Perform simple tests for construction and initialization."""
 
     description = ZeversolarEntityDescription(
         key="pac",
@@ -41,149 +36,15 @@ async def test_ZeversolarEntityDescription_constructor(hass: HomeAssistant) -> N
     assert issubclass(type(description), SensorEntityDescription)
 
 
-def async_add_entities2(entities):
-    """Add entities to a sensor as simulation for unit test. Helper method."""
-    for value in entities:
-        assert value.__class__ is ZeversolarSensor
+async def test_sensors(
+    hass: HomeAssistant, entity_registry: er.EntityRegistry, snapshot: SnapshotAssertion
+) -> None:
+    """Test sensors."""
 
+    with patch(
+        "homeassistant.components.zeversolar.PLATFORMS",
+        [Platform.SENSOR],
+    ):
+        entry = await init_integration(hass)
 
-async def test_async_setup_entry(hass: HomeAssistant) -> None:
-    """Test the sensor setup."""
-    mock_integration(hass, MockModule(DOMAIN))
-
-    config = MockConfigEntry(
-        data={
-            "host": "my host",
-            "port": 10200,
-            "data": "test",
-        },
-        domain=DOMAIN,
-        options={},
-        title="My NEW_DOMAIN",
-    )
-
-    zeverData = ZeverSolarData(
-        wifi_enabled=False,
-        serial_or_registry_id="1223",
-        registry_key="A-2",
-        hardware_version="M10",
-        software_version="123-23",
-        reported_datetime="19900101 23:00",
-        communication_status=StatusEnum.OK,
-        num_inverters=1,
-        serial_number="123456778",
-        pac=1234,
-        energy_today=123,
-        status=StatusEnum.OK,
-        meter_status=StatusEnum.OK,
-    )
-
-    if hass.data.get(DOMAIN) is None:
-        hass.data.setdefault(
-            DOMAIN,
-            {
-                "data": "test2",
-            },
-        )
-
-    config.add_to_hass(hass)
-
-    with patch.object(ZeverSolarClient, "get_data") as client_mock:
-        coordinator = ZeversolarCoordinator(hass=hass, entry=config)
-        coordinator.data = zeverData
-        hass.data[DOMAIN][config.entry_id] = coordinator
-
-        client_mock.return_value = zeverData
-
-        await coordinator._async_update_data()
-        assert coordinator.last_update_success
-
-        await async_setup_entry(hass, config, async_add_entities2)
-
-    # assert
-    # is done in async_add_entities
-
-
-async def test_ZeversolarSensor_native_value(hass: HomeAssistant) -> None:
-    """Simple test for construction and initialization."""
-
-    description = ZeversolarEntityDescription(
-        key="pac",
-        translation_key="pac",
-        native_unit_of_measurement=UnitOfPower.WATT,
-        state_class=SensorStateClass.MEASUREMENT,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        device_class=SensorDeviceClass.POWER,
-        value_fn=lambda data: data.pac,
-    )
-
-    config = MockConfigEntry(
-        data={
-            "host": "my host",
-            "port": 10200,
-            "data": "test",
-        },
-        domain=DOMAIN,
-        options={},
-        title="My NEW_DOMAIN",
-    )
-
-    mock_integration(hass, MockModule(DOMAIN))
-
-    config = MockConfigEntry(
-        data={
-            "host": "my host",
-            "port": 10200,
-            "data": "test",
-        },
-        domain=DOMAIN,
-        options={},
-        title="My NEW_DOMAIN",
-    )
-
-    zeverData = ZeverSolarData(
-        wifi_enabled=False,
-        serial_or_registry_id="1223",
-        registry_key="A-2",
-        hardware_version="M10",
-        software_version="123-23",
-        reported_datetime="19900101 23:00",
-        communication_status=StatusEnum.OK,
-        num_inverters=1,
-        serial_number="123456778",
-        pac=1234,
-        energy_today=123,
-        status=StatusEnum.OK,
-        meter_status=StatusEnum.OK,
-    )
-
-    if hass.data.get(DOMAIN) is None:
-        hass.data.setdefault(
-            DOMAIN,
-            {
-                "data": "test2",
-            },
-        )
-
-    config.add_to_hass(hass)
-
-    with patch.object(ZeverSolarClient, "get_data") as client_mock:
-        coordinator = ZeversolarCoordinator(hass=hass, entry=config)
-        coordinator.data = zeverData
-        hass.data[DOMAIN][config.entry_id] = coordinator
-
-        client_mock.return_value = zeverData
-
-        await coordinator._async_update_data()
-        assert coordinator.last_update_success
-
-        await async_setup_entry(hass, config, async_add_entities2)
-
-        sensor = ZeversolarSensor(description=description, coordinator=coordinator)
-
-        assert type(sensor) is ZeversolarSensor
-        assert issubclass(type(sensor), ZeversolarEntity)
-        assert issubclass(type(sensor), SensorEntity)
-
-        result_value = sensor.native_value
-        assert result_value == zeverData.pac
+        await snapshot_platform(hass, entity_registry, snapshot, entry.entry_id)
