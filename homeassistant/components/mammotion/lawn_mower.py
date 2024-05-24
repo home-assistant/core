787de0a08e14b6378eb58b3458c9535a75df9e1b
Michael Arthur <michael.arthur@gameglass.gg>
2024-05-24 16:37:14 +1200

    further fixes that make activity work

--- homeassistant/components/mammotion/lawn_mower.py
+++ homeassistant/components/mammotion/lawn_mower.py
@@ -1,5 +1,8 @@
 """Luba lawn mowers."""
+
 from __future__ import annotations
+
+from pyluba.utility.constant.device_constant import work_mode
 
 from homeassistant.components.lawn_mower import (
     LawnMowerActivity,
@@ -7,56 +10,101 @@ from homeassistant.components.lawn_mower import (
     LawnMowerEntityFeature,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN
+from .coordinator import MammotionDataUpdateCoordinator
+
+SUPPORTED_FEATURES = (
+    LawnMowerEntityFeature.DOCK
+    | LawnMowerEntityFeature.PAUSE
+    | LawnMowerEntityFeature.START_MOWING
+)
 
 
 async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
+    coordinator: MammotionDataUpdateCoordinator,
     async_add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up luba lawn mower."""
+
     async_add_entities(
         [
-            LubaLawnMower(
-                "uuid of Luba",
-                "Luba (serial number or name)",
-                LawnMowerActivity.PAUSED, # find out what state Luba is in
-                LawnMowerEntityFeature.DOCK
-                | LawnMowerEntityFeature.PAUSE
-                | LawnMowerEntityFeature.START_MOWING,
-            ),
-        ]
+            MammotionLawnMowerEntity(config.get("title"), coordinator),
+        ],
+        update_before_add=True,
     )
 
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    config_entry: ConfigEntry,
+    entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the Luba config entry."""
-    await async_setup_platform(hass, {}, async_add_entities)
+    coordinator: MammotionDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+    await async_setup_platform(
+        hass, {"title": entry.title}, coordinator, async_add_entities
+    )
 
 
-class LubaLawnMower(LawnMowerEntity):
+class MammotionLawnMowerEntity(
+    CoordinatorEntity[MammotionDataUpdateCoordinator], LawnMowerEntity
+):
     """Representation of a Luba lawn mower."""
 
+    _attr_supported_features = SUPPORTED_FEATURES
+    _attr_has_entity_name = True
+
     def __init__(
-        self,
-        unique_id: str,
-        name: str,
-        activity: LawnMowerActivity,
-        features: LawnMowerEntityFeature = LawnMowerEntityFeature(0),
+        self, device_name: str, coordinator: MammotionDataUpdateCoordinator
     ) -> None:
         """Initialize the lawn mower."""
-        self._attr_name = name
-        self._attr_unique_id = unique_id
-        self._attr_supported_features = features
-        self._attr_activity = activity
+        super().__init__(coordinator)
+        self._attr_name = device_name
+        self._attr_unique_id = f"{device_name}"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, device_name)},
+            manufacturer="Mammotion",
+            name=device_name,
+            suggested_area="Garden",
+        )
+
+    def _get_mower_activity(self) -> LawnMowerActivity:
+        mode = "FAIL"
+        if "sys" in self.coordinator.device.raw_data:
+            if "toappReportData" in self.coordinator.device.raw_data["sys"]:
+                mode = self.coordinator.device.raw_data["sys"]["toappReportData"][
+                    "dev"
+                ]["sysStatus"]
+        print(mode)
+        if mode == work_mode.MODE_PAUSE.value:
+            return LawnMowerActivity.PAUSED
+        if mode == work_mode.MODE_WORKING.value:
+            return LawnMowerActivity.MOWING
+        if mode == work_mode.MODE_LOCK.value:
+            return LawnMowerActivity.ERROR
+        if (
+            mode == work_mode.MODE_CHARGING.value
+            or mode == work_mode.MODE_READY.value
+            or mode == work_mode.MODE_RETURNING.value
+        ):
+            return LawnMowerActivity.DOCKED
+
+        return self._attr_activity
+
+    @property
+    def activity(self) -> LawnMowerActivity:
+        """Return the state of the mower."""
+        # productkey = coordinator.device.raw_data['net']['toappWifiIotStatus']['productkey']
+        # devicename = coordinator.device.raw_data['net']['toappWifiIotStatus']['devicename']
+        return self._get_mower_activity()
 
     async def async_start_mowing(self) -> None:
         """Start mowing."""
@@ -71,4 +119,12 @@ class LubaLawnMower(LawnMowerEntity):
     async def async_pause(self) -> None:
         """Pause mower."""
         self._attr_activity = LawnMowerActivity.PAUSED
+        self.async_write_ha_state()
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        print("coordinator callback")
+        print(self.coordinator.device.raw_data)
+        self._attr_activity = self._get_mower_activity()
         self.async_write_ha_state()
--- homeassistant/generated/config_flows.py
+++ homeassistant/generated/config_flows.py
@@ -390,6 +390,7 @@ FLOWS = {
         "lyric",
         "madvr",
         "mailgun",
+	"mammotion",
         "mastodon",
         "matter",
         "mcp",
