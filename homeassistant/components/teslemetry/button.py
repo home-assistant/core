f4ad758249c428585bde1fba78c130826960f103
Brett <brett.whynot@gmail.com>
2024-05-24 09:43:34 +0000

    import Awaitable

--- homeassistant/components/teslemetry/button.py
+++ homeassistant/components/teslemetry/button.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from collections.abc import Callable
+from collections.abc import Awaitable, Callable
 from dataclasses import dataclass
 from typing import Any
 
