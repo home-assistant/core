68a221d2a24bb170523ba3c213f67cb03f00397e
Robert Hillis <tkdrob4390@yahoo.com>
2024-05-26 13:03:16 -0400

    move assertion to snapshot

--- tests/components/radarr/test_calendar.py
+++ tests/components/radarr/test_calendar.py
@@ -3,9 +3,10 @@
 from datetime import timedelta
 
 from freezegun.api import FrozenDateTimeFactory
+from syrupy import SnapshotAssertion
 
 from homeassistant.components.radarr.const import DOMAIN
-from homeassistant.const import STATE_OFF, STATE_ON
+from homeassistant.const import STATE_OFF
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
@@ -18,20 +19,14 @@ async def test_calendar(
     hass: HomeAssistant,
     aioclient_mock: AiohttpClientMocker,
     freezer: FrozenDateTimeFactory,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test for successfully setting up the Radarr platform."""
     freezer.move_to("2021-12-02 00:00:00-08:00")
     entry = await setup_integration(hass, aioclient_mock)
     coordinator: DataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]["calendar"]
 
-    state = hass.states.get("calendar.mock_title")
-    assert state.state == STATE_ON
-    assert state.attributes.get("all_day") is True
-    assert state.attributes.get("description") == "test2"
-    assert state.attributes.get("end_time") == "2021-12-03 00:00:00"
-    assert state.attributes.get("message") == "test"
-    assert state.attributes.get("release_type") == "physicalRelease"
-    assert state.attributes.get("start_time") == "2021-12-02 00:00:00"
+    assert hass.states.get("calendar.mock_title") == snapshot
 
     freezer.tick(timedelta(hours=16))
     await coordinator.async_refresh()
