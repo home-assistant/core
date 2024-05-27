22bb8379223df4027a27f40c3466b37efd0611e5
francoisverbeek <46888871+francoisverbeek@users.noreply.github.com>
2024-05-27 18:52:59 +0100

    Update homeassistant/components/sunsynkweb/__init__.py
    Co-authored-by: Josef Zweck <24647999+zweckj@users.noreply.github.com>

--- homeassistant/components/sunsynkweb/__init__.py
+++ homeassistant/components/sunsynkweb/__init__.py
@@ -17,7 +17,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})
     coordinator = PlantUpdateCoordinator(hass, entry)
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    entry.runtime_data = coordinator
     await coordinator.async_config_entry_first_refresh()
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
