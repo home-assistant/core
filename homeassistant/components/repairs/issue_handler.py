3c1c09cb577f389448d6217dd1cf9914644d4a5c
epenet <6771947+epenet@users.noreply.github.com>
2024-05-27 10:19:00 +0000

    Revert "More core files"
    This reverts commit 9978b9370629af402a9a18f184b6f3a7ad45b08d.

--- homeassistant/components/repairs/issue_handler.py
+++ homeassistant/components/repairs/issue_handler.py
@@ -9,9 +9,12 @@ import voluptuous as vol
 from homeassistant import data_entry_flow
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers import issue_registry as ir
 from homeassistant.helpers.integration_platform import (
     async_process_integration_platforms,
+)
+from homeassistant.helpers.issue_registry import (
+    async_delete_issue,
+    async_get as async_get_issue_registry,
 )
 
 from .const import DOMAIN
@@ -34,7 +37,7 @@ class ConfirmRepairFlow(RepairsFlow):
         if user_input is not None:
             return self.async_create_entry(data={})
 
-        issue_registry = ir.async_get(self.hass)
+        issue_registry = async_get_issue_registry(self.hass)
         description_placeholders = None
         if issue := issue_registry.async_get_issue(self.handler, self.issue_id):
             description_placeholders = issue.translation_placeholders
@@ -60,7 +63,7 @@ class RepairsFlowManager(data_entry_flow.FlowManager):
         assert data and "issue_id" in data
         issue_id = data["issue_id"]
 
-        issue_registry = ir.async_get(self.hass)
+        issue_registry = async_get_issue_registry(self.hass)
         issue = issue_registry.async_get_issue(handler_key, issue_id)
         if issue is None or not issue.is_fixable:
             raise data_entry_flow.UnknownStep
@@ -84,7 +87,7 @@ class RepairsFlowManager(data_entry_flow.FlowManager):
     ) -> data_entry_flow.FlowResult:
         """Complete a fix flow."""
         if result.get("type") != data_entry_flow.FlowResultType.ABORT:
-            ir.async_delete_issue(self.hass, flow.handler, flow.init_data["issue_id"])
+            async_delete_issue(self.hass, flow.handler, flow.init_data["issue_id"])
         if "result" not in result:
             result["result"] = None
         return result
