76ed0fddc55b5feac03bcdf6801c62c1fe14477c
mib1185 <mail@mib85.de>
2024-05-27 10:52:37 +0000

    apply suggestions

--- homeassistant/components/feedreader/coordinator.py
+++ homeassistant/components/feedreader/coordinator.py
@@ -135,26 +135,22 @@ class FeedReaderCoordinator(DataUpdateCoordinator[None]):
         """Publish new entries to the event bus."""
         assert self._feed is not None
         new_entry_count = 0
-        firstrun = True
+        firstrun = False
         self._last_entry_timestamp = self._storage.get_timestamp(self._feed_id)
-        if self._last_entry_timestamp:
-            firstrun = False
+        if not self._last_entry_timestamp:
+            firstrun = True
         else:
             # Set last entry timestamp as epoch time if not available
             self._last_entry_timestamp = dt_util.utc_from_timestamp(0).timetuple()
         # locally cache self._last_entry_timestamp so that entries published at identical times can be processed
         last_entry_timestamp = self._last_entry_timestamp
         for entry in self._feed.entries:
-            if (
-                firstrun
-                or (
-                    "published_parsed" in entry
-                    and entry.published_parsed > last_entry_timestamp
+            if firstrun or (
+                (
+                    time_stamp := entry.get("updated_parsed")
+                    or entry.get("published_parsed")
                 )
-                or (
-                    "updated_parsed" in entry
-                    and entry.updated_parsed > last_entry_timestamp
-                )
+                and time_stamp > last_entry_timestamp
             ):
                 self._update_and_fire_entry(entry)
                 new_entry_count += 1
