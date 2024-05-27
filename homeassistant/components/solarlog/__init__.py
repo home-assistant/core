dc92749327a08f07969440d41862421dc5cf057d
dontinelli <73341522+dontinelli@users.noreply.github.com>
2024-05-27 15:55:04 +0000

    add entity uid migration

--- homeassistant/components/solarlog/__init__.py
+++ homeassistant/components/solarlog/__init__.py
@@ -5,6 +5,7 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from .const import DOMAIN
 from .coordinator import SolarlogData
@@ -38,12 +39,29 @@ async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) ->
 
     if config_entry.version == 1:
         if config_entry.minor_version < 2:
+            # migrate config_entry
             new = {**config_entry.data}
             new["extended_data"] = False
 
             hass.config_entries.async_update_entry(
                 config_entry, data=new, minor_version=2, version=1
             )
+
+            # migrate old entity unique id
+            entity_reg = er.async_get(hass)
+            entities: list[er.RegistryEntry] = er.async_entries_for_config_entry(
+                entity_reg, config_entry.entry_id
+            )
+
+            for entity in entities:
+                if "time" in entity.unique_id:
+                    new_uid = entity.unique_id.replace("time", "last_updated")
+                    _LOGGER.debug(
+                        "migrate unique id '%s' to '%s'", entity.unique_id, new_uid
+                    )
+                    entity_reg.async_update_entity(
+                        entity.entity_id, new_unique_id=new_uid
+                    )
 
     _LOGGER.debug(
         "Migration to version %s.%s successful",
