8aa28b6708cee2988bb8d44a20922fc8a45dc405
francoisverbeek <46888871+francoisverbeek@users.noreply.github.com>
2024-05-27 18:55:21 +0100

    Update homeassistant/components/sunsynkweb/config_flow.py
    Co-authored-by: Josef Zweck <24647999+zweckj@users.noreply.github.com>

--- homeassistant/components/sunsynkweb/config_flow.py
+++ homeassistant/components/sunsynkweb/config_flow.py
@@ -63,7 +63,6 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 class ConfigFlow(BaseConfigFlow, domain=DOMAIN):
     """Handle a config flow for Sunsynk Inverter Web."""
 
-    VERSION = 1
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
