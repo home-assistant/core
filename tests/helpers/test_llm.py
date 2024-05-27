b9225050abc8ef98a23f0f71c4bbb35054913aee
Paulus Schoutsen <balloob@gmail.com>
2024-05-27 01:04:49 +0000

    Add exposed entities to the Assist LLM API prompt

--- tests/helpers/test_llm.py
+++ tests/helpers/test_llm.py
@@ -11,12 +11,16 @@ from homeassistant.helpers import (
     area_registry as ar,
     config_validation as cv,
     device_registry as dr,
+    entity_registry as er,
     floor_registry as fr,
     intent,
     llm,
 )
+from homeassistant.setup import async_setup_component
+from homeassistant.util import yaml
 
 from tests.common import MockConfigEntry
+from tests.typing import WebSocketGenerator
 
 
 async def test_get_api_no_existing(hass: HomeAssistant) -> None:
@@ -157,10 +161,13 @@ async def test_assist_api_description(hass: HomeAssistant) -> None:
 async def test_assist_api_prompt(
     hass: HomeAssistant,
     device_registry: dr.DeviceRegistry,
+    entity_registry: er.EntityRegistry,
     area_registry: ar.AreaRegistry,
     floor_registry: fr.FloorRegistry,
+    hass_ws_client: WebSocketGenerator,
 ) -> None:
     """Test prompt for the assist API."""
+    assert await async_setup_component(hass, "homeassistant", {})
     context = Context()
     tool_input = llm.ToolInput(
         tool_name=None,
@@ -169,41 +176,99 @@ async def test_assist_api_prompt(
         context=context,
         user_prompt="test_text",
         language="*",
-        assistant="test_assistant",
+        assistant="conversation",
         device_id="test_device",
     )
     api = llm.async_get_api(hass, "assist")
     prompt = await api.async_get_api_prompt(tool_input)
     assert prompt == (
-        "Call the intent tools to control Home Assistant."
-        " Just pass the name to the intent."
+        "Only if the user wants to control a device, tell them to expose entities to their "
+        "voice assistant in Home Assistant."
     )
 
+    # Expose entities
     entry = MockConfigEntry(title=None)
     entry.add_to_hass(hass)
-    tool_input.device_id = device_registry.async_get_or_create(
+    device = device_registry.async_get_or_create(
         config_entry_id=entry.entry_id,
         connections={("test", "1234")},
-        name="Test Device",
-        manufacturer="Test Manufacturer",
-        model="Test Model",
         suggested_area="Test Area",
-    ).id
-    prompt = await api.async_get_api_prompt(tool_input)
-    assert prompt == (
-        "Call the intent tools to control Home Assistant."
-        " Just pass the name to the intent. You are in Test Area."
+    )
+    area = area_registry.async_get_area_by_name("Test Area")
+    area_registry.async_update(area.id, aliases=["Alternative name"])
+    entry1 = entity_registry.async_get_or_create(
+        "light", "kitchen", "mock-id-kitchen", original_name="Kitchen"
+    )
+    entry2 = entity_registry.async_get_or_create(
+        "light",
+        "living_room",
+        "mock-id-living-room",
+        original_name="Living Room",
+        device_id=device.id,
+    )
+    hass.states.async_set(entry1.entity_id, "on", {"friendly_name": "Kitchen"})
+    hass.states.async_set(entry2.entity_id, "on", {"friendly_name": "Living Room"})
+    # Set options for registered entities
+    ws_client = await hass_ws_client(hass)
+    await ws_client.send_json_auto_id(
+        {
+            "type": "homeassistant/expose_entity",
+            "assistants": ["conversation"],
+            "entity_ids": [entry1.entity_id, entry2.entity_id],
+            "should_expose": True,
+        }
+    )
+    response = await ws_client.receive_json()
+    assert response["success"]
+
+    exposed_entities = llm._get_exposed_entities(hass, tool_input.assistant)
+    assert exposed_entities == {
+        entry1.entity_id: {
+            "names": "Kitchen",
+            "state": "on",
+        },
+        entry2.entity_id: {
+            "areas": "Test Area, Alternative name",
+            "names": "Living Room",
+            "state": "on",
+        },
+    }
+    exposed_entities_prompt = (
+        "An overview of the areas and the devices in this smart home:\n"
+        + yaml.dump(exposed_entities)
+    )
+    first_part_prompt = (
+        "Call the intent tools to control Home Assistant. "
+        "Just pass the name to the intent. "
+        "When controlling an area, prefer passing area name."
     )
 
+    prompt = await api.async_get_api_prompt(tool_input)
+    assert prompt == (
+        f"""{first_part_prompt}
+{exposed_entities_prompt}"""
+    )
+
+    # Fake that request is made from a specific device ID
+    tool_input.device_id = device.id
+    prompt = await api.async_get_api_prompt(tool_input)
+    assert prompt == (
+        f"""{first_part_prompt}
+You are in Test Area.
+{exposed_entities_prompt}"""
+    )
+
+    # Add floor
     floor = floor_registry.async_create("second floor")
-    area = area_registry.async_get_area_by_name("Test Area")
     area_registry.async_update(area.id, floor_id=floor.floor_id)
     prompt = await api.async_get_api_prompt(tool_input)
     assert prompt == (
-        "Call the intent tools to control Home Assistant."
-        " Just pass the name to the intent. You are in Test Area (second floor)."
+        f"""{first_part_prompt}
+You are in Test Area (second floor).
+{exposed_entities_prompt}"""
     )
 
+    # Add user
     context.user_id = "12345"
     mock_user = Mock()
     mock_user.id = "12345"
@@ -211,7 +276,8 @@ async def test_assist_api_prompt(
     with patch("homeassistant.auth.AuthManager.async_get_user", return_value=mock_user):
         prompt = await api.async_get_api_prompt(tool_input)
     assert prompt == (
-        "Call the intent tools to control Home Assistant."
-        " Just pass the name to the intent. You are in Test Area (second floor)."
-        " The user name is Test User."
+        f"""{first_part_prompt}
+You are in Test Area (second floor).
+The user name is Test User.
+{exposed_entities_prompt}"""
     )
