df723cc0b144adc1613e9050ea3fc912eb6d60bf
cRemE-fReSh <42785404+cRemE-fReSh@users.noreply.github.com>
2024-05-29 20:59:58 +0200

    Update number.py
    Support for znrb added

--- homeassistant/components/tuya/number.py
+++ homeassistant/components/tuya/number.py
@@ -277,6 +277,14 @@ NUMBERS: dict[str, tuple[NumberEntityDescription, ...]] = {
             device_class=NumberDeviceClass.TEMPERATURE,
         ),
     ),
+    # Poolsana Pool HeatPump
+    "znrb": (
+        NumberEntityDescription(
+            key=DPCode.TEMP_SET,
+            name="Set Temperature",
+            device_class=NumberDeviceClass.TEMPERATURE,
+        ),
+    ),
 }
 
 
