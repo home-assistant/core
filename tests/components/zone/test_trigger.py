ecb6dbb3002fd64503e932cc2a79a9240870e9e3
epenet <6771947+epenet@users.noreply.github.com>
2024-05-29 09:30:41 +0200

    Improve typing for `calls` fixture in tests (m-z) (#118350)
    * Improve typing for `calls` fixture in tests (m-z)
    
    * More
    
    * More

--- tests/components/zone/test_trigger.py
+++ tests/components/zone/test_trigger.py
@@ -4,7 +4,7 @@ import pytest
 
 from homeassistant.components import automation, zone
 from homeassistant.const import ATTR_ENTITY_ID, ENTITY_MATCH_ALL, SERVICE_TURN_OFF
-from homeassistant.core import Context, HomeAssistant
+from homeassistant.core import Context, HomeAssistant, ServiceCall
 from homeassistant.helpers import entity_registry as er
 from homeassistant.setup import async_setup_component
 
@@ -17,7 +17,7 @@ def stub_blueprint_populate_autouse(stub_blueprint_populate: None) -> None:
 
 
 @pytest.fixture
-def calls(hass):
+def calls(hass: HomeAssistant) -> list[ServiceCall]:
     """Track calls to a mock service."""
     return async_mock_service(hass, "test", "automation")
 
@@ -42,7 +42,9 @@ def setup_comp(hass):
     )
 
 
-async def test_if_fires_on_zone_enter(hass: HomeAssistant, calls) -> None:
+async def test_if_fires_on_zone_enter(
+    hass: HomeAssistant, calls: list[ServiceCall]
+) -> None:
     """Test for firing on zone enter."""
     context = Context()
     hass.states.async_set(
@@ -112,7 +114,7 @@ async def test_if_fires_on_zone_enter(hass: HomeAssistant, calls) -> None:
 
 
 async def test_if_fires_on_zone_enter_uuid(
-    hass: HomeAssistant, entity_registry: er.EntityRegistry, calls
+    hass: HomeAssistant, entity_registry: er.EntityRegistry, calls: list[ServiceCall]
 ) -> None:
     """Test for firing on zone enter when device is specified by entity registry id."""
     context = Context()
@@ -188,7 +190,9 @@ async def test_if_fires_on_zone_enter_uuid(
     assert len(calls) == 1
 
 
-async def test_if_not_fires_for_enter_on_zone_leave(hass: HomeAssistant, calls) -> None:
+async def test_if_not_fires_for_enter_on_zone_leave(
+    hass: HomeAssistant, calls: list[ServiceCall]
+) -> None:
     """Test for not firing on zone leave."""
     hass.states.async_set(
         "test.entity", "hello", {"latitude": 32.880586, "longitude": -117.237564}
@@ -219,7 +223,9 @@ async def test_if_not_fires_for_enter_on_zone_leave(hass: HomeAssistant, calls) 
     assert len(calls) == 0
 
 
-async def test_if_fires_on_zone_leave(hass: HomeAssistant, calls) -> None:
+async def test_if_fires_on_zone_leave(
+    hass: HomeAssistant, calls: list[ServiceCall]
+) -> None:
     """Test for firing on zone leave."""
     hass.states.async_set(
         "test.entity", "hello", {"latitude": 32.880586, "longitude": -117.237564}
@@ -250,7 +256,9 @@ async def test_if_fires_on_zone_leave(hass: HomeAssistant, calls) -> None:
     assert len(calls) == 1
 
 
-async def test_if_not_fires_for_leave_on_zone_enter(hass: HomeAssistant, calls) -> None:
+async def test_if_not_fires_for_leave_on_zone_enter(
+    hass: HomeAssistant, calls: list[ServiceCall]
+) -> None:
     """Test for not firing on zone enter."""
     hass.states.async_set(
         "test.entity", "hello", {"latitude": 32.881011, "longitude": -117.234758}
@@ -281,7 +289,7 @@ async def test_if_not_fires_for_leave_on_zone_enter(hass: HomeAssistant, calls) 
     assert len(calls) == 0
 
 
-async def test_zone_condition(hass: HomeAssistant, calls) -> None:
+async def test_zone_condition(hass: HomeAssistant, calls: list[ServiceCall]) -> None:
     """Test for zone condition."""
     hass.states.async_set(
         "test.entity", "hello", {"latitude": 32.880586, "longitude": -117.237564}
@@ -310,7 +318,7 @@ async def test_zone_condition(hass: HomeAssistant, calls) -> None:
 
 
 async def test_unknown_zone(
-    hass: HomeAssistant, calls, caplog: pytest.LogCaptureFixture
+    hass: HomeAssistant, calls: list[ServiceCall], caplog: pytest.LogCaptureFixture
 ) -> None:
     """Test for firing on zone enter."""
     context = Context()
