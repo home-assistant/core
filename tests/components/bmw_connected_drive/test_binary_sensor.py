22b154fb769a39eb51979a0955e54a0755027647
Richard <rikroe@users.noreply.github.com>
2024-05-29 23:23:58 +0200

    Use entity_registry_enabled_by_default fixture

--- tests/components/bmw_connected_drive/test_binary_sensor.py
+++ tests/components/bmw_connected_drive/test_binary_sensor.py
@@ -14,6 +14,7 @@ from . import setup_mocked_integration
 async def test_entity_state_attrs(
     hass: HomeAssistant,
     bmw_fixture: respx.Router,
+    entity_registry_enabled_by_default: None,
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test binary sensor states and attributes."""
