7ee2f09fe120e57a7b272a1b07bd33fd33988220
J. Nick Koston <nick@koston.org>
2024-05-29 09:55:34 -1000

    Ensure paho.mqtt.client is imported in the executor (#118412)
    fixes #118405

--- homeassistant/components/mqtt/__init__.py
+++ homeassistant/components/mqtt/__init__.py
@@ -244,7 +244,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             websocket_api.async_register_command(hass, websocket_subscribe)
             websocket_api.async_register_command(hass, websocket_mqtt_info)
             hass.data[DATA_MQTT] = mqtt_data = MqttData(config=mqtt_yaml, client=client)
-        client.start(mqtt_data)
+        await client.async_start(mqtt_data)
 
         # Restore saved subscriptions
         if mqtt_data.subscriptions_to_restore:
