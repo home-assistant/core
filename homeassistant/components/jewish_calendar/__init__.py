3f6df28ef38cb5e5c886d555600dc2a7064fe2b0
Tsvi Mostovicz <ttmost@gmail.com>
2024-05-31 13:35:40 +0300

    Fix YAML deprecation breaking version in jewish calendar and media extractor (#118546)
    * Fix YAML deprecation breaking version
    
    * Update
    
    * fix media extractor deprecation as well
    
    * Add issue_domain

--- homeassistant/components/jewish_calendar/__init__.py
+++ homeassistant/components/jewish_calendar/__init__.py
@@ -96,7 +96,8 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         HOMEASSISTANT_DOMAIN,
         f"deprecated_yaml_{DOMAIN}",
         is_fixable=False,
-        breaks_in_ha_version="2024.10.0",
+        issue_domain=DOMAIN,
+        breaks_in_ha_version="2024.12.0",
         severity=IssueSeverity.WARNING,
         translation_key="deprecated_yaml",
         translation_placeholders={
