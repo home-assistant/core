724bcf8d7a7cbf786c32d1abff1342994fc3a379
J. Nick Koston <nick@koston.org>
2024-05-30 18:11:41 -1000

    fix

--- homeassistant/components/openweathermap/utils.py
+++ homeassistant/components/openweathermap/utils.py
@@ -33,5 +33,5 @@ def build_data_and_options(
 ) -> tuple[dict[str, Any], dict[str, Any]]:
     """Split combined data and options."""
     data = {k: v for k, v in combined_data.items() if k not in OPTION_DEFAULTS}
-    options = {k: combined_data.get(v, v) for k, v in OPTION_DEFAULTS.items()}
+    options = {k: combined_data.get(k, v) for k, v in OPTION_DEFAULTS.items()}
     return (data, options)
