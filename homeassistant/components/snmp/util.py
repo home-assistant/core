8e882ab9dbad1861894d83c829bb6138bb41315d
J. Nick Koston <nick@koston.org>
2024-05-30 15:03:45 -1000

    engine does i/o as well

--- homeassistant/components/snmp/util.py
+++ homeassistant/components/snmp/util.py
@@ -40,17 +40,17 @@ async def async_create_request_cmd_args(
 ) -> RequestArgsType:
     """Create request arguments."""
     return await hass.async_add_executor_job(
-        _create_request_cmd_args, snmp_engine(), auth_data, target, object_id
+        _create_request_cmd_args, auth_data, target, object_id
     )
 
 
 def _create_request_cmd_args(
-    engine: SnmpEngine,
     auth_data: UsmUserData | CommunityData,
     target: UdpTransportTarget | Udp6TransportTarget,
     object_id: str,
 ) -> RequestArgsType:
     """Create request arguments."""
+    engine = snmp_engine()
     context_data = ContextData()
     object_type = ObjectType(ObjectIdentity(object_id))
     return (engine, auth_data, target, context_data, object_type)
