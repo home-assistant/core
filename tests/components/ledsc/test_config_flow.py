bf0e38c49ea6c0185a5de8e69844df21bdb075e2
Patrik Kratochvil <patrik.k.010@seznam.cz>
2024-06-02 13:25:22 +0200

    Fix tests && Up websc-client to 1.0.3

--- tests/components/ledsc/test_config_flow.py
+++ tests/components/ledsc/test_config_flow.py
@@ -1,7 +1,11 @@
-from unittest.mock import patch
+"""Test the LedSc config flow."""
 
-from homeassistant import config_entries, data_entry_flow
+from unittest.mock import patch, AsyncMock
+
+from homeassistant import config_entries
+from homeassistant.data_entry_flow import FlowResultType
 from homeassistant.components.ledsc.consts import DOMAIN
+from websc_client.exceptions import WebSClientConnectionError
 
 USER_INPUT = {"host": "127.0.0.1", "port": 8080}
 IMPORT_CONFIG = {"host": "127.0.0.1", "port": 8080}
@@ -13,14 +17,14 @@ RESULT = {
 ENTRY_CONFIG = {"host": "127.0.0.1", "port": 8080}
 
 
-async def test_form(hass):
+async def test_form(hass) -> None:
     """Test that the form is served with no input."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_USER},
     )
 
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] is FlowResultType.FORM
     assert result["errors"] == {}
 
     with patch(
@@ -30,47 +34,56 @@ async def test_form(hass):
         "homeassistant.components.ledsc.async_setup_entry",
         return_value=True,
     ):
-        result2 = await hass.config_entries.flow.async_configure(
+        result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             USER_INPUT,
         )
-    assert result2["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result2["title"] == RESULT["title"]
-    assert result2["data"] == USER_INPUT
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == RESULT["title"]
+    assert result["data"] == USER_INPUT
 
 
-async def test_form_cannot_connect(hass):
+async def test_form_cannot_connect(hass) -> None:
     """Test we handle errors."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_USER},
     )
 
-    with patch(
-            "homeassistant.components.ledsc.config_flow.validate_input",
-            side_effect=ConnectionError,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            USER_INPUT,
-        )
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        USER_INPUT,
+    )
 
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "cannot_connect"}
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {"base": "cannot_connect"}
 
 
 async def test_form_unique_id_check(hass):
-    """Test we handle unique id check"""
+    """Try to create 2 intance integration with the same configuration"""
     await hass.config_entries.flow.async_init(
         DOMAIN,
         context={"source": config_entries.SOURCE_USER},
         data=USER_INPUT,
     )
 
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={"source": config_entries.SOURCE_USER},
-        data=USER_INPUT,
-    )
-    assert result["type"] == data_entry_flow.RESULT_TYPE_ABORT
+    magic_websc = AsyncMock()
+
+    with patch(
+            "homeassistant.components.ledsc.config_flow.WebSClient",
+            autospe=True, return_value=magic_websc,
+    ) as mock_websc:
+        mock_websc.connect.side_effect = WebSClientConnectionError
+        await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+            data=USER_INPUT,
+        )
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": config_entries.SOURCE_USER},
+            data=USER_INPUT,
+        )
+
+    assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "already_configured"
