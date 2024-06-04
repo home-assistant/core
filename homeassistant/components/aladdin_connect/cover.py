c702174fa0e8ed9929f9e1c27e639c65a6305951
Joost Lekkerkerker <joostlek@outlook.com>
2024-06-04 10:51:28 +0200

    Add coordinator to Aladdin Connect (#118781)

--- homeassistant/components/aladdin_connect/cover.py
+++ homeassistant/components/aladdin_connect/cover.py
@@ -1,9 +1,7 @@
 """Cover Entity for Genie Garage Door."""
 
-from datetime import timedelta
 from typing import Any
 
-from genie_partner_sdk.client import AladdinConnectClient
 from genie_partner_sdk.model import GarageDoor
 
 from homeassistant.components.cover import (
@@ -11,52 +9,36 @@ from homeassistant.components.cover import (
     CoverEntity,
     CoverEntityFeature,
 )
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import PlatformNotReady
 import homeassistant.helpers.device_registry as dr
-from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from . import api
+from . import AladdinConnectConfigEntry, AladdinConnectCoordinator
 from .const import DOMAIN
-
-SCAN_INTERVAL = timedelta(seconds=15)
+from .entity import AladdinConnectEntity
 
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    config_entry: ConfigEntry,
+    config_entry: AladdinConnectConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the Aladdin Connect platform."""
-    session: api.AsyncConfigEntryAuth = config_entry.runtime_data
-    acc = AladdinConnectClient(session)
-    doors = await acc.get_doors()
-    if doors is None:
-        raise PlatformNotReady("Error from Aladdin Connect getting doors")
-    device_registry = dr.async_get(hass)
-    doors_to_add = []
-    for door in doors:
-        existing = device_registry.async_get(door.unique_id)
-        if existing is None:
-            doors_to_add.append(door)
+    coordinator = config_entry.runtime_data
 
-    async_add_entities(
-        (AladdinDevice(acc, door, config_entry) for door in doors_to_add),
-    )
-    remove_stale_devices(hass, config_entry, doors)
+    async_add_entities(AladdinDevice(coordinator, door) for door in coordinator.doors)
+    remove_stale_devices(hass, config_entry)
 
 
 def remove_stale_devices(
-    hass: HomeAssistant, config_entry: ConfigEntry, devices: list[GarageDoor]
+    hass: HomeAssistant, config_entry: AladdinConnectConfigEntry
 ) -> None:
     """Remove stale devices from device registry."""
     device_registry = dr.async_get(hass)
     device_entries = dr.async_entries_for_config_entry(
         device_registry, config_entry.entry_id
     )
-    all_device_ids = {door.unique_id for door in devices}
+    all_device_ids = {door.unique_id for door in config_entry.runtime_data.doors}
 
     for device_entry in device_entries:
         device_id: str | None = None
@@ -75,45 +57,38 @@ def remove_stale_devices(
             )
 
 
-class AladdinDevice(CoverEntity):
+class AladdinDevice(AladdinConnectEntity, CoverEntity):
     """Representation of Aladdin Connect cover."""
 
     _attr_device_class = CoverDeviceClass.GARAGE
     _attr_supported_features = CoverEntityFeature.OPEN | CoverEntityFeature.CLOSE
-    _attr_has_entity_name = True
     _attr_name = None
 
     def __init__(
-        self, acc: AladdinConnectClient, device: GarageDoor, entry: ConfigEntry
+        self, coordinator: AladdinConnectCoordinator, device: GarageDoor
     ) -> None:
         """Initialize the Aladdin Connect cover."""
-        self._acc = acc
-        self._device_id = device.device_id
-        self._number = device.door_number
-
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, device.unique_id)},
-            name=device.name,
-            manufacturer="Overhead Door",
-        )
+        super().__init__(coordinator, device)
         self._attr_unique_id = device.unique_id
 
     async def async_open_cover(self, **kwargs: Any) -> None:
         """Issue open command to cover."""
-        await self._acc.open_door(self._device_id, self._number)
+        await self.coordinator.acc.open_door(
+            self._device.device_id, self._device.door_number
+        )
 
     async def async_close_cover(self, **kwargs: Any) -> None:
         """Issue close command to cover."""
-        await self._acc.close_door(self._device_id, self._number)
-
-    async def async_update(self) -> None:
-        """Update status of cover."""
-        await self._acc.update_door(self._device_id, self._number)
+        await self.coordinator.acc.close_door(
+            self._device.device_id, self._device.door_number
+        )
 
     @property
     def is_closed(self) -> bool | None:
         """Update is closed attribute."""
-        value = self._acc.get_door_status(self._device_id, self._number)
+        value = self.coordinator.acc.get_door_status(
+            self._device.device_id, self._device.door_number
+        )
         if value is None:
             return None
         return bool(value == "closed")
@@ -121,7 +96,9 @@ class AladdinDevice(CoverEntity):
     @property
     def is_closing(self) -> bool | None:
         """Update is closing attribute."""
-        value = self._acc.get_door_status(self._device_id, self._number)
+        value = self.coordinator.acc.get_door_status(
+            self._device.device_id, self._device.door_number
+        )
         if value is None:
             return None
         return bool(value == "closing")
@@ -129,7 +106,9 @@ class AladdinDevice(CoverEntity):
     @property
     def is_opening(self) -> bool | None:
         """Update is opening attribute."""
-        value = self._acc.get_door_status(self._device_id, self._number)
+        value = self.coordinator.acc.get_door_status(
+            self._device.device_id, self._device.door_number
+        )
         if value is None:
             return None
         return bool(value == "opening")
