7fc03425b434f45314e85c7f33bebab8c31e181f
Joost Lekkerkerker <joostlek@outlook.com>
2024-06-04 11:34:21 +0200

    Move Aladdin stale device removal to init module (#118784)

--- homeassistant/components/aladdin_connect/cover.py
+++ homeassistant/components/aladdin_connect/cover.py
@@ -10,11 +10,9 @@ from homeassistant.components.cover import (
     CoverEntityFeature,
 )
 from homeassistant.core import HomeAssistant
-import homeassistant.helpers.device_registry as dr
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import AladdinConnectConfigEntry, AladdinConnectCoordinator
-from .const import DOMAIN
 from .entity import AladdinConnectEntity
 
 
@@ -27,34 +25,6 @@ async def async_setup_entry(
     coordinator = config_entry.runtime_data
 
     async_add_entities(AladdinDevice(coordinator, door) for door in coordinator.doors)
-    remove_stale_devices(hass, config_entry)
-
-
-def remove_stale_devices(
-    hass: HomeAssistant, config_entry: AladdinConnectConfigEntry
-) -> None:
-    """Remove stale devices from device registry."""
-    device_registry = dr.async_get(hass)
-    device_entries = dr.async_entries_for_config_entry(
-        device_registry, config_entry.entry_id
-    )
-    all_device_ids = {door.unique_id for door in config_entry.runtime_data.doors}
-
-    for device_entry in device_entries:
-        device_id: str | None = None
-
-        for identifier in device_entry.identifiers:
-            if identifier[0] == DOMAIN:
-                device_id = identifier[1]
-                break
-
-        if device_id is None or device_id not in all_device_ids:
-            # If device_id is None an invalid device entry was found for this config entry.
-            # If the device_id is not in existing device ids it's a stale device entry.
-            # Remove config entry from this device entry in either case.
-            device_registry.async_update_device(
-                device_entry.id, remove_config_entry_id=config_entry.entry_id
-            )
 
 
 class AladdinDevice(AladdinConnectEntity, CoverEntity):
