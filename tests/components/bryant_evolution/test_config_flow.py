abcd303494cbaaf2a281c3260ede7addec056186
Daniel Myers <danielsmyers@gmail.com>
2024-06-04 19:25:25 +0000

    Update newly created tests so that they pass.

--- tests/components/bryant_evolution/test_config_flow.py
+++ tests/components/bryant_evolution/test_config_flow.py
@@ -3,12 +3,12 @@
 from unittest.mock import AsyncMock, patch
 
 from homeassistant import config_entries
-from homeassistant.components.bryant_evolution.config_flow import (
-    CannotConnect,
-    InvalidAuth,
+from homeassistant.components.bryant_evolution.const import (
+    CONF_SYSTEM_ID,
+    CONF_ZONE_ID,
+    DOMAIN,
 )
-from homeassistant.components.bryant_evolution.const import DOMAIN
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
@@ -22,76 +22,25 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     assert result["errors"] == {}
 
     with patch(
-        "homeassistant.components.bryant_evolution.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
+        "evolutionhttp.BryantEvolutionClient.read_hvac_mode",
+        return_value="COOL",
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
                 CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
+                CONF_SYSTEM_ID: 1,
+                CONF_ZONE_ID: 2,
             },
         )
-        await hass.async_block_till_done()
+    await hass.async_block_till_done()
 
     assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["title"] == "Name of the device"
+    assert result["title"] == "System 1 Zone 2"
     assert result["data"] == {
         CONF_HOST: "1.1.1.1",
-        CONF_USERNAME: "test-username",
-        CONF_PASSWORD: "test-password",
-    }
-    assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_invalid_auth(
-    hass: HomeAssistant, mock_setup_entry: AsyncMock
-) -> None:
-    """Test we handle invalid auth."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.bryant_evolution.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
-            },
-        )
-
-    assert result["type"] == FlowResultType.FORM
-    assert result["errors"] == {"base": "invalid_auth"}
-
-    # Make sure the config flow tests finish with either an
-    # FlowResultType.CREATE_ENTRY or FlowResultType.ABORT so
-    # we can show the config flow is able to recover from an error.
-    with patch(
-        "homeassistant.components.bryant_evolution.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
-    ):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
-            },
-        )
-        await hass.async_block_till_done()
-
-    assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["title"] == "Name of the device"
-    assert result["data"] == {
-        CONF_HOST: "1.1.1.1",
-        CONF_USERNAME: "test-username",
-        CONF_PASSWORD: "test-password",
+        CONF_SYSTEM_ID: 1,
+        CONF_ZONE_ID: 2,
     }
     assert len(mock_setup_entry.mock_calls) == 1
 
@@ -105,15 +54,15 @@ async def test_form_cannot_connect(
     )
 
     with patch(
-        "homeassistant.components.bryant_evolution.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
+        "evolutionhttp.BryantEvolutionClient.read_hvac_mode",
+        return_value=None,
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
                 CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
+                CONF_SYSTEM_ID: 1,
+                CONF_ZONE_ID: 2,
             },
         )
 
@@ -125,24 +74,24 @@ async def test_form_cannot_connect(
     # we can show the config flow is able to recover from an error.
 
     with patch(
-        "homeassistant.components.bryant_evolution.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
+        "evolutionhttp.BryantEvolutionClient.read_hvac_mode",
+        return_value="COOL",
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
                 CONF_HOST: "1.1.1.1",
-                CONF_USERNAME: "test-username",
-                CONF_PASSWORD: "test-password",
+                CONF_SYSTEM_ID: 1,
+                CONF_ZONE_ID: 2,
             },
         )
         await hass.async_block_till_done()
 
     assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["title"] == "Name of the device"
+    assert result["title"] == "System 1 Zone 2"
     assert result["data"] == {
         CONF_HOST: "1.1.1.1",
-        CONF_USERNAME: "test-username",
-        CONF_PASSWORD: "test-password",
+        CONF_SYSTEM_ID: 1,
+        CONF_ZONE_ID: 2,
     }
     assert len(mock_setup_entry.mock_calls) == 1
