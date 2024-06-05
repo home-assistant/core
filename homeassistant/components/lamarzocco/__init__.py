ce71519eea54ba1d470275cc27c37550d25f43e3
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-06-05 12:33:12 +0200

    revert runtimedata

--- homeassistant/components/lamarzocco/__init__.py
+++ homeassistant/components/lamarzocco/__init__.py
@@ -134,7 +134,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
 
 
 async def async_migrate_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
