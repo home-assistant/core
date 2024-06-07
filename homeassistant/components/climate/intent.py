87114bf19b97df930e47a09e106c3a830cc3c1fb
Michael Hansen <mike@rhasspy.org>
2024-06-06 20:41:25 -0500

    Fix exposure checks on some intents (#118988)
    * Check exposure in climate intent
    
    * Check exposure in todo list
    
    * Check exposure for weather
    
    * Check exposure in humidity intents
    
    * Add extra checks to weather tests
    
    * Add more checks to todo intent test
    
    * Move climate intents to async_match_targets
    
    * Update test_intent.py
    
    * Update test_intent.py
    
    * Remove patch

--- homeassistant/components/climate/intent.py
+++ homeassistant/components/climate/intent.py
@@ -4,11 +4,10 @@ from __future__ import annotations
 
 import voluptuous as vol
 
-from homeassistant.core import HomeAssistant, State
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import intent
-from homeassistant.helpers.entity_component import EntityComponent
 
-from . import DOMAIN, ClimateEntity
+from . import DOMAIN
 
 INTENT_GET_TEMPERATURE = "HassClimateGetTemperature"
 
@@ -23,7 +22,10 @@ class GetTemperatureIntent(intent.IntentHandler):
 
     intent_type = INTENT_GET_TEMPERATURE
     description = "Gets the current temperature of a climate device or entity"
-    slot_schema = {vol.Optional("area"): str, vol.Optional("name"): str}
+    slot_schema = {
+        vol.Optional("area"): intent.non_empty_string,
+        vol.Optional("name"): intent.non_empty_string,
+    }
     platforms = {DOMAIN}
 
     async def async_handle(self, intent_obj: intent.Intent) -> intent.IntentResponse:
@@ -31,74 +33,24 @@ class GetTemperatureIntent(intent.IntentHandler):
         hass = intent_obj.hass
         slots = self.async_validate_slots(intent_obj.slots)
 
-        component: EntityComponent[ClimateEntity] = hass.data[DOMAIN]
-        entities: list[ClimateEntity] = list(component.entities)
-        climate_entity: ClimateEntity | None = None
-        climate_state: State | None = None
+        name: str | None = None
+        if "name" in slots:
+            name = slots["name"]["value"]
 
-        if not entities:
-            raise intent.IntentHandleError("No climate entities")
+        area: str | None = None
+        if "area" in slots:
+            area = slots["area"]["value"]
 
-        name_slot = slots.get("name", {})
-        entity_name: str | None = name_slot.get("value")
-        entity_text: str | None = name_slot.get("text")
-
-        area_slot = slots.get("area", {})
-        area_id = area_slot.get("value")
-
-        if area_id:
-            # Filter by area and optionally name
-            area_name = area_slot.get("text")
-
-            for maybe_climate in intent.async_match_states(
-                hass, name=entity_name, area_name=area_id, domains=[DOMAIN]
-            ):
-                climate_state = maybe_climate
-                break
-
-            if climate_state is None:
-                raise intent.NoStatesMatchedError(
-                    reason=intent.MatchFailedReason.AREA,
-                    name=entity_text or entity_name,
-                    area=area_name or area_id,
-                    floor=None,
-                    domains={DOMAIN},
-                    device_classes=None,
-                )
-
-            climate_entity = component.get_entity(climate_state.entity_id)
-        elif entity_name:
-            # Filter by name
-            for maybe_climate in intent.async_match_states(
-                hass, name=entity_name, domains=[DOMAIN]
-            ):
-                climate_state = maybe_climate
-                break
-
-            if climate_state is None:
-                raise intent.NoStatesMatchedError(
-                    reason=intent.MatchFailedReason.NAME,
-                    name=entity_name,
-                    area=None,
-                    floor=None,
-                    domains={DOMAIN},
-                    device_classes=None,
-                )
-
-            climate_entity = component.get_entity(climate_state.entity_id)
-        else:
-            # First entity
-            climate_entity = entities[0]
-            climate_state = hass.states.get(climate_entity.entity_id)
-
-        assert climate_entity is not None
-
-        if climate_state is None:
-            raise intent.IntentHandleError(f"No state for {climate_entity.name}")
-
-        assert climate_state is not None
+        match_constraints = intent.MatchTargetsConstraints(
+            name=name, area_name=area, domains=[DOMAIN], assistant=intent_obj.assistant
+        )
+        match_result = intent.async_match_targets(hass, match_constraints)
+        if not match_result.is_match:
+            raise intent.MatchFailedError(
+                result=match_result, constraints=match_constraints
+            )
 
         response = intent_obj.create_response()
         response.response_type = intent.IntentResponseType.QUERY_ANSWER
-        response.async_set_states(matched_states=[climate_state])
+        response.async_set_states(matched_states=match_result.states)
         return response
