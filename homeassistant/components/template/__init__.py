46e4aa57cb3cc99aed62178ff448748bbb56677a
dougiteixeira <31328123+dougiteixeira@users.noreply.github.com>
2024-06-11 21:11:53 +0000

    Revert make sure we update the links between the devices and config entries when the changes source device

--- homeassistant/components/template/__init__.py
+++ homeassistant/components/template/__init__.py
@@ -3,11 +3,12 @@
 from __future__ import annotations
 
 import asyncio
+import dataclasses
 import logging
 
 from homeassistant import config as conf_util
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_UNIQUE_ID, SERVICE_RELOAD
+from homeassistant.const import CONF_DEVICE_ID, CONF_UNIQUE_ID, SERVICE_RELOAD
 from homeassistant.core import Event, HomeAssistant, ServiceCall
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr, discovery
@@ -20,6 +21,16 @@ from .const import CONF_TRIGGER, DOMAIN, PLATFORMS
 from .coordinator import TriggerUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class TemplateData:
+    """Runtime configuration data."""
+
+    device: str | None = None
+
+
+TemplateConfigEntry = ConfigEntry[TemplateData]
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
@@ -57,6 +68,11 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up a config entry."""
+
+    entry.runtime_data = TemplateData(
+        device=entry.options.get(CONF_DEVICE_ID, None),
+    )
+
     await hass.config_entries.async_forward_entry_setups(
         entry, (entry.options["template_type"],)
     )
@@ -67,17 +83,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def config_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Update listener, called when the config entry options are changed."""
 
-    # Remove device link for entry, the source device may have changed.
-    # The link will be recreated after load.
-    device_registry = dr.async_get(hass)
-    devices = device_registry.devices.get_devices_for_config_entry_id(entry.entry_id)
-
-    for device in devices:
-        device_registry.async_update_device(
-            device.id, remove_config_entry_id=entry.entry_id
-        )
+    # Device id before reload
+    old_device = entry.runtime_data.device
 
     await hass.config_entries.async_reload(entry.entry_id)
+
+    # If the old device is different from the current device it is removed from the config entry
+    if old_device != entry.options.get(CONF_DEVICE_ID, None) and old_device is not None:
+        device_registry = dr.async_get(hass)
+        device_registry.async_update_device(
+            old_device, remove_config_entry_id=entry.entry_id
+        )
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
