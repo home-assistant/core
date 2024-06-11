f5c396616e14542e7edf08774e3eeca5180fbcd3
Sebastian Schneider <sese.tailor@gmx.net>
2024-06-11 09:53:33 +0200

    Update switch.py

--- homeassistant/components/tuya/switch.py
+++ homeassistant/components/tuya/switch.py
@@ -524,11 +524,6 @@ SWITCHES: dict[str, tuple[SwitchEntityDescription, ...]] = {
     # https://developer.tuya.com/en/docs/iot/f?id=K9gf45ld5l0t9
     "wk": (
         SwitchEntityDescription(
-            key=DPCode.SWITCH,
-            translation_key="on_off",
-            entity_category=EntityCategory.CONFIG,
-        ),
-        SwitchEntityDescription(
             key=DPCode.CHILD_LOCK,
             translation_key="child_lock",
             entity_category=EntityCategory.CONFIG,
