5f5b30a7454b516dfddb7ba92eceba34f0dcd6d2
epenet <6771947+epenet@users.noreply.github.com>
2024-06-12 13:30:05 +0000

    Ignore existing fixme pylint warnings in tests

--- tests/components/zha/test_climate.py
+++ tests/components/zha/test_climate.py
@@ -1458,7 +1458,8 @@ async def test_set_moes_operation_mode(
     [
         (0, PRESET_AWAY),
         (1, PRESET_SCHEDULE),
-        # (2, PRESET_NONE),  # TODO: why does this not work?
+        # pylint: disable-next=fixme
+        (2, PRESET_NONE),  # TODO: why does this not work?
         (4, PRESET_ECO),
         (5, PRESET_BOOST),
         (7, PRESET_TEMP_MANUAL),
