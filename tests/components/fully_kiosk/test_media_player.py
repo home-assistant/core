c20de61ac59738397f869d180eb6d010d4a49d12
tronikos <tronikos@users.noreply.github.com>
2024-06-13 00:47:47 +0000

    improve test coverage

--- tests/components/fully_kiosk/test_media_player.py
+++ tests/components/fully_kiosk/test_media_player.py
@@ -2,11 +2,14 @@
 
 from unittest.mock import MagicMock, Mock, patch
 
+import pytest
+
 from homeassistant.components import media_player
 from homeassistant.components.fully_kiosk.const import DOMAIN, MEDIA_SUPPORT_FULLYKIOSK
 from homeassistant.components.media_source import DOMAIN as MS_DOMAIN
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 from homeassistant.setup import async_setup_component
 
@@ -84,12 +87,33 @@ async def test_media_player(
     )
     assert len(mock_fully_kiosk.setAudioVolume.mock_calls) == 1
 
+    assert entry.device_id
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.configuration_url == "http://192.168.1.234:2323"
+    assert device_entry.entry_type is None
+    assert device_entry.hw_version is None
+    assert device_entry.identifiers == {(DOMAIN, "abcdef-123456")}
+    assert device_entry.manufacturer == "amzn"
+    assert device_entry.model == "KFDOWI"
+    assert device_entry.name == "Amazon Fire"
+    assert device_entry.sw_version == "1.42.5"
+
+
+@pytest.mark.parametrize("media_content_type", ["video", "video/mp4"])
+async def test_media_player_video(
+    hass: HomeAssistant,
+    mock_fully_kiosk: MagicMock,
+    init_integration: MockConfigEntry,
+    media_content_type: str,
+) -> None:
+    """Test Fully Kiosk media player for videos."""
     await hass.services.async_call(
         media_player.DOMAIN,
         "play_media",
         {
             ATTR_ENTITY_ID: "media_player.amazon_fire",
-            "media_content_type": "video",
+            "media_content_type": media_content_type,
             "media_content_id": "test.mp4",
         },
         blocking=True,
@@ -109,17 +133,25 @@ async def test_media_player(
     )
     mock_fully_kiosk.sendCommand.assert_called_with("stopVideo")
 
-    assert entry.device_id
-    device_entry = device_registry.async_get(entry.device_id)
-    assert device_entry
-    assert device_entry.configuration_url == "http://192.168.1.234:2323"
-    assert device_entry.entry_type is None
-    assert device_entry.hw_version is None
-    assert device_entry.identifiers == {(DOMAIN, "abcdef-123456")}
-    assert device_entry.manufacturer == "amzn"
-    assert device_entry.model == "KFDOWI"
-    assert device_entry.name == "Amazon Fire"
-    assert device_entry.sw_version == "1.42.5"
+
+async def test_media_player_unsupported(
+    hass: HomeAssistant,
+    mock_fully_kiosk: MagicMock,
+    init_integration: MockConfigEntry,
+) -> None:
+    """Test Fully Kiosk media player for unsupported media."""
+    with pytest.raises(HomeAssistantError) as error:
+        await hass.services.async_call(
+            media_player.DOMAIN,
+            "play_media",
+            {
+                ATTR_ENTITY_ID: "media_player.amazon_fire",
+                "media_content_type": "playlist",
+                "media_content_id": "test.m4u",
+            },
+            blocking=True,
+        )
+    assert error.value.args[0] == "Unsupported media type playlist"
 
 
 async def test_browse_media(
