4d5bc38c6111db2c09716a67e081650b124eba63
Rami Mosleh <engrbm87@gmail.com>
2024-06-13 11:54:05 +0000

    update typing

--- homeassistant/components/glances/__init__.py
+++ homeassistant/components/glances/__init__.py
@@ -65,7 +65,7 @@ async def async_setup_entry(
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: GlancesConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
