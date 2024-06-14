a8c0913edaa861f3c21b335005fdd96107ddd096
Brett Adams <Bre77@users.noreply.github.com>
2024-06-15 06:58:41 +1000

    Update homeassistant/components/teslemetry/__init__.py
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- homeassistant/components/teslemetry/__init__.py
+++ homeassistant/components/teslemetry/__init__.py
@@ -47,7 +47,7 @@ PLATFORMS: Final = [
 
 type TeslemetryConfigEntry = ConfigEntry[TeslemetryData]
 
-CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
