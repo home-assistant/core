115b2da2d67e6d09d008d611d61f88bec4b60ae5
Joostlek <joostlek@outlook.com>
2024-06-14 14:46:55 +0200

    Add Mealie integration

--- homeassistant/components/mealie/config_flow.py
+++ homeassistant/components/mealie/config_flow.py
@@ -28,12 +28,13 @@ class MealieConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle a flow initialized by the user."""
         errors: dict[str, str] = {}
         if user_input:
+            self._async_abort_entries_match({CONF_HOST: user_input[CONF_HOST]})
             session = async_get_clientsession(self.hass)
             client = MealieClient(
                 user_input[CONF_HOST], token=user_input[CONF_API_TOKEN], session=session
             )
             try:
-                await client.get_recipes()
+                await client.get_mealplan_today()
             except MealieConnectionError:
                 errors["base"] = "cannot_connect"
             except MealieAuthenticationError:
