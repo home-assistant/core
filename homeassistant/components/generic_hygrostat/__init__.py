639f50174ac1645dcaa50d1667b3d9c3b91fed94
Joakim Plate <elupus@ecce.se>
2024-06-15 10:28:17 +0200

    Handle on load

--- homeassistant/components/generic_hygrostat/__init__.py
+++ homeassistant/components/generic_hygrostat/__init__.py
@@ -10,6 +10,7 @@ from homeassistant.helpers import (
     config_validation as cv,
     device_registry as dr,
     discovery,
+    entity_registry as er,
 )
 from homeassistant.helpers.typing import ConfigType
 
@@ -82,6 +83,9 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up from a config entry."""
+    await async_remove_stale_device_links(
+        hass, entry.entry_id, entry.options[CONF_HUMIDIFIER]
+    )
     await hass.config_entries.async_forward_entry_setups(entry, (Platform.HUMIDIFIER,))
     entry.async_on_unload(entry.add_update_listener(config_entry_update_listener))
     return True
@@ -89,15 +93,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def config_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Update listener, called when the config entry options are changed."""
-    # Remove device link for entry, the source device may have changed.
-    # The link will be recreated after load.
-    device_registry = dr.async_get(hass)
-    devices = device_registry.devices.get_devices_for_config_entry_id(entry.entry_id)
-
-    for device in devices:
-        device_registry.async_update_device(
-            device.id, remove_config_entry_id=entry.entry_id
-        )
     await hass.config_entries.async_reload(entry.entry_id)
 
 
@@ -106,3 +101,24 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return await hass.config_entries.async_unload_platforms(
         entry, (Platform.HUMIDIFIER,)
     )
+
+
+async def async_remove_stale_device_links(
+    hass: HomeAssistant, entry_id: str, entity_id: str
+) -> None:
+    """Remove device link for entry, the source device may have changed."""
+
+    device_registry = dr.async_get(hass)
+    entity_registry = er.async_get(hass)
+
+    # Resolve source entity device
+    device_id = None
+    if entity := entity_registry.async_get(entity_id):
+        device_id = entity.device_id
+
+    # Removes all devices from the config entry that are not the same as the current device
+    devices = device_registry.devices.get_devices_for_config_entry_id(entry_id)
+    for device in devices:
+        if device.id == device_id:
+            continue
+        device_registry.async_update_device(device.id, remove_config_entry_id=entry_id)
--- tests/components/generic_hygrostat/test_init.py
+++ tests/components/generic_hygrostat/test_init.py
@@ -7,35 +7,43 @@ from homeassistant.helpers import device_registry as dr, entity_registry as er
 from tests.common import MockConfigEntry
 
 
-async def test_entry_changed(hass: HomeAssistant) -> None:
-    """Test reconfiguring."""
-
+def _create_mock_entity(
+    hass: HomeAssistant, domain: str, name: str
+) -> er.RegistryEntry:
     device_registry = dr.async_get(hass)
     entity_registry = er.async_get(hass)
 
-    def _create_mock_entity(domain: str, name: str) -> er.RegistryEntry:
-        config_entry = MockConfigEntry(
-            data={},
-            domain="test",
-            title=f"{name}",
-        )
-        config_entry.add_to_hass(hass)
-        device_entry = device_registry.async_get_or_create(
-            identifiers={("test", name)}, config_entry_id=config_entry.entry_id
-        )
-        return entity_registry.async_get_or_create(
-            domain, "test", name, suggested_object_id=name, device_id=device_entry.id
-        )
+    config_entry = MockConfigEntry(
+        data={},
+        domain="test",
+        title=f"{name}",
+    )
+    config_entry.add_to_hass(hass)
+    device_entry = device_registry.async_get_or_create(
+        identifiers={("test", name)}, config_entry_id=config_entry.entry_id
+    )
+    return entity_registry.async_get_or_create(
+        domain, "test", name, suggested_object_id=name, device_id=device_entry.id
+    )
 
-    def _get_device_config_entries(entry: er.RegistryEntry) -> set[str]:
-        assert entry.device_id
-        device = device_registry.async_get(entry.device_id)
-        assert device
-        return device.config_entries
+
+def _get_device_config_entries(
+    hass: HomeAssistant, entry: er.RegistryEntry
+) -> set[str]:
+    device_registry = dr.async_get(hass)
+
+    assert entry.device_id
+    device = device_registry.async_get(entry.device_id)
+    assert device
+    return device.config_entries
+
+
+async def test_entry_changed(hass: HomeAssistant) -> None:
+    """Test reconfiguring."""
 
     # Set up entities, with backing devices and config entries
-    initial_entry = _create_mock_entity("switch", "initial")
-    changed_entry = _create_mock_entity("switch", "changed")
+    initial_entry = _create_mock_entity(hass, "switch", "initial")
+    changed_entry = _create_mock_entity(hass, "switch", "changed")
 
     # Setup the config entry
     config_entry = MockConfigEntry(
@@ -55,14 +63,23 @@ async def test_entry_changed(hass: HomeAssistant) -> None:
     assert await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
 
-    assert config_entry.entry_id in _get_device_config_entries(initial_entry)
-    assert config_entry.entry_id not in _get_device_config_entries(changed_entry)
+    assert config_entry.entry_id in _get_device_config_entries(hass, initial_entry)
+    assert config_entry.entry_id not in _get_device_config_entries(hass, changed_entry)
 
+    # Check that the config entry association is retained if the source is kept
+    hass.config_entries.async_update_entry(
+        config_entry, options={**config_entry.options, "dry_tolerance": "3.0"}
+    )
+    await hass.async_block_till_done()
+
+    assert config_entry.entry_id in _get_device_config_entries(hass, initial_entry)
+    assert config_entry.entry_id not in _get_device_config_entries(hass, changed_entry)
+
+    # Check that the config entry association has updated if the source changes
     hass.config_entries.async_update_entry(
         config_entry, options={**config_entry.options, "humidifier": "switch.changed"}
     )
     await hass.async_block_till_done()
 
-    # Check that the config entry association has updated
-    assert config_entry.entry_id not in _get_device_config_entries(initial_entry)
-    assert config_entry.entry_id in _get_device_config_entries(changed_entry)
+    assert config_entry.entry_id not in _get_device_config_entries(hass, initial_entry)
+    assert config_entry.entry_id in _get_device_config_entries(hass, changed_entry)
