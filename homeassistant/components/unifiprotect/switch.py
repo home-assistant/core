6e10577418f222524a977b42c9c6f91463227052
J. Nick Koston <nick@koston.org>
2024-06-15 18:00:08 -0500

    remove dead code

--- homeassistant/components/unifiprotect/switch.py
+++ homeassistant/components/unifiprotect/switch.py
@@ -487,7 +487,6 @@ class ProtectSwitch(ProtectDeviceEntity, SwitchEntity):
         """Initialize an UniFi Protect Switch."""
         super().__init__(data, device, description)
         self._attr_name = f"{self.device.display_name} {self.entity_description.name}"
-        self._switch_type = self.entity_description.key
 
     def _async_update_device_from_protect(self, device: ProtectModelWithId) -> None:
         super()._async_update_device_from_protect(device)
