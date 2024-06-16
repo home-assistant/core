aff725c6e822e85a07c12c136022d4694581253e
dougiteixeira <douglas.teixeira2@gmail.com>
2024-06-16 21:44:42 +0000

    Add test_entity_id_to_device_id

--- tests/helpers/test_device_utils.py
+++ tests/helpers/test_device_utils.py
@@ -10,6 +10,42 @@ from homeassistant.helpers import (
 from tests.common import MockConfigEntry
 
 
+async def test_entity_id_to_device_id(
+    hass: HomeAssistant,
+    device_registry: dr.DeviceRegistry,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test cleanup works."""
+    config_entry = MockConfigEntry(domain="hue")
+    config_entry.add_to_hass(hass)
+
+    device = device_registry.async_get_or_create(
+        identifiers={("test", "current_device")},
+        connections={("mac", "30:31:32:33:34:00")},
+        config_entry_id=config_entry.entry_id,
+    )
+    assert device is not None
+
+    # Source entity registry
+    entity = entity_registry.async_get_or_create(
+        "sensor",
+        "test",
+        "source",
+        config_entry=config_entry,
+        device_id=device.id,
+    )
+    await hass.async_block_till_done()
+    assert entity_registry.async_get("sensor.test_source") is not None
+
+    # Manual cleanup should unlink stales devices from the config entry
+    device_id = du.async_entity_id_to_device_id(
+        hass,
+        entity_id=entity.entity_id,
+    )
+
+    assert device_id == device.id
+
+
 async def test_device_info_to_link(
     hass: HomeAssistant,
     device_registry: dr.DeviceRegistry,
