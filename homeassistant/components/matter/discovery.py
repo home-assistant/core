e6023c6dc686e1e2dff3a69a33af391902fe85f3
jvmahon <jvm33@columbia.edu>
2024-06-16 07:26:14 -0400

    Update discovery.py

--- homeassistant/components/matter/discovery.py
+++ homeassistant/components/matter/discovery.py
@@ -22,7 +22,6 @@ from .number import DISCOVERY_SCHEMAS as NUMBER_SCHEMAS
 from .sensor import DISCOVERY_SCHEMAS as SENSOR_SCHEMAS
 from .switch import DISCOVERY_SCHEMAS as SWITCH_SCHEMAS
 from .select import DISCOVERY_SCHEMAS as SELECT_SCHEMAS
-from .number import DISCOVERY_SCHEMAS as NUMBER_SCHEMAS
 
 DISCOVERY_SCHEMAS: dict[Platform, list[MatterDiscoverySchema]] = {
     Platform.BINARY_SENSOR: BINARY_SENSOR_SCHEMAS,
@@ -36,7 +35,6 @@ DISCOVERY_SCHEMAS: dict[Platform, list[MatterDiscoverySchema]] = {
     Platform.SENSOR: SENSOR_SCHEMAS,
     Platform.SWITCH: SWITCH_SCHEMAS,
     Platform.SELECT: SELECT_SCHEMAS,
-    Platform.NUMBER: NUMBER_SCHEMAS,
 }
 SUPPORTED_PLATFORMS = tuple(DISCOVERY_SCHEMAS)
 
