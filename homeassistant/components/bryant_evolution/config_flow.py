cec9ad0674e3f21d82b8f4efa366abcb6f6b482e
danielsmyers <danielsmyers@gmail.com>
2024-06-17 13:37:59 -0700

    Update homeassistant/components/bryant_evolution/config_flow.py
    Co-authored-by: Josef Zweck <24647999+zweckj@users.noreply.github.com>

--- homeassistant/components/bryant_evolution/config_flow.py
+++ homeassistant/components/bryant_evolution/config_flow.py
@@ -43,7 +43,6 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Bryant Evolution."""
 
-    VERSION = 1
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
