2555827030a1bcb97bd378ae8b4a41a9bbae7b3e
dontinelli <73341522+dontinelli@users.noreply.github.com>
2024-06-18 09:06:22 +0200

    Replace Solarlog unmaintained library (#117484)
    Co-authored-by: Robert Resch <robert@resch.dev>

--- tests/components/solarlog/test_config_flow.py
+++ tests/components/solarlog/test_config_flow.py
@@ -1,8 +1,9 @@
 """Test the solarlog config flow."""
 
-from unittest.mock import patch
+from unittest.mock import AsyncMock, patch
 
 import pytest
+from solarlog_cli.solarlog_exceptions import SolarLogConnectionError, SolarLogError
 
 from homeassistant import config_entries
 from homeassistant.components.solarlog import config_flow
@@ -17,7 +18,7 @@ NAME = "Solarlog test 1 2 3"
 HOST = "http://1.1.1.1"
 
 
-async def test_form(hass: HomeAssistant) -> None:
+async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
     """Test we get the form."""
 
     result = await hass.config_entries.flow.async_init(
@@ -29,32 +30,20 @@ async def test_form(hass: HomeAssistant) -> None:
     with (
         patch(
             "homeassistant.components.solarlog.config_flow.SolarLogConfigFlow._test_connection",
-            return_value={"title": "solarlog test 1 2 3"},
-        ),
-        patch(
-            "homeassistant.components.solarlog.async_setup_entry",
             return_value=True,
-        ) as mock_setup_entry,
+        ),
     ):
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"], {"host": HOST, "name": NAME}
+            result["flow_id"],
+            {CONF_HOST: HOST, CONF_NAME: NAME, "extended_data": False},
         )
         await hass.async_block_till_done()
 
     assert result2["type"] is FlowResultType.CREATE_ENTRY
     assert result2["title"] == "solarlog_test_1_2_3"
-    assert result2["data"] == {"host": "http://1.1.1.1"}
+    assert result2["data"][CONF_HOST] == "http://1.1.1.1"
+    assert result2["data"]["extended_data"] is False
     assert len(mock_setup_entry.mock_calls) == 1
-
-
-@pytest.fixture(name="test_connect")
-def mock_controller():
-    """Mock a successful _host_in_configuration_exists."""
-    with patch(
-        "homeassistant.components.solarlog.config_flow.SolarLogConfigFlow._test_connection",
-        return_value=True,
-    ):
-        yield
 
 
 def init_config_flow(hass):
@@ -64,19 +53,75 @@ def init_config_flow(hass):
     return flow
 
 
-async def test_user(hass: HomeAssistant, test_connect) -> None:
+@pytest.mark.usefixtures("test_connect")
+async def test_user(
+    hass: HomeAssistant,
+    mock_solarlog: AsyncMock,
+    mock_setup_entry: AsyncMock,
+) -> None:
     """Test user config."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {}
+
+    # tests with all provided
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], {CONF_HOST: HOST, CONF_NAME: NAME, "extended_data": False}
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == "solarlog_test_1_2_3"
+    assert result["data"][CONF_HOST] == HOST
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+@pytest.mark.parametrize(
+    ("exception", "error"),
+    [
+        (SolarLogConnectionError, {CONF_HOST: "cannot_connect"}),
+        (SolarLogError, {CONF_HOST: "unknown"}),
+    ],
+)
+async def test_form_exceptions(
+    hass: HomeAssistant,
+    exception: Exception,
+    error: dict[str, str],
+    mock_solarlog: AsyncMock,
+) -> None:
+    """Test we can handle Form exceptions."""
     flow = init_config_flow(hass)
 
     result = await flow.async_step_user()
     assert result["type"] is FlowResultType.FORM
     assert result["step_id"] == "user"
 
-    # tets with all provided
-    result = await flow.async_step_user({CONF_NAME: NAME, CONF_HOST: HOST})
+    mock_solarlog.return_value.test_connection.side_effect = exception
+
+    # tests with connection error
+    result = await flow.async_step_user(
+        {CONF_NAME: NAME, CONF_HOST: HOST, "extended_data": False}
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user"
+    assert result["errors"] == error
+
+    mock_solarlog.return_value.test_connection.side_effect = None
+
+    # tests with all provided
+    result = await flow.async_step_user(
+        {CONF_NAME: NAME, CONF_HOST: HOST, "extended_data": False}
+    )
+    await hass.async_block_till_done()
+
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "solarlog_test_1_2_3"
     assert result["data"][CONF_HOST] == HOST
+    assert result["data"]["extended_data"] is False
 
 
 async def test_import(hass: HomeAssistant, test_connect) -> None:
@@ -85,18 +130,24 @@ async def test_import(hass: HomeAssistant, test_connect) -> None:
 
     # import with only host
     result = await flow.async_step_import({CONF_HOST: HOST})
+    await hass.async_block_till_done()
+
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "solarlog"
     assert result["data"][CONF_HOST] == HOST
 
     # import with only name
     result = await flow.async_step_import({CONF_NAME: NAME})
+    await hass.async_block_till_done()
+
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "solarlog_test_1_2_3"
     assert result["data"][CONF_HOST] == DEFAULT_HOST
 
     # import with host and name
     result = await flow.async_step_import({CONF_HOST: HOST, CONF_NAME: NAME})
+    await hass.async_block_till_done()
+
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "solarlog_test_1_2_3"
     assert result["data"][CONF_HOST] == HOST
@@ -111,7 +162,7 @@ async def test_abort_if_already_setup(hass: HomeAssistant, test_connect) -> None
 
     # Should fail, same HOST different NAME (default)
     result = await flow.async_step_import(
-        {CONF_HOST: HOST, CONF_NAME: "solarlog_test_7_8_9"}
+        {CONF_HOST: HOST, CONF_NAME: "solarlog_test_7_8_9", "extended_data": False}
     )
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "already_configured"
@@ -123,7 +174,7 @@ async def test_abort_if_already_setup(hass: HomeAssistant, test_connect) -> None
 
     # SHOULD pass, diff HOST (without http://), different NAME
     result = await flow.async_step_import(
-        {CONF_HOST: "2.2.2.2", CONF_NAME: "solarlog_test_7_8_9"}
+        {CONF_HOST: "2.2.2.2", CONF_NAME: "solarlog_test_7_8_9", "extended_data": False}
     )
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "solarlog_test_7_8_9"
@@ -131,8 +182,10 @@ async def test_abort_if_already_setup(hass: HomeAssistant, test_connect) -> None
 
     # SHOULD pass, diff HOST, same NAME
     result = await flow.async_step_import(
-        {CONF_HOST: "http://2.2.2.2", CONF_NAME: NAME}
+        {CONF_HOST: "http://2.2.2.2", CONF_NAME: NAME, "extended_data": False}
     )
+    await hass.async_block_till_done()
+
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "solarlog_test_1_2_3"
     assert result["data"][CONF_HOST] == "http://2.2.2.2"
