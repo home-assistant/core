b63c7e720c05ddc8c52858e556c49d6529725344
kaareseras <kaare@seras.dk>
2024-06-18 21:57:16 +0000

    needed to only have 'Azure'  for having ADX sdk to be less verbose

--- homeassistant/components/azure_data_explorer/client.py
+++ homeassistant/components/azure_data_explorer/client.py
@@ -73,7 +73,7 @@ class AzureDataExplorerClient:
         self.query_client = KustoClient(kcsb_query)
 
         # Suppress very verbose logging from client
-        logger = logging.getLogger("azure.kusto")
+        logger = logging.getLogger("azure")
         logger.setLevel(logging.WARNING)
 
     def test_connection(self) -> None:
