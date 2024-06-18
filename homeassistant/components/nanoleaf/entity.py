53e0d8e7cb3e1cc7b932a8dd2a51b18e90348c83
Joostlek <joostlek@outlook.com>
2024-06-18 16:17:17 +0200

    Extract coordinator to separate module in Nanoleaf

--- homeassistant/components/nanoleaf/entity.py
+++ homeassistant/components/nanoleaf/entity.py
@@ -1,32 +1,26 @@
 """Base class for Nanoleaf entity."""
 
-from aionanoleaf import Nanoleaf
-
 from homeassistant.helpers.device_registry import DeviceInfo
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-)
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
+from . import NanoleafCoordinator
 from .const import DOMAIN
 
 
-class NanoleafEntity(CoordinatorEntity[DataUpdateCoordinator[None]]):
+class NanoleafEntity(CoordinatorEntity[NanoleafCoordinator]):
     """Representation of a Nanoleaf entity."""
 
     _attr_has_entity_name = True
 
-    def __init__(
-        self, nanoleaf: Nanoleaf, coordinator: DataUpdateCoordinator[None]
-    ) -> None:
+    def __init__(self, coordinator: NanoleafCoordinator) -> None:
         """Initialize a Nanoleaf entity."""
         super().__init__(coordinator)
-        self._nanoleaf = nanoleaf
+        self._nanoleaf = coordinator.nanoleaf
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, nanoleaf.serial_no)},
-            manufacturer=nanoleaf.manufacturer,
-            model=nanoleaf.model,
-            name=nanoleaf.name,
-            sw_version=nanoleaf.firmware_version,
-            configuration_url=f"http://{nanoleaf.host}",
+            identifiers={(DOMAIN, self._nanoleaf.serial_no)},
+            manufacturer=self._nanoleaf.manufacturer,
+            model=self._nanoleaf.model,
+            name=self._nanoleaf.name,
+            sw_version=self._nanoleaf.firmware_version,
+            configuration_url=f"http://{self._nanoleaf.host}",
         )
