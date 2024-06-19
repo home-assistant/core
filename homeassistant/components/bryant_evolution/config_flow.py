9121d99bed73c5a23b54fc797da8597241377edf
Daniel Myers <danielsmyers@gmail.com>
2024-06-19 19:02:29 +0000

    Simplify config flow.

--- homeassistant/components/bryant_evolution/config_flow.py
+++ homeassistant/components/bryant_evolution/config_flow.py
@@ -11,8 +11,6 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.const import CONF_HOST
-from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import HomeAssistantError
 
 from .const import CONF_SYSTEM_ID, CONF_ZONE_ID, DOMAIN
 
@@ -27,17 +25,11 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
-    """Validate the user input allows us to connect.
-
-    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
-    """
-
+async def can_reach_device(host: str) -> bool:
+    """Return whether we can reach the device at the given host."""
     # Verify that we can read S1Z1 to check that the host is valid.
-    client = BryantEvolutionClient(data[CONF_HOST], 1, 1)
-    if await client.read_hvac_mode() is None:
-        raise CannotConnect
-    return {"title": f"System {data[CONF_SYSTEM_ID]} Zone {data[CONF_ZONE_ID]}"}
+    client = BryantEvolutionClient(host, 1, 1)
+    return await client.read_hvac_mode() is not None
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -49,24 +41,12 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         errors: dict[str, str] = {}
         if user_input is not None:
-            try:
-                info = await validate_input(self.hass, user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except Exception:
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-            else:
-                return self.async_create_entry(title=info["title"], data=user_input)
-
+            if await can_reach_device(user_input[CONF_HOST]):
+                return self.async_create_entry(
+                    title=f"System {user_input[CONF_SYSTEM_ID]} Zone {user_input[CONF_ZONE_ID]}",
+                    data=user_input,
+                )
+            errors["base"] = "cannot_connect"
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
-
-
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(HomeAssistantError):
-    """Error to indicate there is invalid auth."""
