245a662d6e963788566ab9032d03f1aa6a33a65b
Glenn Waters <glenn@watrs.ca>
2024-06-20 13:38:38 -0400

    Bump env_canada lib to 0.7.0. Remove precip_yesterday sensor as deprecated by Environment Canada.

--- homeassistant/components/environment_canada/sensor.py
+++ homeassistant/components/environment_canada/sensor.py
@@ -115,14 +115,6 @@ SENSOR_TYPES: tuple[ECSensorEntityDescription, ...] = (
         value_fn=lambda data: data.conditions.get("pop", {}).get("value"),
     ),
     ECSensorEntityDescription(
-        key="precip_yesterday",
-        translation_key="precip_yesterday",
-        device_class=SensorDeviceClass.PRECIPITATION,
-        native_unit_of_measurement=UnitOfPrecipitationDepth.MILLIMETERS,
-        state_class=SensorStateClass.MEASUREMENT,
-        value_fn=lambda data: data.conditions.get("precip_yesterday", {}).get("value"),
-    ),
-    ECSensorEntityDescription(
         key="pressure",
         translation_key="pressure",
         device_class=SensorDeviceClass.PRESSURE,
