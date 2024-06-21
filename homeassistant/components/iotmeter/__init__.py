101a412b94c88cf8a4ca5a3ec01074ff7bb5161d
Lipic <lipapave@seznam.cz>
2024-06-21 13:50:31 +0200

    bugfixes

--- homeassistant/components/iotmeter/__init__.py
+++ homeassistant/components/iotmeter/__init__.py
@@ -1,23 +1,15 @@
 import logging
 from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.config_entries import ConfigEntry
-import voluptuous as vol
-from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN
 from .coordinator import IotMeterDataUpdateCoordinator
+from homeassistant.const import Platform
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.setLevel(logging.DEBUG)
 
-'''SERVICE_SET_CHARGING_CURRENT = 'set_charging_current'
-ATTR_CURRENT = 'current'
-
-SET_CHARGING_CURRENT_SCHEMA = vol.Schema({
-    vol.Required('entity_id'): cv.entity_id,
-    vol.Required(ATTR_CURRENT): vol.Coerce(int),
-})
-'''
+PLATFORMS = [Platform.NUMBER, Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -40,27 +32,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         }
 
         _LOGGER.debug("Forwarding entry setup for sensor platform.")
-        await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "number"])
+        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
         entry.async_on_unload(entry.add_update_listener(update_listener))
 
-        # Register the service
-        '''
-        async def async_set_charging_current(service_call: ServiceCall):
-            entity_id = service_call.data['entity_id']
-            current = service_call.data[ATTR_CURRENT]
-            entity = hass.states.get(entity_id)
-            _LOGGER.debug(f"Service called for entity {entity_id} with current {current}.")
-            if entity and hasattr(entity, 'async_set_native_value'):
-                await entity.async_set_native_value(current)
-                _LOGGER.debug(f"Set charging current to {current}")
-            else:
-                _LOGGER.error(f"Entity {entity_id} does not have 'async_set_native_value' method")
-
-        hass.services.async_register(
-            DOMAIN, SERVICE_SET_CHARGING_CURRENT, async_set_charging_current, schema=SET_CHARGING_CURRENT_SCHEMA
-        )
-        _LOGGER.debug("Service registered successfully.")
-        '''
         return True
     except Exception as e:
         _LOGGER.error(f"Error setting up IoTMeter integration: {e}")
