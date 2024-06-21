b8e55a193934c7e3af453d7aaa5fe4e9d5418b8b
Lipic <lipapave@seznam.cz>
2024-06-21 17:28:44 +0200

    bugfixes

--- homeassistant/components/iotmeter/config_flow.py
+++ homeassistant/components/iotmeter/config_flow.py
@@ -1,5 +1,9 @@
+"""Config flow for IoTMeter integration."""
+
 import voluptuous as vol
+
 from homeassistant import config_entries
+
 from .const import DOMAIN
 
 
@@ -15,26 +19,25 @@ class EVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             if ip_address and port:
                 return self.async_create_entry(title="IoTMeter", data=user_input)
-            else:
-                errors["base"] = "invalid_input"
+            errors["base"] = "invalid_input"
 
-        data_schema = vol.Schema({
-            vol.Required("ip_address"): str,
-            vol.Optional("port", default=8000): int
-        })
+        data_schema = vol.Schema(
+            {vol.Required("ip_address"): str, vol.Optional("port", default=8000): int}
+        )
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=data_schema,
-            errors=errors
+            step_id="user", data_schema=data_schema, errors=errors
         )
 
     async def async_step_reconfigure(self, user_input=None):
+        """Handle the reconfiguration step."""
         errors = {}
         # Get the current configuration from config_entry
-        config_entry = self.hass.config_entries.async_get_entry(self.context['entry_id'])
-        current_ip = config_entry.data.get("ip_address", config_entry.data.get("ip_address", ""))
-        current_port = config_entry.data.get("port", config_entry.data.get("port", 8000))
+        config_entry = self.hass.config_entries.async_get_entry(
+            self.context["entry_id"]
+        )
+        current_ip = config_entry.data.get("ip_address", "")
+        current_port = config_entry.data.get("port", 8000)
 
         if user_input is not None:
             ip_address = user_input.get("ip_address")
@@ -42,18 +45,19 @@ class EVConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             if ip_address and port:
                 # Update the config_entry with new data
-                self.hass.config_entries.async_update_entry(config_entry, data=user_input)
+                self.hass.config_entries.async_update_entry(
+                    config_entry, data=user_input
+                )
                 return self.async_abort(reason="reconfigured")
-            else:
-                errors["base"] = "invalid_input"
+            errors["base"] = "invalid_input"
 
-        data_schema = vol.Schema({
-            vol.Required("ip_address", default=current_ip): str,
-            vol.Optional("port", default=current_port): int
-        })
+        data_schema = vol.Schema(
+            {
+                vol.Required("ip_address", default=current_ip): str,
+                vol.Optional("port", default=current_port): int,
+            }
+        )
 
         return self.async_show_form(
-            step_id="reconfigure",
-            data_schema=data_schema,
-            errors=errors
+            step_id="reconfigure", data_schema=data_schema, errors=errors
         )
