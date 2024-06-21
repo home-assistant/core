011c5a16c7aac5e8c750db1606060b737a100738
J. Nick Koston <nick@koston.org>
2024-06-21 11:24:36 -0500

    cleanup

--- homeassistant/util/loop.py
+++ homeassistant/util/loop.py
@@ -20,12 +20,6 @@ from homeassistant.loader import async_suggest_report_issue
 
 _LOGGER = logging.getLogger(__name__)
 
-_STABILITY_REPORT_TEXT = (
-    "This is causing stability issues. "
-    "Please create a bug report at "
-    "https://github.com/home-assistant/core/issues?q=is%3Aopen+is%3Aissue"
-)
-
 
 def _get_line_from_cache(filename: str, lineno: int) -> str:
     """Get line from cache or read from file."""
