26c2ec4b1edefdb6e1612b24c701e353a3ba7b64
Matthew FitzGerald-Chamberlain <mattfitzgeraldchamberlain@proton.me>
2024-06-22 18:45:03 -0500

    Code review updates

--- homeassistant/components/aprilaire/select.py
+++ homeassistant/components/aprilaire/select.py
@@ -2,9 +2,13 @@
 
 from __future__ import annotations
 
+from collections.abc import Awaitable, Callable
+from dataclasses import dataclass
+from typing import cast
+
 from pyaprilaire.const import Attribute
 
-from homeassistant.components.select import SelectEntity
+from homeassistant.components.select import SelectEntity, SelectEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -30,170 +34,124 @@ async def async_setup_entry(
 
     assert config_entry.unique_id is not None
 
-    entities: list[SelectEntity] = []
+    descriptions: list[AprilaireSelectDescription] = []
 
     if coordinator.data.get(Attribute.AIR_CLEANING_AVAILABLE) == 1:
-        entities.append(
-            AprilaireAirCleaningEventEntity(coordinator, config_entry.unique_id)
+        descriptions.append(
+            AprilaireSelectDescription(
+                key="air_cleaning_event",
+                translation_key="air_cleaning_event",
+                options_map=AIR_CLEANING_EVENT_MAP,
+                event_value_key=Attribute.AIR_CLEANING_EVENT,
+                mode_value_key=Attribute.AIR_CLEANING_MODE,
+                is_event=True,
+                select_option_fn=coordinator.client.set_air_cleaning,
+            )
         )
 
-        entities.append(
-            AprilaireAirCleaningModeEntity(coordinator, config_entry.unique_id)
+        descriptions.append(
+            AprilaireSelectDescription(
+                key="air_cleaning_mode",
+                translation_key="air_cleaning_mode",
+                options_map=AIR_CLEANING_MODE_MAP,
+                event_value_key=Attribute.AIR_CLEANING_EVENT,
+                mode_value_key=Attribute.AIR_CLEANING_MODE,
+                is_event=False,
+                select_option_fn=coordinator.client.set_air_cleaning,
+            )
         )
 
     if coordinator.data.get(Attribute.VENTILATION_AVAILABLE) == 1:
-        entities.append(
-            AprilaireFreshAirEventEntity(coordinator, config_entry.unique_id)
+        descriptions.append(
+            AprilaireSelectDescription(
+                key="fresh_air_event",
+                translation_key="fresh_air_event",
+                options_map=FRESH_AIR_EVENT_MAP,
+                event_value_key=Attribute.FRESH_AIR_EVENT,
+                mode_value_key=Attribute.FRESH_AIR_MODE,
+                is_event=True,
+                select_option_fn=coordinator.client.set_fresh_air,
+            )
         )
 
-        entities.append(
-            AprilaireFreshAirModeEntity(coordinator, config_entry.unique_id)
+        descriptions.append(
+            AprilaireSelectDescription(
+                key="fresh_air_mode",
+                translation_key="fresh_air_mode",
+                options_map=FRESH_AIR_MODE_MAP,
+                event_value_key=Attribute.FRESH_AIR_EVENT,
+                mode_value_key=Attribute.FRESH_AIR_MODE,
+                is_event=False,
+                select_option_fn=coordinator.client.set_fresh_air,
+            )
         )
+
+    entities = (
+        AprilaireSelectEntity(coordinator, description, config_entry.unique_id)
+        for description in descriptions
+    )
 
     async_add_entities(entities)
 
 
-class AprilaireAirCleaningEventEntity(BaseAprilaireEntity, SelectEntity):
-    """Aprilaire air cleaning event entity."""
+@dataclass(frozen=True, kw_only=True)
+class AprilaireSelectDescription(SelectEntityDescription):
+    """Class describing Aprilaire select entities."""
 
-    _attr_translation_key = "air_cleaning_event"
-
-    def __init__(self, coordinator: AprilaireCoordinator, unique_id: str) -> None:
-        """Initialize an Aprilaire air cleaning event entity."""
-
-        super().__init__(coordinator, unique_id)
-
-        self._attr_options = list(set(AIR_CLEANING_EVENT_MAP.values()))
-
-    @property
-    def current_option(self) -> str:
-        """Get the current air cleaning event."""
-
-        air_cleaning_event = int(
-            self.coordinator.data.get(Attribute.AIR_CLEANING_EVENT, 0)
-        )
-
-        return AIR_CLEANING_EVENT_MAP.get(air_cleaning_event, "off")
-
-    async def async_select_option(self, option: str) -> None:
-        """Set the air cleaning event."""
-
-        current_air_cleaning_mode = self.coordinator.data.get(
-            Attribute.AIR_CLEANING_MODE, 0
-        )
-
-        air_cleaning_event_value = next(
-            key for key, value in AIR_CLEANING_EVENT_MAP.items() if value == option
-        )
-
-        await self.coordinator.client.set_air_cleaning(
-            current_air_cleaning_mode, air_cleaning_event_value
-        )
+    options_map: dict[int, str]
+    event_value_key: str
+    mode_value_key: str
+    is_event: bool
+    select_option_fn: Callable[[int, int], Awaitable]
 
 
-class AprilaireAirCleaningModeEntity(BaseAprilaireEntity, SelectEntity):
-    """Aprilaire air cleaning mode entity."""
+class AprilaireSelectEntity(BaseAprilaireEntity, SelectEntity):
+    """Base sensor entity for Aprilaire."""
 
-    _attr_translation_key = "air_cleaning_mode"
+    entity_description: AprilaireSelectDescription
 
-    def __init__(self, coordinator: AprilaireCoordinator, unique_id: str) -> None:
-        """Initialize an Aprilaire air cleaning mode entity."""
+    def __init__(
+        self,
+        coordinator: AprilaireCoordinator,
+        description: AprilaireSelectDescription,
+        unique_id: str,
+    ) -> None:
+        """Initialize a sensor for an Aprilaire device."""
+
+        self.entity_description = description
+        self.values_map = {v: k for k, v in description.options_map.items()}
 
         super().__init__(coordinator, unique_id)
 
-        self._attr_options = list(set(AIR_CLEANING_MODE_MAP.values()))
-
-    @property
-    def current_option(self) -> str:
-        """Get the current air cleaning mode."""
-
-        air_cleaning_mode = int(
-            self.coordinator.data.get(Attribute.AIR_CLEANING_MODE, 0)
-        )
-
-        return AIR_CLEANING_MODE_MAP.get(air_cleaning_mode, "off")
-
-    async def async_select_option(self, option: str) -> None:
-        """Set the air cleaning mode."""
-
-        current_air_cleaning_event = self.coordinator.data.get(
-            Attribute.AIR_CLEANING_EVENT, 0
-        )
-
-        air_cleaning_mode_value = next(
-            key for key, value in AIR_CLEANING_MODE_MAP.items() if value == option
-        )
-
-        await self.coordinator.client.set_air_cleaning(
-            air_cleaning_mode_value, current_air_cleaning_event
-        )
-
-
-class AprilaireFreshAirEventEntity(BaseAprilaireEntity, SelectEntity):
-    """Aprilaire fresh air event entity."""
-
-    _attr_translation_key = "fresh_air_event"
-
-    def __init__(self, coordinator: AprilaireCoordinator, unique_id: str) -> None:
-        """Initialize an Aprilaire fresh air event entity."""
-
-        super().__init__(coordinator, unique_id)
-
-        self._attr_options = list(set(FRESH_AIR_EVENT_MAP.values()))
-
-    @property
-    def current_option(self) -> str:
-        """Get the current fresh air event."""
-
-        fresh_air_event = int(self.coordinator.data.get(Attribute.FRESH_AIR_EVENT, 0))
-
-        return FRESH_AIR_EVENT_MAP.get(fresh_air_event, "off")
-
-    async def async_select_option(self, option: str) -> None:
-        """Set the fresh air event."""
-
-        current_fresh_air_mode = self.coordinator.data.get(Attribute.FRESH_AIR_MODE, 0)
-
-        fresh_air_event_value = next(
-            key for key, value in FRESH_AIR_EVENT_MAP.items() if value == option
-        )
-
-        await self.coordinator.client.set_fresh_air(
-            current_fresh_air_mode, fresh_air_event_value
-        )
-
-
-class AprilaireFreshAirModeEntity(BaseAprilaireEntity, SelectEntity):
-    """Aprilaire fresh air mode entity."""
-
-    _attr_translation_key = "fresh_air_mode"
-
-    def __init__(self, coordinator: AprilaireCoordinator, unique_id: str) -> None:
-        """Initialize an Aprilaire fresh air mode entity."""
-
-        super().__init__(coordinator, unique_id)
-
-        self._attr_options = list(set(FRESH_AIR_MODE_MAP.values()))
+        self._attr_options = list(set(description.options_map.values()))
 
     @property
     def current_option(self) -> str:
         """Get the current fresh air mode."""
 
-        fresh_air_mode = int(self.coordinator.data.get(Attribute.FRESH_AIR_MODE, 0))
+        if self.entity_description.is_event:
+            value_key = self.entity_description.event_value_key
+        else:
+            value_key = self.entity_description.mode_value_key
 
-        return FRESH_AIR_MODE_MAP.get(fresh_air_mode, "off")
+        current_value = int(self.coordinator.data.get(value_key, 0))
+
+        return self.entity_description.options_map.get(current_value, "off")
 
     async def async_select_option(self, option: str) -> None:
         """Set the fresh air mode."""
 
-        current_fresh_air_event = self.coordinator.data.get(
-            Attribute.AIR_CLEANING_EVENT, 0
-        )
+        if self.entity_description.is_event:
+            event_value = self.values_map[option]
 
-        fresh_air_mode_value = next(
-            key for key, value in FRESH_AIR_MODE_MAP.items() if value == option
-        )
+            mode_value = cast(
+                int, self.coordinator.data.get(self.entity_description.mode_value_key)
+            )
+        else:
+            mode_value = self.values_map[option]
 
-        await self.coordinator.client.set_fresh_air(
-            fresh_air_mode_value, current_fresh_air_event
-        )
+            event_value = cast(
+                int, self.coordinator.data.get(self.entity_description.event_value_key)
+            )
+
+        await self.entity_description.select_option_fn(mode_value, event_value)
