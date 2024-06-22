6b5fa78dda78cad362779f92bb637b998484b580
Teemu Rytilahti <tpr@iki.fi>
2024-06-22 20:36:17 +0200

    Call _async_call_update_attrs from async_added_to_hass

--- homeassistant/components/tplink/entity.py
+++ homeassistant/components/tplink/entity.py
@@ -169,6 +169,11 @@ class CoordinatedTPLinkEntity(CoordinatorEntity[TPLinkDataUpdateCoordinator], AB
         # it needs to implement this.
         raise NotImplementedError
 
+    async def async_added_to_hass(self) -> None:
+        """Handle being added to hass."""
+        self._async_call_update_attrs()
+        return await super().async_added_to_hass()
+
     @abstractmethod
     @callback
     def _async_update_attrs(self) -> None:
@@ -226,7 +231,6 @@ class CoordinatedTPLinkFeatureEntity(CoordinatedTPLinkEntity, ABC):
         """Initialize the entity."""
         self.entity_description = description
         super().__init__(device, coordinator, parent=parent, feature=feature)
-        self._async_call_update_attrs()
 
     def _get_unique_id(self) -> str:
         """Return unique ID for the entity."""
