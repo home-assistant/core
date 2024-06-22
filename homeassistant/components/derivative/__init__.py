c898c35cca3301b754364214a719bbfa6cecdc01
dougiteixeira <31328123+dougiteixeira@users.noreply.github.com>
2024-06-22 12:37:21 +0000

    Use device helpers

--- homeassistant/components/derivative/__init__.py
+++ homeassistant/components/derivative/__init__.py
@@ -5,13 +5,15 @@ from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_SOURCE, Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers.device import (
+    async_remove_stale_devices_links_keep_entity_device,
+)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Derivative from a config entry."""
 
-    await async_remove_stale_device_links(
+    async_remove_stale_devices_links_keep_entity_device(
         hass, entry.entry_id, entry.options[CONF_SOURCE]
     )
 
@@ -28,27 +30,3 @@ async def config_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, (Platform.SENSOR,))
-
-
-async def async_remove_stale_device_links(
-    hass: HomeAssistant, entry_id: str, entity_id: str
-) -> None:
-    """Remove device link for entry, the source device may have changed."""
-
-    device_registry = dr.async_get(hass)
-    entity_registry = er.async_get(hass)
-
-    # Resolve source entity device
-    current_device_id = None
-    if ((source_entity := entity_registry.async_get(entity_id)) is not None) and (
-        source_entity.device_id is not None
-    ):
-        current_device_id = source_entity.device_id
-
-    devices_in_entry = device_registry.devices.get_devices_for_config_entry_id(entry_id)
-
-    # Removes all devices from the config entry that are not the same as the current device
-    for device in devices_in_entry:
-        if device.id == current_device_id:
-            continue
-        device_registry.async_update_device(device.id, remove_config_entry_id=entry_id)
