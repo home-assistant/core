b3af17cc7e9a82ba1a1eb375ee5a29aa2c242818
Alexandre CUER <alexandrep.cuer@gmail.com>
2024-06-22 21:01:01 +0000

    Leave specific sensor consts in the sensor module

--- homeassistant/components/emoncms/sensor.py
+++ homeassistant/components/emoncms/sensor.py
@@ -2,13 +2,19 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
 from datetime import timedelta
 from typing import Any
 
 from pyemoncms import EmoncmsClient
 import voluptuous as vol
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+from homeassistant.components.sensor import (
+    PLATFORM_SCHEMA,
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
 from homeassistant.const import (
     CONF_API_KEY,
     CONF_ID,
@@ -35,7 +41,6 @@ from .const import (
     ATTR_SIZE,
     ATTR_TAG,
     ATTR_USERID,
-    SENSORS,
 )
 from .coordinator import EmoncmsCoordinator
 
@@ -66,6 +71,66 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_UNIT_OF_MEASUREMENT, default=DEFAULT_UNIT): cv.string,
     }
 )
+
+
+@dataclass
+class SensorEmoncmsDescription:
+    """Description of an emoncms sensor."""
+
+    device_class: SensorDeviceClass
+    state_class: SensorStateClass
+
+
+SENSORS: dict[str | None, SensorEmoncmsDescription] = {
+    "kWh": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.ENERGY,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+    ),
+    "Wh": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.ENERGY,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+    ),
+    "W": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.POWER,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    "V": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.VOLTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    "A": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    "VA": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.APPARENT_POWER,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    "°C": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    "°F": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    "K": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    "Hz": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.FREQUENCY,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    "hPa": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.PRESSURE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    "dB": SensorEmoncmsDescription(
+        device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+}
 
 
 async def async_setup_platform(
