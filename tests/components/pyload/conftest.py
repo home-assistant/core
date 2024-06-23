a5efbfdfca1b3a96b285716f85dbc8e345f39bd8
tr4nt0r <manni@zapto.de>
2024-06-23 16:44:27 +0000

    Fix unique_id missing in mocked config entry

--- tests/components/pyload/conftest.py
+++ tests/components/pyload/conftest.py
@@ -106,5 +106,9 @@ def mock_pyloadapi() -> Generator[AsyncMock, None, None]:
 def mock_config_entry() -> MockConfigEntry:
     """Mock pyLoad configuration entry."""
     return MockConfigEntry(
-        domain=DOMAIN, title=DEFAULT_NAME, data=USER_INPUT, entry_id="XXXXXXXXXXXXXX"
+        domain=DOMAIN,
+        title=DEFAULT_NAME,
+        data=USER_INPUT,
+        unique_id="01J1306EB6MBEW6MK6S28W9WER",
+        entry_id="01J1306EB6MBEW6MK6S28W9WER",
     )
