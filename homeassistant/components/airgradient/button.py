b5a2417f3267d54e54a0648034f7236fa68bbd5d
Joostlek <joostlek@outlook.com>
2024-06-24 11:37:42 +0200

    Fix

--- homeassistant/components/airgradient/button.py
+++ homeassistant/components/airgradient/button.py
@@ -71,11 +71,8 @@ async def async_setup_entry(
             and added_entities
         ):
             entity_registry = er.async_get(hass)
-            unique_ids = [
-                f"{coordinator.serial_number}-{entity_description.key}"
-                for entity_description in (CO2_CALIBRATION, LED_BAR_TEST)
-            ]
-            for unique_id in unique_ids:
+            for entity_description in (CO2_CALIBRATION, LED_BAR_TEST):
+                unique_id = f"{coordinator.serial_number}-{entity_description.key}"
                 if entity_id := entity_registry.async_get_entity_id(
                     BUTTON_DOMAIN, DOMAIN, unique_id
                 ):
--- homeassistant/components/airgradient/number.py
+++ homeassistant/components/airgradient/number.py
@@ -88,11 +88,8 @@ async def async_setup_entry(
             and added_entities
         ):
             entity_registry = er.async_get(hass)
-            unique_ids = [
-                f"{coordinator.serial_number}-{entity_description.key}"
-                for entity_description in (DISPLAY_BRIGHTNESS, LED_BAR_BRIGHTNESS)
-            ]
-            for unique_id in unique_ids:
+            for entity_description in (DISPLAY_BRIGHTNESS, LED_BAR_BRIGHTNESS):
+                unique_id = f"{coordinator.serial_number}-{entity_description.key}"
                 if entity_id := entity_registry.async_get_entity_id(
                     NUMBER_DOMAIN, DOMAIN, unique_id
                 ):
