8f72143d0e211b9803dde14c804803dd5476fa60
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2024-06-25 20:19:03 +0000

    fix: remove event data

--- homeassistant/components/madvr/coordinator.py
+++ homeassistant/components/madvr/coordinator.py
@@ -8,8 +8,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from .const import DOMAIN
-
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -35,43 +33,10 @@ class MadVRCoordinator(DataUpdateCoordinator[dict]):
         self.my_api = my_api
         self.mac = mac
         self.name = name
-        self.previous_data: dict = {}
         self.my_api.set_update_callback(self.handle_push_data)
         _LOGGER.debug("MadVRCoordinator initialized")
 
     def handle_push_data(self, data: dict):
         """Handle new data pushed from the API."""
         _LOGGER.debug("Received push data: %s", data)
-        if self.previous_data:
-            # Compare power state
-            if data.get("is_on") != self.previous_data.get("is_on"):
-                event_data = {
-                    "device_id": self.entry_id,
-                    "type": "power_state_changed",
-                    "new_state": data.get("is_on"),
-                    "old_state": self.previous_data.get("is_on"),
-                }
-                self.hass.bus.async_fire(f"{DOMAIN}_event", event_data)
-
-            # Compare HDR flag
-            if data.get("hdr_flag") != self.previous_data.get("hdr_flag"):
-                event_data = {
-                    "device_id": self.entry_id,
-                    "type": "hdr_flag_changed",
-                    "new_state": data.get("hdr_flag"),
-                    "old_state": self.previous_data.get("hdr_flag"),
-                }
-                self.hass.bus.async_fire(f"{DOMAIN}_event", event_data)
-
-            # Compare aspect ratio
-            if data.get("aspect_dec") != self.previous_data.get("aspect_dec"):
-                event_data = {
-                    "device_id": self.entry_id,
-                    "type": "aspect_dec_changed",
-                    "new_state": data.get("aspect_dec"),
-                    "old_state": self.previous_data.get("aspect_dec"),
-                }
-                self.hass.bus.async_fire(f"{DOMAIN}_event", event_data)
-
-        self.previous_data = data
         self.async_set_updated_data(data)
