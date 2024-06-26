768c64bdd30ade9bcf24845f24e86a5525e6f604
G Johansson <goran.johansson@shiftit.se>
2024-06-26 13:40:28 +0000

    Add more tests

--- tests/components/statistics/test_config_flow.py
+++ tests/components/statistics/test_config_flow.py
@@ -15,6 +15,7 @@ from homeassistant.components.statistics.sensor import (
     CONF_STATE_CHARACTERISTIC,
     DEFAULT_NAME,
     STAT_AVERAGE_LINEAR,
+    STAT_COUNT,
 )
 from homeassistant.const import CONF_ENTITY_ID, CONF_NAME
 from homeassistant.core import HomeAssistant
@@ -23,8 +24,8 @@ from homeassistant.data_entry_flow import FlowResultType
 from tests.common import MockConfigEntry
 
 
-async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
-    """Test we get the form."""
+async def test_form_sensor(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
+    """Test we get the form for sensor."""
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -62,6 +63,57 @@ async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
         CONF_NAME: DEFAULT_NAME,
         CONF_ENTITY_ID: "sensor.test_monitored",
         CONF_STATE_CHARACTERISTIC: STAT_AVERAGE_LINEAR,
+        CONF_SAMPLES_MAX_BUFFER_SIZE: 20.0,
+        CONF_MAX_AGE: {"hours": 8, "minutes": 0, "seconds": 0},
+        CONF_KEEP_LAST_SAMPLE: False,
+        CONF_PERCENTILE: 50.0,
+        CONF_PRECISION: 2.0,
+    }
+
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_form_binary_sensor(
+    hass: HomeAssistant, mock_setup_entry: AsyncMock
+) -> None:
+    """Test we get the form for binary sensor."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["step_id"] == "user"
+    assert result["type"] is FlowResultType.FORM
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_NAME: DEFAULT_NAME,
+            CONF_ENTITY_ID: "binary_sensor.test_monitored",
+        },
+    )
+    await hass.async_block_till_done()
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_STATE_CHARACTERISTIC: STAT_COUNT,
+        },
+    )
+    await hass.async_block_till_done()
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_SAMPLES_MAX_BUFFER_SIZE: 20.0,
+            CONF_MAX_AGE: {"hours": 8, "minutes": 0, "seconds": 0},
+        },
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["version"] == 1
+    assert result["options"] == {
+        CONF_NAME: DEFAULT_NAME,
+        CONF_ENTITY_ID: "binary_sensor.test_monitored",
+        CONF_STATE_CHARACTERISTIC: STAT_COUNT,
         CONF_SAMPLES_MAX_BUFFER_SIZE: 20.0,
         CONF_MAX_AGE: {"hours": 8, "minutes": 0, "seconds": 0},
         CONF_KEEP_LAST_SAMPLE: False,
