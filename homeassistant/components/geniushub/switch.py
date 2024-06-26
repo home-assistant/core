3540c76159c3ac7df8bd514ae2f77427efc76452
GeoffAtHome <geoff@soord.org.uk>
2024-06-26 18:53:56 +0100

    Fix Ruff undefined error

--- homeassistant/components/geniushub/switch.py
+++ homeassistant/components/geniushub/switch.py
@@ -12,6 +12,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv, entity_platform
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import VolDictType
 
 from . import ATTR_DURATION, DOMAIN, GeniusZone
 
