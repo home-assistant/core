779f24d28013e347f48e2bdce710055799bd71d3
tr4nt0r <manni@zapto.de>
2024-06-26 11:22:40 +0000

    Convert dataclass to dict in diagnostics

--- homeassistant/components/pyload/diagnostics.py
+++ homeassistant/components/pyload/diagnostics.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from dataclasses import asdict
 from typing import Any
 
 from homeassistant.components.diagnostics import async_redact_data
@@ -22,5 +23,5 @@ async def async_get_config_entry_diagnostics(
 
     return {
         "config_entry_data": async_redact_data(dict(config_entry.data), TO_REDACT),
-        "pyload_data": pyload_data,
+        "pyload_data": asdict(pyload_data),
     }
