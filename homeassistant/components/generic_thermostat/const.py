a2243188e0f9b3fd4242a0f4bcbc9aa34abdc7c5
dougiteixeira <31328123+dougiteixeira@users.noreply.github.com>
2024-06-28 20:35:28 +0000

    Sort CONF_ constants

--- homeassistant/components/generic_thermostat/const.py
+++ homeassistant/components/generic_thermostat/const.py
@@ -5,9 +5,9 @@ from homeassistant.const import Platform
 DOMAIN = "generic_thermostat"
 PLATFORMS = [Platform.CLIMATE]
 
-CONF_HEATER = "heater"
-CONF_SENSOR = "target_sensor"
 CONF_AC_MODE = "ac_mode"
 CONF_COLD_TOLERANCE = "cold_tolerance"
+CONF_HEATER = "heater"
 CONF_HOT_TOLERANCE = "hot_tolerance"
 CONF_MIN_DUR = "min_cycle_duration"
+CONF_SENSOR = "target_sensor"
