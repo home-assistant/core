15fafd1a5587465750b71a07b055fe4096346544
J. Nick Koston <nick@koston.org>
2024-06-29 08:09:52 -0500

    Improve publish cancellation handling in MQTT
    fixes #120805

--- tests/components/mqtt/test_client.py
+++ tests/components/mqtt/test_client.py
@@ -1194,6 +1194,32 @@ async def test_handle_mqtt_on_callback(
         assert "No ACK from MQTT server" not in caplog.text
 
 
+@patch("homeassistant.components.mqtt.client.TIMEOUT_ACK", 0.3)
+async def test_handle_mqtt_on_callback_cancelled(
+    hass: HomeAssistant,
+    caplog: pytest.LogCaptureFixture,
+    setup_with_birth_msg_client_mock: MqttMockPahoClient,
+) -> None:
+    """Test cancel waiting for a publish."""
+    mqtt_client_mock = setup_with_birth_msg_client_mock
+    with patch.object(mqtt_client_mock, "get_mid", return_value=100):
+        # Simulate an ACK for mid == 100, this will call mqtt_mock._async_get_mid_future(mid)
+        mqtt_client_mock.on_publish(mqtt_client_mock, None, 100)
+        await hass.async_block_till_done()
+        # Make sure the ACK has been received
+        await hass.async_block_till_done()
+        # Now call publish without call back, this will call _async_async_wait_for_mid(msg_info.mid)
+        task = hass.async_create_task(
+            mqtt.async_publish(hass, "no_callback/test-topic", "test-payload")
+        )
+        await asyncio.sleep(0)
+        # Cancel the task so the future is cancelled
+        task.cancel()
+        # Since the future was cancelled, we should not see any timeout warning in the log
+        await hass.async_block_till_done()
+        assert "No ACK from MQTT server" not in caplog.text
+
+
 async def test_handle_mqtt_on_callback_after_timeout(
     hass: HomeAssistant,
     caplog: pytest.LogCaptureFixture,
