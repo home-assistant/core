96e439447f2871400294b247bf0e0fcc72b07bcf
Russell VanderMey <russell@vandermey.us>
2024-06-29 20:30:29 +0000

    Initial commit with errors

--- homeassistant/generated/config_flows.py
+++ homeassistant/generated/config_flows.py
@@ -584,6 +584,7 @@ FLOWS = {
         "trafikverket_train",
         "trafikverket_weatherstation",
         "transmission",
+        "triggercmd",
         "tuya",
         "twentemilieu",
         "twilio",
