b7bfe32b2756ba2ca9e5b0071d49960e996d208d
Robert Svensson <Kane610@users.noreply.github.com>
2024-06-30 01:11:12 +0200

    Mock web request

--- tests/components/deconz/conftest.py
+++ tests/components/deconz/conftest.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from collections.abc import Callable
 from types import MappingProxyType
 from typing import Any
 from unittest.mock import patch
@@ -11,11 +12,12 @@ import pytest
 
 from homeassistant.components.deconz.const import DOMAIN as DECONZ_DOMAIN
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_API_KEY, CONF_HOST, CONF_PORT
+from homeassistant.const import CONF_API_KEY, CONF_HOST, CONF_PORT, CONTENT_TYPE_JSON
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
 from tests.components.light.conftest import mock_light_profiles  # noqa: F401
+from tests.test_util.aiohttp import AiohttpClientMocker
 
 # Config entry fixtures
 
@@ -57,6 +59,94 @@ def fixture_config_entry_data() -> MappingProxyType[str, Any]:
 def fixture_config_entry_options() -> MappingProxyType[str, Any]:
     """Define a config entry options fixture."""
     return {}
+
+
+# deCONZ request mocks
+
+
+@pytest.fixture(name="mock_requests")
+def fixture_request(
+    aioclient_mock: AiohttpClientMocker,
+    config_payload: dict[str, Any],
+    group_payload: dict[str, Any],
+    light_payload: dict[str, Any],
+    sensor_payload: dict[str, Any],
+) -> Callable[[str], None]:
+    """Mock default UniFi requests responses."""
+
+    def __mock_requests(host: str = HOST) -> None:
+        url = f"https://{host}:{PORT}/api/{API_KEY}"
+
+        aioclient_mock.get(
+            url,
+            json={
+                "config": config_payload,
+                "groups": group_payload,
+                "lights": light_payload,
+                "sensors": sensor_payload,
+            },
+            headers={"content-type": CONTENT_TYPE_JSON},
+        )
+
+    return __mock_requests
+
+
+# Request payload fixtures
+
+
+@pytest.fixture(name="alarm_system_payload")
+def fixture_alarm_system_data() -> dict[str, Any]:
+    """Alarm system data."""
+    return {}
+
+
+@pytest.fixture(name="config_payload")
+def fixture_config_data() -> dict[str, Any]:
+    """Config data."""
+    return {}
+
+
+@pytest.fixture(name="group_payload")
+def fixture_group_data() -> dict[str, Any]:
+    """Group data."""
+    return {}
+
+
+@pytest.fixture(name="light_payload")
+def fixture_light_data() -> dict[str, Any]:
+    """Light data."""
+    return {}
+
+
+@pytest.fixture(name="sensor_payload")
+def fixture_sensor_data() -> dict[str, Any]:
+    """Sensor data."""
+    return {}
+
+
+@pytest.fixture(name="config_entry_factory")
+async def fixture_config_entry_factory(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    mock_requests: Callable[[str, str], None],
+) -> Callable[[], ConfigEntry]:
+    """Fixture factory that can set up UniFi network integration."""
+
+    async def __mock_setup_config_entry() -> ConfigEntry:
+        mock_requests(config_entry.data[CONF_HOST])
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+        return config_entry
+
+    return __mock_setup_config_entry
+
+
+@pytest.fixture(name="config_entry_setup")
+async def fixture_config_entry_setup(
+    hass: HomeAssistant, config_entry_factory: Callable[[], ConfigEntry]
+) -> ConfigEntry:
+    """Fixture providing a set up instance of deCONZ integration."""
+    return await config_entry_factory()
 
 
 # Websocket fixtures
