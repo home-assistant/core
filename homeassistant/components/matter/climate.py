d49b899557d518bb705ea5c8517fb78f24331707
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-07-01 17:30:23 +0200

    Mark dry/fan-only climate modes as supported for Panasonic room air conditioner (#120939)

--- homeassistant/components/matter/climate.py
+++ homeassistant/components/matter/climate.py
@@ -60,6 +60,7 @@ SUPPORT_DRY_MODE_DEVICES: set[tuple[int, int]] = {
     # In the list below specify tuples of (vendorid, productid) of devices that
     # support dry mode.
     (0x0001, 0x0108),
+    (0x0001, 0x010A),
     (0x1209, 0x8007),
 }
 
@@ -68,6 +69,7 @@ SUPPORT_FAN_MODE_DEVICES: set[tuple[int, int]] = {
     # In the list below specify tuples of (vendorid, productid) of devices that
     # support fan-only mode.
     (0x0001, 0x0108),
+    (0x0001, 0x010A),
     (0x1209, 0x8007),
 }
 
