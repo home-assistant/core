4700c19d5c47789951a1cecf767ca5fec55bdd47
epenet <6771947+epenet@users.noreply.github.com>
2024-07-02 13:16:08 +0000

    Adjust

--- pylint/plugins/hass_imports.py
+++ pylint/plugins/hass_imports.py
@@ -398,12 +398,6 @@ _OBSOLETE_IMPORT: dict[str, list[ObsoleteImportMatch]] = {
             constant=re.compile(r"^save_json|find_paths_unserializable_data$"),
         ),
     ],
-    "typing": [
-        ObsoleteImportMatch(
-            reason="should be imported from collections.abc",
-            constant=re.compile(r"^(Async)?Generator$"),
-        ),
-    ],
     "typing_extensions": [
         ObsoleteImportMatch(
             reason="should be imported from collections.abc",
