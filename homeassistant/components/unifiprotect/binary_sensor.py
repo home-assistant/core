31ee74b2e0460ae5848a051592d408927b8f380f
J. Nick Koston <nick@koston.org>
2024-07-04 04:40:01 -0500

    drop bugfix

--- homeassistant/components/unifiprotect/binary_sensor.py
+++ homeassistant/components/unifiprotect/binary_sensor.py
@@ -674,16 +674,18 @@ class ProtectDiskBinarySensor(ProtectNVREntity, BinarySensorEntity):
     def _async_update_device_from_protect(self, device: ProtectDeviceType) -> None:
         super()._async_update_device_from_protect(device)
         slot = self._disk.slot
-        ustorage = self.device.system_info.ustorage
+        self._attr_available = False
+        available = self.data.last_update_success
+
         # should not be possible since it would require user to
         # _downgrade_ to make ustorage disppear
-        assert ustorage is not None
-        disk = next((d for d in ustorage.disks if d.slot == slot), None)
-        if disk is None:
-            # disk was removed
-            self._attr_available = False
-            return
-        self._disk = disk
+        assert self.device.system_info.ustorage is not None
+        for disk in self.device.system_info.ustorage.disks:
+            if disk.slot == slot:
+                self._disk = disk
+                self._attr_available = available
+                break
+
         self._attr_is_on = not self._disk.is_healthy
 
 
