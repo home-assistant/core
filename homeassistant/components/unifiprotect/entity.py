6da07178ea493dc36da895b7649a8377a77b52a5
J. Nick Koston <nick@koston.org>
2024-07-04 04:11:42 -0500

    fix storage

--- homeassistant/components/unifiprotect/entity.py
+++ homeassistant/components/unifiprotect/entity.py
@@ -204,7 +204,6 @@ class BaseProtectEntity(Entity, ABC):
     def _async_update_device_from_protect(self, device: ProtectDeviceType) -> None:
         """Update Entity object from Protect device."""
         was_available = self._attr_available
-        async_get_ufp_enabled = self._async_get_ufp_enabled
         if last_updated_success := self.data.last_update_success:
             self.device = device
 
@@ -216,6 +215,7 @@ class BaseProtectEntity(Entity, ABC):
             connected = device.state is StateType.CONNECTED or (
                 not device.is_adopted_by_us and device.can_adopt
             )
+            async_get_ufp_enabled = self._async_get_ufp_enabled
             enabled = not async_get_ufp_enabled or async_get_ufp_enabled(device)
 
         available = last_updated_success and connected and enabled
