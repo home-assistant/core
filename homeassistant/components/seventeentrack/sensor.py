5546eef61925de94996c1096883d54d2b4151326
shai.ungar <shai.ungar@riskified.com>
2024-07-06 01:21:27 +0300

    update breaks in ha version

--- homeassistant/components/seventeentrack/sensor.py
+++ homeassistant/components/seventeentrack/sensor.py
@@ -319,7 +319,7 @@ def deprecate_sensor_issue(hass: HomeAssistant, entry_id: str) -> None:
         hass,
         DOMAIN,
         f"deprecate_sensor_{entry_id}",
-        breaks_in_ha_version="2025.1.0",
+        breaks_in_ha_version="2025.2.0",
         issue_domain=DOMAIN,
         is_fixable=True,
         is_persistent=True,
