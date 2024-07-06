a9e0ed75bb4084ce3441553efcb14c49033c54ec
Brett <brett.whynot@gmail.com>
2024-07-06 07:12:17 +0000

    Add switches

--- homeassistant/components/tessie/entity.py
+++ homeassistant/components/tessie/entity.py
@@ -127,7 +127,7 @@ class TessieEnergyEntity(TessieBaseEntity):
         key: str,
     ) -> None:
         """Initialize common aspects of a Tessie energy site entity."""
-
+        self.api = data.api
         self._attr_unique_id = f"{data.id}-{key}"
         self._attr_device_info = data.device
 
