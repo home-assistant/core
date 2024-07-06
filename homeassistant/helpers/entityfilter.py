5e25f0d02a4fc9689287c6ba508aa8da105ca4a2
J. Nick Koston <nick@koston.org>
2024-07-06 09:40:25 -0500

    Add ability to update entity filters

--- homeassistant/helpers/entityfilter.py
+++ homeassistant/helpers/entityfilter.py
@@ -6,6 +6,7 @@ from collections.abc import Callable
 import fnmatch
 from functools import lru_cache
 import re
+from typing import Final
 
 import voluptuous as vol
 
@@ -20,14 +21,25 @@ from homeassistant.core import split_entity_id
 
 from . import config_validation as cv
 
-CONF_INCLUDE_DOMAINS = "include_domains"
-CONF_INCLUDE_ENTITY_GLOBS = "include_entity_globs"
-CONF_INCLUDE_ENTITIES = "include_entities"
-CONF_EXCLUDE_DOMAINS = "exclude_domains"
-CONF_EXCLUDE_ENTITY_GLOBS = "exclude_entity_globs"
-CONF_EXCLUDE_ENTITIES = "exclude_entities"
+CONF_INCLUDE_DOMAINS: Final = "include_domains"
+CONF_INCLUDE_ENTITY_GLOBS: Final = "include_entity_globs"
+CONF_INCLUDE_ENTITIES: Final = "include_entities"
+CONF_EXCLUDE_DOMAINS: Final = "exclude_domains"
+CONF_EXCLUDE_ENTITY_GLOBS: Final = "exclude_entity_globs"
+CONF_EXCLUDE_ENTITIES: Final = "exclude_entities"
 
-CONF_ENTITY_GLOBS = "entity_globs"
+CONF_ENTITY_GLOBS: Final = "entity_globs"
+
+_KEYS = frozenset(
+    [
+        CONF_INCLUDE_DOMAINS,
+        CONF_INCLUDE_ENTITY_GLOBS,
+        CONF_INCLUDE_ENTITIES,
+        CONF_EXCLUDE_DOMAINS,
+        CONF_EXCLUDE_ENTITY_GLOBS,
+        CONF_EXCLUDE_ENTITIES,
+    ]
+)
 
 
 class EntityFilter:
@@ -35,8 +47,25 @@ class EntityFilter:
 
     def __init__(self, config: dict[str, list[str]]) -> None:
         """Init the filter."""
+        self.config = {key: config.get(key, []) for key in _KEYS}
+        self._generate_filter()
+
+    def remove_config(self, config: dict[str, list[str]]) -> None:
+        """Remove from the filter config."""
+        for key, values in config.items():
+            self.config[key] = list(set(self.config[key]) - set(values))
+        self._generate_filter()
+
+    def add_config(self, config: dict[str, list[str]]) -> None:
+        """Add to the filter config."""
+        for key, values in config.items():
+            self.config[key] = list(set(self.config[key] + list(values)))
+        self._generate_filter()
+
+    def _generate_filter(self) -> None:
+        """Generate the filter."""
+        config = self.config
         self.empty_filter: bool = sum(len(val) for val in config.values()) == 0
-        self.config = config
         self._include_e = set(config[CONF_INCLUDE_ENTITIES])
         self._exclude_e = set(config[CONF_EXCLUDE_ENTITIES])
         self._include_d = set(config[CONF_INCLUDE_DOMAINS])
