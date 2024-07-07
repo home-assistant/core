94e25b72e140a9a3669c1333fdcc9c18d59b7b7d
Joostlek <joostlek@outlook.com>
2024-07-07 17:04:52 +0200

    Add mealie version to device info

--- homeassistant/components/mealie/__init__.py
+++ homeassistant/components/mealie/__init__.py
@@ -2,10 +2,17 @@
 
 from __future__ import annotations
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
+from aiomealie import MealieAuthenticationError, MealieClient, MealieConnectionError
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_API_TOKEN, CONF_HOST, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryError, ConfigEntryNotReady
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.device_registry import DeviceEntryType
+
+from .const import DOMAIN
 from .coordinator import MealieCoordinator
 
 PLATFORMS: list[Platform] = [Platform.CALENDAR]
@@ -16,8 +23,28 @@ type MealieConfigEntry = ConfigEntry[MealieCoordinator]
 
 async def async_setup_entry(hass: HomeAssistant, entry: MealieConfigEntry) -> bool:
     """Set up Mealie from a config entry."""
+    client = MealieClient(
+        entry.data[CONF_HOST],
+        token=entry.data[CONF_API_TOKEN],
+        session=async_get_clientsession(hass),
+    )
+    try:
+        about = await client.get_about()
+    except MealieAuthenticationError as error:
+        raise ConfigEntryError("Authentication failed") from error
+    except MealieConnectionError as error:
+        raise ConfigEntryNotReady(error) from error
 
-    coordinator = MealieCoordinator(hass)
+    assert entry.unique_id
+    device_registry = dr.async_get(hass)
+    device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        identifiers={(DOMAIN, entry.unique_id)},
+        entry_type=DeviceEntryType.SERVICE,
+        sw_version=about.version,
+    )
+
+    coordinator = MealieCoordinator(hass, client)
 
     await coordinator.async_config_entry_first_refresh()
 
--- homeassistant/components/mealie/coordinator.py
+++ homeassistant/components/mealie/coordinator.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 from datetime import timedelta
-from typing import TYPE_CHECKING
 
 from aiomealie import (
     MealieAuthenticationError,
@@ -13,17 +12,13 @@ from aiomealie import (
     MealplanEntryType,
 )
 
-from homeassistant.const import CONF_API_TOKEN, CONF_HOST
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryError
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 import homeassistant.util.dt as dt_util
 
 from .const import LOGGER
-
-if TYPE_CHECKING:
-    from . import MealieConfigEntry
 
 WEEK = timedelta(days=7)
 
@@ -31,18 +26,14 @@ WEEK = timedelta(days=7)
 class MealieCoordinator(DataUpdateCoordinator[dict[MealplanEntryType, list[Mealplan]]]):
     """Class to manage fetching Mealie data."""
 
-    config_entry: MealieConfigEntry
+    config_entry: ConfigEntry
 
-    def __init__(self, hass: HomeAssistant) -> None:
+    def __init__(self, hass: HomeAssistant, client: MealieClient) -> None:
         """Initialize coordinator."""
         super().__init__(
             hass, logger=LOGGER, name="Mealie", update_interval=timedelta(hours=1)
         )
-        self.client = MealieClient(
-            self.config_entry.data[CONF_HOST],
-            token=self.config_entry.data[CONF_API_TOKEN],
-            session=async_get_clientsession(hass),
-        )
+        self.client = client
 
     async def _async_update_data(self) -> dict[MealplanEntryType, list[Mealplan]]:
         next_week = dt_util.now() + WEEK
