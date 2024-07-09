0f39c41c717575a50183abdb302120ea58add88a
Klaas Schoute <klaas_schoute@hotmail.com>
2024-07-09 14:03:24 +0200

    Update every 5 minutes

--- homeassistant/components/autarco/const.py
+++ homeassistant/components/autarco/const.py
@@ -8,7 +8,7 @@ from typing import Final
 
 DOMAIN: Final = "autarco"
 LOGGER = logging.getLogger(__package__)
-SCAN_INTERVAL = timedelta(minutes=1)
+SCAN_INTERVAL = timedelta(minutes=5)
 
 CONF_PUBLIC_KEY: Final = "public_key"
 
