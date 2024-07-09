95905ed438a1d1e3d7f5e3f7c16878ae34c7cd43
J. Nick Koston <nick@koston.org>
2024-07-09 14:12:43 -0700

    more cleanups

--- homeassistant/components/emonitor/__init__.py
+++ homeassistant/components/emonitor/__init__.py
@@ -14,8 +14,6 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers import aiohttp_client
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from .const import DOMAIN
-
 type EmonitorConfigEntry = ConfigEntry[DataUpdateCoordinator[EmonitorStatus]]
 
 _LOGGER = logging.getLogger(__name__)
@@ -40,20 +38,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: EmonitorConfigEntry) -> 
     )
 
     await coordinator.async_config_entry_first_refresh()
-
     entry.runtime_data = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: EmonitorConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
 def name_short_mac(short_mac):
