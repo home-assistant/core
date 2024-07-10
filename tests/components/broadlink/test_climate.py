d5a376074adb41f9e42b4956ba09a9a0fb8d98e5
fustom <fustom@gmail.com>
2024-07-10 10:22:09 +0000

    Parametrizing tests

--- tests/components/broadlink/test_climate.py
+++ tests/components/broadlink/test_climate.py
@@ -1,5 +1,7 @@
 """Tests for Broadlink climate."""
 
+import pytest
+
 from homeassistant.components.broadlink.climate import SensorMode
 from homeassistant.components.broadlink.const import DOMAIN
 from homeassistant.components.climate import (
@@ -18,19 +20,83 @@ from homeassistant.helpers.entity_component import async_update_entity
 
 from . import get_device
 
+testdata = [
+    (
+        SensorMode.INNER_SENSOR_CONTROL,
+        1,
+        0,
+        1,
+        22,
+        23,
+        30,
+        HVACMode.HEAT,
+        22,
+        23,
+        HVACAction.HEATING,
+    ),
+    (
+        SensorMode.OUTER_SENSOR_CONTROL,
+        1,
+        1,
+        0,
+        22,
+        23,
+        30,
+        HVACMode.AUTO,
+        30,
+        23,
+        HVACAction.IDLE,
+    ),
+    (
+        SensorMode.INNER_SENSOR_CONTROL,
+        0,
+        0,
+        0,
+        22,
+        23,
+        30,
+        HVACMode.OFF,
+        22,
+        23,
+        HVACAction.OFF,
+    ),
+]
 
-async def test_climate_inner_heat_heating(
+
+@pytest.mark.parametrize(
+    (
+        "sensor",
+        "power",
+        "auto_mode",
+        "active",
+        "room_temp",
+        "thermostat_temp",
+        "external_temp",
+        "expected_state",
+        "expected_current_temperature",
+        "expected_temperature",
+        "expected_hvac_action",
+    ),
+    testdata,
+)
+async def test_climate(
+    sensor,
+    power,
+    auto_mode,
+    active,
+    room_temp,
+    thermostat_temp,
+    external_temp,
+    expected_state,
+    expected_current_temperature,
+    expected_temperature,
+    expected_hvac_action,
     hass: HomeAssistant,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
 ) -> None:
-    """Test Broadlink climate.
+    """Test Broadlink climate."""
 
-    The test initialized with:
-    - SensorMode.INNER_SENSOR_CONTROL
-    - HVACMode.HEAT
-    - HVACAction.HEATING
-    """
     device = get_device("Guest room")
     mock_setup = await device.setup_entry(hass)
 
@@ -44,22 +110,42 @@ async def test_climate_inner_heat_heating(
     climate = climates[0]
 
     mock_setup.api.get_full_status.return_value = {
-        "sensor": SensorMode.INNER_SENSOR_CONTROL.value,
-        "power": 1,
-        "auto_mode": 0,
-        "active": 1,
-        "room_temp": 22,
-        "thermostat_temp": 23,
-        "external_temp": 30,
+        "sensor": sensor.value,
+        "power": power,
+        "auto_mode": auto_mode,
+        "active": active,
+        "room_temp": room_temp,
+        "thermostat_temp": thermostat_temp,
+        "external_temp": external_temp,
     }
 
     await async_update_entity(hass, climate.entity_id)
     assert mock_setup.api.get_full_status.call_count == 2
     state = hass.states.get(climate.entity_id)
-    assert state.state == HVACMode.HEAT
-    assert state.attributes["current_temperature"] == 22
-    assert state.attributes["temperature"] == 23
-    assert state.attributes["hvac_action"] == HVACAction.HEATING
+    assert state.state == expected_state
+    assert state.attributes["current_temperature"] == expected_current_temperature
+    assert state.attributes["temperature"] == expected_temperature
+    assert state.attributes["hvac_action"] == expected_hvac_action
+
+
+async def test_climate_set_temperature_turn_off_turn_on(
+    hass: HomeAssistant,
+    device_registry: dr.DeviceRegistry,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test Broadlink climate."""
+
+    device = get_device("Guest room")
+    mock_setup = await device.setup_entry(hass)
+
+    device_entry = device_registry.async_get_device(
+        identifiers={(DOMAIN, mock_setup.entry.unique_id)}
+    )
+    entries = er.async_entries_for_device(entity_registry, device_entry.id)
+    climates = [entry for entry in entries if entry.domain == Platform.CLIMATE]
+    assert len(climates) == 1
+
+    climate = climates[0]
 
     await hass.services.async_call(
         CLIMATE_DOMAIN,
@@ -71,10 +157,6 @@ async def test_climate_inner_heat_heating(
         blocking=True,
     )
     state = hass.states.get(climate.entity_id)
-    assert state.state == HVACMode.HEAT
-    assert state.attributes["current_temperature"] == 22
-    assert state.attributes["temperature"] == 24
-    assert state.attributes["hvac_action"] == HVACAction.HEATING
 
     await hass.services.async_call(
         CLIMATE_DOMAIN,
@@ -87,9 +169,6 @@ async def test_climate_inner_heat_heating(
     state = hass.states.get(climate.entity_id)
 
     assert state.state == HVACMode.OFF
-    assert state.attributes["current_temperature"] == 22
-    assert state.attributes["temperature"] == 24
-    assert state.attributes["hvac_action"] == HVACAction.HEATING
 
     await hass.services.async_call(
         CLIMATE_DOMAIN,
@@ -102,92 +181,3 @@ async def test_climate_inner_heat_heating(
     state = hass.states.get(climate.entity_id)
 
     assert state.state == HVACMode.HEAT
-    assert state.attributes["current_temperature"] == 22
-    assert state.attributes["temperature"] == 24
-    assert state.attributes["hvac_action"] == HVACAction.HEATING
-
-
-async def test_climate_outer_auto_idle(
-    hass: HomeAssistant,
-    device_registry: dr.DeviceRegistry,
-    entity_registry: er.EntityRegistry,
-) -> None:
-    """Test Broadlink climate.
-
-    The test initialized with:
-    - SensorMode.OUTER_SENSOR_CONTROL
-    - HVACMode.AUTO
-    - HVACAction.IDLE
-    """
-    device = get_device("Guest room")
-    mock_setup = await device.setup_entry(hass)
-
-    device_entry = device_registry.async_get_device(
-        identifiers={(DOMAIN, mock_setup.entry.unique_id)}
-    )
-    entries = er.async_entries_for_device(entity_registry, device_entry.id)
-    climates = [entry for entry in entries if entry.domain == Platform.CLIMATE]
-    assert len(climates) == 1
-
-    climate = climates[0]
-
-    mock_setup.api.get_full_status.return_value = {
-        "sensor": SensorMode.OUTER_SENSOR_CONTROL.value,
-        "power": 1,
-        "auto_mode": 1,
-        "active": 0,
-        "room_temp": 22,
-        "thermostat_temp": 23,
-        "external_temp": 30,
-    }
-
-    await async_update_entity(hass, climate.entity_id)
-    assert mock_setup.api.get_full_status.call_count == 2
-    state = hass.states.get(climate.entity_id)
-    assert state.state == HVACMode.AUTO
-    assert state.attributes["current_temperature"] == 30
-    assert state.attributes["temperature"] == 23
-    assert state.attributes["hvac_action"] == HVACAction.IDLE
-
-
-async def test_climate_inner_off_off(
-    hass: HomeAssistant,
-    device_registry: dr.DeviceRegistry,
-    entity_registry: er.EntityRegistry,
-) -> None:
-    """Test Broadlink climate.
-
-    The test initialized with:
-    - SensorMode.INNER_SENSOR_CONTROL
-    - HVACMode.OFF
-    - HVACAction.OFF
-    """
-    device = get_device("Guest room")
-    mock_setup = await device.setup_entry(hass)
-
-    device_entry = device_registry.async_get_device(
-        identifiers={(DOMAIN, mock_setup.entry.unique_id)}
-    )
-    entries = er.async_entries_for_device(entity_registry, device_entry.id)
-    climates = [entry for entry in entries if entry.domain == Platform.CLIMATE]
-    assert len(climates) == 1
-
-    climate = climates[0]
-
-    mock_setup.api.get_full_status.return_value = {
-        "sensor": SensorMode.INNER_SENSOR_CONTROL.value,
-        "power": 0,
-        "auto_mode": 0,
-        "active": 0,
-        "room_temp": 22,
-        "thermostat_temp": 23,
-        "external_temp": 30,
-    }
-
-    await async_update_entity(hass, climate.entity_id)
-    assert mock_setup.api.get_full_status.call_count == 2
-    state = hass.states.get(climate.entity_id)
-    assert state.state == HVACMode.OFF
-    assert state.attributes["current_temperature"] == 22
-    assert state.attributes["temperature"] == 23
-    assert state.attributes["hvac_action"] == HVACAction.OFF
