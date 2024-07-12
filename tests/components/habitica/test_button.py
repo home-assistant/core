70fc7c84d88d9225d69f8c498d6b46c9c7f4744f
tr4nt0r <manni@zapto.de>
2024-07-12 14:33:32 +0000

    Add tests for button platform

---
+++ tests/components/habitica/test_button.py
@@ -0,0 +1,168 @@
+"""Tests for Habitica button platform."""
+
+from collections.abc import Generator
+from http import HTTPStatus
+import re
+from unittest.mock import patch
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.components.button import DOMAIN as BUTTON_DOMAIN, SERVICE_PRESS
+from homeassistant.components.habitica.const import DEFAULT_URL
+from homeassistant.config_entries import ConfigEntryState
+from homeassistant.const import ATTR_ENTITY_ID, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ServiceValidationError
+from homeassistant.helpers import entity_registry as er
+
+from .conftest import json_data
+
+from tests.common import MockConfigEntry, snapshot_platform
+from tests.test_util.aiohttp import AiohttpClientMocker
+
+
+@pytest.fixture(autouse=True)
+def button_only() -> Generator[None]:
+    """Enable only the button platform."""
+    with patch(
+        "homeassistant.components.habitica.PLATFORMS",
+        [Platform.BUTTON],
+    ):
+        yield
+
+
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_buttons(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+    mock_habitica: AiohttpClientMocker,
+) -> None:
+    """Test button entities."""
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
+
+
+@pytest.mark.parametrize(
+    ("entity_id", "api_url"),
+    [
+        ("button.test_user_allocate_all_stat_points", "user/allocate-now"),
+        ("button.test_user_buy_a_health_potion", "user/buy-health-potion"),
+        ("button.test_user_revive_from_death", "user/revive"),
+        ("button.test_user_start_my_day", "cron"),
+    ],
+)
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_button_press(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_habitica: AiohttpClientMocker,
+    entity_id: str,
+    api_url: str,
+) -> None:
+    """Test button press method."""
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    mock_habitica.post(f"{DEFAULT_URL}/api/v3/{api_url}", json={"data": None})
+
+    await hass.services.async_call(
+        BUTTON_DOMAIN,
+        SERVICE_PRESS,
+        {ATTR_ENTITY_ID: entity_id},
+        blocking=True,
+    )
+    assert await mock_habitica.match_request(
+        "post",
+        f"{DEFAULT_URL}/api/v3/{api_url}",
+    )
+
+
+@pytest.mark.parametrize(
+    ("entity_id", "api_url"),
+    [
+        ("button.test_user_allocate_all_stat_points", "user/allocate-now"),
+        ("button.test_user_buy_a_health_potion", "user/buy-health-potion"),
+        ("button.test_user_revive_from_death", "user/revive"),
+        ("button.test_user_start_my_day", "cron"),
+    ],
+    ids=["allocate-points", "health-potion", "revive", "run-cron"],
+)
+@pytest.mark.parametrize(
+    ("status_code", "msg"),
+    [
+        (HTTPStatus.TOO_MANY_REQUESTS, "Currently rate limited"),
+        (HTTPStatus.BAD_REQUEST, "Unable to connect to Habitica"),
+        (HTTPStatus.UNAUTHORIZED, "Unable to carry out this action"),
+    ],
+)
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_button_press_exceptions(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_habitica: AiohttpClientMocker,
+    entity_id: str,
+    api_url: str,
+    status_code: HTTPStatus,
+    msg: str,
+) -> None:
+    """Test button press exceptions."""
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    mock_habitica.post(
+        f"{DEFAULT_URL}/api/v3/{api_url}",
+        status=status_code,
+        json={"data": None},
+    )
+
+    with pytest.raises(ServiceValidationError, match=msg):
+        await hass.services.async_call(
+            BUTTON_DOMAIN,
+            SERVICE_PRESS,
+            {ATTR_ENTITY_ID: entity_id},
+            blocking=True,
+        )
+    assert await mock_habitica.match_request(
+        "post",
+        f"{DEFAULT_URL}/api/v3/{api_url}",
+    )
+
+
+async def test_button_unavailable(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    aioclient_mock: AiohttpClientMocker,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test buttons are unavailable if conditions are not met."""
+
+    aioclient_mock.get(
+        f"{DEFAULT_URL}/api/v3/user", json=json_data("user_buttons_unavailable")
+    )
+    aioclient_mock.get(re.compile(r".*"), json={"data": []})
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
--- tests/components/habitica/test_switch.py
+++ tests/components/habitica/test_switch.py
@@ -1,14 +1,23 @@
 """Tests for the Habitica switch platform."""
 
 from collections.abc import Generator
+from http import HTTPStatus
 from unittest.mock import patch
 
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
+from homeassistant.components.habitica.const import DEFAULT_URL
+from homeassistant.components.switch import (
+    DOMAIN as SWITCH_DOMAIN,
+    SERVICE_TOGGLE,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+)
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import Platform
+from homeassistant.const import ATTR_ENTITY_ID, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError, ServiceValidationError
 from homeassistant.helpers import entity_registry as er
 
 from tests.common import MockConfigEntry, snapshot_platform
@@ -41,3 +50,87 @@ async def test_switch(
     assert config_entry.state is ConfigEntryState.LOADED
 
     await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
+
+
+@pytest.mark.parametrize(
+    ("service_call"),
+    [
+        SERVICE_TURN_ON,
+        SERVICE_TURN_OFF,
+        SERVICE_TOGGLE,
+    ],
+)
+async def test_turn_on_off_toggle(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    service_call: str,
+    entity_registry: er.EntityRegistry,
+    mock_habitica: AiohttpClientMocker,
+) -> None:
+    """Test switch turn on/off, toggle method."""
+
+    mock_habitica.post(
+        f"{DEFAULT_URL}/api/v3/user/sleep",
+        json={"success": True, "data": False},
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        service_call,
+        {ATTR_ENTITY_ID: "switch.test_user_rest_in_the_inn"},
+        blocking=True,
+    )
+
+    assert await mock_habitica.match_request("post", f"{DEFAULT_URL}/api/v3/user/sleep")
+
+
+@pytest.mark.parametrize(
+    ("service_call"),
+    [
+        SERVICE_TURN_ON,
+        SERVICE_TURN_OFF,
+        SERVICE_TOGGLE,
+    ],
+)
+@pytest.mark.parametrize(
+    ("status_code", "exception"),
+    [
+        (HTTPStatus.TOO_MANY_REQUESTS, ServiceValidationError),
+        (HTTPStatus.BAD_REQUEST, HomeAssistantError),
+    ],
+)
+async def test_turn_on_off_toggle_exceptions(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    service_call: str,
+    mock_habitica: AiohttpClientMocker,
+    status_code: HTTPStatus,
+    exception: Exception,
+) -> None:
+    """Test switch turn on/off, toggle method."""
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    mock_habitica.post(
+        f"{DEFAULT_URL}/api/v3/user/sleep",
+        status=status_code,
+        json={"success": True, "data": False},
+    )
+
+    with pytest.raises(expected_exception=exception):
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            service_call,
+            {ATTR_ENTITY_ID: "switch.test_user_rest_in_the_inn"},
+            blocking=True,
+        )
+    assert await mock_habitica.match_request("post", f"{DEFAULT_URL}/api/v3/user/sleep")
