3ecc0844e42a9c088a0acb1fea085cb1a84f6c5a
Álvaro Fernández Rojas <noltari@gmail.com>
2024-07-13 09:14:43 +0200

    airzone: reload entry on new devices
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- homeassistant/components/airzone/coordinator.py
+++ homeassistant/components/airzone/coordinator.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 import logging
 from typing import Any
 
+from aioairzone.const import AZD_NEW_SYSTEMS, AZD_NEW_ZONES
 from aioairzone.exceptions import AirzoneError
 from aioairzone.localapi import AirzoneLocalApi
 
@@ -34,6 +35,15 @@ class AirzoneUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
             update_interval=SCAN_INTERVAL,
         )
 
+    async def _async_check_new_devices(self, data: dict[str, Any]) -> None:
+        """Reload entry if there are new devices."""
+        if (
+            len(data.get(AZD_NEW_SYSTEMS, [])) > 0
+            or len(data.get(AZD_NEW_ZONES, [])) > 0
+        ):
+            assert self.config_entry
+            self.hass.config_entries.async_schedule_reload(self.config_entry.entry_id)
+
     async def _async_update_data(self) -> dict[str, Any]:
         """Update data via library."""
         async with timeout(AIOAIRZONE_DEVICE_TIMEOUT_SEC):
@@ -41,4 +51,6 @@ class AirzoneUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
                 await self.airzone.update()
             except AirzoneError as error:
                 raise UpdateFailed(error) from error
-            return self.airzone.data()
+            data = self.airzone.data()
+        await self._async_check_new_devices(data)
+        return data
