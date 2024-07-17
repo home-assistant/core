4cc322dec56d1b41cf063721e1fd8dae5f8f37af
Franck Nijhof <git@frenck.dev>
2024-07-17 16:33:03 +0200

    Migrate HomeWizard to config entry runtime data

--- homeassistant/components/homewizard/__init__.py
+++ homeassistant/components/homewizard/__init__.py
@@ -5,12 +5,14 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN, PLATFORMS
-from .coordinator import HWEnergyDeviceUpdateCoordinator as Coordinator
+from .coordinator import HWEnergyDeviceUpdateCoordinator
+
+type HomeWizardConfigEntry = ConfigEntry[HWEnergyDeviceUpdateCoordinator]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Homewizard from a config entry."""
-    coordinator = Coordinator(hass)
+    coordinator = HWEnergyDeviceUpdateCoordinator(hass)
     try:
         await coordinator.async_config_entry_first_refresh()
 
@@ -22,7 +24,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
         raise
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    entry.runtime_data = coordinator
 
     # Abort reauth config flow if active
     for progress_flow in hass.config_entries.flow.async_progress_by_handler(DOMAIN):
@@ -41,6 +43,5 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        coordinator = hass.data[DOMAIN].pop(entry.entry_id)
-        await coordinator.api.close()
+        entry.runtime_data.api.close()
     return unload_ok
