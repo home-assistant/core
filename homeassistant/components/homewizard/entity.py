474e8b7a439e262a2b668f8260ec89159f05fd5c
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2024-07-19 08:22:06 +0200

    Implement model_id in HomeWizard (#122130)
    * Add model_id and use hardcoded model names for HomeWizard
    
    * Update names

--- homeassistant/components/homewizard/entity.py
+++ homeassistant/components/homewizard/entity.py
@@ -2,12 +2,22 @@
 
 from __future__ import annotations
 
-from homeassistant.const import ATTR_CONNECTIONS, ATTR_IDENTIFIERS
+from homeassistant.const import ATTR_CONNECTIONS, ATTR_IDENTIFIERS, ATTR_MODEL
 from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC, DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 from .coordinator import HWEnergyDeviceUpdateCoordinator
+
+TYPE_MODEL_MAP = {
+    "HWE-P1": "Wi-Fi P1 Meter",
+    "HWE-SKT": "Wi-Fi Energy Socket",
+    "HWE-WTR": "Wi-Fi Water Meter",
+    "HWE-KWH1": "Wi-Fi kWh Meter",
+    "HWE-KWH3": "Wi-Fi kWh Meter",
+    "SDM230-wifi": "Wi-Fi kWh Meter",
+    "SDM630-wifi": "Wi-Fi kWh Meter",
+}
 
 
 class HomeWizardEntity(CoordinatorEntity[HWEnergyDeviceUpdateCoordinator]):
@@ -21,8 +31,11 @@ class HomeWizardEntity(CoordinatorEntity[HWEnergyDeviceUpdateCoordinator]):
         self._attr_device_info = DeviceInfo(
             manufacturer="HomeWizard",
             sw_version=coordinator.data.device.firmware_version,
-            model=coordinator.data.device.product_type,
+            model_id=coordinator.data.device.product_type,
         )
+
+        if product_type := coordinator.data.device.product_type:
+            self._attr_device_info[ATTR_MODEL] = TYPE_MODEL_MAP.get(product_type)
 
         if (serial_number := coordinator.data.device.serial) is not None:
             self._attr_device_info[ATTR_CONNECTIONS] = {
