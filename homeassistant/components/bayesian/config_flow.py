f9aca0f6aab18c5ba5737b59badce83aa583d613
HarvsG <11440490+HarvsG@users.noreply.github.com>
2024-07-24 16:45:26 +0000

    Basic config

--- homeassistant/components/bayesian/config_flow.py
+++ homeassistant/components/bayesian/config_flow.py
@@ -1,14 +1,100 @@
 """Config flow for the Bayesian integration."""
 
-from homeassistant import config_entries
+from collections.abc import Mapping
+from typing import Any
 
-from .const import DOMAIN
+import voluptuous as vol
+
+from homeassistant.components.binary_sensor import BinarySensorDeviceClass
+from homeassistant.const import CONF_DEVICE_CLASS, CONF_NAME
+from homeassistant.helpers import selector
+from homeassistant.helpers.schema_config_entry_flow import (
+    SchemaCommonFlowHandler,
+    SchemaConfigFlowHandler,
+    SchemaFlowFormStep,
+)
+
+from .const import (
+    CONF_PRIOR,
+    CONF_PROBABILITY_THRESHOLD,
+    DEFAULT_NAME,
+    DEFAULT_PROBABILITY_THRESHOLD,
+    DOMAIN,
+)
 
 
-class BayesianConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+async def _validate_mode(
+    handler: SchemaCommonFlowHandler, user_input: dict[str, Any]
+) -> dict[str, Any]:
+    """Validate the threshold mode, and set limits to None if not set."""
+    return {**user_input}
+
+
+OPTIONS_SCHEMA = vol.Schema(
+    {
+        vol.Required(
+            CONF_PROBABILITY_THRESHOLD, default=DEFAULT_PROBABILITY_THRESHOLD * 100
+        ): selector.NumberSelector(
+            selector.NumberSelectorConfig(
+                mode=selector.NumberSelectorMode.SLIDER,
+                step=1.0,
+                min=0,
+                max=100,
+                unit_of_measurement="%",
+            ),
+        ),
+        vol.Required(
+            CONF_PRIOR, default=DEFAULT_PROBABILITY_THRESHOLD * 100
+        ): selector.NumberSelector(
+            selector.NumberSelectorConfig(
+                mode=selector.NumberSelectorMode.SLIDER,
+                step=1.0,
+                min=0,
+                max=100,
+                unit_of_measurement="%",
+            ),
+        ),
+        vol.Optional(CONF_DEVICE_CLASS): selector.SelectSelector(
+            selector.SelectSelectorConfig(
+                options=[cls.value for cls in BinarySensorDeviceClass],
+                mode=selector.SelectSelectorMode.DROPDOWN,
+                translation_key="binary_sensor_device_class",
+                sort=True,
+            ),
+        ),
+    }
+)
+CONFIG_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME, default=DEFAULT_NAME): selector.TextSelector(),
+    }
+).extend(OPTIONS_SCHEMA.schema)
+
+CONFIG_FLOW = {
+    "user": SchemaFlowFormStep(
+        CONFIG_SCHEMA, preview="bayesian", validate_user_input=_validate_mode
+    )
+}
+
+OPTIONS_FLOW = {
+    "init": SchemaFlowFormStep(
+        OPTIONS_SCHEMA, preview="bayesian", validate_user_input=_validate_mode
+    )
+}
+
+
+class BayesianConfigFlowHandler(SchemaConfigFlowHandler, domain=DOMAIN):
     """Example config flow."""
 
     # The schema version of the entries that it creates
     # Home Assistant will call your migrate method if the version changes
     VERSION = 1
     MINOR_VERSION = 1
+
+    config_flow = CONFIG_FLOW
+    options_flow = OPTIONS_FLOW
+
+    def async_config_entry_title(self, options: Mapping[str, Any]) -> str:
+        """Return config entry title."""
+        name: str = options[CONF_NAME]
+        return name
