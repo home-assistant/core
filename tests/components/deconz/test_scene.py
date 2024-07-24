5bda07214183d7475741ffe9a4faaf0a00ba6353
Robert Svensson <Kane610@users.noreply.github.com>
2024-07-24 17:32:57 +0200

    Use snapshot in deCONZ scene tests (#122540)

--- tests/components/deconz/test_scene.py
+++ tests/components/deconz/test_scene.py
@@ -2,17 +2,19 @@
 
 from collections.abc import Callable
 from typing import Any
+from unittest.mock import patch
 
 import pytest
+from syrupy import SnapshotAssertion
 
 from homeassistant.components.scene import DOMAIN as SCENE_DOMAIN, SERVICE_TURN_ON
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_ENTITY_ID, STATE_UNAVAILABLE
+from homeassistant.const import ATTR_ENTITY_ID, STATE_UNAVAILABLE, Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import entity_registry as er
 
-from .conftest import WebsocketDataType
+from .conftest import ConfigEntryFactoryType, WebsocketDataType
 
+from tests.common import snapshot_platform
 from tests.test_util.aiohttp import AiohttpClientMocker
 
 TEST_DATA = [
@@ -29,14 +31,7 @@ TEST_DATA = [
             }
         },
         {
-            "entity_count": 2,
-            "device_count": 3,
             "entity_id": "scene.light_group_scene",
-            "unique_id": "01234E56789A/groups/1/scenes/1",
-            "entity_category": None,
-            "attributes": {
-                "friendly_name": "Light group Scene",
-            },
             "request": "/groups/1/scenes/1/recall",
         },
     ),
@@ -46,36 +41,16 @@ TEST_DATA = [
 @pytest.mark.parametrize(("group_payload", "expected"), TEST_DATA)
 async def test_scenes(
     hass: HomeAssistant,
-    device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
-    config_entry_setup: ConfigEntry,
+    config_entry_factory: ConfigEntryFactoryType,
     mock_put_request: Callable[[str, str], AiohttpClientMocker],
     expected: dict[str, Any],
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test successful creation of scene entities."""
-    assert len(hass.states.async_all()) == expected["entity_count"]
-
-    # Verify state data
-
-    scene = hass.states.get(expected["entity_id"])
-    assert scene.attributes == expected["attributes"]
-
-    # Verify entity registry data
-
-    ent_reg_entry = entity_registry.async_get(expected["entity_id"])
-    assert ent_reg_entry.entity_category is expected["entity_category"]
-    assert ent_reg_entry.unique_id == expected["unique_id"]
-
-    # Verify device registry data
-
-    assert (
-        len(
-            dr.async_entries_for_config_entry(
-                device_registry, config_entry_setup.entry_id
-            )
-        )
-        == expected["device_count"]
-    )
+    with patch("homeassistant.components.deconz.PLATFORMS", [Platform.SCENE]):
+        config_entry = await config_entry_factory()
+    await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
 
     # Verify button press
 
@@ -91,12 +66,12 @@ async def test_scenes(
 
     # Unload entry
 
-    await hass.config_entries.async_unload(config_entry_setup.entry_id)
+    await hass.config_entries.async_unload(config_entry.entry_id)
     assert hass.states.get(expected["entity_id"]).state == STATE_UNAVAILABLE
 
     # Remove entry
 
-    await hass.config_entries.async_remove(config_entry_setup.entry_id)
+    await hass.config_entries.async_remove(config_entry.entry_id)
     await hass.async_block_till_done()
     assert len(hass.states.async_all()) == 0
 
