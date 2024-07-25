2ceaeb8cb80cfb006dc294952e1009dfdc88a7b9
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-07-25 14:13:22 +0200

    adjust tests

--- tests/components/matter/test_fan.py
+++ tests/components/matter/test_fan.py
@@ -27,6 +27,14 @@ from .common import (
 )
 
 
+@pytest.fixture(name="fan_node")
+async def simple_fan_fixture(
+    hass: HomeAssistant, matter_client: MagicMock
+) -> MatterNode:
+    """Fixture for a Fan node."""
+    return await setup_integration_with_node_fixture(hass, "fan", matter_client)
+
+
 @pytest.fixture(name="air_purifier")
 async def air_purifier_fixture(
     hass: HomeAssistant, matter_client: MagicMock
@@ -100,6 +108,7 @@ async def test_fan_base(
     assert state.attributes["percentage"] == 0
 
 
+@pytest.mark.parametrize("expected_lingering_tasks", [True])
 async def test_fan_turn_on_with_percentage(
     hass: HomeAssistant,
     matter_client: MagicMock,
@@ -119,43 +128,6 @@ async def test_fan_turn_on_with_percentage(
         attribute_path="1/514/2",
         value=50,
     )
-
-
-@pytest.mark.parametrize("expected_lingering_tasks", [True])
-async def test_fan_turn_on_with_preset_mode(
-    hass: HomeAssistant,
-    matter_client: MagicMock,
-    air_purifier: MatterNode,
-) -> None:
-    """Test turning on the fan with a specific preset mode."""
-    entity_id = "fan.air_purifier_fan"
-    await hass.services.async_call(
-        FAN_DOMAIN,
-        SERVICE_TURN_ON,
-        {ATTR_ENTITY_ID: entity_id, ATTR_PRESET_MODE: "medium"},
-        blocking=True,
-    )
-    assert matter_client.write_attribute.call_count == 1
-    assert matter_client.write_attribute.call_args == call(
-        node_id=air_purifier.node_id,
-        attribute_path="1/514/0",
-        value=2,
-    )
-    # test again with wind feature as preset mode
-    for preset_mode, value in (("natural_wind", 2), ("sleep_wind", 1)):
-        matter_client.write_attribute.reset_mock()
-        await hass.services.async_call(
-            FAN_DOMAIN,
-            SERVICE_TURN_ON,
-            {ATTR_ENTITY_ID: entity_id, ATTR_PRESET_MODE: preset_mode},
-            blocking=True,
-        )
-        assert matter_client.write_attribute.call_count == 1
-        assert matter_client.write_attribute.call_args == call(
-            node_id=air_purifier.node_id,
-            attribute_path="1/514/10",
-            value=value,
-        )
     # test again where preset_mode is omitted in the service call
     # which should select the last active percentage
     matter_client.write_attribute.reset_mock()
@@ -171,9 +143,46 @@ async def test_fan_turn_on_with_preset_mode(
         attribute_path="1/514/2",
         value=255,
     )
+
+
+@pytest.mark.parametrize("expected_lingering_tasks", [True])
+async def test_fan_turn_on_with_preset_mode(
+    hass: HomeAssistant,
+    matter_client: MagicMock,
+    fan_node: MatterNode,
+) -> None:
+    """Test turning on the fan with a specific preset mode."""
+    entity_id = "fan.mocked_fan_switch_fan"
+    await hass.services.async_call(
+        FAN_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: entity_id, ATTR_PRESET_MODE: "medium"},
+        blocking=True,
+    )
+    assert matter_client.write_attribute.call_count == 1
+    assert matter_client.write_attribute.call_args == call(
+        node_id=fan_node.node_id,
+        attribute_path="1/514/0",
+        value=2,
+    )
+    # test again with wind feature as preset mode
+    for preset_mode, value in (("natural_wind", 2), ("sleep_wind", 1)):
+        matter_client.write_attribute.reset_mock()
+        await hass.services.async_call(
+            FAN_DOMAIN,
+            SERVICE_TURN_ON,
+            {ATTR_ENTITY_ID: entity_id, ATTR_PRESET_MODE: preset_mode},
+            blocking=True,
+        )
+        assert matter_client.write_attribute.call_count == 1
+        assert matter_client.write_attribute.call_args == call(
+            node_id=fan_node.node_id,
+            attribute_path="1/514/10",
+            value=value,
+        )
     # test again if wind mode is explicitly turned off when we set a new preset mode
     matter_client.write_attribute.reset_mock()
-    set_node_attribute(air_purifier, 1, 514, 10, 2)
+    set_node_attribute(fan_node, 1, 514, 10, 2)
     await trigger_subscription_callback(hass, matter_client)
     await hass.services.async_call(
         FAN_DOMAIN,
@@ -183,14 +192,32 @@ async def test_fan_turn_on_with_preset_mode(
     )
     assert matter_client.write_attribute.call_count == 2
     assert matter_client.write_attribute.call_args_list[0] == call(
-        node_id=air_purifier.node_id,
+        node_id=fan_node.node_id,
         attribute_path="1/514/10",
         value=0,
     )
     assert matter_client.write_attribute.call_args == call(
-        node_id=air_purifier.node_id,
+        node_id=fan_node.node_id,
         attribute_path="1/514/0",
         value=2,
+    )
+    # test again where preset_mode is omitted in the service call
+    # which should select the last active preset
+    matter_client.write_attribute.reset_mock()
+    set_node_attribute(fan_node, 1, 514, 0, 1)
+    set_node_attribute(fan_node, 1, 514, 10, 0)
+    await trigger_subscription_callback(hass, matter_client)
+    await hass.services.async_call(
+        FAN_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: entity_id},
+        blocking=True,
+    )
+    assert matter_client.write_attribute.call_count == 1
+    assert matter_client.write_attribute.call_args == call(
+        node_id=fan_node.node_id,
+        attribute_path="1/514/0",
+        value=1,
     )
 
 
