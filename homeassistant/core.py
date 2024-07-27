c57cc64474096ba3cc6e275fc78d322819f83c19
J. Nick Koston <nick@koston.org>
2024-07-27 15:15:22 -0500

    empty

--- homeassistant/core.py
+++ homeassistant/core.py
@@ -1575,12 +1575,12 @@ class EventBus:
             )
 
         event: Event[_DataT] | None = None
-        for job, event_filter in self._listeners.get(
-            event_type,
-            EMPTY_LIST
-            if event_type in EVENTS_EXCLUDED_FROM_MATCH_ALL
-            else self._match_all_listeners,
-        ).copy():
+        if not (listeners := self._listeners.get(event_type)):
+            if event_type in EVENTS_EXCLUDED_FROM_MATCH_ALL:
+                return
+            listeners = self._match_all_listeners
+
+        for job, event_filter in listeners.copy():
             if event_filter is not None:
                 try:
                     if event_data is None or not event_filter(event_data):
