22b1593ab65a1157b5998b3e346a06b09ff71697
cayossarian <scrap-bursary.0r@icloud.com>
2024-07-29 15:39:52 -0700

    Reverted ConfigEntry to Base class

--- homeassistant/components/mopeka/__init__.py
+++ homeassistant/components/mopeka/__init__.py
@@ -51,6 +51,6 @@ async def update_listener(hass: HomeAssistant, entry: MopekaConfigEntry) -> None
     await hass.config_entries.async_reload(entry.entry_id)
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: MopekaConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
