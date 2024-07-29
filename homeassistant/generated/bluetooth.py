70df4ca461610afeadbb5a4c9d18cd9a8de5bb2a
Mr. Bubbles <manni@zapto.de>
2024-07-29 11:44:01 +0200

    Integration for IronOS (Pinecil V2) soldering irons (#120802)
    * Add Pinecil integration
    
    * Refactor with new library
    
    * Add tests for config flow, remove unused code
    
    * requested changes
    
    * update requirements
    
    * Move some sensor values to diagnostics, add tests for sensors
    
    * User service uuid in discovery
    
    * fix manufacturer name
    
    * Bump pynecil to version 0.2.0
    
    * Rename integration to IronOS
    
    * Recreate snapshot
    
    * Update strings
    
    * type checking
    
    * Update snapshot
    
    * Add async_setup to coordinator
    
    * Show device id with serial number
    
    * Added missing boost to operation mode states
    
    * remove super call
    
    * Refactor
    
    * tests

--- homeassistant/generated/bluetooth.py
+++ homeassistant/generated/bluetooth.py
@@ -322,6 +322,11 @@ BLUETOOTH: Final[list[dict[str, bool | str | int | list[int]]]] = [
         "local_name": "tps",
     },
     {
+        "connectable": True,
+        "domain": "iron_os",
+        "service_uuid": "9eae1000-9d0d-48c5-aa55-33e27f9bc533",
+    },
+    {
         "connectable": False,
         "domain": "kegtron",
         "manufacturer_id": 65535,
