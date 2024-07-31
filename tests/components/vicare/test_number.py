4d2d6abccbf8efbb8c4882a737018a67e7dff33c
Christopher Fenner <Christopher.Fenner@me.com>
2024-08-01 00:45:15 +0200

    add test case

---
+++ tests/components/vicare/test_number.py
@@ -0,0 +1,34 @@
+"""Test ViCare number entity."""
+
+from unittest.mock import patch
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from . import MODULE, setup_integration
+from .conftest import Fixture, MockPyViCare
+
+from tests.common import MockConfigEntry, snapshot_platform
+
+
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_all_entities(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    mock_config_entry: MockConfigEntry,
+    # mock_vicare_gas_boiler: MagicMock,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test the ViCare number entity."""
+    fixtures: list[Fixture] = [Fixture({"type:boiler"}, "vicare/Vitodens300W.json")]
+    with (
+        patch(f"{MODULE}.vicare_login", return_value=MockPyViCare(fixtures)),
+        patch(f"{MODULE}.PLATFORMS", [Platform.NUMBER]),
+    ):
+        await setup_integration(hass, mock_config_entry)
+
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
