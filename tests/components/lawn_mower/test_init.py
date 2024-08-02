ad7c135e9d68e72fef01a8980fcd09e0cfe24ec5
cociweb <cociweb@users.noreply.github.com>
2024-08-03 00:42:15 +0200

    Add Tests for extended functions of lawn_mower integration

--- tests/components/lawn_mower/test_init.py
+++ tests/components/lawn_mower/test_init.py
@@ -138,6 +138,17 @@ async def test_sync_start_mowing(hass: HomeAssistant) -> None:
     assert lawn_mower.start_mowing.called
 
 
+async def test_sync_fixed_mowing(hass: HomeAssistant) -> None:
+    """Test if async fixed_mowing calls sync fixed_mowing."""
+    lawn_mower = MockLawnMowerEntity()
+    lawn_mower.hass = hass
+
+    lawn_mower.fixed_mowing = MagicMock()
+    await lawn_mower.async_fixed_mowing()
+
+    assert lawn_mower.fixed_mowing.called
+
+
 async def test_sync_dock(hass: HomeAssistant) -> None:
     """Test if async dock calls sync dock."""
     lawn_mower = MockLawnMowerEntity()
@@ -158,6 +169,28 @@ async def test_sync_pause(hass: HomeAssistant) -> None:
     await lawn_mower.async_pause()
 
     assert lawn_mower.pause.called
+
+
+async def test_sync_resume(hass: HomeAssistant) -> None:
+    """Test if async resume calls sync resume."""
+    lawn_mower = MockLawnMowerEntity()
+    lawn_mower.hass = hass
+
+    lawn_mower.resume = MagicMock()
+    await lawn_mower.async_resume()
+
+    assert lawn_mower.resume.called
+
+
+async def test_sync_cancel(hass: HomeAssistant) -> None:
+    """Test if async cancel calls sync cancel."""
+    lawn_mower = MockLawnMowerEntity()
+    lawn_mower.hass = hass
+
+    lawn_mower.cancel = MagicMock()
+    await lawn_mower.async_cancel()
+
+    assert lawn_mower.cancel.called
 
 
 async def test_lawn_mower_default(hass: HomeAssistant) -> None:
