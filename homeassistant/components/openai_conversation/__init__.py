8dca48f92c2ee4f0eda84c1031cce08e3368ab77
Paulus Schoutsen <balloob@gmail.com>
2024-08-02 07:42:45 +0000

    OpenAI reload when options update

--- homeassistant/components/openai_conversation/__init__.py
+++ homeassistant/components/openai_conversation/__init__.py
@@ -101,9 +101,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: OpenAIConfigEntry) -> bo
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
+    entry.async_on_unload(entry.add_update_listener(_async_entry_update_listener))
+
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload OpenAI."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
+async def _async_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
