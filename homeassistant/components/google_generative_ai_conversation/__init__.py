79bca0d6e8f8e5b0e3ddcd339f53f352c6b84ee2
Paulus Schoutsen <balloob@gmail.com>
2024-08-02 07:41:35 +0000

    Better function name

--- homeassistant/components/google_generative_ai_conversation/__init__.py
+++ homeassistant/components/google_generative_ai_conversation/__init__.py
@@ -120,7 +120,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    entry.async_on_unload(entry.add_update_listener(update_listener))
+    entry.async_on_unload(entry.add_update_listener(_async_entry_update_listener))
 
     return True
 
@@ -133,6 +133,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+async def _async_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Handle options update."""
     await hass.config_entries.async_reload(entry.entry_id)
