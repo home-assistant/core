21ccd57309ab3693d6ab45d9435a439ad48f35f0
Guy Lowe <github@gunk.gen.nz>
2024-08-03 17:20:54 +1200

    Add unit test for SNMP switches (integer unknown)

--- tests/components/snmp/test_integer_switch.py
+++ tests/components/snmp/test_integer_switch.py
@@ -57,3 +57,28 @@ async def test_snmp_integer_switch_on(hass: HomeAssistant) -> None:
         await hass.async_block_till_done()
         state = hass.states.get("switch.snmp")
         assert state.state == "on"
+
+
+async def test_snmp_integer_switch_unknown(hass: HomeAssistant) -> None:
+    """Test snmp switch returning int 3 (not a configured payload) for unknown."""
+
+    config = {
+        SWITCH_DOMAIN: {
+            "platform": "snmp",
+            "host": "192.168.1.32",
+            # ippower-mib::ippoweroutlet1.0
+            "baseoid": "1.3.6.1.4.1.38107.1.3.1.0",
+            "payload_on": 1,
+            "payload_off": 0,
+        },
+    }
+
+    mock_data = Integer32(3)
+    with patch(
+        "homeassistant.components.snmp.switch.getCmd",
+        return_value=(None, None, None, [[mock_data]]),
+    ):
+        assert await async_setup_component(hass, SWITCH_DOMAIN, config)
+        await hass.async_block_till_done()
+        state = hass.states.get("switch.snmp")
+        assert state.state == "unknown"
