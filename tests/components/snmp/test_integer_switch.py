550649a0c7315d5d8ae250c1b04c4e13ff48a35c
Guy Lowe <github@gunk.gen.nz>
2024-08-04 09:22:57 +1200

    Use a single configuration for all test_integer_switch tests

--- tests/components/snmp/test_integer_switch.py
+++ tests/components/snmp/test_integer_switch.py
@@ -8,20 +8,20 @@ from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
+config = {
+    SWITCH_DOMAIN: {
+        "platform": "snmp",
+        "host": "192.168.1.32",
+        # ippower-mib::ippoweroutlet1.0
+        "baseoid": "1.3.6.1.4.1.38107.1.3.1.0",
+        "payload_on": 1,
+        "payload_off": 0,
+    },
+}
+
 
 async def test_snmp_integer_switch_off(hass: HomeAssistant) -> None:
     """Test snmp switch returning int 0 for off."""
-
-    config = {
-        SWITCH_DOMAIN: {
-            "platform": "snmp",
-            "host": "192.168.1.32",
-            # ippower-mib::ippoweroutlet1.0
-            "baseoid": "1.3.6.1.4.1.38107.1.3.1.0",
-            "payload_on": 1,
-            "payload_off": 0,
-        },
-    }
 
     mock_data = Integer32(0)
     with patch(
@@ -37,17 +37,6 @@ async def test_snmp_integer_switch_off(hass: HomeAssistant) -> None:
 async def test_snmp_integer_switch_on(hass: HomeAssistant) -> None:
     """Test snmp switch returning int 1 for on."""
 
-    config = {
-        SWITCH_DOMAIN: {
-            "platform": "snmp",
-            "host": "192.168.1.32",
-            # ippower-mib::ippoweroutlet1.0
-            "baseoid": "1.3.6.1.4.1.38107.1.3.1.0",
-            "payload_on": 1,
-            "payload_off": 0,
-        },
-    }
-
     mock_data = Integer32(1)
     with patch(
         "homeassistant.components.snmp.switch.getCmd",
@@ -61,17 +50,6 @@ async def test_snmp_integer_switch_on(hass: HomeAssistant) -> None:
 
 async def test_snmp_integer_switch_unknown(hass: HomeAssistant) -> None:
     """Test snmp switch returning int 3 (not a configured payload) for unknown."""
-
-    config = {
-        SWITCH_DOMAIN: {
-            "platform": "snmp",
-            "host": "192.168.1.32",
-            # ippower-mib::ippoweroutlet1.0
-            "baseoid": "1.3.6.1.4.1.38107.1.3.1.0",
-            "payload_on": 1,
-            "payload_off": 0,
-        },
-    }
 
     mock_data = Integer32(3)
     with patch(
