c93717d93f38ec0608866b71fba41fbf0635fb11
farmio <farmio@alphart.net>
2024-08-04 01:35:01 +0200

    Use KNX UI entity platform controller class
    instead of KNXConfigStore to add and reload UI entities

--- homeassistant/components/knx/storage/config_store.py
+++ homeassistant/components/knx/storage/config_store.py
@@ -1,6 +1,5 @@
 """KNX entity configuration store."""
 
-from collections.abc import Callable
 import logging
 from typing import Any, Final, TypedDict
 
@@ -8,12 +7,11 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PLATFORM, Platform
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers import entity_registry as er
-from homeassistant.helpers.dispatcher import async_dispatcher_send
 from homeassistant.helpers.storage import Store
 from homeassistant.util.ulid import ulid_now
 
 from ..const import DOMAIN
-from ..knx_entity import SIGNAL_ENTITY_REMOVE
+from ..knx_entity import KnxUiEntityPlatformController
 from .const import CONF_DATA
 
 _LOGGER = logging.getLogger(__name__)
@@ -46,12 +44,7 @@ class KNXConfigStore:
         self.config_entry = config_entry
         self._store = Store[KNXConfigStoreModel](hass, STORAGE_VERSION, STORAGE_KEY)
         self.data = KNXConfigStoreModel(entities={})
-
-        # entities and async_add_entity are filled by platform / entity setups
-        self.entities: set[str] = set()  # unique_id as values
-        self.async_add_entity: dict[
-            Platform, Callable[[str, dict[str, Any]], None]
-        ] = {}
+        self.platform_controllers: dict[Platform, KnxUiEntityPlatformController] = {}
 
     async def load_data(self) -> None:
         """Load config store data from storage."""
@@ -66,10 +59,14 @@ class KNXConfigStore:
         self, platform: Platform, data: dict[str, Any]
     ) -> str | None:
         """Create a new entity."""
-        if platform not in self.async_add_entity:
-            raise ConfigStoreException(f"Entity platform not ready: {platform}")
+        try:
+            platform_controller = self.platform_controllers[platform]
+        except KeyError as err:
+            raise ConfigStoreException(
+                f"Entity platform not ready: {platform}"
+            ) from err
         unique_id = f"knx_es_{ulid_now()}"
-        self.async_add_entity[platform](unique_id, data)
+        await platform_controller.add_entity(unique_id, data)
         # store data after entity was added to be sure config didn't raise exceptions
         self.data["entities"].setdefault(platform, {})[unique_id] = data
         await self._store.async_save(self.data)
@@ -95,8 +92,13 @@ class KNXConfigStore:
         self, platform: Platform, entity_id: str, data: dict[str, Any]
     ) -> None:
         """Update an existing entity."""
-        if platform not in self.async_add_entity:
-            raise ConfigStoreException(f"Entity platform not ready: {platform}")
+        try:
+            platform_controller = self.platform_controllers[platform]
+        except KeyError as err:
+            raise ConfigStoreException(
+                f"Entity platform not ready: {platform}"
+            ) from err
+        unique_id = f"knx_es_{ulid_now()}"
         entity_registry = er.async_get(self.hass)
         if (entry := entity_registry.async_get(entity_id)) is None:
             raise ConfigStoreException(f"Entity not found: {entity_id}")
@@ -108,8 +110,7 @@ class KNXConfigStore:
             raise ConfigStoreException(
                 f"Entity not found in storage: {entity_id} - {unique_id}"
             )
-        async_dispatcher_send(self.hass, SIGNAL_ENTITY_REMOVE.format(unique_id))
-        self.async_add_entity[platform](unique_id, data)
+        await platform_controller.reload_entity(entity_id, unique_id, data)
         # store data after entity is added to make sure config doesn't raise exceptions
         self.data["entities"][platform][unique_id] = data
         await self._store.async_save(self.data)
@@ -125,23 +126,21 @@ class KNXConfigStore:
             raise ConfigStoreException(
                 f"Entity not found in {entry.domain}: {entry.unique_id}"
             ) from err
-        try:
-            self.entities.remove(entry.unique_id)
-        except KeyError:
-            _LOGGER.warning("Entity not initialized when deleted: %s", entity_id)
         entity_registry.async_remove(entity_id)
         await self._store.async_save(self.data)
 
     def get_entity_entries(self) -> list[er.RegistryEntry]:
-        """Get entity_ids of all configured entities by platform."""
+        """Get entity_ids of all UI configured entities."""
         entity_registry = er.async_get(self.hass)
-
+        unique_ids = {
+            uid for platform in self.data["entities"].values() for uid in platform
+        }
         return [
             registry_entry
             for registry_entry in er.async_entries_for_config_entry(
                 entity_registry, self.config_entry.entry_id
             )
-            if registry_entry.unique_id in self.entities
+            if registry_entry.unique_id in unique_ids
         ]
 
 
