aec29a2f7acf31129209dab5185955d975f823da
David Bonnes <zxdavb@bonnes.me>
2024-08-04 23:51:00 +0200

    tweak const

--- tests/components/evohome/const.py
+++ tests/components/evohome/const.py
@@ -9,21 +9,11 @@ REFRESH_TOKEN: Final = "rf_jg68ZCKYdxEI3fF..."
 SESSION_ID: Final = "F7181186..."
 USERNAME: Final = "test_user@gmail.com"
 
+# The h-numbers refer to issues in HA's core repo
 TEST_INSTALLS: Final = {
-    "minimal": {
-        "num_zones": 1,
-    },  # evohome (single zone, no DHW)
-    "default": {
-        "num_zones": 7,
-        "num_dhw": 1,
-    },  # evohome
-    "h032585": {
-        "num_svcs": 4,
-        "num_zones": 1,
-    },  # VisionProWifi
+    "minimal": {"num_zones": 1},  # evohome (single zone, no DHW)
+    "default": {"num_zones": 7, "num_dhw": 1},  # evohome
+    "h032585": {"num_svcs": 4, "num_zones": 1},  # VisionProWifi
     "h099625": {"num_svcs": 4, "num_zones": 2},  # RoundThermostat
-    "system_004": {
-        "num_svcs": 4,
-        "num_zones": 1,
-    },  # Multiple
+    "system_004": {"num_svcs": 4, "num_zones": 1},  # RoundModulation
 }
