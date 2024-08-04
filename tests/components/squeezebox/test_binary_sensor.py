a5ed27fcd26ebd2ea2da3deb752fb58b4a067ee7
pssc <phillip.camp+git@gmail.com>
2024-08-05 00:39:34 +0100

    Fix import

--- tests/components/squeezebox/test_binary_sensor.py
+++ tests/components/squeezebox/test_binary_sensor.py
@@ -2,8 +2,6 @@
 
 from unittest.mock import patch
 
-from syrupy.assertion import SnapshotAssertion
-
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
