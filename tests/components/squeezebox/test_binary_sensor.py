1be72dc089a816c046d0274170b2c62dc012a375
pssc <phillip.camp+git@gmail.com>
2024-08-05 00:36:55 +0100

    add tests for binary sensors

---
+++ tests/components/squeezebox/test_binary_sensor.py
@@ -0,0 +1,31 @@
+"""Test BMW binary sensors."""
+
+from unittest.mock import patch
+
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from . import FAKE_STATUS, setup_mocked_integration
+
+
+async def test_binary_sensor(
+    hass: HomeAssistant,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test binary sensor states and attributes."""
+
+    # Setup component
+    with (
+        patch(
+            "homeassistant.components.squeezebox.PLATFORMS",
+            [Platform.BINARY_SENSOR],
+        ),
+        patch(
+            "pysqueezebox.Server.async_query",
+            return_value=FAKE_STATUS,
+        ),
+    ):
+        await setup_mocked_integration(hass)
