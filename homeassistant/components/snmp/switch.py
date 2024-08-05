9e6385b869e1fd344fe868f1a5cc7dddbd15c3de
Guy Lowe <github@gunk.gen.nz>
2024-08-06 08:56:41 +1200

    Tweak unknown SNMP response warning

--- homeassistant/components/snmp/switch.py
+++ homeassistant/components/snmp/switch.py
@@ -278,10 +278,9 @@ class SnmpSwitch(SwitchEntity):
                     self._state = False
                 else:
                     _LOGGER.warning(
-                        "SNMP warning: "
-                        "did not recognise %s as a valid response payload, "
-                        "state will be unknown",
+                        "Invalid payload '%s' received for entity %s, state is unknown",
                         resrow[-1],
+                        self.entity_id,
                     )
                     self._state = None
 
