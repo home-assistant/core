3cf3780587be1e8a287507375de07b421dff31df
Joost Lekkerkerker <joostlek@outlook.com>
2024-08-06 16:28:37 +0200

    Bump mficlient to 0.5.0 (#123250)

--- script/licenses.py
+++ script/licenses.py
@@ -182,9 +182,6 @@ TODO = {
     "asterisk_mbox": AwesomeVersion(
         "0.5.0"
     ),  # No license, integration is deprecated and scheduled for removal in 2024.9.0
-    "mficlient": AwesomeVersion(
-        "0.3.0"
-    ),  # No license https://github.com/kk7ds/mficlient/issues/4
     "pyflic": AwesomeVersion("2.0.3"),  # No OSI approved license CC0-1.0 Universal)
     "uvcclient": AwesomeVersion(
         "0.11.0"
