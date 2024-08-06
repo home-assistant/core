7b53369fb65078a2e615dcf99c8ce6e1d1359175
Steve Repsher <steverep@users.noreply.github.com>
2024-08-06 02:53:14 +0000

    Convert anchor test to directory index test

--- tests/components/http/test_static.py
+++ tests/components/http/test_static.py
@@ -30,31 +30,19 @@ async def mock_http_client(hass: HomeAssistant, aiohttp_client: ClientSessionGen
     return await aiohttp_client(hass.http.app, server_kwargs={"skip_url_asserts": True})
 
 
-@pytest.mark.parametrize(
-    ("url", "canonical_url"),
-    [
-        ("//a", "//a"),
-        ("///a", "///a"),
-        ("/c:\\a\\b", "/c:%5Ca%5Cb"),
-    ],
-)
-async def test_static_path_blocks_anchors(
-    hass: HomeAssistant,
-    mock_http_client: TestClient,
-    tmp_path: Path,
-    url: str,
-    canonical_url: str,
+async def test_static_resource_show_index(
+    hass: HomeAssistant, mock_http_client: TestClient, tmp_path: Path
 ) -> None:
-    """Test static paths block anchors."""
+    """Test static resource will return a directory index."""
     app = hass.http.app
 
-    resource = CachingStaticResource(url, str(tmp_path))
-    assert resource.canonical == canonical_url
+    resource = CachingStaticResource("/", tmp_path, show_index=True)
     app.router.register_resource(resource)
     app[KEY_ALLOW_CONFIGURED_CORS](resource)
 
-    resp = await mock_http_client.get(canonical_url, allow_redirects=False)
-    assert resp.status == 403
+    resp = await mock_http_client.get("/")
+    assert resp.status == 200
+    assert resp.content_type == "text/html"
 
 
 async def test_async_register_static_paths(
