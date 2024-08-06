affc68b792cb8ac675e3b61facce01ab84b74a95
IceBotYT <34712694+IceBotYT@users.noreply.github.com>
2024-08-06 15:55:41 +0000

    Fixes suggested by @joostlek

--- homeassistant/components/nice_go/config_flow.py
+++ homeassistant/components/nice_go/config_flow.py
@@ -2,15 +2,14 @@
 
 from __future__ import annotations
 
-from collections.abc import Mapping
 from datetime import datetime
 import logging
 from typing import Any
 
-from nice_go import AuthFailedError, NiceGOApi
+from nice_go import ApiError, AuthFailedError, NiceGOApi
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigEntry, ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
@@ -20,10 +19,12 @@ from .const import CONF_REFRESH_TOKEN, CONF_REFRESH_TOKEN_CREATION_TIME, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER_DATA_SCHEMA = {
-    vol.Required(CONF_EMAIL): str,
-    vol.Required(CONF_PASSWORD): str,
-}
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_EMAIL): str,
+        vol.Required(CONF_PASSWORD): str,
+    }
+)
 
 
 async def validate_input(
@@ -59,58 +60,52 @@ class NiceGOConfigFlow(ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    def __init__(self) -> None:
-        """Initialize the config flow."""
-        self._reauth_entry: ConfigEntry | None = None
-
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
-        data_schema = vol.Schema(STEP_USER_DATA_SCHEMA)
-
-        if user_input is None:
-            return self.async_show_form(step_id="user", data_schema=data_schema)
-
-        await self.async_set_unique_id(user_input[CONF_EMAIL])
-        self._abort_if_unique_id_configured()
 
         errors = {}
 
-        try:
-            info = await validate_input(self.hass, user_input)
-        except InvalidAuth:
-            errors["base"] = "invalid_auth"
-        except Exception:
-            _LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
-        else:
-            # Check if we are reauthenticating
-            if self._reauth_entry is not None:
-                self.hass.config_entries.async_update_entry(
-                    self._reauth_entry,
+        if user_input is not None:
+            await self.async_set_unique_id(user_input[CONF_EMAIL])
+            self._abort_if_unique_id_configured()
+
+            hub = NiceGOApi()
+
+            try:
+                refresh_token = await hub.authenticate(
+                    user_input[CONF_EMAIL],
+                    user_input[CONF_PASSWORD],
+                    async_get_clientsession(self.hass),
+                )
+            except AuthFailedError:
+                errors["base"] = "invalid_auth"
+            except ApiError as e:
+                _LOGGER.exception("API error")
+                if "UserNotFoundException" in str(e.__context__):
+                    errors["base"] = "user_not_found"
+                else:
+                    errors["base"] = "unknown"
+            except Exception:
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                info = {
+                    CONF_EMAIL: user_input[CONF_EMAIL],
+                    CONF_PASSWORD: user_input[CONF_PASSWORD],
+                    CONF_REFRESH_TOKEN: refresh_token,
+                    CONF_REFRESH_TOKEN_CREATION_TIME: datetime.now().timestamp(),
+                }
+
+                return self.async_create_entry(
+                    title=user_input[CONF_EMAIL],
                     data=info,
                 )
-                await self.hass.config_entries.async_reload(self._reauth_entry.entry_id)
-                return self.async_abort(reason="reauth_successful")
-
-            return self.async_create_entry(
-                title=user_input[CONF_EMAIL],
-                data=info,
-            )
 
         return self.async_show_form(
-            step_id="user", data_schema=data_schema, errors=errors
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
-
-    async def async_step_reauth(
-        self, entry_data: Mapping[str, Any]
-    ) -> ConfigFlowResult:
-        """Reauth in case of a password change or other error."""
-        self._reauth_entry = self.hass.config_entries.async_get_entry(
-            self.context["entry_id"]
-        )
-        return await self.async_step_user()
 
 
 class InvalidAuth(HomeAssistantError):
