dec0b425576253ce13bad1f57a0bdca0e9e7a78e
Brett <brett.whynot@gmail.com>
2024-08-07 10:34:15 +0000

    async_register_implementation in init

--- homeassistant/components/tesla_fleet/config_flow.py
+++ homeassistant/components/tesla_fleet/config_flow.py
@@ -13,7 +13,7 @@ from homeassistant.config_entries import ConfigEntry, ConfigFlowResult
 from homeassistant.helpers import config_entry_oauth2_flow
 
 from .application_credentials import TeslaOAuth2Implementation
-from .const import CLIENT_ID, DOMAIN, LOGGER
+from .const import CLIENT_ID, DOMAIN, LOGGER, NAME
 
 
 class OAuth2FlowHandler(
@@ -36,7 +36,7 @@ class OAuth2FlowHandler(
         self.async_register_implementation(
             self.hass,
             TeslaOAuth2Implementation(
-                self.hass, DOMAIN, ClientCredential(CLIENT_ID, "", "Home Assistant")
+                self.hass, DOMAIN, ClientCredential(CLIENT_ID, "", NAME)
             ),
         )
 
