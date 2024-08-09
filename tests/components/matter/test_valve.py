d78e954e8d8a8f7bcda7fa6335e9a8a89f293ff6
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-08-09 17:27:12 +0200

    Update test_valve.py

--- tests/components/matter/test_valve.py
+++ tests/components/matter/test_valve.py
@@ -1,1 +1,21 @@
-te
+"""Test Matter valve."""
+
+from math import floor
+from unittest.mock import MagicMock, call
+
+from chip.clusters import Objects as clusters
+import pytest
+
+from homeassistant.components.cover import (
+    STATE_CLOSED,
+    STATE_OPEN,
+    STATE_OPENING,
+    ValveEntityFeature,
+)
+from homeassistant.core import HomeAssistant
+
+from .common import (
+    set_node_attribute,
+    setup_integration_with_node_fixture,
+    trigger_subscription_callback,
+)
