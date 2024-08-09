5ae7d475ddacf096f77539f5f349b93f5914a9d1
wittypluck <wittypluck@users.noreply.github.com>
2024-08-09 11:21:05 +0200

    Set uptime to None if key is missing

--- homeassistant/components/glances/coordinator.py
+++ homeassistant/components/glances/coordinator.py
@@ -45,13 +45,10 @@ class GlancesDataUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
         except exceptions.GlancesApiError as err:
             raise UpdateFailed from err
         # Update computed values
-        up_duration: timedelta | None = (
-            parse_duration(data["uptime"]) if "uptime" in data else None
-        )
-        uptime: datetime | None = (
-            self.data["computed"]["uptime"] if up_duration and self.data else None
-        )
-        if up_duration:
+        uptime: datetime | None = None
+        up_duration: timedelta | None = None
+        if "uptime" in data and (up_duration := parse_duration(data["uptime"])):
+            uptime = self.data["computed"]["uptime"] if self.data else None
             # Update uptime if previous value is None or previous uptime is bigger than
             # new uptime (i.e. server restarted)
             if uptime is None or self.data["computed"]["uptime_duration"] > up_duration:
