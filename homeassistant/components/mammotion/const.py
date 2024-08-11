979fd374e7d89bdf82c0f4f2d7172585512c16e0
Michael Arthur <michael.arthur@gameglass.gg>
2024-08-11 15:40:28 +1200

    tmp

--- homeassistant/components/mammotion/const.py
+++ homeassistant/components/mammotion/const.py
@@ -5,6 +5,8 @@ from typing import Final
 
 DOMAIN: Final = "mammotion"
 
+DEVICE_SUPPORT = ("Luba", "Yuka")
+
 DEFAULT_RETRY_COUNT = 3
 CONF_RETRY_COUNT = "retry_count"
 LOGGER: Final = logging.getLogger(__package__)
