629bc5e31f147a4459f1df528a7b48132805c6d0
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2024-08-12 08:24:49 +0200

    add DHW target temp number entity

--- homeassistant/components/vicare/number.py
+++ homeassistant/components/vicare/number.py
@@ -51,6 +51,18 @@ class ViCareNumberEntityDescription(NumberEntityDescription, ViCareRequiredKeysM
 
 DEVICE_ENTITY_DESCRIPTIONS: tuple[ViCareNumberEntityDescription, ...] = (
     ViCareNumberEntityDescription(
+        key="dhw_temperature",
+        translation_key="dhw_temperature",
+        entity_category=EntityCategory.CONFIG,
+        device_class=NumberDeviceClass.TEMPERATURE,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        value_getter=lambda api: api.getDomesticHotWaterConfiguredTemperature(),
+        value_setter=lambda api, value: api.setDomesticHotWaterTemperature(value),
+        native_min_value=lambda api: api.getDomesticHotWaterMinTemperature(),
+        native_max_value=lambda api: api.getDomesticHotWaterMaxTemperature(),
+        native_step=1,
+    ),
+    ViCareNumberEntityDescription(
         key="dhw_secondary_temperature",
         translation_key="dhw_secondary_temperature",
         entity_category=EntityCategory.CONFIG,
