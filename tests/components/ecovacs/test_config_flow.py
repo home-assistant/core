401e36b8857c7f3370f057bab281752a7afaf70f
G Johansson <goran.johansson@shiftit.se>
2024-08-12 09:09:51 +0200

    Remove deprecated yaml import from Ecovacs (#123605)

--- tests/components/ecovacs/test_config_flow.py
+++ tests/components/ecovacs/test_config_flow.py
@@ -11,27 +11,22 @@ from deebot_client.mqtt_client import create_mqtt_config
 import pytest
 
 from homeassistant.components.ecovacs.const import (
-    CONF_CONTINENT,
     CONF_OVERRIDE_MQTT_URL,
     CONF_OVERRIDE_REST_URL,
     CONF_VERIFY_MQTT_CERTIFICATE,
     DOMAIN,
     InstanceMode,
 )
-from homeassistant.config_entries import SOURCE_IMPORT, SOURCE_USER
-from homeassistant.const import CONF_COUNTRY, CONF_MODE, CONF_USERNAME
-from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
+from homeassistant.config_entries import SOURCE_USER
+from homeassistant.const import CONF_MODE, CONF_USERNAME
+from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
-from homeassistant.helpers import issue_registry as ir
 
 from .const import (
-    IMPORT_DATA,
     VALID_ENTRY_DATA_CLOUD,
     VALID_ENTRY_DATA_SELF_HOSTED,
     VALID_ENTRY_DATA_SELF_HOSTED_WITH_VALIDATE_CERT,
 )
-
-from tests.common import MockConfigEntry
 
 _USER_STEP_SELF_HOSTED = {CONF_MODE: InstanceMode.SELF_HOSTED}
 
@@ -303,116 +298,3 @@ async def test_user_flow_self_hosted_error(
     mock_setup_entry.assert_called()
     mock_authenticator_authenticate.assert_called()
     mock_mqtt_client.verify_config.assert_called()
-
-
-async def test_import_flow(
-    hass: HomeAssistant,
-    issue_registry: ir.IssueRegistry,
-    mock_setup_entry: AsyncMock,
-    mock_authenticator_authenticate: AsyncMock,
-    mock_mqtt_client: Mock,
-) -> None:
-    """Test importing yaml config."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={"source": SOURCE_IMPORT},
-        data=IMPORT_DATA.copy(),
-    )
-    mock_authenticator_authenticate.assert_called()
-
-    assert result["type"] is FlowResultType.CREATE_ENTRY
-    assert result["title"] == VALID_ENTRY_DATA_CLOUD[CONF_USERNAME]
-    assert result["data"] == VALID_ENTRY_DATA_CLOUD
-    assert (HOMEASSISTANT_DOMAIN, f"deprecated_yaml_{DOMAIN}") in issue_registry.issues
-    mock_setup_entry.assert_called()
-    mock_mqtt_client.verify_config.assert_called()
-
-
-async def test_import_flow_already_configured(
-    hass: HomeAssistant, issue_registry: ir.IssueRegistry
-) -> None:
-    """Test importing yaml config where entry already configured."""
-    entry = MockConfigEntry(domain=DOMAIN, data=VALID_ENTRY_DATA_CLOUD)
-    entry.add_to_hass(hass)
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={"source": SOURCE_IMPORT},
-        data=IMPORT_DATA.copy(),
-    )
-    assert result["type"] is FlowResultType.ABORT
-    assert result["reason"] == "already_configured"
-    assert (HOMEASSISTANT_DOMAIN, f"deprecated_yaml_{DOMAIN}") in issue_registry.issues
-
-
-@pytest.mark.parametrize("show_advanced_options", [True, False])
-@pytest.mark.parametrize(
-    ("side_effect", "reason"),
-    [
-        (ClientError, "cannot_connect"),
-        (InvalidAuthenticationError, "invalid_auth"),
-        (Exception, "unknown"),
-    ],
-)
-async def test_import_flow_error(
-    hass: HomeAssistant,
-    issue_registry: ir.IssueRegistry,
-    mock_authenticator_authenticate: AsyncMock,
-    mock_mqtt_client: Mock,
-    side_effect: Exception,
-    reason: str,
-    show_advanced_options: bool,
-) -> None:
-    """Test handling invalid connection."""
-    mock_authenticator_authenticate.side_effect = side_effect
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={
-            "source": SOURCE_IMPORT,
-            "show_advanced_options": show_advanced_options,
-        },
-        data=IMPORT_DATA.copy(),
-    )
-    assert result["type"] is FlowResultType.ABORT
-    assert result["reason"] == reason
-    assert (
-        DOMAIN,
-        f"deprecated_yaml_import_issue_{reason}",
-    ) in issue_registry.issues
-    mock_authenticator_authenticate.assert_called()
-
-
-@pytest.mark.parametrize("show_advanced_options", [True, False])
-@pytest.mark.parametrize(
-    ("reason", "user_input"),
-    [
-        ("invalid_country_length", IMPORT_DATA | {CONF_COUNTRY: "too_long"}),
-        ("invalid_country_length", IMPORT_DATA | {CONF_COUNTRY: "a"}),  # too short
-        ("invalid_continent_length", IMPORT_DATA | {CONF_CONTINENT: "too_long"}),
-        ("invalid_continent_length", IMPORT_DATA | {CONF_CONTINENT: "a"}),  # too short
-        ("continent_not_match", IMPORT_DATA | {CONF_CONTINENT: "AA"}),
-    ],
-)
-async def test_import_flow_invalid_data(
-    hass: HomeAssistant,
-    issue_registry: ir.IssueRegistry,
-    reason: str,
-    user_input: dict[str, Any],
-    show_advanced_options: bool,
-) -> None:
-    """Test handling invalid connection."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={
-            "source": SOURCE_IMPORT,
-            "show_advanced_options": show_advanced_options,
-        },
-        data=user_input,
-    )
-    assert result["type"] is FlowResultType.ABORT
-    assert result["reason"] == reason
-    assert (
-        DOMAIN,
-        f"deprecated_yaml_import_issue_{reason}",
-    ) in issue_registry.issues
