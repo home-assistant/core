c3e2010126cf40b3e18c5bc3226dccf52f956012
J. Nick Koston <nick@koston.org>
2024-08-13 09:04:41 -0500

    override

--- homeassistant/components/yale/gateway.py
+++ homeassistant/components/yale/gateway.py
@@ -2,7 +2,6 @@
 
 from http import HTTPStatus
 from pathlib import Path
-from typing import Any
 
 from aiohttp import ClientError, ClientSession
 from aiohttp.client_exceptions import ClientResponseError
@@ -10,16 +9,7 @@ from yalexs.authenticator_common import Authentication
 from yalexs.manager.exceptions import CannotConnect, InvalidAuth
 from yalexs.manager.gateway import Gateway
 
-from homeassistant.const import CONF_USERNAME
 from homeassistant.helpers import config_entry_oauth2_flow
-
-from .const import (
-    CONF_ACCESS_TOKEN_CACHE_FILE,
-    CONF_BRAND,
-    CONF_INSTALL_ID,
-    CONF_LOGIN_METHOD,
-    DEFAULT_BRAND,
-)
 
 
 class YaleGateway(Gateway):
@@ -40,17 +30,6 @@ class YaleGateway(Gateway):
         if not self._oauth_session.valid_token:
             await self._oauth_session.async_ensure_token_valid()
         return self._oauth_session.token["access_token"]
-
-    def config_entry(self) -> dict[str, Any]:
-        """Config entry."""
-        assert self._config is not None
-        return {
-            CONF_BRAND: self._config.get(CONF_BRAND, DEFAULT_BRAND),
-            CONF_LOGIN_METHOD: self._config[CONF_LOGIN_METHOD],
-            CONF_USERNAME: self._config[CONF_USERNAME],
-            CONF_INSTALL_ID: self._config.get(CONF_INSTALL_ID),
-            CONF_ACCESS_TOKEN_CACHE_FILE: self._access_token_cache_file,
-        }
 
     async def async_authenticate(self) -> Authentication:
         """Authenticate with the details provided to setup."""
