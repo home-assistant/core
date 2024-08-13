b490a01a821cc1d8fadb81c8b49f2be490ce2bed
Erik <erik@montnemery.com>
2024-08-13 09:10:22 +0200

    Remove unnecessary assignment of Template.hass from template

--- homeassistant/components/template/light.py
+++ homeassistant/components/template/light.py
@@ -126,7 +126,7 @@ async def _async_create_entities(hass, config):
     lights = []
 
     for object_id, entity_config in config[CONF_LIGHTS].items():
-        entity_config = rewrite_common_legacy_to_modern_conf(entity_config)
+        entity_config = rewrite_common_legacy_to_modern_conf(hass, entity_config)
         unique_id = entity_config.get(CONF_UNIQUE_ID)
 
         lights.append(
