ba54a19d4b9819f2f35ad628089370c6c2a37c7f
epenet <6771947+epenet@users.noreply.github.com>
2024-08-13 18:01:06 +0200

    Simplify mock_tts_cache_dir_autouse fixture (#123783)

--- tests/components/cloud/conftest.py
+++ tests/components/cloud/conftest.py
@@ -187,9 +187,8 @@ def set_cloud_prefs_fixture(
 
 
 @pytest.fixture(autouse=True)
-def mock_tts_cache_dir_autouse(mock_tts_cache_dir: Path) -> Path:
+def mock_tts_cache_dir_autouse(mock_tts_cache_dir: Path) -> None:
     """Mock the TTS cache dir with empty dir."""
-    return mock_tts_cache_dir
 
 
 @pytest.fixture(autouse=True)
