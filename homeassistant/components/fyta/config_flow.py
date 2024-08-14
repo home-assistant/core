ad97e4edc024e0b33324e793e904e4d602e49c30
dontinelli <73341522+dontinelli@users.noreply.github.com>
2024-08-14 20:06:40 +0000

    Code adjustments to enable strit typing

--- homeassistant/components/fyta/config_flow.py
+++ homeassistant/components/fyta/config_flow.py
@@ -12,6 +12,7 @@ from fyta_cli.fyta_exceptions import (
     FytaConnectionError,
     FytaPasswordError,
 )
+from fyta_cli.fyta_models import Credentials
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
@@ -23,7 +24,7 @@ from homeassistant.helpers.selector import (
 )
 
 from . import FytaConfigEntry
-from .const import CONF_EXPIRATION, DOMAIN
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -54,7 +55,7 @@ class FytaConfigFlow(ConfigFlow, domain=DOMAIN):
 
     def __init__(self) -> None:
         """Initialize FytaConfigFlow."""
-        self.credentials: dict[str, Any] = {}
+        self.credentials: Credentials
         self._entry: FytaConfigEntry | None = None
 
     async def async_auth(self, user_input: Mapping[str, Any]) -> dict[str, str]:
@@ -75,10 +76,6 @@ class FytaConfigFlow(ConfigFlow, domain=DOMAIN):
         finally:
             await fyta.client.close()
 
-        self.credentials[CONF_EXPIRATION] = self.credentials[
-            CONF_EXPIRATION
-        ].isoformat()
-
         return {}
 
     async def async_step_user(
@@ -90,7 +87,8 @@ class FytaConfigFlow(ConfigFlow, domain=DOMAIN):
             self._async_abort_entries_match({CONF_USERNAME: user_input[CONF_USERNAME]})
 
             if not (errors := await self.async_auth(user_input)):
-                user_input |= self.credentials
+                user_input |= {"access_token": self.credentials.access_token}
+                user_input |= {"expiration": self.credentials.expiration.isoformat()}
                 return self.async_create_entry(
                     title=user_input[CONF_USERNAME], data=user_input
                 )
@@ -114,7 +112,8 @@ class FytaConfigFlow(ConfigFlow, domain=DOMAIN):
         assert self._entry is not None
 
         if user_input and not (errors := await self.async_auth(user_input)):
-            user_input |= self.credentials
+            user_input |= {"access_token": self.credentials.access_token}
+            user_input |= {"expiration": self.credentials.expiration.isoformat()}
             return self.async_update_reload_and_abort(
                 self._entry, data={**self._entry.data, **user_input}
             )
