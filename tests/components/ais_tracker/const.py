60f701b98bd008c40e09edeefbbfd55334542085
mib1185 <mail@mib85.de>
2024-08-15 20:29:21 +0000

    try to mock the udp receiver

--- tests/components/ais_tracker/const.py
+++ tests/components/ais_tracker/const.py
@@ -3,4 +3,4 @@
 from homeassistant.components.ais_tracker.const import CONF_MMSIS
 from homeassistant.const import CONF_PORT
 
-MOCK_USER_DATA = {CONF_PORT: 12346, CONF_MMSIS: ["123456789"]}
+MOCK_USER_DATA = {CONF_PORT: 12346, CONF_MMSIS: ["211517950"]}
--- tests/components/ais_tracker/test_init.py
+++ tests/components/ais_tracker/test_init.py
@@ -1,7 +1,10 @@
 """Tests for AIS tracker config flow."""
 
-from unittest.mock import patch
+from collections.abc import Generator
+from unittest.mock import MagicMock, patch
 
+from pyais.messages import NMEAMessage
+from pyais.stream import SocketStream
 from syrupy import SnapshotAssertion
 
 from homeassistant.components.ais_tracker.const import DOMAIN
@@ -10,6 +13,27 @@ from homeassistant.core import HomeAssistant
 from .const import MOCK_USER_DATA
 
 from tests.common import MockConfigEntry
+
+
+class MockUDPReceiver(SocketStream):
+    """Mock UDP receiver."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize the mock."""
+        self.messages = [
+            NMEAMessage(b"!AIVDM,1,1,,B,139f1wPP0CPwM>JM<3eiK?wf25KT,0*60"),
+            NMEAMessage(
+                b"!AIVDM,2,1,4,A,539f1w`00000@?GK;D1<<Phu=<H4DQ8D0000000t20=72t0Ht1P00000,0*6D"
+                b"!AIVDM,2,2,4,A,000000000000000,2*20"
+            ),
+        ]
+        socket = MagicMock()
+        super().__init__(socket, preprocessor=None)
+
+    def __iter__(self) -> Generator[NMEAMessage, None, None]:
+        """Iterate over messages."""
+        while True:
+            yield from [self.messages.pop()]
 
 
 async def test_init(
@@ -21,11 +45,11 @@ async def test_init(
     mock_config.add_to_hass(hass)
 
     with patch(
-        "homeassistant.components.ais_tracker.coordinator.AisTrackerCoordinator.async_setup"
-    ) as mock_setup_coordinator:
+        "homeassistant.components.ais_tracker.coordinator.UDPReceiver",
+        new=MockUDPReceiver,
+    ):
         await hass.config_entries.async_setup(mock_config.entry_id)
-
-    assert mock_setup_coordinator.called
+        await hass.async_block_till_done()
 
     states = hass.states.async_all()
     assert len(states) == 8
