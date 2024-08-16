bd3f3341c4dd345b8ee5e9c02b7750cc4694f6c6
GeoffAtHome <geoff@soord.org.uk>
2024-08-16 17:20:38 +0000

    Improve tests.

--- tests/components/geniushub/test_cloud_setup.py
+++ tests/components/geniushub/test_cloud_setup.py
@@ -5,21 +5,41 @@ from unittest.mock import AsyncMock
 import pytest
 
 from homeassistant.components.geniushub import DOMAIN
-from homeassistant.config_entries import SOURCE_USER
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_TOKEN, CONF_USERNAME
+from homeassistant.const import CONF_TOKEN
 from homeassistant.core import HomeAssistant
-from homeassistant.data_entry_flow import FlowResultType
 from homeassistant.helpers import entity_registry as er
 
-from tests.common import load_json_object_fixture
+from tests.common import (
+    MockConfigEntry,
+    load_json_array_fixture,
+    load_json_object_fixture,
+)
 from tests.test_util.aiohttp import AiohttpClientMocker
+
+
+async def init_cloud_integration(
+    hass: HomeAssistant,
+) -> MockConfigEntry:
+    """Set up the AccuWeather integration in Home Assistant."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="Genius hub",
+        data={
+            CONF_TOKEN: "abcdef",
+        },
+    )
+    entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    return entry
 
 
 @pytest.fixture(autouse=True)
 def mock_cloud_all(aioclient_mock: AiohttpClientMocker) -> None:
     """Mock all setup requests."""
-    zones = load_json_object_fixture("zones_cloud_test_data.json", DOMAIN)
-    devices = load_json_object_fixture("devices_cloud_test_data.json", DOMAIN)
+    zones = load_json_array_fixture("zones_cloud_test_data.json", DOMAIN)
+    devices = load_json_array_fixture("devices_cloud_test_data.json", DOMAIN)
     aioclient_mock.get(
         "https://my.geniushub.co.uk/v1/version",
         json={
@@ -62,88 +82,47 @@ def mock_cloud_single_zone_with_switch(aioclient_mock: AiohttpClientMocker) -> N
 
 async def test_cloud_all_sensors(
     hass: HomeAssistant,
-    mock_cloud_config_entry: AsyncMock,
     mock_cloud_all: AsyncMock,
     entity_registry: er.EntityRegistry,
 ) -> None:
-    """Test full cloud flow."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
-    assert result["type"] is FlowResultType.MENU
-    assert result["step_id"] == "user"
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {"next_step_id": "cloud_api"},
-    )
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "cloud_api"
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {
-            CONF_TOKEN: "abcdef",
-        },
-    )
-    assert result["type"] is FlowResultType.CREATE_ENTRY
-    assert result["title"] == "Genius hub"
-    assert result["data"] == {
-        CONF_TOKEN: "abcdef",
-    }
-
-
-async def xtest_cloud_single_zone_and_switch(
-    hass: HomeAssistant,
-    mock_cloud_config_entry: AsyncMock,
-    mock_cloud_single_zone_with_switch: AsyncMock,
-    entity_registry: er.EntityRegistry,
-) -> None:
     """Test full local flow."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
-    await hass.async_block_till_done()
+    await init_cloud_integration(hass)
 
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {"next_step_id": "local_api"},
-    )
-    await hass.async_block_till_done()
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
+    entities = [
         {
-            CONF_HOST: "10.0.0.130",
-            CONF_USERNAME: "test-username",
-            CONF_PASSWORD: "test-password",
+            "id": "binary_sensor.single_channel_receiver_22",
+            "name": "Single Channel Receiver 22",
         },
-    )
+        {"id": "sensor.geniushub_errors", "name": "GeniusHub Errors"},
+        {"id": "sensor.geniushub_information", "name": "GeniusHub Information"},
+        {"id": "sensor.geniushub_warnings", "name": "GeniusHub Warnings"},
+        {"id": "sensor.radiator_valve_11", "name": "Radiator Valve 11"},
+        {"id": "sensor.radiator_valve_56", "name": "Radiator Valve 56"},
+        {"id": "sensor.radiator_valve_68", "name": "Radiator Valve 68"},
+        {"id": "sensor.radiator_valve_78", "name": "Radiator Valve 78"},
+        {"id": "sensor.radiator_valve_85", "name": "Radiator Valve 85"},
+        {"id": "sensor.radiator_valve_88", "name": "Radiator Valve 88"},
+        {"id": "sensor.radiator_valve_89", "name": "Radiator Valve 89"},
+        {"id": "sensor.radiator_valve_90", "name": "Radiator Valve 90"},
+        {"id": "sensor.room_sensor_16", "name": "Room Sensor 16"},
+        {"id": "sensor.room_sensor_17", "name": "Room Sensor 17"},
+        {"id": "sensor.room_sensor_18", "name": "Room Sensor 18"},
+        {"id": "sensor.room_sensor_20", "name": "Room Sensor 20"},
+        {"id": "sensor.room_sensor_21", "name": "Room Sensor 21"},
+        {"id": "sensor.room_sensor_50", "name": "Room Sensor 50"},
+        {"id": "sensor.room_sensor_53", "name": "Room Sensor 53"},
+        {"id": "switch.bedroom_socket", "name": "Bedroom Socket"},
+        {"id": "switch.kitchen_socket", "name": "Kitchen Socket"},
+        {"id": "switch.study_socket", "name": "Study Socket"},
+        {"id": "climate.ensuite", "name": "Ensuite"},
+        {"id": "climate.bedroom", "name": "Bedroom"},
+        {"id": "climate.guest_room", "name": "Guest room"},
+        {"id": "climate.hall", "name": "Hall"},
+        {"id": "climate.kitchen", "name": "Kitchen"},
+        {"id": "climate.lounge", "name": "Lounge"},
+        {"id": "climate.study", "name": "Study"},
+    ]
 
-    test_entity_id = "switch.study_socket"
-    switch = hass.states.get(test_entity_id)
-    assert switch.state == "off"
-
-    # call the HA turn_on service
-    await hass.services.async_call(
-        "switch",
-        "turn_on",
-        {"entity_id": test_entity_id},
-        blocking=True,
-    )
-
-    # The state should change but will not change until the next refresh
-    # How do I force a refresh?
-    await hass.async_block_till_done()
-    assert switch.state == "off"
-
-    # now call the HA turn_off service
-    await hass.services.async_call(
-        "switch",
-        "turn_off",
-        {"entity_id": test_entity_id},
-        blocking=True,
-    )
-
-    await hass.async_block_till_done()
-    assert switch.state == "off"
+    for entity in entities:
+        state = hass.states.get(entity["id"])
+        assert state.name == entity["name"]
