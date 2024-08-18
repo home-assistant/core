d58c6ce015b62fcfa37b2f17f94fb8079ae47b3d
Antoine Reversat <a.reversat@gmail.com>
2024-08-18 20:07:21 +0000

    Add test for reauth when an exception occurs.

--- tests/components/fujitsu_fglair/test_config_flow.py
+++ tests/components/fujitsu_fglair/test_config_flow.py
@@ -143,3 +143,66 @@ async def test_reauth_success(
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "reauth_successful"
     assert mock_config_entry.data[CONF_PASSWORD] == TEST_PASSWORD2
+
+
+@pytest.mark.parametrize(
+    ("exception", "err_msg"),
+    [
+        (AylaAuthError, "invalid_auth"),
+        (TimeoutError, "cannot_connect"),
+        (Exception, "unknown"),
+    ],
+)
+async def test_reauth_exceptions(
+    hass: HomeAssistant,
+    exception: Exception,
+    err_msg: str,
+    mock_setup_entry: AsyncMock,
+    mock_ayla_api: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test reauth flow when an exception occurs."""
+    mock_config_entry.add_to_hass(hass)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={
+            "source": SOURCE_REAUTH,
+            "entry_id": mock_config_entry.entry_id,
+            "title_placeholders": {"name": "test"},
+        },
+        data={
+            CONF_USERNAME: TEST_USERNAME,
+            CONF_PASSWORD: TEST_PASSWORD,
+            CONF_EUROPE: False,
+        },
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reauth_confirm"
+
+    mock_ayla_api.async_sign_in.side_effect = exception
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_PASSWORD: TEST_PASSWORD2,
+        },
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reauth_confirm"
+    assert result["errors"] == {"base": err_msg}
+
+    mock_ayla_api.async_sign_in.side_effect = None
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_PASSWORD: TEST_PASSWORD2,
+        },
+    )
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reauth_successful"
+    assert mock_config_entry.data[CONF_PASSWORD] == TEST_PASSWORD2
