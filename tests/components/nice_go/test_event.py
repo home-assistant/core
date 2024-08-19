831726a375238cc00745466153d85e67c1e1f50b
IceBotYT <34712694+IceBotYT@users.noreply.github.com>
2024-08-19 19:51:27 +0000

    Better assertions

--- tests/components/nice_go/test_event.py
+++ tests/components/nice_go/test_event.py
@@ -2,6 +2,9 @@
 
 from unittest.mock import AsyncMock, MagicMock
 
+from freezegun.api import FrozenDateTimeFactory
+
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from . import setup_integration
@@ -10,13 +13,25 @@ from tests.common import MockConfigEntry
 
 
 async def test_barrier_obstructed(
-    hass: HomeAssistant, mock_nice_go: AsyncMock, mock_config_entry: MockConfigEntry
+    hass: HomeAssistant,
+    mock_nice_go: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test barrier obstructed."""
     mock_nice_go.event = MagicMock()
-    await setup_integration(hass, mock_config_entry, ["event"])
+    await setup_integration(hass, mock_config_entry, [Platform.EVENT])
 
     await mock_nice_go.event.call_args_list[2][0][0]({"deviceId": "1"})
     await hass.async_block_till_done()
 
-    assert hass.states.get("event.nice_go_1_barrier_obstructed") != "unknown"
+    assert (
+        hass.states.get("event.test_garage_1_barrier_obstructed").state
+        == freezer.time_to_freeze.isoformat(timespec="milliseconds") + "+00:00"
+    )
+    assert (
+        hass.states.get("event.test_garage_1_barrier_obstructed").attributes[
+            "event_type"
+        ]
+        == "barrier_obstructed"
+    )
