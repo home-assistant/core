59785390ef8f6c3f9a3862053449cdfc78b63112
J. Nick Koston <nick@koston.org>
2024-08-19 08:17:33 -0500

    fixes

--- tests/components/shelly/test_sensor.py
+++ tests/components/shelly/test_sensor.py
@@ -1296,15 +1296,18 @@ async def test_rpc_device_sensor_goes_unavailable_on_disconnect(
     monkeypatch.setattr(mock_rpc_device, "connected", False)
     monkeypatch.setattr(mock_rpc_device, "initialized", False)
     mock_rpc_device.mock_disconnected()
+    await hass.async_block_till_done()
+    temp_sensor_state = hass.states.get("sensor.test_name_temperature")
     assert temp_sensor_state.state == STATE_UNAVAILABLE
 
     freezer.tick(60)
     async_fire_time_changed(hass)
     await hass.async_block_till_done()
-    # There should be no log messages
-    assert caplog.get_records("call") == []
+    assert "NotInitialized" not in caplog.text
 
     monkeypatch.setattr(mock_rpc_device, "connected", True)
     monkeypatch.setattr(mock_rpc_device, "initialized", True)
     mock_rpc_device.mock_initialized()
+    await hass.async_block_till_done()
+    temp_sensor_state = hass.states.get("sensor.test_name_temperature")
     assert temp_sensor_state.state != STATE_UNAVAILABLE
