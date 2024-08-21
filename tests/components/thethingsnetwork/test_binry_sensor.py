5d2a50856cc7f97990d98501d0ba2f985cad8d93
Angel Nunez Mencias <git@angelnu.com>
2024-08-22 00:32:36 +0200

    add testcases for binary_sensor support to thethingsnetwork

--- tests/components/thethingsnetwork/test_binry_sensor.py
+++ tests/components/thethingsnetwork/test_binry_sensor.py
@@ -1,21 +1,22 @@
 """Define tests for the The Things Network sensor."""
 
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 from . import init_integration
 from .conftest import (
     APP_ID,
+    BINARY_SENSOR_FIELD,
+    BINARY_SENSOR_FIELD_2,
     DATA_UPDATE,
     DEVICE_ID,
     DEVICE_ID_2,
     DOMAIN,
-    SENSOR_FIELD,
-    SENSOR_FIELD_2,
 )
 
 
-async def test_sensor(
+async def test_binary_sensor(
     hass: HomeAssistant,
     entity_registry: er.EntityRegistry,
     device_registry: dr.DeviceRegistry,
@@ -35,9 +36,21 @@ async def test_sensor(
     )
 
     # Check entities
-    assert entity_registry.async_get(f"sensor.{DEVICE_ID}_{SENSOR_FIELD}")
+    assert (
+        entity_registry.async_get(
+            f"binary_sensor.{DEVICE_ID}_{BINARY_SENSOR_FIELD}"
+        ).domain
+        == Platform.BINARY_SENSOR
+    )
 
-    assert not entity_registry.async_get(f"sensor.{DEVICE_ID_2}_{SENSOR_FIELD_2}")
+    assert not entity_registry.async_get(
+        f"binary_sensor.{DEVICE_ID_2}_{BINARY_SENSOR_FIELD_2}"
+    )
     push_callback = mock_ttnclient.call_args.kwargs["push_callback"]
     await push_callback(DATA_UPDATE)
-    assert entity_registry.async_get(f"sensor.{DEVICE_ID_2}_{SENSOR_FIELD_2}")
+    assert (
+        entity_registry.async_get(
+            f"binary_sensor.{DEVICE_ID_2}_{BINARY_SENSOR_FIELD_2}"
+        ).domain
+        == Platform.BINARY_SENSOR
+    )
