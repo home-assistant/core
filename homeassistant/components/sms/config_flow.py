f277d12fc07f94a9ce7f4db9937e5f10ed1cd11d
epenet <6771947+epenet@users.noreply.github.com>
2024-08-21 09:29:53 +0000

    Revert sms

--- homeassistant/components/sms/config_flow.py
+++ homeassistant/components/sms/config_flow.py
@@ -1,12 +1,11 @@
 """Config flow for SMS integration."""
 
 import logging
-from typing import Any
 
 import gammu
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import CONF_DEVICE
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
@@ -57,9 +56,7 @@ class SMSFlowHandler(ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
+    async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
