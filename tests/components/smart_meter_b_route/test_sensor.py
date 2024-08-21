09161784109ade55e84dbad62d987afa608dc1b8
Ravaka Razafimanantsoa <contact@ravaka.dev>
2024-08-21 12:18:24 +0000

    Fixing sensor tests

--- tests/components/smart_meter_b_route/test_sensor.py
+++ tests/components/smart_meter_b_route/test_sensor.py
@@ -1,19 +1,22 @@
 """Tests for the Smart Meter B-Route sensor."""
 
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
+from momonga import MomongaError
 import pytest
 
-from homeassistant.components.smart_meter_b_route.coordinator import (
-    BRouteUpdateCoordinator,
-)
+from homeassistant.components.smart_meter_b_route.const import DEFAULT_SCAN_INTERVAL
 from homeassistant.components.smart_meter_b_route.sensor import (
     SENSOR_DESCRIPTIONS,
     async_setup_entry,
 )
+from homeassistant.const import STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
+import homeassistant.util.dt as dt_util
 
 from . import configure_integration
+
+from tests.common import async_fire_time_changed
 
 
 async def test_async_setup_entry(hass: HomeAssistant, mock_momonga) -> None:
@@ -44,26 +47,29 @@ async def test_smart_meter_b_route_sensor_update(
 ) -> None:
     """Test SmartMeterBRouteSensor update."""
     config_entry = configure_integration(hass)
-    coordinator: BRouteUpdateCoordinator = config_entry.runtime_data
     await hass.config_entries.async_setup(config_entry.entry_id)
-    await coordinator.async_refresh()
     await hass.async_block_till_done()
+    async_fire_time_changed(hass, dt_util.utcnow() + DEFAULT_SCAN_INTERVAL)
+    await hass.async_block_till_done(wait_background_tasks=True)
+
     entity = hass.states.get(entity_id)
-    assert entity.state == index + 1
-    mock_momonga.assert_called()
+    assert entity.state == str(index + 1)
 
 
 async def test_smart_meter_b_route_sensor_no_update(
     hass: HomeAssistant, mock_momonga
 ) -> None:
     """Test SmartMeterBRouteSensor with no update."""
-    entity_id = "sensor.smart_meter_b_route_b_route_id_total_consumption"
+
+    entity_id = "sensor.smart_meter_b_route_b_route_id_instantaneous_current_r_phase"
     config_entry = configure_integration(hass)
     await hass.config_entries.async_setup(config_entry.entry_id)
-    coordinator: BRouteUpdateCoordinator = config_entry.runtime_data
-    coordinator.data = {}
     await hass.async_block_till_done()
+    with patch.object(
+        mock_momonga, "get_instantaneous_current", side_effect=MomongaError
+    ):
+        async_fire_time_changed(hass, dt_util.utcnow() + DEFAULT_SCAN_INTERVAL)
+        await hass.async_block_till_done(wait_background_tasks=True)
 
     entity = hass.states.get(entity_id)
-    assert entity.state is None
-    mock_momonga.assert_not_called()
+    assert entity.state is STATE_UNAVAILABLE
