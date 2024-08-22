f341b1f62d1c4baed22355f0fb1a3a74f62e127a
mrpasztoradam <mrpasztoradam@gmail.com>
2024-08-22 20:41:50 +0200

    Add ADS Valve entity

---
+++ homeassistant/components/ads/valve.py
@@ -0,0 +1,123 @@
+"""Support for ADS valve."""
+
+from datetime import timedelta
+
+import pyads
+import voluptuous as vol
+
+from homeassistant.components.valve import (
+    PLATFORM_SCHEMA as VALVE_PLATFORM_SCHEMA,
+    ValveEntity,
+    ValveEntityFeature,
+)
+from homeassistant.const import CONF_NAME
+from homeassistant.core import HomeAssistant
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+from .. import ads
+from . import AdsEntity
+
+SCAN_INTERVAL = timedelta(seconds=5)
+DEFAULT_NAME = "ADS Valve"
+
+CONF_ADS_VAR_STATE = "adsvar_state"
+
+PLATFORM_SCHEMA = VALVE_PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(CONF_ADS_VAR_STATE): cv.string,
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    }
+)
+
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up the valve platform for ADS."""
+    ads_hub = hass.data.get(ads.DATA_ADS)
+    ads_var_state = config.get(CONF_ADS_VAR_STATE)
+    name = config[CONF_NAME]
+
+    add_entities(
+        [
+            AdsValve(
+                ads_hub,
+                ads_var_state,
+                name,
+            )
+        ]
+    )
+
+
+class AdsValve(AdsEntity, ValveEntity):
+    """Representation of ADS valve entity."""
+
+    def __init__(
+        self,
+        ads_hub,
+        ads_var_state,
+        name,
+    ):
+        """Initialize AdsValve entity."""
+        super().__init__(ads_hub, name, ads_var_state)
+        self._ads_var_state = ads_var_state
+        self._name = name
+        self._attr_is_closed = False
+        self._attr_supported_features = (
+            ValveEntityFeature.OPEN | ValveEntityFeature.CLOSE
+        )
+        self._attr_reports_position = False
+
+    async def async_added_to_hass(self) -> None:
+        """Register device notification."""
+        if self._ads_var_state:
+            await self.async_initialize_device(self._ads_var_state, pyads.PLCTYPE_BOOL)
+
+        self.async_schedule_update_ha_state(True)
+
+    @property
+    def should_poll(self) -> bool:
+        """Return True if the entity should be polled."""
+        return True
+
+    @property
+    def name(self) -> str:
+        """Return the name of the valve device."""
+        return self._name
+
+    @property
+    def is_closed(self) -> bool | None:
+        """Return True if the valve is open."""
+        return self._attr_is_closed
+
+    @property
+    def supported_features(self) -> ValveEntityFeature:
+        """Return the list of supported features."""
+        return ValveEntityFeature.OPEN | ValveEntityFeature.CLOSE
+
+    async def async_open_valve(self) -> None:
+        """Open the valve."""
+        self._ads_hub.write_by_name(self._ads_var_state, True, pyads.PLCTYPE_BOOL)
+        self._attr_is_closed = False
+        self.async_schedule_update_ha_state(True)
+
+    async def async_close_valve(self) -> None:
+        """Close the valve."""
+        self._ads_hub.write_by_name(self._ads_var_state, False, pyads.PLCTYPE_BOOL)
+        self._attr_is_closed = True
+        self.async_schedule_update_ha_state(True)
+
+    async def async_update(self) -> None:
+        """Retrieve the latest state from the ADS device."""
+        # Read the valve state from the ADS device
+        state_value = self._ads_hub.read_by_name(
+            self._ads_var_state, pyads.PLCTYPE_BOOL
+        )
+        self._attr_is_closed = not state_value
+
+        self.async_write_ha_state()
