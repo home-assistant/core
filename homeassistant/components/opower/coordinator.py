460e9d0f828032e29e40e6010058a0262d3ea91b
tronikos <tronikos@users.noreply.github.com>
2024-08-23 10:07:40 +0000

    Add debug statements in get_cost_reads in opower

--- homeassistant/components/opower/coordinator.py
+++ homeassistant/components/opower/coordinator.py
@@ -227,9 +227,11 @@ class OpowerCoordinator(DataUpdateCoordinator[dict[str, Forecast]]):
         else:
             start = datetime.fromtimestamp(start_time, tz=tz) - timedelta(days=30)
         end = dt_util.now(tz)
+        _LOGGER.debug("Getting monthly cost reads: %s - %s", start, end)
         cost_reads = await self.api.async_get_cost_reads(
             account, AggregateType.BILL, start, end
         )
+        _LOGGER.debug("Got %s monthly cost reads", len(cost_reads))
         if account.read_resolution == ReadResolution.BILLING:
             return cost_reads
 
@@ -240,9 +242,11 @@ class OpowerCoordinator(DataUpdateCoordinator[dict[str, Forecast]]):
                 start = cost_reads[0].start_time
             assert start
             start = max(start, end - timedelta(days=3 * 365))
+        _LOGGER.debug("Getting daily cost reads: %s - %s", start, end)
         daily_cost_reads = await self.api.async_get_cost_reads(
             account, AggregateType.DAY, start, end
         )
+        _LOGGER.debug("Got %s daily cost reads", len(daily_cost_reads))
         _update_with_finer_cost_reads(cost_reads, daily_cost_reads)
         if account.read_resolution == ReadResolution.DAY:
             return cost_reads
@@ -252,8 +256,11 @@ class OpowerCoordinator(DataUpdateCoordinator[dict[str, Forecast]]):
         else:
             assert start
             start = max(start, end - timedelta(days=2 * 30))
+        _LOGGER.debug("Getting hourly cost reads: %s - %s", start, end)
         hourly_cost_reads = await self.api.async_get_cost_reads(
             account, AggregateType.HOUR, start, end
         )
+        _LOGGER.debug("Got %s hourly cost reads", len(hourly_cost_reads))
         _update_with_finer_cost_reads(cost_reads, hourly_cost_reads)
+        _LOGGER.debug("Got %s cost reads", len(cost_reads))
         return cost_reads
