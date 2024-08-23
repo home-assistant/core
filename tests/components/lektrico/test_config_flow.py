35899434b4aa3593b7ea6e4f8d95a028e0f49a37
lektrico <devs@lektri.co>
2024-08-23 14:07:47 +0000

    End test_user_setup_device_offline with CREATE_ENTRY

--- tests/components/lektrico/test_config_flow.py
+++ tests/components/lektrico/test_config_flow.py
@@ -112,6 +112,18 @@ async def test_user_setup_device_offline(
 
     assert result["type"] is FlowResultType.FORM
     assert result["errors"] == {CONF_HOST: "cannot_connect"}
+    assert result["step_id"] == "user"
+
+    mock_device_config.side_effect = None
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_HOST: MOCKED_DEVICE_IP_ADDRESS,
+        },
+    )
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
 
 
 async def test_discovered_zeroconf(
