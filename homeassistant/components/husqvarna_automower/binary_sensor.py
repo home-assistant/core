e8dea1089b1f8c84d187442dca0853a863793c38
Michael Arthur <michael.arthur@gameglass.gg>
2024-08-26 16:46:13 +1200

    deprecate returning to dock binary sensor

--- homeassistant/components/husqvarna_automower/binary_sensor.py
+++ homeassistant/components/husqvarna_automower/binary_sensor.py
@@ -2,23 +2,42 @@
 
 from collections.abc import Callable
 from dataclasses import dataclass
+from enum import StrEnum
 import logging
+from typing import TYPE_CHECKING
 
 from aioautomower.model import MowerActivities, MowerAttributes
 
 from homeassistant.components.binary_sensor import (
+    DOMAIN as BINARY_SENSOR_DOMAIN,
     BinarySensorDeviceClass,
     BinarySensorEntity,
     BinarySensorEntityDescription,
 )
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.issue_registry import (
+    IssueSeverity,
+    async_create_issue,
+    async_delete_issue,
+)
 
 from . import AutomowerConfigEntry
+from .const import DOMAIN
 from .coordinator import AutomowerDataUpdateCoordinator
 from .entity import AutomowerBaseEntity
+from .util import entity_used_in
 
 _LOGGER = logging.getLogger(__name__)
+
+
+class AutomowerBinarySensorEntityTypes(StrEnum):
+    """Automower Entities."""
+
+    BATTERY_CHARGING = "battery_charging"
+    LEAVING_DOCK = "leaving_dock"
+    RETURNING_TO_DOCK = "returning_to_dock"
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -30,18 +49,18 @@ class AutomowerBinarySensorEntityDescription(BinarySensorEntityDescription):
 
 BINARY_SENSOR_TYPES: tuple[AutomowerBinarySensorEntityDescription, ...] = (
     AutomowerBinarySensorEntityDescription(
-        key="battery_charging",
+        key=AutomowerBinarySensorEntityTypes.BATTERY_CHARGING,
         value_fn=lambda data: data.mower.activity == MowerActivities.CHARGING,
         device_class=BinarySensorDeviceClass.BATTERY_CHARGING,
     ),
     AutomowerBinarySensorEntityDescription(
-        key="leaving_dock",
-        translation_key="leaving_dock",
+        key=AutomowerBinarySensorEntityTypes.LEAVING_DOCK,
+        translation_key=AutomowerBinarySensorEntityTypes.LEAVING_DOCK,
         value_fn=lambda data: data.mower.activity == MowerActivities.LEAVING,
     ),
     AutomowerBinarySensorEntityDescription(
-        key="returning_to_dock",
-        translation_key="returning_to_dock",
+        key=AutomowerBinarySensorEntityTypes.RETURNING_TO_DOCK,
+        translation_key=AutomowerBinarySensorEntityTypes.RETURNING_TO_DOCK,
         value_fn=lambda data: data.mower.activity == MowerActivities.GOING_HOME,
     ),
 )
@@ -81,3 +100,37 @@ class AutomowerBinarySensorEntity(AutomowerBaseEntity, BinarySensorEntity):
     def is_on(self) -> bool:
         """Return the state of the binary sensor."""
         return self.entity_description.value_fn(self.mower_attributes)
+
+    async def async_added_to_hass(self) -> None:
+        """Raise issue when entity is registered and was not disabled."""
+        if TYPE_CHECKING:
+            assert self.unique_id
+        if entity_id := er.async_get(self.hass).async_get_entity_id(
+            BINARY_SENSOR_DOMAIN, DOMAIN, self.unique_id
+        ):
+            if (
+                self.enabled
+                and self.entity_description.key
+                in AutomowerBinarySensorEntityTypes.RETURNING_TO_DOCK
+                and entity_used_in(self.hass, entity_id)
+            ):
+                async_create_issue(
+                    self.hass,
+                    DOMAIN,
+                    f"deprecated_task_entity_{self.entity_description.key}",
+                    breaks_in_ha_version="2025.2.0",
+                    is_fixable=False,
+                    severity=IssueSeverity.WARNING,
+                    translation_key="deprecated_task_entity",
+                    translation_placeholders={
+                        "task_name": str(self.name),
+                        "entity": entity_id,
+                    },
+                )
+            else:
+                async_delete_issue(
+                    self.hass,
+                    DOMAIN,
+                    f"deprecated_task_entity_{self.entity_description.key}",
+                )
+        await super().async_added_to_hass()
