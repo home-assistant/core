3b03957ff857c89b42f1bef6a773802a8f484bf8
Alexey ALERT Rubasheff <alexey.rubasheff@gmail.com>
2024-08-26 10:32:41 +0300

    MartinHjelmare indentation request

--- homeassistant/components/overkiz/binary_sensor.py
+++ homeassistant/components/overkiz/binary_sensor.py
@@ -115,14 +115,16 @@ BINARY_SENSOR_DESCRIPTIONS: list[OverkizBinarySensorDescription] = [
     OverkizBinarySensorDescription(
         key=OverkizState.MODBUSLINK_DHW_ABSENCE_MODE,
         name="Absence mode",
-        value_fn=lambda state: state
-        in (OverkizCommandParam.ON, OverkizCommandParam.PROG),
+        value_fn=(
+            lambda state: state in (OverkizCommandParam.ON, OverkizCommandParam.PROG)
+        ),
     ),
     OverkizBinarySensorDescription(
         key=OverkizState.MODBUSLINK_DHW_BOOST_MODE,
         name="Boost mode",
-        value_fn=lambda state: state
-        in (OverkizCommandParam.ON, OverkizCommandParam.PROG),
+        value_fn=(
+            lambda state: state in (OverkizCommandParam.ON, OverkizCommandParam.PROG)
+        ),
     ),
 ]
 
