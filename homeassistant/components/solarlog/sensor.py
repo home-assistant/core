178598ea3ed68171e83843dd8ff68afffed45847
dontinelli <73341522+dontinelli@users.noreply.github.com>
2024-08-26 12:20:34 +0000

    Split up sensor types

--- homeassistant/components/solarlog/sensor.py
+++ homeassistant/components/solarlog/sensor.py
@@ -22,7 +22,6 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import SolarlogConfigEntry
-from .coordinator import SolarLogCoordinator
 from .entity import SolarLogCoordinatorEntity, SolarLogInverterEntity
 
 
@@ -35,23 +34,13 @@ class SolarLogSensorEntityDescription(SensorEntityDescription):
     )
 
 
-SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
+SOLARLOG_SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="last_updated",
         translation_key="last_update",
         device_class=SensorDeviceClass.TIMESTAMP,
     ),
     SolarLogSensorEntityDescription(
-        # Inverter entity
-        key="current_power",
-        translation_key="current_power",
-        native_unit_of_measurement=UnitOfPower.WATT,
-        device_class=SensorDeviceClass.POWER,
-        state_class=SensorStateClass.MEASUREMENT,
-    ),
-    SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="power_ac",
         translation_key="power_ac",
         native_unit_of_measurement=UnitOfPower.WATT,
@@ -59,7 +48,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="power_dc",
         translation_key="power_dc",
         native_unit_of_measurement=UnitOfPower.WATT,
@@ -67,7 +55,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="voltage_ac",
         translation_key="voltage_ac",
         native_unit_of_measurement=UnitOfElectricPotential.VOLT,
@@ -75,7 +62,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="voltage_dc",
         translation_key="voltage_dc",
         native_unit_of_measurement=UnitOfElectricPotential.VOLT,
@@ -83,7 +69,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="yield_day",
         translation_key="yield_day",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -91,7 +76,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="yield_yesterday",
         translation_key="yield_yesterday",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -99,7 +83,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="yield_month",
         translation_key="yield_month",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -107,7 +90,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="yield_year",
         translation_key="yield_year",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -115,7 +97,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="yield_total",
         translation_key="yield_total",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -124,7 +105,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="consumption_ac",
         translation_key="consumption_ac",
         native_unit_of_measurement=UnitOfPower.WATT,
@@ -132,7 +112,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="consumption_day",
         translation_key="consumption_day",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -140,7 +119,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="consumption_yesterday",
         translation_key="consumption_yesterday",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -148,7 +126,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="consumption_month",
         translation_key="consumption_month",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -156,7 +133,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="consumption_year",
         translation_key="consumption_year",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -164,7 +140,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity, Inverter entity
         key="consumption_total",
         translation_key="consumption_total",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -173,7 +148,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value / 1000, 3),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="self_consumption_year",
         translation_key="self_consumption_year",
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
@@ -181,14 +155,12 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         state_class=SensorStateClass.TOTAL_INCREASING,
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="total_power",
         translation_key="total_power",
         native_unit_of_measurement=UnitOfPower.WATT,
         device_class=SensorDeviceClass.POWER,
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="alternator_loss",
         translation_key="alternator_loss",
         native_unit_of_measurement=UnitOfPower.WATT,
@@ -196,7 +168,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="capacity",
         translation_key="capacity",
         native_unit_of_measurement=PERCENTAGE,
@@ -205,7 +176,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value * 100, 1),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="efficiency",
         translation_key="efficiency",
         native_unit_of_measurement=PERCENTAGE,
@@ -214,7 +184,6 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         value_fn=lambda value: round(value * 100, 1),
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="power_available",
         translation_key="power_available",
         native_unit_of_measurement=UnitOfPower.WATT,
@@ -222,13 +191,29 @@ SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SolarLogSensorEntityDescription(
-        # Coordinator entity
         key="usage",
         translation_key="usage",
         native_unit_of_measurement=PERCENTAGE,
         device_class=SensorDeviceClass.POWER_FACTOR,
         state_class=SensorStateClass.MEASUREMENT,
         value_fn=lambda value: round(value * 100, 1),
+    ),
+)
+
+INVERTER_SENSOR_TYPES: tuple[SolarLogSensorEntityDescription, ...] = (
+    SolarLogSensorEntityDescription(
+        key="current_power",
+        translation_key="current_power",
+        native_unit_of_measurement=UnitOfPower.WATT,
+        device_class=SensorDeviceClass.POWER,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    SolarLogSensorEntityDescription(
+        key="consumption_year",
+        translation_key="consumption_year",
+        native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
+        device_class=SensorDeviceClass.ENERGY,
+        value_fn=lambda value: round(value / 1000, 3),
     ),
 )
 
@@ -239,14 +224,13 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Add solarlog entry."""
-    coordinator: SolarLogCoordinator = entry.runtime_data
+    coordinator = entry.runtime_data
 
     # https://github.com/python/mypy/issues/14294
 
     entities: list[SensorEntity] = [
         SolarLogCoordinatorSensor(coordinator, sensor)  # noqa: PGH003
-        for sensor in SENSOR_TYPES
-        if sensor.key in coordinator.data
+        for sensor in SOLARLOG_SENSOR_TYPES
     ]
 
     device_data = coordinator.data.get("devices", {})
@@ -257,7 +241,7 @@ async def async_setup_entry(
             if coordinator.solarlog.device_enabled(device_id):
                 entities.extend(
                     SolarLogInverterSensor(coordinator, sensor, device_id)  # noqa: PGH003
-                    for sensor in SENSOR_TYPES
+                    for sensor in INVERTER_SENSOR_TYPES
                     if sensor.key in device_data[device_id]
                 )
 
