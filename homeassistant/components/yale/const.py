fab177ab4ce2a7038ac6a256500b3f242dc66032
J. Nick Koston <nick@koston.org>
2024-08-28 05:46:03 -1000

    Split august and yale integrations (#124677)
    * Split august and yale integrations [part 1] (#122253)
    
    * merge with dev
    
    * Remove unused constant
    
    * Remove yale IPv6 workaround (#123409)
    
    * Convert yale to use oauth (#123806)
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>
    
    * Update yale for switch from pubnub to websockets (#124675)
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

---
+++ homeassistant/components/yale/const.py
@@ -0,0 +1,47 @@
+"""Constants for Yale devices."""
+
+from yalexs.const import Brand
+
+from homeassistant.const import Platform
+
+DEFAULT_TIMEOUT = 25
+
+CONF_ACCESS_TOKEN_CACHE_FILE = "access_token_cache_file"
+CONF_BRAND = "brand"
+CONF_LOGIN_METHOD = "login_method"
+CONF_INSTALL_ID = "install_id"
+
+VERIFICATION_CODE_KEY = "verification_code"
+
+DEFAULT_BRAND = Brand.YALE_HOME
+
+MANUFACTURER = "Yale Home Inc."
+
+DEFAULT_NAME = "Yale"
+DOMAIN = "yale"
+
+OPERATION_METHOD_AUTORELOCK = "autorelock"
+OPERATION_METHOD_REMOTE = "remote"
+OPERATION_METHOD_KEYPAD = "keypad"
+OPERATION_METHOD_MANUAL = "manual"
+OPERATION_METHOD_TAG = "tag"
+OPERATION_METHOD_MOBILE_DEVICE = "mobile"
+
+ATTR_OPERATION_AUTORELOCK = "autorelock"
+ATTR_OPERATION_METHOD = "method"
+ATTR_OPERATION_REMOTE = "remote"
+ATTR_OPERATION_KEYPAD = "keypad"
+ATTR_OPERATION_MANUAL = "manual"
+ATTR_OPERATION_TAG = "tag"
+
+LOGIN_METHODS = ["phone", "email"]
+DEFAULT_LOGIN_METHOD = "email"
+
+PLATFORMS = [
+    Platform.BINARY_SENSOR,
+    Platform.BUTTON,
+    Platform.CAMERA,
+    Platform.EVENT,
+    Platform.LOCK,
+    Platform.SENSOR,
+]
