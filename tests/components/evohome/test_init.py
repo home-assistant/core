1c3062b3bbbc8fdde2fd176a2e8cb2c1cba95b6c
David Bonnes <zxdavb@bonnes.me>
2024-08-28 21:40:57 +0100

    Add evohome test for setup  (#123129)
    * allow for different systems
    
    * installation is a load_json_*fixture param
    
    * allow installation to be parameterized
    
    * test setup of various systems
    
    * add more fixtures
    
    * test setup of integration
    
    * tweak test
    
    * tweak const
    
    * add expected state/services
    
    * extend setup test
    
    * tidy up
    
    * tidy up tweaks
    
    * code tweaks
    
    * refactor expected results dicts
    
    * woops
    
    * refatcor serialize
    
    * refactor test
    
    * tweak
    
    * tweak code
    
    * rename symbol
    
    * ensure actual I/O remains blocked
    
    * tweak
    
    * typo
    
    * use constants
    
    * Update conftest.py
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * change filename
    
    * add config fixture
    
    * config is a fixture
    
    * config is a fixture now 2
    
    * lint
    
    * lint
    
    * refactor
    
    * lint
    
    * lint
    
    * restore email addr
    
    * use const
    
    * use snapshots instead of helper class
    
    * doctweak
    
    * correct snapshot
    
    ---------
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

---
+++ tests/components/evohome/test_init.py
@@ -0,0 +1,25 @@
+"""The tests for evohome."""
+
+from __future__ import annotations
+
+import pytest
+from syrupy import SnapshotAssertion
+
+from homeassistant.core import HomeAssistant
+
+from .conftest import setup_evohome
+from .const import TEST_INSTALLS
+
+
+@pytest.mark.parametrize("install", TEST_INSTALLS)
+async def test_entities(
+    hass: HomeAssistant,
+    evo_config: dict[str, str],
+    install: str,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test entities and state after setup of a Honeywell TCC-compatible system."""
+
+    await setup_evohome(hass, evo_config, install=install)
+
+    assert hass.states.async_all() == snapshot
