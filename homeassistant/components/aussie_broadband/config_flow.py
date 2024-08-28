4559d687bbdf3900a4c299966a4f9ebb7bbd2d53
epenet <6771947+epenet@users.noreply.github.com>
2024-08-28 13:14:26 +0200

    Apply suggestions from code review
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/aussie_broadband/config_flow.py
+++ homeassistant/components/aussie_broadband/config_flow.py
@@ -103,12 +103,7 @@ class AussieBroadbandConfigFlow(ConfigFlow, domain=DOMAIN):
                     self.context["entry_id"]
                 )
                 assert entry
-                self.hass.config_entries.async_update_entry(
-                    entry,
-                    data=data,
-                )
-                await self.hass.config_entries.async_reload(entry.entry_id)
-                return self.async_abort(reason="reauth_successful")
+                self.self.async_update_reload_and_abort(entry, data=data)
 
         return self.async_show_form(
             step_id="reauth_confirm",
