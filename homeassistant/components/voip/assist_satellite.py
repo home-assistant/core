4e27d8ec78d251df8955ce95d9ce9c2df517d651
Paulus Schoutsen <balloob@gmail.com>
2024-08-30 16:51:21 +0200

    Minor refactor assist satellite (#124912)
    * Extract entity ID for pipeline to property
    
    * Add super calls
    
    * Mock tts

--- homeassistant/components/voip/assist_satellite.py
+++ homeassistant/components/voip/assist_satellite.py
@@ -112,12 +112,24 @@ class VoipAssistSatellite(VoIPEntity, AssistSatelliteEntity, RtpDatagramProtocol
         self._tones = tones
         self._processing_tone_done = asyncio.Event()
 
+    @property
+    def pipeline_entity_id(self) -> str | None:
+        """Return the entity ID of the pipeline to use for the next conversation."""
+        return self.voip_device.get_pipeline_entity_id(self.hass)
+
+    @property
+    def vad_sensitivity_entity_id(self) -> str | None:
+        """Return the entity ID of the VAD sensitivity to use for the next conversation."""
+        return self.voip_device.get_vad_sensitivity_entity_id(self.hass)
+
     async def async_added_to_hass(self) -> None:
         """Run when entity about to be added to hass."""
+        await super().async_added_to_hass()
         self.voip_device.protocol = self
 
     async def async_will_remove_from_hass(self) -> None:
         """Run when entity will be removed from hass."""
+        await super().async_will_remove_from_hass()
         assert self.voip_device.protocol == self
         self.voip_device.protocol = None
 
@@ -155,15 +167,9 @@ class VoipAssistSatellite(VoIPEntity, AssistSatelliteEntity, RtpDatagramProtocol
             # Run pipeline with a timeout
             _LOGGER.debug("Starting pipeline")
             async with asyncio.timeout(_PIPELINE_TIMEOUT_SEC):
-                await self._async_accept_pipeline_from_satellite(  # noqa: SLF001
+                await self.async_accept_pipeline_from_satellite(
                     audio_stream=queue_to_iterable(
                         self._audio_queue, timeout=self._audio_chunk_timeout
-                    ),
-                    pipeline_entity_id=self.voip_device.get_pipeline_entity_id(
-                        self.hass
-                    ),
-                    vad_sensitivity_entity_id=self.voip_device.get_vad_sensitivity_entity_id(
-                        self.hass
                     ),
                 )
 
