154fc6b4ec4427c8ca527c59122b87d9aa934865
Allen Porter <allen@thebends.org>
2024-08-30 08:31:24 -0700

    Add Google Photos reauth support (#124933)
    * Add Google Photos reauth support
    
    * Update tests/components/google_photos/test_config_flow.py
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- tests/components/google_photos/conftest.py
+++ tests/components/google_photos/conftest.py
@@ -18,16 +18,18 @@ from homeassistant.setup import async_setup_component
 from tests.common import MockConfigEntry, load_json_array_fixture
 
 USER_IDENTIFIER = "user-identifier-1"
+CONFIG_ENTRY_ID = "user-identifier-1"
 CLIENT_ID = "1234"
 CLIENT_SECRET = "5678"
 FAKE_ACCESS_TOKEN = "some-access-token"
 FAKE_REFRESH_TOKEN = "some-refresh-token"
+EXPIRES_IN = 3600
 
 
 @pytest.fixture(name="expires_at")
 def mock_expires_at() -> int:
     """Fixture to set the oauth token expiration time."""
-    return time.time() + 3600
+    return time.time() + EXPIRES_IN
 
 
 @pytest.fixture(name="token_entry")
@@ -37,17 +39,26 @@ def mock_token_entry(expires_at: int) -> dict[str, Any]:
         "access_token": FAKE_ACCESS_TOKEN,
         "refresh_token": FAKE_REFRESH_TOKEN,
         "scope": " ".join(OAUTH2_SCOPES),
-        "token_type": "Bearer",
+        "type": "Bearer",
         "expires_at": expires_at,
+        "expires_in": EXPIRES_IN,
     }
 
 
+@pytest.fixture(name="config_entry_id")
+def mock_config_entry_id() -> str | None:
+    """Provide a json fixture file to load for list media item api responses."""
+    return CONFIG_ENTRY_ID
+
+
 @pytest.fixture(name="config_entry")
-def mock_config_entry(token_entry: dict[str, Any]) -> MockConfigEntry:
+def mock_config_entry(
+    config_entry_id: str, token_entry: dict[str, Any]
+) -> MockConfigEntry:
     """Fixture for a config entry."""
     return MockConfigEntry(
         domain=DOMAIN,
-        unique_id="config-entry-id-123",
+        unique_id=config_entry_id,
         data={
             "auth_implementation": DOMAIN,
             "token": token_entry,
@@ -73,12 +84,20 @@ def mock_fixture_name() -> str | None:
     return None
 
 
+@pytest.fixture(name="user_identifier")
+def mock_user_identifier() -> str | None:
+    """Provide a json fixture file to load for list media item api responses."""
+    return USER_IDENTIFIER
+
+
 @pytest.fixture(name="setup_api")
-def mock_setup_api(fixture_name: str) -> Generator[Mock, None, None]:
+def mock_setup_api(
+    fixture_name: str, user_identifier: str
+) -> Generator[Mock, None, None]:
     """Set up fake Google Photos API responses from fixtures."""
     with patch("homeassistant.components.google_photos.api.build") as mock:
         mock.return_value.userinfo.return_value.get.return_value.execute.return_value = {
-            "id": USER_IDENTIFIER,
+            "id": user_identifier,
             "name": "Test Name",
         }
 
