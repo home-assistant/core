4bfaf85d6eea65aea8048c1da16d0a26c9fc6b5d
civita <14911217+civita@users.noreply.github.com>
2024-08-30 16:53:28 +0000

    fix ruff error

--- homeassistant/components/tailscale/config_flow.py
+++ homeassistant/components/tailscale/config_flow.py
@@ -13,7 +13,8 @@ from homeassistant.const import CONF_API_KEY
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import CONF_TAILNET, DOMAIN, AUTHKEYS_URL
+from .const import AUTHKEYS_URL, CONF_TAILNET, DOMAIN
+
 
 async def validate_input(hass: HomeAssistant, *, tailnet: str, api_key: str) -> None:
     """Try using the give tailnet & api key against the Tailscale API."""
@@ -65,9 +66,7 @@ class TailscaleFlowHandler(ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            description_placeholders={
-                "authkeys_url": AUTHKEYS_URL
-            },
+            description_placeholders={"authkeys_url": AUTHKEYS_URL},
             data_schema=vol.Schema(
                 {
                     vol.Required(
@@ -122,9 +121,7 @@ class TailscaleFlowHandler(ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="reauth_confirm",
-            description_placeholders={
-                "authkeys_url": AUTHKEYS_URL
-            },
+            description_placeholders={"authkeys_url": AUTHKEYS_URL},
             data_schema=vol.Schema({vol.Required(CONF_API_KEY): str}),
             errors=errors,
         )
