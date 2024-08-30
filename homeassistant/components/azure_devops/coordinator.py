ghindsy/HAmatter

Add work item data

homeassistant/components/azure_devops/coordinator.py
@@ -6,8 +6,14 @@ import logging
 from typing import Final
 
 from aioazuredevops.client import DevOpsClient
+from aioazuredevops.helper import (
+    WorkItemTypeAndState,
+    work_item_types_states_filter,
+    work_items_by_type_and_state,
+)
 from aioazuredevops.models.builds import Build
 from aioazuredevops.models.core import Project
+from aioazuredevops.models.work_item_types import Category
 import aiohttp
 
 from homeassistant.config_entries import ConfigEntry
@@ -20,6 +26,7 @@ from .const import CONF_ORG, DOMAIN
 from .data import AzureDevOpsData
 
 BUILDS_QUERY: Final = "?queryOrder=queueTimeDescending&maxBuildsPerDefinition=1"
+IGNORED_CATEGORIES: Final[list[Category]] = [Category.COMPLETED, Category.REMOVED]
 
 
 def ado_exception_none_handler(func: Callable) -> Callable:
@@ -104,13 +111,60 @@ class AzureDevOpsDataUpdateCoordinator(DataUpdateCoordinator[AzureDevOpsData]):
             BUILDS_QUERY,
         )
 
+    @ado_exception_none_handler
+    async def _get_work_items(
+        self, project_name: str
+    ) -> list[WorkItemTypeAndState] | None:
+        """Get the work items."""
+
+        if (
+            work_item_types := await self.client.get_work_item_types(
+                self.organization,
+                project_name,
+            )
+        ) is None:
+            # If no work item types are returned, return an empty list
+            return []
+
+        if (
+            work_item_ids := await self.client.get_work_item_ids(
+                self.organization,
+                project_name,
+                # Filter out completed and removed work items so we only get active work items
+                states=work_item_types_states_filter(
+                    work_item_types,
+                    ignored_categories=IGNORED_CATEGORIES,
+                ),
+            )
+        ) is None:
+            # If no work item ids are returned, return an empty list
+            return []
+
+        if (
+            work_items := await self.client.get_work_items(
+                self.organization,
+                project_name,
+                work_item_ids,
+            )
+        ) is None:
+            # If no work items are returned, return an empty list
+            return []
+
+        return work_items_by_type_and_state(
+            work_item_types,
+            work_items,
+            ignored_categories=IGNORED_CATEGORIES,
+        )
+
     async def _async_update_data(self) -> AzureDevOpsData:
         """Fetch data from Azure DevOps."""
         # Get the builds from the project
         builds = await self._get_builds(self.project.name)
+        work_items = await self._get_work_items(self.project.name)
 
         return AzureDevOpsData(
             organization=self.organization,
             project=self.project,
             builds=builds,
+            work_items=work_items,
         )

home-assistant/core

homeassistant/components/azure_devops/coordinator.py
@@ -6,8 +6,14 @@ import logging
 from typing import Final
 
 from aioazuredevops.client import DevOpsClient
+from aioazuredevops.helper import (
+    WorkItemTypeAndState,
+    work_item_types_states_filter,
+    work_items_by_type_and_state,
+)
 from aioazuredevops.models.build import Build
 from aioazuredevops.models.core import Project
+from aioazuredevops.models.work_item_type import Category
 import aiohttp
 
 from homeassistant.config_entries import ConfigEntry
@@ -20,6 +26,7 @@ from .const import CONF_ORG, DOMAIN
 from .data import AzureDevOpsData
 
 BUILDS_QUERY: Final = "?queryOrder=queueTimeDescending&maxBuildsPerDefinition=1"
+IGNORED_CATEGORIES: Final[list[Category]] = [Category.COMPLETED, Category.REMOVED]
 
 
 def ado_exception_none_handler(func: Callable) -> Callable:
@@ -105,13 +112,60 @@ class AzureDevOpsDataUpdateCoordinator(DataUpdateCoordinator[AzureDevOpsData]):
             BUILDS_QUERY,
         )
 
+    @ado_exception_none_handler
+    async def _get_work_items(
+        self, project_name: str
+    ) -> list[WorkItemTypeAndState] | None:
+        """Get the work items."""
+
+        if (
+            work_item_types := await self.client.get_work_item_types(
+                self.organization,
+                project_name,
+            )
+        ) is None:
+            # If no work item types are returned, return an empty list
+            return []
+
+        if (
+            work_item_ids := await self.client.get_work_item_ids(
+                self.organization,
+                project_name,
+                # Filter out completed and removed work items so we only get active work items
+                states=work_item_types_states_filter(
+                    work_item_types,
+                    ignored_categories=IGNORED_CATEGORIES,
+                ),
+            )
+        ) is None:
+            # If no work item ids are returned, return an empty list
+            return []
+
+        if (
+            work_items := await self.client.get_work_items(
+                self.organization,
+                project_name,
+                work_item_ids,
+            )
+        ) is None:
+            # If no work items are returned, return an empty list
+            return []
+
+        return work_items_by_type_and_state(
+            work_item_types,
+            work_items,
+            ignored_categories=IGNORED_CATEGORIES,
+        )
+
     async def _async_update_data(self) -> AzureDevOpsData:
         """Fetch data from Azure DevOps."""
         # Get the builds from the project
         builds = await self._get_builds(self.project.name)
+        work_items = await self._get_work_items(self.project.name)
 
         return AzureDevOpsData(
             organization=self.organization,
             project=self.project,
             builds=builds,
+            work_items=work_items,
         )