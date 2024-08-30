033bc1bbe51c68fb8c51b67341573f9ce4ffacee
Michael Hansen <mike@rhasspy.org>
2024-08-30 09:52:38 -0500

    Add Assist satellite entity + VoIP (#123830)
    * Add assist_satellite and implement VoIP
    
    * Fix tests
    
    * More tests
    
    * Improve test
    
    * Update entity state
    
    * Set state correctly
    
    * Move more functionality into base class
    
    * Move RTP protocol into entity
    
    * Fix tests
    
    * Remove string
    
    * Move to util method
    
    * Align states better with pipeline events
    
    * Remove public async_get_satellite_entity
    
    * WAITING_FOR_WAKE_WORD
    
    * Pass entity ids for pipeline/vad sensitivity
    
    * Remove connect/disconnect
    
    * Clean up
    
    * Final cleanup

--- tests/util/test_async.py
+++ tests/util/test_async.py
@@ -213,3 +213,43 @@ async def test_get_scheduled_timer_handles(hass: HomeAssistant) -> None:
     timer_handle.cancel()
     timer_handle2.cancel()
     timer_handle3.cancel()
+
+
+async def test_queue_to_iterable() -> None:
+    """Test queue_to_iterable."""
+    queue: asyncio.Queue[int | None] = asyncio.Queue()
+    expected_items = list(range(10))
+
+    for i in expected_items:
+        await queue.put(i)
+
+    # Will terminate the stream
+    await queue.put(None)
+
+    actual_items = [item async for item in hasync.queue_to_iterable(queue)]
+
+    assert expected_items == actual_items
+
+    # Check timeout
+    assert queue.empty()
+
+    # Time out on first item
+    async with asyncio.timeout(1):
+        with pytest.raises(asyncio.TimeoutError):  # noqa: PT012
+            # Should time out very quickly
+            async for _item in hasync.queue_to_iterable(queue, timeout=0.01):
+                await asyncio.sleep(1)
+
+    # Check timeout on second item
+    assert queue.empty()
+    await queue.put(12345)
+
+    # Time out on second item
+    async with asyncio.timeout(1):
+        with pytest.raises(asyncio.TimeoutError):  # noqa: PT012
+            # Should time out very quickly
+            async for item in hasync.queue_to_iterable(queue, timeout=0.01):
+                if item != 12345:
+                    await asyncio.sleep(1)
+
+    assert queue.empty()
