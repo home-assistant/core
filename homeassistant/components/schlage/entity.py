51764222993ed418f5bfe67b277b5fb77d9f883c
David Knowles <dknowles2@gmail.com>
2024-08-31 15:55:32 +0000

    Only mark unavailable if lock is not present

--- homeassistant/components/schlage/entity.py
+++ homeassistant/components/schlage/entity.py
@@ -46,8 +46,4 @@ class SchlageEntity(CoordinatorEntity[SchlageDataUpdateCoordinator]):
     def available(self) -> bool:
         """Return if entity is available."""
         # When is_locked is None the lock is unavailable.
-        return (
-            super().available
-            and self._lock is not None
-            and self._lock.is_locked is not None
-        )
+        return super().available and self.device_id in self.coordinator.data.locks
