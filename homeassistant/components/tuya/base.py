efa952ae9e1c6719561a59bb52c1c66a83038e1f
azerty9971 <doyen.gilles@gmail.com>
2024-09-01 21:23:52 +0200

    Update base.py

--- homeassistant/components/tuya/base.py
+++ homeassistant/components/tuya/base.py
@@ -126,9 +126,7 @@ class ElectricityTypeData:
         electriccurrent = 0.0
         power = 0.0
         if len(raw) < 8:
-            return cls(
-            electriccurrent=None, power=None, voltage=None
-        )
+            return cls(electriccurrent=None, power=None, voltage=None)
         voltage = struct.unpack(">H", raw[0:2])[0] / 10.0
         electriccurrent = struct.unpack(">L", b"\x00" + raw[2:5])[0] / 1000.0
         power = struct.unpack(">L", b"\x00" + raw[5:8])[0] / 1000.0
