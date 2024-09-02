28f3adac215a424ee3797a7ca6a89253f0843d08
Michael Arthur <michael.arthur@gameglass.gg>
2024-09-02 12:51:20 +1200

    update core to hacs version

--- homeassistant/components/mammotion/entity.py
+++ homeassistant/components/mammotion/entity.py
@@ -1,10 +1,10 @@
 """Base class for entities."""
 
-from pymammotion.utility.device_type import DeviceType
-
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from pymammotion.utility.device_type import DeviceType
 
+from . import DEFAULT_RETRY_COUNT
 from .const import CONF_RETRY_COUNT, DOMAIN
 from .coordinator import MammotionDataUpdateCoordinator
 
@@ -17,34 +17,45 @@ class MammotionBaseEntity(CoordinatorEntity[MammotionDataUpdateCoordinator]):
     def __init__(self, coordinator: MammotionDataUpdateCoordinator, key: str) -> None:
         """Initialize the lawn mower."""
         super().__init__(coordinator)
-        swversion = "0.0.0"
+        self._attr_unique_id = f"{coordinator.device_name}_{key}"
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        mower = self.coordinator.manager.mower(
+                    self.coordinator.device_name
+                )
+        swversion = None
         if (
             len(
-                coordinator.manager.mower(
-                    coordinator.device_name
-                ).net.toapp_devinfo_resp.resp_ids
+                mower.net.toapp_devinfo_resp.resp_ids
             )
             > 0
         ):
             swversion = (
-                coordinator.manager.mower(coordinator.device_name)
+                mower
                 .net.toapp_devinfo_resp.resp_ids[0]
                 .info
             )
 
-        self._attr_unique_id = f"{coordinator.device_name}_{key}"
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, coordinator.device_name)},
+        product_key = mower.net.toapp_wifi_iot_status.productkey
+        if product_key is None or product_key == "":
+            if self.coordinator.manager.cloud_client:
+                device_list = self.coordinator.manager.cloud_client.get_devices_by_account_response().data.data
+                device = next((device for device in device_list if device.deviceName == self.coordinator.device_name), None)
+                product_key = device.productKey
+
+        device_model = DeviceType.value_of_str(
+                self.coordinator.device_name,
+                product_key,
+            ).get_model()
+
+        return DeviceInfo(
+            identifiers={(DOMAIN, self.coordinator.device_name)},
             manufacturer="Mammotion",
-            serial_number=coordinator.device_name.split("-", 1)[-1],
-            name=coordinator.device_name,
+            serial_number=self.coordinator.device_name.split("-", 1)[-1],
+            name=self.coordinator.device_name,
             sw_version=swversion,
-            model=DeviceType.value_of_str(
-                coordinator.device_name,
-                coordinator.manager.mower(
-                    coordinator.device_name
-                ).net.toapp_wifi_iot_status.productkey,
-            ).get_model(),
+            model=device_model,
             suggested_area="Garden",
         )
 
@@ -54,5 +65,7 @@ class MammotionBaseEntity(CoordinatorEntity[MammotionDataUpdateCoordinator]):
         return (
             self.coordinator.data is not None
             and self.coordinator.update_failures
-            <= self.coordinator.config_entry.options[CONF_RETRY_COUNT]
+            <= self.coordinator.config_entry.options.get(
+                CONF_RETRY_COUNT, DEFAULT_RETRY_COUNT
+            )
         )
