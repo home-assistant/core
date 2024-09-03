432c79695d19533ed34364c95e37a69593dd9f9a
Lenn <lennperik@hotmail.nl>
2024-09-03 12:45:10 +0000

    Remove info from diagnostics

--- homeassistant/components/motionblinds_ble/diagnostics.py
+++ homeassistant/components/motionblinds_ble/diagnostics.py
@@ -36,58 +36,19 @@ async def async_get_config_entry_diagnostics(
                 "blind_type": device.blind_type.value,
                 "display_name": device.display_name,
                 "timezone": device.timezone,
-                "states": {
-                    "_end_position_info": None
-                    if not device._end_position_info  # noqa: SLF001
-                    else {
-                        "end_positions": device._end_position_info.end_positions.value,  # noqa: SLF001
-                        "favorite": device._end_position_info.favorite_position,  # noqa: SLF001
-                    },
-                    **{
-                        attr: getattr(device, attr)
-                        for attr in (
-                            "_position",
-                            "_tilt",
-                            "_calibration_type",
-                        )
-                    },
+                "_connection_type": device._connection_type.value,  # noqa: SLF001
+                "_end_position_info": None
+                if not device._end_position_info  # noqa: SLF001
+                else {
+                    "end_positions": device._end_position_info.end_positions.value,  # noqa: SLF001
+                    "favorite": device._end_position_info.favorite_position,  # noqa: SLF001
                 },
-                "connection": {
-                    "has_ble_device": device.ble_device is not None,  # noqa: SLF001
-                    "has_bleak_client": device._current_bleak_client is not None,  # noqa: SLF001
-                    "has_disconnect_timer": device._disconnect_timer is not None,  # noqa: SLF001
-                    "is_set_received_end_position_info_event": device._received_end_position_info_event.is_set(),  # noqa: SLF001
-                    "_connection_type": device._connection_type.value,  # noqa: SLF001
-                    **{
-                        attr: getattr(device, attr)
-                        for attr in (
-                            "rssi",
-                            "_permanent_connection",
-                            "_connect_status_query_time",
-                            "_custom_setting_disconnect_time",
-                            "_disconnect_time",
-                        )
-                    },
-                },
-                "callbacks": {
-                    callback: [
-                        f"{method.__self__.__class__.__name__}.{method.__name__}"
-                        if hasattr(method, "__self__")
-                        else method.__name__
-                        for method in getattr(device, callback)
-                    ]
-                    for callback in (
-                        "_battery_callbacks",
-                        "_calibration_callbacks",
-                        "_connection_callbacks",
-                        "_disabled_connection_callbacks",
-                        "_end_position_callbacks",
-                        "_feedback_callbacks",
-                        "_position_callbacks",
-                        "_running_callbacks",
-                        "_signal_strength_callbacks",
-                        "_speed_callbacks",
-                        "_status_callbacks",
+                **{
+                    attr: getattr(device, attr)
+                    for attr in (
+                        "_position",
+                        "_tilt",
+                        "_calibration_type",
                     )
                 },
             },
