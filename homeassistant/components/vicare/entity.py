260f586134eb6c178e42e3b5b781fc6940846cda
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2024-09-03 13:01:56 +0200

    Update entity.py

--- homeassistant/components/vicare/entity.py
+++ homeassistant/components/vicare/entity.py
@@ -33,15 +33,6 @@ class ViCareEntity(Entity):
             component if component else device
         )
         self._attr_unique_id = f"{identifier}-{unique_id_suffix}"
-    ) -> None:
-        """Initialize the entity."""
-        gatewaySerial = device_config.getConfig().serial
-        deviceSerial = device.getSerial()
-        identifier = f"{gatewaySerial}_{deviceSerial}"
-        self._api: PyViCareDevice | PyViCareHeatingDeviceComponent = (
-            component if component else device
-        )
-        self._attr_unique_id = f"{identifier}-{unique_id_suffix}"
         if component:
             self._attr_unique_id += f"-{component.id}"
 
