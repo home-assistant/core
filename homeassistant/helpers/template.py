ed52b95acd12a2b969f2e389146e3537a635414a
Erik <erik@montnemery.com>
2024-09-03 13:56:17 +0200

    Log deprecation warning when template.Template is created without hass

--- homeassistant/helpers/template.py
+++ homeassistant/helpers/template.py
@@ -508,8 +508,20 @@ class Template:
 
     def __init__(self, template: str, hass: HomeAssistant | None = None) -> None:
         """Instantiate a template."""
+        # pylint: disable-next=import-outside-toplevel
+        from .frame import report
+
         if not isinstance(template, str):
             raise TypeError("Expected template to be a string")
+
+        if not hass:
+            report(
+                (
+                    "creates a template object without passing hass, "
+                    "which will stop working in HA Core 2025.10"
+                ),
+                error_if_core=False,
+            )
 
         self.template: str = template.strip()
         self._compiled_code: CodeType | None = None
