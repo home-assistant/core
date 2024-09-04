a0195fb4dbe5402f154396103cc4e2e32d3dc851
Iskra kranj <162285659+iskrakranj@users.noreply.github.com>
2024-09-04 15:33:23 +0200

    Add Iskra integration (#121488)
    * Add iskra integration
    
    * iskra non resettable counters naming fix
    
    * added iskra config_flow test
    
    * fixed iskra integration according to code review
    
    * changed iskra config flow test
    
    * iskra integration, fixed codeowners
    
    * Removed counters code & minor fixes
    
    * added comment
    
    * Update homeassistant/components/iskra/__init__.py
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>
    
    * Updated Iskra integration according to review
    
    * Update homeassistant/components/iskra/strings.json
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>
    
    * Updated iskra integration according to review
    
    * minor iskra integration change
    
    * iskra integration changes according to review
    
    * iskra integration changes according to review
    
    * Changed iskra integration according to review
    
    * added iskra config_flow range validation
    
    * Fixed tests for iskra integration
    
    * Update homeassistant/components/iskra/coordinator.py
    
    * Update homeassistant/components/iskra/config_flow.py
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>
    
    * Fixed iskra integration according to review
    
    * Changed voluptuous schema for iskra integration and added data_descriptions
    
    * Iskra integration tests lint error fix
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

---
+++ tests/components/iskra/const.py
@@ -0,0 +1,10 @@
+"""Constants used in the Iskra component tests."""
+
+SG_MODEL = "SG-W1"
+PQ_MODEL = "MC784"
+SERIAL = "XXXXXXX"
+HOST = "192.1.0.1"
+MODBUS_PORT = 10001
+MODBUS_ADDRESS = 33
+USERNAME = "test_username"
+PASSWORD = "test_password"
