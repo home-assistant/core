412fc9c629eed4bcac339cbd0e550bee51417020
Erik Montnemery <erik@montnemery.com>
2024-09-04 09:52:41 +0200

    Teach recorder data migrator base class to remove index (#125168)
    * Teach recorder data migrator base class to remove index
    
    * Fix tests

--- tests/components/recorder/test_v32_migration.py
+++ tests/components/recorder/test_v32_migration.py
@@ -219,6 +219,7 @@ async def test_migrate_times(
         await hass.async_stop()
 
 
+@pytest.mark.parametrize("enable_migrate_entity_ids", [True])
 @pytest.mark.parametrize("persistent_database", [True])
 @pytest.mark.usefixtures("hass_storage")  # Prevent test hass from writing to storage
 async def test_migrate_can_resume_entity_id_post_migration(
@@ -321,6 +322,7 @@ async def test_migrate_can_resume_entity_id_post_migration(
         await hass.async_stop()
 
 
+@pytest.mark.parametrize("enable_migrate_entity_ids", [True])
 @pytest.mark.parametrize("enable_migrate_event_ids", [True])
 @pytest.mark.parametrize("persistent_database", [True])
 @pytest.mark.usefixtures("hass_storage")  # Prevent test hass from writing to storage
@@ -625,6 +627,7 @@ async def test_out_of_disk_space_while_rebuild_states_table(
 
 @pytest.mark.usefixtures("skip_by_db_engine")
 @pytest.mark.skip_on_db_engine(["sqlite"])
+@pytest.mark.parametrize("enable_migrate_entity_ids", [True])
 @pytest.mark.parametrize("enable_migrate_event_ids", [True])
 @pytest.mark.parametrize("persistent_database", [True])
 @pytest.mark.usefixtures("hass_storage")  # Prevent test hass from writing to storage
