5cc05cc91cf955bf5e18e0ec5ac2383426d64fdb
Andre Lengwenus <alengwenus@gmail.com>
2024-09-05 15:24:31 +0000

    Add snapshots for test_scene

--- tests/components/lcn/test_scene.py
+++ tests/components/lcn/test_scene.py
@@ -3,14 +3,10 @@
 from unittest.mock import patch
 
 from pypck.lcn_defs import OutputPort, RelayPort
+from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.scene import DOMAIN as DOMAIN_SCENE
-from homeassistant.const import (
-    ATTR_ENTITY_ID,
-    SERVICE_TURN_ON,
-    STATE_UNAVAILABLE,
-    STATE_UNKNOWN,
-)
+from homeassistant.const import ATTR_ENTITY_ID, SERVICE_TURN_ON
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
@@ -18,7 +14,9 @@ from .conftest import MockConfigEntry, MockModuleConnection, MockPchkConnectionM
 
 
 async def test_setup_lcn_scene(
-    hass: HomeAssistant, lcn_connection: MockPchkConnectionManager
+    hass: HomeAssistant,
+    lcn_connection: MockPchkConnectionManager,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test the setup of switch."""
     for entity_id in (
@@ -27,7 +25,7 @@ async def test_setup_lcn_scene(
     ):
         state = hass.states.get(entity_id)
         assert state is not None
-        assert state.state == STATE_UNKNOWN
+        assert state.state == snapshot(name=f"{state.entity_id}-state")
 
 
 async def test_entity_attributes(
@@ -35,46 +33,55 @@ async def test_entity_attributes(
     entity_registry: er.EntityRegistry,
     entry: MockConfigEntry,
     lcn_connection: MockPchkConnectionManager,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test the attributes of an entity."""
     entity = entity_registry.async_get("scene.romantic")
 
     assert entity
     assert entity.unique_id == f"{entry.entry_id}-m000007-0.0"
-    assert entity.original_name == "Romantic"
+    assert entity.original_name == snapshot(name=f"{entity.entity_id}-original_name")
 
     entity_transition = entity_registry.async_get("scene.romantic_transition")
 
     assert entity_transition
     assert entity_transition.unique_id == f"{entry.entry_id}-m000007-0.1"
-    assert entity_transition.original_name == "Romantic Transition"
+    assert entity_transition.original_name == snapshot(
+        name=f"{entity_transition.entity_id}-original_name"
+    )
 
 
-@patch.object(MockModuleConnection, "activate_scene")
 async def test_scene_activate(
-    activate_scene, hass: HomeAssistant, lcn_connection: MockPchkConnectionManager
+    hass: HomeAssistant,
+    lcn_connection: MockPchkConnectionManager,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test the scene is activated."""
-    await hass.services.async_call(
-        DOMAIN_SCENE,
-        SERVICE_TURN_ON,
-        {ATTR_ENTITY_ID: "scene.romantic"},
-        blocking=True,
-    )
+    with patch.object(MockModuleConnection, "activate_scene") as activate_scene:
+        await hass.services.async_call(
+            DOMAIN_SCENE,
+            SERVICE_TURN_ON,
+            {ATTR_ENTITY_ID: "scene.romantic"},
+            blocking=True,
+        )
 
-    state = hass.states.get("scene.romantic")
-    assert state is not None
-    assert state.attributes["friendly_name"] == "Romantic"
-    activate_scene.assert_awaited_with(
-        0, 0, [OutputPort.OUTPUT1, OutputPort.OUTPUT2], [RelayPort.RELAY1], None
-    )
+        state = hass.states.get("scene.romantic")
+        assert state is not None
+        assert state.attributes["friendly_name"] == snapshot(
+            name=f"{state.entity_id}-friendly-name"
+        )
+        activate_scene.assert_awaited_with(
+            0, 0, [OutputPort.OUTPUT1, OutputPort.OUTPUT2], [RelayPort.RELAY1], None
+        )
 
 
 async def test_unload_config_entry(
     hass: HomeAssistant,
     entry: MockConfigEntry,
     lcn_connection: MockPchkConnectionManager,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test the scene is removed when the config entry is unloaded."""
     await hass.config_entries.async_forward_entry_unload(entry, DOMAIN_SCENE)
-    assert hass.states.get("scene.romantic").state == STATE_UNAVAILABLE
+    state = hass.states.get("scene.romantic")
+    assert state.state == snapshot(name=f"{state.entity_id}-state")
