f3640176d4a5c8e459ff0cd497c0d3f29081a40f
Adam Pasztor <mrpasztoradam@gmail.com>
2024-09-05 22:23:44 +0200

    fix: added unit of measurement to ADS sensor

--- homeassistant/components/ads/sensor.py
+++ homeassistant/components/ads/sensor.py
@@ -11,7 +11,7 @@ from homeassistant.components.sensor import (
     STATE_CLASSES_SCHEMA as SENSOR_STATE_CLASSES_SCHEMA,
     SensorEntity,
 )
-from homeassistant.const import CONF_DEVICE_CLASS, CONF_NAME
+from homeassistant.const import CONF_DEVICE_CLASS, CONF_NAME, CONF_UNIT_OF_MEASUREMENT
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -52,6 +52,7 @@ PLATFORM_SCHEMA = SENSOR_PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Optional(CONF_DEVICE_CLASS): SENSOR_DEVICE_CLASSES_SCHEMA,
         vol.Optional(CONF_STATE_CLASS): SENSOR_STATE_CLASSES_SCHEMA,
+        vol.Optional(CONF_UNIT_OF_MEASUREMENT): cv.string,
     }
 )
 
@@ -72,6 +73,7 @@ def setup_platform(
     factor = config.get(CONF_ADS_FACTOR)
     device_class = config.get(CONF_DEVICE_CLASS)
     state_class = config.get(CONF_STATE_CLASS)
+    unit_of_measurement = config.get(CONF_UNIT_OF_MEASUREMENT)
 
     entity = AdsSensor(
         ads_hub,
@@ -81,6 +83,7 @@ def setup_platform(
         factor,
         device_class,
         state_class,
+        unit_of_measurement,
     )
 
     add_entities([entity])
@@ -98,6 +101,7 @@ class AdsSensor(AdsEntity, SensorEntity):
         factor: int | None,
         device_class,
         state_class,
+        unit_of_measurement,
     ) -> None:
         """Initialize AdsSensor entity."""
         super().__init__(ads_hub, name, ads_var)
@@ -105,6 +109,7 @@ class AdsSensor(AdsEntity, SensorEntity):
         self._factor = factor
         self._attr_device_class = device_class
         self._attr_state_class = state_class
+        self._attr_native_unit_of_measurement = unit_of_measurement
         self._state = None
 
     async def async_added_to_hass(self) -> None:
