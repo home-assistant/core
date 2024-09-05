6e1c86ece9159d4ff11f4f3c633d3ead29469df4
Malte Franken <exxamalte@users.noreply.github.com>
2024-09-05 16:44:29 +1000

    make test work

--- tests/components/gdacs/test_diagnostics.py
+++ tests/components/gdacs/test_diagnostics.py
@@ -2,33 +2,34 @@
 
 from __future__ import annotations
 
+from unittest.mock import patch
+
+import pytest
 from syrupy import SnapshotAssertion
 
-from homeassistant.const import CONF_RADIUS, EVENT_HOMEASSISTANT_START
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
 from tests.components.diagnostics import get_diagnostics_for_config_entry
 from tests.typing import ClientSessionGenerator
 
-CONFIG = {CONF_RADIUS: 200}
 
-
+@pytest.mark.freeze_time("2024-09-05 15:00:00")
 async def test_entry_diagnostics(
     hass: HomeAssistant,
     hass_client: ClientSessionGenerator,
-    config_entry: MockConfigEntry,
     snapshot: SnapshotAssertion,
+    config_entry: MockConfigEntry,
 ) -> None:
     """Test config entry diagnostics."""
-    config_entry.add_to_hass(hass)
-    hass.config_entries.async_update_entry(
-        config_entry, data=config_entry.data | CONFIG
-    )
-    assert await hass.config_entries.async_setup(config_entry.entry_id)
-    # Artificially trigger update and collect events.
-    hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
-    await hass.async_block_till_done()
+    with (
+        patch("aio_georss_client.feed.GeoRssFeed.update") as mock_feed_update,
+    ):
+        mock_feed_update.return_value = "OK", []
 
-    result = await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
-    assert result == snapshot
+        config_entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+        result = await get_diagnostics_for_config_entry(hass, hass_client, config_entry)
+        assert result == snapshot
