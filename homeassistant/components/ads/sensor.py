f110a18321613b72176cc6ea63b8ec132fafe0a2
Adam Pasztor <mrpasztoradam@gmail.com>
2024-09-06 08:10:00 +0200

    fix: addressing review suggestions.

--- homeassistant/components/ads/sensor.py
+++ homeassistant/components/ads/sensor.py
@@ -9,7 +9,9 @@ from homeassistant.components.sensor import (
     DEVICE_CLASSES_SCHEMA as SENSOR_DEVICE_CLASSES_SCHEMA,
     PLATFORM_SCHEMA as SENSOR_PLATFORM_SCHEMA,
     STATE_CLASSES_SCHEMA as SENSOR_STATE_CLASSES_SCHEMA,
+    SensorDeviceClass,
     SensorEntity,
+    SensorStateClass,
 )
 from homeassistant.const import CONF_DEVICE_CLASS, CONF_NAME, CONF_UNIT_OF_MEASUREMENT
 from homeassistant.core import HomeAssistant
@@ -99,9 +101,9 @@ class AdsSensor(AdsEntity, SensorEntity):
         ads_type: str,
         name: str,
         factor: int | None,
-        device_class,
-        state_class,
-        unit_of_measurement,
+        device_class: SensorDeviceClass | None,
+        state_class: SensorStateClass | None,
+        unit_of_measurement: str | None,
     ) -> None:
         """Initialize AdsSensor entity."""
         super().__init__(ads_hub, name, ads_var)
@@ -110,7 +112,6 @@ class AdsSensor(AdsEntity, SensorEntity):
         self._attr_device_class = device_class
         self._attr_state_class = state_class
         self._attr_native_unit_of_measurement = unit_of_measurement
-        self._state = None
 
     async def async_added_to_hass(self) -> None:
         """Register device notification."""
