0dade590ea720c86c0ebbbd2737e836938e9df77
Willem-Jan L. van Rootselaar <liudgervr@gmail.com>
2024-09-06 13:56:06 +0200

    refactor: Improve BSBLANClimate async_set_preset_mode method
    This commit refactors the async_set_preset_mode method in the BSBLANClimate class to improve code readability and maintainability. The method now checks if the HVAC mode is not set to AUTO and the preset mode is not NONE before raising a ServiceValidationError.
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/bsblan/climate.py
+++ homeassistant/components/bsblan/climate.py
@@ -126,15 +126,13 @@ class BSBLANClimate(BSBLanEntity, ClimateEntity):
 
     async def async_set_preset_mode(self, preset_mode: str) -> None:
         """Set preset mode."""
-        # only allow preset mode when hvac mode is auto
-        if self.hvac_mode == HVACMode.AUTO:
-            await self.async_set_data(preset_mode=preset_mode)
-        else:
+        if self.hvac_mode != HVACMode.AUTO and preset_mode != PRESET_NONE:
             raise ServiceValidationError(
                 translation_domain=DOMAIN,
                 translation_key="set_preset_mode_error",
                 translation_placeholders={"preset_mode": preset_mode},
             )
+        await self.async_set_data(preset_mode=preset_mode)
 
     async def async_set_temperature(self, **kwargs: Any) -> None:
         """Set new target temperatures."""
@@ -148,11 +146,14 @@ class BSBLANClimate(BSBLanEntity, ClimateEntity):
         if ATTR_HVAC_MODE in kwargs:
             data[ATTR_HVAC_MODE] = kwargs[ATTR_HVAC_MODE]
         if ATTR_PRESET_MODE in kwargs:
-            # If preset mode is None, set hvac to auto
-            if kwargs[ATTR_PRESET_MODE] == PRESET_NONE:
-                data[ATTR_HVAC_MODE] = HVACMode.AUTO
+            if kwargs[ATTR_PRESET_MODE] == PRESET_ECO:
+                data[ATTR_HVAC_MODE] = PRESET_ECO
+            elif kwargs[ATTR_PRESET_MODE] == PRESET_NONE:
+                # Don't change the HVAC mode when setting to PRESET_NONE
+                pass
             else:
-                data[ATTR_HVAC_MODE] = kwargs[ATTR_PRESET_MODE]
+                data[ATTR_HVAC_MODE] = HVACMode.AUTO
+
         try:
             await self.coordinator.client.thermostat(**data)
         except BSBLANError as err:
