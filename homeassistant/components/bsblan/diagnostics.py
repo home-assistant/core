6b3b4e224b908b76ef84955e9e55362ac35902f6
Willem-Jan L. van Rootselaar <liudgervr@gmail.com>
2024-09-06 16:31:32 +0200

    refactor: Simplify async_get_config_entry_diagnostics method

--- homeassistant/components/bsblan/diagnostics.py
+++ homeassistant/components/bsblan/diagnostics.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import asyncio
 from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
@@ -19,15 +18,9 @@ async def async_get_config_entry_diagnostics(
     data: BSBLanData = hass.data[DOMAIN][entry.entry_id]
 
     return {
-        "info": await data.info.to_dict()
-        if asyncio.iscoroutinefunction(data.info.to_dict)
-        else data.info.to_dict(),
-        "device": await data.device.to_dict()
-        if asyncio.iscoroutinefunction(data.device.to_dict)
-        else data.device.to_dict(),
+        "info": data.info.to_dict(),
+        "device": data.device.to_dict(),
         "coordinator_data": {
-            "state": await data.coordinator.data.state.to_dict()
-            if asyncio.iscoroutinefunction(data.coordinator.data.state.to_dict)
-            else data.coordinator.data.state.to_dict(),
+            "state": data.coordinator.data.state.to_dict(),
         },
     }
