2d2704c09415a3350dc58cb682652c9bab7c0b94
Willem-Jan L. van Rootselaar <liudgervr@gmail.com>
2024-09-06 16:52:51 +0200

    refactor: Add static data to async_get_config_entry_diagnostics
    right place

--- homeassistant/components/bsblan/diagnostics.py
+++ homeassistant/components/bsblan/diagnostics.py
@@ -22,6 +22,6 @@ async def async_get_config_entry_diagnostics(
         "device": data.device.to_dict(),
         "coordinator_data": {
             "state": data.coordinator.data.state.to_dict(),
-            "static": data.static.to_dict(),
         },
+        "static": data.static.to_dict(),
     }
