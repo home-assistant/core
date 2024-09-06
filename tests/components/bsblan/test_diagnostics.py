c2b97fbdb6e1ed064172c097c60e235841bfce42
Willem-Jan L. van Rootselaar <liudgervr@gmail.com>
2024-09-06 09:29:15 +0200

    chore: removed set_static_value

--- tests/components/bsblan/test_diagnostics.py
+++ tests/components/bsblan/test_diagnostics.py
@@ -2,7 +2,6 @@
 
 from unittest.mock import AsyncMock
 
-import pytest
 from syrupy import SnapshotAssertion
 
 from homeassistant.core import HomeAssistant
@@ -12,17 +11,14 @@ from tests.components.diagnostics import get_diagnostics_for_config_entry
 from tests.typing import ClientSessionGenerator
 
 
-@pytest.mark.parametrize("static_file", ["static.json"])
 async def test_diagnostics(
     hass: HomeAssistant,
     mock_bsblan: AsyncMock,
     hass_client: ClientSessionGenerator,
     init_integration: MockConfigEntry,
     snapshot: SnapshotAssertion,
-    static_file: str,
 ) -> None:
     """Test diagnostics."""
-    await mock_bsblan.set_static_values(static_file)
 
     diagnostics_data = await get_diagnostics_for_config_entry(
         hass, hass_client, init_integration
