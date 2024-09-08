b496134cf0581143062c1b03b1bb121792513661
Noah Husby <noahhusby@gmail.com>
2024-09-08 22:36:36 +0000

    Add zeroconf discovery to Cambridge Audio

--- homeassistant/components/cambridge_audio/config_flow.py
+++ homeassistant/components/cambridge_audio/config_flow.py
@@ -5,8 +5,9 @@ from typing import Any
 from aiostreammagic import StreamMagicClient, StreamMagicError
 import voluptuous as vol
 
+from homeassistant.components import zeroconf
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
-from homeassistant.const import CONF_HOST
+from homeassistant.const import CONF_HOST, CONF_MODEL
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
@@ -23,6 +24,47 @@ class FlowHandler(ConfigFlow, domain=DOMAIN):
     """Cambridge Audio configuration flow."""
 
     VERSION = 1
+
+    def __init__(self) -> None:
+        """Initialize the config flow."""
+        self.data: dict[str, Any] = {}
+        self.client: StreamMagicClient | None = None
+
+    async def async_step_zeroconf(
+        self, discovery_info: zeroconf.ZeroconfServiceInfo
+    ) -> ConfigFlowResult:
+        """Handle zeroconf discovery."""
+        host = discovery_info.host
+        self.data[CONF_MODEL] = discovery_info.properties["model"]
+
+        await self.async_set_unique_id(discovery_info.properties["serial"])
+        self._abort_if_unique_id_configured(updates={CONF_HOST: host})
+        session = async_get_clientsession(self.hass)
+        self.client = StreamMagicClient(host, session=session)
+        await self.client.get_info()
+
+        self.context["title_placeholders"] = {
+            "model": self.data[CONF_MODEL],
+        }
+        return await self.async_step_discovery_confirm()
+
+    async def async_step_discovery_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Confirm discovery."""
+        if user_input is not None:
+            return self.async_create_entry(
+                title=self.data[CONF_MODEL],
+                data={CONF_HOST: self.data[CONF_HOST]},
+            )
+
+        self._set_confirm_only()
+        return self.async_show_form(
+            step_id="discovery_confirm",
+            description_placeholders={
+                "model": self.data[CONF_MODEL],
+            },
+        )
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
--- homeassistant/components/cambridge_audio/media_player.py
+++ homeassistant/components/cambridge_audio/media_player.py
@@ -7,7 +7,13 @@ from homeassistant.components.media_player import (
     MediaPlayerEntityFeature,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import STATE_OFF, STATE_ON
+from homeassistant.const import (
+    STATE_IDLE,
+    STATE_OFF,
+    STATE_ON,
+    STATE_PAUSED,
+    STATE_PLAYING,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -53,14 +59,23 @@ class CambridgeAudioDevice(CambridgeAudioEntity, MediaPlayerEntity):
         super().__init__(coordinator)
         self._attr_unique_id = coordinator.data.info.unit_id
         if coordinator.data.state.pre_amp_mode:
-            self._attr_supported_features = FEATURES
-        else:
             self._attr_supported_features = FEATURES_PREAMP
+        else:
+            self._attr_supported_features = FEATURES
 
     @property
     def state(self):
         """Return the state of the device."""
+        media_state = self.coordinator.data.play_state.state
+        if media_state == "NETWORK":
+            return STATE_OFF
         if self.coordinator.data.state.power:
+            if media_state == "play":
+                return STATE_PLAYING
+            if media_state == "pause":
+                return STATE_PAUSED
+            if media_state == "stop":
+                return STATE_IDLE
             return STATE_ON
         return STATE_OFF
 
@@ -68,6 +83,38 @@ class CambridgeAudioDevice(CambridgeAudioEntity, MediaPlayerEntity):
     def source_list(self):
         """Return a list of available input sources."""
         return [item.name for item in self.coordinator.data.sources]
+
+    @property
+    def source(self):
+        """Return the current input source."""
+        return next(
+            (
+                item.name
+                for item in self.coordinator.data.sources
+                if item.id == self.coordinator.data.state.source
+            ),
+            None,
+        )
+
+    @property
+    def media_title(self) -> str | None:
+        """Title of current playing media."""
+        return self.coordinator.data.play_state.metadata.title
+
+    @property
+    def media_artist(self):
+        """Artist of current playing media, music track only."""
+        return self.coordinator.data.play_state.metadata.artist
+
+    @property
+    def media_album_name(self):
+        """Album name of current playing media, music track only."""
+        return self.coordinator.data.play_state.metadata.album
+
+    @property
+    def media_image_url(self):
+        """Image url of current playing media."""
+        return self.coordinator.data.play_state.metadata.art_url
 
     @property
     def is_volume_muted(self):
@@ -81,15 +128,3 @@ class CambridgeAudioDevice(CambridgeAudioEntity, MediaPlayerEntity):
         if volume_percent is None:
             return None
         return float(volume_percent) / 100
-
-    @property
-    def source(self):
-        """Return the current input source."""
-        return next(
-            (
-                item.name
-                for item in self.coordinator.data.sources
-                if item.id == self.coordinator.data.state.source
-            ),
-            None,
-        )
