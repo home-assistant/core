d0ff08ae3e175464a12ca73912493b0196aecf5f
epenet <6771947+epenet@users.noreply.github.com>
2024-09-08 19:24:45 +0000

    Remove usedefault

--- homeassistant/components/airvisual/__init__.py
+++ homeassistant/components/airvisual/__init__.py
@@ -222,7 +222,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: AirVisualConfigEntry) ->
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
 
     await coordinator.async_config_entry_first_refresh()
-    hass.data.setdefault(DOMAIN, {})
     entry.runtime_data = coordinator
 
     # Reassess the interval between 2 server requests
