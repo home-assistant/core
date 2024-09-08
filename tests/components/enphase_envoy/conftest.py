74b78307eef58eec6efee6251014149acb05d003
Arie Catsman <120491684+catsmanac@users.noreply.github.com>
2024-09-08 12:15:00 +0200

    Add balanced grid import/export to enphase_envoy (#123154)
    * Add balanced grid import/export to enphase_envoy
    
    * rebuild sensor snapshot after dev merge
    
    * Cleanup snapshot file

--- tests/components/enphase_envoy/conftest.py
+++ tests/components/enphase_envoy/conftest.py
@@ -150,6 +150,8 @@ def _load_json_2_production_data(
     """Fill envoy production data from fixture."""
     if item := json_fixture["data"].get("system_consumption"):
         mocked_data.system_consumption = EnvoySystemConsumption(**item)
+    if item := json_fixture["data"].get("system_net_consumption"):
+        mocked_data.system_net_consumption = EnvoySystemConsumption(**item)
     if item := json_fixture["data"].get("system_production"):
         mocked_data.system_production = EnvoySystemProduction(**item)
     if item := json_fixture["data"].get("system_consumption_phases"):
@@ -157,6 +159,12 @@ def _load_json_2_production_data(
         for sub_item, item_data in item.items():
             mocked_data.system_consumption_phases[sub_item] = EnvoySystemConsumption(
                 **item_data
+            )
+    if item := json_fixture["data"].get("system_net_consumption_phases"):
+        mocked_data.system_net_consumption_phases = {}
+        for sub_item, item_data in item.items():
+            mocked_data.system_net_consumption_phases[sub_item] = (
+                EnvoySystemConsumption(**item_data)
             )
     if item := json_fixture["data"].get("system_production_phases"):
         mocked_data.system_production_phases = {}
