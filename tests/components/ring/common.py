f3e99114aa477196a8473f26bacec3685d78a274
Steven B. <51370195+sdb9696@users.noreply.github.com>
2024-09-08 17:17:30 +0100

    Fix ring notifications (#124879)
    * Enable ring event listener to fix missing notifications
    
    * Fix pylint test CI fail
    
    * Reinstate binary sensor and add deprecation issues
    
    * Add tests
    
    * Update post review
    
    * Remove PropertyMock
    
    * Update post review
    
    * Split out adding event platform

--- tests/components/ring/common.py
+++ tests/components/ring/common.py
@@ -2,6 +2,7 @@
 
 from unittest.mock import patch
 
+from homeassistant.components.automation import DOMAIN as AUTOMATION_DOMAIN
 from homeassistant.components.ring import DOMAIN
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -18,3 +19,18 @@ async def setup_platform(hass: HomeAssistant, platform: Platform) -> None:
     with patch("homeassistant.components.ring.PLATFORMS", [platform]):
         assert await async_setup_component(hass, DOMAIN, {})
     await hass.async_block_till_done(wait_background_tasks=True)
+
+
+async def setup_automation(hass: HomeAssistant, alias: str, entity_id: str) -> None:
+    """Set up an automation for tests."""
+    assert await async_setup_component(
+        hass,
+        AUTOMATION_DOMAIN,
+        {
+            AUTOMATION_DOMAIN: {
+                "alias": alias,
+                "trigger": {"platform": "state", "entity_id": entity_id, "to": "on"},
+                "action": {"action": "notify.notify", "metadata": {}, "data": {}},
+            }
+        },
+    )
