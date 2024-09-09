89b28b8e8e689605f26fca027c9e8611e97a7375
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-09-09 15:21:31 +0200

    Fixture for a Eve Door & Window node
    Fixture for a Eve Door & Window node to check BatVoltage attribute from PowerSource cluster

--- tests/components/matter/test_sensor.py
+++ tests/components/matter/test_sensor.py
@@ -64,6 +64,16 @@ async def temperature_sensor_node_fixture(
     )
 
 
+@pytest.fixture(name="eve_contact_sensor_node")
+async def eve_energy_plug_node_fixture(
+    hass: HomeAssistant, matter_client: MagicMock
+) -> MatterNode:
+    """Fixture for a Eve Door & Window node."""
+    return await setup_integration_with_node_fixture(
+        hass, "eve-contact-sensor", matter_client
+    )
+
+
 @pytest.fixture(name="eve_energy_plug_node")
 async def eve_energy_plug_node_fixture(
     hass: HomeAssistant, matter_client: MagicMock
