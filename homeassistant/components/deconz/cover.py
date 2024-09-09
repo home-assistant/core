2785fbefc7616fe3e8ea7f001b067194c54863e1
epenet <6771947+epenet@users.noreply.github.com>
2024-09-09 08:00:54 +0000

    Add alias to DOMAIN import (part 4)

--- homeassistant/components/deconz/cover.py
+++ homeassistant/components/deconz/cover.py
@@ -12,7 +12,7 @@ from pydeconz.models.light.cover import Cover
 from homeassistant.components.cover import (
     ATTR_POSITION,
     ATTR_TILT_POSITION,
-    DOMAIN,
+    DOMAIN as COVER_DOMAIN,
     CoverDeviceClass,
     CoverEntity,
     CoverEntityFeature,
@@ -38,7 +38,7 @@ async def async_setup_entry(
 ) -> None:
     """Set up covers for deCONZ component."""
     hub = DeconzHub.get_hub(hass, config_entry)
-    hub.entities[DOMAIN] = set()
+    hub.entities[COVER_DOMAIN] = set()
 
     @callback
     def async_add_cover(_: EventType, cover_id: str) -> None:
@@ -54,7 +54,7 @@ async def async_setup_entry(
 class DeconzCover(DeconzDevice[Cover], CoverEntity):
     """Representation of a deCONZ cover."""
 
-    TYPE = DOMAIN
+    TYPE = COVER_DOMAIN
 
     def __init__(self, cover_id: str, hub: DeconzHub) -> None:
         """Set up cover device."""
