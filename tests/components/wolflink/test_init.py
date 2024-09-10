e99e650b3fdff21acd23e6d5b0d5cd4f5c4746fa
epenet <6771947+epenet@users.noreply.github.com>
2024-09-10 09:57:49 +0000

    Migrate identifiers

--- tests/components/wolflink/test_init.py
+++ tests/components/wolflink/test_init.py
@@ -4,20 +4,30 @@ from unittest.mock import patch
 
 from httpx import RequestError
 
-from homeassistant.components.wolflink.const import DEVICE_ID, DOMAIN
+from homeassistant.components.wolflink.const import DEVICE_ID, DOMAIN, MANUFACTURER
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 
 from .const import CONFIG
 
 from tests.common import MockConfigEntry
 
 
-async def test_unique_id_migration(hass: HomeAssistant) -> None:
+async def test_unique_id_migration(
+    hass: HomeAssistant, device_registry: dr.DeviceRegistry
+) -> None:
     """Test already configured while creating entry."""
     config_entry = MockConfigEntry(
         domain=DOMAIN, unique_id=CONFIG[DEVICE_ID], data=CONFIG
     )
     config_entry.add_to_hass(hass)
+
+    device_id = device_registry.async_get_or_create(
+        config_entry_id=config_entry.entry_id,
+        identifiers={(DOMAIN, CONFIG[DEVICE_ID])},
+        configuration_url="https://www.wolf-smartset.com/",
+        manufacturer=MANUFACTURER,
+    ).id
 
     assert config_entry.unique_id == 1234
     assert (
@@ -25,6 +35,7 @@ async def test_unique_id_migration(hass: HomeAssistant) -> None:
         is config_entry
     )
     assert hass.config_entries.async_entry_for_domain_unique_id(DOMAIN, "1234") is None
+    assert device_registry.async_get(device_id).identifiers == {(DOMAIN, 1234)}
 
     with (
         patch(
@@ -40,3 +51,5 @@ async def test_unique_id_migration(hass: HomeAssistant) -> None:
         is config_entry
     )
     assert hass.config_entries.async_entry_for_domain_unique_id(DOMAIN, 1234) is None
+
+    assert device_registry.async_get(device_id).identifiers == {(DOMAIN, "1234")}
