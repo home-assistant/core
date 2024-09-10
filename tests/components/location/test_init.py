97bc5c98e624989539db1980917d03839264827e
Joostlek <joostlek@outlook.com>
2024-09-10 15:02:53 +0200

    Fix

--- tests/components/location/test_init.py
+++ tests/components/location/test_init.py
@@ -1,6 +1,24 @@
 """Tests for the Location component."""
+from unittest.mock import patch
+
 from homeassistant.core import HomeAssistant
+
+from homeassistant.generated import locations as loc_gen
 
 
 async def test_setup(hass: HomeAssistant) -> None:
     """Test setup."""
+
+    mock_loc = {
+        "NL": [
+            "buienradar",
+        ],
+    }
+    with (
+        patch.dict(
+            loc_gen.LOCATIONS,
+            mock_loc,
+            clear=True,
+        ),
+        patch.object(hass.config_entries.flow, "async_init") as mock_config_flow,
+    ):
