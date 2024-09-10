e9470d84535b27b3d6ff2274cbab25acf3ce5a91
Phill (pssc) <pssc@users.noreply.github.com>
2024-09-10 16:17:26 +0100

    Add squeezebox service sensors (#125349)
    * Add server sensors
    
    * Fix Platforms order
    
    * Fix spelling
    
    * Fix translations
    
    * Add sensor test
    
    * Case changes
    
    * refactor to use native_value attr override
    
    * Fix typing
    
    * Fix cast to type
    
    * add cast
    
    * use update platform for LMS versions
    
    * Fix translation
    
    * remove update entity
    
    * remove possible update entites
    
    * Fix and clarify
    
    * update to icon trans remove update plaform entitiy supporting items
    
    * add UOM to sensors
    
    * correct criptic prettier fail
    
    * reword other players
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/squeezebox/coordinator.py
+++ homeassistant/components/squeezebox/coordinator.py
@@ -3,15 +3,18 @@
 from asyncio import timeout
 from datetime import timedelta
 import logging
+import re
 
 from pysqueezebox import Server
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.util import dt as dt_util
 
 from .const import (
     SENSOR_UPDATE_INTERVAL,
     STATUS_API_TIMEOUT,
+    STATUS_SENSOR_LASTSCAN,
     STATUS_SENSOR_NEEDSRESTART,
     STATUS_SENSOR_RESCAN,
 )
@@ -32,6 +35,7 @@ class LMSStatusDataUpdateCoordinator(DataUpdateCoordinator):
             always_update=False,
         )
         self.lms = lms
+        self.newversion_regex = re.compile("<.*$")
 
     async def _async_update_data(self) -> dict:
         """Fetch data fromn LMS status call.
@@ -50,10 +54,19 @@ class LMSStatusDataUpdateCoordinator(DataUpdateCoordinator):
     def _prepare_status_data(self, data: dict) -> dict:
         """Sensors that need the data changing for HA presentation."""
 
+        # Binary sensors
         # rescan bool are we rescanning alter poll not present if false
         data[STATUS_SENSOR_RESCAN] = STATUS_SENSOR_RESCAN in data
         # needsrestart bool pending lms plugin updates not present if false
         data[STATUS_SENSOR_NEEDSRESTART] = STATUS_SENSOR_NEEDSRESTART in data
+
+        # Sensors that need special handling
+        # 'lastscan': '1718431678', epoc -> ISO 8601 not always present
+        data[STATUS_SENSOR_LASTSCAN] = (
+            dt_util.utc_from_timestamp(int(data[STATUS_SENSOR_LASTSCAN]))
+            if STATUS_SENSOR_LASTSCAN in data
+            else None
+        )
 
         _LOGGER.debug("Processed serverstatus %s=%s", self.lms.name, data)
         return data
