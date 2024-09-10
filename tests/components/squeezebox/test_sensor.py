f2b8feb2e9889e613caa3d4c16c8b4e9b76ac694
Phill (pssc) <pssc@users.noreply.github.com>
2024-09-10 16:17:26 +0100

    Add squeezebox service sensors (#125349)
    * Add server sensors
    
    * Fix Platforms order
    
    * Fix spelling
    
    * Fix translations
    
    * Add sensor test
    
    * Case changes
    
    * refactor to use native_value attr override
    
    * Fix typing
    
    * Fix cast to type
    
    * add cast
    
    * use update platform for LMS versions
    
    * Fix translation
    
    * remove update entity
    
    * remove possible update entites
    
    * Fix and clarify
    
    * update to icon trans remove update plaform entitiy supporting items
    
    * add UOM to sensors
    
    * correct criptic prettier fail
    
    * reword other players
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- tests/components/squeezebox/test_sensor.py
+++ tests/components/squeezebox/test_sensor.py
@@ -1,34 +1,29 @@
-"""Test squeezebox binary sensors."""
+"""Test squeezebox sensors."""
 
-import copy
 from unittest.mock import patch
 
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
 
 from . import FAKE_QUERY_RESPONSE, setup_mocked_integration
 
 
-async def test_binary_sensor(
-    hass: HomeAssistant,
-    entity_registry: er.EntityRegistry,
-) -> None:
+async def test_sensor(hass: HomeAssistant) -> None:
     """Test binary sensor states and attributes."""
 
     # Setup component
     with (
         patch(
             "homeassistant.components.squeezebox.PLATFORMS",
-            [Platform.BINARY_SENSOR],
+            [Platform.SENSOR],
         ),
         patch(
             "homeassistant.components.squeezebox.Server.async_query",
-            return_value=copy.deepcopy(FAKE_QUERY_RESPONSE),
+            return_value=FAKE_QUERY_RESPONSE,
         ),
     ):
         await setup_mocked_integration(hass)
-    state = hass.states.get("binary_sensor.fakelib_library_rescan")
+    state = hass.states.get("sensor.fakelib_player_count")
 
     assert state is not None
-    assert state.state == "on"
+    assert state.state == "10"
