3d6e78946b29ddb09a0ba8b35dcf8d6765e79a9d
Joost Lekkerkerker <joostlek@outlook.com>
2024-09-10 17:06:21 +0200

    Apply suggestions from code review

--- tests/components/squeezebox/test_sensor.py
+++ tests/components/squeezebox/test_sensor.py
@@ -1,19 +1,14 @@
 """Test squeezebox sensors."""
 
-import copy
 from unittest.mock import patch
 
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
 
 from . import FAKE_QUERY_RESPONSE, setup_mocked_integration
 
 
-async def test_sensor(
-    hass: HomeAssistant,
-    entity_registry: er.EntityRegistry,
-) -> None:
+async def test_sensor(hass: HomeAssistant) -> None:
     """Test binary sensor states and attributes."""
 
     # Setup component
@@ -24,7 +19,7 @@ async def test_sensor(
         ),
         patch(
             "homeassistant.components.squeezebox.Server.async_query",
-            return_value=copy.deepcopy(FAKE_QUERY_RESPONSE),
+            return_value=FAKE_QUERY_RESPONSE,
         ),
     ):
         await setup_mocked_integration(hass)
