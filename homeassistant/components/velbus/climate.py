1c248f9bad5e626c01ff6adaddbb7e60a380aa2a
Roelf Zomerman <rozome@microsoft.com>
2024-09-10 09:36:18 +0400

    Update climate.py

--- homeassistant/components/velbus/climate.py
+++ homeassistant/components/velbus/climate.py
@@ -92,8 +92,7 @@ class VelbusClimate(VelbusEntity, ClimateEntity):
             raise ServiceValidationError(
                 translation_domain=DOMAIN,
                 translation_key="invalid_hvac_mode",
-                translation_placeholders={"hvac_mode": hvac_mode},
-            )
+                translation_placeholders={"hvac_mode": hvac_mode})
         await self._channel.set_mode(hvac_mode)
         self.async_write_ha_state()
 
