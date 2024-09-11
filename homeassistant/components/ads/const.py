0ec7db3eb09c5b16f7967c8c94217e9226062aa3
epenet <6771947+epenet@users.noreply.github.com>
2024-09-11 09:41:53 +0000

    Adjust

--- homeassistant/components/ads/const.py
+++ homeassistant/components/ads/const.py
@@ -1,5 +1,7 @@
 """Support for Automation Device Specification (ADS)."""
 
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from homeassistant.util.hass_dict import HassKey
