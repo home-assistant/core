ec97884da14e25555f8ebe25ef37bd2835c691f3
Willem-Jan L. van Rootselaar <liudgervr@gmail.com>
2024-09-11 22:16:20 +0200

    refactor: let hass use translation_key
    fix raise

--- homeassistant/components/bsblan/sensor.py
+++ homeassistant/components/bsblan/sensor.py
@@ -74,23 +74,19 @@ class BSBLanSensor(BSBLanEntity, SensorEntity):
         super().__init__(data.coordinator, data)
         self.entity_description = description
         self._attr_unique_id = f"{data.device.MAC}-{description.key}"
-        self._attr_name = self._generate_name(
-            description.translation_key, description.key
-        )
-
-    @staticmethod
-    def _generate_name(translation_key: str | None, description_key: str) -> str:
-        """Generate a name from the translation key."""
-        if translation_key is None:
-            # use key as name
-            return description_key.replace("_", " ").title()
-        return translation_key.replace("_", " ").title()
 
     @property
     def native_value(self) -> float | None:
         """Return the state of the sensor."""
         try:
             value = self.entity_description.value_fn(self.coordinator.data)
-            return float(value) if value is not None else None
-        except (ValueError, AttributeError):
+        except AttributeError:
+            return None
+
+        if value is None:
+            return None
+
+        try:
+            return float(value)
+        except ValueError:
             return None
