72cbaa2bfae7a1cac7fca6f312612b66b3fd1014
epdevlab <epdevlab@gmail.com>
2024-09-11 07:38:07 +0000

    remove OptionsFlow

--- homeassistant/components/inels/config_flow.py
+++ homeassistant/components/inels/config_flow.py
@@ -8,14 +8,9 @@ from inelsmqtt import InelsMqtt
 from inelsmqtt.const import MQTT_TRANSPORT
 import voluptuous as vol
 
-from homeassistant.config_entries import (
-    ConfigEntry,
-    ConfigFlow,
-    ConfigFlowResult,
-    OptionsFlow,
-)
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_PORT, CONF_USERNAME
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, TITLE
 
@@ -26,14 +21,6 @@ class INelsConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle of iNELS config flow."""
 
     VERSION = 1
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(
-        config_entry: ConfigEntry,
-    ) -> InelsOptionsFlowHandler:
-        """Get the options flow for this handler."""
-        return InelsOptionsFlowHandler(config_entry)
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -80,64 +67,6 @@ class INelsConfigFlow(ConfigFlow, domain=DOMAIN):
                     vol.Required(MQTT_TRANSPORT, default="tcp"): vol.In(
                         ["tcp", "websockets"]
                     ),
-                }
-            ),
-            errors=errors,
-            last_step=True,
-        )
-
-
-class InelsOptionsFlowHandler(OptionsFlow):
-    """Handle iNELS options."""
-
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input: None = None) -> ConfigFlowResult:
-        """Manage the options."""
-        return await self.async_step_setup()
-
-    async def async_step_setup(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Manage the MQTT options."""
-        errors = {}
-        current_config = self.config_entry.data
-
-        if user_input is not None:
-            test_connect = await self.hass.async_add_executor_job(
-                try_connection,
-                self.hass,
-                user_input[CONF_HOST],
-                user_input[CONF_PORT],
-                user_input.get(CONF_USERNAME, ""),
-                user_input.get(CONF_PASSWORD, ""),
-                user_input[MQTT_TRANSPORT],
-            )
-
-            if test_connect is None:
-                return self.async_create_entry(title=TITLE, data=user_input)
-
-            errors["base"] = connect_val_to_error(test_connect)
-
-        return self.async_show_form(
-            step_id="setup",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_HOST, default=current_config[CONF_HOST]): str,
-                    vol.Required(
-                        CONF_PORT, default=current_config[CONF_PORT]
-                    ): vol.Coerce(int),
-                    vol.Optional(
-                        CONF_USERNAME, default=current_config.get(CONF_USERNAME, "")
-                    ): str,
-                    vol.Optional(
-                        CONF_PASSWORD, default=current_config.get(CONF_PASSWORD, "")
-                    ): str,
-                    vol.Required(
-                        MQTT_TRANSPORT, default=current_config[MQTT_TRANSPORT]
-                    ): vol.In(["tcp", "websockets"]),
                 }
             ),
             errors=errors,
