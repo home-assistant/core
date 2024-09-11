dbec5d6416b0432c6f8c426b659cbc249f3a0493
epenet <6771947+epenet@users.noreply.github.com>
2024-09-11 12:50:21 +0000

    Use HassKey in trace

--- homeassistant/components/trace/const.py
+++ homeassistant/components/trace/const.py
@@ -1,7 +1,18 @@
 """Shared constants for script and automation tracing and debugging."""
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from homeassistant.util.hass_dict import HassKey
+
+if TYPE_CHECKING:
+    from . import TraceData
+
+DOMAIN = "trace"
+
 CONF_STORED_TRACES = "stored_traces"
-DATA_TRACE = "trace"
+DATA_TRACE: HassKey[TraceData] = HassKey(DOMAIN)
 DATA_TRACE_STORE = "trace_store"
 DATA_TRACES_RESTORED = "trace_traces_restored"
 DEFAULT_STORED_TRACES = 5  # Stored traces per script or automation
