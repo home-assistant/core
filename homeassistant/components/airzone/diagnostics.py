82691c692787ef7ad669eeeed311d7acd6ea8d79
epenet <6771947+epenet@users.noreply.github.com>
2024-09-12 11:14:55 +0000

    Use root import for async_redact_data in diagnostics

--- homeassistant/components/airzone/diagnostics.py
+++ homeassistant/components/airzone/diagnostics.py
@@ -6,7 +6,7 @@ from typing import Any
 
 from aioairzone.const import API_MAC, AZD_MAC
 
-from homeassistant.components.diagnostics.util import async_redact_data
+from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.const import CONF_UNIQUE_ID
 from homeassistant.core import HomeAssistant
 
