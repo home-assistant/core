e2e081df1d4e251f448dec996f37121a9b25b724
Louis Christ <mail@louischrist.de>
2024-09-12 23:05:37 +0200

    Add tests for join and unjoin

--- tests/components/bluesound/test_media_player_custom_actions.py
+++ tests/components/bluesound/test_media_player_custom_actions.py
@@ -1,7 +1,10 @@
 """Test custom actions."""
 
+import asyncio
+import dataclasses
 from unittest.mock import call
 
+from pyblu import PairedPlayer
 import pytest
 
 from homeassistant.core import HomeAssistant
@@ -57,3 +60,53 @@ async def test_join_cannot_join_to_self(
         )
 
     assert str(exc.value) == "Cannot join player to itself"
+
+
+async def test_join(
+    hass: HomeAssistant,
+    setup_config_entry: None,
+    setup_config_entry_secondary: None,
+    player_mocks: PlayerMocks,
+) -> None:
+    """Test the join action."""
+    await hass.services.async_call(
+        "bluesound",
+        "join",
+        {
+            "entity_id": "media_player.player_name1111",
+            "master": "media_player.player_name2222",
+        },
+        blocking=True,
+    )
+
+    player_mocks.player_data_secondary.player.add_slave.assert_called_once_with(
+        "1.1.1.1", 11000
+    )
+
+
+async def test_unjoin(
+    hass: HomeAssistant,
+    setup_config_entry: None,
+    setup_config_entry_secondary: None,
+    player_mocks: PlayerMocks,
+) -> None:
+    """Test the unjoin action."""
+    updated_sync_status = dataclasses.replace(
+        player_mocks.player_data.sync_status_store.get(),
+        master=PairedPlayer("2.2.2.2", 11000),
+    )
+    player_mocks.player_data.sync_status_store.set(updated_sync_status)
+
+    # this might be flaky, but we do not have a way to wait for the master to be set
+    await asyncio.sleep(0)
+
+    await hass.services.async_call(
+        "bluesound",
+        "unjoin",
+        {"entity_id": "media_player.player_name1111"},
+        blocking=True,
+    )
+
+    player_mocks.player_data_secondary.player.remove_slave.assert_called_once_with(
+        "1.1.1.1", 11000
+    )
