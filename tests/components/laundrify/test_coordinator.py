d0f4747e1977eaf20334012f3c1f4d4b767b0a4e
Mike Mülhaupt <mike.muelhaupt@gmail.com>
2024-09-12 18:24:26 +0000

    refactor(tests): create a separate UpdateCoordinator instead of using the internal

--- tests/components/laundrify/test_coordinator.py
+++ tests/components/laundrify/test_coordinator.py
@@ -2,17 +2,18 @@
 
 from laundrify_aio import exceptions
 
-from homeassistant.components.laundrify.const import DOMAIN
+from homeassistant.components.laundrify.const import DEFAULT_POLL_INTERVAL
+from homeassistant.components.laundrify.coordinator import LaundrifyUpdateCoordinator
 from homeassistant.core import HomeAssistant
-
-from tests.common import MockConfigEntry
 
 
 async def test_coordinator_update_success(
-    hass: HomeAssistant, laundrify_config_entry: MockConfigEntry
+    hass: HomeAssistant, laundrify_api_mock
 ) -> None:
     """Test the coordinator update is performed successfully."""
-    coordinator = hass.data[DOMAIN][laundrify_config_entry.entry_id]["coordinator"]
+    coordinator = LaundrifyUpdateCoordinator(
+        hass, laundrify_api_mock, DEFAULT_POLL_INTERVAL
+    )
     await coordinator.async_refresh()
     await hass.async_block_till_done()
 
@@ -20,11 +21,14 @@ async def test_coordinator_update_success(
 
 
 async def test_coordinator_update_unauthorized(
-    hass: HomeAssistant, laundrify_api_mock, laundrify_config_entry: MockConfigEntry
+    hass: HomeAssistant, laundrify_api_mock
 ) -> None:
     """Test the coordinator update fails if an UnauthorizedException is thrown."""
-    coordinator = hass.data[DOMAIN][laundrify_config_entry.entry_id]["coordinator"]
-    laundrify_api_mock.side_effect = exceptions.UnauthorizedException
+    coordinator = LaundrifyUpdateCoordinator(
+        hass, laundrify_api_mock, DEFAULT_POLL_INTERVAL
+    )
+    laundrify_api_mock.get_machines.side_effect = exceptions.UnauthorizedException
+
     await coordinator.async_refresh()
     await hass.async_block_till_done()
 
@@ -32,11 +36,14 @@ async def test_coordinator_update_unauthorized(
 
 
 async def test_coordinator_update_connection_failed(
-    hass: HomeAssistant, laundrify_api_mock, laundrify_config_entry: MockConfigEntry
+    hass: HomeAssistant, laundrify_api_mock
 ) -> None:
     """Test the coordinator update fails if an ApiConnectionException is thrown."""
-    coordinator = hass.data[DOMAIN][laundrify_config_entry.entry_id]["coordinator"]
-    laundrify_api_mock.side_effect = exceptions.ApiConnectionException
+    coordinator = LaundrifyUpdateCoordinator(
+        hass, laundrify_api_mock, DEFAULT_POLL_INTERVAL
+    )
+    laundrify_api_mock.get_machines.side_effect = exceptions.ApiConnectionException
+
     await coordinator.async_refresh()
     await hass.async_block_till_done()
 
