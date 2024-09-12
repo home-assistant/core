01681b2c261fb4b149b0b34af24433503f2ee10f
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-09-12 12:20:14 +0000

    Z-Wave JS: Use start/stop level change to open/close Window Covering CC covers

--- tests/components/zwave_js/test_cover.py
+++ tests/components/zwave_js/test_cover.py
@@ -994,3 +994,102 @@ async def test_nice_ibt4zwave_cover(
     assert args["value"] == 99
 
     client.async_send_command.reset_mock()
+
+
+async def test_window_covering_open_close(
+    hass: HomeAssistant, client, window_covering_outbound_bottom, integration
+) -> None:
+    """Tests that a Window Covering device with position support opens/closes with the start/stop level change properties."""
+    entity_id = "cover.node_2_outbound_bottom"
+    state = hass.states.get(entity_id)
+
+    # The entity has position support, but not tilt
+    assert state
+    assert ATTR_CURRENT_POSITION in state.attributes
+    assert ATTR_CURRENT_TILT_POSITION not in state.attributes
+
+    # Test opening
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_OPEN_COVER,
+        {ATTR_ENTITY_ID: entity_id},
+        blocking=True,
+    )
+    assert len(client.async_send_command.call_args_list) == 1
+    args = client.async_send_command.call_args[0][0]
+    assert args["command"] == "node.set_value"
+    assert args["nodeId"] == 2
+    assert args["valueId"] == {
+        "commandClass": 106,
+        "endpoint": 0,
+        "property": "levelChangeUp",
+        "propertyKey": 13,
+    }
+    assert args["value"] is True
+
+    client.async_send_command.reset_mock()
+
+    # Test stop after opening
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_STOP_COVER,
+        {ATTR_ENTITY_ID: entity_id},
+        blocking=True,
+    )
+
+    assert len(client.async_send_command.call_args_list) == 1
+    args = client.async_send_command.call_args[0][0]
+    assert args["command"] == "node.set_value"
+    assert args["nodeId"] == 2
+    assert args["valueId"] == {
+        "commandClass": 106,
+        "endpoint": 0,
+        "property": "levelChangeUp",
+        "propertyKey": 13,
+    }
+    assert args["value"] is False
+
+    client.async_send_command.reset_mock()
+
+    # Test closing
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_CLOSE_COVER,
+        {ATTR_ENTITY_ID: entity_id},
+        blocking=True,
+    )
+    assert len(client.async_send_command.call_args_list) == 1
+    args = client.async_send_command.call_args[0][0]
+    assert args["command"] == "node.set_value"
+    assert args["nodeId"] == 2
+    assert args["valueId"] == {
+        "commandClass": 106,
+        "endpoint": 0,
+        "property": "levelChangeDown",
+        "propertyKey": 13,
+    }
+    assert args["value"] is True
+
+    client.async_send_command.reset_mock()
+
+    # Test stop after closing
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_STOP_COVER,
+        {ATTR_ENTITY_ID: entity_id},
+        blocking=True,
+    )
+
+    assert len(client.async_send_command.call_args_list) == 1
+    args = client.async_send_command.call_args[0][0]
+    assert args["command"] == "node.set_value"
+    assert args["nodeId"] == 2
+    assert args["valueId"] == {
+        "commandClass": 106,
+        "endpoint": 0,
+        "property": "levelChangeUp",
+        "propertyKey": 13,
+    }
+    assert args["value"] is False
+
+    client.async_send_command.reset_mock()
