a4c88a8591b8c26def88aec8bb61c4c1405d657b
Noah Husby <32528627+noahhusby@users.noreply.github.com>
2024-09-12 10:09:53 -0400

    Add entity available attribute to Cambridge Audio (#125831)
    * Bump aiostreammagic to 2.2.4
    
    * Move callback handling to entity class
    
    * Wrap all module exceptions in HA errors for Cambridge Audio

--- homeassistant/components/cambridge_audio/entity.py
+++ homeassistant/components/cambridge_audio/entity.py
@@ -1,11 +1,36 @@
 """Base class for Cambridge Audio entities."""
 
+from collections.abc import Awaitable, Callable, Coroutine
+from functools import wraps
+from typing import Any, Concatenate
+
 from aiostreammagic import StreamMagicClient
 
+from homeassistant.core import callback
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 
+from . import STREAM_MAGIC_EXCEPTIONS
 from .const import DOMAIN
+
+
+def command[_EntityT: CambridgeAudioEntity, **_P](
+    func: Callable[Concatenate[_EntityT, _P], Awaitable[None]],
+) -> Callable[Concatenate[_EntityT, _P], Coroutine[Any, Any, None]]:
+    """Wrap async calls to raise on request error."""
+
+    @wraps(func)
+    async def decorator(self: _EntityT, *args: _P.args, **kwargs: _P.kwargs) -> None:
+        """Wrap all command methods."""
+        try:
+            await func(self, *args, **kwargs)
+        except STREAM_MAGIC_EXCEPTIONS as exc:
+            raise HomeAssistantError(
+                f"Error executing {func.__name__} on entity {self.entity_id},"
+            ) from exc
+
+    return decorator
 
 
 class CambridgeAudioEntity(Entity):
@@ -24,3 +49,17 @@ class CambridgeAudioEntity(Entity):
             serial_number=client.info.unit_id,
             configuration_url=f"http://{client.host}",
         )
+
+    @callback
+    async def _state_update_callback(self, _client: StreamMagicClient) -> None:
+        """Call when the device is notified of changes."""
+        self._attr_available = _client.is_connected()
+        self.async_write_ha_state()
+
+    async def async_added_to_hass(self) -> None:
+        """Register callback handlers."""
+        await self.client.register_state_update_callbacks(self._state_update_callback)
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Remove callbacks."""
+        await self.client.unregister_state_update_callbacks(self._state_update_callback)
