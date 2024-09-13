86ed9cd15596da3f127ea301d7540fd2ad111bd2
G Johansson <goran.johansson@shiftit.se>
2024-09-13 17:17:25 +0000

    Add/fix tests

--- tests/components/local_file/test_init.py
+++ tests/components/local_file/test_init.py
@@ -2,7 +2,10 @@
 
 from __future__ import annotations
 
-from homeassistant.config_entries import ConfigEntryState
+from unittest.mock import Mock, patch
+
+from homeassistant.components.local_file.const import DOMAIN
+from homeassistant.config_entries import SOURCE_USER, ConfigEntryState
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
@@ -15,3 +18,30 @@ async def test_unload_entry(hass: HomeAssistant, loaded_entry: MockConfigEntry) 
     assert await hass.config_entries.async_unload(loaded_entry.entry_id)
     await hass.async_block_till_done()
     assert loaded_entry.state is ConfigEntryState.NOT_LOADED
+
+
+async def test_file_not_readable_during_startup(
+    hass: HomeAssistant,
+    get_config: dict[str, str],
+) -> None:
+    """Test a warning is shown setup when file is not readable."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        source=SOURCE_USER,
+        options=get_config,
+        entry_id="1",
+    )
+    config_entry.add_to_hass(hass)
+
+    with (
+        patch("os.path.isfile", Mock(return_value=True)),
+        patch("os.access", Mock(return_value=False)),
+        patch(
+            "homeassistant.components.local_file.camera.mimetypes.guess_type",
+            Mock(return_value=(None, None)),
+        ),
+    ):
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.SETUP_ERROR
