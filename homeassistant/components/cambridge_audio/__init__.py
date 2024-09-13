20368fef8d51fdfc0a01c345468853e84b48a655
Noah Husby <32528627+noahhusby@users.noreply.github.com>
2024-09-13 10:57:39 -0400

    Add logs on disconnect/reconnect for Cambridge Audio (#125904)
    * Bump aiostreammagic to 2.3.0
    
    * Add logging on disconnect/reconnect for Cambridge Audio

--- homeassistant/components/cambridge_audio/__init__.py
+++ homeassistant/components/cambridge_audio/__init__.py
@@ -3,17 +3,21 @@
 from __future__ import annotations
 
 import asyncio
+import logging
 
 from aiostreammagic import StreamMagicClient
+from aiostreammagic.models import CallbackType
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import CONNECT_TIMEOUT, STREAM_MAGIC_EXCEPTIONS
 
 PLATFORMS: list[Platform] = [Platform.MEDIA_PLAYER]
+
+_LOGGER = logging.getLogger(__name__)
 
 type CambridgeAudioConfigEntry = ConfigEntry[StreamMagicClient]
 
@@ -24,6 +28,19 @@ async def async_setup_entry(
     """Set up Cambridge Audio integration from a config entry."""
 
     client = StreamMagicClient(entry.data[CONF_HOST])
+
+    @callback
+    async def _connection_update_callback(
+        _client: StreamMagicClient, _callback_type: CallbackType
+    ) -> None:
+        """Call when the device is notified of changes."""
+        if _callback_type == CallbackType.CONNECTION:
+            if _client.is_connected():
+                _LOGGER.warning("Reconnected to device at %s", entry.data[CONF_HOST])
+            else:
+                _LOGGER.warning("Disconnected from device at %s", entry.data[CONF_HOST])
+
+    await client.register_state_update_callbacks(_connection_update_callback)
 
     try:
         async with asyncio.timeout(CONNECT_TIMEOUT):
