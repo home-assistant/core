7a5ddb04125a81e7d2a70f9f9ae396ad988d47b9
Álvaro Fernández Rojas <noltari@gmail.com>
2024-09-13 19:26:55 +0200

    Add Airzone Cloud main zone mode select
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- tests/components/airzone_cloud/test_select.py
+++ tests/components/airzone_cloud/test_select.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 
 import pytest
 
-from homeassistant.components.select import DOMAIN as SELECT_DOMAIN
+from homeassistant.components.select import ATTR_OPTIONS, DOMAIN as SELECT_DOMAIN
 from homeassistant.const import ATTR_ENTITY_ID, ATTR_OPTION, SERVICE_SELECT_OPTION
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ServiceValidationError
@@ -22,8 +22,20 @@ async def test_airzone_create_selects(hass: HomeAssistant) -> None:
     state = hass.states.get("select.dormitorio_air_quality_mode")
     assert state.state == "auto"
 
+    state = hass.states.get("select.dormitorio_mode")
+    assert state is None
+
     state = hass.states.get("select.salon_air_quality_mode")
     assert state.state == "auto"
+
+    state = hass.states.get("select.salon_mode")
+    assert state.state == "cool"
+    assert state.attributes.get(ATTR_OPTIONS) == [
+        "cool",
+        "dry",
+        "fan",
+        "heat",
+    ]
 
 
 async def test_airzone_select_air_quality_mode(hass: HomeAssistant) -> None:
@@ -58,3 +70,37 @@ async def test_airzone_select_air_quality_mode(hass: HomeAssistant) -> None:
 
     state = hass.states.get("select.dormitorio_air_quality_mode")
     assert state.state == "off"
+
+
+async def test_airzone_select_mode(hass: HomeAssistant) -> None:
+    """Test select HVAC mode."""
+
+    await async_init_integration(hass)
+
+    with pytest.raises(ServiceValidationError):
+        await hass.services.async_call(
+            SELECT_DOMAIN,
+            SERVICE_SELECT_OPTION,
+            {
+                ATTR_ENTITY_ID: "select.salon_mode",
+                ATTR_OPTION: "Invalid",
+            },
+            blocking=True,
+        )
+
+    with patch(
+        "homeassistant.components.airzone_cloud.AirzoneCloudApi.api_patch_device",
+        return_value=None,
+    ):
+        await hass.services.async_call(
+            SELECT_DOMAIN,
+            SERVICE_SELECT_OPTION,
+            {
+                ATTR_ENTITY_ID: "select.salon_mode",
+                ATTR_OPTION: "heat",
+            },
+            blocking=True,
+        )
+
+    state = hass.states.get("select.salon_mode")
+    assert state.state == "heat"
