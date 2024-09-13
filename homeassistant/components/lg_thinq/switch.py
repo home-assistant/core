cd46dab4b51aaebb3f12101e8e17fa859def1aa4
jangwon.lee <jangwon.lee@lge.com>
2024-09-13 09:25:51 +0900

    Add mqtt to LG ThinQ integration

--- homeassistant/components/lg_thinq/switch.py
+++ homeassistant/components/lg_thinq/switch.py
@@ -61,7 +61,7 @@ async def async_setup_entry(
 ) -> None:
     """Set up an entry for switch platform."""
     entities: list[ThinQSwitchEntity] = []
-    for coordinator in entry.runtime_data.values():
+    for coordinator in entry.runtime_data.coordinators.values():
         if (
             descriptions := DEVICE_TYPE_SWITCH_MAP.get(
                 coordinator.api.device.device_type
