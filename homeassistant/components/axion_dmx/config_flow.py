bb46d99be179d159714d861b855242a36ad54463
Vrncanac <jana13eng@gmail.com>
2024-09-15 17:47:32 +0000

    Make it a selector with a translation key for light type, and inline _validate_and_authenticate function

--- homeassistant/components/axion_dmx/config_flow.py
+++ homeassistant/components/axion_dmx/config_flow.py
@@ -10,6 +10,7 @@ import voluptuous as vol
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_HOST, CONF_PASSWORD
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers import selector
 
 from .const import _LOGGER, CONF_CHANNEL, CONF_LIGHT_TYPE, DOMAIN
 
@@ -18,8 +19,21 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
         vol.Required(CONF_HOST): str,
         vol.Required(CONF_PASSWORD): str,
         vol.Required(CONF_CHANNEL): vol.All(vol.Coerce(int), vol.Range(min=1)),
-        vol.Required(CONF_LIGHT_TYPE): vol.In(
-            ["White", "Tunable White", "RGB", "RGBW", "RGBWW"]
+        vol.Required(CONF_LIGHT_TYPE): selector.SelectSelector(
+            selector.SelectSelectorConfig(
+                options=[
+                    selector.SelectOptionDict(value="White", label="White"),
+                    selector.SelectOptionDict(
+                        value="Tunable White", label="Tunable White"
+                    ),
+                    selector.SelectOptionDict(value="RGB", label="RGB"),
+                    selector.SelectOptionDict(value="RGBW", label="RGBW"),
+                    selector.SelectOptionDict(value="RGBWW", label="RGBWW"),
+                ],
+                translation_key="light_type",
+                multiple=False,
+                mode=selector.SelectSelectorMode.DROPDOWN,
+            )
         ),
     }
 )
@@ -36,9 +50,11 @@ class AxionConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         errors: dict[str, str] = {}
         if user_input is not None:
+            api = AxionDmxApi(user_input[CONF_HOST], user_input[CONF_PASSWORD])
             # Validate the user input and authenticate
             try:
-                await self._validate_and_authenticate(user_input)
+                if not await api.authenticate():
+                    raise InvalidAuth  # noqa: TRY301
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except InvalidAuth:
@@ -56,13 +72,6 @@ class AxionConfigFlow(ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
-
-    async def _validate_and_authenticate(self, user_input: dict[str, Any]) -> None:
-        """Validate and authenticate the Axion DMX API."""
-        api = AxionDmxApi(user_input[CONF_HOST], user_input[CONF_PASSWORD])
-
-        if not await api.authenticate():
-            raise InvalidAuth
 
 
 class CannotConnect(HomeAssistantError):
