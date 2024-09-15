90f8eb07002d0487d4bcdfdb7ba366381c7e216f
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2024-09-15 17:30:29 +0000

    feat: test connection

--- homeassistant/components/ezbeq/config_flow.py
+++ homeassistant/components/ezbeq/config_flow.py
@@ -3,7 +3,9 @@
 import logging
 from typing import Any
 
+from httpx import HTTPStatusError, RequestError
 from pyezbeq.consts import DEFAULT_PORT, DISCOVERY_ADDRESS
+from pyezbeq.ezbeq import EzbeqClient
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry, ConfigFlow, ConfigFlowResult
@@ -47,17 +49,16 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 # Shared schema
-SHARED_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_EDITION_SENSOR): selector.EntitySelector(
-            selector.EntitySelectorConfig(domain="sensor")
-        ),
-        vol.Required(CONF_TITLE_SENSOR): selector.EntitySelector(
-            selector.EntitySelectorConfig(domain="sensor")
-        ),
-        vol.Optional(CONF_PREFERRED_AUTHOR): str,
-    }
-)
+SHARED_DATA_SCHEMA = {
+    vol.Required(CONF_EDITION_SENSOR): selector.EntitySelector(
+        selector.EntitySelectorConfig(domain="sensor")
+    ),
+    vol.Required(CONF_TITLE_SENSOR): selector.EntitySelector(
+        selector.EntitySelectorConfig(domain="sensor")
+    ),
+    vol.Optional(CONF_PREFERRED_AUTHOR): str,
+}
+
 
 # Plex schema
 STEP_PLEX_DATA_SCHEMA = vol.Schema(
@@ -75,7 +76,7 @@ STEP_PLEX_DATA_SCHEMA = vol.Schema(
             selector.EntitySelectorConfig(domain="sensor")
         ),
     }
-)
+).extend(SHARED_DATA_SCHEMA)
 
 # Jellyfin schema
 STEP_JF_DATA_SCHEMA = vol.Schema(
@@ -93,7 +94,7 @@ STEP_JF_DATA_SCHEMA = vol.Schema(
             selector.EntitySelectorConfig(domain="sensor")
         ),
     }
-)
+).extend(SHARED_DATA_SCHEMA)
 
 
 class EzBEQConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -106,18 +107,43 @@ class EzBEQConfigFlow(ConfigFlow, domain=DOMAIN):
         """Initialize the config flow."""
         self.ezbeq_data: dict[str, Any] = {}
 
+    async def test_connection(self) -> bool:
+        """Test if we can connect to the ezbeq server."""
+        client = EzbeqClient(
+            host=self.ezbeq_data[CONF_HOST], port=self.ezbeq_data[CONF_PORT]
+        )
+        try:
+            await client.get_version()
+        except (HTTPStatusError, RequestError) as e:
+            _LOGGER.error("Error connecting to ezbeq: %s", e)
+            return False
+        finally:
+            await client.client.aclose()
+
+        return True
+
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
+        errors: dict[str, str] = {}
         if user_input is None:
-            schema = STEP_USER_DATA_SCHEMA
-            return self.async_show_form(step_id="user", data_schema=schema)
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
 
         self.ezbeq_data.update(user_input)
         # abort on same host
         await self.async_set_unique_id(CONF_HOST)
         self._abort_if_unique_id_configured()
+
+        # make sure the connection is working
+        if not await self.test_connection():
+            _LOGGER.error("CannotConnect error caught")
+            errors["base"] = "cannot_connect"
+            return self.async_show_form(
+                step_id="user", errors=errors, data_schema=STEP_USER_DATA_SCHEMA
+            )
 
         return await self.async_step_entities()
 
@@ -127,11 +153,13 @@ class EzBEQConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle the entities step."""
         if user_input is None:
             if self.ezbeq_data[CONF_SOURCE_TYPE] == "Plex":
-                schema = SHARED_DATA_SCHEMA.extend(STEP_PLEX_DATA_SCHEMA)
-                return self.async_show_form(step_id="entities", data_schema=schema)
+                return self.async_show_form(
+                    step_id="entities", data_schema=STEP_PLEX_DATA_SCHEMA
+                )
             # Jellyfin
-            schema = SHARED_DATA_SCHEMA.extend(STEP_JF_DATA_SCHEMA)
-            return self.async_show_form(step_id="entities", data_schema=schema)
+            return self.async_show_form(
+                step_id="entities", data_schema=STEP_JF_DATA_SCHEMA
+            )
 
         self.ezbeq_data.update(user_input)
 
