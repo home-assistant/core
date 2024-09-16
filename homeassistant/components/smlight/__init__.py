e9364f4c3aa6c935015ac24327a3fc4b34f78f05
TimL <tl@smlight.tech>
2024-09-16 22:14:15 +1000

    Add update platform for Smlight integration (#125943)
    * Create update coordinator for update entities
    
    * fix type errors
    
    * update info fixture with zigbee version
    
    * Add fixtures for Firmware objects
    
    * mock get_firmware_version function
    
    * Add update platform for Smlight integration
    
    * Add strings for update platform
    
    * Add tests for update platform
    
    * add snapshot for update tests
    
    * Split out base coordinator
    
    * Update homeassistant/components/smlight/strings.json
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>
    
    * overwrite coordinator types
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/smlight/__init__.py
+++ homeassistant/components/smlight/__init__.py
@@ -2,29 +2,55 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
+
+from pysmlight import Api2
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .coordinator import SmDataUpdateCoordinator
+from .coordinator import SmDataUpdateCoordinator, SmFirmwareUpdateCoordinator
 
 PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
     Platform.BUTTON,
     Platform.SENSOR,
     Platform.SWITCH,
+    Platform.UPDATE,
 ]
-type SmConfigEntry = ConfigEntry[SmDataUpdateCoordinator]
+
+
+@dataclass(kw_only=True)
+class SmlightData:
+    """Coordinator data class."""
+
+    data: SmDataUpdateCoordinator
+    firmware: SmFirmwareUpdateCoordinator
+
+
+type SmConfigEntry = ConfigEntry[SmlightData]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: SmConfigEntry) -> bool:
     """Set up SMLIGHT Zigbee from a config entry."""
-    coordinator = SmDataUpdateCoordinator(hass, entry.data[CONF_HOST])
-    await coordinator.async_config_entry_first_refresh()
-    entry.runtime_data = coordinator
+    client = Api2(host=entry.data[CONF_HOST], session=async_get_clientsession(hass))
+    entry.async_create_background_task(hass, client.sse.client(), "smlight-sse-client")
+
+    data_coordinator = SmDataUpdateCoordinator(hass, entry.data[CONF_HOST], client)
+    firmware_coordinator = SmFirmwareUpdateCoordinator(
+        hass, entry.data[CONF_HOST], client
+    )
+
+    await data_coordinator.async_config_entry_first_refresh()
+    await firmware_coordinator.async_config_entry_first_refresh()
+
+    entry.runtime_data = SmlightData(
+        data=data_coordinator, firmware=firmware_coordinator
+    )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
     return True
 
 
