328f11c1d9ca81d032d1036c35e04585e906645f
epenet <6771947+epenet@users.noreply.github.com>
2024-09-16 11:28:42 +0200

    Force root import of references from other components (#125816)
    * Force root import of references from other components
    
    * Improve
    
    * Adjust
    
    * Tweak exceptions
    
    * Another
    
    * Another
    
    * Another
    
    * Another
    
    * Another
    
    * Another
    
    * Another
    
    * Another
    
    * Another
    
    * Another
    
    * Another
    
    * Another
    
    * Adjust
    
    * More
    
    * Ignore violations in test
    
    * Improve

--- tests/components/voip/test_voip.py
+++ tests/components/voip/test_voip.py
@@ -13,6 +13,8 @@ from voip_utils import CallInfo
 
 from homeassistant.components import assist_pipeline, assist_satellite, tts, voip
 from homeassistant.components.assist_satellite import AssistSatelliteEntity
+
+# pylint: disable-next=hass-component-root-import
 from homeassistant.components.assist_satellite.entity import AssistSatelliteState
 from homeassistant.components.voip import HassVoipDatagramProtocol
 from homeassistant.components.voip.assist_satellite import Tones, VoipAssistSatellite
