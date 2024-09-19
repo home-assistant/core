732c5275fa34aa1bfa0f9e3247f89b08b6e97b8b
farmio <farmio@alphart.net>
2024-09-20 00:27:34 +0200

    Optionally ignore telegram sending order in tests
    because we can't know which platform initialises first

--- tests/components/knx/test_light.py
+++ tests/components/knx/test_light.py
@@ -1230,8 +1230,10 @@ async def test_light_ui_load(
     """Test loading a light from storage."""
     await knx.setup_integration({})
 
-    await knx.assert_read("1/0/21", response=True)  # test light
-    await knx.assert_read("1/0/45", response=True)  # test switch - unrelated here
+    await knx.assert_read("1/0/21", response=True, ignore_order=True)
+    # unrelated switch in config store
+    await knx.assert_read("1/0/45", response=True, ignore_order=True)
+
     state = hass.states.get("light.test")
     assert state.state is STATE_ON
 
