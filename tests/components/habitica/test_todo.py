bdf805b8424019a85216a5860d807931b457e965
tr4nt0r <manni@zapto.de>
2024-09-21 23:47:31 +0000

    test move todo

--- tests/components/habitica/test_todo.py
+++ tests/components/habitica/test_todo.py
@@ -26,6 +26,7 @@ from .conftest import assert_mock_called_with
 
 from tests.common import MockConfigEntry, snapshot_platform
 from tests.test_util.aiohttp import AiohttpClientMocker
+from tests.typing import WebSocketGenerator
 
 
 @pytest.fixture(autouse=True)
@@ -90,13 +91,13 @@ async def test_todo_items(
     assert result == snapshot
 
 
-async def test_score_up_todo_item(
+async def test_complete_todo_item(
     hass: HomeAssistant,
     config_entry: MockConfigEntry,
     mock_habitica: AiohttpClientMocker,
 ) -> None:
     """Test completing an item on the todo list."""
-    uid = "77777777-7777-7777-7777-777777777777"
+    uid = "88de7cd9-af2b-49ce-9afd-bf941d87336b"
     config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
@@ -124,13 +125,13 @@ async def test_score_up_todo_item(
     )
 
 
-async def test_score_down_todo_item(
+async def test_uncomplete_todo_item(
     hass: HomeAssistant,
     config_entry: MockConfigEntry,
     mock_habitica: AiohttpClientMocker,
 ) -> None:
     """Test uncompleting an item on the todo list."""
-    uid = "10101010-1010-1010-1010-101010101010"
+    uid = "162f0bbe-a097-4a06-b4f4-8fbeed85d2ba"
     config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
@@ -164,7 +165,7 @@ async def test_update_todo_item(
     mock_habitica: AiohttpClientMocker,
 ) -> None:
     """Test update details of a item on the todo list."""
-    uid = "77777777-7777-7777-7777-777777777777"
+    uid = "88de7cd9-af2b-49ce-9afd-bf941d87336b"
     config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
@@ -248,7 +249,7 @@ async def test_delete_todo_item(
     mock_habitica: AiohttpClientMocker,
 ) -> None:
     """Test deleting a todo item from the todo list."""
-    uid = "10101010-1010-1010-1010-101010101010"
+    uid = "2f6fcabc-f670-4ec3-ba65-817e8deea490"
     config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
@@ -272,13 +273,42 @@ async def test_delete_todo_item(
     )
 
 
+async def test_delete_completed_todo_items(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_habitica: AiohttpClientMocker,
+) -> None:
+    """Test deleting completed todo items from the todo list."""
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    mock_habitica.post(
+        f"{DEFAULT_URL}/api/v3/tasks/clearCompletedTodos",
+        json={"data": {}, "success": True},
+    )
+    await hass.services.async_call(
+        TODO_DOMAIN,
+        TodoServices.REMOVE_COMPLETED_ITEMS,
+        {},
+        target={ATTR_ENTITY_ID: "todo.test_user_to_do_s"},
+        blocking=True,
+    )
+
+    assert_mock_called_with(
+        mock_habitica, "post", f"{DEFAULT_URL}/api/v3/tasks/clearCompletedTodos"
+    )
+
+
 @pytest.mark.usefixtures("mock_habitica")
 async def test_get_todo_items(
     hass: HomeAssistant,
     config_entry: MockConfigEntry,
     snapshot: SnapshotAssertion,
 ) -> None:
-    """Test deleting completed todo items from the todo list."""
+    """Test get todo items from the todo list."""
     config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
@@ -295,3 +325,55 @@ async def test_get_todo_items(
     )
 
     assert result == snapshot
+
+
+async def test_move_todo_item(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_habitica: AiohttpClientMocker,
+    snapshot: SnapshotAssertion,
+    hass_ws_client: WebSocketGenerator,
+) -> None:
+    """Test get todo items from the todo list."""
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    for pos in (0, 1):
+        mock_habitica.post(
+            f"{DEFAULT_URL}/api/v3/tasks/1aa3137e-ef72-4d1f-91ee-41933602f438/move/to/{pos}",
+            json={"data": {}, "success": True},
+        )
+
+    client = await hass_ws_client()
+    # move to second position
+    data = {
+        "id": id,
+        "type": "todo/item/move",
+        "entity_id": "todo.test_user_to_do_s",
+        "uid": "1aa3137e-ef72-4d1f-91ee-41933602f438",
+        "previous_uid": "88de7cd9-af2b-49ce-9afd-bf941d87336b",
+    }
+    await client.send_json_auto_id(data)
+    resp = await client.receive_json()
+    assert resp.get("success")
+
+    # move to top position
+    data = {
+        "id": id,
+        "type": "todo/item/move",
+        "entity_id": "todo.test_user_to_do_s",
+        "uid": "1aa3137e-ef72-4d1f-91ee-41933602f438",
+    }
+    await client.send_json_auto_id(data)
+    resp = await client.receive_json()
+    assert resp.get("success")
+
+    for pos in (0, 1):
+        assert_mock_called_with(
+            mock_habitica,
+            "post",
+            f"{DEFAULT_URL}/api/v3/tasks/1aa3137e-ef72-4d1f-91ee-41933602f438/move/to/{pos}",
+        )
