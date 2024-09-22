3a096f17d16b63aeb53fa9b7253ef4fbb3b492c4
Joostlek <joostlek@outlook.com>
2024-09-22 20:56:58 +0200

    Add NYT Games integration

---
+++ tests/components/nyt_games/test_sensor.py
@@ -0,0 +1,25 @@
+"""Tests for the NYT Games sensor platform."""
+
+from unittest.mock import AsyncMock
+
+from syrupy import SnapshotAssertion
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from . import setup_integration
+
+from tests.common import MockConfigEntry, snapshot_platform
+
+
+async def test_all_entities(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    mock_nyt_games_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test all entities."""
+    await setup_integration(hass, mock_config_entry)
+
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
