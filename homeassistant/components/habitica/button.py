d4199a1d770402a85bae2d145a53bf5c66a68959
tr4nt0r <manni@zapto.de>
2024-09-22 16:57:28 +0000

    Remove old code

--- homeassistant/components/habitica/button.py
+++ homeassistant/components/habitica/button.py
@@ -298,25 +298,6 @@ async def async_setup_entry(
     async_add_entities(
         HabiticaButton(coordinator, description) for description in BUTTON_DESCRIPTIONS
     )
-    if coordinator.data.user["stats"]["lvl"] >= 10:
-        if coordinator.data.user["stats"]["class"] == MAGE:
-            async_add_entities(
-                HabiticaButton(coordinator, description) for description in MAGE_SKILLS
-            )
-        if coordinator.data.user["stats"]["class"] == WARRIOR:
-            async_add_entities(
-                HabiticaButton(coordinator, description)
-                for description in WARRIOR_SKILLS
-            )
-        if coordinator.data.user["stats"]["class"] == ROGUE:
-            async_add_entities(
-                HabiticaButton(coordinator, description) for description in ROGUE_SKILLS
-            )
-        if coordinator.data.user["stats"]["class"] == HEALER:
-            async_add_entities(
-                HabiticaButton(coordinator, description)
-                for description in HEALER_SKILLS
-            )
 
 
 class HabiticaButton(HabiticaBase, ButtonEntity):
