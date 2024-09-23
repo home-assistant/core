aa1cf9ae8a93e51074913e6f7e75d0b8cc331f3d
epenet <6771947+epenet@users.noreply.github.com>
2024-09-23 15:56:59 +0200

    Move tolo coordinator to separate module (#126550)
    * Move tolo coordinator to separate module
    
    * Adjust tests

--- homeassistant/components/tolo/switch.py
+++ homeassistant/components/tolo/switch.py
@@ -13,8 +13,8 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from . import ToloSaunaUpdateCoordinator
 from .const import DOMAIN
+from .coordinator import ToloSaunaUpdateCoordinator
 from .entity import ToloSaunaCoordinatorEntity
 
 
