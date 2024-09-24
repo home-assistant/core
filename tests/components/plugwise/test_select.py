ef57fec647fcddbd8630cceec29f045b8725ae26
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-09-24 10:44:46 +0200

    Improve docstring as suggested, correct mock-device

--- tests/components/plugwise/test_select.py
+++ tests/components/plugwise/test_select.py
@@ -80,7 +80,7 @@ async def test_adam_select_regulation_mode(
 
 
 async def test_legacy_anna_select_entities(
-    hass: HomeAssistant, mock_smile_adam: MagicMock, init_integration: MockConfigEntry
+    hass: HomeAssistant, mock_smile_legacy_anna: MagicMock, init_integration: MockConfigEntry
 ) -> None:
-    """Test a legacy thermostat select."""
+    """Test not creating a select-entity for a legacy Anna without a thermostat-schedule."""
     assert not hass.states.get("select.anna_thermostat_schedule")
