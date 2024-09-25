1e7c19cc3c833b1d1b1c5ea0a6aeb5a1fde4b7b9
epenet <6771947+epenet@users.noreply.github.com>
2024-09-25 08:37:04 +0000

    Prevent session scope fixtures in component tests

--- pylint/plugins/hass_decorator.py
+++ pylint/plugins/hass_decorator.py
@@ -18,14 +18,56 @@ class HassDecoratorChecker(BaseChecker):
             "hass-async-callback-decorator",
             "Used when a coroutine function has an invalid @callback decorator",
         ),
+        "W7472": (
+            "Fixture %s is invalid here, please use %s",
+            "hass-pytest-fixture-decorator",
+            "Used when a pytest fixture is invalid",
+        ),
     }
+
+    def _get_pytest_fixture_node(self, node: nodes.FunctionDef) -> nodes.Call | None:
+        for decorator in node.decorators.nodes:
+            if not isinstance(decorator, nodes.Call):
+                pass
+            if decorator.func.as_string() == "pytest.fixture":
+                return decorator
+
+        return None
+
+    def _check_pytest_fixture(
+        self, node: nodes.FunctionDef, decoratornames: set[str]
+    ) -> None:
+        if (
+            "_pytest.fixtures.FixtureFunctionMarker" not in decoratornames
+            or (root_name := node.root().name) == "tests.components.conftest"
+            or not root_name.startswith("tests.components.")
+            or (decorator := self._get_pytest_fixture_node(node)) is None
+        ):
+            return
+
+        for keyword in decorator.keywords:
+            if (
+                keyword.arg == "scope"
+                and isinstance(keyword.value, nodes.Const)
+                and keyword.value.value == "session"
+            ):
+                self.add_message(
+                    "hass-pytest-fixture-decorator",
+                    node=node,
+                    args=(f"scope `{keyword.value.value}`", "`package` or lower"),
+                )
 
     def visit_asyncfunctiondef(self, node: nodes.AsyncFunctionDef) -> None:
         """Apply checks on an AsyncFunctionDef node."""
-        if (
-            decoratornames := node.decoratornames()
-        ) and "homeassistant.core.callback" in decoratornames:
-            self.add_message("hass-async-callback-decorator", node=node)
+        if decoratornames := node.decoratornames():
+            if "homeassistant.core.callback" in decoratornames:
+                self.add_message("hass-async-callback-decorator", node=node)
+            self._check_pytest_fixture(node, decoratornames)
+
+    def visit_functiondef(self, node: nodes.FunctionDef) -> None:
+        """Apply checks on an AsyncFunctionDef node."""
+        if decoratornames := node.decoratornames():
+            self._check_pytest_fixture(node, decoratornames)
 
 
 def register(linter: PyLinter) -> None:
--- tests/pylint/test_decorator.py
+++ tests/pylint/test_decorator.py
@@ -8,6 +8,7 @@ from pylint.interfaces import UNDEFINED
 from pylint.testutils import MessageTest
 from pylint.testutils.unittest_linter import UnittestLinter
 from pylint.utils.ast_walker import ASTWalker
+import pytest
 
 from . import assert_adds_messages, assert_no_messages
 
@@ -59,6 +60,93 @@ def test_bad_callback(linter: UnittestLinter, decorator_checker: BaseChecker) ->
             col_offset=0,
             end_line=5,
             end_col_offset=15,
+        ),
+    ):
+        walker.walk(root_node)
+
+
+@pytest.mark.parametrize(
+    ("scope", "path"),
+    [
+        ("function", "tests.test_bootstrap"),
+        ("class", "tests.test_bootstrap"),
+        ("module", "tests.test_bootstrap"),
+        ("package", "tests.test_bootstrap"),
+        ("session", "tests.test_bootstrap"),
+        ("function", "tests.components.conftest"),
+        ("class", "tests.components.conftest"),
+        ("module", "tests.components.conftest"),
+        ("package", "tests.components.conftest"),
+        ("session", "tests.components.conftest"),
+        ("function", "tests.components.pylint_test"),
+        ("class", "tests.components.pylint_test"),
+        ("module", "tests.components.pylint_test"),
+        ("package", "tests.components.pylint_test"),
+    ],
+)
+def test_good_fixture(
+    linter: UnittestLinter, decorator_checker: BaseChecker, scope: str, path: str
+) -> None:
+    """Test good `@pytest.fixture` decorator."""
+    code = f"""
+    import pytest
+
+    @pytest.fixture
+    def setup(
+        arg1, arg2
+    ):
+        pass
+
+    @pytest.fixture(scope="{scope}")
+    def setup_session(
+        arg1, arg2
+    ):
+        pass
+    """
+
+    root_node = astroid.parse(code, path)
+    walker = ASTWalker(linter)
+    walker.add_checker(decorator_checker)
+
+    with assert_no_messages(linter):
+        walker.walk(root_node)
+
+
+def test_bad_fixture_scope(
+    linter: UnittestLinter, decorator_checker: BaseChecker
+) -> None:
+    """Test bad `@pytest.fixture` decorator."""
+    code = """
+    import pytest
+
+    @pytest.fixture
+    def setup(
+        arg1, arg2
+    ):
+        pass
+
+    @pytest.fixture(scope="session")
+    def setup_session(
+        arg1, arg2
+    ):
+        pass
+    """
+
+    root_node = astroid.parse(code, "tests.components.pylint_test")
+    walker = ASTWalker(linter)
+    walker.add_checker(decorator_checker)
+
+    with assert_adds_messages(
+        linter,
+        MessageTest(
+            msg_id="hass-pytest-fixture-decorator",
+            line=11,
+            node=root_node.body[2],
+            args=("scope `session`", "`package` or lower"),
+            confidence=UNDEFINED,
+            col_offset=0,
+            end_line=11,
+            end_col_offset=17,
         ),
     ):
         walker.walk(root_node)
