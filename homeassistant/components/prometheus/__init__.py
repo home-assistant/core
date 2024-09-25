06ab90cb415d8e10f6964a2e7e8c7da68a73e0d9
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-25 12:19:13 -0700

    Rolling back some logic changes to focus solely on tests

--- homeassistant/components/prometheus/__init__.py
+++ homeassistant/components/prometheus/__init__.py
@@ -42,7 +42,6 @@ from homeassistant.components.humidifier import ATTR_AVAILABLE_MODES, ATTR_HUMID
 from homeassistant.components.light import ATTR_BRIGHTNESS
 from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import (
-    ATTR_AREA_ID,
     ATTR_BATTERY_LEVEL,
     ATTR_DEVICE_CLASS,
     ATTR_FRIENDLY_NAME,
@@ -307,22 +306,6 @@ class PrometheusMetrics:
             except (ValueError, TypeError):
                 pass
 
-    @staticmethod
-    def _get_label_keys(
-        extra_labels: list[str] | None = None,
-    ) -> list[str]:
-        labels = [
-            "entity",
-            "friendly_name",
-            "domain",
-            "area",
-            "object_id",
-            "device_class",
-        ]
-        if extra_labels is not None:
-            labels.extend(extra_labels)
-        return list(labels)
-
     def _metric[_MetricBaseT: MetricWrapperBase](
         self,
         metric: str,
@@ -330,7 +313,9 @@ class PrometheusMetrics:
         documentation: str,
         extra_labels: list[str] | None = None,
     ) -> _MetricBaseT:
-        labels = self._get_label_keys(extra_labels=extra_labels)
+        labels = ["entity", "friendly_name", "domain"]
+        if extra_labels is not None:
+            labels.extend(extra_labels)
 
         try:
             return cast(_MetricBaseT, self._metrics[metric])
@@ -376,9 +361,6 @@ class PrometheusMetrics:
             "entity": state.entity_id,
             "domain": state.domain,
             "friendly_name": state.attributes.get(ATTR_FRIENDLY_NAME),
-            "area": state.attributes.get(ATTR_AREA_ID) or "",
-            "object_id": state.object_id,
-            "device_class": state.attributes.get(ATTR_DEVICE_CLASS) or "",
         }
 
     def _battery(self, state: State) -> None:
