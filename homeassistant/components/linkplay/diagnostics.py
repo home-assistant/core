1319f9d3fce8c04fe1ca86ebc5c4ed24d7192f73
silamon <slam@velleman.eu>
2024-09-25 17:08:19 +0000

    Diagnostics

--- homeassistant/components/linkplay/diagnostics.py
+++ homeassistant/components/linkplay/diagnostics.py
@@ -2,16 +2,14 @@
 
 from __future__ import annotations
 
-from dataclasses import asdict
 from typing import Any
 
 from homeassistant.components.diagnostics import async_redact_data
-from homeassistant.const import CONF_HOST, CONF_MAC
 from homeassistant.core import HomeAssistant
 
 from . import LinkPlayConfigEntry
 
-TO_REDACT = {CONF_HOST, CONF_MAC}
+TO_REDACT = {"MAC"}
 
 
 async def async_get_config_entry_diagnostics(
@@ -21,7 +19,7 @@ async def async_get_config_entry_diagnostics(
     data = entry.runtime_data
     return async_redact_data(
         {
-            "device_info": asdict(data),
+            "device_info": data.bridge.to_dict(),
             "config_entry_data": entry.data,
         },
         TO_REDACT,
