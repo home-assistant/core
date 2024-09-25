760d7f529a3021fc2fe7c64922475fbee7853efb
David Bonnes <zxdavb@bonnes.me>
2024-09-25 15:47:34 +0100

    undo undo

--- tests/components/evohome/conftest.py
+++ tests/components/evohome/conftest.py
@@ -100,14 +100,6 @@ def mock_get_factory(install: str) -> Callable:
     return mock_get
 
 
-async def block_request(
-    self: Broker, method: HTTPMethod, url: str, **kwargs: Any
-) -> None:
-    """Fail if the code attempts any actual I/O via aiohttp."""
-
-    pytest.fail(f"Unexpected request: {method} {url}")
-
-
 @pytest.fixture
 def config() -> dict[str, str]:
     "Return a default/minimal configuration."
@@ -117,8 +109,6 @@ def config() -> dict[str, str]:
     }
 
 
-@patch("evohomeasync.broker.Broker._make_request", block_request)
-@patch("evohomeasync2.broker.Broker._client", block_request)
 async def setup_evohome(
     hass: HomeAssistant,
     test_config: dict[str, str],
