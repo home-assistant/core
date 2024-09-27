b992842a643685609443b166133143b122d9ec7b
Brett <brett.whynot@gmail.com>
2024-09-27 11:30:51 +0000

    Fix history

--- homeassistant/components/teslemetry/entity.py
+++ homeassistant/components/teslemetry/entity.py
@@ -174,10 +174,12 @@ class TeslemetryEnergyHistoryEntity(TeslemetryEntity):
         key: str,
     ) -> None:
         """Initialize common aspects of a Teslemetry Energy Site Info entity."""
+
+        self.api = data.api
         self._attr_unique_id = f"{data.id}-{key}"
         self._attr_device_info = data.device
 
-        super().__init__(data.history_coordinator, data.api, key)
+        super().__init__(data.history_coordinator, key)
 
 
 class TeslemetryWallConnectorEntity(TeslemetryEntity):
