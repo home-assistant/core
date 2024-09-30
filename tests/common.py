d335555bafda08fbfad28f94c89ebcd3251f22b0
epenet <6771947+epenet@users.noreply.github.com>
2024-09-30 16:38:34 +0200

    Use start_reauth_flow helper in ezviz and netatmo tests (#127100)
    * Use start_reauth_flow helper in netatmo tests
    
    * Use start_reauth_flow helper in ezviz tests

--- tests/common.py
+++ tests/common.py
@@ -1063,21 +1063,31 @@ class MockConfigEntry(config_entries.ConfigEntry):
         context: dict[str, Any] | None = None,
         data: dict[str, Any] | None = None,
     ) -> ConfigFlowResult:
-        """Start a reauthentication flow for a config entry.
+        """Start a reauthentication flow."""
+        return await start_reauth_flow(hass, self, context, data)
 
-        This helper method should be aligned with `ConfigEntry._async_init_reauth`.
-        """
-        return await hass.config_entries.flow.async_init(
-            self.domain,
-            context={
-                "source": config_entries.SOURCE_REAUTH,
-                "entry_id": self.entry_id,
-                "title_placeholders": {"name": self.title},
-                "unique_id": self.unique_id,
-            }
-            | (context or {}),
-            data=self.data | (data or {}),
-        )
+
+async def start_reauth_flow(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    context: dict[str, Any] | None = None,
+    data: dict[str, Any] | None = None,
+) -> ConfigFlowResult:
+    """Start a reauthentication flow for a config entry.
+
+    This helper method should be aligned with `ConfigEntry._async_init_reauth`.
+    """
+    return await hass.config_entries.flow.async_init(
+        entry.domain,
+        context={
+            "source": config_entries.SOURCE_REAUTH,
+            "entry_id": entry.entry_id,
+            "title_placeholders": {"name": entry.title},
+            "unique_id": entry.unique_id,
+        }
+        | (context or {}),
+        data=entry.data | (data or {}),
+    )
 
 
 def patch_yaml_files(files_dict, endswith=True):
