cd499f059a1449e6bf84b18fab16350947d22c14
mj23000 <markusjacobsen@hotmail.com>
2024-10-02 17:54:56 +0200

    Add button events

---
+++ homeassistant/components/bang_olufsen/event.py
@@ -0,0 +1,88 @@
+"""Event entities for the Bang & Olufsen integration."""
+
+from __future__ import annotations
+
+from mozart_api.mozart_client import MozartClient
+
+from homeassistant.components.event import EventDeviceClass, EventEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from . import BangOlufsenData
+from .const import (
+    CONNECTION_STATUS,
+    DEVICE_BUTTON_EVENTS,
+    DEVICE_BUTTONS,
+    DOMAIN,
+    WebsocketNotification,
+)
+from .entity import BangOlufsenEntity
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Sensor entities from config entry."""
+    data: BangOlufsenData = hass.data[DOMAIN][config_entry.entry_id]
+
+    # Add physical "buttons"
+    entities: list[EventEntity] = [
+        BangOlufsenButtonEvent(config_entry, data.client, button_type)
+        for button_type in DEVICE_BUTTONS
+    ]
+
+    async_add_entities(new_entities=entities)
+
+
+class BangOlufsenEvent(BangOlufsenEntity, EventEntity):
+    """Base Event class."""
+
+    _attr_entity_registry_enabled_default = False
+
+    @callback
+    def _async_handle_event(self, event: str) -> None:
+        """Handle event."""
+        self._trigger_event(event)
+        self.async_write_ha_state()
+
+
+class BangOlufsenButtonEvent(BangOlufsenEvent):
+    """Event class for Button events."""
+
+    _attr_device_class = EventDeviceClass.BUTTON
+    _attr_event_types = DEVICE_BUTTON_EVENTS
+    _attr_icon = "mdi:gesture-tap-button"
+
+    def __init__(
+        self, entry: ConfigEntry, client: MozartClient, button_type: str
+    ) -> None:
+        """Initialize Button."""
+        super().__init__(entry, client)
+
+        self._attr_unique_id = f"{self._unique_id}_{button_type}"
+
+        # Make the native button name Home Assistant compatible
+        self._attr_translation_key = button_type.lower()
+
+        self._button_type = button_type
+
+    async def async_added_to_hass(self) -> None:
+        """Listen to WebSocket button events."""
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass,
+                f"{self._unique_id}_{CONNECTION_STATUS}",
+                self._async_update_connection_state,
+            )
+        )
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass,
+                f"{self._unique_id}_{WebsocketNotification.BUTTON}_{self._button_type}",
+                self._async_handle_event,
+            )
+        )
