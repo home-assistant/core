a87aa6458a1683e1959d5b49d10aa38dcd0d5ac3
epenet <6771947+epenet@users.noreply.github.com>
2024-10-02 13:12:46 +0000

    Use _get_reconfigure_entry in smhi

--- homeassistant/components/smhi/config_flow.py
+++ homeassistant/components/smhi/config_flow.py
@@ -40,7 +40,7 @@ class SmhiFlowHandler(ConfigFlow, domain=DOMAIN):
     """Config flow for SMHI component."""
 
     VERSION = 2
-    config_entry: ConfigEntry | None
+    config_entry: ConfigEntry
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -86,9 +86,7 @@ class SmhiFlowHandler(ConfigFlow, domain=DOMAIN):
         self, entry_data: Mapping[str, Any]
     ) -> ConfigFlowResult:
         """Handle a reconfiguration flow initialized by the user."""
-        self.config_entry = self.hass.config_entries.async_get_entry(
-            self.context["entry_id"]
-        )
+        self.config_entry = self._get_reconfigure_entry()
         return await self.async_step_reconfigure_confirm()
 
     async def async_step_reconfigure_confirm(
@@ -96,7 +94,6 @@ class SmhiFlowHandler(ConfigFlow, domain=DOMAIN):
     ) -> ConfigFlowResult:
         """Handle a reconfiguration flow initialized by the user."""
         errors: dict[str, str] = {}
-        assert self.config_entry
 
         if user_input is not None:
             lat: float = user_input[CONF_LOCATION][CONF_LATITUDE]
