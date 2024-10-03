22f23635c7e37775d697716d112f416b31c8e6ba
epenet <6771947+epenet@users.noreply.github.com>
2024-10-03 12:23:20 +0000

    Adjust

--- homeassistant/components/blue_current/config_flow.py
+++ homeassistant/components/blue_current/config_flow.py
@@ -14,12 +14,7 @@ from bluecurrent_api.exceptions import (
 )
 import voluptuous as vol
 
-from homeassistant.config_entries import (
-    SOURCE_REAUTH,
-    ConfigEntry,
-    ConfigFlow,
-    ConfigFlowResult,
-)
+from homeassistant.config_entries import SOURCE_REAUTH, ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_API_TOKEN
 
 from .const import DOMAIN, LOGGER
@@ -31,7 +26,6 @@ class BlueCurrentConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle the config flow for Blue Current."""
 
     VERSION = 1
-    _reauth_entry: ConfigEntry
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -63,9 +57,10 @@ class BlueCurrentConfigFlow(ConfigFlow, domain=DOMAIN):
                     self._abort_if_unique_id_configured()
                     return self.async_create_entry(title=email, data=user_input)
 
-                if self._reauth_entry.unique_id == customer_id:
+                reauth_entry = self._get_reauth_entry()
+                if reauth_entry.unique_id == customer_id:
                     return self.async_update_reload_and_abort(
-                        self._reauth_entry, data=user_input
+                        reauth_entry, data=user_input
                     )
 
                 return self.async_abort(
@@ -80,5 +75,4 @@ class BlueCurrentConfigFlow(ConfigFlow, domain=DOMAIN):
         self, entry_data: Mapping[str, Any]
     ) -> ConfigFlowResult:
         """Handle a reauthorization flow request."""
-        self._reauth_entry = self._get_reauth_entry()
         return await self.async_step_user()
