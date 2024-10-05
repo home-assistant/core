b7735afbc787d56b518a3407cdc4e2f6ae16df7d
Arie Catsman <catsmanac@outlook.com>
2024-10-05 13:15:00 +0000

    Add reload on config update for Enphase Envoy

--- homeassistant/components/enphase_envoy/__init__.py
+++ homeassistant/components/enphase_envoy/__init__.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import httpx
 from pyenphase import Envoy
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
@@ -58,7 +59,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: EnphaseConfigEntry) -> b
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
+    # Reload entry when it is updated.
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+
     return True
+
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload the config entry when it changed."""
+    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: EnphaseConfigEntry) -> bool:
