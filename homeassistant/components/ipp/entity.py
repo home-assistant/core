e4a2e1404ab66404176298272f72eb6213337fd2
mib1185 <mail@mib85.de>
2024-10-06 11:30:26 +0000

    calc uptime timestamp in coordinator

--- homeassistant/components/ipp/entity.py
+++ homeassistant/components/ipp/entity.py
@@ -25,12 +25,13 @@ class IPPEntity(CoordinatorEntity[IPPDataUpdateCoordinator]):
 
         self.entity_description = description
 
+        data = self.coordinator.data["printer"]
         self._attr_unique_id = f"{coordinator.device_id}_{description.key}"
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, coordinator.device_id)},
-            manufacturer=self.coordinator.data.info.manufacturer,
-            model=self.coordinator.data.info.model,
-            name=self.coordinator.data.info.name,
-            sw_version=self.coordinator.data.info.version,
-            configuration_url=self.coordinator.data.info.more_info,
+            manufacturer=data.info.manufacturer,
+            model=data.info.model,
+            name=data.info.name,
+            sw_version=data.info.version,
+            configuration_url=data.info.more_info,
         )
