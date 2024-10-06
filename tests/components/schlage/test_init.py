e79dacaa6ca0fb5123174873cd0b74ce6997ef35
David Knowles <dknowles2@gmail.com>
2024-10-06 01:42:39 -0400

    Fix problems with automatic management of Schlage locks (#127689)
    Use the correct identifiers for existing lock devices

--- tests/components/schlage/test_init.py
+++ tests/components/schlage/test_init.py
@@ -12,6 +12,7 @@ from syrupy.assertion import SnapshotAssertion
 from homeassistant.components.schlage.const import DOMAIN, UPDATE_INTERVAL
 from homeassistant.config_entries import ConfigEntry, ConfigEntryState
 from homeassistant.core import HomeAssistant
+import homeassistant.helpers.device_registry as dr
 from homeassistant.helpers.device_registry import DeviceRegistry
 
 from tests.common import MockConfigEntry, async_fire_time_changed
@@ -125,6 +126,10 @@ async def test_auto_add_device(
     """Test new devices are auto-added to the device registry."""
     device = device_registry.async_get_device(identifiers={(DOMAIN, "test")})
     assert device is not None
+    all_devices = dr.async_entries_for_config_entry(
+        device_registry, mock_added_config_entry.entry_id
+    )
+    assert len(all_devices) == 1
 
     mock_lock_attrs["device_id"] = "test2"
     new_mock_lock = create_autospec(Lock)
@@ -139,19 +144,21 @@ async def test_auto_add_device(
     new_device = device_registry.async_get_device(identifiers={(DOMAIN, "test2")})
     assert new_device is not None
 
+    all_devices = dr.async_entries_for_config_entry(
+        device_registry, mock_added_config_entry.entry_id
+    )
+    assert len(all_devices) == 2
+
 
 async def test_auto_remove_device(
     hass: HomeAssistant,
     device_registry: DeviceRegistry,
     mock_added_config_entry: ConfigEntry,
     mock_schlage: Mock,
-    mock_lock: Mock,
-    mock_lock_attrs: dict[str, Any],
     freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test new devices are auto-added to the device registry."""
-    device = device_registry.async_get_device(identifiers={(DOMAIN, "test")})
-    assert device is not None
+    assert device_registry.async_get_device(identifiers={(DOMAIN, "test")}) is not None
 
     mock_schlage.locks.return_value = []
 
@@ -160,5 +167,8 @@ async def test_auto_remove_device(
     async_fire_time_changed(hass)
     await hass.async_block_till_done(wait_background_tasks=True)
 
-    new_device = device_registry.async_get_device(identifiers={(DOMAIN, "test")})
-    assert new_device is None
+    assert device_registry.async_get_device(identifiers={(DOMAIN, "test")}) is None
+    all_devices = dr.async_entries_for_config_entry(
+        device_registry, mock_added_config_entry.entry_id
+    )
+    assert len(all_devices) == 0
