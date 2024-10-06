5f090f2012402c19de4d6cd767a79c0d2fd3312e
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2024-10-06 22:05:25 +0000

    Add update_task action

--- homeassistant/components/habitica/__init__.py
+++ homeassistant/components/habitica/__init__.py
@@ -1,15 +1,19 @@
 """The habitica integration."""
 
+from datetime import datetime, time
 from http import HTTPStatus
 import logging
 from typing import Any
+import uuid
 
 from aiohttp import ClientResponseError
 from habitipy.aio import HabitipyAsync
 import voluptuous as vol
 
+from homeassistant.components.todo import ATTR_DESCRIPTION, ATTR_RENAME
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
+    ATTR_DATE,
     ATTR_NAME,
     CONF_API_KEY,
     CONF_NAME,
@@ -34,19 +38,42 @@ from homeassistant.helpers.selector import ConfigEntrySelector
 from homeassistant.helpers.typing import ConfigType
 
 from .const import (
+    ATTR_ADD_CHECKLIST_ITEM,
+    ATTR_ALIAS,
     ATTR_ARGS,
+    ATTR_CLEAR_DATE,
+    ATTR_CLEAR_REMINDER,
     ATTR_CONFIG_ENTRY,
+    ATTR_COST,
+    ATTR_COUNTER_DOWN,
+    ATTR_COUNTER_UP,
     ATTR_DATA,
+    ATTR_FREQUENCY,
     ATTR_PATH,
+    ATTR_PRIORITY,
+    ATTR_REMINDER,
+    ATTR_REMOVE_CHECKLIST_ITEM,
+    ATTR_REMOVE_REMINDER,
+    ATTR_REMOVE_TAG,
+    ATTR_SCORE_CHECKLIST_ITEM,
     ATTR_SKILL,
+    ATTR_TAG,
     ATTR_TASK,
+    ATTR_UNSCORE_CHECKLIST_ITEM,
+    ATTR_UP_DOWN,
     CONF_API_USER,
     DOMAIN,
     EVENT_API_CALL_SUCCESS,
+    PRIORITIES,
     SERVICE_API_CALL,
     SERVICE_CAST_SKILL,
+    SERVICE_UPDATE_DAILY,
+    SERVICE_UPDATE_HABIT,
+    SERVICE_UPDATE_REWARD,
+    SERVICE_UPDATE_TODO,
 )
 from .coordinator import HabiticaDataUpdateCoordinator
+from .util import get_config_entry, lookup_task
 
 _LOGGER = logging.getLogger(__name__)
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
@@ -71,9 +98,41 @@ SERVICE_CAST_SKILL_SCHEMA = vol.Schema(
         vol.Optional(ATTR_TASK): cv.string,
     }
 )
+SERVICE_UPDATE_TASK_SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_CONFIG_ENTRY): ConfigEntrySelector(),
+        vol.Required(ATTR_TASK): cv.string,
+        vol.Optional(ATTR_RENAME): cv.string,
+        vol.Optional(ATTR_DESCRIPTION): cv.string,
+        vol.Optional(ATTR_TAG): vol.All(cv.ensure_list, [str]),
+        vol.Optional(ATTR_REMOVE_TAG): vol.All(cv.ensure_list, [str]),
+        vol.Optional(ATTR_ALIAS): vol.All(
+            cv.string, cv.matches_regex("^[a-zA-Z0-9-_]+$")
+        ),
+        vol.Optional(ATTR_PRIORITY): vol.All(cv.string, vol.In(set(PRIORITIES.keys()))),
+        vol.Optional(ATTR_DATE): cv.date,
+        vol.Optional(ATTR_CLEAR_DATE): cv.boolean,
+        vol.Optional(ATTR_REMINDER): vol.All(cv.ensure_list, [cv.datetime]),
+        vol.Optional(ATTR_REMOVE_REMINDER): vol.All(cv.ensure_list, [cv.datetime]),
+        vol.Optional(ATTR_CLEAR_REMINDER): cv.boolean,
+        vol.Optional(ATTR_COST): vol.All(int, float),
+        vol.Optional(ATTR_ADD_CHECKLIST_ITEM): vol.All(cv.ensure_list, [str]),
+        vol.Optional(ATTR_REMOVE_CHECKLIST_ITEM): vol.All(cv.ensure_list, [str]),
+        vol.Optional(ATTR_SCORE_CHECKLIST_ITEM): vol.All(cv.ensure_list, [str]),
+        vol.Optional(ATTR_UNSCORE_CHECKLIST_ITEM): vol.All(cv.ensure_list, [str]),
+        vol.Optional(ATTR_UP_DOWN): vol.All(
+            cv.ensure_list, [vol.In({"positive", "negative"})]
+        ),
+        vol.Optional(ATTR_FREQUENCY): vol.All(
+            cv.string, vol.In({"daily", "weekly", "monthly"})
+        ),
+        vol.Optional(ATTR_COUNTER_UP): int,
+        vol.Optional(ATTR_COUNTER_DOWN): int,
+    }
+)
 
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:  # noqa: C901
     """Set up the Habitica service."""
 
     async def cast_skill(call: ServiceCall) -> ServiceResponse:
@@ -143,6 +202,171 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             await coordinator.async_request_refresh()
             return response
 
+    async def update_task(call: ServiceCall) -> ServiceResponse:
+        """Update task action."""
+        entry: HabiticaConfigEntry = get_config_entry(
+            hass, call.data[ATTR_CONFIG_ENTRY]
+        )
+        coordinator = entry.runtime_data
+        await coordinator.async_refresh()
+
+        current_task = lookup_task(coordinator.data.tasks, call.data[ATTR_TASK])
+        task_id = current_task["id"]
+        data = {}
+
+        if rename := call.data.get(ATTR_RENAME):
+            data.update({"text": rename})
+
+        if description := call.data.get(ATTR_DESCRIPTION):
+            data.update({"notes": description})
+
+        if priority := call.data.get(ATTR_PRIORITY):
+            data.update({"priority": PRIORITIES[priority]})
+
+        if reminder := call.data.get(ATTR_REMINDER):
+            existing_reminder_times = {
+                datetime.fromisoformat(r["time"]).replace(tzinfo=None)
+                for r in current_task.get("reminders", [])
+            }
+
+            data.update(
+                {
+                    "reminders": (
+                        [
+                            {
+                                "id": str(uuid.uuid4()),
+                                "time": r.isoformat(),
+                            }
+                            for r in reminder
+                            if r not in existing_reminder_times
+                        ]
+                        + current_task.get("reminders", [])
+                    )
+                }
+            )
+        if remove_reminder := call.data.get(ATTR_REMOVE_REMINDER):
+            reminders = list(
+                filter(
+                    lambda r: datetime.fromisoformat(r["time"]).replace(tzinfo=None)
+                    not in remove_reminder,
+                    current_task.get("reminders", []),
+                )
+            )
+            data.update({"reminders": reminders})
+
+        if call.data.get(ATTR_CLEAR_REMINDER):
+            data.update({"reminders": []})
+
+        if date := call.data.get(ATTR_DATE):
+            data.update({"date": (datetime.combine(date, time()).isoformat())})
+
+        if call.data.get(ATTR_CLEAR_DATE):
+            data.update({"date": None})
+
+        tags = call.data.get(ATTR_TAG)
+        remove_tags = call.data.get(ATTR_REMOVE_TAG)
+
+        if tags or remove_tags:
+            update_tags = set(
+                next(
+                    (
+                        task.get("tags", [])
+                        for task in coordinator.data.tasks
+                        if task["id"] == task_id
+                    ),
+                    [],
+                )
+            )
+            user_tags = {
+                tag["name"].lower(): tag["id"]
+                for tag in coordinator.data.user.get("tags", [])
+            }
+
+            if tags:
+                # Creates new tag if it doesn't exist
+                update_tags.update(
+                    {
+                        user_tags.get(tag_name.lower())
+                        or (await coordinator.api.tags.post(name=tag_name)).get("id")
+                        for tag_name in tags
+                    }
+                )
+            if remove_tags:
+                update_tags.difference_update(
+                    {
+                        user_tags[tag_name.lower()]
+                        for tag_name in remove_tags
+                        if tag_name.lower() in user_tags
+                    }
+                )
+
+            data.update({"tags": list(update_tags)})
+
+        if alias := call.data.get(ATTR_ALIAS):
+            data.update({"alias": alias})
+
+        if cost := call.data.get(ATTR_COST):
+            data.update({"value": cost})
+
+        add_checklist_item = call.data.get(ATTR_ADD_CHECKLIST_ITEM)
+        remove_checklist_item = call.data.get(ATTR_REMOVE_CHECKLIST_ITEM)
+        score_checklist_item = call.data.get(ATTR_SCORE_CHECKLIST_ITEM, [])
+        unscore_checklist_item = call.data.get(ATTR_UNSCORE_CHECKLIST_ITEM, [])
+
+        if (
+            add_checklist_item
+            or remove_checklist_item
+            or score_checklist_item
+            or unscore_checklist_item
+        ):
+            checklist = current_task.get("checklist", [])
+
+            if add_checklist_item:
+                checklist.extend(
+                    {"completed": False, "id": str(uuid.uuid4()), "text": item}
+                    for item in add_checklist_item
+                    if not any(i["text"] == item for i in checklist)
+                )
+            if remove_checklist_item:
+                checklist = [
+                    item
+                    for item in checklist
+                    if item["text"] not in remove_checklist_item
+                ]
+            if score_checklist_item or unscore_checklist_item:
+                checklist = [
+                    {
+                        **item,
+                        "completed": (
+                            True
+                            if item["text"] in score_checklist_item
+                            else False
+                            if item["text"] in unscore_checklist_item
+                            else item["completed"]
+                        ),
+                    }
+                    for item in checklist
+                ]
+
+            data.update({"checklist": checklist})
+
+        if frequency := call.data.get(ATTR_FREQUENCY):
+            data.update({"frequency": frequency})
+        if up_down := call.data.get(ATTR_UP_DOWN):
+            data.update(
+                {
+                    "up": "positive" in up_down,
+                    "down": "negative" in up_down,
+                }
+            )
+
+        if counter_up := call.data.get(ATTR_COUNTER_UP):
+            data.update({"counterUp": counter_up})
+        if counter_down := call.data.get(ATTR_COUNTER_DOWN):
+            data.update({"counterDown": counter_down})
+
+        return await coordinator.api.tasks[task_id].put(**data)
+
     hass.services.async_register(
         DOMAIN,
         SERVICE_CAST_SKILL,
@@ -150,6 +374,20 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         schema=SERVICE_CAST_SKILL_SCHEMA,
         supports_response=SupportsResponse.ONLY,
     )
+    for service in (
+        SERVICE_UPDATE_DAILY,
+        SERVICE_UPDATE_HABIT,
+        SERVICE_UPDATE_REWARD,
+        SERVICE_UPDATE_TODO,
+    ):
+        hass.services.async_register(
+            DOMAIN,
+            service,
+            update_task,
+            schema=SERVICE_UPDATE_TASK_SCHEMA,
+            supports_response=SupportsResponse.ONLY,
+        )
+
     return True
 
 
