a8974cc6769cb1b0be90a118bc1b5a9210517978
Franck Nijhof <git@frenck.dev>
2024-10-07 09:27:38 +0200

    Revert "Add testing framework for LG ThinQ" (#127779)
    Revert "Add testing framework for LG ThinQ (#127237)"
    
    This reverts commit 62ecd50910cfb6d8696c8f6550b8b2f75542f301.

--- tests/components/lg_thinq/test_config_flow.py
+++ tests/components/lg_thinq/test_config_flow.py
@@ -14,10 +14,7 @@ from tests.common import MockConfigEntry
 
 
 async def test_config_flow(
-    hass: HomeAssistant,
-    mock_thinq_api: AsyncMock,
-    mock_uuid: AsyncMock,
-    mock_setup_entry: AsyncMock,
+    hass: HomeAssistant, mock_thinq_api: AsyncMock, mock_uuid: AsyncMock
 ) -> None:
     """Test that an thinq entry is normally created."""
     result = await hass.config_entries.flow.async_init(
