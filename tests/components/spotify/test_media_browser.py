6a61669d6cda2fe47cd25fcc2468065532ecaac7
Joost Lekkerkerker <joostlek@outlook.com>
2024-10-07 17:36:39 +0200

    Improve Spotify mock (#127825)
    * Improve Spotify mock
    
    * Fix comments
    
    * Fix comments
    
    * Fix comments
    
    * Fix comments
    
    * Fix comments
    
    * Fix comments
    
    * Fix comments
    
    * Fix comments

--- tests/components/spotify/test_media_browser.py
+++ tests/components/spotify/test_media_browser.py
@@ -1,44 +1,65 @@
 """Test the media browser interface."""
 
+from unittest.mock import MagicMock
+
 import pytest
 from syrupy import SnapshotAssertion
 
 from homeassistant.components.spotify import DOMAIN
 from homeassistant.components.spotify.browse_media import async_browse_media
+from homeassistant.const import CONF_ID
 from homeassistant.core import HomeAssistant
-from homeassistant.setup import async_setup_component
+
+from . import setup_integration
+from .conftest import SCOPES
 
 from tests.common import MockConfigEntry
 
 
-async def setup_integration(hass: HomeAssistant, config_entry: MockConfigEntry) -> None:
-    """Fixture for setting up the component."""
-    config_entry.add_to_hass(hass)
-
-    await hass.config_entries.async_setup(config_entry.entry_id)
-    await hass.async_block_till_done(wait_background_tasks=True)
-    await async_setup_component(hass, DOMAIN, {})
-    await hass.async_block_till_done(wait_background_tasks=True)
-
-
+@pytest.mark.usefixtures("setup_credentials")
 async def test_browse_media_root(
     hass: HomeAssistant,
+    mock_spotify: MagicMock,
+    mock_config_entry: MockConfigEntry,
     snapshot: SnapshotAssertion,
-    spotify_setup,
+    expires_at: int,
 ) -> None:
     """Test browsing the root."""
+    await setup_integration(hass, mock_config_entry)
+    # We add a second config entry to test that lowercase entry_ids also work
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="spotify_2",
+        unique_id="second_fake_id",
+        data={
+            CONF_ID: "second_fake_id",
+            "name": "spotify_account_2",
+            "auth_implementation": DOMAIN,
+            "token": {
+                "access_token": "mock-access-token",
+                "refresh_token": "mock-refresh-token",
+                "expires_at": expires_at,
+                "scope": SCOPES,
+            },
+        },
+        entry_id="32oesphrnacjcf7vw5bf6odx3",
+    )
+    await setup_integration(hass, config_entry)
     response = await async_browse_media(hass, None, None)
     assert response.as_dict() == snapshot
 
 
+@pytest.mark.usefixtures("setup_credentials")
 async def test_browse_media_categories(
     hass: HomeAssistant,
+    mock_spotify: MagicMock,
+    mock_config_entry: MockConfigEntry,
     snapshot: SnapshotAssertion,
-    spotify_setup,
 ) -> None:
     """Test browsing categories."""
+    await setup_integration(hass, mock_config_entry)
     response = await async_browse_media(
-        hass, "spotify://library", "spotify://01J5TX5A0FF6G5V0QJX6HBC94T"
+        hass, "spotify://library", f"spotify://{mock_config_entry.entry_id}"
     )
     assert response.as_dict() == snapshot
 
@@ -46,13 +67,31 @@ async def test_browse_media_categories(
 @pytest.mark.parametrize(
     ("config_entry_id"), [("01J5TX5A0FF6G5V0QJX6HBC94T"), ("32oesphrnacjcf7vw5bf6odx3")]
 )
+@pytest.mark.usefixtures("setup_credentials")
 async def test_browse_media_playlists(
     hass: HomeAssistant,
-    snapshot: SnapshotAssertion,
     config_entry_id: str,
-    spotify_setup,
+    mock_spotify: MagicMock,
+    snapshot: SnapshotAssertion,
+    expires_at: int,
 ) -> None:
     """Test browsing playlists for the two config entries."""
+    mock_config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="Spotify",
+        unique_id="1112264649",
+        data={
+            "auth_implementation": DOMAIN,
+            "token": {
+                "access_token": "mock-access-token",
+                "refresh_token": "mock-refresh-token",
+                "expires_at": expires_at,
+                "scope": SCOPES,
+            },
+        },
+        entry_id=config_entry_id,
+    )
+    await setup_integration(hass, mock_config_entry)
     response = await async_browse_media(
         hass,
         "spotify://current_user_playlists",
