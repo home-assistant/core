bde35dc37ecc28f177c9dc5bac25cdfe55505ab2
pssc <phillip.camp+git@gmail.com>
2024-10-08 23:56:23 +0100

    test for restart fail

--- tests/components/squeezebox/test_update.py
+++ tests/components/squeezebox/test_update.py
@@ -4,6 +4,8 @@ import copy
 from datetime import timedelta
 from unittest.mock import patch
 
+import pytest
+
 from homeassistant.components.squeezebox.const import (
     SENSOR_UPDATE_INTERVAL,
     STATUS_UPDATE_NEWPLUGINS,
@@ -15,6 +17,7 @@ from homeassistant.components.update import (
 )
 from homeassistant.const import ATTR_ENTITY_ID, STATE_OFF, STATE_ON, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.util import dt as dt_util
 
 from .conftest import FAKE_QUERY_RESPONSE
@@ -108,6 +111,50 @@ async def test_update_plugins_install_fallback(
         )
         await hass.async_block_till_done(wait_background_tasks=True)
 
+    state = hass.states.get(entity_id)
+    assert state is not None
+    assert state.state == STATE_ON
+
+    attrs = state.attributes
+    assert not attrs[ATTR_IN_PROGRESS]
+
+
+async def test_update_plugins_install_restart_fail(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+) -> None:
+    """Test binary sensor states and attributes."""
+
+    entity_id = "update.fakelib_updated_plugins"
+    # Setup component
+    with (
+        patch(
+            "homeassistant.components.squeezebox.PLATFORMS",
+            [Platform.UPDATE],
+        ),
+        patch(
+            "homeassistant.components.squeezebox.Server.async_query",
+            return_value=copy.deepcopy(FAKE_QUERY_RESPONSE),
+        ),
+    ):
+        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.async_block_till_done(wait_background_tasks=True)
+
+    with (
+        patch(
+            "homeassistant.components.squeezebox.Server.async_query",
+            return_value=True,
+        ),
+        pytest.raises(HomeAssistantError),
+    ):
+        await hass.services.async_call(
+            UPDATE_DOMAIN,
+            SERVICE_INSTALL,
+            {
+                ATTR_ENTITY_ID: entity_id,
+            },
+            blocking=True,
+        )
     state = hass.states.get(entity_id)
     assert state is not None
     assert state.state == STATE_ON
