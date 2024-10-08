b44418dde4bf63b57001414c8a9021926f39be55
G Johansson <goran.johansson@shiftit.se>
2024-10-08 16:21:37 +0000

    Allow input_boolean as switch in Generic thermostat config

--- homeassistant/components/generic_thermostat/config_flow.py
+++ homeassistant/components/generic_thermostat/config_flow.py
@@ -7,7 +7,7 @@ from typing import Any, cast
 
 import voluptuous as vol
 
-from homeassistant.components import fan, switch
+from homeassistant.components import fan, input_boolean, switch
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN, SensorDeviceClass
 from homeassistant.const import CONF_NAME, DEGREE
 from homeassistant.helpers import selector
@@ -38,7 +38,13 @@ OPTIONS_SCHEMA = {
         )
     ),
     vol.Required(CONF_HEATER): selector.EntitySelector(
-        selector.EntitySelectorConfig(domain=[fan.DOMAIN, switch.DOMAIN])
+        selector.EntitySelectorConfig(
+            domain=[
+                fan.DOMAIN,
+                input_boolean.DOMAIN,
+                switch.DOMAIN,
+            ]
+        )
     ),
     vol.Required(
         CONF_COLD_TOLERANCE, default=DEFAULT_TOLERANCE
