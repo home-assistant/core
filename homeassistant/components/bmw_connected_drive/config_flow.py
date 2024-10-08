54c4fb5f569279f36fcec611287e6a7a9a30727c
Richard Kroegel <42204099+rikroe@users.noreply.github.com>
2024-10-08 07:42:10 +0200

    BMW: Add reconfiguration flow (#127726)
    * BMW: Add reconfiguration flow
    
    * Implement requested changes
    
    --------
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Abort if unique_id changes, small adjustments
    
    ---------
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>

--- homeassistant/components/bmw_connected_drive/config_flow.py
+++ homeassistant/components/bmw_connected_drive/config_flow.py
@@ -13,6 +13,7 @@ import voluptuous as vol
 
 from homeassistant.config_entries import (
     SOURCE_REAUTH,
+    SOURCE_RECONFIGURE,
     ConfigEntry,
     ConfigFlow,
     ConfigFlowResult,
@@ -20,6 +21,7 @@ from homeassistant.config_entries import (
 )
 from homeassistant.const import CONF_PASSWORD, CONF_REGION, CONF_SOURCE, CONF_USERNAME
 from homeassistant.core import HomeAssistant, callback
+from homeassistant.data_entry_flow import AbortFlow
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.selector import SelectSelector, SelectSelectorConfig
 
@@ -72,7 +74,8 @@ class BMWConfigFlow(ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    _reauth_entry: ConfigEntry
+    _existing_entry_data: Mapping[str, Any] | None = None
+    _existing_entry_unique_id: str | None = None
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -83,9 +86,14 @@ class BMWConfigFlow(ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             unique_id = f"{user_input[CONF_REGION]}-{user_input[CONF_USERNAME]}"
 
-            if self.source != SOURCE_REAUTH:
+            if self.source not in {SOURCE_REAUTH, SOURCE_RECONFIGURE}:
                 await self.async_set_unique_id(unique_id)
                 self._abort_if_unique_id_configured()
+            elif (
+                self.source in {SOURCE_REAUTH, SOURCE_RECONFIGURE}
+                and unique_id != self._existing_entry_unique_id
+            ):
+                raise AbortFlow("account_mismatch")
 
             info = None
             try:
@@ -102,23 +110,22 @@ class BMWConfigFlow(ConfigFlow, domain=DOMAIN):
 
             if info:
                 if self.source == SOURCE_REAUTH:
-                    self.hass.config_entries.async_update_entry(
-                        self._reauth_entry, data=entry_data
+                    return self.async_update_reload_and_abort(
+                        self._get_reauth_entry(), data=entry_data
                     )
-                    self.hass.async_create_task(
-                        self.hass.config_entries.async_reload(
-                            self._reauth_entry.entry_id
-                        )
+                if self.source == SOURCE_RECONFIGURE:
+                    return self.async_update_reload_and_abort(
+                        self._get_reconfigure_entry(),
+                        data=entry_data,
                     )
-                    return self.async_abort(reason="reauth_successful")
-
                 return self.async_create_entry(
                     title=info["title"],
                     data=entry_data,
                 )
 
         schema = self.add_suggested_values_to_schema(
-            DATA_SCHEMA, self._reauth_entry.data if self.source == SOURCE_REAUTH else {}
+            DATA_SCHEMA,
+            self._existing_entry_data,
         )
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
@@ -127,7 +134,17 @@ class BMWConfigFlow(ConfigFlow, domain=DOMAIN):
         self, entry_data: Mapping[str, Any]
     ) -> ConfigFlowResult:
         """Handle configuration by re-auth."""
-        self._reauth_entry = self._get_reauth_entry()
+        self._existing_entry_data = entry_data
+        self._existing_entry_unique_id = self._get_reauth_entry().unique_id
+        return await self.async_step_user()
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle a reconfiguration flow initialized by the user."""
+        reconfigure_entry = self._get_reconfigure_entry()
+        self._existing_entry_data = reconfigure_entry.data
+        self._existing_entry_unique_id = reconfigure_entry.unique_id
         return await self.async_step_user()
 
     @staticmethod
