3d31db5e142f4abfecfb7eec223746f2f818a5b5
Thomas <thomas.protzner@gmail.com>
2024-10-09 15:13:49 +0000

    add stay out zones

--- homeassistant/components/husqvarna_automower/switch.py
+++ homeassistant/components/husqvarna_automower/switch.py
@@ -32,6 +32,7 @@ async def async_setup_entry(
     coordinator = entry.runtime_data
     entities: list[SwitchEntity] = []
     current_work_areas = {}
+    current_stay_out_zones = {}
     entities.extend(
         AutomowerScheduleSwitchEntity(mower_id, coordinator)
         for mower_id in coordinator.data
@@ -46,7 +47,7 @@ async def async_setup_entry(
                     )
                     for stay_out_zone_uid in _stay_out_zones.zones
                 )
-            async_remove_entities(hass, coordinator, entry, mower_id)
+                current_stay_out_zones[mower_id] = set(_stay_out_zones.zones)
         if coordinator.data[mower_id].capabilities.work_areas:
             _work_areas = coordinator.data[mower_id].work_areas
             if _work_areas is not None:
@@ -72,7 +73,44 @@ async def async_setup_entry(
                             for work_area_id in new_work_areas
                         )
 
+    def _remove_stay_out_zone_entities(
+        removed_stay_out_zones: set, mower_id: str
+    ) -> None:
+        """Remove all unused work areas for all platforms."""
+        entity_reg = er.async_get(hass)
+        for entity_entry in er.async_entries_for_config_entry(
+            entity_reg, entry.entry_id
+        ):
+            for stay_out_zone_uid in removed_stay_out_zones:
+                if entity_entry.unique_id.startswith(f"{mower_id}_{stay_out_zone_uid}"):
+                    entity_reg.async_remove(entity_entry.entity_id)
+
+    def _async_stay_out_zone_listener() -> None:
+        """Listen for new stay out zones and add switch entities if they did not exist."""
+        for mower_id in coordinator.data:
+            if coordinator.data[mower_id].capabilities.stay_out_zones:
+                _stay_out_zones = coordinator.data[mower_id].stay_out_zones
+                if _stay_out_zones is not None:
+                    received_stay_out_zones = set(_stay_out_zones.zones)
+                    new_stay_out_zones = (
+                        received_stay_out_zones - current_stay_out_zones[mower_id]
+                    )
+                    removed_stay_out_zones = (
+                        current_stay_out_zones[mower_id] - received_stay_out_zones
+                    )
+                    if new_stay_out_zones:
+                        current_stay_out_zones[mower_id].update(new_stay_out_zones)
+                        async_add_entities(
+                            AutomowerStayOutZoneSwitchEntity(
+                                coordinator, mower_id, stay_out_zone_uid
+                            )
+                            for stay_out_zone_uid in new_stay_out_zones
+                        )
+                    if removed_stay_out_zones:
+                        _remove_stay_out_zone_entities(removed_stay_out_zones, mower_id)
+
     coordinator.async_add_listener(_async_work_area_listener)
+    coordinator.async_add_listener(_async_stay_out_zone_listener)
 
 
 class AutomowerScheduleSwitchEntity(AutomowerControlEntity, SwitchEntity):
--- tests/components/husqvarna_automower/test_switch.py
+++ tests/components/husqvarna_automower/test_switch.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from unittest.mock import AsyncMock, patch
 
 from aioautomower.exceptions import ApiException
-from aioautomower.model import MowerModes
+from aioautomower.model import MowerModes, Zone
 from aioautomower.utils import mower_list_to_dictionary_dataclass
 from freezegun.api import FrozenDateTimeFactory
 import pytest
@@ -216,29 +216,66 @@ async def test_work_area_switch_commands(
     assert len(mocked_method.mock_calls) == 2
 
 
+async def test_add_stay_out_zone(
+    hass: HomeAssistant,
+    mock_automower_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    freezer: FrozenDateTimeFactory,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test adding a stay out zone in runtime."""
+    await setup_integration(hass, mock_config_entry)
+    entry = hass.config_entries.async_entries(DOMAIN)[0]
+    current_entites = len(
+        er.async_entries_for_config_entry(entity_registry, entry.entry_id)
+    )
+    values = mower_list_to_dictionary_dataclass(
+        load_json_value_fixture("mower.json", DOMAIN)
+    )
+
+    values[TEST_MOWER_ID].stay_out_zones.zones.update(
+        {
+            1: Zone(
+                name="future_zone",
+                enabled=True,
+            )
+        }
+    )
+    mock_automower_client.get_status.return_value = values
+    freezer.tick(SCAN_INTERVAL)
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+    current_entites_after_addition = len(
+        er.async_entries_for_config_entry(entity_registry, entry.entry_id)
+    )
+    assert current_entites_after_addition == current_entites + 1
+
+
 async def test_zones_deleted(
     hass: HomeAssistant,
     mock_automower_client: AsyncMock,
     mock_config_entry: MockConfigEntry,
+    freezer: FrozenDateTimeFactory,
     entity_registry: er.EntityRegistry,
 ) -> None:
     """Test if stay-out-zone is deleted after removed."""
-
+    await setup_integration(hass, mock_config_entry)
+    entry = hass.config_entries.async_entries(DOMAIN)[0]
+    current_entites = len(
+        er.async_entries_for_config_entry(entity_registry, entry.entry_id)
+    )
     values = mower_list_to_dictionary_dataclass(
         load_json_value_fixture("mower.json", DOMAIN)
     )
-    await setup_integration(hass, mock_config_entry)
-    current_entries = len(
-        er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id)
-    )
-
-    del values[TEST_MOWER_ID].stay_out_zones.zones[TEST_ZONE_ID]
+    values[TEST_MOWER_ID].stay_out_zones.zones.pop(TEST_ZONE_ID)
     mock_automower_client.get_status.return_value = values
-    await hass.config_entries.async_reload(mock_config_entry.entry_id)
+    freezer.tick(SCAN_INTERVAL)
+    async_fire_time_changed(hass)
     await hass.async_block_till_done()
-    assert len(
-        er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id)
-    ) == (current_entries - 1)
+    current_entites_after_addition = len(
+        er.async_entries_for_config_entry(entity_registry, entry.entry_id)
+    )
+    assert current_entites_after_addition == current_entites - 1
 
 
 async def test_switch_snapshot(
