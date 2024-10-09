3c24e453020ce2b1fa8a6b7bcfebce2a844534ba
Erik <erik@montnemery.com>
2024-10-09 10:06:13 +0200

    Fix stale docstring

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -3017,7 +3017,7 @@ class ConfigSubentryFlowManager(
 
 
 class ConfigSubentryFlow(data_entry_flow.FlowHandler[FlowContext, SubentryFlowResult]):
-    """Base class for config options flows."""
+    """Base class for config subentry flows."""
 
     _flow_result = SubentryFlowResult
     handler: str
