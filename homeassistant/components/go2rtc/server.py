ba9d6fd5469ec122b2ed492ef2cb690d58ca761b
Robert Resch <robert@resch.dev>
2024-10-10 10:45:06 +0200

    Optimize go2rtc binary handling

--- homeassistant/components/go2rtc/server.py
+++ homeassistant/components/go2rtc/server.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import logging
 import subprocess
 from tempfile import NamedTemporaryFile
-from threading import Thread
+from threading import Event, Thread
 
 from .const import DOMAIN
 
@@ -19,12 +19,13 @@ class Server(Thread):
         """Initialize the server."""
         super().__init__(name=DOMAIN, daemon=True)
         self._binary = binary
-        self._stop_requested = False
+        self._process: subprocess.Popen | None = None
+        self._stop_event = Event()
 
     def run(self) -> None:
         """Run the server."""
         _LOGGER.debug("Starting go2rtc server")
-        self._stop_requested = False
+        self._stop_event.clear()
         with (
             NamedTemporaryFile(prefix="go2rtc", suffix=".yaml") as file,
             subprocess.Popen(
@@ -33,24 +34,31 @@ class Server(Thread):
                 stderr=subprocess.STDOUT,
             ) as process,
         ):
-            while not self._stop_requested and process.poll() is None:
+            self._process = process
+            while process.poll() is None and not self._stop_event.is_set():
                 assert process.stdout
-                line = process.stdout.readline()
-                if line == b"":
-                    break
-                _LOGGER.debug(line[:-1].decode())
-
-            _LOGGER.debug("Terminating go2rtc server")
-            process.terminate()
-            try:
-                process.wait(timeout=5)
-            except subprocess.TimeoutExpired:
-                _LOGGER.warning("Go2rtc server didn't terminate gracefully.Killing it")
-                process.kill()
+                for line in process.stdout:
+                    _LOGGER.debug(line[:-1].decode())
+        self._process = None
         _LOGGER.debug("Go2rtc server has been stopped")
+
+    def _terminate_process(self):
+        """Terminate the subprocess, ensuring cleanup."""
+        self._stop_event.set()
+
+        if not self._process:
+            return
+
+        _LOGGER.debug("Terminating go2rtc server")
+        self._process.terminate()
+        try:
+            self._process.wait(timeout=5)
+        except subprocess.TimeoutExpired:
+            _LOGGER.warning("Go2rtc server didn't terminate gracefully. Killing it.")
+            self._process.kill()
 
     def stop(self) -> None:
         """Stop the server."""
-        self._stop_requested = True
+        self._terminate_process()
         if self.is_alive():
             self.join()
