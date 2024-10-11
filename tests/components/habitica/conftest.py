8540343d7f993f219edc764bd7c60c72a80bfb04
Manu <4445816+tr4nt0r@users.noreply.github.com>
2024-10-11 20:03:23 +0200

    Add tests for Habitica integration (#126445)
    * Add tests for sensor platform
    
    * Add tests for switch platform
    
    * Add tests for button platform
    
    * Add tests for todo platform
    
    * Add tests for todo platform
    
    * Load json data fixtures
    
    * Update snapshot
    
    * Add fixtures
    
    * test move todo
    
    * parametrize todo tests, test drop notification
    
    * test todo exceptions
    
    * some minor improvements
    
    * test setup retry
    
    * Test update failed
    
    * Test coordinator rate limit
    
    * Test date utils
    
    * Reduce scope of PR
    
    * remove unused assert_mock_called_with  function
    
    * update snapshot
    
    * Update tests/components/habitica/test_init.py
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- tests/components/habitica/conftest.py
+++ tests/components/habitica/conftest.py
@@ -4,6 +4,12 @@ from unittest.mock import patch
 
 import pytest
 
+from homeassistant.components.habitica.const import CONF_API_USER, DEFAULT_URL, DOMAIN
+from homeassistant.const import CONF_API_KEY, CONF_URL
+
+from tests.common import MockConfigEntry, load_json_object_fixture
+from tests.test_util.aiohttp import AiohttpClientMocker
+
 
 @pytest.fixture(autouse=True)
 def disable_plumbum():
@@ -13,3 +19,38 @@ def disable_plumbum():
     """
     with patch("plumbum.local"), patch("plumbum.colors"):
         yield
+
+
+@pytest.fixture
+def mock_habitica(aioclient_mock: AiohttpClientMocker) -> AiohttpClientMocker:
+    """Mock aiohttp requests."""
+
+    aioclient_mock.get(
+        f"{DEFAULT_URL}/api/v3/user", json=load_json_object_fixture("user.json", DOMAIN)
+    )
+    aioclient_mock.get(
+        f"{DEFAULT_URL}/api/v3/tasks/user",
+        params={"type": "completedTodos"},
+        json=load_json_object_fixture("completed_todos.json", DOMAIN),
+    )
+    aioclient_mock.get(
+        f"{DEFAULT_URL}/api/v3/tasks/user",
+        json=load_json_object_fixture("tasks.json", DOMAIN),
+    )
+
+    return aioclient_mock
+
+
+@pytest.fixture(name="config_entry")
+def mock_config_entry() -> MockConfigEntry:
+    """Mock Habitica configuration entry."""
+    return MockConfigEntry(
+        domain=DOMAIN,
+        title="test-user",
+        data={
+            CONF_URL: DEFAULT_URL,
+            CONF_API_USER: "test-api-user",
+            CONF_API_KEY: "test-api-key",
+        },
+        unique_id="00000000-0000-0000-0000-000000000000",
+    )
