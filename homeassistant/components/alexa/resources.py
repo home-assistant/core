1e54c3fd078f67d0fceb1644fbd24963302517ab
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2024-10-12 15:21:31 +0000

    Replace repeated literal with constant

--- homeassistant/components/alexa/resources.py
+++ homeassistant/components/alexa/resources.py
@@ -2,6 +2,8 @@
 
 from typing import Any
 
+AT_TYPE = "@type"
+
 
 class AlexaGlobalCatalog:
     """The Global Alexa catalog.
@@ -235,10 +237,10 @@ class AlexaCapabilityResource:
         label_dict: dict[str, Any]
         for label in resources:
             if label in AlexaGlobalCatalog.__dict__.values():
-                label_dict = {"@type": "asset", "value": {"assetId": label}}
+                label_dict = {AT_TYPE: "asset", "value": {"assetId": label}}
             else:
                 label_dict = {
-                    "@type": "text",
+                    AT_TYPE: "text",
                     "value": {"text": label, "locale": "en-US"},
                 }
 
@@ -401,7 +403,7 @@ class AlexaSemantics:
     def add_states_to_value(self, states: list[str], value: Any) -> None:
         """Add StatesToValue stateMappings."""
         self._add_state_mapping(
-            {"@type": self.STATES_TO_VALUE, "states": states, "value": value}
+            {AT_TYPE: self.STATES_TO_VALUE, "states": states, "value": value}
         )
 
     def add_states_to_range(
@@ -410,7 +412,7 @@ class AlexaSemantics:
         """Add StatesToRange stateMappings."""
         self._add_state_mapping(
             {
-                "@type": self.STATES_TO_RANGE,
+                AT_TYPE: self.STATES_TO_RANGE,
                 "states": states,
                 "range": {"minimumValue": min_value, "maximumValue": max_value},
             }
@@ -422,7 +424,7 @@ class AlexaSemantics:
         """Add ActionsToDirective actionMappings."""
         self._add_action_mapping(
             {
-                "@type": self.ACTIONS_TO_DIRECTIVE,
+                AT_TYPE: self.ACTIONS_TO_DIRECTIVE,
                 "actions": actions,
                 "directive": {"name": directive, "payload": payload},
             }
