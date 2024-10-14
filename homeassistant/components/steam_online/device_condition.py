0d530c15196b10893fe681f3f55d4ac3abda3bcb
juliusmarminge <julius0216@outlook.com>
2024-10-14 15:34:51 +0200

    make condition singular

--- homeassistant/components/steam_online/device_condition.py
+++ homeassistant/components/steam_online/device_condition.py
@@ -5,29 +5,26 @@ from __future__ import annotations
 import voluptuous as vol
 
 from homeassistant.const import (
-    ATTR_ENTITY_ID,
+    ATTR_DEVICE_ID,
     CONF_CONDITION,
     CONF_DEVICE_ID,
     CONF_DOMAIN,
-    CONF_ENTITY_ID,
     CONF_TYPE,
 )
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant, State, callback
 from homeassistant.helpers import (
     condition,
     config_validation as cv,
-    entity_registry as er,
+    device_registry as dr,
 )
 from homeassistant.helpers.typing import ConfigType, TemplateVarsType
 
 from .const import CONF_ACCOUNT, DOMAIN
 
-# TODO specify your supported condition types.
 CONDITION_TYPES = {"is_same_game_as_primary"}
 
 CONDITION_SCHEMA = cv.DEVICE_CONDITION_BASE_SCHEMA.extend(
     {
-        vol.Required(CONF_ENTITY_ID): cv.entity_id,
         vol.Required(CONF_TYPE): vol.In(CONDITION_TYPES),
     }
 )
@@ -37,24 +34,16 @@ async def async_get_conditions(
     hass: HomeAssistant, device_id: str
 ) -> list[dict[str, str]]:
     """List device conditions for Steam devices."""
-    registry = er.async_get(hass)
+    # registry = er.async_get(hass)
     conditions = []
 
-    # Get all the integrations entities for this device
-    for entry in er.async_entries_for_device(registry, device_id):
-        if entry.platform != DOMAIN:
-            continue
+    base_condition = {
+        CONF_CONDITION: "device",
+        CONF_DEVICE_ID: device_id,
+        CONF_DOMAIN: DOMAIN,
+    }
 
-        # Add conditions for each entity that belongs to this integration
-        # TODO add your own conditions.
-        base_condition = {
-            CONF_CONDITION: "device",
-            CONF_DEVICE_ID: device_id,
-            CONF_DOMAIN: DOMAIN,
-            CONF_ENTITY_ID: entry.entity_id,
-        }
-
-        conditions += [{**base_condition, CONF_TYPE: cond} for cond in CONDITION_TYPES]
+    conditions += [{**base_condition, CONF_TYPE: cond} for cond in CONDITION_TYPES]
 
     return conditions
 
@@ -64,13 +53,17 @@ def async_condition_from_config(
     hass: HomeAssistant, config: ConfigType
 ) -> condition.ConditionCheckerType:
     """Create a function to test a device condition."""
-    # print("\033[96m  async_condition_from_config ::", config, "\033[0m")
 
     config_entry = hass.config_entries.async_get_entry(
-        er.async_get(hass).async_get(config[ATTR_ENTITY_ID]).config_entry_id
+        list(dr.async_get(hass).async_get(config[ATTR_DEVICE_ID]).config_entries)[0]
     )
-    primary_user = config_entry.data[CONF_ACCOUNT]
-    primary_user_entity_id = f"sensor.steam_{primary_user}"
+
+    @callback
+    def default_checker_method(
+        hass: HomeAssistant, variables: TemplateVarsType
+    ) -> bool:
+        """Checker method for unmatched condition types."""
+        return False
 
     @callback
     def test_is_same_game_as_primary(
@@ -78,23 +71,22 @@ def async_condition_from_config(
     ) -> bool:
         """Test if an entity is a certain state."""
 
-        event_game = hass.states.get(config[ATTR_ENTITY_ID]).attributes.get("game_id")
+        trigger = variables.get("trigger")
+        # from_state: State = trigger.get("from_state")
+        # from_game = from_state.attributes.get("game_id")
+
+        to_state: State = trigger.get("to_state")
+        to_game = to_state.attributes.get("game_id")
+
+        primary_user = config_entry.data[CONF_ACCOUNT]
+        primary_user_entity_id = f"sensor.steam_{primary_user}"
         primary_game = hass.states.get(primary_user_entity_id).attributes.get("game_id")
 
-        is_same_game_as_primary = (
-            primary_game is not None and primary_game == event_game
-        )
+        return primary_game is not None and primary_game == to_game
 
-        print(
-            "\n\033[96m  test_is_state :: primary user game",
-            primary_game,
-            "event game",
-            event_game,
-            " ===> ",
-            is_same_game_as_primary,
-            "\033[0m\n",
-        )
+    if config[CONF_TYPE] == "is_same_game_as_primary":
+        test_method = test_is_same_game_as_primary
+    else:
+        test_method = default_checker_method
 
-        return is_same_game_as_primary
-
-    return test_is_same_game_as_primary
+    return test_method
