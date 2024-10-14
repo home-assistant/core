aee0b7421a3a70284a307bc8ade4613ead1642df
Simon Lamon <32477463+silamon@users.noreply.github.com>
2024-10-14 20:53:50 +0200

    Update homeassistant/components/tag/__init__.py
    Co-authored-by: G Johansson <goran.johansson@shiftit.se>

--- homeassistant/components/tag/__init__.py
+++ homeassistant/components/tag/__init__.py
@@ -256,9 +256,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             )
         if change_type == collection.CHANGE_ADDED:
             # When tags are added to storage
-            entity = _create_entry(
-                entity_registry, updated_config[CONF_ID], updated_config.get(CONF_NAME)
-            )
+            entity = _create_entry(entity_registry, updated_config[CONF_ID], None)
             if TYPE_CHECKING:
                 assert entity.original_name
             await component.async_add_entities(
