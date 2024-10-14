ca4167cfedd21223bf9761b6ed1f09e2d84bf3e2
epenet <6771947+epenet@users.noreply.github.com>
2024-10-14 12:57:15 +0200

    Cleanup unnecessary reconfigure_confirm from config flows (#128086)

--- homeassistant/components/mealie/config_flow.py
+++ homeassistant/components/mealie/config_flow.py
@@ -116,12 +116,6 @@ class MealieConfigFlow(ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle reconfiguration of the integration."""
-        return await self.async_step_reconfigure_confirm()
-
-    async def async_step_reconfigure_confirm(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Handle reconfiguration confirmation."""
         errors: dict[str, str] = {}
         if user_input:
             self.host = user_input[CONF_HOST]
@@ -141,7 +135,7 @@ class MealieConfigFlow(ConfigFlow, domain=DOMAIN):
                     },
                 )
         return self.async_show_form(
-            step_id="reconfigure_confirm",
+            step_id="reconfigure",
             data_schema=USER_SCHEMA,
             errors=errors,
         )
