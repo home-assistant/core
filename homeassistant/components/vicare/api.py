e273bc05b51f742ff13154e3aae15bb55a931170
Christopher Fenner <Christopher.Fenner@me.com>
2024-10-15 07:44:19 +0200

    add get/post

--- homeassistant/components/vicare/api.py
+++ homeassistant/components/vicare/api.py
@@ -2,7 +2,7 @@
 
 from asyncio import run_coroutine_threadsafe
 
-from PyViCare.PyViCareAbstractOAuthManager import AbstractViCareOAuthManager
+from PyViCare.PyViCareAbstractOAuthManager import AbstractViCareOAuthManager, API_BASE_URL
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_entry_oauth2_flow
@@ -31,3 +31,23 @@ class ConfigEntryAuth(AbstractViCareOAuthManager):
         run_coroutine_threadsafe(
             self.session.async_ensure_token_valid(), self.hass.loop
         ).result()
+
+    def get(self, url: str) -> Any:
+        try:
+            self.session.async_request(
+                method="GET",
+                url=f"{API_BASE_URL}{url}",
+            )
+            logger.debug(self.__oauth)
+            response = self.__oauth.get(f"{API_BASE_URL}{url}", timeout=31).json()
+            logger.debug("Response to get request: %s", response)
+            self.__handle_expired_token(response)
+            self.__handle_rate_limit(response)
+            self.__handle_server_error(response)
+            return response
+        except TokenExpiredError:
+            self.renewToken()
+            return self.get(url)
+        except InvalidTokenError:
+            self.renewToken()
+            return self.get(url)
