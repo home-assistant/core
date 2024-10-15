38f5bea3b9ffdea3741fefb549ba9366029d8086
Iqra Ammad <ammad@chalmers.se>
2024-10-15 12:43:56 +0000

    L418 - Define a constant instead of duplicating this literal mdi:weather-partly-cloudy 5 times

--- homeassistant/components/buienradar/sensor.py
+++ homeassistant/components/buienradar/sensor.py
@@ -61,6 +61,7 @@ from .util import BrData
 
 _MDI_GAUGE_ICON = "mdi:gauge"
 _MDI_WEATHER_POURING_ICON = "mdi:weather-pouring"
+WEATHER_PARTLY_CLOUDY_ICON = "mdi:weather-partly-cloudy"
 _LOGGER = logging.getLogger(__name__)
 
 MEASURED_LABEL = "Measured"
@@ -417,31 +418,31 @@ SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
         key="sunchance_1d",
         translation_key="sunchance_1d",
         native_unit_of_measurement=PERCENTAGE,
-        icon="mdi:weather-partly-cloudy",
+        icon=WEATHER_PARTLY_CLOUDY_ICON,
     ),
     SensorEntityDescription(
         key="sunchance_2d",
         translation_key="sunchance_2d",
         native_unit_of_measurement=PERCENTAGE,
-        icon="mdi:weather-partly-cloudy",
+        icon=WEATHER_PARTLY_CLOUDY_ICON,
     ),
     SensorEntityDescription(
         key="sunchance_3d",
         translation_key="sunchance_3d",
         native_unit_of_measurement=PERCENTAGE,
-        icon="mdi:weather-partly-cloudy",
+        icon=WEATHER_PARTLY_CLOUDY_ICON,
     ),
     SensorEntityDescription(
         key="sunchance_4d",
         translation_key="sunchance_4d",
         native_unit_of_measurement=PERCENTAGE,
-        icon="mdi:weather-partly-cloudy",
+        icon=WEATHER_PARTLY_CLOUDY_ICON,
     ),
     SensorEntityDescription(
         key="sunchance_5d",
         translation_key="sunchance_5d",
         native_unit_of_measurement=PERCENTAGE,
-        icon="mdi:weather-partly-cloudy",
+        icon=WEATHER_PARTLY_CLOUDY_ICON,
     ),
     SensorEntityDescription(
         key="windforce_1d",
