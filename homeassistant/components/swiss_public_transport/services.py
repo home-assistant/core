9e3560cc860ee12cfa5422c2c392045d61e414ba
Cyrill Raccaud <cyrill.raccaud@aposphere.com>
2024-10-15 19:59:29 +0000

    fix rebase

--- homeassistant/components/swiss_public_transport/services.py
+++ homeassistant/components/swiss_public_transport/services.py
@@ -74,7 +74,9 @@ def setup_services(hass: HomeAssistant) -> None:
         )
         limit = call.data.get(ATTR_LIMIT) or CONNECTIONS_COUNT
         try:
-            connections = await config_entry.fetch_connections_as_json(limit=int(limit))
+            connections = await config_entry.runtime_data.fetch_connections_as_json(
+                limit=int(limit)
+            )
         except UpdateFailed as e:
             raise HomeAssistantError(
                 translation_domain=DOMAIN,
