85459411db2f6c228fc1468af79a59c697ee08dc
J. Diego Rodríguez Royo <jdrr1998@hotmail.com>
2024-10-16 15:17:41 +0000

    Use `is` instead of `==`

--- tests/components/home_connect/test_time.py
+++ tests/components/home_connect/test_time.py
@@ -34,9 +34,9 @@ async def test_time(
 ) -> None:
     """Test time entity."""
     get_appliances.side_effect = get_all_appliances
-    assert config_entry.state == ConfigEntryState.NOT_LOADED
+    assert config_entry.state is ConfigEntryState.NOT_LOADED
     assert await integration_setup()
-    assert config_entry.state == ConfigEntryState.LOADED
+    assert config_entry.state is ConfigEntryState.LOADED
 
 
 @pytest.mark.parametrize("appliance", ["Oven"], indirect=True)
@@ -81,9 +81,9 @@ async def test_time_entity_functionality(
     get_appliances.return_value = [appliance]
     appliance.status.update({setting_key: setting_value})
 
-    assert config_entry.state == ConfigEntryState.NOT_LOADED
+    assert config_entry.state is ConfigEntryState.NOT_LOADED
     assert await integration_setup()
-    assert config_entry.state == ConfigEntryState.LOADED
+    assert config_entry.state is ConfigEntryState.LOADED
     assert hass.states.is_state(entity_id, expected_state)
 
     new_value = 30
@@ -126,10 +126,10 @@ async def test_time_entity_error(
     """Test time entity error."""
     get_appliances.return_value = [problematic_appliance]
 
-    assert config_entry.state == ConfigEntryState.NOT_LOADED
+    assert config_entry.state is ConfigEntryState.NOT_LOADED
     problematic_appliance.status.update({setting_key: {}})
     assert await integration_setup()
-    assert config_entry.state == ConfigEntryState.LOADED
+    assert config_entry.state is ConfigEntryState.LOADED
 
     with pytest.raises(HomeConnectError):
         getattr(problematic_appliance, mock_attr)()
