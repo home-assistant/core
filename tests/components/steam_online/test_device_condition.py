86639178fa4acfb35bc19879ac303f31717d4429
juliusmarminge <julius0216@outlook.com>
2024-10-17 22:28:25 +0200

    use mock helper from existing tests

--- tests/components/steam_online/test_device_condition.py
+++ tests/components/steam_online/test_device_condition.py
@@ -6,10 +6,8 @@ from pytest_unordered import unordered
 
 from homeassistant.components import automation
 from homeassistant.components.device_automation import DeviceAutomationType
-from homeassistant.components.sensor import DOMAIN as SENSOR_PLATFORM
 from homeassistant.components.steam_online.const import (
     CONDITION_PRIMARY_GAME,
-    CONF_ACCOUNT,
     DOMAIN,
     STATE_ONLINE,
 )
@@ -17,9 +15,9 @@ from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 from homeassistant.setup import async_setup_component
 
-from tests.common import MockConfigEntry, async_get_device_automations
+from . import ACCOUNT_1, ACCOUNT_2, create_entry
 
-PRIMARY_USER_ID = "123456789"
+from tests.common import async_get_device_automations
 
 
 async def test_get_conditions(
@@ -28,14 +26,10 @@ async def test_get_conditions(
     entity_registry: er.EntityRegistry,
 ) -> None:
     """Test we get the expected conditions from a steam_online."""
-    config_entry = MockConfigEntry(domain=DOMAIN, data={})
-    config_entry.add_to_hass(hass)
+    config_entry = create_entry(hass)
     device_entry = device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
-        identifiers={(DOMAIN, "1234")},
-    )
-    entity_registry.async_get_or_create(
-        SENSOR_PLATFORM, DOMAIN, "5678", device_id=device_entry.id
+        identifiers={(DOMAIN, ACCOUNT_1)},
     )
     expected_conditions = [
         {
@@ -59,22 +53,19 @@ async def test_if_state(
     service_calls: list[ServiceCall],
 ) -> None:
     """Test for turn_on and turn_off conditions."""
-
-    # Setting up the ID for the primary user for the config
-    config_entry = MockConfigEntry(domain=DOMAIN, data={CONF_ACCOUNT: PRIMARY_USER_ID})
-    config_entry.add_to_hass(hass)
+    config_entry = create_entry(hass)
 
     # Setting up the device
     device_entry = device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
-        identifiers={(DOMAIN, "1234")},
+        identifiers={(DOMAIN, ACCOUNT_1)},
     )
 
     # Setting up the primary sensor
     primary_sensor = entity_registry.async_get_or_create(
         DOMAIN,
         "test",
-        PRIMARY_USER_ID,
+        ACCOUNT_1,
         device_id=device_entry.id,
         config_entry=config_entry,
     )
@@ -83,7 +74,7 @@ async def test_if_state(
     test_sensor = entity_registry.async_get_or_create(
         DOMAIN,
         "test",
-        "5678",
+        ACCOUNT_2,
         device_id=device_entry.id,
         config_entry=config_entry,
     )
@@ -94,7 +85,6 @@ async def test_if_state(
         {
             automation.DOMAIN: [
                 {
-                    # "trigger": {"platform": "event", "event_type": "test_event1"},
                     "trigger": {
                         "platform": "state",
                         "entity_id": test_sensor.entity_id,
@@ -122,12 +112,14 @@ async def test_if_state(
         },
     )
 
+    # Changing primary should not pass condition
     hass.states.async_set(
         primary_sensor.entity_id, STATE_ONLINE, attributes={"game_id": "123"}
     )
     await hass.async_block_till_done()
     assert len(service_calls) == 0
 
+    # Condition passes when test_sensor starts playing same game as primary
     hass.states.async_set(
         test_sensor.entity_id, STATE_ONLINE, attributes={"game_id": "123"}
     )
@@ -135,18 +127,21 @@ async def test_if_state(
     assert len(service_calls) == 1
     assert service_calls[0].data["some"] == f"is_same - state - {test_sensor.entity_id}"
 
+    # Condition does not pass when test_sensor starts playing different game
     hass.states.async_set(
         test_sensor.entity_id, STATE_ONLINE, attributes={"game_id": "321"}
     )
     await hass.async_block_till_done()
     assert len(service_calls) == 1
 
+    # Condition does not pass when primary starts playing same game as test_sensor
     hass.states.async_set(
         primary_sensor.entity_id, STATE_ONLINE, attributes={"game_id": "321"}
     )
     await hass.async_block_till_done()
     assert len(service_calls) == 1
 
+    # Does not trigger when the game is None
     hass.states.async_set(
         primary_sensor.entity_id, STATE_ONLINE, attributes={"game_id": None}
     )
@@ -157,6 +152,7 @@ async def test_if_state(
     await hass.async_block_till_done()
     assert len(service_calls) == 1
 
+    # Condition passes when they start playing the same game again
     hass.states.async_set(
         primary_sensor.entity_id, STATE_ONLINE, attributes={"game_id": "456"}
     )
@@ -167,12 +163,3 @@ async def test_if_state(
     await hass.async_block_till_done()
     assert len(service_calls) == 2
     assert service_calls[0].data["some"] == f"is_same - state - {test_sensor.entity_id}"
-
-    # assert service_calls[0].data["some"] == "is_same - event - test_event1"
-
-    # hass.states.async_set("steam_online.entity", STATE_OFF)
-    # hass.bus.async_fire("test_event1")
-    # hass.bus.async_fire("test_event2")
-    # await hass.async_block_till_done()
-    # assert len(service_calls) == 2
-    # assert service_calls[1].data["some"] == "is_off - event - test_event2"
