81c123931afb36a33c119c95569bf6f6c37aeeab
Andrew <34544450+10100011@users.noreply.github.com>
2024-10-19 19:42:47 +0100

    Adding OpenWeatherMap Minutely forecast action

---
+++ homeassistant/components/openweathermap/services.py
@@ -0,0 +1,35 @@
+"""Services for OpenWeatherMap."""
+
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant, ServiceCall, SupportsResponse
+
+from . import WeatherUpdateCoordinator
+from .const import ATTR_API_MINUTELY_FORECAST, DOMAIN, OWM_MODE_V30
+
+SERVICE_GET_MINUTELY_FORECAST = f"get_{ATTR_API_MINUTELY_FORECAST}"
+
+
+async def async_setup_services(
+    hass: HomeAssistant,
+    mode: str,
+    weather_coordinator: WeatherUpdateCoordinator,
+) -> None:
+    """Set up OpenWeatherMap services."""
+
+    def handle_get_minutely_forecasts(call: ServiceCall) -> None:
+        """Handle the service action call."""
+        return weather_coordinator.data[ATTR_API_MINUTELY_FORECAST]
+
+    if mode == OWM_MODE_V30:
+        hass.services.async_register(
+            domain=DOMAIN,
+            service=SERVICE_GET_MINUTELY_FORECAST,
+            service_func=handle_get_minutely_forecasts,
+            supports_response=SupportsResponse.ONLY,
+        )
+    else:
+        hass.services.async_remove(
+            domain=DOMAIN,
+            service=SERVICE_GET_MINUTELY_FORECAST,
+        )
