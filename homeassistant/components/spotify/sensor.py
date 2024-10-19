f3f290faebea8ba0bd736435fbb75bb28846eb8b
Joostlek <joostlek@outlook.com>
2024-10-19 16:50:44 +0200

    Add audio feature sensors to Spotify

--- homeassistant/components/spotify/sensor.py
+++ homeassistant/components/spotify/sensor.py
@@ -1,18 +1,19 @@
 """Sensor platform for Spotify."""
 
+from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Callable
 
 from spotifyaio.models import AudioFeatures
 
+from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import StateType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
 from . import DOMAIN, SpotifyConfigEntry
 from .coordinator import SpotifyCoordinator
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
-from homeassistant.helpers.typing import StateType
-from homeassistant.helpers.device_registry import DeviceInfo
-from homeassistant.core import HomeAssistant
-from ...helpers.entity_platform import AddEntitiesCallback
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -23,10 +24,32 @@ class SpotifyAudioFeaturesSensorEntityDescription(SensorEntityDescription):
 
 
 AUDIO_FEATURE_SENSORS: tuple[SpotifyAudioFeaturesSensorEntityDescription, ...] = (
+    SpotifyAudioFeaturesSensorEntityDescription(
+        key="bpm",
+        translation_key="song_tempo",
+        native_unit_of_measurement="bpm",
+        suggested_display_precision=0,
+        value_fn=lambda audio_features: audio_features.tempo,
+    ),
 )
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: SpotifyConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: SpotifyConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Spotify sensor based on a config entry."""
+    coordinator = entry.runtime_data.coordinator
+
+    user_id = entry.unique_id
+
+    assert user_id is not None
+
+    async_add_entities(
+        SpotifyAudioFeatureSensor(coordinator, description, user_id, entry.title)
+        for description in AUDIO_FEATURE_SENSORS
+    )
 
 
 class SpotifyAudioFeatureSensor(CoordinatorEntity[SpotifyCoordinator], SensorEntity):
@@ -40,6 +63,7 @@ class SpotifyAudioFeatureSensor(CoordinatorEntity[SpotifyCoordinator], SensorEnt
         coordinator: SpotifyCoordinator,
         entity_description: SpotifyAudioFeaturesSensorEntityDescription,
         user_id: str,
+        name: str,
     ) -> None:
         """Initialize."""
         super().__init__(coordinator)
@@ -47,6 +71,11 @@ class SpotifyAudioFeatureSensor(CoordinatorEntity[SpotifyCoordinator], SensorEnt
         self.entity_description = entity_description
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, user_id)},
+            manufacturer="Spotify AB",
+            model=f"Spotify {coordinator.current_user.product}",
+            name=f"Spotify {name}",
+            entry_type=DeviceEntryType.SERVICE,
+            configuration_url="https://open.spotify.com",
         )
 
     @property
