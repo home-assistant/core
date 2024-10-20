4d8eae171d5e6889f8e662f3c0e5e20697d6e7b7
Simone Chemelli <simone.chemelli@gmail.com>
2024-10-20 13:24:30 +0000

    add missing tests

--- tests/components/comelit/const.py
+++ tests/components/comelit/const.py
@@ -1,5 +1,6 @@
 """Common stuff for Comelit SimpleHome tests."""
 
+from aiocomelit import ComelitVedoAreaObject, ComelitVedoZoneObject
 from aiocomelit.api import ComelitSerialBridgeObject
 from aiocomelit.const import (
     CLIMATE,
@@ -10,6 +11,8 @@ from aiocomelit.const import (
     SCENARIO,
     VEDO,
     WATT,
+    AlarmAreaState,
+    AlarmZoneState,
 )
 
 from homeassistant.components.comelit.const import DOMAIN
@@ -38,7 +41,7 @@ MOCK_USER_VEDO_DATA = MOCK_CONFIG[DOMAIN][CONF_DEVICES][1]
 
 FAKE_PIN = 5678
 
-DEVICE_QUERY = {
+BRIDGE_DEVICE_QUERY = {
     CLIMATE: {},
     COVER: {
         0: ComelitSerialBridgeObject(
@@ -71,4 +74,33 @@ DEVICE_QUERY = {
     OTHER: {},
     IRRIGATION: {},
     SCENARIO: {},
+}
+
+VEDO_DEVICE_QUERY = {
+    "aree": {
+        0: ComelitVedoAreaObject(
+            index=0,
+            name="Area0",
+            p1=True,
+            p2=False,
+            ready=False,
+            armed=False,
+            alarm=False,
+            alarm_memory=False,
+            sabotage=False,
+            anomaly=False,
+            in_time=False,
+            out_time=False,
+            human_status=AlarmAreaState.UNKNOWN,
+        )
+    },
+    "zone": {
+        0: ComelitVedoZoneObject(
+            index=0,
+            name="Zone0",
+            status_api="0x000",
+            status=0,
+            human_status=AlarmZoneState.REST,
+        )
+    },
 }
--- tests/components/comelit/test_diagnostics.py
+++ tests/components/comelit/test_diagnostics.py
@@ -13,6 +13,8 @@ from aiocomelit.const import (
     OTHER,
     SCENARIO,
     WATT,
+    AlarmAreaState,
+    AlarmZoneState,
 )
 
 from homeassistant.components.comelit.const import DOMAIN
@@ -22,7 +24,12 @@ from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import CONF_TYPE
 from homeassistant.core import HomeAssistant
 
-from .const import DEVICE_QUERY, MOCK_USER_BRIDGE_DATA
+from .const import (
+    BRIDGE_DEVICE_QUERY,
+    MOCK_USER_BRIDGE_DATA,
+    MOCK_USER_VEDO_DATA,
+    VEDO_DEVICE_QUERY,
+)
 
 from tests.common import MockConfigEntry
 from tests.components.diagnostics import get_diagnostics_for_config_entry
@@ -41,7 +48,7 @@ async def test_entry_diagnostics(
         patch("aiocomelit.api.ComeliteSerialBridgeApi.login"),
         patch(
             "aiocomelit.api.ComeliteSerialBridgeApi.get_all_devices",
-            return_value=DEVICE_QUERY,
+            return_value=BRIDGE_DEVICE_QUERY,
         ),
     ):
         await hass.config_entries.async_setup(entry.entry_id)
@@ -106,6 +113,71 @@ async def test_entry_diagnostics(
                 },
                 {
                     SCENARIO: [],
+                },
+            ],
+        },
+    }
+
+    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_USER_VEDO_DATA)
+    entry.add_to_hass(hass)
+
+    with (
+        patch("aiocomelit.api.ComelitVedoApi.login"),
+        patch(
+            "aiocomelit.api.ComelitVedoApi.get_all_areas_and_zones",
+            return_value=VEDO_DEVICE_QUERY,
+        ),
+    ):
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    assert entry.state == ConfigEntryState.LOADED
+
+    entry_dict = entry.as_dict()
+    for key in TO_REDACT:
+        entry_dict["data"][key] = REDACTED
+    result = await get_diagnostics_for_config_entry(hass, hass_client, entry)
+
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+
+    assert result == {
+        "entry": entry_dict,
+        "type": entry.data.get(CONF_TYPE, BRIDGE),
+        "device_info": {
+            "last_update success": coordinator.last_update_success,
+            "last_exception": repr(coordinator.last_exception),
+            "devices": [
+                {
+                    "aree": [
+                        {
+                            "0": {
+                                "alarm": False,
+                                "alarm_memory": False,
+                                "anomaly": False,
+                                "armed": False,
+                                "human_status": AlarmAreaState.UNKNOWN.value,
+                                "in_time": False,
+                                "name": "Area0",
+                                "out_time": False,
+                                "p1": True,
+                                "p2": False,
+                                "ready": False,
+                                "sabotage": False,
+                            }
+                        },
+                    ],
+                },
+                {
+                    "zone": [
+                        {
+                            "0": {
+                                "human_status": AlarmZoneState.REST.value,
+                                "name": "Zone0",
+                                "status": 0,
+                                "status_api": "0x000",
+                            }
+                        },
+                    ],
                 },
             ],
         },
