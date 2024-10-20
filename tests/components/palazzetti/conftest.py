2c5dbe0865e675b457468143f777a054b707c46a
dotvav <mycrub@mycrub.net>
2024-10-20 10:10:35 +0000

    Increase test coverage

--- tests/components/palazzetti/conftest.py
+++ tests/components/palazzetti/conftest.py
@@ -16,7 +16,11 @@ def mock_config_entry() -> MockConfigEntry:
     return MockConfigEntry(
         title="palazzetti",
         domain=DOMAIN,
-        data={CONF_NAME: "name", CONF_HOST: "example", CONF_MAC: "mac"},
+        data={
+            CONF_NAME: "Stove",
+            CONF_HOST: "127.0.0.1",
+            CONF_MAC: "11:22:33:44:55:66",
+        },
         unique_id="unique_id",
     )
 
@@ -34,4 +38,17 @@ def mock_palazzetti():
         client.update_state = AsyncMock(return_value=True)
         client.mac = AsyncMock(return_value="11:22:33:44:55:66")
         client.name = AsyncMock(return_value="Stove")
+        client.sw_version = AsyncMock(return_value="0.0.0")
+        client.hw_version = AsyncMock(return_value="1.1.1")
+        client.fan_speed_min = AsyncMock(return_value=1)
+        client.fan_speed_max = AsyncMock(return_value=5)
+        client.has_fan_silent = AsyncMock(return_value=False)
+        client.has_fan_high = AsyncMock(return_value=True)
+        client.has_fan_auto = AsyncMock(return_value=True)
+        client.has_on_off_switch = AsyncMock(return_value=True)
+        client.connected = AsyncMock(return_value=True)
+        client.is_heating = AsyncMock(return_value=True)
+        client.room_temperature = AsyncMock(18)
+        client.target_temperature = AsyncMock(21)
+        client.fan_speed = AsyncMock(3)
         yield client
--- tests/components/palazzetti/test_climate.py
+++ tests/components/palazzetti/test_climate.py
@@ -1,29 +1,32 @@
-"""Tests for the Palazzetti integration."""
+"""Tests for the Palazzetti climate platform."""
 
 from unittest.mock import AsyncMock
 
 from homeassistant.config_entries import ConfigEntryState
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from tests.common import MockConfigEntry
 
+CLIMATE_ID = f"{Platform.CLIMATE}.stove_stove"
 
-async def test_load_unload_config_entry(
+
+async def test_climate(
     hass: HomeAssistant,
+    entity_registry: er.EntityRegistry,
     mock_config_entry: MockConfigEntry,
     mock_palazzetti: AsyncMock,
 ) -> None:
-    """Test the Palazzetti configuration entry loading/unloading."""
+    """Test the creation and values of Palazzetti climate device."""
     mock_config_entry.add_to_hass(hass)
 
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.LOADED
+    assert entity_registry.async_is_registered(CLIMATE_ID)
 
-    await hass.config_entries.async_unload(mock_config_entry.entry_id)
-    await hass.async_block_till_done()
+    entity = entity_registry.async_get(CLIMATE_ID)
 
-    assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
-
-    # change
+    assert entity.unique_id == "11:22:33:44:55:66-climate"
