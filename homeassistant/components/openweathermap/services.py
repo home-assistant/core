ede911f8ca52d09dafc4bc734669a8c28f3259f6
Andrew <34544450+10100011@users.noreply.github.com>
2024-10-21 20:27:22 +0000

    Check for correct mode per action call

--- homeassistant/components/openweathermap/services.py
+++ homeassistant/components/openweathermap/services.py
@@ -3,9 +3,10 @@
 from __future__ import annotations
 
 from homeassistant.core import HomeAssistant, ServiceCall, SupportsResponse
+from homeassistant.exceptions import ServiceValidationError
 
 from . import WeatherUpdateCoordinator
-from .const import ATTR_API_MINUTE_FORECAST, DOMAIN, OWM_MODE_V30
+from .const import ATTR_API_MINUTE_FORECAST, DEFAULT_NAME, DOMAIN, OWM_MODE_V30
 
 SERVICE_GET_MINUTE_FORECAST = f"get_{ATTR_API_MINUTE_FORECAST}"
 
@@ -19,17 +20,17 @@ async def async_setup_services(
 
     def handle_get_minute_forecasts(call: ServiceCall) -> None:
         """Handle the service action call."""
-        return weather_coordinator.data[ATTR_API_MINUTE_FORECAST]
+        if mode == OWM_MODE_V30:
+            return weather_coordinator.data[ATTR_API_MINUTE_FORECAST]
+        raise ServiceValidationError(
+            translation_domain=DOMAIN,
+            translation_key="service_minute_forecast_mode",
+            translation_placeholders={"name": DEFAULT_NAME},
+        )
 
-    if mode == OWM_MODE_V30:
-        hass.services.async_register(
-            domain=DOMAIN,
-            service=SERVICE_GET_MINUTE_FORECAST,
-            service_func=handle_get_minute_forecasts,
-            supports_response=SupportsResponse.ONLY,
-        )
-    else:
-        hass.services.async_remove(
-            domain=DOMAIN,
-            service=SERVICE_GET_MINUTE_FORECAST,
-        )
+    hass.services.async_register(
+        domain=DOMAIN,
+        service=SERVICE_GET_MINUTE_FORECAST,
+        service_func=handle_get_minute_forecasts,
+        supports_response=SupportsResponse.ONLY,
+    )
