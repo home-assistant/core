4b30280e093e2b530985109770835f2e4446ecbf
Steven B. <51370195+sdb9696@users.noreply.github.com>
2024-10-21 14:30:05 +0100

    Add reconfigure flow to ring integration (#128357)
    Co-authored-by: Christopher Fenner <9592452+CFenner@users.noreply.github.com>
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/ring/config_flow.py
+++ homeassistant/components/ring/config_flow.py
@@ -9,7 +9,12 @@ from ring_doorbell import Auth, AuthenticationError, Requires2FAError
 import voluptuous as vol
 
 from homeassistant.components import dhcp
-from homeassistant.config_entries import SOURCE_REAUTH, ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import (
+    SOURCE_REAUTH,
+    SOURCE_RECONFIGURE,
+    ConfigFlow,
+    ConfigFlowResult,
+)
 from homeassistant.const import (
     CONF_DEVICE_ID,
     CONF_NAME,
@@ -136,6 +141,11 @@ class RingConfigFlow(ConfigFlow, domain=DOMAIN):
                     {**self.user_pass, **user_input}
                 )
 
+            if self.source == SOURCE_RECONFIGURE:
+                return await self.async_step_reconfigure(
+                    {**self.user_pass, **user_input}
+                )
+
             return await self.async_step_user({**self.user_pass, **user_input})
 
         return self.async_show_form(
@@ -188,6 +198,48 @@ class RingConfigFlow(ConfigFlow, domain=DOMAIN):
             description_placeholders={
                 CONF_USERNAME: reauth_entry.data[CONF_USERNAME],
                 CONF_NAME: reauth_entry.data[CONF_USERNAME],
+            },
+        )
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Trigger a reconfiguration flow."""
+        errors: dict[str, str] = {}
+        reconfigure_entry = self._get_reconfigure_entry()
+        username = reconfigure_entry.data[CONF_USERNAME]
+        await self.async_set_unique_id(username)
+        if user_input:
+            user_input[CONF_USERNAME] = username
+            # Reconfigure will generate a new hardware id and create a new
+            # authorised device at ring.com.
+            if not self.hardware_id:
+                self.hardware_id = str(uuid.uuid4())
+            try:
+                assert self.hardware_id
+                token = await validate_input(self.hass, self.hardware_id, user_input)
+            except Require2FA:
+                self.user_pass = user_input
+                return await self.async_step_2fa()
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
+            except Exception:
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                data = {
+                    CONF_USERNAME: username,
+                    CONF_TOKEN: token,
+                    CONF_DEVICE_ID: self.hardware_id,
+                }
+                return self.async_update_reload_and_abort(reconfigure_entry, data=data)
+
+        return self.async_show_form(
+            step_id="reconfigure",
+            data_schema=STEP_RECONFIGURE_DATA_SCHEMA,
+            errors=errors,
+            description_placeholders={
+                CONF_USERNAME: username,
             },
         )
 
