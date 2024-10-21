19ac2f2b5be5f6a1054e751bb5226416bd6b13cc
Christopher Fenner <Christopher.Fenner@me.com>
2024-10-21 14:49:06 +0200

    remove reauth steps

--- homeassistant/components/vicare/config_flow.py
+++ homeassistant/components/vicare/config_flow.py
@@ -12,10 +12,12 @@ from typing import Any
 import voluptuous as vol
 
 # from homeassistant.components import dhcp
+from homeassistant.components import dhcp
 from homeassistant.config_entries import ConfigEntry, ConfigFlowResult
 
 # from homeassistant.const import CONF_CLIENT_ID, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.helpers import config_entry_oauth2_flow
+from homeassistant.helpers.device_registry import format_mac
 
 from .const import CONF_HEATING_TYPE, DEFAULT_HEATING_TYPE, DOMAIN, HeatingType
 
@@ -69,6 +71,21 @@ class OAuth2FlowHandler(
             "scope": " ".join(SCOPES),
         }
 
+    async def async_step_dhcp(
+        self, discovery_info: dhcp.DhcpServiceInfo
+    ) -> ConfigFlowResult:
+        """Invoke when a Viessmann MAC address is discovered on the network."""
+        formatted_mac = format_mac(discovery_info.macaddress)
+        _LOGGER.debug("Found device with mac %s", formatted_mac)
+
+        await self.async_set_unique_id(formatted_mac)
+        self._abort_if_unique_id_configured()
+
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        return await self.async_step_user()
+
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
@@ -95,58 +112,3 @@ class OAuth2FlowHandler(
         #     )
 
         return await super().async_step_user(user_input)
-
-    # async def async_step_reauth(
-    #     self, entry_data: Mapping[str, Any]
-    # ) -> ConfigFlowResult:
-    #     """Handle re-authentication with ViCare."""
-    #     self.entry = self.hass.config_entries.async_get_entry(self.context["entry_id"])
-    #     return await self.async_step_reauth_confirm()
-
-    # async def async_step_reauth_confirm(
-    #     self, user_input: dict[str, Any] | None = None
-    # ) -> ConfigFlowResult:
-    #     """Confirm re-authentication with ViCare."""
-    #     errors: dict[str, str] = {}
-    #     assert self.entry is not None
-
-    #     if user_input:
-    #         data = {
-    #             **self.entry.data,
-    #             **user_input,
-    #         }
-
-    #         try:
-    #             await self.hass.async_add_executor_job(vicare_login, data)
-    #         except (PyViCareInvalidConfigurationError, PyViCareInvalidCredentialsError):
-    #             errors["base"] = "invalid_auth"
-    #         else:
-    #             self.hass.config_entries.async_update_entry(
-    #                 self.entry,
-    #                 data=data,
-    #             )
-    #             await self.hass.config_entries.async_reload(self.entry.entry_id)
-    #             return self.async_abort(reason="reauth_successful")
-
-    #     return self.async_show_form(
-    #         step_id="reauth_confirm",
-    #         data_schema=self.add_suggested_values_to_schema(
-    #             REAUTH_SCHEMA, self.entry.data
-    #         ),
-    #         errors=errors,
-    #     )
-
-    # async def async_step_dhcp(
-    #     self, discovery_info: dhcp.DhcpServiceInfo
-    # ) -> ConfigFlowResult:
-    #     """Invoke when a Viessmann MAC address is discovered on the network."""
-    #     formatted_mac = format_mac(discovery_info.macaddress)
-    #     _LOGGER.debug("Found device with mac %s", formatted_mac)
-
-    #     await self.async_set_unique_id(formatted_mac)
-    #     self._abort_if_unique_id_configured()
-
-    #     if self._async_current_entries():
-    #         return self.async_abort(reason="single_instance_allowed")
-
-    #     return await self.async_step_user()
