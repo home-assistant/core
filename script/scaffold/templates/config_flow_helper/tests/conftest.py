203e080f7852067b06da43ad4343c9ff8fe4f2bd
lukcz <czarnecki.lucas@gmail.com>
2024-10-21 17:38:23 +0000

    Revert unintended change in scaffold template.

--- script/scaffold/templates/config_flow_helper/tests/conftest.py
+++ script/scaffold/templates/config_flow_helper/tests/conftest.py
@@ -10,6 +10,6 @@ import pytest
 def mock_setup_entry() -> Generator[AsyncMock]:
     """Override async_setup_entry."""
     with patch(
-        "homeassistant.components.microsoft_speech.async_setup_entry", return_value=True
+        "homeassistant.components.NEW_DOMAIN.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         yield mock_setup_entry
