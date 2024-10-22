f23488aa8e234290ab38d571595554c0dad77898
J. Nick Koston <nick@koston.org>
2024-10-21 16:46:43 -1000

    success reauth test

--- tests/components/xiaomi_ble/test_config_flow.py
+++ tests/components/xiaomi_ble/test_config_flow.py
@@ -2,7 +2,7 @@
 
 from unittest.mock import patch
 
-from xiaomi_ble import XiaomiBluetoothDeviceData as DeviceData
+from xiaomi_ble import XiaomiBluetoothDeviceData as DeviceData, XiaomiCloudBLEDevice
 
 from homeassistant import config_entries
 from homeassistant.components.bluetooth import BluetoothChange
@@ -1118,6 +1118,69 @@ async def test_async_step_reauth_v4_wrong_key(hass: HomeAssistant) -> None:
     )
     assert result4["type"] is FlowResultType.ABORT
     assert result4["reason"] == "reauth_successful"
+
+
+async def test_async_step_reauth_v4_from_cloud(hass: HomeAssistant) -> None:
+    """Test reauth with a v4 key from the cloud."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="54:EF:44:E3:9C:BC",
+    )
+    entry.add_to_hass(hass)
+    saved_callback = None
+
+    def _async_register_callback(_hass, _callback, _matcher, _mode):
+        nonlocal saved_callback
+        saved_callback = _callback
+        return lambda: None
+
+    with patch(
+        "homeassistant.components.bluetooth.update_coordinator.async_register_callback",
+        _async_register_callback,
+    ):
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    assert len(hass.states.async_all()) == 0
+
+    # WARNING: This test data is synthetic, rather than captured from a real device
+    # obj type is 0x1310, payload len is 0x2 and payload is 0x6000
+    saved_callback(
+        make_advertisement(
+            "54:EF:44:E3:9C:BC",
+            b"XY\x97\tf\xbc\x9c\xe3D\xefT\x01\x08\x12\x05\x00\x00\x00q^\xbe\x90",
+        ),
+        BluetoothChange.ADVERTISEMENT,
+    )
+
+    await hass.async_block_till_done()
+
+    results = hass.config_entries.flow.async_progress()
+    assert len(results) == 1
+    result = results[0]
+
+    assert result["step_id"] == "get_encryption_key_4_5_choose_method"
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={"next_step_id": "cloud_auth"},
+    )
+    device = XiaomiCloudBLEDevice(
+        name="x",
+        mac="54:EF:44:E3:9C:BC",
+        bindkey="5b51a7c91cde6707c9ef18dfda143a58",
+    )
+    with patch(
+        "homeassistant.components.xiaomi_ble.config_flow.XiaomiCloudTokenFetch.get_device_info",
+        return_value=device,
+    ):
+        result3 = await hass.config_entries.flow.async_configure(
+            result2["flow_id"],
+            user_input={"username": "x@x.x", "password": "x"},
+        )
+
+    assert result3["type"] is FlowResultType.ABORT
+    assert result3["reason"] == "reauth_successful"
 
 
 async def test_async_step_reauth_abort_early(hass: HomeAssistant) -> None:
