b0276f922e02c12a1476bfa2135ef4a8f3df5829
Jeroen Vaes <48211189+jeroenvaes@users.noreply.github.com>
2024-10-22 18:53:35 +0200

    fan 3 speed support, remove presets (#3)
    Co-authored-by: Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>

--- homeassistant/components/niko_home_control/fan.py
+++ homeassistant/components/niko_home_control/fan.py
@@ -25,7 +25,7 @@ async def async_setup_entry(
         entity = None
         action_type = action.action_type
         if action_type == 3:
-            NikoHomeControlFan(action, hub, options=entry.data["options"])
+            entity = NikoHomeControlFan(action, hub, options=entry.data["options"])
 
         if entity:
             hub.entities.append(entity)
@@ -44,11 +44,13 @@ class NikoHomeControlFan(FanEntity):
         self._attr_name = action.name
         self._attr_is_on = action.is_on
         self._attr_unique_id = f"fan-{action.action_id}"
-        self._fan_speed = action.state
+        self._attr_speed_count = 3
+        self._enable_turn_on_off_backwards_compatibility = False
         self._attr_supported_features = (
-            FanEntityFeature.SET_SPEED | FanEntityFeature.PRESET_MODE
+            FanEntityFeature.SET_SPEED
         )
-        self._preset_modes = ["low", "medium", "high", "very_high"]
+        self._percentages = [33, 66, 100]
+        self._attr_percentage = self._percentages[action.state]
 
         area = None
         if options["importLocations"] is not False:
@@ -59,6 +61,7 @@ class NikoHomeControlFan(FanEntity):
                 "manufacturer": "Niko",
                 "name": action.name,
                 "model": "P.O.M",
+                "suggested_area": action.location,
                 "via_device": hub._via_device,
                 "suggested_area": area,
             }
@@ -76,43 +79,31 @@ class NikoHomeControlFan(FanEntity):
         return self._action.action_id
 
     @property
-    def preset_mode(self) -> str:
-        """Return the current preset mode."""
-        return self._fan_speed
-
-    @property
     def supported_features(self):
         """Return supported features."""
         return self._attr_supported_features
 
     def set_percentage(self, percentage: int) -> None:
         """Set the fan speed preset based on a given percentage."""
-        mode = percentage_to_ordered_list_item(self._preset_modes, percentage)
-        if mode == "low":
-            self._action.set_fan_speed(0)
-        elif mode == "medium":
-            self._action.set_fan_speed(1)
-        elif mode == "high":
-            self._action.set_fan_speed(2)
-        elif mode == "very_high":
-            self._action.set_fan_speed(3)
+        mode = 0 # low
+        if percentage > 67:
+            mode = 2 # high
+        elif percentage <= 67 and percentage > 33:
+            mode = 1 # medium
 
-        self.schedule_update_ha_state()
-
-    def set_preset_mode(self, preset_mode: str) -> None:
-        """Set the preset mode of the fan."""
-        if preset_mode == "low":
-            self._action.set_fan_speed(0)
-        elif preset_mode == "medium":
-            self._action.set_fan_speed(1)
-        elif preset_mode == "high":
-            self._action.set_fan_speed(2)
-        elif preset_mode == "very_high":
-            self._action.set_fan_speed(3)
-
+        self._attr_percentage = self._percentages[mode]
+        self._action.set_fan_speed(mode)
         self.schedule_update_ha_state()
 
     def update_state(self, state):
         """Update HA state."""
-        self._fan_speed = state
-        self.schedule_update_ha_state()
+        if state > 2: # map "boost" to high state
+            state = 2
+
+        self._attr_percentage = self._percentages[state]
+        self.async_write_ha_state()
+
+    async def async_set_percentage(self, percentage: int) -> None:
+        """Set the speed of the fan, as a percentage."""
+        """Implemeted here as we do not want to call turn_off() when percentage == 0"""
+        await self.hass.async_add_executor_job(self.set_percentage, percentage)
