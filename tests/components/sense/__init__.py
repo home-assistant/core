0709955b4262d5ce476df632e4b88521250872e4
Keilin Bickar <TrumpetGod@gmail.com>
2024-10-23 10:51:40 -0400

    Update tests/components/sense/__init__.py
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>

--- tests/components/sense/__init__.py
+++ tests/components/sense/__init__.py
@@ -10,7 +10,7 @@ from tests.common import MockConfigEntry
 
 
 async def setup_platform(
-    hass: HomeAssistant, config_entry: MockConfigEntry, platform: str | None = None
+    hass: HomeAssistant, config_entry: MockConfigEntry, platform: Platform
 ) -> MockConfigEntry:
     """Set up the Sense platform."""
     config_entry.add_to_hass(hass)
