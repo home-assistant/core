28846dea1a1358fed610b28c9ebbf9eb4304c132
Mike Degatano <michael.degatano@gmail.com>
2024-10-24 16:45:35 -0400

    Fix null hass error in supervisor update entities (#129030)
    * Fix null hass error in supervisor update entities
    
    * Share the supervisor client with coordinator
    
    * Remove unnecessary patch of helper
    
    * Attribute not property

--- homeassistant/components/hassio/update.py
+++ homeassistant/components/hassio/update.py
@@ -17,7 +17,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_ICON, ATTR_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import (
@@ -31,7 +30,6 @@ from .const import (
     DATA_KEY_OS,
     DATA_KEY_SUPERVISOR,
 )
-from .coordinator import HassioDataUpdateCoordinator
 from .entity import (
     HassioAddonEntity,
     HassioCoreEntity,
@@ -43,7 +41,6 @@ from .handler import (
     async_update_core,
     async_update_os,
     async_update_supervisor,
-    get_supervisor_client,
 )
 
 ENTITY_DESCRIPTION = UpdateEntityDescription(
@@ -99,16 +96,6 @@ class SupervisorAddonUpdateEntity(HassioAddonEntity, UpdateEntity):
         | UpdateEntityFeature.BACKUP
         | UpdateEntityFeature.RELEASE_NOTES
     )
-
-    def __init__(
-        self,
-        coordinator: HassioDataUpdateCoordinator,
-        entity_description: EntityDescription,
-        addon: dict[str, Any],
-    ) -> None:
-        """Initialize object."""
-        super().__init__(coordinator, entity_description, addon)
-        self._supervisor_client = get_supervisor_client(self.hass)
 
     @property
     def _addon_data(self) -> dict:
@@ -179,7 +166,7 @@ class SupervisorAddonUpdateEntity(HassioAddonEntity, UpdateEntity):
     ) -> None:
         """Install an update."""
         try:
-            await self._supervisor_client.store.update_addon(
+            await self.coordinator.supervisor_client.store.update_addon(
                 self._addon_slug, StoreAddonUpdate(backup=backup)
             )
         except SupervisorError as err:
