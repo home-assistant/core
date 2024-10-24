b52f8c85f3583d02733c77bea392f6c546c64af7
Joost Lekkerkerker <joostlek@outlook.com>
2024-10-24 14:54:19 +0200

    Add unique id and tests for Smarty (#129078)

--- tests/components/smarty/conftest.py
+++ tests/components/smarty/conftest.py
@@ -37,10 +37,24 @@ def mock_smarty() -> Generator[AsyncMock]:
     ):
         client = mock_client.return_value
         client.update.return_value = True
+        client.fan_speed = 100
+        client.warning = False
+        client.alarm = False
+        client.boost = False
+        client.supply_air_temperature = 20
+        client.extract_air_temperature = 23
+        client.outdoor_air_temperature = 24
+        client.supply_fan_speed = 66
+        client.extract_fan_speed = 100
+        client.filter_timer = 31
         yield client
 
 
 @pytest.fixture
 def mock_config_entry() -> MockConfigEntry:
     """Return the default mocked config entry."""
-    return MockConfigEntry(domain=DOMAIN, data={CONF_HOST: "192.168.0.2"})
+    return MockConfigEntry(
+        domain=DOMAIN,
+        data={CONF_HOST: "192.168.0.2"},
+        entry_id="01JAZ5DPW8C62D620DGYNG2R8H",
+    )
