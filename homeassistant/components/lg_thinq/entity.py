08abbfafb837c9e4aacad76125bf52ca41845ded
LG-ThinQ-Integration <LG-ThinQ-Integration@lge.com>
2024-10-25 11:14:33 +0900

    Update entity.py

--- homeassistant/components/lg_thinq/entity.py
+++ homeassistant/components/lg_thinq/entity.py
@@ -102,14 +102,13 @@ class ThinQEntity(CoordinatorEntity[DeviceDataUpdateCoordinator]):
         """Call the given api and handle exception."""
         try:
             await target
-        except (ThinQAPIException, ValueError) as exc:
+        except ThinQAPIException as exc:
             if on_fail_method:
                 on_fail_method()
-
             raise ServiceValidationError(
-                exc.message if isinstance(exc, ThinQAPIException) else str(exc),
-                translation_domain=DOMAIN,
-                translation_key=(
-                    exc.code if isinstance(exc, ThinQAPIException) else None
-                ),
+                exc.message, translation_domain=DOMAIN, translation_key=exc.code
             ) from exc
+        except ValueError as exc:
+            if on_fail_method:
+                on_fail_method()
+            raise ServiceValidationError(exc) from exc
