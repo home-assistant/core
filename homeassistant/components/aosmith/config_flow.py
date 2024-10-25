d4d2bda6456e323ae5014c8d12133373e5a97599
Christopher Fenner <Christopher.Fenner@me.com>
2024-10-25 12:01:55 +0200

    fix reauth title

--- homeassistant/components/aosmith/config_flow.py
+++ homeassistant/components/aosmith/config_flow.py
@@ -10,7 +10,7 @@ from py_aosmith import AOSmithAPIClient, AOSmithInvalidCredentialsException
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
-from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
+from homeassistant.const import CONF_EMAIL, CONF_NAME, CONF_PASSWORD
 from homeassistant.helpers import aiohttp_client
 
 from .const import DOMAIN
@@ -99,6 +99,9 @@ class AOSmithConfigFlow(ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="reauth_confirm",
             data_schema=vol.Schema({vol.Required(CONF_PASSWORD): str}),
-            description_placeholders={CONF_EMAIL: self._reauth_email},
+            description_placeholders={
+                CONF_EMAIL: self._reauth_email,
+                CONF_NAME: self._get_reauth_entry().title,
+            },
             errors=errors,
         )
