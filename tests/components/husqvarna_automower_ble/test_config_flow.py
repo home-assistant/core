4775da17bd7dd65b2c1903c5dc4be373cc661ce4
Joostlek <joostlek@outlook.com>
2024-10-25 15:29:07 +0200

    Fix config flow tests

--- tests/components/husqvarna_automower_ble/test_config_flow.py
+++ tests/components/husqvarna_automower_ble/test_config_flow.py
@@ -17,6 +17,7 @@ from . import (
     AUTOMOWER_UNSUPPORTED_GROUP_SERVICE_INFO,
 )
 
+from tests.common import MockConfigEntry
 from tests.components.bluetooth import inject_bluetooth_service_info
 
 pytestmark = pytest.mark.usefixtures("mock_setup_entry")
@@ -56,6 +57,7 @@ async def test_user_selection(hass: HomeAssistant) -> None:
         result["flow_id"],
         user_input={},
     )
+    assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "Husqvarna Automower"
     assert result["result"].unique_id == "00000000-0000-0000-0000-000000000001"
 
@@ -79,6 +81,7 @@ async def test_bluetooth(hass: HomeAssistant) -> None:
         result["flow_id"],
         user_input={},
     )
+    assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "Husqvarna Automower"
     assert result["result"].unique_id == "00000000-0000-0000-0000-000000000003"
 
@@ -132,6 +135,7 @@ async def test_failed_connect(
         result["flow_id"],
         user_input={},
     )
+    assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "Husqvarna Automower"
     assert result["result"].unique_id == "00000000-0000-0000-0000-000000000001"
 
@@ -139,6 +143,36 @@ async def test_failed_connect(
         CONF_ADDRESS: "00000000-0000-0000-0000-000000000001",
         CONF_CLIENT_ID: 1197489078,
     }
+
+
+async def test_duplicate_entry(
+    hass: HomeAssistant,
+    mock_automower_client: Mock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test we can select a device."""
+
+    mock_config_entry.add_to_hass(hass)
+
+    inject_bluetooth_service_info(hass, AUTOMOWER_SERVICE_INFO)
+
+    await hass.async_block_till_done(wait_background_tasks=True)
+
+    # Test we should not discover the already configured device
+    assert len(hass.config_entries.flow.async_progress_by_handler(DOMAIN)) == 0
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={CONF_ADDRESS: "00000000-0000-0000-0000-000000000003"},
+    )
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "already_configured"
 
 
 async def test_exception_connect(
