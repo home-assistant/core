76aa69b9ac35f69604ae82bca265bd2d37b05b24
David Bonnes <zxdavb@bonnes.me>
2024-10-25 09:57:37 +0100

    Switch to using a fixture for evohome Climate tests (of zones) (#129100)

--- tests/components/evohome/conftest.py
+++ tests/components/evohome/conftest.py
@@ -11,12 +11,14 @@ from unittest.mock import MagicMock, patch
 from aiohttp import ClientSession
 from evohomeasync2 import EvohomeClient
 from evohomeasync2.broker import Broker
+from evohomeasync2.zone import Zone
 import pytest
 
 from homeassistant.components.evohome import CONF_PASSWORD, CONF_USERNAME, DOMAIN
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
-from homeassistant.util import dt as dt_util
+from homeassistant.util import dt as dt_util, slugify
 from homeassistant.util.json import JsonArrayType, JsonObjectType
 
 from .const import ACCESS_TOKEN, REFRESH_TOKEN, USERNAME
@@ -173,3 +175,18 @@ async def evohome(
 
     async for mock_client in setup_evohome(hass, config, install=install):
         yield mock_client
+
+
+@pytest.fixture
+async def zone_id(
+    hass: HomeAssistant,
+    config: dict[str, str],
+    install: MagicMock,
+) -> AsyncGenerator[str]:
+    """Return the entity_id of the evohome integration' first Climate zone."""
+
+    async for mock_client in setup_evohome(hass, config, install=install):
+        evo: EvohomeClient = mock_client.return_value
+        zone: Zone = list(evo._get_single_tcs().zones.values())[0]
+
+        yield f"{Platform.CLIMATE}.{slugify(zone.name)}"
