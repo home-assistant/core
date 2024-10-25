4119fe38e74ce50101f319db2947b2caaa336ee3
epenet <6771947+epenet@users.noreply.github.com>
2024-10-25 14:35:41 +0000

    Improve

--- homeassistant/components/airthings_ble/__init__.py
+++ homeassistant/components/airthings_ble/__init__.py
@@ -2,12 +2,8 @@
 
 from __future__ import annotations
 
-from bleak_retry_connector import close_stale_connections_by_address
-
-from homeassistant.components import bluetooth
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import MAX_RETRIES_AFTER_STARTUP
 from .coordinator import AirthingsBLEConfigEntry, AirthingsBLEDataUpdateCoordinator
@@ -19,21 +15,7 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: AirthingsBLEConfigEntry
 ) -> bool:
     """Set up Airthings BLE device from a config entry."""
-    address = entry.unique_id
-
-    assert address is not None
-
-    await close_stale_connections_by_address(address)
-
-    ble_device = bluetooth.async_ble_device_from_address(hass, address)
-
-    if not ble_device:
-        raise ConfigEntryNotReady(
-            f"Could not find Airthings device with address {address}"
-        )
-
-    coordinator = AirthingsBLEDataUpdateCoordinator(hass, ble_device)
-
+    coordinator = AirthingsBLEDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
 
     # Once its setup and we know we are not going to delay
--- homeassistant/components/airthings_ble/coordinator.py
+++ homeassistant/components/airthings_ble/coordinator.py
@@ -7,9 +7,12 @@ import logging
 
 from airthings_ble import AirthingsBluetoothDeviceData, AirthingsDevice
 from bleak.backends.device import BLEDevice
+from bleak_retry_connector import close_stale_connections_by_address
 
+from homeassistant.components import bluetooth
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 from homeassistant.util.unit_system import METRIC_SYSTEM
 
@@ -23,18 +26,37 @@ type AirthingsBLEConfigEntry = ConfigEntry[AirthingsBLEDataUpdateCoordinator]
 class AirthingsBLEDataUpdateCoordinator(DataUpdateCoordinator[AirthingsDevice]):
     """Class to manage fetching Airthings BLE data."""
 
-    def __init__(self, hass: HomeAssistant, ble_device: BLEDevice) -> None:
+    ble_device: BLEDevice
+    config_entry: AirthingsBLEConfigEntry
+
+    def __init__(self, hass: HomeAssistant, entry: AirthingsBLEConfigEntry) -> None:
         """Initialize the coordinator."""
         self.airthings = AirthingsBluetoothDeviceData(
             _LOGGER, hass.config.units is METRIC_SYSTEM
         )
-        self.ble_device = ble_device
         super().__init__(
             hass,
             _LOGGER,
+            config_entry=entry,
             name=DOMAIN,
             update_interval=timedelta(seconds=DEFAULT_SCAN_INTERVAL),
         )
+
+    async def _async_setup(self) -> None:
+        """Set up the coordinator."""
+        address = self.config_entry.unique_id
+
+        assert address is not None
+
+        await close_stale_connections_by_address(address)
+
+        ble_device = bluetooth.async_ble_device_from_address(self.hass, address)
+
+        if not ble_device:
+            raise ConfigEntryNotReady(
+                f"Could not find Airthings device with address {address}"
+            )
+        self.ble_device = ble_device
 
     async def _async_update_data(self) -> AirthingsDevice:
         """Get data from Airthings BLE."""
