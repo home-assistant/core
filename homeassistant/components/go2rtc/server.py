19b8f444cd4e1eb8ceb7faa1d91cc94098f656f5
J. Nick Koston <nick@koston.org>
2024-10-25 16:53:38 -1000

    Fix go2rtc server start not using posix_spawn/vfork
    Without close_fds=False the legacy fork()/exec() model is used with Python older
    than 3.13

--- homeassistant/components/go2rtc/server.py
+++ homeassistant/components/go2rtc/server.py
@@ -62,6 +62,7 @@ class Server:
             config_file,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.STDOUT,
+            close_fds=False,  # required for posix_spawn on cPython < 3.13
         )
 
         self._hass.async_create_background_task(
