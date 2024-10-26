9e6af468f9eaab94728e547e7b013c1bd01764e7
Simone Chemelli <simone.chemelli@gmail.com>
2024-10-26 16:14:12 +0000

    rework approach based on IQS021 rule

--- homeassistant/components/vodafone_station/coordinator.py
+++ homeassistant/components/vodafone_station/coordinator.py
@@ -7,14 +7,11 @@ from typing import Any
 
 from aiovodafone import VodafoneStationDevice, VodafoneStationSercommApi, exceptions
 
-from homeassistant.components.device_tracker import (
-    DEFAULT_CONSIDER_HOME,
-    DOMAIN as DEVICE_TRACKER_DOMAIN,
-)
+from homeassistant.components.device_tracker import DEFAULT_CONSIDER_HOME
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 from homeassistant.util import dt as dt_util
@@ -69,6 +66,8 @@ class VodafoneStationRouter(DataUpdateCoordinator[UpdateCoordinatorDataType]):
             update_interval=timedelta(seconds=SCAN_INTERVAL),
         )
 
+        self.previous_devices: set[str] = set({})
+
     def _calculate_update_time_and_consider_home(
         self, device: VodafoneStationDevice, utc_point_in_time: datetime
     ) -> tuple[datetime | None, bool]:
@@ -122,8 +121,6 @@ class VodafoneStationRouter(DataUpdateCoordinator[UpdateCoordinatorDataType]):
             await self.api.close()
             raise
 
-        await self._cleanup_device_tracker_entities()
-
         utc_point_in_time = dt_util.utcnow()
         data_devices = {
             dev_info.mac: VodafoneStationDeviceInfo(
@@ -134,72 +131,21 @@ class VodafoneStationRouter(DataUpdateCoordinator[UpdateCoordinatorDataType]):
             )
             for dev_info in (raw_data_devices).values()
         }
+        current_devices = set(data_devices)
+        if stale_devices := self.previous_devices - current_devices:
+            device_registry = dr.async_get(self.hass)
+            for device_id in stale_devices:
+                device = device_registry.async_get_device(
+                    identifiers={(DOMAIN, device_id)}
+                )
+                if device:
+                    device_registry.async_update_device(
+                        device_id=device.id,
+                        remove_config_entry_id=self.config_entry.entry_id,
+                    )
+            self.previous_devices = current_devices
+
         return UpdateCoordinatorDataType(data_devices, data_sensors)
-
-    async def _cleanup_device_tracker_entities(self) -> None:
-        """Cleanup stale device trackers."""
-        entity_reg: er.EntityRegistry = er.async_get(self.hass)
-
-        ha_entity_reg_list: list[er.RegistryEntry] = er.async_entries_for_config_entry(
-            entity_reg, self.config_entry.entry_id
-        )
-        entities_removed: bool = False
-
-        device_hosts_macs = set()
-        device_hosts_names = set()
-        for mac, device_info in self.data.devices.items():
-            device_hosts_macs.add(mac)
-            device_hosts_names.add(device_info.device.name)
-
-        for entry in ha_entity_reg_list:
-            if entry.original_name is None:
-                continue
-            entry_name = entry.name or entry.original_name
-            entry_host = entry_name.split(" ")[0]
-            entry_mac = entry.unique_id.split("_")[0]
-
-            if entry.platform != DEVICE_TRACKER_DOMAIN or (
-                entry_mac in device_hosts_macs and entry_host in device_hosts_names
-            ):
-                _LOGGER.debug(
-                    "Skipping entity %s [mac=%s, host=%s]",
-                    entry_name,
-                    entry_mac,
-                    entry_host,
-                )
-                continue
-            _LOGGER.info("Removing entity: %s", entry_name)
-            entity_reg.async_remove(entry.entity_id)
-            entities_removed = True
-
-        if entities_removed:
-            self._async_remove_empty_devices(entity_reg, self.config_entry)
-
-    @callback
-    def _async_remove_empty_devices(
-        self, entity_reg: er.EntityRegistry, config_entry: ConfigEntry
-    ) -> None:
-        """Remove devices with no entities."""
-
-        device_reg = dr.async_get(self.hass)
-        device_list = dr.async_entries_for_config_entry(
-            device_reg, config_entry.entry_id
-        )
-        for device_entry in device_list:
-            if not er.async_entries_for_device(
-                entity_reg,
-                device_entry.id,
-                include_disabled_entities=True,
-            ):
-                _LOGGER.info("Removing device: %s", device_entry.name)
-                device_reg.async_update_device(
-                    device_entry.id, remove_config_entry_id=self.config_entry.entry_id
-                )
-
-    async def async_setup(self) -> None:
-        """Set up the coordinator."""
-        await self.async_config_entry_first_refresh()
-        await self._cleanup_device_tracker_entities()
 
     @property
     def signal_device_new(self) -> str:
