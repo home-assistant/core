a2a24912bb3a6c840de7c23bcb0f4bfb6f55902b
Simone Chemelli <simone.chemelli@gmail.com>
2024-10-26 16:14:12 +0000

    rework approach based on IQS021 rule

--- homeassistant/components/vodafone_station/__init__.py
+++ homeassistant/components/vodafone_station/__init__.py
@@ -20,7 +20,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         entry.unique_id,
     )
 
-    await coordinator.async_setup()
+    await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
