9774bdf8e92b581fdc5b0b2ea119e220412747f3
Nick Conway <nick@conway.dev>
2024-10-27 15:18:31 -0400

    feat(switchbot-keypad-touch): convert variables to snakecase

--- homeassistant/components/switchbot/sensor.py
+++ homeassistant/components/switchbot/sensor.py
@@ -75,8 +75,8 @@ SENSOR_TYPES: dict[str, SensorEntityDescription] = {
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.POWER,
     ),
-    "attemptState": SensorEntityDescription(
-        key="attemptState",
+    "attempt_state": SensorEntityDescription(
+        key="attempt_state",
         translation_key="attempt_state",
         native_unit_of_measurement="",
         state_class=SensorStateClass.MEASUREMENT,
