3f8ca8ccf02d35c8b120a58270964e97d900fbc4
Petro <mhpetrosino@yahoo.com>
2024-10-27 14:50:09 +0000

    fix mypy and codespell

--- homeassistant/components/template/switch.py
+++ homeassistant/components/template/switch.py
@@ -106,7 +106,7 @@ SWITCH_CONFIG_SCHEMA = vol.Schema(
 def rewrite_legacy_to_modern_conf(
     hass: HomeAssistant, config: dict[str, dict]
 ) -> list[dict]:
-    """Rewrite legacy switch configuration defitions to modern ones."""
+    """Rewrite legacy switch configuration definitions to modern ones."""
     switches = []
 
     for object_id, entity_conf in config.items():
@@ -222,15 +222,15 @@ class SwitchTemplate(TemplateEntity, SwitchEntity, RestoreEntity):
         super().__init__(
             hass, config=config, fallback_name=object_id, unique_id=unique_id
         )
-        friendly_name = self._attr_name
+        friendly_name = self._attr_name or DEFAULT_NAME
         self._template = config.get(CONF_STATE)
         self._on_script = (
-            Script(hass, config.get(CONF_TURN_ON), friendly_name, DOMAIN)
+            Script(hass, config[CONF_TURN_ON], friendly_name, DOMAIN)
             if config.get(CONF_TURN_ON) is not None
             else None
         )
         self._off_script = (
-            Script(hass, config.get(CONF_TURN_OFF), friendly_name, DOMAIN)
+            Script(hass, config[CONF_TURN_OFF], friendly_name, DOMAIN)
             if config.get(CONF_TURN_OFF) is not None
             else None
         )
@@ -313,10 +313,8 @@ class TriggerSwitchEntity(TriggerEntity, SwitchEntity, RestoreEntity):
         """Initialize the entity."""
         super().__init__(hass, coordinator, config)
         friendly_name = self._rendered.get(CONF_NAME, DEFAULT_NAME)
-        self._on_script = Script(hass, config.get(CONF_TURN_ON), friendly_name, DOMAIN)
-        self._off_script = Script(
-            hass, config.get(CONF_TURN_OFF), friendly_name, DOMAIN
-        )
+        self._on_script = Script(hass, config[CONF_TURN_ON], friendly_name, DOMAIN)
+        self._off_script = Script(hass, config[CONF_TURN_OFF], friendly_name, DOMAIN)
 
         self._state: bool | None = None
         if (tmpl := config.get(CONF_STATE)) is not None and isinstance(
@@ -362,8 +360,8 @@ class TriggerSwitchEntity(TriggerEntity, SwitchEntity, RestoreEntity):
             self.async_set_context(self.coordinator.data["context"])
             self.async_write_ha_state()
         elif self._attr_assumed_state and len(self._rendered) > 0:
-            # Incase name, icon, or friendly name have a template but
-            # states does not.
+            # In case name, icon, or friendly name have a template but
+            # states does not
             self.async_write_ha_state()
 
     @property
