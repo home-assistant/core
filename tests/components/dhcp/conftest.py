9f13cdcf284e58eef9d7d55669d423209bbf9671
epenet <6771947+epenet@users.noreply.github.com>
2024-10-28 14:03:54 +0100

    Update conftest.py

--- tests/components/dhcp/conftest.py
+++ tests/components/dhcp/conftest.py
@@ -12,7 +12,7 @@ def _ensure_path_exists():
 
     See https://github.com/secdev/scapy/pull/4558
     """
-    for file in (".cache", ".config"):
-        path = pathlib.Path(os.path.join(os.path.expanduser("~"), file))
+    for sub_dir in (".cache", ".config"):
+        path = pathlib.Path(os.path.join(os.path.expanduser("~"), sub_dir))
         if not path.exists():
             path.mkdir(mode=0o700, exist_ok=True)
