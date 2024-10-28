e7b082eddde43a1d2ae463144f22f4f29e4e1495
Erik <erik@montnemery.com>
2024-10-28 14:28:05 +0100

    Refactor esphome ffmpeg proxy

--- homeassistant/components/esphome/ffmpeg_proxy.py
+++ homeassistant/components/esphome/ffmpeg_proxy.py
@@ -131,11 +131,10 @@ class FFmpegConvertResponse(web.StreamResponse):
         self.proxy_data = proxy_data
         self.chunk_size = chunk_size
 
-    async def prepare(self, request: BaseRequest) -> AbstractStreamWriter | None:
+    async def transcode(
+        self, request: BaseRequest, writer: AbstractStreamWriter
+    ) -> None:
         """Stream url through ffmpeg conversion and out to HTTP client."""
-        writer = await super().prepare(request)
-        assert writer is not None
-
         command_args = [
             "-i",
             self.convert_info.media_url,
@@ -172,6 +171,14 @@ class FFmpegConvertResponse(web.StreamResponse):
         # Only one conversion process per device is allowed
         self.convert_info.proc = proc
 
+        await self._write_ffmpeg_data(request, writer, proc)
+
+    async def _write_ffmpeg_data(
+        self,
+        request: BaseRequest,
+        writer: AbstractStreamWriter,
+        proc: asyncio.subprocess.Process,
+    ) -> None:
         assert proc.stdout is not None
         assert proc.stderr is not None
 
@@ -184,8 +191,7 @@ class FFmpegConvertResponse(web.StreamResponse):
                 and (proc.returncode is None)
                 and (chunk := await proc.stdout.read(self.chunk_size))
             ):
-                await writer.write(chunk)
-                await writer.drain()
+                await self.write(chunk)
         except asyncio.CancelledError:
             raise  # don't log error
         except:
@@ -205,8 +211,6 @@ class FFmpegConvertResponse(web.StreamResponse):
 
             # Close connection
             await writer.write_eof()
-
-        return writer
 
 
 class FFmpegProxyView(HomeAssistantView):
@@ -246,6 +250,10 @@ class FFmpegProxyView(HomeAssistantView):
             convert_info.proc = None
 
         # Stream converted audio back to client
-        return FFmpegConvertResponse(
+        resp = FFmpegConvertResponse(
             self.manager, convert_info, device_id, self.proxy_data
         )
+        writer = await resp.prepare(request)
+        assert writer is not None
+        await resp.transcode(request, writer)
+        return resp
