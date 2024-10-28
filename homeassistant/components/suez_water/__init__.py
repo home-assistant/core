8f9f8182cb07f714515b8a847d80c823f1ccc3fc
jb101010-2 <dev.julien.basson@gmail.com>
2024-10-28 12:29:32 +0100

    Add first test to suez init

--- homeassistant/components/suez_water/__init__.py
+++ homeassistant/components/suez_water/__init__.py
@@ -18,31 +18,37 @@ from .coordinator import SuezWaterCoordinator
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 
+def _get_client(entry: ConfigEntry) -> SuezClient:
+    try:
+        client = SuezClient(
+            entry.data[CONF_USERNAME],
+            entry.data[CONF_PASSWORD],
+            entry.data[CONF_COUNTER_ID],
+            provider=None,
+        )
+        if not client.check_credentials():
+            raise ConfigEntryError
+    except PySuezError as ex:
+        raise ConfigEntryNotReady from ex
+    return client
+
+
+async def _get_coordinator(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> SuezWaterCoordinator:
+    loop = asyncio.get_running_loop()
+    client = await loop.run_in_executor(None, _get_client, entry)
+    coordinator = SuezWaterCoordinator(hass, client, entry.data[CONF_COUNTER_ID])
+    await coordinator.async_config_entry_first_refresh()
+    return coordinator
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Suez Water from a config entry."""
 
-    def get_client() -> SuezClient:
-        try:
-            client = SuezClient(
-                entry.data[CONF_USERNAME],
-                entry.data[CONF_PASSWORD],
-                entry.data[CONF_COUNTER_ID],
-                provider=None,
-            )
-            if not client.check_credentials():
-                raise ConfigEntryError
-        except PySuezError as ex:
-            raise ConfigEntryNotReady from ex
-        return client
-
-    async def get_coordinator() -> SuezWaterCoordinator:
-        loop = asyncio.get_running_loop()
-        client = await loop.run_in_executor(None, get_client)
-        coordinator = SuezWaterCoordinator(hass, client, entry.data[CONF_COUNTER_ID])
-        await coordinator.async_config_entry_first_refresh()
-        return coordinator
-
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = await get_coordinator()
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = await _get_coordinator(
+        hass, entry
+    )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
--- tests/components/suez_water/conftest.py
+++ tests/components/suez_water/conftest.py
@@ -3,7 +3,37 @@
 from collections.abc import Generator
 from unittest.mock import AsyncMock, patch
 
+from pysuez.client import SuezClient
 import pytest
+
+from homeassistant.components.suez_water.const import DOMAIN
+from homeassistant.components.suez_water.coordinator import SuezWaterCoordinator
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+
+MOCK_DATA = {
+    "username": "test-username",
+    "password": "test-password",
+    "counter_id": "test-counter",
+}
+
+
+async def create_config_entry(hass: HomeAssistant) -> MockConfigEntry:
+    """Create an entry in hass."""
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="Suez mock device",
+        data=MOCK_DATA,
+    )
+
+    entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    return entry
 
 
 @pytest.fixture
@@ -13,3 +43,38 @@ def mock_setup_entry() -> Generator[AsyncMock]:
         "homeassistant.components.suez_water.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         yield mock_setup_entry
+
+
+@pytest.fixture
+def mock_client() -> Generator[None]:
+    """Fixture to mock _get_devices which makes a call to the API."""
+
+    client = SuezClient(
+        MOCK_DATA["username"],
+        MOCK_DATA["password"],
+        MOCK_DATA["counter_id"],
+        provider=None,
+    )
+
+    with patch(
+        "homeassistant.components.suez_water._get_client",
+        return_value=client,
+    ):
+        yield
+
+
+@pytest.fixture
+def mock_coordinator(mock_client: SuezClient, hass: HomeAssistant) -> Generator[None]:
+    """Fixture to mock _get_devices which makes a call to the API."""
+
+    coordinator = SuezWaterCoordinator(
+        hass,
+        mock_client,
+        MOCK_DATA["counter_id"],
+    )
+
+    with patch(
+        "homeassistant.components.suez_water._get_coordinator",
+        return_value=coordinator,
+    ):
+        yield
