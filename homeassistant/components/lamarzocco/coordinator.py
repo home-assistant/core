7b6b24963863793d8af820f9ac67aa5173182c09
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-10-29 16:47:50 +0100

    Update coordinator.py

--- homeassistant/components/lamarzocco/coordinator.py
+++ homeassistant/components/lamarzocco/coordinator.py
@@ -4,7 +4,7 @@ from collections.abc import Callable, Coroutine
 from datetime import timedelta
 import logging
 from time import time
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 from lmcloud.client_bluetooth import LaMarzoccoBluetoothClient
 from lmcloud.client_cloud import LaMarzoccoCloudClient
@@ -26,21 +26,30 @@ STATISTICS_UPDATE_INTERVAL = 300
 
 _LOGGER = logging.getLogger(__name__)
 
+if TYPE_CHECKING:
+    from . import LaMarzoccoConfigEntry
 
 class LaMarzoccoUpdateCoordinator(DataUpdateCoordinator[None]):
     """Class to handle fetching data from the La Marzocco API centrally."""
 
-    config_entry: ConfigEntry
+    config_entry: LaMarzoccoConfigEntry
 
     def __init__(
         self,
         hass: HomeAssistant,
+        entry: LaMarzoccoConfigEntry,
         cloud_client: LaMarzoccoCloudClient,
         local_client: LaMarzoccoLocalClient | None,
         bluetooth_client: LaMarzoccoBluetoothClient | None,
     ) -> None:
         """Initialize coordinator."""
-        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=SCAN_INTERVAL)
+        super().__init__(
+            hass,
+            _LOGGER,
+            config_entry=entry,
+            name=DOMAIN,
+            update_interval=SCAN_INTERVAL
+        )
         self.local_connection_configured = local_client is not None
 
         assert self.config_entry.unique_id
