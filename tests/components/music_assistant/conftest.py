639b00e14e7acfc40418620481f51c2cf9b7ded7
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-10-30 19:54:19 +0100

    Add client fixture

--- tests/components/music_assistant/conftest.py
+++ tests/components/music_assistant/conftest.py
@@ -1,15 +1,19 @@
 """Music Assistant test fixtures."""
 
-from collections.abc import Generator
-from unittest.mock import patch
+import asyncio
+from collections.abc import AsyncGenerator, Generator
+from unittest.mock import MagicMock, patch
 
 from music_assistant_models.api import ServerInfoMessage
 import pytest
 
 from homeassistant.components.music_assistant.config_flow import CONF_URL
 from homeassistant.components.music_assistant.const import DOMAIN
+from homeassistant.core import HomeAssistant
 
 from tests.common import AsyncMock, MockConfigEntry, load_fixture
+
+MOCK_SERVER_ID = "1234"
 
 
 @pytest.fixture
@@ -22,6 +26,53 @@ def mock_get_server_info() -> Generator[AsyncMock]:
             load_fixture("server_info_message.json", DOMAIN)
         )
         yield mock_get_server_info
+
+
+@pytest.fixture(name="music_assistant_client")
+async def music_assistant_client_fixture() -> AsyncGenerator[MagicMock]:
+    """Fixture for a Music Assistant client."""
+    with patch(
+        "homeassistant.components.music_assistant.MusicAssistantClient", autospec=True
+    ) as client_class:
+        client = client_class.return_value
+
+        async def connect() -> None:
+            """Mock connect."""
+            await asyncio.sleep(0)
+
+        async def listen(init_ready: asyncio.Event | None) -> None:
+            """Mock listen."""
+            if init_ready is not None:
+                init_ready.set()
+            listen_block = asyncio.Event()
+            await listen_block.wait()
+            pytest.fail("Listen was not cancelled!")
+
+        client.connect = AsyncMock(side_effect=connect)
+        client.start_listening = AsyncMock(side_effect=listen)
+        client.server_info = ServerInfoMessage(
+            server_id=MOCK_SERVER_ID,
+            server_version="0.0.0",
+            schema_version=1,
+            min_supported_schema_version=1,
+            base_url="http://localhost:8095",
+            homeassistant_addon=False,
+            onboard_done=True,
+        )
+
+        yield client
+
+
+@pytest.fixture(name="integration")
+async def integration_fixture(
+    hass: HomeAssistant, music_assistant_client: MagicMock
+) -> MockConfigEntry:
+    """Set up the Music Assistant integration."""
+    entry = MockConfigEntry(domain=DOMAIN, data={CONF_URL: "http://localhost:8095"})
+    entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+    return entry
 
 
 @pytest.fixture
