56b2c6362e29a42d95994b931d5a6defc6beae14
Steven B <51370195+sdb9696@users.noreply.github.com>
2024-10-30 17:28:14 +0000

    Use webrtc async offer methods

--- homeassistant/components/ring/camera.py
+++ homeassistant/components/ring/camera.py
@@ -11,6 +11,7 @@ from typing import TYPE_CHECKING, Any, Generic
 from aiohttp import web
 from haffmpeg.camera import CameraMjpeg
 from ring_doorbell import RingDoorBell
+from ring_doorbell.webrtcstream import RingWebRtcMessage
 
 from homeassistant.components import ffmpeg
 from homeassistant.components.camera import (
@@ -18,6 +19,10 @@ from homeassistant.components.camera import (
     CameraEntityDescription,
     CameraEntityFeature,
     StreamType,
+    WebRTCAnswer,
+    WebRTCCandidate,
+    WebRTCError,
+    WebRTCSendMessage,
 )
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.aiohttp_client import async_aiohttp_proxy_stream
@@ -111,6 +116,7 @@ class RingCam(RingEntity[RingDoorBell], Camera):
         if description.live_stream:
             self._attr_supported_features |= CameraEntityFeature.STREAM
             self._attr_frontend_stream_type = StreamType.WEB_RTC
+        self._webrtc_sync_offer = True
 
     @callback
     def _handle_coordinator_update(self) -> None:
@@ -179,9 +185,32 @@ class RingCam(RingEntity[RingDoorBell], Camera):
         finally:
             await stream.close()
 
-    async def async_handle_web_rtc_offer(self, offer_sdp: str) -> str | None:
+    async def async_handle_async_webrtc_offer(
+        self, offer_sdp: str, session_id: str, send_message: WebRTCSendMessage
+    ) -> None:
         """Return the source of the stream."""
-        return await self._device.generate_webrtc_stream(offer_sdp)
+
+        def message_wrapper(ring_message: RingWebRtcMessage) -> None:
+            if ring_message.error_code:
+                msg = ring_message.error_message or ""
+                send_message(WebRTCError(ring_message.error_code, msg))
+            elif ring_message.answer:
+                send_message(WebRTCAnswer(ring_message.answer))
+            elif ring_message.candidate:
+                send_message(WebRTCCandidate(ring_message.candidate))
+
+        return await self._device.generate_async_webrtc_stream(
+            offer_sdp, session_id, message_wrapper
+        )
+
+    async def async_on_webrtc_candidate(self, session_id: str, candidate: str) -> None:
+        """Handle a WebRTC candidate."""
+        await self._device.on_webrtc_candidate(session_id, candidate)
+
+    @callback
+    def close_webrtc_session(self, session_id: str) -> None:
+        """Close a WebRTC session."""
+        self._device.sync_close_webrtc_stream(session_id)
 
     async def async_update(self) -> None:
         """Update camera entity and refresh attributes."""
