7f7f0500e131f91dc8eeab3f506e2619210df053
Brett <brett.whynot@gmail.com>
2024-10-30 23:28:32 +0000

    refactor for mypy

--- homeassistant/components/tesla_fleet/device_tracker.py
+++ homeassistant/components/tesla_fleet/device_tracker.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import STATE_HOME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.restore_state import RestoreEntity
@@ -84,7 +85,7 @@ class TeslaFleetDeviceTrackerRouteEntity(TeslaFleetDeviceTrackerEntity):
     @property
     def location_name(self) -> str | None:
         """Return a location name for the current location of the device."""
-        if location := self.get("drive_state_active_route_name") == "Home":
-            # Return the translatable name for home
-            return "home"
+        location = self.get("drive_state_active_route_name")
+        if location == "Home":
+            return STATE_HOME
         return location
