23dcf6ab8cebf9e9c46c889e1f12c9d7720bd31a
peteS-UK <peter@sketch.cc>
2024-10-31 19:47:00 +0000

    Move play_announcement to _player

--- homeassistant/components/squeezebox/media_player.py
+++ homeassistant/components/squeezebox/media_player.py
@@ -429,14 +429,6 @@ class SqueezeBoxMediaPlayerEntity(
         await self._player.async_set_power(True)
         await self.coordinator.async_refresh()
 
-    async def async_play_announcement(self, media_id: str) -> None:
-        """Play announcement url."""
-        await self._player.async_command("playlist", "play", media_id, "Announcement")
-
-        await self._player.async_set_repeat("none")
-
-        await self._player._wait_for_property("mode", "stop", 120)
-
     async def async_play_media(
         self,
         media_type: MediaType | str,
@@ -498,7 +490,7 @@ class SqueezeBoxMediaPlayerEntity(
                 )  # Stop the current playlist before changing the volume
                 await self._player.async_set_volume(_announce_volume)
 
-            await self.async_play_announcement(media_id)
+            await self._player.async_play_announcement(media_id)
 
             await self._player.async_command(
                 "playlist",
