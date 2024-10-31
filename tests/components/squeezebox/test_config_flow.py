e79c5ddcbe0ecbbe14699cefea79662ed2c10f0a
peteS-UK <peter@sketch.cc>
2024-10-31 18:34:20 +0000

    update test to remove invalid value

--- tests/components/squeezebox/test_config_flow.py
+++ tests/components/squeezebox/test_config_flow.py
@@ -133,41 +133,6 @@ async def test_options_form(hass: HomeAssistant) -> None:
     }
 
 
-async def test_options_form_invalid_values(hass: HomeAssistant) -> None:
-    """Test we can configure options."""
-    entry = MockConfigEntry(
-        data={
-            CONF_HOST: HOST,
-            CONF_PORT: PORT,
-            CONF_USERNAME: "",
-            CONF_PASSWORD: "",
-            CONF_HTTPS: False,
-        },
-        unique_id=UUID,
-        domain=DOMAIN,
-        options={CONF_BROWSE_LIMIT: 1000, CONF_VOLUME_STEP: 5},
-    )
-
-    entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(entry.entry_id)
-    await hass.async_block_till_done()
-
-    result = await hass.config_entries.options.async_init(entry.entry_id)
-
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "init"
-
-    # simulate manual input of options
-    result2 = await hass.config_entries.options.async_configure(
-        result["flow_id"],
-        user_input={CONF_BROWSE_LIMIT: 0, CONF_VOLUME_STEP: 0},
-    )
-
-    # put some meaningful asserts here
-    assert result2["type"] is FlowResultType.FORM
-    assert result2["errors"] == {"base": "value"}
-
-
 async def test_user_form_timeout(hass: HomeAssistant) -> None:
     """Test we handle server search timeout."""
     with (
