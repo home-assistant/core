920d693025dad49e948b034fdff416791985df5a
Marco Dutto <marco.dutto@yahoo.it>
2024-11-01 08:55:03 +0000

    Test Switch

--- tests/components/smarty/test_switch.py
+++ tests/components/smarty/test_switch.py
@@ -4,7 +4,13 @@ from unittest.mock import AsyncMock, patch
 
 from syrupy import SnapshotAssertion
 
-from homeassistant.const import Platform
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+    Platform,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
@@ -25,3 +31,26 @@ async def test_all_entities(
         await setup_integration(hass, mock_config_entry)
 
     await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
+
+
+async def test_setting_value(
+    hass: HomeAssistant,
+    mock_smarty: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test setting value."""
+    await setup_integration(hass, mock_config_entry)
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        SERVICE_TURN_ON,
+        target={ATTR_ENTITY_ID: "switch.smarty_post_data_to_smarty"},
+        blocking=True,
+    )
+
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        SERVICE_TURN_OFF,
+        target={ATTR_ENTITY_ID: "switch.smarty_post_data_to_smarty"},
+        blocking=True,
+    )
