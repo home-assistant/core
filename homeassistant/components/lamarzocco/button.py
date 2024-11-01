34923c7de6ab14029d93741196cd9f0ad9a7fa39
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-11-01 08:15:55 +0100

    change notify method

--- homeassistant/components/lamarzocco/button.py
+++ homeassistant/components/lamarzocco/button.py
@@ -68,4 +68,4 @@ class LaMarzoccoButtonEntity(LaMarzoccoEntity, ButtonEntity):
                     "key": self.entity_description.key,
                 },
             ) from exc
-        await self.coordinator.async_update_listeners()
+        await self.coordinator.async_set_updated_data(None)
