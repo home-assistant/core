f0cfeb3cdc0987989af6451e1ff00a01a8889184
peteS-UK <peter@sketch.cc>
2024-11-01 17:15:32 +0000

    Remove config_entry __init__

--- homeassistant/components/squeezebox/config_flow.py
+++ homeassistant/components/squeezebox/config_flow.py
@@ -270,10 +270,6 @@ OPTIONS_SCHEMA = vol.Schema(
 class OptionsFlowHandler(OptionsFlow):
     """Options Flow Handler."""
 
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
