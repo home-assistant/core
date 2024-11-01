3b28bf07d1f920d6997dea196f1b55dca4b1e7a9
Marco <46717884+marcodutto@users.noreply.github.com>
2024-11-01 06:08:55 -0400

    Add boost switch to Smarty (#129466)

--- homeassistant/components/smarty/__init__.py
+++ homeassistant/components/smarty/__init__.py
@@ -30,7 +30,7 @@ CONFIG_SCHEMA = vol.Schema(
     extra=vol.ALLOW_EXTRA,
 )
 
-PLATFORMS = [Platform.BINARY_SENSOR, Platform.FAN, Platform.SENSOR]
+PLATFORMS = [Platform.BINARY_SENSOR, Platform.FAN, Platform.SENSOR, Platform.SWITCH]
 
 
 async def async_setup(hass: HomeAssistant, hass_config: ConfigType) -> bool:
