f37046b1902b54c0cbf05ab4233ae09945360d76
dotvav <mycrub@mycrub.net>
2024-11-02 07:30:32 +0000

    Palazzeti DHCP

--- homeassistant/components/palazzetti/config_flow.py
+++ homeassistant/components/palazzetti/config_flow.py
@@ -6,6 +6,7 @@ from pypalazzetti.client import PalazzettiClient
 from pypalazzetti.exceptions import CommunicationError
 import voluptuous as vol
 
+from homeassistant.components import dhcp
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_HOST
 from homeassistant.helpers import device_registry as dr
@@ -42,6 +43,42 @@ class PalazzettiConfigFlow(ConfigFlow, domain=DOMAIN):
                     title=client.name,
                     data=user_input,
                 )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({vol.Required(CONF_HOST): str}),
+            errors=errors,
+        )
+
+    async def async_step_dhcp(
+        self, discovery_info: dhcp.DhcpServiceInfo
+    ) -> ConfigFlowResult:
+        """Handle DHCP discovery."""
+
+        LOGGER.debug(
+            "DHCP discovery detected Palazzetti: %s", discovery_info.macaddress
+        )
+
+        self._async_abort_entries_match({CONF_HOST: discovery_info.ip})
+
+        await self.async_set_unique_id(dr.format_mac(discovery_info.macaddress))
+        self._abort_if_unique_id_configured()
+        client = PalazzettiClient(hostname=discovery_info.ip)
+
+        errors: dict[str, str] = {}
+        try:
+            await client.connect()
+        except CommunicationError:
+            LOGGER.exception("Communication error")
+            errors["base"] = "cannot_connect"
+        else:
+            # Abort the flow if a config entry with the same unique ID exists
+            self._abort_if_unique_id_configured()
+
+            return self.async_create_entry(
+                title=client.name,
+                data={CONF_HOST: discovery_info.ip},
+            )
 
         return self.async_show_form(
             step_id="user",
