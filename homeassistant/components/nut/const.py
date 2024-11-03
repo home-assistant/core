f6deb9d61b29867926cfb9bfe326440efdb2511f
tdfountain <174762217+tdfountain@users.noreply.github.com>
2024-11-03 14:02:08 -0600

    Move ambient constant to sensor platform since used exclusively by those functions

--- homeassistant/components/nut/const.py
+++ homeassistant/components/nut/const.py
@@ -37,14 +37,6 @@ STATE_TYPES = {
     "TEST": "Battery Testing",
 }
 
-AMBIENT_THRESHOLD_STATUS_OPTIONS = [
-    "good",
-    "warning-low",
-    "critical-low",
-    "warning-high",
-    "critical-high",
-]
-
 COMMAND_BEEPER_DISABLE = "beeper.disable"
 COMMAND_BEEPER_ENABLE = "beeper.enable"
 COMMAND_BEEPER_MUTE = "beeper.mute"
--- homeassistant/components/nut/sensor.py
+++ homeassistant/components/nut/sensor.py
@@ -39,13 +39,7 @@ from homeassistant.helpers.update_coordinator import (
 )
 
 from . import NutConfigEntry, PyNUTData
-from .const import (
-    AMBIENT_THRESHOLD_STATUS_OPTIONS,
-    DOMAIN,
-    KEY_STATUS,
-    KEY_STATUS_DISPLAY,
-    STATE_TYPES,
-)
+from .const import DOMAIN, KEY_STATUS, KEY_STATUS_DISPLAY, STATE_TYPES
 
 NUT_DEV_INFO_TO_DEV_INFO: dict[str, str] = {
     "manufacturer": ATTR_MANUFACTURER,
@@ -53,6 +47,14 @@ NUT_DEV_INFO_TO_DEV_INFO: dict[str, str] = {
     "firmware": ATTR_SW_VERSION,
     "serial": ATTR_SERIAL_NUMBER,
 }
+
+AMBIENT_THRESHOLD_STATUS_OPTIONS = [
+    "good",
+    "warning-low",
+    "critical-low",
+    "warning-high",
+    "critical-high",
+]
 
 _LOGGER = logging.getLogger(__name__)
 
