b223c60dac0a107093f02c4f59be54976e764ff8
Robert Resch <robert@resch.dev>
2024-11-04 13:58:12 +0100

    Update go2rtc stream if stream_source is not matching (#129804)

--- tests/components/go2rtc/test_init.py
+++ tests/components/go2rtc/test_init.py
@@ -239,6 +239,18 @@ async def _test_setup_and_signaling(
 
     rest_client.streams.add.assert_called_once_with(entity_id, "rtsp://stream")
 
+    # Stream exists but the source is different
+    rest_client.streams.add.reset_mock()
+    rest_client.streams.list.return_value = {
+        entity_id: Stream([Producer("rtsp://different")])
+    }
+
+    receive_message_callback.reset_mock()
+    ws_client.reset_mock()
+    await test()
+
+    rest_client.streams.add.assert_called_once_with(entity_id, "rtsp://stream")
+
     # If the stream is already added, the stream should not be added again.
     rest_client.streams.add.reset_mock()
     rest_client.streams.list.return_value = {
