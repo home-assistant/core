0ddad2f85014cad2dbd927a6b7348910ea8b87b4
Paul Wheeler <paul.william.wheeler@gmail.com>
2024-11-05 22:19:37 +0000

    Removed unneeded update listener left over from reconfigure flow

--- homeassistant/components/tfl/__init__.py
+++ homeassistant/components/tfl/__init__.py
@@ -44,17 +44,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: TfLConfigEntry) -> bool:
     )
 
     entry.runtime_data = stop_point_api
-    entry.async_on_unload(entry.add_update_listener(update_listener))
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
-
-
-async def update_listener(hass: HomeAssistant, entry: TfLConfigEntry) -> None:
-    """Handle options update."""
-    _LOGGER.debug("update_listener called")
-    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: TfLConfigEntry) -> bool:
