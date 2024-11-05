25a05eb1567da4c0dcb4af9da7f786cec7aa9212
Robert Resch <robert@resch.dev>
2024-11-05 12:29:51 +0100

    Append a 1 to all go2rtc ports to avoid port conflicts (#129881)

--- homeassistant/components/go2rtc/__init__.py
+++ homeassistant/components/go2rtc/__init__.py
@@ -38,7 +38,7 @@ from homeassistant.helpers.typing import ConfigType
 from homeassistant.util.hass_dict import HassKey
 from homeassistant.util.package import is_docker_env
 
-from .const import CONF_DEBUG_UI, DEBUG_UI_URL_MESSAGE, DEFAULT_URL, DOMAIN
+from .const import CONF_DEBUG_UI, DEBUG_UI_URL_MESSAGE, DOMAIN, HA_MANAGED_URL
 from .server import Server
 
 _LOGGER = logging.getLogger(__name__)
@@ -125,7 +125,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
         hass.bus.async_listen(EVENT_HOMEASSISTANT_STOP, on_stop)
 
-        url = DEFAULT_URL
+        url = HA_MANAGED_URL
 
     hass.data[_DATA_GO2RTC] = url
     discovery_flow.async_create_flow(
