ef35b1e85a27289d7f0af81594ef50cc1438a7ef
epenet <6771947+epenet@users.noreply.github.com>
2024-11-05 07:46:58 +0000

    Initialize instance attribute in init method

--- homeassistant/components/androidtv/config_flow.py
+++ homeassistant/components/androidtv/config_flow.py
@@ -183,17 +183,18 @@ class AndroidTVFlowHandler(ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlowHandler:
         """Get the options flow for this handler."""
-        return OptionsFlowHandler()
+        return OptionsFlowHandler(config_entry)
 
 
 class OptionsFlowHandler(OptionsFlow):
     """Handle an option flow for Android Debug Bridge."""
 
-    _apps: dict[str, Any]
-    _state_det_rules: dict[str, Any]
-
-    def __init__(self) -> None:
+    def __init__(self, config_entry: ConfigEntry) -> None:
         """Initialize options flow."""
+        self._apps: dict[str, Any] = config_entry.options.get(CONF_APPS, {})
+        self._state_det_rules: dict[str, Any] = config_entry.options.get(
+            CONF_STATE_DETECTION_RULES, {}
+        )
         self._conf_app_id: str | None = None
         self._conf_rule_id: str | None = None
 
@@ -228,9 +229,6 @@ class OptionsFlowHandler(OptionsFlow):
     @callback
     def _async_init_form(self) -> ConfigFlowResult:
         """Return initial configuration form."""
-
-        self._apps = self.options.setdefault(CONF_APPS, {})
-        self._state_det_rules = self.options.setdefault(CONF_STATE_DETECTION_RULES, {})
 
         apps_list = {k: f"{v} ({k})" if v else k for k, v in self._apps.items()}
         apps = [SelectOptionDict(value=APPS_NEW_ID, label="Add new")] + [
