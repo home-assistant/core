a8ef81238280b3e348d7d0acd1cca0593fc4170f
Richard Polzer <richard@polzer-eb.de>
2024-11-06 22:45:36 +0000

    added event

--- homeassistant/components/ekeybionyx/config_flow.py
+++ homeassistant/components/ekeybionyx/config_flow.py
@@ -1,21 +1,25 @@
 """Config flow for Ekey Bionyx."""
 
 from collections.abc import Sequence
-import json
 import logging
 from typing import Any
 
 import ekey_bionyxpy
 import voluptuous as vol
 
+from homeassistant.components.webhook import (
+    async_generate_id as webhook_generate_id,
+    async_generate_path as webhook_generate_path,
+)
 from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.const import CONF_TOKEN
 from homeassistant.helpers import config_entry_oauth2_flow
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.network import get_url
 from homeassistant.helpers.selector import SelectOptionDict, SelectSelector
 
 from . import api
-from .const import DOMAIN, LOGGER, SCOPE
+from .const import DOMAIN, SCOPE
 
 
 class OAuth2FlowHandler(
@@ -70,25 +74,72 @@ class OAuth2FlowHandler(
             return self.async_show_form(
                 step_id="choose_system", data_schema=vol.Schema(data_schema)
             )
-        await self.async_set_unique_id(user_input["system"])
         self._data["system"] = [
             s for s in self._data["systems"] if s.system_id == user_input["system"]
         ][0]
-        self._abort_if_unique_id_configured()
         return await self.async_step_webhooks(user_input=None)
 
     async def async_step_webhooks(
         self, user_input: dict[str, Any] | None
     ) -> ConfigFlowResult:
         """Dialog to setup webhooks."""
-        LOGGER.info(json.dumps(self._data["system"].function_webhook_quotas))
-        if user_input is None or user_input == {}:
-            # show the form
-            return self.async_create_entry(
-                title=self._data["system"].system_name,
-                data={**self._data["data"]},
+        system: ekey_bionyxpy.System = self._data["system"]
+        await self.async_set_unique_id(system.system_id)
+        self._abort_if_unique_id_configured()
+        if (
+            system.function_webhook_quotas["free"] == 0
+            and system.function_webhook_quotas["used"] == 0
+        ):
+            return self.async_abort(reason="no_availible_webhooks")
+        if system.function_webhook_quotas["used"] > 0:
+            return await self.async_step_delete_webhooks()
+        if user_input is None:
+            data_schema: dict[Any, Any] = {
+                vol.Optional(f"Webhook {i+1}"): str
+                for i in range(self._data["system"].function_webhook_quotas["free"])
+            }
+            data_schema[
+                vol.Required(
+                    "url",
+                    default=get_url(self.hass, allow_external=False, allow_ip=True),
+                )
+            ] = str
+            return self.async_show_form(
+                step_id="webhooks", data_schema=vol.Schema(data_schema)
             )
+        webhook_data = [
+            {"webhook_id": webhook_generate_id(), "name": webhooks[1]}
+            for webhooks in user_input.items()
+            if webhooks[0] != "url"
+        ]
+        for webhook in webhook_data:
+            wh_def: ekey_bionyxpy._typing.WebhookData = {
+                "integrationName": "Home Assistant",
+                "functionName": webhook["name"],
+                "locationName": "Home Assistant",
+                "definition": {
+                    "url": user_input["url"]
+                    + webhook_generate_path(webhook["webhook_id"]),
+                    "authentication": {"apiAuthenticationType": "None"},
+                    "securityLevel": "AllowHttp",
+                    "method": "Post",
+                    "body": {"contentType": "text/plain", "content": "test"},
+                },
+            }
+            webhook.update(ekey_id=(await system.add_webhook(wh_def)).webhook_id)
         return self.async_create_entry(
             title=self._data["system"].system_name,
-            data={**self._data["data"]},
+            data={"webhooks": webhook_data},
         )
+
+    async def async_step_delete_webhooks(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Form to delete Webhooks."""
+        if user_input is None:
+            return self.async_show_form(step_id="delete_webhooks")
+        system: ekey_bionyxpy.System = self._data["system"]
+        webhooks = await system.get_webhooks()
+        for webhook in webhooks:
+            await webhook.delete()
+        return self.async_abort(reason="webhook_deletion_requested")
