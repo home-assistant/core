56212c6fa5f43624d93059a4d307b28e1a846f9f
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-11-07 08:24:47 +0100

    Update numpy to 2.1.2 and pandas to 2.2.3 (#129958)

--- script/gen_requirements_all.py
+++ script/gen_requirements_all.py
@@ -127,7 +127,8 @@ httpcore==1.0.5
 hyperframe>=5.2.0
 
 # Ensure we run compatible with musllinux build env
-numpy==1.26.4
+numpy==2.1.2
+pandas~=2.2.3
 
 # Constrain multidict to avoid typing issues
 # https://github.com/home-assistant/core/pull/67046
@@ -184,9 +185,6 @@ charset-normalizer==3.4.0
 # dacite: Ensure we have a version that is able to handle type unions for
 # Roborock, NAM, Brother, and GIOS.
 dacite>=1.7.0
-
-# Musle wheels for pandas 2.2.0 cannot be build for any architecture.
-pandas==2.1.4
 
 # chacha20poly1305-reuseable==0.12.x is incompatible with cryptography==43.0.x
 chacha20poly1305-reuseable>=0.13.0
