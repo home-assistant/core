4efa65e74f356fb38f01c16fc8a318fae51643e0
Alexandre CUER <alexandrep.cuer@gmail.com>
2024-11-07 21:19:48 +0000

    Improve readability with unique id functions

--- homeassistant/components/emoncms/__init__.py
+++ homeassistant/components/emoncms/__init__.py
@@ -17,31 +17,35 @@ PLATFORMS: list[Platform] = [Platform.SENSOR]
 type EmonCMSConfigEntry = ConfigEntry[EmoncmsCoordinator]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: EmonCMSConfigEntry) -> bool:
-    """Load a config entry."""
-    emoncms_client = EmoncmsClient(
-        entry.data[CONF_URL],
-        entry.data[CONF_API_KEY],
-        session=async_get_clientsession(hass),
-    )
+def _migrate_unique_id(
+    hass: HomeAssistant, entry: EmonCMSConfigEntry, emoncms_unique_id: str
+) -> None:
+    """Migrate to emoncms unique id if needed."""
+    if entry.unique_id != emoncms_unique_id:
+        ent_reg = er.async_get(hass)
+        entry_entities = ent_reg.entities.get_entries_for_config_entry_id(
+            entry.entry_id
+        )
+        for entity in entry_entities:
+            if entity.unique_id.split("-")[0] == entry.entry_id:
+                feed_id = entity.unique_id.split("-")[-1]
+                LOGGER.debug(f"moving feed {feed_id} to hardware uuid")
+                ent_reg.async_update_entity(
+                    entity.entity_id, new_unique_id=f"{emoncms_unique_id}-{feed_id}"
+                )
+        hass.config_entries.async_update_entry(
+            entry,
+            unique_id=emoncms_unique_id,
+        )
+
+
+async def _check_unique_id_migration(
+    hass: HomeAssistant, entry: EmonCMSConfigEntry, emoncms_client: EmoncmsClient
+) -> None:
+    """Check if we can migrate to the emoncms uuid."""
     emoncms_unique_id = await emoncms_client.async_get_uuid()
     if emoncms_unique_id:
-        if entry.unique_id != emoncms_unique_id:
-            ent_reg = er.async_get(hass)
-            entry_entities = ent_reg.entities.get_entries_for_config_entry_id(
-                entry.entry_id
-            )
-            for entity in entry_entities:
-                if entity.unique_id.split("-")[0] == entry.entry_id:
-                    feed_id = entity.unique_id.split("-")[-1]
-                    LOGGER.debug(f"moving feed {feed_id} to hardware uuid")
-                    ent_reg.async_update_entity(
-                        entity.entity_id, new_unique_id=f"{emoncms_unique_id}-{feed_id}"
-                    )
-            hass.config_entries.async_update_entry(
-                entry,
-                unique_id=emoncms_unique_id,
-            )
+        _migrate_unique_id(hass, entry, emoncms_unique_id)
     else:
         async_create_issue(
             hass,
@@ -56,6 +60,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: EmonCMSConfigEntry) -> b
                 "doc_url": EMONCMS_UUID_DOC_URL,
             },
         )
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: EmonCMSConfigEntry) -> bool:
+    """Load a config entry."""
+    emoncms_client = EmoncmsClient(
+        entry.data[CONF_URL],
+        entry.data[CONF_API_KEY],
+        session=async_get_clientsession(hass),
+    )
+    await _check_unique_id_migration(hass, entry, emoncms_client)
     coordinator = EmoncmsCoordinator(hass, emoncms_client)
     await coordinator.async_config_entry_first_refresh()
     entry.runtime_data = coordinator
