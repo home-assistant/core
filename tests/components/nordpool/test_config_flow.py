2848742aa34d73e70e0d0d6a55242f7caba5b8bc
G Johansson <goran.johansson@shiftit.se>
2024-11-08 19:53:52 +0100

    Add reconfigure flow to Nord Pool (#130151)

--- tests/components/nordpool/test_config_flow.py
+++ tests/components/nordpool/test_config_flow.py
@@ -15,11 +15,14 @@ from pynordpool import (
 import pytest
 
 from homeassistant import config_entries
-from homeassistant.components.nordpool.const import DOMAIN
+from homeassistant.components.nordpool.const import CONF_AREAS, DOMAIN
+from homeassistant.const import CONF_CURRENCY
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
 from . import ENTRY_CONFIG
+
+from tests.common import MockConfigEntry
 
 
 @pytest.mark.freeze_time("2024-11-05T18:00:00+00:00")
@@ -149,3 +152,94 @@ async def test_empty_data(hass: HomeAssistant, get_data: DeliveryPeriodData) -> 
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "Nord Pool"
     assert result["data"] == {"areas": ["SE3", "SE4"], "currency": "SEK"}
+
+
+@pytest.mark.freeze_time("2024-11-05T18:00:00+00:00")
+async def test_reconfigure(
+    hass: HomeAssistant,
+    load_int: MockConfigEntry,
+    get_data: DeliveryPeriodData,
+) -> None:
+    """Test reconfiguration."""
+
+    result = await load_int.start_reconfigure_flow(hass)
+
+    with (
+        patch(
+            "homeassistant.components.nordpool.coordinator.NordPoolClient.async_get_delivery_period",
+            return_value=get_data,
+        ),
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_AREAS: ["SE3"],
+                CONF_CURRENCY: "EUR",
+            },
+        )
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reconfigure_successful"
+    assert load_int.data == {
+        "areas": [
+            "SE3",
+        ],
+        "currency": "EUR",
+    }
+
+
+@pytest.mark.freeze_time("2024-11-05T18:00:00+00:00")
+@pytest.mark.parametrize(
+    ("error_message", "p_error"),
+    [
+        (NordPoolConnectionError, "cannot_connect"),
+        (NordPoolAuthenticationError, "cannot_connect"),
+        (NordPoolError, "cannot_connect"),
+        (NordPoolResponseError, "cannot_connect"),
+    ],
+)
+async def test_reconfigure_cannot_connect(
+    hass: HomeAssistant,
+    load_int: MockConfigEntry,
+    get_data: DeliveryPeriodData,
+    error_message: Exception,
+    p_error: str,
+) -> None:
+    """Test cannot connect error in a reeconfigure flow."""
+
+    result = await load_int.start_reconfigure_flow(hass)
+
+    with patch(
+        "homeassistant.components.nordpool.coordinator.NordPoolClient.async_get_delivery_period",
+        side_effect=error_message,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_AREAS: ["SE3"],
+                CONF_CURRENCY: "EUR",
+            },
+        )
+
+    assert result["errors"] == {"base": p_error}
+
+    with patch(
+        "homeassistant.components.nordpool.coordinator.NordPoolClient.async_get_delivery_period",
+        return_value=get_data,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_AREAS: ["SE3"],
+                CONF_CURRENCY: "EUR",
+            },
+        )
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reconfigure_successful"
+    assert load_int.data == {
+        "areas": [
+            "SE3",
+        ],
+        "currency": "EUR",
+    }
