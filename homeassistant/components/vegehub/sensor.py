e5db90f9c6df65592114bee34b38679bf55cf967
GhoweVege <85890024+GhoweVege@users.noreply.github.com>
2024-11-08 16:20:48 +0000

    Removed commented out code and unused file

--- homeassistant/components/vegehub/sensor.py
+++ homeassistant/components/vegehub/sensor.py
@@ -22,9 +22,6 @@ from .const import (
 _LOGGER = logging.getLogger(__name__)
 
 
-# async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-#     """Set up VegeHub sensors."""
-#     async_add_entities([VegeHubSensor()], True)
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
