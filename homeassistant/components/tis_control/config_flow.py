6fe5e258ed0866af37b9ff1309edb5c949ed146a
Mustafa744 <mustafaashraf744@gmail.com>
2024-11-08 15:04:56 +0000

    solving ci errors

--- homeassistant/components/tis_control/config_flow.py
+++ homeassistant/components/tis_control/config_flow.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-import logging
-
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
@@ -11,8 +9,6 @@ from homeassistant.const import CONF_PORT
 from homeassistant.core import callback
 
 from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
 
 STEP_USER_DATA_SCHEMA = vol.Schema({vol.Required(CONF_PORT): int}, required=True)
 
@@ -26,12 +22,10 @@ class TISConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle a flow initiated by the user."""
         errors = {}
         if user_input is not None:
-            logging.debug("recieved user input %s", user_input)
             # Assuming a function `validate_port` that returns True if the port is valid
             is_valid = await self.validate_port(user_input[CONF_PORT])
             if not is_valid:
                 errors["base"] = "invalid_port"  # Custom error key
-                logging.error("Provided port is invalid: %s", user_input[CONF_PORT])
 
             if not errors:
                 return self.async_create_entry(
@@ -39,7 +33,6 @@ class TISConfigFlow(ConfigFlow, domain=DOMAIN):
                 )
             else:  # noqa: RET505
                 # If there are errors, show the form again with the error message
-                logging.warning("Errors occurred: %s", errors)
                 return self._show_setup_form(errors)
 
         # If user_input is None (initial step), show the setup form
