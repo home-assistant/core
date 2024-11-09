5d0277a0d1a07db1659268f5f96b912651eedfb1
Manu <4445816+tr4nt0r@users.noreply.github.com>
2024-11-09 19:34:25 +0100

    Add actions for quest handling to Habitica (#129650)

--- tests/components/habitica/test_services.py
+++ tests/components/habitica/test_services.py
@@ -13,7 +13,13 @@ from homeassistant.components.habitica.const import (
     ATTR_TASK,
     DEFAULT_URL,
     DOMAIN,
+    SERVICE_ABORT_QUEST,
+    SERVICE_ACCEPT_QUEST,
+    SERVICE_CANCEL_QUEST,
     SERVICE_CAST_SKILL,
+    SERVICE_LEAVE_QUEST,
+    SERVICE_REJECT_QUEST,
+    SERVICE_START_QUEST,
 )
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
@@ -23,6 +29,9 @@ from .conftest import mock_called_with
 
 from tests.common import MockConfigEntry
 from tests.test_util.aiohttp import AiohttpClientMocker
+
+REQUEST_EXCEPTION_MSG = "Unable to connect to Habitica, try again later"
+RATE_LIMIT_EXCEPTION_MSG = "Rate limit exceeded, try again later"
 
 
 @pytest.fixture(autouse=True)
@@ -168,7 +177,7 @@ async def test_cast_skill(
             },
             HTTPStatus.TOO_MANY_REQUESTS,
             ServiceValidationError,
-            "Rate limit exceeded, try again later",
+            RATE_LIMIT_EXCEPTION_MSG,
         ),
         (
             {
@@ -195,7 +204,7 @@ async def test_cast_skill(
             },
             HTTPStatus.BAD_REQUEST,
             HomeAssistantError,
-            "Unable to connect to Habitica, try again later",
+            REQUEST_EXCEPTION_MSG,
         ),
     ],
 )
@@ -268,6 +277,103 @@ async def test_get_config_entry(
                 ATTR_TASK: "2f6fcabc-f670-4ec3-ba65-817e8deea490",
                 ATTR_SKILL: "smash",
             },
+            return_response=True,
+            blocking=True,
+        )
+
+
+@pytest.mark.parametrize(
+    ("service", "command"),
+    [
+        (SERVICE_ABORT_QUEST, "abort"),
+        (SERVICE_ACCEPT_QUEST, "accept"),
+        (SERVICE_CANCEL_QUEST, "cancel"),
+        (SERVICE_LEAVE_QUEST, "leave"),
+        (SERVICE_REJECT_QUEST, "reject"),
+        (SERVICE_START_QUEST, "force-start"),
+    ],
+    ids=[],
+)
+async def test_handle_quests(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_habitica: AiohttpClientMocker,
+    service: str,
+    command: str,
+) -> None:
+    """Test Habitica actions for quest handling."""
+
+    mock_habitica.post(
+        f"{DEFAULT_URL}/api/v3/groups/party/quests/{command}",
+        json={"success": True, "data": {}},
+    )
+
+    await hass.services.async_call(
+        DOMAIN,
+        service,
+        service_data={ATTR_CONFIG_ENTRY: config_entry.entry_id},
+        return_response=True,
+        blocking=True,
+    )
+
+    assert mock_called_with(
+        mock_habitica,
+        "post",
+        f"{DEFAULT_URL}/api/v3/groups/party/quests/{command}",
+    )
+
+
+@pytest.mark.parametrize(
+    (
+        "http_status",
+        "expected_exception",
+        "expected_exception_msg",
+    ),
+    [
+        (
+            HTTPStatus.TOO_MANY_REQUESTS,
+            ServiceValidationError,
+            RATE_LIMIT_EXCEPTION_MSG,
+        ),
+        (
+            HTTPStatus.NOT_FOUND,
+            ServiceValidationError,
+            "Unable to complete action, quest or group not found",
+        ),
+        (
+            HTTPStatus.UNAUTHORIZED,
+            ServiceValidationError,
+            "Action not allowed, only quest leader or group leader can perform this action",
+        ),
+        (
+            HTTPStatus.BAD_REQUEST,
+            HomeAssistantError,
+            REQUEST_EXCEPTION_MSG,
+        ),
+    ],
+)
+@pytest.mark.usefixtures("mock_habitica")
+async def test_handle_quests_exceptions(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_habitica: AiohttpClientMocker,
+    http_status: HTTPStatus,
+    expected_exception: Exception,
+    expected_exception_msg: str,
+) -> None:
+    """Test Habitica handle quests action exceptions."""
+
+    mock_habitica.post(
+        f"{DEFAULT_URL}/api/v3/groups/party/quests/accept",
+        json={"success": True, "data": {}},
+        status=http_status,
+    )
+
+    with pytest.raises(expected_exception, match=expected_exception_msg):
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_ACCEPT_QUEST,
+            service_data={ATTR_CONFIG_ENTRY: config_entry.entry_id},
             return_response=True,
             blocking=True,
         )
