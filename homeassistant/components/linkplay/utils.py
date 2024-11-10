bd714b610550881e47db301989be8c48fe2049a0
MahrWe <28512631+MahrWe@users.noreply.github.com>
2024-11-10 01:21:22 +0100

    Adding some project-strings to identify device manufacturer & model

--- homeassistant/components/linkplay/utils.py
+++ homeassistant/components/linkplay/utils.py
@@ -13,17 +13,25 @@ from .const import DATA_SESSION, DOMAIN
 MANUFACTURER_ARTSOUND: Final[str] = "ArtSound"
 MANUFACTURER_ARYLIC: Final[str] = "Arylic"
 MANUFACTURER_IEAST: Final[str] = "iEAST"
+MANUFACTURER_GGMM: Final[str] = "GGMM"
+MANUFACTURER_MEDION: Final[str] = "Medion"
 MANUFACTURER_GENERIC: Final[str] = "Generic"
 MODELS_ARTSOUND_SMART_ZONE4: Final[str] = "Smart Zone 4 AMP"
 MODELS_ARTSOUND_SMART_HYDE: Final[str] = "Smart Hyde"
 MODELS_ARYLIC_S50: Final[str] = "S50+"
 MODELS_ARYLIC_S50_PRO: Final[str] = "S50 Pro"
 MODELS_ARYLIC_A30: Final[str] = "A30"
+MODELS_ARYLIC_A50: Final[str] = "A50"
 MODELS_ARYLIC_A50S: Final[str] = "A50+"
+MODELS_ARYLIC_UP2STREAM_AMP: Final[str] = "Up2Stream Amp 2.0"
 MODELS_ARYLIC_UP2STREAM_AMP_V3: Final[str] = "Up2Stream Amp v3"
 MODELS_ARYLIC_UP2STREAM_AMP_V4: Final[str] = "Up2Stream Amp v4"
+MODELS_ARYLIC_UP2STREAM_PRO: Final[str] = "Up2Stream Pro v1"
 MODELS_ARYLIC_UP2STREAM_PRO_V3: Final[str] = "Up2Stream Pro v3"
+MODELS_ARYLIC_UP2STREAM_PLATE_AMP: Final[str] = "Up2Stream Plate Amp"
 MODELS_IEAST_AUDIOCAST_M5: Final[str] = "AudioCast M5"
+MODELS_GGMM_GGMM_E2: Final[str] = "GGMM E2"
+MODELS_MEDION_MD_43970: Final[str] = "Life P66970 (MD 43970)"
 MODELS_GENERIC: Final[str] = "Generic"
 
 
@@ -40,16 +48,26 @@ def get_info_from_project(project: str) -> tuple[str, str]:
             return MANUFACTURER_ARYLIC, MODELS_ARYLIC_S50_PRO
         case "RP0011_WB60_S":
             return MANUFACTURER_ARYLIC, MODELS_ARYLIC_A30
+        case "X-50":
+            return MANUFACTURER_ARYLIC, MODELS_ARYLIC_A50
         case "ARYLIC_A50S":
             return MANUFACTURER_ARYLIC, MODELS_ARYLIC_A50S
+        case "RP0011_WB60":
+            return MANUFACTURER_ARYLIC, MODELS_ARYLIC_UP2STREAM_AMP
         case "UP2STREAM_AMP_V3":
             return MANUFACTURER_ARYLIC, MODELS_ARYLIC_UP2STREAM_AMP_V3
         case "UP2STREAM_AMP_V4":
             return MANUFACTURER_ARYLIC, MODELS_ARYLIC_UP2STREAM_AMP_V4
         case "UP2STREAM_PRO_V3":
             return MANUFACTURER_ARYLIC, MODELS_ARYLIC_UP2STREAM_PRO_V3
+        case "ARYLIC_V20":
+            return MANUFACTURER_ARYLIC, MODELS_ARYLIC_UP2STREAM_PLATE_AMP
         case "iEAST-02":
             return MANUFACTURER_IEAST, MODELS_IEAST_AUDIOCAST_M5
+        case "GGMM_E2A":
+            return MANUFACTURER_GGMM, MODELS_GGMM_GGMM_E2
+        case "A16":
+            return MANUFACTURER_MEDION, MODELS_MEDION_MD_43970
         case _:
             return MANUFACTURER_GENERIC, MODELS_GENERIC
 
