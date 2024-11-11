1e26cf13d64ea50e904819a296d1a449b5169ede
Lennard Beers <l.beers@outlook.de>
2024-11-11 10:59:50 +0100

    Use runtime data for eq3btsmart (#130334)

--- homeassistant/components/eq3btsmart/entity.py
+++ homeassistant/components/eq3btsmart/entity.py
@@ -1,10 +1,8 @@
 """Base class for all eQ-3 entities."""
 
-from eq3btsmart.thermostat import Thermostat
-
 from homeassistant.helpers.entity import Entity
 
-from .models import Eq3Config
+from . import Eq3ConfigEntry
 
 
 class Eq3Entity(Entity):
@@ -12,8 +10,8 @@ class Eq3Entity(Entity):
 
     _attr_has_entity_name = True
 
-    def __init__(self, eq3_config: Eq3Config, thermostat: Thermostat) -> None:
+    def __init__(self, entry: Eq3ConfigEntry) -> None:
         """Initialize the eq3 entity."""
 
-        self._eq3_config = eq3_config
-        self._thermostat = thermostat
+        self._eq3_config = entry.runtime_data.eq3_config
+        self._thermostat = entry.runtime_data.thermostat
