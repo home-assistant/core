970e682eb5e9ef7a1e753eef60cb50523f2e2b06
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-11-11 10:26:54 +0100

    move and rename test

--- tests/components/acaia/test_init.py
+++ tests/components/acaia/test_init.py
@@ -1,13 +1,16 @@
 """Test init of acaia integration."""
 
+from datetime import timedelta
 from unittest.mock import MagicMock
 
+from aioacaia.exceptions import AcaiaDeviceNotFound, AcaiaError
+from freezegun.api import FrozenDateTimeFactory
 import pytest
 
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, async_fire_time_changed
 
 pytestmark = pytest.mark.usefixtures("init_integration")
 
@@ -25,3 +28,24 @@ async def test_load_unload_config_entry(
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
+
+
+@pytest.mark.parametrize(
+    "exception", [AcaiaError, AcaiaDeviceNotFound("Boom"), TimeoutError]
+)
+async def test_update_exception_leads_to_active_disconnect(
+    hass: HomeAssistant,
+    mock_scale: MagicMock,
+    freezer: FrozenDateTimeFactory,
+    exception: Exception,
+) -> None:
+    """Test scale gets disconnected on exception."""
+
+    mock_scale.connect.side_effect = exception
+    mock_scale.connected = False
+
+    freezer.tick(timedelta(minutes=10))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    mock_scale.device_disconnected_handler.assert_called_once()
