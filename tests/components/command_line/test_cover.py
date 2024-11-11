27a0807bbeb1755974894755a5b6ddc6e381ca0d
G Johansson <goran.johansson@shiftit.se>
2024-11-11 21:05:50 +0000

    Update examples

--- tests/components/command_line/test_cover.py
+++ tests/components/command_line/test_cover.py
@@ -422,7 +422,7 @@ async def test_icon_template(hass: HomeAssistant) -> None:
                             "command_close": f"echo 0 > {path}",
                             "command_stop": f"echo 0 > {path}",
                             "name": "Test",
-                            "icon": "{% if this.state=='open' and value=='0' %} mdi:closed {% elif this.state=='closed' and value=='100' %} mdi:opened {% else %} mdi:window {% endif %}",
+                            "icon": '{% if this.attributes.icon=="mdi:icon2" %} mdi:icon1 {% else %} mdi:icon2 {% endif %}',
                         }
                     }
                 ]
@@ -444,7 +444,7 @@ async def test_icon_template(hass: HomeAssistant) -> None:
         )
         entity_state = hass.states.get("cover.test")
         assert entity_state
-        assert entity_state.attributes.get("icon") == "mdi:closed"
+        assert entity_state.attributes.get("icon") == "mdi:icon1"
 
         await hass.services.async_call(
             COVER_DOMAIN,
@@ -454,4 +454,4 @@ async def test_icon_template(hass: HomeAssistant) -> None:
         )
         entity_state = hass.states.get("cover.test")
         assert entity_state
-        assert entity_state.attributes.get("icon") == "mdi:opened"
+        assert entity_state.attributes.get("icon") == "mdi:icon2"
