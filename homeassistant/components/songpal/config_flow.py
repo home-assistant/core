30e597b9d32c07f7ac41b525f7e51eeb0a494906
epenet <6771947+epenet@users.noreply.github.com>
2024-11-12 13:53:55 +0000

    Use TYPE_CHECKING

--- homeassistant/components/songpal/config_flow.py
+++ homeassistant/components/songpal/config_flow.py
@@ -21,9 +21,11 @@ _LOGGER = logging.getLogger(__name__)
 class SongpalConfig:
     """Device Configuration."""
 
-    def __init__(self, name: str, host: str, endpoint: str) -> None:
+    def __init__(self, name: str, host: str | None, endpoint: str) -> None:
         """Initialize Configuration."""
         self.name = name
+        if TYPE_CHECKING:
+            assert host is not None
         self.host = host
         self.endpoint = endpoint
 
@@ -69,7 +71,6 @@ class SongpalConfigFlow(ConfigFlow, domain=DOMAIN):
                 errors={"base": "cannot_connect"},
             )
 
-        assert parsed_url.hostname
         self.conf = SongpalConfig(name, parsed_url.hostname, endpoint)
 
         return await self.async_step_init(user_input)
@@ -150,7 +151,6 @@ class SongpalConfigFlow(ConfigFlow, domain=DOMAIN):
             _LOGGER.error("Import from yaml configuration failed: %s", ex)
             return self.async_abort(reason="cannot_connect")
 
-        assert parsed_url.hostname
         self.conf = SongpalConfig(name, parsed_url.hostname, endpoint)
 
         return await self.async_step_init(import_data)
