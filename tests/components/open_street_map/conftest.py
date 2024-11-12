d05d00ddbd6596a0946f806810d3aa8fae4a4b54
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2024-11-12 13:22:03 +0000

    Component is now in custom components instead, which makes more clear that this is our component.

--- tests/components/open_street_map/conftest.py
+++ tests/components/open_street_map/conftest.py
@@ -10,6 +10,6 @@ import pytest
 def mock_setup_entry() -> Generator[AsyncMock]:
     """Override async_setup_entry."""
     with patch(
-        "homeassistant.components.open_street_map.async_setup_entry", return_value=True
+        "config.custom_components.open_street_map.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         yield mock_setup_entry
