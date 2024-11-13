1a3a38b00b735712346db10a395a862bd3fee701
Johnny Willemsen <jwillemsen@remedy.nl>
2024-11-13 13:38:51 +0100

    Update diagnostics.py

--- homeassistant/components/home_connect/diagnostics.py
+++ homeassistant/components/home_connect/diagnostics.py
@@ -1,4 +1,5 @@
 """Diagnostics support for Home Connect Diagnostics."""
+
 from __future__ import annotations
 
 from typing import Any
@@ -7,11 +8,11 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceEntry
 
-from .const import (
-    DOMAIN,
-)
+from .const import DOMAIN)
 
-async def async_get_config_entry_diagnostics(hass: HomeAssistant, config_entry: ConfigEntry) -> dict[str, Any]:
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry
+) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     data = {}
     for device in hass.data[DOMAIN][config_entry.entry_id].devices:
