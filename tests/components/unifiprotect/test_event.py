29a851b150012ffa7db900e80ca3bf393be0f10b
RaHehl <7577984+RaHehl@users.noreply.github.com>
2024-11-18 18:04:51 +0100

    added test for unifiprotect fingerprint scanned but not identified

--- tests/components/unifiprotect/test_event.py
+++ tests/components/unifiprotect/test_event.py
@@ -268,3 +268,61 @@ async def test_doorbell_fingerprint_identified(
     assert state.attributes.get("ulp_id") == "test_ulp_id"
 
     unsub()
+
+
+async def test_doorbell_no_fingerprint_identified(
+    hass: HomeAssistant,
+    ufp: MockUFPFixture,
+    doorbell: Camera,
+    unadopted_camera: Camera,
+    fixed_now: datetime,
+) -> None:
+    """Test a doorbell fingerprint identified event."""
+
+    await init_entry(hass, ufp, [doorbell, unadopted_camera])
+    assert_entity_counts(hass, Platform.EVENT, 3, 3)
+    events: list[HAEvent] = []
+
+    @callback
+    def _capture_event(event: HAEvent) -> None:
+        events.append(event)
+
+    _, entity_id = ids_from_device_description(
+        Platform.EVENT, doorbell, EVENT_DESCRIPTIONS[2]
+    )
+
+    unsub = async_track_state_change_event(hass, entity_id, _capture_event)
+    event = Event(
+        model=ModelType.EVENT,
+        id="test_event_id",
+        type=EventType.FINGERPRINT_IDENTIFIED,
+        start=fixed_now - timedelta(seconds=1),
+        end=None,
+        score=100,
+        smart_detect_types=[],
+        smart_detect_event_ids=[],
+        camera_id=doorbell.id,
+        api=ufp.api,
+        metadata={"fingerprint": {}},
+    )
+
+    new_camera = doorbell.copy()
+    new_camera.last_fingerprint_identified_event_id = "test_event_id"
+    ufp.api.bootstrap.cameras = {new_camera.id: new_camera}
+    ufp.api.bootstrap.events = {event.id: event}
+
+    mock_msg = Mock()
+    mock_msg.changed_data = {}
+    mock_msg.new_obj = event
+    ufp.ws_msg(mock_msg)
+
+    await hass.async_block_till_done()
+
+    assert len(events) == 1
+    state = events[0].data["new_state"]
+    assert state
+    assert state.attributes[ATTR_ATTRIBUTION] == DEFAULT_ATTRIBUTION
+    assert state.attributes[ATTR_EVENT_ID] == "test_event_id"
+    assert state.attributes.get("ulp_id") == ""
+
+    unsub()
