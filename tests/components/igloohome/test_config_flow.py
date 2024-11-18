4a2785f10fb2337c843b5dafb8a44dc89bd6e7a4
Keith <keithle888@gmail.com>
2024-11-18 16:49:04 +0000

    - Added test for connection failure

--- tests/components/igloohome/test_config_flow.py
+++ tests/components/igloohome/test_config_flow.py
@@ -2,6 +2,7 @@
 
 from unittest.mock import AsyncMock, patch
 
+from aiohttp import ClientError
 from igloohome_api import AuthException
 
 from homeassistant import config_entries
@@ -67,6 +68,55 @@ async def test_form_invalid_auth(
     # Make sure the config flow tests finish with either an
     # FlowResultType.CREATE_ENTRY or FlowResultType.ABORT so
     # we can show the config flow is able to recover from an error.
+    with patch(
+        "igloohome_api.Auth.async_get_access_token",
+        return_value="mock_access_token",
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_CLIENT_ID: "test-client-id",
+                CONF_CLIENT_SECRET: "test-client-secret",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] == FlowResultType.CREATE_ENTRY
+    assert result["title"] == "Client Credentials"
+    assert result["data"] == {
+        CONF_CLIENT_ID: "test-client-id",
+        CONF_CLIENT_SECRET: "test-client-secret",
+    }
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_form_cannot_connect(
+    hass: HomeAssistant, mock_setup_entry: AsyncMock
+) -> None:
+    """Test we handle cannot connect error."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    with patch(
+        "igloohome_api.Auth.async_get_access_token",
+        side_effect=ClientError(),
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_CLIENT_ID: "test-client-id",
+                CONF_CLIENT_SECRET: "test-client-secret",
+            },
+        )
+
+    assert result["type"] == FlowResultType.FORM
+    assert result["errors"] == {"base": "cannot_connect"}
+
+    # Make sure the config flow tests finish with either an
+    # FlowResultType.CREATE_ENTRY or FlowResultType.ABORT so
+    # we can show the config flow is able to recover from an error.
+
     with patch(
         "igloohome_api.Auth.async_get_access_token",
         return_value="mock_access_token",
