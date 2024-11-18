943fed658b9fc16eb0794016327a5f88085a0ecc
Daniel Hjelseth Høyer <github@dahoiv.net>
2024-11-18 19:24:44 +0100

    Mill sensors
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- homeassistant/components/mill/sensor.py
+++ homeassistant/components/mill/sensor.py
@@ -33,6 +33,8 @@ from .const import (
     CONNECTION_TYPE,
     CONSUMPTION_TODAY,
     CONSUMPTION_YEAR,
+    CONTROL_SIGNAL,
+    CURRENT_POWER,
     DOMAIN,
     ECO2,
     HUMIDITY,
@@ -56,6 +58,19 @@ HEATER_SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
         device_class=SensorDeviceClass.ENERGY,
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
+    ),
+    SensorEntityDescription(
+        key=CURRENT_POWER,
+        translation_key="current_power",
+        device_class=SensorDeviceClass.POWER,
+        native_unit_of_measurement=UnitOfPower.WATT,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    SensorEntityDescription(
+        key=CONTROL_SIGNAL,
+        translation_key="control_signal",
+        native_unit_of_measurement=PERCENTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
 )
 
@@ -118,6 +133,16 @@ LOCAL_SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
     ),
 )
 
+SOCKET_SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
+    SensorEntityDescription(
+        key=HUMIDITY,
+        device_class=SensorDeviceClass.HUMIDITY,
+        native_unit_of_measurement=PERCENTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    *HEATER_SENSOR_TYPES,
+)
+
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
@@ -145,7 +170,9 @@ async def async_setup_entry(
         )
         for mill_device in mill_data_coordinator.data.values()
         for entity_description in (
-            HEATER_SENSOR_TYPES
+            SOCKET_SENSOR_TYPES
+            if isinstance(mill_device, mill.Socket)
+            else HEATER_SENSOR_TYPES
             if isinstance(mill_device, mill.Heater)
             else SENSOR_TYPES
         )
