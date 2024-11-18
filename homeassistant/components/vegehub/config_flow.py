eb9089144fcc5eeff240b89c4c9da42c7e517c2e
GhoweVege <85890024+GhoweVege@users.noreply.github.com>
2024-11-18 15:42:26 +0000

    Simplify unique_id assertion.

--- homeassistant/components/vegehub/config_flow.py
+++ homeassistant/components/vegehub/config_flow.py
@@ -27,7 +27,6 @@ ip_dict: dict[str, str] = {}
 class VegeHubConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for VegeHub integration."""
 
-
     def __init__(self) -> None:
         """Initialize the VegeHub config flow."""
         self._hub: VegeHub | None = None
