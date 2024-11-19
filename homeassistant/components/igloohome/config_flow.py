544f11fdcda76a3bac4b073b0d151875511d4aba
Keith <keithle888@gmail.com>
2024-11-19 08:37:02 +0000

    - Merge validation and handling user input in config_flow code to reduce custom error classes

--- homeassistant/components/igloohome/config_flow.py
+++ homeassistant/components/igloohome/config_flow.py
@@ -5,13 +5,12 @@ from __future__ import annotations
 import logging
 from typing import Any
 
+from aiohttp import ClientError
 from igloohome_api import Auth, AuthException
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET
-from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
@@ -26,63 +25,33 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
-    """Validate the user input allows us to connect.
-
-    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
-    """
-
-    auth = Auth(
-        session=async_get_clientsession(hass),
-        client_id=data[CONF_CLIENT_ID],
-        client_secret=data[CONF_CLIENT_SECRET],
-    )
-
-    try:
-        _ = await auth.async_get_access_token()
-    except AuthException as e:
-        raise InvalidAuth from e
-    except Exception as e:
-        raise CannotConnect from e
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    return {"title": "Client Credentials"}
-
-
 class MyConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for igloohome."""
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
-        """Handle the initial step."""
-        errors: dict[str, str] = {}
+        """Handle the config flow step."""
+
         if user_input is not None:
+            auth = Auth(
+                session=async_get_clientsession(self.hass),
+                client_id=user_input[CONF_CLIENT_ID],
+                client_secret=user_input[CONF_CLIENT_SECRET],
+            )
+            errors: dict[str, str] = {}
+
             try:
-                info = await validate_input(self.hass, user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
+                _ = await auth.async_get_access_token()
+            except AuthException:
                 errors["base"] = "invalid_auth"
-            except Exception:
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+            except ClientError:
+                errors["base"] = "cannot_connect"
             else:
-                return self.async_create_entry(title=info["title"], data=user_input)
+                return self.async_create_entry(
+                    title="Client Credentials", data=user_input
+                )
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
-
-
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(HomeAssistantError):
-    """Error to indicate there is invalid auth."""
