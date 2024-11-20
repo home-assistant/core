8623d0dd8ebe61e2152b35870d0482ccdc9fecde
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2024-11-20 20:38:34 +0100

    Update homeassistant/components/niko_home_control/__init__.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/niko_home_control/__init__.py
+++ homeassistant/components/niko_home_control/__init__.py
@@ -39,6 +39,6 @@ async def async_setup_entry(
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: NikoHomeControlConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
