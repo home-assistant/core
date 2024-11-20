ef2b7a0c01116b975db06af4ce7a1e9d02499fb9
Joost Lekkerkerker <joostlek@outlook.com>
2024-11-20 17:43:02 +0100

    Change to rule slugs (#131043)

--- script/hassfest/quality_scale.py
+++ script/hassfest/quality_scale.py
@@ -10,11 +10,66 @@ from homeassistant.util.yaml import load_yaml_dict
 
 from .model import Config, Integration
 
+RULES = [
+    "action-exceptions",
+    "action-setup",
+    "appropriate-polling",
+    "async-dependency",
+    "brands",
+    "common-modules",
+    "config-entry-unloading",
+    "config-flow",
+    "config-flow-test-coverage",
+    "dependency-transparency",
+    "devices",
+    "diagnostics",
+    "discovery",
+    "discovery-update-info",
+    "docs-actions",
+    "docs-configuration-parameters",
+    "docs-data-update",
+    "docs-examples",
+    "docs-high-level-description",
+    "docs-installation-instructions",
+    "docs-installation-parameters",
+    "docs-known-limitations",
+    "docs-removal-instructions",
+    "docs-supported-devices",
+    "docs-supported-functions",
+    "docs-troubleshooting",
+    "docs-use-cases",
+    "dynamic-devices",
+    "entity-category",
+    "entity-device-class",
+    "entity-disabled-by-default",
+    "entity-event-setup",
+    "entity-translations",
+    "entity-unavailable",
+    "entity-unique-id",
+    "exception-translations",
+    "has-entity-name",
+    "icon-translations",
+    "inject-websession",
+    "integration-owner",
+    "log-when-unavailable",
+    "parallel-updates",
+    "reauthentication-flow",
+    "reconfiguration-flow",
+    "repair-issues",
+    "runtime-data",
+    "stale-devices",
+    "strict-typing",
+    "test-before-configure",
+    "test-before-setup",
+    "test-coverage",
+    "unique-config-entry",
+]
+
 SCHEMA = vol.Schema(
     {
         vol.Required("rules"): vol.Schema(
             {
-                str: vol.Any(
+                vol.Optional(rule): vol.Any(
                     vol.In(["todo", "done"]),
                     vol.Schema(
                         {
@@ -23,6 +78,7 @@ SCHEMA = vol.Schema(
                         }
                     ),
                 )
+                for rule in RULES
             }
         )
     }
