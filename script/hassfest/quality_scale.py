825b3229abd7b0e301e2eca212d23cb1e84c31fb
Joostlek <joostlek@outlook.com>
2024-11-20 16:27:23 +0100

    Change to rule slugs

--- script/hassfest/quality_scale.py
+++ script/hassfest/quality_scale.py
@@ -10,11 +10,66 @@ from homeassistant.util.yaml import load_yaml_dict
 
 from .model import Config, Integration
 
+RULES = [
+    "action_exceptions",
+    "action_setup",
+    "appropriate_polling",
+    "async_dependency",
+    "brands",
+    "common_modules",
+    "config_entry_unloading",
+    "config_flow",
+    "config_flow_test_coverage",
+    "dependency_transparency",
+    "devices",
+    "diagnostics",
+    "discovery",
+    "discovery_update_info",
+    "docs_actions",
+    "docs_configuration_parameters",
+    "docs_data_update",
+    "docs_examples",
+    "docs_high_level_description",
+    "docs_installation_instructions",
+    "docs_installation_parameters",
+    "docs_known_limitations",
+    "docs_removal_instructions",
+    "docs_supported_devices",
+    "docs_supported_functions",
+    "docs_troubleshooting",
+    "docs_use_cases",
+    "dynamic_devices",
+    "entity_category",
+    "entity_device_class",
+    "entity_disabled_by_default",
+    "entity_event_setup",
+    "entity_translations",
+    "entity_unavailable",
+    "entity_unique_id",
+    "exception_translations",
+    "has_entity_name",
+    "icon_translations",
+    "inject_websession",
+    "integration_owner",
+    "log_when_unavailable",
+    "parallel_updates",
+    "reauthentication_flow",
+    "reconfiguration_flow",
+    "repair_issues",
+    "runtime_data",
+    "stale_devices",
+    "strict_typing",
+    "test_before_configure",
+    "test_before_setup",
+    "test_coverage",
+    "unique_config_entry",
+]
+
 SCHEMA = vol.Schema(
     {
         vol.Required("rules"): vol.Schema(
             {
-                str: vol.Any(
+                vol.Optional(rule): vol.Any(
                     vol.In(["todo", "done"]),
                     vol.Schema(
                         {
@@ -23,6 +78,7 @@ SCHEMA = vol.Schema(
                         }
                     ),
                 )
+                for rule in RULES
             }
         )
     }
