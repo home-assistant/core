3c96c559dc52304d74d8b3b9ee7f6e3813a5b034
G Johansson <goran.johansson@shiftit.se>
2024-11-21 20:42:36 +0100

    Remove config entry unique id from trafikverket_train (#130989)

--- homeassistant/components/trafikverket_train/config_flow.py
+++ homeassistant/components/trafikverket_train/config_flow.py
@@ -37,7 +37,7 @@ from homeassistant.helpers.selector import (
 import homeassistant.util.dt as dt_util
 
 from .const import CONF_FILTER_PRODUCT, CONF_FROM, CONF_TIME, CONF_TO, DOMAIN
-from .util import create_unique_id, next_departuredate
+from .util import next_departuredate
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -125,6 +125,7 @@ class TVTrainConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Trafikverket Train integration."""
 
     VERSION = 1
+    MINOR_VERSION = 2
 
     @staticmethod
     @callback
@@ -202,11 +203,16 @@ class TVTrainConfigFlow(ConfigFlow, domain=DOMAIN):
                 filter_product,
             )
             if not errors:
-                unique_id = create_unique_id(
-                    train_from, train_to, train_time, train_days
+                self._async_abort_entries_match(
+                    {
+                        CONF_API_KEY: api_key,
+                        CONF_FROM: train_from,
+                        CONF_TO: train_to,
+                        CONF_TIME: train_time,
+                        CONF_WEEKDAY: train_days,
+                        CONF_FILTER_PRODUCT: filter_product,
+                    }
                 )
-                await self.async_set_unique_id(unique_id)
-                self._abort_if_unique_id_configured()
                 return self.async_create_entry(
                     title=name,
                     data={
