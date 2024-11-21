fe94c8b50c97fd331d205facbc54be0a5c748575
epenet <6771947+epenet@users.noreply.github.com>
2024-11-21 18:01:43 +0100

    Apply suggestions from code review

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -1494,7 +1494,7 @@ class ConfigEntriesFlowManager(
                 "exists",
                 core_behavior=ReportBehavior.LOG,
                 core_integration_behavior=ReportBehavior.LOG,
-                custom_integration_behavior=ReportBehavior.IGNORE,
+                custom_integration_behavior=ReportBehavior.LOG,
                 integration_domain=flow.handler,
             )
 
