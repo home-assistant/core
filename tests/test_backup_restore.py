c1111ea13e5f9888a0e3496fdb2c594f36051083
Erik Montnemery <erik@montnemery.com>
2024-11-22 08:39:53 +0100

    Teach core backup to restore only database or only settings (#131225)

--- tests/test_backup_restore.py
+++ tests/test_backup_restore.py
@@ -19,22 +19,26 @@ from .common import get_test_config_dir
         (
             None,
             '{"path": "test"}',
+            None,
+        ),
+        (
+            None,
+            '{"path": "test", "password": "psw", "restore_database": false, "restore_homeassistant": true}',
             backup_restore.RestoreBackupFileContent(
-                backup_file_path=Path("test"), password=None
+                backup_file_path=Path("test"),
+                password="psw",
+                restore_database=False,
+                restore_homeassistant=True,
             ),
         ),
         (
             None,
-            '{"path": "test", "password": "psw"}',
+            '{"path": "test", "password": null, "restore_database": true, "restore_homeassistant": false}',
             backup_restore.RestoreBackupFileContent(
-                backup_file_path=Path("test"), password="psw"
-            ),
-        ),
-        (
-            None,
-            '{"path": "test", "password": null}',
-            backup_restore.RestoreBackupFileContent(
-                backup_file_path=Path("test"), password=None
+                backup_file_path=Path("test"),
+                password=None,
+                restore_database=True,
+                restore_homeassistant=False,
             ),
         ),
     ],
@@ -65,7 +69,10 @@ def test_restoring_backup_that_does_not_exist() -> None:
         mock.patch(
             "homeassistant.backup_restore.restore_backup_file_content",
             return_value=backup_restore.RestoreBackupFileContent(
-                backup_file_path=backup_file_path
+                backup_file_path=backup_file_path,
+                password=None,
+                restore_database=True,
+                restore_homeassistant=True,
             ),
         ),
         mock.patch("pathlib.Path.read_text", side_effect=FileNotFoundError),
@@ -94,7 +101,10 @@ def test_restoring_backup_that_is_not_a_file() -> None:
         mock.patch(
             "homeassistant.backup_restore.restore_backup_file_content",
             return_value=backup_restore.RestoreBackupFileContent(
-                backup_file_path=backup_file_path
+                backup_file_path=backup_file_path,
+                password=None,
+                restore_database=True,
+                restore_homeassistant=True,
             ),
         ),
         mock.patch("pathlib.Path.exists", return_value=True),
@@ -118,7 +128,10 @@ def test_aborting_for_older_versions() -> None:
         mock.patch(
             "homeassistant.backup_restore.restore_backup_file_content",
             return_value=backup_restore.RestoreBackupFileContent(
-                backup_file_path=backup_file_path
+                backup_file_path=backup_file_path,
+                password=None,
+                restore_database=True,
+                restore_homeassistant=True,
             ),
         ),
         mock.patch("securetar.SecureTarFile"),
@@ -133,13 +146,73 @@ def test_aborting_for_older_versions() -> None:
         assert backup_restore.restore_backup(config_dir) is True
 
 
-def test_removal_of_current_configuration_when_restoring() -> None:
+@pytest.mark.parametrize(
+    (
+        "restore_backup_content",
+        "expected_removed_files",
+        "expected_removed_directories",
+        "expected_copied_files",
+        "expected_copied_trees",
+    ),
+    [
+        (
+            backup_restore.RestoreBackupFileContent(
+                backup_file_path=None,
+                password=None,
+                restore_database=True,
+                restore_homeassistant=True,
+            ),
+            (
+                ".HA_RESTORE",
+                ".HA_VERSION",
+                "home-assistant_v2.db",
+                "home-assistant_v2.db-wal",
+            ),
+            ("www",),
+            (),
+            ("data",),
+        ),
+        (
+            backup_restore.RestoreBackupFileContent(
+                backup_file_path=None,
+                password=None,
+                restore_database=False,
+                restore_homeassistant=True,
+            ),
+            (".HA_RESTORE", ".HA_VERSION"),
+            ("www",),
+            (),
+            ("data",),
+        ),
+        (
+            backup_restore.RestoreBackupFileContent(
+                backup_file_path=None,
+                password=None,
+                restore_database=True,
+                restore_homeassistant=False,
+            ),
+            ("home-assistant_v2.db", "home-assistant_v2.db-wal"),
+            (),
+            ("home-assistant_v2.db", "home-assistant_v2.db-wal"),
+            (),
+        ),
+    ],
+)
+def test_removal_of_current_configuration_when_restoring(
+    restore_backup_content: backup_restore.RestoreBackupFileContent,
+    expected_removed_files: tuple[str, ...],
+    expected_removed_directories: tuple[str, ...],
+    expected_copied_files: tuple[str, ...],
+    expected_copied_trees: tuple[str, ...],
+) -> None:
     """Test that we are removing the current configuration directory."""
     config_dir = Path(get_test_config_dir())
-    backup_file_path = Path(config_dir, "backups", "test.tar")
+    restore_backup_content.backup_file_path = Path(config_dir, "backups", "test.tar")
     mock_config_dir = [
         {"path": Path(config_dir, ".HA_RESTORE"), "is_file": True},
         {"path": Path(config_dir, ".HA_VERSION"), "is_file": True},
+        {"path": Path(config_dir, "home-assistant_v2.db"), "is_file": True},
+        {"path": Path(config_dir, "home-assistant_v2.db-wal"), "is_file": True},
         {"path": Path(config_dir, "backups"), "is_file": False},
         {"path": Path(config_dir, "www"), "is_file": False},
     ]
@@ -156,12 +229,10 @@ def test_removal_of_current_configuration_when_restoring() -> None:
     with (
         mock.patch(
             "homeassistant.backup_restore.restore_backup_file_content",
-            return_value=backup_restore.RestoreBackupFileContent(
-                backup_file_path=backup_file_path
-            ),
+            return_value=restore_backup_content,
         ),
         mock.patch("securetar.SecureTarFile"),
-        mock.patch("homeassistant.backup_restore.TemporaryDirectory"),
+        mock.patch("homeassistant.backup_restore.TemporaryDirectory") as temp_dir_mock,
         mock.patch("homeassistant.backup_restore.HA_VERSION", "2013.09.17"),
         mock.patch("pathlib.Path.read_text", _patched_path_read_text),
         mock.patch("pathlib.Path.is_file", _patched_path_is_file),
@@ -170,19 +241,33 @@ def test_removal_of_current_configuration_when_restoring() -> None:
             "pathlib.Path.iterdir",
             return_value=[x["path"] for x in mock_config_dir],
         ),
-        mock.patch("pathlib.Path.unlink") as unlink_mock,
+        mock.patch("pathlib.Path.unlink", autospec=True) as unlink_mock,
+        mock.patch("shutil.copy") as copy_mock,
         mock.patch("shutil.copytree") as copytree_mock,
         mock.patch("shutil.rmtree") as rmtree_mock,
     ):
-        assert backup_restore.restore_backup(config_dir) is True
-        assert unlink_mock.call_count == 2
-        assert copytree_mock.call_count == 1
-        assert (
-            rmtree_mock.call_count == 1
-        )  # We have 2 directories in the config directory, but backups is kept
+        temp_dir_mock.return_value.__enter__.return_value = "tmp"
 
+        assert backup_restore.restore_backup(config_dir) is True
+
+        tmp_ha = Path("tmp", "homeassistant")
+        assert copy_mock.call_count == len(expected_copied_files)
+        copied_files = {Path(call.args[0]) for call in copy_mock.mock_calls}
+        assert copied_files == {Path(tmp_ha, "data", f) for f in expected_copied_files}
+
+        assert copytree_mock.call_count == len(expected_copied_trees)
+        copied_trees = {Path(call.args[0]) for call in copytree_mock.mock_calls}
+        assert copied_trees == {Path(tmp_ha, t) for t in expected_copied_trees}
+
+        assert unlink_mock.call_count == len(expected_removed_files)
+        removed_files = {Path(call.args[0]) for call in unlink_mock.mock_calls}
+        assert removed_files == {Path(config_dir, f) for f in expected_removed_files}
+
+        assert rmtree_mock.call_count == len(expected_removed_directories)
         removed_directories = {Path(call.args[0]) for call in rmtree_mock.mock_calls}
-        assert removed_directories == {Path(config_dir, "www")}
+        assert removed_directories == {
+            Path(config_dir, d) for d in expected_removed_directories
+        }
 
 
 def test_extracting_the_contents_of_a_backup_file() -> None:
@@ -209,6 +294,9 @@ def test_extracting_the_contents_of_a_backup_file() -> None:
             "homeassistant.backup_restore.restore_backup_file_content",
             return_value=backup_restore.RestoreBackupFileContent(
                 backup_file_path=backup_file_path,
+                password=None,
+                restore_database=True,
+                restore_homeassistant=True,
             ),
         ),
         mock.patch(
@@ -245,15 +333,7 @@ def test_pw_to_key(password: str | None, expected: bytes | None) -> None:
     assert backup_restore.password_to_key(password) == expected
 
 
-@pytest.mark.parametrize(
-    ("password", "expected"),
-    [
-        (None, None),
-        ("test", b"\xf0\x9b\xb9\x1f\xdc,\xff\xd5x\xd6\xd6\x8fz\x19.\x0f"),
-        ("lorem ipsum...", b"#\xe0\xfc\xe0\xdb?_\x1f,$\rQ\xf4\xf5\xd8\xfb"),
-    ],
-)
-def test_pw_to_key_none(password: str | None, expected: bytes | None) -> None:
+def test_pw_to_key_none() -> None:
     """Test password to key conversion."""
     with pytest.raises(AttributeError):
         backup_restore.password_to_key(None)
