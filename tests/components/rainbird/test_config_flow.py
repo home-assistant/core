80d1e126b83235a0ed2fdfa7b96dab06b217cd34
Allen Porter <allen.porter@gmail.com>
2024-11-24 04:46:42 +0000

    Add reauthentication support for Rainbird

--- tests/components/rainbird/test_config_flow.py
+++ tests/components/rainbird/test_config_flow.py
@@ -268,6 +268,28 @@ async def test_controller_cannot_connect(
     assert not mock_setup.mock_calls
 
 
+async def test_controller_invalid_auth(
+    hass: HomeAssistant,
+    mock_setup: Mock,
+    responses: list[AiohttpClientMockResponse],
+    aioclient_mock: AiohttpClientMocker,
+) -> None:
+    """Test an invalid password."""
+
+    # Controller response with a failure
+    responses.clear()
+    responses.append(
+        AiohttpClientMockResponse("POST", URL, status=HTTPStatus.FORBIDDEN)
+    )
+
+    result = await complete_flow(hass)
+    assert result.get("type") is FlowResultType.FORM
+    assert result.get("step_id") == "user"
+    assert result.get("errors") == {"base": "invalid_auth"}
+
+    assert not mock_setup.mock_calls
+
+
 async def test_controller_timeout(
     hass: HomeAssistant,
     mock_setup: Mock,
@@ -284,6 +306,54 @@ async def test_controller_timeout(
         assert result.get("errors") == {"base": "timeout_connect"}
 
     assert not mock_setup.mock_calls
+
+
+@pytest.mark.parametrize(
+    ("responses", "config_entry_data"),
+    [
+        (
+            [
+                mock_response(SERIAL_RESPONSE),
+                mock_json_response(WIFI_PARAMS_RESPONSE),
+            ],
+            {
+                **CONFIG_ENTRY_DATA,
+                CONF_PASSWORD: "old-password",
+            },
+        ),
+    ],
+)
+async def test_reauth_flow(
+    hass: HomeAssistant,
+    mock_setup: Mock,
+    config_entry: MockConfigEntry,
+) -> None:
+    """Test the controller is setup correctly."""
+    assert config_entry.data.get(CONF_PASSWORD) == "old-password"
+    config_entry.async_start_reauth(hass)
+    await hass.async_block_till_done()
+
+    flows = hass.config_entries.flow.async_progress()
+    assert len(flows) == 1
+    result = flows[0]
+
+    assert result.get("step_id") == "reauth_confirm"
+    assert not result.get("errors")
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_PASSWORD: PASSWORD},
+    )
+    assert result.get("type") == FlowResultType.ABORT
+    assert result.get("reason") == "reauth_successful"
+
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
+    entry = entries[0]
+    assert entry.unique_id == MAC_ADDRESS_UNIQUE_ID
+    assert entry.data.get(CONF_PASSWORD) == PASSWORD
+
+    assert len(mock_setup.mock_calls) == 1
 
 
 async def test_options_flow(hass: HomeAssistant, mock_setup: Mock) -> None:
