767ff87c6c99f4b26672db5c0c4ec86a809ed6f2
Franck Nijhof <git@frenck.dev>
2024-11-24 16:15:36 +0100

    Slightly tweak migration + translations

--- homeassistant/components/stookwijzer/__init__.py
+++ homeassistant/components/stookwijzer/__init__.py
@@ -9,8 +9,10 @@ from stookwijzer import Stookwijzer
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_LATITUDE, CONF_LOCATION, CONF_LONGITUDE, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryError
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
+from .const import DOMAIN
 from .coordinator import StookwijzerCoordinator
 
 PLATFORMS = [Platform.SENSOR]
@@ -18,30 +20,6 @@ PLATFORMS = [Platform.SENSOR]
 _LOGGER = logging.getLogger(__name__)
 
 type StookwijzerConfigEntry = ConfigEntry[StookwijzerCoordinator]
-
-
-async def async_migrate_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Migrate old entry."""
-    _LOGGER.debug("Migrating from version %s", entry.version)
-
-    if entry.version == 1:
-        session = async_get_clientsession(hass)
-        x, y = await Stookwijzer.async_transform_coordinates(
-            session,
-            entry.data[CONF_LOCATION][CONF_LATITUDE],
-            entry.data[CONF_LOCATION][CONF_LONGITUDE],
-        )
-
-        if not x or not y:
-            _LOGGER.error("Migration to version %s not successful", entry.version)
-            return False
-        hass.config_entries.async_update_entry(
-            entry, version=2, data={CONF_LATITUDE: x, CONF_LONGITUDE: y}
-        )
-
-        _LOGGER.debug("Migration to version %s successful", entry.version)
-
-    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -63,3 +41,29 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Stookwijzer config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
+async def async_migrate_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Migrate old entry."""
+    _LOGGER.debug("Migrating from version %s", entry.version)
+
+    if entry.version == 1:
+        session = async_get_clientsession(hass)
+        x, y = await Stookwijzer.async_transform_coordinates(
+            session,
+            entry.data[CONF_LOCATION][CONF_LATITUDE],
+            entry.data[CONF_LOCATION][CONF_LONGITUDE],
+        )
+
+        if not x or not y:
+            raise ConfigEntryError(
+                translation_domain=DOMAIN,
+                translation_key="location_migration_failed",
+            )
+        hass.config_entries.async_update_entry(
+            entry, version=2, data={CONF_LATITUDE: x, CONF_LONGITUDE: y}
+        )
+
+        _LOGGER.debug("Migration to version %s successful", entry.version)
+
+    return True
