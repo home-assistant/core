904ab291cf2edbc999c7ca4cfd60799cde55c180
dotvav <mycrub@mycrub.net>
2024-11-25 08:02:01 +0000

    Remove device diagnostics, return redacted data

--- homeassistant/components/palazzetti/diagnostics.py
+++ homeassistant/components/palazzetti/diagnostics.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from typing import Any
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.device_registry import DeviceEntry
 
 from . import PalazzettiConfigEntry
 
@@ -17,13 +16,5 @@ async def async_get_config_entry_diagnostics(
     client = entry.runtime_data.client
 
     return {
-        "api_data": client.to_json(),
+        "api_data": client.to_dict(redact=True),
     }
-
-
-async def async_get_device_diagnostics(
-    hass: HomeAssistant, entry: PalazzettiConfigEntry, device: DeviceEntry
-) -> dict[str, Any]:
-    """Return diagnostics for a device entry."""
-
-    return await async_get_config_entry_diagnostics(hass, entry)
