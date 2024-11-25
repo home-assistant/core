541983b57cacc1280750c2931b18909858f72000
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2024-11-25 14:10:13 +0100

    Add error handling test

--- tests/components/homewizard/test_config_flow.py
+++ tests/components/homewizard/test_config_flow.py
@@ -453,3 +453,48 @@ async def test_reconfigure_wrongdevice(
 
     # entry should still be original entry
     assert mock_config_entry.data[CONF_IP_ADDRESS] == "127.0.0.1"
+
+
+@pytest.mark.parametrize(
+    ("exception", "reason"),
+    [(DisabledError, "api_not_enabled"), (RequestError, "network_error")],
+)
+async def test_reconfigure_cannot_connect(
+    hass: HomeAssistant,
+    mock_homewizardenergy: MagicMock,
+    mock_config_entry: MockConfigEntry,
+    exception: Exception,
+    reason: str,
+) -> None:
+    """Test reconfiguration fails when not able to connect."""
+    mock_config_entry.add_to_hass(hass)
+    result = await mock_config_entry.start_reconfigure_flow(hass)
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+    assert result["errors"] == {}
+
+    mock_homewizardenergy.device.side_effect = exception
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_IP_ADDRESS: "1.0.0.127",
+        },
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {"base": reason}
+    assert result["data_schema"]({}) == {CONF_IP_ADDRESS: "127.0.0.1"}
+
+    # attempt with valid IP should work
+    mock_homewizardenergy.device.side_effect = None
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_IP_ADDRESS: "1.0.0.127",
+        },
+    )
+    await hass.async_block_till_done()
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reconfigure_successful"
+
+    # changed entry
+    assert mock_config_entry.data[CONF_IP_ADDRESS] == "1.0.0.127"
