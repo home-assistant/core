2fa97342bde7c9efad3fd9172e03777233451638
clumpyabbe <albinpersson73@gmail.com>
2024-11-25 18:23:38 +0100

    add basic functionallity for input in user flow

--- homeassistant/components/geocaching/config_flow.py
+++ homeassistant/components/geocaching/config_flow.py
@@ -7,6 +7,7 @@ import logging
 from typing import Any
 
 from geocachingapi.geocachingapi import GeocachingApi
+import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
@@ -41,7 +42,7 @@ class GeocachingFlowHandler(AbstractOAuth2FlowHandler, domain=DOMAIN):
         return await self.async_step_user()
 
     async def async_oauth_create_entry(self, data: dict[str, Any]) -> ConfigFlowResult:
-        """Create an oauth config entry or update existing entry for reauth."""
+        """Handle OAuth response and redirect to additional configuration."""
         api = GeocachingApi(
             environment=ENVIRONMENT,
             token=data["token"]["access_token"],
@@ -57,4 +58,33 @@ class GeocachingFlowHandler(AbstractOAuth2FlowHandler, domain=DOMAIN):
             self.hass.config_entries.async_update_entry(existing_entry, data=data)
             await self.hass.config_entries.async_reload(existing_entry.entry_id)
             return self.async_abort(reason="reauth_successful")
-        return self.async_create_entry(title=status.user.username, data=data)
+
+        # Create the final config entry
+        return await self.async_step_additional_config(
+            None, title=status.user.username, data=data
+        )
+
+    async def async_step_additional_config(
+        self,
+        user_input: dict[str, Any] | None,
+        title: str,
+        data: Mapping[str, Any],
+    ) -> ConfigFlowResult:
+        """Handle additional user input after authentication."""
+        # Check for existing entry and update or create a new entry
+
+        if user_input is None:
+            # Show the form to collect additional input
+            return self.async_show_form(
+                step_id="additional_config",
+                data_schema=vol.Schema(
+                    {
+                        vol.Required("setting_1"): str,
+                        vol.Required("setting_2"): str,
+                    }
+                ),
+                # description_placeholders={"username": self.data["username"]},
+            )
+        # Combine OAuth data with user-provided additional settings
+        # self.update(user_input)
+        return self.async_create_entry(title=title, data=data)
