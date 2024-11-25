4661ee95f70fe455f6782786ba33ff1c8cb0a7ff
Christopher Fenner <Christopher.Fenner@me.com>
2024-11-25 17:30:08 +0100

    set icon

--- homeassistant/components/vicare/fan.py
+++ homeassistant/components/vicare/fan.py
@@ -161,6 +161,30 @@ class ViCareFan(ViCareEntity, FanEntity):
         # Viessmann ventilation unit cannot be turned off
         return True
 
+    @property
+    def icon(self) -> str | None:
+        """Return the icon to use in the frontend."""
+        if hasattr(self, "_attr_preset_mode"):
+            if self._attr_preset_mode == VentilationMode.VENTILATION:
+                return "mdi:fan-clock"
+            if self._attr_preset_mode in [
+                VentilationMode.SENSOR_DRIVEN,
+                VentilationMode.SENSOR_OVERRIDE,
+            ]:
+                return "mdi:fan-auto"
+            if self._attr_preset_mode == VentilationMode.PERMANENT:
+                if self._attr_percentage == 0:
+                    return "mdi:fan-off"
+                if self._attr_percentage is not None:
+                    level = 1 + ORDERED_NAMED_FAN_SPEEDS.index(
+                        percentage_to_ordered_list_item(
+                            ORDERED_NAMED_FAN_SPEEDS, self._attr_percentage
+                        )
+                    )
+                    if level < 4:  # fan-speed- only supports 1-3
+                        return f"mdi:fan-speed-{level}"
+        return "mdi:fan"
+
     def set_percentage(self, percentage: int) -> None:
         """Set the speed of the fan, as a percentage."""
         if self._attr_preset_mode != str(VentilationMode.PERMANENT):
