4a8f3eea69bfb8d4f7fcfc2eb029fd5c7135be43
Franck Nijhof <git@frenck.dev>
2024-11-25 21:33:47 +0100

    Bump stookwijzer to v1.5.1 (#131567)

--- homeassistant/components/stookwijzer/sensor.py
+++ homeassistant/components/stookwijzer/sensor.py
@@ -12,7 +12,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN, StookwijzerState
+from .const import DOMAIN
 
 SCAN_INTERVAL = timedelta(minutes=60)
 
@@ -30,37 +30,33 @@ async def async_setup_entry(
 class StookwijzerSensor(SensorEntity):
     """Defines a Stookwijzer binary sensor."""
 
-    _attr_attribution = "Data provided by stookwijzer.nu"
+    _attr_attribution = "Data provided by atlasleefomgeving.nl"
     _attr_device_class = SensorDeviceClass.ENUM
     _attr_has_entity_name = True
-    _attr_name = None
-    _attr_translation_key = "stookwijzer"
+    _attr_translation_key = "advice"
 
     def __init__(self, client: Stookwijzer, entry: ConfigEntry) -> None:
         """Initialize a Stookwijzer device."""
         self._client = client
-        self._attr_options = [cls.value for cls in StookwijzerState]
+        self._attr_options = ["code_yellow", "code_orange", "code_red"]
         self._attr_unique_id = entry.entry_id
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, f"{entry.entry_id}")},
-            name="Stookwijzer",
-            manufacturer="stookwijzer.nu",
+            identifiers={(DOMAIN, entry.entry_id)},
+            manufacturer="Atlas Leefomgeving",
             entry_type=DeviceEntryType.SERVICE,
-            configuration_url="https://www.stookwijzer.nu",
+            configuration_url="https://www.atlasleefomgeving.nl/stookwijzer",
         )
 
-    def update(self) -> None:
+    async def async_update(self) -> None:
         """Update the data from the Stookwijzer handler."""
-        self._client.update()
+        await self._client.async_update()
 
     @property
     def available(self) -> bool:
         """Return if entity is available."""
-        return self._client.state is not None
+        return self._client.advice is not None
 
     @property
     def native_value(self) -> str | None:
         """Return the state of the device."""
-        if self._client.state is None:
-            return None
-        return StookwijzerState(self._client.state).value
+        return self._client.advice
