b1001aa920b36a8bc97f0e010d722028155f7d39
mj23000 <markusjacobsen@hotmail.com>
2024-11-25 13:16:47 +0100

    Add diagnostics

---
+++ homeassistant/components/bang_olufsen/diagnostics.py
@@ -0,0 +1,20 @@
+"""Support for the Bang & Olufsen diagnostics."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.core import HomeAssistant
+
+from . import BangOlufsenConfigEntry
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: BangOlufsenConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+
+    return {
+        "websocket_connection_state": config_entry.runtime_data.client.websocket_connected,
+        "config_entry": config_entry.as_dict(),
+    }
