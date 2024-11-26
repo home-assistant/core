9a999e87422658b9fc1b66d9e5062ab6d393ca8e
Klaas Schoute <klaas_schoute@hotmail.com>
2024-11-26 12:30:50 +0100

    Use ConfigEntry runtime_data in Garages Amsterdam (#131611)

--- homeassistant/components/garages_amsterdam/__init__.py
+++ homeassistant/components/garages_amsterdam/__init__.py
@@ -9,7 +9,6 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN
 from .coordinator import GaragesAmsterdamDataUpdateCoordinator
 
 PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.SENSOR]
@@ -17,24 +16,23 @@ PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.SENSOR]
 type GaragesAmsterdamConfigEntry = ConfigEntry[GaragesAmsterdamDataUpdateCoordinator]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(
+    hass: HomeAssistant, entry: GaragesAmsterdamConfigEntry
+) -> bool:
     """Set up Garages Amsterdam from a config entry."""
     client = ODPAmsterdam(session=async_get_clientsession(hass))
     coordinator = GaragesAmsterdamDataUpdateCoordinator(hass, client)
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    entry.runtime_data = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(
+    hass: HomeAssistant, entry: GaragesAmsterdamConfigEntry
+) -> bool:
     """Unload Garages Amsterdam config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if len(hass.config_entries.async_entries(DOMAIN)) == 1:
-        hass.data.pop(DOMAIN)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
