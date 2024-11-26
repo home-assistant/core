5d73c1b79166ee6b9c065c9eb4a4b62b3a882926
krauseerl <4983703+krauseerl@users.noreply.github.com>
2024-11-26 20:31:47 +0100

    homekit bridge linked_doorbell_sensor is an invalid option for homekit locks 129443

--- tests/components/homekit/test_util.py
+++ tests/components/homekit/test_util.py
@@ -157,10 +157,25 @@ def test_validate_entity_config() -> None:
     }
 
     assert vec({"lock.demo": {}}) == {
-        "lock.demo": {ATTR_CODE: None, CONF_LOW_BATTERY_THRESHOLD: 20}
+        "lock.demo": {
+            ATTR_CODE: None,
+            CONF_LOW_BATTERY_THRESHOLD: 20,
+            CONF_LINKED_DOORBELL_SENSOR: None,
+        }
     }
-    assert vec({"lock.demo": {ATTR_CODE: "1234"}}) == {
-        "lock.demo": {ATTR_CODE: "1234", CONF_LOW_BATTERY_THRESHOLD: 20}
+    assert vec(
+        {
+            "lock.demo": {
+                ATTR_CODE: "1234",
+                CONF_LINKED_DOORBELL_SENSOR: "event.doorbell",
+            }
+        }
+    ) == {
+        "lock.demo": {
+            ATTR_CODE: "1234",
+            CONF_LOW_BATTERY_THRESHOLD: 20,
+            CONF_LINKED_DOORBELL_SENSOR: "event.doorbell",
+        }
     }
 
     assert vec({"media_player.demo": {}}) == {
