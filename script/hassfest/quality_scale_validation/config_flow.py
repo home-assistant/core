a232843bde40d9125b3f6e88b06c0242b47b4c54
epenet <6771947+epenet@users.noreply.github.com>
2024-11-28 16:17:04 +0000

    Use integration.config_flow property

--- script/hassfest/quality_scale_validation/config_flow.py
+++ script/hassfest/quality_scale_validation/config_flow.py
@@ -3,23 +3,22 @@
 https://developers.home-assistant.io/docs/core/integration-quality-scale/rules/config-flow/
 """
 
-from homeassistant.generated import config_flows
 from script.hassfest.model import Integration
 
 
 def validate(integration: Integration) -> list[str] | None:
     """Validate that the integration implements config flow."""
 
+    if not integration.config_flow:
+        return [
+            "Integration does not set config_flow in its manifest "
+            f"homeassistant/components/{integration.domain}/manifest.json",
+        ]
+
     config_flow_file = integration.path / "config_flow.py"
     if not config_flow_file.exists():
         return [
             "Integration does not implement config flow (is missing config_flow.py)",
-        ]
-
-    if integration.domain not in config_flows.FLOWS["integration"]:
-        return [
-            "Integration is missing from homeassistant/generator/config_flow.py "
-            f"(or homeassistant/components/{integration.domain}/manifest.json)",
         ]
 
     return None
