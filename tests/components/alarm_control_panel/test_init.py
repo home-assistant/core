660e492d5efdb53ad7943725db65d142343200b5
Robert Resch <robert@resch.dev>
2024-11-28 08:59:17 +0100

    Remove deprecated alarm control panel constants

--- tests/components/alarm_control_panel/test_init.py
+++ tests/components/alarm_control_panel/test_init.py
@@ -34,7 +34,6 @@ from tests.common import (
     MockConfigEntry,
     MockModule,
     help_test_all,
-    import_and_test_deprecated_constant_enum,
     mock_integration,
     setup_test_component_platform,
 )
@@ -64,44 +63,6 @@ async def help_test_async_alarm_control_panel_service(
 def test_all(module: ModuleType) -> None:
     """Test module.__all__ is correctly set."""
     help_test_all(module)
-
-
-@pytest.mark.parametrize(
-    "code_format",
-    list(alarm_control_panel.CodeFormat),
-)
-@pytest.mark.parametrize(
-    "module",
-    [alarm_control_panel, alarm_control_panel.const],
-)
-def test_deprecated_constant_code_format(
-    caplog: pytest.LogCaptureFixture,
-    code_format: alarm_control_panel.CodeFormat,
-    module: ModuleType,
-) -> None:
-    """Test deprecated format constants."""
-    import_and_test_deprecated_constant_enum(
-        caplog, module, code_format, "FORMAT_", "2025.1"
-    )
-
-
-@pytest.mark.parametrize(
-    "entity_feature",
-    list(alarm_control_panel.AlarmControlPanelEntityFeature),
-)
-@pytest.mark.parametrize(
-    "module",
-    [alarm_control_panel, alarm_control_panel.const],
-)
-def test_deprecated_support_alarm_constants(
-    caplog: pytest.LogCaptureFixture,
-    entity_feature: alarm_control_panel.AlarmControlPanelEntityFeature,
-    module: ModuleType,
-) -> None:
-    """Test deprecated support alarm constants."""
-    import_and_test_deprecated_constant_enum(
-        caplog, module, entity_feature, "SUPPORT_ALARM_", "2025.1"
-    )
 
 
 def test_deprecated_supported_features_ints(caplog: pytest.LogCaptureFixture) -> None:
