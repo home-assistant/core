8120e608ff249b0035d9e1781a88d58a78b58b39
Giacomo <borile.giacomo@gmail.com>
2024-11-28 18:18:50 +0000

    Fix vari gestione eventi

--- homeassistant/components/borsmarthome/cover.py
+++ homeassistant/components/borsmarthome/cover.py
@@ -1,5 +1,4 @@
 import logging
-import random
 from typing import Any
 
 import requests
@@ -11,7 +10,7 @@ from homeassistant.components.cover import (
     CoverEntity,
     CoverEntityFeature,
 )
-from homeassistant.core import Event, EventStateChangedData, HomeAssistant, callback
+from homeassistant.core import Event, EventStateChangedData, HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.event import async_track_state_change_event
 
@@ -38,28 +37,41 @@ class TapparellaEntity(CoverEntity):
     _attr_has_entity_name = True
     _attr_should_poll = False
     _attr_device_class = CoverDeviceClass.SHUTTER
+    unsub = None
 
-    def SetCurrentPosition(self, position: float):
+    def set_current_position(self, position: float):
+        """Set position."""
         self.is_closed = position == 0
         self.current_cover_position = position
 
-    @callback
-    async def _async_on_change(self, event: Event[EventStateChangedData]) -> None:
+    async def async_on_change(self, event: Event[EventStateChangedData]) -> None:
+        """On change callback."""
         new_state = event.data["new_state"]
-        currentPosition = new_state.attributes["current_position"]
-        self.SetCurrentPosition(currentPosition)
-        self.is_opening = False
-        self.is_closing = False
+
+        current_position = new_state.attributes.get("currentPosition")
+        if current_position:
+            self.set_current_position(current_position)
+
+        is_opening = new_state.attributes.get("isOpening")
+        if is_opening is not None:
+            self.is_opening = is_opening
+
+        is_closing = new_state.attributes.get("isClosing")
+        if is_closing is not None:
+            self.is_closing = is_closing
+
         await self._roller.async_publish_updates()
-        # await self.async_write_ha_state()
-        # await self._roller.stop()
-        # self.async_update_ha_state()
+
+    async def async_will_remove_from_hass(self):
+        """On remove from hass."""
+        self.unsub()
+        self._roller.remove_callback(self.async_write_ha_state)
 
     async def async_added_to_hass(self):
-        unsub = async_track_state_change_event(
-            self.hass, self.entity_id, self._async_on_change
+        """On add to hass."""
+        self.unsub = async_track_state_change_event(
+            self.hass, self.entity_id, self.async_on_change
         )
-        # unsub()
         self._roller.register_callback(self.async_write_ha_state)
 
     def __init__(self, roller: RollerShutter):
@@ -67,8 +79,7 @@ class TapparellaEntity(CoverEntity):
         self._name = roller.name
         self._roller = roller
         self._channel = roller.name
-        self.is_closed = roller.position == 0
-        self.current_cover_position = roller.position
+        self.set_current_position(roller.position)
         self.unique_id = f"avebus_{roller.name}"
 
     @property
@@ -123,31 +134,17 @@ class TapparellaEntity(CoverEntity):
         except requests.RequestException as e:
             _LOGGER.error("Errore durante il fermo della tapparella: %s", e)
 
-    # def async_update_ha_state(self, force_refresh=False):
-    #     return super().async_update_ha_state(force_refresh)
-
-    # def async_update_ha_state(self, force_refresh=False):
-    #     return super().async_update_ha_state(force_refresh)
-
     async def async_set_cover_position(self, **kwargs):
         """Set position."""
         try:
             position = kwargs["position"]
-            response = requests.post(
-                f"{self._url}/avebus/{self._channel}?percentage={position}",
-                headers=self._headers,
-                timeout=30,
-            )
-            response.raise_for_status()
+            await self._roller.async_request_position(position)
 
             if position > self.current_cover_position:
                 self.is_opening = True
             else:
                 self.is_closing = True
 
-            self.SetCurrentPosition(position)
-
-            await self.async_write_ha_state()
-            _LOGGER.debug("Set position at %s", kwargs["position"])
+            await self._roller.async_publish_updates()
         except requests.RequestException as e:
             _LOGGER.error("Errore durante il set della tapparella: %s", e)
