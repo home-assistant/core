a47928f43f4f7771ab769ce4daef94dfd82ee253
Allen Porter <allen.porter@gmail.com>
2024-11-29 15:28:42 +0000

    Patch with changes from epenet

--- script/hassfest/quality_scale_validation/runtime_data.py
+++ script/hassfest/quality_scale_validation/runtime_data.py
@@ -8,44 +8,46 @@ import ast
 from script.hassfest.model import Integration
 
 
-def _sets_attribute(tree: ast.AST, node_name: str, attribute_name: str) -> bool:
-    """Check that an attribute is set on a node within the tree."""
-    for node in ast.walk(tree):
+def _sets_runtime_data(
+    async_setup_entry_function: ast.AsyncFunctionDef, config_entry_argument: ast.arg
+) -> bool:
+    """Check that `entry.runtime` gets set within `async_setup_entry`."""
+    for node in ast.walk(async_setup_entry_function):
         if (
             isinstance(node, ast.Attribute)
             and isinstance(node.value, ast.Name)
-            and node.value.id == node_name
-            and node.attr == attribute_name
+            and node.value.id == config_entry_argument.arg
+            and node.attr == "runtime_data"
             and isinstance(node.ctx, ast.Store)
         ):
             return True
     return False
 
 
-def _sets_runtime_data_on_setup(init: ast.AST, filename: str) -> bool:
-    """Check if the integration sets entry.runtime_data in async_setup_entry."""
-    for item in init.body:
-        if not (
-            isinstance(item, ast.AsyncFunctionDef) and item.name == "async_setup_entry"
-        ):
-            continue
-        if len(item.args.args) != 2:
-            raise ValueError(
-                f"async_setup_entry in {filename} has incorrect signature (expected 2 arguments)"
-            )
-        config_entry_arg = item.args.args[1].arg
-        return _sets_attribute(item, config_entry_arg, "runtime_data")
-    return False
+def _get_setup_entry_function(module: ast.Module) -> ast.AsyncFunctionDef | None:
+    """Get async_setup_entry function."""
+    for item in module.body:
+        if isinstance(item, ast.AsyncFunctionDef) and item.name == "async_setup_entry":
+            return item
+    return None
 
 
 def validate(integration: Integration) -> list[str] | None:
-    """Validate that the integration uses ConfigEntry.runtime_data to store runtime data."""
+    """Validate correct use of ConfigEntry.runtime_data."""
     init_file = integration.path / "__init__.py"
     init = ast.parse(init_file.read_text())
 
-    if not _sets_runtime_data_on_setup(init, init_file):
+    # Should not happen, but better to be safe
+    if not (async_setup_entry := _get_setup_entry_function(init)):
+        return [f"Could not find `async_setup_entry` in {init_file}"]
+    if len(async_setup_entry.args.args) != 2:
+        return [f"async_setup_entry has incorrect signature in {init_file}"]
+    config_entry_argument = async_setup_entry.args.args[1]
+
+    if not _sets_runtime_data(async_setup_entry, config_entry_argument):
         return [
-            "Integration does not set entry.runtime_data in async_setup_entry",
+            "Integration does not set entry.runtime_data in async_setup_entry"
+            f"({init_file})"
         ]
 
     return None
