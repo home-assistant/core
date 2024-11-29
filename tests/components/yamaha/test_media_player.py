1ec67e8f34be108064e7444a584f9aedd4730eee
Marc Vanbrabant <slyoldfox@gmail.com>
2024-11-29 10:05:59 +0100

    #130820 Add config flow to Yamaha - increase test coverage for issue creation

--- tests/components/yamaha/test_media_player.py
+++ tests/components/yamaha/test_media_player.py
@@ -4,12 +4,15 @@ from collections.abc import Generator
 from unittest.mock import MagicMock, PropertyMock, call, patch
 
 import pytest
+from requests.exceptions import ConnectionError
 from rxv.ssdp import RxvDetails
 
+from homeassistant.components import ssdp
 from homeassistant.components.media_player import DOMAIN as MP_DOMAIN
 from homeassistant.components.yamaha import media_player as yamaha
 from homeassistant.components.yamaha.const import DOMAIN
-from homeassistant.core import HomeAssistant
+from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
+from homeassistant.helpers import issue_registry as ir
 from homeassistant.setup import async_setup_component
 
 CONFIG = {"media_player": {"platform": "yamaha", "host": "127.0.0.1"}}
@@ -71,7 +74,7 @@ def device_fixture(main_zone):
             "homeassistant.components.yamaha.YamahaConfigInfo.get_rxv_details",
             return_value=RxvDetails(
                 model_name="MC20",
-                ctrl_url=None,
+                ctrl_url=device.ctrl_url,
                 unit_desc_url=None,
                 friendly_name=None,
                 serial_number="1234567890",
@@ -93,7 +96,7 @@ def device2_fixture(main_zone):
             "homeassistant.components.yamaha.YamahaConfigInfo.get_rxv_details",
             return_value=RxvDetails(
                 model_name="AX100",
-                ctrl_url=None,
+                ctrl_url=device.ctrl_url,
                 unit_desc_url=None,
                 friendly_name=None,
                 serial_number="0987654321",
@@ -103,7 +106,71 @@ def device2_fixture(main_zone):
         yield device
 
 
-async def test_setup_host(hass: HomeAssistant, device, device2, main_zone) -> None:
+@pytest.fixture
+def mock_invalid_discovery_information():
+    """Mock that the ssdp scanner returns a useful upnp description."""
+    with (
+        patch(
+            "homeassistant.components.yamaha.YamahaConfigInfo.check_yamaha_ssdp",
+            return_value=True,
+        ),
+        patch(
+            "homeassistant.components.ssdp.async_get_discovery_info_by_st",
+            return_value=[
+                ssdp.SsdpServiceInfo(
+                    ssdp_usn="mock_usn",
+                    ssdp_st="mock_st",
+                    ssdp_location="http://127.0.0.1:9000/MediaRenderer/desc.xml",
+                    ssdp_headers={
+                        "_host": "127.0.0.1",
+                    },
+                    upnp={
+                        ssdp.ATTR_UPNP_SERIAL: "1234567890",
+                        ssdp.ATTR_UPNP_MODEL_NAME: "MC20",
+                    },
+                )
+            ],
+        ),
+        patch(
+            "homeassistant.components.yamaha.YamahaConfigInfo.get_rxv_details",
+            side_effect=ConnectionError("mocked error"),
+        ),
+    ):
+        yield
+
+
+@pytest.fixture
+def mock_valid_discovery_information():
+    """Mock that the ssdp scanner returns a useful upnp description."""
+    with (
+        patch(
+            "homeassistant.components.ssdp.async_get_discovery_info_by_st",
+            return_value=[
+                ssdp.SsdpServiceInfo(
+                    ssdp_usn="mock_usn",
+                    ssdp_st="mock_st",
+                    ssdp_location="http://127.0.0.1:9000/MediaRenderer/desc.xml",
+                    ssdp_headers={
+                        "_host": "127.0.0.1",
+                    },
+                    upnp={
+                        ssdp.ATTR_UPNP_SERIAL: "1234567890",
+                        ssdp.ATTR_UPNP_MODEL_NAME: "MC20",
+                    },
+                )
+            ],
+        ),
+    ):
+        yield
+
+
+async def test_setup_host(
+    hass: HomeAssistant,
+    device,
+    device2,
+    main_zone,
+    issue_registry: ir.IssueRegistry,
+) -> None:
     """Test set up integration with host."""
     assert await async_setup_component(hass, MP_DOMAIN, CONFIG)
     await hass.async_block_till_done()
@@ -142,7 +209,9 @@ async def test_setup_find_errors(hass: HomeAssistant, device, main_zone, error) 
     assert state.state == "off"
 
 
-async def test_setup_no_host(hass: HomeAssistant, device, main_zone) -> None:
+async def test_setup_no_host(
+    hass: HomeAssistant, device, main_zone, issue_registry: ir.IssueRegistry
+) -> None:
     """Test set up integration without host."""
     assert await async_setup_component(
         hass, MP_DOMAIN, {"media_player": {"platform": "yamaha"}}
@@ -152,6 +221,71 @@ async def test_setup_no_host(hass: HomeAssistant, device, main_zone) -> None:
     state = hass.states.get("media_player.yamaha_receiver_main_zone")
 
     assert state is None
+
+    assert len(issue_registry.issues) == 2
+    issue = issue_registry.async_get_issue(
+        DOMAIN, "deprecated_yaml_import_issue_no_discover"
+    )
+    assert issue
+    assert issue.translation_key == "deprecated_yaml_import_issue_no_discover"
+
+    issue2 = issue_registry.async_get_issue(
+        HOMEASSISTANT_DOMAIN, f"deprecated_yaml_{DOMAIN}"
+    )
+    assert issue2
+    assert issue2.translation_key == "deprecated_yaml"
+    assert issue2.issue_domain == DOMAIN
+
+
+async def test_setup_discovery(
+    hass: HomeAssistant,
+    device,
+    main_zone,
+    mock_valid_discovery_information,
+    issue_registry: ir.IssueRegistry,
+) -> None:
+    """Test set up integration via discovery."""
+    assert await async_setup_component(
+        hass, MP_DOMAIN, {"media_player": {"platform": "yamaha"}}
+    )
+    await hass.async_block_till_done()
+
+    state = hass.states.get("media_player.yamaha_receiver_main_zone")
+
+    assert state is not None
+    assert state.state == "off"
+
+    assert len(issue_registry.issues) == 1
+    issue = issue_registry.async_get_issue(
+        HOMEASSISTANT_DOMAIN, f"deprecated_yaml_{DOMAIN}"
+    )
+    assert issue
+    assert issue.translation_key == "deprecated_yaml"
+    assert issue.issue_domain == DOMAIN
+
+
+async def test_setup_discovery_invalid_host(
+    hass: HomeAssistant,
+    mock_invalid_discovery_information,
+    issue_registry: ir.IssueRegistry,
+) -> None:
+    """Test set up integration via discovery."""
+    assert await async_setup_component(
+        hass, MP_DOMAIN, {"media_player": {"platform": "yamaha"}}
+    )
+    await hass.async_block_till_done()
+
+    state = hass.states.get("media_player.yamaha_receiver_main_zone")
+
+    assert state is None
+
+    assert len(issue_registry.issues) == 1
+    issue = issue_registry.async_get_issue(
+        DOMAIN, "deprecated_yaml_import_issue_127.0.0.1_cannot_connect"
+    )
+    assert issue
+    assert issue.translation_key == "deprecated_yaml_import_issue_cannot_connect"
+    assert issue.issue_domain == DOMAIN
 
 
 async def test_enable_output(hass: HomeAssistant, device, main_zone) -> None:
