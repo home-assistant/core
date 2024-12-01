4801b403a0e50f5e3986b90e600d3ea20bd8920a
Myles Eftos <myles@madpilot.com.au>
2024-12-02 06:41:41 +1100

    Remove advanced price from actual interval

--- tests/components/amberelectric/helpers.py
+++ tests/components/amberelectric/helpers.py
@@ -30,7 +30,6 @@ def generate_actual_interval(channel_type: ChannelType, end_time: datetime) -> I
             channel_type=channel_type,
             spike_status=SpikeStatus.NONE,
             descriptor=PriceDescriptor.LOW,
-            advanced_price=AdvancedPrice(low=10, high=20),
         )
     )
 
