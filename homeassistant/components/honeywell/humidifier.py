aea14840c0f181952e9c0ca137a57aded0d17063
mkmer <mike.j.kasper@gmail.com>
2024-12-02 19:21:12 +0000

    finish humidifier functions

--- homeassistant/components/honeywell/humidifier.py
+++ homeassistant/components/honeywell/humidifier.py
@@ -29,24 +29,39 @@ DEHUMIDIFIER_KEY = "dehumidfier"
 class HoneywellHumidifierEntityDescription(HumidifierEntityDescription):
     """Describes a Honeywell sensor entity."""
 
-    current_humidity: Callable[[Device], Any] | None = None
-    humidity: Callable[[Device], Any] | None = None
-
-    # value_fn: Callable[[Device], Any]
-    # unit_fn: Callable[[Device], Any]
+    current_humidity: Callable[[Device], Any]
+    current_set_humidity: Callable[[Device], Any]
+    max_humidity: Callable[[Device], Any]
+    min_humidity: Callable[[Device], Any]
+    set_humidity: Callable[[Device], Any]
+    mode: Callable[[Device], Any]
+    off: Callable[[Device], Any]
+    on: Callable[[Device], Any]
 
 
 HUMIDIFIERS: dict[str, HoneywellHumidifierEntityDescription] = {
     "Humidifier": HoneywellHumidifierEntityDescription(
         key=HUMIDIFIER_KEY,
         current_humidity=lambda device: device.current_humidity,
-        humidity=lambda device: device.set_humidifier_setpoint,
+        set_humidity=lambda device: device.set_humidifier_setpoint,
+        min_humidity=lambda device: device.humidifier_lower_limit,
+        max_humidity=lambda device: device.humidifier_upper_limit,
+        current_set_humidity=lambda device: device.humidifier_setpoint,
+        mode=lambda device: device.humidifer_mode,
+        off=lambda device: device.set_dehumidifer_off,
+        on=lambda device: device.set_dehumidifer_auto,
         device_class=HumidifierDeviceClass.HUMIDIFIER,
     ),
     "Dehumidifier": HoneywellHumidifierEntityDescription(
         key=DEHUMIDIFIER_KEY,
         current_humidity=lambda device: device.current_humidity,
-        humidity=lambda device: device.set_dehumidifier_setpoint,
+        set_humidity=lambda device: device.set_dehumidifier_setpoint,
+        min_humidity=lambda device: device.dehumidifier_lower_limit,
+        max_humidity=lambda device: device.dehumidifier_upper_limit,
+        current_set_humidity=lambda device: device.dehumidifier_setpoint,
+        mode=lambda device: device.dehumidifer_mode,
+        off=lambda device: device.set_dehumidifer_off,
+        on=lambda device: device.set_dehumidifer_auto,
         device_class=HumidifierDeviceClass.DEHUMIDIFIER,
     ),
 }
@@ -82,12 +97,61 @@ class HoneywellHumidifier(HumidifierEntity):
         self.entity_description = description
         self._attr_unique_id = f"{device.deviceid}_{description.key}"
         self._attr_native_unit_of_measurement = description.unit_fn(device)
-
+        self._set_humidity = description.current_set_humidity(device)
+        self._attr_min_humidity = description.min_humidity(device)
+        self._attr_max_humidity = description.max_humidity(device)
+        self._current_humidity = description.current_humidity(device)
+        self._mode = description.mode(device)
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, device.deviceid)},
             name=device.name,
             manufacturer="Honeywell",
         )
+
+    @property
+    def is_on(self) -> bool:
+        """Return the device is on or off."""
+        return self.entity_description.mode(self._device) != 0
+
+    @property
+    def mode(self) -> str | None:
+        """Return the current mode."""
+        return self.entity_description.mode(self._device)
+
+    @property
+    def target_humidity(self) -> int | None:
+        """Return the humidity we try to reach."""
+        if self._set_humidity is None:
+            return None
+
+        humidity = self.entity_description.current_set_humidity(self._device)
+        if humidity is None:
+            return None
+
+        return humidity
+
+    @property
+    def current_humidity(self) -> int | None:
+        """Return the current humidity."""
+        if self._current_humidity is None:
+            return None
+        return self.entity_description.current_humidity(self._device)
+
+    def turn_on(self, **kwargs: Any) -> None:
+        """Turn the device on."""
+        self.entity_description.on(self._device)
+
+    def turn_off(self, **kwargs: Any) -> None:
+        """Turn the device off."""
+        self.entity_description.off(self._device)
+
+    def set_humidity(self, humidity: int) -> None:
+        """Set new target humidity."""
+        if self._set_humidity is None:
+            raise RuntimeError(
+                "Cannot set humidity, device doesn't provide methods to set it"
+            )
+        self.entity_description.set_humidity(self._device)
 
 
 # Look at Tuya humidifier for help....
