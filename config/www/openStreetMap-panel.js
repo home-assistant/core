284ff67e0e64496ca23f5fb272185480408bf994
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2024-12-02 13:38:43 +0000

    Comment out unused imports

--- config/www/openStreetMap-panel.js
+++ config/www/openStreetMap-panel.js
@@ -1,9 +1,9 @@
 import "https://unpkg.com/wired-card@2.1.0/lib/wired-card.js?module";
-import {
-  LitElement,
-  html,
-  css,
-} from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
+// import {
+//   LitElement,
+//   html,
+//   css,
+// } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
 class OpenStreetMapPanel extends HTMLElement {
     set hass(hass) {
