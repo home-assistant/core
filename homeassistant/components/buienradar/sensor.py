eaf9af38751d4c4243f79c40ee72b79d95aa0628
abmantis <amfcalt@gmail.com>
2024-12-02 11:56:23 +0000

    Remove redundant comment

--- homeassistant/components/buienradar/sensor.py
+++ homeassistant/components/buienradar/sensor.py
@@ -780,7 +780,6 @@ class BrSensor(SensorEntity):
     @callback
     def _load_data(self, data):  # noqa: C901
         """Load the sensor with relevant data."""
-        # Find sensor
         # Check if we have a new measurement,
         # otherwise we do not have to update the sensor
         if self._measured == data.get(MEASURED):
