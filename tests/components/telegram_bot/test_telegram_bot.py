2ba65ed529e4783ca1fe5d34b7592cc1447aa2b8
tyron <tyron@users.noreply.github.com>
2024-12-02 02:51:35 +0000

    added boilerplate for testing

--- tests/components/telegram_bot/test_telegram_bot.py
+++ tests/components/telegram_bot/test_telegram_bot.py
@@ -1,7 +1,9 @@
 """Tests for the telegram_bot component."""
 
+import base64
+import io
 from typing import Any
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import AsyncMock, MagicMock, Mock, mock_open, patch
 
 import pytest
 from telegram import Update
@@ -12,6 +14,7 @@ from homeassistant.components.telegram_bot import (
     ATTR_MESSAGE_THREAD_ID,
     DOMAIN,
     SERVICE_SEND_MESSAGE,
+    SERVICE_SEND_PHOTO,
 )
 from homeassistant.components.telegram_bot.webhooks import TELEGRAM_WEBHOOK_URL
 from homeassistant.const import EVENT_HOMEASSISTANT_START
@@ -46,6 +49,62 @@ async def test_send_message(hass: HomeAssistant, webhook_platform) -> None:
         return_response=True,
     )
     await hass.async_block_till_done()
+
+    assert len(events) == 1
+    assert events[0].context == context
+
+    assert len(response["chats"]) == 1
+    assert (response["chats"][0]["message_id"]) == 12345
+
+
+@patch(
+    "builtins.open",
+    mock_open(
+        read_data=base64.b64decode(
+            "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubXdtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJCKE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHChahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9kLp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+qXkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMsOF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8QyqwZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjlithJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ekokRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6EhOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFwg59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+gqjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDFNx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVOJ3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HUtlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASUVORK5CYII="
+        )
+    ),
+    create=True,
+)
+def _read_file_as_bytesio_mock(file_path):
+    """Convert file to BytesIO for testing."""
+    _file = None
+
+    with open(file_path, encoding="utf8") as file_handler:
+        _file = io.BytesIO(file_handler.read())
+
+    _file.name = "image.jpg"
+    _file.seek(0)
+
+    return _file
+
+
+@patch("os.path.isfile", Mock(return_value=True))
+@patch("os.access", Mock(return_value=True))
+async def test_send_photo(hass: HomeAssistant, webhook_platform) -> None:
+    """Test the send_message service."""
+    context = Context()
+    events = async_capture_events(hass, "telegram_sent")
+
+    hass.config.allowlist_external_dirs.add("/media/")
+
+    # Mock the file handler read with our 1x1 base64 encoded fixture image
+    with patch(
+        "homeassistant.components.telegram_bot._read_file_as_bytesio",
+        _read_file_as_bytesio_mock,
+    ):
+        response = await hass.services.async_call(
+            DOMAIN,
+            SERVICE_SEND_PHOTO,
+            {
+                "file": "/media/image.jpg",
+                ATTR_MESSAGE_THREAD_ID: "123",
+            },
+            blocking=True,
+            context=context,
+            return_response=True,
+        )
+        await hass.async_block_till_done()
 
     assert len(events) == 1
     assert events[0].context == context
