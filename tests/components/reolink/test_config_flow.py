891412d7d3b4edfff418f1d23119421592e5fe4f
starkillerOG <starkiller.og@gmail.com>
2024-12-02 15:01:28 +0100

    Remove option to update settings using second config flow in Reolink (#131695)

--- tests/components/reolink/test_config_flow.py
+++ tests/components/reolink/test_config_flow.py
@@ -286,51 +286,6 @@ async def test_options_flow(hass: HomeAssistant, mock_setup_entry: MagicMock) ->
     }
 
 
-async def test_change_connection_settings(
-    hass: HomeAssistant, mock_setup_entry: MagicMock
-) -> None:
-    """Test changing connection settings by issuing a second user config flow."""
-    config_entry = MockConfigEntry(
-        domain=DOMAIN,
-        unique_id=format_mac(TEST_MAC),
-        data={
-            CONF_HOST: TEST_HOST,
-            CONF_USERNAME: TEST_USERNAME,
-            CONF_PASSWORD: TEST_PASSWORD,
-            CONF_PORT: TEST_PORT,
-            CONF_USE_HTTPS: TEST_USE_HTTPS,
-        },
-        options={
-            CONF_PROTOCOL: DEFAULT_PROTOCOL,
-        },
-        title=TEST_NVR_NAME,
-    )
-    config_entry.add_to_hass(hass)
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "user"
-    assert result["errors"] == {}
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {
-            CONF_HOST: TEST_HOST2,
-            CONF_USERNAME: TEST_USERNAME2,
-            CONF_PASSWORD: TEST_PASSWORD2,
-        },
-    )
-
-    assert result["type"] is FlowResultType.ABORT
-    assert result["reason"] == "already_configured"
-    assert config_entry.data[CONF_HOST] == TEST_HOST2
-    assert config_entry.data[CONF_USERNAME] == TEST_USERNAME2
-    assert config_entry.data[CONF_PASSWORD] == TEST_PASSWORD2
-
-
 async def test_reauth(hass: HomeAssistant, mock_setup_entry: MagicMock) -> None:
     """Test a reauth flow."""
     config_entry = MockConfigEntry(
