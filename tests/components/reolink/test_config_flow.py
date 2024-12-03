fa1f470cae63632689bb738656044cdd57b08ceb
starkillerOG <starkiller.og@gmail.com>
2024-12-03 09:02:16 +0100

    Update tests

--- tests/components/reolink/test_config_flow.py
+++ tests/components/reolink/test_config_flow.py
@@ -311,14 +311,6 @@ async def test_reauth(hass: HomeAssistant, mock_setup_entry: MagicMock) -> None:
     result = await config_entry.start_reauth_flow(hass)
 
     assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "reauth_confirm"
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {},
-    )
-
-    assert result["type"] is FlowResultType.FORM
     assert result["step_id"] == "user"
     assert result["errors"] == {}
 
