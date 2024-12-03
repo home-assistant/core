6c6ae6c841002d02e39cb6cfc78116a30c26329f
jb101010-2 <dev.julien.basson@gmail.com>
2024-12-03 22:11:34 +0100

    wip: fix few things

--- tests/components/suez_water/test_init.py
+++ tests/components/suez_water/test_init.py
@@ -6,9 +6,9 @@ from zoneinfo import ZoneInfo
 
 from freezegun.api import FrozenDateTimeFactory
 import pytest
+from syrupy import SnapshotAssertion
 
 from homeassistant.components.suez_water.const import CONF_COUNTER_ID, DOMAIN
-from homeassistant.components.recorder import Recorder
 from homeassistant.components.recorder.statistics import statistics_during_period
 from homeassistant.components.suez_water.const import DATA_REFRESH_INTERVAL
 from homeassistant.components.suez_water.coordinator import (
@@ -54,6 +54,7 @@ async def test_initialization_setup_api_error(
     assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
 
 
+@pytest.mark.usefixtures("recorder_mock")
 @pytest.mark.parametrize(
     ("statistic", "daily_value"),
     [
@@ -65,8 +66,8 @@ async def test_statistics(
     hass: HomeAssistant,
     suez_client: AsyncMock,
     mock_config_entry: MockConfigEntry,
-    recorder_mock: Recorder,
     freezer: FrozenDateTimeFactory,
+    snapshot: SnapshotAssertion,
     statistic: str,
     daily_value: float,
 ) -> None:
@@ -93,6 +94,7 @@ async def test_statistics(
     await _test_for_data(
         hass,
         suez_client,
+        snapshot,
         statistic,
         origin,
         mock_config_entry.data[CONF_COUNTER_ID],
@@ -109,6 +111,7 @@ async def test_statistics(
     await _test_for_data(
         hass,
         suez_client,
+        snapshot,
         statistic,
         origin,
         mock_config_entry.data[CONF_COUNTER_ID],
@@ -126,6 +129,7 @@ async def test_statistics(
     await _test_for_data(
         hass,
         suez_client,
+        snapshot,
         statistic,
         origin,
         mock_config_entry.data[CONF_COUNTER_ID],
@@ -144,6 +148,7 @@ async def test_statistics(
     await _test_for_data(
         hass,
         suez_client,
+        snapshot,
         statistic,
         origin,
         mock_config_entry.data[CONF_COUNTER_ID],
@@ -157,6 +162,7 @@ async def test_statistics(
 async def _test_for_data(
     hass: HomeAssistant,
     suez_client: AsyncMock,
+    snapshot: SnapshotAssertion,
     statistic: str,
     origin: datetime,
     counter_id: str,
@@ -180,20 +186,21 @@ async def _test_for_data(
         None,
         {"start", "state", "mean", "min", "max", "last_reset", "sum"},
     )
+    assert stats == snapshot
 
-    assert stats.get(statistic_id) is not None
-    assert len(stats[statistic_id]) == nb_samples + extra_samples
-    _sum = 0
-    for _k, stat in enumerate(stats[statistic_id]):
-        assert stat["state"] == daily_value
-        assert stat["last_reset"] is None
+    # assert stats.get(statistic_id) is not None
+    # assert len(stats[statistic_id]) == nb_samples + extra_samples
+    # _sum = 0
+    # for _k, stat in enumerate(stats[statistic_id]):
+    #     assert stat["state"] == daily_value
+    #     assert stat["last_reset"] is None
 
-        _sum += daily_value
-        assert stat["sum"] == _sum
-        assert stat.get("max") is None
-        assert stat.get("min") is None
-        assert stat.get("mean") is None
-        assert stat.get("last_reset") is None
+    #     _sum += daily_value
+    #     assert stat["sum"] == _sum
+    #     assert stat.get("max") is None
+    #     assert stat.get("min") is None
+    #     assert stat.get("mean") is None
+    #     assert stat.get("last_reset") is None
 
 
 async def test_migration_version_rollback(
