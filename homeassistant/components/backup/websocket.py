37853fdaed6140c9871cc18c065b49b16339417e
Erik <erik@montnemery.com>
2024-12-04 14:32:27 +0100

    Revert "Make sure backup directory exists"
    This reverts commit 142dedc76855271e898d3d0fa5953e831d162f9b.

--- homeassistant/components/backup/websocket.py
+++ homeassistant/components/backup/websocket.py
@@ -11,7 +11,6 @@ from .config import ScheduleState
 from .const import DATA_MANAGER, LOGGER
 from .manager import BackupEvent
 from .models import Folder
-from .util import make_backup_dir
 
 
 @callback
@@ -264,7 +263,6 @@ async def backup_agents_download(
     try:
         path = manager.temp_backup_dir / f"{msg["backup_id"]}.tar"
         stream = await agent.async_download_backup(msg["backup_id"])
-        make_backup_dir(manager.temp_backup_dir)
         f = await hass.async_add_executor_job(path.open, "wb")
         try:
             async for chunk in stream:
