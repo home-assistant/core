889528cf6f88ff1f4350f928196d68a1757e2c3c
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2024-12-04 20:28:43 +0100

    Clean up common modules in Husqvarna Automower (#132290)

--- homeassistant/components/husqvarna_automower/__init__.py
+++ homeassistant/components/husqvarna_automower/__init__.py
@@ -67,7 +67,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: AutomowerConfigEntry) ->
         # without the scope. So only polling would be possible.
         raise ConfigEntryAuthFailed
 
-    coordinator = AutomowerDataUpdateCoordinator(hass, automower_api, entry)
+    coordinator = AutomowerDataUpdateCoordinator(hass, automower_api)
     await coordinator.async_config_entry_first_refresh()
     available_devices = list(coordinator.data)
     cleanup_removed_devices(hass, coordinator.config_entry, available_devices)
