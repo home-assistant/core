20a14c461f16cd8d43ada73a01aed07d362f9c34
TJ Horner <me@tjhorner.com>
2024-12-05 21:11:41 +0000

    refactor(weatherkit): refactor coordinator tests into three different tests

--- tests/components/weatherkit/test_coordinator.py
+++ tests/components/weatherkit/test_coordinator.py
@@ -14,11 +14,11 @@ from . import init_integration, mock_weather_response
 from tests.common import async_fire_time_changed
 
 
-async def test_failed_updates(
+async def test_update_uses_stale_data_before_threshold(
     hass: HomeAssistant,
     freezer: FrozenDateTimeFactory,
 ) -> None:
-    """Test that we properly handle failed updates and recover from them."""
+    """Test that stale data from the last successful update is used if an update failure occurs before the threshold."""
     with mock_weather_response():
         await init_integration(hass)
 
@@ -34,7 +34,7 @@ async def test_failed_updates(
         "homeassistant.components.weatherkit.WeatherKitApiClient.get_weather_data",
         side_effect=WeatherKitApiClientError,
     ):
-        freezer.tick(timedelta(minutes=5))
+        freezer.tick(timedelta(minutes=59))
         async_fire_time_changed(hass)
         await hass.async_block_till_done()
 
@@ -42,13 +42,22 @@ async def test_failed_updates(
     assert state
     assert state.state == initial_state
 
+
+async def test_update_becomes_unavailable_after_threshold(
+    hass: HomeAssistant,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test that the entity becomes unavailable if an update failure occurs after the threshold."""
+    with mock_weather_response():
+        await init_integration(hass)
+
     # Expect state to be unavailable after one hour
 
     with patch(
         "homeassistant.components.weatherkit.WeatherKitApiClient.get_weather_data",
         side_effect=WeatherKitApiClientError,
     ):
-        freezer.tick(timedelta(hours=1))
+        freezer.tick(timedelta(hours=1, minutes=5))
         async_fire_time_changed(hass)
         await hass.async_block_till_done()
 
@@ -56,7 +65,26 @@ async def test_failed_updates(
     assert state
     assert state.state == STATE_UNAVAILABLE
 
-    # Expect state to be available if we can recover
+
+async def test_update_recovers_after_failure(
+    hass: HomeAssistant,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test that a successful update after repeated failures recovers the entity's state."""
+    with mock_weather_response():
+        await init_integration(hass)
+
+    # Trigger a failure after threshold
+
+    with patch(
+        "homeassistant.components.weatherkit.WeatherKitApiClient.get_weather_data",
+        side_effect=WeatherKitApiClientError,
+    ):
+        freezer.tick(timedelta(hours=1, minutes=5))
+        async_fire_time_changed(hass)
+        await hass.async_block_till_done()
+
+    # Expect that a successful update recovers the entity
 
     with mock_weather_response():
         freezer.tick(timedelta(minutes=5))
