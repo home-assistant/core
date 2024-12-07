cfe84b8d3580498b4156fac0f7464a970fbe908e
Imou-OpenPlatform <cloud_openteam_service@imou.com>
2024-12-07 10:14:00 +0800

    remove useless code

--- homeassistant/components/imou_life/__init__.py
+++ homeassistant/components/imou_life/__init__.py
@@ -10,7 +10,7 @@ from homeassistant.helpers.device_registry import DeviceEntry
 from .const import PARAM_APP_ID, PARAM_APP_SECRET, PARAM_API_URL, PLATFORMS, DOMAIN
 from .coordinator import ImouDataUpdateCoordinator
 from pyimouapi.ha_device import ImouHaDeviceManager
-from pyimouapi.device import ImouDeviceManager, ImouDevice
+from pyimouapi.device import ImouDeviceManager
 from pyimouapi.openapi import ImouOpenApiClient
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
