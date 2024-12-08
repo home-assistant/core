7810617f0bd7d0b317040014a668dbc55e496e37
Alberto Geniola <albertogeniola@gmail.com>
2024-12-08 01:11:25 +0100

    Make build_ssl_context async friendly.

--- homeassistant/components/elmax/common.py
+++ homeassistant/components/elmax/common.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+import asyncio
+import functools
 import ssl
 
 from elmax_api.model.panel import PanelEntry
@@ -16,12 +18,15 @@ def get_direct_api_url(host: str, port: int, use_ssl: bool) -> str:
     return f"{schema}://{host}:{port}/{ELMAX_LOCAL_API_PATH}"
 
 
-def build_direct_ssl_context(cadata: str) -> ssl.SSLContext:
+async def async_build_direct_ssl_context(cadata: str) -> ssl.SSLContext:
     """Create a custom SSL context for direct-api verification."""
     context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
     context.check_hostname = False
     context.verify_mode = ssl.CERT_REQUIRED
-    context.load_verify_locations(cadata=cadata)
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(
+        None, functools.partial(context.load_verify_locations, cadata=cadata)
+    )
     return context
 
 
