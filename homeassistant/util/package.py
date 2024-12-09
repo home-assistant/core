6f2a89cf499fd49eefcc876a31b7bf5a823c320d
Craig Andrews <candrews@integralblue.com>
2024-12-09 11:21:56 -0500

    Use is_official_image to check if running in a docker container

--- homeassistant/util/package.py
+++ homeassistant/util/package.py
@@ -15,6 +15,8 @@ from urllib.parse import urlparse
 
 from packaging.requirements import InvalidRequirement, Requirement
 
+from homeassistant.helpers.system_info import is_official_image
+
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -33,6 +35,7 @@ def is_docker_env() -> bool:
         Path("/run/.containerenv").exists()
         or Path("/.dockerenv").exists()
         or os.environ.get("container")  # noqa: SIM112
+        or is_official_image()
     )
 
 
