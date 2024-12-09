7f64b3a93f3900efdb73bbeafb3b9e545865420e
Åke S - Piper <ake@strandberg.eu>
2024-12-09 20:47:27 +0000

    Add more tests of reauth flow

--- tests/components/myuplink/test_config_flow.py
+++ tests/components/myuplink/test_config_flow.py
@@ -14,7 +14,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 from homeassistant.helpers import config_entry_oauth2_flow
 
-from .const import CLIENT_ID
+from .const import CLIENT_ID, UNIQUE_ID
 
 from tests.common import MockConfigEntry
 from tests.test_util.aiohttp import AiohttpClientMocker
@@ -76,7 +76,7 @@ async def test_full_flow(
 
 
 @pytest.mark.usefixtures("current_request_with_host")
-async def test_flow_reauth(
+async def test_flow_reauth_bad_scope(
     hass: HomeAssistant,
     hass_client_no_auth: ClientSessionGenerator,
     aioclient_mock: AiohttpClientMocker,
@@ -158,3 +158,161 @@ async def test_flow_reauth(
     assert len(hass.config_entries.async_entries(DOMAIN)) == 1
     assert len(mock_setup.mock_calls) == 1
     assert mock_config_entry.data["token"]["scope"] == CURRENT_SCOPE
+
+
+@pytest.mark.usefixtures("current_request_with_host")
+async def test_flow_reauth_account_mismatch(
+    hass: HomeAssistant,
+    hass_client_no_auth: ClientSessionGenerator,
+    aioclient_mock: AiohttpClientMocker,
+    setup_credentials: None,
+    mock_config_entry: MockConfigEntry,
+    access_token: str,
+    expires_at: float,
+) -> None:
+    """Test reauth step with mismatching account."""
+
+    CURRENT_TOKEN = {
+        "auth_implementation": DOMAIN,
+        "token": {
+            "access_token": access_token,
+            "scope": CURRENT_SCOPE,
+            "expires_in": 86399,
+            "refresh_token": "3012bc9f-7a65-4240-b817-9154ffdcc30f",
+            "token_type": "Bearer",
+            "expires_at": expires_at,
+        },
+    }
+    assert hass.config_entries.async_update_entry(
+        mock_config_entry, data=CURRENT_TOKEN, unique_id="wrong_uid"
+    )
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+
+    result = await mock_config_entry.start_reauth_flow(hass)
+
+    assert result["step_id"] == "reauth_confirm"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={}
+    )
+    assert result["step_id"] == "auth"
+
+    state = config_entry_oauth2_flow._encode_jwt(
+        hass,
+        {
+            "flow_id": result["flow_id"],
+            "redirect_uri": REDIRECT_URL,
+        },
+    )
+    assert result["url"] == (
+        f"{OAUTH2_AUTHORIZE}?response_type=code&client_id={CLIENT_ID}"
+        f"&redirect_uri={REDIRECT_URL}"
+        f"&state={state}"
+        f"&scope={CURRENT_SCOPE.replace(' ', '+')}"
+    )
+
+    client = await hass_client_no_auth()
+    resp = await client.get(f"/auth/external/callback?code=abcd&state={state}")
+    assert resp.status == 200
+    assert resp.headers["content-type"] == "text/html; charset=utf-8"
+
+    aioclient_mock.post(
+        OAUTH2_TOKEN,
+        json={
+            "refresh_token": "updated-refresh-token",
+            "access_token": access_token,
+            "type": "Bearer",
+            "expires_in": "60",
+            "scope": CURRENT_SCOPE,
+        },
+    )
+
+    with patch(
+        f"homeassistant.components.{DOMAIN}.async_setup_entry", return_value=True
+    ):
+        result = await hass.config_entries.flow.async_configure(result["flow_id"])
+        await hass.async_block_till_done()
+
+    assert result.get("type") is FlowResultType.ABORT
+    assert result.get("reason") == "account_mismatch"
+
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+
+
+@pytest.mark.usefixtures("current_request_with_host")
+async def test_flow_reauth(
+    hass: HomeAssistant,
+    hass_client_no_auth: ClientSessionGenerator,
+    aioclient_mock: AiohttpClientMocker,
+    setup_credentials: None,
+    mock_config_entry: MockConfigEntry,
+    access_token: str,
+    expires_at: float,
+) -> None:
+    """Test reauth step with correct input."""
+
+    CURRENT_TOKEN = {
+        "auth_implementation": DOMAIN,
+        "token": {
+            "access_token": access_token,
+            "scope": CURRENT_SCOPE,
+            "expires_in": 86399,
+            "refresh_token": "3012bc9f-7a65-4240-b817-9154ffdcc30f",
+            "token_type": "Bearer",
+            "expires_at": expires_at,
+        },
+    }
+    assert hass.config_entries.async_update_entry(
+        mock_config_entry, data=CURRENT_TOKEN, unique_id=UNIQUE_ID
+    )
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+
+    result = await mock_config_entry.start_reauth_flow(hass)
+
+    assert result["step_id"] == "reauth_confirm"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={}
+    )
+    assert result["step_id"] == "auth"
+
+    state = config_entry_oauth2_flow._encode_jwt(
+        hass,
+        {
+            "flow_id": result["flow_id"],
+            "redirect_uri": REDIRECT_URL,
+        },
+    )
+    assert result["url"] == (
+        f"{OAUTH2_AUTHORIZE}?response_type=code&client_id={CLIENT_ID}"
+        f"&redirect_uri={REDIRECT_URL}"
+        f"&state={state}"
+        f"&scope={CURRENT_SCOPE.replace(' ', '+')}"
+    )
+
+    client = await hass_client_no_auth()
+    resp = await client.get(f"/auth/external/callback?code=abcd&state={state}")
+    assert resp.status == 200
+    assert resp.headers["content-type"] == "text/html; charset=utf-8"
+
+    aioclient_mock.post(
+        OAUTH2_TOKEN,
+        json={
+            "refresh_token": "updated-refresh-token",
+            "access_token": access_token,
+            "type": "Bearer",
+            "expires_in": "60",
+            "scope": CURRENT_SCOPE,
+        },
+    )
+
+    with patch(
+        f"homeassistant.components.{DOMAIN}.async_setup_entry", return_value=True
+    ):
+        result = await hass.config_entries.flow.async_configure(result["flow_id"])
+        await hass.async_block_till_done()
+
+    assert result.get("type") is FlowResultType.ABORT
+    assert result.get("reason") == "reauth_successful"
+
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
