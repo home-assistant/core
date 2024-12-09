45c9aef921fd1e09ab44125db536c5724723b85b
Brett <brett.whynot@gmail.com>
2024-12-09 10:43:55 +0000

    Update tests

--- tests/components/teslemetry/test_sensor.py
+++ tests/components/teslemetry/test_sensor.py
@@ -5,8 +5,10 @@ from unittest.mock import AsyncMock
 from freezegun.api import FrozenDateTimeFactory
 import pytest
 from syrupy.assertion import SnapshotAssertion
+from teslemetry_stream import Signal
 
 from homeassistant.components.teslemetry.coordinator import VEHICLE_INTERVAL
+from homeassistant.components.teslemetry.models import TeslemetryData
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
@@ -24,6 +26,7 @@ async def test_sensors(
     entity_registry: er.EntityRegistry,
     freezer: FrozenDateTimeFactory,
     mock_vehicle_data: AsyncMock,
+    mock_listen: AsyncMock,
 ) -> None:
     """Tests that the sensor entities are correct."""
 
@@ -37,6 +40,23 @@ async def test_sensors(
     mock_vehicle_data.return_value = VEHICLE_DATA_ALT
     freezer.tick(VEHICLE_INTERVAL)
     async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    # Stream update
+    runtime_data: TeslemetryData = entry.runtime_data
+    for listener, _ in runtime_data.vehicles[0].stream._listeners.values():
+        listener(
+            {
+                "vin": VEHICLE_DATA_ALT["response"]["vin"],
+                "data": {
+                    Signal.DETAILED_CHARGE_STATE: "DetailedChargeStateCharging",
+                    Signal.BATTERY_LEVEL: 90,
+                    Signal.AC_CHARGING_ENERGY_IN: 10,
+                    Signal.AC_CHARGING_POWER: 2,
+                },
+                "createdAt": "2024-10-04T10:45:17.537Z",
+            }
+        )
     await hass.async_block_till_done()
 
     assert_entities_alt(hass, entry.entry_id, entity_registry, snapshot)
