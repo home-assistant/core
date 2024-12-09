f7718535dbd3d5c77388ea9d673a92f4f7abf6fd
Erik <erik@montnemery.com>
2024-12-09 10:00:51 +0100

    Add WS command backup/generate_with_stored_settings

--- homeassistant/components/backup/websocket.py
+++ homeassistant/components/backup/websocket.py
@@ -178,6 +178,34 @@ async def handle_create(
     connection.send_result(msg["id"], backup)
 
 
+@websocket_api.require_admin
+@websocket_api.websocket_command(
+    {
+        vol.Required("type"): "backup/generate_with_stored_settings",
+    }
+)
+@websocket_api.async_response
+async def handle_create_with_stored_settings(
+    hass: HomeAssistant,
+    connection: websocket_api.ActiveConnection,
+    msg: dict[str, Any],
+) -> None:
+    """Generate a backup with stored settings."""
+
+    config_data = hass.data[DATA_MANAGER].config.data
+    backup = await hass.data[DATA_MANAGER].async_initiate_backup(
+        agent_ids=config_data.create_backup.agent_ids,
+        include_addons=config_data.create_backup.include_addons,
+        include_all_addons=config_data.create_backup.include_all_addons,
+        include_database=config_data.create_backup.include_database,
+        include_folders=config_data.create_backup.include_folders,
+        include_homeassistant=True,  # always include HA
+        name=config_data.create_backup.name,
+        password=config_data.create_backup.password,
+    )
+    connection.send_result(msg["id"], backup)
+
+
 @websocket_api.ws_require_user(only_supervisor=True)
 @websocket_api.websocket_command({vol.Required("type"): "backup/start"})
 @websocket_api.async_response
