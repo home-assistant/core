caa6f11801dbaf398b3d98c149e2a7efb3ff2e93
Noah Husby <32528627+noahhusby@users.noreply.github.com>
2024-12-09 14:39:09 -0500

    Add reconfigure flow to Cambridge Audio (#131091)
    * Add reconfigure flow to Cambridge Audio
    
    * Update
    
    * Add reconfigure flow to Cambridge Audio
    
    * Fix
    
    * Add helper method to reconfigure tests
    
    * Update quality scale

--- tests/components/cambridge_audio/test_config_flow.py
+++ tests/components/cambridge_audio/test_config_flow.py
@@ -7,7 +7,7 @@ from aiostreammagic import StreamMagicError
 
 from homeassistant.components.cambridge_audio.const import DOMAIN
 from homeassistant.components.zeroconf import ZeroconfServiceInfo
-from homeassistant.config_entries import SOURCE_USER, SOURCE_ZEROCONF
+from homeassistant.config_entries import SOURCE_USER, SOURCE_ZEROCONF, ConfigFlowResult
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
@@ -192,3 +192,55 @@ async def test_zeroconf_duplicate(
     )
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "already_configured"
+
+
+async def _start_reconfigure_flow(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry
+) -> ConfigFlowResult:
+    """Initialize a reconfigure flow."""
+    mock_config_entry.add_to_hass(hass)
+
+    reconfigure_result = await mock_config_entry.start_reconfigure_flow(hass)
+
+    assert reconfigure_result["type"] is FlowResultType.FORM
+    assert reconfigure_result["step_id"] == "reconfigure"
+
+    return await hass.config_entries.flow.async_configure(
+        reconfigure_result["flow_id"],
+        {CONF_HOST: "192.168.20.219"},
+    )
+
+
+async def test_reconfigure_flow(
+    hass: HomeAssistant,
+    mock_stream_magic_client: AsyncMock,
+    mock_setup_entry: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test reconfigure flow."""
+
+    result = await _start_reconfigure_flow(hass, mock_config_entry)
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reconfigure_successful"
+
+    entry = hass.config_entries.async_get_entry(mock_config_entry.entry_id)
+    assert entry
+    assert entry.data == {
+        CONF_HOST: "192.168.20.219",
+    }
+
+
+async def test_reconfigure_unique_id_mismatch(
+    hass: HomeAssistant,
+    mock_stream_magic_client: AsyncMock,
+    mock_setup_entry: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Ensure reconfigure flow aborts when the bride changes."""
+    mock_stream_magic_client.info.unit_id = "different_udn"
+
+    result = await _start_reconfigure_flow(hass, mock_config_entry)
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "wrong_device"
