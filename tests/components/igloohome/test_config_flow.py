b894682c9f65a7fe8ea687b089e672d223088f2f
keith <keithle888@gmail.com>
2024-12-10 04:39:22 +0000

    - Replaced repeated form input with constant

--- tests/components/igloohome/test_config_flow.py
+++ tests/components/igloohome/test_config_flow.py
@@ -12,6 +12,11 @@ from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
+FORM_USER_INPUT = {
+    CONF_CLIENT_ID: "test-client-id",
+    CONF_CLIENT_SECRET: "test-client-secret",
+}
+
 
 async def test_form(
     hass: HomeAssistant, mock_setup_entry: AsyncMock, auth_successful
@@ -26,19 +31,13 @@ async def test_form(
     with auth_successful:
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            {
-                CONF_CLIENT_ID: "test-client-id",
-                CONF_CLIENT_SECRET: "test-client-secret",
-            },
+            FORM_USER_INPUT,
         )
         await hass.async_block_till_done()
 
     assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["title"] == "Client Credentials"
-    assert result["data"] == {
-        CONF_CLIENT_ID: "test-client-id",
-        CONF_CLIENT_SECRET: "test-client-secret",
-    }
+    assert result["data"] == FORM_USER_INPUT
     assert len(mock_setup_entry.mock_calls) == 1
 
 
@@ -60,10 +59,7 @@ async def test_form_invalid_input(
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            {
-                CONF_CLIENT_ID: "test-client-id",
-                CONF_CLIENT_SECRET: "test-client-secret",
-            },
+            FORM_USER_INPUT,
         )
 
     assert result["type"] == FlowResultType.FORM
@@ -78,17 +74,11 @@ async def test_form_invalid_input(
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
-            {
-                CONF_CLIENT_ID: "test-client-id",
-                CONF_CLIENT_SECRET: "test-client-secret",
-            },
+            FORM_USER_INPUT,
         )
         await hass.async_block_till_done()
 
     assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["title"] == "Client Credentials"
-    assert result["data"] == {
-        CONF_CLIENT_ID: "test-client-id",
-        CONF_CLIENT_SECRET: "test-client-secret",
-    }
+    assert result["data"] == FORM_USER_INPUT
     assert len(mock_setup_entry.mock_calls) == 1
