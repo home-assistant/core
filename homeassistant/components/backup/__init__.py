070b79e9f222784d143fc750f4ad178eb55e5398
Erik <erik@montnemery.com>
2024-12-11 15:38:58 +0100

    Revert changes to action service backup.create

--- homeassistant/components/backup/__init__.py
+++ homeassistant/components/backup/__init__.py
@@ -1,8 +1,5 @@
 """The Backup integration."""
 
-import voluptuous as vol
-
-from homeassistant.const import CONF_PASSWORD
 from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.hassio import is_hassio
@@ -47,8 +44,6 @@ __all__ = [
 
 CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
 
-SERVICE_CREATE_SCHEMA = vol.Schema({vol.Optional(CONF_PASSWORD): str})
-
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Backup integration."""
@@ -80,16 +75,11 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             include_folders=None,
             include_homeassistant=True,
             name=None,
-            password=call.data.get(CONF_PASSWORD),
+            password=None,
         )
 
     if not with_hassio:
-        hass.services.async_register(
-            DOMAIN,
-            "create",
-            async_handle_create_service,
-            schema=SERVICE_CREATE_SCHEMA,
-        )
+        hass.services.async_register(DOMAIN, "create", async_handle_create_service)
 
     async_register_http_views(hass)
 
