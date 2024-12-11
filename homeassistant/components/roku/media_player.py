0b6dcef12323e2ae3a78f19b6d4d4383f29cb314
Chris Talkington <chris@talkingtontech.com>
2024-12-10 19:17:57 -0600

    Update media_player.py

--- homeassistant/components/roku/media_player.py
+++ homeassistant/components/roku/media_player.py
@@ -47,7 +47,6 @@ from .helpers import format_channel_name, roku_exception_handler
 
 _LOGGER = logging.getLogger(__name__)
 
-
 STREAM_FORMAT_TO_MEDIA_TYPE = {
     "dash": MediaType.VIDEO,
     "hls": MediaType.VIDEO,
@@ -80,6 +79,9 @@ ATTRS_TO_PLAY_ON_ROKU_AUDIO_PARAMS = {
 }
 
 SEARCH_SCHEMA: VolDictType = {vol.Required(ATTR_KEYWORD): str}
+
+# Coordinator is used to centralize the data updates
+PARALLEL_UPDATES = 1
 
 
 async def async_setup_entry(
