7a806004892feca8f9fe7e9389793b8814db820f
epenet <6771947+epenet@users.noreply.github.com>
2024-12-12 12:34:16 +0000

    Cleanup alarm_control_panel

--- tests/components/alarm_control_panel/common.py
+++ tests/components/alarm_control_panel/common.py
@@ -1,125 +1,12 @@
-"""Collection of helper methods.
-
-All containing methods are legacy helpers that should not be used by new
-components. Instead call the service directly.
-"""
+"""Collection of helpers."""
 
 from homeassistant.components.alarm_control_panel import (
-    DOMAIN,
     AlarmControlPanelEntity,
     AlarmControlPanelEntityFeature,
     AlarmControlPanelState,
 )
-from homeassistant.const import (
-    ATTR_CODE,
-    ATTR_ENTITY_ID,
-    ENTITY_MATCH_ALL,
-    SERVICE_ALARM_ARM_AWAY,
-    SERVICE_ALARM_ARM_CUSTOM_BYPASS,
-    SERVICE_ALARM_ARM_HOME,
-    SERVICE_ALARM_ARM_NIGHT,
-    SERVICE_ALARM_ARM_VACATION,
-    SERVICE_ALARM_DISARM,
-    SERVICE_ALARM_TRIGGER,
-)
-from homeassistant.core import HomeAssistant
 
 from tests.common import MockEntity
-
-
-async def async_alarm_disarm(
-    hass: HomeAssistant, code: str | None = None, entity_id: str = ENTITY_MATCH_ALL
-) -> None:
-    """Send the alarm the command for disarm."""
-    data = {}
-    if code:
-        data[ATTR_CODE] = code
-    if entity_id:
-        data[ATTR_ENTITY_ID] = entity_id
-
-    await hass.services.async_call(DOMAIN, SERVICE_ALARM_DISARM, data, blocking=True)
-
-
-async def async_alarm_arm_home(
-    hass: HomeAssistant, code: str | None = None, entity_id: str = ENTITY_MATCH_ALL
-) -> None:
-    """Send the alarm the command for disarm."""
-    data = {}
-    if code:
-        data[ATTR_CODE] = code
-    if entity_id:
-        data[ATTR_ENTITY_ID] = entity_id
-
-    await hass.services.async_call(DOMAIN, SERVICE_ALARM_ARM_HOME, data, blocking=True)
-
-
-async def async_alarm_arm_away(
-    hass: HomeAssistant, code: str | None = None, entity_id: str = ENTITY_MATCH_ALL
-) -> None:
-    """Send the alarm the command for disarm."""
-    data = {}
-    if code:
-        data[ATTR_CODE] = code
-    if entity_id:
-        data[ATTR_ENTITY_ID] = entity_id
-
-    await hass.services.async_call(DOMAIN, SERVICE_ALARM_ARM_AWAY, data, blocking=True)
-
-
-async def async_alarm_arm_night(
-    hass: HomeAssistant, code: str | None = None, entity_id: str = ENTITY_MATCH_ALL
-) -> None:
-    """Send the alarm the command for disarm."""
-    data = {}
-    if code:
-        data[ATTR_CODE] = code
-    if entity_id:
-        data[ATTR_ENTITY_ID] = entity_id
-
-    await hass.services.async_call(DOMAIN, SERVICE_ALARM_ARM_NIGHT, data, blocking=True)
-
-
-async def async_alarm_arm_vacation(
-    hass: HomeAssistant, code: str | None = None, entity_id: str = ENTITY_MATCH_ALL
-) -> None:
-    """Send the alarm the command for vacation mode."""
-    data = {}
-    if code:
-        data[ATTR_CODE] = code
-    if entity_id:
-        data[ATTR_ENTITY_ID] = entity_id
-
-    await hass.services.async_call(
-        DOMAIN, SERVICE_ALARM_ARM_VACATION, data, blocking=True
-    )
-
-
-async def async_alarm_trigger(
-    hass: HomeAssistant, code: str | None = None, entity_id: str = ENTITY_MATCH_ALL
-) -> None:
-    """Send the alarm the command for disarm."""
-    data = {}
-    if code:
-        data[ATTR_CODE] = code
-    if entity_id:
-        data[ATTR_ENTITY_ID] = entity_id
-
-    await hass.services.async_call(DOMAIN, SERVICE_ALARM_TRIGGER, data, blocking=True)
-
-
-async def async_alarm_arm_custom_bypass(
-    hass: HomeAssistant, code: str | None = None, entity_id: str = ENTITY_MATCH_ALL
-) -> None:
-    """Send the alarm the command for disarm."""
-    data = {}
-    if code:
-        data[ATTR_CODE] = code
-    if entity_id:
-        data[ATTR_ENTITY_ID] = entity_id
-
-    await hass.services.async_call(
-        DOMAIN, SERVICE_ALARM_ARM_CUSTOM_BYPASS, data, blocking=True
-    )
 
 
 class MockAlarm(MockEntity, AlarmControlPanelEntity):
