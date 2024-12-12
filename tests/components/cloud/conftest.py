ed279e602557d24f52172559f9b75d2b1dba5961
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2024-12-12 15:46:11 +0200

    Remove unused change

--- tests/components/cloud/conftest.py
+++ tests/components/cloud/conftest.py
@@ -143,9 +143,7 @@ async def cloud_fixture() -> AsyncGenerator[MagicMock]:
 
         # Methods that we mock with a custom side effect.
 
-        async def mock_login(
-            email: str, password: str, code: str | None = None
-        ) -> None:
+        async def mock_login(email: str, password: str) -> None:
             """Mock login.
 
             When called, it should call the on_start callback.
