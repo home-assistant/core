5754e117126f00f5b7cec88101f81226ec1af691
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2024-12-15 13:07:10 +0100

    Improve MQTT json color_temp validation (#133174)
    * Improve MQTT json color_temp validation
    
    * Revert unrelated changes and assert on logs
    
    * Typo

--- tests/components/mqtt/test_light_json.py
+++ tests/components/mqtt/test_light_json.py
@@ -2185,7 +2185,9 @@ async def test_white_scale(
     ],
 )
 async def test_invalid_values(
-    hass: HomeAssistant, mqtt_mock_entry: MqttMockHAClientGenerator
+    hass: HomeAssistant,
+    mqtt_mock_entry: MqttMockHAClientGenerator,
+    caplog: pytest.LogCaptureFixture,
 ) -> None:
     """Test that invalid color/brightness/etc. values are ignored."""
     await mqtt_mock_entry()
@@ -2286,6 +2288,10 @@ async def test_invalid_values(
     # Bad color temperature
     async_fire_mqtt_message(
         hass, "test_light_rgb", '{"state":"ON", "color_temp": "badValue"}'
+    )
+    assert (
+        "Invalid color temp value 'badValue' received for entity light.test"
+        in caplog.text
     )
 
     # Color temperature should not have changed
