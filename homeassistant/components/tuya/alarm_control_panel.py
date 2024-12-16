95a9f30fedff60fca5f419f41550c55660cc4430
Asaf Has <asafhas@yahoo.com>
2024-12-16 22:58:50 +0200

    Checking for DP-Codes on init

--- homeassistant/components/tuya/alarm_control_panel.py
+++ homeassistant/components/tuya/alarm_control_panel.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from base64 import b64decode
+from dataclasses import dataclass
 from enum import StrEnum
 
 from tuya_sharing import CustomerDevice, Manager
@@ -19,8 +20,15 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import TuyaConfigEntry
 from .const import TUYA_DISCOVERY_NEW, DPCode, DPType
-from .entity import TuyaEntity
+from .entity import EnumTypeData, TuyaEntity
 
+
+@dataclass(frozen=True)
+class TuyaAlarmControlPanelEntityDescription(AlarmControlPanelEntityDescription):
+    """Describe a Tuya Alarm Control Panel entity."""
+
+    master_state: DPCode | None = None
+    alarm_msg: DPCode | None = None
 
 class Mode(StrEnum):
     """Alarm modes."""
@@ -46,12 +54,14 @@ STATE_MAPPING: dict[str, AlarmControlPanelState] = {
 
 # All descriptions can be found here:
 # https://developer.tuya.com/en/docs/iot/standarddescription?id=K9i5ql6waswzq
-ALARM: dict[str, tuple[AlarmControlPanelEntityDescription, ...]] = {
+ALARM: dict[str, tuple[TuyaAlarmControlPanelEntityDescription, ...]] = {
     # Alarm Host
     # https://developer.tuya.com/en/docs/iot/categorymal?id=Kaiuz33clqxaf
     "mal": (
-        AlarmControlPanelEntityDescription(
+        TuyaAlarmControlPanelEntityDescription(
             key=DPCode.MASTER_MODE,
+            master_state=DPCode.MASTER_STATE,
+            alarm_msg=DPCode.ALARM_MSG,
             name="Alarm",
         ),
     )
@@ -90,12 +100,14 @@ class TuyaAlarmEntity(TuyaEntity, AlarmControlPanelEntity):
 
     _attr_name = None
     _attr_code_arm_required = False
+    _master_state: EnumTypeData | None = None
+    _alarm_msg_dpcode: DPCode  | None = None
 
     def __init__(
         self,
         device: CustomerDevice,
         device_manager: Manager,
-        description: AlarmControlPanelEntityDescription,
+        description: TuyaAlarmControlPanelEntityDescription,
     ) -> None:
         """Init Tuya Alarm."""
         super().__init__(device, device_manager)
@@ -115,25 +127,39 @@ class TuyaAlarmEntity(TuyaEntity, AlarmControlPanelEntity):
             if Mode.SOS in supported_modes.range:
                 self._attr_supported_features |= AlarmControlPanelEntityFeature.TRIGGER
 
+        # Determine master state
+        if enum_type := self.find_dpcode(
+                description.master_state, dptype=DPType.ENUM, prefer_function=True
+            ):
+                self._master_state = enum_type
+
+        # Determine alarm message
+        if dp_code := self.find_dpcode(
+                description.alarm_msg, dptype=DPType.RAW, prefer_function=True
+            ):
+                self._alarm_msg_dpcode = dp_code
+
     @property
     def alarm_state(self) -> AlarmControlPanelState | None:
         """Return the state of the device."""
-        mode = self.device.status.get(DPCode.MASTER_MODE)
-        state = self.device.status.get(DPCode.MASTER_STATE)
         # When the alarm is triggered, only its 'state' is changing. From 'normal' to 'alarm'.
         # The 'mode' doesn't change, and stays as 'arm' or 'home'.
-        if state == State.ALARM:
-            return AlarmControlPanelState.TRIGGERED
-        return STATE_MAPPING.get(mode)
+        if self._master_state is not None:
+            if self.device.status.get(self._master_state.dpcode) == State.ALARM:
+                return AlarmControlPanelState.TRIGGERED
+
+        if not (status := self.device.status.get(self.entity_description.key)):
+            return None
+        return STATE_MAPPING.get(status)
 
     @property
     def changed_by(self) -> str | None:
         """Last change triggered by."""
-        state = self.device.status.get(DPCode.MASTER_STATE)
-        if state == State.ALARM:
-            encoded_msg = self.device.status.get(DPCode.ALARM_MSG)
-            if encoded_msg:
-                return b64decode(encoded_msg).decode('utf-16be')
+        if self._master_state is not None and self._alarm_msg_dpcode is not None:
+            if self.device.status.get(self._master_state.dpcode) == State.ALARM:
+                encoded_msg = self.device.status.get(self._alarm_msg_dpcode)
+                if encoded_msg:
+                    return b64decode(encoded_msg).decode('utf-16be')
         return None
 
     def alarm_disarm(self, code: str | None = None) -> None:
