d243c9193fc3bc469c6ab151fc54b129d81d9195
Daniel Raper <me@danr.uk>
2024-12-16 16:31:33 +0000

    Add common _validate_account method for reauth and user config flow steps

--- homeassistant/components/ohme/config_flow.py
+++ homeassistant/components/ohme/config_flow.py
@@ -6,7 +6,7 @@ from typing import Any
 from ohme import ApiException, AuthException, OhmeApiClient
 import voluptuous as vol
 
-from homeassistant.config_entries import SOURCE_REAUTH, ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 from homeassistant.helpers.selector import (
     TextSelector,
@@ -33,6 +33,17 @@ USER_SCHEMA = vol.Schema(
     }
 )
 
+REAUTH_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_PASSWORD): TextSelector(
+            TextSelectorConfig(
+                type=TextSelectorType.PASSWORD,
+                autocomplete="current-password",
+            ),
+        ),
+    }
+)
+
 
 class OhmeConfigFlow(ConfigFlow, domain=DOMAIN):
     """Config flow."""
@@ -45,22 +56,12 @@ class OhmeConfigFlow(ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
 
         if user_input is not None:
-            if self.source != SOURCE_REAUTH:
-                self._async_abort_entries_match({CONF_EMAIL: user_input[CONF_EMAIL]})
+            self._async_abort_entries_match({CONF_EMAIL: user_input[CONF_EMAIL]})
 
-            instance = OhmeApiClient(user_input[CONF_EMAIL], user_input[CONF_PASSWORD])
-            try:
-                await instance.async_login()
-            except AuthException:
-                errors["base"] = "invalid_auth"
-            except ApiException:
-                errors["base"] = "unknown"
-            else:
-                if self.source == SOURCE_REAUTH:
-                    return self.async_update_reload_and_abort(
-                        self._get_reauth_entry(),
-                        data_updates=user_input,
-                    )
+            errors = await self._validate_account(
+                user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
+            )
+            if not errors:
                 return self.async_create_entry(
                     title=user_input[CONF_EMAIL], data=user_input
                 )
@@ -73,4 +74,43 @@ class OhmeConfigFlow(ConfigFlow, domain=DOMAIN):
         self, entry_data: Mapping[str, Any]
     ) -> ConfigFlowResult:
         """Handle re-authentication."""
-        return await self.async_step_user()
+        return await self.async_step_reauth_confirm()
+
+    async def async_step_reauth_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle re-authentication confirmation."""
+        errors: dict[str, str] = {}
+        reauth_entry = self._get_reauth_entry()
+        if user_input is not None:
+            errors = await self._validate_account(
+                reauth_entry.data[CONF_EMAIL],
+                user_input[CONF_PASSWORD],
+            )
+            if not errors:
+                return self.async_update_reload_and_abort(
+                    reauth_entry,
+                    data_updates=user_input,
+                )
+        return self.async_show_form(
+            step_id="reauth_confirm",
+            data_schema=REAUTH_SCHEMA,
+            description_placeholders={"email": reauth_entry.data[CONF_EMAIL]},
+            errors=errors,
+        )
+
+    async def _validate_account(self, email: str, password: str) -> dict[str, str]:
+        """Validate Ohme account and return dict of errors."""
+        errors: dict[str, str] = {}
+        client = OhmeApiClient(
+            email,
+            password,
+        )
+        try:
+            await client.async_login()
+        except AuthException:
+            errors["base"] = "invalid_auth"
+        except ApiException:
+            errors["base"] = "unknown"
+
+        return errors
