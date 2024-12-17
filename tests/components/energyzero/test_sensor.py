ad2121262ba330add5fb1f55efb5c06a3c1cbde2
Klaas Schoute <klaas_schoute@hotmail.com>
2024-12-17 22:08:57 +0100

    Use snapshot assertion for sensors and diagnostics

--- tests/components/energyzero/test_sensor.py
+++ tests/components/energyzero/test_sensor.py
@@ -1,81 +1,36 @@
 """Tests for the sensors provided by the EnergyZero integration."""
 
-from unittest.mock import MagicMock
+from unittest.mock import AsyncMock, MagicMock, patch
 
 from energyzero import EnergyZeroNoDataError
 import pytest
 from syrupy.assertion import SnapshotAssertion
-from syrupy.filters import props
 
-from homeassistant.components.energyzero.const import DOMAIN
 from homeassistant.components.homeassistant import SERVICE_UPDATE_ENTITY
 from homeassistant.const import ATTR_ENTITY_ID, STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import entity_registry as er
 from homeassistant.setup import async_setup_component
 
-from tests.common import MockConfigEntry
+from . import setup_integration
+
+from tests.common import MockConfigEntry, snapshot_platform
 
 pytestmark = [pytest.mark.freeze_time("2022-12-07 15:00:00")]
 
 
-@pytest.mark.parametrize(
-    ("entity_id", "entity_unique_id", "device_identifier"),
-    [
-        (
-            "sensor.energyzero_today_energy_current_hour_price",
-            "today_energy_current_hour_price",
-            "today_energy",
-        ),
-        (
-            "sensor.energyzero_today_energy_average_price",
-            "today_energy_average_price",
-            "today_energy",
-        ),
-        (
-            "sensor.energyzero_today_energy_max_price",
-            "today_energy_max_price",
-            "today_energy",
-        ),
-        (
-            "sensor.energyzero_today_energy_highest_price_time",
-            "today_energy_highest_price_time",
-            "today_energy",
-        ),
-        (
-            "sensor.energyzero_today_energy_hours_priced_equal_or_lower",
-            "today_energy_hours_priced_equal_or_lower",
-            "today_energy",
-        ),
-        (
-            "sensor.energyzero_today_gas_current_hour_price",
-            "today_gas_current_hour_price",
-            "today_gas",
-        ),
-    ],
-)
 async def test_sensor(
     hass: HomeAssistant,
-    init_integration: MockConfigEntry,
-    device_registry: dr.DeviceRegistry,
+    mock_energyzero: AsyncMock,
+    mock_config_entry: MockConfigEntry,
     entity_registry: er.EntityRegistry,
     snapshot: SnapshotAssertion,
-    entity_id: str,
-    entity_unique_id: str,
-    device_identifier: str,
 ) -> None:
     """Test the EnergyZero - Energy sensors."""
-    entry_id = init_integration.entry_id
-    assert (state := hass.states.get(entity_id))
-    assert state == snapshot
-    assert (entity_entry := entity_registry.async_get(entity_id))
-    assert entity_entry == snapshot(exclude=props("unique_id"))
-    assert entity_entry.unique_id == f"{entry_id}_{entity_unique_id}"
+    with patch("homeassistant.components.energyzero.PLATFORMS", ["sensor"]):
+        await setup_integration(hass, mock_config_entry)
 
-    assert entity_entry.device_id
-    assert (device_entry := device_registry.async_get(entity_entry.device_id))
-    assert device_entry == snapshot(exclude=props("identifiers"))
-    assert device_entry.identifiers == {(DOMAIN, f"{entry_id}_{device_identifier}")}
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
 
 
 @pytest.mark.usefixtures("init_integration")
