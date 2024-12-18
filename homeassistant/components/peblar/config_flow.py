c3f35161a375d0c5a7aa3f9d829d0eb2ad5cc6b0
Franck Nijhof <git@frenck.dev>
2024-12-18 16:52:15 +0000

    Add Peblar Rocksolid EV Chargers integration

---
+++ homeassistant/components/peblar/config_flow.py
@@ -0,0 +1,71 @@
+"""Config flow to configure the Peblar integration."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from aiohttp import CookieJar
+from peblar import Peblar, PeblarAuthenticationError, PeblarConnectionError
+import voluptuous as vol
+
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.const import CONF_HOST, CONF_PASSWORD
+from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from homeassistant.helpers.selector import (
+    TextSelector,
+    TextSelectorConfig,
+    TextSelectorType,
+)
+
+from .const import DOMAIN, LOGGER
+
+
+class PeblarFlowHandler(ConfigFlow, domain=DOMAIN):
+    """Handle a Peblar config flow."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle a flow initiated by the user."""
+        errors = {}
+
+        if user_input is not None:
+            try:
+                peblar = Peblar(
+                    host=user_input[CONF_HOST],
+                    session=async_create_clientsession(
+                        self.hass, cookie_jar=CookieJar(unsafe=True)
+                    ),
+                )
+                await peblar.login(password=user_input[CONF_PASSWORD])
+                info = await peblar.system_information()
+            except PeblarAuthenticationError:
+                errors[CONF_PASSWORD] = "invalid_auth"
+            except PeblarConnectionError:
+                errors[CONF_HOST] = "cannot_connect"
+            except Exception:  # noqa: BLE001
+                LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                await self.async_set_unique_id(info.product_serial_number)
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(title="Peblar", data=user_input)
+        else:
+            user_input = {}
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_HOST, default=user_input.get(CONF_HOST)
+                    ): TextSelector(TextSelectorConfig(autocomplete="off")),
+                    vol.Required(CONF_PASSWORD): TextSelector(
+                        TextSelectorConfig(type=TextSelectorType.PASSWORD)
+                    ),
+                }
+            ),
+            errors=errors,
+        )
---
+++ homeassistant/components/peblar/sensor.py
@@ -0,0 +1,74 @@
+"""Support for Peblar sensors."""
+
+from __future__ import annotations
+
+from collections.abc import Callable
+from dataclasses import dataclass
+
+from peblar import PeblarMeter
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+)
+from homeassistant.const import UnitOfEnergy
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .coordinator import PeblarConfigEntry
+from .entity import PeblarEntity
+
+
+@dataclass(frozen=True, kw_only=True)
+class PeblarSensorDescription(SensorEntityDescription):
+    """Describe an Peblar sensor."""
+
+    value_fn: Callable[[PeblarMeter], int | None]
+
+
+SENSORS: tuple[PeblarSensorDescription, ...] = (
+    PeblarSensorDescription(
+        key="energy_total",
+        device_class=SensorDeviceClass.ENERGY,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        suggested_display_precision=2,
+        suggested_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
+        value_fn=lambda x: x.energy_total,
+    ),
+)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: PeblarConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Peblar sensors based on a config entry."""
+    async_add_entities(
+        PeblarSensorEntity(entry, description) for description in SENSORS
+    )
+
+
+class PeblarSensorEntity(PeblarEntity, SensorEntity):
+    """Defines a Peblar sensor."""
+
+    entity_description: PeblarSensorDescription
+
+    def __init__(
+        self,
+        entry: PeblarConfigEntry,
+        description: PeblarSensorDescription,
+    ) -> None:
+        """Initialize the Peblar entity."""
+        super().__init__(entry)
+        self.entity_description = description
+        self._attr_unique_id = f"{DOMAIN}_{entry.unique_id}_{description.key}"
+
+    @property
+    def native_value(self) -> int | None:
+        """Return the state of the sensor."""
+        return self.entity_description.value_fn(self.coordinator.data)
