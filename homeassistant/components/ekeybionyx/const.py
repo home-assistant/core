c07252c312db663381fe2eba082d080c796144d2
Richard Polzer <richard@polzer-eb.de>
2024-12-18 08:40:48 +0000

    cleanup

--- homeassistant/components/ekeybionyx/const.py
+++ homeassistant/components/ekeybionyx/const.py
@@ -8,4 +8,5 @@ LOGGER = logging.getLogger(__package__)
 
 OAUTH2_AUTHORIZE = "https://ekeybionyxprod.b2clogin.com/ekeybionyxprod.onmicrosoft.com/B2C_1_sign_in_v2/oauth2/v2.0/authorize"
 OAUTH2_TOKEN = "https://ekeybionyxprod.b2clogin.com/ekeybionyxprod.onmicrosoft.com/B2C_1_sign_in_v2/oauth2/v2.0/token"
+API_URL = "https://api.bionyx.io/3rd-party/api"
 SCOPE = "https://ekeybionyxprod.onmicrosoft.com/3rd-party-api/api-access"
