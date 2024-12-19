1cb1931a8b5a6a85d16a4180509adc4343d1c027
J. Nick Koston <nick@koston.org>
2024-12-18 23:03:12 -1000

    reverts

--- tests/components/recorder/test_statistics.py
+++ tests/components/recorder/test_statistics.py
@@ -1922,10 +1922,7 @@ def test_cache_key_for_generate_statistics_at_time_stmt() -> None:
     cache_key_2 = stmt2._generate_cache_key()
     assert cache_key_1 == cache_key_2
     stmt3 = _generate_statistics_at_time_stmt(
-        StatisticsShortTerm,
-        {0},
-        0.0,
-        {"sum", "mean"},
+        StatisticsShortTerm, {0}, 0.0, {"sum", "mean"}
     )
     cache_key_3 = stmt3._generate_cache_key()
     assert cache_key_1 != cache_key_3
