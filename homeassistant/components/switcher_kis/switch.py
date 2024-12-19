65f06de8905c018fd0f29cbc50e92811126276fc
Shay Levy <levyshay1@gmail.com>
2024-12-19 18:07:48 +0200

    Update homeassistant/components/switcher_kis/switch.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/switcher_kis/switch.py
+++ homeassistant/components/switcher_kis/switch.py
@@ -193,7 +193,7 @@ class SwitchereShutterChildLockBaseSwitchEntity(SwitcherEntity, SwitchEntity):
     def _handle_coordinator_update(self) -> None:
         """When device updates, clear control result that overrides state."""
         self.control_result = None
-        self.async_write_ha_state()
+        super._handle_coordinator_update()
 
     @property
     def is_on(self) -> bool:
