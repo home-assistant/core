6b0fc9612d5cfbedf7e9608ac30895625d5bb927
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-12-19 19:42:50 +0100

    review feedback

--- homeassistant/components/music_assistant/services.py
+++ homeassistant/components/music_assistant/services.py
@@ -15,7 +15,7 @@ from homeassistant.core import (
     SupportsResponse,
     callback,
 )
-from homeassistant.exceptions import HomeAssistantError, ServiceValidationError
+from homeassistant.exceptions import ServiceValidationError
 import homeassistant.helpers.config_validation as cv
 
 from .const import (
@@ -74,57 +74,6 @@ def get_music_assistant_client(
 @callback
 def register_actions(hass: HomeAssistant) -> None:
     """Register custom actions."""
-    register_search_action(hass)
-    register_get_library_action(hass)
-
-
-def register_search_action(hass: HomeAssistant) -> None:
-    """Register search action."""
-
-    async def handle_search(call: ServiceCall) -> ServiceResponse:
-        """Handle queue_command action."""
-        mass = get_music_assistant_client(hass, call.data[ATTR_CONFIG_ENTRY_ID])
-        search_name = call.data[ATTR_SEARCH_NAME]
-        search_artist = call.data.get(ATTR_SEARCH_ARTIST)
-        search_album = call.data.get(ATTR_SEARCH_ALBUM)
-        if search_album and search_artist:
-            search_name = f"{search_artist} - {search_album} - {search_name}"
-        elif search_album:
-            search_name = f"{search_album} - {search_name}"
-        elif search_artist:
-            search_name = f"{search_artist} - {search_name}"
-        search_results = await mass.music.search(
-            search_query=search_name,
-            media_types=call.data.get(ATTR_MEDIA_TYPE, MediaType.ALL),
-            limit=call.data[ATTR_LIMIT],
-            library_only=call.data[ATTR_LIBRARY_ONLY],
-        )
-        response: ServiceResponse = SEARCH_RESULT_SCHEMA(
-            {
-                ATTR_ARTISTS: [
-                    media_item_dict_from_mass_item(mass, item)
-                    for item in search_results.artists
-                ],
-                ATTR_ALBUMS: [
-                    media_item_dict_from_mass_item(mass, item)
-                    for item in search_results.albums
-                ],
-                ATTR_TRACKS: [
-                    media_item_dict_from_mass_item(mass, item)
-                    for item in search_results.tracks
-                ],
-                ATTR_PLAYLISTS: [
-                    media_item_dict_from_mass_item(mass, item)
-                    for item in search_results.playlists
-                ],
-                ATTR_RADIO: [
-                    media_item_dict_from_mass_item(mass, item)
-                    for item in search_results.radio
-                ],
-            }
-        )
-        return response
-
     hass.services.async_register(
         DOMAIN,
         SERVICE_SEARCH,
@@ -144,64 +93,6 @@ def register_search_action(hass: HomeAssistant) -> None:
         ),
         supports_response=SupportsResponse.ONLY,
     )
-
-
-def register_get_library_action(hass: HomeAssistant) -> None:
-    """Register get_library action."""
-
-    async def handle_get_library(call: ServiceCall) -> ServiceResponse:
-        """Handle get_library action."""
-        mass = get_music_assistant_client(hass, call.data[ATTR_CONFIG_ENTRY_ID])
-        media_type = call.data[ATTR_MEDIA_TYPE]
-        limit = call.data.get(ATTR_LIMIT, DEFAULT_LIMIT)
-        offset = call.data.get(ATTR_OFFSET, DEFAULT_OFFSET)
-        order_by = call.data.get(ATTR_ORDER_BY, DEFAULT_SORT_ORDER)
-        base_params = {
-            "favorite": call.data.get(ATTR_FAVORITE),
-            "search": call.data.get(ATTR_SEARCH),
-            "limit": limit,
-            "offset": offset,
-            "order_by": order_by,
-        }
-        if media_type == MediaType.ALBUM:
-            library_result = await mass.music.get_library_albums(
-                **base_params,
-                album_types=call.data.get(ATTR_ALBUM_TYPE),
-            )
-        elif media_type == MediaType.ARTIST:
-            library_result = await mass.music.get_library_artists(
-                **base_params,
-                album_artists_only=call.data.get(ATTR_ALBUM_ARTISTS_ONLY),
-            )
-        elif media_type == MediaType.TRACK:
-            library_result = await mass.music.get_library_tracks(
-                **base_params,
-            )
-        elif media_type == MediaType.RADIO:
-            library_result = await mass.music.get_library_radios(
-                **base_params,
-            )
-        elif media_type == MediaType.PLAYLIST:
-            library_result = await mass.music.get_library_playlists(
-                **base_params,
-            )
-        else:
-            raise HomeAssistantError(f"Unsupported media type {media_type}")
-
-        response: ServiceResponse = LIBRARY_RESULTS_SCHEMA(
-            {
-                ATTR_ITEMS: [
-                    media_item_dict_from_mass_item(mass, item)
-                    for item in library_result
-                ],
-                ATTR_LIMIT: limit,
-                ATTR_OFFSET: offset,
-                ATTR_ORDER_BY: order_by,
-                ATTR_MEDIA_TYPE: media_type,
-            }
-        )
-        return response
-
     hass.services.async_register(
         DOMAIN,
         SERVICE_GET_LIBRARY,
@@ -221,3 +112,101 @@ def register_get_library_action(hass: HomeAssistant) -> None:
         ),
         supports_response=SupportsResponse.ONLY,
     )
+
+
+async def handle_search(call: ServiceCall) -> ServiceResponse:
+    """Handle queue_command action."""
+    mass = get_music_assistant_client(call.hass, call.data[ATTR_CONFIG_ENTRY_ID])
+    search_name = call.data[ATTR_SEARCH_NAME]
+    search_artist = call.data.get(ATTR_SEARCH_ARTIST)
+    search_album = call.data.get(ATTR_SEARCH_ALBUM)
+    if search_album and search_artist:
+        search_name = f"{search_artist} - {search_album} - {search_name}"
+    elif search_album:
+        search_name = f"{search_album} - {search_name}"
+    elif search_artist:
+        search_name = f"{search_artist} - {search_name}"
+    search_results = await mass.music.search(
+        search_query=search_name,
+        media_types=call.data.get(ATTR_MEDIA_TYPE, MediaType.ALL),
+        limit=call.data[ATTR_LIMIT],
+        library_only=call.data[ATTR_LIBRARY_ONLY],
+    )
+    response: ServiceResponse = SEARCH_RESULT_SCHEMA(
+        {
+            ATTR_ARTISTS: [
+                media_item_dict_from_mass_item(mass, item)
+                for item in search_results.artists
+            ],
+            ATTR_ALBUMS: [
+                media_item_dict_from_mass_item(mass, item)
+                for item in search_results.albums
+            ],
+            ATTR_TRACKS: [
+                media_item_dict_from_mass_item(mass, item)
+                for item in search_results.tracks
+            ],
+            ATTR_PLAYLISTS: [
+                media_item_dict_from_mass_item(mass, item)
+                for item in search_results.playlists
+            ],
+            ATTR_RADIO: [
+                media_item_dict_from_mass_item(mass, item)
+                for item in search_results.radio
+            ],
+        }
+    )
+    return response
+
+
+async def handle_get_library(call: ServiceCall) -> ServiceResponse:
+    """Handle get_library action."""
+    mass = get_music_assistant_client(call.hass, call.data[ATTR_CONFIG_ENTRY_ID])
+    media_type = call.data[ATTR_MEDIA_TYPE]
+    limit = call.data.get(ATTR_LIMIT, DEFAULT_LIMIT)
+    offset = call.data.get(ATTR_OFFSET, DEFAULT_OFFSET)
+    order_by = call.data.get(ATTR_ORDER_BY, DEFAULT_SORT_ORDER)
+    base_params = {
+        "favorite": call.data.get(ATTR_FAVORITE),
+        "search": call.data.get(ATTR_SEARCH),
+        "limit": limit,
+        "offset": offset,
+        "order_by": order_by,
+    }
+    if media_type == MediaType.ALBUM:
+        library_result = await mass.music.get_library_albums(
+            **base_params,
+            album_types=call.data.get(ATTR_ALBUM_TYPE),
+        )
+    elif media_type == MediaType.ARTIST:
+        library_result = await mass.music.get_library_artists(
+            **base_params,
+            album_artists_only=call.data.get(ATTR_ALBUM_ARTISTS_ONLY),
+        )
+    elif media_type == MediaType.TRACK:
+        library_result = await mass.music.get_library_tracks(
+            **base_params,
+        )
+    elif media_type == MediaType.RADIO:
+        library_result = await mass.music.get_library_radios(
+            **base_params,
+        )
+    elif media_type == MediaType.PLAYLIST:
+        library_result = await mass.music.get_library_playlists(
+            **base_params,
+        )
+    else:
+        raise ServiceValidationError(f"Unsupported media type {media_type}")
+
+    response: ServiceResponse = LIBRARY_RESULTS_SCHEMA(
+        {
+            ATTR_ITEMS: [
+                media_item_dict_from_mass_item(mass, item) for item in library_result
+            ],
+            ATTR_LIMIT: limit,
+            ATTR_OFFSET: offset,
+            ATTR_ORDER_BY: order_by,
+            ATTR_MEDIA_TYPE: media_type,
+        }
+    )
+    return response
