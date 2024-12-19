bb8a53beb39a94ff3668f190278637ec5289a267
Mick Vleeshouwer <mick@imick.nl>
2024-12-19 22:38:43 +0000

    Add execute command service

--- homeassistant/components/overkiz/__init__.py
+++ homeassistant/components/overkiz/__init__.py
@@ -11,15 +11,18 @@ from pyoverkiz.const import SUPPORTED_SERVERS
 from pyoverkiz.enums import APIType, OverkizState, UIClass, UIWidget
 from pyoverkiz.exceptions import (
     BadCredentialsException,
+    InvalidCommandException,
     MaintenanceException,
     NotSuchTokenException,
     TooManyRequestsException,
 )
-from pyoverkiz.models import Device, OverkizServer, Scenario
+from pyoverkiz.models import Command, Device, OverkizServer, Scenario
 from pyoverkiz.utils import generate_local_server
+import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
+    ATTR_ENTITY_ID,
     CONF_HOST,
     CONF_PASSWORD,
     CONF_TOKEN,
@@ -27,10 +30,15 @@ from homeassistant.const import (
     CONF_VERIFY_SSL,
     Platform,
 )
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant, ServiceCall, callback
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import (
+    config_validation as cv,
+    device_registry as dr,
+    entity_registry as er,
+)
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from homeassistant.helpers.service import async_register_admin_service
 
 from .const import (
     CONF_API_TYPE,
@@ -43,6 +51,11 @@ from .const import (
     UPDATE_INTERVAL_ALL_ASSUMED_STATE,
 )
 from .coordinator import OverkizDataUpdateCoordinator
+from .entity import OverkizEntity
+
+SERVICE_EXECUTE_COMMAND = "execute_command"
+ATTR_COMMAND = "command"
+ATTR_ARGS = "args"
 
 
 @dataclass
@@ -161,6 +174,43 @@ async def async_setup_entry(hass: HomeAssistant, entry: OverkizDataConfigEntry) 
         )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    async def handle_execute_command(service: ServiceCall):
+        """Handle execute command service."""
+        entity_registry = er.async_get(hass)
+
+        for entity_id in service.data[ATTR_ENTITY_ID]:
+            registry_entity = entity_registry.async_get(entity_id)
+            entity: OverkizEntity = [
+                e
+                for e in hass.data["entity_components"][registry_entity.domain].entities
+                if e.unique_id == registry_entity.unique_id
+            ][0]
+
+            try:
+                await entity.coordinator.client.execute_command(
+                    entity.unique_id,
+                    Command(service.data[ATTR_COMMAND], service.data.get(ATTR_ARGS)),
+                    "Home Assistant Service",
+                )
+            except InvalidCommandException as exception:
+                LOGGER.error(exception)
+
+    async_register_admin_service(
+        hass,
+        DOMAIN,
+        SERVICE_EXECUTE_COMMAND,
+        handle_execute_command,
+        vol.Schema(
+            {
+                vol.Required(ATTR_ENTITY_ID): cv.ensure_list,
+                vol.Required(ATTR_COMMAND): cv.string,
+                vol.Optional(ATTR_ARGS, default=[]): vol.All(
+                    cv.ensure_list, [vol.Any(str, int)]
+                ),
+            },
+        ),
+    )
 
     return True
 
