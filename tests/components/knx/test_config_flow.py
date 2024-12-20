23d32762db2f230ee9832fd3c08e6aa7000fe2c1
farmio <farmio@alphart.net>
2024-12-20 23:13:22 +0100

    Add test for config entry with routing starting options flow

--- tests/components/knx/test_config_flow.py
+++ tests/components/knx/test_config_flow.py
@@ -1433,6 +1433,64 @@ async def test_options_flow_secure_manual_to_keyfile(
     knx_setup.assert_called_once()
 
 
+async def test_options_flow_routing(hass: HomeAssistant, knx_setup) -> None:
+    """Test options flow changing routing settings."""
+    mock_config_entry = MockConfigEntry(
+        title="KNX",
+        domain="knx",
+        data={
+            **DEFAULT_ENTRY_DATA,
+            CONF_KNX_CONNECTION_TYPE: CONF_KNX_ROUTING,
+        },
+    )
+    gateway = _gateway_descriptor("192.168.0.1", 3676)
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    menu_step = await hass.config_entries.options.async_init(mock_config_entry.entry_id)
+
+    with patch(
+        "homeassistant.components.knx.config_flow.GatewayScanner"
+    ) as gateway_scanner_mock:
+        gateway_scanner_mock.return_value = GatewayScannerMock([gateway])
+        result = await hass.config_entries.options.async_configure(
+            menu_step["flow_id"],
+            {"next_step_id": "connection_type"},
+        )
+        assert result["type"] is FlowResultType.FORM
+        assert result["step_id"] == "connection_type"
+
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_KNX_CONNECTION_TYPE: CONF_KNX_ROUTING,
+            },
+        )
+        assert result2["type"] is FlowResultType.FORM
+        assert result2["step_id"] == "routing"
+        assert result2["errors"] == {}
+
+    result3 = await hass.config_entries.options.async_configure(
+        result2["flow_id"],
+        {
+            CONF_KNX_INDIVIDUAL_ADDRESS: "2.0.4",
+        },
+    )
+    assert result3["type"] is FlowResultType.CREATE_ENTRY
+    assert mock_config_entry.data == {
+        **DEFAULT_ENTRY_DATA,
+        CONF_KNX_CONNECTION_TYPE: CONF_KNX_ROUTING,
+        CONF_KNX_MCAST_GRP: DEFAULT_MCAST_GRP,
+        CONF_KNX_MCAST_PORT: DEFAULT_MCAST_PORT,
+        CONF_KNX_LOCAL_IP: None,
+        CONF_KNX_INDIVIDUAL_ADDRESS: "2.0.4",
+        CONF_KNX_SECURE_DEVICE_AUTHENTICATION: None,
+        CONF_KNX_SECURE_USER_ID: None,
+        CONF_KNX_SECURE_USER_PASSWORD: None,
+        CONF_KNX_TUNNEL_ENDPOINT_IA: None,
+    }
+    knx_setup.assert_called_once()
+
+
 async def test_options_communication_settings(
     hass: HomeAssistant, knx_setup, mock_config_entry: MockConfigEntry
 ) -> None:
