af6eedd942743f500f4bb9112e56bf8c725f2af3
Arie Catsman <catsmanac@outlook.com>
2024-12-20 16:55:48 +0000

    Don't cache reauth_entry in enphase_envoy config flow

--- homeassistant/components/enphase_envoy/config_flow.py
+++ homeassistant/components/enphase_envoy/config_flow.py
@@ -13,7 +13,6 @@ import voluptuous as vol
 from homeassistant.components import zeroconf
 from homeassistant.config_entries import (
     SOURCE_REAUTH,
-    ConfigEntry,
     ConfigFlow,
     ConfigFlowResult,
     OptionsFlow,
@@ -56,8 +55,6 @@ class EnphaseConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Enphase Envoy."""
 
     VERSION = 1
-
-    _reauth_entry: ConfigEntry
 
     def __init__(self) -> None:
         """Initialize an envoy flow."""
@@ -155,7 +152,6 @@ class EnphaseConfigFlow(ConfigFlow, domain=DOMAIN):
         self, entry_data: Mapping[str, Any]
     ) -> ConfigFlowResult:
         """Handle configuration by re-auth."""
-        self._reauth_entry = self._get_reauth_entry()
         return await self.async_step_user()
 
     def _async_envoy_name(self) -> str:
@@ -168,9 +164,9 @@ class EnphaseConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         errors: dict[str, str] = {}
         description_placeholders: dict[str, str] = {}
-
         if self.source == SOURCE_REAUTH:
-            host = self._reauth_entry.data[CONF_HOST]
+            reauth_entry = self._get_reauth_entry()
+            host = reauth_entry.data[CONF_HOST]
         else:
             host = (user_input or {}).get(CONF_HOST) or self.ip_address or ""
 
@@ -196,8 +192,8 @@ class EnphaseConfigFlow(ConfigFlow, domain=DOMAIN):
 
                 if self.source == SOURCE_REAUTH:
                     return self.async_update_reload_and_abort(
-                        self._reauth_entry,
-                        data=self._reauth_entry.data | user_input,
+                        reauth_entry,
+                        data=reauth_entry.data | user_input,
                     )
 
                 if not self.unique_id:
