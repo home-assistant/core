be7595fc202598633264adc05d96b65933377111
Maciej Bieniek <bieniu@users.noreply.github.com>
2024-12-21 12:27:10 +0100

    Remove unnecessary code

--- homeassistant/components/twinkly/__init__.py
+++ homeassistant/components/twinkly/__init__.py
@@ -12,7 +12,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import ATTR_VERSION, DOMAIN
+from .const import ATTR_VERSION
 
 PLATFORMS = [Platform.LIGHT]
 
@@ -31,8 +31,6 @@ type TwinklyConfigEntry = ConfigEntry[TwinklyData]
 
 async def async_setup_entry(hass: HomeAssistant, entry: TwinklyConfigEntry) -> bool:
     """Set up entries from config flow."""
-    hass.data.setdefault(DOMAIN, {})
-
     # We setup the client here so if at some point we add any other entity for this device,
     # we will be able to properly share the connection.
     host = entry.data[CONF_HOST]
