2d787cc19e5eb6a320aee6e6fd13459f89e230b8
J. Nick Koston <nick@koston.org>
2024-12-21 09:43:47 -1000

    preen unused

--- homeassistant/components/recorder/models/database.py
+++ homeassistant/components/recorder/models/database.py
@@ -18,22 +18,5 @@ class DatabaseEngine:
     """Properties of the database engine."""
 
     dialect: SupportedDialect
-    optimizer: DatabaseOptimizer
     max_bind_vars: int
     version: AwesomeVersion | None
-
-
-@dataclass
-class DatabaseOptimizer:
-    """Properties of the database optimizer for the configured database engine."""
-
-    # Some MariaDB versions have a bug that causes a slow query when using
-    # a range in a select statement with an IN clause.
-    #
-    # https://jira.mariadb.org/browse/MDEV-25020
-    #
-    # PostgreSQL does not support a skip/loose index scan so its
-    # also slow for large distinct queries:
-    # https://wiki.postgresql.org/wiki/Loose_indexscan
-    # https://github.com/home-assistant/core/issues/126084
-    slow_range_in_select: bool
