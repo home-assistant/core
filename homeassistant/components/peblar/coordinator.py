176bfcf2cb5f27ab83d5e5e9677cf2aef72723ba
Franck Nijhof <git@frenck.dev>
2024-12-21 10:55:00 +0100

    Add update platform to Peblar Rocksolid EV Chargers integration (#133570)
    * Add update platform to Peblar Rocksolid EV Chargers integration
    
    * Use device class translations

--- homeassistant/components/peblar/coordinator.py
+++ homeassistant/components/peblar/coordinator.py
@@ -1,16 +1,67 @@
 """Data update coordinator for Peblar EV chargers."""
 
+from __future__ import annotations
+
+from dataclasses import dataclass
 from datetime import timedelta
 
-from peblar import PeblarApi, PeblarError, PeblarMeter
+from peblar import Peblar, PeblarApi, PeblarError, PeblarMeter, PeblarVersions
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from tests.components.peblar.conftest import PeblarSystemInformation
 
 from .const import LOGGER
 
-type PeblarConfigEntry = ConfigEntry[PeblarMeterDataUpdateCoordinator]
+
+@dataclass(kw_only=True)
+class PeblarRuntimeData:
+    """Class to hold runtime data."""
+
+    system_information: PeblarSystemInformation
+    meter_coordinator: PeblarMeterDataUpdateCoordinator
+    version_coordinator: PeblarVersionDataUpdateCoordinator
+
+
+type PeblarConfigEntry = ConfigEntry[PeblarRuntimeData]
+
+
+@dataclass(kw_only=True, frozen=True)
+class PeblarVersionInformation:
+    """Class to hold version information."""
+
+    current: PeblarVersions
+    available: PeblarVersions
+
+
+class PeblarVersionDataUpdateCoordinator(
+    DataUpdateCoordinator[PeblarVersionInformation]
+):
+    """Class to manage fetching Peblar version information."""
+
+    def __init__(
+        self, hass: HomeAssistant, entry: PeblarConfigEntry, peblar: Peblar
+    ) -> None:
+        """Initialize the coordinator."""
+        self.peblar = peblar
+        super().__init__(
+            hass,
+            LOGGER,
+            config_entry=entry,
+            name=f"Peblar {entry.title} version",
+            update_interval=timedelta(hours=2),
+        )
+
+    async def _async_update_data(self) -> PeblarVersionInformation:
+        """Fetch data from the Peblar device."""
+        try:
+            return PeblarVersionInformation(
+                current=await self.peblar.current_versions(),
+                available=await self.peblar.available_versions(),
+            )
+        except PeblarError as err:
+            raise UpdateFailed(err) from err
 
 
 class PeblarMeterDataUpdateCoordinator(DataUpdateCoordinator[PeblarMeter]):
