8dc79a51f2a7543436f31c39e7b1d3f7ebe5e8c6
crug80 <claudio@cr-tech.it>
2024-12-21 21:28:08 +0000

    add test

--- tests/components/modbus/test_init.py
+++ tests/components/modbus/test_init.py
@@ -1158,22 +1158,61 @@ async def test_integration_reload(
     hass: HomeAssistant,
     caplog: pytest.LogCaptureFixture,
     mock_modbus,
-    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Run test for integration reload."""
 
     caplog.set_level(logging.DEBUG)
     caplog.clear()
 
-    yaml_path = get_fixture_path("configuration.yaml", "modbus")
+    async_fire_time_changed(hass, dt_util.utcnow() + timedelta(minutes=10))
+    await hass.async_block_till_done()
+
+    yaml_path = get_fixture_path("configuration.yaml", DOMAIN)
     with mock.patch.object(hass_config, "YAML_CONFIG_FILE", yaml_path):
-        await hass.services.async_call(DOMAIN, SERVICE_RELOAD, blocking=True)
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_RELOAD,
+            {},
+            blocking=True,
+        )
         await hass.async_block_till_done()
-        for _ in range(4):
-            freezer.tick(timedelta(seconds=1))
-            async_fire_time_changed(hass)
-            await hass.async_block_till_done()
     assert "Modbus reloading" in caplog.text
+    state_sensor_1 = hass.states.get("sensor.dummy")
+    state_sensor_2 = hass.states.get("sensor.dummy_2")
+    assert state_sensor_1
+    assert not state_sensor_2
+
+    caplog.clear()
+    yaml_path = get_fixture_path("configuration_2.yaml", DOMAIN)
+    with mock.patch.object(hass_config, "YAML_CONFIG_FILE", yaml_path):
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_RELOAD,
+            {},
+            blocking=True,
+        )
+        await hass.async_block_till_done()
+    assert "Modbus reloading" in caplog.text
+    state_sensor_1 = hass.states.get("sensor.dummy")
+    state_sensor_2 = hass.states.get("sensor.dummy_2")
+    assert state_sensor_1
+    assert state_sensor_2
+
+    caplog.clear()
+    yaml_path = get_fixture_path("configuration_empty.yaml", DOMAIN)
+    with mock.patch.object(hass_config, "YAML_CONFIG_FILE", yaml_path):
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_RELOAD,
+            {},
+            blocking=True,
+        )
+        await hass.async_block_till_done()
+    assert "Modbus not present anymore" in caplog.text
+    state_sensor_1 = hass.states.get("sensor.dummy")
+    state_sensor_2 = hass.states.get("sensor.dummy_2")
+    assert not state_sensor_1
+    assert not state_sensor_2
 
 
 @pytest.mark.parametrize("do_config", [{}])
