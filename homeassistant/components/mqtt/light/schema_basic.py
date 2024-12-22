65b9b634dfdcc48e9500ede4bf7a23cadd0cd026
jbouwh <jan@jbsoft.nl>
2024-12-22 22:36:31 +0000

    Allow to process kelvin as color_temp for mqtt basic light

--- homeassistant/components/mqtt/light/schema_basic.py
+++ homeassistant/components/mqtt/light/schema_basic.py
@@ -82,6 +82,7 @@ CONF_COLOR_TEMP_COMMAND_TEMPLATE = "color_temp_command_template"
 CONF_COLOR_TEMP_COMMAND_TOPIC = "color_temp_command_topic"
 CONF_COLOR_TEMP_STATE_TOPIC = "color_temp_state_topic"
 CONF_COLOR_TEMP_VALUE_TEMPLATE = "color_temp_value_template"
+CONF_COLOR_TEMP_KELVIN = "color_temp_kelvin"
 CONF_EFFECT_COMMAND_TEMPLATE = "effect_command_template"
 CONF_EFFECT_COMMAND_TOPIC = "effect_command_topic"
 CONF_EFFECT_LIST = "effect_list"
@@ -93,6 +94,8 @@ CONF_HS_STATE_TOPIC = "hs_state_topic"
 CONF_HS_VALUE_TEMPLATE = "hs_value_template"
 CONF_MAX_MIREDS = "max_mireds"
 CONF_MIN_MIREDS = "min_mireds"
+CONF_MAX_KELVIN = "max_kelvin"
+CONF_MIN_KELVIN = "min_kelvin"
 CONF_RGB_COMMAND_TEMPLATE = "rgb_command_template"
 CONF_RGB_COMMAND_TOPIC = "rgb_command_topic"
 CONF_RGB_STATE_TOPIC = "rgb_state_topic"
@@ -182,6 +185,7 @@ PLATFORM_SCHEMA_MODERN_BASIC = (
             vol.Optional(CONF_COLOR_TEMP_COMMAND_TOPIC): valid_publish_topic,
             vol.Optional(CONF_COLOR_TEMP_STATE_TOPIC): valid_subscribe_topic,
             vol.Optional(CONF_COLOR_TEMP_VALUE_TEMPLATE): cv.template,
+            vol.Optional(CONF_COLOR_TEMP_KELVIN, default=False): cv.boolean,
             vol.Optional(CONF_EFFECT_COMMAND_TEMPLATE): cv.template,
             vol.Optional(CONF_EFFECT_COMMAND_TOPIC): valid_publish_topic,
             vol.Optional(CONF_EFFECT_LIST): vol.All(cv.ensure_list, [cv.string]),
@@ -193,6 +197,8 @@ PLATFORM_SCHEMA_MODERN_BASIC = (
             vol.Optional(CONF_HS_VALUE_TEMPLATE): cv.template,
             vol.Optional(CONF_MAX_MIREDS): cv.positive_int,
             vol.Optional(CONF_MIN_MIREDS): cv.positive_int,
+            vol.Optional(CONF_MAX_KELVIN): cv.positive_int,
+            vol.Optional(CONF_MIN_KELVIN): cv.positive_int,
             vol.Optional(CONF_NAME): vol.Any(cv.string, None),
             vol.Optional(CONF_ON_COMMAND_TYPE, default=DEFAULT_ON_COMMAND_TYPE): vol.In(
                 VALUES_ON_COMMAND_TYPE
@@ -239,6 +245,7 @@ class MqttLight(MqttEntity, LightEntity, RestoreEntity):
     _attributes_extra_blocked = MQTT_LIGHT_ATTRIBUTES_BLOCKED
     _topic: dict[str, str | None]
     _payload: dict[str, str]
+    _color_temp_kelvin: bool
     _command_templates: dict[
         str, Callable[[PublishPayloadType, TemplateVarsType], PublishPayloadType]
     ]
@@ -263,16 +270,18 @@ class MqttLight(MqttEntity, LightEntity, RestoreEntity):
 
     def _setup_from_config(self, config: ConfigType) -> None:
         """(Re)Setup the entity."""
+        self._color_temp_kelvin = config[CONF_COLOR_TEMP_KELVIN]
         self._attr_min_color_temp_kelvin = (
             color_util.color_temperature_mired_to_kelvin(max_mireds)
             if (max_mireds := config.get(CONF_MAX_MIREDS))
-            else DEFAULT_MIN_KELVIN
+            else config.get(CONF_MIN_KELVIN, DEFAULT_MIN_KELVIN)
         )
         self._attr_max_color_temp_kelvin = (
             color_util.color_temperature_mired_to_kelvin(min_mireds)
             if (min_mireds := config.get(CONF_MIN_MIREDS))
-            else DEFAULT_MAX_KELVIN
+            else config.get(CONF_MAX_KELVIN, DEFAULT_MAX_KELVIN)
         )
+
         self._attr_effect_list = config.get(CONF_EFFECT_LIST)
 
         topic: dict[str, str | None] = {
@@ -526,6 +535,9 @@ class MqttLight(MqttEntity, LightEntity, RestoreEntity):
 
         if self._optimistic_color_mode:
             self._attr_color_mode = ColorMode.COLOR_TEMP
+        if self._color_temp_kelvin:
+            self._attr_color_temp_kelvin = int(payload)
+            return
         self._attr_color_temp_kelvin = color_util.color_temperature_mired_to_kelvin(
             int(payload)
         )
@@ -818,7 +830,9 @@ class MqttLight(MqttEntity, LightEntity, RestoreEntity):
         ):
             ct_command_tpl = self._command_templates[CONF_COLOR_TEMP_COMMAND_TEMPLATE]
             color_temp = ct_command_tpl(
-                color_util.color_temperature_kelvin_to_mired(
+                kwargs[ATTR_COLOR_TEMP_KELVIN]
+                if self._color_temp_kelvin
+                else color_util.color_temperature_kelvin_to_mired(
                     kwargs[ATTR_COLOR_TEMP_KELVIN]
                 ),
                 None,
