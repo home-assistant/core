4d09f233cf0014b9b420ea5360dfce37fe46ce94
Martin Hjelmare <marhje52@gmail.com>
2024-12-23 00:07:58 +0100

    Test restore backup with agent error

--- tests/components/backup/test_manager.py
+++ tests/components/backup/test_manager.py
@@ -2514,3 +2514,88 @@ async def test_restore_backup_when_busy(
     assert result["success"] is False
     assert result["error"]["code"] == "home_assistant_error"
     assert result["error"]["message"] == "Backup manager busy: create_backup"
+
+
+@pytest.mark.usefixtures("mock_backup_generation")
+@pytest.mark.parametrize(
+    ("exception", "error_code", "error_message"),
+    [
+        (BackupAgentError("Boom!"), "home_assistant_error", "Boom!"),
+        (Exception("Boom!"), "unknown_error", "Unknown error"),
+    ],
+)
+async def test_restore_backup_agent_error(
+    hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
+    exception: Exception,
+    error_code: str,
+    error_message: str,
+) -> None:
+    """Test restore backup with agent error."""
+    remote_agent = BackupAgentTest("remote", backups=[TEST_BACKUP_ABC123])
+    await async_setup_component(hass, DOMAIN, {})
+    await hass.async_block_till_done()
+    await setup_backup_platform(
+        hass,
+        domain="test",
+        platform=Mock(
+            async_get_backup_agents=AsyncMock(return_value=[remote_agent]),
+            spec_set=BackupAgentPlatformProtocol,
+        ),
+    )
+    ws_client = await hass_ws_client(hass)
+
+    await ws_client.send_json_auto_id({"type": "backup/subscribe_events"})
+
+    result = await ws_client.receive_json()
+    assert result["event"] == {"manager_state": BackupManagerState.IDLE}
+
+    result = await ws_client.receive_json()
+    assert result["success"] is True
+
+    with (
+        patch("pathlib.Path.exists", return_value=True),
+        patch("pathlib.Path.open"),
+        patch("pathlib.Path.write_text") as mocked_write_text,
+        patch("homeassistant.core.ServiceRegistry.async_call") as mocked_service_call,
+        patch(
+            "homeassistant.components.backup.manager.validate_password"
+        ) as validate_password_mock,
+        patch.object(
+            remote_agent, "async_download_backup", side_effect=exception
+        ) as download_mock,
+    ):
+        validate_password_mock.return_value = False
+        await ws_client.send_json_auto_id(
+            {
+                "type": "backup/restore",
+                "backup_id": TEST_BACKUP_ABC123.backup_id,
+                "agent_id": remote_agent.agent_id,
+            }
+        )
+
+        result = await ws_client.receive_json()
+        assert result["event"] == {
+            "manager_state": BackupManagerState.RESTORE_BACKUP,
+            "stage": None,
+            "state": RestoreBackupState.IN_PROGRESS,
+        }
+
+        result = await ws_client.receive_json()
+        assert result["event"] == {
+            "manager_state": BackupManagerState.RESTORE_BACKUP,
+            "stage": None,
+            "state": RestoreBackupState.FAILED,
+        }
+
+        result = await ws_client.receive_json()
+        assert result["event"] == {"manager_state": BackupManagerState.IDLE}
+
+        result = await ws_client.receive_json()
+        assert not result["success"]
+        assert result["error"]["code"] == error_code
+        assert result["error"]["message"] == error_message
+
+    assert download_mock.call_count == 1
+    assert mocked_write_text.call_count == 0
+    assert mocked_service_call.call_count == 0
