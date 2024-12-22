7cf4bd5ac41148277e10985033aaca0ab5bb7285
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2024-12-22 15:30:41 +0100

    Add cover test

---
+++ tests/components/niko_home_control/test_cover.py
@@ -0,0 +1,123 @@
+"""Tests for the Niko Home Control Light platform."""
+
+from unittest.mock import AsyncMock, patch
+
+import pytest
+from syrupy import SnapshotAssertion
+
+from homeassistant.components.cover import DOMAIN as COVER_DOMAIN
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    SERVICE_CLOSE_COVER,
+    SERVICE_OPEN_COVER,
+    Platform,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from . import setup_integration
+
+from tests.common import MockConfigEntry, snapshot_platform
+
+
+async def test_cover(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    mock_niko_home_control_connection: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test all entities."""
+    with patch(
+        "homeassistant.components.niko_home_control.PLATFORMS", [Platform.COVER]
+    ):
+        await setup_integration(hass, mock_config_entry)
+
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
+
+
+@pytest.mark.parametrize(
+    ("cover_id", "entity_id"),
+    [
+        (0, "cover.cover"),
+    ],
+)
+async def test_open_cover(
+    hass: HomeAssistant,
+    mock_niko_home_control_connection: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    cover_id: int,
+    entity_id: int,
+) -> None:
+    """Test opening the cover."""
+    await setup_integration(hass, mock_config_entry)
+
+    await hass.services.async_call(
+        COVER_DOMAIN,
+        SERVICE_OPEN_COVER,
+        {ATTR_ENTITY_ID: entity_id},
+        blocking=True,
+    )
+    mock_niko_home_control_connection.covers[
+        cover_id
+    ].open_cover().assert_called_once_with(set_position=100)
+
+
+@pytest.mark.parametrize(
+    ("cover_id", "entity_id"),
+    [
+        (0, "cover.cover"),
+    ],
+)
+async def test_close_cover(
+    hass: HomeAssistant,
+    mock_niko_home_control_connection: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    cover_id: int,
+    entity_id: str,
+) -> None:
+    """Test closing the cover."""
+    await setup_integration(hass, mock_config_entry)
+
+    await hass.services.async_call(
+        COVER_DOMAIN,
+        SERVICE_CLOSE_COVER,
+        {ATTR_ENTITY_ID: entity_id},
+        blocking=True,
+    )
+    mock_niko_home_control_connection.covers[
+        cover_id
+    ].close_cover.assert_called_once_with()
+
+
+async def test_updating(
+    hass: HomeAssistant,
+    mock_niko_home_control_connection: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test turning on the light."""
+    await setup_integration(hass, mock_config_entry)
+
+    # assert hass.states.get("light.light").state == STATE_ON
+
+    # await mock_niko_home_control_connection.register_callback.call_args_list[0][0][1](0)
+    # await hass.async_block_till_done()
+
+    # assert hass.states.get("light.light").state == STATE_OFF
+
+    # assert hass.states.get("light.dimmable_light").state == STATE_ON
+    # assert hass.states.get("light.dimmable_light").attributes[ATTR_BRIGHTNESS] == 255
+
+    # await mock_niko_home_control_connection.register_callback.call_args_list[1][0][1](
+    #     80
+    # )
+    # await hass.async_block_till_done()
+
+    # assert hass.states.get("light.dimmable_light").state == STATE_ON
+    # assert hass.states.get("light.dimmable_light").attributes[ATTR_BRIGHTNESS] == 204
+
+    # await mock_niko_home_control_connection.register_callback.call_args_list[1][0][1](0)
+    # await hass.async_block_till_done()
+
+    # assert hass.states.get("light.dimmable_light").state == STATE_OFF
+    # assert hass.states.get("light.dimmable_light").attributes[ATTR_BRIGHTNESS] is None
