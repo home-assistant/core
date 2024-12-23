59d8c79371b4b7029e7faf64d53934c32e53e099
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2024-12-23 10:27:53 +0100

    Use user defined charge limit for charge limit range in Peblar (#133868)

--- homeassistant/components/peblar/number.py
+++ homeassistant/components/peblar/number.py
@@ -46,7 +46,7 @@ DESCRIPTIONS = [
         entity_category=EntityCategory.CONFIG,
         native_step=1,
         native_min_value=6,
-        native_max_value_fn=lambda x: x.system_information.hardware_max_current,
+        native_max_value_fn=lambda x: x.user_configuration_coordinator.data.user_defined_charge_limit_current,
         native_unit_of_measurement=UnitOfElectricCurrent.AMPERE,
         set_value_fn=lambda x, v: x.ev_interface(charge_current_limit=int(v) * 1000),
         value_fn=lambda x: round(x.ev.charge_current_limit / 1000),
