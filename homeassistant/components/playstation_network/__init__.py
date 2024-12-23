d2a74b6144ba907dcb91acf1bd825089fe29409d
Jack Powell <jackattack51@gmail.com>
2024-12-23 16:40:05 +0000

    clean pull request

---
+++ homeassistant/components/playstation_network/__init__.py
@@ -0,0 +1,36 @@
+"""The PlayStation Network integration."""
+
+from __future__ import annotations
+
+from psnawp_api.psn import PlaystationNetwork
+
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+
+from .const import CONF_NPSSO
+from .coordinator import PlaystationNetworkConfigEntry, PlaystationNetworkCoordinator
+
+PLATFORMS: list[Platform] = [Platform.MEDIA_PLAYER]
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: PlaystationNetworkConfigEntry
+) -> bool:
+    """Set up Playstation Network from a config entry."""
+
+    psn = PlaystationNetwork(entry.data[CONF_NPSSO])
+    user = await hass.async_add_executor_job(psn.get_user)
+
+    coordinator = PlaystationNetworkCoordinator(hass, psn, user)
+    await coordinator.async_config_entry_first_refresh()
+    entry.runtime_data = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant, entry: PlaystationNetworkConfigEntry
+) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
