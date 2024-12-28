1f9b4ec5f73adc6192a01434f491603f6f91263c
Rob Bierbooms <mail@robbierbooms.nl>
2024-12-28 20:16:02 +0000

    Add config flow tests

--- tests/components/influxdb/__init__.py
+++ tests/components/influxdb/__init__.py
@@ -1,1 +1,59 @@
 """Tests for the influxdb component."""
+
+from homeassistant.components import influxdb
+
+BASE_V1_CONFIG = {
+    "api_version": influxdb.DEFAULT_API_VERSION,
+    "host": "host",
+    "port": 123,
+    "username": "user",
+    "password": "password",
+    "verify_ssl": "False",
+    "ssl": "False",
+    "database": "db",
+    "max_retries": 0,
+    "include": {
+        "entity_globs": [],
+        "entities": [],
+        "domains": [],
+    },
+    "exclude": {
+        "entity_globs": [],
+        "entities": [],
+        "domains": [],
+    },
+    "tags": {},
+    "tags_attributes": [],
+    "measurement_attr": "unit_of_measurement",
+    "ignore_attributes": [],
+    "component_config": {},
+    "component_config_glob": {},
+    "component_config_domain": {},
+}
+BASE_V2_CONFIG = {
+    "api_version": influxdb.API_VERSION_2,
+    "host": "host",
+    "port": 123,
+    "token": "token",
+    "organization": "organization",
+    "bucket": "Home Assistant",
+    "verify_ssl": "True",
+    "include": {
+        "entity_globs": [],
+        "entities": [],
+        "domains": [],
+    },
+    "exclude": {
+        "entity_globs": [],
+        "entities": [],
+        "domains": [],
+    },
+    "max_retries": 0,
+    "tags": {},
+    "tags_attributes": [],
+    "measurement_attr": "unit_of_measurement",
+    "ignore_attributes": [],
+    "component_config": {},
+    "component_config_glob": {},
+    "component_config_domain": {},
+}
