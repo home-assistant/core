7408cd63018b4d14417de6c40af13e9e00afc750
Maikel Punie <maikel.punie@gmail.com>
2024-12-28 08:54:39 +0100

    updates

--- tests/components/velbus/conftest.py
+++ tests/components/velbus/conftest.py
@@ -1,21 +1,21 @@
 """Fixtures for the Velbus tests."""
 
-from unittest.mock import MagicMock, patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
+from velbusaio.channels import Button
 
 from homeassistant.components.velbus import VelbusConfigEntry
 from homeassistant.components.velbus.const import DOMAIN
 from homeassistant.const import CONF_NAME, CONF_PORT
 from homeassistant.core import HomeAssistant
-from homeassistant.setup import async_setup_component
 
 from .const import PORT_TCP
 
 from tests.common import MockConfigEntry
 
 ButtonOn = {
-    "get_categories.return_value": ["binary_sensor"],
+    "get_categories.return_value": ["binary_sensor", "led", "button"],
     "get_name.return_value": "ButtonOn",
     "get_module_address.return_value": 1,
     "get_channel_number.return_value": 1,
@@ -23,25 +23,57 @@ ButtonOn = {
     "get_full_name.return_value": "Channel full name",
     "get_module_sw_version.return_value": "1.0.0",
     "get_module_serial.return_value": "a1b2c3d4e5f6",
+    "is_closed.return_value": False,
     "is_on.return_value": True,
+    # TODO set_led_state
+    # TODO press
+}
+
+BlindUp = {
+    "get_categories.return_value": ["cover"],
+    "get_name.return_value": "BlindUp",
+    "get_module_address.return_value": 2,
+    "get_channel_number.return_value": 3,
+    "get_module_type_name.return_value": "VMB2BL",
+    "get_full_name.return_value": "Blind name",
+    "get_module_sw_version.return_value": "1.2.0",
+    "get_module_serial.return_value": "a2b3c4d5e6f7",
+    "get_position.return_value": 50,
+    "get_state.return_value": "up",
+    "is_opening.return_value": False,
+    "is_closing.return_value": False,
+    "is_stopped.return_value": False,
+    "is_closed.return_value": True,
+    "is_open.return_value": False,
+    "support_position.return_value": True,
+    # TODO open
+    # TODO close
+    # TODO stop
+    # TODO set_position
 }
 
 
 @pytest.fixture(name="controller")
-def mock_full_controller() -> MagicMock:
+def mock_controller(mock_buttonOn: AsyncMock) -> MagicMock:
     """Mock a successful velbus controller."""
-    with patch("homeassistant.components.velbus.Velbus", autospec=True) as controller:
-        controller.get_all_binary_sensor.return_value = [
-            mock_channel("Button", ButtonOn),
-        ]
+    with (
+        patch("homeassistant.components.velbus.Velbus", autospec=True) as controller,
+        patch(
+            "homeassistant.components.velbus.config_flow.velbusaio.controller.Velbus",
+            new=controller,
+        ),
+    ):
+        controller.get_all_binary_sensor.return_value = [mock_buttonOn]
+        print(controller.get_all_binary_sensor())
         yield controller
 
 
-def mock_channel(channel_type: str, channel_data: dict) -> MagicMock:
+@pytest.fixture
+def mock_buttonOn() -> AsyncMock:
     """Mock a successful velbus channel."""
-    with patch(f"velbusaio.channels.{channel_type}") as channel:
-        channel.configure_mock(**channel_data)
-        return channel
+    channel = AsyncMock(spec=Button)
+    channel.configure_mock(**ButtonOn)
+    return channel
 
 
 # moddify this one to set the runtime_data correctly
@@ -57,17 +89,3 @@ async def mock_config_entry(
     )
     config_entry.add_to_hass(hass)
     return config_entry
-
-
-@pytest.fixture
-async def init_integration(
-    hass: HomeAssistant,
-    config_entry: VelbusConfigEntry,
-) -> None:
-    """Load the Velbus integration."""
-    hass.config.components.add(DOMAIN)
-
-    assert await async_setup_component(hass, DOMAIN, {}) is True
-    await hass.async_block_till_done()
-    assert await hass.config_entries.async_setup(config_entry.entry_id) is True
-    await hass.async_block_till_done()
--- tests/components/velbus/test_binary_sensor.py
+++ tests/components/velbus/test_binary_sensor.py
@@ -1,23 +1,30 @@
 """Velbus binary_sensor platform tests."""
 
+from unittest.mock import patch
+
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.const import STATE_ON
+from homeassistant.const import STATE_ON, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
-from tests.common import MockConfigEntry
+from . import init_integration
+
+from tests.common import MockConfigEntry, snapshot_platform
 
 
-async def test_binary_sensors(
+async def test_entities(
     hass: HomeAssistant,
-    init_integration: MockConfigEntry,
-    entity_registry: er.EntityRegistry,
     snapshot: SnapshotAssertion,
+    config_entry: MockConfigEntry,
+    entity_registry: er.EntityRegistry,
 ) -> None:
-    """Test if all binary_sensors get created."""
-    assert len(hass.states.async_entity_ids("binary_sensor")) == 1
+    """Test all entities."""
+    with patch("homeassistant.components.velbus.PLATFORMS", [Platform.BINARY_SENSOR]):
+        await init_integration(hass, config_entry)
 
     state = hass.states.get("binary_sensor.ButtonOn")
     assert state
     assert state.state == STATE_ON
+
+    await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
