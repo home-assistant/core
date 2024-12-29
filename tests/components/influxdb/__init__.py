86299b9a23d99d4443238c78aab7fdf93af35e69
Rob Bierbooms <mail@robbierbooms.nl>
2024-12-29 19:13:04 +0000

    Refactor config in tests

--- tests/components/influxdb/__init__.py
+++ tests/components/influxdb/__init__.py
@@ -4,62 +4,6 @@ from homeassistant.components import influxdb
 
 BASE_V1_CONFIG = {
     "api_version": influxdb.DEFAULT_API_VERSION,
-    "host": "host",
-    "port": 123,
-    "username": "user",
-    "password": "password",
-    "verify_ssl": False,
-    "ssl": False,
-    "database": "db",
-    "max_retries": 0,
-    "include": {
-        "entity_globs": [],
-        "entities": [],
-        "domains": [],
-    },
-    "exclude": {
-        "entity_globs": [],
-        "entities": [],
-        "domains": [],
-    },
-    "tags": {},
-    "tags_attributes": [],
-    "measurement_attr": "unit_of_measurement",
-    "ignore_attributes": [],
-    "component_config": {},
-    "component_config_glob": {},
-    "component_config_domain": {},
-}
-BASE_V2_CONFIG = {
-    "api_version": influxdb.API_VERSION_2,
-    "host": "host",
-    "port": 123,
-    "token": "token",
-    "organization": "organization",
-    "bucket": "Home Assistant",
-    "verify_ssl": True,
-    "include": {
-        "entity_globs": [],
-        "entities": [],
-        "domains": [],
-    },
-    "exclude": {
-        "entity_globs": [],
-        "entities": [],
-        "domains": [],
-    },
-    "max_retries": 0,
-    "tags": {},
-    "tags_attributes": [],
-    "measurement_attr": "unit_of_measurement",
-    "ignore_attributes": [],
-    "component_config": {},
-    "component_config_glob": {},
-    "component_config_domain": {},
-}
-
-IMPORT_V1_CONFIG = {
-    "api_version": influxdb.DEFAULT_API_VERSION,
     "host": "localhost",
     "verify_ssl": True,
     "database": "home_assistant",
@@ -83,13 +27,12 @@ IMPORT_V1_CONFIG = {
     "component_config_domain": {},
     "bucket": "Home Assistant",
 }
-IMPORT_V2_CONFIG = {
+BASE_V2_CONFIG = {
     "api_version": influxdb.API_VERSION_2,
     "host": "us-west-2-1.aws.cloud2.influxdata.com",
     "url": "https://us-west-2-1.aws.cloud2.influxdata.com",
     "token": "token",
     "organization": "org",
-    "database": "home_assistant",
     "bucket": "Home Assistant",
     "verify_ssl": True,
     "ssl": True,
@@ -111,6 +54,7 @@ IMPORT_V2_CONFIG = {
     "component_config": {},
     "component_config_glob": {},
     "component_config_domain": {},
+    "database": "home_assistant",
 }
 
 
