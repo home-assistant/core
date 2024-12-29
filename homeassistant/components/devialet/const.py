14bbf7c3d80c5e721b290ce96666e9a3623b4c79
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2024-12-29 14:24:15 +0000

    Remove unused constant

--- homeassistant/components/devialet/const.py
+++ homeassistant/components/devialet/const.py
@@ -1,6 +1,5 @@
 """Constants for the Devialet integration."""
 
-from collections.abc import Mapping
 from typing import Final
 
 DOMAIN: Final = "devialet"
@@ -11,19 +10,4 @@ SOUND_MODES = {
     "Flat": "flat",
     "Night mode": "night mode",
     "Voice": "voice",
-}
-
-# Translation of MediaMetadata keys to DIDL-Lite keys.
-# See https://developers.google.com/cast/docs/reference/messages#MediaData via
-# https://www.home-assistant.io/integrations/media_player/ for HA keys.
-# See http://www.upnp.org/specs/av/UPnP-av-ContentDirectory-v4-Service.pdf for
-# DIDL-Lite keys.
-MEDIA_METADATA_DIDL: Mapping[str, str] = {
-    "subtitle": "longDescription",
-    "releaseDate": "date",
-    "studio": "publisher",
-    "season": "episodeSeason",
-    "episode": "episodeNumber",
-    "albumName": "album",
-    "trackNumber": "originalTrackNumber",
 }
