5204a82b6bcbc5b0f59b62ca97ad2aea6619c43d
Rob Bierbooms <mail@robbierbooms.nl>
2024-12-29 11:39:43 +0000

    Validate influxdb connection on import

--- homeassistant/components/influxdb/config_flow.py
+++ homeassistant/components/influxdb/config_flow.py
@@ -1,13 +1,33 @@
 """Config flow for InfluxDB integration."""
 
 import logging
+from typing import Any
 
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigFlowResult
+from homeassistant.core import HomeAssistant
 
-from . import DOMAIN
+from . import DOMAIN, get_influx_connection
 
 _LOGGER = logging.getLogger(__name__)
+
+
+async def _validate_influxdb_connection(
+    hass: HomeAssistant, data: dict[str, Any]
+) -> dict[str, str] | None:
+    """Validate connection to influxdb."""
+
+    try:
+        influx = await hass.async_add_executor_job(get_influx_connection, data, True)
+    except ConnectionError:
+        return {"base": "cannot_connect"}
+    except Exception:
+        _LOGGER.exception("Unknown error")
+        return {"base": "unknown"}
+
+    influx.close()
+
+    return None
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -22,5 +42,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         await self.async_set_unique_id(host)
         self._abort_if_unique_id_configured(import_config)
+
+        errors = await _validate_influxdb_connection(self.hass, import_config)
+        if errors:
+            return self.async_abort(reason=errors["base"])
 
         return self.async_create_entry(title=host, data=import_config)
