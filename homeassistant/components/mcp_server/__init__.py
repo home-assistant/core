bdffbbb24706848f92033de5b75a8876d6f37147
Allen Porter <allen.porter@gmail.com>
2024-12-29 22:21:06 +0000

    Remove unusued code in init

--- homeassistant/components/mcp_server/__init__.py
+++ homeassistant/components/mcp_server/__init__.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.typing import ConfigType
@@ -14,9 +13,10 @@ from .types import MCPServerConfigEntry
 
 __all__ = [
     "DOMAIN",
+    "async_setup",
+    "async_setup_entry",
+    "async_unload_entry",
 ]
-
-PLATFORMS: list[Platform] = []
 
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
@@ -30,7 +30,6 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 async def async_setup_entry(hass: HomeAssistant, entry: MCPServerConfigEntry) -> bool:
     """Set up Model Context Protocol Server from a config entry."""
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.runtime_data = SessionManager()
 
     return True
@@ -40,4 +39,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: MCPServerConfigEntry) -
     """Unload a config entry."""
     session_manager = entry.runtime_data
     session_manager.close()
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    return True
