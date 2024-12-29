3729a2656bfdf0c8ee97c33ba3f7e564b1ea96a0
Joostlek <joostlek@outlook.com>
2024-12-29 11:24:51 +0100

    Push Overseerr updates via webhook

--- homeassistant/components/overseerr/const.py
+++ homeassistant/components/overseerr/const.py
@@ -17,4 +17,29 @@ REGISTERED_NOTIFICATIONS = (
     | NotificationType.REQUEST_PROCESSING_FAILED
     | NotificationType.REQUEST_AUTOMATICALLY_APPROVED
 )
-JSON_PAYLOAD = '"{\\"notification_type\\":\\"{{notification_type}}\\",\\"event\\":\\"{{event}}\\",\\"subject\\":\\"{{subject}}\\",\\"message\\":\\"{{message}}\\",\\"image\\":\\"{{image}}\\",\\"{{media}}\\":{\\"media_type\\":\\"{{media_type}}\\",\\"tmdbId\\":\\"{{media_tmdbid}}\\",\\"tvdbId\\":\\"{{media_tvdbid}}\\",\\"status\\":\\"{{media_status}}\\",\\"status4k\\":\\"{{media_status4k}}\\"},\\"{{request}}\\":{\\"request_id\\":\\"{{request_id}}\\",\\"requestedBy_email\\":\\"{{requestedBy_email}}\\",\\"requestedBy_username\\":\\"{{requestedBy_username}}\\",\\"requestedBy_avatar\\":\\"{{requestedBy_avatar}}\\",\\"requestedBy_settings_discordId\\":\\"{{requestedBy_settings_discordId}}\\",\\"requestedBy_settings_telegramChatId\\":\\"{{requestedBy_settings_telegramChatId}}\\"},\\"{{issue}}\\":{\\"issue_id\\":\\"{{issue_id}}\\",\\"issue_type\\":\\"{{issue_type}}\\",\\"issue_status\\":\\"{{issue_status}}\\",\\"reportedBy_email\\":\\"{{reportedBy_email}}\\",\\"reportedBy_username\\":\\"{{reportedBy_username}}\\",\\"reportedBy_avatar\\":\\"{{reportedBy_avatar}}\\",\\"reportedBy_settings_discordId\\":\\"{{reportedBy_settings_discordId}}\\",\\"reportedBy_settings_telegramChatId\\":\\"{{reportedBy_settings_telegramChatId}}\\"},\\"{{comment}}\\":{\\"comment_message\\":\\"{{comment_message}}\\",\\"commentedBy_email\\":\\"{{commentedBy_email}}\\",\\"commentedBy_username\\":\\"{{commentedBy_username}}\\",\\"commentedBy_avatar\\":\\"{{commentedBy_avatar}}\\",\\"commentedBy_settings_discordId\\":\\"{{commentedBy_settings_discordId}}\\",\\"commentedBy_settings_telegramChatId\\":\\"{{commentedBy_settings_telegramChatId}}\\"},\\"{{extra}}\\":[]\\n}"'
+JSON_PAYLOAD = (
+    '"{\\"notification_type\\":\\"{{notification_type}}\\",\\"event\\":\\"'
+    '{{event}}\\",\\"subject\\":\\"{{subject}}\\",\\"message\\":\\"{{messa'
+    'ge}}\\",\\"image\\":\\"{{image}}\\",\\"{{media}}\\":{\\"media_type\\"'
+    ':\\"{{media_type}}\\",\\"tmdbId\\":\\"{{media_tmdbid}}\\",\\"tvdbId\\'
+    '":\\"{{media_tvdbid}}\\",\\"status\\":\\"{{media_status}}\\",\\"statu'
+    's4k\\":\\"{{media_status4k}}\\"},\\"{{request}}\\":{\\"request_id\\":'
+    '\\"{{request_id}}\\",\\"requestedBy_email\\":\\"{{requestedBy_email}}'
+    '\\",\\"requestedBy_username\\":\\"{{requestedBy_username}}\\",\\"requ'
+    'estedBy_avatar\\":\\"{{requestedBy_avatar}}\\",\\"requestedBy_setting'
+    's_discordId\\":\\"{{requestedBy_settings_discordId}}\\",\\"requestedB'
+    'y_settings_telegramChatId\\":\\"{{requestedBy_settings_telegramChatId'
+    '}}\\"},\\"{{issue}}\\":{\\"issue_id\\":\\"{{issue_id}}\\",\\"issue_ty'
+    'pe\\":\\"{{issue_type}}\\",\\"issue_status\\":\\"{{issue_status}}\\",'
+    '\\"reportedBy_email\\":\\"{{reportedBy_email}}\\",\\"reportedBy_usern'
+    'ame\\":\\"{{reportedBy_username}}\\",\\"reportedBy_avatar\\":\\"{{rep'
+    'ortedBy_avatar}}\\",\\"reportedBy_settings_discordId\\":\\"{{reported'
+    'By_settings_discordId}}\\",\\"reportedBy_settings_telegramChatId\\":'
+    '\\"{{reportedBy_settings_telegramChatId}}\\"},\\"{{comment}}\\":{\\"c'
+    'omment_message\\":\\"{{comment_message}}\\",\\"commentedBy_email\\":'
+    '\\"{{commentedBy_email}}\\",\\"commentedBy_username\\":\\"{{commented'
+    'By_username}}\\",\\"commentedBy_avatar\\":\\"{{commentedBy_avatar}}'
+    '\\",\\"commentedBy_settings_discordId\\":\\"{{commentedBy_settings_di'
+    'scordId}}\\",\\"commentedBy_settings_telegramChatId\\":\\"{{commented'
+    'By_settings_telegramChatId}}\\"},\\"{{extra}}\\":[]\\n}"'
+)
