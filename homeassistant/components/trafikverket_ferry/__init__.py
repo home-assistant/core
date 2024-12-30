7f72bde38531536f0f6c0679eeb10b6ba0da7003
G Johansson <goran.johansson@shiftit.se>
2024-12-30 21:43:46 +0000

    Use typed config entry everywhere in Trafikverket Ferry

--- homeassistant/components/trafikverket_ferry/__init__.py
+++ homeassistant/components/trafikverket_ferry/__init__.py
@@ -22,6 +22,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: TVFerryConfigEntry) -> b
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: TVFerryConfigEntry) -> bool:
     """Unload Trafikverket Ferry config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
