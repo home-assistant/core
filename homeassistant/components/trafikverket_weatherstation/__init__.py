38420a2b10d673e82becedb5d3b5dc15a9cacb0e
G Johansson <goran.johansson@shiftit.se>
2024-12-30 21:58:06 +0000

    Explicitly set config entry in Trafikverket Weatherstation coordinator

--- homeassistant/components/trafikverket_weatherstation/__init__.py
+++ homeassistant/components/trafikverket_weatherstation/__init__.py
@@ -14,7 +14,7 @@ TVWeatherConfigEntry = ConfigEntry[TVDataUpdateCoordinator]
 async def async_setup_entry(hass: HomeAssistant, entry: TVWeatherConfigEntry) -> bool:
     """Set up Trafikverket Weatherstation from a config entry."""
 
-    coordinator = TVDataUpdateCoordinator(hass)
+    coordinator = TVDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
     entry.runtime_data = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
