ea9f7baa444583e45a736e5e6059d8113d3efb6a
tronikos <tronikos@users.noreply.github.com>
2024-12-30 09:12:26 +0000

    Log ConfigEntryNotReady in Opower

--- homeassistant/components/opower/__init__.py
+++ homeassistant/components/opower/__init__.py
@@ -2,21 +2,31 @@
 
 from __future__ import annotations
 
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN
 from .coordinator import OpowerCoordinator
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
+_LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Opower from a config entry."""
 
     coordinator = OpowerCoordinator(hass, entry.data)
-    await coordinator.async_config_entry_first_refresh()
+    try:
+        await coordinator.async_config_entry_first_refresh()
+    except ConfigEntryNotReady:
+        _LOGGER.exception("Unable to setup Opower")
+        raise
+
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
