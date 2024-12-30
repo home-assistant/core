6e0306ced14fa91c88f8c9f5ef66ea3c28eda4a9
G Johansson <goran.johansson@shiftit.se>
2024-12-30 23:15:54 +0100

    Use typed config entry everywhere in Trafikverket Weatherstation (#134308)

--- homeassistant/components/trafikverket_weatherstation/__init__.py
+++ homeassistant/components/trafikverket_weatherstation/__init__.py
@@ -22,6 +22,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: TVWeatherConfigEntry) ->
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: TVWeatherConfigEntry) -> bool:
     """Unload Trafikverket Weatherstation config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
