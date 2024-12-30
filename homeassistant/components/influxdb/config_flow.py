4ca30d85375e24c7b6a839f69b54c10a30be3745
Rob Bierbooms <mail@robbierbooms.nl>
2024-12-30 09:49:49 +0000

    Add database or bucket to unique id and title

--- homeassistant/components/influxdb/config_flow.py
+++ homeassistant/components/influxdb/config_flow.py
@@ -5,9 +5,11 @@ from typing import Any
 
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigFlowResult
+from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 
 from . import DOMAIN, get_influx_connection
+from .const import CONF_API_VERSION, CONF_BUCKET, CONF_DB_NAME, DEFAULT_API_VERSION
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -39,13 +41,22 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_import(self, import_config=None) -> ConfigFlowResult:
         """Handle the initial step."""
-        host = import_config.get("host")
+        host = import_config.get(CONF_HOST)
+        database = import_config.get(CONF_DB_NAME)
+        bucket = import_config.get(CONF_BUCKET)
 
-        await self.async_set_unique_id(host)
+        if import_config[CONF_API_VERSION] == DEFAULT_API_VERSION:
+            unique_id = f"{host}_{database}"
+            title = f"{database} ({host})"
+        else:
+            unique_id = f"{host}_{bucket}"
+            title = f"{bucket} ({host})"
+
+        await self.async_set_unique_id(unique_id)
         self._abort_if_unique_id_configured(import_config)
 
         errors = await _validate_influxdb_connection(self.hass, import_config)
         if errors:
             return self.async_abort(reason=errors["base"])
 
-        return self.async_create_entry(title=host, data=import_config)
+        return self.async_create_entry(title=title, data=import_config)
