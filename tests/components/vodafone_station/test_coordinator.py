35d98bf3184eba516bb8daf1f0b3fcbce6f91903
Simone Chemelli <simone.chemelli@gmail.com>
2024-12-31 12:53:46 +0000

    apply review comment

--- tests/components/vodafone_station/test_coordinator.py
+++ tests/components/vodafone_station/test_coordinator.py
@@ -58,11 +58,15 @@ async def test_coordinator_device_cleanup(
 ) -> None:
     """Test Device cleanup on coordinator update."""
 
+    device_tracker = f"device_tracker.vodafone_station_{DEVICE_1_MAC}"
     entry = MockConfigEntry(domain=DOMAIN, data=MOCK_USER_DATA)
     entry.add_to_hass(hass)
 
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
+
+    state = hass.states.get(device_tracker)
+    assert state is None
 
     mock_vodafone_station_router.get_devices_data = DEVICE_2
 
@@ -70,5 +74,5 @@ async def test_coordinator_device_cleanup(
     async_fire_time_changed(hass)
     await hass.async_block_till_done(wait_background_tasks=True)
 
-    state = hass.states.get(f"device_tracker.vodafone_station_{DEVICE_1_MAC}")
+    state = hass.states.get(device_tracker)
     assert state is None
