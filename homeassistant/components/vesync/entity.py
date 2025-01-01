bae5657bc938bfd0330bb750817d859f0b490365
Indu Prakash <leolite1@gmail.com>
2025-01-01 14:04:39 +0000

    Removed async_added_to_hass

--- homeassistant/components/vesync/entity.py
+++ homeassistant/components/vesync/entity.py
@@ -51,13 +51,6 @@ class VeSyncBaseEntity(CoordinatorEntity[VeSyncDataCoordinator]):
             sw_version=self.device.current_firm_version,
         )
 
-    async def async_added_to_hass(self) -> None:
-        """When entity is added to hass."""
-        await super().async_added_to_hass()
-        self.async_on_remove(
-            self.coordinator.async_add_listener(self.async_write_ha_state)
-        )
-
 
 class VeSyncDevice(VeSyncBaseEntity, ToggleEntity):
     """Base class for VeSync Device Representations."""
