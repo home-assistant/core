8b7e5777dd8430860e6b441362d7a412c809d59a
Indu Prakash <leolite1@gmail.com>
2025-01-02 03:58:30 +0000

    Added tests for energy update
    The new file defines the data for the "Outlet"
    device.

--- tests/components/vesync/test_coordinator.py
+++ tests/components/vesync/test_coordinator.py
@@ -10,7 +10,12 @@ from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
 
-from .common import mock_air_purifier_400s_update_response, mock_devices_response
+from .common import (
+    mock_air_purifier_400s_update_response,
+    mock_device_response,
+    mock_multiple_device_responses,
+    mock_outlet_energy_response,
+)
 
 from tests.common import MockConfigEntry, async_fire_time_changed
 
@@ -33,30 +38,55 @@ async def test_coordinator_update(
         entry_id="1",
     )
 
-    mock_devices_response(requests_mock, "Air Purifier 400s")
+    mock_multiple_device_responses(requests_mock, ["Air Purifier 400s", "Outlet"])
+
+    expected_entities = [
+        # From "Air Purifier 400s"
+        "fan.air_purifier_400s",
+        "sensor.air_purifier_400s_filter_lifetime",
+        "sensor.air_purifier_400s_air_quality",
+        "sensor.air_purifier_400s_pm2_5",
+        # From Outlet
+        "switch.outlet",
+        "sensor.outlet_current_power",
+        "sensor.outlet_energy_use_today",
+        "sensor.outlet_energy_use_weekly",
+        "sensor.outlet_energy_use_monthly",
+        "sensor.outlet_energy_use_yearly",
+        "sensor.outlet_current_voltage",
+    ]
+
     config_entry.add_to_hass(hass)
 
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
     assert config_entry.state is ConfigEntryState.LOADED
 
-    # Air Purifier 400s should generate 1 fan and 3 sensor entities
-    assert hass.states.get("fan.air_purifier_400s").state != STATE_UNAVAILABLE
-    assert (
-        hass.states.get("sensor.air_purifier_400s_filter_lifetime").state
-        != STATE_UNAVAILABLE
-    )
-    assert (
-        hass.states.get("sensor.air_purifier_400s_air_quality").state
-        != STATE_UNAVAILABLE
-    )
-    assert hass.states.get("sensor.air_purifier_400s_pm2_5").state != STATE_UNAVAILABLE
+    for entity_id in expected_entities:
+        assert hass.states.get(entity_id).state != STATE_UNAVAILABLE
 
-    # Update the mock to supply air_quality=15
+    assert hass.states.get("sensor.air_purifier_400s_air_quality").state == "5"
+    assert hass.states.get("sensor.outlet_current_voltage").state == "120.0"
+    assert hass.states.get("sensor.outlet_energy_use_weekly").state == "0"
+
+    # Update the mock responses
     mock_air_purifier_400s_update_response(requests_mock)
+    mock_outlet_energy_response(requests_mock, "Outlet", {"totalEnergy": 2.2})
+    mock_device_response(requests_mock, "Outlet", {"voltage": 129})
 
     freezer.tick(timedelta(seconds=UPDATE_INTERVAL))
     async_fire_time_changed(hass)
     await hass.async_block_till_done(True)
 
     assert hass.states.get("sensor.air_purifier_400s_air_quality").state == "15"
+    assert hass.states.get("sensor.outlet_current_voltage").state == "129.0"
+
+    # Test energy update
+    # pyvesync only updates energy parameters once every 6 hours and hence the 21600.
+    freezer.tick(timedelta(seconds=21600))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done(True)
+
+    assert hass.states.get("sensor.air_purifier_400s_air_quality").state == "15"
+    assert hass.states.get("sensor.outlet_current_voltage").state == "129.0"
+    assert hass.states.get("sensor.outlet_energy_use_weekly").state == "2.2"
