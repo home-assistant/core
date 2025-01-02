f87cd0c3c5e46022a7dc57033f34eadf12608061
GhoweVege <85890024+GhoweVege@users.noreply.github.com>
2025-01-02 19:58:18 +0000

    Got rid of unused constants

--- homeassistant/components/vegehub/const.py
+++ homeassistant/components/vegehub/const.py
@@ -7,12 +7,3 @@ NAME = "VegeHub"
 PLATFORMS = [Platform.SENSOR]
 MANUFACTURER = "vegetronix"
 MODEL = "VegeHub"
-OPTION_DATA_TYPE_CHOICES = [
-    "Raw Voltage",
-    "VH400",
-    "THERM200",
-]
-CHAN_TYPE_SENSOR = "sensor"
-CHAN_TYPE_ACTUATOR = "actuator"
-CHAN_TYPE_BATTERY = "battery"
-API_PATH = "/api/vegehub/update"
