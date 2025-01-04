dfd0494f074b8419d56161308c4f96ae756a9085
moshe <m@inn.co.il>
2025-01-04 23:24:56 +0200

    When I am cooling, all is OK When I am heating, the origin function is set only the new parameter, but the CCM15 want to receive the all settings of the current A/C. We need to set them again and again, in every change (fan mode, ac mode, temperature).
    The other option will freeze me and my wife in the rainy winter (true story).
    
    As the py-ccm15 is not managed anymore, I was forked it and update all we need.

--- homeassistant/components/ccm15/climate.py
+++ homeassistant/components/ccm15/climate.py
@@ -145,15 +145,17 @@ class CCM15Climate(CoordinatorEntity[CCM15Coordinator], ClimateEntity):
     async def async_set_temperature(self, **kwargs: Any) -> None:
         """Set the target temperature."""
         if (temperature := kwargs.get(ATTR_TEMPERATURE)) is not None:
-            await self.coordinator.async_set_temperature(self._ac_index, temperature)
+            await self.coordinator.async_set_temperature(
+                self._ac_index, self.data, temperature
+            )
 
     async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:
         """Set the hvac mode."""
-        await self.coordinator.async_set_hvac_mode(self._ac_index, hvac_mode)
+        await self.coordinator.async_set_hvac_mode(self._ac_index, self.data, hvac_mode)
 
     async def async_set_fan_mode(self, fan_mode: str) -> None:
         """Set the fan mode."""
-        await self.coordinator.async_set_fan_mode(self._ac_index, fan_mode)
+        await self.coordinator.async_set_fan_mode(self._ac_index, self.data, fan_mode)
 
     async def async_turn_off(self) -> None:
         """Turn off."""
