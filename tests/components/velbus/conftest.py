62ce020068fad2d8d1094656a1b7589df2c49e52
Maikel Punie <maikel.punie@gmail.com>
2025-01-04 14:07:25 +0100

    Add velbus cover platform testcases (#134654)

--- tests/components/velbus/conftest.py
+++ tests/components/velbus/conftest.py
@@ -5,6 +5,7 @@ from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 from velbusaio.channels import (
+    Blind,
     Button,
     ButtonCounter,
     Dimmer,
@@ -38,6 +39,8 @@ def mock_controller(
     mock_dimmer: AsyncMock,
     mock_module_no_subdevices: AsyncMock,
     mock_module_subdevices: AsyncMock,
+    mock_cover: AsyncMock,
+    mock_cover_no_position: AsyncMock,
 ) -> Generator[AsyncMock]:
     """Mock a successful velbus controller."""
     with (
@@ -61,6 +64,7 @@ def mock_controller(
         ]
         cont.get_all_light.return_value = [mock_dimmer]
         cont.get_all_led.return_value = [mock_button]
+        cont.get_all_cover.return_value = [mock_cover, mock_cover_no_position]
         cont.get_modules.return_value = {
             1: mock_module_no_subdevices,
             2: mock_module_no_subdevices,
@@ -248,6 +252,48 @@ def mock_dimmer() -> AsyncMock:
     channel.get_module_serial.return_value = "a1b2c3d4e5f6g7"
     channel.is_on.return_value = False
     channel.get_dimmer_state.return_value = 33
+    return channel
+
+
+@pytest.fixture
+def mock_cover() -> AsyncMock:
+    """Mock a successful velbus channel."""
+    channel = AsyncMock(spec=Blind)
+    channel.get_categories.return_value = ["cover"]
+    channel.get_name.return_value = "CoverName"
+    channel.get_module_address.return_value = 201
+    channel.get_channel_number.return_value = 2
+    channel.get_module_type_name.return_value = "VMB2BLE"
+    channel.get_full_name.return_value = "Full cover name"
+    channel.get_module_sw_version.return_value = "1.0.1"
+    channel.get_module_serial.return_value = "1234"
+    channel.support_position.return_value = True
+    channel.get_position.return_value = 50
+    channel.is_closed.return_value = False
+    channel.is_open.return_value = True
+    channel.is_opening.return_value = False
+    channel.is_closing.return_value = False
+    return channel
+
+
+@pytest.fixture
+def mock_cover_no_position() -> AsyncMock:
+    """Mock a successful velbus channel."""
+    channel = AsyncMock(spec=Blind)
+    channel.get_categories.return_value = ["cover"]
+    channel.get_name.return_value = "CoverNameNoPos"
+    channel.get_module_address.return_value = 200
+    channel.get_channel_number.return_value = 1
+    channel.get_module_type_name.return_value = "VMB2BLE"
+    channel.get_full_name.return_value = "Full cover name no position"
+    channel.get_module_sw_version.return_value = "1.0.1"
+    channel.get_module_serial.return_value = "12345"
+    channel.support_position.return_value = False
+    channel.get_position.return_value = None
+    channel.is_closed.return_value = False
+    channel.is_open.return_value = True
+    channel.is_opening.return_value = True
+    channel.is_closing.return_value = True
     return channel
 
 
