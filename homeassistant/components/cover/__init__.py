b861e4eabeccafc670bfcec939cd4162515c5e53
Franck Nijhof <git@frenck.dev>
2025-01-07 00:08:02 +0100

    Revert "Remove deprecated supported features warning in ..." (multiple) (#134933)

--- homeassistant/components/cover/__init__.py
+++ homeassistant/components/cover/__init__.py
@@ -300,6 +300,10 @@ class CoverEntity(Entity, cached_properties=CACHED_PROPERTIES_WITH_ATTR_):
     def supported_features(self) -> CoverEntityFeature:
         """Flag supported features."""
         if (features := self._attr_supported_features) is not None:
+            if type(features) is int:  # noqa: E721
+                new_features = CoverEntityFeature(features)
+                self._report_deprecated_supported_features_values(new_features)
+                return new_features
             return features
 
         supported_features = (
