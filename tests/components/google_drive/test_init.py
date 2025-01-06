ace8e577014a11c5e7ad198697eb6fcdde6498fd
tronikos <tronikos@users.noreply.github.com>
2025-01-06 23:48:39 +0000

    Add test_init.py

---
+++ tests/components/google_drive/test_init.py
@@ -0,0 +1,204 @@
+"""Tests for Google Drive."""
+
+from collections.abc import Awaitable, Callable, Coroutine
+import http
+import time
+from typing import Any
+
+import pytest
+
+from homeassistant.components.application_credentials import (
+    ClientCredential,
+    async_import_client_credential,
+)
+from homeassistant.components.google_drive import DOMAIN
+from homeassistant.config_entries import ConfigEntryState
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
+
+from tests.common import MockConfigEntry
+from tests.test_util.aiohttp import AiohttpClientMocker
+
+TEST_FOLDER_ID = "google-folder-it"
+
+type ComponentSetup = Callable[[], Awaitable[None]]
+
+
+@pytest.fixture(name="expires_at")
+def mock_expires_at() -> int:
+    """Fixture to set the oauth token expiration time."""
+    return time.time() + 3600
+
+
+@pytest.fixture(name="config_entry")
+def mock_config_entry(expires_at: int) -> MockConfigEntry:
+    """Fixture for MockConfigEntry."""
+    return MockConfigEntry(
+        domain=DOMAIN,
+        unique_id=TEST_FOLDER_ID,
+        data={
+            "auth_implementation": DOMAIN,
+            "token": {
+                "access_token": "mock-access-token",
+                "refresh_token": "mock-refresh-token",
+                "expires_at": expires_at,
+                "scope": "https://www.googleapis.com/auth/drive.file",
+            },
+        },
+    )
+
+
+@pytest.fixture(name="setup_integration")
+async def mock_setup_integration(
+    hass: HomeAssistant, config_entry: MockConfigEntry
+) -> Callable[[], Coroutine[Any, Any, None]]:
+    """Fixture for setting up the component."""
+    config_entry.add_to_hass(hass)
+
+    assert await async_setup_component(hass, "application_credentials", {})
+    await async_import_client_credential(
+        hass,
+        DOMAIN,
+        ClientCredential("client-id", "client-secret"),
+        DOMAIN,
+    )
+
+    async def func() -> None:
+        assert await async_setup_component(hass, DOMAIN, {})
+        await hass.async_block_till_done()
+
+    return func
+
+
+async def test_setup_success(
+    hass: HomeAssistant,
+    setup_integration: ComponentSetup,
+    aioclient_mock: AiohttpClientMocker,
+) -> None:
+    """Test successful setup and unload."""
+    # Setup looks up existing folder to make sure it still exists
+    aioclient_mock.get(
+        f"https://www.googleapis.com/drive/v3/files/{TEST_FOLDER_ID}?fields=",
+        json={},
+    )
+
+    await setup_integration()
+
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
+    assert entries[0].state is ConfigEntryState.LOADED
+
+    await hass.config_entries.async_unload(entries[0].entry_id)
+    await hass.async_block_till_done()
+
+    assert not hass.data.get(DOMAIN)
+    assert entries[0].state is ConfigEntryState.NOT_LOADED
+    assert not hass.services.async_services().get(DOMAIN, {})
+
+
+@pytest.mark.parametrize(
+    ("status", "expected_state"),
+    [
+        (
+            http.HTTPStatus.BAD_REQUEST,
+            ConfigEntryState.SETUP_ERROR,
+        ),
+        (
+            http.HTTPStatus.NOT_FOUND,
+            ConfigEntryState.SETUP_ERROR,
+        ),
+        (
+            http.HTTPStatus.INTERNAL_SERVER_ERROR,
+            ConfigEntryState.SETUP_RETRY,
+        ),
+    ],
+    ids=["bad_request", "not_found", "transient_failure"],
+)
+async def test_setup_error(
+    hass: HomeAssistant,
+    setup_integration: ComponentSetup,
+    aioclient_mock: AiohttpClientMocker,
+    status: http.HTTPStatus,
+    expected_state: ConfigEntryState,
+) -> None:
+    """Test setup error."""
+    # Simulate failure looking up existing folder
+    aioclient_mock.get(
+        f"https://www.googleapis.com/drive/v3/files/{TEST_FOLDER_ID}?fields=",
+        status=status,
+    )
+
+    await setup_integration()
+
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
+    assert entries[0].state is expected_state
+
+
+@pytest.mark.parametrize("expires_at", [time.time() - 3600], ids=["expired"])
+async def test_expired_token_refresh_success(
+    hass: HomeAssistant,
+    setup_integration: ComponentSetup,
+    aioclient_mock: AiohttpClientMocker,
+) -> None:
+    """Test expired token is refreshed."""
+    # Setup looks up existing folder to make sure it still exists
+    aioclient_mock.get(
+        f"https://www.googleapis.com/drive/v3/files/{TEST_FOLDER_ID}?fields=",
+        json={},
+    )
+
+    aioclient_mock.post(
+        "https://oauth2.googleapis.com/token",
+        json={
+            "access_token": "updated-access-token",
+            "refresh_token": "updated-refresh-token",
+            "expires_at": time.time() + 3600,
+            "expires_in": 3600,
+        },
+    )
+
+    await setup_integration()
+
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
+    assert entries[0].state is ConfigEntryState.LOADED
+    assert entries[0].data["token"]["access_token"] == "updated-access-token"
+    assert entries[0].data["token"]["expires_in"] == 3600
+
+
+@pytest.mark.parametrize(
+    ("expires_at", "status", "expected_state"),
+    [
+        (
+            time.time() - 3600,
+            http.HTTPStatus.UNAUTHORIZED,
+            ConfigEntryState.SETUP_ERROR,
+        ),
+        (
+            time.time() - 3600,
+            http.HTTPStatus.INTERNAL_SERVER_ERROR,
+            ConfigEntryState.SETUP_RETRY,
+        ),
+    ],
+    ids=["failure_requires_reauth", "transient_failure"],
+)
+async def test_expired_token_refresh_failure(
+    hass: HomeAssistant,
+    setup_integration: ComponentSetup,
+    aioclient_mock: AiohttpClientMocker,
+    status: http.HTTPStatus,
+    expected_state: ConfigEntryState,
+) -> None:
+    """Test failure while refreshing token with a transient error."""
+
+    aioclient_mock.post(
+        "https://oauth2.googleapis.com/token",
+        status=status,
+    )
+
+    await setup_integration()
+
+    # Verify a transient failure has occurred
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert entries[0].state is expected_state
