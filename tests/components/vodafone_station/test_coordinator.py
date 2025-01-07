f74a1e5f81549b0e28c0e2a61f9e5550c5c6a9e1
Simone Chemelli <simone.chemelli@gmail.com>
2025-01-07 12:16:46 +0000

    cleanup test

--- tests/components/vodafone_station/test_coordinator.py
+++ tests/components/vodafone_station/test_coordinator.py
@@ -3,6 +3,7 @@
 from unittest.mock import AsyncMock
 
 from freezegun.api import FrozenDateTimeFactory
+import pytest
 
 from homeassistant.components.vodafone_station.const import DOMAIN, SCAN_INTERVAL
 from homeassistant.core import HomeAssistant
@@ -12,6 +13,7 @@ from .const import DEVICE_1_MAC, DEVICE_2, MOCK_USER_DATA
 from tests.common import MockConfigEntry, async_fire_time_changed
 
 
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
 async def test_coordinator_device_cleanup(
     hass: HomeAssistant,
     freezer: FrozenDateTimeFactory,
@@ -19,17 +21,18 @@ async def test_coordinator_device_cleanup(
 ) -> None:
     """Test Device cleanup on coordinator update."""
 
-    device_tracker = f"device_tracker.vodafone_station_{DEVICE_1_MAC}"
     entry = MockConfigEntry(domain=DOMAIN, data=MOCK_USER_DATA)
     entry.add_to_hass(hass)
+
+    device_tracker = f"device_tracker.vodafone_station_{DEVICE_1_MAC.replace(":", "_")}"
 
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
     state = hass.states.get(device_tracker)
-    assert state is None
+    assert state is not None
 
-    mock_vodafone_station_router.get_devices_data = DEVICE_2
+    mock_vodafone_station_router.get_devices_data.return_value = DEVICE_2
 
     freezer.tick(SCAN_INTERVAL)
     async_fire_time_changed(hass)
