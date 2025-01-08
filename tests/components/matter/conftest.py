6bfeeede18e0ce636f40dac6999260028d0ddf72
Ludovic BOUE <lboue@users.noreply.github.com>
2025-01-08 14:59:23 +0100

    Add test in conftest

--- tests/components/matter/conftest.py
+++ tests/components/matter/conftest.py
@@ -104,6 +104,7 @@ async def integration_fixture(
         "pressure_sensor",
         "room_airconditioner",
         "silabs_dishwasher",
+        "silabs_laundrywasher",
         "smoke_detector",
         "switch_unit",
         "temperature_sensor",
