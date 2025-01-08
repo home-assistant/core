5c769828cab4a78d25d18e2061e1c9ccc9639a23
G Johansson <goran.johansson@shiftit.se>
2025-01-08 20:18:43 +0000

    Change sequence

--- homeassistant/components/sensibo/binary_sensor.py
+++ homeassistant/components/sensibo/binary_sensor.py
@@ -154,33 +154,28 @@ async def async_setup_entry(
         if new_devices:
             entities.extend(
                 SensiboMotionSensor(
-                    coordinator,
-                    device_id,
-                    sensor_id,
-                    sensor_data,
-                    description,
+                    coordinator, device_id, sensor_id, sensor_data, description
                 )
-                for description in MOTION_SENSOR_TYPES
                 for device_id, device_data in coordinator.data.parsed.items()
                 if device_data.motion_sensors
                 for sensor_id, sensor_data in device_data.motion_sensors.items()
                 if sensor_id in new_devices
+                for description in MOTION_SENSOR_TYPES
             )
 
             entities.extend(
                 SensiboDeviceSensor(coordinator, device_id, description)
-                for description in MOTION_DEVICE_SENSOR_TYPES
                 for device_id, device_data in coordinator.data.parsed.items()
-                if device_data.motion_sensors
-                if device_id in new_devices
+                if device_data.motion_sensors and device_id in new_devices
+                for description in MOTION_DEVICE_SENSOR_TYPES
             )
             entities.extend(
                 SensiboDeviceSensor(coordinator, device_id, description)
                 for device_id, device_data in coordinator.data.parsed.items()
+                if device_id in new_devices
                 for description in DESCRIPTION_BY_MODELS.get(
                     device_data.model, DEVICE_SENSOR_TYPES
                 )
-                if device_id in new_devices
             )
 
         async_add_entities(entities)
