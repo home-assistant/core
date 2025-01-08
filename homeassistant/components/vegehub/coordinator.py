e6ffed7444c745da422f2597b36beb4695706ae5
GhoweVege <85890024+GhoweVege@users.noreply.github.com>
2025-01-08 16:12:28 +0000

    Implemented a coordinator

---
+++ homeassistant/components/vegehub/coordinator.py
@@ -0,0 +1,25 @@
+"""Coordinator for the Vegetronix VegeHub."""
+
+from __future__ import annotations
+
+import logging
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class VegeHubCoordinator(DataUpdateCoordinator):
+    """The DataUpdateCoordinator for VegeHub."""
+
+    def __init__(self, hass: HomeAssistant, device_id: str) -> None:
+        """Initialize VegeHub data coordinator."""
+        super().__init__(hass, _LOGGER, name=f"{device_id} DataUpdateCoordinator")
+        self.device_id = device_id
+        self.data = {}
+
+    async def async_update_data(self, new_data):
+        """Update the coordinator with new data from the webhook."""
+        self.data.update(new_data)
+        self.async_update_listeners()
