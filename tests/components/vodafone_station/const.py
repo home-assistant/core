36ac603c39aef18741610fdf3e661fb68d7f29aa
Simone Chemelli <simone.chemelli@gmail.com>
2025-01-10 09:47:48 +0000

    align test to latest code

--- tests/components/vodafone_station/const.py
+++ tests/components/vodafone_station/const.py
@@ -1,3 +1,4 @@
 """Common stuff for Vodafone Station tests."""
 
 DEVICE_1_MAC = "xx:xx:xx:xx:xx:xx"
+DEVICE_2_MAC = "yy:yy:yy:yy:yy:yy"
--- tests/components/vodafone_station/test_coordinator.py
+++ tests/components/vodafone_station/test_coordinator.py
@@ -2,13 +2,15 @@
 
 from unittest.mock import AsyncMock
 
+from aiovodafone import VodafoneStationDevice
 from freezegun.api import FrozenDateTimeFactory
 import pytest
 
-from homeassistant.components.vodafone_station.const import DOMAIN, SCAN_INTERVAL
+from homeassistant.components.vodafone_station.const import SCAN_INTERVAL
 from homeassistant.core import HomeAssistant
 
-from .const import DEVICE_1_MAC, DEVICE_2, MOCK_USER_DATA
+from . import setup_integration
+from .const import DEVICE_1_MAC, DEVICE_2_MAC
 
 from tests.common import MockConfigEntry, async_fire_time_changed
 
@@ -18,21 +20,28 @@ async def test_coordinator_device_cleanup(
     hass: HomeAssistant,
     freezer: FrozenDateTimeFactory,
     mock_vodafone_station_router: AsyncMock,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test Device cleanup on coordinator update."""
 
-    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_USER_DATA)
-    entry.add_to_hass(hass)
+    await setup_integration(hass, mock_config_entry)
 
     device_tracker = f"device_tracker.vodafone_station_{DEVICE_1_MAC.replace(":", "_")}"
-
-    await hass.config_entries.async_setup(entry.entry_id)
-    await hass.async_block_till_done()
 
     state = hass.states.get(device_tracker)
     assert state is not None
 
-    mock_vodafone_station_router.get_devices_data.return_value = DEVICE_2
+    mock_vodafone_station_router.get_devices_data.return_value = {
+        DEVICE_2_MAC: VodafoneStationDevice(
+            connected=True,
+            connection_type="lan",
+            ip_address="192.168.1.11",
+            name="LanDevice0",
+            mac=DEVICE_2_MAC,
+            type="desktop",
+            wifi="",
+        ),
+    }
 
     freezer.tick(SCAN_INTERVAL)
     async_fire_time_changed(hass)
