54a5287e5bf61508caebc301a89aaa13e929442e
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2025-01-10 20:05:17 +0100

    Move HomeWizard API initialization to async_setup_entry

--- homeassistant/components/homewizard/coordinator.py
+++ homeassistant/components/homewizard/coordinator.py
@@ -3,6 +3,8 @@
 from __future__ import annotations
 
 from homewizard_energy import HomeWizardEnergy
+
+from homewizard_energy import HomeWizardEnergy
 from homewizard_energy.errors import DisabledError, RequestError
 from homewizard_energy.models import CombinedModels as DeviceResponseEntry
 
