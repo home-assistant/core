98b35c4d22875036fbae17f22e75bd785a47c22c
cdnninja <jaydenaphillips@gmail.com>
2025-01-10 07:28:42 -0700

    ruff

--- homeassistant/components/vesync/__init__.py
+++ homeassistant/components/vesync/__init__.py
@@ -100,7 +100,6 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    
     hass.data[DOMAIN][VS_LISTENERS]()
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
