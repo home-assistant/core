109a2877c6bd16aaf11c50d0c6a785a5772b0737
Shay Levy <levyshay1@gmail.com>
2025-01-10 19:16:25 +0200

    Use runtime_data in LG webOS TV (#135301)

--- homeassistant/components/webostv/const.py
+++ homeassistant/components/webostv/const.py
@@ -9,7 +9,6 @@ from homeassistant.const import Platform
 
 DOMAIN = "webostv"
 PLATFORMS = [Platform.MEDIA_PLAYER]
-DATA_CONFIG_ENTRY = "config_entry"
 DATA_HASS_CONFIG = "hass_config"
 DEFAULT_NAME = "LG webOS TV"
 
