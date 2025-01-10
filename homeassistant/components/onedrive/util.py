ae2f5dde0631a6fc51935fecaf6b1fffd4c61243
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2025-01-10 09:14:18 +0100

    remove util func

--- homeassistant/components/onedrive/util.py
+++ homeassistant/components/onedrive/util.py
@@ -8,14 +8,6 @@ import json
 from homeassistant.components.backup import AgentBackup
 
 
-async def bytes_to_async_iterator(
-    data: bytes, chunk_size: int = 1024
-) -> AsyncIterator[bytes]:
-    """Convert a bytes object into an AsyncIterator[bytes]."""
-    for i in range(0, len(data), chunk_size):
-        yield data[i : i + chunk_size]
-
-
 async def async_iterator_to_bytesio(async_iterator: AsyncIterator[bytes]) -> BytesIO:
     """Convert an AsyncIterator[bytes] to a BytesIO object."""
     buffer = BytesIO()
