f1410e9886dc248121cc2fc29877b10e43cb6a8e
javicalle <31999997+javicalle@users.noreply.github.com>
2025-01-11 21:17:00 +0100

    Allow RFLink BinarySensors from events
    Currently RFLink only allows generating BinarySensors from commands.
    This new version also allows creating them from RFLink events.

--- homeassistant/components/rflink/binary_sensor.py
+++ homeassistant/components/rflink/binary_sensor.py
@@ -21,13 +21,24 @@ from homeassistant.const import (
 )
 from homeassistant.core import HomeAssistant, callback
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 import homeassistant.helpers.event as evt
 from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-from .const import CONF_ALIASES
+from .const import (
+    CONF_ALIASES,
+    DATA_ENTITY_GROUP_LOOKUP,
+    DATA_ENTITY_LOOKUP,
+    EVENT_KEY_COMMAND,
+    EVENT_KEY_SENSOR,
+    SIGNAL_AVAILABILITY,
+    SIGNAL_HANDLE_EVENT,
+    TMP_ENTITY,
+)
 from .entity import RflinkDevice
+from .utils import identify_event_type
 
 CONF_OFF_DELAY = "off_delay"
 DEFAULT_FORCE_UPDATE = False
@@ -94,20 +105,92 @@ class RflinkBinarySensor(RflinkDevice, BinarySensorEntity, RestoreEntity):
         super().__init__(device_id, **kwargs)
 
     async def async_added_to_hass(self) -> None:
-        """Restore RFLink BinarySensor state."""
-        await super().async_added_to_hass()
+        """Register update callback."""
+        # Restore RFLink BinarySensor state
         if (old_state := await self.async_get_last_state()) is not None:
             if self._off_delay is None:
                 self._state = old_state.state == STATE_ON
             else:
                 self._state = False
+        # Remove temporary bogus entity_id if added
+        tmp_entity = TMP_ENTITY.format(self._device_id)
+        if (
+            tmp_entity
+            in self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_SENSOR][self._device_id]
+        ):
+            self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_SENSOR][
+                self._device_id
+            ].remove(tmp_entity)
+
+        if (
+            tmp_entity
+            in self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][self._device_id]
+        ):
+            self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][
+                self._device_id
+            ].remove(tmp_entity)
+
+        # Register id and aliases
+        self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][self._device_id].append(
+            self.entity_id
+        )
+        self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_SENSOR][self._device_id].append(
+            self.entity_id
+        )
+
+        if self._group:
+            self.hass.data[DATA_ENTITY_GROUP_LOOKUP][EVENT_KEY_COMMAND][
+                self._device_id
+            ].append(self.entity_id)
+        # aliases respond to both normal and group commands (allon/alloff)
+        if self._aliases:
+            for _id in self._aliases:
+                self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_SENSOR][_id].append(
+                    self.entity_id
+                )
+                self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][_id].append(
+                    self.entity_id
+                )
+                self.hass.data[DATA_ENTITY_GROUP_LOOKUP][EVENT_KEY_COMMAND][_id].append(
+                    self.entity_id
+                )
+        # group_aliases only respond to group commands (allon/alloff)
+        if self._group_aliases:
+            for _id in self._group_aliases:
+                self.hass.data[DATA_ENTITY_GROUP_LOOKUP][EVENT_KEY_COMMAND][_id].append(
+                    self.entity_id
+                )
+        # nogroup_aliases only respond to normal commands
+        if self._nogroup_aliases:
+            for _id in self._nogroup_aliases:
+                self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][_id].append(
+                    self.entity_id
+                )
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass, SIGNAL_AVAILABILITY, self._availability_callback
+            )
+        )
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass,
+                SIGNAL_HANDLE_EVENT.format(self.entity_id),
+                self.handle_event_callback,
+            )
+        )
+
+        # Process the initial event now that the entity is created
+        if self._initial_event:
+            self.handle_event_callback(self._initial_event)
 
     def _handle_event(self, event):
         """Domain specific event handler."""
-        command = event["command"]
-        if command in ["on", "allon"]:
+
+        event_type = identify_event_type(event)
+        value = event[event_type]
+        if value in ["on", "allon", "low"]
             self._state = True
-        elif command in ["off", "alloff"]:
+        elif value in ["off", "alloff", "ok"]:
             self._state = False
 
         if self._state and self._off_delay is not None:
