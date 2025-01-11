8e6eafa47ef8942012274b20aa7ce2a62588f7b1
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2025-01-11 23:12:06 +0100

    Get config_flow coverage back to 100%

--- tests/components/homewizard/test_init.py
+++ tests/components/homewizard/test_init.py
@@ -14,12 +14,12 @@ from homeassistant.core import HomeAssistant
 from tests.common import MockConfigEntry, async_fire_time_changed
 
 
-async def test_load_unload(
+async def test_load_unload_v1(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
     mock_homewizardenergy: MagicMock,
 ) -> None:
-    """Test loading and unloading of integration."""
+    """Test loading and unloading of integration with v1 config."""
     mock_config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
@@ -31,6 +31,25 @@ async def test_load_unload(
     await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
+
+
+async def test_load_unload_v2(
+    hass: HomeAssistant,
+    mock_config_entry_v2: MockConfigEntry,
+    mock_homewizardenergy_v2: MagicMock,
+) -> None:
+    """Test loading and unloading of integration with v2 config."""
+    mock_config_entry_v2.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry_v2.entry_id)
+    await hass.async_block_till_done()
+
+    assert mock_config_entry_v2.state is ConfigEntryState.LOADED
+    assert len(mock_homewizardenergy_v2.combined.mock_calls) == 1
+
+    await hass.config_entries.async_unload(mock_config_entry_v2.entry_id)
+    await hass.async_block_till_done()
+
+    assert mock_config_entry_v2.state is ConfigEntryState.NOT_LOADED
 
 
 async def test_load_failed_host_unavailable(
