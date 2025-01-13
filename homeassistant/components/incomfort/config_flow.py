3e91827df5121a292b51d2128d9798454e2b4dc7
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2025-01-13 19:50:06 +0100

    Add option to allow to use setpoint instead of override for legacy incomfort RF gateway (#135143)
    * Add option to allow to use setpoint in stead of override for legacy incomfort RF gateway
    
    * Add test to assert state with legacy_setpoint_status option
    
    * Use selector
    
    * Update homeassistant/components/incomfort/strings.json
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>
    
    * Follow up on code review
    
    * Rephrase data_description
    
    * Rephrase
    
    * Use async_schedule_reload helper
    
    * Move option flow after config flow
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/incomfort/config_flow.py
+++ homeassistant/components/incomfort/config_flow.py
@@ -1,21 +1,30 @@
 """Config flow support for Intergas InComfort integration."""
 
+from __future__ import annotations
+
 from typing import Any
 
 from aiohttp import ClientResponseError
 from incomfortclient import IncomfortError, InvalidHeaterList
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import (
+    ConfigEntry,
+    ConfigFlow,
+    ConfigFlowResult,
+    OptionsFlow,
+)
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.selector import (
+    BooleanSelector,
+    BooleanSelectorConfig,
     TextSelector,
     TextSelectorConfig,
     TextSelectorType,
 )
 
-from .const import DOMAIN
+from .const import CONF_LEGACY_SETPOINT_STATUS, DOMAIN
 from .coordinator import async_connect_gateway
 
 TITLE = "Intergas InComfort/Intouch Lan2RF gateway"
@@ -31,6 +40,14 @@ CONFIG_SCHEMA = vol.Schema(
         vol.Optional(CONF_PASSWORD): TextSelector(
             TextSelectorConfig(type=TextSelectorType.PASSWORD)
         ),
+    }
+)
+
+OPTIONS_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_LEGACY_SETPOINT_STATUS, default=False): BooleanSelector(
+            BooleanSelectorConfig()
+        )
     }
 )
 
@@ -66,6 +83,14 @@ async def async_try_connect_gateway(
 class InComfortConfigFlow(ConfigFlow, domain=DOMAIN):
     """Config flow to set up an Intergas InComfort boyler and thermostats."""
 
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> InComfortOptionsFlowHandler:
+        """Get the options flow for this handler."""
+        return InComfortOptionsFlowHandler()
+
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
@@ -80,4 +105,30 @@ class InComfortConfigFlow(ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user", data_schema=CONFIG_SCHEMA, errors=errors
+        )
+
+
+class InComfortOptionsFlowHandler(OptionsFlow):
+    """Handle InComfort Lan2RF gateway options."""
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Manage the options."""
+        errors: dict[str, str] | None = None
+        if user_input is not None:
+            new_options: dict[str, Any] = self.config_entry.options | user_input
+            self.hass.config_entries.async_update_entry(
+                self.config_entry, options=new_options
+            )
+            self.hass.config_entries.async_schedule_reload(self.config_entry.entry_id)
+            return self.async_create_entry(data=new_options)
+
+        data_schema = self.add_suggested_values_to_schema(
+            OPTIONS_SCHEMA, self.config_entry.options
+        )
+        return self.async_show_form(
+            step_id="init",
+            data_schema=data_schema,
+            errors=errors,
         )
