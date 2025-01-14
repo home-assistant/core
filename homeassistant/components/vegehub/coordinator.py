a0eb4a006ddaa1aa0332798ab5c485d1fd91cfcc
GhoweVege <85890024+GhoweVege@users.noreply.github.com>
2025-01-14 16:33:44 +0000

    Fixed errors in coordinator.py

--- homeassistant/components/vegehub/coordinator.py
+++ homeassistant/components/vegehub/coordinator.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import logging
+from typing import Any
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
@@ -10,15 +11,14 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 _LOGGER = logging.getLogger(__name__)
 
 
-class VegeHubCoordinator(DataUpdateCoordinator):
+class VegeHubCoordinator(DataUpdateCoordinator[dict[str, Any]]):
     """The DataUpdateCoordinator for VegeHub."""
 
     def __init__(self, hass: HomeAssistant, device_id: str) -> None:
         """Initialize VegeHub data coordinator."""
         super().__init__(hass, _LOGGER, name=f"{device_id} DataUpdateCoordinator")
         self.device_id = device_id
-        self.data = {}
 
-    async def async_update_data(self, new_data):
+    async def async_update_data(self, new_data: dict[str, Any]) -> None:
         """Update the coordinator with new data from the webhook."""
         super().async_set_updated_data(new_data)
