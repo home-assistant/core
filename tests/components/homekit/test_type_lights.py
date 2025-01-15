cefcd9eda558c2713a182dfc6ce29668531d8215
J. Nick Koston <nick@koston.org>
2025-01-15 13:17:54 -1000

    add more coverage

--- tests/components/homekit/test_type_lights.py
+++ tests/components/homekit/test_type_lights.py
@@ -1,6 +1,7 @@
 """Test different accessory types: Lights."""
 
 from datetime import timedelta
+import sys
 
 from pyhap.const import HAP_REPR_AID, HAP_REPR_CHARS, HAP_REPR_IID, HAP_REPR_VALUE
 import pytest
@@ -736,6 +737,74 @@ async def test_light_invalid_hs_color(
     assert acc.char_color_temp.value == 404
     assert acc.char_hue.value == 100
     assert acc.char_saturation.value == 100
+
+
+async def test_light_invalid_values(
+    hass: HomeAssistant, hk_driver, events: list[Event]
+) -> None:
+    """Test light with a variety of invalid values."""
+    entity_id = "light.demo"
+
+    hass.states.async_set(
+        entity_id,
+        STATE_ON,
+        {
+            ATTR_SUPPORTED_COLOR_MODES: ["color_temp", "hs"],
+            ATTR_COLOR_MODE: "hs",
+            ATTR_HS_COLOR: (-1, -1),
+        },
+    )
+    await hass.async_block_till_done()
+    acc = Light(hass, hk_driver, "Light", entity_id, 1, None)
+    hk_driver.add_accessory(acc)
+
+    assert acc.char_color_temp.value == 153
+    assert acc.char_hue.value == 0
+    assert acc.char_saturation.value == 0
+    hass.states.async_set(
+        entity_id,
+        STATE_ON,
+        {
+            ATTR_SUPPORTED_COLOR_MODES: ["color_temp", "hs"],
+            ATTR_COLOR_MODE: "color_temp",
+            ATTR_COLOR_TEMP_KELVIN: -1,
+        },
+    )
+    await hass.async_block_till_done()
+    acc.run()
+
+    assert acc.char_color_temp.value == 153
+    assert acc.char_hue.value == 16
+    assert acc.char_saturation.value == 100
+    hass.states.async_set(
+        entity_id,
+        STATE_ON,
+        {
+            ATTR_SUPPORTED_COLOR_MODES: ["color_temp", "hs"],
+            ATTR_COLOR_MODE: "color_temp",
+            ATTR_COLOR_TEMP_KELVIN: sys.maxsize,
+        },
+    )
+    await hass.async_block_till_done()
+
+    assert acc.char_color_temp.value == 153
+    assert acc.char_hue.value == 220
+    assert acc.char_saturation.value == 41
+
+    hass.states.async_set(
+        entity_id,
+        STATE_ON,
+        {
+            ATTR_SUPPORTED_COLOR_MODES: ["color_temp", "hs"],
+            ATTR_COLOR_MODE: "color_temp",
+            ATTR_COLOR_TEMP_KELVIN: 2000,
+        },
+    )
+    await hass.async_block_till_done()
+
+    assert acc.char_color_temp.value == 500
+    assert acc.char_hue.value == 31
+    assert acc.char_saturation.value == 95
 
 
 @pytest.mark.parametrize(
