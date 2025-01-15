4cac8a8a34760d0e231c339e9d7c35be46df75ef
epenet <6771947+epenet@users.noreply.github.com>
2025-01-15 15:49:45 +0100

    Use new ServiceInfo location in components (part 3) (#135687)

--- homeassistant/components/tado/config_flow.py
+++ homeassistant/components/tado/config_flow.py
@@ -10,7 +10,6 @@ from PyTado.interface import Tado
 import requests.exceptions
 import voluptuous as vol
 
-from homeassistant.components import zeroconf
 from homeassistant.config_entries import (
     ConfigEntry,
     ConfigFlow,
@@ -20,6 +19,10 @@ from homeassistant.config_entries import (
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers.service_info.zeroconf import (
+    ATTR_PROPERTIES_ID,
+    ZeroconfServiceInfo,
+)
 
 from .const import (
     CONF_FALLBACK,
@@ -104,14 +107,14 @@ class TadoConfigFlow(ConfigFlow, domain=DOMAIN):
         )
 
     async def async_step_homekit(
-        self, discovery_info: zeroconf.ZeroconfServiceInfo
+        self, discovery_info: ZeroconfServiceInfo
     ) -> ConfigFlowResult:
         """Handle HomeKit discovery."""
         self._async_abort_entries_match()
         properties = {
             key.lower(): value for (key, value) in discovery_info.properties.items()
         }
-        await self.async_set_unique_id(properties[zeroconf.ATTR_PROPERTIES_ID])
+        await self.async_set_unique_id(properties[ATTR_PROPERTIES_ID])
         self._abort_if_unique_id_configured()
         return await self.async_step_user()
 
