571867891d61bfcd7d8f4c61eccde3dfe8eccd3f
Marko Todoric <marko.todoric@addiko.com>
2025-01-16 20:19:51 +0100

    Initial commit for Backup SFTP Component

---
+++ homeassistant/components/backup_sftp/const.py
@@ -0,0 +1,26 @@
+"""Constants for the SFTP Backup Storage integration."""
+
+from __future__ import annotations
+
+from collections.abc import Callable
+import logging
+from typing import Final
+
+from homeassistant.util.hass_dict import HassKey
+
+DOMAIN: Final = "backup_sftp"
+
+LOGGER = logging.getLogger(__package__)
+
+SEPARATOR: Final = "#!#!#"
+
+CONF_HOST: Final = 'host'
+CONF_PORT: Final = 'port'
+CONF_USERNAME: Final = 'username'
+CONF_PASSWORD: Final = 'password'
+CONF_PRIVATE_KEY_FILE: Final = 'private_key_file'
+CONF_BACKUP_LOCATION: Final = 'backup_location'
+
+DATA_BACKUP_AGENT_LISTENERS: HassKey[list[Callable[[], None]]] = HassKey(
+    f"{DOMAIN}.backup_agent_listeners"
+)
