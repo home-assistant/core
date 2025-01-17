caa77d5b1fbcdcfcde95b4014cbab5febc4f0112
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-01-17 13:42:00 +0000

    revert

--- tests/components/iron_os/test_init.py
+++ tests/components/iron_os/test_init.py
@@ -10,6 +10,8 @@ import pytest
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
+import homeassistant.helpers.device_registry as dr
+from homeassistant.helpers.device_registry import CONNECTION_BLUETOOTH
 
 from .conftest import DEFAULT_NAME
 
@@ -88,3 +90,47 @@ async def test_v223_entities_not_loaded(
     ) is not None
 
     assert len(state.attributes["options"]) == 2
+
+
+@pytest.mark.usefixtures("ble_device")
+async def test_device_info_update(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_pynecil: AsyncMock,
+    device_registry: dr.DeviceRegistry,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test device info gets updated."""
+
+    mock_pynecil.get_device_info.return_value = DeviceInfoResponse()
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    device = device_registry.async_get_device(
+        connections={(CONNECTION_BLUETOOTH, config_entry.unique_id)}
+    )
+    assert device
+    assert device.sw_version is None
+    assert device.serial_number is None
+
+    mock_pynecil.get_device_info.return_value = DeviceInfoResponse(
+        build="v2.22",
+        device_id="c0ffeeC0",
+        address="c0:ff:ee:c0:ff:ee",
+        device_sn="0000c0ffeec0ffee",
+        name=DEFAULT_NAME,
+    )
+
+    freezer.tick(timedelta(seconds=60))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    device = device_registry.async_get_device(
+        connections={(CONNECTION_BLUETOOTH, config_entry.unique_id)}
+    )
+    assert device
+    assert device.sw_version == "v2.22"
+    assert device.serial_number == "0000c0ffeec0ffee (ID:c0ffeeC0)"
