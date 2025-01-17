cdc0a9702e413a7c4af195c16c264550336a7cb3
Erwin Douna <e.douna@gmail.com>
2025-01-17 14:45:43 +0000

    Remove patch

--- tests/components/sma/test_diagnostics.py
+++ tests/components/sma/test_diagnostics.py
@@ -1,7 +1,5 @@
 """Test the SMA diagnostics."""
 
-from unittest.mock import patch
-
 from syrupy import SnapshotAssertion
 from syrupy.filters import props
 
@@ -20,10 +18,8 @@ async def test_get_config_entry_diagnostics(
 ) -> None:
     """Test if get_config_entry_diagnostics returns the correct data."""
     mock_config_entry.add_to_hass(hass)
-
-    with patch("homeassistant.components.sma.async_setup_entry", return_value=True):
-        await hass.config_entries.async_setup(mock_config_entry.entry_id)
-        await hass.async_block_till_done()
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
 
     diagnostics = await get_diagnostics_for_config_entry(
         hass, hass_client, mock_config_entry
