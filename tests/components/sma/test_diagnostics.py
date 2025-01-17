a113c6a289de0e94d95ab986d598f44d0cb2d6fd
Erwin Douna <e.douna@gmail.com>
2025-01-17 14:37:58 +0000

    Add diagnostics

---
+++ tests/components/sma/test_diagnostics.py
@@ -0,0 +1,33 @@
+"""Test the SMA diagnostics."""
+
+from unittest.mock import patch
+
+from syrupy import SnapshotAssertion
+from syrupy.filters import props
+
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+from tests.components.diagnostics import get_diagnostics_for_config_entry
+from tests.typing import ClientSessionGenerator
+
+
+async def test_get_config_entry_diagnostics(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    hass_client: ClientSessionGenerator,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test if get_config_entry_diagnostics returns the correct data."""
+    mock_config_entry.add_to_hass(hass)
+
+    with patch("homeassistant.components.sma.async_setup_entry", return_value=True):
+        await hass.config_entries.async_setup(mock_config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    diagnostics = await get_diagnostics_for_config_entry(
+        hass, hass_client, mock_config_entry
+    )
+    assert diagnostics == snapshot(
+        exclude=props("created_at", "modified_at", "entry_id")
+    )
