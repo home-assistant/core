595f49ee9f16433deda21d1987404117390d7431
Manu <4445816+tr4nt0r@users.noreply.github.com>
2025-01-18 16:35:35 +0100

    Set strict-typing in Habitica quality scale record (#135899)
    * Set strict-typing in Habitica quality scale record
    
    * cast

--- homeassistant/components/habitica/calendar.py
+++ homeassistant/components/habitica/calendar.py
@@ -6,7 +6,7 @@ from abc import abstractmethod
 from dataclasses import asdict
 from datetime import date, datetime, timedelta
 from enum import StrEnum
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 from uuid import UUID
 
 from dateutil.rrule import rrule
@@ -95,9 +95,11 @@ class HabiticaCalendarEntity(HabiticaBase, CalendarEntity):
     ) -> list[datetime]:
         """Calculate recurrence dates based on start_date and end_date."""
         if end_date:
-            return recurrences.between(
+            recurrence_dates = recurrences.between(
                 start_date, end_date - timedelta(days=1), inc=True
             )
+
+            return cast(list[datetime], recurrence_dates)
         # if no end_date is given, return only the next recurrence
         return [recurrences.after(start_date, inc=True)]
 
