94c05eeab939b9b78b9368868b49b9ab5808d121
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-01-18 21:49:21 +0000

    nitpicks

--- homeassistant/components/playstation_network/__init__.py
+++ homeassistant/components/playstation_network/__init__.py
@@ -2,14 +2,12 @@
 
 from __future__ import annotations
 
-from psnawp_api.core.psnawp_exceptions import PSNAWPAuthenticationError
 from psnawp_api.psn import PlaystationNetwork
 
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
 
-from .const import CONF_NPSSO, DOMAIN
+from .const import CONF_NPSSO
 from .coordinator import PlaystationNetworkConfigEntry, PlaystationNetworkCoordinator
 
 PLATFORMS: list[Platform] = [Platform.MEDIA_PLAYER]
@@ -21,12 +19,8 @@ async def async_setup_entry(
     """Set up Playstation Network from a config entry."""
 
     psn = PlaystationNetwork(entry.data[CONF_NPSSO])
-    try:
-        user = await hass.async_add_executor_job(psn.get_user)
-    except PSNAWPAuthenticationError as error:
-        raise ConfigEntryNotReady(DOMAIN, "not_ready") from error
 
-    coordinator = PlaystationNetworkCoordinator(hass, psn, user)
+    coordinator = PlaystationNetworkCoordinator(hass, psn)
     await coordinator.async_config_entry_first_refresh()
     entry.runtime_data = coordinator
 
