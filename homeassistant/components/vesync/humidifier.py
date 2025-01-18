c161e9176751eb021089a45fc996c1b1d6550643
cdnninja <jaydenaphillips@gmail.com>
2025-01-18 22:57:43 +0000

    Align mode to parameter as well

--- homeassistant/components/vesync/humidifier.py
+++ homeassistant/components/vesync/humidifier.py
@@ -125,7 +125,7 @@ class VeSyncHumidifierHA(VeSyncBaseEntity, HumidifierEntity):
     @property
     def mode(self) -> str | None:
         """Get the current preset mode."""
-        return _get_ha_mode(self.device.details["mode"])
+        return _get_ha_mode(self.device.mode)
 
     def set_humidity(self, humidity: int) -> None:
         """Set the target humidity of the device."""
