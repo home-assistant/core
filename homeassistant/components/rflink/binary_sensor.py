fa349dc2954bd382d8382fcaed79e1bde3a755a7
javicalle <31999997+javicalle@users.noreply.github.com>
2025-01-19 15:17:58 +0100

    reduce duplicate code
    Reduce duplicate code for better testing

--- homeassistant/components/rflink/binary_sensor.py
+++ homeassistant/components/rflink/binary_sensor.py
@@ -29,12 +29,8 @@ from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from .const import (
     CONF_ALIASES,
-    DATA_ENTITY_GROUP_LOOKUP,
     DATA_ENTITY_LOOKUP,
-    EVENT_KEY_COMMAND,
     EVENT_KEY_SENSOR,
-    SIGNAL_AVAILABILITY,
-    SIGNAL_HANDLE_EVENT,
     TMP_ENTITY,
 )
 from .entity import RflinkDevice
@@ -104,15 +100,8 @@ class RflinkBinarySensor(RflinkDevice, BinarySensorEntity, RestoreEntity):
         self._delay_listener = None
         super().__init__(device_id, **kwargs)
 
-    # pylint: disable-next=hass-missing-super-call
     async def async_added_to_hass(self) -> None:
         """Register update callback."""
-        # Restore RFLink BinarySensor state
-        if (old_state := await self.async_get_last_state()) is not None:
-            if self._off_delay is None:
-                self._state = old_state.state == STATE_ON
-            else:
-                self._state = False
         # Remove temporary bogus entity_id if added
         tmp_entity = TMP_ENTITY.format(self._device_id)
         if (
@@ -123,66 +112,24 @@ class RflinkBinarySensor(RflinkDevice, BinarySensorEntity, RestoreEntity):
                 self._device_id
             ].remove(tmp_entity)
 
-        if (
-            tmp_entity
-            in self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][self._device_id]
-        ):
-            self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][
-                self._device_id
-            ].remove(tmp_entity)
-
         # Register id and aliases
-        self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][self._device_id].append(
-            self.entity_id
-        )
         self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_SENSOR][self._device_id].append(
             self.entity_id
         )
 
-        if self._group:
-            self.hass.data[DATA_ENTITY_GROUP_LOOKUP][EVENT_KEY_COMMAND][
-                self._device_id
-            ].append(self.entity_id)
         # aliases respond to both normal and group commands (allon/alloff)
         if self._aliases:
             for _id in self._aliases:
                 self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_SENSOR][_id].append(
                     self.entity_id
                 )
-                self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][_id].append(
-                    self.entity_id
-                )
-                self.hass.data[DATA_ENTITY_GROUP_LOOKUP][EVENT_KEY_COMMAND][_id].append(
-                    self.entity_id
-                )
-        # group_aliases only respond to group commands (allon/alloff)
-        if self._group_aliases:
-            for _id in self._group_aliases:
-                self.hass.data[DATA_ENTITY_GROUP_LOOKUP][EVENT_KEY_COMMAND][_id].append(
-                    self.entity_id
-                )
-        # nogroup_aliases only respond to normal commands
-        if self._nogroup_aliases:
-            for _id in self._nogroup_aliases:
-                self.hass.data[DATA_ENTITY_LOOKUP][EVENT_KEY_COMMAND][_id].append(
-                    self.entity_id
-                )
-        self.async_on_remove(
-            async_dispatcher_connect(
-                self.hass, SIGNAL_AVAILABILITY, self._availability_callback
-            )
-        )
-        self.async_on_remove(
-            async_dispatcher_connect(
-                self.hass,
-                SIGNAL_HANDLE_EVENT.format(self.entity_id),
-                self.handle_event_callback,
-            )
-        )
-
-        # Process the initial event now that the entity is created
-        if self._initial_event:
-            self.handle_event_callback(self._initial_event)
+        await super().async_added_to_hass()
+		# Restore RFLink BinarySensor state
+        if (old_state := await self.async_get_last_state()) is not None:
+            if self._off_delay is None:
+                self._state = old_state.state == STATE_ON
+            else:
+                self._state = False
 
     def _handle_event(self, event):
         """Domain specific event handler."""
