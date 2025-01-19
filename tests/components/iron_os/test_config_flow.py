6661533c847be8c65e0df105cc17ae626dada02c
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-01-19 18:48:49 +0000

    test-before-configure

--- tests/components/iron_os/test_config_flow.py
+++ tests/components/iron_os/test_config_flow.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from unittest.mock import AsyncMock, MagicMock
 
+from pynecil import CommunicationError
 import pytest
 
 from homeassistant.components.iron_os import DOMAIN
@@ -16,7 +17,7 @@ from .conftest import DEFAULT_NAME, PINECIL_SERVICE_INFO, USER_INPUT
 from tests.common import MockConfigEntry
 
 
-@pytest.mark.usefixtures("discovery")
+@pytest.mark.usefixtures("discovery", "mock_pynecil")
 async def test_async_step_user(
     hass: HomeAssistant, mock_setup_entry: AsyncMock
 ) -> None:
@@ -34,10 +35,52 @@ async def test_async_step_user(
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == DEFAULT_NAME
     assert result["data"] == {}
+    assert result["result"].unique_id == "c0:ff:ee:c0:ff:ee"
     assert len(mock_setup_entry.mock_calls) == 1
 
 
+@pytest.mark.parametrize(
+    ("raise_error", "text_error"),
+    [
+        (CommunicationError, "cannot_connect"),
+        (Exception, "unknown"),
+    ],
+)
 @pytest.mark.usefixtures("discovery")
+async def test_async_step_user_errors(
+    hass: HomeAssistant,
+    mock_pynecil: AsyncMock,
+    raise_error: Exception,
+    text_error: str,
+) -> None:
+    """Test the user config flow errors."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+
+    mock_pynecil.connect.side_effect = raise_error
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        USER_INPUT,
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {"base": text_error}
+
+    mock_pynecil.connect.side_effect = None
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        USER_INPUT,
+    )
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == DEFAULT_NAME
+    assert result["data"] == {}
+    assert result["result"].unique_id == "c0:ff:ee:c0:ff:ee"
+
+
+@pytest.mark.usefixtures("discovery", "mock_pynecil")
 async def test_async_step_user_device_added_between_steps(
     hass: HomeAssistant, config_entry: MockConfigEntry
 ) -> None:
@@ -73,6 +116,7 @@ async def test_form_no_device_discovered(
     assert result["reason"] == "no_devices_found"
 
 
+@pytest.mark.usefixtures("mock_pynecil")
 async def test_async_step_bluetooth(hass: HomeAssistant) -> None:
     """Test discovery via bluetooth."""
     result = await hass.config_entries.flow.async_init(
@@ -92,6 +136,49 @@ async def test_async_step_bluetooth(hass: HomeAssistant) -> None:
     assert result["result"].unique_id == "c0:ff:ee:c0:ff:ee"
 
 
+@pytest.mark.parametrize(
+    ("raise_error", "text_error"),
+    [
+        (CommunicationError, "cannot_connect"),
+        (Exception, "unknown"),
+    ],
+)
+async def test_async_step_bluetooth_errors(
+    hass: HomeAssistant,
+    mock_pynecil: AsyncMock,
+    raise_error: Exception,
+    text_error: str,
+) -> None:
+    """Test discovery via bluetooth errors."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_BLUETOOTH},
+        data=PINECIL_SERVICE_INFO,
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "bluetooth_confirm"
+
+    mock_pynecil.connect.side_effect = raise_error
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={}
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {"base": text_error}
+
+    mock_pynecil.connect.side_effect = None
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        USER_INPUT,
+    )
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == DEFAULT_NAME
+    assert result["data"] == {}
+    assert result["result"].unique_id == "c0:ff:ee:c0:ff:ee"
+
+
+@pytest.mark.usefixtures("mock_pynecil")
 async def test_async_step_bluetooth_devices_already_setup(
     hass: HomeAssistant, config_entry: AsyncMock
 ) -> None:
@@ -108,7 +195,7 @@ async def test_async_step_bluetooth_devices_already_setup(
     assert result["reason"] == "already_configured"
 
 
-@pytest.mark.usefixtures("discovery")
+@pytest.mark.usefixtures("discovery", "mock_pynecil")
 async def test_async_step_user_setup_replaces_igonored_device(
     hass: HomeAssistant, config_entry_ignored: AsyncMock
 ) -> None:
