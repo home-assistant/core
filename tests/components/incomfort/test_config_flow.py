b6201897733e978f5bfbc2f10957e5b221e67598
jbouwh <jan@jbsoft.nl>
2025-01-20 08:45:10 +0000

    Add tests for assertion on via device

--- tests/components/incomfort/test_config_flow.py
+++ tests/components/incomfort/test_config_flow.py
@@ -12,6 +12,7 @@ from homeassistant.config_entries import SOURCE_DHCP, SOURCE_USER
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.service_info.dhcp import DhcpServiceInfo
 
 from .conftest import MOCK_CONFIG
@@ -126,9 +127,14 @@ async def test_form_validation(
 
 
 async def test_dhcp_flow_simple(
-    hass: HomeAssistant, mock_setup_entry: AsyncMock, mock_incomfort: MagicMock
+    hass: HomeAssistant,
+    mock_incomfort: MagicMock,
+    device_registry: dr.DeviceRegistry,
 ) -> None:
-    """Test dhcp flow for gateway with authentication."""
+    """Test dhcp flow for older gateway without authentication needed.
+
+    Assert on the creation of the gateway device, climate and boiler devices.
+    """
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": SOURCE_DHCP}, data=DHCP_SERVICE_INFO
     )
@@ -140,11 +146,37 @@ async def test_dhcp_flow_simple(
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["data"] == {"host": "192.168.1.12"}
 
+    entry_id = result["result"].entry_id
+
+    await hass.async_block_till_done(wait_background_tasks=True)
+
+    # Check the gateway device is discovered
+    gateway_device = device_registry.async_get_device(
+        identifiers={(DOMAIN, dr.format_mac(DHCP_SERVICE_INFO.macaddress))}
+    )
+    assert gateway_device is not None
+    assert gateway_device.name == "RFGateway"
+    assert gateway_device.manufacturer == "Intergas"
+    assert gateway_device.connections == {("mac", "00:04:a3:de:ad:ff")}
+
+    devices = device_registry.devices.get_devices_for_config_entry_id(entry_id)
+    assert len(devices) == 3
+    boiler_device = device_registry.async_get_device(
+        identifiers={(DOMAIN, "c0ffeec0ffee")}
+    )
+    assert boiler_device.via_device_id == gateway_device.id
+    assert boiler_device is not None
+    climate_device = device_registry.async_get_device(
+        identifiers={(DOMAIN, "c0ffeec0ffee_1")}
+    )
+    assert climate_device is not None
+    assert climate_device.via_device_id == gateway_device.id
+
 
 async def test_dhcp_flow_wih_auth(
     hass: HomeAssistant, mock_setup_entry: AsyncMock, mock_incomfort: MagicMock
 ) -> None:
-    """Test dhcp flow for older gateway without authentication needed."""
+    """Test dhcp flow for with authentication."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": SOURCE_DHCP}, data=DHCP_SERVICE_INFO
     )
