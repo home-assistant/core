547034a86393d09caa4aacb4bba7588c42747000
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2025-01-21 16:19:40 +0100

    Add test to ensure init triggers reauth

--- tests/components/homewizard/test_init.py
+++ tests/components/homewizard/test_init.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from unittest.mock import MagicMock
 
 from freezegun.api import FrozenDateTimeFactory
-from homewizard_energy.errors import DisabledError
+from homewizard_energy.errors import DisabledError, UnauthorizedError
 import pytest
 
 from homeassistant.components.homewizard.const import DOMAIN
@@ -89,6 +89,31 @@ async def test_load_detect_api_disabled(
     assert "context" in flow
     assert flow["context"].get("source") == SOURCE_REAUTH
     assert flow["context"].get("entry_id") == mock_config_entry.entry_id
+
+
+async def test_load_detect_invalid_token(
+    hass: HomeAssistant,
+    mock_config_entry_v2: MockConfigEntry,
+    mock_homewizardenergy_v2: MagicMock,
+) -> None:
+    """Test setup detects invalid token."""
+    mock_homewizardenergy_v2.combined.side_effect = UnauthorizedError()
+    mock_config_entry_v2.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry_v2.entry_id)
+    await hass.async_block_till_done()
+
+    assert mock_config_entry_v2.state is ConfigEntryState.SETUP_ERROR
+
+    flows = hass.config_entries.flow.async_progress()
+    assert len(flows) == 1
+
+    flow = flows[0]
+    assert flow.get("step_id") == "reauth_confirm"
+    assert flow.get("handler") == DOMAIN
+
+    assert "context" in flow
+    assert flow["context"].get("source") == SOURCE_REAUTH
+    assert flow["context"].get("entry_id") == mock_config_entry_v2.entry_id
 
 
 @pytest.mark.usefixtures("mock_homewizardenergy")
