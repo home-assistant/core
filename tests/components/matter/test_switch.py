a991642665ffd39b5cc4724179d439d00afe97c4
Lukas Cerveny <cerv.luk@gmail.com>
2025-01-21 12:11:03 +0100

    Implement thorough Child lock testing

--- tests/components/matter/test_switch.py
+++ tests/components/matter/test_switch.py
@@ -125,19 +125,39 @@ async def test_eve_thermo_child_lock(
     assert state.state == "off"
     # name should be derived from description attribute
     assert state.attributes["friendly_name"] == "Eve Thermo Child lock"
+    # test attribute changes
     set_node_attribute(matter_node, 1, 516, 1, 1)
     await trigger_subscription_callback(hass, matter_client)
     state = hass.states.get("switch.eve_thermo_child_lock")
     assert state.state == "on"
+    set_node_attribute(matter_node, 1, 516, 1, 0)
+    await trigger_subscription_callback(hass, matter_client)
+    state = hass.states.get("switch.eve_thermo_child_lock")
+    assert state.state == "off"
     # test switch service
+    await hass.services.async_call(
+        "switch",
+        "turn_on",
+        {"entity_id": "switch.eve_thermo_child_lock"},
+        blocking=True,
+    )
+    assert matter_client.write_attribute.call_count == 1
+    assert matter_client.write_attribute.call_args_list[0] == call(
+        node_id=matter_node.node_id,
+        attribute_path=create_attribute_path_from_attribute(
+            endpoint_id=1,
+            attribute=clusters.ThermostatUserInterfaceConfiguration.Attributes.KeypadLockout,
+        ),
+        value=1,
+    )
     await hass.services.async_call(
         "switch",
         "turn_off",
         {"entity_id": "switch.eve_thermo_child_lock"},
         blocking=True,
     )
-    assert matter_client.write_attribute.call_count == 1
-    assert matter_client.write_attribute.call_args_list[0] == call(
+    assert matter_client.write_attribute.call_count == 2
+    assert matter_client.write_attribute.call_args_list[1] == call(
         node_id=matter_node.node_id,
         attribute_path=create_attribute_path_from_attribute(
             endpoint_id=1,
