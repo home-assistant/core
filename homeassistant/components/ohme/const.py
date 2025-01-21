9eee3a6c4e92d64a324acc73374eefc02c4e668a
Daniel Raper <me@danr.uk>
2025-01-21 22:16:30 +0000

    Initial number platform implementation

--- homeassistant/components/ohme/const.py
+++ homeassistant/components/ohme/const.py
@@ -3,4 +3,4 @@
 from homeassistant.const import Platform
 
 DOMAIN = "ohme"
-PLATFORMS = [Platform.BUTTON, Platform.SENSOR, Platform.SWITCH]
+PLATFORMS = [Platform.BUTTON, Platform.NUMBER, Platform.SENSOR, Platform.SWITCH]
