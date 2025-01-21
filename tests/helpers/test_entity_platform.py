1fd4229b4d2fa6402c36e6a5b24a43f3d590ef5c
epenet <6771947+epenet@users.noreply.github.com>
2025-01-21 07:05:45 +0000

    Prefer from...import...as over import...as in core tests

--- tests/helpers/test_entity_platform.py
+++ tests/helpers/test_entity_platform.py
@@ -38,7 +38,7 @@ from homeassistant.helpers.entity_component import (
 )
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-import homeassistant.util.dt as dt_util
+from homeassistant.util import dt as dt_util
 
 from tests.common import (
     MockConfigEntry,
