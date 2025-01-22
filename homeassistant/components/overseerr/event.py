fc8201d6df190d83659e442839091a36b80e2065
Joostlek <joostlek@outlook.com>
2025-01-22 15:58:19 +0100

    Only add Overseerr event if we are push based

--- homeassistant/components/overseerr/event.py
+++ homeassistant/components/overseerr/event.py
@@ -4,11 +4,13 @@ from dataclasses import dataclass
 from typing import Any
 
 from homeassistant.components.event import EventEntity, EventEntityDescription
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from . import EVENT_KEY
+from . import DOMAIN, EVENT_KEY
 from .coordinator import OverseerrConfigEntry, OverseerrCoordinator
 from .entity import OverseerrEntity
 
@@ -47,9 +49,16 @@ async def async_setup_entry(
     """Set up Overseerr sensor entities based on a config entry."""
 
     coordinator = entry.runtime_data
-    async_add_entities(
-        OverseerrEvent(coordinator, description) for description in EVENTS
+    ent_reg = er.async_get(hass)
+
+    event_entities_setup_before = ent_reg.async_get_entity_id(
+        Platform.EVENT, DOMAIN, f"{entry.entry_id}-media"
     )
+
+    if coordinator.push or event_entities_setup_before:
+        async_add_entities(
+            OverseerrEvent(coordinator, description) for description in EVENTS
+        )
 
 
 class OverseerrEvent(OverseerrEntity, EventEntity):
@@ -88,7 +97,7 @@ class OverseerrEvent(OverseerrEntity, EventEntity):
     @property
     def available(self) -> bool:
         """Return True if entity is available."""
-        return self._attr_available
+        return self._attr_available and self.coordinator.push
 
 
 def parse_event(event: dict[str, Any], nullable_fields: list[str]) -> dict[str, Any]:
