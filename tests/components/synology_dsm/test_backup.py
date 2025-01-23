30dfabea20c35d7a7ed2db95c966aad7a4a2b77f
mib1185 <mail@mib85.de>
2025-01-23 18:24:52 +0000

    fix tests

--- tests/components/synology_dsm/test_backup.py
+++ tests/components/synology_dsm/test_backup.py
@@ -221,6 +221,8 @@ async def test_agents_list_backups_error(
         "backups": [],
         "last_attempted_automatic_backup": None,
         "last_completed_automatic_backup": None,
+        "next_automatic_backup": None,
+        "next_automatic_backup_additional": False,
     }
 
 
