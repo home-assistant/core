c0bc38fbf20adc132bbae923dad790ff8735633a
Erik Montnemery <erik@montnemery.com>
2025-01-23 19:49:10 +0100

    Add config subentry support to device registry (#128157)
    * Add config subentry support to device registry
    
    * Apply suggestions from code review
    
    * Update syrupy serializer
    
    * Update snapshots
    
    * Address review comments
    
    * Allow a device to be connected to no or a single subentry of a config entry
    
    * Update snapshots
    
    * Revert "Allow a device to be connected to no or a single subentry of a config entry"
    
    This reverts commit ec6f613151cb4a806b7961033c004b71b76510c2.
    
    * Update test snapshots
    
    * Bump release version in comments
    
    * Rename config_subentries to config_entries_subentries

--- script/hassfest/translations.py
+++ script/hassfest/translations.py
@@ -285,7 +285,7 @@ def gen_strings_schema(config: Config, integration: Integration) -> vol.Schema:
                     "user" if integration.integration_type == "helper" else None
                 ),
             ),
-            vol.Optional("config_subentries"): cv.schema_with_slug_keys(
+            vol.Optional("config_entries_subentries"): cv.schema_with_slug_keys(
                 gen_data_entry_schema(
                     config=config,
                     integration=integration,
