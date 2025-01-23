04add729e9601bff40bc89a5f2eec0d866bcd598
Claudio Ruggeri - CR-Tech <41435902+crug80@users.noreply.github.com>
2025-01-23 22:12:02 +0100

    Fix slave id equal to 0 (#136263)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/modbus/modbus.py
+++ homeassistant/components/modbus/modbus.py
@@ -378,7 +378,9 @@ class ModbusHub:
         self, slave: int | None, address: int, value: int | list[int], use_call: str
     ) -> ModbusPDU | None:
         """Call sync. pymodbus."""
-        kwargs: dict[str, Any] = {"slave": slave} if slave else {}
+        kwargs: dict[str, Any] = (
+            {ATTR_SLAVE: slave} if slave is not None else {ATTR_SLAVE: 1}
+        )
         entry = self._pb_request[use_call]
         kwargs[entry.value_attr_name] = value
         try:
