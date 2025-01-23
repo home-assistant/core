b1bb6d2678914ce3ce8408c008a06578354dfef0
Steven B. <51370195+sdb9696@users.noreply.github.com>
2025-01-23 18:51:56 +0000

    Fix tplink deprecated entity cleanup (#136160)

--- tests/components/tplink/test_init.py
+++ tests/components/tplink/test_init.py
@@ -1052,6 +1052,10 @@ async def test_automatic_module_device_addition_and_removal(
         ip_address=IP_ADDRESS3,
         mac=MAC_ADDRESS3,
     )
+    # Set the parent property for the dynamic children as mock_device only sets
+    # it on initialization
+    for child in children.values():
+        child.parent = mock_device
 
     with override_side_effect(mock_connect["connect"], lambda *_, **__: mock_device):
         mock_camera_config_entry.add_to_hass(hass)
@@ -1150,3 +1154,73 @@ async def test_automatic_module_device_addition_and_removal(
         )
         assert device_entry
         assert device_entry.via_device_id == parent_device.id
+
+
+async def test_automatic_device_addition_does_not_remove_disabled_default(
+    hass: HomeAssistant,
+    mock_camera_config_entry: MockConfigEntry,
+    mock_connect: AsyncMock,
+    mock_discovery: AsyncMock,
+    entity_registry: er.EntityRegistry,
+    device_registry: dr.DeviceRegistry,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test for automatic device addition does not remove disabled default entities."""
+
+    features = ["ssid", "signal_level"]
+    children = {
+        f"child{index}": _mocked_device(
+            alias=f"child {index}",
+            features=features,
+            device_id=f"child{index}",
+        )
+        for index in range(1, 5)
+    }
+
+    mock_device = _mocked_device(
+        alias="hub",
+        children=[children["child1"], children["child2"]],
+        features=features,
+        device_type=DeviceType.Hub,
+        device_id="hub_parent",
+        ip_address=IP_ADDRESS3,
+        mac=MAC_ADDRESS3,
+    )
+    # Set the parent property for the dynamic children as mock_device only sets
+    # it on initialization
+    for child in children.values():
+        child.parent = mock_device
+
+    with override_side_effect(mock_connect["connect"], lambda *_, **__: mock_device):
+        mock_camera_config_entry.add_to_hass(hass)
+        await hass.config_entries.async_setup(mock_camera_config_entry.entry_id)
+        await hass.async_block_till_done()
+
+    def check_entities(entity_id_device):
+        entity_id = f"sensor.{entity_id_device}_signal_level"
+        state = hass.states.get(entity_id)
+        assert state
+        reg_ent = entity_registry.async_get(entity_id)
+        assert reg_ent
+        assert reg_ent.disabled is False
+
+        entity_id = f"sensor.{entity_id_device}_ssid"
+        state = hass.states.get(entity_id)
+        assert state is None
+        reg_ent = entity_registry.async_get(entity_id)
+        assert reg_ent
+        assert reg_ent.disabled is True
+        assert reg_ent.disabled_by is er.RegistryEntryDisabler.INTEGRATION
+
+    check_entities("hub")
+    for child_id in (1, 2):
+        check_entities(f"child_{child_id}")
+
+    # Add child devices
+    mock_device.children = [children["child1"], children["child2"], children["child3"]]
+    freezer.tick(5)
+    async_fire_time_changed(hass)
+
+    check_entities("hub")
+    for child_id in (1, 2, 3):
+        check_entities(f"child_{child_id}")
