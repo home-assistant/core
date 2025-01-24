a79c24d39d01645d020ae21b50bd43aea1ea6764
epenet <6771947+epenet@users.noreply.github.com>
2025-01-24 13:46:17 +0000

    Use runtime_data in dexcom

--- homeassistant/components/dexcom/__init__.py
+++ homeassistant/components/dexcom/__init__.py
@@ -2,16 +2,15 @@
 
 from pydexcom import AccountError, Dexcom, SessionError
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
-from .const import CONF_SERVER, DOMAIN, PLATFORMS, SERVER_OUS
-from .coordinator import DexcomCoordinator
+from .const import CONF_SERVER, PLATFORMS, SERVER_OUS
+from .coordinator import DexcomConfigEntry, DexcomCoordinator
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: DexcomConfigEntry) -> bool:
     """Set up Dexcom from a config entry."""
     try:
         dexcom = await hass.async_add_executor_job(
@@ -28,15 +27,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator = DexcomCoordinator(hass, entry=entry, dexcom=dexcom)
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    entry.runtime_data = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: DexcomConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
