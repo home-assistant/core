1830634370252e1fe3c0f606db898b28525b0ca0
epenet <6771947+epenet@users.noreply.github.com>
2025-01-24 14:37:49 +0000

    Clarify

--- homeassistant/components/dynalite/services.py
+++ homeassistant/components/dynalite/services.py
@@ -7,6 +7,7 @@ import voluptuous as vol
 from homeassistant.core import HomeAssistant, ServiceCall, callback
 from homeassistant.helpers import config_validation as cv
 
+from .bridge import DynaliteBridge
 from .const import (
     ATTR_AREA,
     ATTR_CHANNEL,
@@ -17,47 +18,62 @@ from .const import (
     SERVICE_REQUEST_CHANNEL_LEVEL,
 )
 
-_SERVICES = {
-    SERVICE_REQUEST_AREA_PRESET: vol.Schema(
-        {
-            vol.Optional(ATTR_HOST): cv.string,
-            vol.Required(ATTR_AREA): int,
-            vol.Optional(ATTR_CHANNEL): int,
-        }
-    ),
-    SERVICE_REQUEST_CHANNEL_LEVEL: vol.Schema(
-        {
-            vol.Optional(ATTR_HOST): cv.string,
-            vol.Required(ATTR_AREA): int,
-            vol.Required(ATTR_CHANNEL): int,
-        }
-    ),
-}
 
-
-async def _run_dynalite_service_call(service_call: ServiceCall) -> None:
-    data = service_call.data
-    host = data.get(ATTR_HOST, "")
+@callback
+def _get_bridges(service_call: ServiceCall) -> list[DynaliteBridge]:
+    host = service_call.data.get(ATTR_HOST, "")
     bridges = [
         bridge
         for bridge in service_call.hass.data[DOMAIN].values()
         if not host or bridge.host == host
     ]
     LOGGER.debug("Selected bridged for service call: %s", bridges)
-    if service_call.service == SERVICE_REQUEST_AREA_PRESET:
-        bridge_attr = "request_area_preset"
-    elif service_call.service == SERVICE_REQUEST_CHANNEL_LEVEL:
-        bridge_attr = "request_channel_level"
+    return bridges
+
+
+async def _request_area_preset(service_call: ServiceCall) -> None:
+    bridges = _get_bridges(service_call)
+    data = service_call.data
     for bridge in bridges:
-        getattr(bridge.dynalite_devices, bridge_attr)(
+        bridge.dynalite_devices.request_area_preset(
             data[ATTR_AREA], data.get(ATTR_CHANNEL)
+        )
+
+
+async def _request_channel_level(service_call: ServiceCall) -> None:
+    bridges = _get_bridges(service_call)
+    data = service_call.data
+    for bridge in bridges:
+        bridge.dynalite_devices.request_channel_level(
+            data[ATTR_AREA], data[ATTR_CHANNEL]
         )
 
 
 @callback
 def setup_services(hass: HomeAssistant) -> None:
     """Set up the Dynalite platform."""
-    for service_name, service_schema in _SERVICES.items():
-        hass.services.async_register(
-            DOMAIN, service_name, _run_dynalite_service_call, service_schema
-        )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_REQUEST_AREA_PRESET,
+        _request_area_preset,
+        vol.Schema(
+            {
+                vol.Optional(ATTR_HOST): cv.string,
+                vol.Required(ATTR_AREA): int,
+                vol.Optional(ATTR_CHANNEL): int,
+            }
+        ),
+    )
+
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_REQUEST_CHANNEL_LEVEL,
+        _request_channel_level,
+        vol.Schema(
+            {
+                vol.Optional(ATTR_HOST): cv.string,
+                vol.Required(ATTR_AREA): int,
+                vol.Required(ATTR_CHANNEL): int,
+            }
+        ),
+    )
