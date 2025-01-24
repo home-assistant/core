7754a11c2c20719f096dd306feee242d1e68ec61
epenet <6771947+epenet@users.noreply.github.com>
2025-01-24 14:26:37 +0000

    Schema is different

--- homeassistant/components/dynalite/services.py
+++ homeassistant/components/dynalite/services.py
@@ -17,7 +17,22 @@ from .const import (
     SERVICE_REQUEST_CHANNEL_LEVEL,
 )
 
-_SERVICES = [SERVICE_REQUEST_AREA_PRESET, SERVICE_REQUEST_CHANNEL_LEVEL]
+_SERVICES = {
+    SERVICE_REQUEST_AREA_PRESET: vol.Schema(
+        {
+            vol.Optional(ATTR_HOST): cv.string,
+            vol.Required(ATTR_AREA): int,
+            vol.Optional(ATTR_CHANNEL): int,
+        }
+    ),
+    SERVICE_REQUEST_CHANNEL_LEVEL: vol.Schema(
+        {
+            vol.Optional(ATTR_HOST): cv.string,
+            vol.Required(ATTR_AREA): int,
+            vol.Required(ATTR_CHANNEL): int,
+        }
+    ),
+}
 
 
 async def _run_dynalite_service_call(service_call: ServiceCall) -> None:
@@ -42,16 +57,7 @@ async def _run_dynalite_service_call(service_call: ServiceCall) -> None:
 @callback
 def setup_services(hass: HomeAssistant) -> None:
     """Set up the Dynalite platform."""
-    for service_name in _SERVICES:
+    for service_name, service_schema in _SERVICES.items():
         hass.services.async_register(
-            DOMAIN,
-            service_name,
-            _run_dynalite_service_call,
-            vol.Schema(
-                {
-                    vol.Optional(ATTR_HOST): cv.string,
-                    vol.Required(ATTR_AREA): int,
-                    vol.Required(ATTR_CHANNEL): int,
-                }
-            ),
+            DOMAIN, service_name, _run_dynalite_service_call, service_schema
         )
