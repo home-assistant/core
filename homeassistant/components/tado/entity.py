a473814988972b7a747962174c3a40962adce4fe
Joostlek <joostlek@outlook.com>
2025-01-24 12:27:56 +0100

    Fix

--- homeassistant/components/tado/entity.py
+++ homeassistant/components/tado/entity.py
@@ -20,7 +20,6 @@ class TadoCoordinatorEntity(CoordinatorEntity[TadoDataUpdateCoordinator]):
 class TadoDeviceEntity(TadoCoordinatorEntity):
     """Base implementation for Tado device."""
 
-
     def __init__(
         self, device_info: dict[str, str], coordinator: TadoDataUpdateCoordinator
     ) -> None:
@@ -42,7 +41,6 @@ class TadoDeviceEntity(TadoCoordinatorEntity):
 
 class TadoHomeEntity(TadoCoordinatorEntity):
     """Base implementation for Tado home."""
-
 
     def __init__(self, coordinator: TadoDataUpdateCoordinator) -> None:
         """Initialize a Tado home."""
