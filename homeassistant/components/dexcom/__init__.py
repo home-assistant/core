a75f1a515434f01cb7135c06c2ffcd1589830f37
epenet <6771947+epenet@users.noreply.github.com>
2025-01-24 14:20:23 +0100

    Move dexcom coordinator to separate module (#136433)

--- homeassistant/components/dexcom/__init__.py
+++ homeassistant/components/dexcom/__init__.py
@@ -1,21 +1,14 @@
 """The Dexcom integration."""
 
-from datetime import timedelta
-import logging
-
-from pydexcom import AccountError, Dexcom, GlucoseReading, SessionError
+from pydexcom import AccountError, Dexcom, SessionError
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import CONF_SERVER, DOMAIN, PLATFORMS, SERVER_OUS
-
-_LOGGER = logging.getLogger(__name__)
-
-SCAN_INTERVAL = timedelta(seconds=180)
+from .coordinator import DexcomCoordinator
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -32,20 +25,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     except SessionError as error:
         raise ConfigEntryNotReady from error
 
-    async def async_update_data():
-        try:
-            return await hass.async_add_executor_job(dexcom.get_current_glucose_reading)
-        except SessionError as error:
-            raise UpdateFailed(error) from error
-
-    coordinator = DataUpdateCoordinator[GlucoseReading](
-        hass,
-        _LOGGER,
-        config_entry=entry,
-        name=DOMAIN,
-        update_method=async_update_data,
-        update_interval=SCAN_INTERVAL,
-    )
+    coordinator = DexcomCoordinator(hass, entry=entry, dexcom=dexcom)
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
