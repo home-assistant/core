67647b68529877d3dc614d789a762e7dd4646184
Elias Wernicke <e.w.1111111119@gmail.com>
2025-01-25 22:37:51 +0000

    add tests

--- tests/components/calendar/test_init.py
+++ tests/components/calendar/test_init.py
@@ -6,20 +6,29 @@ from collections.abc import Generator
 from datetime import timedelta
 from http import HTTPStatus
 from typing import Any
+from unittest.mock import Mock, patch
 
 from freezegun import freeze_time
 import pytest
 from syrupy.assertion import SnapshotAssertion
 import voluptuous as vol
 
-from homeassistant.components.calendar import DOMAIN, SERVICE_GET_EVENTS
-from homeassistant.core import HomeAssistant
+from homeassistant.components import calendar, conversation
+from homeassistant.components.calendar import (
+    DOMAIN,
+    SERVICE_GET_EVENTS,
+    intent as calendar_intent,
+)
+from homeassistant.components.calendar.intent import INTENT_CALENDAR_GET_EVENTS
+from homeassistant.core import HomeAssistant, SupportsResponse
 from homeassistant.exceptions import HomeAssistantError, ServiceNotSupported
+from homeassistant.helpers import intent
 from homeassistant.setup import async_setup_component
 import homeassistant.util.dt as dt_util
 
 from .conftest import MockCalendarEntity, MockConfigEntry
 
+from tests.common import async_mock_service
 from tests.typing import ClientSessionGenerator, WebSocketGenerator
 
 
@@ -53,6 +62,97 @@ async def mock_setup_platform(
     """Fixture to setup platforms used in the test and fixtures are set up in the right order."""
     assert await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
+
+
+@pytest.fixture(autouse=True)
+async def setup_intents(hass: HomeAssistant) -> None:
+    """Set up the intents."""
+    assert await async_setup_component(hass, "homeassistant", {})
+    await calendar_intent.async_setup_intents(hass)
+
+
+async def test_calendar_get_events_intent(hass: HomeAssistant) -> None:
+    """Test the calendar get events intent."""
+    calls = async_mock_service(
+        hass,
+        domain=calendar.DOMAIN,
+        service=calendar.SERVICE_GET_EVENTS,
+        schema=calendar.SERVICE_GET_EVENTS_SCHEMA,
+        response={
+            "calendar.test_calendar": {
+                "events": [
+                    {
+                        "start": "2025-09-17",
+                        "end": "2025-09-18",
+                        "summary": "Home Assistant 12th birthday",
+                        "description": "",
+                    },
+                    {
+                        "start": "2025-09-17T14:00:00-05:00",
+                        "end": "2025-09-18T15:00:00-05:00",
+                        "summary": "Champagne",
+                        "description": "",
+                    },
+                ]
+            }
+        },
+        supports_response=SupportsResponse.ONLY,
+    )
+
+    now = dt_util.now()
+    with (
+        patch.multiple(
+            "homeassistant.util.dt",
+            now=Mock(return_value=now),
+        ),
+        patch(
+            "homeassistant.helpers.intent.async_match_targets",
+            return_value=Mock(
+                is_match=True, states=[Mock(entity_id="calendar.test_calendar")]
+            ),
+        ),
+    ):
+        response = await intent.async_handle(
+            hass,
+            DOMAIN,
+            INTENT_CALENDAR_GET_EVENTS,
+            {
+                "calendar": {"value": "test_calendar"},
+                "range": {"value": "today"},
+            },
+            assistant=conversation.DOMAIN,
+        )
+
+        assert len(calls) == 1
+        call = calls[0]
+        assert call.domain == calendar.DOMAIN
+        assert call.service == calendar.SERVICE_GET_EVENTS
+        assert call.data == {
+            "entity_id": ["calendar.test_calendar"],
+            "start_date_time": now,
+            "end_date_time": dt_util.start_of_local_day() + timedelta(days=1),
+        }
+
+        assert response.speech_slots["events"] == [
+            {
+                "start": "2025-09-17",
+                "end": "2025-09-18",
+                "all_day": True,
+                "summary": "Home Assistant 12th birthday",
+                "description": "",
+                "location": "",
+                "recurring": None,
+            },
+            {
+                "start": "2025-09-17T14:00:00-05:00",
+                "end": "2025-09-18T15:00:00-05:00",
+                "all_day": False,
+                "summary": "Champagne",
+                "description": "",
+                "location": "",
+                "recurring": None,
+            },
+        ]
 
 
 async def test_events_http_api(
