88756d4a88408b3d76da09295d245ad25176cb94
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2025-01-25 11:46:26 +0000

    Use quality scale

--- script/hassfest/quality_scale.py
+++ script/hassfest/quality_scale.py
@@ -499,7 +499,6 @@ INTEGRATIONS_WITHOUT_QUALITY_SCALE_FILE = [
     "http",
     "huawei_lte",
     "hue",
-    "hue_ble",
     "huisbaasje",
     "hunterdouglas_powerview",
     "husqvarna_automower_ble",
