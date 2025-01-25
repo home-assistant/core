e3812fa30ab2ed72f460861771f8f1bde27d8cf7
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-01-25 23:26:53 +0100

    Fix

--- homeassistant/components/flux_led/config_flow.py
+++ homeassistant/components/flux_led/config_flow.py
@@ -299,7 +299,8 @@ class FluxLedConfigFlow(ConfigFlow, domain=DOMAIN):
             # AKA `HF-LPB100-ZJ200`
             return device
         bulb = async_wifi_bulb_for_host(host, discovery=device)
-        bulb.discovery = discovery
+        if discovery is not None:
+            bulb.discovery = discovery
         try:
             await bulb.async_setup(lambda: None)
         finally:
