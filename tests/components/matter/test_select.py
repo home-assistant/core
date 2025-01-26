77e90f326d5ec0247cae29ed04da4cbaf6fb25d6
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-01-26 12:17:17 +0100

    Update test_select.py

--- tests/components/matter/test_select.py
+++ tests/components/matter/test_select.py
@@ -113,6 +113,39 @@ async def test_list_select_entities(
     matter_node: MatterNode,
 ) -> None:
     """Test ListSelect entities are discovered and working from a laundrywasher fixture."""
+    state = hass.states.get("select.laundrywasher_temperature_level")
+    assert state
+    assert state.state == "Colors"
+    assert state.attributes["options"] == ["Cold", "Colors", "Whites"]
+    # Change temperature_level
+    set_node_attribute(matter_node, 1, 86, 4, 0)
+    await trigger_subscription_callback(hass, matter_client)
+    state = hass.states.get("select.laundrywasher_temperature_level")
+    assert state.state == "Cold"
+    # test select option
+    await hass.services.async_call(
+        "select",
+        "select_option",
+        {
+            "entity_id": "select.laundrywasher_temperature_level",
+            "option": "Whites",
+        },
+        blocking=True,
+    )
+    assert matter_client.send_device_command.call_count == 1
+    assert matter_client.send_device_command.call_args == call(
+        node_id=matter_node.node_id,
+        endpoint_id=1,
+        command=clusters.TemperatureControl.Commands.SetTemperature(
+            targetTemperatureLevel=2
+        ),
+    )
+    # test that an invalid value (e.g. 253) leads to an unknown state
+    set_node_attribute(matter_node, 1, 86, 4, 253)
+    await trigger_subscription_callback(hass, matter_client)
+    state = hass.states.get("select.laundrywasher_temperature_level")
+    assert state.state == "unknown"
+
     # SpinSpeedCurrent
     state = hass.states.get("select.laundrywasher_spin_speed")
     assert state
@@ -122,7 +155,7 @@ async def test_list_select_entities(
     await trigger_subscription_callback(hass, matter_client)
     state = hass.states.get("select.laundrywasher_spin_speed")
     assert state.state == "High"
-
+    # test select option
     await hass.services.async_call(
         "select",
         "select_option",
