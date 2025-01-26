b1204701b657e37459bde8b3990f9e82fe4db86b
Andrew Sayre <6730289+andrewsayre@users.noreply.github.com>
2025-01-26 09:41:04 -0600

    Use typed ConfigEntry throughout HEOS (#136569)

--- homeassistant/components/heos/media_player.py
+++ homeassistant/components/heos/media_player.py
@@ -38,9 +38,8 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util.dt import utcnow
 
-from . import HeosConfigEntry
 from .const import DOMAIN as HEOS_DOMAIN
-from .coordinator import HeosCoordinator
+from .coordinator import HeosConfigEntry, HeosCoordinator
 
 PARALLEL_UPDATES = 0
 
