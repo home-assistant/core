b99f3e3c33a5e70bb5b389d970ce025cada40985
Taraman17 <mozilla@adrario.de>
2025-01-27 17:40:34 +0000

    use mock instead of real HomeeNode

--- tests/components/homee/test_cover.py
+++ tests/components/homee/test_cover.py
@@ -2,26 +2,65 @@
 
 from unittest.mock import AsyncMock, MagicMock
 
-from pyHomee import HomeeNode
-
 from homeassistant.components.cover import DOMAIN as COVER_DOMAIN, CoverState
-from homeassistant.components.homee.const import DOMAIN
 from homeassistant.const import ATTR_ENTITY_ID, SERVICE_CLOSE_COVER, SERVICE_OPEN_COVER
 from homeassistant.core import HomeAssistant
 
 from . import setup_integration
 
-from tests.common import MockConfigEntry, load_json_object_fixture
+from tests.common import MockConfigEntry
 
 
-async def test_cover_open(
-    hass: HomeAssistant, mock_homee: AsyncMock, mock_config_entry: MockConfigEntry
+async def test_open_cover(
+    hass: HomeAssistant,
+    cover: AsyncMock,
+    mock_homee: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test opening the cover."""
+    mock_homee.nodes = [cover]
+
+    await setup_integration(hass, mock_config_entry)
+
+    await hass.services.async_call(
+        COVER_DOMAIN,
+        SERVICE_OPEN_COVER,
+        {ATTR_ENTITY_ID: "cover.test_cover"},
+        blocking=True,
+    )
+    mock_homee.set_value.assert_called_once_with(cover.id, 1, 0)
+
+
+async def test_close_cover(
+    hass: HomeAssistant,
+    cover: AsyncMock,
+    mock_homee: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test opening the cover."""
+    mock_homee.nodes = [cover]
+
+    await setup_integration(hass, mock_config_entry)
+
+    await hass.services.async_call(
+        COVER_DOMAIN,
+        SERVICE_CLOSE_COVER,
+        {ATTR_ENTITY_ID: "cover.test_cover"},
+        blocking=True,
+    )
+
+    mock_homee.set_value.assert_called_once_with(cover.id, 1, 1)
+
+
+async def test_cover_positions(
+    hass: HomeAssistant,
+    cover: AsyncMock,
+    mock_homee: MagicMock,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test an open cover."""
     # Cover open, tilt open.
-    cover_json = load_json_object_fixture("cover1.json", DOMAIN)
-    cover_node = HomeeNode(cover_json)
-    mock_homee.nodes = [cover_node]
+    mock_homee.nodes = [cover]
 
     await setup_integration(hass, mock_config_entry)
 
@@ -32,93 +71,32 @@ async def test_cover_open(
     assert attributes.get("current_position") == 100
     assert attributes.get("current_tilt_position") == 100
 
-
-async def test_cover_closed(
-    hass: HomeAssistant, mock_homee: MagicMock, mock_config_entry: MockConfigEntry
-) -> None:
-    """Test a closed cover."""
-    # Cover closed, tilt closed.
-    cover_json = load_json_object_fixture("cover2.json", DOMAIN)
-    cover_node = HomeeNode(cover_json)
-    mock_homee.nodes = [cover_node]
-
-    await setup_integration(hass, mock_config_entry)
+    mock_homee.nodes[0].attributes[0].current_value = 0
+    mock_homee.nodes[0].attributes[1].current_value = 1
+    mock_homee.nodes[0].attributes[2].current_value = 0
+    mock_homee.nodes[0].add_on_changed_listener.call_args_list[0](mock_homee.nodes[0])
 
     assert hass.states.get("cover.test_cover").state == CoverState.CLOSED
     attributes = hass.states.get("cover.test_cover").attributes
     assert attributes.get("current_position") == 0
     assert attributes.get("current_tilt_position") == 0
 
-
-async def test_cover_opening(
-    hass: HomeAssistant, mock_homee: MagicMock, mock_config_entry: MockConfigEntry
-) -> None:
-    """Test an opening cover."""
-    # opening, 75% homee / 25% HA
-    cover_json = load_json_object_fixture("cover3.json", DOMAIN)
-    cover_node = HomeeNode(cover_json)
-    mock_homee.nodes = [cover_node]
-
-    await setup_integration(hass, mock_config_entry)
+    mock_homee.nodes[0].attributes[0].current_value = 75
+    mock_homee.nodes[0].attributes[1].current_value = 3
+    mock_homee.nodes[0].attributes[2].current_value = 75
+    mock_homee.nodes[0].add_on_changed_listener.call_args_list[0](mock_homee.nodes[0])
 
     assert hass.states.get("cover.test_cover").state == CoverState.OPENING
     attributes = hass.states.get("cover.test_cover").attributes
     assert attributes.get("current_position") == 25
     assert attributes.get("current_tilt_position") == 25
 
-
-async def test_cover_closing(
-    hass: HomeAssistant, mock_homee: MagicMock, mock_config_entry: MockConfigEntry
-) -> None:
-    """Test a closing cover."""
-    # closing, 25% homee / 75% HA
-    cover_json = load_json_object_fixture("cover4.json", DOMAIN)
-    cover_node = HomeeNode(cover_json)
-    mock_homee.nodes = [cover_node]
-
-    await setup_integration(hass, mock_config_entry)
+    mock_homee.nodes[0].attributes[0].current_value = 25
+    mock_homee.nodes[0].attributes[1].current_value = 4
+    mock_homee.nodes[0].attributes[2].current_value = 25
+    mock_homee.nodes[0].add_on_changed_listener.call_args_list[0](mock_homee.nodes[0])
 
     assert hass.states.get("cover.test_cover").state == CoverState.CLOSING
     attributes = hass.states.get("cover.test_cover").attributes
     assert attributes.get("current_position") == 75
     assert attributes.get("current_tilt_position") == 74
-
-
-async def test_open_cover(
-    hass: HomeAssistant, mock_homee: MagicMock, mock_config_entry: MockConfigEntry
-) -> None:
-    """Test opening the cover."""
-    # Cover closed, tilt closed.
-    cover_json = load_json_object_fixture("cover2.json", DOMAIN)
-    cover_node = HomeeNode(cover_json)
-    mock_homee.nodes = [cover_node]
-
-    await setup_integration(hass, mock_config_entry)
-
-    await hass.services.async_call(
-        COVER_DOMAIN,
-        SERVICE_OPEN_COVER,
-        {ATTR_ENTITY_ID: "cover.test_cover"},
-        blocking=True,
-    )
-    mock_homee.set_value.assert_called_once_with(cover_node.id, 1, 0)
-
-
-async def test_close_cover(
-    hass: HomeAssistant, mock_homee: MagicMock, mock_config_entry: MockConfigEntry
-) -> None:
-    """Test opening the cover."""
-    # Cover open, tilt open.
-    cover_json = load_json_object_fixture("cover1.json", DOMAIN)
-    cover_node = HomeeNode(cover_json)
-    mock_homee.nodes = [cover_node]
-
-    await setup_integration(hass, mock_config_entry)
-
-    await hass.services.async_call(
-        COVER_DOMAIN,
-        SERVICE_CLOSE_COVER,
-        {ATTR_ENTITY_ID: "cover.test_cover"},
-        blocking=True,
-    )
-    mock_homee.set_value.assert_called_once_with(cover_node.id, 1, 1)
