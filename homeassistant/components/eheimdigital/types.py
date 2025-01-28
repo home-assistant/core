65fbf830fdbbedcdd7d0f56542770168b6e0adfb
autinerd <27780930+autinerd@users.noreply.github.com>
2025-01-28 20:00:08 +0100

    Add test and coverage exclusion

--- homeassistant/components/eheimdigital/types.py
+++ homeassistant/components/eheimdigital/types.py
@@ -1,26 +1,27 @@
 """Types for EHEIM Digital."""
 
-from typing import Literal, Protocol, overload
+from typing import TYPE_CHECKING, Literal, Protocol, overload  # pragma: no cover
 
-from eheimdigital.device import EheimDigitalDevice
+from eheimdigital.device import EheimDigitalDevice  # pragma: no cover
 
-from .entity import EheimDigitalEntity
+from .entity import EheimDigitalEntity  # pragma: no cover
 
+if TYPE_CHECKING:
 
-class AsyncSetupDeviceEntitiesCallback(Protocol):
-    """Callback to setup device entities."""
+    class AsyncSetupDeviceEntitiesCallback(Protocol):
+        """Callback to setup device entities."""
 
-    @overload
-    def __call__(
-        self, device_address: str, *, return_entities: Literal[False]
-    ) -> None: ...
+        @overload
+        def __call__(
+            self, device_address: str, *, return_entities: Literal[False]
+        ) -> None: ...
 
-    @overload
-    def __call__(
-        self, device_address: str, *, return_entities: Literal[True]
-    ) -> list[EheimDigitalEntity[EheimDigitalDevice]]: ...
+        @overload
+        def __call__(
+            self, device_address: str, *, return_entities: Literal[True]
+        ) -> list[EheimDigitalEntity[EheimDigitalDevice]]: ...
 
-    def __call__(
-        self, device_address: str, *, return_entities: bool
-    ) -> None | list[EheimDigitalEntity[EheimDigitalDevice]]:
-        """Set up EHEIM Digital device entities."""
+        def __call__(
+            self, device_address: str, *, return_entities: bool
+        ) -> None | list[EheimDigitalEntity[EheimDigitalDevice]]:
+            """Set up EHEIM Digital device entities."""
