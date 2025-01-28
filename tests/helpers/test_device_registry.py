8fb13aff44c6d704ff86a93db0af087550acbee3
Erik Montnemery <erik@montnemery.com>
2025-01-28 04:49:18 +0100

    Clean up subentries from deleted devices when removing config entry (#136669)
    * Clean up subentries from deleted devices when removing config entry
    
    * Move

--- tests/helpers/test_device_registry.py
+++ tests/helpers/test_device_registry.py
@@ -1423,6 +1423,10 @@ async def test_removing_config_entries(
     assert entry.id == entry2.id
     assert entry.id != entry3.id
     assert entry2.config_entries == {config_entry_1.entry_id, config_entry_2.entry_id}
+    assert entry2.config_entries_subentries == {
+        config_entry_1.entry_id: {None},
+        config_entry_2.entry_id: {None},
+    }
 
     device_registry.async_clear_config_entry(config_entry_1.entry_id)
     entry = device_registry.async_get_device(identifiers={("bridgeid", "0123")})
@@ -1431,6 +1435,7 @@ async def test_removing_config_entries(
     )
 
     assert entry.config_entries == {config_entry_2.entry_id}
+    assert entry.config_entries_subentries == {config_entry_2.entry_id: {None}}
     assert entry3_removed is None
 
     await hass.async_block_till_done()
@@ -1507,6 +1512,10 @@ async def test_deleted_device_removing_config_entries(
     assert entry.id == entry2.id
     assert entry.id != entry3.id
     assert entry2.config_entries == {config_entry_1.entry_id, config_entry_2.entry_id}
+    assert entry2.config_entries_subentries == {
+        config_entry_1.entry_id: {None},
+        config_entry_2.entry_id: {None},
+    }
 
     device_registry.async_remove_device(entry.id)
     device_registry.async_remove_device(entry3.id)
@@ -1544,10 +1553,16 @@ async def test_deleted_device_removing_config_entries(
     device_registry.async_clear_config_entry(config_entry_1.entry_id)
     assert len(device_registry.devices) == 0
     assert len(device_registry.deleted_devices) == 2
+    entry = device_registry.deleted_devices.get_entry({("bridgeid", "0123")}, None)
+    assert entry.config_entries == {config_entry_2.entry_id}
+    assert entry.config_entries_subentries == {config_entry_2.entry_id: {None}}
 
     device_registry.async_clear_config_entry(config_entry_2.entry_id)
     assert len(device_registry.devices) == 0
     assert len(device_registry.deleted_devices) == 2
+    entry = device_registry.deleted_devices.get_entry({("bridgeid", "0123")}, None)
+    assert entry.config_entries == set()
+    assert entry.config_entries_subentries == {}
 
     # No event when a deleted device is purged
     await hass.async_block_till_done()
