be219202faff62e42b114f5c593eef2d5737d04c
puddly <32534428+puddly@users.noreply.github.com>
2025-01-28 23:58:38 -0500

    Unit test `register_firmware_info_callback` (otbr)

--- tests/components/otbr/test_homeassistant_hardware.py
+++ tests/components/otbr/test_homeassistant_hardware.py
@@ -1,9 +1,13 @@
 """Test Home Assistant Hardware platform for OTBR."""
 
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, Mock, call, patch
 
+import pytest
 import voluptuous as vol
 
+from homeassistant.components.homeassistant_hardware.helpers import (
+    register_firmware_info_callback,
+)
 from homeassistant.components.homeassistant_hardware.util import (
     ApplicationType,
     FirmwareInfo,
@@ -14,10 +18,12 @@ from homeassistant.components.otbr.homeassistant_hardware import async_get_firmw
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.setup import async_setup_component
+
+from . import TEST_COPROCESSOR_VERSION
 
 from tests.common import MockConfigEntry
-
-COPROCESSOR_VERSION = "OPENTHREAD/thread-reference-20200818-1740-g33cc75ed3; NRF52840; Jun 2 2022 14:25:49"
+from tests.test_util.aiohttp import AiohttpClientMocker
 
 
 async def test_get_firmware_info(hass: HomeAssistant) -> None:
@@ -37,7 +43,7 @@ async def test_get_firmware_info(hass: HomeAssistant) -> None:
     otbr.mock_state(hass, ConfigEntryState.LOADED)
 
     otbr.runtime_data = AsyncMock()
-    otbr.runtime_data.get_coprocessor_version.return_value = COPROCESSOR_VERSION
+    otbr.runtime_data.get_coprocessor_version.return_value = TEST_COPROCESSOR_VERSION
 
     with (
         patch(
@@ -54,7 +60,7 @@ async def test_get_firmware_info(hass: HomeAssistant) -> None:
     assert fw_info == FirmwareInfo(
         device="/dev/ttyUSB1",
         firmware_type=ApplicationType.SPINEL,
-        firmware_version=COPROCESSOR_VERSION,
+        firmware_version=TEST_COPROCESSOR_VERSION,
         source="otbr",
         owners=[
             OwningIntegration(config_entry_id=otbr.entry_id),
@@ -96,7 +102,7 @@ async def test_get_firmware_info_bad_addon(hass: HomeAssistant) -> None:
     otbr.mock_state(hass, ConfigEntryState.LOADED)
 
     otbr.runtime_data = AsyncMock()
-    otbr.runtime_data.get_coprocessor_version.return_value = COPROCESSOR_VERSION
+    otbr.runtime_data.get_coprocessor_version.return_value = TEST_COPROCESSOR_VERSION
 
     with (
         patch(
@@ -113,7 +119,7 @@ async def test_get_firmware_info_bad_addon(hass: HomeAssistant) -> None:
     assert fw_info == FirmwareInfo(
         device="/dev/ttyUSB1",
         firmware_type=ApplicationType.SPINEL,
-        firmware_version=COPROCESSOR_VERSION,
+        firmware_version=TEST_COPROCESSOR_VERSION,
         source="otbr",
         owners=[
             OwningIntegration(config_entry_id=otbr.entry_id),
@@ -157,3 +163,63 @@ async def test_get_firmware_info_no_coprocessor_version(hass: HomeAssistant) -> 
             OwningIntegration(config_entry_id=otbr.entry_id),
         ],
     )
+
+
+@pytest.mark.parametrize(
+    ("version", "expected_version"),
+    [
+        (TEST_COPROCESSOR_VERSION, TEST_COPROCESSOR_VERSION),
+        (HomeAssistantError(), None),
+    ],
+)
+async def test_hardware_firmware_info_provider_notification(
+    hass: HomeAssistant,
+    version: str | Exception,
+    expected_version: str | None,
+    get_active_dataset_tlvs: AsyncMock,
+    get_border_agent_id: AsyncMock,
+    get_extended_address: AsyncMock,
+    get_coprocessor_version: AsyncMock,
+    aioclient_mock: AiohttpClientMocker,
+) -> None:
+    """Test that the OTBR provides hardware and firmware information."""
+    otbr = MockConfigEntry(
+        domain="otbr",
+        unique_id="some_unique_id",
+        data={
+            "device": "/dev/ttyUSB1",
+            "url": "http://openthread_border_router:8888",
+        },
+        version=1,
+        minor_version=2,
+    )
+    otbr.add_to_hass(hass)
+
+    await async_setup_component(hass, "homeassistant_hardware", {})
+
+    callback = Mock()
+    register_firmware_info_callback(hass, "/dev/ttyUSB1", callback)
+
+    with (
+        patch(
+            "homeassistant.components.otbr.homeassistant_hardware.is_hassio",
+            return_value=True,
+        ),
+    ):
+        get_coprocessor_version.side_effect = version
+        await hass.config_entries.async_setup(otbr.entry_id)
+
+    assert callback.mock_calls == [
+        call(
+            FirmwareInfo(
+                device="/dev/ttyUSB1",
+                firmware_type=ApplicationType.SPINEL,
+                firmware_version=expected_version,
+                source="otbr",
+                owners=[
+                    OwningIntegration(config_entry_id=otbr.entry_id),
+                    OwningAddon(slug="openthread_border_router"),
+                ],
+            )
+        )
+    ]
