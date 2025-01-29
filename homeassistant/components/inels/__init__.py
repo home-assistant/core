425f168526edd8976d7f9ad4cd0ef08707646e9a
epdevlab <epdevlab@gmail.com>
2025-01-29 10:40:13 +0000

    implement "runtime-data" rule

--- homeassistant/components/inels/__init__.py
+++ homeassistant/components/inels/__init__.py
@@ -2,9 +2,10 @@
 
 from __future__ import annotations
 
-from typing import Any
+from dataclasses import dataclass, field
 
 from inelsmqtt import InelsMqtt
+from inelsmqtt.devices import Device
 from inelsmqtt.discovery import InelsDiscovery
 
 from homeassistant.config_entries import ConfigEntry
@@ -13,25 +14,41 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 
-from .const import BROKER, BROKER_CONFIG, DEVICES, DOMAIN, LOGGER, OLD_ENTITIES
+from .const import LOGGER
+
+type InelsConfigEntry = ConfigEntry[InelsData]
 
 PLATFORMS: list[Platform] = [
     Platform.SWITCH,
 ]
 
 
-async def async_remove_old_entities(hass: HomeAssistant, entry: ConfigEntry) -> None:
+@dataclass
+class InelsData:
+    """Represents the data structure for INELS runtime data."""
+
+    mqtt: InelsMqtt
+    devices: list[Device] = field(default_factory=list)
+    old_entities: dict[Platform, list[str]] = field(default_factory=dict)
+
+
+async def async_remove_old_entities(
+    hass: HomeAssistant, entry: InelsConfigEntry
+) -> None:
     """Remove old entities that aren't being used."""
     entity_registry = er.async_get(hass)
-    remaining_entries = hass.data[DOMAIN][entry.entry_id][OLD_ENTITIES]
+    remaining_entries = entry.runtime_data.old_entities
 
     for platform in remaining_entries:
         for entity_id in remaining_entries[platform]:
             entity_registry.async_remove(entity_id)
 
+        # Clear the list for the platform after removing entities
+        remaining_entries[platform].clear()
+
 
 async def async_remove_devices_with_no_entities(
-    hass: HomeAssistant, entry: ConfigEntry
+    hass: HomeAssistant, entry: InelsConfigEntry
 ) -> None:
     """Remove devices with no entities."""
     device_registry = dr.async_get(hass)
@@ -52,26 +69,22 @@ async def async_remove_devices_with_no_entities(
             device_registry.async_remove_device(device_id=device_id)
 
 
-async def _async_config_entry_updated(hass: HomeAssistant, entry: ConfigEntry) -> None:
+async def _async_config_entry_updated(
+    hass: HomeAssistant, entry: InelsConfigEntry
+) -> None:
     """Call when config entry being updated."""
 
-    client: InelsMqtt = hass.data[BROKER]
-
-    await hass.async_add_executor_job(client.disconnect)
+    await hass.async_add_executor_job(entry.runtime_data.mqtt.disconnect)
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: InelsConfigEntry) -> bool:
     """Set up iNELS from a config entry."""
 
-    inels_data: dict[str, Any] = {
-        BROKER_CONFIG: entry.data,
-    }
-
-    mqtt = InelsMqtt(inels_data[BROKER_CONFIG])
-    inels_data[BROKER] = mqtt
+    mqtt = InelsMqtt(entry.data)
+    inels_data = InelsData(mqtt=mqtt)
 
     # Test connection and check for authentication errors
-    conn_result = await hass.async_add_executor_job(inels_data[BROKER].test_connection)
+    conn_result = await hass.async_add_executor_job(mqtt.test_connection)
     if isinstance(conn_result, int):  # None -> no error, int -> error code
         await hass.async_add_executor_job(mqtt.close)
         if conn_result in (4, 5):
@@ -80,35 +93,29 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             raise ConfigEntryNotReady("MQTT Broker is offline or cannot be reached")
         return False
 
-    if hass.data.get(DOMAIN) is None:
-        hass.data.setdefault(DOMAIN, {})[entry.entry_id] = inels_data
+    entry.runtime_data = inels_data
 
     try:
-        i_disc = InelsDiscovery(inels_data[BROKER])
+        i_disc = InelsDiscovery(mqtt)
         await hass.async_add_executor_job(i_disc.discovery)
 
-        inels_data[DEVICES] = i_disc.devices
+        inels_data.devices = i_disc.devices
     except Exception as exc:
         await hass.async_add_executor_job(mqtt.close)
         raise ConfigEntryNotReady from exc
 
     LOGGER.debug("Finished discovery, setting up platforms")
 
-    # save entity ids of old entities
-    old_entries: dict[str, list[str]] = {}
-
     entity_registry = er.async_get(hass)
     registry_entries: list[er.RegistryEntry] = er.async_entries_for_config_entry(
         entity_registry, entry.entry_id
     )
     for entity in registry_entries:
-        if entity.domain not in old_entries:
-            old_entries[entity.domain] = []
-        old_entries[entity.domain].append(entity.entity_id)
+        platform = Platform(entity.domain)
+        if platform not in inels_data.old_entities:
+            inels_data.old_entities[platform] = []
+        inels_data.old_entities[platform].append(entity.entity_id)
 
-    inels_data[OLD_ENTITIES] = old_entries
-
-    hass.data[DOMAIN][entry.entry_id] = inels_data
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     LOGGER.info("Cleaning up entities/devices")
@@ -119,24 +126,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+async def async_reload_entry(hass: HomeAssistant, entry: InelsConfigEntry) -> None:
     """Reload all devices."""
     await hass.config_entries.async_reload(entry.entry_id)
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: InelsConfigEntry) -> bool:
     """Unload a config entry."""
-    hass_data = hass.data[DOMAIN][entry.entry_id]
-    broker: InelsMqtt = hass_data[BROKER]
+    entry.runtime_data.mqtt.unsubscribe_listeners()
+    entry.runtime_data.mqtt.disconnect()
 
-    broker.unsubscribe_listeners()
-    broker.disconnect()
-
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    if not hass.data[DOMAIN]:
-        hass.data.pop(DOMAIN)
-
-    return True
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
