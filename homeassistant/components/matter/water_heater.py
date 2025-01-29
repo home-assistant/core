a342d05444c1756bf5c47ed38e9d2e6b54b7dac3
Ludovic BOUE <lboue@users.noreply.github.com>
2025-01-29 23:48:54 +0100

    First try with water_heater

--- homeassistant/components/matter/water_heater.py
+++ homeassistant/components/matter/water_heater.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, cast
 
 from chip.clusters import Objects as clusters
 from matter_server.client.models import device_types
@@ -10,6 +10,9 @@ from matter_server.client.models import device_types
 from homeassistant.components.water_heater import (
     STATE_ECO,
     STATE_ELECTRIC,
+    STATE_HEAT_PUMP,
+    STATE_HIGH_DEMAND,
+    STATE_OFF,
     STATE_PERFORMANCE,
     WaterHeaterEntity,
     WaterHeaterEntityDescription,
@@ -19,11 +22,10 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     ATTR_TEMPERATURE,
     PRECISION_WHOLE,
-    STATE_OFF,
     Platform,
     UnitOfTemperature,
 )
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .entity import MatterEntity
@@ -34,8 +36,8 @@ SUPPORT_FLAGS_HEATER = (
     WaterHeaterEntityFeature.TARGET_TEMPERATURE
     | WaterHeaterEntityFeature.ON_OFF
     | WaterHeaterEntityFeature.OPERATION_MODE
-    | WaterHeaterEntityFeature.AWAY_MODE
 )
+TEMPERATURE_SCALING_FACTOR = 100
 
 
 async def async_setup_entry(
@@ -52,21 +54,24 @@ class MatterWaterHeater(MatterEntity, WaterHeaterEntity):
     """Representation of a Matter WaterHeater entity."""
 
     _attr_current_temperature: float | None = None
-    _attr_min_temp = 30.0  # Replace with dynamic attribute
-    _attr_max_temp = 70.0  # Replace with dynamic attribute
+    # _attr_min_temp: float | None = None
+    # _attr_max_temp: float | None = None
+    _attr_operation_list = [
+        STATE_ECO,
+        STATE_ELECTRIC,
+        STATE_HEAT_PUMP,
+        STATE_HIGH_DEMAND,
+        STATE_OFF,
+        STATE_PERFORMANCE,
+    ]
     _attr_precision = PRECISION_WHOLE
-
-    _attr_temperature_unit = UnitOfTemperature.CELSIUS
     _attr_supported_features = (
         WaterHeaterEntityFeature.TARGET_TEMPERATURE
         | WaterHeaterEntityFeature.OPERATION_MODE
     )
-    _attr_operation_list = [
-        STATE_ECO,
-        STATE_OFF,
-        STATE_PERFORMANCE,
-        STATE_ELECTRIC,
-    ]
+    _attr_target_temperature: float | None = None
+    _attr_temperature_unit = UnitOfTemperature.CELSIUS
+    _platform_translation_key = "water_heater"
 
     def set_temperature(self, **kwargs: Any) -> None:
         """Set new target temperatures."""
@@ -86,6 +91,42 @@ class MatterWaterHeater(MatterEntity, WaterHeaterEntity):
         """Turn off water heater."""
         self.set_operation_mode("off")
 
+    @callback
+    def _update_from_device(self) -> None:
+        """Update from device."""
+        # self._calculate_features()
+        self._attr_current_temperature = self._get_temperature_in_degrees(
+            clusters.Thermostat.Attributes.LocalTemperature
+        )
+        self._attr_temperature = cast(
+            float,
+            self._get_temperature_in_degrees(
+                clusters.Thermostat.Attributes.OccupiedHeatingSetpoint
+            ),
+        )
+        self._attr_min_temp = cast(
+            float,
+            self._get_temperature_in_degrees(
+                clusters.Thermostat.Attributes.AbsMinHeatSetpointLimit
+            ),
+        )
+        self._attr_max_temp = cast(
+            float,
+            self._get_temperature_in_degrees(
+                clusters.Thermostat.Attributes.AbsMaxHeatSetpointLimit
+            ),
+        )
+        # self._attr_operation_mode = STATE_HEAT_PUMP
+
+    @callback
+    def _get_temperature_in_degrees(
+        self, attribute: type[clusters.ClusterAttributeDescriptor]
+    ) -> float | None:
+        """Return the scaled temperature value for the given attribute."""
+        if value := self.get_matter_attribute_value(attribute):
+            return float(value) / TEMPERATURE_SCALING_FACTOR
+        return None
+
 
 # Discovery schema(s) to map Matter Attributes to HA entities
 DISCOVERY_SCHEMAS = [
@@ -93,16 +134,20 @@ DISCOVERY_SCHEMAS = [
         platform=Platform.WATER_HEATER,
         entity_description=WaterHeaterEntityDescription(
             key="MatterWaterHeater",
-            name=None,
         ),
         entity_class=MatterWaterHeater,
         required_attributes=(
-            clusters.WaterHeaterManagement.Attributes.BoostState,
-            clusters.WaterHeaterManagement.Attributes.HeaterTypes,
-            clusters.WaterHeaterManagement.Attributes.HeatDemand,
+            clusters.Thermostat.Attributes.OccupiedHeatingSetpoint,
+            clusters.Thermostat.Attributes.AbsMinHeatSetpointLimit,
+            clusters.Thermostat.Attributes.AbsMaxHeatSetpointLimit,
+            clusters.Thermostat.Attributes.LocalTemperature,
             clusters.WaterHeaterManagement.Attributes.FeatureMap,
         ),
-        optional_attributes=(),
+        optional_attributes=(
+            clusters.WaterHeaterManagement.Attributes.HeaterTypes,
+            clusters.WaterHeaterManagement.Attributes.BoostState,
+            clusters.WaterHeaterManagement.Attributes.HeatDemand,
+        ),
         device_type=(device_types.WaterHeater,),
         allow_multi=True,  # also used for sensor entity
     ),
