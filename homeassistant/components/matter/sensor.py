322f54a9016827cb90d449a147b0614514ee4025
Marcel van der Veldt <m.vanderveldt@outlook.com>
2025-01-29 09:16:30 +0100

    Add remaining Matter Operational State sensor discovery schemas (#136741)

--- homeassistant/components/matter/sensor.py
+++ homeassistant/components/matter/sensor.py
@@ -72,6 +72,9 @@ OPERATIONAL_STATE_MAP = {
     clusters.OperationalState.Enums.OperationalStateEnum.kRunning: "running",
     clusters.OperationalState.Enums.OperationalStateEnum.kPaused: "paused",
     clusters.OperationalState.Enums.OperationalStateEnum.kError: "error",
+    clusters.RvcOperationalState.Enums.OperationalStateEnum.kSeekingCharger: "seeking_charger",
+    clusters.RvcOperationalState.Enums.OperationalStateEnum.kCharging: "charging",
+    clusters.RvcOperationalState.Enums.OperationalStateEnum.kDocked: "docked",
 }
 
 
@@ -96,6 +99,18 @@ class MatterListSensorEntityDescription(MatterSensorEntityDescription):
 
     # list attribute: the attribute descriptor to get the list of values (= list of strings)
     list_attribute: type[ClusterAttributeDescriptor]
+
+
+@dataclass(frozen=True, kw_only=True)
+class MatterOperationalStateSensorEntityDescription(MatterSensorEntityDescription):
+    """Describe Matter sensor entities from Matter OperationalState objects."""
+
+    # list attribute: the attribute descriptor to get the list of values (= list of structs)
+    # needs to be set for handling OperationalState not on the OperationalState cluster, but
+    # on one of its derived clusters (e.g. RvcOperationalState)
+    state_list_attribute: type[ClusterAttributeDescriptor] = (
+        clusters.OperationalState.Attributes.OperationalStateList
+    )
 
 
 class MatterSensor(MatterEntity, SensorEntity):
@@ -147,6 +162,7 @@ class MatterDraftElectricalMeasurementSensor(MatterEntity, SensorEntity):
 class MatterOperationalStateSensor(MatterSensor):
     """Representation of a sensor for Matter Operational State."""
 
+    entity_description: MatterOperationalStateSensorEntityDescription
     states_map: dict[int, str]
 
     @callback
@@ -157,10 +173,11 @@ class MatterOperationalStateSensor(MatterSensor):
         # therefore it is not possible to provide a fixed list of options
         # or to provide a mapping to a translateable string for all options
         operational_state_list = self.get_matter_attribute_value(
-            clusters.OperationalState.Attributes.OperationalStateList
+            self.entity_description.state_list_attribute
         )
         if TYPE_CHECKING:
             operational_state_list = cast(
+                # cast to the generic OperationalStateStruct type just to help typing
                 list[clusters.OperationalState.Structs.OperationalStateStruct],
                 operational_state_list,
             )
@@ -782,7 +799,7 @@ DISCOVERY_SCHEMAS = [
     ),
     MatterDiscoverySchema(
         platform=Platform.SENSOR,
-        entity_description=MatterSensorEntityDescription(
+        entity_description=MatterOperationalStateSensorEntityDescription(
             key="OperationalState",
             device_class=SensorDeviceClass.ENUM,
             translation_key="operational_state",
@@ -808,6 +825,32 @@ DISCOVERY_SCHEMAS = [
     ),
     MatterDiscoverySchema(
         platform=Platform.SENSOR,
+        entity_description=MatterListSensorEntityDescription(
+            key="RvcOperationalStateCurrentPhase",
+            translation_key="current_phase",
+            list_attribute=clusters.RvcOperationalState.Attributes.PhaseList,
+        ),
+        entity_class=MatterListSensor,
+        required_attributes=(
+            clusters.RvcOperationalState.Attributes.CurrentPhase,
+            clusters.RvcOperationalState.Attributes.PhaseList,
+        ),
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.SENSOR,
+        entity_description=MatterListSensorEntityDescription(
+            key="OvenCavityOperationalStateCurrentPhase",
+            translation_key="current_phase",
+            list_attribute=clusters.OvenCavityOperationalState.Attributes.PhaseList,
+        ),
+        entity_class=MatterListSensor,
+        required_attributes=(
+            clusters.OvenCavityOperationalState.Attributes.CurrentPhase,
+            clusters.OvenCavityOperationalState.Attributes.PhaseList,
+        ),
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.SENSOR,
         entity_description=MatterSensorEntityDescription(
             key="ThermostatLocalTemperature",
             native_unit_of_measurement=UnitOfTemperature.CELSIUS,
@@ -819,5 +862,34 @@ DISCOVERY_SCHEMAS = [
         required_attributes=(clusters.Thermostat.Attributes.LocalTemperature,),
         device_type=(device_types.Thermostat,),
         allow_multi=True,  # also used for climate entity
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.SENSOR,
+        entity_description=MatterOperationalStateSensorEntityDescription(
+            key="RvcOperationalState",
+            device_class=SensorDeviceClass.ENUM,
+            translation_key="operational_state",
+            state_list_attribute=clusters.RvcOperationalState.Attributes.OperationalStateList,
+        ),
+        entity_class=MatterOperationalStateSensor,
+        required_attributes=(
+            clusters.RvcOperationalState.Attributes.OperationalState,
+            clusters.RvcOperationalState.Attributes.OperationalStateList,
+        ),
+        allow_multi=True,  # also used for vacuum entity
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.SENSOR,
+        entity_description=MatterOperationalStateSensorEntityDescription(
+            key="OvenCavityOperationalState",
+            device_class=SensorDeviceClass.ENUM,
+            translation_key="operational_state",
+            state_list_attribute=clusters.OvenCavityOperationalState.Attributes.OperationalStateList,
+        ),
+        entity_class=MatterOperationalStateSensor,
+        required_attributes=(
+            clusters.OvenCavityOperationalState.Attributes.OperationalState,
+            clusters.OvenCavityOperationalState.Attributes.OperationalStateList,
+        ),
     ),
 ]
