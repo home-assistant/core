3677d88ea906539d1ca5b7d8f09c15fc2a7f1e28
epdevlab <epdevlab@gmail.com>
2025-01-30 13:03:39 +0000

    Remove automatic cleanup of unused entities/devices

--- homeassistant/components/inels/__init__.py
+++ homeassistant/components/inels/__init__.py
@@ -13,7 +13,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
-from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 from .const import LOGGER
 
@@ -30,7 +29,6 @@ class InelsData:
 
     mqtt: InelsMqtt
     devices: list[Device] = field(default_factory=list)
-    old_entities: dict[Platform, list[str]] = field(default_factory=dict)
 
 
 def inels_discovery(inels_data: InelsData) -> None:
@@ -60,43 +58,6 @@ def inels_discovery(inels_data: InelsData) -> None:
         )
         inels_data.mqtt.close()
         raise ConfigEntryNotReady from exc
-
-
-async def async_remove_old_entities(
-    hass: HomeAssistant, entry: InelsConfigEntry
-) -> None:
-    """Remove old entities that aren't being used."""
-    entity_registry = er.async_get(hass)
-    remaining_entries = entry.runtime_data.old_entities
-
-    for platform in remaining_entries:
-        for entity_id in remaining_entries[platform]:
-            entity_registry.async_remove(entity_id)
-
-        # Clear the list for the platform after removing entities
-        remaining_entries[platform].clear()
-
-
-async def async_remove_devices_with_no_entities(
-    hass: HomeAssistant, entry: InelsConfigEntry
-) -> None:
-    """Remove devices with no entities."""
-    device_registry = dr.async_get(hass)
-    entity_registry = er.async_get(hass)
-
-    registered_devices = [
-        entry.id
-        for entry in dr.async_entries_for_config_entry(
-            registry=device_registry, config_entry_id=entry.entry_id
-        )
-    ]
-
-    for device_id in registered_devices:
-        if not er.async_entries_for_device(
-            entity_registry, device_id, include_disabled_entities=True
-        ):
-            LOGGER.info("Removing device %s, because it has no entities", device_id)
-            device_registry.async_remove_device(device_id=device_id)
 
 
 async def _async_config_entry_updated(
@@ -131,21 +92,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: InelsConfigEntry) -> boo
 
     LOGGER.debug("Finished discovery, setting up platforms")
 
-    entity_registry = er.async_get(hass)
-    registry_entries: list[er.RegistryEntry] = er.async_entries_for_config_entry(
-        entity_registry, entry.entry_id
-    )
-    for entity in registry_entries:
-        platform = Platform(entity.domain)
-        if platform not in inels_data.old_entities:
-            inels_data.old_entities[platform] = []
-        inels_data.old_entities[platform].append(entity.entity_id)
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
-    LOGGER.info("Cleaning up entities/devices")
-    await async_remove_old_entities(hass, entry)
-    await async_remove_devices_with_no_entities(hass, entry)
 
     LOGGER.info("Platform setup complete")
     return True
--- tests/components/inels/test_init.py
+++ tests/components/inels/test_init.py
@@ -1,108 +1,17 @@
 """Tests for iNELS integration."""
 
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import pytest
 
 from homeassistant.config_entries import ConfigEntryAuthFailed, ConfigEntryNotReady
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 from . import HA_INELS_PATH
 from .common import DOMAIN, inels
-from .conftest import setup_inels_test_integration
 from .test_config_flow import default_config  # noqa: F401
 
 from tests.common import MockConfigEntry
-
-
-async def test_remove_devices_with_no_entities(hass: HomeAssistant, mock_mqtt) -> None:
-    """Test that devices with no entities are removed."""
-    await setup_inels_test_integration(hass)
-
-    config_entry = next(
-        entry
-        for entry in hass.config_entries.async_entries(DOMAIN)
-        if entry.domain == DOMAIN
-    )
-
-    device_registry = dr.async_get(hass)
-    entity_registry = er.async_get(hass)
-
-    device_1 = device_registry.async_get_or_create(
-        config_entry_id=config_entry.entry_id,
-        identifiers={(DOMAIN, "device_1")},
-        name="Device_with_entity",
-    )
-    device_2 = device_registry.async_get_or_create(
-        config_entry_id=config_entry.entry_id,
-        identifiers={(DOMAIN, "device_2")},
-        name="Device_to_be_removed",
-    )
-
-    # Add an entity for device_1 but not for device_2
-    entity_registry.async_get_or_create(
-        domain="sensor",
-        platform=DOMAIN,
-        unique_id="entity_1",
-        config_entry=config_entry,
-        device_id=device_1.id,
-    )
-
-    with patch.object(
-        device_registry, "async_remove_device", new_callable=Mock
-    ) as mock_remove_device:
-        await inels.async_remove_devices_with_no_entities(hass, config_entry)
-
-        assert mock_remove_device.call_count == 1
-        mock_remove_device.assert_called_with(device_id=device_2.id)
-
-
-async def test_remove_old_entities(hass: HomeAssistant, mock_mqtt) -> None:
-    """Test that old entities are removed."""
-    await setup_inels_test_integration(hass)
-
-    config_entry = next(
-        entry
-        for entry in hass.config_entries.async_entries(DOMAIN)
-        if entry.domain == DOMAIN
-    )
-
-    inels_data = inels.InelsData(mqtt=mock_mqtt)
-    inels_data.old_entities = {
-        "sensor": ["sensor.old_entity_1"],
-        "light": ["light.old_entity_2"],
-    }
-    config_entry.runtime_data = inels_data
-
-    entity_registry = er.async_get(hass)
-
-    # Add old entities to the entity registry
-    entity_registry.async_get_or_create(
-        domain="sensor",
-        platform=DOMAIN,
-        unique_id="old_entity_1",
-        config_entry=config_entry,
-    )
-    entity_registry.async_get_or_create(
-        domain="light",
-        platform=DOMAIN,
-        unique_id="old_entity_2",
-        config_entry=config_entry,
-    )
-
-    with patch.object(
-        entity_registry, "async_remove", new_callable=Mock
-    ) as mock_remove_entity:
-        await inels.async_remove_old_entities(hass, config_entry)
-
-        # Ensure all pending tasks are completed
-        await hass.async_block_till_done()
-
-        # Verify that old entities were removed
-        assert mock_remove_entity.call_count == 2
-        mock_remove_entity.assert_any_call("sensor.old_entity_1")
-        mock_remove_entity.assert_any_call("light.old_entity_2")
 
 
 @pytest.mark.parametrize(
