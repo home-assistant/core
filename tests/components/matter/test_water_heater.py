97883b28b4dc03ce4c157d34afce8f1c5d18c7c3
Ludovic BOUE <lboue@users.noreply.github.com>
2025-01-31 17:23:23 +0100

    Update tests

--- tests/components/matter/test_water_heater.py
+++ tests/components/matter/test_water_heater.py
@@ -6,7 +6,7 @@ from matter_server.client.models.node import MatterNode
 import pytest
 from syrupy import SnapshotAssertion
 
-from homeassistant.components.water_heater import STATE_ECO
+from homeassistant.components.water_heater import STATE_ECO, WaterHeaterEntityFeature
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
@@ -36,4 +36,14 @@ async def test_water_heater(
     assert state.attributes["min_temp"] == 40
     assert state.attributes["max_temp"] == 65
     assert state.attributes["temperature"] == 65
+    assert state.attributes["operation_list"] == ["eco", "high_demand", "off"]
     assert state.state == STATE_ECO
+
+    # test supported features correctly parsed
+    # including temperature_range support
+    mask = (
+        WaterHeaterEntityFeature.TARGET_TEMPERATURE
+        | WaterHeaterEntityFeature.ON_OFF
+        | WaterHeaterEntityFeature.OPERATION_MODE
+    )
+    assert state.attributes["supported_features"] & mask == mask
