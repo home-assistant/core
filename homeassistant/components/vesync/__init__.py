34eccad125984d935ad870cf44aa7370a9359d25
cdnninja <jaydenaphillips@gmail.com>
2025-02-01 01:25:06 +0000

    Updates to migration logic.

--- homeassistant/components/vesync/__init__.py
+++ homeassistant/components/vesync/__init__.py
@@ -7,6 +7,7 @@ from pyvesync import VeSync
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
 from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 from .common import async_generate_device_list
@@ -96,21 +97,27 @@ async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) ->
     """Migrate old entry."""
     if config_entry.version == 1:
         # Migrate switch/outlets entity to a new unique ID
-        # How do we get unique id of the switch entity?
-        entity_registry = await hass.helpers.entity_registry.async_get_registry()
-        old_unique_ids = [
-            entry.unique_id
-            for entry in entity_registry.entities.values()
-            if DOMAIN in entry.platform and "--" not in entry.unique_id
-        ]
-
-        for old_unique_id in old_unique_ids:
-            new_unique_id = f"{old_unique_id}-device_status"
-            entity_registry = await hass.helpers.entity_registry.async_get_registry()
-            entity_entry = entity_registry.async_get(old_unique_id)
-            if entity_entry:
-                entity_registry.async_update_entity(
-                    entity_entry.entity_id, new_unique_id=new_unique_id
+        _LOGGER.debug("Migrating VeSync config entry from version 1 to version 2")
+        entity_registry = er.async_get(hass)
+        registry_entries = er.async_entries_for_config_entry(
+            entity_registry, config_entry.entry_id
+        )
+        for reg_entry in registry_entries:
+            if (
+                "--" not in reg_entry.unique_id
+                and reg_entry.platform == Platform.SWITCH
+            ):
+                _LOGGER.debug(
+                    "Migrating switch/outlet entity from unique_id: %s to unique_id: %s",
+                    reg_entry.unique_id,
+                    reg_entry.unique_id + "--device_status",
                 )
+                entity_registry.async_update_entity(
+                    reg_entry.entity_id,
+                    new_unique_id=reg_entry.unique_id + "--device_status",
+                )
+            else:
+                _LOGGER.debug("Skipping entity with unique_id: %s", reg_entry.unique_id)
         hass.config_entries.async_update_entry(config_entry, version=2)
+
     return True
