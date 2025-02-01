6cc1644eb5a21377f8997822bf5f04c72288d555
J. Diego Rodríguez Royo <jdrr1998@hotmail.com>
2025-02-01 08:38:05 +0000

    cache `ha_id`
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/home_connect/coordinator.py
+++ homeassistant/components/home_connect/coordinator.py
@@ -121,9 +121,10 @@ class HomeConnectCoordinator(
         while True:
             try:
                 async for event_message in self.client.stream_all_events():
+                    event_message_ha_id = event_message.ha_id
                     match event_message.type:
                         case EventType.STATUS:
-                            statuses = self.data[event_message.ha_id].status
+                            statuses = self.data[event_message_ha_id].status
                             for event in event_message.data.items:
                                 status_key = StatusKey(event.key)
                                 if status_key in statuses:
@@ -137,8 +138,8 @@ class HomeConnectCoordinator(
                             self._call_event_listener(event_message)
 
                         case EventType.NOTIFY:
-                            settings = self.data[event_message.ha_id].settings
-                            events = self.data[event_message.ha_id].events
+                            settings = self.data[event_message_ha_id].settings
+                            events = self.data[event_message_ha_id].events
                             for event in event_message.data.items:
                                 if event.key in SettingKey:
                                     setting_key = SettingKey(event.key)
@@ -155,21 +156,21 @@ class HomeConnectCoordinator(
                             self._call_event_listener(event_message)
 
                         case EventType.EVENT:
-                            events = self.data[event_message.ha_id].events
+                            events = self.data[event_message_ha_id].events
                             for event in event_message.data.items:
                                 events[event.key] = event
                             self._call_event_listener(event_message)
 
                         case EventType.CONNECTED:
-                            self.data[event_message.ha_id].info.connected = True
+                            self.data[event_message_ha_id].info.connected = True
                             self._call_all_event_listeners_for_appliance(
-                                event_message.ha_id
+                                event_message_ha_id
                             )
 
                         case EventType.DISCONNECTED:
-                            self.data[event_message.ha_id].info.connected = False
+                            self.data[event_message_ha_id].info.connected = False
                             self._call_all_event_listeners_for_appliance(
-                                event_message.ha_id
+                                event_message_ha_id
                             )
 
             except (EventStreamInterruptedError, HomeConnectRequestError) as error:
