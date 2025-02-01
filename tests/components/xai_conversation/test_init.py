8245c51c35336507df71c24f03dc4991078b8829
Edward Vella <hello@dwardu.com>
2025-02-01 12:19:55 +0000

    Adding xAI Conversation Integration

---
+++ tests/components/xai_conversation/test_init.py
@@ -0,0 +1,89 @@
+"""Tests for the OpenAI integration."""
+
+from unittest.mock import patch
+
+from httpx import Response
+from openai import APIConnectionError, AuthenticationError, BadRequestError
+import pytest
+
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
+
+from tests.common import MockConfigEntry
+
+
+@pytest.mark.parametrize(
+    ("service_data", "expected_args"),
+    [
+        (
+            {"prompt": "Picture of a dog"},
+            {
+                "prompt": "Picture of a dog",
+                "size": "1024x1024",
+                "quality": "standard",
+                "style": "vivid",
+            },
+        ),
+        (
+            {
+                "prompt": "Picture of a dog",
+                "size": "1024x1792",
+                "quality": "hd",
+                "style": "vivid",
+            },
+            {
+                "prompt": "Picture of a dog",
+                "size": "1024x1792",
+                "quality": "hd",
+                "style": "vivid",
+            },
+        ),
+        (
+            {
+                "prompt": "Picture of a dog",
+                "size": "1792x1024",
+                "quality": "standard",
+                "style": "natural",
+            },
+            {
+                "prompt": "Picture of a dog",
+                "size": "1792x1024",
+                "quality": "standard",
+                "style": "natural",
+            },
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    ("side_effect", "error"),
+    [
+        (APIConnectionError(request=None), "Connection error"),
+        (
+            AuthenticationError(
+                response=Response(status_code=None, request=""), body=None, message=None
+            ),
+            "Invalid API key",
+        ),
+        (
+            BadRequestError(
+                response=Response(status_code=None, request=""), body=None, message=None
+            ),
+            "openai_conversation integration not ready yet: None",
+        ),
+    ],
+)
+async def test_init_error(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    caplog: pytest.LogCaptureFixture,
+    side_effect,
+    error,
+) -> None:
+    """Test initialization errors."""
+    with patch(
+        "openai.resources.models.AsyncModels.list",
+        side_effect=side_effect,
+    ):
+        assert await async_setup_component(hass, "openai_conversation", {})
+        await hass.async_block_till_done()
+        assert error in caplog.text
