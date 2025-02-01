1c862132985cc2c0796eee1bad2e3695e1fbec0d
J. Diego Rodríguez Royo <jdrr1998@hotmail.com>
2025-02-01 09:20:02 +0000

    Intialize HomeConnectEntity first at SelectProgramEntity

--- homeassistant/components/home_connect/select.py
+++ homeassistant/components/home_connect/select.py
@@ -66,16 +66,16 @@ class HomeConnectProgramSelectEntity(HomeConnectEntity, SelectEntity):
         desc: SelectEntityDescription,
     ) -> None:
         """Initialize the entity."""
-        self._attr_options = [
-            PROGRAMS_TRANSLATION_KEYS_MAP[program.key]
-            for program in appliance.programs
-            if program.key != ProgramKey.UNKNOWN
-        ]
         super().__init__(
             coordinator,
             appliance,
             desc,
         )
+        self._attr_options = [
+            PROGRAMS_TRANSLATION_KEYS_MAP[program.key]
+            for program in appliance.programs
+            if program.key != ProgramKey.UNKNOWN
+        ]
         self.start_on_select = desc.key == EventKey.BSH_COMMON_ROOT_ACTIVE_PROGRAM
 
     def update_native_value(self) -> None:
