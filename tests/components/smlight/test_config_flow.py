71edb4a421cdb0b968d88f60699c8e3cf06cc816
Tim Lunn <tl@smlight.tech>
2025-02-01 22:50:41 +0000

    Add test for user flow override discovery

--- tests/components/smlight/test_config_flow.py
+++ tests/components/smlight/test_config_flow.py
@@ -262,6 +262,44 @@ async def test_user_device_exists_abort(
 
 
 @pytest.mark.usefixtures("mock_smlight_client")
+async def test_user_flow_can_override_discovery(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_setup_entry: AsyncMock,
+) -> None:
+    """Test manual user flow can override discovery in progress."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_ZEROCONF}, data=DISCOVERY_INFO
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "confirm_discovery"
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == SOURCE_USER
+    assert result["errors"] == {}
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_HOST: MOCK_HOST,
+        },
+    )
+
+    assert result2["type"] is FlowResultType.CREATE_ENTRY
+    assert result2["context"]["source"] == SOURCE_USER
+    assert result2["data"] == {
+        CONF_HOST: MOCK_HOST,
+    }
+    assert result2["context"]["unique_id"] == "aa:bb:cc:dd:ee:ff"
+    assert len(mock_setup_entry.mock_calls) == 1
+
+
+@pytest.mark.usefixtures("mock_smlight_client")
 async def test_zeroconf_device_exists_abort(
     hass: HomeAssistant, mock_config_entry: MockConfigEntry
 ) -> None:
