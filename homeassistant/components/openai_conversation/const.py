0f9238a763ca82bc2511ac464969d780ca9a4bba
Denis Shulyaka <Shulyaka@gmail.com>
2025-02-02 23:10:14 +0300

    Reenable audio models (they also support text)

--- homeassistant/components/openai_conversation/const.py
+++ homeassistant/components/openai_conversation/const.py
@@ -28,9 +28,4 @@ UNSUPPORTED_MODELS = [
     "gpt-4o-realtime-preview-2024-10-01",
     "gpt-4o-mini-realtime-preview",
     "gpt-4o-mini-realtime-preview-2024-12-17",
-    "gpt-4o-audio-preview",
-    "gpt-4o-audio-preview-2024-12-17",
-    "gpt-4o-audio-preview-2024-10-01",
-    "gpt-4o-mini-audio-preview",
-    "gpt-4o-mini-audio-preview-2024-12-17",
 ]
