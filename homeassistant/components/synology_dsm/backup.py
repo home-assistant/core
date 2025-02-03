1b20fb56769d43c6157bddd9c5c8340a3f8f064c
mib1185 <mail@mib85.de>
2025-02-03 20:31:52 +0000

    only raise when error ne 408

--- homeassistant/components/synology_dsm/backup.py
+++ homeassistant/components/synology_dsm/backup.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 from collections.abc import AsyncIterator, Callable, Coroutine
-from contextlib import suppress
 import logging
 from typing import TYPE_CHECKING, Any
 
@@ -162,13 +161,20 @@ class SynologyDSMBackupAgent(BackupAgent):
 
         :param backup_id: The ID of the backup that was returned in async_list_backups.
         """
-        with suppress(SynologyDSMAPIErrorException):
-            await self._file_station.delete_file(
-                path=self.path, filename=f"{backup_id}.tar"
-            )
-            await self._file_station.delete_file(
-                path=self.path, filename=f"{backup_id}_meta.json"
-            )
+        for filename in (f"{backup_id}.tar", f"{backup_id}_meta.json"):
+            try:
+                await self._file_station.delete_file(path=self.path, filename=filename)
+            except SynologyDSMAPIErrorException as err:
+                err_args: dict = err.args[0]
+                if int(err_args.get("code", 0)) != 900 or (
+                    (err_details := err_args.get("details")) is not None
+                    and isinstance(err_details, list)
+                    and isinstance(err_details[0], dict)
+                    and int(err_details[0].get("code", 0))
+                    != 408  # No such file or directory
+                ):
+                    LOGGER.error("Failed to delete backup: %s", err)
+                    raise BackupAgentError("Failed to delete backup") from err
 
     async def async_list_backups(self, **kwargs: Any) -> list[AgentBackup]:
         """List backups."""
