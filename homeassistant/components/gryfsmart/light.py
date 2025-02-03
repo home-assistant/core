0227fd1b56fd8af3542105c06875108eacd6078c
karlowiczpl <karol.kar1@outlook.com>
2025-02-03 18:09:47 +0100

    Initialise commit

--- homeassistant/components/gryfsmart/light.py
+++ homeassistant/components/gryfsmart/light.py
@@ -1,6 +1,7 @@
 """Handle the Gryf Smart light platform functionality."""
 
 import logging
+from typing import Any
 
 from pygryfsmart.device import _GryfDevice, _GryfOutput
 
@@ -12,7 +13,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from .const import CONF_API, CONF_DEVICES, CONF_ID, CONF_NAME, DOMAIN
-from .entity import _GryfSmartEntityBase
+from .entity import GryfConfigFlowEntity, GryfYamlEntity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,16 +25,17 @@ async def async_setup_platform(
     discovery_info: DiscoveryInfoType | None,
 ) -> None:
     """Set up the Light platform."""
+
     lights = []
 
-    for conf in hass.data[DOMAIN].get("lights", {}):
+    for conf in hass.data[DOMAIN].get(Platform.LIGHT, {}):
         device = _GryfOutput(
             conf.get(CONF_NAME),
             conf.get(CONF_ID) // 10,
             conf.get(CONF_ID) % 10,
-            hass.data[DOMAIN]["api"],
+            hass.data[DOMAIN][CONF_API],
         )
-        lights.append(GryfLight(device, hass))
+        lights.append(GryfYamlLight(device))
 
     async_add_entities(lights)
 
@@ -53,61 +55,62 @@ async def async_setup_entry(
                 conf.get(CONF_NAME),
                 conf.get(CONF_ID) // 10,
                 conf.get(CONF_ID) % 10,
-                hass.data[DOMAIN][CONF_API],
+                config_entry.runtime_data[CONF_API],
             )
-            lights.append(GryfLight(device, hass))
+            lights.append(GryfConfigFlowLight(device, config_entry))
 
     async_add_entities(lights)
 
 
 class GryfLightBase(LightEntity):
-    """Gryf Smart light base."""
+    """Gryf Light entity base."""
 
     _is_on = False
     _device: _GryfDevice
-    _hass: HomeAssistant
-
-    async def update(self, state):
-        """Update state function."""
-        self._is_on = state
-        self.async_write_ha_state()
-
-    def __init__(self) -> None:
-        """Initialize Gryf Smart Light base."""
-        self._device.subscribe(self.update)
 
     @property
     def is_on(self):
-        """Return state."""
+        """Return is on."""
+
         return self._is_on
 
-    async def async_turn_on(self, **kwargs):
+    async def async_update(self, is_on):
+        """Update state."""
+
+        self._is_on = is_on
+        self.async_write_ha_state()
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn light on."""
+
         await self._device.turn_on()
 
-    async def async_turn_off(self, **kwargs):
+    async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn light off."""
+
         await self._device.turn_off()
 
-    async def async_added_to_hass(self) -> None:
-        """Add to hass."""
-        await super().async_added_to_hass()
 
-    async def async_removed_from_hass(self) -> None:
-        """Remove from hass."""
-        await super().async_will_remove_from_hass()
-
-
-class GryfLight(_GryfSmartEntityBase, GryfLightBase):
-    """Gryf Smart Light class."""
+class GryfConfigFlowLight(GryfConfigFlowEntity, GryfLightBase):
+    """Gryf Smart config flow Light class."""
 
     def __init__(
         self,
         device: _GryfDevice,
-        hass: HomeAssistant,
+        config_entry: ConfigEntry,
     ) -> None:
         """Init the Gryf Light."""
-        self._device = device
-        self._hass = hass
-        GryfLightBase.__init__(self)
-        _GryfSmartEntityBase.__init__(self)
+
+        self._config_entry = config_entry
+        super().__init__(config_entry, device)
+        self._device.subscribe(self.async_update)
+
+
+class GryfYamlLight(GryfYamlEntity, GryfLightBase):
+    """Gryf Smart Yaml Light class."""
+
+    def __init__(self, device: _GryfDevice) -> None:
+        """Init the Gryf Light."""
+
+        super().__init__(device)
+        device.subscribe(self.async_update)
