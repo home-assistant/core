546f9508b50671c138582a1e23b1203a8d3ccaef
karlowiczpl <karol.kar1@outlook.com>
2025-02-03 21:34:52 +0100

    Initialise commit

--- homeassistant/components/gryfsmart/schema.py
+++ homeassistant/components/gryfsmart/schema.py
@@ -4,7 +4,7 @@ import voluptuous as vol
 
 from homeassistant.helpers import config_validation as cv
 
-from .const import CONF_ID, CONF_MODULE_COUNT, CONF_NAME, CONF_PORT, DOMAIN
+from .const import CONF_ID, CONF_MODULE_COUNT, CONF_NAME, CONF_PORT, CONF_PWM, DOMAIN
 
 STANDARD_SCHEMA = vol.Schema(
     {
@@ -20,6 +20,7 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Optional("light"): vol.All(cv.ensure_list, [STANDARD_SCHEMA]),
                 vol.Required(CONF_PORT): cv.string,
                 vol.Optional(CONF_MODULE_COUNT): cv.positive_int,
+                vol.Optional(CONF_PWM): vol.All(cv.ensure_list, [STANDARD_SCHEMA]),
             }
         )
     },
