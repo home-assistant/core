61d3c726d7d3953a42a90557d71a0625e210760c
Remco Poelstra <r.poelstra@vogels.com>
2025-02-04 10:12:32 +0100

    Use parameter for testing various system states

--- tests/components/motionmount/test_sensor.py
+++ tests/components/motionmount/test_sensor.py
@@ -2,7 +2,8 @@
 
 from unittest.mock import MagicMock, PropertyMock
 
-import motionmount
+from motionmount import MotionMountSystemError
+import pytest
 
 from homeassistant.core import HomeAssistant
 
@@ -13,51 +14,26 @@ from tests.common import MockConfigEntry
 MAC = bytes.fromhex("c4dd57f8a55f")
 
 
-async def test_error_status_sensor_none(
+@pytest.mark.parametrize(
+    "system_status",
+    [
+        (None, "none"),
+        (MotionMountSystemError.MotorError, "motor"),
+        (MotionMountSystemError.ObstructionDetected, "obstruction"),
+        (MotionMountSystemError.TVWidthConstraintError, "tv_width_constraint"),
+        (MotionMountSystemError.HDMICECError, "hdmi_cec"),
+        (MotionMountSystemError.InternalError, "internal"),
+    ],
+)
+async def test_error_status_sensor_states(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
     mock_motionmount_config_flow: MagicMock,
+    system_status: (MotionMountSystemError, str),
 ) -> None:
     """Tests the state attributes."""
-    mock_config_entry.add_to_hass(hass)
+    (status, state) = system_status
 
-    type(mock_motionmount_config_flow).name = PropertyMock(return_value=ZEROCONF_NAME)
-    type(mock_motionmount_config_flow).mac = PropertyMock(return_value=MAC)
-    type(mock_motionmount_config_flow).is_authenticated = PropertyMock(
-        return_value=True
-    )
-    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
-
-    assert hass.states.get("sensor.my_motionmount_error_status").state == "none"
-
-
-async def test_error_status_sensor_motor(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_motionmount_config_flow: MagicMock,
-) -> None:
-    """Tests the state attributes."""
-    mock_config_entry.add_to_hass(hass)
-
-    type(mock_motionmount_config_flow).name = PropertyMock(return_value=ZEROCONF_NAME)
-    type(mock_motionmount_config_flow).mac = PropertyMock(return_value=MAC)
-    type(mock_motionmount_config_flow).is_authenticated = PropertyMock(
-        return_value=True
-    )
-    type(mock_motionmount_config_flow).system_status = PropertyMock(
-        return_value=[motionmount.MotionMountSystemError.MotorError]
-    )
-    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
-
-    assert hass.states.get("sensor.my_motionmount_error_status").state == "motor"
-
-
-async def test_error_status_sensor_obstruction(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_motionmount_config_flow: MagicMock,
-) -> None:
-    """Tests the state attributes."""
     mock_config_entry.add_to_hass(hass)
 
     type(mock_motionmount_config_flow).name = PropertyMock(return_value=ZEROCONF_NAME)
@@ -66,74 +42,8 @@ async def test_error_status_sensor_obstruction(
         return_value=True
     )
     type(mock_motionmount_config_flow).system_status = PropertyMock(
-        return_value=[motionmount.MotionMountSystemError.ObstructionDetected]
+        return_value=[status]
     )
     assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
 
-    assert hass.states.get("sensor.my_motionmount_error_status").state == "obstruction"
-
-
-async def test_error_status_sensor_tv_width_constraint(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_motionmount_config_flow: MagicMock,
-) -> None:
-    """Tests the state attributes."""
-    mock_config_entry.add_to_hass(hass)
-
-    type(mock_motionmount_config_flow).name = PropertyMock(return_value=ZEROCONF_NAME)
-    type(mock_motionmount_config_flow).mac = PropertyMock(return_value=MAC)
-    type(mock_motionmount_config_flow).is_authenticated = PropertyMock(
-        return_value=True
-    )
-    type(mock_motionmount_config_flow).system_status = PropertyMock(
-        return_value=[motionmount.MotionMountSystemError.TVWidthConstraintError]
-    )
-    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
-
-    assert (
-        hass.states.get("sensor.my_motionmount_error_status").state
-        == "tv_width_constraint"
-    )
-
-
-async def test_error_status_sensor_hdmi_cec(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_motionmount_config_flow: MagicMock,
-) -> None:
-    """Tests the state attributes."""
-    mock_config_entry.add_to_hass(hass)
-
-    type(mock_motionmount_config_flow).name = PropertyMock(return_value=ZEROCONF_NAME)
-    type(mock_motionmount_config_flow).mac = PropertyMock(return_value=MAC)
-    type(mock_motionmount_config_flow).is_authenticated = PropertyMock(
-        return_value=True
-    )
-    type(mock_motionmount_config_flow).system_status = PropertyMock(
-        return_value=[motionmount.MotionMountSystemError.HDMICECError]
-    )
-    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
-
-    assert hass.states.get("sensor.my_motionmount_error_status").state == "hdmi_cec"
-
-
-async def test_error_status_sensor_internal(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_motionmount_config_flow: MagicMock,
-) -> None:
-    """Tests the state attributes."""
-    mock_config_entry.add_to_hass(hass)
-
-    type(mock_motionmount_config_flow).name = PropertyMock(return_value=ZEROCONF_NAME)
-    type(mock_motionmount_config_flow).mac = PropertyMock(return_value=MAC)
-    type(mock_motionmount_config_flow).is_authenticated = PropertyMock(
-        return_value=True
-    )
-    type(mock_motionmount_config_flow).system_status = PropertyMock(
-        return_value=[motionmount.MotionMountSystemError.InternalError]
-    )
-    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
-
-    assert hass.states.get("sensor.my_motionmount_error_status").state == "internal"
+    assert hass.states.get("sensor.my_motionmount_error_status").state == state
