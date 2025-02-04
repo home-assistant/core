344d3a997d3577369968082c77a258bd127c8721
Remco Poelstra <r.poelstra@vogels.com>
2025-02-04 13:10:52 +0100

    Use patch to create mock

--- tests/components/motionmount/test_sensor.py
+++ tests/components/motionmount/test_sensor.py
@@ -1,6 +1,6 @@
 """Tests for the MotionMount Sensor platform."""
 
-from unittest.mock import MagicMock, PropertyMock
+from unittest.mock import patch
 
 from motionmount import MotionMountSystemError
 import pytest
@@ -28,22 +28,22 @@ MAC = bytes.fromhex("c4dd57f8a55f")
 async def test_error_status_sensor_states(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
-    mock_motionmount_config_flow: MagicMock,
     system_status: (MotionMountSystemError, str),
 ) -> None:
     """Tests the state attributes."""
     (status, state) = system_status
 
-    mock_config_entry.add_to_hass(hass)
+    with patch(
+        "homeassistant.components.motionmount.motionmount.MotionMount",
+        autospec=True,
+    ) as motionmount_mock:
+        motionmount_mock.return_value.name = ZEROCONF_NAME
+        motionmount_mock.return_value.mac = MAC
+        motionmount_mock.return_value.is_authenticated = True
+        motionmount_mock.return_value.system_status = [status]
 
-    type(mock_motionmount_config_flow).name = PropertyMock(return_value=ZEROCONF_NAME)
-    type(mock_motionmount_config_flow).mac = PropertyMock(return_value=MAC)
-    type(mock_motionmount_config_flow).is_authenticated = PropertyMock(
-        return_value=True
-    )
-    type(mock_motionmount_config_flow).system_status = PropertyMock(
-        return_value=[status]
-    )
-    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+        mock_config_entry.add_to_hass(hass)
 
-    assert hass.states.get("sensor.my_motionmount_error_status").state == state
+        assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+
+        assert hass.states.get("sensor.my_motionmount_error_status").state == state
