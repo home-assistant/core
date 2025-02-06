15d51e4eb21c1c7ffc5d36b08390ac0ce9190512
Matthias Lohr <mail@mlohr.com>
2025-02-06 22:57:04 +0100

    tolo: allow to reconfigure host/IP address

--- homeassistant/components/tolo/config_flow.py
+++ homeassistant/components/tolo/config_flow.py
@@ -1,14 +1,20 @@
-"""Config flow for tolo."""
+"""Config flow for TOLO integration."""
 
 from __future__ import annotations
 
 import logging
+from types import MappingProxyType
 from typing import Any
 
 from tololib import ToloClient, ToloCommunicationError
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import (
+    SOURCE_DHCP,
+    SOURCE_RECONFIGURE,
+    ConfigFlow,
+    ConfigFlowResult,
+)
 from homeassistant.const import CONF_HOST
 from homeassistant.helpers.device_registry import format_mac
 from homeassistant.helpers.service_info.dhcp import DhcpServiceInfo
@@ -17,13 +23,19 @@ from .const import DEFAULT_NAME, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+CONFIG_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_HOST): str,
+    }
+)
 
-class ToloSaunaConfigFlow(ConfigFlow, domain=DOMAIN):
-    """ConfigFlow for TOLO Sauna."""
+
+class ToloConfigFlow(ConfigFlow, domain=DOMAIN):
+    """ConfigFlow for the TOLO Integration."""
 
     VERSION = 1
 
-    _discovered_host: str
+    _dhcp_discovery_info: DhcpServiceInfo | None = None
 
     @staticmethod
     def _check_device_availability(host: str) -> bool:
@@ -37,28 +49,47 @@ class ToloSaunaConfigFlow(ConfigFlow, domain=DOMAIN):
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
-        """Handle a flow initialized by the user."""
+        """Handle a config flow initialized by the user."""
         errors = {}
 
         if user_input is not None:
-            self._async_abort_entries_match({CONF_HOST: user_input[CONF_HOST]})
-
             device_available = await self.hass.async_add_executor_job(
                 self._check_device_availability, user_input[CONF_HOST]
             )
 
-            if not device_available:
-                errors["base"] = "cannot_connect"
-            else:
-                return self.async_create_entry(
-                    title=DEFAULT_NAME, data={CONF_HOST: user_input[CONF_HOST]}
-                )
+            if device_available:
+                if self.source == SOURCE_RECONFIGURE:
+                    return self.async_update_reload_and_abort(
+                        self._get_reconfigure_entry(), data_updates=user_input
+                    )
+                return self.async_create_entry(title=DEFAULT_NAME, data=user_input)
+
+            errors["base"] = "cannot_connect"
+
+        schema_values: dict[str, Any] | MappingProxyType[str, Any] | None = None
+        if user_input is not None:
+            schema_values = user_input
+        elif self.source == SOURCE_RECONFIGURE:
+            schema_values = self._get_reconfigure_entry().data
+        elif self.source == SOURCE_DHCP and self._dhcp_discovery_info is not None:
+            schema_values = {CONF_HOST: self._dhcp_discovery_info.ip}
+        else:
+            schema_values = {}
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({vol.Required(CONF_HOST): str}),
+            data_schema=self.add_suggested_values_to_schema(
+                CONFIG_SCHEMA,
+                schema_values,
+            ),
             errors=errors,
         )
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle a reconfiguration config flow initialized by the user."""
+        return await self.async_step_user(user_input)
 
     async def async_step_dhcp(
         self, discovery_info: DhcpServiceInfo
@@ -73,21 +104,6 @@ class ToloSaunaConfigFlow(ConfigFlow, domain=DOMAIN):
         )
 
         if device_available:
-            self._discovered_host = discovery_info.ip
-            return await self.async_step_confirm()
+            self._dhcp_discovery_info = discovery_info
+            return await self.async_step_user()
         return self.async_abort(reason="not_tolo_device")
-
-    async def async_step_confirm(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Handle user-confirmation of discovered node."""
-        if user_input is not None:
-            self._async_abort_entries_match({CONF_HOST: self._discovered_host})
-            return self.async_create_entry(
-                title=DEFAULT_NAME, data={CONF_HOST: self._discovered_host}
-            )
-
-        return self.async_show_form(
-            step_id="confirm",
-            description_placeholders={CONF_HOST: self._discovered_host},
-        )
