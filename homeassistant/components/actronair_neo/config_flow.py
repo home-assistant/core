63a1760f8bc536f62af938113518f48f202373e8
Kurt Clifford <kclif9@gmail.com>
2025-02-07 07:23:13 +0000

    Changing the config_flow to save the pairing code only, and will handle serials in the coordinator

--- homeassistant/components/actronair_neo/config_flow.py
+++ homeassistant/components/actronair_neo/config_flow.py
@@ -7,12 +7,7 @@ from actron_neo_api import ActronNeoAPI, ActronNeoAPIError, ActronNeoAuthError
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
-from homeassistant.const import (
-    CONF_API_TOKEN,
-    CONF_DEVICE_ID,
-    CONF_PASSWORD,
-    CONF_USERNAME,
-)
+from homeassistant.const import CONF_API_TOKEN, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.helpers import instance_id
 
 from .const import DOMAIN, ERROR_API_ERROR, ERROR_INVALID_AUTH
@@ -35,7 +30,6 @@ class ActronNeoConfigFlow(ConfigFlow, domain=DOMAIN):
     def __init__(self) -> None:
         """Initialize the config flow."""
         self.api = None
-        self.ac_systems = None
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -44,11 +38,11 @@ class ActronNeoConfigFlow(ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
 
         if user_input:
+            username = user_input["username"]
+            password = user_input["password"]
             _LOGGER.debug("Connecting to Actron Neo API")
             try:
-                self.api = ActronNeoAPI(
-                    username=user_input["username"], password=user_input["password"]
-                )
+                self.api = ActronNeoAPI(username, password)
             except ActronNeoAuthError:
                 errors["base"] = ERROR_INVALID_AUTH
                 return self.async_show_form(
@@ -66,20 +60,14 @@ class ActronNeoConfigFlow(ConfigFlow, domain=DOMAIN):
             except ActronNeoAPIError:
                 errors["base"] = ERROR_API_ERROR
 
-            systems = await self.api.get_ac_systems()
-            self.ac_systems = systems.get("_embedded", {}).get("ac-system", [])
-
-            for system in self.ac_systems:
-                serial_number = system["serial"]
-                await self.async_set_unique_id(serial_number)
-                self._abort_if_unique_id_configured()
-                return self.async_create_entry(
-                    title=system["description"],
-                    data={
-                        CONF_API_TOKEN: self.api.pairing_token,
-                        CONF_DEVICE_ID: serial_number,
-                    },
-                )
+            await self.async_set_unique_id(username.lower())
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(
+                title=username,
+                data={
+                    CONF_API_TOKEN: self.api.pairing_token,
+                },
+            )
 
         return self.async_show_form(
             step_id="user",
