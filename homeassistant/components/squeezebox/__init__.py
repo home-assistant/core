b30034278742033ee34d697834bb4112f3e9af26
mib1185 <mail@mib85.de>
2025-02-09 17:35:38 +0000

    explicitly pass in the config_entry in coordinator

--- homeassistant/components/squeezebox/__init__.py
+++ homeassistant/components/squeezebox/__init__.py
@@ -151,7 +151,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: SqueezeboxConfigEntry) -
             else:
                 _LOGGER.debug("Adding new entity: %s", player)
                 player_coordinator = SqueezeBoxPlayerUpdateCoordinator(
-                    hass, player, lms.uuid
+                    hass, entry, player, lms.uuid
                 )
                 known_players.append(player.player_id)
                 async_dispatcher_send(
