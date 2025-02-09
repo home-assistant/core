05d6ad0cfd7b35607c9a8ebe22641ae80817beee
Marko Todoric <maretodoric@gmail.com>
2025-02-09 12:38:40 +0100

    Accept `backup_location` argument for `SSHClient`. Remove the need for `remote_path` argument in `SSHClient.get_identifier` method.

--- homeassistant/components/backup_sftp/client.py
+++ homeassistant/components/backup_sftp/client.py
@@ -7,6 +7,11 @@ from homeassistant.exceptions import ConfigEntryError
 
 
 class SSHClient(paramiko.SSHClient):
+    """
+    Derrived class from `paramiko.SSHClient` - allowing for automatic SSH connection establishment
+    and proper error handling.
+    """
+
     def __init__(
         self,
         host: str,
@@ -14,12 +19,14 @@ class SSHClient(paramiko.SSHClient):
         username: str,
         password: str = "",
         private_key_file: str = "",
+        backup_location: str = "",
     ):
         super().__init__()
         self.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy)
         self._host = host
         self._port = port
         self._username = username
+        self._backup_location = backup_location
 
         if private_key_file:
             # Check if private key exists.
@@ -62,27 +69,23 @@ class SSHClient(paramiko.SSHClient):
                 f"Please configure password or private key file location for SFTP Backup Storage."
             )
 
-    def get_identifier(self, remote_path: str) -> str:
+    def get_identifier(self) -> str:
         """
         Returns unique identifier that consists of:
         `backup_sftp.<host>.<port>.<username>.<remote_path>`.
         `remote_path` has all non-alphanumeric characters replaced by `_`, as well as `host`
         and `username`.
 
-        Parameter
-        ----------
-        remote_path : str
-            Path where remote backups are stored.
 
         Example
         ----------
-        >>> client = SSHClient(host='192.168.0.100', port=22, username='user', password='S3c5e7p@sSw0rD')
-        >>> client.get_identifier('/mnt/backup_storage')
+        >>> client = SSHClient(host='192.168.0.100', port=22, username='user', password='S3c5e7p@sSw0rD', backup_location='/mnt/backup_storage')
+        >>> client.get_identifier()
         'backup_sftp.192_168_0_100.22.user.mnt_backup_storage'
         """
 
         host = re.sub(r"[^a-zA-Z\d\s:]", "_", self._host)
-        remote_path = re.sub(r"[^a-zA-Z\d\s:]", "_", remote_path)
+        remote_path = re.sub(r"[^a-zA-Z\d\s:]", "_", self._backup_location)
         user = re.sub(r"[^a-zA-Z\d\s:]", "_", self._username)
 
         return f"backup_sftp.{host}.{self._port}.{user}.{remote_path}"
