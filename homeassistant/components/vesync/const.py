f32b0eb046199e1bb3ba5b14737963e522c33bc4
Kevin Brown-Silva <github@kevin-brown.com>
2025-02-08 22:42:10 -0500

    Make VeSync static constants a bit more dynamic
    This improves the two VeSync static constants, `DEV_TYPE_TO_HA` and
    `SKU_TO_BASE_DEVICE`, to make them built automatically based on the
    supported devices within pyvesync. This should reduce the amount of
    changes necessary in the future to support new devices and makes the
    integration slightly more resilient to new skus released for devices
    that already exist.
    
    Since switches and bulbs use multiple device types, we need to detect
    the supported device based on the features that are exposed.

--- homeassistant/components/vesync/const.py
+++ homeassistant/components/vesync/const.py
@@ -1,6 +1,14 @@
 """Constants for VeSync Component."""
 
-from pyvesync.vesyncfan import VeSyncHumid200300S, VeSyncSuperior6000S
+from pyvesync.vesyncbulb import feature_dict as bulb_features
+from pyvesync.vesyncfan import (
+    VeSyncHumid200300S,
+    VeSyncSuperior6000S,
+    air_features,
+    humid_features,
+)
+from pyvesync.vesyncoutlet import outlet_config
+from pyvesync.vesyncswitch import feature_dict as switch_features
 
 DOMAIN = "vesync"
 VS_DISCOVERY = "vesync_discovery_{}"
@@ -32,69 +40,29 @@ VS_HUMIDIFIER_MODE_SLEEP = "sleep"
 VeSyncHumidifierDevice = VeSyncHumid200300S | VeSyncSuperior6000S
 """Humidifier device types"""
 
-DEV_TYPE_TO_HA = {
-    "wifi-switch-1.3": "outlet",
-    "ESW03-USA": "outlet",
-    "ESW01-EU": "outlet",
-    "ESW15-USA": "outlet",
-    "ESWL01": "switch",
-    "ESWL03": "switch",
-    "ESO15-TB": "outlet",
-    "LV-PUR131S": "fan",
-    "Core200S": "fan",
-    "Core300S": "fan",
-    "Core400S": "fan",
-    "Core600S": "fan",
-    "EverestAir": "fan",
-    "Vital200S": "fan",
-    "Vital100S": "fan",
-    "SmartTowerFan": "fan",
-    "ESD16": "walldimmer",
-    "ESWD16": "walldimmer",
-    "ESL100": "bulb-dimmable",
-    "ESL100CW": "bulb-tunable-white",
-}
+DEV_TYPE_TO_HA = {}
 
-SKU_TO_BASE_DEVICE = {
-    # Air Purifiers
-    "LV-PUR131S": "LV-PUR131S",
-    "LV-RH131S": "LV-PUR131S",  # Alt ID Model LV-PUR131S
-    "Core200S": "Core200S",
-    "LAP-C201S-AUSR": "Core200S",  # Alt ID Model Core200S
-    "LAP-C202S-WUSR": "Core200S",  # Alt ID Model Core200S
-    "Core300S": "Core300S",
-    "LAP-C301S-WJP": "Core300S",  # Alt ID Model Core300S
-    "LAP-C301S-WAAA": "Core300S",  # Alt ID Model Core300S
-    "LAP-C302S-WUSB": "Core300S",  # Alt ID Model Core300S
-    "Core400S": "Core400S",
-    "LAP-C401S-WJP": "Core400S",  # Alt ID Model Core400S
-    "LAP-C401S-WUSR": "Core400S",  # Alt ID Model Core400S
-    "LAP-C401S-WAAA": "Core400S",  # Alt ID Model Core400S
-    "Core600S": "Core600S",
-    "LAP-C601S-WUS": "Core600S",  # Alt ID Model Core600S
-    "LAP-C601S-WUSR": "Core600S",  # Alt ID Model Core600S
-    "LAP-C601S-WEU": "Core600S",  # Alt ID Model Core600S,
-    "Vital200S": "Vital200S",
-    "LAP-V201S-AASR": "Vital200S",  # Alt ID Model Vital200S
-    "LAP-V201S-WJP": "Vital200S",  # Alt ID Model Vital200S
-    "LAP-V201S-WEU": "Vital200S",  # Alt ID Model Vital200S
-    "LAP-V201S-WUS": "Vital200S",  # Alt ID Model Vital200S
-    "LAP-V201-AUSR": "Vital200S",  # Alt ID Model Vital200S
-    "LAP-V201S-AEUR": "Vital200S",  # Alt ID Model Vital200S
-    "LAP-V201S-AUSR": "Vital200S",  # Alt ID Model Vital200S
-    "Vital100S": "Vital100S",
-    "LAP-V102S-WUS": "Vital100S",  # Alt ID Model Vital100S
-    "LAP-V102S-AASR": "Vital100S",  # Alt ID Model Vital100S
-    "LAP-V102S-WEU": "Vital100S",  # Alt ID Model Vital100S
-    "LAP-V102S-WUK": "Vital100S",  # Alt ID Model Vital100S
-    "EverestAir": "EverestAir",
-    "LAP-EL551S-AUS": "EverestAir",  # Alt ID Model EverestAir
-    "LAP-EL551S-AEUR": "EverestAir",  # Alt ID Model EverestAir
-    "LAP-EL551S-WEU": "EverestAir",  # Alt ID Model EverestAir
-    "LAP-EL551S-WUS": "EverestAir",  # Alt ID Model EverestAir
-    "SmartTowerFan": "SmartTowerFan",
-    "LTF-F422S-KEU": "SmartTowerFan",  # Alt ID Model SmartTowerFan
-    "LTF-F422S-WUSR": "SmartTowerFan",  # Alt ID Model SmartTowerFan
-    "LTF-F422_WJP": "SmartTowerFan",  # Alt ID Model SmartTowerFan
-    "LTF-F422S-WUS": "SmartTowerFan",  # Alt ID Model SmartTowerFan
-}
+for device_name, device in bulb_features.items():
+    if "dimmable" in device["features"]:
+        DEV_TYPE_TO_HA[device_name] = "bulb-dimmable"
+
+    if "color_temp" in device["features"]:
+        DEV_TYPE_TO_HA[device_name] = "bulb-tunable-white"
+
+for device_name in air_features:
+    DEV_TYPE_TO_HA[device_name] = "fan"
+
+for device_name in outlet_config:
+    DEV_TYPE_TO_HA[device_name] = "outlet"
+
+for device_name, device in switch_features.items():
+    if "dimmable" in device["features"]:
+        DEV_TYPE_TO_HA[device_name] = "walldimmer"
+    else:
+        DEV_TYPE_TO_HA[device_name] = "switch"
+
+SKU_TO_BASE_DEVICE = {}
+
+for device_name, device_data in (air_features | humid_features).items():
+    for sku in device_data["models"]:
+        SKU_TO_BASE_DEVICE[sku] = device_name
