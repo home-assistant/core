db8956d478d9f7eb6dd3960f49cdf203eed98b21
ZephireNZ <brynley+github@zephire.nz>
2025-02-10 03:37:48 +0000

    Add diagnostics to flick_electric

---
+++ homeassistant/components/flick_electric/diagnostics.py
@@ -0,0 +1,25 @@
+"""Diagnostics for the Flick Electric integration."""
+
+from typing import Any
+
+from homeassistant.const import CONF_CLIENT_SECRET, CONF_PASSWORD
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.redact import async_redact_data
+
+from .coordinator import FlickConfigEntry
+
+TO_REDACT = [
+    CONF_PASSWORD,
+    CONF_CLIENT_SECRET,
+]
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: FlickConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+
+    return {
+        "entry_data": async_redact_data(entry.data, TO_REDACT),
+        "data": entry.runtime_data.data,
+    }
