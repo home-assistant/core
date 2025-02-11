18921b832c54cbd9e2d82a248abc39a4aa808ddd
Ravaka Razafimanantsoa <contact@ravaka.dev>
2025-02-11 11:35:49 +0000

    Updating code

--- homeassistant/components/smart_meter_b_route/sensor.py
+++ homeassistant/components/smart_meter_b_route/sensor.py
@@ -12,7 +12,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfElectricCurrent, UnitOfEnergy, UnitOfPower
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceInfo
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
@@ -73,7 +73,7 @@ SENSOR_DESCRIPTIONS = (
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback,
+    async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Set up Smart Meter B-route entry."""
     coordinator: BRouteUpdateCoordinator = entry.runtime_data
