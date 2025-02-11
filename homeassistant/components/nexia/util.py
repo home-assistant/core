4dc8ce9dcc6b79bc9814330690d0823627565fef
David Curran <currand@gmail.com>
2025-02-11 23:02:03 +0000

    Add closest_value function in utils

--- homeassistant/components/nexia/util.py
+++ homeassistant/components/nexia/util.py
@@ -16,3 +16,18 @@ def percent_conv(val):
     if val is None:
         return None
     return round(val * 100.0, 1)
+
+
+def closest_value(range_tuple, step, target):
+    """Find the closest integer value to the target within a specified range and step.
+
+    :param range_tuple: Tuple with the start and end of the range
+    :param step: Step between values
+    :param target: Target value
+    :return: Closest value
+    """
+    # Generate values in the specified range with the given step
+    values = list(range(range_tuple[0], range_tuple[1] + 1, step))
+
+    # Find the closest value
+    return min(values, key=lambda v: abs(v - target))
