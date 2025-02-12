ccf58ef826392cb99a23a8a611af396f4c671124
proohit <direnc99@gmail.com>
2025-02-12 19:00:07 +0000

    refactor(tado): simplifies tests

--- tests/components/tado/test_switch.py
+++ tests/components/tado/test_switch.py
@@ -2,6 +2,8 @@
 
 from unittest.mock import patch
 
+import pytest
+
 from homeassistant.components.switch import (
     DOMAIN as SWITCH_DOMAIN,
     SERVICE_TURN_OFF,
@@ -12,18 +14,21 @@ from homeassistant.core import HomeAssistant
 
 from .util import async_init_integration
 
-child_lock_switch_entity = "switch.baseboard_heater_child_lock"
+CHILD_LOCK_SWITCH_ENTITY = "switch.baseboard_heater_child_lock"
 
 
 async def test_child_lock(hass: HomeAssistant) -> None:
     """Test creation of child lock entity."""
 
     await async_init_integration(hass)
-    state = hass.states.get(child_lock_switch_entity)
+    state = hass.states.get(CHILD_LOCK_SWITCH_ENTITY)
     assert state.state == STATE_OFF
 
 
-async def test_set_child_lock_on(hass: HomeAssistant) -> None:
+@pytest.mark.parametrize(
+    ("method", "expected"), [(SERVICE_TURN_ON, True), (SERVICE_TURN_OFF, False)]
+)
+async def test_set_child_lock(hass: HomeAssistant, method, expected) -> None:
     """Test enable child lock on switch."""
 
     await async_init_integration(hass)
@@ -33,29 +38,10 @@ async def test_set_child_lock_on(hass: HomeAssistant) -> None:
     ) as mock_set_state:
         await hass.services.async_call(
             SWITCH_DOMAIN,
-            SERVICE_TURN_ON,
-            {ATTR_ENTITY_ID: child_lock_switch_entity},
+            method,
+            {ATTR_ENTITY_ID: CHILD_LOCK_SWITCH_ENTITY},
             blocking=True,
         )
 
     mock_set_state.assert_called_once()
-    assert mock_set_state.call_args[0][1] is True
-
-
-async def test_set_child_lock_off(hass: HomeAssistant) -> None:
-    """Test disable child lock on switch."""
-
-    await async_init_integration(hass)
-
-    with patch(
-        "homeassistant.components.tado.PyTado.interface.api.Tado.set_child_lock"
-    ) as mock_set_state:
-        await hass.services.async_call(
-            SWITCH_DOMAIN,
-            SERVICE_TURN_OFF,
-            {ATTR_ENTITY_ID: child_lock_switch_entity},
-            blocking=True,
-        )
-
-    mock_set_state.assert_called_once()
-    assert mock_set_state.call_args[0][1] is False
+    assert mock_set_state.call_args[0][1] is expected
