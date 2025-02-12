7b59ad386db45c3ff1dd70ad52e71f96a8a1082e
Steven B <51370195+sdb9696@users.noreply.github.com>
2025-02-12 11:11:00 +0000

    Revert "Fix tplink child updates taking up to 60s (#137782)"
    This reverts commit eab510f4400425b6cfaa1b4b4099a8b5095f2f69.

--- homeassistant/components/tplink/entity.py
+++ homeassistant/components/tplink/entity.py
@@ -151,13 +151,7 @@ def async_refresh_after[_T: CoordinatedTPLinkEntity, **_P](
                     "exc": str(ex),
                 },
             ) from ex
-        coordinator = self.coordinator
-        if coordinator.parent_coordinator:
-            # If there is a parent coordinator we need to refresh
-            # the parent as its what provides the power state data
-            # for the child entities.
-            coordinator = coordinator.parent_coordinator
-        await coordinator.async_request_refresh()
+        await self.coordinator.async_request_refresh()
 
     return _async_wrap
 
