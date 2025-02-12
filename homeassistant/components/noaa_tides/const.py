a230227075ec6b45daff02d5676a056eb82a895a
Jeff Delaney <20731268+jdelaney72@users.noreply.github.com>
2025-02-12 18:08:46 +0000

    Add constants file

---
+++ homeassistant/components/noaa_tides/const.py
@@ -0,0 +1,11 @@
+"""Const for NOAA Tides."""
+
+DOMAIN = "noaa_tides"
+NAME = "NOAA Tides"
+
+CONF_STATION_ID = "station_id"
+
+TIMEZONES = ["gmt", "lst", "lst_ldt"]
+UNIT_SYSTEMS = ["english", "metric"]
+
+DEFAULT_TIMEZONE = "lst_ldt"
