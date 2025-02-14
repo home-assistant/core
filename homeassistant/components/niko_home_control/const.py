6dc50d872139fafe8380d8ba608f3eedf050df97
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-02-14 20:53:02 +0100

    changes

--- homeassistant/components/niko_home_control/const.py
+++ homeassistant/components/niko_home_control/const.py
@@ -6,8 +6,16 @@ DOMAIN = "niko_home_control"
 _LOGGER = logging.getLogger(__name__)
 
 
-NIKO_HOME_CONTROL_THERMOSTAT_MODES = {
-    "OFF": 3,
+NIKO_HOME_CONTROL_THERMOSTAT_MODES_MAP = {
+    "off": 3,
     "cool": 4,
     "auto": 5,
 }
+
+
+class NIKO_HOME_CONTROL_THERMOSTAT_MODES(int):
+    """Niko Home Control thermostat modes."""
+
+    OFF = 3
+    COOL = 4
+    AUTO = 5
