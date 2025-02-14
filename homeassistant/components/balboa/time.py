1382d155386b337211e0f35e4dfc075c74955d39
Nathan Spencer <natekspencer@gmail.com>
2025-02-14 19:46:56 +0000

    Reduce code in time entity

--- homeassistant/components/balboa/time.py
+++ homeassistant/components/balboa/time.py
@@ -2,13 +2,13 @@
 
 from __future__ import annotations
 
-from collections.abc import Awaitable, Callable
-from dataclasses import dataclass
 from datetime import time
+import itertools
+from typing import Any
 
 from pybalboa import SpaClient
 
-from homeassistant.components.time import TimeEntity, TimeEntityDescription
+from homeassistant.components.time import TimeEntity
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
@@ -16,49 +16,7 @@ from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from . import BalboaConfigEntry
 from .entity import BalboaEntity
 
-
-@dataclass(frozen=True, kw_only=True)
-class BalboaTimeEntityDescription(TimeEntityDescription):
-    """A class that describes Balboa time entities."""
-
-    set_fn: Callable[[SpaClient, time], Awaitable[None]]
-    value_fn: Callable[[SpaClient], time | None]
-
-
-TIME_DESCRIPTIONS = (
-    BalboaTimeEntityDescription(
-        key="filter_cycle_1_start",
-        entity_category=EntityCategory.CONFIG,
-        translation_key="filter_cycle_start",
-        translation_placeholders={"index": "1"},
-        set_fn=lambda spa, start: spa.configure_filter_cycle(1, start=start),
-        value_fn=lambda spa: spa.filter_cycle_1_start,
-    ),
-    BalboaTimeEntityDescription(
-        key="filter_cycle_1_end",
-        entity_category=EntityCategory.CONFIG,
-        translation_key="filter_cycle_end",
-        translation_placeholders={"index": "1"},
-        set_fn=lambda spa, end: spa.configure_filter_cycle(1, end=end),
-        value_fn=lambda spa: spa.filter_cycle_1_end,
-    ),
-    BalboaTimeEntityDescription(
-        key="filter_cycle_2_start",
-        entity_category=EntityCategory.CONFIG,
-        translation_key="filter_cycle_start",
-        translation_placeholders={"index": "2"},
-        set_fn=lambda spa, start: spa.configure_filter_cycle(2, start=start),
-        value_fn=lambda spa: spa.filter_cycle_2_start,
-    ),
-    BalboaTimeEntityDescription(
-        key="filter_cycle_2_end",
-        entity_category=EntityCategory.CONFIG,
-        translation_key="filter_cycle_end",
-        translation_placeholders={"index": "2"},
-        set_fn=lambda spa, end: spa.configure_filter_cycle(2, end=end),
-        value_fn=lambda spa: spa.filter_cycle_2_end,
-    ),
-)
+FILTER_CYCLE = "filter_cycle_"
 
 
 async def async_setup_entry(
@@ -69,27 +27,30 @@ async def async_setup_entry(
     """Set up the spa's times."""
     spa = entry.runtime_data
     async_add_entities(
-        BalboaTimeEntity(spa, description) for description in TIME_DESCRIPTIONS
+        BalboaTimeEntity(spa, index, period)
+        for index, period in itertools.product((1, 2), ("start", "end"))
     )
 
 
 class BalboaTimeEntity(BalboaEntity, TimeEntity):
     """Representation of a Balboa time entity."""
 
-    entity_description: BalboaTimeEntityDescription
+    entity_category = EntityCategory.CONFIG
 
-    def __init__(
-        self, spa: SpaClient, description: BalboaTimeEntityDescription
-    ) -> None:
+    def __init__(self, spa: SpaClient, index: int, period: str) -> None:
         """Initialize a Balboa time entity."""
-        super().__init__(spa, description.key)
-        self.entity_description = description
+        super().__init__(spa, f"{FILTER_CYCLE}{index}_{period}")
+        self.index = index
+        self.period = period
+        self._attr_translation_key = f"{FILTER_CYCLE}{period}"
+        self._attr_translation_placeholders = {"index": str(index)}
 
     @property
     def native_value(self) -> time | None:
         """Return the value reported by the time."""
-        return self.entity_description.value_fn(self._client)
+        return getattr(self._client, f"{FILTER_CYCLE}{self.index}_{self.period}")
 
     async def async_set_value(self, value: time) -> None:
         """Change the time."""
-        await self.entity_description.set_fn(self._client, value)
+        args: dict[str, Any] = {self.period: value}
+        await self._client.configure_filter_cycle(self.index, **args)
