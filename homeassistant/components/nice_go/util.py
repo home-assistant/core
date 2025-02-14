7011644628a2cd81fbf2d8884d3aecebd850d5ef
IceBotYT <34712694+IceBotYT@users.noreply.github.com>
2025-02-14 18:55:17 +0000

    Decorator typing

--- homeassistant/components/nice_go/util.py
+++ homeassistant/components/nice_go/util.py
@@ -1,6 +1,8 @@
 """Utilities for Nice G.O."""
 
+from collections.abc import Callable, Coroutine
 from functools import wraps
+from typing import Any, Protocol, runtime_checkable
 
 from aiohttp import ClientError
 from nice_go import ApiError, AuthFailedError
@@ -11,12 +13,27 @@ from homeassistant.helpers.update_coordinator import UpdateFailed
 from .const import DOMAIN
 
 
-def retry(translation_key: str):
+@runtime_checkable
+class _ArgsProtocol(Protocol):
+    coordinator: Any
+    hass: Any
+
+
+def retry[_R, **P](
+    translation_key: str,
+) -> Callable[
+    [Callable[P, Coroutine[Any, Any, _R]]], Callable[P, Coroutine[Any, Any, _R]]
+]:
     """Retry decorator to handle API errors."""
 
-    def decorator(func):
+    def decorator(
+        func: Callable[P, Coroutine[Any, Any, _R]],
+    ) -> Callable[P, Coroutine[Any, Any, _R]]:
         @wraps(func)
-        async def wrapper(*args, **kwargs):
+        async def wrapper(*args: P.args, **kwargs: P.kwargs):
+            instance = args[0]
+            if not isinstance(instance, _ArgsProtocol):
+                raise TypeError("First argument must have correct attributes")
             try:
                 return await func(*args, **kwargs)
             except (ApiError, ClientError) as err:
@@ -28,7 +45,7 @@ def retry(translation_key: str):
             except AuthFailedError:
                 # Try refreshing token and retry
                 try:
-                    await args[0].coordinator.update_refresh_token()
+                    await instance.coordinator.update_refresh_token()
                     return await func(*args, **kwargs)
                 except (ApiError, ClientError, UpdateFailed) as err:
                     raise HomeAssistantError(
@@ -37,7 +54,7 @@ def retry(translation_key: str):
                         translation_placeholders={"exception": str(err)},
                     ) from err
                 except (AuthFailedError, ConfigEntryAuthFailed) as err:
-                    args[0].coordinator.config_entry.async_start_reauth(args[0].hass)
+                    instance.coordinator.config_entry.async_start_reauth(instance.hass)
                     raise HomeAssistantError(
                         translation_domain=DOMAIN,
                         translation_key=translation_key,
