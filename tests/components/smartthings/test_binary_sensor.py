910696985f6e17a8cb0b654dddbc336ee36a197b
Joostlek <joostlek@outlook.com>
2025-02-14 17:44:01 +0100

    Finalize

--- tests/components/smartthings/test_binary_sensor.py
+++ tests/components/smartthings/test_binary_sensor.py
@@ -2,13 +2,15 @@
 
 from unittest.mock import AsyncMock
 
+from pysmartthings import Attribute, Capability
+import pytest
 from syrupy import SnapshotAssertion
 
-from homeassistant.const import Platform
+from homeassistant.const import STATE_OFF, STATE_ON, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
-from . import setup_integration, snapshot_smartthings_entities
+from . import setup_integration, snapshot_smartthings_entities, trigger_update
 
 from tests.common import MockConfigEntry
 
@@ -26,3 +28,26 @@ async def test_all_entities(
     snapshot_smartthings_entities(
         hass, entity_registry, snapshot, Platform.BINARY_SENSOR
     )
+
+
+@pytest.mark.parametrize("fixture", ["da_ref_normal_000001"])
+async def test_state_update(
+    hass: HomeAssistant,
+    devices: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test state update."""
+    await setup_integration(hass, mock_config_entry)
+
+    assert hass.states.get("binary_sensor.refrigerator_contact").state == STATE_OFF
+
+    await trigger_update(
+        hass,
+        devices,
+        "7db87911-7dce-1cf2-7119-b953432a2f09",
+        Capability.CONTACT_SENSOR,
+        Attribute.CONTACT,
+        "open",
+    )
+
+    assert hass.states.get("binary_sensor.refrigerator_contact").state == STATE_ON
