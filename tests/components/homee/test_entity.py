771545b67510cd3e35576fc7369a9d8cc8d65c81
Taraman17 <mozilla@adrario.de>
2025-02-14 15:16:56 +0000

    add tests for better coverage

---
+++ tests/components/homee/test_entity.py
@@ -0,0 +1,105 @@
+"""Test Homee entity in general."""
+
+from unittest.mock import MagicMock
+
+from homeassistant.components.homeassistant import (
+    DOMAIN as HA_DOMAIN,
+    SERVICE_UPDATE_ENTITY,
+)
+from homeassistant.const import ATTR_ENTITY_ID, STATE_UNAVAILABLE
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
+
+from . import build_mock_node, setup_integration
+
+from tests.common import MockConfigEntry
+
+
+async def test_entity_connection_listener(
+    hass: HomeAssistant,
+    mock_homee: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test if loss of connection is sensed correctly."""
+    mock_homee.nodes = [build_mock_node("sensors.json")]
+    await setup_integration(hass, mock_config_entry)
+
+    states = hass.states.get("sensor.test_multisensor_energy_1")
+    assert states.state != STATE_UNAVAILABLE
+
+    mock_homee.add_connection_listener.call_args_list[1][0][0](False)
+    await hass.async_block_till_done()
+
+    states = hass.states.get("sensor.test_multisensor_energy_1")
+    assert states.state == STATE_UNAVAILABLE
+
+    mock_homee.add_connection_listener.call_args_list[1][0][0](True)
+    await hass.async_block_till_done()
+
+    states = hass.states.get("sensor.test_multisensor_energy_1")
+    assert states.state != STATE_UNAVAILABLE
+
+
+async def test_node_entity_connection_listener(
+    hass: HomeAssistant,
+    mock_homee: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test if loss of connection is sensed correctly."""
+    mock_homee.nodes = [build_mock_node("cover_with_position_slats.json")]
+    await setup_integration(hass, mock_config_entry)
+
+    states = hass.states.get("cover.test_cover")
+    assert states.state != STATE_UNAVAILABLE
+
+    mock_homee.add_connection_listener.call_args_list[1][0][0](False)
+    await hass.async_block_till_done()
+
+    states = hass.states.get("cover.test_cover")
+    assert states.state == STATE_UNAVAILABLE
+
+    mock_homee.add_connection_listener.call_args_list[1][0][0](True)
+    await hass.async_block_till_done()
+
+    states = hass.states.get("cover.test_cover")
+    assert states.state != STATE_UNAVAILABLE
+
+
+async def test_entity_update_action(
+    hass: HomeAssistant,
+    mock_homee: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test the update_entity action for a HomeeEntity."""
+    mock_homee.nodes = [build_mock_node("sensors.json")]
+    await setup_integration(hass, mock_config_entry)
+    await async_setup_component(hass, HA_DOMAIN, {})
+
+    await hass.services.async_call(
+        HA_DOMAIN,
+        SERVICE_UPDATE_ENTITY,
+        {ATTR_ENTITY_ID: "sensor.test_multisensor_temperature"},
+        blocking=True,
+    )
+
+    mock_homee.update_attribute.assert_called_once_with(1, 23)
+
+
+async def test_node_entity_update_action(
+    hass: HomeAssistant,
+    mock_homee: MagicMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test the update_entity action for a HomeeEntity."""
+    mock_homee.nodes = [build_mock_node("cover_with_position_slats.json")]
+    await setup_integration(hass, mock_config_entry)
+    await async_setup_component(hass, HA_DOMAIN, {})
+
+    await hass.services.async_call(
+        HA_DOMAIN,
+        SERVICE_UPDATE_ENTITY,
+        {ATTR_ENTITY_ID: "cover.test_cover"},
+        blocking=True,
+    )
+
+    mock_homee.update_node.assert_called_once_with(3)
--- tests/components/homee/test_init.py
+++ tests/components/homee/test_init.py
@@ -1,11 +1,9 @@
-"""Test Homee entity in general."""
+"""Test Homee initialization."""
 
 from unittest.mock import MagicMock
 
 from homeassistant.components.homee.const import DOMAIN
-from homeassistant.components.sensor import SensorDeviceClass, SensorStateClass
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import STATE_UNAVAILABLE, UnitOfTemperature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 
@@ -23,13 +21,14 @@ async def test_general_data(
     device_registry: dr.DeviceRegistry,
 ) -> None:
     """Test if data is set correctly."""
-    mock_homee.nodes = [build_mock_node("cover_with_position_slats.json")]
+    mock_homee.nodes = [
+        build_mock_node("cover_with_position_slats.json"),
+        build_mock_node("homee.json"),
+    ]
     await setup_integration(hass, mock_config_entry)
 
     # Verify hub created correctly.
-    hub = device = device_registry.async_get_device(
-        identifiers={(DOMAIN, f"{HOMEE_ID}")}
-    )
+    hub = device_registry.async_get_device(identifiers={(DOMAIN, f"{HOMEE_ID}")})
     assert hub.name == HOMEE_NAME
     assert hub.manufacturer == "homee"
     assert hub.model == "homee"
@@ -58,69 +57,39 @@ async def test_general_data(
     assert entity_registry.async_get(cover.entity_id).unique_id == f"{HOMEE_ID}-3-1"
 
     # For a NodeSensor.
-    node_state = entity_registry.async_get("sensor.test_cover_node_state")
+    node_state = entity_registry.async_get("sensor.testhomee_node_state")
     assert node_state.disabled
-    assert node_state.unique_id == f"{HOMEE_ID}-3-state"
+    assert node_state.unique_id == f"{HOMEE_ID}--1-state"
 
 
-async def test_connection_listener(
+async def test_software_version(
     hass: HomeAssistant,
     mock_homee: MagicMock,
     mock_config_entry: MockConfigEntry,
+    device_registry: dr.DeviceRegistry,
 ) -> None:
-    """Test if loss of connection is sensed correctly."""
-    mock_homee.nodes = [build_mock_node("cover_with_position_slats.json")]
+    """Test sw_version for device with only AttributeType.SOFTWARE_VERSION."""
+    mock_homee.nodes = [build_mock_node("cover_without_position.json")]
     await setup_integration(hass, mock_config_entry)
 
-    states = hass.states.get("cover.test_cover")
-    assert states.state != STATE_UNAVAILABLE
-
-    mock_homee.add_connection_listener.call_args_list[1][0][0](False)
-    await hass.async_block_till_done()
-
-    states = hass.states.get("cover.test_cover")
-    assert states.state == STATE_UNAVAILABLE
-
-    mock_homee.add_connection_listener.call_args_list[1][0][0](True)
-    await hass.async_block_till_done()
-
-    states = hass.states.get("cover.test_cover")
-    assert states.state != STATE_UNAVAILABLE
+    device = device_registry.async_get_device(identifiers={(DOMAIN, f"{HOMEE_ID}-3")})
+    assert device.sw_version == "1.45"
 
 
-async def test_homee_entity_listener(
+async def test_invalid_profile(
     hass: HomeAssistant,
     mock_homee: MagicMock,
     mock_config_entry: MockConfigEntry,
+    device_registry: dr.DeviceRegistry,
 ) -> None:
-    """Test update listener of HomeeEntity."""
-    mock_homee.nodes = [build_mock_node("cover_with_position_slats.json")]
+    """Test unknown value passed to get_name_for_enum."""
+    mock_homee.nodes = [build_mock_node("cover_without_position.json")]
+    # This is a profile, that does not exist in the enum.
+    mock_homee.nodes[0].profile = 77
     await setup_integration(hass, mock_config_entry)
 
-    states = hass.states.get("sensor.test_cover_temperature")
-    assert states.state == "20.3"
-    assert states.attributes["friendly_name"] == "Test Cover Temperature"
-    assert states.attributes["state_class"] == SensorStateClass.MEASUREMENT
-    assert states.attributes["device_class"] == SensorDeviceClass.TEMPERATURE
-    assert states.attributes["unit_of_measurement"] == UnitOfTemperature.CELSIUS
-
-    temp_sensor = mock_homee.nodes[0].attributes[3]
-    temp_sensor.current_value = -5.2
-    temp_sensor.add_on_changed_listener.call_args_list[0][0][0](temp_sensor)
-    await hass.async_block_till_done()
-
-    states = hass.states.get("sensor.test_cover_temperature")
-    assert states.state == "-5.2"
-
-
-async def test_homee_node_entity_listener(
-    hass: HomeAssistant,
-    mock_homee: MagicMock,
-    mock_config_entry: MockConfigEntry,
-) -> None:
-    """Test update listener of HomeeNodeEntity."""
-    mock_homee.nodes = [build_mock_node("cover_with_position_slats.json")]
-    await setup_integration(hass, mock_config_entry)
+    device = device_registry.async_get_device(identifiers={(DOMAIN, f"{HOMEE_ID}-3")})
+    assert device.model is None
 
 
 async def test_unload_entry(
