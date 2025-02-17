eef03db3e3a2bff68d9d55904d496e42efc1a9b8
Jonas Fors Lellky <lellky@gmail.com>
2025-02-17 16:18:46 +0100

    Add translations for exceptions (#138669)
    * Add translations for exceptions
    
    * Review comment
    
    * Add translation for exception in the coordinator
    
    * Use same translation string for switch exceptions

--- homeassistant/components/flexit_bacnet/climate.py
+++ homeassistant/components/flexit_bacnet/climate.py
@@ -25,6 +25,7 @@ from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from .const import (
+    DOMAIN,
     MAX_TEMP,
     MIN_TEMP,
     PRESET_TO_VENTILATION_MODE_MAP,
@@ -133,7 +134,13 @@ class FlexitClimateEntity(FlexitEntity, ClimateEntity):
         try:
             await self.device.set_ventilation_mode(ventilation_mode)
         except (asyncio.exceptions.TimeoutError, ConnectionError, DecodingError) as exc:
-            raise HomeAssistantError from exc
+            raise HomeAssistantError(
+                translation_domain=DOMAIN,
+                translation_key="set_preset_mode",
+                translation_placeholders={
+                    "preset": str(ventilation_mode),
+                },
+            ) from exc
         finally:
             await self.coordinator.async_refresh()
 
@@ -153,6 +160,12 @@ class FlexitClimateEntity(FlexitEntity, ClimateEntity):
             else:
                 await self.device.set_ventilation_mode(VENTILATION_MODE_HOME)
         except (asyncio.exceptions.TimeoutError, ConnectionError, DecodingError) as exc:
-            raise HomeAssistantError from exc
+            raise HomeAssistantError(
+                translation_domain=DOMAIN,
+                translation_key="set_hvac_mode",
+                translation_placeholders={
+                    "mode": str(hvac_mode),
+                },
+            ) from exc
         finally:
             await self.coordinator.async_refresh()
