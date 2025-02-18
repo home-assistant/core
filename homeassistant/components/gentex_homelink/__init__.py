c6b72e4590ec3bb05dbaba84ecb1367e8550d5d7
Ryan Jones <ryan.jones@gentex.com>
2025-02-18 19:43:47 +0000

    Remove update_listener

--- homeassistant/components/gentex_homelink/__init__.py
+++ homeassistant/components/gentex_homelink/__init__.py
@@ -49,18 +49,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: HomeLinkConfigEntry) -> 
     }
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    if update_listener not in entry.update_listeners:
-        entry.add_update_listener(update_listener)
-
-    if update_listener not in entry.update_listeners:
-        entry.add_update_listener(update_listener)
     await coordinator.async_config_entry_first_refresh()
     return True
-
-
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Update listener."""
-    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: HomeLinkConfigEntry) -> bool:
