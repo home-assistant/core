0fd8e8d1039c1ae9697abc89191ceaadb12d5dac
Josef Zweck <josef@zweck.dev>
2025-02-19 13:28:09 +0000

    adapt signaltype

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -1253,9 +1253,7 @@ class ConfigEntry[_DataT = Any]:
         )
 
     @callback
-    def async_create_task[
-        _R
-    ](
+    def async_create_task[_R](
         self,
         hass: HomeAssistant,
         target: Coroutine[Any, Any, _R],
@@ -1279,9 +1277,7 @@ class ConfigEntry[_DataT = Any]:
         return task
 
     @callback
-    def async_create_background_task[
-        _R
-    ](
+    def async_create_background_task[_R](
         self,
         hass: HomeAssistant,
         target: Coroutine[Any, Any, _R],
@@ -1635,9 +1631,9 @@ class ConfigEntriesFlowManager(
         if existing_entry is not None:
             # Unload and remove the existing entry, but don't clean up devices and
             # entities until the new entry is added
-            await self.config_entries._async_remove(
+            await self.config_entries._async_remove(  # noqa: SLF001
                 existing_entry.entry_id
-            )  # noqa: SLF001
+            )
         await self.config_entries.async_add(entry)
 
         if existing_entry is not None:
