ad744144ccb136134b22f986a19c78e7ff8d243d
Ryan Jones <ryan.jones@gentex.com>
2025-02-19 15:21:43 +0000

    Fix config_flow formatting

--- homeassistant/components/gentex_homelink/config_flow.py
+++ homeassistant/components/gentex_homelink/config_flow.py
@@ -1,32 +1,33 @@
 """Config flow for homelink."""
 
 import asyncio
+import logging
 import time
 from typing import Any
 
+import botocore.exceptions
 from homelink.auth.srp_auth import SRPAuth
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 
 @config_entries.HANDLERS.register(DOMAIN)
 class SRPFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow to handle homelink OAuth2 authentication."""
 
-    DOMAIN = DOMAIN
-    VERSION = 1
-    MINOR_VERSION = 1
-
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> config_entries.ConfigFlowResult:
         """Ask for username and password."""
-        errors = {}
+        errors: dict[str, str] = {}
         if user_input is not None:
-            self._async_abort_entries_match({"email": user_input["email"]})
+            self._async_abort_entries_match({CONF_EMAIL: user_input[CONF_EMAIL]})
 
             srp_auth = SRPAuth()
             loop = asyncio.get_running_loop()
@@ -34,11 +35,14 @@ class SRPFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 tokens = await loop.run_in_executor(
                     None,
                     srp_auth.async_get_access_token,
-                    user_input["email"],
-                    user_input["password"],
+                    user_input[CONF_EMAIL],
+                    user_input[CONF_PASSWORD],
                 )
-            except Exception:  # noqa: BLE001
+            except botocore.exceptions.ClientError:
                 errors["base"] = "Error authenticating HomeLink account"
+            except Exception:
+                _LOGGER.exception("An unexpected error occurred")
+                errors["base"] = "unknown"
             else:
                 new_token = {}
                 new_token["access_token"] = tokens["AuthenticationResult"][
@@ -54,18 +58,18 @@ class SRPFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 )
 
                 return self.async_create_entry(
-                    title="Token entry",
+                    title=f"{user_input[CONF_EMAIL]} HomeLink integration",
                     data={
                         "token": new_token,
                         "auth_implementation": DOMAIN,
                         "last_update_id": None,
-                        "email": user_input["email"],
+                        CONF_EMAIL: user_input[CONF_EMAIL],
                     },
                 )
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema(
-                {vol.Required("email"): str, vol.Required("password"): str}
+                {vol.Required(CONF_EMAIL): str, vol.Required(CONF_PASSWORD): str}
             ),
             errors=errors,
         )
