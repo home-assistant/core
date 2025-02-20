3b41ff564f66932062edc0eee494d3767e42e525
Tom Matheussen <tommatheussen5@hotmail.com>
2025-02-20 11:10:56 +0000

    Add initial options flow

--- homeassistant/components/satel_integra/config_flow.py
+++ homeassistant/components/satel_integra/config_flow.py
@@ -2,14 +2,28 @@
 
 from __future__ import annotations
 
+from copy import deepcopy
+import logging
+from typing import Any
+
 from satel_integra.satel_integra import AsyncSatel
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult, OptionsFlow
 from homeassistant.const import CONF_CODE, CONF_HOST, CONF_PORT
+from homeassistant.core import callback
 from homeassistant.helpers import config_validation as cv
 
-from .const import DEFAULT_PORT, DOMAIN
+from .const import DEFAULT_PORT, DOMAIN, SatelConfigEntry
+
+_LOGGER = logging.getLogger(__package__)
+
+CONF_ACTION_NUMBER = "number"
+CONF_ACTION = "action"
+
+ACTION_EDIT = "edit"
+ACTION_ADD = "add"
+ACTION_DELETE = "delete"
 
 CONNECTION_SCHEMA = vol.Schema(
     {
@@ -19,11 +33,25 @@ CONNECTION_SCHEMA = vol.Schema(
     }
 )
 
+CODE_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_CODE): cv.string,
+    }
+)
+
 
 class SatelConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a Satel Integra config flow."""
 
     VERSION = 1
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: SatelConfigEntry,
+    ) -> SatelOptionsFlow:
+        """Create the options flow."""
+        return SatelOptionsFlow(config_entry)
 
     async def async_step_user(self, user_input: dict | None = None) -> ConfigFlowResult:
         """Handle a flow initialized by the user."""
@@ -36,7 +64,12 @@ class SatelConfigFlow(ConfigFlow, domain=DOMAIN):
 
             if valid:
                 return self.async_create_entry(
-                    title=user_input[CONF_HOST], data=user_input
+                    title=user_input[CONF_HOST],
+                    data={
+                        CONF_HOST: user_input[CONF_HOST],
+                        CONF_PORT: user_input[CONF_PORT],
+                    },
+                    options={CONF_CODE: user_input.get(CONF_CODE)},
                 )
 
             errors["base"] = "cannot_connect"
@@ -55,3 +88,35 @@ class SatelConfigFlow(ConfigFlow, domain=DOMAIN):
         controller.close()
 
         return result
+
+
+class SatelOptionsFlow(OptionsFlow):
+    """Handle Satel options flow."""
+
+    def __init__(self, config_entry: SatelConfigEntry) -> None:
+        """Initialize Satel options."""
+        self.options = deepcopy(dict(config_entry.options))
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Init step."""
+        return self.async_show_menu(
+            step_id="init", menu_options=["general", "partitions"]
+        )
+
+    async def async_step_general(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """General step."""
+        if user_input is not None:
+            return self.async_create_entry(
+                data={**self.options, CONF_CODE: user_input.get(CONF_CODE)}
+            )
+
+        return self.async_show_form(
+            step_id="general",
+            data_schema=self.add_suggested_values_to_schema(
+                CODE_SCHEMA, self.options.get(CONF_CODE)
+            ),
+        )
