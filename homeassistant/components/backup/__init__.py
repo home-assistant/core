34964462c0a3666232bc4e7d8fe7c9854aa87af0
Erik Montnemery <erik@montnemery.com>
2025-02-20 16:06:33 +0100

    Validate hassio backup settings (#138880)
    * Validate hassio backup settings
    
    * Add snapshots
    
    * Don't reset addon and folder settings
    
    * Adapt to changes in BackupConfig.update

--- homeassistant/components/backup/__init__.py
+++ homeassistant/components/backup/__init__.py
@@ -16,7 +16,7 @@ from .agent import (
     BackupAgentPlatformProtocol,
     LocalBackupAgent,
 )
-from .config import BackupConfig
+from .config import BackupConfig, CreateBackupParametersDict
 from .const import DATA_MANAGER, DOMAIN
 from .http import async_register_http_views
 from .manager import (
@@ -55,6 +55,7 @@ __all__ = [
     "BackupReaderWriter",
     "BackupReaderWriterError",
     "CreateBackupEvent",
+    "CreateBackupParametersDict",
     "CreateBackupStage",
     "CreateBackupState",
     "Folder",
