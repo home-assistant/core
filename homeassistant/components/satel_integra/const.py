f74d0b1271ea3d99627216b98558412d70bfd0a7
Tom Matheussen <tommatheussen5@hotmail.com>
2025-02-20 14:10:40 +0000

    Clean up some constants

--- homeassistant/components/satel_integra/const.py
+++ homeassistant/components/satel_integra/const.py
@@ -5,18 +5,14 @@ from dataclasses import dataclass
 from satel_integra.satel_integra import AsyncSatel
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
 
 DEFAULT_CONF_ARM_HOME_MODE = 1
 DEFAULT_PORT = 7094
-DEFAULT_ZONE_TYPE = "motion"
 
 DOMAIN = "satel_integra"
 
-CONF_DEVICE_CODE = "code"
 CONF_DEVICE_PARTITIONS = "partitions"
 CONF_ARM_HOME_MODE = "arm_home_mode"
-CONF_ZONE_NAME = "name"
 CONF_ZONE_TYPE = "type"
 CONF_ZONES = "zones"
 CONF_OUTPUTS = "outputs"
@@ -28,12 +24,6 @@ SIGNAL_PANEL_MESSAGE = "satel_integra.panel_message"
 
 SIGNAL_ZONES_UPDATED = "satel_integra.zones_updated"
 SIGNAL_OUTPUTS_UPDATED = "satel_integra.outputs_updated"
-
-SUPPORTED_PLATFORMS = [
-    Platform.ALARM_CONTROL_PANEL,
-    Platform.BINARY_SENSOR,
-    Platform.SWITCH,
-]
 
 type SatelConfigEntry = ConfigEntry[SatelData]
 
