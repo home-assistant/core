9a92c51dc6256237db9298166bad17c72671b01f
GhoweVege <85890024+GhoweVege@users.noreply.github.com>
2025-02-20 17:56:34 +0000

    Fixed tests, removed unused data, moved sensor tests to snapshots

--- tests/components/vegehub/test_sensor.py
+++ tests/components/vegehub/test_sensor.py
@@ -1,41 +1,64 @@
 """Unit tests for the VegeHub integration's sensor.py."""
 
-from unittest.mock import MagicMock
+from unittest.mock import patch
 
-from homeassistant.components.sensor import SensorDeviceClass
-from homeassistant.components.vegehub.sensor import VegeHubSensor, async_setup_entry
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import UnitOfElectricPotential
+from syrupy.assertion import SnapshotAssertion
+
+from homeassistant.const import (
+    CONF_DEVICE,
+    CONF_HOST,
+    CONF_IP_ADDRESS,
+    CONF_MAC,
+    CONF_WEBHOOK_ID,
+    Platform,
+)
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
-TEST_API_KEY = "1234567890ABCD"
-TEST_SERVER = "http://example.com"
+from . import init_integration
+
+from tests.common import MockConfigEntry, snapshot_platform
+
+TEST_IP = "192.168.0.100"
+TEST_SIMPLE_MAC = "A1B2C3D4E5F6"
+TEST_HOSTNAME = "VegeHub"
+TEST_WEBHOOK_ID = "webhook_id"
+HUB_DATA = {
+    "first_boot": False,
+    "page_updated": False,
+    "error_message": 0,
+    "num_channels": 2,
+    "num_actuators": 2,
+    "version": "3.4.5",
+    "agenda": 1,
+    "batt_v": 9.0,
+    "num_vsens": 0,
+    "is_ac": 0,
+    "has_sd": 0,
+    "on_ap": 0,
+}
 
 
-async def test_async_setup_entry_with_mocked_lib(
-    hass: HomeAssistant, config_entry: ConfigEntry
+async def test_entities(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
 ) -> None:
-    """Test async_setup_entry for adding sensors."""
-    async_add_entities = MagicMock()
-
-    # Call async_setup_entry to add sensors
-    await async_setup_entry(hass, config_entry, async_add_entities)
-
-    # Assert that sensors were added correctly
-    assert async_add_entities.call_count == 1
-    added_sensors = async_add_entities.call_args[0][0]
-    assert len(added_sensors) == 5  # 2 sensors + 2 actuators + 1 battery
-
-
-def test_vegehub_sensor_properties(config_entry: ConfigEntry) -> None:
-    """Test VegeHubSensor properties."""
-    sensor = VegeHubSensor(
-        mac_address="1234567890AB",
-        index=1,
-        dev_name="VegeHub1",
-        coordinator=config_entry.runtime_data.coordinator,
+    """Test all entities."""
+    config_entry = MockConfigEntry(
+        domain="vegehub",
+        data={
+            CONF_MAC: TEST_SIMPLE_MAC,
+            CONF_IP_ADDRESS: TEST_IP,
+            CONF_HOST: TEST_HOSTNAME,
+            CONF_DEVICE: HUB_DATA,
+            CONF_WEBHOOK_ID: TEST_WEBHOOK_ID,
+        },
+        unique_id=TEST_SIMPLE_MAC,
     )
+    config_entry.add_to_hass(hass)
 
-    assert sensor.device_class == SensorDeviceClass.VOLTAGE
-    assert sensor.native_unit_of_measurement == UnitOfElectricPotential.VOLT
-    assert sensor.unique_id == "1234567890ab_1"
+    with patch("homeassistant.components.vegehub.PLATFORMS", [Platform.SENSOR]):
+        await init_integration(hass, config_entry)
+
+    await snapshot_platform(hass, entity_registry, snapshot, config_entry.entry_id)
