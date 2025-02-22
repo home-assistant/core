74f3515064e1955b926331e0654977a46f23501e
Marko Todoric <maretodoric@gmail.com>
2025-02-22 21:16:21 +0100

    Add test case for reauth flow during setup stage.

--- tests/components/backup_sftp/test_init.py
+++ tests/components/backup_sftp/test_init.py
@@ -6,9 +6,12 @@ from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 
+from homeassistant.components.backup_sftp import SFTPConfigEntryData
 from homeassistant.components.backup_sftp.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
+
+from .conftest import USER_INPUT
 
 from tests.common import MockConfigEntry
 
@@ -68,3 +71,41 @@ async def test_setup_error(
     entries = hass.config_entries.async_entries(DOMAIN)
     assert len(entries) == 1
     assert entries[0].state is ConfigEntryState.SETUP_RETRY
+
+
+@patch("homeassistant.components.backup_sftp.BackupAgentClient")
+async def test_setup_error_reauth(
+    backup_agent_client: MagicMock,
+    hass: HomeAssistant,
+    setup_integration: ComponentSetup,
+    async_cm_mock: AsyncMock,
+) -> None:
+    """Test reauth trigger on `BackupAgentClient` failure."""
+    backup_agent_client.return_value = async_cm_mock
+    async_cm_mock.list_backup_location.side_effect = OSError("Error message")
+    await setup_integration()
+
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
+    assert entries[0].state is ConfigEntryState.SETUP_ERROR
+
+    await hass.config_entries.async_unload(entries[0].entry_id)
+    await hass.async_block_till_done()
+
+    assert entries[0].state is ConfigEntryState.NOT_LOADED
+
+
+async def test_config_entry_data_password_hidden() -> None:
+    """Test hiding password in `SFTPConfigEntryData` string representation."""
+    entry_data = SFTPConfigEntryData(**USER_INPUT)
+    assert "password='<hidden>'" in str(entry_data)
+
+    user_input = USER_INPUT.copy()
+    user_input["password"] = None
+    entry_data = SFTPConfigEntryData(**user_input)
+    assert "password=None" in str(entry_data)
+
+    user_input = USER_INPUT.copy()
+    user_input["password"] = ""
+    entry_data = SFTPConfigEntryData(**user_input)
+    assert "password=''" in str(entry_data)
