fb0d44d1dbe882120ba4a7f27a352832b3d1631d
Marko Todoric <maretodoric@gmail.com>
2025-02-23 22:52:31 +0100

    Remove empty line after class docstring.

--- homeassistant/components/backup_sftp/config_flow.py
+++ homeassistant/components/backup_sftp/config_flow.py
@@ -34,7 +34,6 @@ from .const import (
 class SFTPFlowHandler(ConfigFlow, domain=DOMAIN):
     """Handle an SFTP Backup Storage config flow."""
 
-
     async def async_step_reauth(
         self, entry_data: Mapping[str, Any]
     ) -> ConfigFlowResult:
