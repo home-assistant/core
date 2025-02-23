463e40d98a16ed8f3ff968535ac5d49230566e9c
Richard Polzer <richard@polzer-eb.de>
2025-02-23 13:07:54 +0000

    added tests for ekeybionyx config flow

--- tests/components/ekeybionyx/test_config_flow.py
+++ tests/components/ekeybionyx/test_config_flow.py
@@ -80,20 +80,74 @@ async def test_full_flow(
             "expires_in": 60,
         },
     )
+    flow = await hass.config_entries.flow.async_configure(result["flow_id"])
+    assert flow.get("step_id") == "choose_system"
+
+    flow2 = await hass.config_entries.flow.async_configure(
+        flow["flow_id"], {"system": "946DA01F-9ABD-4D9D-80C7-02AF85C822A8"}
+    )
+    assert flow2.get("step_id") == "webhooks"
 
     with patch(
         "homeassistant.components.ekeybionyx.async_setup_entry", return_value=True
     ) as mock_setup:
-        flow = await hass.config_entries.flow.async_configure(result["flow_id"])
-        flow2 = await hass.config_entries.flow.async_configure(
-            flow["flow_id"],
+        flow3 = await hass.config_entries.flow.async_configure(
+            flow2["flow_id"],
             {
                 "webhook1": "Test",
                 "url": "http://localhost:8123",
             },
         )
     assert len(hass.config_entries.async_entries(EKEYBIONYX_DOMAIN)) == 1
-    assert flow.get("step_id") == "webhooks"
-    assert flow2.get("type") is FlowResultType.CREATE_ENTRY
+
+    assert flow3.get("type") is FlowResultType.CREATE_ENTRY
 
     assert len(mock_setup.mock_calls) == 1
+
+
+@pytest.mark.usefixtures("current_request_with_host")
+async def test_no_own_system(
+    hass: HomeAssistant,
+    hass_client_no_auth: ClientSessionGenerator,
+    aioclient_mock: AiohttpClientMocker,
+    setup_credentials: None,
+    no_own_system: None,
+) -> None:
+    """Check no own System flow."""
+    result = await hass.config_entries.flow.async_init(
+        EKEYBIONYX_DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    state = config_entry_oauth2_flow._encode_jwt(
+        hass,
+        {
+            "flow_id": result["flow_id"],
+            "redirect_uri": "https://example.com/auth/external/callback",
+        },
+    )
+
+    assert result["url"] == (
+        f"{OAUTH2_AUTHORIZE}?response_type=code&client_id={CLIENT_ID}"
+        "&redirect_uri=https://example.com/auth/external/callback"
+        f"&state={state}"
+        f"&scope={SCOPE}"
+    )
+
+    client = await hass_client_no_auth()
+    resp = await client.get(f"/auth/external/callback?code=abcd&state={state}")
+    assert resp.status == 200
+    assert resp.headers["content-type"] == "text/html; charset=utf-8"
+
+    aioclient_mock.post(
+        OAUTH2_TOKEN,
+        json={
+            "refresh_token": "mock-refresh-token",
+            "access_token": "mock-access-token",
+            "type": "Bearer",
+            "expires_in": 60,
+        },
+    )
+    flow = await hass.config_entries.flow.async_configure(result["flow_id"])
+
+    assert len(hass.config_entries.async_entries(EKEYBIONYX_DOMAIN)) == 0
+
+    assert flow.get("type") is FlowResultType.ABORT
