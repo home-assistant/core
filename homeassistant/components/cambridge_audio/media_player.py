aa07ae01321099bab164bbfb59c3b9e692d29d92
ichbinsteffen <ichbinsteffen@gmail.com>
2025-02-23 20:00:16 +0100

    [CambridgeAudio Integration] Add switch to enable Control Bus Mode

--- homeassistant/components/cambridge_audio/media_player.py
+++ homeassistant/components/cambridge_audio/media_player.py
@@ -10,6 +10,7 @@ from aiostreammagic import (
     ShuffleMode,
     StreamMagicClient,
     TransportControl,
+    ControlBusMode
 )
 
 from homeassistant.components.media_player import (
@@ -91,6 +92,8 @@ class CambridgeAudioDevice(CambridgeAudioEntity, MediaPlayerEntity):
         features = BASE_FEATURES
         if self.client.state.pre_amp_mode:
             features |= PREAMP_FEATURES
+        if self.client.state.control_bus_mode == ControlBusMode.AMPLIFIER:
+            features |= MediaPlayerEntityFeature.VOLUME_STEP
         if TransportControl.PLAY_PAUSE in controls:
             features |= MediaPlayerEntityFeature.PLAY | MediaPlayerEntityFeature.PAUSE
         for control in controls:
