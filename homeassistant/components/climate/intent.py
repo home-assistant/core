7e54bbe4a863f0e95d4d6288c642608e94f74e22
Michael Hansen <mike@rhasspy.org>
2025-02-24 16:36:25 -0600

    Check area temperature sensors in get temperature intent

--- homeassistant/components/climate/intent.py
+++ homeassistant/components/climate/intent.py
@@ -2,11 +2,14 @@
 
 from __future__ import annotations
 
+from collections.abc import Collection
+
 import voluptuous as vol
 
+from homeassistant.components import sensor
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_validation as cv, intent
+from homeassistant.helpers import area_registry as ar, config_validation as cv, intent
 
 from . import (
     ATTR_TEMPERATURE,
@@ -32,6 +35,9 @@ class GetTemperatureIntent(intent.IntentHandler):
     slot_schema = {
         vol.Optional("area"): intent.non_empty_string,
         vol.Optional("name"): intent.non_empty_string,
+        vol.Optional("floor"): intent.non_empty_string,
+        vol.Optional("preferred_area_id"): cv.string,
+        vol.Optional("preferred_floor_id"): cv.string,
     }
     platforms = {DOMAIN}
 
@@ -48,10 +54,71 @@ class GetTemperatureIntent(intent.IntentHandler):
         if "area" in slots:
             area = slots["area"]["value"]
 
-        match_constraints = intent.MatchTargetsConstraints(
-            name=name, area_name=area, domains=[DOMAIN], assistant=intent_obj.assistant
+        floor_name: str | None = None
+        if "floor" in slots:
+            floor_name = slots["floor"]["value"]
+
+        match_preferences = intent.MatchTargetsPreferences(
+            area_id=slots.get("preferred_area_id", {}).get("value"),
+            floor_id=slots.get("preferred_floor_id", {}).get("value"),
         )
-        match_result = intent.async_match_targets(hass, match_constraints)
+
+        if (not name) and (area or match_preferences.area_id):
+            # Look for temperature sensors assigned to an area
+            area_registry = ar.async_get(hass)
+            area_temperature_ids: dict[str, str] = {}
+
+            # Keep candidates that are registered as area temperature sensors
+            def area_candidate_filter(
+                candidate: intent.MatchTargetsCandidate,
+                possible_area_ids: Collection[str],
+            ) -> bool:
+                for area_id in possible_area_ids:
+                    temperature_id = area_temperature_ids.get(area_id)
+                    if (temperature_id is None) and (
+                        area_entry := area_registry.async_get_area(area_id)
+                    ):
+                        temperature_id = area_entry.temperature_entity_id or ""
+                        area_temperature_ids[area_id] = temperature_id
+
+                    if candidate.state.entity_id == temperature_id:
+                        return True
+
+                return False
+
+            match_constraints = intent.MatchTargetsConstraints(
+                area_name=area,
+                floor_name=floor_name,
+                domains=[sensor.DOMAIN],
+                device_classes=[sensor.SensorDeviceClass.TEMPERATURE],
+                assistant=intent_obj.assistant,
+                single_target=True,
+            )
+            match_result = intent.async_match_targets(
+                hass,
+                match_constraints,
+                match_preferences,
+                area_candidate_filter=area_candidate_filter,
+            )
+            if match_result.is_match:
+                # Found temperature sensor
+                response = intent_obj.create_response()
+                response.response_type = intent.IntentResponseType.QUERY_ANSWER
+                response.async_set_states(matched_states=match_result.states)
+                return response
+
+        # Look for climate devices
+        match_constraints = intent.MatchTargetsConstraints(
+            name=name,
+            area_name=area,
+            floor_name=floor_name,
+            domains=[DOMAIN],
+            assistant=intent_obj.assistant,
+            single_target=True,
+        )
+        match_result = intent.async_match_targets(
+            hass, match_constraints, match_preferences
+        )
         if not match_result.is_match:
             raise intent.MatchFailedError(
                 result=match_result, constraints=match_constraints
