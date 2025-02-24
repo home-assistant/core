3d7910d975cd45b0fb5ac0e505deb63062a069d6
Erik <erik@montnemery.com>
2025-02-24 11:14:45 +0100

    Fix race in WS command recorder/info

--- homeassistant/components/recorder/basic_websocket_api.py
+++ homeassistant/components/recorder/basic_websocket_api.py
@@ -23,27 +23,24 @@ def async_setup(hass: HomeAssistant) -> None:
         vol.Required("type"): "recorder/info",
     }
 )
-@callback
-def ws_info(
+@websocket_api.async_response
+async def ws_info(
     hass: HomeAssistant, connection: websocket_api.ActiveConnection, msg: dict[str, Any]
 ) -> None:
     """Return status of the recorder."""
-    if instance := get_instance(hass):
-        backlog = instance.backlog
-        migration_in_progress = instance.migration_in_progress
-        migration_is_live = instance.migration_is_live
-        recording = instance.recording
-        # We avoid calling is_alive() as it can block waiting
-        # for the thread state lock which will block the event loop.
-        is_running = instance.is_running
-        max_backlog = instance.max_backlog
-    else:
-        backlog = None
-        migration_in_progress = False
-        migration_is_live = False
-        recording = False
-        is_running = False
-        max_backlog = None
+    # pylint: disable-next=import-outside-toplevel
+    from homeassistant.helpers import recorder as recorder_helper
+
+    await hass.data[recorder_helper.DATA_RECORDER].db_connected
+    instance = get_instance(hass)
+    backlog = instance.backlog
+    migration_in_progress = instance.migration_in_progress
+    migration_is_live = instance.migration_is_live
+    recording = instance.recording
+    # We avoid calling is_alive() as it can block waiting
+    # for the thread state lock which will block the event loop.
+    is_running = instance.is_running
+    max_backlog = instance.max_backlog
 
     recorder_info = {
         "backlog": backlog,
