10b56798846b1d69d0955f17aede29393f6cd3ce
Simone Chemelli <simone.chemelli@gmail.com>
2025-02-24 18:02:55 +0000

    leftover

--- homeassistant/components/shelly/config_flow.py
+++ homeassistant/components/shelly/config_flow.py
@@ -141,7 +141,7 @@ class ShellyConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Shelly."""
 
     VERSION = 1
-    MINOR_VERSION = 3
+    MINOR_VERSION = 2
 
     host: str = ""
     port: int = DEFAULT_HTTP_PORT
