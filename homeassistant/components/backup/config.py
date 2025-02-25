8fbd85efec6d8f4ae197d007971cbf7b8db235f3
Martin Hjelmare <marhje52@gmail.com>
2025-02-25 15:49:38 +0100

    Address comment

--- homeassistant/components/backup/config.py
+++ homeassistant/components/backup/config.py
@@ -580,20 +580,6 @@ def check_unavailable_agents(hass: HomeAssistant, manager: BackupManager) -> Non
             "Agents %s are configured for automatic backup but are unavailable",
             missing_agent_ids,
         )
-        for agent_id in missing_agent_ids:
-            ir.async_create_issue(
-                hass,
-                DOMAIN,
-                f"{AUTOMATIC_BACKUP_AGENTS_UNAVAILABLE_ISSUE_ID}_{agent_id}",
-                is_fixable=False,
-                learn_more_url="homeassistant://config/backup",
-                severity=ir.IssueSeverity.WARNING,
-                translation_key="automatic_backup_agents_unavailable",
-                translation_placeholders={
-                    "agent_id": agent_id,
-                    "backup_settings": "/config/backup/settings",
-                },
-            )
 
     # Remove issues for unavailable agents that are not unavailable anymore.
     issue_registry = ir.async_get(hass)
@@ -604,8 +590,24 @@ def check_unavailable_agents(hass: HomeAssistant, manager: BackupManager) -> Non
         and issue_id.startswith(AUTOMATIC_BACKUP_AGENTS_UNAVAILABLE_ISSUE_ID)
     }
     current_missing_agent_issue_ids = {
-        f"{AUTOMATIC_BACKUP_AGENTS_UNAVAILABLE_ISSUE_ID}_{agent_id}"
+        f"{AUTOMATIC_BACKUP_AGENTS_UNAVAILABLE_ISSUE_ID}_{agent_id}": agent_id
         for agent_id in missing_agent_ids
     }
-    for issue_id in existing_missing_agent_issue_ids - current_missing_agent_issue_ids:
+    for issue_id in existing_missing_agent_issue_ids - set(
+        current_missing_agent_issue_ids
+    ):
         ir.async_delete_issue(hass, DOMAIN, issue_id)
+    for issue_id, agent_id in current_missing_agent_issue_ids.items():
+        ir.async_create_issue(
+            hass,
+            DOMAIN,
+            issue_id,
+            is_fixable=False,
+            learn_more_url="homeassistant://config/backup",
+            severity=ir.IssueSeverity.WARNING,
+            translation_key="automatic_backup_agents_unavailable",
+            translation_placeholders={
+                "agent_id": agent_id,
+                "backup_settings": "/config/backup/settings",
+            },
+        )
