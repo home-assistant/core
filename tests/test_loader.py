a3259d9739b010d5ad79b2dc1fc3402fcb730495
J. Nick Koston <nick@koston.org>
2025-02-25 16:06:39 +0000

    tweak test

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2054,7 +2054,7 @@ async def test_async_get_integration(hass: HomeAssistant) -> None:
 
     with patch.object(hass, "async_add_executor_job", slow_load_failure):
         task1 = hass.async_create_task(
-            loader.async_get_integrations(hass, ["does_not_exist"])
+            loader.async_get_integrations(hass, ["does_not_exist", "does_not_exist2"])
         )
         # Task one should now be waiting for executor job
     task2 = hass.async_create_task(
@@ -2062,12 +2062,14 @@ async def test_async_get_integration(hass: HomeAssistant) -> None:
     )
     # Task two should be waiting for the futures created in task one
     task3 = hass.async_create_task(
-        loader.async_get_integrations(hass, ["does_not_exist"])
+        loader.async_get_integrations(hass, ["does_not_exist2", "does_not_exist"])
     )
     # Task three should be waiting for the futures created in task one
     integrations_1 = await task1
     assert isinstance(integrations_1["does_not_exist"], loader.IntegrationNotFound)
+    assert isinstance(integrations_1["does_not_exist2"], loader.IntegrationNotFound)
     integrations_2 = await task2
     assert isinstance(integrations_2["does_not_exist"], loader.IntegrationNotFound)
     integrations_3 = await task3
+    assert isinstance(integrations_3["does_not_exist2"], loader.IntegrationNotFound)
     assert isinstance(integrations_3["does_not_exist"], loader.IntegrationNotFound)
