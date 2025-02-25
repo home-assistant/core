81b1aada03ef234a2af35b393c1392775aa7c4b8
elmurato <1382097+elmurato@users.noreply.github.com>
2025-02-25 13:17:53 +0000

    Add test for service already configured in case of a Java Edition server

--- tests/components/minecraft_server/test_config_flow.py
+++ tests/components/minecraft_server/test_config_flow.py
@@ -88,10 +88,37 @@ async def test_full_flow_bedrock(hass: HomeAssistant) -> None:
         assert result["data"][CONF_TYPE] == MinecraftServerType.BEDROCK_EDITION
 
 
-async def test_service_already_configured(
+async def test_service_already_configured_java(
+    hass: HomeAssistant, java_mock_config_entry: MockConfigEntry
+) -> None:
+    """Test config flow abort if a Java Edition server is already configured."""
+    java_mock_config_entry.add_to_hass(hass)
+
+    with (
+        patch(
+            "homeassistant.components.minecraft_server.api.BedrockServer.lookup",
+            side_effect=ValueError,
+        ),
+        patch(
+            "homeassistant.components.minecraft_server.api.JavaServer.async_lookup",
+            return_value=JavaServer(host=TEST_HOST, port=TEST_PORT),
+        ),
+        patch(
+            "homeassistant.components.minecraft_server.api.JavaServer.async_status",
+            return_value=TEST_JAVA_STATUS_RESPONSE,
+        ),
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_USER}, data=USER_INPUT
+        )
+        assert result["type"] is FlowResultType.ABORT
+        assert result["reason"] == "already_configured"
+
+
+async def test_service_already_configured_bedrock(
     hass: HomeAssistant, bedrock_mock_config_entry: MockConfigEntry
 ) -> None:
-    """Test config flow abort if service is already configured."""
+    """Test config flow abort if a Bedrock Edition server is already configured."""
     bedrock_mock_config_entry.add_to_hass(hass)
 
     with (
@@ -112,7 +139,7 @@ async def test_service_already_configured(
 
 
 async def test_recovery_java(hass: HomeAssistant) -> None:
-    """Test config flow recovery (successful connection after a failed connection)."""
+    """Test config flow recovery with a Java Edition server (successful connection after a failed connection)."""
     with (
         patch(
             "homeassistant.components.minecraft_server.api.BedrockServer.lookup",
@@ -157,7 +184,7 @@ async def test_recovery_java(hass: HomeAssistant) -> None:
 
 
 async def test_recovery_bedrock(hass: HomeAssistant) -> None:
-    """Test config flow recovery (successful connection after a failed connection)."""
+    """Test config flow recovery with a Bedrock Edition server (successful connection after a failed connection)."""
     with (
         patch(
             "homeassistant.components.minecraft_server.api.BedrockServer.lookup",
