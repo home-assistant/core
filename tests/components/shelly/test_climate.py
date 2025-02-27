ca40db1335bd170c42157f0f9cd2c5d0d29e6aa4
Simone Chemelli <simone.chemelli@gmail.com>
2025-02-27 23:00:23 +0100

    Update tests/components/shelly/test_climate.py
    Co-authored-by: Maciej Bieniek <bieniu@users.noreply.github.com>

--- tests/components/shelly/test_climate.py
+++ tests/components/shelly/test_climate.py
@@ -751,8 +751,8 @@ async def test_wall_display_thermostat_mode_external_actuator(
 
     new_status = deepcopy(mock_rpc_device.status)
     new_status["sys"]["relay_in_thermostat"] = False
+    new_status.pop("cover:0")
     monkeypatch.setattr(mock_rpc_device, "status", new_status)
-    monkeypatch.delitem(mock_rpc_device.status, "cover:0")
 
     await init_integration(hass, 2, model=MODEL_WALL_DISPLAY)
 
