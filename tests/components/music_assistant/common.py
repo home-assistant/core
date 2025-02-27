c94f6e2fe0a09441a419707a3c33361ee2e70883
Marcel van der Veldt <m.vanderveldt@outlook.com>
2025-02-27 14:30:29 +0100

    Fix Music Assistant media player entity features (#139428)
    * Fix Music Assistant supported media player features
    
    * Update supported features when player config changes
    
    * Add tests

--- tests/components/music_assistant/common.py
+++ tests/components/music_assistant/common.py
@@ -2,9 +2,11 @@
 
 from __future__ import annotations
 
+import asyncio
 from typing import Any
 from unittest.mock import AsyncMock, MagicMock
 
+from music_assistant_models.api import MassEvent
 from music_assistant_models.enums import EventType
 from music_assistant_models.media_items import Album, Artist, Playlist, Radio, Track
 from music_assistant_models.player import Player
@@ -134,15 +136,42 @@ async def trigger_subscription_callback(
     hass: HomeAssistant,
     client: MagicMock,
     event: EventType = EventType.PLAYER_UPDATED,
+    object_id: str | None = None,
     data: Any = None,
 ) -> None:
     """Trigger a subscription callback."""
     # trigger callback on all subscribers
-    for sub in client.subscribe_events.call_args_list:
-        callback = sub.kwargs["callback"]
-        event_filter = sub.kwargs.get("event_filter")
-        if event_filter in (None, event):
-            callback(event, data)
+    for sub in client.subscribe.call_args_list:
+        cb_func = sub.kwargs.get("cb_func", sub.args[0])
+        event_filter = sub.kwargs.get(
+            "event_filter", sub.args[1] if len(sub.args) > 1 else None
+        )
+        id_filter = sub.kwargs.get(
+            "id_filter", sub.args[2] if len(sub.args) > 2 else None
+        )
+        if not (
+            event_filter is None
+            or event == event_filter
+            or (isinstance(event_filter, list) and event in event_filter)
+        ):
+            continue
+        if not (
+            id_filter is None
+            or object_id == id_filter
+            or (isinstance(id_filter, list) and object_id in id_filter)
+        ):
+            continue
+
+        event = MassEvent(
+            event=event,
+            object_id=object_id,
+            data=data,
+        )
+        if asyncio.iscoroutinefunction(cb_func):
+            await cb_func(event)
+        else:
+            cb_func(event)
+
     await hass.async_block_till_done()
 
 
