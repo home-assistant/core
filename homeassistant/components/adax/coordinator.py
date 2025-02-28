f5e01bac327c3be3ffa86eba53ad4cb9fa25914a
Peter Åslund <peter@peteraslund.me>
2025-02-28 11:47:31 +0100

    Optimized coordinator updates

--- homeassistant/components/adax/coordinator.py
+++ homeassistant/components/adax/coordinator.py
@@ -7,17 +7,13 @@ from typing import Any
 from adax import Adax
 from adax_local import Adax as AdaxLocal
 
-from homeassistant.core import HomeAssistant
-from homeassistant.const import (
-    CONF_IP_ADDRESS,
-    CONF_PASSWORD,
-    CONF_TOKEN,
-)
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_IP_ADDRESS, CONF_PASSWORD, CONF_TOKEN
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import ACCOUNT_ID, CONNECTION_TYPE, DOMAIN, LOCAL
+from .const import ACCOUNT_ID, CONNECTION_TYPE, LOCAL
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -25,7 +21,7 @@ _LOGGER = logging.getLogger(__name__)
 class AdaxCoordinator(DataUpdateCoordinator[dict[str, Any]]):
     """Coordinator for updating data to and from Adax."""
 
-    rooms: dict[str, Any]
+    rooms: list[dict[str, Any]]
 
     def __init__(
         self,
@@ -51,7 +47,8 @@ class AdaxCoordinator(DataUpdateCoordinator[dict[str, Any]]):
 
     def get_room(self, room_id: int) -> dict[str, Any]:
         """Get a specific room from the loaded Adax data."""
-        for room in filter(lambda r: r['id'] == room_id, self.get_rooms()):
+        rooms = self.rooms or []
+        for room in filter(lambda r: r['id'] == room_id, rooms):
             _LOGGER.info("Get Room: %s", room)
             return room
         return None
@@ -60,7 +57,7 @@ class AdaxCoordinator(DataUpdateCoordinator[dict[str, Any]]):
         """Gets all rooms for the account."""
         return self.rooms or []
 
-    async def _async_update_data(self) -> None:
+    async def _async_update_data(self) -> list[dict[str, Any]]:
         """Fetch data from the Adax."""
         try:
             _LOGGER.info("Getting data from Adax")
