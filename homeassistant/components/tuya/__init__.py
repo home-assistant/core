887d706c6523c142502b25438d4d61009a36acb6
jbouwh <jan@jbsoft.nl>
2025-02-28 12:46:07 +0000

    Make the Tuya backend library compatible with the newer paho mqtt client.

--- homeassistant/components/tuya/__init__.py
+++ homeassistant/components/tuya/__init__.py
@@ -3,7 +3,8 @@
 from __future__ import annotations
 
 import logging
-from typing import Any, NamedTuple
+from typing import TYPE_CHECKING, Any, NamedTuple
+from urllib.parse import urlsplit
 
 from tuya_sharing import (
     CustomerDevice,
@@ -11,6 +12,7 @@ from tuya_sharing import (
     SharingDeviceListener,
     SharingTokenListener,
 )
+from tuya_sharing.mq import SharingMQ, SharingMQConfig
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
@@ -45,13 +47,73 @@ class HomeAssistantTuyaData(NamedTuple):
     listener: SharingDeviceListener
 
 
+if TYPE_CHECKING:
+    import paho.mqtt.client as mqtt
+
+
+class CompatManager(Manager):
+    """Compatibility Manager for Tuya.
+
+    This code can be removed when https://github.com/tuya/tuya-device-sharing-sdk/pull/25
+    is merged. It patches the `tuya_sharing.Manager` class to use the correct
+    `paho.mqtt.client` calls to ensure compatibility with the newer versions
+    of the `paho-mqtt` library.
+    """
+
+    def refresh_mq(self):
+        """Refresh the MQTT connection."""
+        if self.mq is not None:
+            self.mq.stop()
+            self.mq = None
+
+        home_ids = [home.id for home in self.user_homes]
+        device = [
+            device
+            for device in self.device_map.values()
+            if hasattr(device, "id") and getattr(device, "set_up", False)
+        ]
+
+        sharing_mq = CompatSharingMQ(self.customer_api, home_ids, device)
+        sharing_mq.start()
+        sharing_mq.add_message_listener(self.on_message)
+        self.mq = sharing_mq
+
+
+class CompatSharingMQ(SharingMQ):
+    """Compatibility wrapper for Tuya SharingMQ class."""
+
+    def _start(self, mq_config: SharingMQConfig) -> mqtt.Client:
+        """Start the MQTT client."""
+        # We don't import on the top because some integrations
+        # should be able to optionally rely on MQTT.
+        import paho.mqtt.client as mqtt  # pylint: disable=import-outside-toplevel
+
+        mqttc = mqtt.Client(client_id=mq_config.client_id)
+        mqttc.username_pw_set(mq_config.username, mq_config.password)
+        mqttc.user_data_set({"mqConfig": mq_config})
+        mqttc.on_connect = self._on_connect
+        mqttc.on_message = self._on_message
+        mqttc.on_subscribe = self._on_subscribe
+        mqttc.on_log = self._on_log
+        mqttc.on_disconnect = self._on_disconnect
+
+        url = urlsplit(mq_config.url)
+        if url.scheme == "ssl":
+            mqttc.tls_set()
+
+        mqttc.connect(url.hostname, url.port)
+
+        mqttc.loop_start()
+        return mqttc
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: TuyaConfigEntry) -> bool:
     """Async setup hass config entry."""
     if CONF_APP_TYPE in entry.data:
         raise ConfigEntryAuthFailed("Authentication failed. Please re-authenticate.")
 
     token_listener = TokenListener(hass, entry)
-    manager = Manager(
+    manager = CompatManager(
         TUYA_CLIENT_ID,
         entry.data[CONF_USER_CODE],
         entry.data[CONF_TERMINAL_ID],
