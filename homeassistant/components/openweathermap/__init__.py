0dae8e4149be910b832e6188a3395b0b6f9c6464
wittypluck <wittypluck@users.noreply.github.com>
2025-03-01 23:56:14 +0100

    Use generic DataUpdateCoordinator to prepare for multiple Coordinator types

--- homeassistant/components/openweathermap/__init__.py
+++ homeassistant/components/openweathermap/__init__.py
@@ -10,6 +10,7 @@ from pyopenweathermap import create_owm_client
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_API_KEY, CONF_LANGUAGE, CONF_MODE, CONF_NAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from .const import CONFIG_FLOW_VERSION, DEFAULT_OWM_MODE, OWM_MODES, PLATFORMS
 from .coordinator import WeatherUpdateCoordinator
@@ -27,7 +28,7 @@ class OpenweathermapData:
 
     name: str
     mode: str
-    coordinator: WeatherUpdateCoordinator
+    coordinator: DataUpdateCoordinator
 
 
 async def async_setup_entry(
@@ -45,13 +46,13 @@ async def async_setup_entry(
         async_delete_issue(hass, entry.entry_id)
 
     owm_client = create_owm_client(api_key, mode, lang=language)
-    weather_coordinator = WeatherUpdateCoordinator(hass, entry, owm_client)
+    owm_coordinator = WeatherUpdateCoordinator(hass, entry, owm_client)
 
-    await weather_coordinator.async_config_entry_first_refresh()
+    await owm_coordinator.async_config_entry_first_refresh()
 
     entry.async_on_unload(entry.add_update_listener(async_update_options))
 
-    entry.runtime_data = OpenweathermapData(name, mode, weather_coordinator)
+    entry.runtime_data = OpenweathermapData(name, mode, owm_coordinator)
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
