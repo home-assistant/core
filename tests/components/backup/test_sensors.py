33acd8f53b41499f4b1f6da0ea2a9f6ab664eb2a
mib1185 <mail@mib85.de>
2025-03-01 17:03:43 +0000

    fix sensor tests

--- tests/components/backup/test_sensors.py
+++ tests/components/backup/test_sensors.py
@@ -5,14 +5,13 @@ from unittest.mock import patch
 from syrupy import SnapshotAssertion
 
 from homeassistant.components.backup.const import DOMAIN
-from homeassistant.config_entries import SOURCE_SYSTEM, ConfigEntryState
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
 from .common import setup_backup_integration
 
-from tests.common import MockConfigEntry, snapshot_platform
+from tests.common import snapshot_platform
 
 
 async def test_setup_sensors(
@@ -21,13 +20,9 @@ async def test_setup_sensors(
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test setup backup config entry."""
-    await setup_backup_integration(hass, with_hassio=False)
-    entry = MockConfigEntry(domain=DOMAIN, source=SOURCE_SYSTEM)
-    entry.add_to_hass(hass)
-
     with patch("homeassistant.components.backup.PLATFORMS", [Platform.SENSOR]):
-        assert await hass.config_entries.async_setup(entry.entry_id)
+        await setup_backup_integration(hass, with_hassio=False)
         await hass.async_block_till_done(wait_background_tasks=True)
-    assert entry.state is ConfigEntryState.LOADED
 
+    entry = hass.config_entries.async_entries(DOMAIN)[0]
     await snapshot_platform(hass, entity_registry, snapshot, entry.entry_id)
