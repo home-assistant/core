ad04b5361518f1afd980e1d4f2ba1d271e90f88a
J. Nick Koston <nick@koston.org>
2025-03-02 08:50:35 -0700

    Fix broken link in ESPHome BLE repair (#139639)
    ESPHome always uses .0 in the URL for the changelog,
    and we never had a patch version in the stable
    BLE version field so we need to switch it to
    .0 for the URL.

--- homeassistant/components/esphome/const.py
+++ homeassistant/components/esphome/const.py
@@ -18,6 +18,8 @@ STABLE_BLE_VERSION = AwesomeVersion(STABLE_BLE_VERSION_STR)
 PROJECT_URLS = {
     "esphome.bluetooth-proxy": "https://esphome.github.io/bluetooth-proxies/",
 }
-DEFAULT_URL = f"https://esphome.io/changelog/{STABLE_BLE_VERSION_STR}.html"
+# ESPHome always uses .0 for the changelog URL
+STABLE_BLE_URL_VERSION = f"{STABLE_BLE_VERSION.major}.{STABLE_BLE_VERSION.minor}.0"
+DEFAULT_URL = f"https://esphome.io/changelog/{STABLE_BLE_URL_VERSION}.html"
 
 DATA_FFMPEG_PROXY = f"{DOMAIN}.ffmpeg_proxy"
