8780497991ffff91a5e0543c2bed53610b94adf3
cs12ag <70966712+cs12ag@users.noreply.github.com>
2025-03-02 10:26:53 +0000

    Rewrite of test_migrate_config_entry_and_identifiers after feedback

--- homeassistant/components/tradfri/__init__.py
+++ homeassistant/components/tradfri/__init__.py
@@ -150,7 +150,8 @@ def migrate_config_entry_and_identifiers(
     device_id: str
 
     device_reg = dr.async_get(hass)
-    # Get all devices associated to contextual gateway config_entry and loop through list of devices.
+    # Get all devices associated to contextual gateway config_entry
+    # and loop through list of devices.
     for device in dr.async_entries_for_config_entry(device_reg, config_entry.entry_id):
         related_device_flag = False
         for identifier in device.identifiers:
@@ -163,7 +164,8 @@ def migrate_config_entry_and_identifiers(
 
             # Identify gateway device.
             if _id == config_entry.data[CONF_GATEWAY_ID]:
-                related_device_flag = False  # Using this to avoid updating gateway's own device registry entry
+                # Using this to avoid updating gateway's own device registry entry
+                related_device_flag = False
                 break
 
             device_id = str(_id)
@@ -183,14 +185,13 @@ def migrate_config_entry_and_identifiers(
             # Check that the 'other' config entry is also a tradfri config entry
             other_entry = hass.config_entries.async_get_entry(config_entry_id)
 
-            if other_entry is not None:
-                if other_entry.domain != DOMAIN:
-                    continue
+            if other_entry is None or other_entry.domain != DOMAIN:
+                continue
 
-                # Remove non-primary 'tradfri' config entry from device's config_entry_ids
-                device_reg.async_update_device(
-                    device.id, remove_config_entry_id=config_entry_id
-                )
+            # Remove non-primary 'tradfri' config entry from device's config_entry_ids
+            device_reg.async_update_device(
+                device.id, remove_config_entry_id=config_entry_id
+            )
 
         if config_entry.data[CONF_GATEWAY_ID] in device_id:
             continue
@@ -223,7 +224,7 @@ def remove_stale_devices(
     device_entries = dr.async_entries_for_config_entry(
         device_registry, config_entry.entry_id
     )
-    all_device_ids = {device.id for device in devices}
+    all_device_ids = {str(device.id) for device in devices}
 
     for device_entry in device_entries:
         device_id: str | None = None
@@ -240,7 +241,7 @@ def remove_stale_devices(
                 gateway_id = _id
                 break
 
-            device_id = _id
+            device_id = _id.replace(f"{config_entry.data[CONF_GATEWAY_ID]}-", "")
             break
 
         if gateway_id is not None:
--- tests/components/tradfri/test_init.py
+++ tests/components/tradfri/test_init.py
@@ -1,25 +1,20 @@
 """Tests for Tradfri setup."""
 
-from typing import Any
 from unittest.mock import MagicMock
 
-import pytest
-from pytradfri.device import Device
+from pytradfri.const import ATTR_FIRMWARE_VERSION, ATTR_GATEWAY_ID
+from pytradfri.gateway import Gateway
 
 from homeassistant.components import tradfri
-from homeassistant.components.light import (
-    ATTR_BRIGHTNESS,
-    ATTR_COLOR_MODE,
-    ATTR_SUPPORTED_COLOR_MODES,
-    ColorMode,
-)
 from homeassistant.components.tradfri.const import DOMAIN
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import device_registry as dr
+from homeassistant.setup import async_setup_component
 
 from . import GATEWAY_ID, GATEWAY_ID1, GATEWAY_ID2
+from .common import CommandStore
 
-from tests.common import MockConfigEntry, load_fixture
+from tests.common import MockConfigEntry, load_json_object_fixture
 
 
 async def test_entry_setup_unload(
@@ -105,37 +100,12 @@ async def test_remove_stale_devices(
     assert device_entry.model == "E1526"
 
 
-@pytest.fixture(scope="module")
-def bulb_w() -> str:
-    """Return a bulb W response."""
-    return load_fixture("bulb_w.json", DOMAIN)
-
-
-@pytest.mark.parametrize(
-    ("device", "entity_id", "state_attributes"),
-    [
-        (
-            "bulb_w",
-            "light.test_w",
-            {
-                ATTR_BRIGHTNESS: 250,
-                ATTR_SUPPORTED_COLOR_MODES: [ColorMode.BRIGHTNESS],
-                ATTR_COLOR_MODE: ColorMode.BRIGHTNESS,
-            },
-        ),
-    ],
-    indirect=["device"],
-)
 async def test_migrate_config_entry_and_identifiers(
     hass: HomeAssistant,
     device_registry: dr.DeviceRegistry,
-    entity_registry: er.EntityRegistry,
-    device: Device,
-    entity_id: str,
-    state_attributes: dict[str, Any],
+    command_store: CommandStore,
 ) -> None:
     """Test correction of device registry entries."""
-
     config_entry1 = MockConfigEntry(
         domain=tradfri.DOMAIN,
         data={
@@ -145,6 +115,26 @@ async def test_migrate_config_entry_and_identifiers(
             tradfri.CONF_GATEWAY_ID: GATEWAY_ID1,
         },
     )
+
+    def mock_gateway_fixture(command_store: CommandStore, gateway_id: str) -> Gateway:
+        """Mock a Tradfri gateway."""
+        gateway = Gateway()
+        command_store.register_response(
+            gateway.get_gateway_info(),
+            {ATTR_GATEWAY_ID: gateway_id, ATTR_FIRMWARE_VERSION: "1.2.1234"},
+        )
+        command_store.register_response(
+            gateway.get_devices(),
+            [],
+        )
+        return gateway
+
+    gateway_1 = mock_gateway_fixture(command_store, GATEWAY_ID1)
+    command_store.register_device(
+        gateway_1, load_json_object_fixture("bulb_w.json", DOMAIN)
+    )
+    config_entry1.add_to_hass(hass)
+
     config_entry2 = MockConfigEntry(
         domain=tradfri.DOMAIN,
         data={
@@ -155,50 +145,24 @@ async def test_migrate_config_entry_and_identifiers(
         },
     )
 
-    config_entry1.add_to_hass(hass)
     config_entry2.add_to_hass(hass)
 
-    # No need to explicitly run async_setup() for each config-entry;
-    # __init__.py will loop through all configured gateways itself when invoked.
-    await hass.config_entries.async_setup(config_entry1.entry_id)
-    await hass.async_block_till_done()
+    # Create bulb 1 on gateway 1 in Device Registry - this has the old identifiers format
+    gateway_1_bulb = device_registry.async_get_or_create(
+        config_entry_id=config_entry1.entry_id,
+        identifiers={(tradfri.DOMAIN, 65537)},
+        name="bulb1",
+    )
 
-    # Update bulb 1 identifiers on gateway 1 reflect old-style ID
     # Update bulb 1 device to have both config-entry IDs
     # This is to simulate existing data scenario with older version of tradfri component
-    gateway_1_bulb = device_registry.async_get_device(
-        identifiers={(tradfri.DOMAIN, f"{GATEWAY_ID1}-65537")}
-    )
     device_registry.async_update_device(
         gateway_1_bulb.id,
         add_config_entry_id=config_entry2.entry_id,
-        new_identifiers={(tradfri.DOMAIN, 65537)},
     )
-    # Remove bulb 1 from gateway 2 completely
-    # This is to simulate existing data scenario with older version of tradfri component
-    gateway_2_bulb = device_registry.async_get_device(
-        identifiers={(tradfri.DOMAIN, f"{GATEWAY_ID2}-65537")}
-    )
-    device_registry.async_remove_device(gateway_2_bulb.id)
 
-    # Validate that gateway 1 has 2 devices associated to it
-    device_entries = dr.async_entries_for_config_entry(
-        device_registry, config_entry1.entry_id
-    )
-    assert len(device_entries) == 2
-
-    # Validate that gateway 2 still technically has 2 devices associated to it
-    # despite removing the bulb - async_entries_for_config_entry() will qualify
-    # the bulb device that is now associated to both config-entries
-    device_entries = dr.async_entries_for_config_entry(
-        device_registry, config_entry2.entry_id
-    )
-    assert len(device_entries) == 2
-
-    # Force updates of all config entries to re-run async_setup_entry with device_registry and entity_registry persisting
-    await hass.config_entries.async_reload(config_entry1.entry_id)
-    await hass.async_block_till_done()
-    await hass.config_entries.async_reload(config_entry2.entry_id)
+    # Set up all tradfri config entries.
+    await async_setup_component(hass, DOMAIN, {})
     await hass.async_block_till_done()
 
     # Validate that gateway 1 bulb 1 is still the same device entry
@@ -216,7 +180,9 @@ async def test_migrate_config_entry_and_identifiers(
     # Validate that gateway 1 bulb 1 only has only gateway 1's config ID associated to it
     assert device_entries[1].config_entries == {config_entry1.entry_id}
 
-    # Validate that gateway 2 bulb 1 has been re-added to device registry and with correct unique identifiers
+    # Validate that gateway 2 bulb 1 has been added to device registry and with correct unique identifiers
+    # (This exists because the command_store seems to be executed for each gateway being set up - not
+    # _only_ for the gateway that was in context in the register_device() function. )
     device_entries = dr.async_entries_for_config_entry(
         device_registry, config_entry2.entry_id
     )
