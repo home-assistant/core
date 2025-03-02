fe5a1e80e0a0a96db5e18f713375cf9e3f9b77a4
Paulus Schoutsen <balloob@gmail.com>
2025-03-02 14:27:29 -0500

    VoIP block non-TTS announcements

--- homeassistant/components/voip/assist_satellite.py
+++ homeassistant/components/voip/assist_satellite.py
@@ -28,6 +28,7 @@ from homeassistant.components.assist_satellite import (
 from homeassistant.components.network import async_get_source_ip
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Context, HomeAssistant, callback
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from .const import (
@@ -193,6 +194,11 @@ class VoipAssistSatellite(VoIPEntity, AssistSatelliteEntity, RtpDatagramProtocol
 
         Optionally run a voice pipeline after the announcement has finished.
         """
+        if announcement.media_id_source != "tts":
+            raise HomeAssistantError(
+                "VoIP does not currently support non-TTS announcements"
+            )
+
         self._announcement_future = asyncio.Future()
         self._run_pipeline_after_announce = run_pipeline_after
 
