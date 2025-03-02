b2d28a44323c019dce1b133c2b9c7180a7724fe3
wittypluck <wittypluck@users.noreply.github.com>
2025-03-02 20:24:33 +0100

    Add snapshot tests for sensors

---
+++ tests/components/openweathermap/conftest.py
@@ -0,0 +1,42 @@
+"""Configure tests for the OpenWeatherMap integration."""
+
+# import pytest
+# from syrupy import SnapshotAssertion
+
+from homeassistant.components.openweathermap.const import DEFAULT_LANGUAGE, DOMAIN
+from homeassistant.const import (
+    CONF_API_KEY,
+    CONF_LANGUAGE,
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+    CONF_MODE,
+    CONF_NAME,
+)
+
+from .test_config_flow import _create_static_weather_report
+
+from tests.common import MockConfigEntry
+
+API_KEY = "test_api_key"
+LATITUDE = 12.34
+LONGITUDE = 56.78
+NAME = "openweathermap"
+
+# Define test data for mocked weather report
+static_weather_report = _create_static_weather_report()
+
+
+def mock_config_entry(mode: str) -> MockConfigEntry:
+    """Create a mock OpenWeatherMap config entry."""
+    return MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            CONF_API_KEY: API_KEY,
+            CONF_LATITUDE: LATITUDE,
+            CONF_LONGITUDE: LONGITUDE,
+            CONF_NAME: NAME,
+        },
+        options={CONF_MODE: mode, CONF_LANGUAGE: DEFAULT_LANGUAGE},
+        entry_id="test",
+        version=5,
+    )
--- tests/components/openweathermap/test_weather.py
+++ tests/components/openweathermap/test_weather.py
@@ -4,51 +4,24 @@ import pytest
 from syrupy import SnapshotAssertion
 
 from homeassistant.components.openweathermap.const import (
-    DEFAULT_LANGUAGE,
     DOMAIN,
     OWM_MODE_FREE_CURRENT,
     OWM_MODE_V30,
 )
 from homeassistant.components.openweathermap.weather import SERVICE_GET_MINUTE_FORECAST
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import (
-    CONF_API_KEY,
-    CONF_LANGUAGE,
-    CONF_LATITUDE,
-    CONF_LONGITUDE,
-    CONF_MODE,
-    CONF_NAME,
-)
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ServiceValidationError
 
+from .conftest import mock_config_entry
 from .test_config_flow import _create_static_weather_report
 
 from tests.common import AsyncMock, MockConfigEntry, patch
 
 ENTITY_ID = "weather.openweathermap"
-API_KEY = "test_api_key"
-LATITUDE = 12.34
-LONGITUDE = 56.78
-NAME = "openweathermap"
 
 # Define test data for mocked weather report
 static_weather_report = _create_static_weather_report()
-
-
-def mock_config_entry(mode: str) -> MockConfigEntry:
-    """Create a mock OpenWeatherMap config entry."""
-    return MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            CONF_API_KEY: API_KEY,
-            CONF_LATITUDE: LATITUDE,
-            CONF_LONGITUDE: LONGITUDE,
-            CONF_NAME: NAME,
-        },
-        options={CONF_MODE: mode, CONF_LANGUAGE: DEFAULT_LANGUAGE},
-        version=5,
-    )
 
 
 @pytest.fixture
