717deeebb979021c613ba7b12d7653c84511bd33
wittypluck <wittypluck@users.noreply.github.com>
2025-03-03 00:36:44 +0100

    Use snapshot_platform, split platforms for snapshot tests

--- tests/components/openweathermap/test_weather.py
+++ tests/components/openweathermap/test_weather.py
@@ -1,22 +1,26 @@
 """Test the OpenWeatherMap weather entity."""
 
+from pyopenweathermap import WeatherReport
 import pytest
 from syrupy import SnapshotAssertion
 
 from homeassistant.components.openweathermap.const import (
     DOMAIN,
     OWM_MODE_FREE_CURRENT,
+    OWM_MODE_FREE_FORECAST,
     OWM_MODE_V30,
 )
 from homeassistant.components.openweathermap.weather import SERVICE_GET_MINUTE_FORECAST
 from homeassistant.config_entries import ConfigEntryState
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ServiceValidationError
+from homeassistant.helpers import entity_registry as er
 
-from .conftest import mock_config_entry
+from .conftest import mock_config_entry, setup_platform
 from .test_config_flow import _create_static_weather_report
 
-from tests.common import AsyncMock, MockConfigEntry, patch
+from tests.common import AsyncMock, MockConfigEntry, patch, snapshot_platform
 
 ENTITY_ID = "weather.openweathermap"
 
@@ -92,3 +96,41 @@ async def test_mode_fail(
             blocking=True,
             return_response=True,
         )
+
+
+@pytest.mark.parametrize(
+    ("mode", "patched_function", "mock_return"),
+    [
+        (
+            OWM_MODE_FREE_CURRENT,
+            "pyopenweathermap.client.free_client.OWMFreeClient.get_weather",
+            static_weather_report,
+        ),
+        (
+            OWM_MODE_FREE_FORECAST,
+            "pyopenweathermap.client.free_client.OWMFreeClient.get_weather",
+            static_weather_report,
+        ),
+        (
+            OWM_MODE_V30,
+            "pyopenweathermap.client.onecall_client.OWMOneCallClient.get_weather",
+            static_weather_report,
+        ),
+    ],
+)
+async def test_weather_states(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+    mode: str,
+    patched_function: str,
+    mock_return: WeatherReport,
+) -> None:
+    """Test weather states are correctly collected from library with different modes and mocked function responses."""
+
+    entry = mock_config_entry(mode)
+
+    with patch(patched_function, new_callable=AsyncMock, return_value=mock_return):
+        await setup_platform(hass, entry, [Platform.WEATHER])
+
+    await snapshot_platform(hass, entity_registry, snapshot, entry.entry_id)
