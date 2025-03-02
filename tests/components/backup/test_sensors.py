103f350ebbf5d966126c19bee6b6423925c6be80
mib1185 <mail@mib85.de>
2025-03-02 12:15:42 +0000

    extend sensor test

--- tests/components/backup/test_sensors.py
+++ tests/components/backup/test_sensors.py
@@ -2,6 +2,7 @@
 
 from unittest.mock import patch
 
+import pytest
 from syrupy import SnapshotAssertion
 
 from homeassistant.components.backup.const import DOMAIN
@@ -12,17 +13,35 @@ from homeassistant.helpers import entity_registry as er
 from .common import setup_backup_integration
 
 from tests.common import snapshot_platform
+from tests.typing import WebSocketGenerator
 
 
-async def test_setup_sensors(
+@pytest.mark.usefixtures("mock_backup_generation")
+async def test_sensors(
     hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
     entity_registry: er.EntityRegistry,
     snapshot: SnapshotAssertion,
 ) -> None:
-    """Test setup backup config entry."""
+    """Test setup and update of backup sensors."""
     with patch("homeassistant.components.backup.PLATFORMS", [Platform.SENSOR]):
         await setup_backup_integration(hass, with_hassio=False)
         await hass.async_block_till_done(wait_background_tasks=True)
 
     entry = hass.config_entries.async_entries(DOMAIN)[0]
     await snapshot_platform(hass, entity_registry, snapshot, entry.entry_id)
+
+    # start backup and check sensor states again
+    client = await hass_ws_client(hass)
+    await hass.async_block_till_done()
+    await client.send_json_auto_id(
+        {"type": "backup/generate", "agent_ids": ["backup.local"]}
+    )
+
+    assert await client.receive_json()
+    state = hass.states.get("sensor.backup_backup_manager_state")
+    assert state.state == "create_backup"
+
+    await hass.async_block_till_done(wait_background_tasks=True)
+    state = hass.states.get("sensor.backup_backup_manager_state")
+    assert state.state == "idle"
