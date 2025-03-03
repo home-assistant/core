e4c25b714e975ecda1b29e4fb8b7ce5d6b9de305
FredericMa <frederic.dev@outlook.com>
2025-03-03 19:41:10 +0000

    Update test to freeze time instead of mocking time.

--- tests/components/risco/test_services.py
+++ tests/components/risco/test_services.py
@@ -3,6 +3,8 @@
 from datetime import datetime
 from unittest.mock import patch
 
+import pytest
+
 from homeassistant.components.risco import DOMAIN
 from homeassistant.components.risco.const import SERVICE_SET_TIME
 from homeassistant.core import HomeAssistant
@@ -27,25 +29,18 @@ async def test_set_time_service(
         mock.assert_called_once_with(time)
 
 
+@pytest.mark.freeze_time("2025-02-21T12:00:00Z")
 async def test_set_time_service_with_no_time(
     hass: HomeAssistant, setup_risco_local, local_config_entry
 ) -> None:
     """Test the set_time service when no time is provided."""
-    fixed_time = datetime(2025, 2, 21, 12, 0, 0)
+    with patch("homeassistant.components.risco.RiscoLocal.set_time") as mock_set_time:
+        data = {
+            "config_entry_id": local_config_entry.entry_id,
+        }
 
-    with patch("homeassistant.components.risco.services.datetime") as mock_datetime:
-        mock_datetime.now.return_value = fixed_time
-        mock_datetime.fromisoformat = datetime.fromisoformat
+        await hass.services.async_call(
+            DOMAIN, SERVICE_SET_TIME, service_data=data, blocking=True
+        )
 
-        with patch(
-            "homeassistant.components.risco.RiscoLocal.set_time"
-        ) as mock_set_time:
-            data = {
-                "config_entry_id": local_config_entry.entry_id,
-            }
-
-            await hass.services.async_call(
-                DOMAIN, SERVICE_SET_TIME, service_data=data, blocking=True
-            )
-
-            mock_set_time.assert_called_once_with(fixed_time)
+        mock_set_time.assert_called_once_with(datetime.now())
