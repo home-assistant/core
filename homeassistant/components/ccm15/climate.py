c84949a0051fdfca6f34029871b9a58c17eafed2
moshe <m@inn.co.il>
2025-03-03 10:58:50 +0200

    fix types (mypy)

--- homeassistant/components/ccm15/climate.py
+++ homeassistant/components/ccm15/climate.py
@@ -1,7 +1,7 @@
 """Climate device for CCM15 coordinator."""
 
 import logging
-from typing import Any
+from typing import Any, cast
 
 from ccm15 import CCM15DeviceState
 
@@ -143,7 +143,7 @@ class CCM15Climate(CoordinatorEntity[CCM15Coordinator], ClimateEntity):
 
     async def async_set_temperature(self, **kwargs: Any) -> None:
         """Set the target temperature."""
-        hvac: HVACMode = kwargs.get("hvac_mode")
+        hvac: HVACMode | None = cast(HVACMode | None, kwargs.get("hvac_mode"))
         if (temperature := kwargs.get(ATTR_TEMPERATURE)) is not None:
             await self.coordinator.async_set_temperature(
                 self._ac_index, self.data, temperature, hvac
