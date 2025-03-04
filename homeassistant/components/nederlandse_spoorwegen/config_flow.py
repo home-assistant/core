3d32476dcc9f464e0e6c96c5a14a87077c4303ac
Martreides <martreides+github@proton.me>
2025-03-04 20:52:31 +0000

    Improve config flow.

--- homeassistant/components/nederlandse_spoorwegen/config_flow.py
+++ homeassistant/components/nederlandse_spoorwegen/config_flow.py
@@ -7,9 +7,10 @@ from ns_api import RequestParametersError
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.helpers.selector import selector
 from homeassistant.const import CONF_API_KEY
 
-from .const import CONF_STATION_FROM, CONF_STATION_TO, DOMAIN
+from .const import CONF_STATION_FROM, CONF_STATION_TO, CONF_STATION_VIA, DOMAIN
 
 
 class NederlandseSpoorwegenConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -22,6 +23,7 @@ class NederlandseSpoorwegenConfigFlow(ConfigFlow, domain=DOMAIN):
         """Initialize the config flow."""
 
         self.api_key: str
+        self._stations: None
 
     async def async_step_user(self, user_input) -> ConfigFlowResult:
         """Handle a flow initialized by the user."""
@@ -31,7 +33,15 @@ class NederlandseSpoorwegenConfigFlow(ConfigFlow, domain=DOMAIN):
             try:
                 nsapi = ns_api.NSAPI(self.api_key)
                 loop = asyncio.get_running_loop()
-                await loop.run_in_executor(None, nsapi.get_stations)
+                station_data = await loop.run_in_executor(None, nsapi.get_stations)
+                self._stations = {
+                    station.code: station.names["long"]
+                    for station in station_data
+                    if station.country == "NL"
+                }
+                self._stations = dict(
+                    sorted(self._stations.items(), key=lambda item: item[1])
+                )
             except RequestParametersError:
                 errors["base"] = "invalid_api_key"
             else:
@@ -55,6 +65,7 @@ class NederlandseSpoorwegenConfigFlow(ConfigFlow, domain=DOMAIN):
                 data={
                     CONF_API_KEY: self.api_key,
                     CONF_STATION_FROM: user_input[CONF_STATION_FROM],
+                    CONF_STATION_VIA: user_input.get(CONF_STATION_VIA, None),
                     CONF_STATION_TO: user_input[CONF_STATION_TO],
                 },
             )
@@ -63,8 +74,9 @@ class NederlandseSpoorwegenConfigFlow(ConfigFlow, domain=DOMAIN):
             step_id="stations",
             data_schema=vol.Schema(
                 {
-                    vol.Required(CONF_STATION_FROM): str,
-                    vol.Required(CONF_STATION_TO): str,
+                    vol.Required(CONF_STATION_FROM): vol.In(self._stations),
+                    vol.Optional(CONF_STATION_VIA): vol.In(self._stations),
+                    vol.Required(CONF_STATION_TO): vol.In(self._stations),
                 }
             ),
         )
