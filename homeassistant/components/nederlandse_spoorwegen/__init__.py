546169aa6503a5c565b14513d67ed7c8372d8b86
Martreides <martreides+github@proton.me>
2025-03-05 16:11:28 +0000

    Implement sub-entry.

--- homeassistant/components/nederlandse_spoorwegen/__init__.py
+++ homeassistant/components/nederlandse_spoorwegen/__init__.py
@@ -9,5 +9,6 @@ PLATFORMS = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up NS API as config entry."""
+    print("test_init")
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
--- homeassistant/components/nederlandse_spoorwegen/config_flow.py
+++ homeassistant/components/nederlandse_spoorwegen/config_flow.py
@@ -1,13 +1,21 @@
 """Adds config flow for Nederlandse Spoorwegen."""
 
 import asyncio
+from typing import Any
 
 import ns_api
 from ns_api import RequestParametersError
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import (
+    ConfigEntry,
+    ConfigFlow,
+    ConfigFlowResult,
+    ConfigSubentryFlow,
+    SubentryFlowResult,
+)
 from homeassistant.const import CONF_API_KEY
+from homeassistant.core import callback
 from homeassistant.helpers.selector import TimeSelector
 
 from .const import (
@@ -30,6 +38,15 @@ class NederlandseSpoorwegenConfigFlow(ConfigFlow, domain=DOMAIN):
 
         self.api_key: str
         self._stations: None
+        self._stations_short: None
+
+    @classmethod
+    @callback
+    def async_get_supported_subentry_types(
+        cls, config_entry: ConfigEntry
+    ) -> dict[str, type[ConfigSubentryFlow]]:
+        """Return subentries supported by this integration."""
+        return {"train": NederlandseSpoorwegenSubentryFlowHandler}
 
     async def async_step_user(self, user_input) -> ConfigFlowResult:
         """Handle a flow initialized by the user."""
@@ -42,6 +59,11 @@ class NederlandseSpoorwegenConfigFlow(ConfigFlow, domain=DOMAIN):
                 station_data = await loop.run_in_executor(None, nsapi.get_stations)
                 self._stations = {
                     station.code: station.names["long"]
+                    for station in station_data
+                    if station.country == "NL"
+                }
+                self._stations_short = {
+                    station.code: station.names["middle"]
                     for station in station_data
                     if station.country == "NL"
                 }
@@ -69,12 +91,21 @@ class NederlandseSpoorwegenConfigFlow(ConfigFlow, domain=DOMAIN):
                 + (user_input.get(CONF_TIME, ""))
             )
             self._abort_if_unique_id_configured()
+            station_via = user_input.get(CONF_STATION_VIA, None)
+
+            title = self._stations_short[user_input[CONF_STATION_FROM]]
+            title = (
+                title + " - " + self._stations_short[station_via]
+                if station_via
+                else title
+            )
+            title = title + " - " + self._stations_short[user_input[CONF_STATION_TO]]
             return self.async_create_entry(
-                title=user_input[CONF_STATION_FROM] + user_input[CONF_STATION_TO],
+                title=title,
                 data={
                     CONF_API_KEY: self.api_key,
                     CONF_STATION_FROM: user_input[CONF_STATION_FROM],
-                    CONF_STATION_VIA: user_input.get(CONF_STATION_VIA, None),
+                    CONF_STATION_VIA: station_via,
                     CONF_STATION_TO: user_input[CONF_STATION_TO],
                     CONF_TIME: user_input.get(CONF_TIME, None),
                 },
@@ -90,4 +121,30 @@ class NederlandseSpoorwegenConfigFlow(ConfigFlow, domain=DOMAIN):
                     vol.Optional(CONF_TIME): TimeSelector(),
                 }
             ),
+        )
+
+
+class NederlandseSpoorwegenSubentryFlowHandler(ConfigSubentryFlow):
+    """Handle subentry flow for adding and modifying a location."""
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> SubentryFlowResult:
+        """User flow to add a new train."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            return self.async_create_entry(
+                title="subentry_train2",
+                data={
+                    CONF_API_KEY: "bf85caa2826c41f5b8e0c5c7dd404f5a",
+                    CONF_STATION_FROM: "Ut",
+                    CONF_STATION_VIA: None,
+                    CONF_STATION_TO: "Nkk",
+                    CONF_TIME: None,
+                },
+            )
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({vol.Required(CONF_API_KEY): str}),
+            errors=errors,
         )
