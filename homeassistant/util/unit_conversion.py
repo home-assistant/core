a12350756b31561787ce498de8a9e6caf31cf7d0
J. Nick Koston <nick@koston.org>
2025-03-07 10:46:27 -1000

    coverage

--- homeassistant/util/unit_conversion.py
+++ homeassistant/util/unit_conversion.py
@@ -152,10 +152,7 @@ class BaseUnitConverter:
     ) -> float:
         """Get floored log ratio between units of measurement."""
         from_ratio, to_ratio = cls._get_from_to_ratio(from_unit, to_unit)
-        ratio = from_ratio / to_ratio
-        # Scale the precision when converting to a larger unit
-        # For example 1.1 Wh should be rendered as 0.0011 kWh, not 0.0 kWh
-        return floor(max(0, log10(ratio)))
+        return floor(max(0, log10(from_ratio / to_ratio)))
 
     @classmethod
     @lru_cache
