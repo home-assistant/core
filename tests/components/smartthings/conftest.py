57feaa2cf3edbe1263c58343a1df9b1e05831b98
Joost Lekkerkerker <joostlek@outlook.com>
2025-03-07 12:55:32 +0100

    Fix SmartThings disabling working capabilities (#140039)

--- tests/components/smartthings/conftest.py
+++ tests/components/smartthings/conftest.py
@@ -101,6 +101,7 @@ def mock_smartthings() -> Generator[AsyncMock]:
         "da_wm_dw_000001",
         "da_wm_wd_000001",
         "da_wm_wm_000001",
+        "da_wm_wm_000001_1",
         "da_rvc_normal_000001",
         "da_ks_microwave_0101x",
         "hue_color_temperature_bulb",
