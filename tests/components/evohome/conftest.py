1652b3c75998e08a4f23dbbf7ef7b635b171b2f2
David Bonnes <zxdavb@bonnes.me>
2025-03-07 16:18:34 +0000

    bugfix test fixture

--- tests/components/evohome/conftest.py
+++ tests/components/evohome/conftest.py
@@ -127,26 +127,26 @@ def mock_make_request(install: str) -> Callable:
         await self._headers()
 
         # assume GET is valid, and return the JSON for that web API
-        if url == "accountInfo":  # /v0/accountInfo
+        if url == "accountInfo":  # v0, accountInfo
             return {}  # will throw a KeyError -> BadApiResponseError
 
-        if url.startswith("locations/"):  # /v0/locations?userId={id}&allData=True
+        if url.startswith("locations/"):  # v0, locations?userId={id}&allData=True
             return []  # user has no locations
 
-        if url == "userAccount":  # /v2/userAccount
+        if url == "userAccount":  # v2, userAccount
             return user_account_config_fixture(install)
 
         if url.startswith("location/"):
-            if "installationInfo" in url:  # /v2/location/installationInfo?userId={id}
+            if "installationInfo" in url:  # v2, location/installationInfo?userId={id}
                 return user_locations_config_fixture(install)
-            if "status" in url:  # /v2/location/{id}/status
+            if "status" in url:  # v2, location/{id}/status
                 return location_status_fixture(install)
 
         elif "schedule" in url:
-            if url.startswith("domesticHotWater"):  # /v2/domesticHotWater/{id}/schedule
-                return dhw_schedule_fixture(install, url[16:23])
-            if url.startswith("temperatureZone"):  # /v2/temperatureZone/{id}/schedule
-                return zone_schedule_fixture(install, url[16:23])
+            if url.startswith("domesticHotWater"):  # v2, domesticHotWater/{id}/schedule
+                return dhw_schedule_fixture(install, url.split("/")[1])
+            if url.startswith("temperatureZone"):  # v2, temperatureZone/{id}/schedule
+                return zone_schedule_fixture(install, url.split("/")[1])
 
         pytest.fail(f"Unexpected request: {HTTPMethod.GET} {url}")
 
