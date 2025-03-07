113cd4bfccaab379f38db7803182692a2ed53495
David Bonnes <zxdavb@bonnes.me>
2025-03-07 09:12:21 +0000

    Fix regression to evohome debug logging (#140000)
    * fix regression in debug logging
    
    * lint

--- homeassistant/components/evohome/coordinator.py
+++ homeassistant/components/evohome/coordinator.py
@@ -11,6 +11,7 @@ from typing import Any
 import evohomeasync as ec1
 import evohomeasync2 as ec2
 from evohomeasync2.const import (
+    SZ_DHW,
     SZ_GATEWAY_ID,
     SZ_GATEWAY_INFO,
     SZ_GATEWAYS,
@@ -19,8 +20,9 @@ from evohomeasync2.const import (
     SZ_TEMPERATURE_CONTROL_SYSTEMS,
     SZ_TIME_ZONE,
     SZ_USE_DAYLIGHT_SAVE_SWITCHING,
+    SZ_ZONES,
 )
-from evohomeasync2.schemas.typedefs import EvoLocStatusResponseT
+from evohomeasync2.schemas.typedefs import EvoLocStatusResponseT, EvoTcsConfigResponseT
 
 from homeassistant.const import CONF_SCAN_INTERVAL
 from homeassistant.core import HomeAssistant
@@ -113,17 +115,19 @@ class EvoDataUpdateCoordinator(DataUpdateCoordinator):
                     SZ_USE_DAYLIGHT_SAVE_SWITCHING
                 ],
             }
+            tcs_info: EvoTcsConfigResponseT = self.tcs.config  # type: ignore[assignment]
+            tcs_info[SZ_ZONES] = [zone.config for zone in self.tcs.zones]
+            if self.tcs.hotwater:
+                tcs_info[SZ_DHW] = self.tcs.hotwater.config
             gwy_info = {
                 SZ_GATEWAY_ID: self.loc.gateways[0].id,
-                SZ_TEMPERATURE_CONTROL_SYSTEMS: [
-                    self.loc.gateways[0].systems[0].config
-                ],
+                SZ_TEMPERATURE_CONTROL_SYSTEMS: [tcs_info],
             }
             config = {
                 SZ_LOCATION_INFO: loc_info,
                 SZ_GATEWAYS: [{SZ_GATEWAY_INFO: gwy_info}],
             }
-            self.logger.debug("Config = %s", config)
+            self.logger.debug("Config = %s", [config])
 
     async def call_client_api(
         self,
