84255191e16e29817668dbff3339575b7a118eaa
Martreides <martreides+github@proton.me>
2025-03-08 10:39:18 +0000

    Fix not updating after adding sub entry.

--- homeassistant/components/nederlandse_spoorwegen/__init__.py
+++ homeassistant/components/nederlandse_spoorwegen/__init__.py
@@ -35,6 +35,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         ) from ex
     else:
         await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(update_listener))
 
     return True
 
@@ -42,3 +43,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
+async def update_listener(hass, entry):
+    """Handle update."""
+    await hass.config_entries.async_reload(entry.entry_id)
