4a8f1e9215bbd990d2a242f2f66385c8251fdf24
Joost Lekkerkerker <joostlek@outlook.com>
2025-03-08 11:27:26 +0100

    Support null supported Thermostat modes in SmartThings (#140101)

--- tests/components/smartthings/conftest.py
+++ tests/components/smartthings/conftest.py
@@ -122,6 +122,7 @@ def mock_smartthings() -> Generator[AsyncMock]:
         "fake_fan",
         "generic_fan_3_speed",
         "heatit_ztrm3_thermostat",
+        "generic_ef00_v1",
         "bosch_radiator_thermostat_ii",
     ]
 )
