b8d2aa7b939a7192c083556b44667aa455a297f3
Martreides <martreides+github@proton.me>
2025-03-08 09:26:51 +0000

    Move setting up API to init.

--- homeassistant/components/nederlandse_spoorwegen/__init__.py
+++ homeassistant/components/nederlandse_spoorwegen/__init__.py
@@ -1,13 +1,39 @@
 """The nederlandse_spoorwegen component."""
 
+import asyncio
+from dataclasses import dataclass
+
+import ns_api
+from ns_api import RequestParametersError
+
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
+from homeassistant.const import CONF_API_KEY, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryAuthFailed
 
 PLATFORMS = [Platform.SENSOR]
 
 
+@dataclass
+class NederlandseSpoorwegenData:
+    """Data structure for runtime data."""
+
+    nsapi: ns_api.NSAPI
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up NS API as config entry."""
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    try:
+        nsapi = ns_api.NSAPI(entry.data[CONF_API_KEY])
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(None, nsapi.get_stations)
+        entry.runtime_data = NederlandseSpoorwegenData(nsapi)
+    except RequestParametersError as ex:
+        raise ConfigEntryAuthFailed(
+            "Could not insantiate the Nederlandse Spoorwegen API."
+        ) from ex
+    else:
+        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
