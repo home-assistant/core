83b46fcaa54b494a458f36397ef9a97129762c37
Joostlek <joostlek@outlook.com>
2025-03-11 12:50:58 +0100

    Fix no temperature unit in SmartThings

--- tests/components/smartthings/conftest.py
+++ tests/components/smartthings/conftest.py
@@ -120,6 +120,7 @@ def mock_smartthings() -> Generator[AsyncMock]:
         "sensibo_airconditioner_1",
         "ecobee_sensor",
         "ecobee_thermostat",
+        "ecobee_thermostat_offline",
         "fake_fan",
         "generic_fan_3_speed",
         "heatit_ztrm3_thermostat",
