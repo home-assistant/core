79f70d13db2e743b851f61f627fc7f5def5b1cc7
Joost Lekkerkerker <joostlek@outlook.com>
2025-03-14 00:28:08 +0100

    Fix windowShadeLevel capability in SmartThings (#140552)

--- tests/components/smartthings/conftest.py
+++ tests/components/smartthings/conftest.py
@@ -134,6 +134,7 @@ def mock_smartthings() -> Generator[AsyncMock]:
         "im_speaker_ai_0001",
         "abl_light_b_001",
         "tplink_p110",
+        "ikea_kadrilj",
     ]
 )
 def device_fixture(
