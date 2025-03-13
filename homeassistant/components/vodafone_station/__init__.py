d21e5b51850843496de7d9b7680a9a1563d3132c
Simone Chemelli <simone.chemelli@gmail.com>
2025-03-13 11:53:58 +0000

    revert unwanted change

--- homeassistant/components/vodafone_station/__init__.py
+++ homeassistant/components/vodafone_station/__init__.py
@@ -23,6 +23,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: VodafoneConfigEntry) -> 
 
     entry.runtime_data = coordinator
 
+    entry.async_on_unload(entry.add_update_listener(update_listener))
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
@@ -37,3 +39,9 @@ async def async_unload_entry(hass: HomeAssistant, entry: VodafoneConfigEntry) ->
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def update_listener(hass: HomeAssistant, entry: VodafoneConfigEntry) -> None:
+    """Update when config_entry options update."""
+    if entry.options:
+        await hass.config_entries.async_reload(entry.entry_id)
