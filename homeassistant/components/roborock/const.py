2f5efc782297f980f2d13bf8647219c84d68b8c6
Luke <conway220@gmail.com>
2025-03-14 13:39:18 +0000

    Set A01 to 1 minute

--- homeassistant/components/roborock/const.py
+++ homeassistant/components/roborock/const.py
@@ -58,8 +58,8 @@ SET_VACUUM_GOTO_POSITION_SERVICE_NAME = "set_vacuum_goto_position"
 GET_VACUUM_CURRENT_POSITION_SERVICE_NAME = "get_vacuum_current_position"
 
 
-A01_UPDATE_INTERVAL = timedelta(seconds=30)
-V1_CLOUD_IN_CLEANING_INTERVAL = timedelta(seconds=20)
+A01_UPDATE_INTERVAL = timedelta(minutes=1)
+V1_CLOUD_IN_CLEANING_INTERVAL = timedelta(seconds=30)
 V1_CLOUD_NOT_CLEANING_INTERVAL = timedelta(minutes=1)
 V1_LOCAL_IN_CLEANING_INTERVAL = timedelta(seconds=15)
 V1_LOCAL_NOT_CLEANING_INTERVAL = timedelta(seconds=30)
