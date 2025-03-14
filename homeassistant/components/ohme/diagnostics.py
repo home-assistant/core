57495d26bcb9d2b5deec6155299fc35db97a8292
Daniel Raper <me@danr.uk>
2025-03-14 18:31:11 +0000

    Add reconfigure step, diagnostics and default disabled entities to Ohme

---
+++ homeassistant/components/ohme/diagnostics.py
@@ -0,0 +1,25 @@
+"""Provides diagnostics for Ohme."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.core import HomeAssistant
+
+from .coordinator import OhmeConfigEntry
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: OhmeConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    coordinators = config_entry.runtime_data
+    client = coordinators.charge_session_coordinator.client
+
+    # Return only the relevant children
+    return {
+        "device_info": client.device_info,
+        "available": client.available,
+        "vehicles": client.vehicles,
+        "ct_connected": client.ct_connected,
+        "cap_available": client.cap_available,
+    }
