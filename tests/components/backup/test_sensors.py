dddd1e915efce623917cc98a0cf45f27930e9b36
mib1185 <mail@mib85.de>
2025-03-14 20:34:41 +0000

    add sensor update tests

--- tests/components/backup/test_sensors.py
+++ tests/components/backup/test_sensors.py
@@ -1,10 +1,13 @@
 """Tests for the sensors of the Backup integration."""
 
+from typing import Any
 from unittest.mock import patch
 
+from freezegun.api import FrozenDateTimeFactory
 import pytest
 from syrupy import SnapshotAssertion
 
+from homeassistant.components.backup import store
 from homeassistant.components.backup.const import DOMAIN
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -12,7 +15,7 @@ from homeassistant.helpers import entity_registry as er
 
 from .common import setup_backup_integration
 
-from tests.common import snapshot_platform
+from tests.common import async_fire_time_changed, snapshot_platform
 from tests.typing import WebSocketGenerator
 
 
@@ -23,7 +26,7 @@ async def test_sensors(
     entity_registry: er.EntityRegistry,
     snapshot: SnapshotAssertion,
 ) -> None:
-    """Test setup and update of backup sensors."""
+    """Test setup of backup sensors."""
     with patch("homeassistant.components.backup.PLATFORMS", [Platform.SENSOR]):
         await setup_backup_integration(hass, with_hassio=False)
         await hass.async_block_till_done(wait_background_tasks=True)
@@ -45,3 +48,89 @@ async def test_sensors(
     await hass.async_block_till_done(wait_background_tasks=True)
     state = hass.states.get("sensor.backup_backup_manager_state")
     assert state.state == "idle"
+
+
+async def test_sensor_updates(
+    hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
+    freezer: FrozenDateTimeFactory,
+    hass_storage: dict[str, Any],
+) -> None:
+    """Test update of backup sensors."""
+    client = await hass_ws_client(hass)
+    await hass.config.async_set_time_zone("Europe/Amsterdam")
+    freezer.move_to("2024-11-12T12:00:00+01:00")
+    storage_data = {
+        "backups": [],
+        "config": {
+            "agents": {},
+            "automatic_backups_configured": False,
+            "create_backup": {
+                "agent_ids": ["test.test-agent"],
+                "include_addons": [],
+                "include_all_addons": False,
+                "include_database": True,
+                "include_folders": [],
+                "name": "test-name",
+                "password": "test-password",
+            },
+            "retention": {"copies": None, "days": None},
+            "last_attempted_automatic_backup": "2024-11-11T04:45:00+01:00",
+            "last_completed_automatic_backup": "2024-11-11T04:45:00+01:00",
+            "schedule": {
+                "days": [],
+                "recurrence": "daily",
+                "state": "never",
+                "time": "06:00",
+            },
+        },
+    }
+    hass_storage[DOMAIN] = {
+        "data": storage_data,
+        "key": DOMAIN,
+        "version": store.STORAGE_VERSION,
+        "minor_version": store.STORAGE_VERSION_MINOR,
+    }
+
+    with patch("homeassistant.components.backup.PLATFORMS", [Platform.SENSOR]):
+        await setup_backup_integration(
+            hass, with_hassio=False, remote_agents=["test.remote"]
+        )
+        await hass.async_block_till_done(wait_background_tasks=True)
+
+    state = hass.states.get("sensor.backup_last_successful_automatic_backup")
+    assert state.state == "2024-11-11T03:45:00+00:00"
+    state = hass.states.get("sensor.backup_next_scheduled_automatic_backup")
+    assert state.state == "2024-11-13T05:00:00+00:00"
+
+    # freezer.move_to("2024-11-13T12:00:00+01:00")
+
+    await client.send_json_auto_id(
+        {
+            "type": "backup/config/update",
+            "create_backup": {
+                "agent_ids": ["test.remote"],
+                "include_addons": None,
+                "include_all_addons": False,
+                "include_database": True,
+                "include_folders": None,
+                "name": None,
+                "password": None,
+            },
+        }
+    )
+    await client.receive_json()
+    freezer.move_to("2024-11-13T12:00:00+01:00")
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+    await client.send_json_auto_id({"type": "backup/generate_with_automatic_settings"})
+    await client.receive_json()
+    await hass.async_block_till_done()
+    freezer.tick()
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.backup_last_successful_automatic_backup")
+    assert state.state == "2024-11-13T11:00:00+00:00"
+    state = hass.states.get("sensor.backup_next_scheduled_automatic_backup")
+    assert state.state == "2024-11-14T05:00:00+00:00"
