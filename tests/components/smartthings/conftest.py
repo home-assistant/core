bff73ee5f8f92a886f79c20fd213074541f2f1e9
Joost Lekkerkerker <joostlek@outlook.com>
2025-03-15 20:28:04 +0100

    Add EHS test fixture to SmartThings (#140199)

--- tests/components/smartthings/conftest.py
+++ tests/components/smartthings/conftest.py
@@ -106,6 +106,7 @@ def mock_smartthings() -> Generator[AsyncMock]:
         "da_ref_normal_000001",
         "vd_network_audio_002s",
         "iphone",
+        "da_sac_ehs_000001_sub",
         "da_wm_dw_000001",
         "da_wm_wd_000001",
         "da_wm_wd_000001_1",
