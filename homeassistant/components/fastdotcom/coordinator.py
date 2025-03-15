b460912ffe7790bb8d52d5e93a689082066368bf
rosenrot00 <121129193+rosenrot00@users.noreply.github.com>
2025-03-15 22:05:32 +0100

    Enhance for upload and ping measurements

--- homeassistant/components/fastdotcom/coordinator.py
+++ homeassistant/components/fastdotcom/coordinator.py
@@ -4,18 +4,19 @@ from __future__ import annotations
 
 from datetime import timedelta
 
-from fastdotcom import fast_com
+from fastdotcom2 import fast_com2
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import DEFAULT_INTERVAL, DOMAIN, LOGGER
+from functools import partial
 
 type FastdotcomConfigEntry = ConfigEntry[FastdotcomDataUpdateCoordinator]
 
 
-class FastdotcomDataUpdateCoordinator(DataUpdateCoordinator[float]):
+class FastdotcomDataUpdateCoordinator(DataUpdateCoordinator[str]):
     """Class to manage fetching Fast.com data API."""
 
     def __init__(self, hass: HomeAssistant, entry: FastdotcomConfigEntry) -> None:
@@ -28,9 +29,9 @@ class FastdotcomDataUpdateCoordinator(DataUpdateCoordinator[float]):
             update_interval=timedelta(hours=DEFAULT_INTERVAL),
         )
 
-    async def _async_update_data(self) -> float:
+    async def _async_update_data(self) -> str:
         """Run an executor job to retrieve Fast.com data."""
         try:
-            return await self.hass.async_add_executor_job(fast_com)
+            return await self.hass.async_add_executor_job(partial(fast_com2,maxtime=10))
         except Exception as exc:
             raise UpdateFailed(f"Error communicating with Fast.com: {exc}") from exc
