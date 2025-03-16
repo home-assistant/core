2c19b2fb29c63bf810baa7f822264c200038e6f8
Brett <brett.whynot@gmail.com>
2025-03-16 11:44:53 +1000

    Fix auto seat heater

--- homeassistant/components/teslemetry/switch.py
+++ homeassistant/components/teslemetry/switch.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 from itertools import chain
 from typing import Any
 
-from tesla_fleet_api.const import Scope, Seat
+from tesla_fleet_api.const import Scope
 from teslemetry_stream import TeslemetryStreamVehicle
 
 from homeassistant.components.switch import (
@@ -62,21 +62,15 @@ VEHICLE_DESCRIPTIONS: tuple[TeslemetrySwitchEntityDescription, ...] = (
     TeslemetrySwitchEntityDescription(
         key="climate_state_auto_seat_climate_left",
         streaming_listener=lambda x, y: x.listen_AutoSeatClimateLeft(y),
-        on_func=lambda api: api.remote_auto_seat_climate_request(Seat.FRONT_LEFT, True),
-        off_func=lambda api: api.remote_auto_seat_climate_request(
-            Seat.FRONT_LEFT, False
-        ),
+        on_func=lambda api: api.remote_auto_seat_climate_request(1, True),
+        off_func=lambda api: api.remote_auto_seat_climate_request(1, False),
         scopes=[Scope.VEHICLE_CMDS],
     ),
     TeslemetrySwitchEntityDescription(
         key="climate_state_auto_seat_climate_right",
         streaming_listener=lambda x, y: x.listen_AutoSeatClimateRight(y),
-        on_func=lambda api: api.remote_auto_seat_climate_request(
-            Seat.FRONT_RIGHT, True
-        ),
-        off_func=lambda api: api.remote_auto_seat_climate_request(
-            Seat.FRONT_RIGHT, False
-        ),
+        on_func=lambda api: api.remote_auto_seat_climate_request(2, True),
+        off_func=lambda api: api.remote_auto_seat_climate_request(2, False),
         scopes=[Scope.VEHICLE_CMDS],
     ),
     TeslemetrySwitchEntityDescription(
