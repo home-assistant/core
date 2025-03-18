c2a3128bb6fb7d5cd73203a311a550631e3eaf39
KarimTIS <karim@tiscontrol.com>
2025-03-18 17:39:39 +0200

    Update config_flow.py

--- homeassistant/components/tis_control/config_flow.py
+++ homeassistant/components/tis_control/config_flow.py
@@ -41,7 +41,7 @@ class TISConfigFlow(ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user",
             data_schema=STEP_USER_DATA_SCHEMA,
-            errors=errors if errors else {},
+            errors=errors,
             description_placeholders={CONF_PORT: "Port"},
         )
 
