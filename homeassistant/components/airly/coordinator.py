595ac9fef2a9e072f8fe4177edb7f5f30c68b099
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-03-18 11:38:01 +0100

    Add exception translations

--- homeassistant/components/airly/coordinator.py
+++ homeassistant/components/airly/coordinator.py
@@ -105,7 +105,11 @@ class AirlyDataUpdateCoordinator(DataUpdateCoordinator[dict[str, str | float | i
             try:
                 await measurements.update()
             except (AirlyError, ClientConnectorError) as error:
-                raise UpdateFailed(error) from error
+                raise UpdateFailed(
+                    translation_domain=DOMAIN,
+                    translation_key="update_error",
+                    translation_placeholders={"error": repr(error)},
+                ) from error
 
         _LOGGER.debug(
             "Requests remaining: %s/%s",
@@ -126,7 +130,7 @@ class AirlyDataUpdateCoordinator(DataUpdateCoordinator[dict[str, str | float | i
         standards = measurements.current["standards"]
 
         if index["description"] == NO_AIRLY_SENSORS:
-            raise UpdateFailed("Can't retrieve data: no Airly sensors in this area")
+            raise UpdateFailed(translation_domain=DOMAIN, translation_key="no_station")
         for value in values:
             data[value["name"]] = value["value"]
         for standard in standards:
