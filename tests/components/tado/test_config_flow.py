25b850b178dd5687d7a34111dd54b1fc4c99dbcf
Erwin Douna <e.douna@gmail.com>
2025-03-19 20:48:20 +0000

    Add reauth test

--- tests/components/tado/test_config_flow.py
+++ tests/components/tado/test_config_flow.py
@@ -7,9 +7,9 @@ from unittest.mock import AsyncMock, MagicMock, patch
 from PyTado.exceptions import TadoException
 from PyTado.http import DeviceActivationStatus
 
-from homeassistant.components.tado import CONF_REFRESH_TOKEN
 from homeassistant.components.tado.const import (
     CONF_FALLBACK,
+    CONF_REFRESH_TOKEN,
     CONST_OVERLAY_TADO_DEFAULT,
     DOMAIN,
 )
@@ -55,6 +55,56 @@ async def test_full_flow(
     assert result["title"] == "home name"
     assert result["data"] == {CONF_REFRESH_TOKEN: "refresh"}
     assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_full_flow_reauth(
+    hass: HomeAssistant,
+    mock_tado_api: MagicMock,
+    mock_setup_entry: AsyncMock,
+) -> None:
+    """Test the full flow of the config when reauthticating."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        unique_id="ABC-123-DEF-456",
+        data={CONF_REFRESH_TOKEN: "totally_refresh_for_reauth"},
+    )
+    entry.add_to_hass(hass)
+
+    result = await entry.start_reauth_flow(hass)
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reauth_confirm"
+
+    # The no user input
+    result = await hass.config_entries.flow.async_configure(result["flow_id"])
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reauth_confirm"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={}
+    )
+
+    event = threading.Event()
+
+    def mock_tado_api_device_activation() -> None:
+        # Simulate the device activation process
+        event.wait(timeout=5)
+
+    mock_tado_api.device_activation = mock_tado_api_device_activation
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.SHOW_PROGRESS
+    assert result["step_id"] == "user"
+
+    event.set()
+    await hass.async_block_till_done()
+
+    result = await hass.config_entries.flow.async_configure(result["flow_id"])
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == "home name"
+    assert result["data"] == {CONF_REFRESH_TOKEN: "refresh"}
 
 
 async def test_auth_timeout(
