37a2b888445eb0259274f50826eb711286f82d2b
puddly <32534428+puddly@users.noreply.github.com>
2025-03-20 17:50:15 -0400

    Register the USB event handler globally to react better to re-plugs

--- homeassistant/components/homeassistant_sky_connect/__init__.py
+++ homeassistant/components/homeassistant_sky_connect/__init__.py
@@ -10,10 +10,41 @@ from homeassistant.components.usb import USBDevice, async_register_port_event_ca
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.typing import ConfigType
 
-from .const import DESCRIPTION, DEVICE, FIRMWARE, FIRMWARE_VERSION, PRODUCT
+from .const import DESCRIPTION, DEVICE, DOMAIN, FIRMWARE, FIRMWARE_VERSION, PRODUCT
 
 _LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the ZBT-1 integration."""
+
+    @callback
+    def async_port_event_callback(
+        added: set[USBDevice], removed: set[USBDevice]
+    ) -> None:
+        """Handle USB port events."""
+        current_entries = {
+            entry.data[DEVICE]: entry
+            for entry in hass.config_entries.async_entries(DOMAIN)
+        }
+
+        for device in added | removed:
+            path = device.device
+            entry = current_entries.get(path)
+
+            if entry is not None:
+                _LOGGER.debug(
+                    "Device %r has changed state, reloading config entry %s",
+                    path,
+                    entry,
+                )
+                hass.async_create_task(hass.config_entries.async_reload(entry.entry_id))
+
+    async_register_port_event_callback(hass, async_port_event_callback)
+
+    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -23,28 +54,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     device_path = entry.data[DEVICE]
     if not await hass.async_add_executor_job(os.path.exists, device_path):
         raise ConfigEntryNotReady
-
-    @callback
-    def async_port_event_callback(
-        added: set[USBDevice], removed: set[USBDevice]
-    ) -> None:
-        """Handle USB port events."""
-        if not added and not removed:
-            return
-
-        for device in removed:
-            if device.device == device_path:
-                _LOGGER.debug(
-                    "Device %r has been unplugged, reloading config entry",
-                    device.device,
-                )
-                hass.async_create_task(hass.config_entries.async_reload(entry.entry_id))
-                return
-
-    # Handle the ZBT-1 being unplugged
-    entry.async_on_unload(
-        async_register_port_event_callback(hass, async_port_event_callback)
-    )
 
     await hass.config_entries.async_forward_entry_setups(entry, ["update"])
 
