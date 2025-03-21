fedb71f140b91c439c2fed663914308cc0d3ea07
Thomas <thomas.protzner@gmail.com>
2025-03-21 22:20:25 +0000

    Add diagnostics

---
+++ homeassistant/components/remote_calendar/diagnostics.py
@@ -0,0 +1,22 @@
+"""Provides diagnostics for local calendar."""
+
+import datetime
+from typing import Any
+
+from homeassistant.core import HomeAssistant
+from homeassistant.util import dt as dt_util
+
+from . import RemoteCalendarConfigEntry
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: RemoteCalendarConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    coordinator = entry.runtime_data
+    return {
+        "now": dt_util.now().isoformat(),
+        "timezone": str(dt_util.get_default_time_zone()),
+        "system_timezone": str(datetime.datetime.now().astimezone().tzinfo),
+        "data": coordinator.data,
+    }
