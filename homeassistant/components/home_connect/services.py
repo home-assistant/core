e73859937d389094acd6b82739947ed3c78861dd
J. Diego Rodríguez Royo <jdrr1998@hotmail.com>
2025-03-22 11:23:38 +0000

    Move more fuctions to module level

--- homeassistant/components/home_connect/services.py
+++ homeassistant/components/home_connect/services.py
@@ -435,90 +435,95 @@ async def async_service_setting(call: ServiceCall) -> None:
         ) from err
 
 
+async def async_service_pause_program(call: ServiceCall) -> None:
+    """Service for pausing a program."""
+    await _async_service_command(call, CommandKey.BSH_COMMON_PAUSE_PROGRAM)
+
+
+async def async_service_resume_program(call: ServiceCall) -> None:
+    """Service for resuming a paused program."""
+    await _async_service_command(call, CommandKey.BSH_COMMON_RESUME_PROGRAM)
+
+
+async def async_service_select_program(call: ServiceCall) -> None:
+    """Service for selecting a program."""
+    await _async_service_program(call, False)
+
+
+async def async_service_set_program_and_options(call: ServiceCall) -> None:
+    """Service for setting a program and options."""
+    data = dict(call.data)
+    program = data.pop(ATTR_PROGRAM, None)
+    affects_to = data.pop(ATTR_AFFECTS_TO)
+    client, ha_id = await _get_client_and_ha_id(call.hass, data.pop(ATTR_DEVICE_ID))
+
+    options: list[Option] = []
+
+    for option, value in data.items():
+        if option in PROGRAM_ENUM_OPTIONS:
+            options.append(
+                Option(
+                    PROGRAM_ENUM_OPTIONS[option][0],
+                    PROGRAM_ENUM_OPTIONS[option][1][value],
+                )
+            )
+        elif option in PROGRAM_OPTIONS:
+            option_key = PROGRAM_OPTIONS[option][0]
+            options.append(Option(option_key, value))
+
+    method_call: Awaitable[Any]
+    exception_translation_key: str
+    if program:
+        program = (
+            program
+            if isinstance(program, ProgramKey)
+            else TRANSLATION_KEYS_PROGRAMS_MAP[program]
+        )
+
+        if affects_to == AFFECTS_TO_ACTIVE_PROGRAM:
+            method_call = client.start_program(
+                ha_id, program_key=program, options=options
+            )
+            exception_translation_key = "start_program"
+        elif affects_to == AFFECTS_TO_SELECTED_PROGRAM:
+            method_call = client.set_selected_program(
+                ha_id, program_key=program, options=options
+            )
+            exception_translation_key = "select_program"
+    else:
+        array_of_options = ArrayOfOptions(options)
+        if affects_to == AFFECTS_TO_ACTIVE_PROGRAM:
+            method_call = client.set_active_program_options(
+                ha_id, array_of_options=array_of_options
+            )
+            exception_translation_key = "set_options_active_program"
+        else:
+            # affects_to is AFFECTS_TO_SELECTED_PROGRAM
+            method_call = client.set_selected_program_options(
+                ha_id, array_of_options=array_of_options
+            )
+            exception_translation_key = "set_options_selected_program"
+
+    try:
+        await method_call
+    except HomeConnectError as err:
+        raise HomeAssistantError(
+            translation_domain=DOMAIN,
+            translation_key=exception_translation_key,
+            translation_placeholders={
+                **get_dict_from_home_connect_error(err),
+                **({"program": program} if program else {}),
+            },
+        ) from err
+
+
+async def async_service_start_program(call: ServiceCall) -> None:
+    """Service for starting a program."""
+    await _async_service_program(call, True)
+
+
 def register_actions(hass: HomeAssistant) -> None:
     """Register custom actions."""
-
-    async def async_service_pause_program(call: ServiceCall) -> None:
-        """Service for pausing a program."""
-        await _async_service_command(call, CommandKey.BSH_COMMON_PAUSE_PROGRAM)
-
-    async def async_service_resume_program(call: ServiceCall) -> None:
-        """Service for resuming a paused program."""
-        await _async_service_command(call, CommandKey.BSH_COMMON_RESUME_PROGRAM)
-
-    async def async_service_select_program(call: ServiceCall) -> None:
-        """Service for selecting a program."""
-        await _async_service_program(call, False)
-
-    async def async_service_set_program_and_options(call: ServiceCall) -> None:
-        """Service for setting a program and options."""
-        data = dict(call.data)
-        program = data.pop(ATTR_PROGRAM, None)
-        affects_to = data.pop(ATTR_AFFECTS_TO)
-        client, ha_id = await _get_client_and_ha_id(hass, data.pop(ATTR_DEVICE_ID))
-
-        options: list[Option] = []
-
-        for option, value in data.items():
-            if option in PROGRAM_ENUM_OPTIONS:
-                options.append(
-                    Option(
-                        PROGRAM_ENUM_OPTIONS[option][0],
-                        PROGRAM_ENUM_OPTIONS[option][1][value],
-                    )
-                )
-            elif option in PROGRAM_OPTIONS:
-                option_key = PROGRAM_OPTIONS[option][0]
-                options.append(Option(option_key, value))
-
-        method_call: Awaitable[Any]
-        exception_translation_key: str
-        if program:
-            program = (
-                program
-                if isinstance(program, ProgramKey)
-                else TRANSLATION_KEYS_PROGRAMS_MAP[program]
-            )
-
-            if affects_to == AFFECTS_TO_ACTIVE_PROGRAM:
-                method_call = client.start_program(
-                    ha_id, program_key=program, options=options
-                )
-                exception_translation_key = "start_program"
-            elif affects_to == AFFECTS_TO_SELECTED_PROGRAM:
-                method_call = client.set_selected_program(
-                    ha_id, program_key=program, options=options
-                )
-                exception_translation_key = "select_program"
-        else:
-            array_of_options = ArrayOfOptions(options)
-            if affects_to == AFFECTS_TO_ACTIVE_PROGRAM:
-                method_call = client.set_active_program_options(
-                    ha_id, array_of_options=array_of_options
-                )
-                exception_translation_key = "set_options_active_program"
-            else:
-                # affects_to is AFFECTS_TO_SELECTED_PROGRAM
-                method_call = client.set_selected_program_options(
-                    ha_id, array_of_options=array_of_options
-                )
-                exception_translation_key = "set_options_selected_program"
-
-        try:
-            await method_call
-        except HomeConnectError as err:
-            raise HomeAssistantError(
-                translation_domain=DOMAIN,
-                translation_key=exception_translation_key,
-                translation_placeholders={
-                    **get_dict_from_home_connect_error(err),
-                    **({"program": program} if program else {}),
-                },
-            ) from err
-
-    async def async_service_start_program(call: ServiceCall) -> None:
-        """Service for starting a program."""
-        await _async_service_program(call, True)
 
     hass.services.async_register(
         DOMAIN,
