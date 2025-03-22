6f1931fd36a6a5cdd2869b76d85dbe07d7fbe64c
tdfountain <174762217+tdfountain@users.noreply.github.com>
2025-03-22 16:51:36 -0700

    Update homeassistant/components/nut/switch.py
    Co-authored-by: J. Nick Koston <nick+github@koston.org>

--- homeassistant/components/nut/switch.py
+++ homeassistant/components/nut/switch.py
@@ -94,7 +94,6 @@ class NUTSwitch(NUTBaseEntity, SwitchEntity):
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the device."""
-        _LOGGER.debug("turn_off -> kwargs: %s", kwargs)
 
         name_list = self.entity_description.key.split(".")
         command_name = f"{name_list[0]}.{name_list[1]}.load.off"
