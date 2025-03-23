ff441f816df35772a817d201f581750439bfe79a
tdfountain <174762217+tdfountain@users.noreply.github.com>
2025-03-22 21:33:06 -0500

    Make switch description creation more pythonic

--- homeassistant/components/nut/switch.py
+++ homeassistant/components/nut/switch.py
@@ -25,8 +25,6 @@ async def async_setup_entry(
     async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Set up the NUT switches."""
-    available_switch_types: dict[str, SwitchEntityDescription] = {}
-
     pynut_data = config_entry.runtime_data
     coordinator = pynut_data.coordinator
     status = coordinator.data
@@ -37,39 +35,29 @@ async def async_setup_entry(
 
     data = pynut_data.data
     unique_id = pynut_data.unique_id
-    for outlet_num in range(1, int(num_outlets) + 1):
-        outlet_num_str = str(outlet_num)
-        outlet_name: str = status.get(f"outlet.{outlet_num_str}.name") or outlet_num_str
-
-        # Add when outlet is switchable and both on and off are available
+    user_available_commands = pynut_data.user_available_commands
+    switch_descriptions = [
+        SwitchEntityDescription(
+            key=f"outlet.{outlet_num!s}.load.poweronoff",
+            translation_key="outlet_number_load_poweronoff",
+            translation_placeholders={
+                "outlet_name": status.get(f"outlet.{outlet_num!s}.name")
+                or str(outlet_num)
+            },
+            device_class=SwitchDeviceClass.OUTLET,
+            entity_registry_enabled_default=True,
+        )
+        for outlet_num in range(1, int(num_outlets) + 1)
         if (
-            (status.get(f"outlet.{outlet_num_str}.switchable") == "yes")
-            and (
-                f"outlet.{outlet_num_str}.load.on" in pynut_data.user_available_commands
-            )
-            and (
-                f"outlet.{outlet_num_str}.load.off"
-                in pynut_data.user_available_commands
-            )
-        ):
-            available_switch_types |= {
-                f"outlet.{outlet_num_str}.load.poweronoff": SwitchEntityDescription(
-                    key=f"outlet.{outlet_num_str}.load.poweronoff",
-                    translation_key="outlet_number_load_poweronoff",
-                    translation_placeholders={"outlet_name": outlet_name},
-                    device_class=SwitchDeviceClass.OUTLET,
-                    entity_registry_enabled_default=True,
-                ),
-            }
+            status.get(f"outlet.{outlet_num!s}.switchable") == "yes"
+            and f"outlet.{outlet_num!s}.load.on" in user_available_commands
+            and f"outlet.{outlet_num!s}.load.off" in user_available_commands
+        )
+    ]
 
     async_add_entities(
-        NUTSwitch(
-            coordinator,
-            available_switch_types[switch_type],
-            data,
-            unique_id,
-        )
-        for switch_type in available_switch_types
+        NUTSwitch(coordinator, description, data, unique_id)
+        for description in switch_descriptions
     )
 
 
