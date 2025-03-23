4059963837adce6c172ab8a47a2ccf9a11dc64ec
Galorhallen <andrea.ponte1987@gmail.com>
2025-03-23 10:47:26 +0000

    Removed commented code

--- homeassistant/components/govee_light_local/__init__.py
+++ homeassistant/components/govee_light_local/__init__.py
@@ -95,8 +95,6 @@ async def update_options_listener(
             config_entry.options[CONF_IPS_TO_REMOVE].remove(ip)
             config_entry.options[CONF_MANUAL_DEVICES].remove(ip)
 
-        # await hass.config_entries.async_reload(config_entry.entry_id)
-
     if (
         config.option_mode == OptionMode.CONFIGURE_AUTO_DISCOVERY
         and coordinator.discovery_enabled != config.auto_discovery
