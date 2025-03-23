5dd56214663d644b1934d9dad89eb5ea10d02d4c
Simone Chemelli <simone.chemelli@gmail.com>
2025-03-23 15:40:37 +0000

    Add device reconfigure to Vodafone Station config flow

--- homeassistant/components/vodafone_station/config_flow.py
+++ homeassistant/components/vodafone_station/config_flow.py
@@ -139,6 +139,49 @@ class VodafoneStationConfigFlow(ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle reconfiguration of the device."""
+        reconfigure_entry = self._get_reconfigure_entry()
+        if not user_input:
+            return self.async_show_form(
+                step_id="reconfigure", data_schema=user_form_schema(user_input)
+            )
+
+        updated_host = user_input[CONF_HOST]
+
+        if reconfigure_entry.data.get(CONF_HOST) != updated_host:
+            self._async_abort_entries_match({CONF_HOST: updated_host})
+
+        errors: dict[str, str] = {}
+
+        errors = {}
+
+        try:
+            await validate_input(self.hass, user_input)
+        except aiovodafone_exceptions.AlreadyLogged:
+            errors["base"] = "already_logged"
+        except aiovodafone_exceptions.CannotConnect:
+            errors["base"] = "cannot_connect"
+        except aiovodafone_exceptions.CannotAuthenticate:
+            errors["base"] = "invalid_auth"
+        except aiovodafone_exceptions.ModelNotSupported:
+            errors["base"] = "model_not_supported"
+        except Exception:  # noqa: BLE001
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            return self.async_update_reload_and_abort(
+                reconfigure_entry, data_updates={CONF_HOST: updated_host}
+            )
+
+        return self.async_show_form(
+            step_id="reconfigure",
+            data_schema=user_form_schema(user_input),
+            errors=errors,
+        )
+
 
 class VodafoneStationOptionsFlowHandler(OptionsFlow):
     """Handle a option flow."""
