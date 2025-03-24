0a3a75c44954a72420a1fc757b4918a2dd7b0f2c
Mark Hannon <mark.hannon@gmail.com>
2025-03-24 01:20:36 +0000

    Config_flow with support for auto-discovery of one or more zcc or fallback to manual configuration.

--- homeassistant/components/zimi/config_flow.py
+++ homeassistant/components/zimi/config_flow.py
@@ -20,6 +20,12 @@ from homeassistant import config_entries
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import CONF_HOST, CONF_MAC, CONF_PORT
 from homeassistant.helpers.device_registry import format_mac
+from homeassistant.helpers.selector import (
+    SelectOptionDict,
+    SelectSelector,
+    SelectSelectorConfig,
+    SelectSelectorMode,
+)
 
 from . import async_connect_to_controller
 from .const import DEFAULT_PORT, DOMAIN, TITLE
@@ -32,6 +38,8 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
         vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
     }
 )
+
+SELECTED_HOST_AND_PORT = "selected_host_and_port"
 
 
 class ZimiConfigErrors(StrEnum):
@@ -51,6 +59,7 @@ class ZimiConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for zcc."""
 
     api: ControlPoint = None
+    api_descriptions: list[ControlPointDescription]
     data: dict[str, Any]
 
     def __del__(self):
@@ -63,36 +72,57 @@ class ZimiConfigFlow(ConfigFlow, domain=DOMAIN):
     ) -> config_entries.ConfigFlowResult:
         """Handle the initial auto-discovery step."""
 
-        api_descriptions: list[ControlPointDescription]
-
-        self.data: dict[str, str] = {}
+        self.data = {}
 
         try:
-            api_descriptions = await ControlPointDiscoveryService().discovers()
+            self.api_descriptions = await ControlPointDiscoveryService().discovers()
         except ControlPointError as e:
             _LOGGER.error(e)
             return await self.async_step_manual()
 
-        for api_description in api_descriptions:
-            self.data[CONF_HOST] = api_description.host
-            self.data[CONF_PORT] = api_description.port
+        if len(self.api_descriptions) == 1:
+            self.data[CONF_HOST] = self.api_descriptions[0].host
+            self.data[CONF_PORT] = self.api_descriptions[0].port
+            return await self.create_entry()
 
-            with contextlib.suppress(ControlPointError):
-                self.api = await async_connect_to_controller(
-                    self.data[CONF_HOST], self.data[CONF_PORT], fast=True
+        return await self.async_step_selection()
+
+    async def async_step_selection(
+        self, user_input: dict[str, Any] | None = None
+    ) -> config_entries.ConfigFlowResult:
+        """Handle selection of zcc to configure if multiple are discovered."""
+
+        errors: dict[str, str] = {}
+
+        if user_input is not None:
+            self.data[CONF_HOST] = user_input[SELECTED_HOST_AND_PORT].split(":")[0]
+            self.data[CONF_PORT] = int(user_input[SELECTED_HOST_AND_PORT].split(":")[1])
+            return await self.create_entry()
+
+        available_options = [
+            SelectOptionDict(
+                label=f"{description.host}:{description.port}",
+                value=f"{description.host}:{description.port}",
+            )
+            for description in self.api_descriptions
+        ]
+
+        available_schema = vol.Schema(
+            {
+                vol.Required(
+                    SELECTED_HOST_AND_PORT, default=available_options[0]["value"]
+                ): SelectSelector(
+                    SelectSelectorConfig(
+                        options=available_options,
+                        mode=SelectSelectorMode.DROPDOWN,
+                    )
                 )
+            }
+        )
 
-            if self.api and self.api.ready:
-                self.data[CONF_MAC] = format_mac(self.api.mac)
-                self.api.disconnect()
-                await self.async_set_unique_id(self.data[CONF_MAC])
-                self._abort_if_unique_id_configured()
-                return self.async_create_entry(
-                    title=f"{TITLE} ({self.data[CONF_HOST]}:{self.data[CONF_PORT]})",
-                    data=self.data,
-                )
-
-        return await self.async_step_manual()
+        return self.async_show_form(
+            step_id="selection", data_schema=available_schema, errors=errors
+        )
 
     async def async_step_manual(
         self, user_input: dict[str, Any] | None = None
@@ -108,16 +138,8 @@ class ZimiConfigFlow(ConfigFlow, domain=DOMAIN):
                 self.data[CONF_HOST], self.data[CONF_PORT]
             )
 
-        if self.api and not errors:
-            self.api.disconnect()
-            self.api = None
-
-            await self.async_set_unique_id(self.data[CONF_MAC])
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(
-                title=f"{TITLE} ({self.data[CONF_HOST]}:{self.data[CONF_PORT]})",
-                data=self.data,
-            )
+            if not errors:
+                return await self.create_entry()
 
         return self.async_show_form(
             step_id="manual",
@@ -126,6 +148,26 @@ class ZimiConfigFlow(ConfigFlow, domain=DOMAIN):
             ),
             errors=errors,
         )
+
+    async def create_entry(self) -> config_entries.ConfigFlowResult:
+        """Create entry for zcc."""
+
+        with contextlib.suppress(ControlPointError):
+            self.api = await async_connect_to_controller(
+                self.data[CONF_HOST], self.data[CONF_PORT], fast=True
+            )
+
+        if self.api and self.api.ready:
+            self.data[CONF_MAC] = format_mac(self.api.mac)
+            self.api.disconnect()
+            await self.async_set_unique_id(self.data[CONF_MAC])
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(
+                title=f"{TITLE} ({self.data[CONF_HOST]}:{self.data[CONF_PORT]})",
+                data=self.data,
+            )
+
+        return self.async_abort(reason="cannot_connect")
 
     async def validate_connection(self, host: str, port: int) -> dict[str, str]:
         """Check for errors with configuration.
--- tests/components/zimi/test_config_flow.py
+++ tests/components/zimi/test_config_flow.py
@@ -1,6 +1,6 @@
 """Tests for the zimi config flow."""
 
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 from zcc import ControlPoint, ControlPointDescription, ControlPointError
@@ -14,11 +14,15 @@ from homeassistant.data_entry_flow import FlowResultType
 from homeassistant.helpers.device_registry import format_mac
 
 INPUT_MAC = "aa:bb:cc:dd:ee:ff"
+INPUT_MAC_EXTRA = "aa:bb:cc:dd:ee:ee"
 INPUT_HOST = "192.168.1.100"
+INPUT_HOST_EXTRA = "192.168.1.101"
 INPUT_PORT = 5003
+INPUT_PORT_EXTRA = 5004
 
 INVALID_INPUT_MAC = "xyz"
 MISMATCHED_INPUT_MAC = "aa:bb:cc:dd:ee:ee"
+SELECTED_HOST_AND_PORT = "selected_host_and_port"
 
 
 @pytest.fixture
@@ -39,7 +43,6 @@ def discovery_mock():
         "homeassistant.components.zimi.config_flow.ControlPointDiscoveryService",
         autospec=True,
     ) as mock:
-        mock.discover = AsyncMock()
         mock.return_value = mock
         yield mock
 
@@ -80,13 +83,48 @@ async def test_user_discovery_success(
     }
 
 
+async def test_user_discovery_success_selection(
+    hass: HomeAssistant,
+    api_mock: MagicMock,
+    discovery_mock: MagicMock,
+    socket_mock: MagicMock,
+) -> None:
+    """Test user form transitions via selection to creation if zcc discovery succeeds has multiple hosts."""
+
+    discovery_mock.discovers.return_value = [
+        ControlPointDescription(host=INPUT_HOST, port=INPUT_PORT),
+        ControlPointDescription(host=INPUT_HOST_EXTRA, port=INPUT_PORT_EXTRA),
+    ]
+
+    api_mock.return_value.mac = INPUT_MAC_EXTRA
+    api_mock.return_value.ready = True
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            SELECTED_HOST_AND_PORT: INPUT_HOST_EXTRA + ":" + str(INPUT_PORT_EXTRA),
+        },
+    )
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["data"] == {
+        "host": INPUT_HOST_EXTRA,
+        "port": INPUT_PORT_EXTRA,
+        "mac": format_mac(INPUT_MAC_EXTRA),
+    }
+
+
 async def test_user_discovery_failure(
     hass: HomeAssistant,
     discovery_mock: MagicMock,
 ) -> None:
     """Test user form transitions to finish step if zcc discovery fails."""
 
-    discovery_mock.discover.side_effect = ControlPointError("Discovery failed")
+    discovery_mock.discovers.side_effect = ControlPointError("Discovery failed")
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -104,7 +142,7 @@ async def test_finish_manual_success(
 ) -> None:
     """Test finish form transitions to creation with valid data."""
 
-    discovery_mock.discover.side_effect = ControlPointError("Discovery failed")
+    discovery_mock.discovers.side_effect = ControlPointError("Discovery failed")
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -138,7 +176,7 @@ async def test_finish_manual_cannot_connect(
 ) -> None:
     """Test finish form transitions via cannot_connect to creation."""
 
-    discovery_mock.discover.side_effect = ControlPointError("Discovery failed")
+    discovery_mock.discovers.side_effect = ControlPointError("Discovery failed")
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -189,7 +227,7 @@ async def test_finish_manual_gethostbyname_error(
 ) -> None:
     """Test finish form transitions via gethostbyname failure to creation."""
 
-    discovery_mock.discover.side_effect = ControlPointError("Discovery failed")
+    discovery_mock.discovers.side_effect = ControlPointError("Discovery failed")
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -250,7 +288,7 @@ async def test_finish_manual_socket_errors(
 ) -> None:
     """Test finish form transitions via socket errors to creation."""
 
-    discovery_mock.discover.side_effect = ControlPointError("Discovery failed")
+    discovery_mock.discovers.side_effect = ControlPointError("Discovery failed")
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
