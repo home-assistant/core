bf63fe7859b6449809edab2fa8f28279db66b67f
Allen Porter <allen.porter@gmail.com>
2025-03-24 15:13:31 +0000

    Fix double glob

--- homeassistant/components/nest/media_source.py
+++ homeassistant/components/nest/media_source.py
@@ -281,7 +281,7 @@ class NestEventMediaStore(EventMediaStore):
             for device_id, valid_timestamps in event_timestamps.items():
                 media_files = list(media_path.glob(f"{device_id}/*"))
                 _LOGGER.debug("Found %d files (device=%s)", len(media_files), device_id)
-                for media_file in pathlib.Path(self._media_path).glob(f"{device_id}/*"):
+                for media_file in media_files:
                     if "-" not in media_file.name:
                         continue
                     try:
