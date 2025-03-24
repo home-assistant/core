68e5a3f7cf011cac293a38ced3e5500d5f00b249
Robert Resch <robert@resch.dev>
2025-03-24 20:18:57 +0100

    Add missing sensor recorder tests

--- tests/components/sensor/test_recorder.py
+++ tests/components/sensor/test_recorder.py
@@ -1,6 +1,6 @@
 """The tests for sensor recorder platform."""
 
-from collections.abc import Iterable
+from collections.abc import Callable, Iterable
 from datetime import datetime, timedelta
 import logging
 import math
@@ -32,6 +32,8 @@ from homeassistant.components.recorder.models import (
     process_timestamp,
 )
 from homeassistant.components.recorder.statistics import (
+    DEG_TO_RAD,
+    RAD_TO_DEG,
     async_import_statistics,
     get_metadata,
     list_statistic_ids,
@@ -784,7 +786,7 @@ async def test_compile_hourly_statistics_with_all_same_last_updated(
             None,
             StatisticMeanType.CIRCULAR,
             [350, 0, 355, 15],
-        ),  # todo check
+        ),
     ],
 )
 async def test_compile_hourly_statistics_only_state_is_at_end_of_period(
@@ -2684,9 +2686,20 @@ async def test_compile_hourly_statistics_unchanged_angle(
     assert "Error while processing event StatisticsTask" not in caplog.text
 
 
-# todo angle
+@pytest.mark.parametrize(
+    ("attributes", "expected_mean", "expected_min", "expected_max"),
+    [
+        (TEMPERATURE_SENSOR_ATTRIBUTES, 21.1864406779661, 10.0, 25.0),
+        (WIND_DIRECTION_ATTRIBUTES, 21.202479155239875, None, None),
+    ],
+)
 async def test_compile_hourly_statistics_partially_unavailable(
-    hass: HomeAssistant, caplog: pytest.LogCaptureFixture
+    hass: HomeAssistant,
+    caplog: pytest.LogCaptureFixture,
+    attributes: dict,
+    expected_mean: float,
+    expected_min: float | None,
+    expected_max: float | None,
 ) -> None:
     """Test compiling hourly statistics, with the sensor being partially unavailable."""
     zero = get_start_time(dt_util.utcnow())
@@ -2694,7 +2707,7 @@ async def test_compile_hourly_statistics_partially_unavailable(
     # Wait for the sensor recorder platform to be added
     await async_recorder_block_till_done(hass)
     four, states = await async_record_states_partially_unavailable(
-        hass, zero, "sensor.test1", TEMPERATURE_SENSOR_ATTRIBUTES
+        hass, zero, "sensor.test1", attributes
     )
     await async_wait_recording_done(hass)
     hist = history.get_significant_states(
@@ -2710,9 +2723,9 @@ async def test_compile_hourly_statistics_partially_unavailable(
             {
                 "start": process_timestamp(zero).timestamp(),
                 "end": process_timestamp(zero + timedelta(minutes=5)).timestamp(),
-                "mean": pytest.approx(21.1864406779661),
-                "min": pytest.approx(10.0),
-                "max": pytest.approx(25.0),
+                "mean": pytest.approx(expected_mean),
+                "min": pytest.approx(expected_min),
+                "max": pytest.approx(expected_max),
                 "last_reset": None,
                 "state": None,
                 "sum": None,
@@ -4290,20 +4303,17 @@ async def test_compile_hourly_statistics_changing_device_class_2(
     assert "Error while processing event StatisticsTask" not in caplog.text
 
 
-# todo angle
 @pytest.mark.parametrize(
     (
         "device_class",
         "state_unit",
-        "display_unit",
-        "statistics_unit",
         "unit_class",
         "mean",
         "min",
         "max",
     ),
     [
-        (None, None, None, None, "unitless", 13.050847, -10, 30),
+        (None, None, "unitless", 13.050847, -10, 30),
     ],
 )
 async def test_compile_hourly_statistics_changing_state_class(
@@ -4311,8 +4321,6 @@ async def test_compile_hourly_statistics_changing_state_class(
     caplog: pytest.LogCaptureFixture,
     device_class,
     state_unit,
-    display_unit,
-    statistics_unit,
     unit_class,
     mean,
     min,
@@ -4444,7 +4452,6 @@ async def test_compile_hourly_statistics_changing_state_class(
     assert "Error while processing event StatisticsTask" not in caplog.text
 
 
-# todo angle
 @pytest.mark.timeout(25)
 @pytest.mark.parametrize("recorder_config", [{CONF_COMMIT_INTERVAL: 3600 * 4}])
 @pytest.mark.freeze_time("2021-09-01 05:00")  # August 31st, 23:00 local time
@@ -4473,10 +4480,11 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
         "unit_of_measurement": "EUR",
     }
 
+    durations = [50, 200, 45]
+
     def _weighted_average(seq, i, last_state):
         total = 0
         duration = 0
-        durations = [50, 200, 45]
         if i > 0:
             total += last_state * 5
             duration += 5
@@ -4484,6 +4492,24 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
             total += seq[j] * dur
             duration += dur
         return total / duration
+
+    def _time_weighted_circular_mean(values: list[tuple[float, int]]):
+        sin_sum = 0
+        cos_sum = 0
+        for x, dur in values:
+            sin_sum += math.sin(x * DEG_TO_RAD) * dur
+            cos_sum += math.cos(x * DEG_TO_RAD) * dur
+
+        return (RAD_TO_DEG * math.atan2(sin_sum, cos_sum)) % 360
+
+    def _circular_mean(values: list[float]) -> float:
+        sin_sum = 0
+        cos_sum = 0
+        for x in values:
+            sin_sum += math.sin(x * DEG_TO_RAD)
+            cos_sum += math.cos(x * DEG_TO_RAD)
+
+        return (RAD_TO_DEG * math.atan2(sin_sum, cos_sum)) % 360
 
     def _min(seq, last_state):
         if last_state is None:
@@ -4506,17 +4532,24 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
         "sensor.test2": [],
         "sensor.test3": [],
         "sensor.test4": [],
+        "sensor.test5": [],
     }
     expected_minima = {"sensor.test1": [], "sensor.test2": [], "sensor.test3": []}
     expected_maxima = {"sensor.test1": [], "sensor.test2": [], "sensor.test3": []}
-    expected_averages = {"sensor.test1": [], "sensor.test2": [], "sensor.test3": []}
+    expected_means = {
+        "sensor.test1": [],
+        "sensor.test2": [],
+        "sensor.test3": [],
+        "sensor.test5": [],
+    }
     expected_states = {"sensor.test4": []}
     expected_sums = {"sensor.test4": []}
-    last_states = {
+    last_states: dict[str, float | None] = {
         "sensor.test1": None,
         "sensor.test2": None,
         "sensor.test3": None,
         "sensor.test4": None,
+        "sensor.test5": None,
     }
     start = zero
     for i in range(24):
@@ -4529,7 +4562,7 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
         last_state = last_states["sensor.test1"]
         expected_minima["sensor.test1"].append(_min(seq, last_state))
         expected_maxima["sensor.test1"].append(_max(seq, last_state))
-        expected_averages["sensor.test1"].append(_weighted_average(seq, i, last_state))
+        expected_means["sensor.test1"].append(_weighted_average(seq, i, last_state))
         last_states["sensor.test1"] = seq[-1]
         # test2 values change: min/max at the last state
         seq = [-10 * (i + 1), 15 * (i + 1), 30 * (i + 1)]
@@ -4540,7 +4573,7 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
         last_state = last_states["sensor.test2"]
         expected_minima["sensor.test2"].append(_min(seq, last_state))
         expected_maxima["sensor.test2"].append(_max(seq, last_state))
-        expected_averages["sensor.test2"].append(_weighted_average(seq, i, last_state))
+        expected_means["sensor.test2"].append(_weighted_average(seq, i, last_state))
         last_states["sensor.test2"] = seq[-1]
         # test3 values change: min/max at the first state
         seq = [-10 * (23 - i + 1), 15 * (23 - i + 1), 30 * (23 - i + 1)]
@@ -4551,7 +4584,7 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
         last_state = last_states["sensor.test3"]
         expected_minima["sensor.test3"].append(_min(seq, last_state))
         expected_maxima["sensor.test3"].append(_max(seq, last_state))
-        expected_averages["sensor.test3"].append(_weighted_average(seq, i, last_state))
+        expected_means["sensor.test3"].append(_weighted_average(seq, i, last_state))
         last_states["sensor.test3"] = seq[-1]
         # test4 values grow
         seq = [i, i + 0.5, i + 0.75]
@@ -4573,6 +4606,18 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
             _sum(seq, last_state, expected_sums["sensor.test4"])
         )
         last_states["sensor.test4"] = seq[-1]
+
+        # test5 circular mean
+        seq = [350 - i, 0 + (i / 2.0), 15 + i]
+        four, _states = await async_record_states(
+            hass, freezer, start, "sensor.test5", WIND_DIRECTION_ATTRIBUTES, seq
+        )
+        states["sensor.test5"] += _states["sensor.test5"]
+        values = [(seq, durations[j]) for j, seq in enumerate(seq)]
+        if (state := last_states["sensor.test5"]) is not None:
+            values.append((state, 5))
+        expected_means["sensor.test5"].append(_time_weighted_circular_mean(values))
+        last_states["sensor.test5"] = seq[-1]
 
         start += timedelta(minutes=5)
     await async_wait_recording_done(hass)
@@ -4639,6 +4684,17 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
             "statistics_unit_of_measurement": "EUR",
             "unit_class": None,
         },
+        {
+            "statistic_id": "sensor.test5",
+            "display_unit_of_measurement": DEGREE,
+            "has_mean": False,
+            "mean_type": StatisticMeanType.CIRCULAR,
+            "has_sum": False,
+            "name": None,
+            "source": "recorder",
+            "statistics_unit_of_measurement": DEGREE,
+            "unit_class": None,
+        },
     ]
 
     # Adjust the inserted statistics
@@ -4657,6 +4713,7 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
         "sensor.test2": [],
         "sensor.test3": [],
         "sensor.test4": [],
+        "sensor.test5": [],
     }
     start = zero
     end = zero + timedelta(minutes=5)
@@ -4666,11 +4723,10 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
             "sensor.test2",
             "sensor.test3",
             "sensor.test4",
+            "sensor.test5",
         ):
             expected_average = (
-                expected_averages[entity_id][i]
-                if entity_id in expected_averages
-                else None
+                expected_means[entity_id][i] if entity_id in expected_means else None
             )
             expected_minimum = (
                 expected_minima[entity_id][i] if entity_id in expected_minima else None
@@ -4700,176 +4756,78 @@ async def test_compile_statistics_hourly_daily_monthly_summary(
         end += timedelta(minutes=5)
     assert stats == expected_stats
 
-    stats = statistics_during_period(hass, zero, period="hour")
-    expected_stats = {
-        "sensor.test1": [],
-        "sensor.test2": [],
-        "sensor.test3": [],
-        "sensor.test4": [],
-    }
-    start = zero
-    end = zero + timedelta(hours=1)
-    for i in range(2):
-        for entity_id in (
-            "sensor.test1",
-            "sensor.test2",
-            "sensor.test3",
-            "sensor.test4",
-        ):
-            expected_average = (
-                mean(expected_averages[entity_id][i * 12 : (i + 1) * 12])
-                if entity_id in expected_averages
-                else None
-            )
-            expected_minimum = (
-                min(expected_minima[entity_id][i * 12 : (i + 1) * 12])
-                if entity_id in expected_minima
-                else None
-            )
-            expected_maximum = (
-                max(expected_maxima[entity_id][i * 12 : (i + 1) * 12])
-                if entity_id in expected_maxima
-                else None
-            )
-            expected_state = (
-                expected_states[entity_id][(i + 1) * 12 - 1]
-                if entity_id in expected_states
-                else None
-            )
-            expected_sum = (
-                expected_sums[entity_id][(i + 1) * 12 - 1]
-                if entity_id in expected_sums
-                else None
-            )
-            expected_stats[entity_id].append(
-                {
-                    "start": process_timestamp(start).timestamp(),
-                    "end": process_timestamp(end).timestamp(),
-                    "mean": pytest.approx(expected_average),
-                    "min": pytest.approx(expected_minimum),
-                    "max": pytest.approx(expected_maximum),
-                    "last_reset": None,
-                    "state": expected_state,
-                    "sum": expected_sum,
-                }
-            )
-        start += timedelta(hours=1)
-        end += timedelta(hours=1)
-    assert stats == expected_stats
+    def verify_stats(
+        period: Literal["5minute", "day", "hour", "week", "month"],
+        start: datetime,
+        next_datetime: Callable[[datetime], datetime],
+    ) -> None:
+        stats = statistics_during_period(hass, zero, period=period)
+        expected_stats = {
+            "sensor.test1": [],
+            "sensor.test2": [],
+            "sensor.test3": [],
+            "sensor.test4": [],
+            "sensor.test5": [],
+        }
+        end = next_datetime(start)
+        for i in range(2):
+            for entity_id, mean_fn in (
+                ("sensor.test1", mean),
+                ("sensor.test2", mean),
+                ("sensor.test3", mean),
+                ("sensor.test4", mean),
+                ("sensor.test5", _circular_mean),
+            ):
+                expected_average = (
+                    mean_fn(expected_means[entity_id][i * 12 : (i + 1) * 12])
+                    if entity_id in expected_means
+                    else None
+                )
+                expected_minimum = (
+                    min(expected_minima[entity_id][i * 12 : (i + 1) * 12])
+                    if entity_id in expected_minima
+                    else None
+                )
+                expected_maximum = (
+                    max(expected_maxima[entity_id][i * 12 : (i + 1) * 12])
+                    if entity_id in expected_maxima
+                    else None
+                )
+                expected_state = (
+                    expected_states[entity_id][(i + 1) * 12 - 1]
+                    if entity_id in expected_states
+                    else None
+                )
+                expected_sum = (
+                    expected_sums[entity_id][(i + 1) * 12 - 1]
+                    if entity_id in expected_sums
+                    else None
+                )
+                expected_stats[entity_id].append(
+                    {
+                        "start": process_timestamp(start).timestamp(),
+                        "end": process_timestamp(end).timestamp(),
+                        "mean": pytest.approx(expected_average),
+                        "min": pytest.approx(expected_minimum),
+                        "max": pytest.approx(expected_maximum),
+                        "last_reset": None,
+                        "state": expected_state,
+                        "sum": expected_sum,
+                    }
+                )
+            start = next_datetime(start)
+            end = next_datetime(end)
+        assert stats == expected_stats
 
-    stats = statistics_during_period(hass, zero, period="day")
-    expected_stats = {
-        "sensor.test1": [],
-        "sensor.test2": [],
-        "sensor.test3": [],
-        "sensor.test4": [],
-    }
+    verify_stats("hour", zero, lambda v: v + timedelta(hours=1))
+
     start = dt_util.parse_datetime("2021-08-31T06:00:00+00:00")
-    end = start + timedelta(days=1)
-    for i in range(2):
-        for entity_id in (
-            "sensor.test1",
-            "sensor.test2",
-            "sensor.test3",
-            "sensor.test4",
-        ):
-            expected_average = (
-                mean(expected_averages[entity_id][i * 12 : (i + 1) * 12])
-                if entity_id in expected_averages
-                else None
-            )
-            expected_minimum = (
-                min(expected_minima[entity_id][i * 12 : (i + 1) * 12])
-                if entity_id in expected_minima
-                else None
-            )
-            expected_maximum = (
-                max(expected_maxima[entity_id][i * 12 : (i + 1) * 12])
-                if entity_id in expected_maxima
-                else None
-            )
-            expected_state = (
-                expected_states[entity_id][(i + 1) * 12 - 1]
-                if entity_id in expected_states
-                else None
-            )
-            expected_sum = (
-                expected_sums[entity_id][(i + 1) * 12 - 1]
-                if entity_id in expected_sums
-                else None
-            )
-            expected_stats[entity_id].append(
-                {
-                    "start": process_timestamp(start).timestamp(),
-                    "end": process_timestamp(end).timestamp(),
-                    "mean": pytest.approx(expected_average),
-                    "min": pytest.approx(expected_minimum),
-                    "max": pytest.approx(expected_maximum),
-                    "last_reset": None,
-                    "state": expected_state,
-                    "sum": expected_sum,
-                }
-            )
-        start += timedelta(days=1)
-        end += timedelta(days=1)
-    assert stats == expected_stats
+    assert start
+    verify_stats("day", start, lambda v: v + timedelta(days=1))
 
-    stats = statistics_during_period(hass, zero, period="month")
-    expected_stats = {
-        "sensor.test1": [],
-        "sensor.test2": [],
-        "sensor.test3": [],
-        "sensor.test4": [],
-    }
     start = dt_util.parse_datetime("2021-08-01T06:00:00+00:00")
-    end = dt_util.parse_datetime("2021-09-01T06:00:00+00:00")
-    for i in range(2):
-        for entity_id in (
-            "sensor.test1",
-            "sensor.test2",
-            "sensor.test3",
-            "sensor.test4",
-        ):
-            expected_average = (
-                mean(expected_averages[entity_id][i * 12 : (i + 1) * 12])
-                if entity_id in expected_averages
-                else None
-            )
-            expected_minimum = (
-                min(expected_minima[entity_id][i * 12 : (i + 1) * 12])
-                if entity_id in expected_minima
-                else None
-            )
-            expected_maximum = (
-                max(expected_maxima[entity_id][i * 12 : (i + 1) * 12])
-                if entity_id in expected_maxima
-                else None
-            )
-            expected_state = (
-                expected_states[entity_id][(i + 1) * 12 - 1]
-                if entity_id in expected_states
-                else None
-            )
-            expected_sum = (
-                expected_sums[entity_id][(i + 1) * 12 - 1]
-                if entity_id in expected_sums
-                else None
-            )
-            expected_stats[entity_id].append(
-                {
-                    "start": process_timestamp(start).timestamp(),
-                    "end": process_timestamp(end).timestamp(),
-                    "mean": pytest.approx(expected_average),
-                    "min": pytest.approx(expected_minimum),
-                    "max": pytest.approx(expected_maximum),
-                    "last_reset": None,
-                    "state": expected_state,
-                    "sum": expected_sum,
-                }
-            )
-        start = (start + timedelta(days=31)).replace(day=1)
-        end = (end + timedelta(days=31)).replace(day=1)
-    assert stats == expected_stats
+    assert start
+    verify_stats("month", start, lambda v: (v + timedelta(days=31)).replace(day=1))
 
     assert "Error while processing event StatisticsTask" not in caplog.text
 
