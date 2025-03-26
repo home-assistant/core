4df7869ef7c0485cf5f65ed0f93f031f9448500b
Joostlek <joostlek@outlook.com>
2025-03-26 12:07:27 +0100

    Fix

--- homeassistant/components/bosch_alarm/__init__.py
+++ homeassistant/components/bosch_alarm/__init__.py
@@ -57,5 +57,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: BoschAlarmConfigEntry) -
 
 async def async_unload_entry(hass: HomeAssistant, entry: BoschAlarmConfigEntry) -> bool:
     """Unload a config entry."""
-    await entry.runtime_data.disconnect()
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        await entry.runtime_data.disconnect()
+    return unload_ok
