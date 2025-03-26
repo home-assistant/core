1e5cd64c38c236a6dd607f81397052e9046921d9
Tsvi Mostovicz <ttmost@gmail.com>
2025-03-26 10:39:59 +0000

    Initial commit for creating a Jewish Calendar coordinator

--- homeassistant/components/jewish_calendar/entity.py
+++ homeassistant/components/jewish_calendar/entity.py
@@ -1,28 +1,12 @@
 """Entity representing a Jewish Calendar sensor."""
 
-from dataclasses import dataclass
+from hdate.translator import set_language
 
-from hdate import Location
-from hdate.translator import Language, set_language
-
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity import Entity, EntityDescription
 
 from .const import DOMAIN
-
-type JewishCalendarConfigEntry = ConfigEntry[JewishCalendarData]
-
-
-@dataclass
-class JewishCalendarData:
-    """Jewish Calendar runtime dataclass."""
-
-    language: Language
-    diaspora: bool
-    location: Location
-    candle_lighting_offset: int
-    havdalah_offset: int
+from .coordinator import JewishCalendarConfigEntry
 
 
 class JewishCalendarEntity(Entity):
@@ -42,9 +26,11 @@ class JewishCalendarEntity(Entity):
             entry_type=DeviceEntryType.SERVICE,
             identifiers={(DOMAIN, config_entry.entry_id)},
         )
-        data = config_entry.runtime_data
-        self._location = data.location
-        self._candle_lighting_offset = data.candle_lighting_offset
-        self._havdalah_offset = data.havdalah_offset
-        self._diaspora = data.diaspora
-        set_language(data.language)
+        self.coordinator = config_entry.runtime_data
+        config = self.coordinator.data
+        self.values = config.results
+        self._location = config.location
+        self._candle_lighting_offset = config.candle_lighting_offset
+        self._havdalah_offset = config.havdalah_offset
+        self._diaspora = config.diaspora
+        set_language(config.language)
