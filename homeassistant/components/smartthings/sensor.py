6eb35241df3f0f745bd86a389b7b02177bdef21c
Joostlek <joostlek@outlook.com>
2025-03-26 13:02:44 +0100

    Add select entity for dishwasher operating state in SmartThings

--- homeassistant/components/smartthings/sensor.py
+++ homeassistant/components/smartthings/sensor.py
@@ -1103,7 +1103,11 @@ class SmartThingsSensor(SmartThingsEntity, SensorEntity):
         await super().async_added_to_hass()
         if (
             self.capability
-            not in {Capability.DRYER_OPERATING_STATE, Capability.WASHER_OPERATING_STATE}
+            not in {
+                Capability.DISHWASHER_OPERATING_STATE,
+                Capability.DRYER_OPERATING_STATE,
+                Capability.WASHER_OPERATING_STATE,
+            }
             or self._attribute is not Attribute.MACHINE_STATE
         ):
             return
