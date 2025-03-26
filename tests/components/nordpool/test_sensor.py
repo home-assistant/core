10710776c6cbc51efddeb1a55ba7e0ccf3c6baa1
Mikko Koo <mikko@notsofool.com>
2025-03-26 08:50:50 +0000

    improve testing

--- tests/components/nordpool/test_sensor.py
+++ tests/components/nordpool/test_sensor.py
@@ -35,21 +35,21 @@ async def test_sensor(
 
 @pytest.mark.freeze_time("2024-11-05T18:00:00+00:00")
 @pytest.mark.usefixtures("entity_registry_enabled_by_default")
-async def test_sensor_current_price_is_0(
+async def test_sensor_current_and_averager_price_is_0(
     hass: HomeAssistant, load_int: ConfigEntry
 ) -> None:
-    """Test the Nord Pool sensor."""
+    """Test the Nord Pool sensor working if prices are 0."""
 
     current_price = hass.states.get("sensor.nord_pool_se4_current_price")
-    last_price = hass.states.get("sensor.nord_pool_se4_previous_price")
-    next_price = hass.states.get("sensor.nord_pool_se4_next_price")
+    average_price_se3 = hass.states.get("sensor.nord_pool_se3_daily_average")
+    average_price_se4 = hass.states.get("sensor.nord_pool_se4_daily_average")
 
     assert current_price is not None
-    assert last_price is not None
-    assert next_price is not None
     assert current_price.state == "0.0"  # SE4 2024-11-05T17:00:00Z
-    assert last_price.state == "0.0"  # SE4 2024-11-05T18:00:00Z
-    assert next_price.state == "0.0"  # SE4 2024-11-05T19:00:00Z"
+    assert average_price_se3 is not None
+    assert average_price_se3.state == "0.90074"
+    assert average_price_se4 is not None
+    assert average_price_se4.state == "0.0"
 
 
 @pytest.mark.freeze_time("2024-11-05T18:00:00+00:00")
