cb488b74f10d93897cb9b03fa8200d6d274e30d8
zerzhang <916250497@qq.com>
2025-03-27 19:22:45 +0800

    modify scripts as suggest

--- tests/components/switchbot/test_cover.py
+++ tests/components/switchbot/test_cover.py
@@ -1,8 +1,7 @@
 """Test the switchbot covers."""
 
+from collections.abc import Callable
 from unittest.mock import AsyncMock, patch
-
-import pytest
 
 from homeassistant.components.cover import (
     ATTR_CURRENT_POSITION,
@@ -12,12 +11,8 @@ from homeassistant.components.cover import (
     DOMAIN as COVER_DOMAIN,
     CoverState,
 )
-from homeassistant.components.switchbot.const import DOMAIN
 from homeassistant.const import (
     ATTR_ENTITY_ID,
-    CONF_ADDRESS,
-    CONF_NAME,
-    CONF_SENSOR_TYPE,
     SERVICE_CLOSE_COVER,
     SERVICE_CLOSE_COVER_TILT,
     SERVICE_OPEN_COVER,
@@ -28,7 +23,6 @@ from homeassistant.const import (
     SERVICE_STOP_COVER_TILT,
 )
 from homeassistant.core import HomeAssistant, State
-from homeassistant.setup import async_setup_component
 
 from . import WOBLINDTILT_SERVICE_INFO, WOCURTAIN3_SERVICE_INFO, make_advertisement
 
@@ -36,21 +30,13 @@ from tests.common import MockConfigEntry, mock_restore_cache
 from tests.components.bluetooth import inject_bluetooth_service_info
 
 
-@pytest.mark.usefixtures("entity_registry_enabled_by_default")
-async def test_curtain3_setup(hass: HomeAssistant) -> None:
+async def test_curtain3_setup(
+    hass: HomeAssistant, mock_entry_factory: Callable[[str], MockConfigEntry]
+) -> None:
     """Test setting up the Curtain3."""
-    await async_setup_component(hass, DOMAIN, {})
     inject_bluetooth_service_info(hass, WOCURTAIN3_SERVICE_INFO)
 
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            CONF_ADDRESS: "aa:bb:cc:dd:ee:ff",
-            CONF_NAME: "test-name",
-            CONF_SENSOR_TYPE: "curtain",
-        },
-        unique_id="aabbccddeeff",
-    )
+    entry = mock_entry_factory(sensor_type="curtain")
 
     entity_id = "cover.test_name"
     mock_restore_cache(
@@ -72,21 +58,13 @@ async def test_curtain3_setup(hass: HomeAssistant) -> None:
     assert hass.states.get(entity_id).attributes[ATTR_CURRENT_POSITION] == 50
 
 
-@pytest.mark.usefixtures("entity_registry_enabled_by_default")
-async def test_curtain3_controlling(hass: HomeAssistant) -> None:
+async def test_curtain3_controlling(
+    hass: HomeAssistant, mock_entry_factory: Callable[[str], MockConfigEntry]
+) -> None:
     """Test Curtain3 controlling."""
-    await async_setup_component(hass, DOMAIN, {})
     inject_bluetooth_service_info(hass, WOCURTAIN3_SERVICE_INFO)
 
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            CONF_ADDRESS: "aa:bb:cc:dd:ee:ff",
-            CONF_NAME: "test-name",
-            CONF_SENSOR_TYPE: "curtain",
-        },
-        unique_id="aabbccddeeff",
-    )
+    entry = mock_entry_factory(sensor_type="curtain")
     entry.add_to_hass(hass)
 
     with (
@@ -173,22 +151,13 @@ async def test_curtain3_controlling(hass: HomeAssistant) -> None:
         assert hass.states.get(entity_id).attributes[ATTR_CURRENT_POSITION] == 60
 
 
-@pytest.mark.usefixtures("entity_registry_enabled_by_default")
-async def test_blindtilt_setup(hass: HomeAssistant) -> None:
+async def test_blindtilt_setup(
+    hass: HomeAssistant, mock_entry_factory: Callable[[str], MockConfigEntry]
+) -> None:
     """Test setting up the blindtilt."""
-    await async_setup_component(hass, DOMAIN, {})
     inject_bluetooth_service_info(hass, WOBLINDTILT_SERVICE_INFO)
 
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            CONF_ADDRESS: "aa:bb:cc:dd:ee:ff",
-            CONF_NAME: "test-name",
-            CONF_SENSOR_TYPE: "blind_tilt",
-        },
-        unique_id="aabbccddeeff",
-    )
-
+    entry = mock_entry_factory(sensor_type="blind_tilt")
     entity_id = "cover.test_name"
     mock_restore_cache(
         hass,
@@ -210,27 +179,26 @@ async def test_blindtilt_setup(hass: HomeAssistant) -> None:
         assert hass.states.get(entity_id).attributes[ATTR_CURRENT_TILT_POSITION] == 40
 
 
-@pytest.mark.usefixtures("entity_registry_enabled_by_default")
-async def test_blindtilt_controlling(hass: HomeAssistant) -> None:
+async def test_blindtilt_controlling(
+    hass: HomeAssistant, mock_entry_factory: Callable[[str], MockConfigEntry]
+) -> None:
     """Test blindtilt controlling."""
-    await async_setup_component(hass, DOMAIN, {})
     inject_bluetooth_service_info(hass, WOBLINDTILT_SERVICE_INFO)
 
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        data={
-            CONF_ADDRESS: "aa:bb:cc:dd:ee:ff",
-            CONF_NAME: "test-name",
-            CONF_SENSOR_TYPE: "blind_tilt",
-        },
-        unique_id="aabbccddeeff",
-    )
+    entry = mock_entry_factory(sensor_type="blind_tilt")
     entry.add_to_hass(hass)
-
+    info = {
+        "motionDirection": {
+            "opening": False,
+            "closing": False,
+            "up": False,
+            "down": False,
+        },
+    }
     with (
         patch(
-            "switchbot.SwitchbotBlindTilt._get_basic_info",
-            new=AsyncMock(return_value=b"\xfbgA`\x98\xe8\x1d%2\x11\x84"),
+            "switchbot.SwitchbotBlindTilt.get_basic_info",
+            new=AsyncMock(return_value=info),
         ),
         patch(
             "switchbot.SwitchbotBlindTilt.open", new=AsyncMock(return_value=True)
@@ -262,8 +230,8 @@ async def test_blindtilt_controlling(hass: HomeAssistant) -> None:
             blocking=True,
         )
         with patch(
-            "switchbot.SwitchbotBlindTilt._get_basic_info",
-            return_value=manufacturer_data,
+            "switchbot.SwitchbotBlindTilt.get_basic_info",
+            return_value=info,
         ):
             inject_bluetooth_service_info(
                 hass, make_advertisement(address, manufacturer_data, service_data)
@@ -271,6 +239,7 @@ async def test_blindtilt_controlling(hass: HomeAssistant) -> None:
             await hass.async_block_till_done()
 
             mock_open.assert_awaited_once()
+
             assert hass.states.get(entity_id).state == CoverState.OPEN
             assert (
                 hass.states.get(entity_id).attributes[ATTR_CURRENT_TILT_POSITION] == 70
@@ -285,8 +254,8 @@ async def test_blindtilt_controlling(hass: HomeAssistant) -> None:
             blocking=True,
         )
         with patch(
-            "switchbot.SwitchbotBlindTilt._get_basic_info",
-            return_value=manufacturer_data,
+            "switchbot.SwitchbotBlindTilt.get_basic_info",
+            return_value=info,
         ):
             inject_bluetooth_service_info(
                 hass, make_advertisement(address, manufacturer_data, service_data)
@@ -308,8 +277,8 @@ async def test_blindtilt_controlling(hass: HomeAssistant) -> None:
             blocking=True,
         )
         with patch(
-            "switchbot.SwitchbotBlindTilt._get_basic_info",
-            return_value=manufacturer_data,
+            "switchbot.SwitchbotBlindTilt.get_basic_info",
+            return_value=info,
         ):
             inject_bluetooth_service_info(
                 hass, make_advertisement(address, manufacturer_data, service_data)
@@ -331,8 +300,8 @@ async def test_blindtilt_controlling(hass: HomeAssistant) -> None:
             blocking=True,
         )
         with patch(
-            "switchbot.SwitchbotBlindTilt._get_basic_info",
-            return_value=manufacturer_data,
+            "switchbot.SwitchbotBlindTilt.get_basic_info",
+            return_value=info,
         ):
             inject_bluetooth_service_info(
                 hass, make_advertisement(address, manufacturer_data, service_data)
