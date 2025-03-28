859f2b8f8912987334e6922fba53c37bbc0d933a
acidcoke <acidcoke@acidic.codes>
2025-03-28 21:41:07 +0100

    Implement HassMediaPrevious handler

--- homeassistant/components/media_player/intent.py
+++ homeassistant/components/media_player/intent.py
@@ -59,20 +59,7 @@ async def async_setup_intents(hass: HomeAssistant) -> None:
     intent.async_register(hass, MediaUnpauseHandler(last_paused))
     intent.async_register(hass, MediaPauseHandler(last_paused))
     intent.async_register(hass, MediaNextHandler())
-    intent.async_register(
-        hass,
-        intent.ServiceIntentHandler(
-            INTENT_MEDIA_PREVIOUS,
-            DOMAIN,
-            SERVICE_MEDIA_PREVIOUS_TRACK,
-            required_domains={DOMAIN},
-            required_features=MediaPlayerEntityFeature.PREVIOUS_TRACK,
-            required_states={MediaPlayerState.PLAYING, MediaPlayerState.PAUSED},
-            description="Replays the previous item for a media player",
-            platforms={DOMAIN},
-            device_classes={MediaPlayerDeviceClass},
-        ),
-    )
+    intent.async_register(hass, MediaPreviousHandler())
     intent.async_register(
         hass,
         intent.ServiceIntentHandler(
@@ -223,6 +210,60 @@ class MediaNextHandler(intent.ServiceIntentHandler):
         match_preferences: intent.MatchTargetsPreferences | None = None,
     ) -> intent.IntentResponse:
         """Play next media item."""
+        if match_result.is_match and (not match_constraints.name):
+            if len(match_result.states) == 1:
+                return await super().async_handle_states(
+                    intent_obj, match_result, match_constraints
+                )
+            if len(match_result.states) > 1:
+                playing_players = [
+                    state
+                    for state in match_result.states
+                    if state.state == MediaPlayerState.PLAYING
+                ]
+                if len(playing_players) == 1:
+                    match_result.states = playing_players
+                    return await super().async_handle_states(
+                        intent_obj, match_result, match_constraints
+                    )
+
+                resp = intent.IntentResponse(intent_obj.language, intent_obj)
+                resp.async_set_error(
+                    intent.IntentResponseErrorCode.NO_VALID_TARGETS,
+                    "Too many possible targets",
+                )
+                return resp
+
+        return await super().async_handle_states(
+            intent_obj, match_result, match_constraints
+        )
+
+
+class MediaPreviousHandler(intent.ServiceIntentHandler):
+    """Handler for media previous intent."""
+
+    def __init__(self) -> None:
+        """Initialize handler."""
+        super().__init__(
+            INTENT_MEDIA_PREVIOUS,
+            DOMAIN,
+            SERVICE_MEDIA_PREVIOUS_TRACK,
+            required_domains={DOMAIN},
+            required_features=MediaPlayerEntityFeature.PREVIOUS_TRACK,
+            required_states={MediaPlayerState.PLAYING, MediaPlayerState.PAUSED},
+            description="Replays the previous item for a media player",
+            platforms={DOMAIN},
+            device_classes={MediaPlayerDeviceClass},
+        )
+
+    async def async_handle_states(
+        self,
+        intent_obj: intent.Intent,
+        match_result: intent.MatchTargetsResult,
+        match_constraints: intent.MatchTargetsConstraints,
+        match_preferences: intent.MatchTargetsPreferences | None = None,
+    ) -> intent.IntentResponse:
+        """Replay previous media item."""
         if match_result.is_match and (not match_constraints.name):
             if len(match_result.states) == 1:
                 return await super().async_handle_states(
