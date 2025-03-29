c7af712ec9b50926276d6724e84b3a514521bbb6
J. Nick Koston <nick@koston.org>
2025-03-28 22:43:33 -1000

    make sure all callbacks fire

--- homeassistant/components/esphome/entry_data.py
+++ homeassistant/components/esphome/entry_data.py
@@ -319,12 +319,12 @@ class RuntimeEntryData:
                 infos_by_type[info_type] = []
             infos_by_type[info_type].append(info)
 
-        for type_, callbacks_ in self.entity_info_callbacks.items():
+        for type_, callbacks in self.entity_info_callbacks.items():
             # If all entities for a type are removed, we
             # still need to call the callbacks with an empty list
             # to make sure the entities are removed.
             entity_infos = infos_by_type.get(type_, [])
-            for callback_ in callbacks_:
+            for callback_ in callbacks:
                 callback_(entity_infos)
 
         # Finally update static info subscriptions
