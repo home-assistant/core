d5c089d2a1449ce6245201338870dc6672e5860f
Marko Todoric <maretodoric@gmail.com>
2025-03-29 01:09:45 +0100

    Use `setup_integration` from `__init__.py` to avoid code duplication.

--- tests/components/backup_sftp/test_init.py
+++ tests/components/backup_sftp/test_init.py
@@ -1,49 +1,29 @@
 """Tests for SFTP Backup Location."""
 
-from collections.abc import Awaitable, Callable, Coroutine
-from typing import Any
 from unittest.mock import AsyncMock, MagicMock, patch
-
-import pytest
 
 from homeassistant.components.backup_sftp import SFTPConfigEntryData
 from homeassistant.components.backup_sftp.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
+from . import setup_integration
 from .conftest import USER_INPUT
 
 from tests.common import MockConfigEntry
-
-type ComponentSetup = Callable[[], Awaitable[None]]
-
-
-@pytest.fixture(name="setup_integration")
-async def mock_setup_integration(
-    hass: HomeAssistant,
-    config_entry: MockConfigEntry,
-) -> Callable[[], Coroutine[Any, Any, None]]:
-    """Fixture for setting up the component."""
-    config_entry.add_to_hass(hass)
-
-    async def func() -> None:
-        await hass.config_entries.async_setup(config_entry.entry_id)
-        await hass.async_block_till_done()
-
-    return func
 
 
 @patch("homeassistant.components.backup_sftp.BackupAgentClient")
 async def test_setup_success(
     backup_agent_client: MagicMock,
     hass: HomeAssistant,
-    setup_integration: ComponentSetup,
+    config_entry: MockConfigEntry,
     async_cm_mock: AsyncMock,
 ) -> None:
     """Test successful setup and unload."""
     backup_agent_client.return_value = async_cm_mock
     async_cm_mock.list_backup_location.return_value = []
-    await setup_integration()
+    await setup_integration(hass, config_entry)
 
     entries = hass.config_entries.async_entries(DOMAIN)
     assert len(entries) == 1
@@ -59,14 +39,14 @@ async def test_setup_success(
 async def test_setup_error(
     backup_agent_client: MagicMock,
     hass: HomeAssistant,
-    setup_integration: ComponentSetup,
+    config_entry: MockConfigEntry,
     async_cm_mock: AsyncMock,
 ) -> None:
     """Test setup error."""
     backup_agent_client.return_value = async_cm_mock
     async_cm_mock.list_backup_location.return_value = None
 
-    await setup_integration()
+    await setup_integration(hass, config_entry)
 
     entries = hass.config_entries.async_entries(DOMAIN)
     assert len(entries) == 1
@@ -77,11 +57,11 @@ async def test_setup_error(
 async def test_setup_error_reauth(
     mock_connect: AsyncMock,
     hass: HomeAssistant,
-    setup_integration: ComponentSetup,
+    config_entry: MockConfigEntry,
 ) -> None:
     """Test reauth trigger on `BackupAgentClient` failure."""
     mock_connect.side_effect = OSError("Error message")
-    await setup_integration()
+    await setup_integration(hass, config_entry)
 
     entries = hass.config_entries.async_entries(DOMAIN)
     assert len(entries) == 1
