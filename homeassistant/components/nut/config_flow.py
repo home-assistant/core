dfbcaedcceb3dd02ffc2724a88ce56ea024170cd
tdfountain <174762217+tdfountain@users.noreply.github.com>
2025-03-30 08:27:25 -0700

    Update homeassistant/components/nut/config_flow.py
    Set unique ID and abort only if value is defined
    
    Co-authored-by: J. Nick Koston <nick+github@koston.org>

--- homeassistant/components/nut/config_flow.py
+++ homeassistant/components/nut/config_flow.py
@@ -126,9 +126,9 @@ class NutConfigFlow(ConfigFlow, domain=DOMAIN):
                 if self._host_port_alias_already_configured(nut_config):
                     return self.async_abort(reason="already_configured")
 
-                unique_id = _unique_id_from_status(info["available_resources"])
-                await self.async_set_unique_id(unique_id)
-                self._abort_if_unique_id_configured()
+                if unique_id := _unique_id_from_status(info["available_resources"]):
+                    await self.async_set_unique_id(unique_id)
+                    self._abort_if_unique_id_configured()
 
                 title = _format_host_port_alias(nut_config)
                 return self.async_create_entry(title=title, data=nut_config)
