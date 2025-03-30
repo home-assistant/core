79520cbbdd94ac65d6a5d3258a9a71e5ddb2f1a4
tdfountain <174762217+tdfountain@users.noreply.github.com>
2025-03-30 11:00:34 -0500

    Add duplicate ID test case for multiple devices

--- tests/components/nut/test_config_flow.py
+++ tests/components/nut/test_config_flow.py
@@ -562,6 +562,71 @@ async def test_abort_duplicate_unique_ids(hass: HomeAssistant) -> None:
         assert result2["reason"] == "already_configured"
 
 
+async def test_abort_multiple_ups_duplicate_unique_ids(hass: HomeAssistant) -> None:
+    """Test we abort on multiple devices if unique_id is already setup."""
+
+    list_vars = {
+        "device.mfr": "Some manufacturer",
+        "device.model": "Some model",
+        "device.serial": "0000-1",
+    }
+
+    mock_pynut = _get_mock_nutclient(
+        list_ups={"ups2": "UPS 2", "ups3": "UPS 3"}, list_vars=list_vars
+    )
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {}
+
+    with patch(
+        "homeassistant.components.nut.AIONUTClient",
+        return_value=mock_pynut,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_HOST: "1.1.1.1",
+                CONF_PORT: 2222,
+            },
+        )
+        await hass.async_block_till_done()
+
+        assert result2["step_id"] == "ups"
+        assert result2["type"] is FlowResultType.FORM
+
+    await async_init_integration(
+        hass,
+        list_ups={"ups1": "UPS 1"},
+        list_vars=list_vars,
+    )
+
+    with (
+        patch(
+            "homeassistant.components.nut.AIONUTClient",
+            return_value=mock_pynut,
+        ),
+        patch(
+            "homeassistant.components.nut.async_setup_entry",
+            return_value=True,
+        ),
+    ):
+        result3 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_ALIAS: "ups2"},
+        )
+        await hass.async_block_till_done()
+
+        assert result3["type"] is FlowResultType.ABORT
+        assert result3["reason"] == "already_configured"
+
+
 async def test_abort_if_already_setup_alias(hass: HomeAssistant) -> None:
     """Test we abort if component is already setup with same alias."""
     config_entry = MockConfigEntry(
