5e97d6e8a4c44ec76b978929f49f064a43efb438
innovodrivers <dev@innovo.net>
2025-03-31 15:33:35 +0000

    lea_amp

--- homeassistant/components/lea_amp/zone.py
+++ homeassistant/components/lea_amp/zone.py
@@ -96,13 +96,14 @@ class LeaZone:
         await self._controller.turn_on_off(self._zone_id, str(power))
         self._power = power
 
-    async def set_zone_volume(self, volume: int, HAVolume: int) -> None:
+    async def set_zone_volume(self, volume: int, HAVolume: float) -> None:
         """Set Zone Volume."""
         _LOGGER.log(logging.INFO, "set_zone_volume value to send: %s", str(volume))
         await self._controller.set_volume(self._zone_id, volume)
-        HAVolume = int(HAVolume / 100)
         _LOGGER.log(logging.INFO, "HA value:  %s", str(HAVolume))
-        self._volume = float(HAVolume)
+        HAVolume = HAVolume / 100
+        _LOGGER.log(logging.INFO, "HA value:  %s", str(HAVolume))
+        self._volume = HAVolume
 
     async def set_zone_mute(self, mute: bool) -> None:
         """Set Zone Mute."""
