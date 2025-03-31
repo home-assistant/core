e8e114592fc202e4496f6e5b57a98a11a9be4a62
zerzhang <916250497@qq.com>
2025-03-31 15:19:03 +0800

    Add roller shade and hubmini matter support

--- tests/components/switchbot/__init__.py
+++ tests/components/switchbot/__init__.py
@@ -386,3 +386,27 @@ def make_advertisement(
         connectable=True,
         tx_power=-127,
     )
+
+HUBMINI_MATTER_SERVICE_INFO = BluetoothServiceInfoBleak(
+    name="HubMini Matter",
+    manufacturer_data={
+        2409: b"\xe6\xa1\xcd\x1f[e\x00\x00\x00\x00\x00\x00\x14\x01\x985\x00",
+    },
+    service_data={"0000fd3d-0000-1000-8000-00805f9b34fb": b"%\x00"},
+    service_uuids=["cba20d00-224d-11e6-9fb8-0002a5d5c51b"],
+    address="AA:BB:CC:DD:EE:FF",
+    rssi=-60,
+    source="local",
+    advertisement=generate_advertisement_data(
+        local_name="HubMini Matter",
+        manufacturer_data={
+            2409: b"\xe6\xa1\xcd\x1f[e\x00\x00\x00\x00\x00\x00\x14\x01\x985\x00",
+        },
+        service_data={"0000fd3d-0000-1000-8000-00805f9b34fb": b"v\x00"},
+        service_uuids=["cba20d00-224d-11e6-9fb8-0002a5d5c51b"],
+    ),
+    device=generate_ble_device("AA:BB:CC:DD:EE:FF", "HubMini Matter"),
+    time=0,
+    connectable=True,
+    tx_power=-127,
+)
