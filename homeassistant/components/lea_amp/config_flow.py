9791ade75c9ff1d19ec334324eff896e428fe759
innovodrivers <dev@innovo.net>
2025-04-01 08:00:45 +0000

    lea_amp

--- homeassistant/components/lea_amp/config_flow.py
+++ homeassistant/components/lea_amp/config_flow.py
@@ -2,18 +2,14 @@
 
 from __future__ import annotations
 
-import asyncio
-from contextlib import suppress
 import logging
 
 import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigFlowResult
-from homeassistant.core import HomeAssistant
 
-from .const import CONNECTION_TIMEOUT, DOMAIN, PORT
-from .controller import LeaController
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -40,45 +36,3 @@ class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 }
             ),
         )
-
-
-async def _async_has_devices(hass: HomeAssistant) -> bool:
-    """Return if there are devices that can be discovered."""
-
-    ip_address = hass.data["ip_address"]
-    hass.data[DOMAIN] = {"ip": ip_address}
-
-    _LOGGER.log(logging.INFO, "async_setup_entry")
-    _LOGGER.log(logging.INFO, "ip_address: %s", str(ip_address))
-
-    controller: LeaController = LeaController(
-        loop=hass.loop,
-        port=PORT,
-        ip_address=ip_address,
-        discovery_enabled=True,
-        discovery_interval=1,
-        update_enabled=False,
-    )
-
-    try:
-        await controller.start()
-    except OSError as ex:
-        _LOGGER.error("Start failed, errno: %d", ex.errno)
-        return False
-
-    try:
-        async with asyncio.timeout(delay=CONNECTION_TIMEOUT):
-            while not controller.zones:
-                await asyncio.sleep(delay=1)
-    except TimeoutError:
-        _LOGGER.debug("HELLO No devices found")
-
-    devices_count = len(controller.zones)
-    cleanup_complete: asyncio.Event = controller.cleanup()
-    with suppress(TimeoutError):
-        await asyncio.wait_for(cleanup_complete.wait(), 1)
-
-    return devices_count > 0
-
-
-# config_entry_flow.register_discovery_flow(DOMAIN, "LEA AMP local", _async_has_devices)
