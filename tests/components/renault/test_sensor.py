da9b3dc68b004f80d0acba76cd6b17e6bc4336a8
tmenguy <thomas.menguy@gmail.com>
2025-04-01 17:14:21 +0200

    Better throttling handling for the Renault API (#141667)
    * Added some better throttling handling for the Renault API, it fixes #106777 HA ticket
    
    * Added some better throttling handling for the Renault API, it fixes #106777 HA ticket, test fixing
    
    * Update homeassistant/components/renault/coordinator.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update homeassistant/components/renault/coordinator.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update homeassistant/components/renault/coordinator.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update homeassistant/components/renault/coordinator.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update homeassistant/components/renault/coordinator.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update homeassistant/components/renault/coordinator.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update homeassistant/components/renault/coordinator.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update homeassistant/components/renault/renault_hub.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * bigger testsuite for  #106777 HA ticket
    
    * bigger testsuite for  #106777 HA ticket
    
    * bigger testsuite for  #106777 HA ticket
    
    * bigger testsuite for  #106777 HA ticket
    
    * Adjust tests
    
    * Update homeassistant/components/renault/coordinator.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update homeassistant/components/renault/renault_hub.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update homeassistant/components/renault/renault_hub.py
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    * Update tests/components/renault/test_sensor.py
    
    * Update tests/components/renault/test_sensor.py
    
    * Update tests/components/renault/test_sensor.py
    
    * requested changes  #106777 HA ticket
    
    * Use unkown
    
    * Fix test
    
    * Fix test again
    
    * Reduce and fix
    
    * Use assumed_state
    
    * requested changes  #106777 HA ticket
    
    ---------
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>

--- tests/components/renault/test_sensor.py
+++ tests/components/renault/test_sensor.py
@@ -10,7 +10,7 @@ from renault_api.kamereon.exceptions import QuotaLimitException
 from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import STATE_UNAVAILABLE, Platform
+from homeassistant.const import ATTR_ASSUMED_STATE, STATE_UNAVAILABLE, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 
@@ -184,7 +184,7 @@ async def test_sensor_throttling_during_setup(
     for get_data_mock in patches.values():
         get_data_mock.side_effect = None
     patches["battery_status"].return_value.batteryLevel = 55
-    freezer.tick(datetime.timedelta(minutes=10))
+    freezer.tick(datetime.timedelta(minutes=20))
     async_fire_time_changed(hass)
     await hass.async_block_till_done()
 
@@ -196,6 +196,7 @@ async def test_sensor_throttling_after_init(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
     vehicle_type: str,
+    caplog: pytest.LogCaptureFixture,
     freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test for Renault sensors with a throttling error during setup."""
@@ -209,8 +210,11 @@ async def test_sensor_throttling_after_init(
     # Initial state
     entity_id = "sensor.reg_number_battery"
     assert hass.states.get(entity_id).state == "60"
+    assert not hass.states.get(entity_id).attributes.get(ATTR_ASSUMED_STATE)
+    assert "Renault API throttled: scan skipped" not in caplog.text
 
     # Test QuotaLimitException state
+    caplog.clear()
     for get_data_mock in patches.values():
         get_data_mock.side_effect = QuotaLimitException(
             "err.func.wired.overloaded", "You have reached your quota limit"
@@ -219,14 +223,21 @@ async def test_sensor_throttling_after_init(
     async_fire_time_changed(hass)
     await hass.async_block_till_done()
 
-    assert hass.states.get(entity_id).state == STATE_UNAVAILABLE
+    assert hass.states.get(entity_id).state == "60"
+    assert hass.states.get(entity_id).attributes.get(ATTR_ASSUMED_STATE)
+    assert "Renault API throttled" in caplog.text
+    assert "Renault hub currently throttled: scan skipped" in caplog.text
 
     # Test QuotaLimitException recovery, with new battery level
+    caplog.clear()
     for get_data_mock in patches.values():
         get_data_mock.side_effect = None
     patches["battery_status"].return_value.batteryLevel = 55
-    freezer.tick(datetime.timedelta(minutes=10))
+    freezer.tick(datetime.timedelta(minutes=20))
     async_fire_time_changed(hass)
     await hass.async_block_till_done()
 
     assert hass.states.get(entity_id).state == "55"
+    assert not hass.states.get(entity_id).attributes.get(ATTR_ASSUMED_STATE)
+    assert "Renault API throttled" not in caplog.text
+    assert "Renault hub currently throttled: scan skipped" not in caplog.text
