157b207d7a6f7b3ec3dd932d317ccdd6a95890bc
wangzhengyu <wangzhengyu@foscam.com>
2025-04-02 14:00:02 +0800

    Add more Foscam switches

--- homeassistant/components/foscam/coordinator.py
+++ homeassistant/components/foscam/coordinator.py
@@ -4,13 +4,12 @@ import asyncio
 from datetime import timedelta
 from typing import Any
 
-from libpyfoscam import FoscamCamera
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from .const import DOMAIN, LOGGER
+from .foscamcgi import FoscamCamera
 
 type FoscamConfigEntry = ConfigEntry[FoscamCoordinator]
 
@@ -34,24 +33,52 @@ class FoscamCoordinator(DataUpdateCoordinator[dict[str, Any]]):
         )
         self.session = session
 
+    def gather_all_configs(self):
+        """Get all Foscam configurations."""
+        configs = {}
+
+        ret, dev_info = self.session.get_dev_info()
+        configs["dev_info"] = dev_info
+
+        ret, all_info = self.session.get_product_all_info()
+        configs["product_info"] = all_info
+
+        ret, infra_led_config = self.session.get_infra_led_config()
+        configs["is_openIr"] = infra_led_config["mode"]
+
+        ret, mirror_flip_setting = self.session.get_mirror_and_flip_setting()
+        configs["is_Flip"] = mirror_flip_setting["isFlip"]
+        configs["is_Mirror"] = mirror_flip_setting["isMirror"]
+
+        ret, sleep_setting = self.session.is_asleep()
+        configs["is_asleep"] = {"supported": ret == 0, "status": sleep_setting}
+
+        ret, is_openWhiteLight = self.session.getWhiteLightBrightness()
+        configs["is_openWhiteLight"] = is_openWhiteLight["enable"]
+
+        ret, is_sirenalarm = self.session.getSirenConfig()
+        configs["is_sirenalarm"] = is_sirenalarm["sirenEnable"]
+
+        ret, Volume = self.session.getAudioVolume()
+        configs["Volume"] = Volume["volume"]
+
+        ret, SpeakVolume = self.session.getSpeakVolume()
+        configs["SpeakVolume"] = SpeakVolume["SpeakVolume"]
+
+        ret, is_TurnOffVolume = self.session.getVoiceEnableState()
+        configs["is_TurnOffVolume"] = 0 if int(is_TurnOffVolume["isEnable"]) == 1 else 1
+
+        ret, is_TurnOffLight = self.session.getLedEnableState()
+        configs["is_TurnOffLight"] = 0 if int(is_TurnOffLight["isEnable"]) == 1 else 1
+        if ((1 << 8) & int(all_info["reserve3"])) != 0:
+            ret, is_OpenWdr = self.session.getWdrMode()
+            configs["is_OpenWdr"] = is_OpenWdr["mode"]
+        else:
+            ret, is_OpenHdr = self.session.getHdrMode()
+            configs["is_OpenHdr"] = is_OpenHdr["mode"]
+        return configs
+
     async def _async_update_data(self) -> dict[str, Any]:
         """Fetch data from API endpoint."""
-
         async with asyncio.timeout(30):
-            data = {}
-            ret, dev_info = await self.hass.async_add_executor_job(
-                self.session.get_dev_info
-            )
-            if ret == 0:
-                data["dev_info"] = dev_info
-
-            all_info = await self.hass.async_add_executor_job(
-                self.session.get_product_all_info
-            )
-            data["product_info"] = all_info[1]
-
-            ret, is_asleep = await self.hass.async_add_executor_job(
-                self.session.is_asleep
-            )
-            data["is_asleep"] = {"supported": ret == 0, "status": is_asleep}
-            return data
+            return await self.hass.async_add_executor_job(self.gather_all_configs)
