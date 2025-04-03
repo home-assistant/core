c11dd02416ea77fa3aea7b0b4890d9dae31cd6c8
Sanjay Govind <sanjay.govind9@gmail.com>
2025-04-03 10:30:25 +0000

    make one device per sensor, remove device class guessing

--- homeassistant/components/bosch_alarm/binary_sensor.py
+++ homeassistant/components/bosch_alarm/binary_sensor.py
@@ -2,14 +2,9 @@
 
 from __future__ import annotations
 
-import re
-
 from bosch_alarm_mode2 import Panel
 
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
-)
+from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
@@ -32,22 +27,6 @@ async def async_setup_entry(
     )
 
 
-def _guess_device_class(name: str) -> BinarySensorDeviceClass | None:
-    if re.search(r"\b(win(d)?(ow)?|wn)\b", name):
-        return BinarySensorDeviceClass.WINDOW
-    if re.search(r"\b(door|dr)\b", name):
-        return BinarySensorDeviceClass.DOOR
-    if re.search(r"\b(motion|md)\b", name):
-        return BinarySensorDeviceClass.MOTION
-    if re.search(r"\bco\b", name):
-        return BinarySensorDeviceClass.CO
-    if re.search(r"\bsmoke\b", name):
-        return BinarySensorDeviceClass.SMOKE
-    if re.search(r"\bglassbr(ea)?k\b", name):
-        return BinarySensorDeviceClass.TAMPER
-    return None
-
-
 PARALLEL_UPDATES = 0
 
 
@@ -55,19 +34,23 @@ class PointSensor(BinarySensorEntity):
     """A binary sensor entity for a point in a bosch alarm panel."""
 
     _attr_has_entity_name = True
+    _attr_name = None
 
     def __init__(self, panel: Panel, point_id: int, unique_id: str) -> None:
         """Set up a binary sensor entity for a point in a bosch alarm panel."""
         self.panel = panel
         self._attr_unique_id = f"{unique_id}_point_{point_id}"
         self._point = panel.points[point_id]
-        self._attr_name = self._point.name
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, unique_id)},
-            name=f"Bosch {panel.model}",
+            identifiers={(DOMAIN, self._attr_unique_id)},
             manufacturer="Bosch Security Systems",
+            name=self._point.name,
             model=panel.model,
             sw_version=panel.firmware_version,
+            via_device=(
+                DOMAIN,
+                unique_id,
+            ),
         )
 
     @property
@@ -79,11 +62,6 @@ class PointSensor(BinarySensorEntity):
     def available(self) -> bool:
         """Return if this point sensor is available."""
         return super().available and (self._point.is_open() or self._point.is_normal())
-
-    @property
-    def device_class(self) -> BinarySensorDeviceClass | None:
-        """Return a guess for the device class for this point sensor."""
-        return _guess_device_class(self._point.name.lower())
 
     async def async_added_to_hass(self) -> None:
         """Run when entity attached to hass."""
