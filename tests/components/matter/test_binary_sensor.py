5218ba72e4d484fc41914d2c643b9fcda82ecb36
Ludovic BOUE <lboue@users.noreply.github.com>
2025-04-04 16:26:21 +0200

    Update test binary

--- tests/components/matter/test_binary_sensor.py
+++ tests/components/matter/test_binary_sensor.py
@@ -156,6 +156,7 @@ async def test_evse_sensor(
     matter_node: MatterNode,
 ) -> None:
     """Test evse sensors."""
+    # Test StateEnum value with binary_sensor.evse_charging_status
     entity_id = "binary_sensor.evse_charging_status"
     state = hass.states.get(entity_id)
     assert state
@@ -169,8 +170,9 @@ async def test_evse_sensor(
     assert state
     assert state.state == "off"
 
-    # binary_sensor.evse_plug
-    state = hass.states.get("binary_sensor.evse_plug")
+    # Test StateEnum value with binary_sensor.evse_plug
+    entity_id = "binary_sensor.evse_plug"
+    state = hass.states.get(entity_id)
     assert state
     assert state.state == "on"
     # switch to NotPluggedIn state
@@ -182,12 +184,16 @@ async def test_evse_sensor(
     assert state
     assert state.state == "off"
 
-    # binary_sensor.evse_supply_charging_state
-    state = hass.states.get("binary_sensor.evse_supply_charging_state")
+    # Test SupplyStateEnum value with binary_sensor.evse_supply_charging
+    entity_id = "binary_sensor.evse_supply_charging_state"
+    state = hass.states.get(entity_id)
     assert state
     assert state.state == "on"
     # switch to Disabled state
-    set_node_attribute(matter_node, 1, 153, 0, 0)
+    set_node_attribute(matter_node, 1, 153, 1, 0)
     await trigger_subscription_callback(
         hass, matter_client, data=(matter_node.node_id, "1/153/1", 0)
     )
+    state = hass.states.get(entity_id)
+    assert state
+    assert state.state == "off"
