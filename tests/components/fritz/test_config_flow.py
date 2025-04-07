724bef57d5adf3a5d9074a23d378712a0fc61852
Simone Chemelli <simone.chemelli@gmail.com>
2025-04-07 18:45:11 +0000

    update test

--- tests/components/fritz/test_config_flow.py
+++ tests/components/fritz/test_config_flow.py
@@ -772,11 +772,12 @@ async def test_upnp_not_enabled(hass: HomeAssistant) -> None:
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
-        context={"source": SOURCE_USER, "show_advanced_options": False},
+        context={"source": SOURCE_USER},
     )
     assert result["type"] is FlowResultType.FORM
     assert result["step_id"] == "user"
 
+    # Disable UPnP
     services = deepcopy(MOCK_FB_SERVICES)
     services["X_AVM-DE_UPnP1"]["GetInfo"]["NewEnable"] = False
 
@@ -791,3 +792,27 @@ async def test_upnp_not_enabled(hass: HomeAssistant) -> None:
         assert result["type"] is FlowResultType.FORM
         assert result["step_id"] == "user"
         assert result["errors"]["base"] == ERROR_UPNP_NOT_CONFIGURED
+
+    # Enable UPnP
+    services["X_AVM-DE_UPnP1"]["GetInfo"]["NewEnable"] = True
+
+    with (
+        patch(
+            "homeassistant.components.fritz.config_flow.FritzConnection",
+            return_value=FritzConnectionMock(services),
+        ),
+        patch(
+            "homeassistant.components.fritz.config_flow.socket.gethostbyname",
+            return_value=MOCK_IPS["fritz.box"],
+        ),
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input=MOCK_USER_INPUT_SIMPLE
+        )
+
+        assert result["type"] is FlowResultType.CREATE_ENTRY
+        assert result["data"][CONF_HOST] == "fake_host"
+        assert result["data"][CONF_PASSWORD] == "fake_pass"
+        assert result["data"][CONF_USERNAME] == "fake_user"
+        assert result["data"][CONF_PORT] == 49000
+        assert result["data"][CONF_SSL] is False
