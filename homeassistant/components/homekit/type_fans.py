de2fa4004dca306477a2a562cffba26aecc8b439
Maarten Staa <maarten@staa.dev>
2025-04-07 12:40:56 +0200

    Remove no longer needed if statement in fan init

--- homeassistant/components/homekit/type_fans.py
+++ homeassistant/components/homekit/type_fans.py
@@ -70,10 +70,9 @@ class Fan(HomeAccessory):
             )
         )
 
-        if state is not None:
-            features = state.attributes.get(ATTR_SUPPORTED_FEATURES, 0)
-            percentage_step = state.attributes.get(ATTR_PERCENTAGE_STEP, 1)
-            self.preset_modes = state.attributes.get(ATTR_PRESET_MODES)
+        features = state.attributes.get(ATTR_SUPPORTED_FEATURES, 0)
+        percentage_step = state.attributes.get(ATTR_PERCENTAGE_STEP, 1)
+        self.preset_modes = state.attributes.get(ATTR_PRESET_MODES)
 
         if features & FanEntityFeature.DIRECTION:
             self.chars.append(CHAR_ROTATION_DIRECTION)
