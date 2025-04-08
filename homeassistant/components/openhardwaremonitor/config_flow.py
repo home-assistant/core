459b92f298410ead022da8c8caecfdd07c25e54c
Peter Åslund <peter@peteraslund.me>
2025-04-08 15:57:23 +0200

    Reconfigure config flow

--- homeassistant/components/openhardwaremonitor/config_flow.py
+++ homeassistant/components/openhardwaremonitor/config_flow.py
@@ -6,7 +6,10 @@ from typing import Any
 
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import (
+    ConfigFlow,
+    ConfigFlowResult,
+)
 from homeassistant.const import CONF_HOST, CONF_PORT
 import homeassistant.helpers.config_validation as cv
 
@@ -41,6 +44,32 @@ class OpenHardwareMonitorConfigFlow(ConfigFlow, domain=DOMAIN):
         return self.async_create_entry(
             title=f"{host}:{port}",
             data=user_input,
+        )
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle reconfiguration."""
+        config_entry = self._get_reconfigure_entry()
+        data = dict(config_entry.data)
+        current_device_grouping = data.get(GROUP_DEVICES_PER_DEPTH_LEVEL, 0)
+        data_schema = vol.Schema(
+            {
+                vol.Optional(
+                    GROUP_DEVICES_PER_DEPTH_LEVEL, default=current_device_grouping
+                ): int,
+            }
+        )
+        if user_input is None:
+            return self.async_show_form(data_schema=data_schema)
+
+        return self.async_update_reload_and_abort(
+            config_entry,
+            data_updates={
+                GROUP_DEVICES_PER_DEPTH_LEVEL: user_input.get(
+                    GROUP_DEVICES_PER_DEPTH_LEVEL, current_device_grouping
+                )
+            },
         )
 
     async def async_step_import(self, import_data: dict[str, Any]) -> ConfigFlowResult:
