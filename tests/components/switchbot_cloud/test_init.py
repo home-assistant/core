5549b90a11b299d711f75212e2a3f153e5c9992e
Gigatrappeur <gigatrappeur@gmail.com>
2025-04-08 20:30:03 +0000

    Add unit tests

--- tests/components/switchbot_cloud/test_init.py
+++ tests/components/switchbot_cloud/test_init.py
@@ -2,15 +2,20 @@
 
 from unittest.mock import patch
 
+from aiohttp.test_utils import TestClient
 import pytest
 from switchbot_api import CannotConnect, Device, InvalidAuth, PowerState, Remote
 
 from homeassistant.components.switchbot_cloud import SwitchBotAPI
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import EVENT_HOMEASSISTANT_START
+from homeassistant.const import CONF_WEBHOOK_ID, EVENT_HOMEASSISTANT_START
 from homeassistant.core import HomeAssistant
+from homeassistant.core_config import async_process_ha_core_config
+from homeassistant.setup import async_setup_component
 
 from . import configure_integration
+
+from tests.typing import ClientSessionGenerator
 
 
 @pytest.fixture
@@ -27,10 +32,52 @@ def mock_get_status():
         yield mock_get_status
 
 
+@pytest.fixture
+def mock_get_webook_configuration():
+    """Mock get_status."""
+    with patch.object(
+        SwitchBotAPI, "get_webook_configuration"
+    ) as mock_get_webook_configuration:
+        yield mock_get_webook_configuration
+
+
+@pytest.fixture
+def mock_delete_webhook():
+    """Mock get_status."""
+    with patch.object(SwitchBotAPI, "delete_webhook") as mock_delete_webhook:
+        yield mock_delete_webhook
+
+
+@pytest.fixture
+def mock_setup_webhook():
+    """Mock get_status."""
+    with patch.object(SwitchBotAPI, "setup_webhook") as mock_setup_webhook:
+        yield mock_setup_webhook
+
+
+@pytest.fixture
+async def mock_client(
+    hass: HomeAssistant, hass_client: ClientSessionGenerator
+) -> TestClient:
+    """Create http client for webhooks."""
+    await async_setup_component(hass, "webhook", {})
+    return await hass_client()
+
+
 async def test_setup_entry_success(
-    hass: HomeAssistant, mock_list_devices, mock_get_status
+    hass: HomeAssistant,
+    mock_list_devices,
+    mock_get_status,
+    mock_get_webook_configuration,
+    mock_delete_webhook,
+    mock_setup_webhook,
 ) -> None:
     """Test successful setup of entry."""
+    await async_process_ha_core_config(
+        hass,
+        {"external_url": "https://example.com"},
+    )
+    mock_get_webook_configuration.return_value = {"urls": ["https://example.com"]}
     mock_list_devices.return_value = [
         Remote(
             version="V1.0",
@@ -67,8 +114,15 @@ async def test_setup_entry_success(
             deviceType="Hub 2",
             hubDeviceId="test-hub-id",
         ),
+        Device(
+            deviceId="vacuum-1",
+            deviceName="vacuum-name-1",
+            deviceType="K10+",
+            hubDeviceId=None,
+        ),
     ]
     mock_get_status.return_value = {"power": PowerState.ON.value}
+
     entry = await configure_integration(hass)
     assert entry.state is ConfigEntryState.LOADED
 
@@ -76,6 +130,9 @@ async def test_setup_entry_success(
     await hass.async_block_till_done()
     mock_list_devices.assert_called_once()
     mock_get_status.assert_called()
+    mock_get_webook_configuration.assert_called_once()
+    mock_delete_webhook.assert_called_once()
+    mock_setup_webhook.assert_called_once()
 
 
 @pytest.mark.parametrize(
@@ -124,3 +181,51 @@ async def test_setup_entry_fails_when_refreshing(
     await hass.async_block_till_done()
     mock_list_devices.assert_called_once()
     mock_get_status.assert_called()
+
+
+async def test_posting_to_webhook(
+    hass: HomeAssistant,
+    mock_list_devices,
+    mock_get_status,
+    mock_get_webook_configuration,
+    mock_delete_webhook,
+    mock_setup_webhook,
+    mock_client,
+) -> None:
+    """Test handler webhook call."""
+    await async_process_ha_core_config(
+        hass,
+        {"external_url": "https://example.com"},
+    )
+    mock_get_webook_configuration.return_value = {"urls": ["https://example.com"]}
+    mock_list_devices.return_value = [
+        Device(
+            deviceId="vacuum-1",
+            deviceName="vacuum-name-1",
+            deviceType="K10+",
+            hubDeviceId=None,
+        ),
+    ]
+    mock_get_status.return_value = {"power": PowerState.ON.value}
+    mock_delete_webhook.return_value = {}
+    mock_setup_webhook.return_value = {}
+
+    entry = await configure_integration(hass)
+    assert entry.state is ConfigEntryState.LOADED
+
+    hass.bus.async_fire(EVENT_HOMEASSISTANT_START)
+
+    webhook_id = entry.data[CONF_WEBHOOK_ID]
+    # fire webhook
+    await mock_client.post(
+        f"/api/webhook/{webhook_id}",
+        json={
+            "eventType": "changeReport",
+            "eventVersion": "1",
+            "context": {"deviceType": "...", "deviceMac": "vacuum-1"},
+        },
+    )
+
+    await hass.async_block_till_done()
+
+    mock_setup_webhook.assert_called_once()
