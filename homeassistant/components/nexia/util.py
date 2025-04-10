48a0b47d52e015337249450ea96db74cdd53f219
jrhillery <john.hillery2@gmail.com>
2025-04-10 10:45:13 -0400

    Add single shot timer for nexia integration

--- homeassistant/components/nexia/util.py
+++ homeassistant/components/nexia/util.py
@@ -1,6 +1,14 @@
 """Utils for Nexia / Trane XL Thermostats."""
 
+import asyncio
+from collections.abc import Callable, Coroutine
+from datetime import timedelta
 from http import HTTPStatus
+from typing import Any
+
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
+from homeassistant.core import HassJob, HomeAssistant, callback
+from homeassistant.helpers.event import async_call_later
 
 
 def is_invalid_auth_code(http_status_code):
@@ -16,3 +24,64 @@ def percent_conv(val):
     if val is None:
         return None
     return round(val * 100.0, 1)
+
+
+class SingleShot:
+    """Provides a single shot timer that can be reset.
+
+    Fires a while following the *last* call to `reset_delayed_action_trigger`.
+    """
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        delay: timedelta,
+        delayed_call: Callable[[], Coroutine[Any, Any, None]],
+    ) -> None:
+        """Initialize this single shot timer."""
+        self._hass = hass
+        self._delay = delay
+        self._delayed_call = delayed_call
+        self._cancel_delayed_action: Callable[[], None] | None = None
+        self._execute_lock = asyncio.Lock()
+        self._shutting_down = False
+        self._job = HassJob(
+            self._delayed_action,
+            f"resettable single shot action {self._delay}",
+        )
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, self._async_shutdown)
+
+    async def _delayed_action(self, now: Any) -> None:
+        """Perform the action now that the delay has completed."""
+        self._cancel_delayed_action = None
+
+        async with self._execute_lock:
+            # Abort if rescheduled while waiting for the lock or shutting down.
+            if self._cancel_delayed_action or self._shutting_down:
+                return
+
+            # Perform the primary action for this timer.
+            await self._delayed_call()
+
+    def reset_delayed_action_trigger(self) -> None:
+        """Set or reset the delayed action trigger.
+
+        Perform the action a while after this call.
+        """
+        if self._shutting_down:
+            return
+        if self._cancel_delayed_action:
+            self._cancel_delayed_action()
+
+        self._cancel_delayed_action = async_call_later(
+            self._hass, self._delay, self._job
+        )
+
+    @callback
+    def _async_shutdown(self, event: Any) -> None:
+        """Handle Home Assistant stopping."""
+        self._shutting_down = True
+        if self._cancel_delayed_action:
+            self._cancel_delayed_action()
+            self._cancel_delayed_action = None
+        self._delayed_call = None
--- tests/components/nexia/test_util.py
+++ tests/components/nexia/test_util.py
@@ -1,8 +1,14 @@
 """The sensor tests for the nexia platform."""
 
+from datetime import timedelta
 from http import HTTPStatus
+from unittest.mock import AsyncMock
 
 from homeassistant.components.nexia import util
+from homeassistant.core import HomeAssistant
+from homeassistant.util import utcnow
+
+from tests.common import async_fire_time_changed
 
 
 async def test_is_invalid_auth_code() -> None:
@@ -18,3 +24,32 @@ async def test_percent_conv() -> None:
 
     assert util.percent_conv(0.12) == 12.0
     assert util.percent_conv(0.123) == 12.3
+
+
+async def test_resettable_single_shot(hass: HomeAssistant) -> None:
+    """Test class SingleShot."""
+    calls = []
+    single_shot = util.SingleShot(
+        hass, timedelta(seconds=0.01), AsyncMock(side_effect=lambda: calls.append(None))
+    )
+
+    # Fire once.
+    single_shot.reset_delayed_action_trigger()
+    assert len(calls) == 0
+    assert single_shot._cancel_delayed_action is not None
+
+    # Fire again while pending.
+    single_shot.reset_delayed_action_trigger()
+    assert len(calls) == 0
+    assert single_shot._cancel_delayed_action is not None
+
+    # Wait for time to run.
+    async_fire_time_changed(hass, utcnow() + timedelta(seconds=1))
+    await hass.async_block_till_done()
+    assert len(calls) == 1
+    assert single_shot._cancel_delayed_action is None
+
+    # Fire again, then exit to exercise shutdown path.
+    single_shot.reset_delayed_action_trigger()
+    assert len(calls) == 1
+    assert single_shot._cancel_delayed_action is not None
