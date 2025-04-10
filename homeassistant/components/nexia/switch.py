870e7a4df7219ebbe213a1f99a81faeb84aed1f9
jrhillery <john.hillery2@gmail.com>
2025-04-10 10:45:13 -0400

    Provide ability to select RoomIQ sensors

--- homeassistant/components/nexia/switch.py
+++ homeassistant/components/nexia/switch.py
@@ -5,11 +5,14 @@ from __future__ import annotations
 from typing import Any
 
 from nexia.const import OPERATION_MODE_OFF
+from nexia.roomiq import NexiaRoomIQHarmonizer
+from nexia.sensor import NexiaSensor
 from nexia.thermostat import NexiaThermostat
 from nexia.zone import NexiaThermostatZone
 
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.core import HomeAssistant
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
+from homeassistant.core import Event, HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from .coordinator import NexiaDataUpdateCoordinator
@@ -25,7 +28,8 @@ async def async_setup_entry(
     """Set up switches for a Nexia device."""
     coordinator = config_entry.runtime_data
     nexia_home = coordinator.nexia_home
-    entities: list[NexiaHoldSwitch | NexiaEmergencyHeatSwitch] = []
+    entities: list[SwitchEntity] = []
+    room_iq_zones: dict[int, NexiaRoomIQHarmonizer] = {}
     for thermostat_id in nexia_home.get_thermostat_ids():
         thermostat: NexiaThermostat = nexia_home.get_thermostat_by_id(thermostat_id)
         if thermostat.has_emergency_heat():
@@ -33,8 +37,22 @@ async def async_setup_entry(
         for zone_id in thermostat.get_zone_ids():
             zone: NexiaThermostatZone = thermostat.get_zone_by_id(zone_id)
             entities.append(NexiaHoldSwitch(coordinator, zone))
+            if len(zone_sensors := zone.get_sensors()) > 1:
+                entities.extend(
+                    NexiaRoomIQSwitch(coordinator, zone, sensor, room_iq_zones)
+                    for sensor in zone_sensors
+                )
 
     async_add_entities(entities)
+    for harmonizer in room_iq_zones.values():
+
+        async def _stop_obj(_: Event, obj=harmonizer) -> None:
+            """Run the shutdown method when preparing to stop."""
+            await obj.async_shutdown()
+
+        config_entry.async_on_unload(
+            hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, _stop_obj)
+        )
 
 
 class NexiaHoldSwitch(NexiaThermostatZoneEntity, SwitchEntity):
@@ -66,6 +84,61 @@ class NexiaHoldSwitch(NexiaThermostatZoneEntity, SwitchEntity):
         """Disable permanent hold."""
         await self._zone.call_return_to_schedule()
         self._signal_zone_update()
+
+
+class NexiaRoomIQSwitch(NexiaThermostatZoneEntity, SwitchEntity):
+    """Provides Nexia RoomIQ sensor switch support."""
+
+    _attr_translation_key = "room_iq_sensor"
+
+    def __init__(
+        self,
+        coordinator: NexiaDataUpdateCoordinator,
+        zone: NexiaThermostatZone,
+        sensor: NexiaSensor,
+        room_iq_zones: dict[int, NexiaRoomIQHarmonizer],
+    ) -> None:
+        """Initialize the RoomIQ sensor switch."""
+        super().__init__(coordinator, zone, f"{sensor.id}_room_iq_sensor")
+        self._attr_translation_placeholders = {"sensor_name": sensor.name}
+        self._sensor_id = sensor.id
+        self._harmonizer = room_iq_zones.setdefault(
+            zone.zone_id,
+            NexiaRoomIQHarmonizer(
+                zone, coordinator.async_refresh, self._signal_zone_update
+            ),
+        )
+
+    @property
+    def is_on(self) -> bool:
+        """Return if the sensor is part of the zone average temperature."""
+        if self._harmonizer.request_pending():
+            return self._sensor_id in self._harmonizer.selected_sensor_ids
+
+        included = self._zone.get_sensor_by_id(self._sensor_id).weight > 0.0
+        # Keep our collection of selected RoomIQ sensors up to date
+        if included:
+            self._harmonizer.selected_sensor_ids.add(self._sensor_id)
+        else:
+            self._harmonizer.selected_sensor_ids.discard(self._sensor_id)
+        return included
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Include this sensor."""
+        self._harmonizer.trigger_add_sensor(self._sensor_id)
+        self._signal_zone_update()
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Remove this sensor."""
+        self._harmonizer.trigger_remove_sensor(self._sensor_id)
+        self._signal_zone_update()
+
+    @property
+    def extra_state_attributes(self) -> dict[str, Any]:
+        """Return our extra state information."""
+        return {
+            "request_pending": self._harmonizer.request_pending(),
+        }
 
 
 class NexiaEmergencyHeatSwitch(NexiaThermostatEntity, SwitchEntity):
--- tests/components/nexia/test_switch.py
+++ tests/components/nexia/test_switch.py
@@ -1,12 +1,81 @@
 """The switch tests for the nexia platform."""
 
-from homeassistant.const import STATE_ON
+from datetime import timedelta
+
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    EVENT_HOMEASSISTANT_STOP,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+    STATE_OFF,
+    STATE_ON,
+    Platform,
+)
 from homeassistant.core import HomeAssistant
+from homeassistant.util import utcnow
 
 from .util import async_init_integration
+
+from tests.common import async_fire_time_changed
 
 
 async def test_hold_switch(hass: HomeAssistant) -> None:
     """Test creation of the hold switch."""
     await async_init_integration(hass)
     assert hass.states.get("switch.nick_office_hold").state == STATE_ON
+
+
+async def test_nexia_sensor_switch(hass: HomeAssistant) -> None:
+    """Test NexiaRoomIQSensorSwitch."""
+    await async_init_integration(hass, house_fixture="nexia/sensors_xl1050_house.json")
+    sw1_id = f"{Platform.SWITCH}.center_nativezone_include_center"
+    sw1 = {ATTR_ENTITY_ID: sw1_id}
+    sw2_id = f"{Platform.SWITCH}.center_nativezone_include_upstairs"
+    sw2 = {ATTR_ENTITY_ID: sw2_id}
+
+    # Switch starts out on.
+    assert (entity_state := hass.states.get(sw1_id)) is not None
+    assert entity_state.attributes.get("request_pending") is False
+    assert entity_state.state == STATE_ON
+
+    # Turn switch off.
+    await hass.services.async_call(SWITCH_DOMAIN, SERVICE_TURN_OFF, sw1, blocking=True)
+    entity_state = hass.states.get(sw1_id)
+    assert entity_state.attributes.get("request_pending") is True
+    assert entity_state.state == STATE_OFF
+
+    # Turn switch back on.
+    await hass.services.async_call(SWITCH_DOMAIN, SERVICE_TURN_ON, sw1, blocking=True)
+    assert hass.states.get(sw1_id).state == STATE_ON
+
+    # The other switch also starts out on.
+    assert (entity_state := hass.states.get(sw2_id)) is not None
+    assert entity_state.state == STATE_ON
+
+    # Turn both switches off, an invalid combination.
+    await hass.services.async_call(SWITCH_DOMAIN, SERVICE_TURN_OFF, sw1, blocking=True)
+    await hass.services.async_call(SWITCH_DOMAIN, SERVICE_TURN_OFF, sw2, blocking=True)
+    assert hass.states.get(sw1_id).state == STATE_OFF
+    assert hass.states.get(sw2_id).state == STATE_OFF
+
+    # Wait for switches to revert to device status.
+    async_fire_time_changed(hass, utcnow() + timedelta(seconds=4))
+    await hass.async_block_till_done()
+    entity_state = hass.states.get(sw1_id)
+    assert entity_state.attributes.get("request_pending") is False
+    assert entity_state.state == STATE_ON
+    assert hass.states.get(sw2_id).state == STATE_ON
+
+    # Turn switch off.
+    await hass.services.async_call(SWITCH_DOMAIN, SERVICE_TURN_OFF, sw2, blocking=True)
+    entity_state = hass.states.get(sw2_id)
+    assert entity_state.attributes.get("request_pending") is True
+    assert entity_state.state == STATE_OFF
+
+    # Exercise shutdown path.
+    hass.bus.async_fire(EVENT_HOMEASSISTANT_STOP)
+    await hass.async_block_till_done()
+    entity_state = hass.states.get(sw2_id)
+    assert entity_state.attributes.get("request_pending") is False
+    assert entity_state.state == STATE_ON
