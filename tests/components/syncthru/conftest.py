bf0d2e9bd2833516009261fa2f32c0015d14a340
Joost Lekkerkerker <joostlek@outlook.com>
2025-04-10 21:24:38 +0200

    Extract Syncthru coordinator in separate file (#142620)

--- tests/components/syncthru/conftest.py
+++ tests/components/syncthru/conftest.py
@@ -27,7 +27,7 @@ def mock_syncthru() -> Generator[AsyncMock]:
     """Mock the SyncThru class."""
     with (
         patch(
-            "homeassistant.components.syncthru.SyncThru",
+            "homeassistant.components.syncthru.coordinator.SyncThru",
             autospec=True,
         ) as mock_syncthru,
         patch(
