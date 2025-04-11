9647f145dee3963ae99704d7e08c7e36d9a367fc
wilbiev <vilsteren@kpnmail.nl>
2025-04-11 18:41:12 +0000

    Add new dryer-washer button capabilities

--- homeassistant/components/smartthings/button.py
+++ homeassistant/components/smartthings/button.py
@@ -1,8 +1,8 @@
-"""Support for button entities through the SmartThings cloud API."""
+"""Support for buttons through the SmartThings cloud API."""
 
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from pysmartthings import Capability, Command, SmartThings
 
@@ -16,24 +16,58 @@ from .entity import SmartThingsEntity
 
 
 @dataclass(frozen=True, kw_only=True)
-class SmartThingsButtonDescription(ButtonEntityDescription):
-    """Class describing SmartThings button entities."""
+class SmartThingsButtonEntityDescription(ButtonEntityDescription):
+    """Describe a SmartThings binary sensor entity."""
 
-    key: Capability
-    command: Command
+    command_list: list[Command] = field(default_factory=list)
 
 
-CAPABILITIES_TO_BUTTONS: dict[Capability | str, SmartThingsButtonDescription] = {
-    Capability.OVEN_OPERATING_STATE: SmartThingsButtonDescription(
-        key=Capability.OVEN_OPERATING_STATE,
-        translation_key="stop",
-        command=Command.STOP,
-    ),
-    Capability.CUSTOM_WATER_FILTER: SmartThingsButtonDescription(
-        key=Capability.CUSTOM_WATER_FILTER,
-        translation_key="reset_water_filter",
-        command=Command.RESET_WATER_FILTER,
-    ),
+CAPABILITY_TO_BUTTONS: dict[
+    Capability, dict[Command, list[SmartThingsButtonEntityDescription]]
+] = {
+    Capability.SAMSUNG_CE_WASHER_OPERATING_STATE: {
+        Command.START: [
+            SmartThingsButtonEntityDescription(
+                key=Command.START,
+                translation_key="state_start",
+            )
+        ],
+        Command.CANCEL: [
+            SmartThingsButtonEntityDescription(
+                key=Command.CANCEL,
+                translation_key="state_cancel",
+            )
+        ],
+        Command.RESUME: [
+            SmartThingsButtonEntityDescription(
+                key="pause_resume",
+                translation_key="state_pause_resume",
+                command_list=[Command.PAUSE, Command.RESUME],
+            )
+        ],
+        Command.ESTIMATE_OPERATION_TIME: [
+            SmartThingsButtonEntityDescription(
+                key=Command.ESTIMATE_OPERATION_TIME,
+                translation_key="estimate_operation_time",
+            )
+        ],
+    },
+    Capability.OVEN_OPERATING_STATE: {
+        Command.STOP: [
+            SmartThingsButtonEntityDescription(
+                key=Capability.OVEN_OPERATING_STATE,
+                translation_key="stop",
+            ),
+        ]
+    },
+    Capability.CUSTOM_WATER_FILTER: {
+        Command.RESET_WATER_FILTER: [
+            SmartThingsButtonEntityDescription(
+                key=Capability.CUSTOM_WATER_FILTER,
+                translation_key="reset_water_filter",
+            ),
+        ]
+    },
 }
 
 
@@ -42,37 +76,56 @@ async def async_setup_entry(
     entry: SmartThingsConfigEntry,
     async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
-    """Add button entities for a config entry."""
+    """Add buttons for a config entry."""
     entry_data = entry.runtime_data
     async_add_entities(
-        SmartThingsButtonEntity(
-            entry_data.client, device, CAPABILITIES_TO_BUTTONS[capability]
+        SmartThingsButton(
+            entry_data.client,
+            device,
+            description,
+            capability,
+            command,
+            component,
         )
         for device in entry_data.devices.values()
-        for capability in device.status[MAIN]
-        if capability in CAPABILITIES_TO_BUTTONS
+        for capability, commands in CAPABILITY_TO_BUTTONS.items()
+        for component in device.status
+        if capability in device.status[component]
+        for command, descriptions in commands.items()
+        for description in descriptions
     )
 
 
-class SmartThingsButtonEntity(SmartThingsEntity, ButtonEntity):
+class SmartThingsButton(SmartThingsEntity, ButtonEntity):
     """Define a SmartThings button."""
 
-    entity_description: SmartThingsButtonDescription
+    entity_description: SmartThingsButtonEntityDescription
 
     def __init__(
         self,
         client: SmartThings,
         device: FullDevice,
-        entity_description: SmartThingsButtonDescription,
+        entity_description: SmartThingsButtonEntityDescription,
+        capability: Capability,
+        command: Command,
+        component: str = MAIN,
     ) -> None:
-        """Initialize the instance."""
-        super().__init__(client, device, set())
+        """Init the class."""
+        super().__init__(client, device, {capability}, component=component)
+        self._attr_unique_id = f"{device.device.device_id}_{component}_{capability}_{entity_description.key}"
+        self.command = command
+        self.capability = capability
         self.entity_description = entity_description
-        self._attr_unique_id = f"{device.device.device_id}_{MAIN}_{entity_description.key}_{entity_description.command}"
 
     async def async_press(self) -> None:
         """Press the button."""
+        if self.entity_description.command_list:
+            item = self.entity_description.command_list.index(self.command)
+            if item == (len(self.entity_description.command_list) - 1):
+                self.command = self.entity_description.command_list[0]
+            else:
+                self.command = self.entity_description.command_list[item + 1]
         await self.execute_device_command(
-            self.entity_description.key,
-            self.entity_description.command,
+            self.capability,
+            self.command,
         )
