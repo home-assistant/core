74c1027dcc6babeaf76c8d0f51ed8a364a8e219b
wang.xiangtao <wang.xiangtao90@gmail.com>
2025-04-12 22:42:12 +0800

    Modify Dreo i18 device control failed message

--- homeassistant/components/dreo/const.py
+++ homeassistant/components/dreo/const.py
@@ -3,8 +3,8 @@
 DOMAIN = "dreo"
 
 # Error messages
-ERROR_TURN_ON_FAILED = "exceptions.turn_on_failed.message"
-ERROR_TURN_OFF_FAILED = "exceptions.turn_off_failed.message"
-ERROR_SET_PRESET_MODE_FAILED = "exceptions.set_preset_mode_failed.message"
-ERROR_SET_SPEED_FAILED = "exceptions.set_speed_failed.message"
-ERROR_SET_OSCILLATE_FAILED = "exceptions.set_oscillate_failed.message"
+ERROR_TURN_ON_FAILED = "turn_on_failed"
+ERROR_TURN_OFF_FAILED = "turn_off_failed"
+ERROR_SET_PRESET_MODE_FAILED = "set_preset_mode_failed"
+ERROR_SET_SPEED_FAILED = "set_speed_failed"
+ERROR_SET_OSCILLATE_FAILED = "set_oscillate_failed"
