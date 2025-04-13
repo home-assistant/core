adc7c526be7f48bef64a5cf521ba403c8afbedbe
Joostlek <joostlek@outlook.com>
2025-04-13 05:00:05 +0200

    Grade Syncthru on the quality scale

--- script/hassfest/quality_scale.py
+++ script/hassfest/quality_scale.py
@@ -971,7 +971,6 @@ INTEGRATIONS_WITHOUT_QUALITY_SCALE_FILE = [
     "switcher_kis",
     "switchmate",
     "syncthing",
-    "syncthru",
     "synology_chat",
     "synology_dsm",
     "synology_srm",
