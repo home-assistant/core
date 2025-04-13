6897a40b4d6f464fc998cc7bc9e6e4a8baf6f952
J. Nick Koston <nick@koston.org>
2025-04-13 13:15:09 -1000

    merge

--- homeassistant/generated/bluetooth.py
+++ homeassistant/generated/bluetooth.py
@@ -373,6 +373,21 @@ BLUETOOTH: Final[list[dict[str, bool | str | int | list[int]]]] = [
     },
     {
         "connectable": True,
+        "domain": "inkbird",
+        "local_name": "Ink@IAM-T1",
+    },
+    {
+        "connectable": True,
+        "domain": "inkbird",
+        "manufacturer_data_start": [
+            67,
+            45,
+            54,
+        ],
+        "manufacturer_id": 12628,
+    },
+    {
+        "connectable": True,
         "domain": "iron_os",
         "service_uuid": "9eae1000-9d0d-48c5-aa55-33e27f9bc533",
     },
