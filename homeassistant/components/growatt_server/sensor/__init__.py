f1ba9c187b5ece2cfb1c42fd62d5218b8360969c
Thimo Seitz <thimo.seitz@portamis.de>
2025-04-13 20:22:41 +0200

    Update __init__.py

--- homeassistant/components/growatt_server/sensor/__init__.py
+++ homeassistant/components/growatt_server/sensor/__init__.py
@@ -49,11 +49,8 @@ def get_device_list(api, config):
         if login_response["msg"] == LOGIN_INVALID_AUTH_CODE:
             raise ConfigEntryError("Username, Password or URL may be incorrect!")
         if login_response["msg"] == LOGIN_LOCKED_CODE:
-            raise ConfigEntryError(
-                login_response["error"]
-                + "; lockDuration="
-                + login_response["lockDuration"]
-            )
+            raise ConfigEntryError(login_response["error"])
+        raise ConfigEntryError(f"Unkown error, server responds: {login_response["error"]}")
     user_id = login_response["user"]["id"]
     if plant_id == DEFAULT_PLANT_ID:
         plant_info = api.plant_list(user_id)
