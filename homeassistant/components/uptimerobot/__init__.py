f414316455d50fd1ceabdd624ba55c084af5ae23
Simone Chemelli <simone.chemelli@gmail.com>
2025-04-13 14:51:21 +0000

    fix unload

--- homeassistant/components/uptimerobot/__init__.py
+++ homeassistant/components/uptimerobot/__init__.py
@@ -9,7 +9,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN, PLATFORMS
+from .const import PLATFORMS
 from .coordinator import UptimeRobotConfigEntry, UptimeRobotDataUpdateCoordinator
 
 
@@ -41,8 +41,4 @@ async def async_unload_entry(
     hass: HomeAssistant, entry: UptimeRobotConfigEntry
 ) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
