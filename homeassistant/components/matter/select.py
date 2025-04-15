21b24aa8b72819aa260dad896af9764c1bcf564f
Ludovic BOUE <lboue@users.noreply.github.com>
2025-04-15 12:58:55 +0200

    Remove MatterWaterHeaterMode selector

--- homeassistant/components/matter/select.py
+++ homeassistant/components/matter/select.py
@@ -322,18 +322,6 @@ DISCOVERY_SCHEMAS = [
     MatterDiscoverySchema(
         platform=Platform.SELECT,
         entity_description=MatterSelectEntityDescription(
-            key="MatterWaterHeaterMode",
-            translation_key="mode",
-        ),
-        entity_class=MatterModeSelectEntity,
-        required_attributes=(
-            clusters.WaterHeaterMode.Attributes.CurrentMode,
-            clusters.WaterHeaterMode.Attributes.SupportedModes,
-        ),
-    ),
-    MatterDiscoverySchema(
-        platform=Platform.SELECT,
-        entity_description=MatterSelectEntityDescription(
             key="MatterStartUpOnOff",
             entity_category=EntityCategory.CONFIG,
             translation_key="startup_on_off",
