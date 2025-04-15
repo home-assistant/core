047f4b7c57b12f4e90898ac798db5ba6c42691e0
Simone Chemelli <simone.chemelli@gmail.com>
2025-04-15 09:55:16 +0200

    Fix switch state for Comelit (#142978)

--- homeassistant/components/comelit/switch.py
+++ homeassistant/components/comelit/switch.py
@@ -81,4 +81,7 @@ class ComelitSwitchEntity(CoordinatorEntity[ComelitSerialBridge], SwitchEntity):
     @property
     def is_on(self) -> bool:
         """Return True if switch is on."""
-        return self.coordinator.data[OTHER][self._device.index].status == STATE_ON
+        return (
+            self.coordinator.data[self._device.type][self._device.index].status
+            == STATE_ON
+        )
