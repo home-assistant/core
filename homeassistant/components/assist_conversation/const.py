cb9ccc360ed6909d741f89584da99b252218e13e
Artur Pragacz <artur@pragacz.com>
2025-04-16 04:22:39 +0200

    Fix lint

--- homeassistant/components/assist_conversation/const.py
+++ homeassistant/components/assist_conversation/const.py
@@ -1,5 +1,7 @@
 """Const for assist conversation integration."""
 
-from homeassistant.components.conversation import DOMAIN as CONVERSATION_DOMAIN
+from homeassistant.components.conversation import (
+    DOMAIN as CONVERSATION_DOMAIN,  # noqa: F401
+)
 
 DOMAIN = "assist_conversation"
