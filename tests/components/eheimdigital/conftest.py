4dd62d7c39e5286f6d59632e7e1878fb231c928a
autinerd <27780930+autinerd@users.noreply.github.com>
2025-04-17 17:40:28 +0200

    Add time platform to eheimdigital

--- tests/components/eheimdigital/conftest.py
+++ tests/components/eheimdigital/conftest.py
@@ -1,6 +1,7 @@
 """Configurations for the EHEIM Digital tests."""
 
 from collections.abc import Generator
+from datetime import time, timedelta, timezone
 from unittest.mock import AsyncMock, MagicMock, patch
 
 from eheimdigital.classic_led_ctrl import EheimDigitalClassicLEDControl
@@ -66,6 +67,8 @@ def heater_mock():
     heater_mock.is_heating = True
     heater_mock.is_active = True
     heater_mock.operation_mode = HeaterMode.MANUAL
+    heater_mock.day_start_time = time(8, 0, tzinfo=timezone(timedelta(hours=1)))
+    heater_mock.night_start_time = time(20, 0, tzinfo=timezone(timedelta(hours=1)))
     return heater_mock
 
 
@@ -81,6 +84,10 @@ def classic_vario_mock():
     classic_vario_mock.current_speed = 75
     classic_vario_mock.manual_speed = 75
     classic_vario_mock.day_speed = 80
+    classic_vario_mock.day_start_time = time(8, 0, tzinfo=timezone(timedelta(hours=1)))
+    classic_vario_mock.night_start_time = time(
+        20, 0, tzinfo=timezone(timedelta(hours=1))
+    )
     classic_vario_mock.night_speed = 20
     classic_vario_mock.is_active = True
     classic_vario_mock.filter_mode = FilterMode.MANUAL
