9a4e791d411766658356199f383eaf50f14af5d5
J. Nick Koston <nick@koston.org>
2025-04-18 02:58:27 -1000

    try another way

--- tests/components/bluetooth/test_config_flow.py
+++ tests/components/bluetooth/test_config_flow.py
@@ -30,10 +30,7 @@ from tests.typing import WebSocketGenerator
 
 
 @pytest.mark.usefixtures(
-    "disable_bluez_manager_socket",
-    "macos_adapter",
-    "mock_bleak_scanner_start",
-    "mock_bluetooth_adapters",
+    "macos_adapter", "mock_bleak_scanner_start", "mock_bluetooth_adapters"
 )
 async def test_options_flow_disabled_not_setup(
     hass: HomeAssistant, hass_ws_client: WebSocketGenerator
