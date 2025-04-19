22784438fca4e9409033aeb4900761185649f1a9
Åke Strandberg <ake@strandberg.eu>
2025-04-19 12:06:52 +0000

    Add zeroconf discovery

--- homeassistant/components/miele/config_flow.py
+++ homeassistant/components/miele/config_flow.py
@@ -9,9 +9,13 @@ from homeassistant.config_entries import (
     SOURCE_RECONFIGURE,
     ConfigFlowResult,
 )
+from homeassistant.const import CONF_NAME
 from homeassistant.helpers import config_entry_oauth2_flow
+from homeassistant.helpers.service_info.zeroconf import ZeroconfServiceInfo
 
 from .const import DOMAIN
+
+UNIQUE_ID = "miele"
 
 
 class OAuth2FlowHandler(
@@ -20,6 +24,7 @@ class OAuth2FlowHandler(
     """Config flow to handle Miele OAuth2 authentication."""
 
     DOMAIN = DOMAIN
+    name: str = ""
 
     @property
     def logger(self) -> logging.Logger:
@@ -71,3 +76,27 @@ class OAuth2FlowHandler(
                 self._get_reconfigure_entry(), data=data
             )
         return await super().async_oauth_create_entry(data)
+
+    async def async_step_zeroconf(
+        self, discovery_info: ZeroconfServiceInfo
+    ) -> ConfigFlowResult:
+        """Prepare configuration for a zeroconf discovered Miele device."""
+        self.name = discovery_info.name.split(".", 1)[0]
+        # The unique_id here is needed for zeroconf discovery to behave properly
+        await self.async_set_unique_id(unique_id=UNIQUE_ID)
+        self._abort_if_unique_id_configured()
+
+        return await self.async_step_zeroconf_confirm()
+
+    async def async_step_zeroconf_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle a flow initiated by zeroconf."""
+
+        if user_input is not None:
+            return await self.async_step_user()
+
+        return self.async_show_form(
+            step_id="zeroconf_confirm",
+            description_placeholders={CONF_NAME: self.name},
+        )
