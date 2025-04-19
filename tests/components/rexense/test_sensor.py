1914e6bb89dde07a3965b01179c35dd2d3abc14a
xiaoqiang.yxq <yuxiaoqiang@rexense.com>
2025-04-19 21:12:01 +0800

    Add Rexense integration

---
+++ tests/components/rexense/test_sensor.py
@@ -0,0 +1,21 @@
+import pytest
+
+from homeassistant.components.rexense.sensor import RexenseSensor
+from homeassistant.components.rexense.websocket_client import RexenseWebsocketClient
+from homeassistant.components.rexense.const import REXSENSE_SENSOR_TYPES, DOMAIN
+
+class DummyClient:
+    def __init__(self):
+        self.device_id = 'test'
+        self.model = 'REX-PLUG-01'
+        self.feature_map = [{'EP':1,'Attributes': ['Voltage']}]
+        self.last_values = {'Voltage': 3.3}
+        self.signal_update = f"{DOMAIN}_test_update"
+        self.connected = True
+
+
+def test_native_value_and_available(tmp_path, monkeypatch):
+    client = DummyClient()
+    sensor = RexenseSensor(client, 'Voltage', 'voltage', 'V', None, None)
+    assert sensor.native_value == 3.3
+    assert sensor.available is True
