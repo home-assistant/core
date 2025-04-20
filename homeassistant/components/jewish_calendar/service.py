82e46779596505e06979c3a9e8c6d3639fa55ff6
Amit Finkelstein <amittein@gmail.com>
2025-04-20 12:39:25 +0000

    Sfirat Omer for "now" ("date" becomes optional)

--- homeassistant/components/jewish_calendar/service.py
+++ homeassistant/components/jewish_calendar/service.py
@@ -8,7 +8,7 @@ from hdate.omer import Nusach, Omer
 from hdate.translator import Language
 import voluptuous as vol
 
-from homeassistant.const import CONF_LANGUAGE
+from homeassistant.const import CONF_LANGUAGE, SUN_EVENT_SUNSET
 from homeassistant.core import (
     HomeAssistant,
     ServiceCall,
@@ -17,13 +17,15 @@ from homeassistant.core import (
 )
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.selector import LanguageSelector, LanguageSelectorConfig
+from homeassistant.helpers.sun import get_astral_event_date
+from homeassistant.util import dt as dt_util
 
 from .const import ATTR_DATE, ATTR_NUSACH, DOMAIN, SERVICE_COUNT_OMER
 
 SUPPORTED_LANGUAGES = {"en": "english", "fr": "french", "he": "hebrew"}
 OMER_SCHEMA = vol.Schema(
     {
-        vol.Required(ATTR_DATE, default=datetime.date.today): cv.date,
+        vol.Optional(ATTR_DATE): cv.date,
         vol.Required(ATTR_NUSACH, default="sfarad"): vol.In(
             [nusach.name.lower() for nusach in Nusach]
         ),
@@ -39,7 +41,18 @@ def async_setup_services(hass: HomeAssistant) -> None:
 
     async def get_omer_count(call: ServiceCall) -> ServiceResponse:
         """Return the Omer blessing for a given date."""
-        hebrew_date = HebrewDate.from_gdate(call.data["date"])
+        if "date" in call.data:
+            hebrew_date = HebrewDate.from_gdate(call.data["date"])
+        else:
+            now = dt_util.now()
+            today = now.date()
+            if (
+                sunset := get_astral_event_date(hass, SUN_EVENT_SUNSET, today)
+            ) is not None and now > dt_util.as_local(sunset):
+                hebrew_date = HebrewDate.from_gdate(today + datetime.timedelta(days=1))
+            else:
+                hebrew_date = HebrewDate.from_gdate(today)
+
         nusach = Nusach[call.data["nusach"].upper()]
 
         # Currently Omer only supports Hebrew, English, and French and requires
--- tests/components/jewish_calendar/test_service.py
+++ tests/components/jewish_calendar/test_service.py
@@ -1,7 +1,10 @@
 """Test jewish calendar service."""
 
 import datetime as dt
+from datetime import UTC
+from unittest import mock
 
+from freezegun.api import FrozenDateTimeFactory
 from hdate.translator import Language
 import pytest
 
@@ -48,6 +51,52 @@ async def test_get_omer_blessing(
         DOMAIN,
         "count_omer",
         {"date": test_date, "nusach": nusach, "language": language},
+        blocking=True,
+        return_response=True,
+    )
+
+    assert result["message"] == expected
+
+
+@pytest.mark.parametrize(
+    ("test_time", "sunset", "expected"),
+    [
+        pytest.param(
+            dt.datetime(2025, 4, 20, 19, 10, tzinfo=UTC),
+            dt.datetime(2025, 4, 20, 19, 15, tzinfo=UTC),
+            "היום שבעה ימים שהם שבוע אחד לעומר",
+            id="before-sunset",
+        ),
+        pytest.param(
+            dt.datetime(2025, 4, 20, 19, 20, tzinfo=UTC),
+            dt.datetime(2025, 4, 20, 19, 15, tzinfo=UTC),
+            "היום שמונה ימים שהם שבוע אחד ויום אחד לעומר",
+            id="after-sunset",
+        ),
+    ],
+)
+@mock.patch("homeassistant.components.jewish_calendar.service.get_astral_event_date")
+async def test_get_current_omer_blessing(
+    mock_get_astral_event_date: mock.MagicMock,
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    freezer: FrozenDateTimeFactory,
+    test_time: dt.datetime,
+    sunset: dt.datetime,
+    expected: str,
+) -> None:
+    """Test get omer blessing of current time."""
+    hass.config.time_zone = "UTC"
+    freezer.move_to(test_time)
+    mock_get_astral_event_date.return_value = sunset
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    result = await hass.services.async_call(
+        DOMAIN,
+        "count_omer",
+        {"nusach": "sfarad", "language": "he"},
         blocking=True,
         return_response=True,
     )
