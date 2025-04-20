1948c90b5cf0dfdbaed6b6f2475ec93c25ad9eaa
Marc Hoersken <info@marc-hoersken.de>
2025-04-20 15:29:58 +0200

    Add support for identify buttons to WMS WebControl pro

---
+++ homeassistant/components/wmspro/button.py
@@ -0,0 +1,39 @@
+"""Identify support for WMS WebControl pro."""
+
+from __future__ import annotations
+
+from wmspro.const import WMS_WebControl_pro_API_actionDescription
+
+from homeassistant.components.button import ButtonDeviceClass, ButtonEntity
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+from . import WebControlProConfigEntry
+from .entity import WebControlProGenericEntity
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: WebControlProConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up the WMS based identify buttons from a config entry."""
+    hub = config_entry.runtime_data
+
+    entities: list[WebControlProGenericEntity] = []
+    for dest in hub.dests.values():
+        if dest.action(WMS_WebControl_pro_API_actionDescription.Identify):
+            entities.append(WebControlProIdentifyButton(config_entry.entry_id, dest))  # noqa: PERF401
+
+    async_add_entities(entities)
+
+
+class WebControlProIdentifyButton(WebControlProGenericEntity, ButtonEntity):
+    """Representation of a WMS based identify button."""
+
+    _attr_device_class = ButtonDeviceClass.IDENTIFY
+
+    async def async_press(self) -> None:
+        """Handle the button press."""
+        action = self._dest.action(WMS_WebControl_pro_API_actionDescription.Identify)
+        await action()
---
+++ tests/components/wmspro/test_button.py
@@ -0,0 +1,52 @@
+"""Test the wmspro button support."""
+
+from unittest.mock import AsyncMock, patch
+
+from syrupy import SnapshotAssertion
+
+from homeassistant.components.button import SERVICE_PRESS
+from homeassistant.const import ATTR_ENTITY_ID, Platform
+from homeassistant.core import HomeAssistant
+
+from . import setup_config_entry
+
+from tests.common import MockConfigEntry
+
+
+async def test_button_update(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_hub_ping: AsyncMock,
+    mock_hub_configuration_prod: AsyncMock,
+    mock_hub_status_prod_awning: AsyncMock,
+    mock_action_call: AsyncMock,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test that a button entity is created and updated correctly."""
+    assert await setup_config_entry(hass, mock_config_entry)
+    assert len(mock_hub_ping.mock_calls) == 1
+    assert len(mock_hub_configuration_prod.mock_calls) == 1
+    assert len(mock_hub_status_prod_awning.mock_calls) == 2
+
+    entity = hass.states.get("button.markise_identify")
+    assert entity is not None
+    assert entity == snapshot
+
+    with patch(
+        "wmspro.destination.Destination.refresh",
+        return_value=True,
+    ):
+        before = len(mock_hub_status_prod_awning.mock_calls)
+        before_state = entity.state
+
+        await hass.services.async_call(
+            Platform.BUTTON,
+            SERVICE_PRESS,
+            {ATTR_ENTITY_ID: entity.entity_id},
+            blocking=True,
+        )
+
+        entity = hass.states.get("button.markise_identify")
+        assert entity is not None
+        assert entity.state != before_state
+        assert len(mock_hub_status_prod_awning.mock_calls) == before
