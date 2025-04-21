0c73d02f7e47d4887a7e029a4a95ddb3c79261c6
Michael Arthur <michael.arthur@gameglass.gg>
2025-04-22 11:50:47 +1200

    prepare for merging to core

--- homeassistant/components/mammotion/entity.py
+++ homeassistant/components/mammotion/entity.py
@@ -3,7 +3,7 @@
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import CONF_ACCOUNTNAME, CONF_RETRY_COUNT, DEFAULT_RETRY_COUNT, DOMAIN
+from .const import CONF_RETRY_COUNT, DEFAULT_RETRY_COUNT, DOMAIN
 from .coordinator import MammotionBaseUpdateCoordinator
 
 
@@ -19,26 +19,9 @@ class MammotionBaseEntity(CoordinatorEntity[MammotionBaseUpdateCoordinator]):
 
     @property
     def device_info(self) -> DeviceInfo:
+        """Return the device information."""
         mower = self.coordinator.data
         swversion = mower.device_firmwares.device_version
-
-        product_key = mower.mower_state.product_key
-        if product_key is None or product_key == "":
-            if self.coordinator.manager.mqtt_list.get(
-                self.coordinator.config_entry.data.get(CONF_ACCOUNTNAME)
-            ):
-                mammotion_cloud = self.coordinator.manager.mqtt_list.get(
-                    self.coordinator.device_name
-                )
-                if mammotion_cloud is not None:
-                    device_list = mammotion_cloud.cloud_client.devices_by_account_response.data.data
-                    device = [
-                        device
-                        for device in device_list
-                        if device.deviceName == self.coordinator.device.deviceName
-                    ].pop()
-
-                    mower.mower_state.product_key = device.productKey
 
         model_id = None
         if mower is not None:
@@ -67,7 +50,7 @@ class MammotionBaseEntity(CoordinatorEntity[MammotionBaseUpdateCoordinator]):
 
     @property
     def available(self) -> bool:
-        """Return True if entity is available."""
+        """Return True if the entity is available."""
         return (
             self.coordinator.data is not None
             and self.coordinator.update_failures
