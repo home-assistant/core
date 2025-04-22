ac2aa90c9bd687674f32b063cef68259eaa05a58
yohaybn <yohaybn@gmail.com>
2025-04-22 16:29:27 +0300

    set after sunset as true for default

--- homeassistant/components/jewish_calendar/service.py
+++ homeassistant/components/jewish_calendar/service.py
@@ -21,14 +21,14 @@ from homeassistant.helpers.selector import LanguageSelector, LanguageSelectorCon
 from homeassistant.helpers.sun import get_astral_event_date
 from homeassistant.util import dt as dt_util
 
-from .const import AFTER_SUNSET, ATTR_DATE, ATTR_NUSACH, DOMAIN, SERVICE_COUNT_OMER
+from .const import ATTR_DATE, ATTR_NUSACH, DOMAIN, SERVICE_COUNT_OMER
 
 _LOGGER = logging.getLogger(__name__)
 SUPPORTED_LANGUAGES = {"en": "english", "fr": "french", "he": "hebrew"}
 OMER_SCHEMA = vol.Schema(
     {
         vol.Optional(ATTR_DATE): cv.date,
-        vol.Optional(AFTER_SUNSET, default=False): cv.boolean,
+        vol.Optional("is_after_sunset", default=True): cv.boolean,
         vol.Required(ATTR_NUSACH, default="sfarad"): vol.In(
             [nusach.name.lower() for nusach in Nusach]
         ),
@@ -39,33 +39,35 @@ OMER_SCHEMA = vol.Schema(
 )
 
 
-# top level for mocking tests
-def get_hebrew_date(hass: HomeAssistant) -> HebrewDate:
-    """Convert a Gregorian date to a Hebrew date."""
-    now = dt_util.now()
-    today = now.date()
-    event_date = get_astral_event_date(hass, SUN_EVENT_SUNSET, today)
-    if event_date is None:
-        _LOGGER.error("Can't get sunset event date for %s", today)
-        raise ValueError("Can't get sunset event date")
-    sunset = dt_util.as_local(event_date)
-    _LOGGER.debug("Now: %s Sunset: %s", now, sunset)
-    if now > sunset:
-        now = now + datetime.timedelta(days=1)
-        _LOGGER.debug("Date after sunset: %s", now)
-    return HebrewDate.from_gdate(now)
-
-
 def async_setup_services(hass: HomeAssistant) -> None:
     """Set up the Jewish Calendar services."""
+
+    def get_hebrew_date(hass: HomeAssistant) -> HebrewDate:
+        """Convert a Gregorian date to a Hebrew date."""
+        after_sunset = is_after_sunset(hass)
+        return HebrewDate.from_gdate(
+            dt_util.now() + datetime.timedelta(days=int(after_sunset))
+        )
+
+    def is_after_sunset(hass: HomeAssistant) -> bool:
+        """Determine if the current time is after sunset."""
+        now = dt_util.now()
+        today = now.date()
+        event_date = get_astral_event_date(hass, SUN_EVENT_SUNSET, today)
+        if event_date is None:
+            _LOGGER.error("Can't get sunset event date for %s", today)
+            raise ValueError("Can't get sunset event date")
+        sunset = dt_util.as_local(event_date)
+        _LOGGER.debug("Now: %s Sunset: %s", now, sunset)
+        return now > sunset
 
     async def get_omer_count(call: ServiceCall) -> ServiceResponse:
         """Return the Omer blessing for a given date."""
         if "date" in call.data:
             date = call.data["date"]
-            if call.data[AFTER_SUNSET]:
-                date = date + datetime.timedelta(days=1)
-            hebrew_date = HebrewDate.from_gdate(date)
+            hebrew_date = HebrewDate.from_gdate(
+                date + datetime.timedelta(days=int(call.data["is_after_sunset"]))
+            )
         else:
             hebrew_date = get_hebrew_date(call.hass)
         nusach = Nusach[call.data["nusach"].upper()]
