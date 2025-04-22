ecafaccb908548fc614bc2aff87e310e685e4b60
Åke Strandberg <ake@strandberg.eu>
2025-04-22 11:03:55 +0000

    Rearrange services according to review comments

--- homeassistant/components/miele/services.py
+++ homeassistant/components/miele/services.py
@@ -27,54 +27,59 @@ SERVICE_PROGRAM = cv.make_entity_service_schema(
 _LOGGER = logging.getLogger(__name__)
 
 
+async def extract_our_config_entry_ids(service_call: ServiceCall) -> list[str]:
+    """Extract config entry IDs from the service call."""
+    hass = service_call.hass
+    return [
+        entry_id
+        for entry_id in await async_extract_config_entry_ids(hass, service_call)
+        if (entry := hass.config_entries.async_get_entry(entry_id))
+        and entry.domain == DOMAIN
+    ]
+
+
+async def set_program(call: ServiceCall) -> None:
+    """Set a program on a Miele appliance."""
+
+    _LOGGER.debug("Set program call: %s", call)
+    entries = await extract_our_config_entry_ids(call)
+    try:
+        config_entry: MieleConfigEntry | None = (
+            call.hass.config_entries.async_get_entry(entries[0])
+        )
+    except IndexError:
+        raise ServiceValidationError(
+            translation_domain=DOMAIN,
+            translation_key="set_program_no_device",
+        ) from None
+    device_reg = dr.async_get(call.hass)
+    api = cast(MieleConfigEntry, config_entry).runtime_data.api
+    for device in call.data[CONF_DEVICE_ID]:
+        device_entry = device_reg.async_get(device)
+
+        data = {PROGRAM_ID: call.data["program_id"]}
+        if "duration" in call.data:
+            data["duration"] = [
+                call.data["duration"] // 60,
+                call.data["duration"] % 60,
+            ]
+        if "temperature" in call.data:
+            data["temperature"] = call.data["temperature"]
+        try:
+            if serial_number := cast(dr.DeviceEntry, device_entry).serial_number:
+                await api.set_program(serial_number, data)
+        except aiohttp.ClientResponseError as ex:
+            raise HomeAssistantError(
+                translation_domain=DOMAIN,
+                translation_key="set_program_error",
+                translation_placeholders={
+                    "status": str(ex.status),
+                    "message": ex.message,
+                },
+            ) from ex
+
+
 async def async_setup_services(hass: HomeAssistant) -> None:
     """Set up services."""
-
-    async def extract_our_config_entry_ids(service_call: ServiceCall):
-        hass = service_call.hass
-        return [
-            entry_id
-            for entry_id in await async_extract_config_entry_ids(hass, service_call)
-            if (entry := hass.config_entries.async_get_entry(entry_id))
-            and entry.domain == DOMAIN
-        ]
-
-    async def set_program(call: ServiceCall):
-        _LOGGER.debug("Set program call: %s", call)
-        entries = await extract_our_config_entry_ids(call)
-        try:
-            config_entry: MieleConfigEntry | None = hass.config_entries.async_get_entry(
-                entries[0]
-            )
-        except IndexError:
-            raise ServiceValidationError(
-                translation_domain=DOMAIN,
-                translation_key="set_program_no_device",
-            ) from None
-        device_reg = dr.async_get(hass)
-        api = cast(MieleConfigEntry, config_entry).runtime_data.api
-        for device in call.data[CONF_DEVICE_ID]:
-            device_entry = device_reg.async_get(device)
-
-            data = {PROGRAM_ID: call.data["program_id"]}
-            if "duration" in call.data:
-                data["duration"] = [
-                    call.data["duration"] // 60,
-                    call.data["duration"] % 60,
-                ]
-            if "temperature" in call.data:
-                data["temperature"] = call.data["temperature"]
-            try:
-                if serial_number := cast(dr.DeviceEntry, device_entry).serial_number:
-                    await api.set_program(serial_number, data)
-            except aiohttp.ClientResponseError as ex:
-                raise HomeAssistantError(
-                    translation_domain=DOMAIN,
-                    translation_key="set_program_error",
-                    translation_placeholders={
-                        "status": str(ex.status),
-                        "message": ex.message,
-                    },
-                ) from ex
 
     hass.services.async_register(DOMAIN, "set_program", set_program, SERVICE_PROGRAM)
