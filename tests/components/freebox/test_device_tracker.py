f2e98cecd68dd2820e77f945467ddd2361008b51
turakamou <62760308+turakamou@users.noreply.github.com>
2025-04-22 23:12:59 +0200

    Update test_device_tracker.py comments

--- tests/components/freebox/test_device_tracker.py
+++ tests/components/freebox/test_device_tracker.py
@@ -36,7 +36,7 @@ async def test_bridge_mode(
     freezer: FrozenDateTimeFactory,
     router_bridge_mode: Mock,
 ) -> None:
-    """Test get_hosts_list invoqued once if freebox into bridge mode."""
+    """Test get_interfaces invoqued once if freebox into bridge mode."""
     await setup_platform(hass, DEVICE_TRACKER_DOMAIN)
 
     assert router_bridge_mode().lan.get_interfaces.call_count == 1
@@ -46,5 +46,5 @@ async def test_bridge_mode(
     async_fire_time_changed(hass)
     await hass.async_block_till_done()
 
-    # If get_hosts_list failed, not called again
+    # If get_interfaces failed, not called again
     assert router_bridge_mode().lan.get_interfaces.call_count == 1
