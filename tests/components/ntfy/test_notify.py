d0af9c723c0bc2c5f89c196297c9efbb56e577a7
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-04-23 14:19:31 +0000

    Add ntfy.publish action

--- tests/components/ntfy/test_notify.py
+++ tests/components/ntfy/test_notify.py
@@ -12,6 +12,7 @@ from aiontfy.exceptions import (
 from freezegun.api import freeze_time
 import pytest
 from syrupy.assertion import SnapshotAssertion
+from yarl import URL
 
 from homeassistant.components.notify import (
     ATTR_MESSAGE,
@@ -19,7 +20,19 @@ from homeassistant.components.notify import (
     DOMAIN as NOTIFY_DOMAIN,
     SERVICE_SEND_MESSAGE,
 )
-from homeassistant.components.ntfy.const import DOMAIN
+from homeassistant.components.ntfy.const import (
+    ATTR_ATTACH,
+    ATTR_CALL,
+    ATTR_CLICK,
+    ATTR_DELAY,
+    ATTR_EMAIL,
+    ATTR_ICON,
+    ATTR_MARKDOWN,
+    ATTR_PRIORITY,
+    ATTR_TAGS,
+    DOMAIN,
+    SERVICE_PUBLISH,
+)
 from homeassistant.config_entries import SOURCE_REAUTH, ConfigEntryState
 from homeassistant.const import ATTR_ENTITY_ID, STATE_UNKNOWN, Platform
 from homeassistant.core import HomeAssistant
@@ -185,3 +198,63 @@ async def test_send_message_reauth_flow(
     assert "context" in flow
     assert flow["context"].get("source") == SOURCE_REAUTH
     assert flow["context"].get("entry_id") == config_entry.entry_id
+
+
+@freeze_time("2025-01-09T12:00:00+00:00")
+async def test_ntfy_publish(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_aiontfy: AsyncMock,
+) -> None:
+    """Test publishing ntfy message via ntfy.publish action."""
+
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    state = hass.states.get("notify.mytopic")
+    assert state
+    assert state.state == STATE_UNKNOWN
+
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_PUBLISH,
+        {
+            ATTR_ENTITY_ID: "notify.mytopic",
+            ATTR_MESSAGE: "Hello",
+            ATTR_TITLE: "World",
+            ATTR_ATTACH: "https://example.org/download.zip",
+            ATTR_CALL: "1234567890",
+            ATTR_CLICK: "https://example.org",
+            ATTR_DELAY: {"days": 1, "seconds": 30},
+            ATTR_EMAIL: "mail@example.org",
+            ATTR_ICON: "https://example.org/logo.png",
+            ATTR_MARKDOWN: True,
+            ATTR_PRIORITY: "5",
+            ATTR_TAGS: ["partying_face", "grin"],
+        },
+        blocking=True,
+    )
+
+    state = hass.states.get("notify.mytopic")
+    assert state
+    assert state.state == "2025-01-09T12:00:00+00:00"
+
+    mock_aiontfy.publish.assert_called_once_with(
+        Message(
+            topic="mytopic",
+            message="Hello",
+            title="World",
+            tags=["partying_face", "grin"],
+            priority=5,
+            click=URL("https://example.org"),
+            attach=URL("https://example.org/download.zip"),
+            markdown=True,
+            icon=URL("https://example.org/logo.png"),
+            delay="1d 30s",
+            email="mail@example.org",
+            call="1234567890",
+        )
+    )
