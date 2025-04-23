4df0615228608e701549f445e3a3815fad4ebb10
Brett <brett.whynot@gmail.com>
2025-04-23 16:03:59 +1000

    test events service

--- tests/components/teslemetry/test_calendar.py
+++ tests/components/teslemetry/test_calendar.py
@@ -6,9 +6,16 @@ from freezegun.api import FrozenDateTimeFactory
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.const import Platform
+from homeassistant.components.calendar import (
+    DOMAIN as CALENDAR_DOMAIN,
+    EVENT_END_DATETIME,
+    EVENT_START_DATETIME,
+    SERVICE_GET_EVENTS,
+)
+from homeassistant.const import ATTR_ENTITY_ID, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
+from homeassistant.util import dt as dt_util
 
 from . import assert_entities, setup_platform
 
@@ -28,3 +35,40 @@ async def test_calandar(
     entry = await setup_platform(hass, [Platform.CALENDAR])
 
     assert_entities(hass, entry.entry_id, entity_registry, snapshot)
+
+
+@pytest.mark.parametrize(
+    "entity_id",
+    [
+        "calendar.test_precondition_schedule",
+        "calendar.test_charging_schedule",
+        "calendar.energy_site_buy_tariff",
+        "calendar.energy_site_sell_tariff",
+    ],
+)
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_calandar_events(
+    hass: HomeAssistant,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+    freezer: FrozenDateTimeFactory,
+    mock_legacy: AsyncMock,
+    entity_id: str,
+) -> None:
+    """Tests that the climate entity is correct."""
+
+    freezer.move_to("2024-01-01 00:00:00+00:00")
+
+    await setup_platform(hass, [Platform.CALENDAR])
+    result = await hass.services.async_call(
+        CALENDAR_DOMAIN,
+        SERVICE_GET_EVENTS,
+        {
+            ATTR_ENTITY_ID: [entity_id],
+            EVENT_START_DATETIME: dt_util.parse_datetime("2024-01-01T00:00:00Z"),
+            EVENT_END_DATETIME: dt_util.parse_datetime("2024-01-07T00:00:00Z"),
+        },
+        blocking=True,
+        return_response=True,
+    )
+    assert result == snapshot()
