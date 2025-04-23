1074713fba8fd184b56a204f8a68eada7b1f4b0c
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-04-23 17:18:42 +0000

    add tests

--- tests/components/ntfy/conftest.py
+++ tests/components/ntfy/conftest.py
@@ -1,10 +1,10 @@
 """Common fixtures for the ntfy tests."""
 
-from collections.abc import Generator
-from datetime import datetime
+from collections.abc import Callable, Generator
+from datetime import UTC, datetime
 from unittest.mock import AsyncMock, MagicMock, patch
 
-from aiontfy import Account, AccountTokenResponse
+from aiontfy import Account, AccountTokenResponse, Event, Notification
 import pytest
 
 from homeassistant.components.ntfy.const import CONF_TOPIC, DOMAIN
@@ -40,6 +40,28 @@ def mock_aiontfy() -> Generator[AsyncMock]:
         client.generate_token.return_value = AccountTokenResponse(
             token="token", last_access=datetime.now()
         )
+
+        async def mock_ws(topics: list[str], callback: Callable[[Notification], None]):
+            callback(
+                Notification(
+                    id="h6Y2hKA5sy0U",
+                    time=datetime(2025, 3, 28, 17, 58, 46, tzinfo=UTC),
+                    expires=datetime(2025, 3, 29, 5, 58, 46, tzinfo=UTC),
+                    event=Event.MESSAGE,
+                    topic="mytopic",
+                    message="Hello",
+                    title="Title",
+                    tags=["octopus"],
+                    priority=3,
+                    click="https://example.com/",
+                    icon="https://example.com/icon.png",
+                    actions=[],
+                    attachment=None,
+                )
+            )
+
+        client.subscribe.side_effect = mock_ws
+
         yield client
 
 
