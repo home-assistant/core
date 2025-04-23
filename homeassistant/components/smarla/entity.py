96a3914feb4e0ed3cb90ae923baa0b5e80d770f8
Robin Lintermann <robin.lintermann@explicatis.com>
2025-04-23 14:41:44 +0000

    Code refactoring

--- homeassistant/components/smarla/entity.py
+++ homeassistant/components/smarla/entity.py
@@ -5,11 +5,13 @@ from pysmarlaapi import Federwiege
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 
-from .const import DOMAIN
+from .const import DEVICE_MODEL_NAME, DOMAIN, MANUFACTURER_NAME
 
 
 class SmarlaBaseEntity(Entity):
     """Common Base Entity class for defining Smarla device."""
+
+    _attr_has_entity_name = True
 
     def __init__(
         self,
@@ -18,12 +20,10 @@ class SmarlaBaseEntity(Entity):
         """Initialise the entity."""
         super().__init__()
 
-        self._attr_has_entity_name = True
-
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, federwiege.serial_number)},
-            name="Smarla",
-            model="Smarla",
-            manufacturer="Swing2Sleep",
+            name=DEVICE_MODEL_NAME,
+            model=DEVICE_MODEL_NAME,
+            manufacturer=MANUFACTURER_NAME,
             serial_number=federwiege.serial_number,
         )
--- tests/components/smarla/test_config_flow.py
+++ tests/components/smarla/test_config_flow.py
@@ -1,7 +1,9 @@
 """Test config flow for Swing2Sleep Smarla integration."""
 
+import json
 from unittest.mock import AsyncMock, patch
 
+from homeassistant import config_entries
 from homeassistant.components.smarla.config_flow import Connection
 from homeassistant.components.smarla.const import DOMAIN
 from homeassistant.const import CONF_ACCESS_TOKEN
@@ -10,9 +12,12 @@ from homeassistant.data_entry_flow import FlowResultType
 
 from tests.common import MockConfigEntry
 
-MOCK_ACCESS_TOKEN = "eyJyZWZyZXNoVG9rZW4iOiJ0ZXN0IiwidG9rZW4iOiJ0ZXN0IiwiZGF0ZUNyZWF0ZWQiOiIyMDI1LTAxLTAxVDIzOjU5OjU5Ljk5OTk5OVoiLCJhcHBJZGVudGlmaWVyIjoiSEEtaG9tZWFzc2lzdGFudHRlc3QiLCJzZXJpYWxOdW1iZXIiOiJBQkNELUFCQ0QiLCJhcHBWZXJzaW9uIjoidW5rbm93biIsImFwcEN1bHR1cmUiOiJkZSJ9"
-MOCK_ACCESS_TOKEN_JSON = '{"refreshToken": "test", "token": "test", "dateCreated": "2025-01-01T23:59:59.999999Z", "appIdentifier": "HA-homeassistanttest", "serialNumber": "ABCD-ABCD", "appVersion": "unknown", "appCulture": "de"}'
-MOCK_SERIAL = "ABCD-ABCD"
+MOCK_ACCESS_TOKEN = "eyJyZWZyZXNoVG9rZW4iOiJ0ZXN0IiwiYXBwSWRlbnRpZmllciI6IkhBLXRlc3QiLCJzZXJpYWxOdW1iZXIiOiJBQkNEIn0="
+MOCK_ACCESS_TOKEN_JSON = {
+    "refreshToken": "test",
+    "appIdentifier": "HA-test",
+    "serialNumber": "ABCD",
+}
 
 
 async def test_show_form(hass: HomeAssistant) -> None:
@@ -30,13 +35,13 @@ async def test_create_entry(hass: HomeAssistant) -> None:
     with patch.object(Connection, "get_token", new=AsyncMock(return_value=True)):
         result = await hass.config_entries.flow.async_init(
             DOMAIN,
-            context={"source": "user"},
+            context={"source": config_entries.SOURCE_USER},
             data={CONF_ACCESS_TOKEN: MOCK_ACCESS_TOKEN},
         )
 
     assert result["type"] == FlowResultType.CREATE_ENTRY
-    assert result["title"] == MOCK_SERIAL
-    assert result["data"] == {CONF_ACCESS_TOKEN: MOCK_ACCESS_TOKEN_JSON}
+    assert result["title"] == MOCK_ACCESS_TOKEN_JSON["serialNumber"]
+    assert result["data"] == {CONF_ACCESS_TOKEN: json.dumps(MOCK_ACCESS_TOKEN_JSON)}
 
 
 async def test_invalid_auth(hass: HomeAssistant) -> None:
@@ -44,7 +49,7 @@ async def test_invalid_auth(hass: HomeAssistant) -> None:
     with patch.object(Connection, "get_token", new=AsyncMock(return_value=None)):
         result = await hass.config_entries.flow.async_init(
             DOMAIN,
-            context={"source": "user"},
+            context={"source": config_entries.SOURCE_USER},
             data={CONF_ACCESS_TOKEN: MOCK_ACCESS_TOKEN},
         )
 
@@ -56,7 +61,7 @@ async def test_invalid_token(hass: HomeAssistant) -> None:
     """Test we handle invalid/malformed tokens."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
-        context={"source": "user"},
+        context={"source": config_entries.SOURCE_USER},
         data={CONF_ACCESS_TOKEN: "invalid_token"},
     )
 
@@ -68,8 +73,8 @@ async def test_device_exists_abort(hass: HomeAssistant) -> None:
     """Test we abort config flow if Smarla device already configured."""
     config_entry = MockConfigEntry(
         domain=DOMAIN,
-        unique_id=MOCK_SERIAL,
-        source="user",
+        unique_id=MOCK_ACCESS_TOKEN_JSON["serialNumber"],
+        source=config_entries.SOURCE_USER,
     )
     config_entry.add_to_hass(hass)
 
@@ -77,7 +82,7 @@ async def test_device_exists_abort(hass: HomeAssistant) -> None:
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN,
-        context={"source": "user"},
+        context={"source": config_entries.SOURCE_USER},
         data={CONF_ACCESS_TOKEN: MOCK_ACCESS_TOKEN},
     )
 
