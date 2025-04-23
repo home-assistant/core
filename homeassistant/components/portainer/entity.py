8192dabf7029c4fb6c0ab1ca7626aa057bde6183
Erwin Douna <e.douna@gmail.com>
2025-04-23 19:03:06 +0000

    Ruff fix

--- homeassistant/components/portainer/entity.py
+++ homeassistant/components/portainer/entity.py
@@ -5,7 +5,6 @@ from pyportainer.models.docker import DockerContainer
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from . import PortainerConfigEntry
 from .const import DEFAULT_NAME, DOMAIN
 from .coordinator import PortainerCoordinator, PortainerCoordinatorData
 
@@ -29,7 +28,9 @@ class PortainerEndpointEntity(PortainerCoordinatorEntity):
         self._device_info = device_info
         self.device_id = self._device_info.endpoint.id
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, f"{coordinator.config_entry.entry_id}_{self.device_id}")},
+            identifiers={
+                (DOMAIN, f"{coordinator.config_entry.entry_id}_{self.device_id}")
+            },
             manufacturer=DEFAULT_NAME,
             model="Endpoint",
             name=self._device_info.endpoint.name,
