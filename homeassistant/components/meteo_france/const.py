2260d9ba984a19dfef399b3649501ab17a34e8ef
Arjan <44190435+vingerha@users.noreply.github.com>
2025-04-24 16:47:42 +0000

    Split mapping of MF API values for daily and hourly

--- homeassistant/components/meteo_france/const.py
+++ homeassistant/components/meteo_france/const.py
@@ -39,7 +39,7 @@ ATTR_NEXT_RAIN_1_HOUR_FORECAST = "1_hour_forecast"
 ATTR_NEXT_RAIN_DT_REF = "forecast_time_ref"
 
 
-CONDITION_CLASSES: dict[str, list[str]] = {
+CONDITION_CLASSES_HOURLY: dict[str, list[str]] = {
     ATTR_CONDITION_CLEAR_NIGHT: ["Nuit Claire", "Nuit claire", "Ciel clair"],
     ATTR_CONDITION_CLOUDY: ["Très nuageux", "Couvert"],
     ATTR_CONDITION_FOG: [
@@ -94,8 +94,68 @@ CONDITION_CLASSES: dict[str, list[str]] = {
     ATTR_CONDITION_WINDY_VARIANT: [],
     ATTR_CONDITION_EXCEPTIONAL: [],
 }
-CONDITION_MAP = {
+CONDITION_CLASSES_DAILY: dict[str, list[str]] = {
+    ATTR_CONDITION_CLEAR_NIGHT: ["Nuit Claire", "Nuit claire"],
+    ATTR_CONDITION_CLOUDY: ["Très nuageux", "Couvert"],
+    ATTR_CONDITION_FOG: [
+        "Brume ou bancs de brouillard",
+        "Brume",
+        "Brouillard",
+        "Brouillard givrant",
+        "Bancs de Brouillard",
+        "Brouillard dense",
+    ],
+    ATTR_CONDITION_HAIL: ["Risque de grêle", "Risque de grèle"],
+    ATTR_CONDITION_LIGHTNING: ["Risque d'orages", "Orages", "Orage avec grêle"],
+    ATTR_CONDITION_LIGHTNING_RAINY: [
+        "Pluie orageuses",
+        "Pluies orageuses",
+        "Averses orageuses",
+    ],
+    ATTR_CONDITION_PARTLYCLOUDY: [
+        "Ciel voilé",
+        "Ciel voilé nuit",
+        "Éclaircies",
+        "Eclaircies",
+        "Peu nuageux",
+        "Variable",
+    ],
+    ATTR_CONDITION_POURING: ["Pluie forte"],
+    ATTR_CONDITION_RAINY: [
+        "Bruine / Pluie faible",
+        "Bruine",
+        "Pluie faible",
+        "Pluies éparses / Rares averses",
+        "Pluies éparses",
+        "Rares averses",
+        "Pluie modérée",
+        "Pluie / Averses",
+        "Averses",
+        "Averses faibles",
+        "Pluie",
+    ],
+    ATTR_CONDITION_SNOWY: [
+        "Neige / Averses de neige",
+        "Neige",
+        "Averses de neige",
+        "Neige forte",
+        "Neige faible",
+        "Averses de neige faible",
+        "Quelques flocons",
+    ],
+    ATTR_CONDITION_SNOWY_RAINY: ["Pluie et neige", "Pluie verglaçante"],
+    ATTR_CONDITION_SUNNY: ["Ensoleillé", "Ciel clair"],
+    ATTR_CONDITION_WINDY: [],
+    ATTR_CONDITION_WINDY_VARIANT: [],
+    ATTR_CONDITION_EXCEPTIONAL: [],
+}
+CONDITION_MAP_HOURLY = {
     cond_code: cond_ha
-    for cond_ha, cond_codes in CONDITION_CLASSES.items()
+    for cond_ha, cond_codes in CONDITION_CLASSES_HOURLY.items()
+    for cond_code in cond_codes
+}
+CONDITION_MAP_DAILY = {
+    cond_code: cond_ha
+    for cond_ha, cond_codes in CONDITION_CLASSES_DAILY.items()
     for cond_code in cond_codes
 }
