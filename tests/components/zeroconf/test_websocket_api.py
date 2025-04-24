1c30ec800d014cace196f1e2977d18e8f38ca59f
J. Nick Koston <nick@koston.org>
2025-04-23 17:18:31 -1000

    cover

--- tests/components/zeroconf/test_websocket_api.py
+++ tests/components/zeroconf/test_websocket_api.py
@@ -1,9 +1,17 @@
-"""The tests for the bluetooth WebSocket API."""
+"""The tests for the zeroconf WebSocket API."""
 
 import asyncio
 import socket
 
-from zeroconf import DNSAddress, DNSPointer, DNSService, DNSText, const
+from zeroconf import (
+    DNSAddress,
+    DNSPointer,
+    DNSService,
+    DNSText,
+    RecordUpdate,
+    const,
+    current_time_millis,
+)
 
 from homeassistant.components.zeroconf import DOMAIN, async_get_async_instance
 from homeassistant.core import HomeAssistant
@@ -21,45 +29,45 @@ async def test_subscribe_discovery(
     instance.zeroconf.cache.async_add_records(
         [
             DNSPointer(
-                "_hap._udp.local.",
+                "_http._tcp.local.",
                 const._TYPE_PTR,
                 const._CLASS_IN,
                 const._DNS_OTHER_TTL,
-                "foo2._hap._udp.local.",
+                "foo2._http._tcp.local.",
             ),
             DNSService(
-                "foo2._hap._udp.local.",
+                "foo2._http._tcp.local.",
                 const._TYPE_SRV,
                 const._CLASS_IN,
                 const._DNS_OTHER_TTL,
                 0,
                 0,
                 1234,
-                "foo.local.",
+                "foo2.local.",
             ),
             DNSAddress(
-                "foo.local.",
+                "foo2.local.",
                 const._TYPE_A,
                 const._CLASS_IN,
                 const._DNS_HOST_TTL,
                 socket.inet_aton("127.0.0.1"),
             ),
             DNSText(
-                "foo.local.",
+                "foo2.local.",
                 const._TYPE_TXT,
                 const._CLASS_IN,
                 const._DNS_HOST_TTL,
                 b"\x13md=HASS Bridge W9DN\x06pv=1.0\x14id=11:8E:DB:5B:5C:C5\x05c#=12\x04s#=1",
             ),
             DNSPointer(
-                "_hap._udp.local.",
+                "_http._tcp.local.",
                 const._TYPE_PTR,
                 const._CLASS_IN,
                 const._DNS_OTHER_TTL,
-                "foo3._hap._udp.local.",
+                "foo3._http._tcp.local.",
             ),
             DNSService(
-                "foo3._hap._udp.local.",
+                "foo3._http._tcp.local.",
                 const._TYPE_SRV,
                 const._CLASS_IN,
                 const._DNS_OTHER_TTL,
@@ -95,25 +103,27 @@ async def test_subscribe_discovery(
         "add": [
             {
                 "ip_addresses": ["127.0.0.1"],
-                "name": "foo2._hap._udp.local.",
+                "name": "foo2._http._tcp.local.",
                 "port": 1234,
                 "properties": {},
-                "type": "_hap._udp.local.",
+                "type": "_http._tcp.local.",
             }
         ]
     }
 
     # now late inject the address record
-    instance.zeroconf.cache.async_add_records(
-        [
-            DNSAddress(
-                "foo.local.",
-                const._TYPE_A,
-                const._CLASS_IN,
-                const._DNS_HOST_TTL,
-                socket.inet_aton("127.0.0.1"),
-            ),
-        ]
+    records = [
+        DNSPointer(
+            "_http._tcp.local.",
+            const._TYPE_PTR,
+            const._CLASS_IN,
+            0,  # TTL = 0
+            "foo2._http._tcp.local.",
+        ),
+    ]
+    instance.zeroconf.cache.async_add_records(records)
+    instance.zeroconf.record_manager.async_updates(
+        current_time_millis(), [RecordUpdate(record, None) for record in records]
     )
     async with asyncio.timeout(1):
         response = await client.receive_json()
@@ -121,10 +131,40 @@ async def test_subscribe_discovery(
         "add": [
             {
                 "ip_addresses": ["127.0.0.1"],
-                "name": "foo3._hap._udp.local.",
+                "name": "foo3._http._tcp.local.",
                 "port": 1234,
                 "properties": {},
-                "type": "_hap._udp.local.",
+                "type": "_http._tcp.local.",
+            }
+        ]
+    }
+
+    # now inject a goodbye
+    records = [
+        DNSPointer(
+            "_http._tcp.local.",
+            const._TYPE_PTR,
+            const._CLASS_IN,
+            0,  # goodbye TTL = 0
+            "foo2._http._tcp.local.",
+        ),
+    ]
+    record_updates = [RecordUpdate(record, record) for record in records]
+    instance.zeroconf.cache.async_add_records(records)
+    instance.zeroconf.record_manager.async_updates(
+        current_time_millis(), record_updates
+    )
+    instance.zeroconf.record_manager.async_updates_complete(False)
+    async with asyncio.timeout(2):
+        response = await client.receive_json()
+    assert response["event"] == {
+        "add": [
+            {
+                "ip_addresses": ["127.0.0.1"],
+                "name": "foo3._http._tcp.local.",
+                "port": 1234,
+                "properties": {},
+                "type": "_http._tcp.local.",
             }
         ]
     }
