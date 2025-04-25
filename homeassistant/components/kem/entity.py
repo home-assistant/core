2fe1d8223bdf6f2d654f3b4fff93fd52eee33e9c
Pete Sage <76050312+PeteRager@users.noreply.github.com>
2025-04-25 12:45:07 -0400

    Update homeassistant/components/kem/entity.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/kem/entity.py
+++ homeassistant/components/kem/entity.py
@@ -51,7 +51,6 @@ class KemEntity(CoordinatorEntity[KemUpdateCoordinator]):
         split_key = self.entity_description.key.split(":")
         self._use_device_key = len(split_key) > 1
         self.key = split_key[1] if self._use_device_key else self.entity_description.key
-        self._attr_translation_key = self.entity_description.translation_key
 
         try:
             mac_address_hex = device_data[DEVICE_DATA_MAC_ADDRESS].replace(":", "")
