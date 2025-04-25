d68d89dccdeb667578a5e0c6701a7f1964e25a9c
ngolf <74095787+ngolf@users.noreply.github.com>
2025-04-25 12:04:40 +0000

    added translation for aquacell's last_update

--- homeassistant/components/aquacell/sensor.py
+++ homeassistant/components/aquacell/sensor.py
@@ -79,6 +79,7 @@ SENSORS: tuple[SoftenerSensorEntityDescription, ...] = (
     ),
     SoftenerSensorEntityDescription(
         key="last_update",
+        translation_key="last_update",
         device_class=SensorDeviceClass.TIMESTAMP,
         value_fn=lambda softener: softener.lastUpdate,  # type: ignore[arg-type, return-value]
     ),
