85951d233507513c9a17a4c87eb844bbc6a72ea1
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-04-26 22:53:14 +0000

    Add reconfigure flow

--- homeassistant/components/ntfy/config_flow.py
+++ homeassistant/components/ntfy/config_flow.py
@@ -90,6 +90,24 @@ STEP_REAUTH_DATA_SCHEMA = vol.Schema(
     }
 )
 
+STEP_RECONFIGURE_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Exclusive(CONF_USERNAME, ATTR_CREDENTIALS): TextSelector(
+            TextSelectorConfig(
+                type=TextSelectorType.TEXT,
+                autocomplete="username",
+            ),
+        ),
+        vol.Optional(CONF_PASSWORD, default=""): TextSelector(
+            TextSelectorConfig(
+                type=TextSelectorType.PASSWORD,
+                autocomplete="current-password",
+            ),
+        ),
+        vol.Exclusive(CONF_TOKEN, ATTR_CREDENTIALS): str,
+    }
+)
+
 STEP_USER_TOPIC_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_TOPIC): str,
@@ -242,6 +260,70 @@ class NtfyConfigFlow(ConfigFlow, domain=DOMAIN):
             ),
             errors=errors,
             description_placeholders={CONF_USERNAME: entry.data[CONF_USERNAME]},
+        )
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle reconfigure flow for ntfy."""
+        errors: dict[str, str] = {}
+
+        entry = self._get_reconfigure_entry()
+
+        if entry.data[CONF_USERNAME]:
+            return self.async_abort(reason="nothing_to_reconfigure")
+
+        if user_input is not None:
+            session = async_get_clientsession(self.hass)
+            if token := user_input.get(CONF_TOKEN):
+                ntfy = Ntfy(
+                    entry.data[CONF_URL],
+                    session,
+                    token=user_input[CONF_TOKEN],
+                )
+            else:
+                ntfy = Ntfy(
+                    entry.data[CONF_URL],
+                    session,
+                    username=user_input[CONF_USERNAME],
+                    password=user_input[CONF_PASSWORD],
+                )
+
+            try:
+                account = await ntfy.account()
+                if not token:
+                    token = (await ntfy.generate_token("Home Assistant")).token
+            except NtfyUnauthorizedAuthenticationError:
+                errors["base"] = "invalid_auth"
+            except NtfyHTTPError as e:
+                _LOGGER.debug("Error %s: %s [%s]", e.code, e.error, e.link)
+                errors["base"] = "cannot_connect"
+            except NtfyException:
+                errors["base"] = "cannot_connect"
+            except Exception:
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                self._async_abort_entries_match(
+                    {
+                        CONF_URL: entry.data[CONF_URL],
+                        CONF_USERNAME: account.username,
+                    }
+                )
+                return self.async_update_reload_and_abort(
+                    entry,
+                    data_updates={
+                        CONF_USERNAME: account.username,
+                        CONF_TOKEN: token,
+                    },
+                )
+        return self.async_show_form(
+            step_id="reconfigure",
+            data_schema=self.add_suggested_values_to_schema(
+                data_schema=STEP_RECONFIGURE_DATA_SCHEMA, suggested_values=user_input
+            ),
+            errors=errors,
+            description_placeholders={CONF_NAME: entry.title},
         )
 
 
--- tests/components/ntfy/test_config_flow.py
+++ tests/components/ntfy/test_config_flow.py
@@ -498,3 +498,205 @@ async def test_flow_reauth_account_mismatch(
 
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "account_mismatch"
+
+
+async def test_flow_reconfigure(
+    hass: HomeAssistant,
+    mock_aiontfy: AsyncMock,
+) -> None:
+    """Test reconfigure flow."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="ntfy.sh",
+        data={
+            CONF_URL: "https://ntfy.sh/",
+            CONF_USERNAME: None,
+            CONF_TOKEN: None,
+        },
+    )
+    mock_aiontfy.generate_token.return_value = AccountTokenResponse(
+        token="newtoken", last_access=datetime.now()
+    )
+    config_entry.add_to_hass(hass)
+    result = await config_entry.start_reconfigure_flow(hass)
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_USERNAME: "username", CONF_PASSWORD: "password"},
+    )
+
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reconfigure_successful"
+    assert config_entry.data[CONF_USERNAME] == "username"
+    assert config_entry.data[CONF_TOKEN] == "newtoken"
+
+    assert len(hass.config_entries.async_entries()) == 1
+
+
+async def test_flow_reconfigure_token(
+    hass: HomeAssistant,
+    mock_aiontfy: AsyncMock,
+) -> None:
+    """Test reconfigure flow with access token."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="ntfy.sh",
+        data={
+            CONF_URL: "https://ntfy.sh/",
+            CONF_USERNAME: None,
+            CONF_TOKEN: None,
+        },
+    )
+
+    config_entry.add_to_hass(hass)
+    result = await config_entry.start_reconfigure_flow(hass)
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_TOKEN: "access_token"},
+    )
+
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reconfigure_successful"
+    assert config_entry.data[CONF_USERNAME] == "username"
+    assert config_entry.data[CONF_TOKEN] == "access_token"
+
+    assert len(hass.config_entries.async_entries()) == 1
+
+
+@pytest.mark.parametrize(
+    ("exception", "error"),
+    [
+        (
+            NtfyHTTPError(418001, 418, "I'm a teapot", ""),
+            "cannot_connect",
+        ),
+        (
+            NtfyUnauthorizedAuthenticationError(
+                40101,
+                401,
+                "unauthorized",
+                "https://ntfy.sh/docs/publish/#authentication",
+            ),
+            "invalid_auth",
+        ),
+        (NtfyException, "cannot_connect"),
+        (TypeError, "unknown"),
+    ],
+)
+async def test_flow_reconfigure_errors(
+    hass: HomeAssistant,
+    mock_aiontfy: AsyncMock,
+    exception: Exception,
+    error: str,
+) -> None:
+    """Test reconfigure flow errors."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="ntfy.sh",
+        data={
+            CONF_URL: "https://ntfy.sh/",
+            CONF_USERNAME: None,
+            CONF_TOKEN: None,
+        },
+    )
+    mock_aiontfy.generate_token.return_value = AccountTokenResponse(
+        token="newtoken", last_access=datetime.now()
+    )
+    mock_aiontfy.account.side_effect = exception
+
+    config_entry.add_to_hass(hass)
+    result = await config_entry.start_reconfigure_flow(hass)
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_USERNAME: "username", CONF_PASSWORD: "password"},
+    )
+
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {"base": error}
+
+    mock_aiontfy.account.side_effect = None
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_USERNAME: "username", CONF_PASSWORD: "password"},
+    )
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reconfigure_successful"
+    assert config_entry.data[CONF_USERNAME] == "username"
+    assert config_entry.data[CONF_TOKEN] == "newtoken"
+
+    assert len(hass.config_entries.async_entries()) == 1
+
+
+@pytest.mark.usefixtures("mock_aiontfy")
+async def test_flow_reconfigure_already_configured(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+) -> None:
+    """Test reconfigure flow already configured."""
+    other_config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="ntfy.sh",
+        data={
+            CONF_URL: "https://ntfy.sh/",
+            CONF_USERNAME: "username",
+        },
+    )
+    other_config_entry.add_to_hass(hass)
+
+    config_entry.add_to_hass(hass)
+    result = await config_entry.start_reconfigure_flow(hass)
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "reconfigure"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {CONF_USERNAME: "username", CONF_PASSWORD: "password"},
+    )
+
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "already_configured"
+
+    assert len(hass.config_entries.async_entries()) == 2
+
+
+@pytest.mark.usefixtures("mock_aiontfy")
+async def test_flow_reconfigure_nothing_to_reconfigure(
+    hass: HomeAssistant,
+) -> None:
+    """Test reconfigure flow abort if nothing to reconfigure."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="ntfy.sh",
+        data={
+            CONF_URL: "https://ntfy.sh/",
+            CONF_USERNAME: "username",
+        },
+    )
+
+    config_entry.add_to_hass(hass)
+    result = await config_entry.start_reconfigure_flow(hass)
+    await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "nothing_to_reconfigure"
+
+    assert len(hass.config_entries.async_entries()) == 1
