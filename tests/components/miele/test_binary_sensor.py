4199c023854a0471ef861c446970ab408a0a4b99
Åke S - Piper <ake@strandberg.eu>
2025-04-28 12:35:53 +0000

    Test that all platforms are loaded

--- tests/components/miele/test_binary_sensor.py
+++ tests/components/miele/test_binary_sensor.py
@@ -1,7 +1,9 @@
 """Tests for miele binary sensor module."""
 
+from datetime import timedelta
 from unittest.mock import MagicMock
 
+from freezegun.api import FrozenDateTimeFactory
 import pytest
 from syrupy import SnapshotAssertion
 
@@ -9,7 +11,7 @@ from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAI
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
-from tests.common import MockConfigEntry, snapshot_platform
+from tests.common import MockConfigEntry, async_fire_time_changed, snapshot_platform
 
 
 @pytest.mark.parametrize("platforms", [(BINARY_SENSOR_DOMAIN,)])
@@ -21,7 +23,15 @@ async def test_binary_sensor_states(
     snapshot: SnapshotAssertion,
     entity_registry: er.EntityRegistry,
     setup_platform: None,
+    freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test binary sensor state."""
 
     await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
+
+    freezer.tick(timedelta(seconds=30))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    assert hass.states.get("binary_sensor.freezer_door").state == "off"
+    assert hass.states.get("binary_sensor.hood_problem").state == "off"
--- tests/components/miele/test_init.py
+++ tests/components/miele/test_init.py
@@ -1,10 +1,12 @@
 """Tests for init module."""
 
+from datetime import timedelta
 import http
 import time
 from unittest.mock import MagicMock
 
 from aiohttp import ClientConnectionError
+from freezegun.api import FrozenDateTimeFactory
 from pymiele import OAUTH2_TOKEN
 import pytest
 from syrupy import SnapshotAssertion
@@ -16,7 +18,7 @@ from homeassistant.helpers import device_registry as dr
 
 from . import setup_integration
 
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, async_fire_time_changed
 from tests.test_util.aiohttp import AiohttpClientMocker
 
 
@@ -118,3 +120,60 @@ async def test_device_info(
     )
     assert device_entry is not None
     assert device_entry == snapshot
+
+
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_setup_platforms(
+    hass: HomeAssistant,
+    mock_miele_client: MagicMock,
+    mock_config_entry: MockConfigEntry,
+    device_registry: dr.DeviceRegistry,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test that all platforms are set up."""
+
+    await setup_integration(hass, mock_config_entry)
+    freezer.tick(timedelta(seconds=30))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    assert hass.states.get("binary_sensor.freezer_door").state == "off"
+    assert hass.states.get("binary_sensor.hood_problem").state == "off"
+
+    assert (
+        hass.states.get("button.washing_machine_start").object_id
+        == "washing_machine_start"
+    )
+
+    assert hass.states.get("climate.freezer_freezer").state == "cool"
+    assert hass.states.get("light.hood_light").state == "on"
+
+    assert hass.states.get("sensor.freezer_temperature").state == "-18.0"
+    assert hass.states.get("sensor.washing_machine").state == "off"
+
+    assert hass.states.get("switch.washing_machine_power").state == "off"
+
+
+@pytest.mark.parametrize(
+    "load_action_file",
+    ["action_freezer.json"],
+    ids=[
+        "freezer",
+    ],
+)
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_setup_climate_platform(
+    hass: HomeAssistant,
+    mock_miele_client: MagicMock,
+    mock_config_entry: MockConfigEntry,
+    device_registry: dr.DeviceRegistry,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test that platforms are set up."""
+
+    await setup_integration(hass, mock_config_entry)
+    freezer.tick(timedelta(seconds=30))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    assert hass.states.get("climate.freezer_freezer").state == "cool"
