56775be7d4a0243d1f2705eb3e1bf3cdfbeb2732
Brian Choromanski <BrianChoromanski@gmail.com>
2025-04-29 19:43:15 -0400

    Linting

--- tests/util/test_dt.py
+++ tests/util/test_dt.py
@@ -297,10 +297,20 @@ def test_parse_time_expression() -> None:
     assert list(range(60)) == dt_util.parse_time_expression(None, 0, 59)
 
     assert list(range(0, 60, 5)) == dt_util.parse_time_expression("/5", 0, 59)
-    
+
     assert dt_util.parse_time_expression("/4", 5, 20) == [8, 12, 16, 20]
     assert dt_util.parse_time_expression("/10", 10, 30) == [10, 20, 30]
-    assert dt_util.parse_time_expression("/3", 4, 29) == [6, 9, 12, 15, 18, 21, 24, 27, 30]
+    assert dt_util.parse_time_expression("/3", 4, 29) == [
+        6,
+        9,
+        12,
+        15,
+        18,
+        21,
+        24,
+        27,
+        30,
+    ]
 
     assert dt_util.parse_time_expression([2, 1, 3], 0, 59) == [1, 2, 3]
 
