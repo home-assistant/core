54a86d218cd8a7891ffc6b023899de29739eabce
Robin Lintermann <robin.lintermann@explicatis.com>
2025-04-30 12:03:30 +0000

    Code refactoring and clean up

--- homeassistant/components/smarla/config_flow.py
+++ homeassistant/components/smarla/config_flow.py
@@ -7,7 +7,7 @@ from typing import Any
 from pysmarlaapi import Connection
 import voluptuous as vol
 
-from homeassistant import config_entries, exceptions
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_ACCESS_TOKEN
 
 from .const import DOMAIN, HOST
@@ -15,68 +15,50 @@ from .const import DOMAIN, HOST
 STEP_USER_DATA_SCHEMA = vol.Schema({CONF_ACCESS_TOKEN: str})
 
 
-class SmarlaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class SmarlaConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Swing2Sleep Smarla."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_PUSH
+
+    async def _handle_token(self, token: str) -> tuple[dict[str, str], dict[str, str]]:
+        """Handle the token input."""
+        errors: dict[str, str] = {}
+        info: dict[str, str] = {}
+
+        try:
+            conn = Connection(url=HOST, token_b64=token)
+        except ValueError:
+            errors["base"] = "invalid_token"
+            return (errors, info)
+
+        if await conn.get_token():
+            info["serial_number"] = conn.token.serialNumber
+            info["token"] = conn.token.get_string()
+        else:
+            errors["base"] = "invalid_auth"
+
+        return (errors, info)
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
-    ) -> config_entries.ConfigFlowResult:
+    ) -> ConfigFlowResult:
         """Handle the initial step."""
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user",
-                data_schema=STEP_USER_DATA_SCHEMA,
-            )
-
         errors: dict[str, str] = {}
 
-        try:
-            info = await self.validate_input(user_input)
-        except InvalidAuth:
-            errors["base"] = "invalid_auth"
-        except InvalidToken:
-            errors["base"] = "invalid_token"
+        if user_input is not None:
+            errors, info = await self._handle_token(token=user_input[CONF_ACCESS_TOKEN])
 
-        if not errors:
-            return self.async_create_entry(
-                title=info["title"],
-                data={CONF_ACCESS_TOKEN: info.get(CONF_ACCESS_TOKEN)},
-            )
+            if not errors:
+                await self.async_set_unique_id(info["serial_number"])
+                self._abort_if_unique_id_configured()
+
+                return self.async_create_entry(
+                    title=info["serial_number"],
+                    data={CONF_ACCESS_TOKEN: info["token"]},
+                )
 
         return self.async_show_form(
             step_id="user",
             data_schema=STEP_USER_DATA_SCHEMA,
             errors=errors,
         )
-
-    async def validate_input(self, data: dict[str, Any]):
-        """Validate the user input allows us to connect.
-
-        Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
-        """
-        try:
-            conn = Connection(url=HOST, token_b64=data[CONF_ACCESS_TOKEN])
-        except ValueError as e:
-            raise InvalidToken from e
-
-        await self.async_set_unique_id(conn.token.serialNumber)
-        self._abort_if_unique_id_configured()
-
-        if not await conn.get_token():
-            raise InvalidAuth
-
-        return {
-            "title": conn.token.serialNumber,
-            CONF_ACCESS_TOKEN: conn.token.get_string(),
-        }
-
-
-class InvalidAuth(exceptions.HomeAssistantError):
-    """Error to indicate there is invalid auth."""
-
-
-class InvalidToken(exceptions.HomeAssistantError):
-    """Error to indicate there is an invalid token."""
