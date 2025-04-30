c562cba030b1b1c2aa59faf583555a10724aee8c
epenet <6771947+epenet@users.noreply.github.com>
2025-04-30 08:48:49 +0200

    Use unique VIN in renault tests (#143925)

--- tests/components/renault/test_init.py
+++ tests/components/renault/test_init.py
@@ -144,7 +144,7 @@ async def test_registry_cleanup(
     """Test being able to remove a disconnected device."""
     assert await async_setup_component(hass, "config", {})
     entry_id = config_entry.entry_id
-    live_id = "VF1AAAAA555777999"
+    live_id = "VF1ZOE40VIN"
     dead_id = "VF1AAAAA555777888"
 
     assert len(dr.async_entries_for_config_entry(device_registry, entry_id)) == 0
@@ -162,7 +162,7 @@ async def test_registry_cleanup(
     await hass.async_block_till_done()
     assert len(dr.async_entries_for_config_entry(device_registry, entry_id)) == 2
 
-    # Try to remove "VF1AAAAA555777999" - fails as it is live
+    # Try to remove "VF1ZOE40VIN" - fails as it is live
     device = device_registry.async_get_device(identifiers={(DOMAIN, live_id)})
     client = await hass_ws_client(hass)
     response = await client.remove_device(device.id, entry_id)
