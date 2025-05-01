7e525a6e47b0de61ce45006dc2f1e67b9a6c58f4
markhannon <mark.hannon@gmail.com>
2025-05-01 21:51:22 +1000

    Update homeassistant/components/zimi/config_flow.py
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/components/zimi/config_flow.py
+++ homeassistant/components/zimi/config_flow.py
@@ -31,7 +31,7 @@ from .const import DEFAULT_PORT, DOMAIN, TITLE
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER_DATA_SCHEMA = vol.Schema(
+STEP_MANUAL_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_HOST): str,
         vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
