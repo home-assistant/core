e5650bff281667872b5dc5db1629362f4d75aba4
tronikos <tronikos@gmail.com>
2025-05-02 07:16:01 +0000

    Don't call async_block_till_done since this can timeout and seems to meant for tests

--- homeassistant/components/opower/coordinator.py
+++ homeassistant/components/opower/coordinator.py
@@ -421,9 +421,6 @@ class OpowerCoordinator(DataUpdateCoordinator[dict[str, Forecast]]):
         for stat_id, stats in processed_stats.items():
             _LOGGER.debug("Applying %d migrated stats for %s", len(stats), stat_id)
             async_add_external_statistics(self.hass, metadata_map[stat_id], stats)
-        # Wait for the migration to finish before continuing
-        # to avoid appending new values to the old statistics.
-        await get_instance(self.hass).async_block_till_done()
 
         ir.async_create_issue(
             self.hass,
@@ -444,6 +441,10 @@ class OpowerCoordinator(DataUpdateCoordinator[dict[str, Forecast]]):
                 ),
             },
         )
+
+        # Wait for the migration to finish before continuing
+        # to avoid appending new values to the old statistics.
+        await get_instance(self.hass).async_block_till_done()
 
     async def _async_get_cost_reads(
         self, account: Account, time_zone_str: str, start_time: float | None = None
