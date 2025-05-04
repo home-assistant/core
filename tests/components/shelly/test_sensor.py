f33f1d54f4573a21395774e406563793c10f677d
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-05-04 14:44:28 +0200

    Better channel name

--- tests/components/shelly/test_sensor.py
+++ tests/components/shelly/test_sensor.py
@@ -677,34 +677,38 @@ async def test_rpc_restored_sleeping_sensor_no_last_state(
 
 
 @pytest.mark.usefixtures("entity_registry_enabled_by_default")
-async def test_rpc_em1_sensors(
+async def test_rpc_energy_meter_1_sensors(
     hass: HomeAssistant, entity_registry: EntityRegistry, mock_rpc_device: Mock
 ) -> None:
     """Test RPC sensors for EM1 component."""
     await init_integration(hass, 2)
 
-    assert (state := hass.states.get("sensor.em0_power"))
+    assert (state := hass.states.get("sensor.energy_meter_0_power"))
     assert state.state == "85.3"
 
-    assert (entry := entity_registry.async_get("sensor.em0_power"))
+    assert (entry := entity_registry.async_get("sensor.energy_meter_0_power"))
     assert entry.unique_id == "123456789ABC-em1:0-power_em1"
 
-    assert (state := hass.states.get("sensor.em1_power"))
+    assert (state := hass.states.get("sensor.energy_meter_1_power"))
     assert state.state == "123.3"
 
-    assert (entry := entity_registry.async_get("sensor.em1_power"))
+    assert (entry := entity_registry.async_get("sensor.energy_meter_1_power"))
     assert entry.unique_id == "123456789ABC-em1:1-power_em1"
 
-    assert (state := hass.states.get("sensor.em0_total_active_energy"))
+    assert (state := hass.states.get("sensor.energy_meter_0_total_active_energy"))
     assert state.state == "123.4564"
 
-    assert (entry := entity_registry.async_get("sensor.em0_total_active_energy"))
+    assert (
+        entry := entity_registry.async_get("sensor.energy_meter_0_total_active_energy")
+    )
     assert entry.unique_id == "123456789ABC-em1data:0-total_act_energy"
 
-    assert (state := hass.states.get("sensor.em1_total_active_energy"))
+    assert (state := hass.states.get("sensor.energy_meter_1_total_active_energy"))
     assert state.state == "987.6543"
 
-    assert (entry := entity_registry.async_get("sensor.em1_total_active_energy"))
+    assert (
+        entry := entity_registry.async_get("sensor.energy_meter_1_total_active_energy")
+    )
     assert entry.unique_id == "123456789ABC-em1data:1-total_act_energy"
 
 
