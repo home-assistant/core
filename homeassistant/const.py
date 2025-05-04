cbc409b0326d6f8af6d01e0ad2f969e1c4fcb15c
Paulus Schoutsen <balloob@gmail.com>
2025-05-04 16:11:29 +0000

    Bump version to 2025.5.0b3

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -25,7 +25,7 @@ if TYPE_CHECKING:
 APPLICATION_NAME: Final = "HomeAssistant"
 MAJOR_VERSION: Final = 2025
 MINOR_VERSION: Final = 5
-PATCH_VERSION: Final = "0b2"
+PATCH_VERSION: Final = "0b3"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"
 REQUIRED_PYTHON_VER: Final[tuple[int, int, int]] = (3, 13, 2)
