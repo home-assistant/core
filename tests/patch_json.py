0f61f8639aa097bf20614fc70bde23f22f52d93d
Erik <erik@montnemery.com>
2025-05-05 15:35:20 +0200

    Fail tests which JSON serialize mocks

---
+++ tests/patch_json.py
@@ -0,0 +1,36 @@
+"""Patch JSON related functions."""
+
+from __future__ import annotations
+
+import functools
+from typing import Any
+
+import orjson
+
+from homeassistant.helpers import json as json_helper
+
+real_json_encoder_default = json_helper.json_encoder_default
+
+mock_objects = []
+
+
+def json_encoder_default(obj: Any) -> Any:
+    """Convert Home Assistant objects.
+
+    Hand other objects to the original method.
+    """
+    if hasattr(obj, "assert_called"):
+        mock_objects.append(obj)
+        raise TypeError(f"Attempting to serialize mock object {obj}")
+    return real_json_encoder_default(obj)
+
+
+json_helper.json_encoder_default = json_encoder_default
+json_helper.json_bytes = functools.partial(
+    orjson.dumps, option=orjson.OPT_NON_STR_KEYS, default=json_encoder_default
+)
+json_helper.json_bytes_sorted = functools.partial(
+    orjson.dumps,
+    option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SORT_KEYS,
+    default=json_encoder_default,
+)
