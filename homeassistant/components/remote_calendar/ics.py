769294d84bc88724b1ab77e87ce8fb5b10e8270e
Allen Porter <allen.porter@gmail.com>
2025-05-05 04:01:06 +0000

    Switch to the ical compat library

--- homeassistant/components/remote_calendar/ics.py
+++ homeassistant/components/remote_calendar/ics.py
@@ -9,6 +9,7 @@ import logging
 
 from ical.calendar import Calendar
 from ical.calendar_stream import IcsCalendarStream
+from ical.compat import enable_compat_mode
 from ical.exceptions import CalendarParseError
 
 from homeassistant.core import HomeAssistant
@@ -20,24 +21,23 @@ class InvalidIcsException(Exception):
     """Exception to indicate that the ICS content is invalid."""
 
 
-def _make_compat(ics: str) -> str:
-    """Make the ICS content compatible with the parser."""
-    # Office 365 returns a calendar with a TZID that is not valid and does not meet
-    # rfc5545. Remove the invalid TZID from the calendar so that it can be parsed correctly.
-    return ics.replace(";TZID=Customized Time Zone", "")
+def _compat_calendar_from_ics(ics: str) -> Calendar:
+    """Parse the ICS content and return a Calendar object.
+
+    This function is called in a separate thread to avoid blocking the event
+    loop while loading packages or parsing the ICS content for large calendars.
+
+    It uses the `enable_compat_mode` context manager to fix known issues with
+    calendar providers that return invalid calendars.
+    """
+    with enable_compat_mode(ics) as compat_ics:
+        return IcsCalendarStream.calendar_from_ics(compat_ics)
 
 
 async def parse_calendar(hass: HomeAssistant, ics: str) -> Calendar:
     """Parse the ICS content and return a Calendar object."""
-
-    ics = _make_compat(ics)
-
-    # calendar_from_ics will dynamically load packages the first time it is called, so we need
-    # to do it in a separate thread to avoid blocking the event loop
     try:
-        return await hass.async_add_executor_job(
-            IcsCalendarStream.calendar_from_ics, ics
-        )
+        return await hass.async_add_executor_job(_compat_calendar_from_ics, ics)
     except CalendarParseError as err:
         _LOGGER.error("Error parsing calendar information: %s", err.message)
         _LOGGER.debug("Additional calendar error detail: %s", str(err.detailed_error))
