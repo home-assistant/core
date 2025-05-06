687c74ee4c380538aa9dae541e35227dd08054b7
epenet <6771947+epenet@users.noreply.github.com>
2025-05-06 12:21:48 +0200

    Remove deprecated freebox reboot service (#144303)

--- homeassistant/components/freebox/__init__.py
+++ homeassistant/components/freebox/__init__.py
@@ -1,22 +1,19 @@
 """Support for Freebox devices (Freebox v6 and Freebox mini 4K)."""
 
 from datetime import timedelta
-import logging
 
 from freebox_api.exceptions import HttpRequestError
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_PORT, EVENT_HOMEASSISTANT_STOP
-from homeassistant.core import Event, HomeAssistant, ServiceCall
+from homeassistant.core import Event, HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.event import async_track_time_interval
 
-from .const import DOMAIN, PLATFORMS, SERVICE_REBOOT
+from .const import DOMAIN, PLATFORMS
 from .router import FreeboxRouter, get_api
 
 SCAN_INTERVAL = timedelta(seconds=30)
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -40,20 +37,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    # Services
-    async def async_reboot(call: ServiceCall) -> None:
-        """Handle reboot service call."""
-        # The Freebox reboot service has been replaced by a
-        # dedicated button entity and marked as deprecated
-        _LOGGER.warning(
-            "The 'freebox.reboot' service is deprecated and "
-            "replaced by a dedicated reboot button entity; please "
-            "use that entity to reboot the freebox instead"
-        )
-        await router.reboot()
-
-    hass.services.async_register(DOMAIN, SERVICE_REBOOT, async_reboot)
-
     async def async_close_connection(event: Event) -> None:
         """Close Freebox connection on HA Stop."""
         await router.close()
@@ -71,6 +54,5 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok:
         router: FreeboxRouter = hass.data[DOMAIN].pop(entry.unique_id)
         await router.close()
-        hass.services.async_remove(DOMAIN, SERVICE_REBOOT)
 
     return unload_ok
