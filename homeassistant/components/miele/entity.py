0ac87135b708300dd1b1a39ddb5bdf62d64887fc
Josef Zweck <josef@zweck.dev>
2025-05-08 21:07:44 +0200

    Update homeassistant/components/miele/entity.py

--- homeassistant/components/miele/entity.py
+++ homeassistant/components/miele/entity.py
@@ -44,7 +44,6 @@ class MieleEntity(CoordinatorEntity[MieleDataUpdateCoordinator]):
     @property
     def device(self) -> MieleDevice:
         """Return the device object."""
-        # print(">>> entity.device: ", self.coordinator.data.devices)
         return self.coordinator.data.devices[self._device_id]
 
     @property
