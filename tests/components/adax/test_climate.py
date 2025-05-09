ad547436a7b5c7576d1ce61fc3611f27419bbd76
Peter Åslund <peter@peteraslund.me>
2025-05-09 23:04:56 +0200

    Resolved merge conflicts and re-implemented test

--- tests/components/adax/test_climate.py
+++ tests/components/adax/test_climate.py
@@ -1,9 +1,11 @@
 """Test Adax climate entity."""
 
+from homeassistant.components.adax.climate import AdaxDevice
 from homeassistant.components.adax.const import SCAN_INTERVAL
 from homeassistant.components.climate import ATTR_CURRENT_TEMPERATURE, HVACMode
 from homeassistant.const import ATTR_TEMPERATURE, STATE_UNAVAILABLE, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_component import async_update_entity
 
 from . import setup_integration
 from .conftest import CLOUD_DEVICE_DATA, LOCAL_DEVICE_DATA
@@ -86,47 +88,40 @@ async def test_climate_local(
 
 
 async def test_climate_set_temperature_to_zero(
-    hass: HomeAssistant, mock_adax_cloud: AsyncMock
+    hass: HomeAssistant,
+    mock_cloud_config_entry: MockConfigEntry,
+    mock_adax_cloud: AsyncMock,
 ) -> None:
     """Test states of the (cloud) Climate entity."""
-    with patch("adax.Adax.update") as mock_adax_update:
-        mock_adax_update.return_value = None
+    mock_adax_cloud.set_room_target_temperature = AsyncMock()
+    mock_adax_cloud.set_room_target_temperature.return_value = None
 
-        with patch("adax.Adax.set_room_target_temperature") as mock_adax_set_temp:
-            mock_adax_set_temp.return_value = None
-            await init_integration(
-                hass,
-                entry_data=CLOUD_CONFIG,
-            )
-            assert len(hass.states.async_entity_ids(Platform.CLIMATE)) == 1
-            entity_id = hass.states.async_entity_ids(Platform.CLIMATE)[0]
+    await setup_integration(hass, mock_cloud_config_entry)
+    assert len(hass.states.async_entity_ids(Platform.CLIMATE)) == 1
+    entity_id = hass.states.async_entity_ids(Platform.CLIMATE)[0]
 
-            device: AdaxDevice = hass.data["domain_entities"][Platform.CLIMATE][
-                entity_id
-            ]
+    device: AdaxDevice = hass.data["domain_entities"][Platform.CLIMATE][entity_id]
 
-            # Assert state before temp change
-            state = hass.states.get(entity_id)
-            assert state
-            assert state.state == HVACMode.HEAT
-            assert (
-                state.attributes[ATTR_TEMPERATURE]
-                == CLOUD_DEVICE_DATA[0]["targetTemperature"]
-            )
+    # Assert state before temp change
+    state = hass.states.get(entity_id)
+    assert state
+    assert state.state == HVACMode.HEAT
+    assert (
+        state.attributes[ATTR_TEMPERATURE] == CLOUD_DEVICE_DATA[0]["targetTemperature"]
+    )
 
-            # Set temperature to 0, which should turn-off the device
-            await device.async_set_temperature(temperature=0)
-            mock_adax_set_temp.assert_called_once_with(
-                device._device_id, device._attr_min_temp, False
-            )
-            CLOUD_DEVICE_DATA[0]["heatingEnabled"] = False
-            CLOUD_DEVICE_DATA[0]["targetTemperature"] = 0
-            mock_adax_cloud.return_value = CLOUD_DEVICE_DATA
+    # Set temperature to 0, which should turn-off the device
+    await device.async_set_temperature(temperature=0)
+    mock_adax_cloud.set_room_target_temperature.assert_called_once_with(
+        device._device_id, device._attr_min_temp, False
+    )
+    CLOUD_DEVICE_DATA[0]["heatingEnabled"] = False
+    CLOUD_DEVICE_DATA[0]["targetTemperature"] = 0
 
-            await async_update_entity(hass, entity_id)
+    await async_update_entity(hass, entity_id)
 
-            # Assert state after temp change
-            state = hass.states.get(entity_id)
-            assert state
-            assert state.state == HVACMode.OFF
-            assert state.attributes[ATTR_TEMPERATURE] == 0
+    # Assert state after temp change
+    state = hass.states.get(entity_id)
+    assert state
+    assert state.state == HVACMode.OFF
+    assert state.attributes[ATTR_TEMPERATURE] == 0
