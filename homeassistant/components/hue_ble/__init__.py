e4b43d7085b2e40fae55cff5a013416d9bad78bf
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2025-05-11 13:05:07 +0000

    Use await

--- homeassistant/components/hue_ble/__init__.py
+++ homeassistant/components/hue_ble/__init__.py
@@ -42,9 +42,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: HueBLEConfigEntry) -> bo
 
     entry.runtime_data = light
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, ["light"])
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["light"])
+
     return True
 
 
