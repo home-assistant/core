08c487ac7ff95ed2bf2499f4caacb8dfb310af48
David Rapan <david@rapan.cz>
2025-05-12 22:23:54 +0200

    refactor: Extract index selection into separate function

--- homeassistant/components/dhcp/__init__.py
+++ homeassistant/components/dhcp/__init__.py
@@ -423,29 +423,32 @@ class DHCPWatcher(WatcherBase):
             response.ip_address, response.hostname, response.mac_address
         )
 
+    async def async_get_adapter_indexes(self) -> list[int] | None:
+        """Get the adapter indexes."""
+        adapters = await network.async_get_adapters(self.hass)
+        if network.async_only_default_interface_enabled(adapters):
+            return None
+        return [
+            adapter["index"]
+            for adapter in adapters
+            if (
+                adapter["enabled"]
+                and adapter["index"] is not None
+                and adapter["ipv4"]
+                and (
+                    addresses := [IPv4Address(ip["address"]) for ip in adapter["ipv4"]]
+                )
+                and any(
+                    ip for ip in addresses if not ip.is_loopback and not ip.is_global
+                )
+            )
+        ]
+
     async def async_start(self) -> None:
         """Start watching for dhcp packets."""
         self._unsub = await aiodhcpwatcher.async_start(
             self._async_process_dhcp_request,
-            [
-                adapter["index"]
-                for adapter in await network.async_get_adapters(self.hass)
-                if (
-                    adapter["enabled"]
-                    and adapter["index"] is not None
-                    and adapter["ipv4"]
-                    and (
-                        addresses := [
-                            IPv4Address(ip["address"]) for ip in adapter["ipv4"]
-                        ]
-                    )
-                    and any(
-                        ip
-                        for ip in addresses
-                        if not ip.is_loopback and not ip.is_global
-                    )
-                )
-            ],
+            await self.async_get_adapter_indexes(),
         )
 
 
