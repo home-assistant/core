a184419708adb86e4ee24457f4891994c5a41731
Erik <erik@montnemery.com>
2025-05-12 11:08:25 +0200

    Make it possible to subscribe to frontend user store

--- homeassistant/components/frontend/storage.py
+++ homeassistant/components/frontend/storage.py
@@ -10,7 +10,7 @@ import voluptuous as vol
 
 from homeassistant.components import websocket_api
 from homeassistant.components.websocket_api import ActiveConnection
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.storage import Store
 from homeassistant.util.hass_dict import HassKey
 
@@ -22,6 +22,7 @@ async def async_setup_frontend_storage(hass: HomeAssistant) -> None:
     """Set up frontend storage."""
     websocket_api.async_register_command(hass, websocket_set_user_data)
     websocket_api.async_register_command(hass, websocket_get_user_data)
+    websocket_api.async_register_command(hass, websocket_subscribe_user_data)
 
 
 async def async_user_store(hass: HomeAssistant, user_id: str) -> UserStore:
@@ -41,6 +42,7 @@ class UserStore:
         """Initialize the user store."""
         self._store = _UserStore(hass, user_id)
         self.data: dict[str, Any] = {}
+        self.subscriptions: dict[str | Any, list[Callable[[], None]]] = {}
 
     async def async_load(self) -> None:
         """Load the data from the store."""
@@ -50,6 +52,23 @@ class UserStore:
         """Set an item item and save the store."""
         self.data[key] = value
         await self._store.async_save(self.data)
+        for cb in self.subscriptions.get(None, []):
+            cb()
+        for cb in self.subscriptions.get(key, []):
+            cb()
+
+    @callback
+    def async_subscribe(
+        self, key: str | None, on_update_callback: Callable[[], None]
+    ) -> Callable[[], None]:
+        """Save the data to the store."""
+        self.subscriptions.setdefault(key, []).append(on_update_callback)
+
+        def unsubscribe() -> None:
+            """Unsubscribe from the store."""
+            self.subscriptions[key].remove(on_update_callback)
+
+        return unsubscribe
 
 
 class _UserStore(Store[dict[str, Any]]):
@@ -124,3 +143,29 @@ async def websocket_get_user_data(
     connection.send_result(
         msg["id"], {"value": data.get(msg["key"]) if "key" in msg else data}
     )
+
+
+@websocket_api.websocket_command(
+    {vol.Required("type"): "frontend/subscribe_user_data", vol.Optional("key"): str}
+)
+@websocket_api.async_response
+@with_user_store
+async def websocket_subscribe_user_data(
+    hass: HomeAssistant,
+    connection: ActiveConnection,
+    msg: dict[str, Any],
+    store: UserStore,
+) -> None:
+    """Handle subscribe to user data command."""
+    key: str | None = msg.get("key")
+
+    def on_data_update() -> None:
+        """Handle user data update."""
+        data = store.data
+        connection.send_event(
+            msg["id"], {"value": data.get(key) if key is not None else data}
+        )
+
+    connection.subscriptions[msg["id"]] = store.async_subscribe(key, on_data_update)
+    on_data_update()
+    connection.send_result(msg["id"])
