e46bf7888a0745e32f3c3613f8076d5665cd9e44
HormCodes <hello@horm.codes>
2025-05-12 18:00:25 +0200

    Improve Airthings test coverage

--- tests/components/airthings/__init__.py
+++ tests/components/airthings/__init__.py
@@ -1,1 +1,9 @@
 """Tests for the Airthings integration."""
+
+from homeassistant.components.airthings import CONF_SECRET
+from homeassistant.const import CONF_ID
+
+TEST_DATA = {
+    CONF_ID: "client_id",
+    CONF_SECRET: "secret",
+}
