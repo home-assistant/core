39085defa1d2153bb07bf78db2159a70f21765ad
Erik <erik@montnemery.com>
2025-05-13 15:29:48 +0200

    Improve support for trigger platforms with multiple triggers

--- homeassistant/components/zwave_js/trigger.py
+++ homeassistant/components/zwave_js/trigger.py
@@ -8,11 +8,17 @@ from homeassistant.helpers.trigger import Trigger
 from .triggers import event, value_updated
 
 TRIGGERS = {
-    event.RELATIVE_PLATFORM_TYPE: event.EventTrigger,
-    value_updated.RELATIVE_PLATFORM_TYPE: value_updated.ValueUpdatedTrigger,
+    event.PLATFORM_TYPE: Trigger(
+        event.async_validate_trigger_config,
+        event.async_attach_trigger,
+    ),
+    value_updated.PLATFORM_TYPE: Trigger(
+        value_updated.async_validate_trigger_config,
+        value_updated.async_attach_trigger,
+    ),
 }
 
 
-async def async_get_triggers(hass: HomeAssistant) -> dict[str, type[Trigger]]:
+async def async_get_triggers(hass: HomeAssistant) -> dict[str, Trigger]:
     """Return the triggers for Z-Wave JS."""
     return TRIGGERS
