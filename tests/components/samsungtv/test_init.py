bd89f13384d83555c2a89f618616b8876dd9741c
epenet <6771947+epenet@users.noreply.github.com>
2025-05-13 18:20:43 +0200

    Add device registry snapshots to samsungtv tests (#144804)
    * Add device registry snapshots to samsungtv tests
    
    * Simplify
    
    * Adjust
    
    * Reduce

--- tests/components/samsungtv/test_init.py
+++ tests/components/samsungtv/test_init.py
@@ -1,43 +1,40 @@
 """Tests for the Samsung TV Integration."""
 
+from typing import Any
 from unittest.mock import AsyncMock, Mock, patch
 
 import pytest
 from samsungtvws.async_remote import SamsungTVWSAsyncRemote
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.components.media_player import (
-    DOMAIN as MP_DOMAIN,
-    MediaPlayerEntityFeature,
-)
+from homeassistant.components.media_player import DOMAIN as MP_DOMAIN
 from homeassistant.components.samsungtv.const import (
     CONF_SESSION_ID,
     CONF_SSDP_MAIN_TV_AGENT_LOCATION,
     CONF_SSDP_RENDERING_CONTROL_LOCATION,
     DOMAIN,
+    METHOD_ENCRYPTED_WEBSOCKET,
+    METHOD_LEGACY,
     METHOD_WEBSOCKET,
     UPNP_SVC_MAIN_TV_AGENT,
     UPNP_SVC_RENDERING_CONTROL,
 )
-from homeassistant.components.samsungtv.media_player import SUPPORT_SAMSUNGTV
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import (
-    ATTR_ENTITY_ID,
-    ATTR_SUPPORTED_FEATURES,
     CONF_HOST,
     CONF_MAC,
     CONF_METHOD,
     CONF_NAME,
     CONF_PORT,
     CONF_TOKEN,
-    SERVICE_VOLUME_UP,
 )
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 from . import setup_samsungtv_entry
 from .const import (
     ENTRYDATA_ENCRYPTED_WEBSOCKET,
+    ENTRYDATA_LEGACY,
     ENTRYDATA_WEBSOCKET,
     MOCK_SSDP_DATA_MAIN_TV_AGENT_ST,
     MOCK_SSDP_DATA_RENDERING_CONTROL_ST,
@@ -54,26 +51,30 @@ MOCK_CONFIG = {
 }
 
 
-@pytest.mark.usefixtures(
-    "remote_websocket", "remote_encrypted_websocket_failing", "rest_api"
+@pytest.mark.parametrize(
+    "entry_data",
+    [ENTRYDATA_LEGACY, ENTRYDATA_ENCRYPTED_WEBSOCKET, ENTRYDATA_WEBSOCKET],
+    ids=[METHOD_LEGACY, METHOD_ENCRYPTED_WEBSOCKET, METHOD_WEBSOCKET],
 )
-async def test_setup(hass: HomeAssistant) -> None:
-    """Test Samsung TV integration is setup."""
-    await setup_samsungtv_entry(hass, MOCK_CONFIG)
-    state = hass.states.get(ENTITY_ID)
+@pytest.mark.usefixtures(
+    "remote_encrypted_websocket",
+    "remote_legacy",
+    "remote_websocket",
+    "rest_api_failing",
+)
+async def test_setup(
+    hass: HomeAssistant,
+    entry_data: dict[str, Any],
+    device_registry: dr.DeviceRegistry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test Samsung TV integration loads and fill device registry."""
+    entry = await setup_samsungtv_entry(hass, entry_data)
 
-    # test name and turn_on
-    assert state
-    assert state.name == "Mock Title"
-    assert (
-        state.attributes[ATTR_SUPPORTED_FEATURES]
-        == SUPPORT_SAMSUNGTV | MediaPlayerEntityFeature.TURN_ON
-    )
+    assert entry.state is ConfigEntryState.LOADED
 
-    # Ensure service is registered
-    await hass.services.async_call(
-        MP_DOMAIN, SERVICE_VOLUME_UP, {ATTR_ENTITY_ID: ENTITY_ID}, True
-    )
+    device_entries = dr.async_entries_for_config_entry(device_registry, entry.entry_id)
+    assert device_entries == snapshot
 
 
 @pytest.mark.usefixtures("remote_websocket", "remote_encrypted_websocket_failing")
