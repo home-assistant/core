3871b82960287dcb4b30f09aad2a6fe62450db7e
Erik <erik@montnemery.com>
2025-05-14 08:05:48 +0200

    Refactor the Trigger class

--- homeassistant/components/zwave_js/triggers/event.py
+++ homeassistant/components/zwave_js/triggers/event.py
@@ -261,24 +261,20 @@ async def async_attach_trigger(
 class EventTrigger(Trigger):
     """Z-Wave JS event trigger."""
 
-    def __init__(self, hass: HomeAssistant, config: ConfigType) -> None:
-        """Initialize trigger."""
-        self._config = config
-        self._hass = hass
-
     @classmethod
-    async def async_validate_config(
+    async def async_validate_trigger_config(
         cls, hass: HomeAssistant, config: ConfigType
     ) -> ConfigType:
         """Validate config."""
         return await async_validate_trigger_config(hass, config)
 
-    async def async_attach(
-        self,
+    @classmethod
+    async def async_attach_trigger(
+        cls,
+        hass: HomeAssistant,
+        config: ConfigType,
         action: TriggerActionType,
         trigger_info: TriggerInfo,
     ) -> CALLBACK_TYPE:
         """Attach a trigger."""
-        return await async_attach_trigger(
-            self._hass, self._config, action, trigger_info
-        )
+        return await async_attach_trigger(hass, config, action, trigger_info)
--- homeassistant/components/zwave_js/triggers/value_updated.py
+++ homeassistant/components/zwave_js/triggers/value_updated.py
@@ -210,24 +210,15 @@ async def async_attach_trigger(
 class ValueUpdatedTrigger(Trigger):
     """Z-Wave JS value updated trigger."""
 
-    def __init__(self, hass: HomeAssistant, config: ConfigType) -> None:
-        """Initialize trigger."""
-        self._config = config
-        self._hass = hass
-
     @classmethod
-    async def async_validate_config(
+    async def async_validate_trigger_config(
         cls, hass: HomeAssistant, config: ConfigType
     ) -> ConfigType:
         """Validate config."""
         return await async_validate_trigger_config(hass, config)
 
-    async def async_attach(
-        self,
-        action: TriggerActionType,
-        trigger_info: TriggerInfo,
-    ) -> CALLBACK_TYPE:
-        """Attach a trigger."""
-        return await async_attach_trigger(
-            self._hass, self._config, action, trigger_info
-        )
+    @classmethod
+    async def async_attach_trigger(
+        cls,
+        hass: HomeAssistant,
+        config: ConfigType,
