20a697935ac1f0537a7978fe0ea08915ff0b244a
David Rapan <david@rapan.cz>
2025-05-14 15:29:14 +0200

    fix: Add test for async_get_last_sensor_data

--- tests/components/starlink/test_init.py
+++ tests/components/starlink/test_init.py
@@ -1,9 +1,13 @@
 """Tests Starlink integration init/unload."""
 
+from datetime import timedelta
+
+from freezegun.api import FrozenDateTimeFactory
+
 from homeassistant.components.starlink.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import CONF_IP_ADDRESS
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, State
 
 from .patchers import (
     HISTORY_STATS_SUCCESS_PATCHER,
@@ -12,14 +16,32 @@ from .patchers import (
     STATUS_DATA_SUCCESS_PATCHER,
 )
 
-from tests.common import MockConfigEntry
+from tests.common import mock_restore_cache_with_extra_data, MockConfigEntry
 
-
-async def test_successful_entry(hass: HomeAssistant) -> None:
+async def test_successful_entry(
+    hass: HomeAssistant, freezer: FrozenDateTimeFactory
+) -> None:
     """Test configuring Starlink."""
     entry = MockConfigEntry(
         domain=DOMAIN,
         data={CONF_IP_ADDRESS: "1.2.3.4:0000"},
+    )
+    entity_id = "sensor.starlink_energy"
+
+    mock_restore_cache_with_extra_data(
+        hass,
+        (
+            (
+                State(
+                    entity_id,
+                    "",
+                ),
+                {
+                    "native_value": 1,
+                    "native_unit_of_measurement": None,
+                },
+            ),
+        ),
     )
 
     with (
@@ -37,6 +59,15 @@ async def test_successful_entry(hass: HomeAssistant) -> None:
         assert entry.runtime_data.data
         assert entry.state is ConfigEntryState.LOADED
 
+        state = hass.states.get(entity_id)
+        assert state.state == str(1)
+
+        freezer.tick(timedelta(minutes=5, seconds=1))
+        await hass.async_block_till_done()
+        await hass.async_block_till_done()
+
+        state = hass.states.get(entity_id)
+        assert state.state == str(1 + 0.007862313684887356)
 
 async def test_unload_entry(hass: HomeAssistant) -> None:
     """Test removing Starlink."""
