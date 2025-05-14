7a18218cf2c91ab865af774d7720ee1b05b5a943
epenet <6771947+epenet@users.noreply.github.com>
2025-05-14 13:35:50 +0000

    Use runtime_data in homematicip_cloud

--- homeassistant/components/homematicip_cloud/hap.py
+++ homeassistant/components/homematicip_cloud/hap.py
@@ -25,6 +25,8 @@ from .errors import HmipcConnectionError
 
 _LOGGER = logging.getLogger(__name__)
 
+type HomematicIPConfigEntry = ConfigEntry[HomematicipHAP]
+
 
 async def build_context_async(
     hass: HomeAssistant, hapid: str | None, authtoken: str | None
@@ -102,7 +104,9 @@ class HomematicipHAP:
 
     home: AsyncHome
 
-    def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    def __init__(
+        self, hass: HomeAssistant, config_entry: HomematicIPConfigEntry
+    ) -> None:
         """Initialize HomematicIP Cloud connection."""
         self.hass = hass
         self.config_entry = config_entry
@@ -133,6 +137,7 @@ class HomematicipHAP:
             "Connected to HomematicIP with HAP %s", self.config_entry.unique_id
         )
 
+        self.config_entry.runtime_data = self
         await self.hass.config_entries.async_forward_entry_setups(
             self.config_entry, PLATFORMS
         )
