9384859c513103bacb46c8047ffe70b9af0824db
Alistair Francis <alistair@alistair23.me>
2025-05-14 21:27:18 +1000

    husqvarna_automower_ble: Simplify the config flow
    Simplify the config flow so that we follow the same method for both
    Bluetooth discovery and manual setup.
    
    This should address:
    https://github.com/home-assistant/core/pull/135440#discussion_r2026933525
    
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- tests/components/husqvarna_automower_ble/test_config_flow.py
+++ tests/components/husqvarna_automower_ble/test_config_flow.py
@@ -94,7 +94,7 @@ async def test_user_selection_incorrect_pin(
     )
 
     assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "reauth_confirm"
+    assert result["step_id"] == "user"
 
     mock_automower_client.connect.return_value = True
 
@@ -123,6 +123,50 @@ async def test_bluetooth(hass: HomeAssistant) -> None:
 
     result = hass.config_entries.flow.async_progress_by_handler(DOMAIN)[0]
     assert result["step_id"] == "bluetooth_confirm"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={CONF_PIN: "1234"},
+    )
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == "Husqvarna Automower"
+    assert result["result"].unique_id == "00000000-0000-0000-0000-000000000003"
+
+    assert result["data"] == {
+        CONF_ADDRESS: "00000000-0000-0000-0000-000000000003",
+        CONF_CLIENT_ID: 1197489078,
+        CONF_PIN: "1234",
+    }
+
+
+async def test_bluetooth_incorrect_pin(
+    hass: HomeAssistant,
+    mock_automower_client: Mock,
+) -> None:
+    """Test we can select a device."""
+
+    await hass.async_block_till_done(wait_background_tasks=True)
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_BLUETOOTH},
+        data=AUTOMOWER_SERVICE_INFO,
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "bluetooth_confirm"
+
+    mock_automower_client.connect.return_value = False
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={CONF_PIN: "5678"},
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "bluetooth_confirm"
+
+    mock_automower_client.connect.return_value = True
 
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"],
