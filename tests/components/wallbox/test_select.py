3358973d373f792cb3ac5fb849fad33fa52409af
Joris Drenth <joris@jorisdrenth.nl>
2025-05-15 20:19:01 +0000

    Update Wallbox select tests

--- tests/components/wallbox/test_select.py
+++ tests/components/wallbox/test_select.py
@@ -33,14 +33,9 @@ TEST_OPTIONS = [
 ]
 
 
-@pytest.mark.parametrize(("mode", "response"), TEST_OPTIONS)
-async def test_wallbox_select_solar_charging_class(
-    hass: HomeAssistant, entry: MockConfigEntry, mode, response
-) -> None:
-    """Test wallbox select class."""
-
-    await setup_integration_select(hass, entry, response)
-
+@pytest.fixture
+def mock_wallbox_patches():
+    """Fixture to patch Wallbox methods."""
     with (
         patch(
             "homeassistant.components.wallbox.Wallbox.authenticate",
@@ -55,18 +50,29 @@ async def test_wallbox_select_solar_charging_class(
             new=Mock(return_value={CHARGER_STATUS_ID_KEY: 193}),
         ),
     ):
-        await hass.services.async_call(
-            SELECT_DOMAIN,
-            SERVICE_SELECT_OPTION,
-            {
-                ATTR_ENTITY_ID: MOCK_SELECT_ENTITY_ID,
-                ATTR_OPTION: mode,
-            },
-            blocking=True,
-        )
+        yield
 
-        state = hass.states.get(MOCK_SELECT_ENTITY_ID)
-        assert state.state == mode
+
+@pytest.mark.parametrize(("mode", "response"), TEST_OPTIONS)
+async def test_wallbox_select_solar_charging_class(
+    hass: HomeAssistant, entry: MockConfigEntry, mode, response, mock_wallbox_patches
+) -> None:
+    """Test wallbox select class."""
+
+    await setup_integration_select(hass, entry, response)
+
+    await hass.services.async_call(
+        SELECT_DOMAIN,
+        SERVICE_SELECT_OPTION,
+        {
+            ATTR_ENTITY_ID: MOCK_SELECT_ENTITY_ID,
+            ATTR_OPTION: mode,
+        },
+        blocking=True,
+    )
+
+    state = hass.states.get(MOCK_SELECT_ENTITY_ID)
+    assert state.state == mode
 
 
 async def test_wallbox_select_no_power_boost_class(
@@ -81,8 +87,9 @@ async def test_wallbox_select_no_power_boost_class(
 
 
 @pytest.mark.parametrize(("mode", "response"), TEST_OPTIONS)
-async def test_wallbox_select_class_connection_error(
-    hass: HomeAssistant, entry: MockConfigEntry, mode, response
+@pytest.mark.parametrize("error", [http_404_error, ConnectionError])
+async def test_wallbox_select_class_error(
+    hass: HomeAssistant, entry: MockConfigEntry, mode, response, error
 ) -> None:
     """Test wallbox select class connection error."""
 
@@ -95,45 +102,11 @@ async def test_wallbox_select_class_connection_error(
         ),
         patch(
             "homeassistant.components.wallbox.Wallbox.disableEcoSmart",
-            new=Mock(side_effect=http_404_error),
+            new=Mock(side_effect=error),
         ),
         patch(
             "homeassistant.components.wallbox.Wallbox.enableEcoSmart",
-            new=Mock(side_effect=http_404_error),
-        ),
-        pytest.raises(HomeAssistantError, match="Error communicating with Wallbox API"),
-    ):
-        await hass.services.async_call(
-            SELECT_DOMAIN,
-            SERVICE_SELECT_OPTION,
-            {
-                ATTR_ENTITY_ID: MOCK_SELECT_ENTITY_ID,
-                ATTR_OPTION: mode,
-            },
-            blocking=True,
-        )
-
-
-@pytest.mark.parametrize(("mode", "response"), TEST_OPTIONS)
-async def test_wallbox_select_class_authentication_error(
-    hass: HomeAssistant, entry: MockConfigEntry, mode, response
-) -> None:
-    """Test wallbox select class authentication error."""
-
-    await setup_integration_select(hass, entry, response)
-
-    with (
-        patch(
-            "homeassistant.components.wallbox.Wallbox.authenticate",
-            new=Mock(return_value=authorisation_response),
-        ),
-        patch(
-            "homeassistant.components.wallbox.Wallbox.disableEcoSmart",
-            new=Mock(side_effect=ConnectionError),
-        ),
-        patch(
-            "homeassistant.components.wallbox.Wallbox.enableEcoSmart",
-            new=Mock(side_effect=ConnectionError),
+            new=Mock(side_effect=error),
         ),
         pytest.raises(HomeAssistantError, match="Error communicating with Wallbox API"),
     ):
