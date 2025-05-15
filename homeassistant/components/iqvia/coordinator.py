f2a3a5cbbd8e08dc8372d2fa94689eab3494a205
epenet <6771947+epenet@users.noreply.github.com>
2025-05-15 15:50:46 +0200

    Move iqvia coordinator to separate module (#144969)
    * Move iqvia coordinator to separate module
    
    * Adjust

---
+++ homeassistant/components/iqvia/coordinator.py
@@ -0,0 +1,47 @@
+"""Support for IQVIA."""
+
+from __future__ import annotations
+
+from collections.abc import Callable, Coroutine
+from datetime import timedelta
+from typing import Any
+
+from pyiqvia.errors import IQVIAError
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import LOGGER
+
+DEFAULT_SCAN_INTERVAL = timedelta(minutes=30)
+
+
+class IqviaUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
+    """Custom DataUpdateCoordinator for IQVIA."""
+
+    config_entry: ConfigEntry
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        config_entry: ConfigEntry,
+        name: str,
+        update_method: Callable[[], Coroutine[Any, Any, dict[str, Any]]],
+    ) -> None:
+        """Initialize the coordinator."""
+        super().__init__(
+            hass,
+            LOGGER,
+            name=name,
+            config_entry=config_entry,
+            update_interval=DEFAULT_SCAN_INTERVAL,
+        )
+        self._update_method = update_method
+
+    async def _async_update_data(self) -> dict[str, Any]:
+        """Fetch data from the API."""
+        try:
+            return await self._update_method()
+        except IQVIAError as err:
+            raise UpdateFailed from err
