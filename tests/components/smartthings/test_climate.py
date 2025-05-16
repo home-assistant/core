775eca23d4ddc3390a6e25b40467bcd6236accc5
Joost Lekkerkerker <joostlek@outlook.com>
2025-05-16 20:14:41 +0200

    Map SmartThings auto mode correctly (#145061)

--- tests/components/smartthings/test_climate.py
+++ tests/components/smartthings/test_climate.py
@@ -119,7 +119,7 @@ async def test_ac_set_hvac_mode_off(
 @pytest.mark.parametrize(
     ("hvac_mode", "argument"),
     [
-        (HVACMode.HEAT_COOL, "auto"),
+        (HVACMode.AUTO, "auto"),
         (HVACMode.COOL, "cool"),
         (HVACMode.DRY, "dry"),
         (HVACMode.HEAT, "heat"),
@@ -174,7 +174,7 @@ async def test_ac_set_hvac_mode_turns_on(
         SERVICE_SET_HVAC_MODE,
         {
             ATTR_ENTITY_ID: "climate.ac_office_granit",
-            ATTR_HVAC_MODE: HVACMode.HEAT_COOL,
+            ATTR_HVAC_MODE: HVACMode.AUTO,
         },
         blocking=True,
     )
@@ -266,7 +266,7 @@ async def test_ac_set_temperature_and_hvac_mode_while_off(
         {
             ATTR_ENTITY_ID: "climate.ac_office_granit",
             ATTR_TEMPERATURE: 23,
-            ATTR_HVAC_MODE: HVACMode.HEAT_COOL,
+            ATTR_HVAC_MODE: HVACMode.AUTO,
         },
         blocking=True,
     )
@@ -316,7 +316,7 @@ async def test_ac_set_temperature_and_hvac_mode(
         {
             ATTR_ENTITY_ID: "climate.ac_office_granit",
             ATTR_TEMPERATURE: 23,
-            ATTR_HVAC_MODE: HVACMode.HEAT_COOL,
+            ATTR_HVAC_MODE: HVACMode.AUTO,
         },
         blocking=True,
     )
@@ -623,7 +623,7 @@ async def test_thermostat_set_hvac_mode(
     await hass.services.async_call(
         CLIMATE_DOMAIN,
         SERVICE_SET_HVAC_MODE,
-        {ATTR_ENTITY_ID: "climate.asd", ATTR_HVAC_MODE: HVACMode.HEAT_COOL},
+        {ATTR_ENTITY_ID: "climate.asd", ATTR_HVAC_MODE: HVACMode.AUTO},
         blocking=True,
     )
     devices.execute_device_command.assert_called_once_with(
