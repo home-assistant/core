a9656ad2178e889d54b36e8d517849d74536b5a4
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-05-17 16:41:16 +0200

    More tests

--- tests/components/shelly/test_devices.py
+++ tests/components/shelly/test_devices.py
@@ -22,7 +22,11 @@ async def test_shelly_2pm_gen3_no_relay_names(
     device_registry: DeviceRegistry,
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
-    """Test Shelly 2PM Gen3 without relay names."""
+    """Test Shelly 2PM Gen3 without relay names.
+
+    This device has two relays/channels,we should get a main device and two sub
+    devices.
+    """
     device_fixture = load_json_object_fixture("2pm_gen3.json", DOMAIN)
     monkeypatch.setattr(mock_rpc_device, "shelly", device_fixture["shelly"])
     monkeypatch.setattr(mock_rpc_device, "status", device_fixture["status"])
@@ -166,6 +170,117 @@ async def test_shelly_2pm_gen3_relay_names(
     assert device_entry.name == "Living room light"
 
     # Main device
+    entity_id = "update.test_name_firmware"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Test name"
+
+
+async def test_shelly_2pm_gen3_cover(
+    hass: HomeAssistant,
+    mock_rpc_device: Mock,
+    entity_registry: EntityRegistry,
+    device_registry: DeviceRegistry,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Test Shelly 2PM Gen3 with cover profile.
+
+    With the cover profile we should only get the main device and no subdevices.
+    """
+    device_fixture = load_json_object_fixture("2pm_gen3_cover.json", DOMAIN)
+    monkeypatch.setattr(mock_rpc_device, "shelly", device_fixture["shelly"])
+    monkeypatch.setattr(mock_rpc_device, "status", device_fixture["status"])
+    monkeypatch.setattr(mock_rpc_device, "config", device_fixture["config"])
+
+    await init_integration(hass, gen=3, model=MODEL_2PM_G3)
+
+    entity_id = "cover.test_name"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Test name"
+
+    entity_id = "sensor.test_name_power"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Test name"
+
+    entity_id = "update.test_name_firmware"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Test name"
+
+
+async def test_shelly_2pm_gen3_cover_with_name(
+    hass: HomeAssistant,
+    mock_rpc_device: Mock,
+    entity_registry: EntityRegistry,
+    device_registry: DeviceRegistry,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Test Shelly 2PM Gen3 with cover profile and the cover name.
+
+    With the cover profile we should only get the main device and no subdevices.
+    """
+    device_fixture = load_json_object_fixture("2pm_gen3_cover.json", DOMAIN)
+    device_fixture["config"]["cover:0"]["name"] = "Bedroom blinds"
+    monkeypatch.setattr(mock_rpc_device, "shelly", device_fixture["shelly"])
+    monkeypatch.setattr(mock_rpc_device, "status", device_fixture["status"])
+    monkeypatch.setattr(mock_rpc_device, "config", device_fixture["config"])
+
+    await init_integration(hass, gen=3, model=MODEL_2PM_G3)
+
+    entity_id = "cover.test_name_bedroom_blinds"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Test name"
+
+    entity_id = "sensor.test_name_bedroom_blinds_power"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Test name"
+
     entity_id = "update.test_name_firmware"
 
     state = hass.states.get(entity_id)
