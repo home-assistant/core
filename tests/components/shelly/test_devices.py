ae580faa0d9c06eb6e72028857f23aa8ee038549
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-05-17 16:27:34 +0200

    Add tests for 2PM Gen3

---
+++ tests/components/shelly/test_devices.py
@@ -0,0 +1,175 @@
+"""Test real devices."""
+
+from unittest.mock import Mock
+
+from aioshelly.const import MODEL_2PM_G3
+import pytest
+
+from homeassistant.components.shelly.const import DOMAIN
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceRegistry
+from homeassistant.helpers.entity_registry import EntityRegistry
+
+from . import init_integration
+
+from tests.common import load_json_object_fixture
+
+
+async def test_shelly_2pm_gen3_no_relay_names(
+    hass: HomeAssistant,
+    mock_rpc_device: Mock,
+    entity_registry: EntityRegistry,
+    device_registry: DeviceRegistry,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Test Shelly 2PM Gen3 without relay names."""
+    device_fixture = load_json_object_fixture("2pm_gen3.json", DOMAIN)
+    monkeypatch.setattr(mock_rpc_device, "shelly", device_fixture["shelly"])
+    monkeypatch.setattr(mock_rpc_device, "status", device_fixture["status"])
+    monkeypatch.setattr(mock_rpc_device, "config", device_fixture["config"])
+
+    await init_integration(hass, gen=3, model=MODEL_2PM_G3)
+
+    # Relay 0 sub-device
+    entity_id = "switch.switch_0"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Switch 0"
+
+    entity_id = "sensor.switch_0_power"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Switch 0"
+
+    # Relay 1 sub-device
+    entity_id = "switch.switch_1"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Switch 1"
+
+    entity_id = "sensor.switch_1_power"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Switch 1"
+
+    # Main device
+    entity_id = "update.test_name_firmware"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Test name"
+
+
+async def test_shelly_2pm_gen3_relay_names(
+    hass: HomeAssistant,
+    mock_rpc_device: Mock,
+    entity_registry: EntityRegistry,
+    device_registry: DeviceRegistry,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Test Shelly 2PM Gen3 with relay names."""
+    device_fixture = load_json_object_fixture("2pm_gen3.json", DOMAIN)
+    device_fixture["config"]["switch:0"]["name"] = "Kitchen light"
+    device_fixture["config"]["switch:1"]["name"] = "Living room light"
+    monkeypatch.setattr(mock_rpc_device, "shelly", device_fixture["shelly"])
+    monkeypatch.setattr(mock_rpc_device, "status", device_fixture["status"])
+    monkeypatch.setattr(mock_rpc_device, "config", device_fixture["config"])
+
+    await init_integration(hass, gen=3, model=MODEL_2PM_G3)
+
+    # Relay 0 sub-device
+    entity_id = "switch.kitchen_light"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Kitchen light"
+
+    entity_id = "sensor.kitchen_light_power"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Kitchen light"
+
+    # Relay 1 sub-device
+    entity_id = "switch.living_room_light"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Living room light"
+
+    entity_id = "sensor.living_room_light_power"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Living room light"
+
+    # Main device
+    entity_id = "update.test_name_firmware"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Test name"
