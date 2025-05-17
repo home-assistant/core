c370dca44ea590f9c439b4b03c3d1876f65cded4
Andrea Turri <andrea.turri92@gmail.com>
2025-05-17 08:33:05 +0000

    add restore test

--- tests/components/miele/test_sensor.py
+++ tests/components/miele/test_sensor.py
@@ -5,13 +5,14 @@ from unittest.mock import MagicMock
 import pytest
 from syrupy import SnapshotAssertion
 
+from homeassistant.components.miele.const import DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
 from .mocks import mock_sensor_transitions
 
-from tests.common import MockConfigEntry, snapshot_platform
+from tests.common import MockConfigEntry, load_json_object_fixture, snapshot_platform
 
 
 @pytest.mark.parametrize("platforms", [(SENSOR_DOMAIN,)])
@@ -55,11 +56,13 @@ async def test_hob_sensor_states(
                 "laundry_scenario/002_washing.json",
                 "laundry_scenario/003_rinse_hold.json",
                 "laundry_scenario/004_wash_end.json",
+                "laundry_scenario/005_drying.json",  # washer remains programmed while starting dryer
             ],
             {
-                "": ["off", "in_use", "rinse_hold", "program_ended"],
+                "": ["off", "in_use", "rinse_hold", "program_ended", "programmed"],
                 "program": [
                     "no_program",
+                    "minimum_iron",
                     "minimum_iron",
                     "minimum_iron",
                     "minimum_iron",
@@ -69,29 +72,33 @@ async def test_hob_sensor_states(
                     "main_wash",
                     "rinse_hold",
                     "anti_crease",
+                    "not_running",
                 ],
-                # "target_temperature": ["unknown", "30", "30", "30"],
-                "spin_speed": ["unknown", "1200", "1200", "1200"],
-                "remaining_time": ["0", "105", "8", "0"],
+                # "target_temperature": ["unknown", "30", "30", "30", "40"],
+                "spin_speed": ["unknown", "1200", "1200", "1200", "1200"],
+                "remaining_time": ["0", "105", "8", "0", "119"],
                 # OFF -> elapsed forced to 0 (some devices continue reporting last value of last cycle)
                 # IN_USE -> elapsed time from API (normal case)
                 # PROGRAM_ENDED -> elapsed time kept from last program (some devices immediately go to 0)
-                "elapsed_time": ["0", "12", "109", "109"],
+                # PROGRAMMED -> elapsed time from API (normal case)
+                "elapsed_time": ["0", "12", "109", "109", "0"],
             },
         ),
         (
             "tumble_dryer",
-            [],
+            [
+                "laundry_scenario/005_drying.json",
+            ],
             {
-                "": ["off"],
-                "program": ["no_program"],
-                "program_phase": ["not_running"],
-                "drying_step": ["unknown"],
-                "remaining_time": ["0"],
+                "": ["off", "in_use"],
+                "program": ["no_program", "minimum_iron"],
+                "program_phase": ["not_running", "drying"],
+                "drying_step": ["unknown", "normal"],
+                "remaining_time": ["0", "49"],
                 # OFF -> elapsed forced to 0 (some devices continue reporting last value of last cycle)
                 # IN_USE -> elapsed time from API (normal case)
                 # PROGRAM_ENDED -> elapsed time kept from last program (some devices immediately go to 0)
-                "elapsed_time": ["0"],
+                "elapsed_time": ["0", "20"],
             },
         ),
     ],
@@ -116,3 +123,37 @@ async def test_laundry_scenario(
         json_sequence,
         expected_sensor_states,
     )
+
+
+@pytest.mark.parametrize("load_device_file", ["laundry_scenario/003_rinse_hold.json"])
+@pytest.mark.parametrize("platforms", [(SENSOR_DOMAIN,)])
+async def test_elapsed_time_sensor_restored(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_miele_client: MagicMock,
+    setup_platform: None,
+) -> None:
+    """Test that elapsed time returns the restored value when program ended."""
+
+    entity_id = "sensor.washing_machine_elapsed_time"
+
+    assert hass.states.get(entity_id).state == "109"
+
+    # load device when status is PROGRAM_ENDED and elapsed time reported by API is 0
+    mock_miele_client.get_devices.return_value = load_json_object_fixture(
+        "laundry_scenario/004_wash_end.json", DOMAIN
+    )
+
+    # unload config entry and reload to make sure that the state is restored
+    await hass.config_entries.async_unload(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert hass.states.get(entity_id).state == "unavailable"
+
+    await hass.config_entries.async_reload(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    # check that elapsed time is the one restored and not the value reported by API (0)
+    state = hass.states.get(entity_id)
+    assert state is not None
+    assert state.state == "109"
