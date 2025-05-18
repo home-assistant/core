0661fb99b508fbe50bf4acbd999e342fe63e1123
Federico Gentile <gentfede+git@gmail.com>
2025-05-18 21:16:19 +0000

    Define icons in icons.json, remove excessive logging

--- homeassistant/components/leneda/const.py
+++ homeassistant/components/leneda/const.py
@@ -19,105 +19,88 @@ SENSOR_TYPES = {
     # Electricity Consumption
     "electricity_consumption_active": {
         "obis_code": ObisCode.ELEC_CONSUMPTION_ACTIVE,
-        "icon": "mdi:lightning-bolt",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_consumption_reactive": {
         "obis_code": ObisCode.ELEC_CONSUMPTION_REACTIVE,
-        "icon": "mdi:lightning-bolt",
         "device_class": None,  # Ideally SensorDeviceClass.REACTIVE_POWER per hour - not supported by Home Assistant
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_consumption_covered_layer1": {
         "obis_code": ObisCode.ELEC_CONSUMPTION_COVERED_LAYER1,
-        "icon": "mdi:lightning-bolt",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_consumption_covered_layer2": {
         "obis_code": ObisCode.ELEC_CONSUMPTION_COVERED_LAYER2,
-        "icon": "mdi:lightning-bolt",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_consumption_covered_layer3": {
         "obis_code": ObisCode.ELEC_CONSUMPTION_COVERED_LAYER3,
-        "icon": "mdi:lightning-bolt",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_consumption_covered_layer4": {
         "obis_code": ObisCode.ELEC_CONSUMPTION_COVERED_LAYER4,
-        "icon": "mdi:lightning-bolt",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_consumption_remaining": {
         "obis_code": ObisCode.ELEC_CONSUMPTION_REMAINING,
-        "icon": "mdi:lightning-bolt",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     # Electricity Production
     "electricity_production_active": {
         "obis_code": ObisCode.ELEC_PRODUCTION_ACTIVE,
-        "icon": "mdi:solar-power",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_production_reactive": {
         "obis_code": ObisCode.ELEC_PRODUCTION_REACTIVE,
-        "icon": "mdi:solar-power",
         "device_class": None,  # Ideally SensorDeviceClass.REACTIVE_POWER per hour - not supported by Home Assistant
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_production_shared_layer1": {
         "obis_code": ObisCode.ELEC_PRODUCTION_SHARED_LAYER1,
-        "icon": "mdi:solar-power",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_production_shared_layer2": {
         "obis_code": ObisCode.ELEC_PRODUCTION_SHARED_LAYER2,
-        "icon": "mdi:solar-power",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_production_shared_layer3": {
         "obis_code": ObisCode.ELEC_PRODUCTION_SHARED_LAYER3,
-        "icon": "mdi:solar-power",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_production_shared_layer4": {
         "obis_code": ObisCode.ELEC_PRODUCTION_SHARED_LAYER4,
-        "icon": "mdi:solar-power",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "electricity_production_remaining": {
         "obis_code": ObisCode.ELEC_PRODUCTION_REMAINING,
-        "icon": "mdi:solar-power",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     # Gas Consumption
     "gas_consumption_volume": {
         "obis_code": ObisCode.GAS_CONSUMPTION_VOLUME,
-        "icon": "mdi:fire",
         "device_class": SensorDeviceClass.GAS,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "gas_consumption_standard_volume": {
         "obis_code": ObisCode.GAS_CONSUMPTION_STANDARD_VOLUME,
-        "icon": "mdi:fire",
         "device_class": None,  # Ideally SensorDeviceClass.GAS, but Nm3 not supported by Hass
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
     "gas_consumption_energy": {
         "obis_code": ObisCode.GAS_CONSUMPTION_ENERGY,
-        "icon": "mdi:fire",
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
     },
