861dd3895128f7f0e8587d1ad8878a8089329c06
Michael <35783820+mib1185@users.noreply.github.com>
2025-05-18 21:28:15 +0200

    Add Immich integration (#145125)
    * add immich integration
    
    * bump aioimmich==0.3.1
    
    * rework to require an url as input and pare it afterwards
    
    * fix doc strings
    
    * remove name attribute from deviceinfo as it is default behaviour
    
    * add translated uom for count sensors
    
    * explicitly pass in the config_entry in coordinator
    
    * fix url in strings to uppercase
    
    * use data_updates attribute instead of data
    
    * remove left over
    
    * match entries only by host
    
    * remove quotes
    
    * import SOURCE_USER directly, instead of config_entries
    
    * split happy and sad flow tests
    
    * remove unneccessary async_block_till_done() calls
    
    * replace url example by "full URL"
    
    * bump aioimmich==0.4.0
    
    * bump aioimmich==0.5.0
    
    * allow multiple users for same immich instance
    
    * Fix tests
    
    * limit entities when user has no admin rights
    
    * Fix tests
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Joostlek <joostlek@outlook.com>

---
+++ homeassistant/components/immich/coordinator.py
@@ -0,0 +1,74 @@
+"""Coordinator for the Immich integration."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import timedelta
+import logging
+
+from aioimmich import Immich
+from aioimmich.const import CONNECT_ERRORS
+from aioimmich.exceptions import ImmichUnauthorizedError
+from aioimmich.server.models import (
+    ImmichServerAbout,
+    ImmichServerStatistics,
+    ImmichServerStorage,
+)
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryAuthFailed
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class ImmichData:
+    """Data class for storing data from the API."""
+
+    server_about: ImmichServerAbout
+    server_storage: ImmichServerStorage
+    server_usage: ImmichServerStatistics | None
+
+
+type ImmichConfigEntry = ConfigEntry[ImmichDataUpdateCoordinator]
+
+
+class ImmichDataUpdateCoordinator(DataUpdateCoordinator[ImmichData]):
+    """Class to manage fetching IMGW-PIB data API."""
+
+    config_entry: ImmichConfigEntry
+
+    def __init__(
+        self, hass: HomeAssistant, entry: ConfigEntry, api: Immich, is_admin: bool
+    ) -> None:
+        """Initialize the data update coordinator."""
+        self.api = api
+        self.is_admin = is_admin
+        super().__init__(
+            hass,
+            _LOGGER,
+            config_entry=entry,
+            name=DOMAIN,
+            update_interval=timedelta(seconds=60),
+        )
+
+    async def _async_update_data(self) -> ImmichData:
+        """Update data via internal method."""
+        try:
+            server_about = await self.api.server.async_get_about_info()
+            server_storage = await self.api.server.async_get_storage_info()
+            server_usage = (
+                await self.api.server.async_get_server_statistics()
+                if self.is_admin
+                else None
+            )
+        except ImmichUnauthorizedError as err:
+            raise ConfigEntryAuthFailed from err
+        except CONNECT_ERRORS as err:
+            raise UpdateFailed from err
+
+        return ImmichData(server_about, server_storage, server_usage)
---
+++ tests/components/immich/conftest.py
@@ -0,0 +1,136 @@
+"""Common fixtures for the Immich tests."""
+
+from collections.abc import AsyncGenerator, Generator
+from datetime import datetime
+from unittest.mock import AsyncMock, patch
+
+from aioimmich import ImmichServer, ImmichUsers
+from aioimmich.server.models import (
+    ImmichServerAbout,
+    ImmichServerStatistics,
+    ImmichServerStorage,
+)
+from aioimmich.users.models import AvatarColor, ImmichUser, UserStatus
+import pytest
+
+from homeassistant.components.immich.const import DOMAIN
+from homeassistant.const import (
+    CONF_API_KEY,
+    CONF_HOST,
+    CONF_PORT,
+    CONF_SSL,
+    CONF_VERIFY_SSL,
+)
+
+from tests.common import MockConfigEntry
+
+
+@pytest.fixture
+def mock_setup_entry() -> Generator[AsyncMock]:
+    """Override async_setup_entry."""
+    with patch(
+        "homeassistant.components.immich.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        yield mock_setup_entry
+
+
+@pytest.fixture
+def mock_config_entry() -> MockConfigEntry:
+    """Mock a config entry."""
+    return MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            CONF_HOST: "localhost",
+            CONF_PORT: 80,
+            CONF_SSL: False,
+            CONF_API_KEY: "api_key",
+            CONF_VERIFY_SSL: True,
+        },
+        unique_id="e7ef5713-9dab-4bd4-b899-715b0ca4379e",
+        title="Someone",
+    )
+
+
+@pytest.fixture
+def mock_immich_server() -> AsyncMock:
+    """Mock the Immich server."""
+    mock = AsyncMock(spec=ImmichServer)
+    mock.async_get_about_info.return_value = ImmichServerAbout(
+        "v1.132.3",
+        "some_url",
+        False,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+    )
+    mock.async_get_storage_info.return_value = ImmichServerStorage(
+        "294.2 GiB",
+        "142.9 GiB",
+        "136.3 GiB",
+        315926315008,
+        153400434688,
+        146402975744,
+        48.56,
+    )
+    mock.async_get_server_statistics.return_value = ImmichServerStatistics(
+        27038, 1836, 119525451912, 54291170551, 65234281361
+    )
+    return mock
+
+
+@pytest.fixture
+def mock_immich_user() -> AsyncMock:
+    """Mock the Immich server."""
+    mock = AsyncMock(spec=ImmichUsers)
+    mock.async_get_my_user.return_value = ImmichUser(
+        "e7ef5713-9dab-4bd4-b899-715b0ca4379e",
+        "user@immich.local",
+        "user",
+        "",
+        AvatarColor.PRIMARY,
+        datetime.fromisoformat("2025-05-11T10:07:46.866Z"),
+        "user",
+        False,
+        True,
+        datetime.fromisoformat("2025-05-11T10:07:46.866Z"),
+        None,
+        None,
+        "",
+        None,
+        None,
+        UserStatus.ACTIVE,
+    )
+    return mock
+
+
+@pytest.fixture
+async def mock_immich(
+    mock_immich_server: AsyncMock, mock_immich_user: AsyncMock
+) -> AsyncGenerator[AsyncMock]:
+    """Mock the Immich API."""
+    with (
+        patch("homeassistant.components.immich.Immich", autospec=True) as mock_immich,
+        patch("homeassistant.components.immich.config_flow.Immich", new=mock_immich),
+    ):
+        client = mock_immich.return_value
+        client.server = mock_immich_server
+        client.users = mock_immich_user
+        yield client
+
+
+@pytest.fixture
+async def mock_non_admin_immich(mock_immich: AsyncMock) -> AsyncMock:
+    """Mock the Immich API."""
+    mock_immich.users.async_get_my_user.return_value.is_admin = False
+    return mock_immich
