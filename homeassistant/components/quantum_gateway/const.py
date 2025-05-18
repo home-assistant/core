tamwahba/homeassistant-core

move constants to central const file

homeassistant/components/quantum_gateway/const.py
@@ -1,0 +1,7 @@
+"""Constants for Quantum Gateway."""
+
+import logging
+
+_LOGGER = logging.getLogger(__package__)
+
+DEFAULT_HOST = "myfiosgateway.com"

home-assistant/core

homeassistant/components/quantum_gateway/const.py
@@ -1,0 +1,7 @@
+"""Constants for Quantum Gateway."""
+
+import logging
+
+LOGGER = logging.getLogger(__package__)
+
+DEFAULT_HOST = "myfiosgateway.com"