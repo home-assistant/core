5975eca2c0bdc6810f016d9665f2512d929838a6
Tsvi Mostovicz <ttmost@gmail.com>
2025-05-19 07:35:28 +0000

    Implement diagnostics

---
+++ homeassistant/components/jewish_calendar/diagnostics.py
@@ -0,0 +1,26 @@
+"""Diagnostics support for Jewish Calendar integration."""
+
+from typing import Any
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.const import CONF_ELEVATION, CONF_LATITUDE, CONF_LONGITUDE
+from homeassistant.core import HomeAssistant
+
+from .entity import JewishCalendarConfigEntry
+
+TO_REDACT = [
+    CONF_ELEVATION,
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+]
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: JewishCalendarConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+
+    return {
+        "entry_data": async_redact_data(entry.data, TO_REDACT),
+        "data": entry.runtime_data,
+    }
