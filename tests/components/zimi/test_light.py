ae26c0bdb836cc2370ecd82bf1b6b3688bf43399
Mark Hannon <mark.hannon@gmail.com>
2025-05-19 13:26:38 +0000

    More refactoring of tests

--- tests/components/zimi/test_light.py
+++ tests/components/zimi/test_light.py
@@ -2,21 +2,29 @@
 
 from unittest.mock import MagicMock
 
+from homeassistant.components.light import ColorMode
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
-from .common import mock_entity, setup_platform
+from .common import ENTITY_INFO, mock_entity, setup_platform
 
 
-async def test_light_entity_registry(
+async def test_light_entity(
     hass: HomeAssistant, entity_registry: er.EntityRegistry, mock_api: MagicMock
 ) -> None:
-    """Tests lights are registered in the entity registry."""
+    """Tests lights entity."""
 
-    mock_api.lights = [mock_entity(device_name="Light Controller", entity_type="light")]
+    device_name = "Light Controller"
+    entity_key = "light.light_controller_test_entity_name"
+    entity_type = "light"
+
+    mock_api.lights = [mock_entity(device_name=device_name, entity_type=entity_type)]
 
     await setup_platform(hass, Platform.LIGHT)
 
-    entity = entity_registry.entities["light.light_controller_test_entity_name"]
-    assert entity.unique_id == "test-entity-id"
+    entity = entity_registry.entities[entity_key]
+    assert entity.unique_id == ENTITY_INFO["id"]
+    assert entity.capabilities == {
+        "supported_color_modes": [ColorMode.ONOFF],
+    }
