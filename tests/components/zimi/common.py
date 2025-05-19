2fa0bf7b318b1eaf933a081607b1ed9579c99076
Mark Hannon <mark.hannon@gmail.com>
2025-05-19 11:16:53 +0000

    Common function, fixtures and first light test case

--- tests/components/zimi/common.py
+++ tests/components/zimi/common.py
@@ -1,12 +1,72 @@
 """Common items for testing the zimi component."""
 
-INPUT_MAC = "aa:bb:cc:dd:ee:ff"
-INPUT_MAC_EXTRA = "aa:bb:cc:dd:ee:ee"
-INPUT_HOST = "192.168.1.100"
-INPUT_HOST_EXTRA = "192.168.1.101"
-INPUT_PORT = 5003
-INPUT_PORT_EXTRA = 5004
+from unittest.mock import MagicMock, patch
 
-INVALID_INPUT_MAC = "xyz"
-MISMATCHED_INPUT_MAC = "aa:bb:cc:dd:ee:ee"
-SELECTED_HOST_AND_PORT = "selected_host_and_port"
+from homeassistant.components.zimi.const import DOMAIN
+from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
+
+from tests.common import MockConfigEntry
+
+DEVICE_INFO = {
+    "id": "test-device-id",
+    "name": "unknown",
+    "manufacturer": "Zimi",
+    "model": "Controller XYZ",
+    "hwVersion": "2.2.2",
+    "fwVersion": "3.3.3",
+}
+
+ENTITY_INFO = {
+    "id": "test-entity-id",
+    "name": "Test Entity Name",
+    "room": "Test Entity Room",
+    "type": "unknown",
+}
+
+INPUT_HOST = "192.168.1.100"
+INPUT_PORT = 5003
+
+
+def mock_entity(
+    device_name: str | None = None, entity_type: str | None = None
+) -> MagicMock:
+    """Mock a Zimi entity with defaults."""
+
+    mock_entity = MagicMock()
+
+    mock_entity.identifier = ENTITY_INFO["id"]
+    mock_entity.room = ENTITY_INFO["room"]
+    mock_entity.name = ENTITY_INFO["name"]
+    mock_entity.type = entity_type or ENTITY_INFO["type"]
+
+    mock_manfacture_info = MagicMock()
+    mock_manfacture_info.identifier = DEVICE_INFO["id"]
+    mock_manfacture_info.manufacturer = DEVICE_INFO["manufacturer"]
+    mock_manfacture_info.model = DEVICE_INFO["model"]
+    mock_manfacture_info.name = device_name or DEVICE_INFO["name"]
+    mock_manfacture_info.hwVersion = DEVICE_INFO["hwVersion"]
+    mock_manfacture_info.firmwareVersion = DEVICE_INFO["fwVersion"]
+
+    mock_entity.manufacture_info = mock_manfacture_info
+
+    return mock_entity
+
+
+async def setup_platform(hass: HomeAssistant, platform: str) -> MockConfigEntry:
+    """Set up the specified Zimi platform."""
+
+    if not platform:
+        raise ValueError("Platform must be specified")
+
+    mock_config = MockConfigEntry(
+        domain=DOMAIN, data={CONF_HOST: INPUT_HOST, CONF_PORT: INPUT_PORT}
+    )
+    mock_config.add_to_hass(hass)
+
+    with patch("homeassistant.components.zimi.PLATFORMS", [platform]):
+        assert await async_setup_component(hass, DOMAIN, {})
+        await hass.async_block_till_done()
+
+    return mock_config
