cd53b1f4d6bdba9a2d0d624fb6c98c623133eb10
epenet <6771947+epenet@users.noreply.github.com>
2025-05-20 06:02:01 +0000

    Mark camera methods and properties as mandatory in pylint plugin

--- pylint/plugins/hass_enforce_type_hints.py
+++ pylint/plugins/hass_enforce_type_hints.py
@@ -1005,18 +1005,22 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
                 TypeHintMatch(
                     function_name="entity_picture",
                     return_type="str",
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="supported_features",
                     return_type="CameraEntityFeature",
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="is_recording",
                     return_type="bool",
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="is_streaming",
                     return_type="bool",
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="brand",
@@ -1025,6 +1029,7 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
                 TypeHintMatch(
                     function_name="motion_detection_enabled",
                     return_type="bool",
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="model",
@@ -1033,6 +1038,7 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
                 TypeHintMatch(
                     function_name="frame_interval",
                     return_type="float",
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="frontend_stream_type",
@@ -1041,6 +1047,7 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
                 TypeHintMatch(
                     function_name="available",
                     return_type="bool",
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="async_create_stream",
@@ -1073,6 +1080,7 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
                         2: "float",
                     },
                     return_type="StreamResponse",
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="handle_async_mjpeg_stream",
@@ -1084,26 +1092,31 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
                 TypeHintMatch(
                     function_name="is_on",
                     return_type="bool",
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="turn_off",
                     return_type=None,
                     has_async_counterpart=True,
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="turn_on",
                     return_type=None,
                     has_async_counterpart=True,
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="enable_motion_detection",
                     return_type=None,
                     has_async_counterpart=True,
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="disable_motion_detection",
                     return_type=None,
                     has_async_counterpart=True,
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="async_handle_async_webrtc_offer",
@@ -1113,6 +1126,7 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
                         3: "WebRTCSendMessage",
                     },
                     return_type=None,
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="async_on_webrtc_candidate",
@@ -1121,6 +1135,7 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
                         2: "RTCIceCandidateInit",
                     },
                     return_type=None,
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="close_webrtc_session",
@@ -1128,10 +1143,12 @@ _INHERITANCE_MATCH: dict[str, list[ClassTypeHintMatch]] = {
                         1: "str",
                     },
                     return_type=None,
+                    mandatory=True,
                 ),
                 TypeHintMatch(
                     function_name="_async_get_webrtc_client_configuration",
                     return_type="WebRTCClientConfiguration",
+                    mandatory=True,
                 ),
             ],
         ),
