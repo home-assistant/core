fa7979fa8b5860d0fa6a22f94ec051e49b0b4db6
Myles Eftos <myles@madpilot.com.au>
2025-05-20 20:35:06 +1000

    Removing deprecation warning - will add back in to the next PR

--- tests/components/amberelectric/test_sensor.py
+++ tests/components/amberelectric/test_sensor.py
@@ -7,16 +7,10 @@ from amberelectric.models.interval import Interval
 from amberelectric.models.range import Range
 import pytest
 
-from homeassistant.components import automation, script
 from homeassistant.components.amberelectric.const import DOMAIN
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import issue_registry as ir
-from homeassistant.setup import async_setup_component
 
-from . import setup_integration
 from .helpers import GENERAL_CHANNEL
-
-from tests.common import MockConfigEntry
 
 
 async def test_general_price_sensor(hass: HomeAssistant, setup_general: Mock) -> None:
@@ -190,67 +184,6 @@ async def test_feed_in_forecast_sensor(hass: HomeAssistant) -> None:
     assert first_forecast["renewables"] == 50
     assert first_forecast["spike_status"] == "none"
     assert first_forecast["descriptor"] == "very_low"
-
-
-@pytest.mark.usefixtures("forecast_prices")
-async def test_create_issue(
-    hass: HomeAssistant,
-    general_only_site_id_amber_config_entry: MockConfigEntry,
-    issue_registry: ir.IssueRegistry,
-) -> None:
-    """Test we create an issue when an automation or script is using a deprecated entity."""
-    entity_id = "sensor.mock_title_general_forecast"
-    issue_id = f"deprecated_forecast_sensor_{entity_id}"
-
-    assert await async_setup_component(
-        hass,
-        automation.DOMAIN,
-        {
-            automation.DOMAIN: {
-                "alias": "test",
-                "trigger": {"platform": "state", "entity_id": entity_id},
-                "action": {
-                    "action": "automation.turn_on",
-                    "target": {
-                        "entity_id": "automation.test",
-                    },
-                },
-            }
-        },
-    )
-    assert await async_setup_component(
-        hass,
-        script.DOMAIN,
-        {
-            script.DOMAIN: {
-                "test": {
-                    "sequence": [
-                        {
-                            "condition": "state",
-                            "entity_id": entity_id,
-                            "state": "on",
-                        },
-                    ],
-                }
-            }
-        },
-    )
-    await setup_integration(hass, general_only_site_id_amber_config_entry)
-
-    assert automation.automations_with_entity(hass, entity_id)[0] == "automation.test"
-    assert script.scripts_with_entity(hass, entity_id)[0] == "script.test"
-
-    assert len(issue_registry.issues) == 1
-    assert issue_registry.async_get_issue(DOMAIN, issue_id)
-
-    await hass.config_entries.async_unload(
-        general_only_site_id_amber_config_entry.entry_id
-    )
-    await hass.async_block_till_done()
-
-    # Assert the issue is no longer present
-    assert not issue_registry.async_get_issue(DOMAIN, issue_id)
-    assert len(issue_registry.issues) == 0
 
 
 @pytest.mark.usefixtures("setup_general")
