1893435e9e536178ca3ac56bb780e7a97de5e977
Jeff Stein <jeffstein@gmail.com>
2025-05-20 14:58:56 -0600

    pre-change for icon translation changes

--- homeassistant/components/weatherflow/sensor.py
+++ homeassistant/components/weatherflow/sensor.py
@@ -46,25 +46,6 @@ from homeassistant.util.unit_system import METRIC_SYSTEM
 
 from .const import DOMAIN, LOGGER, format_dispatch_call
 
-DIRECTION_MAP = {
-    0: "mdi:arrow-up",  # N   (337.5° - 22.5°)
-    1: "mdi:arrow-top-right",  # NE  (22.5° - 67.5°)
-    2: "mdi:arrow-right",  # E   (67.5° - 112.5°)
-    3: "mdi:arrow-bottom-right",  # SE  (112.5° - 157.5°)
-    4: "mdi:arrow-down",  # S   (157.5° - 202.5°)
-    5: "mdi:arrow-bottom-left",  # SW  (202.5° - 247.5°)
-    6: "mdi:arrow-left",  # W   (247.5° - 292.5°)
-    7: "mdi:arrow-top-left",  # NW  (292.5° - 337.5°)
-}
-
-
-def _get_wind_direction_icon(wind_angle: int | None = None) -> str:
-    """Get the wind direction icon based on the wind angle."""
-    if wind_angle is None:
-        return "mdi:compass"
-    sector = int(((wind_angle % 360) + 22.5) / 45) % 8
-    return DIRECTION_MAP[sector]
-
 
 def precipitation_raw_conversion_fn(raw_data: Enum):
     """Parse parse precipitation type."""
@@ -78,7 +59,7 @@ class WeatherFlowSensorEntityDescription(SensorEntityDescription):
     """Describes WeatherFlow sensor entity."""
 
     raw_data_conv_fn: Callable[[WeatherFlowDevice], datetime | StateType]
-    icon_fn: Callable[[int], str] | None = None
+
     event_subscriptions: list[str] = field(default_factory=lambda: [EVENT_OBSERVATION])
     imperial_suggested_unit: str | None = None
 
@@ -285,7 +266,6 @@ SENSORS: tuple[WeatherFlowSensorEntityDescription, ...] = (
     ),
     WeatherFlowSensorEntityDescription(
         key="wind_direction",
-        icon_fn=_get_wind_direction_icon,
         translation_key="wind_direction",
         device_class=SensorDeviceClass.WIND_DIRECTION,
         state_class=SensorStateClass.MEASUREMENT_ANGLE,
@@ -295,7 +275,6 @@ SENSORS: tuple[WeatherFlowSensorEntityDescription, ...] = (
     ),
     WeatherFlowSensorEntityDescription(
         key="wind_direction_average",
-        icon_fn=_get_wind_direction_icon,
         translation_key="wind_direction_average",
         device_class=SensorDeviceClass.WIND_DIRECTION,
         native_unit_of_measurement=DEGREE,
@@ -369,22 +348,6 @@ class WeatherFlowSensorEntity(SensorEntity):
             self._attr_suggested_unit_of_measurement = (
                 description.imperial_suggested_unit
             )
-
-    @property
-    def icon(self) -> str | None:
-        """Get icon."""
-        # Early return if no icon_fn exists
-        if self.entity_description.icon_fn is None:
-            return None
-
-        # Convert native_value to int if possible
-        if self.native_value is not None and isinstance(
-            self.native_value, (int, float, str)
-        ):
-            value = int(self.native_value)
-            return self.entity_description.icon_fn(value)
-
-        return None
 
     @property
     def last_reset(self) -> datetime | None:
