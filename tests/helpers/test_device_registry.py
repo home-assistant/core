f04a758ea0b90e99f937b07208325bee9dbe5cec
Erik <erik@montnemery.com>
2025-05-20 09:36:30 +0200

    Improve test coverage

--- tests/helpers/test_device_registry.py
+++ tests/helpers/test_device_registry.py
@@ -2086,27 +2086,43 @@ async def test_removing_area_id_deleted_device(
     device_registry: dr.DeviceRegistry, mock_config_entry: MockConfigEntry
 ) -> None:
     """Make sure we can clear area id."""
-    entry = device_registry.async_get_or_create(
+    entry1 = device_registry.async_get_or_create(
         config_entry_id=mock_config_entry.entry_id,
         connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
         identifiers={("bridgeid", "0123")},
         manufacturer="manufacturer",
         model="model",
     )
+    entry2 = device_registry.async_get_or_create(
+        config_entry_id=mock_config_entry.entry_id,
+        connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:FF")},
+        identifiers={("bridgeid", "1234")},
+        manufacturer="manufacturer",
+        model="model",
+    )
 
-    entry_w_area = device_registry.async_update_device(entry.id, area_id="12345A")
+    entry1_w_area = device_registry.async_update_device(entry1.id, area_id="12345A")
+    entry2_w_area = device_registry.async_update_device(entry2.id, area_id="12345B")
 
-    device_registry.async_remove_device(entry.id)
+    device_registry.async_remove_device(entry1.id)
+    device_registry.async_remove_device(entry2.id)
 
     device_registry.async_clear_area_id("12345A")
-    entry_wo_area = device_registry.async_get_or_create(
+    entry1_restored = device_registry.async_get_or_create(
         config_entry_id=mock_config_entry.entry_id,
         connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
         identifiers={("bridgeid", "0123")},
     )
+    entry2_restored = device_registry.async_get_or_create(
+        config_entry_id=mock_config_entry.entry_id,
+        connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:FF")},
+        identifiers={("bridgeid", "1234")},
+    )
 
-    assert not entry_wo_area.area_id
-    assert entry_w_area != entry_wo_area
+    assert not entry1_restored.area_id
+    assert entry2_restored.area_id == "12345B"
+    assert entry1_w_area != entry1_restored
+    assert entry2_w_area != entry2_restored
 
 
 async def test_specifying_via_device_create(
@@ -4121,41 +4137,57 @@ async def test_removing_labels_deleted_device(
     """Make sure we can clear labels."""
     config_entry = MockConfigEntry()
     config_entry.add_to_hass(hass)
-    entry = device_registry.async_get_or_create(
+    entry1 = device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
         connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
         identifiers={("bridgeid", "0123")},
         manufacturer="manufacturer",
         model="model",
     )
-    entry = device_registry.async_update_device(entry.id, labels={"label1", "label2"})
+    entry1 = device_registry.async_update_device(entry1.id, labels={"label1", "label2"})
+    entry2 = device_registry.async_get_or_create(
+        config_entry_id=config_entry.entry_id,
+        connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:FF")},
+        identifiers={("bridgeid", "1234")},
+        manufacturer="manufacturer",
+        model="model",
+    )
+    entry2 = device_registry.async_update_device(entry2.id, labels={"label3"})
 
-    device_registry.async_remove_device(entry.id)
+    device_registry.async_remove_device(entry1.id)
+    device_registry.async_remove_device(entry2.id)
 
     device_registry.async_clear_label_id("label1")
-    entry_cleared_label1 = device_registry.async_get_or_create(
+    entry1_cleared_label1 = device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
         connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
         identifiers={("bridgeid", "0123")},
     )
 
-    device_registry.async_remove_device(entry.id)
+    device_registry.async_remove_device(entry1.id)
 
     device_registry.async_clear_label_id("label2")
-    entry_cleared_label2 = device_registry.async_get_or_create(
+    entry1_cleared_label2 = device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
         connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
         identifiers={("bridgeid", "0123")},
     )
+    entry2_restored = device_registry.async_get_or_create(
+        config_entry_id=config_entry.entry_id,
+        connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:FF")},
+        identifiers={("bridgeid", "1234")},
+    )
 
-    assert entry_cleared_label1
-    assert entry_cleared_label2
-    assert entry != entry_cleared_label1
-    assert entry != entry_cleared_label2
-    assert entry_cleared_label1 != entry_cleared_label2
-    assert entry.labels == {"label1", "label2"}
-    assert entry_cleared_label1.labels == {"label2"}
-    assert not entry_cleared_label2.labels
+    assert entry1_cleared_label1
+    assert entry1_cleared_label2
+    assert entry1 != entry1_cleared_label1
+    assert entry1 != entry1_cleared_label2
+    assert entry1_cleared_label1 != entry1_cleared_label2
+    assert entry1.labels == {"label1", "label2"}
+    assert entry1_cleared_label1.labels == {"label2"}
+    assert not entry1_cleared_label2.labels
+    assert entry2 != entry2_restored
+    assert entry2_restored.labels == {"label3"}
 
 
 async def test_entries_for_label(
