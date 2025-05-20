d9efa3e7390087db00c0aab4b11d17fb51d68bf6
Franck Nijhof <git@frenck.dev>
2025-05-20 12:51:08 +0000

    Add concentration conversion support for mg/m³

--- tests/util/test_unit_conversion.py
+++ tests/util/test_unit_conversion.py
@@ -8,6 +8,8 @@ from itertools import chain
 import pytest
 
 from homeassistant.const import (
+    CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    CONCENTRATION_MILLIGRAMS_PER_CUBIC_METER,
     CONCENTRATION_PARTS_PER_BILLION,
     CONCENTRATION_PARTS_PER_MILLION,
     PERCENTAGE,
@@ -38,6 +40,7 @@ from homeassistant.util.unit_conversion import (
     AreaConverter,
     BaseUnitConverter,
     BloodGlucoseConcentrationConverter,
+    ConcentrationConverter,
     ConductivityConverter,
     DataRateConverter,
     DistanceConverter,
@@ -69,6 +72,7 @@ _ALL_CONVERTERS: dict[type[BaseUnitConverter], list[str | None]] = {
     for converter in (
         AreaConverter,
         BloodGlucoseConcentrationConverter,
+        ConcentrationConverter,
         ConductivityConverter,
         DataRateConverter,
         DistanceConverter,
@@ -97,6 +101,11 @@ _GET_UNIT_RATIO: dict[type[BaseUnitConverter], tuple[str | None, str | None, flo
         UnitOfBloodGlucoseConcentration.MILLIGRAMS_PER_DECILITER,
         UnitOfBloodGlucoseConcentration.MILLIMOLE_PER_LITER,
         18,
+    ),
+    ConcentrationConverter: (
+        CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+        CONCENTRATION_MILLIGRAMS_PER_CUBIC_METER,
+        1000,
     ),
     ConductivityConverter: (
         UnitOfConductivity.MICROSIEMENS_PER_CM,
@@ -737,6 +746,22 @@ _CONVERTED_VALUE: dict[
         (5, None, 5000000000, CONCENTRATION_PARTS_PER_BILLION),
         (5, None, 5000000, CONCENTRATION_PARTS_PER_MILLION),
         (5, PERCENTAGE, 0.05, None),
+    ],
+    ConcentrationConverter: [
+        # 1000 µg/m³ = 1 mg/m³
+        (
+            1000,
+            CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+            1,
+            CONCENTRATION_MILLIGRAMS_PER_CUBIC_METER,
+        ),
+        # 2 mg/m³ = 2000 µg/m³
+        (
+            2,
+            CONCENTRATION_MILLIGRAMS_PER_CUBIC_METER,
+            2000,
+            CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+        ),
     ],
     VolumeConverter: [
         (5, UnitOfVolume.LITERS, 1.32086, UnitOfVolume.GALLONS),
