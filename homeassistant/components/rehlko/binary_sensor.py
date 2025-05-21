d6efeb37d3388f730604a6fc8b4ff8ca87eb0cb5
PeteRager <pete.sage@icloud.com>
2025-05-21 18:40:32 -0400

    fix: remove unneeded type

--- homeassistant/components/rehlko/binary_sensor.py
+++ homeassistant/components/rehlko/binary_sensor.py
@@ -33,8 +33,8 @@ _LOGGER = logging.getLogger(__name__)
 class RehlkoBinarySensorEntityDescription(BinarySensorEntityDescription):
     """Class describing Rehlko binary sensor entities."""
 
-    on_value: str | bool | None = True
-    off_value: str | bool | None = False
+    on_value: str | bool = True
+    off_value: str | bool = False
     document_key: str | None = None
     connectivity_key: str | None = DEVICE_DATA_IS_CONNECTED
 
