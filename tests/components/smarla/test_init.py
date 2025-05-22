f6f732b6a7f733e6d98cb473e51b64d3d74fec90
Joostlek <joostlek@outlook.com>
2025-05-22 20:29:17 +0200

    Fix tests

--- tests/components/smarla/test_init.py
+++ tests/components/smarla/test_init.py
@@ -1,7 +1,5 @@
 """Test switch platform for Swing2Sleep Smarla integration."""
 
-from unittest.mock import AsyncMock, patch
-
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
 
@@ -15,11 +13,9 @@ async def test_init_invalid_auth(
     # Add the mock entry to hass
     mock_config_entry.add_to_hass(hass)
 
-    with patch.object(
-        mock_connection, "refresh_token", new=AsyncMock(return_value=False)
-    ):
-        # Set up the platform
-        await hass.config_entries.async_setup(mock_config_entry.entry_id)
-        await hass.async_block_till_done()
+    mock_connection.refresh_token.return_value = False
+    # Set up the platform
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
 
     assert mock_config_entry.state is ConfigEntryState.SETUP_ERROR
