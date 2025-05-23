61f7ce61e729d16c8cad5a5a3067dc8d26b79593
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-05-23 16:01:26 +0200

    Coverage

--- tests/components/shelly/test_devices.py
+++ tests/components/shelly/test_devices.py
@@ -435,3 +435,45 @@ async def test_shelly_pro_3em_with_emeter_name(
     device_entry = device_registry.async_get(entry.device_id)
     assert device_entry
     assert device_entry.name == "Test name Phase C"
+
+
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_block_channel_with_name(
+    hass: HomeAssistant,
+    mock_block_device: Mock,
+    monkeypatch: pytest.MonkeyPatch,
+    entity_registry: EntityRegistry,
+    device_registry: DeviceRegistry,
+) -> None:
+    """Test block channel with name."""
+    monkeypatch.setitem(
+        mock_block_device.settings["relays"][0], "name", "Kitchen light"
+    )
+
+    await init_integration(hass, 1)
+
+    # channel 1 sub-device; num_outputs is 2 so the name of the channel should be used
+    entity_id = "switch.kitchen_light"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Kitchen light"
+
+    # main device
+    entity_id = "update.test_name_firmware"
+
+    state = hass.states.get(entity_id)
+    assert state
+
+    entry = entity_registry.async_get(entity_id)
+    assert entry
+
+    device_entry = device_registry.async_get(entry.device_id)
+    assert device_entry
+    assert device_entry.name == "Test name"
