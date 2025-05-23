f90f7234ec7ad61296f8f52a88d7f229877594eb
Adrien Cognee <adrien@cognee.fr>
2025-05-23 11:05:32 +0000

    Add freebox port forwarding switches

--- homeassistant/components/freebox/switch.py
+++ homeassistant/components/freebox/switch.py
@@ -10,6 +10,7 @@ from freebox_api.exceptions import InsufficientPermissionsError
 from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from .router import FreeboxConfigEntry, FreeboxRouter
@@ -25,6 +26,16 @@ SWITCH_DESCRIPTIONS = [
     )
 ]
 
+PORT_FORWARDING_SWITCH_DESCRIPTIONS = [
+    SwitchEntityDescription(
+        key="port_forwarding",
+        name="Port Forwarding",
+        entity_category=EntityCategory.CONFIG,
+        icon="mdi:forwardburger",
+        entity_registry_enabled_default=False,
+    )
+]
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -33,10 +44,20 @@ async def async_setup_entry(
 ) -> None:
     """Set up the switch."""
     router = entry.runtime_data
-    entities = [
-        FreeboxSwitch(router, entity_description)
-        for entity_description in SWITCH_DESCRIPTIONS
-    ]
+    entities: list[SwitchEntity] = []
+    entities.extend(
+        [
+            FreeboxSwitch(router, entity_description)
+            for entity_description in SWITCH_DESCRIPTIONS
+        ]
+    )
+    for config in router.port_forwarding_config.values():
+        entities.extend(
+            [
+                FreeboxPortForwardingSwitch(router, config, entity_description)
+                for entity_description in PORT_FORWARDING_SWITCH_DESCRIPTIONS
+            ]
+        )
     async_add_entities(entities, True)
 
 
@@ -74,3 +95,57 @@ class FreeboxSwitch(SwitchEntity):
         """Get the state and update it."""
         data = await self._router.wifi.get_global_config()
         self._attr_is_on = bool(data["enabled"])
+
+
+class FreeboxPortForwardingSwitch(SwitchEntity):
+    """Representation of a freebox switch."""
+
+    def __init__(
+        self,
+        router: FreeboxRouter,
+        config: dict[str, Any],
+        entity_description: SwitchEntityDescription,
+    ) -> None:
+        """Initialize the switch."""
+        self.entity_description = entity_description
+        self._router = router
+        self._attr_device_info = router.device_info
+        self._attr_unique_id = f"{router.mac} {entity_description.name} {config['id']}"
+        self._attr_name = f"{entity_description.name} {config['id']}"
+        self._redir_id = config["id"]
+
+    async def _async_set_state(self, enabled: bool) -> None:
+        """Turn the switch on or off."""
+        try:
+            await self._router.port_forwarding.edit_port_forwarding_configuration(
+                self._redir_id, {"enabled": enabled}
+            )
+            await self._router.update_port_forwarding_configs()
+            await self.async_update()
+        except InsufficientPermissionsError as err:
+            raise HomeAssistantError(
+                "Home Assistant does not have permissions to modify the Freebox"
+                " settings. Please refer to documentation"
+            ) from err
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the switch on."""
+        await self._async_set_state(True)
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the switch off."""
+        await self._async_set_state(False)
+
+    async def async_update(self) -> None:
+        """Get the state and update it."""
+        data = self._router.port_forwarding_config.get(self._redir_id, {})
+        self._attr_is_on = bool(data.get("enabled", False))
+        self._attr_extra_state_attributes = {
+            "comment": data.get("comment"),
+            "source": data.get("src_ip"),
+            "destination": data.get("lan_ip"),
+            "hostname": data.get("hostname"),
+            "port_start": data.get("wan_port_start"),
+            "port_end": data.get("wan_port_end"),
+            "port_destination": data.get("lan_port"),
+        }
---
+++ tests/components/freebox/test_switch.py
@@ -0,0 +1,50 @@
+"""Tests for the Freebox switches."""
+
+from unittest.mock import AsyncMock, Mock, patch
+
+from freezegun.api import FrozenDateTimeFactory
+
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN, SERVICE_TOGGLE
+from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.core import HomeAssistant
+import homeassistant.helpers.entity_registry as er
+
+from .common import setup_platform
+
+
+async def test_port_forwarding(
+    hass: HomeAssistant, freezer: FrozenDateTimeFactory, router: Mock
+) -> None:
+    """Test port forwarding switch."""
+    await setup_platform(hass, SWITCH_DOMAIN)
+
+    registry = er.async_get(hass)
+    entity_id = "switch.port_forwarding_1"
+
+    entity = registry.async_get(entity_id)
+    assert entity.disabled_by == "integration"
+
+    # Simulate enabling the switch
+    registry.async_update_entity(entity_id, disabled_by=None)
+    await hass.config_entries.async_reload(entity.config_entry_id)
+    await hass.async_block_till_done()
+    assert hass.states.get(entity_id).state == "on"
+
+    # Simulate toggling the switch
+    with patch(
+        "homeassistant.components.freebox.router.FreeboxRouter.port_forwarding"
+    ) as mock_service:
+        mock_service.edit_port_forwarding_configuration = AsyncMock()
+        mock_service.edit_port_forwarding_configuration.assert_not_called()
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            SERVICE_TOGGLE,
+            service_data={
+                ATTR_ENTITY_ID: entity_id,
+            },
+            blocking=True,
+        )
+        await hass.async_block_till_done()
+        mock_service.edit_port_forwarding_configuration.assert_called_once_with(
+            1, {"enabled": False}
+        )
