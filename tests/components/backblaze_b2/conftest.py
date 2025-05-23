7a8dfb25897f0c8438c4965a0e1cfb9e1dd2422c
Hugo van Rijswijk <git@hugovr.nl>
2025-05-23 23:10:26 +0000

    Improve test coverage

--- tests/components/backblaze_b2/conftest.py
+++ tests/components/backblaze_b2/conftest.py
@@ -1,10 +1,13 @@
 """Common fixtures for the Backblaze B2 tests."""
 
 from collections.abc import Generator
+import hashlib
+import io
 from typing import Any
 from unittest.mock import AsyncMock, patch
 
-from b2sdk.v2 import RawSimulator
+from b2sdk._internal.raw_simulator import BucketSimulator
+from b2sdk.v2 import FileVersion, RawSimulator
 import pytest
 
 from homeassistant.components.backblaze_b2.const import (
@@ -14,7 +17,7 @@ from homeassistant.components.backblaze_b2.const import (
     DOMAIN,
 )
 
-from .const import USER_INPUT
+from .const import BACKUP_METADATA, TEST_BACKUP, USER_INPUT
 
 from tests.common import MockConfigEntry
 
@@ -80,6 +83,53 @@ def b2_fixture():
             bucket_name=USER_INPUT[CONF_BUCKET],
             bucket_type="allPrivate",
         )
+
+        # Create a test backup
+        test_backup_data = b"backup data"
+        upload_url = sim.get_upload_url(api_url, auth_token, bucket["bucketId"])
+        stream = io.BytesIO(test_backup_data)
+        stream.seek(0)
+
+        filename = TEST_BACKUP.name
+        sha1 = hashlib.sha1(test_backup_data).hexdigest()
+        file = sim.upload_file(
+            upload_url["uploadUrl"],
+            upload_url["authorizationToken"],
+            filename,
+            len(test_backup_data),
+            "application/octet-stream",
+            sha1,
+            BACKUP_METADATA,
+            stream,
+        )
+
+        def ls(
+            self,
+            prefix: str = "",
+        ) -> list[tuple[FileVersion, str]]:
+            """List files in the bucket."""
+            return [
+                (
+                    FileVersion(
+                        sim,
+                        file["fileId"],
+                        file["fileName"],
+                        file["contentLength"],
+                        "application/octet-stream",
+                        sha1,
+                        BACKUP_METADATA,
+                        file["uploadTimestamp"],
+                        file["accountId"],
+                        file["bucketId"],
+                        "action",
+                        None,
+                        None,
+                    ),
+                    file["fileName"],
+                )
+            ]
+
+        BucketSimulator.ls = ls
 
         yield BackblazeFixture(application_key_id, application_key, bucket, sim, auth)
 
--- tests/components/backblaze_b2/test_backup.py
+++ tests/components/backblaze_b2/test_backup.py
@@ -1,7 +1,8 @@
 """Test Backblaze B2 backup agent."""
 
 from collections.abc import AsyncGenerator
-from unittest.mock import MagicMock, patch
+import io
+from unittest.mock import MagicMock, Mock, patch
 
 import pytest
 
@@ -18,9 +19,10 @@ from homeassistant.helpers.backup import async_initialize_backup
 from homeassistant.setup import async_setup_component
 
 from . import setup_integration
+from .const import BACKUP_METADATA, TEST_BACKUP
 
 from tests.common import MockConfigEntry
-from tests.typing import WebSocketGenerator
+from tests.typing import ClientSessionGenerator, WebSocketGenerator
 
 
 @pytest.fixture(autouse=True)
@@ -62,6 +64,188 @@ async def test_agents_info(
             },
         ],
     }
+
+
+async def test_agents_list_backups(
+    hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test agent list backups."""
+
+    client = await hass_ws_client(hass)
+    await client.send_json_auto_id({"type": "backup/info"})
+    response = await client.receive_json()
+
+    assert response["success"]
+    assert response["result"]["agent_errors"] == {}
+    assert response["result"]["backups"] == [
+        {
+            "addons": TEST_BACKUP.addons,
+            "backup_id": TEST_BACKUP.backup_id,
+            "date": TEST_BACKUP.date,
+            "database_included": TEST_BACKUP.database_included,
+            "folders": TEST_BACKUP.folders,
+            "homeassistant_included": TEST_BACKUP.homeassistant_included,
+            "homeassistant_version": TEST_BACKUP.homeassistant_version,
+            "name": TEST_BACKUP.name,
+            "extra_metadata": TEST_BACKUP.extra_metadata,
+            "agents": {
+                f"{DOMAIN}.{mock_config_entry.entry_id}": {
+                    "protected": TEST_BACKUP.protected,
+                    "size": TEST_BACKUP.size,
+                }
+            },
+            "failed_agent_ids": [],
+            "with_automatic_settings": None,
+        }
+    ]
+
+
+async def test_agents_get_backup(
+    hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test agent get backup."""
+
+    backup_id = TEST_BACKUP.backup_id
+    client = await hass_ws_client(hass)
+    await client.send_json_auto_id({"type": "backup/details", "backup_id": backup_id})
+    response = await client.receive_json()
+
+    assert response["success"]
+    assert response["result"]["agent_errors"] == {}
+    assert response["result"]["backup"] == {
+        "addons": [],
+        "backup_id": backup_id,
+        "date": TEST_BACKUP.date,
+        "database_included": TEST_BACKUP.database_included,
+        "folders": TEST_BACKUP.folders,
+        "homeassistant_included": TEST_BACKUP.homeassistant_included,
+        "homeassistant_version": TEST_BACKUP.homeassistant_version,
+        "name": TEST_BACKUP.name,
+        "extra_metadata": TEST_BACKUP.extra_metadata,
+        "agents": {
+            f"{DOMAIN}.{mock_config_entry.entry_id}": {
+                "protected": TEST_BACKUP.protected,
+                "size": TEST_BACKUP.size,
+            }
+        },
+        "failed_agent_ids": [],
+        "with_automatic_settings": None,
+    }
+
+
+async def test_agents_download(
+    hass_client: ClientSessionGenerator,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test agent download backup."""
+    with patch("b2sdk.v2.FileVersion.download", return_value=Mock()) as mock_download:
+
+        def iter_content(chunk_size: int = 1) -> io.BytesIO:
+            """Mock iter_content to return bytes."""
+            return io.BytesIO(b"backup data")
+
+        mock_download.return_value.response.iter_content = iter_content
+        client = await hass_client()
+        backup_id = BACKUP_METADATA["backup_id"]
+
+        resp = await client.get(
+            f"/api/backup/download/{backup_id}?agent_id={DOMAIN}.{mock_config_entry.entry_id}"
+        )
+        assert resp.status == 200
+        assert await resp.content.read() == b"backup data"
+
+
+async def test_agents_upload(
+    hass_client: ClientSessionGenerator,
+    caplog: pytest.LogCaptureFixture,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test agent upload backup."""
+    client = await hass_client()
+    with (
+        patch(
+            "homeassistant.components.backup.manager.BackupManager.async_get_backup",
+        ) as fetch_backup,
+        patch(
+            "homeassistant.components.backup.manager.read_backup",
+            return_value=TEST_BACKUP,
+        ),
+        patch("pathlib.Path.open") as mocked_open,
+    ):
+        mocked_open.return_value.read = Mock(side_effect=[b"test", b""])
+        fetch_backup.return_value = TEST_BACKUP
+        resp = await client.post(
+            f"/api/backup/upload?agent_id={DOMAIN}.{mock_config_entry.entry_id}",
+            data={"file": io.StringIO("test")},
+        )
+
+    assert resp.status == 201
+    assert f"Uploading backup {TEST_BACKUP.backup_id}" in caplog.text
+
+
+async def test_agents_delete(
+    hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
+) -> None:
+    """Test agent delete backup."""
+    with patch("b2sdk.v2.FileVersion.delete"):
+        client = await hass_ws_client(hass)
+
+        await client.send_json_auto_id(
+            {
+                "type": "backup/delete",
+                "backup_id": TEST_BACKUP.backup_id,
+            }
+        )
+        response = await client.receive_json()
+
+    assert response["success"]
+    assert response["result"] == {"agent_errors": {}}
+
+
+async def test_agents_error_on_download_not_found(
+    hass_client: ClientSessionGenerator,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test agent download backup."""
+    with patch(
+        "b2sdk._internal.raw_simulator.BucketSimulator.ls",
+        return_value=[],
+    ):
+        client = await hass_client()
+        backup_id = TEST_BACKUP.backup_id
+
+        resp = await client.get(
+            f"/api/backup/download/{backup_id}?agent_id={DOMAIN}.{mock_config_entry.entry_id}"
+        )
+    assert resp.status == 404
+
+
+async def test_agents_delete_not_throwing_on_not_found(
+    hass: HomeAssistant,
+    hass_ws_client: WebSocketGenerator,
+) -> None:
+    """Test agent delete backup does not throw on a backup not found."""
+    with patch(
+        "b2sdk._internal.raw_simulator.BucketSimulator.ls",
+        return_value=[],
+    ):
+        client = await hass_ws_client(hass)
+
+        await client.send_json_auto_id(
+            {
+                "type": "backup/delete",
+                "backup_id": TEST_BACKUP.backup_id,
+            }
+        )
+        response = await client.receive_json()
+
+        assert response["success"]
+        assert response["result"] == {"agent_errors": {}}
 
 
 async def test_listeners_get_cleaned_up(hass: HomeAssistant) -> None:
