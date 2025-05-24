94786df5c5a0f0c74ec6c15894209671f8d1ab93
PeteRager <pete.sage@icloud.com>
2025-05-24 17:40:22 -0400

    fix: update comments

--- tests/components/sonos/test_services.py
+++ tests/components/sonos/test_services.py
@@ -27,11 +27,12 @@ async def test_media_player_join(
     soco_living_room = sonos_setup_two_speakers[0]
     soco_bedroom = sonos_setup_two_speakers[1]
 
+    # After dispatching the join to the speakers, the code waits for the
+    # group to be updated before returning. To simulate this we will create
+    # a ZGS event to simulate the speakers being grouped. This event is
+    # triggered by the firing of the join_complete_event in the join mock.
     join_complete_event = asyncio.Event()
 
-    # Mock the join method to set the event when called
-    # This is needed as the join method is called in a different thread
-    # and we need to wait for it to be called before firing the ZGS event
     def mock_join(*args, **kwargs):
         hass.loop.call_soon_threadsafe(join_complete_event.set)
 
@@ -56,8 +57,10 @@ async def test_media_player_join(
         )
         soco_living_room.zoneGroupTopology.subscribe.return_value._callback(event)
         soco_bedroom.zoneGroupTopology.subscribe.return_value._callback(event)
-
         await hass.async_block_till_done(wait_background_tasks=True)
+
+    # Code logs wanring messages if the join is not successful, so we check
+    # that no warning messages were logged.
     assert len(caplog.records) == 0
     assert soco_bedroom.join.call_count == 1
     assert soco_bedroom.join.call_args[0][0] == soco_living_room
@@ -102,7 +105,6 @@ async def test_media_player_join_timeout(
     soco_living_room = sonos_setup_two_speakers[0]
     soco_bedroom = sonos_setup_two_speakers[1]
 
-    # Call the join service and do not block
     with (
         patch(
             "homeassistant.components.sonos.speaker.asyncio.timeout", instant_timeout
@@ -132,16 +134,16 @@ async def test_media_player_unjoin(
     sonos_setup_two_speakers: list[MockSoCo],
     caplog: pytest.LogCaptureFixture,
 ) -> None:
-    """Test joining two speakers together."""
+    """Test unjoing two speaker."""
     soco_living_room = sonos_setup_two_speakers[0]
     soco_bedroom = sonos_setup_two_speakers[1]
 
+    # First group the speakers together
     event = create_zgs_sonos_event(
         "zgs_group.xml", soco_living_room, soco_bedroom, create_uui_ds=True
     )
     soco_living_room.zoneGroupTopology.subscribe.return_value._callback(event)
     soco_bedroom.zoneGroupTopology.subscribe.return_value._callback(event)
-
     await hass.async_block_till_done(wait_background_tasks=True)
 
     # Now that the speaker are joined, test unjoining
@@ -161,13 +163,15 @@ async def test_media_player_unjoin(
             blocking=False,
         )
         await unjoin_complete_event.wait()
+        # Fire the ZGS event to ungroup the speakers as the unjoin method is waiting
+        # for the speakers to be ungrouped.
         event = create_zgs_sonos_event(
             "zgs_two_single.xml", soco_living_room, soco_bedroom, create_uui_ds=False
         )
         soco_living_room.zoneGroupTopology.subscribe.return_value._callback(event)
         soco_bedroom.zoneGroupTopology.subscribe.return_value._callback(event)
-
         await hass.async_block_till_done(wait_background_tasks=True)
+
     assert len(caplog.records) == 0
     assert soco_bedroom.unjoin.call_count == 1
     assert soco_living_room.unjoin.call_count == 0
