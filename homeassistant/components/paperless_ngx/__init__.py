ef5009f6fdfbc75b3a07f825902e9a7faaa3e4b6
Florian von Garrel <f.von.garrel@outlook.de>
2025-05-25 17:56:07 +0000

    Add comment

--- homeassistant/components/paperless_ngx/__init__.py
+++ homeassistant/components/paperless_ngx/__init__.py
@@ -45,6 +45,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: PaperlessConfigEntry) ->
         )
         await statistics_coordinator.async_config_entry_first_refresh()
     except ConfigEntryNotReady as err:
+        # Catch the error so the integration doesn't fail just because one coordinator fails.
         LOGGER.warning("Could not initialize statistics coordinator: %s", err)
 
     try:
@@ -53,6 +54,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: PaperlessConfigEntry) ->
         )
         await status_coordinator.async_config_entry_first_refresh()
     except ConfigEntryNotReady as err:
+        # Catch the error so the integration doesn't fail just because one coordinator fails.
         LOGGER.warning("Could not initialize status coordinator: %s", err)
 
     entry.runtime_data = PaperlessData(
