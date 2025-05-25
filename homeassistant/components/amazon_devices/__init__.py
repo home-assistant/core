c9bea32f6fb894b506da77311b4b492d433e67ad
Simone Chemelli <simone.chemelli@gmail.com>
2025-05-25 16:15:41 +0000

    Add notify platform to Amazon Devices

--- homeassistant/components/amazon_devices/__init__.py
+++ homeassistant/components/amazon_devices/__init__.py
@@ -5,7 +5,10 @@ from homeassistant.core import HomeAssistant
 
 from .coordinator import AmazonConfigEntry, AmazonDevicesCoordinator
 
-PLATFORMS = [Platform.BINARY_SENSOR]
+PLATFORMS = [
+    Platform.BINARY_SENSOR,
+    Platform.NOTIFY,
+]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: AmazonConfigEntry) -> bool:
