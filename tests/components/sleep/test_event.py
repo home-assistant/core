6ed9aa0160462461f53e627b2124b9910ab97eab
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-05-25 20:04:39 +0000

    changes

--- tests/components/sleep/test_event.py
+++ tests/components/sleep/test_event.py
@@ -6,12 +6,6 @@ from freezegun.api import freeze_time
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.components.sleep.const import (
-    ATTR_EVENT,
-    ATTR_VALUE1,
-    ATTR_VALUE2,
-    ATTR_VALUE3,
-)
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
@@ -41,38 +35,83 @@ async def test_setup(
 
 
 @pytest.mark.parametrize(
-    ("entity", "event", "value1", "value2", "value3"),
+    ("entity", "payload"),
     [
-        ("sleep_tracking", "sleep_tracking_paused", None, None, None),
-        ("sleep_tracking", "sleep_tracking_resumed", None, None, None),
-        ("sleep_tracking", "sleep_tracking_started", None, None, None),
-        ("sleep_tracking", "sleep_tracking_stopped", None, None, None),
-        ("alarm_clock", "alarm_alert_dismiss", "1582719660934", "label", None),
-        ("alarm_clock", "alarm_alert_start", "1582719660934", "label", None),
-        ("alarm_clock", "alarm_rescheduled", None, None, None),
-        ("alarm_clock", "alarm_skip_next", "1582719660934", "label", None),
-        ("alarm_clock", "alarm_snooze_canceled", "1582719660934", "label", None),
-        ("alarm_clock", "alarm_snooze_clicked", "1582719660934", "label", None),
-        ("alarm_clock", "alarm_wake_up_check", None, None, None),
-        ("alarm_clock", "before_smart_period", "label", None, None),
-        ("alarm_clock", "show_skip_next_alarm", "1582719660934", "label", None),
-        ("alarm_clock", "smart_period", None, None, None),
-        ("alarm_clock", "time_to_bed_alarm_alert", "1582719660934", "label", None),
-        ("anti_snoring", "antisnoring", None, None, None),
-        ("sleep_apnea", "apnea_alarm", None, None, None),
-        ("lullaby", "lullaby_start", None, None, None),
-        ("lullaby", "lullaby_stop", None, None, None),
-        ("lullaby", "lullaby_volume_down", None, None, None),
-        ("sleep_phase", "awake", None, None, None),
-        ("sleep_phase", "deep_sleep", None, None, None),
-        ("sleep_phase", "light_sleep", None, None, None),
-        ("sleep_phase", "not_awake", None, None, None),
-        ("sleep_phase", "rem", None, None, None),
-        ("sound_recognition", "sound_event_baby", None, None, None),
-        ("sound_recognition", "sound_event_cough", None, None, None),
-        ("sound_recognition", "sound_event_laugh", None, None, None),
-        ("sound_recognition", "sound_event_snore", None, None, None),
-        ("sound_recognition", "sound_event_talk", None, None, None),
+        ("sleep_tracking", {"event": "sleep_tracking_paused"}),
+        ("sleep_tracking", {"event": "sleep_tracking_resumed"}),
+        ("sleep_tracking", {"event": "sleep_tracking_started"}),
+        ("sleep_tracking", {"event": "sleep_tracking_stopped"}),
+        (
+            "alarm_clock",
+            {
+                "event": "alarm_alert_dismiss",
+                "value1": "1582719660934",
+                "value2": "label",
+            },
+        ),
+        (
+            "alarm_clock",
+            {
+                "event": "alarm_alert_start",
+                "value1": "1582719660934",
+                "value2": "label",
+            },
+        ),
+        ("alarm_clock", {"event": "alarm_rescheduled"}),
+        (
+            "alarm_clock",
+            {"event": "alarm_skip_next", "value1": "1582719660934", "value2": "label"},
+        ),
+        (
+            "alarm_clock",
+            {
+                "event": "alarm_snooze_canceled",
+                "value1": "1582719660934",
+                "value2": "label",
+            },
+        ),
+        (
+            "alarm_clock",
+            {
+                "event": "alarm_snooze_clicked",
+                "value1": "1582719660934",
+                "value2": "label",
+            },
+        ),
+        ("alarm_clock", {"event": "alarm_wake_up_check"}),
+        ("alarm_clock", {"event": "before_smart_period", "value1": "label"}),
+        (
+            "alarm_clock",
+            {
+                "event": "show_skip_next_alarm",
+                "value1": "1582719660934",
+                "value2": "label",
+            },
+        ),
+        ("alarm_clock", {"event": "smart_period"}),
+        (
+            "alarm_clock",
+            {
+                "event": "time_to_bed_alarm_alert",
+                "value1": "1582719660934",
+                "value2": "label",
+            },
+        ),
+        ("anti_snoring", {"event": "antisnoring"}),
+        ("sleep_apnea", {"event": "apnea_alarm"}),
+        ("lullaby", {"event": "lullaby_start"}),
+        ("lullaby", {"event": "lullaby_stop"}),
+        ("lullaby", {"event": "lullaby_volume_down"}),
+        ("sleep_phase", {"event": "awake"}),
+        ("sleep_phase", {"event": "deep_sleep"}),
+        ("sleep_phase", {"event": "light_sleep"}),
+        ("sleep_phase", {"event": "not_awake"}),
+        ("sleep_phase", {"event": "rem"}),
+        ("sound_recognition", {"event": "sound_event_baby"}),
+        ("sound_recognition", {"event": "sound_event_cough"}),
+        ("sound_recognition", {"event": "sound_event_laugh"}),
+        ("sound_recognition", {"event": "sound_event_snore"}),
+        ("sound_recognition", {"event": "sound_event_talk"}),
     ],
 )
 @pytest.mark.usefixtures("entity_registry_enabled_by_default")
@@ -83,10 +122,7 @@ async def test_webhook_event(
     hass_client_no_auth: ClientSessionGenerator,
     snapshot: SnapshotAssertion,
     entity: str,
-    event: str,
-    value1: str | None,
-    value2: str | None,
-    value3: str | None,
+    payload: dict[str, str],
 ) -> None:
     """Test webhook events."""
 
@@ -100,13 +136,6 @@ async def test_webhook_event(
     assert state.state == STATE_UNKNOWN
 
     client = await hass_client_no_auth()
-    payload = {ATTR_EVENT: event}
-    if value1:
-        payload[ATTR_VALUE1] = value1
-    if value2:
-        payload[ATTR_VALUE2] = value2
-    if value3:
-        payload[ATTR_VALUE3] = value3
 
     response = await client.post("/api/webhook/webhook_id", json=payload)
     assert response.status == HTTPStatus.NO_CONTENT
