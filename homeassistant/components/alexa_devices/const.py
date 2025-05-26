c75f01979d2aa707516ef7b151729afb2c89b856
Simone Chemelli <simone.chemelli@gmail.com>
2025-05-26 20:10:24 +0000

    Add actions to Amazon Devices

--- homeassistant/components/alexa_devices/const.py
+++ homeassistant/components/alexa_devices/const.py
@@ -6,3 +6,9 @@ _LOGGER = logging.getLogger(__package__)
 
 DOMAIN = "alexa_devices"
 CONF_LOGIN_DATA = "login_data"
+
+# Services variables
+ATTR_CMD = "cmd"
+ATTR_SOUND = "sound"
+SERVICE_CUSTOM_COMMAND = "send_custom"
+SERVICE_SOUND_NOTIFICATION = "send_sound"
