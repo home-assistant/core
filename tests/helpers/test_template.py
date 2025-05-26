13a6c13b8939092aa07943ea82ded46cd4c6a382
Petro31 <35082313+Petro31@users.noreply.github.com>
2025-05-26 04:56:11 -0700

    Allow base64_encode to support bytes and strings (#145227)

--- tests/helpers/test_template.py
+++ tests/helpers/test_template.py
@@ -1640,12 +1640,17 @@ def test_from_hex(hass: HomeAssistant) -> None:
     )
 
 
-def test_base64_encode(hass: HomeAssistant) -> None:
+@pytest.mark.parametrize(
+    ("value_template", "expected"),
+    [
+        ('{{ "homeassistant" | base64_encode }}', "aG9tZWFzc2lzdGFudA=="),
+        ("{{ int('0F010003', base=16) | pack('>I') | base64_encode }}", "DwEAAw=="),
+        ("{{ 'AA01000200150020' | from_hex | base64_encode }}", "qgEAAgAVACA="),
+    ],
+)
+def test_base64_encode(hass: HomeAssistant, value_template: str, expected: str) -> None:
     """Test the base64_encode filter."""
-    assert (
-        template.Template('{{ "homeassistant" | base64_encode }}', hass).async_render()
-        == "aG9tZWFzc2lzdGFudA=="
-    )
+    assert template.Template(value_template, hass).async_render() == expected
 
 
 def test_base64_decode(hass: HomeAssistant) -> None:
