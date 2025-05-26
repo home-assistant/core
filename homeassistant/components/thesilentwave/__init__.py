d8a256dab08b43e75bcd874bd294ce5ba48c0e47
TheSilentWave <mail@thesilentwave.de>
2025-05-27 00:43:31 +0200

    Update homeassistant/components/thesilentwave/__init__.py
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/thesilentwave/__init__.py
+++ homeassistant/components/thesilentwave/__init__.py
@@ -14,9 +14,6 @@ CONFIG_SCHEMA = cv.config_entry_only_config_schema(domain="thesilentwave")
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup(hass, config):
-    """Set up the component."""
-    return True
 
 
 async def async_setup_entry(hass, entry):
