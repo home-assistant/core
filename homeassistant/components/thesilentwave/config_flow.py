6981d71ab20e497395821a627844ce2ade578d56
TheSilentWave <mail@thesilentwave.de>
2025-05-27 01:17:29 +0200

    scan interval fixed to 10 sec

--- homeassistant/components/thesilentwave/config_flow.py
+++ homeassistant/components/thesilentwave/config_flow.py
@@ -10,7 +10,7 @@ from pysilentwave import SilentWaveClient
 from pysilentwave.exceptions import SilentWaveError
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_HOST, CONF_NAME, CONF_SCAN_INTERVAL
+from homeassistant.const import CONF_HOST, CONF_NAME
 from homeassistant.exceptions import ConfigEntryNotReady
 
 _LOGGER = logging.getLogger(__name__)
@@ -42,7 +42,6 @@ class TheSilentWaveConfigFlow(config_entries.ConfigFlow, domain="thesilentwave")
                     data={
                         CONF_NAME: user_input[CONF_NAME],
                         CONF_HOST: user_input[CONF_HOST],
-                        CONF_SCAN_INTERVAL: user_input.get(CONF_SCAN_INTERVAL, 10),
                     },
                 )
 
@@ -59,9 +58,6 @@ class TheSilentWaveConfigFlow(config_entries.ConfigFlow, domain="thesilentwave")
                 {
                     vol.Required(CONF_NAME, default="TheSilentWave"): str,
                     vol.Required(CONF_HOST): str,
-                    vol.Optional(CONF_SCAN_INTERVAL, default=10): vol.All(
-                        vol.Coerce(int), vol.Range(min=5, max=300)
-                    ),
                 }
             ),
             errors=errors,
