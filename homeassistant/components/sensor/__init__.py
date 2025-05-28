0e071ef4ba1f3de96e5725106ad279d0b3d37aed
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2025-05-28 08:29:58 +0200

    Update homeassistant/components/sensor/__init__.py
    Co-authored-by: Erik Montnemery <erik@montnemery.com>

--- homeassistant/components/sensor/__init__.py
+++ homeassistant/components/sensor/__init__.py
@@ -474,8 +474,7 @@ class SensorEntity(Entity, cached_properties=CACHED_PROPERTIES_WITH_ATTR_):
     @property
     def __native_unit_of_measurement_compat(self) -> str | None:
         """Process ambiguous units."""
-        if (native_unit_of_measurement := self.native_unit_of_measurement) is None:
-            return None
+        native_unit_of_measurement = self.native_unit_of_measurement
         return AMBIGUOUS_UNITS.get(
             native_unit_of_measurement, native_unit_of_measurement
         )
