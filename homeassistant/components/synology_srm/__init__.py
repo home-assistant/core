c35c6e0a24e7d671d5e81f61231726dd0a481f5b
Brian Noriega <855736+noriega3@users.noreply.github.com>
2025-05-28 00:02:03 +0000

    Update synology_srm to ScannerEntity

--- homeassistant/components/synology_srm/__init__.py
+++ homeassistant/components/synology_srm/__init__.py
@@ -1,1 +1,38 @@
 """The Synology SRM component."""
+
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP, Platform
+from homeassistant.core import Event, HomeAssistant
+
+from .device_tracker import SynologySRMConfigEntry, SynologySrmDeviceScanner
+
+PLATFORMS = [Platform.DEVICE_TRACKER]
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, config_entry: SynologySRMConfigEntry
+) -> bool:
+    """Set up the Synology SRM from a config entry."""
+
+    scanner = SynologySrmDeviceScanner(hass, config_entry)
+    await scanner.setup()
+
+    async def async_close_connection(event: Event) -> None:
+        """Close Synology SRM on HA Stop."""
+        await scanner.close()
+
+    config_entry.async_on_unload(
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, async_close_connection)
+    )
+
+    config_entry.runtime_data = scanner
+
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant, config_entry: SynologySRMConfigEntry
+) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
