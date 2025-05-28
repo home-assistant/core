b0cc468030e831935013bf6c4d481eb89eb5223d
epenet <6771947+epenet@users.noreply.github.com>
2025-05-28 10:53:31 +0000

    Improve

--- pylint/plugins/hass_imports.py
+++ pylint/plugins/hass_imports.py
@@ -279,13 +279,19 @@ class HassImportsFormatChecker(BaseChecker):
         self, current_package: str, node: nodes.ImportFrom
     ) -> None:
         """Check for improper 'from ._ import _' invocations."""
-        if node.level <= 1 or (
-            not current_package.startswith("homeassistant.components.")
-            and not current_package.startswith("tests.components.")
+        if not current_package.startswith(
+            ("homeassistant.components.", "tests.components.")
         ):
             return
+
         split_package = current_package.split(".")
         current_component = split_package[2]
+
+        self._check_for_constant_alias(node, current_component, current_component)
+
+        if node.level <= 1:
+            # No need to check relative import
+            return
 
         if not node.modname and len(split_package) == node.level + 1:
             for name in node.names:
@@ -294,12 +300,8 @@ class HassImportsFormatChecker(BaseChecker):
                     self.add_message("hass-absolute-import", node=node)
                     return
             return
-
         if len(split_package) < node.level + 2:
             self.add_message("hass-absolute-import", node=node)
-            return
-
-        self._check_for_constant_alias(node, current_component, current_component)
 
     def _check_for_constant_alias(
         self,
