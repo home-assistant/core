f689bff99c46f2142e748a5b646a4408b27cf89b
Robin Lintermann <robin.lintermann@explicatis.com>
2025-05-28 13:02:11 +0000

    Improve smarla integration tests

--- tests/components/smarla/test_init.py
+++ tests/components/smarla/test_init.py
@@ -10,6 +10,18 @@ from . import setup_integration
 from tests.common import MockConfigEntry
 
 
+async def test_setup(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry, mock_federwiege: MagicMock
+) -> None:
+    """Test the smarla integration setup."""
+    assert await setup_integration(hass, mock_config_entry)
+    assert mock_config_entry.state is ConfigEntryState.LOADED
+
+    assert await hass.config_entries.async_unload(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+    assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
+
+
 async def test_init_invalid_auth(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
