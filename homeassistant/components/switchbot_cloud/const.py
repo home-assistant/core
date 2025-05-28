2f7db853560a53cd6a83f74d709df84406f17db0
xiaoling <540772694@qq.com>
2025-05-28 12:25:02 +0800

    debug: suite for latest version of switch-api

--- homeassistant/components/switchbot_cloud/const.py
+++ homeassistant/components/switchbot_cloud/const.py
@@ -1,7 +1,6 @@
 """Constants for the SwitchBot Cloud integration."""
 
 from datetime import timedelta
-from enum import Enum
 from typing import Final
 
 DOMAIN: Final = "switchbot_cloud"
@@ -16,25 +15,3 @@ VACUUM_FAN_SPEED_QUIET = "quiet"
 VACUUM_FAN_SPEED_STANDARD = "standard"
 VACUUM_FAN_SPEED_STRONG = "strong"
 VACUUM_FAN_SPEED_MAX = "max"
-
-
-class SwitchBotCloudFanCommandS(Enum):
-    """Command types currently supported by SwitchBot Cloud [Battery Circulator Fan] API."""
-
-    SET_WIND_SPEED = "setWindSpeed"
-    SET_WIND_MODE = "setWindMode"
-    SET_NIGHT_LIGHT_MODE = "setNightLightMode"
-
-
-class SwitchBotCloudFanMode(Enum):
-    """Fan mode types currently supported by SwitchBot Cloud [Battery Circulator Fan] API."""
-
-    DIRECT = "direct"
-    NATURAL = "natural"
-    SLEEP = "sleep"
-    BABY = "baby"
-
-    @classmethod
-    def get_all_obj(cls) -> list:
-        """Get all supported mode type as list."""
-        return [cls.DIRECT, cls.NATURAL, cls.SLEEP, cls.BABY]
--- homeassistant/components/switchbot_cloud/fan.py
+++ homeassistant/components/switchbot_cloud/fan.py
@@ -2,7 +2,11 @@
 
 from typing import Any
 
-from switchbot_api import CommonCommands
+from switchbot_api import (
+    BatteryCirculatorFanCommands,
+    BatteryCirculatorFanMode,
+    CommonCommands,
+)
 
 from homeassistant.components.fan import FanEntity, FanEntityFeature
 from homeassistant.config_entries import ConfigEntry
@@ -10,7 +14,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from . import SwitchbotCloudData
-from .const import DOMAIN, SwitchBotCloudFanCommandS, SwitchBotCloudFanMode
+from .const import DOMAIN
 from .entity import SwitchBotCloudEntity
 
 
@@ -38,22 +42,33 @@ class SwitchBotCloudFan(SwitchBotCloudEntity, FanEntity):
         | FanEntityFeature.TURN_OFF
         | FanEntityFeature.TURN_ON
     )
-    _attr_preset_modes = [item.value for item in SwitchBotCloudFanMode.get_all_obj()]
-    _attr_is_on = False
+    _attr_preset_modes = list(BatteryCirculatorFanMode)
+
+    _attr_is_on: bool | None = None
 
     @property
     def is_on(self) -> bool | None:
         """Return true if the entity is on."""
-        if self.percentage == 0 and self.preset_mode is None:
-            response = self.coordinator.data
-            if response:
-                mode = response.get("mode")
-                fan_speed = response.get("fanSpeed")
-                power = response.get("power")
-                self.preset_mode = mode if mode else SwitchBotCloudFanMode.DIRECT.value
-                self.percentage = fan_speed if fan_speed else -1
-                return power is not None and "on" in power
-        return super().is_on
+        if (
+            self._attr_is_on is None
+            and self.percentage == 0
+            and self.preset_mode is None
+        ):
+            response: dict | None = self.coordinator.data
+            assert response is not None
+            power: str | None = response.get("power")
+            mode: str | None = response.get("mode")
+            fan_speed: str | None = response.get("fanSpeed")
+            assert fan_speed is not None
+            self.preset_mode = mode if mode else BatteryCirculatorFanMode.DIRECT.value
+            self.percentage = (
+                int(fan_speed)
+                if self.preset_mode in BatteryCirculatorFanMode.DIRECT.value
+                else 0
+            )
+            assert power is not None
+            self._attr_is_on = "on" in power
+        return self._attr_is_on
 
     async def async_turn_on(
         self,
@@ -62,38 +77,58 @@ class SwitchBotCloudFan(SwitchBotCloudEntity, FanEntity):
         **kwargs: Any,
     ) -> None:
         """Turn on the fan."""
-        response = await self._api.get_status(self._attr_unique_id)
-        self.percentage = response.get("fanSpeed")
-        self.preset_mode = response.get("mode")
-        self._attr_preset_mode = self.preset_mode
-        await self.send_api_command(CommonCommands.ON)
+        response: dict | None = await self._api.get_status(self.unique_id)
+        assert response is not None
+        power: str | None = response.get("power")
+        if self._attr_is_on is False and (power and "off" in power):
+            await self.send_api_command(CommonCommands.ON)
+            self._attr_is_on = True
         self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the fan."""
-        await self.send_api_command(CommonCommands.OFF)
-        self.percentage = -1
-        self.preset_mode = None
+
+        response: dict | None = await self._api.get_status(self.unique_id)
+        assert response is not None
+        power: str | None = response.get("power")
+        if self._attr_is_on is True and (power and "on" in power):
+            await self.send_api_command(CommonCommands.OFF)
+            self._attr_is_on = False
         self.async_write_ha_state()
 
     async def async_set_percentage(self, percentage: int) -> None:
         """Set the speed of the fan, as a percentage."""
         assert 0 <= percentage <= 100
-        response = await self._api.get_status(self._attr_unique_id)
-        if SwitchBotCloudFanMode.DIRECT.value in response.get("mode"):
-            await self.send_api_command(
-                command=SwitchBotCloudFanCommandS.SET_WIND_SPEED,
-                parameters=str(percentage),
-            )
+        response: dict | None = await self._api.get_status(self.unique_id)
+        assert response is not None
+        mode: str | None = response.get("mode")
+        if mode is not None:
+            if mode in BatteryCirculatorFanMode.DIRECT.value:
+                await self.send_api_command(
+                    command=BatteryCirculatorFanCommands.SET_WIND_SPEED,
+                    parameters=str(percentage),
+                )
+                self.percentage = percentage
+                self.preset_mode = BatteryCirculatorFanMode.DIRECT.value
+            else:
+                self.percentage = 0
+                self.preset_mode = mode
+        self.async_write_ha_state()
 
     async def async_set_preset_mode(self, preset_mode: str) -> None:
         """Set new preset mode."""
-        assert preset_mode in [
-            item.value for item in SwitchBotCloudFanMode.get_all_obj()
-        ]
-        await self.send_api_command(
-            command=SwitchBotCloudFanCommandS.SET_WIND_MODE, parameters=preset_mode
-        )
-        self.preset_mode = preset_mode
-        self._attr_preset_mode = preset_mode
+        assert preset_mode in [item.value for item in list(BatteryCirculatorFanMode)]
+        if self._attr_is_on:
+            await self.send_api_command(
+                command=BatteryCirculatorFanCommands.SET_WIND_MODE,
+                parameters=preset_mode,
+            )
+            self.preset_mode = preset_mode
+            response: dict | None = await self._api.get_status(self.unique_id)
+            assert response is not None
+            fan_speed: int | None = response.get("fanSpeed")
+            if preset_mode in BatteryCirculatorFanMode.DIRECT.value:
+                self.percentage = fan_speed if fan_speed else 0
+            else:
+                self.percentage = 0
         self.async_write_ha_state()
