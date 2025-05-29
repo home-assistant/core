c7869bd8c09174463a4446c8c9588f5f4dc07302
hanwg <han.wuguang@gmail.com>
2025-05-29 03:35:44 +0000

    added tests for reauth

--- tests/components/telegram_bot/test_config_flow.py
+++ tests/components/telegram_bot/test_config_flow.py
@@ -2,7 +2,8 @@
 
 from unittest.mock import patch
 
-from telegram.error import NetworkError
+from telegram import User
+from telegram.error import InvalidToken, NetworkError
 
 from homeassistant.components.telegram_bot.const import (
     ATTR_PARSER,
@@ -11,7 +12,7 @@ from homeassistant.components.telegram_bot.const import (
     PLATFORM_BROADCAST,
     PLATFORM_WEBHOOKS,
 )
-from homeassistant.const import CONF_PLATFORM
+from homeassistant.const import CONF_API_KEY, CONF_PLATFORM
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
@@ -75,6 +76,7 @@ async def test_reconfigure_flow_broadcast(
 
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "reconfigure_successful"
+    assert mock_webhooks_config_entry.data[CONF_PLATFORM] == PLATFORM_BROADCAST
 
 
 async def test_reconfigure_flow_webhooks(
@@ -117,9 +119,9 @@ async def test_reconfigure_flow_invalid_proxy(
     assert result["errors"] is None
 
     with patch(
-        "homeassistant.components.telegram_bot.config_flow",
+        "homeassistant.components.telegram_bot.config_flow.Bot.get_me",
     ) as mock_bot:
-        mock_bot.get_me.side_effect = NetworkError("mock invalid proxy")
+        mock_bot.side_effect = NetworkError("mock invalid proxy")
 
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -133,3 +135,59 @@ async def test_reconfigure_flow_invalid_proxy(
     assert result["step_id"] == "reconfigure"
     assert result["type"] is FlowResultType.FORM
     assert result["errors"]["base"] == "invalid_proxy_url"
+
+
+async def test_reauth_flow(
+    hass: HomeAssistant, mock_webhooks_config_entry: MockConfigEntry
+) -> None:
+    """Test a reauthentication flow."""
+    mock_webhooks_config_entry.add_to_hass(hass)
+
+    result = await mock_webhooks_config_entry.start_reauth_flow(
+        hass, data={CONF_API_KEY: "dummy"}
+    )
+    assert result["step_id"] == "reauth_confirm"
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] is None
+
+    with patch(
+        "homeassistant.components.telegram_bot.config_flow.Bot.get_me",
+        return_value=User(123456, "Testbot", True),
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_API_KEY: "new mock api key"},
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "reauth_successful"
+    assert mock_webhooks_config_entry.data[CONF_API_KEY] == "new mock api key"
+
+
+async def test_reauth_flow_error(
+    hass: HomeAssistant, mock_webhooks_config_entry: MockConfigEntry
+) -> None:
+    """Test a reauthentication flow."""
+    mock_webhooks_config_entry.add_to_hass(hass)
+
+    result = await mock_webhooks_config_entry.start_reauth_flow(
+        hass, data={CONF_API_KEY: "dummy"}
+    )
+    assert result["step_id"] == "reauth_confirm"
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] is None
+
+    with patch(
+        "homeassistant.components.telegram_bot.config_flow.Bot.get_me"
+    ) as mock_bot:
+        mock_bot.side_effect = InvalidToken("mock invalid token error")
+
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_API_KEY: "new mock api key"},
+        )
+        await hass.async_block_till_done()
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"]["base"] == "invalid_api_key"
