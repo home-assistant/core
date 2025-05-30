555f8ab587964b0d9f3bfa23fb6446d3730762b1
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-05-30 11:31:03 +0800

    suite for |Smart Lock Lite| and |Smart Lock Ultra|

--- homeassistant/components/switchbot_cloud/sensor.py
+++ homeassistant/components/switchbot_cloud/sensor.py
@@ -136,6 +136,8 @@ SENSOR_DESCRIPTIONS_BY_DEVICE_TYPES = {
     ),
     "Smart Lock Pro": (BATTERY_DESCRIPTION,),
     "Smart Lock": (BATTERY_DESCRIPTION,),
+    "Smart Lock Lite": (BATTERY_DESCRIPTION,),
+    "Smart Lock Ultra": (BATTERY_DESCRIPTION,),
 }
 
 
