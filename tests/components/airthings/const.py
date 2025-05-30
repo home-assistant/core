8f43881640296be54bf854f54845433488836f6f
Ståle Storø Hauknes <stale.semb@airthings.com>
2025-05-30 23:49:35 +0200

    Add test

---
+++ tests/components/airthings/const.py
@@ -0,0 +1,9 @@
+"""Constants for Airthings integration tests."""
+
+from homeassistant.components.airthings.const import CONF_SECRET
+from homeassistant.const import CONF_ID
+
+TEST_DATA = {
+    CONF_ID: "client_id",
+    CONF_SECRET: "secret",
+}
