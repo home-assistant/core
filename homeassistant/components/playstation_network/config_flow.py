08bbe9ef2ee5ecd540b5bf3e3f744dd93bcd1857
Jack Powell <jackattack51@gmail.com>
2025-05-31 00:33:42 +0000

    Suggested changes

--- homeassistant/components/playstation_network/config_flow.py
+++ homeassistant/components/playstation_network/config_flow.py
@@ -5,8 +5,10 @@ from typing import Any
 
 from psnawp_api.core.psnawp_exceptions import (
     PSNAWPAuthenticationError,
+    PSNAWPClientError,
     PSNAWPInvalidTokenError,
     PSNAWPNotFoundError,
+    PSNAWPServerError,
 )
 from psnawp_api.models.user import User
 from psnawp_api.utils.misc import parse_npsso_token
@@ -45,6 +47,10 @@ class PlaystationNetworkConfigFlow(ConfigFlow, domain=DOMAIN):
                     errors["base"] = "invalid_auth"
                 except PSNAWPNotFoundError:
                     errors["base"] = "invalid_account"
+                except PSNAWPServerError:
+                    errors["base"] = "cannot_connect"
+                except PSNAWPClientError:
+                    errors["base"] = "cannot_connect"
                 except Exception:
                     _LOGGER.exception("Unexpected exception")
                     errors["base"] = "unknown"
