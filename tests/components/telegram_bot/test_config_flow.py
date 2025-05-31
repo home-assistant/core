dfe1b03c5358a8f74958710a8a4d754f83c522eb
hanwg <han.wuguang@gmail.com>
2025-05-31 17:19:21 +0000

    added test for duplicated config entry for user step

--- tests/components/telegram_bot/test_config_flow.py
+++ tests/components/telegram_bot/test_config_flow.py
@@ -518,3 +518,42 @@ async def test_import_multiple(
 
         assert result["type"] is FlowResultType.ABORT
         assert result["reason"] == "already_configured"
+
+
+async def test_duplicate_entry(hass: HomeAssistant) -> None:
+    """Test user flow with duplicated entries."""
+
+    data = {
+        CONF_PLATFORM: PLATFORM_BROADCAST,
+        CONF_API_KEY: "mock api key",
+    }
+
+    with patch(
+        "homeassistant.components.telegram_bot.config_flow.Bot.get_me",
+        return_value=User(123456, "Testbot", True),
+    ):
+        # test: import first entry success
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": SOURCE_USER},
+            data=data,
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] is FlowResultType.CREATE_ENTRY
+        assert result["data"][CONF_PLATFORM] == PLATFORM_BROADCAST
+        assert result["data"][CONF_API_KEY] == "mock api key"
+        assert result["options"][ATTR_PARSER] == PARSER_MD
+
+        # test: import 2nd entry failed due to duplicate
+
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={"source": SOURCE_USER},
+            data=data,
+        )
+        await hass.async_block_till_done()
+
+        assert result["type"] is FlowResultType.ABORT
+        assert result["reason"] == "already_configured"
