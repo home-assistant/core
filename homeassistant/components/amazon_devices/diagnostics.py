7b6d16af27b7d9c97b548481dd9086ff7d0896b2
Simone Chemelli <simone.chemelli@gmail.com>
2025-06-01 12:02:40 +0000

    add async_get_device_diagnostics()

--- homeassistant/components/amazon_devices/diagnostics.py
+++ homeassistant/components/amazon_devices/diagnostics.py
@@ -4,9 +4,12 @@ from __future__ import annotations
 
 from typing import Any
 
+from aioamazondevices.api import AmazonDevice
+
 from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.const import CONF_NAME, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceEntry
 
 from .coordinator import AmazonConfigEntry
 
@@ -21,22 +24,7 @@ async def async_get_config_entry_diagnostics(
     coordinator = entry.runtime_data
 
     devices: list[dict[str, dict[str, Any]]] = [
-        {
-            device.serial_number: {
-                "account name": device.account_name,
-                "capabilities": device.capabilities,
-                "device family": device.device_family,
-                "device type": device.device_type,
-                "device cluster members": device.device_cluster_members,
-                "online": device.online,
-                "serial number": device.serial_number,
-                "software version": device.software_version,
-                "do not disturb": device.do_not_disturb,
-                "response style": device.response_style,
-                "bluetooth state": device.bluetooth_state,
-            }
-        }
-        for device in coordinator.data.values()
+        build_device_data(device) for device in coordinator.data.values()
     ]
 
     return {
@@ -46,4 +34,33 @@ async def async_get_config_entry_diagnostics(
             "last_exception": repr(coordinator.last_exception),
             "devices": devices,
         },
+    }
+
+
+async def async_get_device_diagnostics(
+    hass: HomeAssistant, entry: AmazonConfigEntry, device_entry: DeviceEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a device."""
+
+    coordinator = entry.runtime_data
+
+    assert device_entry.serial_number
+
+    return build_device_data(coordinator.data[device_entry.serial_number])
+
+
+def build_device_data(device: AmazonDevice) -> dict[str, Any]:
+    """Build device data for diagnostics."""
+    return {
+        "account name": device.account_name,
+        "capabilities": device.capabilities,
+        "device family": device.device_family,
+        "device type": device.device_type,
+        "device cluster members": device.device_cluster_members,
+        "online": device.online,
+        "serial number": device.serial_number,
+        "software version": device.software_version,
+        "do not disturb": device.do_not_disturb,
+        "response style": device.response_style,
+        "bluetooth state": device.bluetooth_state,
     }
