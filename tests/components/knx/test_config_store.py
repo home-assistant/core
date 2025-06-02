b2b366b1404b26277351729248984ce6e4fee9ec
farmio <farmio@alphart.net>
2025-06-02 21:36:33 +0200

    More tests

--- tests/components/knx/test_config_store.py
+++ tests/components/knx/test_config_store.py
@@ -380,6 +380,7 @@ async def test_validate_entity(
     await knx.setup_integration()
     client = await hass_ws_client(hass)
 
+    # valid data
     await client.send_json_auto_id(
         {
             "type": "knx/validate_entity",
@@ -409,6 +410,37 @@ async def test_validate_entity(
     assert res["success"], res
     assert res["result"]["success"] is False
     assert res["result"]["errors"][0]["path"] == ["data", "knx", "ga_switch", "write"]
+    assert res["result"]["errors"][0]["error_message"] == "required key not provided"
+    assert res["result"]["error_base"].startswith("required key not provided")
+
+    # invalid group_select data
+    await client.send_json_auto_id(
+        {
+            "type": "knx/validate_entity",
+            "platform": Platform.LIGHT,
+            "data": {
+                "entity": {"name": "test_name"},
+                "knx": {
+                    "color": {
+                        "ga_red_brightness": {"write": "1/2/3"},
+                        "ga_green_brightness": {"write": "1/2/4"},
+                        # ga_blue_brightness is missing - which is required
+                    }
+                },
+            },
+        }
+    )
+    res = await client.receive_json()
+    assert res["success"], res
+    assert res["result"]["success"] is False
+    # This shall test that a required key of the second GroupSelect schema is missing
+    # and not not yield the "extra keys not allowed" error of the first GroupSelect Schema
+    assert res["result"]["errors"][0]["path"] == [
+        "data",
+        "knx",
+        "color",
+        "ga_blue_brightness",
+    ]
     assert res["result"]["errors"][0]["error_message"] == "required key not provided"
     assert res["result"]["error_base"].startswith("required key not provided")
 
