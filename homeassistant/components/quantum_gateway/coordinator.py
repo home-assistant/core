440170f81033fc72e164da09f6e32145678de16e
Tamer Wahba <tamwahba@hotmail.com>
2025-06-01 21:56:03 -0400

    add data coordinator to wrap device scanner

---
+++ homeassistant/components/quantum_gateway/coordinator.py
@@ -0,0 +1,55 @@
+"""Coordinator for the Quantum Gateway integration."""
+
+from typing import override
+
+from quantum_gateway import QuantumGatewayScanner
+
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_SCAN_INTERVAL, CONF_SSL
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import LOGGER
+
+
+def get_scanner(host: str, password: str, use_https: bool) -> QuantumGatewayScanner:
+    """Return a Quantum Gateway scanner."""
+    return QuantumGatewayScanner(host, password, use_https)
+
+
+class QuantumGatewayCoordinator(DataUpdateCoordinator[dict[str, str]]):
+    """Class to manage fetching data from the Quantum Gateway router."""
+
+    def __init__(self, hass: HomeAssistant, options: ConfigType) -> None:
+        """Initialize the data coordinator."""
+        update_interval = options.get(CONF_SCAN_INTERVAL)
+        super().__init__(
+            hass,
+            LOGGER,
+            name="Quantum Gateway device scanner",
+            update_interval=update_interval,
+            always_update=False,
+        )
+        self.options = options
+        self._scanner: QuantumGatewayScanner | None = None
+
+    @override
+    async def _async_setup(self):
+        """Set up the Quantum Gateway device scanner."""
+        self._scanner = await self.hass.async_add_executor_job(
+            get_scanner,
+            self.options[CONF_HOST],
+            self.options[CONF_PASSWORD],
+            self.options[CONF_SSL],
+        )
+
+    @override
+    async def _async_update_data(self):
+        """Fetch the latest data from the Quantum Gateway."""
+        if self._scanner is None:
+            raise UpdateFailed("Scanner not initialized.")
+        try:
+            macs = await self.hass.async_add_executor_job(self._scanner.scan_devices)
+            return {mac: self._scanner.get_device_name(mac) for mac in macs}
+        except Exception as err:
+            raise UpdateFailed(f"Error scanning for devices: {err}") from err
