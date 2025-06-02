beed8144efbe143a6995d1d3f4f5d93bd9cf5f66
epenet <6771947+epenet@users.noreply.github.com>
2025-06-02 05:43:17 +0000

    Remove unnecessary DOMAIN alias in tests (l-r)

--- tests/components/lirc/test_init.py
+++ tests/components/lirc/test_init.py
@@ -2,6 +2,7 @@
 
 from unittest.mock import Mock, patch
 
+from homeassistant.components.lirc import DOMAIN
 from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
 from homeassistant.helpers import issue_registry as ir
 from homeassistant.setup import async_setup_component
@@ -13,19 +14,15 @@ async def test_repair_issue_is_created(
     issue_registry: ir.IssueRegistry,
 ) -> None:
     """Test repair issue is created."""
-    from homeassistant.components.lirc import (  # pylint: disable=import-outside-toplevel
-        DOMAIN as LIRC_DOMAIN,
-    )
-
     assert await async_setup_component(
         hass,
-        LIRC_DOMAIN,
+        DOMAIN,
         {
-            LIRC_DOMAIN: {},
+            DOMAIN: {},
         },
     )
     await hass.async_block_till_done()
     assert (
         HOMEASSISTANT_DOMAIN,
-        f"deprecated_system_packages_yaml_integration_{LIRC_DOMAIN}",
+        f"deprecated_system_packages_yaml_integration_{DOMAIN}",
     ) in issue_registry.issues
