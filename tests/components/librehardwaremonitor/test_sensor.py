ad00c2aafde2e2194f663a547e847d9f89cf07ce
Sab44 <64696149+Sab44@users.noreply.github.com>
2025-06-03 17:15:34 +0200

    add snapshot test to assert created entities state

--- tests/components/librehardwaremonitor/test_sensor.py
+++ tests/components/librehardwaremonitor/test_sensor.py
@@ -6,6 +6,7 @@ from unittest.mock import AsyncMock
 
 from freezegun.api import FrozenDateTimeFactory
 from librehardwaremonitor_api import LibreHardwareMonitorConnectionError
+from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.librehardwaremonitor.const import DEFAULT_SCAN_INTERVAL
 from homeassistant.const import STATE_UNAVAILABLE, STATE_UNKNOWN
@@ -19,24 +20,15 @@ async def test_sensors_are_created(
     mock_lhm_client: AsyncMock,
     mock_config_entry: MockConfigEntry,
     freezer: FrozenDateTimeFactory,
+    snapshot: SnapshotAssertion,
 ) -> None:
     """Test sensors are created."""
     mock_config_entry.add_to_hass(hass)
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
-    entity_id = "sensor.amd_ryzen_7_7800x3d_package_temperature"
-
-    state = hass.states.get(entity_id)
-
-    assert state
-    assert state.state != STATE_UNAVAILABLE
-    assert state.name == "AMD Ryzen 7 7800X3D Package Temperature"
-    assert state.state == "39.4"
-    assert state.attributes
-    assert state.attributes.get("min_value") == "37.4"
-    assert state.attributes.get("max_value") == "73.0"
-    assert state.attributes.get("unit_of_measurement") == "°C"
+    initial_states = hass.states.async_all()
+    assert initial_states == snapshot(name="valid_sensor_data")
 
     mock_lhm_client.get_data.side_effect = LibreHardwareMonitorConnectionError()
 
@@ -44,9 +36,8 @@ async def test_sensors_are_created(
     async_fire_time_changed(hass)
     await hass.async_block_till_done()
 
-    state = hass.states.get(entity_id)
-    assert state
-    assert state.state == STATE_UNAVAILABLE
+    unavailable_states = hass.states.async_all()
+    assert all(state.state == STATE_UNAVAILABLE for state in unavailable_states)
 
     mock_lhm_client.get_data.side_effect = None
 
@@ -54,11 +45,8 @@ async def test_sensors_are_created(
     async_fire_time_changed(hass)
     await hass.async_block_till_done()
 
-    state = hass.states.get(entity_id)
-    assert state
-    assert state.state != STATE_UNAVAILABLE
-    assert state.name == "AMD Ryzen 7 7800X3D Package Temperature"
-    assert state.state == "39.4"
+    recovered_states = hass.states.async_all()
+    assert recovered_states == snapshot(name="valid_sensor_data")
 
 
 async def test_sensors_are_updated(
@@ -78,7 +66,6 @@ async def test_sensors_are_updated(
 
     assert state
     assert state.state != STATE_UNAVAILABLE
-    assert state.name == "AMD Ryzen 7 7800X3D Package Temperature"
     assert state.state == "39.4"
 
     updated_data = copy.deepcopy(mock_lhm_client.get_data.return_value)
@@ -93,7 +80,6 @@ async def test_sensors_are_updated(
 
     assert state
     assert state.state != STATE_UNAVAILABLE
-    assert state.name == "AMD Ryzen 7 7800X3D Package Temperature"
     assert state.state == "42.1"
 
     mock_lhm_client.reset_mock()
@@ -113,7 +99,6 @@ async def test_sensor_state_is_unknown_when_lhm_indicates_missing_value(
 
     assert state
     assert state.state == STATE_UNKNOWN
-    assert state.name == "MSI MAG B650M MORTAR WIFI (MS-7D76) System Fan #1 Fan"
 
 
 async def test_sensor_state_is_unknown_when_no_sensor_data_is_provided(
@@ -133,7 +118,6 @@ async def test_sensor_state_is_unknown_when_no_sensor_data_is_provided(
 
     assert state
     assert state.state != STATE_UNAVAILABLE
-    assert state.name == "AMD Ryzen 7 7800X3D Package Temperature"
     assert state.state == "39.4"
 
     del mock_lhm_client.get_data.return_value.sensor_data["amdcpu-0-temperature-3"]
@@ -146,4 +130,3 @@ async def test_sensor_state_is_unknown_when_no_sensor_data_is_provided(
 
     assert state
     assert state.state == STATE_UNKNOWN
-    assert state.name == "AMD Ryzen 7 7800X3D Package Temperature"
