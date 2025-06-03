db4bf4b1fef8d6642b24eaa42572aa5a124ce6d4
Sab44 <64696149+Sab44@users.noreply.github.com>
2025-06-03 08:52:47 +0200

    patch async_setup_entry to avoid leaking setups

--- tests/components/librehardwaremonitor/test_config_flow.py
+++ tests/components/librehardwaremonitor/test_config_flow.py
@@ -19,7 +19,10 @@ from tests.common import MockConfigEntry
 
 
 async def test_create_entry(
-    hass: HomeAssistant, mock_lhm_client: AsyncMock, mock_config_entry: MockConfigEntry
+    hass: HomeAssistant,
+    mock_setup_entry: AsyncMock,
+    mock_lhm_client: AsyncMock,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test that a complete config entry is created."""
     result = await hass.config_entries.flow.async_init(
@@ -28,6 +31,7 @@ async def test_create_entry(
 
     assert result
     assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["result"].unique_id is None
 
     mock_config_entry = result["result"]
     assert (
@@ -39,9 +43,11 @@ async def test_create_entry(
         CONF_PORT: VALID_CONFIG[CONF_PORT],
     }
 
+    assert mock_setup_entry.call_count == 1
+
 
 async def test_errors_and_flow_recovery(
-    hass: HomeAssistant, mock_lhm_client: AsyncMock
+    hass: HomeAssistant, mock_setup_entry: AsyncMock, mock_lhm_client: AsyncMock
 ) -> None:
     """Test that errors are shown as expected."""
     mock_lhm_client.get_data.side_effect = LibreHardwareMonitorConnectionError()
@@ -73,9 +79,11 @@ async def test_errors_and_flow_recovery(
     assert result
     assert result["type"] is FlowResultType.CREATE_ENTRY
 
+    assert mock_setup_entry.call_count == 1
+
 
 async def test_lhm_server_already_exists(
-    hass: HomeAssistant, mock_config_entry: MockConfigEntry
+    hass: HomeAssistant, mock_setup_entry: AsyncMock, mock_config_entry: MockConfigEntry
 ) -> None:
     """Test we only allow a single entry per server."""
     mock_config_entry.add_to_hass(hass)
@@ -87,3 +95,5 @@ async def test_lhm_server_already_exists(
     assert result
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "already_configured"
+
+    assert mock_setup_entry.call_count == 0
