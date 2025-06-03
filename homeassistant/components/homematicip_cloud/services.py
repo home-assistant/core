5ef583e70d63b99f8b1afebc1d09c8cc694c8a28
epenet <6771947+epenet@users.noreply.github.com>
2025-06-03 11:12:27 +0200

    Fix incorrect merge

--- homeassistant/components/homematicip_cloud/services.py
+++ homeassistant/components/homematicip_cloud/services.py
@@ -123,7 +123,7 @@ SCHEMA_SET_HOME_COOLING_MODE = vol.Schema(
 async def async_setup_services(hass: HomeAssistant) -> None:
     """Set up the HomematicIP Cloud services."""
 
-    @verify_domain_entity_control(hass, DOMAIN)
+    @verify_domain_entity_control(DOMAIN)
     async def async_call_hmipc_service(service: ServiceCall) -> None:
         """Call correct HomematicIP Cloud service."""
         service_name = service.service
