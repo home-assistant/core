7af638b5f5fce6a266e9bd7dde756fce9fb6057d
Adrian Freund <adrian@freund.io>
2025-06-05 12:55:51 +0200

    Add air quality support to Open Meteo integration
    This PR depends on an accompanying PR in the open_meteo package: https://github.com/frenck/python-open-meteo/pull/1042
    To keep this PR small it currently only adds state attributes to the
    weather entity.
    
    Future PRs can add support for those attributes to the forecast service
    (though the air quality api only supports hourly data, so we would have
    to calculate the daily data ourselfes based on that) and provide further
    air quality metrics not supported by weather entities (such as pollen)
    as sensor entities.

--- homeassistant/components/open_meteo/coordinator.py
+++ homeassistant/components/open_meteo/coordinator.py
@@ -2,7 +2,12 @@
 
 from __future__ import annotations
 
+import asyncio
+from typing import NamedTuple
+
 from open_meteo import (
+    AirQuality,
+    AirQualityParameters,
     DailyParameters,
     Forecast,
     HourlyParameters,
@@ -24,7 +29,14 @@ from .const import DOMAIN, LOGGER, SCAN_INTERVAL
 type OpenMeteoConfigEntry = ConfigEntry[OpenMeteoDataUpdateCoordinator]
 
 
-class OpenMeteoDataUpdateCoordinator(DataUpdateCoordinator[Forecast]):
+class OpenMeteoData(NamedTuple):
+    """A named tuple representing open meteo data."""
+
+    forecast: Forecast
+    air_quality: AirQuality
+
+
+class OpenMeteoDataUpdateCoordinator(DataUpdateCoordinator[OpenMeteoData]):
     """A Open-Meteo Data Update Coordinator."""
 
     config_entry: OpenMeteoConfigEntry
@@ -41,33 +53,46 @@ class OpenMeteoDataUpdateCoordinator(DataUpdateCoordinator[Forecast]):
         session = async_get_clientsession(hass)
         self.open_meteo = OpenMeteo(session=session)
 
-    async def _async_update_data(self) -> Forecast:
+    async def _async_update_data(self) -> OpenMeteoData:
         """Fetch data from Sensibo."""
         if (zone := self.hass.states.get(self.config_entry.data[CONF_ZONE])) is None:
             raise UpdateFailed(f"Zone '{self.config_entry.data[CONF_ZONE]}' not found")
 
         try:
-            return await self.open_meteo.forecast(
-                latitude=zone.attributes[ATTR_LATITUDE],
-                longitude=zone.attributes[ATTR_LONGITUDE],
-                current_weather=True,
-                daily=[
-                    DailyParameters.PRECIPITATION_SUM,
-                    DailyParameters.TEMPERATURE_2M_MAX,
-                    DailyParameters.TEMPERATURE_2M_MIN,
-                    DailyParameters.WEATHER_CODE,
-                    DailyParameters.WIND_DIRECTION_10M_DOMINANT,
-                    DailyParameters.WIND_SPEED_10M_MAX,
-                ],
-                hourly=[
-                    HourlyParameters.PRECIPITATION,
-                    HourlyParameters.TEMPERATURE_2M,
-                    HourlyParameters.WEATHER_CODE,
-                ],
-                precipitation_unit=PrecipitationUnit.MILLIMETERS,
-                temperature_unit=TemperatureUnit.CELSIUS,
-                timezone="UTC",
-                wind_speed_unit=WindSpeedUnit.KILOMETERS_PER_HOUR,
+            return OpenMeteoData(
+                *await asyncio.gather(
+                    self.open_meteo.forecast(
+                        latitude=zone.attributes[ATTR_LATITUDE],
+                        longitude=zone.attributes[ATTR_LONGITUDE],
+                        current_weather=True,
+                        daily=[
+                            DailyParameters.PRECIPITATION_SUM,
+                            DailyParameters.TEMPERATURE_2M_MAX,
+                            DailyParameters.TEMPERATURE_2M_MIN,
+                            DailyParameters.WEATHER_CODE,
+                            DailyParameters.WIND_DIRECTION_10M_DOMINANT,
+                            DailyParameters.WIND_SPEED_10M_MAX,
+                        ],
+                        hourly=[
+                            HourlyParameters.PRECIPITATION,
+                            HourlyParameters.TEMPERATURE_2M,
+                            HourlyParameters.WEATHER_CODE,
+                        ],
+                        precipitation_unit=PrecipitationUnit.MILLIMETERS,
+                        temperature_unit=TemperatureUnit.CELSIUS,
+                        timezone="UTC",
+                        wind_speed_unit=WindSpeedUnit.KILOMETERS_PER_HOUR,
+                    ),
+                    self.open_meteo.air_quality(
+                        latitude=zone.attributes[ATTR_LATITUDE],
+                        longitude=zone.attributes[ATTR_LONGITUDE],
+                        timezone="UTC",
+                        current=[
+                            AirQualityParameters.OZONE,
+                            AirQualityParameters.UV_INDEX,
+                        ],
+                    ),
+                )
             )
         except OpenMeteoError as err:
             raise UpdateFailed("Open-Meteo API communication error") from err
