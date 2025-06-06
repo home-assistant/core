1e9abdb4c4350e1a51848397b5ef9d5dcc787795
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-06-06 23:52:01 +0800

    Circulator Fan has no battery

--- homeassistant/components/switchbot_cloud/sensor.py
+++ homeassistant/components/switchbot_cloud/sensor.py
@@ -92,7 +92,6 @@ CO2_DESCRIPTION = SensorEntityDescription(
 SENSOR_DESCRIPTIONS_BY_DEVICE_TYPES = {
     "Bot": (BATTERY_DESCRIPTION,),
     "Battery Circulator Fan": (BATTERY_DESCRIPTION,),
-    "Circulator Fan": (BATTERY_DESCRIPTION,),
     "Meter": (
         TEMPERATURE_DESCRIPTION,
         HUMIDITY_DESCRIPTION,
