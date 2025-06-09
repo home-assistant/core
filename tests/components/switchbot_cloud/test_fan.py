fc3b9440218ce86882c27dfb108205619db81b78
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-06-09 15:55:18 +0800

    added fan turn_off test func

--- tests/components/switchbot_cloud/test_fan.py
+++ tests/components/switchbot_cloud/test_fan.py
@@ -2,11 +2,11 @@
 
 from unittest.mock import patch
 
-from switchbot_api import Device, SwitchBotAPI
+from switchbot_api import CommonCommands, Device, SwitchBotAPI
 
 from homeassistant.components.fan import DOMAIN as FAN_DOMAIN, SERVICE_TURN_ON
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import ATTR_ENTITY_ID, STATE_OFF, STATE_ON
+from homeassistant.const import ATTR_ENTITY_ID, SERVICE_TURN_OFF, STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
 
 from . import configure_integration
@@ -248,52 +248,167 @@ async def test_fan_turn_on_7(
         await hass.services.async_call(
             FAN_DOMAIN, SERVICE_TURN_ON, {ATTR_ENTITY_ID: entity_id}, blocking=True
         )
-    mock_send_command.assert_called()
+    mock_send_command.assert_not_called()
     state = hass.states.get(entity_id)
     assert state.state == STATE_ON
 
 
-#
-# async def test_fan_turn_off(
-#     hass: HomeAssistant, mock_list_devices, mock_get_status
-# ) -> None:
-#     """Test turning off the fan."""
-#     mock_list_devices.return_value = [
-#         Device(
-#             version="V1.0",
-#             deviceId="battery-fan-id-1",
-#             deviceName="battery-fan-1",
-#             deviceType="Battery Circulator Fan",
-#             hubDeviceId="test-hub-id",
-#         ),
-#     ]
-#     mock_get_status.side_effect = [
-#         {"power": "on", "mode": "direct", "fanSpeed": "0"},
-#         {"power": "on", "mode": "direct", "fanSpeed": "0"},
-#     ]
-#
-#     entry = await configure_integration(hass)
-#
-#     assert entry.state is ConfigEntryState.LOADED
-#
-#     entity_id = "fan.battery_fan_1"
-#     state = hass.states.get(entity_id)
-#
-#     assert state.state == STATE_ON
-#
-#     with patch.object(SwitchBotAPI, "send_command") as mock_send_command:
-#         await hass.services.async_call(
-#             FAN_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: entity_id}, blocking=True
-#         )
-#
-#         mock_send_command.assert_called_once_with(
-#             "battery-fan-id-1", CommonCommands.OFF, "command", "default"
-#         )
-#
-#     state = hass.states.get(entity_id)
-#     assert state.state == STATE_OFF
-#
-#
+async def test_fan_turn_off_1(
+    hass: HomeAssistant, mock_list_devices, mock_get_status
+) -> None:
+    """Test turning off the fan."""
+    mock_list_devices.return_value = [
+        Device(
+            version="V1.0",
+            deviceId="battery-fan-id-1",
+            deviceName="battery-fan-1",
+            deviceType="Battery Circulator Fan",
+            hubDeviceId="test-hub-id",
+        ),
+    ]
+    mock_get_status.side_effect = [
+        {"power": "on", "mode": "direct", "fanSpeed": "0"},
+        {"power": "on", "mode": "direct", "fanSpeed": "0"},
+    ]
+
+    entry = await configure_integration(hass)
+
+    assert entry.state is ConfigEntryState.LOADED
+
+    entity_id = "fan.battery_fan_1"
+    state = hass.states.get(entity_id)
+
+    assert state.state == STATE_ON
+
+    with patch.object(SwitchBotAPI, "send_command") as mock_send_command:
+        await hass.services.async_call(
+            FAN_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: entity_id}, blocking=True
+        )
+
+        mock_send_command.assert_called_once_with(
+            "battery-fan-id-1", CommonCommands.OFF, "command", "default"
+        )
+
+    state = hass.states.get(entity_id)
+    assert state.state == STATE_OFF
+
+
+async def test_fan_turn_off_2(
+    hass: HomeAssistant, mock_list_devices, mock_get_status
+) -> None:
+    """Test turning off the fan."""
+    mock_list_devices.return_value = [
+        Device(
+            version="V1.0",
+            deviceId="battery-fan-id-1",
+            deviceName="battery-fan-1",
+            deviceType="Battery Circulator Fan",
+            hubDeviceId="test-hub-id",
+        ),
+    ]
+    mock_get_status.side_effect = [
+        {"power": "on", "mode": "direct", "fanSpeed": "0"},
+        {"power": "on", "mode": "baby", "fanSpeed": "0"},
+    ]
+
+    entry = await configure_integration(hass)
+
+    assert entry.state is ConfigEntryState.LOADED
+
+    entity_id = "fan.battery_fan_1"
+    state = hass.states.get(entity_id)
+
+    assert state.state == STATE_ON
+
+    with patch.object(SwitchBotAPI, "send_command") as mock_send_command:
+        await hass.services.async_call(
+            FAN_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: entity_id}, blocking=True
+        )
+
+        mock_send_command.assert_called_once_with(
+            "battery-fan-id-1", CommonCommands.OFF, "command", "default"
+        )
+
+    state = hass.states.get(entity_id)
+    assert state.state == STATE_OFF
+
+
+async def test_fan_turn_off_3(
+    hass: HomeAssistant, mock_list_devices, mock_get_status
+) -> None:
+    """Test turning off the fan."""
+    mock_list_devices.return_value = [
+        Device(
+            version="V1.0",
+            deviceId="battery-fan-id-1",
+            deviceName="battery-fan-1",
+            deviceType="Battery Circulator Fan",
+            hubDeviceId="test-hub-id",
+        ),
+    ]
+    mock_get_status.side_effect = [
+        {"power": "on", "mode": "direct", "fanSpeed": "0"},
+        {"power": "off", "mode": "baby", "fanSpeed": "0"},
+    ]
+
+    entry = await configure_integration(hass)
+
+    assert entry.state is ConfigEntryState.LOADED
+
+    entity_id = "fan.battery_fan_1"
+    state = hass.states.get(entity_id)
+
+    assert state.state == STATE_ON
+
+    with patch.object(SwitchBotAPI, "send_command") as mock_send_command:
+        await hass.services.async_call(
+            FAN_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: entity_id}, blocking=True
+        )
+
+        mock_send_command.assert_not_called()
+
+    state = hass.states.get(entity_id)
+    assert state.state == STATE_OFF
+
+
+async def test_fan_turn_off_4(
+    hass: HomeAssistant, mock_list_devices, mock_get_status
+) -> None:
+    """Test turning off the fan."""
+    mock_list_devices.return_value = [
+        Device(
+            version="V1.0",
+            deviceId="battery-fan-id-1",
+            deviceName="battery-fan-1",
+            deviceType="Battery Circulator Fan",
+            hubDeviceId="test-hub-id",
+        ),
+    ]
+    mock_get_status.side_effect = [
+        {"power": "off", "mode": "direct", "fanSpeed": "0"},
+        {"power": "off", "mode": "baby", "fanSpeed": "0"},
+    ]
+
+    entry = await configure_integration(hass)
+
+    assert entry.state is ConfigEntryState.LOADED
+
+    entity_id = "fan.battery_fan_1"
+    state = hass.states.get(entity_id)
+
+    assert state.state == STATE_OFF
+
+    with patch.object(SwitchBotAPI, "send_command") as mock_send_command:
+        await hass.services.async_call(
+            FAN_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: entity_id}, blocking=True
+        )
+
+        mock_send_command.assert_not_called()
+
+    state = hass.states.get(entity_id)
+    assert state.state == STATE_OFF
+
+
 # async def test_fan_set_percentage(
 #     hass: HomeAssistant, mock_list_devices, mock_get_status
 # ) -> None:
