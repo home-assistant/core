acdc88aa648edec34b987519bfdd6fa3a17635fb
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-06-09 21:24:32 +0800

    debug test code

--- tests/components/switchbot_cloud/test_light.py
+++ tests/components/switchbot_cloud/test_light.py
@@ -1,4 +1,4 @@
-"""Test for the Switchbot Battery Circulator Fan."""
+"""Test for the Switchbot Light Entity."""
 
 from unittest.mock import patch
 
@@ -18,7 +18,7 @@ from homeassistant.core import HomeAssistant
 from . import configure_integration
 
 
-async def test_fan_turn_off_1(
+async def test_light_turn_off_1(
     hass: HomeAssistant, mock_list_devices, mock_get_status
 ) -> None:
     """Test turning off the off."""
@@ -44,7 +44,7 @@ async def test_fan_turn_off_1(
     assert state.state is STATE_OFF
 
 
-async def test_fan_turn_off_2(
+async def test_light_turn_off_2(
     hass: HomeAssistant, mock_list_devices, mock_get_status
 ) -> None:
     """Test turning off the off."""
@@ -69,7 +69,7 @@ async def test_fan_turn_off_2(
     assert state.state is STATE_OFF
 
 
-async def test_fan_turn_off_3(
+async def test_light_turn_off_3(
     hass: HomeAssistant, mock_list_devices, mock_get_status
 ) -> None:
     """Test turning off the off."""
@@ -97,12 +97,12 @@ async def test_fan_turn_off_3(
         await hass.services.async_call(
             LIGHT_DOMAIN, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: entity_id}, blocking=True
         )
-    mock_send_command.assert_called()
+    mock_send_command.assert_not_called()
     state = hass.states.get(entity_id)
     assert state.state is STATE_OFF
 
 
-async def test_fan_turn_off_4(
+async def test_light_turn_off_4(
     hass: HomeAssistant, mock_list_devices, mock_get_status
 ) -> None:
     """Test turning off the off."""
@@ -135,7 +135,7 @@ async def test_fan_turn_off_4(
     assert state.state is STATE_OFF
 
 
-async def test_fan_turn_on_1(
+async def test_light_turn_on_1(
     hass: HomeAssistant, mock_list_devices, mock_get_status
 ) -> None:
     """Test turning off the off."""
