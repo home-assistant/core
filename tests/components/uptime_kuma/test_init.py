c1694d96f6d9b32768e39ed7ed90daa4e1858504
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-06-09 19:56:51 +0000

    tests

---
+++ tests/components/uptime_kuma/test_init.py
@@ -0,0 +1,54 @@
+"""Tests for the Uptime Kuma integration."""
+
+from unittest.mock import AsyncMock
+
+import pytest
+from pyuptimekuma import UptimeKumaAuthenticationException, UptimeKumaException
+
+from homeassistant.config_entries import ConfigEntryState
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+
+
+@pytest.mark.usefixtures("mock_pyuptimekuma")
+async def test_entry_setup_unload(
+    hass: HomeAssistant, config_entry: MockConfigEntry
+) -> None:
+    """Test integration setup and unload."""
+
+    config_entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is ConfigEntryState.LOADED
+
+    assert await hass.config_entries.async_unload(config_entry.entry_id)
+
+    assert config_entry.state is ConfigEntryState.NOT_LOADED
+
+
+@pytest.mark.parametrize(
+    ("exception", "state"),
+    [
+        (UptimeKumaAuthenticationException, ConfigEntryState.SETUP_ERROR),
+        (UptimeKumaException, ConfigEntryState.SETUP_RETRY),
+        ([None, UptimeKumaAuthenticationException], ConfigEntryState.SETUP_RETRY),
+        ([None, UptimeKumaException], ConfigEntryState.SETUP_RETRY),
+    ],
+)
+async def test_config_entry_not_ready(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    mock_pyuptimekuma: AsyncMock,
+    exception: Exception | list[Exception | None],
+    state: ConfigEntryState,
+) -> None:
+    """Test config entry not ready."""
+
+    mock_pyuptimekuma.async_get_monitors.side_effect = exception
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert config_entry.state is state
