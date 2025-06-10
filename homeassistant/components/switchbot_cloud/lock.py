e4fc75e7b6b27cc2e260d1180d44d60020c7cbdb
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-06-10 18:10:57 +0800

    add reload config func

--- homeassistant/components/switchbot_cloud/lock.py
+++ homeassistant/components/switchbot_cloud/lock.py
@@ -1,15 +1,16 @@
 """Support for the Switchbot lock."""
 
+from types import MappingProxyType
 from typing import Any
 
-from switchbot_api import LockCommands
+from switchbot_api import Device, LockCommands, Remote, SwitchBotAPI
 
 from homeassistant.components.lock import LockEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
-from . import SwitchbotCloudData
+from . import SwitchbotCloudData, SwitchBotCoordinator
 from .const import DOMAIN
 from .entity import SwitchBotCloudEntity
 
@@ -22,15 +23,34 @@ async def async_setup_entry(
     """Set up SwitchBot Cloud entry."""
     data: SwitchbotCloudData = hass.data[DOMAIN][config.entry_id]
     async_add_entities(
-        SwitchBotCloudLock(data.api, device, coordinator)
+        SwitchBotCloudLock(data.api, device, coordinator, config.options)
         for device, coordinator in data.devices.locks
     )
+
+    config.async_on_unload(config.add_update_listener(_async_update_listener))
+
+
+async def _async_update_listener(hass: HomeAssistant, config: ConfigEntry) -> None:
+    """Deal with entry update."""
+    hass.data[DOMAIN][config.entry_id].api.closed()
+    await hass.config_entries.async_reload(config.entry_id)
 
 
 class SwitchBotCloudLock(SwitchBotCloudEntity, LockEntity):
     """Representation of a SwitchBot lock."""
 
     _attr_name = None
+
+    def __init__(
+        self,
+        api: SwitchBotAPI,
+        device: Device | Remote,
+        coordinator: SwitchBotCoordinator,
+        entity_options: MappingProxyType,
+    ) -> None:
+        """Init SwitchBotCloudLock."""
+        super().__init__(api, device, coordinator)
+        self.entity_options = entity_options
 
     def _set_attributes(self) -> None:
         """Set attributes from coordinator data."""
