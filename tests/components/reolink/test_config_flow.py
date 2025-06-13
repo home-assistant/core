d2271ed000a9edbea612b6b51218c3f33ddb4e6d
starkillerOG <starkiller.og@gmail.com>
2025-06-13 21:48:32 +0000

    Add test

--- tests/components/reolink/test_config_flow.py
+++ tests/components/reolink/test_config_flow.py
@@ -156,6 +156,49 @@ async def test_config_flow_privacy_success(
     reolink_connect.baichuan.privacy_mode.return_value = False
 
 
+async def test_config_flow_baichuan_only(
+    hass: HomeAssistant, reolink_connect: MagicMock, mock_setup_entry: MagicMock
+) -> None:
+    """Successful flow manually initialized by the user for baichuan only device."""
+    reolink_connect.baichuan_only = True
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user"
+    assert result["errors"] == {}
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_USERNAME: TEST_USERNAME,
+            CONF_PASSWORD: TEST_PASSWORD,
+            CONF_HOST: TEST_HOST,
+        },
+    )
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == TEST_NVR_NAME
+    assert result["data"] == {
+        CONF_HOST: TEST_HOST,
+        CONF_USERNAME: TEST_USERNAME,
+        CONF_PASSWORD: TEST_PASSWORD,
+        CONF_PORT: TEST_PORT,
+        CONF_USE_HTTPS: TEST_USE_HTTPS,
+        CONF_SUPPORTS_PRIVACY_MODE: TEST_PRIVACY,
+        CONF_BC_PORT: TEST_BC_PORT,
+        CONF_BC_ONLY: True,
+    }
+    assert result["options"] == {
+        CONF_PROTOCOL: DEFAULT_PROTOCOL,
+    }
+    assert result["result"].unique_id == TEST_MAC
+
+    reolink_connect.baichuan_only = False
+
+
 async def test_config_flow_errors(
     hass: HomeAssistant, reolink_connect: MagicMock, mock_setup_entry: MagicMock
 ) -> None:
