da3b632c25e7abac193ad859d8939515b905212d
noahhusby <32528627+noahhusby@users.noreply.github.com>
2025-06-13 12:01:22 -0500

    Clean up diagnostics

--- tests/components/russound_rio/test_diagnostics.py
+++ tests/components/russound_rio/test_diagnostics.py
@@ -1,9 +1,5 @@
 """Tests for the diagnostics data provided by the Russound RIO integration."""
-import asyncio
-import concurrent
-import sys
-import threading
-import traceback
+
 from unittest.mock import AsyncMock
 
 from syrupy.assertion import SnapshotAssertion
@@ -12,7 +8,7 @@ from homeassistant.core import HomeAssistant
 
 from . import setup_integration
 
-from tests.common import MockConfigEntry, load_json_object_fixture
+from tests.common import MockConfigEntry
 from tests.components.diagnostics import get_diagnostics_for_config_entry
 from tests.typing import ClientSessionGenerator
 
@@ -27,16 +23,7 @@ async def test_entry_diagnostics(
     """Test config entry diagnostics."""
     await setup_integration(hass, mock_config_entry)
 
-    print("Threads before diagnostics:")
-    for t in threading.enumerate():
-        print(t.name)
-    print("Threads after diagnostics:")
-    for t in threading.enumerate():
-        print(t.name)
-
     result = await get_diagnostics_for_config_entry(
         hass, hass_client, mock_config_entry
     )
     assert result == snapshot
-
-
