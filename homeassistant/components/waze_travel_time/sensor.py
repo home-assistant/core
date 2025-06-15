2a93d60663b15da0dc0e2175636efe53a3304781
etiennec78 <etienne.coriou@free.fr>
2025-06-15 13:03:05 +0200

    Remove unused variables

--- homeassistant/components/waze_travel_time/sensor.py
+++ homeassistant/components/waze_travel_time/sensor.py
@@ -17,7 +17,7 @@ from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import CONF_DESTINATION, CONF_ORIGIN, DEFAULT_NAME, DOMAIN
+from .const import DEFAULT_NAME, DOMAIN
 from .coordinator import WazeTravelTimeCoordinator
 
 _LOGGER = logging.getLogger(__name__)
@@ -30,14 +30,9 @@ async def async_setup_entry(
 ) -> None:
     """Set up a Waze travel time sensor entry."""
     name = config_entry.data.get(CONF_NAME, DEFAULT_NAME)
-    origin = config_entry.data[CONF_ORIGIN]
-    destination = config_entry.data[CONF_DESTINATION]
-
     coordinator = config_entry.runtime_data
 
-    sensor = WazeTravelTimeSensor(
-        config_entry.entry_id, name, origin, destination, coordinator
-    )
+    sensor = WazeTravelTimeSensor(config_entry.entry_id, name, coordinator)
 
     async_add_entities([sensor], False)
 
@@ -61,16 +56,12 @@ class WazeTravelTimeSensor(CoordinatorEntity[WazeTravelTimeCoordinator], SensorE
         self,
         unique_id: str,
         name: str,
-        origin: str,
-        destination: str,
         coordinator: WazeTravelTimeCoordinator,
     ) -> None:
         """Initialize the Waze travel time sensor."""
         super().__init__(coordinator)
         self._attr_unique_id = unique_id
         self._attr_name = name
-        self._origin = origin
-        self._destination = destination
 
     async def async_added_to_hass(self) -> None:
         """Handle when the entity is added to Home Assistant."""
