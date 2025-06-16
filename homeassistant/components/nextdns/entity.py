dc094546088a77afc27c0a617afa512a12154a88
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-06-16 15:17:10 +0200

    Create DeviceInfo in entity module

--- homeassistant/components/nextdns/entity.py
+++ homeassistant/components/nextdns/entity.py
@@ -1,8 +1,10 @@
 """Define NextDNS entities."""
 
+from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
+from .const import DOMAIN
 from .coordinator import CoordinatorDataT, NextDnsUpdateCoordinator
 
 
@@ -18,6 +20,12 @@ class NextDnsEntity(CoordinatorEntity[NextDnsUpdateCoordinator[CoordinatorDataT]
     ) -> None:
         """Initialize."""
         super().__init__(coordinator)
-        self._attr_device_info = coordinator.device_info
+        self._attr_device_info = DeviceInfo(
+            configuration_url=f"https://my.nextdns.io/{coordinator.profile_id}/setup",
+            entry_type=DeviceEntryType.SERVICE,
+            identifiers={(DOMAIN, str(coordinator.profile_id))},
+            manufacturer="NextDNS Inc.",
+            name=coordinator.nextdns.get_profile_name(coordinator.profile_id),
+        )
         self._attr_unique_id = f"{coordinator.profile_id}_{description.key}"
         self.entity_description = description
