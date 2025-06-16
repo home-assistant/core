5e797bdb869f56be444d589714af5debaa55706c
etiennec78 <etienne.coriou@free.fr>
2025-06-16 14:56:45 +0200

    Set duration sensors unit to seconds

--- homeassistant/components/here_travel_time/sensor.py
+++ homeassistant/components/here_travel_time/sensor.py
@@ -55,14 +55,14 @@ def sensor_descriptions(travel_mode: str) -> tuple[SensorEntityDescription, ...]
             icon=ICONS.get(travel_mode, ICON_CAR),
             key=ATTR_DURATION,
             state_class=SensorStateClass.MEASUREMENT,
-            native_unit_of_measurement=UnitOfTime.MINUTES,
+            native_unit_of_measurement=UnitOfTime.SECONDS,
         ),
         SensorEntityDescription(
             translation_key="duration_in_traffic",
             icon=ICONS.get(travel_mode, ICON_CAR),
             key=ATTR_DURATION_IN_TRAFFIC,
             state_class=SensorStateClass.MEASUREMENT,
-            native_unit_of_measurement=UnitOfTime.MINUTES,
+            native_unit_of_measurement=UnitOfTime.SECONDS,
         ),
         SensorEntityDescription(
             translation_key="distance",
