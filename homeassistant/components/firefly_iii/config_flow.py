897510a6beca346aca945cf33f5447c074e834fd
Erwin Douna <e.douna@gmail.com>
2025-06-17 22:08:38 +0000

    needed clean

--- homeassistant/components/firefly_iii/config_flow.py
+++ homeassistant/components/firefly_iii/config_flow.py
@@ -20,13 +20,8 @@ _LOGGER = logging.getLogger(__name__)
 
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
-        vol.Required(
-            CONF_HOST, default="http://192.168.2.102:8883"
-        ): str,  # TODO: clean this up later!
-        vol.Required(
-            CONF_API_KEY,
-            default="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNDMyNTVlZGMxZjkwZTA3OWFiYzk1YmM1N2Y3NGY1ZWE1Y2ZmNTNjNmI3YjZjZWI3ZjEwNzlmZTljOTYxZjk0MDliNGUzNGMyZDQ5MDI2MzAiLCJpYXQiOjE3NTAwOTUzNTAuNzkyMTYzLCJuYmYiOjE3NTAwOTUzNTAuNzkyMTY5LCJleHAiOjE3ODE2MzEzNTAuNDA0NjY5LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.yoADj8gmZt6lBUL3n4M82zKaZiPy5mWaW5Kr67yJKx3WIRdGsCEw8Nl5dKxv-TzydgMFTQYQjgkfWRX-Lw3Do8kGtMWwd1TVw5bb5UtVaH43NyZz5owjywgLn6BFwpA9Z25pcwF5H_1YqUb0Y5RJFeB5Y_13-T8r9LS3oELHkj8jB6dicmtEvL1fs31sHO19j8k5PkrziQ4Vp1N6Cnqo2LYU594zBGnWdyeN0LkNVeh3uxb0wLcLnD2GybPg1W518PzXez8dCCLt_FbTuIHVKQAHmfMp-hWWj0wT8hzeocmuqSqScfsrbtSrL2FvH6z3y6a4tT1XXIifAxq5RBwYNv8KklmEjWUMF1AftBCz3UDP4wZ6swFILTI7CpnYx3XrH0AGhwrlUwO3OpDFS_5Kd0hIAkkuQTrDY2RPut43i-VPJ49SqyXctq552QF6czasuunvCoxU3zl9VKd8xSooUS3Ap8woHp7ANflXGla2IZu2D1Ft4PVdC-i0p9y3eBLdySaBS1uFK_EkdDWm6lMcGEcUJhfAu2J_dwiuRxehvpmm31dM1BVdV9U5xjxoPDu2m0L2bVKwmvGVeRTAhKKxnWcnb8LxnAE1Oa_DfalSMvYX4DHx53bbvO2BLoA55wCjxbmMPi71981pHJ-l2OyXR2Q8HQY8ZKAUCv0mCRy7IXs",
-        ): str,  # TODO: clean this up later!
+        vol.Required(CONF_HOST): str,
+        vol.Required(CONF_API_KEY): str,
         vol.Optional(CONF_VERIFY_SSL, default=True): bool,
     }
 )
