8fe6d75e58e7db3229b85fa7579cb3c919c0b0a3
Jules Dejaeghere <dej.j@live.be>
2025-06-18 18:02:35 +0200

    Fix IrmKmiBaseEntity

--- homeassistant/components/irm_kmi/entity.py
+++ homeassistant/components/irm_kmi/entity.py
@@ -4,7 +4,7 @@ import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN, IRM_KMI_NAME
 from .utils import preferred_language
@@ -12,14 +12,18 @@ from .utils import preferred_language
 _LOGGER = logging.getLogger(__name__)
 
 
-class IrmKmiBaseEntity(Entity):
+class IrmKmiBaseEntity(CoordinatorEntity):
     """Base methods for IRM KMI entities."""
+
+    _attr_attribution = (
+        "Weather data from the Royal Meteorological Institute of Belgium meteo.be"
+    )
 
     def __init__(self, entry: ConfigEntry, name: str | None) -> None:
         """Init base properties for IRM KMI entities."""
-        self._attr_attribution = (
-            "Weather data from the Royal Meteorological Institute of Belgium meteo.be"
-        )
+        coordinator = entry.runtime_data.coordinator
+        CoordinatorEntity.__init__(self, coordinator)
+
         self._attr_device_info = DeviceInfo(
             entry_type=DeviceEntryType.SERVICE,
             identifiers={(DOMAIN, entry.entry_id)},
