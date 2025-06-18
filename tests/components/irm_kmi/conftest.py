c4bb857fa8a5b9e983f986664ff2f19be7c88471
Jules Dejaeghere <dej.j@live.be>
2025-06-18 21:53:39 +0200

    Clean test fixtures

--- tests/components/irm_kmi/conftest.py
+++ tests/components/irm_kmi/conftest.py
@@ -1,7 +1,5 @@
 """Fixtures for the IRM KMI integration tests."""
 
-from __future__ import annotations
-
 from collections.abc import Generator
 import json
 from unittest.mock import MagicMock, patch
@@ -60,19 +58,9 @@ def mock_get_forecast_api_error():
     """Mock a call to IrmKmiApiClient.get_forecasts_coord() so that it raises an error."""
     with patch(
         "homeassistant.components.irm_kmi.config_flow.IrmKmiApiClient.get_forecasts_coord",
-        side_effet=IrmKmiApiError,
+        side_effect=IrmKmiApiError,
     ):
-        return
-
-
-@pytest.fixture
-def mock_get_forecast_api_error_repair():
-    """Mock a call to IrmKmiApiClient.get_forecasts_coord() so that it raises an error."""
-    with patch(
-        "homeassistant.components.irm_kmi.repairs.IrmKmiApiClient.get_forecasts_coord",
-        side_effet=IrmKmiApiError,
-    ):
-        return
+        yield
 
 
 @pytest.fixture
@@ -83,38 +71,6 @@ def mock_irm_kmi_api(request: pytest.FixtureRequest) -> Generator[None, MagicMoc
     forecast = json.loads(load_fixture(fixture, "irm_kmi"))
     with patch(
         "homeassistant.components.irm_kmi.IrmKmiApiClientHa", autospec=True
-    ) as irm_kmi_api_mock:
-        irm_kmi = irm_kmi_api_mock.return_value
-        irm_kmi.get_forecasts_coord.return_value = forecast
-        yield irm_kmi
-
-
-@pytest.fixture
-def mock_irm_kmi_api_repair_in_benelux(
-    request: pytest.FixtureRequest,
-) -> Generator[None, MagicMock]:
-    """Return a mocked IrmKmi api client."""
-    fixture: str = "forecast.json"
-
-    forecast = json.loads(load_fixture(fixture, "irm_kmi"))
-    with patch(
-        "homeassistant.components.irm_kmi.repairs.IrmKmiApiClient", autospec=True
-    ) as irm_kmi_api_mock:
-        irm_kmi = irm_kmi_api_mock.return_value
-        irm_kmi.get_forecasts_coord.return_value = forecast
-        yield irm_kmi
-
-
-@pytest.fixture
-def mock_irm_kmi_api_repair_out_of_benelux(
-    request: pytest.FixtureRequest,
-) -> Generator[None, MagicMock]:
-    """Return a mocked IrmKmi api client."""
-    fixture: str = "forecast_out_of_benelux.json"
-
-    forecast = json.loads(load_fixture(fixture, "irm_kmi"))
-    with patch(
-        "homeassistant.components.irm_kmi.repairs.IrmKmiApiClient", autospec=True
     ) as irm_kmi_api_mock:
         irm_kmi = irm_kmi_api_mock.return_value
         irm_kmi.get_forecasts_coord.return_value = forecast
