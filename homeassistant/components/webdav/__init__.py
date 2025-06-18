29ccf3cf3790ba980bc1877dae26f394be713caa
Jan-Philipp Benecke <jan-philipp@bnck.me>
2025-06-18 19:01:19 +0200

    Catch access denied errors in webdav and display propper message

--- homeassistant/components/webdav/__init__.py
+++ homeassistant/components/webdav/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import logging
 
 from aiowebdav2.client import Client
-from aiowebdav2.exceptions import UnauthorizedError
+from aiowebdav2.exceptions import AccessDeniedError, UnauthorizedError
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_URL, CONF_USERNAME, CONF_VERIFY_SSL
@@ -40,6 +40,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: WebDavConfigEntry) -> bo
         raise ConfigEntryError(
             translation_domain=DOMAIN,
             translation_key="invalid_username_password",
+        ) from err
+    except AccessDeniedError as err:
+        raise ConfigEntryError(
+            translation_domain=DOMAIN,
+            translation_key="cannot_access_or_create_backup_path",
         ) from err
 
     # Check if we can connect to the WebDAV server
