1ac98193fac186525b7c0fb197adc9a8ed3dbccd
Patrick Walsh <hello@mossymaker.com>
2025-06-19 18:13:17 +0000

    Add digest auth option to downloader integration
    Also moves some validation out of the thread to avoid swallowing exceptions.

---
+++ tests/components/downloader/conftest.py
@@ -0,0 +1,34 @@
+"""Configure pytest for downloader tests."""
+
+from unittest.mock import patch
+
+import pytest
+from requests_mock import Mocker
+
+from homeassistant.components.downloader.const import CONF_DOWNLOAD_DIR, DOMAIN
+from homeassistant.core import HomeAssistant
+
+from tests.common import MockConfigEntry
+
+
+@pytest.fixture
+async def loaded_config_entry(hass: HomeAssistant) -> MockConfigEntry:
+    """Set up loaded config entry."""
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            CONF_DOWNLOAD_DIR: "/test_dir",
+        },
+    )
+    config_entry.add_to_hass(hass)
+
+    with patch("os.path.isdir", return_value=True):
+        await hass.config_entries.async_setup(config_entry.entry_id)
+
+    return config_entry
+
+
+@pytest.fixture
+def auth_requests_mock(requests_mock: Mocker) -> Mocker:
+    """Set up request mock."""
+    return requests_mock
