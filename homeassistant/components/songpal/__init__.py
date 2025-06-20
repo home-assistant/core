f6c0a20368b5196f6f92bc3258b36e675a463b52
Yoshi Walsh <yoshi@yoshiwalsh.me>
2025-06-21 00:33:10 +1000

    Refactor Songpal integration to use a coordinator
    Also prepares re-usable functionality which will be used to create additional entities to expose various Songpal settings.
    
    Since this is a significant refactoring, I have also added myself to the codeowners list to indicate my willingness to provide ongoing support.

--- homeassistant/components/songpal/__init__.py
+++ homeassistant/components/songpal/__init__.py
@@ -1,14 +1,26 @@
 """The songpal component."""
 
+import asyncio
+from dataclasses import dataclass
+import logging
+from typing import Any
+
+from songpal import Device, SongpalException
 import voluptuous as vol
 
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import CONF_NAME, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.update_coordinator import UpdateFailed
 
 from .const import CONF_ENDPOINT, DOMAIN
+from .coordinator import SongpalCoordinator
+
+_LOGGER = logging.getLogger(__name__)
 
 SONGPAL_CONFIG_SCHEMA = vol.Schema(
     {vol.Optional(CONF_NAME): cv.string, vol.Required(CONF_ENDPOINT): cv.string}
@@ -19,7 +31,29 @@ CONFIG_SCHEMA = vol.Schema(
     extra=vol.ALLOW_EXTRA,
 )
 
-PLATFORMS = [Platform.MEDIA_PLAYER]
+PLATFORMS = [
+    Platform.MEDIA_PLAYER,
+]
+
+
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+    discovery_info: Any | None = None,
+) -> None:
+    """Set up from legacy configuration file. Obsolete."""
+    _LOGGER.error(
+        "Configuring Songpal through media_player platform is no longer supported."
+        " Convert to songpal platform or UI configuration"
+    )
+
+
+@dataclass
+class RuntimeData:
+    """Class to hold data that should be easily accessible throughout the integration."""
+
+    coordinator: SongpalCoordinator
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
@@ -37,9 +71,44 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up songpal media player."""
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Set up songpal coordinator and entities."""
+
+    _LOGGER.warning("Setup entry")
+
+    name = entry.data[CONF_NAME]
+    endpoint = entry.data[CONF_ENDPOINT]
+    device = Device(endpoint)
+
+    try:
+        async with asyncio.timeout(
+            10
+        ):  # set timeout to avoid blocking the setup process
+            await device.get_supported_methods()
+    except (SongpalException, TimeoutError) as ex:
+        _LOGGER.warning("[%s(%s)] Unable to connect", name, endpoint)
+        _LOGGER.debug("Unable to get methods from songpal: %s", ex)
+        raise UpdateFailed(f"[{name}({endpoint})] Unable to connect") from ex
+
+    _LOGGER.warning("Setup coordinator")
+    coordinator = SongpalCoordinator(hass, entry, name, device)
+
+    _LOGGER.warning("First refresh")
+    await coordinator.async_config_entry_first_refresh()
+
+    _LOGGER.warning("Refresh complete")
+    if not coordinator.initialized:
+        _LOGGER.warning("Coordinator not initialised")
+        raise ConfigEntryNotReady
+
+    _LOGGER.warning("Runtime data")
+    entry.runtime_data = RuntimeData(coordinator)
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
 
 
---
+++ homeassistant/components/songpal/entity.py
@@ -0,0 +1,114 @@
+"""Base class for all Songpal entities."""
+
+# I would really like this file to be called "base.py" or "base_entity.py", but C7461 hass-enforce-class-module prevents that
+
+import logging
+import re
+
+from songpal.containers import Setting
+
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN
+from .coordinator import SongpalCoordinator
+from .device import device_info, device_unique_id
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SongpalBaseEntity(CoordinatorEntity):
+    """Songpal Base Entity Class.
+
+    This provides shared functionality between all Songpal entities.
+    """
+
+    coordinator: SongpalCoordinator
+
+    _attr_has_entity_name = True
+
+    def __init__(self, hass: HomeAssistant, coordinator: SongpalCoordinator) -> None:
+        """Initialise entity."""
+        super().__init__(coordinator)
+        self.hass = hass
+
+    @callback
+    def update_state(self, data) -> None:
+        """Process data from coordinator."""
+
+        raise NotImplementedError("Songpal entity failed to override update_state")
+
+    def get_initial_state(self) -> None:
+        """Fetch & process data from coordinator when entity is created."""
+        self.update_state(self.coordinator.data)
+        self.async_write_ha_state()
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Update latest data from coordinator."""
+        # This method is called by SongpalCoordinator when a successful update runs.
+        self.update_state(self.coordinator.data)
+        self.async_write_ha_state()
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device info."""
+        return device_info(self.coordinator.device_name, self.coordinator.data)
+
+
+class SongpalSettingEntity(SongpalBaseEntity):
+    """Base class for songpal entities that expose a single setting."""
+
+    setting: Setting | None
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        coordinator: SongpalCoordinator,
+        setting_bank: str,
+        setting: Setting,
+    ) -> None:
+        """Init."""
+
+        self._setting_bank = setting_bank
+        self._setting_target = setting.target
+        self._settingName = self.get_friendly_setting_name(setting.target)
+        self._settingId = (
+            f"{self._setting_bank}_{self._settingName.replace(' ', '_').lower()}"
+        )
+
+        self.setting = setting
+
+        super().__init__(hass, coordinator)
+
+    def update_state(self, data) -> None:
+        """Process data from coordinator."""
+
+        for setting in self.coordinator.data[self._setting_bank]:
+            if setting.target == self._setting_target:
+                self.setting = setting
+                break
+        else:
+            self.setting = None
+
+    def get_friendly_setting_name(self, setting_name):
+        """Convert setting name from camelCase to space-delimited words."""
+
+        return " ".join(
+            word if len(word) > 1 and word.isupper() else word.lower()
+            for word in re.sub(
+                "([A-Z][a-z]+)", r" \1", re.sub("([A-Z]+)", r" \1", setting_name)
+            ).split()
+        )
+
+    @property
+    def name(self) -> str:
+        """Return the friendly name of the entity."""
+        return self._settingName
+
+    @property
+    def unique_id(self) -> str:
+        """Return unique id."""
+
+        return f"{DOMAIN}-{device_unique_id(self.coordinator.data)}-{self._settingId}"
