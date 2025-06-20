cd51070219b35d7ca641f04304964969457e8a3e
epenet <6771947+epenet@users.noreply.github.com>
2025-06-20 11:39:13 +0200

    Migrate kmtronic to use runtime_data (#147193)

--- homeassistant/components/kmtronic/__init__.py
+++ homeassistant/components/kmtronic/__init__.py
@@ -3,18 +3,16 @@
 from pykmtronic.auth import Auth
 from pykmtronic.hub import KMTronicHubAPI
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import aiohttp_client
 
-from .const import DATA_COORDINATOR, DATA_HUB, DOMAIN
-from .coordinator import KMtronicCoordinator
+from .coordinator import KMTronicConfigEntry, KMtronicCoordinator
 
 PLATFORMS = [Platform.SWITCH]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: KMTronicConfigEntry) -> bool:
     """Set up kmtronic from a config entry."""
     session = aiohttp_client.async_get_clientsession(hass)
     auth = Auth(
@@ -27,11 +25,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator = KMtronicCoordinator(hass, entry, hub)
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = {
-        DATA_HUB: hub,
-        DATA_COORDINATOR: coordinator,
-    }
+    entry.runtime_data = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
@@ -40,15 +34,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-async def async_update_options(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+async def async_update_options(
+    hass: HomeAssistant, config_entry: KMTronicConfigEntry
+) -> None:
     """Update options."""
     await hass.config_entries.async_reload(config_entry.entry_id)
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: KMTronicConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
