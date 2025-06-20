f9a83d2574d5603bfefb70d4ed1b514a3b1af041
Jonathan Keslin <github@jonathankeslin.com>
2025-06-20 09:40:50 -0700

    Always set thread_parent, allow string reactions, improve tests

--- homeassistant/components/matrix/services.py
+++ homeassistant/components/matrix/services.py
@@ -2,8 +2,7 @@
 
 from __future__ import annotations
 
-import re
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 import voluptuous as vol
 
@@ -50,35 +49,9 @@ SERVICE_SCHEMA_SEND_MESSAGE = vol.Schema(
     }
 )
 
-compiledEmojiRegex = re.compile(
-    "[\U0001f600-\U0001f64f"  # Emoticons
-    "\U0001f300-\U0001f5ff"  # Symbols & Pictographs
-    "\U0001f680-\U0001f6ff"  # Transport & Map Symbols
-    "\U0001f1e0-\U0001f1ff"  # Flags (iOS)
-    "\U00002700-\U000027bf"  # Dingbats
-    "\U00002600-\U000026ff"  # Miscellaneous Symbols
-    "\U0001f900-\U0001f9ff"  # Supplemental Symbols and Pictographs
-    "\U0001fa70-\U0001faff"  # Symbols and Pictographs Extended-A
-    "\U00002500-\U00002bef"  # Chinese/Japanese/Korean characters
-    "]+",
-    flags=re.UNICODE,
-)
-
-
-def isEmoji(value: Any) -> str:
-    """Validate that value is an emoji."""
-    if not isinstance(value, str):
-        raise vol.Invalid(f"not a string value: {value}")
-
-    if not compiledEmojiRegex.match(value):
-        raise vol.Invalid(f"value {value} is not a valid emoji")
-
-    return value
-
-
 SERVICE_SCHEMA_REACT = vol.Schema(
     {
-        vol.Required(ATTR_REACTION): isEmoji,
+        vol.Required(ATTR_REACTION): cv.string,
         vol.Required(ATTR_ROOM): cv.matches_regex(CONF_ROOMS_REGEX),
         vol.Required(ATTR_MESSAGE_ID): cv.string,
     }
