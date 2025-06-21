851fea9d79ba337599f90d10d1c966d174850320
Alex Biddulph <abiddulph@4tel.com.au>
2025-06-21 22:12:15 +1000

    Update unit tests

--- tests/components/enphase_envoy/test_sensor.py
+++ tests/components/enphase_envoy/test_sensor.py
@@ -792,27 +792,35 @@ async def test_sensor_inverter_detailed_data(
     entity_base = f"{Platform.SENSOR}.inverter"
 
     for sn, inverter in mock_envoy.data.inverters.items():
-        assert (dc_voltage := hass.states.get(f"{entity_base}_{sn}_voltage"))
+        assert (dc_voltage := hass.states.get(f"{entity_base}_{sn}_dc_voltage"))
         assert float(dc_voltage.state) == (inverter.dc_voltage)
-        assert (dc_current := hass.states.get(f"{entity_base}_{sn}_current"))
+        assert (dc_current := hass.states.get(f"{entity_base}_{sn}_dc_current"))
         assert float(dc_current.state) == (inverter.dc_current)
-        assert (ac_voltage := hass.states.get(f"{entity_base}_{sn}_voltage_2"))
+        assert (ac_voltage := hass.states.get(f"{entity_base}_{sn}_ac_voltage"))
         assert float(ac_voltage.state) == (inverter.ac_voltage)
-        assert (ac_current := hass.states.get(f"{entity_base}_{sn}_current_2"))
+        assert (ac_current := hass.states.get(f"{entity_base}_{sn}_ac_current"))
         assert float(ac_current.state) == (inverter.ac_current)
-        assert (ac_frequency := hass.states.get(f"{entity_base}_{sn}_frequency"))
+        assert (ac_frequency := hass.states.get(f"{entity_base}_{sn}_ac_frequency"))
         assert float(ac_frequency.state) == (inverter.ac_frequency)
         assert (temperature := hass.states.get(f"{entity_base}_{sn}_temperature"))
         assert int(temperature.state) == (inverter.temperature)
-        assert (lifetime_energy := hass.states.get(f"{entity_base}_{sn}_energy"))
-        assert int(lifetime_energy.state) == (inverter.lifetime_energy)
-        assert (energy_today := hass.states.get(f"{entity_base}_{sn}_energy_2"))
+        assert (
+            lifetime_energy := hass.states.get(f"{entity_base}_{sn}_lifetime_energy")
+        )
+        assert float(lifetime_energy.state) == (inverter.lifetime_energy / 1000.0)
+        assert (energy_today := hass.states.get(f"{entity_base}_{sn}_energy_today"))
         assert int(energy_today.state) == (inverter.energy_today)
-        assert (last_report_duration := hass.states.get(f"{entity_base}_{sn}_duration"))
+        assert (
+            last_report_duration := hass.states.get(
+                f"{entity_base}_{sn}_last_report_duration"
+            )
+        )
         assert int(last_report_duration.state) == (inverter.last_report_duration)
-        assert (energy_produced := hass.states.get(f"{entity_base}_{sn}_energy_3"))
+        assert (
+            energy_produced := hass.states.get(f"{entity_base}_{sn}_energy_produced")
+        )
         assert float(energy_produced.state) == (inverter.energy_produced)
-        assert (max_reported := hass.states.get(f"{entity_base}_{sn}_power"))
+        assert (max_reported := hass.states.get(f"{entity_base}_{sn}_max_reported"))
         assert int(max_reported.state) == (inverter.max_report_watts)
 
 
@@ -845,17 +853,18 @@ async def test_sensor_inverter_disabled_by_integration(
         in integration_disabled_entities(entity_registry, config_entry)
         for sn in mock_envoy.data.inverters
         for key in (
-            "voltage",
-            "current",
-            "voltage_2",
-            "current_2",
-            "frequency",
+            "dc_voltage",
+            "dc_current",
+            "ac_voltage",
+            "ac_current",
+            "ac_frequency",
             "temperature",
-            "energy",
-            "energy_2",
-            "duration",
-            "energy_3",
+            "lifetime_energy",
+            "energy_today",
+            "last_report_duration",
+            "energy_produced",
             "last_reported",
+            "max_reported",
         )
     )
 
