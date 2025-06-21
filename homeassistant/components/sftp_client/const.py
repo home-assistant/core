eff2a97eedd83553d250cdb88a80f847d57c196e
cyr-ius <cyr-ius@ipocus.net>
2025-06-21 13:47:02 +0000

    Rename CONF_BACKUP_PATH

--- homeassistant/components/sftp_client/const.py
+++ homeassistant/components/sftp_client/const.py
@@ -5,7 +5,7 @@ from collections.abc import Callable
 from homeassistant.util.hass_dict import HassKey
 
 DOMAIN = "sftp_client"
-CONF_BACKUP_PATH = "backup"
+CONF_BACKUP_PATH = "backup_path"
 DEFAULT_BACKUP_PATH = "backup"
 
 DATA_BACKUP_AGENT_LISTENERS: HassKey[list[Callable[[], None]]] = HassKey(
