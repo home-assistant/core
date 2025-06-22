19bdaa145b210579e6676c8d72b287ac697a5dd0
Yoshi Walsh <yoshi@yoshiwalsh.me>
2025-06-22 14:27:34 +1000

    Add re-usable functionality in preparation for adding per-setting entities

--- homeassistant/components/songpal/entity.py
+++ homeassistant/components/songpal/entity.py
@@ -49,3 +49,60 @@ class SongpalBaseEntity(CoordinatorEntity):
     def device_info(self) -> DeviceInfo:
         """Return the device info."""
         return device_info(self.coordinator.device_name, self.coordinator.data)
+
+
+class SongpalSettingEntity(SongpalBaseEntity):
+    """Base class for songpal entities that expose a single setting."""
+
+    setting: Setting | None
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        coordinator: SongpalCoordinator,
+        setting_bank: str,
+        setting: Setting,
+    ) -> None:
+        """Init."""
+
+        self._setting_bank = setting_bank
+        self._setting_target = setting.target
+        self._settingName = self.get_friendly_setting_name(setting.target)
+        self._settingId = (
+            f"{self._setting_bank}_{self._settingName.replace(' ', '_').lower()}"
+        )
+
+        self.setting = setting
+
+        super().__init__(hass, coordinator)
+
+    def update_state(self, data) -> None:
+        """Process data from coordinator."""
+
+        for setting in self.coordinator.data[self._setting_bank]:
+            if setting.target == self._setting_target:
+                self.setting = setting
+                break
+        else:
+            self.setting = None
+
+    def get_friendly_setting_name(self, setting_name):
+        """Convert setting name from camelCase to space-delimited words."""
+
+        return " ".join(
+            word if len(word) > 1 and word.isupper() else word.lower()
+            for word in re.sub(
+                "([A-Z][a-z]+)", r" \1", re.sub("([A-Z]+)", r" \1", setting_name)
+            ).split()
+        )
+
+    @property
+    def name(self) -> str:
+        """Return the friendly name of the entity."""
+        return self._settingName
+
+    @property
+    def unique_id(self) -> str:
+        """Return unique id."""
+
+        return f"{DOMAIN}-{device_unique_id(self.coordinator.data)}-{self._settingId}"
