9ca95e92d557296c9cba1b6987c0c0360c3e8cc7
David Bugl <bugl@lunatone.com>
2025-06-23 13:47:45 +0000

    Add version dependent code parts in light entity

--- homeassistant/components/lunatone/light.py
+++ homeassistant/components/lunatone/light.py
@@ -2,8 +2,10 @@
 
 from __future__ import annotations
 
+import asyncio
 from typing import Any
 
+from awesomeversion import AwesomeVersion
 from lunatone_dali_api_client import Device
 from lunatone_dali_api_client.models import ControlData
 
@@ -25,9 +27,14 @@ async def async_setup_entry(
     info = config_entry.runtime_data.info
     devices = config_entry.runtime_data.devices
 
+    interface_version = AwesomeVersion(info.data.version.split("/")[0][1:])
+
     # Add devices
     async_add_entities(
-        [LunatoneLight(device, info.data.device.serial) for device in devices.devices],
+        [
+            LunatoneLight(device, info.data.device.serial, interface_version)
+            for device in devices.devices
+        ],
         update_before_add=True,
     )
 
@@ -38,8 +45,11 @@ class LunatoneLight(LightEntity):
     _attr_color_mode = ColorMode.ONOFF
     _attr_supported_color_modes = {ColorMode.ONOFF}
 
-    def __init__(self, device: Device, unique_id_prefix: str) -> None:
+    def __init__(
+        self, device: Device, unique_id_prefix: str, interface_version: AwesomeVersion
+    ) -> None:
         """Initialize a LunatoneLight."""
+        self._interface_version = interface_version
         self._device = device
         self._attr_unique_id = f"{unique_id_prefix}-device{self._device.data.id}"
 
@@ -77,4 +87,6 @@ class LunatoneLight(LightEntity):
 
         This is the only method that should fetch new data for Home Assistant.
         """
+        if self._interface_version >= "1.10.0" and self._interface_version < "1.15.0":
+            await asyncio.sleep(0.02)
         await self._device.async_update()
