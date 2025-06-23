532eef3cf846d73f6de7772abcbb6292bde90f1a
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-06-23 21:28:06 +0000

    change domain

--- homeassistant/components/sleep_as_android/const.py
+++ homeassistant/components/sleep_as_android/const.py
@@ -1,10 +1,8 @@
 """Constants for the Sleep as Android integration."""
 
-DOMAIN = "sleep"
+DOMAIN = "sleep_as_android"
 
 ATTR_EVENT = "event"
 ATTR_VALUE1 = "value1"
 ATTR_VALUE2 = "value2"
 ATTR_VALUE3 = "value3"
-
-SLEEP_EVENT = "sleep_event"
--- homeassistant/generated/config_flows.py
+++ homeassistant/generated/config_flows.py
@@ -574,7 +574,7 @@ FLOWS = {
         "sky_remote",
         "skybell",
         "slack",
-        "sleep",
+        "sleep_as_android",
         "sleepiq",
         "slide_local",
         "slimproto",
