326d895b841198522aa0fa5b6d093a6dd7593042
epenet <6771947+epenet@users.noreply.github.com>
2025-06-23 08:50:54 +0000

    Migrate lcn to use runtime_data

--- homeassistant/components/lcn/entity.py
+++ homeassistant/components/lcn/entity.py
@@ -2,7 +2,6 @@
 
 from collections.abc import Callable
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ADDRESS, CONF_DOMAIN, CONF_NAME
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
@@ -13,6 +12,7 @@ from .helpers import (
     AddressType,
     DeviceConnectionType,
     InputType,
+    LcnConfigEntry,
     generate_unique_id,
     get_device_connection,
     get_resource,
@@ -29,7 +29,7 @@ class LcnEntity(Entity):
     def __init__(
         self,
         config: ConfigType,
-        config_entry: ConfigEntry,
+        config_entry: LcnConfigEntry,
     ) -> None:
         """Initialize the LCN device."""
         self.config = config
