b89a8eea9f0a01a44369e09f503ccc73ee1fd9e0
David Bugl <bugl@lunatone.com>
2025-06-23 07:59:06 +0000

    Add info data update coordinator

--- homeassistant/components/lunatone/__init__.py
+++ homeassistant/components/lunatone/__init__.py
@@ -5,13 +5,15 @@ from __future__ import annotations
 from dataclasses import dataclass
 import logging
 
-from lunatone_dali_api_client import Auth, Devices
+from lunatone_dali_api_client import Auth, Devices, Info
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_URL, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+from .const import DOMAIN
 
 PLATFORMS: list[Platform] = [Platform.LIGHT]
 
@@ -22,7 +24,10 @@ _LOGGER = logging.getLogger(__name__)
 class LunatoneDALIIoTData:
     """Lunatone DALI IoT data class."""
 
-    coordinator: DataUpdateCoordinator
+    coordinator_info: DataUpdateCoordinator
+    info: Info
+
+    coordinator_devices: DataUpdateCoordinator
     devices: Devices
 
 
@@ -33,17 +38,27 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Lunatone from a config entry."""
 
     auth = Auth(async_get_clientsession(hass), entry.data[CONF_URL])
+    info = Info(auth)
     devices = Devices(auth)
 
-    coordinator = DataUpdateCoordinator(
+    coordinator_info = DataUpdateCoordinator(
         hass,
         _LOGGER,
-        name="Lunatone",
+        name=DOMAIN + "-info",
+        update_method=info.async_update,
+    )
+    await coordinator_info.async_config_entry_first_refresh()
+    coordinator_devices = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name=DOMAIN + "-devices",
         update_method=devices.async_update,
     )
-    await coordinator.async_config_entry_first_refresh()
+    await coordinator_devices.async_config_entry_first_refresh()
 
-    entry.runtime_data = LunatoneDALIIoTData(coordinator, devices)
+    entry.runtime_data = LunatoneDALIIoTData(
+        coordinator_info, info, coordinator_devices, devices
+    )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
