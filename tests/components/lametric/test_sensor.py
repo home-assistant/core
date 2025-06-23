b2520394f483b922474fae06cc8325fb70ac606a
rrooggiieerr <rogier@batoid.com>
2025-06-23 14:47:52 +0300

    Lametric add configuration url (#147118)
    * Set cofiguration URL to LaMetric device web interface
    
    * Update LaMetric unit tests to accomodate fro configuration url

--- tests/components/lametric/test_sensor.py
+++ tests/components/lametric/test_sensor.py
@@ -41,7 +41,7 @@ async def test_wifi_signal(
 
     device = device_registry.async_get(entry.device_id)
     assert device
-    assert device.configuration_url is None
+    assert device.configuration_url == "https://127.0.0.1/"
     assert device.connections == {(dr.CONNECTION_NETWORK_MAC, "aa:bb:cc:dd:ee:ff")}
     assert device.entry_type is None
     assert device.hw_version is None
