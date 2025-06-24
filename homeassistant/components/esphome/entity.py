b8bfe0241bc6364d788b15fd4eb21207afa63ea7
J. Nick Koston <nick@koston.org>
2025-06-24 17:05:36 +0200

    cleanups

--- homeassistant/components/esphome/entity.py
+++ homeassistant/components/esphome/entity.py
@@ -80,8 +80,7 @@ def async_static_info_updated(
         new_unique_id = build_device_unique_id(device_info.mac_address, info)
         entity_id = ent_reg.async_get_entity_id(platform.domain, DOMAIN, old_unique_id)
 
-        if not entity_id:
-            continue
+        assert entity_id is not None
 
         updates: dict[str, Any] = {}
 
