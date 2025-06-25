4b8316cae9cdbb929740f8f420e4e922e3daf3b4
Kurt Clifford <kclif9@gmail.com>
2025-06-25 20:26:11 +0000

    removing default version

--- homeassistant/components/actronair_neo/config_flow.py
+++ homeassistant/components/actronair_neo/config_flow.py
@@ -22,8 +22,6 @@ ACTRON_AIR_SCHEMA = vol.Schema(
 class ActronNeoConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Actron Air Neo."""
 
-    VERSION = 1
-
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
