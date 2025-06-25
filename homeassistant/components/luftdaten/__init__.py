909d950b50081f348359a14964ac82403bf52ca8
epenet <6771947+epenet@users.noreply.github.com>
2025-06-25 08:07:34 +0200

    Migrate luftdaten to use runtime_data (#147480)

--- homeassistant/components/luftdaten/__init__.py
+++ homeassistant/components/luftdaten/__init__.py
@@ -8,17 +8,16 @@ from __future__ import annotations
 
 from luftdaten import Luftdaten
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-from .const import CONF_SENSOR_ID, DOMAIN
-from .coordinator import LuftdatenDataUpdateCoordinator
+from .const import CONF_SENSOR_ID
+from .coordinator import LuftdatenConfigEntry, LuftdatenDataUpdateCoordinator
 
 PLATFORMS = [Platform.SENSOR]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: LuftdatenConfigEntry) -> bool:
     """Set up Sensor.Community as config entry."""
 
     # For backwards compat, set unique ID
@@ -32,14 +31,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator = LuftdatenDataUpdateCoordinator(hass, entry, sensor_community)
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    entry.runtime_data = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: LuftdatenConfigEntry) -> bool:
     """Unload an Sensor.Community config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        del hass.data[DOMAIN][entry.entry_id]
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
