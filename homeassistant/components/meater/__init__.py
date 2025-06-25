d2c25107adda504e74f8893f94eac87cd9670508
Joostlek <joostlek@outlook.com>
2025-06-25 09:27:31 +0200

    Migrate Meater to use HassKey

--- homeassistant/components/meater/__init__.py
+++ homeassistant/components/meater/__init__.py
@@ -2,11 +2,14 @@
 
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.util.hass_dict import HassKey
 
 from .const import DOMAIN
 from .coordinator import MeaterConfigEntry, MeaterCoordinator
 
 PLATFORMS = [Platform.SENSOR]
+
+MEATER_DATA: HassKey[set[str]] = HassKey(DOMAIN)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: MeaterConfigEntry) -> bool:
@@ -15,7 +18,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: MeaterConfigEntry) -> bo
     coordinator = MeaterCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {}).setdefault("known_probes", set())
+    hass.data.setdefault(MEATER_DATA, set())
 
     entry.runtime_data = coordinator
 
