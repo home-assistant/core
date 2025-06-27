b26376eafd55eaf5442b1bbff289e23e814db916
farkasdi <93778865+farkasdi@users.noreply.github.com>
2025-06-27 11:20:39 +0200

    Update binary_sensor.py by using entity specific process fn
    BinarySensors's async_update_callback refactored to use defined value_fn to calculate entity state like in sensor.py

--- homeassistant/components/netatmo/binary_sensor.py
+++ homeassistant/components/netatmo/binary_sensor.py
@@ -39,9 +39,6 @@ class NetatmoBinarySensorEntityDescription(BinarySensorEntityDescription):
 
 BINARY_SENSOR_TYPES: tuple[NetatmoBinarySensorEntityDescription, ...] = (
     NetatmoBinarySensorEntityDescription(
-
-#BINARY_SENSOR_TYPES: tuple[BinarySensorEntityDescription, ...] = (
-#    BinarySensorEntityDescription(
         key="reachable",
         netatmo_name="reachable",
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
@@ -92,16 +89,10 @@ class NetatmoWeatherBinarySensor(NetatmoWeatherModuleEntity, BinarySensorEntity)
     @callback
     def async_update_callback(self) -> None:
         """Update the entity's state."""
-        if self.entity_description.key == "reachable":
-            #self._attr_native_value = self.device.reachable
-            self._attr_is_on = self.device.reachable
-        elif self.entity_description.key == "status":
-            value = cast(
-                StateType, getattr(self.device, self.entity_description.key)
-            )
-            if value is not None:
-                value = process_status(value)
-                #value = self.entity_description.value_fn(value)
-            self._attr_is_on = value
-
+        value = cast(
+                StateType, getattr(self.device, self.entity_description.netatmo_name)
+        )
+        if value is not None:
+            value = self.entity_description.value_fn(value)
+        self._attr_is_on = value
         self.async_write_ha_state()
