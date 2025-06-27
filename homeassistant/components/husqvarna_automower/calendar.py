54d0b1900f7a696553963fb8a5bceb1575159d6f
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2025-06-27 20:34:50 +0200

    Add device prefix to summary in Husqvarna Automower (#147405)

--- homeassistant/components/husqvarna_automower/calendar.py
+++ homeassistant/components/husqvarna_automower/calendar.py
@@ -2,15 +2,18 @@
 
 from datetime import datetime
 import logging
+from typing import TYPE_CHECKING
 
 from aioautomower.model import make_name_string
 
 from homeassistant.components.calendar import CalendarEntity, CalendarEvent
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.util import dt as dt_util
 
 from . import AutomowerConfigEntry
+from .const import DOMAIN
 from .coordinator import AutomowerDataUpdateCoordinator
 from .entity import AutomowerBaseEntity
 
@@ -52,6 +55,19 @@ class AutomowerCalendarEntity(AutomowerBaseEntity, CalendarEntity):
         self._event: CalendarEvent | None = None
 
     @property
+    def device_name(self) -> str:
+        """Return the prefix for the event summary."""
+        device_registry = dr.async_get(self.hass)
+        device_entry = device_registry.async_get_device(
+            identifiers={(DOMAIN, self.mower_id)}
+        )
+        if TYPE_CHECKING:
+            assert device_entry is not None
+            assert device_entry.name is not None
+
+        return device_entry.name_by_user or device_entry.name
+
+    @property
     def event(self) -> CalendarEvent | None:
         """Return the current or next upcoming event."""
         schedule = self.mower_attributes.calendar
@@ -66,7 +82,7 @@ class AutomowerCalendarEntity(AutomowerBaseEntity, CalendarEntity):
                 program_event.work_area_id
             ]
         return CalendarEvent(
-            summary=make_name_string(work_area_name, program_event.schedule_no),
+            summary=f"{self.device_name} {make_name_string(work_area_name, program_event.schedule_no)}",
             start=program_event.start,
             end=program_event.end,
             rrule=program_event.rrule_str,
@@ -93,7 +109,7 @@ class AutomowerCalendarEntity(AutomowerBaseEntity, CalendarEntity):
                 ]
             calendar_events.append(
                 CalendarEvent(
-                    summary=make_name_string(work_area_name, program_event.schedule_no),
+                    summary=f"{self.device_name} {make_name_string(work_area_name, program_event.schedule_no)}",
                     start=program_event.start.replace(tzinfo=start_date.tzinfo),
                     end=program_event.end.replace(tzinfo=start_date.tzinfo),
                     rrule=program_event.rrule_str,
