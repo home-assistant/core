70c76109f6b8256399693d9de351a639463d867a
Paulus Schoutsen <balloob@gmail.com>
2025-06-30 11:15:12 +0200

    Type Z-Wave JS config entry (#147456)
    * Type Z-Wave JS config entry
    
    * Migrate to data class

--- homeassistant/components/zwave_js/sensor.py
+++ homeassistant/components/zwave_js/sensor.py
@@ -7,7 +7,6 @@ from dataclasses import dataclass
 from typing import Any
 
 import voluptuous as vol
-from zwave_js_server.client import Client as ZwaveClient
 from zwave_js_server.const import CommandClass
 from zwave_js_server.const.command_class.meter import (
     RESET_METER_OPTION_TARGET_VALUE,
@@ -28,7 +27,6 @@ from homeassistant.components.sensor import (
     SensorEntityDescription,
     SensorStateClass,
 )
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     CONCENTRATION_PARTS_PER_MILLION,
     LIGHT_LUX,
@@ -56,7 +54,6 @@ from .const import (
     ATTR_METER_TYPE,
     ATTR_METER_TYPE_NAME,
     ATTR_VALUE,
-    DATA_CLIENT,
     DOMAIN,
     ENTITY_DESC_KEY_BATTERY_LEVEL,
     ENTITY_DESC_KEY_BATTERY_LIST_STATE,
@@ -94,6 +91,7 @@ from .discovery_data_template import (
 from .entity import ZWaveBaseEntity
 from .helpers import get_device_info, get_valueless_base_unique_id
 from .migrate import async_migrate_statistics_sensors
+from .models import ZwaveJSConfigEntry
 
 PARALLEL_UPDATES = 0
 
@@ -576,11 +574,11 @@ def get_entity_description(
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    config_entry: ConfigEntry,
+    config_entry: ZwaveJSConfigEntry,
     async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Set up Z-Wave sensor from config entry."""
-    client: ZwaveClient = config_entry.runtime_data[DATA_CLIENT]
+    client = config_entry.runtime_data.client
     driver = client.driver
     assert driver is not None  # Driver is ready before platforms are loaded.
 
@@ -717,7 +715,7 @@ class ZwaveSensor(ZWaveBaseEntity, SensorEntity):
 
     def __init__(
         self,
-        config_entry: ConfigEntry,
+        config_entry: ZwaveJSConfigEntry,
         driver: Driver,
         info: ZwaveDiscoveryInfo,
         entity_description: SensorEntityDescription,
@@ -756,7 +754,7 @@ class ZWaveNumericSensor(ZwaveSensor):
 
     def __init__(
         self,
-        config_entry: ConfigEntry,
+        config_entry: ZwaveJSConfigEntry,
         driver: Driver,
         info: ZwaveDiscoveryInfo,
         entity_description: SensorEntityDescription,
@@ -831,7 +829,7 @@ class ZWaveListSensor(ZwaveSensor):
 
     def __init__(
         self,
-        config_entry: ConfigEntry,
+        config_entry: ZwaveJSConfigEntry,
         driver: Driver,
         info: ZwaveDiscoveryInfo,
         entity_description: SensorEntityDescription,
@@ -870,7 +868,7 @@ class ZWaveConfigParameterSensor(ZWaveListSensor):
 
     def __init__(
         self,
-        config_entry: ConfigEntry,
+        config_entry: ZwaveJSConfigEntry,
         driver: Driver,
         info: ZwaveDiscoveryInfo,
         entity_description: SensorEntityDescription,
@@ -906,7 +904,7 @@ class ZWaveNodeStatusSensor(SensorEntity):
     _attr_translation_key = "node_status"
 
     def __init__(
-        self, config_entry: ConfigEntry, driver: Driver, node: ZwaveNode
+        self, config_entry: ZwaveJSConfigEntry, driver: Driver, node: ZwaveNode
     ) -> None:
         """Initialize a generic Z-Wave device entity."""
         self.config_entry = config_entry
@@ -968,7 +966,7 @@ class ZWaveControllerStatusSensor(SensorEntity):
     _attr_has_entity_name = True
     _attr_translation_key = "controller_status"
 
-    def __init__(self, config_entry: ConfigEntry, driver: Driver) -> None:
+    def __init__(self, config_entry: ZwaveJSConfigEntry, driver: Driver) -> None:
         """Initialize a generic Z-Wave device entity."""
         self.config_entry = config_entry
         self.controller = driver.controller
@@ -1030,7 +1028,7 @@ class ZWaveStatisticsSensor(SensorEntity):
 
     def __init__(
         self,
-        config_entry: ConfigEntry,
+        config_entry: ZwaveJSConfigEntry,
         driver: Driver,
         statistics_src: ZwaveNode | Controller,
         description: ZWaveJSStatisticsSensorEntityDescription,
