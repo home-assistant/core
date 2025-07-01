526d7b485a122e648b7c6e3de1d0ce2c88755ac3
David Bugl <bugl@lunatone.com>
2025-07-01 12:46:05 +0000

    Add options flow handler to handle device scanning

--- homeassistant/components/lunatone/config_flow.py
+++ homeassistant/components/lunatone/config_flow.py
@@ -1,15 +1,26 @@
 """Config flow for Lunatone."""
 
+import asyncio
+from enum import StrEnum
 import logging
-from typing import Any
+from typing import Any, Final
+from urllib.parse import urlparse
 
 import aiohttp
-from lunatone_dali_api_client import Auth, Info
+from lunatone_dali_api_client import Auth, DALIScan, Info
+from lunatone_dali_api_client.models import ScanState, StartScanData
 import voluptuous as vol
 
-from homeassistant.config_entries import SOURCE_USER, ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import (
+    SOURCE_USER,
+    ConfigEntry,
+    ConfigFlow,
+    ConfigFlowResult,
+    OptionsFlow,
+)
 from homeassistant.const import CONF_URL
-from homeassistant.helpers import config_validation as cv
+from homeassistant.core import callback
+from homeassistant.helpers import config_validation as cv, selector
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
@@ -21,6 +32,18 @@ DATA_SCHEMA = vol.Schema(
     {vol.Required(CONF_URL, default="http://"): cv.string},
 )
 RECONFIGURE_SCHEMA = DATA_SCHEMA
+
+
+class DeviceScanMethod(StrEnum):
+    """Device scan methods."""
+
+    CURRENT_DEVICE_LIST = "current_device_list"
+    SYSTEM_EXTENSION = "system_extension"
+    NEW_INSTALLATION = "new_installation"
+
+
+DEFAULT_DEVICE_SCAN_METHOD: Final = DeviceScanMethod.CURRENT_DEVICE_LIST
+DEVICE_SCAN_METHODS: Final[list[str]] = [option.value for option in DeviceScanMethod]
 
 
 class LunatoneDALIIoTConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -105,3 +128,90 @@ class LunatoneDALIIoTConfigFlow(ConfigFlow, domain=DOMAIN):
     ) -> ConfigFlowResult:
         """Handle a reconfiguration flow initialized by the user."""
         return await self.async_step_user(user_input)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> OptionsFlow:
+        """Create the options flow."""
+        return OptionsFlowHandler(config_entry)
+
+
+class OptionsFlowHandler(OptionsFlow):
+    """Lunatone DALI IoT options flow."""
+
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+        self.device_scan_task: asyncio.Task | None = None
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Manage the options."""
+        if user_input is not None:
+            method = user_input["device_scan_method"]
+            if not self.device_scan_task:
+                self.device_scan_task = self.hass.async_create_task(
+                    self._async_device_scan(method)
+                )
+        if self.device_scan_task:
+            if not self.device_scan_task.done():
+                return self.async_show_progress(
+                    progress_action="device_scan",
+                    progress_task=self.device_scan_task,
+                )
+            self.device_scan_task = None
+            return self.async_show_progress_done(next_step_id="finish")
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        "device_scan_method", default=DEFAULT_DEVICE_SCAN_METHOD
+                    ): selector.SelectSelector(
+                        selector.SelectSelectorConfig(
+                            options=DEVICE_SCAN_METHODS,
+                            translation_key="device_scan_method",
+                            mode=selector.SelectSelectorMode.LIST,
+                        ),
+                    ),
+                }
+            ),
+        )
+
+    async def async_step_finish(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle device scan."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+        return self.async_show_form()
+
+    async def _async_device_scan(self, method: DeviceScanMethod) -> None:
+        auth = Auth(
+            session=async_get_clientsession(self.hass),
+            base_url=self.config_entry.data[CONF_URL],
+        )
+        scan = DALIScan(auth)
+
+        start_scan_data = None
+        if method == DeviceScanMethod.CURRENT_DEVICE_LIST:
+            start_scan_data = StartScanData(noAddressing=True)
+        elif method == DeviceScanMethod.SYSTEM_EXTENSION:
+            start_scan_data = StartScanData()
+        elif method == DeviceScanMethod.NEW_INSTALLATION:
+            start_scan_data = StartScanData(newInstallation=True)
+
+        if start_scan_data is None:
+            return
+        await scan.async_start(start_scan_data)
+        await scan.async_update()
+
+        for _ in range(360):
+            if scan.data.status == ScanState.DONE:
+                return
+            await asyncio.sleep(5)
+            await scan.async_update()
+        raise RuntimeError
