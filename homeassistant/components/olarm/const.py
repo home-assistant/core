7253f318027527edf51e7befd2bde6e4b3d7e3d3
pelicarno <peter.pelicarno@gmail.com>
2025-07-01 16:29:14 +0000

    Fix tests and formatting

--- homeassistant/components/olarm/const.py
+++ homeassistant/components/olarm/const.py
@@ -5,4 +5,3 @@ OAUTH2_AUTHORIZE = "https://oauth.olarm.com/oauth2/authorize"
 OAUTH2_TOKEN = "https://oauth.olarm.com/oauth2/token"
 OAUTH2_CLIENT_ID = "4at9g5tkr5mpbv9bi2m9qa2dal"
 OAUTH2_CLIENT_SECRET = ""  # Public OAuth client
-
