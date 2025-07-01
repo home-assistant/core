cf8ed05bcd5e3977beb71cd69c6e07b6fdb01630
Erik Montnemery <erik@montnemery.com>
2025-07-01 10:46:13 +0200

    Correct Google generative AI config entry migration (#147856)

--- homeassistant/components/google_generative_ai_conversation/config_flow.py
+++ homeassistant/components/google_generative_ai_conversation/config_flow.py
@@ -92,6 +92,7 @@ class GoogleGenerativeAIConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Google Generative AI Conversation."""
 
     VERSION = 2
+    MINOR_VERSION = 2
 
     async def async_step_api(
         self, user_input: dict[str, Any] | None = None
