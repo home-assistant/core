6b39a6d01fc3b01a652d216638cc41edbde25e1b
Raj Laud <raj.laud@gmail.com>
2025-07-02 14:49:58 +0000

    WIP add quality scale

--- homeassistant/components/victron_ble/__init__.py
+++ homeassistant/components/victron_ble/__init__.py
@@ -52,7 +52,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: VictronBLEConfigEntry) -
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: VictronBLEConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = False
 
