76dbaa00dae01cb1ef6c9e8708262efeb787489b
epenet <6771947+epenet@users.noreply.github.com>
2025-07-05 09:54:54 +0200

    Add initial tuya snapshot tests (#148034)
    Co-authored-by: Franck Nijhof <git@frenck.dev>

---
+++ tests/components/tuya/test_humidifier.py
@@ -0,0 +1,36 @@
+"""Test Tuya humidifier platform."""
+
+from __future__ import annotations
+
+from unittest.mock import patch
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+from tuya_sharing import CustomerDevice
+
+from homeassistant.components.tuya import ManagerCompat
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
+
+from . import initialize_entry
+
+from tests.common import MockConfigEntry, snapshot_platform
+
+
+@pytest.mark.parametrize(
+    "mock_device_code", ["cs_arete_two_12l_dehumidifier_air_purifier"]
+)
+@patch("homeassistant.components.tuya.PLATFORMS", [Platform.HUMIDIFIER])
+async def test_platform_setup_and_discovery(
+    hass: HomeAssistant,
+    mock_manager: ManagerCompat,
+    mock_config_entry: MockConfigEntry,
+    mock_device: CustomerDevice,
+    entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test platform setup and discovery."""
+    await initialize_entry(hass, mock_manager, mock_config_entry, mock_device)
+
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
