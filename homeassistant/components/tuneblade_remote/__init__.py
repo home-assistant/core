feae7305e57253327ff65800b8c8b4ce8e2d0239
Matthew Morgan MMO <M.Morgan@oxb.com>
2025-07-05 14:49:12 +0100

    Further fixes7

--- homeassistant/components/tuneblade_remote/__init__.py
+++ homeassistant/components/tuneblade_remote/__init__.py
@@ -44,13 +44,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, ["media_player"])
 
+    entry.runtime_data = {}
+
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload TuneBlade config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(
-        entry, ["media_player", "switch"]
+        entry, ["media_player"]
     )
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
