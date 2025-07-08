70f21ea8832941dd03142df302b195ba3bfab71b
Erik <erik@montnemery.com>
2025-07-08 14:23:45 +0200

    Improve subentry related test

--- tests/helpers/test_entity_registry.py
+++ tests/helpers/test_entity_registry.py
@@ -1822,10 +1822,19 @@ async def test_remove_config_subentry_from_device_removes_entities(
     assert not entity_registry.async_is_registered(entry_3.entity_id)
 
 
+@pytest.mark.parametrize(
+    ("subentries_in_device", "subentry_in_entity"),
+    [
+        (["mock-subentry-id-1", "mock-subentry-id-2"], None),
+        ([None, "mock-subentry-id-2"], "mock-subentry-id-1"),
+    ],
+)
 async def test_remove_config_subentry_from_device_removes_entities_2(
     hass: HomeAssistant,
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
+    subentries_in_device: list[str | None],
+    subentry_in_entity: str | None,
 ) -> None:
     """Test that we don't remove entities with no config entry when device is modified."""
     config_entry_1 = MockConfigEntry(
@@ -1856,24 +1865,20 @@ async def test_remove_config_subentry_from_device_removes_entities_2(
     )
     config_entry_1.add_to_hass(hass)
 
-    # Create device with three config subentries
+    # Create device with two config subentries
     device_registry.async_get_or_create(
         config_entry_id=config_entry_1.entry_id,
-        config_subentry_id="mock-subentry-id-1",
-        connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
-    )
-    device_registry.async_get_or_create(
-        config_entry_id=config_entry_1.entry_id,
-        config_subentry_id="mock-subentry-id-2",
+        config_subentry_id=subentries_in_device[0],
         connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
     )
     device_entry = device_registry.async_get_or_create(
         config_entry_id=config_entry_1.entry_id,
+        config_subentry_id=subentries_in_device[1],
         connections={(dr.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
     )
     assert device_entry.config_entries == {config_entry_1.entry_id}
     assert device_entry.config_entries_subentries == {
-        config_entry_1.entry_id: {None, "mock-subentry-id-1", "mock-subentry-id-2"},
+        config_entry_1.entry_id: set(subentries_in_device),
     }
 
     # Create an entity without config entry or subentry
@@ -1883,15 +1888,16 @@ async def test_remove_config_subentry_from_device_removes_entities_2(
         "5678",
         device_id=device_entry.id,
     )
-    # Create an entity same config entry but no subentry
+    # Create an entity for same config entry but no subentry not in device
     entry_2 = entity_registry.async_get_or_create(
         "light",
         "some_helper",
         "5678",
         config_entry=config_entry_1,
+        config_subentry_id=subentry_in_entity,
         device_id=device_entry.id,
     )
-    # Create an entity same config entry but subentry not in device
+    # Create an entity for same config entry but subentry not in device
     entry_3 = entity_registry.async_get_or_create(
         "light",
         "some_helper",
@@ -1910,7 +1916,7 @@ async def test_remove_config_subentry_from_device_removes_entities_2(
     device_registry.async_update_device(
         device_entry.id,
         remove_config_entry_id=config_entry_1.entry_id,
-        remove_config_subentry_id=None,
+        remove_config_subentry_id=subentries_in_device[0],
     )
     await hass.async_block_till_done()
 
@@ -1924,7 +1930,7 @@ async def test_remove_config_subentry_from_device_removes_entities_2(
     device_registry.async_update_device(
         device_entry.id,
         remove_config_entry_id=config_entry_1.entry_id,
-        remove_config_subentry_id="mock-subentry-id-1",
+        remove_config_subentry_id=subentries_in_device[0],
     )
     await hass.async_block_till_done()
 
