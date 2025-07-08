235a64031ce47ba2c341150d898f9161ac760a88
Joostlek <joostlek@outlook.com>
2025-07-08 16:47:04 +0200

    Rework tests

--- tests/components/snapcast/conftest.py
+++ tests/components/snapcast/conftest.py
@@ -1,11 +1,13 @@
 """Test the snapcast config flow."""
 
-from collections.abc import Generator, Mapping
-from typing import Any
-from unittest.mock import AsyncMock, MagicMock, patch
+from collections.abc import Generator
+from unittest.mock import AsyncMock, patch
 
 import pytest
+from snapcast.control.client import Snapclient
+from snapcast.control.group import Snapgroup
 from snapcast.control.server import CONTROL_PORT
+from snapcast.control.stream import Snapstream
 
 from homeassistant.components.snapcast.const import DOMAIN
 from homeassistant.components.snapcast.coordinator import Snapserver
@@ -25,13 +27,28 @@ def mock_setup_entry() -> Generator[AsyncMock]:
 
 
 @pytest.fixture
-def mock_create_server() -> Generator[AsyncMock]:
+def mock_create_server(
+    mock_group: AsyncMock,
+    mock_client: AsyncMock,
+    mock_stream_1: AsyncMock,
+    mock_stream_2: AsyncMock,
+) -> Generator[AsyncMock]:
     """Create mock snapcast connection."""
-    mock_connection = AsyncMock()
-    mock_connection.start = AsyncMock(return_value=None)
-    mock_connection.stop = MagicMock()
-    with patch("snapcast.control.create_server", return_value=mock_connection):
-        yield mock_connection
+    with patch(
+        "homeassistant.components.snapcast.coordinator.Snapserver", autospec=True
+    ) as mock_snapserver:
+        mock_server = mock_snapserver.return_value
+        mock_server.groups = [mock_group]
+        mock_server.clients = [mock_client]
+        mock_server.streams = [mock_stream_1, mock_stream_2]
+        mock_server.group.return_value = mock_group
+        mock_server.client.return_value = mock_client
+
+        def get_stream(identifier: str) -> AsyncMock:
+            return {s.identifier: s for s in mock_server.streams}[identifier]
+
+        mock_server.stream = get_stream
+        yield mock_server
 
 
 @pytest.fixture
@@ -58,90 +75,96 @@ def mock_server_connection() -> Generator[Snapserver]:
 
 
 @pytest.fixture
-def mock_server_state() -> Mapping[str, Any]:
-    """Create a mock server state."""
+def mock_group(stream: str, streams: dict[str, AsyncMock]) -> AsyncMock:
+    """Create a mock Snapgroup."""
+    group = AsyncMock(spec=Snapgroup)
+    group.identifier = "4dcc4e3b-c699-a04b-7f0c-8260d23c43e1"
+    group.name = "test_group"
+    group.friendly_name = "test_group"
+    group.stream = stream
+    group.muted = False
+    group.stream_status = streams[stream].status
+    group.volume = 48
+    group.streams_by_name.return_value = {s.friendly_name: s for s in streams.values()}
+    return group
+
+
+@pytest.fixture
+def mock_client(mock_group: AsyncMock) -> AsyncMock:
+    """Create a mock Snapclient."""
+    client = AsyncMock(spec=Snapclient)
+    client.identifier = "00:21:6a:7d:74:fc#2"
+    client.friendly_name = "test_client"
+    client.version = "0.10.0"
+    client.connected = True
+    client.name = "Snapclient"
+    client.latency = 6
+    client.muted = False
+    client.volume = 48
+    client.group = mock_group
+    mock_group.clients = [client.identifier]
+    return client
+
+
+@pytest.fixture
+def mock_stream_1() -> AsyncMock:
+    """Create a mock stream."""
+    stream = AsyncMock(spec=Snapstream)
+    stream.identifier = "test_stream_1"
+    stream.status = "playing"
+    stream.name = "Test Stream 1"
+    stream.friendly_name = "Test Stream 1"
+    stream.metadata = {
+        "album": "Test Album",
+        "artist": ["Test Artist 1", "Test Artist 2"],
+        "title": "Test Title",
+        "artUrl": "http://localhost/test_art.jpg",
+        "albumArtist": [
+            "Test Album Artist 1",
+            "Test Album Artist 2",
+        ],
+        "trackNumber": 10,
+        "duration": 60.0,
+    }
+    stream.meta = stream.metadata
+    stream.properties = {
+        "position": 30.0,
+        **stream.metadata,
+    }
+    stream.path = None
+    return stream
+
+
+@pytest.fixture
+def mock_stream_2() -> AsyncMock:
+    """Create a mock stream."""
+    stream = AsyncMock(spec=Snapstream)
+    stream.identifier = "test_stream_2"
+    stream.status = "idle"
+    stream.name = "Test Stream 2"
+    stream.friendly_name = "Test Stream 2"
+    stream.metadata = None
+    stream.meta = None
+    stream.properties = None
+    stream.path = None
+    return stream
+
+
+@pytest.fixture(
+    params=[
+        "test_stream_1",
+        "test_stream_2",
+    ]
+)
+def stream(request: pytest.FixtureRequest) -> Generator[str]:
+    """Return every device."""
+    return request.param
+
+
+@pytest.fixture
+def streams(mock_stream_1: AsyncMock, mock_stream_2: AsyncMock) -> dict[str, AsyncMock]:
+    """Return a dictionary of mock streams."""
     return {
-        "server": {
-            "server": {
-                "snapserver": {
-                    "controlProtocolVersion": 1,
-                    "name": "Snapserver",
-                    "protocolVersion": 1,
-                    "version": "0.10.0",
-                },
-            },
-            "groups": [
-                {
-                    "clients": [
-                        {
-                            "id": "00:21:6a:7d:74:fc#2",
-                            "connected": True,
-                            "lastSeen": {"sec": 1488025751, "usec": 654777},
-                            "config": {
-                                "instance": 2,
-                                "latency": 6,
-                                "name": "test_client",
-                                "volume": {"muted": False, "percent": 48},
-                            },
-                            "snapclient": {
-                                "name": "Snapclient",
-                                "protocolVersion": 2,
-                                "version": "0.10.0",
-                            },
-                        }
-                    ],
-                    "id": "4dcc4e3b-c699-a04b-7f0c-8260d23c43e1",
-                    "muted": False,
-                    "name": "test_group",
-                    "stream_id": "test_stream_1",
-                }
-            ],
-            "streams": [
-                {
-                    "id": "test_stream_1",
-                    "status": "playing",
-                    "uri": {
-                        "fragment": "",
-                        "host": "",
-                        "query": {
-                            "chunk_ms": "20",
-                            "codec": "flac",
-                            "name": "Test Stream 1",
-                            "sampleformat": "48000:16:2",
-                        },
-                        "scheme": "pipe",
-                    },
-                    "properties": {
-                        "position": 30.0,
-                        "metadata": {
-                            "album": "Test Album",
-                            "artist": ["Test Artist 1", "Test Artist 2"],
-                            "title": "Test Title",
-                            "artUrl": "http://localhost/test_art.jpg",
-                            "albumArtist": [
-                                "Test Album Artist 1",
-                                "Test Album Artist 2",
-                            ],
-                            "trackNumber": 10,
-                            "duration": 60.0,
-                        },
-                    },
-                },
-                {
-                    "id": "test_stream_2",
-                    "status": "idle",
-                    "uri": {
-                        "fragment": "",
-                        "host": "",
-                        "query": {
-                            "chunk_ms": "20",
-                            "codec": "flac",
-                            "name": "Test Stream 2",
-                            "sampleformat": "48000:16:2",
-                        },
-                        "scheme": "pipe",
-                    },
-                },
-            ],
-        }
+        mock_stream_1.identifier: mock_stream_1,
+        mock_stream_2.identifier: mock_stream_2,
     }
--- tests/components/snapcast/test_media_player.py
+++ tests/components/snapcast/test_media_player.py
@@ -1,132 +1,30 @@
 """Test the snapcast media player implementation."""
 
-import pytest
+from unittest.mock import AsyncMock, patch
 
-from homeassistant.components.media_player import (
-    ATTR_INPUT_SOURCE,
-    ATTR_INPUT_SOURCE_LIST,
-    ATTR_MEDIA_ALBUM_ARTIST,
-    ATTR_MEDIA_ALBUM_NAME,
-    ATTR_MEDIA_ARTIST,
-    ATTR_MEDIA_DURATION,
-    ATTR_MEDIA_POSITION,
-    ATTR_MEDIA_TITLE,
-    ATTR_MEDIA_TRACK,
-    ATTR_MEDIA_VOLUME_LEVEL,
-    ATTR_MEDIA_VOLUME_MUTED,
-    DOMAIN as MEDIA_PLAYER_DOMAIN,
-    SERVICE_SELECT_SOURCE,
-)
-from homeassistant.components.snapcast.const import DOMAIN
+from syrupy.assertion import SnapshotAssertion
+
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import STATE_PLAYING
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from . import setup_integration
-from .const import TEST_CLIENT_ENTITY_ID, TEST_GROUP_ENTITY_ID
+
+from tests.common import MockConfigEntry, snapshot_platform
 
 
-@pytest.mark.usefixtures("mock_server_connection")
-async def test_state(hass: HomeAssistant, mock_config_entry, mock_server_state) -> None:
+async def test_state(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+    mock_create_server: AsyncMock,
+    entity_registry: er.EntityRegistry,
+    snapshot: SnapshotAssertion,
+) -> None:
     """Test basic state information."""
 
     # Setup and verify the integration is loaded
-    await setup_integration(hass, mock_config_entry)
-    assert mock_config_entry.state is ConfigEntryState.LOADED
+    with patch("secrets.token_hex", return_value="mock_token"):
+        await setup_integration(hass, mock_config_entry)
+        assert mock_config_entry.state is ConfigEntryState.LOADED
 
-    # Fetch the coordinator
-    coordinator = hass.data[DOMAIN][mock_config_entry.entry_id]
-
-    # Load the test server data and manually update the coordinator
-    coordinator.server.synchronize(mock_server_state)
-    coordinator.async_update_listeners()
-
-    # Asset basic state matches in both client and group entities
-    for entity_id in (TEST_CLIENT_ENTITY_ID, TEST_GROUP_ENTITY_ID):
-        state = hass.states.get(entity_id)
-        assert state
-
-        assert state.state == STATE_PLAYING
-        assert state.attributes.get(ATTR_MEDIA_VOLUME_MUTED) is False
-        assert state.attributes.get(ATTR_MEDIA_VOLUME_LEVEL) == 0.48
-        assert state.attributes.get(ATTR_INPUT_SOURCE) == "test_stream_1"
-        assert state.attributes.get(ATTR_INPUT_SOURCE_LIST) == [
-            "Test Stream 1",
-            "Test Stream 2",
-        ]
-
-
-@pytest.mark.usefixtures("mock_server_connection")
-async def test_metadata(
-    hass: HomeAssistant, mock_config_entry, mock_server_state
-) -> None:
-    """Test metadata is parsed from Snapcast stream."""
-
-    # Setup and verify the integration is loaded
-    await setup_integration(hass, mock_config_entry)
-    assert mock_config_entry.state is ConfigEntryState.LOADED
-
-    # Fetch the coordinator
-    coordinator = hass.data[DOMAIN][mock_config_entry.entry_id]
-
-    # Load the test server data and manually update the coordinator
-    coordinator.server.synchronize(mock_server_state)
-    coordinator.async_update_listeners()
-
-    # Asset metadata matches in both client and group entities
-    for entity_id in (TEST_CLIENT_ENTITY_ID, TEST_GROUP_ENTITY_ID):
-        state = hass.states.get(entity_id)
-        assert state
-
-        assert state.attributes[ATTR_MEDIA_ARTIST] == "Test Artist 1, Test Artist 2"
-        assert (
-            state.attributes[ATTR_MEDIA_ALBUM_ARTIST]
-            == "Test Album Artist 1, Test Album Artist 2"
-        )
-        assert state.attributes[ATTR_MEDIA_ALBUM_NAME] == "Test Album"
-        assert state.attributes[ATTR_MEDIA_TITLE] == "Test Title"
-        assert state.attributes[ATTR_MEDIA_TRACK] == 10
-        assert state.attributes[ATTR_MEDIA_DURATION] == 60
-        assert state.attributes[ATTR_MEDIA_POSITION] == 30
-
-
-@pytest.mark.usefixtures("mock_server_connection")
-async def test_no_metadata(
-    hass: HomeAssistant, mock_config_entry, mock_server_state
-) -> None:
-    """Test no metadata exists when a stream has no metadata."""
-
-    # Setup and verify the integration is loaded
-    await setup_integration(hass, mock_config_entry)
-    assert mock_config_entry.state is ConfigEntryState.LOADED
-
-    # Fetch the coordinator
-    coordinator = hass.data[DOMAIN][mock_config_entry.entry_id]
-
-    # Load the test server data and manually update the coordinator
-    coordinator.server.synchronize(mock_server_state)
-    coordinator.async_update_listeners()
-
-    # Switch to the stream without metadata
-    await hass.services.async_call(
-        MEDIA_PLAYER_DOMAIN,
-        SERVICE_SELECT_SOURCE,
-        service_data={"entity_id": TEST_GROUP_ENTITY_ID, "source": "Test Stream 2"},
-        blocking=True,
-    )
-
-    # Manually update coordinator and entities
-    coordinator.async_update_listeners()
-
-    # Assert that no metadata attributes are present in both client and group entities
-    for entity_id in (TEST_CLIENT_ENTITY_ID, TEST_GROUP_ENTITY_ID):
-        state = hass.states.get(entity_id)
-        assert state
-
-        assert ATTR_MEDIA_ARTIST not in state.attributes
-        assert ATTR_MEDIA_ALBUM_ARTIST not in state.attributes
-        assert ATTR_MEDIA_ALBUM_NAME not in state.attributes
-        assert ATTR_MEDIA_TITLE not in state.attributes
-        assert ATTR_MEDIA_TRACK not in state.attributes
-        assert ATTR_MEDIA_DURATION not in state.attributes
-        assert ATTR_MEDIA_POSITION not in state.attributes
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
