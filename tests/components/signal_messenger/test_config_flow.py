f94c3a458f0d3b7e5daca85c5a461018ae160d97
Robin Thoni <robin@rthoni.com>
2025-07-08 21:10:21 +0000

    Added failure test

--- tests/components/signal_messenger/test_config_flow.py
+++ tests/components/signal_messenger/test_config_flow.py
@@ -2,6 +2,8 @@
 
 from unittest.mock import patch
 
+import requests
+
 from homeassistant import config_entries
 from homeassistant.components.signal_messenger.const import (
     CONF_RECP_NR,
@@ -51,3 +53,33 @@ async def test_form_user_success(hass: HomeAssistant) -> None:
     }
     assert len(hass.services.async_services()) == 1
     assert hass.services.has_service("notify", "signal_my_signal")
+
+
+async def test_form_user_can_not_connect(hass: HomeAssistant) -> None:
+    """Test we get the user form."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {}
+
+    with (
+        patch(
+            "requests.get",
+        ) as mock_request_get,
+    ):
+        mock_request_get.exc = requests.exceptions.ConnectionError
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_NAME: "My Signal",
+                CONF_SIGNAL_CLI_REST_API: "http://127.0.0.1:8123/",
+                CONF_SENDER_NR: "+1234567890",
+                CONF_RECP_NR: "+0987654321 +1122334455",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result2.get("step_id") == "user"
+    assert result2.get("errors") == {"base": "cannot_connect"}
