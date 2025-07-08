3c86b00dca6ab06ae87f3e456edf6e3b18df5cef
Robin Thoni <robin@rthoni.com>
2025-07-08 21:00:59 +0000

    Added basic config_flow test

---
+++ tests/components/signal_messenger/test_config_flow.py
@@ -0,0 +1,65 @@
+"""Test the signal_messenger config flow."""
+
+from unittest.mock import patch
+
+from homeassistant import config_entries
+from homeassistant.components.signal_messenger.const import (
+    CONF_RECP_NR,
+    CONF_SENDER_NR,
+    CONF_SIGNAL_CLI_REST_API,
+    DOMAIN,
+)
+from homeassistant.const import CONF_NAME
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResultType
+
+
+async def test_form_user(hass: HomeAssistant) -> None:
+    """Test we get the user form."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] == {}
+
+    def request_get_mock(*args, **kwargs):
+        """Mock the requests.get method."""
+
+        class MockResponse:
+            def __init__(self, status_code) -> None:
+                self.status_code = status_code
+
+            def json(self):
+                return {"status": "ok"}
+
+        return MockResponse(200)
+
+    with (
+        patch(
+            "requests.get",
+        ) as mock_request_get,
+    ):
+        mock_request_get.return_value.status_code = 200
+        mock_request_get.return_value.content = "{}"
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_NAME: "My Signal",
+                CONF_SIGNAL_CLI_REST_API: "http://127.0.0.1:8123/",
+                CONF_SENDER_NR: "+1234567890",
+                CONF_RECP_NR: "+0987654321 +1122334455",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] is FlowResultType.CREATE_ENTRY
+    assert result2["title"] == "Signal My Signal"
+    assert result2["data"] == {
+        CONF_NAME: "My Signal",
+        CONF_SIGNAL_CLI_REST_API: "http://127.0.0.1:8123/",
+        CONF_SENDER_NR: "+1234567890",
+        CONF_RECP_NR: ["+0987654321", "+1122334455"],
+    }
+    assert len(hass.services.async_services()) == 1
+    assert hass.services.has_service("notify", "signal_my_signal")
