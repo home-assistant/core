bf8de6e5d8e801de5168f7d236ebc93eff92c78f
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-07-09 16:15:12 +0800

    change follow comments

--- homeassistant/components/switchbot_cloud/fan.py
+++ homeassistant/components/switchbot_cloud/fan.py
@@ -55,25 +55,16 @@ class SwitchBotCloudFan(SwitchBotCloudEntity, FanEntity):
         """Set attributes from coordinator data."""
         if self.coordinator.data is None:
             return
-        if (
-            self._attr_is_on is None
-            and self.percentage == 0
-            and self.preset_mode is None
-        ):
-            response: dict | None = self.coordinator.data
-            assert response is not None
-            power: str | None = response.get("power")
-            mode: str | None = response.get("mode")
-            fan_speed: str | None = response.get("fanSpeed")
-            assert fan_speed is not None
-            self.preset_mode = mode if mode else BatteryCirculatorFanMode.DIRECT.value
-            self.percentage = (
-                int(fan_speed)
-                if self.preset_mode in BatteryCirculatorFanMode.DIRECT.value
-                else 0
-            )
-            assert power is not None
-            self._attr_is_on = "on" in power
+
+        power: str | None = self.coordinator.data.get("power")
+        mode: str | None = self.coordinator.data.get("mode")
+        fan_speed: str | None = self.coordinator.data.get("fanSpeed")
+        assert power is not None
+        self._attr_is_on = "on" in power
+        assert mode is not None
+        self.preset_mode = mode
+        assert fan_speed is not None
+        self.percentage = int(fan_speed)
 
     async def async_turn_on(
         self,
@@ -82,116 +73,42 @@ class SwitchBotCloudFan(SwitchBotCloudEntity, FanEntity):
         **kwargs: Any,
     ) -> None:
         """Turn on the fan."""
-        response: dict | None = await self._api.get_status(self.unique_id)
-        assert response is not None
-
-        power: str | None = response.get("power")
-        fan_speed: int | None = response.get("fanSpeed")
-        mode: str | None = response.get("mode")
-        if self._attr_is_on is False and (power and "off" in power):
-            await self.send_api_command(CommonCommands.ON)
-            self._attr_is_on = True
-        elif self._attr_is_on is False and (power and "on" in power):
-            self._attr_is_on = True
-        else:
-            pass
-        if self.preset_mode is None:
-            self.preset_mode = BatteryCirculatorFanMode.DIRECT.value
-        # if mode exist, do something
-        if mode is not None:
-            # if current mode != preset_mode, send set mode command again
-            if mode != self.preset_mode:
-                await self.send_api_command(
-                    command=BatteryCirculatorFanCommands.SET_WIND_MODE,
-                    parameters=self.preset_mode,
-                )
-        # if mode is None, set as default
-        else:
-            self.preset_mode = BatteryCirculatorFanMode.DIRECT.value
-            await self.send_api_command(
-                command=BatteryCirculatorFanCommands.SET_WIND_MODE,
-                parameters=self.preset_mode,
-            )
-
-        # if fanspeed exist, do something
-        if (
-            fan_speed is not None
-            and fan_speed != self.percentage
-            and self.preset_mode in BatteryCirculatorFanMode.DIRECT.value
-        ):
-            await self.send_api_command(
-                command=BatteryCirculatorFanCommands.SET_WIND_SPEED,
-                parameters=str(percentage),
-            )
-        else:
-            self.percentage = 0
+        await self.coordinator.async_refresh()
+        await self.send_api_command(CommonCommands.ON)
+        self._attr_is_on = True
         self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the fan."""
-
-        response: dict | None = await self._api.get_status(self.unique_id)
-        assert response is not None
-        power: str | None = response.get("power")
-        fan_speed: int | None = response.get("fanSpeed")
-        mode: str | None = response.get("mode")
-
-        # update self.percentage and self.preset_mode
-        if fan_speed is not None:
-            self.percentage = fan_speed
-        if mode is not None:
-            self.preset_mode = mode
-            if mode not in BatteryCirculatorFanMode.DIRECT.value:
-                self.percentage = 0
-
-        if self._attr_is_on is True and (power and "on" in power):
-            await self.send_api_command(CommonCommands.OFF)
-            self._attr_is_on = False
-        elif self._attr_is_on is True and (power and "off" in power):
-            self._attr_is_on = False
-        else:
-            pass
+        await self.coordinator.async_refresh()
+        await self.send_api_command(CommonCommands.OFF)
+        self.percentage = 0
+        self._attr_is_on = False
         self.async_write_ha_state()
 
     async def async_set_percentage(self, percentage: int) -> None:
         """Set the speed of the fan, as a percentage."""
-        assert 0 <= percentage <= 100
-        response: dict | None = await self._api.get_status(self.unique_id)
-        assert response is not None
-        mode: str | None = response.get("mode")
-        if self._attr_is_on is False:
+        await self.coordinator.async_refresh()
+        if self.is_on and self.preset_mode == BatteryCirculatorFanMode.DIRECT.value:
+            await self.send_api_command(
+                command=BatteryCirculatorFanCommands.SET_WIND_SPEED,
+                parameters=str(percentage),
+            )
+            self.percentage = percentage
+        else:
             self.percentage = 0
-            self.async_write_ha_state()
-            return
 
-        if mode is not None:
-            if mode in BatteryCirculatorFanMode.DIRECT.value:
-                await self.send_api_command(
-                    command=BatteryCirculatorFanCommands.SET_WIND_SPEED,
-                    parameters=str(percentage),
-                )
-                self.percentage = percentage
-                self.preset_mode = mode
-            else:
-                self.percentage = 0
-                self.preset_mode = mode
         self.async_write_ha_state()
 
     async def async_set_preset_mode(self, preset_mode: str) -> None:
         """Set new preset mode."""
-        assert preset_mode in [item.value for item in list(BatteryCirculatorFanMode)]
-        if preset_mode != self.preset_mode:
-            await self.send_api_command(
-                command=BatteryCirculatorFanCommands.SET_WIND_MODE,
-                parameters=preset_mode,
-            )
+        await self.coordinator.async_refresh()
+        await self.send_api_command(
+            command=BatteryCirculatorFanCommands.SET_WIND_MODE,
+            parameters=preset_mode,
+        )
         self.preset_mode = preset_mode
 
-        response: dict | None = await self._api.get_status(self.unique_id)
-        assert response is not None
-        fan_speed: int | None = response.get("fanSpeed")
-        if preset_mode in BatteryCirculatorFanMode.DIRECT.value:
-            self.percentage = int(fan_speed) if fan_speed else 0
-        else:
+        if self.preset_mode != BatteryCirculatorFanMode.DIRECT.value:
             self.percentage = 0
         self.async_write_ha_state()
