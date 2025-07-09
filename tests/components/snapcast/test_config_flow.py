f8e742c71f5aac1625a9688f167d0d7bb2e8bb27
Joostlek <joostlek@outlook.com>
2025-07-09 15:34:00 +0200

    Fix

--- tests/components/snapcast/test_config_flow.py
+++ tests/components/snapcast/test_config_flow.py
@@ -19,7 +19,7 @@ TEST_CONNECTION = {CONF_HOST: "127.0.0.1", CONF_PORT: 1705}
 async def test_full_flow(
     hass: HomeAssistant, mock_setup_entry: AsyncMock, mock_server: AsyncMock
 ) -> None:
-    """Test we get the form and handle errors and successful connection."""
+    """Test the full flow."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": SOURCE_USER}
     )
@@ -81,7 +81,9 @@ async def test_exceptions(
     assert result["type"] is FlowResultType.CREATE_ENTRY
 
 
-async def test_abort(hass: HomeAssistant, mock_config_entry: MockConfigEntry) -> None:
+async def test_already_setup(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry
+) -> None:
     """Test config flow abort if device is already configured."""
     mock_config_entry.add_to_hass(hass)
 
