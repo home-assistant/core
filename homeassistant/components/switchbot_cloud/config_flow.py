e56b8521e3a88cbf26cbb061bfe6569ebde66003
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-07-09 19:35:30 +0800

    reset config_flow

--- homeassistant/components/switchbot_cloud/config_flow.py
+++ homeassistant/components/switchbot_cloud/config_flow.py
@@ -10,24 +10,12 @@ from switchbot_api import (
 )
 import voluptuous as vol
 
-from homeassistant.config_entries import (
-    ConfigEntry,
-    ConfigFlow,
-    ConfigFlowResult,
-    OptionsFlow,
-)
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_API_KEY, CONF_API_TOKEN
-from homeassistant.core import callback
-from homeassistant.helpers import selector
-from homeassistant.helpers.selector import (
-    DeviceFilterSelectorConfig,
-    DeviceSelectorConfig,
-)
 
 from .const import DOMAIN, ENTRY_TITLE
 
 _LOGGER = getLogger(__name__)
-
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_API_TOKEN): str,
@@ -36,45 +24,10 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 
-class SwitchbotCloudOptionsFlowHandler(OptionsFlow):
-    """Handle Switchbot options."""
-
-    async def async_step_init(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Manage Switchbot Cloud options."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-        options: dict[vol.Optional, Any] = {
-            vol.Optional(
-                "SetNightLatchMode",
-                default=self.config_entry.options.get("SetNightLatchMode"),
-            ): selector.DeviceSelector(
-                DeviceSelectorConfig(
-                    multiple=True,
-                    filter=[
-                        DeviceFilterSelectorConfig(model="Smart Lock"),
-                        DeviceFilterSelectorConfig(model="Smart Lock Pro"),
-                        DeviceFilterSelectorConfig(model="Smart Lock Ultra"),
-                    ],
-                )
-            )
-        }
-        return self.async_show_form(step_id="init", data_schema=vol.Schema(options))
-
-
 class SwitchBotCloudConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for SwitchBot via API."""
 
     VERSION = 1
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(
-        config_entry: ConfigEntry,
-    ) -> SwitchbotCloudOptionsFlowHandler:
-        """Get the options flow for this handler."""
-        return SwitchbotCloudOptionsFlowHandler()
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -99,7 +52,6 @@ class SwitchBotCloudConfigFlow(ConfigFlow, domain=DOMAIN):
                 )
                 self._abort_if_unique_id_configured()
                 return self.async_create_entry(title=ENTRY_TITLE, data=user_input)
-
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
