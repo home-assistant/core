b674014754deb0891ca49a3b15eb4ba54e59cff3
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-07-09 19:40:50 +0800

    reset lock

--- homeassistant/components/switchbot_cloud/lock.py
+++ homeassistant/components/switchbot_cloud/lock.py
@@ -2,14 +2,14 @@
 
 from typing import Any
 
-from switchbot_api import Device, LockCommands, LockV2Commands, Remote, SwitchBotAPI
+from switchbot_api import LockCommands
 
 from homeassistant.components.lock import LockEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
-from . import SwitchbotCloudData, SwitchBotCoordinator
+from . import SwitchbotCloudData
 from .const import DOMAIN
 from .entity import SwitchBotCloudEntity
 
@@ -21,36 +21,16 @@ async def async_setup_entry(
 ) -> None:
     """Set up SwitchBot Cloud entry."""
     data: SwitchbotCloudData = hass.data[DOMAIN][config.entry_id]
-    night_latch_options: list = config.options.get("SetNightLatchMode", [])
     async_add_entities(
-        SwitchBotCloudLock(data.api, device, coordinator, night_latch_options)
+        SwitchBotCloudLock(data.api, device, coordinator)
         for device, coordinator in data.devices.locks
     )
-
-    config.async_on_unload(config.add_update_listener(_async_update_listener))
-
-
-async def _async_update_listener(hass: HomeAssistant, config: ConfigEntry) -> None:
-    """Deal with entry update."""
-    await hass.data[DOMAIN][config.entry_id].api.close()
-    await hass.config_entries.async_reload(config.entry_id)
 
 
 class SwitchBotCloudLock(SwitchBotCloudEntity, LockEntity):
     """Representation of a SwitchBot lock."""
 
     _attr_name = None
-
-    def __init__(
-        self,
-        api: SwitchBotAPI,
-        device: Device | Remote,
-        coordinator: SwitchBotCoordinator,
-        entity_options: list,
-    ) -> None:
-        """Init SwitchBotCloudLock."""
-        super().__init__(api, device, coordinator)
-        self.entity_options = entity_options
 
     def _set_attributes(self) -> None:
         """Set attributes from coordinator data."""
@@ -59,20 +39,12 @@ class SwitchBotCloudLock(SwitchBotCloudEntity, LockEntity):
 
     async def async_lock(self, **kwargs: Any) -> None:
         """Lock the lock."""
-        entry_id: str | None = self.device_entry.id if self.device_entry else None
-        if entry_id and (entry_id in self.entity_options):
-            await self.send_api_command(LockV2Commands.DEADBOLT)
-        else:
-            await self.send_api_command(LockCommands.LOCK)
+        await self.send_api_command(LockCommands.LOCK)
         self._attr_is_locked = True
         self.async_write_ha_state()
 
     async def async_unlock(self, **kwargs: Any) -> None:
         """Unlock the lock."""
-        entry_id: str | None = self.device_entry.id if self.device_entry else None
-        if entry_id and (entry_id in self.entity_options):
-            await self.send_api_command(LockV2Commands.UNLOCK)
-        else:
-            await self.send_api_command(LockCommands.UNLOCK)
+        await self.send_api_command(LockCommands.UNLOCK)
         self._attr_is_locked = False
         self.async_write_ha_state()
