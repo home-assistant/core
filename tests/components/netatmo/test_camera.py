b9eed52e35608856f63846fbf02c81c446a00463
farkasdi <93778865+farkasdi@users.noreply.github.com>
2025-07-10 09:40:41 +0000

    Netatmo: Adjust test_setup_component_no_devices assertion for updated API calls

--- tests/components/netatmo/test_camera.py
+++ tests/components/netatmo/test_camera.py
@@ -528,7 +528,7 @@ async def test_setup_component_no_devices(
         assert await hass.config_entries.async_setup(config_entry.entry_id)
         await hass.async_block_till_done()
 
-        assert fake_post_hits == 8
+        assert fake_post_hits == 9
 
 
 async def test_camera_image_raises_exception(
