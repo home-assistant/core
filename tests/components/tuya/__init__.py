eb6d4cb857dc81f1a644f728e9d90a15c306bcdf
epenet <6771947+epenet@users.noreply.github.com>
2025-07-10 15:55:22 +0200

    Add tuya snapshot tests for wsdcg and zndb category (#148554)

--- tests/components/tuya/__init__.py
+++ tests/components/tuya/__init__.py
@@ -83,6 +83,14 @@ DEVICE_MOCKS = {
         Platform.CLIMATE,
         Platform.SWITCH,
     ],
+    "wsdcg_temperature_humidity": [
+        # https://github.com/home-assistant/core/issues/102769
+        Platform.SENSOR,
+    ],
+    "zndb_smart_meter": [
+        # https://github.com/home-assistant/core/issues/138372
+        Platform.SENSOR,
+    ],
 }
 
 
