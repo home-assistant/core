82d622b0b3411d52ab2036abe376a86b4bceea16
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-07-11 10:33:49 +0800

    add test code for init

--- homeassistant/components/switchbot_cloud/__init__.py
+++ homeassistant/components/switchbot_cloud/__init__.py
@@ -324,7 +324,6 @@ def _create_handle_webhook(
             return
 
         deviceMac = data["context"]["deviceMac"]
-
         if deviceMac not in coordinators_by_id:
             _LOGGER.error(
                 "Received data for unknown entity from switchbot webhook: %s", data
