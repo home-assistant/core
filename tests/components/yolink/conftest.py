6aa0ad51a0c7a255b21d8527e2497f29232c5715
Matrix <justin@yosmart.com>
2025-07-15 01:45:05 +0000

    fix tests as suggest

--- tests/components/yolink/conftest.py
+++ tests/components/yolink/conftest.py
@@ -2,7 +2,8 @@
 
 from __future__ import annotations
 
-from unittest.mock import AsyncMock, MagicMock
+from collections.abc import Generator
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 from yolink.home_manager import YoLinkHome
@@ -13,6 +14,8 @@ from homeassistant.components.application_credentials import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
+
+from tests.common import MockConfigEntry
 
 CLIENT_ID = "12345"
 CLIENT_SECRET = "6789"
@@ -31,12 +34,52 @@ async def setup_credentials(hass: HomeAssistant) -> None:
 
 
 @pytest.fixture(name="mock_auth_mgr")
-def mock_auth_mgr():
+def mock_auth_mgr() -> MagicMock:
     """Mock the authentication manager."""
     return MagicMock()
 
 
 @pytest.fixture(name="mock_yolink_home")
-def mock_yolink_home():
+def mock_yolink_home() -> AsyncMock:
     """Mock YoLink home instance."""
     return AsyncMock(spec=YoLinkHome)
+
+
+@pytest.fixture
+def mock_config_entry(hass: HomeAssistant) -> MockConfigEntry:
+    """Mock a config entry for YoLink."""
+    config_entry = MockConfigEntry(
+        unique_id=DOMAIN,
+        domain=DOMAIN,
+        title="yolink",
+        data={
+            "auth_implementation": DOMAIN,
+            "token": {
+                "refresh_token": "mock-refresh-token",
+                "access_token": "mock-access-token",
+                "type": "Bearer",
+                "expires_in": 60,
+                "scope": "create",
+            },
+        },
+        options={},
+    )
+    config_entry.add_to_hass(hass)
+    return config_entry
+
+
+@pytest.fixture
+def mock_setup_client(
+    mock_auth_mgr: MagicMock, mock_yolink_home: AsyncMock
+) -> Generator[None]:
+    """Mock the setup of the YoLink client."""
+    with (
+        patch(
+            "homeassistant.components.yolink.api.ConfigEntryAuth",
+            return_value=mock_auth_mgr,
+        ),
+        patch(
+            "homeassistant.components.yolink.YoLinkHome", return_value=mock_yolink_home
+        ),
+    ):
+        yield
--- tests/components/yolink/test_init.py
+++ tests/components/yolink/test_init.py
@@ -1,7 +1,5 @@
 """Tests for the yolink integration."""
 
-from unittest.mock import MagicMock, patch
-
 import pytest
 
 from homeassistant.components.yolink import DOMAIN
@@ -11,57 +9,30 @@ from homeassistant.helpers import device_registry as dr
 from tests.common import MockConfigEntry
 
 
-@pytest.mark.usefixtures("setup_credentials")
+@pytest.mark.usefixtures("setup_credentials", "mock_setup_client")
 async def test_device_remove_devices(
     hass: HomeAssistant,
     device_registry: dr.DeviceRegistry,
-    mock_auth_mgr: MagicMock,
-    mock_yolink_home: MagicMock,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test we can only remove a device that no longer exists."""
-    with (
-        patch(
-            "homeassistant.components.yolink.api.ConfigEntryAuth",
-            return_value=mock_auth_mgr,
-        ),
-        patch(
-            "homeassistant.components.yolink.YoLinkHome",
-            return_value=mock_yolink_home,
-        ),
-    ):
-        mock_config_entry = MockConfigEntry(
-            entry_id="test",
-            domain=DOMAIN,
-            title="yolink",
-            data={
-                "auth_implementation": DOMAIN,
-                "token": {
-                    "refresh_token": "mock-refresh-token",
-                    "access_token": "mock-access-token",
-                    "type": "Bearer",
-                    "expires_in": 60,
-                    "scope": "create",
-                },
-            },
-            options={},
-        )
-        mock_config_entry.add_to_hass(hass)
-        device_registry.async_get_or_create(
-            config_entry_id=mock_config_entry.entry_id,
-            identifiers={(DOMAIN, "stale_device_id")},
-        )
-        device_entries = dr.async_entries_for_config_entry(
-            device_registry, mock_config_entry.entry_id
-        )
 
-        assert len(device_entries) == 1
-        device_entry = device_entries[0]
-        assert device_entry.identifiers == {(DOMAIN, "stale_device_id")}
+    device_registry.async_get_or_create(
+        config_entry_id=mock_config_entry.entry_id,
+        identifiers={(DOMAIN, "stale_device_id")},
+    )
+    device_entries = dr.async_entries_for_config_entry(
+        device_registry, mock_config_entry.entry_id
+    )
 
-        assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
-        await hass.async_block_till_done()
+    assert len(device_entries) == 1
+    device_entry = device_entries[0]
+    assert device_entry.identifiers == {(DOMAIN, "stale_device_id")}
 
-        device_entries = dr.async_entries_for_config_entry(
-            device_registry, mock_config_entry.entry_id
-        )
-        assert len(device_entries) == 0
+    assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    device_entries = dr.async_entries_for_config_entry(
+        device_registry, mock_config_entry.entry_id
+    )
+    assert len(device_entries) == 0
