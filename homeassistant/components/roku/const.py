53c74cdabccb815989954a8e8e6aa4caf727c181
Grayson Cupit <grayson@Graysons-MacBook-Air.local>
2025-07-14 19:23:15 -0500

    update const file

--- homeassistant/components/roku/const.py
+++ homeassistant/components/roku/const.py
@@ -5,6 +5,7 @@ DOMAIN = "roku"
 # Attributes
 ATTR_ARTIST_NAME = "artist_name"
 ATTR_CONTENT_ID = "content_id"
+ATTR_NETWORK_ID = "network_id"
 ATTR_FORMAT = "format"
 ATTR_KEYWORD = "keyword"
 ATTR_MEDIA_TYPE = "media_type"
