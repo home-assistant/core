6edc1ee5a1a454737cbfa3a260288d1f45ff9a7d
Andrew Jackson <andrew@codechimp.org>
2025-07-15 11:36:12 +0100

    Remove logging

--- homeassistant/components/mealie/config_flow.py
+++ homeassistant/components/mealie/config_flow.py
@@ -71,9 +71,6 @@ class MealieConfigFlow(ConfigFlow, domain=DOMAIN):
     ) -> ConfigFlowResult:
         """Handle a flow initialized by the user."""
 
-        # TODO - Remove these after testing
-        LOGGER.warning("Here")
-
         errors: dict[str, str] = {}
         if user_input:
             self.host = user_input[CONF_HOST]
@@ -88,6 +85,7 @@ class MealieConfigFlow(ConfigFlow, domain=DOMAIN):
                     title="Mealie",
                     data=user_input,
                 )
+
         return self.async_show_form(
             step_id="user",
             data_schema=USER_SCHEMA,
