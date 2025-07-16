194c54844e8db9a6273927a24e3666284a33ec1d
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-07-16 09:05:07 +0200

    Refactor thermostat modes to use IntEnum
    Replaces the custom int class for thermostat modes with Python's IntEnum for improved clarity and maintainability in the niko_home_control integration.

--- homeassistant/components/niko_home_control/const.py
+++ homeassistant/components/niko_home_control/const.py
@@ -1,21 +1,19 @@
 """Constants for niko_home_control integration."""
 
-import logging
+from enum import IntEnum
 
 DOMAIN = "niko_home_control"
-_LOGGER = logging.getLogger(__name__)
-
 
 NIKO_HOME_CONTROL_THERMOSTAT_MODES_MAP = {
     "off": 3,
     "cool": 4,
     "auto": 5,
 }
-
-
-class NIKO_HOME_CONTROL_THERMOSTAT_MODES(int):
-    """Niko Home Control thermostat modes."""
-
-    OFF = 3
-    COOL = 4
-    AUTO = 5
+NIKO_HOME_CONTROL_THERMOSTAT_MODES = IntEnum(
+    "NIKO_HOME_CONTROL_THERMOSTAT_MODES",
+    {
+        "OFF": 3,
+        "COOL": 4,
+        "AUTO": 5,
+    },
+)
