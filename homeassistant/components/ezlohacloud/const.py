ec77241b7b65cf491157be63de99dc40e14c6328
fahad <fahad@ezlo.com>
2025-07-16 17:06:59 +0000

    backup running integration

--- homeassistant/components/ezlohacloud/const.py
+++ homeassistant/components/ezlohacloud/const.py
@@ -5,4 +5,8 @@ DOMAIN = "ezlohacloud"
 STORAGE_KEY = "ezlo_user_data"
 STORAGE_VERSION = 1
 
-EZLO_API_URI = "http://testtunnelezlo.info:8888"
+EZLO_API_URI = "https://haapi-dev.ezlo.com"
+
+# EZLO_API_URI = "http://testtunnelezlo.info:8888"
+
+# https://haapi-dev.ezlo.com
