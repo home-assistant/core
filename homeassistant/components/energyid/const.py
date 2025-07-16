17cac90a3462e49f8350dc6f2fde412d26b2b3db
Oscar Swyns <oscar.swyns@energieid.be>
2025-07-16 13:36:49 +0000

    refactor(const): reorganize constants and remove unused entries

--- homeassistant/components/energyid/const.py
+++ homeassistant/components/energyid/const.py
@@ -4,21 +4,21 @@ from typing import Final
 
 DOMAIN: Final = "energyid"
 
+# --- Config Flow and Entry Data ---
 CONF_PROVISIONING_KEY: Final = "provisioning_key"
 CONF_PROVISIONING_SECRET: Final = "provisioning_secret"
 CONF_DEVICE_ID: Final = "device_id"
 CONF_DEVICE_NAME: Final = "device_name"
-CONF_RECORD_NUMBER: Final = "record_number"
-CONF_RECORD_NAME: Final = "record_name"
+
+# --- Subentry (Mapping) Data ---
 CONF_HA_ENTITY_ID: Final = "ha_entity_id"
 CONF_ENERGYID_KEY: Final = "energyid_key"
 
+# --- Data stored in hass.data ---
 DATA_CLIENT: Final = "client"
-DATA_LISTENERS: Final = "listeners"
-DATA_MAPPINGS: Final = "mappings"
 
+# --- Signals for dispatcher ---
 SIGNAL_CONFIG_ENTRY_CHANGED = f"{DOMAIN}_config_entry_changed"
 
+# --- Defaults ---
 DEFAULT_UPLOAD_INTERVAL_SECONDS: Final = 60
-
-LISTENER_TYPE_STATE = "state_change"
