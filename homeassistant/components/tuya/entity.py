23af31797e31dd93ba4ba63774b05b018be24ed4
FredrikM97 <fredrik9779@gmail.com>
2025-07-17 16:05:24 +0000

    fix undo changes

--- homeassistant/components/tuya/entity.py
+++ homeassistant/components/tuya/entity.py
@@ -14,8 +14,7 @@ from .const import DOMAIN, LOGGER, TUYA_HA_SIGNAL_UPDATE_ENTITY, DPCode, DPType
 from .models import EnumTypeData, IntegerTypeData
 
 _DPTYPE_MAPPING: dict[str, DPType] = {
-    "Bitmap": DPType.RAW,
-    "bitmap": DPType.RAW,
+    "bitmap": DPType.BITMAP,
     "bool": DPType.BOOLEAN,
     "enum": DPType.ENUM,
     "json": DPType.JSON,
