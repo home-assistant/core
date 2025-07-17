1dd3ca7336bf72f546bc3fe868fa7ece3dfa56fd
Joostlek <joostlek@outlook.com>
2025-07-17 21:35:20 +0200

    Fix flow

--- homeassistant/components/waqi/__init__.py
+++ homeassistant/components/waqi/__init__.py
@@ -48,9 +48,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         await coordinator.async_config_entry_first_refresh()
         hass.data[DOMAIN][entry.entry_id][subentry.subentry_id] = coordinator
 
+    entry.async_on_unload(entry.add_update_listener(async_update_options))
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
+
+
+async def async_update_options(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Update options."""
+    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
