afc7ade0b64fd5c1233fe806c15296e0ebda6e21
Josef Zweck <josef@zweck.dev>
2025-07-17 10:52:21 +0200

    Update homeassistant/components/huum/__init__.py

--- homeassistant/components/huum/__init__.py
+++ homeassistant/components/huum/__init__.py
@@ -23,6 +23,6 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: HuumConfigEntry) 
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, config_entry: HuumConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
