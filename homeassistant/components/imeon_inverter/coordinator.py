26443d0ae8f131461d7c252d8d0e2c5a4ce2d9c7
TheBushBoy <theodavid@icloud.com>
2025-07-17 12:41:48 +0000

    Use previous format of the api

--- homeassistant/components/imeon_inverter/coordinator.py
+++ homeassistant/components/imeon_inverter/coordinator.py
@@ -88,7 +88,10 @@ class InverterCoordinator(DataUpdateCoordinator[dict[str, str | float | int]]):
 
         # Store data
         for key, val in self._api.storage.items():
-            for sub_key, sub_val in val.items():
-                data[f"{key}_{sub_key}"] = sub_val
+            if key == "timeline":
+                data[key] = val
+            else:
+                for sub_key, sub_val in val.items():
+                    data[f"{key}_{sub_key}"] = sub_val
 
         return data
