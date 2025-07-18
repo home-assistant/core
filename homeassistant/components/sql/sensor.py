cbae7d196b68f5a909ca18665a95d129a43f102f
G Johansson <goran.johansson@shiftit.se>
2025-07-18 08:54:34 +0000

    Fix sql

--- homeassistant/components/sql/sensor.py
+++ homeassistant/components/sql/sensor.py
@@ -404,11 +404,10 @@ class SQLSensor(ManualTriggerSensorEntity):
                 _value = self._template.async_render_as_value_template(
                     self.entity_id, variables, None
                 )
-                self._set_native_value_with_possible_timestamp(value)
+                self._set_native_value_with_possible_timestamp(_value)
+                self._process_manual_data(variables)
         else:
             self._attr_native_value = data
-
-        self._process_manual_data(variables)
 
         if data is None:
             _LOGGER.warning("%s returned no results", self._query)
