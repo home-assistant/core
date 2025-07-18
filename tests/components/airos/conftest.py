fac61aa0efa88fd355754ff314daa309276b5d68
Tom Scholten <git@scholten.nu>
2025-07-18 13:11:15 +0200

    Add sensor testing for codecov patch requirement

--- tests/components/airos/conftest.py
+++ tests/components/airos/conftest.py
@@ -1,9 +1,18 @@
 """Common fixtures for the Ubiquiti airOS tests."""
 
 from collections.abc import Generator
+import json
 from unittest.mock import AsyncMock, patch
 
 import pytest
+
+from tests.common import load_fixture
+
+
+@pytest.fixture
+def ap_fixture():
+    """Load fixture data for AP mode."""
+    return json.loads(load_fixture("airos/ap-ptp.json"))
 
 
 @pytest.fixture
