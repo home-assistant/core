de79c40cf3206a62fa01d9cba2f5d016b4ac844b
jvmahon <jvm33@columbia.edu>
2025-07-19 20:36:01 +0000

    Updated snapshot and testing for lock component

--- tests/components/matter/test_lock.py
+++ tests/components/matter/test_lock.py
@@ -76,14 +76,14 @@ async def test_lock(
     await hass.async_block_till_done()
     state = hass.states.get("lock.mock_door_lock")
     assert state
-    assert state.state == LockState.LOCKING
+    assert state.state == LockState.JAMMED
 
     set_node_attribute(matter_node, 1, 257, 0, 0)
     await trigger_subscription_callback(hass, matter_client)
 
     state = hass.states.get("lock.mock_door_lock")
     assert state
-    assert state.state == LockState.UNLOCKED
+    assert state.state == LockState.JAMMED
 
     set_node_attribute(matter_node, 1, 257, 0, 2)
     await trigger_subscription_callback(hass, matter_client)
@@ -197,15 +197,15 @@ async def test_lock_with_unbolt(
         blocking=True,
     )
     assert matter_client.send_device_command.call_count == 1
-    # unlock should unbolt on a lock with unbolt feature
+    # unlock should only unlock, even on a lock with unbolt feature
     assert matter_client.send_device_command.call_args == call(
         node_id=matter_node.node_id,
         endpoint_id=1,
-        command=clusters.DoorLock.Commands.UnboltDoor(),
+        command=clusters.DoorLock.Commands.UnlockDoor(),
         timed_request_timeout_ms=1000,
     )
     matter_client.send_device_command.reset_mock()
-    # test open / unlatch
+    # open should unbolt on a lock with unbolt / open feature.
     await hass.services.async_call(
         "lock",
         "open",
@@ -218,7 +218,7 @@ async def test_lock_with_unbolt(
     assert matter_client.send_device_command.call_args == call(
         node_id=matter_node.node_id,
         endpoint_id=1,
-        command=clusters.DoorLock.Commands.UnlockDoor(),
+        command=clusters.DoorLock.Commands.UnboltDoor(),
         timed_request_timeout_ms=1000,
     )
 
@@ -232,7 +232,7 @@ async def test_lock_with_unbolt(
 
     state = hass.states.get("lock.mock_door_lock")
     assert state
-    assert state.state == LockState.UNLOCKED
+    assert state.state == LockState.JAMMED
 
     set_node_attribute(matter_node, 1, 257, 0, 3)
     await trigger_subscription_callback(hass, matter_client)
