eaf6637b242e8a4837c6db9877d5d27730d3efca
Tsvi Mostovicz <ttmost@gmail.com>
2025-07-21 10:42:17 +0000

    Create using copilot

--- homeassistant/components/jewish_calendar/const.py
+++ homeassistant/components/jewish_calendar/const.py
@@ -10,11 +10,34 @@ CONF_ALTITUDE = "altitude"  # The name used by the hdate library for elevation
 CONF_DIASPORA = "diaspora"
 CONF_CANDLE_LIGHT_MINUTES = "candle_lighting_minutes_before_sunset"
 CONF_HAVDALAH_OFFSET_MINUTES = "havdalah_minutes_after_sunset"
+CONF_CALENDAR_EVENTS = "calendar_events"
 
 DEFAULT_NAME = "Jewish Calendar"
 DEFAULT_CANDLE_LIGHT = 18
 DEFAULT_DIASPORA = False
 DEFAULT_HAVDALAH_OFFSET_MINUTES = 0
 DEFAULT_LANGUAGE = "en"
+DEFAULT_CALENDAR_EVENTS = [
+    "date",
+    "holiday",
+    "weekly_portion",
+    "omer_count",
+    "daf_yomi",
+]
 
 SERVICE_COUNT_OMER = "count_omer"
+
+# Available calendar event types
+CALENDAR_EVENT_TYPES = [
+    "date",
+    "holiday",
+    "weekly_portion",
+    "omer_count",
+    "daf_yomi",
+    "candle_lighting",
+    "havdalah",
+    "fast_day",
+    "rosh_chodesh",
+    "minor_fast",
+    "modern_holiday",
+]
