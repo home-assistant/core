4937958dc1d0921f2c96cb5600319a5cc40ba0f9
Sab44 <64696149+Sab44@users.noreply.github.com>
2025-07-21 10:59:10 +0200

    add underscores to component name

--- homeassistant/components/libre_hardware_monitor/const.py
+++ homeassistant/components/libre_hardware_monitor/const.py
@@ -1,6 +1,6 @@
 """Constants for the LibreHardwareMonitor integration."""
 
-DOMAIN = "librehardwaremonitor"
+DOMAIN = "libre_hardware_monitor"
 DEFAULT_HOST = "localhost"
 DEFAULT_PORT = 8085
 DEFAULT_SCAN_INTERVAL = 10
