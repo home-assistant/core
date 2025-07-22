21e7360c47ba61f4ea522e430989a4cd5402114e
RaHehl <7577984+RaHehl@users.noreply.github.com>
2025-07-22 20:54:04 +0000

    Improve API key creation logic in UniFi Protect setup: include location name and ensure key is set correctly

--- homeassistant/components/unifiprotect/__init__.py
+++ homeassistant/components/unifiprotect/__init__.py
@@ -97,14 +97,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: UFPConfigEntry) -> bool:
     # Check if API key is missing
     if not protect.is_api_key_set() and auth_user and nvr_info.can_write(auth_user):
         try:
-            new_api_key = await protect.create_api_key(name="Home Assistant")
-            if new_api_key:
-                protect.set_api_key(new_api_key)
-                hass.config_entries.async_update_entry(
-                    entry, data={**entry.data, CONF_API_KEY: new_api_key}
-                )
+            new_api_key = await protect.create_api_key(
+                name=f"Home Assistant {hass.config.location_name}"
+            )
         except NotAuthorized as err:
             _LOGGER.error("Failed to create API key: %s", err)
+        else:
+            protect.set_api_key(new_api_key)
+            hass.config_entries.async_update_entry(
+                entry, data={**entry.data, CONF_API_KEY: new_api_key}
+            )
 
     if not protect.is_api_key_set():
         hass.async_create_task(
