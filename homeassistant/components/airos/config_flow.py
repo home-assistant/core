7dc390618ef1c355413a2a8c524464d3bb59883a
Tom Scholten <git@scholten.nu>
2025-07-23 15:48:46 +0200

    Local ruff miss

--- homeassistant/components/airos/config_flow.py
+++ homeassistant/components/airos/config_flow.py
@@ -20,7 +20,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
-from .coordinator import AirOS, AirOSData
+from .coordinator import AirOS
 
 _LOGGER = logging.getLogger(__name__)
 
