46efd21d58d6e7f53b41731875d975a8c9949544
jvmahon <jvm33@columbia.edu>
2025-07-27 10:56:29 +0000

    Add event support and related attributes

--- homeassistant/components/matter/lock.py
+++ homeassistant/components/matter/lock.py
@@ -3,9 +3,11 @@
 from __future__ import annotations
 
 import asyncio
+from collections.abc import Mapping
 from typing import Any
 
 from chip.clusters import Objects as clusters
+from matter_server.common.models import EventType, MatterNodeEvent
 
 from homeassistant.components.lock import (
     LockEntity,
@@ -21,6 +23,24 @@ from .const import LOGGER
 from .entity import MatterEntity
 from .helpers import get_matter
 from .models import MatterDiscoverySchema
+
+ATTR_USER_INDEX: str = "user_index"
+
+DOOR_LOCK_OPERATION_SOURCE = {
+    # mapping from operation source id's to textual representation
+    0: "Unspecified",
+    1: "Manual",  # [Optional]
+    2: "Proprietary Remote",  # [Optional]
+    3: "Keypad",  # [Optional]
+    4: "Auto",  # [Optional]
+    5: "Button",  # [Optional]
+    6: "Schedule",  # [HDSCH]
+    7: "Remote",  # [M]
+    8: "RFID",  # [RID]
+    9: "Biometric",  # [USR]
+    10: "Aliro",  # [Aliro]
+}
+
 
 DoorLockFeature = clusters.DoorLock.Bitmaps.Feature
 
@@ -41,6 +61,72 @@ class MatterLock(MatterEntity, LockEntity):
     _feature_map: int | None = None
     _optimistic_timer: asyncio.TimerHandle | None = None
     _platform_translation_key = "lock"
+    _attr_user_index: int | None = None
+    _attr_changed_by = "Unknown"
+
+    async def async_added_to_hass(self) -> None:
+        """Subscribe to events."""
+        await super().async_added_to_hass()
+        # subscribe to NodeEvent events
+        self._unsubscribes.append(
+            self.matter_client.subscribe_events(
+                callback=self._on_matter_node_event,
+                event_filter=EventType.NODE_EVENT,
+                node_filter=self._endpoint.node.node_id,
+            )
+        )
+
+    @callback
+    def _on_matter_node_event(
+        self,
+        event: EventType,
+        nodeEvent: MatterNodeEvent,
+    ) -> None:
+        """Call on NodeEvent."""
+        if (nodeEvent.endpoint_id != self._endpoint.endpoint_id) or (
+            nodeEvent.cluster_id != clusters.DoorLock.id
+        ):
+            return
+
+        LOGGER.debug(
+            "Received _on_matter_node_event: event type %s, event id %s for %s with data %s",
+            event,
+            nodeEvent.event_id,
+            self.entity_id,
+            nodeEvent.data,
+        )
+
+        # handle the DoorLock events
+        nodeEventData: dict[str, int] = nodeEvent.data or {}
+        match nodeEvent.event_id:
+            case (
+                clusters.DoorLock.Events.LockOperation.event_id
+            ):  # Lock cluster event 2
+                # update the changed_by attribute to indicate lock operation source
+                operation_source: int = nodeEventData.get("operationSource", -1)
+                self._attr_changed_by = DOOR_LOCK_OPERATION_SOURCE.get(
+                    operation_source, "Unknown"
+                )
+                # update the user index attribute to indicate which user performed the operation
+                self._attr_user_index = nodeEventData.get("userIndex")
+                self.async_write_ha_state()
+            case (
+                clusters.DoorLock.Events.LockOperationError.event_id
+            ):  # Lock cluster event 3
+                # if an operation error occurs, clear the optimistic state
+                self._reset_optimistic_state(write_state=True)
+
+    @property
+    def user_index(self) -> int | None:
+        """Return the user index for the lock."""
+        return self._attr_user_index
+
+    @property
+    def extra_state_attributes(self) -> Mapping[str, Any] | None:
+        """Return the additional user_index state attribute of the lock."""
+        attrs = dict((super().extra_state_attributes or {}).items())
+        attrs[ATTR_USER_INDEX] = self._attr_user_index
+        return attrs
 
     @property
     def code_format(self) -> str | None:
