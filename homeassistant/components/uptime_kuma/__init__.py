7fb347306044b242a57dad2f85bc4625738b3434
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-07-27 00:43:58 +0000

    changes

--- homeassistant/components/uptime_kuma/__init__.py
+++ homeassistant/components/uptime_kuma/__init__.py
@@ -8,7 +8,6 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.typing import ConfigType
 from homeassistant.util.hass_dict import HassKey
 
 from .const import DOMAIN
@@ -24,21 +23,16 @@ CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 UPTIME_KUMA_KEY: HassKey[UptimeKumaSoftwareUpdateCoordinator] = HassKey(DOMAIN)
 
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up Uptime Kuma."""
-
-    session = async_get_clientsession(hass)
-    update_checker = UpdateChecker(session)
-
-    update_coordinator = UptimeKumaSoftwareUpdateCoordinator(hass, update_checker)
-    await update_coordinator.async_request_refresh()
-
-    hass.data[UPTIME_KUMA_KEY] = update_coordinator
-    return True
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: UptimeKumaConfigEntry) -> bool:
     """Set up Uptime Kuma from a config entry."""
+    if UPTIME_KUMA_KEY not in hass.data:
+        session = async_get_clientsession(hass)
+        update_checker = UpdateChecker(session)
+
+        update_coordinator = UptimeKumaSoftwareUpdateCoordinator(hass, update_checker)
+        await update_coordinator.async_request_refresh()
+
+        hass.data[UPTIME_KUMA_KEY] = update_coordinator
 
     coordinator = UptimeKumaDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
@@ -51,4 +45,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: UptimeKumaConfigEntry) -
 
 async def async_unload_entry(hass: HomeAssistant, entry: UptimeKumaConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
+
+    if not hass.config_entries.async_loaded_entries(DOMAIN):
+        await hass.data[UPTIME_KUMA_KEY].async_shutdown()
+        hass.data.pop(UPTIME_KUMA_KEY)
+    return unload_ok
