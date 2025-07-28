f1952668961d5493a4ab4ec27dbafa9947bb0dfc
epenet <6771947+epenet@users.noreply.github.com>
2025-07-28 17:42:18 +0200

    Add service tests to Tuya select platform (#149156)
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- tests/components/tuya/test_select.py
+++ tests/components/tuya/test_select.py
@@ -8,9 +8,14 @@ import pytest
 from syrupy.assertion import SnapshotAssertion
 from tuya_sharing import CustomerDevice
 
+from homeassistant.components.select import (
+    DOMAIN as SELECT_DOMAIN,
+    SERVICE_SELECT_OPTION,
+)
 from homeassistant.components.tuya import ManagerCompat
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ServiceValidationError
 from homeassistant.helpers import entity_registry as er
 
 from . import DEVICE_MOCKS, initialize_entry
@@ -53,3 +58,62 @@ async def test_platform_setup_no_discovery(
     assert not er.async_entries_for_config_entry(
         entity_registry, mock_config_entry.entry_id
     )
+
+
+@pytest.mark.parametrize(
+    "mock_device_code",
+    ["cl_am43_corded_motor_zigbee_cover"],
+)
+async def test_select_option(
+    hass: HomeAssistant,
+    mock_manager: ManagerCompat,
+    mock_config_entry: MockConfigEntry,
+    mock_device: CustomerDevice,
+) -> None:
+    """Test fan mode with windspeed."""
+    entity_id = "select.kitchen_blinds_motor_mode"
+    await initialize_entry(hass, mock_manager, mock_config_entry, mock_device)
+
+    state = hass.states.get(entity_id)
+    assert state is not None, f"{entity_id} does not exist"
+    await hass.services.async_call(
+        SELECT_DOMAIN,
+        SERVICE_SELECT_OPTION,
+        {
+            "entity_id": entity_id,
+            "option": "forward",
+        },
+        blocking=True,
+    )
+    mock_manager.send_commands.assert_called_once_with(
+        mock_device.id, [{"code": "control_back_mode", "value": "forward"}]
+    )
+
+
+@pytest.mark.parametrize(
+    "mock_device_code",
+    ["cl_am43_corded_motor_zigbee_cover"],
+)
+async def test_select_invalid_option(
+    hass: HomeAssistant,
+    mock_manager: ManagerCompat,
+    mock_config_entry: MockConfigEntry,
+    mock_device: CustomerDevice,
+) -> None:
+    """Test fan mode with windspeed."""
+    entity_id = "select.kitchen_blinds_motor_mode"
+    await initialize_entry(hass, mock_manager, mock_config_entry, mock_device)
+
+    state = hass.states.get(entity_id)
+    assert state is not None, f"{entity_id} does not exist"
+    with pytest.raises(ServiceValidationError) as exc:
+        await hass.services.async_call(
+            SELECT_DOMAIN,
+            SERVICE_SELECT_OPTION,
+            {
+                "entity_id": entity_id,
+                "option": "hello",
+            },
+            blocking=True,
+        )
+    assert exc.value.translation_key == "not_valid_option"
