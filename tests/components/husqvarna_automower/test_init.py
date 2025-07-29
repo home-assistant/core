09e584fb462801bae1976ef3ebe50acc5d479be7
Thomas <thomas.protzner@gmail.com>
2025-07-29 15:47:46 +0000

    cleanup

--- tests/components/husqvarna_automower/test_init.py
+++ tests/components/husqvarna_automower/test_init.py
@@ -14,8 +14,7 @@ from aioautomower.exceptions import (
     HusqvarnaTimeoutError,
     HusqvarnaWSServerHandshakeError,
 )
-from aioautomower.model import Calendar, MessageData, MowerAttributes, WorkArea
-from aioautomower.model.model_message import MessageAttributes
+from aioautomower.model import Calendar, MowerAttributes, WorkArea
 from freezegun.api import FrozenDateTimeFactory
 import pytest
 from syrupy.assertion import SnapshotAssertion
@@ -298,7 +297,6 @@ async def test_coordinator_automatic_registry_cleanup(
     device_registry: dr.DeviceRegistry,
     entity_registry: er.EntityRegistry,
     values: dict[str, MowerAttributes],
-    messages: MessageData,
     freezer: FrozenDateTimeFactory,
 ) -> None:
     """Test automatic registry cleanup."""
@@ -314,8 +312,6 @@ async def test_coordinator_automatic_registry_cleanup(
         dr.async_entries_for_config_entry(device_registry, entry.entry_id)
     )
     # Remove mower 2 and check if it worked
-    mower2_messages = messages.pop("1234")
-    mock_automower_client.async_get_messages.return_value = mower2_messages
     values_copy = deepcopy(values)
     mower2 = values_copy.pop("1234")
     mock_automower_client.get_status.return_value = values_copy
@@ -332,8 +328,6 @@ async def test_coordinator_automatic_registry_cleanup(
         == current_devices - 1
     )
     # Add mower 2 and check if it worked
-    messages["1234"] = mower2_messages
-    mock_automower_client.async_get_messages.return_value = messages
     values_copy = deepcopy(values)
     values_copy["1234"] = mower2
     mock_automower_client.get_status.return_value = values_copy
@@ -350,8 +344,6 @@ async def test_coordinator_automatic_registry_cleanup(
     )
 
     # Remove mower 1 and check if it worked
-    mower1_messages = messages.pop(TEST_MOWER_ID)
-    mock_automower_client.async_get_messages.return_value = messages
     values_copy = deepcopy(values)
     mower1 = values_copy.pop(TEST_MOWER_ID)
     mock_automower_client.get_status.return_value = values_copy
@@ -368,32 +360,6 @@ async def test_coordinator_automatic_registry_cleanup(
         == current_devices - 1
     )
     # Add mower 1 and check if it worked
-    messages[TEST_MOWER_ID] = mower1_messages
-
-    def get_message_side_effect(mower_id: str) -> MessageData:
-        return messages.get(
-            mower_id,
-            MessageData(
-                type="messages",
-                id=mower_id,
-                attributes=MessageAttributes(
-                    messages=[
-                        {
-                            "time": 1751146587,
-                            "code": 2,
-                            "severity": "ERROR",
-                            "latitude": 49,
-                            "longitude": 10,
-                        },
-                    ]
-                ),
-            ),
-        )
-
-    mock_automower_client.async_get_messages = AsyncMock(
-        side_effect=get_message_side_effect
-    )
-
     values_copy = deepcopy(values)
     values_copy[TEST_MOWER_ID] = mower1
     mock_automower_client.get_status.return_value = values_copy
