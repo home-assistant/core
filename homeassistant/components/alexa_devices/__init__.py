790897239e4096442a43621a093cd81f85e82907
Simone Chemelli <simone.chemelli@gmail.com>
2025-07-30 11:22:57 +0000

    ruff

--- homeassistant/components/alexa_devices/__init__.py
+++ homeassistant/components/alexa_devices/__init__.py
@@ -2,7 +2,7 @@
 
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_validation as cv, aiohttp_client
+from homeassistant.helpers import aiohttp_client, config_validation as cv
 from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
