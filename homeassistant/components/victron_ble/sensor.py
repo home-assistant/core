633df646be7077007771ee8b2abf1986fd3e61a3
Raj Laud <raj.laud@gmail.com>
2025-07-30 14:57:53 +0000

    Gracefully skip sensors that we don't have descriptions for

--- homeassistant/components/victron_ble/sensor.py
+++ homeassistant/components/victron_ble/sensor.py
@@ -396,14 +396,17 @@ def sensor_update_to_bluetooth_data_update(
                 device_key.key
             ]
             for device_key in sensor_update.entity_descriptions
+            if device_key.key in SENSOR_DESCRIPTIONS
         },
         entity_data={
             _device_key_to_bluetooth_entity_key(device_key): sensor_values.native_value
             for device_key, sensor_values in sensor_update.entity_values.items()
+            if device_key.key in SENSOR_DESCRIPTIONS
         },
         entity_names={
             _device_key_to_bluetooth_entity_key(device_key): sensor_values.name
             for device_key, sensor_values in sensor_update.entity_values.items()
+            if device_key.key in SENSOR_DESCRIPTIONS
         },
     )
 
