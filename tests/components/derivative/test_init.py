1dada8c10338dea96555d46e3e50c2772f90c346
jbouwh <jan@jbsoft.nl>
2025-07-31 20:32:57 +0000

    Fix migration tests

--- tests/components/derivative/test_init.py
+++ tests/components/derivative/test_init.py
@@ -519,7 +519,7 @@ async def test_migration_1_1(hass: HomeAssistant, unit_prefix, expect_prefix) ->
     assert config_entry.options.get("unit_prefix") == expect_prefix
 
     assert config_entry.version == 1
-    assert config_entry.minor_version == 3
+    assert config_entry.minor_version == 4
 
 
 async def test_migration_1_2(
@@ -570,7 +570,7 @@ async def test_migration_1_2(
     assert derivative_entity_entry.device_id == sensor_entity_entry.device_id
 
     assert derivative_config_entry.version == 1
-    assert derivative_config_entry.minor_version == 3
+    assert derivative_config_entry.minor_version == 4
 
 
 async def test_migration_from_future_version(
