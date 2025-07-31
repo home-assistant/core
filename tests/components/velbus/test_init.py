6b2ccc2310718e077fc1e3b286b0845a6b80effe
Erik <erik@montnemery.com>
2025-07-31 13:59:39 +0200

    Fix flaky velbus test

--- tests/components/velbus/test_init.py
+++ tests/components/velbus/test_init.py
@@ -176,7 +176,7 @@ async def test_device_registry(
     device_entries = dr.async_entries_for_config_entry(
         device_registry, config_entry.entry_id
     )
-    assert device_entries == snapshot
+    assert sorted(device_entries, key=lambda x: list(x.identifiers)[0][1]) == snapshot
 
     device_parent = device_registry.async_get_device(identifiers={(DOMAIN, "88")})
     assert device_parent.via_device_id is None
