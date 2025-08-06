6990f3fc16271d3ae522fb3d34cff0993ecae16e
J. Nick Koston <nick@koston.org>
2025-08-06 13:31:09 -1000

    Bump inkbird-ble to 1.1.0 to add support for IAM-T2
    changelog: https://github.com/Bluetooth-Devices/inkbird-ble/compare/v0.16.2...v1.1.0

--- homeassistant/generated/bluetooth.py
+++ homeassistant/generated/bluetooth.py
@@ -389,12 +389,26 @@ BLUETOOTH: Final[list[dict[str, bool | str | int | list[int]]]] = [
     {
         "connectable": True,
         "domain": "inkbird",
+        "local_name": "Ink@IAM-T2",
+    },
+    {
+        "connectable": True,
+        "domain": "inkbird",
         "manufacturer_data_start": [
             65,
             67,
             45,
         ],
         "manufacturer_id": 12628,
+    },
+    {
+        "connectable": True,
+        "domain": "inkbird",
+        "manufacturer_data_start": [
+            0,
+            62,
+        ],
+        "manufacturer_id": 12884,
     },
     {
         "connectable": True,
