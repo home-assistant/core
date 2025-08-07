2baa9d087d76dfd3f00a5fbf8d44e233e7193649
David Bugl <bugl@lunatone.com>
2025-08-07 07:02:09 +0000

    Update unit tests

--- tests/components/lunatone/ test_init.py
+++ tests/components/lunatone/ test_init.py
@@ -30,32 +30,72 @@ async def test_load_unload_config_entry(
     assert config_entry.state is ConfigEntryState.NOT_LOADED
 
 
-async def test_config_entry_not_ready(
+async def test_config_entry_not_ready_cause_of_info_object(
     hass: HomeAssistant,
     mock_lunatone_info: AsyncMock,
-    setup_integration: MockConfigEntry,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test the Lunatone configuration entry not ready."""
     mock_lunatone_info.async_update.side_effect = aiohttp.ClientConnectionError()
 
-    config_entry = setup_integration
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
 
-    assert mock_lunatone_info.async_update.call_count == 1
-    assert config_entry.state is ConfigEntryState.SETUP_RETRY
+    mock_lunatone_info.async_update.assert_called_once()
+    assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
 
 
-async def test_config_entry_not_ready_no_data(
+async def test_config_entry_not_ready_cause_of_devices_object(
     hass: HomeAssistant,
     mock_lunatone_info: AsyncMock,
-    setup_integration: MockConfigEntry,
+    mock_lunatone_devices: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test the Lunatone configuration entry not ready."""
+    mock_lunatone_devices.async_update.side_effect = aiohttp.ClientConnectionError()
+
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    mock_lunatone_info.async_update.assert_called_once()
+    mock_lunatone_devices.async_update.assert_called_once()
+    assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
+
+
+async def test_config_entry_not_ready_no_info_data(
+    hass: HomeAssistant,
+    mock_lunatone_info: AsyncMock,
+    mock_config_entry: MockConfigEntry,
 ) -> None:
     """Test the Lunatone configuration entry not ready."""
     mock_lunatone_info.data = None
 
-    config_entry = setup_integration
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
 
-    mock_lunatone_info.async_update.assert_called()
-    assert config_entry.state is ConfigEntryState.SETUP_RETRY
+    mock_lunatone_info.async_update.assert_called_once()
+    assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
+
+
+async def test_config_entry_not_ready_no_devices_data(
+    hass: HomeAssistant,
+    mock_lunatone_info: AsyncMock,
+    mock_lunatone_devices: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test the Lunatone configuration entry not ready."""
+    mock_lunatone_devices.data = None
+
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    mock_lunatone_info.async_update.assert_called_once()
+    mock_lunatone_devices.async_update.assert_called_once()
+    assert mock_config_entry.state is ConfigEntryState.SETUP_RETRY
 
 
 async def test_device_info(
