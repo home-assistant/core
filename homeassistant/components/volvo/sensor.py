21af8803a57e7f5258848a90fa32b185ce643307
Thomas D <11554546+thomasddn@users.noreply.github.com>
2025-08-08 13:41:24 +0000

    Remove electricRange

--- homeassistant/components/volvo/sensor.py
+++ homeassistant/components/volvo/sensor.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from dataclasses import dataclass, replace
+from dataclasses import dataclass
 import logging
 from typing import Any, cast
 
@@ -47,7 +47,6 @@ _LOGGER = logging.getLogger(__name__)
 class VolvoSensorDescription(VolvoEntityDescription, SensorEntityDescription):
     """Describes a Volvo sensor entity."""
 
-    source_fields: list[str] | None = None
     value_fn: Callable[[VolvoCarsValue], Any] | None = None
 
 
@@ -235,11 +234,15 @@ _DESCRIPTIONS: tuple[VolvoSensorDescription, ...] = (
             "none",
         ],
     ),
-    # statistics & energy state endpoint
+    # statistics endpoint
+    # We're not using `electricRange` from the energy state endpoint because
+    # the official app seems to use `distanceToEmptyBattery`.
+    # In issue #150213, a user described to behavior as follows:
+    # - For a `distanceToEmptyBattery` of 250km, the `electricRange` was 150mi
+    # - For a `distanceToEmptyBattery` of 260km, the `electricRange` was 160mi
     VolvoSensorDescription(
         key="distance_to_empty_battery",
-        api_field="",
-        source_fields=["distanceToEmptyBattery", "electricRange"],
+        api_field="distanceToEmptyBattery",
         native_unit_of_measurement=UnitOfLength.KILOMETERS,
         device_class=SensorDeviceClass.DISTANCE,
         state_class=SensorStateClass.MEASUREMENT,
@@ -357,12 +360,7 @@ async def async_setup_entry(
             if description.key in added_keys:
                 continue
 
-            if description.source_fields:
-                for field in description.source_fields:
-                    if field in coordinator.data:
-                        description = replace(description, api_field=field)
-                        _add_entity(coordinator, description)
-            elif description.api_field in coordinator.data:
+            if description.api_field in coordinator.data:
                 _add_entity(coordinator, description)
 
     async_add_entities(entities)
