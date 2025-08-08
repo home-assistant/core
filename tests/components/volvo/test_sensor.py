a1731cd21029dc3d75bc20e26f9eef860d7c6013
Thomas D <11554546+thomasddn@users.noreply.github.com>
2025-08-08 21:28:29 +0200

    Volvo: fix distance to empty battery (#150278)

--- tests/components/volvo/test_sensor.py
+++ tests/components/volvo/test_sensor.py
@@ -30,3 +30,19 @@ async def test_sensor(
         assert await setup_integration()
 
     await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
+
+
+@pytest.mark.parametrize(
+    "full_model",
+    ["xc40_electric_2024"],
+)
+async def test_distance_to_empty_battery(
+    hass: HomeAssistant,
+    setup_integration: Callable[[], Awaitable[bool]],
+) -> None:
+    """Test using `distanceToEmptyBattery` instead of `electricRange`."""
+
+    with patch("homeassistant.components.volvo.PLATFORMS", [Platform.SENSOR]):
+        assert await setup_integration()
+
+    assert hass.states.get("sensor.volvo_xc40_distance_to_empty_battery").state == "250"
