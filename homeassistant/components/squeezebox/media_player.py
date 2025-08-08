7a297e5ec2f91c3cfd6f94cbf6d5e1c83f71e768
peteS-UK <64092177+peteS-UK@users.noreply.github.com>
2025-08-08 12:01:35 +0000

    update

--- homeassistant/components/squeezebox/media_player.py
+++ homeassistant/components/squeezebox/media_player.py
@@ -325,7 +325,7 @@ class SqueezeBoxMediaPlayerEntity(SqueezeboxEntity, MediaPlayerEntity):
     @property
     def volume_level(self) -> float | None:
         """Volume level of the media player (0..1)."""
-        if self._player.volume or self._player.volume == 0:
+        if self._player.volume is not None:
             return int(float(self._player.volume)) / 100.0
 
         return None
