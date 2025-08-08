2e03e17bc2b639957d729e2a9ea38b0f6022d7b4
Ludovic BOUE <lboue@users.noreply.github.com>
2025-08-08 10:33:56 +0200

    Update tests

--- tests/components/matter/test_media_player.py
+++ tests/components/matter/test_media_player.py
@@ -1,7 +1,8 @@
 """Test Matter media_player."""
 
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 
+from chip.clusters import Objects as clusters
 from matter_server.client.models.node import MatterNode
 import pytest
 from syrupy.assertion import SnapshotAssertion
@@ -21,7 +22,7 @@ async def test_media_player(
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test that the correct entities get created for a media_player device."""
-    snapshot_matter_entities(hass, entity_registry, snapshot, Platform.VACUUM)
+    snapshot_matter_entities(hass, entity_registry, snapshot, Platform.MEDIA_PLAYER)
 
 
 @pytest.mark.parametrize("node_fixture", ["speaker"])
@@ -36,6 +37,24 @@ async def test_media_player_actions(
     entity_id = "media_player.mock_speaker"
     state = hass.states.get(entity_id)
     assert state
+
+    # test mute_volume action (from idle state)
+    await hass.services.async_call(
+        "media_player",
+        "mute_volume",
+        {
+            "entity_id": entity_id,
+        },
+        blocking=True,
+    )
+
+    assert matter_client.send_device_command.call_count == 1
+    assert matter_client.send_device_command.call_args == call(
+        node_id=matter_node.node_id,
+        endpoint_id=1,
+        command=clusters.OnOff.Commands.Off(),
+    )
+    matter_client.send_device_command.reset_mock()
 
 
 @pytest.mark.parametrize("node_fixture", ["speaker"])
