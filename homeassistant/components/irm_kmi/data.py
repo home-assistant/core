c52b8795b6851eef0348a93283da6ff31966aa08
Jules Dejaeghere <dej.j@live.be>
2025-08-09 16:36:49 +0200

    Use dataclass instead of dict

--- homeassistant/components/irm_kmi/data.py
+++ homeassistant/components/irm_kmi/data.py
@@ -1,16 +1,21 @@
 """Define data classes for the IRM KMI integration."""
 
-from typing import Any
+from dataclasses import dataclass, field
 
 from irm_kmi_api import CurrentWeatherData, ExtendedForecast
 
 from homeassistant.components.weather import Forecast
 
 
-class ProcessedCoordinatorData(dict[str, Any]):
-    """Data class that will be exposed to the entities consuming data from an IrmKmiCoordinator."""
+@dataclass
+class ProcessedCoordinatorData:
+    """Dataclass that will be exposed to the entities consuming data from an IrmKmiCoordinator."""
 
     current_weather: CurrentWeatherData
-    hourly_forecast: list[Forecast] | None
-    daily_forecast: list[ExtendedForecast] | None
     country: str
+    hourly_forecast: list[Forecast] = field(default_factory=list)
+    daily_forecast: list[ExtendedForecast] = field(default_factory=list)
+
+    def get(self, key, default=None):
+        """Return the value for key if key is in the ProcessedCoordinatorData, else default."""
+        return getattr(self, key, default)
