abe2d2d81a295194291b1aaa274e9fb0e3dbc0be
HarvsG <11440490+HarvsG@users.noreply.github.com>
2025-08-11 14:13:21 +0100

    Update __init__.py

--- homeassistant/components/pi_hole/__init__.py
+++ homeassistant/components/pi_hole/__init__.py
@@ -220,7 +220,8 @@ async def determine_api_version(
     else:
         # It seems that occasionally the auth can succeed unexpectedly when there is a valid session
         _LOGGER.warning(
-            "Authenticated with %s : succeeded with an incorrect password", holeV6.base_url
+            "Authenticated with %s : succeeded with an incorrect password", 
+            holeV6.base_url,
         )
         return 6
     holeV5 = api_by_version(hass, entry, 5, password="wrong_token")
