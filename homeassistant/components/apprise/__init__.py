233104aaf01c3bd25b295f9f31e71721f4df646e
Avery Dorgan <avery@averydorgan.com>
2025-08-12 04:54:28 +0000

    MVP

--- homeassistant/components/apprise/__init__.py
+++ homeassistant/components/apprise/__init__.py
@@ -2,33 +2,26 @@
 
 import apprise
 
-from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
-from homeassistant.const import CONF_NAME, CONF_URL
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import config_validation as cv, discovery
 from homeassistant.helpers.typing import ConfigType
 
 from .config_flow import validate_apprise_connection
-from .const import DEFAULT_NAME, DOMAIN
+from .const import DATA_HASS_CONFIG, DOMAIN
 
 type AppriseConfigEntry = ConfigEntry[apprise.AppriseConfig]
+
+PLATFORMS = [Platform.NOTIFY]
 
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Apprise component from YAML config."""
-    if DOMAIN not in config:
-        return True
+    """Set up the Apprise component."""
 
-    hass.async_create_task(
-        hass.config_entries.flow.async_init(
-            DOMAIN,
-            context={"source": SOURCE_IMPORT},
-            data=config[DOMAIN],
-        )
-    )
-
+    hass.data[DATA_HASS_CONFIG] = config
     return True
 
 
@@ -36,15 +29,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: AppriseConfigEntry) -> b
     """Set up Apprise from a config entry."""
 
     conf = dict(entry.data)
-    name = conf.get(CONF_NAME, DEFAULT_NAME)
-    config = conf.get("config")
-    url = conf.get(CONF_URL)
 
     if not await validate_apprise_connection(hass, conf):
         return False
 
-    apprise_config = apprise.AppriseConfig(name=name, config=config, url=url)
-    entry.runtime_data = apprise_config
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
+
+    hass.async_create_task(
+        discovery.async_load_platform(
+            hass, Platform.NOTIFY, DOMAIN, dict(entry.data), hass.data[DATA_HASS_CONFIG]
+        )
+    )
 
     return True
 
