cc4b9e0eca2436be9ae989e87edb19bbb327651a
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-08-14 11:46:06 +0200

    Extend UnitOfReactivePower with 'mvar' (#150415)

--- tests/components/sensor/test_init.py
+++ tests/components/sensor/test_init.py
@@ -2978,7 +2978,6 @@ def test_device_class_converters_are_complete() -> None:
         SensorDeviceClass.PM1,
         SensorDeviceClass.PM10,
         SensorDeviceClass.PM25,
-        SensorDeviceClass.REACTIVE_POWER,
         SensorDeviceClass.SIGNAL_STRENGTH,
         SensorDeviceClass.SOUND_PRESSURE,
         SensorDeviceClass.SULPHUR_DIOXIDE,
