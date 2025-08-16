b677aeafc9b6025a49a5d23d0f3b86117e923f08
h7io <git@copper.lt>
2025-08-16 20:01:44 +0300

    filter lovelace views for a non-admin users to only those visible

--- homeassistant/components/lovelace/websocket.py
+++ homeassistant/components/lovelace/websocket.py
@@ -7,6 +7,7 @@ from functools import wraps
 from typing import TYPE_CHECKING, Any
 
 import voluptuous as vol
+import orjson
 
 from homeassistant.components import websocket_api
 from homeassistant.core import HomeAssistant
@@ -116,7 +117,16 @@ async def websocket_lovelace_config(
     config: LovelaceConfig,
 ) -> json_fragment:
     """Send Lovelace UI config over WebSocket connection."""
-    return await config.async_json(msg["force"])
+    config = await config.async_json(msg["force"])
+
+    if not connection.user.is_admin:
+        deserialized_config = orjson.loads(orjson.dumps(config))
+        """Filter to include only views visible to the current non-admin user."""
+        deserialized_config["views"] = list(filter(lambda view : any(user["user"] == connection.user.id for user in view["visible"]) if "visible" in view else True, deserialized_config["views"]))
+
+        config = json_fragment(orjson.dumps(deserialized_config))
+
+    return config
 
 
 @websocket_api.require_admin
