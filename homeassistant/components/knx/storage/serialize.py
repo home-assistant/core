70cc44d7df19258ec87e9d74a16821401fccd044
farmio <farmio@alphart.net>
2025-08-19 21:43:58 +0200

    update serializer typing for voluptuous-serialize 2.7.0

--- homeassistant/components/knx/storage/serialize.py
+++ homeassistant/components/knx/storage/serialize.py
@@ -1,9 +1,9 @@
 """Custom serializer for KNX schemas."""
 
-from typing import Any
+from typing import Any, cast
 
 import voluptuous as vol
-from voluptuous_serialize import UNSUPPORTED, convert
+from voluptuous_serialize import UNSUPPORTED, UnsupportedType, convert
 
 from homeassistant.const import Platform
 from homeassistant.helpers import selector
@@ -14,11 +14,14 @@ from .knx_selector import AllSerializeFirst, GroupSelectSchema, KNXSelectorBase
 
 def knx_serializer(
     schema: vol.Schema,
-) -> dict[str, Any] | list[dict[str, Any]]:
+) -> dict[str, Any] | list[dict[str, Any]] | UnsupportedType:
     """Serialize KNX schema."""
     if isinstance(schema, GroupSelectSchema):
         return [
-            convert(option, custom_serializer=knx_serializer)
+            cast(
+                dict[str, Any],  # GroupSelectOption converts to a dict with subschema
+                convert(option, custom_serializer=knx_serializer),
+            )
             for option in schema.validators
         ]
     if isinstance(schema, KNXSelectorBase):
@@ -27,12 +30,12 @@ def knx_serializer(
             result["schema"] = convert(schema.schema, custom_serializer=knx_serializer)
         return result
     if isinstance(schema, AllSerializeFirst):
-        return convert(schema.validators[0], custom_serializer=knx_serializer)  # type: ignore[no-any-return]
+        return convert(schema.validators[0], custom_serializer=knx_serializer)
 
     if isinstance(schema, selector.Selector):
         return schema.serialize() | {"type": "ha_selector"}
 
-    return UNSUPPORTED  # type: ignore[no-any-return]
+    return UNSUPPORTED
 
 
 def get_serialized_schema(
@@ -40,5 +43,5 @@ def get_serialized_schema(
 ) -> dict[str, Any] | list[dict[str, Any]] | None:
     """Get the schema for a specific platform."""
     if knx_schema := KNX_SCHEMA_FOR_PLATFORM.get(platform):
-        return convert(knx_schema, custom_serializer=knx_serializer)  # type: ignore[no-any-return]
+        return convert(knx_schema, custom_serializer=knx_serializer)
     return None
