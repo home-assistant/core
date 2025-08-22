c5ceecce79b85b9364f99b933aa3c5fe97a6e49c
hanwg <han.wuguang@gmail.com>
2025-08-22 01:02:46 +0000

    add service UI

--- homeassistant/components/telegram_bot/__init__.py
+++ homeassistant/components/telegram_bot/__init__.py
@@ -8,6 +8,7 @@ from types import ModuleType
 from typing import Any
 
 from telegram import Bot
+from telegram.constants import InputMediaType
 from telegram.error import InvalidToken, TelegramError
 import voluptuous as vol
 
@@ -51,6 +52,7 @@ from .const import (
     ATTR_IS_BIG,
     ATTR_KEYBOARD,
     ATTR_KEYBOARD_INLINE,
+    ATTR_MEDIA_TYPE,
     ATTR_MESSAGE,
     ATTR_MESSAGE_TAG,
     ATTR_MESSAGE_THREAD_ID,
@@ -86,6 +88,7 @@ from .const import (
     SERVICE_DELETE_MESSAGE,
     SERVICE_EDIT_CAPTION,
     SERVICE_EDIT_MESSAGE,
+    SERVICE_EDIT_MESSAGE_MEDIA,
     SERVICE_EDIT_REPLYMARKUP,
     SERVICE_LEAVE_CHAT,
     SERVICE_SEND_ANIMATION,
@@ -200,6 +203,32 @@ SERVICE_SCHEMA_EDIT_MESSAGE = SERVICE_SCHEMA_SEND_MESSAGE.extend(
     }
 )
 
+SERVICE_SCHEMA_EDIT_MESSAGE_MEDIA = vol.Schema(
+    {
+        vol.Optional(CONF_CONFIG_ENTRY_ID): cv.string,
+        vol.Required(ATTR_MESSAGEID): vol.Any(
+            cv.positive_int, vol.All(cv.string, "last")
+        ),
+        vol.Required(ATTR_CHAT_ID): vol.Coerce(int),
+        vol.Required(ATTR_MEDIA_TYPE): vol.In(
+            (
+                InputMediaType.ANIMATION,
+                InputMediaType.AUDIO,
+                InputMediaType.VIDEO,
+                InputMediaType.DOCUMENT,
+                InputMediaType.PHOTO,
+            )
+        ),
+        vol.Optional(ATTR_URL): cv.string,
+        vol.Optional(ATTR_FILE): cv.string,
+        vol.Optional(ATTR_USERNAME): cv.string,
+        vol.Optional(ATTR_PASSWORD): cv.string,
+        vol.Optional(ATTR_AUTHENTICATION): cv.string,
+        vol.Optional(ATTR_VERIFY_SSL): cv.boolean,
+        vol.Optional(ATTR_KEYBOARD_INLINE): cv.ensure_list,
+    }
+)
+
 SERVICE_SCHEMA_EDIT_CAPTION = vol.Schema(
     {
         vol.Optional(CONF_CONFIG_ENTRY_ID): cv.string,
@@ -277,6 +306,7 @@ SERVICE_MAP = {
     SERVICE_SEND_LOCATION: SERVICE_SCHEMA_SEND_LOCATION,
     SERVICE_SEND_POLL: SERVICE_SCHEMA_SEND_POLL,
     SERVICE_EDIT_MESSAGE: SERVICE_SCHEMA_EDIT_MESSAGE,
+    SERVICE_EDIT_MESSAGE_MEDIA: SERVICE_SCHEMA_EDIT_MESSAGE_MEDIA,
     SERVICE_EDIT_CAPTION: SERVICE_SCHEMA_EDIT_CAPTION,
     SERVICE_EDIT_REPLYMARKUP: SERVICE_SCHEMA_EDIT_REPLYMARKUP,
     SERVICE_ANSWER_CALLBACK_QUERY: SERVICE_SCHEMA_ANSWER_CALLBACK_QUERY,
