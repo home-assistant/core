9634ecd9cccf45fdc798cf6886a54e560e0c8b1f
Michael Toscano <33842918+toscano@users.noreply.github.com>
2025-08-22 19:13:22 +0000

    Add search text services

--- homeassistant/components/apple_tv/remote.py
+++ homeassistant/components/apple_tv/remote.py
@@ -5,8 +5,10 @@ from collections.abc import Iterable
 import logging
 from typing import Any
 
-from pyatv.const import InputAction
+from pyatv.const import InputAction, KeyboardFocusState
+import voluptuous as vol
 
+from homeassistant.components import conversation
 from homeassistant.components.remote import (
     ATTR_DELAY_SECS,
     ATTR_HOLD_SECS,
@@ -17,9 +19,15 @@ from homeassistant.components.remote import (
 )
 from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv, entity_platform
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from . import AppleTvConfigEntry
+from .const import (
+    SERVICE_APPEND_SEARCH_TEXT,
+    SERVICE_CLEAR_SEARCH_TEXT,
+    SERVICE_SET_SEARCH_TEXT,
+)
 from .entity import AppleTVEntity
 
 _LOGGER = logging.getLogger(__name__)
@@ -41,6 +49,24 @@ async def async_setup_entry(
     async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Load Apple TV remote based on a config entry."""
+    platform = entity_platform.async_get_current_platform()
+    # Register the services
+    platform.async_register_entity_service(
+        SERVICE_SET_SEARCH_TEXT,
+        {vol.Required(conversation.ATTR_TEXT): cv.string},
+        "async_set_search_text",
+    )
+    platform.async_register_entity_service(
+        SERVICE_APPEND_SEARCH_TEXT,
+        {vol.Required(conversation.ATTR_TEXT): cv.string},
+        "async_append_search_text",
+    )
+    platform.async_register_entity_service(
+        SERVICE_CLEAR_SEARCH_TEXT,
+        None,
+        "async_clear_search_text",
+    )
+
     name: str = config_entry.data[CONF_NAME]
     # apple_tv config entries always have a unique id
     assert config_entry.unique_id is not None
@@ -52,7 +78,7 @@ class AppleTVRemote(AppleTVEntity, RemoteEntity):
     """Device that sends commands to an Apple TV."""
 
     @property
-    def is_on(self) -> bool:
+    def is_on(self) -> bool:  # pyright: ignore[reportIncompatibleVariableOverride]
         """Return true if device is on."""
         return self.atv is not None
 
@@ -94,3 +120,43 @@ class AppleTVRemote(AppleTVEntity, RemoteEntity):
                     await attr_value()
 
                 await asyncio.sleep(delay)
+
+    def ok_to_change_text(self) -> bool:
+        """Check the status of the keyboard."""
+        if not self.atv:
+            _LOGGER.error("Unable to set text, not connected to Apple TV")
+            return False
+
+        if not self.atv.keyboard:
+            _LOGGER.error("Keyboard not available on Apple TV")
+            return False
+
+        if self.atv.keyboard.text_focus_state != KeyboardFocusState.Focused:
+            _LOGGER.error("Keyboard not focused on Apple TV")
+            return False
+
+        return True
+
+    async def async_set_search_text(self, text: str) -> None:
+        """Set the search text on the Apple TV."""
+        if not self.ok_to_change_text():
+            return
+
+        _LOGGER.debug("Setting search text to '%s'", text)
+        await self.atv.keyboard.text_set(text)  # type: ignore[union-attr]
+
+    async def async_append_search_text(self, text: str) -> None:
+        """Append text to the current search text on the Apple TV."""
+        if not self.ok_to_change_text():
+            return
+
+        _LOGGER.debug("Appending search text '%s'", text)
+        await self.atv.keyboard.text_append(text)  # type: ignore[union-attr]
+
+    async def async_clear_search_text(self) -> None:
+        """Clear the current search text on the Apple TV."""
+        if not self.ok_to_change_text():
+            return
+
+        _LOGGER.debug("Clearing search text")
+        await self.atv.keyboard.text_clear()  # type: ignore[union-attr]
