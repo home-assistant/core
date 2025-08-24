12f9b27baa7c4775704c002a4d20f107088aa7d3
TheOgre <dogg123456789@gmail.com>
2025-08-24 13:02:57 -0400

    Update __init__.py

--- homeassistant/components/sharkiq/__init__.py
+++ homeassistant/components/sharkiq/__init__.py
@@ -15,7 +15,7 @@ from homeassistant import exceptions
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_REGION, CONF_USERNAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
 from .const import (
     API_TIMEOUT,
@@ -37,6 +37,7 @@ async def async_connect_or_timeout(ayla_api: AylaApi) -> bool:
     try:
         async with asyncio.timeout(API_TIMEOUT):
             LOGGER.debug("Initialize connection to Ayla networks API")
+            await ayla_api.async_set_cookie()
             await ayla_api.async_sign_in()
     except SharkIqAuthError:
         LOGGER.error("Authentication error connecting to Shark IQ api")
@@ -59,7 +60,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     ayla_api = get_ayla_api(
         username=config_entry.data[CONF_USERNAME],
         password=config_entry.data[CONF_PASSWORD],
-        websession=async_get_clientsession(hass),
+        websession=async_create_clientsession(hass),
         europe=(config_entry.data[CONF_REGION] == SHARKIQ_REGION_EUROPE),
     )
 
@@ -94,7 +95,7 @@ async def async_disconnect_or_timeout(coordinator: SharkIqUpdateCoordinator):
             await coordinator.ayla_api.async_sign_out()
 
 
-async def async_update_options(hass, config_entry):
+async def async_update_options(hass: HomeAssistant, config_entry):
     """Update options."""
     await hass.config_entries.async_reload(config_entry.entry_id)
 
