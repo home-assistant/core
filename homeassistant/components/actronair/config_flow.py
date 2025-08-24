54b7e1ee32eec63554a8f76314aafa9aa0161699
Kurt Clifford <kclif9@gmail.com>
2025-08-24 12:22:28 +0000

    use progress config flow to make authentication more seamless

--- homeassistant/components/actronair/config_flow.py
+++ homeassistant/components/actronair/config_flow.py
@@ -1,12 +1,13 @@
 """Setup config flow for Actron Neo integration."""
 
+import asyncio
 from typing import Any
 
 from actron_neo_api import ActronNeoAPI, ActronNeoAuthError
-import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_API_TOKEN
+from homeassistant.exceptions import HomeAssistantError
 
 from .const import _LOGGER, DOMAIN
 
@@ -21,38 +22,17 @@ class ActronNeoConfigFlow(ConfigFlow, domain=DOMAIN):
         self._user_code: str = ""
         self._verification_uri: str = ""
         self._expires_minutes: str = "30"
+        self.login_task: asyncio.Task | None = None
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
-        errors: dict[str, str] = {}
-
-        if self._api is not None and self._device_code is not None:
-            try:
-                token_data = await self._api.poll_for_token(self._device_code)
-            except ActronNeoAuthError as err:
-                _LOGGER.error("Error checking authorization: %s", err)
-                errors["base"] = "oauth2_error"
-
-            if token_data is None:
-                errors["base"] = "authorization_pending"
-            else:
-                user_data = await self._api.get_user_info()
-                await self.async_set_unique_id(user_data["id"])
-                self._abort_if_unique_id_configured()
-                return self.async_create_entry(
-                    title=user_data["email"],
-                    data={
-                        CONF_API_TOKEN: self._api.refresh_token_value,
-                    },
-                )
-
-        else:
+        if self._api is None:
+            _LOGGER.debug("Initiating device authorization")
             try:
                 self._api = ActronNeoAPI()
                 device_code_response = await self._api.request_device_code()
-
             except ActronNeoAuthError as err:
                 _LOGGER.error("OAuth2 flow failed: %s", err)
                 return self.async_abort(reason="oauth2_error")
@@ -62,13 +42,75 @@ class ActronNeoConfigFlow(ConfigFlow, domain=DOMAIN):
             self._verification_uri = device_code_response["verification_uri_complete"]
             self._expires_minutes = str(device_code_response["expires_in"] // 60)
 
-        return self.async_show_form(
+        async def _wait_for_authorization() -> None:
+            """Wait for the user to authorize the device."""
+            assert self._api is not None
+            assert self._device_code is not None
+            _LOGGER.debug("Waiting for device authorization")
+            try:
+                await self._api.poll_for_token(self._device_code)
+                _LOGGER.debug("Authorization successful")
+            except ActronNeoAuthError as ex:
+                _LOGGER.exception("Error while waiting for device authorization")
+                raise CannotConnect from ex
+
+        _LOGGER.debug("Checking login task")
+        if self.login_task is None:
+            _LOGGER.debug("Creating task for device authorization")
+            self.login_task = self.hass.async_create_task(_wait_for_authorization())
+
+        if self.login_task.done():
+            _LOGGER.debug("Login task is done, checking results")
+            if self.login_task.exception():
+                return self.async_show_progress_done(next_step_id="timeout")
+            return self.async_show_progress_done(next_step_id="finish_login")
+
+        return self.async_show_progress(
             step_id="user",
-            data_schema=vol.Schema({}),
-            errors=errors,
+            progress_action="wait_for_authorization",
             description_placeholders={
                 "user_code": self._user_code,
                 "verification_uri": self._verification_uri,
                 "expires_minutes": self._expires_minutes,
             },
+            progress_task=self.login_task,
         )
+
+    async def async_step_finish_login(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle the finalization of login."""
+        _LOGGER.debug("Finalizing authorization")
+        assert self._api is not None
+
+        try:
+            user_data = await self._api.get_user_info()
+        except ActronNeoAuthError as err:
+            _LOGGER.error("Error getting user info: %s", err)
+            return self.async_abort(reason="oauth2_error")
+
+        unique_id = str(user_data["id"])
+        await self.async_set_unique_id(unique_id)
+        self._abort_if_unique_id_configured()
+
+        return self.async_create_entry(
+            title=user_data["email"],
+            data={CONF_API_TOKEN: self._api.refresh_token_value},
+        )
+
+    async def async_step_timeout(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle timeout from progress step."""
+        if user_input is None:
+            return self.async_show_form(step_id="timeout")
+
+        # Reset state and try again
+        self._api = None
+        self._device_code = None
+        self.login_task = None
+        return await self.async_step_user()
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
