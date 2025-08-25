eca41000be9d862583c2962f2d85f47ae0d75eda
刀剑剑非刀 <1252677296@qq.com>
2025-08-25 16:24:10 +0800

    update test file

--- homeassistant/components/daybetter_light_local/__init__.py
+++ homeassistant/components/daybetter_light_local/__init__.py
@@ -26,7 +26,6 @@ async def async_setup_entry(
     """Set up DayBetter light local from a config entry."""
     coordinator = DayBetterLocalApiCoordinator(hass, entry)
 
-
     await coordinator.async_config_entry_first_refresh()
 
     if not coordinator.devices:  # 设备列表为空
