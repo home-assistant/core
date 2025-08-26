ea73d5f9e94715520be5ed765b4c734acbeaa50d
G Johansson <goran.johansson@shiftit.se>
2025-08-26 16:55:59 +0000

    Remove not needed debug

--- homeassistant/components/bayesian/__init__.py
+++ homeassistant/components/bayesian/__init__.py
@@ -1,13 +1,9 @@
 """The bayesian component."""
 
-import logging
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import PLATFORMS
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -20,19 +16,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Bayesian config entry."""
-    _LOGGER.debug(
-        "Unloading sensor for entry_id %s with options %s",
-        entry.entry_id,
-        entry.options,
-    )
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
 async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Handle options update."""
-    _LOGGER.debug(
-        "Options flow update for entry_id %s with options %s",
-        entry.entry_id,
-        entry.options,
-    )
     hass.config_entries.async_schedule_reload(entry.entry_id)
