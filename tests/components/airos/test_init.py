b0f655128896d07c6aef7589be54760fe6d41205
Tom Scholten <git@scholten.nu>
2025-08-27 19:38:43 +0200

    Revert commit as minor_version

--- tests/components/airos/test_init.py
+++ tests/components/airos/test_init.py
@@ -4,8 +4,8 @@ from __future__ import annotations
 
 from unittest.mock import ANY, MagicMock
 
-from homeassistant.components.airos.const import DEFAULT_SSL, DOMAIN
-from homeassistant.config_entries import ConfigEntryState
+from homeassistant.components.airos.const import DEFAULT_SSL, DEFAULT_VERIFY_SSL, DOMAIN
+from homeassistant.config_entries import SOURCE_USER, ConfigEntryState
 from homeassistant.const import (
     CONF_HOST,
     CONF_PASSWORD,
@@ -17,11 +17,25 @@ from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
 
+MOCK_CONFIG_V1 = {
+    CONF_HOST: "1.1.1.1",
+    CONF_USERNAME: "ubnt",
+    CONF_PASSWORD: "test-password",
+}
+
 MOCK_CONFIG_PLAIN = {
     CONF_HOST: "1.1.1.1",
     CONF_USERNAME: "ubnt",
     CONF_PASSWORD: "test-password",
     CONF_SSL: False,
+}
+
+MOCK_CONFIG_V1_2 = {
+    CONF_HOST: "1.1.1.1",
+    CONF_USERNAME: "ubnt",
+    CONF_PASSWORD: "test-password",
+    CONF_SSL: DEFAULT_SSL,
+    CONF_VERIFY_SSL: DEFAULT_VERIFY_SSL,
 }
 
 
@@ -81,3 +95,49 @@ async def test_setup_entry_without_ssl(
 
     assert entry.data[CONF_SSL] is False
     assert entry.data[CONF_VERIFY_SSL] is False
+
+
+async def test_migrate_entry(hass: HomeAssistant, mock_airos_client: MagicMock) -> None:
+    """Test migrate entry unique id."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        source=SOURCE_USER,
+        data=MOCK_CONFIG_V1,
+        entry_id="1",
+        unique_id="airos_device",
+        version=1,
+    )
+    entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert entry.state is ConfigEntryState.LOADED
+    assert entry.version == 1
+    assert entry.minor_version == 2
+    assert entry.data == MOCK_CONFIG_V1_2
+
+
+async def test_load_unload_entry(
+    hass: HomeAssistant, mock_airos_client: MagicMock
+) -> None:
+    """Test setup and unload config entry."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data=MOCK_CONFIG_V1_2,
+        entry_id="1",
+        unique_id="airos_device",
+        version=1,
+        minor_version=2,
+    )
+    entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert entry.state is ConfigEntryState.LOADED
+
+    assert await hass.config_entries.async_unload(entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert entry.state is ConfigEntryState.NOT_LOADED
