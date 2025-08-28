e9ece7e5c5e45dff4fc99764a7b7921ef0a0c8ac
刀剑剑非刀 <1252677296@qq.com>
2025-08-28 19:07:56 +0800

    reset file

--- homeassistant/components/daybetter_light_local/config_flow.py
+++ homeassistant/components/daybetter_light_local/config_flow.py
@@ -15,7 +15,6 @@ from homeassistant import config_entries
 from homeassistant.components import network
 from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import HomeAssistantError
 
 from .const import (
     CONF_LISTENING_PORT_DEFAULT,
@@ -33,10 +32,7 @@ async def _async_discover_devices(
 ) -> list[dict[str, Any]] | str:
     """Discover DayBetter devices. If host is given, probe only that host."""
 
-    try:
-        adapter = await network.async_get_source_ip(hass, network.PUBLIC_TARGET_IP)
-    except (HomeAssistantError, ValueError, RuntimeError):
-        adapter = "0.0.0.0"
+    adapter = await network.async_get_source_ip(hass, network.PUBLIC_TARGET_IP)
 
     controller = DayBetterController(
         loop=hass.loop,
--- tests/components/daybetter_light_local/test_light.py
+++ tests/components/daybetter_light_local/test_light.py
@@ -198,7 +198,7 @@ async def test_light_setup_error(
         entry.add_to_hass(hass)
 
         await hass.config_entries.async_setup(entry.entry_id)
-        assert entry.state is ConfigEntryState.SETUP_ERROR
+        assert entry.state is ConfigEntryState.SETUP_RETRY
 
 
 async def test_light_on_off(hass: HomeAssistant, mock_DayBetter_api: MagicMock) -> None:
