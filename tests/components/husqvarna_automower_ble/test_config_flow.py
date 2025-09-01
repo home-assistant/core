1ebd0f34708814c8397256d21dab249641276d08
Christopher Fenner <Christopher.Fenner@me.com>
2025-09-01 16:19:27 +0200

    extend test case

--- tests/components/husqvarna_automower_ble/test_config_flow.py
+++ tests/components/husqvarna_automower_ble/test_config_flow.py
@@ -45,6 +45,22 @@ async def test_user_selection(hass: HomeAssistant) -> None:
     assert result["type"] is FlowResultType.FORM
     assert result["step_id"] == "user"
 
+    # mock connection error
+    with patch(
+        "homeassistant.components.husqvarna_automower_ble.config_flow.HusqvarnaAutomowerBleConfigFlow.probe_mower",
+        return_value=None,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input={
+                CONF_ADDRESS: "00000000-0000-0000-0000-000000000001",
+                CONF_PIN: "1234",
+            },
+        )
+        assert result["type"] is FlowResultType.FORM
+        assert result["step_id"] == "user"
+        assert result["errors"] == {"base": "cannot_connect"}
+
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"],
         user_input={
