c8d0d86af0c4029fdcb5e28248a602e7d0579c7a
Brandon Rothweiler <2292715+bdr99@users.noreply.github.com>
2025-09-01 23:43:35 -0400

    Finish select platform implementation; add tests

--- homeassistant/components/aosmith/select.py
+++ homeassistant/components/aosmith/select.py
@@ -2,8 +2,7 @@
 
 from homeassistant.components.select import SelectEntity
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from . import AOSmithConfigEntry
 from .coordinator import AOSmithStatusCoordinator
@@ -17,13 +16,11 @@ HWP_LEVEL_HA_TO_AOSMITH = {
 }
 HWP_LEVEL_AOSMITH_TO_HA = {value: key for key, value in HWP_LEVEL_HA_TO_AOSMITH.items()}
 
-x = list(HWP_LEVEL_AOSMITH_TO_HA)
-
 
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: AOSmithConfigEntry,
-    async_add_entities: AddEntitiesCallback,
+    async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Set up A. O. Smith select platform."""
     data = entry.runtime_data
@@ -40,8 +37,9 @@ async def async_setup_entry(
 class AOSmithHotWaterPlusSelectEntity(AOSmithStatusEntity, SelectEntity):
     """Class for the Hot Water+ select entity."""
 
+    _attr_translation_key = "hot_water_plus_level"
+    _attr_icon = "mdi:water-plus"
     _attr_options = list(HWP_LEVEL_HA_TO_AOSMITH)
-    _attr_translation_key = "hot_water_plus"
 
     def __init__(self, coordinator: AOSmithStatusCoordinator, junction_id: str) -> None:
         """Initialize the entity."""
@@ -49,15 +47,22 @@ class AOSmithHotWaterPlusSelectEntity(AOSmithStatusEntity, SelectEntity):
         self._attr_unique_id = junction_id
 
     @property
+    def suggested_object_id(self) -> str | None:
+        """Override the suggested object id to make '+' get converted to 'plus' in the entity id."""
+        return "hot_water_plus_level"
+
+    @property
     def current_option(self) -> str | None:
         """Return the current Hot Water+ mode."""
-        return HWP_LEVEL_HA_TO_AOSMITH.get(self.device.status.hot_water_plus_level)
+        hot_water_plus_level = self.device.status.hot_water_plus_level
+        return (
+            None
+            if hot_water_plus_level is None
+            else HWP_LEVEL_AOSMITH_TO_HA.get(hot_water_plus_level)
+        )
 
     async def async_select_option(self, option: str) -> None:
         """Set the Hot Water+ mode."""
-        if option not in self.options:
-            raise HomeAssistantError(f"Invalid option: {option}")
-
         aosmith_hwp_level = HWP_LEVEL_HA_TO_AOSMITH.get(option)
         if aosmith_hwp_level is not None:
             await self.client.update_mode(
@@ -67,8 +72,3 @@ class AOSmithHotWaterPlusSelectEntity(AOSmithStatusEntity, SelectEntity):
             )
 
             await self.coordinator.async_request_refresh()
-
-    @property
-    def options(self) -> list[str]:
-        """Return the list of available operation modes."""
-        return [mode.original_name for mode in self.device.supported_modes]
