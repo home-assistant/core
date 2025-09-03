1e8ea27d4ea4ddbe2a58e3963ef6e590ac9dcfa3
Kevin McCormack <kevin@mccormack.tech>
2025-09-03 00:27:59 +0000

    Add const

--- homeassistant/components/opnsense/__init__.py
+++ homeassistant/components/opnsense/__init__.py
@@ -1,4 +1,4 @@
-"""Support for OPNSense Routers."""
+"""Support for OPNsense Routers."""
 
 import logging
 
@@ -12,14 +12,9 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.discovery import load_platform
 from homeassistant.helpers.typing import ConfigType
 
+from .const import CONF_API_SECRET, CONF_TRACKER_INTERFACES, DOMAIN, OPNSENSE_DATA
+
 _LOGGER = logging.getLogger(__name__)
-
-CONF_API_SECRET = "api_secret"
-CONF_TRACKER_INTERFACE = "tracker_interfaces"
-
-DOMAIN = "opnsense"
-
-OPNSENSE_DATA = DOMAIN
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -29,7 +24,7 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Required(CONF_API_KEY): cv.string,
                 vol.Required(CONF_API_SECRET): cv.string,
                 vol.Optional(CONF_VERIFY_SSL, default=False): cv.boolean,
-                vol.Optional(CONF_TRACKER_INTERFACE, default=[]): vol.All(
+                vol.Optional(CONF_TRACKER_INTERFACES, default=[]): vol.All(
                     cv.ensure_list, [cv.string]
                 ),
             }
@@ -47,7 +42,7 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     api_key = conf[CONF_API_KEY]
     api_secret = conf[CONF_API_SECRET]
     verify_ssl = conf[CONF_VERIFY_SSL]
-    tracker_interfaces = conf[CONF_TRACKER_INTERFACE]
+    tracker_interfaces = conf[CONF_TRACKER_INTERFACES]
 
     interfaces_client = diagnostics.InterfaceClient(
         api_key, api_secret, url, verify_ssl, timeout=20
@@ -73,7 +68,7 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
     hass.data[OPNSENSE_DATA] = {
         "interfaces": interfaces_client,
-        CONF_TRACKER_INTERFACE: tracker_interfaces,
+        CONF_TRACKER_INTERFACES: tracker_interfaces,
     }
 
     load_platform(hass, Platform.DEVICE_TRACKER, DOMAIN, tracker_interfaces, config)
