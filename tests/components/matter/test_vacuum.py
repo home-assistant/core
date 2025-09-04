aa06124719cf18ed33dccfc8939b86121763c616
Ludovic BOUE <lboue@users.noreply.github.com>
2025-09-04 11:00:01 +0200

    Add test for Switchbot K11 vacuum entity actions

--- tests/components/matter/test_vacuum.py
+++ tests/components/matter/test_vacuum.py
@@ -137,6 +137,38 @@ async def test_vacuum_actions(
     matter_client.send_device_command.reset_mock()
 
 
+@pytest.mark.parametrize("node_fixture", ["switchbot_K11"])
+async def test_k11_vacuum_actions(
+    hass: HomeAssistant,
+    matter_client: MagicMock,
+    matter_node: MatterNode,
+) -> None:
+    """Test vacuum entity actions."""
+    # Fetch translations
+    await async_setup_component(hass, "homeassistant", {})
+    entity_id = "vacuum.k11"
+    state = hass.states.get(entity_id)
+    assert state
+    """
+    await hass.services.async_call(
+        "vacuum",
+        "select_areas",
+        {
+            "entity_id": entity_id,
+        },
+        blocking=True,
+    )
+    selected_areas=[1, 2, 3]
+    assert matter_client.send_device_command.call_count == 1
+    assert matter_client.send_device_command.call_args == call(
+        node_id=matter_node.node_id,
+        endpoint_id=1,
+        command=clusters.ServiceArea.Commands.SelectAreas(newAreas=selected_areas),
+    )
+    matter_client.send_device_command.reset_mock()
+    """
+
+
 @pytest.mark.parametrize("node_fixture", ["vacuum_cleaner"])
 async def test_vacuum_updates(
     hass: HomeAssistant,
