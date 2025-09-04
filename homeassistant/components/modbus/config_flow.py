7bf1b162c425b1bb7353a2823bb30a141e629d5b
jan iversen <jancasacondor@gmail.com>
2025-09-04 10:09:09 +0000

    Second round.

--- homeassistant/components/modbus/config_flow.py
+++ homeassistant/components/modbus/config_flow.py
@@ -4,38 +4,36 @@ from typing import Any
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 
-from .const import _LOGGER, MODBUS_DOMAIN
+from .const import _LOGGER, MODBUS_DOMAIN, MODBUS_UIID
 
 
 class ModbusConfigFlow(ConfigFlow, domain=MODBUS_DOMAIN):
     """mondbus integration config flow."""
 
     VERSION = 1
-    MINOR_VERION = 1
+    MINOR_VERION = 0
 
     def __init__(self) -> None:
-        """Initialize flow."""
+        """Set up flow instance."""
 
     async def async_step_reconfigure(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Reconfigure a flow initialized by the user."""
-        _LOGGER.debug(f"modbus_cf async_step_reconfigure called <<{user_input}>>")
-        return await self.async_step_user(user_input=user_input)
+        return self.async_abort(reason="no_online_reconfig")
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle a flow initialized by the user."""
-        _LOGGER.debug(f"modbus_cf async_step_user called <<{user_input}>>")
-        if user_input is not None:
-            return self.async_create_entry(
-                title=MODBUS_DOMAIN + "_integration",
-                data=user_input,
-            )
-        return self.async_show_form(step_id="user")
+        _LOGGER.error("JAN async_step_user")
+        return self.async_abort(reason="no_online_config")
 
     async def async_step_import(self, import_data: dict[str, Any]) -> ConfigFlowResult:
         """Handle import from configuration.yaml."""
-        _LOGGER.debug(f"modbus_cf async_step_import called <<{import_data}>>")
-        return await self.async_step_user(import_data)
+        await self.async_set_unique_id(MODBUS_UIID)
+        self._abort_if_unique_id_configured()
+        return self.async_create_entry(
+            title=MODBUS_DOMAIN + "_integration",
+            data=import_data,
+        )
--- homeassistant/components/modbus/const.py
+++ homeassistant/components/modbus/const.py
@@ -159,6 +159,7 @@ DEFAULT_TEMP_UNIT = "C"
 DEFAULT_HVAC_ON_VALUE = 1
 DEFAULT_HVAC_OFF_VALUE = 0
 MODBUS_DOMAIN = "modbus"
+MODBUS_UIID = "modbus_integration"
 
 ACTIVE_SCAN_INTERVAL = 2  # limit to force an extra update
 
