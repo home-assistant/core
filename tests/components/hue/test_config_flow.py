af2ada394c37818c9c7570331c95cd0a3d12441b
Joostlek <joostlek@outlook.com>
2025-09-05 09:28:01 +0200

    First draft for the tests

--- tests/components/hue/test_config_flow.py
+++ tests/components/hue/test_config_flow.py
@@ -743,35 +743,47 @@ async def test_bsb003_bridge_discovery(
     entry = MockConfigEntry(
         domain=const.DOMAIN,
         data={"host": "192.168.1.217", "api_version": 2, "api_key": "abc"},
-        unique_id="bsb002_v2",
+        unique_id="bsb002_00000",
     )
     entry.add_to_hass(hass)
     device_registry.async_get_or_create(
         config_entry_id=entry.entry_id,
-        identifiers={(const.DOMAIN, "bsb002_v2")},
+        identifiers={(const.DOMAIN, "bsb002000_v2")},
         connections={(dr.CONNECTION_NETWORK_MAC, "AA:BB:CC:DD:EE:FF")},
     )
     create_mock_api_discovery(
-        aioclient_mock, [("192.168.1.217", "bsb002_v2"), ("192.168.1.218", "bsb003_v2")]
+        aioclient_mock,
+        [("192.168.1.217", "bsb002_00000"), ("192.168.1.218", "bsb003_00000")],
     )
-    result = await hass.config_entries.flow.async_init(
-        const.DOMAIN,
-        context={"source": config_entries.SOURCE_ZEROCONF},
-        data=ZeroconfServiceInfo(
-            ip_address=ip_address("192.168.1.218"),
-            ip_addresses=[ip_address("192.168.1.218")],
-            port=443,
-            hostname="Philips-hue.local",
-            type="_hue._tcp.local.",
-            name="Philips Hue - ABCABC._hue._tcp.local.",
-            properties={
-                "bridgeid": "bsb003_v2",
-                "modelid": "BSB003",
-            },
+    disc_bridge = get_discovered_bridge(bridge_id="bsb003_00000", supports_v2=True)
+
+    with (
+        patch(
+            "homeassistant.components.hue.config_flow.discover_bridge",
+            return_value=disc_bridge,
         ),
-    )
+        patch(
+            "homeassistant.components.hue.config_flow.HueBridgeV2",
+            autospec=True,
+        ) as mock_bridge,
+    ):
+        mock_bridge.return_value.fetch_full_state.return_value = {}
+        result = await hass.config_entries.flow.async_init(
+            const.DOMAIN,
+            context={"source": config_entries.SOURCE_ZEROCONF},
+            data=ZeroconfServiceInfo(
+                ip_address=ip_address("192.168.1.218"),
+                ip_addresses=[ip_address("192.168.1.218")],
+                port=443,
+                hostname="Philips-hue.local",
+                type="_hue._tcp.local.",
+                name="Philips Hue - ABCABC._hue._tcp.local.",
+                properties={
+                    "bridgeid": "bsb003_00000",
+                    "modelid": "BSB003",
+                },
+            ),
+        )
 
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "link"
-
-    pytest.fail("Should be implemented")
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "migrated_bridge"
