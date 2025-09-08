77d3fed58aa1c42eaf01179cfc54c227be416806
Simone Chemelli <simone.chemelli@gmail.com>
2025-09-08 07:42:07 +0000

    add test

--- tests/components/uptimerobot/test_config_flow.py
+++ tests/components/uptimerobot/test_config_flow.py
@@ -335,3 +335,39 @@ async def test_reconfigure_successful(
 
     # changed entry
     assert config_entry.data[CONF_API_KEY] == new_key
+
+
+async def test_reconfigure_failed(
+    hass: HomeAssistant,
+) -> None:
+    """Test that the entry reconfigure fails with a wrong key."""
+    config_entry = MockConfigEntry(
+        **{**MOCK_UPTIMEROBOT_CONFIG_ENTRY_DATA, "unique_id": None}
+    )
+    config_entry.add_to_hass(hass)
+
+    result = await config_entry.start_reconfigure_flow(hass)
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] is None
+    assert result["step_id"] == "reconfigure"
+
+    wrong_key = "u0242ac120003-wrong"
+
+    with (
+        patch(
+            "pyuptimerobot.UptimeRobot.async_get_account_details",
+            side_effect=UptimeRobotAuthenticationException,
+        ),
+        patch(
+            "homeassistant.components.uptimerobot.async_setup_entry",
+            return_value=True,
+        ),
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            user_input={CONF_API_KEY: wrong_key},
+        )
+
+    assert result2["type"] is FlowResultType.FORM
+    assert result2["errors"]["base"] == "invalid_api_key"
