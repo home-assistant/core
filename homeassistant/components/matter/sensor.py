28e4f8022c64f369d2f3fcf7026ef7a0521613d7
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-09-09 19:53:13 +0200

    Retrieve the error ID from the ErrorStateStruct structure

--- homeassistant/components/matter/sensor.py
+++ homeassistant/components/matter/sensor.py
@@ -1147,7 +1147,11 @@ DISCOVERY_SCHEMAS = [
             translation_key="operational_error",
             device_class=SensorDeviceClass.ENUM,
             options=list(OPERATIONAL_STATE_ERROR_MAP.values()),
-            device_to_ha=OPERATIONAL_STATE_ERROR_MAP.get,
+            # device_to_ha=OPERATIONAL_STATE_ERROR_MAP.get,
+            device_to_ha = lambda x: OPERATIONAL_STATE_ERROR_MAP.get(
+                x.errorStateID,  # We retrieve the error ID from the ErrorStateStruct structure
+                "no_error"   # Default value
+            ),
         ),
         entity_class=MatterSensor,
         required_attributes=(clusters.OperationalState.Attributes.OperationalError,),
@@ -1223,7 +1227,11 @@ DISCOVERY_SCHEMAS = [
             translation_key="operational_error",
             device_class=SensorDeviceClass.ENUM,
             options=list(RVC_OPERATIONAL_STATE_ERROR_MAP.values()),
-            device_to_ha=RVC_OPERATIONAL_STATE_ERROR_MAP.get,
+            # device_to_ha=RVC_OPERATIONAL_STATE_ERROR_MAP.get,
+            device_to_ha = lambda x: RVC_OPERATIONAL_STATE_ERROR_MAP.get(
+                x.errorStateID,  # We retrieve the error ID from the ErrorStateStruct structure
+                "no_error"   # Default value
+            ),
         ),
         entity_class=MatterSensor,
         required_attributes=(clusters.RvcOperationalState.Attributes.OperationalError,),
