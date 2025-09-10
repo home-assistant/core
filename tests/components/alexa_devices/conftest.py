5b513792754011c520c6e3eef309449dcd3b3374
Simone Chemelli <simone.chemelli@gmail.com>
2025-09-10 17:39:27 +0000

    align post merge

--- tests/components/alexa_devices/conftest.py
+++ tests/components/alexa_devices/conftest.py
@@ -14,7 +14,7 @@ from homeassistant.components.alexa_devices.const import (
 )
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 
-from .const import TEST_PASSWORD, TEST_USERNAME
+from .const import TEST_DEVICE_1, TEST_DEVICE_1_SN, TEST_PASSWORD, TEST_USERNAME
 
 from tests.common import MockConfigEntry
 
@@ -46,7 +46,9 @@ def mock_amazon_devices_client() -> Generator[AsyncMock]:
         client.login_mode_interactive.return_value = {
             "customer_info": {"user_id": TEST_USERNAME},
         }
-        client.get_devices_data.return_value = {TEST_DEVICE_1_SN: TEST_DEVICE_1}
+        client.get_devices_data.return_value = {
+            TEST_DEVICE_1_SN: deepcopy(TEST_DEVICE_1)
+        }
         client.get_model_details = lambda device: DEVICE_TYPE_TO_MODEL.get(
             device.device_type
         )
