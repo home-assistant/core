d21d708efd4e7d820daee4f53372c76c9afb763e
J. Nick Koston <nick@home-assistant.io>
2025-09-10 13:22:21 -0500

    update more tests

--- tests/components/switchbot/test_config_flow.py
+++ tests/components/switchbot/test_config_flow.py
@@ -1256,12 +1256,19 @@ async def test_user_setup_worelay_switch_1pm_auth_switchbot_api_down(
 ) -> None:
     """Test the user initiated form for a relay switch 1pm when the switchbot api is down."""
 
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.MENU
+    assert result["step_id"] == "user"
+
     with patch(
         "homeassistant.components.switchbot.config_flow.async_discovered_service_info",
         return_value=[WORELAY_SWITCH_1PM_SERVICE_INFO],
     ):
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": SOURCE_USER}
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {"next_step_id": "select_device"},
         )
     assert result["type"] is FlowResultType.MENU
     assert result["step_id"] == "encrypted_choose_method"
