bdd952e0d7b959c4b67cc1165ce09ade864682a1
Benjamin Pearce <admin@bcpearce.com>
2025-09-10 03:48:24 +0000

    Add remote codes which can be used with remote.send_command to diagnostics

--- homeassistant/components/braviatv/diagnostics.py
+++ homeassistant/components/braviatv/diagnostics.py
@@ -18,8 +18,10 @@ async def async_get_config_entry_diagnostics(
     coordinator = config_entry.runtime_data
 
     device_info = await coordinator.client.get_system_info()
+    command_list = await coordinator.client.get_command_list()
 
     return {
+        "command_list": command_list,
         "config_entry": async_redact_data(config_entry.as_dict(), TO_REDACT),
         "device_info": async_redact_data(device_info, TO_REDACT),
     }
