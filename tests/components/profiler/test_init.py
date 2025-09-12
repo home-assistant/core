2d9b6e9c147dc78c448e4b5c99ebb0842ecdf52e
Jan Čermák <sairon@sairon.cz>
2025-09-12 16:24:28 +0200

    Add service for logging Python audit events to profiler integration
    Python has "auditing" API which provides a way to track certain actions
    which are executed in runtime. This can be useful for instance when user
    wants to track what integrations are making outbound network connection
    or accessing files on the filesystem, which may be needed also to
    identify sources of resource leaks.
    
    This PR adds services for configuring what audit events should be
    logged, with optional filtering and full traceback capture.
    
    The Audit API was defined by PEP 578 and is available in the standard
    library since Python 3.8: https://peps.python.org/pep-0578
    
    List of events which can be audited:
    https://docs.python.org/3/library/audit_events.html

--- tests/components/profiler/test_init.py
+++ tests/components/profiler/test_init.py
@@ -15,8 +15,13 @@ import pytest
 from homeassistant.components.profiler import (
     _LRU_CACHE_WRAPPER_OBJECT,
     _SQLALCHEMY_LRU_OBJECT,
+    AUDITED_EVENTS,
+    AUDITING_HOOK_ADDED,
     CONF_ENABLED,
+    CONF_EVENTS,
+    CONF_FILTER,
     CONF_SECONDS,
+    CONF_VERBOSE,
     SERVICE_DUMP_LOG_OBJECTS,
     SERVICE_LOG_CURRENT_TASKS,
     SERVICE_LOG_EVENT_LOOP_SCHEDULED,
@@ -25,8 +30,10 @@ from homeassistant.components.profiler import (
     SERVICE_MEMORY,
     SERVICE_SET_ASYNCIO_DEBUG,
     SERVICE_START,
+    SERVICE_START_AUDITING_EVENTS,
     SERVICE_START_LOG_OBJECT_SOURCES,
     SERVICE_START_LOG_OBJECTS,
+    SERVICE_STOP_AUDITING_EVENTS,
     SERVICE_STOP_LOG_OBJECT_SOURCES,
     SERVICE_STOP_LOG_OBJECTS,
 )
@@ -37,6 +44,13 @@ from homeassistant.exceptions import HomeAssistantError
 from homeassistant.util import dt as dt_util
 
 from tests.common import MockConfigEntry, async_fire_time_changed
+
+
+@pytest.fixture
+def sys_addaudithook():
+    """Mock sys.addaudithook."""
+    with patch("sys.addaudithook") as mock_addaudithook:
+        yield mock_addaudithook
 
 
 async def test_basic_usage(hass: HomeAssistant, tmp_path: Path) -> None:
@@ -447,3 +461,183 @@ async def test_set_asyncio_debug(
 
     assert await hass.config_entries.async_unload(entry.entry_id)
     await hass.async_block_till_done()
+
+
+async def test_auditing_events_mocked(
+    hass: HomeAssistant, caplog: pytest.LogCaptureFixture, sys_addaudithook
+) -> None:
+    """Test service calls for events auditing with mocked sys.addaudithook."""
+
+    entry = MockConfigEntry(domain=DOMAIN)
+    entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert hass.services.has_service(DOMAIN, SERVICE_START_AUDITING_EVENTS)
+
+    assert hass.data[DOMAIN][AUDITING_HOOK_ADDED] is False
+
+    # Capture 'open' in non-verbose mode
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_START_AUDITING_EVENTS,
+        {CONF_EVENTS: ["open"], CONF_VERBOSE: False},
+        blocking=True,
+    )
+    assert hass.data[DOMAIN][AUDITING_HOOK_ADDED] is True
+    event_open = hass.data[DOMAIN][AUDITED_EVENTS].get("open")
+    assert event_open.verbose is False
+    assert event_open.filter is None
+    assert "Enabling auditing for event open" in caplog.text
+    assert sys_addaudithook.call_count == 1
+
+    # Add 'import' in verbose mode
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_START_AUDITING_EVENTS,
+        {CONF_EVENTS: ["import"], CONF_VERBOSE: True},
+        blocking=True,
+    )
+    event_import = hass.data[DOMAIN][AUDITED_EVENTS].get("import")
+    assert event_import.verbose is True
+    assert event_import.filter is None
+    event_open = hass.data[DOMAIN][AUDITED_EVENTS].get("open")
+    assert event_open.verbose is False
+    assert event_open.filter is None
+    assert "Enabling verbose auditing for event import" in caplog.text
+    assert sys_addaudithook.call_count == 1
+
+    caplog.clear()
+
+    # Add 'exec' in verbose mode and set 'open' to verbose
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_START_AUDITING_EVENTS,
+        {CONF_EVENTS: ["exec", "open"], CONF_VERBOSE: True},
+        blocking=True,
+    )
+    event_exec = hass.data[DOMAIN][AUDITED_EVENTS].get("exec")
+    assert event_exec.verbose is True
+    assert event_exec.filter is None
+    event_import = hass.data[DOMAIN][AUDITED_EVENTS].get("import")
+    assert event_import.verbose is True
+    assert event_import.filter is None
+    event_open = hass.data[DOMAIN][AUDITED_EVENTS].get("open")
+    assert event_open.verbose is True
+    assert event_open.filter is None
+    assert sys_addaudithook.call_count == 1
+    assert "Enabling verbose auditing for event exec" in caplog.text
+    assert "Enabling verbose auditing for event open" in caplog.text
+
+    caplog.clear()
+
+    # Add filtering for 'exec' in verbose mode
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_START_AUDITING_EVENTS,
+        {CONF_EVENTS: ["exec"], CONF_VERBOSE: True, CONF_FILTER: "test"},
+        blocking=True,
+    )
+    event_exec = hass.data[DOMAIN][AUDITED_EVENTS].get("exec")
+    assert event_exec.filter == "test"
+    assert hass.data[DOMAIN][AUDITED_EVENTS].get("import").filter is None
+    assert hass.data[DOMAIN][AUDITED_EVENTS].get("open").filter is None
+    assert sys_addaudithook.call_count == 1
+    assert "Enabling verbose auditing for event exec with filter 'test'" in caplog.text
+
+    caplog.clear()
+
+    # Remove 'exec' from audited events
+    await hass.services.async_call(
+        DOMAIN, SERVICE_STOP_AUDITING_EVENTS, {CONF_EVENTS: ["exec"]}, blocking=True
+    )
+    assert "exec" not in hass.data[DOMAIN][AUDITED_EVENTS]
+    assert len(hass.data[DOMAIN][AUDITED_EVENTS]) == 2
+    assert "Disabling auditing for event exec" in caplog.text
+
+    caplog.clear()
+    assert await hass.config_entries.async_unload(entry.entry_id)
+    await hass.async_block_till_done()
+    assert "Python auditing hook cannot be removed" in caplog.text
+
+
+async def test_auditing_events(
+    hass: HomeAssistant, caplog: pytest.LogCaptureFixture
+) -> None:
+    """Test actual Python events auditing."""
+    entry = MockConfigEntry(domain=DOMAIN)
+    entry.add_to_hass(hass)
+
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert hass.services.has_service(DOMAIN, SERVICE_START_AUDITING_EVENTS)
+
+    assert hass.data[DOMAIN][AUDITING_HOOK_ADDED] is False
+
+    # Capture 'exec' in non-verbose mode
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_START_AUDITING_EVENTS,
+        {CONF_EVENTS: ["exec"], CONF_VERBOSE: False},
+        blocking=True,
+    )
+    assert hass.data[DOMAIN][AUDITING_HOOK_ADDED] is True
+    assert hass.data[DOMAIN][AUDITED_EVENTS].get("exec").verbose is False
+    assert "Enabling auditing for event exec" in caplog.text
+    caplog.clear()
+    exec("1+1")  # noqa: S102
+    assert "Audited event: exec" in caplog.text
+    assert "traceback" not in caplog.text
+
+    # Test verbose capture
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_START_AUDITING_EVENTS,
+        {CONF_EVENTS: ["exec"], CONF_VERBOSE: True},
+        blocking=True,
+    )
+    caplog.clear()
+    exec("1+1")  # noqa: S102
+    assert "Audited event: exec" in caplog.text
+    assert "traceback" in caplog.text
+    assert 'exec("1+1")' in caplog.text
+
+    # Test filtered capture (matched filter)
+    # the argument of exec is: <code object <module> at 0x7f2d5e9c9140, file "<string>", line 1>
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_START_AUDITING_EVENTS,
+        {
+            CONF_EVENTS: ["exec"],
+            CONF_VERBOSE: True,
+            CONF_FILTER: 'file "<string>", line 1',
+        },
+        blocking=True,
+    )
+    caplog.clear()
+    exec("1+1")  # noqa: S102
+    assert "Audited event: exec" in caplog.text
+    assert "traceback" in caplog.text
+    assert 'exec("1+1")' in caplog.text
+
+    # Test filtered capture (unmatched filter)
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_START_AUDITING_EVENTS,
+        {CONF_EVENTS: ["exec"], CONF_VERBOSE: True, CONF_FILTER: "random filter"},
+        blocking=True,
+    )
+    caplog.clear()
+    exec("1+1")  # noqa: S102
+    assert "Audited event: exec" not in caplog.text
+
+    assert await hass.config_entries.async_unload(entry.entry_id)
+    await hass.async_block_till_done()
+    assert "Python auditing hook cannot be removed" in caplog.text
+
+    # Nothing should be captured after unloading the integration
+    caplog.clear()
+    exec("1+1")  # noqa: S102
+    assert "Audited event" not in caplog.text
