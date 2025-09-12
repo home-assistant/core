62326d7b506e55b4750da6ab9f17f816856fd17f
Joostlek <joostlek@outlook.com>
2025-09-12 22:19:31 +0200

    Fix

--- tests/components/nederlandse_spoorwegen/test_config_flow.py
+++ tests/components/nederlandse_spoorwegen/test_config_flow.py
@@ -44,6 +44,44 @@ async def test_full_flow(
     assert len(mock_setup_entry.mock_calls) == 1
 
 
+async def test_creating_route(
+    hass: HomeAssistant,
+    mock_nsapi: AsyncMock,
+    mock_setup_entry: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test creating a route after setting up the main config entry."""
+    mock_config_entry.add_to_hass(hass)
+    assert len(mock_config_entry.subentries) == 1
+    result = await hass.config_entries.subentries.async_init(
+        (mock_config_entry.entry_id, "route"), context={"source": SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user"
+    assert not result["errors"]
+
+    result = await hass.config_entries.subentries.async_configure(
+        result["flow_id"],
+        user_input={
+            CONF_FROM: "ASD",
+            CONF_TO: "RTD",
+            CONF_VIA: "HT",
+            CONF_NAME: "Home to Work",
+            CONF_TIME: "08:30",
+        },
+    )
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == "Home to Work"
+    assert result["data"] == {
+        CONF_FROM: "ASD",
+        CONF_TO: "RTD",
+        CONF_VIA: "HT",
+        CONF_NAME: "Home to Work",
+        CONF_TIME: "08:30",
+    }
+    assert len(mock_config_entry.subentries) == 2
+
+
 @pytest.mark.parametrize(
     ("exception", "expected_error"),
     [
@@ -87,6 +125,23 @@ async def test_flow_exceptions(
     assert result["title"] == "Nederlandse Spoorwegen"
     assert result["data"] == {CONF_API_KEY: API_KEY}
     assert len(mock_setup_entry.mock_calls) == 1
+
+
+async def test_fetching_stations_failed(
+    hass: HomeAssistant,
+    mock_nsapi: AsyncMock,
+    mock_setup_entry: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+) -> None:
+    """Test creating a route after setting up the main config entry."""
+    mock_config_entry.add_to_hass(hass)
+    assert len(mock_config_entry.subentries) == 1
+    mock_nsapi.get_stations.side_effect = RequestsConnectionError("Unexpected error")
+    result = await hass.config_entries.subentries.async_init(
+        (mock_config_entry.entry_id, "route"), context={"source": SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "cannot_connect"
 
 
 async def test_already_configured(
