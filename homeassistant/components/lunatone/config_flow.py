b2afd62167445096c97efa2994b1fa554a4c8fe2
David Bugl <bugl@lunatone.com>
2025-09-12 05:21:39 +0000

    Remove all DALI device scan related parts

--- homeassistant/components/lunatone/config_flow.py
+++ homeassistant/components/lunatone/config_flow.py
@@ -1,60 +1,27 @@
 """Config flow for Lunatone."""
 
-import asyncio
-from enum import StrEnum
 import logging
 from typing import Any, Final
 
 import aiohttp
-from lunatone_rest_api_client import Auth, DALIScan, Info
-from lunatone_rest_api_client.models import StartScanData
+from lunatone_rest_api_client import Auth, Info
 import voluptuous as vol
 
 from homeassistant.config_entries import (
     SOURCE_RECONFIGURE,
-    SOURCE_USER,
     ConfigFlow,
     ConfigFlowResult,
 )
 from homeassistant.const import CONF_URL
-from homeassistant.helpers import config_validation as cv, selector
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-
-class DALIDeviceScanMethod(StrEnum):
-    """DALI device scan methods."""
-
-    CURRENT_DEVICE_LIST = "current_device_list"
-    SYSTEM_EXTENSION = "system_extension"
-    NEW_INSTALLATION = "new_installation"
-
-
-DEFAULT_DALI_DEVICE_SCAN_METHOD: Final = DALIDeviceScanMethod.CURRENT_DEVICE_LIST
-DALI_DEVICE_SCAN_METHODS: Final[list[str]] = [
-    option.value for option in DALIDeviceScanMethod
-]
-DALI_DEVICE_SCAN_TIMEOUT_MINUTES: Final = 20
-
 DATA_SCHEMA: Final[vol.Schema] = vol.Schema(
     {vol.Required(CONF_URL, default="http://"): cv.string},
-)
-CONF_SCAN_METHOD: Final = "device_scan_method"
-DALI_SCAN_SCHEMA: Final[vol.Schema] = vol.Schema(
-    {
-        vol.Required(
-            CONF_SCAN_METHOD, default=DEFAULT_DALI_DEVICE_SCAN_METHOD
-        ): selector.SelectSelector(
-            selector.SelectSelectorConfig(
-                options=DALI_DEVICE_SCAN_METHODS,
-                translation_key=CONF_SCAN_METHOD,
-                mode=selector.SelectSelectorMode.LIST,
-            ),
-        ),
-    }
 )
 
 
@@ -69,7 +36,6 @@ class LunatoneConfigFlow(ConfigFlow, domain=DOMAIN):
         self._url: str | None = None
         self._name: str | None = None
         self._serial_number: int | None = None
-        self._dali_device_scan_task: asyncio.Task | None = None
 
     @property
     def _title(self) -> str:
@@ -102,16 +68,18 @@ class LunatoneConfigFlow(ConfigFlow, domain=DOMAIN):
                     self._name = info.name
                     self._serial_number = info.serial_number
                     await self.async_set_unique_id(str(self._serial_number))
-                    if self.source == SOURCE_USER:
-                        self._abort_if_unique_id_configured()
-                        return await self.async_step_dali()
-                    self._abort_if_unique_id_mismatch()
-                    return self.async_update_reload_and_abort(
-                        self._get_reconfigure_entry(),
-                        data_updates=data,
+                    if self.source == SOURCE_RECONFIGURE:
+                        self._abort_if_unique_id_mismatch()
+                        return self.async_update_reload_and_abort(
+                            self._get_reconfigure_entry(),
+                            data_updates=data,
+                            title=self._title,
+                        )
+                    self._abort_if_unique_id_configured()
+                    return self.async_create_entry(
                         title=self._title,
+                        data={CONF_URL: self._url},
                     )
-
         return self.async_show_form(
             step_id="user",
             data_schema=DATA_SCHEMA,
@@ -119,89 +87,8 @@ class LunatoneConfigFlow(ConfigFlow, domain=DOMAIN):
             last_step=bool(self.source == SOURCE_RECONFIGURE),
         )
 
-    async def async_step_dali(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Handle a flow that does DALI related stuff."""
-        step_id = "dali"
-
-        if self._dali_device_scan_task:
-            next_step_id = None
-            if self._dali_device_scan_task.cancelled():
-                _LOGGER.debug("DALI device scan timeout")
-                next_step_id = "scan_timeout"
-            elif self._dali_device_scan_task.done():
-                next_step_id = "finish"
-            if next_step_id:
-                self._dali_device_scan_task = None
-                return self.async_show_progress_done(next_step_id=next_step_id)
-
-        if user_input is not None:
-            method = user_input[CONF_SCAN_METHOD]
-            self._dali_device_scan_task = self.hass.async_create_task(
-                self._async_start_dali_device_scan(method)
-            )
-            return self.async_show_progress(
-                step_id=step_id,
-                progress_action="device_scan",
-                progress_task=self._dali_device_scan_task,
-            )
-        return self.async_show_form(
-            step_id=step_id,
-            data_schema=DALI_SCAN_SCHEMA,
-            last_step=False,
-        )
-
-    async def async_step_scan_timeout(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Handle DALI device scan timeout config flow."""
-        return self.async_abort(reason="dali_device_scan_timeout")
-
-    async def async_step_finish(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Handle finishing the config flow."""
-        if user_input is not None:
-            return self.async_create_entry(
-                title=self._title,
-                data={CONF_URL: self._url},
-            )
-        return self.async_show_form()
-
     async def async_step_reconfigure(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle a reconfiguration flow initialized by the user."""
         return await self.async_step_user(user_input)
-
-    async def _async_start_dali_device_scan(self, method: DALIDeviceScanMethod) -> None:
-        auth = Auth(
-            session=async_get_clientsession(self.hass),
-            base_url=self._url,
-        )
-        scan = DALIScan(auth)
-        await scan.async_cancel()
-        if not await self._async_is_dali_device_scan_done(scan):
-            raise asyncio.CancelledError
-
-        if method == DALIDeviceScanMethod.SYSTEM_EXTENSION:
-            start_scan_data = StartScanData()
-        elif method == DALIDeviceScanMethod.NEW_INSTALLATION:
-            start_scan_data = StartScanData(newInstallation=True)
-        else:
-            start_scan_data = StartScanData(noAddressing=True)
-
-        await scan.async_start(start_scan_data)
-        if not await self._async_is_dali_device_scan_done(scan):
-            raise asyncio.CancelledError
-
-    async def _async_is_dali_device_scan_done(self, scan: DALIScan) -> bool:
-        await asyncio.sleep(1)
-        for _ in range(DALI_DEVICE_SCAN_TIMEOUT_MINUTES * 60):
-            await scan.async_update()
-            if not scan.is_busy:
-                return True
-            self.async_update_progress(scan.progress / 100)
-            await asyncio.sleep(1)
-        return False
--- tests/components/lunatone/test_config_flow.py
+++ tests/components/lunatone/test_config_flow.py
@@ -1,16 +1,9 @@
 """Define tests for the Lunatone config flow."""
 
-from collections.abc import Generator
-from unittest.mock import AsyncMock, PropertyMock, patch
+from unittest.mock import AsyncMock
 
 import aiohttp
-import pytest
 
-from homeassistant.components.lunatone.config_flow import (
-    CONF_SCAN_METHOD,
-    DALI_DEVICE_SCAN_TIMEOUT_MINUTES,
-    DALIDeviceScanMethod,
-)
 from homeassistant.components.lunatone.const import DOMAIN
 from homeassistant.config_entries import SOURCE_USER
 from homeassistant.const import CONF_URL
@@ -20,31 +13,9 @@ from homeassistant.data_entry_flow import FlowResultType
 from tests.common import MockConfigEntry
 
 
-@pytest.fixture
-def mock_lunatone_scan(mock_lunatone_auth: AsyncMock) -> Generator[AsyncMock]:
-    """Mock a Lunatone scan object."""
-    with patch(
-        "homeassistant.components.lunatone.config_flow.DALIScan", autospec=True
-    ) as mock_scan:
-        scan = mock_scan.return_value
-        scan._auth = mock_lunatone_auth
-        scan.is_busy = False
-        yield scan
-
-
-@pytest.mark.parametrize(
-    "scan_method",
-    [
-        DALIDeviceScanMethod.CURRENT_DEVICE_LIST,
-        DALIDeviceScanMethod.SYSTEM_EXTENSION,
-        DALIDeviceScanMethod.NEW_INSTALLATION,
-    ],
-)
-async def test_full_flow_with_scan(
+async def test_full_flow(
     hass: HomeAssistant,
-    scan_method: DALIDeviceScanMethod,
     mock_lunatone_info: AsyncMock,
-    mock_lunatone_scan: AsyncMock,
     mock_setup_entry: AsyncMock,
 ) -> None:
     """Test full flow with all DALI device scan methods."""
@@ -59,22 +30,6 @@ async def test_full_flow_with_scan(
         result["flow_id"],
         {CONF_URL: "http://10.0.0.131"},
     )
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "dali"
-
-    with patch("homeassistant.components.lunatone.config_flow.asyncio.sleep"):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {CONF_SCAN_METHOD: scan_method},
-        )
-        assert result["type"] is FlowResultType.SHOW_PROGRESS
-        assert result["step_id"] == "dali"
-
-    result = await hass.config_entries.flow.async_configure(result["flow_id"])
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "finish"
-
-    result = await hass.config_entries.flow.async_configure(result["flow_id"], {})
     assert result["type"] is FlowResultType.CREATE_ENTRY
     assert result["title"] == "Test 12345"
     assert result["data"] == {CONF_URL: "http://10.0.0.131"}
@@ -100,106 +55,6 @@ async def test_full_flow_fail_because_of_missing_device_infos(
     assert result["type"] is FlowResultType.FORM
     assert result["step_id"] == "user"
     assert result["errors"] == {"base": "missing_device_info"}
-
-
-@pytest.mark.parametrize(
-    "scan_method",
-    [
-        DALIDeviceScanMethod.CURRENT_DEVICE_LIST,
-        DALIDeviceScanMethod.SYSTEM_EXTENSION,
-        DALIDeviceScanMethod.NEW_INSTALLATION,
-    ],
-)
-async def test_full_flow_fail_on_timeout_before_starting_dali_device_scan(
-    hass: HomeAssistant,
-    scan_method: DALIDeviceScanMethod,
-    mock_lunatone_info: AsyncMock,
-    mock_lunatone_scan: AsyncMock,
-) -> None:
-    """Test full flow."""
-    mock_lunatone_scan.is_busy = True
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "user"
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {CONF_URL: "http://10.0.0.131"},
-    )
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "dali"
-
-    with patch("homeassistant.components.lunatone.config_flow.asyncio.sleep"):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input={CONF_SCAN_METHOD: scan_method}
-        )
-        assert result["type"] is FlowResultType.SHOW_PROGRESS
-        assert result["step_id"] == "dali"
-
-        mock_lunatone_scan.async_cancel.assert_called_once()
-        mock_lunatone_scan.async_update.assert_called()
-        assert (
-            mock_lunatone_scan.async_update.call_count
-            == DALI_DEVICE_SCAN_TIMEOUT_MINUTES * 60
-        )
-
-    result = await hass.config_entries.flow.async_configure(result["flow_id"])
-    assert result["type"] is FlowResultType.ABORT
-    assert result["reason"] == "dali_device_scan_timeout"
-
-
-@pytest.mark.parametrize(
-    "scan_method",
-    [
-        DALIDeviceScanMethod.CURRENT_DEVICE_LIST,
-        DALIDeviceScanMethod.SYSTEM_EXTENSION,
-        DALIDeviceScanMethod.NEW_INSTALLATION,
-    ],
-)
-async def test_full_flow_fail_on_timeout_after_starting_dali_device_scan(
-    hass: HomeAssistant,
-    scan_method: DALIDeviceScanMethod,
-    mock_lunatone_info: AsyncMock,
-    mock_lunatone_scan: AsyncMock,
-) -> None:
-    """Test full flow."""
-    type(mock_lunatone_scan).is_busy = PropertyMock(
-        side_effect=[False] + [True] * DALI_DEVICE_SCAN_TIMEOUT_MINUTES * 60
-    )
-
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": SOURCE_USER}
-    )
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "user"
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-        {CONF_URL: "http://10.0.0.131"},
-    )
-    assert result["type"] is FlowResultType.FORM
-    assert result["step_id"] == "dali"
-
-    with patch("homeassistant.components.lunatone.config_flow.asyncio.sleep"):
-        result = await hass.config_entries.flow.async_configure(
-            result["flow_id"], user_input={CONF_SCAN_METHOD: scan_method}
-        )
-        assert result["type"] is FlowResultType.SHOW_PROGRESS
-        assert result["step_id"] == "dali"
-
-        mock_lunatone_scan.async_cancel.assert_called_once()
-        mock_lunatone_scan.async_update.assert_called()
-        assert (
-            mock_lunatone_scan.async_update.call_count
-            == DALI_DEVICE_SCAN_TIMEOUT_MINUTES * 60 + 1
-        )
-
-    result = await hass.config_entries.flow.async_configure(result["flow_id"])
-    assert result["type"] is FlowResultType.ABORT
-    assert result["reason"] == "dali_device_scan_timeout"
 
 
 async def test_device_already_configured(
