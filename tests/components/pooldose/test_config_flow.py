77519d2748648b307c5d2207c0e780360893c1e8
Lukas <12813107+lmaertin@users.noreply.github.com>
2025-09-13 18:52:04 +0000

    push full test coverage of conflig_flow

--- tests/components/pooldose/test_config_flow.py
+++ tests/components/pooldose/test_config_flow.py
@@ -258,3 +258,20 @@ async def test_dhcp_flow(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> No
     assert result["title"] == "PoolDose TEST123456789"
     assert result["data"] == {CONF_HOST: "192.168.0.123"}
     assert result["result"].unique_id == "TEST123456789"
+
+
+async def test_dhcp_no_serial_number(
+    hass: HomeAssistant, mock_pooldose_client: AsyncMock, mock_setup_entry: AsyncMock
+) -> None:
+    """Test that the DHCP flow aborts if no serial number is found."""
+    mock_pooldose_client.device_info = {"NAME": "Pool Device", "MODEL": "POOL DOSE"}
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": SOURCE_DHCP},
+        data=DhcpServiceInfo(
+            ip="192.168.0.123", hostname="kommspot", macaddress="a4e57caabbcc"
+        ),
+    )
+    assert result["type"] is FlowResultType.ABORT
+    assert result["reason"] == "no_serial_number"
