e40ecdfb000934d12f86fe753eeca04fe2e2c228
Shay Levy <levyshay1@gmail.com>
2025-09-14 23:43:37 +0300

    Remove Shelly empty sub-devices (#152251)

--- homeassistant/components/shelly/utils.py
+++ homeassistant/components/shelly/utils.py
@@ -884,3 +884,27 @@ def remove_stale_blu_trv_devices(
 
         LOGGER.debug("Removing stale BLU TRV device %s", device.name)
         dev_reg.async_update_device(device.id, remove_config_entry_id=entry.entry_id)
+
+
+@callback
+def remove_empty_sub_devices(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Remove sub devices without entities."""
+    dev_reg = dr.async_get(hass)
+    entity_reg = er.async_get(hass)
+
+    devices = dev_reg.devices.get_devices_for_config_entry_id(entry.entry_id)
+
+    for device in devices:
+        if not device.via_device_id:
+            # Device is not a sub-device, skip
+            continue
+
+        if er.async_entries_for_device(entity_reg, device.id, True):
+            # Device has entities, skip
+            continue
+
+        if any(identifier[0] == DOMAIN for identifier in device.identifiers):
+            LOGGER.debug("Removing empty sub-device %s", device.name)
+            dev_reg.async_update_device(
+                device.id, remove_config_entry_id=entry.entry_id
+            )
