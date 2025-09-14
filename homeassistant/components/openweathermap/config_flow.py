f6536ab7b3ebd3f122c1d3fa63e75060d81b401d
Christopher Fenner <Christopher.Fenner@me.com>
2025-09-14 19:57:28 +0200

    add schema

--- homeassistant/components/openweathermap/config_flow.py
+++ homeassistant/components/openweathermap/config_flow.py
@@ -32,6 +32,24 @@ from .const import (
 )
 from .utils import build_data_and_options, validate_api_key
 
+USER_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_API_KEY): str,
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
+        vol.Optional(CONF_LATITUDE): cv.latitude,
+        vol.Optional(CONF_LONGITUDE): cv.longitude,
+        vol.Optional(CONF_MODE, default=DEFAULT_OWM_MODE): vol.In(OWM_MODES),
+        vol.Optional(CONF_LANGUAGE): vol.In(LANGUAGES),
+    }
+)
+
+OPTIONS_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_MODE): vol.In(OWM_MODES),
+        vol.Optional(CONF_LANGUAGE): vol.In(LANGUAGES),
+    }
+)
+
 
 class OpenWeatherMapConfigFlow(ConfigFlow, domain=DOMAIN):
     """Config flow for OpenWeatherMap."""
@@ -50,49 +68,42 @@ class OpenWeatherMapConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle a flow initialized by the user."""
         errors = {}
         description_placeholders = {}
+        ha_data = {
+            CONF_LATITUDE: self.hass.config.latitude,
+            CONF_LONGITUDE: self.hass.config.longitude,
+            CONF_LANGUAGE: self.hass.config.language,
+        }
+
+        schema_data = {
+            **ha_data,
+            **user_input,
+        }
 
         if user_input is not None:
-            latitude = user_input[CONF_LATITUDE]
-            longitude = user_input[CONF_LONGITUDE]
-            mode = user_input[CONF_MODE]
+            latitude = schema_data[CONF_LATITUDE]
+            longitude = schema_data[CONF_LONGITUDE]
+            mode = schema_data[CONF_MODE]
 
             await self.async_set_unique_id(f"{latitude}-{longitude}")
             self._abort_if_unique_id_configured()
 
             errors, description_placeholders = await validate_api_key(
-                user_input[CONF_API_KEY], mode
+                schema_data[CONF_API_KEY], mode
             )
 
             if not errors:
-                data, options = build_data_and_options(user_input)
+                data, options = build_data_and_options(schema_data)
                 return self.async_create_entry(
-                    title=user_input[CONF_NAME], data=data, options=options
+                    title=schema_data[CONF_NAME], data=data, options=options
                 )
 
         description_placeholders["doc_url"] = (
             "https://www.home-assistant.io/integrations/openweathermap/"
         )
 
-        schema = vol.Schema(
-            {
-                vol.Required(CONF_API_KEY): str,
-                vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
-                vol.Optional(
-                    CONF_LATITUDE, default=self.hass.config.latitude
-                ): cv.latitude,
-                vol.Optional(
-                    CONF_LONGITUDE, default=self.hass.config.longitude
-                ): cv.longitude,
-                vol.Optional(CONF_MODE, default=DEFAULT_OWM_MODE): vol.In(OWM_MODES),
-                vol.Optional(CONF_LANGUAGE, default=DEFAULT_LANGUAGE): vol.In(
-                    LANGUAGES
-                ),
-            }
-        )
-
         return self.async_show_form(
             step_id="user",
-            data_schema=schema,
+            data_schema=self.add_suggested_values_to_schema(USER_SCHEMA, schema_data),
             errors=errors,
             description_placeholders=description_placeholders,
         )
@@ -108,25 +119,17 @@ class OpenWeatherMapOptionsFlow(OptionsFlow):
 
         return self.async_show_form(
             step_id="init",
-            data_schema=self._get_options_schema(),
-        )
-
-    def _get_options_schema(self):
-        return vol.Schema(
-            {
-                vol.Optional(
-                    CONF_MODE,
-                    default=self.config_entry.options.get(
+            data_schema=self.add_suggested_values_to_schema(
+                OPTIONS_SCHEMA,
+                {
+                    CONF_MODE: self.config_entry.options.get(
                         CONF_MODE,
                         self.config_entry.data.get(CONF_MODE, DEFAULT_OWM_MODE),
                     ),
-                ): vol.In(OWM_MODES),
-                vol.Optional(
-                    CONF_LANGUAGE,
-                    default=self.config_entry.options.get(
+                    CONF_LANGUAGE: self.config_entry.options.get(
                         CONF_LANGUAGE,
                         self.config_entry.data.get(CONF_LANGUAGE, DEFAULT_LANGUAGE),
                     ),
-                ): vol.In(LANGUAGES),
-            }
+                },
+            ),
         )
