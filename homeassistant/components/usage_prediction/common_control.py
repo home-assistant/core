6da864f803ca86163a40f7ab1b28a7271169e104
Paulus Schoutsen <balloob@gmail.com>
2025-09-15 17:09:41 -0400

    Apply suggestions from code review
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- homeassistant/components/usage_prediction/common_control.py
+++ homeassistant/components/usage_prediction/common_control.py
@@ -232,7 +232,7 @@ def _fetch_and_process_data(session: Session, user_id: str) -> EntityUsagePredic
 
 
 def _fetch_with_session(
-    hass: HomeAssistant, fetch_func: Callable[[Session], EntityUsagePredictions], *args
+    hass: HomeAssistant, fetch_func: Callable[[Session], EntityUsagePredictions], *args: object
 ) -> EntityUsagePredictions:
     """Execute a fetch function with a database session."""
     with session_scope(hass=hass, read_only=True) as session:
