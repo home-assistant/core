d7a9529ac8613d3527ade7a9007f421c752ede80
Erik Montnemery <erik@montnemery.com>
2025-09-15 13:01:24 +0200

    Update tests/components/synology_dsm/test_sensor.py

--- tests/components/synology_dsm/test_sensor.py
+++ tests/components/synology_dsm/test_sensor.py
@@ -210,86 +210,75 @@ async def test_external_usb_new_device(
 ) -> None:
     """Test Synology DSM USB adding new device."""
 
+    expected_sensors_disk_1 = {
+        "sensor.nas_meontheinternet_com_usb_disk_1_status": ("normal", {}),
+        "sensor.nas_meontheinternet_com_usb_disk_1_partition_1_partition_size": (
+            "14901.998046875",
+            {},
+        ),
+        "sensor.nas_meontheinternet_com_usb_disk_1_partition_1_partition_used_space": (
+            "5803.1650390625",
+            {},
+        ),
+        "sensor.nas_meontheinternet_com_usb_disk_1_partition_1_partition_used": (
+            "38.9",
+            {},
+        ),
+    }
+    expected_sensors_disk_2 = {
+        "sensor.nas_meontheinternet_com_usb_disk_2_status": ("normal", {}),
+        "sensor.nas_meontheinternet_com_usb_disk_2_partition_1_partition_size": (
+            "14901.998046875",
+            {
+                "device_class": "data_size",
+                "state_class": "measurement",
+                "unit_of_measurement": "GiB",
+                "attribution": "Data provided by Synology",
+            },
+        ),
+        "sensor.nas_meontheinternet_com_usb_disk_2_partition_1_partition_used_space": (
+            "5803.1650390625",
+            {
+                "device_class": "data_size",
+                "state_class": "measurement",
+                "unit_of_measurement": "GiB",
+                "attribution": "Data provided by Synology",
+            },
+        ),
+        "sensor.nas_meontheinternet_com_usb_disk_2_partition_1_partition_used": (
+            "38.9",
+            {
+                "state_class": "measurement",
+                "unit_of_measurement": "%",
+                "attribution": "Data provided by Synology",
+            },
+        ),
+    }
+
+    # Initial check of existing sensors
+    for sensor_id, (expected_state, expected_attrs) in expected_sensors_disk_1.items():
+        sensor = hass.states.get(sensor_id)
+        assert sensor is not None
+        assert sensor.state == expected_state
+        for attr_key, attr_value in expected_attrs.items():
+            assert sensor.attributes[attr_key] == attr_value
+    for sensor_id in expected_sensors_disk_2:
+        assert hass.states.get(sensor_id) is None
+
     # Mock the get_devices method to simulate a USB disk being added
     setup_dsm_with_usb.external_usb.get_devices = mock_dsm_external_usb_devices_usb2()
     # Coordinator refresh
     await setup_dsm_with_usb.mock_entry.runtime_data.coordinator_central.async_request_refresh()
     await hass.async_block_till_done()
 
-    # Test USB Disk 1 status sensor
-    sensor = hass.states.get("sensor.nas_meontheinternet_com_usb_disk_1_status")
-    assert sensor is not None
-    assert sensor.state == "normal"
-
-    # Test USB Disk 1 Partition 1 size sensor
-    sensor = hass.states.get(
-        "sensor.nas_meontheinternet_com_usb_disk_1_partition_1_partition_size"
-    )
-    assert sensor is not None
-    assert sensor.state == "14901.998046875"
-
-    # Test USB Disk 1 Partition 1 partition used space sensor
-    sensor = hass.states.get(
-        "sensor.nas_meontheinternet_com_usb_disk_1_partition_1_partition_used_space"
-    )
-    assert sensor is not None
-    assert sensor.state == "5803.1650390625"
-
-    # Test USB Disk 1 Partition 1 partition used sensor
-    sensor = hass.states.get(
-        "sensor.nas_meontheinternet_com_usb_disk_1_partition_1_partition_used"
-    )
-    assert sensor is not None
-    assert sensor.state == "38.9"
-
-    # Test USB Disk 2 status sensor
-    sensor = hass.states.get("sensor.nas_meontheinternet_com_usb_disk_2_status")
-    assert sensor is not None
-    assert sensor.state == "normal"
-
-    # Test USB Disk 2 Partition 1 partition size sensor
-    sensor = hass.states.get(
-        "sensor.nas_meontheinternet_com_usb_disk_2_partition_1_partition_size"
-    )
-    assert sensor is not None
-    assert sensor.state == "14901.998046875"
-    assert (
-        sensor.attributes["friendly_name"]
-        == "nas.meontheinternet.com (USB Disk 2 Partition 1) Partition size"
-    )
-    assert sensor.attributes["device_class"] == "data_size"
-    assert sensor.attributes["state_class"] == "measurement"
-    assert sensor.attributes["unit_of_measurement"] == "GiB"
-    assert sensor.attributes["attribution"] == "Data provided by Synology"
-
-    # Test USB Disk 2 Partition 1 partition used space sensor
-    sensor = hass.states.get(
-        "sensor.nas_meontheinternet_com_usb_disk_2_partition_1_partition_used_space"
-    )
-    assert sensor is not None
-    assert sensor.state == "5803.1650390625"
-    assert (
-        sensor.attributes["friendly_name"]
-        == "nas.meontheinternet.com (USB Disk 2 Partition 1) Partition used space"
-    )
-    assert sensor.attributes["device_class"] == "data_size"
-    assert sensor.attributes["state_class"] == "measurement"
-    assert sensor.attributes["unit_of_measurement"] == "GiB"
-    assert sensor.attributes["attribution"] == "Data provided by Synology"
-
-    # Test USB Disk 2 Partition 1 partition used sensor
-    sensor = hass.states.get(
-        "sensor.nas_meontheinternet_com_usb_disk_2_partition_1_partition_used"
-    )
-    assert sensor is not None
-    assert sensor.state == "38.9"
-    assert (
-        sensor.attributes["friendly_name"]
-        == "nas.meontheinternet.com (USB Disk 2 Partition 1) Partition used"
-    )
-    assert sensor.attributes["state_class"] == "measurement"
-    assert sensor.attributes["unit_of_measurement"] == "%"
-    assert sensor.attributes["attribution"] == "Data provided by Synology"
+    for sensor_id, (expected_state, expected_attrs) in chain(
+        expected_sensors_disk_1.items(), expected_sensors_disk_2.items()
+    ):
+        sensor = hass.states.get(sensor_id)
+        assert sensor is not None
+        assert sensor.state == expected_state
+        for attr_key, attr_value in expected_attrs.items():
+            assert sensor.attributes[attr_key] == attr_value
 
 
 async def test_no_external_usb(
