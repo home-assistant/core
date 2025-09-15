c4391859a3629ea89ce42dd29fec66ff3c8f84e4
Franck Nijhof <git@frenck.dev>
2025-09-15 02:48:29 +0200

    Refactor template engine: Extract math & statistical functions into MathExtension (#152266)

--- homeassistant/helpers/template/extensions/__init__.py
+++ homeassistant/helpers/template/extensions/__init__.py
@@ -2,5 +2,6 @@
 
 from .base64 import Base64Extension
 from .crypto import CryptoExtension
+from .math import MathExtension
 
-__all__ = ["Base64Extension", "CryptoExtension"]
+__all__ = ["Base64Extension", "CryptoExtension", "MathExtension"]
