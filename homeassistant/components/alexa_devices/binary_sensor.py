c1f9084ec6cb6e598a9b97305060ea08e9dc33d4
Simone Chemelli <simone.chemelli@gmail.com>
2025-09-16 19:07:08 +0000

    remove entities cleanup as requested

--- homeassistant/components/alexa_devices/binary_sensor.py
+++ homeassistant/components/alexa_devices/binary_sensor.py
@@ -18,9 +18,7 @@ from homeassistant.components.binary_sensor import (
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
-import homeassistant.helpers.entity_registry as er
 
-from .const import _LOGGER, DOMAIN
 from .coordinator import AmazonConfigEntry
 from .entity import AmazonEntity
 from .utils import async_update_unique_id
@@ -59,41 +57,6 @@ BINARY_SENSORS: Final = (
 )
 
 
-DEPRECATED_BINARY_SENSORS: Final = (
-    AmazonBinarySensorEntityDescription(
-        key="bluetooth",
-        entity_category=EntityCategory.DIAGNOSTIC,
-        translation_key="bluetooth",
-        is_on_fn=lambda device, key: False,
-    ),
-    AmazonBinarySensorEntityDescription(
-        key="babyCryDetectionState",
-        translation_key="baby_cry_detection",
-        is_on_fn=lambda device, key: False,
-    ),
-    AmazonBinarySensorEntityDescription(
-        key="beepingApplianceDetectionState",
-        translation_key="beeping_appliance_detection",
-        is_on_fn=lambda device, key: False,
-    ),
-    AmazonBinarySensorEntityDescription(
-        key="coughDetectionState",
-        translation_key="cough_detection",
-        is_on_fn=lambda device, key: False,
-    ),
-    AmazonBinarySensorEntityDescription(
-        key="dogBarkDetectionState",
-        translation_key="dog_bark_detection",
-        is_on_fn=lambda device, key: False,
-    ),
-    AmazonBinarySensorEntityDescription(
-        key="waterSoundsDetectionState",
-        translation_key="water_sounds_detection",
-        is_on_fn=lambda device, key: False,
-    ),
-)
-
-
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: AmazonConfigEntry,
@@ -102,8 +65,6 @@ async def async_setup_entry(
     """Set up Alexa Devices binary sensors based on a config entry."""
 
     coordinator = entry.runtime_data
-
-    entity_registry = er.async_get(hass)
 
     # Replace unique id for "detectionState" binary sensor
     await async_update_unique_id(
@@ -120,16 +81,6 @@ async def async_setup_entry(
         for serial_num in coordinator.data
         if sensor_desc.is_supported(coordinator.data[serial_num], sensor_desc.key)
     )
-
-    # Clean up deprecated sensors
-    for sensor_desc in DEPRECATED_BINARY_SENSORS:
-        for serial_num in coordinator.data:
-            unique_id = f"{serial_num}-{sensor_desc.key}"
-            if entity_id := entity_registry.async_get_entity_id(
-                BINARY_SENSOR_DOMAIN, DOMAIN, unique_id
-            ):
-                _LOGGER.debug("Removing deprecated entity %s", entity_id)
-                entity_registry.async_remove(entity_id)
 
 
 class AmazonBinarySensorEntity(AmazonEntity, BinarySensorEntity):
