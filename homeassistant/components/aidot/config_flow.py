be6a72802550da442d10f126e594461e2bf00d65
s1eedz <ldxapp2016@gmail.com>
2025-09-18 17:54:15 +0800

    fix

--- homeassistant/components/aidot/config_flow.py
+++ homeassistant/components/aidot/config_flow.py
@@ -5,12 +5,13 @@ from __future__ import annotations
 from typing import Any
 
 from aidot.client import AidotClient
-from aidot.const import CONF_LOGIN_INFO, DEFAULT_COUNTRY_NAME, SUPPORTED_COUNTRY_NAMES
+from aidot.const import CONF_LOGIN_INFO, DEFAULT_COUNTRY_CODE, SUPPORTED_COUNTRY_CODES
 from aidot.exceptions import AidotUserOrPassIncorrect
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
-from homeassistant.const import CONF_COUNTRY, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_COUNTRY_CODE, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.helpers import selector
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
@@ -27,7 +28,7 @@ class AidotConfigFlow(ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             client = AidotClient(
                 session=async_get_clientsession(self.hass),
-                country_name=user_input[CONF_COUNTRY],
+                country_code=user_input[CONF_COUNTRY_CODE],
                 username=user_input[CONF_USERNAME],
                 password=user_input[CONF_PASSWORD],
             )
@@ -40,7 +41,7 @@ class AidotConfigFlow(ConfigFlow, domain=DOMAIN):
 
             if not errors:
                 return self.async_create_entry(
-                    title=f"{user_input[CONF_USERNAME]} {user_input[CONF_COUNTRY]}",
+                    title=f"{user_input[CONF_USERNAME]} {user_input[CONF_COUNTRY_CODE]}",
                     data={
                         CONF_LOGIN_INFO: login_info,
                     },
@@ -49,9 +50,13 @@ class AidotConfigFlow(ConfigFlow, domain=DOMAIN):
         DATA_SCHEMA = vol.Schema(
             {
                 vol.Required(
-                    CONF_COUNTRY,
-                    default=DEFAULT_COUNTRY_NAME,
-                ): vol.In(SUPPORTED_COUNTRY_NAMES),
+                    CONF_COUNTRY_CODE,
+                    default=DEFAULT_COUNTRY_CODE,
+                ): selector.CountrySelector(
+                    selector.CountrySelectorConfig(
+                        countries=SUPPORTED_COUNTRY_CODES,
+                    )
+                ),
                 vol.Required(CONF_USERNAME): str,
                 vol.Required(CONF_PASSWORD): str,
             }
