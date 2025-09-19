912580d9be18230dea0addb50d46fc7204471ac4
J. Nick Koston <nick@home-assistant.io>
2025-09-19 07:45:14 -0600

    add missing test

--- tests/components/sensor/test_init.py
+++ tests/components/sensor/test_init.py
@@ -2158,6 +2158,7 @@ async def test_non_numeric_device_class_with_unit_of_measurement(
         SensorDeviceClass.PM1,
         SensorDeviceClass.PM10,
         SensorDeviceClass.PM25,
+        SensorDeviceClass.PM4,
         SensorDeviceClass.POWER_FACTOR,
         SensorDeviceClass.POWER,
         SensorDeviceClass.PRECIPITATION_INTENSITY,
