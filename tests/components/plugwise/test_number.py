d427a08e3fe2cbf7050d60e0ed30f8d144756687
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-09-20 09:33:21 +0200

    Implement snapshot testing for Plugwise number tests

--- tests/components/plugwise/test_number.py
+++ tests/components/plugwise/test_number.py
@@ -12,81 +12,23 @@ from homeassistant.components.number import (
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ServiceValidationError
+from homeassistant.helpers import entity_registry as er
+from syrupy.assertion import SnapshotAssertion
 
-from tests.common import MockConfigEntry
-
-
-@pytest.mark.parametrize("chosen_env", ["anna_heatpump_heating"], indirect=True)
-@pytest.mark.parametrize("cooling_present", [True], indirect=True)
-async def test_anna_number_entities(
-    hass: HomeAssistant, mock_smile_anna: MagicMock, init_integration: MockConfigEntry
-) -> None:
-    """Test creation of a number."""
-    state = hass.states.get("number.opentherm_maximum_boiler_temperature_setpoint")
-    assert state
-    assert float(state.state) == 60.0
+from tests.common import MockConfigEntry, snapshot_platform
 
 
-@pytest.mark.parametrize("chosen_env", ["anna_heatpump_heating"], indirect=True)
-@pytest.mark.parametrize("cooling_present", [True], indirect=True)
-async def test_anna_max_boiler_temp_change(
-    hass: HomeAssistant, mock_smile_anna: MagicMock, init_integration: MockConfigEntry
-) -> None:
-    """Test changing of number entities."""
-    await hass.services.async_call(
-        NUMBER_DOMAIN,
-        SERVICE_SET_VALUE,
-        {
-            ATTR_ENTITY_ID: "number.opentherm_maximum_boiler_temperature_setpoint",
-            ATTR_VALUE: 65,
-        },
-        blocking=True,
-    )
-
-    assert mock_smile_anna.set_number.call_count == 1
-    mock_smile_anna.set_number.assert_called_with(
-        "1cbf783bb11e4a7c8a6843dee3a86927", "maximum_boiler_temperature", 65.0
-    )
-
-
-@pytest.mark.parametrize("chosen_env", ["m_adam_heating"], indirect=True)
-@pytest.mark.parametrize("cooling_present", [False], indirect=True)
-async def test_adam_dhw_setpoint_change(
+@pytest.mark.parametrize("platforms", [(NUMBER_DOMAIN,)])
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_adam_number_entities(
     hass: HomeAssistant,
-    mock_smile_adam_heat_cool: MagicMock,
-    init_integration: MockConfigEntry,
+    mock_smile_adam: MagicMock,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+    setup_platform: MockConfigEntry,
 ) -> None:
-    """Test changing of number entities."""
-    state = hass.states.get("number.opentherm_domestic_hot_water_setpoint")
-    assert state
-    assert float(state.state) == 60.0
-
-    await hass.services.async_call(
-        NUMBER_DOMAIN,
-        SERVICE_SET_VALUE,
-        {
-            ATTR_ENTITY_ID: "number.opentherm_domestic_hot_water_setpoint",
-            ATTR_VALUE: 55,
-        },
-        blocking=True,
-    )
-
-    assert mock_smile_adam_heat_cool.set_number.call_count == 1
-    mock_smile_adam_heat_cool.set_number.assert_called_with(
-        "056ee145a816487eaa69243c3280f8bf", "max_dhw_temperature", 55.0
-    )
-
-
-async def test_adam_temperature_offset(
-    hass: HomeAssistant, mock_smile_adam: MagicMock, init_integration: MockConfigEntry
-) -> None:
-    """Test creation of the temperature_offset number."""
-    state = hass.states.get("number.zone_thermostat_jessie_temperature_offset")
-    assert state
-    assert float(state.state) == 0.0
-    assert state.attributes.get("min") == -2.0
-    assert state.attributes.get("max") == 2.0
-    assert state.attributes.get("step") == 0.1
+    """Test Adam number snapshot."""
+    await snapshot_platform(hass, entity_registry, snapshot, setup_platform.entry_id)
 
 
 async def test_adam_temperature_offset_change(
@@ -123,3 +65,68 @@ async def test_adam_temperature_offset_out_of_bounds_change(
             },
             blocking=True,
         )
+
+
+@pytest.mark.parametrize("chosen_env", ["m_adam_heating"], indirect=True)
+@pytest.mark.parametrize("cooling_present", [False], indirect=True)
+async def test_adam_dhw_setpoint_change(
+    hass: HomeAssistant,
+    mock_smile_adam_heat_cool: MagicMock,
+    init_integration: MockConfigEntry,
+) -> None:
+    """Test changing of number entities."""
+    state = hass.states.get("number.opentherm_domestic_hot_water_setpoint")
+    assert state
+    assert float(state.state) == 60.0
+
+    await hass.services.async_call(
+        NUMBER_DOMAIN,
+        SERVICE_SET_VALUE,
+        {
+            ATTR_ENTITY_ID: "number.opentherm_domestic_hot_water_setpoint",
+            ATTR_VALUE: 55,
+        },
+        blocking=True,
+    )
+
+    assert mock_smile_adam_heat_cool.set_number.call_count == 1
+    mock_smile_adam_heat_cool.set_number.assert_called_with(
+        "056ee145a816487eaa69243c3280f8bf", "max_dhw_temperature", 55.0
+    )
+
+
+@pytest.mark.parametrize("chosen_env", ["anna_heatpump_heating"], indirect=True)
+@pytest.mark.parametrize("cooling_present", [True], indirect=True)
+@pytest.mark.parametrize("platforms", [(NUMBER_DOMAIN,)])
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_anna_number_entities(
+    hass: HomeAssistant,
+    mock_smile_anna: MagicMock,
+    snapshot: SnapshotAssertion,
+    entity_registry: er.EntityRegistry,
+    setup_platform: MockConfigEntry,
+) -> None:
+    """Test Anna number snapshot."""
+    await snapshot_platform(hass, entity_registry, snapshot, setup_platform.entry_id)
+
+
+@pytest.mark.parametrize("chosen_env", ["anna_heatpump_heating"], indirect=True)
+@pytest.mark.parametrize("cooling_present", [True], indirect=True)
+async def test_anna_max_boiler_temp_change(
+    hass: HomeAssistant, mock_smile_anna: MagicMock, init_integration: MockConfigEntry
+) -> None:
+    """Test changing of number entities."""
+    await hass.services.async_call(
+        NUMBER_DOMAIN,
+        SERVICE_SET_VALUE,
+        {
+            ATTR_ENTITY_ID: "number.opentherm_maximum_boiler_temperature_setpoint",
+            ATTR_VALUE: 65,
+        },
+        blocking=True,
+    )
+
+    assert mock_smile_anna.set_number.call_count == 1
+    mock_smile_anna.set_number.assert_called_with(
+        "1cbf783bb11e4a7c8a6843dee3a86927", "maximum_boiler_temperature", 65.0
+    )
