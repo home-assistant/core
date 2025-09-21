09d9e8869f3dcec09127a4a55b0abfae17c90da6
Luke <conway220@gmail.com>
2025-09-21 02:16:06 +0000

    Set loading map to be cloud based.

--- homeassistant/components/roborock/coordinator.py
+++ homeassistant/components/roborock/coordinator.py
@@ -425,7 +425,7 @@ class RoborockDataUpdateCoordinator(DataUpdateCoordinator[DeviceProp]):
         for map_flag in map_flags:
             if map_flag != cur_map:
                 # Only change the map and sleep if we have multiple maps.
-                await self.api.load_multi_map(map_flag)
+                await self.cloud_api.load_multi_map(map_flag)
                 self.current_map = map_flag
                 # We cannot get the map until the roborock servers fully process the
                 # map change.
@@ -443,7 +443,7 @@ class RoborockDataUpdateCoordinator(DataUpdateCoordinator[DeviceProp]):
             # Set the map back to the map the user previously had selected so that it
             # does not change the end user's app.
             # Only needs to happen when we changed maps above.
-            await self.api.load_multi_map(cur_map)
+            await self.cloud_api.load_multi_map(cur_map)
             self.current_map = cur_map
 
 
