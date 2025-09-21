79356244f75e44960bc7dfd80b441322c161ee68
andy <andy.thaller@googlemail.com>
2025-09-21 17:29:28 +0000

    Add test for message cancelling

--- tests/components/pushover/test_notify.py
+++ tests/components/pushover/test_notify.py
@@ -9,13 +9,13 @@ from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
 
+RECEIPT = "1758472711"
+
 
 @pytest.fixture(autouse=False)
 def mock_pushover():
     """Mock pushover."""
-    with patch(
-        "pushover_complete.PushoverAPI._generic_post", return_value={}
-    ) as mock_generic_post:
+    with patch("pushover_complete.PushoverAPI._generic_post") as mock_generic_post:
         yield mock_generic_post
 
 
@@ -23,7 +23,26 @@ def mock_pushover():
 def mock_send_message():
     """Patch PushoverAPI.send_message for TTL test."""
     with patch(
-        "homeassistant.components.pushover.notify.PushoverAPI.send_message"
+        "homeassistant.components.pushover.notify.PushoverAPI.send_message",
+    ) as mock:
+        yield mock
+
+
+@pytest.fixture
+def mock_send_message_prio2():
+    """Patch PushoverAPI.send_message for cancel test."""
+    with patch(
+        "homeassistant.components.pushover.notify.PushoverAPI.send_message",
+        return_value={"receipt": RECEIPT},
+    ) as mock:
+        yield mock
+
+
+@pytest.fixture
+def mock_cancel_receipt():
+    """Patch PushoverAPI.cancel_receipt for cancel test."""
+    with patch(
+        "homeassistant.components.pushover.notify.PushoverAPI.cancel_receipt"
     ) as mock:
         yield mock
 
@@ -69,3 +88,61 @@ async def test_send_message(
         html=0,
         ttl=900,
     )
+
+
+async def test_cancel_message(
+    hass: HomeAssistant,
+    mock_pushover: MagicMock,
+    mock_send_message_prio2: MagicMock,
+    mock_cancel_receipt: MagicMock,
+) -> None:
+    """Test cancelling a message."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "name": "pushover",
+            "api_key": "API_KEY",
+            "user_key": "USER_KEY",
+        },
+    )
+    entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    await hass.services.async_call(
+        "notify",
+        "pushover",
+        {
+            "message": "Hello Emergency",
+            "data": {"ttl": 900, "priority": 2, "retry": 30, "tags": "tag1"},
+        },
+        blocking=True,
+    )
+
+    mock_send_message_prio2.assert_called_once_with(
+        user="USER_KEY",
+        message="Hello Emergency",
+        device="",
+        title="Home Assistant",
+        url=None,
+        url_title=None,
+        image=None,
+        priority=2,
+        retry=30,
+        expire=None,
+        callback_url=None,
+        timestamp=None,
+        sound=None,
+        html=0,
+        ttl=900,
+    )
+
+    await hass.services.async_call(
+        "pushover",
+        "cancel",
+        {"tag": "tag1"},
+        blocking=True,
+    )
+
+    mock_cancel_receipt.assert_called_once_with(RECEIPT)
