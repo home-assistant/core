ba3553db2b41fe831667ce27ab7e468737f2f7b3
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-09-22 16:16:36 +0200

    Move test to the test_init.py file

--- tests/components/accuweather/test_init.py
+++ tests/components/accuweather/test_init.py
@@ -1,5 +1,6 @@
 """Test init of AccuWeather integration."""
 
+from datetime import timedelta
 from unittest.mock import AsyncMock
 
 from accuweather import ApiError, InvalidApiKeyError
@@ -133,6 +134,37 @@ async def test_auth_error(
     mock_config_entry = await init_integration(hass)
 
     assert mock_config_entry.state is ConfigEntryState.SETUP_ERROR
+
+    flows = hass.config_entries.flow.async_progress()
+    assert len(flows) == 1
+
+    flow = flows[0]
+    assert flow.get("step_id") == "reauth_confirm"
+    assert flow.get("handler") == DOMAIN
+
+    assert "context" in flow
+    assert flow["context"].get("source") == SOURCE_REAUTH
+    assert flow["context"].get("entry_id") == mock_config_entry.entry_id
+
+
+async def test_auth_error_whe_polling_data(
+    hass: HomeAssistant,
+    freezer: FrozenDateTimeFactory,
+    mock_accuweather_client: AsyncMock,
+) -> None:
+    """Test authentication error when polling data."""
+    mock_config_entry = await init_integration(hass)
+
+    assert mock_config_entry.state is ConfigEntryState.LOADED
+
+    mock_accuweather_client.async_get_current_conditions.side_effect = (
+        InvalidApiKeyError("Invalid API Key")
+    )
+    freezer.tick(timedelta(minutes=10))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
+
+    assert mock_config_entry.state is ConfigEntryState.LOADED
 
     flows = hass.config_entries.flow.async_progress()
     assert len(flows) == 1
