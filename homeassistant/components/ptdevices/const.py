86ae947ff0bac7f86cdc8ca73307da54a2dea1b1
Matthew Gibson <matthew.gibzon@gmail.com>
2025-09-22 19:54:27 +0000

    Increased Update interval to 30s, was 5

--- homeassistant/components/ptdevices/const.py
+++ homeassistant/components/ptdevices/const.py
@@ -4,4 +4,4 @@ from datetime import timedelta
 
 DOMAIN = "ptdevices"
 DEFAULT_URL = "https://www.ptdevices.com/token/v1/device/"
-UPDATE_INTERVAL = timedelta(seconds=5)
+UPDATE_INTERVAL = timedelta(seconds=30)
