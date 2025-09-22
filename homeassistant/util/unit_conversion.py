f821996025239eaf9d7b3b74011f974d4601beaa
Abílio Costa <abmantis@users.noreply.github.com>
2025-09-22 15:19:31 +0100

    Fix unitless converter missing valid units (#152665)

--- homeassistant/util/unit_conversion.py
+++ homeassistant/util/unit_conversion.py
@@ -742,6 +742,8 @@ class UnitlessRatioConverter(BaseUnitConverter):
     }
     VALID_UNITS = {
         None,
+        CONCENTRATION_PARTS_PER_BILLION,
+        CONCENTRATION_PARTS_PER_MILLION,
         PERCENTAGE,
     }
 
