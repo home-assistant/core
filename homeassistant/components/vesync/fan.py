97c387c4d4495779cea72ce89290e6e5b1c8179c
cdnninja <jaydenaphillips@gmail.com>
2025-09-23 00:08:32 +0000

    Correct enum values

--- homeassistant/components/vesync/fan.py
+++ homeassistant/components/vesync/fan.py
@@ -141,7 +141,7 @@ class VeSyncFanHA(VeSyncBaseEntity, FanEntity):
             attr["active_time"] = self.device.state.active_time
 
         if hasattr(self.device.state, "display_status"):
-            attr["display_status"] = self.device.state.display_status
+            attr["display_status"] = self.device.state.display_status.value
 
         if hasattr(self.device.state, "child_lock"):
             attr["child_lock"] = self.device.state.child_lock
