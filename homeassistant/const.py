fb30df66129c07704917e9ad5b0b864104f4c060
Franck Nijhof <git@frenck.dev>
2025-09-24 23:55:32 +0200

    Bump version to 2025.11.0dev0 (#152915)

--- homeassistant/const.py
+++ homeassistant/const.py
@@ -25,7 +25,7 @@ if TYPE_CHECKING:
 
 APPLICATION_NAME: Final = "HomeAssistant"
 MAJOR_VERSION: Final = 2025
-MINOR_VERSION: Final = 10
+MINOR_VERSION: Final = 11
 PATCH_VERSION: Final = "0.dev0"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"
