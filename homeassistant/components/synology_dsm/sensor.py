bdd0b74d5109c88f302508e2949a1595e0b1408c
Patrick <lezmaka@gmail.com>
2025-09-24 05:26:22 -0400

    Enhance Synology DSM handling of external USB drives (#145943)
    Co-authored-by: Michael <35783820+mib1185@users.noreply.github.com>

--- homeassistant/components/synology_dsm/sensor.py
+++ homeassistant/components/synology_dsm/sensor.py
@@ -523,6 +523,22 @@ class SynoDSMExternalUSBSensor(SynologyDSMDeviceEntity, SynoDSMSensor):
 
         return attr  # type: ignore[no-any-return]
 
+    @property
+    def available(self) -> bool:
+        """Return True if entity is available."""
+        external_usb = self._api.external_usb
+        assert external_usb is not None
+        if "device" in self.entity_description.key:
+            for device in external_usb.get_devices.values():
+                if device.device_name == self._device_id:
+                    return super().available
+        elif "partition" in self.entity_description.key:
+            for device in external_usb.get_devices.values():
+                for partition in device.device_partitions.values():
+                    if partition.partition_title == self._device_id:
+                        return super().available
+        return False
+
 
 class SynoDSMInfoSensor(SynoDSMSensor):
     """Representation a Synology information sensor."""
