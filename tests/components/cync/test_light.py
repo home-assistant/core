61ea1b8cac2f234403f8cc920b19a20c5c069d50
Joostlek <joostlek@outlook.com>
2025-09-24 17:47:57 +0200

    Fix

--- tests/components/cync/test_light.py
+++ tests/components/cync/test_light.py
@@ -2,16 +2,15 @@
 
 from syrupy.assertion import SnapshotAssertion
 
-from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
 from . import setup_integration
 
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, snapshot_platform
 
 
-async def test_light_attributes(
+async def test_entities(
     hass: HomeAssistant,
     mock_config_entry: MockConfigEntry,
     snapshot: SnapshotAssertion,
@@ -21,8 +20,4 @@ async def test_light_attributes(
 
     await setup_integration(hass, mock_config_entry)
 
-    entities = hass.states.async_all(Platform.LIGHT)
-    for entity_state in entities:
-        entity_entry = entity_registry.async_get(entity_state.entity_id)
-        assert entity_entry == snapshot(name=f"{entity_entry.entity_id}-entry")
-        assert entity_state == snapshot(name=f"{entity_entry.entity_id}-state")
+    await snapshot_platform(hass, entity_registry, snapshot, mock_config_entry.entry_id)
