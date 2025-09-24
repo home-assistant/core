34536503ac6403c381f3e513e247ac21ea5b4aa1
Ravaka Razafimanantsoa <contact@ravaka.dev>
2025-09-24 12:39:03 +0000

    Enhance test for async setup entry by adding unload state assertion

--- tests/components/route_b_smart_meter/test_init.py
+++ tests/components/route_b_smart_meter/test_init.py
@@ -13,4 +13,7 @@ async def test_async_setup_entry_success(
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
     assert mock_config_entry.state is ConfigEntryState.LOADED
+
+    assert await hass.config_entries.async_unload(mock_config_entry.entry_id)
     await hass.async_block_till_done()
+    assert mock_config_entry.state is ConfigEntryState.NOT_LOADED
