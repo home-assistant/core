37a0af1fd4c69697b13fd02c57b037d47dae1eb9
mistic100 <contact@git.strangeplanet.fr>
2025-09-24 23:09:39 +0200

    Add remove item intent for todo component

--- homeassistant/components/todo/intent.py
+++ homeassistant/components/todo/intent.py
@@ -11,12 +11,14 @@ from . import TodoItem, TodoItemStatus, TodoListEntity
 from .const import DATA_COMPONENT, DOMAIN
 
 INTENT_LIST_ADD_ITEM = "HassListAddItem"
+INTENT_LIST_REMOVE_ITEM = "HassListRemoveItem"
 INTENT_LIST_COMPLETE_ITEM = "HassListCompleteItem"
 
 
 async def async_setup_intents(hass: HomeAssistant) -> None:
     """Set up the todo intents."""
     intent.async_register(hass, ListAddItemIntent())
+    intent.async_register(hass, ListRemoveItemIntent())
     intent.async_register(hass, ListCompleteItemIntent())
 
 
@@ -63,6 +65,72 @@ class ListAddItemIntent(intent.IntentHandler):
         await target_list.async_create_todo_item(
             TodoItem(summary=item, status=TodoItemStatus.NEEDS_ACTION)
         )
+
+        response: intent.IntentResponse = intent_obj.create_response()
+        response.async_set_results(
+            [
+                intent.IntentResponseTarget(
+                    type=intent.IntentResponseTargetType.ENTITY,
+                    name=list_name,
+                    id=match_result.states[0].entity_id,
+                )
+            ]
+        )
+        return response
+
+
+class ListRemoveItemIntent(intent.IntentHandler):
+    """Handle LisRemoveItemIntent intents."""
+
+    intent_type = INTENT_LIST_REMOVE_ITEM
+    description = "Remove one or more items from a todo list"
+    slot_schema = {
+        vol.Required("item"): intent.non_empty_string,
+        vol.Required("name"): intent.non_empty_string,
+    }
+    platforms = {DOMAIN}
+
+    async def async_handle(self, intent_obj: intent.Intent) -> intent.IntentResponse:
+        """Handle the intent."""
+        hass = intent_obj.hass
+
+        slots = self.async_validate_slots(intent_obj.slots)
+        item = slots["item"]["value"]
+        list_name = slots["name"]["value"]
+
+        target_list: TodoListEntity | None = None
+
+        # Find matching list
+        match_constraints = intent.MatchTargetsConstraints(
+            name=list_name, domains=[DOMAIN], assistant=intent_obj.assistant
+        )
+        match_result = intent.async_match_targets(hass, match_constraints)
+        if not match_result.is_match:
+            raise intent.MatchFailedError(
+                result=match_result, constraints=match_constraints
+            )
+
+        target_list = hass.data[DATA_COMPONENT].get_entity(
+            match_result.states[0].entity_id
+        )
+        if target_list is None:
+            raise intent.IntentHandleError(
+                f"No to-do list: {list_name}", "list_not_found"
+            )
+
+        # Find items in list
+        matching_item_uids = [
+            todo_item.uid
+            for todo_item in target_list.todo_items or ()
+            if item in (todo_item.uid, todo_item.summary) and todo_item.uid is not None
+        ]
+        if not matching_item_uids:
+            raise intent.IntentHandleError(
+                f"Item '{item}' not found on list", "item_not_found"
+            )
+
+        # Remove items
+        await target_list.async_delete_todo_items(matching_item_uids)
 
         response: intent.IntentResponse = intent_obj.create_response()
         response.async_set_results(
