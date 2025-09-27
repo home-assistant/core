cc5cb794ded760073c48dfaee1a9b7711336f70a
Erwin Douna <e.douna@gmail.com>
2025-09-27 07:33:24 +0000

    Fix

--- tests/components/portainer/test_config_flow.py
+++ tests/components/portainer/test_config_flow.py
@@ -11,7 +11,7 @@ import pytest
 
 from homeassistant.components.portainer.const import DOMAIN
 from homeassistant.config_entries import SOURCE_USER
-from homeassistant.const import CONF_API_TOKEN, CONF_URL
+from homeassistant.const import CONF_API_TOKEN, CONF_URL, CONF_VERIFY_SSL
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 
@@ -22,6 +22,7 @@ from tests.common import MockConfigEntry
 MOCK_USER_SETUP = {
     CONF_URL: "https://127.0.0.1:9000/",
     CONF_API_TOKEN: "test_api_token",
+    CONF_VERIFY_SSL: True,
 }
 
 
