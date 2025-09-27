9f5e6caf700e12d5d47ece61c8a73f6c2b91176d
G Johansson <goran.johansson@shiftit.se>
2025-09-27 16:48:36 +0000

    Use automatic reload options flow in switch_as_x

--- homeassistant/components/switch_as_x/__init__.py
+++ homeassistant/components/switch_as_x/__init__.py
@@ -69,7 +69,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             source_entity_removed=source_entity_removed,
         )
     )
-    entry.async_on_unload(entry.add_update_listener(config_entry_update_listener))
 
     await hass.config_entries.async_forward_entry_setups(
         entry, (entry.options[CONF_TARGET_DOMAIN],)
@@ -111,11 +110,6 @@ async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) ->
     )
 
     return True
-
-
-async def config_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Update listener, called when the config entry options are changed."""
-    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
