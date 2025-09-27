996adcf2e2810bd72d27ef44d881618f635f9f5e
G Johansson <goran.johansson@shiftit.se>
2025-09-27 16:57:03 +0000

    Use automatic reload options flow in history_stats

--- homeassistant/components/history_stats/__init__.py
+++ homeassistant/components/history_stats/__init__.py
@@ -86,7 +86,6 @@ async def async_setup_entry(
     )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(update_listener))
 
     return True
 
@@ -130,8 +129,3 @@ async def async_unload_entry(
 ) -> bool:
     """Unload History stats config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
