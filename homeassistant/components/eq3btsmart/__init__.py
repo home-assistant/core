b65403b7887466eda124ad4500c1467a33006b70
Lennard Beers <l.beers@outlook.de>
2025-09-27 03:06:50 +0000

    Only disconnect when connected, remove obsolete update listener

--- homeassistant/components/eq3btsmart/__init__.py
+++ homeassistant/components/eq3btsmart/__init__.py
@@ -37,8 +37,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: Eq3ConfigEntry) -> bool:
     coordinator = Eq3Coordinator(hass, entry, device)
     entry.runtime_data = Eq3ConfigEntryData(coordinator)
 
-    entry.async_on_unload(entry.add_update_listener(update_listener))
-
     await coordinator.async_config_entry_first_refresh()
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
@@ -48,11 +46,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: Eq3ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: Eq3ConfigEntry) -> bool:
     """Handle config entry unload."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        await entry.runtime_data.coordinator.thermostat.async_disconnect()
+        if entry.runtime_data.coordinator.thermostat.is_connected:
+            await entry.runtime_data.coordinator.thermostat.async_disconnect()
 
     return unload_ok
-
-
-async def update_listener(hass: HomeAssistant, entry: Eq3ConfigEntry) -> None:
-    """Handle config entry update."""
-    await hass.config_entries.async_reload(entry.entry_id)
