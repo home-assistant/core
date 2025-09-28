7f4841059055263f3115b3e0832b1f72166cceae
Erwin Douna <e.douna@gmail.com>
2025-09-28 20:37:41 +0000

    Feedback

--- homeassistant/components/portainer/diagnostics.py
+++ homeassistant/components/portainer/diagnostics.py
@@ -11,8 +11,41 @@ from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.device_registry import DeviceEntry
 
 from . import PortainerConfigEntry
+from .coordinator import PortainerCoordinator
 
 TO_REDACT = [CONF_API_TOKEN]
+
+
+def _serialize_coordinator(coordinator: PortainerCoordinator) -> dict[str, Any]:
+    """Serialize coordinator data into a JSON-safe structure."""
+
+    serialized_endpoints: list[dict[str, Any]] = []
+    for endpoint_id, endpoint_data in coordinator.data.items():
+        serialized_endpoints.append(
+            {
+                "id": endpoint_id,
+                "name": endpoint_data.name,
+                "endpoint": {
+                    "status": endpoint_data.endpoint.status,
+                    "url": endpoint_data.endpoint.url,
+                    "public_url": endpoint_data.endpoint.public_url,
+                },
+                "containers": [
+                    {
+                        "id": container.id,
+                        "names": list(container.names)
+                        if container.names
+                        else [],  # Docker names are built-upon asliases as well. Here I include them all. Maybe I should only allow the first record that represents the "main" name?
+                        "image": container.image,
+                        "state": container.state,
+                        "status": container.status,
+                    }
+                    for container in endpoint_data.containers.values()
+                ],
+            }
+        )
+
+    return {"endpoints": serialized_endpoints}
 
 
 async def async_get_config_entry_diagnostics(
@@ -22,7 +55,7 @@ async def async_get_config_entry_diagnostics(
 
     return {
         "config_entry": async_redact_data(config_entry.as_dict(), TO_REDACT),
-        "coordinator_data": config_entry.runtime_data,
+        "coordinator": _serialize_coordinator(config_entry.runtime_data),
     }
 
 
