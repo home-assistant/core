c53b5f8c8f122643e18db2539e464134f77b43b9
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2025-09-29 13:33:05 +0200

    Use `chip.clusters` instead of hard coded values

--- tests/components/matter/test_climate.py
+++ tests/components/matter/test_climate.py
@@ -162,7 +162,7 @@ async def test_thermostat_base(
     assert state.attributes["temperature"] == 20
 
 
-@pytest.mark.parametrize("node_fixture", ["tado_smart_radiator_thermostat_x"])
+@pytest.mark.parametrize("node_fixture", ["thermostat"])
 async def test_thermostat_humidity(
     hass: HomeAssistant,
     matter_client: MagicMock,
@@ -170,30 +170,49 @@ async def test_thermostat_humidity(
 ) -> None:
     """Test thermostat humidity attribute and state updates."""
     # test entity attributes
-    state = hass.states.get("climate.smart_radiator_thermostat_x")
+    state = hass.states.get("climate.longan_link_hvac")
     assert state
-    assert state.attributes["current_humidity"] is not None
+
+    measured_value = clusters.RelativeHumidityMeasurement.Attributes.MeasuredValue
 
     # test current humidity update from device
-    set_node_attribute(matter_node, 1, 1029, 0, 4567)
+    set_node_attribute(
+        matter_node,
+        1,
+        measured_value.cluster_id,
+        measured_value.attribute_id,
+        1234,
+    )
     await trigger_subscription_callback(hass, matter_client)
-    state = hass.states.get("climate.smart_radiator_thermostat_x")
+    state = hass.states.get("climate.longan_link_hvac")
     assert state
-    assert state.attributes["current_humidity"] == 45.67
+    assert state.attributes["current_humidity"] == 12.34
 
     # test current humidity update from device with zero value
-    set_node_attribute(matter_node, 1, 1029, 0, 0)
+    set_node_attribute(
+        matter_node,
+        1,
+        measured_value.cluster_id,
+        measured_value.attribute_id,
+        0,
+    )
     await trigger_subscription_callback(hass, matter_client)
-    state = hass.states.get("climate.smart_radiator_thermostat_x")
+    state = hass.states.get("climate.longan_link_hvac")
     assert state
     assert state.attributes["current_humidity"] == 0.0
 
     # test current humidity update from device with None value
-    set_node_attribute(matter_node, 1, 1029, 0, None)
+    set_node_attribute(
+        matter_node,
+        1,
+        measured_value.cluster_id,
+        measured_value.attribute_id,
+        None,
+    )
     await trigger_subscription_callback(hass, matter_client)
-    state = hass.states.get("climate.smart_radiator_thermostat_x")
+    state = hass.states.get("climate.longan_link_hvac")
     assert state
-    assert state.attributes.get("current_humidity") is None
+    assert "current_humidity" not in state.attributes
 
 
 @pytest.mark.parametrize("node_fixture", ["thermostat"])
