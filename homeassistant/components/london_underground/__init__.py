463d4228b8af3ff73c77fcdf67ea827843faf22f
HarvsG <11440490+HarvsG@users.noreply.github.com>
2025-09-30 19:50:04 +0000

    removes redundant reload

--- homeassistant/components/london_underground/__init__.py
+++ homeassistant/components/london_underground/__init__.py
@@ -31,8 +31,3 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry."""
-    await hass.config_entries.async_reload(entry.entry_id)
