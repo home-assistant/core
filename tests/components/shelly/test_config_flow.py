574b62c13be6f8872c49f5bd54d44e8bedfbaa3b
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-10-01 17:14:32 +0200

    Add missing test

--- tests/components/shelly/test_config_flow.py
+++ tests/components/shelly/test_config_flow.py
@@ -65,6 +65,15 @@ DISCOVERY_INFO_WITH_MAC = ZeroconfServiceInfo(
     properties={ATTR_PROPERTIES_ID: "shelly1pm-AABBCCDDEEFF"},
     type="mock_type",
 )
+DISCOVERY_INFO_WRONG_NAME = ZeroconfServiceInfo(
+    ip_address=ip_address("1.1.1.1"),
+    ip_addresses=[ip_address("1.1.1.1")],
+    hostname="mock_hostname",
+    name="Shelly Plus 2PM [DDEEFF]",
+    port=None,
+    properties={ATTR_PROPERTIES_ID: "shelly2pm-AABBCCDDEEFF"},
+    type="mock_type",
+)
 
 
 @pytest.mark.parametrize(
@@ -1751,3 +1760,53 @@ async def test_zeroconf_rejects_ipv6(hass: HomeAssistant) -> None:
     )
     assert result["type"] is FlowResultType.ABORT
     assert result["reason"] == "ipv6_not_supported"
+
+
+async def test_zeroconf_wrong_device_name(
+    hass: HomeAssistant,
+    mock_rpc_device: Mock,
+    mock_setup_entry: AsyncMock,
+    mock_setup: AsyncMock,
+) -> None:
+    """Test zeroconf."""
+
+    with patch(
+        "homeassistant.components.shelly.config_flow.get_info",
+        return_value={
+            "mac": "test-mac",
+            "model": MODEL_PLUS_2PM,
+            "auth": False,
+            "gen": 2,
+        },
+    ):
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            data=DISCOVERY_INFO_WRONG_NAME,
+            context={"source": config_entries.SOURCE_ZEROCONF},
+        )
+        assert result["type"] is FlowResultType.FORM
+        assert result["errors"] == {}
+        context = next(
+            flow["context"]
+            for flow in hass.config_entries.flow.async_progress()
+            if flow["flow_id"] == result["flow_id"]
+        )
+        assert context["title_placeholders"]["name"] == "Shelly Plus 2PM [DDEEFF]"
+        assert context["confirm_only"] is True
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {},
+    )
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+    assert result["title"] == "Test name"
+    assert result["data"] == {
+        CONF_HOST: "1.1.1.1",
+        CONF_MODEL: MODEL_PLUS_2PM,
+        CONF_SLEEP_PERIOD: 0,
+        CONF_GEN: 2,
+    }
+    assert result["result"].unique_id == "test-mac"
+    assert len(mock_setup.mock_calls) == 1
+    assert len(mock_setup_entry.mock_calls) == 1
