04c9d55e0343b70d7e841d7830a5693762f01210
epenet <6771947+epenet@users.noreply.github.com>
2025-10-01 10:13:59 +0000

    Add quirk for g1cp07dsqnbdbbki

--- homeassistant/components/tuya/quirks/__init__.py
+++ homeassistant/components/tuya/quirks/__init__.py
@@ -17,6 +17,22 @@ __all__ = [
 TUYA_QUIRKS_REGISTRY = QuirksRegistry()
 
 (
+    # This model has percent_state and percent_control but percent_state never
+    # gets updated - force percent_control instead
+    DeviceQuirk()
+    .applies_to(category="cl", product_id="g1cp07dsqnbdbbki")
+    .add_cover(
+        key="control",
+        translation_key="curtain",
+        translation_string="curtain",
+        current_state_dp_code="control",
+        current_position_dp_code="percent_control",
+        set_position_dp_code="percent_control",
+        device_class=TuyaCoverDeviceClass.CURTAIN,
+    )
+    .register(TUYA_QUIRKS_REGISTRY)
+)
+(
     # This model has percent_control / percent_state / situation_set
     # but they never get updated - use control instead to get the state
     DeviceQuirk()
