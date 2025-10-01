f85f093640b4774277200d467fbf241d76c495f4
Erik <erik@montnemery.com>
2025-10-01 15:38:16 +0200

    Remove to_native method from recorder database schemas

--- tests/components/recorder/common.py
+++ tests/components/recorder/common.py
@@ -28,11 +28,17 @@ from homeassistant.components.recorder import (
     statistics,
 )
 from homeassistant.components.recorder.db_schema import (
+    EventData,
     Events,
     EventTypes,
     RecorderRuns,
+    StateAttributes,
     States,
     StatesMeta,
+)
+from homeassistant.components.recorder.models import (
+    bytes_to_ulid_or_none,
+    bytes_to_uuid_hex_or_none,
 )
 from homeassistant.components.recorder.tasks import RecorderTask, StatisticsTask
 from homeassistant.components.sensor import SensorDeviceClass, SensorStateClass
@@ -40,6 +46,7 @@ from homeassistant.const import DEGREE, UnitOfTemperature
 from homeassistant.core import Event, HomeAssistant, State
 from homeassistant.helpers import recorder as recorder_helper
 from homeassistant.util import dt as dt_util
+from homeassistant.util.json import json_loads, json_loads_object
 
 from . import db_schema_0
 
@@ -492,3 +499,80 @@ async def async_attach_db_engine(hass: HomeAssistant) -> None:
             )
 
     await instance.async_add_executor_job(_mock_setup_recorder_connection)
+
+
+EVENT_ORIGIN_ORDER = [ha.EventOrigin.local, ha.EventOrigin.remote]
+
+
+def db_event_to_native(event: Events, validate_entity_id: bool = True) -> Event | None:
+    """Convert to a native HA Event."""
+    context = ha.Context(
+        id=bytes_to_ulid_or_none(event.context_id_bin),
+        user_id=bytes_to_uuid_hex_or_none(event.context_user_id_bin),
+        parent_id=bytes_to_ulid_or_none(event.context_parent_id_bin),
+    )
+    return Event(
+        event.event_type or "",
+        json_loads_object(event.event_data) if event.event_data else {},
+        ha.EventOrigin(event.origin)
+        if event.origin
+        else EVENT_ORIGIN_ORDER[event.origin_idx or 0],
+        event.time_fired_ts or 0,
+        context=context,
+    )
+
+
+def db_event_data_to_native(event_data: EventData) -> dict[str, Any]:
+    """Convert to an event data dictionary."""
+    shared_data = event_data.shared_data
+    if shared_data is None:
+        return {}
+    return cast(dict[str, Any], json_loads(shared_data))
+
+
+def db_state_to_native(state: States, validate_entity_id: bool = True) -> State | None:
+    """Convert to an HA state object."""
+    context = ha.Context(
+        id=bytes_to_ulid_or_none(state.context_id_bin),
+        user_id=bytes_to_uuid_hex_or_none(state.context_user_id_bin),
+        parent_id=bytes_to_ulid_or_none(state.context_parent_id_bin),
+    )
+    attrs = json_loads_object(state.attributes) if state.attributes else {}
+    # try:
+    #    attrs = json_loads_object(state.attributes) if state.attributes else {}
+    # except JSON_DECODE_EXCEPTIONS:
+    #    # When json_loads fails
+    #    _LOGGER.exception("Error converting row to state: %s", state)
+    #    return None
+    last_updated = dt_util.utc_from_timestamp(state.last_updated_ts or 0)
+    if state.last_changed_ts is None or state.last_changed_ts == state.last_updated_ts:
+        last_changed = dt_util.utc_from_timestamp(state.last_updated_ts or 0)
+    else:
+        last_changed = dt_util.utc_from_timestamp(state.last_changed_ts or 0)
+    if (
+        state.last_reported_ts is None
+        or state.last_reported_ts == state.last_updated_ts
+    ):
+        last_reported = dt_util.utc_from_timestamp(state.last_updated_ts or 0)
+    else:
+        last_reported = dt_util.utc_from_timestamp(state.last_reported_ts or 0)
+    return State(
+        state.entity_id or "",
+        state.state,  # type: ignore[arg-type]
+        # Join the state_attributes table on attributes_id to get the attributes
+        # for newer states
+        attrs,
+        last_changed=last_changed,
+        last_reported=last_reported,
+        last_updated=last_updated,
+        context=context,
+        validate_entity_id=validate_entity_id,
+    )
+
+
+def db_state_attributes_to_native(state_attrs: StateAttributes) -> dict[str, Any]:
+    """Convert to a state attributes dictionary."""
+    shared_attrs = state_attrs.shared_attrs
+    if shared_attrs is None:
+        return {}
+    return cast(dict[str, Any], json_loads(shared_attrs))
