1665e497476be210d4c0d4ff0d695fd8f643be4e
Erwin Douna <e.douna@gmail.com>
2025-10-01 14:48:55 +0000

    Initial commit

---
+++ homeassistant/components/sma/coordinator.py
@@ -0,0 +1,74 @@
+"""Coordinator for the SMA integration."""
+
+from __future__ import annotations
+
+import logging
+
+from pysma import SMA
+from pysma.exceptions import (
+    SmaAuthenticationException,
+    SmaConnectionException,
+    SmaReadException,
+)
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_SCAN_INTERVAL
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+from .const import DEFAULT_SCAN_INTERVAL, DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SMADataUpdateCoordinator(DataUpdateCoordinator):
+    """Data Update Coordinator for SMA."""
+
+    config_entry: ConfigEntry
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        config_entry: ConfigEntry,
+        sma: SMA,
+    ) -> None:
+        """Initialize the SMA Data Update Coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            config_entry=config_entry,
+            name=DOMAIN,
+            update_interval=config_entry.options.get(
+                CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+            ),
+        )
+        self.sma = sma
+
+    async def _async_setup(self) -> None:
+        """Set up the SMA Data Update Coordinator."""
+        try:
+            sma_device_info = await self.sma.device_info()
+            sensor_def = await self.sma.get_sensors()
+        except (
+            SmaReadException,
+            SmaConnectionException,
+        ) as exc:
+            raise ConfigEntryNotReady from exc
+        except SmaAuthenticationException as exc:
+            raise ConfigEntryAuthFailed from exc
+
+    async def _async_update_data(self) -> None:
+        """Update the used SMA sensors."""
+        try:
+            # @Erwin: fetch the sensor data from the data
+            await self.sma.read(sensor_def)
+        except (
+            SmaReadException,
+            SmaConnectionException,
+        ) as err:
+            raise UpdateFailed(
+                translation_domain=DOMAIN,
+                translation_key="cannot_connect",
+                translation_placeholders={"error": repr(err)},
+            ) from err
