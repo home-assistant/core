a5376b609e788e289be2813ed81732fd4300de34
Erik Montnemery <erik@montnemery.com>
2025-10-01 11:27:17 +0200

    Add device class filter to switcher_kis services (#153248)

--- homeassistant/components/switcher_kis/switch.py
+++ homeassistant/components/switcher_kis/switch.py
@@ -63,12 +63,14 @@ async def async_setup_entry(
         SERVICE_SET_AUTO_OFF_NAME,
         SERVICE_SET_AUTO_OFF_SCHEMA,
         "async_set_auto_off_service",
+        entity_device_classes=(SwitchDeviceClass.SWITCH,),
     )
 
     platform.async_register_entity_service(
         SERVICE_TURN_ON_WITH_TIMER_NAME,
         SERVICE_TURN_ON_WITH_TIMER_SCHEMA,
         "async_turn_on_with_timer_service",
+        entity_device_classes=(SwitchDeviceClass.SWITCH,),
     )
 
     @callback
@@ -134,22 +136,6 @@ class SwitcherBaseSwitchEntity(SwitcherEntity, SwitchEntity):
         await self._async_call_api(API_CONTROL_DEVICE, Command.OFF)
         self._attr_is_on = self.control_result = False
         self.async_write_ha_state()
-
-    async def async_set_auto_off_service(self, auto_off: timedelta) -> None:
-        """Use for handling setting device auto-off service calls."""
-        _LOGGER.warning(
-            "Service '%s' is not supported by %s",
-            SERVICE_SET_AUTO_OFF_NAME,
-            self.coordinator.name,
-        )
-
-    async def async_turn_on_with_timer_service(self, timer_minutes: int) -> None:
-        """Use for turning device on with a timer service calls."""
-        _LOGGER.warning(
-            "Service '%s' is not supported by %s",
-            SERVICE_TURN_ON_WITH_TIMER_NAME,
-            self.coordinator.name,
-        )
 
 
 class SwitcherPowerPlugSwitchEntity(SwitcherBaseSwitchEntity):
