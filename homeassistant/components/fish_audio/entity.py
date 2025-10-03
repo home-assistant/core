591ee56241eef20b41be87d8e79de423666ccd1a
Noam <noambav@gmail.com>
2025-10-03 14:35:26 +0000

    Changed entry type to FishAudioConfigEntry

--- homeassistant/components/fish_audio/entity.py
+++ homeassistant/components/fish_audio/entity.py
@@ -2,11 +2,11 @@
 
 from __future__ import annotations
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity import Entity
 
 from .const import DOMAIN
+from .types import FishAudioConfigEntry
 
 
 class FishAudioEntity(Entity):
@@ -14,9 +14,8 @@ class FishAudioEntity(Entity):
 
     _attr_has_entity_name = True
 
-    def __init__(self, entry: ConfigEntry) -> None:
+    def __init__(self, entry: FishAudioConfigEntry) -> None:
         """Initialize the entity."""
-        super().__init__()
         self._session = entry.runtime_data
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, entry.entry_id)},
