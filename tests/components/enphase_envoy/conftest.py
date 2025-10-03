73a09b156c292c83e4976e3e0a9bbab4364cc604
Arie Catsman <120491684+catsmanac@users.noreply.github.com>
2025-10-04 01:24:11 +0200

    Extend enphase_envoy test data with new library data fields (#153591)

--- tests/components/enphase_envoy/conftest.py
+++ tests/components/enphase_envoy/conftest.py
@@ -193,6 +193,22 @@ def _load_json_2_meter_data(
     mocked_data: EnvoyData, json_fixture: dict[str, Any]
 ) -> None:
     """Fill envoy meter data from fixture."""
+    if meters := json_fixture["data"].get("ctmeters"):
+        mocked_data.ctmeters = {}
+        [
+            mocked_data.ctmeters.update({meter: EnvoyMeterData(**meter_data)})
+            for meter, meter_data in meters.items()
+        ]
+    if meters := json_fixture["data"].get("ctmeters_phases"):
+        mocked_data.ctmeters_phases = {}
+        for meter, meter_data in meters.items():
+            meter_phase_data: dict[str, EnvoyMeterData] = {}
+            [
+                meter_phase_data.update({phase: EnvoyMeterData(**phase_data)})
+                for phase, phase_data in meter_data.items()
+            ]
+            mocked_data.ctmeters_phases.update({meter: meter_phase_data})
+
     if item := json_fixture["data"].get("ctmeter_production"):
         mocked_data.ctmeter_production = EnvoyMeterData(**item)
     if item := json_fixture["data"].get("ctmeter_consumption"):
