4690deb06b8d3aa2b481ae9dfa30920af7a19d3f
hanwg <han.wuguang@gmail.com>
2025-10-04 03:24:55 +0000

    refactor entity

---
+++ homeassistant/components/telegram_bot/entity.py
@@ -0,0 +1,31 @@
+"""Base entity for Telegram bot integration."""
+
+import telegram
+
+from homeassistant.const import CONF_PLATFORM
+from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
+from homeassistant.helpers.entity import Entity
+
+from . import TelegramBotConfigEntry
+from .const import DOMAIN
+
+
+class TelegramBotEntity(Entity):
+    """Base entity."""
+
+    def __init__(self, config_entry: TelegramBotConfigEntry) -> None:
+        """Initialize the entity."""
+
+        self.bot_id = config_entry.runtime_data.bot.id
+
+        self._attr_device_info = DeviceInfo(
+            name=config_entry.title,
+            entry_type=DeviceEntryType.SERVICE,
+            manufacturer="Telegram",
+            model=config_entry.data[CONF_PLATFORM].capitalize(),
+            sw_version=telegram.__version__,
+            identifiers={(DOMAIN, f"{self.bot_id}")},
+        )
+
+        self.config_entry = config_entry
+        self.service = config_entry.runtime_data
