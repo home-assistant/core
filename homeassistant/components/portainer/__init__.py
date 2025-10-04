74bf909590ee4a4c6c550526b9c1e8aef9af08c0
Erwin Douna <e.douna@gmail.com>
2025-10-04 19:00:37 +0000

    FIx merge

--- homeassistant/components/portainer/__init__.py
+++ homeassistant/components/portainer/__init__.py
@@ -18,7 +18,7 @@ from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
 from .coordinator import PortainerCoordinator
 
-_PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.BUTTON]
+_PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.BUTTON, Platform.SWITCH]
 
 type PortainerConfigEntry = ConfigEntry[PortainerCoordinator]
 
