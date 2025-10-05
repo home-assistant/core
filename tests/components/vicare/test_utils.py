3f68d24eb33a3a150924229d8534d917773cb55f
Christopher Fenner <Christopher.Fenner@me.com>
2025-10-05 23:35:20 +0200

    add test case

--- tests/components/vicare/test_utils.py
+++ tests/components/vicare/test_utils.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from homeassistant.components.vicare.utils import filter_state
+from homeassistant.components.vicare.utils import filter_state, format_zigbee
 
 
 @pytest.mark.parametrize(
@@ -21,3 +21,20 @@ async def test_filter_state(
     """Test filter_state."""
 
     assert filter_state(state) == expected_result
+
+
+@pytest.mark.parametrize(
+    ("ieee", "expected_result"),
+    [
+        ("anything", "anything"),
+        ("1234567812345678", "12:34:56:78:12:34:56:78"),
+        ("12:34:56:78:12:34:56:78", "12:34:56:78:12:34:56:78"),
+    ],
+)
+async def test_format_zigbee(
+    ieee: str | None,
+    expected_result: str | None,
+) -> None:
+    """Test format_zigbee."""
+
+    assert format_zigbee(ieee) == expected_result
