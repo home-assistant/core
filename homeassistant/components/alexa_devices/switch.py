b7b57a819d7d1cc4b124d9928078fef7cdc1713f
Simone Chemelli <simone.chemelli@gmail.com>
2025-10-05 14:26:16 +0200

    Fix sensors availability check for Alexa Devices (#153743)

--- homeassistant/components/alexa_devices/switch.py
+++ homeassistant/components/alexa_devices/switch.py
@@ -29,7 +29,9 @@ class AmazonSwitchEntityDescription(SwitchEntityDescription):
 
     is_on_fn: Callable[[AmazonDevice], bool]
     is_available_fn: Callable[[AmazonDevice, str], bool] = lambda device, key: (
-        device.online and device.sensors[key].error is False
+        device.online
+        and (sensor := device.sensors.get(key)) is not None
+        and sensor.error is False
     )
     method: str
 
