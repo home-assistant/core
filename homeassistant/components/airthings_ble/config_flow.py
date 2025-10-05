98f8f15e908c96224fb050a387c60a073b3497f7
Ståle Storø Hauknes <stale.semb@airthings.com>
2025-10-05 16:18:47 +0200

    Fix crash when setting up Airthings BLE device (#153510)

--- homeassistant/components/airthings_ble/config_flow.py
+++ homeassistant/components/airthings_ble/config_flow.py
@@ -8,6 +8,7 @@ from typing import Any
 
 from airthings_ble import AirthingsBluetoothDeviceData, AirthingsDevice
 from bleak import BleakError
+from habluetooth import BluetoothServiceInfoBleak
 import voluptuous as vol
 
 from homeassistant.components import bluetooth
@@ -155,21 +156,27 @@ class AirthingsConfigFlow(ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title=discovery.name, data={})
 
         current_addresses = self._async_current_ids(include_ignore=False)
+        devices: list[BluetoothServiceInfoBleak] = []
         for discovery_info in async_discovered_service_info(self.hass):
             address = discovery_info.address
             if address in current_addresses or address in self._discovered_devices:
                 continue
-
             if MFCT_ID not in discovery_info.manufacturer_data:
                 continue
-
             if not any(uuid in SERVICE_UUIDS for uuid in discovery_info.service_uuids):
                 continue
+            devices.append(discovery_info)
 
+        for discovery_info in devices:
+            address = discovery_info.address
             try:
                 device = await self._get_device_data(discovery_info)
             except AirthingsDeviceUpdateError:
-                return self.async_abort(reason="cannot_connect")
+                _LOGGER.error(
+                    "Error connecting to and getting data from %s",
+                    discovery_info.address,
+                )
+                continue
             except Exception:
                 _LOGGER.exception("Unknown error occurred")
                 return self.async_abort(reason="unknown")
