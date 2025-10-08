f65b04d1bd1c32ae323c93cad917969fbeac1166
Thomas D <11554546+thomasddn@users.noreply.github.com>
2025-10-08 11:40:50 +0000

    Remove entity states

--- homeassistant/components/volvo/diagnostics.py
+++ homeassistant/components/volvo/diagnostics.py
@@ -4,7 +4,6 @@ from typing import Any
 
 from homeassistant.const import CONF_ACCESS_TOKEN, CONF_API_KEY
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.redact import async_redact_data
 
 from .const import CONF_VIN
@@ -31,17 +30,9 @@ async def async_get_config_entry_diagnostics(
     """Return diagnostics for a config entry."""
     context = entry.runtime_data.interval_coordinators[0].context
 
-    entity_registry = er.async_get(hass)
-    entities: dict[str, str | None] = {}
-
-    for reg_entry in er.async_entries_for_config_entry(entity_registry, entry.entry_id):
-        state = hass.states.get(reg_entry.entity_id)
-        entities[reg_entry.entity_id] = state.state if state else None
-
     return {
         "entry_data": async_redact_data(entry.data, _TO_REDACT_ENTRY),
-        "vechicle": async_redact_data(_to_dict(context.vehicle), _TO_REDACT_DATA),
-        "entities": entities,
+        "vehicle": async_redact_data(_to_dict(context.vehicle), _TO_REDACT_DATA),
         **{
             coordinator.name: async_redact_data(
                 _to_dict(coordinator.data), _TO_REDACT_DATA
