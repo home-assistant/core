d01aac38b3e60ad8c8c87ad0654d1db90a621042
Marcus Gustavsson <mgustgithub@m.tump.info>
2025-10-10 23:27:35 +0000

    Passed the httpx client from HomeAssistant to avoid multiple initialisations

--- homeassistant/components/prowl/helpers.py
+++ homeassistant/components/prowl/helpers.py
@@ -5,11 +5,12 @@ import asyncio
 import prowlpy
 
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.httpx_client import get_async_client
 
 
 async def async_verify_key(hass: HomeAssistant, api_key: str) -> bool:
     """Validate API key."""
-    prowl = prowlpy.AsyncProwl(api_key)
+    prowl = prowlpy.AsyncProwl(api_key, client=get_async_client(hass))
     try:
         async with asyncio.timeout(10):
             await prowl.verify_key()
