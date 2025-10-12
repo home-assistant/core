081e2b0cd78e576c4beb613f3d856c4da1535e0c
Michael Davie <michael.davie@gmail.com>
2025-10-12 19:52:31 +0000

    Fix ruff

--- homeassistant/components/environment_canada/config_flow.py
+++ homeassistant/components/environment_canada/config_flow.py
@@ -8,6 +8,7 @@ import aiohttp
 from env_canada import ECWeather, ec_exc
 from env_canada.ec_weather import get_ec_sites_list
 import voluptuous as vol
+
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_LANGUAGE, CONF_LATITUDE, CONF_LONGITUDE
 from homeassistant.helpers import config_validation as cv
