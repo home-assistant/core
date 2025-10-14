da63a2d09023dc03b4a32230e1d8e1641f220360
Leslie Fernando <lesliefernando68@gmail.com>
2025-10-14 20:19:34 +0530

    Fix test to use Bluetooth fixture

--- tests/components/sensirion_ble/test_diagnostics.py
+++ tests/components/sensirion_ble/test_diagnostics.py
@@ -13,11 +13,13 @@ async def test_entry_diagnostics(
     hass: HomeAssistant,
     hass_client: ClientSessionGenerator,
     mock_config_entry: MockConfigEntry,
+    mock_sensirion_ble,
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test the Sensirion BLE entry diagnostics."""
     mock_config_entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
 
     result = await get_diagnostics_for_config_entry(
         hass, hass_client, mock_config_entry
