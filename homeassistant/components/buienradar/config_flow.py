17998cbcc8db004d0c6f54be2ee200635d307aad
FancyPantz1337 <156546416+FancyPantz1337@users.noreply.github.com>
2025-10-14 20:50:32 +0200

    Removed Unnecessary Async

--- homeassistant/components/buienradar/config_flow.py
+++ homeassistant/components/buienradar/config_flow.py
@@ -57,7 +57,7 @@ OPTIONS_SCHEMA = vol.Schema(
 )
 
 
-async def _options_suggested_values(handler: SchemaCommonFlowHandler) -> dict[str, Any]:
+def _options_suggested_values(handler: SchemaCommonFlowHandler) -> dict[str, Any]:
     parent_handler = cast(SchemaOptionsFlowHandler, handler.parent_handler)
     suggested_values = copy.deepcopy(dict(parent_handler.config_entry.data))
     suggested_values.update(parent_handler.options)
