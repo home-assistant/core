755101171e8660e591524c551d659a2c0fc2e7f7
Stuart Jones <47328516+stujonesuk@users.noreply.github.com>
2025-10-14 14:24:33 +0100

    Ensure boto3 client is disposed in the event of errors in the integration initialization

--- homeassistant/components/aws_s3/__init__.py
+++ homeassistant/components/aws_s3/__init__.py
@@ -42,6 +42,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: S3ConfigEntry) -> bool:
     model.from_dict(cast(dict, entry.data))
 
     try:
+        client = None
         session = AioSession()
         # pylint: disable-next=unnecessary-dunder-call
         client = await session.create_client(
@@ -52,37 +53,51 @@ async def async_setup_entry(hass: HomeAssistant, entry: S3ConfigEntry) -> bool:
         ).__aenter__()
         await client.head_bucket(Bucket=model[CONF_BUCKET])
     except ClientError as err:
+        if client is not None:
+            await client.__aexit__(None, None, None)
         raise ConfigEntryError(
             translation_domain=DOMAIN,
             translation_key="invalid_credentials",
         ) from err
     except NoCredentialsError as err:
+        if client is not None:
+            await client.__aexit__(None, None, None)
         raise ConfigEntryError(
             translation_domain=DOMAIN,
             translation_key="no_credentials_implicit",
         ) from err
     except ProfileNotFound as err:
+        if client is not None:
+            await client.__aexit__(None, None, None)
         raise ConfigEntryError(
             translation_domain=DOMAIN,
             translation_key="no_credentials_profile",
         ) from err
     except TokenRetrievalError as err:
+        if client is not None:
+            await client.__aexit__(None, None, None)
         raise ConfigEntryError(
             translation_domain=DOMAIN,
             translation_key="invalid_credentials",
         ) from err
     except ParamValidationError as err:
+        if client is not None:
+            await client.__aexit__(None, None, None)
         if "Invalid bucket name" in str(err):
             raise ConfigEntryError(
                 translation_domain=DOMAIN,
                 translation_key="invalid_bucket_name",
             ) from err
     except ValueError as err:
+        if client is not None:
+            await client.__aexit__(None, None, None)
         raise ConfigEntryError(
             translation_domain=DOMAIN,
             translation_key="invalid_endpoint_url",
         ) from err
     except ConnectionError as err:
+        if client is not None:
+            await client.__aexit__(None, None, None)
         raise ConfigEntryNotReady(
             translation_domain=DOMAIN,
             translation_key="cannot_connect",
