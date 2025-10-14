07203ef7b39ab18bf3ea3677cb26887585f3fe28
Sahitya-kokkonda <ksahitya2412@gmail.com>
2025-10-14 10:10:57 +0000

    Refactor def websocket_list_languages

--- homeassistant/components/assist_pipeline/select.py
+++ homeassistant/components/assist_pipeline/select.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import asyncio
 from collections.abc import Iterable
 from dataclasses import replace
 
@@ -134,6 +135,7 @@ class AssistPipelineSelect(SelectEntity, restore_state.RestoreEntity):
         """Handle pipeline update."""
         self._update_options()
         self.async_write_ha_state()
+        await asyncio.sleep(0)
 
     @callback
     def _update_options(self) -> None:
