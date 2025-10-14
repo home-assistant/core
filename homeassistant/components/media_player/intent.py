55a7437debc951ab6ea3305487d44caa75e76a7a
acidcoke <acidcoke@acidic.codes>
2025-10-14 19:21:33 +0000

    Extract redundant code into function

--- homeassistant/components/media_player/intent.py
+++ homeassistant/components/media_player/intent.py
@@ -378,6 +378,62 @@ class MediaSearchAndPlayHandler(intent.IntentHandler):
         return response
 
 
+def get_entities(hass, slots, intent_obj, component):
+    # Entity name to match
+    name_slot = slots.get("name", {})
+    entity_name: str | None = name_slot.get("value")
+
+    # Get area/floor info
+    area_slot = slots.get("area", {})
+    area_id = area_slot.get("value")
+
+    floor_slot = slots.get("floor", {})
+    floor_id = floor_slot.get("value")
+
+    # Find matching entities
+    match_constraints = intent.MatchTargetsConstraints(
+        name=entity_name,
+        area_name=area_id,
+        floor_name=floor_id,
+        domains={DOMAIN},
+        assistant=intent_obj.assistant,
+        features=MediaPlayerEntityFeature.VOLUME_SET,
+    )
+    match_preferences = intent.MatchTargetsPreferences(
+        area_id=slots.get("preferred_area_id", {}).get("value"),
+        floor_id=slots.get("preferred_floor_id", {}).get("value"),
+    )
+    match_result = intent.async_match_targets(
+        hass, match_constraints, match_preferences
+    )
+
+    if not match_result.is_match:
+        # No targets
+        raise intent.MatchFailedError(
+            result=match_result, constraints=match_constraints
+        )
+
+    if (
+        match_result.is_match
+        and (len(match_result.states) > 1)
+        and ("name" not in intent_obj.slots)
+    ):
+        # Multiple targets not by name, so we need to check state
+        match_result.states = [
+            s for s in match_result.states if s.state == STATE_PLAYING
+        ]
+        if not match_result.states:
+            # No media players are playing
+            raise intent.MatchFailedError(
+                result=intent.MatchTargetsResult(
+                    is_match=False, no_match_reason=intent.MatchFailedReason.STATE
+                ),
+                constraints=match_constraints,
+                preferences=match_preferences,
+            )
+    return match_result
+
+
 class MediaSetVolumeHandler(intent.IntentHandler):
     """Handler for setting volume."""
 
@@ -407,64 +463,11 @@ class MediaSetVolumeHandler(intent.IntentHandler):
 
         volume_level = slots["volume_level"]["value"]
 
-        # Entity name to match
-        name_slot = slots.get("name", {})
-        entity_name: str | None = name_slot.get("value")
-
-        # Get area/floor info
-        area_slot = slots.get("area", {})
-        area_id = area_slot.get("value")
-
-        floor_slot = slots.get("floor", {})
-        floor_id = floor_slot.get("value")
-
-        # Find matching entities
-        match_constraints = intent.MatchTargetsConstraints(
-            name=entity_name,
-            area_name=area_id,
-            floor_name=floor_id,
-            domains={DOMAIN},
-            assistant=intent_obj.assistant,
-            features=MediaPlayerEntityFeature.VOLUME_SET,
-        )
-        match_preferences = intent.MatchTargetsPreferences(
-            area_id=slots.get("preferred_area_id", {}).get("value"),
-            floor_id=slots.get("preferred_floor_id", {}).get("value"),
-        )
-        match_result = intent.async_match_targets(
-            hass, match_constraints, match_preferences
-        )
-
-        if not match_result.is_match:
-            # No targets
-            raise intent.MatchFailedError(
-                result=match_result, constraints=match_constraints
-            )
-
-        if (
-            match_result.is_match
-            and (len(match_result.states) > 1)
-            and ("name" not in intent_obj.slots)
-        ):
-            # Multiple targets not by name, so we need to check state
-            match_result.states = [
-                s for s in match_result.states if s.state == STATE_PLAYING
-            ]
-            if not match_result.states:
-                # No media players are playing
-                raise intent.MatchFailedError(
-                    result=intent.MatchTargetsResult(
-                        is_match=False, no_match_reason=intent.MatchFailedReason.STATE
-                    ),
-                    constraints=match_constraints,
-                    preferences=match_preferences,
-                )
-
+        match_result = get_entities(hass, slots, intent_obj, component)
         target_entity_ids = {s.entity_id for s in match_result.states}
         target_entities = [
             e for e in component.entities if e.entity_id in target_entity_ids
         ]
-
         coros = [e.async_set_volume_level(volume_level) for e in target_entities]
 
         try:
@@ -511,58 +514,7 @@ class MediaSetVolumeRelativeHandler(intent.IntentHandler):
         slots = self.async_validate_slots(intent_obj.slots)
         volume_step = slots["volume_step"]["value"]
 
-        # Entity name to match
-        name_slot = slots.get("name", {})
-        entity_name: str | None = name_slot.get("value")
-
-        # Get area/floor info
-        area_slot = slots.get("area", {})
-        area_id = area_slot.get("value")
-
-        floor_slot = slots.get("floor", {})
-        floor_id = floor_slot.get("value")
-
-        # Find matching entities
-        match_constraints = intent.MatchTargetsConstraints(
-            name=entity_name,
-            area_name=area_id,
-            floor_name=floor_id,
-            domains={DOMAIN},
-            assistant=intent_obj.assistant,
-            features=MediaPlayerEntityFeature.VOLUME_SET,
-        )
-        match_preferences = intent.MatchTargetsPreferences(
-            area_id=slots.get("preferred_area_id", {}).get("value"),
-            floor_id=slots.get("preferred_floor_id", {}).get("value"),
-        )
-        match_result = intent.async_match_targets(
-            hass, match_constraints, match_preferences
-        )
-
-        if not match_result.is_match:
-            # No targets
-            raise intent.MatchFailedError(
-                result=match_result, constraints=match_constraints
-            )
-
-        if (
-            match_result.is_match
-            and (len(match_result.states) > 1)
-            and ("name" not in intent_obj.slots)
-        ):
-            # Multiple targets not by name, so we need to check state
-            match_result.states = [
-                s for s in match_result.states if s.state == STATE_PLAYING
-            ]
-            if not match_result.states:
-                # No media players are playing
-                raise intent.MatchFailedError(
-                    result=intent.MatchTargetsResult(
-                        is_match=False, no_match_reason=intent.MatchFailedReason.STATE
-                    ),
-                    constraints=match_constraints,
-                    preferences=match_preferences,
-                )
+        match_result = get_entities(hass, slots, intent_obj, component)
 
         target_entity_ids = {s.entity_id for s in match_result.states}
         target_entities = [
