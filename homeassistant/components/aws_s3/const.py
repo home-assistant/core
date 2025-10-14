51c4131b18272492dbce0e75c9c75dbb2255150e
Stuart Jones <47328516+stujonesuk@users.noreply.github.com>
2025-10-14 13:48:51 +0100

    Add new Authentication Mode constants

--- homeassistant/components/aws_s3/const.py
+++ homeassistant/components/aws_s3/const.py
@@ -7,6 +7,9 @@ from homeassistant.util.hass_dict import HassKey
 
 DOMAIN: Final = "aws_s3"
 
+CONF_AUTH_MODE = "auth_mode"
+CONF_AUTH_MODE_IMPLICIT = "implicit"
+CONF_AUTH_MODE_EXPLICIT = "explicit"
 CONF_ACCESS_KEY_ID = "access_key_id"
 CONF_SECRET_ACCESS_KEY = "secret_access_key"
 CONF_ENDPOINT_URL = "endpoint_url"
