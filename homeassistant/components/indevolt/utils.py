942e9cf72c18c40274395b9007d624440e991243
solarmanpv <alert@igen-tech.com>
2025-10-14 15:30:00 +0800

    delete indevolt integration

--- homeassistant/components/indevolt/utils.py
+++
@@ -1,8 +0,0 @@
-"""Utility functions for the Indevolt component."""
-
-
-def get_device_gen(str) -> int:
-    """Return the device generation."""
-    if str == "BK1600/BK1600Ultra":
-        return 1
-    return 2
