e1df194fe55b4c494fc0e123b0458c9e733e98b6
Leslie Fernando <lesliefernando68@gmail.com>
2025-10-14 20:27:38 +0530

    Fix test to wait for async setup

--- tests/components/sensorpush/test_diagnostics.py
+++ tests/components/sensorpush/test_diagnostics.py
@@ -18,6 +18,7 @@ async def test_entry_diagnostics(
     """Test the SensorPush entry diagnostics."""
     mock_config_entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
 
     result = await get_diagnostics_for_config_entry(
         hass, hass_client, mock_config_entry
