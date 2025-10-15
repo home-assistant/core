9f7cfba0b878506b8dcab0a07010c35b13469b10
Magnus <magnusknutas@gmail.com>
2025-10-15 16:11:16 +0200

    Adding __all__ export to device_tracker (#154525)

--- homeassistant/components/device_tracker/__init__.py
+++ homeassistant/components/device_tracker/__init__.py
@@ -2,12 +2,12 @@
 
 from __future__ import annotations
 
-from homeassistant.const import ATTR_GPS_ACCURACY, STATE_HOME  # noqa: F401
+from homeassistant.const import STATE_HOME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.loader import bind_hass
 
-from .config_entry import (  # noqa: F401
+from .config_entry import (
     ScannerEntity,
     ScannerEntityDescription,
     TrackerEntity,
@@ -15,7 +15,7 @@ from .config_entry import (  # noqa: F401
     async_setup_entry,
     async_unload_entry,
 )
-from .const import (  # noqa: F401
+from .const import (
     ATTR_ATTRIBUTES,
     ATTR_BATTERY,
     ATTR_DEV_ID,
@@ -37,7 +37,7 @@ from .const import (  # noqa: F401
     SCAN_INTERVAL,
     SourceType,
 )
-from .legacy import (  # noqa: F401
+from .legacy import (
     PLATFORM_SCHEMA,
     PLATFORM_SCHEMA_BASE,
     SERVICE_SEE,
@@ -61,3 +61,44 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the device tracker."""
     async_setup_legacy_integration(hass, config)
     return True
+
+
+__all__ = (
+    "ATTR_ATTRIBUTES",
+    "ATTR_BATTERY",
+    "ATTR_DEV_ID",
+    "ATTR_GPS",
+    "ATTR_HOST_NAME",
+    "ATTR_IP",
+    "ATTR_LOCATION_NAME",
+    "ATTR_MAC",
+    "ATTR_SOURCE_TYPE",
+    "CONF_CONSIDER_HOME",
+    "CONF_NEW_DEVICE_DEFAULTS",
+    "CONF_SCAN_INTERVAL",
+    "CONF_TRACK_NEW",
+    "CONNECTED_DEVICE_REGISTERED",
+    "DEFAULT_CONSIDER_HOME",
+    "DEFAULT_TRACK_NEW",
+    "DOMAIN",
+    "ENTITY_ID_FORMAT",
+    "PLATFORM_SCHEMA",
+    "PLATFORM_SCHEMA_BASE",
+    "SCAN_INTERVAL",
+    "SERVICE_SEE",
+    "SERVICE_SEE_PAYLOAD_SCHEMA",
+    "SOURCE_TYPES",
+    "AsyncSeeCallback",
+    "DeviceScanner",
+    "ScannerEntity",
+    "ScannerEntityDescription",
+    "SeeCallback",
+    "SourceType",
+    "TrackerEntity",
+    "TrackerEntityDescription",
+    "async_setup",
+    "async_setup_entry",
+    "async_unload_entry",
+    "is_on",
+    "see",
+)
