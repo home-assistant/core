7ae1c2a95e3b33e0ac17cd026acbb320e7fb930f
Jordan Harvey <jordan@hrvy.uk>
2025-10-15 10:42:07 +0100

    update migration to use async_update_entry

--- homeassistant/components/probe_plus/__init__.py
+++ homeassistant/components/probe_plus/__init__.py
@@ -15,10 +15,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ProbePlusConfigEntry) ->
     # Perform a migration to ensure the model is added to the config entry schema.
     if CONF_MODEL not in entry.data:
         # The config entry adds the model number of the device to the start of its title
-        entry.data = {
-            **entry.data,
-            CONF_MODEL: entry.title.split(" ")[0]
-        }
+        hass.config_entries.async_update_entry(
+            entry, data={**entry.data, CONF_MODEL: entry.title.split(" ")[0]}
+        )
     coordinator = ProbePlusDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
     entry.runtime_data = coordinator
