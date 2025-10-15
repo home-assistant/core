a4a2a966081b12e316ebe69c0ca637d57f651ec5
Jordan Harvey <jordan@hrvy.uk>
2025-10-15 16:36:58 +0100

    remove unnecessary test

--- tests/components/nintendo_parental_controls/test_number.py
+++ tests/components/nintendo_parental_controls/test_number.py
@@ -2,8 +2,6 @@
 
 from unittest.mock import AsyncMock, patch
 
-from pynintendoparental.exceptions import DailyPlaytimeOutOfRangeError
-import pytest
 from syrupy.assertion import SnapshotAssertion
 
 from homeassistant.components.number import (
@@ -13,7 +11,6 @@ from homeassistant.components.number import (
 )
 from homeassistant.const import ATTR_ENTITY_ID, Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ServiceValidationError
 from homeassistant.helpers import entity_registry as er
 
 from . import setup_integration
@@ -59,30 +56,3 @@ async def test_set_number(
         blocking=True,
     )
     assert len(mock_nintendo_device.update_max_daily_playtime.mock_calls) == 1
-
-
-async def test_set_number_service_exceptions(
-    hass: HomeAssistant,
-    mock_config_entry: MockConfigEntry,
-    mock_nintendo_client: AsyncMock,
-    mock_nintendo_device: AsyncMock,
-) -> None:
-    """Test number platform service validation errors."""
-    mock_nintendo_device.update_max_daily_playtime.side_effect = (
-        DailyPlaytimeOutOfRangeError(None)
-    )
-    with patch(
-        "homeassistant.components.nintendo_parental_controls._PLATFORMS",
-        [Platform.NUMBER],
-    ):
-        await setup_integration(hass, mock_config_entry)
-    with pytest.raises(ServiceValidationError) as err:
-        await hass.services.async_call(
-            NUMBER_DOMAIN,
-            SERVICE_SET_VALUE,
-            service_data={ATTR_VALUE: "361"},
-            target={ATTR_ENTITY_ID: "number.home_assistant_test_max_screentime_today"},
-            blocking=True,
-        )
-    assert len(mock_nintendo_device.update_max_daily_playtime.mock_calls) == 1
-    assert err.value.translation_key == "daily_playtime_out_of_range"
