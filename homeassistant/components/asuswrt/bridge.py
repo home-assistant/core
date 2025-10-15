432e626484be2327fb4676301db3fb5ed25fa707
kennedyshead <magnusknutas@gmail.com>
2025-10-15 11:01:31 +0200

    import of ConnectionState was done by a secondary import in client

--- homeassistant/components/asuswrt/bridge.py
+++ homeassistant/components/asuswrt/bridge.py
@@ -12,7 +12,8 @@ from aioasuswrt.asuswrt import AsusWrt as AsusWrtLegacy
 from aiohttp import ClientSession
 from asusrouter import AsusRouter, AsusRouterError
 from asusrouter.config import ARConfigKey
-from asusrouter.modules.client import AsusClient, ConnectionState
+from asusrouter.modules.client import AsusClient
+from asusrouter.modules.connection import ConnectionState
 from asusrouter.modules.data import AsusData
 from asusrouter.modules.homeassistant import convert_to_ha_data, convert_to_ha_sensors
 from asusrouter.tools.connection import get_cookie_jar
