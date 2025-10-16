356867af7b6875a8bba8e1841348285181e62c91
metwally <gusmetwsh@student.gu.se>
2025-10-16 19:07:54 +0200

    shahd – fixing type hint issue

--- homeassistant/components/assist_pipeline/pipeline.py
+++ homeassistant/components/assist_pipeline/pipeline.py
@@ -553,7 +553,7 @@ class PipelineRun:
     start_stage: PipelineStage
     end_stage: PipelineStage
     event_callback: PipelineEventCallback
-    language: str = None  # type: ignore[assignment]
+    language: str | None = None  # type: ignore[assignment]
     runner_data: Any | None = None
     intent_agent: conversation.AgentInfo | None = None
     tts_audio_output: str | dict[str, Any] | None = None
