87b727a55a4ba065bc74c9db7b550170d146469b
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-10-17 20:02:19 +0200

    Add Matter fixture for Silabs light switch (#154701)

--- tests/components/matter/conftest.py
+++ tests/components/matter/conftest.py
@@ -119,6 +119,7 @@ async def integration_fixture(
         "silabs_dishwasher",
         "silabs_evse_charging",
         "silabs_laundrywasher",
+        "silabs_light_switch",
         "silabs_refrigerator",
         "silabs_water_heater",
         "smoke_detector",
