b323404233e89daa4cee97b6c859f38c4c375b29
Simone Chemelli <simone.chemelli@gmail.com>
2025-10-18 15:38:23 +0000

    missing commit

--- homeassistant/components/uptimerobot/switch.py
+++ homeassistant/components/uptimerobot/switch.py
@@ -19,7 +19,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
-from .const import API_ATTR_OK, DOMAIN
+from .const import API_ATTR_OK, DOMAIN, STATUS_DOWN, STATUS_UP
 from .coordinator import UptimeRobotConfigEntry
 from .entity import UptimeRobotEntity
 from .utils import new_device_listener
@@ -63,7 +63,7 @@ class UptimeRobotSwitch(UptimeRobotEntity, SwitchEntity):
     @property
     def is_on(self) -> bool:
         """Return True if the entity is on."""
-        return bool(self.monitor.status != 0)
+        return bool(self.monitor.status == STATUS_UP)
 
     async def _async_edit_monitor(self, **kwargs: Any) -> None:
         """Edit monitor status."""
@@ -80,18 +80,19 @@ class UptimeRobotSwitch(UptimeRobotEntity, SwitchEntity):
             ) from exception
 
         if response.status != API_ATTR_OK:
+            message = response.error.message if response.error else "Unknown error"
             raise HomeAssistantError(
                 translation_domain=DOMAIN,
                 translation_key="api_exception",
-                translation_placeholders={"error": response.error.message},
+                translation_placeholders={"error": message},
             )
 
         await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn on switch."""
-        await self._async_edit_monitor(id=self.monitor.id, status=0)
+        await self._async_edit_monitor(monitor_id=self.monitor.id, status=STATUS_DOWN)
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn off switch."""
-        await self._async_edit_monitor(id=self.monitor.id, status=1)
+        await self._async_edit_monitor(monitor_id=self.monitor.id, status=STATUS_UP)
