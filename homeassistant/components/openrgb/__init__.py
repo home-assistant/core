aa157b4ff94786acd5d7772868a01f70b03bd10a
Felipe Santos <felipecassiors@gmail.com>
2025-10-18 03:36:42 +0000

    Allow to remove OpenRGB devices that are disconnected

--- homeassistant/components/openrgb/__init__.py
+++ homeassistant/components/openrgb/__init__.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from homeassistant.const import CONF_NAME, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.device_registry import DeviceEntry
 
 from .const import DOMAIN
 from .coordinator import OpenRGBConfigEntry, OpenRGBCoordinator
@@ -48,3 +49,27 @@ async def async_setup_entry(hass: HomeAssistant, entry: OpenRGBConfigEntry) -> b
 async def async_unload_entry(hass: HomeAssistant, entry: OpenRGBConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
+async def async_remove_config_entry_device(
+    hass: HomeAssistant, entry: OpenRGBConfigEntry, device_entry: DeviceEntry
+) -> bool:
+    """Remove the config entry if the device is no longer connected."""
+    coordinator = entry.runtime_data
+
+    for domain, identifier in device_entry.identifiers:
+        if domain != DOMAIN:
+            continue
+
+        # Block removal of the OpenRGB SDK Server device
+        if identifier == entry.entry_id:
+            return False
+
+        # Block removal of the OpenRGB device if it is still connected
+        if identifier in coordinator.data:
+            return False
+
+        return True
+
+    # Not our device
+    return True
