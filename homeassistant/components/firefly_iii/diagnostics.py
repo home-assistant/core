3271434698356ffbe26ff227a7c79938e629d61b
Erwin Douna <e.douna@gmail.com>
2025-10-18 14:36:38 +0000

    Feedback

--- homeassistant/components/firefly_iii/diagnostics.py
+++ homeassistant/components/firefly_iii/diagnostics.py
@@ -5,13 +5,13 @@ from __future__ import annotations
 from typing import Any
 
 from homeassistant.components.diagnostics import async_redact_data
-from homeassistant.const import CONF_API_KEY
+from homeassistant.const import CONF_API_KEY, CONF_URL
 from homeassistant.core import HomeAssistant
 
 from . import FireflyConfigEntry
 from .coordinator import FireflyDataUpdateCoordinator
 
-TO_REDACT = [CONF_API_KEY]
+TO_REDACT = [CONF_API_KEY, CONF_URL]
 
 
 async def async_get_config_entry_diagnostics(
@@ -22,16 +22,5 @@ async def async_get_config_entry_diagnostics(
 
     return {
         "config_entry": async_redact_data(entry.as_dict(), TO_REDACT),
-        "data": {
-            "primary_currency": coordinator.data.primary_currency.to_dict(),
-            "accounts": [account.to_dict() for account in coordinator.data.accounts],
-            "categories": [
-                category.to_dict() for category in coordinator.data.categories
-            ],
-            "category_details": [
-                detail.to_dict() for detail in coordinator.data.category_details
-            ],
-            "budgets": [budget.to_dict() for budget in coordinator.data.budgets],
-            "bills": [bill.to_dict() for bill in coordinator.data.bills],
-        },
+        "data": {"primary_currency": coordinator.data.primary_currency.to_dict()},
     }
