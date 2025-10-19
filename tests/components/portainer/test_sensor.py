d2c8478426928e5d97474a133545dbcf13390b41
Erwin Douna <e.douna@gmail.com>
2025-10-19 18:44:53 +0000

    Feedback

--- tests/components/portainer/test_sensor.py
+++ tests/components/portainer/test_sensor.py
@@ -14,6 +14,11 @@ from . import setup_integration
 from tests.common import MockConfigEntry, snapshot_platform
 
 
+@pytest.fixture(autouse=True)
+def enable_all_entities(entity_registry_enabled_by_default: None) -> None:
+    """Make sure all entities are enabled."""
+
+
 @pytest.mark.usefixtures("mock_portainer_client")
 async def test_all_entities(
     hass: HomeAssistant,
