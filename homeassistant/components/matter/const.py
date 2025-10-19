033c0180be026ed93d9723ebb5cfa29ce6559733
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-10-19 17:40:41 +0000

    Cleaning

--- homeassistant/components/matter/const.py
+++ homeassistant/components/matter/const.py
@@ -15,9 +15,3 @@ ID_TYPE_DEVICE_ID = "deviceid"
 ID_TYPE_SERIAL = "serial"
 
 FEATUREMAP_ATTRIBUTE_ID = 65532
-
-# WaterHeaterManagement.Commands.Boost
-ATTR_DURATION = "duration"
-ATTR_EMERGENCY_BOOST = "emergency_boost"
-ATTR_TEMPORARY_SETPOINT = "temporary_setpoint"
-SERVICE_WATER_HEATER_BOOST = "water_heater_boost"
