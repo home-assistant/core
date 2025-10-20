2a393ea867ca3997773a78dd81adee647a6e6f21
Michael Hansen <mike@rhasspy.org>
2025-10-20 13:57:44 -0500

    Cache wake words in hass data

--- homeassistant/components/esphome/assist_satellite.py
+++ homeassistant/components/esphome/assist_satellite.py
@@ -47,6 +47,7 @@ from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.network import get_url
+from homeassistant.util.hass_dict import HassKey
 
 from .const import DOMAIN, WAKE_WORDS_API_PATH, WAKE_WORDS_DIR_NAME
 from .entity import EsphomeAssistEntity, convert_api_error_ha_error
@@ -99,6 +100,9 @@ _WAKE_WORD_CONFIG_SCHEMA = vol.Schema(
     },
     extra=vol.ALLOW_EXTRA,
 )
+_DATA_WAKE_WORDS: HassKey[dict[str, VoiceAssistantExternalWakeWord]] = HassKey(
+    "wake_word_cache"
+)
 
 
 async def async_setup_entry(
@@ -146,7 +150,6 @@ class EsphomeAssistSatellite(
         self._active_pipeline_index = 0
 
         # id -> wake word info
-        self._external_wake_words: dict[str, VoiceAssistantExternalWakeWord] = {}
         self._wake_words_dir = Path(self.hass.config.path(WAKE_WORDS_DIR_NAME))
 
     def _get_entity_id(self, suffix: str) -> str | None:
@@ -200,62 +203,14 @@ class EsphomeAssistSatellite(
         # Ensure configuration is updated
         await self._update_satellite_config()
 
-    def _get_custom_wake_words(self) -> dict[str, VoiceAssistantExternalWakeWord]:
-        """Get a list of custom wake words."""
-        wake_words = {}
-        for config_path in self._wake_words_dir.glob("*.json"):
-            wake_word_id = config_path.stem
-            model_path = config_path.with_suffix(".tflite")
-            if not model_path.exists():
-                # Missing model file
-                continue
-
-            with open(config_path, encoding="utf-8") as config_file:
-                config_dict = json.load(config_file)
-                try:
-                    config = _WAKE_WORD_CONFIG_SCHEMA(config_dict)
-                except vol.Invalid as err:
-                    # Invalid config
-                    _LOGGER.debug(
-                        "Invalid wake word config: path=%s, error=%s",
-                        config_path,
-                        humanize_error(config_dict, err),
-                    )
-                    continue
-
-                with open(model_path, "rb") as model_file:
-                    model_hash = hashlib.sha256(model_file.read()).hexdigest()
-
-                model_size = model_path.stat().st_size
-                config_rel_path = config_path.relative_to(self._wake_words_dir)
-
-                # Only intended for the internal network
-                base_url = get_url(self.hass, prefer_external=False, allow_cloud=False)
-
-                wake_words[wake_word_id] = VoiceAssistantExternalWakeWord.from_dict(
-                    {
-                        "id": wake_word_id,
-                        "wake_word": config["wake_word"],
-                        "trained_languages": config_dict.get("trained_languages", []),
-                        "model_type": config["type"],
-                        "model_size": model_size,
-                        "model_hash": model_hash,
-                        "url": f"{base_url}{WAKE_WORDS_API_PATH}/{config_rel_path}",
-                    }
-                )
-
-        return wake_words
-
     async def _update_satellite_config(self) -> None:
         """Get the latest satellite configuration from the device."""
-        self._external_wake_words = await self.hass.async_add_executor_job(
-            self._get_custom_wake_words
-        )
+        wake_words = self.hass.data.get(_DATA_WAKE_WORDS, {})
 
         try:
             config = await self.cli.get_voice_assistant_configuration(
                 _CONFIG_TIMEOUT_SEC,
-                external_wake_words=list(self._external_wake_words.values()),
+                external_wake_words=list(wake_words.values()),
             )
         except TimeoutError:
             # Placeholder config will be used
@@ -859,11 +814,60 @@ class VoiceAssistantUDPServer(asyncio.DatagramProtocol):
 
 async def async_setup(hass: HomeAssistant) -> None:
     """Set up the satellite."""
+    wake_words_dir = Path(hass.config.path(WAKE_WORDS_DIR_NAME))
+    wake_words = hass.data.setdefault(_DATA_WAKE_WORDS, {})
+
+    def _cache_wake_words() -> None:
+        for config_path in wake_words_dir.glob("*.json"):
+            wake_word_id = config_path.stem
+            model_path = config_path.with_suffix(".tflite")
+            if not model_path.exists():
+                # Missing model file
+                continue
+
+            with open(config_path, encoding="utf-8") as config_file:
+                config_dict = json.load(config_file)
+                try:
+                    config = _WAKE_WORD_CONFIG_SCHEMA(config_dict)
+                except vol.Invalid as err:
+                    # Invalid config
+                    _LOGGER.debug(
+                        "Invalid wake word config: path=%s, error=%s",
+                        config_path,
+                        humanize_error(config_dict, err),
+                    )
+                    continue
+
+                with open(model_path, "rb") as model_file:
+                    model_hash = hashlib.sha256(model_file.read()).hexdigest()
+
+                model_size = model_path.stat().st_size
+                config_rel_path = config_path.relative_to(wake_words_dir)
+
+                # Only intended for the internal network
+                base_url = get_url(hass, prefer_external=False, allow_cloud=False)
+
+                wake_words[wake_word_id] = VoiceAssistantExternalWakeWord.from_dict(
+                    {
+                        "id": wake_word_id,
+                        "wake_word": config["wake_word"],
+                        "trained_languages": config_dict.get("trained_languages", []),
+                        "model_type": config["type"],
+                        "model_size": model_size,
+                        "model_hash": model_hash,
+                        "url": f"{base_url}{WAKE_WORDS_API_PATH}/{config_rel_path}",
+                    }
+                )
+
+    # Cache wake word models in config/custom_wake_words
+    await hass.async_add_executor_job(_cache_wake_words)
+
+    # Satellites will pull model files over HTTP
     await hass.http.async_register_static_paths(
         [
             StaticPathConfig(
                 url_path=WAKE_WORDS_API_PATH,
-                path=hass.config.path(WAKE_WORDS_DIR_NAME),
+                path=str(wake_words_dir),
             )
         ]
     )
