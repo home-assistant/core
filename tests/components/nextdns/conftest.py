bedefa8d3916509861b644aa673e02e29e686152
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-10-20 22:01:10 +0200

    Move data to fixtures

--- tests/components/nextdns/conftest.py
+++ tests/components/nextdns/conftest.py
@@ -18,102 +18,23 @@ import pytest
 from homeassistant.components.nextdns.const import CONF_PROFILE_ID, DOMAIN
 from homeassistant.const import CONF_API_KEY
 
-from tests.common import MockConfigEntry
+from tests.common import (
+    MockConfigEntry,
+    load_json_array_fixture,
+    load_json_object_fixture,
+)
 
-ANALYTICS_DNSSEC = AnalyticsDnssec(not_validated_queries=25, validated_queries=75)
-ANALYTICS_ENCRYPTION = AnalyticsEncryption(encrypted_queries=60, unencrypted_queries=40)
-ANALYTICS_IP_VERSIONS = AnalyticsIpVersions(ipv4_queries=90, ipv6_queries=10)
-ANALYTICS_PROTOCOLS = AnalyticsProtocols(
-    doh_queries=20,
-    doh3_queries=15,
-    doq_queries=10,
-    dot_queries=30,
-    tcp_queries=0,
-    udp_queries=40,
+ANALYTICS = load_json_object_fixture("analytics.json", DOMAIN)
+ANALYTICS_DNSSEC = AnalyticsDnssec(**ANALYTICS["dnssec"])
+ANALYTICS_ENCRYPTION = AnalyticsEncryption(**ANALYTICS["encryption"])
+ANALYTICS_IP_VERSIONS = AnalyticsIpVersions(**ANALYTICS["ip_versions"])
+ANALYTICS_PROTOCOLS = AnalyticsProtocols(**ANALYTICS["protocols"])
+ANALYTICS_STATUS = AnalyticsStatus(**ANALYTICS["status"])
+CONNECTION_STATUS = ConnectionStatus(
+    **load_json_object_fixture("connection_status.json", DOMAIN)
 )
-ANALYTICS_STATUS = AnalyticsStatus(
-    default_queries=40, allowed_queries=30, blocked_queries=20, relayed_queries=10
-)
-CONNECTION_STATUS = ConnectionStatus(connected=True, profile_id="abcdef")
-PROFILES = [{"id": "xyz12", "fingerprint": "aabbccdd123", "name": "Fake Profile"}]
-SETTINGS = Settings(
-    ai_threat_detection=True,
-    allow_affiliate=True,
-    anonymized_ecs=True,
-    bav=True,
-    block_bypass_methods=True,
-    block_csam=True,
-    block_ddns=True,
-    block_disguised_trackers=True,
-    block_nrd=True,
-    block_page=False,
-    block_parked_domains=True,
-    cache_boost=True,
-    cname_flattening=True,
-    cryptojacking_protection=True,
-    dga_protection=True,
-    dns_rebinding_protection=True,
-    google_safe_browsing=False,
-    idn_homograph_attacks_protection=True,
-    logs=True,
-    logs_location="ch",
-    logs_retention=720,
-    safesearch=False,
-    threat_intelligence_feeds=True,
-    typosquatting_protection=True,
-    web3=True,
-    youtube_restricted_mode=False,
-    block_9gag=True,
-    block_amazon=True,
-    block_bereal=True,
-    block_blizzard=True,
-    block_chatgpt=True,
-    block_dailymotion=True,
-    block_discord=True,
-    block_disneyplus=True,
-    block_ebay=True,
-    block_facebook=True,
-    block_fortnite=True,
-    block_google_chat=True,
-    block_hbomax=True,
-    block_hulu=True,
-    block_imgur=True,
-    block_instagram=True,
-    block_leagueoflegends=True,
-    block_mastodon=True,
-    block_messenger=True,
-    block_minecraft=True,
-    block_netflix=True,
-    block_pinterest=True,
-    block_playstation_network=True,
-    block_primevideo=True,
-    block_reddit=True,
-    block_roblox=True,
-    block_signal=True,
-    block_skype=True,
-    block_snapchat=True,
-    block_spotify=True,
-    block_steam=True,
-    block_telegram=True,
-    block_tiktok=True,
-    block_tinder=True,
-    block_tumblr=True,
-    block_twitch=True,
-    block_twitter=True,
-    block_vimeo=True,
-    block_vk=True,
-    block_whatsapp=True,
-    block_xboxlive=True,
-    block_youtube=True,
-    block_zoom=True,
-    block_dating=True,
-    block_gambling=True,
-    block_online_gaming=True,
-    block_piracy=True,
-    block_porn=True,
-    block_social_networks=True,
-    block_video_streaming=True,
-)
+PROFILES = load_json_array_fixture("profiles.json", DOMAIN)
+SETTINGS = Settings(**load_json_object_fixture("settings.json", DOMAIN))
 
 
 @pytest.fixture
@@ -161,8 +82,6 @@ def mock_nextdns_client() -> Generator[AsyncMock]:
         client.get_profiles.return_value = PROFILES
         client.get_settings.return_value = SETTINGS
         client.set_setting.return_value = True
-        client.profiles = [
-            ProfileInfo(id="xyz12", fingerprint="fake_profile", name="Fake Profile")
-        ]
+        client.profiles = [ProfileInfo(**PROFILES[0])]
 
         yield client
