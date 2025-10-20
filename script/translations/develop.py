c006bc6b28f64a054ab3044257b24c2ae7c7842e
puddly <32534428+puddly@users.noreply.github.com>
2025-10-20 11:14:25 -0400

    Check translations in CI

--- script/translations/develop.py
+++ script/translations/develop.py
@@ -10,6 +10,10 @@ from .const import INTEGRATIONS_DIR
 from .util import flatten_translations, get_base_arg_parser, substitute_references
 
 
+class InvalidSubstitutionKey(Exception):
+    """Exception for invalid substitution keys."""
+
+
 def valid_integration(integration):
     """Test if it's a valid integration."""
     if not (INTEGRATIONS_DIR / integration).is_dir():
@@ -28,6 +32,47 @@ def get_arguments() -> argparse.Namespace:
     )
     parser.add_argument("--all", action="store_true", help="Process all integrations.")
     return parser.parse_args()
+
+
+def substitute_translation_references(integration_strings, flattened_translations):
+    """Recursively processes all translation strings for the integration."""
+    result = {}
+    for key, value in integration_strings.items():
+        if isinstance(value, dict):
+            sub_dict = substitute_translation_references(value, flattened_translations)
+            result[key] = sub_dict
+        elif isinstance(value, str):
+            try:
+                result[key] = substitute_reference(value, flattened_translations)
+            except InvalidSubstitutionKey as err:
+                print(f"Error: {err}")
+                sys.exit(1)
+
+    return result
+
+
+def substitute_reference(value, flattened_translations):
+    """Substitute localization key references in a translation string."""
+    matches = re.findall(r"\[\%key:([a-z0-9_]+(?:::(?:[a-z0-9-_])+)+)\%\]", value)
+    if not matches:
+        return value
+
+    new = value
+    for key in matches:
+        if key in flattened_translations:
+            new = new.replace(
+                f"[%key:{key}%]",
+                # New value can also be a substitution reference
+                substitute_reference(
+                    flattened_translations[key], flattened_translations
+                ),
+            )
+        else:
+            raise InvalidSubstitutionKey(
+                f"Invalid substitution key '{key}' found in string '{value}'"
+            )
+
+    return new
 
 
 def run_single(translations, flattened_translations, integration):
