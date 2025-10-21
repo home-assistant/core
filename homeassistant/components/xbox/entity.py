3bd235485b8758510730c47ec37d421b7dd486d8
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-10-21 14:32:02 +0000

    Refactor media player entity

--- homeassistant/components/xbox/entity.py
+++ homeassistant/components/xbox/entity.py
@@ -2,12 +2,23 @@
 
 from __future__ import annotations
 
+from xbox.webapi.api.provider.smartglass.models import ConsoleType, SmartglassConsole
+
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
-from .coordinator import Person, XboxUpdateCoordinator
+from .coordinator import ConsoleData, Person, XboxUpdateCoordinator
+
+MAP_MODEL = {
+    ConsoleType.XboxOne: "Xbox One",
+    ConsoleType.XboxOneS: "Xbox One S",
+    ConsoleType.XboxOneSDigital: "Xbox One S All-Digital",
+    ConsoleType.XboxOneX: "Xbox One X",
+    ConsoleType.XboxSeriesS: "Xbox Series S",
+    ConsoleType.XboxSeriesX: "Xbox Series X",
+}
 
 
 class XboxBaseEntity(CoordinatorEntity[XboxUpdateCoordinator]):
@@ -21,7 +32,7 @@ class XboxBaseEntity(CoordinatorEntity[XboxUpdateCoordinator]):
         xuid: str,
         entity_description: EntityDescription,
     ) -> None:
-        """Initialize Xbox binary sensor."""
+        """Initialize Xbox entity."""
         super().__init__(coordinator)
         self.xuid = xuid
         self.entity_description = entity_description
@@ -40,3 +51,35 @@ class XboxBaseEntity(CoordinatorEntity[XboxUpdateCoordinator]):
     def data(self) -> Person:
         """Return coordinator data for this console."""
         return self.coordinator.data.presence[self.xuid]
+
+
+class XboxConsoleBaseEntity(CoordinatorEntity[XboxUpdateCoordinator]):
+    """Console base entity for the Xbox integration."""
+
+    _attr_has_entity_name = True
+
+    def __init__(
+        self,
+        console: SmartglassConsole,
+        coordinator: XboxUpdateCoordinator,
+    ) -> None:
+        """Initialize the Xbox Console entity."""
+
+        super().__init__(coordinator)
+        self.client = coordinator.client
+        self._console = console
+
+        self._attr_name = None
+        self._attr_unique_id = console.id
+
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, console.id)},
+            manufacturer="Microsoft Corporation",
+            model=MAP_MODEL.get(self._console.console_type, "Unknown"),
+            name=console.name,
+        )
+
+    @property
+    def data(self) -> ConsoleData:
+        """Return coordinator data for this console."""
+        return self.coordinator.data.consoles[self._console.id]
